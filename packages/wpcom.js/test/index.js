5b83625a2223ab9f26ec06a1c1b009e8ec8bd418
Damian Suarez <rdsuarez@gmail.com>
2014-03-17 23:06:44 -0300

    test: add client id test

--- packages/wpcom.js/test/index.js
+++ packages/wpcom.js/test/index.js
@@ -12,11 +12,29 @@ var WPCONN = require('../');
 var pkg = require('../package.json');
 
 /**
- * Start tests
+ * Testing data
+ */
+
+var wpapp = require('./data');
+
+/**
+ * WPCONN instance
+ */
+
+var wpconn = new WPCONN();
+
+/**
+ * Sync tests
  */
 
 describe('version', function(){
   it('should have x.x.x format', function(){
     pkg.version.should.match(/\d\.\d\.\d/);
+  });
+});
+
+describe('client id', function(){
+  it('should be a string of numbers', function(){
+    (Number(wpapp.client_id)).should.be.a.Number;
   });
 });
