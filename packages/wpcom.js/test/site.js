dd8063b36e3a05924209e0bdfce04d80437981aa
Damian Suarez <rdsuarez@gmail.com>
2014-03-18 16:21:11 -0300

    test: add some site tests

--- packages/wpcom.js/test/site.js
+++ packages/wpcom.js/test/site.js
@@ -26,12 +26,28 @@ var wpconn = new WPCONN();
 describe('site', function(){
   it('should be an instance of `Site`', function(){
     var wpconn = new WPCONN();
-
-    wpconn.site
-      .should.be.an.instanceOf(Site);
+    wpconn.site.should.be.an.instanceOf(Site);
   });
-});
 
-describe('get site info', function(){
-  
+  it('should be set site identifier', function(){
+    var wpconn = new WPCONN();
+    var id = 'koke.me';
+    wpconn.site.setId('koke.me');
+
+    wpconn.site.id
+      .should.be.eql('koke.me');
+  });
+
+  it('should require site data', function(done){
+    var wpconn = new WPCONN();
+    var site = wpconn.site;
+
+    site.setId('koke.me');
+
+    site.get(function(err, data){
+      if (err) throw err;
+
+      done();
+    });
+  });
 });
