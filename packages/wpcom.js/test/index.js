71a24e3cef396363a6d6f8eafb2c68d721595e5d
Damian Suarez <rdsuarez@gmail.com>
2014-03-18 11:21:34 -0300

    test: refact -> add getInstance useful function

--- packages/wpcom.js/test/index.js
+++ packages/wpcom.js/test/index.js
@@ -78,17 +78,36 @@ describe('WPCONN object', function(){
 
 describe('me', function(){
   it('should get user object', function(done){
-    var wpconn = new WPCONN();
-    var token = wpapp.token;
-    wpconn.setToken(token);
+    var wpconn = getInstance();
 
     wpconn.me(function(err, me){
       if (err) throw err;
 
+      // testing object
       me.should.be.ok
       .and.an.instanceOf(Object);
+
+      // testing user data
+      me.ID
+        .should.be.an.instanceOf(Number);
 
       done();
     });
   });
 });
+
+/**
+ * Testing useful function
+ */
+
+/**
+ * Return a WPCONN instance
+ */
+
+function getInstance(){
+  var wpconn = new WPCONN();
+  var token = wpapp.token;
+  wpconn.setToken(token);
+
+  return wpconn;
+}
