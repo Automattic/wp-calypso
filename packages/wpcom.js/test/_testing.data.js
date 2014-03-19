fa1f7c806ff9599abbd00ab7c2e01957bd53c714
Damian Suarez <rdsuarez@gmail.com>
2014-03-19 11:40:34 -0300

    test: tidy testing data tests

--- packages/wpcom.js/test/_testing.data.js
+++ packages/wpcom.js/test/_testing.data.js
@@ -22,47 +22,49 @@ var tdata = require('./data');
  * Sync tests
  */
 
-describe('package', function(){
-  it('version should have x.x.x format', function(){
-    pkg.version.should.match(/\d\.\d\.\d/);
-  });
-});
-
 describe('testing data', function(){
-  it('`client id` should be a string of numbers', function(){
-    (Number(tdata.client_id)).should.be.a.Number;
+  describe('package', function(){
+    it('version should have x.x.x format', function(){
+      pkg.version.should.match(/\d\.\d\.\d/);
+    });
   });
 
-  it('`client secret` length and type', function(){
-    tdata.client_secret
-      .should.be.an.instanceOf(String)
-      .and.length(64);
+  describe('data', function(){
+    it('`client id` should be a string of numbers', function(){
+      (Number(tdata.client_id)).should.be.a.Number;
+    });
+
+    it('`client secret` length and type', function(){
+      tdata.client_secret
+        .should.be.an.instanceOf(String)
+        .and.length(64);
+    });
+
+    it('`token` should be a String', function(){
+      tdata.token
+        .should.be.an.instanceOf(String);
+    });
+
+    it('`site` should be defined', function(){
+      tdata.site
+        .should.be.ok
+        .and.an.instanceOf(String);
+    });
   });
 
-  it('`token` should be a String', function(){
-    tdata.token
-      .should.be.an.instanceOf(String);
-  });
+  describe('util', function(){
+    it('should create a wpconn instance', function(){
+      var wpconn = util.wpconn();
+      wpconn.should.be.an.instanceOf(WPCONN);
+    });
 
-  it('`site` should be defined', function(){
-    tdata.site
-      .should.be.ok
-      .and.an.instanceOf(String);
-  });
-});
+    it('should create a wpconn instance setting site `id`', function(){
+      var wpconn = util.site();
+      wpconn
+        .should.be.an.instanceOf(WPCONN);
 
-describe('util', function(){
-  it('should create a wpconn instance', function(){
-    var wpconn = util.wpconn();
-    wpconn.should.be.an.instanceOf(WPCONN);
-  });
-
-  it('should create a wpconn instance setting site `id`', function(){
-    var wpconn = util.site();
-    wpconn
-      .should.be.an.instanceOf(WPCONN);
-
-    wpconn.site.id
-      .should.be.eql(tdata.site);
+      wpconn.site.id
+        .should.be.eql(tdata.site);
+    });
   });
 });
