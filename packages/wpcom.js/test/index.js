6fbb6011d2e0bfdd53cf78c8e86549153f479bce
Damian Suarez <rdsuarez@gmail.com>
2014-03-19 09:10:43 -0300

    test: remove testing data test

--- packages/wpcom.js/test/index.js
+++ packages/wpcom.js/test/index.js
@@ -6,43 +6,10 @@
 var WPCONN = require('../');
 
 /**
- * Package
- */
-
-var pkg = require('../package.json');
-
-/**
  * Testing data
  */
 
 var wpapp = require('./data');
-
-/**
- * Sync tests
- */
-
-describe('package', function(){
-  it('version should have x.x.x format', function(){
-    pkg.version.should.match(/\d\.\d\.\d/);
-  });
-});
-
-describe('testing data', function(){
-  it('`client id` should be a string of numbers', function(){
-    (Number(wpapp.client_id)).should.be.a.Number;
-  });
-
-  it('`client secret` length and type', function(){
-    wpapp.client_secret
-      .should.be.an.instanceOf(String)
-      .and.length(64);
-  });
-
-  it('`token` should be a String', function(){
-    wpapp.token
-      .should.be.an.instanceOf(String);
-  });
-});
 
 /**
  * WPCONN object
@@ -121,45 +88,6 @@ describe('posts', function(){
 
         list.posts
           .should.be.an.instanceOf(Array);
-
-        done();
-      });
-    });
-  });
-});
-
-/**
- * ADD POST
- */
-
-describe('post.add', function(){
-  it('should add a new post', function(done){
-    var wpconn = getInstance();
-
-    wpconn.me(function(err, me){
-      if (err) throw err;
-
-      // testing user token site id
-      var token_site = me.token_site_id;
-      var date = Date.parse(new Date().toString());
-
-      var new_post = {
-        "date": date,
-        "title": "A new post blog from testing",
-        "content": "<div>The content</div>"
-      };
-
-      wpconn.post.add(new_post, token_site, function(err, post){
-        if (err) throw err;
-
-        // testing object post data
-        post.site_ID
-          .should.be.an.instanceOf(Number)
-          .and.be.eql(token_site);
-
-        // crossing data between user and post
-        post.author.ID
-          .should.be.eql(me.ID);
 
         done();
       });
