8b428755f2fdc77ae34a1b009a68374d0722a467
Damian Suarez <rdsuarez@gmail.com>
2014-03-19 09:10:28 -0300

    test: add test for testing data

---
+++ packages/wpcom.js/test/testing_data.js
@@ -0,0 +1,39 @@
+
+/**
+ * Package
+ */
+
+var pkg = require('../package.json');
+
+/**
+ * Testing data
+ */
+
+var tdata = require('./data');
+
+/**
+ * Sync tests
+ */
+
+describe('package', function(){
+  it('version should have x.x.x format', function(){
+    pkg.version.should.match(/\d\.\d\.\d/);
+  });
+});
+
+describe('testing data', function(){
+  it('`client id` should be a string of numbers', function(){
+    (Number(tdata.client_id)).should.be.a.Number;
+  });
+
+  it('`client secret` length and type', function(){
+    tdata.client_secret
+      .should.be.an.instanceOf(String)
+      .and.length(64);
+  });
+
+  it('`token` should be a String', function(){
+    tdata.token
+      .should.be.an.instanceOf(String);
+  });
+});
