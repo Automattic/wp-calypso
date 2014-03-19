f384541b78641ddf5eecb351b21c013695be7b0d
Damian Suarez <rdsuarez@gmail.com>
2014-03-19 10:22:27 -0300

    test: use util instead of private function

--- packages/wpcom.js/test/index.js
+++ packages/wpcom.js/test/index.js
@@ -1,4 +1,3 @@
-
 /**
  * WPCONN module
  */
@@ -9,7 +8,8 @@ var WPCONN = require('../');
  * Testing data
  */
 
-var wpapp = require('./data');
+var tdata = require('./data');
+var util = require('./util');
 
 /**
  * WPCONN object
@@ -23,7 +23,7 @@ describe('WPCONN', function(){
 
   it('should set the token', function(){
     var wpconn = new WPCONN();
-    var token = wpapp.token;
+    var token = tdata.token;
     wpconn.setToken(token);
 
     wpconn.opts.token
@@ -39,7 +39,7 @@ describe('WPCONN', function(){
 
 describe('me', function(){
   it('should require user object', function(done){
-    var wpconn = getInstance();
+    var wpconn = util.wpconn();
 
     wpconn.me(function(err, me){
       if (err) throw err;
@@ -64,7 +64,7 @@ describe('me', function(){
 
 describe('posts', function(){
   it('should request posts list', function(done){
-    var wpconn = getInstance();
+    var wpconn = util.wpconn();
 
     wpconn.me(function(err, me){
       if (err) throw err;
@@ -72,7 +72,7 @@ describe('posts', function(){
       // testing user token site id
       var token_site = me.token_site_id;
 
-      // me object data testing
+      // `me` object data testing
       token_site.should.be.an.instanceOf(Number);
 
       wpconn.posts(token_site, function(err, list){
@@ -94,19 +94,3 @@ describe('posts', function(){
     });
   });
 });
-
-/**
- * Testing useful function
- */
-
-/**
- * Return a WPCONN instance
- */
-
-function getInstance(){
-  var wpconn = new WPCONN();
-  var token = wpapp.token;
-  wpconn.setToken(token);
-
-  return wpconn;
-}
