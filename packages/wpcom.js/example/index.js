9fd3c9164baa71f7f72a6f0dba36ee632608a2d9
Damian Suarez <rdsuarez@gmail.com>
2014-03-19 22:15:25 -0300

    core: get posts and info from blog

--- packages/wpcom.js/example/index.js
+++ packages/wpcom.js/example/index.js
@@ -12,6 +12,15 @@ var WPCONN = require('../');
 
 var wpapp = require('../test/data');
 
+/**
+ * Create a WPCONN instance
+ */
+
+var wpconn = WPCONN();
+
+// set token app
+wpconn.token(wpapp.token);
+
 // Path to our public directory
 
 var pub = __dirname + '/public';
@@ -26,7 +35,20 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
-  res.render('layout');
+  // set site id
+  wpconn.site.id(wpapp.public_site);
+
+  // get site info
+  wpconn.site.info(function(err, site){
+    if (err) return console.log(err);
+
+    // get lastest post
+    wpconn.site.posts({ number: 10 }, function(err, posts) {
+      if (err) return console.log(err);
+
+      res.render('layout', { site: site, posts: posts });
+    });
+  });
 });
 
 app.listen(3000);
