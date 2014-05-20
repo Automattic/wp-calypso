dd5839bd1992c4b3990597116a7cc54c624ca9a9
Nathan Rajlich <nathan@tootallnate.net>
2014-05-20 15:35:05 -0700

    examples: a few tweaks

--- packages/wpcom.js/examples/node/freshlyPressed.js
+++ packages/wpcom.js/examples/node/freshlyPressed.js
@@ -4,7 +4,10 @@ var WPCOM = require('../../');
 // anonymous auth since `freshlyPressed()` doesn't require auth
 var wpcom = WPCOM();
 
-wpcom.freshlyPressed(function (err, res) {
+wpcom.freshlyPressed(function (err, data) {
   if (err) throw err;
-  console.log(res);
+  console.log('Freshly Pressed Posts:');
+  data.posts.forEach(function (post) {
+    console.log('  %s - %s', post.title, post.short_URL);
+  });
 });
