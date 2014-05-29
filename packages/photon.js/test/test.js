d205dc510abe313be12937e7c64d855f28789667
Nathan Rajlich <nathan@tootallnate.net>
2014-05-28 20:20:20 -0700

    test: initial tests

---
+++ packages/photon.js/test/test.js
@@ -0,0 +1,14 @@
+
+/**
+ * Module dependencies.
+ */
+
+var photon = require('../');
+var assert = require('assert');
+
+describe('photon()', function () {
+
+  it('should be a "function"', function () {
+    assert('function' === typeof photon);
+  });
+});
