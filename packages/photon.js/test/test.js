4adfca48552e6d123a61b87f261efb9df9df9c1a
Nathan Rajlich <nathan@tootallnate.net>
2014-06-02 13:54:45 -0700

    test: add another test

--- packages/photon.js/test/test.js
+++ packages/photon.js/test/test.js
@@ -12,6 +12,11 @@ describe('photon()', function () {
     assert('function' === typeof photon);
   });
 
+  it('should Photon-ify a basic image URL', function () {
+    var url = 'http://example.com/image.png';
+    assert(RegExp('https://i[0-2].wp.com/example.com/image.png').test(photon(url)));
+  });
+
   it('should not Photon-ify a Photon URL', function () {
     var url = 'https://i1.wp.com/www.gravatar.com/avatar/693307b4e0cb9366f34862c9dfacd7fc';
     assert(photon(url) === url);
