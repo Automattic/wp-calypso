1d19c1d030696bdfbf660aa7899a01107ea686e0
Nathan Rajlich <nathan@tootallnate.net>
2014-06-24 18:44:25 -0700

    index: inline the `sendRequest()` function
    I don't like "util" libs.

--- packages/wpcom.js/index.js
+++ packages/wpcom.js/index.js
@@ -5,7 +5,6 @@
 
 var Me = require('./lib/me');
 var Site = require('./lib/site');
-var utilrequest = require('./lib/util/request');
 var debug = require('debug')('wpcom');
 
 /**
@@ -57,10 +56,48 @@ WPCOM.prototype.freshlyPressed = function(query, fn){
 };
 
 /**
- * Expose util/request in sendRequest
+ * Request to WordPress REST API
+ *
+ * @param {String|Object} params
+ * @param {Object} [query]
+ * @param {Object} [body]
+ * @param {Function} fn
+ * @api private
  */
 
-WPCOM.prototype.sendRequest = utilrequest;
+WPCOM.prototype.sendRequest = function (params, query, body, fn){
+  // `params` can be just the path (String)
+  if ('string' == typeof params) {
+    params = { path: params };
+  }
+
+  debug('sendRequest(%o)', params.path);
+
+  // set `method` request param
+  params.method = (params.method || 'get').toUpperCase();
+
+  // `query` is optional
+  if ('function' == typeof query) {
+    fn = query;
+    query = null;
+  }
+
+  // `body` is optional
+  if ('function' == typeof body) {
+    fn = body;
+    body = null;
+  }
+
+  // pass `query` and/or `body` to request params
+  if (query) params.query = query;
+  if (body) params.body = body;
+
+  // callback `fn` function is optional
+  if (!fn) fn = function(err){ if (err) throw err; };
+
+  // request method
+  this.request(params, fn);
+};
 
 /**
  * Expose `WPCOM` module
