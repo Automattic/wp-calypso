957960b9f7292289969d73a850a2eb8735a1b284
Damián Suárez <rdsuarez@gmail.com>
2014-11-17 11:22:43 -0300

    Merge pull request #67 from Automattic/fix/index-test
    test: remove WPCOM test

--- packages/wpcom.js/test/test.index.js
+++ packages/wpcom.js/test/test.index.js
@@ -21,11 +21,6 @@ describe('WPCOM', function(){
 
   describe('sync', function(){
 
-    it('should create a WPCOM object', function(){
-      var wpcom = new WPCOM();
-      assert.ok(wpcom instanceof WPCOM);
-    });
-
     it('should set the token', function(){
       var wpcom = new WPCOM(test.site.private.token);
       assert.equal('string', typeof wpcom.token);
