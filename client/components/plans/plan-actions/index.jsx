9dfbdc6e620fe82a794103cbd682141451f0070e
Teemu Sarapisto <tsarpf@gmail.com>
2015-10-22 18:21:18 +0300

    Free Trials: Add free trial text to existing user upgrade button

--- client/components/plans/plan-actions/index.jsx
+++ client/components/plans/plan-actions/index.jsx
@@ -164,7 +164,7 @@ module.exports = React.createClass( {
 				</button>
 
 				<small className="plan-actions__trial-period">
-					{ this.translate( 'Try it free for 14 days or', { context: 'Store action' } ) }&nbsp;
+					{ this.translate( 'Try it free for 14 days, no credit card needed, or', { context: 'Store action' } ) }&nbsp;
 					<a href='#'
 						onClick={ this.handleAddToCart.bind( null, this.cartItem( { isFreeTrial: false } ), 'link' ) }>
 						{ this.translate( 'upgrade now', { context: 'Store action' } ) }
