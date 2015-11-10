76c83731c41462225774e7f7927d461abe67dbdc
Enej Bajgoric <enej.bajgoric@gmail.com>
2015-11-10 14:43:12 -0800

    Plugin Action: Allows us to set a InfoPopup if the feature is disabled

--- client/my-sites/plugins/plugin-action/plugin-action.jsx
+++ client/my-sites/plugins/plugin-action/plugin-action.jsx
@@ -7,20 +7,50 @@ var React = require( 'react/addons' ),
 /**
  * Internal dependencies
  */
-var CompactToggle = require( 'components/forms/form-toggle/compact' );
+var CompactToggle = require( 'components/forms/form-toggle/compact' ),
+	InfoPopover = require( 'components/info-popover' );
 
 module.exports = React.createClass( {
 
 	displayName: 'PluginAction',
 
+	getInitialState() {
+		return {
+			showPopover: false,
+			previousClickOutside: false
+		};
+	},
+
+	handleAction: function() {
+		if ( ! this.props.disabledInfo ) {
+			this.props.action();
+		} else {
+			this.togglePopover( ! this.state.showPopover );
+		}
+	},
+
+	togglePopover: function( state ) {
+		this.setState( { showPopover: state } );
+	},
+
 	renderLabel: function( id ) {
 		if ( this.props.label ) {
-			return ( <label className="plugin-action__label" onClick={ this.props.action } htmlFor={ id }>{ this.props.label }</label> );
+			return ( <label className="plugin-action__label" onClick={ this.handleAction } htmlFor={ id }>{ this.props.label }</label> );
 		}
 		return null;
 	},
 
 	renderToggle: function( id ) {
+		if ( this.props.disabledInfo ) {
+			return (
+				<div className="plugin-action__disabled-info">
+					<InfoPopover position="bottom left" ref="infoPopover" isVisible={ this.state.showPopover } toggle={ this.togglePopover } >
+						{ this.props.disabledInfo }
+					</InfoPopover>
+					{ this.renderLabel( id ) }
+				</div>
+			);
+		}
 		return (
 			<CompactToggle
 				onChange={ this.props.action }
