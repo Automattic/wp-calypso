67a6ccad55d6540bfa94cde7c10df88d3d3d77dc
Enej Bajgoric <enej.bajgoric@gmail.com>
2015-11-10 14:46:23 -0800

    Plugin Autoupdate: Add disableInfo for time when we know that the autoupdates woun't work as expected

--- client/my-sites/plugins/plugin-autoupdate-toggle/index.jsx
+++ client/my-sites/plugins/plugin-autoupdate-toggle/index.jsx
@@ -40,23 +40,51 @@ module.exports = React.createClass( {
 		}
 	},
 
+	getDisabledInfo: function() {
+		if ( ! this.props.site ) { // we don't have enough info
+			return null;
+		}
+
+		if ( ! this.props.wporg ) {
+			return this.translate( 'This Plugin is not part of the WordPress.org plugin reposetory. We cannot verify its updates.' );
+		}
+
+		if ( ! this.props.site.canUpdateFiles ) {
+			return this.translate( '%(site)s doesn\'t have the right permission to update files.', {
+				args: { site: this.props.site.title }
+			} );
+		}
+
+		if ( this.props.site.options.file_mod_disabled ) {
+			return this.translate( '%(site)s doesn\'t have the ability to modify files.', {
+				args: { site: this.props.site.title }
+			} );
+		}
+
+		return null;
+	},
+
 	render: function() {
 		var inProgress = PluginsLog.isInProgressAction( this.props.site.ID, this.props.plugin.slug, [
-			'ENABLE_AUTOUPDATE_PLUGIN',
-			'DISABLE_AUTOUPDATE_PLUGIN'
-		] );
-
-		if ( this.props.site.canUpdateFiles && this.props.wporg ) {
-			return (
-				<PluginAction
-					label={ this.translate( 'Autoupdates', { context: 'this goes next to an icon that displays if the plugin has "autoupdates" active' } ) }
-					status={ this.props.plugin.autoupdate }
-					action={ this.toggleAutoupdates }
-					inProgress={ inProgress }
-					htmlFor={ 'autoupdates-' + this.props.plugin.slug + '-' + this.props.site.ID }
-				/>
-			);
-		}
-		return null;
+				'ENABLE_AUTOUPDATE_PLUGIN',
+				'DISABLE_AUTOUPDATE_PLUGIN'
+			] ),
+			getDisabledInfo = this.getDisabledInfo(),
+			label = getDisabledInfo
+				? this.translate( 'Autoupdates disabled', {
+					context: 'this goes next to an icon that displays if the plugin has "autoupdates disabled" active'
+				} )
+				: this.translate( 'Autoupdates', {
+					context: 'this goes next to an icon that displays if the plugin has "autoupdates" active'
+				} );
+		return (
+			<PluginAction
+				label={ label }
+				status={ this.props.plugin.autoupdate }
+				action={ this.toggleAutoupdates }
+				inProgress={ inProgress }
+				disabledInfo={ getDisabledInfo }
+				htmlFor={ 'autoupdates-' + this.props.plugin.slug + '-' + this.props.site.ID } />
+		);
 	}
 } );
