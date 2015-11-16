189ee34106659d1ee0bc57ac7df10cffe4be7c98
Enej Bajgoric <enej.bajgoric@gmail.com>
2015-11-16 15:12:53 -0800

    Plugin Autoupdates: Improve reasons for why the Jetpack autoupdates are disabled on a site

--- client/my-sites/plugins/plugin-autoupdate-toggle/index.jsx
+++ client/my-sites/plugins/plugin-autoupdate-toggle/index.jsx
@@ -9,6 +9,7 @@ var React = require( 'react' );
 var PluginsActions = require( 'lib/plugins/actions' ),
 	PluginsLog = require( 'lib/plugins/log-store' ),
 	PluginAction = require( 'my-sites/plugins/plugin-action/plugin-action' ),
+	ExternalLink = require( 'components/external-link' ),
 	analytics = require( 'analytics' );
 
 module.exports = React.createClass( {
@@ -46,26 +47,83 @@ module.exports = React.createClass( {
 		}
 
 		if ( ! this.props.wporg ) {
-			return this.translate( 'This Plugin is not part of the WordPress.org plugin reposetory. We cannot verify its updates.' );
+			return array( this.translate( 'This plugin is not in the WordPress.org plugin repository, so we can\'t autoupdate it.' ) );
 		}
 
-		if ( ! this.props.site.canUpdateFiles ) {
-			return this.translate( '%(site)s doesn\'t have the right permission to update files.', {
+		if ( ! this.props.site.hasMinimumJetpackVersion ) {
+			return this.translate( '%(site)s is not running an up to date version of Jetpack', {
 				args: { site: this.props.site.title }
 			} );
 		}
 
-		if ( this.props.site.options.file_mod_disabled ) {
-			return this.translate( '%(site)s doesn\'t have the ability to modify files.', {
+		if ( this.props.site.options.is_multi_network ) {
+			return this.translate( '%(site)s is part of a multi-network installation and is therefore not supported.', {
 				args: { site: this.props.site.title }
 			} );
 		}
 
+		if ( this.props.site.options.unmapped_url !== this.props.site.options.main_network_site ) {
+			return this.translate( 'Only the main site on a multi-site installation can enable autoupdates for plugins.', {
+				args: { site: this.props.site.title }
+			} );
+		}
+
+		let reasons = [];
+		if ( ! this.props.site.canUpdateFiles && this.props.site.options.file_mod_disabled ) {
+			for ( let clue of this.props.site.options.file_mod_disabled ) {
+				switch ( clue ) {
+					case 'is_version_controlled':
+						reasons.push(
+							this.translate( '%(site)s is under version control, so we can\'t autoupdate plugins.', {
+								args: { site: this.props.site.title }
+							} )
+						);
+						break;
+					case 'has_no_file_system_write_access':
+						reasons.push(
+							this.translate( 'The file access permissions on this server prevent us from autoupdating plugins.', {
+								args: { site: this.props.site.title }
+							} )
+						);
+						break;
+					case 'automatic_updater_disabled':
+						reasons.push(
+							this.translate( 'The administrator of %(site)s has explicitly disabled any autoupdates.', {
+								args: { site: this.props.site.title }
+							} )
+						);
+						break;
+					case 'wp_auto_update_core_disabled':
+						reasons.push(
+							this.translate( 'The administrator of %(site)s has explicitly disabled core autoupdates.', {
+								args: { site: this.props.site.title }
+							} )
+						);
+						break;
+					case 'disallow_file_edit':
+						reasons.push(
+							this.translate( 'The administrator of %(site)s has explicitly prevented file edits.', {
+								args: { site: this.props.site.title }
+							} )
+						);
+						break;
+					case 'disallow_file_mods':
+						reasons.push(
+							this.translate( 'The administrator of %(site)s has explicitly prevented file modifications.', {
+								args: { site: this.props.site.title }
+							} )
+						);
+						break;
+				}
+			}
+			reasons.push( <ExternalLink href="https://jetpack.me/support/site-management/#file-update-disabled" >{ this.translate( 'How to fix it?' ) }</ExternalLink> );
+			return reasons.map( ( reason, i ) => ( <p key={ i } > { reason }</p> ) );
+		}
 		return null;
 	},
 
 	render: function() {
-		var inProgress = PluginsLog.isInProgressAction( this.props.site.ID, this.props.plugin.slug, [
+		const inProgress = PluginsLog.isInProgressAction( this.props.site.ID, this.props.plugin.slug, [
 				'ENABLE_AUTOUPDATE_PLUGIN',
 				'DISABLE_AUTOUPDATE_PLUGIN'
 			] ),
@@ -77,6 +135,7 @@ module.exports = React.createClass( {
 				: this.translate( 'Autoupdates', {
 					context: 'this goes next to an icon that displays if the plugin has "autoupdates" active'
 				} );
+
 		return (
 			<PluginAction
 				label={ label }
