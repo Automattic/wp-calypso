d7caa93de78eb36da777b0b50e2fdb67d9cecafc
Eric Binnion <ericbinnion@gmail.com>
2015-11-19 11:12:48 -0600

    Me: ESLint fixes for profile links creators

--- client/me/profile-links/creators.jsx
+++ client/me/profile-links/creators.jsx
@@ -1,55 +1,44 @@
 /**
- * External dependencies
- */
- var React = require( 'react' ),
-	debug = require( 'debug' )( 'calypso:me:profile' );
+* External dependencies
+*/
+var React = require( 'react' );
 
 /**
- * Internal dependencies
- */
+* Internal dependencies
+*/
 
- var ProfileLinksAddWordPress = require( 'me/profile-links-add-wordpress' ),
-  ProfileLinksAddOther = require( 'me/profile-links-add-other' ),
-  Card = require( 'components/card' );
-
+var ProfileLinksAddWordPress = require( 'me/profile-links-add-wordpress' ),
+	ProfileLinksAddOther = require( 'me/profile-links-add-other' );
 
 module.exports = React.createClass( {
 
-  displayName: 'ProfileLinksCreators',
+	displayName: 'ProfileLinksCreators',
 
-  getDefaultProps: function() {
-    return{
-      showingForm: false
-    };
-  },
+	getDefaultProps: function() {
+		return{
+			showingForm: false
+		};
+	},
 
-  render: function() {
-    return(
-      <div>
-        {
-          'wordpress' === this.props.showingForm
-          ? (
-            <ProfileLinksAddWordPress
-              userProfileLinks={ this.props.userProfileLinks }
-              onSuccess={ this.props.hideForms }
-              onCancel={ this.props.hideForms }
-            />
-          )
-          : null
-        }
+	render: function() {
+		return(
+			<div>
+				{ 'wordpress' === this.props.showingForm &&
+					<ProfileLinksAddWordPress
+						userProfileLinks={ this.props.userProfileLinks }
+						onSuccess={ this.props.hideForms }
+						onCancel={ this.props.hideForms }
+					/>
+				}
 
-        {
-          'other' === this.props.showingForm
-          ? (
-            <ProfileLinksAddOther
-              userProfileLinks={ this.props.userProfileLinks }
-              onSuccess={ this.props.hideForms }
-              onCancel={ this.props.hideForms }
-            />
-          )
-          : null
-        }
-        </div>
-    );
-  }
+				{ 'other' === this.props.showingForm &&
+					<ProfileLinksAddOther
+						userProfileLinks={ this.props.userProfileLinks }
+						onSuccess={ this.props.hideForms }
+						onCancel={ this.props.hideForms }
+					/>
+				}
+			</div>
+		);
+	}
 } );
