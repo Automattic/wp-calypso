1807c085dbe94c3ec0ea3f833ae1fd1e1480c7cb
Matías Ventura <mv@matiasventura.com>
2015-11-23 12:44:11 +0100

    Use core sidebar items for links. Add gridicons.

--- client/devdocs/sidebar.jsx
+++ client/devdocs/sidebar.jsx
@@ -6,7 +6,7 @@ import React from 'react/addons';
 /**
  * Internal dependencies
  */
-
+import Gridicon from 'components/gridicon';
 
 export default React.createClass( {
 
@@ -18,30 +18,38 @@ export default React.createClass( {
 		return (
 			<div className="wpcom-sidebar sidebar devdocs__sidebar">
 				<h1 className="devdocs__title">Calypso Docs</h1>
-				<ul className="devdocs__navigation">
+				<ul className="sidebar-menu">
 					<li className="devdocs__navigation-item">
+						<Gridicon icon="search" />
 						<a className="devdocs__sidebar-item" href="/devdocs">
-							Home
+							Search
 						</a>
 					</li>
 					<li className="devdocs__navigation-item">
+						<Gridicon icon="location" />
 						<a className="devdocs__sidebar-item" href="/devdocs/docs/guide/index.md">
 							The Calypso Guide
 						</a>
 					</li>
 					<li className="devdocs__navigation-item">
+						<Gridicon icon="pencil" />
+						<a className="devdocs__sidebar-item" href="/devdocs/CONTRIBUTING.md">
+							Contributing
+						</a>
+					</li>
+				</ul>
+				<h2 className="sidebar-heading">Live Docs</h2>
+				<ul className="sidebar-menu">
+					<li className="devdocs__navigation-item">
+						<Gridicon icon="layout-blocks" />
 						<a className="devdocs__sidebar-item" href="/devdocs/design">
 							UI Components
 						</a>
 					</li>
 					<li className="devdocs__navigation-item">
+						<Gridicon icon="types" />
 						<a className="devdocs__sidebar-item" href="/devdocs/docs/icons.md">
 							Icons
-						</a>
-					</li>
-					<li className="devdocs__navigation-item">
-						<a className="devdocs__sidebar-item" href="/devdocs/CONTRIBUTING.md">
-							Contributing
 						</a>
 					</li>
 				</ul>
