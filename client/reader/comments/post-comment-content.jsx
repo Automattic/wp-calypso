5ead4431fcac3408951e5ad6d1fcdb8dbb488b16
bluefuton <chris@bluefuton.com>
2015-11-24 12:08:26 +0000

    Reader comments: only trust comment content from the API

---
+++ client/reader/comments/post-comment-content.jsx
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react';
+import CommentConstants from 'lib/comment-store/constants';
+
+const PostCommentContent = React.createClass( {
+	propTypes: {
+		content: PropTypes.string.isRequired,
+		state: PropTypes.string.isRequired,
+	},
+
+	render() {
+		// Don't trust comment content unless it was provided by the API
+		if ( this.props.state !== CommentConstants.state.COMPLETE ) {
+			return <div className="comment__content">{ this.props.content }</div>;
+		}
+
+		return (
+			<div className="comment__content" dangerouslySetInnerHTML={{ __html: this.props.content }}>
+			</div>
+		);
+	}
+} );
+
+export default PostCommentContent;
