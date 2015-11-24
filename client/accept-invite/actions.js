a099f9dbc3b5aab2565788db8db5b6d9212a6005
Miguel Lezama <miguel@automattic.com>
2015-11-24 12:26:04 -0300

    Invites: Add invite message related actions

--- client/accept-invite/actions.js
+++ client/accept-invite/actions.js
@@ -1,7 +1,14 @@
 /**
+ * External dependencies
+ */
+import page from 'page';
+
+/**
  * Internal dependencies
  */
 import wpcom from 'lib/wp' ;
+import Dispatcher from 'dispatcher';
+import { DISPLAY_INVITE_ACCEPTED, DISPLAY_INVITE_ACCEPTED_DISMISS, DISPLAY_INVITE_DECLINED, DISPLAY_INVITE_DECLINED_DISMISS } from './invite-message/constants'
 
 export function createAccount( userData, callback ) {
 	return wpcom.undocumented().usersNew(
@@ -20,4 +27,28 @@ export function acceptInvite( invite, bearerToken, callback ) {
 		invite.invite_slug,
 		callback
 	);
+}
+
+export function displayInviteAccepted() {
+	Dispatcher.handleViewAction( {
+		type: DISPLAY_INVITE_ACCEPTED
+	} );
+}
+
+export function dismissInviteAccepted() {
+	Dispatcher.handleViewAction( {
+		type: DISPLAY_INVITE_ACCEPTED_DISMISS
+	} );
+}
+
+export function displayInviteDeclined() {
+	Dispatcher.handleViewAction( {
+		type: DISPLAY_INVITE_DECLINED
+	} );
+}
+
+export function dismissInviteDeclined() {
+	Dispatcher.handleViewAction( {
+		type: DISPLAY_INVITE_DECLINED_DISMISS
+	} );
 }
---
+++ client/accept-invite/invite-message/constants.js
@@ -0,0 +1,9 @@
+/**
+ * External dependencies
+ */
+import keyMirror from 'react/lib/keyMirror';
+
+export default keyMirror( {
+	DISPLAY_INVITE_ACCEPTED: null,
+	DISPLAY_INVITE_ACCEPTED_DISMISS: null
+} );
