a47b6738283bf0687b8759fea711123d904550ab
Andrew Duthie <andrew@andrewduthie.com>
2015-11-24 09:20:15 -0500

    Media: Render gallery placeholder while loading

--- client/post-editor/media-modal/gallery/preview.jsx
+++ client/post-editor/media-modal/gallery/preview.jsx
@@ -7,11 +7,11 @@ import React, { PropTypes } from 'react';
  * Internal dependencies
  */
 import SimpleNotice from 'notices/simple-notice';
-import GalleryShortcode from 'components/gallery-shortcode';
 import SegmentedControl from 'components/segmented-control';
 import SegmentedControlItem from 'components/segmented-control/item';
-import markup from '../markup';
 import EditorMediaModalGalleryEdit from './edit';
+import EditorMediaModalGalleryPreviewShortcode from './preview-shortcode';
+import EditorMediaModalGalleryPreviewIndividual from './preview-individual';
 
 export default React.createClass( {
 	displayName: 'EditorMediaModalGalleryPreview',
@@ -72,34 +72,17 @@ export default React.createClass( {
 			);
 		}
 
-		if ( settings && 'individual' === settings.type ) {
+		if ( 'individual' === settings.type ) {
 			return (
-				<div className="editor-media-modal-gallery__preview-individual">
-					<div className="editor-media-modal-gallery__preview-individual-content">
-						{ settings.items.map( ( item ) => {
-							const caption = markup.caption( item );
-
-							if ( null === caption ) {
-								return <div key={ item.ID } dangerouslySetInnerHTML={ { __html: markup.get( item ) } } />;
-							}
-
-							return React.cloneElement( caption, { key: item.ID } );
-						} ) }
-					</div>
-				</div>
+				<EditorMediaModalGalleryPreviewIndividual
+					items={ settings.items } />
 			);
 		}
 
 		return (
-			<GalleryShortcode
+			<EditorMediaModalGalleryPreviewShortcode
 				siteId={ site.ID }
-				items={ settings.items }
-				type={ settings.type }
-				columns={ settings.columns }
-				orderBy={ settings.orderBy }
-				link={ settings.link }
-				size={ settings.size }
-				className="editor-media-modal-gallery__preview-iframe" />
+				settings={ settings } />
 		);
 	},
 
