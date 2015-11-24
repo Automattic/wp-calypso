5d5030a31bdefd2147db41a7320a19f4ef5f1715
John Godley <johng75@gmail.com>
2015-11-24 10:06:43 +0000

    Globally check for invalid oauth token
    We need to globally check for a bad oauth token. This indicates that the oauth connection was revoked, and so we should just log out.

--- client/lib/wpcom-xhr-wrapper/index.js
+++ client/lib/wpcom-xhr-wrapper/index.js
@@ -1,13 +1,29 @@
+/**
+ * External dependencies
+ */
 import xhr from 'wpcom-xhr-request';
+import debugModule from 'debug';
+
+/**
+ * Module variables
+ */
+const debug = debugModule( 'calypso:wpcom-xhr-wrapper' );
 
 export default function( params, callback ) {
 	return xhr( params, function( error, response ) {
-		if ( error && error.response && typeof error.response.body ) {
-			// Extend the error object in a way to match wpcom-proxy-request
-			error.httpMessage = error.message;
-			error.message = error.response.body.message;
-			error.error = error.response.body.error;
-			error.statusCode = error.status;
+		if ( error ) {
+			if ( error.response && typeof error.response.body ) {
+				// Extend the error object in a way to match wpcom-proxy-request
+				error.httpMessage = error.message;
+				error.message = error.response.body.message;
+				error.error = error.response.body.error;
+				error.statusCode = error.status;
+			}
+
+			if ( error.error === 'invalid_token' ) {
+				debug( 'Invalid token error detected, authorisation probably revoked - logging out' );
+				require( 'lib/user/utils' ).logout();
+			}
 		}
 
 		callback( error, response );
