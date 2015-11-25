3dab05afcfaba9cc231f3d7ee436fa060a9cd2a7
bluefuton <chris@bluefuton.com>
2015-11-25 14:51:14 +0000

    Import react/addons for PureRenderMixin

--- client/reader/comments/post-comment-content.jsx
+++ client/reader/comments/post-comment-content.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes } from 'react/addons';
 import CommentConstants from 'lib/comment-store/constants';
 
 const PostCommentContent = React.createClass( {
