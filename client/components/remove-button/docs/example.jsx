3b96c13229ddfa35c8ae42c711bedff482bbb4bc
Eric Binnion <ericbinnion@gmail.com>
2015-11-30 22:40:56 -0600

    Docs: Fixes formatting and simplifies remove buttons example

--- client/components/remove-button/docs/example.jsx
+++ client/components/remove-button/docs/example.jsx
@@ -33,70 +33,40 @@ var RemoveButtons = React.createClass( {
 	},
 
 	renderButtons: function() {
-		if ( ! this.state.compactButtons ) {
-			return (
-				<Card>
-					<div className="design-assets__button-row">
-						<RemoveButton>Remove</RemoveButton>
-						<RemoveButton icon="trash">Trash</RemoveButton>
-						<RemoveButton icon="delete">Delete</RemoveButton>
-            <RemoveButton icon="disconnect">Disconnect</RemoveButton>
-            <RemoveButton icon="deactivate">Deactivate</RemoveButton>
-						<RemoveButton disabled >Remove Disabled</RemoveButton>
-					</div>
-					<div className="design-assets__button-row">
-						<RemoveButton></RemoveButton>
-						<RemoveButton disabled ></RemoveButton>
-						<RemoveButton icon="trash"></RemoveButton>
-						<RemoveButton icon="disconnect"></RemoveButton>
-
-					</div>
-          <div className="design-assets__button-row">
-						<RemoveButton scary>Remove</RemoveButton>
-						<RemoveButton scary icon="trash">Trash</RemoveButton>
-						<RemoveButton scary icon="delete">Delete</RemoveButton>
-            <RemoveButton scary icon="disconnect">Disconnect</RemoveButton>
-            <RemoveButton scary icon="deactivate">Deactivate</RemoveButton>
-						<RemoveButton scary disabled >Remove Disabled</RemoveButton>
-					</div>
-					<div className="design-assets__button-row">
-						<RemoveButton scary></RemoveButton>
-						<RemoveButton scary disabled ></RemoveButton>
-						<RemoveButton scary icon="trash"></RemoveButton>
-            <RemoveButton scary icon="disconnect"></RemoveButton>
-					</div>
-				</Card>
-			);
-		} else {
-			return (
-				<Card>
-          <div className="design-assets__button-row">
-            <RemoveButton compact>Remove Compact</RemoveButton>
-            <RemoveButton compact icon="trash">Trash</RemoveButton>
-            <RemoveButton compact icon="delete">Delete</RemoveButton>
-            <RemoveButton compact icon="disconnect">Disconnect</RemoveButton>
-            <RemoveButton compact icon="deactivate">Deactivate</RemoveButton>
-            <RemoveButton compact disabled >Remove Disabled</RemoveButton>
-          </div>
-          <div className="design-assets__button-row">
-						<RemoveButton compact scary>Remove Scary Compact</RemoveButton>
-						<RemoveButton compact scary icon="trash">Trash</RemoveButton>
-						<RemoveButton compact scary icon="delete">Delete</RemoveButton>
-            <RemoveButton compact scary icon="disconnect">Disconnect</RemoveButton>
-            <RemoveButton compact scary icon="deactivate">Deactivate</RemoveButton>
-						<RemoveButton compact scary disabled >Remove Disabled</RemoveButton>
-					</div>
-					<div className="design-assets__button-row">
-						<RemoveButton compact scary></RemoveButton>
-						<RemoveButton compact scary icon="trash"></RemoveButton>
-						<RemoveButton compact scary icon="delete"></RemoveButton>
-						<RemoveButton compact scary icon="disconnect"></RemoveButton>
-						<RemoveButton compact scary icon="deactivate"></RemoveButton>
-						<RemoveButton compact scary disabled ></RemoveButton>
-					</div>
-				</Card>
-			);
-		}
+		return (
+			<Card>
+				<div className="design-assets__button-row">
+					<RemoveButton compact={ this.state.compactButtons }>
+						{ this.state.compactButtons ? 'Remove Compact' : 'Remove' }
+					</RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } icon="trash">Trash</RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } icon="delete">Delete</RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } icon="disconnect">Disconnect</RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } icon="deactivate">Deactivate</RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } disabled >Remove Disabled</RemoveButton>
+				</div>
+				<div className="design-assets__button-row">
+					<RemoveButton compact={ this.state.compactButtons }></RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } disabled ></RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } icon="trash"></RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } icon="disconnect"></RemoveButton>
+				</div>
+				<div className="design-assets__button-row">
+					<RemoveButton compact={ this.state.compactButtons } scary>Remove</RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } scary icon="trash">Trash</RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } scary icon="delete">Delete</RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } scary icon="disconnect">Disconnect</RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } scary icon="deactivate">Deactivate</RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } scary disabled >Remove Disabled</RemoveButton>
+				</div>
+				<div className="design-assets__button-row">
+					<RemoveButton compact={ this.state.compactButtons } scary></RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } scary disabled ></RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } scary icon="trash"></RemoveButton>
+					<RemoveButton compact={ this.state.compactButtons } scary icon="disconnect"></RemoveButton>
+				</div>
+			</Card>
+		);
 	},
 
 	toggleButtons: function() {
