b296d70481a929ddd003ec408a0169eeb60554f6
Radford Smith <contact@radsmith.com>
2015-12-01 13:46:28 +0100

    Domain Management: DNS: Fix bug where protocol wasn't being sent

--- client/my-sites/upgrades/domain-management/dns/srv-record.jsx
+++ client/my-sites/upgrades/domain-management/dns/srv-record.jsx
@@ -21,7 +21,8 @@ const SrvRecord = React.createClass( {
 			aux: 10,
 			weight: 10,
 			target: '',
-			port: ''
+			port: '',
+			protocol: 'tcp'
 		}
 	},
 
@@ -65,7 +66,9 @@ const SrvRecord = React.createClass( {
 				<FormFieldset>
 					<FormLabel>{ this.translate( 'Protocol', { context: 'Dns Record' } ) }</FormLabel>
 
-					<FormSelect onChange={ this.props.onChange( 'protocol' ) } value={ protocol }>
+					<FormSelect name="protocol"
+							onChange={ this.props.onChange( 'protocol' ) }
+							value={ protocol }>
 						{ options }
 					</FormSelect>
 				</FormFieldset>
