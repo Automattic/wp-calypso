ae6a26127eba800a05744cc2462111d4709daa45
artpi <artpi@post.pl>
2015-12-03 11:59:13 +0100

    SiteSettings: Add stopPropagation to SELECT onClick events
    onClick was propagating to label, triggering click events higher
    in DOM tree in Safari. Fixes #86

--- client/my-sites/site-settings/form-base.js
+++ client/my-sites/site-settings/form-base.js
@@ -62,6 +62,11 @@ module.exports = {
 
 	},
 
+	recordClickEventAndStop: function( recordObject, clickEvent ) {
+		this.recordEvent( recordObject );
+		clickEvent.preventDefault();
+	},
+
 	recordEvent: function( eventAction ) {
 		analytics.ga.recordEvent( 'Site Settings', eventAction );
 	},
