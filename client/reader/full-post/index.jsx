2b6be3a51a64a2214adef85086ad0686a4226ad8
Ben Lowery <ben.lowery@automattic.com>
2015-12-03 14:30:43 -0500

    Reader: Send article opened more aggressively than page views

--- client/reader/full-post/index.jsx
+++ client/reader/full-post/index.jsx
@@ -77,7 +77,6 @@ function readerPageView( blogId, blogUrl, postId, isPrivate ) {
 		params.priv = 1;
 	}
 	analytics.mc.bumpStatWithPageView( params );
-	analytics.tracks.recordEvent( 'calypso_reader_article_opened' );
 }
 
 /**
@@ -380,6 +379,8 @@ FullPostContainer = React.createClass( {
 			readerPageView( site.get( 'ID' ), site.get( 'URL' ), post.ID, site.get( 'is_private' ) );
 			this.hasSentPageView = true;
 		}
+
+		analytics.tracks.recordEvent( 'calypso_reader_article_opened' );
 	},
 
 	// Add change listeners to stores
