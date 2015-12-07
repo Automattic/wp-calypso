a37d6220141d199af40138f37153a27ca2bcd32b
Eric Binnion <ericbinnion@gmail.com>
2015-12-07 13:30:25 -0600

    Me: ESLint fixes for decodeUserSettingsEntities method of user-settings

--- client/lib/user-settings/index.js
+++ client/lib/user-settings/index.js
@@ -15,19 +15,20 @@ var Emitter = require( 'lib/mixins/emitter' ),
 	wpcom = require( 'lib/wp' ).undocumented(),
 	user = require( 'lib/user' )();
 
-/**
+/*
  * Decodes entities in those specific user settings properties
  * that the REST API returns already HTML-encoded
  */
 function decodeUserSettingsEntities( data ) {
 	let decodedValues = {
-			display_name: data.display_name && decodeEntities( data.display_name ),
-			description: data.description && decodeEntities( data.description ),
-			user_URL: data.user_URL && decodeEntities( data.user_URL )
+		display_name: data.display_name && decodeEntities( data.display_name ),
+		description: data.description && decodeEntities( data.description ),
+		user_URL: data.user_URL && decodeEntities( data.user_URL )
 	};
 
 	return assign( {}, data, decodedValues );
 }
+
 /**
  * Initialize UserSettings with defaults
  */
