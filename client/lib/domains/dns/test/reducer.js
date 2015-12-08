25b88f0013e3a9f0118a491bd49c3b8017e957ba
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2015-12-08 10:04:21 +0100

    Domains: Add unit tests for reducer and DNS delete action

---
+++ client/lib/domains/dns/test/reducer.js
@@ -0,0 +1,51 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { action as ActionTypes } from 'lib/upgrades/constants';
+import { DOMAIN_NAME, RECORD_A, RECORD_TXT } from 'data';
+import { reducer } from '../reducer';
+
+describe( 'Domains: DNS reducer', () => {
+	it( 'should return the same state when no matching record passed in the delete action', () => {
+		const state = {
+				[ DOMAIN_NAME ]: {
+					records: [ RECORD_A ]
+				}
+			},
+			payload = {
+				action: {
+					type: ActionTypes.DNS_DELETE,
+					domainName: DOMAIN_NAME,
+					record: RECORD_TXT
+				}
+			};
+
+		const result = reducer( state, payload );
+
+		expect( result ).to.be.eql( state );
+	} );
+
+	it( 'should return state without record passed in the delete action', () => {
+		const state = {
+				[ DOMAIN_NAME ]: {
+					records: [ RECORD_TXT ]
+				}
+			},
+			payload = {
+				action: {
+					type: ActionTypes.DNS_DELETE,
+					domainName: DOMAIN_NAME,
+					record: RECORD_TXT
+				}
+			};
+
+		const result = reducer( state, payload );
+
+		expect( result ).to.be.eql( { [ DOMAIN_NAME ]: { records: [] } } );
+	} );
+} );
