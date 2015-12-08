a3e42066e044a68a9149b94bb6832296acf4a23d
Shaun Andrews <shaun@automattic.com>
2015-12-08 01:41:37 -0500

    Adding a fade and icon overlay to the sidebar when its scrollable.

--- client/reader/sidebar/sidebar.jsx
+++ client/reader/sidebar/sidebar.jsx
@@ -80,6 +80,30 @@ module.exports = React.createClass( {
 		ReaderLists.on( 'change', this.updateState );
 		ReaderLists.on( 'create', this.highlightNewList );
 		ReaderTeams.on( 'change', this.updateState );
+		window.addEventListener( 'resize', this.checkSidebarScrollable );
+	},
+
+	checkSidebarScrollable: function() {
+		var windowHeight = window.innerHeight,
+			sidebar = React.findDOMNode( this.refs.sidebar ),
+			sidebarHeight = sidebar.scrollHeight;
+
+		if ( sidebarHeight > ( windowHeight ) ) {
+			this.setState( { isSidebarScrollable: true } );
+		} else {
+			this.setState( { isSidebarScrollable: false } );
+		}
+	},
+
+	checkSidebarScrolled: function() {
+		var sidebar = React.findDOMNode( this.refs.sidebar ),
+			isAtBottom = ( sidebar.scrollTop === ( sidebar.scrollHeight - sidebar.offsetHeight ) );
+
+		if ( isAtBottom ) {
+			this.setState( { isSidebarScrolled: true } );
+		} else {
+			this.setState( { isSidebarScrolled: false } );
+		}
 	},
 
 	componentWillUnmount: function() {
@@ -88,10 +112,15 @@ module.exports = React.createClass( {
 		ReaderLists.off( 'change', this.updateState );
 		ReaderLists.off( 'create', this.highlightNewList );
 		ReaderTeams.off( 'change', this.updateState );
+
+		window.removeEventListener( 'resize', this.adjustOverflowOverlay );
 	},
 
 	getInitialState: function() {
-		return this.getStateFromStores();
+		return assign( {
+			isSidebarScrollable: false,
+			isSidebarScrolled: false
+		}, this.getStateFromStores() );
 	},
 
 	getStateFromStores: function() {
@@ -112,6 +141,7 @@ module.exports = React.createClass( {
 
 	updateState: function() {
 		this.setState( this.getStateFromStores() );
+		this.checkSidebarScrollable();
 	},
 
 	followTag: function( event ) {
@@ -270,79 +300,90 @@ module.exports = React.createClass( {
 	render: function() {
 		let followingEditLink = this.getFollowingEditLink();
 
+		var containerClasses = classnames( {
+			'is-scrollable': this.state.isSidebarScrollable,
+			'is-scrolled': this.state.isSidebarScrolled
+		}, 'wp-sidebar-container' );
+
+		var sidebarClasses = classnames( 'wpcom-sidebar', 'sidebar', 'reader-sidebar' );
+
 		return (
-			<ul className="wpcom-sidebar sidebar reader-sidebar" onClick={ this.handleClick }>
-				<li className="sidebar-menu sidebar-streams">
-					<h2 className="sidebar-heading">{ this.translate( 'Streams' ) }</h2>
-					<ul>
-						<li className={ this.itemLinkClass( '/', { 'sidebar-streams__following': true } ) }>
-							<a href="/">
-								<Gridicon icon="checkmark-circle" size={ 24 } />
-								<span className="menu-link-text">{ this.translate( 'Followed Sites' ) }</span>
-							</a>
-							<a href={ followingEditLink.url } rel={ followingEditLink.rel } className="add-new">{ this.translate( 'Manage' ) }</a>
-						</li>
+			<div className={ containerClasses }>
+				<ul id="wp-sidebar" className={ sidebarClasses } onClick={ this.handleClick } onScroll={ this.checkSidebarScrolled } ref="sidebar">
+					<li className="sidebar-menu sidebar-streams">
+						<h2 className="sidebar-heading">{ this.translate( 'Streams' ) }</h2>
+						<ul>
+							<li className={ this.itemLinkClass( '/', { 'sidebar-streams__following': true } ) }>
+								<a href="/">
+									<Gridicon icon="checkmark-circle" size={ 24 } />
+									<span className="menu-link-text">{ this.translate( 'Followed Sites' ) }</span>
+								</a>
+								<a href={ followingEditLink.url } rel={ followingEditLink.rel } className="add-new">{ this.translate( 'Manage' ) }</a>
+							</li>
 
-						{ this.renderTeams() }
+							{ this.renderTeams() }
 
-						{
-							discoverHelper.isEnabled()
-							? (
-									<li className={ this.itemLinkClass( '/discover', { 'sidebar-streams__discover': true } ) }>
-										<a href="/discover">
-											<Gridicon icon="my-sites" />
-											<span className="menu-link-text">{ this.translate( 'Discover' ) }</span>
-										</a>
-									</li>
-								) : null
+							{
+								discoverHelper.isEnabled()
+								? (
+										<li className={ this.itemLinkClass( '/discover', { 'sidebar-streams__discover': true } ) }>
+											<a href="/discover">
+												<Gridicon icon="my-sites" />
+												<span className="menu-link-text">{ this.translate( 'Discover' ) }</span>
+											</a>
+										</li>
+									) : null
+							}
+
+							{
+								config.isEnabled( 'reader/recommendations' )
+								? ( <li className={ this.itemLinkClassStartsWithOneOf( [ '/recommendations', '/tags' ], { 'sidebar-streams__recommendations': true } ) }>
+									<a href="/recommendations">
+										<svg className="gridicon menu-link-icon" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 24 24"><g><path d="M7.189,12.664l0.624-0.046l0.557-0.409l0.801-1.115l0.578-1.228l0.357-0.91l0.223-0.523l0.267-0.432 l0.49-0.409l0.578-0.5l0.445-0.682l0.267-1.046l0.29-1.934V3.159L12.931,3l0.467,0.046l0.534,0.227l0.49,0.363L14.8,4.25 l0.177,0.75V5.66l-0.088,0.865l-0.223,0.615l-0.378,0.75l-0.2,0.5l-0.246,0.546l-0.133,0.5v0.432l0.111,0.273l2.38-0.023 l1.135,0.069l0.823,0.113l0.49,0.159l0.288,0.319l0.424,0.523l0.156,0.454v0.319l-0.09,0.296l-0.2,0.227l-0.29,0.5l0.111,0.296 l0.223,0.409l0.201,0.204l0.111,0.364l-0.09,0.273l-0.267,0.296l-0.267,0.34l-0.111,0.364l0.088,0.319l0.157,0.363l0.11,0.342v0.25 l-0.11,0.363l-0.223,0.273l-0.313,0.296l-0.223,0.273l-0.088,0.273v0.319l0.023,0.409l-0.111,0.25l-0.313,0.342l-0.4,0.363 c0,0-0.156,0.137-0.378,0.25c-0.223,0.114-0.868,0.273-0.868,0.273l-0.846,0.091l-1.868-0.023l-1.937-0.091l-1.379-0.159 l-2.916-0.523L7.189,12.664z M3,13.986c0-0.939,0.761-1.7,1.702-1.7c0.939,0,1.702,0.762,1.702,1.7v4.596 c0,0.939-0.762,1.7-1.702,1.7C3.761,20.283,3,19.52,3,18.582V13.986z"/></g></svg>
+										<span className="menu-link-text">{ this.translate( 'Recommendations' ) }</span>
+									</a>
+							</li> )
+							: ( <li className={ this.itemLinkClass( '/recommendations', { 'sidebar-streams__recommendations': true } ) }>
+								<a href="https://wordpress.com/recommendations/" rel="external">
+									<Gridicon icon="star-outline" />
+									<span className="menu-link-text">{ this.translate( 'Recommended Blogs' ) }</span>
+								</a>
+							</li> )
 						}
 
-						{
-							config.isEnabled( 'reader/recommendations' )
-							? ( <li className={ this.itemLinkClassStartsWithOneOf( [ '/recommendations', '/tags' ], { 'sidebar-streams__recommendations': true } ) }>
-								<a href="/recommendations">
-									<svg className="gridicon menu-link-icon" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 24 24"><g><path d="M7.189,12.664l0.624-0.046l0.557-0.409l0.801-1.115l0.578-1.228l0.357-0.91l0.223-0.523l0.267-0.432 l0.49-0.409l0.578-0.5l0.445-0.682l0.267-1.046l0.29-1.934V3.159L12.931,3l0.467,0.046l0.534,0.227l0.49,0.363L14.8,4.25 l0.177,0.75V5.66l-0.088,0.865l-0.223,0.615l-0.378,0.75l-0.2,0.5l-0.246,0.546l-0.133,0.5v0.432l0.111,0.273l2.38-0.023 l1.135,0.069l0.823,0.113l0.49,0.159l0.288,0.319l0.424,0.523l0.156,0.454v0.319l-0.09,0.296l-0.2,0.227l-0.29,0.5l0.111,0.296 l0.223,0.409l0.201,0.204l0.111,0.364l-0.09,0.273l-0.267,0.296l-0.267,0.34l-0.111,0.364l0.088,0.319l0.157,0.363l0.11,0.342v0.25 l-0.11,0.363l-0.223,0.273l-0.313,0.296l-0.223,0.273l-0.088,0.273v0.319l0.023,0.409l-0.111,0.25l-0.313,0.342l-0.4,0.363 c0,0-0.156,0.137-0.378,0.25c-0.223,0.114-0.868,0.273-0.868,0.273l-0.846,0.091l-1.868-0.023l-1.937-0.091l-1.379-0.159 l-2.916-0.523L7.189,12.664z M3,13.986c0-0.939,0.761-1.7,1.702-1.7c0.939,0,1.702,0.762,1.702,1.7v4.596 c0,0.939-0.762,1.7-1.702,1.7C3.761,20.283,3,19.52,3,18.582V13.986z"/></g></svg>
-									<span className="menu-link-text">{ this.translate( 'Recommendations' ) }</span>
+							<li className={ this.itemLinkClass( '/activities/likes', { 'sidebar-activity__likes': true } ) }>
+								<a href="/activities/likes">
+									<Gridicon icon="star" size={ 24 } />
+									<span className="menu-link-text">{ this.translate( 'My Likes' ) }</span>
 								</a>
-						</li> )
-						: ( <li className={ this.itemLinkClass( '/recommendations', { 'sidebar-streams__recommendations': true } ) }>
-							<a href="https://wordpress.com/recommendations/" rel="external">
-								<Gridicon icon="star-outline" />
-								<span className="menu-link-text">{ this.translate( 'Recommended Blogs' ) }</span>
-							</a>
-						</li> )
-					}
+							</li>
+						</ul>
+					</li>
 
-						<li className={ this.itemLinkClass( '/activities/likes', { 'sidebar-activity__likes': true } ) }>
-							<a href="/activities/likes">
-								<Gridicon icon="star" size={ 24 } />
-								<span className="menu-link-text">{ this.translate( 'My Likes' ) }</span>
-							</a>
-						</li>
-					</ul>
-				</li>
+					<li className="sidebar-menu sidebar-dynamic-menu">
+						<h2 className="sidebar-heading">{ this.translate( 'Lists' ) }</h2>
+						<ul>
+							{ this.renderLists() }
 
-				<li className="sidebar-menu sidebar-dynamic-menu">
-					<h2 className="sidebar-heading">{ this.translate( 'Lists' ) }</h2>
-					<ul>
-						{ this.renderLists() }
+							<li className="sidebar-dynamic-menu__add" key="add-list">
+								<input className="sidebar-dynamic-menu__add-input" type="text" placeholder={ this.translate( 'New List' ) } ref="addListInput" onKeyDown={ this.handleCreateListKeyDown } />
+							</li>
+						</ul>
+					</li>
 
-						<li className="sidebar-dynamic-menu__add" key="add-list">
-							<input className="sidebar-dynamic-menu__add-input" type="text" placeholder={ this.translate( 'New List' ) } ref="addListInput" onKeyDown={ this.handleCreateListKeyDown } />
-						</li>
-					</ul>
-				</li>
+					<li className="sidebar-menu sidebar-dynamic-menu">
+						<h2 className="sidebar-heading">{ this.translate( 'Tags' ) }</h2>
+						<ul>
+							{ this.renderTags() }
+							<li className="sidebar-dynamic-menu__add" key="add-tag">
+								<input className="sidebar-dynamic-menu__add-input" type="text" placeholder={ this.translate( 'Follow a Tag' ) } ref="addTagInput" onKeyDown={ this.handleFollowTagKeyDown } />
+							</li>
+						</ul>
+					</li>
+				</ul>
 
-				<li className="sidebar-menu sidebar-dynamic-menu">
-					<h2 className="sidebar-heading">{ this.translate( 'Tags' ) }</h2>
-					<ul>
-						{ this.renderTags() }
-						<li className="sidebar-dynamic-menu__add" key="add-tag">
-							<input className="sidebar-dynamic-menu__add-input" type="text" placeholder={ this.translate( 'Follow a Tag' ) } ref="addTagInput" onKeyDown={ this.handleFollowTagKeyDown } />
-						</li>
-					</ul>
-				</li>
-			</ul>
+				<div className="scroll-indicator"><Gridicon icon="chevron-down" /></div>
+			</div>
 		);
 	}
 } );
