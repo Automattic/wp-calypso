a21b398a472371d82954eddd424030e84d208fb7
Tugdual de Kerviler <dekervit@gmail.com>
2015-12-09 18:45:52 +0100

    Free Trials: Add styling for `plan-status`

--- client/my-sites/plans/plan-overview/plan-status/index.jsx
+++ client/my-sites/plans/plan-overview/plan-status/index.jsx
@@ -2,13 +2,14 @@
  * External dependencies
  */
 import React from 'react';
+import classNames from 'classnames';
 
 /**
  * Internal dependencies
  */
-import Card from 'components/card';
 import CompactCard from 'components/card/compact';
 import ProgressBar from 'components/progress-bar';
+import { isPremium, isBusiness } from 'lib/products-values';
 
 const PlanStatus = React.createClass( {
 	propTypes: {
@@ -17,10 +18,11 @@ const PlanStatus = React.createClass( {
 
 	renderProgressBar() {
 		const { subscribedMoment, userFacingExpiryMoment } = this.props.plan,
-			subscribedDayMoment = subscribedMoment.set( { hour: 0, minute: 0, second: 0, millisecond: 0 } ),
+			timeConfig = { hour: 0, minute: 0, second: 0, millisecond: 0 },
+			subscribedDayMoment = subscribedMoment.set( timeConfig ),
 			// we strip the hour/minute/second/millisecond data here from `subscribed_date` to match `expiry`
 			trialPeriodInDays = userFacingExpiryMoment.diff( subscribedDayMoment, 'days' ),
-			todayMoment = this.moment().set( { hour: 0, minute: 0, second: 0, millisecond: 0 } ),
+			todayMoment = this.moment().set( timeConfig ),
 			timeUntilExpiryInDays = userFacingExpiryMoment.diff( todayMoment, 'days' );
 
 		return (
@@ -31,32 +33,43 @@ const PlanStatus = React.createClass( {
 	},
 
 	render() {
+		const { plan } = this.props,
+			iconClasses = classNames( 'plan-status__icon', {
+				'is-premium': isPremium( plan ),
+				'is-business': isBusiness( plan )
+			} );
+
 		return (
-			<span>
-				<Card>
-					<span className="plan-status__text">
-						{ this.translate( 'Your Current Plan:' ) }
-					</span>
-					<h1 className="plan-status__plan">
-						{
-							this.translate( '%(planName)s Free Trial', {
-								args: { planName: this.props.plan.productName }
-							} )
-						}
-					</h1>
-				</Card>
+			<div className="plan-status">
+				<CompactCard className="plan-status__info">
+					<div className={ iconClasses } />
+					<div className="plan-status__header">
+						<span className="plan-status__text">
+							{ this.translate( 'Your Current Plan:' ) }
+						</span>
+						<h1 className="plan-status__plan">
+							{
+								this.translate( '%(planName)s Free Trial', {
+									args: { planName: this.props.plan.productName }
+								} )
+							}
+						</h1>
+					</div>
+				</CompactCard>
 				<CompactCard>
-					<span className="plan-status__time-until-expiry">
+					<div className="plan-status__time-until-expiry">
 						{
 							this.translate( '%(timeUntilExpiry)s remaining', {
 								args: { timeUntilExpiry: this.props.plan.userFacingExpiryMoment.toNow( true ) },
 								context: 'The amount of time until the trial plan expires, e.g. "5 days remaining"'
 							} )
 						}
-					</span>
-					{ this.renderProgressBar() }
+					</div>
+					<div className="plan-status__progress">
+						{ this.renderProgressBar() }
+					</div>
 				</CompactCard>
-			</span>
+			</div>
 		);
 	}
 } );
