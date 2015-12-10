4e49f3ce32d5c4ddca0fa6cb4be305367b6fbfcf
Eduardo Villuendas <eduardo.villuendas@automattic.com>
2015-12-10 18:19:02 +0100

    Support User: First implementation of the design

--- client/components/support-user/index.jsx
+++ client/components/support-user/index.jsx
@@ -12,6 +12,12 @@ import User from 'lib/user';
 import userSettings from 'lib/user-settings';
 import Dialog from 'components/dialog';
 import KeyboardShortcuts from 'lib/keyboard-shortcuts';
+import FormButton from 'components/forms/form-button';
+import FormFieldset from 'components/forms/form-fieldset';
+import FormLabel from 'components/forms/form-label';
+import FormTextInput from 'components/forms/form-text-input';
+import FormPasswordInput from 'components/forms/form-password-input';
+import Gravatar from 'components/gravatar';
 
 module.exports = React.createClass( {
 	displayName: 'SupportUser',
@@ -49,6 +55,7 @@ module.exports = React.createClass( {
 	},
 
 	closeDialog: function() {
+		this.setState( { showDialog: false } );
 	},
 
 	onChangeUser: function( e ) {
@@ -79,47 +86,98 @@ module.exports = React.createClass( {
 		}
 	},
 
+	getButtonsSupportUser: function() {
+		var buttons;
+
+		buttons = [
+			<FormButton
+				key="supportuser"
+				onClick={ this.onChangeUser }>
+					Change user
+			</FormButton>,
+			<FormButton
+				key="cancel"
+				isPrimary={ false }
+				onClick={ this.closeDialog }>
+					Cancel
+			</FormButton>
+		];
+
+		return buttons;
+	},
+
+	getButtonsRestoreUser: function() {
+		var buttons;
+
+		buttons = [
+			<FormButton
+				key="restoreuser"
+				onClick={ this.onRestoreUser }>
+					Restore user
+			</FormButton>,
+			<FormButton
+				key="cancel"
+				isPrimary={ false }
+				onClick={ this.closeDialog }>
+					Cancel
+			</FormButton>
+		];
+
+		return buttons;
+	},
+
 	render: function() {
 		if ( this.state.isSupportUser ) {
 		return (
-			<Dialog additionalClassNames="support-user" isVisible={ this.state.showDialog } onClose={ this.closeDialog }>
-			<div className="restore-user">
-				<form onSubmit={ this.onRestoreUser }>
-					<button type="submit" className="button">
-					Restore User
-					</button>
-				</form>
-			</div>
+			<Dialog 
+				isVisible={ this.state.showDialog }
+				onClose={ this.closeDialog }
+				buttons={ this.getButtonsRestoreUser() }
+				additionalClassNames="support-user__dialog" 
+			>
+				<div className="support-user__people-profile">
+					<div className="support-user__gravatar">
+						<img className="gravatar" src="https://0.gravatar.com/avatar/3c063e69ebd9a94b87a36749505b5561?s=96&d=mm&r=G" />
+					</div>
+					<div className="support-user__detail">
+						<div className="support-user__username">
+							Eduardo Villuendas
+						</div>
+						<div className="support-user__login">
+							<span>@evilluendas</span>
+						</div>
+					</div>
+				</div>
 			</Dialog>
 		);
 		} else {
 		return (
-			<Dialog additionalClassNames="support-user" isVisible={ this.state.showDialog } onClose={ this.closeDialog }>
-			<div className="support-user">
-				<form onSubmit={ this.onChangeUser }>
-					<label>
-					Username
-					<input
-						type="text"
-						name="supportUser"
-						id="supportUser"
-						valueLink={ this.linkState( 'supportUser' ) }
-					/>
-					</label>
-					<label>
-					Support Password
-					<input
-						type="password"
-						name="supportPassword"
-						id="supportPassword"
-						valueLink={ this.linkState( 'supportPassword' ) }
-					/>
-					</label>
-					<button type="submit" className="button">
-					Change to Support User
-					</button>
-				</form>
-			</div>
+			<Dialog 
+				isVisible={ this.state.showDialog } 
+				onClose={ this.closeDialog }
+				buttons={ this.getButtonsSupportUser() }
+				additionalClassNames="support-user__dialog" 
+			>
+				<h2 className="support-user__heading">Support user</h2>
+				<FormFieldset>
+					<FormLabel>
+						<span>Username</span>
+						<FormTextInput
+							name="supportUser"
+							id="supportUser"
+							valueLink={ this.linkState( 'supportUser' ) }
+						/>
+					</FormLabel>
+
+					<FormLabel>
+						<span>User support password</span>
+						<FormPasswordInput
+							name="supportPassword"
+							id="supportPassword"
+							valueLink={ this.linkState( 'supportPassword' ) }
+						/>
+					</FormLabel>
+				</FormFieldset>
 			</Dialog>
 		); }
 	}
