83623d4ccf671d471e1961dd4563edba1d1d88f6
Stéphane Thomas <email@stephanethomas.com>
2015-12-10 12:47:08 -0500

    Plans: Extract plans status progress section into its own component

--- client/my-sites/plans/plan-overview/plan-status/index.jsx
+++ client/my-sites/plans/plan-overview/plan-status/index.jsx
@@ -8,44 +8,13 @@ import classNames from 'classnames';
  * Internal dependencies
  */
 import CompactCard from 'components/card/compact';
+import PlanStatusProgress from './progress';
 import ProgressBar from 'components/progress-bar';
 import { isPremium, isBusiness } from 'lib/products-values';
 
 const PlanStatus = React.createClass( {
 	propTypes: {
 		plan: React.PropTypes.object.isRequired
-	},
-
-	getDaysUntilExpiry() {
-		const { userFacingExpiryMoment } = this.props.plan;
-
-		return userFacingExpiryMoment.diff( this.moment(), 'days' );
-	},
-
-	renderProgressBar() {
-		const { subscribedMoment, userFacingExpiryMoment } = this.props.plan,
-			// we strip the hour/minute/second/millisecond data here from `subscribed_date` to match `expiry`
-			trialPeriodInDays = userFacingExpiryMoment.diff( subscribedMoment, 'days' ),
-			timeUntilExpiryInDays = this.getDaysUntilExpiry(),
-			progress = Math.max( 0.5, trialPeriodInDays - timeUntilExpiryInDays );
-
-		return (
-			<ProgressBar
-				value={ progress }
-				total={ trialPeriodInDays } />
-		);
-	},
-
-	renderDaysRemaining() {
-		return this.translate(
-			'%(daysUntilExpiry)s day remaining',
-			'%(daysUntilExpiry)s days remaining',
-			{
-				args: { daysUntilExpiry: this.getDaysUntilExpiry() },
-				count: this.getDaysUntilExpiry(),
-				context: 'The amount of time until the trial plan expires, e.g. "5 days remaining"'
-			}
-		);
 	},
 
 	render() {
@@ -72,14 +41,8 @@ const PlanStatus = React.createClass( {
 						</h1>
 					</div>
 				</CompactCard>
-				<CompactCard>
-					<div className="plan-status__time-until-expiry">
-						{ this.renderDaysRemaining() }
-					</div>
-					<div className="plan-status__progress">
-						{ this.renderProgressBar() }
-					</div>
-				</CompactCard>
+
+				<PlanStatusProgress plan={ this.props.plan } />
 			</div>
 		);
 	}
