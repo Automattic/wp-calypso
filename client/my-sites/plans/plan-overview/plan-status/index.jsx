064a74ee8be119bcd9c865caa02007da4d01fc22
Stéphane Thomas <email@stephanethomas.com>
2015-12-11 15:34:13 -0500

    Plans: Allow purchasing of plans on trial on Plan Overview page

--- client/my-sites/plans/plan-overview/plan-status/index.jsx
+++ client/my-sites/plans/plan-overview/plan-status/index.jsx
@@ -3,18 +3,32 @@
  */
 import React from 'react';
 import classNames from 'classnames';
+import page from 'page';
 
 /**
  * Internal dependencies
  */
+import Button from 'components/button';
+import { cartItems } from 'lib/cart-values';
 import CompactCard from 'components/card/compact';
 import PlanStatusProgress from './progress';
 import ProgressBar from 'components/progress-bar';
 import { isPremium, isBusiness } from 'lib/products-values';
+import * as upgradesActions from 'lib/upgrades/actions';
 
 const PlanStatus = React.createClass( {
 	propTypes: {
-		plan: React.PropTypes.object.isRequired
+		plan: React.PropTypes.object.isRequired,
+		selectedSite: React.PropTypes.oneOfType( [
+			React.PropTypes.object,
+			React.PropTypes.bool
+		] ).isRequired
+	},
+
+	purchasePlan() {
+		upgradesActions.addItem( cartItems.planItem( this.props.plan.productSlug ) );
+
+		page( '/checkout/' + this.props.selectedSite.slug );
 	},
 
 	render() {
@@ -42,6 +56,12 @@ const PlanStatus = React.createClass( {
 							}
 						</h1>
 					</div>
+
+					<Button
+						className="plan-status__button"
+						onClick={ this.purchasePlan }>
+						{ this.translate( 'Purchase Now' ) }
+					</Button>
 				</CompactCard>
 
 				<PlanStatusProgress plan={ this.props.plan } />
