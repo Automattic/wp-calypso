61df6fd9625a6f9b6100101347ab62f9afc5c3d4
Stéphane Thomas <email@stephanethomas.com>
2015-12-11 14:36:00 -0500

    Checkout: Reorder requires alphabetically in Cart Items module

--- client/lib/cart-values/cart-items.js
+++ client/lib/cart-values/cart-items.js
@@ -2,35 +2,35 @@
  * External dependencies
  */
 var React = require( 'react/addons' ),
-	where = require( 'lodash/collection/where' ),
-	some = require( 'lodash/collection/some' ),
 	every = require( 'lodash/collection/every' ),
-	isEqual = require( 'lodash/lang/isEqual' ),
-	flow = require( 'lodash/function/flow' ),
-	reject = require( 'lodash/collection/reject' ),
 	extend = require( 'lodash/object/assign' ),
+	flow = require( 'lodash/function/flow' ),
+	isEqual = require( 'lodash/lang/isEqual' ),
 	merge = require( 'lodash/object/merge' ),
-	rest = require( 'lodash/array/rest' );
+	reject = require( 'lodash/collection/reject' ),
+	rest = require( 'lodash/array/rest' ),
+	some = require( 'lodash/collection/some' ),
+	where = require( 'lodash/collection/where' );
 
 /**
  * Internal dependencies
  */
 var productsValues = require( 'lib/products-values' ),
 	formatProduct = productsValues.formatProduct,
-	isPlan = productsValues.isPlan,
-	isGoogleApps = productsValues.isGoogleApps,
-	isDomainProduct = productsValues.isDomainProduct,
-	isSiteRedirect = productsValues.isSiteRedirect,
-	isDependentProduct = productsValues.isDependentProduct,
-	isDomainRegistration = productsValues.isDomainRegistration,
-	isNoAds = productsValues.isNoAds,
 	isCustomDesign = productsValues.isCustomDesign,
+	isDependentProduct = productsValues.isDependentProduct,
+	isDomainProduct = productsValues.isDomainProduct,
 	isDomainRedemption = productsValues.isDomainRedemption,
-	isVideoPress = productsValues.isVideoPress,
+	isDomainRegistration = productsValues.isDomainRegistration,
+	isGoogleApps = productsValues.isGoogleApps,
+	isNoAds = productsValues.isNoAds,
+	isPlan = productsValues.isPlan,
+	isPrivateRegistration = productsValues.isPrivateRegistration,
+	isSiteRedirect = productsValues.isSiteRedirect,
+	isSpaceUpgrade = productsValues.isSpaceUpgrade,
 	isUnlimitedSpace = productsValues.isUnlimitedSpace,
 	isUnlimitedThemes = productsValues.isUnlimitedThemes,
-	isSpaceUpgrade = productsValues.isSpaceUpgrade,
-	isPrivateRegistration = productsValues.isPrivateRegistration,
+	isVideoPress = productsValues.isVideoPress,
 	sortProducts = require( 'lib/products-values/sort' );
 
 /**
