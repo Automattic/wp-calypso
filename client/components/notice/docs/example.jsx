24433d0bd4368e08a8979889243ed98c003f0997
Matías Ventura <mv@matiasventura.com>
2015-12-15 14:43:49 +0100

    Notices: absorb <Flag> component as compact <Notice>.

--- client/components/notice/docs/example.jsx
+++ client/components/notice/docs/example.jsx
@@ -28,40 +28,54 @@ var Notices = React.createClass( {
 					<a className="design-assets__toggle button" onClick={ this.toggleNotices }>{ toggleNoticesText }</a>
 				</h2>
 
-				<Notice
-					text="I'm a notice with no status."
-					showDismiss={ false }
-					isCompact={ this.state.compactNotices ? true : null } />
-				<Notice
-					status="is-info"
-					text="I'm an `is-info` notice with custom icon."
-					icon="heart"
-					isCompact={ this.state.compactNotices ? true : null } />
-				<Notice status="is-success" text="I'm an `is-success` notice." isCompact={ this.state.compactNotices ? true : null } />
-				<Notice status="is-error" text="I'm an `is-error` notice." isCompact={ this.state.compactNotices ? true : null } />
-				<Notice
-					status="is-warning"
-					icon="mention"
-					text="I'm an `is-warning` notice with custom icon."
-					isCompact={ this.state.compactNotices ? true : null } />
-				<Notice
-					status="is-warning"
-					isCompact={ this.state.compactNotices ? true : null }
-					showDismiss={ false }
-					text={ "I'm an `is-warning` notice with an action." }>
-					<NoticeAction href="#">
-						{ "Update" }
-					</NoticeAction>
-				</Notice>
-				<Notice
-					status="is-success"
-					isCompact={ this.state.compactNotices ? true : null }
-					showDismiss={ false }
-					text={ "I'm an `is-success` notice with an arrow link." }>
-					<NoticeAction href="#" external={ true }>
-						{ "Preview" }
-					</NoticeAction>
-				</Notice>
+				<div>
+					<Notice
+						text="I'm a notice with no status."
+						showDismiss={ false }
+						isCompact={ this.state.compactNotices ? true : null } />
+				</div>
+				<div>
+					<Notice
+						status="is-info"
+						text="I'm an `is-info` notice with custom icon."
+						icon="heart"
+						isCompact={ this.state.compactNotices ? true : null } />
+				</div>
+				<div>
+					<Notice status="is-success" text="I'm an `is-success` notice." isCompact={ this.state.compactNotices ? true : null } />
+				</div>
+				<div>
+					<Notice status="is-error" text="I'm an `is-error` notice." isCompact={ this.state.compactNotices ? true : null } />
+				</div>
+				<div>
+					<Notice
+						status="is-warning"
+						icon="mention"
+						text="I'm an `is-warning` notice with custom icon."
+						isCompact={ this.state.compactNotices ? true : null } />
+				</div>
+				<div>
+					<Notice
+						status="is-warning"
+						isCompact={ this.state.compactNotices ? true : null }
+						showDismiss={ false }
+						text={ "I'm an `is-warning` notice with an action." }>
+						<NoticeAction href="#">
+							{ "Update" }
+						</NoticeAction>
+					</Notice>
+				</div>
+				<div>
+					<Notice
+						status="is-success"
+						isCompact={ this.state.compactNotices ? true : null }
+						showDismiss={ false }
+						text={ "I'm an `is-success` notice with an arrow link." }>
+						<NoticeAction href="#" external={ true }>
+							{ "Preview" }
+						</NoticeAction>
+					</Notice>
+				</div>
 			</div>
 		);
 	},
