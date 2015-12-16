1f56da83c5954091c70db11711a853e949007643
bluefuton <chris@bluefuton.com>
2015-12-17 08:53:27 +1100

    Bump TAGS_PER_PAGE to 50

--- client/lib/reader-lists-tags/actions.js
+++ client/lib/reader-lists-tags/actions.js
@@ -6,7 +6,7 @@ import wpcom from 'lib/wp';
 import { requestInflight, requestTracker } from 'lib/inflight';
 import { action } from './constants';
 
-const TAGS_PER_PAGE = 10;
+const TAGS_PER_PAGE = 50;
 
 export function fetchMoreTags( listOwner, listSlug, page ) {
 	if ( requestInflight( action.ACTION_RECEIVE_READER_LIST_TAGS ) ) {
