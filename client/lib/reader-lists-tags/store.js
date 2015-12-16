32b27e20b62d4b5a5e2a6b54addfb5588da86700
bluefuton <chris@bluefuton.com>
2015-12-17 09:11:59 +1100

    Move receiveTags() out of ReaderListsTagsStore - does not need to be exported

--- client/lib/reader-lists-tags/store.js
+++ client/lib/reader-lists-tags/store.js
@@ -15,35 +15,7 @@ const initialState = {
 	isFetching: false
 };
 
-const ReaderListsTagsStore = createReducerStore( ( state, payload ) => {
-	switch ( payload.action.type ) {
-		case actionTypes.ACTION_RECEIVE_READER_LIST_TAGS:
-			return ReaderListsTagsStore.receiveTags( payload.action.data );
-
-		case actionTypes.ACTION_RECEIVE_READER_LIST_TAGS_ERROR:
-			const errors = state.get( 'errors' );
-			return state.set( 'errors', errors.push( payload.action.error ) );
-
-		case actionTypes.ACTION_FETCH_READER_LIST_TAGS:
-			return state.set( 'isFetching', true );
-
-		case actionTypes.ACTION_FETCH_READER_LIST_TAGS_COMPLETE:
-			return state.set( 'isFetching', false );
-	}
-
-	return state;
-}, fromJS( initialState ) );
-
-ReaderListsTagsStore.getTagsForList = function( listId ) {
-	const state = ReaderListsTagsStore.get();
-	return state.get( 'tags' ).filter( function( tag ) {
-		return tag.get( 'list_ID' ) === parseInt( listId );
-	} ).toJS();
-};
-
-ReaderListsTagsStore.receiveTags = function( data ) {
-	const state = ReaderListsTagsStore.get();
-
+function receiveTags( state, data ) {
 	// Is it the last page?
 	let isLastPage = state.get( 'isLastPage' );
 	if ( data.number === 0 ) {
@@ -67,6 +39,32 @@ ReaderListsTagsStore.receiveTags = function( data ) {
 	currentPage = currentPage.set( data.list_ID, data.page );
 
 	return state.set( 'isLastPage', isLastPage ).set( 'tags', tags ).set( 'currentPage', currentPage );
+};
+
+const ReaderListsTagsStore = createReducerStore( ( state, payload ) => {
+	switch ( payload.action.type ) {
+		case actionTypes.ACTION_RECEIVE_READER_LIST_TAGS:
+			return receiveTags( state, payload.action.data );
+
+		case actionTypes.ACTION_RECEIVE_READER_LIST_TAGS_ERROR:
+			const errors = state.get( 'errors' );
+			return state.set( 'errors', errors.push( payload.action.error ) );
+
+		case actionTypes.ACTION_FETCH_READER_LIST_TAGS:
+			return state.set( 'isFetching', true );
+
+		case actionTypes.ACTION_FETCH_READER_LIST_TAGS_COMPLETE:
+			return state.set( 'isFetching', false );
+	}
+
+	return state;
+}, fromJS( initialState ) );
+
+ReaderListsTagsStore.getTagsForList = function( listId ) {
+	const state = ReaderListsTagsStore.get();
+	return state.get( 'tags' ).filter( function( tag ) {
+		return tag.get( 'list_ID' ) === parseInt( listId );
+	} ).toJS();
 };
 
 ReaderListsTagsStore.isFetching = function() {
