abed5bd7c3eb423b1e8a53685f6b609c94d425a0
Daryl L. L. Houston <daryl@learnhouston.com>
2015-12-16 16:15:34 -0500

    Help: Add placeholder markup and CSS for the /help/contact screen for while we're fetching data about the availability of chat.

--- client/me/help/help-contact/index.jsx
+++ client/me/help/help-contact/index.jsx
@@ -257,7 +257,19 @@ module.exports = React.createClass( {
 		}
 
 		if ( ! ( olark.isOlarkReady && sitesInitialized ) ) {
-			return <div className="help-contact__placeholder" />;
+
+			return (
+				<div className="help-contact__placeholder">
+					<h4 className="help-contact__header">Loading contact form</h4>
+					<div className="help-contact__textarea" />
+
+					<h4 className="help-contact__header">Loading contact form</h4>
+					<div className="help-contact__textarea" />
+
+					<h4 className="help-contact__header">Loading contact form</h4>
+					<div className="help-contact__textarea" />
+				</div>
+			);
 		}
 
 		if ( olark.details.isConversing && olark.isOperatorAvailable ) {
