e353a93db214a9efe08d009f1db2aa56ca48b963
artpi <artpi@post.pl>
2015-12-18 15:37:35 +0100

    Redux: helpers change coercion to toString

--- client/state/utils/actions.js
+++ client/state/utils/actions.js
@@ -6,7 +6,7 @@ import { UTILS_UNIQUEID_INCREMENT } from 'state/action-types';
 export function uniqueId( prefix ) {
 	return ( dispatch, getState ) => {
 		dispatch( { type: UTILS_UNIQUEID_INCREMENT } );
-		const id = getState().utils.uniqueId + '';
+		const id = getState().utils.uniqueId.toString();
 		return prefix ? prefix + id : id;
 	};
 }
