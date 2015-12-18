005cda37271071f0265bae2ccd412e379f26b5e6
Drew Blaisdell <drew.blaisdell@gmail.com>
2015-12-17 16:52:18 -0800

    Free Trials: Add expired state to `PlanStatus`

--- client/my-sites/plans/plan-overview/plan-status/index.jsx
+++ client/my-sites/plans/plan-overview/plan-status/index.jsx
@@ -11,7 +11,9 @@ import page from 'page';
 import Button from 'components/button';
 import { cartItems } from 'lib/cart-values';
 import CompactCard from 'components/card/compact';
-import { getDaysUntilUserFacingExpiry } from 'lib/plans';
+import Gridicon from 'components/gridicon';
+import { getDaysUntilUserFacingExpiry, isInGracePeriod } from 'lib/plans';
+import Notice from 'components/notice';
 import PlanStatusProgress from './progress';
 import { isPremium, isBusiness } from 'lib/products-values';
 import * as upgradesActions from 'lib/upgrades/actions';
@@ -31,6 +33,33 @@ const PlanStatus = React.createClass( {
 		page( `/checkout/${ this.props.selectedSite.slug }` );
 	},
 
+	renderNotice() {
+		const { plan } = this.props;
+
+		if ( isInGracePeriod( plan ) ) {
+			const daysAfterUserFacingExpiry = Math.abs( getDaysUntilUserFacingExpiry( plan ) );
+			let noticeText;
+
+			if ( daysAfterUserFacingExpiry === 0 ) {
+				noticeText = this.translate( 'Expired today' );
+			} else {
+				noticeText = this.translate(
+					'Expired %(days)d day ago',
+					'Expired %(days)d days ago', {
+						args: { days: daysAfterUserFacingExpiry },
+						count: daysAfterUserFacingExpiry
+					}
+				);
+			}
+
+			return (
+				<Notice isCompact status="is-error">
+					{ noticeText }
+				</Notice>
+			);
+		}
+	},
+
 	render() {
 		const { plan } = this.props,
 			iconClasses = classNames( 'plan-status__icon', {
@@ -41,7 +70,9 @@ const PlanStatus = React.createClass( {
 		return (
 			<div className="plan-status">
 				<CompactCard className="plan-status__info">
-					<div className={ iconClasses } />
+					<div className={ iconClasses }>
+						{ isInGracePeriod( plan ) && <Gridicon icon="notice" /> }
+					</div>
 
 					<div className="plan-status__header">
 						<span className="plan-status__text">
@@ -55,6 +86,7 @@ const PlanStatus = React.createClass( {
 								} )
 							}
 						</h1>
+						{ this.renderNotice() }
 					</div>
 
 					<Button
