1cae069c5e2fd45495eb3e04eb9b7b902e13f916
Rocco Tripaldi <tripaldir@gmail.com>
2015-12-23 15:30:16 -0500

    Settings: - updating writing section to use Form components
    - updating discussion section to use Form components
    
    - updating the Analytics section to use Form components

--- client/my-sites/site-settings/form-writing.jsx
+++ client/my-sites/site-settings/form-writing.jsx
@@ -10,7 +10,10 @@ var formBase = require( './form-base' ),
 	protectForm = require( 'lib/mixins/protect-form' ),
 	config = require( 'config' ),
 	PressThisLink = require( './press-this-link' ),
-	dirtyLinkedState = require( 'lib/mixins/dirty-linked-state' );
+	dirtyLinkedState = require( 'lib/mixins/dirty-linked-state' ),
+	FormSelect = require( 'components/forms/form-select' ),
+	FormFieldset = require( 'components/forms/form-fieldset' ),
+	FormLabel = require( 'components/forms/form-label' );
 
 module.exports = React.createClass( {
 
@@ -57,31 +60,29 @@ module.exports = React.createClass( {
 					disabled={ this.state.fetchingSettings || this.state.submittingForm }>
 						{ this.state.submittingForm ? this.translate( 'Saving…' ) : this.translate( 'Save Settings' ) }
 				</button>
-				<fieldset>
-					<label htmlFor="default_category">
+				<FormFieldset>
+					<FormLabel htmlFor="default_category">
 						{ this.translate( 'Default Post Category' ) }
-					</label>
-					<select
+					</FormLabel>
+					<FormSelect
 						name="default_category"
 						id="default_category"
 						valueLink={ this.linkState( 'default_category' ) }
 						disabled={ this.state.fetchingSettings }
-						onClick={ this.recordEvent.bind( this, 'Selected Default Post Category' ) }
-					>
+						onClick={ this.recordEvent.bind( this, 'Selected Default Post Category' ) }>
 						{ this.state.post_categories.map( function( category ) {
 							return <option value={ category.value } key={ 'post-category-' + category.value }>{ category.name }</option>;
 						} ) }
-					</select>
-					<label htmlFor="default_post_format">
+					</FormSelect>
+					<FormLabel htmlFor="default_post_format">
 						{ this.translate( 'Default Post Format' ) }
-					</label>
-					<select
+					</FormLabel>
+					<FormSelect
 						name="default_post_format"
 						id="default_post_format"
 						valueLink={ this.linkState( 'default_post_format' ) }
 						disabled={ this.state.fetchingSettings }
-						onClick={ this.recordEvent.bind( this, 'Selected Default Post Format' ) }
-					>
+						onClick={ this.recordEvent.bind( this, 'Selected Default Post Format' ) }>
 						<option value="0">{ this.translate( 'Standard', { context: 'Post format' } ) }</option>
 						<option value="aside">{ this.translate( 'Aside', { context: 'Post format' } ) }</option>
 						<option value="chat">{ this.translate( 'Chat', { context: 'Post format' } ) }</option>
@@ -92,12 +93,12 @@ module.exports = React.createClass( {
 						<option value="status">{ this.translate( 'Status', { context: 'Post format' } ) }</option>
 						<option value="video">{ this.translate( 'Video', { context: 'Post format' } ) }</option>
 						<option value="audio">{ this.translate( 'Audio', { context: 'Post format' } ) }</option>
-					</select>
-				</fieldset>
+					</FormSelect>
+				</FormFieldset>
 
 				{ config.isEnabled( 'press-this' ) &&
 					<div className="press-this">
-						<label>{ this.translate( 'Press This', { context: 'name of browser bookmarklet tool' } ) }</label>
+						<FormLabel>{ this.translate( 'Press This', { context: 'name of browser bookmarklet tool' } ) }</FormLabel>
 						<p>{ this.translate( 'Press This is a bookmarklet: a little app that runs in your browser and lets you grab bits of the web.' ) }</p>
 						<p>{ this.translate( 'Use Press This to clip text, images and videos from any web page. Then edit and add more straight from Press This before you save or publish it in a post on your site.' ) }</p>
 						<p>{ this.translate( 'Drag-and-drop the following link to your bookmarks bar or right click it and add it to your favorites for a posting shortcut.' ) }</p>
@@ -105,8 +106,7 @@ module.exports = React.createClass( {
 							<PressThisLink
 								site={ this.props.site }
 								onClick={ this.recordEvent.bind( this, 'Clicked Press This Button' ) }
-								onDragStart={ this.recordEvent.bind( this, 'Dragged Press This Button' ) }
-							>
+								onDragStart={ this.recordEvent.bind( this, 'Dragged Press This Button' ) }>
 								<span className="noticon noticon-pinned"></span>
 								{ this.translate( 'Press This', { context: 'name of browser bookmarklet tool' } ) }
 							</PressThisLink>
