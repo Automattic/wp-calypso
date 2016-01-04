1ccb84cf8fe1d3d565f7b677babdb2b2e2ed141a
Maciej Adwent <MAdwent@walmartlabs.com>
2016-01-04 14:41:08 -0800

    filter updates

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+var tagFilter = require("./lib/tag_filter");
+
 var plugin = {
   initialize: function (argv) {
     plugin.settings.initialize(argv);
   },
   iterator: require("./lib/get_tests"),
   filters: {
-    tag: require("./lib/tag_filter")
+    tag: tagFilter,
+    tags: tagFilter,
+    group: require("./lib/group_filter"),
+    test: require("./lib/single_test_filter")
   },
   settings: require("./lib/settings"),
   TestRun: require("./lib/test_run")
