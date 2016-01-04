dabc47c509439758f860204ec86ec67327eb5701
Bernhard Reiter <ockham@raz.or.at>
2016-01-04 14:34:08 +0100

    layout/logged-out: ES6ify, functionalize

--- client/layout/logged-out.jsx
+++ client/layout/logged-out.jsx
@@ -1,42 +1,37 @@
 /**
  * External dependencies
  */
-var React = require( 'react' ),
-	classNames = require( 'classnames' ),
-	connect = require( 'react-redux' ).connect;
+import React from 'react';
+import classNames from 'classnames';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
+import MasterbarLoggedOut from 'layout/masterbar/logged-out';
+import GlobalNotices from 'components/global-notices';
+import notices from 'notices';
 
-var MasterbarLoggedOut = require( 'layout/masterbar/logged-out' ),
-	GlobalNotices = require( 'components/global-notices' ),
-	notices = require( 'notices' ),
-	LoggedOutLayout;
+const LoggedOutLayout = ( { section, hasSidebar } ) => {
+	const sectionClass = section ? ' is-section-' + section : '';
+	const classes = classNames( 'wp', sectionClass, {
+		'has-no-sidebar': ! hasSidebar
+	} );
 
-LoggedOutLayout = React.createClass( {
-	displayName: 'LayoutLoggedOut',
-
-	render: function() {
-		var sectionClass = this.props.section ? ' is-section-' + this.props.section : '',
-			classes = classNames( 'wp', sectionClass, {
-				'has-no-sidebar': ! this.props.hasSidebar
-			} );
-
-		return (
-			<div className={ classes }>
-				<MasterbarLoggedOut />
-				<div id="content" className="wp-content">
-					<GlobalNotices id="notices" notices={ notices.list } />
-					<div id="primary" className="wp-primary wp-section" />
-					<div id="secondary" className="wp-secondary" />
-				</div>
-				<div id="tertiary" className="wp-overlay fade-background" />
+	return (
+		<div className={ classes }>
+			<MasterbarLoggedOut />
+			<div id="content" className="wp-content">
+				<GlobalNotices id="notices" notices={ notices.list } />
+				<div id="primary" className="wp-primary wp-section" />
+				<div id="secondary" className="wp-secondary" />
 			</div>
-		);
-	}
+			<div id="tertiary" className="wp-overlay fade-background" />
+		</div>
+	);
+}
 
-} );
+LoggedOutLayout.displayName = 'LoggedOutLayout';
 
 export default connect(
 	( state ) => {
