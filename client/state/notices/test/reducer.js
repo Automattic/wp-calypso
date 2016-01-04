3f90c3a875b31e725d6b072dd480475f73239690
artpi <artpi@post.pl>
2016-01-04 14:31:09 +0100

    Notices: Tests and README
    Notices: Store tests
    
    Notices: README for notices state
    
    Notices readme: typo fixes

---
+++ client/state/notices/test/reducer.js
@@ -0,0 +1,46 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { NEW_NOTICE, REMOVE_NOTICE } from 'state/action-types';
+import { items } from '../reducer';
+
+describe( 'reducer', () => {
+	describe( '#items()', () => {
+		it( 'should default to an empty array', () => {
+			const state = items( undefined, [] );
+			expect( state ).to.eql( [] );
+		} );
+
+		it( 'should properly add new notice', () => {
+			const notice = { text: 'Example Notice Text' },
+				state = items( undefined, {
+					type: NEW_NOTICE,
+					notice: notice
+				} );
+
+			expect( state ).to.eql( [ notice ] );
+		} );
+
+		it( 'should properly remove selected notice', () => {
+			const notices = [
+					{ noticeId: 1 },
+					{ noticeId: 2 },
+					{ noticeId: 3 }
+				],
+				state = items( notices, {
+					type: REMOVE_NOTICE,
+					noticeId: 2
+				} );
+
+			expect( state ).to.eql( [
+				{ noticeId: 1 },
+				{ noticeId: 3 }
+			] );
+		} );
+	} );
+} );
