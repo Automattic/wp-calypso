643b19507bc2156f11219e6a58803603d3dae167
artpi <artpi@post.pl>
2016-01-06 17:29:42 +0100

    Redux helpers: seperate increment action creator

--- client/state/utils/actions.js
+++ client/state/utils/actions.js
@@ -3,9 +3,13 @@
  */
 import { UTILS_UNIQUEID_INCREMENT } from 'state/action-types';
 
+export function uniqueIdIncrement() {
+	return { type: UTILS_UNIQUEID_INCREMENT };
+}
+
 export function uniqueId( prefix ) {
 	return ( dispatch, getState ) => {
-		dispatch( { type: UTILS_UNIQUEID_INCREMENT } );
+		dispatch( uniqueIdIncrement() );
 		const id = getState().utils.uniqueId.toString();
 		return prefix ? prefix + id : id;
 	};
