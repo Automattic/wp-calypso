f7ea7c827ab208694b6099cba1e44ba6e8d4144e
nobuti <buti@nobuti.com>
2016-01-11 13:20:33 +0100

    Tests.

---
+++ client/components/checkbox/test/index.jsx
@@ -0,0 +1,56 @@
+require( 'lib/react-test-env-setup' )();
+
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import sinon from 'sinon';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Gridicon from 'components/gridicon';
+
+/**
+ * Internal dependencies
+ */
+import Checkbox from '../index';
+
+describe( 'Checkbox', function() {
+	describe( 'Rendering ', function() {
+		it( 'should render not Gridicon when not checked', function() {
+			const checkbox = shallow( <Checkbox checked={ false } /> );
+			expect( checkbox.find( Gridicon ) ).to.have.length( 0 );
+			expect( checkbox.html() ).to.not.contain( 'checked' );
+		} );
+
+		it( 'should render Gridicon when checked', function() {
+			const checkbox = shallow( <Checkbox checked={ true } /> );
+			expect( checkbox.find( Gridicon ) ).to.have.length( 1 );
+			expect( checkbox.html() ).to.contain( 'checked' );
+		} );
+
+		it( 'should render custom classes properly', function() {
+			const checkbox = shallow( <Checkbox className="foo" checked={ true } /> );
+			expect( checkbox.find( '.foo' ) ).to.have.length( 1 );
+		} );
+
+		it( 'should not render disabled by default', function() {
+			const checkbox = shallow( <Checkbox checked={ true } /> );
+			expect( checkbox.html() ).to.not.contain( 'disabled' );
+		} );
+
+		it( 'should render disabled if true', function() {
+			const checkbox = shallow( <Checkbox disabled={ true } checked={ true } /> );
+			expect( checkbox.html() ).to.contain( 'disabled' );
+		} );
+	} );
+
+	describe( 'Events', function() {
+		it('should pass change events', function() {
+			const onChange = sinon.spy();
+			const checkbox = shallow( <Checkbox checked={ false } onChange={ onChange } /> );
+
+			checkbox.find( 'input' ).simulate( 'change' );
+			expect( onChange.calledOnce ).to.equal( true );
+		} );
+	} );
+} );
