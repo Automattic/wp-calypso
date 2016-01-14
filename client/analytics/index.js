bf8984ffdf4296e5f01fa7810ef07c59eff0719a
Daniel Walmsley <goldsounds@gmail.com>
2016-01-14 09:25:58 -0800

    Use lodash's startsWith for more expressive comparisons

--- client/analytics/index.js
+++ client/analytics/index.js
@@ -4,6 +4,7 @@
 var debug = require( 'debug' )( 'calypso:analytics' ),
 	assign = require( 'lodash/object/assign' ),
 	omit = require( 'lodash/object/omit' ),
+	startsWith = require( 'lodash/string/startsWith' ),
 	isUndefined = require( 'lodash/lang/isUndefined' );
 
 /**
@@ -160,17 +161,17 @@ var analytics = {
 				// prevent explosion of read list metrics
 				// this is a hack - ultimately we want to report this URLs in a more generic way to 
 				// google analytics
-				if ( featureSlug.indexOf( 'read_list' ) === 0 ) {
+				if ( startsWith( featureSlug, 'read_list' ) ) {
 					featureSlug = 'read_list';
-				} else if ( featureSlug.indexOf( 'tag_' ) === 0 ) {
+				} else if ( startsWith( featureSlug, 'tag_' ) ) {
 					featureSlug = 'tag__id';
-				} else if ( featureSlug.indexOf( 'domains_add_suggestion_' ) === 0 ) {
+				} else if ( startsWith( featureSlug, 'domains_add_suggestion_' ) ) {
 					featureSlug = 'domains_add_suggestion__suggestion__domain';
-				} else if ( document.location.pathname.indexOf( '/plugins/browse/' ) === 0 ) {
+				} else if ( startsWith( document.location.pathname, '/plugins/browse/' ) ) {
 					featureSlug = 'plugins_browse__site';
-				} else if ( featureSlug.indexOf( 'read_post_feed_' ) === 0 ) {
+				} else if ( startsWith( featureSlug, 'read_post_feed_' ) ) {
 					featureSlug = 'read_post_feed__id';
-				} else if ( featureSlug.indexOf( 'read_post_id_' ) === 0 ) {
+				} else if ( startsWith( featureSlug, 'read_post_id_' ) ) {
 					featureSlug = 'read_post_id__id';
 				} else if ( ( matched = featureSlug.match( /^start_(.*)_(..)$/ ) ) != null ) {
 					featureSlug = `start_${matched[1]}`;
