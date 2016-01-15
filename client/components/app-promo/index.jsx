6566d921817295fe7da0a3a9628a1842609f57fe
Marcus Kazmierczak <marcus@mkaz.com>
2016-01-15 12:29:42 -0800

    Update promo spot with 6 different versions, with promo code - change link to go to /me/get-apps and include promo code

--- client/components/app-promo/index.jsx
+++ client/components/app-promo/index.jsx
@@ -16,7 +16,18 @@ export default React.createClass( {
 
 	getInitialState: function() {
 		var has_dismissed = store.get('desktop_promo_dismissed');
+		var promo_options = [
+			{ promo_code: 'a0001', message: 'WordPress.com your way  — apps now available in three delicious flavors — desktop, iOS, and Android.' },
+			{ promo_code: 'a0002', message: 'Get WordPress.com apps for desktop and mobile: download now!' },
+		    { promo_code: 'a0003', message: 'WordPress.com apps now available for desktop and mobile: download now!' },
+			{ promo_code: 'a0004', message: 'WordPress.com wherever you are — apps now available for desktop and mobile.' },
+			{ promo_code: 'a0005', message: 'WordPress.com at your fingertips — download apps for desktop and mobile.' },
+			{ promo_code: 'a0006', message: 'Blog anywhere, any time: download WordPress.com apps for desktop and mobile.' }
+		];
+
+		var item = promo_options[Math.floor(Math.random()*promo_options.length)];
 		return {
+			promo_item: item,
 			dismissed: has_dismissed
 		};
 	},
@@ -24,7 +35,7 @@ export default React.createClass( {
 	componentDidMount: function() {
 		// record promo view event
 		if (!this.state.dismissed ) {
-			analytics.tracks.recordEvent( 'calypso_desktop_promo_write_view', { 'promo_code': 'a0001' } );
+			analytics.tracks.recordEvent( 'calypso_desktop_promo_write_view', { 'promo_code': this.state.promo_item.promo_code } );
 		}
 	},
 
@@ -44,15 +55,16 @@ export default React.createClass( {
 			'dismissed': false
 		} ), element;
 
-
+		var promo_link = "https://wordpress.com/me/get-apps?ref=promo_write_" + this.state.promo_item.promo_code;
 		element = (
 			<div className={ classes }>
 				<span tabIndex="0" className="app-promo__dismiss" onClick={ this.dismiss } >
 					<Gridicon icon="cross" size={ 24 } />
 					<span className="screen-reader-text">{ this.translate( 'Dismiss' ) }</span>
 				</span>
-				<a className="app-promo__link" title="Try the desktop app!" href="https://desktop.wordpress.com" target="_blank">
-					<img className="app-promo__icon" src="https://s0.wp.com/wp-content/themes/a8c/desktop/i/icon.png" width="32" height="32" alt="WordPress Desktop Icon" /> Manage all your WordPress.com and Jetpack-enabled sites in one place
+				<a className="app-promo__link" title="Try the desktop app!" href={promo_link} target="_blank">
+					<img className="app-promo__icon" src="https://s0.wp.com/wp-content/themes/a8c/desktop/i/icon.png" width="32" height="32" alt="WordPress Desktop Icon" /> 
+					{this.state.promo_item.message}
 				</a>
 			</div>
 		);
