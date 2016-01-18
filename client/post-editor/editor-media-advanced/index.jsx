fd2e2f50c5572064b82d13fd6c5c3be95c0ae1ce
Andrew Duthie <andrew@andrewduthie.com>
2016-01-18 13:39:48 -0500

    Media: Render advanced editing modal with ID and save

--- client/post-editor/editor-media-advanced/index.jsx
+++ client/post-editor/editor-media-advanced/index.jsx
@@ -10,23 +10,44 @@ import { connect } from 'react-redux';
 import { setEditorMediaEditItem } from 'state/ui/editor/media/actions';
 import Dialog from 'components/dialog';
 
-function EditorMediaAdvanced( { visible, toggleVisible } ) {
-	return (
-		<Dialog isVisible={ visible } onClose={ toggleVisible }>
-			WIP
-		</Dialog>
-	);
-}
+const EditorMediaAdvanced = React.createClass( {
+	propTypes: {
+		item: PropTypes.object,
+		resetEditItem: PropTypes.func,
+		insertMedia: PropTypes.func
+	},
 
-EditorMediaAdvanced.propTypes = {
-	visible: PropTypes.bool,
-	toggleVisible: PropTypes.func
-};
+	getDefaultProps() {
+		return {
+			resetEditItem: () => {},
+			insertMedia: () => {}
+		};
+	},
 
-EditorMediaAdvanced.defaultProps = {
-	visible: false,
-	toggleVisible: () => {}
-};
+	getButtonSettings() {
+		return [
+			{
+				action: 'confirm',
+				label: this.translate( 'Save' ),
+				isPrimary: true,
+				onClick: () => this.props.insertMedia( 'Updated' )
+			}
+		];
+	},
+
+	render() {
+		const { item, resetEditItem } = this.props;
+
+		return (
+			<Dialog
+				isVisible={ !! item }
+				buttons={ this.getButtonSettings() }
+				onClose={ resetEditItem }>
+				ID { item && item.media.ID }
+			</Dialog>
+		);
+	}
+} );
 
 export default connect(
 	( state ) => {
