94aee59b10020bc259d50a29d0ae95943dd784cb
Rocco Tripaldi <tripaldir@gmail.com>
2016-01-18 14:51:46 -0500

    Update the button text for the invite-accept form for logged in users. The action should reflect the role of the invited user.

--- client/my-sites/invites/invite-accept-logged-out/index.jsx
+++ client/my-sites/invites/invite-accept-logged-out/index.jsx
@@ -30,7 +30,15 @@ export default React.createClass( {
 	},
 
 	submitButtonText() {
-		return this.translate( 'Sign Up & Join' );
+		let text = '';
+		if ( 'follower' === this.props.role ) {
+			text = this.translate( 'Sign Up & Follow' );
+		} else if ( 'viewer' === this.props.role ) {
+			text = this.translate( 'Sign Up & View' );
+		} else {
+			text = this.translate( 'Sign Up & Join' );
+		}
+		return text;
 	},
 
 	clickSignInLink() {
