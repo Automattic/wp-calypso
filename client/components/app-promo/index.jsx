e59a165e3f38d993b817204dd5276b3fb6dea6a1
Marcus Kazmierczak <marcus@mkaz.com>
2016-01-19 11:15:28 -0800

    Try to make eslint happy

--- client/components/app-promo/index.jsx
+++ client/components/app-promo/index.jsx
@@ -2,9 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import assign from 'lodash/object/assign';
 import classNames from 'classnames';
-import noop from 'lodash/utility/noop';
 import Gridicon from 'components/gridicon';
 
 import analytics from 'analytics';
@@ -19,7 +17,7 @@ export default React.createClass( {
 	},
 
 	getInitialState: function() {
-		var has_dismissed = store.get('desktop_promo_dismissed');
+		var has_dismissed = store.get( 'desktop_promo_dismissed' );
 		var promo_options = [
 			{ promo_code: 'a0001', message: 'WordPress.com your way  — apps now available in three delicious flavors — desktop, iOS, and Android.' },
 			{ promo_code: 'a0002', message: 'Get WordPress.com apps for desktop and mobile: download now!' },
@@ -29,7 +27,7 @@ export default React.createClass( {
 			{ promo_code: 'a0006', message: 'Blog anywhere, any time: download WordPress.com apps for desktop and mobile.' }
 		];
 
-		var item = promo_options[Math.floor(Math.random()*promo_options.length)];
+		var item = promo_options[Math.floor( Math.random() * promo_options.length )];
 		return {
 			promo_item: item,
 			dismissed: has_dismissed
@@ -38,41 +36,42 @@ export default React.createClass( {
 
 	componentDidMount: function() {
 		// record promo view event
-		if (!this.state.dismissed ) {
-			analytics.tracks.recordEvent( 'calypso_desktop_promo_view', { 'promo_location': this.props.location, 'promo_code': this.state.promo_item.promo_code } );
+		if ( ! this.state.dismissed ) {
+			analytics.tracks.recordEvent( 'calypso_desktop_promo_view', { promo_location: this.props.location, promo_code: this.state.promo_item.promo_code } );
 		}
 	},
 
 	recordClickEvent: function() {
-		analytics.tracks.recordEvent( 'calypso_desktop_promo_click', { 'promo_location': this.props.location, 'promo_code': this.state.promo_item.promo_code } );
+		analytics.tracks.recordEvent( 'calypso_desktop_promo_click', { promo_location: this.props.location, promo_code: this.state.promo_item.promo_code } );
 	},
 
 	dismiss: function() {
 		this.setState( { dismissed: true } );
 
 		// store as dismissed
-		store.set('desktop_promo_dismissed', true);
+		store.set( 'desktop_promo_dismissed', true );
 	},
 
-	render() {
+	render: function() {
 		if ( this.state.dismissed ) {
 			return null;
 		}
 
 		var classes = classNames( 'app-promo', {
-			'dismissed': false
-		} ), element;
+				dismissed: false
+			}
+		), element;
 
-		var promo_link = "https://wordpress.com/me/get-apps?ref=promo_write_" + this.state.promo_item.promo_code;
+		var promo_link = 'https://wordpress.com/me/get-apps?ref=promo_write_' + this.state.promo_item.promo_code;
 		element = (
 			<div className={ classes }>
 				<span tabIndex="0" className="app-promo__dismiss" onClick={ this.dismiss } >
 					<Gridicon icon="cross" size={ 24 } />
 					<span className="screen-reader-text">{ this.translate( 'Dismiss' ) }</span>
 				</span>
-				<a onClick={this.recordClickEvent} className="app-promo__link" title="Try the desktop app!" href={promo_link} target="_blank">
+				<a onClick={ this.recordClickEvent } className="app-promo__link" title="Try the desktop app!" href={ promo_link } target="_blank">
 					<img className="app-promo__icon" src="https://s0.wp.com/wp-content/themes/a8c/desktop/i/icon.png" width="32" height="32" alt="WordPress Desktop Icon" />
-					{this.state.promo_item.message}
+					{ this.state.promo_item.message }
 				</a>
 			</div>
 		);
