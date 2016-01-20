85c2a50470b7d961f530c4dce8c6a5d07097808e
Steve Seear <steve.seear@gmail.com>
2016-01-20 15:22:38 +0000

    Masterbar: add login/signup links to logged-out masterbar
    Init i18n on server with default locale for now.
    
    Components that need injecting with i18n mixin on the server
    require (deprecated) `react/addons` for unknown reason.

--- client/layout/masterbar/minimal.jsx
+++ client/layout/masterbar/minimal.jsx
@@ -3,24 +3,33 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React from 'react/addons';
 import Masterbar from './masterbar';
 
 /**
  * Internal dependencies
  */
 import Item from './item';
+import config from 'config';
 
-const MasterbarMinimal = ( { url } ) => (
-	<Masterbar>
-		<Item url={ url } icon="my-sites" className="masterbar__item-logo">
-			WordPress<span className="tld">.com</span>
-		</Item>
-	</Masterbar>
-);
+export default React.createClass( {
+	propTypes: {
+		url: React.PropTypes.string.isRequired
+	},
 
-MasterbarMinimal.propTypes = {
-	url: React.PropTypes.string.isRequired
-};
-
-export default MasterbarMinimal;
+	render() {
+		return(
+			<Masterbar>
+				<Item url={ this.props.url } icon="my-sites" className="masterbar__item-logo">
+					WordPress<span className="tld">.com</span>
+				</Item>
+				<Item url={ '/start' } className="masterbar_item-signup">
+					{ this.translate( 'Sign up', { context: 'Toolbar, must be shorter than ~12 chars' } ) }
+				</Item>
+				<Item url={ config( 'login_url' ) } className="masterbar_item-login">
+					{ this.translate( 'Log in', { context: 'Toolbar, must be shorter than ~12 chars' } ) }
+				</Item>
+			</Masterbar>
+		);
+	},
+} );
