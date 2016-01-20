e812371a2a8490665b4fcf34578e347e43e1824c
Steve Seear <steve.seear@gmail.com>
2016-01-20 15:22:38 +0000

    Masterbar: add login link to logged-out masterbar

--- client/layout/masterbar/logged-out.jsx
+++ client/layout/masterbar/logged-out.jsx
@@ -10,11 +10,22 @@ import Masterbar from './masterbar';
  * Internal dependencies
  */
 import Item from './item';
+import config from 'config';
 
-export default () => (
-	<Masterbar>
-		<Item url="/" icon="my-sites" className="masterbar__item-logo">
-			WordPress<span className="tld">.com</span>
-		</Item>
-	</Masterbar>
-);
+export default React.createClass( {
+
+	render() {
+		return(
+			<Masterbar>
+				<Item url="/" icon="my-sites" className="masterbar__item-logo">
+					WordPress<span className="tld">.com</span>
+				</Item>
+				<Item url={ config( 'login_url' ) } className="masterbar_item-login">
+					{ this.translate( 'Log in', { context: 'Toolbar, must be shorter than ~12 chars' } ) }
+				</Item>
+			</Masterbar>
+		);
+	},
+
+} );
+
