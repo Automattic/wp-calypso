22edc94bd332f372c1f19d22cd64ee3b21651d69
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-01-19 16:33:02 -0800

    Purchases: Add dialog for removing a purchase

--- client/me/purchases/remove-purchase/index.jsx
+++ client/me/purchases/remove-purchase/index.jsx
@@ -8,7 +8,8 @@ import React from 'react';
  */
 import CompactCard from 'components/card/compact';
 import Dialog from 'components/dialog';
-import { isDataLoading } from '../utils';
+import { getPurchase, isDataLoading } from '../utils';
+import { getName, isExpiring } from 'lib/purchases';
 
 const RemovePurchase = React.createClass( {
 	propTypes: {
@@ -21,8 +22,68 @@ const RemovePurchase = React.createClass( {
 
 	getInitialState() {
 		return {
-			isDialogOpen: false
+			isDialogVisible: false
 		};
+	},
+
+	closeDialog() {
+		this.setState( { isDialogVisible: false } );
+	},
+
+	openDialog( event ) {
+		event.preventDefault();
+
+		this.setState( { isDialogVisible: true } );
+	},
+
+	removePurchase() {
+		// TODO: actually remove the purchase
+	},
+
+	renderCard() {
+		const productName = getName( getPurchase( this.props ) );
+
+		return (
+			<CompactCard href="#" onClick={ this.openDialog }>
+				{ this.translate( 'Remove %(productName)s', { args: { productName } } ) }
+			</CompactCard>
+		);
+	},
+
+	renderDialog() {
+		const buttons = [ {
+				action: 'cancel',
+				disabled: false,
+				label: this.translate( 'Cancel' )
+			},
+			{
+				action: 'remove',
+				disabled: false,
+				isPrimary: true,
+				label: this.translate( 'Remove Now' ),
+				onClick: this.removePurchase
+			} ],
+			purchase = getPurchase( this.props ),
+			productName = getName( purchase );
+
+		return (
+			<Dialog
+				buttons={ buttons }
+				isVisible={ this.state.isDialogVisible }
+				onClose={ this.closeDialog }>
+				<h1>{ this.translate( 'Remove %(productName)s', { args: { productName } } ) }</h1>
+				<p>
+					{
+						this.translate( 'Are you sure you want to remove %(productName)s from {{em}}%(domain)s{{/em}}?', {
+							args: { productName, domain: this.props.selectedSite.slug },
+							components: { em: <em /> }
+						} )
+					}
+					{ ' ' }
+					{ this.translate( 'By removing it, you will not be able to reuse it again without purchasing a new subscription.' ) }
+				</p>
+			</Dialog>
+		);
 	},
 
 	render() {
@@ -30,8 +91,17 @@ const RemovePurchase = React.createClass( {
 			return null;
 		}
 
+		const purchase = getPurchase( this.props );
+
+		if ( ! isExpiring( purchase ) ) {
+			return null;
+		}
+
 		return (
-			<CompactCard href="#">Remove Purchase</CompactCard>
+			<span>
+				{ this.renderCard() }
+				{ this.renderDialog() }
+			</span>
 		);
 	}
 } );
