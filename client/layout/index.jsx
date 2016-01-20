19c54700f065119604235ccc93e8c11650605f3a
artpi <artpi@post.pl>
2016-01-20 11:30:23 -0500

    Offline: Added class to layout for offline indication

--- client/layout/index.jsx
+++ client/layout/index.jsx
@@ -76,7 +76,7 @@ Layout = React.createClass( {
 	},
 
 	render: function() {
-		var sectionClass = 'wp layout is-section-' + this.props.section + ' focus-' + this.props.focus.getCurrent(),
+		var sectionClass = 'wp layout is-section-' + this.props.section + ' focus-' + this.props.focus.getCurrent() + ' network-' + this.props.isOnline,
 			showWelcome = this.props.nuxWelcome.getWelcome(),
 			newestSite = this.newestSite(),
 			translatorInvitation = this.props.translatorInvitation,
@@ -119,6 +119,11 @@ Layout = React.createClass( {
 export default connect(
 	( state ) => {
 		const { isLoading, section, hasSidebar } = state.ui;
-		return { isLoading, section, hasSidebar };
+		return { 
+			isLoading,
+			section,
+			hasSidebar,
+			isOnline: state.application.isOnline
+		};
 	}
 )( Layout );
