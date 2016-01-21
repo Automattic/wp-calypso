629fd57885023599fa376f101f93448ba2938d8e
jancavan <janmcavan@gmail.com>
2016-01-20 16:19:45 -0800

    Use StatsModule component

--- client/my-sites/stats/stats-detail-months/index.jsx
+++ client/my-sites/stats/stats-detail-months/index.jsx
@@ -12,6 +12,7 @@ import observe from 'lib/mixins/data-observe';
 import Card from 'components/card';
 import Gridicon from 'components/gridicon';
 import StatsModulePlaceholder from '../stats-module/placeholder';
+import StatsModuleContent from '../stats-module/content-text';
 
 export default React.createClass( {
 	displayName: 'StatsPostDetailMonths',
@@ -130,29 +131,27 @@ export default React.createClass( {
 						</li>
 					</ul>
 				</div>
-				<div className="module-content">
-					<div className="module-content-text module-content-text-info">
-						<p>
-							{ this.translate( 'This panel gives you an overview of how many views your website gets on average.', { context: 'Info box description for post stats page in Stats' } ) }
-						</p>
-						<span className="legend achievement">{
-							this.translate(
-								'%(value)s = The all-time highest value.',
-								{ args:
-									{ value: ( this.numberFormat( highest ) ) },
-									context: 'Legend for post stats page in Stats'
-								}
-							)
-						}</span>
-					</div>
-					<StatsModulePlaceholder isLoading={ isLoading } />
-					<div className="module-content-table">
-						<div className="module-content-table-scroll">
-							<table cellPadding="0" cellSpacing="0">
-								{ tableHeader }
-								{ tableBody }
-							</table>
-						</div>
+				<StatsModuleContent className="module-content-text-info">
+					<p>
+						{ this.translate( 'This panel gives you an overview of how many views your website gets on average.', { context: 'Info box description for post stats page in Stats' } ) }
+					</p>
+					<span className="legend achievement">{
+						this.translate(
+							'%(value)s = The all-time highest value.',
+							{ args:
+								{ value: ( this.numberFormat( highest ) ) },
+								context: 'Legend for post stats page in Stats'
+							}
+						)
+					}</span>
+				</StatsModuleContent>
+				<StatsModulePlaceholder isLoading={ isLoading } />
+				<div className="module-content-table">
+					<div className="module-content-table-scroll">
+						<table cellPadding="0" cellSpacing="0">
+							{ tableHeader }
+							{ tableBody }
+						</table>
 					</div>
 				</div>
 			</Card>
