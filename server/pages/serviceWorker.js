5a01658dc4422f6d3143c03baf0290f7c1fb168b
kerryliu <kerryliu@gmail.com>
2016-01-21 17:24:09 -0500

    more assets for editor

--- server/pages/serviceWorker.js
+++ server/pages/serviceWorker.js
@@ -2,13 +2,17 @@
 //Needs work.
 
 var CACHE_NAME = 'CALYPSO';
+//TODO generate this during build
 var urlsToCache = [
 	'http://fonts.gstatic.com/s/opensans/v13/cJZKeOuBrn4kERxqtaUH3ZBw1xU1rKptJj_0jans920.woff2',
 	'http://fonts.gstatic.com/s/opensans/v13/xjAJXh38I15wypJXxuGMBogp9Q8gbYrhqGlRav_IXfk.woff2',
 	'http://fonts.gstatic.com/s/opensans/v13/DXI1ORHCpsQm3Vp6mXoaTRampu5_7CjHW5spxoeN3Vs.woff2',
 	'http://fonts.gstatic.com/s/merriweather/v8/RFda8w1V0eDZheqfcyQ4EBampu5_7CjHW5spxoeN3Vs.woff2',
 	'http://s1.wp.com/wp-includes/js/tinymce/skins/lightgray/content.min.css',
-	'http://s1.wp.com/wp-includes/js/tinymce/skins/lightgray/skin.min.css'
+	'http://s1.wp.com/wp-includes/js/tinymce/skins/lightgray/skin.min.css',
+	'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,300,600|Merriweather:700,400,700italic,400italic',
+	'https://s1.wp.com/i/noticons/noticons.css?v=20150727',
+	'https://s1.wp.com/wp-includes/css/dashicons.css?v=20150727'
 ];
 
 //Here are going to be regexes of stuff we want to cache.
@@ -19,8 +23,7 @@ var pathsToCache = [
 self.addEventListener( 'install', function( event ) {
 	// Perform install steps
 	event.waitUntil(
-		caches.open( CACHE_NAME )
-		.then( function( cache ) {
+		caches.open( CACHE_NAME ).then( function( cache ) {
 			console.log( '[CACHE] preloading ', urlsToCache );
 			return cache.addAll( urlsToCache );
 		} )
