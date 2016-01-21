5aa0ebd3fceaa333caa0f313c59b9b1ce1c40eb7
Marcus Kazmierczak <marcus@mkaz.com>
2016-01-21 10:25:16 -0800

    Add feature gating of desktop-promo to not show within desktop application

--- client/components/app-promo/index.jsx
+++ client/components/app-promo/index.jsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import Gridicon from 'components/gridicon';
 
+import config from 'config';
 import analytics from 'analytics';
 import store from 'store';
 import userUtils from 'lib/user/utils';
@@ -19,6 +20,10 @@ export default React.createClass( {
 
 	getInitialState: function() {
 		var show_promo = true;
+
+		if ( ! config.isEnabled( 'desktop-promo' ) ) {
+			show_promo = false;
+		}
 
 		if ( store.get( 'desktop_promo_dismissed' ) ) {
 			show_promo = false;
