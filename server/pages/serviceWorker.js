f21419d45ae655d0fa97bc8658a3f3b0b060a05b
kerryliu <kerryliu@gmail.com>
2016-01-20 19:28:20 -0500

    only cache preloading whitelist

--- server/pages/serviceWorker.js
+++ server/pages/serviceWorker.js
@@ -38,35 +38,6 @@ self.addEventListener( 'fetch', function( event ) {
 			console.log( '[CACHE] hit', response.url );
 			return response;
 		}
-
-		// IMPORTANT: Clone the request. A request is a stream and
-		// can only be consumed once. Since we are consuming this
-		// once by cache and once by the browser for fetch, we need
-		// to clone the response
-		var fetchRequest = event.request.clone();
-
-		return fetch( fetchRequest ).then(
-			function( response ) {
-				// Check if we received a valid response
-				if ( !response || response.status !== 200 ) {
-					console.log( '[CACHE] will not cache', event.request );
-					return response;
-				}
-
-				// IMPORTANT: Clone the response. A response is a stream
-				// and because we want the browser to consume the response
-				// as well as the cache consuming the response, we need
-				// to clone it so we have 2 stream.
-				var responseToCache = response.clone();
-
-				caches.open( CACHE_NAME )
-				.then( function( cache ) {
-					console.log( '[CACHE] caching', response.url );
-					cache.put( event.request, responseToCache );
-				} );
-				return response;
-			}
-		);
 	} )
 	);
 } );
