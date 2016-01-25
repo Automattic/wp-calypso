9175b1fad510a2acfa6a73e05f67c7064738b498
Brie Anne Demkiw <brie@automattic.com>
2016-01-25 10:23:11 -0800

    Checkout: Update TOS link variable names to be less confusing.

--- client/my-sites/upgrades/checkout/terms-of-service.jsx
+++ client/my-sites/upgrades/checkout/terms-of-service.jsx
@@ -27,11 +27,11 @@ module.exports = React.createClass( {
 		// Need to add check for subscription products in the cart so we don't show this for one-off purchases like themes
 		if ( this.props.hasRenewableSubscription ) {
 			message =  this.translate(
-				'By checking out, you agree to our {{tosLink}}Terms of Service{{/tosLink}} and authorize your payment method to be charged on a recurring basis until you cancel, which you can do at any time. You understand {{purchasesLink}}how your subscription works{{/purchasesLink}} and {{cancelLink}}how to cancel{{/cancelLink}}.', {
+				'By checking out, you agree to our {{tosLink}}Terms of Service{{/tosLink}} and authorize your payment method to be charged on a recurring basis until you cancel, which you can do at any time. You understand {{autoRenewalSupportPage}}how your subscription works{{/autoRenewalSupportPage}} and {{managePurchasesSupportPage}}how to cancel{{/managePurchasesSupportPage}}.', {
 				components: {
 					tosLink: <a href="//wordpress.com/tos/" target="_blank" />,
-					cancelLink: <a href="//support.wordpress.com/manage-purchases/" target="_blank" />,
-					purchasesLink: <a href="//support.wordpress.com/auto-renewal/" target="_blank" />
+					autoRenewalSupportPage: <a href="//support.wordpress.com/auto-renewal/" target="_blank" />,
+					managePurchasesSupportPage: <a href="//support.wordpress.com/manage-purchases/" target="_blank" />
 				}
 			} );
 		}
