64d8cf56b63784873dfbdf33f3e741eed64f7eed
Umur Kontaci <umur@umur.io>
2016-01-26 10:59:52 -0800

    Domains: Edit Site Address: Use form fields (#2473)

--- client/my-sites/upgrades/domain-management/edit-site-address/index.jsx
+++ client/my-sites/upgrades/domain-management/edit-site-address/index.jsx
@@ -13,19 +13,44 @@ import Main from 'components/main';
 import { getSelectedDomain } from 'lib/domains';
 import paths from 'my-sites/upgrades/paths';
 
+import FormFieldset from 'components/forms/form-fieldset';
+import FormInputValidation from 'components/forms/form-input-validation';
+import FormLabel from 'components/forms/form-label';
+import FormTextInput from 'components/forms/form-text-input';
+import FormTextInputWithAffixes from 'components/forms/form-text-input-with-affixes';
+
+import Card from 'components/card/compact';
+import SectionHeader from 'components/section-header';
+
 const EditSiteAddress = React.createClass( {
+	onFieldChange( event ) {
+
+	},
+
 	render() {
 		const domain = getSelectedDomain( this.props );
 
 		if ( ! domain ) {
-			return <DomainMainPlaceholder goBack={ this.goToDomainManagement } />;
+			return <DomainMainPlaceholder goBack={ this.goToDomainManagement }/>;
 		}
 
 		return (
 			<Main>
 				<Header onClick={ this.goToDomainManagement } selectedDomainName={ this.props.selectedDomainName }>
-					{ this.translate( 'Domain Settings' ) }
+					{ this.translate( 'Edit Site Address' ) }
 				</Header>
+				<SectionHeader label={ this.translate( 'Edit Site Address' ) }/>
+				<Card>
+					<FormFieldset>
+						<FormLabel htmlFor="new-address">{ this.translate( 'New Address' ) }</FormLabel>
+						<FormTextInputWithAffixes suffix="wordpress.com" id="new-address" name="new-address" type="text" onChange={ this.onFieldChange }/>
+					</FormFieldset>
+
+					<FormFieldset>
+						<FormLabel htmlFor="new-address-repeat">{ this.translate( 'New Address Again' ) }</FormLabel>
+						<FormTextInputWithAffixes suffix="wordpress.com" id="new-address-repeat" name="new-address-repeat" type="text" onChange={ this.onFieldChange }/>
+					</FormFieldset>
+				</Card>
 			</Main>
 		);
 	},
