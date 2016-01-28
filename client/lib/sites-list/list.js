Automattic/wp-calypso

commit 48c750245af5ebf441afef1cac3fc1d5cfe885fd

Sites: Extract `sync` method from `fetch`
Extract `sync` method from `fetch` in order to use with the list of
sites returned when accepting an invite

client/lib/sites-list/list.js
@@ -82,34 +82,34 @@ SitesList.prototype.fetch = function() {
 	this.fetching = true;
 	debug( 'getting SitesList from api' );
 	wpcom.me().sites( { site_visibility: siteVisiblity }, function( error, data ) {
-		var sites, changed;
-
 		if ( error ) {
 			debug( 'error fetching SitesList from api', error );
 			this.fetching = false;
 			return;
 		}
+		this.sync( data );
+	}.bind( this ) );
+};
 
-		debug( 'SitesList fetched from api:', data.sites );
+SitesList.prototype.sync = function( data ) {
+	debug( 'SitesList fetched from api:', data.sites );
 
-		sites = this.parse( data );
-
-		if ( ! this.initialized ) {
-			this.initialize( sites );
+	let sites = this.parse( data );
+	if ( ! this.initialized ) {
+		this.initialize( sites );
+		this.fetched = true;
+		this.emit( 'change' );
+	} else {
+		let changed = this.transaction( this.update, sites );
+		if ( changed || ! this.fetched ) {
 			this.fetched = true;
+			debug( 'SitesList changed via update' );
 			this.emit( 'change' );
-		} else {
-			changed = this.transaction( this.update, sites );
-			if ( changed || ! this.fetched ) {
-				this.fetched = true;
-				debug( 'SitesList changed via update' );
-				this.emit( 'change' );
-			}
 		}
 		this.fetching = false;
 		store.set( 'SitesList', sites );
-	}.bind( this ) );
-};
+	}
+}
 
 /**
  * Initialize data with Site objects

commit b12c433987cbc1d7e2458165250ce2161f2ecd11

Sites: always store sites
bug introduced in:
https://github.com/Automattic/wp-calypso/commit/48c750245af5ebf441afef1c
ac3fc1d5cfe885fd

client/lib/sites-list/list.js
@@ -87,12 +87,13 @@ SitesList.prototype.fetch = function() {
 	this.fetching = true;
 	debug( 'getting SitesList from api' );
 	wpcom.me().sites( { site_visibility: siteVisiblity }, function( error, data ) {
-		this.fetching = false;
 		if ( error ) {
 			debug( 'error fetching SitesList from api', error );
+			this.fetching = false;
 			return;
 		}
 		this.sync( data );
+		this.fetching = false;
 	}.bind( this ) );
 };
 
@@ -111,8 +112,8 @@ SitesList.prototype.sync = function( data ) {
 			debug( 'SitesList changed via update' );
 			this.emit( 'change' );
 		}
-		store.set( 'SitesList', sites );
 	}
+	store.set( 'SitesList', sites );
 }
 
 /**