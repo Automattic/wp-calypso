a2c61e5965364fd0507bf5b83bb5b76e62277f1e
johnHackworth <javier.alvarez.lop@gmail.com>
2016-01-29 13:05:10 +0100

    Jetpack-plans: Use a single instructions file

--- client/my-sites/plugins/plan-setup/instructions.jsx
+++ client/my-sites/plugins/plan-setup/instructions.jsx
@@ -1,19 +1,18 @@
 /**
  * External dependencies
  */
-import React from 'react'
-import classNames from 'classnames'
+import React from 'react';
+import classNames from 'classnames';
 
 /**
  * Internal dependencies
  */
-import Card from 'components/card'
-import MainComponent from 'components/main'
-import SectionHeader from 'components/section-header'
-import SidebarNavigation from 'my-sites/sidebar-navigation'
-import VaultPressInstructions from './plugins/vaultpress'
-import AkismetInstructions from './plugins/akismet'
-import PolldaddyInstructions from './plugins/polldaddy'
+import Card from 'components/card';
+import MainComponent from 'components/main';
+import SectionHeader from 'components/section-header';
+import SidebarNavigation from 'my-sites/sidebar-navigation';
+import plugins from './plugins.js';
+import Button from 'components/button';
 
 module.exports = React.createClass( {
 
@@ -23,21 +22,22 @@ module.exports = React.createClass( {
 		return site.plan.product_name_short || '';
 	},
 
-	renderPremium() {
+	renderInstructions( plugin ) {
 		return (
-			<ul className='plugin-plans'>
-				<VaultPressInstructions />
-				<AkismetInstructions />
-			</ul>
+			<li className={ 'plugin-plan ' + this.props.additionalClass } key={ plugin.name }>
+				<div className="plugin-plan__details">
+					<h3>{ plugin.name }</h3>
+					<p>{ plugin.description }</p>
+				</div>
+				<Button href={ plugin.buttonHref }>{ plugin.buttonDescription }</Button>
+			</li>
 		);
 	},
 
-	renderBusiness() {
+	renderPlugins( filteredPlugins ) {
 		return (
-			<ul className='plugin-plans'>
-				<VaultPressInstructions />
-				<AkismetInstructions />
-				<PolldaddyInstructions />
+			<ul className="plugin-plans">
+				{ filteredPlugins.map( this.renderInstructions ) }
 			</ul>
 		);
 	},
@@ -50,12 +50,17 @@ module.exports = React.createClass( {
 		} );
 
 		let PlanDetails = null;
-
+		let filteredPlugins = [];
 		if ( 'jetpack_premium' === site.plan.product_slug ) {
-			PlanDetails = this.renderPremium();
+			filteredPlugins = plugins.filter( ( plugin ) => {
+				return plugin.isPremium;
+			} );
 		} else if ( 'jetpack_business' === site.plan.product_slug ) {
-			PlanDetails = this.renderBusiness();
+			filteredPlugins = plugins.filter( ( plugin ) => {
+				return plugin.isBusiness;
+			} );
 		}
+		PlanDetails = this.renderPlugins( filteredPlugins );
 
 		return (
 			<MainComponent>
