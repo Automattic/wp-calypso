8007c7f2ddb66849f58340cb3178f9a76fdad26f
artpi <artpi@post.pl>
2016-02-01 20:34:30 -0500

    Revert "Add handling of command line post adding"
    This reverts commit ca02c0b63070acfed4d9cc58be101a2a42d2078d.

--- client/lib/route/page-notifier.js
+++ client/lib/route/page-notifier.js
@@ -1,33 +1,22 @@
 /*eslint no-multi-spaces: 0*/
 // node dependency
 var EventEmitter   = require( 'events' ).EventEmitter;
-var ipc = require( 'electron' ).ipcRenderer;
-var includes = require( 'lodash' ).includes;
 
 // internal dependency
-var page		   = require( 'page' );
-var Route		  = page.Route;
+var page           = require( 'page' );
+var Route          = page.Route;
 
 // routing state and middleware
-var location	   = new EventEmitter();
+var location       = new EventEmitter();
 var currentContext = null;
-var commandLine	= {};
-var route		  = new Route( '*' );
+var route          = new Route( '*' );
 
 // Inject our route at the front so we are always called
 page.callbacks.unshift( route.middleware( function( context, next ) {
-	context.commandLine = commandLine;
 	next();
 	location.emit( 'change', context, currentContext );
 	currentContext = context;
 } ) );
-
-ipc.on( 'command-line-arguments', function( sender, args ) {
-	commandLine = args;
-	if ( includes( args._, 'post' ) ) {
-		page.redirect( '/post/' + args.site );
-	}
-} );
 
 // Export a function that attaches a listener to the location change event
 // If a page has already been rendered the listener will be called with
