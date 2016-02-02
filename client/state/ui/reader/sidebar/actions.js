d709ee91c4624f985dc80487840033cf8eb8a5b1
Ben Lowery <ben.lowery@automattic.com>
2016-02-02 10:40:41 -0500

    Push a stat to tracks too

--- client/state/ui/reader/sidebar/actions.js
+++ client/state/ui/reader/sidebar/actions.js
@@ -8,6 +8,7 @@ const stats = require( 'reader/stats' );
 export function toggleReaderSidebarLists() {
 	stats.recordAction( 'sidebar_toggle_lists_menu' );
 	stats.recordGaEvent( 'Toggle Lists Menu' );
+	stats.recordTrack( 'calypso_reader_sidebar_list_toggle' );
 	return {
 		type: READER_SIDEBAR_LISTS_TOGGLE
 	};
@@ -16,6 +17,7 @@ export function toggleReaderSidebarLists() {
 export function toggleReaderSidebarTags() {
 	stats.recordAction( 'sidebar_toggle_tags_menu' );
 	stats.recordGaEvent( 'Toggle Tags Menu' );
+	stats.recordTrack( 'calypso_reader_sidebar_tags_toggle' );
 	return {
 		type: READER_SIDEBAR_TAGS_TOGGLE
 	};
