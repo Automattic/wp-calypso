Automattic/wp-calypso

commit 752fa72346341b12978f402e9f78891306586f49

Signup: Add theme preselection flow
We want people to be able to sign up with a theme already chosen from an
external source, such as the Theme Showcase(s).

We'll want to specify the theme to preselect using an URL, e.g.
/start/with-theme?theme=twentyten. The selected theme should then be set
as the selected theme, with no additional visual feedback or interaction
necessary.

- addDomainItemsToCart switch to theme if it's free & add to cart if it's premium
- add with-theme flow and modify getCheckoutDestination
- add theme dependencies when submitting a domain step

client/signup/config/steps.js
@@ -74,7 +74,7 @@ module.exports = {
 	domains: {
 		stepName: 'domains',
 		apiRequestFunction: stepActions.addDomainItemsToCart,
-		providesDependencies: [ 'siteSlug', 'domainItem' ],
+		providesDependencies: [ 'siteSlug', 'domainItem', 'themeItem' ],
 		delayApiRequestUntilComplete: true
 	},
 

client/lib/signup/step-actions.js
@@ -42,8 +42,18 @@ function fetchSitesAndUser( siteSlug, onComplete ) {
 	], onComplete );
 }
 
+function setThemeOnSite( callback, { siteSlug }, { themeSlug } ) {
+	if ( isEmpty( themeSlug ) ) {
+		return defer( callback );
+	}
+
+	wpcom.undocumented().changeTheme( siteSlug, { theme: themeSlug }, function( errors ) {
+		callback( isEmpty( errors ) ? undefined : [ errors ] );
+	} );
+}
+
 module.exports = {
-	addDomainItemsToCart( callback, dependencies, { domainItem, googleAppsCartItem, isPurchasingItem, siteUrl } ) {
+	addDomainItemsToCart( callback, dependencies, { domainItem, googleAppsCartItem, isPurchasingItem, siteUrl, themeSlug, themeItem } ) {
 		wpcom.undocumented().sitesNew( {
 			blog_name: siteUrl,
 			blog_title: siteUrl,
@@ -57,28 +67,42 @@ module.exports = {
 				return callback( error );
 			}
 
-			const siteSlug = response.blog_details.blogname + '.wordpress.com',
-				providedDependencies = {
-					siteSlug,
-					domainItem
-				},
-				addToCartAndProceed = () => {
-					if ( isPurchasingItem ) {
-						let newCartItems = [ domainItem ];
+			const siteSlug = response.blog_details.blogname + '.wordpress.com';
+			const isFreeThemePreselected = themeSlug && ! themeItem;
+			const providedDependencies = {
+				siteSlug,
+				domainItem,
+				themeItem,
+			};
+			const addToCartAndProceed = () => {
+				let newCartItems = [];
 
-						if ( googleAppsCartItem ) {
-							newCartItems = newCartItems.concat( googleAppsCartItem );
-						}
+				if ( domainItem ) {
+					newCartItems = [ ...newCartItems, domainItem ];
+				}
+				if ( googleAppsCartItem ) {
+					newCartItems = [ ...newCartItems, googleAppsCartItem ];
+				}
+				if ( themeItem ) {
+					newCartItems = [ ...newCartItems, themeItem ];
+				}
 
-						SignupCart.addToCart( siteSlug, newCartItems, function( cartError ) {
-							callback( cartError, providedDependencies );
-						} );
-					} else {
-						callback( [], providedDependencies );
-					}
-				};
+				if ( newCartItems.length ) {
+					SignupCart.addToCart( siteSlug, newCartItems, function( cartError ) {
+						callback( cartError, providedDependencies );
+					} );
+				} else {
+					callback( [], providedDependencies );
+				}
+			};
 
-			if ( user.get() ) {
+			if ( ! user.get() && isFreeThemePreselected ) {
+				return setThemeOnSite( addToCartAndProceed, { siteSlug }, { themeSlug } );
+			}
+
+			if ( user.get() && isFreeThemePreselected ) {
+				return fetchSitesAndUser( siteSlug, setThemeOnSite.bind( this, addToCartAndProceed, { siteSlug }, { themeSlug } ) );
+			} else if ( user.get() ) {
 				return fetchSitesAndUser( siteSlug, addToCartAndProceed );
 			}
 
@@ -139,13 +163,5 @@ module.exports = {
 		} );
 	},
 
-	setThemeOnSite( callback, { siteSlug }, { themeSlug } ) {
-		if ( isEmpty( themeSlug ) ) {
-			return defer( callback );
-		}
-
-		wpcom.undocumented().changeTheme( siteSlug, { theme: themeSlug }, function( errors ) {
-			callback( isEmpty( errors ) ? undefined : [ errors ] );
-		} );
-	}
+	setThemeOnSite: setThemeOnSite
 };

client/signup/config/flows.js
@@ -12,7 +12,7 @@ var config = require( 'config' ),
 	user = require( 'lib/user' )();
 
 function getCheckoutDestination( dependencies ) {
-	if ( dependencies.cartItem || dependencies.domainItem ) {
+	if ( dependencies.cartItem || dependencies.domainItem || dependencies.themeItem ) {
 		return '/checkout/' + dependencies.siteSlug;
 	}
 
@@ -45,6 +45,13 @@ const flows = {
 		},
 		description: 'Create an account and a blog and then add the business plan to the users cart.',
 		lastModified: '2016-01-21'
+	},
+
+	'with-theme': {
+		steps: [ 'domains', 'plans', 'user' ],
+		destination: getCheckoutDestination,
+		description: 'Preselect a theme to activate/buy from an external source',
+		lastModified: '2016-01-27'
 	},
 
 	main: {

client/signup/steps/domains/index.jsx
@@ -75,6 +75,16 @@ module.exports = React.createClass( {
 		} );
 	},
 
+	getThemeArgs: function() {
+		const isPurchasingTheme = this.props.queryObject && this.props.queryObject.premium;
+		const themeSlug = this.props.queryObject ? this.props.queryObject.theme : undefined;
+		const themeItem = isPurchasingTheme
+			? cartItems.themeItem( themeSlug, 'signup-with-theme' )
+			: undefined;
+
+		return { themeSlug, themeItem };
+	},
+
 	submitWithDomain: function( googleAppsCartItem ) {
 		const suggestion = this.props.step.suggestion,
 			isPurchasingItem = Boolean( suggestion.product_slug ),
@@ -88,7 +98,7 @@ module.exports = React.createClass( {
 				} ) :
 				undefined;
 
-		SignupActions.submitSignupStep( {
+		SignupActions.submitSignupStep( Object.assign( {
 			processingMessage: this.translate( 'Adding your domain' ),
 			stepName: this.props.stepName,
 			domainItem,
@@ -96,7 +106,7 @@ module.exports = React.createClass( {
 			isPurchasingItem,
 			siteUrl,
 			stepSectionName: this.props.stepSectionName
-		}, [], { domainItem } );
+		}, this.getThemeArgs() ), [], { domainItem } );
 
 		this.props.goToNextStep();
 	},
@@ -104,7 +114,7 @@ module.exports = React.createClass( {
 	handleAddMapping: function( sectionName, domain, state ) {
 		const domainItem = cartItems.domainMapping( { domain } );
 
-		SignupActions.submitSignupStep( {
+		SignupActions.submitSignupStep( Object.assign( {
 			processingMessage: this.translate( 'Adding your domain mapping' ),
 			stepName: this.props.stepName,
 			[ sectionName ]: state,
@@ -112,7 +122,7 @@ module.exports = React.createClass( {
 			isPurchasingItem: true,
 			siteUrl: domain,
 			stepSectionName: this.props.stepSectionName
-		} );
+		}, this.getThemeArgs() ) );
 
 		this.props.goToNextStep();
 	},

commit fb4437ba16f4f8273898b40e03a3fd3041c0a7b7

Signup: Headstart: Update the provided dependencies in Headstart's `domains-with-theme` step.
This fixes a fatal error introduced by 752fa72346341b12978f402e9f78891306586f49.

client/signup/config/steps.js
@@ -81,7 +81,7 @@ module.exports = {
 	'domains-with-theme': {
 		stepName: 'domains-with-theme',
 		apiRequestFunction: stepActions.addDomainItemsToCart,
-		providesDependencies: [ 'siteSlug', 'domainItem' ],
+		providesDependencies: [ 'siteSlug', 'domainItem', 'themeItem' ],
 		dependencies: [ 'theme' ],
 		delayApiRequestUntilComplete: true
 	},