8ce2dc17522268dc2d6c0c1756d87eff91a52bfe
Samuel Clemens <yury@michurin.co.il>
2016-02-03 04:52:02 +0200

    Validate reduxStore interface in non production environment

--- client/lib/react-helpers/index.js
+++ client/lib/react-helpers/index.js
@@ -4,12 +4,22 @@
 import ReactDom from 'react-dom';
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
+import config from 'config';
 
 export default {
 	renderWithReduxStore( reactElement, domContainer, reduxStore ) {
 		const domContainerNode = ( 'string' === typeof domContainer )
 				? document.getElementById( domContainer )
 				: domContainer;
+
+		if ( config( 'env' ) !== 'production' ) {
+			// validate that what's passed has indeed the interface of redux store
+			if ( typeof reduxStore.getState !== 'function'
+				|| typeof reduxStore.dispatch !== 'function'
+				|| typeof reduxStore.subscribe !== 'function' ) {
+				throw new Error('Invalid redux store supplied!');
+			}
+		}
 
 		return ReactDom.render(
 			React.createElement( ReduxProvider, { store: reduxStore }, reactElement ),
