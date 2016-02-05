2e473f225edeaee6618dd23ee2f2297a5565933e
Eric Binnion <ericbinnion@gmail.com>
2016-02-05 14:47:51 -0600

    Components: Fix ESLint issue in CountedTextarea

--- client/components/forms/counted-textarea/index.jsx
+++ client/components/forms/counted-textarea/index.jsx
@@ -34,34 +34,32 @@ export default React.createClass( {
 	},
 
 	renderCountPanel() {
-		const length = this.props.value.length;
-
+		let length = this.props.value.length;
 		if ( ! length && this.props.countPlaceholderLength ) {
 			length = this.props.placeholder.length;
 		}
 
+		let panelText;
 		if ( this.props.showRemainingCharacters && this.props.acceptableLength ) {
-			return ( <div className="counted-textarea__count-panel">
-				{ this.translate( '%d character remaining', '%d characters remaining', {
-					context: 'Input length',
-					args: [ this.props.acceptableLength - length ],
-					count: this.props.acceptableLength - length
-				} ) }
-					{ this.props.children }
-				</div>
-			);
+			panelText = this.translate( '%d character remaining', '%d characters remaining', {
+				context: 'Input length',
+				args: [ this.props.acceptableLength - length ],
+				count: this.props.acceptableLength - length
+			} );
 		} else {
-			return (
-				<div className="counted-textarea__count-panel">
-					{ this.translate( '%d character', '%d characters', {
-						context: 'Input length',
-						args: [ length ],
-						count: length
-					} ) }
-					{ this.props.children }
-				</div>
-			);
+			panelText = this.translate( '%d character', '%d characters', {
+				context: 'Input length',
+				args: [ length ],
+				count: length
+			} );
 		}
+
+		return (
+			<div className="counted-textarea__count-panel">
+				{ panelText }
+				{ this.props.children }
+			</div>
+		);
 	},
 
 	render() {
