3b709022a325c0977fc8464b84493554f8d13ab3
Samuel Clemens <yury@michurin.co.il>
2016-02-06 17:51:33 +0200

    Refactored Reader comments UI to use Redux
    - Removed CommentStore and CommentLikeStore
    - Should validate refs before accessing them
    - Validate post prop
    - Refactored form component to handle auto growth a bit better
    - Fix the click on reply issue when another reply box is active
    - Fixed style
    - Simplified the replay click fix
    - Remove all remaining comment store and comment like store files, and turn off associated tests
    - humanDate: show "just now" for times less than a minute ago
    - If a comment has 5 or fewer replies, show all replies (no hide/show controls)
    - Remove isRequired on props that may be null before provided by a Redux selector
    - Full post: check if we need to scroll to comments when receiving new shouldShowComments prop
    - Move 'cancel reply' link to right hand side
    - Order nested replies in chronological order
    - Return comment form to bottom after reply posted
    - When adding a new comment from the current user, skip the date sort so that it appears at the top of the current branch
    - Remove handleBlur in comment form. Component was unmounting before delay() completed, resulting in setState warnings

--- client/lib/comment-store/utils.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	key: function( siteId, postId ) {
-		if ( ! siteId || ! postId ) {
-			throw new Error( 'siteId and postId must be non-empty' );
-		}
-		return siteId + '-' + postId;
-	}
-};
