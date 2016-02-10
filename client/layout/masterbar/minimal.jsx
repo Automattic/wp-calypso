b60047d65c22ca642404eae873de976465f6c594
Steve Seear <steve.seear@gmail.com>
2016-02-10 16:53:58 +0000

    Masterbar: Tidy minimal masterbar render func

--- client/layout/masterbar/minimal.jsx
+++ client/layout/masterbar/minimal.jsx
@@ -12,21 +12,18 @@ import classnames from 'classnames';
  */
 import Item from './item';
 
-const MasterbarMinimal = ( { url, loggedOut, children } ) => {
-	const logoClassNames = classnames(
-		'masterbar__item-logo',
-		{ 'is-loggedout': loggedOut }
-	);
-
-	return(
-		<Masterbar>
-			<Item url={ url } icon="my-sites" className={ logoClassNames }>
-				WordPress<span className="tld">.com</span>
-			</Item>
-			{ children }
-		</Masterbar>
-	);
-};
+const MasterbarMinimal = ( { url, loggedOut, children } ) => (
+	<Masterbar>
+		<Item url={ url }
+			icon="my-sites"
+			className={ classnames(
+				'masterbar__item-logo',
+				{ 'is-loggedout': loggedOut } ) }>
+			WordPress<span className="tld">.com</span>
+		</Item>
+		{ children }
+	</Masterbar>
+);
 
 MasterbarMinimal.propTypes = {
 	url: React.PropTypes.string.isRequired,
