a56d49164f8bb912e9fa3d7a151af4c4edc72ee0
Andrew Duthie <andrew@andrewduthie.com>
2016-02-11 10:51:24 -0500

    Framework: Add post types Redux actions and reducer

---
+++ client/state/post-types/test/actions.js
@@ -0,0 +1,112 @@
+/**
+ * External dependencies
+ */
+import nock from 'nock';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import Chai, { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import {
+	POST_TYPES_RECEIVE,
+	POST_TYPES_REQUEST,
+	POST_TYPES_REQUEST_SUCCESS,
+	POST_TYPES_REQUEST_FAILURE
+} from 'state/action-types';
+import {
+	receivePostTypes,
+	requestPostTypes
+} from '../actions';
+
+describe( 'actions', () => {
+	const spy = sinon.spy();
+
+	before( () => {
+		Chai.use( sinonChai );
+	} );
+
+	beforeEach( () => {
+		spy.reset();
+	} );
+
+	after( () => {
+		nock.restore();
+	} );
+
+	describe( '#receivePostTypes()', () => {
+		it( 'should return an action object', () => {
+			const action = receivePostTypes( 2916284, [
+				{ name: 'post', label: 'Posts' }
+			] );
+
+			expect( action ).to.eql( {
+				type: POST_TYPES_RECEIVE,
+				siteId: 2916284,
+				types: [ { name: 'post', label: 'Posts' } ]
+			} );
+		} );
+	} );
+
+	describe( '#requestPostTypes()', () => {
+		before( () => {
+			nock( 'https://public-api.wordpress.com:443' )
+				.persist()
+				.get( '/rest/v1.1/sites/2916284/post-types' )
+				.reply( 200, {
+					found: 2,
+					post_types: [
+						{ name: 'post', label: 'Posts' },
+						{ name: 'page', label: 'Pages' }
+					]
+				} )
+				.get( '/rest/v1.1/sites/77203074/post-types' )
+				.reply( 403, {
+					error: 'authorization_required',
+					message: 'User cannot access this private blog.'
+				} );
+		} );
+
+		after( () => {
+			nock.cleanAll();
+		} );
+
+		it( 'should dispatch fetch action when thunk triggered', () => {
+			requestPostTypes( 2916284 )( spy );
+
+			expect( spy ).to.have.been.calledWith( {
+				type: POST_TYPES_REQUEST,
+				siteId: 2916284
+			} );
+		} );
+
+		it( 'should dispatch receive action when request completes', () => {
+			return requestPostTypes( 2916284 )( spy ).then( () => {
+				expect( spy ).to.have.been.calledWith( receivePostTypes( 2916284, [
+					{ name: 'post', label: 'Posts' },
+					{ name: 'page', label: 'Pages' }
+				] ) );
+			} );
+		} );
+
+		it( 'should dispatch request success action when request completes', () => {
+			return requestPostTypes( 2916284 )( spy ).then( () => {
+				expect( spy ).to.have.been.calledWith( {
+					type: POST_TYPES_REQUEST_SUCCESS,
+					siteId: 2916284
+				} );
+			} );
+		} );
+
+		it( 'should dispatch fail action when request fails', () => {
+			return requestPostTypes( 77203074 )( spy ).then( () => {
+				expect( spy ).to.have.been.calledWith( {
+					type: POST_TYPES_REQUEST_FAILURE,
+					siteId: 77203074,
+					error: sinon.match( { message: 'User cannot access this private blog.' } )
+				} );
+			} );
+		} );
+	} );
+} );
---
+++ client/state/post-types/test/reducer.js
@@ -0,0 +1,219 @@
+/**
+ * External dependencies
+ */
+import Chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import deepFreeze from 'deep-freeze';
+
+/**
+ * Internal dependencies
+ */
+import {
+	POST_TYPES_RECEIVE,
+	POST_TYPES_REQUEST,
+	POST_TYPES_REQUEST_FAILURE,
+	POST_TYPES_REQUEST_SUCCESS,
+	SERIALIZE,
+	DESERIALIZE
+} from 'state/action-types';
+import { requesting, items } from '../reducer';
+
+describe( 'reducer', () => {
+	before( () => {
+		Chai.use( sinonChai );
+		sinon.stub( console, 'warn' );
+	} );
+
+	after( () => {
+		console.warn.restore();
+	} );
+
+	describe( '#requesting()', () => {
+		it( 'should default to an empty object', () => {
+			const state = requesting( undefined, {} );
+
+			expect( state ).to.eql( {} );
+		} );
+
+		it( 'should set site ID to true value if request in progress', () => {
+			const state = requesting( undefined, {
+				type: POST_TYPES_REQUEST,
+				siteId: 2916284
+			} );
+
+			expect( state ).to.eql( {
+				2916284: true
+			} );
+		} );
+
+		it( 'should accumulate requesting values', () => {
+			const state = requesting( deepFreeze( {
+				2916284: true
+			} ), {
+				type: POST_TYPES_REQUEST,
+				siteId: 77203074
+			} );
+
+			expect( state ).to.eql( {
+				2916284: true,
+				77203074: true
+			} );
+		} );
+
+		it( 'should set site ID to false if request finishes successfully', () => {
+			const state = requesting( deepFreeze( {
+				2916284: true
+			} ), {
+				type: POST_TYPES_REQUEST_SUCCESS,
+				siteId: 2916284
+			} );
+
+			expect( state ).to.eql( {
+				2916284: false
+			} );
+		} );
+
+		it( 'should set site ID to false if request finishes with failure', () => {
+			const state = requesting( deepFreeze( {
+				2916284: true
+			} ), {
+				type: POST_TYPES_REQUEST_FAILURE,
+				siteId: 2916284
+			} );
+
+			expect( state ).to.eql( {
+				2916284: false
+			} );
+		} );
+
+		it( 'should not persist state', () => {
+			const state = requesting( deepFreeze( {
+				2916284: true
+			} ), {
+				type: SERIALIZE
+			} );
+
+			expect( state ).to.eql( {} );
+		} );
+
+		it( 'should not load persisted state', () => {
+			const state = requesting( deepFreeze( {
+				2916284: true
+			} ), {
+				type: DESERIALIZE
+			} );
+
+			expect( state ).to.eql( {} );
+		} );
+	} );
+
+	describe( '#items()', () => {
+		it( 'should default to an empty object', () => {
+			const state = items( undefined, {} );
+
+			expect( state ).to.eql( {} );
+		} );
+
+		it( 'should index post types by site ID, post type name pairing', () => {
+			const state = items( null, {
+				type: POST_TYPES_RECEIVE,
+				siteId: 2916284,
+				types: [
+					{ name: 'post', label: 'Posts' },
+					{ name: 'page', label: 'Pages' }
+				]
+			} );
+
+			expect( state ).to.eql( {
+				2916284: {
+					post: { name: 'post', label: 'Posts' },
+					page: { name: 'page', label: 'Pages' }
+				}
+			} );
+		} );
+
+		it( 'should accumulate sites', () => {
+			const state = items( deepFreeze( {
+				2916284: {
+					post: { name: 'post', label: 'Posts' },
+					page: { name: 'page', label: 'Pages' }
+				}
+			} ), {
+				type: POST_TYPES_RECEIVE,
+				siteId: 77203074,
+				types: [
+					{ name: 'post', label: 'Posts' }
+				]
+			} );
+
+			expect( state ).to.eql( {
+				2916284: {
+					post: { name: 'post', label: 'Posts' },
+					page: { name: 'page', label: 'Pages' }
+				},
+				77203074: {
+					post: { name: 'post', label: 'Posts' }
+				}
+			} );
+		} );
+
+		it( 'should override previous post types of same site ID', () => {
+			const state = items( deepFreeze( {
+				2916284: {
+					post: { name: 'post', label: 'Posts' },
+					page: { name: 'page', label: 'Pages' }
+				}
+			} ), {
+				type: POST_TYPES_RECEIVE,
+				siteId: 2916284,
+				types: [
+					{ name: 'post', label: 'Posts' }
+				]
+			} );
+
+			expect( state ).to.eql( {
+				2916284: {
+					post: { name: 'post', label: 'Posts' }
+				}
+			} );
+		} );
+
+		it( 'should persist state', () => {
+			const state = items( deepFreeze( {
+				2916284: {
+					post: { name: 'post', label: 'Posts' }
+				}
+			} ), { type: SERIALIZE } );
+
+			expect( state ).to.eql( {
+				2916284: {
+					post: { name: 'post', label: 'Posts' }
+				}
+			} );
+		} );
+
+		it( 'should load valid persisted state', () => {
+			const state = items( deepFreeze( {
+				2916284: {
+					post: { name: 'post', label: 'Posts' }
+				}
+			} ), { type: DESERIALIZE } );
+
+			expect( state ).to.eql( {
+				2916284: {
+					post: { name: 'post', label: 'Posts' }
+				}
+			} );
+		} );
+
+		it( 'should not load invalid persisted state', () => {
+			const state = items( deepFreeze( {
+				post: { name: 'post', label: 'Posts' }
+			} ), { type: DESERIALIZE } );
+
+			expect( state ).to.eql( {} );
+			expect( console.warn ).to.have.been.calledOnce;
+		} );
+	} );
+} );
