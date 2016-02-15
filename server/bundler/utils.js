f2579c4418c2611ce1a7fa66fd4da44cc301d9b5
Bernhard Reiter <ockham@raz.or.at>
2016-02-15 13:06:56 +0100

    Remove the `utils.regExpToString` polyfill
    That was needed for node 0.x, which we no longer support.
    
    This is basically reverting commit f472d91e5b3f3764acbb4d8471d7f1073644d323 from
    the old repo (`calypso-pre-oss`).

--- server/bundler/utils.js
+++ server/bundler/utils.js
@@ -18,22 +18,7 @@ function pathToRegExp( path ) {
 	return new RegExp( '^' + path + '(/.*)?$' );
 }
 
-/**
- * Node 0.x does not correctly escape slashes in `RegExp.prototype.toString`,
- * but node 4.x does. This is a polyfill that should return the escaped string
- * in either version. It can also accept any other object that has a `toString`
- * method (like Strings).
- */
-function regExpToString( exp ) {
-	var str = exp.toString();
-	if ( /\\\//.test( str ) ) {
-		return str;
-	}
-	return str.replace( /\//g, '\\/' );
-}
-
 module.exports = {
 	getAssets: getAssets,
-	pathToRegExp: pathToRegExp,
-	regExpToString: regExpToString
+	pathToRegExp: pathToRegExp
 };
