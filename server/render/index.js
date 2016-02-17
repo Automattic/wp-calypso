4217bb509660fa068e860b6822f25eb06e7f7a20
ehg <chris.blower@jumpwire.co.uk>
2016-02-17 17:30:04 +0000

    Server Side Rendering: Use lru-cache instead of LruMap
    We already have lru-cache, it's ISC, and doesn't cause build issues for
    desktop.

--- server/render/index.js
+++ server/render/index.js
@@ -5,15 +5,15 @@ import ReactDomServer from 'react-dom/server';
 import Helmet from 'react-helmet';
 import memoize from 'lodash/function/memoize';
 import superagent from 'superagent';
-import LruMap from 'lru-map';
+import Lru from 'lru-cache';
 
 /**
  * Internal dependencies
  */
 import config from 'config';
 
+const markupCache = new Lru( { max: 1000 } );
 const memoizedRenderToString = memoize( ReactDomServer.renderToString, element => JSON.stringify( element ) );
-memoizedRenderToString.cache = new LruMap( { maxSize: 1000 } );
 
 function bumpStat( group, name ) {
 	const url = `http://pixel.wp.com/g.gif?v=wpcom-no-pv&x_${ group }=${ name }&t=${ Math.random() }`;
@@ -24,6 +24,8 @@ function bumpStat( group, name ) {
 }
 
 export function render( element ) {
+	memoizedRenderToString.cache = markupCache;
+
 	if ( ! memoizedRenderToString.cache.has( element ) ) {
 		bumpStat( 'calypso-ssr', 'loggedout-design-cache-miss' );
 	}
