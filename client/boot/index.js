f9ce5fdfe5a0dc14f3fe51bbab017e1951892f6d
Rastislav Lamos <lamos.rasto@gmail.com>
2016-02-22 10:29:13 +0100

    Add route handler for 404 requests
    Add a new catch-all route which handles 404 requests. If code-splitting
    is disabled and we got to this route handler, it means no section was
    matched. If code-splitting was enabled and no `state.ui.chunkName` is
    set, it means that no section partial route was matched and therefore no
    section JS chunk is loaded.

--- client/boot/index.js
+++ client/boot/index.js
@@ -348,6 +348,29 @@ function reduxStoreReady( reduxStore ) {
 		require( 'lib/rubberband-scroll-disable' )( document.body );
 	}
 
+	// 404 handler.
+	page( '*', function( context, next ) {
+		if ( ! config.isEnabled( 'code-splitting' ) ) {
+			// If code-splitting is not enabled and we got to this route handler, no section route was matched.
+
+			// Show 404 component.
+
+			return;
+		}
+
+		var appState = context.store.getState();
+
+		// When code-splitting is enabled, chunkName is set when a section route is matched (e.g.: /stats).
+		// If it is not set, no section route was matched and no section JS chunk is loaded. Show 404.
+		if ( ! appState.ui.chunkName ) {
+			// Show 404 component.
+			
+			return;
+		}
+
+		next();
+	} );
+
 	detectHistoryNavigation.start();
 	page.start();
 }
