1befa248e46e9fd0f774462bfa845284be68c17e
Alex Gyoshev <alex@gyoshev.net>
2016-02-23 15:05:36 +0200

    Use count method instead of length.
    Add tests for last page flag.
    
    Closes #3493

--- client/lib/recommended-sites-store/test/store-tests.js
+++ client/lib/recommended-sites-store/test/store-tests.js
@@ -6,6 +6,10 @@ import store from '../store';
 
 
 describe( 'Recommended Sites Store', function() {
+	beforeEach( function() {
+		store._reset();
+	} );
+
 	it( 'picks up recs from a successful response', function() {
 		const foundBlog = {
 			blog_id: 63142948,
@@ -24,5 +28,38 @@ describe( 'Recommended Sites Store', function() {
 
 		expect( store.get() ).to.have.length( 1 );
 		expect( store.get()[0] ).to.eql( foundBlog );
+		expect( store.isLastPage() ).to.eql( false );
+	} );
+
+	it( 'sets last page flag if no blogs are returned', function() {
+		Dispatcher.handleServerAction( {
+			type: ACTION_RECEIVE_SITE_RECOMMENDATIONS,
+			data: {
+				blogs: [ ]
+			}
+		} );
+
+		expect( store.isLastPage() ).to.eql( true );
+	} );
+
+	it( 'sets last page flag if maximum recommendations are reached', function() {
+		const tooManyBlogs = [];
+
+		for (let i = 0; i < store.maxRecommendations(); i++) {
+			tooManyBlogs.push( {
+				blog_id: i,
+				follow_reco_id: 'freshly_pressed',
+				reason: 'Featured on Freshly Pressed'
+			} );
+		}
+
+		Dispatcher.handleServerAction( {
+			type: ACTION_RECEIVE_SITE_RECOMMENDATIONS,
+			data: {
+				blogs: tooManyBlogs
+			}
+		} );
+
+		expect( store.isLastPage() ).to.eql( true );
 	} );
 } );
