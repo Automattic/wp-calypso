0081e103c5c7de4ace6387514900792aa57f58c3
Rastislav Lamos <lamos.rasto@gmail.com>
2016-02-23 15:47:59 +0100

    Unmount sidebar when displaying 404 page

--- client/boot/index.js
+++ client/boot/index.js
@@ -371,6 +371,7 @@ function reduxStoreReady( reduxStore ) {
 }
 
 function show404( context ) {
+	ReactDom.unmountComponentAtNode( document.getElementById( 'secondary' ) );
 	context.store.dispatch( setSection( null, { hasSidebar: false } ) );
 
 	Page404 = require( 'layout/404' );
