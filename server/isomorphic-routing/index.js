a1570745d3a598e4b82885a6f267c4f515007d7e
Miguel Fonseca <miguelcsf@gmail.com>
2016-02-24 01:51:27 +0000

    isomorphic-routing: propagate context via middleware folding
    - N.B.: applyMiddlewares & compose have been tested together in vitro,
      but this whole refactored file hasn't.
    
    - The idea is that, by controlling how middleware is applied, we can
      bind a certain shared context to all the middleware that gets folded
      for the handling of a specific server request.
    
    - This frees us from keeping state like `context` and `store` as
      dangling variables, potentially shared across requests.

--- server/isomorphic-routing/index.js
+++ server/isomorphic-routing/index.js
@@ -4,7 +4,6 @@
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import pick from 'lodash/object/pick';
-import isEmpty from 'lodash/lang/isEmpty';
 
 /**
  * Internal dependencies
@@ -17,70 +16,68 @@ import { render } from 'render';
 import { createReduxStore } from 'state';
 import { setSection } from 'state/ui/actions';
 
-function getEnhancedContext( context, req ) {
+function getEnhancedContext( context, req, res ) {
 	return Object.assign( {}, context, {
+		isLoggedIn: req.cookies.wordpress_logged_in,
+		isServerSide: true,
 		path: req.path,
 		params: Object.assign( {}, context.params, req.params ),
-		query: {}
+		query: {},
+		store: createReduxStore(),
+		res,
 	} );
+}
+
+function applyMiddlewares( context, mws ) {
+	const liftedMws = mws.map( mw => next => mw( context, next ) );
+	compose( ...liftedMws )();
+}
+
+function compose( ...fns ) {
+	return fns.reduceRight( ( composed, f ) => (
+		next => f( composed ) // eslint-disable-line no-unused-vars
+	), () => {} );
 }
 
 export default function( expressApp, getDefaultContext ) {
 	sections.get()
 		.filter( section => section.routing === 'isomorphic' )
 		.forEach( section => {
-			// Since each middleware can mutate context, we need to set it up here
-			// and preserve it over iterations. If this turns out to be too complex,
-			// we might want to resort to the inverse approach, i.e. using
-			// https://github.com/kethinov/page.js-express-mapper.js on the client side.
-			let context = {};
-			let store = createReduxStore();
+			section.require( section.module )( serverRouter );
 
-			// Maybe inline those functions below?
+			function serverRouter( route, ...middlewares ) {
+				expressApp.get( route, combinedMiddlewares );
 
-			// Top-level routes
-			section.paths.forEach( path => expressApp.get( path, setUpRoute ) );
-			// Individual routes from chunk
-			sections.require( section.module )( serverRouter ); // possibly also pass context.isLoggedIn/isServerSide?
-			// Render!
-			section.paths.forEach( path => expressApp.get( path, serverRender ) );
+				function combinedMiddlewares( req ) {
+					const context = getDefaultContext( req );
+					getEnhancedContext( context );
+					applyMiddlewares( context, ...[
+						setUpRoute,
+						...middlewares,
+						serverRender
+					] );
+				}
+			}
 
-			function setUpRoute( req, res, next ) {
+			function setUpRoute( context, next ) {
 				i18n.initialize();
-				context.isServerSide = true;
-				context.isLoggedIn = req.cookies.wordpress_logged_in;
-
-				store.dispatch( setSection( section.name, {
+				context.store.dispatch( setSection( section.name, {
 					hasSidebar: false,
 					isFullScreen: true
 				} ) ); // FIXME
-				context.initialReduxState = pick( store.getState(), 'ui' );
+				context.initialReduxState = pick( context.store.getState(), 'ui' );
 				next();
 			}
 
-			function liftMiddleware( pageMiddleware ) {
-				return ( req, res, next ) => {
-					if ( isEmpty( context ) ) {
-						context = getDefaultContext( req );
-					}
-					getEnhancedContext( context, req ); // Update path etc.
-					pageMiddleware( context, next );
-				}
-			}
-
-			function serverRouter( route, ...mws ) {
-				expressApp.get( route, mws.map( liftMiddleware ) );
-			}
-
-			function serverRender( req, res ) {
+			function serverRender( context ) {
 				if ( config.isEnabled( 'server-side-rendering' ) ) {
 					Object.assign( context, render( (
-						<ReduxProvider store={ store }>
-							<LayoutLoggedOutDesign store={ store } primary={ context.primary } />
+						<ReduxProvider store={ context.store }>
+							<LayoutLoggedOutDesign store={ context.store } primary={ context.primary } />
 						</ReduxProvider>
 					) ) ); // FIXME: tier (general props), Head
 				}
-				res.render( 'index.jade', context );
+				context.res.render( 'index.jade', context );
 			}
 		} );
 };
