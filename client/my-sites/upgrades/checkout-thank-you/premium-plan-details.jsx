584b052d01faf3cf16e407f3f3edcc8838dd8567
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-02-24 17:13:49 -0800

    Checkout: Thank You: Update `PremiumPlanDetails` with new copy

--- client/my-sites/upgrades/checkout-thank-you/premium-plan-details.jsx
+++ client/my-sites/upgrades/checkout-thank-you/premium-plan-details.jsx
@@ -21,15 +21,24 @@ const PremiumPlanDetails = ( { isFreeTrial, selectedSite } ) => {
 				showGetFreeDomainTip
 				? <PurchaseDetail
 						additionalClass="get-free-domain"
-						title={ i18n.translate( 'Get a free domain' ) }
-						description={ i18n.translate( 'WordPress.com Premium includes a free domain for your site.' ) }
-						buttonText={ i18n.translate( 'Add Free Domain' ) }
+						title={ i18n.translate( 'Get your custom domain' ) }
+						description={
+							i18n.translate(
+								"Replace your site's address, {{em}}%(siteDomain)s{{/em}}, with a custom domain. " +
+								'A free domain is included with your plan.',
+								{
+									args: { siteDomain: selectedSite.domain },
+									components: { em: <em /> }
+								}
+							)
+						}
+						buttonText={ i18n.translate( 'Claim your free domain' ) }
 						href={ '/domains/add/' + selectedSite.slug } />
 				: <PurchaseDetail
 						additionalClass="ads-have-been-removed"
 						title={ i18n.translate( 'Ads have been removed!' ) }
 						description={ i18n.translate( 'WordPress.com ads will not show up on your blog.' ) }
-						buttonText={ i18n.translate( 'View Your Site' ) }
+						buttonText={ i18n.translate( 'View your site' ) }
 						href={ selectedSite.URL }
 						target="_blank" />
 			}
@@ -37,16 +46,26 @@ const PremiumPlanDetails = ( { isFreeTrial, selectedSite } ) => {
 			<PurchaseDetail
 				additionalClass="customize-fonts-and-colors"
 				title={ i18n.translate( 'Customize Fonts & Colors' ) }
-				description={ i18n.translate( 'You now have access to full font and CSS editing capabilites.' ) }
-				buttonText={ i18n.translate( 'Customize Your Site' ) }
+				description={
+					i18n.translate(
+						"You now have direct control over your site's fonts and colors in the customizer. " +
+						"Change your site's entire look in a few clicks."
+					)
+				}
+				buttonText={ i18n.translate( 'Start customizing' ) }
 				href={ customizeLink }
 				target={ config.isEnabled( 'manage/customize' ) ? undefined : '_blank' } />
 
 			<PurchaseDetail
 				additionalClass="upload-to-videopress"
 				title={ i18n.translate( 'Upload to VideoPress' ) }
-				description={ i18n.translate( "Uploading videos to your blog couldn't be easier." ) }
-				buttonText={ i18n.translate( 'Start Using VideoPress' ) }
+				description={
+					i18n.translate(
+						'Upload video and audio files directly to your site, with no ads or limits. ' +
+						'The Premium plan also adds 10GB of file storage.'
+					)
+				}
+				buttonText={ i18n.translate( 'Upload files' ) }
 				href={ selectedSite.URL + '/wp-admin/media-new.php' }
 				target="_blank" />
 		</ul>
