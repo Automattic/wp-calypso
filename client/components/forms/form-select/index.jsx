b6f2b11216c706a1a3982028a83514914f291a5d
Igor Klimer <igor.klimer@automattic.com>
2016-02-25 02:09:01 +0100

    Components: add support for isError state in form-select

--- client/components/forms/form-select/index.jsx
+++ client/components/forms/form-select/index.jsx
@@ -2,13 +2,23 @@
  * External dependencies
  */
 import React from 'react';
-import classnames from 'classnames';
+import classNames from 'classnames';
 import omit from 'lodash/omit';
 
 const FormSelect = React.createClass( {
+	getDefaultProps() {
+		return {
+			isError: false,
+		};
+	},
+
 	render() {
+		const classes = classNames( this.props.className, 'form-select', {
+			'is-error': this.props.isError,
+		} );
+
 		return (
-			<select { ...omit( this.props, 'classname' ) } className={ classnames( this.props.className, 'form-select' ) } >
+			<select { ...omit( this.props, 'className' ) } className={ classes } >
 				{ this.props.children }
 			</select>
 		);
