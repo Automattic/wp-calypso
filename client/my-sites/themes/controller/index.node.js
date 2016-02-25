9ea8363cca6a25883ec73cb290104c023d9688c9
Bernhard Reiter <ockham@raz.or.at>
2016-02-25 16:01:48 +0100

    Themes: Add `package.json` switch
    Add a `package.json` that sets different entry points on the server and in
    the browser (not everything required by `my-sites/themes/index.js` is
    SSR-ready yet, so we need a pruned version for the server).

--- client/my-sites/themes/controller/index.node.js
+++ client/my-sites/themes/controller/index.node.js
@@ -8,56 +8,16 @@ import omit from 'lodash/omit';
 /**
  * Internal Dependencies
  */
-import SingleSiteComponent from 'my-sites/themes/single-site';
-import MultiSiteComponent from 'my-sites/themes/multi-site';
-import LoggedOutComponent from 'my-sites/themes/logged-out';
 import { ThemeSheet as ThemeSheetComponent } from 'my-sites/themes/sheet';
 import ThemeDetailsComponent from 'components/data/theme-details';
-import analytics from 'analytics';
 import i18n from 'lib/mixins/i18n';
-import trackScrollPage from 'lib/track-scroll-page';
 import buildTitle from 'lib/screen-title/utils';
-import { getAnalyticsData } from './helpers';
 import { getCurrentUser } from 'state/current-user/selectors';
 import { setSection } from 'state/ui/actions';
 import ClientSideEffects from 'components/client-side-effects';
 import LayoutLoggedOut from 'layout/logged-out';
 
-function getProps( context ) {
-	const { tier, site_id: siteId } = context.params;
-
-	const title = buildTitle(
-		i18n.translate( 'Themes', { textOnly: true } ),
-		{ siteID: siteId } );
-
-	const { basePath, analyticsPageTitle } = getAnalyticsData(
-		context.path,
-		tier,
-		siteId
-	);
-
-	const runClientAnalytics = function() {
-		analytics.pageView.record( basePath, analyticsPageTitle );
-	};
-
-	const boundTrackScrollPage = function() {
-		trackScrollPage(
-			basePath,
-			analyticsPageTitle,
-			'Themes'
-		);
-	};
-
-	return {
-		title,
-		tier,
-		search: context.query.s,
-		trackScrollPage: boundTrackScrollPage,
-		runClientAnalytics
-	};
-}
-
-function makeElement( ThemesComponent, Head, store, props ) {
+export function makeElement( ThemesComponent, Head, store, props ) {
 	return(
 		<ReduxProvider store={ store }>
 			<Head title={ props.title } tier={ props.tier || 'all' }>
@@ -84,49 +44,6 @@ export function makeLoggedOutLayout( context, next ) {
 	next();
 };
 
-export function singleSite( context, next ) {
-	const Head = require( 'layout/head' );
-	const { site_id: siteId } = context.params;
-	const props = getProps( context );
-
-	props.key = siteId;
-	props.siteId = siteId;
-
-	context.store.dispatch( setSection( 'design', {
-		hasSidebar: true,
-		isFullScreen: false
-	} ) );
-
-	context.primary = makeElement( SingleSiteComponent, Head, context.store, props );
-	next();
-}
-
-export function multiSite( context, next ) {
-	const Head = require( 'layout/head' );
-	const props = getProps( context );
-
-	context.store.dispatch( setSection( 'design', {
-		hasSidebar: true,
-		isFullScreen: false
-	} ) );
-
-	context.primary = makeElement( MultiSiteComponent, Head, context.store, props );
-	next();
-}
-
-export function loggedOut( context, next ) {
-	const Head = require( 'my-sites/themes/head' );
-	const props = getProps( context );
-
-	context.store.dispatch( setSection( 'design', {
-		hasSidebar: false,
-		isFullScreen: false
-	} ) );
-
-	context.primary = makeElement( LoggedOutComponent, Head, context.store, props );
-	next();
-}
-
 export function details( context, next ) {
 	const user = getCurrentUser( context.store.getState() );
 	const Head = user
@@ -146,7 +63,6 @@ export function details( context, next ) {
 		isFullScreen: true
 	} ) );
 
-	context.secondary = null; // When we're logged in, we need to remove the sidebar.
 	//TODO: use makeElement()
 	context.primary = (
 		<ReduxProvider store={ context.store } >
@@ -157,5 +73,6 @@ export function details( context, next ) {
 			</Head>
 		</ReduxProvider>
 	);
+	context.secondary = null; // When we're logged in, we need to remove the sidebar.
 	next();
 }
---
+++ client/my-sites/themes/index.node.js
@@ -0,0 +1,32 @@
+/**
+ * Internal dependencies
+ */
+import config from 'config';
+import { details, makeLoggedOutLayout } from './controller';
+
+// FIXME: These routes will SSR the logged-out Layout even if logged-in.
+// While subsequently replaced by the logged-in Layout on the client-side,
+// we'll want to render it on the server, too.
+
+// `logged-out` middleware isn't SSR-compliant yet, but we can at least render
+// the layout.
+// FIXME: Also create loggedOut/multiSite/singleSite elements, depending on route.
+const designRoutes = {
+	'/design': [ makeLoggedOutLayout ],
+	'/design/type/:tier': [ makeLoggedOutLayout ]
+};
+
+const themesRoutes = {
+	'/theme/:slug/:section?/:site_id?': [ details, makeLoggedOutLayout ]
+};
+
+const routes = Object.assign( {},
+	config.isEnabled( 'manage/themes' ) ? designRoutes : {},
+	config.isEnabled( 'manage/themes/details' ) ? themesRoutes : {}
+)
+
+export default function( router ) {
+	Object.keys( routes ).forEach( route => {
+		router( route, ...routes[ route ] );
+	} )
+};
