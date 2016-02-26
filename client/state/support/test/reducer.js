1c09b76d525530cef0f89492ea9e11de201b8afa
Kerry Liu <kerryliu@gmail.com>
2016-02-26 15:43:55 -0800

    Framework: do not persist support

--- client/state/support/test/reducer.js
+++ client/state/support/test/reducer.js
@@ -13,13 +13,16 @@ import {
 } from 'state/action-types';
 import {
 	isSupportUser,
+	isTransitioning,
+	showDialog,
+	errorMessage
 } from '../reducer';
 
 describe( 'reducer', () => {
 	describe( '#isSupportUser()', () => {
 		it( 'should set to true after activate', () => {
 			const state = isSupportUser( false, {
-				type: SUPPORT_USER_ACTIVATE,
+				type: SUPPORT_USER_ACTIVATE
 			} );
 
 			expect( state ).to.equal( true );
@@ -37,6 +40,51 @@ describe( 'reducer', () => {
 				type: DESERIALIZE
 			} );
 			expect( state ).to.equal( false );
+		} );
+	} );
+	describe( '#isTransitioning()', () => {
+		it( 'should never persist state', () => {
+			const state = isTransitioning( true, {
+				type: SERIALIZE
+			} );
+			expect( state ).to.equal( false );
+		} );
+
+		it( 'should never load persisted state', () => {
+			const state = isTransitioning( true, {
+				type: DESERIALIZE
+			} );
+			expect( state ).to.equal( false );
+		} );
+	} );
+	describe( '#showDialog()', () => {
+		it( 'should never persist state', () => {
+			const state = showDialog( true, {
+				type: SERIALIZE
+			} );
+			expect( state ).to.equal( false );
+		} );
+
+		it( 'should never load persisted state', () => {
+			const state = showDialog( true, {
+				type: DESERIALIZE
+			} );
+			expect( state ).to.equal( false );
+		} );
+	} );
+	describe( '#errorMessage()', () => {
+		it( 'should never persist state', () => {
+			const state = errorMessage( true, {
+				type: SERIALIZE
+			} );
+			expect( state ).to.equal( null );
+		} );
+
+		it( 'should never load persisted state', () => {
+			const state = errorMessage( true, {
+				type: DESERIALIZE
+			} );
+			expect( state ).to.equal( null );
 		} );
 	} );
 } );
