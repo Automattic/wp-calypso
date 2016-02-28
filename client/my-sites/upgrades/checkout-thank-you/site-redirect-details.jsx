e4ee5410b86cd775216be86d9beef1bd9f3ee1d0
Stéphane Thomas <email@stephanethomas.com>
2016-02-28 16:03:36 +0100

    Checkout: Thank You: Update copy for site redirect

--- client/my-sites/upgrades/checkout-thank-you/site-redirect-details.jsx
+++ client/my-sites/upgrades/checkout-thank-you/site-redirect-details.jsx
@@ -10,29 +10,38 @@ import { getDomainManagementUrl } from './utils';
 import i18n from 'lib/mixins/i18n';
 import PurchaseDetail from 'components/purchase-detail';
 
-const SiteRedirectDetails = ( { selectedSite } ) => {
+const SiteRedirectDetails = ( { selectedSite, domain } ) => {
 	return (
 		<div>
 			<PurchaseDetail
 				icon="external"
-				title={ i18n.translate( 'Redirect now working' ) }
-				description={ i18n.translate( 'Visitors to your site will be redirected to your chosen target.' ) }
-				buttonText={ i18n.translate( 'Test Redirect' ) }
-				href={ selectedSite.URL }
+				title={ i18n.translate( 'Test the redirect' ) }
+				description={
+					i18n.translate(
+						'Visitors to your site will be automatically redirected to {{em}}%(url)s{{/em}}.',
+						{
+							args: { url: domain },
+							components: { em: <em /> }
+						}
+					)
+				}
+				buttonText={ i18n.translate( 'Try it now' ) }
+				href={ `http://${ selectedSite.wpcom_url }` }
 				target="_blank" />
 
 			<PurchaseDetail
 				icon="cog"
 				title={ i18n.translate( 'Change redirect settings' ) }
-				description={ i18n.translate( 'You can disable the redirect or change the target at any time.' ) }
-				buttonText={ i18n.translate( 'My Domains' ) }
-				href={ getDomainManagementUrl( selectedSite ) } />
+				description={ i18n.translate( 'Disable the redirect by choosing a different primary domain, or change the target address.' ) }
+				buttonText={ i18n.translate( 'Manage redirect' ) }
+				href={ getDomainManagementUrl( selectedSite, domain ) } />
 		</div>
 	);
 };
 
 SiteRedirectDetails.propTypes = {
-	selectedSite: React.PropTypes.object.isRequired
+	selectedSite: React.PropTypes.object.isRequired,
+	domain: React.PropTypes.string.isRequired
 };
 
 export default SiteRedirectDetails;
