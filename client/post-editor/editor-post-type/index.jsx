9568e73cb6d800ffaca30da0eed329211a6ff987
Andrew Duthie <andrew@andrewduthie.com>
2016-03-03 13:58:25 -0500

    Editor: Remove deprecated post type switcher feature

--- client/post-editor/editor-post-type/index.jsx
+++ client/post-editor/editor-post-type/index.jsx
@@ -3,21 +3,6 @@
  */
 import React from 'react';
 import PureRenderMixin from 'react-pure-render/mixin';
-import page from 'page';
-import classNames from 'classnames';
-
-/**
- * Internal dependencies
- */
-import config from 'config';
-import Gridicon from 'components/gridicon';
-import PopoverMenu from 'components/popover/menu';
-import PopoverMenuItem from 'components/popover/menu-item';
-
-/**
- * Module variables
- */
-const isSwitcherEnabled = config.isEnabled( 'post-editor/post-type-switch' );
 
 export default React.createClass( {
 
@@ -34,25 +19,7 @@ export default React.createClass( {
 
 	propTypes: {
 		isNew: React.PropTypes.bool,
-		type: React.PropTypes.string,
-		siteSlug: React.PropTypes.string
-	},
-
-	getInitialState() {
-		return {
-			showMenu: false
-		};
-	},
-
-	toggleMenu() {
-		if ( isSwitcherEnabled ) {
-			this.setState( { showMenu: ! this.state.showMenu } );
-		}
-	},
-
-	startNew( type ) {
-		this.setState( { showMenu: false } );
-		page( '/' + type + '/' + this.props.siteSlug );
+		type: React.PropTypes.string
 	},
 
 	getLabel() {
@@ -74,34 +41,9 @@ export default React.createClass( {
 	},
 
 	render() {
-		const classes = classNames( 'editor-post-type', {
-			'is-switcher-enabled': isSwitcherEnabled
-		} );
-
 		return (
-			<span className={ classes } ref="postType" onClick={ this.toggleMenu }>
+			<span className="editor-post-type">
 				{ this.getLabel() }
-				{ isSwitcherEnabled && (
-					<Gridicon icon="chevron-down" size={ 16 } />
-				) }
-				{ isSwitcherEnabled && (
-					<PopoverMenu
-						className="popover editor-post-type__menu"
-						isVisible={ this.state.showMenu }
-						onClose={ this.toggleMenu }
-						position={ 'bottom' }
-						context={ this.refs && this.refs.postType }
-					>
-						<PopoverMenuItem onClick={ this.startNew.bind( this, 'post' ) }>
-							<Gridicon icon="posts" size={ 18 } />
-							{ this.translate( 'Start a new post' ) }
-						</PopoverMenuItem>
-						<PopoverMenuItem onClick={ this.startNew.bind( this, 'page' ) }>
-							<Gridicon icon="pages" size={ 18 } />
-							{ this.translate( 'Start a new page' ) }
-						</PopoverMenuItem>
-					</PopoverMenu>
-				) }
 			</span>
 		);
 	}
