647f2a317d4338c1d5b190e54c849b9b494c4f59
Ben Lowery <ben.lowery@automattic.com>
2016-03-05 21:08:12 -0500

    Use a clean cache, debug for logging.
    Since we're running all the tests in one big process now, we have to use a clean cache if we're mocking anything that another test run might require. A clean cache invalidates any requires done so far and lets the newly mocked modules get pulled in.

--- client/test/quick-mock.js
+++ client/test/quick-mock.js
@@ -2,14 +2,19 @@ import useMockery from './use-mockery';
 import glob from 'glob';
 import mockery from 'mockery';
 import path from 'path';
+import debug from 'debug';
+
+const log = debug( 'calypso:test:quick-mock' );
 
 function findQuickMocks( dirpath ) {
 	const fakes = glob.sync( '*/**/*.+(js|jsx)', {
 		cwd: dirpath
 	} );
 	fakes.forEach( function( fake ) {
-		const moduleName = fake.substring( fake, fake.lastIndexOf( '.' ) );
-		mockery.registerSubstitute( moduleName, path.join( dirpath, fake ) );
+		const moduleName = fake.substring( fake, fake.lastIndexOf( '.' ) ),
+			fakePath = path.join( dirpath, fake );
+		log( 'registering %s -> %s', moduleName, fakePath );
+		mockery.registerSubstitute( moduleName, fakePath );
 	} );
 }
 
