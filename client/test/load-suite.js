804ab00c7e30691e43753645e8a8903564ad1a38
Ben Lowery <ben.lowery@automattic.com>
2016-03-06 08:53:58 -0500

    Teach runner about the whitelist, use it from make

--- client/test/load-suite.js
+++ client/test/load-suite.js
@@ -1,22 +1,3 @@
-/**
- * External dependencies
- */
-import Chai from 'chai';
-import sinonChai from 'sinon-chai';
-import nock from 'nock';
-
-// These act like global before / after lifecycle bits
-before( () => {
-	Chai.use( sinonChai );
-	nock.disableNetConnect();
-} );
-
-after( () => {
-	nock.cleanAll();
-	nock.enableNetConnect();
-	nock.restore();
-} );
-
 function requireTestFiles( config, path = '' ) {
 	Object.keys( config ).map( ( folderName ) => {
 		const folderConfig = config[ folderName ];
--- client/test/runner.js
+++ client/test/runner.js
@@ -1,19 +1,15 @@
 require( 'babel/register' );
 
 const Mocha = require( 'mocha' ),
-	glob = require( 'glob' ),
 	Chai = require( 'chai' ),
 	sinonChai = require( 'sinon-chai' ),
-	nock = require( 'nock' );
+	nock = require( 'nock' ),
+	path = require( 'path' );
 
 const mocha = new Mocha( {
 	ui: 'bdd',
 	reporter: 'spec'
 } );
-
-const tests = process.argv.length > 2
-	? process.argv.slice( 2 )
-	: glob.sync( '**/test/index.js', {} );
 
 mocha.suite.beforeAll( function() {
 	Chai.use( sinonChai );
@@ -26,9 +22,14 @@ mocha.suite.afterAll( function() {
 	nock.restore();
 } );
 
-tests.forEach( function( file ) {
-	mocha.addFile( file );
-} );
+// we could also discover all the tests using a glob?
+if ( process.argv.length > 2 ) {
+	process.argv.slice( 2 ).forEach( function( file ) {
+		mocha.addFile( file );
+	} );
+} else {
+	mocha.addFile( path.join( __dirname, 'load-suite.js' ) );
+}
 
 mocha.run( function( failures ) {
 	process.on( 'exit', function() {
