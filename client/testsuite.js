ed5e73509d1838d52c01646dfc0019334bc266c7
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-03-06 10:11:39 +0000

    Framework: Move nested test requires to single test runner config object

--- client/testsuite.js
+++ client/testsuite.js
@@ -19,27 +19,130 @@ after( () => {
 
 function requireTestFiles( config, path = '' ) {
 	Object.keys( config ).map( ( folderName ) => {
-		describe( folderName, () => {
-			const folderConfig = config[ folderName ],
-				folderPath = `${path}${folderName}/`;
+		const folderConfig = config[ folderName ];
 
-			if ( Array.isArray( folderConfig ) ) {
-				folderConfig.forEach( fileName => require( `${folderPath}test/${fileName}.js` ) );
-			} else {
-				requireTestFiles( folderConfig, folderPath );
-			}
-		} );
+		if ( folderName === 'test' ) {
+			folderConfig.forEach( fileName => require( `${path}test/${fileName}.js` ) );
+		} else {
+			describe( folderName, () => {
+				requireTestFiles( folderConfig, `${path}${folderName}/` );
+			} );
+		}
 	} );
 }
 
 requireTestFiles( {
 	lib: {
-		domains: [ 'index' ],
-		'post-formats': [ 'index' ],
-		'post-normalizer': [ 'index' ],
-		'post-metadata': [ 'index' ],
-		posts: [ 'index' ],
-		store: [ 'index-test' ]
+		domains: {
+			dns: {
+				test: [ 'index-test', 'reducer-test' ]
+			},
+			'email-forwarding': {
+				test: [ 'reducer-test', 'store-test' ]
+			},
+			nameservers: {
+				test: [ 'store-test' ]
+			},
+			test: [ 'assembler-test' ],
+			whois: {
+				test: [ 'assembler-test', 'store-test' ]
+			}
+		},
+		'post-formats': {
+			test: [ 'actions', 'store' ]
+		},
+		'post-metadata': {
+			test: [ 'index' ]
+		},
+		'post-normalizer': {
+			test: [ 'index' ]
+		},
+		posts: {
+			test: [ 'actions', 'post-edit-store', 'post-list-store', 'utils' ]
+		},
+		store: {
+			test: [ 'index-test' ]
+		}
 	},
-	state: [ 'index' ]
+	state: {
+		application: {
+			test: [ 'actions', 'reducer' ]
+		},
+		comments: {
+			test: [ 'actions', 'reducer', 'selectors' ]
+		},
+		'current-user': {
+			test: [ 'actions', 'reducer', 'selectors' ]
+		},
+		notices: {
+			test: [ 'actions', 'reducer' ]
+		},
+		plugins: {
+			wporg: {
+				test: [ 'reducer', 'test-actions', 'test-selectors' ]
+			}
+		},
+		'post-types': {
+			test: [ 'actions', 'reducer', 'selectors' ]
+		},
+		posts: {
+			test: [ 'actions', 'reducer', 'selectors' ]
+		},
+		receipts: {
+			test: [ 'reducer' ]
+		},
+		sharing: {
+			publicize: {
+				test: [ 'actions', 'reducer', 'selectors' ]
+			}
+		},
+		'site-settings': {
+			exporter: {
+				test: [ 'actions', 'reducer', 'selectors' ]
+			}
+		},
+		sites: {
+			plans: {
+				test: [ 'actions', 'reducer' ]
+			},
+			test: [ 'actions', 'reducer' ]
+		},
+		support: {
+			test: [ 'actions', 'reducer' ]
+		},
+		themes: {
+			'current-theme': {
+				test: [ 'index', 'reducer' ]
+			},
+			'theme-details': {
+				test: [ 'reducer', 'selectors' ]
+			},
+			themes: {
+				test: [ 'index', 'reducer' ]
+			},
+			'themes-last-query': {
+				test: [ 'reducer' ]
+			},
+			'themes-list': {
+				test: [ 'index', 'reducer' ]
+			}
+		},
+		ui: {
+			editor: {
+				'contact-form': {
+					test: [ 'actions', 'reducer' ]
+				}
+			},
+			reader: {
+				fullpost: {
+					test: [ 'actions', 'reducer' ]
+				}
+			},
+			test: [ 'actions', 'reducer', 'selectors' ]
+		},
+		users: {
+			test: [ 'actions', 'reducer', 'selectors' ]
+		},
+		test: [ 'index' ]
+	}
 } );
