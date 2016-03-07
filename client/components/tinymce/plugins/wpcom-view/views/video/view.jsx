1972b8cab9bab0b642b2c75364b40cd1346a42ef
Andrew Duthie <andrew@andrewduthie.com>
2016-03-07 16:35:43 -0500

    Editor: Implement VideoView for rendering video shortcodes

---
+++ client/components/tinymce/plugins/wpcom-view/views/video/view.jsx
@@ -0,0 +1,44 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { merge } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import resizableView from '../../resizable-view';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { stringify, parse } from 'lib/shortcode';
+import Shortcode from 'components/shortcode';
+
+function VideoView( { siteId, content, width } ) {
+	if ( ! siteId || ! width ) {
+		return null;
+	}
+
+	const shortcode = stringify( merge( {
+		attrs: {
+			named: {
+				w: width
+			}
+		}
+	}, parse( content ) ) );
+
+	return (
+		<Shortcode sandbox={ null } { ...{ siteId, width } }>
+			{ shortcode }
+		</Shortcode>
+	);
+}
+
+VideoView.propTypes = {
+	siteId: PropTypes.number,
+	content: PropTypes.string,
+	width: PropTypes.number
+};
+
+export default connect( ( state ) => ( {
+	siteId: getSelectedSiteId( state )
+} ) )( resizableView( VideoView ) );
