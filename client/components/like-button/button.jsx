2ae7e7d94ac3612a2dc118c19d057041ed10330f
bluefuton <chris@bluefuton.com>
2016-03-07 15:38:00 +1300

    Like button: use lower case "1 like/3 likes" rather than "1 Like/3 Likes"

--- client/components/like-button/button.jsx
+++ client/components/like-button/button.jsx
@@ -69,7 +69,7 @@ var LikeButton = React.createClass( {
 
 		// Override the label with a counter
 		if ( likeCount > 0 || this.props.showCount ) {
-			likeLabel = this.translate( 'Like', 'Likes', {
+			likeLabel = this.translate( 'like', 'likes', {
 				count: likeCount,
 				comment: 'Displayed when a person "likes" a post.'
 			} );
