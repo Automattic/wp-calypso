de52710a74efabb491bb0486325388112f5d7d5c
jancavan <janmcavan@gmail.com>
2016-03-08 12:16:06 -0800

    Reader: add empty state to Manage Following

---
+++ client/reader/following-edit/empty.jsx
@@ -0,0 +1,46 @@
+var React = require( 'react' );
+
+var EmptyContent = require( 'components/empty-content' ),
+	stats = require( 'reader/stats' ),
+	discoverHelper = require( 'reader/discover/helper' );
+
+var FollowingEmptyContent = React.createClass( {
+	shouldComponentUpdate: function() {
+		return false;
+	},
+
+	recordAction: function() {
+		stats.recordAction( 'clicked_discover_on_empty' );
+		stats.recordGaEvent( 'Clicked Discover on EmptyContent' );
+	},
+
+	recordSecondaryAction: function() {
+		stats.recordAction( 'clicked_recommendations_on_empty' );
+		stats.recordGaEvent( 'Clicked Recommendations on EmptyContent' );
+	},
+
+	render: function() {
+		var action = discoverHelper.isEnabled()
+		? (
+			<a
+				className="empty-content__action button is-primary"
+				onClick={ this.recordAction }
+				href="/discover">{ this.translate( 'Explore Discover' ) }</a> ) : null,
+			secondaryAction = (
+				<a
+					className="empty-content__action button"
+					onClick={ this.recordSecondaryAction }
+					href="/recommendations">{ this.translate( 'Get recommendations on who to follow' ) }</a> );
+
+		return ( <EmptyContent
+			title={ this.translate( 'You haven\t added sites to follow' ) }
+			line={ this.translate( 'Search for a site above or get recommendations.' ) }
+			action={ action }
+			secondaryAction={ secondaryAction }
+			illustration={ '/calypso/images/drake/drake-all-done.svg' }
+			illustrationWidth={ 500 }
+			/> );
+	}
+} );
+
+module.exports = FollowingEmptyContent;
