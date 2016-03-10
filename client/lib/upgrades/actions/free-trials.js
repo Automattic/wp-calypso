acea191de74d32896c1b81762bd654e48d1a237e
Tugdual de Kerviler <dekervit@gmail.com>
2016-03-10 16:14:23 +0100

    Plans: Add documentation for new methods in `lib/upgrades/actions/free-trial`

--- client/lib/upgrades/actions/free-trials.js
+++ client/lib/upgrades/actions/free-trials.js
@@ -8,6 +8,16 @@ import { submitTransaction } from './checkout';
 
 const productsList = productsListFactory();
 
+/*
+ * Submit a free cart transaction
+ *
+ * This an helper method to be used for when we want to make a
+ * quick (free) transaction without a valid payment method.
+ * The default payment method for free carts is used here.
+ *
+ * {cart} [partialCart] - A cart-like object. We just need a valid `product_slug` property for cart items
+ * {function} [onComplete] Callback function called on completion
+ */
 function submitFreeTransaction( partialCart, onComplete ) {
 	const cart = fillInAllCartItemAttributes( partialCart, productsList.get() ),
 		transaction = {
@@ -17,6 +27,16 @@ function submitFreeTransaction( partialCart, onComplete ) {
 	submitTransaction( { cart, transaction }, onComplete );
 }
 
+/*
+ * Start a free trial for a given site and plan
+ *
+ * This method creates a free cart containing our plan as a cart item with the
+ * `isFreeTrial` option set to `true`. It uses `submitFreeTransaction` to make the request.
+ *
+ * {int} [siteId] - The ID of the site this free trial is for.
+ * {Object} [plan] - A plan-like object. We just need a valid `product_slug` property to make a cartItem out of it.
+ * {function} [onComplete] Callback function called on completion
+ */
 export function startFreeTrial( siteId, plan, onComplete ) {
 	let cart = emptyCart( siteId, { temporary: true } );
 	const planItem = cartItems.getItemForPlan( plan, { isFreeTrial: true } );
