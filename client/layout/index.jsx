5713b4d3495bc519da0651809dd78509efb1ef80
Matías Ventura <mv@matiasventura.com>
2016-03-10 17:20:34 +0100

    Declare "secondary" attribute in sections.js to render sidebar.
    Split devdocs paths for the one case that doesn't have a sidebar.

--- client/layout/index.jsx
+++ client/layout/index.jsx
@@ -143,7 +143,7 @@ Layout = React.createClass( {
 				`is-section-${this.props.section.name}`,
 				`focus-${this.props.focus.getCurrent()}`,
 				{ 'is-support-user': this.props.isSupportUser },
-				{ 'has-no-sidebar': ! this.props.hasSidebar },
+				{ 'has-no-sidebar': ! this.props.section.secondary },
 				{ 'full-screen': this.props.isFullScreen }
 			),
 			loadingClass = classnames( {
@@ -176,12 +176,11 @@ Layout = React.createClass( {
 
 export default connect(
 	( state ) => {
-		const { isLoading, section, hasSidebar, isFullScreen, chunkName } = state.ui;
+		const { isLoading, section, isFullScreen } = state.ui;
 		return {
 			isLoading,
 			isSupportUser: state.support.isSupportUser,
 			section,
-			hasSidebar,
 			isFullScreen,
 			isOffline: isOffline( state )
 		};
--- client/sections.js
+++ client/sections.js
@@ -14,72 +14,84 @@ sections = [
 		name: 'post-editor',
 		paths: [ '/post', '/page' ],
 		module: 'post-editor',
-		group: 'editor'
+		group: 'editor',
+		secondary: true
 	},
 	{
 		name: 'me',
 		paths: [ '/me', '/purchases' ],
 		module: 'me',
-		group: 'me'
+		group: 'me',
+		secondary: true
 	},
 	{
 		name: 'media',
 		paths: [ '/media' ],
 		module: 'my-sites/media',
-		group: 'sites'
+		group: 'sites',
+		secondary: true
 	},
 	{
 		name: 'menus',
 		paths: [ '/menus' ],
 		module: 'my-sites/menus',
+		secondary: true,
 		group: 'sites'
 	},
 	{
 		name: 'people',
 		paths: [ '/people' ],
 		module: 'my-sites/people',
+		secondary: true,
 		group: 'sites'
 	},
 	{
 		name: 'plugins',
 		paths: [ '/plugins' ],
 		module: 'my-sites/plugins',
+		secondary: true,
 		group: 'sites'
 	},
 	{
 		name: 'posts-pages',
 		paths: [ '/pages' ],
 		module: 'my-sites/pages',
+		secondary: true,
 		group: 'sites'
 	},
 	{
 		name: 'posts-pages',
 		paths: [ '/posts' ],
 		module: 'my-sites/posts',
+		secondary: true,
 		group: 'sites'
 	},
 	{
 		name: 'settings',
 		paths: [ '/settings' ],
 		module: 'my-sites/site-settings',
+		secondary: true,
 		group: 'sites'
 	},
 	{
 		name: 'sharing',
 		paths: [ '/sharing' ],
 		module: 'my-sites/sharing',
+		secondary: true,
 		group: 'sites'
 	},
 	{
 		name: 'signup',
 		paths: [ '/start', '/phone', '/log-in', '/jetpack' ],
 		module: 'signup',
+		secondary: false,
 		enableLoggedOut: true
 	},
 	{
 		name: 'stats',
 		paths: [ '/stats' ],
 		module: 'my-sites/stats',
+		secondary: true,
 		group: 'sites'
 	},
 	{
@@ -94,12 +106,14 @@ sections = [
 		name: 'upgrades',
 		paths: [ '/domains', '/checkout' ],
 		module: 'my-sites/upgrades',
+		secondary: true,
 		group: 'sites'
 	},
 	{
 		name: 'upgrades',
 		paths: [ '/plans' ],
 		module: 'my-sites/plans',
+		secondary: true,
 		group: 'sites'
 	}
 ];
@@ -109,6 +123,7 @@ if ( config.isEnabled( 'manage/ads' ) ) {
 		name: 'ads',
 		paths: [ '/ads' ],
 		module: 'my-sites/ads',
+		secondary: true,
 		group: 'sites'
 	} );
 }
@@ -118,6 +133,7 @@ if ( config.isEnabled( 'manage/drafts' ) ) {
 		name: 'posts-pages',
 		paths: [ '/drafts' ],
 		module: 'my-sites/drafts',
+		secondary: true,
 		group: 'sites'
 	} );
 }
@@ -142,6 +158,7 @@ if ( config.isEnabled( 'reader' ) ) {
 		name: 'reader',
 		paths: readerPaths,
 		module: 'reader',
+		secondary: true,
 		group: 'reader'
 	} );
 }
@@ -151,6 +168,15 @@ if ( config.isEnabled( 'devdocs' ) ) {
 		name: 'devdocs',
 		paths: [ '/devdocs' ],
 		module: 'devdocs',
+		secondary: true,
+		enableLoggedOut: true
+	} );
+
+	sections.push( {
+		name: 'devdocs',
+		paths: [ '/devdocs/start' ],
+		module: 'devdocs',
+		secondary: false,
 		enableLoggedOut: true
 	} );
 }
@@ -159,7 +185,8 @@ if ( config.isEnabled( 'vip' ) ) {
 	sections.push( {
 		name: 'vip',
 		paths: [ '/vip', '/vip/deploys', '/vip/billing', '/vip/support', '/vip/backups', '/vip/logs' ],
-		module: 'vip'
+		module: 'vip',
+		secondary: true
 	} );
 }
 
@@ -168,6 +195,7 @@ if ( config.isEnabled( 'help' ) ) {
 		name: 'help',
 		paths: [ '/help' ],
 		module: 'me/help',
+		secondary: true,
 		group: 'me'
 	} );
 }
@@ -187,6 +215,7 @@ if ( config.isEnabled( 'oauth' ) ) {
 		name: 'auth',
 		paths: [ '/login' ],
 		module: 'auth',
+		secondary: false,
 		enableLoggedOut: true
 	} );
 }
@@ -204,7 +233,8 @@ if ( config.isEnabled( 'manage/custom-post-types' ) ) {
 	sections.push( {
 		name: 'posts-custom',
 		paths: [ '/types' ],
-		module: 'my-sites/types'
+		module: 'my-sites/types',
+		secondary: true
 	} );
 }
 
