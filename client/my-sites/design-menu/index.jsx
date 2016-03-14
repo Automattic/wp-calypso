d75982e7a5ab6fde3f5bac2ffc6c1484642b1777
Matías Ventura <mv@matiasventura.com>
2016-03-14 13:15:16 +0100

    Render DesignMenu in navigation.

---
+++ client/my-sites/design-menu/index.jsx
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+
+const DesignMenu = React.createClass( {
+
+	render() {
+		return (
+			<div className="design-menu">
+				<Card>
+					Design Tools!
+				</Card>
+			</div>
+		);
+	}
+} );
+
+export default DesignMenu;
