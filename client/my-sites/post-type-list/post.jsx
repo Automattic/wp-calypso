f5850987636f8fa8c85c24bb88f7b1efe5410359
Andrew Duthie <andrew@andrewduthie.com>
2016-03-15 13:11:53 -0400

    Components: Render basic layout of post list item

--- client/my-sites/post-type-list/post.jsx
+++ client/my-sites/post-type-list/post.jsx
@@ -7,13 +7,42 @@ import { connect } from 'react-redux';
 /**
  * Internal dependencies
  */
+import { getEditorPath } from 'state/ui/editor/selectors';
 import { getPost } from 'state/posts/selectors';
 import Card from 'components/card';
+import PostRelativeTimeStatus from 'my-sites/post-relative-time-status';
+import Button from 'components/button';
+import Gridicon from 'components/gridicon';
+import resizeImageUrl from 'lib/resize-image-url';
 
-function PostTypePost( { post } ) {
+function PostTypePost( { post, editUrl } ) {
 	return (
 		<Card compact className="post-type-list__post">
-			{ post.title }
+			<div className="post-type-list__post-detail">
+				{ post.featured_image && (
+					<div className="post-type-list__post-thumbnail-wrapper">
+						<img
+							alt="Post thumbnail"
+							src={ resizeImageUrl( post.featured_image, { w: 80 } ) }
+							className="post-type-list__post-thumbnail" />
+					</div>
+				) }
+				<div className="post-type-list__post-title-meta">
+					<h1 className="post-type-list__post-title">
+						<a href={ editUrl }>
+							{ post.title }
+						</a>
+					</h1>
+					<div className="post-type-list__post-meta">
+						<PostRelativeTimeStatus post={ post } />
+					</div>
+				</div>
+			</div>
+			<div className="post-type-list__post-actions">
+				<Button href={ post.URL } target="_blank" borderless>
+					<Gridicon icon="external" />
+				</Button>
+			</div>
 		</Card>
 	);
 }
@@ -24,7 +53,10 @@ PostTypePost.propTypes = {
 };
 
 export default connect( ( state, ownProps ) => {
+	const post = getPost( state, ownProps.globalId );
+
 	return {
-		post: getPost( state, ownProps.globalId )
+		post,
+		editUrl: getEditorPath( state, state.ui.selectedSiteId, post.ID )
 	};
 } )( PostTypePost );
