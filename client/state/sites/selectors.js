0d86da5c5e43b10f3ceb6c083457c0a63b652958
Andrew Duthie <andrew@andrewduthie.com>
2016-03-15 13:11:21 -0400

    State: Add selector to retrieve site slug

--- client/state/sites/selectors.js
+++ client/state/sites/selectors.js
@@ -10,3 +10,23 @@
 export function getSite( state, siteId ) {
 	return state.sites.items[ siteId ] || null;
 }
+
+/**
+ * Returns the slug for a site, or null if the site is unknown.
+ *
+ * @param  {Object}  state  Global state tree
+ * @param  {Number}  siteId Site ID
+ * @return {?String}        Site slug
+ */
+export function getSiteSlug( state, siteId ) {
+	const site = getSite( state, siteId );
+	if ( ! site ) {
+		return null;
+	}
+
+	if ( site.options && site.options.is_redirect ) {
+		return site.options.unmapped_url.replace( /^https?:\/\//, '' );
+	}
+
+	return site.URL.replace( /^https?:\/\//, '' ).replace( /\//g, '::' );
+}
--- client/state/sites/test/selectors.js
+++ client/state/sites/test/selectors.js
@@ -6,7 +6,7 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import { getSite } from '../selectors';
+import { getSite, getSiteSlug } from '../selectors';
 
 describe( 'selectors', () => {
 	describe( '#getSite()', () => {
@@ -30,6 +30,52 @@ describe( 'selectors', () => {
 			}, 2916284 );
 
 			expect( site ).to.eql( { ID: 2916284, name: 'WordPress.com Example Blog' } );
+		} );
+	} );
+
+	describe( '#getSiteSlug()', () => {
+		it( 'should return null if the site is not known', () => {
+			const slug = getSiteSlug( {
+				sites: {
+					items: {}
+				}
+			}, 2916284 );
+
+			expect( slug ).to.be.null;
+		} );
+
+		it( 'should return the unmapped hostname for a redirect site', () => {
+			const slug = getSiteSlug( {
+				sites: {
+					items: {
+						77203074: {
+							ID: 77203074,
+							URL: 'https://testonesite2014.wordpress.com',
+							options: {
+								is_redirect: true,
+								unmapped_url: 'https://example.wordpress.com'
+							}
+						}
+					}
+				}
+			}, 77203074 );
+
+			expect( site ).to.equal( 'example.wordpress.com' );
+		} );
+
+		it( 'should return the URL with scheme removed and paths separated', () => {
+			const slug = getSiteSlug( {
+				sites: {
+					items: {
+						77203199: {
+							ID: 77203199,
+							URL: 'https://testtwosites2014.wordpress.com/path/to/site'
+						}
+					}
+				}
+			}, 77203199 );
+
+			expect( slug ).to.equal( 'testtwosites2014.wordpress.com::path::to::site' );
 		} );
 	} );
 } );
