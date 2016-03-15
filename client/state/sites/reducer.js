bcc533194473c3f5e9c3b4bd5a661e3f90918b50
Andrew Duthie <andrew@andrewduthie.com>
2016-03-15 15:54:12 -0400

    State: Strip computed attributes from received site object

--- client/state/sites/reducer.js
+++ client/state/sites/reducer.js
@@ -2,13 +2,23 @@
  * External dependencies
  */
 import { combineReducers } from 'redux';
+import pick from 'lodash/pick';
 
 /**
  * Internal dependencies
  */
 import { plans } from './plans/reducer';
 import mediaStorage from './media-storage/reducer';
-import { SITE_RECEIVE, SERIALIZE, DESERIALIZE } from 'state/action-types';
+import { SITE_RECEIVE, DESERIALIZE } from 'state/action-types';
+import { sitesSchema } from './schema';
+import { isValidStateWithSchema } from 'state/utils';
+
+/**
+ * Constants
+ */
+// [TODO]: This validation is only necessary so long as we continue to receive
+// decorated sites from the `lib/sites-list` module.
+const VALID_SITE_KEYS = Object.keys( sitesSchema.patternProperties[ '^\\d+$' ].properties );
 
 /**
  * Tracks all known site objects, indexed by site ID.
@@ -20,14 +30,19 @@ import { SITE_RECEIVE, SERIALIZE, DESERIALIZE } from 'state/action-types';
 export function items( state = {}, action ) {
 	switch ( action.type ) {
 		case SITE_RECEIVE:
+			const site = pick( action.site, VALID_SITE_KEYS );
 			return Object.assign( {}, state, {
-				[ action.site.ID ]: action.site
+				[ site.ID ]: site
 			} );
-		case SERIALIZE:
-			return {};
+
 		case DESERIALIZE:
+			if ( isValidStateWithSchema( state, sitesSchema ) ) {
+				return state;
+			}
+
 			return {};
 	}
+
 	return state;
 }
 
