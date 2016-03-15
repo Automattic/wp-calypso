6b0530171ca5ea2b6ef58090cfadd19d18a2478c
Steve Seear <steve.seear@gmail.com>
2016-03-15 13:36:36 +0000

    Themes: Add missing translate call

--- client/my-sites/themes/sheet.jsx
+++ client/my-sites/themes/sheet.jsx
@@ -76,7 +76,7 @@ export const ThemeSheet = React.createClass( {
 	renderBar() {
 		const placeholder = <span className="themes__sheet-placeholder">loading.....</span>;
 		const title = this.props.name || placeholder;
-		const tag = this.props.author ? 'by ' + this.props.author : placeholder;
+		const tag = this.props.author ? i18n.translate( 'by ' ) + this.props.author : placeholder;
 
 		return (
 			<div className="themes__sheet-bar">
