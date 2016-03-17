65bb2fa57fd15af977bd1e036e7e841889ffc252
Nathan Rajlich <nathan@tootallnate.net>
2016-03-17 10:43:20 -0700

    Components: inline the FilePicker doc examples
    No need for an extracted `renderPickers()` function that
    only gets called once, that's an anti-pattern.

--- client/components/file-picker/docs/example.jsx
+++ client/components/file-picker/docs/example.jsx
@@ -13,8 +13,6 @@ import FilePicker from 'components/file-picker';
 export default class FilePickers extends React.Component {
 	constructor( props ) {
 		super( props );
-		this.onSingle = this.onSingle.bind( this );
-		this.onMulti = this.onMulti.bind( this );
 	}
 
 	onSingle( files ) {
@@ -33,41 +31,36 @@ export default class FilePickers extends React.Component {
 				<h2>
 					<a href="/devdocs/design/file-pickers">File Picker</a>
 				</h2>
-				{ this.renderPickers() }
+
+				<Card>
+
+					<h4>Select a single file:</h4>
+					<FilePicker onPick={ this.onSingle } >
+						<Button>Single Item</Button>
+					</FilePicker>
+
+					<h4>Select a multiple files:</h4>
+					<FilePicker multiple onPick={ this.onMulti } >
+						<Button>Multiple Items</Button>
+					</FilePicker>
+
+					<h4>Select a directory:</h4>
+					<FilePicker directory onPick={ this.onMulti } >
+						<Button>Directory</Button>
+					</FilePicker>
+
+					<h4>Select an image file:</h4>
+					<FilePicker accept="image/*" onPick={ this.onSingle } >
+						<Button>JPEG / PNG / GIF</Button>
+					</FilePicker>
+
+					<h4>Any internal content works:</h4>
+					<FilePicker onPick={ this.onSingle } >
+						<a href="#">Select File…</a>
+					</FilePicker>
+
+				</Card>
 			</div>
-		);
-	}
-
-	renderPickers() {
-		return (
-			<Card>
-
-				<h4>Select a single file:</h4>
-				<FilePicker onPick={ this.onSingle } >
-					<Button>Single Item</Button>
-				</FilePicker>
-
-				<h4>Select a multiple files:</h4>
-				<FilePicker multiple onPick={ this.onMulti } >
-					<Button>Multiple Items</Button>
-				</FilePicker>
-
-				<h4>Select a directory:</h4>
-				<FilePicker directory onPick={ this.onMulti } >
-					<Button>Directory</Button>
-				</FilePicker>
-
-				<h4>Select an image file:</h4>
-				<FilePicker accept="image/*" onPick={ this.onSingle } >
-					<Button>JPEG / PNG / GIF</Button>
-				</FilePicker>
-
-				<h4>Any internal content works:</h4>
-				<FilePicker onPick={ this.onSingle } >
-					<a href="#">Select File…</a>
-				</FilePicker>
-
-			</Card>
 		);
 	}
 }
