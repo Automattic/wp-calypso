73abc6ef94889f8fb91d45564e9777704c2de376
Ben Lowery <ben.lowery@automattic.com>
2016-03-16 21:50:04 -0400

    Add in tracks for discover card attribution clicks

--- client/reader/discover/post-attribution.jsx
+++ client/reader/discover/post-attribution.jsx
@@ -7,7 +7,8 @@ var React = require( 'react' ),
 /**
  * Internal dependencies
  */
-var DiscoverHelper = require( './helper' );
+var DiscoverHelper = require( './helper' ),
+	stats = require( 'reader/stats' );
 
 var arrowGridicon = ( <svg className="gridicon gridicon-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 11H6.414l6.293-6.293-1.414-1.414L2.586 12l8.707 8.707 1.414-1.414L6.414 13H20"/></svg> );
 
@@ -24,7 +25,19 @@ var DiscoverPostAttribution = React.createClass( {
 		siteUrl: React.PropTypes.string.isRequired
 	},
 
-	render: function() {
+	recordAuthorClick() {
+		stats.recordTrack( 'calypso_reader_author_on_discover_card_clicked', {
+			author_url: this.props.attribution.author_url
+		} );
+	},
+
+	recordSiteClick() {
+		stats.recordTrack( 'calypso_reader_site_on_discover_card_clicked', {
+			site_url: this.props.siteUrl
+		} );
+	},
+
+	render() {
 		const attribution = this.props.attribution;
 		const classes = classNames( 'discover-attribution is-post', {
 			'is-missing-avatar': ! attribution.avatar_url
@@ -37,9 +50,9 @@ var DiscoverPostAttribution = React.createClass( {
 				{ attribution.avatar_url ? <img className="gravatar" src={ encodeURI( attribution.avatar_url ) } alt="Avatar" width="20" height="20" /> : arrowGridicon }
 				<span className="discover-attribution__text">
 					{ this.translate( 'Originally posted by' ) }
-					<a className="discover-attribution__author" rel="external" target="_blank" href={ encodeURI( attribution.author_url ) }>{ attribution.author_name }</a>
+					<a className="discover-attribution__author" rel="external" target="_blank" onClick={ this.recordAuthorClick } href={ encodeURI( attribution.author_url ) }>{ attribution.author_name }</a>
 					{ this.translate( 'on' ) }
-					<a {...siteLinkProps} className={ siteClasses } href={ encodeURI( this.props.siteUrl ) }>{ attribution.blog_name }</a>
+					<a {...siteLinkProps} className={ siteClasses } onClick={ this.recordSiteClick } href={ encodeURI( this.props.siteUrl ) }>{ attribution.blog_name }</a>
 				</span>
 			</div>
 		);
