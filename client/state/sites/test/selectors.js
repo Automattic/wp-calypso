437efe0851c1bd423eb837dc8d0d9c1d45e1c82f
Andrew Duthie <andrew@andrewduthie.com>
2016-03-18 12:57:12 -0400

    State: Use site unmapped URL for slug if conflicting

--- client/state/sites/test/selectors.js
+++ client/state/sites/test/selectors.js
@@ -6,7 +6,7 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import { getSite, getSiteSlug } from '../selectors';
+import { getSite, getSiteCollisions, isSiteConflicting, getSiteSlug } from '../selectors';
 
 describe( 'selectors', () => {
 	describe( '#getSite()', () => {
@@ -33,7 +33,88 @@ describe( 'selectors', () => {
 		} );
 	} );
 
+	describe( '#getSiteCollisions', () => {
+		beforeEach( () => {
+			getSiteCollisions.memoizedSelector.cache.clear();
+		} );
+
+		it( 'should not consider distinct URLs as conflicting', () => {
+			const collisions = getSiteCollisions( {
+				sites: {
+					items: {
+						77203199: { ID: 77203199, URL: 'https://example.com', jetpack: false },
+						77203074: { ID: 77203074, URL: 'https://example.net', jetpack: true }
+					}
+				}
+			} );
+
+			expect( collisions ).to.eql( [] );
+		} );
+
+		it( 'should return an array of conflicting site IDs', () => {
+			const collisions = getSiteCollisions( {
+				sites: {
+					items: {
+						77203199: { ID: 77203199, URL: 'https://example.com', jetpack: false },
+						77203074: { ID: 77203074, URL: 'https://example.com', jetpack: true }
+					}
+				}
+			} );
+
+			expect( collisions ).to.eql( [ 77203199 ] );
+		} );
+
+		it( 'should ignore URL protocol in considering conflict', () => {
+			const collisions = getSiteCollisions( {
+				sites: {
+					items: {
+						77203199: { ID: 77203199, URL: 'https://example.com', jetpack: false },
+						77203074: { ID: 77203074, URL: 'http://example.com', jetpack: true }
+					}
+				}
+			} );
+
+			expect( collisions ).to.eql( [ 77203199 ] );
+		} );
+	} );
+
+	describe( '#isSiteConflicting()', () => {
+		beforeEach( () => {
+			getSiteCollisions.memoizedSelector.cache.clear();
+		} );
+
+		it( 'it should return false if the specified site ID is not included in conflicting set', () => {
+			const isConflicting = isSiteConflicting( {
+				sites: {
+					items: {
+						77203199: { ID: 77203199, URL: 'https://example.com', jetpack: false },
+						77203074: { ID: 77203074, URL: 'https://example.net', jetpack: true }
+					}
+				}
+			}, 77203199 );
+
+			expect( isConflicting ).to.be.false;
+		} );
+
+		it( 'should return true if the specified site ID is included in the conflicting set', () => {
+			const isConflicting = isSiteConflicting( {
+				sites: {
+					items: {
+						77203199: { ID: 77203199, URL: 'https://example.com', jetpack: false },
+						77203074: { ID: 77203074, URL: 'https://example.com', jetpack: true }
+					}
+				}
+			}, 77203199 );
+
+			expect( isConflicting ).to.be.true;
+		} );
+	} );
+
 	describe( '#getSiteSlug()', () => {
+		beforeEach( () => {
+			getSiteCollisions.memoizedSelector.cache.clear();
+		} );
+
 		it( 'should return null if the site is not known', () => {
 			const slug = getSiteSlug( {
 				sites: {
@@ -60,7 +141,28 @@ describe( 'selectors', () => {
 				}
 			}, 77203074 );
 
-			expect( site ).to.equal( 'example.wordpress.com' );
+			expect( slug ).to.equal( 'example.wordpress.com' );
+		} );
+
+		it( 'should return the unmapped hostname for a conflicting site', () => {
+			const slug = getSiteSlug( {
+				sites: {
+					items: {
+						77203199: {
+							ID: 77203199,
+							URL: 'https://example.com',
+							jetpack: false,
+							options: {
+								is_redirect: false,
+								unmapped_url: 'https://testtwosites2014.wordpress.com'
+							}
+						},
+						77203074: { ID: 77203074, URL: 'https://example.com', jetpack: true }
+					}
+				}
+			}, 77203199 );
+
+			expect( slug ).to.equal( 'testtwosites2014.wordpress.com' );
 		} );
 
 		it( 'should return the URL with scheme removed and paths separated', () => {
