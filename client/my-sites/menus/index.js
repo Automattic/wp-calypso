1e588a6ba46e022912066b8c300ddd88f14dd093
Bernhard Reiter <ockham@raz.or.at>
2016-03-19 02:12:19 +0100

    Menus: ES6ify index.js

--- client/my-sites/menus/index.js
+++ client/my-sites/menus/index.js
@@ -1,13 +1,13 @@
 /**
  * Internal dependencies
  */
-var config = require( 'config' ),
-	controller = require( 'my-sites/controller' ),
-	menusController = require( './controller' );
+import config from 'config';
+import { siteSelection, navigation, sites } from 'my-sites/controller';
+import { menus } from './controller';
 
 export default function( router ) {
 	if ( config.isEnabled( 'manage/menus' ) ) {
-		router( '/menus/:site_id', controller.siteSelection, controller.navigation, menusController.menus );
-		router( '/menus', controller.siteSelection, controller.sites );
+		router( '/menus/:site_id', siteSelection, navigation, menus );
+		router( '/menus', siteSelection, sites );
 	}
 };
