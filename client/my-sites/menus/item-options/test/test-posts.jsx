bb4bfe3a31383ef10de1c31b665aef9d93e30a1c
Steve Seear <steve.seear@gmail.com>
2016-03-21 16:17:53 +0000

    Menus: item-options test to single runner

--- client/my-sites/menus/item-options/test/test-posts.jsx
+++ client/my-sites/menus/item-options/test/test-posts.jsx
@@ -1,60 +1,57 @@
-
-require( 'lib/react-test-env-setup' )();
-
 /**
  * External Dependencies
  */
-var	sinon = require( 'sinon' ),
-	sinonChai = require( 'sinon-chai' ),
-	chai = require( 'chai' );
+import chai from 'chai';
 
 /**
  * Internal Dependencies
  */
-var Posts = require( '../posts' ),
-	siteMenus = require( 'lib/menu-data' ),
-	helpers = require( 'my-sites/pages/helpers' );
+import useFakeDom from 'test/helpers/use-fake-dom';
+import helpers from 'my-sites/pages/helpers';
+import { useSandbox } from 'test/helpers/use-sinon';
 
-chai.use( sinonChai );
+describe( 'Posts', function() {
+	let Posts, siteMenus;
+	useFakeDom();
+	useSandbox();
 
-describe( 'Posts' , function () {
-	describe( 'maybeInjectPosts', function () {
+	before( function() {
+		Posts = require( '../posts' );
+		siteMenus = require( 'lib/menu-data' );
+	} );
 
-		beforeEach( function () {
+	describe( 'maybeInjectPosts', function() {
+		beforeEach( function() {
 			this.itemToInject = {};
 			siteMenus.generateNewPageMenuItem = () => ( {} );
-			this.siteMenusStub = sinon.stub( siteMenus, 'generateHomePageMenuItem', function () {
+			this.siteMenusStub = this.sandbox.stub( siteMenus, 'generateHomePageMenuItem', function() {
 				return this.itemToInject;
 			}.bind( this ) );
 		} );
 
-		afterEach( function () {
-			this.siteMenusStub.restore();
+		afterEach( function() {
+			this.sandbox.restore();
 		} );
 
-		it( 'should inject when the menu type is "page"', function () {
-			var postsNode = new Posts( {posts: [], type: "page" } ),
+		it( 'should inject when the menu type is "page"', function() {
+			var postsNode = new Posts( {posts: [], type: 'page' } ),
 				injectedPosts = postsNode.maybeInjectPosts();
 			chai.expect( injectedPosts[0] ).to.equal( this.itemToInject );
 		} );
 
-		it( 'should not inject when the menu type is not "page"', function () {
-			var postsNode = new Posts( {posts: [], type: "post" } ),
+		it( 'should not inject when the menu type is not "page"', function() {
+			var postsNode = new Posts( {posts: [], type: 'post' } ),
 				injectedPosts = postsNode.maybeInjectPosts();
 			chai.expect( injectedPosts.length ).to.equal( 0 );
 		} );
 
-		it( 'should inject with pass title to generator when there is a front page set', function () {
-			var isFrontPageStub = sinon.stub( helpers, 'isFrontPage', function () { return true; } ),
+		it( 'should inject with pass title to generator when there is a front page set', function() {
+			var isFrontPageStub = this.sandbox.stub( helpers, 'isFrontPage', () => true ),
 				post = {title: 'Test'},
-				postsNode = new Posts( { posts: [post], type: "page" } );
+				postsNode = new Posts( { posts: [post], type: 'page' } );
 			postsNode.maybeInjectPosts();
 			chai.expect( isFrontPageStub ).to.have.been.calledWith( post );
 			chai.expect( this.siteMenusStub ).to.have.been.calledWith( post.title );
-
-			isFrontPageStub.restore();
 		} );
-
-
 	} );
 } );
