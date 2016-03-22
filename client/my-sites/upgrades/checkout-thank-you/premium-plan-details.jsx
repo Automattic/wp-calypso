f8018f7a23271d1229b3a1f7e0f2cfcca3be9010
Ben Dwyer <ben@scruffian.com>
2016-03-22 11:34:01 +0000

    Post Checkout: Refactor the custom domain purchase detail to a new component so it can be shared

--- client/my-sites/upgrades/checkout-thank-you/premium-plan-details.jsx
+++ client/my-sites/upgrades/checkout-thank-you/premium-plan-details.jsx
@@ -8,6 +8,7 @@ import React from 'react';
  * Internal dependencies
  */
 import config from 'config';
+import CustomDomainPurchaseDetail from './custom-domain-purchase-detail';
 import i18n from 'lib/mixins/i18n';
 import { isPremium } from 'lib/products-values';
 import paths from 'lib/paths';
@@ -20,23 +21,7 @@ const PremiumPlanDetails = ( { selectedSite, sitePlans } ) => {
 
 	return (
 		<div>
-			{ plan.hasDomainCredit && (
-				<PurchaseDetail
-					icon="globe"
-					title={ i18n.translate( 'Get your custom domain' ) }
-					description={
-						i18n.translate(
-							"Replace your site's address, {{em}}%(siteDomain)s{{/em}}, with a custom domain. " +
-							'A free domain is included with your plan.',
-							{
-								args: { siteDomain: selectedSite.domain },
-								components: { em: <em /> }
-							}
-						)
-					}
-					buttonText={ i18n.translate( 'Claim your free domain' ) }
-					href={ '/domains/add/' + selectedSite.slug } />
-			) }
+			{ plan.hasDomainCredit && <CustomDomainPurchaseDetail selectedSite={ selectedSite } /> }
 
 			<PurchaseDetail
 				icon="customize"
