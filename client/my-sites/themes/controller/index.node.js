af794ee2c0abfde31d9dff35130f7ea341195b58
Alexander Kirk <alexander@kirk.at>
2016-03-23 19:53:05 +0100

    Improve variable

--- client/my-sites/themes/controller/index.node.js
+++ client/my-sites/themes/controller/index.node.js
@@ -69,8 +69,8 @@ export function details( context, next ) {
 	const { slug } = context.params;
 	const user = getCurrentUser( context.store.getState() );
 	const themeName = ( getThemeDetails( context.store.getState(), slug ) || false ).name;
-	const title = i18n.translate( '%(theme)s Theme', {
-		args: { theme: themeName },
+	const title = i18n.translate( '%(themeName)s Theme', {
+		args: { themeName: themeName },
 		textOnly: true
 	} );
 	const Head = user
