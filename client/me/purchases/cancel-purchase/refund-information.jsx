346b9aad9d4197f9bbead6b001029f4d4305b7eb
Stéphane Thomas <email@stephanethomas.com>
2016-03-28 13:04:29 +0200

    Purchases: Convert Cancel Purchase Refund Information component to stateless

--- client/me/purchases/cancel-purchase/refund-information.jsx
+++ client/me/purchases/cancel-purchase/refund-information.jsx
@@ -6,54 +6,52 @@ import React from 'react';
 /**
  * Internal Dependencies
  */
+import i18n from 'lib/mixins/i18n';
 import { getSubscriptionEndDate, isRefundable } from 'lib/purchases';
 
-const CancelPurchaseRefundInformation = React.createClass( {
-	propTypes: {
-		purchase: React.PropTypes.object.isRequired
-	},
+const CancelPurchaseRefundInformation = ( { purchase } ) => {
+	const { priceText, refundPeriodInDays } = purchase,
+		refundsSupportLink = <a href="https://support.wordpress.com/refunds/" target="_blank" />;
 
-	render() {
-		const purchase = this.props.purchase,
-			{ priceText, refundPeriodInDays } = purchase,
-			refundsSupportLink = <a href="https://support.wordpress.com/refunds/" target="_blank" />;
-
-		if ( isRefundable( purchase ) ) {
-			return (
-				<p>{ this.translate(
-					'Yes! You are canceling this purchase within the %(refundPeriodInDays)d day refund period. ' +
-					'Once you confirm, your card will be refunded %(priceText)s. {{a}}Learn more{{/a}}.',
-					{
-						args: {
-							refundPeriodInDays,
-							priceText
-						},
-						components: {
-							a: refundsSupportLink
-						},
-						context: 'priceText is of the form "[currency-symbol][amount] [currency-code]" i.e. "$20 USD"'
-					}
-				) }</p>
-			);
-		}
-
+	if ( isRefundable( purchase ) ) {
 		return (
-			<p>{ this.translate(
-				'You are canceling after the %(refundPeriodInDays)d day refund period, so you will not receive a refund. ' +
-				'Canceling will prevent automatic renewal, and we will leave this purchase active until %(subscriptionEndDate)s. ' +
-				'{{a}}Learn more.{{/a}}',
+			<p>{ i18n.translate(
+				'Yes! You are canceling this purchase within the %(refundPeriodInDays)d day refund period. ' +
+				'Once you confirm, your card will be refunded %(priceText)s. {{a}}Learn more{{/a}}.',
 				{
 					args: {
-						subscriptionEndDate: getSubscriptionEndDate( purchase ),
-						refundPeriodInDays
+						refundPeriodInDays,
+						priceText
 					},
 					components: {
 						a: refundsSupportLink
-					}
+					},
+					context: 'priceText is of the form "[currency-symbol][amount] [currency-code]" i.e. "$20 USD"'
 				}
 			) }</p>
 		);
 	}
-} );
+
+	return (
+		<p>{ i18n.translate(
+			'You are canceling after the %(refundPeriodInDays)d day refund period, so you will not receive a refund. ' +
+			'Canceling will prevent automatic renewal, and we will leave this purchase active until %(subscriptionEndDate)s. ' +
+			'{{a}}Learn more.{{/a}}',
+			{
+				args: {
+					subscriptionEndDate: getSubscriptionEndDate( purchase ),
+					refundPeriodInDays
+				},
+				components: {
+					a: refundsSupportLink
+				}
+			}
+		) }</p>
+	);
+};
+
+CancelPurchaseRefundInformation.propTypes = {
+	purchase: React.PropTypes.object.isRequired
+};
 
 export default CancelPurchaseRefundInformation;
