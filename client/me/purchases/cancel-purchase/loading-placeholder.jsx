0fbf72adbb6599b59ffe762933c1c0cac2d6b211
Stéphane Thomas <email@stephanethomas.com>
2016-03-28 13:04:10 +0200

    Purchases: Convert Cancel Purchase Loading Placeholder component to stateless

--- client/me/purchases/cancel-purchase/loading-placeholder.jsx
+++ client/me/purchases/cancel-purchase/loading-placeholder.jsx
@@ -13,32 +13,31 @@ import LoadingPlaceholder from 'me/purchases/components/loading-placeholder';
 import { managePurchase } from 'me/purchases/paths';
 import titles from 'me/purchases/titles';
 
-const CancelPurchaseLoadingPlaceholder = React.createClass( {
-	propTypes: {
-		purchaseId: React.PropTypes.string.isRequired,
-		selectedSite: React.PropTypes.oneOfType( [
-			React.PropTypes.bool,
-			React.PropTypes.object
-		] ).isRequired
-	},
+const CancelPurchaseLoadingPlaceholder = ( { purchaseId, selectedSite } ) => {
+	return (
+		<LoadingPlaceholder
+			title={ titles.cancelPurchase }
+			path={ managePurchase( selectedSite.slug, purchaseId ) }>
+			<Card className="cancel-purchase-loading-placeholder__card">
+				<h2 className="loading-placeholder__content cancel-purchase-loading-placeholder__header" />
+				<div className="loading-placeholder__content cancel-purchase-loading-placeholder__subheader" />
+				<div className="loading-placeholder__content cancel-purchase-loading-placeholder__reason" />
+				<div className="loading-placeholder__content cancel-purchase-loading-placeholder__reason" />
+			</Card>
 
-	render() {
-		return (
-			<LoadingPlaceholder
-				title={ titles.cancelPurchase }
-				path={ managePurchase( this.props.selectedSite.slug, this.props.purchaseId ) }>
-				<Card className="cancel-purchase-loading-placeholder__card">
-					<h2 className="loading-placeholder__content cancel-purchase-loading-placeholder__header" />
-					<div className="loading-placeholder__content cancel-purchase-loading-placeholder__subheader" />
-					<div className="loading-placeholder__content cancel-purchase-loading-placeholder__reason" />
-					<div className="loading-placeholder__content cancel-purchase-loading-placeholder__reason" />
-				</Card>
-				<CompactCard>
-					<Button className="cancel-purchase-loading-placeholder__cancel-button" />
-				</CompactCard>
-			</LoadingPlaceholder>
-		);
-	}
-} );
+			<CompactCard>
+				<Button className="cancel-purchase-loading-placeholder__cancel-button" />
+			</CompactCard>
+		</LoadingPlaceholder>
+	);
+};
+
+CancelPurchaseLoadingPlaceholder.propTypes = {
+	purchaseId: React.PropTypes.string.isRequired,
+	selectedSite: React.PropTypes.oneOfType( [
+		React.PropTypes.bool,
+		React.PropTypes.object
+	] ).isRequired
+};
 
 export default CancelPurchaseLoadingPlaceholder;
