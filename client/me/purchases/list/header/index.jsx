1fd42b477b94c17d04dd7bb1b6d56e6003848c1d
Stéphane Thomas <email@stephanethomas.com>
2016-03-28 13:05:00 +0200

    Purchases: Convert Purchases Header component to stateless

--- client/me/purchases/list/header/index.jsx
+++ client/me/purchases/list/header/index.jsx
@@ -6,35 +6,36 @@ import React from 'react';
 /**
  * Internal dependencies
  */
+import i18n from 'lib/mixins/i18n';
 import NavItem from 'components/section-nav/item';
 import NavTabs from 'components/section-nav/tabs';
 import paths from '../../paths.js';
 import SectionNav from 'components/section-nav';
 
-const PurchasesHeader = React.createClass( {
-	propTypes: {
-		section: React.PropTypes.string.isRequired
-	},
+const PurchasesHeader = ( { section } ) => {
+	let text = i18n.translate( 'Billing History' );
 
-	getSelectedText( activeSection ) {
-		if ( activeSection === 'purchases' ) {
-			return this.translate( 'Purchases' );
-		}
-		return this.translate( 'Billing History' );
-	},
-
-	render() {
-		const activeSection = this.props.section;
-
-		return(
-			<SectionNav selectedText={ this.getSelectedText( activeSection ) }>
-				<NavTabs>
-					<NavItem path={ paths.list() } selected={ activeSection === 'purchases' }>{ this.translate( 'Purchases' ) }</NavItem>
-					<NavItem path="/me/billing" selected= { activeSection === 'billing' } >{ this.translate( 'Billing History' ) }</NavItem>
-				</NavTabs>
-			</SectionNav>
-		);
+	if ( section === 'purchases' ) {
+		text = i18n.translate( 'Purchases' );
 	}
-} );
+
+	return (
+		<SectionNav selectedText={ text }>
+			<NavTabs>
+				<NavItem path={ paths.list() } selected={ section === 'purchases' }>
+					{ i18n.translate( 'Purchases' ) }
+				</NavItem>
+				
+				<NavItem path="/me/billing" selected={ section === 'billing' }>
+					{ i18n.translate( 'Billing History' ) }
+				</NavItem>
+			</NavTabs>
+		</SectionNav>
+	);
+};
+
+PurchasesHeader.propTypes = {
+	section: React.PropTypes.string.isRequired
+};
 
 export default PurchasesHeader;
