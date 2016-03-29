981353124ee0890f3b3606839a9affdda5d0fea2
Andrew Duthie <andrew@andrewduthie.com>
2016-03-29 09:18:50 -0400

    Sites: Display last draft title

--- client/my-sites/resume-editing/index.jsx
+++ client/my-sites/resume-editing/index.jsx
@@ -1,22 +1,37 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
+import { getEditorLastDraftPost } from 'state/ui/editor/last-draft/selectors';
+import { decodeEntities } from 'lib/formatting';
 
 const ResumeEditing = React.createClass( {
+	propTypes: {
+		draft: PropTypes.object
+	},
+
 	render: function() {
+		const { draft } = this.props;
+		if ( ! draft ) {
+			return null;
+		}
+
 		return (
 			<div className="resume-editing">
 				<span className="resume-editing__label">{ this.translate( 'Continue Editing' ) }</span>
-				<span className="resume-editing__post-title">My Post</span>
+				<span className="resume-editing__post-title">{ decodeEntities( draft.title ) }</span>
 			</div>
 		);
 	}
 } );
 
-export default ResumeEditing;
+export default connect( ( state ) => {
+	return {
+		draft: getEditorLastDraftPost( state )
+	};
+} )( ResumeEditing );
