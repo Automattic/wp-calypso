97aa3e7514a0640e0486b85f04286a4631ca9821
Stéphane Thomas <email@stephanethomas.com>
2016-03-29 14:33:40 +0200

    Plans: Convert Jetpack Plan Details component to stateless

--- client/my-sites/plans/jetpack-plan-details/index.jsx
+++ client/my-sites/plans/jetpack-plan-details/index.jsx
@@ -3,23 +3,22 @@
  */
 import React from 'react';
 
-const JetpackPlanDetails = React.createClass( {
-	propTypes: {
-		plan: React.PropTypes.object.isRequired
-	},
-	
-	render() {
-		return (
-			<div>
-				<p>{ this.props.plan.description }</p>
-				<ul>
-					<li>{ this.props.plan.feature_1 }</li>
-					<li>{ this.props.plan.feature_2 }</li>
-					<li>{ this.props.plan.feature_3 }</li>
-				</ul>
-			</div>
-		);
-	}
-} );
+const JetpackPlanDetails = ( { plan } ) => {
+	return (
+		<div>
+			<p>{ plan.description }</p>
+
+			<ul>
+				<li>{ plan.feature_1 }</li>
+				<li>{ plan.feature_2 }</li>
+				<li>{ plan.feature_3 }</li>
+			</ul>
+		</div>
+	);
+};
+
+JetpackPlanDetails.propTypes = {
+	plan: React.PropTypes.object.isRequired
+};
 
 export default JetpackPlanDetails;
