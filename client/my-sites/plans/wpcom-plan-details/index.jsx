072f87ab19cc7907eefe6827b142ed702e484bd4
Stéphane Thomas <email@stephanethomas.com>
2016-03-29 14:34:08 +0200

    Plans: Convert Wpcom Plan Details component to stateless

--- client/my-sites/plans/wpcom-plan-details/index.jsx
+++ client/my-sites/plans/wpcom-plan-details/index.jsx
@@ -3,22 +3,29 @@
  */
 import React from 'react';
 
-const WpcomPlanDetails = React.createClass( {
-	propTypes: {
-		comparePlansUrl: React.PropTypes.string.isRequired,
-		handleLearnMoreClick: React.PropTypes.func.isRequired,
-		plan: React.PropTypes.object.isRequired
-	},
+/**
+ * Internal dependencies
+ */
+import i18n from 'lib/mixins/i18n';
 
-	render() {
-		return (
-			<div>
-				<p>{ this.props.plan.description }</p>
-				<a href={ this.props.comparePlansUrl } onClick={ this.props.handleLearnMoreClick }
-					className="plan__learn-more">{ this.translate( 'Learn more', { context: 'Find out more details about a plan' } ) }</a>
-			</div>
-		);
-	}
-} );
+const WpcomPlanDetails = ( { comparePlansUrl, handleLearnMoreClick, plan } ) => {
+	return (
+		<div>
+			<p>{ plan.description }</p>
+
+			<a href={ comparePlansUrl }
+				onClick={ handleLearnMoreClick }
+				className="plan__learn-more">
+				{ i18n.translate( 'Learn more', { context: 'Find out more details about a plan' } ) }
+			</a>
+		</div>
+	);
+};
+
+WpcomPlanDetails.propTypes = {
+	comparePlansUrl: React.PropTypes.string.isRequired,
+	handleLearnMoreClick: React.PropTypes.func.isRequired,
+	plan: React.PropTypes.object.isRequired
+};
 
 export default WpcomPlanDetails;
