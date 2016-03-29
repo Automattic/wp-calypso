1743e404eb7e0da8448566b57c67ef607ad96bbe
Andrew Duthie <andrew@andrewduthie.com>
2016-03-29 09:53:01 -0400

    Sites: Render query component for last draft in resume editing

--- client/my-sites/resume-editing/index.jsx
+++ client/my-sites/resume-editing/index.jsx
@@ -7,22 +7,30 @@ import { connect } from 'react-redux';
 /**
  * Internal dependencies
  */
-import { getEditorLastDraftPost } from 'state/ui/editor/last-draft/selectors';
+import {
+	getEditorLastDraftPost,
+	getEditorLastDraftSiteId,
+	getEditorLastDraftPostId
+} from 'state/ui/editor/last-draft/selectors';
 import { decodeEntities } from 'lib/formatting';
+import QueryPosts from 'components/data/query-posts';
 
 const ResumeEditing = React.createClass( {
 	propTypes: {
+		siteId: PropTypes.number,
+		postId: PropTypes.number,
 		draft: PropTypes.object
 	},
 
 	render: function() {
-		const { draft } = this.props;
+		const { siteId, postId, draft } = this.props;
 		if ( ! draft ) {
 			return null;
 		}
 
 		return (
 			<div className="resume-editing">
+				<QueryPosts siteId={ siteId } postId={ postId } />
 				<span className="resume-editing__label">{ this.translate( 'Continue Editing' ) }</span>
 				<span className="resume-editing__post-title">{ decodeEntities( draft.title ) }</span>
 			</div>
@@ -32,6 +40,8 @@ const ResumeEditing = React.createClass( {
 
 export default connect( ( state ) => {
 	return {
+		siteId: getEditorLastDraftSiteId( state ),
+		postId: getEditorLastDraftPostId( state ),
 		draft: getEditorLastDraftPost( state )
 	};
 } )( ResumeEditing );
