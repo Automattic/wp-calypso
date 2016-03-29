2aee1321919d7180587b250de3c3389387ea90e4
Stéphane Thomas <email@stephanethomas.com>
2016-03-29 14:34:19 +0200

    Plans: Convert Wpcom Plan Price component to stateless

--- client/my-sites/plans/wpcom-plan-price/index.jsx
+++ client/my-sites/plans/wpcom-plan-price/index.jsx
@@ -3,23 +3,22 @@
  */
 import React from 'react';
 
-const WpcomPlanPrice = React.createClass( {
-	propTypes: {
-		getPrice: React.PropTypes.func.isRequired,
-		hasDiscount: React.PropTypes.bool,
-		periodLabel: React.PropTypes.string.isRequired
-	},
+const WpcomPlanPrice = ( { getPrice, hasDiscount, periodLabel } ) => {
+	return (
+		<div className={ hasDiscount ? "wpcom-plan-price wpcom-plan-price__discount" : "wpcom-plan-price" }>
+			<span>{ getPrice() }</span>
 
-	render() {
-		return (
-			<div className={ this.props.hasDiscount ? "wpcom-plan-price wpcom-plan-price__discount" : "wpcom-plan-price" }>
-				<span>{ this.props.getPrice() }</span>
-				<small className="wpcom-plan-price__billing-period">
-					{ this.props.periodLabel }
-				</small>
-			</div>
-		);
-	}
-} );
+			<small className="wpcom-plan-price__billing-period">
+				{ periodLabel }
+			</small>
+		</div>
+	);
+};
+
+WpcomPlanPrice.propTypes = {
+	getPrice: React.PropTypes.func.isRequired,
+	hasDiscount: React.PropTypes.bool,
+	periodLabel: React.PropTypes.string.isRequired
+};
 
 export default WpcomPlanPrice;
