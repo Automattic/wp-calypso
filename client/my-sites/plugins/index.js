15462e9ba501f4c4aa1603f9e92f89bb7e4e6fec
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2016-03-30 19:44:17 -0300

    New routes for WordPress.com plugins page and catch all. Controlled by manage/plugins/wpcom feature flag.

--- client/my-sites/plugins/index.js
+++ client/my-sites/plugins/index.js
@@ -19,10 +19,20 @@ module.exports = function() {
 	if ( config.isEnabled( 'manage/plugins' ) ) {
 		page( '/plugins/browse/:category/:site', controller.siteSelection, controller.navigation, pluginsController.browsePlugins );
 		page( '/plugins/browse/:siteOrCategory?', controller.siteSelection, controller.navigation, pluginsController.browsePlugins );
-		page( '/plugins', controller.siteSelection, controller.navigation, pluginsController.plugins.bind( null, 'all' ) );
+
+		if ( config.isEnabled( 'manage/plugins/wpcom' ) ) {
+			page( '/plugins', controller.siteSelection, controller.sites );
+			[ 'standard', 'premium', 'business' ].forEach( function( filter ) {
+				page( '/plugins/' + filter + '/:site_id?', controller.siteSelection, controller.navigation, pluginsController.jetpackCanUpdate.bind( null, filter ), pluginsController.plugins.bind( null, filter ) );
+			} );
+		} else {
+			page( '/plugins', controller.siteSelection, controller.navigation, pluginsController.plugins.bind( null, 'all' ) );
+		}
+
 		[ 'active', 'inactive', 'updates' ].forEach( function( filter ) {
 			page( '/plugins/' + filter + '/:site_id?', controller.siteSelection, controller.navigation, pluginsController.jetpackCanUpdate.bind( null, filter ), pluginsController.plugins.bind( null, filter ) );
 		} );
+
 		page( '/plugins/:plugin/:business_plugin?/:site_id?', controller.siteSelection, controller.navigation, pluginsController.plugin );
 	}
 };
