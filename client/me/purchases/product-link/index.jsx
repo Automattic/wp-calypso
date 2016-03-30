e6b5b258ce2eafc2e620c280011ffeb344a15950
Ben Dwyer <ben@scruffian.com>
2016-03-30 14:00:09 +0100

    Purchases: Turning ProductLink into stateless component

--- client/me/purchases/product-link/index.jsx
+++ client/me/purchases/product-link/index.jsx
@@ -12,54 +12,50 @@ import { googleAppsSettingsUrl } from 'lib/google-apps';
 import i18n from 'lib/mixins/i18n';
 import { isDomainProduct, isGoogleApps, isPlan, isSiteRedirect, isTheme } from 'lib/products-values';
 
-const ProductLink = React.createClass( {
-	propTypes: {
-		selectedPurchase: React.PropTypes.object.isRequired,
-		selectedSite: React.PropTypes.oneOfType( [
-			React.PropTypes.bool,
-			React.PropTypes.object
-		] ).isRequired
-	},
+const ProductLink = ( { selectedPurchase, selectedSite } ) => {
+	let url, text;
 
-	render() {
-		const { selectedPurchase, selectedSite } = this.props;
-
-		let url, text;
-
-		if ( ! selectedSite ) {
-			return null;
-		}
-
-		if ( isPlan( selectedPurchase ) ) {
-			url = '/plans/compare';
-			text = i18n.translate( 'View Plan Features' );
-		}
-
-		if ( isDomainProduct( selectedPurchase ) || isSiteRedirect( selectedPurchase ) ) {
-			url = domainManagementEdit( selectedSite.slug, selectedPurchase.meta );
-			text = i18n.translate( 'Domain Settings' );
-		}
-
-		if ( isGoogleApps( selectedPurchase ) ) {
-			url = googleAppsSettingsUrl( selectedPurchase.meta );
-			text = i18n.translate( 'Google Apps Settings' );
-		}
-
-		if ( isTheme( selectedPurchase ) ) {
-			url = getThemeDetailsUrl( { id: selectedPurchase.meta }, { slug: selectedPurchase.domain } );
-			text = i18n.translate( 'Theme Details' );
-		}
-
-		if ( url && text ) {
-			return (
-				<a className="product-link" href={ url }>
-					{ text }
-				</a>
-			);
-		}
-
-		return null;
+	if ( ! selectedSite ) {
+		return <span />;
 	}
-} );
+
+	if ( isPlan( selectedPurchase ) ) {
+		url = '/plans/compare';
+		text = i18n.translate( 'View Plan Features' );
+	}
+
+	if ( isDomainProduct( selectedPurchase ) || isSiteRedirect( selectedPurchase ) ) {
+		url = domainManagementEdit( selectedSite.slug, selectedPurchase.meta );
+		text = i18n.translate( 'Domain Settings' );
+	}
+
+	if ( isGoogleApps( selectedPurchase ) ) {
+		url = googleAppsSettingsUrl( selectedPurchase.meta );
+		text = i18n.translate( 'Google Apps Settings' );
+	}
+
+	if ( isTheme( selectedPurchase ) ) {
+		url = getThemeDetailsUrl( { id: selectedPurchase.meta }, { slug: selectedPurchase.domain } );
+		text = i18n.translate( 'Theme Details' );
+	}
+
+	if ( url && text ) {
+		return (
+			<a className="product-link" href={ url }>
+				{ text }
+			</a>
+		);
+	}
+
+	return <span />;
+};
+
+ProductLink.propTypes = {
+	selectedPurchase: React.PropTypes.object.isRequired,
+	selectedSite: React.PropTypes.oneOfType( [
+		React.PropTypes.bool,
+		React.PropTypes.object
+	] ).isRequired
+};
 
 export default ProductLink;
