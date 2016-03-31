af62514fff7cce5952c70f9334b0765550f46819
Kelly Dwan <kelly.dwan@gmail.com>
2016-03-31 12:35:06 -0400

    Jetpack Plans: Add redux to store the premium plugin installation info & progress.
    - Uses the Flux dispatcher in addition to redux dispatcher for compatibility with existing plugins store
    - Includes tests for reducer and selectors.

---
+++ client/state/plugins/premium/selectors.js
@@ -0,0 +1,54 @@
+import find from 'lodash/find';
+import filter from 'lodash/filter';
+
+const isRequesting = function( state, siteId ) {
+	// if the `isRequesting` attribute doesn't exist yet,
+	// we assume we are still launching the fetch action, so it's true
+	if ( typeof state[ siteId ] === 'undefined' ) {
+		return true;
+	}
+	return state[ siteId ];
+};
+
+const getPluginsForSite = function( state, siteId ) {
+	let pluginList = state[ siteId ];
+	if ( typeof pluginList === 'undefined' ) {
+		return [];
+	}
+	return pluginList;
+}
+
+const isFinished = function( state, siteId ) {
+	let pluginList = getPluginsForSite( state, siteId );
+	if ( pluginList.length === 0 ) {
+		return false;
+	}
+	pluginList = filter( pluginList, ( item ) => {
+		return ( ! item.status.done && ( item.error === null ) );
+	} );
+	return ( pluginList.length === 0 );
+}
+
+const getActivePlugin = function( state, siteId ) {
+	let pluginList = getPluginsForSite( state, siteId );
+	let plugin = find( pluginList, ( item ) => {
+		return ( item.status.start );
+	} );
+	if ( typeof plugin === 'undefined' ) {
+		return false;
+	}
+	return plugin;
+};
+
+const getNextPlugin = function( state, siteId ) {
+	let pluginList = getPluginsForSite( state, siteId );
+	let plugin = find( pluginList, ( item ) => {
+		return ( ! item.status.start && ! item.status.done && ( item.error === null ) );
+	} );
+	if ( typeof plugin === 'undefined' ) {
+		return false;
+	}
+	return plugin;
+}
+
+export default { isRequesting, isFinished, getPluginsForSite, getActivePlugin, getNextPlugin };
