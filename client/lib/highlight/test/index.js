59754104ddee97203cda09d3e4a6729355fe8455
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-04-01 12:04:50 +0200

    Test: Fix ESLint warning in client/lib/hightlight tests

--- client/lib/highlight/test/index.js
+++ client/lib/highlight/test/index.js
@@ -11,11 +11,9 @@ import highlight from '../';
 import useFakeDom from 'test/helpers/use-fake-dom';
 
 describe( 'highlight', function() {
-
 	useFakeDom();
 
 	context( 'unit test', function() {
-
 		it( 'should return html as is if there\'s no term', function() {
 			expect( highlight( '', 'lorem ipsum hello world' ) )
 				.to.equal( 'lorem ipsum hello world' );
@@ -27,28 +25,25 @@ describe( 'highlight', function() {
 		} );
 
 		it( 'should wrap the term with the custom wrapper node when provided', function() {
-			var span = document.createElement('span');
-			span.setAttribute('class', 'marker');
+			var span = document.createElement( 'span' );
+			span.setAttribute( 'class', 'marker' );
 			expect( highlight( 'hello', 'hello world', span ) )
 				.to.contain( '<span class="marker">hello</span> world' );
 		} );
 
 		it( 'should wrap case-insensitively, within a single word, with multiple matches, over multiple lines',
 			function() {
-				expect( highlight ( 'HEllO', 'merhabaHEllO\nselamHEllOdunya' ) )
+				expect( highlight( 'HEllO', 'merhabaHEllO\nselamHEllOdunya' ) )
 					.to.contain( 'merhaba<mark>HEllO</mark>\nselam<mark>HEllO</mark>dunya' );
 			} );
 
 		it( 'should be able to handle html elements with children and text nodes', function() {
 			expect( highlight( 'hello', '<div>hello world<span>hello world</span></div>' ) )
 				.to.contain( '<div><mark>hello</mark> world<span><mark>hello</mark> world</span></div>' );
-
 		} );
 	} );
 
-
 	context( 'edge cases', function() {
-
 		context( 'unicode', function() {
 			it( 'should wrap when the text and query contains unicode chars', function() {
 				expect( highlight( 'üşiöqç', 'lorem ıpsum üşiöqç' ) )
@@ -72,8 +67,6 @@ describe( 'highlight', function() {
 					.to.contain( '<a href="/hello">hello</a>' );
 			} );
 		} );
-
 	} );
-
 } );
 
