cbb2b4a03ffce345eb5fcb0dceefc744ce0ea115
Tugdual de Kerviler <dekervit@gmail.com>
2016-04-04 13:52:55 +0200

    Purchases: Add tests for isCancelable

--- client/lib/purchases/test/data/index.js
+++ client/lib/purchases/test/data/index.js
@@ -4,12 +4,25 @@ const DOMAIN_PURCHASE = {
 	isDomainRegistration: true,
 	meta: 'foo.com',
 	productName: 'Domain Registration',
-	productSlug: 'domain_reg'
+	productSlug: 'domain_reg',
+	isRefundable: true,
+	isCancelable: true
 };
 
 const DOMAIN_PURCHASE_EXPIRED = Object.assign( {}, DOMAIN_PURCHASE, {
 	expiryStatus: 'expired',
-	id: 10002
+	id: 10002,
+	isCancelable: false
+} );
+
+const DOMAIN_PURCHASE_CANCELATION_PERIOD_EXPIRED = Object.assign( {}, DOMAIN_PURCHASE, {
+	id: 10003,
+	isCancelable: false
+} );
+
+const DOMAIN_PURCHASE_INCLUDED_IN_PLAN = Object.assign( {}, DOMAIN_PURCHASE, {
+	id: 10004,
+	expiryStatus: 'included'
 } );
 
 const DOMAIN_MAPPING_PURCHASE = {
@@ -18,12 +31,14 @@ const DOMAIN_MAPPING_PURCHASE = {
 	isDomainRegistration: false,
 	meta: 'boo.com',
 	productName: 'Domain Mapping',
-	productSlug: 'domain_map'
+	productSlug: 'domain_map',
+	isCancelable: true
 };
 
 const DOMAIN_MAPPING_PURCHASE_EXPIRED = Object.assign( {}, DOMAIN_MAPPING_PURCHASE, {
 	expiryStatus: 'expired',
-	id: 20002
+	id: 20002,
+	isCancelable: false
 } );
 
 const SITE_REDIRECT_PURCHASE = {
@@ -32,7 +47,8 @@ const SITE_REDIRECT_PURCHASE = {
 	isDomainRegistration: false,
 	meta: '',
 	productName: 'Site Redirect',
-	productSlug: 'offsite_redirect'
+	productSlug: 'offsite_redirect',
+	isCancelable: false
 };
 
 const SITE_REDIRECT_PURCHASE_EXPIRED = Object.assign( {}, SITE_REDIRECT_PURCHASE, {
@@ -40,11 +56,26 @@ const SITE_REDIRECT_PURCHASE_EXPIRED = Object.assign( {}, SITE_REDIRECT_PURCHASE
 	id: 30002
 } );
 
+const PLAN_PURCHASE = {
+	expiryStatus: 'active',
+	id: 40001,
+	meta: '',
+	productName: 'WordPress.com Premium',
+	productSlug: 'value_bundle',
+	isRefundable: true,
+	isCancelable: true,
+	isRenewable: true,
+	isDomainRegistration: false
+};
+
 export default {
 	DOMAIN_PURCHASE,
 	DOMAIN_PURCHASE_EXPIRED,
+	DOMAIN_PURCHASE_CANCELATION_PERIOD_EXPIRED,
+	DOMAIN_PURCHASE_INCLUDED_IN_PLAN,
 	DOMAIN_MAPPING_PURCHASE,
 	DOMAIN_MAPPING_PURCHASE_EXPIRED,
+	PLAN_PURCHASE,
 	SITE_REDIRECT_PURCHASE,
 	SITE_REDIRECT_PURCHASE_EXPIRED
 }
--- client/lib/purchases/test/index.js
+++ client/lib/purchases/test/index.js
@@ -9,12 +9,15 @@ import { expect } from 'chai';
 import {
 	DOMAIN_PURCHASE,
 	DOMAIN_PURCHASE_EXPIRED,
+	DOMAIN_PURCHASE_CANCELATION_PERIOD_EXPIRED,
+	DOMAIN_PURCHASE_INCLUDED_IN_PLAN,
 	DOMAIN_MAPPING_PURCHASE,
 	DOMAIN_MAPPING_PURCHASE_EXPIRED,
+	PLAN_PURCHASE,
 	SITE_REDIRECT_PURCHASE,
 	SITE_REDIRECT_PURCHASE_EXPIRED
 } from './data';
-import { isRemovable } from '../index';
+import { isRemovable, isCancelable } from '../index';
 
 describe( 'index', () => {
 	describe( '#isRemovable', () => {
@@ -40,6 +43,27 @@ describe( 'index', () => {
 
 		it( 'should be removable when site redirect purchase is expired', () => {
 			expect( isRemovable( SITE_REDIRECT_PURCHASE_EXPIRED ) ).to.be.true;
+		} );
+	} );
+	describe( '#isCancelable', () => {
+		it( 'should not be cancelable when cancelation period is expired', () => {
+			expect( isCancelable( DOMAIN_PURCHASE_CANCELATION_PERIOD_EXPIRED ) ).to.be.false;
+		} );
+
+		it( 'should not be cancelable when the purchase is included in a plan', () => {
+			expect( isCancelable( DOMAIN_PURCHASE_INCLUDED_IN_PLAN ) ).to.be.false;
+		} );
+
+		it( 'should not be cancelable when the purchase is expired', () => {
+			expect( isCancelable( DOMAIN_PURCHASE_EXPIRED ) ).to.be.false;
+		} );
+
+		it( 'should be cancelable when the purchase is refundable', () => {
+			expect( isCancelable( DOMAIN_PURCHASE ) ).to.be.true;
+		} );
+
+		it( 'should be cancelable when the purchase can have auto-renew disabled', () => {
+			expect( isCancelable( PLAN_PURCHASE ) ).to.be.true;
 		} );
 	} );
 } );
