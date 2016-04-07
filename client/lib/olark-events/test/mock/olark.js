b258ab175ff4a182d5b7fee643aff222c3df6e25
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-04-07 15:30:18 +0200

    Test: Move client/lib/olark-events to single test runner

---
+++ client/lib/olark-events/test/mock/olark.js
@@ -0,0 +1,16 @@
+/**
+ * External dependencies
+ */
+import noop from 'lodash/noop';
+
+/**
+ * An empty function that at a high level should look like the olark api provided by https://www.olark.com/api
+ */
+function olark() {
+
+}
+
+olark.configure = noop;
+olark.identify = noop;
+
+export default olark;
