f1682aa29657416e57ad63c2e7c47ae96538acf3
artpi <artpi@post.pl>
2016-04-07 18:52:45 +0100

    Plans: create Google Analytics feature page

--- client/my-sites/plans/index.js
+++ client/my-sites/plans/index.js
@@ -11,6 +11,7 @@ import controller from 'my-sites/controller';
 import paths from './paths';
 import plansController from './controller';
 import { retarget } from 'analytics/ad-tracking';
+import googleAnalyticsLandingPage from './features/google-analytics';
 
 export default function() {
 	if ( config.isEnabled( 'manage/plans' ) ) {
@@ -50,6 +51,14 @@ export default function() {
 			retarget,
 			controller.siteSelection,
 			plansController.redirectToCheckout
+		);
+
+		page(
+			'/plans/features/google-analytics/:domain',
+			retarget,
+			controller.siteSelection,
+			controller.navigation,
+			googleAnalyticsLandingPage
 		);
 
 		page(
