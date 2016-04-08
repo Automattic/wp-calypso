5345f89022c50f66dd6863d26bedf3519da8aa51
Miguel Fonseca <miguelcsf@gmail.com>
2016-04-08 17:06:30 +0100

    State: add nextGuidesTourStep & getGuidesTourState
    So that we can get rid of state within the GuidesTours component.

--- client/state/ui/selectors.js
+++ client/state/ui/selectors.js
@@ -4,6 +4,7 @@
  * Internal dependencies
  */
 import { getSite } from 'state/sites/selectors';
+import guidesToursConfig from 'guidestours/config';
 
 /**
  * Returns the site object for the currently selected site.
@@ -28,4 +29,13 @@ export function getSelectedSite( state ) {
  */
 export function getSelectedSiteId( state ) {
 	return state.ui.selectedSiteId;
+}
+
+export function getGuidesTourState( state ) {
+	const { shouldShow, stepName = '' } = state.ui.guidesTour;
+	const stepConfig = guidesToursConfig[ stepName ] || false;
+	return Object.assign( {}, state.ui.guidesTour, {
+		stepConfig,
+		showPreview: shouldShow && stepConfig.showPreview,
+	} );
 }
