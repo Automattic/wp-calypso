1e37702ff89d922b7c1276790aab0e21d6818035
Miguel Fonseca <miguelcsf@gmail.com>
2016-04-08 19:15:44 +0100

    GuidesPreview: take up entire viewport but masterbar
    Greatly simplifies component logic w.r.t. calculating, guessing, and
    keeping track of elements' positions.

--- client/guidestours/preview.js
+++ client/guidestours/preview.js
@@ -7,44 +7,44 @@ import React, { Component } from 'react';
  * Internal dependencies
  */
 import WebPreview from 'components/web-preview';
-import { getWindowInnerWidth } from 'lib/viewport';
+//import { getWindowInnerWidth } from 'lib/viewport';
 
-const guessedSidebarWidth = () => {
-	const winWidth = getWindowInnerWidth();
-	if ( winWidth <= 660 ) return 0;
-	if ( winWidth <= 960 ) return 229;
-	return 273;
-}
+//const guessedSidebarWidth = () => {
+//	const winWidth = getWindowInnerWidth();
+//	if ( winWidth <= 660 ) return 0;
+//	if ( winWidth <= 960 ) return 229;
+//	return 273;
+//}
 
 export default class GuidesPreview extends Component {
 	constructor( props ) {
 		super( props );
-		this.state = { left: `${ guessedSidebarWidth() }px` };
-		this.handleResize = this.handleResize.bind( this );
+		//this.state = { left: `${ guessedSidebarWidth() }px` };
+		//this.handleResize = this.handleResize.bind( this );
 	}
 
-	componentDidMount() {
-		// TODO(mcsf): The sidebar adjusts _after_ a resize, so if a user's
-		// resizing only generates one event the resizing performed here will
-		// not be based on the latest dimensions of the sidebar. Is it worth
-		// listening to DOM events on the sidebar?…
-		global.window.addEventListener( 'resize', this.handleResize );
-		global.window.addEventListener( 'load', this.handleResize );
-		this.handleResize();
-	}
+	//componentDidMount() {
+	//	// TODO(mcsf): The sidebar adjusts _after_ a resize, so if a user's
+	//	// resizing only generates one event the resizing performed here will
+	//	// not be based on the latest dimensions of the sidebar. Is it worth
+	//	// listening to DOM events on the sidebar?…
+	//	global.window.addEventListener( 'resize', this.handleResize );
+	//	global.window.addEventListener( 'load', this.handleResize );
+	//	this.handleResize();
+	//}
 
-	componentWillUnmount() {
-		global.window.removeEventListener( 'resize', this.handleResize );
-		global.window.removeEventListener( 'load', this.handleResize );
-	}
+	//componentWillUnmount() {
+	//	global.window.removeEventListener( 'resize', this.handleResize );
+	//	global.window.removeEventListener( 'load', this.handleResize );
+	//}
 
-	handleResize() {
-		const base = global.window.document.querySelector( '#secondary .sidebar' );
-		if ( ! base ) return;
+	//handleResize() {
+	//	const base = global.window.document.querySelector( '#secondary .sidebar' );
+	//	if ( ! base ) return;
 
-		const { left, width } = base.getBoundingClientRect();
-		this.setState( { left: `${ left + width }px` } );
-	}
+	//	const { left, width } = base.getBoundingClientRect();
+	//	this.setState( { left: `${ left + width }px` } );
+	//}
 
 	render() {
 		const {
@@ -57,7 +57,7 @@ export default class GuidesPreview extends Component {
 			: '';
 
 		return <WebPreview className="guidestour-preview"
-				style={ this.state }
+				//style={ this.state }
 				{ ...{ showPreview, previewUrl } } />;
 	}
 }
