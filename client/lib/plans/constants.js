0260d74c5e938f41fb276fa963d73922acb69809
artpi <artpi@post.pl>
2016-04-11 09:50:48 +0100

    Upgrades: standalone google analytics page refactor
    This splits off certain google analytics page functions
    to lib, also introduces footer component
    
    Plans: controller slight refactor
    
    Plans: move standalone feature pages to plans-features
    
    Revert "Plans: controller slight refactor"
    
    This reverts commit a1087bceb971ad0f85e190d4e6c10a713e5dcf40.
    
    Upgrades: Move feature titles to utils
    
    Upgrades: Plan libs constants translation
    
    Upgrades: Move plans logic to utils
    
    Upgrades: change class of free plan in feature landing to current
    
    Upgrades: Fix CSS
    
    Upgrades: Back button tweaks
    
    Upgrades: Jetpack protection
    
    Upgrades: move footer to seperate component
    
    Upgrades: move plans-features to plan-feature
    
    Upgrades: fix classes
    
    Upgrades: fix css classnames
    
    Change extension to jsx
    
    Change translate to mixin
    
    Change FeatureFooter to PlanFeatureFooter
    
    Change CSS classes
    
    This reverts commit a94bc9daa9ea79d1f1856314f13436d8f60f3c57.
    
    Conflicts:
    	client/my-sites/plans/plan-feature/footer.jsx
    
    Upgrades: use lodash helpers

--- client/lib/plans/constants.js
+++ client/lib/plans/constants.js
@@ -1,28 +1,58 @@
+import i18n from 'lib/mixins/i18n';
+
 export const plansList = {
-	'free_plan': {},
-	'value_bundle': {},
-	'business-bundle': {},
+	'free_plan': {
+		getTitle: () => i18n.translate( 'Free' ),
+		getPriceTitle: () => i18n.translate( 'Free for life' )
+	},
+	'value_bundle': {
+		getTitle: () => i18n.translate( 'Premium' ),
+		getPriceTitle: () => i18n.translate( '$99 per year' )
+	},
+	'business-bundle': {
+		getTitle: () => i18n.translate( 'Business' ),
+		getPriceTitle: () => i18n.translate( '$299 per year' )
+	},
 	'jetpack_free': {},
 	'jetpack_business': {}
 };
 
+const allPaidPlans = [
+	'value_bundle',
+	'business-bundle'
+];
+
 export const featuresList = {
 	'google-analytics': {
+		getTitle: () => i18n.translate( 'Google Analytics' ),
 		plans: [ 'business-bundle' ]
 	},
-	'extra-storage': {
-		plans: [ 'value_bundle', 'business-bundle' ]
+	'unlimited-storage': {
+		getTitle: () => i18n.translate( 'Unlimited Storage' ),
+		plans: [ 'business-bundle' ]
 	},
 	'custom-domain': {
-		plans: [ 'value_bundle', 'business-bundle' ]
+		getTitle: () => i18n.translate( 'Custom Domain' ),
+		plans: allPaidPlans
 	},
 	'unlimited-premium-themes': {
+		getTitle: () => i18n.translate( 'Unlimited Premium Themes' ),
 		plans: [ 'business-bundle' ]
 	},
 	'video-upload': {
-		plans: [ 'value_bundle', 'business-bundle' ]
+		getTitle: () => i18n.translate( 'VideoPress' ),
+		plans: allPaidPlans
 	},
 	'custom-design': {
-		plans: [ 'value_bundle', 'business-bundle' ]
+		getTitle: () => i18n.translate( 'Custom Design' ),
+		plans: allPaidPlans
+	},
+	'no-ads': {
+		getTitle: () => i18n.translate( 'No Ads' ),
+		plans: allPaidPlans
+	},
+	'live-chat-support': {
+		getTitle: () => i18n.translate( 'Live Chat Support' ),
+		plans: [ 'business-bundle' ]
 	}
 };
