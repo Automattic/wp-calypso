daadfb1553e848fe40fa8c26e9509e91c2634ef1
Alex Gyoshev <alex@gyoshev.net>
2016-04-12 00:22:28 +0300

    Add email verification dialog
    Hide email verification notification on post editor page.

--- client/post-editor/post-editor.jsx
+++ client/post-editor/post-editor.jsx
@@ -38,6 +38,7 @@ const actions = require( 'lib/posts/actions' ),
 	PreferencesActions = require( 'lib/preferences/actions' ),
 	InvalidURLDialog = require( 'post-editor/invalid-url-dialog' ),
 	RestorePostDialog = require( 'post-editor/restore-post-dialog' ),
+	VerifyEmailDialog = require( 'post-editor/verify-email-dialog' ),
 	utils = require( 'lib/posts/utils' ),
 	i18n = require( 'lib/mixins/i18n' ),
 	EditorPreview = require( './editor-preview' ),
@@ -215,6 +216,7 @@ const PostEditor = React.createClass( {
 			isSaving: false,
 			isPublishing: false,
 			notice: false,
+			showVerifyEmailDialog: false,
 			showAutosaveDialog: true,
 			isLoadingAutosave: false,
 			isTitleFocused: false
@@ -435,6 +437,13 @@ const PostEditor = React.createClass( {
 						onRestore={ this.onSaveTrashed }
 					/>
 				: null }
+				{ this.state.showVerifyEmailDialog
+					? <VerifyEmailDialog
+						user={ this.props.user }
+						onClose={ this.closeVerifyEmailDialog }
+						onTryAgain={ this.onPublishAfterVerify }
+					/>
+				: null }
 				{ isInvalidURL
 					? <InvalidURLDialog
 						post={ this.state.post }
@@ -475,6 +484,10 @@ const PostEditor = React.createClass( {
 
 	closeAutosaveDialog: function() {
 		this.setState( { showAutosaveDialog: false } );
+	},
+
+	closeVerifyEmailDialog: function() {
+		this.setState( { showVerifyEmailDialog: false } );
 	},
 
 	getMessage: function( name ) {
@@ -744,8 +757,29 @@ const PostEditor = React.createClass( {
 		}
 	},
 
+	onPublishAfterVerify: function() {
+		var user = this.props.user;
+
+		user.off( 'change', this.onPublish );
+		user.once( 'change', this.onPublish );
+
+		user.fetch();
+	},
+
 	onPublish: function() {
 		var edits = { status: 'publish' };
+		var userInfo = this.props.user.get();
+
+		// do not allow publish for unverified e-mails
+		if ( ! userInfo.email_verified ) {
+			this.setState( {
+				showVerifyEmailDialog: true
+			} );
+
+			return;
+		}
+
+		this.setState( { showVerifyEmailDialog: false } );
 
 		// determine if this is a private publish
 		if ( utils.isPrivate( this.state.post ) ) {
