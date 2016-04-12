cbe9bf61d8a650b2198434b4b978c1d11eaa414e
Payton Swick <payton@foolord.com>
2016-04-12 16:50:13 -0400

    Layout: add DesignPreview to layout if 'preview-layout' is set

--- client/layout/index.jsx
+++ client/layout/index.jsx
@@ -36,6 +36,7 @@ var MasterbarLoggedIn = require( 'layout/masterbar/logged-in' ),
 
 import { isOffline } from 'state/application/selectors';
 import { getGuidedTourState } from 'state/ui/guided-tours/selectors';
+import DesignPreview from 'my-sites/design-preview';
 
 if ( config.isEnabled( 'keyboard-shortcuts' ) ) {
 	KeyboardShortcutsMenu = require( 'lib/keyboard-shortcuts/menu' );
@@ -144,6 +145,17 @@ Layout = React.createClass( {
 		);
 	},
 
+	renderPreview() {
+		if ( config.isEnabled( 'preview-layout' ) && this.props.section.group === 'sites' ) {
+			return (
+				<DesignPreview
+					className="layout__preview"
+					showPreview={ this.props.focus.getCurrent() === 'preview' }
+				/>
+			);
+		}
+	},
+
 	render: function() {
 		var sectionClass = classnames(
 				'wp',
@@ -178,6 +190,7 @@ Layout = React.createClass( {
 				<TranslatorLauncher
 					isEnabled={ translator.isEnabled() }
 					isActive={ translator.isActivated() }/>
+				{ this.renderPreview() }
 			</div>
 		);
 	}
