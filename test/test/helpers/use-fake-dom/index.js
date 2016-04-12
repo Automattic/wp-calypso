1e929f5c917d49f29acf1f15a257890c687d475c
Andrew Duthie <andrew@andrewduthie.com>
2016-04-12 15:01:09 -0400

    Framework: Delete `page` from require cache after DOM cleanup

--- test/test/helpers/use-fake-dom/index.js
+++ test/test/helpers/use-fake-dom/index.js
@@ -13,6 +13,13 @@ export default function domWrapper( markup, features ) {
 	after( function cleanupFakeDom() {
 		log( 'cleaning dom env' );
 		reactTestEnvSetup.cleanup();
+
+		// While it may not be the case that `page` is loaded in the context of
+		// a DOM-dependent test, if it is, we must ensure that it's uncached
+		// after the test finishes, since it stores a reference to the document
+		// at the time of module load which will no longer exist after the
+		// document is destroyed.
+		delete require.cache[ require.resolve( 'page' ) ];
 	} );
 }
 
