ea1c8ee75c89cf18a67f5426f2cfc83a1dcb88f9
retrofox <rdsuarez@gmail.com>
2016-04-14 16:17:51 -0300

    plugins-wpcom: move plugins list to default file. Add i18n.

--- client/my-sites/plugins-wpcom/plugin-panel.jsx
+++ client/my-sites/plugins-wpcom/plugin-panel.jsx
@@ -6,12 +6,17 @@ import { getSelectedSiteId } from 'state/ui/selectors';
 import { getSiteSlug } from 'state/sites/selectors';
 
 import InfoHeader from './info-header';
+
 import StandardPluginsPanel from './standard-plugins-panel';
 import PremiumPluginsPanel from './premium-plugins-panel';
-
 import BusinessPluginsPanel from './business-plugins-panel';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
-import { defaultBusinessPlugins } from './default-plugins';
+
+import {
+	defaultStandardPlugins,
+	defaultPremiumPlugins,
+	defaultBusinessPlugins
+} from './default-plugins';
 
 /*
  * Interpolate the given plugin propagating `siteSlug` value
@@ -28,17 +33,22 @@ export const PluginPanel = React.createClass( {
 		const standardPluginsLink = `/plugins/standard/${ siteSlug }`;
 
 		const interpolateLink = linkInterpolator( siteSlug );
+
+		const standardPlugins = defaultStandardPlugins.map( interpolateLink );
+		const premiumPlugins = defaultPremiumPlugins.map( interpolateLink );
 		const businessPlugins = defaultBusinessPlugins.map( interpolateLink );
 
 		return (
 			<div className="wpcom-plugin-panel">
 				<PageViewTracker path="/plugins/:site" title="Plugins > WPCOM Site" />
 				<InfoHeader />
-				<StandardPluginsPanel displayCount={ 6 } />
+				<StandardPluginsPanel plugins={ standardPlugins } displayCount={ 6 } />
+
 				<Card className="wpcom-plugin-panel__panel-footer" href={ standardPluginsLink }>
 					{ this.translate( 'View all standard plugins' ) }
 				</Card>
-				<PremiumPluginsPanel />
+
+				<PremiumPluginsPanel plugins={ premiumPlugins } />
 				<BusinessPluginsPanel plugins={ businessPlugins } />
 			</div>
 		);
