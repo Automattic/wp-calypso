17872e996f5b035bf5148833463d8110c39de682
ehg <chris.blower@jumpwire.co.uk>
2016-04-15 13:18:09 +0100

    GuidedTours: Add step tracking
    - Use actions and new analytics middleware to track events
    - Add tracks events for tour start, next step, quit & finish
    - Clean up step propTypes
    
    Props @lsinger

--- client/state/ui/actions.js
+++ client/state/ui/actions.js
@@ -8,6 +8,11 @@ import {
 	UPDATE_GUIDESTOUR,
 } from 'state/action-types';
 
+import {
+	withAnalytics,
+	recordTracksEvent,
+} from 'state/analytics/actions';
+
 /**
  * Returns an action object to be used in signalling that a site has been set
  * as selected.
@@ -50,19 +55,48 @@ export function setSection( section, options = {} ) {
  * @param {Object} options Options object, see fn signature.
  * @return {Object} Action object
  */
-export function showGuidesTour( { shouldShow, shouldDelay = false, tour = 'main', siteId = null } ) {
-	return {
+export function showGuidesTour( { shouldShow, shouldDelay = false, tour = 'main' } ) {
+	const showAction = {
 		type: SHOW_GUIDESTOUR,
 		shouldShow,
 		shouldDelay,
 		tour,
-		siteId,
-	}
+	};
+
+	const trackEvent = recordTracksEvent( 'calypso_guided_tours_show', {
+		tour,
+	} );
+
+	return withAnalytics( trackEvent, showAction );
 }
 
-export function nextGuidesTourStep( stepName ) {
-	return {
+export function quitGuidesTour( { tour = 'main', stepName, finished } ) {
+	const quitAction = {
+		type: UPDATE_GUIDESTOUR,
+		shouldShow: false,
+		shouldReallyShow: false,
+		shouldDelay: false,
+		tour,
+		stepName,
+	};
+
+	const trackEvent = recordTracksEvent( `calypso_guided_tours_${ finished ? 'finished' : 'quit' }`, {
+		step: stepName,
+		tour,
+	} );
+
+	return withAnalytics( trackEvent, quitAction );
+}
+export function nextGuidesTourStep( { tour = 'main', stepName } ) {
+	const nextAction = {
 		type: UPDATE_GUIDESTOUR,
 		stepName,
 	};
+
+	const trackEvent = recordTracksEvent( 'calypso_guided_tours_next_step', {
+		step: stepName,
+		tour,
+	} );
+
+	return withAnalytics( trackEvent, nextAction );
 }
