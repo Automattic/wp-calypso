8103124c5dffcc69eb1a208c82338ee7b042fdb7
retrofox <rdsuarez@gmail.com>
2016-04-18 11:26:50 -0300

    plugins-wpcom: fix google-analytics link

--- client/my-sites/plugins-wpcom/default-plugins.js
+++ client/my-sites/plugins-wpcom/default-plugins.js
@@ -165,7 +165,7 @@ export const defaultPremiumPlugins = [
 export const defaultBusinessPlugins = [
 	{
 		name: i18n.translate( 'Google Analytics' ),
-		descriptionLink: '/plans/features/{siteSlug}',
+		descriptionLink: '/plans/features/google-analytics/{siteSlug}',
 		icon: 'stats',
 		plan: 'Business',
 		description: i18n.translate( 'Advanced features to complement WordPress.com stats. Funnel reports, goal conversion, and more.' )
