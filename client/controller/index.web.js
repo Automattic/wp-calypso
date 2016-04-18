68bf03f8ea67c41da49ee350b3d715f055cef4b8
Bernhard Reiter <ockham@raz.or.at>
2016-04-18 14:37:10 +0200

    client/controller: Simplify destructuring

--- client/controller/index.web.js
+++ client/controller/index.web.js
@@ -72,7 +72,7 @@ export const ReduxWrappedLayout = ( { store, primary, secondary, tertiary } ) =>
  * divs.
  */
 export function clientRouter( route, ...middlewares ) {
-	page( route, ...[ ...middlewares, render ] );
+	page( route, ...middlewares, render );
 }
 
 function render( context ) {
