3de403673a88b3b79a6f5fa7624dafaa88af198d
Dennis Snell <dennis.snell@automattic.com>
2016-04-18 14:58:36 +0100

    Update PageViewTracker to use new analytics redux actions (#4802)
    * Update PageViewTracker to use new analytics redux actions
    
    Previously the PageViewTracker performed some shady moves to be at the
    same time testable and side-effecting in how it fired off the analytics
    action.
    
    In #4699 a new analytics Redux middlware was introduced that allows us
    to stop calling the analytics code directly, leaving pure React
    components in the wake.
    
    This update uses a new analytics action with the middleware to simplify
    and purify the new PageViewTracker component.
    
    Props to @Tug for his help reviewing

--- client/lib/analytics/page-view-tracker/index.jsx
+++ client/lib/analytics/page-view-tracker/index.jsx
@@ -1,8 +1,56 @@
-import analytics from 'lib/analytics';
-import PageViewTracker from './component';
+import React, { PropTypes } from 'react';
+import flowRight from 'lodash/flowRight';
+import has from 'lodash/has';
+import noop from 'lodash/noop';
+import { connect } from 'react-redux';
+import { recordPageView } from 'state/analytics/actions';
 
-const recorder = ( path, title ) => analytics.pageView.record( path, title );
+export const PageViewTracker = React.createClass( {
+	getInitialState: () => ( {
+		timer: null
+	} ),
 
-const Tracker = PageViewTracker( recorder );
+	componentDidMount() {
+		this.queuePageView();
+	},
 
-export default Tracker;
+	componentWillUnmount() {
+		clearTimeout( this.state.timer );
+	},
+
+	queuePageView() {
+		const {
+			delay = 0,
+			path,
+			recorder = noop,
+			title
+		} = this.props;
+
+		if ( this.state.timer ) {
+			return;
+		}
+
+		if ( ! delay ) {
+			return recorder( path, title );
+		}
+
+		this.setState( {
+			timer: setTimeout( () => recorder( path, title ), delay )
+		} );
+	},
+
+	render: () => null
+} );
+
+PageViewTracker.propTypes = {
+	delay: PropTypes.number,
+	path: PropTypes.string.isRequired,
+	recorder: PropTypes.func,
+	title: PropTypes.string.isRequired
+};
+
+const mapDispatchToProps = dispatch => ( {
+	recorder: flowRight( dispatch, recordPageView )
+} );
+
+export default connect( null, mapDispatchToProps )( PageViewTracker );
