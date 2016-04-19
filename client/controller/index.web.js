d8696e11bfbfc7206efde6f4f4e2b4a2cb38a35c
Steve Seear <steve.seear@gmail.com>
2016-04-19 17:05:16 +0100

    Revert "Revert "Framework: Fix errors on transition from themes to signup""

--- client/controller/index.web.js
+++ client/controller/index.web.js
@@ -4,36 +4,60 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
-import { setSection as setSectionAction } from 'state/ui/actions';
-import noop from 'lodash/noop';
+import page from 'page';
 
 /**
- * Internal dependencies
+ * Internal Dependencies
  */
-import page from 'page';
-import LayoutLoggedOut from 'layout/logged-out';
+import Layout from 'layout';
+import layoutFocus from 'lib/layout-focus';
+import nuxWelcome from 'layout/nux-welcome';
+import translatorInvitation from 'layout/community-translator/invitation-utils';
+import userFactory from 'lib/user';
+import sitesFactory from 'lib/sites-list';
 import debugFactory from 'debug';
 
+/**
+ * Re-export
+ */
+export { makeLoggedOutLayout, setSection } from './index.node.js';
+
+const user = userFactory();
+const sites = sitesFactory();
 const debug = debugFactory( 'calypso:controller' );
 
-/**
- * @param { object } context -- Middleware context
- * @param { function } next -- Call next middleware in chain
- *
- * Produce a `LayoutLoggedOut` element in `context.layout`, using
- * `context.primary`, `context.secondary`, and `context.tertiary` to populate it.
-*/
-export function makeLoggedOutLayout( context, next ) {
+export function makeLayout( context, next ) {
 	const { store, primary, secondary, tertiary } = context;
+
 	context.layout = (
-		<ReduxProvider store={ store }>
-			<LayoutLoggedOut primary={ primary }
-				secondary={ secondary }
-				tertiary={ tertiary } />
-		</ReduxProvider>
+		<ReduxWrappedLayout store={ store }
+			primary={ primary }
+			secondary={ secondary }
+			tertiary={ tertiary }
+		/>
 	);
 	next();
 };
+
+export const ReduxWrappedLayout = ( { store, primary, secondary, tertiary } ) => (
+	<ReduxProvider store={ store }>
+		{ user.get()
+			? <Layout primary={ primary }
+				secondary={ secondary }
+				tertiary={ tertiary }
+				user={ user }
+				sites={ sites }
+				focus={ layoutFocus }
+				nuxWelcome={ nuxWelcome }
+				translatorInvitation={ translatorInvitation }
+			/>
+			: <Layout primary={ primary }
+				secondary={ secondary }
+				tertiary={ tertiary }
+				focus={ layoutFocus } />
+		}
+	</ReduxProvider>
+);
 
 /**
  * Isomorphic routing helper, client side
@@ -51,14 +75,6 @@ export function makeLoggedOutLayout( context, next ) {
  */
 export function clientRouter( route, ...middlewares ) {
 	page( route, ...[ ...middlewares, render ] );
-}
-
-export function setSection( section ) {
-	return ( context, next = noop ) => {
-		context.store.dispatch( setSectionAction( section ) );
-
-		next();
-	}
 }
 
 function render( context ) {
--- client/signup/index.js
+++ client/signup/index.js
@@ -1,41 +1,39 @@
 /**
- * External dependencies
- */
-var page = require( 'page' );
-
-/**
  * Internal dependencies
  */
 var controller = require( './controller' ),
 	jetpackConnectController = require( './jetpack-connect/controller' ),
 	adTracking = require( 'lib/analytics/ad-tracking' ),
-	config = require( 'config' );
+	config = require( 'config' ),
+	makeLayout = require( 'controller' ).makeLayout;
 
-module.exports = function() {
+module.exports = function( router ) {
 	if ( config.isEnabled( 'phone_signup' ) ) {
-		page( '/phone/:lang?', controller.phoneSignup );
+		router( '/phone/:lang?', controller.phoneSignup, makeLayout );
 	}
 
-	page(
+	router(
 		'/start/:flowName?/:stepName?/:stepSectionName?/:lang?',
 		adTracking.retarget,
 		controller.saveRefParameter,
 		controller.saveQueryObject,
 		controller.redirectWithoutLocaleIfLoggedIn,
 		controller.redirectToFlow,
-		controller.start
+		controller.start,
+		makeLayout
 	);
 
 	if ( config.isEnabled( 'login' ) ) {
-		page( '/log-in/:lang?', controller.login );
+		router( '/log-in/:lang?', controller.login, makeLayout );
 	}
 
 	if ( config.isEnabled( 'jetpack/calypso-first-signup-flow' ) ) {
-		page( '/jetpack/connect', jetpackConnectController.connect );
-		page(
+		router( '/jetpack/connect', jetpackConnectController.connect, makeLayout );
+		router(
 			'/jetpack/connect/authorize',
 			jetpackConnectController.saveQueryObject,
-			jetpackConnectController.authorizeForm
+			jetpackConnectController.authorizeForm,
+			makeLayout
 		);
 	}
 };
