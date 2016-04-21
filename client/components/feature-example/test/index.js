409c1f5074867969565266a46a1fa33c8e99eecb
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-04-21 10:09:25 +0200

    Test: Fix client/components/feature-example test

--- client/components/feature-example/test/index.js
+++ client/components/feature-example/test/index.js
@@ -11,14 +11,13 @@ import { shallow } from 'enzyme';
 import FeatureExample from '../index';
 
 describe( 'Feature Example', function() {
-	it( 'should have Feature-example class', function() {
+	it( 'should have Feature-example class', () => {
 		const featureExample = shallow( <FeatureExample /> );
-		assert.equal( 1, featureExample.find( '.feature-example' ).length );
+		assert.lengthOf( featureExample.find( '.feature-example' ), 1 );
 	} );
 
-	// TODO: fix it
-	it.skip( 'should contains the passed children wrapped by a feature-example div', function() {
+	it( 'should contains the passed children wrapped by a feature-example div', () => {
 		const featureExample = shallow( <FeatureExample><div>test</div></FeatureExample> );
-		assert.equal( true, featureExample.contains( '<div>test</div>' ) );
+		assert.isTrue( featureExample.contains( <div>test</div> ) );
 	} );
 } );
