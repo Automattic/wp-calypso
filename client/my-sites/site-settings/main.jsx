16af51d8ec975c03e94e944cb76d7b29a9d0af6b
retrofox <rdsuarez@gmail.com>
2016-04-22 10:18:54 -0300

    site-settings: refact -> ES6ify. Classify. Tidy.

--- client/my-sites/site-settings/main.jsx
+++ client/my-sites/site-settings/main.jsx
@@ -1,127 +1,187 @@
 /**
  * External dependencies
  */
-var React = require( 'react' ),
-	debug = require( 'debug' )( 'calypso:my-sites:site-settings' );
+import React, { PropTypes, Component } from 'react';
+import debugFactory from 'debug';
 
 /**
  * Internal dependencies
  */
-var config = require( 'config' ),
-	SectionNav = require( 'components/section-nav' ),
-	NavTabs = require( 'components/section-nav/tabs' ),
-	NavItem = require( 'components/section-nav/item' ),
-	GeneralSettings = require( './section-general' ),
-	WritingSettings = require( './form-writing' ),
-	DiscussionSettings = require( './section-discussion' ),
-	AnalyticsSettings = require( './section-analytics' ),
-	ImportSettings = require( './section-import' ),
-	ExportSettings = require( './section-export' ),
-	SiteSecurity = require( './section-security' ),
-	SidebarNavigation = require( 'my-sites/sidebar-navigation' );
+import config from 'config';
+import SectionNav from 'components/section-nav';
+import NavTabs from 'components/section-nav/tabs';
+import NavItem from 'components/section-nav/item';
+import GeneralSettings from './section-general';
+import WritingSettings from './form-writing';
+import DiscussionSettings from './section-discussion';
+import AnalyticsSettings from './section-analytics';
+import ImportSettings from './section-import';
+import ExportSettings from './section-export';
+import SiteSecurity from './section-security';
+import SidebarNavigation from 'my-sites/sidebar-navigation';
 
-module.exports = React.createClass( {
+/**
+ * Module vars
+ */
+const debug = debugFactory( 'calypso:my-sites:site-settings' );
 
-	displayName: 'SiteSettings',
+export class SiteSettings extends Component {
+	constructor( props ) {
+		super( props );
 
-	getInitialState: function() {
-		return { site: this.props.sites.getSelectedSite() };
-	},
+		this.state = {
+			site: this.props.sites.getSelectedSite()
+		};
+	}
 
-	componentWillMount: function() {
+	componentWillMount() {
 		debug( 'Mounting SiteSettings React component.' );
-		this.props.sites.on( 'change', this._updateSite );
-	},
+		this.props.sites.on( 'change', this.updateSite );
+	}
 
-	componentWillUnmount: function() {
-		this.props.sites.off( 'change', this._updateSite );
-	},
+	componentWillUnmount() {
+		this.props.sites.off( 'change', this.updateSite );
+	}
 
-	getImportPath: function() {
-		var site = this.state.site,
-			path = '/settings/import';
+	getImportPath() {
+		var { site } = this.state;
+		let path = '/settings/import';
 
 		if ( site.jetpack ) {
 			return site.options.admin_url + 'import.php';
 		}
 
 		return [ path, site.slug ].join( '/' );
-	},
+	}
 
-	getExportPath: function() {
-		var site = this.state.site;
-		return site.jetpack ? site.options.admin_url + 'export.php' : '/settings/export/' + site.slug;
-	},
+	getExportPath() {
+		var { site } = this.state;
+		return site.jetpack
+			? `${ site.options.admin_url }export.php`
+			: `/settings/export/${ site.slug }`;
+	}
 
-	getStrings: function() {
+	getStrings() {
 		return {
 			general: this.translate( 'General', { context: 'settings screen' } ),
 			writing: this.translate( 'Writing', { context: 'settings screen' } ),
 			discussion: this.translate( 'Discussion', { context: 'settings screen' } ),
 			analytics: this.translate( 'Analytics', { context: 'settings screen' } ),
 			security: this.translate( 'Security', { context: 'settings screen' } ),
-			'import': this.translate( 'Import', { context: 'settings screen' } ),
-			'export': this.translate( 'Export', { context: 'settings screen' } ),
+			import: this.translate( 'Import', { context: 'settings screen' } ),
+			export: this.translate( 'Export', { context: 'settings screen' } ),
 		};
-	},
+	}
 
-	getSections: function() {
-		var site = this.state.site;
+	getSections() {
+		var { site, purchases, context } = this.state;
 		return {
 			general: (
 				<GeneralSettings
 					site={ site }
-					purchases={ this.props.purchases } />
+					purchases={ purchases } />
 			),
 			writing: <WritingSettings site={ site } />,
 			discussion: <DiscussionSettings site={ site } />,
 			security: <SiteSecurity site={ site } />,
 			analytics: <AnalyticsSettings site={ site } />,
-			'import': <ImportSettings site={ site } />,
-			'export': <ExportSettings site={ site } store={ this.props.context.store } />
+			import: <ImportSettings site={ site } />,
+			export: <ExportSettings site={ site } store={ context.store } />
 		};
-	},
+	}
 
-	render: function() {
-		var site = this.state.site,
-			section = this.props.section,
-			strings = this.getStrings(),
-			selectedText = strings[ section ],
-			settingsSection = this.getSections();
+	renderSectioNav() {
+		const { site, section } = site;
+		const strings = this.getStrings();
+		const selectedText = strings[ section ];
+
+		if ( ! site ) {
+			return ( <SectionNav /> );
+		}
+
+		return (
+			<SectionNav selectedText={ selectedText } >
+				<NavTabs>
+					<NavItem
+						path={ `/settings/general/${ site.slug }` }
+						selected={ section === 'general' } >
+							{ strings.general }
+					</NavItem>
+
+					<NavItem
+						path={ `/settings/writing/${ site.slug }` }
+						selected={ section === 'writing' } >
+							{ strings.writing }
+					</NavItem>
+
+					<NavItem
+						path={ `/settings/discussion/${ site.slug }` }
+						selected={ section === 'discussion' } >
+							{ strings.discussion }
+					</NavItem>
+
+					{
+						! site.jetpack && config.isEnabled( 'manage/plans' ) &&
+							<NavItem
+								path={ `/settings/analytics/${ site.slug }` }
+								selected={ section === 'analytics' } >
+									{ strings.analytics }
+							</NavItem>
+					}
+
+					{
+						config.isEnabled( 'manage/security' ) && site.jetpack &&
+							<NavItem path={ `/settings/security/${ site.slug }` }
+							selected={ section === 'security' } >
+								{ strings.security }
+						</NavItem>
+					}
+
+					{
+						config.isEnabled( 'manage/import' ) &&
+							<NavItem
+								path={ this.getImportPath() }
+								selected={ section === 'import' }
+								isExternalLink={ !! site.jetpack } >
+									{ strings.import }
+							</NavItem>
+					}
+
+					{
+						config.isEnabled( 'manage/export' ) &&
+							<NavItem
+								path={ this.getExportPath() }
+								selected={ section === 'export' }
+								isExternalLink={ !! site.jetpack } >
+									{ strings.export }
+							</NavItem>
+					}
+				</NavTabs>
+			</SectionNav>
+		);
+	}
+
+	render() {
+		var { site, section } = this.state.site;
+		const settingsSection = this.getSections();
 
 		return (
 			<section className="site-settings">
 				<div className="main main-column" role="main">
 					<SidebarNavigation />
-					{ site ?
-					<SectionNav selectedText={ selectedText }>
-						<NavTabs>
-							<NavItem path={ '/settings/general/' + site.slug } selected={ section === 'general' }>{ strings.general }</NavItem>
-							<NavItem path={ '/settings/writing/' + site.slug } selected={ section === 'writing' } >{ strings.writing }</NavItem>
-							<NavItem path={ '/settings/discussion/' + site.slug } selected={ section === 'discussion' } >{ strings.discussion }</NavItem>
-							{ ! site.jetpack && config.isEnabled( 'manage/plans' ) ?
-								<NavItem path={ '/settings/analytics/' + site.slug } selected={ section === 'analytics' } >{ strings.analytics }</NavItem>
-							: null }
-							{ ( config.isEnabled( 'manage/security' ) && site.jetpack ) ?
-								<NavItem path={ '/settings/security/' + site.slug } selected={ section === 'security' } >{ strings.security }</NavItem>
-							: null }
-							{ config.isEnabled( 'manage/import' ) ?
-								<NavItem path={ this.getImportPath() } selected={ section === 'import' } isExternalLink={ !! site.jetpack } >{ strings.import }</NavItem>
-							: null }
-							{ config.isEnabled( 'manage/export' ) ?
-								<NavItem path={ this.getExportPath() } selected={ section === 'export' } isExternalLink={ !! site.jetpack } >{ strings.export }</NavItem>
-							: null }
-						</NavTabs>
-					</SectionNav>
-					: <SectionNav /> }
+
+					{ this.renderSectioNav( site ) }
 					{ site && settingsSection[ this.props.section ] }
 				</div>
 			</section>
 		);
-	},
-
-	_updateSite: function() {
-		this.setState( { site: this.props.sites.getSelectedSite() } );
 	}
 
-} );
+	_pdateSite() {
+		this.setState( { site: this.props.sites.getSelectedSite() } );
+	}
+}
+
+SiteSettings.propTypes = {
+	site: PropTypes.object
+};
