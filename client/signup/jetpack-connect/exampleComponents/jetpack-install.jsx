df6ed6e4291f5ab2130dac42c1675dcf5b79acfe
johnHackworth <javier.alvarez.lop@gmail.com>
2016-04-22 17:51:32 +0200

    Jetpack connect: new install step

---
+++ client/signup/jetpack-connect/exampleComponents/jetpack-install.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default React.createClass( {
+	displayName: 'JetpackConnectExampleInstall',
+
+	render() {
+		return (
+			<div className="jetpack-connect__example">
+				something something install
+			</div>
+		);
+	}
+} );
