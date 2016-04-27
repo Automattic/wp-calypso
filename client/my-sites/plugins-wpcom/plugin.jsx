6de008bba594570d568652b77555d24e992e60aa
Dave Whitley <drw158@gmail.com>
2016-04-27 14:39:41 -0500

    Plugins (wpcom): Update design and text (#4995)
    - Added a hover state that makes it more obvious that the links are external.
    - Refactored the plugin component
    - Updated several plugin descriptions

--- client/my-sites/plugins-wpcom/plugin.jsx
+++ client/my-sites/plugins-wpcom/plugin.jsx
@@ -1,13 +1,23 @@
 import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
-import get from 'lodash/get';
 import noop from 'lodash/noop';
-
-import { recordTracksEvent } from 'state/analytics/actions';
 
 import Gridicon from 'components/gridicon';
 
-export const StandardPlugin = React.createClass( {
+const hasHttpProtocol = url => ( /^https?:\/\//.test( url ) );
+
+export const Plugin = React.createClass( {
+	getInitialState() {
+		return { isUnderMouse: false };
+	},
+
+	startHover() {
+		this.setState( { isUnderMouse: true } );
+	},
+
+	stopHover() {
+		this.setState( { isUnderMouse: false } );
+	},
+
 	render() {
 		const {
 			category,
@@ -18,11 +28,29 @@ export const StandardPlugin = React.createClass( {
 			descriptionLink
 		} = this.props;
 
+		const { isUnderMouse } = this.state;
+
+		const isExternalLink = hasHttpProtocol( descriptionLink );
+
+		const target = isExternalLink
+			? '_blank'
+			: '_self';
+
+		const linkIcon = ( isExternalLink && isUnderMouse )
+			? 'external'
+			: icon;
+
 		return (
 			<div className="wpcom-plugins__plugin-item">
-				<a onClick={ onClick } href={ descriptionLink } target="_blank">
+				<a
+					href={ descriptionLink }
+					onClick={ onClick }
+					onMouseEnter={ this.startHover }
+					onMouseLeave={ this.stopHover }
+					target={ target }
+				>
 					<div className="wpcom-plugins__plugin-icon">
-						<Gridicon { ...{ icon } } />
+						<Gridicon icon={ linkIcon } />
 					</div>
 					<div className="wpcom-plugins__plugin-title">{ name }</div>
 					<div className="wpcom-plugins__plugin-category">{ category }</div>
@@ -33,7 +61,7 @@ export const StandardPlugin = React.createClass( {
 	}
 } );
 
-StandardPlugin.propTypes = {
+Plugin.propTypes = {
 	category: PropTypes.string.isRequired,
 	description: PropTypes.string.isRequired,
 	icon: PropTypes.string,
@@ -42,16 +70,4 @@ StandardPlugin.propTypes = {
 	descriptionLink: PropTypes.string.isRequired
 };
 
-const trackClick = name => recordTracksEvent(
-	'calypso_plugin_wpcom_click',
-	{
-		plugin_name: name,
-		plugin_plan: 'standard'
-	}
-);
-
-const mapDispatchToProps = ( dispatch, props ) => ( {
-	onClick: get( props, 'onClick', () => dispatch( trackClick( props.name ) ) )
-} );
-
-export default connect( null, mapDispatchToProps )( StandardPlugin );
+export default Plugin;
