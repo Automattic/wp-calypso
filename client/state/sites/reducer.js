12fa21b9d0cfa56fb1e7439e3e0789fa4a47e2b5
Kerry Liu <kerryliu@gmail.com>
2016-04-27 16:43:52 -0700

    Framework: update sites reducer to handle all sites

--- client/state/sites/reducer.js
+++ client/state/sites/reducer.js
@@ -3,13 +3,22 @@
  */
 import { combineReducers } from 'redux';
 import pick from 'lodash/pick';
+import keyBy from 'lodash/keyBy';
 
 /**
  * Internal dependencies
  */
 import { plans } from './plans/reducer';
 import mediaStorage from './media-storage/reducer';
-import { SITE_RECEIVE, DESERIALIZE } from 'state/action-types';
+import {
+	SITE_RECEIVE,
+	SITES_RECEIVE,
+	SITES_REQUEST,
+	SITES_REQUEST_FAILURE,
+	SITES_REQUEST_SUCCESS,
+	DESERIALIZE,
+	SERIALIZE
+} from 'state/action-types';
 import { sitesSchema } from './schema';
 import { isValidStateWithSchema } from 'state/utils';
 
@@ -34,20 +43,44 @@ export function items( state = {}, action ) {
 			return Object.assign( {}, state, {
 				[ site.ID ]: site
 			} );
-
+		case SITES_RECEIVE:
+			return keyBy( action.sites, 'ID' );
 		case DESERIALIZE:
 			if ( isValidStateWithSchema( state, sitesSchema ) ) {
 				return state;
 			}
-
 			return {};
 	}
 
 	return state;
 }
 
+/**
+ * Tracks sites fetching state
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action payload
+ * @return {Object}        Updated state
+ */
+export function fetchingItems( state = {}, action ) {
+	switch ( action.type ) {
+		case SITES_REQUEST:
+		case SITES_REQUEST_FAILURE:
+		case SITES_REQUEST_SUCCESS:
+			return Object.assign( {}, state, {
+				all: action.type === SITES_REQUEST
+			} );
+		case SERIALIZE:
+			return {};
+		case DESERIALIZE:
+			return {};
+	}
+	return state;
+}
+
 export default combineReducers( {
 	items,
+	fetchingItems,
 	mediaStorage,
 	plans
 } );
