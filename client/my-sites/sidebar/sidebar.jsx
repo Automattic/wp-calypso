6472d608074473530c5489b01534254e6828b336
artpi <artpi@post.pl>
2016-04-28 14:27:01 +0200

    Remove feature flag

--- client/my-sites/sidebar/sidebar.jsx
+++ client/my-sites/sidebar/sidebar.jsx
@@ -348,7 +348,6 @@ module.exports = React.createClass( {
 
 		// Show plan details for upgraded sites
 		if (
-			config.isEnabled( 'manage/plans/my-plan' ) &&
 			abtest( 'sidebarPlanLink' ) === 'plans/my-plan' &&
 			site &&
 			( isPremium( site.plan ) || isBusiness( site.plan ) )
