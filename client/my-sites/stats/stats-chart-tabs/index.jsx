46b6eaed1a1b2bb8f14de38e1d704266da5b1871
Matías Ventura <mv@matiasventura.com>
2016-04-29 11:06:07 +0200

    Remove stats 31_more_likes test variation.

--- client/my-sites/stats/stats-chart-tabs/index.jsx
+++ client/my-sites/stats/stats-chart-tabs/index.jsx
@@ -7,7 +7,6 @@ import classNames from 'classnames';
 /**
  * Internal dependencies
  */
-
 import ElementChart from 'components/chart';
 import Legend from 'components/chart/legend';
 import StatTabs from '../stats-tabs';
@@ -15,9 +14,6 @@ import analytics from 'lib/analytics';
 import observe from 'lib/mixins/data-observe';
 import StatsModulePlaceholder from '../stats-module/placeholder';
 import Card from 'components/card';
-import UpgradeNudge from 'my-sites/upgrade-nudge';
-import { abtest } from 'lib/abtest';
-import TrackComponentView from 'lib/analytics/track-component-view';
 
 export default React.createClass( {
 	displayName: 'StatModuleChartTabs',
@@ -213,25 +209,6 @@ export default React.createClass( {
 		return chartData;
 	},
 
-	renderNudge: function() {
-		if ( abtest( 'statsTabsLikesNudge' ) === 'dataInformedBelowChart' ) {
-			return (
-				<UpgradeNudge
-					title={ this.translate( 'Sites with Premium get 31% more likes' ) }
-					message={ this.translate( 'Premium plan owners get a domain, custom design, and on avarage 31% more likes!' ) }
-					event={ 'stats_likes_31_more' }
-				/>
-			);
-		} else {
-			return (
-				<TrackComponentView
-					eventName={ 'calypso_upgrade_nudge_hide' }
-					eventProperties={ { cta_name: 'stats_likes_31_more' } }
-				/>
-			);
-		}
-	},
-
 	render: function() {
 		var data = this.buildChartData(),
 			activeTab = this.getActiveTab(),
@@ -266,7 +243,6 @@ export default React.createClass( {
 					<ElementChart loading={ activeTabLoading } data={ data } barClick={ this.props.barClick } />
 					<StatTabs dataList={ visitsList } tabs={ this.props.charts } switchTab={ this.props.switchTab } selectedTab={ this.props.chartTab } activeIndex={ this.props.queryDate } activeKey="period" />
 				</Card>
-				{ this.props.chartTab === 'likes' && this.renderNudge() }
 			</div>
 		);
 	}
