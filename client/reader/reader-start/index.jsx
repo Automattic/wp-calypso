cd069786e8e38dad8aa0c1110e457d3d4ad84409
bluefuton <chris@bluefuton.com>
2016-05-02 16:54:36 +0100

    Added card and post preview components, marked up sample content, and rough cut of CSS

--- client/reader/reader-start/index.jsx
+++ client/reader/reader-start/index.jsx
@@ -6,8 +6,7 @@ import { connect } from 'react-redux';
 // Internal dependencies
 import Main from 'components/main';
 import Button from 'components/button';
-import PostCard from './post-card';
-import SiteCard from './site-card';
+import ReaderStartCard from './card';
 
 const debug = debugModule( 'calypso:reader:start' ); //eslint-disable-line no-unused-vars
 
@@ -16,20 +15,25 @@ const ReaderStart = React.createClass( {
 		return (
 			<Main className="reader-start">
 				<header>
-					<h2>{ this.translate( 'Add sites to my Reader' ) }</h2>
+					<h2>{ this.translate( 'Welcome to the Reader' ) }</h2>
 					<Button disabled primary>{ this.translate( "OK, I'm all set!" ) }</Button>
 				</header>
-				<p>{ this.translate( 'Follow these great new reads!' ) }</p>
+				<p>{ this.translate( "Discover great stories and read your favourite sites' posts all in one place." ) }&nbsp;
+				{ this.translate( "Every time there are new updates to the sites you follow, you'll be the first to know!" ) }
+				</p>
 
-				<PostCard />
-				<SiteCard />
+				<div className="reader-start__cards">
+					<ReaderStartCard />
+					<ReaderStartCard />
+					<ReaderStartCard />
+				</div>
 			</Main>
 		);
 	}
 } );
 
 export default connect(
-	( state, ownProps ) => {
+	( state ) => {
 		return {
 			recommendations: {} //getColdStartRecommendations( state )
 		};
