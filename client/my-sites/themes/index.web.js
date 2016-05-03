f448723f1b60060d0e14a6d991205b00e6c41013
Bernhard Reiter <ockham@raz.or.at>
2016-05-04 01:07:07 +0200

    Themes: Remove obsolete comment

--- client/my-sites/themes/index.web.js
+++ client/my-sites/themes/index.web.js
@@ -7,14 +7,6 @@ import { makeLayout } from 'controller';
 import { navigation, siteSelection } from 'my-sites/controller';
 import { singleSite, multiSite, loggedOut } from './controller';
 
-// FIXME: These routes will SSR the logged-out Layout even if logged-in.
-// While subsequently replaced by the logged-in Layout on the client-side,
-// we'll want to render it on the server, too.
-
-// `logged-out` middleware isn't SSR-compliant yet, but we can at least render
-// the layout.
-// FIXME: Also create loggedOut/multiSite/singleSite elements, depending on route.
-
 export default function( router ) {
 	const user = userFactory();
 	const isLoggedIn = !! user.get();
