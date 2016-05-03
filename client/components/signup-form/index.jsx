ffabceab56648ed0e204cff824fb9721a8c3e030
Biser Perchinkov <biser@bisko.be>
2016-05-03 20:59:56 +0300

    [WIP] Query the API to get a valid username suggestion.
    When a username is generated from the domain name, sometimes that username is already taken. A check must be done with the API to guarantee that the username is available.

--- client/components/signup-form/index.jsx
+++ client/components/signup-form/index.jsx
@@ -75,6 +75,7 @@ export default React.createClass( {
 		// Fetch the suggested username from local storage
 		const suggestedUsername = this.context.store.getState().signup.optionalDependencies.suggestedUsername;
 
+
 		return mergeFormWithValue( {
 			form,
 			fieldName: 'username',
@@ -128,6 +129,70 @@ export default React.createClass( {
 				username: sanitizedUsername
 			} );
 		}
+	},
+
+	/**
+	 * Gets username suggestions from the API.
+	 *
+	 * Ask the API to validate a username.
+	 *
+	 * If the API returns a suggestion, then the username is already taken.
+	 * If there is no error from the API, then the username is free.
+	 *
+	 * @param {string} username The username to get suggestions for.
+	 */
+	getUsernameSuggestion( username ) {
+
+		let fields = {
+			givesuggestions: 1,
+			username: username
+		};
+
+		wpcom.undocumented().validateNewUser( fields, ( error, response ) => {
+			if ( this.state.readyToLogin || this.props.submitting ) {
+				// this is a stale callback, we have already signed up or are logging in
+				return;
+			}
+
+			if ( error || !response ) {
+				return debug( error || 'User validation failed.' );
+			}
+
+			let resulting_username = null;
+
+			/**
+			 * Only start checking for suggested username if the API returns an error for the validation.
+			 */
+			if ( !response.success ) {
+
+				let { messages } = response;
+
+				/**
+				 * The only case we want to update username field is when the username is already taken.
+				 *
+				 * This ensures that the validation is done
+				 *
+				 * Check for:
+				 * 	- username taken error -
+				 * 	- a valid suggested username
+				 */
+				if ( messages.username && messages.username.taken && messages.suggested_username ) {
+
+					resulting_username = messages.suggested_username.suggested_username;
+
+				}
+			}
+
+			/**
+			 * If there is a valid suggested username from the server - use it.
+			 * Otherwise fall back to the default behavior and use the username provided.
+			 */
+			this.formStateController.handleFieldChange( {
+				name: 'username',
+				value: resulting_username || username
+			} );
+
+		} );
 	},
 
 	validate( fields, onComplete ) {
