f87a256e28c5e9bd34a6f2f188930e21c54fbb0c
bluefuton <chris@bluefuton.com>
2016-05-06 15:17:57 +0100

    Reader Start: add components and style cards
    - Add done bar to bottom
    - Fix flexbox
    - Add dummy hero image

--- client/reader/start/main.jsx
+++ client/reader/start/main.jsx
@@ -1,20 +1,43 @@
 // External dependencies
 import React from 'react';
+import debugModule from 'debug';
 import { connect } from 'react-redux';
 
 // Internal dependencies
 import Main from 'components/main';
 import { getRecommendations } from 'state/reader/start/selectors';
 import QueryReaderStartRecommendations from 'components/data/query-reader-start-recommendations';
+import Button from 'components/button';
+import StartCard from './card';
+import RootChild from 'components/root-child';
+
+const debug = debugModule( 'calypso:reader:start' ); //eslint-disable-line no-unused-vars
 
 const Start = React.createClass( {
 	render() {
 		return (
 			<Main className="reader-start">
 				<QueryReaderStartRecommendations />
-				<header>
-					<h2>{ this.translate( 'Welcome to the Reader' ) }</h2>
+				<header className="reader-start__intro">
+					<h1>{ this.translate( 'Welcome to the Reader' ) }</h1>
+					<p>{ this.translate( "Discover great stories and read your favorite sites' posts all in one place. Every time there are new updates to the sites you follow, you'll be the first to know!" ) }
+				</p>
 				</header>
+
+				<div className="reader-start__cards">
+					<StartCard />
+					<StartCard />
+					<StartCard />
+					<StartCard />
+					<StartCard />
+				</div>
+
+				<RootChild className="reader-start-footer">
+					<div className="reader-start-footer__action main">
+						<span>Follow one or more sites to get started</span>
+						<Button disabled>{ this.translate( "OK, I'm all set!" ) }</Button>
+					</div>
+				</RootChild>
 			</Main>
 		);
 	}
