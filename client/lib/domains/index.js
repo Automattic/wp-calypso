35a46746e149cd28bd2f76e8302aa9c7e2787f20
Igor Klimer <klimeryk@users.noreply.github.com>
2016-05-10 22:58:12 +0200

    Domains: Improve error handling for searched mapped domains
    Fixes #859

--- client/lib/domains/index.js
+++ client/lib/domains/index.js
@@ -41,10 +41,12 @@ function canRegister( domainName, onComplete ) {
 		var errorCode;
 		if ( serverError ) {
 			errorCode = serverError.error;
-		} else if ( ! data.is_registrable ) {
-			errorCode = 'not_registrable';
 		} else if ( ! data.is_available && data.is_mappable ) {
 			errorCode = 'not_available_but_mappable';
+		} else if ( ! data.is_mappable && data.unmappability_reason ) {
+			errorCode = `mappable_but_${data.unmappability_reason}`;
+		} else if ( ! data.is_registrable ) {
+			errorCode = 'not_registrable';
 		} else if ( ! data.is_available ) {
 			errorCode = 'not_available';
 		}
