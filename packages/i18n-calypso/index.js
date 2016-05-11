9bb2f161e17c324a58c09f353caa0ca180bb89ca
Tugdual de Kerviler <dekervit@gmail.com>
2016-05-11 13:59:05 +0200

    First version, extracted from wp-calypso i18n mixin

---
+++ packages/i18n-calypso/index.js
@@ -0,0 +1,26 @@
+/**
+ * Internal dependencies
+ */
+var i18n = require( './lib' ),
+	mixin = require( './lib/mixin' ),
+	localize = require( './lib/localize' );
+
+module.exports = {
+	moment: i18n.moment,
+	numberFormat: i18n.numberFormat.bind( i18n ),
+	translate: i18n.translate.bind( i18n ),
+	configure: i18n.configure.bind( i18n ),
+	initialize: i18n.initialize.bind( i18n ),
+	setLocale: i18n.setLocale.bind( i18n ),
+	getLocale: i18n.getLocale.bind( i18n ),
+	getLocaleSlug: i18n.getLocaleSlug.bind( i18n ),
+	reRenderTranslations: i18n.reRenderTranslations.bind( i18n ),
+	registerComponentUpdateHook: i18n.registerComponentUpdateHook.bind( i18n ),
+	registerTranslateHook: i18n.registerTranslateHook.bind( i18n ),
+	mixin: mixin,
+	localize: localize,
+	state: i18n.state,
+	stateObserver: i18n.stateObserver,
+	I18N: i18n.I18N
+};
+
---
+++ packages/i18n-calypso/lib/mixin/index.js
@@ -0,0 +1,42 @@
+var i18n = require( '..' ),
+	moment = require( 'moment-timezone' ),
+	tzDetect = require( './timezone' );
+
+var i18nStateObserver = i18n.stateObserver,
+	componentUpdateHooks = i18n.componentUpdateHooks;
+
+module.exports = {
+	moment: moment,
+
+	numberFormat: i18n.numberFormat.bind( i18n ),
+
+	translate: i18n.translate.bind( i18n ),
+
+	componentWillMount: function() {
+		moment.tz.setDefault( tzDetect() );
+	},
+
+	componentDidMount: function() {
+		i18nStateObserver.on( 'change', this.updateTranslation );
+		componentUpdateHooks.forEach( function( hook ) {
+			hook();
+		} );
+	},
+
+	componentDidUpdate: function() {
+		componentUpdateHooks.forEach( function( hook ) {
+			hook();
+		} );
+	},
+
+	componentWillUnmount: function() {
+		i18nStateObserver.off( 'change', this.updateTranslation );
+	},
+
+	updateTranslation: function() {
+		debug( 'Re-rendering ' + this.constructor.displayName + ' component.' );
+		if ( this.isMounted() ) {
+			this.forceUpdate();
+		}
+	}
+};
