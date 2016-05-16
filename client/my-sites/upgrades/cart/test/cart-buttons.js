fafc88f0acfb6ad325aa5d316553bea80ec8127d
Bernhard Reiter <ockham@raz.or.at>
2016-05-16 17:22:17 +0200

    Upgrades: Use enzyme for CartButtons test
    Using the sinon sandbox to ensure proper cleanup after stubbing (props @gwwar).

--- client/my-sites/upgrades/cart/test/cart-buttons.js
+++ client/my-sites/upgrades/cart/test/cart-buttons.js
@@ -1,65 +1,77 @@
 /**
  * External Dependencies
  */
-const sinon = require( 'sinon' ),
-	ReactDom = require( 'react-dom' ),
-	React = require( 'react' ),
-	TestUtils = require( 'react-addons-test-utils' ),
-	{ expect } = require( 'chai' );
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
 
 /**
  * Internal dependencies
  */
 import useFakeDom from 'test/helpers/use-fake-dom';
+import useMockery from 'test/helpers/use-mockery';
+import { useSandbox } from 'test/helpers/use-sinon';
 
 describe( 'cart-buttons', function() {
+	let recordStub, onKeepSearchingClickStub, CartButtons;
+
+	useSandbox( ( sandbox ) => {
+		recordStub = sandbox.stub();
+		onKeepSearchingClickStub = sandbox.stub();
+	} );
+
+	const AnalyticsMixinStub = () => ( {
+		recordEvent: recordStub
+	} );
+
 	useFakeDom();
 
+	useMockery( mockery => {
+		mockery.registerMock( 'lib/mixins/analytics', AnalyticsMixinStub );
+		CartButtons = require( '../cart-buttons.jsx' );
+	} );
+
 	beforeEach( function() {
-		this.CartButtons = require( '../cart-buttons.jsx' );
-		this.CartButtons.prototype.translate = sinon.stub();
-		this.recordStub = this.CartButtons.prototype.__reactAutoBindMap.recordEvent = sinon.stub();
+		CartButtons.prototype.translate = sinon.stub();
 	} );
 
 	afterEach( function() {
-		delete this.CartButtons.prototype.translate;
-		delete this.CartButtons.prototype.__reactAutoBindMap.recondEvent;
+		delete CartButtons.prototype.translate;
 	} );
 
 	describe( 'Click on Keep Searching Button', function() {
 		beforeEach( function() {
-			this.onKeepSearchingClick = sinon.stub();
-			this.cartButtonsComponent = TestUtils.renderIntoDocument(
-				<this.CartButtons
+			this.cartButtonsComponent = mount(
+				<CartButtons
 					selectedSite={ {slug: 'example.com'} }
 					showKeepSearching={ true }
-					onKeepSearchingClick={ this.onKeepSearchingClick }
+					onKeepSearchingClick={ onKeepSearchingClickStub }
 					/>
 			);
 		} );
 
 		it( 'should track "keepSearchButtonClick" event', function() {
-			TestUtils.Simulate.click( ReactDom.findDOMNode( this.cartButtonsComponent.refs.keepSearchingButton ) );
-			expect( this.recordStub ).to.have.been.calledWith( 'keepSearchButtonClick' );
+			this.cartButtonsComponent.find( '.cart-keep-searching-button' ).simulate( 'click' );
+			expect( recordStub ).to.have.been.calledWith( 'keepSearchButtonClick' );
 		} );
 
 		it( 'call props.onKeepSearchingClick', function() {
-			TestUtils.Simulate.click( ReactDom.findDOMNode( this.cartButtonsComponent.refs.keepSearchingButton ) );
-			expect( this.onKeepSearchingClick ).to.have.been.called;
+			this.cartButtonsComponent.find( '.cart-keep-searching-button' ).simulate( 'click' );
+			expect( onKeepSearchingClickStub ).to.have.been.called;
 		} );
 	} );
 	describe( 'Click on Checkout Button', function() {
 		beforeEach( function() {
-			this.cartButtonsComponent = TestUtils.renderIntoDocument(
-				<this.CartButtons
+			this.cartButtonsComponent = mount(
+				<CartButtons
 					selectedSite={ {slug: 'example.com'} }
 					/>
 			);
 		} );
 
 		it( 'should track "checkoutButtonClick" event', function() {
-			TestUtils.Simulate.click( ReactDom.findDOMNode( this.cartButtonsComponent.refs.checkoutButton ) );
-			expect( this.recordStub ).to.have.been.calledWith( 'checkoutButtonClick' );
+			this.cartButtonsComponent.find( '.cart-checkout-button' ).simulate( 'click' );
+			expect( recordStub ).to.have.been.calledWith( 'checkoutButtonClick' );
 		} );
 	} );
 } );
