c7bf732a9e8f16ebe2d630dff7992c428a6b356a
Dave Furfero <furf@furf.com>
2016-05-16 15:46:22 -0700

    replace click-outside with react-click-outside in Popover component

--- client/components/popover/index.jsx
+++ client/components/popover/index.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import clickOutside from 'click-outside';
+import withClickOutside from 'react-click-outside';
 import defer from 'lodash/defer';
 import ReactDom from 'react-dom';
 import React from 'react';
@@ -15,7 +15,8 @@ import { Provider as ReduxProvider } from 'react-redux';
 import closeOnEsc from 'lib/mixins/close-on-esc';
 import warn from 'lib/warn';
 
-var Content = React.createClass( {
+// Create <Content> component and extend with "click outside" behavior.
+var Content = withClickOutside( React.createClass( {
 	mixins: [ closeOnEsc( '_close' ) ],
 
 	render: function() {
@@ -24,10 +25,14 @@ var Content = React.createClass( {
 		);
 	},
 
+	handleClickOutside( event ) {
+		this.props.onClickOutside( event );
+  },
+
 	_close: function() {
 		this.props.onClose();
 	}
-} );
+} ) );
 
 var Popover = React.createClass( {
 	propTypes: {
@@ -57,11 +62,6 @@ var Popover = React.createClass( {
 	},
 
 	componentWillUnmount: function() {
-		clearTimeout( this._clickOutsideTimeout );
-		if ( this._unbindClickOutside ) {
-			this._unbindClickOutside();
-			this._unbindClickOutside = null;
-		}
 		this._tip.remove();
 		ReactDom.unmountComponentAtNode( this._container );
 		this._container = null;
@@ -71,89 +71,101 @@ var Popover = React.createClass( {
 		return null;
 	},
 
-	_showOrHideTip: function( prevProps ) {
-		if ( ! this._tip ) {
-			this._tip = new Tip( '' );
-			this._tip.classname = this.props.className;
-			this._container = this._tip.inner;
+	_initTip: function() {
+		this._tip = new Tip( '' );
+		this._tip.classname = this.props.className;
+		this._container = this._tip.inner;
+	},
+
+	_renderTip: function() {
+		let content = <Content { ...omit( this.props, 'className' ) } onClickOutside={ this._onClickOutside } onClose={ this._close } />;
+
+		// Context is lost when creating a new render hierarchy, so ensure
+		// that we preserve the context that we care about
+		if ( this.context.store ) {
+			content = (
+				<ReduxProvider store={ this.context.store }>
+					{ content }
+				</ReduxProvider>
+			);
 		}
 
-		if ( this.props.isVisible && this.props.context ) {
-			let content = <Content { ...omit( this.props, 'className' ) } onClose={ this._close } />;
+		// this schedules a render, but does not actually render the content
+		ReactDom.render( content, this._container );
+	},
 
-			// Context is lost when creating a new render hierarchy, so ensure
-			// that we preserve the context that we care about
-			if ( this.context.store ) {
-				content = (
-					<ReduxProvider store={ this.context.store }>
-						{ content }
-					</ReduxProvider>
+	_showTip: function() {
+		// defer showing the content to give it a chance to render
+		defer( () => {
+			const contextNode = ReactDom.findDOMNode( this.props.context );
+			if ( contextNode.nodeType !== Node.ELEMENT_NODE || contextNode.nodeName.toLowerCase() === 'svg' ) {
+				warn(
+					'Popover is attached to a %s element (nodeType %d).  ' +
+					'This causes problems in IE11 - see 12168-gh-calypso-pre-oss.',
+					contextNode.nodeName,
+					contextNode.nodeType
 				);
 			}
 
-			// this schedules a render, but does not actually render the content
-			ReactDom.render( content, this._container );
+			this._tip.position( this.props.position, { auto: true } );
+			this._tip.show( contextNode );
 
+			if ( this.props.onShow ) {
+				this.props.onShow();
+			}
+		} );
+	},
+
+	_hideTip: function() {
+		ReactDom.unmountComponentAtNode( this._container );
+		this._tip.hide();
+	},
+
+	_showOrHideTip: function( prevProps ) {
+		if ( ! this._tip ) {
+			this._initTip();
+		}
+		if ( this.props.isVisible && this.props.context ) {
+			this._renderTip();
 			if ( ! prevProps.isVisible ) {
-				// defer showing the content to give it a chance to render
-				defer( () => {
-					const contextNode = ReactDom.findDOMNode( this.props.context );
-					if ( contextNode.nodeType !== Node.ELEMENT_NODE || contextNode.nodeName.toLowerCase() === 'svg' ) {
-						warn(
-							'Popover is attached to a %s element (nodeType %d).  ' +
-							'This causes problems in IE11 - see 12168-gh-calypso-pre-oss.',
-							contextNode.nodeName,
-							contextNode.nodeType
-						);
-					}
-
-					this._tip.position( this.props.position, { auto: true } );
-					this._tip.show( contextNode );
-
-					if ( this.props.onShow ) {
-						this.props.onShow();
-					}
-				} );
-
-				this._setupClickOutside();
+				this._showTip();
 			}
 		} else {
-			ReactDom.unmountComponentAtNode( this._container );
-			this._tip.hide();
-
-			if ( this._unbindClickOutside ) {
-				this._unbindClickOutside();
-				this._unbindClickOutside = null;
-			}
+			this._hideTip();
 		}
 	},
 
-	_setupClickOutside: function() {
-		if ( this._unbindClickOutside ) {
-			this._unbindClickOutside();
+	_onClickOutside: function( event ) {
+		let shouldClose = true;
+
+		if ( this.props.ignoreContext ) {
+			const ignoreContext = ReactDom.findDOMNode( this.props.ignoreContext );
+			shouldClose = ignoreContext && ignoreContext.contains && ! ignoreContext.contains( event.target );
 		}
 
-		// have to setup clickOutside after a short delay, otherwise it counts the current
-		// click to show the tip and the tip will never be shown
-		this._clickOutsideTimeout = setTimeout( function() {
-			this._unbindClickOutside = clickOutside( this._container, function( event ) {
-				const contextNode = ReactDom.findDOMNode( this.props.context );
-				let shouldClose = ( contextNode && contextNode.contains && ! contextNode.contains( event.target ) );
-
-				if ( this.props.ignoreContext && shouldClose ) {
-					const ignoreContext = ReactDom.findDOMNode( this.props.ignoreContext );
-					shouldClose = shouldClose && ( ignoreContext && ignoreContext.contains && ! ignoreContext.contains( event.target ) );
-				}
-
-				if ( shouldClose ) {
-					this._close( event );
-				}
-			}.bind( this ) );
-		}.bind( this ), 10 );
+		if ( shouldClose ) {
+			// Defer execution of `_close` to prevent "double event" when Popover's
+			// content is visible AND clicking outside of its content AND on its
+			// toggle/open element.
+			// Problem: With synchronous execution, react-click-outside will register
+			// a `click` event on the <Content> element before it registers on the
+			// containing <Popover> element. This bubbling click event will allow
+			// `_close` to be called moments before `_showOrHideTip` which will now
+			// see the component as closed and reopen it.
+			// Solution: By removing the call to `_close` from the call stack with
+			// `defer`, the toggle inside of `_showOrHideTip` behaves as expected
+			// and when the duplicative call is executed, it can be ignored with a
+			// simple check of `this.props.isVisible`.
+			defer( () => this._close( event ) );
+		}
 	},
 
 	_close: function( event ) {
-		this.props.onClose( event );
+		// This check prevents a duplicate call to `this.props.onClose` when the
+		// popover is closed using the toggle.
+		if ( this.props.isVisible ) {
+			this.props.onClose( event );
+		}
 	}
 } );
 
