1f847a0cfdc4ae17a1ece6670d49a4782330d6df
artpi <artpi@post.pl>
2016-05-17 11:20:09 +0200

    Proper actions and reducers

--- client/state/preferences/actions.js
+++ client/state/preferences/actions.js
@@ -2,43 +2,49 @@
  * Internal dependencies
  */
 import wpcom from 'lib/wp';
-import {} from 'state/action-types';
+import {
+	PREFERENCES_SET,
+	PREFERENCES_RECEIVE,
+	PREFERENCES_REMOVE
+} from 'state/action-types';
+
+export const USER_SETTING_KEY = 'calypso_preferences';
 
 export function fetch() {
 	return ( dispatch ) => {
 		return wpcom.me().settings().get().then( ( data ) => {
 			dispatch( {
-				type: 'PREFERENCES_RECEIVE',
+				type: PREFERENCES_RECEIVE,
 				data
 			} );
 		} );
 	};
 }
 
-export function set( key, value ) {
-	return {
-		type: 'PREFERENCES_SET',
+const save = () => ( dispatch, getState ) => {
+	const settings = {};
+	settings[ USER_SETTING_KEY ] = getState().preferences.values;
+	wpcom.me().settings().update( JSON.stringify( settings ) ).then( ( data ) => {
+		dispatch( {
+			type: PREFERENCES_RECEIVE,
+			data
+		} );
+	} );
+};
+
+export const set = ( key, value ) => ( dispatch, getState ) => {
+	dispatch( {
+		type: PREFERENCES_SET,
 		key,
 		value
-	};
-}
+	} );
+	save()( dispatch, getState );
+};
 
-export function remove( key ) {
-	return {
-		type: 'PREFERENCES_REMOVE',
+export const remove = ( key ) => ( dispatch, getState ) => {
+	dispatch( {
+		type: PREFERENCES_REMOVE,
 		key
-	};
-}
-
-export function save() {
-	return ( dispatch, getState ) => {
-		const settings = {};
-		settings[ 'calypso_preferences' ] = getState().preferences.values;
-		wpcom.me().settings().update( JSON.stringify( settings ) ).then( ( data ) => {
-			dispatch( {
-				type: 'PREFERENCES_RECEIVE',
-				data
-			} );
-		} );
-	};
-}
+	} );
+	save()( dispatch, getState );
+};
--- client/state/preferences/reducer.js
+++ client/state/preferences/reducer.js
@@ -5,21 +5,28 @@ import { combineReducers } from 'redux';
 import has from 'lodash/has';
 import get from 'lodash/get';
 import forOwn from 'lodash/forOwn';
+/**
+ * Internal dependencies
+ */
+import {
+	PREFERENCES_SET,
+	PREFERENCES_RECEIVE,
+	PREFERENCES_REMOVE
+} from 'state/action-types';
+import { USER_SETTING_KEY } from './actions';
 
-const USER_SETTING_KEY = 'calypso_preferences';
 function values( state = {}, action ) {
-	if ( action.type === 'PREFERENCES_RECEIVE' && has( action, [ 'data', USER_SETTING_KEY ] ) ) {
+	if ( action.type === PREFERENCES_RECEIVE && has( action, [ 'data', USER_SETTING_KEY ] ) ) {
 		state = Object.assign( {}, state, get( action, [ 'data', USER_SETTING_KEY ] ) );
-
 		forOwn( state, ( value, key ) => {
 			if ( null === value ) {
 				delete state[ key ];
 			}
 		} );
-	} else if ( action.type === 'PREFERENCES_SET' && action.key && action.value ) {
+	} else if ( action.type === PREFERENCES_SET && action.key && action.value ) {
 		state = Object.assign( {}, state );
 		state[ action.key ] = action.value;
-	} else if ( action.type === 'PREFERENCES_REMOVE' && action.key ) {
+	} else if ( action.type === PREFERENCES_REMOVE && action.key ) {
 		state = Object.assign( {}, state );
 		state[ action.key ] = null;
 	}
