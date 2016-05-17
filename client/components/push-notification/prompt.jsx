d5fc691e2fcba726b75613028c0e35aa3bb2b490
Jeff Bowen <jeff@automattic.com>
2016-05-17 15:26:42 -0400

    Use the redux state tree for Browser push notifications Most of the code has been refactored into action creators, selector functions, & a reducer in `client/state/push-notifications`. The reducer is separated into two functions: `settings`, `system`
    As a result of the above, the `push-notifications` directory was otherwise vacant, so this moves the service worker file to `/client/service-worker.js` (It's still served from `/service-worker.js`)
    ...and service worker functions have been moved to their own library: `lib/service-worker`
    
    The `PushNotificationSettings` & `PushNotificationPrompt` components are updated to utilize the store via 'react-redux.connect`.

--- client/components/push-notification/prompt.jsx
+++ client/components/push-notification/prompt.jsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import store from 'store';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
@@ -10,6 +10,19 @@ import store from 'store';
 import Notice from 'components/notice';
 import Button from 'components/button';
 import observe from 'lib/mixins/data-observe';
+import {
+	dismissNotice,
+	toggleEnabled,
+} from 'state/push-notifications/actions';
+import {
+	getStatus,
+	isApiReady,
+	isAuthorizationLoaded,
+	isAuthorized,
+	isBlocked,
+	isEnabled,
+	isNoticeDismissed
+} from 'state/push-notifications/selectors';
 
 const SECTION_NAME_WHITELIST = [
 	'discover',
@@ -30,10 +43,10 @@ const SECTION_NAME_WHITELIST = [
 	'upgrades'
 ];
 
-export default React.createClass( {
+const PushNotificationPrompt = React.createClass( {
 	displayName: 'PushNotificationPrompt',
 
-	mixins: [ observe( 'pushNotifications', 'user' ) ],
+	mixins: [ observe( 'user' ) ],
 
 	propTypes: {
 		user: React.PropTypes.object,
@@ -41,31 +54,11 @@ export default React.createClass( {
 			React.PropTypes.object,
 			React.PropTypes.bool
 		] ),
-		isLoading: React.PropTypes.bool,
-		pushNotifications: React.PropTypes.object
-	},
-
-	getInitialState: function() {
-		return {
-			dismissed: store.get( 'push-notification-notice-dismissed' ),
-			subscribed: false
-		};
-	},
-
-	subscribe: function() {
-		this.props.pushNotifications.subscribe( ( state ) => {
-			if ( 'subscribed' === state ) {
-				this.setState( { subscribed: true } );
-			}
-		} );
-	},
-
-	dismissNotice: function() {
-		store.set( 'push-notification-notice-dismissed', true );
-		this.setState( { dismissed: true } );
+		isLoading: React.PropTypes.bool
 	},
 
 	pushUnsubscribedNotice: function() {
+		const buttonDisabled = ( [ 'disabling', 'enabling', 'unknown' ].includes( this.props.status ) );
 		const noticeText = (
 			<div>
 				<p>
@@ -75,21 +68,29 @@ export default React.createClass( {
 					{ this.translate(
 						'{{enableButton}}Enable Browser Notifications{{/enableButton}}', {
 							components: {
-								enableButton: <Button className={ 'push-notification__prompt-enable' } onClick={ this.subscribe } />
+								enableButton: <Button className={ 'push-notification__prompt-enable' } disabled={ buttonDisabled } onClick={ this.props.toggleEnabled } />
 							} }
 					) }
 				</p>
 			</div>
 		);
 
-		return <Notice className="push-notification__notice" text={ noticeText } icon="bell" onDismissClick={ this.dismissNotice } />;
+		return <Notice className="push-notification__notice" text={ noticeText } icon="bell" onDismissClick={ this.props.dismissNotice } />;
 	},
 
 	render: function() {
-		const pushNotifications = this.props.pushNotifications,
-			user = this.props.user.get();
+		if (
+			! this.props.isAuthorizationLoaded ||
+			! this.props.isApiReady ||
+			this.props.isBlocked ||
+			this.props.isNoticeDismissed ||
+			( this.props.isEnabled && this.props.isAuthorized )
+		) {
+			return null;
+		}
+		const user = this.props.user.get();
 
-		if ( ! user || ! user.email_verified || this.state.dismissed || this.state.subscribed ) {
+		if ( ! user || ! user.email_verified ) {
 			return null;
 		}
 
@@ -97,15 +98,24 @@ export default React.createClass( {
 			return null;
 		}
 
-		switch ( pushNotifications.state ) {
-			case 'unknown':
-			case 'subscribed':
-			case 'denied':
-				return null;
-			case 'unsubscribed':
-				return this.pushUnsubscribedNotice();
-			default:
-				return null;
-		}
+		return this.pushUnsubscribedNotice();
 	}
 } );
+
+export default connect(
+	( state ) => {
+		return {
+			isApiReady: isApiReady( state ),
+			isAuthorizationLoaded: isAuthorizationLoaded( state ),
+			isAuthorized: isAuthorized( state ),
+			isBlocked: isBlocked( state ),
+			isEnabled: isEnabled( state ),
+			isNoticeDismissed: isNoticeDismissed( state ),
+			status: getStatus( state )
+		};
+	},
+	{
+		dismissNotice,
+		toggleEnabled
+	}
+)( PushNotificationPrompt );
--- client/state/index.js
+++ client/state/index.js
@@ -21,6 +21,7 @@ import preview from './preview/reducer';
 import posts from './posts/reducer';
 import plugins from './plugins/reducer';
 import postTypes from './post-types/reducer';
+import pushNotifications from './push-notifications/reducer';
 import reader from './reader/reducer';
 import receipts from './receipts/reducer';
 import sharing from './sharing/reducer';
@@ -54,6 +55,7 @@ export const reducer = combineReducers( {
 	preview,
 	posts,
 	postTypes,
+	pushNotifications,
 	reader,
 	receipts,
 	sharing,
