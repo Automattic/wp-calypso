00371d17bdd13e15330a6f4d5b7487aa0da1401a
Kelly Dwan <kelly.dwan@gmail.com>
2016-05-20 16:16:51 -0400

    Jetpack Plans: If we can auto-install plugins, show the new receipt details block, which links into the install flow.

--- client/my-sites/upgrades/checkout-thank-you/jetpack-plan-details.jsx
+++ client/my-sites/upgrades/checkout-thank-you/jetpack-plan-details.jsx
@@ -10,19 +10,34 @@ import i18n from 'lib/mixins/i18n';
 import PurchaseDetail from 'components/purchase-detail';
 import userFactory from 'lib/user';
 
+var config = require( 'config' );
+
 const user = userFactory();
 
-const JetpackPlanDetails = () => {
+var JetpackPlanDetails = ( { selectedSite } ) => {
+	const props = {
+		icon: 'cog',
+		title: i18n.translate( 'Set up your VaultPress and Akismet accounts' ),
+		description: i18n.translate(
+			'We emailed you at %(email)s with information for setting up Akismet and VaultPress on your site. Follow the instructions in the email to get started.',
+			{ args: { email: user.get().email } }
+		),
+	};
+
+	if ( config.isEnabled( 'manage/plugins/setup' ) ) {
+		props.title = null;
+		props.description = i18n.translate(
+			'We are about to install Akismet and VaultPress for your site, which will automatically protect your site from spam and data loss. If you have any questions along the way, we\'re here to help! You can also perform a manual installation by following {{a}}these instructions{{/a}}.',
+			{ components: {
+				a: <a target="_blank" href="https://en.support.wordpress.com/setting-up-premium-services/" />
+			} }
+		);
+		props.buttonText = i18n.translate( 'Set up your plan' );
+		props.href = `/plugins/setup/${selectedSite.slug}`;
+	}
+
 	return (
-		<PurchaseDetail
-			icon="cog"
-			title={ i18n.translate( 'Set up your VaultPress and Akismet accounts' ) }
-			description={
-				i18n.translate(
-					'We emailed you at %(email)s with information for setting up Akismet and VaultPress on your site. Follow the instructions in the email to get started.',
-					{ args: { email: user.get().email } }
-				)
-			} />
+		<PurchaseDetail { ...props }/>
 	);
 };
 
