68d5a13d5d8ec8d846d6026498825f73f7216a2e
Michele Bosi <hello@michelebosi.com>
2016-05-23 21:34:53 +0100

    CSS Hot Reload: added new cssBuildFailed and cssBuilding actions

--- client/state/application/actions.js
+++ client/state/application/actions.js
@@ -1,8 +1,20 @@
 /**
  * Internal dependencies
  */
-import { CONNECTION_LOST, CONNECTION_RESTORED } from 'state/action-types';
-import { warningNotice, successNotice, removeNotice } from 'state/notices/actions';
+import { 
+	CONNECTION_LOST, 
+	CONNECTION_RESTORED, 
+	CSS_BUILD_FAILED, 
+	CSS_BUILDING 
+} from 'state/action-types';
+
+import { 
+	warningNotice, 
+	successNotice, 
+	removeNotice, 
+	errorNotice, 
+	infoNotice 
+} from 'state/notices/actions';
 
 export function connectionLost( noticeText ) {
 	return ( dispatch ) => {
@@ -35,3 +47,36 @@ export function connectionRestored( noticeText ) {
 		dispatch( { type: CONNECTION_RESTORED } );
 	};
 }
+
+export function cssBuildFailed( noticeText ) {
+	return ( dispatch ) => {
+		dispatch( removeNotice( 'cssBuilding' ) );
+		dispatch( errorNotice(
+			noticeText
+			, {
+				showDismiss: true,
+				isPersistent: true,
+				id: 'cssBuildFailed',
+				duration: 5000
+			} )
+		);
+		dispatch( { type: CSS_BUILD_FAILED } );
+	};
+}
+
+export function cssBuilding( noticeText ) {
+	return ( dispatch ) => {
+		dispatch( removeNotice( 'cssBuildFailed' ) );
+		dispatch( infoNotice(
+			noticeText
+			, {
+				showDismiss: true,
+				isPersistent: true,
+				id: 'cssBuilding',
+				duration: 5000
+			} )
+		);
+		dispatch( { type: CSS_BUILDING } );
+	};
+}
+
