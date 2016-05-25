5550789b1010632cbdada36b12188e47090cd1e4
Michele Bosi <hello@michelebosi.com>
2016-05-25 22:22:07 +0100

    CSS Hot Reload: use md5-file to compute file hash

--- server/bundler/css-hot-reload.js
+++ server/bundler/css-hot-reload.js
@@ -4,11 +4,11 @@
 var os = require( 'os' ),
 	fs = require( 'fs' ),
 	path = require( 'path' ),
-	crypto = require( 'crypto' ),
 	spawn = require( 'child_process' ).spawn,
 	debug = require( 'debug' )( 'css-hot-reload' ),
 	chalk = require( 'chalk' ),
-	chokidar = require('chokidar');
+	chokidar = require('chokidar'),
+	md5File = require('md5-file');
 
 /**
  * Initialize server CSS hot-reloading logic
@@ -93,24 +93,7 @@ function setup( io ) {
 	}
 
 	/**
-	 * Computes 10 digits md5 hash of a file
 	 */
-	// Isn't there a common utils library that we can put this in?
-	function hashFile( path ) {
-		var data, hash,
-			md5 = crypto.createHash( 'md5' );
-		try {
-			data = fs.readFileSync( path );
-			md5.update( data );
-			hash = md5.digest( 'hex' );
-			hash = hash.slice( 0, 10 );
-		} catch ( e ) {
-			hash = new Date().getTime().toString();
-		}
-
-		return hash;
-	}
-
 	/**
 	 * Returns the list of changed CSS files.
 	 */
@@ -118,7 +101,7 @@ function setup( io ) {
 		var changedFiles = [],
 			hash, filePath;
 		for( filePath in publicCssFiles ) {
-			hash = hashFile( filePath );
+			hash = md5File.sync( filePath );
 			if ( hash !== publicCssFiles[ filePath ] ) {
 				changedFiles.push( path.basename( filePath ) );
 				publicCssFiles[ filePath ] = hash;
@@ -132,7 +115,7 @@ function setup( io ) {
 	fs.readdirSync( PUBLIC_DIR ).forEach( function( file ) {
 		if ( file.match( /^.*\.css$/ ) ) {
 			var fullPath = path.join( PUBLIC_DIR, file );
-			publicCssFiles[ fullPath ] = hashFile( fullPath );
+			publicCssFiles[ fullPath ] = md5File.sync( fullPath );
 		}
 	} );
 	
