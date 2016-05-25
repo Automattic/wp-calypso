0de8d2d4ace8e922d8784a4a3bd5de0e7a031080
Ben Lowery <ben.lowery@automattic.com>
2016-05-25 10:48:54 -0400

    Post Normalizer: Refactor rules into individual files
    This will make it easier to work on the normalizer, and makes each sync rule truly sync, which makes
    using them from action responders much simpler.

---
+++ client/lib/post-normalizer/rule-wait-for-images-to-load.js
@@ -0,0 +1,88 @@
+/**
+ * External Dependencies
+ */
+import filter from 'lodash/filter';
+import flow from 'lodash/flow';
+import forEach from 'lodash/forEach';
+import map from 'lodash/map';
+import pick from 'lodash/pick';
+import pull from 'lodash/pull';
+import uniq from 'lodash/uniq';
+
+/**
+ * Internal Dependencies
+ */
+
+function convertImageToObject( image ) {
+	return pick( image, [ 'src', 'naturalWidth', 'naturalHeight' ] );
+}
+
+function imageForURL( imageUrl ) {
+	var img = new Image();
+	img.src = imageUrl;
+	return img;
+}
+
+function promiseForImage( image ) {
+	if ( image.complete && image.naturalWidth > 0 ) {
+		return Promise.resolve();
+	}
+	return new Promise( ( resolve, reject ) => {
+		image.onload = () => resolve( image );
+		image.onerror = () => reject( image );
+	} );
+}
+
+export default function waitForImagesToLoad( post ) {
+	return new Promise( ( resolve ) => {
+		function acceptLoadedImages( images ) {
+			post.images = map( images, convertImageToObject );
+
+			post.content_images = filter( map( post.content_images, function( image ) {
+				return find( post.images, { src: image.src } );
+			} ), Boolean );
+
+			resolve( post );
+		}
+
+		let imagesToCheck = [];
+
+		if ( post.featured_image ) {
+			imagesToCheck.push( post.featured_image );
+		}
+
+		forEach( post.content_images, function( image ) {
+			imagesToCheck.push( image.src );
+		} );
+
+		if ( imagesToCheck.length === 0 ) {
+			resolve( post );
+			return;
+		}
+
+		// dedupe the set of images
+		imagesToCheck = uniq( imagesToCheck );
+
+		// convert to image objects to start the load process
+		let promises = map( imagesToCheck, flow( imageForURL, promiseForImage ) );
+
+		const imagesLoaded = [];
+
+		forEach( promises, promise => {
+			promise.then( image => {
+				// keep track of what loaded successfully
+				imagesLoaded.push( image );
+			} ).catch( () => {
+				// ignore what did not, but return the promise chain to success
+				return null;
+			} ).then( () => {
+				// check to see if all of the promises have settled
+				// if so, accept what loaded and resolve the main promise
+				promises = pull( promises, promise );
+				if ( promises.length === 0 ) {
+					acceptLoadedImages( imagesLoaded );
+				}
+			} );
+		} );
+	} );
+}
