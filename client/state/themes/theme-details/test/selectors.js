3d58f4a006b6d8627456f95b08b995eedf827335
Steve Seear <steve.seear@gmail.com>
2016-05-26 16:12:14 +0100

    Themes: Add price test for theme-details selector

--- client/state/themes/theme-details/test/selectors.js
+++ client/state/themes/theme-details/test/selectors.js
@@ -11,19 +11,43 @@ import { getThemeDetails } from '../selectors';
 
 describe( 'selectors', () => {
 	describe( '#getThemeDetails()', () => {
-		it( 'should return details for a theme given its ID', () => {
-			const details = getThemeDetails( {
-				themes: {
-					themeDetails: Map( {
-						mood: Map( {
-							name: 'Mood',
-							author: 'Automattic'
-						} )
-					} )
-				}
-			}, 'mood' );
 
-			expect( details ).to.eql( { name: 'Mood', author: 'Automattic' } );
+		const themes = {
+			themes: {
+				themeDetails: Map( {
+					mood: Map( {
+						name: 'Mood',
+						author: 'Automattic',
+						price: {
+							value: 65,
+							currency: 'USD',
+							display: '<abbr title=\"United States Dollars\">$</abbr>65'
+						},
+					} ),
+					twentysixteen: Map( {
+						name: 'Twenty Sixteen',
+						author: 'Automattic',
+						price: {
+							value: 0,
+							currency: 'USD',
+							display: 'Free'
+						},
+					} ),
+				} )
+			}
+		};
+
+		it( 'should return details for a theme given its ID', () => {
+			const details = getThemeDetails( themes, 'mood' );
+			expect( details ).to.eql( { name: 'Mood', author: 'Automattic', price: '$65' } );
+		} );
+
+		it( 'should format the price as plaintext', () => {
+			const mood = getThemeDetails( themes, 'mood' );
+			expect( mood.price ).to.eql( '$65' );
+
+			const twentysixteen = getThemeDetails( themes, 'twentysixteen' );
+			expect( twentysixteen.price ).to.eql( 'Free' );
 		} );
 	} );
 } );
