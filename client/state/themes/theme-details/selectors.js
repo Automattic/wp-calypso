5e5b04a0ee4bf7782779a67fb48dd4f33c85f6b6
Steve Seear <steve.seear@gmail.com>
2016-05-30 13:05:21 +0100

    Themes: Fix theme sheets for IE/Edge (#5606)
    * Themes: Fix theme sheets for IE/Edge
    * Themes: Add price test for theme-details selector
    
    Use of 'default' locale was causing an error in IE/Edge. Also,
    `style: 'currency'` not supported in most browsers, leading
    to a missing currency symbol.
    
    Remove the markup from the API result using a regex instead
    of `toLocaleString()`.

--- client/state/themes/theme-details/selectors.js
+++ client/state/themes/theme-details/selectors.js
@@ -12,14 +12,7 @@ export function getThemeDetails( state, id ) {
 // Convert price to format used by v1.2 themes API to fit with existing components.
 // TODO (seear): remove when v1.2 theme details endpoint is added
 function formatPrice( price ) {
-	if ( price.value === 0 ) {
-		// Free themes have plaintext string
-		return price.display;
-	}
-	// Premium themes have markup in price.display, so create our own plaintext
-	return price.value.toLocaleString( 'default', {
-		style: 'currency',
-		currency: price.currency,
-		minimumFractionDigits: 0,
-	} );
+	// premium theme price.display example: "<abbr title="United States Dollars">$</abbr>65"
+	const priceMatcher = /^<[^>]*>([^<]*)<[^>]*>(\d*)$/;
+	return price.display.replace( priceMatcher, '$1$2' );
 }
