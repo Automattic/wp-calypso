7ef64ddb6c222c7dd5a3c1f06b136dcd4f238a21
Jordan West <jordwest@users.noreply.github.com>
2016-06-01 20:44:03 +1000

    Exporter: More consistent loading placeholders (#5478)
    * Exporter: More consistent loading placeholders
    
    This replaces the loading placeholders for the Exporter's advanced settings
    with pulsing selects for a less jarring experience when the data loads.
    
    * FormSelect: Gray out text for all disabled FormSelects
    * FormSelect: Add disabled and is-error states to devdocs

--- client/my-sites/exporter/post-type-options.jsx
+++ client/my-sites/exporter/post-type-options.jsx
@@ -4,6 +4,8 @@
 import React, { PropTypes } from 'react';
 import PureRenderMixin from 'react-pure-render/mixin';
 import { connect } from 'react-redux';
+import { map } from 'lodash/collection';
+import { get } from 'lodash/object';
 
 /**
  * Internal dependencies
@@ -43,9 +45,9 @@ const mapDispatchToProps = ( dispatch, ownProps ) => ( {
 } );
 
 /**
- * Displays a list of select menus with a checkbox legend
+ * Displays a list of select menus with a radio option legend
  *
- * Displays a field group with a checkbox legend and optionally
+ * Displays a field group with a radio legend and optionally
  * a list of select menus, or a description to appear beneath the
  * legend.
  */
@@ -61,27 +63,26 @@ const PostTypeOptions = React.createClass( {
 		legend: PropTypes.string.isRequired,
 	},
 
-	renderPlaceholders() {
-		return (
-			<div className="exporter__option-fieldset-fields">
-				<div className="exporter__placeholder-text">
-					{ this.translate( 'Loading options…' ) }
-				</div>
-			</div>
-		);
-	},
-
 	renderFields() {
 		const {
 			fields,
-			postType,
-			siteId,
 			fieldValues,
+			postType,
+			shouldShowPlaceholders,
+			siteId,
 		} = this.props;
 
+		const fieldsForPostType = get( {
+			'post': [ 'author', 'status', 'start_date', 'end_date', 'category' ],
+			'page': [ 'author', 'status', 'start_date', 'end_date' ],
+		}, postType, [] );
+
 		const Field = ( props ) => {
-			if ( ! props.options ) {
-				// This should be replaced with `return null` in React >= 0.15
+			const options = get( fields, props.options, [] );
+
+			// Should the field be displayed for this post type?
+			if ( fieldsForPostType.indexOf( props.fieldName ) < 0 ) {
+				// This can be replaced with `return null` in React >= 0.15
 				return <span/>;
 			}
 
@@ -90,21 +91,22 @@ const PostTypeOptions = React.createClass( {
 			};
 
 			return <Select
+				className={ shouldShowPlaceholders ? 'exporter__placeholder-select' : '' }
 				onChange={ setFieldValue }
 				key={ props.defaultLabel }
 				defaultLabel={ props.defaultLabel }
-				options={ props.options }
+				options={ options }
 				value={ fieldValues[ props.fieldName ] }
-				disabled={ ! this.props.isEnabled } />;
+				disabled={ shouldShowPlaceholders || ! this.props.isEnabled } />;
 		};
 
 		return (
 			<div className="exporter__option-fieldset-fields">
-				<Field defaultLabel={ this.translate( 'Author…' ) } fieldName="author" options={ fields.authors } />
-				<Field defaultLabel={ this.translate( 'Status…' ) } fieldName="status" options={ fields.statuses } />
-				<Field defaultLabel={ this.translate( 'Start Date…' ) } fieldName="start_date" options={ fields.dates } />
-				<Field defaultLabel={ this.translate( 'End Date…' ) } fieldName="end_date" options={ fields.dates } />
-				<Field defaultLabel={ this.translate( 'Category…' ) } fieldName="category" options={ fields.categories } />
+				<Field defaultLabel={ this.translate( 'Author…' ) } fieldName="author" options="authors" />
+				<Field defaultLabel={ this.translate( 'Status…' ) } fieldName="status" options="statuses" />
+				<Field defaultLabel={ this.translate( 'Start Date…' ) } fieldName="start_date" options="dates" />
+				<Field defaultLabel={ this.translate( 'End Date…' ) } fieldName="end_date" options="dates" />
+				<Field defaultLabel={ this.translate( 'Category…' ) } fieldName="category" options="categories" />
 			</div>
 		);
 	},
@@ -115,7 +117,6 @@ const PostTypeOptions = React.createClass( {
 			onSelect,
 			legend,
 			description,
-			shouldShowPlaceholders
 		} = this.props;
 
 		return (
@@ -134,7 +135,7 @@ const PostTypeOptions = React.createClass( {
 					</p>
 				}
 
-				{ shouldShowPlaceholders ? this.renderPlaceholders() : this.renderFields() }
+				{ this.renderFields() }
 			</div>
 		);
 	}
