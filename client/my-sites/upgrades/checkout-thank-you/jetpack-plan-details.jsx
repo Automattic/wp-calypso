f3b0a5f52b74eb3a5aa15c01138906334864cae4
Kelly Dwan <kelly.dwan@gmail.com>
2016-06-01 17:05:50 -0400

    Jetpack Plans: Add a different message if we detect that we can’t install plugins on this site. Fixes #5729

--- client/my-sites/upgrades/checkout-thank-you/jetpack-plan-details.jsx
+++ client/my-sites/upgrades/checkout-thank-you/jetpack-plan-details.jsx
@@ -26,14 +26,20 @@ var JetpackPlanDetails = ( { selectedSite } ) => {
 
 	if ( config.isEnabled( 'manage/plugins/setup' ) ) {
 		props.title = null;
-		props.description = i18n.translate(
-			'We are about to install Akismet and VaultPress for your site, which will automatically protect your site from spam and data loss. If you have any questions along the way, we\'re here to help! You can also perform a manual installation by following {{a}}these instructions{{/a}}.',
-			{ components: {
-				a: <a target="_blank" href="https://en.support.wordpress.com/setting-up-premium-services/" />
-			} }
-		);
-		props.buttonText = i18n.translate( 'Set up your plan' );
-		props.href = `/plugins/setup/${selectedSite.slug}`;
+		if ( ! selectedSite.canUpdateFiles ) {
+			props.description = i18n.translate( 'You can now install Akismet and VaultPress, which will automatically protect your site from spam and data loss. If you have any questions along the way, we\'re here to help!' );
+			props.buttonText = i18n.translate( 'Installation Instructions' );
+			props.href = 'https://en.support.wordpress.com/setting-up-premium-services/';
+		} else {
+			props.description = i18n.translate(
+				'We are about to install Akismet and VaultPress for your site, which will automatically protect your site from spam and data loss. If you have any questions along the way, we\'re here to help! You can also perform a manual installation by following {{a}}these instructions{{/a}}.',
+				{ components: {
+					a: <a target="_blank" href="https://en.support.wordpress.com/setting-up-premium-services/" />
+				} }
+			);
+			props.buttonText = i18n.translate( 'Set up your plan' );
+			props.href = `/plugins/setup/${selectedSite.slug}`;
+		}
 	}
 
 	return (
