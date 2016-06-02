a1f107d108e756643194fd3b1134c194f1d964c1
retrofox <rdsuarez@gmail.com>
2016-06-02 19:53:08 +0200

    state.sites.vouchers: get assign voucher functions

--- client/state/sites/vouchers/selectors.js
+++ client/state/sites/vouchers/selectors.js
@@ -1,4 +1,10 @@
 /**
+ * Internal dependencies
+ */
+
+import { GOOGLE_AD_CREDITS } from './service-types';
+
+/**
  * Return site vouchers getting from state object and
  * the given siteId
  *
@@ -34,6 +40,19 @@ export const getVouchersBySiteIdAndServiceType = ( state, siteId, serviceType ) 
 };
 
 /**
+ * Return google-ad-credits vouchers
+ *
+ * @param {Object} state - current state object
+ * @param {Object} site - site object
+ * @return {Array} site vouchers
+ */
+
+export const getGoogleAdCredits = ( state, site ) => {
+	const vouchers = getVouchersBySite( state, site );
+	return vouchers[ GOOGLE_AD_CREDITS ] || [];
+};
+
+/**
  * Return requesting state for the given site
  *
  * @param {Object} state - current state object
@@ -42,4 +61,18 @@ export const getVouchersBySiteIdAndServiceType = ( state, siteId, serviceType ) 
  */
 export const isRequestingSiteVouchers = ( state, siteId ) => {
 	return state.sites.vouchers.requesting[ siteId ] || false;
+};
+
+/**
+ * Return assign requesting state for the given site
+ * and serviceType
+ *
+ * @param {Object} state - current state object
+ * @param {Number} siteId - site identifier
+ * @param {String} serviceType - service type
+ * @return {Boolean} true if a voucher is being assigned
+ */
+export const isAssigningSiteVoucher = ( state, siteId, serviceType ) => {
+	const requesting = state.sites.vouchers.requesting[ siteId ];
+	return requesting ? requesting[ serviceType ] : false;
 };
