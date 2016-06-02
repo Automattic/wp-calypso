91313d7d2849faffd4f3476620d7642904284e0f
Paul Sealock <psealock@gmail.com>
2016-06-03 08:46:22 +1200

    Reader: Use embed-container to render Slideshow gallery (#5368)
    * Reader: Use EmbedContainer to render Slideshow gallery
    
    parse content and apply GalleryView when needed
    
    fix lint warnings
    
    Use embed-container to parse DOM and call wp.com scripts
    
    remove unused import
    
    Reader: use shortcodes to render Slideshow gallery
    
    parse content and apply GalleryView when needed
    
    revert merge conflict errors
    
    check if parentNode exists, before removing node
    
    remove no-js remove block
    
    Only load Slideshow dependencies when needed
    
    use element.classList
    
    tidy up comments
    
    * Use flag to monitor if CSS is loaded
    
    * initialize flag with query for CSS
    
    * Add cache busting query param

--- client/components/embed-container/index.jsx
+++ client/components/embed-container/index.jsx
@@ -9,8 +9,9 @@ import filter from 'lodash/filter';
 import forEach from 'lodash/forEach';
 import forOwn from 'lodash/forOwn';
 import noop from 'lodash/noop';
+import assign from 'lodash/assign';
 
-import { loadScript } from 'lib/load-script';
+import { loadScript, loadjQueryDependentScript } from 'lib/load-script';
 
 import debugFactory from 'debug';
 
@@ -20,7 +21,17 @@ const embedsToLookFor = {
 	'blockquote[class^="instagram-"]': embedInstagram,
 	'blockquote[class^="twitter-"]': embedTwitter,
 	'fb\\\:post, [class^=fb-]': embedFacebook,
-	'[class^=tumblr-]': embedTumblr
+	'[class^=tumblr-]': embedTumblr,
+	'.jetpack-slideshow': embedSlideshow
+};
+
+const cacheBustQuery = `?v=${ Math.floor( new Date().getTime() / ( 1000 * 60 * 60 * 24 * 10 ) ) }`; // A new query every 10 days
+
+const SLIDESHOW_URLS = {
+	CSS: `https://s0.wp.com/wp-content/mu-plugins/shortcodes/css/slideshow-shortcode.css${cacheBustQuery}`,
+	CYCLE_JS: `https://s0.wp.com/wp-content/mu-plugins/shortcodes/js/jquery.cycle.js${cacheBustQuery}`,
+	JS: `https://s0.wp.com/wp-content/mu-plugins/shortcodes/js/slideshow-shortcode.js${cacheBustQuery}`,
+	SPINNER: `https://s0.wp.com/wp-content/mu-plugins/shortcodes/img/slideshow-loader.gif${cacheBustQuery}`
 };
 
 function processEmbeds( domNode ) {
@@ -37,6 +48,16 @@ function nodeNeedsProcessing( domNode ) {
 
 	domNode.setAttribute( 'data-wpcom-embed-processed', '1' );
 	return true;
+}
+
+function loadCSS( cssUrl ) {
+	const link = assign( document.createElement( 'link' ), {
+		rel: 'stylesheet',
+		type: 'text/css',
+		href: cssUrl
+	} );
+
+	document.head.appendChild( link );
 }
 
 let loaders = {};
@@ -110,6 +131,58 @@ function embedTumblr( domNode ) {
 	setTimeout( function() {
 		loadScript( 'https://secure.assets.tumblr.com/post.js', removeScript );
 	}, 30 );
+}
+
+function triggerJQueryLoadEvent() {
+	// force JetpackSlideshow to initialize, in case navigation hasn't caused ready event on document
+	window.jQuery( 'body' ).trigger( 'post-load' );
+}
+
+function createSlideshow() {
+	if ( window.JetpackSlideshow ) {
+		triggerJQueryLoadEvent();
+	}
+
+	loadAndRun( SLIDESHOW_URLS.JS, () => {
+		triggerJQueryLoadEvent();
+	} );
+}
+
+let slideshowCSSPresent = document.head.querySelector( `link[href="${ SLIDESHOW_URLS.CSS }"]` );
+
+function embedSlideshow( domNode ) {
+	debug( 'processing slideshow for', domNode );
+
+	// set global variable required by JetpackSlideshow
+	window.jetpackSlideshowSettings = {
+		spinner: SLIDESHOW_URLS.SPINNER
+	};
+
+	if ( ! slideshowCSSPresent ) {
+		slideshowCSSPresent = true;
+		loadCSS( SLIDESHOW_URLS.CSS );
+	}
+
+	// Remove no JS warning so user doesn't have to look at it while several scripts load
+	const warningElements = domNode.parentNode.getElementsByClassName( 'jetpack-slideshow-noscript' );
+	forEach( warningElements, ( el ) => {
+		el.classList.add( 'hidden' );
+	} );
+
+	if ( window.jQuery && window.jQuery.prototype.cycle ) {
+		// jQuery and cylcejs exist
+		createSlideshow();
+	} else if ( window.jQuery && ! window.jQuery.prototype.cycle ) {
+		// Only jQuery exists
+		loadAndRun( SLIDESHOW_URLS.CYCLE_JS, ()=> {
+			createSlideshow();
+		} );
+	} else {
+		// Neither exist
+		loadjQueryDependentScript( SLIDESHOW_URLS.CYCLE_JS, () => {
+			createSlideshow();
+		} );
+	}
 }
 
 export default React.createClass( {
