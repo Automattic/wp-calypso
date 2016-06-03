fcf763a4f8242adeb5efb80ed557e3f913d7d32e
Eric Binnion <ericbinnion@gmail.com>
2016-06-03 00:25:18 -0500

    Jetpack SSO: Show Drake error when error is return from validation

--- client/signup/jetpack-connect/sso.jsx
+++ client/signup/jetpack-connect/sso.jsx
@@ -144,7 +144,7 @@ const JetpackSSOForm = React.createClass( {
 		);
 	},
 
-	renderNoQueryArgsError() {
+	renderBadPathArgsError() {
 		return (
 			<Main>
 				<EmptyContent
@@ -169,10 +169,10 @@ const JetpackSSOForm = React.createClass( {
 
 	render() {
 		const user = this.props.userModule.get();
-		const { ssoNonce, siteId } = this.props;
+		const { ssoNonce, siteId, validationError } = this.props;
 
-		if ( ! ssoNonce || ! siteId ) {
-			return this.renderNoQueryArgsError();
+		if ( ! ssoNonce || ! siteId || validationError ) {
+			return this.renderBadPathArgsError();
 		}
 
 		return (
