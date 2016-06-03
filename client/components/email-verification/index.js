834a733338dd422e7dc300e8c79960883fe468d9
Marco Aurelio <thecoreh@gmail.com>
2016-06-02 21:56:52 -0300

    Move verification signal call to email-verification

--- client/components/email-verification/index.js
+++ client/components/email-verification/index.js
@@ -7,11 +7,17 @@ var startsWith = require( 'lodash/startsWith' );
  * Internal dependencies
  */
 var emitter = require( 'lib/mixins/emitter' );
+var userUtils = require( 'lib/user/utils' );
 
 var emailVerification = {
 	renderNotice: function( context ) {
 		this.showUnverifiedNotice = ! startsWith( context.path, '/checkout' ) && ! startsWith( context.path, '/plans' );
 		this.showVerifiedNotice = '1' === context.query.verified;
+
+		if ( this.showVerifiedNotice ) {
+			userUtils.signalVerification();
+		}
+
 		this.emit( 'change' );
 	}
 };
