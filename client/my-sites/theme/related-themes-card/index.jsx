3151242172cedd136e83dd3d2a0295e3cfdb461f
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2016-06-06 11:27:50 +0200

    Refactor Realted Themes into its own folder
    This is first step. Folders and files placeholders.

---
+++ client/my-sites/theme/related-themes-card/index.jsx
@@ -0,0 +1,20 @@
+/** @ssr-ready **/
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+import i18n from 'lib/mixins/i18n'
+
+var RelatedThemesCard = React.createClass( {
+	render() {
+		return (
+			<div></div>
+		);
+	}
+} );
