160b5bc56985e24ca858dd28660b70741aa8ae25
roccotripaldi <tripaldir@gmail.com>
2016-06-06 19:53:35 -0400

    State: jetpackConnectAuthorize schema adding properties for queryObject

--- client/state/jetpack-connect/schema.js
+++ client/state/jetpack-connect/schema.js
@@ -32,7 +32,21 @@ export const jetpackConnectAuthorizeSchema = {
 				manageActivated: { type: 'boolean' },
 				manageActivatedError: { type: 'boolean' },
 				plansUrl: { type: 'string' },
-				queryObject: { type: 'object' },
+				queryObject: {
+					type: 'object',
+					properties: {
+						_wp_nonce: { type: 'string' },
+						client_id: { type: 'string' },
+						from: { type: 'string' },
+						jp_version: { type: 'string' },
+						redirect_after_auth: { type: 'string' },
+						redirect_uri: { type: 'string' },
+						scope: { type: 'string' },
+						secret: { type: 'string' },
+						site: { type: 'string' },
+						state: { type: 'string' }
+					}
+				},
 				siteReceived: { type: 'boolean' }
 			},
 			additionalProperties: false
