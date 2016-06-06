86312816bb2dfbea51ee780d7110e06ae6d089b2
Rastislav Lamos <lamos.rasto@gmail.com>
2016-06-06 16:58:45 +0200

    Add FAQItem as a child of FAQ component
    Having FAQItems as children of the FAQ component helps with translations
    so that the questions/answers are not being translated outside of a
    component, which might not work.

--- client/components/faq/index.jsx
+++ client/components/faq/index.jsx
@@ -1,35 +1,23 @@
 /**
  * External dependencies
  */
-import React, { PropTypes } from 'react';
+import React from 'react';
 
-const FAQ = ( { heading, items } ) => {
+/**
+ * Internal dependencies
+ */
+import localize from 'lib/mixins/i18n/localize';
+
+function FAQ( { translate, heading = translate( 'Frequently Asked Questions' ), children } ) {
 	return (
 		<div className="faq">
 			<h1 className="faq__heading">{ heading }</h1>
 			<ul className="faq__list">
-				{
-					items.map( ( { question, answer }, ind ) =>
-						<li className="faq__item" key={ `faq-${ ind }` }>
-							<h4 className="faq__question">{ question }</h4>
-							<p className="faq__answer">{ answer }</p>
-						</li>
-					)
-				}
+				{ children }
 			</ul>
 		</div>
 	);
-};
+}
 
-FAQ.propTypes = {
-	heading: PropTypes.string.isRequired,
-	items: PropTypes.arrayOf(
-		PropTypes.shape( {
-			question: PropTypes.string.isRequired,
-			answer: PropTypes.node.isRequired
-		} )
-	).isRequired
-};
-
-export default FAQ;
+export default localize( FAQ );
 
