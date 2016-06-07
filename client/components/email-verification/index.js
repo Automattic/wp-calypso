1176f85fe237c38b4faab75f55e3fadf949bf4ed
Marco Aurelio <thecoreh@gmail.com>
2016-06-07 02:48:15 -0300

    Remove unverified check, signal to user

--- client/components/email-verification/index.js
+++ client/components/email-verification/index.js
@@ -1,21 +1,15 @@
 /**
- * External dependencies
- */
-var startsWith = require( 'lodash/startsWith' );
-
-/**
  * Internal dependencies
  */
 var emitter = require( 'lib/mixins/emitter' );
-var userUtils = require( 'lib/user/utils' );
+var user = require( 'lib/user' )();
 
 var emailVerification = {
 	renderNotice: function( context ) {
-		this.showUnverifiedNotice = ! startsWith( context.path, '/checkout' ) && ! startsWith( context.path, '/plans' );
 		this.showVerifiedNotice = '1' === context.query.verified;
 
 		if ( this.showVerifiedNotice ) {
-			userUtils.signalVerification();
+			user.signalVerification();
 		}
 
 		this.emit( 'change' );
