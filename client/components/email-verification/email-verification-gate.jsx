c9c0e700f21e39d92633bca03abf50610295aa32
Marco Aurelio <thecoreh@gmail.com>
2016-06-07 00:38:09 -0300

    Add EmailVerificationGate

---
+++ client/components/email-verification/email-verification-gate.jsx
@@ -0,0 +1,65 @@
+/**
+ * External dependencies
+ */
+import * as React from 'react';
+
+/**
+ * Internal dependencies
+ */
+
+import EmailUnverifiedNotice from './email-unverified-notice.jsx';
+import userUtils from 'lib/user/utils';
+import _sites from 'lib/sites-list';
+import _user from 'lib/user';
+
+const sites = _sites();
+const user = _user();
+
+export default class EmailVerificationGate extends React.Component {
+
+	constructor( props ) {
+		super( props );
+
+		this.updateVerificationState = this.updateVerificationState.bind( this );
+		this.handleFocus = this.handleFocus.bind( this );
+
+		this.state = {
+			needsVerification: userUtils.needsVerificationForSite( sites.getSelectedSite() ),
+		};
+	}
+
+	componentWillMount() {
+		user.on( 'change', this.updateVerificationState );
+		sites.on( 'change', this.updateVerificationState );
+	}
+
+	componentWillUnmount() {
+		user.off( 'change', this.updateVerificationState );
+		sites.off( 'change', this.updateVerificationState );
+	}
+
+	updateVerificationState() {
+		this.setState( {
+			needsVerification: userUtils.needsVerificationForSite( sites.getSelectedSite() ),
+		} );
+	}
+
+	handleFocus( e ) {
+		e.target.blur();
+	}
+
+	render() {
+		if ( this.state.needsVerification ) {
+			return (
+				<div tabIndex="-1" className="email-verification-gate" onFocus={ this.handleFocus }>
+					<EmailUnverifiedNotice />
+					<div className="email-verification-gate__content">
+						{	this.props.children }
+					</div>
+				</div>
+			);
+		}
+
+		return <div>{ this.props.children }</div>;
+	}
+}
