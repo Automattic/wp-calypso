a29c085986e3fdacb3091f91f2421718e3bf4a0a
Kelly Dwan <kelly.dwan@gmail.com>
2016-06-08 16:57:30 -0400

    Pass the SelectedSite down from the parent, so that we get the fully-synced JetpackSite object.

--- client/my-sites/plugins/jetpack-plugins-setup/index.jsx
+++ client/my-sites/plugins/jetpack-plugins-setup/index.jsx
@@ -333,7 +333,6 @@ const PlansSetup = React.createClass( {
 export default connect(
 	state => {
 		const siteId = getSelectedSiteId( state );
-		const site = getSelectedSite( state );
 
 		return {
 			wporg: state.plugins.wporg.items,
@@ -344,7 +343,6 @@ export default connect(
 			plugins: getPluginsForSite( state, siteId ),
 			activePlugin: getActivePlugin( state, siteId ),
 			nextPlugin: getNextPlugin( state, siteId ),
-			selectedSite: site && site.jetpack ? JetpackSite( site ) : site,
 			isRequestingSites: isRequestingSites( state ),
 			siteId
 		};
