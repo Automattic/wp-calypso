7cba2d5f1c86511f2e3e346f51ff445f2e20e26d
artpi <artpi@post.pl>
2016-06-09 17:10:09 +0200

    Add tests for failing action

--- client/state/preferences/test/actions.js
+++ client/state/preferences/test/actions.js
@@ -10,6 +10,7 @@ import nock from 'nock';
 import {
 	PREFERENCES_RECEIVE,
 	PREFERENCES_FETCH,
+	PREFERENCES_FETCH_FAILURE,
 	PREFERENCES_FETCH_SUCCESS,
 	PREFERENCES_SET
 } from 'state/action-types';
@@ -26,24 +27,22 @@ describe( 'actions', () => {
 		spy = sandbox.spy();
 	} );
 	const responseShape = {
-		_headers: { 'content-type': 'application/json' },
 		[ USER_SETTING_KEY ]: DEFAULT_PREFERENCES
 	};
 	const getState = () => ( { preferences: { values: DEFAULT_PREFERENCES } } );
-	before( () => {
-		nock( 'https://public-api.wordpress.com:443' )
-			.persist()
-			.get( '/rest/v1.1/me/settings' )
-			.reply( 200, responseShape )
-			.post( '/rest/v1.1/me/settings/' )
-			.reply( 200, responseShape );
-	} );
-
-	after( () => {
-		nock.cleanAll();
-	} );
 
 	describe( '#fetchPreferences()', () => {
+		before( () => {
+			nock( 'https://public-api.wordpress.com:443' )
+				.persist()
+				.get( '/rest/v1.1/me/settings' )
+				.reply( 200, responseShape );
+		} );
+
+		after( () => {
+			nock.cleanAll();
+		} );
+
 		it( 'should dispatch fetch action when thunk triggered', () => {
 			fetchPreferences()( spy );
 			expect( spy ).to.have.been.calledWith( {
@@ -53,7 +52,7 @@ describe( 'actions', () => {
 
 		it( 'should dispatch success action when request completes', () => {
 			return fetchPreferences()( spy ).then( () => {
-				expect( spy ).to.have.been.calledWith( {
+				expect( spy ).to.have.been.calledWithMatch( {
 					type: PREFERENCES_FETCH_SUCCESS,
 					data: responseShape,
 				} );
@@ -61,10 +60,42 @@ describe( 'actions', () => {
 		} );
 	} );
 
+	describe( '#fetchPreferences()', () => {
+		before( () => {
+			nock( 'https://public-api.wordpress.com:443' )
+				.persist()
+				.get( '/rest/v1.1/me/settings' )
+				.reply( 404 );
+		} );
+
+		after( () => {
+			nock.cleanAll();
+		} );
+
+		it( 'should dispatch fail action when request fails', () => {
+			return fetchPreferences()( spy ).then( () => {
+				expect( spy ).to.have.been.calledWithMatch( {
+					type: PREFERENCES_FETCH_FAILURE
+				} );
+			} );
+		} );
+	} );
+
 	describe( '#setPreference()', () => {
+		before( () => {
+			nock( 'https://public-api.wordpress.com:443' )
+				.persist()
+				.post( '/rest/v1.1/me/settings/' )
+				.reply( 200, responseShape );
+		} );
+
+		after( () => {
+			nock.cleanAll();
+		} );
+
 		it( 'should dispatch PREFERENCES_SET action when thunk triggered', () => {
 			setPreference( 'preferenceKey', 'preferenceValue' )( spy, getState );
-			expect( spy ).to.have.been.calledWith( {
+			expect( spy ).to.have.been.calledWithMatch( {
 				type: PREFERENCES_SET,
 				key: 'preferenceKey',
 				value: 'preferenceValue'
@@ -73,7 +104,7 @@ describe( 'actions', () => {
 
 		it( 'should dispatch PREFERENCES_RECEIVE action when request completes', () => {
 			return setPreference( 'preferenceKey', 'preferenceValue' )( spy, getState ).then( () => {
-				expect( spy ).to.have.been.calledWith( {
+				expect( spy ).to.have.been.calledWithMatch( {
 					type: PREFERENCES_RECEIVE,
 					data: responseShape
 				} );
