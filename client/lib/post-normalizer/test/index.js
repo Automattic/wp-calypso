4807e298da11e42090b989356308785559afba7f
Ben Lowery <ben.lowery@automattic.com>
2016-06-09 11:48:03 -0400

    update test to use https for poll link

--- client/lib/post-normalizer/test/index.js
+++ client/lib/post-normalizer/test/index.js
@@ -911,7 +911,7 @@ describe( 'index', function() {
 				[
 					normalizer.withContentDOM( [ normalizer.content.detectPolls ] )
 				], function( err, normalized ) {
-					assert.include( normalized.content, '<p><a rel="external" target="_blank" href="http://polldaddy.com/poll/8980420">Take our poll</a></p>' );
+					assert.include( normalized.content, '<p><a rel="external" target="_blank" href="https://polldaddy.com/poll/8980420">Take our poll</a></p>' );
 					done( err );
 				}
 			);
