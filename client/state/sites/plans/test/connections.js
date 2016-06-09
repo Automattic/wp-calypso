89e26b11e7b935a063219aaf1897b3f4112738d7
artpi <artpi@post.pl>
2016-06-09 17:59:51 +0200

    Add createWpcomSpy helper

--- client/state/sites/plans/test/connections.js
+++ client/state/sites/plans/test/connections.js
@@ -10,32 +10,50 @@ import { spy } from 'sinon';
 import { SITE_PLANS_FETCH_COMPLETED, SITE_PLANS_FETCH_FAILED } from 'state/action-types';
 import { connectFetchSitePlans } from '../connections';
 
+/**
+ * Creates a sinon spy out of responseFunction and wraps it in layers of function calls, like wpcom does.
+ * Returned object stubs wpcom.
+ * responseSpy attribute of returned object provides reference to sinon spy.
+ * @param {Array} path
+ * @param {Function} responseFunction
+ * @returns {Object} wpcom-like wrapped object
+ */
+function createWpcomSpy( path, responseFunction ) {
+	const responseSpy = spy( responseFunction );
+	const wpcom =  path.reverse().reduce( ( prev, current, index ) => ( { [ current ]: index ? () => prev : prev } ), responseSpy );
+	wpcom.responseSpy = responseSpy;
+	return wpcom;
+}
+
 describe( 'connections', () => {
 	describe( '#connectFetchSitePlans()', () => {
-		let apiError = null;
 		const dispatchMock = spy();
-		const getSitePlansMock = spy( ( siteId, callback ) => callback( apiError, {} ) );
-		const connectedFetchSitePlans = connectFetchSitePlans( { undocumented: () => ( {
-			getSitePlans: getSitePlansMock
-		} ) } );
 
 		it( 'should pass siteId to API call', () => {
-			connectedFetchSitePlans( { siteId: 5 }, dispatchMock );
-			expect(
-				getSitePlansMock.calledWith( 5 )
-			).ok;
+			const wpcom = createWpcomSpy(
+				[ 'undocumented', 'getSitePlans' ],
+				( siteId, callback ) => callback( null, {} )
+			);
+			connectFetchSitePlans( wpcom )( { siteId: 5 }, dispatchMock );
+			expect( wpcom.responseSpy.calledWith( 5 ) ).ok;
 		} );
 
 		it( 'should dispatch "SITE_PLANS_FETCH_COMPLETED" upon success', () => {
-			apiError = null;
-			connectedFetchSitePlans( { siteId: 5 }, dispatchMock );
+			const wpcom = createWpcomSpy(
+				[ 'undocumented', 'getSitePlans' ],
+				( siteId, callback ) => callback( null, {} )
+			);
+			connectFetchSitePlans( wpcom )( { siteId: 5 }, dispatchMock );
 			expect(
 				dispatchMock.calledWithMatch( { type: SITE_PLANS_FETCH_COMPLETED, siteId: 5 } )
 			).ok;
 		} );
 		it( 'should dispatch "SITE_PLANS_FETCH_FAILED" upon failure and pass proper error message', () => {
-			apiError = { message: 'error message' };
-			connectedFetchSitePlans( { siteId: 5 }, dispatchMock );
+			const wpcom = createWpcomSpy(
+				[ 'undocumented', 'getSitePlans' ],
+				( siteId, callback ) => callback( { message: 'error message' }, {} )
+			);
+			connectFetchSitePlans( wpcom )( { siteId: 5 }, dispatchMock );
 			expect(
 				dispatchMock.calledWithMatch( { type: SITE_PLANS_FETCH_FAILED, siteId: 5, error: 'error message' } )
 			).ok;
