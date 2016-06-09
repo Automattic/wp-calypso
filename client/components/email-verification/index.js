8e6534b69b5cdb5c793151b40f2abf0d32e0a593
Marco Aurelio <thecoreh@gmail.com>
2016-06-09 14:58:44 -0300

    Reorganize imports

--- client/components/email-verification/index.js
+++ client/components/email-verification/index.js
@@ -2,14 +2,13 @@
  * External dependencies
  */
 
-import * as React from 'react';
+import i18n from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 
 import { successNotice } from 'state/notices/actions';
-import i18n from 'i18n-calypso';
 import _user from 'lib/user';
 
 /**
@@ -30,8 +29,8 @@ export default function emailVerification( context, next ) {
 		setTimeout( () => {
 			// TODO: unify these once translations catch up
 			let message = i18n.getLocaleSlug() === 'en'
- 				? i18n.translate( 'Email confirmed!' )
- 				: i18n.translate( "Email confirmed! Now that you've confirmed your email address you can publish posts on your blog." );
+				? i18n.translate( 'Email confirmed!' )
+				: i18n.translate( "Email confirmed! Now that you've confirmed your email address you can publish posts on your blog." );
 			let notice = successNotice( message, { duration: 10000 } );
 			context.store.dispatch( notice );
 		}, 100 ); // A delay is needed here, because the notice state seems to be cleared upon page load
