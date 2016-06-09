d3d16ed4145e3cd0a90fe9f6ab6b81389b8c4c0f
Dennis Snell <dennis.snell@automattic.com>
2016-06-09 14:08:37 -0700

    SEO Search Preview: Style updates (#5913)
    * SEO Search Preview: Style updates
    
     - Removes box shadow
     - Adds margin to preview box
     - Changes color of header text
     - Truncates SEO strings according to an algorithm that somewhat mtaches
    what Google does for the SEO titles and snippets.

--- client/components/seo/search-preview/index.jsx
+++ client/components/seo/search-preview/index.jsx
@@ -1,5 +1,37 @@
 import React, { PropTypes } from 'react';
 import PureRenderMixin from 'react-pure-render/mixin';
+import find from 'lodash/find';
+
+const TITLE_LENGTH = 63;
+const SNIPPET_LENGTH = 160;
+
+const shortEnough = limit => title => title.length <= limit ? title : false;
+
+const truncatedAtSpace = ( lower, upper ) => fullTitle => {
+	const title = fullTitle.slice( 0, upper );
+	const lastSpace = title.lastIndexOf( ' ' );
+
+	return ( lastSpace > lower && lastSpace < upper )
+		? title.slice( 0, lastSpace ).concat( '…' )
+		: false;
+};
+
+const hardTruncation = limit => title => title.slice( 0, limit ).concat( '…' );
+
+const firstValid = ( ...predicates ) => a =>
+	find( predicates, ( p => false !== p( a ) ) )( a );
+
+const googleTitle = firstValid(
+	shortEnough( TITLE_LENGTH ),
+	truncatedAtSpace( TITLE_LENGTH - 40, TITLE_LENGTH + 10 ),
+	hardTruncation( TITLE_LENGTH )
+);
+
+const googleSnippet = firstValid(
+	shortEnough( SNIPPET_LENGTH ),
+	truncatedAtSpace( SNIPPET_LENGTH - 80, SNIPPET_LENGTH + 10 ),
+	hardTruncation( SNIPPET_LENGTH )
+);
 
 export const SearchPreview = React.createClass( {
 	mixins: [ PureRenderMixin ],
@@ -16,13 +48,13 @@ export const SearchPreview = React.createClass( {
 				<h2 className="seo-search-preview__header">{ this.translate( 'Search Preview' ) }</h2>
 				<div className="seo-search-preview__display">
 					<div className="seo-search-preview__title">
-						{ title }
+						{ googleTitle( title ) }
 					</div>
 					<div className="seo-search-preview__url">
 						{ url } ▾
 					</div>
 					<div className="seo-search-preview__snippet">
-						{ snippet }
+						{ googleSnippet( snippet || '' ) }
 					</div>
 				</div>
 			</div>
