b1c615b0816559136710c3f71d377c81c50a2d37
Tugdual de Kerviler <dekervit@gmail.com>
2016-06-10 11:21:40 +0200

    Use bundeled momentjs data instead of our own

--- packages/i18n-calypso/cli/extras/date.js
+++ packages/i18n-calypso/cli/extras/date.js
@@ -11,35 +11,3 @@ i18n.translate( "a month" );
 i18n.translate( "%d months" );
 i18n.translate( "a year" );
 i18n.translate( "%d years" );
-
-// Momentjs formatting strings
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "HH:mm", { context: "momentjs format string (for LT)" } );
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "DD/MM/YYYY", { context: "momentjs format string (for L)" } );
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "D MMMM YYYY", { context: "momentjs format string (for LL)" } );
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "D MMMM YYYY LT", { context: "momentjs format string (for LLL)" } );
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "dddd, D MMMM YYYY LT", { context: "momentjs format string (for LLLL)" } );
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "MMM D", { context: "momentjs format string (for MMM D)" } );
-
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "[Today at] LT", { context: "momentjs format string (sameDay)" } );
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "[Tomorrow at] LT", { context: "momentjs format string (nextDay)" } );
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "dddd [at] LT", { context: "momentjs format string (nextWeek)" } );
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "[Yesterday at] LT", { context: "momentjs format string (lastDay)" } );
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "[Last] dddd [at] LT", { context: "momentjs format string (lastWeek)" } );
-/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
-i18n.translate( "L", { context: "momentjs format string (sameElse)" } );
-
-/* translators: Number representing the first day of the week, 0=Sunday, 1=Monday, etc. Should match typical region formats for the most prominent region where the language is spoken. */
-i18n.translate( "num_first_day_of_week", { context: "momentjs setting" } );
-/* translators: The week that contains Jan (X) is the first week of the year, e.g., `4` means Jan 4. Different regions start counting weeks with different rules, check Wikipedia to be sure. */
-i18n.translate( "num_day_of_year", { context: "momentjs setting" } );
