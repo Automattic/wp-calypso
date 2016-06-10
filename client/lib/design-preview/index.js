b1702ee18414f4dad5487b5517cf49096fc2af1d
Payton Swick <payton@foolord.com>
2016-06-10 13:08:01 -0400

    DesignPreview Lib: initial commit

--- client/lib/design-preview/index.js
+++ client/lib/design-preview/index.js
@@ -1,3 +1,13 @@
+import siteTitle from './updaters/site-title';
+import headerImage from './updaters/header-image';
+import siteLogo from './updaters/site-logo';
+
+const updaterFunctions = [
+	siteTitle,
+	headerImage,
+	siteLogo,
+];
+
 export function updatePreviewWithChanges( previewDocument, customizations ) {
-	// Update the preview here
+	updaterFunctions.map( callback => callback( previewDocument, customizations ) );
 }
