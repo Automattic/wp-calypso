657514c1ae60a0c03b8e7175aa616d8dce557ae0
artpi <artpi@post.pl>
2016-06-13 13:52:03 +0200

    Change wording in wordads success message

--- client/my-sites/ads/main.jsx
+++ client/my-sites/ads/main.jsx
@@ -133,7 +133,7 @@ const AdsMain = React.createClass( {
 		if ( this.props.requestingWordAdsApproval || this.props.wordAdsSuccess ) {
 			notice = (
 				<Notice status="is-success" showDismiss={ false }>
-					{ this.translate( 'You have successfully joined WordAds. Please review these settings:' ) }
+					{ this.translate( 'You have joined the WordAds program. Please review these settings:' ) }
 				</Notice>
 			);
 		} else if ( ! this.props.site.options.wordads && isWordadsInstantActivationEligible( this.props.site ) ) {
