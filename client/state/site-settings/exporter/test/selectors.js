f5b814634e03eb36864ace903ff78a6f46bb20cd
Jordan West <jordwest@users.noreply.github.com>
2016-06-14 12:07:36 +1000

    Export: Validate date range before export (#5850)
    * Export: Validate date range before export
    
    This change adds a validation notice popup when the selected start date
    is after the end date. Submitting the form with the wrong date range
    would result in an empty export, so the Export Selected Content button
    is also disabled.

--- client/state/site-settings/exporter/test/selectors.js
+++ client/state/site-settings/exporter/test/selectors.js
@@ -6,7 +6,7 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import { isExporting } from '../selectors';
+import { isExporting, isDateRangeValid } from '../selectors';
 import { States } from '../constants.js';
 
 describe( 'selectors', () => {
@@ -49,6 +49,63 @@ describe( 'selectors', () => {
 			}, 100658273 );
 
 			expect( exporting ).to.eql( true );
+		} );
+
+		it( 'should return invalid date if start date is after end date', () => {
+			const state = {
+				siteSettings: {
+					exporter: {
+						selectedAdvancedSettings: {
+							100658273: {
+								post: {
+									start_date: '2016-06',
+									end_date: '2004-03',
+								}
+							}
+						}
+					}
+				}
+			};
+			expect( isDateRangeValid( state, 100658273, 'post' ) ).to.equal( false );
+			expect( isDateRangeValid( state, 100658273, 'page' ) ).to.equal( true );
+		} );
+
+		it( 'should return valid date if end date is after start date', () => {
+			const state = {
+				siteSettings: {
+					exporter: {
+						selectedAdvancedSettings: {
+							100658273: {
+								post: {
+									start_date: '2006-06',
+									end_date: '2024-03',
+								}
+							}
+						}
+					}
+				}
+			};
+			expect( isDateRangeValid( state, 100658273, 'post' ) ).to.equal( true );
+			expect( isDateRangeValid( state, 100658273, 'page' ) ).to.equal( true );
+		} );
+
+		it( 'should return valid date if end date is the same as start date', () => {
+			const state = {
+				siteSettings: {
+					exporter: {
+						selectedAdvancedSettings: {
+							100658273: {
+								post: {
+									start_date: '2040-06',
+									end_date: '2040-06',
+								}
+							}
+						}
+					}
+				}
+			};
+			expect( isDateRangeValid( state, 100658273, 'post' ) ).to.equal( true );
+			expect( isDateRangeValid( state, 100658273, 'page' ) ).to.equal( true );
 		} );
 	} );
 } );
