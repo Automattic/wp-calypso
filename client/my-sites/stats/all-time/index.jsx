3b2628eeaa48577a8b7bfec79293573d0e9706f4
Timmy Crawford <timmydcrawford@gmail.com>
2016-06-13 21:33:13 -0700

    Stats: update all-time component to use QuerySiteStats.

--- client/my-sites/stats/all-time/index.jsx
+++ client/my-sites/stats/all-time/index.jsx
@@ -3,47 +3,66 @@
 */
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
+import { connect } from 'react-redux';
+import PureRenderMixin from 'react-pure-render/mixin';
 
 /**
  * Internal dependencies
  */
-import observe from 'lib/mixins/data-observe';
 import Card from 'components/card';
 import User from 'lib/user';
 import StatsTabs from '../stats-tabs';
 import StatsTab from '../stats-tabs/tab';
 import SectionHeader from 'components/section-header';
+import QuerySiteStats from 'components/data/query-site-stats';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import {
+	isRequestingSiteStatsForQuery,
+	getSiteStatsForQuery
+} from 'state/stats/lists/selectors';
 
 const user = User();
 
-export default React.createClass( {
-	displayName: 'StatsAllTime',
-
-	mixins: [ observe( 'allTimeList' ) ],
+const StatsAllTime = React.createClass( {
+	mixins: [ PureRenderMixin ],
 
 	propTypes: {
-		allTimeList: PropTypes.object.isRequired
+		siteId: PropTypes.number,
+		requesting: PropTypes.bool,
+		query: PropTypes.object,
+		posts: PropTypes.number,
+		views: PropTypes.number,
+		views_best_day: PropTypes.string,
+		views_best_day_total: PropTypes.number
 	},
 
 	render() {
-		const allTimeList = this.props.allTimeList.response;
-		const isLoading = this.props.allTimeList.isLoading();
+		const {
+			siteId,
+			requesting,
+			posts,
+			views,
+			visitors,
+			views_best_day,
+			views_best_day_total,
+			query
+		} = this.props;
+		const isLoading = requesting && ! views;
 
 		let bestDay;
 
-		if ( allTimeList['best-views'] && allTimeList['best-views'].day ) {
-			bestDay = this.moment( allTimeList['best-views'].day ).format( 'LL' );
+		if ( views_best_day && ! isLoading ) {
+			bestDay = this.moment( views_best_day ).format( 'LL' );
 		}
 
 		const classes = {
-			'is-loading': this.props.allTimeList.isLoading(),
+			'is-loading': requesting,
 			'is-non-en': user.data.localeSlug && ( user.data.localeSlug !== 'en' )
 		};
 
-		const bestViews = allTimeList['best-views'] ? allTimeList['best-views'].count : null;
-
 		return (
 			<div>
+				{ siteId && <QuerySiteStats siteId={ siteId } statType="stats" query={ query } /> }
 				<SectionHeader label={ this.translate( 'All-time posts, views, and visitors' ) }></SectionHeader>
 				<Card className={ classNames( 'stats-module', 'stats-all-time', classes ) }>
 					<div className="module-content">
@@ -52,23 +71,23 @@ export default React.createClass( {
 								gridicon="posts"
 								label={ this.translate( 'Posts' ) }
 								loading={ isLoading }
-								value={ allTimeList.posts } />
+								value={ posts } />
 							<StatsTab
 								gridicon="visible"
 								label={ this.translate( 'Views' ) }
 								loading={ isLoading }
-								value={ allTimeList.views } />
+								value={ views } />
 							<StatsTab
 								gridicon="user"
 								label={ this.translate( 'Visitors' ) }
 								loading={ isLoading }
-								value={ allTimeList.visitors } />
+								value={ visitors } />
 							<StatsTab
 								className="is-best"
 								gridicon="trophy"
 								label={ this.translate( 'Best Views Ever' ) }
 								loading={ isLoading }
-								value={ bestViews }>
+								value={ views_best_day_total }>
 								<span className="stats-all-time__best-day">{ bestDay }</span>
 							</StatsTab>
 						</StatsTabs>
@@ -78,3 +97,27 @@ export default React.createClass( {
 		);
 	}
 } );
+
+export default connect( ( state ) => {
+	const siteId = getSelectedSiteId( state );
+	const query = {};
+	const allTimeData = getSiteStatsForQuery( state, siteId, 'stats', query ) || { stats: {} };
+	const {
+		posts,
+		views,
+		visitors,
+		views_best_day,
+		views_best_day_total
+	} = allTimeData.stats;
+
+	return {
+		requesting: isRequestingSiteStatsForQuery( state, siteId, 'stats', query ),
+		query,
+		siteId,
+		posts,
+		views,
+		visitors,
+		views_best_day,
+		views_best_day_total
+	};
+} )( StatsAllTime );
