2bfb98b91abba16ca687cc98185f03bc43588ce2
Payton Swick <payton@foolord.com>
2016-06-15 12:11:18 -0400

    Signup: split ThemeSelection into two components for clarity
    Fixes #6038

---
+++ client/signup/steps/theme-selection/signup-themes-list.jsx
@@ -0,0 +1,62 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import noop from 'lodash/noop';
+
+/**
+ * Internal dependencies
+ */
+import getThemes from 'lib/signup/themes';
+import ThemesList from 'components/themes-list';
+
+module.exports = React.createClass( {
+	displayName: 'SignupThemesList',
+
+	propTypes: {
+		surveyQuestion: React.PropTypes.string,
+		designType: React.PropTypes.string,
+		handleScreenshotClick: React.PropTypes.func,
+	},
+
+	getDefaultProps() {
+		return {
+			surveyQuestion: null,
+			designType: null,
+			handleScreenshotClick: noop,
+		};
+	},
+
+	shouldComponentUpdate( nextProps ) {
+		return ( nextProps.surveyQuestion !== this.props.surveyQuestion || nextProps.designType !== this.props.designType );
+	},
+
+	getComputedThemes() {
+		return getThemes( this.props.surveyQuestion, this.props.designType );
+	},
+
+	getScreenshotUrl( slug ) {
+		return 'https://i1.wp.com/s0.wp.com/wp-content/themes/pub/' + slug + '/screenshot.png?w=660';
+	},
+
+	render() {
+		const actionLabel = this.translate( 'Pick' );
+		const getActionLabel = () => actionLabel;
+		const themes = this.getComputedThemes().map( theme => {
+			return {
+				id: theme.slug,
+				name: theme.name,
+				screenshot: this.getScreenshotUrl( theme.slug ),
+			};
+		} );
+		return (
+			<ThemesList
+				getButtonOptions= { noop }
+				onScreenshotClick= { this.props.handleScreenshotClick }
+				onMoreButtonClick= { noop }
+				getActionLabel={ getActionLabel }
+				themes= { themes } />
+		);
+	}
+} );
+
