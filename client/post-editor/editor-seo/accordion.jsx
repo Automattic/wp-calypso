446e5210fb2af1dd4b8b0bb9eba6b3ce0fbac755
Dan Roundhill <dan@automattic.com>
2016-06-15 16:46:36 -0700

    Updates based on feedback:
    * Extends React `component` class.
    * Pass siteId to component instead of the whole object, and use redux store's `getSiteSlug` selector.
    * Removed some unused code.
    * Updated naming to match guidelines.
    * Uses new `localize` method for translations.

--- client/post-editor/editor-seo/accordion.jsx
+++ client/post-editor/editor-seo/accordion.jsx
@@ -1,7 +1,9 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -12,41 +14,36 @@ import CountedTextarea from 'components/forms/counted-textarea';
 import Gridicon from 'components/gridicon';
 import InfoPopover from 'components/info-popover';
 import TokenField from 'components/token-field';
+import { getSiteSlug } from 'state/sites/selectors';
 
-function getSeoTabUrl( slug ) {
-	return `/settings/seo/${ slug }`;
-}
+class SeoAccordion extends Component {
+	constructor() {
+		super();
 
-export default React.createClass( {
-	displayName: 'EditorSeo',
-
-	propTypes: {
-		site: React.PropTypes.object.isRequired,
-		post: React.PropTypes.object.isRequired
-	},
+		this.displayName = 'EditorSeoAccordion';
+	}
 
 	render() {
-		const { slug } = this.props.site;
-
+		const { translate } = this.props;
 		// Temporary placeholder chips for design review
 		const sampleChips = [ 'Post Title', 'Site Title' ];
 
 		return (
 			<Accordion
-				title={ this.translate( 'Advanced SEO' ) }
+				title={ translate( 'Advanced SEO' ) }
 				icon={ <Gridicon icon="search" /> }
-				className="editor-drawer__seo"
+				className="editor-drawer__editor-seo"
 			>
 				<AccordionSection>
 					<span className="editor-drawer__label-text">
-						{ this.translate( 'Meta Title' ) }
+						{ translate( 'Meta Title' ) }
 						<InfoPopover position="top left">
-							{ this.translate(
+							{ translate(
 								'The format for the title as it will appear in search engines. ' +
 								'{{a}}Edit{{/a}}',
 								{
 									components: {
-										a: <a target="_blank" href={ getSeoTabUrl( slug ) } />
+										a: <a target="_blank" href={ this.props.siteSlug } />
 									}
 								}
 							) }
@@ -56,24 +53,32 @@ export default React.createClass( {
 				</AccordionSection>
 				<AccordionSection>
 					<span className="editor-drawer__label-text">
-						{ this.translate( 'Meta Description' ) }
+						{ translate( 'Meta Description' ) }
 						<InfoPopover position="top left">
-							{ this.translate(
+							{ translate(
 								'Craft a description of your post in about 160 characters. ' +
 								'This description can be used in search engine results.'
 							) }
 						</InfoPopover>
 					</span>
 					<CountedTextarea
-						id="seo-meta-description"
-						name="seo-meta-description"
 						maxLength="300"
 						acceptableLength={ 159 }
-						placeholder={ this.translate( 'Write a description…' ) }
-						aria-label={ this.translate( 'Write a description…' ) }
+						placeholder={ translate( 'Write a description…' ) }
+						aria-label={ translate( 'Write a description…' ) }
 					/>
 				</AccordionSection>
 			</Accordion>
 		);
 	}
+}
+
+SeoAccordion.propTypes = {
+	siteId: PropTypes.number.isRequired
+};
+
+const mapStateToProps = ( state, { siteId } ) => ( {
+	siteSlug: getSiteSlug( state, siteId )
 } );
+
+export default connect( mapStateToProps )( localize( SeoAccordion ) );
