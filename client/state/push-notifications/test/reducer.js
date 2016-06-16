5e006cf876a4437929716048b117a992b94622ee
Miklos Juhasz <miklos.juhasz@automattic.com>
2016-06-16 14:25:05 +0200

    Add reducer test for settings persistance.
    Make sure that values meant to be persisted are serialized and deserialized successfully.

--- client/state/push-notifications/test/reducer.js
+++ client/state/push-notifications/test/reducer.js
@@ -12,9 +12,32 @@ import {
 } from 'state/action-types';
 import reducer, {} from '../reducer';
 
+const wpcomSubscription = {
+	ID: 42,
+	lastUpdated: '2016-06-16T14:41:09+02:00',
+	settings: {
+		comments: {
+			desc: 'Comments',
+			long_desc: '"Someone comments one of my posts"',
+			value: '1' }
+	}
+};
+
 describe( 'system reducer', () => {
+
+	it( 'should persist keys', () => {
+		const state = reducer( {
+			system: {
+				wpcomSubscription: wpcomSubscription,
+			}
+		}, { type: SERIALIZE } );
+
+		expect( state.system ).to.eql( {
+			wpcomSubscription,
+		} );
+	} );
+
 	it( 'should refuse to persist particular keys', () => {
-		const wpcomSubscription = { ID: 42 };
 		const state = reducer( {
 			system: {
 				apiReady: true,
@@ -24,6 +47,18 @@ describe( 'system reducer', () => {
 				wpcomSubscription: wpcomSubscription,
 			}
 		}, { type: SERIALIZE } );
+
+		expect( state.system ).to.eql( {
+			wpcomSubscription,
+		} );
+	} );
+
+	it( 'should restore keys', () => {
+		const state = reducer( {
+			system: {
+				wpcomSubscription: wpcomSubscription,
+			}
+		}, { type: DESERIALIZE } );
 
 		expect( state.system ).to.eql( {
 			wpcomSubscription,
@@ -49,19 +84,29 @@ describe( 'system reducer', () => {
 } );
 
 describe( 'settings reducer', () => {
+
+	it( 'should persist keys', () => {
+		const state = reducer( {
+			settings: {
+				enabled: false,
+				dismissedNotice: true,
+				dismissedNoticeAt: 1466067124796,
+			}
+		}, { type: SERIALIZE } );
+
+		expect( state.settings ).to.eql( {
+			dismissedNotice: true,
+			dismissedNoticeAt: 1466067124796,
+			enabled: false,
+		} );
+	} );
+
 	it( 'should refuse to persist particular keys', () => {
-		const wpcomSubscription = { ID: 42 };
 		const state = reducer( {
 			settings: {
 				enabled: true,
 				showingUnblockInstructions: true,
-			},
-			system: {
-				authorized: true,
-				authorizationLoaded: true,
-				blocked: true,
-				wpcomSubscription: wpcomSubscription,
-			},
+			}
 		}, { type: SERIALIZE } );
 
 		expect( state.settings ).to.eql( {
@@ -69,19 +114,28 @@ describe( 'settings reducer', () => {
 		} );
 	} );
 
+	it( 'should restore keys', () => {
+		const state = reducer( {
+			settings: {
+				enabled: false,
+				dismissedNotice: true,
+				dismissedNoticeAt: 1466067124796,
+			}
+		}, { type: DESERIALIZE } );
+
+		expect( state.settings ).to.eql( {
+			dismissedNotice: true,
+			dismissedNoticeAt: 1466067124796,
+			enabled: false,
+		} );
+	} );
+
 	it( 'should refuse to restore particular keys', () => {
-		const wpcomSubscription = { ID: 42 };
 		const state = reducer( {
 			settings: {
 				enabled: true,
 				showingUnblockInstructions: true,
-			},
-			system: {
-				authorized: true,
-				authorizationLoaded: true,
-				blocked: true,
-				wpcomSubscription: wpcomSubscription,
-			},
+			}
 		}, { type: DESERIALIZE } );
 
 		expect( state.settings ).to.eql( {
