5ff1f1e85189eac12e613b796bfcf3ea3c80f54a
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2016-06-17 16:22:56 -0300

    Removed translation functions from personal plan definition

--- client/lib/plans/personal-plan.js
+++ client/lib/plans/personal-plan.js
@@ -1,19 +1,20 @@
-import { translate } from 'i18n-calypso';
-
+/**
+ * Internal dependencies
+ */
 import { PLAN_PERSONAL } from './constants';
 
 export default {
 	product_id: 1009,
-	product_name: translate( 'WordPress.com Personal' ),
+	product_name: 'WordPress.com Personal',
 	prices: {
 		USD: 71.88
 	},
-	product_name_short: translate( 'Personal' ),
+	product_name_short: 'Personal',
 	product_slug: PLAN_PERSONAL,
-	tagline: translate( 'Get your own domain' ),
-	shortdesc: translate( 'Use your own domain and establish your online presence without ads.' ),
-	description: translate( 'Use your own domain and establish your online presence without ads.' ),
-	capability: translate( 'manage_options' ),
+	tagline: 'Get your own domain',
+	shortdesc: 'Use your own domain and establish your online presence without ads.',
+	description: 'Use your own domain and establish your online presence without ads.',
+	capability: 'manage_options',
 	cost: 71.88,
 	features_highlight: [
 		{ items: [ 'no-adverts/no-adverts.php', 'custom-domain', 'support', 'space' ] },
@@ -23,7 +24,7 @@ export default {
 	product_type: 'bundle',
 	available: 'yes',
 	bundle_product_ids: [ 12, 9, 50, 5, 6, 46, 54, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 72, 73, 74, 75, 16 ],
-	bill_period_label: translate( 'per year' ),
+	bill_period_label: 'per year',
 	price: '$71.88',
 	formatted_price: '$71.88',
 	raw_price: 71.88
