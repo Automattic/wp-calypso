70b088229e129a9bfa21c69f92c87b7fb1f63739
Marco Aurélio <thecoreh@gmail.com>
2016-06-20 20:15:08 -0300

    Retry: Add reactive email verification (#6158)
    * Add verification polling and signaling functions to userUtil
    
    * Move verification signal call to email-verification
    
    * Use global user and userUtils instead of a prop
    
    * Remove global user verification notice
    
    * Fix broken email verification notice code
    
    * Add email verification notice to menu editor.
    
    * Move verification polling to user
    
    * Refactor email-verification-notice
    
    * Add EmailVerificationGate
    
    * Use EmailVerificationGate in specific sections
    
    * Remove unverified check, signal to user
    
    * Add verified notice.
    
    * Add email verified notice to layout
    
    * Only ping server if there are listeners
    
    * Add debug calls
    
    * Use verify instead of verified for the event
    
    * Update i18n require.
    
    * Use a global notice for verification
    
    * Use simpler message, and old translation for non-en
    
    * Reorganize imports
    
    * Remove unused style
    
    * Reenable margin for notice
    
    * Tweak notification gate placement
    
    * Use Calypso naming convention for factories
    
    * Make sure to emit a change event if data is already loaded
    
    * Add a config flag for EmailVerificationGate
    
    * Only enable reactive verification if config prop is set

--- client/lib/user/user.js
+++ client/lib/user/user.js
@@ -31,6 +31,10 @@ function User() {
 	this.initialize();
 }
 
+/**
+ * Constants
+ */
+const VERIFICATION_POLL_INTERVAL = 15000;
 
 /**
  * Mixins
@@ -44,6 +48,10 @@ User.prototype.initialize = function() {
 	debug( 'Initializing User' );
 	this.fetching = false;
 	this.initialized = false;
+
+	if ( config( 'email_verification_gate' ) ) {
+		this.on( 'change', this.checkVerification.bind( this ) );
+	}
 
 	if ( isSupportUserSession() ) {
 		supportUserBoot();
@@ -75,6 +83,7 @@ User.prototype.initialize = function() {
 
 	if ( this.data ) {
 		this.initialized = true;
+		this.emit( 'change' );
 	}
 };
 
@@ -253,6 +262,91 @@ User.prototype.set = function( attributes ) {
 	}
 
 	return changed;
+};
+
+/**
+ * Called every VERIFICATION_POLL_INTERVAL milliseconds
+ * if the email is not verified.
+ *
+ * May also be called by a localStorage event, on which case
+ * the `signal` parameter is set to true.
+ *
+ * @private
+ */
+
+User.prototype.verificationPollerCallback = function( signal ) {
+	// skip server poll if page is hidden or there are no listeners
+	// and this was not triggered by a localStorage signal
+	if ( ( document.hidden || this.listeners( 'verify' ).length === 0 ) && !signal ) {
+		return;
+	}
+
+	debug( 'Verification: POLL' );
+
+	this.once( 'change', () => {
+		if ( this.get().email_verified ) {
+			// email is verified, stop polling
+			clearInterval( this.verificationPoller );
+			this.verificationPoller = null;
+			debug( 'Verification: VERIFIED' );
+			this.emit( 'verify' );
+		}
+	} );
+
+	this.fetch();
+};
+
+/**
+ * Checks if the user email is verified, and starts polling
+ * for verification if that's not the case.
+ *
+ * Also registers a listener to localStorage events.
+ *
+ * @private
+ */
+
+User.prototype.checkVerification = function() {
+	if ( !this.get() ) {
+		// not loaded, do nothing
+		return;
+	}
+
+	if ( this.get().email_verified ) {
+		// email already verified, do nothing
+		return;
+	}
+
+	if ( this.verificationPoller ) {
+		// already polling, do nothing
+		return;
+	}
+
+	this.verificationPoller = setInterval( this.verificationPollerCallback.bind( this ), VERIFICATION_POLL_INTERVAL );
+
+	// wait for localStorage event (from other windows)
+	window.addEventListener( 'storage', ( e ) => {
+		if ( e.key === '__email_verified_signal__' && e.newValue ) {
+			debug( 'Verification: RECEIVED SIGNAL' );
+			window.localStorage.removeItem( '__email_verified_signal__' );
+			this.verificationPollerCallback( true );
+		}
+	} );
+};
+
+/**
+ * Writes to local storage, signalling all other windows
+ * that the user has been verified.
+ *
+ * This should be called from the verification successful
+ * message, so that all the windows update instantaneously
+ */
+
+User.prototype.signalVerification = function() {
+	if ( window.localStorage ) {
+		// use localStorage to signal to other browser windows that the user's email was verified
+		window.localStorage.setItem( '__email_verified_signal__', 1 );
+		debug( 'Verification: SENT SIGNAL' );
+	}
 };
 
 /**
