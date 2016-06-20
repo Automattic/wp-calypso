9567ac1578ad78fc6edfe9aa14ba5efbc17633ed
Ben Lowery <blowery@users.noreply.github.com>
2016-06-20 16:30:35 -0400

    Reader: Update small post layout to put title first, simpler style (#6129)
    * Use a layout with the title on top, site name, author below
    
    * Fix link odd behavior
    
    * use new style
    
    * special case empty authors
    
    * Remove extra console.log
    
    * fix spacing
    
    * Use display_name (name) instead of nice_name
    
    * Switch to commas, on site

--- client/components/post-card/small.jsx
+++ client/components/post-card/small.jsx
@@ -5,34 +5,66 @@ import React from 'react';
 import classnames from 'classnames';
 import noop from 'lodash/noop';
 import partial from 'lodash/partial';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal Dependencies
  */
 import Card from 'components/card/compact';
-import SiteIcon from 'components/site-icon';
 import safeImageUrl from 'lib/safe-image-url';
 import resizeImageUrl from 'lib/resize-image-url';
 
-export default function SmallPostCard( { post, site, onPostClick = noop, onSiteClick = noop } ) {
+export function SmallPostCard( { translate, post, site, onPostClick = noop, onSiteClick = noop } ) {
 	const classes = classnames( 'post-card small', {
 		'has-image': post.canonical_image
 	} );
+	const displayName = post.author.name;
+	const siteName = site && site.title || post.site_name;
+
+	const username = (
+		<span className="post-card__author">
+			<a href={ `/read/blogs/${post.site_ID}` } onClick={ partial( onSiteClick, site, post ) }>{ displayName }</a>
+		</span>
+	);
+
+	const sitename = ( <span className="post-card__site-title">
+		<a href={ `/read/blogs/${post.site_ID}` } onClick={ partial( onSiteClick, site, post ) }>{ siteName }
+		</a>
+	</span> );
+
+	const thumbnailUrl = post.canonical_image && resizeImageUrl( safeImageUrl( post.canonical_image.uri ), { resize: '96,72' } );
+
 	return (
 		<Card className={ classes }>
 			<div className="post-card__site-info-title">
-				<a className="post-card__site-info" href={ `/read/blogs/${post.site_ID}` } onClick={ partial( onSiteClick, site, post ) }>
-					<SiteIcon site={ site } size={ 16 } />
-					<span className="post-card__site-title">{ site && site.title || post.site_name }</span>
-				</a>
 				<h1 className="post-card__title">
 					<a className="post-card__anchor" href={ `/read/blogs/${post.site_ID}/posts/${post.ID}` } onClick={ partial( onPostClick, post ) }>{ post.title }</a>
 				</h1>
+				<div className="post-card__site-info">
+					{
+						displayName === '' || siteName === displayName
+						? translate( 'On {{sitename/}}', {
+							components: {
+								sitename
+							}
+						} )
+						: translate( 'By {{username/}}, {{sitename/}}', {
+							components: {
+								username,
+								sitename
+							}
+						} )
+				}
+				</div>
 			</div>
 			<div>
 			{ post.canonical_image && (
-					<a href={ `/read/blogs/${post.site_ID}/posts/${post.ID}` } onClick={ partial( onPostClick, post ) }><img className="post-card__thumbnail" src={ resizeImageUrl( safeImageUrl( post.canonical_image.uri ), { resize: '96,72' } ) } /></a> ) }
+					<a href={ `/read/blogs/${post.site_ID}/posts/${post.ID}` } onClick={ partial( onPostClick, post ) }>
+						<img className="post-card__thumbnail" src={ thumbnailUrl } />
+					</a> ) }
 			</div>
 		</Card>
 	);
 }
+
+export default localize( SmallPostCard );
