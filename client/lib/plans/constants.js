8a5870ed46a95261896476002d3abc94e5967b51
Rastislav Lamos <lamos.rasto@gmail.com>
2016-06-20 19:45:48 +0200

    Plans: add main plan component — PlanFeatures (#6002)
    * Plans: add main plan component — PlanFeatures
    
    This component is dumb (ie receives props only) and will be rendered in PlanFeaturesList which will be a smart component.
    
    * Started refactoring PlanFeatures devdocs
    
    * Connected PlanFeatures to redux tree
    
    + some other minor modifications
    
    * Add PlanFeatures to plan-features devdocs ...
    
    ... and remove partial components (ie PlanFeaturesHeader)
    
    * Add all remaining features to plan constants
    
    * Fix current plan selection
    
    * Get plan description in PlanFeatures render loop
    
    Fixes problems with translations being outside the render loop. Props
    @gwwar for the idea.
    
    * Add ability to order plan features
    
    * Reference plan features in plansList constants
    
    * Fix missing popular prop of PlanFeaturesHeader
    
    * Put header banner height to a var
    
    In case we want to change header banner height, the top margin of the
    popular plan is recalculated automatically.
    
    * Use isCurrentSitePlan instead of lib/sites-list
    
    This way, we drop dependency on legacy `lib/sites-list` and
    `/sites/$site/plans` endpoint.
    
    * Add PropTypes and onUpgradeClick prop
    
    * Plan Features: namespace Sass var
    
    * PlanFeatures: Render nothing if having no data

--- client/lib/plans/constants.js
+++ client/lib/plans/constants.js
@@ -1,7 +1,9 @@
 /** @ssr-ready **/
 
+/**
+ * External dependencies
+ */
 import React from 'react';
-
 import i18n from 'i18n-calypso';
 
 // plans constants
@@ -22,14 +24,21 @@ export const PLAN_MONTHLY_PERIOD = 31;
 export const PLAN_ANNUAL_PERIOD  = 365;
 
 // features constants
-export const FEATURE_CUSTOM_DESIGN = 'custom-design';
+export const FEATURE_FREE_SITE = 'free-site';
+export const FEATURE_WP_SUBDOMAIN = 'wordpress-subdomain';
 export const FEATURE_CUSTOM_DOMAIN = 'custom-domain';
+export const FEATURE_FREE_THEMES = 'free-themes';
+export const FEATURE_UNLIMITED_PREMIUM_THEMES = 'premium-themes';
+export const FEATURE_3GB_STORAGE = '3gb-storage';
+export const FEATURE_13GB_STORAGE = '13gb-storage';
+export const FEATURE_UNLIMITED_STORAGE = 'unlimited-storage';
+export const FEATURE_COMMUNITY_SUPPORT = 'community-support';
+export const FEATURE_EMAIL_LIVE_CHAT_SUPPORT = 'email-live-chat-support';
+export const FEATURE_CUSTOM_DESIGN = 'custom-design';
 export const FEATURE_GOOGLE_ANALYTICS = 'google-analytics';
 export const FEATURE_GOOGLE_AD_CREDITS = 'google-ad-credits';
 export const FEATURE_LIVE_CHAT_SUPPORT = 'live-chat-support';
 export const FEATURE_NO_ADS = 'no-adverts';
-export const FEATURE_UNLIMITED_PREMIUM_THEMES = 'premium-themes';
-export const FEATURE_UNLIMITED_STORAGE = 'unlimited-storage';
 export const FEATURE_VIDEO_UPLOADS = 'video-upload';
 export const WORDADS_INSTANT = 'wordads-instant';
 
@@ -38,14 +47,33 @@ export const plansList = {
 		getTitle: () => i18n.translate( 'Free' ),
 		getPriceTitle: () => i18n.translate( 'Free for life' ),
 		getProductId: () => 1,
-		getDescription: () => i18n.translate( 'Get a free blog and be on your way to publishing your first post in less than five minutes.' )
+		getDescription: () => i18n.translate( 'Get a free blog and be on your way to publishing your first post in less than five minutes.' ),
+		getFeatures: () => [ // pay attention to ordering, it is used on /plan page
+			FEATURE_FREE_SITE,
+			FEATURE_WP_SUBDOMAIN,
+			FEATURE_FREE_THEMES,
+			FEATURE_3GB_STORAGE,
+			FEATURE_COMMUNITY_SUPPORT
+		]
 	},
 
 	[ PLAN_PREMIUM ]: {
 		getTitle: () => i18n.translate( 'Premium' ),
 		getPriceTitle: () => i18n.translate( '$99 per year' ),
 		getProductId: () => 1003,
-		getDescription: () => i18n.translate( 'Your own domain name, powerful customization options, lots of space for audio and video, and $100 advertising credit.' )
+		getDescription: () => i18n.translate( 'Your own domain name, powerful customization options, lots of space for audio and video, and $100 advertising credit.' ),
+		getFeatures: () => [ // pay attention to ordering, it is used on /plan page
+			FEATURE_FREE_SITE,
+			FEATURE_CUSTOM_DOMAIN,
+			FEATURE_FREE_THEMES,
+			FEATURE_13GB_STORAGE,
+			FEATURE_EMAIL_LIVE_CHAT_SUPPORT,
+			FEATURE_NO_ADS,
+			FEATURE_CUSTOM_DESIGN,
+			FEATURE_VIDEO_UPLOADS,
+			FEATURE_GOOGLE_AD_CREDITS,
+			WORDADS_INSTANT
+		]
 	},
 
 	[ PLAN_BUSINESS ]: {
@@ -54,6 +82,19 @@ export const plansList = {
 		getProductId: () => 1008,
 		getDescription: () => i18n.translate( 'Everything included with Premium, as well as live chat support, unlimited access to premium themes, and Google Analytics.' ),
 		getDescriptionWithWordAdsCredit: () => i18n.translate( 'Everything included with Premium, as well as live chat support, unlimited access to premium themes, Google Analytics, and $200 advertising credit.' ),
+		getFeatures: () => [ // pay attention to ordering, it is used on /plan page
+			FEATURE_FREE_SITE,
+			FEATURE_CUSTOM_DOMAIN,
+			FEATURE_UNLIMITED_PREMIUM_THEMES,
+			FEATURE_UNLIMITED_STORAGE,
+			FEATURE_EMAIL_LIVE_CHAT_SUPPORT,
+			FEATURE_NO_ADS,
+			FEATURE_CUSTOM_DESIGN,
+			FEATURE_VIDEO_UPLOADS,
+			FEATURE_GOOGLE_AD_CREDITS,
+			WORDADS_INSTANT,
+			FEATURE_GOOGLE_ANALYTICS
+		]
 	},
 
 	[ PLAN_JETPACK_FREE ]: {},
@@ -61,6 +102,12 @@ export const plansList = {
 };
 
 const allPaidPlans = [
+	PLAN_PREMIUM,
+	PLAN_BUSINESS
+];
+
+const allPlans = [
+	PLAN_FREE,
 	PLAN_PREMIUM,
 	PLAN_BUSINESS
 ];
@@ -101,11 +148,6 @@ export const featuresList = {
 		plans: allPaidPlans
 	},
 
-	[ FEATURE_LIVE_CHAT_SUPPORT ]: {
-		getTitle: () => i18n.translate( 'Live Chat Support' ),
-		plans: [ PLAN_BUSINESS ]
-	},
-
 	[ FEATURE_GOOGLE_AD_CREDITS ]: {
 		getTitle: () => i18n.translate( 'Advertising Credit' ),
 		getDescription: () => i18n.translate( '$100 Google AdWords credit after spending the first $25. Offer valid in US and Canada.' ),
@@ -115,12 +157,48 @@ export const featuresList = {
 				hr: <hr className="plans-compare__info-hr"/>
 			}
 		} ),
+		order: 10,
 		plans: allPaidPlans
 	},
 
 	[ WORDADS_INSTANT ]: {
 		getTitle: () => i18n.translate( 'Monetize Your Site' ),
 		getDescription: () => i18n.translate( 'Add advertising to your site through our WordAds program and get paid.' ),
+		plans: allPaidPlans
+	},
+
+	[ FEATURE_FREE_SITE ]: {
+		getTitle: () => i18n.translate( 'Free site' ),
+		plans: allPlans
+	},
+
+	[ FEATURE_WP_SUBDOMAIN ]: {
+		getTitle: () => i18n.translate( 'WordPress.com subdomain' ),
+		plans: [ PLAN_FREE ]
+	},
+
+	[ FEATURE_FREE_THEMES ]: {
+		getTitle: () => i18n.translate( 'Hundreds of free themes' ),
+		plans: [ PLAN_FREE, PLAN_PREMIUM ]
+	},
+
+	[ FEATURE_3GB_STORAGE ]: {
+		getTitle: () => i18n.translate( '3GB of storage' ),
+		plans: [ PLAN_FREE ]
+	},
+
+	[ FEATURE_13GB_STORAGE ]: {
+		getTitle: () => i18n.translate( '13GB of storage' ),
+		plans: [ PLAN_PREMIUM ]
+	},
+
+	[ FEATURE_COMMUNITY_SUPPORT ]: {
+		getTitle: () => i18n.translate( 'Community support' ),
+		plans: [ PLAN_FREE ]
+	},
+
+	[ FEATURE_EMAIL_LIVE_CHAT_SUPPORT ]: {
+		getTitle: () => i18n.translate( 'Email and live chat support' ),
 		plans: allPaidPlans
 	}
 };
@@ -134,4 +212,12 @@ export const getPlanObject = planName => {
 	} );
 
 	return objectPlan;
+};
+
+export const getPlanFeaturesObject = planName => {
+	const planFeaturesList = plansList[ planName ].getFeatures();
+
+	return planFeaturesList.map( featureConstant =>
+		featuresList[ featureConstant ]
+	);
 };
