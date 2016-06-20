8df6d1f98eb478c436e508b1a09fb1727112c815
Bernhard Reiter <ockham@raz.or.at>
2016-06-21 00:20:23 +0200

    Current Theme Card: Use getDetailsUrl and getSupportUrl selectors

--- client/my-sites/themes/current-theme/index.jsx
+++ client/my-sites/themes/current-theme/index.jsx
@@ -12,12 +12,11 @@ import Card from 'components/card';
 import CurrentThemeButton from './button';
 import {
 	getCustomizeUrl,
-	getDetailsUrl,
-	getSupportUrl,
 	isPremium,
 	trackClick
 } from '../helpers';
 import { getCurrentTheme } from 'state/themes/current-theme/selectors';
+import { getThemeDetailsUrl, getThemeSupportUrl } from 'state/themes/themes/selectors';
 import QueryCurrentTheme from 'components/data/query-current-theme';
 
 /**
@@ -27,12 +26,15 @@ import QueryCurrentTheme from 'components/data/query-current-theme';
 const CurrentTheme = React.createClass( {
 
 	propTypes: {
-		currentTheme: React.PropTypes.object,
 		site: React.PropTypes.oneOfType( [
 			React.PropTypes.object,
 			React.PropTypes.bool
 		] ).isRequired,
-		canCustomize: React.PropTypes.bool.isRequired
+		canCustomize: React.PropTypes.bool.isRequired,
+		// connected props
+		currentTheme: React.PropTypes.object,
+		detailsUrl: React.PropTypes.string,
+		supportUrl: React.PropTypes.string
 	},
 
 	trackClick: trackClick.bind( null, 'current theme' ),
@@ -66,12 +68,12 @@ const CurrentTheme = React.createClass( {
 					<CurrentThemeButton name="details"
 						label={ this.translate( 'Details' ) }
 						noticon="info"
-						href={ currentTheme ? getDetailsUrl( currentTheme, site ) : null }
+						href={ currentTheme ? this.props.detailsUrl : null }
 						onClick={ this.trackClick } />
 					{ displaySupportButton && <CurrentThemeButton name="support"
 						label={ this.translate( 'Setup' ) }
 						noticon="help"
-						href={ currentTheme ? getSupportUrl( currentTheme, site ) : null }
+						href={ currentTheme ? this.props.supportUrl : null }
 						onClick={ this.trackClick } /> }
 				</div>
 			</Card>
@@ -80,7 +82,13 @@ const CurrentTheme = React.createClass( {
 } );
 
 export default connect(
-	( state, props ) => (
-		{ currentTheme: getCurrentTheme( state, props.site && props.site.ID ) }
-	)
+	( state, props ) => {
+		const currentTheme = getCurrentTheme( state, props.site && props.site.ID );
+
+		return {
+			currentTheme,
+			detailsUrl: getThemeDetailsUrl( state, currentTheme ),
+			supportUrl: getThemeSupportUrl( state, currentTheme )
+		};
+	}
 )( CurrentTheme );
