5c9d9f9f4468ccfe671bc11f1d08a57108ffec1d
Ben Lowery <ben.lowery@automattic.com>
2016-06-21 10:38:39 -0400

    Reader: Release Related Posts to everyone

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -119,13 +119,4 @@ module.exports = {
 		defaultVariation: 'enabled',
 		allowExistingUsers: false,
 	},
-	readerRelatedPosts: {
-		datestamp: '20160620',
-		variations: {
-			disabled: 90,
-			enabled: 10
-		},
-		defaultVariation: 'disabled',
-		allowExistingUsers: true
-	}
 };
