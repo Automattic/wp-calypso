132900198e4546eacd9a3fb0fd7abca86c878119
Dan Roundhill <dan@automattic.com>
2016-06-22 15:18:03 -0700

    Advanced SEO: Add SEO Accordion to Editor (#6069)
    * Adds non-functional SEO accordion to the editor.
    
    Refs #5834
    
    * Updates based on feedback:
    
    * Extends React `component` class.
    * Pass siteId to component instead of the whole object, and use redux store's `getSiteSlug` selector.
    * Removed some unused code.
    * Updated naming to match guidelines.
    * Uses new `localize` method for translations.
    
    * Renamed class to `EditorSeoAccordion`, which means we no longer need to set `displayName`, and can remove the constructor.
    
    Also matched up the CSS naming to match propery with the new class name.
    
    * Fixed stylesheet path
    
    * A few more changes based on review feedback:
    
     * Use `getSelectedSiteId` from state instead of passing a siteId prop.
     * Added more propTypes
     * Used just `disabled` to disable the `TokenField`.
    
    * Updated strings and fixed seo settings tab url.
    
    * Use a stateless function for the component.

---
+++ client/post-editor/editor-seo-accordion/index.jsx
@@ -0,0 +1,79 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Accordion from 'components/accordion';
+import AccordionSection from 'components/accordion/section';
+import CountedTextarea from 'components/forms/counted-textarea';
+import Gridicon from 'components/gridicon';
+import InfoPopover from 'components/info-popover';
+import TokenField from 'components/token-field';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { getSiteSlug } from 'state/sites/selectors';
+
+function EditorSeoAccordion( props ) {
+	const { translate, siteSlug } = props;
+	// Temporary placeholder chips for design review
+	const sampleChips = [ 'Post Title', 'Site Title' ];
+
+	const seoTabUrl = `/settings/seo/${ siteSlug }`;
+
+	return (
+		<Accordion
+			title={ translate( 'Advanced SEO' ) }
+			icon={ <Gridicon icon="search" /> }
+			className="editor-seo-accordion"
+		>
+			<AccordionSection>
+				<span className="editor-drawer__label-text">
+					{ translate( 'Meta Title' ) }
+					<InfoPopover position="top left">
+						{ translate(
+							'To edit the format of the meta title, go to your site\'s {{a}}SEO settings{{/a}}.',
+							{
+								components: {
+									a: <a target="_blank" href={ seoTabUrl } />
+								}
+							}
+						) }
+					</InfoPopover>
+				</span>
+				<TokenField value={ sampleChips } disabled />
+			</AccordionSection>
+			<AccordionSection>
+				<span className="editor-drawer__label-text">
+					{ translate( 'Meta Description' ) }
+					<InfoPopover position="top left">
+						{ translate(
+							'Craft a description of your post for search engine results. ' +
+							'The post content is used by default.'
+						) }
+					</InfoPopover>
+				</span>
+				<CountedTextarea
+					maxLength="300"
+					acceptableLength={ 159 }
+					placeholder={ translate( 'Write a description…' ) }
+					aria-label={ translate( 'Write a description…' ) }
+				/>
+			</AccordionSection>
+		</Accordion>
+	);
+}
+
+EditorSeoAccordion.propTypes = {
+	translate: PropTypes.func,
+	siteSlug: PropTypes.string
+};
+
+const mapStateToProps = ( state ) => ( {
+	siteSlug: getSiteSlug( state, getSelectedSiteId( state ) )
+} );
+
+export default connect( mapStateToProps )( localize( EditorSeoAccordion ) );
