8b95c771eb85b5b1d7d6ee6129ac57b548b43bb0
Bernhard Reiter <ockham@raz.or.at>
2016-06-23 17:53:03 +0200

    Theme Sheet: Remove /overview slug for default section (#6250)
    Enable access to the theme sheet's default section by just going to `/theme/themename` instead of `/theme/themename/overview`. This is done by changing some object keys and array items to `''`, and by modifying our route handlers to explicitly specify possible section names. This way, validation is done by the route handler, and it won't take `sitename` in a URL like `/theme/themename/sitename` for a section.
    
    If in the latter example `sidename` is invalid, `my-sites`’s `siteSelection` middleware will take care of that and set the selected site to null (i.e. all sites), which is communicated to the theme sheet via the `getSelectedSite` selector.
    
    This leads to the following behavior for invalid slugs:
    * /theme/mood/gsafa displays the 'Overview' section in multi-site mode
    * /theme/mood/support/ghfgsg displays the 'Support' section in multi-site mode (when activating/purchasing, site selector is shown)
    * /theme/mood/fdjshdrg/hrgefqw 404s
    
    Note that `validateSections` is only needed for one edge case now that's not already filtered by the route, and that is a request for the 'Setup' section for a theme that doesn't have one (i.e. a free one, e.g. `/theme/twentysixteen/setup`). (The route handler can't do that for us since at routing stage, we don' know yet if a theme is free or premium.)
    
    Fixes #6216

--- client/my-sites/theme/index.node.js
+++ client/my-sites/theme/index.node.js
@@ -7,6 +7,6 @@ import { details, fetchThemeDetailsData } from './controller';
 
 export default function( router ) {
 	if ( config.isEnabled( 'manage/themes/details' ) ) {
-		router( '/theme/:slug/:section?/:site_id?', fetchThemeDetailsData, details, makeLayout );
+		router( '/theme/:slug/:section(setup|support)?/:site_id?', fetchThemeDetailsData, details, makeLayout );
 	}
 }
