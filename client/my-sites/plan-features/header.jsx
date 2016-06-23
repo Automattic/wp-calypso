39bc349ce0dbbcc04628229aa6c0e2da4cafb51d
Kerry Liu <kerryliu@gmail.com>
2016-06-23 09:59:02 -0700

    Plans: plans feature price should not render if currencyCode or rawPrice is not provided

--- client/my-sites/plan-features/header.jsx
+++ client/my-sites/plan-features/header.jsx
@@ -49,7 +49,7 @@ class PlanFeaturesHeader extends Component {
 				</div>
 				<div className="plan-features__header-text">
 					<h4 className="plan-features__header-title">{ title }</h4>
-					{ currencyCode && <PlanFeaturesPrice currencyCode={ currencyCode } rawPrice={ rawPrice } /> }
+					<PlanFeaturesPrice currencyCode={ currencyCode } rawPrice={ rawPrice } />
 					<p className="plan-features__header-timeframe">
 						{ billingTimeFrame }
 					</p>
