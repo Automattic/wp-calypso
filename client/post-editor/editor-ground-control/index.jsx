5b1320950cbca3880eedaf0d12a549466e7b0a37
Marco Aurelio <thecoreh@gmail.com>
2016-06-23 17:08:46 -0300

    Add missing space

--- client/post-editor/editor-ground-control/index.jsx
+++ client/post-editor/editor-ground-control/index.jsx
@@ -66,7 +66,7 @@ export default React.createClass( {
 	},
 
 	componentDidMount: function() {
-		if ( !this.props.user ) {
+		if ( ! this.props.user ) {
 			return;
 		}
 
@@ -76,7 +76,7 @@ export default React.createClass( {
 	},
 
 	componentWillUnmount: function() {
-		if ( !this.props.user ) {
+		if ( ! this.props.user ) {
 			return;
 		}
 
