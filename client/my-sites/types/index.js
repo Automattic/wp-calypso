fc821f9c80a27c27ba1440066bee40e49d261570
Andrew Duthie <andrew@andrewduthie.com>
2016-06-24 16:40:46 -0400

    CPT: Render list screen using isomorphic route middleware

--- client/my-sites/types/index.js
+++ client/my-sites/types/index.js
@@ -1,21 +1,16 @@
 /**
- * External dependencies
- */
-import page from 'page';
-
-/**
  * Internal dependencies
  */
 import { siteSelection, navigation, sites } from 'my-sites/controller';
 import { list, redirect } from './controller';
 import config from 'config';
 
-export default function() {
+export default function( router ) {
 	if ( ! config.isEnabled( 'manage/custom-post-types' ) ) {
 		return;
 	}
 
-	page( '/types/:type/:status?/:site', siteSelection, navigation, list );
-	page( '/types/:type', siteSelection, sites );
-	page( '/types', redirect );
-};
+	router( '/types/:type/:status?/:site', siteSelection, navigation, list );
+	router( '/types/:type', siteSelection, sites );
+	router( '/types', redirect );
+}
