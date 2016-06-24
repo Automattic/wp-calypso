8e780c59bf543fb47d5a4c25175ca0259e76f9c2
Andrew Duthie <andrew@andrewduthie.com>
2016-06-24 16:47:27 -0400

    CPT: Track page view using PageViewTracker

--- client/my-sites/types/controller.jsx
+++ client/my-sites/types/controller.jsx
@@ -7,8 +7,6 @@ import page from 'page';
 /**
  * Internal Dependencies
  */
-import { getSiteFragment, sectionify } from 'lib/route';
-import { pageView } from 'lib/analytics';
 import Types from './main';
 
 export function redirect() {
@@ -16,16 +14,6 @@ export function redirect() {
 }
 
 export function list( context, next ) {
-	const siteId = getSiteFragment( context.path );
-	const sectionedPath = sectionify( context.path );
-
-	// Analytics
-	let baseAnalyticsPath = sectionedPath;
-	if ( siteId ) {
-		baseAnalyticsPath += '/:site';
-	}
-	pageView.record( baseAnalyticsPath, 'Custom Post Type' );
-
 	context.primary = (
 		<Types query={ {
 			type: context.params.type,
