ff44596d8ec2415579fc8116897aed91a5f99166
Andrew Duthie <andrew@andrewduthie.com>
2016-06-24 13:52:06 -0400

    State: Restructure notices state as object

--- client/state/notices/test/reducer.js
+++ client/state/notices/test/reducer.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
 
 /**
  * Internal dependencies
@@ -9,63 +10,97 @@ import { expect } from 'chai';
 import {
 	NEW_NOTICE,
 	REMOVE_NOTICE,
-	SERIALIZE,
-	DESERIALIZE
+	ROUTE_SET
 } from 'state/action-types';
 import { items } from '../reducer';
 
 describe( 'reducer', () => {
 	describe( '#items()', () => {
-		it( 'should default to an empty array', () => {
-			const state = items( undefined, [] );
-			expect( state ).to.eql( [] );
+		it( 'should default to an empty object', () => {
+			const state = items( undefined, {} );
+			expect( state ).to.eql( {} );
 		} );
 
 		it( 'should properly add new notice', () => {
-			const notice = { text: 'Example Notice Text' },
-				state = items( undefined, {
-					type: NEW_NOTICE,
-					notice: notice
-				} );
+			const notice = { noticeId: 1, text: 'Example Notice Text' };
+			const original = deepFreeze( {} );
+			const state = items( original, {
+				type: NEW_NOTICE,
+				notice: notice
+			} );
 
-			expect( state ).to.eql( [ notice ] );
+			expect( state ).to.eql( {
+				1: notice
+			} );
 		} );
 
 		it( 'should properly remove selected notice', () => {
-			const notices = [
-					{ noticeId: 1 },
-					{ noticeId: 2 },
-					{ noticeId: 3 }
-				],
-				state = items( notices, {
-					type: REMOVE_NOTICE,
-					noticeId: 2
-				} );
+			const original = deepFreeze( {
+				1: { noticeId: 1 },
+				2: { noticeId: 2 },
+				3: { noticeId: 3 }
+			} );
+			const state = items( original, {
+				type: REMOVE_NOTICE,
+				noticeId: 2
+			} );
 
-			expect( state ).to.eql( [
-				{ noticeId: 1 },
-				{ noticeId: 3 }
-			] );
+			expect( state ).to.eql( {
+				1: { noticeId: 1 },
+				3: { noticeId: 3 }
+			} );
 		} );
 
-		it( 'never persists state', () => {
-			const notices = [
-				{ noticeId: 1 },
-				{ noticeId: 2 },
-				{ noticeId: 3 }
-			];
-			const state = items( notices, { type: SERIALIZE } );
-			expect( state ).to.eql( [] );
+		it( 'should return same state on remove attempt if notice not tracked', () => {
+			const original = deepFreeze( {
+				1: { noticeId: 1 },
+				3: { noticeId: 3 }
+			} );
+			const state = items( original, {
+				type: REMOVE_NOTICE,
+				noticeId: 2
+			} );
+
+			expect( state ).to.equal( original );
 		} );
 
-		it( 'never loads persisted state', () => {
-			const notices = [
-				{ noticeId: 1 },
-				{ noticeId: 2 },
-				{ noticeId: 3 }
-			];
-			const state = items( notices, { type: DESERIALIZE } );
-			expect( state ).to.eql( [] );
+		it( 'should remove notices on route set', () => {
+			const original = deepFreeze( {
+				1: { noticeId: 1 }
+			} );
+			const state = items( original, {
+				type: ROUTE_SET
+			} );
+
+			expect( state ).to.eql( {} );
+		} );
+
+		it( 'should preserve persistent notices on route set', () => {
+			const original = deepFreeze( {
+				1: { noticeId: 1 },
+				2: { noticeId: 2, isPersistent: true }
+			} );
+			const state = items( original, {
+				type: ROUTE_SET
+			} );
+
+			expect( state ).to.eql( {
+				2: { noticeId: 2, isPersistent: true }
+			} );
+		} );
+
+		it( 'should preserve altered notice to be displayed on next page on route set', () => {
+			const original = deepFreeze( {
+				1: { noticeId: 1 },
+				2: { noticeId: 2, displayOnNextPage: true }
+			} );
+			const state = items( original, {
+				type: ROUTE_SET
+			} );
+
+			expect( state ).to.eql( {
+				2: { noticeId: 2, displayOnNextPage: false }
+			} );
 		} );
 	} );
 } );
