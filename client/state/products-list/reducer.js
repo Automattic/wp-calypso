30ffed4071ebc89a3d30cdeef22ed4a10bb9bc6b
Jordan West <jordwest@gmail.com>
2016-06-27 16:02:47 +1000

    Use state/utils/createReducer to simplify

--- client/state/products-list/reducer.js
+++ client/state/products-list/reducer.js
@@ -10,57 +10,21 @@ import {
 	PRODUCTS_LIST_RECEIVE,
 	PRODUCTS_LIST_REQUEST,
 	PRODUCTS_LIST_REQUEST_FAILURE,
-	DESERIALIZE,
-	SERIALIZE,
 } from 'state/action-types';
 import { productsListSchema } from './schema';
-import { isValidStateWithSchema } from 'state/utils';
+import { createReducer } from 'state/utils';
 
-/**
- * Stores the complete list of products, indexed by the product key
- *
- * @param  {Object} state  Current state
- * @param  {Object} action Action payload
- * @return {Object}        Updated state
- */
-export function items( state = null, action ) {
-	switch ( action.type ) {
-		case PRODUCTS_LIST_RECEIVE:
-			return action.productsList;
+// Stores the complete list of products, indexed by the product key
+export const items = createReducer( null, {
+	[ PRODUCTS_LIST_RECEIVE ]: ( state, action ) => action.productsList,
+}, productsListSchema );
 
-		case SERIALIZE:
-			return state;
-
-		case DESERIALIZE:
-			if ( state !== null && isValidStateWithSchema( state, productsListSchema ) ) {
-				return state;
-			}
-			return null;
-	}
-
-	return state;
-}
-
-/**
- * Tracks sites fetching state
- *
- * @param  {Object} state  Current state
- * @param  {Object} action Action payload
- * @return {Object}        Updated state
- */
-export function isFetching( state = false, action ) {
-	switch ( action.type ) {
-		case PRODUCTS_LIST_REQUEST:
-			return true;
-		case PRODUCTS_LIST_RECEIVE:
-		case PRODUCTS_LIST_REQUEST_FAILURE:
-			return false;
-		case SERIALIZE:
-		case DESERIALIZE:
-			return false;
-	}
-	return state;
-}
+// Tracks product list fetching state
+export const isFetching = createReducer( false, {
+	[ PRODUCTS_LIST_REQUEST ]: () => true,
+	[ PRODUCTS_LIST_RECEIVE ]: () => false,
+	[ PRODUCTS_LIST_REQUEST_FAILURE ]: () => false
+} );
 
 export default combineReducers( {
 	isFetching,
