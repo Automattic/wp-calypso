f4951c41010c2ffab2c89ea96f5e6fb55c76f31b
Andrew Duthie <andrew@andrewduthie.com>
2016-06-28 11:19:28 -0400

    CPT: Remove property specific selectors in favor of object select

--- client/my-sites/post-type-list/post-actions.jsx
+++ client/my-sites/post-type-list/post-actions.jsx
@@ -8,22 +8,26 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
-import { getPostId, getPostSiteId, getPostUrl, getPostStatus } from 'state/posts/selectors';
+import { getPost } from 'state/posts/selectors';
 import { trashPost, deletePost } from 'state/posts/actions';
 import Button from 'components/button';
 import Gridicon from 'components/gridicon';
 
-function PostTypeListPostActions( props ) {
+function PostTypeListPostActions( { translate, post, dispatchTrashPost, dispatchDeletePost } ) {
 	function onTrash() {
+		if ( ! post ) {
+			return;
+		}
+
 		let dispatchAction;
-		if ( 'trash' !== props.postStatus ) {
-			dispatchAction = props.trashPost;
-		} else if ( confirm( props.translate( 'Are you sure you want to permanently delete this post?' ) ) ) {
-			dispatchAction = props.deletePost;
+		if ( 'trash' !== post.status ) {
+			dispatchAction = dispatchTrashPost;
+		} else if ( confirm( translate( 'Are you sure you want to permanently delete this post?' ) ) ) {
+			dispatchAction = dispatchDeletePost;
 		}
 
 		if ( dispatchAction ) {
-			dispatchAction( props.postSiteId, props.postId );
+			dispatchAction( post.site_ID, post.ID );
 		}
 	}
 
@@ -32,15 +36,15 @@ function PostTypeListPostActions( props ) {
 			<Button onClick={ onTrash } borderless>
 				<Gridicon icon="trash" />
 				<span className="screen-reader-text">
-					{ 'trash' === props.postStatus
-						? props.translate( 'Delete Permanently' )
-						: props.translate( 'Trash', { context: 'verb' } ) }
+					{ post && 'trash' === post.status
+						? translate( 'Delete Permanently' )
+						: translate( 'Trash', { context: 'verb' } ) }
 				</span>
 			</Button>
-			<Button href={ props.postUrl } target="_blank" borderless>
+			<Button href={ post ? post.URL : '' } target="_blank" borderless>
 				<Gridicon icon="external" />
 				<span className="screen-reader-text">
-					{ props.translate( 'View', { context: 'verb' } ) }
+					{ translate( 'View', { context: 'verb' } ) }
 				</span>
 			</Button>
 		</div>
@@ -50,19 +54,19 @@ function PostTypeListPostActions( props ) {
 PostTypeListPostActions.propTypes = {
 	globalId: PropTypes.string,
 	translate: PropTypes.func,
-	trash: PropTypes.func,
-	postUrl: PropTypes.string,
-	postStatus: PropTypes.string
+	post: PropTypes.object,
+	trashPost: PropTypes.func,
+	deletePost: PropTypes.func
 };
 
 export default connect(
 	( state, ownProps ) => {
 		return {
-			postId: getPostId( state, ownProps.globalId ),
-			postSiteId: getPostSiteId( state, ownProps.globalId ),
-			postUrl: getPostUrl( state, ownProps.globalId ),
-			postStatus: getPostStatus( state, ownProps.globalId )
+			post: getPost( state, ownProps.globalId )
 		};
 	},
-	{ trashPost, deletePost }
+	{
+		dispatchTrashPost: trashPost,
+		dispatchDeletePost: deletePost
+	}
 )( localize( PostTypeListPostActions ) );
