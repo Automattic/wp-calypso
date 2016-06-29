e24e4362432415deb776cb0e8d23347d7bfb9429
Tugdual de Kerviler <dekervit@gmail.com>
2016-06-29 15:23:53 +0200

    Framework: Stored Cards: Add selectors

---
+++ client/state/stored-cards/selectors.js
@@ -0,0 +1,17 @@
+/**
+ * Return user's stored cards from state object
+ *
+ * @param {Object} state - current state object
+ * @return {Array} Stored Cards
+ */
+export const getCards = state => state.storedCards.items;
+
+/**
+ * Returns a Stored Card
+ * @param  {Object} state      global state
+ * @param  {Number} cardId  the card id
+ * @return {Object} the matching card if there is one
+ */
+export const getByCardId = ( state, cardId ) => (
+	getCards( state ).filter( card => card.id === cardId ).shift()
+);
