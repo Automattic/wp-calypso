f2df206fee8dd81d285fdbc6e3faaf4b981a9f06
Tugdual de Kerviler <dekervit@gmail.com>
2016-06-29 15:23:13 +0200

    Framework: Stored Cards: Add assembler

---
+++ client/state/stored-cards/assembler.js
@@ -0,0 +1,11 @@
+export const createStoredCard = card => {
+	return {
+		id: Number( card.stored_details_id ),
+		expiry: card.expiry,
+		number: Number( card.card ),
+		type: card.card_type,
+		name: card.name
+	};
+};
+
+export const createStoredCardsArray = ( dataTransferArray = [] ) => dataTransferArray.map( createStoredCard );
