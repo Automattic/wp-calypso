7ee306b81d572b185a64e9dd4c6f7b6867ca38fd
Andrew Duthie <andrew@andrewduthie.com>
2016-06-30 11:22:47 -0400

    CPT: Enable customization of actions items

--- client/my-sites/post-type-list/post-actions-ellipsis-menu/index.jsx
+++ client/my-sites/post-type-list/post-actions-ellipsis-menu/index.jsx
@@ -1,32 +1,56 @@
 /**
  * External dependencies
  */
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Children, cloneElement } from 'react';
 
 /**
  * Internal dependencies
  */
 import EllipsisMenu from 'components/ellipsis-menu';
+import PopoverMenuSeparator from 'components/popover/menu-separator';
 import PostActionsEllipsisMenuEdit from './edit';
 import PostActionsEllipsisMenuPublish from './publish';
 import PostActionsEllipsisMenuTrash from './trash';
 import PostActionsEllipsisMenuView from './view';
 import PostActionsEllipsisMenuRestore from './restore';
 
-export default function PostActionsEllipsisMenu( { globalId } ) {
+export default function PostActionsEllipsisMenu( { globalId, includeDefaultActions, children } ) {
+	let actions = [];
+
+	if ( includeDefaultActions ) {
+		actions.push(
+			<PostActionsEllipsisMenuView key="view" />,
+			<PostActionsEllipsisMenuPublish key="publish" />,
+			<PostActionsEllipsisMenuEdit key="edit" />,
+			<PostActionsEllipsisMenuRestore key="restore" />,
+			<PostActionsEllipsisMenuTrash key="trash" />
+		);
+	}
+
+	children = Children.toArray( children );
+	if ( children.length ) {
+		if ( actions.length ) {
+			actions.push( <PopoverMenuSeparator key="separator" /> );
+		}
+
+		actions = actions.concat( children );
+	}
+
 	return (
 		<div className="post-actions-ellipsis-menu">
 			<EllipsisMenu position="bottom left">
-				<PostActionsEllipsisMenuView globalId={ globalId } />
-				<PostActionsEllipsisMenuPublish globalId={ globalId } />
-				<PostActionsEllipsisMenuEdit globalId={ globalId } />
-				<PostActionsEllipsisMenuRestore globalId={ globalId } />
-				<PostActionsEllipsisMenuTrash globalId={ globalId } />
+				{ actions.map( ( action ) => cloneElement( action, { globalId } ) ) }
 			</EllipsisMenu>
 		</div>
 	);
 }
 
 PostActionsEllipsisMenu.propTypes = {
-	globalId: PropTypes.string
+	globalId: PropTypes.string,
+	includeDefaultActions: PropTypes.bool,
+	children: PropTypes.node
+};
+
+PostActionsEllipsisMenu.defaultProps = {
+	includeDefaultActions: true
 };
