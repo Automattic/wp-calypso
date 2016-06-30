dc6f01f485cb6c0aaee2a1c6284c80c1a1760713
kwight <kwight@kwight.ca>
2016-06-29 17:00:24 -0700

    Signup: Add an AB test for offering a Pressable store option in the design step.

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -139,4 +139,13 @@ module.exports = {
 		defaultVariation: 'disabled',
 		allowExistingUsers: true,
 	},
+	signupStore: {
+		datestamp: '20160629',
+		variations: {
+			disabled: 70,
+			enabled: 30,
+		},
+		defaultVariation: 'disabled',
+		allowExistingUsers: true,
+	},
 };
