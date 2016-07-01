2e33fab095469f2728709cc96cd0b3d3a7ca6f17
Ben Lowery <blowery@users.noreply.github.com>
2016-07-01 11:53:29 -0500

    State: Stop altering the original site schema in reader/sites (#6409)
    Properly clone out the pattern object before adding in the new property

--- client/state/reader/sites/schema.js
+++ client/state/reader/sites/schema.js
@@ -1,5 +1,16 @@
 import { sitesSchema } from 'state/sites/schema';
 
-export const readerSitesSchema = Object.assign( {}, sitesSchema );
-
-readerSitesSchema.patternProperties['^\\d+$'].properties.feed_ID = { type: 'number' };
+// we're based on the normal site endpoint schema, but we want to add in a feed_ID property
+export const readerSitesSchema = {
+	...sitesSchema,
+	patternProperties: {
+		...sitesSchema.patternProperties,
+		[ '^\\d+$' ]: {
+			...sitesSchema.patternProperties[ '^\\d+$' ],
+			properties: {
+				...sitesSchema.patternProperties[ '^\\d+$' ].properties,
+				feed_ID: { type: 'number ' }
+			}
+		}
+	}
+};
