0437dd17234855994cf86ce6b124f1ab944c2654
retrofox <rdsuarez@gmail.com>
2016-07-01 19:31:19 +0200

    plan-features: add tooltip info if the feature has description

--- client/my-sites/plan-features/item.jsx
+++ client/my-sites/plan-features/item.jsx
@@ -7,12 +7,30 @@ import React from 'react';
  * Internal dependencies
  */
 import Gridicon from 'components/gridicon';
+import InfoPopover from 'components/info-popover';
 
-export default function PlanFeaturesItem( { children } ) {
+export default function PlanFeaturesItem( { description, children } ) {
+	const renderTipinfo = () => {
+		if ( ! description ) {
+			return null;
+		}
+
+		return (
+			<div className="plan-features__item-tip-info">
+				<InfoPopover
+					position="right"
+				>
+					{ description }
+				</InfoPopover>
+			</div>
+		);
+	};
+
 	return (
 		<li className="plan-features__item">
 			<Gridicon className="plan-features__item-checkmark" size={ 18 } icon="checkmark" />
 			{ children }
+			{ renderTipinfo() }
 		</li>
 	);
 }
