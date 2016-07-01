f6d042070ac12ea6eda1d73c4e48730a822cbe66
Marco Aurélio <thecoreh@gmail.com>
2016-07-01 14:14:47 -0300

    Add “store” signup step (#6350)
    * Add initial implementation of store signup step
    
    * Rename store step to pressable-store
    
    * Rename react class.
    
    * Update config to point to new location.
    
    * Rename CSS classes
    
    * Use descriptive variable name.
    
    * Add link to Pressable Privacy Policy
    
    * Fix import in SCSS
    
    * Remove reference to SignupActions
    
    * Add heading style and hero image
    
    * Use actual referal URL
    
    * Tweak style to better match mock up
    
    * Add autofocus to pressable form
    
    * Update copy
    
    * Remove analytics reference
    
    * Add email validation
    
    * Reuse string and add external gridicon
    
    * Remove extraneous space
    
    * Cleanup code according to feedback
    
    * Tweak margins to match spacing in the mockup (which conforms to our 8px grid)
    
    * Avoid two setState calls.
    
    * Avoid using nested rules in CSS
    
    * Remove unused prop with filler text
    
    * Rename valid -> isValid
    
    * Add reactive heading margin
    
    * Move handler to form onSubmit event
    
    * Update URL to add GA params

---
+++ client/signup/steps/pressable-store/index.jsx
@@ -0,0 +1,115 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import EmailValidator from 'email-validator';
+
+/**
+ * Internal dependencies
+ */
+import StepWrapper from 'signup/step-wrapper';
+import Gridicon from 'components/gridicon';
+
+import LoggedOutForm from 'components/logged-out-form';
+import LoggedOutFormFooter from 'components/logged-out-form/footer';
+import LoggedOutFormLinks from 'components/logged-out-form/links';
+import LoggedOutFormLinkItem from 'components/logged-out-form/link-item';
+import FormTextInput from 'components/forms/form-text-input';
+import FormInputValidation from 'components/forms/form-input-validation';
+import FormButton from 'components/forms/form-button';
+import FormLabel from 'components/forms/form-label';
+import FormSectionHeading from 'components/forms/form-section-heading';
+
+import HeroImage from './hero-image';
+
+export default React.createClass( {
+	displayName: 'PressableStoreStep',
+
+	propTypes: {
+		stepName: PropTypes.string.isRequired,
+		goToNextStep: PropTypes.func.isRequired,
+		signupDependencies: PropTypes.object.isRequired,
+	},
+
+	getInitialState() {
+		return {
+			email: '',
+			isValid: false,
+			error: null,
+		};
+	},
+
+	componentDidMount() {
+		if ( this._input ) {
+			this._input.focus();
+		}
+	},
+
+	onEmailChange( event ) {
+		const email = event.target.value;
+		const isValid = EmailValidator.validate( email );
+		const error = this.state.error && isValid
+			? null
+			: this.state.error;
+		this.setState( {
+			email,
+			isValid,
+			error,
+		} );
+	},
+
+	onSubmit( event ) {
+		event.preventDefault();
+
+		if ( ! this.state.isValid ) {
+			this.setState( {
+				error: this.translate( 'Please provide a valid email address.' ),
+			} );
+			return;
+		}
+
+		window.location.href = `https://my.pressable.com/signup/ecommerce-five-sites?email=${ encodeURIComponent( this.state.email ) }&utm_source=wordpresscom&utm_medium=signupref&utm_campaign=wpcomecomm1`;
+	},
+
+	onEmailInputRef( input ) {
+		this._input = input;
+	},
+
+	renderStoreForm() {
+		return (
+			<div>
+				<LoggedOutForm className="pressable-store__form" onSubmit={ this.onSubmit }>
+					<HeroImage />
+
+					<FormSectionHeading className="pressable-store__heading">{ this.translate( 'Get your store for as low as $25 / month' ) }</FormSectionHeading>
+					<p className="pressable-store__copy">{ this.translate( 'We\'ve partnered with Pressable, a top-notch WordPress hosting provider, and WooCommerce, the go-to eCommerce solution for WordPress, to make setting up your store a snap.' ) }</p>
+
+					<LoggedOutFormFooter>
+						<FormLabel className="pressable-store__form-label" for="email">{ this.translate( 'Start by entering your email address:' ) }</FormLabel>
+						<div className="pressable-store__form-fields">
+							<FormTextInput ref={ this.onEmailInputRef } isError={ this.state.error } isValid={ this.state.isValid } onChange={ this.onEmailChange } className="pressable-store__form-email is-spaced" type="email" placeholder="Email Address" name="email" />
+							<FormButton className="pressable-store__form-submit">{ this.translate( 'Get started on Pressable' ) } <Gridicon icon="external" size={ 12 } /></FormButton>
+						</div>
+						{ this.state.error && <FormInputValidation isError={ true } text={ this.state.error } /> }
+					</LoggedOutFormFooter>
+				</LoggedOutForm>
+				<LoggedOutFormLinks>
+					<LoggedOutFormLinkItem className="pressable-store__privacy-policy" target="__blank" href="https://pressable.com/legal/privacy-policy/">
+						{ this.translate( 'Pressable Privacy Policy', { comment: '“Pressable” is the name of a WordPress.org hosting provider' } ) } <Gridicon icon="external" size={ 12 } />
+					</LoggedOutFormLinkItem>
+				</LoggedOutFormLinks>
+			</div>
+		);
+	},
+
+	render() {
+		return (
+			<StepWrapper
+				fallbackHeaderText={ this.translate( 'Create your WordPress Store' ) }
+				fallbackSubHeaderText={ this.translate( 'Our partners at Pressable and WooCommerce are here for you' ) }
+				stepContent={ this.renderStoreForm() }
+				{ ...this.props }
+				goToNextStep={ undefined } />
+		);
+	}
+} );
