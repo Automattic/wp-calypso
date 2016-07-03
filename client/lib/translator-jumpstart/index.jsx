4ffb0235a6a16fcf1b53e29926d15249f10f42a9
Yoav Farhi <yoav@farhi.org>
2016-07-03 14:11:56 +0300

    Use object methods shorthands

--- client/lib/translator-jumpstart/index.jsx
+++ client/lib/translator-jumpstart/index.jsx
@@ -24,7 +24,7 @@ const user = new User(),
 		localeCode: 'en',
 		languageName: 'English',
 		pluralForms: 'nplurals=2; plural=(n != 1)',
-		contentChangedCallback: function() {},
+		contentChangedCallback() {},
 		glotPress: {
 			url: 'https://translate.wordpress.com',
 			project: 'test'
@@ -45,7 +45,7 @@ var	injectUrl, initialized,
  *     will add the data tags that the translator needs.
  */
 const communityTranslatorJumpstart = {
-	isEnabled: function() {
+	isEnabled() {
 		if ( ! config.isEnabled( 'community-translator' ) ) {
 			return false;
 		}
@@ -71,11 +71,11 @@ const communityTranslatorJumpstart = {
 
 		return true;
 	},
-	isActivated: function() {
+	isActivated() {
 		return _shouldWrapTranslations;
 	},
 
-	wrapTranslation: function( originalFromPage, displayedTranslationFromPage, optionsFromPage ) {
+	wrapTranslation( originalFromPage, displayedTranslationFromPage, optionsFromPage ) {
 		var props;
 
 		if ( ! this.isEnabled() || ! this.isActivated() ) {
@@ -119,7 +119,7 @@ const communityTranslatorJumpstart = {
 		return dataElement;
 	},
 
-	init: function() {
+	init() {
 		const languageJson = i18n.getLocale() || { '': {} },
 			localeCode = languageJson[ '' ].localeSlug;
 
@@ -151,7 +151,7 @@ const communityTranslatorJumpstart = {
 		initialized = true;
 	},
 
-	updateTranslationData: function( localeCode, languageJson ) {
+	updateTranslationData( localeCode, languageJson ) {
 		const languages = config( 'languages' );
 
 		if ( translationDataFromPage.localeCode === localeCode ) {
@@ -181,12 +181,12 @@ const communityTranslatorJumpstart = {
 		}
 	},
 
-	setInjectionURL: function( jsFile ) {
+	setInjectionURL( jsFile ) {
 		injectUrl = communityTranslatorBaseUrl + jsFile + '?v=' + communityTranslatorVersion;
 		debug( 'setting injection url', injectUrl );
 	},
 
-	toggle: function() {
+	toggle() {
 		let unregisteredHandleWarning = false;
 
 		translationDataFromPage.contentChangedCallback = () => {
@@ -248,7 +248,7 @@ const communityTranslatorJumpstart = {
 	},
 
 	// Merge a Community Translator TranslationPair into the i18n locale
-	updateTranslation: function( newTranslation ) {
+	updateTranslation( newTranslation ) {
 		const locale = i18n.getLocale(),
 			key = newTranslation.key,
 			plural = newTranslation.plural,
@@ -262,7 +262,7 @@ const communityTranslatorJumpstart = {
 		i18n.setLocale( locale );
 	},
 
-	isValidBrowser: function() {
+	isValidBrowser() {
 		if ( isMobile() ) {
 			return false;
 		}
