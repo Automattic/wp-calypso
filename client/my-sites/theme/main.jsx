1c81301cd1abe42afabfd4dc678371525ab3cb7b
Steve Seear <steve.seear@gmail.com>
2016-07-04 13:03:12 +0100

    Themes: Remove 'purchased' label from sheet button

--- client/my-sites/theme/main.jsx
+++ client/my-sites/theme/main.jsx
@@ -314,10 +314,7 @@ const ThemeSheet = React.createClass( {
 		}
 
 		let price = this.props.price;
-
-		if ( this.props.selectedSite && this.props.purchased ) {
-			price = i18n.translate( 'Purchased' );
-		} else if ( ! isPremium( this.props ) ) {
+		if ( ! isPremium( this.props ) ) {
 			price = i18n.translate( 'Free' );
 		}
 
