22e9dbc86b40f91b8ba777157aebf99b9373d317
Rastislav Lamos <lamos.rasto@gmail.com>
2016-07-04 14:24:18 +0200

    Remove unnecessary tab

--- client/signup/config/steps.js
+++ client/signup/config/steps.js
@@ -66,7 +66,7 @@ module.exports = {
 		dependencies: [ 'siteSlug' ],
 		providesDependencies: [ 'cartItem' ]
 	},
-	
+
 	'select-plan': {
 		stepName: 'select-plan',
 		apiRequestFunction: stepActions.addPlanToCart,
