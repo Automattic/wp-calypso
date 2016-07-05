ab419e3b3b32ab8fdd050460cc0b64158dd547d0
Andrew Duthie <andrew@andrewduthie.com>
2016-07-05 14:13:43 -0400

    CPT: Render post View via WebPreview

--- client/my-sites/post-type-list/post-actions-ellipsis-menu/view.jsx
+++ client/my-sites/post-type-list/post-actions-ellipsis-menu/view.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import includes from 'lodash/includes';
@@ -10,39 +10,63 @@ import includes from 'lodash/includes';
  * Internal dependencies
  */
 import PopoverMenuItem from 'components/popover/menu-item';
-import { getPost } from 'state/posts/selectors';
+import WebPreview from 'components/web-preview';
+import { getPost, getPostPreviewUrl } from 'state/posts/selectors';
 
-function PostActionsEllipsisMenuView( { translate, status } ) {
-	if ( 'trash' === status ) {
-		return null;
+class PostActionsEllipsisMenuView extends Component {
+	static propTypes = {
+		globalId: PropTypes.string,
+		translate: PropTypes.func.isRequired,
+		status: PropTypes.string,
+		previewUrl: PropTypes.string
+	};
+
+	constructor() {
+		super( ...arguments );
+
+		this.previewPost = this.previewPost.bind( this );
+		this.showPreview = this.togglePreview.bind( this, true );
+		this.hidePreview = this.togglePreview.bind( this, false );
+		this.state = { isPreviewing: false };
 	}
 
-	function viewPost() {
-		alert( 'Not Yet Implemented' );
+	togglePreview( isPreviewing ) {
+		this.setState( { isPreviewing } );
 	}
 
-	return (
-		<PopoverMenuItem onClick={ viewPost } icon="external">
-			{ includes( [ 'publish', 'private' ], status )
-				? translate( 'View', { context: 'verb' } )
-				: translate( 'Preview', { context: 'verb' } ) }
-		</PopoverMenuItem>
-	);
+	previewPost( event ) {
+		this.showPreview();
+		event.preventDefault();
+	}
+
+	render() {
+		const { translate, status, previewUrl } = this.props;
+		if ( ! previewUrl ) {
+			return null;
+		}
+
+		return (
+			<PopoverMenuItem
+				href={ previewUrl }
+				onClick={ this.previewPost }
+				icon="external"
+				target="_blank">
+				{ includes( [ 'publish', 'private' ], status )
+					? translate( 'View', { context: 'verb' } )
+					: translate( 'Preview', { context: 'verb' } ) }
+				<WebPreview
+					showPreview={ this.state.isPreviewing }
+					previewUrl={ previewUrl }
+					onClose={ this.hidePreview } />
+			</PopoverMenuItem>
+		);
+	}
 }
-
-PostActionsEllipsisMenuView.propTypes = {
-	globalId: PropTypes.string,
-	translate: PropTypes.func.isRequired,
-	status: PropTypes.string
-};
 
 export default connect( ( state, ownProps ) => {
 	const post = getPost( state, ownProps.globalId );
-	if ( ! post ) {
-		return {};
-	}
-
 	return {
-		status: post.status
+		status: post ? post.status : null,
+		previewUrl: post ? getPostPreviewUrl( state, post.site_ID, post.ID ) : null
 	};
 } )( localize( PostActionsEllipsisMenuView ) );
