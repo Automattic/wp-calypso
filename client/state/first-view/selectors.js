fe30b7759b2ebf114a62c5e01a4e816b24539000
Jonathan Sadowski <sadowski@automattic.com>
2016-07-05 10:05:57 -0500

    Update selectors to account for whether FV exists

--- client/state/first-view/selectors.js
+++ client/state/first-view/selectors.js
@@ -1,9 +1,19 @@
-export function isViewVisible( state, view ) {
-	return ( ! isViewDisabled( state, view ) ) && -1 !== state.firstView.visible.indexOf( view );
+import { FIRST_VIEW_START_DATES } from './constants';
+
+export function doesViewHaveFirstView( view ) {
+	return !! ( FIRST_VIEW_START_DATES[ view ] );
 }
 
-export function isViewDisabled( state, view ) {
-	return -1 !== state.firstView.disabled.indexOf( view );
+export function isViewVisible( state, view ) {
+	return (
+		doesViewHaveFirstView( view ) &&
+		isViewEnabled( state, view ) &&
+		-1 !== state.firstView.visible.indexOf( view )
+	);
+}
+
+export function isViewEnabled( state, view ) {
+	return doesViewHaveFirstView( view ) && -1 === state.firstView.disabled.indexOf( view );
 }
 
 export function getDisabledViews( state ) {
