90d1caf6acc4ea352daf0c91649360e4ab23f31c
Rick Banister <rick.banister@automattic.com>
2016-07-05 14:52:53 -0400

    Jetpack My Plan: edited some text for the my plan page

--- client/my-sites/plans/current-plan/jetpack.jsx
+++ client/my-sites/plans/current-plan/jetpack.jsx
@@ -22,7 +22,7 @@ const JetpackPlanDetails = ( { selectedSite } ) => {
 				icon="flag"
 				title={ 'Backups & Security' }
 				description={ i18n.translate(
-					'VaultPress makes it easy to keep an up-to-date backup of your site with both daily and realtime syncing of all your WordPress content. To ensure your site stays safe, VaultPress performs security scans daily and makes it easy to review and fix threats.'
+					'VaultPress makes it easy to keep an up-to-date backup of your site with both daily and real-time syncing of all your WordPress content. To ensure your site stays safe, VaultPress performs security scans daily and makes it easy to review and fix threats.'
 				) }
 				buttonText={ i18n.translate( 'View your backups' ) }
 				href="https://dashboard.vaultpress.com/" />
@@ -32,7 +32,7 @@ const JetpackPlanDetails = ( { selectedSite } ) => {
 				icon="comment"
 				title={ 'Anti-Spam' }
 				description={ i18n.translate(
-					'Akismet filters out your comment spam for you, so you can focus on more important things.'
+					'Akismet filters out comment and other forms of spam, so you can focus on more important things.'
 				) } />
 			}
 
@@ -58,9 +58,9 @@ const JetpackPlanDetails = ( { selectedSite } ) => {
 
 			<PurchaseDetail
 				icon="plugins"
-				title={ 'Calypso Features' }
+				title={ 'Get the most from WordPress.com' }
 				description={ i18n.translate(
-					'Enable plugin auto-updates, Browse your stats, Try out the new editor, {{a}}Download WordPress.com apps{{/a}}.',
+					'Enable plugin auto-updates, browse your stats, try the improved WordPress.com editor, {{a}}Download WordPress.com apps{{/a}}.',
 					{
 						components: {
 							a: <a href="https://apps.wordpress.com/" />
