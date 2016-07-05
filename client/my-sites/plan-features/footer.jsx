e8fde65a072a595d2e1fb44ba1796d4518f5f921
Kerry Liu <kerryliu@gmail.com>
2016-07-05 14:16:18 -0700

    Plans: remove longest hidden longest text

--- client/my-sites/plan-features/footer.jsx
+++ client/my-sites/plan-features/footer.jsx
@@ -4,7 +4,6 @@
 import { localize } from 'i18n-calypso';
 import noop from 'lodash/noop';
 import React, { PropTypes } from 'react';
-import reduce from 'lodash/reduce';
 
 /**
  * Internal dependencies
@@ -12,39 +11,16 @@ import reduce from 'lodash/reduce';
 import Button from 'components/button';
 import Gridicon from 'components/gridicon';
 import classNames from 'classnames';
-import {
-	isJetpack,
-	JETPACK_PLANS,
-	plansList,
-	WPCOM_PLANS
-} from 'lib/plans/constants';
-
-function getLongestDescription( planName ) {
-	if ( ! planName ) {
-		return null;
-	}
-	return reduce(
-		isJetpack( planName ) ? JETPACK_PLANS : WPCOM_PLANS,
-		( longestDescription, plan ) => {
-			const currentDescription = plansList[ plan ].getDescription();
-			return currentDescription.length > longestDescription.length
-				? currentDescription : longestDescription;
-		},
-		''
-	);
-}
 
 const PlanFeaturesFooter = ( {
 	available = true,
 	current = false,
 	description,
 	onUpgradeClick = noop,
-	planType,
-	translate,
+	translate
 } ) => {
-
 	let upgradeButton;
-	
+
 	if ( current ) {
 		upgradeButton = (
 			<Button className="plan-features__footer-button is-current" disabled>
@@ -61,16 +37,12 @@ const PlanFeaturesFooter = ( {
 	}
 
 	const classes = classNames( 'plan-features__footer', { 'has-description': !! description } );
-	const longestDescription = getLongestDescription( planType );
 	return (
 		<footer className={ classes }>
 			{ description &&
 				<p className="plan-features__footer-desc">
 					<span className="plan-features__footer-desc-text">
 						{ description }
-					</span>
-					<span className="plan-features__footer-desc-longest">
-						{ longestDescription }
 					</span>
 				</p>
 			}
@@ -86,7 +58,6 @@ PlanFeaturesFooter.propTypes = {
 	available: PropTypes.bool,
 	description: PropTypes.string,
 	onUpgradeClick: PropTypes.func,
-	planType: PropTypes.string
 };
 
 export default localize( PlanFeaturesFooter );
