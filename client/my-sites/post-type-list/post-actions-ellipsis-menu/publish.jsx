207b245f8cb14ac2d4f3951798a45392d05a8d41
Andrew Duthie <andrew@andrewduthie.com>
2016-07-05 13:09:05 -0400

    CPT: Dispatch save post on publish action

--- client/my-sites/post-type-list/post-actions-ellipsis-menu/publish.jsx
+++ client/my-sites/post-type-list/post-actions-ellipsis-menu/publish.jsx
@@ -11,15 +11,18 @@ import includes from 'lodash/includes';
  */
 import PopoverMenuItem from 'components/popover/menu-item';
 import { getPost } from 'state/posts/selectors';
+import { savePost } from 'state/posts/actions';
 import { canCurrentUser } from 'state/current-user/selectors';
 
-function PostActionsEllipsisMenuPublish( { translate, status, canPublish } ) {
+function PostActionsEllipsisMenuPublish( { translate, status, siteId, postId, canPublish, dispatchSavePost } ) {
 	if ( ! canPublish || ! includes( [ 'pending', 'draft' ], status ) ) {
 		return null;
 	}
 
 	function publishPost() {
-		alert( 'Not Yet Implemented' );
+		if ( siteId && postId ) {
+			dispatchSavePost( { status: 'publish' }, siteId, postId );
+		}
 	}
 
 	return (
@@ -33,17 +36,25 @@ PostActionsEllipsisMenuPublish.propTypes = {
 	globalId: PropTypes.string,
 	translate: PropTypes.func.isRequired,
 	status: PropTypes.string,
-	canPublish: PropTypes.bool
+	siteId: PropTypes.number,
+	postId: PropTypes.number,
+	canPublish: PropTypes.bool,
+	dispatchSavePost: PropTypes.func
 };
 
-export default connect( ( state, ownProps ) => {
-	const post = getPost( state, ownProps.globalId );
-	if ( ! post ) {
-		return {};
-	}
+export default connect(
+	( state, ownProps ) => {
+		const post = getPost( state, ownProps.globalId );
+		if ( ! post ) {
+			return {};
+		}
 
-	return {
-		status: post.status,
-		canPublish: canCurrentUser( state, post.site_ID, 'publish_posts' )
-	};
-} )( localize( PostActionsEllipsisMenuPublish ) );
+		return {
+			status: post.status,
+			siteId: post.site_ID,
+			postId: post.ID,
+			canPublish: canCurrentUser( state, post.site_ID, 'publish_posts' )
+		};
+	},
+	{ dispatchSavePost: savePost }
+)( localize( PostActionsEllipsisMenuPublish ) );
