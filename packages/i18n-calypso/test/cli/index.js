bb3688b2438984b08267924a936ed72f47c7317d
Igor Zinovyev <zinigor@gmail.com>
2016-07-07 18:36:00 +0400

    Merge pull request #7 from Automattic/add/textdomain-option
    PHP formatter: added a textdomain option.

--- packages/i18n-calypso/test/cli/index.js
+++ packages/i18n-calypso/test/cli/index.js
@@ -350,4 +350,43 @@ describe( 'index', function() {
 			expect( output ).to.have.string( 'context with a literal string key' );
 		} );
 	} );
+
+	describe( 'PHP with an additional textdomain parameter', function() {
+		var output;
+
+		describe( 'that has no special symbols', function() {
+			before( function() {
+				output = i18nCalypsoCLI( {
+					projectName: 'i18nTest',
+					inputPaths: buildFiles,
+					phpArrayName: 'arrayName',
+					format: 'PHP',
+					extras: [ 'date' ],
+					textdomain: 'some_domain'
+				} );
+			} );
+
+			it( 'should create a simple __() translation', function() {
+				expect( output ).to.have.string( '__( "My hat has three corners.", "some_domain" ),' );
+			} );
+		} );
+
+		describe( 'that has double quotes', function() {
+			before( function() {
+				output = i18nCalypsoCLI( {
+					projectName: 'i18nTest',
+					inputPaths: buildFiles,
+					phpArrayName: 'arrayName',
+					format: 'PHP',
+					extras: [ 'date' ],
+					textdomain: '"some"weird-!=domain"'
+				} );
+			} );
+
+			it( 'should escape double quotes', function() {
+				expect( output ).to.have.string( '"\\"some\\"weird-!=domain\\""' );
+			} );
+		} );
+
+	} );
 } );
