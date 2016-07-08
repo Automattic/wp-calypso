a748bc1902127145a2513a51c691bb77b503a5de
Eric Binnion <ericbinnion@gmail.com>
2016-07-08 11:47:48 -0500

    Jetpack Sync: Fix selectors doc block

--- client/state/jetpack-sync/selectors.js
+++ client/state/jetpack-sync/selectors.js
@@ -6,7 +6,7 @@ import { get } from 'lodash';
 /**
  * Returns a sync status object by site ID.
  * @param  {Object} state    Global state tree
- * @param  {Number} siteId   Post global ID
+ * @param  {Number} siteId   Site ID
  * @return {Object}          Sync status object
  */
 function getSyncStatus( state, siteId ) {
@@ -16,7 +16,7 @@ function getSyncStatus( state, siteId ) {
 /**
  * Returns a full sync request object by site ID.
  * @param  {Object} state    Global state tree
- * @param  {Number} siteId   Post global ID
+ * @param  {Number} siteId   Site ID
  * @return {Object}          Full sync request object
  */
 function getFullSyncRequest( state, siteId ) {
