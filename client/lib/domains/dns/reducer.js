Automattic/wp-calypso

commit c904b65c88b647f71096dea50c2b5539f1c30a0f

Domain Management: DNS: Prompt the user about Email Forwards before deleting the last MX record
Resolves #5752.

client/lib/domains/dns/reducer.js
@@ -3,6 +3,8 @@
  */
 import findIndex from 'lodash/findIndex';
 import isUndefined from 'lodash/isUndefined';
+import omitBy from 'lodash/omitBy';
+import pick from 'lodash/pick';
 import reject from 'lodash/reject';
 import update from 'react-addons-update';
 
@@ -85,9 +87,9 @@ function updateDnsState( state, domainName, record, updatedFields ) {
 	return update( state, command );
 }
 
-function findDnsIndex( records, { id, data, name, type } ) {
-	const matchingFields = isUndefined( id ) ? { data, name, type } : { id, data, name, type };
-	return findIndex( records, matchingFields );
+function findDnsIndex( records, record ) {
+	const matchingFields = pick( record, [ 'id', 'data', 'name', 'type' ] );
+	return findIndex( records, omitBy( matchingFields, isUndefined ) );
 }
 
 function getInitialStateForDomain() {

client/lib/domains/dns/index.js
@@ -177,10 +177,20 @@ function isBeingProcessed( record ) {
 	return record.isBeingDeleted || record.isBeingAdded;
 }
 
+function isDeletingLastMXRecord( recordToDelete, records ) {
+	const currentMXRecords = filter( records, { type: 'MX' } );
+
+	return (
+		recordToDelete.type === 'MX' &&
+		currentMXRecords.length === 1
+	);
+}
+
 export {
 	addMissingWpcomRecords,
 	getNormalizedData,
 	removeDuplicateWpcomRecords,
 	validateAllFields,
-	isBeingProcessed
+	isBeingProcessed,
+	isDeletingLastMXRecord
 };

client/my-sites/upgrades/domain-management/dns/delete-email-forwards-dialog.jsx
@@ -1,0 +1,75 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import page from 'page';
+
+/**
+ * Internal dependencies
+ */
+import Dialog from 'components/dialog';
+import { domainManagementEmailForwarding } from 'my-sites/upgrades/paths';
+
+const DeleteEmailForwardsDialog = React.createClass( {
+	propTypes: {
+		onClose: React.PropTypes.func,
+		visible: React.PropTypes.bool.isRequired,
+		selectedSite: React.PropTypes.oneOfType( [
+			React.PropTypes.object,
+			React.PropTypes.bool
+		] ).isRequired,
+		selectedDomainName: React.PropTypes.string.isRequired
+	},
+
+	close( result ) {
+		this.props.onClose( result );
+	},
+
+	isVisible() {
+		return this.props.visible;
+	},
+
+	render() {
+		const buttons = [
+				{
+					action: 'delete',
+					label: this.translate( 'Deactivate Email Forwards and Remove Records' ),
+					isPrimary: true,
+					onClick: () => this.close( { shouldDeleteEmailForwards: true } )
+				},
+				{
+					action: 'keep',
+					label: this.translate( 'Keep Records and Email Forwards' )
+				}
+			];
+
+		return (
+			<Dialog
+					isVisible={ this.isVisible() }
+					buttons={ buttons }
+					onClose={ () => this.close( { shouldDeleteEmailForwards: false } ) }
+					className="cancel-purchase-button__warning-dialog">
+				<h1>{ this.translate( 'Are you sure?' ) }</h1>
+				<p>
+				{ this.translate(
+					'Removing this record will delete your current {{a}}Email Forwards{{/a}}.',
+					{
+						components: {
+							a: <a target="_blank" href={ this.getEmailForwardingPath() } />
+						}
+					}
+				) }
+				</p>
+			</Dialog>
+		);
+	},
+
+	getEmailForwardingPath: function() {
+		return domainManagementEmailForwarding(
+			this.props.selectedSite.domain,
+			this.props.selectedDomainName
+		);
+	}
+} );
+
+export default DeleteEmailForwardsDialog;

client/my-sites/upgrades/domain-management/dns/dns-list.jsx
@@ -6,9 +6,11 @@ import React from 'react';
 /**
  * Internal dependencies
  */
+import DeleteEmailForwardsDialog from './delete-email-forwards-dialog';
 import DnsRecord from './dns-record';
 import notices from 'notices';
 import { deleteDns as deleteDnsAction, addDns as addDnsAction } from 'lib/upgrades/actions';
+import { isDeletingLastMXRecord } from 'lib/domains/dns';
 
 const DnsList = React.createClass( {
 	propTypes: {
@@ -20,7 +22,44 @@ const DnsList = React.createClass( {
 		] ).isRequired
 	},
 
-	deleteDns: function( record ) {
+	getInitialState: function() {
+		return { dialog: this.noDialog() };
+	},
+
+	noDialog: function() {
+		return {
+			type: null,
+			onClose: null
+		};
+	},
+
+	openDialog( type, onClose ) {
+		this.setState( {
+			dialog: {
+				type,
+				onClose
+			}
+		} );
+	},
+
+	handleDialogClose( result ) {
+		this.state.dialog.onClose( result );
+		this.setState( { dialog: this.noDialog() } );
+	},
+
+	deleteDns: function( record, confirmed = false ) {
+		const { records } = this.props.dns;
+
+		if ( ! confirmed && isDeletingLastMXRecord( record, records ) ) {
+			this.openDialog( 'deleteEmailForwards', ( result ) => {
+				if ( result.shouldDeleteEmailForwards ) {
+					this.deleteDns( record, true );
+				}
+			} );
+
+			return;
+		}
+
 		deleteDnsAction( this.props.selectedDomainName, record, ( error ) => {
 			if ( error ) {
 				notices.error( error.message || this.translate( 'The DNS record has not been deleted.' ) );
@@ -51,19 +90,28 @@ const DnsList = React.createClass( {
 	},
 
 	render: function() {
-		const dnsRecordsList = this.props.dns.records.map( function( dnsRecord, index ) {
-			return (
-				<DnsRecord
-					key={ index }
-					dnsRecord={ dnsRecord }
-					onDeleteDns={ this.deleteDns }
-					selectedDomainName={ this.props.selectedDomainName }
-					selectedSite={ this.props.selectedSite } />
-			);
-		}, this );
+		const { dialog } = this.state,
+			{ dns, selectedDomainName, selectedSite } = this.props,
+			dnsRecordsList = dns.records.map( function( dnsRecord, index ) {
+				return (
+					<DnsRecord
+						key={ index }
+						dnsRecord={ dnsRecord }
+						onDeleteDns={ this.deleteDns }
+						selectedDomainName={ selectedDomainName }
+						selectedSite={ selectedSite } />
+				);
+			}, this );
 
 		return (
-			<ul className="dns__list">{ dnsRecordsList }</ul>
+			<div className="dns__list">
+				<DeleteEmailForwardsDialog
+					visible={ dialog.type === 'deleteEmailForwards' }
+					onClose={ this.handleDialogClose }
+					selectedDomainName={ selectedDomainName }
+					selectedSite={ selectedSite } />
+				<ul>{ dnsRecordsList }</ul>
+			</div>
 		);
 	}
 } );

client/my-sites/upgrades/domain-management/style.scss
@@ -918,7 +918,7 @@ ul.email-forwarding__list {
 	}
 }
 
-ul.dns__list {
+.dns__list > ul {
 	list-style: none;
 	margin: 15px 0 0 0;
 	padding: 0;

commit 5cf210b580e88b73fcee40d3299474a39530155b

Domains: Fix regression when adding SRV records
This was caused by a very subtle difference - SRV records have empty
`data` fields. However, the one we are submitting are set to
`undefined`, but the ones coming from the backend have `null`. The
additional filtering introduced by @rads in
c904b65c88b647f71096dea50c2b5539f1c30a0f caused this field not to be
taken into account when filtering/searching for the new field. This in
turn, could cause a different, wrong field to match the given predicate
(name and type, bascially). This was even more probable for root
records.

client/lib/domains/dns/reducer.js
@@ -2,8 +2,6 @@
  * External dependencies
  */
 import findIndex from 'lodash/findIndex';
-import isUndefined from 'lodash/isUndefined';
-import omitBy from 'lodash/omitBy';
 import pick from 'lodash/pick';
 import reject from 'lodash/reject';
 import update from 'react-addons-update';
@@ -89,7 +87,7 @@ function updateDnsState( state, domainName, record, updatedFields ) {
 
 function findDnsIndex( records, record ) {
 	const matchingFields = pick( record, [ 'id', 'data', 'name', 'type' ] );
-	return findIndex( records, omitBy( matchingFields, isUndefined ) );
+	return findIndex( records, matchingFields );
 }
 
 function getInitialStateForDomain() {