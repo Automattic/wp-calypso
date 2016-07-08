5b49db0475a69fdec36f94029e05c90c78f5510d
Eric Binnion <ericbinnion@gmail.com>
2016-07-08 12:34:19 -0500

    Jetpack Sync: Adds isPendingSyncStart to selectors

--- client/state/jetpack-sync/selectors.js
+++ client/state/jetpack-sync/selectors.js
@@ -23,7 +23,41 @@ function getFullSyncRequest( state, siteId ) {
 	return get( state, [ 'jetpackSync', 'fullSyncRequest', siteId ] );
 }
 
+/**
+ * Returns a boolean for whether a full sync is pending start.
+ * @param  {Object} state    Global state tree
+ * @param  {Number} siteId   Site ID
+ * @return {Boolean}         Whether a sync is pending start for site
+ */
+function isPendingSyncStart( state, siteId ) {
+	const syncStatus = getSyncStatus( state, siteId );
+	const fullSyncRequest = getFullSyncRequest( state, siteId );
+
+	// Is the sync scheduled and awaiting cron?
+	const isScheduled = get( syncStatus, 'is_scheduled' );
+	if ( isScheduled ) {
+		return true;
+	}
+
+	// Have we requested a full sync from Calypso?
+	const requestingFullSync = get( fullSyncRequest, 'isRequesting' ) || get( fullSyncRequest, 'scheduled' );
+	if ( ! requestingFullSync ) {
+		return false;
+	}
+
+	// If we have requested a full sync, is that request newer than the last time we received sync status?
+	const lastRequested = get( fullSyncRequest, 'lastRequested' );
+	const lastSuccessfulStatus = get( syncStatus, 'lastSuccessfulStatus' );
+
+	if ( ! lastSuccessfulStatus ) {
+		return true;
+	}
+
+	return parseInt( lastRequested, 10 ) > parseInt( lastSuccessfulStatus, 10 );
+}
+
 export default {
 	getSyncStatus,
-	getFullSyncRequest
+	getFullSyncRequest,
+	isPendingSyncStart
 };
--- client/state/jetpack-sync/test/selectors.js
+++ client/state/jetpack-sync/test/selectors.js
@@ -8,13 +8,16 @@ import { expect } from 'chai';
  */
 import {
 	getSyncStatus,
-	getFullSyncRequest
+	getFullSyncRequest,
+	isPendingSyncStart
 } from '../selectors';
 
 const nonExistentId = '111111';
 const requestedSiteId = '123456';
 const successfulSiteId = '1234567';
 const errorSiteId = '12345678';
+const syncScheduledSiteID = '123455';
+const oldSyncSiteId = '987654321';
 
 const syncStatusSuccessful = {
 	isRequesting: false,
@@ -48,6 +51,38 @@ const syncStatusSuccessful = {
 	is_scheduled: false
 };
 
+const syncStatusScheduled = {
+	isRequesting: false,
+	error: false,
+	lastSuccessfulStatus: 1467926563436,
+	started: 1466010260,
+	queue_finished: 1466010260,
+	sent_started: 1466010290,
+	finished: 1466010313,
+	queue: {
+		constants: 1,
+		functions: 1,
+		options: 1,
+		terms: 3,
+		themes: 1,
+		users: 2,
+		posts: 15,
+		comments: 1,
+		updates: 6
+	},
+	sent: {
+		constants: 1,
+		functions: 1,
+		options: 1,
+		terms: 3,
+		themes: 1,
+		users: 2,
+		posts: 15,
+		comments: 1
+	},
+	is_scheduled: true
+};
+
 const syncStatusErrored = {
 	isRequesting: false,
 	error: {
@@ -67,6 +102,13 @@ const fullSyncRequestSuccessful = {
 	lastRequested: 1467944517955
 };
 
+const fullSyncRequestOld = {
+	isRequesting: true,
+	scheduled: true,
+	error: false,
+	lastRequested: 1467926563435
+};
+
 const fullSyncRequestErrored = {
 	isRequesting: false,
 	scheduled: false,
@@ -80,12 +122,15 @@ const testState = {
 	jetpackSync: {
 		syncStatus: {
 			[ successfulSiteId ]: syncStatusSuccessful,
-			[ errorSiteId ]: syncStatusErrored
+			[ errorSiteId ]: syncStatusErrored,
+			[ syncScheduledSiteID ]: syncStatusScheduled,
+			[ oldSyncSiteId ]: syncStatusSuccessful
 		},
 		fullSyncRequest: {
 			[ requestedSiteId ]: fullSyncRequested,
 			[ successfulSiteId ]: fullSyncRequestSuccessful,
-			[ errorSiteId ]: fullSyncRequestErrored
+			[ errorSiteId ]: fullSyncRequestErrored,
+			[ oldSyncSiteId ]: fullSyncRequestOld
 		}
 	}
 };
@@ -122,6 +167,28 @@ describe( 'selectors', () => {
 		it( 'should return full sync status for a site if in state', () => {
 			const fullSyncRequest = getFullSyncRequest( testState, successfulSiteId );
 			expect( fullSyncRequest ).to.be.eql( testState.jetpackSync.fullSyncRequest[ successfulSiteId ] );
+		} );
+	} );
+
+	describe( '#isPendingSyncStart()', () => {
+		it( 'should return true if a sync is scheduled', () => {
+			const test = isPendingSyncStart( testState, syncScheduledSiteID );
+			expect( test ).to.be.true;
+		} );
+
+		it( 'should return false if sync status and full sync request show not scheduled', () => {
+			const test = isPendingSyncStart( testState, errorSiteId );
+			expect( test ).to.be.false;
+		} );
+
+		it( 'should return true if a sync has been requested, but before sync status has updated', () => {
+			const test = isPendingSyncStart( testState, successfulSiteId );
+			expect( test ).to.be.true;
+		} );
+
+		it( 'should be false if a sync has been requested, but sync has already finished', () => {
+			const test = isPendingSyncStart( testState, oldSyncSiteId );
+			expect( test ).to.be.false;
 		} );
 	} );
 } );
