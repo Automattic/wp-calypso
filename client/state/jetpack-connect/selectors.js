85829f77540c9672830feb83e05b77d0c1e581e5
roccotripaldi <tripaldir@gmail.com>
2016-07-08 14:01:21 -0400

    Jetpack Connect: isCalypsoStartedConnection
    Remove references to `isCalypsoStartedConnection` because we no longer
    depend on that variable to tell is to redirect to plans page. We will
    redirect to the plans page by default always.

--- client/state/jetpack-connect/selectors.js
+++ client/state/jetpack-connect/selectors.js
@@ -1,18 +1,3 @@
-const JETPACK_CONNECT_TTL = 60 * 60 * 1000; // an hour
-
-const isCalypsoStartedConnection = function( state, siteSlug ) {
-	if ( ! siteSlug ) {
-		return false;
-	}
-	const site = siteSlug.replace( /.*?:\/\//g, '' );
-	if ( state && state[ site ] ) {
-		const currentTime = ( new Date() ).getTime();
-		return ( currentTime - state[ site ].timestamp < JETPACK_CONNECT_TTL );
-	}
-
-	return false;
-};
-
 const getFlowType = function( state, site ) {
 	const siteSlug = site.slug.replace( /.*?:\/\//g, '' );
 	if ( state && state[ siteSlug ] ) {
@@ -21,4 +6,4 @@ const getFlowType = function( state, site ) {
 	return false;
 };
 
-export default { isCalypsoStartedConnection, getFlowType };
+export default { getFlowType };
