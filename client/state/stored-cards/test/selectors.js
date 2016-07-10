5b2ee7c15091ce03084806598fab959c704b1c9b
Stéphane Thomas <email@stephanethomas.com>
2016-07-10 16:36:53 +0200

    Purchases: Store whether the list of stored cards has been loaded from the server

--- client/state/stored-cards/test/selectors.js
+++ client/state/stored-cards/test/selectors.js
@@ -3,14 +3,15 @@ import deepFreeze from 'deep-freeze';
 import { expect } from 'chai';
 
 // Internal dependencies
-import { getStoredCardById, getStoredCards } from '../selectors';
+import { getStoredCardById, getStoredCards, hasLoadedStoredCardsFromServer } from '../selectors';
 import { STORED_CARDS_FROM_API } from './fixture';
 
 describe( 'selectors', () => {
 	describe( 'getStoredCards', () => {
-		it( 'should return a purchase by its ID, preserving the top-level flags', () => {
+		it( 'should return all cards', () => {
 			const state = deepFreeze( {
 				storedCards: {
+					hasLoadedFromServer: true,
 					isFetching: false,
 					isDeleting: false,
 					items: STORED_CARDS_FROM_API
@@ -22,9 +23,10 @@ describe( 'selectors', () => {
 	} );
 
 	describe( 'getStoredCardById', () => {
-		it( 'should return a purchase by its ID, preserving the top-level flags', () => {
+		it( 'should return a card by its ID, preserving the top-level flags', () => {
 			const state = deepFreeze( {
 				storedCards: {
+					hasLoadedFromServer: true,
 					isFetching: false,
 					isDeleting: false,
 					items: STORED_CARDS_FROM_API
@@ -32,6 +34,21 @@ describe( 'selectors', () => {
 			} );
 
 			expect( getStoredCardById( state, 12345 ) ).to.be.eql( STORED_CARDS_FROM_API[ 1 ] );
+		} );
+	} );
+
+	describe( 'hasLoadedStoredCardsFromServer', () => {
+		it( 'should return the flag that determines whether the list of cards has been loaded from the server', () => {
+			const state = deepFreeze( {
+				storedCards: {
+					hasLoadedFromServer: true,
+					isFetching: false,
+					isDeleting: false,
+					items: STORED_CARDS_FROM_API
+				}
+			} );
+
+			expect( hasLoadedStoredCardsFromServer( state ) ).to.be.true;
 		} );
 	} );
 } );
