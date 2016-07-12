3ac8562f15e4d39909ca32aaee9f95c663a428ae
Bob Ralian <bob.ralian@gmail.com>
2016-07-12 15:31:24 -0500

    address long line linting error

--- client/components/plans/plan-list/index.jsx
+++ client/components/plans/plan-list/index.jsx
@@ -86,7 +86,8 @@ const PlanList = React.createClass( {
 			) {
 				filteredPlans = filteredPlans.map( plan => {
 					if ( plan.product_id === 1003 ) {
-						plan.description = this.translate( 'Your own domain name, powerful customization options, easy monetization with WordAds and lots of space for audio and video.' );
+						plan.description = this.translate( 'Your own domain name, powerful customization options, ' +
+							'easy monetization with WordAds and lots of space for audio and video.' );
 					}
 					return plan;
 				} );
