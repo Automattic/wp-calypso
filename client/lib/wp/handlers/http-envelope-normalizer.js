4fbcd0a54e4b68b30743f3f5e0b39ea62c7a0e19
retrofox <rdsuarez@gmail.com>
2016-07-13 12:00:22 -0300

    wp:handlers:add debug line to httpEnvelopeNormalizer

--- client/lib/wp/handlers/http-envelope-normalizer.js
+++ client/lib/wp/handlers/http-envelope-normalizer.js
@@ -1,22 +1,32 @@
 /**
- * Detect error looking in the reponse data object.
+ * External dependencies
+ */
+import debugFactory from 'debug';
+
+/**
+ * Module variables
+ */
+const debug = debugFactory( 'calypso:http-envelope-normalizer' );
+
+/**
+ * Detect error looking in the response data object
  *
  * @param {Function} handler - wpcom handler
  * @return {Function} handler wrapper
  */
-export function requestHandler( handler ) {
+export function httpEnvelopeNormalizer( handler ) {
 	return ( params, fn ) => {
 		handler( params, ( err, response ) => {
 			const { code, message, data = {} } = response || {};
 			const { status } = data;
 
-			// Create Error object if the response
-			// has `code`, `message` and `status` properties
+			// Create Error object if the response has `code`, `message` and `status` properties
 			if (
 				code && typeof code === 'string' &&
 				message && typeof message === 'string' &&
 				status && typeof status === 'number'
 			) {
+				debug( 'Error detected: %o, %o, $o', code, message, status );
 				return fn( new Error( message ) );
 			}
 
@@ -32,10 +42,10 @@ export function requestHandler( handler ) {
  * @return {WPCOM} wpcom instance with the new request handler
  */
 
-export function injectHandler( wpcom ) {
+export function injectEnvelopeHandler( wpcom ) {
 	const request = wpcom.request.bind( wpcom );
 
 	return Object.assign( wpcom, {
-		request: requestHandler( request )
+		request: httpEnvelopeNormalizer( request )
 	} );
 }
