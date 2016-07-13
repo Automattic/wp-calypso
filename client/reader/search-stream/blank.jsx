6d2f0e59529586b8a29a858c78de10b92485721e
Greg Ichneumon Brown <gbrown5878@gmail.com>
2016-07-13 11:48:02 -0600

    Reader Search: add drake with magnifying glass to blank search page

--- client/reader/search-stream/blank.jsx
+++ client/reader/search-stream/blank.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+const React = require( 'react' );
 
-export default function BlankContent() {
-	return <p />;
-}
+const EmptyContent = require( 'components/empty-content' );
+
+const BlankContent = React.createClass( {
+
+	shouldComponentUpdate: function() {
+		return false;
+	},
+
+	render: function() {
+		return ( <EmptyContent
+			title={ '' }
+			illustration={ '/calypso/images/drake/drake-404.svg' }
+			illustrationWidth={ 500 }
+			/> );
+	}
+} );
+
+module.exports = BlankContent;
