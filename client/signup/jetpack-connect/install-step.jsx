aa8bf53977f3dde01185b0ebc7b3cac06cff21bb
Eric Binnion <ericbinnion@gmail.com>
2016-07-14 11:41:13 -0500

    Jetpack Connect: preventWidows on action when only child

--- client/signup/jetpack-connect/install-step.jsx
+++ client/signup/jetpack-connect/install-step.jsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React from 'react';
+import { omit } from 'lodash';
 
 /**
  * Internal dependencies
@@ -13,23 +14,22 @@ export default React.createClass( {
 	displayName: 'JetpackInstallStep',
 
 	renderText() {
-		const { action } = this.props;
-		let { text } = this.props;
+		let { text, action } = this.props;
 
-		if ( ! action ) {
-			return (
-				<span>
-					{ preventWidows( text ) }
-				</span>
+		if ( ! action || ! React.isValidElement( action ) ) {
+			text = preventWidows( text );
+		} else if ( 1 === React.Children.count( action.props.children ) ) {
+			action = React.cloneElement(
+				action,
+				omit( action.props, 'children' ),
+				React.Children.map( action.props.children, child => {
+					return preventWidows( child );
+				} )
 			);
 		}
 
 		return (
-			<span>
-				{ text }
-				<span> </span>
-				{ action }
-			</span>
+			<span>{ text } { action }</span>
 		);
 	},
 
