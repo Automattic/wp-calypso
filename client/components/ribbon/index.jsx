abc4d933913b1ef2410f542da66d89ac137dbf9d
artpi <artpi@post.pl>
2016-07-14 19:21:17 +0200

    Pass

--- client/components/ribbon/index.jsx
+++ client/components/ribbon/index.jsx
@@ -2,7 +2,8 @@
  * External dependencies
  */
 import React from 'react';
+import classNames from 'classnames';
 
-export default props => <div className="ribbon__wrapper">
+export default props => <div className={ classNames( 'ribbon__wrapper', [ props.className ] ) }>
 	<span className="ribbon__title">{ props.children }</span>
 </div>;
