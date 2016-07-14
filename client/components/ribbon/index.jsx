aa4c6184cfbb512a57a1c0a3154bad49ab29e8e6
artpi <artpi@post.pl>
2016-07-14 19:14:37 +0200

    Breakout Ribbon as a component

---
+++ client/components/ribbon/index.jsx
@@ -0,0 +1,8 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+export default props => <div className="ribbon__wrapper">
+	<span className="ribbon__title">{ props.children }</span>
+</div>;
