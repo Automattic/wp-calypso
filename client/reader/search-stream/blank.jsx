6d28eff05cc06a55a02e4a6c5f2d519630bdc088
Greg Ichneumon Brown <gbrown5878@gmail.com>
2016-07-14 08:26:09 -0600

    Reader Search: switch to CamelCase

--- client/reader/search-stream/blank.jsx
+++ client/reader/search-stream/blank.jsx
@@ -4,21 +4,21 @@ import { localize } from 'i18n-calypso'
 export function BlankContent( { translate, suggestions } ) {
 	var suggest = null;
 	if ( suggestions ) {
-		var sug_list = suggestions.map( function( query ) {
+		var sugList = suggestions.map( function( query ) {
 			return <a href={ '/read/search?q=' + encodeURIComponent( query ) } >{ query }</a>;
 		} );
 		//join the link elements into a comma separated list
-		sug_list = sug_list.slice(1).reduce(function(xs, x, i) {
+		sugList = sugList.slice(1).reduce(function(xs, x, i) {
 			return xs.concat([', ', x]);
-		}, [ sug_list[0] ] );
-		suggest = <p>{translate( 'Staff Suggestions: {{suggestions /}}.', { components: { suggestions: sug_list } })}</p>;
+		}, [ sugList[0] ] );
+		suggest = <p>{translate( 'Staff Suggestions: {{suggestions /}}.', { components: { suggestions: sugList } })}</p>;
 	}
 
-	const img_path = '/calypso/images/drake/drake-404.svg';
+	const imgPath = '/calypso/images/drake/drake-404.svg';
 	return (
 		<div className="search-blank-content">
 			{ suggest }
-			<img src={ img_path } width="500" className="empty-content__illustration" />
+			<img src={ imgPath } width="500" className="empty-content__illustration" />
 		</div>
 	);
 }
