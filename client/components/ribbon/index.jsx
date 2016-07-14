e37c4d42dfaa612e2d693922e7cbfb2b2c56cbab
artpi <artpi@post.pl>
2016-07-14 19:21:42 +0200

    Fix parentheses

--- client/components/ribbon/index.jsx
+++ client/components/ribbon/index.jsx
@@ -4,6 +4,6 @@
 import React from 'react';
 import classNames from 'classnames';
 
-export default props => <div className={ classNames( 'ribbon__wrapper', [ props.className ] ) }>
+export default props => <div className={ classNames( 'ribbon__wrapper', props.className ) }>
 	<span className="ribbon__title">{ props.children }</span>
 </div>;
