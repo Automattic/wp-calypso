423851961073c60668d575b062a49bf8c5671b6a
Igor Klimer <igor@klimer.eu>
2016-07-15 16:28:47 +0200

    Google Apps: Show errors from backend if present

--- client/my-sites/upgrades/domain-management/email/google-apps-users-card.jsx
+++ client/my-sites/upgrades/domain-management/email/google-apps-users-card.jsx
@@ -2,17 +2,20 @@
  * External dependencies
  */
 import React from 'react';
+import groupBy from 'lodash/groupBy';
 
 /**
  * Internal dependencies
  */
 import CompactCard from 'components/card/compact';
+import Notice from 'components/notice';
+import Button from 'components/button';
 import PendingGappsTosNotice from 'my-sites/upgrades/components/domain-warnings/pending-gapps-tos-notice';
 import paths from 'my-sites/upgrades/paths';
 import analyticsMixin from 'lib/mixins/analytics';
 import SectionHeader from 'components/section-header';
 import GoogleAppsUserItem from './google-apps-user-item';
-import { getSelectedDomain } from 'lib/domains';
+import { getSelectedDomain, hasPendingGoogleAppsUsers } from 'lib/domains';
 
 const GoogleAppsUsers = React.createClass( {
 	mixins: [ analyticsMixin( 'domainManagement', 'googleApps' ) ],
@@ -50,42 +53,66 @@ const GoogleAppsUsers = React.createClass( {
 		this.recordEvent( 'addGoogleAppsUserClick', this.props.selectedDomainName );
 	},
 
+	renderDomain( domain, users ) {
+		return (
+			<div key={ `google-apps-user-${ domain }` } className="google-apps-users-card">
+				<SectionHeader
+					label={ domain }>
+					{ this.canAddUsers() && (
+						<Button
+							primary
+							compact
+							href={ paths.domainManagementAddGoogleApps(
+								this.props.selectedSite.slug, domain
+							) }
+							onClick={ this.goToAddGoogleApps }>
+							{ this.translate( 'Add Google Apps User' ) }
+						</Button>
+					) }
+				</SectionHeader>
+				<CompactCard className="google-apps-users-card__user-list">
+					<ul className="google-apps-users-card__user-list-inner">
+						{ users.map( ( user, index ) => this.renderUser( user, index ) ) }
+					</ul>
+				</CompactCard>
+			</div>
+		);
+	},
+
+	renderUser( user, index ) {
+		if ( user.error ) {
+			return (
+				<Notice
+					key={ `google-apps-user-notice-${ user.domain }-${ index }` }
+					showDismiss={ false }
+					status="is-warning"
+					text={ user.error } />
+			);
+		}
+
+		return (
+			<GoogleAppsUserItem
+				key={ `google-apps-user-${ user.domain }-${ index }` }
+				user={ user }
+				onClick={ this.generateClickHandler( user ) }/>
+		);
+	},
+
 	render() {
-		const pendingDomains = this.getDomainsAsList().filter( domain =>
-			domain.googleAppsSubscription &&
-			domain.googleAppsSubscription.pendingUsers &&
-			domain.googleAppsSubscription.pendingUsers.length !== 0 );
+		const pendingDomains = this.getDomainsAsList().filter( hasPendingGoogleAppsUsers ),
+			usersByDomain = groupBy( this.props.googleAppsUsers, 'domain' );
 
 		return (
 			<div>
-				{ pendingDomains.length !== 0 && <PendingGappsTosNotice key="pending-gapps-tos-notice" siteSlug={ this.props.selectedSite.slug } domains={ pendingDomains } section="google-apps" /> }
+				{ pendingDomains.length !== 0 &&
+					<PendingGappsTosNotice
+						key="pending-gapps-tos-notice"
+						siteSlug={ this.props.selectedSite.slug }
+						domains={ pendingDomains }
+						section="google-apps" />
+				}
 
-				<SectionHeader
-					count={ this.props.googleAppsUsers.length }
-					label={ this.translate( 'Google Apps Users' ) }>
-					{ this.canAddUsers() && (
-						<a
-							href={ paths.domainManagementAddGoogleApps(
-								this.props.selectedSite.slug, this.props.selectedDomainName
-							) }
-							className="button is-compact is-primary"
-							onClick={ this.goToAddGoogleApps }>
-							{ this.translate( 'Add Google Apps User' ) }
-						</a>
-					) }
-				</SectionHeader>
-
-				<CompactCard className="google-apps-users-card">
-					<ul className="google-apps-users-card__user-list">
-						{ this.props.googleAppsUsers.map(
-							( user, index ) => (
-								<GoogleAppsUserItem
-									key={ index } user={ user }
-									onClick={ this.generateClickHandler( user ) }/>
-							)
-						) }
-					</ul>
-				</CompactCard>
+				{ Object.keys( usersByDomain ).map( ( domain ) => this.renderDomain( domain, usersByDomain[ domain ] ) ) }
 			</div>
 		);
 	}
