bf330229a93264103fefe8281847bfdde91a619e
Yoav Farhi <yoav@farhi.org>
2016-07-17 12:19:15 +0300

    add logic to enable/disable based on user settings

--- client/lib/serbian-latin/index.jsx
+++ client/lib/serbian-latin/index.jsx
@@ -9,23 +9,58 @@ import to_latin from 'cyrillic-to-latin';
  * Internal dependencies
  */
 import User from 'lib/user';
+import userSettings from 'lib/user-settings';
 const user = new User();
+
+var _tlEnabled = false,
+	initialized = false;
 
 const serbianLatin = {
 
-	init() {
+	isEnabled() {
 		const currentUser = user.get();
 
 		if ( 'sr' !== currentUser.localeSlug ) {
+			return this.toggle( false );
+		}
+
+		if ( ! userSettings.getOriginalSetting( 'enable_sr_latin' ) ) {
+			return this.toggle( false );
+		}
+
+		return this.toggle( true );
+	},
+
+	init() {
+		if ( ! this.isEnabled() ) {
 			return;
 		}
 
+		if ( initialized ) {
+			return;
+		}
+
+		initialized = true;
 		i18n.registerTranslateHook( ( translation ) => {
-			return serbianLatin.convert( translation );
+			return this.convert( translation );
 		} );
+		i18n.reRenderTranslations();
+	},
+
+	toggle( toState ) {
+		if ( _tlEnabled !== toState ) {
+			_tlEnabled = toState;
+			i18n.reRenderTranslations();
+		}
+
+		return _tlEnabled;
 	},
 
 	convert( translation ) {
+		if ( ! _tlEnabled ) {
+			return translation;
+		}
+
 		switch ( typeof( translation ) ) {
 			case 'object':
 				for ( let prop in translation ) {
@@ -45,6 +80,7 @@ const serbianLatin = {
 };
 
 i18n.on( 'change', serbianLatin.init.bind( serbianLatin ) );
+userSettings.on( 'change', serbianLatin.init.bind( serbianLatin ) );
 
 export default serbianLatin;
 
