458c9e40672fd5904f85964214fc9ea861a7a08e
Miguel Fonseca <miguelcsf@gmail.com>
2016-07-18 17:22:34 +0100

    ui.queryArguments: update state less aggressively

--- client/state/ui/query-arguments/reducer.js
+++ client/state/ui/query-arguments/reducer.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { combineReducers } from 'redux';
+import { isEqual, omit } from 'lodash';
 
 /**
  * Internal dependencies
@@ -16,13 +17,21 @@ const timestamped = ( query ) => ( {
 	timestamp: Date.now(),
 } );
 
+const isEqualQuery = ( a, b ) =>
+	isEqual(
+		omit( a, 'timestamp' ),
+		omit( b, 'timestamp' ) );
+
 const initial = createReducer( false, {
 	[ ROUTE_SET ]: ( state, { query } ) =>
 		state === false ? timestamped( query ) : state,
 } );
 
 const current = createReducer( {}, {
-	[ ROUTE_SET ]: ( state, { query } ) => timestamped( query ),
+	[ ROUTE_SET ]: ( state, { query } ) =>
+		! isEqualQuery( state, query )
+			? timestamped( query )
+			: state,
 } );
 
 export default combineReducers( {
