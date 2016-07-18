959558f51217f592286c22e0583e6af0a2aeb3c9
ehg <chris@ehg.sh>
2016-07-18 10:25:05 +0100

    GuidedTours: Remove showGuidedTour actions and reducer branch

--- client/state/ui/guided-tours/actions.js
+++ client/state/ui/guided-tours/actions.js
@@ -2,7 +2,6 @@
  * Internal dependencies
  */
 import {
-	GUIDED_TOUR_SHOW,
 	GUIDED_TOUR_UPDATE,
 } from 'state/action-types';
 
@@ -15,34 +14,11 @@ import guidedToursConfig from 'layout/guided-tours/config';
 import { savePreference } from 'state/preferences/actions';
 import { getPreference } from 'state/preferences/selectors';
 
-/**
- * Returns an action object which will be used to hide or show a specific tour.
- *
- * @param {Object} options Options object, see fn signature.
- * @return {Object} Action object
- */
-export function showGuidedTour( { shouldShow, shouldDelay = false, tour = 'main' } ) {
-	const showAction = {
-		type: GUIDED_TOUR_SHOW,
-		shouldShow,
-		shouldDelay,
-		tour,
-	};
-
-	const trackEvent = recordTracksEvent( 'calypso_guided_tours_show', {
-		tour_version: guidedToursConfig.get( tour ).version,
-		tour,
-	} );
-
-	return shouldDelay ? showAction : withAnalytics( trackEvent, showAction );
-}
-
 export function quitGuidedTour( { tour = 'main', stepName, finished, error } ) {
 	const quitAction = {
 		type: GUIDED_TOUR_UPDATE,
 		shouldShow: false,
 		shouldReallyShow: false,
-		shouldDelay: false,
 		tour,
 		stepName,
 	};
