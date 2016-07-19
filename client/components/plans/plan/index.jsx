5f37a5b5e9a5abf607f5a27e7a453e33c56614f2
Andy Peatling <apeatling@gmail.com>
2016-07-19 10:48:21 -0700

    Remove test detection code

--- client/components/plans/plan/index.jsx
+++ client/components/plans/plan/index.jsx
@@ -68,17 +68,6 @@ const Plan = React.createClass( {
 			);
 		}
 
-		// override plan description during google voucher test
-		if ( this.isUSorCanadaCurrency() && isGoogleVouchersEnabled() ) {
-			if ( plan.product_id === premiumPlan.productId ) {
-				plan.description = premiumPlan.oldDescriptionWithGoogleVouchers;
-			} else if ( plan.product_id === businessPlan.productId ) {
-				plan.description = isWordpressAdCreditsEnabled()
-					? businessPlan.oldDescriptionWithWordAdsCredit
-					: businessPlan.oldDescription;
-			}
-		}
-
 		return (
 			<WpcomPlanDetails
 				comparePlansUrl={ this.getComparePlansUrl() }
