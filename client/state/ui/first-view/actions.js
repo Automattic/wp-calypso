4e3aadfc868934e200109da489e840afd3e3dc52
Jonathan Sadowski <sadowski@automattic.com>
2016-07-19 16:56:32 -0500

    First-View: Record Tracks events and bump stats

--- client/state/ui/first-view/actions.js
+++ client/state/ui/first-view/actions.js
@@ -5,8 +5,14 @@ import {
 	FIRST_VIEW_HIDE
 } from 'state/action-types';
 
+import {
+	bumpStat,
+	recordTracksEvent,
+} from 'state/analytics/actions';
+
 import { savePreference } from 'state/preferences/actions';
 import { getPreference } from 'state/preferences/selectors';
+import { bucketedTimeSpentOnCurrentView } from './selectors';
 
 export function hideView( { view, enabled } ) {
 	const hideAction = {
@@ -15,6 +21,18 @@ export function hideView( { view, enabled } ) {
 	};
 
 	return ( dispatch, getState ) => {
+		const timeBucket = bucketedTimeSpentOnCurrentView( getState() );
+
+		const tracksEvent = recordTracksEvent( `calypso_first_view_dismissed`, {
+			view,
+			showAgain: enabled,
+			timeSpent: timeBucket,
+		} );
+
+
+		dispatch( bumpStat( 'calypso_first_view_dismissed', enabled ? 'show_again' : 'dont_show' ) );
+		dispatch( bumpStat( 'calypso_first_view_duration', timeBucket ) );
+		dispatch( tracksEvent );
 		dispatch( hideAction );
 		dispatch( persistToPreferences( { getState, view, disabled: ! enabled } ) );
 	};
