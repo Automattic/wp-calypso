ef95f6b60f5c450cd94c75b2e2adf60e46fef220
Jordan West <jordwest@users.noreply.github.com>
2016-07-21 15:06:13 +1000

    Plugins Page: Localize the wpcom plugin page info header (#6254)
    * Plugins Page: Localize the wpcom plugin info header

--- client/my-sites/plugins-wpcom/info-header.jsx
+++ client/my-sites/plugins-wpcom/info-header.jsx
@@ -1,22 +1,33 @@
+/**
+ * External dependencies
+ */
 import React from 'react';
+import { localize } from 'i18n-calypso';
 
+/**
+ * Internal dependencies
+ */
 import Notice from 'components/notice';
 import NoticeAction from 'components/notice/notice-action';
 
 export const InfoHeader = React.createClass( {
 	render() {
+		const { translate } = this.props;
 		return (
 			<Notice
 				status="is-info"
 				showDismiss={ false }
-				text={ "Your site comes pre-installed with a wide variety of plugins. Uploading your own plugins is not available on WordPress.com." }
+				text={ translate(
+					'Your site comes pre-installed with a wide variety of plugins. ' +
+					'Uploading your own plugins is not available on WordPress.com.'
+				) }
 			>
 				<NoticeAction href="https://en.support.wordpress.com/plugins/" external={ true }>
-					{ "Learn More" }
+					{ translate( 'Learn More' ) }
 				</NoticeAction>
 			</Notice>
 		);
 	}
 } );
 
-export default InfoHeader;
+export default localize( InfoHeader );
