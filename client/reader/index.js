a0f972c137012547455325f1f43041af2500354b
Ben Lowery <blowery@users.noreply.github.com>
2016-07-25 14:38:47 -0400

    Reader: Make reader/full-post into a section (#6974)
    * Make reader/full-post into a section
    
    * use modern exports instead of exporting an object with params
    
    * remove extra space in imports
    
    * push full post into the reader bundle
    
    * fix up the preloader to use reader instead of reader-full-post
    
    * proper name for preloader

--- client/reader/index.js
+++ client/reader/index.js
@@ -1,13 +1,13 @@
 /**
  * External dependencies
  */
-var page = require( 'page' );
+import page from 'page';
 
 /**
  * Internal dependencies
  */
-var controller = require( './controller' ),
-	config = require( 'config' );
+import controller from './controller';
+import config from 'config';
 
 function forceTeamA8C( context, next ) {
 	context.params.team = 'a8c';
@@ -16,7 +16,15 @@ function forceTeamA8C( context, next ) {
 
 module.exports = function() {
 	if ( config.isEnabled( 'reader' ) ) {
-		page( '/', controller.loadSubscriptions, controller.checkForColdStart, controller.initAbTests, controller.updateLastRoute, controller.removePost, controller.sidebar, controller.following );
+		page( '/',
+			controller.preloadReaderBundle,
+			controller.loadSubscriptions,
+			controller.checkForColdStart,
+			controller.initAbTests,
+			controller.updateLastRoute,
+			controller.removePost,
+			controller.sidebar,
+			controller.following );
 
 		// Old and incomplete paths that should be redirected to /
 		page( '/read/following', '/' );
@@ -28,26 +36,26 @@ module.exports = function() {
 		page( '/read/feed', '/' );
 
 		// Feed stream
-		page( '/read/*', controller.loadSubscriptions, controller.initAbTests );
+		page( '/read/*', controller.preloadReaderBundle, controller.loadSubscriptions, controller.initAbTests );
 		page( '/read/blog/feed/:feed_id', controller.legacyRedirects );
 		page( '/read/feeds/:feed_id/posts', controller.incompleteUrlRedirects );
-		page( '/read/feeds/:feed_id', controller.updateLastRoute, controller.prettyRedirects, controller.removePost, controller.sidebar, controller.feedDiscovery, controller.feedListing );
-		page.exit( '/read/feeds/:feed_id', controller.resetTitle );
-
-		// Feed full post
-		page( '/read/post/feed/:feed_id/:post_id', controller.legacyRedirects );
-		page( '/read/feeds/:feed/posts/:post', controller.updateLastRoute, controller.sidebar, controller.feedPost );
-		page.exit( '/read/feeds/:feed/posts/:post', controller.resetTitle );
+		page( '/read/feeds/:feed_id',
+			controller.updateLastRoute,
+			controller.prettyRedirects,
+			controller.removePost,
+			controller.sidebar,
+			controller.feedDiscovery,
+			controller.feedListing );
 
 		// Blog stream
 		page( '/read/blog/id/:blog_id', controller.legacyRedirects );
 		page( '/read/blogs/:blog_id/posts', controller.incompleteUrlRedirects );
-		page( '/read/blogs/:blog_id', controller.updateLastRoute, controller.prettyRedirects, controller.removePost, controller.sidebar, controller.blogListing );
-
-		// Blog full post
-		page( '/read/post/id/:blog_id/:post_id', controller.legacyRedirects );
-		page( '/read/blogs/:blog/posts/:post', controller.updateLastRoute, controller.sidebar, controller.blogPost );
-		page.exit( '/read/blogs/:blog/posts/:post', controller.resetTitle );
+		page( '/read/blogs/:blog_id',
+			controller.updateLastRoute,
+			controller.prettyRedirects,
+			controller.removePost,
+			controller.sidebar,
+			controller.blogListing );
 	}
 
 	// Automattic Employee Posts
