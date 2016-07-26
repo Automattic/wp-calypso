a75b2de78f5d9f16f888c0e36b32ef2fa3dee92d
Biser Perchinkov <biser@bisko.be>
2016-07-26 17:40:42 +0300

    Update Redux state schema to reflect the data saved in `SignupDependencyStore` in a more descriptive way.

--- client/state/signup/dependency-store/schema.js
+++ client/state/signup/dependency-store/schema.js
@@ -1,3 +1,34 @@
 export const dependencyStoreSchema = {
-	type: 'object'
+	type: 'object',
+	properties: {
+		cartItem: {
+			oneOf: [
+				{ type: 'null' },
+				{
+					type: 'object',
+					properties: {
+						product_slug: 'string',
+						free_trial: 'boolean',
+					}
+				}
+			]
+		},
+		designType: 'string',
+		domainItem: {
+			oneOf: [
+				{ type: 'null' },
+				{
+					type: 'object',
+					properties: {
+						is_domain_registration: 'boolean',
+						product_slug: 'string',
+						meta: 'string',
+					},
+				}
+			]
+		},
+		surveyQuestion: 'string',
+		surveySiteType: 'string',
+		theme: 'string'
+	}
 };
