c520abc453f004b6d73eff1bc3a35948e316076c
Jason Johnston <jasonjohnston76@gmail.com>
2016-07-26 16:02:07 -0700

    refactor discover helper

--- client/reader/discover/helper.jsx
+++ client/reader/discover/helper.jsx
@@ -1,77 +1,64 @@
-//var debug = require( 'debug' )( 'calypso:reader:discover' );
+/**
+ * External dependencies
+ */
+import { find, get } from 'lodash';
+import url from 'url';
+import config from 'config';
+import Debug from 'debug';
 
-// External dependencies
-var find = require( 'lodash/find' ),
-	get = require( 'lodash/get' ),
-	url = require( 'url' ),
-	config = require( 'config' );
-
+const debug = Debug( 'calypso:reader:discover' ); // eslint-disable-line
 /**
  * Internal Dependencies
  */
-var userUtils = require( 'lib/user/utils' ),
-	readerRoute = require( 'reader/route' );
+import userUtils from 'lib/user/utils';
+import { getSiteUrl as readerRouteGetSiteUrl } from 'reader/route';
 
-module.exports = {
-	isEnabled: function() {
-		return userUtils.getLocaleSlug() === 'en';
-	},
+export function isEnabled() {
+	return userUtils.getLocaleSlug() === 'en';
+}
 
-	isDiscoverPost: function( post ) {
-		return post && !! ( post.discover_metadata || post.site_ID === config( 'discover_blog_id' ) );
-	},
+export function isDiscoverPost( post ) {
+	return !! ( get( post, 'discover_metadata' ) || get( post, 'site_ID' ) === config( 'discover_blog_id' ) );
+}
 
-	isDiscoverSitePick: function( post ) {
-		return post && !! ( post.discover_metadata && find( post.discover_metadata.discover_fp_post_formats, { slug: 'site-pick' } ) );
-	},
+export function isDiscoverSitePick( post ) {
+	const metaData = get( post, 'discover_metadata.discover_fp_post_formats' );
+	return !! ( metaData && find( metaData, { slug: 'site-pick' } ) );
+}
 
-	isInternalDiscoverPost: function( post ) {
-		const featured_post_wpcom_data = get( post, 'discover_metadata.featured_post_wpcom_data' );
-		return !! featured_post_wpcom_data;
-	},
+export function isInternalDiscoverPost( post ) {
+	return !! get( post, 'discover_metadata.featured_post_wpcom_data' );
+}
 
-	getSiteUrl: function( post ) {
-		if ( ! get( post, 'discover_metadata' ) ) {
-			return false;
-		}
+export function getSiteUrl( post ) {
+	const blogId = get( post, 'discover_metadata.featured_post_wpcom_data.blog_id' );
+	// If we have a blog ID, we want to send them to the site detail page
+	return blogId ? readerRouteGetSiteUrl( blogId ) : get( post, 'discover_metadata.permalink' );
+}
 
-		// If we have a blog ID, we want to send them to the site detail page
-		const blogId = get( post, 'discover_metadata.featured_post_wpcom_data.blog_id' );
-		if ( blogId ) {
-			return readerRoute.getSiteUrl( blogId );
-		}
+export function hasSource( post ) {
+	return this.isDiscoverPost( post ) && ! this.isDiscoverSitePick( post );
+}
 
-		return post.discover_metadata.permalink;
-	},
+export function getSourceData( post ) {
+	const sourceData = get( post, 'discover_metadata.featured_post_wpcom_data' );
 
-	hasSource( post ) {
-		return post && this.isDiscoverPost( post ) && ! this.isDiscoverSitePick( post );
-	},
-
-	getSourceData: function( post ) {
-		if ( ! this.hasSource( post ) ) {
-			return null;
-		}
-
-		const data = get( post, 'discover_metadata.featured_post_wpcom_data' );
-
-		if ( ! data ) {
-			return null;
-		}
-
+	if ( sourceData && ! this.isDiscoverSitePick( post ) ) {
 		return {
-			blogId: data.blog_id,
-			postId: data.post_id
-		};
-	},
-
-	// Given an external or internal URL, return the relevant link props for an <a> tag
-	getLinkProps: function( linkUrl ) {
-		let parsedUrl = url.parse( linkUrl );
-		const isExternal = ( parsedUrl.hostname && ( parsedUrl.hostname !== window.location.hostname ) );
-		return {
-			rel: isExternal ? 'external' : '',
-			target: isExternal ? '_blank' : ''
+			blogId: get( sourceData, 'blog_id' ),
+			postId: get( sourceData, 'post_id' )
 		};
 	}
-};
+	return null;
+}
+
+export function getLinkProps( linkUrl ) {
+	const parsedUrl = url.parse( linkUrl ),
+		hostname = get( parsedUrl, 'hostname' ),
+		isExternal = hostname && hostname !== window.location.hostname;
+
+	return {
+		rel: isExternal ? 'external' : '',
+		target: isExternal ? '_blank' : ''
+	};
+}
