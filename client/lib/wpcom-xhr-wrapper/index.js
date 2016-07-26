bae5980e7b78c6acbd485209a6f65989bec407a5
retrofox <rdsuarez@gmail.com>
2016-07-26 10:04:08 -0300

    wpcom-xhr-wrapper: use `error.name` instead of `error.error`

--- client/lib/wpcom-xhr-wrapper/index.js
+++ client/lib/wpcom-xhr-wrapper/index.js
@@ -11,7 +11,7 @@ const debug = debugModule( 'calypso:wpcom-xhr-wrapper' );
 
 export default function( params, callback ) {
 	return xhr( params, function( error, response, headers ) {
-		if ( error && error.error === 'invalid_token' ) {
+		if ( error && error.name === 'InvalidTokenError' ) {
 			debug( 'Invalid token error detected, authorisation probably revoked - logging out' );
 			require( 'lib/user/utils' ).logout();
 		}
