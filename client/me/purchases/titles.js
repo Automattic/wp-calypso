dd68784950bd01267ec0b5ca135dfc1995883e2c
Alex Kirk <alex.git@kirk.at>
2016-07-26 12:05:43 +0200

    Remove out-of-place context from translatable strings

--- client/me/purchases/titles.js
+++ client/me/purchases/titles.js
@@ -3,17 +3,13 @@
  */
 import i18n from 'i18n-calypso';
 
-const options = {
-	context: 'Title text'
-};
-
 export default {
-	cancelPrivateRegistration: i18n.translate( 'Cancel Private Registration', options ),
-	cancelPurchase: i18n.translate( 'Cancel Purchase', options ),
-	confirmCancelDomain: i18n.translate( 'Cancel Domain', options ),
-	editCardDetails: i18n.translate( 'Edit Card Details', Object.assign( {}, options, { comment: 'Credit card' } ) ),
-	addCardDetails: i18n.translate( 'Add Card Details', Object.assign( {}, options, { comment: 'Credit card' } ) ),
-	editPaymentMethod: i18n.translate( 'Edit Payment Method', options ),
-	managePurchase: i18n.translate( 'Manage Purchase', options ),
-	purchases: i18n.translate( 'Purchases', options )
+	cancelPrivateRegistration: i18n.translate( 'Cancel Private Registration' ),
+	cancelPurchase: i18n.translate( 'Cancel Purchase' ),
+	confirmCancelDomain: i18n.translate( 'Cancel Domain' ),
+	editCardDetails: i18n.translate( 'Edit Card Details', { comment: 'Credit card' } ),
+	addCardDetails: i18n.translate( 'Add Card Details', { comment: 'Credit card' } ),
+	editPaymentMethod: i18n.translate( 'Edit Payment Method' ),
+	managePurchase: i18n.translate( 'Manage Purchase' ),
+	purchases: i18n.translate( 'Purchases' )
 };
