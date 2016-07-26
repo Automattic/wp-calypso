befb2c1210aaf67310b1b84ded4e700bbf1c56fc
Matías Ventura <mv@matiasventura.com>
2016-07-26 14:37:59 +0200

    Rename app-components sections in devdocs to "Blocks".

--- client/devdocs/index.js
+++ client/devdocs/index.js
@@ -15,9 +15,10 @@ export default function() {
 		page( '/devdocs/form-state-examples/:component?', controller.sidebar, controller.formStateExamples );
 		page( '/devdocs/design/typography', controller.sidebar, controller.typography );
 		page( '/devdocs/design/:component?', controller.sidebar, controller.design );
-		page( '/devdocs/app-components/:component?', controller.sidebar, controller.appComponents );
+		page( '/devdocs/app-components/:component?', controller.sidebar, controller.blocks );
+		page( '/devdocs/blocks/:component?', controller.sidebar, controller.blocks );
 		page( '/devdocs/start', controller.pleaseLogIn );
 		page( '/devdocs/welcome', controller.sidebar, controller.welcome );
 		page( '/devdocs/:path*', controller.sidebar, controller.singleDoc );
 	}
-};
+}
