4ae1f57cae6327eb39f32ceb55ea5b0d835f603e
Ben Lowery <blowery@users.noreply.github.com>
2016-07-27 09:45:31 -0400

    Reader: New Full Post skeleton (#6936)
    * first steps towards a new full post view.
    
    * Hook up the full post view to the state stores

--- client/reader/full-post/controller.js
+++ client/reader/full-post/controller.js
@@ -6,6 +6,7 @@ import ReactDom from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import i18n from 'i18n-calypso';
 import page from 'page';
+import { defer } from 'lodash';
 
 /**
  * Internal Dependencies
@@ -18,6 +19,7 @@ import {
 } from 'reader/controller-helper';
 import titleActions from 'lib/screen-title/actions';
 import FullPostDialog from './main';
+import ReaderFullPost from 'components/reader-full-post';
 
 const analyticsPageTitle = 'Reader';
 // This holds the last title set on the page. Removing the overlay doesn't trigger a re-render, so we need a way to
@@ -77,6 +79,39 @@ export function blogPost( context ) {
 	);
 }
 
+export function blogPostNew( context ) {
+	var blogId = context.params.blog,
+		postId = context.params.post,
+		basePath = '/read/blogs/:blog_id/posts/:post_id',
+		fullPageTitle = analyticsPageTitle + ' > Blog Post > ' + blogId + ' > ' + postId;
+
+	__lastTitle = TitleStore.getState().title;
+
+	trackPageLoad( basePath, fullPageTitle, 'full_post' );
+
+	ReactDom.render(
+		React.createElement( ReduxProvider, { store: context.store },
+			React.createElement( ReaderFullPost, {
+				blogId: blogId,
+				postId: postId,
+				context: context,
+				setPageTitle: setPageTitle,
+				onClose: function() {
+					page.back( context.lastRoute || '/' );
+				},
+				onClosed: removeFullPostDialog,
+				onPostNotFound: renderPostNotFound
+			} )
+		),
+		document.getElementById( 'primary' )
+	);
+	defer( function() {
+		if ( typeof window !== 'undefined' ) {
+			window.scrollTo( 0, 0 );
+		}
+	} );
+}
+
 export function feedPost( context ) {
 	const feedId = context.params.feed,
 		postId = context.params.post,
@@ -105,4 +140,36 @@ export function feedPost( context ) {
 		),
 		document.getElementById( 'tertiary' )
 	);
+}
+
+export function feedPostNew( context ) {
+	var feedId = context.params.feed,
+		postId = context.params.post,
+		basePath = '/read/feeds/:feed_id/posts/:feed_item_id',
+		fullPageTitle = analyticsPageTitle + ' > Feed Post > ' + feedId + ' > ' + postId;
+
+	__lastTitle = TitleStore.getState().title;
+
+	trackPageLoad( basePath, fullPageTitle, 'full_post' );
+
+	function closer() {
+		page.back( context.lastRoute || '/' );
+	}
+
+	ReactDom.render( (
+		<ReduxProvider store={ context.store }>
+			<ReaderFullPost
+				feedId={ feedId }
+				postId={ postId }
+				setPageTitle={ setPageTitle }
+				onClose={ closer }
+				onPostNotFound={ renderPostNotFound } />
+		</ReduxProvider> ),
+		document.getElementById( 'primary' )
+	);
+	defer( function() {
+		if ( typeof window !== 'undefined' ) {
+			window.scrollTo( 0, 0 );
+		}
+	} );
 }
--- client/reader/full-post/index.js
+++ client/reader/full-post/index.js
@@ -6,7 +6,8 @@ import page from 'page';
 /**
  * Internal dependencies
  */
-import { resetTitle, blogPost, feedPost } from './controller';
+import config from 'config';
+import { resetTitle, blogPost, blogPostNew, feedPost, feedPostNew } from './controller';
 import readerController from 'reader/controller';
 import pageNotifier from 'lib/route/page-notifier';
 import { hideReaderFullPost } from 'state/ui/reader/fullpost/actions';
@@ -27,15 +28,15 @@ export default function() {
 	page( '/read/post/feed/:feed_id/:post_id', readerController.legacyRedirects );
 	page( '/read/feeds/:feed/posts/:post',
 		readerController.updateLastRoute,
-		readerController.sidebar,
-		feedPost );
+		config.isEnabled( 'reader/refresh-2016-07' ) ? readerController.unmountSidebar : readerController.sidebar,
+		config.isEnabled( 'reader/refresh-2016-07' ) ? feedPostNew : feedPost );
 	page.exit( '/read/feeds/:feed/posts/:post', resetTitle );
 
 	// Blog full post
 	page( '/read/post/id/:blog_id/:post_id', readerController.legacyRedirects );
 	page( '/read/blogs/:blog/posts/:post',
 		readerController.updateLastRoute,
-		readerController.sidebar,
-		blogPost );
+		config.isEnabled( 'reader/refresh-2016-07' ) ? readerController.unmountSidebar : readerController.sidebar,
+		config.isEnabled( 'reader/refresh-2016-07' ) ? blogPostNew : blogPost );
 	page.exit( '/read/blogs/:blog/posts/:post', resetTitle );
 }
