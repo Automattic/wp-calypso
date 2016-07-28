d20ed576979ca93b3a8111cfa9248b16080e21a8
mattsherman <matt@jam123.com>
2016-07-28 15:24:14 -0400

    Add new ab test for browser notification preferences

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -1,9 +1,31 @@
 module.exports = {
+	// `browserNotifications` controls whether or not users see the
+	// nudge notice to enable browser notifications at the top of
+	// some Calypso screens; any users with this enabled will also
+	// have the preference available in /me/notifications;
+	// note: not renaming this test at this point in time so that we don't
+	// mess with any users that were put in the `enabled` variation -- don't
+	// want to take their browser notifications preference away from them!
 	browserNotifications: {
 		datestamp: '20160628',
 		variations: {
 			disabled: 95,
 			enabled: 5,
+		},
+		defaultVariation: 'disabled',
+		allowExistingUsers: true,
+	},
+	// `browserNotificationsPreferences` controls whether or not users see the
+	// preference to enable browser notifications in /me/notifications;
+	// any users with `browserNotifications` enabled will also see the preference;
+	// this is a temporary test just to allow us to ramp up the load gradually so
+	// that make sure things will be smooth when we go to launching browser
+	// notifications for 100% of users
+	browserNotificationsPreferences: {
+		datestamp: '20160801',
+		variations: {
+			disabled: 0,
+			enabled: 40,
 		},
 		defaultVariation: 'disabled',
 		allowExistingUsers: true,
