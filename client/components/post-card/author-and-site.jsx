0dd5d840041a0576d57951e1f4c0763373d2b163
Jan Cavan Boulas <mj12982@gmail.com>
2016-07-28 11:52:03 -0700

    Clean up Search css links

--- client/components/post-card/author-and-site.jsx
+++ client/components/post-card/author-and-site.jsx
@@ -20,19 +20,19 @@ export function AuthorAndSite( { translate, post, site, feed, showGravatar = fal
 	const siteName = site && site.title || post.site_name;
 
 	const username = (
-		<span className="post-card__author-and-site-author">
-			<a href={ `/read/blogs/${post.site_ID}` } onClick={ partial( onClick, { post, site, feed } ) }>
+		<span className="post-card__search-byline-author">
+			<a className="post-card__byline-link" href={ `/read/blogs/${post.site_ID}` } onClick={ partial( onClick, { post, site, feed } ) }>
 				{ showGravatar && <Gravatar user={ post.author } size={ 16 } /> }
 				{ displayName }
 			</a>
 		</span>
 	);
 
-	const sitename = ( <span className="post-card__author-and-site-site">
-		<a href={ `/read/blogs/${post.site_ID}` } onClick={ partial( onClick, { post, site, feed } ) }>{ siteName }</a>
+	const sitename = ( <span className="post-card__byline-site">
+		<a className="post-card__byline-link" href={ `/read/blogs/${post.site_ID}` } onClick={ partial( onClick, { post, site, feed } ) }>{ siteName }</a>
 	</span> );
 	return (
-		<span className="post-card__author-and-site">
+		<span className="post-card__byline-author-and-site">
 			{
 				displayName === '' || siteName === displayName
 				? sitename
