4f860c35dfbb36c181db8641b34664a1a700fa1a
bluefuton <chris@bluefuton.com>
2016-07-28 17:01:57 +0200

    Use class names, not bare elements

--- client/components/reader-full-post/header-tags.jsx
+++ client/components/reader-full-post/header-tags.jsx
@@ -8,7 +8,11 @@ const ReaderFullPostHeaderTags = ( { tags } ) => {
 	const numberOfTagsToDisplay = 5;
 	const tagsToDisplay = take( values( tags ), numberOfTagsToDisplay );
 	const listItems = map( tagsToDisplay, tag => {
-		return ( <li key={ `post-tag-${tag.slug}` }><a href={ `/tag/${tag.slug}` }>{ tag.display_name }</a></li> );
+		return (
+			<li key={ `post-tag-${tag.slug}` } className="reader-full-post__header-tag-list-item">
+				<a href={ `/tag/${tag.slug}` } className="reader-full-post__header-tag-list-item-link">{ tag.display_name }</a>
+			</li>
+		);
 	} );
 
 	return (
