741a4e4ddcbfd947c92624a11d2eccf792d2d062
Rastislav Lamos <lamos.rasto@gmail.com>
2016-07-29 11:27:26 +0200

    Add proper redirects for /plans section (#7099)

--- client/my-sites/plans/controller.jsx
+++ client/my-sites/plans/controller.jsx
@@ -16,7 +16,7 @@ import route from 'lib/route';
 import sitesFactory from 'lib/sites-list';
 import titleActions from 'lib/screen-title/actions';
 import get from 'lodash/get';
-import { isValidFeatureKey, isPlanFeaturesEnabled } from 'lib/plans';
+import { isValidFeatureKey } from 'lib/plans';
 
 const sites = sitesFactory();
 
@@ -89,13 +89,8 @@ export default {
 			productsList = require( 'lib/products-list' )(),
 			analyticsPageTitle = 'Plans > Compare',
 			site = sites.getSelectedSite(),
-			siteDomain = context.params.domain || '',
 			basePath = route.sectionify( context.path );
 		let baseAnalyticsPath;
-
-		if ( isPlanFeaturesEnabled() ) {
-			return page.redirect( `/plans/features/${ siteDomain }` );
-		}
 
 		if ( site && ! site.isUpgradeable() ) {
 			return page.redirect( '/plans/compare' );
@@ -150,5 +145,15 @@ export default {
 	redirectToCheckout( context ) {
 		// this route is deprecated, use `/checkout/:site/:plan` to link to plan checkout
 		page.redirect( `/checkout/${ context.params.domain }/${ context.params.plan }` );
+	},
+
+	redirectToPlans( context ) {
+		const siteDomain = context.params.domain;
+
+		if ( siteDomain ) {
+			return page.redirect( `/plans/${ siteDomain }` );
+		}
+
+		return page.redirect( '/plans' );
 	}
 };
