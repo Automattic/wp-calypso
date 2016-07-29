51ecd7282396f2240c735f06b3a305eeadccaf37
Andrew Duthie <andrew@andrewduthie.com>
2016-07-29 16:07:29 -0400

    State: Throw on undefined type in action
    Intended to capture cases where forgetting to define action type

--- client/state/utils.js
+++ client/state/utils.js
@@ -105,6 +105,11 @@ export function createReducer( initialState = null, customHandlers = {}, schema 
 	return ( state = initialState, action ) => {
 		const { type } = action;
 
+		if ( 'production' !== process.env.NODE_ENV && 'type' in action && ! type ) {
+			throw new TypeError( 'Reducer called with undefined type.' +
+				' Verify that the action type is defined in state/action-types.js' );
+		}
+
 		if ( handlers.hasOwnProperty( type ) ) {
 			return handlers[ type ]( state, action );
 		}
