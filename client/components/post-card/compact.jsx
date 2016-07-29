cfdf87ead6095b2e5ff24d589e001622da4e1569
Jan Cavan Boulas <mj12982@gmail.com>
2016-07-29 13:21:10 -0700

    Reader: update small to post-card__compact

--- client/components/post-card/compact.jsx
+++ client/components/post-card/compact.jsx
@@ -15,20 +15,20 @@ import safeImageUrl from 'lib/safe-image-url';
 import resizeImageUrl from 'lib/resize-image-url';
 import AuthorAndSite from './author-and-site';
 
-export function SmallPostCard( { translate, post, site, onPostClick = noop, onSiteClick = noop } ) {
-	const classes = classnames( 'post-card small', {
+export function CompactPostCard( { translate, post, site, onPostClick = noop, onSiteClick = noop } ) {
+	const classes = classnames( 'post-card__compact small', {
 		'has-image': post.canonical_image
 	} );
 	const displayName = post.author.name;
 	const siteName = site && site.title || post.site_name;
 
 	const username = (
-		<span className="post-card__author">
+		<span className="post-card__compact-author">
 			<a href={ `/read/blogs/${post.site_ID}` } onClick={ partial( onSiteClick, site, post ) }>{ displayName }</a>
 		</span>
 	);
 
-	const sitename = ( <span className="post-card__site-title">
+	const sitename = ( <span className="post-card__compact-site-title">
 		<a href={ `/read/blogs/${post.site_ID}` } onClick={ partial( onSiteClick, site, post ) }>{ siteName }</a>
 	</span> );
 
@@ -36,22 +36,22 @@ export function SmallPostCard( { translate, post, site, onPostClick = noop, onSi
 
 	return (
 		<Card className={ classes }>
-			<div className="post-card__site-info-title">
-				<h1 className="post-card__title">
-					<a className="post-card__anchor" href={ `/read/blogs/${post.site_ID}/posts/${post.ID}` } onClick={ partial( onPostClick, post ) }>{ post.title }</a>
+			<div className="post-card__compact-site-info-title">
+				<h1 className="post-card__compact-title">
+					<a className="post-card__compact-anchor" href={ `/read/blogs/${post.site_ID}/posts/${post.ID}` } onClick={ partial( onPostClick, post ) }>{ post.title }</a>
 				</h1>
-				<div className="post-card__site-info">
+				<div className="post-card__compact-site-info">
 					<AuthorAndSite post={ post } site={ site } />
 				</div>
 			</div>
 			<div>
 			{ post.canonical_image && (
 					<a href={ `/read/blogs/${post.site_ID}/posts/${post.ID}` } onClick={ partial( onPostClick, post ) }>
-						<img className="post-card__thumbnail" src={ thumbnailUrl } />
+						<img className="post-card__compact-thumbnail" src={ thumbnailUrl } />
 					</a> ) }
 			</div>
 		</Card>
 	);
 }
 
-export default localize( SmallPostCard );
+export default localize( CompactPostCard );
