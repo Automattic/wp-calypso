af3e83b91164c2ba2cab44fde53e213a2a94d25f
bluefuton <chris@bluefuton.com>
2016-07-29 18:47:30 +0200

    Translate follower count

--- client/blocks/author-compact-profile/index.jsx
+++ client/blocks/author-compact-profile/index.jsx
@@ -8,6 +8,9 @@ import React from 'react';
  */
 import Gravatar from 'components/gravatar';
 import ReaderAuthorLink from 'components/reader-author-link';
+import ReaderSiteStreamLink from 'components/reader-site-stream-link';
+import ReaderFollowButton from 'reader/follow-button';
+import { localize } from 'i18n-calypso';
 
 const AuthorCompactProfile = React.createClass( {
 	propTypes: {
@@ -16,14 +19,38 @@ const AuthorCompactProfile = React.createClass( {
 
 	render() {
 		const author = this.props.author;
+
+		// @todo these need to come from props
+		const siteName = 'Bananas';
+		const siteUrl = 'http://wpcalypso.wordpress.com';
+		const followCount = 123;
+		const feedId = 123;
+		const siteId = 123;
+
 		return (
 			<div className="author-compact-profile">
 				<Gravatar size={ 96 } user={ author } />
 				<ReaderAuthorLink author={ author }>{ author.display_name }</ReaderAuthorLink>
+				<ReaderSiteStreamLink feedId={ feedId } siteId={ siteId }>
+					{ siteName }
+				</ReaderSiteStreamLink>
+				<div className="author-compact-profile__follower-count">
+					{ this.props.translate(
+						'%(followCount)d follower',
+						'%(followCount)d followers',
+						{
+							count: followCount,
+							args: {
+								followCount: followCount
+							}
+						}
+					) }
+				</div>
+				<ReaderFollowButton siteUrl={ siteUrl } />
 			</div>
 		);
 	}
 
 } );
 
-export default AuthorCompactProfile;
+export default localize( AuthorCompactProfile );
