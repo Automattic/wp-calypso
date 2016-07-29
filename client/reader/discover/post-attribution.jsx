ceeacff7a366a3bfa521ba231e9f6d4b53c4a16d
Jason Johnston <jasonjohnston76@gmail.com>
2016-07-29 09:03:46 -0700

    Update: add site attribution to full-post view
    Clean up css and adjust for full-post context

--- client/reader/discover/post-attribution.jsx
+++ client/reader/discover/post-attribution.jsx
@@ -9,7 +9,7 @@ import classNames from 'classnames';
  */
 import FollowButton from 'reader/follow-button';
 import { getLinkProps } from './helper';
-import { stats } from 'reader/stats';
+import { recordTrack } from 'reader/stats';
 
 const arrowGridicon = ( <svg className="gridicon gridicon-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 11H6.414l6.293-6.293-1.414-1.414L2.586 12l8.707 8.707 1.414-1.414L6.414 13H20"/></svg> );
 
@@ -28,13 +28,13 @@ const DiscoverPostAttribution = React.createClass( {
 	},
 
 	recordAuthorClick() {
-		stats.recordTrack( 'calypso_reader_author_on_discover_card_clicked', {
+		recordTrack( 'calypso_reader_author_on_discover_card_clicked', {
 			author_url: this.props.attribution.author_url
 		} );
 	},
 
 	recordSiteClick() {
-		stats.recordTrack( 'calypso_reader_site_on_discover_card_clicked', {
+		recordTrack( 'calypso_reader_site_on_discover_card_clicked', {
 			site_url: this.props.siteUrl
 		} );
 	},
