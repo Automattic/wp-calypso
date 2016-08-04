1314b990ca7c75be1d74653dccf0e9cf7847fbe9
Steve Seear <steve.seear@gmail.com>
2016-08-04 16:33:30 +0100

    Add doc

--- client/my-sites/themes/theme-filters.js
+++ client/my-sites/themes/theme-filters.js
@@ -1,5 +1,10 @@
 /**
-* Functions for validating theme search filters.
+* Functions for working with theme search filters. The filter syntax is
+* {taxonomy}:{term}
+* allowing whitespace after the :
+*
+* Valid values for {taxonomy} and {term} are contained in the
+* `taxonomies` object.
 */
 
 /**
@@ -7,6 +12,7 @@
  */
 import forIn from 'lodash/forIn';
 
+// Regular expressions for matching "taxonomy:term" search-box syntax
 const FILTER_REGEX_STRING = '(\\w+)\\:\\s*([\\w-]+)';
 const FILTER_REGEX_GLOBAL = new RegExp( FILTER_REGEX_STRING, 'g' );
 const FILTER_REGEX_SINGLE = new RegExp( '^' + FILTER_REGEX_STRING + '$' );
@@ -182,6 +188,7 @@ function getTermTable() {
 	return termTable;
 }
 
+// return specified part of a taxonomy:term string
 function splitFilter( filter, group ) {
 	const match = filter.match( FILTER_REGEX_SINGLE );
 	if ( match ) {
@@ -190,10 +197,12 @@ function splitFilter( filter, group ) {
 	return '';
 }
 
+// return term from a taxonomy:term string
 function getTerm( filter ) {
 	return splitFilter( filter, FILTER_TERM_GROUP );
 }
 
+// return taxonomy from a taxonomy:term string
 function getTaxonomy( filter ) {
 	return splitFilter( filter, FILTER_TAXONOMY_GROUP );
 }
@@ -203,7 +212,7 @@ function getTaxonomy( filter ) {
  * in "taxonomy:term" search-box format.
  *
  * @param {string} term - the term slug
- * @returns {string} - complete taxonomy:term filter, or empty string if term is not valid
+ * @return {string} - complete taxonomy:term filter, or empty string if term is not valid
  */
 export function getFilter( term ) {
 	const terms = getTermTable();
@@ -214,19 +223,36 @@ export function getFilter( term ) {
 }
 
 /**
- * Checks that a taxonomy:term filter is valid.
+ * Checks that a taxonomy:term filter is valid, using the theme
+ * taxonomy data.
  *
  * @param {string} filter - filter in form taxonomy:term
- * @returns {boolean} true if filter pair is valid
+ * @return {boolean} true if filter pair is valid
  */
 function filterIsValid( filter ) {
 	return getTermTable()[ getTerm( filter ) ] === getTaxonomy( filter );
 }
 
+/**
+ * Return a sorted array of filter terms.
+ *
+ * Sort is alphabetical on the complete "taxonomy:term" string.
+ *
+ * @param {array} terms - Array of term strings
+ * @return {array} sorted array
+ */
 export function sortFilterTerms( terms ) {
 	return terms.map( getFilter ).sort().map( getTerm );
 }
 
+/**
+ * Return a string of valid, sorted, comma-separated filter
+ * terms from an input string. Input may contain search
+ * terms (which will be ignored) as well as filters.
+ *
+ * @param {string} input - the string to parse
+ * @return {string} comma-seperated list of valid filters
+ */
 export function getSortedFilterTerms( input ) {
 	const matches = input.match( FILTER_REGEX_GLOBAL );
 	if ( matches ) {
@@ -236,6 +262,12 @@ export function getSortedFilterTerms( input ) {
 	return '';
 }
 
+/**
+ * Strips any "taxonomy:term" filter strings from the input.
+ *
+ * @param {string} input - the string to parse
+ * @return {string} input string minus any filters
+ */
 export function stripFilters( input ) {
 	return input.replace( FILTER_REGEX_GLOBAL, '' ).trim();
 }
