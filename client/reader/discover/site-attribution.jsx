cd87a16fad895c93edc57829607d1936eda64a5a
Jason Johnston <jasonjohnston76@gmail.com>
2016-08-04 18:52:13 -0700

    update stats handlers for discover components
    plus some ES6 refactoring

--- client/reader/discover/site-attribution.jsx
+++ client/reader/discover/site-attribution.jsx
@@ -7,12 +7,23 @@
 /**
  * Internal dependencies
  */
+ import { translate } from 'i18n-calypso';
  import FollowButton from 'reader/follow-button';
  import { getLinkProps } from './helper';
+ import * as discoverStats from './stats';
 
-var DiscoverSiteAttribution = React.createClass( {
+// var DiscoverSiteAttribution = React.createClass( {
+class DiscoverSiteAttribution extends React.Component {
 
-	propTypes: {
+	constructor( props ) {
+		super( props );
+
+		[ 'recordSiteClick', 'recordFollowToggle' ].forEach( ( method ) => {
+			this[ method ] = this[ method ].bind( this );
+		} );
+	}
+
+	static propTypes = {
 		attribution: React.PropTypes.shape( {
 			blog_name: React.PropTypes.string.isRequired,
 			blog_url: React.PropTypes.string.isRequired,
@@ -20,9 +31,17 @@ var DiscoverSiteAttribution = React.createClass( {
 		} ).isRequired,
 		siteUrl: React.PropTypes.string.isRequired,
 		followUrl: React.PropTypes.string.isRequired
-	},
+	}
 
-	render: function() {
+	recordSiteClick( ) {
+		discoverStats.recordSiteClick( this.props.siteUrl );
+	}
+
+	recordFollowToggle( isFollowing ) {
+		discoverStats.recordFollowToggle( isFollowing, this.props.siteUrl );
+	}
+
+	render() {
 		const attribution = this.props.attribution;
 		const classes = classNames( 'discover-attribution is-site', {
 			'is-missing-avatar': ! attribution.avatar_url
@@ -34,15 +53,15 @@ var DiscoverSiteAttribution = React.createClass( {
 			<div className={ classes }>
 				{ attribution.avatar_url ? <img className="gravatar" src={ encodeURI( attribution.avatar_url ) } alt="Avatar" width="20" height="20" /> : <span className="noticon noticon-website"></span> }
 				<span>
-					<a { ...siteLinkProps } className={ siteClasses } href={ encodeURI( this.props.siteUrl ) }>
-						{ this.translate( 'visit' ) } <em>{ attribution.blog_name }</em>
+					<a { ...siteLinkProps } className={ siteClasses } href={ encodeURI( this.props.siteUrl ) } onClick={ this.recordSiteClick }>
+						{ translate( 'visit' ) } <em>{ attribution.blog_name }</em>
 					</a>
 				</span>
-				{ !! this.props.followUrl ? <FollowButton siteUrl={ this.props.followUrl } iconSize={ 20 } /> : null }
+				{ !! this.props.followUrl ? <FollowButton siteUrl={ this.props.followUrl } iconSize={ 20 } onFollowToggle={ this.recordFollowToggle }/> : null }
 			</div>
 		);
 	}
 
-} );
+}
 
 module.exports = DiscoverSiteAttribution;
