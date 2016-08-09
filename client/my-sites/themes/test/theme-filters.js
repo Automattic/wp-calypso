6fbb602eb17ddbc5597daf54c362246d2b402c61
Steve Seear <steve.seear@gmail.com>
2016-08-09 10:20:30 +0100

    Themes: Sort filters by taxonomy then term (#7271)
    * Themes: sort filters by taxonomy then term
    
    * Tighten the interface to theme-filters module
    
    * Add doc
    
    * Tests for theme-filters.js
    
    * Tweak

---
+++ client/my-sites/themes/test/theme-filters.js
@@ -0,0 +1,84 @@
+/**
+ * Tests for theme-filters.js
+ */
+
+/**
+ * External dependencies
+ */
+import { assert } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import {
+	getFilter,
+	getSortedFilterTerms,
+	stripFilters,
+	sortFilterTerms,
+} from '../theme-filters.js';
+
+describe( 'theme-filters', () => {
+	describe( 'getFilter', () => {
+		it( 'should return taxonomy:term filter given the term', () => {
+			assert.equal( getFilter( 'pink' ), 'color:pink' );
+			assert.equal( getFilter( 'real-estate' ), 'subject:real-estate' );
+		} );
+
+		it( 'should return empty string given an invalid term', () => {
+			assert.equal( getFilter( '' ), '' );
+			assert.equal( getFilter( ' ' ), '' );
+			assert.equal( getFilter( ' pink' ), '' );
+			assert.equal( getFilter( 'pink ' ), '' );
+			assert.equal( getFilter( 'ponk' ), '' );
+		} );
+	} );
+
+	describe( 'sortFilterTerms', () => {
+		it( 'should alpha sort by taxonomy:term', () => {
+			const sorted = sortFilterTerms(
+				[ 'post-slider', 'blue', 'yellow' ]
+			);
+			// alpha-sort following strings:
+			// 'feature:post-slider'
+			// 'color:blue'
+			// 'color:yellow'
+			assert.deepEqual( sorted, [ 'blue', 'yellow', 'post-slider' ] );
+		} );
+
+		it( 'should strip invalid terms', () => {
+			const sorted = sortFilterTerms(
+				[ 'yellow', 'yaya', 'blue' ]
+			);
+			assert.deepEqual( sorted, [ 'blue', 'yellow' ] );
+		} );
+	} );
+
+	describe( 'getSortedFilterTerms', () => {
+		it( 'should return terms from a mixed input string', () => {
+			const terms = getSortedFilterTerms( 'blah rah color:blue yah feature: post-slider blah' );
+			assert.equal( terms, 'blue,post-slider' );
+		} );
+
+		it( 'should ignore invalid filters', () => {
+			const terms = getSortedFilterTerms( 'color:lazer-puce subject:business' );
+			assert.equal( terms, 'business' );
+		} );
+
+		it( 'should return empty string if no valid filters', () => {
+			const terms = getSortedFilterTerms( 'blah color:lazer-puce' );
+			assert.equal( terms, '' );
+		} );
+	} );
+
+	describe( 'stripFilters', () => {
+		it( 'should strip filters from a mixed input string', () => {
+			const remaining = stripFilters( 'blah color:blue yah' );
+			assert.equal( remaining, 'blah  yah' );
+		} );
+
+		it( 'should still strip invalid filters', () => {
+			const remaining = stripFilters( 'color:blue twenty color:lazer-puce' );
+			assert.equal( remaining, 'twenty' );
+		} );
+	} );
+} );
