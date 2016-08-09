8df4db648a7b7b106a49bd69326e1db7c1f0ccdb
ehg <chris@ehg.sh>
2016-08-09 11:34:37 +0100

    GuidedTours: First pass at converting framework to JSX
    To make tours more flexible, and easier to both read and write, we're switching the configuration of Guided Tours to be JSX-based.
    
    Changes:
    - A tour config is a now JSX tree describing the series of steps that make up the tour
    - A tour config must be exported as a component usable by Guided Tours using makeGuidedTour
    - Control flow of a tour is handled by Tour and Step.
    - Tour is the top-level element in a tour config:
      - It is passed state, as well as two bound methods from GuidedTours: next and quit
      - It is responsible for transmitting next and quit to its descendants using React context
      - Using the current stepName, it picks one of its children (Step elements) to render
    - Step represents a single step:
      - It is passed state from Tour's React child context
      - It is passed an optional `when` function via JSX
      - It makes sure that `when( state )` is truthy, otherwise it calls the React context-borne next method, thus skipping to the tour's next step.
      - For valid or absent `when` conditions, it renders its contents (children) similarly to how we did it before
    - Next and Quit are what allows the user to move through the tour:
      - Both retrieve next or quit via React context
      - They render buttons that call those methods on click
    - Continue is a special helper that sets up event listeners on designated targets, or allows a step to continue to based on its `when` prop:
      - Example use case: "Continue the tour once the user clicks on Calypso's My Sites masterbar link"
         - Usage: <Continue target="my-sites" click />
      - Example use case: "Continue the tour once a web preview has been closed"
         - Usage: <Continue when={ previewNotShowing }/>
    
    props @lsinger, @mcsf

---
+++ client/state/ui/guided-tours/contexts.js
@@ -0,0 +1,34 @@
+/** @ssr-ready **/
+
+import config from 'config';
+import { getSectionName, isPreviewShowing, getSelectedSite } from 'state/ui/selectors';
+import { getCurrentUser } from 'state/current-user/selectors';
+
+export const inSection = sectionName => state =>
+	getSectionName( state ) === sectionName;
+
+export const isEnabled = feature => () =>
+	config.isEnabled( feature );
+
+export const previewIsNotShowing = state =>
+	! isPreviewShowing( state );
+
+export const previewIsShowing = state =>
+	isPreviewShowing( state );
+
+const WEEK_IN_MILLISECONDS = 7 * 1000 * 3600 * 24;
+export const isNewUser = state => {
+	const user = getCurrentUser( state );
+	if ( ! user ) {
+		return false;
+	}
+
+	const creation = Date.parse( user.date );
+	return ( Date.now() - creation ) <= WEEK_IN_MILLISECONDS;
+};
+
+export const selectedSiteIsPreviewable = state =>
+	getSelectedSite( state ) && getSelectedSite( state ).is_previewable;
+
+export const selectedSiteIsCustomizable = state =>
+	getSelectedSite( state ) && getSelectedSite( state ).is_customizable;
--- webpack.config.node.js
+++ webpack.config.node.js
@@ -90,7 +90,8 @@ module.exports = {
 		new webpack.NormalModuleReplacementPlugin( /^my-sites\/themes\/thanks-modal$/, 'components/empty-component' ), // Depends on BOM
 		new webpack.NormalModuleReplacementPlugin( /^my-sites\/themes\/themes-site-selector-modal$/, 'components/empty-component' ), // Depends on BOM
 		new webpack.NormalModuleReplacementPlugin( /^state\/ui\/editor\/selectors$/, 'lodash/noop' ), // will never be called server-side
-		new webpack.NormalModuleReplacementPlugin( /^state\/posts\/selectors$/, 'lodash/noop' ) // will never be called server-side
+		new webpack.NormalModuleReplacementPlugin( /^state\/posts\/selectors$/, 'lodash/noop' ), // will never be called server-side
+		new webpack.NormalModuleReplacementPlugin( /^client\/layout\/guided-tours\/config$/, 'components/empty-component' ) // should never be required server side
 	],
 	externals: getExternals()
 };
