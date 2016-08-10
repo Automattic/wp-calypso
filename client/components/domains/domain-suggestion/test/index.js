0a6b0cdbaa8135f61390e02eec76f07ddf5b98ea
Marco Aurelio <thecoreh@gmail.com>
2016-08-10 15:36:40 -0300

    Fix broken test

--- client/components/domains/domain-suggestion/test/index.js
+++ client/components/domains/domain-suggestion/test/index.js
@@ -30,9 +30,13 @@ describe( 'Domain Suggestion', function() {
 		it( 'should have data-e2e-domain attribute for e2e testing', () => {
 			const domainSuggestion = shallow( <DomainSuggestion
 				domain="example.com" isAdded={ false }/> );
-			const domainSuggestionButton = domainSuggestion.find( `.domain-suggestion__select-button` );
-			expect( domainSuggestionButton.length ).to.equal( 1 );
-			expect( domainSuggestionButton.props()[ 'data-e2e-domain' ] ).to.equal( 'example.com' );
+			if ( domainSuggestion.props()[ 'data-e2e-domain' ] ) {
+				expect( domainSuggestion.props()[ 'data-e2e-domain' ] ).to.equal( 'example.com' )
+			} else {
+				const domainSuggestionButton = domainSuggestion.find( `.domain-suggestion__select-button` );
+				expect( domainSuggestionButton.length ).to.equal( 1 );
+				expect( domainSuggestionButton.props()[ 'data-e2e-domain' ] ).to.equal( 'example.com' );
+			}
 		} );
 	} );
 } );
