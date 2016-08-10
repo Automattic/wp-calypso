d3e68262e322a72a0ad26cb19c0017de9ae4cf03
Dennis Snell <dennis.snell@automattic.com>
2016-08-09 17:11:13 -0700

    Pull in unstyled avatars from wrnc

--- client/notifications/avatar.jsx
+++ client/notifications/avatar.jsx
@@ -1,1 +1,57 @@
-export default () => null;
+import React, { PropTypes } from 'react';
+
+import MysteryMan from './mystery-man';
+
+export const Avatar = React.createClass( {
+	getInitialState: () => ( {
+		hasLoaded: false
+	} ),
+
+	componentWillMount() {
+		const { src } = this.props;
+
+		this.fetchAvatar( src, null );
+	},
+
+	componentWillReceiveProps( { src } ) {
+		this.fetchAvatar( src, this.props.src );
+	},
+
+	componentWillUnmount() {
+		this.avatar.removeEventListener( 'load', this.replaceMysteryMan );
+	},
+
+	fetchAvatar( src, prevSrc ) {
+		if ( src === prevSrc ) {
+			return;
+		}
+
+		const avatar = new Image();
+		avatar.addEventListener( 'load', this.replaceMysteryMan );
+		avatar.src = src;
+		this.avatar = avatar;
+
+		this.setState( {
+			hasLoaded: !! avatar.complete
+		} );
+	},
+
+	replaceMysteryMan() {
+		this.setState( { hasLoaded: true } );
+	},
+
+	render() {
+		const { hasLoaded } = this.state;
+		const { src } = this.props;
+
+		return hasLoaded
+			? <img className="avatar" { ...{ src } } />
+			: <div className="avatar"><MysteryMan /></div>;
+	}
+} );
+
+Avatar.propTypes = {
+	src: PropTypes.string
+};
+
+export default Avatar;
