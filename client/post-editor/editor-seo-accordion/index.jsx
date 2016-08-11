3bcac63cc821d71f48680c20e7c6fbeaf1ca48ca
Timmy Crawford <timmydcrawford@gmail.com>
2016-08-11 13:21:19 -0700

    Use EditorDrawerLabel where applicable

--- client/post-editor/editor-seo-accordion/index.jsx
+++ client/post-editor/editor-seo-accordion/index.jsx
@@ -11,8 +11,8 @@ import Accordion from 'components/accordion';
 import AccordionSection from 'components/accordion/section';
 import CountedTextarea from 'components/forms/counted-textarea';
 import Gridicon from 'components/gridicon';
-import InfoPopover from 'components/info-popover';
 import PostActions from 'lib/posts/actions';
+import EditorDrawerLabel from 'post-editor/editor-drawer/label';
 
 function EditorSeoAccordion( { translate, metaDescription = '' } ) {
 	return (
@@ -22,15 +22,13 @@ function EditorSeoAccordion( { translate, metaDescription = '' } ) {
 			className="editor-seo-accordion"
 		>
 			<AccordionSection>
-				<span className="editor-drawer__label-text">
+				<EditorDrawerLabel
+					helpText={ translate(
+						'Craft a description of your post for search engine results. ' +
+						'The post content is used by default.'
+					) }>
 					{ translate( 'Meta Description' ) }
-					<InfoPopover position="top left">
-						{ translate(
-							'Craft a description of your post for search engine results. ' +
-							'The post content is used by default.'
-						) }
-					</InfoPopover>
-				</span>
+				</EditorDrawerLabel>
 				<CountedTextarea
 					maxLength="300"
 					acceptableLength={ 159 }
