c23aa38548662488c500fa85427fd76d811c2d52
Copons <copons@users.noreply.github.com>
2016-08-11 13:21:07 +0200

    Cleaner code with lodash map and pick
    See:
    https://github.com/Automattic/wp-calypso/pull/7320#discussion_r74295297
    https://github.com/Automattic/wp-calypso/pull/7320#discussion_r74295442
    https://github.com/Automattic/wp-calypso/pull/7320#discussion_r74296373

--- client/post-editor/editor-copy-post/index.js
+++ client/post-editor/editor-copy-post/index.js
@@ -4,6 +4,7 @@
 import actions from 'lib/posts/actions';
 import { editPost } from 'state/posts/actions';
 import { setEditorPostId } from 'state/ui/editor/actions';
+import { map, pick } from 'lodash';
 import wpcom from 'lib/wp';
 
 function setPostCopy( post ) {
@@ -12,29 +13,21 @@ function setPostCopy( post ) {
 			type: 'post',
 			content: post.content,
 		},
-		attributes: {
-			categories: Object.keys( post.categories ).map( key => post.categories[ key ].ID ),
-			excerpt: post.excerpt,
-			tags: Object.keys( post.tags ).map( key => post.tags[ key ].name ),
-			title: post.title,
-		},
+		attributes: {},
 	};
 
-	if ( post.canonical_image ) {
-		postCopy.attributes.canonical_image = post.canonical_image;
-	}
-	if ( post.featured_image ) {
-		postCopy.attributes.featured_image = post.featured_image;
-	}
-	if ( post.format ) {
-		postCopy.attributes.format = post.format;
-	}
-	if ( post.metadata ) {
-		postCopy.attributes.metadata = post.metadata;
-	}
-	if ( post.post_thumbnail ) {
-		postCopy.attributes.post_thumbnail = post.post_thumbnail;
-	}
+	postCopy.attributes = pick(
+		post,
+		'canonical_image',
+		'excerpt',
+		'featured_image',
+		'format',
+		'metadata',
+		'post_thumbnail',
+		'title'
+	);
+	postCopy.attributes.categories = map( post.categories, 'ID' );
+	postCopy.attributes.tags = map( post.tags, 'name' );
 
 	return postCopy;
 }
