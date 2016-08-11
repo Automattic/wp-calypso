c931276d368bff954dee1f4597eabcc043d5b3f9
shaunandrews <shaun@automattic.com>
2016-08-11 15:42:08 -0400

    Updating the example SVG to not have a black background and using CSS to handle the fading.

--- client/signup/steps/site-title/example-site-title.js
+++ client/signup/steps/site-title/example-site-title.js
@@ -13,7 +13,7 @@ export default React.createClass( {
 					{ this.translate( 'A site title is your site slogan.' ) }
 				</p>
 				<div className="site-title__example-site-title__browser">
-					<svg width="269" height="135" viewBox="0 0 269 135" xmlns="http://www.w3.org/2000/svg"><title>Group</title><defs><linearGradient x1="0%" y1="0%" x2="0%" y2="100%" id="a"><stop stop-color="#E2EBF2" offset="0%"/><stop stop-color="#F3F6F8" offset="100%"/></linearGradient></defs><g fill="none" fill-rule="evenodd"><path fill="url(#a)" d="M0 24h269v111H0z"/><path d="M0 24h269V3.99c0-2.204-.947-3.99-2.107-3.99H2.107C.943 0 0 1.784 0 3.99V24M28 75h182v12H28zM28 93h139v12H28z" fill="#C8D7E1"/><text font-family="SFUIText-Medium, SF UI Text" font-size="15" font-weight="400" fill="#556875"><tspan x="28.256" y="59">Your Site Title</tspan></text></g></svg>
+					<svg width="269" height="135" viewBox="0 0 269 135" xmlns="http://www.w3.org/2000/svg"><title>Group</title><g fill="none" fill-rule="evenodd"><path fill="#E9EFF3" d="M0 24h269v111H0z"/><path d="M0 24h269V3.99c0-2.204-.947-3.99-2.107-3.99H2.107C.943 0 0 1.784 0 3.99V24M28 75h182v12H28zM28 93h139v12H28z" fill="#C8D7E1"/><text font-family="SFUIText-Medium, SF UI Text" font-size="15" font-weight="400" fill="#556875" transform="translate(-345 -213)"><tspan x="373.256" y="272">Your Site Title</tspan></text></g></svg>
 				</div>
 			</div>
 		);
