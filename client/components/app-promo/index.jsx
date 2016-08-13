bb75e260ab935ef6576154cbdb3296fd76300904
Copons <copons@users.noreply.github.com>
2016-08-13 19:18:37 +0200

    Fix ESLint warnings
    Mostly vars and line lengths.

--- client/components/app-promo/index.jsx
+++ client/components/app-promo/index.jsx
@@ -18,7 +18,7 @@ export default React.createClass( {
 	},
 
 	getInitialState: function() {
-		var show_promo = true;
+		let show_promo = true;
 
 		if ( store.get( 'desktop_promo_dismissed' ) ) {
 			show_promo = false;
@@ -37,14 +37,14 @@ export default React.createClass( {
 			show_promo = false;
 		}
 
-		var promo_options = [
+		const promo_options = [
 			{ promo_code: 'a0001', message: 'WordPress.com your way  — desktop app now available for Mac, Windows, and Linux.' },
 			{ promo_code: 'a0002', message: 'Get WordPress.com app for your desktop.' },
 			{ promo_code: 'a0003', message: 'WordPress.com app now available for desktop.' },
 			{ promo_code: 'a0005', message: 'WordPress.com at your fingertips — download app for desktop.' }
 		];
 
-		var item = promo_options[Math.floor( Math.random() * promo_options.length )];
+		const item = promo_options[ Math.floor( Math.random() * promo_options.length ) ];
 		return {
 			promo_item: item,
 			show_promo: show_promo
@@ -54,12 +54,18 @@ export default React.createClass( {
 	componentDidMount: function() {
 		// record promo view event
 		if ( this.state.show_promo ) {
-			analytics.tracks.recordEvent( 'calypso_desktop_promo_view', { promo_location: this.props.location, promo_code: this.state.promo_item.promo_code } );
+			analytics.tracks.recordEvent( 'calypso_desktop_promo_view', {
+				promo_location: this.props.location,
+				promo_code: this.state.promo_item.promo_code,
+			} );
 		}
 	},
 
 	recordClickEvent: function() {
-		analytics.tracks.recordEvent( 'calypso_desktop_promo_click', { promo_location: this.props.location, promo_code: this.state.promo_item.promo_code } );
+		analytics.tracks.recordEvent( 'calypso_desktop_promo_click', {
+			promo_location: this.props.location,
+			promo_code: this.state.promo_item.promo_code
+		} );
 	},
 
 	dismiss: function() {
@@ -74,20 +80,31 @@ export default React.createClass( {
 			return null;
 		}
 
-		var promo_link = 'https://apps.wordpress.com/desktop/?ref=promo_' + this.props.location + '_' + this.state.promo_item.promo_code;
-		var element = (
+		const promo_link = 'https://apps.wordpress.com/desktop/?ref=promo_' + this.props.location + '_' + this.state.promo_item.promo_code;
+		return (
 			<div className="app-promo">
 				<span tabIndex="0" className="app-promo__dismiss" onClick={ this.dismiss } >
 					<Gridicon icon="cross" size={ 24 } />
-					<span className="screen-reader-text">{ this.translate( 'Dismiss' ) }</span>
+					<span className="app-promo__screen-reader-text screen-reader-text">
+						{ this.translate( 'Dismiss' ) }
+					</span>
 				</span>
-				<a onClick={ this.recordClickEvent } className="app-promo__link" title="Try the desktop app!" href={ promo_link } target="_blank">
-					<img className="app-promo__icon" src="/calypso/images/reader/promo-app-icon.png" width="32" height="32" alt="WordPress Desktop Icon" />
+				<a
+					onClick={ this.recordClickEvent }
+					className="app-promo__link"
+					title="Try the desktop app!"
+					href={ promo_link } target="_blank"
+				>
+					<img
+						className="app-promo__icon"
+						src="/calypso/images/reader/promo-app-icon.png"
+						width="32"
+						height="32"
+						alt="WordPress Desktop Icon"
+					/>
 					{ this.state.promo_item.message }
 				</a>
 			</div>
 		);
-
-		return element;
 	}
 } );
