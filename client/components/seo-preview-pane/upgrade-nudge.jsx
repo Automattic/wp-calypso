5776469a769ce29db0ad501dd52d43fa2786d019
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2016-08-15 19:34:30 -0300

    rollback

---
+++ client/components/seo-preview-pane/upgrade-nudge.jsx
@@ -0,0 +1,114 @@
+/** @ssr-ready **/
+
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import { compact, noop } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import FeatureComparison from 'my-sites/feature-comparison';
+import PlanCompareCard from 'my-sites/plan-compare-card';
+import PlanCompareCardItem from 'my-sites/plan-compare-card/item';
+import { getEditorPostId } from 'state/ui/editor/selectors';
+import { getSitePost } from 'state/posts/selectors';
+import { getSectionName, getSelectedSite } from 'state/ui/selectors';
+import { getFeatureTitle, planHasFeature, getPlan } from 'lib/plans';
+import {
+	PLAN_BUSINESS,
+	FEATURE_ADVANCED_DESIGN,
+	FEATURE_CUSTOM_DOMAIN,
+	FEATURE_NO_ADS,
+	FEATURE_UNLIMITED_PREMIUM_THEMES,
+	FEATURE_UNLIMITED_STORAGE,
+	FEATURE_VIDEO_UPLOADS
+} from 'lib/plans/constants';
+
+const featuresToShow = [
+	FEATURE_UNLIMITED_STORAGE,
+	FEATURE_UNLIMITED_PREMIUM_THEMES,
+	FEATURE_CUSTOM_DOMAIN,
+	FEATURE_NO_ADS,
+	FEATURE_ADVANCED_DESIGN,
+	FEATURE_VIDEO_UPLOADS
+];
+
+const AdvancedSEOUpgradeNudge = ( { translate, site, post } ) => {
+	// <div className="seo-preview-nudge__upgrade">
+	// 	<UpgradeNudge
+	// 		title={ translate( 'Advanced Search Engine Optimization' ) }
+	// 		message={ translate( 'lorem ipsum dolor sit amet' ) }
+	// 		feature="advanced-seo"
+	// 		event="advanced_seo_preview"
+	// 		icon="share"
+	// 	/>
+	// </div	>
+
+	return (
+		<div className="seo-preview-nudge">
+			<div className="seo-preview-nudge__plan">
+				<div className="seo-preview-nudge__plan-icon"></div>
+			</div>
+			<div className="seo-preview-nudge__message">
+				<h2 className="seo-preview-nudge__message-title">{ translate( 'Get Advanced SEO Features' ) }</h2>
+				<h3 className="seo-preview-nudge__message-line">{ translate( 'Supercharge your site with live chat support, unlimited access to premium themes, and Google Analytics.' ) }</h3>
+			</div>
+			<div className="seo-preview-nudge__preview">
+				<img src="/calypso/images/advanced-seo-nudge.png" />
+			</div>
+			<FeatureComparison className="seo-preview-nudge__feature-comparison">
+				<PlanCompareCard
+					title={ getPlan( site.plan.product_slug ).getTitle() }
+					line={ getPlan( site.plan.product_slug ).getPriceTitle() }
+					buttonName={ translate( 'Your Plan' ) }
+					currentPlan={ true } >
+					<PlanCompareCardItem unavailable={ true } >
+						{ translate( 'Advanced Search Engine Optimization' ) }
+					</PlanCompareCardItem>
+					{
+						featuresToShow.map( feature => <PlanCompareCardItem
+								key={ feature }
+								unavailable={ ! planHasFeature( site.plan.product_slug, feature ) }
+							>
+								{ getFeatureTitle( feature ) }
+							</PlanCompareCardItem>
+						)
+					}
+				</PlanCompareCard>
+				<PlanCompareCard
+					title={ getPlan( PLAN_BUSINESS ).getTitle() }
+					line={ getPlan( PLAN_BUSINESS ).getPriceTitle() }
+					buttonName={ translate( 'Upgrade' ) }
+					onClick={ noop }
+					currentPlan={ false }
+					popularRibbon={ true } >
+					<PlanCompareCardItem highlight={ true } >
+						{ translate( 'Advanced Search Engine Optimization' ) }
+					</PlanCompareCardItem>
+					{
+						featuresToShow.map( feature => <PlanCompareCardItem key={ feature }>
+							{ getFeatureTitle( feature ) }
+						</PlanCompareCardItem> )
+					}
+				</PlanCompareCard>
+			</FeatureComparison>
+		</div>
+	);
+}
+
+const mapStateToProps = state => {
+	const site = getSelectedSite( state );
+	const postId = getEditorPostId( state );
+	const isEditorShowing = 'post-editor' === getSectionName( state );
+
+	return {
+		site,
+		post: isEditorShowing && getSitePost( state, site.ID, postId )
+	};
+};
+
+export default connect( mapStateToProps )( localize( AdvancedSEOUpgradeNudge ) );
