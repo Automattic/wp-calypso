36a40f69569917cefc602fe1a74b27e8c175bb57
Jason Johnston <jhnstn@users.noreply.github.com>
2016-08-15 09:19:38 -0700

    Reader: update discover card stats (#7294)
    * Pass isFollowing down to prop function
    
    * Update stats handlers for discover components
    
    * use Gridicon component

---
+++ client/reader/discover/stats.js
@@ -0,0 +1,34 @@
+/**
+* Internal dependencies
+*/
+import {
+	recordAction,
+	recordGaEvent,
+	recordTrack
+ } from 'reader/stats';
+
+export function recordAuthorClick( author ) {
+	recordGaEvent( 'Clicked Discover Card Attribution Author' );
+	recordAction( 'discover_card_author_clicked' );
+	recordTrack( 'calypso_reader_author_on_discover_card_clicked', { author_url: author } );
+}
+
+export function recordSiteClick( siteUrl ) {
+	recordGaEvent( 'Clicked Discover Card Site Link' );
+	recordAction( 'discover_card_site_clicked' );
+	recordTrack( 'calypso_reader_site_on_discover_card_clicked', { site_url: siteUrl } );
+}
+
+export function recordFollowToggle( isFollowing, siteUrl ) {
+	recordAction( 'discover_card_following_clicked' );
+
+	if ( isFollowing ) {
+		recordGaEvent( 'Clicked Follow Discover Site' );
+		recordAction( 'discover_card_site_followed' );
+		recordTrack( 'calypso_reader_discover_site_followed', { site_url: siteUrl } );
+	} else {
+		recordGaEvent( 'Clicked Unfollow Discover Site' );
+		recordAction( 'discover_card_site_unfollowed' );
+		recordTrack( 'calypso_reader_discover_site_unfollowed', { site_url: siteUrl } );
+	}
+}
