add6908dcecc6ecf077c8ba4c400e9e40298bf92
Jordan West <jordwest@users.noreply.github.com>
2016-08-17 18:44:38 +1000

    Guided Transfer: Complete purchase notice (#7185)
    * Guided Transfer: Complete purchase notice
    
    This adds a notice to the export page of site settings prompting the user to complete a Guided Transfer purchase. This should appear when the user has entered host details, but hasn't yet completed checkout.
    
    Connects purchase notice to Redux
    Connect in progress card to Redux

--- client/state/sites/guided-transfer/selectors.js
+++ client/state/sites/guided-transfer/selectors.js
@@ -1,3 +1,31 @@
 export function isRequestingGuidedTransferStatus( state, siteId ) {
 	return state.sites.guidedTransfer.isFetching[ siteId ] === true;
 }
+
+export function isGuidedTransferInProgress( state, siteId ) {
+	const status = state.sites.guidedTransfer.status[ siteId ];
+	if ( ! status ) {
+		return false;
+	}
+
+	return status.upgrade_purchased &&
+		status.host_details_entered;
+}
+
+/**
+ * Returns true if the user has initiated a guided transfer, but
+ * we're still waiting for them to purchase the GT
+ *
+ * @param {Object} state The Redux state object
+ * @param {number} siteId The siteId to check
+ * @return {bool} true if guided transfer is awaiting purchase
+ */
+export function isGuidedTransferAwaitingPurchase( state, siteId ) {
+	const status = state.sites.guidedTransfer.status[ siteId ];
+	if ( ! status ) {
+		return false;
+	}
+
+	return ( ! status.upgrade_purchased ) &&
+		status.host_details_entered;
+}
