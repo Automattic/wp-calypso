1c59ec27803addae716641c05af4181c61463376
Chris Rosser <chris@bluefuton.com>
2016-08-17 14:29:29 +0200

    reader-author-link block: allow a siteUrl to be provided in case author.URL is missing (#7494)
    * reader-author-link block: allow a siteUrl to be provided in case author.URL is missing
    
    * Whitespace tweak

--- client/blocks/reader-author-link/index.jsx
+++ client/blocks/reader-author-link/index.jsx
@@ -9,7 +9,7 @@ import React from 'react';
 import ExternalLink from 'components/external-link';
 import { recordAction, recordGaEvent, recordTrackForPost } from 'reader/stats';
 
-const ReaderAuthorLink = ( { author, post, children } ) => {
+const ReaderAuthorLink = ( { author, post, siteUrl, children } ) => {
 	const recordAuthorClick = ( { } ) => {
 		recordAction( 'click_author' );
 		recordGaEvent( 'Clicked Author Link' );
@@ -18,12 +18,18 @@ const ReaderAuthorLink = ( { author, post, children } ) => {
 		}
 	};
 
-	if ( ! author.URL ) {
-		return ( <span className="reader-author-link">{ children }</span> );
+	let linkUrl = author.URL;
+	if ( ! linkUrl ) {
+		linkUrl = siteUrl;
+	}
+
+	// If we have neither author.URL or siteUrl, just return children
+	if ( ! linkUrl ) {
+		return children;
 	}
 
 	return (
-		<ExternalLink className="reader-author-link" href={ author.URL } target="_blank" onClick={ recordAuthorClick }>
+		<ExternalLink className="reader-author-link" href={ linkUrl } target="_blank" onClick={ recordAuthorClick }>
 			{ children }
 		</ExternalLink>
 	);
@@ -31,7 +37,8 @@ const ReaderAuthorLink = ( { author, post, children } ) => {
 
 ReaderAuthorLink.propTypes = {
 	author: React.PropTypes.object.isRequired,
-	post: React.PropTypes.object // for stats only
+	post: React.PropTypes.object, // for stats only,
+	siteUrl: React.PropTypes.string // used as fallback if author.URL is missing
 };
 
 export default ReaderAuthorLink;
