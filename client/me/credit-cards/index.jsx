344d62c493796111d915125e981a774ff3fc81ec
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-08-18 15:21:18 +0200

    Billing History: ES6ify CreditCards component

--- client/me/credit-cards/index.jsx
+++ client/me/credit-cards/index.jsx
@@ -1,22 +1,24 @@
 /**
  * External dependencies
  */
-var connect = require( 'react-redux' ).connect,
-	React = require( 'react' );
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
-var CreditCardDelete = require( './credit-card-delete' ),
-	Card = require( 'components/card' ),
-	getStoredCards = require( 'state/stored-cards/selectors' ).getStoredCards,
-	hasLoadedStoredCardsFromServer = require( 'state/stored-cards/selectors' ).hasLoadedStoredCardsFromServer,
-	isFetchingStoredCards = require( 'state/stored-cards/selectors' ).isFetchingStoredCards,
-	QueryStoredCards = require( 'components/data/query-stored-cards' ),
-	SectionHeader = require( 'components/section-header' );
+import Card from 'components/card';
+import CreditCardDelete from './credit-card-delete';
+import {
+	getStoredCards,
+	hasLoadedStoredCardsFromServer,
+	isFetchingStoredCards
+} from 'state/stored-cards/selectors';
+import QueryStoredCards from 'components/data/query-stored-cards';
+import SectionHeader from 'components/section-header';
 
-var CreditCards = React.createClass( {
-	renderCards: function() {
+class CreditCards extends Component {
+	renderCards() {
 		if ( this.props.isFetching && ! this.props.hasLoadedFromServer ) {
 			return (
 				<div className="credit-cards__no-results">
@@ -35,31 +37,31 @@ var CreditCards = React.createClass( {
 
 		return this.props.cards.map( function( card ) {
 			return (
-				<div className="credit-cards_single-card" key={ card.stored_details_id }>
+				<div className="credit-cards__single-card" key={ card.stored_details_id }>
 					<CreditCardDelete card={ card } />
 				</div>
 			);
 		}, this );
-	},
+	}
 
-	render: function() {
+	render() {
 		return (
-			<div>
+			<div className="credit-cards">
 				<QueryStoredCards />
 
 				<SectionHeader label={ this.translate( 'Manage Your Credit Cards' ) } />
 
 				<Card>
-					<div className="credit-cards">
+					<div>
 						{ this.renderCards() }
 					</div>
 				</Card>
 			</div>
 		);
 	}
-} );
+}
 
-module.exports = connect(
+export default connect(
 	state => ( {
 		cards: getStoredCards( state ),
 		hasLoadedFromServer: hasLoadedStoredCardsFromServer( state ),
