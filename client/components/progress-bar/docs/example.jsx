4b05dc08ccb188164d4e310a26d92c0f1b76094e
Eric Binnion <ericbinnion@gmail.com>
2016-08-22 08:51:57 -0500

    Devdocs: Fix ESLint warnings, excluding classname warnings

--- client/components/progress-bar/docs/example.jsx
+++ client/components/progress-bar/docs/example.jsx
@@ -1,13 +1,13 @@
 /**
  * External dependencies
  */
-var React = require( 'react' ),
-	PureRenderMixin = require( 'react-pure-render/mixin' );
+import React from 'react';
+import PureRenderMixin from 'react-pure-render/mixin';
 
 /**
  * Internal dependencies
  */
-var ProgressBar = require( 'components/progress-bar' );
+import ProgressBar from 'components/progress-bar';
 
 module.exports = React.createClass( {
 
@@ -18,7 +18,7 @@ module.exports = React.createClass( {
 	getInitialState() {
 		return {
 			compact: false
-		}
+		};
 	},
 
 	toggleCompact() {
