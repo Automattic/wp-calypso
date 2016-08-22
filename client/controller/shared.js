Automattic/wp-calypso

commit 0982dd49dcd05af30d849288fa0fb5587bb9baa5

Framework: Add `makeLayout` (#5081)
* Framework: Add makeLayout() middleware

* Allow elements as props to the layout component
* Add a ReduxWrappedLayout helper function
* Split the entry points for the isomorphic router controller
(allowing client-specific dependencies)

* Framework: fix layout section proptype

* Themes: Remove obsolete comment

* my-sites/controller: Export makeNavigation middleware

* Themes: Use makeNavigation and makeLayout for rendering logged-in

* client/boot: Remove an obsolete check

* my-sites/controller: Use JSX for createNavigation() return value

* client/controller: Make README.md more verbose

* client/controller: Move shared code to shared.js

* Framework: Unmount multi-tree layout before rendering single-tree

* Themes: Remove siteSelection middleware from multi-site route

* Layout: Conditionally add wp-singletree-layout class

* client/controller: Drop layoutFocus import

* client/controller: Move makeLayoutMiddleware to shared.js

client/controller/shared.js
@@ -1,0 +1,37 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import noop from 'lodash/noop';
+
+/**
+ * Internal dependencies
+ */
+import { setSection as setSectionAction } from 'state/ui/actions';
+import { getCurrentUser } from 'state/current-user/selectors';
+
+export function makeLayoutMiddleware( LayoutComponent ) {
+	return ( context, next ) => {
+		const { store, primary, secondary, tertiary } = context;
+
+		// On server, only render LoggedOutLayout when logged-out.
+		if ( ! context.isServerSide || getCurrentUser( context.store.getState() ) ) {
+			context.layout = (
+				<LayoutComponent store={ store }
+					primary={ primary }
+					secondary={ secondary }
+					tertiary={ tertiary }
+				/>
+			);
+		}
+		next();
+	};
+}
+
+export function setSection( section ) {
+	return ( context, next = noop ) => {
+		context.store.dispatch( setSectionAction( section ) );
+
+		next();
+	};
+}

client/boot/index.js
@@ -5,6 +5,7 @@ require( 'lib/local-storage' )();
  * External dependencies
  */
 var React = require( 'react' ),
+	ReactDom = require( 'react-dom' ),
 	store = require( 'store' ),
 	ReactInjection = require( 'react/lib/ReactInjection' ),
 	some = require( 'lodash/some' ),
@@ -38,24 +39,19 @@ var config = require( 'config' ),
 	sites = require( 'lib/sites-list' )(),
 	superProps = require( 'lib/analytics/super-props' ),
 	translatorJumpstart = require( 'lib/translator-jumpstart' ),
-	translatorInvitation = require( 'layout/community-translator/invitation-utils' ),
 	nuxWelcome = require( 'layout/nux-welcome' ),
 	emailVerification = require( 'components/email-verification' ),
 	viewport = require( 'lib/viewport' ),
 	detectHistoryNavigation = require( 'lib/detect-history-navigation' ),
 	pushNotificationsInit = require( 'state/push-notifications/actions' ).init,
-	sections = require( 'sections' ),
 	touchDetect = require( 'lib/touch-detect' ),
 	setRouteAction = require( 'state/ui/actions' ).setRoute,
 	accessibleFocus = require( 'lib/accessible-focus' ),
 	TitleStore = require( 'lib/screen-title/store' ),
 	syncHandler = require( 'lib/wp/sync-handler' ),
-	renderWithReduxStore = require( 'lib/react-helpers' ).renderWithReduxStore,
 	bindWpLocaleState = require( 'lib/wp/localization' ).bindState,
 	supportUser = require( 'lib/user/support-user-interop' ),
-	createReduxStoreFromPersistedInitialState = require( 'state/initial-state' ).default,
-	// The following components require the i18n mixin, so must be required after i18n is initialized
-	Layout;
+	createReduxStoreFromPersistedInitialState = require( 'state/initial-state' ).default;
 
 import { getSelectedSiteId, getSectionName, isSectionIsomorphic } from 'state/ui/selectors';
 import { setNextLayoutFocus, activateNextLayoutFocus } from 'state/ui/layout-focus/actions';
@@ -179,17 +175,15 @@ function boot() {
 }
 
 function renderLayout( reduxStore ) {
-	const props = {};
+	const Layout = require( 'controller' ).ReduxWrappedLayout;
 
-	if ( user.get() ) {
-		Object.assign( props, { user, sites, nuxWelcome, translatorInvitation } );
-	}
+	const layoutElement = React.createElement( Layout, {
+		store: reduxStore
+	} );
 
-	Layout = require( 'layout' );
-	renderWithReduxStore(
-		React.createElement( Layout, props ),
-		document.getElementById( 'wpcom' ),
-		reduxStore
+	ReactDom.render(
+		layoutElement,
+		document.getElementById( 'wpcom' )
 	);
 
 	debug( 'Main layout rendered.' );
@@ -225,12 +219,9 @@ function reduxStoreReady( reduxStore ) {
 		require( 'lib/network-connection' ).init( reduxStore );
 	}
 
-	// Render Layout only for non-isomorphic sections, unless logged-in.
-	// Isomorphic sections will take care of rendering their Layout last themselves,
-	// unless in logged-in mode, where we can't do that yet.
-	// TODO: Remove the ! user.get() check once isomorphic sections render their
-	// Layout themselves when logged in.
-	if ( ! isIsomorphic || user.get() ) {
+	// Render Layout only for non-isomorphic sections.
+	// Isomorphic sections will take care of rendering their Layout last themselves.
+	if ( ! isIsomorphic ) {
 		renderLayout( reduxStore );
 
 		if ( config.isEnabled( 'catch-js-errors' ) ) {
@@ -328,6 +319,7 @@ function reduxStoreReady( reduxStore ) {
 	}
 
 	// Load the application modules for the various sections and features
+	const sections = require( 'sections' );
 	sections.load();
 
 	// delete any lingering local storage data from signup
@@ -431,7 +423,11 @@ function reduxStoreReady( reduxStore ) {
 
 		if ( isMultiTreeLayout && previousLayoutIsSingleTree ) {
 			debug( 'Re-rendering multi-tree layout' );
+
 			renderLayout( context.store );
+		} else if ( ! isMultiTreeLayout && ! previousLayoutIsSingleTree ) {
+			debug( 'Unmounting multi-tree layout' );
+			ReactDom.unmountComponentAtNode( document.getElementById( 'wpcom' ) );
 		}
 		next();
 	} );

client/controller/index.node.js
@@ -1,0 +1,33 @@
+/**
+ * External Dependencies
+ */
+import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { makeLayoutMiddleware } from './shared.js';
+import LayoutLoggedOut from 'layout/logged-out';
+
+/**
+ * Re-export
+ */
+export { setSection } from './shared.js';
+
+const ReduxWrappedLoggedOutLayout = ( { store, primary, secondary, tertiary } ) => (
+	<ReduxProvider store={ store }>
+		<LayoutLoggedOut primary={ primary }
+			secondary={ secondary }
+			tertiary={ tertiary } />
+	</ReduxProvider>
+);
+
+/**
+ * @param { object } context -- Middleware context
+ * @param { function } next -- Call next middleware in chain
+ *
+ * Produce a `LayoutLoggedOut` element in `context.layout`, using
+ * `context.primary`, `context.secondary`, and `context.tertiary` to populate it.
+*/
+export const makeLayout = makeLayoutMiddleware( ReduxWrappedLoggedOutLayout );

client/controller/index.web.js
@@ -4,48 +4,49 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
-import { setSection as setSectionAction } from 'state/ui/actions';
-import noop from 'lodash/noop';
+import page from 'page';
 
 /**
- * Internal dependencies
+ * Internal Dependencies
  */
-import page from 'page';
+import Layout from 'layout';
 import LayoutLoggedOut from 'layout/logged-out';
+import nuxWelcome from 'layout/nux-welcome';
+import translatorInvitation from 'layout/community-translator/invitation-utils';
+import { makeLayoutMiddleware } from './shared.js';
 import { getCurrentUser } from 'state/current-user/selectors';
+import userFactory from 'lib/user';
+import sitesFactory from 'lib/sites-list';
 import debugFactory from 'debug';
 
+/**
+ * Re-export
+ */
+export { setSection } from './shared.js';
+
+const user = userFactory();
+const sites = sitesFactory();
 const debug = debugFactory( 'calypso:controller' );
 
-/**
- * @param { object } context -- Middleware context
- * @param { function } next -- Call next middleware in chain
-*/
-export function makeLayout( context, next ) {
-	const isLoggedIn = !! getCurrentUser( context.store.getState() );
-	if ( ! isLoggedIn ) {
-		context.layout = makeLoggedOutLayout( context );
-	} // TODO: else { makeLoggedInLayout( context ); }
-	next();
-}
-
-/**
- * @param { object } context -- Middleware context
- * @returns { object } `LoggedOutLayout` element
- *
- * Return a `LayoutLoggedOut` element, using `context.primary`,
- * `context.secondary`, and `context.tertiary` to populate it.
-*/
-function makeLoggedOutLayout( context ) {
-	const { store, primary, secondary, tertiary } = context;
-	return (
-		<ReduxProvider store={ store }>
-			<LayoutLoggedOut primary={ primary }
+export const ReduxWrappedLayout = ( { store, primary, secondary, tertiary } ) => (
+	<ReduxProvider store={ store }>
+		{ getCurrentUser( store.getState() )
+			? <Layout primary={ primary }
+				secondary={ secondary }
+				tertiary={ tertiary }
+				user={ user }
+				sites={ sites }
+				nuxWelcome={ nuxWelcome }
+				translatorInvitation={ translatorInvitation }
+			/>
+			: <LayoutLoggedOut primary={ primary }
 				secondary={ secondary }
 				tertiary={ tertiary } />
-		</ReduxProvider>
-	);
-}
+		}
+	</ReduxProvider>
+);
+
+export const makeLayout = makeLayoutMiddleware( ReduxWrappedLayout );
 
 /**
  * Isomorphic routing helper, client side
@@ -63,14 +64,6 @@ function makeLoggedOutLayout( context ) {
  */
 export function clientRouter( route, ...middlewares ) {
 	page( route, ...middlewares, render );
-}
-
-export function setSection( section ) {
-	return ( context, next = noop ) => {
-		context.store.dispatch( setSectionAction( section ) );
-
-		next();
-	};
 }
 
 function render( context ) {

client/layout/index.jsx
@@ -56,6 +56,25 @@ Layout = React.createClass( {
 
 	_sitesPoller: null,
 
+	propTypes: {
+		primary: React.PropTypes.element,
+		secondary: React.PropTypes.element,
+		tertiary: React.PropTypes.element,
+		sites: React.PropTypes.object,
+		user: React.PropTypes.object,
+		nuxWelcome: React.PropTypes.object,
+		translatorInvitation: React.PropTypes.object,
+		focus: React.PropTypes.object,
+		// connected props
+		isLoading: React.PropTypes.bool,
+		isSupportUser: React.PropTypes.bool,
+		section: React.PropTypes.oneOfType( [
+			React.PropTypes.bool,
+			React.PropTypes.object,
+		] ),
+		isOffline: React.PropTypes.bool,
+	},
+
 	componentWillUpdate: function( nextProps ) {
 		if ( this.props.section.group !== nextProps.section.group ) {
 			if ( nextProps.section.group === 'sites' ) {
@@ -168,7 +187,8 @@ Layout = React.createClass( {
 				`is-section-${this.props.section.name}`,
 				`focus-${this.props.currentLayoutFocus}`,
 				{ 'is-support-user': this.props.isSupportUser },
-				{ 'has-no-sidebar': ! this.props.hasSidebar }
+				{ 'has-no-sidebar': ! this.props.hasSidebar },
+				{ 'wp-singletree-layout': !! this.props.primary }
 			),
 			loadingClass = classnames( {
 				layout__loader: true,
@@ -188,10 +208,16 @@ Layout = React.createClass( {
 					{ this.renderWelcome() }
 					{ this.renderPushNotificationPrompt() }
 					<GlobalNotices id="notices" notices={ notices.list } forcePinned={ 'post' === this.props.section.name } />
-					<div id="primary" className="layout__primary" />
-					<div id="secondary" className="layout__secondary" />
+					<div id="primary" className="layout__primary">
+						{ this.props.primary }
+					</div>
+					<div id="secondary" className="layout__secondary">
+						{ this.props.secondary }
+					</div>
 				</div>
-				<div id="tertiary" />
+				<div id="tertiary">
+					{ this.props.tertiary }
+				</div>
 				<TranslatorLauncher
 					isEnabled={ translator.isEnabled() }
 					isActive={ translator.isActivated() }/>

client/my-sites/controller.js
@@ -39,20 +39,25 @@ const sites = sitesFactory();
 /*
  * The main navigation of My Sites consists of a component with
  * the site selector list and the sidebar section items
+ * @param { object } context - Middleware context
+ * @returns { object } React element containing the site selector and sidebar
  */
-function renderNavigation( context, allSitesPath, siteBasePath ) {
-	// Render the My Sites navigation in #secondary
-	ReactDom.render(
-		React.createElement( ReduxProvider, { store: context.store },
-			React.createElement( NavigationComponent, {
-				path: context.path,
-				allSitesPath,
-				siteBasePath,
-				user,
-				sites
-			} )
-		),
-		document.getElementById( 'secondary' )
+function createNavigation( context ) {
+	const siteFragment = route.getSiteFragment( context.pathname );
+	let basePath = context.pathname;
+
+	if ( siteFragment ) {
+		basePath = route.sectionify( context.pathname );
+	}
+
+	return (
+		<ReduxProvider store={ context.store }>
+			<NavigationComponent path={ context.path }
+				allSitesPath={ basePath }
+				siteBasePath={ basePath }
+				user={ user }
+				sites={ sites } />
+		</ReduxProvider>
 	);
 }
 
@@ -240,15 +245,17 @@ module.exports = {
 		checkSiteShouldFetch();
 	},
 
-	navigation( context, next ) {
-		const siteFragment = route.getSiteFragment( context.pathname );
-		let basePath = context.pathname;
+	makeNavigation: function( context, next ) {
+		context.secondary = createNavigation( context );
+		next();
+	},
 
-		if ( siteFragment ) {
-			basePath = route.sectionify( context.pathname );
-		}
-
-		renderNavigation( context, basePath, basePath );
+	navigation: function( context, next ) {
+		// Render the My Sites navigation in #secondary
+		ReactDom.render(
+			createNavigation( context ),
+			document.getElementById( 'secondary' )
+		);
 		next();
 	},
 

client/my-sites/themes/index.web.js
@@ -4,16 +4,8 @@
 import config from 'config';
 import userFactory from 'lib/user';
 import { makeLayout } from 'controller';
-import { navigation, siteSelection } from 'my-sites/controller';
+import { makeNavigation, siteSelection } from 'my-sites/controller';
 import { singleSite, multiSite, loggedOut } from './controller';
-
-// FIXME: These routes will SSR the logged-out Layout even if logged-in.
-// While subsequently replaced by the logged-in Layout on the client-side,
-// we'll want to render it on the server, too.
-
-// `logged-out` middleware isn't SSR-compliant yet, but we can at least render
-// the layout.
-// FIXME: Also create loggedOut/multiSite/singleSite elements, depending on route.
 
 export default function( router ) {
 	const user = userFactory();
@@ -21,10 +13,10 @@ export default function( router ) {
 
 	if ( config.isEnabled( 'manage/themes' ) ) {
 		if ( isLoggedIn ) {
-			router( '/design/:tier(free|premium)?', multiSite, navigation, siteSelection );
-			router( '/design/:tier(free|premium)?/:site_id', singleSite, navigation, siteSelection );
-			router( '/design/:tier(free|premium)?/filter/:filter', multiSite, navigation, siteSelection );
-			router( '/design/:tier(free|premium)?/filter/:filter/:site_id', singleSite, navigation, siteSelection );
+			router( '/design/:tier(free|premium)?', multiSite, makeNavigation, makeLayout );
+			router( '/design/:tier(free|premium)?/:site_id', siteSelection, singleSite, makeNavigation, makeLayout );
+			router( '/design/:tier(free|premium)?/filter/:filter', siteSelection, multiSite, makeNavigation, makeLayout );
+			router( '/design/:tier(free|premium)?/filter/:filter/:site_id', siteSelection, singleSite, makeNavigation, makeLayout );
 		} else {
 			router( '/design/:tier(free|premium)?', loggedOut, makeLayout );
 			router( '/design/:tier(free|premium)?/filter/:filter', loggedOut, makeLayout );

commit df009e88ac5a788578d8694bad517aa65385b528

Server Side Rendering: Fix
SSR was broken by #5081, where I flipped the check for logged-out to logged-in:
https://github.com/Automattic/wp-calypso/pull/5081/files#diff-2ff5c9f67cfab8b6325e63706e3f4970R18 misses the negation that was previously present at
https://github.com/Automattic/wp-calypso/pull/5081/files#diff-a3600f12cca835b2ea80190a8243a8cbL26.
This PR adds it back.

client/controller/shared.js
@@ -15,7 +15,7 @@ export function makeLayoutMiddleware( LayoutComponent ) {
 		const { store, primary, secondary, tertiary } = context;
 
 		// On server, only render LoggedOutLayout when logged-out.
-		if ( ! context.isServerSide || getCurrentUser( context.store.getState() ) ) {
+		if ( ! context.isServerSide || ! getCurrentUser( context.store.getState() ) ) {
 			context.layout = (
 				<LayoutComponent store={ store }
 					primary={ primary }