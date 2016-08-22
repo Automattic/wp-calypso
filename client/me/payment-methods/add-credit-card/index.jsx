556358a67724620ddc1fadf771aed82faa12e7ec
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-08-22 14:04:18 +0200

    Payment Methods: Add initial integration with CreditCardPage component

--- client/me/payment-methods/add-credit-card/index.jsx
+++ client/me/payment-methods/add-credit-card/index.jsx
@@ -1,20 +1,39 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import { connect } from 'react-redux';
+import { noop } from 'lodash';
+import React, { Component } from 'react';
 
 /**
  * Internal dependencies
  */
 import { concatTitle } from 'lib/react-helpers';
+import CreditCardPage from 'me/purchases/components/credit-card-page';
 import DocumentHead from 'components/data/document-head';
 import * as titles from 'me/payment-methods/titles';
 
-const AddCreditCard = () => (
-	<div>
-		<DocumentHead title={ concatTitle( titles.paymentMethods, titles.addCreditCard, ) } />
-		<h1>Add Credit Card</h1>
-	</div>
-);
+class AddCreditCard extends Component {
+	static propTypes = {
+	};
+
+	render() {
+		return (
+			<div>
+				<DocumentHead title={ concatTitle( titles.paymentMethods, titles.addCreditCard ) } />
+				<CreditCardPage
+					goBack={ noop }
+					recordFormSubmitEvent={ noop }
+					successCallback={ noop }
+					title={ titles.addCreditCard } />
+			</div>
+		);
+	}
+}
+
+const mapDispatchToProps = {
+};
+
+export default connect( {}, mapDispatchToProps )( AddCreditCard );
 
 export default AddCreditCard;
