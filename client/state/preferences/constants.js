38ef01e56009908744325b1e4028aee16041de16
Andrew Duthie <andrew@andrewduthie.com>
2016-08-23 14:17:28 -0400

    State: Assign default visual editor preference for editor-mode

--- client/state/preferences/constants.js
+++ client/state/preferences/constants.js
@@ -3,7 +3,7 @@
 export const USER_SETTING_KEY = 'calypso_preferences';
 
 export const DEFAULT_PREFERENCE_VALUES = {
-	'editor-mode': null,
+	'editor-mode': 'tinymce',
 	firstViewHistory: [],
 	mediaModalGalleryInstructionsDismissed: false,
 	mediaModalGalleryInstructionsDismissedForSession: false,
