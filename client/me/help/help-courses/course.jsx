25fcb308a93a361babb90094d543a9994c584dc3
Omar Jackman <github@omarjackman.com>
2016-08-23 14:32:29 -0400

    Add mock course list data and display it on the courses page

--- client/me/help/help-courses/course.jsx
+++ client/me/help/help-courses/course.jsx
@@ -1,24 +1,44 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React from 'react';
 import { localize } from 'i18n-calypso';
-import Card from 'components/card';
 
-class Course extends Component {
-	render() {
-		return (
-			<Card className="help-courses__course">
-				Latest course: Only visible for users with the business plan
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+import CourseScheduleItem from './course-schedule-item';
+
+export default localize( ( props ) => {
+	const {
+		title,
+		description,
+		schedule,
+		showRecentCourseRecordings,
+		translate
+	} = props;
+
+	return (
+		<div className="help-courses__course">
+			<Card compact className="help-courses__course-label">{ translate( 'Next course' ) }</Card>
+			<Card compact>
+				<h1 className="help-courses__course-title">{ title }</h1>
+				<p className="help-courses__course-description">{ description }</p>
 			</Card>
-		);
-	}
-}
+			{ schedule.map( ( item, key ) => <CourseScheduleItem { ...item } key={ key } /> ) }
+			{
+				showRecentCourseRecordings &&
+				<Card className="help-courses__course-recording">
+					Show most recent recording here
+				</Card>
+			}
+		</div>
+	);
+} );
 
 export const CoursePlaceholder = () => {
 	return (
-		<Card className="help-courses__course is-placeholder"></Card>
+		<div className="help-courses__course is-placeholder"></div>
 	);
 };
-
-export default localize( Course );
