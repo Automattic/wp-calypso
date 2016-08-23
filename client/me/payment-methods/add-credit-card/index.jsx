63fb1715695c424c727a7c4a3297e9c20d818cec
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-08-23 09:11:47 +0200

    Payment Methods: Implement Add Credit Card page props

--- client/me/payment-methods/add-credit-card/index.jsx
+++ client/me/payment-methods/add-credit-card/index.jsx
@@ -2,12 +2,13 @@
  * External dependencies
  */
 import { connect } from 'react-redux';
-import { noop } from 'lodash';
+import page from 'page';
 import React, { Component } from 'react';
 
 /**
  * Internal dependencies
  */
+import analytics from 'lib/analytics';
 import { concatTitle } from 'lib/react-helpers';
 import CreditCardPage from 'me/purchases/components/credit-card-page';
 import DocumentHead from 'components/data/document-head';
@@ -17,14 +18,22 @@ class AddCreditCard extends Component {
 	static propTypes = {
 	};
 
+	goToBillingHistory() {
+		page( '/me/billing' );
+	}
+
+	recordFormSubmitEvent() {
+		analytics.tracks.recordEvent( 'calypso_add_credit_card_form_submit' );
+	}
+
 	render() {
 		return (
 			<div>
 				<DocumentHead title={ concatTitle( titles.paymentMethods, titles.addCreditCard ) } />
 				<CreditCardPage
-					goBack={ noop }
-					recordFormSubmitEvent={ noop }
-					successCallback={ noop }
+					goBack={ this.goToBillingHistory }
+					recordFormSubmitEvent={ this.recordFormSubmitEvent }
+					successCallback={ this.goToBillingHistory }
 					title={ titles.addCreditCard } />
 			</div>
 		);
