02a29e79d0b611e01b88fbb59fda9494b38af279
Kerry Liu <kerryliu@gmail.com>
2016-08-23 15:56:27 -0700

    State: better schema warnings

--- client/state/utils.js
+++ client/state/utils.js
@@ -70,6 +70,8 @@ export function createReducer( initialState = null, customHandlers = {}, schema 
 					return state;
 				}
 
+				warn( 'state validation failed - check schema used for:', customHandlers );
+
 				return initialState;
 			}
 		};
