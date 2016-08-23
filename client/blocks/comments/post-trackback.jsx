ed37d5e6fb577f767dc90c48d15dc06b27b35780
Chris Rosser <chris@bluefuton.com>
2016-08-23 15:24:08 +0200

    Comments: move into blocks (#7500)
    * Add a comment count component to display at the top of comment lists
    
    * Duplicate reader/comments into blocks/reader-comments (with added comment-count component)
    
    * Move comments to blocks/comments and resolve linting errors
    
    * Docs and comment cleanup
    
    * Comment tweak
    
    * Update lodash imports
    
    * Add word-break from #7262

---
+++ client/blocks/comments/post-trackback.jsx
@@ -0,0 +1,47 @@
+/***
+ * External dependencies
+ */
+import React from 'react';
+
+/***
+ * Internal dependencies
+ */
+import Gridicon from 'components/gridicon';
+import PostTime from 'reader/post-time';
+
+function unescape( str ) {
+	return str.replace( /&#(\d+);/g, ( match, entity ) => String.fromCharCode( entity ) );
+}
+
+export default class PostTrackback extends React.Component {
+	render() {
+		const commentsTree = this.props.commentsTree;
+		const comment = commentsTree.getIn( [ this.props.commentId, 'data' ] ).toJS();
+		const unescapedAuthorName = unescape( comment.author.name );
+
+		return (
+			<li className={ 'comments__comment depth-0' }>
+				<div className="comments__comment-author">
+					<div className="comments__comment-trackbackicon">
+						<Gridicon icon="link" size={ 24 } />
+					</div>
+
+					{ comment.author.URL
+						? <a href={ comment.author.URL } target="_blank" className="comments__comment-username">{ unescapedAuthorName }<Gridicon icon="external" /></a>
+						: <strong className="comments__comment-username">{ unescapedAuthorName }</strong> }
+
+					<small className="comments__comment-timestamp">
+						<a href={ comment.URL }>
+							<PostTime date={ comment.date } />
+						</a>
+					</small>
+				</div>
+
+			</li>
+		);
+	}
+}
+
+PostTrackback.propTypes = {
+	commentId: React.PropTypes.number
+};
