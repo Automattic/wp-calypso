857f92b28e36af18c07e00accfa1a03fad9005ea
Timmy Crawford <timmydcrawford@gmail.com>
2016-08-24 16:50:22 -0700

    No non-hierarchical terms left behind.

--- client/lib/posts/actions.js
+++ client/lib/posts/actions.js
@@ -21,7 +21,7 @@ var wpcom = require( 'lib/wp' ),
 	versionCompare = require( 'lib/version-compare' ),
 	Dispatcher = require( 'dispatcher' ),
 	stats = require( './stats' );
-import { omit } from 'lodash';
+import { normalizePostForApi } from 'state/posts/utils';
 
 var PostActions;
 
@@ -84,7 +84,7 @@ function handleMetadataOperation( key, value, operation ) {
  */
 function normalizeApiAttributes( attributes ) {
 	attributes = clone( attributes );
-	attributes = omit( attributes, 'terms' );
+	attributes = normalizePostForApi( attributes );
 
 	if ( attributes.author ) {
 		attributes.author = attributes.author.ID;
