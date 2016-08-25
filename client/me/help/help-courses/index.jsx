313acb639adb2080b7f7fb362de2de826f2071e8
Omar Jackman <github@omarjackman.com>
2016-08-25 11:08:03 -0400

    Use isFetching instead of hasFetched to determine the loading state of the courses teaser button as suggested https://github.com/Automattic/wp-calypso/pull/7656#discussion_r76230712

--- client/me/help/help-courses/index.jsx
+++ client/me/help/help-courses/index.jsx
@@ -11,7 +11,7 @@ import { find } from 'lodash';
 import Courses from './courses';
 import {
 	getUserPurchases,
-	hasLoadedUserPurchasesFromServer
+	isFetchingUserPurchases
 } from 'state/purchases/selectors';
 import { getCurrentUserId } from 'state/current-user/selectors';
 import { PLAN_BUSINESS } from 'lib/plans/constants';
@@ -54,7 +54,7 @@ function mapStateToProps( state ) {
 	const userId = getCurrentUserId( state );
 	const purchases = getUserPurchases( state, userId );
 	const isBusinessPlanUser = purchases && !! find( purchases, purchase => purchase.productSlug === PLAN_BUSINESS );
-	const isLoading = ! hasLoadedUserPurchasesFromServer( state );
+	const isLoading = isFetchingUserPurchases( state );
 	const courses = getCourses();
 
 	//TODO: Add tracks pings
