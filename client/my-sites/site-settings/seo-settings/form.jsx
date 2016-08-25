dfebabfc34fbcd1e5981d8895fe4814284485db9
artpi <artpi@post.pl>
2016-08-25 12:46:53 +0200

    Move to /my-sites/upgrade-nudge/expanded

--- client/my-sites/site-settings/seo-settings/form.jsx
+++ client/my-sites/site-settings/seo-settings/form.jsx
@@ -32,7 +32,7 @@ import FormFieldset from 'components/forms/form-fieldset';
 import FormLabel from 'components/forms/form-label';
 import FormSettingExplanation from 'components/forms/form-setting-explanation';
 import CountedTextarea from 'components/forms/counted-textarea';
-import SeoSettingsUpgradeNudge from 'components/seo/settings-upgrade-nudge';
+import SeoSettingsUpgradeNudge from 'my-sites/upgrade-nudge/expanded';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
 import config from 'config';
 import { getSeoTitleFormatsForSite } from 'state/sites/selectors';
