ac7301aeed033de1d3b7fd643235828d99e9828a
Dennis Snell <dennis.snell@automattic.com>
2016-08-25 16:38:38 -0700

    Refactor way to delete tokens
    Remove the `x` on rendered tokens in the editor
    Highlight tokens on hover to indicate "delete"

--- client/components/title-format-editor/token.jsx
+++ client/components/title-format-editor/token.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 export const Token = props => {
 	const { onClick } = props;
 
+	// please ignore the formatting below
+	// if we allow spaces it will mess up
+	// the way the component renders in
+	// the draft-js editor
 	return (
 		<div
 			className="title-format-editor__token"
 			onClick={ onClick( props.entityKey ) }
-		>
-			{ props.children }
-			<div className="title-format-editor__token-close noticon noticon-close-alt" />
-		</div>
+		>{ props.children }</div>
 	);
 };
 
