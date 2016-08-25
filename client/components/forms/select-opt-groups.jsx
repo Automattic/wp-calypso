a0144061bb365770d0fa32d04871c8879afea757
Copons <copons@users.noreply.github.com>
2016-08-25 15:03:53 +0200

    Convert SelectOptGroups into a stateless component

--- client/components/forms/select-opt-groups.jsx
+++ client/components/forms/select-opt-groups.jsx
@@ -1,33 +1,21 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React from 'react';
 import { omit } from 'lodash';
-const debug = require( 'debug' )( 'calypso:forms:select-opt-groups' );
 
-export default class SelectOptGroups extends Component {
-
-	componentWillMount() {
-		debug( 'Mounting SelectOptGroups React component.' );
-	}
-
-	render() {
-		return (
-			<select { ...omit( this.props, 'optGroups' ) }>
-				{ this.props.optGroups.map( optGroup =>
-					<optgroup label={ optGroup.label } key={ `optgroup-${optGroup.label}` } >
-						{ optGroup.options.map( option =>
-							<option
-								value={ option.value }
-								key={ `option-${optGroup.label}${option.label}` }
-							>
-								{ option.label }
-							</option>
-						) }
-					</optgroup>
+export const SelectOptGroups = props =>
+	<select { ...omit( props, 'optGroups' ) }>
+		{ props.optGroups.map( optGroup =>
+			<optgroup label={ optGroup.label } key={ `optgroup-${ optGroup.label }` } >
+				{ optGroup.options.map( option =>
+					<option
+						value={ option.value }
+						key={ `option-${ optGroup.label }${ option.label }` }
+					>
+						{ option.label }
+					</option>
 				) }
-			</select>
-		);
-	}
-
-}
+			</optgroup>
+		) }
+	</select>;
