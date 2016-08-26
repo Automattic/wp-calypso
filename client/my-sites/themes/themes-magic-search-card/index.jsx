4c405ef89cfc817feed9a17999fcc6e3662923b0
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2016-08-26 13:40:01 +0200

    Add buttons instead of dropdown in /design. (#7658)
    Add buttons instead of dropdown in /design, only for dev.

---
+++ client/my-sites/themes/themes-magic-search-card/index.jsx
@@ -0,0 +1,110 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import debounce from 'lodash/debounce';
+
+/**
+ * Internal dependencies
+ */
+import Search from 'components/search';
+import SegmentedControl from 'components/segmented-control';
+import { trackClick } from '../helpers';
+import config from 'config';
+import { isMobile } from 'lib/viewport';
+
+const ThemesMagicSearchCard = React.createClass( {
+	propTypes: {
+		tier: React.PropTypes.string,
+		select: React.PropTypes.func.isRequired,
+		site: React.PropTypes.oneOfType( [
+			React.PropTypes.object,
+			React.PropTypes.bool
+		] ).isRequired,
+		onSearch: React.PropTypes.func.isRequired,
+		search: React.PropTypes.string
+	},
+
+	trackClick: trackClick.bind( null, 'search bar' ),
+
+	componentWillMount() {
+		this.onResize = debounce( () => {
+			this.setState( { isMobile: isMobile() } );
+		}, 250 );
+	},
+
+	componentDidMount() {
+		window.addEventListener( 'resize', this.onResize );
+	},
+
+	componentWillUnmount() {
+		window.removeEventListener( 'resize', this.onResize );
+	},
+
+	getInitialState() {
+		return {
+			isMobile: isMobile(),
+			searchIsOpen: false
+		};
+	},
+
+	getDefaultProps() {
+		return { tier: 'all' };
+	},
+
+	onSearchOpen() {
+		this.setState( { searchIsOpen: true } );
+	},
+
+	onSearchClose() {
+		this.setState( { searchIsOpen: false } );
+	},
+
+	onBlur() {
+		if ( this.state.isMobile ) {
+			this.setState( { searchIsOpen: false } );
+		}
+	},
+
+	render() {
+		const isJetpack = this.props.site && this.props.site.jetpack;
+		const isPremiumThemesEnabled = config.isEnabled( 'upgrades/premium-themes' );
+
+		const tiers = [
+			{ value: 'all', label: this.translate( 'All' ) },
+			{ value: 'free', label: this.translate( 'Free' ) },
+			{ value: 'premium', label: this.translate( 'Premium' ) },
+		];
+
+		const searchField = (
+			<Search
+				onSearch={ this.props.onSearch }
+				initialValue={ this.props.search }
+				ref="url-search"
+				placeholder={ this.translate( 'What kind of theme are you looking for?' ) }
+				analyticsGroup="Themes"
+				delaySearch={ true }
+				onSearchOpen={ this.onSearchOpen }
+				onSearchClose={ this.onSearchClose }
+				onBlur={ this.onBlur }
+				fitsContainer={ this.state.isMobile && this.state.searchIsOpen }
+				hideClose={ isMobile() }
+			/>
+		);
+
+		return (
+			<div className="themes-magic-search-card" data-tip-target="themes-search-card">
+				{ searchField }
+				{ isPremiumThemesEnabled && ! isJetpack &&
+					<SegmentedControl
+						initialSelected={ this.props.tier }
+						options={ tiers }
+						onSelect={ this.props.select }
+					/>
+				}
+			</div>
+		);
+	}
+} );
+
+export default ThemesMagicSearchCard;
