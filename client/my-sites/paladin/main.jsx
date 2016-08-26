8f908bd2ef543c546ccd30581abb47e077ea3855
Payton Swick <payton@foolord.com>
2016-08-26 18:35:27 -0400

    Paladin: set preview type to 'design-preview'

--- client/my-sites/paladin/main.jsx
+++ client/my-sites/paladin/main.jsx
@@ -1,19 +1,23 @@
 /**
  * External Dependencies
  */
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 /**
  * Internal Dependencies
  */
 import { setLayoutFocus } from 'state/ui/layout-focus/actions';
+import { setPreviewType } from 'state/ui/preview/actions';
 
 const NativeCustomizer = React.createClass( {
 	propTypes: {
+		setPreviewType: PropTypes.func.isRequired,
+		setLayoutFocus: PropTypes.func.isRequired,
 	},
 
 	componentWillMount() {
+		this.props.setPreviewType( 'design-preview' );
 		this.props.setLayoutFocus( 'preview-sidebar' );
 	},
 
@@ -29,4 +33,4 @@ const NativeCustomizer = React.createClass( {
 	}
 } );
 
-export default connect( null, { setLayoutFocus } )( NativeCustomizer );
+export default connect( null, { setLayoutFocus, setPreviewType } )( NativeCustomizer );
