69dbd9e8a821a6f12fd24513f8b52536d6f1d86e
Rastislav Lamos <lamos.rasto@gmail.com>
2016-08-29 17:52:16 +0200

    Extract PlanFeaturesPrice to my-sites/plan-price
    This helps to make the component reusable outside of /plans page.

--- client/my-sites/plan-price/index.jsx
+++ client/my-sites/plan-price/index.jsx
@@ -9,27 +9,36 @@ import classNames from 'classnames';
  **/
 import { getCurrencyObject } from 'lib/format-currency';
 
-export default class PlanFeaturesPrice extends Component {
+export default class PlanPrice extends Component {
 
 	render() {
-		const { currencyCode, rawPrice, original, discounted } = this.props;
+		const {
+			currencyCode,
+			rawPrice,
+			original,
+			discounted,
+			className
+		} = this.props;
+
 		if ( ! currencyCode || ( rawPrice !== 0 && ! rawPrice ) ) {
 			return null;
 		}
 		const price = getCurrencyObject( rawPrice, currencyCode );
-		const classes = classNames( 'plan-features__price', {
+
+		const classes = classNames( 'plan-price', className, {
 			'is-original': original,
 			'is-discounted': discounted
 		} );
+
 		return (
 			<h4 className={ classes }>
-				<sup className="plan-features__price-currency-symbol">
+				<sup className="plan-price__currency-symbol">
 					{ price.symbol }
 				</sup>
-				<span className="plan-features__price-integer">
+				<span className="plan-price__integer">
 					{ price.integer }
 				</span>
-				<sup className="plan-features__price-fraction">
+				<sup className="plan-price__fraction">
 					{ rawPrice > 0 && price.fraction }
 				</sup>
 			</h4>
@@ -37,15 +46,17 @@ export default class PlanFeaturesPrice extends Component {
 	}
 }
 
-PlanFeaturesPrice.propTypes = {
+PlanPrice.propTypes = {
 	rawPrice: PropTypes.number,
 	original: PropTypes.bool,
 	discount: PropTypes.bool,
-	currencyCode: PropTypes.string
+	currencyCode: PropTypes.string,
+	className: PropTypes.string
 };
 
-PlanFeaturesPrice.defaultProps = {
+PlanPrice.defaultProps = {
 	currencyCode: 'USD',
 	original: false,
-	discounted: false
+	discounted: false,
+	className: ''
 };
