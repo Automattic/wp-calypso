118b0bd676ac616ff7ec3f740177676e77f7f27a
Payton Swick <payton@foolord.com>
2016-08-28 20:18:54 -0400

    DesignPreview: update README to remove reference to updaters

--- client/lib/design-preview/index.js
+++
@@ -1,9 +0,0 @@
-import siteTitle from './updaters/site-title';
-
-const updaterFunctions = [
-	siteTitle,
-];
-
-export function updatePreviewWithChanges( previewDocument, customizations ) {
-	updaterFunctions.map( callback => callback( previewDocument, customizations ) );
-}
