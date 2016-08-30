619e446b687751eccb2f20386ef631074d7fd690
mattsherman <matt@jam123.com>
2016-08-30 14:01:51 -0400

    Change where support-user-interop is required to fix failing unit tests

--- client/state/push-notifications/actions.js
+++ client/state/push-notifications/actions.js
@@ -43,9 +43,6 @@ import {
 	recordTracksEvent,
 	bumpStat
 } from 'state/analytics/actions';
-import {
-	isSupportUserSession,
-} from 'lib/user/support-user-interop';
 
 const debug = debugFactory( 'calypso:push-notifications' );
 const DAYS_BEFORE_FORCING_REGISTRATION_REFRESH = 15;
@@ -55,6 +52,10 @@ const serviceWorkerOptions = {
 
 export function init() {
 	return dispatch => {
+		// require `lib/user/support-user-interop` here so that unit tests don't
+		// fail because of lack of `window` global when importing this module
+		// from test (before a chance to mock things is possible)
+		const isSupportUserSession = require( 'lib/user/support-user-interop' ).isSupportUserSession;
 		if ( isSupportUserSession() ) {
 			debug( 'Push Notifications are not supported when SU is active' );
 			dispatch( apiNotReady() );
