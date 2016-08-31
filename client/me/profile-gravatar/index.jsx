0a86aa5673025c9f103acf5e032c4eae3f9d34f5
Matías Ventura <mv@matiasventura.com>
2016-08-31 12:33:15 +0200

    Use new <Animate> component for transition.

--- client/me/profile-gravatar/index.jsx
+++ client/me/profile-gravatar/index.jsx
@@ -7,6 +7,7 @@ import debugFactory from 'debug';
 /**
  * Internal dependencies
  */
+import Animate from 'components/animate';
 import Gravatar from 'components/gravatar';
 import eventRecorder from 'me/event-recorder';
 
@@ -26,23 +27,23 @@ export default React.createClass( {
 
 		return (
 			<div className="profile-gravatar">
-				<a
-					href="https://secure.gravatar.com/site/wpcom?wpcc-no-close"
-					target="_blank"
-					className="profile-gravatar__edit"
-					onClick={ this.recordClickEvent( 'Gravatar Update Profile Photo in Sidebar' ) } >
+				<Animate type="appear">
+					<a
+						href="https://secure.gravatar.com/site/wpcom?wpcc-no-close"
+						target="_blank"
+						className="profile-gravatar__edit"
+						onClick={ this.recordClickEvent( 'Gravatar Update Profile Photo in Sidebar' ) } >
 
-					<Gravatar user={ this.props.user } size={ 150 } imgSize={ 400 } />
+						<Gravatar user={ this.props.user } size={ 150 } imgSize={ 400 } />
 
-					<span className="profile-gravatar__edit-label-wrap">
-						<span className="profile-gravatar__edit-label">
-							{ this.translate( 'Update Profile Photo' ) }
+						<span className="profile-gravatar__edit-label-wrap">
+							<span className="profile-gravatar__edit-label">
+								{ this.translate( 'Update Profile Photo' ) }
+							</span>
 						</span>
-					</span>
-				</a>
-
+					</a>
+				</Animate>
 				<h2 className="profile-gravatar__user-display-name">{ this.props.user.display_name }</h2>
-
 				<div className="profile-gravatar__user-secondary-info">
 					<a href={ profileURL } target="_blank">@{ this.props.user.username }</a>
 				</div>
