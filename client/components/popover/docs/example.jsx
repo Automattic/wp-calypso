1b360213ec02a905520e3e31c61c6886584e7694
retrofox <rdsuarez@gmail.com>
2016-08-31 09:25:40 -0300

    popover: update main popover class name

--- client/components/popover/docs/example.jsx
+++ client/components/popover/docs/example.jsx
@@ -296,7 +296,7 @@ const Popovers = React.createClass( {
 
 	render() {
 		return (
-			<div className="design-assets__group">
+			<div className="docs__design-assets__group">
 				<h2>
 					<a href="/devdocs/design/popovers">Popovers</a>
 				</h2>
