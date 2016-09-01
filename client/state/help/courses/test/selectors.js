ebdbfce38bc786f9fb2057bdc7826c3a312c8429
Omar Jackman <github@omarjackman.com>
2016-09-01 17:34:57 -0400

    Add tests for the help.courses state subtree

---
+++ client/state/help/courses/test/selectors.js
@@ -0,0 +1,47 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
+
+/**
+ * Internal dependencies
+ */
+import {
+	getHelpCourses,
+} from '../selectors';
+
+describe( 'selectors', () => {
+	describe( '#getHelpCourses()', () => {
+		it( 'should return null for default state', () => {
+			const state = deepFreeze( {
+				help: {
+					courses: {
+						items: null,
+					}
+				}
+			} );
+
+			expect( getHelpCourses( state ) ).to.be.null;
+		} );
+
+		it( 'should return courses for given state', () => {
+			const state = deepFreeze( {
+				help: {
+					courses: {
+						items: [
+							{
+								title: 'title',
+								description: 'description',
+								schedule: [],
+								videos: []
+							}
+						],
+					}
+				}
+			} );
+
+			expect( getHelpCourses( state ) ).to.eql( state.help.courses.items );
+		} );
+	} );
+} );
