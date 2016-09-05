33da9f2a92561a5f30e1a45df5cabaebb96d0b83
bluefuton <chris@bluefuton.com>
2016-09-05 15:22:40 +0100

    Show share button

--- client/blocks/reader-full-post/action-links.jsx
+++ client/blocks/reader-full-post/action-links.jsx
@@ -7,21 +7,23 @@ import React from 'react';
  * Internal dependencies
  */
 import LikeButton from 'reader/like-button';
+import ShareButton from 'reader/share';
 //import { shouldShowComments } from 'blocks/comments/helper';
 import { shouldShowLikes } from 'reader/like-helper';
-//import { shouldShowShare } from 'reader/share/helper';
+import { shouldShowShare } from 'reader/share/helper';
 
 const ReaderFullPostActionLinks = ( { post } ) => {
 	// if ( shouldShowComments ) {
 	// 	buttons.push( <CommentButton key="comment-button" commentCount={ this.props.commentCount } onClick={ this.handleCommentButtonClick } tagName="div" /> );
 	// }
 
-	// if ( shouldShowShare ) {
-	// 	buttons.push( <ShareButton post={ post } position="bottom" tagName="div" /> );
-	// }
-
 	return (
 		<ul className="reader-full-post__action-links">
+			{ shouldShowShare( post ) &&
+				<li className="reader-full-post__action-links-item">
+					<ShareButton post={ post } position="bottom" tagName="div" />
+				</li>
+			}
 			{ shouldShowLikes( post ) &&
 				<li className="reader-full-post__action-links-item">
 					<LikeButton
