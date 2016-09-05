f81d4f44b541477e4f7aec484fb629233c273416
retrofox <rdsuarez@gmail.com>
2016-09-05 15:29:37 -0300

    DocsExample: move into devdocs/design folder

--- client/devdocs/docs-example/index.jsx
+++ client/devdocs/docs-example/index.jsx
@@ -24,32 +24,28 @@ DocsExampleToggle.propTypes = {
 	text: PropTypes.string.isRequired
 };
 
-const DocsExampleStats = ( { count } ) => (
-	<div className="docs-example-stats">
-		<p className="docs-example-stats__item">Used in <Count count={ count } /> components.</p>
-	</div>
-);
+const DocsExampleStats = ( { count } ) =>
+	<div className="docs-example__stats">
+		<p className="docs-example__stats__item">
+			Used in <Count count={ count } /> components.
+		</p>
+	</div>;
 
 DocsExampleStats.propTypes = {
 	count: PropTypes.number.isRequired
 };
 
 const DocsExample = ( {
-	title,
-	url,
+	children,
 	componentUsageStats = {},
 	toggleHandler,
 	toggleText,
-	children
 } ) => {
 	const { count } = componentUsageStats;
 
 	return (
 		<section className="docs-example">
 			<header className="docs-example__header">
-				<h2 className="docs-example__title">
-					<a className="docs-example__link" href={ url }>{ title }</a>
-				</h2>
 				{
 					toggleHandler && toggleText && (
 						<span className="docs-example__toggle">
@@ -58,9 +54,11 @@ const DocsExample = ( {
 					)
 				}
 			</header>
+
 			<div className="docs-example__main">
 				{ children }
 			</div>
+
 			<footer role="contentinfo" className="docs-example__footer">
 				{
 					! isNaN( count ) && (
@@ -75,18 +73,17 @@ const DocsExample = ( {
 };
 
 DocsExample.propTypes = {
-	title: PropTypes.string.isRequired,
-	url: PropTypes.string.isRequired,
+	children: React.PropTypes.element.isRequired,
 	componentUsageStats: PropTypes.shape( {
 		count: PropTypes.number
 	} ),
 	toggleHandler: PropTypes.func,
 	toggleText: PropTypes.string,
-	children: React.PropTypes.element.isRequired
 };
 
 export {
 	DocsExampleToggle,
 	DocsExampleStats
 };
+
 export default DocsExample;
