1640a53a7cf0d95694682ab0c250e27e8e1c9bc0
bluefuton <chris@bluefuton.com>
2016-09-05 15:17:08 +0100

    Added like button

--- client/blocks/reader-full-post/action-links.jsx
+++ client/blocks/reader-full-post/action-links.jsx
@@ -3,13 +3,36 @@
  */
 import React from 'react';
 
+/**
+ * Internal dependencies
+ */
+import LikeButton from 'reader/like-button';
+//import { shouldShowComments } from 'blocks/comments/helper';
+import { shouldShowLikes } from 'reader/like-helper';
+//import { shouldShowShare } from 'reader/share/helper';
+
 const ReaderFullPostActionLinks = ( { post } ) => {
+	// if ( shouldShowComments ) {
+	// 	buttons.push( <CommentButton key="comment-button" commentCount={ this.props.commentCount } onClick={ this.handleCommentButtonClick } tagName="div" /> );
+	// }
+
+	// if ( shouldShowShare ) {
+	// 	buttons.push( <ShareButton post={ post } position="bottom" tagName="div" /> );
+	// }
+
 	return (
 		<ul className="reader-full-post__action-links">
-			<li>edit { post.title }</li>
-			<li>share</li>
-			<li>x comments</li>
-			<li>like</li>
+			{ shouldShowLikes( post ) &&
+				<li className="reader-full-post__action-links-item">
+					<LikeButton
+						key="like-button"
+						siteId={ post.site_ID }
+						postId={ post.ID }
+						fullPost={ true }
+						tagName="div"
+						forceCounter={ true } />
+				</li>
+			}
 		</ul>
 	);
 };
