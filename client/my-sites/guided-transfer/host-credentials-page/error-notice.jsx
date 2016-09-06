83fcc509201dc8138c484390464a81f07c0571f2
Jordan West <jordwest@gmail.com>
2016-09-07 08:22:02 +1000

    Use switch statement for error messages

--- client/my-sites/guided-transfer/host-credentials-page/error-notice.jsx
+++ client/my-sites/guided-transfer/host-credentials-page/error-notice.jsx
@@ -4,7 +4,6 @@
 import React from 'react';
 import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
-import { get } from 'lodash';
 
 /**
  * Internal dependencies
@@ -15,27 +14,25 @@ import supportUrl from 'lib/url/support';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { getGuidedTransferError } from 'state/sites/guided-transfer/selectors';
 
+const getErrorText = ( { translate, errorCode } ) => {
+	switch ( errorCode ) {
+		case 'exceeded-max-validation-attempts':
+			return translate( "You've entered invalid details one too many times. Please contact support." );
+		case 'verify-details-failed':
+			return translate( 'Those hosting details are incorrect. Please try again or contact support.' );
+		case 'details-already-validated':
+			return translate( "We've already confirmed your details. Please contact support if you need to change them." );
+	}
+
+	return translate( 'We had trouble saving your details. Please try again or contact support.' );
+};
+
 const ErrorNotice = localize( ( { translate, errorCode } ) => {
 	if ( ! errorCode ) {
 		return null;
 	}
 
-	const errorList = {
-		'exceeded-max-validation-attempts':
-			translate( "You've entered invalid details one too many times. Please contact support." ),
-		'verify-details-failed':
-			translate( 'Those hosting details are incorrect. Please try again or contact support.' ),
-		'details-already-validated':
-			translate( "We've already confirmed your details. Please contact support if you need to change them." ),
-	};
-
-	const errorText = get(
-		errorList,
-		errorCode,
-		translate( 'We had trouble saving your details. Please try again or contact support.' )
-	);
-
-	return <Notice showDismiss={ false } status="is-error" text={ errorText }>
+	return <Notice showDismiss={ false } status="is-error" text={ getErrorText( { translate, errorCode } ) }>
 		<NoticeAction href={ supportUrl.CALYPSO_CONTACT }>{ translate( 'Get Help' ) }</NoticeAction>
 	</Notice>;
 } );
