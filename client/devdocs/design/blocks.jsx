7982a3803658674cff1aada6a0eecaafba402d2c
Kerry Liu <kerryliu@gmail.com>
2016-09-07 15:49:12 -0700

    Plans: add upgrade nudge example and readme

--- client/devdocs/design/blocks.jsx
+++ client/devdocs/design/blocks.jsx
@@ -44,6 +44,7 @@ import PlanThankYouCard from 'blocks/plan-thank-you-card/docs/example';
 import DismissibleCard from 'blocks/dismissible-card/docs/example';
 import PostEditButton from 'blocks/post-edit-button/docs/example';
 import ReaderAvatar from 'blocks/reader-avatar/docs/example';
+import DomainToPlanNudge from 'blocks/domain-to-plan-nudge/docs/example';
 
 export default React.createClass( {
 
@@ -109,6 +110,7 @@ export default React.createClass( {
 					<PlanThankYouCard />
 					<DismissibleCard />
 					<ReaderAvatar />
+					<DomainToPlanNudge />
 				</Collection>
 			</div>
 		);
