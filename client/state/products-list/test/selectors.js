0f6c740a78181aa5655ccd65e671d49c65d89d83
Jordan West <jordwest@gmail.com>
2016-09-09 16:46:47 +1000

    Added test for isProductsListFetching selector

--- client/state/products-list/test/selectors.js
+++ client/state/products-list/test/selectors.js
@@ -7,10 +7,13 @@ import deepFreeze from 'deep-freeze';
 /**
  * Internal dependencies
  */
-import { getProductDisplayCost } from '../selectors';
+import {
+	getProductDisplayCost,
+	isProductsListFetching,
+} from '../selectors';
 
 describe( 'selectors', () => {
-	describe( 'getProductDisplayCost()', () => {
+	describe( '#getProductDisplayCost()', () => {
 		it( 'should return null when the products list has not been fetched', () => {
 			const state = deepFreeze( { productsList: { items: {} } } );
 
@@ -29,6 +32,18 @@ describe( 'selectors', () => {
 			} );
 
 			expect( getProductDisplayCost( state, 'guided_transfer' ) ).to.equal( 'A$169.00' );
+		} );
+	} );
+
+	describe( '#isProductsListFetching()', () => {
+		it( 'should return false when productsList.isFetching is false', () => {
+			const state = { productsList: { isFetching: false } };
+			expect( isProductsListFetching( state ) ).to.be.false;
+		} );
+
+		it( 'should return true when productsList.isFetching is true', () => {
+			const state = { productsList: { isFetching: true } };
+			expect( isProductsListFetching( state ) ).to.be.true;
 		} );
 	} );
 } );
