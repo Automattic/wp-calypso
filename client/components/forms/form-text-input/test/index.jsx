9da7d40add5104ebeee9dafbd87b0f2526487c36
Marko Andrijasevic <markoandrijasevic@gmail.com>
2016-09-16 15:14:47 -0700

    Refactor assertions

--- client/components/forms/form-text-input/test/index.jsx
+++ client/components/forms/form-text-input/test/index.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import { assert } from 'chai';
+import chai, { assert, expect } from 'chai';
 import { shallow } from 'enzyme';
+import chaienzyme from 'chai-enzyme';
 import FormTextInput from 'components/forms/form-text-input';
+
+chai.use( chaienzyme() );
 
 describe( '<FormTextInput />', () => {
 	describe( 'rendering', () => {
 		it( 'should add the provided class names', () => {
-			const wrapper = shallow( <FormTextInput className="foobar" /> );
+			const wrapper = shallow( <FormTextInput className="foobar" isError isValid /> );
 
-			assert( wrapper.node.props.className, 'form-text-input foobar' );
-		} );
-
-		it( 'should have is-error class', () => {
-			const wrapper = shallow( <FormTextInput isError /> );
-
-			assert.equal( wrapper.node.props.className, 'form-text-input is-error' );
-		} );
-
-		it( 'should have is-valid class', () => {
-			const wrapper = shallow( <FormTextInput isValid /> );
-
-			assert.equal( wrapper.node.props.className, 'form-text-input is-valid' );
+			expect( wrapper ).to.have.className( 'foobar' );
+			expect( wrapper ).to.have.className( 'is-error' );
+			expect( wrapper ).to.have.className( 'is-valid' );
 		} );
 
 		it( 'should not pass props down to the form fields', () => {
