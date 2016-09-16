bc63d1e51c2f6e8ad01fe5f0be32205060a175b7
Paul Sealock <psealock@gmail.com>
2016-09-16 15:11:15 -0700

    Jetpack plugins - prioritize order for updates
    refactor for readability
    
    rename 'reorderPlugins'
    
    order plugins by name, not slug
    
    dynamically use Card v. CompactCard
    
    replace lost prop
    
    ignore jsx-classname-namespace
    
    rename idx -> index

--- client/my-sites/plugins/plugin-item/plugin-item.jsx
+++ client/my-sites/plugins/plugin-item/plugin-item.jsx
@@ -11,6 +11,7 @@ import isEqual from 'lodash/isEqual';
  * Internal dependencies
  */
 import CompactCard from 'components/card/compact';
+import Card from 'components/card';
 import PluginIcon from 'my-sites/plugins/plugin-icon/plugin-icon';
 import PluginsActions from 'lib/plugins/actions';
 import PluginActivateToggle from 'my-sites/plugins/plugin-activate-toggle';
@@ -57,6 +58,7 @@ module.exports = React.createClass( {
 			inProgress: React.PropTypes.array,
 		} ),
 		hasAllNoManageSites: React.PropTypes.bool,
+		hasUpdate: React.PropTypes.func,
 	},
 
 	getDefaultProps() {
@@ -99,12 +101,6 @@ module.exports = React.createClass( {
 
 	ago( date ) {
 		return i18n.moment.utc( date, 'YYYY-MM-DD hh:mma' ).fromNow();
-	},
-
-	hasUpdate() {
-		return this.props.sites.some( function( site ) {
-			return site.plugin && site.plugin.update && site.canUpdateFiles;
-		} );
 	},
 
 	doing() {
@@ -192,7 +188,6 @@ module.exports = React.createClass( {
 				);
 			}
 		}
-
 		if ( this.props.isAutoManaged ) {
 			return (
 				<div className="plugin-item__last_updated">
@@ -201,7 +196,7 @@ module.exports = React.createClass( {
 			);
 		}
 
-		if ( this.hasUpdate() ) {
+		if ( this.props.hasUpdate( pluginData ) ) {
 			return this.renderUpdateFlag();
 		}
 
@@ -310,7 +305,7 @@ module.exports = React.createClass( {
 			numberOfWarningIcons++;
 		}
 
-		if ( this.hasUpdate() ) {
+		if ( this.props.hasUpdate( plugin ) ) {
 			numberOfWarningIcons++;
 		}
 
@@ -341,9 +336,12 @@ module.exports = React.createClass( {
 				</div>
 			);
 		}
+
+		const CardType = this.props.isCompact ? CompactCard : Card;
+		/* eslint-disable wpcalypso/jsx-classname-namespace */
 		return (
 			<div>
-				<CompactCard className="plugin-item">
+				<CardType className="plugin-item">
 					{ ! this.props.isSelectable
 						? null
 						: <input className="plugin-item__checkbox"
@@ -359,10 +357,11 @@ module.exports = React.createClass( {
 						{ this.pluginMeta( plugin ) }
 					</a>
 					{ this.props.selectedSite ? this.renderActions() : this.renderSiteCount() }
-				</CompactCard>
+				</CardType>
 				{ errorNotices }
 			</div>
 		);
+		/* eslint-enable wpcalypso/jsx-classname-namespace */
 	}
 
 } );
