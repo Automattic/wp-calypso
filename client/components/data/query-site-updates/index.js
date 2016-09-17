fedb50b43e6c91ef34368b817e370a353437e153
retrofox <rdsuarez@gmail.com>
2016-09-17 15:20:25 -0700

    component:data: add <QuerySiteUpdates /> component

---
+++ client/components/data/query-site-updates/index.js
@@ -0,0 +1,63 @@
+/**
+ * External dependencies
+ */
+import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { isRequestingSiteUpdates } from 'state/sites/updates/selectors';
+import { requestSiteUpdates as requestUpdates } from 'state/sites/updates/utils';
+
+class QuerySiteUpdates extends Component {
+	static propTypes = {
+		siteId: PropTypes.number,
+		requestingSiteUpdates: PropTypes.bool,
+		requestUpdates: PropTypes.func
+	};
+
+	static defaultProps = {
+		requestUpdates: () => {}
+	};
+
+	constructor( props ) {
+		super( props );
+		this.requestUpdates = this.requestUpdates.bind( this );
+	}
+
+	componentWillMount() {
+		this.requestUpdates();
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		if (
+			nextProps.requestingSiteUpdates ||
+			! nextProps.siteId ||
+			( this.props.siteId === nextProps.siteId )
+		) {
+			return;
+		}
+
+		this.requestUpdates( nextProps );
+	}
+
+	requestUpdates( props = this.props ) {
+		if ( ! props.requestingSiteUpdates && props.siteId ) {
+			props.requestUpdates( props.siteId );
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default connect(
+	( state, ownProps ) => {
+		return {
+			requestingSiteUpdates: isRequestingSiteUpdates( state, ownProps.siteId )
+		};
+	},
+	{ requestUpdates }
+)( QuerySiteUpdates );
