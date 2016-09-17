ce99bef306f0cd385dae7a1d541e145bd4c88185
Marin Atanasov <tyxla@abv.bg>
2016-09-17 12:19:21 -0700

    State: Shortcodes - add selector tests

---
+++ client/state/shortcodes/test/selectors.js
@@ -0,0 +1,50 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { getShortcode } from '../selectors';
+import { LOAD_STATUS } from '../constants';
+
+describe( 'selectors', () => {
+	describe( '#getShortcode()', () => {
+		const state = {
+			shortcodes: {
+				12345678: {
+					'[gallery ids="1,2,3"]': {
+						status: LOAD_STATUS.LOADED,
+						body: '<html></html>',
+						scripts: {},
+						styles: {}
+					},
+				}
+			}
+		};
+
+		it( 'should return the shortcode object for the site ID', () => {
+			const shortcode = getShortcode( state, 12345678, '[gallery ids="1,2,3"]' );
+
+			expect( shortcode ).to.eql( {
+				status: LOAD_STATUS.LOADED,
+				body: '<html></html>',
+				scripts: {},
+				styles: {}
+			} );
+		} );
+
+		it( 'should return undefined if there is no such site', () => {
+			const shortcode = getShortcode( state, 87654321, '[gallery ids="1,2,3"]' );
+
+			expect( shortcode ).to.be.undefined;
+		} );
+
+		it( 'should return undefined if there is no such shortcode for a site', () => {
+			const shortcode = getShortcode( state, 12345678, '[gallery]' );
+
+			expect( shortcode ).to.be.undefined;
+		} );
+	} );
+} );
