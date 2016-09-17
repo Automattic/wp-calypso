113854631131a05e2e6a1a47a77d7d62ce68f5d1
Marin Atanasov <tyxla@abv.bg>
2016-09-16 17:13:29 -0700

    State: Add shortcodes action types and actions

--- client/state/shortcodes/reducer.js
+++ client/state/shortcodes/reducer.js
@@ -1,17 +1,44 @@
 /**
- * External dependencies
+ * Internal dependencies
  */
-import { combineReducers } from 'redux';
+import { createReducer } from 'state/utils';
+import { LOAD_STATUS } from './constants';
+import {
+	SHORTCODE_FETCH,
+	SHORTCODE_RECEIVE
+} from 'state/action-types';
 
-export function status( state = {} ) {
-	return state;
-}
+const siteShortcode = createReducer( {}, {
+	[ SHORTCODE_FETCH ]: ( state ) => {
+		return { ...state, ...{ status: LOAD_STATUS.LOADING } };
+	},
+	[ SHORTCODE_RECEIVE ]: ( state, { error, data } ) => {
+		const { scripts, styles } = data;
+		const body = data.result;
+		const status = error ? LOAD_STATUS.ERROR : LOAD_STATUS.LOADED;
 
-export function shortcodes( state = {} ) {
-	return state;
-}
-
-export default combineReducers( {
-	status,
-	shortcodes
+		return { ...state, ...{ body, scripts, styles, status } };
+	},
 } );
+
+const siteShortcodes = ( state = {}, action ) => {
+	switch ( action.type ) {
+		case SHORTCODE_FETCH:
+		case SHORTCODE_RECEIVE:
+			const { shortcode } = action;
+			return { ...state, ...{ [ shortcode ]: siteShortcode( state[ shortcode ] || {}, action ) } };
+	}
+
+	return state;
+};
+
+export default ( state = {}, action ) => {
+	switch ( action.type ) {
+		case SHORTCODE_FETCH:
+		case SHORTCODE_RECEIVE:
+			const { siteId } = action;
+			return { ...state, ...{ [ siteId ]: siteShortcodes( state[ siteId ] || {}, action ) } };
+	}
+
+	return state;
+};
