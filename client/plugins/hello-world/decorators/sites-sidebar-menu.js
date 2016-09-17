b3c6effa1c1bc9e637a07b3d4b6b7033ee4b754c
Matías Ventura <mv@matiasventura.com>
2016-09-17 15:32:18 -0700

    Plugin stuff.

--- client/plugins/hello-world/decorators/sites-sidebar-menu.js
+++ client/plugins/hello-world/decorators/sites-sidebar-menu.js
@@ -1,1 +1,14 @@
-export default () => {};
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+// import SidebarItem from 'layout/sidebar/item';
+import SitesSidebarMenu from 'my-sites/sidebar/menu';
+
+export default class PostCommentContent extends React.Component {
+	render() {
+		const children = Array.from( this.props.extraChildren ).concat( <p>Hellow World</p> );
+		return <SitesSidebarMenu { ...this.props } extraChidren={ children } />;
+	}
+}
