248f6649c8029454ea4cd185d22c75b6ec2037e8
Miguel Fonseca <miguelcsf@gmail.com>
2016-09-18 15:35:49 -0700

    hello-dolly: prettify sidebar decoration

--- client/plugins/hello-dolly/decorators/sites-sidebar-menu.js
+++ client/plugins/hello-dolly/decorators/sites-sidebar-menu.js
@@ -7,13 +7,26 @@ import { concat } from 'lodash';
 /**
  * Internal dependencies
  */
+import Card from 'components/card';
 import HelloDolly from 'plugins/hello-dolly/decorators/hello-dolly';
 
 export default function HelloDollyDecorator( Base ) {
+	const style = {
+		display: 'inline-block',
+		maxWidth: 'calc(100% - 20px)',
+		overflow: 'hidden',
+		textOverflow: 'ellipsis',
+		whiteSpace: 'nowrap',
+	};
+
 	return class extends React.Component {
 		render() {
 			const children = concat( this.props.extraChildren,
-				<HelloDolly />
+				<Card compact={ true }>
+					<span style={ style }>
+						<HelloDolly />
+					</span>
+				</Card>
 			);
 			return <Base { ...this.props } extraChildren={ children } />;
 		}
