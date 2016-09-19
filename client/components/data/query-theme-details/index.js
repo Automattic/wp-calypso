ada2c0aa99f513954e62a42032c9f5624db4c2d9
Bernhard Reiter <ockham@raz.or.at>
2016-09-19 13:03:22 -0700

    QueryThemeDetails: Turn into an ES6 class

--- client/components/data/query-theme-details/index.js
+++ client/components/data/query-theme-details/index.js
@@ -3,7 +3,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 
 /**
@@ -15,33 +15,35 @@ import { fetchThemeDetails } from 'state/themes/actions';
  * Fetches details for a theme specified by its ID
  * and passes it to the supplied child component.
  */
-const QueryThemeDetails = React.createClass( {
-
-	propTypes: {
-		id: React.PropTypes.string.isRequired,
-	},
+class QueryThemeDetails extends Component {
 
 	componentDidMount() {
 		this.refresh( this.props );
-	},
+	}
 
 	componentWillReceiveProps( nextProps ) {
 		if ( nextProps.id && nextProps.id !== this.props.id ) {
 			this.refresh( nextProps );
 		}
-	},
+	}
 
 	refresh( props ) {
 		// todo (seear): Don't fetch if site matches existing data
 		if ( props.id ) {
 			this.props.fetchThemeDetails( props.id, props.siteId );
 		}
-	},
+	}
 
 	render() {
 		return null;
 	}
-} );
+}
+
+QueryThemeDetails.propTypes = {
+	id: PropTypes.string.isRequired,
+	// connected props
+	fetchThemeDetails: PropTypes.func,
+};
 
 export default connect(
 	null,
