354a10188739743a7cf6816ccc70be0442ddca44
retrofox <rdsuarez@gmail.com>
2016-09-19 12:33:51 -0700

    state:site:updates: add new selectors

--- client/state/sites/updates/selectors.js
+++ client/state/sites/updates/selectors.js
@@ -7,7 +7,20 @@ export const getUpdatesBySiteId = ( state, siteId ) => {
 	return state.sites.updates.items[ siteId ] || [];
 };
 
-export const hasWordPressActualization = ( state, siteId ) => {
-	const { wordpress, wp_update_version } = state.sites.updates.items[ siteId ] || {};
+export const hasUpdates = ( state, siteId ) => {
+	const { total } = state.sites.updates.items[ siteId ] || { total: 0 };
+	return total > 0;
+};
+
+export const hasWordPressUpdate = ( state, siteId ) => {
+	const { wordpress, wp_update_version } = getUpdatesBySiteId( state, siteId );
 	return !! wordpress && wp_update_version;
+};
+
+export const getSectionsToUpdate = ( state, siteId ) => {
+	const updates = getUpdatesBySiteId( state, siteId );
+	const sectionsToUpdate = [ 'plugins', 'themes', 'translations', 'wordpress' ];
+
+	return sectionsToUpdate
+		.filter( section => updates[ section ] !== 'undefined' && updates[ section ] > 0 );
 };
