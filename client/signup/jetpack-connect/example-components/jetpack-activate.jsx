39cb2c383f6d7be5fe8c6bd013f62b97824c5b72
Marin Atanasov <tyxla@abv.bg>
2016-09-19 14:45:25 -0700

    Jetpack Connect: Convert Activate example component to a stateless function component

--- client/signup/jetpack-connect/example-components/jetpack-activate.jsx
+++ client/signup/jetpack-connect/example-components/jetpack-activate.jsx
@@ -8,71 +8,66 @@ import React from 'react';
  */
 import FormTextInput from 'components/forms/form-text-input';
 import Gridicon from 'components/gridicon';
+import { localize } from 'i18n-calypso';
 
-export default React.createClass( {
-	displayName: 'JetpackConnectExampleActivate',
-
-	renderActivate() {
-		if ( this.props.isInstall ) {
-			return (
-				<div className="example-components__content-wp-admin-activate-view">
-					<div className="example-components__content-wp-admin-activate-link" aria-hidden="true">
-						{ this.translate( 'Activate Plugin',
-							{
-								context: 'Jetpack Connect activate plugin instructions, activate link'
-							}
-						) }
-					</div>
+const JetpackConnectExampleActivate = ( { isInstall, url, translate } ) => {
+	return (
+		<div className="example-components__main">
+			<div className="example-components__browser-chrome example-components__site-url-input-container">
+				<div className="example-components__browser-chrome-dots">
+					<div className="example-components__browser-chrome-dot"></div>
+					<div className="example-components__browser-chrome-dot"></div>
+					<div className="example-components__browser-chrome-dot"></div>
 				</div>
-			);
-		}
-		return (
-			<div className="example-components__content-wp-admin-plugin-card">
-				<div className="example-components__content-wp-admin-plugin-name" aria-hidden="true">
-					{ this.translate( 'Jetpack by WordPress.com',
-						{
-							context: 'Jetpack Connect activate plugin instructions, plugin title'
-						}
-					) }
-				</div>
-				<div className="example-components__content-wp-admin-plugin-activate-link" aria-hidden="true">
-					{ this.translate( 'Activate',
-						{
-							context: 'Jetpack Connect activate plugin instructions, activate link'
-						}
-					) }
+				<div className="example-components__site-address-container">
+					<Gridicon
+						size={ 24 }
+						icon="globe" />
+					<FormTextInput
+						className="example-components__browser-chrome-url"
+						disabled="true"
+						placeholder={ url } />
 				</div>
 			</div>
-		);
-	},
-
-	render() {
-		return (
-			<div className="example-components__main">
-				<div className="example-components__browser-chrome example-components__site-url-input-container">
-					<div className="example-components__browser-chrome-dots">
-						<div className="example-components__browser-chrome-dot"></div>
-						<div className="example-components__browser-chrome-dot"></div>
-						<div className="example-components__browser-chrome-dot"></div>
-					</div>
-					<div className="example-components__site-address-container">
-						<Gridicon
-							size={ 24 }
-							icon="globe" />
-						<FormTextInput
-							className="example-components__browser-chrome-url"
-							disabled="true"
-							placeholder={ this.props.url } />
-					</div>
-				</div>
-				<div className="example-components__content example-components__activate-jetpack">
-					<div className="example-components__content-wp-admin-masterbar"></div>
-					<div className="example-components__content-wp-admin-sidebar"></div>
-					<div className="example-components__content-wp-admin-main">
-						{ this.renderActivate() }
-					</div>
+			<div className="example-components__content example-components__activate-jetpack">
+				<div className="example-components__content-wp-admin-masterbar"></div>
+				<div className="example-components__content-wp-admin-sidebar"></div>
+				<div className="example-components__content-wp-admin-main">
+					{ isInstall
+						? (
+							<div className="example-components__content-wp-admin-activate-view">
+								<div className="example-components__content-wp-admin-activate-link" aria-hidden="true">
+									{ translate( 'Activate Plugin',
+										{
+											context: 'Jetpack Connect activate plugin instructions, activate link'
+										}
+									) }
+								</div>
+							</div>
+						)
+						: (
+							<div className="example-components__content-wp-admin-plugin-card">
+								<div className="example-components__content-wp-admin-plugin-name" aria-hidden="true">
+									{ translate( 'Jetpack by WordPress.com',
+										{
+											context: 'Jetpack Connect activate plugin instructions, plugin title'
+										}
+									) }
+								</div>
+								<div className="example-components__content-wp-admin-plugin-activate-link" aria-hidden="true">
+									{ translate( 'Activate',
+										{
+											context: 'Jetpack Connect activate plugin instructions, activate link'
+										}
+									) }
+								</div>
+							</div>
+						)
+					}
 				</div>
 			</div>
-		);
-	}
-} );
+		</div>
+	);
+};
+
+export default localize( JetpackConnectExampleActivate );
