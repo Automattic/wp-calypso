10061d3b5dd534551302354004c5e786edf06c51
sarah <sarah@triggersandsparks.com>
2016-09-19 11:22:13 -0700

    Set up skeleton header styles and content.

--- client/by/profile/header.jsx
+++ client/by/profile/header.jsx
@@ -5,7 +5,9 @@ import React, { PropTypes } from 'react';
 
 const ProfileHeader = ( { username } ) => (
 	<div className="profile__header">
-		<h1>{ username }</h1>
+		<img className="profile__gravatar" src="/calypso/images/people/mystery-person.svg" alt="{ username }" />
+		<h1>Public Display Name</h1>
+		<h2>@{ username }, member since 2006</h2>
 	</div>
 );
 
