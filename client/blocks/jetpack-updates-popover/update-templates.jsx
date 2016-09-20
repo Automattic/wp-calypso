3b0302012217007bc7cda927de806ba4c03a8159
retrofox <rdsuarez@gmail.com>
2016-09-20 12:39:05 -0700

    jetpack-updates-popover: add to blocks/ component

---
+++ client/blocks/jetpack-updates-popover/update-templates.jsx
@@ -0,0 +1,123 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import config from 'config';
+
+/**
+ * Internal dependencies
+ */
+import Gridicon from 'components/gridicon';
+
+const handleWPCOMUpdate = ( event ) => {
+	event.preventDefault();
+};
+
+export const renderWPComTemplate = ( { translate, updates } ) => {
+	// just for testing purpose
+	if ( config.isEnabled( 'gm2016/jetpack-plugin-updates-trashpickup' ) ) {
+		updates.wordpress = 1;
+		updates.wp_update_version = '5.0.0';
+	}
+
+	if ( ! (
+		config.isEnabled( 'jetpack_core_inline_update' ) &&
+		updates.wordpress &&
+		updates.wp_update_version
+	) ) {
+		return null;
+	}
+
+	return (
+		<div className="jetpack-updates-popover__block">
+			<Gridicon icon="my-sites" size={ 18 } />
+			<div className="jetpack-updates-popover__text">
+				{ translate( 'A newer version of WordPress is available.' ) }
+				<a
+					className="jetpack-updates-popover__link"
+					href="#"
+					onClick={ handleWPCOMUpdate }
+				>
+					{
+						translate( 'Update to %(version)s', {
+							args: {
+								version: updates.wp_update_version
+							}
+						} )
+					}
+					<Gridicon icon="external" size={ 12 } />
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export const renderPluginsTemplate = ( { onClose, site, translate, updates } ) => {
+	if ( ! site.canUpdateFiles || ! updates.plugins ) {
+		return null;
+	}
+
+	return (
+		<div className="jetpack-updates-popover__block">
+			<Gridicon icon="plugins" size={ 18 } />
+			<div className="jetpack-updates-popover__text">
+				{
+					translate(
+						'There is %(total)d plugin update available.',
+						'There are %(total)d plugin updates available.',
+						{
+							count: updates.plugins,
+							args: {
+								total: updates.plugins
+							}
+						}
+					)
+				}
+				<a
+					className="jetpack-updates-popover__link"
+					onClick={ onClose }
+					href={ '/plugins/updates/' + site.slug }>
+					{ translate( 'View' ) }
+					<Gridicon icon="external" size={ 12 } />
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export const renderThemesTemplate = ( { onClose, updates, site, translate } ) => {
+	if ( ! updates.themes ) {
+		return null;
+	}
+
+	return (
+		<div className="jetpack-updates-popover__block">
+			<Gridicon icon="themes" size={ 18 } />
+			<div className="jetpack-updates-popover__text">
+				{
+					translate(
+						'There is %(total)d theme update available. {{link}}View{{/link}}',
+						'There are %(total)d theme updates available. {{link}}View{{/link}}',
+						{
+							components: {
+								link:
+									<a
+										className="jetpack-updates-popover__link"
+										onClick={ onClose }
+										target="_blanck"
+										href={ site.options.admin_url + 'update-core.php#update-themes-table' }
+									>
+										<Gridicon icon="external" size={ 12 } />
+									</a>
+							},
+							count: updates.themes,
+							args: {
+								total: updates.themes
+							}
+						}
+					)
+				}
+			</div>
+		</div>
+	);
+};
