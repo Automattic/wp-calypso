4824fdf7d3d5d1b524504c153592730fe50da2b1
Tugdual de Kerviler <dekervit@gmail.com>
2016-09-20 17:09:50 -0700

    Plans: refactor Plans controller into the main Component

--- client/my-sites/plans/main.jsx
+++ client/my-sites/plans/main.jsx
@@ -2,16 +2,23 @@
  * External dependencies
  */
 import { connect } from 'react-redux';
+import i18n from 'i18n-calypso';
 import React from 'react';
 
 /**
  * Internal dependencies
  */
+import analytics from 'lib/analytics';
+import EmptyContentComponent from 'components/empty-content';
 import { getPlansBySite } from 'state/sites/plans/selectors';
 import { getPlans } from 'state/plans/selectors';
 import { getSelectedSite, getSelectedSiteId } from 'state/ui/selectors';
+import { isEnabled } from 'config';
 import Main from 'components/main';
+import MainComponent from 'components/main';
 import PlansFeaturesMain from 'my-sites/plans-features-main';
+import route from 'lib/route';
+import { setDocumentHeadTitle as setTitle } from 'state/document-head/actions';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import UpgradesNavigation from 'my-sites/upgrades/navigation';
 import QueryPlans from 'components/data/query-plans';
@@ -34,7 +41,55 @@ const Plans = React.createClass( {
 		};
 	},
 
+	shouldDisplayJetpackPlansDisabled() {
+		const { selectedSite } = this.props;
+
+		return selectedSite && selectedSite.jetpack && ! isEnabled( 'manage/jetpack-plans' );
+	},
+
+	componentDidMount() {
+		const { context, selectedSite } = this.props,
+			analyticsPageTitle = 'Plans',
+			basePath = route.sectionify( context.path ),
+			analyticsBasePath = ( selectedSite ) ? basePath + '/:site' : basePath;
+
+		// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
+		context.store.dispatch( setTitle( i18n.translate( 'Plans', { textOnly: true } ) ) );
+
+		if ( this.shouldDisplayJetpackPlansDisabled() ) {
+			analytics.pageView.record( basePath + '/jetpack/:site', analyticsPageTitle + ' > Jetpack Plans Not Available' );
+		} else {
+			analytics.tracks.recordEvent( 'calypso_plans_view' );
+			analytics.pageView.record( analyticsBasePath, analyticsPageTitle );
+		}
+
+		// Scroll to the top
+		if ( typeof window !== 'undefined' ) {
+			window.scrollTo( 0, 0 );
+		}
+	},
+
+	renderJetpackPlansDisabled() {
+		return (
+			<MainComponent>
+				<EmptyContentComponent
+					title={ i18n.translate( 'Plans are not available for Jetpack sites yet.' ) }
+					line={ i18n.translate( 'Looking for spam protection?' ) }
+					action={ i18n.translate( 'Try Akismet' ) }
+					actionURL={ '//akismet.com/plans/?ref=calypso-plans' }
+					illustration={ '/calypso/images/drake/drake-nomenus.svg' }
+				/>
+			</MainComponent>
+		);
+	},
+
 	render() {
+		const { selectedSite, selectedSiteId } = this.props;
+
+		if ( this.shouldDisplayJetpackPlansDisabled() ) {
+			this.renderJetpackPlansDisabled();
+		}
+
 		return (
 			<div>
 				<Main wideLayout={ true } >
@@ -45,19 +100,19 @@ const Plans = React.createClass( {
 							sitePlans={ this.props.sitePlans }
 							path={ this.props.context.path }
 							cart={ this.props.cart }
-							selectedSite={ this.props.site } />
+							selectedSite={ selectedSite } />
 
 						<QueryPlans />
-						<QuerySitePlans siteId={ this.props.siteId } />
+						<QuerySitePlans siteId={ selectedSiteId } />
 
 						<PlansFeaturesMain
-							site={ this.props.selectedSite }
+							site={ selectedSite }
 							intervalType={ this.props.intervalType }
 							hideFreePlan={ true }
 							selectedFeature={ this.props.selectedFeature }
 						/>
 					</div>
-				</Main> 
+				</Main>
 			</div>
 		);
 	}
