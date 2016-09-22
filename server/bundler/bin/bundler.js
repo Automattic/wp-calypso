402a09272c6f1d969a41a88c396bd5d23da4c464
Ben Lowery <ben.lowery@automattic.com>
2016-09-21 22:51:03 -0700

    use optimize-js in addition to uglify2

--- server/bundler/bin/bundler.js
+++ server/bundler/bin/bundler.js
@@ -31,26 +31,66 @@ outputOptions = {
 };
 
 function minify( files ) {
-	files.forEach( function( file ) {
-		var child;
+	return new Promise( function( resolve ) {
+		const minifiedFiles = [];
+		files.forEach( function( file ) {
+			var child;
 
-		if ( /\.map$/.test( file ) ) {
-			return;
-		}
-
-		child = cp.spawn( path.join( 'node_modules', '.bin', 'uglifyjs' ), [ '--output', file.replace( '.js', '.min.js' ), file ] );
-
-		_children.push( child );
-
-		child.once( 'exit', function() {
-			_children.splice( _children.indexOf( child ), 1 );
-			if ( _children.length === 0 ) {
-				console.log( 'Minification of all bundles completed.' );
-				console.log( 'Total build time: ' + ( new Date().getTime() - start ) + 'ms' );
+			if ( /\.map$/.test( file ) ) {
+				return;
 			}
+
+			const minifiedPath = file.replace( '.js', '.min.js' );
+
+			child = cp.spawn( path.join( 'node_modules', '.bin', 'uglifyjs' ), [ '--output', minifiedPath, file ] );
+
+			minifiedFiles.push( minifiedPath );
+
+			_children.push( child );
+
+			child.once( 'exit', function() {
+				_children.splice( _children.indexOf( child ), 1 );
+				if ( _children.length === 0 ) {
+					console.log( 'Minification of all bundles completed.' );
+					resolve( minifiedFiles );
+				}
+			} );
 		} );
 	} );
 }
+
+function optimize( files ) {
+	return new Promise( function( resolve ) {
+		files.forEach( function( file ) {
+			var child;
+
+			if ( /\.map$/.test( file ) ) {
+				return;
+			}
+
+			child = cp.spawn(
+				path.join( 'node_modules', '.bin', 'optimize-js' ),
+				[ file ], {
+					stdio: [
+						'pipe',
+						fs.openSync( file.replace( '.js', '.optimize.js' ), 'w' ),
+					 	'pipe'
+					]
+				} );
+
+			_children.push( child );
+
+			child.once( 'exit', function() {
+				_children.splice( _children.indexOf( child ), 1 );
+				if ( _children.length === 0 ) {
+					console.log( 'optimize-js of all bundles completed.' );
+					resolve();
+				}
+			} );
+		} );
+	} );
+}
+
 
 Error.stackTrackLimit = 30;
 
@@ -85,5 +125,7 @@ webpack( webpackConfig, function( error, stats ) {
 		return path.join( process.cwd(), 'public', chunk.file );
 	} );
 
-	minify( files );
+	minify( files ).then( optimize ).then( function() {
+		console.log( 'Total build time: ' + ( new Date().getTime() - start ) + 'ms' );
+	} );
 });
