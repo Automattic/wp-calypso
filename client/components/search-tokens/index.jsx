9ad14379d3ad2d5d2babd0e8b3b83f1c388a55e0
budzanowski <bartosz.budzanowski@gmail.com>
2016-09-24 16:21:58 -0700

    Something somethings tokens wokrk at 100% zoom.

--- client/components/search-tokens/index.jsx
+++ client/components/search-tokens/index.jsx
@@ -15,46 +15,54 @@ const SearchTokens = React.createClass( {
 	getInitialState() {
 		return {
 			input: "",
-			tokens: null
 		};
 	},
 
-	generateTokens( input ) {
-		const tokens = input.split(/(\s+)/);
 
-		return (
-			<div className="search-tokens__tokens">
-				{
-					tokens.map( token => {
-						if ( token.trim() === '' ) {
-							return <span className="search-tokens__white-space">{ token }</span>;
-						} else {
-							return <span className="search-tokens__token">{ token }</span>;
-						}
-					} )
-				}
-			</div>
-		);
+	scrollOverlay: function() {
+	  const _this = this;
+	  window.requestAnimationFrame(function() {
+			console.log( 'init_tokens: ' + _this.refs.tokens.scrollLeft );
+			console.log( 'init_input: ' + _this.refs.inputField.scrollLeft );
+			_this.refs.tokens.scrollLeft = _this.refs.inputField.scrollLeft;
+			_this.refs.inputField.scrollLeft = _this.refs.tokens.scrollLeft;
+			console.log( 'after_tokens: ' + _this.refs.tokens.scrollLeft );
+			console.log( 'after_input: ' + _this.refs.inputField.scrollLeft );
+		} );
+	},
+
+	componentDidUpdate: function() {
+	  this.scrollOverlay();
 	},
 
 	onChange( event ) {
 		this.setState( { input: event.target.value } );
-		const tokens = this.generateTokens( event.target.value );
-		this.setState( { tokens: tokens } );
 	},
 
 //Maybe background has impact on on text rendering.
 //render text in span. and overlay stylinf in before.
 
 	render() {
+		const tokens = this.state.input.split(/(\s+)/);
+
 		return (
 			<div	className="search-tokens">
 				<input
 					type="text"
+					ref="inputField"
 					onChange={ this.onChange }
 					className="search-tokens__input"
 				/>
-				{ this.state.tokens }
+				<div className="search-tokens__tokens" ref="tokens">
+					{
+						tokens.map( ( token, i ) => {
+							const cls = token.trim() === ''
+							? "search-tokens__white-space"
+							: "search-tokens__token";
+								return <span className={ cls } key={ i }>{ token }</span>; // use shortid for key
+						} )
+					}
+				</div>
 			</div>
 		);
 	}
