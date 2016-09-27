e45375dceaad7ec6192782da7b13af6fe1cfd06f
Marin Atanasov <tyxla@users.noreply.github.com>
2016-09-27 09:49:03 +0300

    Jetpack Connect: Convert Install example component to a stateless function component (#8146)

--- client/signup/jetpack-connect/example-components/jetpack-install.jsx
+++ client/signup/jetpack-connect/example-components/jetpack-install.jsx
@@ -8,43 +8,42 @@ import React from 'react';
  */
 import FormTextInput from 'components/forms/form-text-input';
 import Gridicon from 'components/gridicon';
+import { localize } from 'i18n-calypso';
 
-export default React.createClass( {
-	displayName: 'JetpackConnectExampleInstall',
-
-	render() {
-		return (
-			<div className="example-components__main">
-				<div className="example-components__browser-chrome example-components__site-url-input-container">
-					<div className="example-components__browser-chrome-dots">
-						<div className="example-components__browser-chrome-dot"></div>
-						<div className="example-components__browser-chrome-dot"></div>
-						<div className="example-components__browser-chrome-dot"></div>
-					</div>
-					<div className="example-components__site-address-container">
-						<Gridicon
-							size={ 24 }
-							icon="globe" />
-						<FormTextInput
-							className="example-components__browser-chrome-url"
-							disabled="true"
-							placeholder={ this.props.url } />
-					</div>
+const JetpackConnectExampleInstall = ( { url, translate } ) => {
+	return (
+		<div className="example-components__main">
+			<div className="example-components__browser-chrome example-components__site-url-input-container">
+				<div className="example-components__browser-chrome-dots">
+					<div className="example-components__browser-chrome-dot"></div>
+					<div className="example-components__browser-chrome-dot"></div>
+					<div className="example-components__browser-chrome-dot"></div>
 				</div>
-				<div className="example-components__content example-components__install-jetpack">
-					<div className="example-components__install-plugin-header"></div>
-					<div className="example-components__install-plugin-body"></div>
-					<div className="example-components__install-plugin-footer">
-						<div className="example-components__install-plugin-footer-button" aria-hidden="true">
-							{ this.translate( 'Install Now',
-								{
-									context: 'Jetpack Connect install plugin instructions, install button'
-								}
-							) }
-						</div>
+				<div className="example-components__site-address-container">
+					<Gridicon
+						size={ 24 }
+						icon="globe" />
+					<FormTextInput
+						className="example-components__browser-chrome-url"
+						disabled="true"
+						placeholder={ url } />
+				</div>
+			</div>
+			<div className="example-components__content example-components__install-jetpack">
+				<div className="example-components__install-plugin-header"></div>
+				<div className="example-components__install-plugin-body"></div>
+				<div className="example-components__install-plugin-footer">
+					<div className="example-components__install-plugin-footer-button" aria-hidden="true">
+						{ translate( 'Install Now',
+							{
+								context: 'Jetpack Connect install plugin instructions, install button'
+							}
+						) }
 					</div>
 				</div>
 			</div>
-		);
-	}
-} );
+		</div>
+	);
+};
+
+export default localize( JetpackConnectExampleInstall );
