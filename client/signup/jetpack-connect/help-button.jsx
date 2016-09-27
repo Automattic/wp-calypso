f047b363f4833ab79bc921b5736dbad7bde4614b
Marin Atanasov <tyxla@users.noreply.github.com>
2016-09-27 09:15:39 +0300

    Jetpack Connect: Convert HelpButton to a stateless function component (#8135)

--- client/signup/jetpack-connect/help-button.jsx
+++ client/signup/jetpack-connect/help-button.jsx
@@ -8,19 +8,18 @@ import React from 'react';
  */
 import LoggedOutFormLinkItem from 'components/logged-out-form/link-item';
 import Gridicon from 'components/gridicon';
+import { localize } from 'i18n-calypso';
 
-export default React.createClass( {
-	displayName: 'JetpackConnectHelpButton',
+const JetpackConnectHelpButton = ( { translate } ) => {
+	return (
+		<LoggedOutFormLinkItem
+			className="jetpack-connect__help-button"
+			href="https://jetpack.com/contact-support"
+			target="_blank" rel="noopener noreferrer"
+		>
+			<Gridicon icon="help-outline" /> { translate( 'Get help connecting your site' ) }
+		</LoggedOutFormLinkItem>
+	);
+};
 
-	render() {
-		return (
-			<LoggedOutFormLinkItem
-				className="jetpack-connect__help-button"
-				href="https://jetpack.com/contact-support"
-				target="_blank" rel="noopener noreferrer"
-			>
-				<Gridicon icon="help-outline" /> { this.translate( 'Get help connecting your site' ) }
-			</LoggedOutFormLinkItem>
-		);
-	}
-} );
+export default localize( JetpackConnectHelpButton );
