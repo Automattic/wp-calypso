bea38e78563b2c3bae81f4987ed60a3a81fc05cf
Marko Andrijasevic <markoandrijasevic@gmail.com>
2016-09-28 00:30:05 +0200

    Refactor code
    * Remove unnecessary chai and chaienzyme imports
    * Fix lint warnings
    * Remove unneeded describe block.

--- client/components/forms/form-text-input/test/index.jsx
+++ client/components/forms/form-text-input/test/index.jsx
@@ -1,58 +1,51 @@
 import React from 'react';
-import chai, { expect } from 'chai';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import { spy } from 'sinon';
-import chaienzyme from 'chai-enzyme';
 import FormTextInput from 'components/forms/form-text-input';
 
-chai.use( chaienzyme() );
-
 describe( '<FormTextInput />', () => {
-	describe( 'rendering', () => {
-		it( 'should add the provided class names', () => {
-			const wrapper = shallow( <FormTextInput className="test" isError isValid /> );
+	it( 'should add the provided class names', () => {
+		const wrapper = shallow( <FormTextInput className="test" isError isValid /> );
 
-			expect( wrapper ).to.have.className( 'test' );
-			expect( wrapper ).to.have.className( 'is-error' );
-			expect( wrapper ).to.have.className( 'is-valid' );
-		} );
-
-		it( 'should not pass props down to the form fields', () => {
-			const wrapper = shallow( <FormTextInput isValid isError selectOnFocus /> );
-
-			expect( wrapper ).to.not.have.prop( 'isValid' );
-			expect( wrapper ).to.not.have.prop( 'isError' );
-			expect( wrapper ).to.not.have.prop( 'selectOnFocus' );
-		} );
-
-		it( 'should call select if selectOnFocus is true', () => {
-			const wrapper = shallow( <FormTextInput selectOnFocus={ true } /> );
-			const event = {
-				target: {
-					select: () => {}
-				}
-			};
-
-			spy( event.target, 'select' );
-			wrapper.simulate( 'click', event );
-
-			expect( event.target.select ).to.have.been.calledOnce;
-		} );
-
-		it( 'should not call select if selectOnFocus is false', () => {
-			const wrapper = shallow( <FormTextInput selectOnFocus={ false } /> );
-			const event = {
-				target: {
-					select: () => {}
-				}
-			};
-
-			spy( event.target, 'select' );
-			wrapper.simulate( 'click', event );
-
-			expect( event.target.select ).to.not.have.been.called;
-		} );
-
+		expect( wrapper ).to.have.className( 'test' );
+		expect( wrapper ).to.have.className( 'is-error' );
+		expect( wrapper ).to.have.className( 'is-valid' );
 	} );
 
+	it( 'should not pass props down to the form fields', () => {
+		const wrapper = shallow( <FormTextInput isValid isError selectOnFocus /> );
+
+		expect( wrapper ).to.not.have.prop( 'isValid' );
+		expect( wrapper ).to.not.have.prop( 'isError' );
+		expect( wrapper ).to.not.have.prop( 'selectOnFocus' );
+	} );
+
+	it( 'should call select if selectOnFocus is true', () => {
+		const wrapper = shallow( <FormTextInput selectOnFocus={ true } /> );
+		const event = {
+			target: {
+				select: () => {}
+			}
+		};
+
+		spy( event.target, 'select' );
+		wrapper.simulate( 'click', event );
+
+		expect( event.target.select ).to.have.been.calledOnce;
+	} );
+
+	it( 'should not call select if selectOnFocus is false', () => {
+		const wrapper = shallow( <FormTextInput selectOnFocus={ false } /> );
+		const event = {
+			target: {
+				select: () => {}
+			}
+		};
+
+		spy( event.target, 'select' );
+		wrapper.simulate( 'click', event );
+
+		expect( event.target.select ).to.not.have.been.called;
+	} );
 } );
