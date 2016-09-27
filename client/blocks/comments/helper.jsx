2506557b5fe973f9f28c27fe0ad442764cd4de04
bluefuton <chris@bluefuton.com>
2016-09-27 12:16:27 -0700

    Comments block: show form if post is a commentable Discover post

--- client/blocks/comments/helper.jsx
+++ client/blocks/comments/helper.jsx
@@ -3,19 +3,26 @@
  */
 import * as DiscoverHelper from 'reader/discover/helper';
 
-module.exports = {
-	shouldShowComments( post ) {
-		let showComments = false;
-		const isDiscoverPost = DiscoverHelper.isDiscoverPost( post );
-
-		if ( isDiscoverPost ) {
-			if ( DiscoverHelper.isInternalDiscoverPost( post ) && ! DiscoverHelper.isDiscoverSitePick( post ) ) {
-				showComments = true;
-			}
-		} else if ( ! post.is_jetpack && post.discussion && ( post.discussion.comments_open || post.discussion.comment_count > 0 ) ) {
-			showComments = true;
-		}
-
-		return showComments;
+export function shouldShowComments( post ) {
+	if ( isCommentableDiscoverPost( post ) ) {
+		return true;
 	}
-};
+
+	if ( ! post.is_jetpack && post.discussion && ( post.discussion.comments_open || post.discussion.comment_count > 0 ) ) {
+		return true;
+	}
+
+	return false;
+}
+
+export function isCommentableDiscoverPost( post ) {
+	const isDiscoverPost = DiscoverHelper.isDiscoverPost( post );
+
+	if ( isDiscoverPost ) {
+		if ( DiscoverHelper.isInternalDiscoverPost( post ) && ! DiscoverHelper.isDiscoverSitePick( post ) ) {
+			return true;
+		}
+	}
+
+	return false;
+}
