e31ae1f1ace663c093bbfd37c9ba96de930bb6b7
Marko Andrijasevic <markoandrijasevic@gmail.com>
2016-09-29 00:18:03 +0200

    Add test cases
    * Assert that the rendered input includes form-text-input className
    * Assert that props aside from component's own are passed to the input

--- client/components/forms/form-text-input/test/index.jsx
+++ client/components/forms/form-text-input/test/index.jsx
@@ -13,12 +13,24 @@ describe( '<FormTextInput />', () => {
 		expect( wrapper ).to.have.className( 'is-valid' );
 	} );
 
-	it( 'should not pass props down to the form fields', () => {
+	it( 'should have form-text-input class name', () => {
+		const wrapper = shallow( <FormTextInput /> );
+
+		expect( wrapper ).to.have.className( 'form-text-input' );
+	} );
+
+	it( 'should not pass component\'s own props down to the input', () => {
 		const wrapper = shallow( <FormTextInput isValid isError selectOnFocus /> );
 
 		expect( wrapper ).to.not.have.prop( 'isValid' );
 		expect( wrapper ).to.not.have.prop( 'isError' );
 		expect( wrapper ).to.not.have.prop( 'selectOnFocus' );
+	} );
+
+	it( 'should pass props aside from component\'s own to the input', () => {
+		const wrapper = shallow( <FormTextInput placeholder="test placeholder" /> );
+
+		expect( wrapper ).to.have.prop( 'placeholder' );
 	} );
 
 	it( 'should call select if selectOnFocus is true', () => {
