fd9a5aec9ba145fc5fe3978ce2e11b1a014ea672
budzanowski <bartosz.budzanowski@gmail.com>
2016-09-27 19:18:03 -0700

    Add overlay to search - no styling no logic just render.

--- client/components/search/index.jsx
+++ client/components/search/index.jsx
@@ -52,6 +52,7 @@ const Search = React.createClass( {
 		onSearchOpen: PropTypes.func,
 		onSearchClose: PropTypes.func,
 		analyticsGroup: PropTypes.string,
+		overlayStyling: PropTypes.func,
 		autoFocus: PropTypes.bool,
 		disabled: PropTypes.bool,
 		onKeyDown: PropTypes.func,
@@ -84,6 +85,7 @@ const Search = React.createClass( {
 			onSearchOpen: noop,
 			onSearchClose: noop,
 			onKeyDown: noop,
+			overlayStyling: noop,
 			disableAutocorrect: false,
 			searching: false,
 			isOpen: false,
@@ -149,6 +151,8 @@ const Search = React.createClass( {
 			this.props.onSearch( this.state.keyword );
 		}
 		this.props.onSearchChange( this.state.keyword );
+
+		this.scrollOverlay()
 	},
 
 	componentDidMount: function() {
@@ -159,6 +163,17 @@ const Search = React.createClass( {
 		if ( this.props.autoFocus ) {
 			this.focus();
 		}
+	},
+
+	scrollOverlay: function() {
+		const _this = this;
+		window.requestAnimationFrame(function() {
+			console.log( 'init_tokens: ' + _this.refs.overlay.scrollLeft );
+			console.log( 'init_input: ' + _this.refs.searchInput.scrollLeft );
+			_this.refs.overlay.scrollLeft = _this.refs.searchInput.scrollLeft;
+			console.log( 'after_tokens: ' + _this.refs.overlay.scrollLeft );
+			console.log( 'after_input: ' + _this.refs.searchInput.scrollLeft );
+		} );
 	},
 
 	focus: function() {
@@ -331,6 +346,9 @@ const Search = React.createClass( {
 						maxLength={ this.props.maxLength }
 						{ ...autocorrect }
 					/>
+					<div className="search_text-overlay" ref="overlay">
+						{ this.props.overlayStyling( this.state.keyword ) }
+					</div>
 				</div>
 				{ this.closeButton() }
 			</div>
--- client/my-sites/themes/themes-magic-search-card/index.jsx
+++ client/my-sites/themes/themes-magic-search-card/index.jsx
@@ -65,6 +65,18 @@ const ThemesMagicSearchCard = React.createClass( {
 		this.setState( { searchIsOpen: false } );
 	},
 
+	searchTokens( input ) {
+		const tokens = input.split(/(\s+)/);
+		return (
+			tokens.map( ( token, i ) => {
+				const cls = token.trim() === ''
+				? "search-tokens__white-space"
+				: "search-tokens__token";
+					return <span className={ cls } key={ i }>{ token }</span>; // use shortid for key
+			} )
+		);
+	},
+
 	render() {
 		const isJetpack = this.props.site && this.props.site.jetpack;
 		const isPremiumThemesEnabled = config.isEnabled( 'upgrades/premium-themes' );
@@ -85,6 +97,7 @@ const ThemesMagicSearchCard = React.createClass( {
 				delaySearch={ true }
 				onSearchOpen={ this.onSearchOpen }
 				onSearchClose={ this.onSearchClose }
+				overlayStyling={ this.searchTokens }
 				onBlur={ this.onBlur }
 				fitsContainer={ this.state.isMobile && this.state.searchIsOpen }
 				hideClose={ isMobile() }
