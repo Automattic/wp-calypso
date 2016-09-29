74e56eabcf4e6f1ae0a664bf6bcef5c901ee46eb
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-09-28 21:22:03 -0700

    Add Accordion, Gridicon examples

--- client/devdocs/playground/index.jsx
+++ client/devdocs/playground/index.jsx
@@ -8,19 +8,44 @@ import Playground from 'component-playground';
  * Internal dependencies
  */
 import Main from 'components/main';
+
+import Accordion from 'components/accordion';
 import Button from 'components/button';
+import Gridicon from 'components/gridicon';
 import Notice from 'components/notice';
 
-var componentExample = require( "raw!./../../components/notice/docs/example.txt" );
+const component = {
+	accordion: require( 'raw!./../../components/accordion/docs/example.txt' ),
+	button: require( 'raw!./../../components/button/docs/example.txt' ),
+	gridicon: require( 'raw!./../../components/gridicon/docs/example.txt' ),
+	notice: require( 'raw!./../../components/notice/docs/example.txt' )
+};
 
+const docClass = {
+	notice: Notice,
+	button: Button,
+	accordion: Accordion,
+	gridicon: Gridicon,
+};
 
-let DesignPlayGround = React.createClass( {
+const DesignPlayGround = React.createClass( {
 	displayName: 'DesignPlayGround',
 
 	render() {
 		return (
-			<Main className="design">
-				<Playground codeText={ componentExample } theme="material" scope={ { React: React, Button: Button, Notice: Notice } } noRender={ true } />
+			<Main>
+				<Playground
+					codeText={ component[ this.props.component ] }
+					docClass={ docClass[ this.props.component ] }
+					theme="material"
+					scope={ {
+						React: React,
+						Accordion: Accordion,
+						Button: Button,
+						Gridicon: Gridicon,
+						Notice: Notice
+					} }
+					noRender={ true } />
 			</Main>
 		);
 	}
