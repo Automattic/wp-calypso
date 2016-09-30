50b9cc37bde7bd70b057b84ca01fe7e1699edc85
budzanowski <bartosz.budzanowski@gmail.com>
2016-09-30 18:27:28 +0200

    Render suggestion for empty input.

--- client/components/suggestions/index.jsx
+++ client/components/suggestions/index.jsx
@@ -21,12 +21,26 @@ const Suggestions = React.createClass( {
 	componentDidUpdate: function() {
 	},
 
+	renderWelcomeSign: function() {
+		const taxonomies = Object.keys( this.props.terms );
+
+		return (
+			taxonomies.map( ( taxonomy, i ) => {
+				return <span className="suggestions__taxonomy" key={ i }>{ taxonomy }</span>;
+			} )
+		);
+	},
+
 	render() {
+		let suggestion;
+		if( this.props.input === "" ){
+			suggestion = this.renderWelcomeSign();
+		}
 
 		return (
 			<div className="suggestions">
 				<Card >
-					<div>{ this.props.input }</div>
+					<div>{ suggestion }</div>
 				</Card>
 			</div>
 			);
