0107bf4dfb09b4c27439b00f8614489766cee25d
Ben Lowery <ben.lowery@automattic.com>
2016-09-30 15:14:47 -0400

    Add a comment explaining reader-main

--- client/components/reader-main/index.jsx
+++ client/components/reader-main/index.jsx
@@ -8,6 +8,12 @@ import React from 'react';
  */
 import Main from 'components/main';
 
+/**
+ * A specialization of `Main` that adds a class to the body of the document
+ *
+ * This class is used by pieces of the Reader Refresh (circa Sept 2016) to indicate they want "editorial" styles.
+ * Notably, this overrides the background color of the document and is used as a hook by other parts to override styles.
+ */
 export default class ReaderMain extends React.Component {
 	componentWillMount() {
 		document.querySelector( 'body' ).classList.add( 'is-reader-page' );
