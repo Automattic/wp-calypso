bf471dd9cd6375065a5c481504060874e7062d0a
Andrew Duthie <andrew@andrewduthie.com>
2016-10-03 17:57:21 -0400

    Layout: Include HelloVote notice in layout for site-specific screens

---
+++ client/blocks/hello-vote-notice/index.jsx
@@ -0,0 +1,58 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Notice from 'components/notice';
+import NoticeAction from 'components/notice/notice-action';
+import { getSiteSlug } from 'state/sites/selectors';
+import { isSiteSection, getSelectedSiteId } from 'state/ui/selectors';
+import { getPreference, hasReceivedRemotePreferences } from 'state/preferences/selectors';
+import { savePreference } from 'state/preferences/actions';
+
+function HelloVoteNotice( props ) {
+	const { siteSection, siteSlug, hasDismissed, preferencesReceived } = props;
+	if ( ! siteSection || ! siteSlug || hasDismissed || ! preferencesReceived ) {
+		return null;
+	}
+
+	const { translate, onDismiss } = props;
+	return (
+		<Notice
+			status="is-info"
+			text={ 'Voting is important' /* Yep, that's a placeholder */ }
+			onDismissClick={ onDismiss }>
+			<NoticeAction
+				onClick={ onDismiss }
+				href={ `/settings/general/${ siteSlug }` }>
+				{ translate( 'Opt In' ) }
+			</NoticeAction>
+		</Notice>
+	);
+}
+
+HelloVoteNotice.propTypes = {
+	translate: PropTypes.func,
+	siteSection: PropTypes.bool,
+	siteSlug: PropTypes.string,
+	hasDismissed: PropTypes.bool,
+	preferencesReceived: PropTypes.bool,
+	onDismiss: PropTypes.func
+};
+
+export default connect(
+	( state ) => ( {
+		siteSection: isSiteSection( state ),
+		siteSlug: getSiteSlug( state, getSelectedSiteId( state ) ),
+		hasDismissed: getPreference( state, 'helloVoteNoticeDismissed' ),
+		preferencesReceived: hasReceivedRemotePreferences( state )
+	} ),
+	( dispatch ) => ( {
+		onDismiss: () => dispatch( savePreference( 'helloVoteNoticeDismissed', true ) )
+	} )
+)( localize( HelloVoteNotice ) );
