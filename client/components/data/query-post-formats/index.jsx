7fb5f655aa4618742037dcc69638cf992d1a641c
Marin Atanasov <tyxla@abv.bg>
2016-10-03 11:34:08 +0300

    Components: Add QueryPostFormats query component

---
+++ client/components/data/query-post-formats/index.jsx
@@ -0,0 +1,50 @@
+/**
+ * External dependencies
+ */
+import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { isRequestingPostFormats } from 'state/post-types/selectors';
+import { requestPostFormats } from 'state/post-types/actions';
+
+class QueryPostFormats extends Component {
+	static propTypes = {
+		siteId: PropTypes.number.isRequired,
+		requestingPostFormats: PropTypes.bool,
+		requestPostFormats: PropTypes.func
+	};
+
+	componentWillMount() {
+		this.request( this.props );
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		if ( this.props.siteId !== nextProps.siteId ) {
+			this.request( nextProps );
+		}
+	}
+
+	request( props ) {
+		if ( props.requestingPostFormats ) {
+			return;
+		}
+
+		props.requestPostFormats( props.siteId );
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default connect(
+	( state, ownProps ) => {
+		return {
+			requestingPostFormats: isRequestingPostFormats( state, ownProps.siteId )
+		};
+	},
+	{ requestPostFormats }
+)( QueryPostFormats );
