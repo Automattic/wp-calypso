bb5fda3640bb025abe673758ab7b9fa58dd4aa6f
Kerry Liu <kerryliu@gmail.com>
2016-10-03 15:41:23 -0700

    Plans: navigate to checkout instead of one-click puchase

--- client/blocks/domain-to-plan-nudge/index.jsx
+++ client/blocks/domain-to-plan-nudge/index.jsx
@@ -1,11 +1,9 @@
 /**
  * External dependencies
  */
-import debugFactory from 'debug';
 import React, { PropTypes, Component } from 'react';
 import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
-import { get } from 'lodash';
 import page from 'page';
 
 /**
@@ -17,16 +15,10 @@ import { recordTracksEvent } from 'state/analytics/actions';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { getSite, isCurrentSitePlan } from 'state/sites/selectors';
 import { plansList, PLAN_FREE, PLAN_PERSONAL } from 'lib/plans/constants';
-import { storedCardPayment } from 'lib/store-transactions';
-import { emptyCart } from 'lib/cart-values';
-import { add as addCartItem } from 'lib/cart-values/cart-items';
-import { submitTransaction } from 'lib/upgrades/actions/checkout';
 import PlanPrice from 'my-sites/plan-price';
 import PlanIcon from 'components/plans/plan-icon';
 import Gridicon from 'components/gridicon';
-import { errorNotice, infoNotice, removeNotice } from 'state/notices/actions';
 import { getCurrentUserCurrencyCode } from 'state/current-user/selectors';
-import { recordOrder } from 'lib/analytics/ad-tracking';
 import {
 	getSitePlanRawPrice,
 	getPlanDiscountedRawPrice,
@@ -37,31 +29,21 @@ import QuerySitePlans from 'components/data/query-site-plans';
 import formatCurrency from 'lib/format-currency';
 import { canCurrentUser } from 'state/current-user/selectors';
 
-const debug = debugFactory( 'calypso:domain-to-plan-nudge' );
-
 class DomainToPlanNudge extends Component {
 
 	constructor() {
 		super( ...arguments );
-		this.oneClickUpgrade = this.oneClickUpgrade.bind( this );
-		this.handleTransactionComplete = this.handleTransactionComplete.bind( this );
-		this.state = {
-			isSubmitting: false,
-			noticeId: null
-		};
+		this.personalCheckout = this.personalCheckout.bind( this );
 	}
 
 	static propTypes = {
 		discountedRawPrice: PropTypes.number,
-		errorNotice: PropTypes.func,
 		hasFreePlan: PropTypes.bool,
-		infoNotice: PropTypes.func,
 		productId: PropTypes.number,
 		productSlug: PropTypes.string,
 		rawDiscount: PropTypes.number,
 		rawPrice: PropTypes.number,
 		recordTracksEvent: PropTypes.func,
-		removeNotice: PropTypes.func,
 		site: PropTypes.object,
 		siteId: PropTypes.number,
 		sitePlans: PropTypes.object,
@@ -86,103 +68,24 @@ class DomainToPlanNudge extends Component {
 			hasFreePlan;      //has a free wpcom plan
 	}
 
-	getCartItem() {
-		const {
-			productSlug,
-			productId
-		} = this.props;
+	personalCheckout() {
+		const { siteId } = this.props;
 
-		return {
-			product_id: productId,
-			product_slug: productSlug,
-			free_trial: false,
-			is_domain_registration: false
-		};
-	}
-
-	handleTransactionComplete( error, data ) {
-		const { siteId, translate, userCurrency, rawPrice, discountedRawPrice } = this.props;
-
-		const price = discountedRawPrice || rawPrice;
-
-		debug( 'transaction complete', error, data );
-		// see transaction-steps-mixin for matching checkout analytics
-		if ( error ) {
-			if ( this.state.noticeId ) {
-				this.props.removeNotice( this.state.noticeId );
-			}
-			const noticeAction = this.props.errorNotice(
-				translate( 'There was a problem completing the purchase. Please try again.' )
-			);
-			this.setState( {
-				isSubmitting: false,
-				noticeId: get( noticeAction, 'notice.noticeId' )
-			} );
-			this.props.recordTracksEvent(
-				'calypso_checkout_payment_error',
-				{ reason: error.message }
-			);
-			return;
-		}
-
-		const receiptId = data.receipt_id;
-
-		// ad tracking
-		const product = { ...this.getCartItem(), currency: userCurrency, cost: price, volume: 1 };
-		const cart = { products: [ product ], total_cost: price, currency: userCurrency };
-		recordOrder( cart, receiptId );
-
-		// tracks
-		this.props.recordTracksEvent( 'calypso_checkout_payment_success', {
-			coupon_code: '',
-			currency: userCurrency,
-			payment_method: storedCardPayment().paymentMethod,
-			total_cost: price
+		this.props.recordTracksEvent( 'calypso_upgrade_nudge_cta_click', {
+			cta_name: 'domain_to_personal_nudge',
+			cta_feature: 'no-adverts',
+			cta_size: 'banner'
 		} );
 
-		this.props.recordTracksEvent( 'calypso_checkout_product_purchase', this.getCartItem() );
-
-		// redirect to thank you
-		page( `/checkout/thank-you/${ siteId }/${ receiptId }` );
-	}
-
-	oneClickUpgrade() {
-		const { siteId, storedCard, translate } = this.props;
-		const newPayment = storedCardPayment( storedCard );
-		const transaction = {
-			payment: newPayment
-		};
-
-		// set this to temporary so we don't clear the existing user cart on the server
-		let cart = emptyCart( siteId, { temporary: true } );
-
-		const personalCartItem = this.getCartItem();
-		cart = addCartItem( personalCartItem )( cart );
-
-		debug( 'purchasing with', cart, transaction );
-
-		if ( this.state.noticeId ) {
-			this.props.removeNotice( this.state.noticeId );
-		}
-		const noticeAction = this.props.infoNotice( translate( 'Submitting payment' ) );
-		this.setState( {
-			isSubmitting: true,
-			noticeId: get( noticeAction, 'notice.noticeId' )
-		} );
-
-		submitTransaction( { cart, transaction }, this.handleTransactionComplete );
+		page( `/checkout/${ siteId }/personal` );
 	}
 
 	renderCard() {
-		const { isSubmitting } = this.state;
-
 		const {
 			discountedRawPrice,
 			productSlug,
 			rawDiscount,
 			rawPrice,
-			siteId,
-			storedCard,
 			translate,
 			userCurrency
 		} = this.props;
@@ -229,16 +132,6 @@ class DomainToPlanNudge extends Component {
 										icon="checkmark"
 										size={ 24 }
 									/>
-									{ translate( 'Upload up to 3GB of photos and documents' ) }
-								</div>
-							</li>
-							<li>
-								<div className="domain-to-plan-nudge__header-features-item">
-									<Gridicon
-										className="domain-to-plan-nudge__header-features-item-checkmark"
-										icon="checkmark"
-										size={ 24 }
-									/>
 									{ translate( 'Bundled with your domain for the best value!' ) }
 								</div>
 							</li>
@@ -274,28 +167,15 @@ class DomainToPlanNudge extends Component {
 					</div>
 					<div className="domain-to-plan-nudge__upgrade-group">
 						<Button
-							onClick={ this.oneClickUpgrade }
-							disabled={ ! storedCard || isSubmitting }
+							onClick={ this.personalCheckout }
 							primary
 						>
-							{ isSubmitting
-								? translate( 'Completing your purchase' )
-								: translate( 'Upgrade Now for %s', {
+							{
+								translate( 'Upgrade Now for %s', {
 									args: formatCurrency( discountedRawPrice || rawPrice, userCurrency )
 								} )
 							}
 						</Button>
-						<div className="domain-to-plan-nudge__credit-card-info">
-							<a
-								className="domain-to-plan-nudge__credit-card-info-link"
-								href={ `/checkout/${ siteId }/personal` }>
-								{
-									translate( 'Using credit card ****%s', {
-										args: storedCard ? storedCard.card : 'xxxx'
-									} )
-								}
-							</a>
-						</div>
 					</div>
 				</div>
 			</DismissibleCard>
@@ -339,9 +219,6 @@ export default connect(
 		};
 	},
 	{
-		infoNotice,
-		errorNotice,
-		recordTracksEvent,
-		removeNotice
+		recordTracksEvent
 	}
 )( localize( DomainToPlanNudge ) );
