b36528f24f10bc6fb1f8eb5de86a2ceff7802cc1
Konstantin Obenland <obenland@automattic.com>
2016-10-03 08:51:09 -0700

    Reduxifyed States List (#8202)
    * Reduxifyed States List
    
    First pass at a reduxified states list, introducing actions, reducers,
    selectors, and corresponding tests.
    
    See #7922.
    
    * Fix ESlint warnings.
    
    See
    https://github.com/Automattic/wp-calypso/commit/2cedce86d1250a54fe011f14
    38415265b5147655#commitcomment-19177600
    
    * Use test helpers to avoid having to specify lifecycle methods.
    
    See
    https://github.com/Automattic/wp-calypso/commit/2cedce86d1250a54fe011f14
    38415265b5147655#commitcomment-19177527,
    https://github.com/Automattic/wp-calypso/commit/2cedce86d1250a54fe011f14
    38415265b5147655#commitcomment-19177560
    
    * Make it easier to add/remove tests in the future.
    
    Persists passing request and uses a variant to for failing requests.
    
    See
    https://github.com/Automattic/wp-calypso/commit/2cedce86d1250a54fe011f14
    38415265b5147655#commitcomment-19177543
    
    * Key `isFetching` state by countryCode.
    
    Allows us to block duplicate requests for the same country code, but
    allow parallel requests for different country codes.
    
    See
    https://github.com/Automattic/wp-calypso/commit/2cedce86d1250a54fe011f14
    38415265b5147655#commitcomment-19177670
    
    * Don't indent, ESlint is not a fan.
    
    * More accurate JSON schema for statesList.
    
    Props @gwwar.
    See
    https://github.com/Automattic/wp-calypso/pull/8202#commitcomment-1917735
    2,
    https://github.com/Automattic/wp-calypso/pull/8202#discussion_r80551903
    
    * Update selectors to work with current shape of state.
    
    `is fetching` now is keyed by country code, and `statesList` is a
    proper of the `statesList` reducer.
    
    See
    https://github.com/Automattic/wp-calypso/commit/2cedce86d1250a54fe011f14
    38415265b5147655#commitcomment-19177379
    
    * Use `items` rather than `statesList`.
    
    `items` is conventionally used to contain the entities of a particular
    subtree (e.g. `state.statesList.items`).
    
    See
    https://github.com/Automattic/wp-calypso/commit/2cedce86d1250a54fe011f14
    38415265b5147655#commitcomment-19177379
    
    * Make default state an empty object.
    
    See 4bbf4c3f21b200cb9862f5b3af7180c932e3da3e
    See
    https://github.com/Automattic/wp-calypso/pull/8202#discussion_r80587487
    
    * Move to `countryStates` namespace.
    
    The `-list` suffix is a convention that was used in emitter object
    stores, but most directories in `client/state` don't use it.
    
    See
    https://github.com/Automattic/wp-calypso/pull/8202#discussion_r80548603
    
    * Use lowercase country codes throughout.
    
    Avoids a situation where we could request both `us` and `US` and they'd
    be tracked independently, since object keys are case sensitive.
    
    See
    https://github.com/Automattic/wp-calypso/commit/2cedce86d1250a54fe011f14
    38415265b5147655#commitcomment-19177461
    
    * Use separate action type to denote successful requests.
    
    Allows us to differentiate between successful API requests and
    receiving data. It’s possible to receive data from a different data
    source, which would make that action ambiguous.
    
    See
    https://github.com/Automattic/wp-calypso/pull/8202#discussion_r81200070
    
    * Add countryStates reducer to global list.
    
    See
    https://github.com/Automattic/wp-calypso/pull/8202#issuecomment-25062281
    5
    
    * JSdocs and tests for country states selectors.
    
    See
    https://github.com/Automattic/wp-calypso/pull/8202#discussion_r81258077
    
    * Introduce query component for country states.

---
+++ client/state/country-states/actions.js
@@ -0,0 +1,45 @@
+/**
+ * Internal dependencies
+ */
+import wpcom from 'lib/wp';
+import {
+	COUNTRY_STATES_RECEIVE,
+	COUNTRY_STATES_REQUEST,
+	COUNTRY_STATES_REQUEST_FAILURE,
+	COUNTRY_STATES_REQUEST_SUCCESS,
+} from 'state/action-types';
+
+export function receiveCountryStates( countryStates, countryCode ) {
+	countryCode = countryCode.toLowerCase();
+
+	return {
+		type: COUNTRY_STATES_RECEIVE,
+		countryCode,
+		countryStates,
+	};
+}
+
+export function requestCountryStates( countryCode ) {
+	countryCode = countryCode.toLowerCase();
+
+	return ( dispatch ) => {
+		dispatch( {
+			type: COUNTRY_STATES_REQUEST,
+			countryCode,
+		} );
+
+		return wpcom.undocumented().getDomainRegistrationSupportedStates( countryCode )
+			.then( ( countryStates ) => {
+				dispatch( receiveCountryStates( countryStates, countryCode ) );
+				dispatch( {
+					type: COUNTRY_STATES_REQUEST_SUCCESS,
+					countryCode,
+				} );
+			} )
+			.catch( ( error ) => dispatch( {
+				type: COUNTRY_STATES_REQUEST_FAILURE,
+				countryCode,
+				error,
+			} ) );
+	};
+}
