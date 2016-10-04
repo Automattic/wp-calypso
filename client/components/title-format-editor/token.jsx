4690df944dcd2c3e7e903d63a25f39a48b04be81
Dennis Snell <dennis.snell@automattic.com>
2016-10-04 16:24:57 -0700

    SEO: Adds `x` to tokens to better indicate they can be deleted
    Previously, the tokens in the title format editor simply turned red when
    hovering over them with the mouse. This behavior was to indicate that by
    clicking on the token it would be removed.
    
    Because there are issues changing the contents of those tokens inside of
    the draft-js editor, we did not provide the token from the get-go.
    
    Now, I have added in a prototype (poorly-styled) `x` on hover and have
    set the editor to _read-only_ during that hover state. By doing this we
    can take control of the token without causing the cursor glitches there
    when the content inside of the token is expanded.

--- client/components/title-format-editor/token.jsx
+++ client/components/title-format-editor/token.jsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 
-export const Token = props => {
-	const { onClick } = props;
+import Gridicon from 'components/gridicon';
 
-	// please ignore the formatting below
-	// if we allow spaces it will mess up
-	// the way the component renders in
-	// the draft-js editor
-	return (
-		<span
-			className="title-format-editor__token"
-			onClick={ onClick( props.entityKey ) }
-		>{ props.children }</span>
-	);
-};
+export class Token extends Component {
+	static propTypes = {
+		onClick: PropTypes.func,
+		onMouseEnter: PropTypes.func,
+		onMouseLeave: PropTypes.func,
+	};
+
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			isActive: false,
+		};
+	}
+
+	activate = () => this.setState( { isActive: true }, this.props.onMouseEnter );
+	deactivate = () => this.setState( { isActive: false }, this.props.onMouseLeave );
+
+	render() {
+		const {
+			children,
+			entityKey,
+			onClick,
+		} = this.props;
+		const { isActive } = this.state;
+
+		// please ignore the formatting below
+		// if we allow spaces it will mess up
+		// the way the component renders in
+		// the draft-js editor
+		return (
+			<span
+				className="title-format-editor__token"
+				onClick={ onClick( entityKey ) }
+				onMouseEnter={ this.activate }
+				onMouseLeave={ this.deactivate }
+			>{ children }{ isActive &&
+				<Gridicon
+					className="title-format-editor__token-close"
+					icon="cross-small"
+				/>
+			}</span>
+		);
+	}
+}
 
 export default Token;
