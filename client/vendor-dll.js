33044692bf9e3392f2ed5b859d40d2d668940fa6
Ben Lowery <ben.lowery@automattic.com>
2016-10-04 10:51:31 -0400

    better comments for the vendor dll config

--- client/vendor-dll.js
+++ client/vendor-dll.js
@@ -1,8 +1,11 @@
-// things we'd like to live in our vendor dll
+/**
+ * Dependencies we would like to like in the vendor chunk. These can be external or internal deps, but either way,
+ * they should be things that the majority of the application uses, and things that don't change very often.
+ */
 require( 'classnames' );
 require( 'debug' );
 require( 'i18n-calypso' );
-// lodash would go here, but we leave it out because we use tree shaking to pull parts of it
+// lodash would go here, but we leave it out because we use tree shaking to only require the bits that we use.
 require( 'moment' );
 require( 'page' );
 require( 'react-redux' );
