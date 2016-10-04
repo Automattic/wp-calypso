cc2789f37e1a550c85ed959ace30746d51a503fd
Ben Lowery <ben.lowery@automattic.com>
2016-10-04 10:48:33 -0400

    remove redundant jed require. i18n-calypso pulls it in.

--- client/vendor-dll.js
+++ client/vendor-dll.js
@@ -2,7 +2,6 @@
 require( 'classnames' );
 require( 'debug' );
 require( 'i18n-calypso' );
-require( 'jed' );
 // lodash would go here, but we leave it out because we use tree shaking to pull parts of it
 require( 'moment' );
 require( 'page' );
