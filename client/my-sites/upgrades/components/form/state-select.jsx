8e0e9f24617bf148aa0019db4f0c50729ffa3457
Konstantin Obenland <obenland@gmx.de>
2016-10-04 15:06:32 -0700

    Second pass at wiring up reduxified country states.
    * Uses Redux based Google Analytics.
    * Moves `QueryCountryStates` inline.
    * Removes `lib/states-list`.

--- client/my-sites/upgrades/components/form/state-select.jsx
+++ client/my-sites/upgrades/components/form/state-select.jsx
@@ -11,20 +11,20 @@ import ReactDom from 'react-dom';
 /**
  * Internal dependencies
  */
-import analytics from 'lib/analytics';
 import FormLabel from 'components/forms/form-label';
 import FormSelect from 'components/forms/form-select';
 import FormInputValidation from 'components/forms/form-input-validation';
 import { getCountryStates } from 'state/country-states/selectors';
 import Input from './input';
 import QueryCountryStates from 'components/data/query-country-states';
+import { recordGoogleEvent } from 'state/analytics/actions';
 import scrollIntoViewport from 'lib/scroll-into-viewport';
 
 class StateSelect extends Component {
 
-	recordStateSelectClick() {
-		if ( this.props.eventFormName ) {
-			analytics.ga.recordEvent( 'Upgrades', `Clicked ${ this.props.eventFormName } State Select` );
+	recordStateSelectClick( eventFormName ) {
+		if ( eventFormName ) {
+			recordGoogleEvent( 'Upgrades', `Clicked ${ eventFormName } State Select` );
 		}
 	}
 
@@ -38,47 +38,32 @@ class StateSelect extends Component {
 		}
 	}
 
-	query() {
-		return this.props.countryCode && <QueryCountryStates countryCode={ this.props.countryCode } />;
-	}
-
 	render() {
 		const classes = classNames( this.props.additionalClasses, 'state' );
 
-		this.query();
-
-		if ( isEmpty( this.props.countryStates ) ) {
-			return (
-				<Input ref="state" { ...this.props } />
-			);
-		}
-
-		this.props.countryStates.push( { code: '', name: this.translate( 'Select State' ) } );
-
 		return (
-			<div className={ classes }>
-				<div>
-					<FormLabel htmlFor={ this.props.name }>{ this.props.label }</FormLabel>
-					<FormSelect
-						ref="input"
-						name={ this.props.name }
-						value={ this.props.value }
-						disabled={ this.props.disabled }
-						onChange={ this.props.onChange }
-						onClick={ this.recordStateSelectClick }
-						isError={ this.props.isError } >
-						{ this.props.countryStates.map( ( state ) => {
-							let disabled;
+			<div>
+				{ this.props.countryCode && <QueryCountryStates countryCode={ this.props.countryCode } /> }
+				{ isEmpty( this.props.countryStates )
+					? <Input ref="input" { ...this.props } />
+					: <div className={ classes }>
+						<FormLabel htmlFor={ this.props.name }>{ this.props.label }</FormLabel>
+						<FormSelect
+							ref="input"
+							name={ this.props.name }
+							value={ this.props.value }
+							disabled={ this.props.disabled }
+							onChange={ this.props.onChange }
+							onClick={ this.recordStateSelectClick.bind( this, this.props.eventFormName ) }
+							isError={ this.props.isError } >
 
-							if ( ! state.code ) {
-								state.code = '--';
-								disabled = 'disabled';
-							}
-
-							return <option key={ state.code } value={ state.code } disabled={ disabled }>{ state.name }</option>;
-						} ) }
-					</FormSelect>
-				</div>
+							<option key="--" value="--" disabled="disabled">{ this.props.translate( 'Select State' ) }</option>
+							{ this.props.countryStates.map( ( state ) =>
+								<option key={ state.code } value={ state.code }>{ state.name }</option>
+							) }
+						</FormSelect>
+					</div>
+				}
 				{ this.props.errorMessage && <FormInputValidation text={ this.props.errorMessage } isError /> }
 			</div>
 		);
@@ -89,7 +74,7 @@ StateSelect.propTypes = {
 	additionalClasses: PropTypes.string,
 	countryCode: PropTypes.string,
 	countryStates: PropTypes.array,
-	diabled: PropTypes.bool,
+	disabled: PropTypes.bool,
 	errorMessage: PropTypes.string,
 	eventFormName: PropTypes.string,
 	isError: PropTypes.bool,
