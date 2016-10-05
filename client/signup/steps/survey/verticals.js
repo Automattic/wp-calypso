f1f7fdf6c5cf4f3825ae0853d06dcb59e586b706
Marco Aurélio <thecoreh@gmail.com>
2016-10-05 03:48:15 -0300

    Signup: Add A/B test for Verticals v2 (#7700)
    AB test the new design for the Verticals step in Signup.

--- client/signup/steps/survey/verticals.js
+++ client/signup/steps/survey/verticals.js
@@ -4,8 +4,9 @@
 import shuffle from 'lodash/shuffle';
 import find from 'lodash/find';
 import { translate } from 'i18n-calypso';
+import { abtest } from 'lib/abtest';
 
-const verticals = [
+const verticalsV1 = [
 	{ value: 'a8c.1', label: translate( 'Arts & Entertainment' ), icon: 'video-camera', stepTwo: [
 		{ value: 'a8c.1', label: translate( 'General Arts & Entertainment' ), isGeneral: true },
 		{ value: 'a8c.1.3.1', label: translate( 'Creative Arts & Design' ) },
@@ -73,6 +74,26 @@ const verticals = [
 	] },
 ];
 
+const verticalsV2 = [
+	{ value: 'a8c.14.7', label: translate( 'Wedding' ) },
+	{ value: 'a8c.20', label: translate( 'Travel' ) },
+	{ value: 'a8c.1.6', label: translate( 'Music' ) },
+	{ value: 'a8c.8', label: translate( 'Food / Drink' ) },
+	{ value: 'a8c.9.23', label: translate( 'Photography' ) },
+	{ value: 'a8c.3', label: translate( 'Professional Services' ) },
+	{ value: 'a8c.6', label: translate( 'Family / Parenting' ) },
+	{ value: 'a8c.19', label: translate( 'Technology' ) },
+	{ value: 'a8c.0.1', label: translate( 'Personal / Random Thoughts' ) },
+	{ value: 'a8c.1', label: translate( 'Art' ) },
+	{ value: 'a8c.5', label: translate( 'Education' ) },
+	{ value: 'a8c.18', label: translate( 'Fashion / Beauty' ) },
+	{ value: 'a8c.0.2', label: translate( 'Lifestyle / Inspiration' ) },
+	{ value: 'a8c.7', label: translate( 'Health / Wellness' ) },
+	{ value: 'a8c.23', label: translate( 'Religion / Spirituality' ) },
+	{ value: 'a8c.17', label: translate( 'Sports / Recreation' ) },
+	{ value: 'a8c.11', label: translate( 'Government / Politics' ) },
+];
+
 /**
  * Shuffle a multi-dimensional array of verticals, but put the General vertical last.
  *
@@ -92,10 +113,14 @@ function shuffleVerticals( elements ) {
 		newVerticals.push( general );
 	}
 	return newVerticals;
-};
+}
 
 export default {
 	get() {
-		return shuffleVerticals( verticals );
+		switch ( abtest( 'signupSurveyStep' ) ) {
+			case 'surveyStepV1': return shuffleVerticals( verticalsV1 );
+			case 'surveyStepV2': return shuffleVerticals( verticalsV2 );
+			default: throw new Error( 'Unknown variation' );
+		}
 	}
-}
+};
