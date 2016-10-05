f8e1220815d91fc8a2988834eeea3d9db2a835b5
Andrew Duthie <andrew@andrewduthie.com>
2016-10-05 14:10:55 -0400

    Editor: Replace sourcecode entities on load from textarea value

--- client/components/tinymce/plugins/wpcom-sourcecode/plugin.js
+++ client/components/tinymce/plugins/wpcom-sourcecode/plugin.js
@@ -8,12 +8,22 @@ import tinymce from 'tinymce/tinymce';
  */
 const REGEXP_CODE_SHORTCODE = new RegExp( '(?:<p>\\s*)?(?:<pre>\\s*)?(\\[(code|sourcecode)[^\\]]*\\][\\s\\S]*?\\[\\/\\2\\])(?:\\s*<\\/pre>)?(?:\\s*<\\/p>)?', 'gi' );
 
-export function wrapPre( content, initial ) {
+export function wrapPre( { content, initial, load } ) {
 	return content = content.replace( REGEXP_CODE_SHORTCODE, function( match, shortcode ) {
 		shortcode = shortcode.replace( /\r/, '' );
 		shortcode = shortcode.replace( /<br ?\/?>\n?/g, '\n' ).replace( /<\/?p( [^>]*)?>\n?/g, '\n' );
 
-		if ( ! initial ) {
+		// Ensure shortcode contents hit TinyMCE with characters escaped, else
+		// they'll be rendered as DOM nodes.
+		//
+		// See: https://github.com/tinymce/tinymce/blob/a9561a4/js/tinymce/classes/Editor.js#L1619
+		//
+		// Event arguments:
+		//  - `initial`: When calling `setContent` on a TinyMCE component
+		//    instance, value is assumed to already be escaped
+		//  - `load`: When TinyMCE loads, it sets content from the underlying
+		//    textarea, whose value is decoded and needs entities replaced
+		if ( ! initial || load ) {
 			shortcode = shortcode.replace( /&/g, '&amp;' ).replace( /</g, '&lt;' ).replace( />/g, '&gt;' );
 		}
 
@@ -21,7 +31,7 @@ export function wrapPre( content, initial ) {
 	} );
 }
 
-export function unwrapPre( content ) {
+export function unwrapPre( { content } ) {
 	if ( ! content || content.indexOf( '[' ) === -1 ) {
 		return content;
 	}
@@ -39,7 +49,7 @@ function sourcecode( editor ) {
 			return;
 		}
 
-		event.content = wrapPre( event.content, event.initial );
+		event.content = wrapPre( event );
 	} );
 
 	editor.on( 'GetContent', ( event ) => {
@@ -47,7 +57,7 @@ function sourcecode( editor ) {
 			return;
 		}
 
-		event.content = unwrapPre( event.content );
+		event.content = unwrapPre( event );
 	} );
 
 	editor.on( 'PostProcess', ( event ) => {
@@ -55,7 +65,7 @@ function sourcecode( editor ) {
 			return;
 		}
 
-		event.content = unwrapPre( event.content );
+		event.content = unwrapPre( event );
 	} );
 }
 
--- client/components/tinymce/plugins/wpcom-sourcecode/test/plugin.js
+++ client/components/tinymce/plugins/wpcom-sourcecode/test/plugin.js
@@ -21,30 +21,64 @@ describe( 'wpcom-sourcecode', () => {
 
 	describe( '#wrapPre()', () => {
 		it( 'should wrap a code shortcode', () => {
-			const wrapped = wrapPre( '[code lang="javascript"]var foo;[/code]' );
-			expect( wrapped ).to.equal( '<pre>[code lang="javascript"]var foo;[/code]</pre>' );
+			const wrapped = wrapPre( {
+				content: '[code lang="javascript"]const noop = () => {};[/code]'
+			} );
+
+			expect( wrapped ).to.equal( '<pre>[code lang="javascript"]const noop = () =&gt; {};[/code]</pre>' );
+		} );
+
+		it( 'should not encode entities when initial non-load', () => {
+			const wrapped = wrapPre( {
+				content: '[code lang="javascript"]const noop = () =&gt; {};[/code]',
+				initial: true
+			} );
+
+			expect( wrapped ).to.equal( '<pre>[code lang="javascript"]const noop = () =&gt; {};[/code]</pre>' );
+		} );
+
+		it( 'should encode entities when initial load', () => {
+			const wrapped = wrapPre( {
+				content: '[code lang="javascript"]const noop = () => {};[/code]',
+				initial: true,
+				load: true
+			} );
+
+			expect( wrapped ).to.equal( '<pre>[code lang="javascript"]const noop = () =&gt; {};[/code]</pre>' );
 		} );
 
 		it( 'should wrap a sourcecode shortcode', () => {
-			const wrapped = wrapPre( '[sourcecode lang="javascript"]var foo;[/sourcecode]' );
-			expect( wrapped ).to.equal( '<pre>[sourcecode lang="javascript"]var foo;[/sourcecode]</pre>' );
+			const wrapped = wrapPre( {
+				content: '[sourcecode lang="javascript"]const noop = () => {};[/sourcecode]'
+			} );
+
+			expect( wrapped ).to.equal( '<pre>[sourcecode lang="javascript"]const noop = () =&gt; {};[/sourcecode]</pre>' );
 		} );
 	} );
 
 	describe( '#unwrapPre()', () => {
 		it( 'should unwrap a code shortcode', () => {
-			const unwrapped = unwrapPre( '<pre>[code lang="javascript"]var foo;[/code]</pre>' );
-			expect( unwrapped ).to.equal( '<p>[code lang="javascript"]var foo;[/code]</p>' );
+			const unwrapped = unwrapPre( {
+				content: '<pre>[code lang="javascript"]const noop = () =&gt; {};[/code]</pre>'
+			} );
+
+			expect( unwrapped ).to.equal( '<p>[code lang="javascript"]const noop = () => {};[/code]</p>' );
 		} );
 
 		it( 'should unwrap a sourcecode shortcode', () => {
-			const unwrapped = unwrapPre( '<pre>[sourcecode lang="javascript"]var foo;[/sourcecode]</pre>' );
-			expect( unwrapped ).to.equal( '<p>[sourcecode lang="javascript"]var foo;[/sourcecode]</p>' );
+			const unwrapped = unwrapPre( {
+				content: '<pre>[sourcecode lang="javascript"]const noop = () =&gt; {};[/sourcecode]</pre>'
+			} );
+
+			expect( unwrapped ).to.equal( '<p>[sourcecode lang="javascript"]const noop = () => {};[/sourcecode]</p>' );
 		} );
 
 		it( 'should gracefully handle surrounding content', () => {
-			const unwrapped = unwrapPre( '<p>foo</p><p><pre>[sourcecode lang="javascript"]var foo;[/sourcecode]</pre></p><p>bar</p>' );
-			expect( unwrapped ).to.equal( '<p>foo</p><p>[sourcecode lang="javascript"]var foo;[/sourcecode]</p><p>bar</p>' );
+			const unwrapped = unwrapPre( {
+				content: '<p>foo</p><p><pre>[sourcecode lang="javascript"]const noop = () =&gt; {};[/sourcecode]</pre></p><p>bar</p>'
+			} );
+
+			expect( unwrapped ).to.equal( '<p>foo</p><p>[sourcecode lang="javascript"]const noop = () => {};[/sourcecode]</p><p>bar</p>' );
 		} );
 	} );
 } );
