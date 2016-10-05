787253dd724e8f81c62723323e72523dbef212be
Steve Seear <steve.seear@gmail.com>
2016-10-05 11:15:09 +0100

    Revert "Revert "Search styling overlay""

--- client/my-sites/themes/theme-filters.js
+++ client/my-sites/themes/theme-filters.js
@@ -230,7 +230,7 @@ export function getFilter( term ) {
  * @param {string} filter - filter in form taxonomy:term
  * @return {boolean} true if filter pair is valid
  */
-function filterIsValid( filter ) {
+export function filterIsValid( filter ) {
 	return getTermTable()[ getTerm( filter ) ] === getTaxonomy( filter );
 }
 
