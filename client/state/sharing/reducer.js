84a13983205b1b8f4e59063df03802175c3b9a99
Konstantin Obenland <obenland@gmx.de>
2016-10-06 11:06:36 -0700

    Make services equivalent to publicize.
    (Even though publicize should be part of services)

--- client/state/sharing/reducer.js
+++ client/state/sharing/reducer.js
@@ -7,7 +7,7 @@ import { combineReducers } from 'redux';
  * Internal dependencies
  */
 import publicize from './publicize/reducer';
-import services from './keyring/services/reducer';
+import services from './services/reducer';
 
 export default combineReducers( {
 	publicize,
