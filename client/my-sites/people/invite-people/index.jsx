578fee33d1d0e65c5b5cf23fb0393dcc9e7b0339
Javier Villanueva <javier@medialounge.co.uk>
2016-10-06 15:55:26 +0100

    Change fieldset to div to avoid overflowing parent

--- client/my-sites/people/invite-people/index.jsx
+++ client/my-sites/people/invite-people/index.jsx
@@ -286,7 +286,12 @@ const InvitePeople = React.createClass( {
 
 	renderRoleExplanation() {
 		return (
-			<a target="_blank" rel="noopener noreferrer" href="http://en.support.wordpress.com/user-roles/" onClick={ this.onClickRoleExplanation }>
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="http://en.support.wordpress.com/user-roles/"
+				onClick={ this.onClickRoleExplanation }
+			>
 				{ this.translate( 'Learn more about roles' ) }
 			</a>
 		);
@@ -315,7 +320,7 @@ const InvitePeople = React.createClass( {
 				<Card>
 					<EmailVerificationGate>
 						<form onSubmit={ this.submitForm } >
-							<FormFieldset>
+							<div role="group" className="invite-people__token-field-wrapper">
 								<FormLabel>{ this.translate( 'Usernames or Emails' ) }</FormLabel>
 								<TokenField
 									isBorderless
@@ -326,14 +331,14 @@ const InvitePeople = React.createClass( {
 									value={ this.getTokensWithStatus() }
 									onChange={ this.onTokensChange }
 									onFocus={ this.onFocusTokenField }
-									disabled={ this.state.sendingInvites }/>
+									disabled={ this.state.sendingInvites } />
 								<FormSettingExplanation>
 									{ this.translate(
 										'Invite up to 10 email addresses and/or WordPress.com usernames. ' +
 										'Those needing a username will be sent instructions on how to create one.'
 									) }
 								</FormSettingExplanation>
-							</FormFieldset>
+							</div>
 
 							<RoleSelect
 								id="role"
