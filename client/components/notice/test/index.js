c90281d4fbf3de587bac0b19b7b91ff991e0f7b7
Joen Asmussen <joen@automattic.com>
2016-10-06 13:23:54 +0200

    Notice unit test: Cleanups and extra class tests.

--- client/components/notice/test/index.js
+++ client/components/notice/test/index.js
@@ -9,49 +9,42 @@ import { identity } from 'lodash';
 /**
  * Internal dependencies
  */
-import { UnwrappedNotice } from '../index';
+import { Notice } from '../index';
 
 describe( 'index', function() {
-	require( 'test/helpers/use-fake-dom' )();
 	it( 'should output the component', function() {
-		const wrapper = shallow( <UnwrappedNotice translate={ identity } /> );
+		const wrapper = shallow( <Notice translate={ identity } /> );
 		assert.equal( true, wrapper.find( '.notice' ).length );
 	} );
 
 	it( 'should have dismiss button when showDismiss passed as true', function() {
-		const wrapper = shallow( <UnwrappedNotice showDismiss={ true } translate={ identity } /> );
+		const wrapper = shallow( <Notice showDismiss={ true } translate={ identity } /> );
 		assert.equal( true, wrapper.find( '.is-dismissable' ).length );
 	} );
 
 	it( 'should have compact look when isCompact passed as true', function() {
-		const wrapper = shallow( <UnwrappedNotice isCompact={ true } translate={ identity } /> );
+		const wrapper = shallow( <Notice isCompact={ true } translate={ identity } /> );
 		assert.equal( true, wrapper.find( '.is-compact' ).length );
 	} );
 
-	it( 'should have proper class and icon for is-info status parameter', function() {
-		const wrapper = shallow( <UnwrappedNotice status="is-info" translate={ identity } /> );
+	it( 'should have proper class for is-info status parameter', function() {
+		const wrapper = shallow( <Notice status="is-info" translate={ identity } /> );
 		assert.equal( true, wrapper.find( '.is-info' ).length );
+	} );
 
-		console.log( wrapper.children().nodes );
-		
-		/*expect( wrapper.children().contains( [
-			<svg />,
-		] ) ).to.equal( true ); */
-		//	<svg className="gridicon gridicons-info notice__icon" height="24" width="24" />,
-
-
-		//assert.equal( true, wrapper.find( 'svg' ).length );
-		//assert.equal( true, wrapper.children().find( '.gridicons-info' ).length );
-		//console.log( wrapper.children() );
-
-/*		const wrapper = shallow( <UnwrappedNotice status="is-success" translate={ identity } /> );
+	it( 'should have proper class for is-success status parameter', function() {
+		const wrapper = shallow( <Notice status="is-success" translate={ identity } /> );
 		assert.equal( true, wrapper.find( '.is-success' ).length );
+	} );
 
-		const wrapper = shallow( <UnwrappedNotice status="is-error" translate={ identity } /> );
+	it( 'should have proper class for is-error status parameter', function() {
+		const wrapper = shallow( <Notice status="is-error" translate={ identity } /> );
 		assert.equal( true, wrapper.find( '.is-error' ).length );
+	} );
 
-		const wrapper = shallow( <UnwrappedNotice status="is-warning" translate={ identity } /> );
-		assert.equal( true, wrapper.find( '.is-warning' ).length ); */
+	it( 'should have proper class for is-warning status parameter', function() {
+		const wrapper = shallow( <Notice status="is-warning" translate={ identity } /> );
+		assert.equal( true, wrapper.find( '.is-warning' ).length );
 	} );
 
 } );
