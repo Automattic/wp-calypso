2324b0e66eebe0b1dd55c7b68e801c4c6503cce2
Andrew Duthie <andrew@andrewduthie.com>
2016-10-06 10:46:32 -0400

    Components: Add AsyncLoad component for bundle loading

---
+++ client/components/async-load/index.jsx
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { omit } from 'lodash';
+
+export default class AsyncLoad extends Component {
+	static propTypes = {
+		require: PropTypes.func.isRequired,
+		placeholder: PropTypes.node
+	};
+
+	constructor() {
+		super( ...arguments );
+
+		this.state = {
+			component: null
+		};
+	}
+
+	componentWillMount() {
+		this.props.require( ( component ) => {
+			this.setState( { component } );
+		} );
+	}
+
+	render() {
+		if ( this.state.component ) {
+			const props = omit( this.props, [ 'require', 'placeholder' ] );
+			return <this.state.component { ...props } />;
+		}
+
+		if ( this.props.placeholder ) {
+			return this.props.placeholder;
+		}
+
+		return null;
+	}
+}
