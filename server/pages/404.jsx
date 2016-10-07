fa56e7357b2578de89461c8442374018bcbba585
Bernhard Reiter <ockham@raz.or.at>
2016-10-07 16:55:25 +0200

    Move head.jsx and badge.jsx to components/

--- server/pages/404.jsx
+++ server/pages/404.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import Head from './head';
+import Head from './components/head';
 
 const NotFoundError = ( { urls: { 'style.css': stylesheetUrl } } ) => (
 	<html lang="en">
