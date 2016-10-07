aa5a662b7efb4fc97116a94f6998ea4b71344b21
Bernhard Reiter <ockham@raz.or.at>
2016-10-07 16:26:49 +0200

    Badge: Spaces -> tabs

--- server/pages/badge.jsx
+++ server/pages/badge.jsx
@@ -15,27 +15,27 @@ const BranchName = ( { branchName, commitChecksum } ) => (
 );
 
 const DevDocsLink = ( { devDocsUrl, docs } ) => (
-  <span className="environment is-docs">
-    <a href={ devDocsUrl } title="DevDocs">
-      { docs }
-    </a>
-  </span>
+	<span className="environment is-docs">
+		<a href={ devDocsUrl } title="DevDocs">
+			{ docs }
+		</a>
+	</span>
 );
 
 const Badge = ( {
-  badge,
-  branchName,
-  commitChecksum,
-  devDocs,
-  devDocsURL: devDocsUrl,
-  docs,
-  feedbackURL: feedbackUrl
+	badge,
+	branchName,
+	commitChecksum,
+	devDocs,
+	devDocsURL: devDocsUrl,
+	docs,
+	feedbackURL: feedbackUrl
 } ) => (
 	<div className="environment-badge">
 		{ config.isEnabled( 'dev/test-helper' ) && <div className="environment is-tests" /> }
 		{ branchName && branchName !== 'master' &&
-      <BranchName branchName={ branchName } commitChecksum={ commitChecksum } />
-    }
+			<BranchName branchName={ branchName } commitChecksum={ commitChecksum } />
+		}
 		{ devDocs && <DevDocsLink devdocsURL={ devDocsUrl } docs={ docs } /> }
 		<span className={ 'environment is-' + badge }>
 			{ badge }
