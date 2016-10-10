72cadb84d73d0dcd2e231e43accc4245da60554f
Steve Seear <steve.seear@gmail.com>
2016-10-10 17:36:55 +0100

    Create test

---
+++ client/my-sites/theme/test/main.jsx
@@ -0,0 +1,52 @@
+/**
+ * External dependencies
+ */
+import { assert } from 'chai';
+import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
+import ReactDomServer from 'react-dom/server';
+import mockery from 'mockery';
+import noop from 'lodash/noop';
+
+/**
+ * Internal dependencies
+ */
+import { createReduxStore } from 'state';
+import { LayoutLoggedOut } from 'layout/logged-out';
+import useFakeDom from 'test/helpers/use-fake-dom';
+import useMockery from 'test/helpers/use-mockery';
+
+describe( 'main', function() {
+	context( 'when trying to renderToString() without theme data', function() {
+		useMockery();
+		useFakeDom();
+
+		before( function() {
+			mockery.registerSubstitute( 'matches-selector', 'component-matches-selector' );
+			mockery.registerMock( 'lib/wp', {
+				me: () => ( {
+					get: noop
+				} ),
+				undocumented: () => ( {
+					getProducts: noop
+				} )
+			} );
+
+			const store = createReduxStore();
+			const ThemeSheetComponent = require( '../main' );
+
+			const primary = <ThemeSheetComponent id={ 'twentysixteen' } />;
+
+			this.layout = (
+				<ReduxProvider store={ store }>
+					<LayoutLoggedOut primary={ primary } />
+				</ReduxProvider>
+			);
+		} );
+
+		it( "doesn't throw an exception", function() {
+			assert.doesNotThrow( ReactDomServer.renderToString( this.layout ) );
+		} );
+	} );
+} );
+
