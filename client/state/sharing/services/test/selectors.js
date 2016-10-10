39879199fb18f30aecbed4488d6f930485b59b4b
Konstantin Obenland <obenland@gmx.de>
2016-10-10 08:59:58 -0700

    Update selector tests after 5cb9d4d.
    See
    https://github.com/Automattic/wp-calypso/pull/8595/commits/5cb9d4d8f5ac3
    9eabaae25fa245719f5a63efc61

--- client/state/sharing/services/test/selectors.js
+++ client/state/sharing/services/test/selectors.js
@@ -128,13 +128,13 @@ describe( 'selectors', () => {
 		};
 
 		it( 'should return empty object if there are no services', () => {
-			const services = getEligibleKeyringServices( defaultState, 'other' );
+			const services = getEligibleKeyringServices( defaultState, 2916284, 'other' );
 
 			expect( services ).to.eql( [] );
 		} );
 
 		it( 'should return the keyring services with the correct type', () => {
-			const services = getEligibleKeyringServices( state, 'publicize' );
+			const services = getEligibleKeyringServices( state, 2916284, 'publicize' );
 
 			expect( services ).to.eql( [
 				{ ID: 'facebook', type: 'publicize', jetpack_support: true },
@@ -144,7 +144,7 @@ describe( 'selectors', () => {
 
 		it( 'should omit eventbrite if user can not manage_options', () => {
 			state.currentUser.capabilities[2916284].manage_options = false;
-			const services = getEligibleKeyringServices( state, 'other' );
+			const services = getEligibleKeyringServices( state, 2916284, 'other' );
 			state.currentUser.capabilities[2916284].manage_options = true;
 
 			expect( services ).to.eql( [] );
@@ -152,14 +152,14 @@ describe( 'selectors', () => {
 
 		it( 'should omit publicize services if user can not publish_posts', () => {
 			state.currentUser.capabilities[2916284].publish_posts = false;
-			const services = getEligibleKeyringServices( state, 'publicize' );
+			const services = getEligibleKeyringServices( state, 2916284, 'publicize' );
 			state.currentUser.capabilities[2916284].publish_posts = true;
 
 			expect( services ).to.eql( [] );
 		} );
 
 		it( 'should include services if required module is activated', () => {
-			const services = getEligibleKeyringServices( state, 'other' );
+			const services = getEligibleKeyringServices( state, 2916284, 'other' );
 
 			expect( services ).to.eql( [
 				{
@@ -173,7 +173,7 @@ describe( 'selectors', () => {
 
 		it( 'should omit services if required module is not activated', () => {
 			state.sites.items[2916284].options.active_modules = [];
-			const services = getEligibleKeyringServices( state, 'other' );
+			const services = getEligibleKeyringServices( state, 2916284, 'other' );
 			state.sites.items[2916284].options.active_modules = [ 'publicize' ];
 
 			expect( services ).to.eql( [] );
