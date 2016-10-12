47292db2713b446cbd65b9616f207f7cf3d0e5cf
Leif Singer <git@singer.sh>
2016-10-12 10:16:23 +0200

    debug: are we "waiting" somewhere ...?

--- client/layout/guided-tours/wait.js
+++ client/layout/guided-tours/wait.js
@@ -2,12 +2,16 @@
  * External dependencies
  */
 import noop from 'lodash/noop';
+import debugFactory from 'debug';
 
 const WAIT_INITIAL = 1; // initial wait in milliseconds
 const WAIT_MULTIPLIER = 2;
 const WAIT_MAX = 2048; // give up waiting when delay has grown to ~4 seconds
+const debug = debugFactory( 'calypso:guided-tours' );
 
 const wait = ( { condition, consequence, delay = 0, onError = noop } ) => {
+	debug( 'wait.js: condition, consequence, delay', condition, consequence, delay );
+
 	if ( condition() ) {
 		consequence();
 		return;
