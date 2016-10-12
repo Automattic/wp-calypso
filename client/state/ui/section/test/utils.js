25f376ccb94fd5dc74bd5632f9a7d855f5d4f2e3
Andrew Duthie <andrew@andrewduthie.com>
2016-10-12 11:45:59 -0400

    State: Test state reference for same state comparison

--- client/state/ui/section/test/utils.js
+++ client/state/ui/section/test/utils.js
@@ -26,17 +26,6 @@ describe( 'utils', () => {
 			expect( state ).to.be.null;
 		} );
 
-		it( 'should return the same state if action section doesn\'t include key', () => {
-			const state = reducer( null, {
-				type: SECTION_SET,
-				section: {
-					baz: 'qux'
-				}
-			} );
-
-			expect( state ).to.be.null;
-		} );
-
 		it( 'should return the new value if action section includes key', () => {
 			const state = reducer( null, {
 				type: SECTION_SET,
@@ -46,6 +35,24 @@ describe( 'utils', () => {
 			} );
 
 			expect( state ).to.equal( 'bar' );
+		} );
+
+		it( 'should return the same state if action section doesn\'t include key', () => {
+			const originalState = reducer( null, {
+				type: SECTION_SET,
+				section: {
+					foo: 'bar'
+				}
+			} );
+
+			const state = reducer( originalState, {
+				type: SECTION_SET,
+				section: {
+					baz: 'qux'
+				}
+			} );
+
+			expect( state ).to.equal( originalState );
 		} );
 	} );
 } );
