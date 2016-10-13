3364efc6a5d3966dc1779a0082456fe12c483912
Konstantin Obenland <obenland@gmx.de>
2016-10-13 14:04:38 -0700

    Add a query component for keyring connections

---
+++ client/components/data/query-keyring-connections/index.jsx
@@ -0,0 +1,33 @@
+/**
+ * External dependencies
+ */
+import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { isKeyringConnectionsFetching } from 'state/sharing/keyring/selectors';
+import { requestKeyringConnections } from 'state/sharing/keyring/actions';
+
+class QueryKeyringConnections extends Component {
+	static propTypes = {
+		isRequesting: PropTypes.bool,
+		requestKeyringConnections: PropTypes.func,
+	};
+
+	componentWillMount() {
+		if ( ! this.props.isRequesting ) {
+			this.props.requestKeyringConnections();
+		}
+	}
+
+	render() { return null; }
+}
+
+export default connect(
+	( state ) => ( {
+		isRequesting: isKeyringConnectionsFetching( state )
+	} ),
+	{ requestKeyringConnections }
+)( QueryKeyringConnections );
