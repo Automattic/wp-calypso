da1079e0747ebb6ed4cf11dfbd43888e72e7b71c
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-10-13 11:56:15 +0200

    Purchases: Move Add Credit Card page to /me/purchases folder

--- client/me/payment-methods/titles.js
+++
@@ -1,7 +0,0 @@
-/**
- * External dependencies
- */
-import { translate } from 'i18n-calypso';
-
-export const addCreditCard = translate( 'Add Credit Card' );
-export const paymentMethods = translate( 'Payment Methods' );
