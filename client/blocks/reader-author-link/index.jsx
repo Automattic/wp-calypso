d8448f358e4c8e783f3afc2b3493dc156207cb2c
bluefuton <chris@bluefuton.com>
2016-10-14 15:20:17 +1300

    Move author name blacklist into ReaderAuthorLink

--- client/blocks/reader-author-link/index.jsx
+++ client/blocks/reader-author-link/index.jsx
@@ -2,23 +2,32 @@
  * External dependencies
  */
 import React from 'react';
+import { get, includes } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import { recordAction, recordGaEvent, recordTrackForPost } from 'reader/stats';
+import * as stats from 'reader/stats';
 
 const ReaderAuthorLink = ( { author, post, siteUrl, children } ) => {
 	const recordAuthorClick = ( { } ) => {
-		recordAction( 'click_author' );
-		recordGaEvent( 'Clicked Author Link' );
+		stats.recordAction( 'click_author' );
+		stats.recordGaEvent( 'Clicked Author Link' );
 		if ( post ) {
-			recordTrackForPost( 'calypso_reader_author_link_clicked', post );
+			stats.recordTrackForPost( 'calypso_reader_author_link_clicked', post );
 		}
 	};
 
 	if ( ! siteUrl ) {
 		siteUrl = author.URL;
+	}
+
+	const authorName = get( author, 'name' );
+	const authorNameBlacklist = [ 'admin' ];
+
+	// If the author name is blacklisted, don't return anything
+	if ( typeof authorName === 'string' && includes( authorNameBlacklist, author.name.toLowerCase() ) ) {
+		return null;
 	}
 
 	// If we have neither author.URL or siteUrl, just return children
