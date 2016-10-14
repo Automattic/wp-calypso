1ed249c4321b28430a9528280dd68f97d416b8b9
Konstantin Obenland <obenland@gmx.de>
2016-10-14 14:42:05 -0700

    Update keyring reducer to use correct format.
    Turns out that publicize connection objects don’t actually look like I
    thought the look like.

--- client/state/sharing/keyring/reducer.js
+++ client/state/sharing/keyring/reducer.js
@@ -28,11 +28,11 @@ export const isFetching = createReducer( false, {
 // Stores the list of available keyring connections
 export const items = createReducer( {}, {
 	[ KEYRING_CONNECTIONS_RECEIVE ]: ( state, { connections } ) => ( { ...keyBy( connections, 'ID' ) } ),
-	[ PUBLICIZE_CONNECTION_CREATE ]: ( state, { ID, siteId } ) => ( { ...state, [ ID ]: {
-		...state[ ID ], sites: [ ...state[ ID ].sites, siteId.toString() ]
+	[ PUBLICIZE_CONNECTION_CREATE ]: ( state, { connection: { keyring_connection_ID: ID, site_ID } } ) => ( { ...state, [ ID ]: {
+		...state[ ID ], sites: [ ...state[ ID ].sites, site_ID.toString() ]
 	} } ),
-	[ PUBLICIZE_CONNECTION_DELETE ]: ( state, { ID, siteId } ) => ( { ...state, [ ID ]: {
-		...state[ ID ], sites: without( state[ ID ].sites, siteId.toString() )
+	[ PUBLICIZE_CONNECTION_DELETE ]: ( state, { connection: { keyring_connection_ID: ID, site_ID } } ) => ( { ...state, [ ID ]: {
+		...state[ ID ], sites: without( state[ ID ].sites, site_ID.toString() )
 	} } ),
 }, itemSchema );
 
