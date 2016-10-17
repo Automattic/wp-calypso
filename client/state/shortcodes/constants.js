74d7cefe42a802f8b47c662e34f02e09a98792d4
Marin Atanasov <tyxla@abv.bg>
2016-10-17 18:38:34 +0300

    State: Refactor shortcodes reducer and its tests

--- client/state/shortcodes/constants.js
+++
@@ -1,5 +0,0 @@
-export const LOAD_STATUS = {
-	LOADING: 'LOADING',
-	ERROR: 'ERROR',
-	LOADED: 'LOADED'
-};
--- client/state/shortcodes/reducer.js
+++ client/state/shortcodes/reducer.js
@@ -1,50 +1,66 @@
 /**
+ * External dependencies
+ */
+import { combineReducers } from 'redux';
+import { has } from 'lodash';
+
+/**
  * Internal dependencies
  */
-import { createReducer } from 'state/utils';
-import { LOAD_STATUS } from './constants';
 import { shortcodesSchema } from './schema';
+import { createReducer } from 'state/utils';
 import {
 	SHORTCODE_FETCH,
 	SHORTCODE_RECEIVE
 } from 'state/action-types';
 
-const siteShortcode = ( state = {}, { type, error, data } ) => {
-	switch ( type ) {
-		case SHORTCODE_FETCH:
-			return { ...state, ...{ status: LOAD_STATUS.LOADING } };
-		case SHORTCODE_RECEIVE:
-			if ( error ) {
-				return { ...state, ...{ status: LOAD_STATUS.ERROR } };
+const requestingShortcode = ( state, siteId, shortcode, requesting ) => {
+	const siteShortcodes = has( state, siteId ) ? state[ siteId ] : {};
+	return {
+		...state,
+		[ siteId ]: {
+			...siteShortcodes,
+			[ shortcode ]: requesting
+		}
+	};
+};
+
+/**
+ * Returns the updated requests state after an action has been dispatched. The
+ * state maps site ID keys to the object with the site's shortcodes. Each shortcode
+ * is true if it's being currently requested, false otherwise.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action payload
+ * @return {Object}        Updated state
+ */
+export const requesting = createReducer( {}, {
+	[ SHORTCODE_FETCH ]: ( state, { siteId, shortcode } ) => requestingShortcode( state, siteId, shortcode, true ),
+	[ SHORTCODE_RECEIVE ]: ( state, { siteId, shortcode } ) => requestingShortcode( state, siteId, shortcode, false ),
+} );
+
+/**
+ * Returns the updated items state after an action has been dispatched. The
+ * state maps site ID keys to an object that contains the site shortcodes.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action payload
+ * @return {Object}        Updated state
+ */
+export const items = createReducer( {}, {
+	[ SHORTCODE_RECEIVE ]: ( state, { siteId, shortcode, data } ) => {
+		const siteShortcodes = has( state, siteId ) ? state[ siteId ] : {};
+		return {
+			...state,
+			[ siteId ]: {
+				...siteShortcodes,
+				[ shortcode ]: data
 			}
-
-			const { scripts, styles } = data;
-			const body = data.result;
-			const status = LOAD_STATUS.LOADED;
-
-			return { ...state, ...{ body, scripts, styles, status } };
+		};
 	}
-
-	return state;
-};
-
-const siteShortcodes = ( state = {}, action ) => {
-	switch ( action.type ) {
-		case SHORTCODE_FETCH:
-		case SHORTCODE_RECEIVE:
-			const { shortcode } = action;
-			return { ...state, ...{ [ shortcode ]: siteShortcode( state[ shortcode ] || {}, action ) } };
-	}
-
-	return state;
-};
-
-const shortcodes = ( state = {}, action ) => {
-	const { siteId } = action;
-	return ( { ...state, ...{ [ siteId ]: siteShortcodes( state[ siteId ] || {}, action ) } } );
-};
-
-export default createReducer( {}, {
-	[ SHORTCODE_FETCH ]: shortcodes,
-	[ SHORTCODE_RECEIVE ]: shortcodes
 }, shortcodesSchema );
+
+export default combineReducers( {
+	requesting,
+	items
+} );
