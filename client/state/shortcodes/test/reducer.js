de770dc1de7ed5674e58c6dcd451faa0a8dd11af
Marin Atanasov <tyxla@abv.bg>
2016-10-17 18:38:34 +0300

    State: Refactor shortcodes reducer and its tests

--- client/state/shortcodes/test/reducer.js
+++ client/state/shortcodes/test/reducer.js
@@ -9,120 +9,289 @@ import deepFreeze from 'deep-freeze';
  */
 import {
 	SHORTCODE_FETCH,
-	SHORTCODE_RECEIVE
+	SHORTCODE_RECEIVE,
+	SERIALIZE,
+	DESERIALIZE
 } from 'state/action-types';
-import { LOAD_STATUS } from '../constants';
-import reducer from '../reducer';
+import reducer, { requesting, items } from '../reducer';
+import { useSandbox } from 'test/helpers/use-sinon';
 
 describe( 'reducer', () => {
-	it( 'should default to {}', () => {
-		const state = reducer( undefined, {} );
-
-		expect( state ).to.eql( {} );
+	useSandbox( ( sandbox ) => {
+		sandbox.stub( console, 'warn' );
 	} );
 
-	it( 'should change the status to loading when fetching', () => {
-		const state = reducer( {}, {
-			type: SHORTCODE_FETCH,
-			siteId: 1,
-			shortcode: 'test_shortcode',
+	it( 'should include expected keys in return value', () => {
+		expect( reducer( undefined, {} ) ).to.have.keys( [
+			'requesting',
+			'items'
+		] );
+	} );
+
+	describe( '#requesting()', () => {
+		it( 'should default to an empty object', () => {
+			const state = requesting( undefined, {} );
+
+			expect( state ).to.eql( {} );
 		} );
 
-		expect( state ).to.eql( {
-			1: {
-				test_shortcode: {
-					status: LOAD_STATUS.LOADING
+		it( 'should set shortcode of that site ID to true value if a request is initiated', () => {
+			const state = requesting( undefined, {
+				type: SHORTCODE_FETCH,
+				siteId: 12345678,
+				shortcode: 'test_shortcode'
+			} );
+
+			expect( state ).to.eql( {
+				12345678: {
+					test_shortcode: true
 				}
-			}
+			} );
+		} );
+
+		it( 'should accumulate the requested site IDs', () => {
+			const state = requesting( deepFreeze( {
+				12345678: {
+					test_shortcode: true
+				}
+			} ), {
+				type: SHORTCODE_FETCH,
+				siteId: 87654321,
+				shortcode: 'another_shortcode'
+			} );
+
+			expect( state ).to.eql( {
+				12345678: {
+					test_shortcode: true
+				},
+				87654321: {
+					another_shortcode: true
+				}
+			} );
+		} );
+
+		it( 'should accumulate the requested shortcodes of given site IDs', () => {
+			const state = requesting( deepFreeze( {
+				12345678: {
+					test_shortcode: false
+				},
+				87654321: {
+					another_shortcode: true
+				}
+			} ), {
+				type: SHORTCODE_FETCH,
+				siteId: 12345678,
+				shortcode: 'some_shortcode'
+			} );
+
+			expect( state ).to.eql( {
+				12345678: {
+					test_shortcode: false,
+					some_shortcode: true
+				},
+				87654321: {
+					another_shortcode: true
+				}
+			} );
+		} );
+
+		it( 'should set shortcode of that site ID to false if request finishes successfully', () => {
+			const state = requesting( deepFreeze( {
+				12345678: {
+					test_shortcode: true
+				}
+			} ), {
+				type: SHORTCODE_RECEIVE,
+				siteId: 12345678,
+				shortcode: 'test_shortcode'
+			} );
+
+			expect( state ).to.eql( {
+				12345678: {
+					test_shortcode: false
+				},
+			} );
+		} );
+
+		it( 'should not persist state', () => {
+			const state = requesting( deepFreeze( {
+				12345678: {
+					test_shortcode: true
+				}
+			} ), {
+				type: SERIALIZE
+			} );
+
+			expect( state ).to.eql( {} );
+		} );
+
+		it( 'should not load persisted state', () => {
+			const state = requesting( deepFreeze( {
+				12345678: {
+					test_shortcode: true
+				}
+			} ), {
+				type: DESERIALIZE
+			} );
+
+			expect( state ).to.eql( {} );
 		} );
 	} );
 
-	it( 'should set an error status if an error is received', () => {
-		const initialState = deepFreeze( {
-			1: {
-				test_shortcode: {
-					status: LOAD_STATUS.LOADING,
+	describe( '#items()', () => {
+		const shortcodeData = {
+			status: true,
+			body: 'body',
+			scripts: 'scripts',
+			styles: 'styles'
+		};
+
+		it( 'should default to an empty object', () => {
+			const state = items( undefined, {} );
+
+			expect( state ).to.eql( {} );
+		} );
+
+		it( 'should index shortcodes by site ID', () => {
+			const state = items( null, {
+				type: SHORTCODE_RECEIVE,
+				siteId: 12345678,
+				shortcode: 'test_shortcode',
+				data: shortcodeData
+			} );
+
+			expect( state ).to.eql( {
+				12345678: {
+					test_shortcode: shortcodeData
 				}
-			}
+			} );
 		} );
 
-		const state = reducer( initialState, {
-			type: SHORTCODE_RECEIVE,
-			siteId: 1,
-			shortcode: 'test_shortcode',
-			error: 'Some error happened',
-		} );
-
-		expect( state ).to.eql( {
-			1: {
-				test_shortcode: {
-					status: LOAD_STATUS.ERROR,
+		it( 'should accumulate sites', () => {
+			const state = items( deepFreeze( {
+				12345678: {
+					test_shortcode: shortcodeData
 				}
-			}
-		} );
-	} );
+			} ), {
+				type: SHORTCODE_RECEIVE,
+				siteId: 87654321,
+				shortcode: 'test_shortcode',
+				data: { ...shortcodeData, status: false }
+			} );
 
-	it( 'should only modify status if an error is received', () => {
-		const initialState = deepFreeze( {
-			1: {
-				test_shortcode: {
-					status: LOAD_STATUS.LOADED,
-					body: 'body',
-					scripts: 'scripts',
-					styles: 'styles'
+			expect( state ).to.eql( {
+				12345678: {
+					test_shortcode: shortcodeData
+				},
+				87654321: {
+					test_shortcode: { ...shortcodeData, status: false }
 				}
-			}
+			} );
 		} );
 
-		const state = reducer( initialState, {
-			type: SHORTCODE_RECEIVE,
-			siteId: 1,
-			shortcode: 'test_shortcode',
-			error: 'Some error happened',
-			data: 'Some broken data'
-		} );
-
-		expect( state ).to.eql( {
-			1: {
-				test_shortcode: {
-					status: LOAD_STATUS.ERROR,
-					body: 'body',
-					scripts: 'scripts',
-					styles: 'styles'
+		it( 'should accumulate shortcodes in sites', () => {
+			const state = items( deepFreeze( {
+				12345678: {
+					test_shortcode: shortcodeData
+				},
+				87654321: {
+					test_shortcode: { ...shortcodeData, status: false }
 				}
-			}
-		} );
-	} );
+			} ), {
+				type: SHORTCODE_RECEIVE,
+				siteId: 12345678,
+				shortcode: 'another_shortcode',
+				data: { ...shortcodeData, status: false }
+			} );
 
-	it( 'should change status to loaded when shortcode received', () => {
-		const initialState = deepFreeze( {
-			1: {
-				test_shortcode: {
-					status: LOAD_STATUS.LOADING,
+			expect( state ).to.eql( {
+				12345678: {
+					test_shortcode: shortcodeData,
+					another_shortcode: { ...shortcodeData, status: false }
+				},
+				87654321: {
+					test_shortcode: { ...shortcodeData, status: false }
 				}
-			}
+			} );
 		} );
 
-		const state = reducer( initialState, {
-			type: SHORTCODE_RECEIVE,
-			siteId: 1,
-			shortcode: 'test_shortcode',
-			data: {
-				result: 'body',
-				scripts: 'scripts',
-				styles: 'styles'
-			}
-		} );
-
-		expect( state ).to.eql( {
-			1: {
-				test_shortcode: {
-					status: LOAD_STATUS.LOADED,
-					body: 'body',
-					scripts: 'scripts',
-					styles: 'styles'
+		it( 'should override previous shortcodes of same site ID', () => {
+			const state = items( deepFreeze( {
+				12345678: {
+					test_shortcode: shortcodeData
+				},
+				87654321: {
+					test_shortcode: { ...shortcodeData, status: false }
 				}
-			}
+			} ), {
+				type: SHORTCODE_RECEIVE,
+				siteId: 87654321,
+				shortcode: 'test_shortcode',
+				data: shortcodeData
+			} );
+
+			expect( state ).to.eql( {
+				12345678: {
+					test_shortcode: shortcodeData
+				},
+				87654321: {
+					test_shortcode: shortcodeData
+				}
+			} );
+		} );
+
+		it( 'should persist state', () => {
+			const state = items( deepFreeze( {
+				12345678: {
+					test_shortcode: shortcodeData
+				},
+				87654321: {
+					test_shortcode: { ...shortcodeData, status: false }
+				}
+			} ), {
+				type: SERIALIZE
+			} );
+
+			expect( state ).to.eql( {
+				12345678: {
+					test_shortcode: shortcodeData
+				},
+				87654321: {
+					test_shortcode: { ...shortcodeData, status: false }
+				}
+			} );
+		} );
+
+		it( 'should load valid persisted state', () => {
+			const state = items( deepFreeze( {
+				12345678: {
+					test_shortcode: shortcodeData
+				},
+				87654321: {
+					test_shortcode: { ...shortcodeData, status: false }
+				}
+			} ), {
+				type: DESERIALIZE
+			} );
+
+			expect( state ).to.eql( {
+				12345678: {
+					test_shortcode: shortcodeData
+				},
+				87654321: {
+					test_shortcode: { ...shortcodeData, status: false }
+				}
+			} );
+		} );
+
+		it( 'should not load invalid persisted state', () => {
+			const state = items( deepFreeze( {
+				1234567: 'test_shortcode'
+			} ), {
+				type: DESERIALIZE
+			} );
+
+			expect( state ).to.eql( {} );
 		} );
 	} );
 } );
