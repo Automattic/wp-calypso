40cbe716427396dec7fe6e1e68e27005198d480c
Biser Perchinkov <biser@bisko.be>
2016-10-18 14:24:00 +0300

    Update Site Title step reducer name (#8788)

--- client/state/signup/steps/site-title/actions.js
+++ client/state/signup/steps/site-title/actions.js
@@ -1,11 +1,11 @@
 /**
  * Internal dependencies
  */
-import { SIGNUP_STEPS_SITE_TITLE } from 'state/action-types';
+import { SIGNUP_STEPS_SITE_TITLE_SET } from 'state/action-types';
 
 export function setSiteTitle( siteTitle ) {
 	return {
-		type: SIGNUP_STEPS_SITE_TITLE,
+		type: SIGNUP_STEPS_SITE_TITLE_SET,
 		siteTitle
 	};
 }
