7db5b4e5cd7ef2d913db5c856469137c6b53fd53
Joen Asmussen <joen@automattic.com>
2016-10-19 13:46:15 +0200

    Remove Add New Site from permanent sidebar fixture.
    Also style the site selector Add New Site instead.

--- client/layout/sidebar/footer.jsx
+++ client/layout/sidebar/footer.jsx
@@ -15,9 +15,6 @@ import HappychatButton from 'components/happychat/button';
 const SidebarFooter = ( { translate, children } ) => (
 	<div className="sidebar__footer">
 		{ children }
-		<Button borderless href={ config( 'signup_url' ) + '?ref=calypso-selector' }>
-			<Gridicon icon="add-outline" /> { translate( 'Add New Site' ) }
-		</Button>
 		<Button className="sidebar__footer-help" borderless href="/help" title={ translate( 'Help' ) }>
 			<Gridicon icon="help-outline" />
 		</Button>
