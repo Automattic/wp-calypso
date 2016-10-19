63219930d57d54b8de319e13c2bebe1d7d38e68c
Ben Lowery <ben.lowery@automattic.com>
2016-10-19 17:08:59 -0400

    Reader: Reload posts that are marked with _should_reload when they come into redux

--- client/state/reader/posts/actions.js
+++ client/state/reader/posts/actions.js
@@ -1,11 +1,15 @@
 /**
  * External Dependencies
  */
-import filter from 'lodash/filter';
-import forEach from 'lodash/forEach';
-import map from 'lodash/map';
-import isUndefined from 'lodash/isUndefined';
-import reject from 'lodash/reject';
+import {
+	filter,
+	forEach,
+	has,
+	isUndefined,
+	map,
+	omit,
+	partition,
+	reject } from 'lodash';
 
 /**
  * Internal dependencies
@@ -17,9 +21,68 @@ import analytics from 'lib/analytics';
 import { runFastRules, runSlowRules } from './normalization-rules';
 import Dispatcher from 'dispatcher';
 import { action } from 'lib/feed-post-store/constants';
+import wpcom from 'lib/wp';
 
 function trackRailcarRender( post ) {
 	analytics.tracks.recordEvent( 'calypso_traintracks_render', post.railcar );
+}
+
+function postToKey( post ) {
+	return {
+		feedId: post.feed_ID,
+		postId: post.ID,
+		blogId: ! post.is_external && post.site_ID ? post.site_ID : undefined
+	};
+}
+
+function fetchForKey( postKey ) {
+	let req;
+	if ( postKey.blogId ) {
+		req = wpcom.undocumented().readSitePost( {
+			site: postKey.blogId,
+			postId: postKey.postId
+		} );
+	} else {
+		req = wpcom.undocumented().readFeedPost( postKey );
+	}
+	return req;
+}
+
+export function fetchPost( postKey ) {
+	return function( dispatch ) {
+		return fetchForKey( postKey ).then(
+			( data ) => {
+				dispatch( receivePosts( [ data ] ) );
+			},
+			( err ) => {
+				dispatch( {
+					type: READER_POSTS_RECEIVE,
+					posts: [ {
+						feed_ID: postKey.feedId,
+						ID: postKey.ID,
+						site_ID: postKey.blogId,
+						is_external: ! postKey.blogId,
+						is_error: true,
+						global_ID: `${ postKey.feedId }-${ postKey.blogId }-${ postKey.postId }`,
+						error: err
+					} ]
+				} );
+			}
+		);
+	};
+}
+
+export function reloadPost( post ) {
+	return function( dispatch ) {
+		// keep track of any railcars we might have
+		const railcar = post.railcar;
+		return fetchForKey( postToKey( post ) ).then(
+			( data ) => {
+				data.railcar = railcar;
+				dispatch( receivePosts( [ data ] ) );
+			}
+		);
+	};
 }
 
 /**
@@ -29,14 +92,21 @@ function trackRailcarRender( post ) {
  * @return {Object} Action object
  */
 export function receivePosts( posts ) {
-	if ( ! posts ) {
-		return Promise.resolve( [] );
-	}
 	return function( dispatch ) {
+		if ( ! posts ) {
+			return Promise.resolve( [] );
+		}
+
 		const withoutUndefined = reject( posts, isUndefined );
 		const normalizedPosts = map( withoutUndefined, runFastRules );
 
-		forEach( map( normalizedPosts, runSlowRules ), slowPromise => {
+		const [ postsToReload, postsToProcess ] = partition( normalizedPosts, '_should_reload' );
+		forEach( postsToReload, post => {
+			delete post._should_reload;
+			dispatch( reloadPost( post ) );
+		} );
+
+		forEach( map( postsToProcess, runSlowRules ), slowPromise => {
 			slowPromise.then( post => {
 				dispatch( {
 					type: READER_POSTS_RECEIVE,
@@ -58,13 +128,14 @@ export function receivePosts( posts ) {
 
 		// keep the old feed post store in sync
 		forEach( normalizedPosts, post => {
+			const postForFlux = has( post, '_should_reload' ) ? omit( post, '_should_reload' ) : post;
 			Dispatcher.handleServerAction( {
-				data: post,
+				data: postForFlux,
 				type: action.RECEIVE_NORMALIZED_FEED_POST
 			} );
 		} );
 
-		forEach( filter( normalizedPosts, 'railcar' ), trackRailcarRender );
+		forEach( filter( postsToProcess, 'railcar' ), trackRailcarRender );
 
 		return Promise.resolve( normalizedPosts );
 	};
