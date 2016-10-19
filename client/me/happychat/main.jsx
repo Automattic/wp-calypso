d7fdb2ff811cb725195f15e67afbbbe8ec7e667c
Robert Collins <robert@automattic.com>
2016-10-19 10:46:21 -0700

    Adds full page class selector

--- client/me/happychat/main.jsx
+++ client/me/happychat/main.jsx
@@ -18,7 +18,7 @@ import { timeline, composer } from 'components/happychat/helpers';
 class HappychatPage extends Component {
 	componentDidMount() {
 		this.props.openChat();
-	},
+	}
 
 	onFocus() {
 		const composerNode = findDOMNode( this.refs.composer );
