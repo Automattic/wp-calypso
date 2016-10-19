6c683c1faea84de5bbf77a2c8feddadc072fb035
Marin Atanasov <tyxla@abv.bg>
2016-10-19 14:49:29 +0300

    State: Improve shortcodes selectors and their tests

--- client/state/shortcodes/selectors.js
+++ client/state/shortcodes/selectors.js
@@ -1,4 +1,26 @@
 /**
+ * External dependencies
+ */
+import { has } from 'lodash';
+
+/**
+ * Returns true if currently requesting that shortcode for the specified site ID, or
+ * false otherwise.
+ *
+ * @param  {Object}  state     Global state tree
+ * @param  {Number}  siteId    Site ID
+ * @param  {String}  shortcode Shortcode
+ * @return {Boolean}           Whether that shortcode is being requested
+ */
+export const isRequestingShortcode = ( state, siteId, shortcode ) => {
+	if ( ! has( state, [ 'shortcodes', 'requesting', siteId, shortcode ] ) ) {
+		return false;
+	}
+
+	return !! state.shortcodes.requesting[ siteId ][ shortcode ];
+};
+
+/**
  * Retrieve the data of a certain shortcode for a particular site
  * @param  {Object} state      Global state tree
  * @param  {Number} siteId     Site ID
@@ -6,9 +28,9 @@
  * @return {Object}            Shortcode data
  */
 export const getShortcode = ( state, siteId, shortcode ) => {
-	if ( ! state.shortcodes[ siteId ] || ! state.shortcodes[ siteId ][ shortcode ] ) {
+	if ( ! has( state, [ 'shortcodes', 'items', siteId, shortcode ] ) ) {
 		return;
 	}
 
-	return state.shortcodes[ siteId ][ shortcode ];
+	return state.shortcodes.items[ siteId ][ shortcode ];
 };
