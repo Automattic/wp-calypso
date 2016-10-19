3c71d84f9ab0fec912e60ca5cfd6b6ce228c5170
Marin Atanasov <tyxla@abv.bg>
2016-10-19 14:17:40 +0300

    State: More granular actions for shortcodes

--- client/state/shortcodes/actions.js
+++ client/state/shortcodes/actions.js
@@ -3,32 +3,38 @@
  */
 import wpcom from 'lib/wp';
 import {
-	SHORTCODE_FETCH,
-	SHORTCODE_RECEIVE
+	SHORTCODE_RECEIVE,
+	SHORTCODE_REQUEST,
+	SHORTCODE_REQUEST_FAILURE,
+	SHORTCODE_REQUEST_SUCCESS
 } from 'state/action-types';
 
 export function fetchShortcode( siteId, shortcode ) {
 	return ( dispatch ) => {
 		dispatch( {
-			type: SHORTCODE_FETCH,
+			type: SHORTCODE_REQUEST,
 			siteId,
 			shortcode
 		} );
 
 		return wpcom.undocumented().site( siteId ).shortcodes( { shortcode } ).then( ( data ) => {
 			dispatch( {
-				type: SHORTCODE_RECEIVE,
+				type: SHORTCODE_REQUEST_SUCCESS,
 				siteId,
-				shortcode,
-				data,
-				error: null
+				shortcode
 			} );
-		} ).catch( ( error ) => {
+
 			dispatch( {
 				type: SHORTCODE_RECEIVE,
 				siteId,
 				shortcode,
-				data: null,
+				data
+			} );
+		} ).catch( ( error ) => {
+			dispatch( {
+				type: SHORTCODE_REQUEST_FAILURE,
+				siteId,
+				shortcode,
 				error
 			} );
 		} );
