2c9fe0347a3438248c91da3b7ca3d5df6a062ee8
Matt Wondra <mattwondra@gmail.com>
2016-10-19 22:48:50 -0500

    Fetch data for server-rendered logged-out state

--- client/my-sites/themes/controller.jsx
+++ client/my-sites/themes/controller.jsx
@@ -11,6 +11,8 @@ import MultiSiteComponent from 'my-sites/themes/multi-site';
 import LoggedOutComponent from './logged-out';
 import trackScrollPage from 'lib/track-scroll-page';
 import { getAnalyticsData } from './helpers';
+import { fetchNextPage, query } from 'state/themes/actions';
+import { PER_PAGE } from 'state/themes/themes-list/constants';
 
 function getProps( context ) {
 	const { tier, filter, vertical, site_id: siteId } = context.params;
@@ -70,7 +72,16 @@ export function multiSite( context, next ) {
 
 export function loggedOut( context, next ) {
 	const props = getProps( context );
+	const queryParams = {
+		search: context.query.s,
+		tier: context.params.tier,
+		filter: props.filter,
+		page: 0,
+		perPage: PER_PAGE,
+	};
 
 	context.primary = <LoggedOutComponent { ...props } />;
-	next();
+
+	context.store.dispatch( query( queryParams ) );
+	context.store.dispatch( fetchNextPage( false ) ).then( () => next() );
 }
