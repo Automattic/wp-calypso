19d103aacc821c52d61c4dd586957715fc160340
Dan Roundhill <dan@automattic.com>
2016-10-20 10:42:50 -0700

    Don't show the section header that is very WordPress.com specific for Jetpack sites.

--- client/my-sites/site-settings/seo-settings/form.jsx
+++ client/my-sites/site-settings/seo-settings/form.jsx
@@ -465,22 +465,25 @@ export const SeoForm = React.createClass( {
 					/>
 				}
 
-				<SectionHeader label={ this.translate( 'Search Engine Optimization' ) }>
-				</SectionHeader>
-				<Card>
-					{ this.translate(
-						'{{b}}WordPress.com has great SEO{{/b}} out of the box. All of our themes are optimized ' +
-						'for search engines, so you don\'t have to do anything extra. However, you can tweak ' +
-						'these settings if you\'d like more advanced control. Read more about what you can do ' +
-						'to {{a}}optimize your site\'s SEO{{/a}}.',
-						{
-							components: {
-								a: <a href={ 'https://en.blog.wordpress.com/2013/03/22/seo-on-wordpress-com/' } />,
-								b: <strong />
-							}
-						}
-					) }
-				</Card>
+				{ ! jetpack &&
+					<div>
+						<SectionHeader label={ this.translate( 'Search Engine Optimization' ) } />
+						<Card>
+							{ this.translate(
+								'{{b}}WordPress.com has great SEO{{/b}} out of the box. All of our themes are optimized ' +
+								'for search engines, so you don\'t have to do anything extra. However, you can tweak ' +
+								'these settings if you\'d like more advanced control. Read more about what you can do ' +
+								'to {{a}}optimize your site\'s SEO{{/a}}.',
+								{
+									components: {
+										a: <a href={ 'https://en.blog.wordpress.com/2013/03/22/seo-on-wordpress-com/' } />,
+										b: <strong />
+									}
+								}
+							) }
+						</Card>
+					</div>
+				}
 
 				<form onChange={ this.markChanged } className="seo-settings__seo-form">
 					{ showAdvancedSeo && config.isEnabled( 'manage/advanced-seo/custom-title' ) &&
