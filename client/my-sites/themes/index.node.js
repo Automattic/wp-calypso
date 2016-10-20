c018de69364393aaf54e1c829ec9eed1e81d316e
Matt Wondra <mattwondra@gmail.com>
2016-10-19 21:09:51 -0500

    Stub and fix dependencies that had SSR errors so now empty ThemeShowcase renders server-side

--- client/my-sites/themes/index.node.js
+++ client/my-sites/themes/index.node.js
@@ -4,26 +4,20 @@
 import config from 'config';
 import { makeLayout } from 'controller';
 import { getSubjects } from './theme-filters.js';
-import React from 'react';
+import { loggedOut } from './controller';
 
 // `logged-out` middleware isn't SSR-compliant yet, but we can at least render
 // the layout.
 // FIXME: Also create loggedOut/multiSite/singleSite elements, depending on route.
-
-function serverRender( context, next ) {
-	// Temporary dummy middleware to validate that server rendering is working.
-	context.primary = React.createElement( 'div', {}, 'Loading...' );
-	next();
-}
 
 export default function( router ) {
 	const verticals = getSubjects().join( '|' );
 
 	if ( config.isEnabled( 'manage/themes' ) ) {
 		if ( config.isEnabled( 'manage/themes-ssr' ) ) {
-			router( `/design/:vertical(${ verticals })?/:tier(free|premium)?`, serverRender, makeLayout );
-			router( `/design/:vertical(${ verticals })?/:tier(free|premium)?/filter/:filter`, serverRender, makeLayout );
-			router( '/design/*', serverRender, makeLayout ); // Needed so direct hits don't result in a 404.
+			router( `/design/:vertical(${ verticals })?/:tier(free|premium)?`, loggedOut, makeLayout );
+			router( `/design/:vertical(${ verticals })?/:tier(free|premium)?/filter/:filter`, loggedOut, makeLayout );
+			router( '/design/*', loggedOut, makeLayout ); // Needed so direct hits don't result in a 404.
 		} else {
 			router( `/design/:vertical(${ verticals })?/:tier(free|premium)?`, makeLayout );
 			router( `/design/:vertical(${ verticals })?/:tier(free|premium)?/filter/:filter`, makeLayout );
