0b437b398a0025653a128809ad7f6335639473a7
Rastislav Lamos <lamos.rasto@gmail.com>
2016-10-21 15:17:08 +0200

    Image Editor: add ability to pass in a local file (#8878)
    * Image Editor: add support for object URLs
    
    * Image Editor Devdocs: refactor to stateful component
    
    * Image Editor Devdocs: add option to upload files
    
    * Image Editor: update README with `media.src` info

--- client/blocks/image-editor/docs/example.jsx
+++ client/blocks/image-editor/docs/example.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { get } from 'lodash';
 
@@ -11,49 +11,89 @@ import { get } from 'lodash';
 import ImageEditor from '../';
 import { getCurrentUser } from 'state/current-user/selectors';
 
-function ImageEditorExample( { primarySiteId } ) {
-	const media = {
-		URL: 'https://cldup.com/mA_hqNVj0w.jpg'
-	};
+class ImageEditorExample extends Component {
+	constructor() {
+		super();
+		
+		this.state = {
+			media: {
+				URL: 'https://cldup.com/mA_hqNVj0w.jpg'
+			}
+		};
+	}
 
-	const getTestingImage = () => document.querySelector( "#devdocs-example-image-editor-result" );
+	getTestingImage = () => document.querySelector( "#devdocs-example-image-editor-result" );
 
-	const onImageEditorDone = ( error, blob ) => {
+	onImageEditorDone = ( error, blob ) => {
 		if ( error ) {
 			return;
 		}
 		
 		const imageUrl = window.URL.createObjectURL( blob );
 
-		getTestingImage().src = imageUrl;
+		this.getTestingImage().src = imageUrl;
 	};
 
-	const onImageEditorReset = () => {
-		getTestingImage().src = media.URL;
+	onImageEditorReset = () => {
+		this.getTestingImage().src = this.state.media.URL || this.state.media.src;
+	};
+	
+	componentDidMount() {
+		const fileInput = document.querySelector( '#devdocs-example-image-editor-file-input' );
+		
+		fileInput.addEventListener( 'change', this.onImageUpload );
+	}
+
+	onImageUpload = ( e ) => {
+		const imageFile = e.target.files[ 0 ];
+		
+		const imageObjectUrl = URL.createObjectURL( imageFile );
+		
+		this.setState( {
+			media: {
+				src: imageObjectUrl
+			}
+		} );
+
+		this.getTestingImage().src = imageObjectUrl;
 	};
 
-	return (
-		<div>
-			<div style={ { height: '80vh' } }>
-				<ImageEditor
-					siteId={ primarySiteId }
-					media={ media }
-					onDone={ onImageEditorDone }
-					onReset={ onImageEditorReset }
-				/>
+	render() {
+		const {
+			primarySiteId
+		} = this.props;
+		
+		return (
+			<div>
+				<div style={ {
+					marginBottom: '20px'
+				} }>
+					<h4>Upload an image</h4>
+					<input type="file" id="devdocs-example-image-editor-file-input" />
+				</div>
+
+				<div style={ { height: '80vh' } }>
+					<ImageEditor
+						siteId={ primarySiteId }
+						media={ this.state.media }
+						onDone={ this.onImageEditorDone }
+						onReset={ this.onImageEditorReset }
+					/>
+				</div>
+
+				<div style={ {
+					textAlign: 'center',
+					marginTop: '15px'
+				} }>
+					<h4>Changes to the image above are shown below</h4>
+					<img
+						id="devdocs-example-image-editor-result"
+						src={ this.state.media.URL }
+					/>
+				</div>
 			</div>
-			<div style={ {
-				textAlign: 'center',
-				marginTop: '15px'
-			} }>
-				<h4>Changes to the image above are shown below</h4>
-				<img
-					id="devdocs-example-image-editor-result"
-					src={ media.URL }
-				/>
-			</div>
-		</div>
-	);
+		);
+	}
 }
 
 const ConnectedImageEditorExample = connect( ( state ) => {
