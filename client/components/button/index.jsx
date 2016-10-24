fb6d729f991b18b6ae986813d987123acf9d9a77
Andrew Duthie <andrew@andrewduthie.com>
2016-10-24 12:41:49 -0400

    Components: Specify noreferrer rel when including target on button

--- client/components/button/index.jsx
+++ client/components/button/index.jsx
@@ -5,7 +5,7 @@
  */
 import { PropTypes, createElement, PureComponent } from 'react';
 import classNames from 'classnames';
-import { omit } from 'lodash';
+import { omit, compact } from 'lodash';
 
 export default class Button extends PureComponent {
 	static propTypes = {
@@ -35,8 +35,15 @@ export default class Button extends PureComponent {
 			omitProps.push( 'target', 'rel' );
 		}
 
+		const props = omit( this.props, omitProps );
+
+		// Block referrers when external link
+		if ( props.target ) {
+			props.rel = compact( [ props.rel, 'noopener', 'noreferrer' ] ).join( ' ' );
+		}
+
 		return createElement( tag, {
-			...omit( this.props, omitProps ),
+			...props,
 			className: classNames( 'button', this.props.className, {
 				'is-compact': this.props.compact,
 				'is-primary': this.props.primary,
