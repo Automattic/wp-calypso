1756bc50ceb6e003dceb1276a17a7035f414e09b
Andrew Duthie <andrew@andrewduthie.com>
2016-10-24 12:40:56 -0400

    State: Add site getCustomizerUrl selector

--- client/lib/url/index.js
+++ client/lib/url/index.js
@@ -10,6 +10,7 @@ import startsWith from 'lodash/startsWith';
  * Internal dependencies
  */
 import config from 'config';
+import addQueryArgs from 'lib/route/add-query-args';
 
 /**
  * Check if a URL is located outside of Calypso.
@@ -108,4 +109,6 @@ export default {
 	setUrlScheme,
 	urlToSlug,
 	slugToUrl,
+	// [TODO]: Move lib/route/add-query-args contents here
+	addQueryArgs
 };
