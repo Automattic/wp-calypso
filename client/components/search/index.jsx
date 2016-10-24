e3a34098c98821d9dde4ddb4df9ff48d8884335c
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2016-10-25 00:30:56 +0200

    Calypso Magic Search spike. (#8282)
    Suggestions component in Themes Search.

--- client/components/search/index.jsx
+++ client/components/search/index.jsx
@@ -56,6 +56,7 @@ const Search = React.createClass( {
 		autoFocus: PropTypes.bool,
 		disabled: PropTypes.bool,
 		onKeyDown: PropTypes.func,
+		onClick: PropTypes.func,
 		disableAutocorrect: PropTypes.bool,
 		onBlur: PropTypes.func,
 		searching: PropTypes.bool,
@@ -85,6 +86,10 @@ const Search = React.createClass( {
 			onSearchOpen: noop,
 			onSearchClose: noop,
 			onKeyDown: noop,
+			onClick: noop,
+			//undefined value for overlayStyling is an optimization that will
+			//disable overlay scrolling calculation when no overlay is provided.
+			overlayStyling: undefined,
 			disableAutocorrect: false,
 			searching: false,
 			isOpen: false,
@@ -190,9 +195,9 @@ const Search = React.createClass( {
 		this.setState( { keyword: '' } );
 	},
 
-	onBlur: function() {
+	onBlur: function( event ) {
 		if ( this.props.onBlur ) {
-			this.props.onBlur();
+			this.props.onBlur( event );
 		}
 
 		this.setState( { hasFocus: false } );
@@ -335,6 +340,7 @@ const Search = React.createClass( {
 						onChange={ this.onChange }
 						onKeyUp={ this.keyUp }
 						onKeyDown={ this.keyDown }
+						onMouseUp={ this.props.onClick }
 						onFocus={ this.onFocus }
 						onBlur={ this.onBlur }
 						disabled={ this.props.disabled }
