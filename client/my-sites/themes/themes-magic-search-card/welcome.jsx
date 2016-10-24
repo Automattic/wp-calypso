d6e2790741f44a6b38cdbfce0a55847f49f6644d
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2016-10-25 00:30:56 +0200

    Calypso Magic Search spike. (#8282)
    Suggestions component in Themes Search.

---
+++ client/my-sites/themes/themes-magic-search-card/welcome.jsx
@@ -0,0 +1,52 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import { noop } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import Gridicon from 'components/gridicon';
+import { taxonomyToGridicon, taxonomyToColor } from './taxonomy-styling.js';
+
+const MagicSearchWelcome = React.createClass( {
+
+	propTypes: {
+		taxonomies: PropTypes.array,
+		topSearches: PropTypes.array,
+		suggestionsCallback: PropTypes.func,
+	},
+
+	defaultProps: {
+		taxonomies: [],
+		topSearches: [],
+		suggestionsCallback: noop,
+	},
+
+	onMouseDown: function( event ) {
+		this.props.suggestionsCallback( event.target.textContent + ':' );
+	},
+
+	render() {
+		return (
+			<div className="themes-magic-search-card__welcome" >
+				<span className="themes-magic-search-card__welcome-header">{ this.translate('SEARCH BY') }</span>
+				<div className="themes-magic-search-card__welcome-taxonomies">
+					{ this.props.taxonomies.map( taxonomy =>
+						<div
+							className="themes-magic-search-card__welcome-taxonomy"
+							style={ taxonomyToColor( taxonomy ) }
+							onMouseDown={ this.onMouseDown }
+							key={ taxonomy }
+						>
+							<Gridicon icon={ taxonomyToGridicon( taxonomy ) } className="themes-magic-search-card__welcome-taxonomy-icon" />
+							{taxonomy}
+						</div> ) }
+				</div>
+			</div>
+		);
+	}
+} );
+
+export default MagicSearchWelcome;
