4cccb72e301673892c4db308935338f884695ba1
Matt Wondra <mattwondra@gmail.com>
2016-10-24 22:03:03 -0500

    Get server-side data caching in place for Theme Showcase

--- client/my-sites/themes/controller.jsx
+++ client/my-sites/themes/controller.jsx
@@ -1,6 +1,7 @@
 /**
  * External Dependencies
  */
+import debugFactory from 'debug';
 import React from 'react';
 
 /**
@@ -10,9 +11,22 @@ import SingleSiteComponent from 'my-sites/themes/single-site';
 import MultiSiteComponent from 'my-sites/themes/multi-site';
 import LoggedOutComponent from './logged-out';
 import trackScrollPage from 'lib/track-scroll-page';
-import { getAnalyticsData } from './helpers';
-import { fetchNextPage, query } from 'state/themes/actions';
 import { PER_PAGE } from 'state/themes/themes-list/constants';
+import { getQueryParams } from 'state/themes/themes-list/selectors';
+import wpcom from 'lib/wp';
+import {
+	incrementThemesPage,
+	query,
+	receiveThemes,
+	receiveServerError
+} from 'state/themes/actions';
+import {
+	getAnalyticsData,
+	generateCacheKey
+} from './helpers';
+
+const debug = debugFactory( 'calypso:themes' );
+const themesQueryCache = new Map();
 
 function getProps( context ) {
 	const { tier, filter, vertical, site_id: siteId } = context.params;
@@ -82,14 +96,55 @@ export function fetchThemeData( context, next ) {
 		return next();
 	}
 
-	const queryParams = {
+	const userQueryParams = {
 		search: context.query.s,
 		tier: context.params.tier,
 		filter: context.params.filter,
 		page: 0,
 		perPage: PER_PAGE,
 	};
+	const queryKey = generateCacheKey( userQueryParams );
+	const queryResults = themesQueryCache.get( queryKey );
+	const startTime = new Date().getTime();
+	const HOUR_IN_MS = 3600000;
 
-	context.store.dispatch( query( queryParams ) );
-	context.store.dispatch( fetchNextPage( false ) ).then( () => next() );
+	context.store.dispatch( query( userQueryParams ) );
+	context.store.dispatch( incrementThemesPage( false ) );
+
+	const stateQueryParams = getQueryParams( context.store.getState() );
+	if ( queryResults && ( queryResults.timestamp + HOUR_IN_MS > Date.now() ) ) {
+		debug( 'found themes!', queryKey );
+		context.store.dispatch( receiveThemes(
+			queryResults.themes,
+			queryResults.site,
+			queryResults.queryParams,
+			queryResults.responseTime
+		) );
+		return next();
+	}
+
+	wpcom.undocumented().themes( false, stateQueryParams )
+		.then( themes => {
+			const timestamp = Date.now();
+			const responseTime = timestamp - startTime;
+
+			debug( 'caching', queryKey, JSON.stringify( themes ) );
+			themesQueryCache.set( queryKey, {
+				themes,
+				queryParams: stateQueryParams,
+				responseTime,
+				timestamp,
+				site: false
+			} );
+
+			context.renderCacheKey = context.path + timestamp;
+			context.store.dispatch( receiveThemes( themes, false, stateQueryParams, responseTime ) );
+			next();
+		} )
+		.catch( error => {
+			debug( `Error fetching theme showcase for ${ JSON.stringify( userQueryParams ) } details: `, error.message || error );
+			context.store.dispatch( receiveServerError( error ) );
+			context.renderCacheKey = 'themes not found';
+			next();
+		} );
 }
