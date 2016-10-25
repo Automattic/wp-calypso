cd9258b053e1329d91dab58254b5bdb65c5e9c63
Bernhard Reiter <ockham@raz.or.at>
2016-10-25 21:35:59 +0200

    Fix logged-out

--- client/my-sites/themes/logged-out.jsx
+++ client/my-sites/themes/logged-out.jsx
@@ -2,37 +2,28 @@
  * External dependencies
  */
 import React from 'react';
-import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import ThemeShowcase from './theme-showcase';
-import {
-	preview,
-	signup,
-	separator,
-	info,
-	support,
-	help,
-	bindOptions
-} from './theme-options';
-
-const BoundThemeShowcase = connect( ...bindOptions )( ThemeShowcase );
+import { ThemeOptions } from './theme-options';
 
 export default props => (
-	<BoundThemeShowcase { ...props }
-	options={ {
-		signup,
-		preview,
-		separator,
-		info,
-		support,
-		help
-	} }
+	<ThemeOptions { ...props }
+	options={ [
+		'signup',
+		'preview',
+		'separator',
+		'info',
+		'support',
+		'help'
+	] }
 	defaultOption="signup"
 	getScreenshotOption={ function() {
 		return 'info';
 	} }
-	source="showcase" />
+	source="showcase">
+		<ThemeShowcase { ...props } />
+	</ThemeOptions>
 );
