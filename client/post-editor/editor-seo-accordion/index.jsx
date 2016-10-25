c8c061057b70c4fbc8eb7b2a06a14642c0af2a09
Dan Roundhill <dan@automattic.com>
2016-10-25 15:16:05 -0700

    Added Preview button to seo editor accordion. Will only show on Jetpack sites.
    Launches a `WebPreview` component that shows only the `SEO` tab.

--- client/post-editor/editor-seo-accordion/index.jsx
+++ client/post-editor/editor-seo-accordion/index.jsx
@@ -1,46 +1,101 @@
 /**
  * External dependencies
  */
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import identity from 'lodash/identity';
 import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import Accordion from 'components/accordion';
+import Button from 'components/button';
 import AccordionSection from 'components/accordion/section';
 import CountedTextarea from 'components/forms/counted-textarea';
 import Gridicon from 'components/gridicon';
 import PostActions from 'lib/posts/actions';
 import EditorDrawerLabel from 'post-editor/editor-drawer/label';
+import WebPreview from 'components/web-preview';
+import { isJetpackSite } from 'state/sites/selectors';
+import { getSelectedSiteId } from 'state/ui/selectors';
 
-function EditorSeoAccordion( { translate, metaDescription = '' } ) {
-	return (
-		<Accordion
-			title={ translate( 'SEO Description' ) }
-			icon={ <Gridicon icon="search" /> }
-			className="editor-seo-accordion"
-		>
-			<AccordionSection>
-				<EditorDrawerLabel
-					helpText={ translate(
-						'Craft a description of your post for search engine results. ' +
-						'The post content is used by default.'
-					) }
-					labelText={ translate( 'Meta Description' ) }
-				>
-					<CountedTextarea
-						maxLength="300"
-						acceptableLength={ 159 }
-						placeholder={ translate( 'Write a description…' ) }
-						aria-label={ translate( 'Write a description…' ) }
-						value={ metaDescription }
-						onChange={ onMetaChange }
-					/>
-				</EditorDrawerLabel>
-			</AccordionSection>
-		</Accordion>
-	);
+class EditorSeoAccordion extends Component {
+	static propTypes = {
+		translate: PropTypes.func,
+		metaDescription: PropTypes.string
+	};
+
+	static defaultProps = {
+		metaDescription: '',
+		translate: identity
+	};
+
+	constructor( props ) {
+		super( props );
+
+		this.showPreview = this.showPreview.bind( this );
+		this.hidePreview = this.hidePreview.bind( this );
+
+		this.state = { showPreview: false };
+	}
+
+	showPreview() {
+		this.setState( { showPreview: true } );
+	}
+
+	hidePreview() {
+		this.setState( { showPreview: false } );
+	}
+
+	render() {
+		const { translate, metaDescription, isJetpack } = this.props;
+		const { showPreview } = this.state;
+
+		return (
+			<Accordion
+				title={ translate( 'SEO Description' ) }
+				icon={ <Gridicon icon="search" /> }
+				className="editor-seo-accordion"
+			>
+				<AccordionSection>
+					<EditorDrawerLabel
+						helpText={ translate(
+							'Craft a description of your post for search engine results. ' +
+							'The post content is used by default.'
+						) }
+						labelText={ translate( 'Meta Description' ) }
+					>
+						<CountedTextarea
+							maxLength="300"
+							acceptableLength={ 159 }
+							placeholder={ translate( 'Write a description…' ) }
+							aria-label={ translate( 'Write a description…' ) }
+							value={ metaDescription }
+							onChange={ onMetaChange }
+						/>
+					</EditorDrawerLabel>
+					{ isJetpack &&
+						<div>
+							<Button
+								className="editor-seo-accordion__preview-button"
+								onClick={ this.showPreview }
+							>
+								{ translate( 'Preview' ) }
+							</Button>
+							<WebPreview
+								showPreview={ showPreview }
+								onClose={ this.hidePreview }
+								showDeviceSwitcher={ false }
+								showExternal={ false }
+								defaultViewportDevice="seo"
+							/>
+						</div>
+					}
+				</AccordionSection>
+			</Accordion>
+		);
+	}
 }
 
 function onMetaChange( event ) {
@@ -49,9 +104,17 @@ function onMetaChange( event ) {
 	} );
 }
 
-EditorSeoAccordion.propTypes = {
-	translate: PropTypes.func,
-	metaDescription: PropTypes.string
+const mapStateToProps = ( state ) => {
+	const siteId = getSelectedSiteId( state );
+
+	return {
+		isJetpack: isJetpackSite( state, siteId )
+	};
 };
 
-export default localize( EditorSeoAccordion );
+export default connect(
+	mapStateToProps,
+	null,
+	null,
+	{ pure: false } // defaults to true, but this component has internal state
+)( localize( EditorSeoAccordion ) );
