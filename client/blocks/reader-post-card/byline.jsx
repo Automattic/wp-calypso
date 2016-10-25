12e705bb9ecd3fa3ab273c7f8be3af335f5af2fc
Ben Lowery <ben.lowery@automattic.com>
2016-10-25 16:42:53 -0400

    Move the logic for picking the reader avatar

--- client/blocks/reader-post-card/byline.jsx
+++ client/blocks/reader-post-card/byline.jsx
@@ -59,15 +59,15 @@ class PostByline extends React.Component {
 		const streamUrl = getStreamUrl( feedId, siteId );
 		const siteIcon = get( site, 'icon.img' );
 		const feedIcon = get( feed, 'image' );
-		const hasGravatar = get( post, 'author.has_avatar' );
 
 		/* eslint-disable wpcalypso/jsx-gridicon-size */
 		return (
 			<div className="reader-post-card__byline ignore-click">
 				<ReaderAvatar
-					siteIcon={ hasGravatar ? null : siteIcon }
-					feedIcon={ hasGravatar ? null : feedIcon }
-					author={ hasGravatar ? post.author : {} } />
+					siteIcon={ siteIcon }
+					feedIcon={ feedIcon }
+					author={ post.author }
+					preferGravatar={ true } />
 				<div className="reader-post-card__byline-details">
 					{ shouldDisplayAuthor &&
 						<ReaderAuthorLink
