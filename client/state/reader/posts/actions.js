c1d59d8a2d449799e29a7d63a67a046cbde315fd
Ben Lowery <blowery@users.noreply.github.com>
2016-10-25 13:33:01 -0400

    Reader: Reload posts that are marked as such (#8890)
    * Reader: Reload posts that are marked with _should_reload when they come into redux
    
    * Add in tests for the new bits
    
    * Add tests, shorten up some code

--- client/state/reader/posts/actions.js
+++ client/state/reader/posts/actions.js
@@ -1,11 +1,15 @@
 /**
  * External Dependencies
  */
-import filter from 'lodash/filter';
-import forEach from 'lodash/forEach';
-import map from 'lodash/map';
-import isUndefined from 'lodash/isUndefined';
-import reject from 'lodash/reject';
+import {
+	filter,
+	forEach,
+	has,
+	isUndefined,
+	map,
+	omit,
+	partition,
+	reject } from 'lodash';
 
 /**
  * Internal dependencies
@@ -17,9 +21,70 @@ import analytics from 'lib/analytics';
 import { runFastRules, runSlowRules } from './normalization-rules';
 import Dispatcher from 'dispatcher';
 import { action } from 'lib/feed-post-store/constants';
+import wpcom from 'lib/wp';
 
 function trackRailcarRender( post ) {
 	analytics.tracks.recordEvent( 'calypso_traintracks_render', post.railcar );
+}
+
+export function postToKey( post ) {
+	if ( post && post.feed_ID && post.feed_item_ID ) {
+		return {
+			feedId: post.feed_ID,
+			postId: post.feed_item_ID
+		};
+	}
+
+	if ( post.is_external ) {
+		return {
+			feedId: post.feed_ID,
+			postId: post.ID
+		};
+	}
+
+	return {
+		postId: post.ID,
+		blogId: post.site_ID
+	};
+}
+
+function fetchForKey( postKey ) {
+	if ( postKey.blogId ) {
+		return wpcom.undocumented().readSitePost( {
+			site: postKey.blogId,
+			postId: postKey.postId
+		} );
+	}
+	return wpcom.undocumented().readFeedPost( postKey );
+}
+
+export function fetchPost( postKey ) {
+	return dispatch => fetchForKey( postKey ).then(
+		data => dispatch( receivePosts( [ data ] ) ),
+		err => dispatch( {
+			type: READER_POSTS_RECEIVE,
+			posts: [ {
+				feed_ID: postKey.feedId,
+				ID: postKey.postId,
+				site_ID: postKey.blogId,
+				is_external: ! postKey.blogId,
+				is_error: true,
+				global_ID: `${ postKey.feedId || 'na' }-${ postKey.blogId || 'na' }-${ postKey.postId }`,
+				error: err
+			} ]
+		} )
+	);
+}
+
+export function reloadPost( post ) {
+	return dispatch => {
+		// keep track of any railcars we might have
+		const railcar = post.railcar;
+		return fetchForKey( postToKey( post ) ).then( data => {
+			data.railcar = railcar;
+			return dispatch( receivePosts( [ data ] ) );
+		} );
+	};
 }
 
 /**
@@ -29,14 +94,21 @@ function trackRailcarRender( post ) {
  * @return {Object} Action object
  */
 export function receivePosts( posts ) {
-	if ( ! posts ) {
-		return Promise.resolve( [] );
-	}
 	return function( dispatch ) {
+		if ( ! posts ) {
+			return Promise.resolve( [] );
+		}
+
 		const withoutUndefined = reject( posts, isUndefined );
 		const normalizedPosts = map( withoutUndefined, runFastRules );
 
-		forEach( map( normalizedPosts, runSlowRules ), slowPromise => {
+		const [ postsToReload, postsToProcess ] = partition( normalizedPosts, '_should_reload' );
+		forEach( postsToReload, post => {
+			delete post._should_reload;
+			dispatch( reloadPost( post ) );
+		} );
+
+		forEach( map( postsToProcess, runSlowRules ), slowPromise => {
 			slowPromise.then( post => {
 				dispatch( {
 					type: READER_POSTS_RECEIVE,
@@ -58,13 +130,14 @@ export function receivePosts( posts ) {
 
 		// keep the old feed post store in sync
 		forEach( normalizedPosts, post => {
+			const postForFlux = has( post, '_should_reload' ) ? omit( post, '_should_reload' ) : post;
 			Dispatcher.handleServerAction( {
-				data: post,
+				data: postForFlux,
 				type: action.RECEIVE_NORMALIZED_FEED_POST
 			} );
 		} );
 
-		forEach( filter( normalizedPosts, 'railcar' ), trackRailcarRender );
+		forEach( filter( postsToProcess, 'railcar' ), trackRailcarRender );
 
 		return Promise.resolve( normalizedPosts );
 	};
