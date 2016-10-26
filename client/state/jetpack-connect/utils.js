fb7949b8cc1d9dc504d6ded01c56113bcdcee471
Marin Atanasov <tyxla@abv.bg>
2016-10-26 16:31:40 +0300

    Jetpack Connect: Persist only non-stale session data

--- client/state/jetpack-connect/utils.js
+++ client/state/jetpack-connect/utils.js
@@ -5,10 +5,10 @@ import { JETPACK_CONNECT_TTL } from './constants';
 
 /***
  * Whether a Jetpack Connect store timestamp is stale.
- * @param {Object} Item to check.
- * @returns {Boolean} True if the timestamp is stale, false otherwise.
+ * @param   {Number} timestamp  Item to check.
+ * @returns {Boolean}           True if the timestamp is stale, false otherwise.
  */
 export function isStale( timestamp ) {
 	const now = new Date().getTime();
-	return ( now - timestamp ) > JETPACK_CONNECT_TTL;
+	return ( now - timestamp ) >= JETPACK_CONNECT_TTL;
 }
