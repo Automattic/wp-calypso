de6a7e6a5707d2f407b78d80eaa04a80ded46450
Konstantin Obenland <obenland@gmx.de>
2016-10-27 10:03:17 -0700

    Move SharingServiceTip to use ES6 class
    Also adds a whitelist for services we provide tips for.

--- client/my-sites/sharing/connections/service-tip.jsx
+++ client/my-sites/sharing/connections/service-tip.jsx
@@ -1,69 +1,98 @@
 /**
  * External dependencies
  */
-var React = require( 'react' );
+import React, { Component, PropTypes } from 'react';
+import { localize } from 'i18n-calypso';
 
-module.exports = React.createClass( {
-	displayName: 'SharingServiceTip',
+/**
+ * Module constants
+ */
+/**
+ * Whitelist of services that we provide tips for.
+ *
+ * When adding tips for more services, please update the whitelist in addition to adding
+ * a method with the tip's content.
+ *
+ * @type {string[]}
+ */
+const SERVICES_WHITELIST = [
+	'facebook',
+	'twitter',
+	'instagram',
+	'google_plus',
+	'eventbrite',
+];
 
-	getDefaultProps: function() {
-		return {
-			tips: Object.freeze( {
-				facebook: function() {
-					return this.translate( 'You can also add a {{likeBoxLink}}Like Box{{/likeBoxLink}}, a {{shareButtonLink}}share button{{/shareButtonLink}}, or {{embedLink}}embed{{/embedLink}} your page or profile on your site.', {
-						components: {
-							likeBoxLink: <a href="https://support.wordpress.com/facebook-integration/#facebook-like-box" />,
-							shareButtonLink: <a href="https://support.wordpress.com/sharing/" />,
-							embedLink: <a href="https://support.wordpress.com/facebook-integration/facebook-embeds/" />
-						},
-						context: 'Sharing: Tip in settings'
-					} );
-				},
-				twitter: function() {
-					return this.translate( 'You can also add a {{widgetLink}}Twitter Timeline Widget{{/widgetLink}} to display any public timeline on your site.', {
-						components: {
-							widgetLink: <a href="https://support.wordpress.com/widgets/twitter-timeline-widget/" />
-						},
-						context: 'Sharing: Tip in settings'
-					} );
-				},
-				instagram: function() {
-					return this.translate( 'You can also add an {{widgetLink}}Instagram Widget{{/widgetLink}} to display your latest Instagram photos on your site.', {
-						components: {
-							widgetLink: <a href="https://support.wordpress.com/instagram/instagram-widget/" />
-						},
-						context: 'Sharing: Tip in settings'
-					} );
-				},
-				google_plus: function() {
-					return this.translate( 'You can also {{embedLink}}embed a Google+ post{{/embedLink}} onto a post or page.', {
-						components: {
-							embedLink: <a href="https://support.wordpress.com/google-plus-embeds/" />
-						},
-						context: 'Sharing: Tip in settings'
-					} );
-				},
-				eventbrite: function() {
-					return this.translate( 'You can also add the {{embedLink}}Eventbrite widget{{/embedLink}} to display events in a sidebar.', {
-						components: {
-							embedLink: <a href="https://support.wordpress.com/widgets/eventbrite-event-calendarlisting-widget/" />
-						},
-						context: 'Sharing: Tip in settings'
-					} );
-				}
-			} )
-		};
-	},
+class SharingServiceTip extends Component {
+	static propTypes = {
+		service: PropTypes.object.isRequired,
+		translate: PropTypes.func,
+	};
 
-	render: function() {
-		if ( 'function' === typeof this.props.tips[ this.props.service.ID ] ) {
-			return (
-				<div className="sharing-service-tip">
-					<span className="noticon noticon-info"></span>{ this.props.tips[ this.props.service.ID ].call( this ) }
-				</div>
-			);
-		} else {
+	facebook() {
+		return this.props.translate(
+			'You can also add a {{likeBoxLink}}Like Box{{/likeBoxLink}}, a {{shareButtonLink}}share button{{/shareButtonLink}}, or {{embedLink}}embed{{/embedLink}} your page or profile on your site.', {
+				components: {
+					likeBoxLink: <a href="https://support.wordpress.com/facebook-integration/#facebook-like-box" />,
+					shareButtonLink: <a href="https://support.wordpress.com/sharing/" />,
+					embedLink: <a href="https://support.wordpress.com/facebook-integration/facebook-embeds/" />
+				},
+				context: 'Sharing: Tip in settings'
+			} );
+	}
+
+	twitter() {
+		return this.props.translate(
+			'You can also add a {{widgetLink}}Twitter Timeline Widget{{/widgetLink}} to display any public timeline on your site.', {
+				components: {
+					widgetLink: <a href="https://support.wordpress.com/widgets/twitter-timeline-widget/" />
+				},
+				context: 'Sharing: Tip in settings'
+			} );
+	}
+
+	instagram() {
+		return this.props.translate(
+			'You can also add an {{widgetLink}}Instagram Widget{{/widgetLink}} to display your latest Instagram photos on your site.', {
+				components: {
+					widgetLink: <a href="https://support.wordpress.com/instagram/instagram-widget/" />
+				},
+				context: 'Sharing: Tip in settings'
+			} );
+	}
+
+	google_plus() {
+		return this.props.translate(
+			'You can also {{embedLink}}embed a Google+ post{{/embedLink}} onto a post or page.', {
+				components: {
+					embedLink: <a href="https://support.wordpress.com/google-plus-embeds/" />
+				},
+				context: 'Sharing: Tip in settings'
+			} );
+	}
+
+	eventbrite() {
+		return this.props.translate(
+			'You can also add the {{embedLink}}Eventbrite widget{{/embedLink}} to display events in a sidebar.', {
+				components: {
+					embedLink: <a href="https://support.wordpress.com/widgets/eventbrite-event-calendarlisting-widget/" />
+				},
+				context: 'Sharing: Tip in settings'
+			} );
+	}
+
+	render() {
+		if ( -1 === SERVICES_WHITELIST.indexOf( this.props.service.ID ) ) {
 			return <div className="sharing-service-tip" />;
 		}
+
+		return (
+			<div className="sharing-service-tip">
+				<span className="noticon noticon-info" />
+				{ this[ this.props.service.ID ]() }
+			</div>
+		);
 	}
-} );
+}
+
+export default localize( SharingServiceTip );
