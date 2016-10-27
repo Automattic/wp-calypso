3647dbd982ed371103250034f962b96dd7225c3e
Daryl L. L. Houston <daryl@learnhouston.com>
2016-10-27 13:29:01 -0400

    Make it possible to do a Guided Transfer to Pressable.

---
+++ client/my-sites/guided-transfer/host-credentials-page/pressable.jsx
@@ -0,0 +1,78 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import CompactCard from 'components/card/compact';
+import {
+	Email,
+	Password,
+	CreateAccountTip,
+	SubmitSection,
+	WPOrgURL,
+} from './fields';
+
+class Pressable extends Component {
+	static propTypes = {
+		hostInfo: PropTypes.shape( {
+			label: PropTypes.string.isRequired,
+			url: PropTypes.string.isRequired
+		} ).isRequired
+	};
+
+	render() {
+		const {
+			hostInfo,
+			translate,
+			fieldValues,
+			onFieldChange,
+			isSubmitting,
+			submit,
+		} = this.props;
+
+		return (
+			<div>
+				<CompactCard>
+					<p>
+						{ translate(
+							'Please enter your account details. They will be stored securely so that one ' +
+							'of our Happiness Engineers can get the transfer going for you.' ) }
+					</p>
+					<div>
+						<Email
+							value={ fieldValues.email }
+							onChange={ onFieldChange( 'email' ) }
+							hostLabel={ hostInfo.label }
+							disabled={ isSubmitting }
+						/>
+						<Password
+							value={ fieldValues.password }
+							onChange={ onFieldChange( 'password' ) }
+							hostLabel={ hostInfo.label }
+							disabled={ isSubmitting }
+						/>
+					</div>
+					<CreateAccountTip
+						hostLabel={ hostInfo.label }
+						hostUrl={ hostInfo.url }
+					/>
+					<WPOrgURL
+						value={ fieldValues.wporg_url }
+						onChange={ onFieldChange( 'wporg_url' ) }
+						disabled={ isSubmitting }
+					/>
+				</CompactCard>
+				<SubmitSection
+					submit={ submit }
+					isSubmitting={ isSubmitting }
+				/>
+			</div>
+		);
+	}
+}
+
+export default localize( Pressable );
