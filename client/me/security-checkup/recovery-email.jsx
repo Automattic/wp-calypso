581e63ffb96a5c9ba76da61c93862d27aad28a30
James Tien <southp0105@gmail.com>
2016-10-27 17:32:17 +0800

    Refactor `manage-contact`, `recovery-phone` and `recovery-email` components as ES6 class (#8916)
    * Move `manage-contact`, `recovery-phone` and `recovery-email` to ES6
    classes.
    
    * Fix all the code review change requests.
    
    - One variable definition per line.
    - Remove `displayName` in favor of ES6 classes
    - Use `react-redux` `connect` instead of the old mixin.
    - Remove a dead function, `haveEmail`.
    
    * Re-adapt `React.createClass` since there is no current good replacement
    for the `observe` mixin.
    
    * Use `localize` to inject `translate` to the components.
    
    * should be `this.props.translate` instead of `this.translate`
    
    * Use `localize` to inject `tranlate` in the `ManageContact` component
    
    * Remove the unnecessary `QueryPreference`

--- client/me/security-checkup/recovery-email.jsx
+++ client/me/security-checkup/recovery-email.jsx
@@ -1,51 +1,52 @@
 /**
  * External dependencies
  */
-var React = require( 'react' ),
-	assign = require( 'lodash/assign' );
+import React, { Component } from 'react';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-var AccountRecoveryStore = require( 'lib/security-checkup/account-recovery-store' ),
-	SecurityCheckupActions = require( 'lib/security-checkup/actions' ),
-	ManageContact = require( './manage-contact' ),
-	EditEmail = require( './edit-email' ),
-	accept = require( 'lib/accept' );
+import AccountRecoveryStore from 'lib/security-checkup/account-recovery-store';
+import SecurityCheckupActions from 'lib/security-checkup/actions';
+import ManageContact from './manage-contact';
+import EditEmail from './edit-email';
+import accept from 'lib/accept';
 
-module.exports = React.createClass( {
-	displayName: 'SecurityCheckupRecoveryEmail',
+class RecoveryEmail extends Component {
+	constructor( props ) {
+		super( props );
 
-	componentDidMount: function() {
+		this.state = this.getDataFromStores();
+	}
+
+	componentDidMount() {
 		AccountRecoveryStore.on( 'change', this.refreshData );
-	},
+	}
 
-	componentWillUnmount: function() {
+	componentWillUnmount() {
 		AccountRecoveryStore.off( 'change', this.refreshData );
-	},
+	}
 
-	getInitialState: function() {
-		return assign( {}, this.getDataFromStores() );
-	},
-
-	refreshData: function() {
+	refreshData = () => {
 		this.setState( this.getDataFromStores() );
-	},
+	}
 
-	getDataFromStores: function() {
+	getDataFromStores() {
 		return AccountRecoveryStore.getEmail();
-	},
+	}
 
-	render: function() {
-		var email = this.state.data ? this.state.data.email : false,
-			primaryEmail = this.props.userSettings.getSetting( 'user_email' );
+	render() {
+		const email = this.state.data ? this.state.data.email : false;
+		const primaryEmail = this.props.userSettings.getSetting( 'user_email' );
+		const { translate } = this.props;
 
 		return (
 			<ManageContact
 				type="email"
 				isLoading={ this.state.loading }
-				title={ this.translate( 'Recovery Email Address' ) }
-				subtitle={ email ? email : this.translate( 'Not set' ) }
+				title={ translate( 'Recovery Email Address' ) }
+				subtitle={ email ? email : translate( 'Not set' ) }
 				hasValue={ !! email }
 				lastNotice={ this.state.lastNotice }
 
@@ -59,25 +60,23 @@ module.exports = React.createClass( {
 						/>
 				</ManageContact>
 		);
-	},
+	}
 
-	haveEmail: function() {
-		return !! this.state.data.email;
-	},
-
-	onSave: function( email ) {
+	onSave = ( email ) => {
 		SecurityCheckupActions.updateEmail( email, this.state.data.email );
-	},
+	}
 
-	onDelete: function() {
-		accept( this.translate( 'Are you sure you want to remove the email address?' ), function( accepted ) {
+	onDelete = () => {
+		accept( this.props.translate( 'Are you sure you want to remove the email address?' ), function( accepted ) {
 			if ( accepted ) {
 				SecurityCheckupActions.deleteEmail();
 			}
 		} );
-	},
+	}
 
-	onDismissNotice: function() {
+	onDismissNotice = () => {
 		SecurityCheckupActions.dismissEmailNotice();
 	}
-} );
+}
+
+export default localize( RecoveryEmail );
