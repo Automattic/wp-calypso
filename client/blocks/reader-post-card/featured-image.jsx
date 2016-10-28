39678150a4338b0d66329570b30cfaa558b57fad
Chris Rosser <chris@bluefuton.com>
2016-10-28 15:33:30 +1300

    Reader Stream Refresh: split featured asset components into separate files (#8970)
    * Split featured asset components into separate files
    
    * Add prop types
    
    * Use localize HOC on featured video

---
+++ client/blocks/reader-post-card/featured-image.jsx
@@ -0,0 +1,37 @@
+/**
+ * External Dependencies
+ */
+import React from 'react';
+import { noop } from 'lodash';
+
+const FeaturedImage = ( { imageUri, href, children, onClick } ) => {
+	if ( imageUri === undefined ) {
+		return null;
+	}
+
+	const featuredImageStyle = {
+		backgroundImage: 'url(' + imageUri + ')',
+		backgroundSize: 'cover',
+		backgroundRepeat: 'no-repeat',
+		backgroundPosition: '50% 50%'
+	};
+
+	return (
+		<a className="reader-post-card__featured-image" href={ href } style={ featuredImageStyle } onClick={ onClick } >
+			{ children }
+		</a>
+	);
+};
+
+FeaturedImage.propTypes = {
+	imageUri: React.PropTypes.string,
+	href: React.PropTypes.string,
+	onClick: React.PropTypes.func,
+};
+
+FeaturedImage.defaultProps = {
+	onClick: noop,
+};
+
+export default FeaturedImage;
+
