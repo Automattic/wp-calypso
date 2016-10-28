662a64376c418bcc589d3efe70bc025935356d58
Marin Atanasov <tyxla@abv.bg>
2016-10-28 16:14:32 +0300

    Jetpack Connect: Introduce expiration parameter to isStale()

--- client/state/jetpack-connect/utils.js
+++ client/state/jetpack-connect/utils.js
@@ -6,12 +6,13 @@ import { JETPACK_CONNECT_TTL } from './constants';
 /***
  * Whether a Jetpack Connect store timestamp is stale.
  * @param   {Number} timestamp  Item to check.
+ * @param   {Number} expiration Expiration to compare with, in milliseconds. Default is JETPACK_CONNECT_TTL.
  * @returns {Boolean}           True if the timestamp is stale, false otherwise.
  */
-export function isStale( timestamp ) {
+export function isStale( timestamp, expiration = JETPACK_CONNECT_TTL ) {
 	const now = new Date().getTime();
 	if ( ! timestamp ) {
 		return false;
 	}
-	return ( now - timestamp ) >= JETPACK_CONNECT_TTL;
+	return ( now - timestamp ) >= expiration;
 }
