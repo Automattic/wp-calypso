d65cabd4e01efb632c9994bb01ad91d78bdd9254
Marin Atanasov <tyxla@abv.bg>
2016-10-28 16:14:32 +0300

    Jetpack Connect: Introduce expiration parameter to isStale()

--- client/state/jetpack-connect/test/utils.js
+++ client/state/jetpack-connect/test/utils.js
@@ -29,5 +29,13 @@ describe( 'utils', () => {
 		it( 'should return true if the passed timestamp is stale', () => {
 			expect( isStale( 1 ) ).to.be.true;
 		} );
+
+		it( 'should return false if the timestamp is not stale with a specific expiration', () => {
+			expect( isStale( new Date().getTime(), 60 ) ).to.be.false;
+		} );
+
+		it( 'should return true if the timestamp is stale with a specific expiration', () => {
+			expect( isStale( new Date().getTime() - 61, 60 ) ).to.be.true;
+		} );
 	} );
 } );
