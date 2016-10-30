4ee40ce9fe101e552d3155a052aec2f4c7cd5b4f
artpi <artpi@post.pl>
2016-10-30 13:53:14 -0500

    Hook up image editor

--- client/my-sites/media/main.jsx
+++ client/my-sites/media/main.jsx
@@ -12,6 +12,9 @@ import SidebarNavigation from 'my-sites/sidebar-navigation';
 import observe from 'lib/mixins/data-observe';
 import Dialog from 'components/dialog';
 import { EditorMediaModalDetail } from 'post-editor/media-modal/detail';
+import ImageEditor from 'blocks/image-editor';
+import MediaActions from 'lib/media/actions';
+import MediaUtils from 'lib/media/utils';
 
 export default React.createClass( {
 	displayName: 'Media',
@@ -24,7 +27,8 @@ export default React.createClass( {
 
 	getInitialState: function() {
 		return {
-			editedItem: null
+			editedItem: null,
+			openedDetails: null,
 		};
 	},
 
@@ -49,11 +53,51 @@ export default React.createClass( {
 	},
 
 	openDetailsModal( item ) {
-		this.setState( { editedItem: item } );
+		this.setState( { openedDetails: item } );
 	},
 
 	closeDetailsModal() {
-		this.setState( { editedItem: null } );
+		this.setState( { openedDetails: null, editedItem: null } );
+	},
+
+	editImage() {
+		this.setState( { openedDetails: null, editedItem: this.state.openedDetails } );
+	},
+
+	onImageEditorCancel: function( imageEditorProps ) {
+		const {	resetAllImageEditorState } = imageEditorProps;
+		this.setState( { openedDetails: this.state.editedItem, editedItem: null } );
+
+		resetAllImageEditorState();
+	},
+	onImageEditorDone( error, blob, imageEditorProps ) {
+		if ( error ) {
+			this.onEditImageCancel( imageEditorProps );
+
+			return;
+		}
+
+		const {
+			fileName,
+			site,
+			ID,
+			resetAllImageEditorState
+		} = imageEditorProps;
+
+		const mimeType = MediaUtils.getMimeType( fileName );
+
+		const item = {
+			ID: ID,
+			media: {
+				fileName: fileName,
+				fileContents: blob,
+				mimeType: mimeType
+			}
+		};
+
+		MediaActions.update( site.ID, item, true );
+		resetAllImageEditorState();
+		this.setState( { openedDetails: null, editedItem: null } );
 	},
 
 	render: function() {
@@ -61,20 +105,30 @@ export default React.createClass( {
 		return (
 			<div ref="container" className="main main-column media" role="main">
 				<SidebarNavigation />
-				{ this.state.editedItem &&
+				{ ( this.state.editedItem || this.state.openedDetails ) &&
 					<Dialog
 						isVisible={ true }
 						additionalClassNames="editor-media-modal"
 						onClickOutside={ this.closeDetailsModal }
 						onClose={ this.closeDetailsModal }
 					>
-
+					{ this.state.openedDetails &&
 						<EditorMediaModalDetail
 							site={ site }
-							items={ [ this.state.editedItem ] }
+							items={ [ this.state.openedDetails ] }
 							selectedIndex={ 0 }
 							onReturnToList={ this.closeDetailsModal }
+							onEditItem={ this.editImage }
 						/>
+					}
+					{ this.state.editedItem &&
+						<ImageEditor
+							siteId={ site && site.ID }
+							media={ this.state.editedItem }
+							onDone={ this.onImageEditorDone }
+							onCancel={ this.onImageEditorCancel }
+						/>
+					}
 					</Dialog>
 				}
 				<MediaLibrary
