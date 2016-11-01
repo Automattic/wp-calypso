61204f7afb02ee7e92038608b5f2032f95b121f2
Valerie <valerie@automattic.com>
2016-11-01 19:40:24 +0200

    Gravatar: Show local image after upload

--- client/state/current-user/gravatar-status/test/selectors.js
+++ client/state/current-user/gravatar-status/test/selectors.js
@@ -8,7 +8,8 @@ import { set } from 'lodash';
  * Internal dependencies
  */
 import {
-	isCurrentUserUploadingGravatar
+	isCurrentUserUploadingGravatar,
+	getUserTempGravatar,
 } from '../selectors';
 
 describe( 'selectors', () => {
@@ -27,6 +28,46 @@ describe( 'selectors', () => {
 			set( notUploadingState, 'currentUser.gravatarStatus.isUploading', false );
 			expect( isCurrentUserUploadingGravatar( notUploadingState ) )
 				.to.equal( false );
+		} );
+	} );
+
+	describe( '#getUserTempGravatar', () => {
+		it( 'returns false if user ID is not passed in', () => {
+			const state = {};
+			set( state, 'currentUser.gravatarStatus.tempImage.src', 'image' );
+			set( state, 'currentUser.id', 987612 );
+			expect( getUserTempGravatar( state ) ).to.equal( false );
+		} );
+
+		it( 'returns false if the user ID passed is not the current user ID', () => {
+			const state = {};
+			set( state, 'currentUser.gravatarStatus.tempImage.src', 'image' );
+			set( state, 'currentUser.id', 1 );
+			expect( getUserTempGravatar( state, 2 ) ).to.equal( false );
+		} );
+
+		it( 'returns false if the current user does not have temp image set', () => {
+			const currentUserId = 1;
+
+			const emptyTempImage = {};
+			set( emptyTempImage, 'currentUser.id', currentUserId );
+			set( emptyTempImage, 'currentUser.gravatarStatus.tempImage', {} );
+			expect( getUserTempGravatar( emptyTempImage, currentUserId ) )
+				.to.equal( false );
+
+			const missingGravatarStatus = {};
+			set( missingGravatarStatus, 'currentUser.id', currentUserId );
+			expect( getUserTempGravatar( missingGravatarStatus, currentUserId ) )
+				.to.equal( false );
+		} );
+
+		it( 'returns image src if given the current user ID, and the current user has a temp image set', () => {
+			const currentUserId = 1;
+			const state = {};
+			const imageSrc = 'image';
+			set( state, 'currentUser.gravatarStatus.tempImage.src', imageSrc );
+			set( state, 'currentUser.id', currentUserId );
+			expect( getUserTempGravatar( state, currentUserId ) ).to.equal( imageSrc );
 		} );
 	} );
 } );
