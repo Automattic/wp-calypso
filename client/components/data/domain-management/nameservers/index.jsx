42818d1d3682526559ef4541ebfee883d6e65a80
Dennis Snell <dennis.snell@automattic.com>
2016-10-31 18:05:57 -0700

    Remove DomainsStore from DomainManagement.Nameservers
    After switching over to remove the call to `sites-list` in the
    nameserver configuration component we started receiving an error when
    navigating back from the DNS settings to the nameserver config panel.
    
    This was caused by a store listener in the `<StoreConnection />`
    component which was causing `setState()` _inside_ of the `render()`
    cycle.
    
    To fix this we had to stop listening to the domain store and start using
    the domain selectors with the `<QuerySiteDomains />` component.
    
    There is now no longer any error.

--- client/components/data/domain-management/nameservers/index.jsx
+++ client/components/data/domain-management/nameservers/index.jsx
@@ -7,29 +7,25 @@ import { connect } from 'react-redux';
 /**
  * Internal dependencies
  */
-import DomainsStore from 'lib/domains/store';
 import NameserversStore from 'lib/domains/nameservers/store';
+import QuerySiteDomains from 'components/data/query-site-domains';
 import StoreConnection from 'components/data/store-connection';
-import { fetchDomains, fetchNameservers } from 'lib/upgrades/actions';
+import { fetchNameservers } from 'lib/upgrades/actions';
+import { getDomainsBySite } from 'state/sites/domains/selectors';
 import { getSelectedSite } from 'state/ui/selectors';
 
 const stores = [
-	DomainsStore,
 	NameserversStore
 ];
 
 function getStateFromStores( props ) {
-	let domains;
-
-	if ( props.selectedSite ) {
-		domains = DomainsStore.getBySite( props.selectedSite.ID );
-	}
-
 	return {
-		domains,
+		domains: props.domains,
 		nameservers: NameserversStore.getByDomainName( props.selectedDomainName ),
 		selectedDomainName: props.selectedDomainName,
-		selectedSite: props.selectedSite
+		// undefined is not permitted by the passed component
+		// so we have to explicitly choose a bool in that case
+		selectedSite: props.selectedSite || false,
 	};
 }
 
@@ -43,42 +39,55 @@ export class NameserversData extends Component {
 	constructor( props ) {
 		super( props );
 
-		if ( props.selectedSite ) {
-			this.loadDomains( props.selectedSite.ID );
-		}
-
 		this.loadNameservers();
 	}
 
-	componentWillUpdate( nextProps ) {
-		const { selectedSite: nextSite } = nextProps;
-		const { selectedSite: prevSite } = this.props;
-
-		if ( nextSite && nextSite !== prevSite ) {
-			this.loadDomains( nextSite.ID );
-		}
-
+	componentWillUpdate() {
 		this.loadNameservers();
 	}
 
-	loadDomains = siteId => fetchDomains( siteId );
 	loadNameservers = () => fetchNameservers( this.props.selectedDomainName );
 
 	render() {
+		const { selectedSite } = this.props;
+
 		return (
-			<StoreConnection
-				component={ this.props.component }
-				stores={ stores }
-				getStateFromStores={ getStateFromStores }
-				selectedDomainName={ this.props.selectedDomainName }
-				selectedSite={ this.props.selectedSite }
-			/>
+			<div>
+				<QuerySiteDomains siteId={ selectedSite && selectedSite.ID } />
+				<StoreConnection
+					component={ this.props.component }
+					domains={ this.props.domains }
+					stores={ stores }
+					getStateFromStores={ getStateFromStores }
+					selectedDomainName={ this.props.selectedDomainName }
+					selectedSite={ selectedSite }
+				/>
+			</div>
 		);
 	}
 }
 
-const mapStateToProps = state => ( {
-	selectedSite: getSelectedSite( state ),
-} );
+const mapStateToProps = state => {
+	const selectedSite = getSelectedSite( state );
+	const domains = getDomainsBySite( state, selectedSite );
+
+	return {
+		domains: {
+			// this doesn't appear to be currently
+			// stored in our app state as it was in
+			// the domains store. we know if we are
+			// currently fetching, but not if a fetch
+			// has taken place. in this case, I'm
+			// using the length of the array to indicate
+			// `hasLoaded` because this component should
+			// never exist in the app without at least
+			// one domain. thus, if we have at least one
+			// then we have loaded them.
+			hasLoadedFromServer: !! domains.length,
+			list: domains,
+		},
+		selectedSite
+	};
+};
 
 export default connect( mapStateToProps )( NameserversData );
