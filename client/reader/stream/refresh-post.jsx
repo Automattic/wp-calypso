acc8a9e619a513ade81be7f178e22c43e0055761
Chris Rosser <chris@bluefuton.com>
2016-11-03 12:21:21 +1300

    Open original post on Discover where available (#9080)

--- client/reader/stream/refresh-post.jsx
+++ client/reader/stream/refresh-post.jsx
@@ -13,11 +13,14 @@ import { getSite } from 'state/reader/sites/selectors';
 import { getFeed } from 'state/reader/feeds/selectors';
 import QueryReaderSite from 'components/data/query-reader-site';
 import QueryReaderFeed from 'components/data/query-reader-feed';
+import * as DiscoverHelper from 'reader/discover/helper';
+import FeedPostStore from 'lib/feed-post-store';
+import smartSetState from 'lib/react-smart-set-state';
 
-export class ReaderPostCardAdapter extends React.Component {
+class ReaderPostCardAdapter extends React.Component {
 
-	onClick = () => {
-		this.props.handleClick && this.props.handleClick( this.props.post );
+	onClick = ( postToOpen ) => {
+		this.props.handleClick && this.props.handleClick( postToOpen );
 	}
 
 	onCommentClick = () => {
@@ -38,6 +41,7 @@ export class ReaderPostCardAdapter extends React.Component {
 		return (
 			<ReaderPostCard
 				post={ this.props.post }
+				originalPost={ this.props.originalPost }
 				site={ this.props.site }
 				feed={ this.props.feed }
 				onClick={ this.onClick }
@@ -50,14 +54,70 @@ export class ReaderPostCardAdapter extends React.Component {
 	}
 }
 
-export default connect(
+const ConnectedReaderPostCardAdapter = connect(
 	( state, ownProps ) => {
 		const siteId = get( ownProps, 'post.site_ID' );
 		const isExternal = get( ownProps, 'post.is_external' );
 		const feedId = get( ownProps, 'post.feed_ID' );
 		return {
 			site: isExternal ? null : getSite( state, siteId ),
-			feed: getFeed( state, feedId )
+			feed: getFeed( state, feedId ),
+			originalPost: ownProps.originalPost
 		};
 	}
 )( ReaderPostCardAdapter );
+
+/**
+ * A container for the ReaderPostCardAdapter responsible for binding to Flux stores
+ */
+export default class ReaderPostCardAdapterFluxContainer extends React.Component {
+	constructor( props ) {
+		super( props );
+		this.state = this.getStateFromStores( props );
+		this.smartSetState = smartSetState;
+	}
+
+	getStateFromStores( props = this.props ) {
+		const post = props.post;
+		const isDiscoverPost = post && DiscoverHelper.isDiscoverPost( post );
+		let isDiscoverSitePick = false;
+		if ( isDiscoverPost ) {
+			isDiscoverSitePick = post && DiscoverHelper.isDiscoverSitePick( post );
+		}
+
+		// If it's a discover post (but not a site pick), we want the original post too
+		let originalPost;
+		if ( isDiscoverPost && ! isDiscoverSitePick && get( post, 'discover_metadata.featured_post_wpcom_data.blog_id' ) ) {
+			originalPost = FeedPostStore.get( {
+				blogId: post.discover_metadata.featured_post_wpcom_data.blog_id,
+				postId: post.discover_metadata.featured_post_wpcom_data.post_id
+			} );
+		}
+
+		return {
+			originalPost
+		};
+	}
+
+	updateState = ( newState = this.getStateFromStores() ) => {
+		this.smartSetState( newState );
+	}
+
+	componentWillMount() {
+		FeedPostStore.on( 'change', this.updateState );
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		this.updateState( this.getStateFromStores( nextProps ) );
+	}
+
+	componentWillUnmount() {
+		FeedPostStore.off( 'change', this.updateState );
+	}
+
+	render() {
+		return ( <ConnectedReaderPostCardAdapter
+					{ ...this.props }
+					originalPost={ this.state.originalPost } /> );
+	}
+}
