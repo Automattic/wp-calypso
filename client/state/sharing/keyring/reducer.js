c1092cc93bf1d2357dd0247d0e81abb59ed322d9
Konstantin Obenland <obenland@gmx.de>
2016-11-02 09:55:30 -0700

    Limit variable destruction to one level deep.
    See
    https://github.com/Automattic/wp-calypso/pull/9068#discussion_r86141197

--- client/state/sharing/keyring/reducer.js
+++ client/state/sharing/keyring/reducer.js
@@ -28,17 +28,19 @@ export const isFetching = createReducer( false, {
 // Stores the list of available keyring connections
 export const items = createReducer( {}, {
 	[ KEYRING_CONNECTIONS_RECEIVE ]: ( state, { connections } ) => ( { ...keyBy( connections, 'ID' ) } ),
-	[ PUBLICIZE_CONNECTION_CREATE ]: ( state, { connection: { keyring_connection_ID: id, site_ID } } ) => {
-		const connection = state[ id ];
-		const sites = [ ...connection.sites, site_ID.toString() ];
+	[ PUBLICIZE_CONNECTION_CREATE ]: ( state, { connection } ) => {
+		const { keyring_connection_ID: id, site_ID: siteId } = connection;
+		const keyringConnection = state[ id ];
+		const sites = [ ...keyringConnection.sites, siteId.toString() ];
 
-		return { ...state, [ id ]: { ...connection, sites } };
+		return { ...state, [ id ]: { ...keyringConnection, sites } };
 	},
-	[ PUBLICIZE_CONNECTION_DELETE ]: ( state, { connection: { keyring_connection_ID: id, site_ID } } ) => {
-		const connection = state[ id ];
-		const sites = without( connection.sites, site_ID.toString() );
+	[ PUBLICIZE_CONNECTION_DELETE ]: ( state, { connection } ) => {
+		const { keyring_connection_ID: id, site_ID: siteId } = connection;
+		const keyringConnection = state[ id ];
+		const sites = without( keyringConnection.sites, siteId.toString() );
 
-		return { ...state, [ id ]: { ...connection, sites } };
+		return { ...state, [ id ]: { ...keyringConnection, sites } };
 	},
 }, itemSchema );
 
