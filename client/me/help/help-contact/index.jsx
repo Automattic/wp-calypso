70ec78616b4786a8f9a1d9186ed9282dffee9a93
Marcus Kazmierczak <marcus@mkaz.com>
2016-11-03 13:24:11 -0700

    Add Happychat button label to staging environment

--- client/me/help/help-contact/index.jsx
+++ client/me/help/help-contact/index.jsx
@@ -481,7 +481,7 @@ const HelpContact = React.createClass( {
 			},
 			showHappychatVariation && {
 				onSubmit: this.startHappychat,
-				buttonLabel: ( config( 'env' ) === 'development' ) ? 'Happychat with us' : this.translate( 'Chat with us' ) 
+				buttonLabel: ( ( config( 'env' ) === 'development' ) || ( config( 'env' ) === 'staging' ) ) ? 'Happychat' : this.translate( 'Chat with us' )
 			}
 		);
 
