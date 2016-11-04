8effd111e29545218c23e8a305a088687b2bc6a1
Andrija Vucinic <andrija.vucinic@gmail.com>
2016-11-05 00:38:11 +0100

    Happiness Engineers: Add selector to check if we received HEs
    - Add selector to check if we have loaded the happiness engineers
    from network
    - Add tests

--- client/state/happiness-engineers/selectors.js
+++ client/state/happiness-engineers/selectors.js
@@ -1,6 +1,7 @@
 /**
  * Returns happiness engineers
- * @param {{}} state - Previous state
+ *
+ * @param {{}} state currents state
  * @returns {Array} happiness engineers
  */
 export function getHappinessEngineers( state ) {
@@ -9,9 +10,20 @@ export function getHappinessEngineers( state ) {
 
 /**
  * Returns whether we have loaded some data. Always false when fetching data
- * @param {{}} state - Previous state
- * @returns {boolean} - Whether we loaded some data or not
+ *
+ * @param {{}} state currents state
+ * @returns {boolean} Whether we loaded some data or not
  */
 export function isRequestingHappinessEngineers( state ) {
 	return state.happinessEngineers.requesting;
+}
+
+/**
+ * Check if we have happiness engineers loaded
+ *
+ * @param {{}} state currents state
+ * @returns {boolean} true if loaded, false otherwise
+ */
+export function hasReceivedHappinessEngineers( state ) {
+	return state.happinessEngineers.items !== null;
 }
