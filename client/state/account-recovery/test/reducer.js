1712c3e8ece46fd06436dd8345bbc16c0da49cd1
James Tien <southp0105@gmail.com>
2016-11-04 15:31:52 +0800

    Populate `isFetching: true` on receiving `ACCOUNT_RECOVERY_FETCH`, and set it as `false` on receiving `ACCOUNT_RECOVERY_FETCH_SUCCESS` or `ACCOUNT_RECOVERY_FETCH_FAILED`.

--- client/state/account-recovery/test/reducer.js
+++ client/state/account-recovery/test/reducer.js
@@ -9,7 +9,9 @@ import deepFreeze from 'deep-freeze';
  */
 
 import {
+	ACCOUNT_RECOVERY_FETCH,
 	ACCOUNT_RECOVERY_FETCH_SUCCESS,
+	ACCOUNT_RECOVERY_FETCH_FAILED,
 } from 'state/action-types';
 
 import dummyData from './test-data';
@@ -21,6 +23,7 @@ describe( 'account-recovery reducer', () => {
 		emailValidated: dummyData.email_validated,
 		phone: dummyData.phone,
 		phoneValidated: dummyData.phone_validated,
+		isFetching: false,
 	};
 
 	it( 'should return an initial object with the settings data.', () => {
@@ -47,5 +50,30 @@ describe( 'account-recovery reducer', () => {
 			...prevState,
 			...expectedState,
 		} );
+	} );
+
+	it( 'should populate isFetching in the state', () => {
+		const state = reducer( null, {
+			type: ACCOUNT_RECOVERY_FETCH,
+		} );
+
+		assert( state.isFetching );
+	} );
+
+	it( 'ACCOUNT_RECOVERY_FETCH_SUCCESS action should set isFetching to false', () => {
+		const state = reducer( { isFetching: true }, {
+			type: ACCOUNT_RECOVERY_FETCH_SUCCESS,
+			...dummyData,
+		} );
+
+		assert.isFalse( state.isFetching );
+	} );
+
+	it( 'ACCOUNT_RECOVERY_FETCH_FAILED action should set isFetching to false', () => {
+		const state = reducer( { isFetching: true }, {
+			type: ACCOUNT_RECOVERY_FETCH_FAILED,
+		} );
+
+		assert.isFalse( state.isFetching );
 	} );
 } );
