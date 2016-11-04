0f5ea7aec31827fa7a92663315beeb7a5961f5dc
Jan Cavan Boulas <mj12982@gmail.com>
2016-11-04 10:28:10 -0700

    Reader: Update RP on DevDocs

--- client/blocks/reader-related-card-v2/docs/example.jsx
+++ client/blocks/reader-related-card-v2/docs/example.jsx
@@ -32,48 +32,25 @@ const smallItems = [
 			title: 'All the catsss'
 		}
 	},
-	{
-		post: {
-			ID: 2,
-			title: 'No Site? No Problem.',
-			site_ID: 99,
-			site_name: '99 Problems',
-			global_ID: 2,
-			author: {
-				name: 'wolvy'
-			},
-			canonical_image: {
-				uri: 'http://lorempixel.com/1024/256/sports/',
-				height: 256,
-				width: 1024
-			},
-			site_URL: 'http://discover.wordpress.com',
-			short_excerpt: 'Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko.'
-		},
-		site: {
-			title: '99 Problems'
-		}
-	},
 ];
 
 const RelatedPostCardv2Example = () => (
 	<div className="design-assets__group">
 		<Card>
-			<div className="reader-related-card-v2__container">
-				<h1 className="reader-related-card-v2__heading">More in <a href="#" className="reader-related-card-v2__link">Cats and Cats</a></h1>
-				<div className="reader-related-card-v2__posts is-site-post">
-					{ smallItems.map( item => <RelatedPostCard key={ item.post.global_ID } post={ item.post } site={ item.site } /> ) }
-				</div>
-			</div>
-		</Card>
-
-		<Card>
-			<div className="reader-related-card-v2__container">
-				<h1 className="reader-related-card-v2__heading">More in <a href="#" className="reader-related-card-v2__link">WordPress.com</a></h1>
-				<div className="reader-related-card-v2__posts is-global-post">
-					{ smallItems.map( item => <RelatedPostCard key={ item.post.global_ID } post={ item.post } site={ item.site } /> ) }
-				</div>
-			</div>
+		<div className="reader-related-card-v2__blocks is-same-site">
+			<h1 className="reader-related-card-v2__heading">More in <a className="reader-related-card-v2__link">Cats and Furballs</a></h1>
+				<ul className="reader-related-card-v2__list">
+					<li className="reader-related-card-v2__list-item">{ smallItems.map( item => <RelatedPostCard key={ item.post.global_ID } post={ item.post } site={ item.site } /> ) }</li>
+					<li className="reader-related-card-v2__list-item">{ smallItems.map( item => <RelatedPostCard key={ item.post.global_ID } post={ item.post } site={ item.site } /> ) }</li>
+				</ul>
+		</div>
+		<div className="reader-related-card-v2__blocks is-other-site">
+			<h1 className="reader-related-card-v2__heading">More in <a className="reader-related-card-v2__link">WordPress.com</a></h1>
+				<ul className="reader-related-card-v2__list">
+					<li className="reader-related-card-v2__list-item">{ smallItems.map( item => <RelatedPostCard key={ item.post.global_ID } post={ item.post } site={ item.site } /> ) }</li>
+					<li className="reader-related-card-v2__list-item">{ smallItems.map( item => <RelatedPostCard key={ item.post.global_ID } post={ item.post } site={ item.site } /> ) }</li>
+				</ul>
+		</div>
 		</Card>
 	</div>
 );
