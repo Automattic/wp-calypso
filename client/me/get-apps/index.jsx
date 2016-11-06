eb5fcc84384a7c8504d59b8da7946744f6293e94
jchimienti89 <jchimien@mail.usf.edu>
2016-11-06 06:59:02 -0500

    change translate to i18n-calypso
    before: this.translate

--- client/me/get-apps/index.jsx
+++ client/me/get-apps/index.jsx
@@ -2,6 +2,8 @@
  * External dependencies
  */
 import React from 'react';
+import { translate } from 'i18n-calypso';
+
 
 /**
  * Internal dependencies
@@ -20,55 +22,55 @@ module.exports = React.createClass( {
 		return (
 			<Main className="get-apps">
 				<MeSidebarNavigation />
-				<SectionHeader label={ this.translate( 'Desktop apps' ) } />
+				<SectionHeader label={ translate( 'Desktop apps' ) } />
 				<Card className="get-apps__desktop">
 
 					<section className="get-apps__app">
 						<div className="get-apps__app-icon">
-							<img src="/calypso/images/me/get-apps-osx.svg" alt={ this.translate( 'Mac' ) } width="96" height="96" />
+							<img src="/calypso/images/me/get-apps-osx.svg" alt={ translate( 'Mac' ) } width="96" height="96" />
 						</div>
-						<p><strong>{ this.translate( 'Mac' ) }</strong><br />
-						{ this.translate( 'Requires 10.11 or newer' ) }</p>
-						<Button href="https://apps.wordpress.com/d/osx?ref=getapps">{ this.translate( 'Download' ) }</Button>
+						<p><strong>{ translate( 'Mac' ) }</strong><br />
+						{ translate( 'Requires 10.11 or newer' ) }</p>
+						<Button href="https://apps.wordpress.com/d/osx?ref=getapps">{ translate( 'Download' ) }</Button>
 					</section>
 
 					<section className="get-apps__app">
 						<div className="get-apps__app-icon">
-							<img src="/calypso/images/me/get-apps-windows.svg" alt={ this.translate( 'Windows' ) } width="96" height="96" />
+							<img src="/calypso/images/me/get-apps-windows.svg" alt={ translate( 'Windows' ) } width="96" height="96" />
 						</div>
-						<p><strong>{ this.translate( 'Windows' ) }</strong><br />
-						{ this.translate( 'Requires 7 or newer' ) }</p>
-						<Button href="https://apps.wordpress.com/d/windows?ref=getapps">{ this.translate( 'Download' ) }</Button>
+						<p><strong>{ translate( 'Windows' ) }</strong><br />
+						{ translate( 'Requires 7 or newer' ) }</p>
+						<Button href="https://apps.wordpress.com/d/windows?ref=getapps">{ translate( 'Download' ) }</Button>
 					</section>
 
 					<section className="get-apps__app">
 						<div className="get-apps__app-icon">
-							<img src="/calypso/images/me/get-apps-linux.svg" alt={ this.translate( 'Linux' ) } width="96" height="96" />
+							<img src="/calypso/images/me/get-apps-linux.svg" alt={ translate( 'Linux' ) } width="96" height="96" />
 						</div>
-						<p><strong>{ this.translate( 'Linux' ) }</strong><br />
-						{ this.translate( 'Choose your distribution' ) }</p>
-						<Button href="https://apps.wordpress.com/d/linux?ref=getapps">{ this.translate( '.TAR.GZ' ) }</Button> <Button href="https://apps.wordpress.com/d/linux-deb?ref=getapps">{ this.translate( '.DEB' ) }</Button>
+						<p><strong>{ translate( 'Linux' ) }</strong><br />
+						{ translate( 'Choose your distribution' ) }</p>
+						<Button href="https://apps.wordpress.com/d/linux?ref=getapps">{ translate( '.TAR.GZ' ) }</Button> <Button href="https://apps.wordpress.com/d/linux-deb?ref=getapps">{ translate( '.DEB' ) }</Button>
 					</section>
 
 				</Card>
 
-				<SectionHeader label={ this.translate( 'Mobile apps' ) } />
+				<SectionHeader label={ translate( 'Mobile apps' ) } />
 				<Card className="get-apps__mobile">
 
 					<section className="get-apps__app">
 						<div className="get-apps__app-icon">
-							<img src="/calypso/images/me/get-apps-ios.svg" alt={ this.translate( 'iOS' ) } width="96" height="96" />
+							<img src="/calypso/images/me/get-apps-ios.svg" alt={ translate( 'iOS' ) } width="96" height="96" />
 						</div>
-						<p><strong>{ this.translate( 'iOS' ) }</strong></p>
-						<a href="https://itunes.apple.com/us/app/wordpress/id335703880?mt=8"><img src="/calypso/images/me/get-apps-app-store.png" alt={ this.translate( 'Get on the iOS App Store' ) } /></a>
+						<p><strong>{ translate( 'iOS' ) }</strong></p>
+						<a href="https://itunes.apple.com/us/app/wordpress/id335703880?mt=8"><img src="/calypso/images/me/get-apps-app-store.png" alt={ translate( 'Get on the iOS App Store' ) } /></a>
 					</section>
 
 					<section className="get-apps__app">
 						<div className="get-apps__app-icon">
-							<img src="/calypso/images/me/get-apps-android.svg" alt={ this.translate( 'Android' ) } width="96" height="96" />
+							<img src="/calypso/images/me/get-apps-android.svg" alt={ translate( 'Android' ) } width="96" height="96" />
 						</div>
-						<p><strong>{ this.translate( 'Android' ) }</strong></p>
-						<a href="https://play.google.com/store/apps/details?id=org.wordpress.android"><img src="/calypso/images/me/get-apps-google-play.png" alt={ this.translate( 'Get on Google Play' ) } /></a>
+						<p><strong>{ translate( 'Android' ) }</strong></p>
+						<a href="https://play.google.com/store/apps/details?id=org.wordpress.android"><img src="/calypso/images/me/get-apps-google-play.png" alt={ translate( 'Get on Google Play' ) } /></a>
 					</section>
 
 				</Card>
