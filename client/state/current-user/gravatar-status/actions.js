3c8d21d25000a3b48fab4887d87a9a04b3a915c0
Valerie <valerie@automattic.com>
2016-11-07 22:01:28 +0100

    Me: Add Gravatar uploading to profile section

---
+++ client/state/current-user/gravatar-status/actions.js
@@ -0,0 +1,40 @@
+/**
+ * External dependencies
+ */
+import request from 'superagent';
+
+/**
+ * Internal dependencies
+ */
+import {
+	GRAVATAR_UPLOAD_RECEIVE,
+	GRAVATAR_UPLOAD_REQUEST,
+	GRAVATAR_UPLOAD_REQUEST_SUCCESS,
+	GRAVATAR_UPLOAD_REQUEST_FAILURE
+} from 'state/action-types';
+
+export function uploadGravatar( file, bearerToken, email ) {
+	return dispatch => {
+		dispatch( { type: GRAVATAR_UPLOAD_REQUEST } );
+		const data = new FormData();
+		data.append( 'filedata', file );
+		data.append( 'account', email );
+		return request
+			.post( 'https://api.gravatar.com/v1/upload-image' )
+			.send( data )
+			.set( 'Authorization', 'Bearer ' + bearerToken )
+			.then( () => {
+				dispatch( {
+					type: GRAVATAR_UPLOAD_RECEIVE
+				} );
+				dispatch( {
+					type: GRAVATAR_UPLOAD_REQUEST_SUCCESS
+				} );
+			} )
+			.catch( () => {
+				dispatch( {
+					type: GRAVATAR_UPLOAD_REQUEST_FAILURE
+				} );
+			} );
+	};
+}
