2a1dd0a52a847ee0e1035c1994d9c4502f85dc99
Marin Atanasov <tyxla@users.noreply.github.com>
2016-11-08 13:15:38 +0200

    Jetpack Connect: Increase authorization data TTL back to 1 hour (#9211)

--- client/state/jetpack-connect/constants.js
+++ client/state/jetpack-connect/constants.js
@@ -1,2 +1,2 @@
-export const JETPACK_CONNECT_TTL = 60 * 60 * 1000; // an hour
-export const JETPACK_CONNECT_AUTHORIZE_TTL = 5 * 60 * 1000; // 5 minutes
+export const JETPACK_CONNECT_TTL = 60 * 60 * 1000; // 1 hour
+export const JETPACK_CONNECT_AUTHORIZE_TTL = 60 * 60 * 1000; // 1 hour
