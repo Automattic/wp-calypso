5446efed1a71dcec8a06873ba61fe6f2a1f53ff3
Jake Fried <jzfisgreat@gmail.com>
2016-11-07 21:07:13 -0800

    addressed some of blowery's offline comments

--- client/blocks/reader-post-card/featured-asset.jsx
+++ client/blocks/reader-post-card/featured-asset.jsx
@@ -2,7 +2,8 @@
  * External Dependencies
  */
 import React from 'react';
-import { find } from 'lodash';
+import { find, endsWith, some, } from 'lodash';
+import url from 'url';
 
 /**
  * Internal Dependencies
@@ -18,42 +19,48 @@ const candidateForFeature = ( media ) => {
 	if ( media.mediaType === 'image' ) {
 		const image = media;
 
-		// image is not wide enough
-		if ( image.naturalWidth < 350 ) {
-			return false;
-		}
+		const imageIsTallEnough = 350 <= image.width || 350 <= image.naturalWidth;
+		const imageIsWideEnough = 85 <= image.height || 85 <= image.naturalHeight;
 
-		// image does not have enough area
-		if ( ( image.naturalWidth * image.naturalHeight ) < 30000 ) {
-			return false;
-		}
+		return imageIsTallEnough && imageIsWideEnough;
 	} else if ( media.mediaType === 'video' ) {
-		// we do not know how to autoplay it
+		// we need to have a thumbnail and know how to autoplay it
 		return media.thumbnailUrl && media.autoplayIframe;
 	}
 
 	return true;
 };
 
+// jetpack lies about thumbnails so we need to make sure its actually an image. See: thumbIsLikelyImage in pick-canonical-image rule
+const uriIsLikelyAnImage = ( uri ) => {
+	const withoutQuery = url.parse( uri ).pathname;
+	return some( [ '.jpg', '.jpeg', '.png', '.gif' ], ext => endsWith( withoutQuery, ext ) );
+};
+
+/**
+ * Given a post:
+ *  1. prefer to return the post's featured image ( post.post_thumbnail )
+ *  2. if there is no usable featured image, use the media that appears first in the content of the post
+ *  3. if there is no eligible asset, return null
+ */
 const FeaturedAsset = ( { post } ) => {
 	if ( ! post ) {
 		return null;
 	}
 
-	// take either the canonical image or if that doesn't exist, the first usable media in the content of the post
-	const featuredMedia = candidateForFeature( post.featured_image )
-		? post.featured_image
-		: find( post.featured_media, candidateForFeature );
-
-	if ( ! featuredMedia ) {
-		return null;
+	if ( post.featured_image && uriIsLikelyAnImage( post.featured_image ) ) {
+		return <FeaturedImage imageUri={ post.featured_image } href={ post.URL } />;
 	}
 
-	const featuredAsset = featuredMedia.mediaType === 'video'
-		? <FeaturedVideo { ...featuredMedia } videoEmbed={ featuredMedia } />
-		: <FeaturedImage imageUri={ featuredMedia.src } href={ post.URL } />;
+	const featuredMedia = find( post.content_media, candidateForFeature );
 
-	return featuredAsset;
+	if ( featuredMedia && featuredMedia.mediaType === 'video' ) {
+		return <FeaturedVideo { ...featuredMedia } videoEmbed={ featuredMedia } />;
+	} else if ( featuredMedia && featuredMedia.mediaType === 'image' ) {
+		return <FeaturedImage imageUri={ featuredMedia.src } href={ post.URL } />;
+	}
+
+	return null;
 };
 
 FeaturedAsset.propTypes = {
