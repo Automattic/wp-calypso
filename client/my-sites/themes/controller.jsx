81b0a830f13b8417938733029d1dfcd15ea399ac
Bernhard Reiter <ockham@raz.or.at>
2016-11-08 15:20:25 +0100

    Revert "Themes (single-site): Fix siteId/siteSlug mixup" (#9217)

--- client/my-sites/themes/controller.jsx
+++ client/my-sites/themes/controller.jsx
@@ -57,12 +57,18 @@ function getProps( context ) {
 }
 
 export function singleSite( context, next ) {
+	const { site_id: siteId } = context.params;
+	const props = getProps( context );
+
+	props.key = siteId;
+	props.siteId = siteId;
+
 	// Scroll to the top
 	if ( typeof window !== 'undefined' ) {
 		window.scrollTo( 0, 0 );
 	}
 
-	context.primary = <SingleSiteComponent { ...getProps( context ) } />;
+	context.primary = <SingleSiteComponent { ...props } />;
 	next();
 }
 
