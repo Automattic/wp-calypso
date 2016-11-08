8a5f61edc7a7ce78d1d2a9cc6dbcb2145d712c84
Matt Wondra <matt@apartmenttherapy.com>
2016-11-08 05:30:46 -0600

    Theme Showcase: Cache server-side data responses (#8942)
    * Get server-side data caching in place for Theme Showcase
    * A little more DRY implementation of server-caching Showcase.
    * Implement Lru cache and only cache data from certain URLs

--- client/my-sites/themes/controller.jsx
+++ client/my-sites/themes/controller.jsx
@@ -1,6 +1,9 @@
 /**
  * External Dependencies
  */
+import compact from 'lodash/compact';
+import debugFactory from 'debug';
+import Lru from 'lru-cache';
 import React from 'react';
 
 /**
@@ -10,9 +13,20 @@ import SingleSiteComponent from 'my-sites/themes/single-site';
 import MultiSiteComponent from 'my-sites/themes/multi-site';
 import LoggedOutComponent from './logged-out';
 import trackScrollPage from 'lib/track-scroll-page';
-import { getAnalyticsData } from './helpers';
-import { fetchNextPage, query } from 'state/themes/actions';
 import { PER_PAGE } from 'state/themes/themes-list/constants';
+import {
+	fetchThemes,
+	incrementThemesPage,
+	query
+} from 'state/themes/actions';
+import { getAnalyticsData } from './helpers';
+
+const debug = debugFactory( 'calypso:themes' );
+const HOUR_IN_MS = 3600000;
+const themesQueryCache = new Lru( {
+	max: 500,
+	maxAge: HOUR_IN_MS
+} );
 
 function getProps( context ) {
 	const { tier, filter, vertical, site_id: siteId } = context.params;
@@ -77,7 +91,7 @@ export function loggedOut( context, next ) {
 	next();
 }
 
-export function fetchThemeData( context, next ) {
+export function fetchThemeData( context, next, shouldUseCache = false ) {
 	if ( ! context.isServerSide ) {
 		return next();
 	}
@@ -85,11 +99,43 @@ export function fetchThemeData( context, next ) {
 	const queryParams = {
 		search: context.query.s,
 		tier: context.params.tier,
-		filter: context.params.filter,
+		filter: compact( [ context.params.filter, context.params.vertical ] ).join( ',' ),
 		page: 0,
 		perPage: PER_PAGE,
 	};
+	const cacheKey = context.path;
 
 	context.store.dispatch( query( queryParams ) );
-	context.store.dispatch( fetchNextPage( false ) ).then( () => next() );
+	context.store.dispatch( incrementThemesPage( false ) );
+
+	if ( shouldUseCache ) {
+		const cachedData = themesQueryCache.get( cacheKey );
+		if ( cachedData ) {
+			debug( `found theme data in cache key=${ cacheKey }` );
+			context.store.dispatch( cachedData.action );
+			context.renderCacheKey = context.path + cachedData.timestamp;
+			return next();
+		}
+	}
+
+	context.store.dispatch( fetchThemes( false ) )
+		.then( action => {
+			if ( shouldUseCache ) {
+				const timestamp = Date.now();
+				themesQueryCache.set( cacheKey, { action, timestamp } );
+				context.renderCacheKey = context.path + timestamp;
+				debug( `caching theme data key=${ cacheKey }` );
+			}
+			next();
+		} )
+		.catch( () => next() );
+}
+
+export function fetchThemeDataWithCaching( context, next ) {
+	if ( Object.keys( context.query ).length > 0 ) {
+		// Don't cache URLs with query params for now
+		return fetchThemeData( context, next, false );
+	}
+
+	return fetchThemeData( context, next, true );
 }
