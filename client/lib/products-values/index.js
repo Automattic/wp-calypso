b632b128499a33e67ef50c95bd95fc9a158abb14
Biser Perchinkov <biser@bisko.be>
2016-11-09 17:46:30 +0200

    Revert "Domains: Improve free subdomain handling in domain suggestion screen" (#9247)

--- client/lib/products-values/index.js
+++ client/lib/products-values/index.js
@@ -4,6 +4,7 @@
  * External dependencies
  */
 var assign = require( 'lodash/assign' ),
+	endsWith = require( 'lodash/endsWith' ),
 	difference = require( 'lodash/difference' ),
 	isEmpty = require( 'lodash/isEmpty' ),
 	pick = require( 'lodash/pick' );
@@ -261,11 +262,6 @@ function isDependentProduct( product, dependentProduct, domainsWithPlansOnly ) {
 		product.meta === dependentProduct.meta
 	);
 }
-function isFreeWordPressComDomain( product ) {
-	product = formatProduct( product );
-	assertValidProduct( product );
-	return product.is_free === true;
-}
 
 function isGoogleApps( product ) {
 	product = formatProduct( product );
@@ -323,6 +319,13 @@ function isUnlimitedThemes( product ) {
 	return 'unlimited_themes' === product.product_slug;
 }
 
+function isWordPressDomain( product ) {
+	product = formatProduct( product );
+	assertValidProduct( product );
+
+	return endsWith( product.domain_name, '.wordpress.com' );
+}
+
 function whitelistAttributes( product ) {
 	return pick( product, Object.keys( schema.properties ) );
 }
@@ -355,7 +358,6 @@ module.exports = {
 	isFreePlan,
 	isPersonal,
 	isFreeTrial,
-	isFreeWordPressComDomain,
 	isGoogleApps,
 	isGuidedTransfer,
 	isJetpackBusiness,
@@ -374,5 +376,6 @@ module.exports = {
 	isUnlimitedSpace,
 	isUnlimitedThemes,
 	isVideoPress,
+	isWordPressDomain,
 	whitelistAttributes
 };
