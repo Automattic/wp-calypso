083dbaf481f6d653c46182e036f6cbba2380982e
Marin Atanasov <tyxla@abv.bg>
2016-11-09 18:02:50 +0200

    People: Reduxify the RoleSelect component

--- client/my-sites/people/role-select/index.jsx
+++ client/my-sites/people/role-select/index.jsx
@@ -2,138 +2,87 @@
  * External dependencies
  */
 import React from 'react';
-import debugFactory from 'debug';
 import omit from 'lodash/omit';
-import map from 'lodash/map';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-import RolesStore from 'lib/site-roles/store';
-import RolesActions from 'lib/site-roles/actions';
 import FormFieldset from 'components/forms/form-fieldset';
 import FormLabel from 'components/forms/form-label';
 import FormSelect from 'components/forms/form-select';
 import FormSettingExplanation from 'components/forms/form-setting-explanation';
-import sitesList from 'lib/sites-list';
+import QuerySites from 'components/data/query-sites';
+import QuerySiteRoles from 'components/data/query-site-roles';
+import { getSite } from 'state/sites/selectors';
+import { getSiteRoles } from 'state/site-roles/selectors';
 
-/**
- * Module variables
- */
-const debug = debugFactory( 'calypso:my-sites:people:role-select' );
-const sites = sitesList();
+const getWPCOMFollowerRole = ( { site, translate } ) => {
+	const displayName = site.is_private
+		? translate( 'Viewer', { context: 'Role that is displayed in a select' } )
+		: translate( 'Follower', { context: 'Role that is displayed in a select' } );
 
-export default React.createClass( {
-	displayName: 'RoleSelect',
+	return {
+		display_name: displayName,
+		name: 'follower'
+	};
+};
 
-	getInitialState() {
-		return ( {
-			roles: this.props.siteId ? RolesStore.getRoles( this.props.siteId ) : {}
-		} );
-	},
+const RoleSelect = ( props ) => {
+	const { siteRoles, site, includeFollower, siteId, id, explanation, translate } = props;
+	const roles = siteRoles && siteRoles.slice( 0 );
+	const omitProps = [
+		'site',
+		'key',
+		'siteId',
+		'includeFollower',
+		'explanation',
+		'siteRoles',
+		'dispatch',
+		'moment',
+		'numberFormat',
+		'translate'
+	];
 
-	componentWillMount() {
-		if ( this.props.includeFollower ) {
-			this.refreshRoles();
-		}
-	},
-
-	componentDidMount() {
-		RolesStore.on( 'change', this.refreshRoles );
-		this.fetchRoles();
-	},
-
-	componentWillUnmount() {
-		RolesStore.removeListener( 'change', this.refreshRoles );
-	},
-
-	componentWillReceiveProps( nextProps ) {
-		const siteId = nextProps.siteId || this.props.siteId;
-		this.fetchRoles( siteId );
-		this.refreshRoles( nextProps );
-	},
-
-	getWPCOMFollowerRole( siteId ) {
-		siteId = siteId ? siteId : this.props.siteId;
-
-		const site = sites.getSite( siteId ),
-			displayName = site.is_private
-			? this.translate( 'Viewer', { context: 'Role that is displayed in a select' } )
-			: this.translate( 'Follower', { context: 'Role that is displayed in a select' } );
-
-		return {
-			follower: {
-				display_name: displayName,
-				name: 'follower'
-			}
-		};
-	},
-
-	refreshRoles( nextProps ) {
-		const siteId = nextProps && nextProps.siteId ? nextProps.siteId : this.props.siteId;
-
-		if ( siteId ) {
-			let siteRoles = RolesStore.getRoles( siteId );
-
-			if ( this.props.includeFollower ) {
-				siteRoles = Object.assign( {}, this.getWPCOMFollowerRole(), siteRoles );
-			}
-
-			this.setState( {
-				roles: siteRoles
-			} );
-		}
-	},
-
-	fetchRoles( siteId = this.props.siteId ) {
-		if ( ! siteId ) {
-			debug( 'siteId not set' );
-			return;
-		}
-
-		if ( Object.keys( RolesStore.getRoles( siteId ) ).length ) {
-			debug( 'initial fetch not necessary' );
-			return;
-		}
-
-		debug( 'Fetching roles for ' + siteId );
-
-		// defer fetch requests to avoid dispatcher conflicts
-		setTimeout( function() {
-			const fetching = RolesStore.isFetching( siteId );
-			if ( fetching ) {
-				return;
-			}
-			RolesActions.fetch( siteId );
-		}, 0 );
-	},
-
-	render() {
-		const roleKeys = Object.keys( this.state.roles );
-		return (
-			<FormFieldset key={ this.props.siteId } disabled={ ! roleKeys.length }>
-				<FormLabel htmlFor={ this.props.id }>
-					{ this.translate( 'Role', {
-						context: 'Text that is displayed in a label of a form.'
-					} ) }
-				</FormLabel>
-				<FormSelect { ...omit( this.props, [ 'site', 'key', 'siteId', 'includeFollower', 'explanation' ] ) }>
-					{
-						map( this.state.roles, ( roleObject, key ) => {
-							return (
-								<option value={ key } key={ key }>
-									{ roleObject.display_name }
-								</option>
-							);
-						} )
-					}
-				</FormSelect>
-				{ this.props.explanation &&
-					<FormSettingExplanation>
-						{ this.props.explanation }
-					</FormSettingExplanation>
-				}
-			</FormFieldset>
-		);
+	if ( site && roles && includeFollower ) {
+		roles.push( getWPCOMFollowerRole( props ) );
 	}
-} );
+
+	return (
+		<FormFieldset key={ siteId } disabled={ ! roles }>
+			{ siteId &&
+				<QuerySites siteId={ siteId } /> &&
+				<QuerySiteRoles siteId={ siteId } />
+			}
+			<FormLabel htmlFor={ id }>
+				{ translate( 'Role', {
+					context: 'Text that is displayed in a label of a form.'
+				} ) }
+			</FormLabel>
+			<FormSelect { ...omit( props, omitProps ) }>
+				{
+					roles && roles.map( ( role ) => {
+						return (
+							<option value={ role.name } key={ role.name }>
+								{ role.display_name }
+							</option>
+						);
+					} )
+				}
+			</FormSelect>
+			{ explanation &&
+				<FormSettingExplanation>
+					{ explanation }
+				</FormSettingExplanation>
+			}
+		</FormFieldset>
+	);
+};
+
+export default connect(
+	( state, ownProps ) => ( {
+		site: getSite( state, ownProps.siteId ),
+		siteRoles: getSiteRoles( state, ownProps.siteId ),
+	} )
+)( localize( RoleSelect ) );
