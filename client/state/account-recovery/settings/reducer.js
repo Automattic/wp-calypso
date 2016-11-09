b1655aded398be96137a4264a7581323a0d8ad9a
James Tien <southp0105@gmail.com>
2016-11-10 07:16:29 +0800

    Move the current actions / reducer into a new subdirectory, `settings`, for both a better organization and easier to co-work on this state tree.

---
+++ client/state/account-recovery/settings/reducer.js
@@ -0,0 +1,33 @@
+/**
+ * Internal dependencies
+ */
+import { createReducer } from 'state/utils';
+
+import {
+	ACCOUNT_RECOVERY_FETCH,
+	ACCOUNT_RECOVERY_FETCH_SUCCESS,
+	ACCOUNT_RECOVERY_FETCH_FAILED,
+} from 'state/action-types';
+
+const reducer = createReducer( {}, {
+	[ ACCOUNT_RECOVERY_FETCH ]: ( state ) => ( {
+		...state,
+		isFetching: true,
+	} ),
+
+	[ ACCOUNT_RECOVERY_FETCH_SUCCESS ]: ( state, { email, email_validated, phone, phone_validated } ) => ( {
+		...state,
+		email,
+		emailValidated: email_validated,
+		phone,
+		phoneValidated: phone_validated,
+		isFetching: false,
+	} ),
+
+	[ ACCOUNT_RECOVERY_FETCH_FAILED ]: ( state ) => ( {
+		...state,
+		isFetching: false,
+	} ),
+} );
+
+export default reducer;
