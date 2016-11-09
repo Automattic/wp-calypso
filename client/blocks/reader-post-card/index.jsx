1e37848ebc27b5d27a7e890a5db65f62d2916b0b
Jan Cavan Boulas <janmcavan@gmail.com>
2016-11-09 12:52:25 -0800

    Reader: Fix various card spacing inconsistencies (#9163)
    * Reader: Fix various card spacing inconsistencies
    
    * Treat featured asset like a factory instead of a component
    
    This allows us to know if it's going to return null and make decisions based on that knowledge.
    This _only works_ because FeaturedAsset is a pure function, not a React.Component or class from createClass.
    
    Kinda gross. Need to work out a saner way to do this, but works for now.
    
    Related to #9149
    
    * Fix errors
    
    * Reapply the title fix. Scope only to photo-only cards.
    
    * Correct spacing in different card types
    
    * Fix PostOptions icon in Safari, update gallery card height
    
    * Fix PostOptions, photo-card overlay, breakpoint

--- client/blocks/reader-post-card/index.jsx
+++ client/blocks/reader-post-card/index.jsx
@@ -86,25 +86,25 @@ export default class RefreshPostCard extends React.Component {
 		const { post, originalPost, site, feed, onCommentClick, showPrimaryFollowButton } = this.props;
 		const isPhotoOnly = !! ( post.display_type & DisplayTypes.PHOTO_ONLY );
 		const isGallery = !! ( post.display_type & DisplayTypes.GALLERY );
-		const featuredAsset = ( <FeaturedAsset post={ post } /> );
+		const featuredAsset = FeaturedAsset( { post } ); // this is ... gross? gross.
 		const classes = classnames( 'reader-post-card', {
 			'has-thumbnail': !! featuredAsset,
 			'is-photo': isPhotoOnly,
 			'is-gallery': isGallery
 		} );
 		const showExcerpt = ! isPhotoOnly;
-
-		let followUrl;
-		if ( showPrimaryFollowButton ) {
-			followUrl = feed ? feed.feed_URL : post.site_URL;
-		}
-
 		let title = truncate( post.title, {
 			length: 140,
 			separator: /,? +/
 		} );
-		if ( ! title ) {
+
+		if ( ! title && isPhotoOnly ) {
 			title = '\xa0'; // force to non-breaking space if empty so that the title h1 doesn't collapse and complicate things
+		}
+
+		let followUrl;
+		if ( showPrimaryFollowButton ) {
+			followUrl = feed ? feed.feed_URL : post.site_URL;
 		}
 
 		return (
