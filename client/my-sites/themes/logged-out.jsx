2cf0270c33e6e454d0a4fc228d38116dbed63c2f
Bernhard Reiter <ockham@raz.or.at>
2016-11-10 14:06:27 +0100

    Revert "Revert "Themes: Use Selectors instead of Helpers"" (#9215)

--- client/my-sites/themes/logged-out.jsx
+++ client/my-sites/themes/logged-out.jsx
@@ -1,42 +1,29 @@
 /**
  * External dependencies
  */
-import { connect } from 'react-redux';
+import React from 'react';
 
 /**
  * Internal dependencies
  */
 import ThemeShowcase from './theme-showcase';
-import {
-	preview,
-	signup,
-	separator,
-	info,
-	support,
-	help,
-	bindOptionsToDispatch
-} from './theme-options';
+import { connectOptions } from './theme-options';
 
-const mergeProps = ( stateProps, dispatchProps, ownProps ) => Object.assign(
-	{},
-	ownProps,
-	stateProps,
-	{
-		options: dispatchProps,
-		defaultOption: dispatchProps.signup,
-		getScreenshotOption: () => dispatchProps.info
-	}
+const ConnectedThemeShowcase = connectOptions( ThemeShowcase );
+
+export default props => (
+	<ConnectedThemeShowcase { ...props }
+	options={ [
+		'signup',
+		'preview',
+		'separator',
+		'info',
+		'support',
+		'help'
+	] }
+	defaultOption="signup"
+	getScreenshotOption={ function() {
+		return 'info';
+	} }
+	source="showcase" />
 );
-
-export default connect(
-	null,
-	bindOptionsToDispatch( {
-		signup,
-		preview,
-		separator,
-		info,
-		support,
-		help
-	}, 'showcase' ),
-	mergeProps
-)( ThemeShowcase );
