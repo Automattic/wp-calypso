47c21dcef09624e9a0d0baa8e77ccc66cf2afb69
Bernhard Reiter <ockham@raz.or.at>
2016-11-10 14:06:27 +0100

    Revert "Revert "Themes: Use Selectors instead of Helpers"" (#9215)

--- client/state/themes/current-theme/selectors.js
+++ client/state/themes/current-theme/selectors.js
@@ -4,6 +4,11 @@ export function getCurrentTheme( state, siteId ) {
 	return state.themes.currentTheme.get( 'currentThemes' ).get( siteId );
 }
 
+export function isActiveTheme( state, themeId, siteId ) {
+	const theme = getCurrentTheme( state, siteId );
+	return theme && theme.id === themeId;
+}
+
 export function isActivating( state ) {
 	return state.themes.currentTheme.get( 'isActivating' );
 }
