5613b69a1ba0e1553858c65eb0934b5e3e788c2d
James Tien <southp0105@gmail.com>
2016-11-10 09:34:13 +0800

    Rewrite the notice handlers based on the changes to account recovery state actions in the parent branch.

--- client/state/notices/account-recovery/index.js
+++ client/state/notices/account-recovery/index.js
@@ -6,32 +6,43 @@ import { translate } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import { errorNotice } from 'state/notices/actions';
 import { dispatchError } from '../utils';
 
 import {
-	ACCOUNT_RECOVERY_FETCH_FAILED,
-	ACCOUNT_RECOVERY_PHONE_UPDATE_FAILED,
-	ACCOUNT_RECOVERY_PHONE_DELETE_FAILED,
-	ACCOUNT_RECOVERY_EMAIL_UPDATE_FAILED,
-	ACCOUNT_RECOVERY_EMAIL_DELETE_FAILED,
+	ACCOUNT_RECOVERY_SETTINGS_FETCH_FAILED,
+	ACCOUNT_RECOVERY_SETTINGS_UPDATE_FAILED,
+	ACCOUNT_RECOVERY_SETTINGS_DELETE_FAILED,
 } from 'state/action-types';
 
+const getUpdateErrorNotice = ( target ) => {
+	switch ( target ) {
+		case 'phone':
+			return errorNotice( translate( 'An error occurred while updating your account recovery phone number.' ) );
+		case 'email':
+			return errorNotice( translate( 'An error occurred while updating your account recovery email.' ) );
+		default:
+			return errorNotice( translate( 'An error occurred while updating your account recovery options.' ) );
+	}
+};
+
+const getDeleteErrorNotice = ( target ) => {
+	switch ( target ) {
+		case 'phone':
+			return errorNotice( translate( 'An error occurred while deleting your account recovery phone number.' ) );
+		case 'email':
+			return errorNotice( translate( 'An error occurred while deleting your account recovery email.' ) );
+		default:
+			return errorNotice( translate( 'An error occurred while deleting your account recovery options.' ) );
+	}
+};
+
 const handlers = {
-	[ ACCOUNT_RECOVERY_FETCH_FAILED ]: dispatchError(
+	[ ACCOUNT_RECOVERY_SETTINGS_FETCH_FAILED ]: dispatchError(
 		translate( 'An error occurred while fetching for your account recovery settings.' )
 	),
-	[ ACCOUNT_RECOVERY_PHONE_UPDATE_FAILED ]: dispatchError(
-		translate( 'An error occurred while updating your account recovery phone number.' )
-	),
-	[ ACCOUNT_RECOVERY_PHONE_DELETE_FAILED ]: dispatchError(
-		translate( 'An error occurred while deleting your account recovery phone number.' )
-	),
-	[ ACCOUNT_RECOVERY_EMAIL_UPDATE_FAILED ]: dispatchError(
-		translate( 'An error occurred while updating your account recovery email.' )
-	),
-	[ ACCOUNT_RECOVERY_EMAIL_DELETE_FAILED ]: dispatchError(
-		translate( 'An error occurred while deleting your account recovery email.' )
-	),
+	[ ACCOUNT_RECOVERY_SETTINGS_UPDATE_FAILED ]: ( dispatch, { target } ) => dispatch( getUpdateErrorNotice( target ) ),
+	[ ACCOUNT_RECOVERY_SETTINGS_DELETE_FAILED ]: ( dispatch, { target } ) => dispatch( getDeleteErrorNotice( target ) ),
 };
 
 export default handlers;
