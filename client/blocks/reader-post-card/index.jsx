595f18e1dfaf9b61290ea5e40d3c4ff201749bfe
Ben Lowery <ben.lowery@automattic.com>
2016-11-12 09:59:07 -0500

    Reader: Fix keyboard shortcuts for next and previous in refreshed streams
    Teach the refresh cards about the is-selected prop and add new styles to indicate the selected post.
    
    Fixes #9074

--- client/blocks/reader-post-card/index.jsx
+++ client/blocks/reader-post-card/index.jsx
@@ -25,6 +25,7 @@ export default class RefreshPostCard extends React.Component {
 		post: PropTypes.object.isRequired,
 		site: PropTypes.object,
 		feed: PropTypes.object,
+		isSelected: PropTypes.bool,
 		onClick: PropTypes.func,
 		onCommentClick: PropTypes.func,
 		showPrimaryFollowButton: PropTypes.bool,
@@ -33,7 +34,8 @@ export default class RefreshPostCard extends React.Component {
 
 	static defaultProps = {
 		onClick: noop,
-		onCommentClick: noop
+		onCommentClick: noop,
+		isSelected: false
 	};
 
 	propagateCardClick = () => {
@@ -84,13 +86,14 @@ export default class RefreshPostCard extends React.Component {
 	}
 
 	render() {
-		const { post, originalPost, site, feed, onCommentClick, showPrimaryFollowButton } = this.props;
+		const { post, originalPost, site, feed, onCommentClick, showPrimaryFollowButton, isSelected } = this.props;
 		const isPhotoOnly = !! ( post.display_type & DisplayTypes.PHOTO_ONLY );
 		const isGallery = !! ( post.display_type & DisplayTypes.GALLERY );
 		const classes = classnames( 'reader-post-card', {
 			'has-thumbnail': !! post.canonical_media,
 			'is-photo': isPhotoOnly,
-			'is-gallery': isGallery
+			'is-gallery': isGallery,
+			'is-selected': isSelected
 		} );
 		const showExcerpt = ! isPhotoOnly;
 		let title = truncate( post.title, {
