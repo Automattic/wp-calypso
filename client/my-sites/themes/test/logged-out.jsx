149f8a2536897eab3b2cc0ca54e1c4e5c70334f6
Matt Wondra <mattwondra@gmail.com>
2016-11-14 11:17:16 -0600

    Test various states of the Logged Out Themes Showcase

--- client/my-sites/themes/test/logged-out.jsx
+++ client/my-sites/themes/test/logged-out.jsx
@@ -17,6 +17,12 @@ import { Provider as ReduxProvider } from 'react-redux';
 import { createReduxStore } from 'state';
 import EmptyComponent from 'test/helpers/react/empty-component';
 import useMockery from 'test/helpers/use-mockery';
+import {
+	incrementThemesPage,
+	query,
+	receiveThemes,
+	receiveServerError
+} from 'state/themes/actions';
 
 describe( 'logged-out', () => {
 	context( 'when calling renderToString()', function() {
@@ -27,17 +33,107 @@ describe( 'logged-out', () => {
 			mockery.registerMock( 'lib/analytics/page-view-tracker', EmptyComponent );
 
 			this.LoggedOutShowcase = require( '../logged-out' );
+
+			this.themes = [
+				{
+					author: 'AudioTheme',
+					id: 'wayfarer',
+					stylesheet: 'premium/wayfarer',
+					name: 'Wayfarer',
+					author_uri: 'https://audiotheme.com/',
+					demo_uri: 'https://wayfarerdemo.wordpress.com/',
+					screenshot: 'https://i1.wp.com/theme.wordpress.com/wp-content/themes/premium/wayfarer/screenshot.png',
+					price: '$69'
+				},
+				{
+					author: 'Organic Themes',
+					id: 'natural',
+					stylesheet: 'premium/natural',
+					name: 'Natural',
+					author_uri: 'http://www.organicthemes.com',
+					demo_uri: 'https://naturaldemo.wordpress.com/',
+					screenshot: 'https://i2.wp.com/theme.wordpress.com/wp-content/themes/premium/natural/screenshot.png',
+					price: '$69'
+				},
+				{
+					author: 'Press75',
+					id: 'attache',
+					stylesheet: 'premium/attache',
+					name: 'Attache',
+					author_uri: 'http://www.press75.com/',
+					demo_uri: 'https://attachedemo.wordpress.com/',
+					screenshot: 'https://i0.wp.com/theme.wordpress.com/wp-content/themes/premium/attache/screenshot.png',
+					price: '$69'
+				},
+				{
+					author: 'Anariel Design',
+					id: 'pena',
+					stylesheet: 'premium/pena',
+					name: 'Pena',
+					author_uri: 'http://theme.wordpress.com/themes/by/anariel-design/',
+					demo_uri: 'https://penademo.wordpress.com/',
+					screenshot: 'https://i2.wp.com/theme.wordpress.com/wp-content/themes/premium/pena/screenshot.png',
+					price: '$89'
+				},
+				{
+					author: 'Automattic',
+					id: 'karuna',
+					stylesheet: 'pub/karuna',
+					name: 'Karuna',
+					author_uri: 'http://wordpress.com/themes/',
+					demo_uri: 'https://karunademo.wordpress.com/',
+					screenshot: 'https://i1.wp.com/theme.wordpress.com/wp-content/themes/pub/karuna/screenshot.png'
+				}
+			];
+
+			this.queryParams = { perPage: 5, page: 1, filter: '', id: 1 };
 		} );
 
-		it( 'renders', () => {
-			const store = createReduxStore();
-			const layout = (
-				<ReduxProvider store={ store }>
+		beforeEach( () => {
+			this.store = createReduxStore();
+
+			this.layout = (
+				<ReduxProvider store={ this.store }>
 					<this.LoggedOutShowcase />
 				</ReduxProvider>
 			);
+		} );
 
-			assert.doesNotThrow( () => renderToString( layout ) );
+		it( 'renders without error when no themes are present', () => {
+			let markup;
+			assert.doesNotThrow( () => {
+				markup = renderToString( this.layout );
+			} );
+			// Should show a "No themes found" message
+			assert.isTrue( markup.includes( 'empty-content' ) );
+		} );
+
+		it( 'renders without error when themes are present', () => {
+			this.store.dispatch( query( this.queryParams ) );
+			this.store.dispatch( incrementThemesPage( false ) );
+			this.store.dispatch( receiveThemes( { themes: this.themes }, false, this.queryParams ) );
+
+			let markup;
+			assert.doesNotThrow( () => {
+				markup = renderToString( this.layout );
+			} );
+			// All 5 themes should appear...
+			assert.equal( 5, markup.match( /theme__content/g ).length );
+			// .. and no empty content placeholders should appear
+			assert.isFalse( markup.includes( 'empty-content' ) );
+		} );
+
+		it( 'renders without error when theme fetch fails', () => {
+			this.store.dispatch( query( this.queryParams ) );
+			this.store.dispatch( incrementThemesPage( false ) );
+			this.store.dispatch( receiveServerError( 'Error' ) );
+
+			let markup;
+			assert.doesNotThrow( () => {
+				markup = renderToString( this.layout );
+			} );
+			// Should show a "No themes found" message
+			assert.isTrue( markup.includes( 'empty-content' ) );
 		} );
 	} );
 } );
