1c2fe17d1834fb483514444ce5500be23d40e263
Marin Atanasov <tyxla@abv.bg>
2016-11-15 12:35:57 +0200

    Jetpack Plugins: Remove sitesList.getSelectedSite(), use Redux Jetpack selectors and cleanup PluginsMain

--- client/my-sites/plugins/main.jsx
+++ client/my-sites/plugins/main.jsx
@@ -4,12 +4,8 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import debugModule from 'debug';
 import classNames from 'classnames';
-import getOr from 'lodash/get';
-import some from 'lodash/some';
-import find from 'lodash/find';
-import isEmpty from 'lodash/isEmpty';
+import { find, isEmpty, some } from 'lodash';
 
 /**
  * Internal dependencies
@@ -33,16 +29,10 @@ import PluginsList from './plugins-list';
 import JetpackManageErrorPage from 'my-sites/jetpack-manage-error-page';
 import WpcomPluginPanel from 'my-sites/plugins-wpcom';
 import PluginsBrowser from './plugins-browser';
-
-/**
- * Module variables
- */
-const debug = debugModule( 'calypso:my-sites:plugins' );
+import { getSelectedSite, getSelectedSiteSlug } from 'state/ui/selectors';
+import { isJetpackSite, canJetpackSiteManage, canJetpackSiteUpdateFiles } from 'state/sites/selectors';
 
 const PluginsMain = React.createClass( {
-
-	displayName: 'PluginsMain',
-
 	mixins: [ URLSearch ],
 
 	getInitialState() {
@@ -50,7 +40,6 @@ const PluginsMain = React.createClass( {
 	},
 
 	componentDidMount() {
-		debug( 'Plugins React component mounted.' );
 		this.props.sites.on( 'change', this.refreshPlugins );
 		PluginsStore.on( 'change', this.refreshPlugins );
 	},
@@ -67,7 +56,7 @@ const PluginsMain = React.createClass( {
 	getPluginsFromStore( nextProps, sites ) {
 		const props = nextProps || this.props;
 		let	plugins = null;
-		if ( ! props.sites.selected ) {
+		if ( ! props.selectedSiteSlug ) {
 			plugins = PluginsStore.getPlugins( sites.filter( site => site.visible ), props.filter );
 		} else {
 			plugins = PluginsStore.getPlugins( sites, props.filter );
@@ -118,7 +107,7 @@ const PluginsMain = React.createClass( {
 	},
 
 	getFilters() {
-		const siteFilter = this.props.sites.selected ? '/' + this.props.sites.selected : '';
+		const siteFilter = this.props.selectedSiteSlug ? '/' + this.props.selectedSiteSlug : '';
 
 		return [
 			{
@@ -175,7 +164,7 @@ const PluginsMain = React.createClass( {
 
 	getEmptyContentUpdateData() {
 		const emptyContentData = { illustration: '/calypso/images/drake/drake-ok.svg' },
-			selectedSite = this.props.sites.getSelectedSite();
+			{ selectedSite } = this.props;
 
 		if ( selectedSite ) {
 			emptyContentData.title = this.translate( 'All plugins on %(siteName)s are {{span}}up to date.{{/span}}', {
@@ -196,7 +185,7 @@ const PluginsMain = React.createClass( {
 
 		if ( selectedSite ) {
 			emptyContentData.actionURL = '/plugins/' + selectedSite.slug;
-			if ( selectedSite.jetpack ) {
+			if ( this.props.isJetpackSite( selectedSite.ID ) ) {
 				emptyContentData.illustration = '/calypso/images/drake/drake-jetpack.svg';
 				emptyContentData.title = this.translate( 'Plugins can\'t be updated on %(siteName)s.', {
 					textOnly: true,
@@ -238,13 +227,16 @@ const PluginsMain = React.createClass( {
 	},
 
 	getUpdatesTabVisibility() {
-		const selectedSite = this.props.sites.getSelectedSite();
+		const { selectedSite } = this.props;
 
 		if ( selectedSite ) {
-			return selectedSite.jetpack && selectedSite.canUpdateFiles;
+			return this.props.isJetpackSite( selectedSite.ID ) && this.props.canJetpackSiteUpdateFiles( selectedSite.ID );
 		}
 
-		return some( this.props.sites.getSelectedOrAllWithPlugins(), site => site && site.jetpack && site.canUpdateFiles );
+		return some(
+			this.props.sites.getSelectedOrAllWithPlugins(),
+			site => site && this.props.isJetpackSite( site.ID ) && this.props.canJetpackSiteUpdateFiles( site.ID )
+		);
 	},
 
 	shouldShowPluginListPlaceholders() {
@@ -259,7 +251,7 @@ const PluginsMain = React.createClass( {
 
 	renderPluginsContent() {
 		const plugins = this.state.plugins || [];
-		const selectedSite = this.props.sites.getSelectedSite();
+		const { selectedSite } = this.props;
 
 		if ( isEmpty( plugins ) && ! this.isFetchingPlugins() ) {
 			if ( this.props.search ) {
@@ -337,9 +329,9 @@ const PluginsMain = React.createClass( {
 	},
 
 	render() {
-		const selectedSite = this.props.sites.getSelectedSite();
+		const { selectedSite } = this.props;
 
-		if ( ! getOr( selectedSite, 'jetpack', true ) ) {
+		if ( selectedSite && ! this.props.isJetpackSite( selectedSite.ID ) ) {
 			return (
 				<Main>
 					{ this.renderDocumentHead() }
@@ -363,7 +355,7 @@ const PluginsMain = React.createClass( {
 			);
 		}
 
-		if ( selectedSite && selectedSite.jetpack && ! selectedSite.canManage() ) {
+		if ( selectedSite && this.props.isJetpackSite( selectedSite.ID ) && ! this.props.canJetpackSiteManage( selectedSite.ID ) ) {
 			return (
 				<Main>
 					{ this.renderDocumentHead() }
@@ -428,11 +420,14 @@ const PluginsMain = React.createClass( {
 } );
 
 export default connect(
-	state => {
-		return {
-			wporgPlugins: state.plugins.wporg.items
-		};
-	},
+	state => ( {
+		wporgPlugins: state.plugins.wporg.items,
+		selectedSite: getSelectedSite( state ),
+		selectedSiteSlug: getSelectedSiteSlug( state ),
+		isJetpackSite: siteId => isJetpackSite( state, siteId ),
+		canJetpackSiteManage: siteId => canJetpackSiteManage( state, siteId ),
+		canJetpackSiteUpdateFiles: siteId => canJetpackSiteUpdateFiles( state, siteId ),
+	} ),
 	dispatch => bindActionCreators( {
 		wporgFetchPluginData
 	}, dispatch )
