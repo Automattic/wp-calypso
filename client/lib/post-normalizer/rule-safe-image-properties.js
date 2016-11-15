fe5cc4e713429274a85bacb3b424effd63ae0bdd
Ben Lowery <ben.lowery@automattic.com>
2016-11-14 21:11:13 -0500

    Reader: Suppress features for LS
    LS uses featured images in a weird way. Suppress them and rely on picking things up from content instead.

--- client/lib/post-normalizer/rule-safe-image-properties.js
+++ client/lib/post-normalizer/rule-safe-image-properties.js
@@ -12,6 +12,10 @@ import { makeImageURLSafe } from './utils';
 export default function safeImagePropertiesForWidth( maxWidth ) {
 	return function safeImageProperties( post ) {
 		makeImageURLSafe( post.author, 'avatar_URL', maxWidth );
+		if ( post.feed_ID === 12098 || +post.site_ID === 1065188 ) {
+			post.featured_image = '';
+			delete post.post_thumbnail;
+		}
 		makeImageURLSafe( post, 'featured_image', maxWidth, post.URL );
 		if ( post.post_thumbnail ) {
 			makeImageURLSafe( post.post_thumbnail, 'URL', maxWidth, post.URL );
