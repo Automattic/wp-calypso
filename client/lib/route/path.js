ce306a385c12419ef3488cfd3c61ff3e39296866
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-11-16 10:15:47 -0800

    Fix spelling mistake

--- client/lib/route/path.js
+++ client/lib/route/path.js
@@ -33,7 +33,7 @@ function getSiteFragment( path ) {
 	if ( last_piece && -1 !== last_piece.indexOf( '.' ) ) {
 		return last_piece;
 	}
-	// People section can have usernames in the url that have dits in the name.
+	// People section can have usernames in the url that have dots in the name.
 	// So we can just ignore the second to last part
 	const second_to_last_piece = pieces[ 1 ] === 'people' ? '' : pieces[ pieces.length - 2 ];
 	if ( second_to_last_piece && -1 !== second_to_last_piece.indexOf( '.' ) ) {
