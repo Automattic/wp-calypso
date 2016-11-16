b153fa56b2765d2e71c9d8a14352cb2e02fa32a2
Marin Atanasov <tyxla@abv.bg>
2016-11-16 11:50:12 +0200

    Jetpack Settings: Add deactivation actions, reducer and selectors

--- client/state/jetpack-settings/modules/actions.js
+++ client/state/jetpack-settings/modules/actions.js
@@ -4,7 +4,10 @@
 import {
 	JETPACK_MODULE_ACTIVATE,
 	JETPACK_MODULE_ACTIVATE_FAILURE,
-	JETPACK_MODULE_ACTIVATE_SUCCESS
+	JETPACK_MODULE_ACTIVATE_SUCCESS,
+	JETPACK_MODULE_DEACTIVATE,
+	JETPACK_MODULE_DEACTIVATE_FAILURE,
+	JETPACK_MODULE_DEACTIVATE_SUCCESS
 } from 'state/action-types';
 import wp from 'lib/wp';
 
@@ -26,6 +29,32 @@ export const activateModule = ( siteId, moduleSlug ) => {
 			} ).catch( error => {
 				dispatch( {
 					type: JETPACK_MODULE_ACTIVATE_FAILURE,
+					siteId,
+					moduleSlug,
+					error: error.message
+				} );
+			} );
+	};
+};
+
+export const deactivateModule = ( siteId, moduleSlug ) => {
+	return ( dispatch ) => {
+		dispatch( {
+			type: JETPACK_MODULE_DEACTIVATE,
+			siteId,
+			moduleSlug
+		} );
+
+		return wp.undocumented().jetpackModulesDeactivate( siteId, moduleSlug )
+			.then( () => {
+				dispatch( {
+					type: JETPACK_MODULE_DEACTIVATE_SUCCESS,
+					siteId,
+					moduleSlug
+				} );
+			} ).catch( error => {
+				dispatch( {
+					type: JETPACK_MODULE_DEACTIVATE_FAILURE,
 					siteId,
 					moduleSlug,
 					error: error.message
