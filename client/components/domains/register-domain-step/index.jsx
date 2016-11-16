5e1ca541559d984f0aaf7cce21503c965c4b7243
Andrija Vucinic <andrija.vucinic@gmail.com>
2016-11-16 23:56:57 +0100

    Domains: Add a persistent message for .blog on domain search
    Add a persistent mesesage on domain search saying that
    .blog is a new and available TLD

--- client/components/domains/register-domain-step/index.jsx
+++ client/components/domains/register-domain-step/index.jsx
@@ -14,7 +14,6 @@ import startsWith from 'lodash/startsWith';
 import page from 'page';
 import qs from 'qs';
 import { connect } from 'react-redux';
-import { endsWith } from 'lodash';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -201,6 +200,15 @@ const RegisterDomainStep = React.createClass( {
 		return (
 			<div className="register-domain-step">
 				{ this.searchForm() }
+				{
+					<Notice
+						text={ this.props.translate(
+							'New! {{strong}}.blog{{/strong}} domains are now available for registration.',
+							{ components: { strong: <strong /> } }
+						) }
+						status={ 'is-info' }
+						showDismiss={ false } />
+				}
 				{ this.notices() }
 				{ this.content() }
 				{ this.queryDomainsSuggestions() }
@@ -313,12 +321,6 @@ const RegisterDomainStep = React.createClass( {
 		async.parallel(
 			[
 				callback => {
-					if ( endsWith( domain, '.blog' ) ) {
-						const error = { code: 'dotblog_domain' };
-						this.showValidationErrorMessage( domain, error );
-						return callback();
-					}
-
 					if ( ! domain.match( /^([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)*[a-z0-9]([a-z0-9-]*[a-z0-9])?\.[a-z]{2,63}$/i ) ) {
 						return callback();
 					}
@@ -534,17 +536,6 @@ const RegisterDomainStep = React.createClass( {
 			translate = this.props.translate;
 
 		switch ( error.code ) {
-			case 'dotblog_domain':
-				message = translate(
-					'Coming soon! {{strong}}.blog{{/strong}} domains will be available on {{strong}}November 21st{{/strong}}.',
-					{
-						components: {
-							strong: <strong />
-						}
-					}
-				);
-				severity = 'info';
-				break;
 			case 'available_but_not_registrable':
 				if ( tld ) {
 					message = translate(
