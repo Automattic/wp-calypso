f793b8a60768f45077ce8bd424781ca2296c3e50
Dennis Snell <dennis.snell@automattic.com>
2016-11-16 09:45:32 -0500

    WPCOM Plugins: Update code style (#9421)
    This patch updates some code to more recent Calypso style.
    
    There are CSS naming issues but they are existing and won't be fixed
    here. This patch prefixes updates to the visual style of the WPCOM
    plugins page.

--- client/my-sites/plugins-wpcom/purchase-button.jsx
+++ client/my-sites/plugins-wpcom/purchase-button.jsx
@@ -1,34 +1,35 @@
+/**
+ * External dependencies
+ */
 import React, { PropTypes } from 'react';
+import { localize } from 'i18n-calypso';
+import { identity } from 'lodash';
 
+/**
+ * Internal dependencies
+ */
 import Button from 'components/button';
 import Gridicon from 'components/gridicon';
 
-export const PurchaseButton = React.createClass( {
-	render() {
-		const {
-			href,
-			isActive
-		} = this.props;
-
-		if ( isActive ) {
-			return (
-				<Button className="is-active-plugin" compact borderless>
-					<Gridicon icon="checkmark" />{ this.translate( 'Active' ) }
-				</Button>
-			);
-		}
-
-		return (
-			<Button compact primary { ...{ href } }>
-				{ this.translate( 'Purchase' ) }
-			</Button>
-		);
-	}
-} );
+export const PurchaseButton = ( {
+	href,
+	isActive,
+	translate = identity,
+} ) => isActive
+	? (
+		<Button className="is-active-plugin" compact borderless>
+			<Gridicon icon="checkmark" />{ translate( 'Active' ) }
+		</Button>
+	)
+	: (
+		<Button compact primary { ...{ href } }>
+			{ translate( 'Purchase' ) }
+		</Button>
+	);
 
 PurchaseButton.propTypes = {
 	href: PropTypes.string.isRequired,
-	isActive: PropTypes.bool.isRequired
+	isActive: PropTypes.bool.isRequired,
 };
 
-export default PurchaseButton;
+export default localize( PurchaseButton );
--- client/my-sites/plugins-wpcom/standard-plugins-panel.jsx
+++ client/my-sites/plugins-wpcom/standard-plugins-panel.jsx
@@ -4,6 +4,10 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
+import {
+	flowRight as compose,
+	identity,
+} from 'lodash';
 
 /**
  * Internal dependencies
@@ -17,62 +21,69 @@ import Plugin from './plugin';
 import Notice from 'components/notice';
 import NoticeAction from 'components/notice/notice-action';
 
-export const StandardPluginsPanel = React.createClass( {
-	render() {
-		const {
-			displayCount,
-			onClick,
-			plugins = []
-		} = this.props;
-
-		const shownPlugins = plugins.slice( 0, displayCount );
-
-		return (
-			<div>
-				<SectionHeader label={ this.translate( 'Standard Plugins' ) }>
-					<Button className="is-active-plugin" compact borderless>
-						<Gridicon icon="checkmark" />{ this.translate( 'Active' ) }
-					</Button>
-				</SectionHeader>
-				<CompactCard className="wpcom-plugins__standard-panel">
-					<div className="wpcom-plugins__list">
-						{ shownPlugins.map( ( { name, descriptionLink, icon, category, description } ) =>
-							<Plugin
-								onClick={ () => onClick( name ) }
-								{ ...{ name, key: name, descriptionLink, icon, category, description } }
-							/>
-						) }
-					</div>
-				</CompactCard>
-				<Notice
-					status="is-info"
-					showDismiss={ false }
-					text={ this.translate( 'Uploading your own plugins is not available on WordPress.com.' ) }
-				>
-					<NoticeAction href="https://en.support.wordpress.com/plugins/" external={ true }>
-						{ this.translate( 'Learn More' ) }
-					</NoticeAction>
-				</Notice>
+export const StandardPluginsPanel = ( {
+	displayCount,
+	onClick,
+	plugins = [],
+	translate = identity,
+} ) => (
+	<div>
+		<SectionHeader label={ translate( 'Standard Plugins' ) }>
+			<Button className="is-active-plugin" compact borderless>
+				<Gridicon icon="checkmark" />{ translate( 'Active' ) }
+			</Button>
+		</SectionHeader>
+		<CompactCard className="wpcom-plugins__standard-panel">
+			<div className="wpcom-plugins__list">
+				{ plugins
+					.slice( 0, displayCount )
+					.map( ( { name, descriptionLink, icon, category, description } ) => (
+						<Plugin
+							{ ...{
+								category,
+								description,
+								descriptionLink,
+								icon,
+								key: name,
+								name,
+								onClick,
+							} }
+						/>
+					)
+				) }
 			</div>
-		);
-	}
-} );
+		</CompactCard>
+		<Notice
+			status="is-info"
+			showDismiss={ false }
+			text={ translate(
+				'Uploading your own plugins is ' +
+				'not available on WordPress.com.'
+			) }
+		>
+			<NoticeAction
+				href="https://en.support.wordpress.com/plugins/"
+				external={ true }
+			>
+				{ translate( 'Learn More' ) }
+			</NoticeAction>
+		</Notice>
+	</div>
+);
 
 StandardPluginsPanel.propTypes = {
 	displayCount: PropTypes.number,
-	plugins: PropTypes.array
+	plugins: PropTypes.array,
 };
 
-const trackClick = name => recordTracksEvent(
-	'calypso_plugin_wpcom_click',
-	{
+const trackClick = name =>
+	recordTracksEvent( 'calypso_plugin_wpcom_click', {
 		plugin_name: name,
-		plugin_plan: 'standard'
-	}
-);
+		plugin_plan: 'standard',
+	} );
 
 const mapDispatchToProps = dispatch => ( {
-	onClick: name => dispatch( trackClick( name ) )
+	onClick: compose( dispatch, trackClick ),
 } );
 
 export default connect( null, mapDispatchToProps )( localize( StandardPluginsPanel ) );
