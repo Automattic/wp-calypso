c0358198a1dfb59a6f86cc4d28b6d7a8e240bfbd
Dennis Snell <dennis.snell@automattic.com>
2016-11-16 09:45:32 -0500

    WPCOM Plugins: Update code style (#9421)
    This patch updates some code to more recent Calypso style.
    
    There are CSS naming issues but they are existing and won't be fixed
    here. This patch prefixes updates to the visual style of the WPCOM
    plugins page.

--- client/my-sites/plugins-wpcom/business-plugins-panel.jsx
+++ client/my-sites/plugins-wpcom/business-plugins-panel.jsx
@@ -1,7 +1,15 @@
+/**
+ * External dependencies
+ */
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import { identity } from 'lodash';
 import classNames from 'classnames';
 
+/**
+ * Internal dependencies
+ */
 import Card from 'components/card';
 import SectionHeader from 'components/section-header';
 import PurchaseButton from './purchase-button';
@@ -9,47 +17,40 @@ import { recordTracksEvent } from 'state/analytics/actions';
 
 import Plugin from './plugin';
 
-export const BusinessPluginsPanel = React.createClass( {
-	render() {
-		const {
-			isActive = false,
-			onClick,
-			purchaseLink,
-			plugins = []
-		} = this.props;
+export const BusinessPluginsPanel = ( {
+	isActive = false,
+	onClick,
+	purchaseLink,
+	plugins = [],
+	translate = identity,
+} ) => (
+	<div>
+		<SectionHeader label={ translate( 'Business Plan Upgrades' ) }>
+			<PurchaseButton { ...{ isActive, href: purchaseLink } } />
+		</SectionHeader>
 
-		const cardClasses = classNames( 'wpcom-plugins__business-panel', {
+		<Card className={ classNames( 'wpcom-plugins__business-panel', {
 			'is-disabled': ! isActive
-		} );
-
-		return (
-			<div>
-				<SectionHeader label={ this.translate( 'Business Plan Upgrades' ) }>
-					<PurchaseButton { ...{ isActive, href: purchaseLink } } />
-				</SectionHeader>
-
-				<Card className={ cardClasses }>
-					<div className="wpcom-plugins__list">
-						{ plugins.map( ( { name, descriptionLink, icon, category, description } ) =>
-							<Plugin
-								onClick={ () => onClick( name ) }
-								{ ...{
-									name,
-									key: name,
-									descriptionLink,
-									icon,
-									isActive,
-									category,
-									description
-								} }
-							/>
-						) }
-					</div>
-				</Card>
+		} ) }>
+			<div className="wpcom-plugins__list">
+				{ plugins.map( ( { name, descriptionLink, icon, category, description } ) =>
+					<Plugin
+						{ ...{
+							category,
+							description,
+							descriptionLink,
+							icon,
+							isActive,
+							key: name,
+							name,
+							onClick,
+						} }
+					/>
+				) }
 			</div>
-		);
-	}
-} );
+		</Card>
+	</div>
+);
 
 BusinessPluginsPanel.propTypes = {
 	isActive: PropTypes.bool,
@@ -57,17 +58,15 @@ BusinessPluginsPanel.propTypes = {
 	plugins: PropTypes.array
 };
 
-const trackClick = name => recordTracksEvent(
-	'calypso_plugin_wpcom_click',
-	{
+const trackClick = name =>
+	recordTracksEvent( 'calypso_plugin_wpcom_click', {
 		plugin_name: name,
-		plugin_plan: 'business'
-	}
-);
+		plugin_plan: 'business',
+	} );
 
 const mapDispatchToProps = dispatch => ( {
 	onClick: name => dispatch( trackClick( name ) )
 } );
 
-export default connect( null, mapDispatchToProps )( BusinessPluginsPanel );
+export default connect( null, mapDispatchToProps )( localize( BusinessPluginsPanel ) );
 
--- client/my-sites/plugins-wpcom/plugin-panel.jsx
+++ client/my-sites/plugins-wpcom/plugin-panel.jsx
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import { connect } from 'react-redux';
-import get from 'lodash/get';
+import { get } from 'lodash';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -35,53 +35,54 @@ import {
  */
 const linkInterpolator = replacements => plugin => {
 	const { descriptionLink: link } = plugin;
-	const descriptionLink = Object.keys( replacements )
-		.reduce( ( s, r ) => s.replace( new RegExp( `{${ r }}` ), replacements[ r ] ), link );
+	const descriptionLink = Object
+		.keys( replacements )
+		.reduce(
+			( s, r ) => s.replace( new RegExp( `{${ r }}` ), replacements[ r ] ),
+			link
+		);
 
-	return Object.assign( {}, plugin, { descriptionLink } );
+	return { ...plugin, descriptionLink };
 };
 
-export const PluginPanel = React.createClass( {
-	render() {
-		const {
-			plan,
-			siteSlug
-		} = this.props;
+export const PluginPanel = ( {
+	plan,
+	siteSlug,
+	translate,
+} ) => {
+	const standardPluginsLink = `/plugins/standard/${ siteSlug }`;
+	const purchaseLink = `/plans/${ siteSlug }`;
 
-		const standardPluginsLink = `/plugins/standard/${ siteSlug }`;
-		const purchaseLink = `/plans/${ siteSlug }`;
+	const hasBusiness = isBusiness( plan ) || isEnterprise( plan );
+	const hasPremium = hasBusiness || isPremium( plan );
 
-		const hasBusiness = isBusiness( plan ) || isEnterprise( plan );
-		const hasPremium = hasBusiness || isPremium( plan );
+	const interpolateLink = linkInterpolator( { siteSlug } );
 
-		const interpolateLink = linkInterpolator( { siteSlug } );
+	const standardPlugins = defaultStandardPlugins.map( interpolateLink );
+	const premiumPlugins = defaultPremiumPlugins.map( interpolateLink );
+	const businessPlugins = defaultBusinessPlugins.map( interpolateLink );
 
-		const standardPlugins = defaultStandardPlugins.map( interpolateLink );
-		const premiumPlugins = defaultPremiumPlugins.map( interpolateLink );
-		const businessPlugins = defaultBusinessPlugins.map( interpolateLink );
-
-		return (
-			<div className="wpcom-plugin-panel">
-				<PageViewTracker path="/plugins/:site" title="Plugins > WPCOM Site" />
-				<Card compact className="plugins-wpcom__header">
-					<div className="plugins-wpcom__header-text">
-						<span className="plugins-wpcom__header-title">{ this.translate( 'Included Plugins' ) }</span>
-						<span className="plugins-wpcom__header-subtitle">
-							{ this.translate( 'Every plan includes a set of plugins specially tailored to supercharge your site.' ) }
-						</span>
-					</div>
-					<img className="plugins-wpcom__header-image" src="/calypso/images/plugins/plugins_hero.svg" />
-				</Card>
-				<StandardPluginsPanel plugins={ standardPlugins } displayCount={ 9 } />
-				<Card className="wpcom-plugin-panel__panel-footer" href={ standardPluginsLink }>
-					{ this.translate( 'View all standard plugins' ) }
-				</Card>
-				<PremiumPluginsPanel plugins={ premiumPlugins } isActive={ hasPremium } { ...{ purchaseLink } } />
-				<BusinessPluginsPanel plugins={ businessPlugins } isActive={ hasBusiness } { ...{ purchaseLink } } />
-			</div>
-		);
-	}
-} );
+	return (
+		<div className="wpcom-plugin-panel">
+			<PageViewTracker path="/plugins/:site" title="Plugins > WPCOM Site" />
+			<Card compact className="plugins-wpcom__header">
+				<div className="plugins-wpcom__header-text">
+					<span className="plugins-wpcom__header-title">{ translate( 'Included Plugins' ) }</span>
+					<span className="plugins-wpcom__header-subtitle">
+						{ translate( 'Every plan includes a set of plugins specially tailored to supercharge your site.' ) }
+					</span>
+				</div>
+				<img className="plugins-wpcom__header-image" src="/calypso/images/plugins/plugins_hero.svg" />
+			</Card>
+			<StandardPluginsPanel plugins={ standardPlugins } displayCount={ 9 } />
+			<Card className="wpcom-plugin-panel__panel-footer" href={ standardPluginsLink }>
+				{ translate( 'View all standard plugins' ) }
+			</Card>
+			<PremiumPluginsPanel plugins={ premiumPlugins } isActive={ hasPremium } { ...{ purchaseLink } } />
+			<BusinessPluginsPanel plugins={ businessPlugins } isActive={ hasBusiness } { ...{ purchaseLink } } />
+		</div>
+	);
+};
 
 const mapStateToProps = state => ( {
 	plan: get( getSelectedSite( state ), 'plan', {} ),
