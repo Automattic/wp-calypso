f46fd883b1f87f0c745411f152c50489b98a5d50
Miguel Fonseca <miguelcsf@gmail.com>
2016-11-21 14:28:16 +0000

    Guided Tours: Avoid collisions with First View (#9241)
    * WIP: TDD scaffolding
    
    * First View: getConfigForCurrentView: don't assume action log never empty
    
    * findEligibleTour: fix memoization dependencies
    
    Also, make sure findEligibleTour always returns either a string or
    undefined, avoid other falsy values.
    
    * Add hasViewJustBeenVisible selector for FirstView
    
    * Implement isConflictingWithFirstView w/ tests
    
    - Add a mock Stats tour in GT test fixtures
    - Require FirstView selectors in GT unit tests
    
    * First View: Memoize some selectors
    
    * Play with createSelector caching heuristics
    
    Useless stuff which I'll immediately revert, but I wanted to keep it in
    the history to remind me of how awful the idea is.
    
    * Revert "Play with createSelector caching heuristics"
    
    This reverts commit 3a57f7ee0077b0454d5e8db09f2f8b2b95bcedef.
    
    * Don't let FirstView conflicts affect ongoing tours
    
    At least for now, we'd like to avoid breaking a tour if, when
    navigating, we stumble upon a FV screen.
    
    - Break shouldBail into shouldBailAllTours and shouldBailNewTours
    - Simplify caching of `findEligibleTour`
    
    * hasViewJustBeenVisible -> hasTourJustBeenVisible
    
    Made no sense to care about recent First View triggers, only Guided
    Tours.
    
    * Prevent First View if tour ongoing
    
    Turns out we _do_ need this one anyway. ^_^'
    
    * Avoid circular dependencies
    
    I hate doing this, but maybe this will all become irrelevant either
    with #6914 or even #5954.

---
+++ client/state/ui/guided-tours/selectors/find-ongoing-tour.js
@@ -0,0 +1,20 @@
+/**
+ * External dependencies
+ */
+import { findLast } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { getActionLog } from 'state/ui/action-log/selectors';
+import { GUIDED_TOUR_UPDATE } from 'state/action-types';
+
+/*
+ * When applicable, returns the name of the tour that has been started and not
+ * yet finished or dimissed according to the action log.
+ */
+export default state => {
+	const last = findLast( getActionLog( state ), { type: GUIDED_TOUR_UPDATE } );
+	return last && ( last.shouldShow === undefined ) && last.tour;
+};
+
