e0c90fc0cdbee458d89294a2893590625ecec9b2
James Tien <southp0105@gmail.com>
2016-11-21 17:38:11 +0800

    Add tests for `ticketSupportConfigurationRequest` thunk

--- client/state/ticket-support/test/actions.js
+++ client/state/ticket-support/test/actions.js
@@ -2,23 +2,32 @@
  * External dependencies
  */
 import { assert } from 'chai';
+import sinon from 'sinon';
 
 /**
  * Internal dependencies
  */
 import {
+	ticketSupportConfigurationRequest,
 	ticketSupportConfigurationRequestSuccess,
 	ticketSupportConfigurationRequestFailure,
 } from '../actions';
 
 import {
+	TICKET_SUPPORT_CONFIGURATION_REQUEST,
 	TICKET_SUPPORT_CONFIGURATION_REQUEST_SUCCESS,
 	TICKET_SUPPORT_CONFIGURATION_REQUEST_FAILURE,
 } from 'state/action-types';
 
 import { dummyConfiguration, dummyError } from './test-data';
 
+import { useNock } from 'test/helpers/use-nock';
+import { useSandbox } from 'test/helpers/use-sinon';
+
 describe( 'ticket-support actions', () => {
+	let spy;
+	useSandbox( ( sandbox ) => spy = sandbox.spy() );
+
 	describe( '#ticketSupportConfigurationRequestSuccess', () => {
 		it( 'should return TICKET_SUPPORT_CONFIGURATION_REQUEST_SUCCESS', () => {
 			const action = ticketSupportConfigurationRequestSuccess( dummyConfiguration );
@@ -37,6 +46,51 @@ describe( 'ticket-support actions', () => {
 			assert.deepEqual( action, {
 				type: TICKET_SUPPORT_CONFIGURATION_REQUEST_FAILURE,
 				...dummyError,
+			} );
+		} );
+	} );
+
+	const apiUrl = 'https://public-api.wordpress.com:443';
+	const endpoint = '/rest/v1.1/help/tickets/kayako/mine';
+
+	describe( '#ticketSupportConfigurationRequest success', () => {
+		useNock( ( nock ) => {
+			nock( apiUrl )
+				.get( endpoint )
+				.reply( 200, dummyConfiguration );
+		} );
+
+		it( 'should be successful.', () => {
+			const action = ticketSupportConfigurationRequest()( spy );
+
+			assert( spy.calledWith( { type: TICKET_SUPPORT_CONFIGURATION_REQUEST } ) );
+
+			action.then( () => {
+				assert( spy.calledWith( {
+					type: TICKET_SUPPORT_CONFIGURATION_REQUEST_SUCCESS,
+					...dummyConfiguration,
+				} ) );
+			} );
+		} );
+	} );
+
+	describe( '#ticketSupportConfigurationRequest failed', () => {
+		useNock( ( nock ) => {
+			nock( apiUrl )
+				.get( endpoint )
+				.reply( dummyError.status, dummyError );
+		} );
+
+		it( 'should be failed.', () => {
+			const action = ticketSupportConfigurationRequest()( spy );
+
+			assert( spy.calledWith( { type: TICKET_SUPPORT_CONFIGURATION_REQUEST } ) );
+
+			action.then( () => {
+				assert( spy.calledWith( sinon.match( {
+					type: TICKET_SUPPORT_CONFIGURATION_REQUEST_FAILURE,
+					...dummyError,
+				} ) ) );
 			} );
 		} );
 	} );
