e83f561301ed46b97857d1348433d67da53bedcb
Bernhard Reiter <ockham@raz.or.at>
2016-11-23 00:56:32 +0100

    lib/plans: Drop getCurrentPlan() and addCurrentPlanToCartAndRedirect()
    Have consumers use getCurrentPlan() selector instead, and inline addCurrentPlanToCartAndRedirect().
    
    Fixes #8715

--- client/my-sites/upgrades/cart/cart-trial-ad.jsx
+++ client/my-sites/upgrades/cart/cart-trial-ad.jsx
@@ -2,6 +2,8 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
+import page from 'page';
 import i18n from 'i18n-calypso';
 
 /**
@@ -9,28 +11,30 @@ import i18n from 'i18n-calypso';
  */
 import CartAd from './cart-ad';
 import { cartItems } from 'lib/cart-values';
-import { addCurrentPlanToCartAndRedirect, getCurrentPlan, getDayOfTrial } from 'lib/plans';
+import { getDayOfTrial } from 'lib/plans';
+import { addItem } from 'lib/upgrades/actions';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { getSiteSlug } from 'state/sites/selectors';
+import { getCurrentPlan, isRequestingSitePlans } from 'state/sites/plans/selectors';
 
 const CartTrialAd = React.createClass( {
 	propTypes: {
 		cart: React.PropTypes.object.isRequired,
-		sitePlans: React.PropTypes.object.isRequired,
-		selectedSite: React.PropTypes.oneOfType( [
-			React.PropTypes.object,
-			React.PropTypes.bool
-		] ).isRequired
+		// connected props
+		currentPlan: React.PropTypes.object,
+		isRequestingSitePlans: React.PropTypes.bool,
+		selectedSiteSlug: React.PropTypes.string
 	},
 
 	addPlanAndRedirect( event ) {
 		event.preventDefault();
-
-		addCurrentPlanToCartAndRedirect( this.props.sitePlans, this.props.selectedSite );
+		addItem( cartItems.planItem( this.props.currentPlan.productSlug ) );
+		page( `/checkout/${ this.props.selectedSiteSlug }` );
 	},
 
 	render() {
-		const { cart, sitePlans } = this.props,
-			isDataLoading = ! sitePlans.hasLoadedFromServer || ! cart.hasLoadedFromServer,
-			currentPlan = getCurrentPlan( sitePlans.data );
+		const { cart, currentPlan, isRequestingSitePlans: isRequestingPlans } = this.props,
+			isDataLoading = isRequestingPlans || ! cart.hasLoadedFromServer;
 
 		if ( isDataLoading ||
 			! currentPlan.freeTrial ||
@@ -65,4 +69,13 @@ const CartTrialAd = React.createClass( {
 	}
 } );
 
-export default CartTrialAd;
+export default connect(
+	( state ) => {
+		const selectedSiteId = getSelectedSiteId( state );
+		return {
+			currentPlan: getCurrentPlan( state, selectedSiteId ),
+			isRequestingSitePlans: isRequestingSitePlans( state, selectedSiteId ),
+			selectedSiteSlug: getSiteSlug( state, selectedSiteId )
+		};
+	}
+)( CartTrialAd );
--- client/my-sites/upgrades/domain-search/free-trial-notice.jsx
+++ client/my-sites/upgrades/domain-search/free-trial-notice.jsx
@@ -2,34 +2,39 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
+import page from 'page';
 import i18n from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-import { addCurrentPlanToCartAndRedirect, getCurrentPlan, getDayOfTrial } from 'lib/plans';
+import { cartItems } from 'lib/cart-values';
+import { getDayOfTrial } from 'lib/plans';
+import { addItem } from 'lib/upgrades/actions';
 import Notice from 'components/notice';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { getSiteSlug } from 'state/sites/selectors';
+import { getCurrentPlan, isRequestingSitePlans } from 'state/sites/plans/selectors';
 
 const FreeTrialNotice = React.createClass( {
 	propTypes: {
 		cart: React.PropTypes.object.isRequired,
-		sitePlans: React.PropTypes.object.isRequired,
-		selectedSite: React.PropTypes.oneOfType( [
-			React.PropTypes.object,
-			React.PropTypes.bool
-		] ).isRequired
+		// connected props
+		currentPlan: React.PropTypes.object,
+		isRequestingSitePlans: React.PropTypes.bool,
+		selectedSiteSlug: React.PropTypes.string
 	},
 
 	addPlanAndRedirect( event ) {
 		event.preventDefault();
-
-		addCurrentPlanToCartAndRedirect( this.props.sitePlans, this.props.selectedSite );
+		addItem( cartItems.planItem( this.props.currentPlan.productSlug ) );
+		page( `/checkout/${ this.props.selectedSiteSlug }` );
 	},
 
 	render() {
-		const { cart, sitePlans } = this.props,
-			isDataLoading = ! cart.hasLoadedFromServer || ! sitePlans.hasLoadedFromServer,
-			currentPlan = getCurrentPlan( sitePlans.data );
+		const { cart, currentPlan, isRequestingSitePlans: isRequestingPlans } = this.props,
+			isDataLoading = isRequestingPlans || ! cart.hasLoadedFromServer;
 
 		if ( isDataLoading || ! currentPlan.freeTrial ) {
 			return null;
@@ -62,4 +67,13 @@ const FreeTrialNotice = React.createClass( {
 	}
 } );
 
-export default FreeTrialNotice;
+export default connect(
+	( state ) => {
+		const selectedSiteId = getSelectedSiteId( state );
+		return {
+			currentPlan: getCurrentPlan( state, selectedSiteId ),
+			isRequestingSitePlans: isRequestingSitePlans( state, selectedSiteId ),
+			selectedSiteSlug: getSiteSlug( state, selectedSiteId )
+		};
+	}
+)( FreeTrialNotice );
