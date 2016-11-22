0837a6173a2d0b5c12f3693502ade07cce978d1e
Steve Seear <steve.seear@gmail.com>
2016-11-22 16:00:11 +0000

    Allow file drop

--- client/my-sites/themes/theme-upload/index.jsx
+++ client/my-sites/themes/theme-upload/index.jsx
@@ -11,9 +11,15 @@ import HeaderCake from 'components/header-cake';
 import Card from 'components/card';
 import Gridicon from 'components/gridicon';
 import FilePicker from 'components/file-picker';
+import DropZone from 'components/drop-zone';
 import { localize } from 'i18n-calypso';
+import debugFactory from 'debug';
 
-const onPick = function() {
+const debug = debugFactory( 'calypso:themes:theme-upload' );
+
+const onFileSelect = function( files ) {
+	// files can be FileList object or array
+	debug( 'Chosen files:', files );
 };
 
 function upload( { translate } ) {
@@ -33,8 +39,9 @@ function upload( { translate } ) {
 				<span className="theme-upload__prompt">{ uploadPromptText }</span>
 				<span className="theme-upload__instructions">{ uploadInstructionsText }</span>
 				<div className="theme-upload__dropzone">
+					<DropZone onFilesDrop={ onFileSelect } />
 					<FilePicker accept="application/zip"
-								onPick={ onPick } >
+								onPick={ onFileSelect } >
 						<Gridicon className="theme-upload__dropzone-icon" icon="cloud-upload" size={ 48 } />
 						<span className="theme-upload__dropzone-text">
 							{ translate( 'Drop files or click here to upload' ) }
