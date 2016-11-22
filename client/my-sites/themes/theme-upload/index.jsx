4f64e2f1f04e73d7f17270dbeeec82ed6eb43133
Steve Seear <steve.seear@gmail.com>
2016-11-22 17:41:00 +0000

    Error notices for invalid drop

--- client/my-sites/themes/theme-upload/index.jsx
+++ client/my-sites/themes/theme-upload/index.jsx
@@ -13,56 +13,66 @@ import Gridicon from 'components/gridicon';
 import FilePicker from 'components/file-picker';
 import DropZone from 'components/drop-zone';
 import { localize } from 'i18n-calypso';
+import notices from 'notices';
 import debugFactory from 'debug';
 
 const debug = debugFactory( 'calypso:themes:theme-upload' );
 
-const onFileSelect = function( files ) {
-	// files can be FileList object or array
-	debug( 'Chosen files:', files );
-	if ( files.length !== 1 ) {
-		debug( 'multiple files not allowed' );
-		return;
-	}
-	const file = files[ 0 ] || files.item( 0 );
-	if ( file.type !== 'application/zip' ) {
-		debug( 'file must be a zip' );
-		return;
-	}
-	debug( 'zip file:', file );
-};
+class Upload extends React.Component {
 
-function upload( { translate } ) {
-	const uploadPromptText = translate(
-		'Do you have a custom theme to upload to your site?'
-	);
-	const uploadInstructionsText = translate(
-		"Make sure it's a single zip file, and upload it here."
-	);
-	const dropText = translate(
-		'Drop files or click here to upload'
-	);
+	constructor( props ) {
+		super( props );
+	}
 
-	return (
-		<div>
-			<HeaderCake onClick={ page.back }>{ translate( 'Upload theme' ) }</HeaderCake>
-			<Card>
-				<span className="theme-upload__prompt">{ uploadPromptText }</span>
-				<span className="theme-upload__instructions">{ uploadInstructionsText }</span>
-				<div className="theme-upload__dropzone">
-					<DropZone onFilesDrop={ onFileSelect } />
-					<FilePicker accept="application/zip" onPick={ onFileSelect } >
-						<Gridicon
-							className="theme-upload__dropzone-icon"
-							icon="cloud-upload"
-							size={ 48 } />
-						{ dropText }
-					</FilePicker>
-				</div>
-			</Card>
-		</div>
-	);
+	onFileSelect = ( files ) => {
+		const { translate } = this.props;
+		const errorMessage = translate( 'Please drop a single zip file' );
+
+		if ( files.length !== 1 ) {
+			notices.error( errorMessage );
+			return;
+		}
+		const file = files[ 0 ] || files.item( 0 );
+		if ( file.type !== 'application/zip' ) {
+			notices.error( errorMessage );
+			return;
+		}
+		debug( 'zip file:', file );
+	}
+
+	render() {
+		const { translate } = this.props;
+		const uploadPromptText = translate(
+			'Do you have a custom theme to upload to your site?'
+		);
+		const uploadInstructionsText = translate(
+			"Make sure it's a single zip file, and upload it here."
+		);
+		const dropText = translate(
+			'Drop files or click here to upload'
+		);
+
+		return (
+			<div>
+				<HeaderCake onClick={ page.back }>{ translate( 'Upload theme' ) }</HeaderCake>
+				<Card>
+					<span className="theme-upload__prompt">{ uploadPromptText }</span>
+					<span className="theme-upload__instructions">{ uploadInstructionsText }</span>
+					<div className="theme-upload__dropzone">
+						<DropZone onFilesDrop={ this.onFileSelect } />
+						<FilePicker accept="application/zip" onPick={ this.onFileSelect } >
+							<Gridicon
+								className="theme-upload__dropzone-icon"
+								icon="cloud-upload"
+								size={ 48 } />
+							{ dropText }
+						</FilePicker>
+					</div>
+				</Card>
+			</div>
+		);
+	}
 }
 
-export default localize( upload );
+export default localize( Upload );
 
