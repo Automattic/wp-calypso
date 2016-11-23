dbbe58aae38bd5bd3bfa28af5be1972e12513d5b
bluefuton <chris@bluefuton.com>
2016-11-23 17:08:27 +1300

    CSS tinkering

--- client/reader/list-stream/header.jsx
+++ client/reader/list-stream/header.jsx
@@ -14,24 +14,24 @@ import { isExternal } from 'lib/url';
 import FollowButton from 'components/follow-button/button';
 
 const ListStreamHeader = (
-	{ icon, isPlaceholder, title, description, showEdit, editUrl, showFollow, following, onFollowToggle, translate }
+	{ isPlaceholder, title, description, showEdit, editUrl, showFollow, following, onFollowToggle, translate }
 	) => {
 	const classes = classnames( {
 		'list-stream__header': true,
 		'is-placeholder': isPlaceholder,
-		'has-icon': !! icon,
 		'has-description': !! description
 	} );
 
 	return (
 		<Card className={ classes }>
-			{ icon &&
 			<span className="list-stream__header-icon">
-				{ icon }
-			</span> }
+				<Gridicon icon="list-unordered" size={ 24 } />
+			</span>
 
-			<h1 className="list-stream__header-title">{ title }</h1>
-			{ description && <p className="list-stream__header-description">{ description }</p> }
+			<div className="list-stream__header-details">
+				<h1 className="list-stream__header-title">{ title }</h1>
+				{ description && <p className="list-stream__header-description">{ description }</p> }
+			</div>
 
 			{ showFollow &&
 				<div className="list-stream__header-follow">
@@ -50,7 +50,6 @@ const ListStreamHeader = (
 };
 
 ListStreamHeader.propTypes = {
-	icon: React.PropTypes.element,
 	isPlaceholder: React.PropTypes.bool,
 	title: React.PropTypes.string,
 	description: React.PropTypes.string,
