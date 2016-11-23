fc1a0d5bfec5dbf65f41b9be6527313e5f856867
artpi <artpi@post.pl>
2016-11-23 18:33:33 +0100

    Add proper syntax

--- client/blocks/reader-quote/index.jsx
+++ client/blocks/reader-quote/index.jsx
@@ -3,4 +3,21 @@
  */
 import React from 'react';
 
-export default () => <div>{ 'Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.' }</div>;
+/**
+ * Internal Dependencies
+ */
+import getRandomQuote from './quotes';
+
+export default () => {
+	const quote = getRandomQuote();
+	return (
+		<div className="reader-quote">
+			<div className="reader-quote__content">
+				{ quote.content }
+			</div>
+			<div className="reader-quote__author">
+				{ quote.author }
+			</div>
+		</div>
+	);
+};
