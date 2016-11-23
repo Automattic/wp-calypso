217160ad614c1e4f0a900d01ecbaf1b4d891fa66
Miguel Fonseca <miguelcsf@gmail.com>
2016-11-23 18:43:03 +0000

    Import ThemeSheetTour from previous PoC PR

--- client/layout/guided-tours/config.js
+++ client/layout/guided-tours/config.js
@@ -4,8 +4,10 @@
 import { combineTours } from 'layout/guided-tours/config-elements';
 import { MainTour } from 'layout/guided-tours/main-tour';
 import { DesignShowcaseWelcomeTour } from 'layout/guided-tours/design-showcase-welcome-tour';
+import { ThemeSheetTour } from 'layout/guided-tours/theme-sheet-tour';
 
 export default combineTours( {
 	main: MainTour,
 	designShowcaseWelcome: DesignShowcaseWelcomeTour,
+	theme: ThemeSheetTour,
 } );
