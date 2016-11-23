c4c223d6f3b92e21b4bcd3650e314baa5ede21cf
Copons <copons@users.noreply.github.com>
2016-11-23 23:54:04 +0100

    Order plugins by importance

--- client/my-sites/plugins-wpcom/jetpack-plugins.js
+++ client/my-sites/plugins-wpcom/jetpack-plugins.js
@@ -16,6 +16,20 @@ const jetpackPlugins = ( translate = identity ) => [
 				plan: 'standard',
 			},
 			{
+				name: translate( 'SEO Tools' ),
+				link: 'https://support.wordpress.com/seo-tools/',
+				description: translate( 'Custom meta descriptions, social media previews, and more.' ),
+				plan: 'business',
+				feature: 'advanced-seo'
+			},
+			{
+				name: translate( 'Google Analytics' ),
+				link: 'https://support.wordpress.com/google-analytics/',
+				description: translate( 'Advanced features to complement WordPress.com stats. Funnel reports, goal conversion, and more.' ),
+				plan: 'business',
+				feature: 'google-analytics'
+			},
+			{
 				name: translate( 'Social Media' ),
 				link: 'https://support.wordpress.com/sharing/',
 				description: translate( 'Add social media buttons to your posts and pages.' ),
@@ -50,20 +64,6 @@ const jetpackPlugins = ( translate = identity ) => [
 				link: 'https://support.wordpress.com/comments/',
 				description: translate( 'Comment likes, user mentions, notifications, and more.' ),
 				plan: 'standard',
-			},
-			{
-				name: translate( 'SEO Tools' ),
-				link: 'https://support.wordpress.com/seo-tools/',
-				description: translate( 'Custom meta descriptions, social media previews, and more.' ),
-				plan: 'business',
-				feature: 'advanced-seo'
-			},
-			{
-				name: translate( 'Google Analytics' ),
-				link: 'https://support.wordpress.com/google-analytics/',
-				description: translate( 'Advanced features to complement WordPress.com stats. Funnel reports, goal conversion, and more.' ),
-				plan: 'business',
-				feature: 'google-analytics'
 			},
 		],
 	},
@@ -104,6 +104,13 @@ const jetpackPlugins = ( translate = identity ) => [
 				plan: 'standard',
 			},
 			{
+				name: translate( 'Custom Design' ),
+				link: 'https://support.wordpress.com/custom-design/',
+				description: translate( 'Customize your blog\'s look with custom fonts, a CSS editor, and more.' ),
+				plan: 'premium',
+				feature: 'advanced-design'
+			},
+			{
 				name: translate( 'Extended Customizer' ),
 				link: 'https://support.wordpress.com/customizer/',
 				description: translate( 'Edit colors and backgrounds.' ),
@@ -127,13 +134,6 @@ const jetpackPlugins = ( translate = identity ) => [
 				description: translate( 'Faster image loading and editing.' ),
 				plan: 'standard',
 			},
-			{
-				name: translate( 'Custom Design' ),
-				link: 'https://support.wordpress.com/custom-design/',
-				description: translate( 'Customize your blog\'s look with custom fonts, a CSS editor, and more.' ),
-				plan: 'premium',
-				feature: 'advanced-design'
-			},
 		],
 	},
 	{
@@ -154,6 +154,13 @@ const jetpackPlugins = ( translate = identity ) => [
 				plan: 'standard',
 			},
 			{
+				name: translate( 'Video Uploads' ),
+				link: 'https://support.wordpress.com/videopress/',
+				description: translate( 'Upload and host your video files on your site with VideoPress.' ),
+				plan: 'premium',
+				feature: 'video-upload'
+			},
+			{
 				name: translate( 'Importer' ),
 				link: 'https://support.wordpress.com/import/',
 				description: translate( 'Import your blog content from a variety of other blogging platforms.' ),
@@ -170,13 +177,6 @@ const jetpackPlugins = ( translate = identity ) => [
 				link: 'https://support.wordpress.com/markdown/',
 				description: translate( 'Text formatting using a lightweight markup language.' ),
 				plan: 'standard',
-			},
-			{
-				name: translate( 'Video Uploads' ),
-				link: 'https://support.wordpress.com/videopress/',
-				description: translate( 'Upload and host your video files on your site with VideoPress.' ),
-				plan: 'premium',
-				feature: 'video-upload'
 			},
 		],
 	},
