c8ce44f7d41e8bf9bec2664ba309d76b5ba270a8
Jake F - @samouri <samouri@users.noreply.github.com>
2016-11-23 13:18:10 -0800

    fixed bug where suggestions displayed twice in non-refresh

--- client/reader/search-stream/blank.jsx
+++ client/reader/search-stream/blank.jsx
@@ -6,38 +6,16 @@ import React from 'react';
 /**
  * Internal Dependencies
  */
-import { localize } from 'i18n-calypso';
-import Suggestion from './suggestion';
 
-export function BlankContent( { translate, suggestions } ) {
-	let suggest = null;
-	if ( suggestions ) {
-		let sugList = suggestions
-			.map( function( query ) {
-				return (
-					<Suggestion suggestion={ query } />
-				);
-			} );
-		sugList = sugList
-			.slice( 1 )
-			.reduce( function( xs, x ) {
-				return xs.concat( [ ', ', x ] );
-			}, [ sugList[ 0 ] ] );
-
-		suggest = (
-			<p className="search-stream__blank-suggestions">
-				{ translate( 'Suggestions: {{suggestions /}}.', { components: { suggestions: sugList } } ) }
-			</p> );
-	}
-
+// TODO post-refresh: delete this file
+export function BlankContent() {
 	const imgPath = '/calypso/images/drake/drake-404.svg';
 
 	return (
 		<div className="search-stream__blank">
-			{ suggest }
 			<img src={ imgPath } width="500" className="empty-content__illustration" />
 		</div>
 	);
 }
 
-export default localize( BlankContent );
+export default BlankContent;
