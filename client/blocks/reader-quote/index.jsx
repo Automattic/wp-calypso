f57a6faad0a14744e33056e7073b4ab8f8806d99
artpi <artpi@post.pl>
2016-11-23 18:16:38 +0100

    Add quote component

---
+++ client/blocks/reader-quote/index.jsx
@@ -0,0 +1,6 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+export default () => <div>{ 'Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.' }</div>;
