62ab1f764593774ebc9adde4bc0e5338259184b4
budzanowski <bartosz.budzanowski@gmail.com>
2016-11-24 13:45:59 +0100

    QueryActiveTheme implementation and README.

---
+++ client/components/data/query-active-theme/index.jsx
@@ -0,0 +1,51 @@
+/**
+ * External dependencies
+ */
+import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { requestActiveTheme } from 'state/themes/actions';
+import { isRequestingActiveTheme } from 'state/themes/selectors';
+
+class QueryActiveTheme extends Component {
+	static propTypes = {
+		siteId: PropTypes.oneOfType( [
+			PropTypes.number,
+			PropTypes.oneOf( [ 'wpcom' ] )
+		] ).isRequired,
+		// Connected props
+		isRequesting: PropTypes.bool.isRequired,
+		requestActiveTheme: PropTypes.func.isRequired,
+	}
+
+	componentDidMount() {
+		this.request( this.props );
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		if ( this.props.siteId === nextProps.siteId ) {
+			return;
+		}
+		this.request( nextProps );
+	}
+
+	request( props ) {
+		if ( ! props.isRequesting ) {
+			props.requestActiveTheme( props.siteId );
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default connect(
+	( state, { siteId } ) => ( {
+		isRequesting: isRequestingActiveTheme( state, siteId ),
+	} ),
+	{ requestActiveTheme }
+)( QueryActiveTheme );
