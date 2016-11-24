5a95bcf3769230f128e09a594f27426ca14f472e
Copons <copons@users.noreply.github.com>
2016-11-24 11:59:00 +0100

    Add upgrade nudge on Jetpack plugins page for non-biz .com

--- client/my-sites/plugins-wpcom/plugin-panel.jsx
+++ client/my-sites/plugins-wpcom/plugin-panel.jsx
@@ -21,19 +21,35 @@ import {
 } from 'lib/products-values';
 import JetpackPluginsPanel from './jetpack-plugins-panel';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
+import config from 'config';
+import { FEATURE_UPLOAD_PLUGINS } from 'lib/plans/constants';
+import UpgradeNudge from 'my-sites/upgrade-nudge';
 
 export const PluginPanel = ( {
 	plan,
 	siteSlug,
 	category,
 	search,
+	translate,
 } ) => {
 	const hasBusiness = isBusiness( plan ) || isEnterprise( plan );
 	const hasPremium = hasBusiness || isPremium( plan );
 
 	return (
 		<div className="plugins-wpcom__panel">
+
 			<PageViewTracker path="/plugins/:site" title="Plugins > WPCOM Site" />
+
+			{ config.isEnabled( 'automated-transfer' ) && ! hasBusiness &&
+				<div className="plugins-wpcom__upgrade-nudge">
+					<UpgradeNudge
+						feature={ FEATURE_UPLOAD_PLUGINS }
+						title={ translate( 'Upgrade to the Business plan to install third-party plugins.' ) }
+						message={ translate( 'Upgrade to the Business plan to install plugins.' ) }
+						event={ 'calypso_plugins_page_upgrade_nudge' }
+					/>
+				</div>
+			}
 
 			<JetpackPluginsPanel { ...{
 				siteSlug,
