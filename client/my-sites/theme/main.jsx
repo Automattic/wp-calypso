cfda846aff470acce08b347e197dc98826d159a9
Bernhard Reiter <ockham@raz.or.at>
2016-11-25 16:45:07 +0100

    Theme Sheet: Use QueryActiveTheme component instead of QueryActiveTheme

--- client/my-sites/theme/main.jsx
+++ client/my-sites/theme/main.jsx
@@ -31,7 +31,7 @@ import { isUserPaid } from 'state/purchases/selectors';
 import { getForumUrl } from 'my-sites/themes/helpers';
 import { isPremiumTheme as isPremium } from 'state/themes/utils';
 import ThanksModal from 'my-sites/themes/thanks-modal';
-import QueryCurrentTheme from 'components/data/query-current-theme';
+import QueryActiveTheme from 'components/data/query-active-theme';
 import QuerySitePlans from 'components/data/query-site-plans';
 import QueryUserPurchases from 'components/data/query-user-purchases';
 import ThemesSiteSelectorModal from 'my-sites/themes/themes-site-selector-modal';
@@ -439,7 +439,7 @@ const ThemeSheet = React.createClass( {
 					link={ links } />
 				<PageViewTracker path={ analyticsPath } title={ analyticsPageTitle } />
 				{ this.renderBar() }
-				{ siteID && <QueryCurrentTheme siteId={ siteID } /> }
+				{ siteID && <QueryActiveTheme siteId={ siteID } /> }
 				<ThanksModal
 					site={ this.props.selectedSite }
 					source={ 'details' } />
