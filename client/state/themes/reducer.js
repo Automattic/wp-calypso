82a34dc8a562a7be92bb506f0f61e7a1e7558c5a
budzanowski <bartosz.budzanowski@gmail.com>
2016-11-27 02:46:11 +0100

    Add new reducer and action.

--- client/state/themes/reducer.js
+++ client/state/themes/reducer.js
@@ -22,6 +22,7 @@ import {
 	THEME_ACTIVATE_REQUEST,
 	THEME_ACTIVATE_REQUEST_SUCCESS,
 	THEME_ACTIVATE_REQUEST_FAILURE,
+	THEME_CLEAR_ACTIVATED,
 	ACTIVE_THEME_REQUEST,
 	ACTIVE_THEME_REQUEST_SUCCESS,
 	ACTIVE_THEME_REQUEST_FAILURE,
@@ -57,7 +58,7 @@ export const activeThemes = createReducer( {}, {
 	activeThemesSchema
  );
 
- /**
+/**
  * Returns the updated theme activation state after an action has been
  * dispatched. The state reflects a mapping of site ID to a boolean
  * reflecting whether a theme is being activated on that site.
@@ -83,6 +84,26 @@ export function activationRequests( state = {}, action ) {
 
 	return state;
 }
+
+/**
+ * Returns the updated completed theme activation requess state after an action has been
+ * dispatched. The state reflects a mapping of site ID to boolean, reflecting whether
+ * activation request has finished or has been cleared.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action payload
+ * @return {Object}        Updated state
+ */
+export const completedActivationRequests = createReducer( {}, {
+	[ THEME_ACTIVATE_REQUEST_SUCCESS ]: ( state, { siteId } ) => ( {
+		...state,
+		[ siteId ]: true,
+	} ),
+	[ THEME_CLEAR_ACTIVATED ]: ( state, { siteId } ) => ( {
+		...state,
+		[ siteId ]: false,
+	} ) }
+);
 
 /**
  * Returns the updated active theme request state after an action has been
