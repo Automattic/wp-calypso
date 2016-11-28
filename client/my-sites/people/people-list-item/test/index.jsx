5534c1b2437e21b9f8bd2881d4e35851cde0a6d6
Mark Ryall <mark@ryall.name>
2016-11-28 20:45:56 +1000

    Collapse render to a single line.

--- client/my-sites/people/people-list-item/test/index.jsx
+++ client/my-sites/people/people-list-item/test/index.jsx
@@ -33,10 +33,7 @@ describe( 'PeopleListItem', function() {
 	} );
 
 	it( 'renders CompactCard', function() {
-		const wrapper = shallow(
-			<PeopleListItem
-			/>
-		);
+		const wrapper = shallow( <PeopleListItem /> );
 
 		expect( wrapper.find( CompactCard ).length ).to.equal( 1 );
 		expect( wrapper.find( CompactCard ).props().className ).to.equal( 'people-list-item' );
