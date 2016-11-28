5411ae588f22f55c68b616008b64700f616f5682
Marin Atanasov <tyxla@users.noreply.github.com>
2016-11-28 16:25:49 +0200

    Jetpack Settings: Add selectors for retrieving general module data (#9661)
    * Jetpack Settings: Add getModules and getModule selectors
    
    * Jetpack Settings: Add tests for getModules and getModule selectors

--- client/state/jetpack-settings/modules/test/selectors.js
+++ client/state/jetpack-settings/modules/test/selectors.js
@@ -7,12 +7,15 @@ import {
 	isActivatingModule,
 	isDeactivatingModule,
 	isModuleActive,
-	isFetchingModules
+	isFetchingModules,
+	getModules,
+	getModule
 } from '../selectors';
 
 import {
 	modules as MODULES_FIXTURE,
-	requests as REQUESTS_FIXTURE
+	requests as REQUESTS_FIXTURE,
+	moduleData as MODULE_DATA_FIXTURE
 } from './fixture';
 
 describe( 'selectors', () => {
@@ -151,6 +154,85 @@ describe( 'selectors', () => {
 				siteId = 123456;
 			const output = isFetchingModules( stateIn, siteId );
 			expect( output ).to.be.true;
+		} );
+	} );
+
+	describe( '#getModules', () => {
+		it( 'should return data for all modules for a known site', () => {
+			const stateIn = {
+					jetpackSettings: {
+						jetpackModules: {
+							items: {
+								123456: MODULE_DATA_FIXTURE
+							}
+						}
+					}
+				},
+				siteId = 123456;
+			const output = getModules( stateIn, siteId );
+			expect( output ).to.eql( MODULE_DATA_FIXTURE );
+		} );
+
+		it( 'should return null for an unknown site', () => {
+			const stateIn = {
+					jetpackSettings: {
+						jetpackModules: {
+							items: {
+								654321: MODULE_DATA_FIXTURE
+							}
+						}
+					}
+				},
+				siteId = 123456;
+			const output = getModules( stateIn, siteId );
+			expect( output ).to.be.null;
+		} );
+	} );
+
+	describe( '#getModule', () => {
+		it( 'should return data for a specified module for a known site', () => {
+			const stateIn = {
+					jetpackSettings: {
+						jetpackModules: {
+							items: {
+								123456: MODULE_DATA_FIXTURE
+							}
+						}
+					}
+				},
+				siteId = 123456;
+			const output = getModule( stateIn, siteId, 'module-a' );
+			expect( output ).to.eql( MODULE_DATA_FIXTURE[ 'module-a' ] );
+		} );
+
+		it( 'should return null for an unknown site', () => {
+			const stateIn = {
+					jetpackSettings: {
+						jetpackModules: {
+							items: {
+								654321: MODULE_DATA_FIXTURE
+							}
+						}
+					}
+				},
+				siteId = 123456;
+			const output = getModule( stateIn, siteId, 'module-a' );
+			expect( output ).to.be.null;
+		} );
+
+		it( 'should return null for an unknown module', () => {
+			const stateIn = {
+					jetpackSettings: {
+						jetpackModules: {
+							items: {
+								123456: MODULE_DATA_FIXTURE
+							}
+						}
+					}
+				},
+				siteId = 123456;
+			const output = getModule( stateIn, siteId, 'module-z' );
+			expect( output ).to.be.null;
 		} );
 	} );
 } );
