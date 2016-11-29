1beaeee57f91bb03c43690de1cd186fb004ec4bd
bluefuton <chris@bluefuton.com>
2016-11-30 11:56:54 +1300

    Add getFirstImageForTag selector and tests

--- client/state/reader/tags/images/selectors.js
+++ client/state/reader/tags/images/selectors.js
@@ -1,4 +1,26 @@
 /**
+ * External dependencies
+ */
+import { get, head } from 'lodash';
+
+/**
+ * Returns the first image available for a given tag.
+ *
+ * @param  {Object}  state  Global state tree
+ * @param  {String}  tag 	Tag
+ * @return {Object} Image
+ */
+export function getFirstImageForTag( state, tag ) {
+	const items = get( state, 'reader.tags.images.items' );
+
+	if ( ! items || ! items[ tag ] ) {
+		return undefined;
+	}
+
+	return head( state.reader.tags.images.items[ tag ] );
+}
+
+/**
  * Returns true if a request is in progress to retrieve the tag images
  * for a given tag.
  *
@@ -9,3 +31,4 @@
 export function isRequestingTagImages( state, tag ) {
 	return !! state.reader.tags.images.requesting[ tag ];
 }
+
--- client/state/reader/tags/images/test/selectors.js
+++ client/state/reader/tags/images/test/selectors.js
@@ -7,10 +7,51 @@ import { expect } from 'chai';
  * Internal dependencies
  */
 import {
+	getFirstImageForTag,
 	isRequestingTagImages
 } from '../selectors';
 
 describe( 'selectors', () => {
+	describe( '#getFirstImageForTag()', () => {
+		it( 'should return undefined if there is no image available', () => {
+			const state = {
+				reader: {
+					tags: {
+						images: {
+							items: {}
+						}
+					}
+				}
+			};
+			expect( getFirstImageForTag( state, 'banana' ) ).to.equal( undefined );
+		} );
+
+		it( 'should return the first image if images exist for a tag', () => {
+			const firstBananaImage = { url: 'http://example.com/banana1.jpg' };
+			const secondBananaImage = { url: 'http://example.com/banana2.jpg' };
+			const firstAppleImage = { url: 'http://example.com/apple1.jpg' };
+			const state = {
+				reader: {
+					tags: {
+						images: {
+							items: {
+								banana: [
+									firstBananaImage,
+									secondBananaImage
+								],
+								apple: [
+									firstAppleImage
+								]
+							}
+						}
+					}
+				}
+			};
+			expect( getFirstImageForTag( state, 'banana' ) ).to.eql( firstBananaImage );
+			expect( getFirstImageForTag( state, 'apple' ) ).to.eql( firstAppleImage );
+		} );
+	} );
+
 	describe( '#isRequestingTagImages()', () => {
 		it( 'should return true if requesting images for the specified tag', () => {
 			const state = {
