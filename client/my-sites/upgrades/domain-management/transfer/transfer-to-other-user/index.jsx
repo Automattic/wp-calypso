4a4176a18ee373cc512683cc68649b034206c8f6
Umur Kontaci <umur@umur.io>
2016-11-30 13:38:40 -0500

    Domain Management: Transfer: Use and display correct data in transfer to other users

--- client/my-sites/upgrades/domain-management/transfer/transfer-to-other-user/index.jsx
+++ client/my-sites/upgrades/domain-management/transfer/transfer-to-other-user/index.jsx
@@ -3,6 +3,7 @@
  **/
 import React from 'react';
 import { connect } from 'react-redux';
+import includes from 'lodash/includes';
 
 /**
  * Internal Dependencies
@@ -11,19 +12,46 @@ import { localize } from 'i18n-calypso';
 import Card from 'components/card';
 import { requestSiteRoles } from 'state/site-roles/actions';
 import { getSiteRoles } from 'state/site-roles/selectors';
+import { getCurrentUser } from 'state/current-user/selectors';
+import Header from 'my-sites/upgrades/domain-management/components/header';
+import Main from 'components/main';
+import paths from 'my-sites/upgrades/paths';
+import FormSelect from 'components/forms/form-select';
+import FormButton from 'components/forms/form-button';
+import FormFieldset from 'components/forms/form-fieldset';
 
 class TransferOtherUser extends React.Component {
 	render() {
+		const { selectedSite, selectedDomainName: domainName, translate, users } = this.props;
+		const { slug } = selectedSite;
+
+		const availableUsers = users
+			.filter( user => includes( user.roles, 'administrator' ) && user.ID !== this.props.currentUser.ID );
+
 		return (
-			<Card>
-				<ul>
-					{ this.props.users.map( ( { first_name, last_name, nice_name } ) => (
-						<li>
-							{ ( first_name && last_name ) ? first_name + ' ' + last_name : nice_name }
-						</li>
-					) ) }
-				</ul>
-			</Card>
+			<Main className="transfer-to-other-user">
+				<Header
+					selectedDomainName={ domainName }
+					backHref={ paths.domainManagementEdit( slug, domainName ) }>
+					{ translate( 'Transfer Domain To Another User' ) }
+				</Header>
+				<Card>
+					<div>
+						{ translate( 'Transferring a domain to another user will give all the rights of the domain to that user. ' +
+							'Please choose a user to the transfer %(domainName)s.', { args: { domainName } } ) }
+					</div>
+					<FormFieldset>
+						<FormSelect className="transfer-to-other-user__select">
+							{ availableUsers.map( ( { first_name, last_name, nice_name }, idx ) => (
+								<option key={ idx }>
+									{ ( first_name && last_name ) ? `${ first_name } ${ last_name } (${ nice_name })` : nice_name }
+								</option>
+							) ) }
+						</FormSelect>
+					</FormFieldset>
+					<FormButton>{ translate( 'Transfer Domain' ) }</FormButton>
+				</Card>
+			</Main>
 		);
 	}
 }
@@ -31,7 +59,8 @@ class TransferOtherUser extends React.Component {
 export default connect(
 	( state, ownProps ) => {
 		return {
-			siteRoles: getSiteRoles( state, ownProps.selectedSite.ID )
+			siteRoles: getSiteRoles( state, ownProps.selectedSite.ID ),
+			currentUser: getCurrentUser( state )
 		};
 	},
 	{ requestSiteRoles }
