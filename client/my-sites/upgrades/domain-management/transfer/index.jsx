9cc5df1e0be5fac6ef8959dafdc6428fdd3aa0f4
Umur Kontaci <umur@umur.io>
2016-11-30 12:30:52 -0500

    Domain Management: Transfer: Add header to transfer landing page

--- client/my-sites/upgrades/domain-management/transfer/index.jsx
+++ client/my-sites/upgrades/domain-management/transfer/index.jsx
@@ -1,31 +1,42 @@
 /**
  * External Dependencies
- **/
+ */
 import React from 'react';
 
 /**
  * Internal Dependencies
- **/
+ */
 import { localize } from 'i18n-calypso';
 import paths from 'my-sites/upgrades/paths';
 import VerticalNav from 'components/vertical-nav';
 import VerticalNavItem from 'components/vertical-nav/item';
+import Header from 'my-sites/upgrades/domain-management/components/header';
+import Main from 'components/main';
 
 class Transfer extends React.Component {
 	render() {
+		const slug = this.props.selectedSite.slug,
+			domainName = this.props.selectedDomainName;
 		return (
-		<VerticalNav>
-			<VerticalNavItem path={
-				paths.domainManagementTransferOut( this.props.selectedSite.slug, this.props.selectedDomainName )
-			}>
-				{ this.props.translate( 'Transfer Out' ) }
-			</VerticalNavItem>
-			<VerticalNavItem path={
-				paths.domainManagementTransferToAnotherUser( this.props.selectedSite.slug, this.props.selectedDomainName )
-			}>
-				{ this.props.translate( 'Transfer to another user' ) }
-			</VerticalNavItem>
-		</VerticalNav>
+		<Main className="domain-management-transfer">
+			<Header
+				selectedDomainName={ domainName }
+				backHref={ paths.domainManagementEdit( slug, domainName ) }>
+				{ this.props.translate( 'Transfer Domain' ) }
+			</Header>
+			<VerticalNav>
+				<VerticalNavItem path={
+					paths.domainManagementTransferOut( slug, domainName )
+				}>
+					{ this.props.translate( 'Transfer to another registrar' ) }
+				</VerticalNavItem>
+				<VerticalNavItem path={
+					paths.domainManagementTransferToAnotherUser( slug, domainName )
+				}>
+					{ this.props.translate( 'Transfer to another user' ) }
+				</VerticalNavItem>
+			</VerticalNav>
+		</Main>
 		);
 	}
 }
