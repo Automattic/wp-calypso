17ac1f555d859ab13e061666f2c1e20f71d0ae0e
Umur Kontaci <umur@umur.io>
2016-11-30 12:30:52 -0500

    Domain Management: Transfer: Add header to transfer landing page

--- client/my-sites/upgrades/domain-management/components/header/index.jsx
+++ client/my-sites/upgrades/domain-management/components/header/index.jsx
@@ -11,7 +11,7 @@ const HeaderCake = require( 'components/header-cake' );
 const DomainManagementHeader = React.createClass( {
 	render() {
 		return (
-			<HeaderCake className="domain-management-header" onClick={ this.props.onClick }>
+			<HeaderCake className="domain-management-header" onClick={ this.props.onClick } backHref={ this.props.backHref }>
 				<div className="domain-management-header__children">
 					{ this.domainName() }
 					<span className="domain-management-header__title">
