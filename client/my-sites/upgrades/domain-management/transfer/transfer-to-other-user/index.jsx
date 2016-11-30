07d0da0350714ff9379da6d06e5107be628e9179
Umur Kontaci <umur@umur.io>
2016-11-30 11:35:08 -0500

    Domain Management: Transfer: Add user data

--- client/my-sites/upgrades/domain-management/transfer/transfer-to-other-user/index.jsx
+++ client/my-sites/upgrades/domain-management/transfer/transfer-to-other-user/index.jsx
@@ -2,16 +2,37 @@
  * External Dependencies
  **/
 import React from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal Dependencies
  **/
 import { localize } from 'i18n-calypso';
+import Card from 'components/card';
+import { requestSiteRoles } from 'state/site-roles/actions';
+import { getSiteRoles } from 'state/site-roles/selectors';
 
 class TransferOtherUser extends React.Component {
 	render() {
-		return <div>{ this.props.translate( 'Hello' ) }</div>;
+		return (
+			<Card>
+				<ul>
+					{ this.props.users.map( ( { first_name, last_name, nice_name } ) => (
+						<li>
+							{ ( first_name && last_name ) ? first_name + ' ' + last_name : nice_name }
+						</li>
+					) ) }
+				</ul>
+			</Card>
+		);
 	}
 }
 
-export default localize( TransferOtherUser );
+export default connect(
+	( state, ownProps ) => {
+		return {
+			siteRoles: getSiteRoles( state, ownProps.selectedSite.ID )
+		};
+	},
+	{ requestSiteRoles }
+)( localize( TransferOtherUser ) );
