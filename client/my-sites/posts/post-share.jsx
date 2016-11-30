3af35e13a88b41e9b427739521e23c0167d23744
artpi <artpi@post.pl>
2016-11-30 10:43:56 +0100

    Change subtitle copy to a bit more professional

--- client/my-sites/posts/post-share.jsx
+++ client/my-sites/posts/post-share.jsx
@@ -129,9 +129,16 @@ const PostSharing = React.createClass( {
 				{ this.props.failure && <Notice status="is-error" onDismissClick={ this.dismiss }>{ this.translate( `Something went wrong. Please don't be mad.` ) }</Notice> }
 				<div className={ classes }>
 					{ this.props.siteId && <QueryPostTypes siteId={ this.props.siteId } /> }
-					<h3 className="posts__post-share-title">
-						{ this.translate( 'Share the post and spread the word!' ) }
-					</h3>
+					<h4 className="posts__post-share-title">
+						{ this.translate( 'Publicize your content' ) }
+					</h4>
+					<div className="posts__post-share-subtitle">
+						{ this.translate( 'Share your post on all of your connected social media accounts using {{a}}Publicize{{/a}}', {
+							components: {
+								a: <a href={ '/sharing/' + this.props.siteId } />
+							}
+						} ) }
+					</div>
 					<div className="posts__post-share-main">
 						<div className="posts__post-share-form">
 							{ this.renderMessage() }
