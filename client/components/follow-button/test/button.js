3f4896d00ba8c2f0a761883a02eb27ff5a3c3ab9
bluefuton <chris@bluefuton.com>
2016-11-30 13:06:00 +1300

    Allow a custom follow label to be specified

---
+++ client/components/follow-button/test/button.js
@@ -0,0 +1,19 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { expect } from 'chai';
+import { render } from 'enzyme';
+
+/**
+ * Internal dependencies
+ */
+import FollowButton from '../button';
+
+describe( 'FollowButton', () => {
+	// it should have a class of `card`
+	it( 'should apply a custom follow label', () => {
+		const wrapper = render( <FollowButton followLabel="Follow Tag" /> );
+		expect( wrapper.text() ).to.contain( 'Follow Tag' );
+	} );
+} );
