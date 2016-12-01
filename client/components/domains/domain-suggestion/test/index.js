335da5d311e32dce2cfcfc3923063abf5b12564f
Marco Aurélio <thecoreh@gmail.com>
2016-12-01 14:30:09 -0200

    Signup: Remove clickable domain row AB test (#9309)
    * Remove clickable row abtest
    
    * Code style
    
    * Lint fixes
    
    * Appease the linter ༼ つ ◕_◕ ༽つ
    
    * Remove dead code
    
    * Remove A/B test logic from unit test
    
    * Fix role property
    
    * Add missing newline

--- client/components/domains/domain-suggestion/test/index.js
+++ client/components/domains/domain-suggestion/test/index.js
@@ -14,14 +14,11 @@ import useMockery from 'test/helpers/use-mockery';
 import EmptyComponent from 'test/helpers/react/empty-component';
 
 describe( 'Domain Suggestion', function() {
-	let abtestResult, DomainSuggestion;
+	let DomainSuggestion;
 
 	useFakeDom();
 	useMockery( ( mockery ) => {
 		mockery.registerMock( 'components/plans/premium-popover', EmptyComponent );
-		mockery.registerMock( 'lib/abtest', {
-			abtest: () => abtestResult
-		} );
 	} );
 
 	before( () => {
@@ -30,44 +27,17 @@ describe( 'Domain Suggestion', function() {
 	} );
 
 	describe( 'has attributes', () => {
-		context( 'AB test clickableButton', () => {
-			before( () => {
-				abtestResult = 'clickableButton';
-			} );
+		it( 'should have data-e2e-domain attribute for e2e testing', () => {
+			const domainSuggestion = shallow(
+				<DomainSuggestion
+					buttonContent="Click Me"
+					domain="example.com"
+					isAdded={ false }
+					onButtonClick={ noop }
+					priceRule="PRICE" />
+			);
 
-			it( 'should have data-e2e-domain attribute for e2e testing', () => {
-				const domainSuggestion = shallow(
-					<DomainSuggestion
-						buttonContent="Click Me"
-						domain="example.com"
-						isAdded={ false }
-						onButtonClick={ noop }
-						priceRule="PRICE" />
-				);
-
-				const domainSuggestionButton = domainSuggestion.find( '.domain-suggestion__select-button' );
-				expect( domainSuggestionButton.length ).to.equal( 1 );
-				expect( domainSuggestionButton.props()[ 'data-e2e-domain' ] ).to.equal( 'example.com' );
-			} );
-		} );
-
-		context( 'AB test clickableRow', () => {
-			before( () => {
-				abtestResult = 'clickableRow';
-			} );
-
-			it( 'should have data-e2e-domain attribute for e2e testing', () => {
-				const domainSuggestion = shallow(
-					<DomainSuggestion
-						buttonContent="Click Me"
-						domain="example.com"
-						isAdded={ false }
-						onButtonClick={ noop }
-						priceRule="PRICE" />
-				);
-
-				expect( domainSuggestion.props()[ 'data-e2e-domain' ] ).to.equal( 'example.com' );
-			} );
+			expect( domainSuggestion.props()[ 'data-e2e-domain' ] ).to.equal( 'example.com' );
 		} );
 	} );
 } );
