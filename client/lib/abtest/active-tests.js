45a592405035180950f3a12636d5d66b94d561e8
Matt Husby <matthusby@gmail.com>
2016-12-01 17:27:20 -0500

    Remove the abtest and code for G Suite on signup.

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -98,16 +98,6 @@ module.exports = {
 		allowExistingUsers: true
 	},
 
-	gSuiteOnSignup: {
-		datestamp: '20161025',
-		variations: {
-			original: 50,
-			removed: 50
-		},
-		defaultVariation: 'original',
-		allowExistingUsers: true
-	},
-
 	jetpackConnectPlansFirst: {
 		datestamp: '20161024',
 		variations: {
