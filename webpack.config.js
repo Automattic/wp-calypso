7ebca9433c59a1ba381f4fbef26d82359f159652
ehg <chris@ehg.sh>
2016-12-02 17:12:40 +0000

    Update path lookup.
    don't keep .gitkeep

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ const webpackConfig = {
 	},
 	resolve: {
 		extensions: [ '', '.json', '.js', '.jsx' ],
-		root: [ path.join( __dirname, 'client' ) ],
+		root: [ path.join( __dirname, 'client' ), path.join( __dirname, 'client', 'extensions' ) ],
 		modulesDirectories: [ 'node_modules' ],
 		alias: {
 			'react-virtualized': 'react-virtualized/dist/commonjs',
