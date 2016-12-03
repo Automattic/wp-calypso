7eef8a748fe4ffd2dc34eb91792148933d659651
Miguel Fonseca <miguelcsf@gmail.com>
2016-12-03 12:39:19 +0000

    Update README.md: copy changes, add links
    Add namespace class.
    
    Adjust FAQ copy.
    
    Only concat sections once
    
    Hello World card: copy changes

--- client/extensions/hello-world/index.js
+++ client/extensions/hello-world/index.js
@@ -17,30 +17,32 @@ import FAQItem from 'components/faq/faq-item';
 
 const render = ( context ) => {
 	renderWithReduxStore( (
-		<Main>
+		<Main className="hello-world__main">
 			<SectionHeader label="Hello, World!">🌞</SectionHeader>
 			<Card>
 				<p>This section is the home for the <strong>Hello World</strong> extension.</p>
-				<p>Extensions are setup to function in a semi-isolated environment, with their own url path and code chunk magic (using the power of Webpack) to assure code is loaded only when needed. Think of extensions as individual "apps" you can access in Calypso to interact with your plugin functionality in a focused way.</p>
+				<p>Extensions are set up to function in a semi-isolated environment, with their own URL path and code-chunk magic (using the power of webpack) to assure code is loaded only when needed. Think of extensions as individual "apps" you can access in Calypso to interact with your plugin functionality in a focused way.</p>
 			</Card>
 			<FAQ>
 				<FAQItem
 					question="How can I get started?"
 					answer={ [
-						'If you are looking at writing your own extension head over to the "extensions" folder where we have a more comprehensive readme file to walk you through the process. ',
+						'Head over to the "extensions" folder in the repository where we have a more comprehensive ',
+						'README file to walk you through the process. ',
 						<a href="https://github.com/Automattic/wp-calypso/tree/master/client/extensions" key="get-started">Get Started</a>
 					] }
 				/>
 				<FAQItem
 					question="Learning about Calypso"
 					answer={ [
-						'You can browse our docs without leaving the running application, just head over to the Devdocs section. You can also press the keys "gd" to navigate there from any page! ',
+						'You can browse our docs without leaving the running application, just go to the Devdocs section. ',
+						'Tip: press the keys "gd" to navigate there from any page! ',
 						<a href="https://github.com/Automattic/wp-calypso/tree/master/client/extensions" key="go-devdocs">Go to Devdocs</a>
 					] }
 				/>
 				<FAQItem
 					question="Have more questions?"
-					answer="Head over to the GitHub repository for Calypso and open an issue."
+					answer="Let's talk! Visit the Calypso GitHub repository and open an issue."
 				/>
 			</FAQ>
 		</Main>
