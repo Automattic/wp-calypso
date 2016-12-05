f920d86a027da16254415deeb949e2756df47a58
Alister Scott <alister.scott@automattic.com>
2016-12-06 09:35:08 +1000

    Fix indentation lost in rebase

--- client/blocks/taxonomy-manager/list-item.jsx
+++ client/blocks/taxonomy-manager/list-item.jsx
@@ -154,9 +154,9 @@ class TaxonomyManagerListItem extends Component {
 					</PopoverMenuItem>
 					{ canSetAsDefault && ! isDefault && <PopoverMenuSeparator /> }
 					{ canSetAsDefault && ! isDefault &&
-					<PopoverMenuItem onClick={ this.setAsDefault } icon="checkmark-circle">
-						{ translate( 'Set as default' ) }
-					</PopoverMenuItem>
+						<PopoverMenuItem onClick={ this.setAsDefault } icon="checkmark-circle">
+							{ translate( 'Set as default' ) }
+						</PopoverMenuItem>
 					}
 				</EllipsisMenu>
 				<Dialog
