b592cde2275724eea42b3b8d01302b59e2d81c17
Marco Aurélio <thecoreh@gmail.com>
2016-12-06 16:27:08 -0200

    Add FormTextInputWithAction component. (#8638)
    * Add FormTextInputWithAction component
    
    * Add FormTextInputWithAction to docs
    
    * Disallow action on enter if empty
    
    * Add flex shorthand and min width
    
    * Fix flex shorthand
    
    * Update prop types
    
    * Add support for error and valid states
    
    * Import PropTypes
    
    * Do not specify displayName
    
    * Do not specify displayName
    
    * Add group role to component
    
    * Refactor to use ES6 class syntax
    
    * Fix lint

---
+++ client/components/forms/form-text-input-with-action/index.jsx
@@ -0,0 +1,127 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes, Component } from 'react';
+import classNames from 'classnames';
+import { keys, omit, noop } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import FormTextInput from 'components/forms/form-text-input';
+import FormButton from 'components/forms/form-button';
+
+export default class FormTextInputWithAction extends Component {
+	static propTypes = {
+		action: PropTypes.node,
+		inputRef: PropTypes.func,
+		onFocus: PropTypes.func,
+		onBlur: PropTypes.func,
+		onKeyDown: PropTypes.func,
+		onChange: PropTypes.func,
+		onAction: PropTypes.func,
+		defaultValue: PropTypes.string,
+		disabled: PropTypes.bool,
+		isError: PropTypes.bool,
+		isValid: PropTypes.bool,
+	};
+
+	static defaultProps = {
+		defaultValue: '',
+		onFocus: noop,
+		onBlur: noop,
+		onKeyDown: noop,
+		onChange: noop,
+		onAction: noop,
+		isError: false,
+		isValid: false,
+	};
+
+	constructor() {
+		super();
+		this.state = {
+			focused: false,
+			value: null,
+		};
+	}
+
+	handleFocus = ( e ) => {
+		this.props.onFocus( e );
+		if ( e.defaultPrevented ) {
+			return;
+		}
+		this.setState( {
+			focused: true,
+		} );
+	};
+
+	handleBlur = ( e ) => {
+		this.props.onBlur( e );
+		if ( e.defaultPrevented ) {
+			return;
+		}
+		this.setState( {
+			focused: false,
+		} );
+	};
+
+	handleKeyDown = ( e ) => {
+		this.props.onKeyDown( e );
+		if ( e.defaultPrevented ) {
+			return;
+		}
+		if ( e.which === 13 && this.getValue() !== '' ) {
+			this.props.onAction( e );
+		}
+	};
+
+	handleChange = ( e ) => {
+		this.props.onChange( e );
+		if ( e.defaultPrevented ) {
+			return;
+		}
+		this.setState( {
+			value: e.target.value,
+		} );
+	};
+
+	getValue() {
+		if ( this.state.value === null ) {
+			return this.props.defaultValue;
+		}
+		return this.state.value;
+	}
+
+	render() {
+		return (
+			<div
+				className={ classNames( 'form-text-input-with-action', {
+					'is-focused': this.state.focused,
+					'is-disabled': this.props.disabled,
+					'is-error': this.props.isError,
+					'is-valid': this.props.isValid,
+				} ) }
+				role="group"
+			>
+				<FormTextInput
+					className="form-text-input-with-action__input"
+					ref={ this.props.inputRef }
+					disabled={ this.props.disabled }
+					defaultValue={ this.props.defaultValue }
+					onChange={ this.handleChange }
+					onFocus={ this.handleFocus }
+					onBlur={ this.handleBlur }
+					onKeyDown={ this.handleKeyDown }
+					{ ...omit( this.props, keys( this.constructor.propTypes ) ) }
+				/>
+				<FormButton
+					className="form-text-input-with-action__button is-compact"
+					disabled={ this.props.disabled || this.getValue() === '' }
+					onClick={ this.props.onAction }
+				>
+					{ this.props.action }
+				</FormButton>
+			</div>
+		);
+	}
+}
