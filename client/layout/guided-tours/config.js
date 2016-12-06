4de84d174c6006de7935be1401c7644dc52adda4
Leif Singer <github@singer.sh>
2016-12-06 17:18:17 +0100

    GuidedTours: add a design showcase tour (#9602)
    Add a design showcase tour to show people around `/design`.

--- client/layout/guided-tours/config.js
+++ client/layout/guided-tours/config.js
@@ -3,7 +3,9 @@
  */
 import { combineTours } from 'layout/guided-tours/config-elements';
 import { MainTour } from 'layout/guided-tours/main-tour';
+import { DesignShowcaseWelcomeTour } from 'layout/guided-tours/design-showcase-welcome-tour';
 
 export default combineTours( {
 	main: MainTour,
+	designShowcaseWelcome: DesignShowcaseWelcomeTour,
 } );
