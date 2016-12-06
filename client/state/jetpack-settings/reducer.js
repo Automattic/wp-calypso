8ede32eb66ef4288bde3513491e21437102e4337
Marin Atanasov <tyxla@abv.bg>
2016-12-06 14:44:19 +0200

    Jetpack Settings: Add reducer for retrieving module settings

--- client/state/jetpack-settings/reducer.js
+++ client/state/jetpack-settings/reducer.js
@@ -7,7 +7,9 @@ import { combineReducers } from 'redux';
  * Internal dependencies
  */
 import { reducer as jetpackModules } from './modules/reducer';
+import { reducer as jetpackModuleSettings } from './module-settings/reducer';
 
 export default combineReducers( {
-	jetpackModules
+	jetpackModules,
+	jetpackModuleSettings
 } );
