d715a18ef1b998a93304bd2825fc1c2cb15ed799
Marin Atanasov <tyxla@abv.bg>
2016-12-06 14:53:43 +0200

    Jetpack Settings: Add tests for module settings actions

---
+++ client/state/jetpack-settings/module-settings/test/fixture.js
@@ -0,0 +1,13 @@
+export const moduleSettings = {
+	'module-a': {
+		setting_1: 1234,
+		setting_2: true,
+		setting_3: 'some_value',
+	},
+	'module-b': {
+		setting_1: 1234,
+		setting_2: {
+			setting_3: 'some_value',
+		}
+	}
+};
