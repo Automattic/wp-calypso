7641e35877f4a1115d5755d81483407e1d621a43
Marin Atanasov <tyxla@abv.bg>
2016-12-06 14:41:41 +0200

    Jetpack Settings: Add action for updating settings of a module

--- client/state/jetpack-settings/module-settings/actions.js
+++ client/state/jetpack-settings/module-settings/actions.js
@@ -5,7 +5,10 @@ import {
 	JETPACK_MODULE_SETTINGS_RECEIVE,
 	JETPACK_MODULE_SETTINGS_REQUEST,
 	JETPACK_MODULE_SETTINGS_REQUEST_FAILURE,
-	JETPACK_MODULE_SETTINGS_REQUEST_SUCCESS
+	JETPACK_MODULE_SETTINGS_REQUEST_SUCCESS,
+	JETPACK_MODULE_SETTINGS_UPDATE,
+	JETPACK_MODULE_SETTINGS_UPDATE_SUCCESS,
+	JETPACK_MODULE_SETTINGS_UPDATE_FAILURE
 } from 'state/action-types';
 import wp from 'lib/wp';
 
@@ -42,6 +45,43 @@ export const fetchModuleSettings = ( siteId, moduleSlug ) => {
 					type: JETPACK_MODULE_SETTINGS_REQUEST_FAILURE,
 					siteId,
 					moduleSlug,
+					error: error.message
+				} );
+			} );
+	};
+};
+
+/**
+ * Update the settings of a module for a certain site.
+ *
+ * @param  {Int}      siteId      ID of the site.
+ * @param  {String}   moduleSlug  Slug of the module.
+ * @param  {Object}   settings    New module settings.
+ * @return {Function}             Action thunk to update the module settings when called.
+ */
+export const updateModuleSettings = ( siteId, moduleSlug, settings ) => {
+	return ( dispatch ) => {
+		dispatch( {
+			type: JETPACK_MODULE_SETTINGS_UPDATE,
+			siteId,
+			moduleSlug,
+			settings
+		} );
+
+		return wp.undocumented().updateJetpackModuleSettings( siteId, moduleSlug, settings )
+			.then( () => {
+				dispatch( {
+					type: JETPACK_MODULE_SETTINGS_UPDATE_SUCCESS,
+					siteId,
+					moduleSlug,
+					settings
+				} );
+			} ).catch( ( error ) => {
+				dispatch( {
+					type: JETPACK_MODULE_SETTINGS_UPDATE_FAILURE,
+					siteId,
+					moduleSlug,
+					settings,
 					error: error.message
 				} );
 			} );
