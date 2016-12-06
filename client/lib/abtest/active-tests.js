2def3bbb4568ff90d5416dd7e04186871677500c
Marek Hrabe <marekhrabe@me.com>
2016-12-06 17:37:17 +0100

    disable a/b test by default

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -132,8 +132,8 @@ module.exports = {
 	siteTitleTour: {
 		datestamp: '20161130',
 		variations: {
-			disabled: 50,
-			enabled: 50,
+			disabled: 1,
+			enabled: 0,
 		},
 		defaultVariation: 'disabled',
 		allowExistingUsers: true,
