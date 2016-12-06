604a11f0174adbccf2075953efda9a3fc840890a
Leif Singer <github@singer.sh>
2016-12-06 17:18:17 +0100

    GuidedTours: add a design showcase tour (#9602)
    Add a design showcase tour to show people around `/design`.

--- client/state/ui/guided-tours/contexts.js
+++ client/state/ui/guided-tours/contexts.js
@@ -3,6 +3,7 @@
  */
 import config from 'config';
 import { getSectionName, isPreviewShowing as isPreviewShowingSelector, getSelectedSite } from 'state/ui/selectors';
+import { getLastAction } from 'state/ui/action-log/selectors';
 import { getCurrentUser } from 'state/current-user/selectors';
 import { abtest } from 'lib/abtest';
 
@@ -28,6 +29,9 @@ export const isNewUser = state => {
 	const creation = Date.parse( user.date );
 	return ( Date.now() - creation ) <= WEEK_IN_MILLISECONDS;
 };
+
+export const hasUserInteractedWithComponent = componentName => state =>
+	getLastAction( state ).component === componentName;
 
 export const isSelectedSitePreviewable = state =>
 	getSelectedSite( state ) && getSelectedSite( state ).is_previewable;
