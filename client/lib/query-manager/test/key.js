60f6c22b81079447e605deebcef6dd4b82804f73
Andrew Duthie <andrew@andrewduthie.com>
2016-12-07 14:53:17 -0500

    Framework: Enhance base QueryKey to account for default query

--- client/lib/query-manager/test/key.js
+++ client/lib/query-manager/test/key.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import deepFreeze from 'deep-freeze';
 import { expect } from 'chai';
 
 /**
@@ -9,11 +10,51 @@ import { expect } from 'chai';
 import QueryKey from '../key';
 
 describe( 'QueryKey', () => {
+	describe( '.omit()', () => {
+		it( 'should return same query if omission behavior not defined', () => {
+			const original = deepFreeze( { ok: true } );
+			const pruned = QueryKey.omit( original );
+
+			expect( pruned ).to.equal( original );
+		} );
+
+		it( 'should omit values matching default query of extending class', () => {
+			class QueryKeyWithDefaults extends QueryKey {
+				static DEFAULT_QUERY = { ok: true };
+			}
+
+			const pruned = QueryKeyWithDefaults.omit( { ok: true, foo: null } );
+
+			expect( pruned ).to.eql( { foo: null } );
+		} );
+
+		it( 'should omit null values if configured by extending class', () => {
+			class QueryKeyWithNullOmission extends QueryKey {
+				static OMIT_NULL_VALUES = true;
+			}
+
+			const pruned = QueryKeyWithNullOmission.omit( { ok: true, foo: null } );
+
+			expect( pruned ).to.eql( { ok: true } );
+		} );
+	} );
+
 	describe( '.stringify()', () => {
 		it( 'should return a JSON string of the object', () => {
 			const key = QueryKey.stringify( { ok: true } );
 
 			expect( key ).to.equal( '[["ok",true]]' );
+		} );
+
+		it( 'should prune by omission behavior', () => {
+			class QueryKeyWithOmission extends QueryKey {
+				static DEFAULT_QUERY = { ok: true };
+				static OMIT_NULL_VALUES = true;
+			}
+
+			const key = QueryKeyWithOmission.stringify( { ok: true, foo: null } );
+
+			expect( key ).to.equal( '[]' );
 		} );
 
 		it( 'should return the same string for two objects with different property creation order', () => {
@@ -29,6 +70,17 @@ describe( 'QueryKey', () => {
 			const query = QueryKey.parse( '[["ok",true]]' );
 
 			expect( query ).to.eql( { ok: true } );
+		} );
+
+		it( 'should prune by omission behavior', () => {
+			class QueryKeyWithOmission extends QueryKey {
+				static DEFAULT_QUERY = { ok: true };
+				static OMIT_NULL_VALUES = true;
+			}
+
+			const query = QueryKeyWithOmission.parse( '[["ok",true],["foo",null]]' );
+
+			expect( query ).to.eql( {} );
 		} );
 	} );
 } );
