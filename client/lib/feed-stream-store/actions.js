0c1a07ef0fa667285743f7b337410dca7da16d91
Ben Lowery <ben.lowery@automattic.com>
2016-12-07 11:21:11 -0500

    Reader: es6ify actions and index

--- client/lib/feed-stream-store/actions.js
+++ client/lib/feed-stream-store/actions.js
@@ -1,17 +1,19 @@
-// External Dependencies
-var forEach = require( 'lodash/forEach' ),
-	get = require( 'lodash/get' );
+/**
+ *  External Dependencies
+ */
+import { forEach, get } from 'lodash';
 
-// Internal dependencies
-var Dispatcher = require( 'dispatcher' ),
-	ActionType = require( './constants' ).action,
-	FeedPostStoreActions = require( 'lib/feed-post-store/actions' ),
-	feedPostListCache = require( './feed-stream-cache' ),
-	SiteStoreActions = require( 'lib/reader-site-store/actions' ),
-	FeedStreamActions;
+/**
+ * Internal Dependencies
+ */
+import Dispatcher from 'dispatcher';
+import { action as ActionType } from './constants';
+import FeedPostStoreActions from 'lib/feed-post-store/actions';
+import feedPostListCache from './feed-stream-cache';
+import SiteStoreActions from 'lib/reader-site-store/actions';
 
 function getNextPageParams( store ) {
-	var params = {
+	const params = {
 			orderBy: store.orderBy,
 			number: store.perPage,
 			meta: 'post,discover_original_post'
@@ -32,29 +34,26 @@ function getNextPageParams( store ) {
 	return params;
 }
 
-FeedStreamActions = {
+export function fetchNextPage( id ) {
+	const store = feedPostListCache.get( id );
 
-	fetchNextPage: function( id ) {
-		var store = feedPostListCache.get( id ),
-			params;
+	if ( ! store || store.isLastPage() || store.isFetchingNextPage() || store.hasRecentError() ) {
+		return;
+	}
 
-		if ( ! store || store.isLastPage() || store.isFetchingNextPage() || store.hasRecentError() ) {
-			return;
-		}
+	Dispatcher.handleViewAction( {
+		type: ActionType.FETCH_NEXT_PAGE,
+		id: id
+	} );
 
-		Dispatcher.handleViewAction( {
-			type: ActionType.FETCH_NEXT_PAGE,
-			id: id
-		} );
+	const params = getNextPageParams( store );
 
-		params = getNextPageParams( store );
+	store.onNextPageFetch( params );
 
-		store.onNextPageFetch( params );
+	store.fetcher( params, receivePage.bind( null, store.id ) );
+}
 
-		store.fetcher( params, FeedStreamActions.receivePage.bind( FeedStreamActions, store.id ) );
-	},
-
-	receivePage: function( id, error, data ) {
+export function receivePage( id, error, data ) {
 		// Process metadata. Yes, this is weird.
 		// The idea here is that we're getting posts back as metadata, so we should process them the same way we would have
 		// if they came back via the standard post::fetch action. Rather than trying to make the post store deal with any
@@ -64,121 +63,117 @@ FeedStreamActions = {
 		// would rob them of that chance.
 		//
 		// TODO add a new action that receives an array of posts, so we can batch up this change and only emit once
-		if ( ! error && data && data.posts ) {
-			forEach( data.posts, function( post ) {
-				if ( post && get( post, 'meta.data.discover_original_post' ) ) {
+	if ( ! error && data && data.posts ) {
+		forEach( data.posts, function( post ) {
+			if ( post && get( post, 'meta.data.discover_original_post' ) ) {
 					// Looks like the original post for a Discover post (meta=discover_original_post)
-					FeedPostStoreActions.receivePost( null, post.meta.data.discover_original_post, {
-						blogId: post.meta.data.discover_original_post.site_ID,
-						postId: post.meta.data.discover_original_post.ID
-					} );
-				}
+				FeedPostStoreActions.receivePost( null, post.meta.data.discover_original_post, {
+					blogId: post.meta.data.discover_original_post.site_ID,
+					postId: post.meta.data.discover_original_post.ID
+				} );
+			}
 
-				if ( get( post, 'meta.data.site' ) ) {
-					SiteStoreActions.receiveFetch( post.site_ID, null, post.meta.data.site );
-				}
+			if ( get( post, 'meta.data.site' ) ) {
+				SiteStoreActions.receiveFetch( post.site_ID, null, post.meta.data.site );
+			}
 
-				if ( post && get( post, 'meta.data.post' ) ) {
-					FeedPostStoreActions.receivePost( null, post.meta.data.post, {
-						feedId: post.feed_ID,
-						postId: post.ID
-					} );
-				} else if ( post && post.feed_ID && post.feed_item_ID ) {
+			if ( post && get( post, 'meta.data.post' ) ) {
+				FeedPostStoreActions.receivePost( null, post.meta.data.post, {
+					feedId: post.feed_ID,
+					postId: post.ID
+				} );
+			} else if ( post && post.feed_ID && post.feed_item_ID ) {
 					// 1.2 style
-					FeedPostStoreActions.receivePost( null, post, {
-						feedId: post.feed_ID,
-						postId: post.feed_item_ID
-					} );
-				} else if ( post && post.site_ID ) {
+				FeedPostStoreActions.receivePost( null, post, {
+					feedId: post.feed_ID,
+					postId: post.feed_item_ID
+				} );
+			} else if ( post && post.site_ID ) {
 					// this looks like a full post object
-					FeedPostStoreActions.receivePost( null, post, {
-						blogId: post.site_ID,
-						postId: post.ID
-					} );
-				}
-			} );
-		}
-
-		Dispatcher.handleServerAction( {
-			type: ActionType.RECEIVE_PAGE,
-			id: id,
-			error: error,
-			data: data
-		} );
-	},
-
-	receiveUpdates: function( id, error, data ) {
-		Dispatcher.handleServerAction( {
-			type: ActionType.RECEIVE_UPDATES,
-			id: id,
-			error: error,
-			data: data
-		} );
-	},
-
-	showUpdates: function( id ) {
-		Dispatcher.handleViewAction( {
-			type: ActionType.SHOW_UPDATES,
-			id: id
-		} );
-	},
-
-	selectNextItem: function( id ) {
-		Dispatcher.handleViewAction( {
-			type: ActionType.SELECT_NEXT_ITEM,
-			id: id
-		} );
-	},
-
-	selectPrevItem: function( id ) {
-		Dispatcher.handleViewAction( {
-			type: ActionType.SELECT_PREV_ITEM,
-			id: id
-		} );
-	},
-
-	selectItem: function( id, selectedIndex ) {
-		Dispatcher.handleViewAction( {
-			type: ActionType.SELECT_ITEM,
-			id: id,
-			selectedIndex: selectedIndex
-		} );
-	},
-
-	fillGap: function( id, gap ) {
-		var store = feedPostListCache.get( id ),
-			params;
-		if ( ! store ) {
-			return;
-		}
-
-		Dispatcher.handleViewAction( {
-			type: ActionType.FILL_GAP,
-			id: id,
-			gap: gap
-		} );
-
-		params = {
-			before: gap.to.toISOString(),
-			after: gap.from.toISOString(),
-			number: store.gapFillCount,
-			orderBy: store.orderBy
-		};
-
-		store.onGapFetch( params );
-
-		store.fetcher( params, FeedStreamActions.receiveGap.bind( FeedStreamActions, store.id, gap ) );
-	},
-
-	receiveGap: function( id, gap, error, data ) {
-		Dispatcher.handleServerAction( {
-			type: ActionType.RECEIVE_GAP,
-			id: id,
-			gap: gap,
-			error: error,
-			data: data
+				FeedPostStoreActions.receivePost( null, post, {
+					blogId: post.site_ID,
+					postId: post.ID
+				} );
+			}
 		} );
 	}
-};
 
-module.exports = FeedStreamActions;
+	Dispatcher.handleServerAction( {
+		type: ActionType.RECEIVE_PAGE,
+		id: id,
+		error: error,
+		data: data
+	} );
+}
+
+export function receiveUpdates( id, error, data ) {
+	Dispatcher.handleServerAction( {
+		type: ActionType.RECEIVE_UPDATES,
+		id: id,
+		error: error,
+		data: data
+	} );
+}
+
+export function showUpdates( id ) {
+	Dispatcher.handleViewAction( {
+		type: ActionType.SHOW_UPDATES,
+		id: id
+	} );
+}
+
+export function selectNextItem( id ) {
+	Dispatcher.handleViewAction( {
+		type: ActionType.SELECT_NEXT_ITEM,
+		id: id
+	} );
+}
+
+export function selectPrevItem( id ) {
+	Dispatcher.handleViewAction( {
+		type: ActionType.SELECT_PREV_ITEM,
+		id: id
+	} );
+}
+
+export function selectItem( id, selectedIndex ) {
+	Dispatcher.handleViewAction( {
+		type: ActionType.SELECT_ITEM,
+		id: id,
+		selectedIndex: selectedIndex
+	} );
+}
+
+export function fillGap( id, gap ) {
+	const store = feedPostListCache.get( id );
+	if ( ! store ) {
+		return;
+	}
+
+	Dispatcher.handleViewAction( {
+		type: ActionType.FILL_GAP,
+		id: id,
+		gap: gap
+	} );
+
+	const params = {
+		before: gap.to.toISOString(),
+		after: gap.from.toISOString(),
+		number: store.gapFillCount,
+		orderBy: store.orderBy
+	};
+
+	store.onGapFetch( params );
+
+	store.fetcher( params, receiveGap.bind( null, store.id, gap ) );
+}
+
+export function receiveGap( id, gap, error, data ) {
+	Dispatcher.handleServerAction( {
+		type: ActionType.RECEIVE_GAP,
+		id: id,
+		gap: gap,
+		error: error,
+		data: data
+	} );
+}
