f2e2920558cb824d3588d9d76a90358dc1c47128
bluefuton <chris@bluefuton.com>
2016-12-08 11:03:14 +1300

    Linting

--- client/reader/stats.js
+++ client/reader/stats.js
@@ -1,9 +1,13 @@
-import assign from 'lodash/assign';
+/**
+ * External Dependencies
+ */
+import { assign, partial } from 'lodash';
 import debugFactory from 'debug';
-import partial from 'lodash/partial';
 
+/**
+ * Internal Dependencies
+ */
 import { mc, ga, tracks } from 'lib/analytics';
-
 import SubscriptionStore from 'lib/reader-feed-subscriptions';
 
 const debug = debugFactory( 'calypso:reader:stats' );
@@ -23,7 +27,7 @@ export function recordPermalinkClick( where, post ) {
 		reader_actions: 'visited_post_permalink',
 		reader_permalink_source: where
 	} );
-	recordGaEvent( 'Clicked Post Permalink', where )
+	recordGaEvent( 'Clicked Post Permalink', where );
 	const trackEvent = 'calypso_reader_permalink_click';
 	const args = {
 		source: where
