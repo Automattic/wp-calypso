Automattic/wp-calypso

commit d3bd203562068990fe5d3a2c7b8a36fa4ea2c3a5

tooltip: refact -> ES6ify

client/components/tooltip/index.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 
 /**
@@ -13,26 +13,9 @@ import viewport from 'lib/viewport';
 /**
  * Module variables
  */
- const noop = () => {};
+const noop = () => {};
 
-export default React.createClass( {
-
-	displayName: 'Tooltip',
-
-	getDefaultProps() {
-		return {
-			position: 'top',
-			showOnMobile: false
-		};
-	},
-
-	propTypes: {
-		isVisible: React.PropTypes.bool,
-		position: React.PropTypes.string,
-		status: React.PropTypes.string,
-		showOnMobile: React.PropTypes.bool
-	},
-
+class Tooltip extends Component {
 	render() {
 		if ( ! this.props.showOnMobile && viewport.isMobile() ) {
 			return null;
@@ -41,19 +24,42 @@ export default React.createClass( {
 		const classes = classnames(
 			'popover__container',
 			'tooltip',
-			`is-${ this.props.status }`
+			`is-${ this.props.position }`,
+			this.props.className
 		);
 
 		return (
 			<Popover
+				autoPosition= { this.props.autoPosition }
 				className={ classes }
-				isVisible={ this.props.isVisible }
 				context={ this.props.context }
+				id={ this.props.id }
+				isVisible={ this.props.isVisible }
 				onClose={ noop }
 				position={ this.props.position }
+				showDelay={ this.props.showDelay }
 			>
 				{ this.props.children }
 			</Popover>
 		);
 	}
-} );
+}
+
+Tooltip.propTypes = {
+	autoPosition: PropTypes.bool,
+	className: PropTypes.string,
+	id: PropTypes.string,
+	isVisible: PropTypes.bool,
+	position: PropTypes.string,
+	status: PropTypes.string,
+	showDelay: PropTypes.number,
+	showOnMobile: PropTypes.bool
+};
+
+Tooltip.defaultProps = {
+	showDelay: 100,
+	position: 'top',
+	showOnMobile: false
+};
+
+export default Tooltip;

client/components/tooltip/docs/example.jsx
@@ -8,15 +8,19 @@ import PureRenderMixin from 'react-pure-render/mixin';
 /**
  * Internal dependencies
  */
-import TooltipComponent from 'components/tooltip';
+import Tooltip from 'components/tooltip';
 
-const Tooltip = React.createClass( {
+const TooltipInstance = React.createClass( {
 	mixins: [ PureRenderMixin ],
+
+	componentWillMount() {
+		this.changePosition = this.changePosition.bind( this );
+	},
 
 	getInitialState: function() {
 		return {
-			position: 'bottom left',
-			show: true,
+			position: 'bottom right',
+			show: false,
 		};
 	},
 
@@ -28,45 +32,75 @@ const Tooltip = React.createClass( {
 		this.setState( { show: false } );
 	},
 
+	changePosition( event ) {
+		this.setState( { position: event.target.value } );
+	},
+
 	render() {
-		const size = 20;
+		const size = 30;
 
 		return (
-			<div>
-				tooltip context&nbsp;
-				<span
-					style={ {
-						width: size,
-						height: size,
-						lineHeight: `${ size }px`,
-						display: 'inline-block',
-						borderRadius: parseInt( size / 2 ),
-						backgroundColor: '#444',
-						color: 'white',
-						fontSize: '12px',
-						cursor: 'pointer',
-						textAlign: 'center',
-					} }
-					onMouseEnter={ this.open }
-					onMouseLeave={ this.close }
-					ref="tooltip-reference"
-				>
-					T
-				</span>
+			<div className="docs__design-assets-group">
+				<h2>
+					<a href="/devdocs/design/tooltip-instance">Tooltip</a>
+				</h2>
 
-				<TooltipComponent
-					isVisible={ this.state.show }
-					onClose={ this.close }
-					position={ this.state.position }
-					context={ this.refs && this.refs[ 'tooltip-reference' ] }
-				>
-					<div style={ { padding: '10px' } }>
-						Simple Tooltip Instance
-					</div>
-				</TooltipComponent>
+				<label>Position
+					<select
+						value={ this.state.position }
+						onChange={ this.changePosition }
+					>
+						<option value="top">top</option>
+						<option value="top left">top left</option>
+						<option value="top right">top right</option>
+						<option value="left">left</option>
+						<option value="right">right</option>
+						<option value="bottom">bottom</option>
+						<option value="bottom left">bottom left</option>
+						<option value="bottom right">bottom right</option>
+					</select>
+				</label>
+
+				<hr />
+
+				<div>
+					Tooltip context&nbsp;
+					<span
+						style={ {
+							width: size,
+							height: size,
+							lineHeight: `${ size }px`,
+							display: 'inline-block',
+							borderRadius: parseInt( size / 2 ),
+							backgroundColor: '#444',
+							color: 'white',
+							fontSize: '12px',
+							cursor: 'pointer',
+							textAlign: 'center',
+						} }
+						onMouseEnter={ this.open }
+						onMouseLeave={ this.close }
+						onClick={ this.close }
+						ref="tooltip-reference"
+					>
+						T
+					</span>
+
+					<Tooltip
+						id="tooltip__example"
+						isVisible={ this.state.show }
+						onClose={ this.close }
+						position={ this.state.position }
+						context={ this.refs && this.refs[ 'tooltip-reference' ] }
+					>
+						<div style={ { padding: '10px' } }>
+							Simple Tooltip Instance
+						</div>
+					</Tooltip>
+				</div>
 			</div>
 		);
 	}
 } );
 
-export default Tooltip;
+export default TooltipInstance;

client/components/tooltip/style.scss
@@ -3,17 +3,12 @@
 		border-width: 6px;
 	}
 
-	&.is-bottom .popover__arrow {
-		border-width: 10px;
-	}
-
 	&.is-bottom-right,
 	&.is-bottom-left,
 	&.is-bottom {
 		.popover__arrow {
 			border-bottom-color: darken( $gray, 30% );
-			position: absolute;
-			top: 9px;
+			top: 4px;
 			right: 10px;
 
 			&::before {
@@ -40,21 +35,13 @@
 		}
 	}
 
-	&.is-left,
-	&.is-right {
-		.popover__arrow {
-			margin-top: -6px;
-		}
-	}
-
 	&.is-top,
 	&.is-top-left,
 	&.is-top-right {
 		.popover__arrow {
 			border-top-color: darken( $gray, 30% );
-			position: absolute;
 			bottom: 4px;
-			margin-left: -6px;
+			right: 10px;
 
 			&::before {
 				display: none;
@@ -80,12 +67,18 @@
 		}
 	}
 
+	&.is-top,
+	&.is-bottom {
+		.popover__arrow {
+			margin-left: -6px;
+		}
+	}
+
+	&.is-left,
 	&.is-right {
 		padding-top: 0;
 		.popover__arrow {
-			border-right-color: darken( $gray, 30% );
-			position: absolute;
-			margin-left: 4px;
+			margin-top: -6px;
 
 			&::before {
 				display: none;
@@ -108,6 +101,20 @@
 			.popover__arrow {
 				border-right-color: $alert-green;
 			}
+		}
+	}
+
+	&.is-left {
+		.popover__arrow {
+			margin-right: 4px;
+			border-left-color: darken( $gray, 30% );
+		}
+	}
+
+	&.is-right {
+		.popover__arrow {
+			margin-left: 4px;
+			border-right-color: darken( $gray, 30% );
 		}
 	}
 

commit 6ea78bc77808d6b54bcfb50ebe4abfe2535aa874

Tooltip: Fixes an issue where tooltips with status were broken
Previously broken in d3bd203562068990fe5d3a2c7b8a36fa4ea2c3a5

client/components/tooltip/index.jsx
@@ -43,6 +43,7 @@ class Tooltip extends Component {
 			'popover',
 			'tooltip',
 			`is-${ this.props.position }`,
+			`is-${ this.props.status }`,
 			this.props.className
 		);
 