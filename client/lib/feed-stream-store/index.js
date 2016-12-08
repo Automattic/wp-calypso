5148c4777c47226df60a972d40fb7baa68fdc4cb
Jake F - @samouri <samouri@users.noreply.github.com>
2016-12-08 11:57:08 -0800

    Added in a FAQ for how seeds work

--- client/lib/feed-stream-store/index.js
+++ client/lib/feed-stream-store/index.js
@@ -202,6 +202,22 @@ function getStoreForRecommendedPosts( storeId ) {
 				break;
 			case 'custom_recs_posts_with_images':
 				query.algorithm = 'read:recommendations:posts/es/7';
+
+				/* Seed FAQ:
+				 * Q: What does it do?
+				 * A: It throws a little randomness into the elasticsearch recommendations query so that
+				 * users don't see the same thing every time they reload the page:
+				 * https://www.elastic.co/guide/en/elasticsearch/guide/current/random-scoring.html
+				 *
+				 * Q: How did we pick this range?
+				 * A: The range here is fairly...random.  Its big enough that the same user
+				 * probably won't get it too frequently and small enough that we aren't going
+				 * cause overflows anywhere.
+				 *
+				 * Q: How often do we change the seed?
+				 * A: We change the seed each time the store is generated. Practically speaking
+				 * that means each time the page is refreshed
+				 */
 				query.seed = random( 0, 10000 );
 				break;
 			default:
