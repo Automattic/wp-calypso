53772048186fdd6d7ff676b2b4cb84b92589bab7
Biser Perchinkov <biser@bisko.be>
2016-12-08 20:35:37 +0200

    Update the dotblog subdomain (#9902)

--- client/signup/steps/domains/index.jsx
+++ client/signup/steps/domains/index.jsx
@@ -20,8 +20,9 @@ var StepWrapper = require( 'signup/step-wrapper' ),
 	{ DOMAINS_WITH_PLANS_ONLY } = require( 'state/current-user/constants' ),
 	{ getSurveyVertical } = require( 'state/signup/steps/survey/selectors.js' ),
 	analyticsMixin = require( 'lib/mixins/analytics' ),
-	signupUtils = require( 'signup/utils' ),
-	abtest = require( 'lib/abtest' ).abtest;
+	signupUtils = require( 'signup/utils' );
+
+import { abtest, getABTestVariation } from 'lib/abtest';
 
 import Notice from 'components/notice';
 
@@ -165,6 +166,12 @@ const DomainsStep = React.createClass( {
 		const initialState = this.props.step ? this.props.step.domainForm : this.state.domainForm;
 		const isDeveloperFlow = Boolean( 'developer' === this.props.flowName );
 
+		const includeDotBlogSubdomain = ( this.props.flowName === 'subdomain' ) ||
+			(
+				getABTestVariation( 'noSurveyStep' ) === 'showSurveyStep' &&
+				abtest( 'domainDotBlogSubdomain' ) === 'includeDotBlogSubdomain'
+			);
+
 		return (
 			<RegisterDomainStep
 				path={ this.props.path }
@@ -179,8 +186,7 @@ const DomainsStep = React.createClass( {
 				analyticsSection="signup"
 				domainsWithPlansOnly={ this.props.domainsWithPlansOnly }
 				includeWordPressDotCom
-				includeDotBlogSubdomain={ ( this.props.flowName === 'subdomain' ) ||
-					( abtest( 'domainDotBlogSubdomain' ) === 'includeDotBlogSubdomain' ) }
+				includeDotBlogSubdomain={ includeDotBlogSubdomain }
 				isSignupStep
 				surveyVertical={ this.props.surveyVertical }
 				showExampleSuggestions={ ! isDeveloperFlow }
