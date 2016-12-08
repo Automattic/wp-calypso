a2fcf384b8dbe648ab291e058af8ccba3563200c
Dennis Snell <dennis.snell@automattic.com>
2016-12-08 16:47:37 -0700

    Refactor to optimize performance
    The middleware itself will be called on every dispatch and so should be
    as slim as possible. While `has()` provides some value in legibility it
    increases the overhead associated in determining if a property exists.
    
    Now we use direct access to check for an existant handler instead.

--- client/state/data-layer/wpcom-api-middleware.js
+++ client/state/data-layer/wpcom-api-middleware.js
@@ -1,15 +1,20 @@
 /**
- * External dependencies
- */
-import { has } from 'lodash';
-
-/**
  * Internal dependencies
  */
 import handlers from './wpcom';
 
+/**
+ * WPCOM Middleware API
+ *
+ * Intercepts actions requesting data provided by the
+ * WordPress.com API and passes them off to the
+ * appropriate handler.
+ */
 export const middleware = store => next => action =>
-	has( handlers, action.type )
+	// we won't use has( handlers, action.type )
+	// here becuase of performance implications
+	// this function is run on every dispatch
+	!! handlers[ action.type ]
 		? handlers[ action.type ].forEach( handler => handler( store, action ) )
 		: next( action );
 
