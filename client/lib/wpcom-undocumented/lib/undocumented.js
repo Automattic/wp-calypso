8590e06535f82181e9931fc4c63b2b89db41fbb5
Marin Atanasov <tyxla@abv.bg>
2016-12-09 15:13:42 +0200

    Undocumented: Update path to be part of the POST body in updateJetpackJumpstart()

--- client/lib/wpcom-undocumented/lib/undocumented.js
+++ client/lib/wpcom-undocumented/lib/undocumented.js
@@ -86,11 +86,11 @@ Undocumented.prototype.getJetpackJumpstart = function( siteId, fn ) {
  */
 Undocumented.prototype.updateJetpackJumpstart = function( siteId, active, fn ) {
 	//@TODO: implement and test this endpoint, it's currently not working
-	return this.wpcom.req.post( {
-		path: '/jetpack-blogs/' + siteId + '/rest-api/',
-		query: { path: '/jumpstart/' },
-		body: { active }
-	}, fn );
+	return this.wpcom.req.post(
+		{ path: '/jetpack-blogs/' + siteId + '/rest-api/' },
+		{ path: '/jumpstart/', body: JSON.stringify( { active } ) },
+		fn
+	);
 };
 
 /*
