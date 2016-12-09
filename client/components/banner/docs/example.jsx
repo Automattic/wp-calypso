d575f9d4dfec584ec79ccf361de703df8ebd08e0
Jacopo Tomasone <Copons@users.noreply.github.com>
2016-12-09 17:11:44 +0000

    Component: Banner (#9653)
    * Create new Banner component
    
    * Add plan colors
    
    * Add link indicator
    
    * Add CTA button
    
    * Add checklist
    
    * Add plan icons
    
    * Add plan and feature highlighting
    
    * Add prices
    
    * Add dismissible option

---
+++ client/components/banner/docs/example.jsx
@@ -0,0 +1,60 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import {
+	PLAN_PERSONAL,
+	PLAN_PREMIUM,
+	PLAN_BUSINESS,
+	FEATURE_ADVANCED_SEO,
+} from 'lib/plans/constants';
+import Banner from 'components/banner';
+
+const BannerExample = () =>
+	<div>
+		<Banner title="Banner unrelated to any plan"/>
+		<Banner
+			description="And with a description."
+			icon="star"
+			title="Banner unrelated to any plan"
+		/>
+		<Banner
+			href="#"
+			plan={ PLAN_PERSONAL }
+			title="Upgrade to a Personal Plan!"
+		/>
+		<Banner
+			href="#"
+			plan={ PLAN_PREMIUM }
+			title="Upgrade to a Premium Plan!"
+		/>
+		<Banner
+			href="#"
+			plan={ PLAN_BUSINESS }
+			title="Upgrade to a Business Plan!"
+		/>
+		<Banner
+			callToAction="Upgrade for $9.99"
+			feature={ FEATURE_ADVANCED_SEO }
+			href="#"
+			list={ [ 'Live chat support', 'No advertising' ] }
+			plan={ PLAN_BUSINESS }
+			price={ [ 10.99, 9.99 ] }
+			title="Upgrade to a Business Plan!"
+		/>
+		<Banner
+			callToAction="Upgrade for $9.99"
+			description="Live chat support and no advertising."
+			dismissPreferenceName="devdocs-banner-example"
+			dismissTemporary
+			list={ [ 'Live chat support', 'No advertising' ] }
+			plan={ PLAN_BUSINESS }
+			title="Upgrade to a Business Plan!"
+		/>
+	</div>;
+
+export default BannerExample;
