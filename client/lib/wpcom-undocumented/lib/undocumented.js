5f758a36c1003f5073aba973f0728b62df580149
Marin Atanasov <tyxla@abv.bg>
2016-12-09 14:16:57 +0200

    Undocumented: Update path to be part of the POST body in updateJetpackModuleSettings()

--- client/lib/wpcom-undocumented/lib/undocumented.js
+++ client/lib/wpcom-undocumented/lib/undocumented.js
@@ -126,11 +126,11 @@ Undocumented.prototype.fetchJetpackModuleSettings = function( siteId, moduleSlug
  */
 Undocumented.prototype.updateJetpackModuleSettings = function( siteId, moduleSlug, settings, fn ) {
 	//@TODO: implement and test this endpoint, it's currently not working
-	return this.wpcom.req.post( {
-		path: '/jetpack-blogs/' + siteId + '/rest-api/',
-		query: { path: '/module/' + moduleSlug },
-		body: settings
-	}, fn );
+	return this.wpcom.req.post(
+		{ path: '/jetpack-blogs/' + siteId + '/rest-api/' },
+		{ path: '/module/' + moduleSlug, body: settings },
+		fn
+	);
 };
 
 /**
