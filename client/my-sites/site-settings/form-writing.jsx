a91b54e17aeede0174cc810a8948776360f2de64
johnHackworth <javier.alvarez.lop@gmail.com>
2016-12-12 14:56:46 +0100

    Jetpack settings: Hide press-this header in touch screens

--- client/my-sites/site-settings/form-writing.jsx
+++ client/my-sites/site-settings/form-writing.jsx
@@ -223,7 +223,7 @@ const SiteSettingsFormWriting = React.createClass( {
 				) }
 
 				{ config.isEnabled( 'press-this' ) && (
-					<div>
+					<div className="press-this">
 						{
 							this.renderSectionHeader( this.translate( 'Press This', {
 								context: 'name of browser bookmarklet tool'
@@ -231,7 +231,7 @@ const SiteSettingsFormWriting = React.createClass( {
 						}
 						<Card className="site-settings">
 							<FormFieldset>
-								<div className="press-this">
+								<div>
 									<p>{ this.translate( 'Press This is a bookmarklet: a little app that runs in your browser and lets you grab bits of the web.' ) }</p>
 									<p>{ this.translate( 'Use Press This to clip text, images and videos from any web page. Then edit and add more straight from Press This before you save or publish it in a post on your site.' ) }</p>
 									<p>{ this.translate( 'Drag-and-drop the following link to your bookmarks bar or right click it and add it to your favorites for a posting shortcut.' ) }</p>
