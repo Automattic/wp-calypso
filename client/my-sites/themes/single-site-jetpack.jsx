1023371e1674f70da7931470f6e25af94923bf54
Bernhard Reiter <ockham@raz.or.at>
2016-12-12 14:36:23 +0100

    Themes: Add second ThemesSelection to Single Jetpack Site mode

--- client/my-sites/themes/single-site-jetpack.jsx
+++ client/my-sites/themes/single-site-jetpack.jsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React from 'react';
+import { pickBy } from 'lodash';
 
 /**
  * Internal dependencies
@@ -17,10 +18,20 @@ import { connectOptions } from './theme-options';
 import QuerySitePlans from 'components/data/query-site-plans';
 import QuerySitePurchases from 'components/data/query-site-purchases';
 import ThemeShowcase from './theme-showcase';
+import ThemesSelection from './themes-selection';
+import { addTracking } from './helpers';
 
 export default connectOptions(
 	( props ) => {
-		const { site, siteId, analyticsPath, analyticsPageTitle } = props;
+		const {
+			analyticsPath,
+			analyticsPageTitle,
+			getScreenshotOption,
+			options,
+			search,
+			site,
+			siteId
+		} = props;
 		const jetpackEnabled = config.isEnabled( 'manage/themes-jetpack' );
 
 		if ( ! jetpackEnabled ) {
@@ -54,8 +65,34 @@ export default connectOptions(
 					<ThanksModal
 						site={ site }
 						source={ 'list' } />
+					<ThemesSelection
+						selectedSite={ false }
+						getScreenshotUrl={ function( theme ) {
+							if ( ! getScreenshotOption( theme ).getUrl ) {
+								return null;
+							}
+							return getScreenshotOption( theme ).getUrl( theme );
+						} }
+						onScreenshotClick={ function( theme ) {
+							if ( ! getScreenshotOption( theme ).action ) {
+								return;
+							}
+							getScreenshotOption( theme ).action( theme );
+						} }
+						getActionLabel={ function( theme ) {
+							return getScreenshotOption( theme ).label;
+						} }
+						getOptions={ function( theme ) {
+							return pickBy(
+								addTracking( options ),
+								option => ! ( option.hideForTheme && option.hideForTheme( theme ) )
+							); } }
+						trackScrollPage={ props.trackScrollPage }
+						search={ search }
+						tier={ props.tier }
+						filter={ props.filter }
+						vertical={ props.vertical } />
 				</ThemeShowcase>
-			</div>
-		);
+			</div>		);
 	}
 );
