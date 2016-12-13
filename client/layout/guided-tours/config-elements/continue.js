ff3a0db0d3a61f57e2420d977c2fb21f528ea5a1
Marek Hrabe <marekhrabe@me.com>
2016-12-13 13:59:04 +0100

    Guided Tours: Split config-elements (#9987)

---
+++ client/layout/guided-tours/config-elements/continue.js
@@ -0,0 +1,98 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { translate } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Gridicon from 'components/gridicon';
+import {
+	targetForSlug,
+} from '../positioning';
+import contextTypes from '../context-types';
+
+export default class Continue extends Component {
+	static contextTypes = contextTypes;
+
+	static propTypes = {
+		children: PropTypes.node,
+		click: PropTypes.bool,
+		hidden: PropTypes.bool,
+		icon: PropTypes.string,
+		step: PropTypes.string.isRequired,
+		target: PropTypes.string,
+		when: PropTypes.func,
+	};
+
+	constructor( props, context ) {
+		super( props, context );
+	}
+
+	componentDidMount() {
+		this.addTargetListener();
+	}
+
+	componentWillUnmount() {
+		this.removeTargetListener();
+	}
+
+	componentWillReceiveProps( nextProps, nextContext ) {
+		nextProps.when && nextContext.isValid( nextProps.when ) && this.onContinue();
+	}
+
+	componentWillUpdate() {
+		this.removeTargetListener();
+	}
+
+	componentDidUpdate() {
+		this.addTargetListener();
+	}
+
+	onContinue = () => {
+		const { next, tour, tourVersion, step } = this.context;
+		const { step: nextStepName } = this.props;
+		next( { tour, tourVersion, step, nextStepName } );
+	}
+
+	addTargetListener() {
+		const { target = false, click, when } = this.props;
+		const targetNode = targetForSlug( target );
+
+		if ( click && ! when && targetNode && targetNode.addEventListener ) {
+			targetNode.addEventListener( 'click', this.onContinue );
+			targetNode.addEventListener( 'touchstart', this.onContinue );
+		}
+	}
+
+	removeTargetListener() {
+		const { target = false, click, when } = this.props;
+		const targetNode = targetForSlug( target );
+
+		if ( click && ! when && targetNode && targetNode.removeEventListener ) {
+			targetNode.removeEventListener( 'click', this.onContinue );
+			targetNode.removeEventListener( 'touchstart', this.onContinue );
+		}
+	}
+
+	defaultMessage() {
+		return this.props.icon
+			? translate( 'Click the {{icon/}} to continue.', {
+				components: { icon: <Gridicon icon={ this.props.icon } size={ 24 } /> }
+			} )
+			: translate( 'Click to continue.' );
+	}
+
+	render() {
+		if ( this.props.hidden ) {
+			return null;
+		}
+
+		return (
+			<p className="guided-tours__actionstep-instructions">
+				<em>{ this.props.children || this.defaultMessage() }</em>
+			</p>
+		);
+	}
+}
---
+++ client/layout/guided-tours/config-elements/index.js
@@ -0,0 +1,12 @@
+/**
+ * Internal dependencies
+ */
+export ButtonRow from './button-row';
+export combineTours from './combine-tours';
+export Continue from './continue';
+export Link from './link';
+export makeTour from './make-tour';
+export Next from './next';
+export Quit from './quit';
+export Step from './step';
+export Tour from './tour';
