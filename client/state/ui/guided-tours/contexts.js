9d792d834ce9f1ccfb38d6d2ad8431cc4e1c3a39
Marek Hrabe <marekhrabe@me.com>
2016-12-13 11:05:14 +0100

    improve jsdocs

--- client/state/ui/guided-tours/contexts.js
+++ client/state/ui/guided-tours/contexts.js
@@ -15,7 +15,7 @@ import { hasDefaultSiteTitle } from 'state/sites/selectors';
 const WEEK_IN_MILLISECONDS = 7 * 1000 * 3600 * 24;
 
 /*
- * Returns selector that tests if user is in given section
+ * Returns a selector that tests if the current user is in a given section
  *
  * @param {String} sectionName Name of section
  * @return {Function} Selector function
@@ -24,7 +24,7 @@ export const inSection = sectionName => state =>
 	getSectionName( state ) === sectionName;
 
 /*
- * Returns selector that tests if feature is enabled in config
+ * Returns a selector that tests if a feature is enabled in config
  *
  * @param {String} feature Name of feature
  * @return {Function} Selector function
@@ -33,7 +33,7 @@ export const isEnabled = feature => () =>
 	config.isEnabled( feature );
 
 /*
- * Returns milliseconds since user registration
+ * Returns milliseconds since registration date of the current user
  *
  * @param {Object} state Global state tree
  * @return {Number|Boolean} Milliseconds since registration, false if cannot be determined
@@ -45,7 +45,7 @@ const timeSinceUserRegistration = state => {
 };
 
 /*
- * Returns true if user is considered "new" (less than week since registration)
+ * Returns true if the user is considered "new" (less than a week since registration)
  *
  * @param {Object} state Global state tree
  * @return {Boolean} True if user is new, false otherwise
@@ -56,7 +56,7 @@ export const isNewUser = state => {
 };
 
 /*
- * Returns selector that tests if user is older than given time since registration
+ * Returns a selector that tests if the user is older than a given time
  *
  * @param {Number} age Number of milliseconds
  * @return {Function} Selector function
@@ -67,7 +67,7 @@ export const isUserOlderThan = age => state => {
 };
 
 /*
- * Returns selector that tests if user has registered before given date
+ * Returns a selector that tests if the user has registered before given date
  *
  * @param {Date} date Date of registration
  * @return {Function} Selector function
@@ -80,7 +80,7 @@ export const hasUserRegisteredBefore = date => state => {
 };
 
 /*
- * Returns selector that tests whether user interacted with given component
+ * Returns a selector that tests whether the user has interacted with a given component
  *
  * @param {String} componentName Name of component to test
  * @return {Function} Selector function
@@ -89,7 +89,7 @@ export const hasUserInteractedWithComponent = componentName => state =>
 	getLastAction( state ).component === componentName;
 
 /*
- * Returns true if selected site can be previewed
+ * Returns true if the selected site can be previewed
  *
  * @param {Object} state Global state tree
  * @return {Boolean} True if selected site can be previewed, false otherwise.
@@ -98,7 +98,7 @@ export const isSelectedSitePreviewable = state =>
 	getSelectedSite( state ) && getSelectedSite( state ).is_previewable;
 
 /*
- * Returns true if current user can run customizer for selected site
+ * Returns true if the current user can run customizer for the selected site
  *
  * @param {Object} state Global state tree
  * @return {Boolean} True if user can run customizer, false otherwise.
@@ -107,17 +107,17 @@ export const isSelectedSiteCustomizable = state =>
 	getSelectedSite( state ) && getSelectedSite( state ).is_customizable;
 
 /*
- * Returns true if a/b test is in given variant
+ * Returns a selector that tests whether an a/b test is in a given variant
  *
  * @param {String} testName Name of A/B test
  * @param {String} variant Variant identifier
- * @return {Boolean} True if test is in given variant
+ * @return {Function} Selector function
  */
 export const isAbTestInVariant = ( testName, variant ) => () =>
 	abtest( testName ) === variant;
 
 /*
- * Returns true if the selected site has unchanged site title
+ * Returns true if the selected site has an unchanged site title
  *
  * @param {Object} state Global state tree
  * @return {Boolean} True if site title is default, false otherwise.
@@ -128,7 +128,7 @@ export const hasSelectedSiteDefaultSiteTitle = state => {
 };
 
 /*
- * Returns true if current user can edit settings of selected site
+ * Returns true if the current user can edit settings of the selected site
  *
  * @param {Object} state Global state tree
  * @return {Boolean} True if user can edit settings, false otherwise.
