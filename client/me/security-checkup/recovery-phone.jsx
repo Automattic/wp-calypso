d278ca5b21c69d4b57110c5a154976bab0f54eb8
James Tien <southp0105@gmail.com>
2016-12-14 18:39:54 +0800

    Remove deprecated code

--- client/me/security-checkup/recovery-phone.jsx
+++ client/me/security-checkup/recovery-phone.jsx
@@ -8,35 +8,11 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
-// import AccountRecoveryStore from 'lib/security-checkup/account-recovery-store';
-// import SecurityCheckupActions from 'lib/security-checkup/actions';
 import ManageContact from './manage-contact';
 import EditPhone from './edit-phone';
 import accept from 'lib/accept';
 
 class RecoveryPhone extends Component {
-	// constructor( props ) {
-	// 	super( props );
-    //
-	// 	this.state = this.getDataFromStores();
-	// }
-    //
-	// componentDidMount() {
-	// 	AccountRecoveryStore.on( 'change', this.refreshData );
-	// }
-    //
-	// componentWillUnmount() {
-	// 	AccountRecoveryStore.off( 'change', this.refreshData );
-	// }
-    //
-	// refreshData = () => {
-	// 	this.setState( this.getDataFromStores() );
-	// }
-    //
-	// getDataFromStores() {
-	// 	return AccountRecoveryStore.getPhone();
-	// }
-
 	getTwoStepNotice( twoStepEnabled ) {
 		if ( twoStepEnabled ) {
 			return {
@@ -54,7 +30,6 @@ class RecoveryPhone extends Component {
 	}
 
 	render() {
-		// const phone = ! isEmpty( this.state.data ) ? this.state.data : false;
 		const twoStepEnabled = this.props.userSettings.isTwoStepEnabled();
 		const twoStepNotice = this.getTwoStepNotice( twoStepEnabled );
 
@@ -64,6 +39,8 @@ class RecoveryPhone extends Component {
 			translate,
 		} = this.props;
 
+		// TODO:
+		// lastNotice?
 		return (
 			<ManageContact
 				type="sms"
@@ -73,7 +50,7 @@ class RecoveryPhone extends Component {
 				} ) }
 				subtitle={ phone ? phone.numberFull : translate( 'Not set' ) }
 				hasValue={ !! phone }
-				lastNotice={ twoStepNotice || this.state.lastNotice }
+				lastNotice={ twoStepNotice || null }
 				disabled={ twoStepEnabled }
 
 				onSave={ this.onSave }
@@ -89,7 +66,6 @@ class RecoveryPhone extends Component {
 
 	onSave = ( phone ) => {
 		this.props.updatePhone( phone );
-		// SecurityCheckupActions.updatePhone( phone, this.state.data );
 	}
 
 	onDelete = () => {
@@ -106,7 +82,7 @@ class RecoveryPhone extends Component {
 	}
 
 	onDismissNotice = () => {
-		// SecurityCheckupActions.dismissPhoneNotice();
+		//TODO
 	}
 }
 
