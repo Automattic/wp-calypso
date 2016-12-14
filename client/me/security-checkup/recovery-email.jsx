f31430f27eec9f22acfc823b8a632fb5cce6a355
James Tien <southp0105@gmail.com>
2016-12-14 12:05:19 +0800

    Remove the deprecated code from `<RecoveryEmail />`

--- client/me/security-checkup/recovery-email.jsx
+++ client/me/security-checkup/recovery-email.jsx
@@ -7,8 +7,6 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
-import AccountRecoveryStore from 'lib/security-checkup/account-recovery-store';
-import SecurityCheckupActions from 'lib/security-checkup/actions';
 import ManageContact from './manage-contact';
 import EditEmail from './edit-email';
 import accept from 'lib/accept';
@@ -20,25 +18,7 @@ class RecoveryEmail extends Component {
 		this.state = this.getDataFromStores();
 	}
 
-	componentDidMount() {
-		AccountRecoveryStore.on( 'change', this.refreshData );
-	}
-
-	componentWillUnmount() {
-		AccountRecoveryStore.off( 'change', this.refreshData );
-	}
-
-	refreshData = () => {
-		this.setState( this.getDataFromStores() );
-	}
-
-	getDataFromStores() {
-		return AccountRecoveryStore.getEmail();
-	}
-
 	render() {
-		// const email = this.state.data ? this.state.data.email : false;
-		// const primaryEmail = this.props.userSettings.getSetting( 'user_email' );
 		const {
 			primaryEmail,
 			accountRecoveryEmail,
@@ -67,10 +47,7 @@ class RecoveryEmail extends Component {
 	}
 
 	onSave = ( newEmail ) => {
-		// TODO:
-		// check if email !== primaryEmail
 		this.props.updateAccountRecoveryEmail( newEmail );
-		// SecurityCheckupActions.updateEmail( email, this.state.data.email );
 	}
 
 	onDelete = () => {
@@ -79,10 +56,6 @@ class RecoveryEmail extends Component {
 				this.props.deleteAccountRecoveryEmail();
 			}
 		} );
-	}
-
-	onDismissNotice = () => {
-		SecurityCheckupActions.dismissEmailNotice();
 	}
 }
 
