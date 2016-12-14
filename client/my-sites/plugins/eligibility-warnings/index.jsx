3e61b9184e938d62a06e121518adb23d99297546
Marko Andrijasevic <markoandrijasevic@gmail.com>
2016-12-14 21:54:49 +0100

    Mock warnings and errors data.

--- client/my-sites/plugins/eligibility-warnings/index.jsx
+++ client/my-sites/plugins/eligibility-warnings/index.jsx
@@ -14,50 +14,121 @@ import SectionHeader from 'components/section-header';
 
 class EligibilityWarnings extends Component {
 	static propTypes = {
+		isEligible: PropTypes.bool.isRequired,
+		errors: PropTypes.arrayOf(
+			PropTypes.shape( {
+				title: PropTypes.string,
+				description: PropTypes.string,
+				actionUrl: PropTypes.string,
+			} )
+		),
+		pluginWarnings: PropTypes.arrayOf(
+			PropTypes.shape( {
+				title: PropTypes.string,
+				description: PropTypes.string,
+				actionUrl: PropTypes.string,
+			} )
+		),
+		widgetWarnings: PropTypes.arrayOf(
+			PropTypes.shape( {
+				title: PropTypes.string,
+				description: PropTypes.string,
+				actionUrl: PropTypes.string,
+			} )
+		),
+		backUrl: PropTypes.string,
 		translate: PropTypes.func,
 	};
 
 	render() {
-		const { translate } = this.props;
+		const {
+			translate,
+			backUrl,
+			isEligible,
+			errors,
+			pluginWarnings,
+			widgetWarnings,
+		} = this.props;
 
 		return (
 			<div className="eligibility-warnings">
 				<SectionHeader label={ translate( 'Conflicts' ) } />
-				<Card className="eligibility-warnings__message">
-					<Gridicon icon="notice" className="eligibility-warnings__error-icon"/>
-					<div className="eligibility-warnings__message-content">
-						{ translate( 'Error message' ) }
-					</div>
-					<div className="eligibility-warnings__message-action">
-						<Button> { translate( 'Resolve' ) } </Button>
-					</div>
-				</Card>
 
-				<Card className="eligibility-warnings__message">
-					<Gridicon icon="notice" className="eligibility-warnings__warning-icon"/>
-					<div className="eligibility-warnings__message-content">
-						{ translate( 'Warning message' ) }
-					</div>
-					<div className="eligibility-warnings__message-action">
-						<Gridicon icon="help-outline" className="eligibility-warnings__warning-action"/>
-					</div>
-				</Card>
+				{ errors.map( ( message ) =>
+					<Card key={ message.title } className="eligibility-warnings__message">
+						<Gridicon icon="notice" className="eligibility-warnings__error-icon" />
+						<div className="eligibility-warnings__message-content">
+							<div className="eligibility-warnings__message-title">
+								{ translate( 'Error' ) + `: ${ message.title }` }
+							</div>
+							<div className="eligibility-warnings__message-description">
+								{ message.description }
+							</div>
+						</div>
+						<div className="eligibility-warnings__message-action">
+							<Button href={ message.actionUrl } target="_blank" rel="noopener noreferrer">
+								{ translate( 'Resolve' ) }
+							</Button>
+						</div>
+					</Card>
+				) }
+
+				{ pluginWarnings.map( ( message ) =>
+					<Card key={ message.title } className="eligibility-warnings__message">
+						<Gridicon icon="notice" className="eligibility-warnings__warning-icon" />
+						<div className="eligibility-warnings__message-content">
+							<div className="eligibility-warnings__message-title">
+								{ translate( 'Unsupported feature' ) + `: ${ message.title }` }
+							</div>
+							<div className="eligibility-warnings__message-description">
+								{ message.description }
+							</div>
+						</div>
+						<div className="eligibility-warnings__message-action">
+							<a href={ message.actionUrl } target="_blank" rel="noopener noreferrer">
+								<Gridicon icon="help-outline" className="eligibility-warnings__warning-action" />
+							</a>
+						</div>
+					</Card>
+				) }
+
+				{ widgetWarnings.map( ( message ) =>
+					<Card key={ message.title } className="eligibility-warnings__message">
+						<Gridicon icon="notice" className="eligibility-warnings__warning-icon" />
+						<div className="eligibility-warnings__message-content">
+							<div className="eligibility-warnings__message-title">
+								{ translate( 'Incompatible widget' ) + `: ${ message.title }` }
+							</div>
+							<div className="eligibility-warnings__message-description">
+								{ message.description }
+							</div>
+						</div>
+						<div className="eligibility-warnings__message-action">
+							<a href={ message.actionUrl } target="_blank" rel="noopener noreferrer">
+								<Gridicon icon="help-outline" className="eligibility-warnings__warning-action" />
+							</a>
+						</div>
+					</Card>
+				) }
 
 				<Card className="eligibility-warnings__confirm-box">
-					<Gridicon icon="info-outline" className="eligibility-warnings__confirm-icon"/>
+					<Gridicon icon="info-outline" className="eligibility-warnings__confirm-icon" />
 					<div className="eligibility-warnings__confirm-text">
-						{ translate(
-							'You must resolve the errors above before proceeding. ' +
-							'Have questions? Please contact support.'
+						{ ! isEligible && translate(
+							'You must resolve the errors above before proceeding. '
 						) }
+						{ isEligible && translate(
+							'By proceeding you understand that you will loose these features. '
+						) }
+						{ translate( 'Have questions? Please contact support.' ) }
 					</div>
 
 					<div className="eligibility-warnings__buttons">
-						<Button>
+						<Button href={ backUrl }>
 							{ translate( 'Cancel' ) }
 						</Button>
 
-						<Button primary={ true } disabled={ true }>
+						<Button primary={ true } disabled={ ! isEligible }>
 							{ translate( 'Proceed' ) }
 						</Button>
 					</div>
--- client/my-sites/plugins/plugin-eligibility/index.jsx
+++ client/my-sites/plugins/plugin-eligibility/index.jsx
@@ -13,21 +13,54 @@ import HeaderCake from 'components/header-cake';
 import EligibilityWarnings from '../eligibility-warnings';
 
 class PluginEligibility extends Component {
-
 	static propTypes = {
 		pluginSlug: PropTypes.string,
 		siteSlug: PropTypes.string,
 		translate: PropTypes.func,
 	};
 
-	goBack = () => {
+	getBackUrl = () => {
 		const { pluginSlug, siteSlug } = this.props;
 
-		page( `/plugins/${ pluginSlug }/${ siteSlug }` );
+		return `/plugins/${ pluginSlug }/${ siteSlug }`;
 	};
+
+	goBack = () => page( this.getBackUrl() );
 
 	render() {
 		const { translate } = this.props;
+
+		// Mock some errors and warnings until we get real data from the API
+		const isEligible = false;
+
+		const errors = [
+			{
+				title: translate( 'Business plan required' ),
+				description: translate( 'Only sites with a business plan can install plugins.' ),
+				actionUrl: 'https://en.support.wordpress.com/'
+			},
+			{
+				title: translate( 'Missing custom domain' ),
+				description: translate( 'Only sites with custom domain can install plugins.' ),
+				actionUrl: 'https://en.support.wordpress.com/'
+			}
+		];
+
+		const pluginWarnings = [
+			{
+				title: translate( 'WordPress.com Smileys' ),
+				description: translate( 'WordPress.com Smileys will no longer work after this plugin is installed.' ),
+				actionUrl: 'https://en.support.wordpress.com/'
+			}
+		];
+
+		const widgetWarnings = [
+			{
+				title: translate( 'Authors' ),
+				description: translate( 'Authors widget will no longer work after this plugin is installed.' ),
+				actionUrl: 'https://en.support.wordpress.com/'
+			}
+		];
 
 		return (
 			<MainComponent>
@@ -37,7 +70,13 @@ class PluginEligibility extends Component {
 				>
 					{ translate( 'Install plugin' ) }
 				</HeaderCake>
-				<EligibilityWarnings />
+				<EligibilityWarnings
+					isEligible={ isEligible }
+					backUrl={ this.getBackUrl() }
+					errors={ errors }
+					pluginWarnings={ pluginWarnings }
+					widgetWarnings={ widgetWarnings }
+				/>
 			</MainComponent>
 		);
 	}
