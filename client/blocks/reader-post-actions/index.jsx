15b6141cf79b20436db97878956bf713813cabb0
bluefuton <chris@bluefuton.com>
2016-12-14 18:10:20 +1300

    Don't show post options menu follow button for Discover posts

--- client/blocks/reader-post-actions/index.jsx
+++ client/blocks/reader-post-actions/index.jsx
@@ -20,7 +20,7 @@ import * as stats from 'reader/stats';
 import { localize } from 'i18n-calypso';
 import ExternalLink from 'components/external-link';
 
-const ReaderPostActions = ( { translate, post, site, onCommentClick, showEdit, showVisit, showMenu, iconSize, className } ) => {
+const ReaderPostActions = ( { translate, post, site, onCommentClick, showEdit, showVisit, showMenu, showMenuFollow, iconSize, className } ) => {
 	const onEditClick = () => {
 		stats.recordAction( 'edit_post' );
 		stats.recordGaEvent( 'Clicked Edit Post', 'full_post' );
@@ -83,7 +83,7 @@ const ReaderPostActions = ( { translate, post, site, onCommentClick, showEdit, s
 			}
 			{ showMenu &&
 				<li className="reader-post-actions__item">
-					<ReaderPostOptionsMenu className="ignore-click" post={ post } />
+					<ReaderPostOptionsMenu className="ignore-click" showFollow={ showMenuFollow } post={ post } />
 				</li>
 			}
 		</ul>
@@ -97,14 +97,16 @@ ReaderPostActions.propTypes = {
 	onCommentClick: React.PropTypes.func,
 	showEdit: React.PropTypes.bool,
 	iconSize: React.PropTypes.number,
-	showMenu: React.PropTypes.bool
+	showMenu: React.PropTypes.bool,
+	showMenuFollow: React.PropTypes.bool
 };
 
 ReaderPostActions.defaultProps = {
 	showEdit: true,
 	showVisit: false,
 	showMenu: false,
-	iconSize: 24
+	iconSize: 24,
+	showMenuFollow: true
 };
 
 export default localize( ReaderPostActions );
