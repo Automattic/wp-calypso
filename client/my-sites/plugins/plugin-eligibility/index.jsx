33eccdf28e844907036b60d9884a89ff9d0ed273
Marko Andrijasevic <markoandrijasevic@gmail.com>
2016-12-16 02:36:13 +0100

    Remove test data, for real

--- client/my-sites/plugins/plugin-eligibility/index.jsx
+++ client/my-sites/plugins/plugin-eligibility/index.jsx
@@ -30,38 +30,7 @@ class PluginEligibility extends Component {
 
 	render() {
 		const { translate } = this.props;
-
-		// Mock some errors and warnings until we get real data from the API
 		const isEligible = false;
-
-		const errors = [
-			{
-				title: translate( 'Business plan required' ),
-				description: translate( 'Only sites with a business plan can install plugins.' ),
-				actionUrl: 'https://en.support.wordpress.com/'
-			},
-			{
-				title: translate( 'Missing custom domain' ),
-				description: translate( 'Only sites with custom domain can install plugins.' ),
-				actionUrl: 'https://en.support.wordpress.com/'
-			}
-		];
-
-		const pluginWarnings = [
-			{
-				title: translate( 'WordPress.com Smileys' ),
-				description: translate( 'WordPress.com Smileys will no longer work after this plugin is installed.' ),
-				actionUrl: 'https://en.support.wordpress.com/'
-			}
-		];
-
-		const widgetWarnings = [
-			{
-				title: translate( 'Authors' ),
-				description: translate( 'Authors widget will no longer work after this plugin is installed.' ),
-				actionUrl: 'https://en.support.wordpress.com/'
-			}
-		];
 
 		return (
 			<MainComponent>
@@ -74,9 +43,6 @@ class PluginEligibility extends Component {
 				<EligibilityWarnings
 					isEligible={ isEligible }
 					backUrl={ this.getBackUrl() }
-					errors={ errors }
-					pluginWarnings={ pluginWarnings }
-					widgetWarnings={ widgetWarnings }
 				/>
 			</MainComponent>
 		);
