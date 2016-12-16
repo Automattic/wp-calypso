aaca35e9fe3d7053684e38c3f1bbdeafb62952ab
Riad Benguella <benguella@gmail.com>
2016-12-16 11:20:33 +0100

    Changes per review

--- client/lib/track-form/index.jsx
+++ client/lib/track-form/index.jsx
@@ -4,57 +4,53 @@
 import React, { Component } from 'react';
 import { union } from 'lodash';
 
-export const trackForm = WrappedComponent => {
-	class TrackFormComponent extends Component {
-		state = {
-			dirtyFields: [],
-			fields: {}
-		};
+export const trackForm = WrappedComponent => class TrackFormComponent extends Component {
+	state = {
+		dirtyFields: [],
+		fields: {}
+	};
 
-		updateFields = ( fields, callback ) => {
-			const newState = {
-				dirtyFields: union( this.state.dirtyFields, Object.keys( fields ) ),
-				fields: {
-					...this.state.fields,
-					...fields
-				}
-			};
-
-			this.setState( newState, callback );
-		};
-
-		replaceFields = ( fields, callback ) => {
-			const newFields = {
+	updateFields = ( fields, callback ) => {
+		const newState = {
+			dirtyFields: union( this.state.dirtyFields, Object.keys( fields ) ),
+			fields: {
 				...this.state.fields,
 				...fields
-			};
-
-			this.setState( { fields: newFields }, callback );
+			}
 		};
 
-		clearDirtyFields = () => {
-			this.setState( {
-				dirtyFields: []
-			} );
+		this.setState( newState, callback );
+	};
+
+	replaceFields = ( fields, callback ) => {
+		const newFields = {
+			...this.state.fields,
+			...fields
 		};
 
-		render() {
-			const { fields, dirtyFields } = this.state;
+		this.setState( { fields: newFields }, callback );
+	};
 
-			return (
-				<WrappedComponent
-					fields={ fields }
-					dirtyFields={ dirtyFields }
-					updateFields={ this.updateFields }
-					replaceFields={ this.replaceFields }
-					clearDirtyFields={ this.clearDirtyFields }
-					{ ...this.props }
-				/>
-			);
-		}
+	clearDirtyFields = () => {
+		this.setState( {
+			dirtyFields: []
+		} );
+	};
+
+	render() {
+		const { fields, dirtyFields } = this.state;
+
+		return (
+			<WrappedComponent
+				fields={ fields }
+				dirtyFields={ dirtyFields }
+				updateFields={ this.updateFields }
+				replaceFields={ this.replaceFields }
+				clearDirtyFields={ this.clearDirtyFields }
+				{ ...this.props }
+			/>
+		);
 	}
-
-	return TrackFormComponent;
 };
 
 export default trackForm;
