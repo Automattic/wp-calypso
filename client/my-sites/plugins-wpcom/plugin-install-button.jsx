5e898c923d1fa01cc955bdfa68461e21ec8a540f
Marko Andrijasevic <marko.andrijasevic@automattic.com>
2016-12-16 03:03:12 +0100

    Plugins: Display eligibility warnings for dotcom sites (#10052)
    Add eligibility warnings component and use it to display site eligibility
    errors and warnings upon initiating Install from plugin detail page.
    
    Props @dmsnell

--- client/my-sites/plugins-wpcom/plugin-install-button.jsx
+++ client/my-sites/plugins-wpcom/plugin-install-button.jsx
@@ -9,12 +9,13 @@ import { localize } from 'i18n-calypso';
  */
 import Button from 'components/button';
 
-export const WpcomPluginInstallButton = ( { translate, disabled } ) => {
+export const WpcomPluginInstallButton = ( { translate, disabled, plugin, site } ) => {
 	return <Button
 		onClick={ undefined }
 		primary={ true }
 		type="submit"
 		disabled={ disabled }
+		href={ `/plugins/${ plugin.slug }/${ site.slug }/eligibility` }
 	>
 		{ translate( 'Install' ) }
 	</Button>;
---
+++ client/my-sites/plugins/plugin-eligibility/index.jsx
@@ -0,0 +1,55 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { localize } from 'i18n-calypso';
+import page from 'page';
+
+/**
+ * Internal dependencies
+ */
+import MainComponent from 'components/main';
+import HeaderCake from 'components/header-cake';
+import EligibilityWarnings from 'blocks/eligibility-warnings';
+
+class PluginEligibility extends Component {
+	static propTypes = {
+		pluginSlug: PropTypes.string,
+		siteSlug: PropTypes.string,
+		translate: PropTypes.func,
+		navigateTo: PropTypes.func
+	};
+
+	getBackUrl = () => {
+		const { pluginSlug, siteSlug } = this.props;
+
+		return `/plugins/${ pluginSlug }/${ siteSlug }`;
+	};
+
+	goBack = () => this.props.navigateTo( this.getBackUrl() );
+
+	render() {
+		const { translate } = this.props;
+		const isEligible = false;
+
+		return (
+			<MainComponent>
+				<HeaderCake
+					isCompact={ true }
+					onClick={ this.goBack }
+				>
+					{ translate( 'Install plugin' ) }
+				</HeaderCake>
+				<EligibilityWarnings
+					isEligible={ isEligible }
+					backUrl={ this.getBackUrl() }
+				/>
+			</MainComponent>
+		);
+	}
+}
+
+// It was 2:45AM, I wanted to deploy, and @dmsnell made me do it... props to @dmsnell :)
+const withNavigation = WrappedComponent => props => <WrappedComponent { ...{ ...props, navigateTo: page } } />;
+
+export default withNavigation( localize( PluginEligibility ) );
