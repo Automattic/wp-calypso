ba8e973b8aceedf09a5d8eee809f774684ce2e05
Javi A <javieralvarezlop@gmail.com>
2016-12-16 23:47:37 +0100

    JPC: add instructions link (#9416)
    * JPC: add instructions link
    
    * JPC: Add hover shadow to instructions
    
    * Move hover state to browser only instead of entire card
    
    * Remove side margins from browser content portion

--- client/signup/jetpack-connect/example-components/jetpack-install.jsx
+++ client/signup/jetpack-connect/example-components/jetpack-install.jsx
@@ -10,9 +10,9 @@ import FormTextInput from 'components/forms/form-text-input';
 import Gridicon from 'components/gridicon';
 import { localize } from 'i18n-calypso';
 
-const JetpackConnectExampleInstall = ( { url, translate } ) => {
+const JetpackConnectExampleInstall = ( { url, translate, onClick } ) => {
 	return (
-		<div className="example-components__main">
+		<div className="example-components__main" onClick={ onClick }>
 			<div className="example-components__browser-chrome example-components__site-url-input-container">
 				<div className="example-components__browser-chrome-dots">
 					<div className="example-components__browser-chrome-dot"></div>
@@ -44,6 +44,14 @@ const JetpackConnectExampleInstall = ( { url, translate } ) => {
 			</div>
 		</div>
 	);
+};
+
+JetpackConnectExampleInstall.propTypes = {
+	onClick: React.PropTypes.func
+};
+
+JetpackConnectExampleInstall.defaultProps = {
+	onClick: () => {}
 };
 
 export default localize( JetpackConnectExampleInstall );
