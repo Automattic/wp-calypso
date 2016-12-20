30b15ce3cb17510f95ea545ce7d5d40d86635c11
Ben Dwyer <ben@scruffian.com>
2016-12-20 17:34:59 +0000

    Send the flow name to the new site endpoint when creating a new site

--- client/signup/steps/domains/index.jsx
+++ client/signup/steps/domains/index.jsx
@@ -111,6 +111,7 @@ const DomainsStep = React.createClass( {
 		registerDomainAnalytics.recordEvent( 'submitDomainStepSelection', suggestion, 'signup' );
 
 		SignupActions.submitSignupStep( Object.assign( {
+			flowName: this.props.flowName,
 			processingMessage: this.translate( 'Adding your domain' ),
 			stepName: this.props.stepName,
 			domainItem,
