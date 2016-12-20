a612ba369762e17694c22aea3283ae8ce229e3ab
Chris Rosser <chris@bluefuton.com>
2016-12-21 10:31:18 +1300

    Remove reader/list-management (#10170)

--- client/reader/list-management/README.md
+++
@@ -1,3 +0,0 @@
-# Reader List Management
-
-Management tools for Reader Lists. Currently in-progress.
--- client/reader/list/index.js
+++ client/reader/list/index.js
@@ -6,17 +6,9 @@ import page from 'page';
 /**
  * Internal dependencies
  */
-import config from 'config';
 import controller from './controller';
 import readerController from 'reader/controller';
 
 export default function() {
 	page( '/read/list/:user/:list', readerController.updateLastRoute, readerController.sidebar, controller.listListing );
-
-	if ( config.isEnabled( 'reader/list-management' ) ) {
-		page( '/read/list/:user/:list/sites', readerController.updateLastRoute, readerController.sidebar, controller.listManagementSites );
-		page( '/read/list/:user/:list/tags', readerController.updateLastRoute, readerController.sidebar, controller.listManagementTags );
-		page( '/read/list/:user/:list/edit', readerController.updateLastRoute, readerController.sidebar, controller.listManagementDescriptionEdit );
-		page( '/read/list/:user/:list/followers', readerController.updateLastRoute, readerController.sidebar, controller.listManagementFollowers );
-	}
 }
