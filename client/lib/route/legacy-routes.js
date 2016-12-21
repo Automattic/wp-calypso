c3b2ba6f9093c09195f74442d82dac2baee1dac5
Lance Willett <lance@automattic.com>
2016-12-21 11:37:50 -0700

    Remove references to old my-stats WP.com page (retired)

--- client/lib/route/legacy-routes.js
+++ client/lib/route/legacy-routes.js
@@ -13,7 +13,6 @@ const notEnabled = feature => () => ! config.isEnabled( feature );
 const legacyRoutes = [
 	{ match: /.php$/ },
 	{ match: /^\/?$/, predicate: notEnabled( 'reader' ) },
-	{ match: /^\/my-stats/ },
 	{ match: /^\/notifications/ },
 	{ match: /^\/themes/ },
 	{ match: /^\/manage/ },
