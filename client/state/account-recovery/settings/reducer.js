5b1de8d17250cd46bba83fdde2f85e6843d8457d
James Tien <southp0105@gmail.com>
2016-12-22 15:15:33 +0800

    Separate `data` reducer into `phone`, `email`, `phoneValidated` and `emailValidated` and then combine them back as the `data` reducer.

--- client/state/account-recovery/settings/reducer.js
+++ client/state/account-recovery/settings/reducer.js
@@ -70,68 +70,59 @@ const convertPhoneResponse = ( phoneResponse ) => {
 	};
 };
 
-const convertEmailResponse = ( emailResponse ) => {
-	return emailResponse ? emailResponse : '';
-};
+const convertEmailResponse = ( emailResponse ) => emailResponse || '';
 
-const data = createReducer( null, {
-	[ ACCOUNT_RECOVERY_SETTINGS_FETCH_SUCCESS ]: ( state, { settings } ) => {
-		const {
-			email,
-			email_validated,
-			phone,
-			phone_validated,
-		} = settings;
+const phone = createReducer( null, {
+	[ ACCOUNT_RECOVERY_SETTINGS_FETCH_SUCCESS ]: ( state, { settings } ) =>
+		convertPhoneResponse( settings.phone ),
 
-		// At the moment, the response from /me/account-recovery could have an object or false for the phone field,
-		// and an string or false for the email field. I personally don't like the mixed value so did the conversion
-		// in the following.
-		return {
-			...state,
-			email: convertEmailResponse( email ),
-			emailValidated: email_validated,
-			phone: convertPhoneResponse( phone ),
-			phoneValidated: phone_validated,
-		};
-	},
+	[ ACCOUNT_RECOVERY_SETTINGS_UPDATE_SUCCESS ]: ( state, { target, value } ) =>
+		'phone' === target ? convertPhoneResponse( value ) : state,
 
-	[ ACCOUNT_RECOVERY_SETTINGS_UPDATE_SUCCESS ]: ( state, { target, value } ) => {
-		switch ( target ) {
-			case 'phone':
-				return {
-					...state,
-					phone: convertPhoneResponse( value ),
-				};
-			case 'email':
-				return {
-					...state,
-					email: convertEmailResponse( value ),
-				};
-			default: // do nothing to unknown targets
-				return { ...state };
-		}
-	},
+	[ ACCOUNT_RECOVERY_SETTINGS_DELETE_SUCCESS ]: ( state, { target } ) =>
+		'phone' === target ? null : state,
+} );
 
-	[ ACCOUNT_RECOVERY_SETTINGS_DELETE_SUCCESS ]: ( state, { target } ) => {
-		switch ( target ) {
-			case 'phone':
-				return {
-					...state,
-					phone: null,
-				};
-			case 'email':
-				return {
-					...state,
-					email: '',
-				};
-			default: // do nothing to unknown targets
-				return { ...state };
-		}
-	},
+const email = createReducer( '', {
+	[ ACCOUNT_RECOVERY_SETTINGS_FETCH_SUCCESS ]: ( state, { settings } ) =>
+		convertEmailResponse( settings.email ),
+
+	[ ACCOUNT_RECOVERY_SETTINGS_UPDATE_SUCCESS ]: ( state, { target, value } ) =>
+		'email' === target ? convertEmailResponse( value ) : state,
+
+	[ ACCOUNT_RECOVERY_SETTINGS_DELETE_SUCCESS ]: ( state, { target } ) =>
+		'email' === target ? '' : state,
+} );
+
+const phoneValidated = createReducer( false, {
+	[ ACCOUNT_RECOVERY_SETTINGS_FETCH_SUCCESS ]: ( state, { settings } ) =>
+		settings.phone_validated,
+
+	[ ACCOUNT_RECOVERY_SETTINGS_UPDATE_SUCCESS ]: ( state, { target } ) =>
+		'phone' === target ? false : state,
+
+	[ ACCOUNT_RECOVERY_SETTINGS_DELETE_SUCCESS ]: ( state, { target } ) =>
+		'phone' === target ? false : state,
+} );
+
+const emailValidated = createReducer( false, {
+	[ ACCOUNT_RECOVERY_SETTINGS_FETCH_SUCCESS ]: ( state, { settings } ) =>
+		settings.email_validated,
+
+	[ ACCOUNT_RECOVERY_SETTINGS_UPDATE_SUCCESS ]: ( state, { target } ) =>
+		'email' === target ? false : state,
+
+	[ ACCOUNT_RECOVERY_SETTINGS_DELETE_SUCCESS ]: ( state, { target } ) =>
+		'email' === target ? false : state,
 } );
 
 export default combineReducers( {
-	data,
+	data: combineReducers( {
+		phone,
+		phoneValidated,
+		email,
+		emailValidated,
+	} ),
 	isUpdating,
 	isDeleting,
 } );
