e22c5d23a19e9fa7e447f032cf101404e557ad62
Andrija Vucinic <andrija.vucinic@gmail.com>
2016-12-28 14:41:32 +0100

    Domains: Update Domain Mapping Purchase Nameserver message
    Change the Thank You page for domain mapping to take into account the case when the NS are already setup.
    
    PR: #10208

--- client/my-sites/upgrades/checkout-thank-you/domain-mapping-details.jsx
+++ client/my-sites/upgrades/checkout-thank-you/domain-mapping-details.jsx
@@ -2,21 +2,23 @@
  * External dependencies
  */
 import React from 'react';
-import i18n from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
+import { localize } from 'i18n-calypso';
 import PurchaseDetail from 'components/purchase-detail';
 import supportUrls from 'lib/url/support';
 
-const DomainMappingDetails = ( { domain, registrarSupportUrl } ) => {
-	const registrarSupportLink = registrarSupportUrl ? <a target="_blank" rel="noopener noreferrer" href={ registrarSupportUrl } /> : <span />;
+const DomainMappingDetails = ( { domain, registrarSupportUrl, translate } ) => {
+	const registrarSupportLink = registrarSupportUrl
+		? <a target="_blank" rel="noopener noreferrer" href={ registrarSupportUrl } />
+		: <span />;
 	const description = (
 		<div>
 			<p>
 				{
-					i18n.translate( 'The domain {{em}}%(domain)s{{/em}} still has to be configured to work with WordPress.com.', {
+					translate( 'Your domain {{em}}%(domain)s{{/em}} has to be configured to work with WordPress.com.', {
 						args: { domain },
 						components: { em: <em /> }
 					} )
@@ -24,9 +26,16 @@ const DomainMappingDetails = ( { domain, registrarSupportUrl } ) => {
 			</p>
 			<p>
 				{
-					i18n.translate(
-						'You will need to log into {{registrarSupportLink}}your registrar\'s site{{/registrarSupportLink}} ' +
-						'and change the "Name Servers" to:',
+					translate(
+						'If you already did this yourself, or if the domain was already configured for you, no further action is needed.'
+					)
+				}
+			</p>
+			<p>
+				{
+					translate(
+						'If not, you will need to log into {{registrarSupportLink}}your registrar\'s site{{/registrarSupportLink}} ' +
+						'(where you purchased the domain originally) and change the "Name Servers" to:',
 						{
 							components: {
 								registrarSupportLink: registrarSupportLink
@@ -40,7 +49,11 @@ const DomainMappingDetails = ( { domain, registrarSupportUrl } ) => {
 				<li>ns2.wordpress.com</li>
 				<li>ns3.wordpress.com</li>
 			</ul>
-			<p>{ i18n.translate( 'Once you make the change, just wait a few hours and the domain should start loading your site automatically.' ) }</p>
+			<p>{
+				translate(
+					'Once you make the change, just wait a few hours and the domain should start loading your site automatically.'
+				)
+			}</p>
 		</div>
 	);
 
@@ -48,16 +61,14 @@ const DomainMappingDetails = ( { domain, registrarSupportUrl } ) => {
 		<div className="checkout-thank-you__domain-mapping-details">
 			<PurchaseDetail
 				icon="cog"
-				title={ i18n.translate( 'Finish setting up your domain' ) }
 				description={ description }
-				buttonText={ i18n.translate( 'Learn more' ) }
+				buttonText={ translate( 'Learn more' ) }
 				href={ supportUrls.MAP_EXISTING_DOMAIN }
 				target="_blank"
 				rel="noopener noreferrer"
-				requiredText={ i18n.translate( 'Almost done! One step remaining…' ) }
 				isRequired />
 		</div>
 	);
 };
 
-export default DomainMappingDetails;
+export default localize( DomainMappingDetails );
