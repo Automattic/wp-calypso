9c49b0a44f455237c364bb9cb79e89f36be1ea39
Timmy Crawford <timmyc@users.noreply.github.com>
2016-12-28 14:36:01 -0800

    TinyMCE: Update empty paragraph regex to match core. (#10286)
    * TinyMCE: Update empty paragraph regex to match core.
    
    * Create util for removing empty spaces from empty paragraphs.
    
    * Remove padding from test blocks.
    
    * Change see citation.

---
+++ client/components/tinymce/plugins/wpcom/wpcom-utils.js
@@ -0,0 +1,18 @@
+/**
+ * Removes empty spaces from empty paragraphs
+ * This logic is borrowed from core's TinyMCE Plugin
+ *
+ * @see https://core.trac.wordpress.org/changeset/39204
+ *
+ * @param  {String}   content TinyMCE Editor content
+ * @return {String}           Content with strings removed from empty paragraphs
+ */
+export function removeEmptySpacesInParagraphs( content ) {
+	return content.replace( /<p>([^<>]+)<\/p>/gi, function( tag, text ) {
+		if ( text === '&nbsp;' || ! /\S/.test( text ) ) {
+			return '<p><br /></p>';
+		}
+
+		return tag;
+	} );
+}
