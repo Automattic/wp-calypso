1e2b5578946993d45e6b5ce73f6c44dc55b96dc2
Ben Dwyer <ben@scruffian.com>
2017-01-03 14:24:50 +0000

    only hide the sidebar for domain only sites

--- client/my-sites/sidebar/sidebar.jsx
+++ client/my-sites/sidebar/sidebar.jsx
@@ -706,57 +706,19 @@ export class MySitesSidebar extends Component {
 		);
 	}
 
-	render() {
-		var publish = !! this.publish(),
+	renderSidebarMenus() {
+		const publish = !! this.publish(),
 			appearance = ( !! this.themes() || !! this.menus() ),
 			configuration = ( !! this.sharing() || !! this.users() || !! this.siteSettings() || !! this.plugins() || !! this.upgrades() ),
-			vip = !! this.vip();
+			vip = !! this.vip(),
+			selectedSite = this.getSelectedSite();
 
-		if ( 1 === 1 ) {
-			return (
-				<Sidebar>
-					<SidebarRegion>
-					<CurrentSite
-						sites={ this.props.sites }
-						siteCount={ this.props.currentUser.visible_site_count }
-						onClick={ this.onPreviewSite }
-					/>
-					<SidebarMenu>
-						<ul>
-							<SidebarItem
-								label={ this.props.translate( 'Set up' ) }
-								className={ this.itemLinkClass( '/settings', 'settings' ) }
-								link={ '/domains-prototype/manage' +  this.siteSuffix() }
-								onNavigate={ this.onNavigate }
-								icon="customize"
-								preloadSectionName="settings"
-								tipTarget="settings" />
-							<SidebarItem
-								label={ this.props.translate( 'Settings' ) }
-								className={ this.itemLinkClass( '/settings', 'settings' ) }
-								link={ '/domains/manage' +  this.siteSuffix() }
-								onNavigate={ this.onNavigate }
-								icon="cog"
-								preloadSectionName="settings"
-								tipTarget="settings" />
-						</ul>
-					</SidebarMenu>
-					</SidebarRegion>
-					<SidebarFooter>
-						{ this.addNewSite() }
-					</SidebarFooter>
-				</Sidebar>
-			);
+		if ( selectedSite && selectedSite.options.is_domain_only ) {
+			return null;
 		}
 
 		return (
-			<Sidebar>
-				<SidebarRegion>
-				<CurrentSite
-					sites={ this.props.sites }
-					siteCount={ this.props.currentUser.visible_site_count }
-					onClick={ this.onPreviewSite }
-				/>
+			<div>
 				<SidebarMenu>
 					<ul>
 						{ this.stats() }
@@ -813,6 +775,20 @@ export class MySitesSidebar extends Component {
 					</SidebarMenu>
 					: null
 				}
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<Sidebar>
+				<SidebarRegion>
+				<CurrentSite
+					sites={ this.props.sites }
+					siteCount={ this.props.currentUser.visible_site_count }
+					onClick={ this.onPreviewSite }
+				/>
+				{ this.renderSidebarMenus() }
 				</SidebarRegion>
 				<SidebarFooter>
 					{ this.addNewSite() }
