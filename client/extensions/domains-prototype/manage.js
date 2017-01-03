87bdf1d8168ca0400478c762d1c3d5b6bb7f7761
Ben Dwyer <ben@scruffian.com>
2017-01-03 15:00:05 +0000

    updating the manage page to support more options

--- client/extensions/domains-prototype/manage.js
+++ client/extensions/domains-prototype/manage.js
@@ -1,15 +1,14 @@
 /**
  * External dependencies
  */
-import page from 'page';
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 /**
  * Internal dependencies
  */
+import Card from 'components/card';
 import Main from 'components/main';
-import PlanCompareCard from 'my-sites/plan-compare-card';
 import styles from './styles';
 
 class Manage extends React.Component {
@@ -19,35 +18,36 @@ class Manage extends React.Component {
 		return (
 			<Main wideLayout>
 				<h2 className={ styles.header }>What do you want to use { domain } for?</h2>
-				<div className={ styles.manageContainer }>
-					<PlanCompareCard
-						title="Landing Page"
-						line="Customize a simple, one-page placeholder."
-						buttonName="Create a landing page"
-						onClick={ () => page( '/domains-prototype/manage/landing-page/' + domain ) }
-						currentPlan={ false } />
+				<Card href={ '/domains-prototype/manage/landing-page/' + domain }>
+					<h2 className={ styles.cardHeader }>Landing Page</h2>
+					<p>Customize a simple, one-page placeholder.</p>
+				</Card>
 
-					<PlanCompareCard
-						title="New Site"
-						line="Build a new website or blog."
-						buttonName="Create a new site"
-						onClick={ () => page( '/domains-prototype/manage/start/' + domain ) }
-						currentPlan={ false } />
+				<Card href={ '/domains-prototype/manage/start/' + domain }>
+					<h2 className={ styles.cardHeader }>New Site</h2>
+					<p>Build a new website or blog.</p>
+				</Card>
 
-					<PlanCompareCard
-						title="Existing Site"
-						line="Connect an existing website or redirect to your social media."
-						buttonName="Connect a site"
-						onClick={ () => page( '/domains-prototype/manage/connect/' + domain ) }
-						currentPlan={ false } />
+				<Card href={ '/domains-prototype/manage/connect/' + domain }>
+					<h2 className={ styles.cardHeader }>Existing Site</h2>
+					<p>Connect an existing website or redirect to your social media.</p>
+				</Card>
 
-					<PlanCompareCard
-						title="Add Email"
-						line="Add professional email to your domain."
-						buttonName="Set up email"
-						onClick={ () => page( '/domains/manage/email/' + domain ) }
-						currentPlan={ false } />
-				</div>
+				<Card href={ '/domains/manage/email/' + domain }>
+					<h2 className={ styles.cardHeader }>Add Email</h2>
+					<p>Add professional email to your domain.</p>
+				</Card>
+
+				<Card href={ '/domains/manage/' + domain + '/contacts-privacy/' + domain }>
+					<h2 className={ styles.cardHeader }>Contact information</h2>
+					<p>Keep your contact information up to date.</p>
+				</Card>
+
+				<Card href={ '/domains/manage/' + domain + '/edit/' + domain }>
+					<h2 className={ styles.cardHeader }>Advanced</h2>
+					<p>Make advanced changes to your domain; change the name servers or DNS.</p>
+				</Card>
+
 			</Main>
 		);
 	}
