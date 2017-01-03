79f7d48ff5e30d20d7d1c8f200835d52f7e4d14b
Ben Dwyer <ben@scruffian.com>
2017-01-03 15:52:03 +0000

    layout tidy up

--- client/extensions/domains-prototype/manage.js
+++ client/extensions/domains-prototype/manage.js
@@ -20,32 +20,32 @@ class Manage extends React.Component {
 				<h2 className={ styles.header }>What do you want to use { domain } for?</h2>
 				<Card href={ '/domains-prototype/manage/landing-page/' + domain }>
 					<h2 className={ styles.cardHeader }>Landing Page</h2>
-					<p>Customize a simple, one-page placeholder.</p>
+					<div>Customize a simple, one-page placeholder.</div>
 				</Card>
 
 				<Card href={ '/domains-prototype/manage/start/' + domain }>
 					<h2 className={ styles.cardHeader }>New Site</h2>
-					<p>Build a new website or blog.</p>
+					<div>Build a new website or blog.</div>
 				</Card>
 
 				<Card href={ '/domains-prototype/manage/connect/' + domain }>
 					<h2 className={ styles.cardHeader }>Existing Site</h2>
-					<p>Connect an existing website or redirect to your social media.</p>
+					<div>Connect an existing website or redirect to your social media.</div>
 				</Card>
 
 				<Card href={ '/domains/manage/email/' + domain }>
 					<h2 className={ styles.cardHeader }>Add Email</h2>
-					<p>Add professional email to your domain.</p>
+					<div>Add professional email to your domain.</div>
 				</Card>
 
 				<Card href={ '/domains/manage/' + domain + '/contacts-privacy/' + domain }>
 					<h2 className={ styles.cardHeader }>Contact information</h2>
-					<p>Keep your contact information up to date.</p>
+					<div>Keep your contact information up to date.</div>
 				</Card>
 
 				<Card href={ '/domains/manage/' + domain + '/edit/' + domain }>
 					<h2 className={ styles.cardHeader }>Advanced</h2>
-					<p>Make advanced changes to your domain; change the name servers or DNS.</p>
+					<div>Make advanced changes to your domain; change the name servers or DNS.</div>
 				</Card>
 
 			</Main>
