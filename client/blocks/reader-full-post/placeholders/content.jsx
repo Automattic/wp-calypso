9b32fb8f9a7c8e0e34d1cb778516733348786434
bluefuton <chris@bluefuton.com>
2017-01-04 13:20:43 +1100

    Use min-height instead of lipsum

--- client/blocks/reader-full-post/placeholders/content.jsx
+++ client/blocks/reader-full-post/placeholders/content.jsx
@@ -6,19 +6,8 @@ import React from 'react';
 const ReaderFullPostContentPlaceholder = () => {
 	return (
 		<div className="reader-full-post__story-content is-placeholder">
-			<p className="reader-full-post__story-content-placeholder-text">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-			nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-			reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-
-			<p className="reader-full-post__story-content-placeholder-text">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-			nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-			reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+			<p className="reader-full-post__story-content-placeholder-text"></p>
+			<p className="reader-full-post__story-content-placeholder-text"></p>
 		</div>
 	);
 };
