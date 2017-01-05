8215a32a96b500a5cdfab4b71a95d7245a0d0eee
Chris Rosser <chris@bluefuton.com>
2017-01-06 09:55:32 +1100

    Reader: add basic placeholder for full post view (#10394)
    * Add basic placeholder for full post view
    
    * Use min-height instead of lipsum
    
    * Added placeholder for author compact profile
    
    * Add avatar placeholder
    
    * Use Gravatar component for avatar placeholder
    
    * Standardise Gravatar placeholder animation
    
    * Apply has-gravatar class when we have a placeholder
    
    * Style hoovering
    
    * Fixed long content fade on site title placeholder
    
    * Fix author compact profile placeholder padding and width
    
    * Remove placeholder override

---
+++ client/blocks/author-compact-profile/placeholder.jsx
@@ -0,0 +1,29 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import ReaderAvatar from 'blocks/reader-avatar';
+
+const AuthorCompactProfilePlaceholder = () => {
+	return (
+		<div className="author-compact-profile is-placeholder">
+			<div className="author-compact-profile__avatar-link">
+				<ReaderAvatar showPlaceholder={ true } />
+			</div>
+			<div className="author-compact-profile__site-link is-placeholder">
+				Site name
+			</div>
+			<div className="author-compact-profile__follow">
+				<div className="author-compact-profile__follow-count is-placeholder">
+					Number of followers
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default AuthorCompactProfilePlaceholder;
---
+++ client/blocks/reader-full-post/placeholders/content.jsx
@@ -0,0 +1,15 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+const ReaderFullPostContentPlaceholder = () => {
+	return (
+		<div className="reader-full-post__story-content is-placeholder">
+			<p className="reader-full-post__story-content-placeholder-text"></p>
+			<p className="reader-full-post__story-content-placeholder-text"></p>
+		</div>
+	);
+};
+
+export default ReaderFullPostContentPlaceholder;
