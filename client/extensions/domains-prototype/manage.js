df5d494a27be269285876bc83c3368d550bb587d
Brie Anne Demkiw <brie@automattic.com>
2017-01-08 20:45:29 -0800

    Remove card include.

--- client/extensions/domains-prototype/manage.js
+++ client/extensions/domains-prototype/manage.js
@@ -7,8 +7,7 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 /**
  * Internal dependencies
  */
-import Card from 'components/card';
-import Button from 'components/button';
+import EmptyContent from 'components/empty-content';
 import styles from './styles';
 
 class Manage extends React.Component {
@@ -16,15 +15,18 @@ class Manage extends React.Component {
 	unSetUpState() {
 		const { domain } = this.props;
 		return (
-			<Card className={ styles.manage }>
-				<h2 className={ styles.header }>{ domain } is not set up yet</h2>
-				<Button className={ styles.setUpButton } href={ '/start/site-selected/?siteSlug=' + domain } primary>Set up site</Button>
-				<p>Plans start at $123 per year.<br />Get your first year free.</p>
+			<div className={ styles.manage }>
+				<EmptyContent
+					title= { domain + ' is not set up yet.' }
+					line={ 'Plans start at $123 per year. Get your first year free.' }
+					action={ 'Set Up Site' }
+					actionURL={ '/start/site-selected/?siteSlug=' + domain }
+					illustration={ '/calypso/images/drake/drake-browser.svg' } />
 
 				<div className={ styles.advanced }>
-					<a href={ '/domains/manage/' + domain + '/edit/' + domain }>Advanced</a>
+					<a href={ '/domains/manage/' + domain + '/edit/' + domain }>Advanced...</a>
 				</div>
-			</Card>
+			</div>
 		);
 	}
 
