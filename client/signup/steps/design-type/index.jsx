c99099b09fb5e3d52495ce6571b76d3b35387964
Biser Perchinkov <biser@bisko.be>
2017-01-10 17:40:31 +0200

    Signup: Design Type - ES6-ify the step

--- client/signup/steps/design-type/index.jsx
+++ client/signup/steps/design-type/index.jsx
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -11,27 +12,29 @@ import SignupActions from 'lib/signup/actions';
 import analytics from 'lib/analytics';
 import Card from 'components/card';
 
-export default React.createClass( {
-	displayName: 'DesignType',
+import BlogImage from '../design-type-with-store/blog-image';
+import PageImage from '../design-type-with-store/page-image';
+import GridImage from '../design-type-with-store/grid-image';
 
+class DesignTypeStep extends Component {
 	getChoices() {
 		return [
-			{ type: 'blog', label: this.translate( 'A list of my latest posts' ), image: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 230"><rect x="15" y="15" fill="#E8F0F5" width="280" height="70"/><rect x="15" y="98" fill="#C3EF96" width="194" height="85"/><rect x="15" y="195" fill="#C3EF96" width="194" height="35"/></svg> },
-			{ type: 'page', label: this.translate( 'A welcome page for my site' ), image: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 230"><rect fill="#E8F0F5" width="310" height="110"/><rect x="114" y="205" fill="#E8F0F5" width="82" height="25"/><rect x="15" y="205" fill="#E8F0F5" width="82" height="25"/><rect x="213" y="205" fill="#E8F0F5" width="82" height="25"/><rect x="15" y="36" fill="#D2DEE6" width="153" height="13"/><rect x="15" y="59" fill="#D2DEE6" width="113" height="13"/><rect x="15" y="82" fill="#C3EF96" width="30" height="13"/><rect x="15" y="125" fill="#C3EF96" width="280" height="65"/></svg> },
-			{ type: 'grid', label: this.translate( 'A grid of my latest posts' ), image: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 230"><rect x="15" y="15" fill="#E8F0F5" width="280" height="40"/><rect x="114" y="70" fill="#C3EF96" width="82" height="65"/><rect x="15" y="70" fill="#C3EF96" width="82" height="65"/><rect x="213" y="70" fill="#C3EF96" width="82" height="65"/><rect x="114" y="150" fill="#C3EF96" width="82" height="65"/><rect x="15" y="150" fill="#C3EF96" width="82" height="65"/><rect x="213" y="150" fill="#C3EF96" width="82" height="65"/></svg> },
+			{ type: 'blog', label: this.props.translate( 'A list of my latest posts' ), image: <BlogImage /> },
+			{ type: 'page', label: this.props.translate( 'A welcome page for my site' ), image: <PageImage /> },
+			{ type: 'grid', label: this.props.translate( 'A grid of my latest posts' ), image: <GridImage /> },
 		];
-	},
+	}
 
-	renderChoice( choice ) {
+	renderChoice = ( choice ) => {
 		return (
 			<Card className="design-type__choice" key={ choice.type }>
-				<a className="design-type__choice__link" href="#" onClick={ ( event ) => this.handleChoiceClick( event, choice.type ) }>
+				<a className="design-type__choice-link" href="#" onClick={ ( event ) => this.handleChoiceClick( event, choice.type ) }>
 					{ choice.image }
 					<h2>{ choice.label }</h2>
 				</a>
 			</Card>
 		);
-	},
+	};
 
 	renderChoices() {
 		return (
@@ -40,29 +43,30 @@ export default React.createClass( {
 				<div className="design-type__choice is-spacergif" />
 			</div>
 		);
-	},
+	}
 
 	render() {
+		const { translate } = this.props;
 		return (
-			<div className="design-type__section-wrapper">
+			<div className="design-type">
 				<StepWrapper
 					flowName={ this.props.flowName }
 					stepName={ this.props.stepName }
 					positionInFlow={ this.props.positionInFlow }
-					fallbackHeaderText={ this.translate( 'What would you like your homepage to look like?' ) }
-					fallbackSubHeaderText={ this.translate( 'This will help us figure out what kinds of designs to show you.' ) }
-					subHeaderText={ this.translate( 'First up, what would you like your homepage to look like?' ) }
+					fallbackHeaderText={ translate( 'What would you like your homepage to look like?' ) }
+					fallbackSubHeaderText={ translate( 'This will help us figure out what kinds of designs to show you.' ) }
+					subHeaderText={ translate( 'First up, what would you like your homepage to look like?' ) }
 					signupProgress={ this.props.signupProgress }
 					stepContent={ this.renderChoices() } />
 			</div>
 		);
-	},
+	}
 
 	handleChoiceClick( event, type ) {
 		event.preventDefault();
 		event.stopPropagation();
 		this.handleNextStep( type );
-	},
+	}
 
 	handleNextStep( designType ) {
 		analytics.tracks.recordEvent( 'calypso_triforce_select_design', { category: designType } );
@@ -70,4 +74,6 @@ export default React.createClass( {
 		SignupActions.submitSignupStep( { stepName: this.props.stepName }, [], { designType } );
 		this.props.goToNextStep();
 	}
-} );
+}
+
+export default localize( DesignTypeStep );
