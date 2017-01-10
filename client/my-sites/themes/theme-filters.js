3a13ea6992ac204560132c0190adc19de048067c
Caroline Moore <calobee@gmail.com>
2017-01-10 11:54:16 -0500

    Update/theme taxonomies (#10530)

--- client/my-sites/themes/theme-filters.js
+++ client/my-sites/themes/theme-filters.js
@@ -71,6 +71,7 @@ const taxonomies = {
         "microformats",
         "multiple-menus",
         "one-page",
+        "portfolio",
         "post-formats",
         "post-slider",
         "rtl-language-support",
@@ -86,6 +87,7 @@ const taxonomies = {
     "layout": [
         "fixed-layout",
         "fluid-layout",
+        "grid-layout",
         "responsive-layout"
     ],
     "picks": [
