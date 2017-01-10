9c76af643eb77a1fcfb85918b0db1e1b02c836ce
Jordan West <jordwest@users.noreply.github.com>
2017-01-10 11:45:52 +1000

    Hide "English support only" notice for forum posts (#10491)
    This hides the "Note: Support is only available in English at the moment" notice on /help/contact if the user is only eligible for forum support.
    
    Because forum posts are directed to the relevant locale forum (for popular languages), we shouldn't be saying that support is only available in English.

--- client/me/help/help-contact/index.jsx
+++ client/me/help/help-contact/index.jsx
@@ -501,9 +501,18 @@ const HelpContact = React.createClass( {
 		}
 	},
 
-	getContactFormCommonProps: function() {
+	getContactFormCommonProps: function( variationSlug ) {
 		const { olark, isSubmitting } = this.state;
-		const showHelpLanguagePrompt = ( olark.locale !== i18n.getLocaleSlug() );
+
+		// Let the user know we only offer support in English.
+		// We only need to show the message if:
+		// 1. The user's locale doesn't match the live chat locale (usually English)
+		// 2. The support request isn't sent to the forums. Because forum support
+		//    requests are sent to the language specific forums (for popular languages)
+		//    we don't tell the user that support is only offered in English.
+		const showHelpLanguagePrompt =
+			( olark.locale !== i18n.getLocaleSlug() ) &&
+			SUPPORT_FORUM !== variationSlug;
 
 		return {
 			disabled: isSubmitting,
@@ -568,7 +577,7 @@ const HelpContact = React.createClass( {
 		const supportVariation = this.getSupportVariation();
 
 		const contactFormProps = Object.assign(
-			this.getContactFormCommonProps(),
+			this.getContactFormCommonProps( supportVariation ),
 			this.getContactFormPropsVariation( supportVariation ),
 		);
 
