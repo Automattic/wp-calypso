def9dc795992ea6f8d33915383eaff2960bf78ba
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2017-01-11 18:27:44 -0300

    Plans: Implement InfoPopover component (#8733)
    Removes the custom popover from the Plans page in favor of the InfoPopover component.

--- client/my-sites/plan-features/item.jsx
+++ client/my-sites/plan-features/item.jsx
@@ -7,40 +7,24 @@ import React from 'react';
  * Internal dependencies
  */
 import Gridicon from 'components/gridicon';
+import InfoPopover from 'components/info-popover';
+import viewport from 'lib/viewport';
 
 export default function PlanFeaturesItem( {
 	children,
-	description,
-	onMouseEnter,
-	onMouseLeave,
-	onTouchStart,
+	description
 } ) {
-	const handleOnTouchStart = ( event ) => {
-		onTouchStart( event.currentTarget, description );
-	};
-
-	const handleOnMouseEvent = ( event ) => {
-		onMouseEnter( event.currentTarget, description );
-	};
-
-	const handleOnMouseLeave = ( event ) => {
-		onMouseLeave( event.currentTarget, description );
-	};
-
 	return (
 		<div className="plan-features__item">
 			<Gridicon
 				className="plan-features__item-checkmark"
 				size={ 18 } icon="checkmark" />
 			{ children }
-			<span
-				onMouseEnter={ handleOnMouseEvent }
-				onMouseLeave={ handleOnMouseLeave }
-				onTouchStart={ handleOnTouchStart }
+			<InfoPopover
 				className="plan-features__item-tip-info"
-			>
-				<Gridicon icon="info-outline" size={ 18 } />
-			</span>
+				position={ viewport.isMobile() ? 'top' : 'right' }>
+				{ description }
+			</InfoPopover>
 		</div>
 	);
 }
