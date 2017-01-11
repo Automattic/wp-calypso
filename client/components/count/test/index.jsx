9a8b1cc580d9423543e4f6739b00a6dde3e7adc9
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2017-01-11 23:32:45 +0100

    Es6ify Count and make it SSR friendly. (#10539)

--- client/components/count/test/index.jsx
+++ client/components/count/test/index.jsx
@@ -1,80 +1,26 @@
 /**
  * External dependencies
  */
-var expect = require( 'chai' ).expect,
-	useMockery = require( 'test/helpers/use-mockery' );
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import { identity } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { Count } from '../';
 
 describe( 'Count', function() {
-	var React, ReactClass, TestUtils, Count, renderer;
-
-	// really only using Mockery for the clean module cache
-	useMockery();
-
-	before( function() {
-		React = require( 'react' );
-		ReactClass = require( 'react/lib/ReactClass' );
-		TestUtils = require( 'react-addons-test-utils' );
-
-		ReactClass.injection.injectMixin( require( 'i18n-calypso' ).mixin );
-		Count = require( '../' );
-	} );
-
-	beforeEach( function() {
-		renderer = TestUtils.createRenderer();
-	} );
-
-	it( 'should render the passed count', function() {
-		var result;
-
-		renderer.render( <Count count={ 23 } /> );
-		result = renderer.getRenderOutput();
-
-		expect( result.props.className ).to.equal( 'count' );
-		expect( result.props.children ).to.equal( '23' );
-	} );
-
 	it( 'should use the correct class name', function() {
-		var result;
-
-		renderer.render( <Count count={ 23 } /> );
-		result = renderer.getRenderOutput();
-
-		expect( result.props.className ).to.equal( 'count' );
+		const count = shallow( <Count count={ 23 } numberFormat={ identity } /> );
+		expect( count ).to.have.className( 'count' );
 	} );
 
-	it( 'should internationalize the passed count', function() {
-		var result;
-
-		renderer.render( <Count count={ 2317 } /> );
-		result = renderer.getRenderOutput();
-
-		expect( result.props.children ).to.equal( '2,317' );
-	} );
-
-	it( 'should render zero', function() {
-		var result;
-
-		renderer.render( <Count count={ 0 } /> );
-		result = renderer.getRenderOutput();
-
-		expect( result.props.children ).to.equal( '0' );
-	} );
-
-	it( 'should render negative numbers', function() {
-		var result;
-
-		renderer.render( <Count count={ -1000 } /> );
-		result = renderer.getRenderOutput();
-
-		expect( result.props.children ).to.equal( '-1,000' );
-	} );
-
-	it( 'should cut off floating point numbers', function() {
-		var result;
-
-		renderer.render( <Count count={ 3.1415926 } /> );
-		result = renderer.getRenderOutput();
-
-		expect( result.props.children ).to.equal( '3' );
+	it( 'should call provided as prop numberFormat function', function() {
+		const numberFormatSpy = spy();
+		shallow( <Count count={ 23 } numberFormat={ numberFormatSpy } /> );
+		expect( numberFormatSpy ).to.have.been.calledWith( 23 );
 	} );
 } );
