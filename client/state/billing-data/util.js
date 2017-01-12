0ab7a96e717e2997e2d96029f1705ae2dca7cbee
Marin Atanasov <tyxla@abv.bg>
2017-01-12 11:18:00 +0200

    State: Simplify parseTransactionDate()

--- client/state/billing-data/util.js
+++ client/state/billing-data/util.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { assign } from 'lodash';
-
-/**
  * Parses the date within a transaction.
  * Returns a copy of the updated transaction.
  *
@@ -11,7 +6,5 @@ import { assign } from 'lodash';
  * @return {Object}               Updated transaction with date converted from string to a Date object.
  */
 export const parseTransactionDate = ( transaction ) => {
-	return assign( {}, transaction, {
-		date: new Date( transaction.date )
-	} );
+	return { ...transaction, date: new Date( transaction.date ) };
 };
