7a3f2080546f44bee2b5d05e828e6e5e4ee4fde8
Marin Atanasov <tyxla@users.noreply.github.com>
2017-01-16 09:38:08 +0200

    State: Convert lib/billing-history-data to Redux (#10379)
    * State: Add billing data parseDate utility
    
    * State: Add Billing data actions with tests
    
    * State: Add billing data schema
    
    * State: Add billing data reducer with tests
    
    * State: Add billing data selectors with tests
    
    * State: Have billing data items state default to an empty object
    
    * State: Improve billing data selectors
    
    * State: Simplify billing data reducer
    
    * Billing: Use native Date instead of moment in parseDate()
    
    * State: Improve parseDate() docs
    
    * State: Rename parseDate to parseTransactionDate
    
    * State: Simplify parseTransactionDate()
    
    * State: Parse transaction date in selectors and not in actions
    
    * State: Memoize billing data selectors
    
    * State: Rename billing data to billing transactions
    
    * State: Fix language with regards to billing transactions renaming
    
    * State: Use moment instead of native date in billing transactions
    
    * State: Add a missing dependant in billing history selectors
    
    * State: Migrate billing-transactions selectors to global selectors

---
+++ client/state/selectors/get-past-billing-transaction.js
@@ -0,0 +1,32 @@
+/**
+ * External dependencies
+ */
+import { get, find } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import createSelector from 'lib/create-selector';
+import { getBillingTransactions } from './';
+
+/**
+ * Returns a past billing transaction.
+ * Returns null if the billing transactions have not been fetched yet, or there is no transaction with that ID.
+ *
+ * @param  {Object}  state   Global state tree
+ * @param  {String}  id      ID of the transaction
+ * @return {?Object}         The transaction object
+ */
+const getPastBillingTransaction = createSelector(
+	( state, id ) => {
+		const pastTransactions = get( getBillingTransactions( state ), [ 'past' ], null );
+		if ( ! pastTransactions ) {
+			return null;
+		}
+
+		return find( pastTransactions, { id } ) || null;
+	},
+	getBillingTransactions
+);
+
+export default getPastBillingTransaction;
