5a228839713993d3ffbde6214d9a31fa16a08800
Andrew Duthie <andrew@andrewduthie.com>
2017-01-17 09:36:23 -0500

    Sites: Sync changes to individual sites

--- client/state/sites/test/enhancer.js
+++ client/state/sites/test/enhancer.js
@@ -4,13 +4,14 @@
  * External dependencies
  */
 import { expect } from 'chai';
-import { spy, match } from 'sinon';
+import { match } from 'sinon';
 
 /**
  * Internal dependencies
  */
 import useFakeDom from 'test/helpers/use-fake-dom';
-import { SITES_UPDATE } from 'state/action-types';
+import { useSandbox } from 'test/helpers/use-sinon';
+import { SITES_UPDATE, SITE_RECEIVE } from 'state/action-types';
 import sitesSync from '../enhancer';
 
 /**
@@ -24,25 +25,27 @@ const EXAMPLE_SITE = {
 };
 
 describe( 'sitesSync()', () => {
-	let sitesListFactory, Site, dispatch, createStore, store, sitesList;
+	let sitesListFactory, Site, store, sitesList;
+	const state = { sites: { items: {} } };
 
 	useFakeDom();
 
-	before( () => {
+	useSandbox( ( sandbox ) => {
 		Site = require( 'lib/site' );
 		sitesListFactory = require( 'lib/sites-list' );
 
-		dispatch = spy();
-		createStore = sitesSync( () => ( {
-			dispatch
-		} ) );
+		store = sitesSync( () => ( {
+			dispatch: sandbox.spy(),
+			getState: sandbox.stub().returns( state )
+		} ) )();
+
+		sitesList = sitesListFactory();
+		sandbox.spy( sitesList, 'sync' );
 	} );
 
 	beforeEach( () => {
-		store = createStore();
-		sitesList = sitesListFactory();
+		state.sites.items = {};
 		sitesList.initialize( [] );
-		spy( sitesList, 'sync' );
 	} );
 
 	it( 'should preserve existing sync behavior', () => {
@@ -58,5 +61,31 @@ describe( 'sitesSync()', () => {
 			type: SITES_UPDATE,
 			sites: [ match.instanceOf( Site ) ]
 		} );
+	} );
+
+	it( 'should reflect into state updates to a site', () => {
+		state.sites.items[ 2916284 ] = EXAMPLE_SITE;
+		const site = new Site( EXAMPLE_SITE );
+		site.set( { name: 'WordPress.com Example Blog!' } );
+
+		expect( store.dispatch ).to.have.been.calledWithMatch( {
+			type: SITE_RECEIVE,
+			site: match( { name: 'WordPress.com Example Blog!' } )
+		} );
+	} );
+
+	it( 'should not reflect into state updates to a site if not tracked', () => {
+		const site = new Site( EXAMPLE_SITE );
+		site.set( { name: 'WordPress.com Example Blog!' } );
+
+		expect( store.dispatch ).to.not.have.been.called;
+	} );
+
+	it( 'should not reflect into state updates to a site if no changes', () => {
+		state.sites.items[ 2916284 ] = EXAMPLE_SITE;
+		const site = new Site( EXAMPLE_SITE );
+		site.set( { name: 'WordPress.com Example Blog' } );
+
+		expect( store.dispatch ).to.not.have.been.called;
 	} );
 } );
