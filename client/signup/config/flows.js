891bca12361b6d7cc20e0d3667fb1556cfcd12b5
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-01-17 19:21:13 +0100

    Signup: Pass required dependencies for plans step when skipping it in the domain first flow

--- client/signup/config/flows.js
+++ client/signup/config/flows.js
@@ -225,7 +225,7 @@ const flows = {
 
 if ( config.isEnabled( 'signup/domain-first-flow' ) ) {
 	flows[ 'domain-first' ] = {
-		steps: [ 'domain-only', 'site-or-domain', 'themes', 'domain-first-plans', 'user' ],
+		steps: [ 'domain-only', 'site-or-domain', 'themes', 'plans', 'user' ],
 		destination: getSiteDestination,
 		description: 'An experimental approach for WordPress.com/domains',
 		lastModified: '2017-01-16'
