05ce8184dfc960629c90fbd6c7df10505ceba9d5
bluefuton <chris@bluefuton.com>
2017-01-18 11:54:03 +0000

    Move selector to state/selectors

--- client/state/selectors/is-site-blocked.js
+++ client/state/selectors/is-site-blocked.js
@@ -2,9 +2,9 @@
  * Returns true if a site is known to be blocked
  *
  * @param  {Object}  state  Global state tree
- * @param  {Integer} siteId Site ID
- * @return {Boolean} Is the specified site blocked?
+ * @param  {Number}  siteId Site ID
+ * @return {Boolean}        Whether specified site is blocked
  */
-export function isSiteBlocked( state, siteId ) {
+export default function isSiteBlocked( state, siteId ) {
 	return !! state.reader.siteBlocks.items[ siteId ];
 }
