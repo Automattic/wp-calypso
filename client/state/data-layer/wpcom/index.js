3820502d04bb89e88e1a75e2ac70a662630a7544
retrofox <rdsuarez@gmail.com>
2017-01-19 15:21:25 -0300

    data-layer: add `timezones` communcation layer

--- client/state/data-layer/wpcom/index.js
+++ client/state/data-layer/wpcom/index.js
@@ -5,11 +5,13 @@ import { mergeHandlers } from 'state/data-layer/utils';
 import plans from './plans';
 import sites from './sites';
 import read from './read';
+import timezones from './timezones';
 
 export const handlers = mergeHandlers(
 	plans,
 	sites,
 	read,
+	timezones,
 );
 
 export default handlers;
