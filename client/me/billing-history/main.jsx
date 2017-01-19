f35761c6dfee3a6076572fb9ae92b9ae25b635b9
Marin Atanasov <tyxla@users.noreply.github.com>
2017-01-19 09:24:55 +0200

    Billing: Use PageViewTracker for analytics (#10684)
    * Billing: Use PageViewTracker for analytics
    
    * Billing: Use the right path for receipt PageViewTracker

--- client/me/billing-history/main.jsx
+++ client/me/billing-history/main.jsx
@@ -20,6 +20,7 @@ import UpcomingChargesTable from './upcoming-charges-table';
 import SectionHeader from 'components/section-header';
 import Main from 'components/main';
 import DocumentHead from 'components/data/document-head';
+import PageViewTracker from 'lib/analytics/page-view-tracker';
 import QueryBillingTransactions from 'components/data/query-billing-transactions';
 import purchasesPaths from 'me/purchases/paths';
 import { getBillingTransactions } from 'state/selectors';
@@ -33,6 +34,7 @@ const BillingHistory = React.createClass( {
 		return (
 			<Main className="billing-history">
 				<DocumentHead title={ translate( 'Billing History' ) } />
+				<PageViewTracker path="/me/purchases/billing" title="Me > Billing History" />
 				<MeSidebarNavigation />
 				<QueryBillingTransactions />
 				<PurchasesHeader section={ 'billing' } />
