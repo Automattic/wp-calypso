ae78820664f7aa32be568ae74322bf3eb6109e74
Marin Atanasov <tyxla@abv.bg>
2017-01-19 15:28:12 +0200

    Jetpack Settings: Introduce normalize and sanitize utils for settings

---
+++ client/state/jetpack/settings/utils.js
@@ -0,0 +1,41 @@
+/**
+ * Normalize settings for use in Redux.
+ *
+ * @param  {Object}   settings   Raw settings.
+ * @return {Object}              Normalized settings.
+ */
+export const normalizeSettings = ( settings ) => {
+	return Object.keys( settings ).reduce( ( memo, key ) => {
+		switch ( key ) {
+			case 'wp_mobile_excerpt':
+			case 'wp_mobile_featured_images':
+				memo[ key ] = settings[ key ] === 'enabled';
+				break;
+			default:
+				memo[ key ] = settings[ key ];
+		}
+
+		return memo;
+	}, {} );
+};
+
+/**
+ * Sanitize settings for updating in the Jetpack site.
+ *
+ * @param  {Object}   settings   Settings.
+ * @return {Object}              Normalized settings.
+ */
+export const sanitizeSettings = ( settings ) => {
+	return Object.keys( settings ).reduce( ( memo, key ) => {
+		switch ( key ) {
+			case 'wp_mobile_excerpt':
+			case 'wp_mobile_featured_images':
+				memo[ key ] = !! settings [ key ] ? 'enabled' : 'disabled';
+				break;
+			default:
+				memo[ key ] = settings[ key ];
+		}
+
+		return memo;
+	}, {} );
+};
