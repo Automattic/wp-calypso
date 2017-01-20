ce81a2c0fcdb1f75ea3ca34041734f3ac56f5f2b
Dennis Snell <dennis.snell@automattic.com>
2017-01-20 13:17:13 -0700

    Revert "Update the data layer to allow for passing actions through middleware"

--- client/state/data-layer/wpcom-api-middleware.js
+++ client/state/data-layer/wpcom-api-middleware.js
@@ -1,8 +1,7 @@
 /**
  * Internal dependencies
  */
-import handlerMap from './wpcom';
-import { local } from './utils';
+import handlers from './wpcom';
 
 /**
  * WPCOM Middleware API
@@ -10,47 +9,13 @@ import { local } from './utils';
  * Intercepts actions requesting data provided by the
  * WordPress.com API and passes them off to the
  * appropriate handler.
- *
- * @see state/utils/local indicates that action should bypass data layer
- *
- * Note:
- *
- * This function has been optimized for speed and has
- * in turn sacrificed some readability. It's mainly
- * performing two checks:
- *
- *  - Are there handlers defined for the given action type?
- *  - Is there action meta indicating to bypass these handlers?
- *
- * The optimizations reduce function-calling and object
- * property lookup where possible.
  */
-export const middleware = handlers => store => next => {
-	const localNext = action => next( local( action ) );
+export const middleware = store => next => action =>
+	// we won't use has( handlers, action.type )
+	// here because of performance implications
+	// this function is run on every dispatch
+	!! handlers[ action.type ]
+		? handlers[ action.type ].forEach( handler => handler( store, action ) )
+		: next( action );
 
-	return action => {
-		const handlerChain = handlers[ action.type ];
-
-		// if no handler is defined for the action type
-		// then pass it along the chain untouched
-		if ( ! handlerChain ) {
-			return next( action );
-		}
-
-		const meta = action.meta;
-		if ( meta ) {
-			const dataLayer = meta.dataLayer;
-
-			// if the action indicates that we should
-			// bypass the data layer, then pass it
-			// along the chain untouched
-			if ( true === dataLayer.doBypass ) {
-				return next( action );
-			}
-		}
-
-		return handlerChain.forEach( handler => handler( store, action, localNext ) );
-	}
-};
-
-export default middleware( handlerMap );
+export default middleware;
