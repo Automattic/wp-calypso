78e4e260e4bac79c722b1207637faf9980bf85bf
Dennis Snell <dennis.snell@automattic.com>
2017-01-20 13:03:16 -0700

    Update the data layer to allow for passing actions through middleware (#10287)
    * Update the data layer to allow for passing actions through middleware
    
    The data layer was designed to intercept and discard actions requiring
    side-effecting and asynchronous actions. This was chosen in part because
    we might have situations where multiple middlewares could provide for
    the same types of data needs and we only want the first in the chain to
    actually handle the requests.
    
    While this choice was reasonable for fetching actions such as
    `INSULT_REQUEST` it was less fitting for actions that update data
    remotely because the reducers would still need to respond to these
    changes before hearing back from APIs etc...
    
    For example, `POST_LIKE` should immediately update the state tree to
    indicate that a post has been like, but the API middleware should also
    intercept the action and ship the changes to the server. This middleware
    now needs to pass the action along after processing.
    
    We want to be careful in these situations not to get stuck in loops
    because the continued action could theoretically hit the middleware
    again. Further, response from API requests that dispatch new actions
    could trigger similar cycles in the control flow graph.
    
    To prevent looping I have created `local()` as a wrapper around actions
    to indicate that they should skip all processing by the data layer.
    Actions wrapped by `local()` will thus automatically bypass the
    middleware and are safe to pass along the chain.
    
    In the example of liking a post the middleware would finish by calling
    the following line of code to send the action to the reducers and skip
    further handling.
    
    ```js
    likePost( store, action, next ) {
    	...
    
    	// pass along action to reducers
    	next( local( action ) );
    }
    ```
    
    You may notice that a new parameter `next` has been given to the
    middleware signature. This is the standard `next` from Redux middleware
    and is the dispatcher assigned to continue in the dispatching chain.

--- client/state/data-layer/test/utils.js
+++ client/state/data-layer/test/utils.js
@@ -6,9 +6,34 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import { mergeHandlers } from '../utils';
+import { local, mergeHandlers } from '../utils';
 
 describe( 'Data Layer', () => {
+	describe( '#local', () => {
+		it( 'should wrap an action with the bypass flag', () => {
+			const action = { type: 'ADD_SPLINE', id: 42 };
+			const localAction = local( action );
+
+			expect( localAction ).to.have.deep.property( 'meta.dataLayer.doBypass', true );
+		} );
+
+		it( 'should not destroy existing meta', () => {
+			const action = {
+				type: 'SHAVE_THE_WHALES',
+				meta: {
+					oceanName: 'ARCTIC',
+					dataLayer: {
+						forceRefresh: true,
+					}
+				}
+			};
+			const localAction = local( action );
+
+			expect( localAction ).to.have.deep.property( 'meta.oceanName', 'ARCTIC' );
+			expect( localAction ).to.have.deep.property( 'meta.dataLayer.forceRefresh', true );
+		} );
+	} );
+
 	describe( '#mergeHandlers', () => {
 		const inc = a => a + 1;
 		const triple = a => a * 3;
