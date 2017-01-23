43ca07148459801ad7456a205bde9a08c5e2f15f
Jake F - @samouri <samouri@users.noreply.github.com>
2017-01-23 13:15:40 -0500

    exerimentation with query-component generator function

--- client/components/data/query-reader-lists/index.jsx
+++ client/components/data/query-reader-lists/index.jsx
@@ -1,48 +1,14 @@
 /**
- * External dependencies
- */
-import { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-
-/**
  * Internal dependencies
  */
 import { isRequestingSubscribedLists } from 'state/reader/lists/selectors';
 import { requestSubscribedLists } from 'state/reader/lists/actions';
+import { createQueryComponent } from '../utils';
 
-class QueryReaderLists extends Component {
-	componentWillMount() {
-		if ( this.props.isRequestingSubscribedLists ) {
-			return;
-		}
+const QueryReaderLists = createQueryComponent( {
+	shouldRequest: ( state ) => ! isRequestingSubscribedLists( state ),
+	request: requestSubscribedLists,
+} );
 
-		this.props.requestSubscribedLists();
-	}
+export default QueryReaderLists;
 
-	render() {
-		return null;
-	}
-}
-
-QueryReaderLists.propTypes = {
-	isRequestingSubscribedLists: PropTypes.bool,
-	requestSubscribedLists: PropTypes.func
-};
-
-QueryReaderLists.defaultProps = {
-	requestSubscribedLists: () => {}
-};
-
-export default connect(
-	( state ) => {
-		return {
-			isRequestingSubscribedLists: isRequestingSubscribedLists( state )
-		};
-	},
-	( dispatch ) => {
-		return bindActionCreators( {
-			requestSubscribedLists
-		}, dispatch );
-	}
-)( QueryReaderLists );
