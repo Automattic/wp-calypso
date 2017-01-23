754772594dffc7b87715f010843216cb2696b7b4
Jeff Bowen <jeff@automattic.com>
2017-01-23 14:29:05 -0500

    Include the section in the dependent state

--- client/state/document-head/selectors.js
+++ client/state/document-head/selectors.js
@@ -80,7 +80,11 @@ export const getDocumentHeadFormattedTitle = createSelector(
 
 		return title + 'WordPress.com';
 	},
-	( state ) => [ state.ui.selectedSiteId, state.documentHead ]
+	( state ) => [
+		state.documentHead,
+		state.ui.section,
+		state.ui.selectedSiteId,
+	]
 );
 
 /**
