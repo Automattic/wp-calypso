e0c89c9f097c092c3ed2ed34cccbc57b9daa4243
Dennis Snell <dennis.snell@automattic.com>
2017-01-23 12:29:23 -0700

    Update/data layer/pass actions (#10834)
    * Copy from previous branch
    
    * Fix possible type error
    
    * Add tests
    
    * Why did this not make it into the earlier commits?

--- client/state/data-layer/wpcom-api-middleware.js
+++ client/state/data-layer/wpcom-api-middleware.js
@@ -1,7 +1,8 @@
 /**
  * Internal dependencies
  */
-import handlers from './wpcom';
+import handlerMap from './wpcom';
+import { local } from './utils';
 
 /**
  * WPCOM Middleware API
@@ -9,13 +10,47 @@ import handlers from './wpcom';
  * Intercepts actions requesting data provided by the
  * WordPress.com API and passes them off to the
  * appropriate handler.
+ *
+ * @see state/utils/local indicates that action should bypass data layer
+ *
+ * Note:
+ *
+ * This function has been optimized for speed and has
+ * in turn sacrificed some readability. It's mainly
+ * performing two checks:
+ *
+ *  - Are there handlers defined for the given action type?
+ *  - Is there action meta indicating to bypass these handlers?
+ *
+ * The optimizations reduce function-calling and object
+ * property lookup where possible.
  */
-export const middleware = store => next => action =>
-	// we won't use has( handlers, action.type )
-	// here because of performance implications
-	// this function is run on every dispatch
-	!! handlers[ action.type ]
-		? handlers[ action.type ].forEach( handler => handler( store, action ) )
-		: next( action );
+export const middleware = handlers => store => next => {
+	const localNext = action => next( local( action ) );
 
-export default middleware;
+	return action => {
+		const handlerChain = handlers[ action.type ];
+
+		// if no handler is defined for the action type
+		// then pass it along the chain untouched
+		if ( ! handlerChain ) {
+			return next( action );
+		}
+
+		const meta = action.meta;
+		if ( meta ) {
+			const dataLayer = meta.dataLayer;
+
+			// if the action indicates that we should
+			// bypass the data layer, then pass it
+			// along the chain untouched
+			if ( dataLayer && true === dataLayer.doBypass ) {
+				return next( action );
+			}
+		}
+
+		return handlerChain.forEach( handler => handler( store, action, localNext ) );
+	}
+};
+
+export default middleware( handlerMap );
