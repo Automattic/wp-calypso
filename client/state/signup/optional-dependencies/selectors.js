21d2895ed650f813f9746986919a64908aedf725
Biser Perchinkov <biser@bisko.be>
2017-01-23 18:36:08 +0200

    Signup: Fix username suggestions not working (#10795)
    * Signup: Fix username suggestions not working
    
    Username suggestions were broken due to a prop name change in #10599.
    
    This PR improves the fetching of the suggested username to use Redux and properly just pass the prop to `SignupForm`
    
    * Improve  loading from . Add unit tests for the selector
    
    * Improve analytics middleware handling in User Step

---
+++ client/state/signup/optional-dependencies/selectors.js
@@ -0,0 +1,8 @@
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+export function getSuggestedUsername( state ) {
+	return get( state, 'signup.optionalDependencies.suggestedUsername', '' );
+}
