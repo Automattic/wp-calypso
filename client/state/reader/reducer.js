d86b9d92de4ba5bfbf700982e14d6b9d411f9137
bluefuton <chris@bluefuton.com>
2017-01-24 17:01:44 +0000

    Remove Cold Start

--- client/state/reader/reducer.js
+++ client/state/reader/reducer.js
@@ -10,7 +10,6 @@ import lists from './lists/reducer';
 import feeds from './feeds/reducer';
 import follows from './follows/reducer';
 import sites from './sites/reducer';
-import start from './start/reducer';
 import posts from './posts/reducer';
 import relatedPosts from './related-posts/reducer';
 import siteBlocks from './site-blocks/reducer';
@@ -22,7 +21,6 @@ export default combineReducers( {
 	follows,
 	lists,
 	sites,
-	start,
 	posts,
 	relatedPosts,
 	siteBlocks,
