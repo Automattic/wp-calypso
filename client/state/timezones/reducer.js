7121663d478608e353bca0955dd754092c6449ba
retrofox <rdsuarez@gmail.com>
2017-01-24 15:39:15 -0300

    state-tree: add `timezones` reducer

---
+++ client/state/timezones/reducer.js
@@ -0,0 +1,34 @@
+/**
+ * External dependencies
+ */
+import { combineReducers } from 'redux';
+
+/**
+ * Internal dependencies
+ */
+import { createReducer } from 'state/utils';
+import { TIMEZONES_RECEIVE } from 'state/action-types';
+
+import {
+	rawOffsetsSchema,
+	labelsSchema,
+	continentsSchema
+} from './schema';
+
+export const rawOffsets = createReducer( {}, {
+	[ TIMEZONES_RECEIVE ]: ( state, actions ) => ( actions.rawOffsets )
+}, rawOffsetsSchema );
+
+export const labels = createReducer( {}, {
+	[ TIMEZONES_RECEIVE ]: ( state, actions ) => ( actions.labels )
+}, labelsSchema );
+
+export const byContinents = createReducer( {}, {
+	[ TIMEZONES_RECEIVE ]: ( state, actions ) => ( actions.byContinents )
+}, continentsSchema );
+
+export default combineReducers( {
+	rawOffsets,
+	labels,
+	byContinents,
+} );
