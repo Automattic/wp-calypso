d884e3a1a8f915684a0b21ab64289329be34e542
retrofox <rdsuarez@gmail.com>
2017-01-24 17:56:49 -0300

    state-tree: add `timezones` reducer tests

---
+++ client/state/timezones/test/reducer.js
@@ -0,0 +1,357 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
+
+/**
+ * Internal dependencies
+ */
+import { useSandbox } from 'test/helpers/use-sinon';
+
+import timezonesReducer, {
+	byContinents,
+	labels,
+	rawOffsets,
+} from '../reducer';
+
+import { timezonesReceive } from '../actions';
+
+describe( 'reducer', () => {
+	let sandbox;
+
+	useSandbox( newSandbox => {
+		sandbox = newSandbox;
+		sandbox.stub( console, 'warn' );
+	} );
+
+	it( 'should export expected reducer keys', () => {
+		expect( timezonesReducer( undefined, {} ) ).to.have.keys( [
+			'byContinents',
+			'labels',
+			'rawOffsets',
+		] );
+	} );
+
+	describe( '#rawOffsets()', () => {
+		it( 'should default to an empty Object', () => {
+			expect( rawOffsets( undefined, {} ) ).to.eql( {} );
+		} );
+
+		it( 'should index `rawOffsets` state', () => {
+			const initialState = undefined;
+
+			const action = timezonesReceive( {
+				rawOffsets: {
+					'UTC+0': 'UTC',
+					'UTC-12': 'UTC-12',
+					'UTC-11.5': 'UTC-11:30',
+				}
+			} );
+
+			const expectedState = {
+				'UTC+0': 'UTC',
+				'UTC-12': 'UTC-12',
+				'UTC-11.5': 'UTC-11:30',
+			};
+
+			const newState = rawOffsets( initialState, action );
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should override `rawOffsets` state', () => {
+			const initialState = {
+				'UTC+13.75': 'UTC+13:45',
+				'UTC+14': 'UTC+14',
+			};
+			deepFreeze( initialState );
+
+			const action = timezonesReceive( {
+				rawOffsets: {
+					'UTC+0': 'UTC',
+					'UTC-12': 'UTC-12',
+					'UTC-11.5': 'UTC-11:30',
+				}
+			} );
+
+			const expectedState = {
+				'UTC+0': 'UTC',
+				'UTC-12': 'UTC-12',
+				'UTC-11.5': 'UTC-11:30',
+			};
+
+			const newState = rawOffsets( initialState, action );
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should persist state', () => {
+			const initialState = {
+				rawOffsets: {
+					'UTC+13.75': 'UTC+13:45',
+					'UTC+14': 'UTC+14',
+				}
+			};
+			deepFreeze( initialState );
+
+			const action = { type: 'SERIALIZE' };
+			const expectedState = initialState;
+			const newState = rawOffsets( initialState, action );
+
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should load persisted state', () => {
+			const initialState = {
+				'UTC+0': 'UTC',
+				'UTC+13.75': 'UTC+13:45',
+				'UTC+14': 'UTC+14',
+			};
+			deepFreeze( initialState );
+
+			const action = { type: 'DESERIALIZE' };
+			const expectedState = initialState;
+			const newState = rawOffsets( initialState, action );
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should not load invalid persisted state', () => {
+			const initialStateONE = { rawOffsets: { foo: 'bar' } };
+			deepFreeze( initialStateONE );
+			const newStateONE = rawOffsets( initialStateONE, { type: 'DESERIALIZE' } );
+
+			// 'UTC' shouldn't be allowed either.
+			const initialStateTWO = { rawOffsets: { UTC: 'UTC' } };
+			deepFreeze( initialStateTWO );
+			const newStateTWO = rawOffsets( initialStateTWO, { type: 'DESERIALIZE' } );
+
+			expect( newStateONE ).to.eql( {} );
+			expect( newStateTWO ).to.eql( {} );
+		} );
+	} );
+
+	describe( '#labels()', () => {
+		it( 'should default to an empty Object', () => {
+			expect( rawOffsets( undefined, {} ) ).to.eql( {} );
+		} );
+
+		it( 'should index `labels` state', () => {
+			const initialState = undefined;
+
+			const action = timezonesReceive( {
+				labels: {
+					'Asia/Aqtobe': 'Aqtobe',
+					'America/Boa_Vista': 'Boa Vista',
+					'Indian/Comoro': 'Comoro',
+				}
+			} );
+
+			const expectedState = {
+				'Asia/Aqtobe': 'Aqtobe',
+				'America/Boa_Vista': 'Boa Vista',
+				'Indian/Comoro': 'Comoro',
+			};
+
+			const newState = labels( initialState, action );
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should override `labels` state', () => {
+			const initialState = {
+				'Australia/Currie': 'Currie',
+				'Indian/Mauritius': 'Mauritius',
+			};
+			deepFreeze( initialState );
+
+			const action = timezonesReceive( {
+				labels: {
+					'Asia/Aqtobe': 'Aqtobe',
+					'America/Boa_Vista': 'Boa Vista',
+					'Indian/Comoro': 'Comoro',
+				}
+			} );
+
+			const expectedState = {
+				'Asia/Aqtobe': 'Aqtobe',
+				'America/Boa_Vista': 'Boa Vista',
+				'Indian/Comoro': 'Comoro',
+			};
+
+			const newState = labels( initialState, action );
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should persist state', () => {
+			const initialState = {
+				labels: {
+					'Asia/Aqtobe': 'Aqtobe',
+					'America/Boa_Vista': 'Boa Vista',
+					'Indian/Comoro': 'Comoro',
+				}
+			};
+			deepFreeze( initialState );
+
+			const action = { type: 'SERIALIZE' };
+			const expectedState = initialState;
+			const newState = labels( initialState, action );
+
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should load persisted state', () => {
+			const initialState = {
+				'Asia/Aqtobe': 'Aqtobe',
+				'America/Boa_Vista': 'Boa Vista',
+				'Indian/Comoro': 'Comoro',
+			};
+			deepFreeze( initialState );
+
+			const action = { type: 'DESERIALIZE' };
+			const expectedState = initialState;
+			const newState = labels( initialState, action );
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should not load invalid persisted state', () => {
+			const initialStateONE = { labels: { foo: 'bar' } };
+			deepFreeze( initialStateONE );
+			const newStateONE = labels( initialStateONE, { type: 'DESERIALIZE' } );
+			expect( newStateONE ).to.eql( {} );
+		} );
+	} );
+
+	describe( '#byContinents()', () => {
+		it( 'should default to an empty Object', () => {
+			expect( rawOffsets( undefined, {} ) ).to.eql( {} );
+		} );
+
+		it( 'should index `rawOffsets` state', () => {
+			const initialState = undefined;
+
+			const action = timezonesReceive( {
+				byContinents: {
+					Asia: [
+						'Asia/Aqtobe',
+					],
+					America: [
+						'America/Blanc-Sablon',
+						'America/Boa_Vista',
+					],
+					Indian: [
+						'Indian/Comoro',
+					],
+				},
+
+			} );
+
+			const expectedState = {
+				Asia: [
+					'Asia/Aqtobe',
+				],
+				America: [
+					'America/Blanc-Sablon',
+					'America/Boa_Vista',
+				],
+				Indian: [
+					'Indian/Comoro',
+				],
+			};
+
+			const newState = byContinents( initialState, action );
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should override timezones.byContinents state', () => {
+			const initialState = {
+				Pacific: [
+					'Pacific/Funafuti',
+				]
+			};
+			deepFreeze( initialState );
+
+			const action = timezonesReceive( {
+				byContinents: {
+					Asia: [
+						'Asia/Aqtobe',
+					],
+					America: [
+						'America/Blanc-Sablon',
+						'America/Boa_Vista',
+					],
+					Indian: [
+						'Indian/Comoro',
+					],
+				},
+
+			} );
+
+			const expectedState = {
+				Asia: [
+					'Asia/Aqtobe',
+				],
+				America: [
+					'America/Blanc-Sablon',
+					'America/Boa_Vista',
+				],
+				Indian: [
+					'Indian/Comoro',
+				],
+			};
+
+			const newState = byContinents( initialState, action );
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should persist state', () => {
+			const initialState = {
+				byContinents: {
+					Asia: [
+						'Asia/Aqtobe',
+					],
+					America: [
+						'America/Blanc-Sablon',
+						'America/Boa_Vista',
+					],
+					Indian: [
+						'Indian/Comoro',
+					],
+				},
+
+			};
+			deepFreeze( initialState );
+
+			const action = { type: 'SERIALIZE' };
+			const expectedState = initialState;
+			const newState = byContinents( initialState, action );
+
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should load persisted state', () => {
+			const initialState = {
+				Asia: [
+					'Asia/Aqtobe',
+				],
+				America: [
+					'America/Blanc-Sablon',
+					'America/Boa_Vista',
+				],
+				Indian: [
+					'Indian/Comoro',
+				],
+			};
+			deepFreeze( initialState );
+
+			const action = { type: 'DESERIALIZE' };
+			const expectedState = initialState;
+			const newState = byContinents( initialState, action );
+			expect( newState ).to.eql( expectedState );
+		} );
+
+		it( 'should not load invalid persisted state', () => {
+			const initialStateONE = { byContinents: { foo: 'bar' } };
+			deepFreeze( initialStateONE );
+			const newStateONE = byContinents( initialStateONE, { type: 'DESERIALIZE' } );
+			expect( newStateONE ).to.eql( {} );
+		} );
+	} );
+} );
