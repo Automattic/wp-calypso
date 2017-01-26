0286051cfdab617f757da6a45f4b3a0ec733bc02
Marin Atanasov <tyxla@users.noreply.github.com>
2017-01-26 09:37:04 +0200

    State: Move Jetpack jumpstart selectors to global selectors directory (#10930)
    * State: Move isActivatingJetpackJumpstart selector to global selector directory
    
    * State: Move isDeactivatingJetpackJumpstart selector to global selector directory
    
    * State: Move isRequestingJetpackJumpstartStatus selector to global selector directory
    
    * State: Move getJetpackJumpstartStatus selector to global selector directory

---
+++ client/state/selectors/test/fixtures/jetpack-jumpstart.js
@@ -0,0 +1,22 @@
+export const items = {
+	12345678: 'jumpstart_activated',
+	87654321: 'jumpstart_dismissed',
+};
+
+export const requests = {
+	12345678: {
+		activating: true,
+		deactivating: false,
+		requesting: false
+	},
+	87654321: {
+		activating: false,
+		deactivating: true,
+		requesting: false
+	},
+	11223344: {
+		activating: false,
+		deactivating: false,
+		requesting: true
+	},
+};
