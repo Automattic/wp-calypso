9120487f2f9679b9ba082001ebe4c05d9c90d94f
retrofox <rdsuarez@gmail.com>
2017-01-26 16:38:52 -0300

    state:selectors: add `getSiteGmtOffset` selector

--- client/state/selectors/index.js
+++ client/state/selectors/index.js
@@ -39,6 +39,7 @@ export getPostLikes from './get-post-likes';
 export getRawOffsets from './get-raw-offsets';
 export getReaderTeams from './get-reader-teams';
 export getSharingButtons from './get-sharing-buttons';
+export getSiteGmtOffset from './get-site-gmt-offset';
 export getSiteIconId from './get-site-icon-id';
 export getSiteIconUrl from './get-site-icon-url';
 export getSiteStatsQueryDate from './get-site-stats-query-date';
