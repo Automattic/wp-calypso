4e223ecf3e26644c5e76aad8e3e4eb96a0588d64
Jake F - @samouri <samouri@users.noreply.github.com>
2017-01-26 16:45:02 -0500

    addressed @blowery comment about using get

--- client/state/selectors/get-reader-teams.js
+++ client/state/selectors/get-reader-teams.js
@@ -1,14 +1,9 @@
 /**
- * External dependencies
- */
-import { get } from 'lodash';
-
-/**
  * Returns all of the reader teams for a user
  *
  * @param  {Object}  state  Global state tree
  * @return {Array}          Reader Teams
  */
 export default function getReaderTeams( state ) {
-	return get( state, [ 'reader', 'teams', 'items' ] );
+	return state.reader.teams.items;
 }
