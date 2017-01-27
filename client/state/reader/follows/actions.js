e20e90b752dc4270764d391a26ca4a4dac7eea88
Chris Rosser <chris@bluefuton.com>
2017-01-27 20:31:59 +0000

    Revert "Reader: retrieve user subscriptions using Redux" (#11011)

--- client/state/reader/follows/actions.js
+++ client/state/reader/follows/actions.js
@@ -6,14 +6,9 @@ import debugModule from 'debug';
 /**
  * Internal dependencies
  */
-import wpcom from 'lib/wp';
 import {
 	READER_FOLLOW,
-	READER_UNFOLLOW,
-	READER_FOLLOWS_RECEIVE,
-	READER_FOLLOWS_REQUEST,
-	READER_FOLLOWS_REQUEST_SUCCESS,
-	READER_FOLLOWS_REQUEST_FAILURE,
+	READER_UNFOLLOW
 } from 'state/action-types';
 
 /**
@@ -32,7 +27,7 @@ export function recordFollow( url ) {
 		debug( 'User followed ' + url );
 		dispatch( {
 			type: READER_FOLLOW,
-			payload: { url }
+			url
 		} );
 	};
 }
@@ -48,57 +43,7 @@ export function recordUnfollow( url ) {
 		debug( 'User unfollowed ' + url );
 		dispatch( {
 			type: READER_UNFOLLOW,
-			payload: { url }
+			url
 		} );
 	};
 }
-
-/**
- * Returns an action object to signal that followed sites have been received.
- *
- * @param  {Array}  follows Follows received
- * @return {Object} 		Action object
- */
-export function receiveFollows( follows ) {
-	return {
-		type: READER_FOLLOWS_RECEIVE,
-		payload: { follows }
-	};
-}
-
-/**
- * Triggers a network request to fetch user's followed sites.
- *
- * @param  {Integer} page Page number of results
- * @param  {Integer} limit Maximum number of results to return
- * @return {Function} Action thunk
- */
-export function requestFollows( page = 1, limit = 5 ) {
-	return ( dispatch ) => {
-		dispatch( {
-			type: READER_FOLLOWS_REQUEST
-		} );
-
-		const query = {
-			page,
-			number: limit
-		};
-
-		return wpcom.undocumented().readFollowingMine( query ).then( ( payload ) => {
-			dispatch( receiveFollows( payload.subscriptions ) );
-			dispatch( {
-				type: READER_FOLLOWS_REQUEST_SUCCESS,
-				payload
-			} );
-		},
-		( error ) => {
-			dispatch( {
-				type: READER_FOLLOWS_REQUEST_FAILURE,
-				payload: error,
-				error: true,
-			} );
-		}
-		);
-	};
-}
-
--- client/state/reader/follows/reducer.js
+++ client/state/reader/follows/reducer.js
@@ -2,18 +2,16 @@
  * External dependencies
  */
 import { combineReducers } from 'redux';
-import { merge, reduce } from 'lodash';
-
+import { union, without } from 'lodash';
 /**
  * Internal dependencies
  */
 import {
 	READER_FOLLOW,
 	READER_UNFOLLOW,
-	READER_FOLLOWS_RECEIVE,
+	SERIALIZE,
+	DESERIALIZE
 } from 'state/action-types';
-import { prepareComparableUrl } from './utils';
-import { createReducer } from 'state/utils';
 
 /**
  * Tracks all known list objects, indexed by list ID.
@@ -22,35 +20,21 @@ import { createReducer } from 'state/utils';
  * @param  {Object} action Action payload
  * @return {Array}        Updated state
  */
-export const items = createReducer( {}, {
-	[ READER_FOLLOW ]: ( state, action ) => {
-		const urlKey = prepareComparableUrl( action.payload.url );
-		return {
-			...state,
-			[ urlKey ]: merge( {}, state[ urlKey ], { is_following: true } ),
-		};
-	},
-	[ READER_UNFOLLOW ]: ( state, action ) => {
-		const urlKey = prepareComparableUrl( action.payload.url );
-		return {
-			...state,
-			[ urlKey ]: merge( {}, state[ urlKey ], { is_following: false } ),
-		};
-	},
-	[ READER_FOLLOWS_RECEIVE ]: ( state, action ) => {
-		const follows = action.payload.follows;
-		const keyedNewFollows = reduce( follows, ( hash, follow ) => {
-			const urlKey = prepareComparableUrl( follow.URL );
-			const newFollow = {
-				...follow,
-				is_following: true
-			};
-			hash[ urlKey ] = newFollow;
-			return hash;
-		}, {} );
-		return merge( {}, state, keyedNewFollows );
-	},
-} );
+export function items( state = [], action ) {
+	switch ( action.type ) {
+		case READER_FOLLOW:
+			return union( state, [ action.url ] );
+
+		case READER_UNFOLLOW:
+			return without( state, action.url );
+
+		case SERIALIZE:
+		case DESERIALIZE:
+			return [];
+	}
+
+	return state;
+}
 
 export default combineReducers( {
 	items
