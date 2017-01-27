baeddb8791dd7b04a2ba80f87a43b9dea331012f
Rick Banister <rick.banister@automattic.com>
2017-01-27 17:00:22 -0500

    Update related-content-preview.jsx
    Removed the colon after `Example:`

--- client/my-sites/site-settings/related-content-preview.jsx
+++ client/my-sites/site-settings/related-content-preview.jsx
@@ -45,7 +45,7 @@ const RelatedContentPreview = ( { showHeadline, showThumbnails, translate } ) =>
 
 	return (
 		<div id="settings-reading-relatedposts-preview">
-			<FormLabel>{ translate( 'Example:' ) }</FormLabel>
+			<FormLabel>{ translate( 'Example' ) }</FormLabel>
 
 			<div className="site-settings__related-posts">
 				{
