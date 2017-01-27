d318cef285db94bef0c64a7d912bd3766a8861bc
Stéphane Thomas <email@stephanethomas.com>
2017-01-27 14:43:46 +0100

    Domains: Standardize name of privacy protection in controller
    This renames an action mentioning private registration.

--- client/me/purchases/index.js
+++ client/me/purchases/index.js
@@ -64,7 +64,7 @@ export default function() {
 		paths.cancelPrivacyProtection(),
 		meController.sidebar,
 		controller.noSitesMessage,
-		controller.cancelPrivateRegistration
+		controller.cancelPrivacyProtection
 	);
 
 	page(
