1d47c78eb610dd4998160d4d1866c4a0171496b8
Ben Lowery <ben.lowery@automattic.com>
2017-01-30 11:42:21 -0500

    trial with lib/inflight. do not like.

--- client/state/data-layer/wpcom/reader/streams/index.js
+++ client/state/data-layer/wpcom/reader/streams/index.js
@@ -7,6 +7,7 @@ import { find } from 'lodash';
  */
 import wp from 'lib/wp';
 import warn from 'lib/warn';
+import { requestInflight, trackPromise } from 'lib/inflight';
 import {
 	READER_STREAMS_PAGE_REQUEST,
 	READER_STREAMS_PAGE_RECEIVE
@@ -24,36 +25,72 @@ const streamToPathMatchers = [
 	[ /^a8c$/, '1.2', '/read/a8c' ],
 ];
 
-function apiPathForStream( streamId ) {
+function apiForStream( streamId ) {
 	return find( streamToPathMatchers, ( matcher ) => matcher[ 0 ].test( streamId ) );
+}
+
+function keyForRequest( action ) {
+	const { streamId, query } = action;
+	const actionString = Object.keys( query )
+		.sort() // sort the keys to make the string deterministic. key ordering is not.
+		.reduce( ( memo, key ) => memo + `&${ key }=${ query[ key ] }`, '' );
+	return `reader-streams-${ streamId }-${ actionString }`;
 }
 
 export function interceptStreamPageRequest( store, action, next ) {
 	const { streamId } = action;
-	const matcher = apiPathForStream( streamId );
+	const api = apiForStream( streamId );
 
-	if ( ! matcher ) {
+	if ( ! api ) {
 		warn( `Unable to determine api path for ${ streamId }` );
 		next( action );
 		return;
 	}
 
-	const [ _, apiVersion, path ] = matcher; //eslint-disable-line no-unused-vars
+	// is this request already inflight?
+	const requestKey = keyForRequest( action );
+	if ( requestInflight( requestKey ) ) {
+		next( action );
+		return;
+	}
+
+	const [ _, apiVersion, path ] = api; //eslint-disable-line no-unused-vars
+
+	function transformResponse( response ) {
+		return response;
+	}
+
+	function dispatchReceive( response ) {
+		store.dispatch( {
+			...action,
+			type: READER_STREAMS_PAGE_RECEIVE,
+			payload: response,
+		} );
+	}
+
+	function dispatchError( err ) {
+		store.dispatch( {
+			...action,
+			type: READER_STREAMS_PAGE_RECEIVE,
+			payload: err,
+			error: true,
+		} );
+	}
 
 	const request = wp.req.get(
 		path,
 		{ apiVersion },
 		action.query
-	).then( ( response ) => {
-		store.dispatch( {
-			...action,
-			type: READER_STREAMS_PAGE_RECEIVE,
-			payload: response
-		} );
-	} );
+	);
+
+	const tracker = trackPromise( requestKey, request );
 
 	next( action );
-	return request;
+
+	return tracker
+		.then( transformResponse )
+		.then( dispatchReceive )
+		.catch( dispatchError );
 }
 
 export default {
--- client/state/data-layer/wpcom/reader/streams/test/index.js
+++ client/state/data-layer/wpcom/reader/streams/test/index.js
@@ -12,15 +12,15 @@ import useMockery from 'test/helpers/use-mockery';
 describe( 'streams', () => {
 	let interceptStreamPageRequest;
 	const warnSpy = spy();
-	const getStub = stub();
+	let getStub = stub();
 	const wpStub = {
 		req: {
 			get: getStub
 		}
 	};
-	const dispatchSpy = spy();
+	const dispatchStub = stub();
 	const storeStub = {
-		dispatch: dispatchSpy
+		dispatch: dispatchStub
 	};
 
 	useMockery( mockery => {
@@ -33,7 +33,7 @@ describe( 'streams', () => {
 	afterEach( () => {
 		warnSpy.reset();
 		getStub.reset();
-		dispatchSpy.reset();
+		dispatchStub.reset();
 	} );
 
 	it( 'should call next on an unknown stream id', () => {
@@ -65,14 +65,8 @@ describe( 'streams', () => {
 		getStub
 			.withArgs( '/read/following', { apiVersion: '1.2' }, { one: 'time' } )
 			.returns( Promise.resolve( fakeResponse ) );
-
-		const request = interceptStreamPageRequest( storeStub, action, nextSpy );
-
-		expect( getStub ).to.have.been.calledOnce;
-
-		return request.then( () => {
-			expect( dispatchSpy ).to.have.been.calledOnce;
-			expect( dispatchSpy ).to.have.been.calledWith( {
+		storeStub.dispatch = function( args ) {
+			expect( args ).to.eql( {
 				type: 'READER_STREAMS_PAGE_RECEIVE',
 				payload: fakeResponse,
 				query: {
@@ -80,6 +74,44 @@ describe( 'streams', () => {
 				},
 				streamId: 'following'
 			} );
-		} );
+		};
+
+		const promise = interceptStreamPageRequest( storeStub, action, nextSpy );
+
+		expect( getStub ).to.have.been.calledOnce;
+		expect( warnSpy ).to.have.not.been.called;
+		expect( nextSpy ).to.have.been.calledOnce;
+
+		promise.then(
+			( r ) => {
+				storeStub.dispatch = dispatchStub;
+				return r;
+			},
+			( err ) => {
+				storeStub.dispatch = dispatchStub;
+				return Promise.reject( err );
+			}
+		);
+		return promise;
+	} );
+
+	it( 'should prevent duplicate requests inflight', () => {
+		const action = {
+			streamId: 'following',
+			query: { page: 1 }
+		};
+		getStub
+			.withArgs( '/read/following', { apiVersion: '1.2' }, { page: 1 } )
+			.returns( Promise.resolve( { posts: [] } ) );
+
+		const nextSpy = spy();
+		interceptStreamPageRequest( storeStub, action, nextSpy );
+		const abortedNextSpy = spy();
+		getStub.reset();
+		dispatchStub.reset();
+		interceptStreamPageRequest( storeStub, action, abortedNextSpy );
+		expect( getStub ).to.have.not.been.called;
+		expect( abortedNextSpy ).to.have.been.called;
+		expect( dispatchStub ).to.have.not.been.called;
 	} );
 } );
