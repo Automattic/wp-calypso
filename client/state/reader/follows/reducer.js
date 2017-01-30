4615c85591bbdd15b966badab86375e3262bfc01
Chris Rosser <chris@bluefuton.com>
2017-01-30 10:44:04 +0000

    Reader: retrieve user subscriptions using Redux - attempt two (#11012)
    * Revert "Revert "Reader: retrieve user subscriptions using Redux""
    
    * Fix selector path in Cold Start

--- client/state/reader/follows/reducer.js
+++ client/state/reader/follows/reducer.js
@@ -2,16 +2,18 @@
  * External dependencies
  */
 import { combineReducers } from 'redux';
-import { union, without } from 'lodash';
+import { merge, reduce } from 'lodash';
+
 /**
  * Internal dependencies
  */
 import {
 	READER_FOLLOW,
 	READER_UNFOLLOW,
-	SERIALIZE,
-	DESERIALIZE
+	READER_FOLLOWS_RECEIVE,
 } from 'state/action-types';
+import { prepareComparableUrl } from './utils';
+import { createReducer } from 'state/utils';
 
 /**
  * Tracks all known list objects, indexed by list ID.
@@ -20,21 +22,35 @@ import {
  * @param  {Object} action Action payload
  * @return {Array}        Updated state
  */
-export function items( state = [], action ) {
-	switch ( action.type ) {
-		case READER_FOLLOW:
-			return union( state, [ action.url ] );
-
-		case READER_UNFOLLOW:
-			return without( state, action.url );
-
-		case SERIALIZE:
-		case DESERIALIZE:
-			return [];
-	}
-
-	return state;
-}
+export const items = createReducer( {}, {
+	[ READER_FOLLOW ]: ( state, action ) => {
+		const urlKey = prepareComparableUrl( action.payload.url );
+		return {
+			...state,
+			[ urlKey ]: merge( {}, state[ urlKey ], { is_following: true } ),
+		};
+	},
+	[ READER_UNFOLLOW ]: ( state, action ) => {
+		const urlKey = prepareComparableUrl( action.payload.url );
+		return {
+			...state,
+			[ urlKey ]: merge( {}, state[ urlKey ], { is_following: false } ),
+		};
+	},
+	[ READER_FOLLOWS_RECEIVE ]: ( state, action ) => {
+		const follows = action.payload.follows;
+		const keyedNewFollows = reduce( follows, ( hash, follow ) => {
+			const urlKey = prepareComparableUrl( follow.URL );
+			const newFollow = {
+				...follow,
+				is_following: true
+			};
+			hash[ urlKey ] = newFollow;
+			return hash;
+		}, {} );
+		return merge( {}, state, keyedNewFollows );
+	},
+} );
 
 export default combineReducers( {
 	items
