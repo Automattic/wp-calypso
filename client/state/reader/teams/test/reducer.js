9bb315f3d20ef7d793e28180cc4b669ce1445b7d
Jake F - @samouri <samouri@users.noreply.github.com>
2017-01-31 15:47:29 -0500

    added in tests for deserialize

--- client/state/reader/teams/test/reducer.js
+++ client/state/reader/teams/test/reducer.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 
 /**
  * Internal dependencies
@@ -10,10 +10,13 @@ import { items, isRequesting } from '../reducer';
 import {
 	READER_TEAMS_REQUEST,
 	READER_TEAMS_RECEIVE,
+	DESERIALIZE,
 } from 'state/action-types';
 
 const TEAM1 = { slug: 'team one slug', title: 'team one title' };
 const TEAM2 = { slug: 'team two slug', title: 'team two title' };
+const validState = { teams: [ TEAM1 ], };
+const invalidState = { teams: [ 1, 5 ], fish: 'tacos' };
 
 describe( 'reducer', ( ) => {
 	describe( 'items', () => {
@@ -43,6 +46,19 @@ describe( 'reducer', ( ) => {
 			).to.deep.equal( [ TEAM1, TEAM2 ] );
 		} );
 
+		it( 'deserialize: should succeed with good data', () => {
+			assert.deepEqual( validState, items( validState, { type: DESERIALIZE } ) );
+		} );
+
+		it( 'deserialize: should ignore bad data', () => {
+			let state;
+			try {
+				state = items( invalidState, { type: DESERIALIZE } );
+				assert.fail();
+			} catch ( err ) {
+				assert.deepEqual( [], state );
+			}
+		} );
 	} );
 
 	describe( 'isRequesting', () => {
