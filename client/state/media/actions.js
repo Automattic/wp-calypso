79fe327ce3adce596c682e7c7fd7ad9bcbb9a452
Andrew Duthie <andrew@andrewduthie.com>
2017-01-31 09:00:07 -0500

    Media: Add clarifying note for future media network behavior

--- client/state/media/actions.js
+++ client/state/media/actions.js
@@ -85,6 +85,10 @@ export function failMediaRequest( siteId, query ) {
  * Returns an action object used in signalling that media item(s) for the site
  * are to be deleted.
  *
+ * TODO: When network layer behavior is attached to this action type, remember
+ * to ignore media IDs for "transient" items (upload in progress) by validating
+ * numeric ID.
+ *
  * @param  {Number}         siteId   Site ID
  * @param  {(Array|Number)} mediaIds ID(s) of media to be deleted
  * @return {Object}                  Action object
