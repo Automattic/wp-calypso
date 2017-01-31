cde58cff722bc36eca5304bae9832987150ae75f
Marin Atanasov <tyxla@users.noreply.github.com>
2017-01-31 10:08:57 +0200

    Export: ES6ify SiteSettingsExport and remove site prop (#11040)

--- client/my-sites/site-settings/section-export.jsx
+++ client/my-sites/site-settings/section-export.jsx
@@ -1,30 +1,43 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
-import i18n from 'i18n-calypso';
+import React from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import EmptyContent from 'components/empty-content';
 import ExporterContainer from 'my-sites/exporter';
+import { getSelectedSite, getSelectedSiteId } from 'state/ui/selectors';
+import { isJetpackSite } from 'state/sites/selectors';
 
-export default class SiteSettingsExport extends Component {
-	render() {
-		if ( this.props.site.jetpack ) {
-			return (
-				<EmptyContent
-					illustration="/calypso/images/drake/drake-jetpack.svg"
-					title={ i18n.translate( 'Want to export your site?' ) }
-					line={ i18n.translate( `Visit your site's wp-admin for all your import and export needs.` ) }
-					action={ i18n.translate( 'Export %(siteTitle)s', { args: { siteTitle: this.props.site.title } } ) }
-					actionURL={ this.props.site.options.admin_url + 'export.php' }
-					actionTarget="_blank"
-				/>
-			);
-		}
-
-		return <ExporterContainer site={ this.props.site } />;
+const SiteSettingsExport = ( { isJetpack, site, translate } ) => {
+	if ( isJetpack ) {
+		return (
+			<EmptyContent
+				illustration="/calypso/images/drake/drake-jetpack.svg"
+				title={ translate( 'Want to export your site?' ) }
+				line={ translate( 'Visit your site\'s wp-admin for all your import and export needs.' ) }
+				action={ translate( 'Export %(siteTitle)s', { args: { siteTitle: site.title } } ) }
+				actionURL={ site.options.admin_url + 'export.php' }
+				actionTarget="_blank"
+			/>
+		);
 	}
-}
+
+	return <ExporterContainer />;
+};
+
+export default connect(
+	( state ) => {
+		const selectedSiteId = getSelectedSiteId( state );
+		const site = getSelectedSite( state );
+
+		return {
+			isJetpack: selectedSiteId && isJetpackSite( state, selectedSiteId ),
+			site,
+		};
+	}
+)( localize( SiteSettingsExport ) );
