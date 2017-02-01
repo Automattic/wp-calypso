5c52eef7ff53e573a9a88db61afe8953783a337a
Ben Lowery <ben.lowery@automattic.com>
2017-02-01 14:25:16 -0500

    move to read from reader

--- client/state/data-layer/wpcom/index.js
+++ client/state/data-layer/wpcom/index.js
@@ -3,16 +3,14 @@
  */
 import { mergeHandlers } from 'state/data-layer/utils';
 import plans from './plans';
-import reader from './reader';
-import sites from './sites';
 import read from './read';
+import sites from './sites';
 import timezones from './timezones';
 
 export const handlers = mergeHandlers(
 	plans,
-	reader,
-	sites,
 	read,
+	sites,
 	timezones,
 );
 
