d4d3974a60d679195d52b1b4a0a7b40c6e604cb8
Yury Michurin <yury@automattic.com>
2017-02-01 14:10:32 +0200

    Render feature gate instead of redirect
    For domain only site, certain features are disabled
    instead of redirecting we should render a page
    that explains that.

--- client/my-sites/controller.js
+++ client/my-sites/controller.js
@@ -113,6 +113,26 @@ function renderNoVisibleSites( context ) {
 	);
 }
 
+function renderSelectedSiteIsDomainOnly( reactContext, selectedSite ) {
+	const EmptyContentComponent = require( 'components/empty-content' );
+	const { store: reduxStore } = reactContext;
+
+	removeSidebar( reactContext );
+
+	renderWithReduxStore(
+		React.createElement( EmptyContentComponent, {
+			title: i18n.translate( 'This feature is not available for domains' ),
+			line: i18n.translate( 'To use this feature you need to create a site' ),
+			action: i18n.translate( 'Create New Site' ),
+			actionURL: '//dashboard.wordpress.com/wp-admin/index.php?page=my-blogs',
+			secondaryAction: i18n.translate( 'Manage Domain' ),
+			secondaryActionURL: domainManagementList( selectedSite.slug )
+		} ),
+		document.getElementById( 'primary' ),
+		reduxStore
+	);
+}
+
 function isPathAllowedForDomainOnlySite( pathname, domainName ) {
 	const urlPrefixesWhiteListForDomainOnlySite = [
 		domainManagementList( domainName ),
@@ -137,7 +157,7 @@ function onSelectedSiteAvailable( context ) {
 
 	if ( isDomainOnlySite( state, selectedSite.ID ) &&
 		! isPathAllowedForDomainOnlySite( context.pathname, selectedSite.slug ) ) {
-		page.redirect( domainManagementList( selectedSite.slug ) );
+		renderSelectedSiteIsDomainOnly( context, selectedSite );
 		return false;
 	}
 
