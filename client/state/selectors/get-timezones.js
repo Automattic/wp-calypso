a73f627c5c53304e07a311a759a60f2dbe024c1d
retrofox <rdsuarez@gmail.com>
2017-02-02 19:23:22 -0300

    state:selectors: add `getTimezones()`

---
+++ client/state/selectors/get-timezones.js
@@ -0,0 +1,38 @@
+/**
+ * External dependencies
+ */
+import {
+	get,
+	map,
+	toPairs,
+} from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { getTimezonesLabel } from 'state/selectors/';
+
+/**
+ * Return all timezones ordered by arrays with
+ * the following shape:
+ * [
+ *   [ <continent>, [
+ *     [ <timezone-value>, <timezone-label> ],
+ *   ] ]
+ *   ...
+ * ]
+ *
+ * This structure facilitates the creation of a select element.
+ *
+ * @param  {Object} state - Global state tree
+ * @return {Array} Timezones arrays
+ */
+export default function getTimezones( state ) {
+	const continents = toPairs( get( state, 'timezones.byContinents', null ) );
+
+	if ( ! continents ) {
+		return null;
+	}
+
+	return map( continents, zones => [ zones[ 0 ], map( zones[ 1 ], value => [ value, getTimezonesLabel( state, value ) ] ) ] );
+}
