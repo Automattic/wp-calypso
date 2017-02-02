bce655b585c3831c885d7d9d824449d9f6ffe078
retrofox <rdsuarez@gmail.com>
2017-02-02 12:13:53 -0300

    state:selectors: add `getTimezonesLabel()`

--- client/state/selectors/index.js
+++ client/state/selectors/index.js
@@ -42,6 +42,7 @@ export getSharingButtons from './get-sharing-buttons';
 export getSiteIconId from './get-site-icon-id';
 export getSiteIconUrl from './get-site-icon-url';
 export getTimezonesByContinent from './get-timezones-by-continent';
+export getTimezonesLabel from './get-timezones-label';
 export getTimezonesLabels from './get-timezones-labels';
 export getUpcomingBillingTransactions from './get-upcoming-billing-transactions';
 export hasBrokenSiteUserConnection from './has-broken-site-user-connection';
