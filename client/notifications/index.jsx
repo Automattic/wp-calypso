f0442da89e6650d4893b7ce630a945dba35e908c
Dennis Snell <dennis.snell@automattic.com>
2017-02-02 15:45:42 -0700

    Update notes client to latest production build
    @see r150343-wpcom

--- client/notifications/index.jsx
+++ client/notifications/index.jsx
@@ -28,7 +28,7 @@ import userLib from 'lib/user';
 /**
  * Module variables
  */
-const NOTIFICATIONS_CLIENT_VERSION = 'r149819-wpcom-1-g68e4bcd';
+const NOTIFICATIONS_CLIENT_VERSION = 'beta-r149829-wpcom-0-g68e4bcd-dirty';
 
 const user = userLib();
 const widgetDomain = 'https://widgets.wp.com';
