5af80b0c5a61b9880c824b039613a08d1222ee02
Marin Atanasov <tyxla@abv.bg>
2017-02-03 17:05:09 +0200

    State: Implement sendingReceiptEmail reducer

--- client/state/billing-transactions/reducer.js
+++ client/state/billing-transactions/reducer.js
@@ -7,6 +7,9 @@ import { combineReducers } from 'redux';
  * Internal dependencies
  */
 import {
+	BILLING_RECEIPT_EMAIL_SEND,
+	BILLING_RECEIPT_EMAIL_SEND_FAILURE,
+	BILLING_RECEIPT_EMAIL_SEND_SUCCESS,
 	BILLING_TRANSACTIONS_RECEIVE,
 	BILLING_TRANSACTIONS_REQUEST,
 	BILLING_TRANSACTIONS_REQUEST_FAILURE,
@@ -41,7 +44,22 @@ export const requesting = createReducer( false, {
 	[ BILLING_TRANSACTIONS_REQUEST_SUCCESS ]: () => false,
 } );
 
+/**
+ * Returns the updated sending email requests state after an action has been dispatched.
+ * The state contains whether a request for sending a receipt email is in progress.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action payload
+ * @return {Object}        Updated state
+ */
+export const sendingReceiptEmail = createReducer( {}, {
+	[ BILLING_RECEIPT_EMAIL_SEND ]: ( state, { receiptId } ) => ( { ...state, [ receiptId ]: true } ),
+	[ BILLING_RECEIPT_EMAIL_SEND_FAILURE ]: ( state, { receiptId } ) => ( { ...state, [ receiptId ]: false } ),
+	[ BILLING_RECEIPT_EMAIL_SEND_SUCCESS ]: ( state, { receiptId } ) => ( { ...state, [ receiptId ]: false } ),
+} );
+
 export default combineReducers( {
 	items,
 	requesting,
+	sendingReceiptEmail,
 } );
