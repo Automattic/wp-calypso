b235e8a0222698b811595f071ba678e0bd42321f
Dennis Snell <dennis.snell@automattic.com>
2017-02-06 15:43:59 -0700

    Introduce HTTP pipeline for data layer
    Introduce a system for automatically augmenting and optimizing
    network-layer operations from inside Calypso.
    
    The goal of this new system is to live entirely within the language of
    HTTP and perform HTTP-specific transformations on new requests and when
    requests return or fail.

---
+++ client/state/data-layer/wpcom-http/pipeline/remove-duplicate-gets/index.js
@@ -0,0 +1,6 @@
+/**
+ * Prevent sending multiple identical GET requests
+ * while one is still transiting over the network
+ *
+ * @module state/data-layer/wpcom-http/optimizations/remove-duplicate-gets
+ */
