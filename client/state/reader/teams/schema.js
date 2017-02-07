13e025a8d3a9efd87ef1d49a137cdb90f6b402fe
Kerry Liu <gwwar@users.noreply.github.com>
2017-02-07 10:01:52 -0800

    State: fix schema for reader teams (#11219)

--- client/state/reader/teams/schema.js
+++ client/state/reader/teams/schema.js
@@ -1,16 +1,10 @@
 export const itemsSchema = {
-	type: 'object',
-	properties: {
-		number: { type: 'number' },
-		teams: {
-			type: 'array',
-			items: {
-				type: 'object',
-				properties: {
-					title: { type: 'string' },
-					slug: { type: 'string' },
-				}
-			}
+	type: 'array',
+	items: {
+		type: 'object',
+		properties: {
+			title: { type: 'string' },
+			slug: { type: 'string' },
 		}
 	}
 };
