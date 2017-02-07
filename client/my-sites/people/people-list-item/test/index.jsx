c79ffdd23a19ce633204e9a27264250082c239aa
Mark Ryall <mark@ryall.name>
2017-02-07 12:44:15 +1000

    Remove redundant mocks from PeopleListItem test.

--- client/my-sites/people/people-list-item/test/index.jsx
+++ client/my-sites/people/people-list-item/test/index.jsx
@@ -5,33 +5,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import sinon from 'sinon';
 
 /**
  * Internal dependencies
  */
-import useFakeDom from 'test/helpers/use-fake-dom';
-import useMockery from 'test/helpers/use-mockery';
+import { PeopleListItem } from '..';
+import CompactCard from 'components/card/compact';
 
 describe( 'PeopleListItem', function() {
-	let configMock,
-		PeopleListItem,
-		CompactCard;
-
-	useFakeDom();
-
-	useMockery( mockery => {
-		configMock = sinon.stub();
-		configMock.isEnabled = sinon.stub();
-		mockery.registerMock( 'config', configMock );
-		mockery.registerMock( 'lib/analytics', {} );
-	} );
-
-	before( function() {
-		CompactCard = require( 'components/card/compact' );
-		PeopleListItem = require( '..' ).PeopleListItem;
-	} );
-
 	it( 'renders CompactCard', function() {
 		const wrapper = shallow( <PeopleListItem /> );
 
