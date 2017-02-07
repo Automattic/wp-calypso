de0a7373079039fc84c892a777f897162c3d591b
Marin Atanasov <tyxla@abv.bg>
2017-02-07 14:57:03 +0200

    Site Settings: Move default post format in a separate component

--- client/my-sites/site-settings/composing/index.jsx
+++ client/my-sites/site-settings/composing/index.jsx
@@ -2,15 +2,12 @@
  * External dependencies
  */
 import React, { PropTypes } from 'react';
-import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import Card from 'components/card';
-import FormFieldset from 'components/forms/form-fieldset';
-import FormLabel from 'components/forms/form-label';
-import FormSelect from 'components/forms/form-select';
+import DefaultPostFormat from './default-post-format';
 import MarkdownWpcom from './markdown-wpcom';
 
 const Composing = ( {
@@ -19,35 +16,17 @@ const Composing = ( {
 	onChangeField,
 	eventTracker,
 	isRequestingSettings,
-	isSavingSettings,
-	translate
+	isSavingSettings
 } ) => {
 	return (
 		<Card className="site-settings">
-			<FormFieldset>
-				<FormLabel htmlFor="default_post_format">
-					{ translate( 'Default Post Format' ) }
-				</FormLabel>
-				<FormSelect
-					name="default_post_format"
-					id="default_post_format"
-					value={ fields.default_post_format }
-					onChange={ onChangeField( 'default_post_format' ) }
-					disabled={ isRequestingSettings || isSavingSettings }
-					onClick={ eventTracker( 'Selected Default Post Format' ) }
-				>
-					<option value="0">{ translate( 'Standard', { context: 'Post format' } ) }</option>
-					<option value="aside">{ translate( 'Aside', { context: 'Post format' } ) }</option>
-					<option value="chat">{ translate( 'Chat', { context: 'Post format' } ) }</option>
-					<option value="gallery">{ translate( 'Gallery', { context: 'Post format' } ) }</option>
-					<option value="link">{ translate( 'Link', { context: 'Post format' } ) }</option>
-					<option value="image">{ translate( 'Image', { context: 'Post format' } ) }</option>
-					<option value="quote">{ translate( 'Quote', { context: 'Post format' } ) }</option>
-					<option value="status">{ translate( 'Status', { context: 'Post format' } ) }</option>
-					<option value="video">{ translate( 'Video', { context: 'Post format' } ) }</option>
-					<option value="audio">{ translate( 'Audio', { context: 'Post format' } ) }</option>
-				</FormSelect>
-			</FormFieldset>
+			<DefaultPostFormat
+				onChangeField={ onChangeField }
+				eventTracker={ eventTracker }
+				isSavingSettings={ isSavingSettings }
+				isRequestingSettings={ isRequestingSettings }
+				fields={ fields }
+			/>
 
 			{
 				fields.markdown_supported &&
@@ -77,4 +56,4 @@ Composing.propTypes = {
 	fields: PropTypes.object,
 };
 
-export default localize( Composing );
+export default Composing;
