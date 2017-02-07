96d405b9c18c899b1a68d9005b76a45ca04e4b73
Yury Michurin <yury@automattic.com>
2017-02-07 13:24:51 +0200

    Fix wrong parameters to render

--- client/my-sites/controller.js
+++ client/my-sites/controller.js
@@ -123,7 +123,7 @@ function renderSelectedSiteIsDomainOnly( reactContext, selectedSite ) {
 			title: i18n.translate( 'This feature is not available for domains' ),
 			line: i18n.translate( 'To use this feature you need to create a site' ),
 			action: i18n.translate( 'Create New Site' ),
-			actionURL: `/start/site-selected/?siteSlug=${ encodeURIComponent( selectedSite.slug ) }`,
+			actionURL: `/start/site-selected/?siteSlug=${ encodeURIComponent( selectedSite.slug ) }&siteId=${ encodeURIComponent( selectedSite.ID ) }`,
 			secondaryAction: i18n.translate( 'Manage Domain' ),
 			secondaryActionURL: domainManagementList( selectedSite.slug )
 		} ),
