476b68023b0970c78165737ac5c556766372a8bd
Konstantin Obenland <obenland@gmx.de>
2017-02-08 10:07:16 -0800

    Make Connections a functional component

--- client/my-sites/sharing/connections/connections.jsx
+++ client/my-sites/sharing/connections/connections.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { identity } from 'lodash';
 import { localize } from 'i18n-calypso';
 
@@ -11,33 +11,28 @@ import { localize } from 'i18n-calypso';
 import QueryKeyringServices from 'components/data/query-keyring-services';
 import SharingServicesGroup from './services-group';
 
-class SharingConnections extends Component {
-	static propTypes = {
-		connections: PropTypes.object,
-		translate: PropTypes.func,
-	};
+const SharingConnections = ( { connections, translate } ) => (
+	<div className="sharing-settings sharing-connections">
+		<QueryKeyringServices />
+		<SharingServicesGroup
+			type="publicize"
+			title={ translate( 'Publicize Your Posts' ) }
+			connections={ connections } />
+		<SharingServicesGroup
+			type="other"
+			title={ translate( 'Other Connections' ) }
+			connections={ connections } />
+	</div>
+);
 
-	static defaultProps = {
-		connections: Object.freeze( {} ),
-		translate: identity,
-	};
+SharingConnections.propTypes = {
+	connections: PropTypes.object,
+	translate: PropTypes.func,
+};
 
-	render() {
-		return (
-			<div className="sharing-settings sharing-connections">
-				<QueryKeyringServices />
-				<SharingServicesGroup
-					type="publicize"
-					title={ this.props.translate( 'Publicize Your Posts' ) }
-					connections={ this.props.connections } />
-				<SharingServicesGroup
-					type="other"
-					title={ this.props.translate( 'Other Connections' ) }
-					description={ this.props.translate( 'Connect any of these additional services to further enhance your site.' ) }
-					connections={ this.props.connections } />
-			</div>
-		);
-	}
-}
+SharingConnections.defaultProps = {
+	connections: {},
+	translate: identity,
+};
 
 export default localize( SharingConnections );
