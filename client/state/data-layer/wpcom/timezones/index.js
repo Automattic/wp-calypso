fb7063bb7b6f6132ff2d7bd4f64e7cd1ef3c00ff
Kerry Liu <gwwar@users.noreply.github.com>
2017-02-08 11:42:16 -0800

    Revert "Component: add `query-timezones` data component" (#11267)

--- client/state/data-layer/wpcom/timezones/index.js
+++ client/state/data-layer/wpcom/timezones/index.js
@@ -10,8 +10,7 @@ import {
 /**
  * Internal dependencies
  */
-import { http } from 'state/data-layer/wpcom-http/actions';
-import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import wpcom from 'lib/wp';
 
 import { TIMEZONES_REQUEST } from 'state/action-types';
 
@@ -26,7 +25,7 @@ import {
  * keys are the values and whose values are the labels.
  *
  * @example
- * timezonePairsToMap( [ { value: 'foo', label: 'bar' }, { value: 'biz', label: 'bat' } ] )
+ * valueLabelToObject( [ { value: 'foo', label: 'bar' }, { value: 'biz', label: 'bat' } ] )
  * // returns { foo: 'bar', biz: 'bat' }
  *
  * @param {ValueLabelRecord[]} pairs - timezone values and display labels
@@ -48,58 +47,20 @@ export const fromApi = ( { manual_utc_offsets, timezones, timezones_by_continent
 	byContinents: mapValues( timezones_by_continent, zones => map( zones, ( { value } ) => ( value ) ) )
 } );
 
-/**
- * Dispaches a request to fetch timezones data from WordPress WP REST API
- * https://public-api.wordpress.com/wpcom/v2/timezones
- *
- * @param {Function} dispatch - Redux dispatcher
- * @param {Object} action - Redux action
- * @param {Function} next - data-layer-bypassing dispatcher
- * @returns {Object} original action
+/*
+ * Start a request to WordPress.com server to get the timezones data
  */
-export const requestTimezones = ( { dispatch }, action, next ) => {
-	dispatch( http( {
-		apiNamespace: 'wpcom/v2',
-		method: 'GET',
-		path: '/timezones',
-		onSuccess: action,
-		onFailure: action,
-	} ) );
-
-	return next( action );
-};
-
-/**
- * Dispatches returned timezones data
- *
- * @param {Function} dispatch - Redux dispatcher
- * @param {Object} action - Redux action
- * @param {Function} next - dispatches to next middleware in chain
- * @param {Object} timezones - raw data from timezones endpoint response
- */
-export const receiveTimezones = ( { dispatch }, action, next, timezones ) => {
-	dispatch( timezonesRequestSuccess() );
-	dispatch( timezonesReceive( fromApi( timezones ) ) );
-};
-
-/**
- * Dispatches returned error from timezones request
- *
- * @param {Function} dispatch - Redux dispatcher
- * @param {Object} action - Redux action
- * @param {Function} next - dispatches to next middleware in chain
- * @param {Object} rawError - raw error from HTTP request
- */
-export const receiveError = ( { dispatch }, action, next, rawError ) => {
-	const error = rawError instanceof Error
-		? rawError.message
-		: rawError;
-
-	dispatch( timezonesRequestFailure( error ) );
-};
-
-export const dispatchTimezonesRequest = dispatchRequest( requestTimezones, receiveTimezones, receiveError );
+export const fetchTimezones = ( { dispatch } ) => (
+	wpcom.req.get( '/timezones', { apiNamespace: 'wpcom/v2' } )
+		.then( data => {
+			dispatch( timezonesRequestSuccess() );
+			dispatch( timezonesReceive( fromApi( data ) ) );
+		} )
+		.catch( error => {
+			dispatch( timezonesRequestFailure( error ) );
+		} )
+);
 
 export default {
-	[ TIMEZONES_REQUEST ]: [ dispatchTimezonesRequest ],
+	[ TIMEZONES_REQUEST ]: [ fetchTimezones ],
 };
