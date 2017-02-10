0bd9b5b2367e70aaf843234aadef51fd8efda85e
retrofox <rdsuarez@gmail.com>
2017-02-10 15:43:10 -0300

    state: add `requesting` into timezones reducer

--- client/state/selectors/is-requesting-timezones.js
+++ client/state/selectors/is-requesting-timezones.js
@@ -1,10 +1,14 @@
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
 
 /**
  * Returns true if we are requesting the timezones
  *
- * @param  {Object}  state  Global state tree
+ * @param {Object} state - Global state tree
  * @return {Boolean} - Whether timezones is being requested
  */
 export default function isRequestingTimezones( state ) {
-	return state.timezones.requesting;
+	return get( state, 'timezones.isRequesting', false );
 }
