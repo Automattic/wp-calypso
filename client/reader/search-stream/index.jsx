bdea0054a39c02f6fc3a153ae7b67c7b2811e8af
Jake F - @samouri <samouri@users.noreply.github.com>
2017-02-10 16:25:56 -1000

    empty should go through

--- client/reader/search-stream/index.jsx
+++ client/reader/search-stream/index.jsx
@@ -142,7 +142,12 @@ class SearchStream extends Component {
 	updateQuery = ( newValue ) => {
 		this.scrollToTop();
 		const trimmedValue = trim( newValue ).substring( 0, 1024 );
-		if ( trimmedValue !== '' && trimmedValue.length > 1 && trimmedValue !== this.props.query ) {
+		if ( ( trimmedValue !== '' &&
+				trimmedValue.length > 1 &&
+				trimmedValue !== this.props.query
+			) ||
+			newValue === ''
+		) {
 			this.props.onQueryChange( newValue );
 		}
 	}
