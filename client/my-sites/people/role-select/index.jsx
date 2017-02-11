27cea46bfe63342593b6d865a0b39b1475145a6c
Mark Ryall <mark@ryall.name>
2017-02-11 21:02:22 +1000

    Export RoleSelect component as part of definition.

--- client/my-sites/people/role-select/index.jsx
+++ client/my-sites/people/role-select/index.jsx
@@ -29,7 +29,7 @@ const getWpcomFollowerRole = ( { site, translate } ) => {
 	};
 };
 
-const RoleSelect = ( props ) => {
+export const RoleSelect = ( props ) => {
 	let { siteRoles } = props;
 	const { site, includeFollower, siteId, id, explanation, translate } = props;
 	const omitProps = [
@@ -76,7 +76,6 @@ const RoleSelect = ( props ) => {
 	);
 };
 
-export { RoleSelect };
 export default connect(
 	( state, ownProps ) => ( {
 		site: getSite( state, ownProps.siteId ),
