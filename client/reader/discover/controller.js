10e4fcfd2ad934477af579ebc48c7ffd72f437c0
Ben Lowery <blowery@users.noreply.github.com>
2017-02-10 14:57:54 -1000

    Reader: Async load most streams and following management (#11338)
    * Reader: Async load most streams and following management
    
    * render with redux store

--- client/reader/discover/controller.js
+++ client/reader/discover/controller.js
@@ -12,13 +12,13 @@ import feedStreamFactory from 'lib/feed-stream-store';
 import { recordTrack } from 'reader/stats';
 import { ensureStoreLoading, trackPageLoad, trackUpdatesLoaded, trackScrollPage } from 'reader/controller-helper';
 import { renderWithReduxStore } from 'lib/react-helpers';
+import AsyncLoad from 'components/async-load';
 
 const ANALYTICS_PAGE_TITLE = 'Reader';
 
 export default {
 	discover( context ) {
 		const blogId = config( 'discover_blog_id' ),
-			SiteStream = require( 'reader/site-stream' ),
 			basePath = route.sectionify( context.path ),
 			fullAnalyticsPageTitle = ANALYTICS_PAGE_TITLE + ' > Site > ' + blogId,
 			feedStore = feedStreamFactory( 'site:' + blogId ),
@@ -30,25 +30,27 @@ export default {
 		recordTrack( 'calypso_reader_discover_viewed' );
 
 		renderWithReduxStore(
-			React.createElement( SiteStream, {
-				key: 'site-' + blogId,
-				postsStore: feedStore,
-				siteId: +blogId,
-				title: 'Discover',
-				trackScrollPage: trackScrollPage.bind(
-					null,
-					basePath,
-					fullAnalyticsPageTitle,
-					ANALYTICS_PAGE_TITLE,
-					mcKey
-				),
-				onUpdatesShown: trackUpdatesLoaded.bind( null, mcKey ),
-				suppressSiteNameLink: true,
-				showPrimaryFollowButtonOnCards: false,
-				isDiscoverStream: true,
-				showBack: false,
-				className: 'is-discover-stream is-site-stream',
-			} ),
+			<AsyncLoad
+				require="reader/site-stream"
+				key={ 'site-' + blogId }
+				postsStore={ feedStore }
+				siteId={ +blogId }
+				title="Discover"
+				trackScrollPage={ trackScrollPage.bind(
+						null,
+						basePath,
+						fullAnalyticsPageTitle,
+						ANALYTICS_PAGE_TITLE,
+						mcKey
+					)
+				}
+				onUpdatesShown={ trackUpdatesLoaded.bind( null, mcKey ) }
+				suppressSiteNameLink={ true }
+				showPrimaryFollowButtonOnCards={ false }
+				isDiscoverStream={ true }
+				showBack={ false }
+				className="is-discover-stream is-site-stream"
+			/>,
 			document.getElementById( 'primary' ),
 			context.store
 		);
