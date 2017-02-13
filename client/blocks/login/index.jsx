c7fd769e356f6bc994e2c5a829c6e6e50d70df47
johnHackworth <javier.alvarez.lop@gmail.com>
2017-02-13 10:59:48 +0100

    Login: styles

--- client/blocks/login/index.jsx
+++ client/blocks/login/index.jsx
@@ -9,14 +9,33 @@ import React from 'react';
  */
 //import Button from 'components/button';
 import Card from 'components/card';
+import FormTextInput from 'components/forms/form-text-input';
 
-const Login = () => {
+const Login = ( { title, children } ) => {
 	return (
 		<Card className="login">
-			<input></input>
-			<input></input>
+			<div className="login__header">
+				<div className="login__header-title">{ title }</div>
+			</div>
+			{ children }
+			<div className="login__form">
+				<label className="login__form-username">
+					Username or email <FormTextInput className="login__form-username-input" />
+				</label>
+				<label className="login__form-username">
+					Password <input className="login__form-username-password" type="password" />
+				</label>
+			</div>
 		</Card>
 	);
+};
+
+Login.propTypes = {
+	title: React.PropTypes.string
+};
+
+Login.defaultProps = {
+	title: false
 };
 
 export default Login;
