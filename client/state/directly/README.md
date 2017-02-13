c3ed8bbde7f750c9f6461bef51224648976b2d1d
Matthew Wondra <mattwondra@Matthews-MacBook-Pro.local>
2017-02-13 16:33:55 -0600

    Move Directly middleware into action creators.

--- client/state/directly/README.md
+++ client/state/directly/README.md
@@ -3,18 +3,12 @@ Directly state
 
 Provides a Redux interface to the imperative [Directly API](../../api/directly).
 
-## Actions
-These mirror the `api/directly` module very closely. The following actions are available:
+## Functions
+These execute the exported functions from `api/directly` and dispatch related Actions:
 
 #### `initialize( config )`
-Initialize the library with the given configuration options. See [the `api/directly`
+Initializes the library with the given configuration options. See [the `api/directly`
 README](../../api/directly) for config options.
 
 #### `askQuestion( { questionText, name, email } )`
-Open the Directly widget and ask the given question.
-
-#### `scriptLoadFailure( error )`
-Indicates that there was a problem loading the Directly scripts.
-
-## Middleware
-The middleware watches for Directly action types and engages the Directly API correspondingly.
+Opens the Directly widget and asks the given question.
--- client/state/directly/actions.js
+++ client/state/directly/actions.js
@@ -2,25 +2,51 @@
  * Internal dependencies
  */
 import {
-	DIRECTLY_ASK_QUESTION,
-	DIRECTLY_INITIALIZE,
-	DIRECTLY_SCRIPT_LOAD_FAILURE
+	initialize as initializeDirectly,
+	askQuestion as askDirectlyQuestion,
+	maximize as maximizeDirectly,
+	minimize as minimizeDirectly,
+	openAskForm as openDirectlyAskForm,
+} from 'lib/directly';
+import {
+	DIRECTLY_ASKING_QUESTION,
+	DIRECTLY_INITIALIZING,
+	DIRECTLY_INITIALIZED,
+	DIRECTLY_INITIALIZATION_ERROR,
+	DIRECTLY_MAXIMIZING,
+	DIRECTLY_MINIMIZING,
+	DIRECTLY_OPENING_ASK_FORM,
 } from 'state/action-types';
 
-export const askQuestion = ( { questionText, name, email } ) => ( {
-	type: DIRECTLY_ASK_QUESTION,
-	questionText,
-	name,
-	email
-} );
+export const askQuestion = ( { questionText, name, email } ) => ( dispatch ) => {
+	dispatch( { type: DIRECTLY_ASKING_QUESTION, questionText, name, email } );
+	askDirectlyQuestion( { questionText, name, email } );
+};
 
 // See README for list of options
-export const initialize = ( config = {} ) => ( {
-	type: DIRECTLY_INITIALIZE,
-	config: config
-} );
+export const initialize = ( config = {} ) => ( dispatch ) => {
+	dispatch( { type: DIRECTLY_INITIALIZING, config } );
+	initializeDirectly( config, ( error ) => {
+		if ( error ) {
+			// TODO: Do something if there's an error
+			dispatch( { type: DIRECTLY_INITIALIZATION_ERROR, error } );
+		} else {
+			dispatch( { type: DIRECTLY_INITIALIZED } );
+		}
+	} );
+};
 
-export const scriptLoadFailure = ( error ) => ( {
-	type: DIRECTLY_SCRIPT_LOAD_FAILURE,
-	error
-} );
+export const maximize = () => ( dispatch ) => {
+	dispatch( { type: DIRECTLY_MAXIMIZING } );
+	maximizeDirectly();
+};
+
+export const minimize = () => ( dispatch ) => {
+	dispatch( { type: DIRECTLY_MINIMIZING } );
+	minimizeDirectly();
+};
+
+export const openAskForm = () => ( dispatch ) => {
+	dispatch( { type: DIRECTLY_OPENING_ASK_FORM } );
+	openDirectlyAskForm();
+};
