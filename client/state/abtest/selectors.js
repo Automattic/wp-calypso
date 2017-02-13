8c50233cb233147b8361ef927780104be8bf8ee3
Mark Ryall <mark@ryall.name>
2017-02-13 21:38:57 +1000

    Introduce abtest selector for free to paid upsell eligibility.

---
+++ client/state/abtest/selectors.js
@@ -0,0 +1,3 @@
+export const eligibleForFreeToPaidUpsell = ( ) => {
+	return true;
+};
---
+++ client/state/abtest/test/selectors.js
@@ -0,0 +1,20 @@
+/**
+ * External dependencies
+ */
+import deepFreeze from 'deep-freeze';
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import {
+	eligibleForFreeToPaidUpsell
+} from '../selectors';
+
+describe( 'eligibleForFreeToPaidUpsell', () => {
+	it( 'should always return true', () => {
+		const state = deepFreeze( {} );
+		const eligible = eligibleForFreeToPaidUpsell( state, 'site1' );
+		expect( eligible ).to.be.true;
+	} );
+} );
