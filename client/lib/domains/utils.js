e19e154ff5ec3b5153692a93b8a9c81704e981ae
Andrija Vucinic <andrija.vucinic@gmail.com>
2017-02-14 15:31:37 +0100

    Checkout: Fix early or no redirect in case of domain-only sites
    - If the transaction finishes, and a new empty cart is loaded we redirect based on the saved previous cart, which can happen if the transaction takes too long for a domain-only purchase, and the purchase has a different redirect which requires the sites to be reloaded, as the newly purchased domain is the site slug.
    - When we create new blog, it means it's a domain-only site and should always be redirected to domain managament for the bought domain. There's a super rare case when the cartItems get
    updated and are empty at checkout. Use previousCart to get domain registration meta.
    - Skip empty cart redirect if transaction step RECEIVED_WPCOM_RESPONSE
    - Use receipt to get the meta for redirecting domain-first purchases
    - Running fetch if the site list is already polled won't do anything, and won't fire the `change` event thus blocking anything waiting for it
    - Take care or failed domain only purchases (If a no-site purchase fails, redirect to domain management root.)
    
    #11251

--- client/lib/domains/utils.js
+++ client/lib/domains/utils.js
@@ -1,9 +1,16 @@
 /**
+ * External dependencies
+ */
+import { isEmpty, find, values } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import { type as domainTypes } from './constants';
+import { cartItems } from 'lib/cart-values';
+import { isDomainRegistration } from 'lib/products-values';
 
-export function getDomainType( domainFromApi ) {
+function getDomainType( domainFromApi ) {
 	if ( domainFromApi.type === 'redirect' ) {
 		return domainTypes.SITE_REDIRECT;
 	}
@@ -18,3 +25,35 @@ export function getDomainType( domainFromApi ) {
 
 	return domainTypes.MAPPED;
 }
+
+/**
+ * Depending on the current step in checkout, the user's domain can be found in
+ * either the cart or the receipt.
+ *
+ * @param {?Object} receipt - The receipt for the transaction
+ * @param {?Object} cart - The cart for the transaction
+ *
+ * @return {?String} the name of the first domain for the transaction.
+ */
+function getDomainNameFromReceiptOrCart( receipt, cart ) {
+	let domainRegistration;
+
+	if ( receipt && ! isEmpty( receipt.purchases ) ) {
+		domainRegistration = find( values( receipt.purchases ), isDomainRegistration );
+	}
+
+	if ( cartItems.hasDomainRegistration( cart ) ) {
+		domainRegistration = cartItems.getDomainRegistrations( cart )[ 0 ];
+	}
+
+	if ( domainRegistration ) {
+		return domainRegistration.meta;
+	}
+
+	return null;
+}
+
+export {
+	getDomainNameFromReceiptOrCart,
+	getDomainType,
+};
