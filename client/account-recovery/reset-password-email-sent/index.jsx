57e5e0a05cdda95cc7bd9054c52312f2f72622de
James Tien <southp0105@gmail.com>
2017-02-14 17:14:47 +0800

    Add the scalffolding of the component, `ResetPasswordEmailSent`.

---
+++ client/account-recovery/reset-password-email-sent/index.jsx
@@ -0,0 +1,21 @@
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+
+class ResetPasswordEmailSent extends Component {
+	render() {
+		return (
+			<div>
+				<p>{ 'hehheheheheh' }</p>
+			</div>
+		);
+	}
+}
+
+export default localize( ResetPasswordEmailSent );
