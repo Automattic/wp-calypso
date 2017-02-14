9f833e877c8e34cb9a0d5635a98d26e436493351
James Tien <southp0105@gmail.com>
2017-02-14 17:39:09 +0800

    Implement the content of `ResetPasswordEmailSent` component.

--- client/account-recovery/reset-password/reset-password-email-sent/index.jsx
+++ client/account-recovery/reset-password/reset-password-email-sent/index.jsx
@@ -1,21 +1,35 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React from 'react';
 import { localize } from 'i18n-calypso';
 
-/**
- * Internal dependencies
- */
+const ResetPasswordEmailSent = ( props ) => {
+	const {
+		email,
+		translate,
+	} = props;
 
-class ResetPasswordEmailSent extends Component {
-	render() {
-		return (
-			<div>
-				<p>{ 'hehheheheheh' }</p>
-			</div>
-		);
-	}
-}
+	return (
+		<div className="reset-password-email-sent">
+			<h2 className="reset-password-email-sent__title">
+				{ translate( 'Reset your password' ) }
+			</h2>
+			<p>
+				{ translate(
+					'We have sent an email to %(email)s with a reset link. ' +
+					'Click on it to reset your password and recover access to your account.',
+					{
+						args: { email }
+					}
+				) }
+			</p>
+			<p>
+				{ translate( "If you don't find it, please check your spam or junk folder too." ) }
+			</p>
+			<a href="#">{ translate( "You didn't receive it?" ) }</a>
+		</div>
+	);
+};
 
 export default localize( ResetPasswordEmailSent );
