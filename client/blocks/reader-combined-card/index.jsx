70e7e5b6868972aed53a399ed34d2fe0c7b8b2e6
bluefuton <chris@bluefuton.com>
2017-02-15 18:14:05 +0000

    Include ReaderAvatar and SiteStreamLink

--- client/blocks/reader-combined-card/index.jsx
+++ client/blocks/reader-combined-card/index.jsx
@@ -2,17 +2,42 @@
  * External Dependencies
  */
 import React from 'react';
+import { get } from 'lodash';
 
 /**
  * Internal Dependencies
  */
+import { getStreamUrl } from 'reader/route';
+import ReaderAvatar from 'blocks/reader-avatar';
+import ReaderSiteStreamLink from 'blocks/reader-site-stream-link';
+import { siteNameFromSiteAndPost } from 'reader/utils';
 // import AutoDirection from 'components/auto-direction';
 
-const ReaderCombinedCard = ( { /* posts */ } ) => {
+const ReaderCombinedCard = ( { posts, site, feed } ) => {
+	const feedId = get( feed, 'ID' );
+	const siteId = get( site, 'ID' );
+	const siteIcon = get( site, 'icon.img' );
+	const feedIcon = get( feed, 'image' );
+	const streamUrl = getStreamUrl( feedId, siteId );
+	const siteName = siteNameFromSiteAndPost( site, posts[ 0 ] );
+
 	return (
 		<div className="reader-combined-card">
-			<h1>Site name</h1>
-			<p>x posts in timespan</p>
+			<header className="reader-combined-card__header">
+				<ReaderAvatar
+					siteIcon={ siteIcon }
+					feedIcon={ feedIcon }
+					author={ null }
+					preferGravatar={ true }
+					siteUrl={ streamUrl } />
+				<ReaderSiteStreamLink
+					className="reader-combined-card__site-link"
+					feedId={ feedId }
+					siteId={ siteId }>
+					{ siteName }
+				</ReaderSiteStreamLink>
+				<p>x posts in timespan</p>
+			</header>
 			<ul>
 				<li>post</li>
 				<li>post</li>
@@ -23,7 +48,9 @@ const ReaderCombinedCard = ( { /* posts */ } ) => {
 };
 
 ReaderCombinedCard.propTypes = {
-	posts: React.PropTypes.array.isRequired
+	posts: React.PropTypes.array.isRequired,
+	site: React.PropTypes.object,
+	feed: React.PropTypes.object,
 };
 
 export default ReaderCombinedCard;
