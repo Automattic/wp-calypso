a3f9bac47f39431becf796cccada56d06a60ecc6
Ben Lowery <blowery@users.noreply.github.com>
2017-02-15 18:01:40 -0500

    Reader: Cut down image transfer significantly (#11400)
    * Reader: Teach the post preloader how to detect existing dimensions
    
    Also, only try to preload 5 images max.
    
    This cuts down on the number of images we need to transfer to load Reader pages and sigificantly speeds up how we choose what image to show.
    
    * Teach various pieces of the reader to load smaller images
    
    * Use consts for contants, update READER_CONTENT_WIDTH

--- client/blocks/reader-post-card/featured-image.jsx
+++ client/blocks/reader-post-card/featured-image.jsx
@@ -8,6 +8,9 @@ import { noop } from 'lodash';
  * Internal Dependencies
  */
 import cssSafeUrl from 'lib/css-safe-url';
+import resizeImageUrl from 'lib/resize-image-url';
+
+const FEATURED_IMAGE_WIDTH = 250; // typical width of a featured image in the refresh
 
 const FeaturedImage = ( { imageUri, href, children, onClick } ) => {
 	if ( imageUri === undefined ) {
@@ -15,7 +18,7 @@ const FeaturedImage = ( { imageUri, href, children, onClick } ) => {
 	}
 
 	const featuredImageStyle = {
-		backgroundImage: 'url(' + cssSafeUrl( imageUri ) + ')',
+		backgroundImage: 'url(' + cssSafeUrl( resizeImageUrl( imageUri, { w: FEATURED_IMAGE_WIDTH } ) ) + ')',
 		backgroundSize: 'cover',
 		backgroundRepeat: 'no-repeat',
 		backgroundPosition: 'center center'
