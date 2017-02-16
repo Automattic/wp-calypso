7e7bf66868c8cb7dbc888837fa696bb5cfb3f241
James Tien <southp0105@gmail.com>
2017-02-16 14:35:57 +0800

    Remove `SERIALIZE` from accountRecovery.reset.options.items reducer, since it doesn't need to be serialized. Also, clear out `error` on receiving ACCOUNT_RECOVERY_RESET_OPTIONS_REQUEST.

--- client/state/account-recovery/reset/reducer.js
+++ client/state/account-recovery/reset/reducer.js
@@ -11,7 +11,6 @@ import {
 	ACCOUNT_RECOVERY_RESET_OPTIONS_ERROR,
 	ACCOUNT_RECOVERY_RESET_OPTIONS_RECEIVE,
 	ACCOUNT_RECOVERY_RESET_OPTIONS_REQUEST,
-	SERIALIZE,
 } from 'state/action-types';
 
 const isRequesting = ( state = false, action ) => get( {
@@ -21,13 +20,13 @@ const isRequesting = ( state = false, action ) => get( {
 }, action.type, state );
 
 const error = ( state = null, action ) => get( {
+	[ ACCOUNT_RECOVERY_RESET_OPTIONS_REQUEST ]: null,
 	[ ACCOUNT_RECOVERY_RESET_OPTIONS_RECEIVE ]: null,
 	[ ACCOUNT_RECOVERY_RESET_OPTIONS_ERROR ]: action.error,
 }, action.type, state );
 
 const items = ( state = [], action ) => get( {
 	[ ACCOUNT_RECOVERY_RESET_OPTIONS_RECEIVE ]: action.items,
-	[ SERIALIZE ]: [],
 }, action.type, state );
 
 const resetOptions = combineReducers( {
