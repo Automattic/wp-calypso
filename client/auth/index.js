2cca060f76adca7fafcd96e603ef833ecbf167a4
Tugdual de Kerviler <dekervit@gmail.com>
2017-02-16 15:56:15 +0100

    Add featureflag check for auth

--- client/auth/index.js
+++ client/auth/index.js
@@ -6,10 +6,13 @@ import page from 'page';
 /**
  * Internal dependencies
  */
+import config from 'config';
 import controller from './controller';
 
 module.exports = function() {
-	page( '/login', controller.login );
-	page( '/authorize', controller.authorize );
-	page( '/api/oauth/token', controller.getToken );
+	if ( config.isEnabled( 'oauth' ) ) {
+		page( '/login', controller.login );
+		page( '/authorize', controller.authorize );
+		page( '/api/oauth/token', controller.getToken );
+	}
 };
