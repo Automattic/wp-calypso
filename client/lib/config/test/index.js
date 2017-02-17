a80d2dc991f3262faaf29483145f8bc556ca3068
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-02-17 10:28:31 +0100

    Framework: Extract config API into its own file to share code between client and server

---
+++ client/lib/config/test/index.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-console */
+
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import createConfig from '../';
+
+describe( 'index', () => {
+	context( 'config without data', () => {
+		const config = createConfig( {} );
+
+		it( 'has to throw error when the provided key is undefined', () => {
+			expect( () => config( 'keyA' ) ).to.throw( Error );
+		} );
+
+		it( 'has to return false when the feature flags are not specified', () => {
+			const result = config.isEnabled( 'flagA' );
+
+			expect( result ).to.be.false;
+		} );
+
+		it( 'has to return false when the feature flags are not specified', () => {
+			const result = config.anyEnabled( 'flagA' );
+
+			expect( result ).to.be.false;
+		} );
+	} );
+
+	context( 'config with data', () => {
+		const config = createConfig( {
+			keyA: 'value',
+			features: {
+				flagA: false,
+				flagB: false,
+				flagC: true
+			}
+		} );
+
+		it( 'has to return value of the provided key', () => {
+			const result = config( 'keyA' );
+
+			expect( result ).to.equal( 'value' );
+		} );
+
+		it( 'has to return false when the provided feature flag is disabled', () => {
+			const result = config.isEnabled( 'flagA' );
+
+			expect( result ).to.be.false;
+		} );
+
+		it( 'has to return false when the provided feature flag is enabled', () => {
+			const result = config.isEnabled( 'flagC' );
+
+			expect( result ).to.be.true;
+		} );
+
+		it( 'has to return false when all provided feature flags are disabled', () => {
+			const result = config.anyEnabled( 'flagA', 'flagB' );
+
+			expect( result ).to.be.false;
+		} );
+
+		it( 'has to return true when at least one of provided feature flags is enabled', () => {
+			const result = config.anyEnabled( 'flagA', 'flagB', 'flagC' );
+
+			expect( result ).to.be.true;
+		} );
+	} );
+} );
--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,14 @@
+/**
+ * External dependencies
+ */
 const template = require( 'lodash/template' );
+
+/**
+ * Internal dependencies
+ */
 const configPath = require( 'path' ).resolve( __dirname, '..', '..', 'config' );
 const parser = require( './parser' );
+const createConfig = require( 'lib/config' );
 
 const { serverData: data, clientData } = parser( configPath, {
 	env: process.env.CALYPSO_ENV || process.env.NODE_ENV || 'development',
@@ -8,45 +16,11 @@ const { serverData: data, clientData } = parser( configPath, {
 	disabledFeatures: process.env.DISABLE_FEATURES
 } );
 
-/**
- * Return config `key`.
- * Throws an error if the requested `key` is not set in the config file.
- *
- * @param {String} key The key of the config entry.
- * @return {Mixed} Value of config or error if not found.
- * @api public
- */
-function config( key ) {
-	if ( key in data ) {
-		return data[ key ];
-	}
-	throw new Error( 'config key `' + key + '` does not exist' );
-}
-
-function isEnabled( feature ) {
-	return !! data.features[ feature ];
-}
-
-function anyEnabled() {
-	var args = Array.prototype.slice.call( arguments );
-	return args.some( function( feature ) {
-		return isEnabled( feature );
-	} );
-}
-
 const ssrConfig = template(
-	'var data = <%= data %>;' +
-	'<%= config %>' +
-	'<%= isEnabled %>' +
-	'<%= anyEnabled %>'
-) ( {
-	data: JSON.stringify( clientData ),
-	config: config.toString(),
-	isEnabled: isEnabled.toString(),
-	anyEnabled: anyEnabled.toString(),
+	'var configData = <%= data %>;'
+)( {
+	data: JSON.stringify( clientData )
 } );
 
-module.exports = config;
-module.exports.isEnabled = isEnabled;
-module.exports.anyEnabled = anyEnabled;
+module.exports = createConfig( data );
 module.exports.ssrConfig = ssrConfig;
