4650872d14ee80b125b0ce0044d9061a1af8c75c
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-02-17 10:28:31 +0100

    ssr the config (pr 11352): Extract config API into its own file to share code between client and server

--- client/config/index.js
+++ client/config/index.js
@@ -1,19 +1,14 @@
 /**
+ * Internal dependencies
+ */
+import createConfig from 'lib/config';
+
+/**
  * Manages config flags for various deployment builds
  * @module config/index
  */
-
-if ( 'undefined' === typeof window ||  ! window.config ) {
-	throw new ReferenceError( `No configuration was found: please see client/config/README.md for more information` );
+if ( 'undefined' === typeof window || ! window.configData ) {
+	throw new ReferenceError( 'No configuration was found: please see client/config/README.md for more information' );
 }
 
-/**
- * @typedef {Function} CalypsoConfig - retrieves property from config
- * @property {Function} anyEnabled whether one of a set of properties is enabled
- * @property {Function} isEnabled whether a specific property is enabled
- */
-const config = window.config;
-config.isEnabled = window.isEnabled;
-config.anyEnabled = window.anyEnabled;
-
-export default config;
+export default createConfig( window.configData );
