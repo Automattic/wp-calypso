b6c6dc64d6d1592cca741c973e258e04ae8554ea
Riad Benguella <benguella@gmail.com>
2017-02-20 16:14:26 +0100

    Stats: Link to People (Followers) pages instead of custom stats followers pages
    The stats followers page is still used for comments followers

--- client/my-sites/stats/controller.jsx
+++ client/my-sites/stats/controller.jsx
@@ -390,11 +390,9 @@ module.exports = {
 		}
 	},
 
-	follows: function( context, next ) {
+	follows: function( context ) {
 		let siteId = context.params.site_id;
 		const FollowList = require( 'lib/follow-list' );
-		const validFollowTypes = [ 'wpcom', 'email', 'comment' ];
-		const followType = context.params.follow_type;
 		let pageNum = context.params.page_num;
 		const followList = new FollowList();
 		const basePath = route.sectionify( context.path );
@@ -408,9 +406,7 @@ module.exports = {
 		const siteDomain = ( site && ( typeof site.slug !== 'undefined' ) )
 			? site.slug : route.getSiteFragment( context.path );
 
-		if ( -1 === validFollowTypes.indexOf( followType ) ) {
-			next();
-		} else if ( 0 === siteId ) {
+		if ( 0 === siteId ) {
 			if ( 0 === sites.data.length ) {
 				sites.once( 'change', function() {
 					page( context.path );
@@ -428,7 +424,7 @@ module.exports = {
 
 			analytics.pageView.record(
 				basePath.replace( '/' + pageNum, '' ),
-				analyticsPageTitle + ' > Followers > ' + titlecase( followType )
+				analyticsPageTitle + ' > Followers > Comment'
 			);
 
 			const props = {
@@ -439,11 +435,10 @@ module.exports = {
 				domain: siteDomain,
 				sites,
 				siteId,
-				followType,
 				followList,
 			};
 			renderWithReduxStore(
-				<AsyncLoad placeholder={ <StatsPagePlaceholder /> } require="my-sites/stats/follows" { ...props } />,
+				<AsyncLoad placeholder={ <StatsPagePlaceholder /> } require="my-sites/stats/comment-follows" { ...props } />,
 				document.getElementById( 'primary' ),
 				context.store
 			);
