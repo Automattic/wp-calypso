9faed0668378bec598526a24cabc609e8ca18e04
Riad Benguella <benguella@gmail.com>
2017-02-21 08:48:26 +0100

    Stats: Cleanup Stats Overview component from sitesList and User Lib (#11162)

---
+++ client/state/selectors/test/get-visible-sites.js
@@ -0,0 +1,67 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { getVisibleSites } from '../';
+
+describe( 'getVisibleSites()', () => {
+	it( 'should return an empty array if no sites in state', () => {
+		const state = {
+			sites: {
+				items: {}
+			}
+		};
+		const sites = getVisibleSites( state );
+		expect( sites ).to.eql( [] );
+	} );
+
+	it( 'should return the visibles sites in state', () => {
+		const state = {
+			sites: {
+				items: {
+					2916284: {
+						ID: 2916284,
+						visible: true,
+						name: 'WordPress.com Example Blog',
+						URL: 'http://example.com',
+						options: {
+							unmapped_url: 'http://example.com'
+						}
+					},
+					2916285: {
+						ID: 2916285,
+						visible: false,
+						name: 'WordPress.com Non visible Blog',
+						URL: 'http://example2.com',
+						options: {
+							unmapped_url: 'http://example2.com'
+						}
+					}
+				}
+			}
+		};
+		const sites = getVisibleSites( state );
+		expect( sites ).to.eql( [
+			{
+				ID: 2916284,
+				visible: true,
+				name: 'WordPress.com Example Blog',
+				URL: 'http://example.com',
+				title: 'WordPress.com Example Blog',
+				domain: 'example.com',
+				slug: 'example.com',
+				hasConflict: false,
+				is_customizable: false,
+				is_previewable: false,
+				options: {
+					default_post_format: 'standard',
+					unmapped_url: 'http://example.com'
+				}
+			}
+		] );
+	} );
+} );
