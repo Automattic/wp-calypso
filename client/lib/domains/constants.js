cbf0e4b03e98026a4c50cf2e22081bef9023df03
Igor Klimer <igor@klimer.eu>
2017-02-21 18:22:57 +0100

    Domains: Switch to v1.2 of is_available endpoint
    Properly handles TLDs we can't check availability for. Since we can't be sure if
    they are available or not, but we shouldn't prevent users from mapping them
    (unless they are already mapped, etc.).
    
    Fixes #11051

--- client/lib/domains/constants.js
+++ client/lib/domains/constants.js
@@ -49,8 +49,27 @@ const tlds = {
 	vip: 'dotvip_domain',
 };
 
+const domainAvailability = {
+	AVAILABLE: 'available',
+	MAPPABLE: 'mappable',
+	UNKNOWN: 'unknown',
+	NOT_REGISTRABLE: 'available_but_not_registrable',
+	MAINTENANCE: 'tld_in_maintenance',
+	PURCHASES_DISABLED: 'domain_registration_unavailable',
+	FORBIDDEN: 'forbidden_domain',
+	FORBIDDEN_SUBDOMAIN: 'forbidden_subdomain',
+	EMPTY_QUERY: 'empty_query',
+	INVALID: 'invalid_domain',
+	INVALID_TLD: 'invalid_tld',
+	RESTRICTED: 'restricted_domain',
+	BLACKLISTED: 'blacklisted_domain',
+	MAPPED: 'mapped_domain',
+	RECENTLY_UNMAPPED: 'recently_mapped',
+};
+
 export default {
 	type,
 	registrar,
-	tlds
+	tlds,
+	domainAvailability,
 };
