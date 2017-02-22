a43b03717ce66041d5b682f9fc913f4407ae2405
Bernhard Reiter <ockham@raz.or.at>
2017-02-22 20:33:37 +0100

    Themes: On Jetpack sites, include id field in query

--- client/state/themes/utils.js
+++ client/state/themes/utils.js
@@ -235,6 +235,7 @@ export function isThemeMatchingQuery( query, theme ) {
 				) );
 
 				return foundInTaxonomies || (
+					( theme.id && includes( theme.id.toLowerCase(), search ) ) ||
 					( theme.name && includes( theme.name.toLowerCase(), search ) ) ||
 					( theme.author && includes( theme.author.toLowerCase(), search ) ) ||
 					( theme.descriptionLong && includes( theme.descriptionLong.toLowerCase(), search ) )
