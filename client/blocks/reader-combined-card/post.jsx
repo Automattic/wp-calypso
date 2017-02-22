8dcab0f1e0c277f1eac5b2954d64e626c1d91937
bluefuton <chris@bluefuton.com>
2017-02-22 16:43:46 +0000

    Switch to use new Reader blocks

--- client/blocks/reader-combined-card/post.jsx
+++ client/blocks/reader-combined-card/post.jsx
@@ -8,18 +8,18 @@ import { has } from 'lodash';
  * Internal Dependencies
  */
 import AutoDirection from 'components/auto-direction';
-import ReaderPostCardExcerpt from 'blocks/reader-post-card/excerpt';
-import ReaderPostActionsVisitLink from 'blocks/reader-post-actions/visit';
+import ReaderExcerpt from 'blocks/reader-excerpt';
+import ReaderVisitLink from 'blocks/reader-visit-link';
 import ReaderAuthorLink from 'blocks/reader-author-link';
 import { recordPermalinkClick } from 'reader/stats';
 import PostTime from 'reader/post-time';
-import FeaturedImage from 'blocks/reader-post-card/featured-image';
+import ReaderFeaturedImage from 'blocks/reader-featured-image';
 
 const ReaderCombinedCardPost = ( { post, streamUrl /*, site, feed*/ } ) => {
 	const hasAuthorName = has( post, 'author.name' );
 	let featuredAsset = null;
 	if ( post.canonical_media ) {
-		featuredAsset = <FeaturedImage imageUri={ post.canonical_media.src } href={ post.URL } />;
+		featuredAsset = <ReaderFeaturedImage imageUrl={ post.canonical_media.src } href={ post.URL } />;
 	}
 	const recordDateClick = () => {
 		recordPermalinkClick( 'timestamp_combined_card', post );
@@ -33,8 +33,8 @@ const ReaderCombinedCardPost = ( { post, streamUrl /*, site, feed*/ } ) => {
 					<a className="reader-combined-card__post-title-link" href={ post.URL }>{ post.title }</a>
 				</h1>
 			</AutoDirection>
-			<ReaderPostCardExcerpt post={ post } isDiscover={ false } />
-			<ReaderPostActionsVisitLink visitUrl={ post.URL } />
+			<ReaderExcerpt post={ post } isDiscover={ false } />
+			<ReaderVisitLink visitUrl={ post.URL } />
 			{ hasAuthorName &&
 				<ReaderAuthorLink
 					className="reader-combined-card__author-link"
