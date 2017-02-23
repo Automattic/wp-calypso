27390ac1ca558bcca0a22fe880265080bae42947
retrofox <rdsuarez@gmail.com>
2017-02-23 18:14:13 -0300

    plan-storage: remove onClick stuff

--- client/blocks/plan-storage/index.jsx
+++ client/blocks/plan-storage/index.jsx
@@ -4,7 +4,6 @@
 import classNames from 'classnames';
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import noop from 'lodash/noop';
 
 /**
  * Internal dependencies
@@ -19,12 +18,7 @@ class PlanStorage extends Component {
 		className: PropTypes.string,
 		mediaStorage: PropTypes.object,
 		siteId: PropTypes.number,
-		onClick: PropTypes.func
 	};
-
-	static defaultProps = {
-		onClick: noop
-	}
 
 	render() {
 		const { className, site, siteId } = this.props;
@@ -40,7 +34,6 @@ class PlanStorage extends Component {
 					siteSlug={ site.Slug }
 					sitePlanName={ site.plan.product_name_short }
 					mediaStorage={ this.props.mediaStorage }
-					onClick={ this.props.onClick }
 				>
 					{ this.props.children }
 				</PlanStorageBar>
