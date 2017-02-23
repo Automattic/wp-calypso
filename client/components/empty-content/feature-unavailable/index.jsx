b95498ed0df5b1dc3d175121cdcec4ba432fea03
Stéphane Thomas <email@stephanethomas.com>
2017-02-23 19:39:04 +0100

    My Sites: Simplify code that checks if a path is allowed for domain-only sites
    This also removes the magic in the domainManagementEdit() function that inferred the domain name from the site name in favor of a more explicit approach. Some other functions that return paths for pages of the domain management section indeed already have a different behavior when they are called without the second parameter. This change thus makes sure we don't introduce a new behavior that is not consistent with the other functions from this module.

--- client/components/empty-content/feature-unavailable/index.jsx
+++ client/components/empty-content/feature-unavailable/index.jsx
@@ -10,21 +10,25 @@ import React, { PropTypes } from 'react';
 import { domainManagementEdit } from 'my-sites/upgrades/paths';
 import EmptyContent from 'components/empty-content';
 
-const FeatureUnavailable = ( { selectedSite, translate } ) => {
+/**
+ * Renders content to be displayed in response of any action not available in domain-only sites.
+ */
+const FeatureUnavailable = ( { domainName, siteId, translate } ) => {
 	return (
 		<EmptyContent
 			className={ 'feature-unavailable' }
 			title={ translate( 'Add a site to start using this feature.' ) }
 			line={ translate( 'Your domain is only set up with a temporary page. Start a site now to unlock everything WordPress.com can offer.' ) }
 			action={ translate( 'Create Site' ) }
-			actionURL={ `/start/site-selected/?siteSlug=${ encodeURIComponent( selectedSite.slug ) }&siteId=${ encodeURIComponent( selectedSite.ID ) }` }
+			actionURL={ `/start/site-selected/?siteSlug=${ encodeURIComponent( domainName ) }&siteId=${ encodeURIComponent( siteId ) }` }
 			secondaryAction={ translate( 'Manage Domain' ) }
-			secondaryActionURL={ domainManagementEdit( selectedSite.slug ) } />
+			secondaryActionURL={ domainManagementEdit( domainName, domainName ) } />
 	);
 };
 
 FeatureUnavailable.propTypes = {
-	selectedSite: PropTypes.object.isRequired,
+	domainName: PropTypes.string.isRequired,
+	siteId: PropTypes.number.isRequired,
 	translate: PropTypes.func.isRequired
 };
 
