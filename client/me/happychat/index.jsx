27ba0b9186c11522fe517d13c598b632a85d05b4
Jordan West <jordwest@users.noreply.github.com>
2017-02-23 19:50:13 +1000

    Revert "Happychat: Refactor to use middleware" (#11561)

--- client/me/happychat/index.jsx
+++ client/me/happychat/index.jsx
@@ -1,13 +1,13 @@
-/**
- * External Dependencies
- */
+/*
+	External Deps
+*/
 import React from 'react';
 import page from 'page';
 import { translate } from 'i18n-calypso';
 
-/**
- * Internal dependencies
- */
+/*
+	Internal deps
+*/
 import { renderWithReduxStore } from 'lib/react-helpers';
 import controller from 'me/controller';
 import Happychat from './main';
