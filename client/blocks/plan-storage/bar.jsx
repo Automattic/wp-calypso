d6b8596cd6b65f3702b5e8aa4f7431dd3f3e7976
retrofox <rdsuarez@gmail.com>
2017-02-23 18:14:13 -0300

    plan-storage: remove onClick stuff

--- client/blocks/plan-storage/bar.jsx
+++ client/blocks/plan-storage/bar.jsx
@@ -5,7 +5,6 @@ import React, { Component, PropTypes } from 'react';
 import { localize } from 'i18n-calypso';
 import classNames from 'classnames';
 import filesize from 'filesize';
-import noop from 'lodash/noop';
 
 /**
  * Internal dependencies
@@ -21,12 +20,10 @@ class PlanStorageBar extends Component {
 		mediaStorage: PropTypes.object,
 		siteSlug: PropTypes.string.isRequired,
 		sitePlanName: PropTypes.string.isRequired,
-		onClick: PropTypes.func,
 	};
 
 	static defaultProps = {
-		onClick: noop,
-		siteSlug: '',
+		siteSlug: ''
 	};
 
 	render() {
@@ -35,7 +32,6 @@ class PlanStorageBar extends Component {
 			mediaStorage,
 			siteSlug,
 			translate,
-			onClick
 		} = this.props;
 
 		if ( ! mediaStorage || mediaStorage.max_storage_bytes === -1 ) {
@@ -54,7 +50,7 @@ class PlanStorageBar extends Component {
 		const max = filesize( mediaStorage.max_storage_bytes );
 
 		return (
-			<div className={ classes } onClick={ onClick }>
+			<div className={ classes }>
 				<ProgressBar
 					className="plan-storage__bar"
 					value={ percent }
