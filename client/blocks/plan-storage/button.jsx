f0cac2ef222a337cb6075df6d6995c741058a184
retrofox <rdsuarez@gmail.com>
2017-02-23 16:58:46 -0300

    plan-storage: rewrite component using class

--- client/blocks/plan-storage/button.jsx
+++ client/blocks/plan-storage/button.jsx
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { localize } from 'i18n-calypso';
 import classNames from 'classnames';
 import filesize from 'filesize';
 import noop from 'lodash/noop';
@@ -15,52 +16,58 @@ import ProgressBar from 'components/progress-bar';
 const ALERT_PERCENT = 80;
 const WARN_PERCENT = 60;
 
-export default React.createClass( {
+class PlanStorageButton extends Component {
+	static propTypes = {
+		className: PropTypes.string,
+		mediaStorage: PropTypes.object,
+		sitePlanName: PropTypes.string.isRequired,
+		onClick: PropTypes.func,
+	};
 
-	displayName: 'PlanStorageButton',
-
-	propTypes: {
-		sitePlanName: React.PropTypes.string.isRequired,
-		mediaStorage: React.PropTypes.object,
-		onClick: React.PropTypes.func,
-		className: React.PropTypes.string
-	},
-
-	getDefaultProps() {
-		return {
-			onClick: noop
-		}
-	},
+	static defaultProps = {
+		onClick: noop
+	};
 
 	getPlanTranslation() {
-		if ( this.props.sitePlanName === 'Premium' ) {
-			return this.translate( 'Premium Plan', { context: 'Short plan name on storage button' } );
-		} else if ( this.props.sitePlanName === 'Free' ) {
-			return this.translate( 'Free Plan', { context: 'Short plan name on storage button' } );
+		const { sitePlanName, translate } = this.props;
+
+		if ( sitePlanName === 'Premium' ) {
+			return translate( 'Premium Plan', { context: 'Short plan name on storage button' } );
+		} else if ( sitePlanName === 'Free' ) {
+			return translate( 'Free Plan', { context: 'Short plan name on storage button' } );
 		}
 		//This is a fallback if we add a new plan. We ideally want to add any plan levels for proper i18n.
-		return this.translate( '%(planName)s Plan', { args: { planName: this.props.sitePlanName } } );
-	},
+		return translate( '%(planName)s Plan', { args: { planName: sitePlanName } } );
+	}
 
 	render() {
-		if ( ! this.props.mediaStorage || this.props.mediaStorage.max_storage_bytes === -1 ) {
+		const {
+			className,
+			mediaStorage,
+			translate,
+			onClick
+		} = this.props;
+
+		if ( ! mediaStorage || mediaStorage.max_storage_bytes === -1 ) {
 			return null;
 		}
 		const percent = Math.min(
-			Math.round( this.props.mediaStorage.storage_used_bytes / this.props.mediaStorage.max_storage_bytes * 1000 ) / 10,
-			100 );
-		const classes = classNames( this.props.className, 'plan-storage__button', {
+			Math.round( mediaStorage.storage_used_bytes / mediaStorage.max_storage_bytes * 1000 ) / 10,
+		100 );
+
+		const classes = classNames( className, 'plan-storage__button', {
 			'is-alert': percent > ALERT_PERCENT,
 			'is-warn': percent > WARN_PERCENT && percent <= ALERT_PERCENT
 		} );
-		const max = filesize( this.props.mediaStorage.max_storage_bytes );
+
+		const max = filesize( mediaStorage.max_storage_bytes );
 		return (
-			<Button className={ classes } onClick={ this.props.onClick }>
+			<Button className={ classes } onClick={ onClick }>
 				<span className="plan-storage__plan-label">
 					{ this.getPlanTranslation() }
 				</span>
 				<span className="plan-storage__storage-label" >
-					{ this.translate( '%(percent)f%% of %(max)s used', {
+					{ translate( '%(percent)f%% of %(max)s used', {
 						args: {
 							percent: percent,
 							max: max
@@ -76,4 +83,6 @@ export default React.createClass( {
 			</Button>
 		);
 	}
-} );
+}
+
+export default localize( PlanStorageButton );
