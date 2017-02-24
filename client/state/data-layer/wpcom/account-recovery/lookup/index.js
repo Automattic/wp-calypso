1e0febcef7f394c7bb36b951d79b335074672d42
James Tien <southp0105@gmail.com>
2017-02-24 17:04:50 +0800

    Throw on validation failure instead of producing empty values.

--- client/state/data-layer/wpcom/account-recovery/lookup/index.js
+++ client/state/data-layer/wpcom/account-recovery/lookup/index.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { isString } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import wpcom from 'lib/wp';
@@ -14,45 +19,39 @@ const validate = ( { primary_email, primary_sms, secondary_email, secondary_sms 
 	primary_sms,
 	secondary_email,
 	secondary_sms,
-].every( String );
+].every( isString );
 
-export const fromApi = data => (
-	validate( data ) ? [
-		{
-			email: data.primary_email,
-			sms: data.primary_sms,
-			name: 'primary',
-		},
-		{
-			email: data.secondary_email,
-			sms: data.secondary_sms,
-			name: 'secondary',
-		},
-	] : [
-		{
-			email: '',
-			sms: '',
-			name: 'primary',
-		},
-		{
-			email: '',
-			sms: '',
-			name: 'secondary',
-		},
-	] );
+export const fromApi = data => ( [
+	{
+		email: data.primary_email,
+		sms: data.primary_sms,
+		name: 'primary',
+	},
+	{
+		email: data.secondary_email,
+		sms: data.secondary_sms,
+		name: 'secondary',
+	},
+] );
 
 export const requestResetOptions = ( { dispatch }, { userData } ) => (
 	wpcom.req.get( {
 		body: userData,
 		apiNamespace: 'wpcom/v2',
 		path: '/account-recovery/lookup',
-	} ).then( data => dispatch( {
-		type: ACCOUNT_RECOVERY_RESET_OPTIONS_RECEIVE,
-		options: fromApi( data ),
-	} ) )
+	} ).then( data => {
+		if ( validate( data ) ) {
+			dispatch( {
+				type: ACCOUNT_RECOVERY_RESET_OPTIONS_RECEIVE,
+				options: fromApi( data ),
+			} );
+		} else {
+			throw Error( 'Unexpected response format' );
+		}
+	} )
 	.catch( error => dispatch( {
 		type: ACCOUNT_RECOVERY_RESET_OPTIONS_ERROR,
-		error,
+		error: error,
 	} ) )
 );
 
