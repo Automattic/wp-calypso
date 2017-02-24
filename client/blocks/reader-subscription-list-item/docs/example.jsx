2088bd957dccec7fd8e49f19e44b131e8363e7c3
Jake F - @samouri <samouri@users.noreply.github.com>
2017-02-24 10:58:11 -0500

    major progress, need to level up some styling

--- client/blocks/reader-subscription-list-item/docs/example.jsx
+++ client/blocks/reader-subscription-list-item/docs/example.jsx
@@ -8,28 +8,62 @@ import React, { PureComponent } from 'react';
  */
 import ReaderSubscriptionListItem from '../';
 import Card from 'components/card';
+import { getCurrentUser } from 'state/current-user/selectors';
+
+
+const items = [
+	{
+		siteAuthor: {
+			avatar_URL: 'https://2.gravatar.com/avatar/83cb16245c3d8ac5f625ebf91a7827e7?d=identicon&r=G&s=1600A',
+			first_name: 'Allison',
+			last_name: undefined,
+		},
+		siteUrl: 'http://afarmgirlslife.wordpress.com',
+		siteTitle: 'A Farm Girls Life',
+		siteExcerpt: 'Photography, Crafty Things, and Life on the Farm',
+		lastUpdated: 'x days ago',
+	},
+	{
+		siteAuthor: {
+			avatar_URL: 'https://2.gravatar.com/avatar/bd02f74face048cc62c4eda28cea9937?d=mm&r=G&s=96',
+			first_name: undefined,
+			last_name: undefined,
+		},
+		siteUrl: 'https://fourthgenerationfarmgirl.com/',
+		siteTitle: 'fourth generation farmgirl',
+		siteExcerpt: 'wool and wine to tractors and travel',
+		lastUpdated: 'x days ago',
+	},
+	{
+		siteAuthor: {
+			avatar_URL: 'https://1.gravatar.com/avatar/d1becc42ff085294b51c77f3ce850b15?d=mm&r=G&s=96',
+			first_name: 'Morgan',
+			last_name: 'Pencek',
+		},
+		siteUrl: 'https://notyourtypicalhippie.wordpress.com/',
+		siteTitle: 'Not Your Typical Hippi',
+		siteExcerpt: 'Starting Young ♢ Living Healthy',
+		lastUpdated: 'x days ago',
+	}
+]
 
 export default class ReaderSubscriptionListItemExample extends PureComponent {
 	displayName = 'ReaderSubscriptionListItem';
 
 	render() {
-		const feedId = 40474296;
-		const siteId = null;
-		const siteUrl = 'https://readersquad.wordpress.com'
-		const siteTitle = 'readersquad P2'
-		const siteAuthorUsername = 'samauri';
-		const siteAuthorName = 'Jake';
 		return (
 			<Card>
-				<ReaderSubscriptionListItem
-					isFollowing={ true }
-					siteId={ 40474296 }
-					siteUrl = { siteUrl }
-					siteTitle = { siteTitle }
-					siteAuthor = { siteAuthorName }
-					siteAuthorUsername = { siteAuthorUsername }
-					siteExcerpt = 'this is the description for a SubscriptionListItem'
-					/>
+				{ items.map( item =>
+						<ReaderSubscriptionListItem
+							isFollowing={ true }
+							siteUrl = { item.siteUrl }
+							siteTitle = { item.siteTitle }
+							siteAuthor = { item.siteAuthor }
+							siteExcerpt = { item.siteExcerpt }
+							lastUpdated = { item.lastUpdated }
+							key={ item.siteUrl }
+						/>
+				) }
 			</Card>
 		);
 	}
--- client/blocks/reader-subscription-list-item/index.jsx
+++ client/blocks/reader-subscription-list-item/index.jsx
@@ -3,37 +3,53 @@
  */
 import React from 'react';
 import classnames from 'classnames';
+import { trim, isEmpty } from 'lodash';
 
 /**
  * Internal Dependencies
  */
 import Gravatar from 'components/gravatar';
 import FollowButton from 'reader/follow-button';
+import Gridicon from 'gridicons';
 
 function SettingsMenu() {
-	return <p>"Hi I'm a settings icon that works!!!" </p>;
+	return (
+		<span className="reader-subscription-list-item__settings-menu">
+			<Gridicon
+				icon="cog"
+				size={ 20 }
+			/>
+		Settings
+		</span>
+	);
 }
 
 function ReaderSubscriptionListItem( {
 	isFollowing,
 	siteUrl,
 	siteTitle,
-	siteAuthorUsername,
-	siteAuthorName,
+	siteAuthor,
 	siteExcerpt,
 	className = '',
 	onSiteClick = () => {},
 	followSource = 'reader-subscriptions-list-item',
 } ) {
+	const authorName = trim( `${ siteAuthor.first_name || '' } ${ siteAuthor.last_name || '' }` );
 	return (
-		<div className={ classnames( 'reader-subscriptions-list-item', className ) }>
+		<div className={ classnames( 'reader-subscription-list-item', className ) }>
 			<a href={ siteUrl } onClick={ onSiteClick }>
-				<Gravatar user={ siteAuthorUsername } />
+				<Gravatar user={ siteAuthor } />
 			</a>
-			<span>{ <a href={ siteUrl }> { siteTitle } </a> }</span> by <span>{ siteAuthorName }</span>
-			<p>{ siteExcerpt }</p>
+			<span>{ <a href={ siteUrl }> { siteTitle } </a> }</span>
+			{ ! isEmpty( authorName ) &&
+				<span>
+					<span className="reader-subscription-list-item__by-text"> by </span>
+					<span><a href={ siteUrl }> { authorName } </a></span>
+				</span>
+			}
 			<FollowButton siteUrl={ siteUrl } followSource={ followSource } />
 			{ isFollowing && <SettingsMenu /> }
+			<p>{ siteExcerpt }</p>
 		</div>
 	);
 }
