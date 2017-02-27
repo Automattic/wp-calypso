5b847099da36866a61c2f5656199cf1a979014b3
Kevin Killingsworth <kevin@redfenix.com>
2017-02-27 12:15:17 -0600

    extensions: Add lyric ui to hello dolly.
    This adds a lyric line that pulls from redux state.

--- client/extensions/hello-dolly/hello-dolly-page.js
+++ client/extensions/hello-dolly/hello-dolly-page.js
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal Dependencies
@@ -11,53 +12,76 @@ import Card from 'components/card';
 import SectionHeader from 'components/section-header';
 import FAQ from 'components/faq';
 import FAQItem from 'components/faq/faq-item';
+import { getCurrentLyric } from './state/selectors';
+import lyrics from './state/lyrics';
 
-export default function() {
-	return (
-		<Main className="hello-dolly__main">
-			<SectionHeader label="Hello, Dolly!">🐑</SectionHeader>
-			<Card>
-				<p style={ { fontSize: 18, fontWeight: 300 } }>
-					This is not just an extension, it symbolizes the hope and enthusiasm of an entire generation
-					summed up in two words sung most famously by Louis Armstrong.
-				</p>
-				<hr />
-				<p>This section is the home for the <strong>Hello Dolly</strong> extension.</p>
-				<p>
-					Extensions are set up to function in a semi-isolated environment,
-					with their own URL path and code-chunk magic (using the power of webpack)
-					to assure code is loaded only when needed. Think of extensions as
-					individual "apps" you can access in Calypso to interact with your
-					plugin functionality in a focused way.
-				</p>
-			</Card>
-			<FAQ>
-				<FAQItem
-					question="How can I get started?"
-					answer={ [
-						'Head over to the "extensions" folder in the repository where we have a more comprehensive ',
-						'README file to walk you through the process. ',
-						<a href="https://github.com/Automattic/wp-calypso/tree/master/client/extensions" key="get-started">Get Started</a>
-					] }
-				/>
-				<FAQItem
-					question="Learning about Calypso"
-					answer={ [
-						'You can browse our docs without leaving the running application, just go to the Devdocs section. ',
-						'Tip: press the keys "gd" to navigate there from any page! ',
-						<a href="/devdocs" key="go-devdocs">Go to Devdocs</a>
-					] }
-				/>
-				<FAQItem
-					question="Have more questions?"
-					answer={ [
-						'Let\'s talk! Visit the ',
-						<a href="https://github.com/Automattic/wp-calypso/" key="go-repo">Calypso GitHub repository</a>,
-						' and open an issue.'
-					] }
-				/>
-			</FAQ>
-		</Main>
-	);
+class HelloDollyPage extends Component {
+	static propTypes = {
+		currentLyric: PropTypes.string.isRequired,
+	};
+
+	render() {
+		const { currentLyric } = this.props;
+
+		return (
+			<Main className="hello-dolly__main">
+				<SectionHeader label="Hello, Dolly!">🐑</SectionHeader>
+				<Card>
+					<p style={ { fontSize: 18, fontWeight: 300 } }>
+						This is not just an extension, it symbolizes the hope and enthusiasm of an entire generation
+						summed up in two words sung most famously by Louis Armstrong.
+					</p>
+					<p style={ { fontSize: 24, fontWeight: 500, textAlign: 'center' } }>
+						{ currentLyric }
+					</p>
+					<hr />
+					<p>This section is the home for the <strong>Hello Dolly</strong> extension.</p>
+					<p>
+						Extensions are set up to function in a semi-isolated environment,
+						with their own URL path and code-chunk magic (using the power of webpack)
+						to assure code is loaded only when needed. Think of extensions as
+						individual "apps" you can access in Calypso to interact with your
+						plugin functionality in a focused way.
+					</p>
+				</Card>
+				<FAQ>
+					<FAQItem
+						question="How can I get started?"
+						answer={ [
+							'Head over to the "extensions" folder in the repository where we have a more comprehensive ',
+							'README file to walk you through the process. ',
+							<a href="https://github.com/Automattic/wp-calypso/tree/master/client/extensions" key="get-started">Get Started</a>
+						] }
+					/>
+					<FAQItem
+						question="Learning about Calypso"
+						answer={ [
+							'You can browse our docs without leaving the running application, just go to the Devdocs section. ',
+							'Tip: press the keys "gd" to navigate there from any page! ',
+							<a href="/devdocs" key="go-devdocs">Go to Devdocs</a>
+						] }
+					/>
+					<FAQItem
+						question="Have more questions?"
+						answer={ [
+							'Let\'s talk! Visit the ',
+							<a href="https://github.com/Automattic/wp-calypso/" key="go-repo">Calypso GitHub repository</a>,
+							' and open an issue.'
+						] }
+					/>
+				</FAQ>
+			</Main>
+		);
+	}
 }
+
+function mapStateToProps( state ) {
+	const currentLyric = getCurrentLyric( lyrics )( state );
+
+	return {
+		currentLyric
+	};
+}
+
+export default connect( mapStateToProps )( HelloDollyPage );
 
