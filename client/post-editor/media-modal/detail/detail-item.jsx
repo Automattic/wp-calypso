10b88bc8a2f52c2321d8389e1462c592b57755b9
Donna Peplinskie <donnapep@gmail.com>
2017-02-28 09:13:28 -0500

    Pass className as prop in media modal preview components

--- client/post-editor/media-modal/detail/detail-item.jsx
+++ client/post-editor/media-modal/detail/detail-item.jsx
@@ -211,6 +211,7 @@ class EditorMediaModalDetailItem extends Component {
 		}
 
 		return React.createElement( Item, {
+			className: 'editor-media-modal-detail__preview',
 			site: site,
 			item: item
 		} );
