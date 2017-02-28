fd6fdd61f9f364fbf6ee2261577420307347e9f6
Donna Peplinskie <donnapep@gmail.com>
2017-02-28 07:39:58 -0500

    Add video editor reducer

---
+++ client/state/ui/editor/video-editor/reducer.js
@@ -0,0 +1,149 @@
+/**
+ * External dependencies
+ */
+import { combineReducers } from 'redux';
+
+/**
+ * Internal dependencies
+ */
+import {
+	VIDEO_EDITOR_POSTER_UPDATE,
+	VIDEO_EDITOR_POSTER_UPDATE_FAILURE,
+	VIDEO_EDITOR_POSTER_UPDATE_SUCCESS,
+	VIDEO_EDITOR_SCRIPT_LOAD_ERROR,
+	VIDEO_EDITOR_STATE_RESET,
+	VIDEO_EDITOR_STATE_RESET_POSTER,
+	VIDEO_EDITOR_VIDEO_HAS_LOADED,
+} from 'state/action-types';
+
+/**
+ * Tracks video loading state.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action object
+ * @return {Object}        Updated state
+ */
+export const videoIsLoading = ( state = true, action ) => {
+	switch ( action.type ) {
+		case VIDEO_EDITOR_VIDEO_HAS_LOADED:
+			return false;
+
+		case VIDEO_EDITOR_STATE_RESET:
+			return true;
+	}
+
+	return state;
+};
+
+/**
+ * Tracks poster updating state.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action object
+ * @return {Object}        Updated state
+ */
+export const posterIsUpdating = ( state = false, action ) => {
+	switch ( action.type ) {
+		case VIDEO_EDITOR_POSTER_UPDATE:
+			return true;
+
+		case VIDEO_EDITOR_POSTER_UPDATE_SUCCESS:
+		case VIDEO_EDITOR_POSTER_UPDATE_FAILURE:
+		case VIDEO_EDITOR_STATE_RESET:
+		case VIDEO_EDITOR_STATE_RESET_POSTER:
+			return false;
+	}
+
+	return state;
+};
+
+/**
+ * Tracks poster updated state.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action object
+ * @return {Object}        Updated state
+ */
+export const posterIsUpdated = ( state = false, action ) => {
+	switch ( action.type ) {
+		case VIDEO_EDITOR_POSTER_UPDATE_SUCCESS:
+		case VIDEO_EDITOR_POSTER_UPDATE_FAILURE:
+			return true;
+
+		case VIDEO_EDITOR_STATE_RESET:
+		case VIDEO_EDITOR_STATE_RESET_POSTER:
+			return false;
+	}
+
+	return state;
+};
+
+/**
+ * Tracks poster URL state.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action object
+ * @return {Object}        Updated state
+ */
+export const poster = ( state = '', action ) => {
+	switch ( action.type ) {
+		case VIDEO_EDITOR_POSTER_UPDATE_SUCCESS:
+			return action.poster;
+
+		case VIDEO_EDITOR_STATE_RESET:
+		case VIDEO_EDITOR_STATE_RESET_POSTER:
+			return '';
+	}
+
+	return state;
+};
+
+/**
+ * Tracks poster error state.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action object
+ * @return {Object}        Updated state
+ */
+export const hasPosterUpdateError = ( state = false, action ) => {
+	switch ( action.type ) {
+		case VIDEO_EDITOR_POSTER_UPDATE_FAILURE:
+			return true;
+
+		case VIDEO_EDITOR_POSTER_UPDATE:
+		case VIDEO_EDITOR_POSTER_UPDATE_SUCCESS:
+		case VIDEO_EDITOR_STATE_RESET:
+		case VIDEO_EDITOR_STATE_RESET_POSTER:
+			return false;
+	}
+
+	return state;
+};
+
+/**
+ * Tracks VideoPress script loading state.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action object
+ * @return {Object}        Updated state
+ */
+export const hasScriptLoadError = ( state = false, action ) => {
+	switch ( action.type ) {
+		case VIDEO_EDITOR_SCRIPT_LOAD_ERROR:
+			return true;
+
+		case VIDEO_EDITOR_STATE_RESET:
+			return false;
+	}
+
+	return state;
+};
+
+export default combineReducers( {
+	hasPosterUpdateError,
+	hasScriptLoadError,
+	poster,
+	posterIsUpdating,
+	posterIsUpdated,
+	videoIsLoading,
+} );
---
+++ client/state/ui/editor/video-editor/test/reducer.js
@@ -0,0 +1,258 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import {
+	VIDEO_EDITOR_POSTER_UPDATE,
+	VIDEO_EDITOR_POSTER_UPDATE_FAILURE,
+	VIDEO_EDITOR_POSTER_UPDATE_SUCCESS,
+	VIDEO_EDITOR_SCRIPT_LOAD_ERROR,
+	VIDEO_EDITOR_STATE_RESET,
+	VIDEO_EDITOR_STATE_RESET_POSTER,
+	VIDEO_EDITOR_VIDEO_HAS_LOADED,
+} from 'state/action-types';
+
+import reducer, {
+	hasPosterUpdateError,
+	hasScriptLoadError,
+	poster,
+	posterIsUpdating,
+	posterIsUpdated,
+	videoIsLoading,
+} from '../reducer';
+
+describe( 'reducer', () => {
+	it( 'should export expected reducer keys', () => {
+		expect( reducer( undefined, {} ) ).to.have.keys( [
+			'hasPosterUpdateError',
+			'hasScriptLoadError',
+			'poster',
+			'posterIsUpdating',
+			'posterIsUpdated',
+			'videoIsLoading',
+		] );
+	} );
+
+	describe( '#videoIsLoading()', () => {
+		it( 'should default to true', () => {
+			const state = videoIsLoading( undefined, {} );
+
+			expect( state ).to.be.true;
+		} );
+
+		it( 'should change to false on load', () => {
+			const state = videoIsLoading( undefined, {
+				type: VIDEO_EDITOR_VIDEO_HAS_LOADED,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to true on reset', () => {
+			const state = videoIsLoading( undefined, {
+				type: VIDEO_EDITOR_STATE_RESET,
+			} );
+
+			expect( state ).to.be.true;
+		} );
+	} );
+
+	describe( '#posterIsUpdating()', () => {
+		it( 'should default to false', () => {
+			const state = posterIsUpdating( undefined, {} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to true on poster update', () => {
+			const state = posterIsUpdating( undefined, {
+				type: VIDEO_EDITOR_POSTER_UPDATE,
+			} );
+
+			expect( state ).to.be.true;
+		} );
+
+		it( 'should change to false on successful update', () => {
+			const state = posterIsUpdating( undefined, {
+				type: VIDEO_EDITOR_POSTER_UPDATE_SUCCESS,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to false on failed update', () => {
+			const state = posterIsUpdating( undefined, {
+				type: VIDEO_EDITOR_POSTER_UPDATE_FAILURE,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to false on reset', () => {
+			const state = posterIsUpdating( undefined, {
+				type: VIDEO_EDITOR_STATE_RESET,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to false on poster reset', () => {
+			const state = posterIsUpdating( undefined, {
+				type: VIDEO_EDITOR_STATE_RESET_POSTER,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+	} );
+
+	describe( '#posterIsUpdated()', () => {
+		it( 'should default to false', () => {
+			const state = posterIsUpdated( undefined, {} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to true on successful update', () => {
+			const state = posterIsUpdated( undefined, {
+				type: VIDEO_EDITOR_POSTER_UPDATE_SUCCESS,
+			} );
+
+			expect( state ).to.be.true;
+		} );
+
+		it( 'should change to true on failed update', () => {
+			const state = posterIsUpdated( undefined, {
+				type: VIDEO_EDITOR_POSTER_UPDATE_FAILURE,
+			} );
+
+			expect( state ).to.be.true;
+		} );
+
+		it( 'should change to false on reset', () => {
+			const state = posterIsUpdated( undefined, {
+				type: VIDEO_EDITOR_STATE_RESET,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to false on poster reset', () => {
+			const state = posterIsUpdated( undefined, {
+				type: VIDEO_EDITOR_STATE_RESET_POSTER,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+	} );
+
+	describe( '#poster()', () => {
+		const posterUrl = 'https://i1.wp.com/videos.files.wordpress.com/dummy-guid/thumbnail.jpg?ssl=1';
+
+		it( 'should default to empty string', () => {
+			const state = poster( undefined, {} );
+
+			expect( state ).to.eql( '' );
+		} );
+
+		it( 'should change to poster url on successful update', () => {
+			const state = poster( undefined, {
+				type: VIDEO_EDITOR_POSTER_UPDATE_SUCCESS,
+				poster: posterUrl,
+			} );
+
+			expect( state ).to.eql( posterUrl );
+		} );
+
+		it( 'should change to empty string on reset', () => {
+			const state = poster( undefined, {
+				type: VIDEO_EDITOR_STATE_RESET,
+			} );
+
+			expect( state ).to.eql( '' );
+		} );
+
+		it( 'should change to empty string on poster reset', () => {
+			const state = poster( undefined, {
+				type: VIDEO_EDITOR_STATE_RESET_POSTER,
+			} );
+
+			expect( state ).to.eql( '' );
+		} );
+	} );
+
+	describe( '#hasPosterUpdateError()', () => {
+		it( 'should default to false', () => {
+			const state = hasPosterUpdateError( undefined, {} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to true on failed update', () => {
+			const state = hasPosterUpdateError( undefined, {
+				type: VIDEO_EDITOR_POSTER_UPDATE_FAILURE,
+			} );
+
+			expect( state ).to.be.true;
+		} );
+
+		it( 'should change to false on poster update', () => {
+			const state = hasPosterUpdateError( undefined, {
+				type: VIDEO_EDITOR_POSTER_UPDATE,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to false on successful update', () => {
+			const state = hasPosterUpdateError( undefined, {
+				type: VIDEO_EDITOR_POSTER_UPDATE_SUCCESS,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to false on reset', () => {
+			const state = hasPosterUpdateError( undefined, {
+				type: VIDEO_EDITOR_STATE_RESET,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to false on poster reset', () => {
+			const state = hasPosterUpdateError( undefined, {
+				type: VIDEO_EDITOR_STATE_RESET_POSTER,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+	} );
+
+	describe( '#hasScriptLoadError()', () => {
+		it( 'should default to false', () => {
+			const state = hasScriptLoadError( undefined, {} );
+
+			expect( state ).to.be.false;
+		} );
+
+		it( 'should change to true on script load error', () => {
+			const state = hasScriptLoadError( undefined, {
+				type: VIDEO_EDITOR_SCRIPT_LOAD_ERROR,
+			} );
+
+			expect( state ).to.be.true;
+		} );
+
+		it( 'should change to false on reset', () => {
+			const state = hasScriptLoadError( undefined, {
+				type: VIDEO_EDITOR_STATE_RESET,
+			} );
+
+			expect( state ).to.be.false;
+		} );
+	} );
+} );
