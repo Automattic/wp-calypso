69f46e378b22e257415ba3fc133076997709c0da
Stéphane Thomas <email@stephanethomas.com>
2017-02-28 16:12:48 +0100

    My Sites: Add navigation to Feature Unavailable page
    This makes sure users can navigate back to the sidebar on small viewports.

--- client/components/empty-content/feature-unavailable/index.jsx
+++ client/components/empty-content/feature-unavailable/index.jsx
@@ -7,22 +7,30 @@ import React, { PropTypes } from 'react';
 /**
  * Internal dependencies
  */
+import DocumentHead from 'components/data/document-head';
 import { domainManagementEdit } from 'my-sites/upgrades/paths';
 import EmptyContent from 'components/empty-content';
+import SidebarNavigation from 'my-sites/sidebar-navigation';
 
 /**
  * Renders content to be displayed in response of any action not available in domain-only sites.
  */
 const FeatureUnavailable = ( { domainName, siteId, translate } ) => {
 	return (
-		<EmptyContent
-			className={ 'feature-unavailable' }
-			title={ translate( 'Add a site to start using this feature.' ) }
-			line={ translate( 'Your domain is only set up with a temporary page. Start a site now to unlock everything WordPress.com can offer.' ) }
-			action={ translate( 'Create Site' ) }
-			actionURL={ `/start/site-selected/?siteSlug=${ encodeURIComponent( domainName ) }&siteId=${ encodeURIComponent( siteId ) }` }
-			secondaryAction={ translate( 'Manage Domain' ) }
-			secondaryActionURL={ domainManagementEdit( domainName, domainName ) } />
+		<div>
+			<DocumentHead title={ translate( 'Domain Management' ) } />
+
+			<SidebarNavigation />
+
+			<EmptyContent
+				className={ 'feature-unavailable' }
+				title={ translate( 'Add a site to start using this feature.' ) }
+				line={ translate( 'Your domain is only set up with a temporary page. Start a site now to unlock everything WordPress.com can offer.' ) }
+				action={ translate( 'Create Site' ) }
+				actionURL={ `/start/site-selected/?siteSlug=${ encodeURIComponent( domainName ) }&siteId=${ encodeURIComponent( siteId ) }` }
+				secondaryAction={ translate( 'Manage Domain' ) }
+				secondaryActionURL={ domainManagementEdit( domainName, domainName ) } />
+		</div>
 	);
 };
 
