87326b587cfc328d1cf560098a38441fe7c80a04
artpi <artpi@post.pl>
2017-03-01 13:01:18 +0100

    Revert "Remove ennecessary check in buttongroup"
    This reverts commit be960766fe48a46fce376bbf1f3a5b7606304eb1.

--- client/components/button-group/index.jsx
+++ client/components/button-group/index.jsx
@@ -8,6 +8,18 @@ export default React.createClass( {
 
 	displayName: 'ButtonGroup',
 
+	propTypes: {
+		children( props ) {
+			let error = null;
+			React.Children.forEach( props.children, ( child ) => {
+				if ( ! child.props || child.props.type !== 'button' ) {
+					error = new Error( 'All children elements should be a Button.' );
+				}
+			} );
+			return error;
+		}
+	},
+
 	render() {
 		const buttonGroupClasses = classNames( 'button-group', this.props.className );
 
