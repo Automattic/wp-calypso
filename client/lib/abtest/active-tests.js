2f970f7911f777afbee2667dfec9617fe61ed426
Mark Ryall <mark.ryall@gmail.com>
2017-03-01 13:57:09 +1000

    Remove a/b testing around free to paid upsell nudge. (#11661)
    * Remove a/b testing around free to paid upsell nudge.
    
    * Remove trailing comma.

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -123,15 +123,5 @@ module.exports = {
 		defaultVariation: 'hide',
 		allowExistingUsers: true,
 		allowAnyLocale: true
-	},
-
-	freeToPaidUpsell: {
-		datestamp: '20170222',
-		variations: {
-			sidebar: 50,
-			disabled: 50
-		},
-		defaultVariation: 'disabled',
-		allowExistingUsers: true
 	}
 };
