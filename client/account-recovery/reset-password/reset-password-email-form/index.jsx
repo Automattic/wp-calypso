d5d1c2ae8f08ffabc470cd790e28b59a701cdcb6
James Tien <southp0105@gmail.com>
2017-03-03 12:09:03 +0800

    Update the wording as the copy review

--- client/account-recovery/reset-password/reset-password-email-form/index.jsx
+++ client/account-recovery/reset-password/reset-password-email-form/index.jsx
@@ -23,8 +23,7 @@ const ResetPasswordEmailSent = ( props ) => {
 			<Card>
 				<p>
 					{ translate(
-						'We have sent an email to {{strong}}%(email)s{{/strong}} with a reset link. ' +
-						'Click on it to reset your password and recover access to your account.',
+						"We've sent an email to {{strong}}%(email)s{{/strong}} with a link to complete your password reset.",
 						{
 							components: { strong: <strong /> },
 							args: { email }
@@ -32,7 +31,7 @@ const ResetPasswordEmailSent = ( props ) => {
 					) }
 				</p>
 				<p>
-					{ translate( "If you don't find it, please check your spam or junk folder too." ) }
+					{ translate( "If you haven't received it, please check your spam or junk folder." ) }
 				</p>
 				<a href="#">{ translate( "You didn't receive it?" ) }</a>
 			</Card>
