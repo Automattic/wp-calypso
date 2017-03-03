1bb04a324ded17864abb9a146050204b838a36b1
James Tien <southp0105@gmail.com>
2017-03-03 12:20:58 +0800

    Add a new selector: get-account-recovery-reset-options-error

---
+++ client/state/selectors/test/get-account-recovery-reset-options-error.js
@@ -0,0 +1,29 @@
+/**
+ * External dependencies
+ */
+import { assert } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { getAccountRecoveryResetOptionsError } from '../';
+
+describe( 'getAccountRecoveryResetOptionsError()', () => {
+	it( 'should return the error under account recovery state tree.', () => {
+		const expectedError = {
+			status: 404,
+			message: 'Something wrong!',
+		};
+		const state = {
+			accountRecovery: {
+				reset: {
+					options: {
+						error: expectedError,
+					},
+				},
+			},
+		};
+
+		assert.deepEqual( getAccountRecoveryResetOptionsError( state ), expectedError );
+	} );
+} );
