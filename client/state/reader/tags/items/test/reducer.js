3a854accc5716492f6fe76df2f297d731ddfbd38
Jake F - @samouri <samouri@users.noreply.github.com>
2017-03-03 03:46:35 -0500

    fix unit tests.  TODO: give the unit tests a hard look up and down

--- client/state/reader/tags/items/test/reducer.js
+++ client/state/reader/tags/items/test/reducer.js
@@ -10,22 +10,22 @@ import { keyBy } from 'lodash';
 import { items } from '../reducer';
 import { receiveUnfollowTag, receiveTags } from '../actions';
 
-const keyById = tags => keyBy( tags, 'ID' );
+const keyById = tags => keyBy( tags, 'id' );
 
 const TAG1 = {
-	ID: '307',
+	id: '307',
 	slug: 'chickens',
 	title: 'Chickens',
-	display_name: 'chickens',
-	URL: 'https://public-api.wordpress.com/rest/v1.2/read/tags/chickens/posts',
+	displayName: 'chickens',
+	url: 'https://public-api.wordpress.com/rest/v1.2/read/tags/chickens/posts',
 };
 
 const TAG2 = {
-	ID: '148',
+	id: '148',
 	slug: 'design',
 	title: 'Design',
-	display_name: 'design',
-	URL: 'https://public-api.wordpress.com/rest/v1.2/read/tags/design/posts',
+	displayName: 'design',
+	url: 'https://public-api.wordpress.com/rest/v1.2/read/tags/design/posts',
 };
 
 describe( 'reducer', () => {
@@ -40,7 +40,7 @@ describe( 'reducer', () => {
 			const action = receiveTags( { payload: [ TAG1 ] } );
 
 			const state = items( prevState, action );
-			expect( state ).to.eql( { [ TAG1.ID ]: TAG1 } );
+			expect( state ).to.eql( { [ TAG1.id ]: TAG1 } );
 		} );
 
 		it( 'should add multiple tags in the payload to state', () => {
@@ -52,7 +52,7 @@ describe( 'reducer', () => {
 		} );
 
 		it( 'should update tags that have changed', () => {
-			const prevState = { [ TAG1.ID ]: TAG1, [ TAG2.ID ]: TAG2 };
+			const prevState = { [ TAG1.id ]: TAG1, [ TAG2.id ]: TAG2 };
 			const action = receiveTags( {
 				payload: [ { ...TAG1, title: 'NotChickens' }, TAG2 ],
 			} );
@@ -61,20 +61,15 @@ describe( 'reducer', () => {
 			expect( state ).to.eql( keyById( [ { ...TAG1, title: 'NotChickens' }, TAG2 ] ) );
 		} );
 
-		it( 'should remove a tag if request to remove was successful', () => {
-			const prevState = { [ TAG1.ID ]: TAG1, [ TAG2.ID ]: TAG2 };
-			const action = receiveUnfollowTag( { payload: TAG1.ID, error: false } );
+		it( 'should unfollow a tag if requested to do so', () => {
+			const prevState = { [ TAG1.id ]: TAG1, [ TAG2.id ]: TAG2 };
+			const action = receiveUnfollowTag( { payload: TAG1.id } );
 			const state = items( prevState, action );
 
-			expect( state ).to.eql( keyById( [ TAG2 ] ) );
-		} );
-
-		it( 'should not remove a tag if request to remove errored', () => {
-			const prevState = { [ TAG1.ID ]: TAG1, [ TAG2.ID ]: TAG2 };
-			const action = receiveUnfollowTag( { payload: TAG1.ID, error: true } );
-			const state = items( prevState, action );
-
-			expect( state ).to.eql( keyById( [ TAG1, TAG2 ] ) );
+			expect( state ).to.eql( keyById( [
+				{ ...TAG1, isFollowing: false },
+				TAG2
+			] ) );
 		} );
 	} );
 } );
