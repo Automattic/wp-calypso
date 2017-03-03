915ba4cf8f8df33c2698e7a96df58d5a6d8220c9
James Tien <southp0105@gmail.com>
2017-03-03 12:04:07 +0800

    Add missing indentation

--- client/account-recovery/reset-password/reset-password-email-form/index.jsx
+++ client/account-recovery/reset-password/reset-password-email-form/index.jsx
@@ -21,20 +21,20 @@ const ResetPasswordEmailSent = ( props ) => {
 				{ translate( 'Reset your password' ) }
 			</h2>
 			<Card>
-			<p>
-				{ translate(
-					'We have sent an email to {{strong}}%(email)s{{/strong}} with a reset link. ' +
-					'Click on it to reset your password and recover access to your account.',
-					{
-						components: { strong: <strong /> },
-						args: { email }
-					}
-				) }
-			</p>
-			<p>
-				{ translate( "If you don't find it, please check your spam or junk folder too." ) }
-			</p>
-			<a href="#">{ translate( "You didn't receive it?" ) }</a>
+				<p>
+					{ translate(
+						'We have sent an email to {{strong}}%(email)s{{/strong}} with a reset link. ' +
+						'Click on it to reset your password and recover access to your account.',
+						{
+							components: { strong: <strong /> },
+							args: { email }
+						}
+					) }
+				</p>
+				<p>
+					{ translate( "If you don't find it, please check your spam or junk folder too." ) }
+				</p>
+				<a href="#">{ translate( "You didn't receive it?" ) }</a>
 			</Card>
 		</div>
 	);
