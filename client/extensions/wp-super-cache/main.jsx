9a0b40fece861b4e12f1ee5f294d47aa5faa0b0c
Donna Peplinskie <donnapep@gmail.com>
2017-03-03 16:27:10 -0500

    Render proper component for currently selected tab

--- client/extensions/wp-super-cache/main.jsx
+++ client/extensions/wp-super-cache/main.jsx
@@ -8,12 +8,36 @@ import React from 'react';
  */
 import Main from 'components/main';
 import Navigation from './components/navigation';
+import Easy from './components/easy';
+import { Tabs } from './constants';
 
-const WPSuperCache = ( { tab } ) => (
-	<Main className="wp-super-cache__main">
-		<Navigation activeTab={ tab } />
-	</Main>
-);
+const WPSuperCache = ( { tab } ) => {
+	const renderTab = () => {
+		switch ( tab ) {
+			case Tabs.ADVANCED:
+				break;
+			case Tabs.CDN:
+				break;
+			case Tabs.CONTENTS:
+				break;
+			case Tabs.PRELOAD:
+				break;
+			case Tabs.PLUGINS:
+				break;
+			case Tabs.DEBUG:
+				break;
+			default:
+				return <Easy />;
+		}
+	};
+
+	return (
+		<Main className="wp-super-cache__main">
+			<Navigation activeTab={ tab } />
+			{ renderTab() }
+		</Main>
+	);
+};
 
 WPSuperCache.propTypes = {
 	tab: React.PropTypes.string
