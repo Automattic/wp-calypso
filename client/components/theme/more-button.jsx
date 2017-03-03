Automattic/wp-calypso

commit a61bdb68459966ac38e1adeb3dab2e17cff1490e

Implement tracking for theme showcase. (#11032)

client/components/theme/more-button.jsx
@@ -24,24 +24,36 @@ class ThemeMoreButton extends React.Component {
 		this.togglePopover = this.togglePopover.bind( this );
 		this.closePopover = this.closePopover.bind( this );
 		this.onClick = this.onClick.bind( this );
+		this.onPopoverActionClick = this.onPopoverActionClick.bind( this );
 	}
 
 	togglePopover() {
 		this.setState( { showPopover: ! this.state.showPopover } );
-		! this.state.showPopover && this.props.onClick( this.props.theme.id, this.props.index );
+		! this.state.showPopover && this.props.onMoreButtonClick( this.props.theme, this.props.index, 'popup_open' );
 	}
 
 	closePopover( action ) {
 		this.setState( { showPopover: false } );
-		isFunction( action ) && action( this.props.theme );
+		if ( isFunction( action ) ) {
+			action();
+		}
+	}
+
+	popoverAction( action, label ) {
+		action( this.props.theme );
+		this.props.onMoreButtonClick( this.props.theme, this.props.index, 'popup_' + label );
+	}
+
+	onPopoverActionClick( action, label ) {
+		return () => this.popoverAction( action, label );
 	}
 
 	focus( event ) {
 		event.target.focus();
 	}
 
-	onClick( action ) {
-		return this.closePopover.bind( this, action );
+	onClick( action, label ) {
+		return this.closePopover.bind( this, this.onPopoverActionClick( action, label ) );
 	}
 
 	render() {
@@ -71,7 +83,7 @@ class ThemeMoreButton extends React.Component {
 							return (
 								<a className="theme__more-button-menu-item popover__menu-item"
 									onMouseOver={ this.focus }
-									onClick={ this.onClick( option.action ) }
+									onClick={ this.onClick( option.action, option.label ) }
 									key={ option.label }
 									href={ url }
 									target={ isOutsideCalypso( url ) ? '_blank' : null }>
@@ -81,7 +93,9 @@ class ThemeMoreButton extends React.Component {
 						}
 						if ( option.action ) {
 							return (
-								<PopoverMenuItem key={ option.label } action={ option.action }>
+								<PopoverMenuItem
+									key={ option.label }
+									action={ this.onPopoverActionClick( option.action, option.label ) }>
 									{ option.label }
 								</PopoverMenuItem>
 							);
@@ -101,6 +115,9 @@ ThemeMoreButton.propTypes = {
 	theme: React.PropTypes.object,
 	// Index of theme in results list
 	index: React.PropTypes.number,
+	// More elaborate onClick action, used for tracking.
+	// Made to not interfere with DOM onClick
+	onMoreButtonClick: React.PropTypes.func,
 	// Options to populate the popover menu with
 	options: React.PropTypes.objectOf(
 		React.PropTypes.shape( {

client/components/theme/index.jsx
@@ -176,7 +176,7 @@ const Theme = React.createClass( {
 									index={ this.props.index }
 									theme={ this.props.theme }
 									active={ this.props.active }
-									onClick={ this.props.onMoreButtonClick }
+									onMoreButtonClick={ this.props.onMoreButtonClick }
 									options={ this.props.buttonContents } />
 							</TrackInteractions> : null }
 					</div>

client/my-sites/themes/themes-selection.jsx
@@ -3,7 +3,7 @@
  */
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { compact, isEqual, omit, property } from 'lodash';
+import { compact, isEqual, omit, property, snakeCase } from 'lodash';
 
 /**
  * Internal dependencies
@@ -64,14 +64,16 @@ const ThemesSelection = React.createClass( {
 		}
 	},
 
-	recordSearchResultsClick( theme, resultsRank ) {
+	recordSearchResultsClick( theme, resultsRank, action ) {
 		const { query, themes } = this.props;
 		analytics.tracks.recordEvent( 'calypso_themeshowcase_theme_click', {
 			search_term: query.search,
 			theme: theme.id,
 			results_rank: resultsRank + 1,
 			results: themes.map( property( 'id' ) ).join(),
-			page_number: query.page
+			page_number: query.page,
+			theme_on_page: parseInt( ( resultsRank + 1 ) / query.number ),
+			action: snakeCase( action )
 		} );
 	},
 
@@ -87,8 +89,8 @@ const ThemesSelection = React.createClass( {
 
 	onScreenshotClick( theme, resultsRank ) {
 		trackClick( 'theme', 'screenshot' );
-		if ( ! this.props.isThemeActive( theme.id ) ) {
-			this.recordSearchResultsClick( theme, resultsRank );
+		if ( ! this.props.isThemeActive( theme ) ) {
+			this.recordSearchResultsClick( theme, resultsRank, 'screenshot_info' );
 		}
 		this.props.onScreenshotClick && this.props.onScreenshotClick( theme );
 	},
@@ -123,6 +125,7 @@ const ThemesSelection = React.createClass( {
 				<ThemesList themes={ this.props.themes }
 					fetchNextPage={ this.fetchNextPage }
 					getButtonOptions={ this.props.getOptions }
+					onMoreButtonClick={ this.recordSearchResultsClick }
 					onScreenshotClick={ this.onScreenshotClick }
 					getScreenshotUrl={ this.props.getScreenshotUrl }
 					getActionLabel={ this.props.getActionLabel }

commit 94178c3fda2f25eb495afec0d833cc1b9a3508e1

Fix ThemeMoreButton/TrackInteractions relationship
I believe the regression was introduced in
a61bdb68459966ac38e1adeb3dab2e17cff1490e, so let's naively call
`onClick` again.

client/components/theme/more-button.jsx
@@ -30,6 +30,9 @@ class ThemeMoreButton extends React.Component {
 	togglePopover() {
 		this.setState( { showPopover: ! this.state.showPopover } );
 		! this.state.showPopover && this.props.onMoreButtonClick( this.props.theme, this.props.index, 'popup_open' );
+		if ( this.props.onClick ) {
+			this.props.onClick();
+		}
 	}
 
 	closePopover( action ) {