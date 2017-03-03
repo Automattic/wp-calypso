7abb30718fdd1cac0c69aff34a527771eda49843
Konstantin Obenland <obenland@gmx.de>
2017-03-02 17:40:56 -0800

    Allow Sharing to be a functional component
    I could not be more comfortable with that.
    
    See
    https://github.com/Automattic/wp-calypso/pull/11724#discussion_r10405882
    0

--- client/my-sites/sharing/main.jsx
+++ client/my-sites/sharing/main.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { find, get } from 'lodash';
 import { localize } from 'i18n-calypso';
@@ -18,67 +18,57 @@ import SectionNav from 'components/section-nav';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import UpgradeNudge from 'my-sites/upgrade-nudge';
 
-class Sharing extends Component {
-	getFilters() {
-		const { canManageOptions, site, translate } = this.props;
-		const pathSuffix = site ? '/' + site.slug : '';
-		const filters = [];
+export const Sharing = ( { canManageOptions, contentComponent, path, site, translate } ) => {
+	const pathSuffix = site ? '/' + site.slug : '';
+	const filters = [];
 
-		// Include Connections link if all sites are selected. Otherwise,
-		// verify that the required Jetpack module is active
-		if ( ! site || ! site.jetpack || site.isModuleActive( 'publicize' ) ) {
-			filters.push( {
-				id: 'sharing-connections',
-				path: '/sharing' + pathSuffix,
-				title: translate( 'Connections' ),
-			} );
-		}
-
-		// Include Sharing Buttons link if a site is selected and the
-		// required Jetpack module is active
-		if ( site && canManageOptions &&
-			( ! site.jetpack ||
-				( site.isModuleActive( 'sharedaddy' ) && site.versionCompare( '3.4-dev', '>=' ) )
-			)
-		) {
-			filters.push( {
-				id: 'sharing-buttons',
-				path: '/sharing/buttons' + pathSuffix,
-				title: translate( 'Sharing Buttons' ),
-			} );
-		}
-
-		return filters;
+	// Include Connections link if all sites are selected. Otherwise,
+	// verify that the required Jetpack module is active
+	if ( ! site || ! site.jetpack || site.isModuleActive( 'publicize' ) ) {
+		filters.push( {
+			id: 'sharing-connections',
+			route: '/sharing' + pathSuffix,
+			title: translate( 'Connections' ),
+		} );
 	}
 
-	render() {
-		const filters = this.getFilters();
-		const selected = find( filters, { path: this.props.path } );
-
-		return (
-			<Main className="sharing">
-				<DocumentHead title={ this.props.translate( 'Sharing', { textOnly: true } ) } />
-				<SidebarNavigation />
-				<SectionNav selectedText={ get( selected, 'title', '' ) }>
-					<NavTabs>
-						{ filters.map( ( { id, path, title } ) => (
-							<NavItem key={ id } path={ path } selected={ path === this.props.path }>
-								{ title }
-							</NavItem>
-						) ) }
-					</NavTabs>
-				</SectionNav>
-				<UpgradeNudge
-					title={ this.props.translate( 'No Ads with WordPress.com Premium' ) }
-					message={ this.props.translate( 'Prevent ads from showing on your site.' ) }
-					feature="no-adverts"
-					event="sharing_no_ads"
-				/>
-				{ this.props.contentComponent }
-			</Main>
-		);
+	// Include Sharing Buttons link if a site is selected and the
+	// required Jetpack module is active
+	if ( site && canManageOptions &&
+		( ! site.jetpack || ( site.isModuleActive( 'sharedaddy' ) && site.versionCompare( '3.4-dev', '>=' ) ) )
+	) {
+		filters.push( {
+			id: 'sharing-buttons',
+			route: '/sharing/buttons' + pathSuffix,
+			title: translate( 'Sharing Buttons' ),
+		} );
 	}
-}
+
+	const selected = find( filters, { route: path } );
+
+	return (
+		<Main className="sharing">
+			<DocumentHead title={ translate( 'Sharing', { textOnly: true } ) } />
+			<SidebarNavigation />
+			<SectionNav selectedText={ get( selected, 'title', '' ) }>
+				<NavTabs>
+					{ filters.map( ( { id, route, title } ) => (
+						<NavItem key={ id } path={ route } selected={ path === route }>
+							{ title }
+						</NavItem>
+					) ) }
+				</NavTabs>
+			</SectionNav>
+			<UpgradeNudge
+				event="sharing_no_ads"
+				feature="no-adverts"
+				message={ translate( 'Prevent ads from showing on your site.' ) }
+				title={ translate( 'No Ads with WordPress.com Premium' ) }
+			/>
+			{ contentComponent }
+		</Main>
+	);
+};
 
 Sharing.propTypes = {
 	canManageOptions: PropTypes.bool,
