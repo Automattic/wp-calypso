08a42c87bc332436a61b75abb04c1dbf2fdfe7af
retrofox <rdsuarez@gmail.com>
2017-03-04 11:36:48 -0300

    plan-storage: add and use getSitePlanSlug selector

--- client/blocks/plan-storage/index.jsx
+++ client/blocks/plan-storage/index.jsx
@@ -11,7 +11,7 @@ import { connect } from 'react-redux';
 import QueryMediaStorage from 'components/data/query-media-storage';
 import { getMediaStorage } from 'state/sites/media-storage/selectors';
 import {
-	getSitePlan,
+	getSitePlanSlug,
 	getSiteSlug,
 	isJetpackSite
 } from 'state/sites/selectors';
@@ -23,6 +23,8 @@ class PlanStorage extends Component {
 		className: PropTypes.string,
 		mediaStorage: PropTypes.object,
 		siteId: PropTypes.number,
+		sitePlanSlug: PropTypes.string,
+		siteSlug: PropTypes.string,
 	};
 
 	render() {
@@ -30,11 +32,11 @@ class PlanStorage extends Component {
 			className,
 			jetpackSite,
 			siteId,
-			sitePlan,
+			sitePlanSlug,
 			siteSlug,
 		} = this.props;
 
-		if ( jetpackSite || ! sitePlan ) {
+		if ( jetpackSite || ! sitePlanSlug ) {
 			return null;
 		}
 
@@ -43,7 +45,7 @@ class PlanStorage extends Component {
 				<QueryMediaStorage siteId={ siteId } />
 				<PlanStorageBar
 					siteSlug={ siteSlug }
-					sitePlanSlug={ sitePlan.product_slug }
+					sitePlanSlug={ sitePlanSlug }
 					mediaStorage={ this.props.mediaStorage }
 				>
 					{ this.props.children }
@@ -58,7 +60,7 @@ export default connect( ( state, ownProps ) => {
 	return {
 		mediaStorage: getMediaStorage( state, siteId ),
 		jetpackSite: isJetpackSite( state, siteId ),
-		sitePlan: getSitePlan( state, siteId ),
+		sitePlanSlug: getSitePlanSlug( state, siteId ),
 		siteSlug: getSiteSlug( state, siteId ),
 	};
 } )( PlanStorage );
