5b04d5e963b39b5c97439f594d1146cae5e8ce34
Donna Peplinskie <donnapep@gmail.com>
2017-03-06 09:52:01 -0500

    Refactor VideoEditorButtons to be a functional component

--- client/blocks/video-editor/video-editor-buttons.jsx
+++ client/blocks/video-editor/video-editor-buttons.jsx
@@ -1,8 +1,7 @@
 /**
  * External dependencies
  */
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
+import React, { PropTypes } from 'react';
 import { noop } from 'lodash';
 import { localize } from 'i18n-calypso';
 
@@ -12,62 +11,58 @@ import { localize } from 'i18n-calypso';
 import Button from 'components/button';
 import UploadButton from './video-editor-upload-button';
 
-class VideoEditorButtons extends Component {
-	static propTypes = {
-		isPosterUpdating: PropTypes.bool,
-		isVideoLoaded: PropTypes.bool,
-		onCancel: PropTypes.func,
-		onSelectFrame: PropTypes.func,
-		onUploadImage: PropTypes.func,
-		onUploadImageClick: PropTypes.func,
-	};
-
-	static defaultProps = {
-		isPosterUpdating: false,
-		isVideoLoaded: false,
-		onSelectFrame: noop,
-		onUploadImage: noop,
-		onUploadImageClick: noop,
-	};
-
-	render() {
-		const {
-			isPosterUpdating,
-			isVideoLoaded,
-			onCancel,
-			onSelectFrame,
-			onUploadImage,
-			onUploadImageClick,
-			translate,
-		} = this.props;
-
-		return (
-			<div className="video-editor__buttons">
-				{ onCancel &&
-					<Button
-						className="video-editor__buttons-button"
-						disabled={ isPosterUpdating }
-						onClick={ onCancel }>
-						{ translate( 'Cancel' ) }
-					</Button>
-				}
-				<UploadButton
-					className="button video-editor__buttons-button"
-					isPosterUpdating={ isPosterUpdating }
-					onClick={ onUploadImageClick }
-					onUploadImage={ onUploadImage }>
-					{ translate( 'Upload Image' ) }
-				</UploadButton>
+const VideoEditorButtons = ( {
+	isPosterUpdating,
+	isVideoLoaded,
+	onCancel,
+	onSelectFrame,
+	onUploadImage,
+	onUploadImageClick,
+	translate,
+} ) => {
+	return (
+		<div className="video-editor__buttons">
+			{ onCancel &&
 				<Button
 					className="video-editor__buttons-button"
-					disabled={ ! isVideoLoaded || isPosterUpdating }
-					onClick={ onSelectFrame }
-					primary>
-					{ translate( 'Select Frame' ) }
+					disabled={ isPosterUpdating }
+					onClick={ onCancel }>
+					{ translate( 'Cancel' ) }
 				</Button>
-			</div>
-		);
-	}
-}
+			}
+			<UploadButton
+				className="button video-editor__buttons-button"
+				isPosterUpdating={ isPosterUpdating }
+				onClick={ onUploadImageClick }
+				onUploadImage={ onUploadImage }>
+				{ translate( 'Upload Image' ) }
+			</UploadButton>
+			<Button
+				className="video-editor__buttons-button"
+				disabled={ ! isVideoLoaded || isPosterUpdating }
+				onClick={ onSelectFrame }
+				primary>
+				{ translate( 'Select Frame' ) }
+			</Button>
+		</div>
+	);
+};
 
-export default connect()( localize( VideoEditorButtons ) );
+VideoEditorButtons.propTypes = {
+	isPosterUpdating: PropTypes.bool,
+	isVideoLoaded: PropTypes.bool,
+	onCancel: PropTypes.func,
+	onSelectFrame: PropTypes.func,
+	onUploadImage: PropTypes.func,
+	onUploadImageClick: PropTypes.func,
+};
+
+VideoEditorButtons.defaultProps = {
+	isPosterUpdating: false,
+	isVideoLoaded: false,
+	onSelectFrame: noop,
+	onUploadImage: noop,
+	onUploadImageClick: noop,
+};
+
+export default localize( VideoEditorButtons );
