b2b925caf15af59169e52650145754354c69b589
Jake F - @samouri <samouri@users.noreply.github.com>
2017-03-06 07:33:15 -0500

    ssr the config (pr 11352): Remove all usages of config module from wordpress-com section building

--- client/account-recovery/index.js
+++ client/account-recovery/index.js
@@ -8,11 +8,14 @@ import {
 	resetPasswordByTransactionId,
 	redirectLoggedIn
 } from './controller';
+import config from 'config';
 
 export default function( router ) {
 	// Main route for account recovery is the lost password page
-	router( '/account-recovery', redirectLoggedIn, lostPassword );
-	router( '/account-recovery/forgot-username', redirectLoggedIn, forgotUsername );
-	router( '/account-recovery/reset-password', redirectLoggedIn, resetPassword );
-	router( '/account-recovery/reset-password/transaction-id', redirectLoggedIn, resetPasswordByTransactionId );
+	if ( config.isEnabled( 'account-recovery' ) ) {
+		router( '/account-recovery', redirectLoggedIn, lostPassword );
+		router( '/account-recovery/forgot-username', redirectLoggedIn, forgotUsername );
+		router( '/account-recovery/reset-password', redirectLoggedIn, resetPassword );
+		router( '/account-recovery/reset-password/transaction-id', redirectLoggedIn, resetPasswordByTransactionId );
+	}
 }
