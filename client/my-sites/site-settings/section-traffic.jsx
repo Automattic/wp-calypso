e6dc4ac9f316b2d13ee06438f7f601f946e5f3d5
Marin Atanasov <tyxla@abv.bg>
2017-03-06 16:55:45 +0200

    Site Settings: Add Analytics settings to Traffic tab

--- client/my-sites/site-settings/section-traffic.jsx
+++ client/my-sites/site-settings/section-traffic.jsx
@@ -7,6 +7,7 @@ import React, { Component } from 'react';
  * Internal dependencies
  */
 import SeoSettingsMain from 'my-sites/site-settings/seo-settings/main';
+import AnalyticsSettings from 'my-sites/site-settings/form-analytics';
 
 class SiteSettingsTraffic extends Component {
 	render() {
@@ -14,6 +15,7 @@ class SiteSettingsTraffic extends Component {
 		return (
 			<div>
 				<SeoSettingsMain sites={ sites } upgradeToBusiness={ upgradeToBusiness } />
+				<AnalyticsSettings />
 			</div>
 		);
 	}
