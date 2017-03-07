4c7d28ca50042b2d1d850d3665f6c2408068eb18
Steve Seear <steve.seear@gmail.com>
2017-03-07 20:37:24 +0000

    Themes: Prevent active theme appearing in uploaded list
    The theme returned from the /mine endpoint does not have
    enough info to determine if it is a wpcom theme or not.
    In this case, assume it is wpcom theme to prevent it appearing
    in the 'Uploaded themes' list.

--- client/state/themes/utils.js
+++ client/state/themes/utils.js
@@ -212,6 +212,13 @@ export function getSerializedThemesQueryWithoutPage( query, siteId ) {
  * @return {Boolean}      Whether theme is a wpcom theme
  */
 export function isThemeFromWpcom( theme ) {
+	if ( ! theme.hasOwnProperty( 'theme_uri' ) ) {
+		// if not enough information to determine, assume
+		// theme is wpcom to prevent it appearing in
+		// the uploaded themes list
+		return true;
+	}
+
 	return includes( theme.theme_uri, 'wordpress.com' );
 }
 
