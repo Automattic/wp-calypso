f3a0d58598bce81c7f7f61b42ef66fecabbdc04a
Timmy Crawford <timmyc@users.noreply.github.com>
2017-03-07 09:19:26 -0800

    Editor: Fix over-riding of local title edits on auto-save. (#11729)
    * Editor: Fix over-riding of local title edits on auto-save.
    
    * Remove POST_SAVE_SUCCESS from edits reducer.
    
    * Add reducer logic to move edits to post.ID
    
    * Updates per feedback.

--- client/state/posts/test/reducer.js
+++ client/state/posts/test/reducer.js
@@ -903,26 +903,33 @@ describe( 'reducer', () => {
 			expect( state ).to.equal( original );
 		} );
 
-		it( 'should discard edits when the post is saved', () => {
+		it( 'should copy edits when the post is saved and prior postId was null', () => {
 			const state = edits( deepFreeze( {
 				2916284: {
-					841: {
-						title: 'Hello World'
-					},
 					'': {
 						title: 'Ribs & Chicken'
+					},
+					842: {
+						title: 'I like turtles'
 					}
 				}
 			} ), {
 				type: POST_SAVE_SUCCESS,
 				siteId: 2916284,
-				postId: 841
+				postId: null,
+				savedPost: {
+					ID: 841,
+					title: 'Ribs'
+				}
 			} );
 
 			expect( state ).to.eql( {
 				2916284: {
-					'': {
+					841: {
 						title: 'Ribs & Chicken'
+					},
+					842: {
+						title: 'I like turtles'
 					}
 				}
 			} );
