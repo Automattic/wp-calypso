1a4d6c0bdcb8d83d828b94838b46c5882dea0eb2
James Tien <southp0105@gmail.com>
2017-03-07 18:17:11 +0800

    Simplify the syntax of action creators, since they only return plan objects.

--- client/state/account-recovery/reset/actions.js
+++ client/state/account-recovery/reset/actions.js
@@ -8,27 +8,20 @@ import {
 	ACCOUNT_RECOVERY_RESET_UPDATE_USER_DATA,
 } from 'state/action-types';
 
-const fetchResetOptions = ( userData ) => {
-	return {
-		type: ACCOUNT_RECOVERY_RESET_OPTIONS_REQUEST,
-		userData,
-	};
-};
+const fetchResetOptions = userData => ( {
+	type: ACCOUNT_RECOVERY_RESET_OPTIONS_REQUEST,
+	userData,
+} );
 
-export const fetchResetOptionsByLogin = ( user ) => {
-	return fetchResetOptions( { user } );
-};
+export const fetchResetOptionsByLogin = user => fetchResetOptions( { user } );
 
-export const fetchResetOptionsByNameAndUrl = ( firstname, lastname, url ) => {
-	return fetchResetOptions( { firstname, lastname, url } );
-};
+export const fetchResetOptionsByNameAndUrl = ( firstname, lastname, url ) =>
+	fetchResetOptions( { firstname, lastname, url } );
 
-export const fetchResetOptionsSuccess = ( options ) => {
-	return {
-		type: ACCOUNT_RECOVERY_RESET_OPTIONS_RECEIVE,
-		options,
-	};
-};
+export const fetchResetOptionsSuccess = ( options ) => ( {
+	type: ACCOUNT_RECOVERY_RESET_OPTIONS_RECEIVE,
+	options,
+} );
 
 export const fetchResetOptionsError = ( error ) => {
 	return {
