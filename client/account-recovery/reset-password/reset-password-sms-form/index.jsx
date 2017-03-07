074cf4b7b0ccebb6f228de48c4133504dbf508fd
James Tien <southp0105@gmail.com>
2017-03-07 17:28:30 +0800

    Add a new component: `ResetPasswordSmsForm` and register the form under the route `account-recovery/reset-password/sms-form/

---
+++ client/account-recovery/reset-password/reset-password-sms-form/index.jsx
@@ -0,0 +1,40 @@
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+import FormTextInput from 'components/forms/form-text-input';
+import FormButton from 'components/forms/form-button';
+
+class ResetPasswordSmsForm extends Component {
+	render() {
+		const {
+			translate,
+		} = this.props;
+
+		return (
+			<div className="reset-password-sms-form">
+				<h2 className="reset-password-sms-form__title">
+					{ translate( 'Reset your password' ) }
+				</h2>
+				<Card>
+					<p>
+						{ translate( 'Please enter the code you were sent by SMS. ' +
+								'It will look something like 63423423. You may need to wait a few moments before it arrives.' )
+						}
+					</p>
+					<FormTextInput />
+					<a href="#">{ translate( 'No SMS?' ) }</a>
+					<FormButton />
+				</Card>
+			</div>
+		);
+	}
+}
+
+export default localize( ResetPasswordSmsForm );
