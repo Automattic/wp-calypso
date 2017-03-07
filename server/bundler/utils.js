b2bebf8f1ed7f05d12815bb1cf0ab0be01233c5e
Matías Ventura <mv@matiasventura.com>
2017-03-07 13:36:30 +0100

    Add comments explaining purpose of pathToRegex routing function.

--- server/bundler/utils.js
+++ server/bundler/utils.js
@@ -13,7 +13,10 @@ function getAssets( stats ) {
 	} );
 }
 
+// Adapts route paths to also include wildcard
+// subroutes under the root level section.
 function pathToRegExp( path ) {
+	// Prevents root level double dash urls from being validated.
 	if ( path === '/' ) {
 		return path;
 	}
