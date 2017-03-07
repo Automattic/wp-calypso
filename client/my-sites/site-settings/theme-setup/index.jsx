b18e0e19c3720731c40cca4e4dae21056ebc7868
Kirk Wight <kwight@kwight.ca>
2017-03-07 15:07:47 -0800

    Add the main Theme Setup component and its elements. (#11806)

---
+++ client/my-sites/site-settings/theme-setup/index.jsx
@@ -0,0 +1,52 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import page from 'page';
+
+/**
+ * Internal dependencies
+ */
+import HeaderCake from 'components/header-cake';
+import QueryActiveTheme from 'components/data/query-active-theme';
+import QueryTheme from 'components/data/query-theme';
+import ThemeSetupCard from './theme-setup-card';
+import ThemeSetupPlaceholder from './theme-setup-placeholder';
+import { getSelectedSite } from 'state/ui/selectors';
+import { getActiveTheme, getTheme } from 'state/themes/selectors';
+
+let ThemeSetup = ( { site, themeId, theme, translate, activeSiteDomain } ) => {
+	const onBack = () => {
+		page( '/settings/general/' + activeSiteDomain );
+	};
+
+	return (
+		<div className="main theme-setup" role="main">
+			{ site && <QueryActiveTheme siteId={ site.ID } /> }
+			{ themeId && <QueryTheme siteId={ 'wpcom' } themeId={ themeId } /> }
+			<HeaderCake onClick={ onBack }><h1>{ translate( 'Theme Setup' ) }</h1></HeaderCake>
+			{ site && theme
+				? <ThemeSetupCard
+					theme={ theme } />
+				: <ThemeSetupPlaceholder /> }
+		</div>
+	);
+};
+
+ThemeSetup = localize( ThemeSetup );
+
+const mapStateToProps = ( state ) => {
+	const site = getSelectedSite( state );
+	const themeId = site && getActiveTheme( state, site.ID );
+	const theme = themeId && getTheme( state, 'wpcom', themeId );
+	return {
+		site,
+		themeId,
+		theme,
+	};
+};
+
+export default connect( mapStateToProps )( ThemeSetup );
+
