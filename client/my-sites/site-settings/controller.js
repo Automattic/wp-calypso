58279535a907d76b078f3ab0ba54cdfeb0285564
Kirk Wight <kwight@kwight.ca>
2017-03-08 12:36:12 -0800

    Add Theme Setup to the `wpcalypso` environment.

--- client/my-sites/site-settings/controller.js
+++ client/my-sites/site-settings/controller.js
@@ -19,6 +19,7 @@ import SiteSettingsComponent from 'my-sites/site-settings/main';
 import sitesFactory from 'lib/sites-list';
 import StartOver from './start-over';
 import Taxonomies from './taxonomies';
+import ThemeSetup from './theme-setup';
 import DateTimeFormat from './date-time-format';
 import { setDocumentHeadTitle as setTitle } from 'state/document-head/actions';
 import titlecase from 'to-title-case';
@@ -184,6 +185,32 @@ module.exports = {
 		renderPage(
 			context,
 			<DateTimeFormat />
+		);
+	},
+
+	themeSetup( context ) {
+		let site = sites.getSelectedSite();
+
+		if ( sites.initialized ) {
+			if ( site.jetpack ) {
+				return page( '/settings/general/' + site.slug );
+			}
+		} else {
+			sites.once( 'change', function() {
+				site = sites.getSelectedSite();
+				if ( site.jetpack ) {
+					return page( '/settings/general/' + site.slug );
+				}
+			} );
+		}
+
+		if ( ! config.isEnabled( 'settings/theme-setup' ) ) {
+			return page( '/settings/general/' + site.slug );
+		}
+
+		renderPage(
+			context,
+			<ThemeSetup activeSiteDomain={ context.params.site_id } />
 		);
 	},
 
