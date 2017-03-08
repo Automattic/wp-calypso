06974ec6df621d9ea7ea44aaca37f9e67394cec1
James Tien <southp0105@gmail.com>
2017-03-08 18:36:46 +0800

    Complete the classnames and the toggle

--- client/account-recovery/reset-password/reset-password-confirm-form/index.jsx
+++ client/account-recovery/reset-password/reset-password-confirm-form/index.jsx
@@ -1,49 +1,59 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React from 'react';
 import { localize } from 'i18n-calypso';
+import classNames from 'classnames';
+import { identity } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import Card from 'components/card';
 import FormTextInput from 'components/forms/form-text-input';
-import FormLegend from 'components/forms/form-legend';
+import FormLabel from 'components/forms/form-label';
 import FormButton from 'components/forms/form-button';
 import { STRONG_PASSWORD } from 'lib/url/support';
 
-class ResetPasswordConfirmForm extends Component {
-	render() {
-		const {
-			translate,
-		} = this.props;
+const ResetPasswordConfirmForm = ( props ) => {
+	const {
+		translate,
+		showPassword,
+	} = props;
 
-		return (
-			<Card>
-				<h2 className="reset-password-confirm-form__title">{ translate( 'Reset your password' ) }</h2>
-				<FormLegend>{ translate( 'New password' ) }</FormLegend>
-				<a href="#">{ translate( 'Hide' ) }</a>
-				<FormTextInput />
-				<FormButton className="reset-password-confirm-form__button generate-password-button" isPrimary={ false }>
-					{ translate( 'Generate strong password' ) }
-				</FormButton>
-				<p className="reset-password-confirm-form__description">
-					{ translate(
-						'{{a}}Great passwords{{/a}} use upper and lower case characters, numbers, and symbols like !/"$%&',
-						{
-							components: {
-								a: <a href={ STRONG_PASSWORD } target="_blank" rel="noopener noreferrer" />
-							}
+	return (
+		<Card>
+			<h2 className="reset-password-confirm-form__title">{ translate( 'Reset your password' ) }</h2>
+			<FormLabel className="reset-password-confirm-form__text-input-label" htmlFor="password">
+				{ translate( 'New password' ) }
+			</FormLabel>
+			<a className={ classNames( 'reset-password-confirm-form__show-hide-toggle', { show: showPassword } ) } href="#">
+				{ showPassword ? translate( 'Show' ) : translate( 'Hide' ) }
+			</a>
+			<FormTextInput className="reset-password-confirm-form__password-input-field" id="password" />
+			<FormButton className="reset-password-confirm-form__button generate-password-button" isPrimary={ false }>
+				{ translate( 'Generate strong password' ) }
+			</FormButton>
+			<p className="reset-password-confirm-form__description">
+				{ translate(
+					'{{a}}Great passwords{{/a}} use upper and lower case characters, numbers, and symbols like !/"$%&',
+					{
+						components: {
+							a: <a href={ STRONG_PASSWORD } target="_blank" rel="noopener noreferrer" />
 						}
-					) }
-				</p>
-				<FormButton className="reset-password-confirm-form__button submit">
-					{ translate( 'Reset Password' ) }
-				</FormButton>
-			</Card>
-		);
-	}
-}
+					}
+				) }
+			</p>
+			<FormButton className="reset-password-confirm-form__button submit">
+				{ translate( 'Reset Password' ) }
+			</FormButton>
+		</Card>
+	);
+};
+
+ResetPasswordConfirmForm.defaultProps = {
+	translate: identity,
+	showPassword: false,
+};
 
 export default localize( ResetPasswordConfirmForm );
