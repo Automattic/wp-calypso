f385047678667bb8f9d202b79c1a55e2f6f591f8
Marin Atanasov <tyxla@abv.bg>
2017-03-08 15:57:39 +0200

    Site Settings: Disable SSO card in dev mode

--- client/my-sites/site-settings/sso.jsx
+++ client/my-sites/site-settings/sso.jsx
@@ -13,9 +13,14 @@ import JetpackModuleToggle from 'my-sites/site-settings/jetpack-module-toggle';
 import FormFieldset from 'components/forms/form-fieldset';
 import CompactFormToggle from 'components/forms/form-toggle/compact';
 import { getSelectedSiteId } from 'state/ui/selectors';
-import { isJetpackModuleActive } from 'state/selectors';
+import {
+	isJetpackModuleActive,
+	isJetpackModuleUnavailableInDevelopmentMode,
+	isJetpackSiteInDevelopmentMode
+} from 'state/selectors';
 import InfoPopover from 'components/info-popover';
 import ExternalLink from 'components/external-link';
+import QueryJetpackConnection from 'components/data/query-jetpack-connection';
 
 const Sso = ( {
 	fields,
@@ -24,10 +29,13 @@ const Sso = ( {
 	isSavingSettings,
 	selectedSiteId,
 	ssoModuleActive,
+	ssoModuleUnavailable,
 	translate
 } ) => {
 	return (
 		<div>
+			<QueryJetpackConnection siteId={ selectedSiteId } />
+
 			<Card className="sso__card site-settings__security-settings">
 				<FormFieldset>
 					<div className="sso__info-link-container site-settings__info-link-container">
@@ -43,13 +51,13 @@ const Sso = ( {
 						moduleSlug="sso"
 						label={ translate( 'Allow log-in using WordPress.com accounts.' ) }
 						description="Use WordPress.com's secure authentication"
-						disabled={ isRequestingSettings || isSavingSettings }
+						disabled={ isRequestingSettings || isSavingSettings || ssoModuleUnavailable }
 					/>
 
 					<div className="sso__module-settings site-settings__child-settings">
 						<CompactFormToggle
 							checked={ !! fields.jetpack_sso_match_by_email }
-							disabled={ isRequestingSettings || isSavingSettings || ! ssoModuleActive }
+							disabled={ isRequestingSettings || isSavingSettings || ! ssoModuleActive || ssoModuleUnavailable }
 							onChange={ handleToggle( 'jetpack_sso_match_by_email' ) }
 						>
 							{ translate( 'Match accounts using email addresses' ) }
@@ -57,7 +65,7 @@ const Sso = ( {
 
 						<CompactFormToggle
 							checked={ !! fields.jetpack_sso_require_two_step }
-							disabled={ isRequestingSettings || isSavingSettings || ! ssoModuleActive }
+							disabled={ isRequestingSettings || isSavingSettings || ! ssoModuleActive || ssoModuleUnavailable }
 							onChange={ handleToggle( 'jetpack_sso_require_two_step' ) }
 						>
 							{ translate( 'Require two step authentication' ) }
@@ -85,10 +93,13 @@ Sso.propTypes = {
 export default connect(
 	( state ) => {
 		const selectedSiteId = getSelectedSiteId( state );
+		const siteInDevMode = isJetpackSiteInDevelopmentMode( state, selectedSiteId );
+		const moduleUnavailableInDevMode = isJetpackModuleUnavailableInDevelopmentMode( state, selectedSiteId, 'sso' );
 
 		return {
 			selectedSiteId,
 			ssoModuleActive: !! isJetpackModuleActive( state, selectedSiteId, 'sso' ),
+			ssoModuleUnavailable: siteInDevMode && moduleUnavailableInDevMode,
 		};
 	}
 )( localize( Sso ) );
