861ff86d3086a375e62693806606f38213c6e3c3
Dan Hauk <hauk.dan@gmail.com>
2017-03-08 15:38:44 -0500

    Add new domain delay notice to default screen

--- client/my-sites/upgrades/domain-management/list/domain-only.jsx
+++ client/my-sites/upgrades/domain-management/list/domain-only.jsx
@@ -11,14 +11,19 @@ import EmptyContent from 'components/empty-content';
 import { domainManagementEdit } from 'my-sites/upgrades/paths';
 
 const DomainOnly = ( { domainName, siteId, translate } ) => (
-	<EmptyContent
-		title={ translate( '%(domainName)s is ready when you are.', { args: { domainName } } ) }
-		line={ translate( 'Start a site now to unlock everything WordPress.com can offer. Or add email and manage your domain preferences.' ) }
-		action={ translate( 'Create Site' ) }
-		actionURL={ `/start/site-selected/?siteSlug=${ encodeURIComponent( domainName ) }&siteId=${ encodeURIComponent( siteId ) }` }
-		secondaryAction={ translate( 'Manage Domain' ) }
-		secondaryActionURL={ domainManagementEdit( domainName, domainName ) }
-		illustration={ '/calypso/images/drake/drake-browser.svg' } />
+	<div>
+		<EmptyContent
+			title={ translate( '%(domainName)s is ready when you are.', { args: { domainName } } ) }
+			line={ translate( 'Start a site now to unlock everything WordPress.com can offer. Or add email and manage your domain preferences.' ) }
+			action={ translate( 'Create Site' ) }
+			actionURL={ `/start/site-selected/?siteSlug=${ encodeURIComponent( domainName ) }&siteId=${ encodeURIComponent( siteId ) }` }
+			secondaryAction={ translate( 'Manage Domain' ) }
+			secondaryActionURL={ domainManagementEdit( domainName, domainName ) }
+			illustration={ '/calypso/images/drake/drake-browser.svg' } />
+		<div className="domain-only-site__settings-notice">
+			{ translate( 'New domains usually start working immediately, but may be unreliable for the first few hours.' ) }
+		</div>
+	</div>
 );
 
 DomainOnly.propTypes = {
