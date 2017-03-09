cf78172f0a1d07696d5606e98ace2256cd2f4a5a
Justin Shreve <justin.shreve@gmail.com>
2017-03-09 15:27:08 -0800

    Hooks the toggle button up to the form so you can show/hide variation options depending on the product type.

--- client/extensions/woocommerce/products/products-form/index.js
+++ client/extensions/woocommerce/products/products-form/index.js
@@ -12,39 +12,52 @@ import FormLabel from 'components/forms/form-label';
 import FormToggle from 'components/forms/form-toggle';
 import ProductVariationTypesForm from '../products-variation-types-form';
 
-class ProductsForm extends Component {
+export default class ProductsForm extends Component {
+
+	constructor( props ) {
+		super(props);
+		this.state = { productType: props.product ? props.product.productType : 'simple' };
+		this.handleToggle = this.handleToggle.bind(this);
+	}
 
 	static propTypes = {
 		product: PropTypes.shape( {
 			id: PropTypes.number.isRequired,
 			name: PropTypes.string.isRequired,
+			productType: PropTypes.string.isRequired,
 		} )
 	};
 
+	handleToggle() {
+		this.setState( ( prevState, props ) => ( {
+			productType: 'variable' === prevState.productType ? 'simple' : 'variable'
+		}  )  );
+	}
+
 	render() {
 		const isNewProduct = this.props.product ? false : true;
+		let variationsForm = null;
+
+		if ( 'variable' === this.state.productType ) {
+			variationsForm = <ProductVariationTypesForm variations={ [ { type: 'Color', values: [ 'Red' ] } ] } />
+		}
+
 		return (
 			<Card>
 				<p>
-				<FormLabel>
-					{ isNewProduct ? i18n.translate( 'Does this product have options like size and color?' )
-						: i18n.translate( 'Does %(productName)s have options like size and color?', {
-							args: {
-								productName: this.props.product.name,
-							}
-						} )
-					}
-				</FormLabel>
-				<FormToggle />
+				<FormToggle onChange={ this.handleToggle } checked={ 'variable' === this.state.productType }>{ isNewProduct ? i18n.translate( 'Does this product have options like size and color?' )
+					: i18n.translate( 'Does %(productName)s have options like size and color?', {
+						args: {
+							productName: this.props.product.name,
+						}
+					} )
+				}</FormToggle>
 				</p>
 
-				<ProductVariationTypesForm
-					variations={ [ { type: 'Color', values: [ 'Red' ] } ] }
-				/>
+
+				{variationsForm}
 			</Card>
 		);
 	}
 
 }
-
-export default ProductsForm;
