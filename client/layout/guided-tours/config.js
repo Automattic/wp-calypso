92dac54a7a218c0bb4b60cb5bc58e4606254bb1c
Matt Wiebe <wiebe@automattic.com>
2017-03-09 15:45:23 -0600

    initial menu removal tour code

--- client/layout/guided-tours/config.js
+++ client/layout/guided-tours/config.js
@@ -5,9 +5,11 @@ import { combineTours } from 'layout/guided-tours/config-elements';
 import { MainTour } from 'layout/guided-tours/tours/main-tour';
 import { TutorialSitePreviewTour } from 'layout/guided-tours/tours/tutorial-site-preview-tour';
 import { GDocsIntegrationTour } from 'layout/guided-tours/tours/gdocs-integration-tour';
+import { MenuRemovalTour } from 'layout/guided-tours/tours/menu-removal-tour';
 
 export default combineTours( {
 	main: MainTour,
 	tutorialSitePreview: TutorialSitePreviewTour,
 	gdocsIntegrationTour: GDocsIntegrationTour,
+	menuRemoval: MenuRemovalTour,
 } );
