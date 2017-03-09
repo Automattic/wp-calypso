1b3d19701f7066444ba367051c7456a5bf0dbfd6
budzanowski <bartosz.budzanowski@gmail.com>
2017-03-10 00:53:58 +0100

    Create custom receive method for themes query manager.

--- client/lib/query-manager/theme/index.js
+++ client/lib/query-manager/theme/index.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import cloneDeep from 'lodash/cloneDeep';
+
+/**
  * Internal dependencies
  */
 import PaginatedQueryManager from '../paginated';
@@ -9,6 +14,33 @@ import { DEFAULT_THEME_QUERY } from './constants';
  * ThemeQueryManager manages themes which can be queried
  */
 export default class ThemeQueryManager extends PaginatedQueryManager {
+
+	receive( items, options = {} ) {
+		const oldQueries = cloneDeep( this.data.queries );
+		const nextManager = super.receive( items, options );
+		const queryKey = this.constructor.QueryKey.stringify( options.query );
+		const nextQuery = nextManager.data.queries[ queryKey ];
+
+		if ( ! options.query ) {
+			return this.constructor(
+				Object.assign( {}, {
+					items: nextManager.data.items,
+					queries: Object.assign( {}, oldQueries )
+				} ),
+				nextManager.options
+			);
+		}
+
+		return this.constructor(
+			Object.assign( {}, {
+				items: nextManager.data.items,
+				queries: Object.assign( {}, oldQueries, {
+					[ queryKey ]: nextQuery
+				} )
+			} ),
+			nextManager.options
+		);
+	}
 
 	/**
 	 * A sorting function that defines the sort order of items under
