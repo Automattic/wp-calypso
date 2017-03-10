2f0f7afa90f9cc81268e21545fa2fe9b0a3348af
Ben Lowery <ben.lowery@automattic.com>
2017-03-10 08:16:04 -0500

    Loader: Clean up the chunk loader
    Save a ref to the chunk name and hash maps.
    Use those refs instead of repeating them over and over again.

--- server/bundler/plugin.js
+++ server/bundler/plugin.js
@@ -6,6 +6,10 @@ ChunkFileNames.prototype.apply = function( compiler ) {
 		compilation.mainTemplate.plugin("require-ensure", function( _, chunk ) {
 			var chunkMaps = chunk.getChunkMaps();
 			return this.asString( [
+				"var chunkNames = " + JSON.stringify( chunkMaps.name, null, '  ' ) + ";",
+				"var chunkHashes = " + JSON.stringify( chunkMaps.hash, null, '  ' ) + ";",
+				"var chunkName = chunkNames[ chunkId ] || chunkId;",
+				"var chunkHash = chunkHashes[ chunkId ] || chunkId;",
 				"// \"0\" is the signal for \"already loaded\"",
 				"if ( installedChunks[ chunkId ] === 0 ) {",
 				this.indent("return callback.call( null, " + this.requireFn + " );"),
@@ -18,8 +22,8 @@ ChunkFileNames.prototype.apply = function( compiler ) {
 					"// start chunk loading",
 					"installedChunks[ chunkId ] = [ callback ];",
 					"window.__chunkErrors = window.__chunkErrors || {};",
-					"window.__chunkErrors[ " + JSON.stringify( chunkMaps.name ) + "[chunkId]||chunkId ]=null;",
-					"var head = document.getElementsByTagName('head')[0];",
+					"window.__chunkErrors[ chunkName ]=null;",
+					"var head = document.getElementsByTagName( 'head' )[ 0 ];",
 					"var script = document.createElement( 'script' );",
 					"var isDebug = window.app.isDebug;",
 					"script.type = 'text/javascript';",
@@ -29,11 +33,11 @@ ChunkFileNames.prototype.apply = function( compiler ) {
 					this.indent( [
 						"script.onerror = script.onload = script.onreadystatechange = null;",
 						"delete installedChunks[ chunkId ];",
-						"window.__chunkErrors[ " + JSON.stringify( chunkMaps.name ) + "[chunkId]||chunkId ]=new Error();",
+						"window.__chunkErrors[ chunkName ] = new Error( 'could not load ' + chunkName );",
 						"callback.call( null, " + this.requireFn + ")"
 					] ),
 					"};",
-					"script.src = " + this.requireFn + ".p + (" + JSON.stringify( chunkMaps.name ) + "[chunkId]||chunkId) + '.' + (" + JSON.stringify( chunkMaps.hash ) + "[chunkId]||chunkID) + ( isDebug ? '' : '.m' ) + '.js';",
+					"script.src = " + this.requireFn + ".p + ( chunkName ) + '.' + ( chunkHash ) + ( isDebug ? '' : '.m' ) + '.js';",
 					"head.appendChild( script );"
 				] ),
 				"}"
