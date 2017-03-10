3d3b59e469c791f578cac44262cef6319f2b4751
Ben Lowery <ben.lowery@automattic.com>
2017-03-10 09:38:27 -0500

    Pull out the chunk error stuff, which doesn't work. Replace with a retry on each failed chunk and then a retry on the whole app if the chunk retry fails. If chunk loading for the retry fails, emit an error and give up.

--- server/bundler/plugin.js
+++ server/bundler/plugin.js
@@ -19,26 +19,45 @@ ChunkFileNames.prototype.apply = function( compiler ) {
 				this.indent( "installedChunks[ chunkId ].push( callback );" ),
 				" } else { ",
 				this.indent( [
+					"function __loadChunk( src ) {",
+					this.indent( [
+						"var head = document.getElementsByTagName( 'head' )[ 0 ];",
+						"var script = document.createElement( 'script' );",
+						"script.type = 'text/javascript';",
+						"script.charset = 'utf-8';",
+						"script.async = true;",
+						"script.onerror = function() {",
+
+						this.indent( [
+							// try to reload the script once
+							"if( src.indexOf( 'retry=1' ) === -1 ) {",
+							this.indent( [
+								"__loadChunk( src + '?retry=1' );",
+								"return;"
+							] ),
+							"}",
+							// attempt to reload the entire app if we hit a chunk loading error
+							// a chunk loading error typically means that the application has moved on since it was loaded and
+							// the hashes of dependent chunks have changed. We have to reload to pick up the new manifest
+							// and the new set of hashes
+							"if ( window.location.search.indexOf( 'retry=1' ) === -1 ) { ",
+							this.indent( [
+								"window.location.search += ( window.location.search ? '&retry=1' : 'retry=1' );",
+								"return;"
+							] ),
+							"}",
+							// what should we do here? It appears we can't actually load the app.
+							"console && console.error( 'Unable to load ' + chunkName + ' from ' + src );"
+						] ),
+						"};",
+						"script.src = src",
+						"head.appendChild( script );"
+					] ),
+					"}",
 					"// start chunk loading",
 					"installedChunks[ chunkId ] = [ callback ];",
-					"window.__chunkErrors = window.__chunkErrors || {};",
-					"window.__chunkErrors[ chunkName ]=null;",
-					"var head = document.getElementsByTagName( 'head' )[ 0 ];",
-					"var script = document.createElement( 'script' );",
-					"var isDebug = window.app.isDebug;",
-					"script.type = 'text/javascript';",
-					"script.charset = 'utf-8';",
-					"script.async = true;",
-					"script.onerror = function() {",
-					this.indent( [
-						"script.onerror = script.onload = script.onreadystatechange = null;",
-						"delete installedChunks[ chunkId ];",
-						"window.__chunkErrors[ chunkName ] = new Error( 'could not load ' + chunkName );",
-						"callback.call( null, " + this.requireFn + ")"
-					] ),
-					"};",
-					"script.src = " + this.requireFn + ".p + ( chunkName ) + '.' + ( chunkHash ) + ( isDebug ? '' : '.m' ) + '.js';",
-					"head.appendChild( script );"
+					"var src = " + this.requireFn + ".p + ( chunkName ) + '.' + ( chunkHash ) + ( window.app.isDebug ? '' : '.m' ) + '.js';",
+					"__loadChunk( src )"
 				] ),
 				"}"
 			] );
