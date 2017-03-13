a8d3a433dd6cd3f6c855c5d8d5d098685521c300
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-03-13 13:30:37 -0700

    Update the wording, plans and string translations

--- client/blocks/disconnect-jetpack/docs/example.jsx
+++ client/blocks/disconnect-jetpack/docs/example.jsx
@@ -15,26 +15,78 @@ import { getCurrentUser } from 'state/current-user/selectors';
 class DisconnectJetpackExample extends Component {
 	constructor( props ) {
 		super( props );
-		this.toggleVisibility = this.toggleVisibility.bind( this );
+		this.toggleVisibilityFree = this.toggleVisibility.bind( this, 'free' );
+		this.toggleVisibilityPersonal = this.toggleVisibility.bind( this, 'personal' );
+		this.toggleVisibilityPremium = this.toggleVisibility.bind( this, 'premium' );
+		this.toggleVisibilityProfessional = this.toggleVisibility.bind( this, 'professional' );
+		this.toggleVisibilityBroken = this.toggleVisibility.bind( this, 'broken' );
+
 		this.state = {
-			isVisible: false,
+			isVisible : {
+				free: false,
+				personal: false,
+				premium: false,
+				professional: false,
+				broken: false
+			}
 		};
 	}
 
-	toggleVisibility() {
-		this.setState( { 'isVisible': ! this.state.isVisible } );
+	toggleVisibility( type ) {
+		this.state.isVisible[ type ] = !this.state.isVisible[ type ];
+		this.setState( { 'isVisible': this.state.isVisible } );
 	}
 
 	render() {
 		return (
 			<Card>
-				<Button onClick={ this.toggleVisibility } >Disconnect Jetpack </Button>
+
+				<p><Button onClick={ this.toggleVisibilityFree } >Disconnect Free Jetpack </Button></p>
 				<DisconnectJetpack
-					isVisible={ this.state.isVisible }
+					isVisible={ this.state.isVisible.free }
 					siteId={ this.props.primarySiteId }
-					onStay={ this.toggleVisibility }
-					onDisconnect={ this.toggleVisibility }
+					onStay={ this.toggleVisibilityFree }
+					onDisconnect={ this.toggleVisibilityFree }
+					plan='free'
 				/>
+
+				<p><Button onClick={ this.toggleVisibilityPersonal } >Disconnect Personal Jetpack </Button></p>
+				<DisconnectJetpack
+					isVisible={ this.state.isVisible.personal }
+					siteId={ this.props.primarySiteId }
+					onStay={ this.toggleVisibilityPersonal }
+					onDisconnect={ this.toggleVisibilityPersonal }
+					plan='personal'
+				/>
+
+				<p><Button onClick={ this.toggleVisibilityPremium } >Disconnect Premium Jetpack </Button></p>
+				<DisconnectJetpack
+					isVisible={ this.state.isVisible.premium }
+					siteId={ this.props.primarySiteId }
+					onStay={ this.toggleVisibilityPremium }
+					onDisconnect={ this.toggleVisibilityPremium }
+					plan='premium'
+				/>
+
+				<p><Button onClick={ this.toggleVisibilityProfessional } >Disconnect Professional Jetpack </Button></p>
+				<DisconnectJetpack
+					isVisible={ this.state.isVisible.professional }
+					siteId={ this.props.primarySiteId }
+					onStay={ this.toggleVisibilityProfessional }
+					onDisconnect={ this.toggleVisibilityProfessional }
+					plan='professional'
+				/>
+
+				<p><Button onClick={ this.toggleVisibilityBroken } >Disconnect Probably Broken Jetpack </Button></p>
+				<DisconnectJetpack
+					isVisible={ this.state.isVisible.broken }
+					siteId={ this.props.primarySiteId }
+					onStay={ this.toggleVisibilityBroken }
+					onDisconnect={ this.toggleVisibilityBroken }
+					plan='personal'
+					isBroken={ true }
+				/>
+
 			</Card>
 		);
 	}
--- client/blocks/disconnect-jetpack/index.jsx
+++ client/blocks/disconnect-jetpack/index.jsx
@@ -2,37 +2,68 @@
  * External dependencies
  */
 import React, { PropTypes, Component } from 'react';
-import Gridicon from 'gridicons';
 
 /**
  * Internal dependencies
  */
+import { localize } from 'i18n-calypso';
 import Dialog from 'components/dialog';
 import Button from 'components/button';
 
-export default class DisconnectJetpack extends Component {
+class DisconnectJetpack extends Component {
+
+	planFeatures() {
+		const { plan, translate } = this.props;
+		const features = [];
+
+		switch ( plan ) {
+
+			case 'free':
+				features.push( translate( 'Site stats, related content, and sharing tools' ) );
+				features.push( translate( 'Brute force attack protection and uptime monitoring' ) );
+				features.push( translate( 'Unlimited, high-speed image hosting' ) );
+				break;
+
+			case 'personal':
+				features.push( translate( 'Daily, automated backups (unlimited storage)' ) );
+				features.push( translate( 'Priority WordPress and security support' ) );
+				features.push( translate( 'Spam filtering' ) );
+				break;
+
+			case 'premium':
+				features.push( translate( 'Daily, automated backups (unlimited storage)' ) );
+				features.push( translate( 'Daily, automated malware scanning' ) );
+				features.push( translate( 'Priority WordPress and security support' ) );
+				features.push( translate( '13Gb of high-speed video hosting' ) );
+				break;
+
+			case 'professional':
+				features.push( translate( 'Daily, automated backups (unlimited storage)' ) );
+				features.push( translate( 'Daily, automated malware scanning with automated resolution' ) );
+				features.push( translate( 'Priority WordPress and security support' ) );
+				features.push( translate( 'Unlimited high-speed video hosting' ) );
+				features.push( translate( 'SEO preview tools' ) );
+				break;
+		}
+
+		return features.map( ( freature, index ) => {
+			return <div key={ 'disconnect-jetpack__feature-' + index } className="disconnect-jetpack__feature">{ freature } </div>;
+		} );
+	}
 	render() {
+		const { onStay, onDisconnect, isVisible, translate } = this.props;
 		return (
-			<Dialog isVisible={ this.props.isVisible } baseClassName="disconnect-jetpack__dialog" >
-				<h1>Disconnect Jetpack</h1>
-				<p className="disconnect-jetpack__highlight">You are about to disconnect Jetpack and WordPress.com from your website.</p>
-				<p className="disconnect-jetpack__info">If you disconnect, you will experience the following changes:</p>
+			<Dialog isVisible={ isVisible } baseClassName="disconnect-jetpack__dialog" >
+				<h1>{ translate( 'Disconnect Jetpack?' ) }</h1>
+				<p className="disconnect-jetpack__highlight">{ translate( 'By disconnecting Jetpack you will lose these services:' ) }</p>
 
-			<div className="disconnect-jetpack__feature">
-				<Gridicon icon="globe" />Your custom domain will no longer work
-			</div>
-			<div className="disconnect-jetpack__feature">
-				<Gridicon icon="notice" />Your <a href="#" >premium plan</a> benefits won't be available
-			</div>
-			<div className="disconnect-jetpack__feature">
-				<Gridicon icon="user" />Your team won't be able to manage the site using WordPress.com
-			</div>
+				{ this.planFeatures() }
 
-			<div className="disconnect-jetpack__button-wrap">
-				<Button onClick={ this.props.onStay }>Stay Connected</Button>
-				<Button primary scary onClick={ this.props.onDisconnect }>Disconnect</Button>
-			</div>
-			<a className="disconnect-jetpack__more-info-link" href="#">Read More about Jetpack benefits</a>
+				<div className="disconnect-jetpack__button-wrap">
+					<Button onClick={ onStay }>{ translate( 'Stay Connected' ) }</Button>
+					<Button primary scary onClick={ onDisconnect }>{ translate( 'Disconnect' ) }</Button>
+				</div>
+				<a className="disconnect-jetpack__more-info-link" href="#">{ translate( 'Read More about Jetpack benefits' ) }</a>
 			</Dialog>
 		);
 	}
@@ -44,4 +75,8 @@ DisconnectJetpack.propTypes = {
 	isVisible: PropTypes.bool,
 	onDisconnect: PropTypes.func,
 	onStay: PropTypes.func,
+	plan: PropTypes.string,
+	isBroken: PropTypes.bool,
 };
+
+export default localize( DisconnectJetpack );
