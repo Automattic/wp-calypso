5988466ec36521b857d11c5e775cfc97bb00bf1a
Steve Seear <steve.seear@gmail.com>
2017-03-13 15:28:22 +0000

    Remove unnecessary check

--- client/state/themes/reducer.js
+++ client/state/themes/reducer.js
@@ -309,14 +309,7 @@ export const queries = ( () => {
 
 	return createReducer( {}, {
 		[ THEMES_REQUEST_SUCCESS ]: ( state, { siteId, query, themes, found } ) => {
-			return applyToManager(
-				state,
-				siteId,
-				'receive',
-				true,
-				themes,
-				query ? { query, found } : {}
-			);
+			return applyToManager( state, siteId, 'receive', true, themes, { query, found } );
 		},
 		[ THEME_DELETE_SUCCESS ]: ( state, { siteId, themeId } ) => {
 			return applyToManager( state, siteId, 'removeItem', false, themeId );
