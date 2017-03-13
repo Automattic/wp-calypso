a0b186b945191092051ba8739e4ce0baa757ec3c
Steve Seear <steve.seear@gmail.com>
2017-03-13 12:26:34 +0000

    Fall back to generic error

--- client/state/notices/middleware.js
+++ client/state/notices/middleware.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { translate } from 'i18n-calypso';
-import { truncate } from 'lodash';
+import { truncate, includes } from 'lodash';
 
 /**
  * Internal dependencies
@@ -184,6 +184,13 @@ const onThemeDeleteFailure = ( dispatch, { themeId } ) => dispatch(
 	} ) )
 );
 
+const onThemeActivateFailure = ( dispatch, { error } ) => {
+	if ( includes( error.error, 'theme_not_found' ) ) {
+		return dispatch( errorNotice( translate( 'Theme not yet available for this site' ) ) );
+	}
+	return dispatch( errorNotice( translate( 'Unable to activate theme. Contact support.' ) ) );
+};
+
 /**
  * Handler action type mapping
  */
@@ -229,7 +236,7 @@ export const handlers = {
 	[ SITE_FRONT_PAGE_SET_FAILURE ]: dispatchError( translate( 'An error occurred while setting the homepage' ) ),
 	[ THEME_DELETE_FAILURE ]: onThemeDeleteFailure,
 	[ THEME_DELETE_SUCCESS ]: onThemeDeleteSuccess,
-	[ THEME_ACTIVATE_REQUEST_FAILURE ]: dispatchError( translate( 'Theme not yet available for this site' ) ),
+	[ THEME_ACTIVATE_REQUEST_FAILURE ]: onThemeActivateFailure,
 };
 
 /**
