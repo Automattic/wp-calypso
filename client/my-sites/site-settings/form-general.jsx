657c487aef210d0ed4b963293ae8d35ace2123f9
Jacopo Tomasone <Copons@users.noreply.github.com>
2017-03-14 11:54:05 +0000

    Site Settings: add date time format sub-page (#11259)
    Add a General Settings sub-page for Date Format, Time Format, Week Starts On.

--- client/my-sites/site-settings/form-general.jsx
+++ client/my-sites/site-settings/form-general.jsx
@@ -5,7 +5,6 @@ import React, { Component } from 'react';
 import page from 'page';
 import classNames from 'classnames';
 import Gridicon from 'gridicons';
-import { includes, startsWith } from 'lodash';
 
 /**
  * Internal dependencies
@@ -24,7 +23,6 @@ import FormFieldset from 'components/forms/form-fieldset';
 import FormLegend from 'components/forms/form-legend';
 import FormLabel from 'components/forms/form-label';
 import FormRadio from 'components/forms/form-radio';
-import FormSelect from 'components/forms/form-select';
 import FormToggle from 'components/forms/form-toggle';
 import CompactFormToggle from 'components/forms/form-toggle/compact';
 import FormSettingExplanation from 'components/forms/form-setting-explanation';
@@ -37,7 +35,7 @@ import { isBusiness } from 'lib/products-values';
 import { FEATURE_NO_BRANDING } from 'lib/plans/constants';
 import QuerySiteSettings from 'components/data/query-site-settings';
 import { phpToMomentDatetimeFormat } from 'lib/formatting';
-import ExternalLink from 'components/external-link';
+import { getLocalizedDate } from './date-time-format/utils';
 
 class SiteSettingsFormGeneral extends Component {
 	componentWillMount() {
@@ -422,193 +420,48 @@ class SiteSettingsFormGeneral extends Component {
 		);
 	}
 
-	dateFormatOption() {
+	dateTimeFormat() {
 		if ( ! config.isEnabled( 'manage/site-settings/date-time-format' ) ) {
 			return null;
 		}
 
 		const {
-			fields: { date_format, timezone_string },
-			handleRadio,
-			isRequestingSettings,
+			fields: {
+				date_format: dateFormat,
+				start_of_week: startOfWeek,
+				time_format: timeFormat,
+				timezone_string: timezoneString,
+			},
 			moment,
-			onChangeField,
+			site,
 			translate,
 		} = this.props;
 
-		const defaultFormats = [ 'F j, Y', 'Y-m-d', 'm/d/Y', 'd/m/Y' ];
-		const isCustomFormat = ! includes( defaultFormats, date_format );
-		const today = startsWith( timezone_string, 'UTC' )
-			? moment().utcOffset( timezone_string.substring( 3 ) * 60 )
-			: moment.tz( timezone_string );
-
-		const customFieldClasses = classNames(
-			'site-settings__date-time-format-custom',
-			{ 'is-custom': isCustomFormat }
-		);
+		const localizedDate = getLocalizedDate( timezoneString );
+		const weekday = startOfWeek
+			? moment.weekdays( parseInt( startOfWeek, 10 ) )
+			: moment.weekdays( 0 );
 
 		return (
-			<FormFieldset>
-				<FormLabel>
-					{ translate( 'Date Format' ) }
-				</FormLabel>
-				{ defaultFormats.map( ( format, key ) =>
-					<FormLabel key={ key }>
-						<FormRadio
-							checked={ format === date_format }
-							disabled={ isRequestingSettings }
-							name="date_format"
-							onChange={ handleRadio }
-							value={ format }
-						/>
-						<span>{ today.format( phpToMomentDatetimeFormat( format ) ) }</span>
-					</FormLabel>
-				) }
-				<FormLabel className={ customFieldClasses }>
-					<FormRadio
-						checked={ isCustomFormat }
-						disabled={ isRequestingSettings }
-						name="date_format"
-						onChange={ handleRadio }
-						value={ date_format }
-					/>
-					<span>
-						{ translate( 'Custom' ) }
-						<FormInput
-							disabled={ isRequestingSettings }
-							name="date_format_custom"
-							onChange={ onChangeField( 'date_format' ) }
-							type="text"
-							value={ date_format || '' }
-						/>
-						<span className="site-settings__date-time-format-custom-preview">
-							{ isCustomFormat && date_format
-								? today.format( phpToMomentDatetimeFormat( date_format ) )
-								: ''
-							}
-						</span>
-				</span>
-				</FormLabel>
-			</FormFieldset>
-		);
-	}
-
-	timeFormatOption() {
-		if ( ! config.isEnabled( 'manage/site-settings/date-time-format' ) ) {
-			return null;
-		}
-
-		const {
-			fields: { time_format, timezone_string },
-			handleRadio,
-			isRequestingSettings,
-			moment,
-			onChangeField,
-			translate,
-		} = this.props;
-
-		const defaultFormats = [ 'g:i a', 'g:i A', 'H:i' ];
-		const isCustomFormat = ! includes( defaultFormats, time_format );
-		const today = startsWith( timezone_string, 'UTC' )
-			? moment().utcOffset( timezone_string.substring( 3 ) * 60 )
-			: moment.tz( timezone_string );
-
-		const customFieldClasses = classNames(
-			'site-settings__date-time-format-custom',
-			{ 'is-custom': isCustomFormat }
-		);
-
-		return (
-			<FormFieldset>
-				<FormLabel>
-					{ translate( 'Time Format' ) }
-				</FormLabel>
-				{ defaultFormats.map( ( format, key ) =>
-					<FormLabel key={ key }>
-						<FormRadio
-							checked={ format === time_format }
-							disabled={ isRequestingSettings }
-							name="time_format"
-							onChange={ handleRadio }
-							value={ format }
-						/>
-						<span>{ today.format( phpToMomentDatetimeFormat( format ) ) }</span>
-					</FormLabel>
-				) }
-				<FormLabel className={ customFieldClasses }>
-					<FormRadio
-						checked={ isCustomFormat }
-						disabled={ isRequestingSettings }
-						name="time_format"
-						onChange={ handleRadio }
-						value={ time_format }
-					/>
-					<span>
-						{ translate( 'Custom' ) }
-						<FormInput
-							disabled={ isRequestingSettings }
-							name="time_format_custom"
-							onChange={ onChangeField( 'time_format' ) }
-							type="text"
-							value={ time_format || '' }
-						/>
-						<span className="site-settings__date-time-format-custom-preview">
-							{ isCustomFormat && time_format
-								? today.format( phpToMomentDatetimeFormat( time_format ) )
-								: ''
-							}
-						</span>
-					</span>
-					<FormSettingExplanation>
-						<ExternalLink href="https://codex.wordpress.org/Formatting_Date_and_Time" icon>
-							{ translate( 'Documentation on date and time formatting.' ) }
-						</ExternalLink>
-					</FormSettingExplanation>
-				</FormLabel>
-			</FormFieldset>
-		);
-	}
-
-	startOfWeekOption() {
-		if ( ! config.isEnabled( 'manage/site-settings/date-time-format' ) ) {
-			return null;
-		}
-
-		const {
-			fields: { start_of_week },
-			handleSelect,
-			isRequestingSettings,
-			translate,
-		} = this.props;
-
-		const daysOfWeek = [
-			translate( 'Sunday' ),
-			translate( 'Monday' ),
-			translate( 'Tuesday' ),
-			translate( 'Wednesday' ),
-			translate( 'Thursday' ),
-			translate( 'Friday' ),
-			translate( 'Saturday' ),
-		];
-
-		return (
-			<FormFieldset>
-				<FormLabel>
-					{ translate( 'Week Starts On' ) }
-				</FormLabel>
-				<FormSelect
-					disabled={ isRequestingSettings }
-					name="start_of_week"
-					onChange={ handleSelect }
-					value={ start_of_week || 0 }
-				>
-					{ daysOfWeek.map( ( day, index ) =>
-						<option key={ index } value={ index } >
-							{ day }
-						</option>
-					) }
-				</FormSelect>
-			</FormFieldset>
+			<Card
+				className="site-settings__date-time-format"
+				href={ `/settings/date-time-format/${ site.slug }` }
+			>
+				<h2 className="site-settings__date-time-format-title">
+					{ translate( 'Date and Time Format' ) }
+				</h2>
+				<div className="site-settings__date-time-format-info">
+					{
+						dateFormat &&
+							localizedDate.format( phpToMomentDatetimeFormat( dateFormat ) )
+					} &bull; {
+						timeFormat &&
+							localizedDate.format( phpToMomentDatetimeFormat( timeFormat ) )
+					} &bull; {
+						translate( 'Week starts on %s', { args: weekday } )
+					}
+				</div>
+			</Card>
 		);
 	}
 
@@ -699,12 +552,11 @@ class SiteSettingsFormGeneral extends Component {
 						{ this.blogAddress() }
 						{ this.languageOptions() }
 						{ this.Timezone() }
-						{ this.dateFormatOption() }
-						{ this.timeFormatOption() }
-						{ this.startOfWeekOption() }
 						{ this.holidaySnowOption() }
 					</form>
 				</Card>
+
+				{ this.dateTimeFormat() }
 
 				<SectionHeader label={ translate( 'Privacy' ) }>
 					<Button
