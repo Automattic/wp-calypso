5a51d8cd886afe02136932e57e469b2cf960da84
Miguel Fonseca <miguelcsf@gmail.com>
2017-03-15 16:00:28 +0000

    Editor: Media modal: Disable Insert when loading image via URL (#12000)
    * Media modal: Disable Insert when loading image via URL
    
    * Factor out areMediaActionsDisabled & isTransientPreviewable
    
    * Don't just disable visually, but also click handler

--- client/post-editor/media-modal/index.jsx
+++ client/post-editor/media-modal/index.jsx
@@ -42,6 +42,18 @@ import ImageEditor from 'blocks/image-editor';
 import MediaModalDetail from './detail';
 import { withAnalytics, bumpStat, recordGoogleEvent } from 'state/analytics/actions';
 
+function areMediaActionsDisabled( modalView, mediaItems ) {
+	return some( mediaItems, item =>
+		MediaUtils.isItemBeingUploaded( item ) && (
+			// Transients can't be handled by the editor if they are being
+			// uploaded via an external URL
+			! MediaUtils.isTransientPreviewable( item ) ||
+			MediaUtils.getMimePrefix( item ) !== 'image' ||
+			ModalViews.GALLERY === modalView
+		)
+	);
+}
+
 export class EditorMediaModal extends Component {
 	static propTypes = {
 		visible: React.PropTypes.bool,
@@ -109,24 +121,19 @@ export class EditorMediaModal extends Component {
 		};
 	}
 
-	isDisabled() {
-		return some( this.props.mediaLibrarySelectedItems, function( item ) {
-			const mimePrefix = MediaUtils.getMimePrefix( item );
-			return item.transient && ( mimePrefix !== 'image' || ModalViews.GALLERY === this.props.view );
-		}.bind( this ) );
-	}
-
 	confirmSelection = () => {
 		const { view, mediaLibrarySelectedItems } = this.props;
 
-		let value;
-		if ( mediaLibrarySelectedItems.length ) {
-			value = {
+		if ( areMediaActionsDisabled( view, mediaLibrarySelectedItems ) ) {
+			return;
+		}
+
+		const value = mediaLibrarySelectedItems.length
+			? {
 				type: ModalViews.GALLERY === view ? 'gallery' : 'media',
 				items: mediaLibrarySelectedItems,
 				settings: this.state.gallerySettings
-			};
-		}
+			} : undefined;
 
 		this.props.onClose( value );
 	};
@@ -327,8 +334,8 @@ export class EditorMediaModal extends Component {
 			return;
 		}
 
-		const isDisabled = this.isDisabled();
 		const selectedItems = this.props.mediaLibrarySelectedItems;
+		const isDisabled = areMediaActionsDisabled( this.props.view, selectedItems );
 		const buttons = [
 			{
 				action: 'cancel',
