31c86e0b868ef045ffa01b199db621e140bf34c5
Bernhard Reiter <ockham@raz.or.at>
2017-03-15 17:30:21 +0100

    Themes (Single Site Mode): Drop sites-list dependency, use Redux state instead

--- client/my-sites/themes/single-site.jsx
+++ client/my-sites/themes/single-site.jsx
@@ -11,29 +11,25 @@ import { localize } from 'i18n-calypso';
 import Main from 'components/main';
 import SingleSiteThemeShowcaseWpcom from './single-site-wpcom';
 import SingleSiteThemeShowcaseJetpack from './single-site-jetpack';
-import sitesFactory from 'lib/sites-list';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { isJetpackSite } from 'state/sites/selectors';
 import { isThemeActive } from 'state/themes/selectors';
 import { canCurrentUser } from 'state/selectors';
 
 const SingleSiteThemeShowcaseWithOptions = ( props ) => {
-	const { isJetpack, translate } = props;
-	const sites = sitesFactory();
-	const site = sites.getSelectedSite();
+	const { isJetpack, siteId, translate } = props;
 
 	// If we've only just switched from single to multi-site, there's a chance
 	// this component is still being rendered with site unset, so we need to guard
 	// against that case.
-	if ( ! site ) {
+	if ( ! siteId ) {
 		return <Main className="themes" />;
 	}
 
 	if ( isJetpack ) {
 		return (
 			<SingleSiteThemeShowcaseJetpack { ...props }
-				site={ site }
-				siteId={ site.ID }
+				siteId={ siteId }
 				options={ [
 					'customize',
 					'purchase',
@@ -57,8 +53,7 @@ const SingleSiteThemeShowcaseWithOptions = ( props ) => {
 
 	return (
 		<SingleSiteThemeShowcaseWpcom { ...props }
-			site={ site }
-			siteId={ site.ID }
+			siteId={ siteId }
 			options={ [
 				'customize',
 				'preview',
@@ -81,6 +76,7 @@ export default connect(
 	( state ) => {
 		const selectedSiteId = getSelectedSiteId( state );
 		return {
+			siteId: selectedSiteId,
 			isJetpack: isJetpackSite( state, selectedSiteId ),
 			isCustomizable: canCurrentUser( state, selectedSiteId, 'edit_theme_options' ),
 			getScreenshotOption: ( themeId ) => isThemeActive( state, themeId, selectedSiteId ) ? 'customize' : 'info'
