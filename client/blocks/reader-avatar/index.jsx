46c08de8bd6c6036c71d5b4a53249a9641bfce28
Ben Lowery <blowery@users.noreply.github.com>
2017-03-15 11:14:43 -0400

    Reader: Suppress the default feed icon for dotcom sites (#12138)
    Suppress http://s2.wp.com/i/buttonw-com.png, the default feed icon for wpcom sites. It's currently appearing for sites who have no site icon picked.

--- client/blocks/reader-avatar/index.jsx
+++ client/blocks/reader-avatar/index.jsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import { startsWith } from 'lodash';
+import { startsWith, endsWith } from 'lodash';
 
 /**
  * Internal dependencies
@@ -14,6 +14,10 @@ import classnames from 'classnames';
 
 const ReaderAvatar = ( { author, siteIcon, feedIcon, siteUrl, siteIconSize = 96, preferGravatar = false, showPlaceholder = false } ) => {
 	let fakeSite;
+	// don't show the default favicon for some sites
+	if ( endsWith( feedIcon, 'wp.com/i/buttonw-com.png' ) ) {
+		feedIcon = null;
+	}
 	if ( siteIcon ) {
 		fakeSite = {
 			icon: {
