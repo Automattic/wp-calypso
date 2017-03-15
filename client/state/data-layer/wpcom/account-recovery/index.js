46bb706bef9f651e32b4347779371159a75f6ccf
James Tien <southp0105@gmail.com>
2017-03-15 16:35:40 +0800

    Add action creators and the data layer handler for interacting with the (#12101)
    endpoint `/account-recovery/reset`

--- client/state/data-layer/wpcom/account-recovery/index.js
+++ client/state/data-layer/wpcom/account-recovery/index.js
@@ -3,9 +3,11 @@
  */
 import { mergeHandlers } from 'state/data-layer/utils';
 import requestReset from './request-reset';
+import reset from './reset';
 import validate from './validate';
 
 export default mergeHandlers(
 	requestReset,
+	reset,
 	validate,
 );
