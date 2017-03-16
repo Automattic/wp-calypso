686622b9c05d19d60d0f964df79b1f137abe88bf
artpi <artpi@post.pl>
2017-03-15 23:26:56 -0500

    Move to data poller approach

--- client/state/automated-transfer/middleware.js
+++ client/state/automated-transfer/middleware.js
@@ -6,24 +6,12 @@ import {
 	THEME_TRANSFER_INITIATE_REQUEST,
 	THEME_TRANSFER_INITIATE_SUCCESS,
 } from 'state/action-types';
-import { pauseAll, resumePaused } from 'lib/data-poller'; 
-
-const pauseFetching = () => {
-	pauseAll();
-	const sites = require( 'lib/sites-list' )();
-	sites.pauseFetching();
-};
-
-const resumeFetching = () => {
-	resumePaused();
-	const sites = require( 'lib/sites-list' )();
-	sites.resumeFetching();
-};
+import { pauseAll, resumePaused } from 'lib/data-poller';
 
 export const handlers = {
-	[ THEME_TRANSFER_INITIATE_REQUEST ]: pauseFetching,
-	[ THEME_TRANSFER_INITIATE_FAILURE ]: resumeFetching,
-	[ THEME_TRANSFER_INITIATE_SUCCESS ]: resumeFetching
+	[ THEME_TRANSFER_INITIATE_REQUEST ]: pauseAll,
+	[ THEME_TRANSFER_INITIATE_FAILURE ]: resumePaused,
+	[ THEME_TRANSFER_INITIATE_SUCCESS ]: resumePaused
 };
 
 /**
