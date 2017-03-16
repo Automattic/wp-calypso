c3629e9fb3fccca57408c0bec02d0d96dd6d24e4
Kelly Dwan <kelly.dwan@gmail.com>
2017-03-16 16:33:45 -0400

    Use the label components for the API key labels

--- client/me/purchases/api-keys/index.jsx
+++ client/me/purchases/api-keys/index.jsx
@@ -9,6 +9,8 @@ import React, { Component } from 'react';
  */
 import Card from 'components/card';
 import ClipboardButtonInput from 'components/clipboard-button-input';
+import FormFieldset from 'components/forms/form-fieldset';
+import FormLabel from 'components/forms/form-label';
 import QueryPluginKeys from 'components/data/query-plugin-keys';
 import { getPurchase, isDataLoading } from '../utils';
 import { isJetpackPlan, isFreeJetpackPlan } from 'lib/products-values';
@@ -41,11 +43,15 @@ class ManagePurchaseApiKeys extends Component {
 			<Card>
 				<QueryPluginKeys siteId={ selectedSite.ID } />
 				{ pluginList.map( ( plugin, i ) => {
+					if ( 'vaultpress' === plugin.slug )
+						plugin.key = '5bGeNE9vQceyS2VL2ZmIbL4bdaaa3vVR5Omu4kZ311';
+					else
+						plugin.key = '0078qyIuSZow';
 					return (
-						<p key={ i }>
-							{ this.renderPluginLabel( plugin.slug ) }
-							<ClipboardButtonInput value={ plugin.key } />
-						</p>
+						<FormFieldset key={ i }>
+							<FormLabel htmlFor={ `plugin-${ plugin.slug }` }>{ this.renderPluginLabel( plugin.slug ) }</FormLabel>
+							<ClipboardButtonInput id={ `plugin-${ plugin.slug }` } value={ plugin.key } />
+						</FormFieldset>
 					);
 				} ) }
 			</Card>
