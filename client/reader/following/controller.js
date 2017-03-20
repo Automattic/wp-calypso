ae4fe7c256521bef749842ef941ccd72eda09c07
bluefuton <chris@bluefuton.com>
2017-03-20 19:17:03 +0000

    Add route for Manage Following refresh at /following/manage

--- client/reader/following/controller.js
+++ client/reader/following/controller.js
@@ -38,5 +38,29 @@ export default {
 			document.getElementById( 'primary' ),
 			context.store
 		);
+	},
+
+	followingManage( context ) {
+		const basePath = route.sectionify( context.path ),
+			fullAnalyticsPageTitle = analyticsPageTitle + ' > Manage Followed Sites',
+			mcKey = 'following_edit',
+			search = context.query.s;
+
+		setPageTitle( context, i18n.translate( 'Manage Followed Sites' ) );
+
+		trackPageLoad( basePath, fullAnalyticsPageTitle, mcKey );
+
+		renderWithReduxStore(
+			<AsyncLoad
+				require="reader/following-manage"
+				key="following-manage"
+				initialFollowUrl={ context.query.follow }
+				search={ search }
+				context={ context }
+				userSettings={ userSettings }
+			/>,
+			document.getElementById( 'primary' ),
+			context.store
+		);
 	}
 };
