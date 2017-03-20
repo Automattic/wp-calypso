90cd3e80d5e8ca0203244bf8ae9a5903d8692ac8
Marin Atanasov <tyxla@users.noreply.github.com>
2017-03-20 16:07:47 +0200

    Settings: Redirect SEO and Analytics to Traffic (#12309)
    * Settings: Redirect SEO and Analytics to Traffic
    
    * Site Settings: Add legacy SEO and Analytics paths to section

--- client/my-sites/site-settings/index.js
+++ client/my-sites/site-settings/index.js
@@ -15,7 +15,6 @@ module.exports = function() {
 	page( '/settings/general/:site_id', controller.siteSelection, controller.navigation, settingsController.setScroll, settingsController.siteSettings );
 	page( '/settings/writing/:site_id', controller.siteSelection, controller.navigation, settingsController.setScroll, settingsController.siteSettings );
 	page( '/settings/discussion/:site_id', controller.siteSelection, controller.navigation, settingsController.setScroll, settingsController.siteSettings );
-	page( '/settings/analytics/:site_id', controller.siteSelection, controller.navigation, settingsController.setScroll, settingsController.siteSettings );
 	page( '/settings/security/:site_id', controller.siteSelection, controller.navigation, settingsController.setScroll, settingsController.siteSettings );
 
 	page( '/settings/import/:site_id', controller.siteSelection, controller.navigation, settingsController.importSite );
