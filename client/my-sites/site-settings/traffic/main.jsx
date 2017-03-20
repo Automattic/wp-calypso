2f044dd5d9f1c2d62858cb496c8a1fbce7635bc9
Marin Atanasov <tyxla@users.noreply.github.com>
2017-03-20 14:01:56 +0200

    Site Settings: Move Related Posts to Traffic section (#12271)

--- client/my-sites/site-settings/traffic/main.jsx
+++ client/my-sites/site-settings/traffic/main.jsx
@@ -3,6 +3,7 @@
  */
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { flowRight, partialRight, pick } from 'lodash';
 
 /**
  * Internal dependencies
@@ -13,9 +14,16 @@ import SiteSettingsNavigation from 'my-sites/site-settings/navigation';
 import SeoSettingsMain from 'my-sites/site-settings/seo-settings/main';
 import SeoSettingsHelpCard from 'my-sites/site-settings/seo-settings/help';
 import AnalyticsSettings from 'my-sites/site-settings/form-analytics';
+import RelatedPosts from 'my-sites/site-settings/related-posts';
+import wrapSettingsForm from 'my-sites/site-settings/wrap-settings-form';
 import { getSelectedSite } from 'state/ui/selectors';
 
 const SiteSettingsTraffic = ( {
+	fields,
+	handleAutosavingToggle,
+	handleSubmitForm,
+	isRequestingSettings,
+	isSavingSettings,
 	site,
 	sites,
 	upgradeToBusiness
@@ -25,6 +33,13 @@ const SiteSettingsTraffic = ( {
 		<SiteSettingsNavigation site={ site } section="traffic" />
 
 		<SeoSettingsHelpCard />
+		<RelatedPosts
+			onSubmitForm={ handleSubmitForm }
+			handleAutosavingToggle={ handleAutosavingToggle }
+			isSavingSettings={ isSavingSettings }
+			isRequestingSettings={ isRequestingSettings }
+			fields={ fields }
+		/>
 		<AnalyticsSettings />
 		<SeoSettingsMain sites={ sites } upgradeToBusiness={ upgradeToBusiness } />
 	</Main>
@@ -35,8 +50,20 @@ SiteSettingsTraffic.propTypes = {
 	upgradeToBusiness: PropTypes.func.isRequired,
 };
 
-export default connect(
+const connectComponent = connect(
 	( state ) => ( {
 		site: getSelectedSite( state ),
 	} )
+);
+
+const getFormSettings = partialRight( pick, [
+	'jetpack_relatedposts_allowed',
+	'jetpack_relatedposts_enabled',
+	'jetpack_relatedposts_show_headline',
+	'jetpack_relatedposts_show_thumbnails',
+] );
+
+export default flowRight(
+	connectComponent,
+	wrapSettingsForm( getFormSettings )
 )( SiteSettingsTraffic );
