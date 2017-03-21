b9ece6c8f017ffc6573d6847011c7fced92be06a
Chris Rosser <chris@bluefuton.com>
2017-03-21 17:30:04 +0000

    Add route for Manage Following refresh at /following/manage (#12335)

--- client/reader/following/index.js
+++ client/reader/following/index.js
@@ -8,8 +8,12 @@ import page from 'page';
  */
 import controller from './controller';
 import readerController from 'reader/controller';
+import config from 'config';
 
 export default function() {
 	page( '/following/*', readerController.loadSubscriptions, readerController.initAbTests );
 	page( '/following/edit', readerController.updateLastRoute, readerController.sidebar, controller.followingEdit );
+	if ( config.isEnabled( 'reader/following-manage-refresh' ) ) {
+		page( '/following/manage', readerController.updateLastRoute, readerController.sidebar, controller.followingManage );
+	}
 }
