8e2bfb189d0ff6d15034b35a032c696e71f9d273
retrofox <rdsuarez@gmail.com>
2017-03-21 15:08:43 -0500

    state: add connection and sharing actions publicize selectors

---
+++ client/state/selectors/get-site-post-publicize-sharing-actions.js
@@ -0,0 +1,48 @@
+/**
+ * External dependencies
+ */
+import { get, map } from 'lodash';
+import { moment } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import { getPublicizeConnection } from 'state/selectors/';
+
+/**
+ * Return a sharing-actions array propagaring data from publicize connections,
+ * creating a Date (moment) instance for the shareDate, etc.
+ *
+ * @param {Object} state Global state tree
+ * @param {Number} siteId Site ID
+ * @param {Number} postId Post ID
+ * @return {Object} sharing actions array
+ */
+export default function getSitePostPublicizeSharingActions( state, siteId, postId ) {
+	const rawActions = get( state, [ 'sharing', 'publicize', 'sharePostActions', 'items', siteId, postId ], null );
+
+	return map( rawActions, ( {
+		ID,
+		connection_id,
+		message,
+		result,
+		share_date,
+		status,
+		url,
+	} ) => {
+		const connection = getPublicizeConnection( state, connection_id );
+
+		return {
+			ID,
+			connectionId: get( connection, 'ID' ),
+			connection,
+			externalName: get( connection, 'external_name' ),
+			message,
+			result,
+			service: get( connection, 'service', 'twitter' ),
+			shareDate: moment( share_date ).format( 'llll' ),
+			status,
+			url,
+		};
+	} );
+}
