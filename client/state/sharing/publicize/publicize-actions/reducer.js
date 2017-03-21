f1c293dac671753e39dc94e94db3ec09f4de8c99
artpi <artpi@post.pl>
2017-03-21 11:41:27 -0500

    Redux edit and delete actions, some fixes

--- client/state/sharing/publicize/publicize-actions/reducer.js
+++ client/state/sharing/publicize/publicize-actions/reducer.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { combineReducers } from 'redux';
-// import { keyBy, omit, omitBy } from 'lodash';
+import { omit } from 'lodash';
 
 /**
  * Internal dependencies
@@ -11,28 +11,63 @@ import {
 	PUBLICIZE_SHARE_ACTIONS_REQUEST,
 	PUBLICIZE_SHARE_ACTIONS_REQUEST_SUCCESS,
 	PUBLICIZE_SHARE_ACTIONS_REQUEST_FAILURE,
-	// PUBLICIZE_SHARE_ACTION_DELETE,
-	// PUBLICIZE_SHARE_ACTION_DELETE_SUCCESS,
-	// PUBLICIZE_SHARE_ACTION_DELETE_FAILURE,
-	// PUBLICIZE_SHARE_ACTION_EDIT,
-	// PUBLICIZE_SHARE_ACTION_EDIT_SUCCESS,
-	// PUBLICIZE_SHARE_ACTION_EDIT_FAILURE,
+	PUBLICIZE_SHARE_ACTION_DELETE,
+	PUBLICIZE_SHARE_ACTION_DELETE_SUCCESS,
+	PUBLICIZE_SHARE_ACTION_DELETE_FAILURE,
+	PUBLICIZE_SHARE_ACTION_EDIT,
+	PUBLICIZE_SHARE_ACTION_EDIT_SUCCESS,
+	PUBLICIZE_SHARE_ACTION_EDIT_FAILURE,
 } from 'state/action-types';
 import { publicizeActionsSchema } from './schema';
 import { createReducer } from 'state/utils';
 
-export const sharePostActions = createReducer( {}, {
-	[ PUBLICIZE_SHARE_ACTIONS_REQUEST_SUCCESS ]: ( state, { siteId, postId, actions } ) => ( { ...state, [ siteId ]: { ...state[ siteId ], [ postId ]: actions } } ),
+function updateDataForPost( newValue, state, siteId, postId, actionId = undefined ) {
+	if ( actionId !== undefined ) {
+		newValue = {
+			...state[ siteId ][ postId ],
+			newValue
+		};
+	}
+	return (
+		{
+			...state,
+			[ siteId ]: {
+				...state[ siteId ],
+				[ postId ]: newValue
+			}
+		}
+	);
+}
+
+export const items = createReducer( {}, {
+	[ PUBLICIZE_SHARE_ACTIONS_REQUEST_SUCCESS ]: ( state, { siteId, postId, actions } ) => updateDataForPost( actions, state, siteId, postId ),
+	[ PUBLICIZE_SHARE_ACTION_DELETE_SUCCESS ]: ( state, { siteId, postId, actionId } ) => updateDataForPost( omit( state[ siteId ][ postId ], [ actionId ] ), state, siteId, postId ),
+	[ PUBLICIZE_SHARE_ACTION_EDIT_SUCCESS ]: ( state, { siteId, postId, action } ) => updateDataForPost( action, state, siteId, postId, action.ID ),
+
 }, publicizeActionsSchema );
 
 export const fetchingSharePostActions = createReducer( {}, {
-	[ PUBLICIZE_SHARE_ACTIONS_REQUEST_SUCCESS ]: ( state, { siteId, postId } ) => ( { ...state, [ siteId ]: { ...state[ siteId ], [ postId ]: false } } ),
-	[ PUBLICIZE_SHARE_ACTIONS_REQUEST_FAILURE ]: ( state, { siteId, postId } ) => ( { ...state, [ siteId ]: { ...state[ siteId ], [ postId ]: false } } ),
-	[ PUBLICIZE_SHARE_ACTIONS_REQUEST ]: ( state, { siteId, postId } ) => ( { ...state, [ siteId ]: { ...state[ siteId ], [ postId ]: true } } ),
+	[ PUBLICIZE_SHARE_ACTIONS_REQUEST_SUCCESS ]: ( state, { siteId, postId } ) => updateDataForPost( false, state, siteId, postId ),
+	[ PUBLICIZE_SHARE_ACTIONS_REQUEST_FAILURE ]: ( state, { siteId, postId } ) => updateDataForPost( false, state, siteId, postId ),
+	[ PUBLICIZE_SHARE_ACTIONS_REQUEST ]: ( state, { siteId, postId } ) => updateDataForPost( true, state, siteId, postId ),
+} );
+
+export const deletingSharePostAction = createReducer( {}, {
+	[ PUBLICIZE_SHARE_ACTION_DELETE_SUCCESS ]: ( state, { siteId, postId, actionId } ) => updateDataForPost( false, state, siteId, postId, actionId ),
+	[ PUBLICIZE_SHARE_ACTION_DELETE_FAILURE ]: ( state, { siteId, postId, actionId } ) => updateDataForPost( false, state, siteId, postId, actionId ),
+	[ PUBLICIZE_SHARE_ACTION_DELETE ]: ( state, { siteId, postId, actionId } ) => updateDataForPost( true, state, siteId, postId, actionId ),
+} );
+
+export const editingSharePostAction = createReducer( {}, {
+	[ PUBLICIZE_SHARE_ACTION_EDIT_SUCCESS ]: ( state, { siteId, postId, actionId } ) => updateDataForPost( false, state, siteId, postId, actionId ),
+	[ PUBLICIZE_SHARE_ACTION_EDIT_FAILURE ]: ( state, { siteId, postId, actionId } ) => updateDataForPost( false, state, siteId, postId, actionId ),
+	[ PUBLICIZE_SHARE_ACTION_EDIT ]: ( state, { siteId, postId, actionId } ) => updateDataForPost( true, state, siteId, postId, actionId ),
 } );
 
 
 export default combineReducers( {
-	sharePostActions,
+	items,
 	fetchingSharePostActions,
+	deletingSharePostAction,
+	editingSharePostAction
 } );
