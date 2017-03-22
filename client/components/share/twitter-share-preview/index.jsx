f7899227c859d683583979fe0e76891b834ed1ac
Rastislav Lamos <lamos.rasto@gmail.com>
2017-03-22 10:59:57 -0500

    Add Twitter share preview

--- client/components/share/twitter-share-preview/index.jsx
+++ client/components/share/twitter-share-preview/index.jsx
@@ -1,58 +1,62 @@
 /**
  * External dependencies
  */
-import React, { PropTypes, PureComponent } from 'react';
-
-const baseDomain = url =>
-	url
-		.replace( /^[^/]+[/]*/, '' ) // strip leading protocol
-		.replace( /\/.*$/, '' ); // strip everything after the domain
-
-//Mostly copied from Seo Preview
+import React, { PureComponent } from 'react';
 
 export class TwitterSharePreview extends PureComponent {
 	render() {
-		const {
-			url,
-			title,
-			type,
-			description,
-			image
-		} = this.props;
-
-		const previewImageStyle = {
-			backgroundImage: 'url(' + image + ')'
-		};
+		// TODO: use real data
+		const externalProfilePicture = 'https://3.bp.blogspot.com/-W__wiaHUjwI/Vt3Grd8df0I/AAAAAAAAA78/7xqUNj8ujtY/s1600/image02.png';
+		const externalProfileUrl = 'https://example.com';
+		const externalDisplay = 'Style and Gear';
+		const externalName = 'styleandgear';
+		const message = 'Do you have a trip coming up? Check out this winter travel gear roundup!';
+		const articleUrl = 'https://styleandgear.com/2016/01/03/how-to-dress-like-a-proper-we-need-some-long-url';
+		const imageUrl = 'https://www.w3schools.com/css/trolltunga.jpg';
 
 		return (
 			<div className="twitter-share-preview">
-				<div className={ `twitter-share-preview__${ type }` }>
-					{ image &&
-					<div className="twitter-share-preview__image" style={ previewImageStyle } />
-					}
-					<div className="twitter-share-preview__body">
-						<div className="twitter-share-preview__title">
-							{ title }
+				<div className="twitter-share-preview__content">
+					<div className="twitter-share-preview__profile-picture-part">
+						<img
+							className="twitter-share-preview__profile-picture"
+							src={ externalProfilePicture }
+						/>
+					</div>
+					<div className="twitter-share-preview__content-part">
+						<div className="twitter-share-preview__profile-line">
+							<a
+								className="twitter-share-preview__profile-name"
+								href={ externalProfileUrl }
+							>
+								{ externalDisplay }
+							</a>
+							<a
+								className="twitter-share-preview__profile-handle"
+								href={ externalProfileUrl }
+							>
+								@{ externalName }
+							</a>
 						</div>
-						<div className="twitter-share-preview__description">
-							{ description }
+						<div className="twitter-share-preview__message">
+							{ message }
 						</div>
-						<div className="twitter-share-preview__url">
-							{ baseDomain( url ) }
+						<div className="twitter-share-preview__article-url">
+							{ articleUrl }
 						</div>
+						{ imageUrl &&
+							<div className="twitter-share-preview__image-wrapper">
+								<img
+									className="twitter-share-preview__image"
+									src={ imageUrl }
+								/>
+							</div>
+						}
 					</div>
 				</div>
 			</div>
 		);
 	}
 }
-
-TwitterSharePreview.propTypes = {
-	url: PropTypes.string,
-	title: PropTypes.string,
-	type: PropTypes.string,
-	description: PropTypes.string,
-	image: PropTypes.string
-};
 
 export default TwitterSharePreview;
