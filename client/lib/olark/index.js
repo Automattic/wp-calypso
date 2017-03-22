e9e114f6e7316198036f394adedd2c46625e6d60
Omar Jackman <github@omarjackman.com>
2017-03-22 15:43:42 -0400

    Remove the olark api module since HappyChat has taken its place and it is generating javascript errors

--- client/lib/olark/index.js
+++ client/lib/olark/index.js
@@ -3,19 +3,14 @@
  */
 import debugModule from 'debug';
 import config from 'config';
-import cookie from 'cookie';
-import store from 'store';
 import i18n from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-import sitesModule from 'lib/sites-list';
 import wpcom from 'lib/wp';
-import analytics from 'lib/analytics';
 import emitter from 'lib/mixins/emitter';
 import userModule from 'lib/user';
-import olarkApi from 'lib/olark-api';
 import notices from 'notices';
 import olarkEvents from 'lib/olark-events';
 import olarkStore from 'lib/olark-store';
@@ -31,10 +26,8 @@ import {
  * Module variables
  */
 const debug = debugModule( 'calypso:olark' );
-const sites = sitesModule();
 const user = userModule();
 const wpcomUndocumented = wpcom.undocumented();
-const DAY_IN_SECONDS = 86400;
 
 /**
  * Loads the Olark store so that it can start receiving actions
@@ -93,7 +86,6 @@ const olark = {
 
 	configureOlark: function( wpcomOlarkConfig = {}, dispatch ) {
 		var userData = user.get(),
-			siteUrl = this.getSiteUrl(),
 			updateDetailsEvents = [
 				'api.chat.onReady',
 				'api.chat.onOperatorsAway',
@@ -149,15 +141,6 @@ const olark = {
 		} );
 
 		this.setOlarkOptions( userData, wpcomOlarkConfig );
-		this.updateLivechatActiveCookie();
-		this.bindBeginConversationEvent( userData, siteUrl );
-
-		if ( config.isEnabled( 'help' ) ) {
-			// Only show the chatbox when a user is having a conversation. Chat will now be initiated only from the help/contact page
-			olarkApi( 'api.visitor.getDetails', ( details ) => details.isConversing ? this.showChatBox() : olarkActions.hideBox() );
-		} else {
-			this.showChatBox();
-		}
 
 		dispatch( setChatAvailability( wpcomOlarkConfig.availability ) );
 	},
@@ -166,7 +149,6 @@ const olark = {
 		this.getOlarkConfiguration()
 			.then( ( configuration ) => {
 				const isUserEligible = ( 'undefined' === typeof configuration.isUserEligible ) ? true : configuration.isUserEligible;
-				olarkApi( 'api.chat.setOperatorGroup', { group: configuration.group } );
 				olarkActions.setUserEligibility( isUserEligible );
 			} )
 			.catch( ( error ) => this.handleError( error ) );
@@ -187,19 +169,11 @@ const olark = {
 
 	setOlarkOptions( userData, wpcomOlarkConfig = {} ) {
 		const group = wpcomOlarkConfig.group || config( 'olark' ).business_group_id;
-		const identity = wpcomOlarkConfig.identity || config( 'olark' ).business_account_id;
 		const isUserEligible = ( 'undefined' === typeof wpcomOlarkConfig.isUserEligible ) ? true : wpcomOlarkConfig.isUserEligible;
 		const visitorNickname = wpcomOlarkConfig.nickname || ( userData.username + ' | ' + this.userType );
 
-		if ( false !== group ) {
-			olarkApi.configure( 'system.group', group );
-		}
-
 		debug( 'Nickname: ' + visitorNickname );
 		debug( 'Group: ' + group );
-
-		olarkApi.configure( 'system.chat_does_not_follow_external_links', true );
-		olarkApi.configure( 'system.mask_credit_cards', true );
 
 		olarkActions.setUserEligibility( isUserEligible );
 		olarkActions.setClosed( wpcomOlarkConfig.isClosed );
@@ -207,12 +181,6 @@ const olark = {
 		if ( wpcomOlarkConfig.locale ) {
 			olarkActions.setLocale( wpcomOlarkConfig.locale );
 		}
-
-		// NOTE: According to the olark api documentation, "configure" api calls are supposed to go before the "identify" api call.
-		// See: https://www.olark.com/api
-		olarkApi.identify( identity );
-
-		olarkApi( 'api.chat.updateVisitorNickname', { snippet: visitorNickname } );
 	},
 
 	updateOlarkFormatting( username, avatarURL ) {
@@ -283,154 +251,6 @@ const olark = {
 			previousPersonClassName = personClassName;
 		};
 	},
-
-	getSiteUrl() {
-		const primarySite = sites.initialized && sites.getPrimary();
-		const selectedSite = sites.getSelectedSite();
-		if ( selectedSite ) {
-			return selectedSite.URL;
-		}
-
-		// No need to translate this since this is text seen by an HE in the olark console.
-		return primarySite ? primarySite.URL : 'None found. Please check user RC.';
-	},
-
-	sendVisitorDataToOperator( userData, siteUrl ) {
-		const visitorData = [
-			'WordPress User Info',
-			'Username: ' + userData.username + ' : ' + userData.display_name,
-			'User email: ' + userData.email,
-			'User ID: ' + userData.ID,
-			'Site: ' + siteUrl,
-			'Network Admin: https://wordpress.com/wp-admin/network/users.php?submit=Search+Users&s=' + userData.username,
-			'Upgrades and Transactions : https://wordpress.com/wp-admin/network/wpcom-paid-upgrades.php?action=search&username=' + userData.username
-		];
-
-		visitorData.forEach( function( message ) {
-			olarkApi( 'api.chat.updateVisitorStatus', message );
-			olarkApi( 'api.chat.sendNotificationToOperator', { body: message } );
-		} );
-	},
-
-	updateCustomFields( userData, siteUrl ) {
-		olarkApi( 'api.visitor.updateCustomFields', {
-			wpcom_id: userData.ID,
-			wpcom_olark_api_id: this.apiId,
-			from_page: 'calypso',
-			site_url: siteUrl
-		} );
-	},
-
-	bindBeginConversationEvent( userData, siteUrl ) {
-		this.conversationStarted = true;
-		olarkApi( 'api.chat.onBeginConversation', () => {
-			this.setLivechatActiveCookie();
-			this.updateCustomFields( userData, siteUrl );
-			this.sendVisitorDataToOperator( userData, siteUrl );
-			this.bindOnOperatorsAwayEvent();
-			this.bindOnOperatorsAvailableEvent();
-			this.bindOnMessageToVisitor();
-			this.bindOnMessageToOperator();
-			this.bindCommandFromOperator();
-
-			analytics.mc.bumpStat( {
-				olark_chat: 'live_support-begin',
-				olark_chat_begin_source: 'live_support-calypso'
-			} );
-		} );
-	},
-
-	bindCommandFromOperator() {
-		olarkApi( 'api.chat.onCommandFromOperator', function( event ) {
-			if ( event.command.name === 'ticket' ) {
-				olarkApi( 'api.visitor.updateCustomFields', { create_support_ticket: true } );
-			}
-		} );
-	},
-
-	bindOnOperatorsAwayEvent() {
-		this.operatorAvailable = false;
-
-		if ( ! this.conversationStarted ) {
-			return;
-		}
-
-		store.set( this.operatorsAvailableKey, true );
-	},
-
-	bindOnOperatorsAvailableEvent() {
-		this.operatorAvailable = true;
-
-		if ( true !== store.get( this.operatorsAvailableKey ) || false === this.conversationStarted ) {
-			return;
-		}
-
-		store.set( this.operatorsAvailableKey, false );
-	},
-
-	bindOnMessageToVisitor() {
-		olarkApi( 'api.chat.onMessageToVisitor', this.setLivechatActiveCookie );
-	},
-
-	bindOnMessageToOperator() {
-		olarkApi( 'api.chat.onMessageToOperator', this.setLivechatActiveCookie );
-	},
-
-	showChatBox() {
-		olarkApi( 'api.box.show' );
-		analytics.mc.bumpStat( {
-			olark_chat: 'live_support-display',
-			olark_chat_display_source: 'live_support-calypso'
-		} );
-		/**
-		 * TODO: Set up tracking using Nosara
-		 */
-	},
-
-	setLivechatActiveCookie() {
-		const cookieOptions = {
-			maxAge: DAY_IN_SECONDS,
-			path: '/',
-			domain: '.wordpress.com'
-		};
-		document.cookie = cookie.serialize( 'wpcom-livechat-active', 'yes', cookieOptions );
-
-		debug( 'set wpcom-livechat-active cookie' );
-	},
-
-	deleteLivechatActiveCookie() {
-		const cookieOptions = {
-			maxAge: -1, // Expire the cookie
-			path: '/',
-			domain: '.wordpress.com'
-		};
-		document.cookie = cookie.serialize( 'wpcom-livechat-active', '', cookieOptions );
-
-		debug( 'deleted wpcom-livechat-active cookie' );
-	},
-
-	isLivechatActiveCookieSet() {
-		const cookies = cookie.parse( document.cookie );
-		return typeof cookies[ 'wpcom-livechat-active' ] === 'string';
-	},
-
-	updateLivechatActiveCookie() {
-		debug( 'updating wpcom-livechat-active cookie' );
-
-		olarkApi( 'api.chat.onReady', () => { //This function will always run when called if the olark API is ready (It's not a single fire event).
-			olarkApi( 'api.visitor.getDetails', ( details ) => {
-				if ( details.isConversing ) {
-					// Only set the cookie if it's not already set. We don't wish to change the expiration
-					if ( ! this.isLivechatActiveCookieSet() ) {
-						this.setLivechatActiveCookie();
-					}
-				} else {
-					// Delete the cookie if a conversation is not active
-					this.deleteLivechatActiveCookie();
-				}
-			} );
-		} );
-	}
 };
 
 emitter( olark );
