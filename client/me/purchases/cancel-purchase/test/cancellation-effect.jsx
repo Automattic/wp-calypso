e50033c4b4bf001f5ab393188354c9b6fe2d29b4
Mark Ryall <mark@ryall.name>
2017-03-22 23:01:01 +1100

    Add unit test for cancellation-effect functions.

--- client/me/purchases/cancel-purchase/test/cancellation-effect.jsx
+++ client/me/purchases/cancel-purchase/test/cancellation-effect.jsx
@@ -67,6 +67,37 @@ describe( 'cancellation-effect', function() {
 					"Your site's appearance will revert to its previously selected theme and you will be refunded %(cost)s."
 				);
 			} );
+
+			it( 'should return translation of g suite message when product is g suite', function() {
+				productsValues.isTheme = () => false;
+				productsValues.isGoogleApps = () => true;
+				const headline = cancellationEffect.cancellationEffectDetail( purchase, translate );
+				expect( headline.text ).to.equal(
+					'You will be refunded %(cost)s, but your G Suite account will continue working without interruption. ' +
+					'You will be able to manage your G Suite billing directly through Google.'
+				);
+			} );
+
+			it( 'should return translation of jetpack plan message when product is a jetpack plan', function() {
+				productsValues.isTheme = () => false;
+				productsValues.isGoogleApps = () => false;
+				productsValues.isJetpackPlan = () => true;
+				const headline = cancellationEffect.cancellationEffectDetail( purchase, translate );
+				expect( headline.text ).to.equal(
+					'All plan features - spam filtering, backups, and security screening ' +
+					'- will be removed from your site and you will be refunded %(cost)s.'
+				);
+			} );
+
+			it( 'should return translation of plan message when product is not a theme, g suite or a jetpack plan', function() {
+				productsValues.isTheme = () => false;
+				productsValues.isGoogleApps = () => false;
+				productsValues.isJetpackPlan = () => false;
+				const headline = cancellationEffect.cancellationEffectDetail( purchase, translate );
+				expect( headline.text ).to.equal(
+					'All plan features and custom changes will be removed from your site and you will be refunded %(cost)s.'
+				);
+			} );
 		} );
 
 		describe( 'when not refundable', function() {
@@ -80,6 +111,24 @@ describe( 'cancellation-effect', function() {
 				const headline = cancellationEffect.cancellationEffectDetail( purchase, translate );
 				expect( headline.text ).to.equal(
 					'Your G Suite account remains active until it expires on %(subscriptionEndDate)s.'
+				);
+			} );
+
+			it( 'should return translation of domain mapping message when product is a domain mapping', function() {
+				productsValues.isGoogleApps = () => false;
+				productsValues.isDomainMapping = () => true;
+				const headline = cancellationEffect.cancellationEffectDetail( purchase, translate );
+				expect( headline.text ).to.equal(
+					'Your domain mapping remains active until it expires on %(subscriptionEndDate)s.'
+				);
+			} );
+
+			it( 'should return translation of plan message when product is not g suite or a domain mapping', function() {
+				productsValues.isGoogleApps = () => false;
+				productsValues.isDomainMapping = () => false;
+				const headline = cancellationEffect.cancellationEffectDetail( purchase, translate );
+				expect( headline.text ).to.equal(
+					"Your plan's features remain active until your subscription expires on %(subscriptionEndDate)s."
 				);
 			} );
 		} );
