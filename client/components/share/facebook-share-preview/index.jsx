643055d5e652d774b33796aeff874097f10a20b3
Rastislav Lamos <lamos.rasto@gmail.com>
2017-03-22 10:20:29 -0500

    Add scaffolding of Facebook share preview so it can be styled

--- client/components/share/facebook-share-preview/index.jsx
+++ client/components/share/facebook-share-preview/index.jsx
@@ -1,78 +1,63 @@
 /**
  * External dependencies
  */
-import React, { PropTypes, PureComponent } from 'react';
-import compact from 'lodash/compact';
-
-import {
-	firstValid,
-	hardTruncation,
-	shortEnough
-} from '../helpers';
-
-//Mostly copied from Seo Preview
-
-const TITLE_LENGTH = 80;
-const DESCRIPTION_LENGTH = 270;
-
-const baseDomain = url =>
-	url
-		.replace( /^[^/]+[/]*/, '' ) // strip leading protocol
-		.replace( /\/.*$/, '' ); // strip everything after the domain
-
-const facebookTitle = firstValid(
-	shortEnough( TITLE_LENGTH ),
-	hardTruncation( TITLE_LENGTH )
-);
-
-const facebookDescription = firstValid(
-	shortEnough( DESCRIPTION_LENGTH ),
-	hardTruncation( DESCRIPTION_LENGTH )
-);
+import React, { PureComponent } from 'react';
 
 export class FacebookSharePreview extends PureComponent {
 	render() {
-		const {
-			url,
-			type,
-			title,
-			description,
-			image,
-			author
-		} = this.props;
+		// TODO: use real data
+		const externalProfilePicture = 'https://3.bp.blogspot.com/-W__wiaHUjwI/Vt3Grd8df0I/AAAAAAAAA78/7xqUNj8ujtY/s1600/image02.png';
+		const externalProfileUrl = 'https://example.com';
+		const externalName = 'Style and Gear';
+		const message = 'Do you have a trip coming up? Check out this winter travel gear roundup!';
+		const articleUrl = 'https://styleandgear.com/2016/01/03/how-to-dress-like-a-proper-we-need-some-long-url';
+		const imageUrl = 'https://www.w3schools.com/css/trolltunga.jpg';
 
 		return (
-			<div className={ `facebook-share-preview facebook-share-preview__${ type }` }>
+			<div className="facebook-share-preview">
 				<div className="facebook-share-preview__content">
-					{ image &&
-					<div className="facebook-share-preview__image">
-						<img src={ image } />
+					<div className="facebook-share-preview__header">
+						<div className="facebook-share-preview__profile-picture-part">
+							<img
+								className="facebook-share-preview__profile-picture"
+								src={ externalProfilePicture }
+							/>
+						</div>
+						<div className="facebook-share-preview__profile-line-part">
+							<div className="facebook-share-preview__profile-line">
+								<a href={ externalProfileUrl }>
+									{ externalName }
+								</a>
+								published an article on
+								<a href="https://wordpress.com">
+									WordPress.
+								</a>
+							</div>
+							<div className="facebook-share-preview__wp-line">
+								WordPress
+							</div>
+						</div>
 					</div>
-					}
 					<div className="facebook-share-preview__body">
-						<div className="facebook-share-preview__title">
-							{ facebookTitle( title || '' ) }
+						<div className="facebook-share-preview__message">
+							{ message }
 						</div>
-						<div className="facebook-share-preview__description">
-							{ facebookDescription( description || '' ) }
+						<div className="facebook-share-preview__article-url">
+							{ articleUrl }
 						</div>
-						<div className="facebook-share-preview__url">
-							{ compact( [ baseDomain( url ), author ] ).join( ' | ' ) }
-						</div>
+						{ imageUrl &&
+							<div className="facebook-share-preview__image-wrapper">
+								<img
+									className="facebook-share-preview__image"
+									src={ imageUrl }
+								/>
+							</div>
+						}
 					</div>
 				</div>
 			</div>
 		);
 	}
 }
-
-FacebookSharePreview.propTypes = {
-	url: PropTypes.string,
-	type: PropTypes.string,
-	title: PropTypes.string,
-	description: PropTypes.string,
-	image: PropTypes.string,
-	author: PropTypes.string
-};
 
 export default FacebookSharePreview;
