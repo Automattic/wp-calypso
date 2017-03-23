0b3ceb927f9b2df7e4954e97ec2ec32053b54e17
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-03-23 09:49:39 -0700

    Add reducer and actions for the disconnecting state

--- client/state/jetpack/connection/test/actions.js
+++ client/state/jetpack/connection/test/actions.js
@@ -10,9 +10,14 @@ import {
 	JETPACK_CONNECTION_STATUS_RECEIVE,
 	JETPACK_CONNECTION_STATUS_REQUEST,
 	JETPACK_CONNECTION_STATUS_REQUEST_SUCCESS,
-	JETPACK_CONNECTION_STATUS_REQUEST_FAILURE
+	JETPACK_CONNECTION_STATUS_REQUEST_FAILURE,
+	JETPACK_DISCONNECT_REQUEST,
+	JETPACK_DISCONNECT_REQUEST_SUCCESS,
+	JETPACK_DISCONNECT_REQUEST_FAILURE,
+	JETPACK_DISCONNECT_RECEIVE,
+
 } from 'state/action-types';
-import { requestJetpackConnectionStatus } from '../actions';
+import { requestJetpackConnectionStatus, disconnect } from '../actions';
 import { useSandbox } from 'test/helpers/use-sinon';
 import useNock from 'test/helpers/use-nock';
 import { items as ITEMS_FIXTURE } from './fixture';
@@ -82,6 +87,75 @@ describe( 'actions', () => {
 				return requestJetpackConnectionStatus( siteId )( spy ).then( () => {
 					expect( spy ).to.have.been.calledWith( {
 						type: JETPACK_CONNECTION_STATUS_REQUEST_FAILURE,
+						siteId,
+						error: 'Invalid request.'
+					} );
+				} );
+			} );
+		} );
+	} );
+
+	describe( '#disconnect()', () => {
+		const status = {
+			success: true,
+			message: 'Site Name has been disconnected.'
+		};
+
+		describe( 'success', () => {
+			useNock( ( nock ) => {
+				nock( 'https://public-api.wordpress.com:443' )
+					.persist()
+					.post( '/rest/v1.1/jetpack-blogs/' + siteId + '/mine/delete' )
+					.reply( 200, status, {
+						'Content-Type': 'application/json'
+					} );
+			} );
+
+			it( 'should dispatch a request connection status action when thunk triggered', () => {
+				disconnect( siteId )( spy );
+
+				expect( spy ).to.have.been.calledWith( {
+					type: JETPACK_DISCONNECT_REQUEST,
+					siteId
+				} );
+			} );
+
+			it( 'should dispatch success and receive actions when request successfully completes',
+				() => {
+					return disconnect( siteId )( spy ).then( () => {
+						expect( spy ).to.have.been.calledWith( {
+							type: JETPACK_DISCONNECT_RECEIVE,
+							siteId,
+							status,
+						} );
+
+						expect( spy ).to.have.been.calledWith( {
+							type: JETPACK_DISCONNECT_REQUEST_SUCCESS,
+							siteId
+						} );
+					} );
+				} );
+		} );
+
+		describe( 'failure', () => {
+			const error = {
+				code: 'unauthorized',
+				message: 'Invalid request.'
+			};
+
+			useNock( ( nock ) => {
+				nock( 'https://public-api.wordpress.com:443' )
+					.persist()
+					.post( '/rest/v1.1/jetpack-blogs/' + siteId + '/mine/delete' )
+					.reply( 400, error, {
+						'Content-Type': 'application/json'
+					} );
+			} );
+
+			it( 'should dispatch a failure action when request completes unsuccessfully', () => {
+				return disconnect( siteId )( spy ).then( () => {
+					expect( spy ).to.have.been.calledWith( {
+						type: JETPACK_DISCONNECT_REQUEST_FAILURE,
 						siteId,
 						error: 'Invalid request.'
 					} );
