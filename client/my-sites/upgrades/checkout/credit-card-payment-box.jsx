f11adfc0bf6e2c48ecd4b0ed86df01693035d3bb
Ben Dwyer <ben@scruffian.com>
2017-03-27 15:58:21 +0100

    Store: Create a progress bar to show the loading  state on checkout

--- client/my-sites/upgrades/checkout/credit-card-payment-box.jsx
+++ client/my-sites/upgrades/checkout/credit-card-payment-box.jsx
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import classnames from 'classnames';
+import { some } from 'lodash';
 
 /**
  * Internal dependencies
@@ -12,17 +13,73 @@ var PayButton = require( './pay-button' ),
 	TermsOfService = require( './terms-of-service' ),
 	PaymentBox = require( './payment-box' ),
 	analytics = require( 'lib/analytics' ),
-	cartValues = require( 'lib/cart-values' );
+	cartValues = require( 'lib/cart-values' ),
+	transactionStepTypes = require( 'lib/store-transactions/step-types' );
 
 import CartCoupon from 'my-sites/upgrades/cart/cart-coupon';
 import PaymentChatButton from './payment-chat-button';
 import config from 'config';
 import { PLAN_BUSINESS } from 'lib/plans/constants';
-import { some } from 'lodash';
+import ProgressBar from 'components/progress-bar';
 
 var CreditCardPaymentBox = React.createClass( {
 	getInitialState: function() {
-		return { previousCart: null };
+		return {
+			elapsed: 0,
+			previousCart: null
+		};
+	},
+
+	componentWillReceiveProps: function( nextProps ) {
+		if ( nextProps.transactionStep && this.submitting( nextProps.transactionStep ) ) {
+			this.timer = setInterval( this.tick, 100 );
+		}
+	},
+
+	componentWillUnmount: function() {
+		clearInterval( this.timer );
+	},
+
+	tick: function() {
+		const elasped = this.state.elapsed + 1 / 100 * ( 100 - this.state.elapsed );
+		this.setState( {
+			elapsed: elasped
+		} );
+	},
+
+	submitting: function( transactionStep ) {
+		switch ( transactionStep.name ) {
+			case transactionStepTypes.BEFORE_SUBMIT:
+				return false;
+
+			case transactionStepTypes.INPUT_VALIDATION:
+				if ( this.props.transactionStep.error ) {
+					return false;
+				}
+				return true;
+
+			case transactionStepTypes.SUBMITTING_PAYMENT_KEY_REQUEST:
+				return true;
+
+			case transactionStepTypes.RECEIVED_PAYMENT_KEY_RESPONSE:
+				return true;
+
+			case transactionStepTypes.SUBMITTING_WPCOM_REQUEST:
+				return true;
+
+			case transactionStepTypes.RECEIVED_WPCOM_RESPONSE:
+				if ( this.props.transactionStep.error || ! this.props.transactionStep.data.success ) {
+					return false;
+				}
+				return true;
+
+			default:
+				return false;
+		}
+	},
+
+	progress: function() {
+		return Math.round( this.state.elapsed );
 	},
 
 	handleToggle: function( event ) {
@@ -33,17 +90,60 @@ var CreditCardPaymentBox = React.createClass( {
 		this.props.onToggle( 'paypal' );
 	},
 
+	progressBar: function() {
+		return (
+			<div className="credit-card-payment-box__progress-bar">
+				{ this.translate( 'Processing payment…' ) }
+				<ProgressBar value={ this.progress() } isPulsing />
+			</div>
+		);
+	},
+
+	paymentButtons: function() {
+		const cart = this.props.cart,
+			hasBusinessPlanInCart = some( cart.products, { product_slug: PLAN_BUSINESS } ),
+			showPaymentChatButton = config.isEnabled( 'upgrades/presale-chat' ) && hasBusinessPlanInCart,
+			paypalButtonClasses = classnames( 'credit-card-payment-box__switch-link', {
+				'credit-card-payment-box__switch-link-left': showPaymentChatButton
+			} );
+
+		return (
+			<div className="payment-box__payment-buttons">
+				<PayButton
+					cart={ this.props.cart }
+					transactionStep={ this.props.transactionStep } />
+
+				{ cartValues.isPayPalExpressEnabled( cart )
+					? <a className={ paypalButtonClasses } href="" onClick={ this.handleToggle }>{ this.translate( 'or use PayPal' ) }</a>
+					: null
+				}
+
+				{
+					showPaymentChatButton &&
+					<PaymentChatButton
+						paymentType="credits"
+						cart={ this.props.cart }
+						transactionStep={ this.props.transactionStep } />
+				}
+			</div>
+		);
+	},
+
+	paymentBoxActions: function() {
+		let content = this.paymentButtons();
+		if ( this.props.transactionStep && this.submitting( this.props.transactionStep ) ) {
+			content = this.progressBar();
+		}
+
+		return (
+			<div className="payment-box-actions">
+				{ content }
+			</div>
+		);
+	},
+
 	content: function() {
 		var cart = this.props.cart;
-
-		const hasBusinessPlanInCart = some( cart.products, { product_slug: PLAN_BUSINESS } );
-		const showPaymentChatButton =
-			config.isEnabled( 'upgrades/presale-chat' ) &&
-			hasBusinessPlanInCart;
-
-		const paypalButtonClasses = classnames( 'credit-card-payment-box__switch-link', {
-			'credit-card-payment-box__switch-link-left': showPaymentChatButton
-		} );
 
 		return (
 			<form autoComplete="off" onSubmit={ this.props.onSubmit }>
@@ -58,24 +158,7 @@ var CreditCardPaymentBox = React.createClass( {
 
 				<CartCoupon cart={ cart } />
 
-				<div className="payment-box-actions">
-					<PayButton
-						cart={ this.props.cart }
-						transactionStep={ this.props.transactionStep } />
-
-					{ cartValues.isPayPalExpressEnabled( cart )
-						? <a className={ paypalButtonClasses } href="" onClick={ this.handleToggle }>{ this.translate( 'or use PayPal' ) }</a>
-						: null
-					}
-
-					{
-						showPaymentChatButton &&
-						<PaymentChatButton
-							paymentType="credits"
-							cart={ this.props.cart }
-							transactionStep={ this.props.transactionStep } />
-					}
-				</div>
+				{ this.paymentBoxActions() }
 			</form>
 		);
 	},
