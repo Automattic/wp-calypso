945f73ac8fa59d15aabdd9c7353d5fa7db7f314a
James Tien <southp0105@gmail.com>
2017-03-27 18:51:44 +0800

    Move the route string collectively as a constant object `ACCOUNT_RECOVERY_ROUTES`

---
+++ client/state/account-recovery/reset/constants.js
@@ -0,0 +1,9 @@
+export const ACCOUNT_RECOVERY_ROUTES = {
+	ROOT: '/account-recovery',
+	FORGOT_USERNAME: '/account-recovery/forgot-username',
+	RESET_PASSWORD: '/account-recovery/reset-password',
+	RESET_PASSWORD_EMAIL_FORM: '/account-recovery/reset-password/email-form',
+	RESET_PASSWORD_SMS_FORM: '/account-recovery/reset-password/sms-form',
+	RESET_PASSWORD_TRANSACTION_ID: '/account-recovery/reset-password/transaction-id',
+	RESET_PASSWORD_CONFIRM: '/account-recovery/reset-password/confirm',
+};
