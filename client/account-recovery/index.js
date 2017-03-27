2549ab8b6d6c4233d7f460586fa1fa545dc0e22c
James Tien <southp0105@gmail.com>
2017-03-27 18:51:44 +0800

    Move the route string collectively as a constant object `ACCOUNT_RECOVERY_ROUTES`

--- client/account-recovery/index.js
+++ client/account-recovery/index.js
@@ -13,15 +13,17 @@ import {
 } from './controller';
 import config from 'config';
 
+import { ACCOUNT_RECOVERY_ROUTES } from 'state/account-recovery/reset/constants';
+
 export default function( router ) {
 	// Main route for account recovery is the lost password page
 	if ( config.isEnabled( 'account-recovery' ) ) {
-		router( '/account-recovery', redirectLoggedIn, lostPassword );
-		router( '/account-recovery/forgot-username', redirectLoggedIn, forgotUsername );
-		router( '/account-recovery/reset-password', redirectLoggedIn, resetPassword );
-		router( '/account-recovery/reset-password/sms-form', redirectLoggedIn, resetPasswordSmsForm );
-		router( '/account-recovery/reset-password/email-form', redirectLoggedIn, resetPasswordEmailForm );
-		router( '/account-recovery/reset-password/transaction-id', redirectLoggedIn, resetPasswordByTransactionId );
-		router( '/account-recovery/reset-password/confirm', redirectLoggedIn, resetPasswordConfirmForm );
+		router( ACCOUNT_RECOVERY_ROUTES.ROOT, redirectLoggedIn, lostPassword );
+		router( ACCOUNT_RECOVERY_ROUTES.FORGOT_USERNAME, redirectLoggedIn, forgotUsername );
+		router( ACCOUNT_RECOVERY_ROUTES.RESET_PASSWORD, redirectLoggedIn, resetPassword );
+		router( ACCOUNT_RECOVERY_ROUTES.RESET_PASSWORD_EMAIL_FORM, redirectLoggedIn, resetPasswordEmailForm );
+		router( ACCOUNT_RECOVERY_ROUTES.RESET_PASSWORD_SMS_FORM, redirectLoggedIn, resetPasswordSmsForm );
+		router( ACCOUNT_RECOVERY_ROUTES.RESET_PASSWORD_TRANSACTION_ID, redirectLoggedIn, resetPasswordByTransactionId );
+		router( ACCOUNT_RECOVERY_ROUTES.RESET_PASSWORD_CONFIRM, redirectLoggedIn, resetPasswordConfirmForm );
 	}
 }
