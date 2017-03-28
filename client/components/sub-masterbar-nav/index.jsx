6f4522b39942e7e507e27fdf756ec2d927c96d09
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-03-28 09:51:29 +0200

    Add SubMasterbarNav component

---
+++ client/components/sub-masterbar-nav/index.jsx
@@ -0,0 +1,161 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { findDOMNode } from 'react-dom';
+import classNames from 'classnames';
+
+import Gridicon from 'gridicons';
+import Item from './item';
+
+const VIEW_BREAKPOINT = 660;
+const VIEW_PADDING = 50;
+const ITEM_WIDTH = 110;
+
+export default class SubMasterbarNav extends Component {
+	static propTypes = {
+		fallback: PropTypes.shape(
+			{
+				label: PropTypes.string.isRequired,
+				uri: PropTypes.string.isRequired,
+				icon: PropTypes.string
+			}
+		),
+		options: PropTypes.arrayOf(
+			PropTypes.shape( {
+				label: PropTypes.string.isRequired,
+				uri: PropTypes.string.isRequired,
+				icon: PropTypes.string
+			} )
+		),
+		uri: PropTypes.string.isRequired
+	};
+
+	static defaultProps = {
+		options: []
+	}
+
+	state = {
+		collapsed: true
+	};
+
+	componentDidMount() {
+		this.onResize();
+
+		window.addEventListener( 'resize', this.onResize.bind( this ) );
+		window.addEventListener( 'scroll', this.onScroll.bind( this ) );
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener( 'resize', this.onResize.bind( this ) );
+		window.removeEventListener( 'scroll', this.onScroll.bind( this ) );
+	}
+
+	render() {
+		const className = classNames(
+			'sub-masterbar-nav',
+			{ 'is-collapsed': this.state.collapsed }
+		);
+
+		const ellipsisClass = classNames(
+			'sub-masterbar-nav__ellipsis',
+			{ 'is-open': ! this.state.collapsed }
+		);
+
+		return (
+			<div className={ className }>
+				{ this.isDroppable( this.state.width ) &&
+					<div className="sub-masterbar-nav__select" onClick={ this.toggleList.bind( this ) }>
+						<Item
+							isSelected={ true }
+							label={ this.getSelected().label }
+							icon={ this.getSelected().icon }
+						/>
+						<Gridicon icon="chevron-down" className="sub-masterbar-nav__select-icon" />
+					</div>
+				}
+				<div className="sub-masterbar-nav__wrapper">
+					<div className="sub-masterbar-nav__items">
+						{ this.renderItems() }
+					</div>
+				</div>
+				{ this.isFoldable( this.state.width, this.props.options ) && (
+					<div className="sub-masterbar-nav__switch">
+						<Gridicon icon="ellipsis" className={ ellipsisClass } onClick={ this.toggleList.bind( this ) } />
+					</div>
+				)}
+			</div>
+		);
+	}
+
+	renderItems() {
+		return this.props.options.map( ( item, index ) =>
+			<Item
+				key={ index }
+				isSelected={ this.isSelected( item ) }
+				onClick={ this.selectItem.bind( this, item ) }
+				label={ item.label }
+				icon={ item.icon }
+				href={ item.uri }
+			/>
+		);
+	}
+
+	selectItem( option ) {
+		if ( ! option ) {
+			return;
+		}
+
+		this.setState( ( state ) => ( {
+			collapsed: this.isDroppable( state.width ) || state.collapsed
+		} ) );
+	}
+
+	toggleList() {
+		this.setState( ( state ) => ( {
+			collapsed: ! state.collapsed
+		} ) );
+	}
+
+	onScroll() {
+		this.setState( ( state ) => ( {
+			collapsed: state.collapsed || this.isDroppable( state.width )
+		} ) );
+	}
+
+	onResize() {
+		const currentWidth = findDOMNode( this ).offsetWidth;
+
+		this.setState( ( state, props ) => ( {
+			width: currentWidth,
+			collapsed: state.collapsed || this.hasChangedView( state.width, currentWidth, props.options )
+		} ) );
+	}
+
+	hasChangedView( oldWidth, newWidth, options ) {
+		const min = Math.min( oldWidth, newWidth );
+		const max = Math.max( oldWidth, newWidth );
+
+		return ( this.isDroppable( min ) && ! this.isDroppable( max ) ) ||
+			( this.isFoldable( min, options ) && ! this.isFoldable( max, options ) );
+	}
+
+	isFoldable( width, options ) {
+		return VIEW_BREAKPOINT < width && width < options.length * ITEM_WIDTH + VIEW_PADDING;
+	}
+
+	isDroppable( width ) {
+		return width <= VIEW_BREAKPOINT;
+	}
+
+	isSelected( option ) {
+		return option.uri === this.props.uri;
+	}
+
+	getSelected() {
+		return this.props.options.reduce(
+			( selected, current ) => this.isSelected( current ) ? current : selected,
+			this.props.fallback
+		);
+	}
+}
---
+++ client/components/sub-masterbar-nav/test/index.jsx
@@ -0,0 +1,40 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+/**
+ * Internal dependencies
+ */
+import SubMasterbarNav from '../';
+import Item from '../item';
+
+describe( 'SubMasterbarNav', () => {
+	const options = [
+		{ label: 'sites', uri: '/sites' },
+		{ label: 'more' }
+	];
+
+	it( 'should render a navigation bar given a list of options', () => {
+		const wrapper = shallow( <SubMasterbarNav options={ options } uri="" /> );
+		const items = wrapper.find( Item );
+
+		expect( items.length ).to.equal( 2 );
+		expect( items.everyWhere( item => item.prop( 'isSelected' ) ) ).to.equal( false );
+		expect( items.at( 0 ).prop( 'label' ) ).to.equal( 'sites' );
+		expect( items.at( 1 ).prop( 'label' ) ).to.equal( 'more' );
+	} );
+
+	it( 'should mark an option with a matching URI as selected', () => {
+		const wrapper = shallow( <SubMasterbarNav options={ options } uri="/sites" /> );
+		const items = wrapper.find( Item );
+
+		expect( items.length ).to.equal( 2 );
+		expect( items.at( 0 ).prop( 'label' ) ).to.equal( 'sites' );
+		expect( items.at( 1 ).prop( 'label' ) ).to.equal( 'more' );
+		expect( items.at( 0 ).prop( 'isSelected' ) ).to.equal( true );
+		expect( items.at( 1 ).prop( 'isSelected' ) ).to.equal( false );
+	} );
+} );
