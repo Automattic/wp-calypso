11db0bfca267e59e13b7673183422d7da10a135b
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-03-28 18:57:51 +0200

    Add SubMasterbarNav to theme-showcase view for not logged users

--- client/my-sites/themes/theme-showcase.jsx
+++ client/my-sites/themes/theme-showcase.jsx
@@ -14,10 +14,11 @@ import Gridicon from 'gridicons';
 import Main from 'components/main';
 import Button from 'components/button';
 import ThemesSelection from './themes-selection';
+import SubMasterbarNav from 'components/sub-masterbar-nav';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
 import { addTracking, trackClick } from './helpers';
 import DocumentHead from 'components/data/document-head';
-import { prependFilterKeys, getSortedFilterTerms, stripFilters } from './theme-filters.js';
+import { prependFilterKeys, getSortedFilterTerms, stripFilters, getSubjects } from './theme-filters.js';
 import buildUrl from 'lib/mixins/url-search/build-url';
 import { isJetpackSite, getSiteSlug } from 'state/sites/selectors';
 import { getCurrentUserId } from 'state/current-user/selectors';
@@ -46,6 +47,19 @@ const themesMeta = {
 		description: 'Discover Premium WordPress Themes on the WordPress.com Theme Showcase.',
 		canonicalUrl: 'https://wordpress.com/themes/premium',
 	}
+};
+
+const subjectsMeta = {
+	photo: { icon: 'camera', order: 1 },
+	portfolio: { icon: 'custom-post-type', order: 2 },
+	magazine: { icon: 'reader', order: 3 },
+	blog: { icon: 'posts', order: 4 },
+	business: { icon: 'cart', order: 5 },
+	wedding: { icon: 'heart', order: 6 },
+	minimal: { icon: 'minus-small', order: 7 },
+	travel: { icon: 'globe', order: 8 },
+	food: { icon: 'flip-horizontal', order: 9 },
+	music: { icon: 'audio', order: 10 }
 };
 
 const optionShape = PropTypes.shape( {
@@ -130,7 +144,9 @@ const ThemeShowcase = React.createClass( {
 			search,
 			filter,
 			translate,
-			siteSlug
+			siteSlug,
+			vertical,
+			isLoggedIn
 		} = this.props;
 		const tier = config.isEnabled( 'upgrades/premium-themes' ) ? this.props.tier : 'free';
 
@@ -140,58 +156,84 @@ const ThemeShowcase = React.createClass( {
 			{ property: 'og:type', content: 'website' }
 		];
 
+		const headerIcons = [ {
+			label: 'new',
+			uri: '/design',
+			icon: 'star'
+		} ].concat(
+			getSubjects()
+				.map( subject => subjectsMeta[ subject ] && {
+					label: subject,
+					uri: `/design/${ subject }`,
+					icon: subjectsMeta[ subject ].icon,
+					order: subjectsMeta[ subject ].order
+				} )
+				.filter( icon => !! icon )
+				.sort( ( a, b ) => a.order - b.order )
+		);
+
+		const verticalSection = vertical ? `/${ vertical }` : '';
+
 		// FIXME: Logged-in title should only be 'Themes'
 		return (
 			<Main className="themes">
 				<DocumentHead title={ themesMeta[ tier ].title } meta={ metas } />
 				<PageViewTracker path={ this.props.analyticsPath } title={ this.props.analyticsPageTitle } />
-				<ThemesSearchCard
-					onSearch={ this.doSearch }
-					search={ prependFilterKeys( filter ) + search }
-					tier={ tier }
-					select={ this.onTierSelect } />
-				{ this.showUploadButton() && <Button className="themes__upload-button" compact icon
-					onClick={ this.onUploadClick }
-					href={ siteSlug ? `/themes/upload/${ siteSlug }` : '/themes/upload' }>
-					<Gridicon icon="cloud-upload" />
-					{ translate( 'Upload Theme' ) }
-				</Button>
-				}
-				<ThemesSelection
-					search={ search }
-					tier={ this.props.tier }
-					filter={ filter }
-					vertical={ this.props.vertical }
-					siteId={ this.props.siteId }
-					listLabel={ this.props.listLabel }
-					defaultOption={ this.props.defaultOption }
-					secondaryOption={ this.props.secondaryOption }
-					placeholderCount={ this.props.placeholderCount }
-					getScreenshotUrl={ function( theme ) {
-						if ( ! getScreenshotOption( theme ).getUrl ) {
-							return null;
-						}
-						return getScreenshotOption( theme ).getUrl( theme );
-					} }
-					onScreenshotClick={ function( theme ) {
-						if ( ! getScreenshotOption( theme ).action ) {
-							return;
-						}
-						getScreenshotOption( theme ).action( theme );
-					} }
-					getActionLabel={ function( theme ) {
-						return getScreenshotOption( theme ).label;
-					} }
-					getOptions={ function( theme ) {
-						return pickBy(
-							addTracking( options ),
-							option => ! ( option.hideForTheme && option.hideForTheme( theme, siteId ) )
-						); } }
-					trackScrollPage={ this.props.trackScrollPage }
-					emptyContent={ this.props.emptyContent }
-				/>
-				<ThemePreview />
-				{ this.props.children }
+				{ ! isLoggedIn && (
+					<SubMasterbarNav
+						options={ headerIcons }
+						fallback={ headerIcons[ 0 ] }
+						uri={ `/design${ verticalSection }` } />
+				)}
+				<div className="themes__content">
+					<ThemesSearchCard
+						onSearch={ this.doSearch }
+						search={ prependFilterKeys( filter ) + search }
+						tier={ tier }
+						select={ this.onTierSelect } />
+					{ this.showUploadButton() && <Button className="themes__upload-button" compact icon
+						onClick={ this.onUploadClick }
+						href={ siteSlug ? `/themes/upload/${ siteSlug }` : '/themes/upload' }>
+						<Gridicon icon="cloud-upload" />
+						{ translate( 'Upload Theme' ) }
+					</Button>
+					}
+					<ThemesSelection
+						search={ search }
+						tier={ this.props.tier }
+						filter={ filter }
+						vertical={ this.props.vertical }
+						siteId={ this.props.siteId }
+						listLabel={ this.props.listLabel }
+						defaultOption={ this.props.defaultOption }
+						secondaryOption={ this.props.secondaryOption }
+						placeholderCount={ this.props.placeholderCount }
+						getScreenshotUrl={ function( theme ) {
+							if ( ! getScreenshotOption( theme ).getUrl ) {
+								return null;
+							}
+							return getScreenshotOption( theme ).getUrl( theme );
+						} }
+						onScreenshotClick={ function( theme ) {
+							if ( ! getScreenshotOption( theme ).action ) {
+								return;
+							}
+							getScreenshotOption( theme ).action( theme );
+						} }
+						getActionLabel={ function( theme ) {
+							return getScreenshotOption( theme ).label;
+						} }
+						getOptions={ function( theme ) {
+							return pickBy(
+								addTracking( options ),
+								option => ! ( option.hideForTheme && option.hideForTheme( theme, siteId ) )
+							); } }
+						trackScrollPage={ this.props.trackScrollPage }
+						emptyContent={ this.props.emptyContent }
+					/>
+					<ThemePreview />
+					{ this.props.children }
+				</div>
 			</Main>
 		);
 	}
