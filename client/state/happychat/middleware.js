ab867471d827a97abf71462d1e74d7c3e08d15ba
Andrés Maneiro <nosolosw@users.noreply.github.com>
2017-03-28 10:32:33 +0200

    Send route events to happychat (#12180)

--- client/state/happychat/middleware.js
+++ client/state/happychat/middleware.js
@@ -3,9 +3,15 @@
  */
 import {
 	HAPPYCHAT_TRANSCRIPT_REQUEST,
+	ROUTE_SET,
 } from 'state/action-types';
 import { receiveChatTranscript } from './actions';
-import { getHappychatTranscriptTimestamp } from './selectors';
+import {
+	getHappychatTranscriptTimestamp,
+	isHappychatClientConnected,
+	isHappychatChatAssigned
+} from './selectors';
+import { getCurrentUser } from 'state/current-user/selectors';
 
 const debug = require( 'debug' )( 'calypso:happychat:actions' );
 
@@ -16,6 +22,15 @@ export const requestTranscript = ( connection, { getState, dispatch } ) => {
 		result => dispatch( receiveChatTranscript( result.messages, result.timestamp ) ),
 		e => debug( 'failed to get transcript', e )
 	);
+};
+
+export const sendRouteSetEventMessage = ( connection, { getState }, action ) =>{
+	const state = getState();
+	const currentUser = getCurrentUser( state );
+	if ( isHappychatClientConnected( state ) &&
+		isHappychatChatAssigned( state ) ) {
+		connection.sendEvent( `Looking at https://wordpress.com${ action.path }?support_user=${ currentUser.username }` );
+	}
 };
 
 export default function( connection = null ) {
@@ -29,6 +44,9 @@ export default function( connection = null ) {
 		switch ( action.type ) {
 			case HAPPYCHAT_TRANSCRIPT_REQUEST:
 				requestTranscript( connection, store );
+				break;
+			case ROUTE_SET:
+				sendRouteSetEventMessage( connection, store, action );
 				break;
 		}
 		return next( action );
