a62cb2931e9d56f3baa8bc25140b0aa258241545
Jeff Bowen <jeff@automattic.com>
2017-03-29 14:31:18 -0400

    use login url from the config vs. hard-coded

--- client/auth/emailed-login-link-expired.jsx
+++ client/auth/emailed-login-link-expired.jsx
@@ -8,8 +8,14 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import addQueryArgs from 'lib/route/add-query-args';
+import config from 'config';
 import EmptyContent from 'components/empty-content';
 import RedirectWhenLoggedIn from 'components/redirect-when-logged-in';
+
+const lostPasswordURL = addQueryArgs( {
+	action: 'lostpassword',
+}, config( 'login_url' ) );
 
 class EmailedLoginLinkExpired extends React.Component {
 	render() {
@@ -29,7 +35,7 @@ class EmailedLoginLinkExpired extends React.Component {
 					illustrationWidth={ 500 }
 					line={ translate( 'Maybe try resetting your password instead' ) }
 					secondaryAction={ translate( 'Reset my password' ) }
-					secondaryActionURL={ 'https://wordpress.com/wp-login.php?action=lostpassword' }
+					secondaryActionURL={ lostPasswordURL }
 					title={ translate( 'Login link is expired or invalid' ) }
 					/>
 			</div>
