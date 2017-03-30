5ab51cb9c001f1946ca5d95282003b76863e8262
Jarda Snajdr <jsnajdr@gmail.com>
2017-03-30 17:42:01 +0200

    Add suggested languages to LanguagePickerModal
    Display list of suggested languages based on 'navigator.languages', i.e.,
    on browser settings. If the 'navigator.languages' feature is missing from
    the browser, detect that fact and don't show the suggested list in that case.

--- client/components/language-picker/modal.jsx
+++ client/components/language-picker/modal.jsx
@@ -4,7 +4,7 @@
 import React, { PropTypes, PureComponent } from 'react';
 import classNames from 'classnames';
 import { localize } from 'i18n-calypso';
-import { includes, map, noop, partial } from 'lodash';
+import { find, includes, map, noop, partial, startsWith } from 'lodash';
 
 /**
  * Internal dependencies
@@ -33,6 +33,7 @@ class LanguagePickerModal extends PureComponent {
 			filter: 'popular',
 			search: false,
 			selectedLanguageSlug: this.props.selected,
+			suggestedLanguages: this.getSuggestedLanguages(),
 		};
 	}
 
@@ -40,6 +41,12 @@ class LanguagePickerModal extends PureComponent {
 		if ( nextProps.selected !== this.state.selectedLanguageSlug ) {
 			this.setState( {
 				selectedLanguageSlug: nextProps.selected
+			} );
+		}
+
+		if ( nextProps.languages !== this.props.languages ) {
+			this.setState( {
+				suggestedLanguages: this.getSuggestedLanguages()
 			} );
 		}
 	}
@@ -74,6 +81,31 @@ class LanguagePickerModal extends PureComponent {
 			default:
 				return languages;
 		}
+	}
+
+	getSuggestedLanguages() {
+		if ( ! ( typeof navigator === 'object' && 'languages' in navigator ) ) {
+			return null;
+		}
+
+		const { languages } = this.props;
+
+		const suggestedLanguages = [];
+
+		for ( const langSlug of navigator.languages ) {
+			// Find the language first by its full code (e.g. en-US), and when it fails
+			// try only the base code (en). Don't add duplicates.
+			const lcLangSlug = langSlug.toLowerCase();
+			let language = find( languages, lang => lang.langSlug === lcLangSlug );
+			if ( ! language ) {
+				language = find( languages, lang => startsWith( lcLangSlug, lang.langSlug + '-' ) );
+			}
+			if ( language && ! includes( suggestedLanguages, language ) ) {
+				suggestedLanguages.push( language );
+			}
+		}
+
+		return suggestedLanguages;
 	}
 
 	handleSearch = ( search ) => {
@@ -140,6 +172,35 @@ class LanguagePickerModal extends PureComponent {
 		);
 	}
 
+	renderSuggestedLanguages() {
+		const { suggestedLanguages } = this.state;
+		if ( ! suggestedLanguages ) {
+			return null;
+		}
+
+		return (
+			<div className="language-picker__modal-suggested">
+				<div className="language-picker__modal-suggested-label">
+					{ this.props.translate( 'Suggested languages: ' ) }
+				</div>
+				{ suggestedLanguages.map( language => {
+					const isSelected = language.langSlug === this.state.selectedLanguageSlug;
+					const classes = classNames( 'language-picker__modal-text', {
+						'is-selected': isSelected
+					} );
+
+					return (
+						<div
+							key={ language.langSlug }
+							className={ classes }
+							onClick={ partial( this.handleClick, language.langSlug ) }
+						>{ language.name }</div>
+					);
+				} ) }
+			</div>
+		);
+	}
+
 	render() {
 		const { isVisible, translate } = this.props;
 
@@ -181,6 +242,7 @@ class LanguagePickerModal extends PureComponent {
 					/>
 				</SectionNav>
 				{ this.renderLanguageList() }
+				{ this.renderSuggestedLanguages() }
 			</Dialog>
 		);
 	}
