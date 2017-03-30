d2c41cdc42aa349d258e07744ced49872a7df7a4
Jonathan Sadowski <sadowski@automattic.com>
2017-03-30 16:33:31 -0500

    NPS Survey: Show randomly 1/5000 sessions

--- client/layout/nps-survey-notice/index.jsx
+++ client/layout/nps-survey-notice/index.jsx
@@ -9,7 +9,7 @@ import { connect } from 'react-redux';
  */
 import Dialog from 'components/dialog';
 import NpsSurvey from 'blocks/nps-survey';
-import { showNpsSurveyNotice, setNpsSurveyDialogShowing } from 'state/ui/nps-survey-notice/actions';
+import { showNpsSurveyNoticeIfEligible, setNpsSurveyDialogShowing } from 'state/ui/nps-survey-notice/actions';
 import { isNpsSurveyDialogShowing } from 'state/ui/nps-survey-notice/selectors';
 
 class NpsSurveyNotice extends Component {
@@ -27,7 +27,7 @@ class NpsSurveyNotice extends Component {
 		// (1) the user gets a chance to look briefly at the uncluttered screen, and
 		// (2) the user notices the notice more, since it will cause a change to the
 		//     screen they are already looking at
-		setTimeout( this.props.showNpsSurveyNotice, 3000 );
+		setTimeout( this.props.showNpsSurveyNoticeIfEligible, 3000 );
 	}
 
 	render() {
@@ -50,5 +50,5 @@ const mapStateToProps = ( state ) => {
 
 export default connect(
 	mapStateToProps,
-	{ showNpsSurveyNotice, setNpsSurveyDialogShowing }
+	{ showNpsSurveyNoticeIfEligible, setNpsSurveyDialogShowing }
 )( NpsSurveyNotice );
