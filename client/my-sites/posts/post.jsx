ac67283c0819ec693c2e84bcc66161bc5447ad32
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2017-03-30 10:57:50 -0300

    Posts: comment moderation tools and filters prevent loading comments for all users on Reader (#12551)
    * Revert "Revert comment management to fix reader bug (#12542)"
    
    This reverts commit 3e30a33926bd653d75396a9a348462ab99da4d34.
    
    * Comment State defaults status to approved so comments show on the reader.
    
    * Removes comment moderation tools and filters feature flag from wpcalypso.
    
    * Adds feature flag to request post comments actions

--- client/my-sites/posts/post.jsx
+++ client/my-sites/posts/post.jsx
@@ -53,7 +53,8 @@ const Post = React.createClass( {
 		return {
 			showMoreOptions: false,
 			showComments: false,
-			showShare: false
+			showShare: false,
+			commentsFilter: 'all'
 		};
 	},
 
@@ -328,7 +329,14 @@ const Post = React.createClass( {
 					transitionLeaveTimeout={ 300 }>
 					{ this.buildUpdateTemplate() }
 				</ReactCSSTransitionGroup>
-				{ this.state.showComments && <Comments showCommentCount={ false } post={ this.props.post } onCommentsUpdate={ () => {} } /> }
+				{ this.state.showComments &&
+					<Comments
+						showCommentCount={ false }
+						post={ this.props.post }
+						commentsFilter={ config.isEnabled( 'comments/filters-in-posts' ) ? this.state.commentsFilter : 'approved' }
+						onFilterChange={ commentsFilter => this.setState( { commentsFilter } )}
+						onCommentsUpdate={ () => {} } />
+				}
 				{ this.state.showShare && config.isEnabled( 'republicize' ) && <PostShare post={ this.props.post } site={ site } /> }
 			</Card>
 		);
