b2f08be996cc3083a2aa3b6f946414e596cd3948
Dennis Snell <dennis.snell@automattic.com>
2017-03-30 12:44:06 -0700

    Stop sending browser message that isn't needed anymore
    Previously we used message-passing to indicate to the notifications
    client that the panel became visible but now that we're passing props
    directly to it we don't need this anymore, plus it's a bonus because
    prop-passing works much better.
    
    As a consequence however we don't have intrinsic focus of the notes
    panel which we used to have and we will need to find another way to move
    the keyboard focus.

--- client/layout/masterbar/notifications.jsx
+++ client/layout/masterbar/notifications.jsx
@@ -57,7 +57,8 @@ class MasterbarItemNotifications extends Component {
 
 	toggleNotesFrame = ( event ) => {
 		if ( event ) {
-			event.preventDefault();
+			event.preventDefault && event.preventDefault();
+			event.stopPropagation && event.stopPropagation();
 		}
 
 		const {
