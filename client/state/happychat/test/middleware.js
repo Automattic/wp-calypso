5221c9d61e9676ef52d99577026b2a9102d171ea
Matt Wondra <mattwondra@gmail.com>
2017-03-31 12:18:56 -0500

    Happychat: Middleware refactor of HAPPYCHAT_CONNECTING (#12606)
    * Move connectChat with side effects from actions to middleware
    
    * Remove unused code
    
    * Indentation cleanup

--- client/state/happychat/test/middleware.js
+++ client/state/happychat/test/middleware.js
@@ -9,15 +9,97 @@ import { noop } from 'lodash';
 /**
  * Internal dependencies
  */
+import { useSandbox } from 'test/helpers/use-sinon';
+import wpcom from 'lib/wp';
 import {
+	HAPPYCHAT_CONNECTED,
+	HAPPYCHAT_CONNECTING,
+	HAPPYCHAT_RECEIVE_EVENT,
 	HAPPYCHAT_SEND_BROWSER_INFO,
 	HAPPYCHAT_SEND_MESSAGE,
 	HAPPYCHAT_SET_MESSAGE,
+	HAPPYCHAT_SET_AVAILABLE,
+	HAPPYCHAT_SET_CHAT_STATUS,
 	HAPPYCHAT_TRANSCRIPT_RECEIVE,
+	HAPPYCHAT_TRANSCRIPT_REQUEST,
 } from 'state/action-types';
-import middleware, { requestTranscript } from '../middleware';
+import middleware, {
+	connectChat,
+	requestTranscript,
+} from '../middleware';
 
 describe( 'middleware', () => {
+	describe( 'HAPPYCHAT_CONNECTING action', () => {
+		// TODO: Add tests for cases outside the happy path
+		const action = { type: HAPPYCHAT_CONNECTING };
+
+		it( 'should do nothing if Happychat is already connected', () => {
+			const getState = stub().returns( { happychat: { connectionStatus: 'connected' } } );
+			const next = stub();
+			const dispatch = stub();
+			middleware( stub() )( { getState, dispatch } )( next )( action );
+			expect( dispatch ).not.to.have.beenCalled;
+			expect( next ).not.to.have.beenCalled;
+		} );
+
+		describe( 'after successful connection', () => {
+			let connection;
+			let dispatch, getState;
+			const state = deepFreeze( {
+				currentUser: { id: 1 },
+				happychat: { connectionStatus: 'disconnected' },
+				users: { items: { 1: {} } }
+			} );
+
+			useSandbox( sandbox => {
+				connection = {
+					on: sandbox.stub(),
+					open: sandbox.stub().returns( Promise.resolve() )
+				};
+				// Need to add return value after re-assignment, otherwise it will return
+				// a reference to the previous (undefined) connection variable.
+				connection.on.returns( connection );
+
+				dispatch = sandbox.stub();
+				getState = sandbox.stub().returns( state );
+				sandbox.stub( wpcom, 'request', ( args, callback ) => callback( null, {} ) );
+			} );
+
+			it( 'should send notice of the connection state', () => {
+				return connectChat( connection, { dispatch, getState } )
+					.then( () => expect( dispatch ).to.have.been.calledWith( { type: HAPPYCHAT_CONNECTED } ) );
+			} );
+
+			it( 'should fetch transcripts', () => {
+				return connectChat( connection, { dispatch, getState } )
+					.then( () => expect( dispatch ).to.have.been.calledWith( { type: HAPPYCHAT_TRANSCRIPT_REQUEST } ) );
+			} );
+
+			it( 'should set up listeners for various connection events', () => {
+				connection.on.withArgs( 'accept' );
+				connection.on.withArgs( 'message' );
+				connection.on.withArgs( 'status' );
+
+				return connectChat( connection, { dispatch, getState } )
+					.then( () => {
+						expect( connection.on.callCount ).to.equal( 3 );
+
+						// Ensure 'accept' listener was connected by executing a fake message event
+						connection.on.withArgs( 'accept' ).firstCall.args[ 1 ]( true );
+						expect( dispatch ).to.have.been.calledWith( { type: HAPPYCHAT_SET_AVAILABLE, isAvailable: true } );
+
+						// Ensure 'message' listener was connected by executing a fake message event
+						connection.on.withArgs( 'message' ).firstCall.args[ 1 ]( 'some event' );
+						expect( dispatch ).to.have.been.calledWith( { type: HAPPYCHAT_RECEIVE_EVENT, event: 'some event' } );
+
+						// Ensure 'message' listener was connected by executing a fake message event
+						connection.on.withArgs( 'status' ).firstCall.args[ 1 ]( 'ready' );
+						expect( dispatch ).to.have.been.calledWith( { type: HAPPYCHAT_SET_CHAT_STATUS, status: 'ready' } );
+					} );
+			} );
+		} );
+	} );
+
 	describe( 'HAPPYCHAT_SET_MESSAGE action', () => {
 		it( 'should send relevant browser information to the connection', () => {
 			const action = { type: HAPPYCHAT_SEND_BROWSER_INFO, siteUrl: 'http://butt.holdings/' };
