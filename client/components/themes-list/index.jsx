44531520489d17e92c5f64e79ae6fc26e7c4a287
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-03-31 11:51:12 +0200

    Reimplement themes-list grid using react-virtualized

--- client/components/themes-list/index.jsx
+++ client/components/themes-list/index.jsx
@@ -2,24 +2,23 @@
  * External dependencies
  */
 import React from 'react';
+import { findDOMNode } from 'react-dom';
 import times from 'lodash/times';
 import { localize } from 'i18n-calypso';
 import { isEqual, noop } from 'lodash';
+import { InfiniteLoader, WindowScroller, AutoSizer, Grid } from 'react-virtualized';
 
 /**
  * Internal dependencies
  */
 import Theme from 'components/theme';
 import EmptyContent from 'components/empty-content';
-import InfiniteScroll from 'lib/mixins/infinite-scroll';
 import { DEFAULT_THEME_QUERY } from 'state/themes/constants';
 
 /**
  * Component
  */
 export const ThemesList = React.createClass( {
-
-	mixins: [ InfiniteScroll( 'fetchNextPage' ) ],
 
 	propTypes: {
 		themes: React.PropTypes.array.isRequired,
@@ -56,6 +55,14 @@ export const ThemesList = React.createClass( {
 		};
 	},
 
+	isRowLoaded( { index } ) {
+		return !! this.props.themes[ this.getItemsPerRow() * ( index + 1 ) - 1 ];
+	},
+
+	componentDidMount() {
+		this._width = findDOMNode( this ).offsetWidth;
+	},
+
 	shouldComponentUpdate( nextProps ) {
 		return nextProps.loading !== this.props.loading ||
 			! isEqual( nextProps.themes, this.props.themes ) ||
@@ -65,41 +72,26 @@ export const ThemesList = React.createClass( {
 			( nextProps.onMoreButtonClick !== this.props.onMoreButtonClick );
 	},
 
-	renderTheme( theme, index ) {
-		return <Theme
-			key={ 'theme-' + theme.id }
-			buttonContents={ this.props.getButtonOptions( theme.id ) }
-			screenshotClickUrl={ this.props.getScreenshotUrl && this.props.getScreenshotUrl( theme.id ) }
-			onScreenshotClick={ this.props.onScreenshotClick }
-			onMoreButtonClick={ this.props.onMoreButtonClick }
-			actionLabel={ this.props.getActionLabel( theme.id ) }
-			index={ index }
-			theme={ theme }
-			active={ this.props.isActive( theme.id ) }
-			purchased={ this.props.isPurchased( theme.id ) }
-			installing={ this.props.isInstalling( theme.id ) } />;
+	getItemsPerRow( rowWidth ) {
+		return Math.floor( ( rowWidth || this._width ) / 250 );
 	},
 
-	renderLoadingPlaceholders() {
-		return times( this.props.placeholderCount, function( i ) {
-			return <Theme key={ 'placeholder-' + i } theme={ { id: 'placeholder-' + i, name: 'Loading…' } } isPlaceholder={ true } />;
+	loadMoreRows() {
+		return this.fetchNextPage( { triggeredByScroll: true } );
+	},
+
+	onSectionRendered( { columnStartIndex, columnStopIndex, rowStartIndex, rowStopIndex }, onRowsRendered ) {
+		const startIndex = rowStartIndex * this.getItemsPerRow() + columnStartIndex;
+		const stopIndex = rowStopIndex * this.getItemsPerRow() + columnStopIndex;
+
+		onRowsRendered( {
+			startIndex,
+			stopIndex
 		} );
 	},
 
-	// Invisible trailing items keep all elements same width in flexbox grid.
-	renderTrailingItems() {
-		const NUM_SPACERS = 11; // gives enough spacers for a theoretical 12 column layout
-		return times( NUM_SPACERS, function( i ) {
-			return <div className="themes-list__spacer" key={ 'themes-list__spacer-' + i } />;
-		} );
-	},
-
-	renderEmpty() {
-		return this.props.emptyContent ||
-			<EmptyContent
-				title={ this.props.translate( 'Sorry, no themes found.' ) }
-				line={ this.props.translate( 'Try a different search or more filters?' ) }
-				/>;
+	onResize( { width } ) {
+		this._width = width;
 	},
 
 	render() {
@@ -109,10 +101,107 @@ export const ThemesList = React.createClass( {
 
 		return (
 			<div className="themes-list">
-				{ this.props.themes.map( this.renderTheme ) }
-				{ this.props.loading && this.renderLoadingPlaceholders() }
-				{ this.renderTrailingItems() }
+				<InfiniteLoader
+					isRowLoaded={ this.isRowLoaded }
+					loadMoreRows={ this.fetchNextPage }
+					rowCount={ 1000 }
+					threshold={ 2 }
+				>
+					{ ( { onRowsRendered, registerChild } ) => (
+						<WindowScroller>
+							{ ( { height, scrollTop } ) => (
+								<AutoSizer disableHeight onResize={ this.onResize }>
+									{ ( { width } ) => {
+										return this.renderGrid( width, height, scrollTop, registerChild, onRowsRendered );
+									} }
+								</AutoSizer>
+							) }
+						</WindowScroller>
+					) }
+				</InfiniteLoader>
 			</div>
+		);
+	},
+
+	renderGrid( width, height, scrollTop, registerChild, onRowsRendered ) {
+		const columnCount = this.getItemsPerRow( width );
+		const rowCount = Math.ceil( this.props.themes.length / columnCount );
+
+		const onSectionRendered = ( args ) => this.onSectionRendered( args, onRowsRendered );
+
+		return (
+			<Grid
+				width={ width }
+				height={ height }
+				autoHeight={ true }
+				columnCount={ columnCount }
+				columnWidth={ Math.floor( width / columnCount ) }
+				rowCount={ rowCount }
+				rowHeight={ Math.floor( ( ( width / columnCount ) - 20 ) * 0.75 ) + 74 }
+				scrollTop={ scrollTop }
+				cellRenderer={ this.renderCell }
+				onSectionRendered={ onSectionRendered }
+				ref={ registerChild }
+			/>
+		);
+	},
+
+	renderCell( { rowIndex, columnIndex, key, style } ) {
+		const index = rowIndex * this.getItemsPerRow() + columnIndex;
+		const theme = this.props.themes[ index ];
+
+		return (
+			<div key={ key } className="themes-list__cell" style={ style }>
+				{ theme
+					? this.renderTheme( theme, index )
+					: this.renderLoadingPlaceholder( index )
+				}
+			</div>
+		);
+	},
+
+	renderTheme( theme, index ) {
+		return (
+			<Theme
+				key={ 'theme-' + theme.id }
+				buttonContents={ this.props.getButtonOptions( theme.id ) }
+				screenshotClickUrl={ this.props.getScreenshotUrl && this.props.getScreenshotUrl( theme.id ) }
+				onScreenshotClick={ this.props.onScreenshotClick }
+				onMoreButtonClick={ this.props.onMoreButtonClick }
+				actionLabel={ this.props.getActionLabel( theme.id ) }
+				index={ index }
+				theme={ theme }
+				active={ this.props.isActive( theme.id ) }
+				purchased={ this.props.isPurchased( theme.id ) }
+				installing={ this.props.isInstalling( theme.id ) }
+			/>
+		);
+	},
+
+	renderLoadingPlaceholder( index ) {
+		return (
+			<Theme
+				key={ 'placeholder-' + index }
+				theme={ { id: 'placeholder-' + index, name: 'Loading…' } }
+				isPlaceholder={ true }
+			/>
+		);
+	},
+
+	// Invisible trailing items keep all elements same width in flexbox grid.
+	renderTrailingItems() {
+		const NUM_SPACERS = 11; // gives enough spacers for a theoretical 12 column layout
+		return times( NUM_SPACERS, function( i ) {
+			return <div className="themes-list--spacer" key={ 'themes-list--spacer-' + i } />;
+		} );
+	},
+
+	renderEmpty() {
+		return this.props.emptyContent || (
+			<EmptyContent
+				title={ this.props.translate( 'Sorry, no themes found.' ) }
+				line={ this.props.translate( 'Try a different search or more filters?' ) }
+			/>
 		);
 	}
 } );
