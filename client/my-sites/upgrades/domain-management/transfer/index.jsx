c96056004047e8579144d8866973e89fed811211
Andrija Vucinic <andrija.vucinic@gmail.com>
2017-03-31 14:03:01 +0200

    Domain Only: Hide transfer to another user for domain-only sites
    Users shouldn't be able to transfer a domain associated with a domain only site to another user.
    
    #12567

--- client/my-sites/upgrades/domain-management/transfer/index.jsx
+++ client/my-sites/upgrades/domain-management/transfer/index.jsx
@@ -1,23 +1,25 @@
 /**
  * External Dependencies
  */
+import { connect } from 'react-redux';
 import React from 'react';
 
 /**
  * Internal Dependencies
  */
+import { get } from 'lodash';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import Header from 'my-sites/upgrades/domain-management/components/header';
+import { isDomainOnlySite, isSiteAutomatedTransfer } from 'state/selectors';
 import { localize } from 'i18n-calypso';
+import Main from 'components/main';
 import paths from 'my-sites/upgrades/paths';
 import VerticalNav from 'components/vertical-nav';
 import VerticalNavItem from 'components/vertical-nav/item';
-import Header from 'my-sites/upgrades/domain-management/components/header';
-import Main from 'components/main';
-import { get } from 'lodash';
 
 function Transfer( props ) {
-	const { selectedSite, selectedDomainName, translate } = props;
+	const { isAutomatedTransfer, isDomainOnly, selectedSite, selectedDomainName, translate } = props;
 	const slug = get( selectedSite, 'slug' );
-	const canTransferDomain = ! get( selectedSite, 'options.is_automated_transfer', false );
 
 	return (
 		<Main className="domain-management-transfer">
@@ -32,7 +34,7 @@ function Transfer( props ) {
 				}>
 					{ translate( 'Transfer to another registrar' ) }
 				</VerticalNavItem>
-					{ canTransferDomain &&
+					{ ! isAutomatedTransfer && ! isDomainOnly &&
 						<VerticalNavItem path={ paths.domainManagementTransferToAnotherUser( slug, selectedDomainName ) }>
 							{ translate( 'Transfer to another user' ) }
 						</VerticalNavItem> }
@@ -41,4 +43,10 @@ function Transfer( props ) {
 	);
 }
 
-export default localize( Transfer );
+export default connect( ( state ) => {
+	const siteId = getSelectedSiteId( state );
+	return {
+		isAutomatedTransfer: isSiteAutomatedTransfer( state, siteId ),
+		isDomainOnly: isDomainOnlySite( state, siteId )
+	};
+} )( localize( Transfer ) );
