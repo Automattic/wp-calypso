5a09f64d28a134312685f89b58344106837d941f
Steve Seear <steve.seear@gmail.com>
2017-04-03 11:52:16 +0100

    Tweaks

--- client/state/data-layer/wpcom/theme-filters/index.js
+++ client/state/data-layer/wpcom/theme-filters/index.js
@@ -1,15 +1,15 @@
 /**
  * Internal dependencies
  */
-import THEME_FILTERS_REQUEST from 'state/action-types';
-import receiveThemeFilters from 'state/themes/actions';
+import { THEME_FILTERS_REQUEST } from 'state/action-types';
+import { receiveThemeFilters } from 'state/themes/actions';
 import wpcom from 'lib/wp';
 
-const apiResponse = ( dispatch ) => data => dispatch( receiveThemeFilters( data ) );
-
 const fetchThemeFilters = ( { dispatch } ) =>
-	wpcom.req.get( '/v1.2/theme-filters' )
-		.then( apiResponse( dispatch ) );
+	wpcom.req.get( '/theme-filters', { apiVersion: '1.2' } )
+		.then( ( themeFilters ) => {
+			dispatch( receiveThemeFilters( themeFilters ) );
+		} );
 		// TODO: catch and dispatch failure action
 
 export default {
