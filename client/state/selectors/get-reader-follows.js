285d4afb6633571ab3b0f17c10297be92643d61c
Jake F - @samouri <samouri@users.noreply.github.com>
2017-04-03 13:41:38 -0400

    memoize selector for reader follows

--- client/state/selectors/get-reader-follows.js
+++ client/state/selectors/get-reader-follows.js
@@ -3,12 +3,18 @@
  */
 import { filter } from 'lodash';
 
+/**
+ * Internal dependencies
+ */
+import createSelector from 'lib/create-selector';
+
 /*
  * Get all sites/feeds the user follows.
  *
  * @param  {Object}  state  Global state tree
  * @return {Integer} Follow count
  */
-export default function getReaderFollows( state ) {
-	return filter( state.reader.follows.items, [ 'is_following', true ] );
-}
+export default createSelector(
+	state => filter( state.reader.follows.items, [ 'is_following', true ] ),
+	state => [ state.reader.follows.items ],
+);
