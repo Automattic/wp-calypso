739ccc80ff61ab62f35e2d7abe1d11d30e85028f
Matt Wondra <matt.wondra@automattic.com>
2017-04-04 07:39:57 -0500

    Document where CONNECTION_ERROR constants come from

--- client/state/happychat/constants.js
+++ client/state/happychat/constants.js
@@ -1,3 +1,5 @@
+// These CONNECTION_ERROR constants come directly from the Socket.IO client library.
+// These are the possible reasons for a connection disconnect.
 export const HAPPYCHAT_CONNECTION_ERROR_FORCED_CLOSE = 'forced close';
 export const HAPPYCHAT_CONNECTION_ERROR_PING_TIMEOUT = 'ping timeout';
 export const HAPPYCHAT_CONNECTION_ERROR_TRANSPORT_CLOSE = 'transport close';
