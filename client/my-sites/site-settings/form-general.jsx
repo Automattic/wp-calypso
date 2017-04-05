9a19a1805af7145f0c65ae42620403a40ce6b7d8
Beau Lebens <beau@dentedreality.com.au>
2017-04-05 12:44:02 -0600

    Add a8c-only notice to the Jetpack cached-data toggle.

--- client/my-sites/site-settings/form-general.jsx
+++ client/my-sites/site-settings/form-general.jsx
@@ -424,7 +424,7 @@ class SiteSettingsFormGeneral extends Component {
 				>
 					{ translate(
 						'Use synchronized data to boost performance'
-					) }
+					) } (a8c-only experimental feature)
 				</CompactFormToggle>
 			</CompactCard>
 		);
