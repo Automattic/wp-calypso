dfdeffdf21ffbed217fa5d6bd36623733f750792
Nikolay Bachiyski <nb@nikolay.bg>
2017-04-05 10:52:03 +0200

    Add isSiteVip selector

---
+++ client/state/selectors/is-site-vip.js
@@ -0,0 +1,22 @@
+/**
+ * Internal dependencies
+ */
+import { getRawSite } from 'state/sites/selectors';
+
+/**
+ * Returns true if the site is VIP
+ *
+ * If the site is missing returns null.
+ *
+ * @param  {Object}   state  Global state tree
+ * @param  {Number}   siteId Site ID
+ * @return {?Boolean}        Whether site is VIP
+ */
+export default function( state, siteId ) {
+	const site = getRawSite( state, siteId );
+	if ( ! site || typeof site.is_vip === 'undefined' ) {
+		return null;
+	}
+
+	return site.is_vip;
+}
