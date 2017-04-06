f788b4e36dfd3bc05772f27a03cbb6dc419ae088
Marin Atanasov <tyxla@users.noreply.github.com>
2017-04-06 14:11:34 +0300

    Site Settings: Hide SEO help text if SEO feature is not available (#12806)
    * Site Settings: Hide SEO help text if SEO feature is not available
    
    * Site Settings: Sentence case title in SeoSettingsHelpCard

--- client/my-sites/site-settings/seo-settings/help.jsx
+++ client/my-sites/site-settings/seo-settings/help.jsx
@@ -10,10 +10,13 @@ import { localize } from 'i18n-calypso';
  */
 import Card from 'components/card';
 import SectionHeader from 'components/section-header';
+import { hasFeature } from 'lib/plans';
 import { isJetpackSite } from 'state/sites/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
+import { FEATURE_ADVANCED_SEO } from 'lib/plans/constants';
 
 const SeoSettingsHelpCard = ( {
+	siteId,
 	siteIsJetpack,
 	translate
 } ) => {
@@ -23,23 +26,26 @@ const SeoSettingsHelpCard = ( {
 
 	return (
 		<div>
-			<SectionHeader label={ translate( 'Search Engine Optimization' ) } />
-			<Card>
-				<p>
-					{ translate(
-						'{{b}}WordPress.com has great SEO{{/b}} out of the box. All of our themes are optimized ' +
-						'for search engines, so you don\'t have to do anything extra. However, you can tweak ' +
-						'these settings if you\'d like more advanced control. Read more about what you can do ' +
-						'to {{a}}optimize your site\'s SEO{{/a}}.',
-						{
-							components: {
-								a: <a href={ seoHelpLink } />,
-								b: <strong />
+			<SectionHeader label={ translate( 'Search engine optimization' ) } />
+			{
+				hasFeature( FEATURE_ADVANCED_SEO, siteId ) &&
+				<Card>
+					<p>
+						{ translate(
+							'{{b}}WordPress.com has great SEO{{/b}} out of the box. All of our themes are optimized ' +
+							'for search engines, so you don\'t have to do anything extra. However, you can tweak ' +
+							'these settings if you\'d like more advanced control. Read more about what you can do ' +
+							'to {{a}}optimize your site\'s SEO{{/a}}.',
+							{
+								components: {
+									a: <a href={ seoHelpLink } />,
+									b: <strong />
+								}
 							}
-						}
-					) }
-				</p>
-			</Card>
+						) }
+					</p>
+				</Card>
+			}
 		</div>
 	);
 };
@@ -50,6 +56,7 @@ export default connect(
 		const siteIsJetpack = isJetpackSite( state, siteId );
 
 		return {
+			siteId,
 			siteIsJetpack,
 		};
 	}
