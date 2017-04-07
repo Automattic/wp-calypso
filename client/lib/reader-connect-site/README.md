fa42480fb9d4273f2aeec7f8cc40b1b64ce41907
Jake <samouri@users.noreply.github.com>
2017-04-07 08:17:12 -0700

    Reader Search on Manage: search box (#12871)
    * Reader Search on Manage: search box
    
    * fix lint doc warning
    
    * Add README.md and slightly clean up connect-site
    
    * blogId --> siteId

---
+++ client/lib/reader-connect-site/README.md
@@ -0,0 +1,27 @@
+Reader Connect Site
+=======
+This is a HoC that takes in a component and wraps it with one that knows how to fetch sites and feeds.
+The output component expects to be handed either a feedId or a siteId.  It will only initiate network requests if the feed/site is not already part of the state tree.
+
+## Example
+
+```js
+import connectSite from 'lib/reader-connect-site';
+
+const SiteInfo = () => {
+	if ( ! this.props.site ) {
+		return null;
+	}
+	const { title, url, description } = this.props.site;
+	return (
+		<h1> Site Info! </h1>
+		<p> title: { title }</p>
+		<p> url: { url }</p>
+		<p> title: { description }</p>
+	)
+}
+
+const ConnectedSiteInfo = connectSite( SiteInfo );
+...
+<ConnectedSiteInfo siteId='12345' /> 
+```
---
+++ client/lib/reader-connect-site/index.js
@@ -0,0 +1,69 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+/*
+ * Internal Dependencies
+ */
+import { getSite } from 'state/reader/sites/selectors';
+import { getFeed } from 'state/reader/feeds/selectors';
+import QueryReaderSite from 'components/data/query-reader-site';
+import QueryReaderFeed from 'components/data/query-reader-feed';
+
+/**
+ * A HoC function that will take in reader identifiers siteId or feedId and
+ * pass down all of the fetched data objects they represent
+ *
+ *  It supports two
+ *  1. feedId --> feedId, siteId, feed, site
+ *  2. blogId --> feedId, siteId, feed, site
+ *
+ * @param {object} Component the component to wrap
+ * @returns {object} wrapped component that hands down feed/site to its child
+ */
+const connectSite = Component => {
+	class connectSiteFetcher extends React.PureComponent {
+		static propTypes = {
+			feed: PropTypes.object,
+			site: PropTypes.object,
+		}
+
+		render() {
+			return (
+				<div>
+					{ ! this.props.feed && !! this.props.feedId && <QueryReaderFeed feedId={ this.props.feedId } /> }
+					{ ! this.props.site && !! this.props.siteId && <QueryReaderSite siteId={ this.props.siteId } /> }
+					<Component { ...this.props } />
+				</div>
+			);
+		}
+	}
+
+	return connect(
+		( state, ownProps ) => {
+			let { feedId, siteId } = ownProps;
+			let feed = !! feedId ? getFeed( state, feedId ) : undefined;
+			let site = !! siteId ? getSite( state, siteId ) : undefined;
+
+			if ( feed && ! siteId ) {
+				siteId = feed.blog_ID !== 0 && feed.blog_ID;
+				site = !! siteId ? getSite( state, feed.blog_ID ) : undefined;
+			}
+			if ( site && ! feedId ) {
+				feedId = site.feed_ID;
+				feed = !! feedId ? getFeed( state, site.feed_ID ) : undefined;
+			}
+
+			return {
+				feed,
+				site,
+				siteId,
+				feedId,
+			};
+		}
+	)( connectSiteFetcher );
+};
+
+export default connectSite;
