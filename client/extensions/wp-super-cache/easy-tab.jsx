44a5629b072742a9a0008664b3c4b6c8bacf51f6
Drew Butler <hi@dbtlr.com>
2017-04-08 13:59:29 +0200

    Update the caching label to be more descriptive

--- client/extensions/wp-super-cache/easy-tab.jsx
+++ client/extensions/wp-super-cache/easy-tab.jsx
@@ -51,7 +51,7 @@ const EasyTab = ( {
 						disabled={ isRequesting }
 						onChange={ handleToggle( 'is_cache_enabled' ) }>
 						<span>
-							{ translate( 'Caching On {{em}}(Recommended){{/em}}',
+							{ translate( 'Enable Page Caching',
 								{
 									components: { em: <em /> }
 								}
