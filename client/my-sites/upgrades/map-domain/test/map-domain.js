4b4d0b367b61c9ece1057d1f7b1c2f9d72ebd45a
Nikolay Bachiyski <nb@nikolay.bg>
2017-04-09 11:53:05 -0400

    Add more tests

--- client/my-sites/upgrades/map-domain/test/map-domain.js
+++ client/my-sites/upgrades/map-domain/test/map-domain.js
@@ -11,11 +11,12 @@ import { spy } from 'sinon';
  */
 import useFakeDom from 'test/helpers/use-fake-dom';
 import useMockery from 'test/helpers/use-mockery';
+import paths from 'my-sites/upgrades/paths';
 
 describe( 'MapDomain component', () => {
 	const pageSpy = spy();
 	pageSpy.redirect = spy();
-	let MapDomain, MapDomainStep;
+	let MapDomain, MapDomainStep, HeaderCake;
 
 	// needed, because some dependency of dependency uses `window`
 	useFakeDom();
@@ -24,6 +25,7 @@ describe( 'MapDomain component', () => {
 		mockery.registerMock( 'page', pageSpy );
 		MapDomain = require( '../' ).MapDomain;
 		MapDomainStep = require( 'components/domains/map-domain-step' );
+		HeaderCake = require( 'components/header-cake' );
 	} );
 
 	beforeEach( () => {
@@ -63,5 +65,41 @@ describe( 'MapDomain component', () => {
 	it( 'renders a MapDomainStep', () => {
 		const wrapper = shallow( <MapDomain { ...defaultProps } /> );
 		expect( wrapper.find( MapDomainStep ) ).to.have.length( 1 );
+	} );
+
+	it( "goes back when HeaderCake's onClick is fired", () => {
+		const wrapper = shallow( <MapDomain { ...defaultProps } /> );
+		expect( wrapper.find( HeaderCake ).prop( 'onClick' ) ).to.equal( wrapper.instance().goBack );
+	} );
+
+	it( 'goes back to /domains/add if no selected site', () => {
+		const wrapper = shallow( <MapDomain noSelectedSite={ true } { ...defaultProps } /> );
+		wrapper.instance().goBack();
+		expect( pageSpy ).to.have.been.calledWith( '/domains/add' );
+	} );
+
+	it( 'goes back to domain management for VIP sites', () => {
+		const wrapper = shallow( <MapDomain selectedSiteIsVip={ true } selectedSiteSlug="baba" { ...defaultProps } /> );
+		wrapper.instance().goBack();
+		expect( pageSpy ).to.have.been.calledWith( paths.domainManagementList( 'baba' ) );
+	} );
+
+	it( 'goes back to domain add page if non-VIP site', () => {
+		const wrapper = shallow( <MapDomain selectedSiteSlug="baba" { ...defaultProps } /> );
+		wrapper.instance().goBack();
+		expect( pageSpy ).to.have.been.calledWith( '/domains/add/baba' );
+	} );
+
+	it( 'does not render a notice by default', () => {
+		const wrapper = shallow( <MapDomain { ...defaultProps } /> );
+		// we match the notice by props, because enzyme isn't matching the Notice type for some reason
+		expect( wrapper.find( { status: 'is-error' } ) ).to.have.length( 0 );
+	} );
+
+	it( 'render a notice by when there is an errorMessage in the state ', () => {
+		const wrapper = shallow( <MapDomain { ...defaultProps } /> );
+		// we match the notice by props, because enzyme isn't matching the Notice type for some reason
+		wrapper.setState( { errorMessage: 'baba' } );
+		expect( wrapper.find( { status: 'is-error' } ) ).to.have.length( 1 );
 	} );
 } );
