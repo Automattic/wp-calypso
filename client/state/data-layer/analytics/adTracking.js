f4f078c84e1835fa9b0df4129495bcee29892f32
Mark Ryall <mark@ryall.name>
2017-04-10 16:34:33 +1000

    Abort early when vertical is not present in action.

--- client/state/data-layer/analytics/adTracking.js
+++ client/state/data-layer/analytics/adTracking.js
@@ -12,16 +12,18 @@ import {
 } from 'lib/analytics/ad-tracking';
 import { SIGNUP_STEPS_SURVEY_SET } from 'state/action-types';
 
-function recordSelectedVertical( _, action, next ) {
+function recordSelectedVertical( store, action, next ) {
 	next( action );
 
 	const vertical = get( action, 'survey.vertical' );
 
-	if ( vertical ) {
-		const attributes = { vertical };
-		trackCustomFacebookConversionEvent( 'VerticalSelectedEvent', attributes );
-		trackCustomAdWordsRemarketingEvent( attributes );
+	if ( ! vertical ) {
+		return;
 	}
+
+	const attributes = { vertical };
+	trackCustomFacebookConversionEvent( 'VerticalSelectedEvent', attributes );
+	trackCustomAdWordsRemarketingEvent( attributes );
 }
 
 export default {
