623409caad17fb106ff17769de722802345b2179
Drew Butler <hi@dbtlr.com>
2017-04-11 01:15:36 +0200

    change lockdown to a toggle.

--- client/extensions/wp-super-cache/lock-down.jsx
+++ client/extensions/wp-super-cache/lock-down.jsx
@@ -7,59 +7,69 @@ import { pick } from 'lodash';
 /**
  * Internal dependencies
  */
-import Button from 'components/button';
 import Card from 'components/card';
 import SectionHeader from 'components/section-header';
+import FormToggle from 'components/forms/form-toggle/compact';
+import FormFieldset from 'components/forms/form-fieldset';
+import FormSettingExplanation from 'components/forms/form-setting-explanation';
 import WrapSettingsForm from './wrap-settings-form';
 
 const LockDown = ( {
 	fields: {
 		wp_lock_down,
 	},
+	handleToggle,
 	translate,
 } ) => {
 	return (
 		<div>
-			<SectionHeader label={ !! wp_lock_down ? translate( 'Lock Down - Enabled' ) : translate( 'Lock Down - Disabled' ) } />
+			<SectionHeader label="Lock Down" />
 			<Card>
 				<form>
-					<p>
-						{ translate(
-							'Prepare your server for an expected spike in traffic by enabling the lock down. ' +
-							'When this is enabled, new comments on a post will not refresh the cached static files.'
-						) }
-					</p>
-					<p>
-						{ translate(
-							'Developers: Make your plugin lock down compatible by checking the "WPLOCKDOWN" ' +
-							'constant. The following code will make sure your plugin respects the WPLOCKDOWN setting.'
-						) }
-					</p>
-					<p>
-						{ translate(
-							'{{code}}if ( defined( \'WPLOCKDOWN\' ) && constant( \'WPLOCKDOWN\' ) ) { echo ' +
-							'"Sorry. My blog is locked down. Updates will appear shortly"; }{{/code}}',
-							{
-								components: {
-									code: <code />,
+					<FormFieldset>
+						<FormToggle
+							checked={ !! wp_lock_down }
+							disabled={ '1' === wp_lock_down }
+							onChange={ handleToggle( 'wp_lock_down' ) }>
+							<span>
+								{ translate( 'Enable lock down to prepare your server for an expected spike in traffic.' ) }
+							</span>
+						</FormToggle>
+					</FormFieldset>
+					<FormSettingExplanation className="wp-super-cache__lock-down-explaination">
+						<p>
+							{ translate(
+								'When this is enabled, new comments on a post will not refresh the cached static files.'
+							) }
+						</p>
+						<p>
+							{ translate(
+								'Developers: Make your plugin lock down compatible by checking the "WPLOCKDOWN" ' +
+								'constant. The following code will make sure your plugin respects the WPLOCKDOWN setting.'
+							) }
+						</p>
+						<p>
+							{ translate(
+								'{{code}}if ( defined( \'WPLOCKDOWN\' ) && constant( \'WPLOCKDOWN\' ) ) { echo ' +
+								'"Sorry. My blog is locked down. Updates will appear shortly"; }{{/code}}',
+								{
+									components: {
+										code: <code />,
+									}
 								}
-							}
-						) }
-					</p>
-					<p>
-						{ !! wp_lock_down
-							? translate( 'WordPress is locked down. Super Cache static files will not be deleted ' +
-								'when new comments are made.' )
-							: translate( 'WordPress is not locked down. New comments will refresh Super Cache static files as normal.' )
-						}
-					</p>
-					<div>
-						<Button
-							compact
-							type="submit">
-							{ ! wp_lock_down ? translate( 'Enable Lock Down' ) : translate( 'Disable Lock Down' ) }
-						</Button>
-					</div>
+							) }
+						</p>
+						<p>
+							<strong>
+								{ !! wp_lock_down
+									? translate( 'WordPress is locked down. Super Cache static files will not be deleted ' +
+										'when new comments are made.' )
+									: translate( 'WordPress is not locked down. New comments will refresh Super Cache ' +
+										'static files as normal.' )
+								}
+							</strong>
+						</p>
+					</FormSettingExplanation>
 				</form>
 			</Card>
 		</div>
