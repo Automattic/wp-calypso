77f03fa9ba706e45c49f5127c33a187177d1a576
Jarda Snajdr <jsnajdr@gmail.com>
2017-04-10 17:14:55 +0200

    Reduxify the user-settings module: add QueryUserSettings component
    Added a QueryUserSettings React component. Intended to be used when
    we want to trigger loading the user settings from network.

---
+++ client/components/data/query-user-settings/index.jsx
@@ -0,0 +1,22 @@
+/**
+ * External dependencies
+ */
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { fetchUserSettings } from 'state/user-settings/actions';
+
+class QueryUserSettings extends Component {
+	componentWillMount() {
+		this.props.fetchUserSettings();
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default connect( null, { fetchUserSettings } )( QueryUserSettings );
