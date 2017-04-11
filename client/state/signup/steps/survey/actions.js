7f70837e4f89a1f17bd1a76f2b074036ac546291
Mark Ryall <mark@ryall.name>
2017-04-11 18:21:56 +1000

    Include ad tracking analytics in signup steps survey set event.

--- client/state/signup/steps/survey/actions.js
+++ client/state/signup/steps/survey/actions.js
@@ -2,10 +2,26 @@
  * Internal dependencies
  */
 import { SIGNUP_STEPS_SURVEY_SET } from 'state/action-types';
+import {
+	composeAnalytics,
+	recordCustomAdWordsRemarketingEvent,
+	recordCustomFacebookConversionEvent,
+	joinAnalytics
+} from 'state/analytics/actions';
 
 export function setSurvey( survey ) {
-	return {
-		type: SIGNUP_STEPS_SURVEY_SET,
-		survey
-	};
+	const vertical = survey.vertical;
+	const attributes = { vertical };
+	const compositeAnalytics = composeAnalytics(
+		recordCustomAdWordsRemarketingEvent( attributes ),
+		recordCustomFacebookConversionEvent( 'VerticalSelectedEvent', attributes )
+	);
+
+	return joinAnalytics(
+		compositeAnalytics,
+		{
+			type: SIGNUP_STEPS_SURVEY_SET,
+			survey
+		}
+	);
 }
