e43bef11d92e0d66a9b68fa92100ac8d4ad97d4d
Bernhard Reiter <ockham@raz.or.at>
2017-04-12 01:21:26 +0200

    Framework: Enable WPCOM API  Middleware on the Server

--- client/state/index.js
+++ client/state/index.js
@@ -145,7 +145,7 @@ export function createReduxStore( initialState = {} ) {
 		noticesMiddleware,
 		isBrowser && require( './happychat/middleware.js' ).default(),
 		isBrowser && require( './analytics/middleware.js' ).analyticsMiddleware,
-		isBrowser && require( './data-layer/wpcom-api-middleware.js' ).default,
+		require( './data-layer/wpcom-api-middleware.js' ).default,
 		isBrowser && require( './lib/middleware.js' ).default,
 		isAudioSupported && require( './audio/middleware.js' ).default,
 		isBrowser && config.isEnabled( 'automated-transfer' ) && require( './automated-transfer/middleware.js' ).default,
