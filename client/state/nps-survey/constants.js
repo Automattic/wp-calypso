c3703cf91a454afac6445071a6cc812b295c1f66
Jonathan Sadowski <sadowski@automattic.com>
2017-04-11 09:52:20 -0500

    Return constant to expected value

--- client/state/nps-survey/constants.js
+++ client/state/nps-survey/constants.js
@@ -3,4 +3,4 @@ export const SUBMITTING = 'SUBMITTING';
 export const SUBMIT_FAILURE = 'SUBMIT_FAILURE';
 export const SUBMITTED = 'SUBMITTED';
 
-export const NPS_SURVEY_RAND_MAX = 1;
+export const NPS_SURVEY_RAND_MAX = 5000;
