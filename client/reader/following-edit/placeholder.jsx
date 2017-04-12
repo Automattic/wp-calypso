e6463195cd9ff1872650875b94e8fb0c32a910a9
Ben Lowery <ben.lowery@automattic.com>
2017-04-12 10:51:47 -0400

    fix last linter errors

--- client/reader/following-edit/placeholder.jsx
+++ client/reader/following-edit/placeholder.jsx
@@ -1,25 +1,29 @@
 /**
- * External dependencies
+ * External Dependencies
  */
 import React from 'react';
+
+/**
+ * Intenal Dependencies
+ */
 
 import Card from 'components/card';
 import SiteIcon from 'blocks/site-icon';
 
-export default React.createClass( {
+export default class SubscriptionPlaceholder extends React.PureComponent {
 
-	displayName: 'SubscriptionPlaceholder',
+	static displayName = 'SubscriptionPlaceholder'
 
-	render: function() {
+	/* eslint-disable wpcalypso/jsx-classname-namespace */
+	render() {
 		return (
 			<Card className="is-compact reader-list-item__card is-placeholder">
 				<span className="reader-list-item__icon">
 					<SiteIcon size={ 48 } />
 				</span>
-
 				<h2 className="reader-list-item__title"><span className="placeholder-text">Title</span></h2>
 				<p className="reader-list-item__description"><span className="placeholder-text">URL</span></p>
 			</Card>
 			);
 	}
-} );
+}
