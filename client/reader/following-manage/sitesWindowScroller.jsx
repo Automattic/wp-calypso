119789354af6d227f2fbf28503139bc29890b823
Jake F - @samouri <samouri@users.noreply.github.com>
2017-04-12 17:19:38 -0400

    Reader Search on Manage: display subscriptions

---
+++ client/reader/following-manage/sitesWindowScroller.jsx
@@ -0,0 +1,53 @@
+/**
+ * External Dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { List, WindowScroller } from 'react-virtualized';
+import ConnectedSubscriptionListItem from './connected-subscription-list-item';
+
+/**
+ * Internal Dependencies
+ */
+
+class SitesWindowScroller extends Component {
+	static propTypes = {
+		sites: PropTypes.array.isRequired,
+	}
+
+	sitesRowRenderer = ( { index, key, style } ) => {
+		const site = this.props.sites[ index ];
+		return (
+			<div key={ key } style={ style }>
+					<ConnectedSubscriptionListItem
+						url={ site.URL }
+						feedId={ +site.feed_ID }
+						siteId={ +site.blog_ID }
+					/>
+			</div>
+		);
+	};
+
+	render() {
+		const { sites, width } = this.props;
+
+		return (
+			<div className="following-manage__sites-window-scroller">
+				<WindowScroller>
+					{ ( { height, scrollTop } ) => (
+						<List
+							autoHeight
+							height={ height }
+							rowCount={ sites.length }
+							rowHeight={ 83 }
+							rowRenderer={ this.sitesRowRenderer }
+							scrollTop={ scrollTop }
+							width={ width }
+						/>
+					)}
+				</WindowScroller>
+			</div>
+		);
+	}
+}
+
+export default SitesWindowScroller;
--- client/reader/following-manage/subscriptions.jsx
+++ client/reader/following-manage/subscriptions.jsx
@@ -1,25 +1,44 @@
 /**
  * External Dependencies
  */
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import { getReaderFollows } from 'state/selectors';
 
 /**
  * Internal Dependencies
  */
 import ReaderImportButton from 'blocks/reader-import-button';
 import ReaderExportButton from 'blocks/reader-export-button';
+import SitesWindowScroller from './sitesWindowScroller';
+import QueryReaderFollows from 'components/data/query-reader-follows';
 
 class FollowingManageSubscriptions extends Component {
+	static propTypes = {
+		follows: PropTypes.array.isRequired,
+	}
+
 	render() {
+		const { follows, width } = this.props;
 		return (
 			<div className="following-manage__subscriptions">
+				<QueryReaderFollows />
 				<div className="following-manage__subscriptions-controls">
 					<ReaderImportButton />
 					<ReaderExportButton />
+				</div>
+				<div className="following-manage__subscriptions-list">
+					<SitesWindowScroller
+						sites={ follows }
+						width={ width }
+					/>
 				</div>
 			</div>
 		);
 	}
 }
 
-export default FollowingManageSubscriptions;
+export default connect(
+	state => ( { follows: getReaderFollows( state ) } ),
+)( localize( FollowingManageSubscriptions ) );
