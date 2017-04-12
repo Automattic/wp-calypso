1ac85e896a14b819cfbd86fc28e598f97f34f9a3
bluefuton <chris@bluefuton.com>
2017-04-12 16:33:23 +0100

    Add export button, and fire notice on error

--- client/reader/following-manage/subscriptions.jsx
+++ client/reader/following-manage/subscriptions.jsx
@@ -7,6 +7,7 @@ import React, { Component } from 'react';
  * Internal Dependencies
  */
 import FollowingManageImport from './import';
+import ReaderExportButton from 'blocks/reader-export-button';
 
 class FollowingManageSubscriptions extends Component {
 	render() {
@@ -14,6 +15,7 @@ class FollowingManageSubscriptions extends Component {
 			<div className="following-manage__subscriptions">
 				<div className="following-manage__subscriptions-controls">
 					<FollowingManageImport />
+					<ReaderExportButton />
 				</div>
 			</div>
 		);
