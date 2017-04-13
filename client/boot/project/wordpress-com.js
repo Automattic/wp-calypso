f0a026d41cc0057e37c5b931d4fe39f8d02b655b
Kerry Liu <kerryliu@gmail.com>
2017-04-13 15:42:35 -0700

    Framework: remove sitesList dependency from analytics

--- client/boot/project/wordpress-com.js
+++ client/boot/project/wordpress-com.js
@@ -73,6 +73,8 @@ export const configureReduxStore = ( currentUser, reduxStore ) => {
 export function setupMiddlewares( currentUser, reduxStore ) {
 	debug( 'Executing WordPress.com setup middlewares.' );
 
+	analytics.setDispatch( reduxStore.dispatch );
+
 	if ( currentUser.get() ) {
 		// When logged in the analytics module requires user and superProps objects
 		// Inject these here
