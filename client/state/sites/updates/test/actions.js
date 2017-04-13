6fec6c76286b17449cceb1fb3c8fcb7f3e29c8f8
Marin Atanasov <tyxla@abv.bg>
2017-04-13 12:02:30 +0300

    State: Add tests for sites updates actions

---
+++ client/state/sites/updates/test/actions.js
@@ -0,0 +1,74 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import {
+	SITE_UPDATES_RECEIVE,
+	SITE_UPDATES_REQUEST,
+	SITE_UPDATES_REQUEST_SUCCESS,
+	SITE_UPDATES_REQUEST_FAILURE,
+} from 'state/action-types';
+import {
+	siteUpdatesReceiveAction,
+	siteUpdatesRequestAction,
+	siteUpdatesRequestSuccessAction,
+	siteUpdatesRequestFailureAction,
+} from '../actions';
+
+describe( 'actions', () => {
+	const siteId = 2916284;
+
+	describe( '#siteUpdatesReceiveAction()', () => {
+		it( 'should return a site updates receive action object', () => {
+			const updates = {
+				plugins: 1,
+			};
+			const action = siteUpdatesReceiveAction( siteId, updates );
+
+			expect( action ).to.eql( {
+				type: SITE_UPDATES_RECEIVE,
+				siteId,
+				updates,
+			} );
+		} );
+	} );
+
+	describe( '#siteUpdatesReceiveAction()', () => {
+		it( 'should return a site updates request action object', () => {
+			const action = siteUpdatesRequestAction( siteId );
+
+			expect( action ).to.eql( {
+				type: SITE_UPDATES_REQUEST,
+				siteId,
+			} );
+		} );
+	} );
+
+	describe( '#siteUpdatesReceiveAction()', () => {
+		it( 'should return a site updates request success action object', () => {
+			const action = siteUpdatesRequestSuccessAction( siteId );
+
+			expect( action ).to.eql( {
+				type: SITE_UPDATES_REQUEST_SUCCESS,
+				siteId,
+			} );
+		} );
+	} );
+
+	describe( '#siteUpdatesReceiveAction()', () => {
+		it( 'should return a site updates request failure action object', () => {
+			const error = 'There was an error while getting the update data for this site.';
+			const action = siteUpdatesRequestFailureAction( siteId, error );
+
+			expect( action ).to.eql( {
+				type: SITE_UPDATES_REQUEST_FAILURE,
+				siteId,
+				error,
+			} );
+		} );
+	} );
+} );
