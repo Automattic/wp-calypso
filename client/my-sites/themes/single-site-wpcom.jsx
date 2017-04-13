47ca787c0b06bf8b778627748fb89773d93c2bab
Folletto <folletto@gmail.com>
2017-04-13 16:03:06 +0100

    Themes: updated Banner, better copy, tailored to plans

--- client/my-sites/themes/single-site-wpcom.jsx
+++ client/my-sites/themes/single-site-wpcom.jsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
@@ -10,26 +11,41 @@ import CurrentTheme from 'my-sites/themes/current-theme';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import ThanksModal from 'my-sites/themes/thanks-modal';
 import { connectOptions } from './theme-options';
-import { FEATURE_ADVANCED_DESIGN } from 'lib/plans/constants';
-import UpgradeNudge from 'my-sites/upgrade-nudge';
+import Banner from 'components/banner';
+import {
+	PLAN_FREE,
+	PLAN_PERSONAL,
+	PLAN_PREMIUM,
+	PLAN_BUSINESS,
+} from 'lib/plans/constants';
+import { getCurrentPlan } from 'state/sites/plans/selectors';
 import QuerySitePlans from 'components/data/query-site-plans';
 import QuerySitePurchases from 'components/data/query-site-purchases';
 import ThemeShowcase from './theme-showcase';
 
-export default connectOptions(
+const ConnectedSingleSiteWpcom = connectOptions(
 	( props ) => {
-		const { siteId, translate } = props;
+		const { siteId, currentPlanSlug, translate } = props;
 
 		return (
 			<div>
 				<SidebarNavigation />
 				<CurrentTheme siteId={ siteId } />
-				<UpgradeNudge
-					title={ translate( 'Get Custom Design with Premium' ) }
-					message={ translate( 'Customize your theme using premium fonts, color palettes, and the CSS editor.' ) }
-					feature={ FEATURE_ADVANCED_DESIGN }
+				{ ( currentPlanSlug === PLAN_FREE || currentPlanSlug === PLAN_PERSONAL ) && <Banner
+					plan={ PLAN_PREMIUM }
+					title={ translate( 'Access all our premium themes with our Premium and Business plans!' ) }
+					description={ translate( 'Get also advanced customization, more space, and video support.' ) }
 					event="themes_custom_design"
-					/>
+				/>
+				}
+				{ ( currentPlanSlug === PLAN_PREMIUM ) && <Banner
+					plan={ PLAN_BUSINESS }
+					title={ translate( 'Why not upgrade to Business?' ) }
+					description={ translate( 'Get Google Analytics integration, more SEO tools, personalized help, and more.' ) }
+					event="themes_custom_design"
+				/>
+				}
+
 				<ThemeShowcase { ...props } siteId={ siteId }>
 					{ siteId && <QuerySitePlans siteId={ siteId } /> }
 					{ siteId && <QuerySitePurchases siteId={ siteId } /> }
@@ -39,3 +55,12 @@ export default connectOptions(
 		);
 	}
  );
+
+export default connect(
+	( state, { siteId } ) => {
+		const currentPlan = getCurrentPlan( state, siteId ) || null;
+		return {
+			currentPlanSlug: currentPlan && currentPlan.productSlug,
+		};
+	}
+)( ConnectedSingleSiteWpcom );
