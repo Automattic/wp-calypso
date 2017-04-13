e46494b5b240f024d0327b2bb76c4f1cc730f356
Marin Atanasov <tyxla@abv.bg>
2017-04-13 10:54:01 +0300

    State: Update sites updates schema

--- client/state/sites/updates/schema.js
+++ client/state/sites/updates/schema.js
@@ -1,21 +1,17 @@
 export const itemsSchema = {
-	//state.sites.vouchers = {};
 	type: 'object',
 	additionalProperties: false,
 	patternProperties: {
 		'^\\d+$': {
 			type: 'object',
-			items: {
-				type: 'object',
-				properties: {
-					jp_version: { type: 'string' },
-					plugins: { type: 'number' },
-					themes: { type: 'number' },
-					total: { type: 'number' },
-					translations: { type: 'number' },
-					wordress: { type: 'number' },
-					wp_version: { type: 'string' }
-				}
+			properties: {
+				jp_version: { type: 'string' },
+				plugins: { type: 'number' },
+				themes: { type: 'number' },
+				total: { type: 'number' },
+				translations: { type: 'number' },
+				wordress: { type: 'number' },
+				wp_version: { type: 'string' }
 			}
 		}
 	}
