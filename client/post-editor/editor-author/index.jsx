85c6f408c45c36beeb463a22be1bf2649e1fe168
Kerry Liu <kerryliu@gmail.com>
2017-04-14 16:00:43 -0700

    Framework: remove sites-list usage from post-editor

--- client/post-editor/editor-author/index.jsx
+++ client/post-editor/editor-author/index.jsx
@@ -1,7 +1,9 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -11,27 +13,25 @@ import userFactory from 'lib/user';
 import AuthorSelector from 'blocks/author-selector';
 import PostActions from 'lib/posts/actions';
 import touchDetect from 'lib/touch-detect';
-import sitesFactory from 'lib/sites-list';
 import * as stats from 'lib/posts/stats';
+import { getSelectedSite } from 'state/ui/selectors';
 
 /**
  * Module dependencies
  */
 const user = userFactory();
-const sites = sitesFactory();
 
-export default React.createClass( {
-	displayName: 'EditorAuthor',
+export class EditorAuthor extends Component {
 
-	propTypes: {
+	static propTypes = {
 		post: React.PropTypes.object,
 		isNew: React.PropTypes.bool
-	},
+	};
 
-	render: function() {
+	render() {
 		// if it's not a new post and we are still loading
 		// show a placeholder component
-		const post = this.props.post;
+		const { post, translate } = this.props;
 
 		if ( ! post && ! this.props.isNew ) {
 			return this.renderPlaceholder();
@@ -44,36 +44,35 @@ export default React.createClass( {
 
 		return (
 			<div className="editor-author">
-				<Wrapper siteId={ this.props.post.site_ID } onSelect={ this.onSelect } popoverPosition={ popoverPosition }>
+				<Wrapper siteId={ post.site_ID } onSelect={ this.onSelect } popoverPosition={ popoverPosition }>
 					<Gravatar size={ 26 } user={ author } />
 					<span className="editor-author__name">
-							{ this.translate( 'by %(name)s', { args: { name: name } } ) }
+							{ translate( 'by %(name)s', { args: { name: name } } ) }
 					</span>
 				</Wrapper>
 			</div>
 		);
-	},
+	}
 
-	renderPlaceholder: function() {
+	renderPlaceholder() {
 		return (
 			<div className="editor-author is-placeholder">
 				<Gravatar size={ 26 } />
 				<span className="editor-author__name" />
 			</div>
 		);
-	},
+	}
 
-	onSelect: function( author ) {
+	onSelect = ( author ) => {
 		stats.recordStat( 'advanced_author_changed' );
 		stats.recordEvent( 'Changed Author' );
 		// TODO: REDUX - remove flux actions when whole post-editor is reduxified
 		PostActions.edit( { author: author } );
-	},
+	};
 
-	userCanAssignAuthor: function() {
-		var post = this.props.post,
-			reassignCapability = 'edit_others_' + post.type + 's',
-			site = sites.getSite( this.props.post.site_ID );
+	userCanAssignAuthor() {
+		const { post, site } = this.props;
+		const reassignCapability = 'edit_others_' + post.type + 's';
 
 		// if user cannot edit others posts
 		if ( ! site || ! site.capabilities || ! site.capabilities[ reassignCapability ] ) {
@@ -81,6 +80,13 @@ export default React.createClass( {
 		}
 
 		return true;
-	},
+	}
 
-} );
+}
+
+export default connect(
+	( state ) => ( {
+		site: getSelectedSite( state )
+	} )
+)( localize( EditorAuthor ) );
+
