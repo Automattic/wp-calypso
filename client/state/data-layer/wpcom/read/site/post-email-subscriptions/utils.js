5a6c81932f28068278db379e989c7a1a89dfb902
Ben Lowery <ben.lowery@automattic.com>
2017-04-14 16:39:10 -0400

    break out the http bits by suffix

---
+++ client/state/data-layer/wpcom/read/site/post-email-subscriptions/utils.js
@@ -0,0 +1,22 @@
+/**
+ * External Dependencies
+ */
+import { includes } from 'lodash';
+
+/**
+ * Internal Dependencies
+ */
+
+export function buildBody( frequency ) {
+	const validFrequencies = [
+		'instantly',
+		'daily',
+		'weekly',
+	];
+	if ( includes( validFrequencies, frequency ) ) {
+		return {
+			delivery_frequency: frequency
+		};
+	}
+	return {};
+}
