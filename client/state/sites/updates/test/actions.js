90b8ff41628aade36b6ebd3c2d9984db67c8a775
Marin Atanasov <tyxla@abv.bg>
2017-04-15 15:32:09 +0300

    State: Introduce updateWordPress action

--- client/state/sites/updates/test/actions.js
+++ client/state/sites/updates/test/actions.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { expect } from 'chai';
+import sinon, { match } from 'sinon';
 
 /**
  * Internal dependencies
@@ -11,16 +12,26 @@ import {
 	SITE_UPDATES_REQUEST,
 	SITE_UPDATES_REQUEST_SUCCESS,
 	SITE_UPDATES_REQUEST_FAILURE,
+	SITE_WORDPRESS_UPDATE_REQUEST,
+	SITE_WORDPRESS_UPDATE_REQUEST_SUCCESS,
+	SITE_WORDPRESS_UPDATE_REQUEST_FAILURE,
 } from 'state/action-types';
 import {
 	siteUpdatesReceiveAction,
 	siteUpdatesRequestAction,
 	siteUpdatesRequestSuccessAction,
 	siteUpdatesRequestFailureAction,
+	updateWordPress,
 } from '../actions';
+import useNock from 'test/helpers/use-nock';
 
 describe( 'actions', () => {
 	const siteId = 2916284;
+	const spy = sinon.spy();
+
+	beforeEach( () => {
+		spy.reset();
+	} );
 
 	describe( '#siteUpdatesReceiveAction()', () => {
 		it( 'should return a site updates receive action object', () => {
@@ -68,6 +79,59 @@ describe( 'actions', () => {
 				type: SITE_UPDATES_REQUEST_FAILURE,
 				siteId,
 				error,
+			} );
+		} );
+	} );
+
+	describe( '#updateWordPress()', () => {
+		it( 'should dispatch a site wordpress update request action', () => {
+			updateWordPress( siteId )( spy );
+
+			expect( spy ).to.have.been.calledWith( {
+				type: SITE_WORDPRESS_UPDATE_REQUEST,
+				siteId,
+			} );
+		} );
+
+		describe( '#success', () => {
+			useNock( ( nock ) => {
+				nock( 'https://public-api.wordpress.com:443' )
+					.post( '/rest/v1.1/sites/' + siteId + '/core/update' )
+					.reply( 200, {
+						version: 4.7
+					} );
+			} );
+
+			it( 'should dispatch site wordpress update request success action upon success', () => {
+				return updateWordPress( siteId )( spy ).then( () => {
+					expect( spy ).to.have.been.calledWith( {
+						type: SITE_WORDPRESS_UPDATE_REQUEST_SUCCESS,
+						siteId,
+					} );
+				} );
+			} );
+		} );
+
+		describe( '#failure', () => {
+			useNock( ( nock ) => {
+				nock( 'https://public-api.wordpress.com:443' )
+					.post( '/rest/v1.1/sites/' + siteId + '/core/update' )
+					.reply( 400, {
+						error: 'up_to_date',
+						message: 'WordPress is at the latest version.',
+					} );
+			} );
+
+			it( 'should dispatch site wordpress update request failure action upon error', () => {
+				return updateWordPress( siteId )( spy ).then( () => {
+					expect( spy ).to.have.been.calledWith( {
+						type: SITE_WORDPRESS_UPDATE_REQUEST_FAILURE,
+						siteId,
+						error: match( {
+							message: 'WordPress is at the latest version.'
+						} )
+					} );
+				} );
 			} );
 		} );
 	} );
