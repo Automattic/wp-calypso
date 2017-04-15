e8add854d171ecc27795edbf9641ac5fad5fd55d
Marin Atanasov <tyxla@abv.bg>
2017-04-15 14:54:28 +0300

    State: Introduce requestConnectionStatus action

---
+++ client/state/sites/connection/actions.js
@@ -0,0 +1,45 @@
+/**
+ * Internal dependencies
+ */
+import {
+	SITE_CONNECTION_STATUS_RECEIVE,
+	SITE_CONNECTION_STATUS_REQUEST,
+	SITE_CONNECTION_STATUS_REQUEST_FAILURE,
+	SITE_CONNECTION_STATUS_REQUEST_SUCCESS,
+} from 'state/action-types';
+import wp from 'lib/wp';
+
+/**
+ * Request the Jetpack connection status for a certain site.
+ *
+ * @param  {Int}       siteId  ID of the site.
+ * @return {Function}          Action thunk to request the Jetpack connection status when called.
+ */
+export const requestConnectionStatus = ( siteId ) => {
+	return ( dispatch ) => {
+		dispatch( {
+			type: SITE_CONNECTION_STATUS_REQUEST,
+			siteId
+		} );
+
+		return wp.undocumented().testConnectionJetpack( siteId )
+			.then( ( response ) => {
+				dispatch( {
+					type: SITE_CONNECTION_STATUS_RECEIVE,
+					siteId,
+					status: response.connected,
+				} );
+
+				dispatch( {
+					type: SITE_CONNECTION_STATUS_REQUEST_SUCCESS,
+					siteId,
+				} );
+			} ).catch( ( error ) => {
+				dispatch( {
+					type: SITE_CONNECTION_STATUS_REQUEST_FAILURE,
+					siteId,
+					error,
+				} );
+			} );
+	};
+};
