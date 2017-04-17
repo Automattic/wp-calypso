d84fc1422864b311e50123a72cea5580a4ce0304
Kerry Liu <gwwar@users.noreply.github.com>
2017-04-17 11:32:42 -0700

    Revert "Revert "Framework: remove siteslist usage from lib/cart (#13124)" (#13153)"
    This reverts commit dfd6d018f89640cfeca26b1cd1bc71f9609341fa.

--- client/state/lib/middleware.js
+++ client/state/lib/middleware.js
@@ -7,12 +7,25 @@ import { get } from 'lodash';
  * Internal dependencies
  */
 import {
-	ANALYTICS_SUPER_PROPS_UPDATE
+	ANALYTICS_SUPER_PROPS_UPDATE,
+	SELECTED_SITE_SET,
+	SITE_RECEIVE,
+	SITES_RECEIVE,
+	SITES_UPDATE,
 } from 'state/action-types';
 import analytics from 'lib/analytics';
-import { getSelectedSite } from 'state/ui/selectors';
+import cartStore from 'lib/cart/store';
+import { getSelectedSite, getSelectedSiteId } from 'state/ui/selectors';
 import { getCurrentUser } from 'state/current-user/selectors';
 
+/**
+ * Sets the selectedSite and siteCount for lib/analytics. This is used to
+ * populate extra fields on tracks analytics calls.
+ *
+ * @param {function} dispatch - redux dispatch function
+ * @param {object}   action   - the dispatched action
+ * @param {function} getState - redux getState function
+ */
 const updateSelectedSiteForAnalytics = ( dispatch, action, getState ) => {
 	const state = getState();
 	const selectedSite = getSelectedSite( state );
@@ -22,14 +35,38 @@ const updateSelectedSiteForAnalytics = ( dispatch, action, getState ) => {
 	analytics.setSiteCount( siteCount );
 };
 
-export const handlers = {
-	[ ANALYTICS_SUPER_PROPS_UPDATE ]: updateSelectedSiteForAnalytics,
+/**
+ * Sets the selectedSiteId for lib/cart/store
+ *
+ * @param {function} dispatch - redux dispatch function
+ * @param {object}   action   - the dispatched action
+ * @param {function} getState - redux getState function
+ */
+const updateSelectedSiteForCart = ( dispatch, action, getState ) => {
+	const state = getState();
+	const selectedSiteId = getSelectedSiteId( state );
+	cartStore.setSelectedSiteId( selectedSiteId );
+};
+
+const handler = ( dispatch, action, getState ) => {
+	switch ( action.type ) {
+		case ANALYTICS_SUPER_PROPS_UPDATE:
+			return updateSelectedSiteForAnalytics( dispatch, action, getState );
+
+		case SELECTED_SITE_SET:
+		case SITE_RECEIVE:
+		case SITES_RECEIVE:
+		case SITES_UPDATE:
+			//Wait a tick for the reducer to update the state tree
+			setTimeout( () => {
+				updateSelectedSiteForCart( dispatch, action, getState );
+			}, 0 );
+			return;
+	}
 };
 
 export const libraryMiddleware = ( { dispatch, getState } ) => ( next ) => ( action ) => {
-	if ( handlers.hasOwnProperty( action.type ) ) {
-		handlers[ action.type ]( dispatch, action, getState );
-	}
+	handler( dispatch, action, getState );
 
 	return next( action );
 };
