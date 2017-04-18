92f3364dd15d5cbb4f22c84433bec44a51ba89f1
Marin Atanasov <tyxla@abv.bg>
2017-04-18 17:10:25 +0300

    State: Update connection reducer

--- client/state/sites/connection/reducer.js
+++ client/state/sites/connection/reducer.js
@@ -2,12 +2,11 @@
  * External dependencies
  */
 import { combineReducers } from 'redux';
-import { stubFalse, stubTrue } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import { createReducer, keyedReducer } from 'state/utils';
+import { createReducer } from 'state/utils';
 import {
 	SITE_CONNECTION_STATUS_RECEIVE,
 	SITE_CONNECTION_STATUS_REQUEST,
@@ -15,15 +14,18 @@ import {
 	SITE_CONNECTION_STATUS_REQUEST_SUCCESS,
 } from 'state/action-types';
 
+const createRequestingReducer = ( requesting ) =>
+	( state, { siteId } ) => ( { ...state, [ siteId ]: requesting } );
+
 export const items = createReducer( {}, {
 	[ SITE_CONNECTION_STATUS_RECEIVE ]: ( state, { siteId, status } ) => ( { ...state, [ siteId ]: status } ),
 } );
 
-export const requesting = keyedReducer( 'siteId', createReducer( {}, {
-	[ SITE_CONNECTION_STATUS_REQUEST ]: stubTrue,
-	[ SITE_CONNECTION_STATUS_REQUEST_SUCCESS ]: stubFalse,
-	[ SITE_CONNECTION_STATUS_REQUEST_FAILURE ]: stubFalse,
-} ) );
+export const requesting = createReducer( {}, {
+	[ SITE_CONNECTION_STATUS_REQUEST ]: createRequestingReducer( true ),
+	[ SITE_CONNECTION_STATUS_REQUEST_FAILURE ]: createRequestingReducer( false ),
+	[ SITE_CONNECTION_STATUS_REQUEST_SUCCESS ]: createRequestingReducer( false ),
+} );
 
 export default combineReducers( {
 	items,
