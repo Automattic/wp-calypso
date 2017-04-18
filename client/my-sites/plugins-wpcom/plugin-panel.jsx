283eae1b521a60b46ac2deae0752cbdba2aaa862
Kerry Liu <gwwar@users.noreply.github.com>
2017-04-18 15:42:17 -0700

    Framework: update lib/automated-transfer interface and remove usage (try two) (#13200)

--- client/my-sites/plugins-wpcom/plugin-panel.jsx
+++ client/my-sites/plugins-wpcom/plugin-panel.jsx
@@ -23,7 +23,7 @@ import JetpackPluginsPanel from './jetpack-plugins-panel';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
 import { PLAN_BUSINESS, FEATURE_UPLOAD_PLUGINS } from 'lib/plans/constants';
 import Banner from 'components/banner';
-import { isATEnabledForCurrentSite } from 'lib/automated-transfer';
+import { isATEnabled } from 'lib/automated-transfer';
 
 export const PluginPanel = ( {
 	plan,
@@ -31,6 +31,7 @@ export const PluginPanel = ( {
 	category,
 	search,
 	translate,
+	atEnabled
 } ) => {
 	const hasBusiness = isBusiness( plan ) || isEnterprise( plan );
 	const hasPremium = hasBusiness || isPremium( plan );
@@ -40,7 +41,7 @@ export const PluginPanel = ( {
 
 			<PageViewTracker path="/plugins/:site" title="Plugins > WPCOM Site" />
 
-			{ isATEnabledForCurrentSite() && ! hasBusiness &&
+			{ atEnabled && ! hasBusiness &&
 				<Banner
 					feature={ FEATURE_UPLOAD_PLUGINS }
 					plan={ PLAN_BUSINESS }
@@ -60,9 +61,13 @@ export const PluginPanel = ( {
 	);
 };
 
-const mapStateToProps = state => ( {
-	plan: get( getSelectedSite( state ), 'plan', {} ),
-	siteSlug: getSiteSlug( state, getSelectedSiteId( state ) )
-} );
+const mapStateToProps = state => {
+	const selectedSite = getSelectedSite( state );
+	return {
+		atEnabled: isATEnabled( selectedSite ),
+		plan: get( selectedSite, 'plan', {} ),
+		siteSlug: getSiteSlug( state, getSelectedSiteId( state ) )
+	};
+};
 
 export default connect( mapStateToProps )( localize( PluginPanel ) );
