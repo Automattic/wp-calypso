85135b78380510f39b1572e6d6792b7d8f5d5f7b
Jake F - @samouri <samouri@users.noreply.github.com>
2017-04-19 15:53:30 -0400

    add in functionality for custom query key

--- client/lib/url-search/index.js
+++ client/lib/url-search/index.js
@@ -4,13 +4,41 @@
 import React from 'react';
 import debugModule from 'debug';
 import page from 'page';
+import url from 'url';
+import { pick } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import buildUrl from 'lib/mixins/url-search/build-url';
-
 const debug = debugModule( 'calypso:url-search' );
+
+/**
+ * Function for constructing the url to page to. e.g.
+ * { uri: 'google.com', search:'hello' } --> 'google.com?s=hello'
+ *
+ * @param {Object} options the options object
+ * @param {string} options.uri the base uri to modify and add a query to
+ * @param {string} options.search the search term
+ * @param {string} [options.queryKey = s] the key to place in the url.  defaults to s
+ *
+ * @returns {string} The built search url
+ */
+const buildSearchUrl = ( { uri, search, queryKey = 's' } ) => {
+	const parsedUrl = pick(
+		url.parse( uri, true ),
+		'pathname',
+		'hash',
+		'query',
+	);
+
+	if ( search ) {
+		parsedUrl.query[ queryKey ] = search;
+	} else {
+		delete parsedUrl.query[ queryKey ];
+	}
+
+	return url.format( parsedUrl ).replace( /\%20/g, '+' );
+};
 
 const UrlSearch = Component => class extends Component {
 	static displayName = Component.displayName || Component.name || '';
@@ -21,20 +49,24 @@ const UrlSearch = Component => class extends Component {
 
 	componentWillReceiveProps = ( { search } ) => ! search && this.setState( { searchOpen: false } );
 
-	doSearch = ( keywords ) => {
+	doSearch = ( query ) => {
 		this.setState( {
-			searchOpen: ( false !== keywords )
+			searchOpen: ( false !== query )
 		} );
 
 		if ( this.onSearch ) {
-			this.onSearch( keywords );
+			this.onSearch( query );
 			return;
 		}
 
-		const searchURL = buildUrl( window.location.href, keywords );
+		const searchURL = buildSearchUrl( {
+			uri: window.location.href,
+			search: query,
+			queryKey: this.props.queryKey
+		} );
 
-		debug( 'search posts for:', keywords );
-		if ( this.props.search && keywords ) {
+		debug( 'search for:', query );
+		if ( this.props.search && query ) {
 			debug( 'replacing URL: ' + searchURL );
 			page.replace( searchURL );
 		} else {
