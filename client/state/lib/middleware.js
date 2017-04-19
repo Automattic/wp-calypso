2c39ab1c108ded491ba815594d4c3681ca274688
Kerry Liu <kerryliu@gmail.com>
2017-04-18 18:03:00 -0700

    Framework: remove sites-list usage from keyboard shortcuts

--- client/state/lib/middleware.js
+++ client/state/lib/middleware.js
@@ -6,6 +6,7 @@ import { get } from 'lodash';
 /**
  * Internal dependencies
  */
+import config from 'config';
 import {
 	ANALYTICS_SUPER_PROPS_UPDATE,
 	SELECTED_SITE_SET,
@@ -17,6 +18,11 @@ import analytics from 'lib/analytics';
 import cartStore from 'lib/cart/store';
 import { getSelectedSite, getSelectedSiteId } from 'state/ui/selectors';
 import { getCurrentUser } from 'state/current-user/selectors';
+
+let keyboardShortcuts;
+if ( config.isEnabled( 'keyboard-shortcuts' ) ) {
+	keyboardShortcuts = require( 'lib/keyboard-shortcuts/global' )();
+}
 
 /**
  * Sets the selectedSite and siteCount for lib/analytics. This is used to
@@ -48,6 +54,19 @@ const updateSelectedSiteForCart = ( dispatch, action, getState ) => {
 	cartStore.setSelectedSiteId( selectedSiteId );
 };
 
+/**
+ * Sets the selectedSiteId for lib/keyboard-shortcuts/global
+ *
+ * @param {function} dispatch - redux dispatch function
+ * @param {object}   action   - the dispatched action
+ * @param {function} getState - redux getState function
+ */
+const updatedSelectedSiteForKeyboardShortcuts = ( dispatch, action, getState ) => {
+	const state = getState();
+	const selectedSite = getSelectedSite( state );
+	keyboardShortcuts.setSelectedSite( selectedSite );
+};
+
 const handler = ( dispatch, action, getState ) => {
 	switch ( action.type ) {
 		case ANALYTICS_SUPER_PROPS_UPDATE:
@@ -60,6 +79,9 @@ const handler = ( dispatch, action, getState ) => {
 			// Wait a tick for the reducer to update the state tree
 			setTimeout( () => {
 				updateSelectedSiteForCart( dispatch, action, getState );
+				if ( config.isEnabled( 'keyboard-shortcuts' ) ) {
+					updatedSelectedSiteForKeyboardShortcuts( dispatch, action, getState );
+				}
 			}, 0 );
 			return;
 	}
