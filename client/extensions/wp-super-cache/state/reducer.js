9d5343f06d074693345a60f943a713388e41f4d1
Donna Peplinskie <donnapep@gmail.com>
2017-04-19 11:15:29 -0400

    Handle save actions in reducer
    Track the current status of the save operation.
    Update the settings state if the update was successful.

--- client/extensions/wp-super-cache/state/reducer.js
+++ client/extensions/wp-super-cache/state/reducer.js
@@ -13,6 +13,10 @@ import {
 	WP_SUPER_CACHE_REQUEST_SETTINGS,
 	WP_SUPER_CACHE_REQUEST_SETTINGS_FAILURE,
 	WP_SUPER_CACHE_REQUEST_SETTINGS_SUCCESS,
+	WP_SUPER_CACHE_SAVE_SETTINGS,
+	WP_SUPER_CACHE_SAVE_SETTINGS_FAILURE,
+	WP_SUPER_CACHE_SAVE_SETTINGS_SUCCESS,
+	WP_SUPER_CACHE_UPDATE_SETTINGS,
 } from './action-types';
 
 /**
@@ -30,17 +34,60 @@ const requesting = createReducer( {}, {
 } );
 
 /**
+ * Returns the updated saving state after an action has been dispatched.
+ * Saving state tracks whether the settings for a site are currently being saved.
+ *
+ * @param  {Object} state Current saving state
+ * @param  {Object} action Action object
+ * @return {Object} Updated saving state
+ */
+const saveStatus = createReducer( {}, {
+	[ WP_SUPER_CACHE_SAVE_SETTINGS ]: ( state, { siteId } ) => ( {
+		...state,
+		[ siteId ]: {
+			saving: true,
+			status: 'pending',
+			error: false,
+		}
+	} ),
+	[ WP_SUPER_CACHE_SAVE_SETTINGS_SUCCESS ]: ( state, { siteId } ) => ( {
+		...state,
+		[ siteId ]: {
+			saving: false,
+			status: 'success',
+			error: false,
+		}
+	} ),
+	[ WP_SUPER_CACHE_SAVE_SETTINGS_FAILURE ]: ( state, { siteId, error } ) => ( {
+		...state,
+		[ siteId ]: {
+			saving: false,
+			status: 'error',
+			error,
+		}
+	} )
+} );
+
+/**
  * Tracks the settings for a particular site.
  *
- * @param  {String} state Current settings
+ * @param  {Object} state Current settings
  * @param  {Object} action Action object
- * @return {String} Updated settings
+ * @return {Object} Updated settings
  */
 const items = createReducer( {}, {
-	[ WP_SUPER_CACHE_RECEIVE_SETTINGS ]: ( state, { siteId, settings } ) => ( { ...state, [ siteId ]: settings } )
+	[ WP_SUPER_CACHE_RECEIVE_SETTINGS ]: ( state, { siteId, settings } ) => ( { ...state, [ siteId ]: settings } ),
+	[ WP_SUPER_CACHE_UPDATE_SETTINGS ]: ( state, { siteId, settings } ) => ( {
+		...state,
+		[ siteId ]: {
+			...state[ siteId ],
+			...settings,
+		}
+	} ),
 }, settingsSchema );
 
 export default combineReducers( {
 	items,
 	requesting,
+	saveStatus,
 } );
