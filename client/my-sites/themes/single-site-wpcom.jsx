9390e165cdbc579baef61adcdde309ab6ce5e682
Davide Casali <folletto.id@gmail.com>
2017-04-19 12:55:12 +0100

    Themes: updated Banner, better copy, tailored to Free/Personal plans (#13088)
    * Updated Banner.
    * Better copy.
    * Tailored to Free and Personal only.
    * Design tweaks to fix vertical spacing differences.
    * New tracking code

--- client/my-sites/themes/single-site-wpcom.jsx
+++ client/my-sites/themes/single-site-wpcom.jsx
@@ -2,6 +2,8 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies
@@ -10,26 +12,35 @@ import CurrentTheme from 'my-sites/themes/current-theme';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import ThanksModal from 'my-sites/themes/thanks-modal';
 import { connectOptions } from './theme-options';
-import { FEATURE_ADVANCED_DESIGN } from 'lib/plans/constants';
-import UpgradeNudge from 'my-sites/upgrade-nudge';
+import Banner from 'components/banner';
+import {
+	PLAN_FREE,
+	PLAN_PERSONAL,
+	PLAN_PREMIUM,
+} from 'lib/plans/constants';
+import { getCurrentPlan } from 'state/sites/plans/selectors';
 import QuerySitePlans from 'components/data/query-site-plans';
 import QuerySitePurchases from 'components/data/query-site-purchases';
 import ThemeShowcase from './theme-showcase';
 
-export default connectOptions(
+const ConnectedSingleSiteWpcom = connectOptions(
 	( props ) => {
-		const { siteId, translate } = props;
+		const { siteId, currentPlanSlug, translate } = props;
 
 		return (
 			<div>
 				<SidebarNavigation />
 				<CurrentTheme siteId={ siteId } />
-				<UpgradeNudge
-					title={ translate( 'Get Custom Design with Premium' ) }
-					message={ translate( 'Customize your theme using premium fonts, color palettes, and the CSS editor.' ) }
-					feature={ FEATURE_ADVANCED_DESIGN }
-					event="themes_custom_design"
-					/>
+				{ ( currentPlanSlug === PLAN_FREE || currentPlanSlug === PLAN_PERSONAL ) && <Banner
+					plan={ PLAN_PREMIUM }
+					title={ translate( 'Access all our premium themes with our Premium and Business plans!' ) }
+					description={
+						translate( 'Get advanced customization, more storage space, and video support along with all your new themes.' )
+					}
+					event="themes_plans_free_personal"
+				/>
+				}
+
 				<ThemeShowcase { ...props } siteId={ siteId }>
 					{ siteId && <QuerySitePlans siteId={ siteId } /> }
 					{ siteId && <QuerySitePurchases siteId={ siteId } /> }
@@ -39,3 +50,12 @@ export default connectOptions(
 		);
 	}
  );
+
+export default connect(
+	( state, { siteId } ) => {
+		const currentPlan = getCurrentPlan( state, siteId );
+		return {
+			currentPlanSlug: get( currentPlan, 'productSlug', null ),
+		};
+	}
+)( ConnectedSingleSiteWpcom );
