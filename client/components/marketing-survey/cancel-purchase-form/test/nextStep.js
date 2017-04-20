3e6f5860512e1007e6f552b6c7c7ebd912625213
Mark Ryall <mark@ryall.name>
2017-04-20 23:58:16 +1000

    Simplify tests for nextStep and previousStep.

--- client/components/marketing-survey/cancel-purchase-form/test/nextStep.js
+++ client/components/marketing-survey/cancel-purchase-form/test/nextStep.js
@@ -7,58 +7,35 @@ import { expect } from 'chai';
  * Internal dependencies
  */
 import useMockery from 'test/helpers/use-mockery';
-import {
-	INITIAL_STEP,
-	CONCIERGE_STEP,
-	HAPPYCHAT_STEP,
-	FINAL_STEP,
-} from '../steps';
-import mockProductValues from './mockProductValues';
 
 describe( 'nextStep', function() {
 	const product = {};
-	const survey = { questionOneRadio: 'tooHard' };
+	const survey = {};
 
 	let nextStep;
 
 	useMockery( ( mockery ) => {
-		mockery.registerMock( 'lib/product-values', mockProductValues() );
+		const stepsForProductAndSurvey = ( s, p ) => {
+			expect( s ).to.equal( survey );
+			expect( p ).to.equal( product );
+			return [ 'a', 'b', 'c' ];
+		};
+		mockery.registerMock( './stepsForProductAndSurvey', stepsForProductAndSurvey );
 	} );
 
 	before( function() {
 		nextStep = require( '../nextStep' );
 	} );
 
-	beforeEach( function() {
-		product.isBusiness = () => false;
-		product.isPersonal = () => false;
-		product.isPremium = () => false;
+	it( 'should returning false if current step is unknown', function() {
+		expect( nextStep( 'unknown', survey, product ) ).to.equal( false );
 	} );
 
-	it( 'should return false from unknown step', function() {
-		expect( nextStep( 'unknown_step' ) ).to.equal( false );
+	it( 'should returning false if current step is final step', function() {
+		expect( nextStep( 'c', survey, product ) ).to.equal( false );
 	} );
 
-	it( 'should return false from final step', function() {
-		expect( nextStep( FINAL_STEP ) ).to.equal( false );
-	} );
-
-	it( 'should return final step from initial step without survey data or product', function() {
-		expect( nextStep( INITIAL_STEP ) ).to.equal( FINAL_STEP );
-	} );
-
-	it( 'should return concierge step from initial step when question one answer is "too hard" and business product', function() {
-		product.isBusiness = () => true;
-		expect( nextStep( INITIAL_STEP, survey, product ) ).to.equal( CONCIERGE_STEP );
-	} );
-
-	it( 'should return happychat step from initial step when question one answer is "too hard" and personal product', function() {
-		product.isPersonal = () => true;
-		expect( nextStep( INITIAL_STEP, survey, product ) ).to.equal( HAPPYCHAT_STEP );
-	} );
-
-	it( 'should return happychat step from initial step when question one answer is "too hard" and premium product', function() {
-		product.isPremium = () => true;
-		expect( nextStep( INITIAL_STEP, survey, product ) ).to.equal( HAPPYCHAT_STEP );
+	it( 'should return next step current step is earlier step', function() {
+		expect( nextStep( 'a', survey, product ) ).to.equal( 'b' );
 	} );
 } );
--- client/components/marketing-survey/cancel-purchase-form/test/previousStep.js
+++ client/components/marketing-survey/cancel-purchase-form/test/previousStep.js
@@ -7,55 +7,35 @@ import { expect } from 'chai';
  * Internal dependencies
  */
 import useMockery from 'test/helpers/use-mockery';
-import {
-	INITIAL_STEP,
-	CONCIERGE_STEP,
-	HAPPYCHAT_STEP,
-	FINAL_STEP,
-} from '../steps';
-import mockProductValues from './mockProductValues';
 
 describe( 'previousStep', function() {
 	const product = {};
-	const survey = { questionOneRadio: 'tooHard' };
+	const survey = {};
 
 	let previousStep;
 
 	useMockery( ( mockery ) => {
-		mockery.registerMock( 'lib/product-values', mockProductValues() );
+		const stepsForProductAndSurvey = ( s, p ) => {
+			expect( s ).to.equal( survey );
+			expect( p ).to.equal( product );
+			return [ 'a', 'b', 'c' ];
+		};
+		mockery.registerMock( './stepsForProductAndSurvey', stepsForProductAndSurvey );
 	} );
 
 	before( function() {
 		previousStep = require( '../previousStep' );
 	} );
 
-	beforeEach( function() {
-		product.isBusiness = () => false;
-		product.isPersonal = () => false;
-		product.isPremium = () => false;
+	it( 'should returning false if current step is unknown', function() {
+		expect( previousStep( 'unknown', survey, product ) ).to.equal( false );
 	} );
 
 	it( 'should returning false if current step is initial step', function() {
-		expect( previousStep( INITIAL_STEP ) ).to.equal( false );
+		expect( previousStep( 'a', survey, product ) ).to.equal( false );
 	} );
 
-	it( 'should return initial step if current step is happychat step', function() {
-		product.isPremium = () => true;
-		expect( previousStep( HAPPYCHAT_STEP, survey, product ) ).to.equal( INITIAL_STEP );
-	} );
-
-	it( 'should return happychat step from final step when question one answer is "too hard" and premium product', function() {
-		product.isPremium = () => true;
-		expect( previousStep( FINAL_STEP, survey, product ) ).to.equal( HAPPYCHAT_STEP );
-	} );
-
-	it( 'should return initial step if current step is concierge step', function() {
-		product.isBusiness = () => true;
-		expect( previousStep( CONCIERGE_STEP, survey, product ) ).to.equal( INITIAL_STEP );
-	} );
-
-	it( 'should return concierge step from final step when question one answer is "too hard" and business product', function() {
-		product.isBusiness = () => true;
-		expect( previousStep( FINAL_STEP, survey, product ) ).to.equal( CONCIERGE_STEP );
+	it( 'should return previous step current step is subsequent step', function() {
+		expect( previousStep( 'c', survey, product ) ).to.equal( 'b' );
 	} );
 } );
