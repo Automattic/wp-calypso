a80d108f2a164eaa0ce637daff90aac792a465e5
Donna Peplinskie <donnapep@gmail.com>
2017-04-20 07:18:33 -0400

    Refactor selectors to be more concise

--- client/extensions/wp-super-cache/state/selectors.js
+++ client/extensions/wp-super-cache/state/selectors.js
@@ -6,14 +6,12 @@ import { get } from 'lodash';
 /**
  * Returns true if we are requesting settings for the specified site ID, false otherwise.
  *
- * @param  {Object}  reduxState Global state tree
+ * @param  {Object}  state Global state tree
  * @param  {Number}  siteId Site ID
  * @return {Boolean} Whether settings are being requested
  */
-export function isRequestingSettings( reduxState, siteId ) {
-	const state = reduxState.extensions.wpSuperCache;
-
-	return state ? get( state.requesting, [ siteId ], false ) : false;
+export function isRequestingSettings( state, siteId ) {
+	return get( state, [ 'extensions', 'wpSuperCache', 'requesting', siteId ], false );
 }
 
 /**
@@ -41,14 +39,12 @@ export function isSettingsSaveSuccessful( state, siteId ) {
 /**
  * Returns the settings for the specified site ID.
  *
- * @param  {Object} reduxState Global state tree
+ * @param  {Object} state Global state tree
  * @param  {Number} siteId Site ID
  * @return {Object} Settings
  */
-export function getSettings( reduxState, siteId ) {
-	const state = reduxState.extensions.wpSuperCache;
-
-	return state ? get( state.items, [ siteId ], null ) : null;
+export function getSettings( state, siteId ) {
+	return get( state, [ 'extensions', 'wpSuperCache', 'items', siteId ], null );
 }
 
 /**
