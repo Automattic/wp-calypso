b58b7c33219b407ccd0d2e8585bc5af7740a213e
Elio Rivero <eliorivero@gmail.com>
2017-04-21 15:55:58 -0300

    Activity: add icon to rewind button

--- client/my-sites/stats/activity-log-date/index.jsx
+++ client/my-sites/stats/activity-log-date/index.jsx
@@ -3,6 +3,8 @@
  */
 import React from 'react';
 import { localize } from 'i18n-calypso';
+import Gridicon from 'gridicons';
+
 /**
  * Internal dependencies
  */
@@ -12,20 +14,52 @@ import ActivityLogItem from '../activity-log-item';
 
 const ActivityLogDate = React.createClass( {
 
-	render() {
+	/**
+	 * Return a button to rewind to this point.
+	 *
+	 * @param {string} type Whether the button will be a primary or not.
+	 * @returns { object } Button to display.
+	 */
+	getRewindButton( type = '' ) {
+		return (
+			<Button primary={ 'primary' === type } compact>
+				<Gridicon icon={ 'history' } size={ 18 } /> {
+				this.props.translate( 'Rewind to this day' )
+			}
+			</Button>
+		);
+	},
+
+	/**
+	 * Return a heading that serves as parent for many events.
+	 *
+	 * @returns { object } Heading to display with date and number of events
+	 */
+	getEventsHeading() {
 		const {
 			logs,
-			moment
-		} = this.props;
+			moment,
+			translate
+			} = this.props;
+		return (
+			<div>
+				<div className="activity-log-date__day">{ moment( logs[ 0 ].timestamp ).format( 'LL' )}</div>
+				<div className="activity-log-date__events"> { logs.length } { translate( 'Events' ) }</div>
+			</div>
+		);
+	},
 
+	render() {
+		const {
+			logs
+		} = this.props;
 
 		return (
 			<div className="activity-log-date">
 				<FoldableCard
-					header={ <div><div className="activity-log-date__day">{ moment ( logs[0].timestamp ).format( 'LL' )}</div><div className="activity-log-date__events"> { logs.length } Events</div></div> }
-					summary={ <Button primary compact className="button">Rewind to this day</Button> }
-					expandedSummary={ <Button compact className="button">Rewind to this day</Button> }
-					clickableHeader={ true }
+					header={ this.getEventsHeading() }
+					summary={ this.getRewindButton( 'primary' ) }
+					expandedSummary={ this.getRewindButton() }
 				>
 					{ logs.map( ( log, index )  => {
 						return <ActivityLogItem
