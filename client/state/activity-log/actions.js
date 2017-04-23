26b536489ba6f49db71863ccac507b6f8b5bac3b
Derek Smart <smart@automattic.com>
2017-04-23 12:50:25 -0600

    Generate hourly backup objects for the log based on a hard-coded timestamp

--- client/state/activity-log/actions.js
+++ client/state/activity-log/actions.js
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import { pick } from 'lodash';
+// import { pick } from 'lodash';
+import moment from 'moment';
 
 /**
  * Internal dependencies
@@ -20,7 +21,8 @@ import {
 	REWIND_STATUS_REQUEST,
 	REWIND_STATUS_UPDATE,
 	ACTIVITY_LOG_FETCH,
-	ACTIVITY_LOG_FETCH_FAILED,
+	// ACTIVITY_LOG_FETCH_FAILED,
+	// ACTIVITY_LOG_FETCH_SUCCESS,
 	ACTIVITY_LOG_FETCH_SUCCESS,
 } from 'state/action-types';
 import wpcom from 'lib/wp';
@@ -141,152 +143,178 @@ export function rewindRestoreUpdateError( siteId, timestamp, error ) {
 	};
 }
 
+// This generates a fake data set for hourly backups
+const fakebackups = () => {
+	const today = moment( new Date() ),
+		beginning = '2017-2-21 01:00:00',
+		numHours = today.diff( beginning, 'hours' ),
+		backupLogs = [];
+
+	let i,
+		timeIncrement = today.toString();
+
+	for ( i = 0; i < numHours; i++ ) {
+		backupLogs.push(
+			{
+				title: 'Site backed up',
+				description: 'We backed up your site',
+				user: null,
+				type: 'site_backed_up',
+				timestamp: timeIncrement
+			}
+		);
+		timeIncrement = moment( timeIncrement ).subtract( 1, 'h' ).toString();
+	}
+
+	return backupLogs;
+};
+
 export function getActivityLogData( siteId ) {
-	const logs = [
-		{
-			title: 'Site has not backed up',
-			description: 'We couldn\'t establish a connection to your site.',
-			user: null,
-			type: 'site_backed_up',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Site has backed up Failed',
-			description: 'We couldn\'t establish a connection to your site.',
-			user: null,
-			type: 'site_backed_up_failed',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Suspicious code detected in 2 plugin files.',
-			description: 'Rewound in 17 January 2017 at 3:30 PM - We found potential warmful code in two of your Plugins: Yoast SEO and Advanced Custom Fields.',
-			user: null,
-			type: 'suspicious_code',
-			timestamp: 1485220539222,
-			className: 'is-disabled',
-		},
-		{
-			title: 'Akismet activated',
-			description: 'Akismet Plugin was successfully activated.',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'plugin_activated',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Akismet deactivated',
-			description: 'Akismet Plugin was successfully deactivated.',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'plugin_deactivated',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Jetpack version 4.6 is available',
-			description: 'A new version of the Jetpack Plugin was been released. Click here to update, or turn on auto-updates for Plugins and we\'ll manage those for you',
-			user: null,
-			type: 'plugin_needs_update',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Akismet updated to version 3.2',
-			description: 'Akismet Plugin was successfully updated to its latest version: 3.2.',
-			user: null,
-			type: 'plugin_updated',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Twenty Eighteen Theme was activated',
-			description: 'TwentyEighteen Plugin was successfully activated.',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'theme_switched',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Twenty Sixteen updated to version 1.0.1',
-			description: 'Twenty Sixteen Plugin was successfully updated to its latest version: 1.0.1.',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'theme_updated',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Site updated to Professional Plan, Thank you',
-			description: 'Professional Plan was successfully purchased for your site and is valid until February 15, 2018.',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'plan_updated',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Professional Plan Renewed for another month',
-			description: 'Professional Plan was renewed for another month and is valid until February 28, 2017',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'plan_renewed',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Photon was activated',
-			description: 'Photon module was activated in your site. All your images will now be served through the WordPress.com worldwide network.',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'activate_jetpack_feature',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Custom CSS was deactivated',
-			description: 'Custom CSS module was deactivated.',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'deactivate_jetpack_feature',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'This is some really cool post',
-			description: '',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'site_backed_up',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'This is some really cool post',
-			description: '',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'site_backed_up',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'This is some really cool post',
-			description: '',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			type: 'site_backed_up',
-			timestamp: 1485220539222
-		},
-		{
-			title: 'Jetpack updated to 4.5.1',
-			subTitle: 'Plugin Update',
-			description: '',
-			icon: 'plugins',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			time: '4:32pm',
-			actionText: 'Undo',
-			timestamp: 1483351202400
-		},
-		{
-			title: 'Jetpack updated to 4.5.1',
-			subTitle: 'Plugin Activated',
-			description: '',
-			icon: 'plugins',
-			user: { ID: 123, name: 'Jane A', role: 'Admin' },
-			time: '4:32pm',
-			actionText: 'Undo',
-			timestamp: 1483351202420
-		},
-		{
-			title: 'Post Title',
-			subTitle: 'Post Updated',
-			description: '',
-			icon: 'posts',
-			user: { ID: 333, name: 'Jane A', role: 'Admin' },
-			time: '10:55am',
-			actionText: 'Undo',
-			timestamp: 1483264820300
-		}
-	];
+	// const logs = [
+	// 	{
+	// 		title: 'Site backed up',
+	// 		description: 'We backed up your site',
+	// 		user: null,
+	// 		type: 'site_backed_up',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Site has backed up Failed',
+	// 		description: 'We couldn\'t establish a connection to your site.',
+	// 		user: null,
+	// 		type: 'site_backed_up_failed',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Suspicious code detected in 2 plugin files.',
+	// 		description: 'Rewound in 17 January 2017 at 3:30 PM - We found potential warmful code in two of your Plugins: Yoast SEO and Advanced Custom Fields.',
+	// 		user: null,
+	// 		type: 'suspicious_code',
+	// 		timestamp: 1485220539222,
+	// 		className: 'is-disabled',
+	// 	},
+	// 	{
+	// 		title: 'Akismet activated',
+	// 		description: 'Akismet Plugin was successfully activated.',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'plugin_activated',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Akismet deactivated',
+	// 		description: 'Akismet Plugin was successfully deactivated.',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'plugin_deactivated',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Jetpack version 4.6 is available',
+	// 		description: 'A new version of the Jetpack Plugin was been released. Click here to update, or turn on auto-updates for Plugins and we\'ll manage those for you',
+	// 		user: null,
+	// 		type: 'plugin_needs_update',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Akismet updated to version 3.2',
+	// 		description: 'Akismet Plugin was successfully updated to its latest version: 3.2.',
+	// 		user: null,
+	// 		type: 'plugin_updated',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Twenty Eighteen Theme was activated',
+	// 		description: 'TwentyEighteen Plugin was successfully activated.',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'theme_switched',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Twenty Sixteen updated to version 1.0.1',
+	// 		description: 'Twenty Sixteen Plugin was successfully updated to its latest version: 1.0.1.',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'theme_updated',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Site updated to Professional Plan, Thank you',
+	// 		description: 'Professional Plan was successfully purchased for your site and is valid until February 15, 2018.',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'plan_updated',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Professional Plan Renewed for another month',
+	// 		description: 'Professional Plan was renewed for another month and is valid until February 28, 2017',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'plan_renewed',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Photon was activated',
+	// 		description: 'Photon module was activated in your site. All your images will now be served through the WordPress.com worldwide network.',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'activate_jetpack_feature',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Custom CSS was deactivated',
+	// 		description: 'Custom CSS module was deactivated.',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'deactivate_jetpack_feature',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'This is some really cool post',
+	// 		description: '',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'site_backed_up',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'This is some really cool post',
+	// 		description: '',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'site_backed_up',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'This is some really cool post',
+	// 		description: '',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		type: 'site_backed_up',
+	// 		timestamp: 1485220539222
+	// 	},
+	// 	{
+	// 		title: 'Jetpack updated to 4.5.1',
+	// 		subTitle: 'Plugin Update',
+	// 		description: '',
+	// 		icon: 'plugins',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		time: '4:32pm',
+	// 		actionText: 'Undo',
+	// 		timestamp: 1483351202400
+	// 	},
+	// 	{
+	// 		title: 'Jetpack updated to 4.5.1',
+	// 		subTitle: 'Plugin Activated',
+	// 		description: '',
+	// 		icon: 'plugins',
+	// 		user: { ID: 123, name: 'Jane A', role: 'Admin' },
+	// 		time: '4:32pm',
+	// 		actionText: 'Undo',
+	// 		timestamp: 1483351202420
+	// 	},
+	// 	{
+	// 		title: 'Post Title',
+	// 		subTitle: 'Post Updated',
+	// 		description: '',
+	// 		icon: 'posts',
+	// 		user: { ID: 333, name: 'Jane A', role: 'Admin' },
+	// 		time: '10:55am',
+	// 		actionText: 'Undo',
+	// 		timestamp: 1483264820300
+	// 	}
+	// ];
 
 	return ( dispatch ) => {
 		dispatch( {
@@ -294,21 +322,27 @@ export function getActivityLogData( siteId ) {
 			siteId
 		} );
 
-		return wpcom.undocumented().getActivityLog( siteId )
-			.then( ( data ) => {
-				data = logs; // Get some fake data
-				dispatch( {
-					type: ACTIVITY_LOG_FETCH_SUCCESS,
-					siteId,
-					data
-				} );
-			} )
-			.catch( ( error ) => {
-				dispatch( {
-					type: ACTIVITY_LOG_FETCH_FAILED,
-					siteId,
-					error: pick( error, [ 'error', 'status', 'message' ] )
-				} );
-			} );
+		dispatch( {
+			type: ACTIVITY_LOG_FETCH_SUCCESS,
+			siteId,
+			data: fakebackups()
+		} );
+
+		// return wpcom.undocumented().getActivityLog( siteId )
+		// 	.then( ( data ) => {
+		// 		data = fakeBackups; // Get some fake data
+		// 		dispatch( {
+		// 			type: ACTIVITY_LOG_FETCH_SUCCESS,
+		// 			siteId,
+		// 			data
+		// 		} );
+		// 	} )
+		// 	.catch( ( error ) => {
+		// 		dispatch( {
+		// 			type: ACTIVITY_LOG_FETCH_FAILED,
+		// 			siteId,
+		// 			error: pick( error, [ 'error', 'status', 'message' ] )
+		// 		} );
+		// 	} );
 	};
 }
