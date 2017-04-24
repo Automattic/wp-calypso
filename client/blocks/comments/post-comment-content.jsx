140a6850709122fb09e986d6aa12c8d76c6561e1
tellyworth <alex@automattic.com>
2017-04-24 16:03:11 +1000

    Apply newline-to-br formatting on comment placeholder content
    Apply simple nl2br-style to placeholder text in PostCommentContent.
    This helps the content resemble a fully formatted comment.

--- client/blocks/comments/post-comment-content.jsx
+++ client/blocks/comments/post-comment-content.jsx
@@ -8,7 +8,11 @@ export default class PostCommentContent extends React.Component {
 	render() {
 		// Don't trust comment content unless it was provided by the API
 		if ( this.props.isPlaceholder ) {
-			return <div className="comments__comment-content">{ this.props.content }</div>;
+			return <div className="comments__comment-content">{
+				this.props.content.split( '\n' ).map( ( item, key ) => {
+					return <span key={ key }>{ item }<br /></span>;
+				} )
+			}</div>;
 		}
 
 		/*eslint-disable react/no-danger*/
