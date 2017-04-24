63bfc147970411d764bf6b2087a0bee196830053
Benoit Person <ben547@gmail.com>
2017-04-24 18:37:08 +0200

    Editor: Post revisions into redux (#12733)
    * Post: Add `/revision` route + early infrastructure to load revisions: `QueryPostRevisions`
    * Post Revisions: Normalize revisions for state
    * Post Revisions: Add `requesting` to track the status of revisions requests in Redux
    * Post Revisions: Use `data-layer` instead of redux-thunk to fetch revisions
    * Post Revisions: Tests for reducers
    * Post Revisions: Tests for selectors

--- client/state/data-layer/wpcom/index.js
+++ client/state/data-layer/wpcom/index.js
@@ -5,6 +5,7 @@ import { mergeHandlers } from 'state/data-layer/utils';
 import accountRecovery from './account-recovery';
 import me from './me';
 import plans from './plans';
+import posts from './posts';
 import read from './read';
 import sites from './sites';
 import timezones from './timezones';
@@ -15,6 +16,7 @@ export const handlers = mergeHandlers(
 	accountRecovery,
 	me,
 	plans,
+	posts,
 	read,
 	sites,
 	timezones,
