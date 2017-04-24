a0e955e02d00caf4575e268e359fdc9725a613ce
oskosk <oskosk@gmail.com>
2017-04-24 12:30:44 -0300

    Make the siteId prop not be required in JetpackModuleToggle

--- client/my-sites/site-settings/jetpack-module-toggle.jsx
+++ client/my-sites/site-settings/jetpack-module-toggle.jsx
@@ -31,7 +31,7 @@ class JetpackModuleToggle extends Component {
 	};
 
 	static propTypes = {
-		siteId: PropTypes.number.isRequired,
+		siteId: PropTypes.number,
 		moduleSlug: PropTypes.string.isRequired,
 		label: PropTypes.string.isRequired,
 		checked: PropTypes.bool,
