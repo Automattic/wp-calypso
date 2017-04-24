d22e9e1ac04cba19f21c6739bd231ec4958c5905
Bernhard Reiter <ockham@raz.or.at>
2017-04-24 16:52:12 +0200

    SSR: Cache State

---
+++ server/state-cache/index.js
@@ -0,0 +1,12 @@
+/**
+ * External Dependencies
+ */
+import Lru from 'lru';
+
+const HOUR_IN_MS = 3600000;
+const stateCache = new Lru( {
+	max: 500,
+	maxAge: HOUR_IN_MS
+} );
+
+export default stateCache;
