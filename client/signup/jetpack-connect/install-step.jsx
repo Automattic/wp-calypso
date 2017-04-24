57b3034a11bbd2df7f51501ca44dd7e9b8fe472a
Jon Surrell <jon.surrell@automattic.com>
2017-04-24 14:14:49 +0200

    Use localize() for i18n of jetpack-connect components
    Use `localize( Component )` and `this.props.translate` over
    `this.translate`.
    
    In some cases, `i18n.translate` has been used to limit the scope
    of linter-required changes.
    
    This commit should not affect behavior, it is janitorial.
    
    Fixes linter errors `wpcalypso/i18n-no-this-translate.

--- client/signup/jetpack-connect/install-step.jsx
+++ client/signup/jetpack-connect/install-step.jsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React from 'react';
+import i18n from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -31,7 +32,7 @@ export default React.createClass( {
 	renderAlreadyHaveJetpackButton() {
 		return (
 			<a className="jetpack-connect__already-installed-jetpack-button" href="#" onClick={ this.confirmJetpackInstalled }>
-				{ preventWidows( this.translate( 'Already have Jetpack installed?' ) ) }
+				{ preventWidows( i18n.translate( 'Already have Jetpack installed?' ) ) }
 			</a>
 		);
 	},
@@ -39,7 +40,7 @@ export default React.createClass( {
 	renderNotJetpackButton() {
 		return (
 			<a className="jetpack-connect__no-jetpack-button" href="#" onClick={ this.confirmJetpackNotInstalled }>
-				{ preventWidows( this.translate( 'Don\'t have Jetpack installed?' ) ) }
+				{ preventWidows( i18n.translate( 'Don\'t have Jetpack installed?' ) ) }
 			</a>
 		);
 	},
@@ -55,37 +56,37 @@ export default React.createClass( {
 			onClick={ this.props.onClick } />;
 		const steps = {
 			installJetpack: {
-				title: this.translate( '1. Install Jetpack' ),
+				title: i18n.translate( '1. Install Jetpack' ),
 				text: this.props.isInstall
-					? this.translate( 'You will be redirected to your site\'s dashboard to install ' +
+					? i18n.translate( 'You will be redirected to your site\'s dashboard to install ' +
 					'Jetpack. Click the blue "Install Now" button.' )
-					: this.translate( 'You will be redirected to the Jetpack plugin page on your site\'s ' +
+					: i18n.translate( 'You will be redirected to the Jetpack plugin page on your site\'s ' +
 					'dashboard to install Jetpack. Click the blue install button.' ),
 				action: this.renderAlreadyHaveJetpackButton(),
 				example: <JetpackExampleInstall url={ this.props.currentUrl } onClick={ this.props.onClick } />
 			},
 			activateJetpackAfterInstall: {
-				title: this.translate( '2. Activate Jetpack' ),
-				text: this.translate( 'Then you\'ll click the blue "Activate" link to activate Jetpack.' ),
+				title: i18n.translate( '2. Activate Jetpack' ),
+				text: i18n.translate( 'Then you\'ll click the blue "Activate" link to activate Jetpack.' ),
 				action: null,
 				example: <JetpackExampleActivate url={ this.props.currentUrl } isInstall={ true } onClick={ this.props.onClick } />
 			},
 			connectJetpackAfterInstall: {
-				title: this.translate( '3. Connect Jetpack' ),
-				text: this.translate( 'Finally, click the "Connect to WordPress.com" button to finish the process.' ),
+				title: i18n.translate( '3. Connect Jetpack' ),
+				text: i18n.translate( 'Finally, click the "Connect to WordPress.com" button to finish the process.' ),
 				action: null,
 				example: jetpackConnectExample
 			},
 			activateJetpack: {
-				title: this.translate( '1. Activate Jetpack' ),
-				text: this.translate( 'You will be redirected to your site\'s dashboard to activate Jetpack. ' +
+				title: i18n.translate( '1. Activate Jetpack' ),
+				text: i18n.translate( 'You will be redirected to your site\'s dashboard to activate Jetpack. ' +
 					'Click the blue "Activate" link. ' ),
 				action: this.renderNotJetpackButton(),
 				example: <JetpackExampleActivate url={ this.props.currentUrl } isInstall={ false } onClick={ this.props.onClick } />
 			},
 			connectJetpack: {
-				title: this.translate( '2. Connect Jetpack' ),
-				text: this.translate( 'Then click the "Connect to WordPress.com" button to finish the process.' ),
+				title: i18n.translate( '2. Connect Jetpack' ),
+				text: i18n.translate( 'Then click the "Connect to WordPress.com" button to finish the process.' ),
 				action: null,
 				example: jetpackConnectExample
 			}
--- client/signup/jetpack-connect/jetpack-connect-notices.jsx
+++ client/signup/jetpack-connect/jetpack-connect-notices.jsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React, { PropTypes } from 'react';
+import i18n from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -21,7 +22,7 @@ export default React.createClass( {
 		const noticeValues = {
 			icon: 'notice',
 			status: 'is-error',
-			text: this.translate( 'That\'s not a valid url.' ),
+			text: i18n.translate( 'That\'s not a valid url.' ),
 			showDismiss: false
 		};
 
@@ -35,73 +36,73 @@ export default React.createClass( {
 		}
 		if ( this.props.noticeType === 'isDotCom' ) {
 			noticeValues.icon = 'block';
-			noticeValues.text = this.translate( 'That\'s a WordPress.com site, so you don\'t need to connect it.' );
+			noticeValues.text = i18n.translate( 'That\'s a WordPress.com site, so you don\'t need to connect it.' );
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'notWordPress' ) {
 			noticeValues.icon = 'block';
-			noticeValues.text = this.translate( 'That\'s not a WordPress site.' );
+			noticeValues.text = i18n.translate( 'That\'s not a WordPress site.' );
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'notActiveJetpack' ) {
 			noticeValues.icon = 'block';
-			noticeValues.text = this.translate( 'Jetpack is deactivated.' );
+			noticeValues.text = i18n.translate( 'Jetpack is deactivated.' );
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'outdatedJetpack' ) {
 			noticeValues.icon = 'block';
-			noticeValues.text = this.translate( 'You must update Jetpack before connecting.' );
+			noticeValues.text = i18n.translate( 'You must update Jetpack before connecting.' );
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'jetpackIsDisconnected' ) {
 			noticeValues.icon = 'link-break';
-			noticeValues.text = this.translate( 'Jetpack is currently disconnected.' );
+			noticeValues.text = i18n.translate( 'Jetpack is currently disconnected.' );
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'jetpackIsValid' ) {
 			noticeValues.status = 'is-success';
 			noticeValues.icon = 'plugins';
-			noticeValues.text = this.translate( 'Jetpack is connected.' );
+			noticeValues.text = i18n.translate( 'Jetpack is connected.' );
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'notJetpack' ) {
 			noticeValues.status = 'is-noticeType';
 			noticeValues.icon = 'status';
-			noticeValues.text = this.translate( 'Jetpack couldn\'t be found.' );
+			noticeValues.text = i18n.translate( 'Jetpack couldn\'t be found.' );
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'alreadyConnected' ) {
 			noticeValues.status = 'is-success';
 			noticeValues.icon = 'status';
-			noticeValues.text = this.translate( 'This site is already connected!' );
+			noticeValues.text = i18n.translate( 'This site is already connected!' );
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'wordpress.com' ) {
-			noticeValues.text = this.translate( 'Oops, that\'s us.' );
+			noticeValues.text = i18n.translate( 'Oops, that\'s us.' );
 			noticeValues.status = 'is-warning';
 			noticeValues.icon = 'status';
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'retryingAuth' ) {
-			noticeValues.text = this.translate( 'Error authorizing. Page is refreshing for an other attempt.' );
+			noticeValues.text = i18n.translate( 'Error authorizing. Page is refreshing for an other attempt.' );
 			noticeValues.status = 'is-warning';
 			noticeValues.icon = 'notice';
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'retryAuth' ) {
-			noticeValues.text = this.translate( 'In some cases, authorization can take a few attempts. Please try again.' );
+			noticeValues.text = i18n.translate( 'In some cases, authorization can take a few attempts. Please try again.' );
 			noticeValues.status = 'is-warning';
 			noticeValues.icon = 'notice';
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'secretExpired' ) {
-			noticeValues.text = this.translate( 'Oops, that took a while. You\'ll have to try again.' );
+			noticeValues.text = i18n.translate( 'Oops, that took a while. You\'ll have to try again.' );
 			noticeValues.status = 'is-error';
 			noticeValues.icon = 'notice';
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'defaultAuthorizeError' ) {
-			noticeValues.text = this.translate( 'Error authorizing your site. Please {{link}}contact support{{/link}}.', {
+			noticeValues.text = i18n.translate( 'Error authorizing your site. Please {{link}}contact support{{/link}}.', {
 				components: { link: <a href="https://jetpack.com/contact-support" target="_blank" rel="noopener noreferrer" /> }
 			} );
 			noticeValues.status = 'is-error';
@@ -109,7 +110,7 @@ export default React.createClass( {
 			return noticeValues;
 		}
 		if ( this.props.noticeType === 'alreadyConnectedByOtherUser' ) {
-			noticeValues.text = this.translate(
+			noticeValues.text = i18n.translate(
 				'This site is already connected to a different WordPress.com user, ' +
 				'you need to disconnect that user before you can connect another.'
 			);
