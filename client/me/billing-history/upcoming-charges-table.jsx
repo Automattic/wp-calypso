0a12c7c8c66258eb5fa419ca09ece00aefb511c1
Marin Atanasov <tyxla@abv.bg>
2017-04-24 11:39:05 +0300

    Framework: Remove sites-list from billing-history

--- client/me/billing-history/upcoming-charges-table.jsx
+++ client/me/billing-history/upcoming-charges-table.jsx
@@ -1,62 +1,64 @@
 /**
  * External dependencies
  */
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal Dependencies
  */
-var	purchasesPaths = require( 'me/purchases/paths' ),
-	TransactionsTable = require( './transactions-table' );
+import purchasesPaths from 'me/purchases/paths';
+import TransactionsTable from './transactions-table';
+import { getSite } from 'state/sites/selectors';
 
-module.exports = React.createClass( {
+class UpcomingChargesTable extends Component {
+	static propTypes = {
+		transactions: PropTypes.array,
+		// Computed props
+		getSite: PropTypes.func,
+	}
 
-	displayName: 'UpcomingChargesTable',
-
-	propTypes: {
-		sites: PropTypes.shape( {
-			getSite: PropTypes.func.isRequired
-		} ).isRequired
-	},
-
-	render: function() {
-		var transactions = null;
-		const emptyTableText = this.translate(
-			'The upgrades on your account will not renew automatically. ' +
-			'To manage your upgrades or enable Auto Renew visit {{link}}My Upgrades{{/link}}.', {
-				components: { link: <a href={ purchasesPaths.purchasesRoot() } /> }
-			}
-		);
-		const noFilterResultsText = this.translate( 'No upcoming charges found.' );
-
-		if ( this.props.sites.initialized ) {
-			// `TransactionsTable` will render a loading state until the transactions are present
-			transactions = this.props.transactions;
-		}
-
-		return (
-			<TransactionsTable
-				transactions={ transactions }
-				initialFilter={ { date: { newest: 20 } } }
-				emptyTableText={ emptyTableText }
-				noFilterResultsText={ noFilterResultsText }
-				transactionRenderer={ this.renderTransaction } />
-		);
-	},
-
-	renderTransaction: function( transaction ) {
-		var site = this.props.sites.getSite( Number( transaction.blog_id ) );
+	renderTransaction = ( transaction ) => {
+		const { translate } = this.props;
+		const site = this.props.getSite( Number( transaction.blog_id ) );
 
 		if ( ! site ) {
 			return null;
 		}
 
 		return (
-			<div className="transaction-links">
+			<div className="billing-history__transaction-links">
 				<a href={ purchasesPaths.managePurchase( site.slug, transaction.id ) }>
-					{ this.translate( 'Manage Purchase' ) }
+					{ translate( 'Manage Purchase' ) }
 				</a>
 			</div>
 		);
 	}
-} );
+
+	render() {
+		const { translate } = this.props;
+		const emptyTableText = translate(
+			'The upgrades on your account will not renew automatically. ' +
+			'To manage your upgrades or enable Auto Renew visit {{link}}My Upgrades{{/link}}.', {
+				components: { link: <a href={ purchasesPaths.purchasesRoot() } /> }
+			}
+		);
+		const noFilterResultsText = translate( 'No upcoming charges found.' );
+
+		return (
+			<TransactionsTable
+				transactions={ this.props.transactions }
+				initialFilter={ { date: { newest: 20 } } }
+				emptyTableText={ emptyTableText }
+				noFilterResultsText={ noFilterResultsText }
+				transactionRenderer={ this.renderTransaction } />
+		);
+	}
+}
+
+export default connect(
+	( state ) => ( {
+		getSite: ( siteId ) => getSite( state, siteId ),
+	} )
+)( localize( UpcomingChargesTable ) );
