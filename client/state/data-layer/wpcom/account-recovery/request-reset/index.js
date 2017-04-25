7cacfb78dbffb2f66a78422cca3bc400de9b07b4
James Tien <southp0105@gmail.com>
2017-04-25 11:11:52 +0800

    Update `/request-reset` data layer handler so that: 1. it won't block the action from passing down. 2. dispatch `setResetMethod` on success.

--- client/state/data-layer/wpcom/account-recovery/request-reset/index.js
+++ client/state/data-layer/wpcom/account-recovery/request-reset/index.js
@@ -6,16 +6,30 @@ import { ACCOUNT_RECOVERY_RESET_REQUEST } from 'state/action-types';
 import {
 	requestResetSuccess,
 	requestResetError,
+	setResetMethod,
 } from 'state/account-recovery/reset/actions';
 
-export const handleRequestReset = ( { dispatch }, { request } ) => (
+export const handleRequestReset = ( { dispatch }, action, next ) => {
+	const {
+		userData,
+		method,
+	} = action;
+
 	wpcom.req.post( {
-		body: request,
+		body: {
+			...userData,
+			method,
+		},
 		apiNamespace: 'wpcom/v2',
 		path: '/account-recovery/request-reset',
-	} ).then( () => dispatch( requestResetSuccess() ) )
-	.catch( ( error ) => dispatch( requestResetError( error ) ) )
-);
+	} ).then( () => {
+		dispatch( requestResetSuccess() );
+		dispatch( setResetMethod( method ) );
+	} )
+	.catch( ( error ) => dispatch( requestResetError( error ) ) );
+
+	return next( action );
+};
 
 export default {
 	[ ACCOUNT_RECOVERY_RESET_REQUEST ]: [ handleRequestReset ],
