a501c8bff8a568a4f8a2e5016866bc9a7d072046
Jonathan Sadowski <sadowski@automattic.com>
2017-04-25 08:37:55 -0500

    Reposition block

--- client/my-sites/pages/blog-posts-page/index.jsx
+++ client/my-sites/pages/blog-posts-page/index.jsx
@@ -6,20 +6,14 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { identity } from 'lodash';
 import { localize } from 'i18n-calypso';
-import classNames from 'classnames';
 import Gridicon from 'gridicons';
 
 /**
  * Internal dependencies
  */
-import CompactCard from 'components/card/compact';
-import PopoverMenu from 'components/popover/menu';
-import PopoverMenuItem from 'components/popover/menu-item';
-import { isEnabled } from 'config';
+import Card from 'components/card';
 import { getSiteFrontPageType, getSitePostsPage } from 'state/sites/selectors';
 import { setFrontPage } from 'state/sites/actions';
-import { userCan } from 'lib/site/utils';
-import { updateSitesList } from '../helpers';
 
 class BlogPostsPage extends React.Component {
 
@@ -35,21 +29,12 @@ class BlogPostsPage extends React.Component {
 		showPageActions: false,
 	}
 
-	togglePageActions = () => {
-		this.setState( { showPageActions: ! this.state.showPageActions } );
-	}
-
-	setAsHomepage = () => {
-		this.setState( { showPageActions: false } );
-		this.props.setFrontPage( this.props.site.ID, 0, updateSitesList );
-	}
-
-	renderSetAsHomepageItem() {
+	renderNotInUseInfo() {
 		return (
-			<PopoverMenuItem onClick={ this.setAsHomepage }>
-				<Gridicon icon="house" size={ 18 } />
-				{ this.props.translate( 'Set as Homepage' ) }
-			</PopoverMenuItem>
+			<span>
+				<Gridicon size="12" icon="not-visible" />
+				{ this.props.translate( 'Not in use.' ) }
+			</span>
 		);
 	}
 
@@ -58,26 +43,17 @@ class BlogPostsPage extends React.Component {
 
 		const isStaticHomePageWithNoPostsPage = this.props.frontPageType === 'page' && ! this.props.postsPage;
 		const isCurrentlySetAsHomepage = this.props.frontPageType === 'posts';
-		const shouldShow = this.props.isFrontPage ||
-			( isEnabled( 'manage/pages/set-homepage' ) && isStaticHomePageWithNoPostsPage );
-		const shouldShowPageActions = ! isCurrentlySetAsHomepage &&
-			userCan( 'edit_theme_options', this.props.site ) && isEnabled( 'manage/pages/set-homepage' );
 
-		if ( ! shouldShow ) {
-			return null;
-		}
+		const blogPostsPageHref = ! isStaticHomePageWithNoPostsPage && this.props.site.URL;
 
 		return (
-			<CompactCard className="blog-posts-page">
-				{ isStaticHomePageWithNoPostsPage &&
-					<div className="blog-posts-page__not-used-badge">{ translate( 'Not Used' ) }</div> }
-				{ isCurrentlySetAsHomepage &&
-					<Gridicon icon="house" size={ 18 } className="blog-posts-page__home-badge" /> }
+			<Card href={ blogPostsPageHref } target="_blank" rel="noopener noreferrer" className="blog-posts-page">
 				<div className="blog-posts-page__details">
 					<div className="blog-posts-page__title">
 						{ translate( 'Blog Posts' ) }
 					</div>
 					<div className="blog-posts-page__info">
+						{ isStaticHomePageWithNoPostsPage && this.renderNotInUseInfo() }
 						{
 							isCurrentlySetAsHomepage
 							? translate( 'Your latest posts, shown on homepage' )
@@ -85,30 +61,7 @@ class BlogPostsPage extends React.Component {
 						}
 					</div>
 				</div>
-				{
-					shouldShowPageActions
-					? <div className="blog-posts-page__actions">
-							<Gridicon
-								icon="ellipsis"
-								className={ classNames( {
-									'page__actions-toggle': true,
-									'is-active': this.state.showPageActions
-								} ) }
-								onClick={ this.togglePageActions }
-								ref="popoverMenuButton" />
-							<PopoverMenu
-								isVisible={ this.state.showPageActions }
-								onClose={ this.togglePageActions }
-								position={ 'bottom left' }
-								context={ this.refs && this.refs.popoverMenuButton }
-							>
-								{ this.renderSetAsHomepageItem() }
-							</PopoverMenu>
-						</div>
-					: null
-				}
-
-			</CompactCard>
+			</Card>
 		);
 	}
 }
