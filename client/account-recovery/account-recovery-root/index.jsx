0779bae21eb219043f235de06b6edd06cfe6795f
James Tien <southp0105@gmail.com>
2017-04-25 11:13:28 +0800

    Move `selectedMethod` to react state, and update the whole `ResetPasswordForm` according to this and the action changes.

--- client/account-recovery/account-recovery-root/index.jsx
+++ client/account-recovery/account-recovery-root/index.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import classnames from 'classnames';
+import { includes } from 'lodash';
 
 /**
  * Internal dependencies
@@ -16,11 +17,12 @@ import LostPasswordForm from 'account-recovery/lost-password-form';
 import ForgotUsernameForm from 'account-recovery/forgot-username-form';
 import ResetPasswordForm from 'account-recovery/reset-password-form';
 import ResetPasswordEmailForm from 'account-recovery/reset-password-email-form';
-import ResetPasswordSmsForm from 'account-reocvery/reset-password-sms-form';
+import ResetPasswordSmsForm from 'account-recovery/reset-password-sms-form';
 import { ACCOUNT_RECOVERY_STEPS as STEPS } from 'account-recovery/constants';
 import {
 	isAccountRecoveryResetOptionsReady,
 	isAccountRecoveryUserDataReady,
+	getAccountRecoveryResetSelectedMethod,
 } from 'state/selectors';
 
 const getPageInfo = ( translate, step ) => {
@@ -42,6 +44,7 @@ const getPageInfo = ( translate, step ) => {
 			documentHeadTitle: translate( 'Reset Password < Email' ),
 		},
 		[ STEPS.RESET_PASSWORD_SMS ]: {
+			trackerTitle: 'Account Recovery > Reset Password Sms',
 			documentHeadTitle: translate( 'Reset Password ‹ SMS Message' ),
 		},
 	};
@@ -49,7 +52,22 @@ const getPageInfo = ( translate, step ) => {
 	return pageInfo[ step ];
 };
 
-const getCurrentStep = ( { firstStep, isUserDataReady, isResetOptionsReady } ) => {
+const getCurrentStep = ( props ) => {
+	const {
+		firstStep,
+		isUserDataReady,
+		isResetOptionsReady,
+		selectedMethod,
+	} = props;
+
+	if ( selectedMethod ) {
+		if ( includes( selectedMethod, '_email' ) ) {
+			return STEPS.RESET_PASSWORD_EMAIL;
+		}
+
+		return STEPS.RESET_PASSWORD_SMS;
+	}
+
 	if ( isUserDataReady && isResetOptionsReady ) {
 		return STEPS.RESET_PASSWORD;
 	}
@@ -97,5 +115,6 @@ export default connect(
 	( state ) => ( {
 		isResetOptionsReady: isAccountRecoveryResetOptionsReady( state ),
 		isUserDataReady: isAccountRecoveryUserDataReady( state ),
+		selectedMethod: getAccountRecoveryResetSelectedMethod( state ),
 	} )
 )( localize( AccountRecoveryRoot ) );
