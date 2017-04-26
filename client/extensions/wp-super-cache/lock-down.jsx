b7e59222482e299fa57a8522fa42810529ae8ee5
Drew Butler <hi@dbtlr.com>
2017-04-26 11:00:04 +0800

    Fix some p tag embedding issues

--- client/extensions/wp-super-cache/lock-down.jsx
+++ client/extensions/wp-super-cache/lock-down.jsx
@@ -44,36 +44,41 @@ const LockDown = ( {
 							</span>
 						</FormToggle>
 					</FormFieldset>
-					<FormSettingExplanation className="wp-super-cache__lock-down-explanation">
-						<p>
-							{ translate(
-								'When this is enabled, new comments on a post will not refresh the cached static files.'
-							) }
-						</p>
-						<p>
+
+					<div className="wp-super-cache__lock-down-container">
+						<FormSettingExplanation className="wp-super-cache__lock-down-explanation">
+								{ translate(
+									'When this is enabled, new comments on a post will not refresh the cached static files.'
+								) }
+						</FormSettingExplanation>
+						<FormSettingExplanation className="wp-super-cache__lock-down-explanation">
 							{ translate(
 								'Developers: Make your plugin lock down compatible by checking the "WPLOCKDOWN" ' +
 								'constant. The following code will make sure your plugin respects the WPLOCKDOWN setting.'
 							) }
-						</p>
+						</FormSettingExplanation>
 
-						<div className="wp-super-cache__lock-down-code-block">
+						<FormSettingExplanation className="wp-super-cache__lock-down-code-block">
 							<ClipboardButton
 								className="wp-super-cache__lock-down-code-block-button"
 								text={ lockdownCodeSnippet }>
 								<Gridicon icon="clipboard" />
 							</ClipboardButton>
-							<p className="wp-super-cache__lock-down-code-block-snippet">{ lockdownCodeSnippet }</p>
-						</div>
+							<span className="wp-super-cache__lock-down-code-block-snippet">{ lockdownCodeSnippet }</span>
+						</FormSettingExplanation>
 
-						<Notice isCompact={ true } status={ wp_lock_down ? 'is-warning' : 'is-info' } text={ !! wp_lock_down
+						<Notice
+							isCompact={ true }
+							className="wp-super-cache__lock-down-notice"
+							status={ wp_lock_down ? 'is-warning' : 'is-info' }
+							text={ !! wp_lock_down
 							? translate( 'WordPress is locked down. Super Cache static files will not be deleted ' +
 								'when new comments are made.' )
 							: translate( 'WordPress is not locked down. New comments will refresh Super Cache ' +
 								'static files as normal.' )
 						}
 						/>
-					</FormSettingExplanation>
+					</div>
 				</form>
 			</Card>
 		</div>
