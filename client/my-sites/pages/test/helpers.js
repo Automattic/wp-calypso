9285d758a7397190cc07f1cd992cd1a919d2bd2d
Jonathan Sadowski <sadowski@automattic.com>
2017-04-26 10:56:12 -0500

    Make test-data more lint-happy

--- client/my-sites/pages/test/helpers.js
+++ client/my-sites/pages/test/helpers.js
@@ -13,19 +13,19 @@ describe( 'helpers', () => {
 		it( 'should place children under parents', () => {
 			const testData = [
 				{
-					"ID": 1,
-					"parent": {
-						"ID": 2
+					ID: 1,
+					parent: {
+						ID: 2
 					}
 				},
 				{
-					"ID": 2,
-					"parent": false
+					ID: 2,
+					parent: false
 				},
 				{
-					"ID": 3,
-					"parent": {
-						"ID": 1
+					ID: 3,
+					parent: {
+						ID: 1
 					}
 				}
 			];
@@ -34,21 +34,21 @@ describe( 'helpers', () => {
 
 			expect( sortedPages ).to.deep.equal( [
 				{
-					"ID": 2,
-					"parent": false
+					ID: 2,
+					parent: false
 				},
 				{
-					"ID": 1,
-					"indentLevel": 1,
-					"parent": {
-						"ID": 2
+					ID: 1,
+					indentLevel: 1,
+					parent: {
+						ID: 2
 					}
 				},
 				{
-					"ID": 3,
-					"indentLevel": 2,
-					"parent": {
-						"ID": 1
+					ID: 3,
+					indentLevel: 2,
+					parent: {
+						ID: 1
 					}
 				}
 			] );
@@ -57,28 +57,28 @@ describe( 'helpers', () => {
 		it( 'should sort first by hierarchy, then by menu_order', () => {
 			const testData = [
 				{
-					"ID": 1,
-					"menu_order": 0,
-					"parent": false,
+					ID: 1,
+					menu_order: 0,
+					parent: false,
 				},
 				{
-					"ID": 2,
-					"menu_order": 5,
-					"parent": {
-						"ID": 1
+					ID: 2,
+					menu_order: 5,
+					parent: {
+						ID: 1
 					}
 				},
 				{
-					"ID": 3,
-					"menu_order":2,
-					"parent": {
-						"ID": 1
+					ID: 3,
+					menu_order: 2,
+					parent: {
+						ID: 1
 					}
 				},
 				{
-					"ID": 4,
-					"menu_order": 6,
-					"parent": false
+					ID: 4,
+					menu_order: 6,
+					parent: false
 				}
 			];
 
@@ -86,30 +86,30 @@ describe( 'helpers', () => {
 
 			expect( sortedPages ).to.deep.equal( [
 				{
-					"ID": 1,
-					"menu_order": 0,
-					"parent": false
+					ID: 1,
+					menu_order: 0,
+					parent: false
 				},
 				{
-					"ID": 3,
-					"indentLevel": 1,
-					"menu_order": 2,
-					"parent": {
-						"ID": 1
+					ID: 3,
+					indentLevel: 1,
+					menu_order: 2,
+					parent: {
+						ID: 1
 					}
 				},
 				{
-					"ID": 2,
-					"indentLevel": 1,
-					"menu_order": 5,
-					"parent": {
-						"ID": 1
+					ID: 2,
+					indentLevel: 1,
+					menu_order: 5,
+					parent: {
+						ID: 1
 					}
 				},
 				{
-					"ID": 4,
-					"menu_order": 6,
-					"parent": false
+					ID: 4,
+					menu_order: 6,
+					parent: false
 				}
 			] );
 		} );
@@ -117,29 +117,29 @@ describe( 'helpers', () => {
 		it( 'should place orphaned children at top-level, with their children properly beneath them', ()=> {
 			const testData = [
 				{
-					"ID": 1,
-					"menu_order": 1,
-					"parent": false
+					ID: 1,
+					menu_order: 1,
+					parent: false
 				},
 				{
-					"ID": 2,
-					"menu_order": 2,
-					"parent": {
-						"ID": 3
+					ID: 2,
+					menu_order: 2,
+					parent: {
+						ID: 3
 					}
 				},
 				{
-					"ID": 3,
-					"menu_order": 3,
-					"parent": {
-						"ID": 5
+					ID: 3,
+					menu_order: 3,
+					parent: {
+						ID: 5
 					}
 				},
 				{
-					"ID": 4,
-					"menu_order": 4,
-					"parent": {
-						"ID": 1
+					ID: 4,
+					menu_order: 4,
+					parent: {
+						ID: 1
 					}
 				}
 			];
@@ -148,31 +148,31 @@ describe( 'helpers', () => {
 
 			expect( sortedPages ).to.deep.equal( [
 				{
-					"ID": 1,
-					"menu_order": 1,
-					"parent": false
+					ID: 1,
+					menu_order: 1,
+					parent: false
 				},
 				{
-					"ID": 4,
-					"indentLevel": 1,
-					"menu_order": 4,
-					"parent": {
-						"ID": 1
+					ID: 4,
+					indentLevel: 1,
+					menu_order: 4,
+					parent: {
+						ID: 1
 					}
 				},
 				{
-					"ID": 3,
-					"menu_order": 3,
-					"parent": {
-						"ID": 5
+					ID: 3,
+					menu_order: 3,
+					parent: {
+						ID: 5
 					}
 				},
 				{
-					"ID": 2,
-					"indentLevel": 1,
-					"menu_order": 2,
-					"parent": {
-						"ID": 3
+					ID: 2,
+					indentLevel: 1,
+					menu_order: 2,
+					parent: {
+						ID: 3
 					}
 				}
 			] );
