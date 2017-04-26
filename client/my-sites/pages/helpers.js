1a6501ff4e9a7e09d6cb11c9997366aa1b5a79d1
Jonathan Sadowski <sadowski@automattic.com>
2017-04-26 09:26:20 -0500

    Move sortPagesHierarchically to helpers file

--- client/my-sites/pages/helpers.js
+++ client/my-sites/pages/helpers.js
@@ -1,3 +1,21 @@
+/**
+ * External dependencies
+ */
+
+import {
+	assign,
+	forEach,
+	groupBy,
+	includes,
+	map,
+	reduce,
+	sortBy,
+} from 'lodash';
+
+// Helpers used by sortPagesHierarchically but not exposed externally
+const sortByMenuOrder = list => sortBy( list, 'menu_order' );
+const getParentId = page => page.parent && page.parent.ID;
+
 module.exports = {
 	editLinkForPage: function( page, site ) {
 		if ( ! ( page && page.ID ) || ! ( site && site.ID ) ) {
@@ -13,5 +31,38 @@ module.exports = {
 			return false;
 		}
 		return site.options.page_on_front === page.ID;
+	},
+
+	sortPagesHierarchically: function( pages ) {
+		const pageIds = map( pages, 'ID' );
+
+		const pagesByParent = reduce( groupBy( pages, getParentId ), ( result, list, parentId ) => {
+			if ( ! parentId || parentId === 'false' || ! includes( pageIds, parseInt( parentId, 10 ) ) ) {
+				// If we don't have the parent in our list, promote the page to "top level"
+				result.false = sortByMenuOrder( ( result.false || [] ).concat( list ) );
+				return result;
+			}
+
+			result[ parentId ] = sortByMenuOrder( list );
+			return result;
+		}, {} );
+
+		const sortedPages = [];
+
+		const insertChildren = ( pageId, indentLevel ) => {
+			const children = pagesByParent[ pageId ] || [];
+
+			forEach( children, child => {
+				sortedPages.push( assign( {}, child, { indentLevel } ) );
+				insertChildren( child.ID, indentLevel + 1 );
+			} );
+		};
+
+		forEach( pagesByParent.false, topLevelPage => {
+			sortedPages.push( topLevelPage );
+			insertChildren( topLevelPage.ID, 1 );
+		} );
+
+		return sortedPages;
 	}
 };
--- client/my-sites/pages/page-list.jsx
+++ client/my-sites/pages/page-list.jsx
@@ -4,15 +4,6 @@
 var React = require( 'react' ),
 	PureRenderMixin = require( 'react-pure-render/mixin' ),
 	omit = require( 'lodash/omit' );
-import {
-	assign,
-	forEach,
-	groupBy,
-	includes,
-	map,
-	reduce,
-	sortBy,
-} from 'lodash';
 
 /**
  * Internal dependencies
@@ -54,42 +45,6 @@ var PageList = React.createClass( {
 		);
 	}
 } );
-
-const sortByMenuOrder = list => sortBy( list, 'menu_order' );
-const getParentId = page => page.parent && page.parent.ID;
-
-const sortPagesHierarchically = pages => {
-	const pageIds = map( pages, 'ID' );
-
-	const pagesByParent = reduce( groupBy( pages, getParentId ), ( result, list, parentId ) => {
-		if ( ! parentId || parentId === 'false' || ! includes( pageIds, parseInt( parentId, 10 ) ) ) {
-			// If we don't have the parent in our list, promote the page to "top level"
-			result.false = sortByMenuOrder( ( result.false || [] ).concat( list ) );
-			return result;
-		}
-
-		result[ parentId ] = sortByMenuOrder( list );
-		return result;
-	}, {} );
-
-	const sortedPages = [];
-
-	const insertChildren = ( pageId, indentLevel ) => {
-		const children = pagesByParent[ pageId ] || [];
-
-		forEach( children, child => {
-			sortedPages.push( assign( {}, child, { indentLevel } ) );
-			insertChildren( child.ID, indentLevel + 1 );
-		} );
-	};
-
-	forEach( pagesByParent.false, topLevelPage => {
-		sortedPages.push( topLevelPage );
-		insertChildren( topLevelPage.ID, 1 );
-	} );
-
-	return sortedPages;
-};
 
 var Pages = React.createClass( {
 
