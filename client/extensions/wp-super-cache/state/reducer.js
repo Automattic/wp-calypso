fc0c24e131f36b5bdfc6b7e9c66a56a286bcb3c1
Donna Peplinskie <donnapep@gmail.com>
2017-04-26 11:19:41 -0400

    Add Redux actions, reducers and selectors for handling notices state

--- client/extensions/wp-super-cache/state/reducer.js
+++ client/extensions/wp-super-cache/state/reducer.js
@@ -6,8 +6,10 @@ import { combineReducers } from 'redux';
 /**
  * Internal dependencies
  */
+import notices from './notices/reducer';
 import settings from './settings/reducer';
 
 export default combineReducers( {
+	notices,
 	settings,
 } );
