899323206c7a0b11c3e677329fcf7afe22651aff
Jonathan Sadowski <sadowski@automattic.com>
2017-04-25 20:20:50 -0500

    Updated styles; grab front page id

--- client/my-sites/pages/blog-posts-page/index.jsx
+++ client/my-sites/pages/blog-posts-page/index.jsx
@@ -12,7 +12,11 @@ import Gridicon from 'gridicons';
  * Internal dependencies
  */
 import Card from 'components/card';
-import { getSiteFrontPageType, getSitePostsPage } from 'state/sites/selectors';
+import {
+	getSiteFrontPageType,
+	getSitePostsPage,
+	getSiteFrontPage,
+} from 'state/sites/selectors';
 import { setFrontPage } from 'state/sites/actions';
 
 class BlogPostsPage extends React.Component {
@@ -29,36 +33,64 @@ class BlogPostsPage extends React.Component {
 		showPageActions: false,
 	}
 
-	renderNotInUseInfo() {
+	getPostsPageLink( { isStaticHomePageWithNoPostsPage } ) {
+		return ! isStaticHomePageWithNoPostsPage && this.props.site.URL;
+	}
+
+	renderPostsPageInfo( { isStaticHomePageWithNoPostsPage, isCurrentlySetAsHomepage } ) {
+		const { translate } = this.props;
+
+		if ( isStaticHomePageWithNoPostsPage ) {
+			return (
+				<span>
+					<Gridicon size="12" icon="not-visible" />
+					{ this.props.translate( 'Not in use.' ) }
+					{
+						this.props.translate( '"%(pageTitle)s" is the front page.', {
+							args: {
+								pageTitle: this.props.frontPage,
+							}
+						} )
+					}
+				</span>
+			);
+		}
+
+		if ( isCurrentlySetAsHomepage ) {
+			return (
+				<span>
+					{ translate( 'Front page is showing your latest posts.' ) }
+				</span>
+			);
+		}
+
 		return (
 			<span>
-				<Gridicon size="12" icon="not-visible" />
-				{ this.props.translate( 'Not in use.' ) }
+				{
+					translate( '"%(pageTitle)s" page is showing your latest posts.', {
+						args: {
+							pageTitle: this.props.postsPage,
+						}
+					} )
+				}
 			</span>
 		);
 	}
 
 	render() {
 		const { translate } = this.props;
-
 		const isStaticHomePageWithNoPostsPage = this.props.frontPageType === 'page' && ! this.props.postsPage;
 		const isCurrentlySetAsHomepage = this.props.frontPageType === 'posts';
 
-		const blogPostsPageHref = ! isStaticHomePageWithNoPostsPage && this.props.site.URL;
-
 		return (
-			<Card href={ blogPostsPageHref } target="_blank" rel="noopener noreferrer" className="blog-posts-page">
+			<Card href={ this.getPostsPageLink( { isStaticHomePageWithNoPostsPage } ) }
+				target="_blank" rel="noopener noreferrer" className="blog-posts-page">
 				<div className="blog-posts-page__details">
 					<div className="blog-posts-page__title">
 						{ translate( 'Blog Posts' ) }
 					</div>
 					<div className="blog-posts-page__info">
-						{ isStaticHomePageWithNoPostsPage && this.renderNotInUseInfo() }
-						{
-							isCurrentlySetAsHomepage
-							? translate( 'Your latest posts, shown on homepage' )
-							: translate( 'Your latest posts' )
-						}
+						{ this.renderPostsPageInfo( { isStaticHomePageWithNoPostsPage, isCurrentlySetAsHomepage } ) }
 					</div>
 				</div>
 			</Card>
@@ -71,7 +103,8 @@ export default connect(
 		return {
 			frontPageType: getSiteFrontPageType( state, props.site.ID ),
 			isFrontPage: getSiteFrontPageType( state, props.site.ID ) === 'posts',
-			postsPage: getSitePostsPage( state, props.site.ID )
+			postsPage: getSitePostsPage( state, props.site.ID ),
+			frontPage: getSiteFrontPage( state, props.site.ID ),
 		};
 	},
 	( dispatch ) => bindActionCreators( {
