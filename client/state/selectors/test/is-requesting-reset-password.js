76e0654d378e5f758e906c6dbe428f59260197dd
James Tien <southp0105@gmail.com>
2017-04-26 11:30:39 +0800

    Add test suites for the newly-added selectors.

---
+++ client/state/selectors/test/is-requesting-reset-password.js
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ */
+import { assert } from 'chai';
+import deepFreeze from 'deep-freeze';
+
+/**
+ * Internal dependencies
+ */
+import { isRequestingResetPassword } from '../';
+
+describe( 'isRequestingResetPassword()', () => {
+	it( 'should return isRequesting field under resetPassword state tree.', () => {
+		const state = deepFreeze( {
+			accountRecovery: {
+				reset: {
+					resetPassword: {
+						isRequesting: true,
+					},
+				},
+			},
+		} );
+
+		assert.isTrue( isRequestingResetPassword( state ) );
+	} );
+
+	it( 'should return false as default value.', () => {
+		assert.isFalse( isRequestingResetPassword( undefined ) );
+	} );
+} );
