1e589d6ca6cd7262d99bf6510e9863887c995407
James Tien <southp0105@gmail.com>
2017-04-27 11:34:29 +0800

    Clean up redundant code & styles

--- client/account-recovery/reset-password-succeeded/index.js
+++ client/account-recovery/reset-password-succeeded/index.js
@@ -1,10 +1,9 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React from 'react';
 import { localize } from 'i18n-calypso';
 import config from 'config';
-import page from 'page';
 
 /**
  * Internal dependencies
@@ -12,26 +11,20 @@ import page from 'page';
 import WordPressLogo from 'components/wordpress-logo';
 import Button from 'components/button';
 
-class ResetPasswordSucceeded extends Component {
-	redirectToLoginPage = () => {
-		page.redirect( config( 'login_url' ) );
-	}
+const ResetPasswordSucceeded = ( props ) => {
+	const { translate } = props;
 
-	render() {
-		const { translate } = this.props;
-
-		return (
-			<div className="reset-password-succeeded">
-				<WordPressLogo size={ 120 } />
-				<p className="reset-password-succeeded__description">
-					{ translate( 'Congratulations! Your password has been reset.' ) }
-				</p>
-				<Button className="reset-password-succeeded__login-button" onClick={ this.redirectToLoginPage } primary>
-					{ translate( 'Log in' ) }
-				</Button>
-			</div>
-		);
-	}
-}
+	return (
+		<div className="reset-password-succeeded">
+			<WordPressLogo size={ 120 } />
+			<p className="reset-password-succeeded__description">
+				{ translate( 'Congratulations! Your password has been reset.' ) }
+			</p>
+			<Button className="reset-password-succeeded__login-button" href={ config( 'login_url' ) } primary>
+				{ translate( 'Log in' ) }
+			</Button>
+		</div>
+	);
+};
 
 export default localize( ResetPasswordSucceeded );
