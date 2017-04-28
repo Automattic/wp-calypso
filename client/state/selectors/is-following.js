b06aad42f16396199dc15b98c774a390d9b3c04c
Ben Lowery <ben.lowery@automattic.com>
2017-04-28 17:31:19 -0400

    remove comment, use feed_URL in is-following selector

--- client/state/selectors/is-following.js
+++ client/state/selectors/is-following.js
@@ -4,14 +4,13 @@
 import { find } from 'lodash';
 
 export default function isFollowing( state, { feedUrl, feedId, blogId } ) {
-	// should we use prepareComparableUrl here?
 	const predicate = {};
 	if ( feedId ) {
 		predicate.feed_ID = feedId;
 	} else if ( blogId ) {
 		predicate.blog_ID = blogId;
 	} else if ( feedUrl ) {
-		predicate.URL = feedUrl;
+		predicate.feed_URL = feedUrl;
 	}
 	const follow = find( state.reader.follows.items, predicate );
 	return !! follow && follow.is_following;
