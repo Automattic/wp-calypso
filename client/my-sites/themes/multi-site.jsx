a5868a21a401aed00e8072dbeadc69c80a7af2f7
Bernhard Reiter <ockham@raz.or.at>
2017-04-28 21:42:36 +0200

    Themes: Retain filters and query string when activating

--- client/my-sites/themes/multi-site.jsx
+++ client/my-sites/themes/multi-site.jsx
@@ -15,7 +15,7 @@ const MultiSiteThemeShowcase = connectOptions(
 	( props ) => (
 		<div>
 			<SidebarNavigation />
-			<ThemesSiteSelectorModal { ...props } sourcePath="/themes">
+			<ThemesSiteSelectorModal { ...props }>
 				<ThemeShowcase
 					source="showcase"
 					showUploadButton={ false } />
