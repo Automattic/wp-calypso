6ada1efc4056a654910214f650c423fb8d2e25ce
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-04-30 13:38:54 -0700

    Adds documentation for get post comments selector

--- client/state/selectors/get-post-comments.js
+++ client/state/selectors/get-post-comments.js
@@ -11,6 +11,12 @@ import {
  */
 import createSelector from 'lib/create-selector';
 
+/**
+ * @param {Object} state Global app state
+ * @param {Number} siteId Site ID.
+ * @param {Number} postId Post ID.
+ * @return {Object} An array of comments for the specified post.
+ */
 export default createSelector(
 	( state, siteId, postId ) => filter(
 		get( state, [ 'discussions', 'items', siteId ], null ),
