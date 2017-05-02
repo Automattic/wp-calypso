820a7d92a4a27904103a3e507fa51a3a7adcb778
Steve Seear <steve.seear@gmail.com>
2017-05-02 17:41:27 +0100

    Correct POST body format

--- client/state/logstash/actions.js
+++ client/state/logstash/actions.js
@@ -3,9 +3,16 @@
  */
 import { LOGSTASH } from 'state/action-types';
 
+/**
+ * Log to logstash. This method is inefficient because
+ * the data goes over the REST API, so use sparingly.
+ *
+ * @param {Object} params same as wpcom log2logstash params arg
+ * @returns {Object} Action object
+ */
 export function logToLogstash( params ) {
 	return {
 		type: LOGSTASH,
-		params,
+		params: { params: JSON.stringify( params ) },
 	};
 }
