2e5244a3af06226fd596129b76009a97fedfeafd
Kevin Killingsworth <kevin@redfenix.com>
2017-05-03 18:19:13 -0500

    WooCommerce: Product categories, add verification.
    This adds verification of the data received via the API.
    It also adjusts the tests to use the root wc-api reducer so it can
    deal with errors.

--- client/extensions/woocommerce/state/wc-api/product-categories/test/reducer.js
+++ client/extensions/woocommerce/state/wc-api/product-categories/test/reducer.js
@@ -6,39 +6,87 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import handlers from '../reducer';
+import reducer from '../../reducer';
 import {
 	WOOCOMMERCE_API_FETCH_PRODUCT_CATEGORIES,
-	WOOCOMMERCE_API_FETCH_PRODUCT_CATEGORIES_SUCCESS,
 } from '../../../action-types';
 import { fetchProductCategoriesSuccess } from '../actions';
 
-describe( 'productCategoriesGet', () => {
-	it( 'should not change any state', () => {
-		const siteData = {};
-		const handler = handlers[ WOOCOMMERCE_API_FETCH_PRODUCT_CATEGORIES ];
+describe( 'fetchProductCategories', () => {
+	it( 'should only create a site node', () => {
+		const siteId = 123;
+		const state = {};
 
-		expect( handler ).to.exist;
-
-		const newSiteData = handler( siteData, { type: WOOCOMMERCE_API_FETCH_PRODUCT_CATEGORIES, siteId: 123 } );
-		expect( newSiteData ).to.equal( siteData );
+		const newSiteData = reducer( state, { type: WOOCOMMERCE_API_FETCH_PRODUCT_CATEGORIES, payload: { siteId } } );
+		expect( newSiteData[ siteId ] ).to.eql( {} );
 	} );
 } );
 
-describe( 'productCategoriesGetSuccess', () => {
+describe( 'fetchProductCategoriesSuccess', () => {
 	it( 'should store data from the action', () => {
-		const siteData = {};
-		const handler = handlers[ WOOCOMMERCE_API_FETCH_PRODUCT_CATEGORIES_SUCCESS ];
-
-		expect( handler ).to.exist;
+		const siteId = 123;
+		const state = {};
 
 		const categories = [
-			{ name: 'cat1' },
-			{ name: 'cat2' },
+			{ id: 1, name: 'cat1', slug: 'cat-1' },
+			{ id: 2, name: 'cat2', slug: 'cat-2' },
 		];
-		const newSiteData = handler( siteData, fetchProductCategoriesSuccess( 123, categories ) );
-		expect( newSiteData ).to.exist;
-		expect( newSiteData.productCategories ).to.equal( categories );
+		const newState = reducer( state, fetchProductCategoriesSuccess( siteId, categories ) );
+		expect( newState[ siteId ] ).to.exist;
+		expect( newState[ siteId ].productCategories ).to.equal( categories );
+	} );
+
+	it( 'should not allow invalid category objects', () => {
+		const siteId = 123;
+		const state = {};
+
+		const missingId = [
+			{ id: 12, name: 'ok', slug: 'ok' },
+			{ name: 'badId', slug: 'bad-id' },
+		];
+
+		const missingName = [
+			{ id: 12, name: 'ok', slug: 'ok' },
+			{ id: 13, slug: 'missing-name' },
+		];
+
+		const missingSlug = [
+			{ id: 12, name: 'ok', slug: 'ok' },
+			{ id: 13, name: 'Missing Slug' },
+		];
+
+		const badId = [
+			{ id: 12, name: 'ok', slug: 'ok' },
+			{ id: 'bad', name: 'Bad Id', slug: 'bad-id' },
+		];
+
+		const badName = [
+			{ id: 12, name: 'ok', slug: 'ok' },
+			{ id: 13, name: 12.3, slug: 'bad-name' },
+		];
+
+		const badSlug = [
+			{ id: 12, name: 'ok', slug: 'ok' },
+			{ id: 13, name: 'Bad Slug', slug: 15 },
+		];
+
+		let newState = reducer( state, fetchProductCategoriesSuccess( siteId, missingId ) );
+		expect( newState[ siteId ].error.data.message ).to.equal( 'Invalid Categories Array' );
+
+		newState = reducer( state, fetchProductCategoriesSuccess( siteId, missingName ) );
+		expect( newState[ siteId ].error.data.message ).to.equal( 'Invalid Categories Array' );
+
+		newState = reducer( state, fetchProductCategoriesSuccess( siteId, missingSlug ) );
+		expect( newState[ siteId ].error.data.message ).to.equal( 'Invalid Categories Array' );
+
+		newState = reducer( state, fetchProductCategoriesSuccess( siteId, badId ) );
+		expect( newState[ siteId ].error.data.message ).to.equal( 'Invalid Categories Array' );
+
+		newState = reducer( state, fetchProductCategoriesSuccess( siteId, badName ) );
+		expect( newState[ siteId ].error.data.message ).to.equal( 'Invalid Categories Array' );
+
+		newState = reducer( state, fetchProductCategoriesSuccess( siteId, badSlug ) );
+		expect( newState[ siteId ].error.data.message ).to.equal( 'Invalid Categories Array' );
 	} );
 } );
 
