3fbd0a5b43b2e75c59aafdcb08095c5d6e31860b
Kelly Dwan <kelly.dwan@gmail.com>
2017-05-03 11:39:40 -0400

    Plans: Replace the current plan checkmark with a "Your Plan" banner (#13430)
    * Swap out gridicon checkmark for a “Your Plan” banner ribbon
    
    * Change button label to clearly actionable text
    
    * Remove CSS for unused checkmark
    
    * Remove gridicon checkmark

--- client/my-sites/plan-features/actions.jsx
+++ client/my-sites/plan-features/actions.jsx
@@ -5,7 +5,6 @@ import { localize } from 'i18n-calypso';
 import noop from 'lodash/noop';
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
-import Gridicon from 'gridicons';
 
 /**
  * Internal dependencies
@@ -42,8 +41,7 @@ const PlanFeaturesActions = ( {
 	if ( current && ! isInSignup ) {
 		upgradeButton = (
 			<Button className={ classes } href={ manageHref } disabled={ ! manageHref }>
-				<Gridicon size={ 18 } icon="checkmark" />
-				{ canPurchase ? translate( 'Your plan' ) : translate( 'Current plan' ) }
+				{ canPurchase ? translate( 'Manage Plan' ) : translate( 'View Plan' ) }
 			</Button>
 		);
 	} else if ( available || isPlaceholder ) {
