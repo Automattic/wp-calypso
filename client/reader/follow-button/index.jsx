84e79c830c042ca4846dc10ecf63044888c01198
Ben Lowery <ben.lowery@automattic.com>
2017-05-04 16:25:28 -0400

    unbind the reader follow button since we don't need record_follow any more

--- client/reader/follow-button/index.jsx
+++ client/reader/follow-button/index.jsx
@@ -2,8 +2,6 @@
  * External dependencies
  */
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 
 /**
  * Internal dependencies
@@ -14,10 +12,6 @@ import {
 	recordFollow as recordFollowTracks,
 	recordUnfollow as recordUnfollowTracks,
 } from 'reader/stats';
-import {
-	recordFollow as recordFollowAction,
-	recordUnfollow as recordUnfollowAction,
-} from 'state/reader/follows/actions';
 
 function ReaderFollowButton( props ) {
 	const {
@@ -25,17 +19,13 @@ function ReaderFollowButton( props ) {
 		railcar,
 		followSource,
 		isButtonOnly,
-		dispatchRecordFollow,
-		dispatchRecordUnfollow,
 		siteUrl,
 	} = props;
 
 	function recordFollowToggle( isFollowing ) {
 		if ( isFollowing ) {
-			dispatchRecordFollow( siteUrl );
 			recordFollowTracks( siteUrl, railcar, { follow_source: followSource } );
 		} else {
-			dispatchRecordUnfollow( siteUrl );
 			recordUnfollowTracks( siteUrl, railcar, { follow_source: followSource } );
 		}
 
@@ -61,12 +51,4 @@ ReaderFollowButton.propTypes = {
 	followSource: React.PropTypes.string,
 };
 
-export default connect(
-	( state ) => ( {} ), // eslint-disable-line no-unused-vars
-	( dispatch ) => bindActionCreators( {
-		dispatchRecordFollow: recordFollowAction,
-		dispatchRecordUnfollow: recordUnfollowAction,
-	}, dispatch ),
-	null,
-	{ pure: true } // TODO: is this component pure?
-)( ReaderFollowButton );
+export default ReaderFollowButton;
