a43121c607305a5edd75f33ea71b6f9e3d67951c
Chris R <chris@bluefuton.com>
2017-05-04 18:34:30 +0200

    Move legacy redirects into main Reader controller (#13650)

--- client/reader/full-post/index.js
+++ client/reader/full-post/index.js
@@ -8,21 +8,18 @@ import page from 'page';
  */
 import { blogPost, feedPost } from './controller';
 import {
-	legacyRedirects,
 	updateLastRoute,
 	unmountSidebar,
 } from 'reader/controller';
 
 export default function() {
 	// Feed full post
-	page( '/read/post/feed/:feed_id/:post_id', legacyRedirects );
 	page( '/read/feeds/:feed/posts/:post',
 		updateLastRoute,
 		unmountSidebar,
 		feedPost );
 
 	// Blog full post
-	page( '/read/post/id/:blog_id/:post_id', legacyRedirects );
 	page( '/read/blogs/:blog/posts/:post',
 		updateLastRoute,
 		unmountSidebar,
