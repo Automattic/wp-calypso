c3650ba85921ccf26a26887d0878c320d21018d2
Andrija Vucinic <andrija.vucinic@gmail.com>
2017-05-05 22:33:29 +0200

    update existing site svg

--- client/signup/steps/design-type-with-store/existing-site.jsx
+++ client/signup/steps/design-type-with-store/existing-site.jsx
@@ -4,33 +4,56 @@
 import React from 'react';
 
 export default () => (
-	<svg width="172px" height="112px" viewBox="0 0 172 112" version="1.1" xmlns="http://www.w3.org/2000/svg">
+	<svg width="173px" height="114px" viewBox="0 0 173 114" version="1.1" xmlns="http://www.w3.org/2000/svg">
 		<defs>
-			<polygon id="path-1" points="0 15.7603469 171.708196 15.7603469 171.708196 111.769139 0 111.769139"></polygon>
+			<rect id="path-3" x="0" y="0" width="149" height="97"></rect>
 			<mask
-				id="mask-2" maskContentUnits="userSpaceOnUse"
-				maskUnits="objectBoundingBox" x="0" y="0" width="171.708196" height="96.0087922" fill="white">
-				<use xlinkHref="#path-1"></use>
+				id="mask-4" maskContentUnits="userSpaceOnUse" maskUnits="objectBoundingBox"
+				x="0" y="0" width="149" height="97" fill="white">
+				<use xlinkHref="#path-3"></use>
 			</mask>
 		</defs>
 		<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-			<g id="Group-3-Copy">
-				<g id="Group-2">
-					<g id="domain-illustration">
-						<g id="Imported-Layers">
-							<path d="M0.102747909,15.6269981 L0.102747909,2.72646653 C0.102747909,1.30182267
-								1.26010036,0.147016357 2.6878853,0.147016357 L169.225395,0.147016357
-								C170.653386,0.147016357 171.810944,1.30223275 171.810944,2.72708166
-								L171.810944,15.6269981 L0.102747909,15.6269981"
-									id="Fill-1"
-									fill="#CFDEE7">
-							</path>
-							<use id="Fill-6" stroke="#D9E8F0" mask="url(#mask-2)" strokeWidth="2" fill="#FFFFFF" xlinkHref="#path-1"></use>
+			<g id="Group-4">
+				<rect
+					id="Rectangle-1" stroke="#CFDEE7" strokeWidth="1.6" strokeDasharray="6.5"
+					x="1" y="1" width="171" height="112"></rect>
+				<g id="Group-3" transform="translate(11.000000, 8.000000)">
+					<g id="Group-2">
+						<g id="domain-illustration">
+							<g id="Imported-Layers">
+								<path
+									d="M0.0890083632,13.5587189 L0.0890083632,2.36561066 C0.0890083632,1.12952261
+									1.09159857,0.12755831 2.32845878,0.12755831 L146.596418,0.12755831
+									C147.833456,0.12755831 148.836225,1.12987842 148.836225,2.36614438
+									L148.836225,13.5587189 L0.0890083632,13.5587189" id="Fill-1" fill="#CFDEE7"></path>
+								<polygon id="Fill-6" fill="#FFFFFF"
+									points="0 13.6744186 148.747216 13.6744186 148.747216 96.9761648 0 96.9761648"></polygon>
+							</g>
 						</g>
+						<rect
+							id="Rectangle-4" fill="#CFDEE7" x="9.09923664" y="28.4883721"
+							width="96.6793893" height="5.69767442"></rect>
+						<rect
+							id="Rectangle-4-Copy" fill="#CFDEE7" x="9.09923664" y="39.8837209"
+							width="48.9083969" height="5.69767442"></rect>
+						<rect
+							id="Rectangle-4-Copy-2" fill="#B8F28A" x="9.09923664" y="52.4186047"
+							width="29.5725191" height="5.69767442"></rect>
 					</g>
-					<rect id="Rectangle-4" fill="#CFDEE7" x="10.5038168" y="32.834056" width="111.603053" height="6.56681119"></rect>
-					<rect id="Rectangle-4-Copy" fill="#CFDEE7" x="10.5038168" y="45.9676784" width="56.4580153" height="6.56681119"></rect>
-					<rect id="Rectangle-4-Copy-2" fill="#B8F28A" x="10.5038168" y="60.414663" width="34.1374046" height="6.56681119"></rect>
+					<use
+						id="Rectangle-5" stroke="#E6F0F6" mask="url(#mask-4)" strokeWidth="2" fill="#FFFFFF"
+						opacity="0.800000012" xlinkHref="#path-3"></use>
+					<g id="gridicons-add" transform="translate(56.000000, 30.000000)">
+						<rect id="Rectangle-path" x="0" y="0" width="37.7968291" height="37.8676208"></rect>
+						<path d="M18.9975297,7 C12.371294,7 7,12.3813542 7,19.0200005 C7,25.6586467
+						12.371294,31.0400009 18.9975297,31.0400009 C25.6237653,31.0400009 30.9950593,25.6586467
+						30.9950593,19.0200005 C30.9950593,12.3813542 25.6237653,7 18.9975297,7 L18.9975297,7
+						Z M24.9962945,20.2220005 L20.1972826,20.2220005 L20.1972826,25.0300007 L17.7977767,25.0300007
+						L17.7977767,20.2220005 L12.9987648,20.2220005 L12.9987648,17.8180004 L17.7977767,17.8180004
+						L17.7977767,13.0100002 L20.1972826,13.0100002 L20.1972826,17.8180004 L24.9962945,17.8180004
+						L24.9962945,20.2220005 L24.9962945,20.2220005 Z" id="Shape" fill="#284455"></path>
+					</g>
 				</g>
 			</g>
 		</g>
