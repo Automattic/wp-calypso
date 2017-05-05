471030b13c6114ac017944cba8929865823a1322
Ben Lowery <ben.lowery@automattic.com>
2017-05-05 13:52:47 -0400

    Reader: Add redux bound follow button

---
+++ client/reader/follow-button/redux.jsx
@@ -0,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import FollowButtonContainer from 'blocks/follow-button/redux';
+import FollowButton from 'blocks/follow-button/button';
+import {
+	recordFollow as recordFollowTracks,
+	recordUnfollow as recordUnfollowTracks,
+} from 'reader/stats';
+
+function ReaderFollowButton( props ) {
+	const {
+		onFollowToggle,
+		railcar,
+		followSource,
+		isButtonOnly,
+		siteUrl,
+	} = props;
+
+	function recordFollowToggle( isFollowing ) {
+		if ( isFollowing ) {
+			recordFollowTracks( siteUrl, railcar, { follow_source: followSource } );
+		} else {
+			recordUnfollowTracks( siteUrl, railcar, { follow_source: followSource } );
+		}
+
+		if ( onFollowToggle ) {
+			onFollowToggle( isFollowing );
+		}
+	}
+
+	if ( isButtonOnly ) {
+		return (
+			<FollowButton { ...props } onFollowToggle={ recordFollowToggle } />
+		);
+	}
+
+	return (
+		<FollowButtonContainer { ...props } onFollowToggle={ recordFollowToggle } />
+	);
+}
+
+ReaderFollowButton.propTypes = {
+	onFollowToggle: React.PropTypes.func,
+	railcar: React.PropTypes.object,
+	followSource: React.PropTypes.string,
+};
+
+export default ReaderFollowButton;
