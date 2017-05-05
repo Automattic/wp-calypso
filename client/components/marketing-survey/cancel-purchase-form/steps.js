b75eacdf97d1888bfc73ac88bdd374c0fd3d1ca4
Mark Ryall <mark@ryall.name>
2017-05-05 17:33:20 +1000

    add new step constants for automated transfer.

--- client/components/marketing-survey/cancel-purchase-form/steps.js
+++ client/components/marketing-survey/cancel-purchase-form/steps.js
@@ -2,3 +2,5 @@ export const INITIAL_STEP = 'initial_step';
 export const CONCIERGE_STEP = 'concierge_step';
 export const HAPPYCHAT_STEP = 'happychat_step';
 export const FINAL_STEP = 'final_step';
+export const BUSINESS_AT_STEP = 'business_at_step';
+export const UPGRADE_AT_STEP = 'upgrade_at_step';
