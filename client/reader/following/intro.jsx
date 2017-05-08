b0108c8b6cfaf3e984705ff2de8fef0611b6a7e8
Chris R <chris@bluefuton.com>
2017-05-08 17:01:16 +0200

    Make intro card dismissible (#13754)

--- client/reader/following/intro.jsx
+++ client/reader/following/intro.jsx
@@ -4,26 +4,27 @@
 import React from 'react';
 import { localize } from 'i18n-calypso';
 import Gridicon from 'gridicons';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 /**
  * Internal dependencies
  */
+import QueryPreferences from 'components/data/query-preferences';
+import { savePreference } from 'state/preferences/actions';
+import { getPreference } from 'state/preferences/selectors';
 
 const FollowingIntro = ( props ) => {
 
-	// @todo Return null if the user does not have is_new_reader = true in preferences
-
-	const handleIntroClose = () => {
-		// @todo Remove is_new_reader from preferences
-	};
+	if ( ! props.isNewReader ) {
+		return null;
+	}
 
 	return (
 		<header className="following__intro">
-
+			<QueryPreferences />
 			<div className="following__intro-header">
-
 				<div className="following__intro-copy">
-
 						{ props.translate(
 							'{{strong}}Welcome!{{/strong}} Reader is a custom magazine. ' +
 							'{{link}}Follow your favorite sites{{/link}} and their latest ' +
@@ -37,21 +38,29 @@ const FollowingIntro = ( props ) => {
 								}
 							}
 						) }
-
 				</div>
 
 				<div className="following__intro-close"
-					onClick={ handleIntroClose }
+					onClick={ props.dismiss }
 					title={ props.translate( 'Close' ) }
 					aria-label={ props.translate( 'Close' ) }>
 						<Gridicon icon="cross-circle" className="following__intro-close-icon" title={ props.translate( 'Close' ) } />
 						<span className="following__intro-close-icon-bg" />
 				</div>
-
 			</div>
-
 		</header>
 	);
 };
 
-export default localize( FollowingIntro );
+export default connect(
+	( state ) => {
+		return {
+			isNewReader: getPreference( state, 'is_new_reader' )
+		};
+	},
+	( dispatch ) => bindActionCreators( {
+		dismiss: () => {
+			return savePreference( 'is_new_reader', false );
+		}
+	}, dispatch )
+)( localize( FollowingIntro ) );
