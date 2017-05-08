98c001a505f517edab83eeff6a1df02790588302
Kelly Dwan <kelly.dwan@gmail.com>
2017-05-08 13:06:47 -0400

    Add the purchase type name

--- client/me/purchases/manage-purchase/index.jsx
+++ client/me/purchases/manage-purchase/index.jsx
@@ -381,6 +381,9 @@ class ManagePurchase extends Component {
 						<h2 className="manage-purchase__title">
 							{ getName( purchase ) }
 						</h2>
+						<div className="manage-purchase__description">
+							{ purchaseType( purchase ) }
+						</div>
 						<div className="manage-purchase__price">
 							<PlanPrice rawPrice={ purchase.amount } currencyCode={ purchase.currencyCode } />
 						</div>
