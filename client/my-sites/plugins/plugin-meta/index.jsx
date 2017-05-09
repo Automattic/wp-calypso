64193c298de1fdb3fbc1c6bd086f7df87d5047ca
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-05-09 16:35:26 -0700

    Convert to ES6 syntext

--- client/my-sites/plugins/plugin-meta/index.jsx
+++ client/my-sites/plugins/plugin-meta/index.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 import i18n from 'i18n-calypso';
@@ -10,7 +10,7 @@ import get from 'lodash/get';
 import { includes } from 'lodash';
 import { isEmpty } from 'lodash';
 import Gridicon from 'gridicons';
-import { localize } from 'i18n-calypso';
+import { localize, moment } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -45,33 +45,31 @@ import { isAutomatedTransferActive, isSiteAutomatedTransfer } from 'state/select
 import QueryEligibility from 'components/data/query-atat-eligibility';
 import { isATEnabled } from 'lib/automated-transfer';
 
-const PluginMeta = React.createClass( {
-	OUT_OF_DATE_YEARS: 2,
+class PluginMeta extends Component {
+	static OUT_OF_DATE_YEARS = 2;
 
-	propTypes: {
-		siteURL: React.PropTypes.string,
-		sites: React.PropTypes.array,
-		notices: React.PropTypes.object,
-		plugin: React.PropTypes.object.isRequired,
-		isInstalledOnSite: React.PropTypes.bool,
-		isPlaceholder: React.PropTypes.bool,
-		isMock: React.PropTypes.bool,
-		allowedActions: React.PropTypes.shape( {
-			activation: React.PropTypes.bool,
-			autoupdate: React.PropTypes.bool,
-			remove: React.PropTypes.bool,
+	static propTypes = {
+		siteURL: PropTypes.string,
+		sites: PropTypes.array,
+		notices: PropTypes.object,
+		plugin: PropTypes.object.isRequired,
+		isInstalledOnSite: PropTypes.bool,
+		isPlaceholder: PropTypes.bool,
+		isMock: PropTypes.bool,
+		allowedActions: PropTypes.shape( {
+			activation: PropTypes.bool,
+			autoupdate: PropTypes.bool,
+			remove: PropTypes.bool,
 		} ),
-	},
+	};
 
-	getDefaultProps() {
-		return {
-			allowedActions: {
-				activation: true,
-				autoupdate: true,
-				remove: true,
-			}
-		};
-	},
+	static defaultProps = {
+		allowedActions: {
+			activation: true,
+			autoupdate: true,
+			remove: true,
+		}
+	};
 
 	displayBanner() {
 		if ( this.props.plugin && this.props.plugin.banners && ( this.props.plugin.banners.high || this.props.plugin.banners.low ) ) {
@@ -80,16 +78,16 @@ const PluginMeta = React.createClass( {
 						src={ this.props.plugin.banners.high || this.props.plugin.banners.low } />
 					</div>;
 		}
-	},
+	}
 
 	hasBusinessPlan() {
 		if ( ! this.props.selectedSite ) {
 			return false;
 		}
 		return isBusiness( this.props.selectedSite.plan ) || isEnterprise( this.props.selectedSite.plan );
-	},
+	}
 
-	isWpcomPreinstalled: function() {
+	isWpcomPreinstalled = () => {
 		const installedPlugins = [ 'Jetpack by WordPress.com', 'Akismet', 'VaultPress' ];
 
 		if ( ! this.props.selectedSite ) {
@@ -97,13 +95,13 @@ const PluginMeta = React.createClass( {
 		}
 
 		return ! this.props.selectedSite.jetpack && includes( installedPlugins, this.props.plugin.name );
-	},
+	};
 
 	renderActions() {
 		if ( ! this.props.selectedSite ) {
 			return (
 				<div className="plugin-meta__actions">
-					<div className="plugin-item__count">
+					<div className="plugin-meta__count">
 						{
 							this.props.translate( 'Sites {{count/}}',
 								{
@@ -165,7 +163,7 @@ const PluginMeta = React.createClass( {
 						isMock={ this.props.isMock } /> }
 			</div>
 		);
-	},
+	}
 
 	renderName() {
 		if ( ! this.props.plugin || ! this.props.plugin.name ) {
@@ -175,7 +173,7 @@ const PluginMeta = React.createClass( {
 		return (
 			<div className="plugin-meta__name">{ this.props.plugin.name }</div>
 		);
-	},
+	}
 
 	renderAuthorUrl() {
 		if ( ! this.props.plugin || ! ( this.props.plugin.author_url && this.props.plugin.author_name ) ) {
@@ -192,7 +190,7 @@ const PluginMeta = React.createClass( {
 				linkToAuthor
 			}
 		} );
-	},
+	}
 
 	isUnsupportedPluginForAT() {
 		const { plugin } = this.props;
@@ -213,19 +211,19 @@ const PluginMeta = React.createClass( {
 		];
 
 		return includes( unsupportedPlugins, plugin.slug );
-	},
+	}
 
 	isWpcomInstallDisabled() {
 		const { isTransfering } = this.props;
 
 		return ! this.hasBusinessPlan() || this.isUnsupportedPluginForAT() || isTransfering;
-	},
+	}
 
 	isJetpackInstallDisabled() {
 		const { automatedTransferSite } = this.props;
 
 		return automatedTransferSite && this.isUnsupportedPluginForAT();
-	},
+	}
 
 	getInstallButton() {
 		const { selectedSite } = this.props;
@@ -247,7 +245,7 @@ const PluginMeta = React.createClass( {
 				/>
 			);
 		}
-	},
+	}
 
 	maybeDisplayUnsupportedNotice() {
 		const { selectedSite, automatedTransferSite } = this.props;
@@ -265,15 +263,15 @@ const PluginMeta = React.createClass( {
 				</Notice>
 			);
 		}
-	},
+	}
 
 	isOutOfDate() {
 		if ( this.props.plugin && this.props.plugin.last_updated ) {
-			const lastUpdated = this.moment( this.props.plugin.last_updated, 'YYYY-MM-DD' );
-			return this.moment().diff( lastUpdated, 'years' ) >= this.OUT_OF_DATE_YEARS;
+			const lastUpdated = moment( this.props.plugin.last_updated, 'YYYY-MM-DD' );
+			return moment().diff( lastUpdated, 'years' ) >= this.OUT_OF_DATE_YEARS;
 		}
 		return false;
-	},
+	}
 
 	getVersionWarning() {
 		const newVersions = this.getAvailableNewVersions();
@@ -291,7 +289,7 @@ const PluginMeta = React.createClass( {
 				status="is-warning"
 				showDismiss={ false } />;
 		}
-	},
+	}
 
 	getDefaultActionLinks( plugin ) {
 		let adminUrl = get( this.props, 'selectedSite.options.admin_url' );
@@ -304,7 +302,7 @@ const PluginMeta = React.createClass( {
 		return adminUrl
 			? { [ i18n.translate( 'WP Admin' ) ]: adminUrl }
 			: null;
-	},
+	}
 
 	getUpdateWarning() {
 		const newVersions = this.getAvailableNewVersions();
@@ -348,7 +346,7 @@ const PluginMeta = React.createClass( {
 				</Notice>
 			);
 		}
-	},
+	}
 
 	isVersionCompatible() {
 		if ( ! this.props.selectedSite ) {
@@ -368,7 +366,7 @@ const PluginMeta = React.createClass( {
 		return some( this.props.plugin.compatibility, compatibleVersion => {
 			return compatibleVersion.indexOf( siteVersion ) === 0;
 		} );
-	},
+	}
 
 	hasOrgInstallButton() {
 		if ( this.props.selectedSite ) {
@@ -376,7 +374,7 @@ const PluginMeta = React.createClass( {
 				userCan( 'manage_options', this.props.selectedSite ) &&
 				this.props.selectedSite.jetpack;
 		}
-	},
+	}
 
 	getAvailableNewVersions() {
 		return this.props.sites.map( site => {
@@ -392,9 +390,9 @@ const PluginMeta = React.createClass( {
 				}
 			}
 		} ).filter( newVersions => newVersions );
-	},
+	}
 
-	handlePluginUpdatesSingleSite( event ) {
+	handlePluginUpdatesSingleSite = ( event ) => {
 		event.preventDefault();
 		PluginsActions.updatePlugin( this.props.sites[ 0 ], this.props.sites[ 0 ].plugin );
 
@@ -404,9 +402,9 @@ const PluginMeta = React.createClass( {
 			plugin: this.props.sites[ 0 ].plugin.slug,
 			selected_site: this.props.sites[ 0 ].ID
 		} );
-	},
+	}
 
-	handlePluginUpdatesMultiSite( event ) {
+	handlePluginUpdatesMultiSite = ( event ) => {
 		event.preventDefault();
 		this.props.sites.forEach( ( site ) => {
 			const { plugin } = site;
@@ -422,7 +420,7 @@ const PluginMeta = React.createClass( {
 		} );
 
 		analytics.ga.recordEvent( 'Plugins', 'Clicked Update All Sites Plugin', 'Plugin Name', this.props.pluginSlug );
-	},
+	}
 
 	render() {
 		const cardClasses = classNames( 'plugin-meta__information', {
@@ -506,7 +504,7 @@ const PluginMeta = React.createClass( {
 			</div>
 		);
 	}
-} );
+}
 
 const mapStateToProps = state => {
 	const siteId = getSelectedSiteId( state );
