ed010a99c8a64793f697d78f1473bc1da3199183
Donna Peplinskie <donnapep@gmail.com>
2017-05-10 15:27:05 -0400

    Remove return statement since arrow functions return implicitly

--- client/extensions/wp-super-cache/fix-config.jsx
+++ client/extensions/wp-super-cache/fix-config.jsx
@@ -11,23 +11,19 @@ import Button from 'components/button';
 import Card from 'components/card';
 import SectionHeader from 'components/section-header';
 
-const FixConfig = ( { translate } ) => {
-	return (
-		<div>
-			<SectionHeader label={ translate( 'Fix Configuration' ) } />
-			<Card>
-				<form>
-					<div>
-						<Button
-							compact
-							type="submit">
-							{ translate( 'Restore Default Configuration' ) }
-						</Button>
-					</div>
-				</form>
-			</Card>
-		</div>
-	);
-};
+const FixConfig = ( { translate } ) =>
+	<div>
+		<SectionHeader label={ translate( 'Fix Configuration' ) } />
+		<Card>
+			<form>
+				<div>
+					<Button
+						compact>
+						{ translate( 'Restore Default Configuration' ) }
+					</Button>
+				</div>
+			</form>
+		</Card>
+	</div>;
 
 export default localize( FixConfig );
