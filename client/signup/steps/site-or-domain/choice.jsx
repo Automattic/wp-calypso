dbffe49741c7d0c0518698fe2d54b86a40c82b9e
Igor Klimer <igor@klimer.eu>
2017-05-10 23:57:28 +0200

    Wrap the choice in an a tag, to help with a11y

--- client/signup/steps/site-or-domain/choice.jsx
+++ client/signup/steps/site-or-domain/choice.jsx
@@ -19,16 +19,18 @@ export default class SiteOrDomainChoice extends Component {
 	render() {
 		const { choice } = this.props;
 		return (
-			<div className="site-or-domain__choice" key={ choice.type } onClick={ this.handleClickChoice }>
-				<Card compact className="site-or-domain__choice-image">
-					{ choice.image }
-				</Card>
-				<Card compact className="site-or-domain__choice-text">
-					<div className="site-or-domain__choice-button">
-						<Button>{ choice.label }</Button>
-					</div>
-					<p>{ choice.description }</p>
-				</Card>
+			<div className="site-or-domain__choice" key={ choice.type }>
+				<a className="site-or-domain__choice-link" onClick={ this.handleClickChoice }>
+					<Card compact className="site-or-domain__choice-image">
+						{ choice.image }
+					</Card>
+					<Card compact className="site-or-domain__choice-text">
+						<div className="site-or-domain__choice-button">
+							<Button>{ choice.label }</Button>
+						</div>
+						<p>{ choice.description }</p>
+					</Card>
+				</a>
 			</div>
 		);
 	}
