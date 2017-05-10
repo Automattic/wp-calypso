154d12239532dec88fae53ebd181e0e6427c0b69
Ben Lowery <ben.lowery@automattic.com>
2017-05-10 11:38:22 -0400

    fix weird spacing in comments

--- client/state/reader/feeds/selectors.js
+++ client/state/reader/feeds/selectors.js
@@ -1,12 +1,12 @@
 const DAY_IN_MILLIS = 24 * 60 * 1000 * 1000;
 
 /**
-* Returns true if we should fetch the feed
-*
-* @param  {Object}  state  Global state tree
-* @param  {Number}  feedId The feed ID
-* @return {Boolean}        Whether feed should be fetched
-*/
+ * Returns true if we should fetch the feed
+ *
+ * @param  {Object}  state  Global state tree
+ * @param  {Number}  feedId The feed ID
+ * @return {Boolean}        Whether feed should be fetched
+ */
 
 export function shouldFeedBeFetched( state, feedId ) {
 	const isNotQueued = ! state.reader.feeds.queuedRequests[ feedId ];
@@ -22,13 +22,14 @@ function isStale( state, feedId ) {
 	return lastFetched <= ( Date.now() - DAY_IN_MILLIS );
 }
 
+
 /**
-* Returns a feed object
-*
-* @param  {Object}  state  Global state tree
-* @param  {Number}  feedId The feed ID
-* @return {Object}        Feed
-*/
+ * Returns a feed object
+ *
+ * @param  {Object}  state  Global state tree
+ * @param  {Number}  feedId The feed ID
+ * @return {Object}        Feed
+ */
 
 export function getFeed( state, feedId ) {
 	return state.reader.feeds.items[ feedId ];
--- client/state/reader/sites/selectors.js
+++ client/state/reader/sites/selectors.js
@@ -1,12 +1,12 @@
 const DAY_IN_MILLIS = 24 * 60 * 1000 * 1000;
 
 /**
-* Returns true if we should fetch the site
-*
-* @param  {Object}  state  Global state tree
-* @param  {Number}  siteId The site ID
-* @return {Boolean}        Whether site should be fetched
-*/
+ * Returns true if we should fetch the site
+ *
+ * @param  {Object}  state  Global state tree
+ * @param  {Number}  siteId The site ID
+ * @return {Boolean}        Whether site should be fetched
+ */
 
 export function shouldSiteBeFetched( state, siteId ) {
 	const isNotQueued = ! state.reader.sites.queuedRequests[ siteId ];
@@ -23,12 +23,12 @@ function isStale( state, siteId ) {
 }
 
 /**
-* Returns a site object
-*
-* @param  {Object}  state  Global state tree
-* @param  {Number}  siteId The site ID
-* @return {Object}        Site
-*/
+ * Returns a site object
+ *
+ * @param  {Object}  state  Global state tree
+ * @param  {Number}  siteId The site ID
+ * @return {Object}        Site
+ */
 
 export function getSite( state, siteId ) {
 	return state.reader.sites.items[ siteId ];
