30a89dfad40452b6b69cac0ba071862516244d83
Leif Singer <git@singer.sh>
2017-05-11 10:39:50 +0200

    Editor: enable the editor basics tour in all environments
    Reverses #13607.

--- client/layout/guided-tours/tours/editor-basics-tour.js
+++ client/layout/guided-tours/tours/editor-basics-tour.js
@@ -21,7 +21,6 @@ import {
 	Link,
 } from 'layout/guided-tours/config-elements';
 import {
-	isEnabled,
 	isNewUser,
 } from 'state/ui/guided-tours/contexts';
 import { isDesktop } from 'lib/viewport';
@@ -31,7 +30,7 @@ export const EditorBasicsTour = makeTour(
 		name="editorBasicsTour"
 		version="20170503"
 		path="/post/"
-		when={ and( isEnabled( 'guided-tours/editor-basics-tour' ), isDesktop, isNewUser ) }
+		when={ and( isDesktop, isNewUser ) }
 	>
 		<Step
 			name="init"
