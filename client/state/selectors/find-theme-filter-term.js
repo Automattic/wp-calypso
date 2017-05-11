1b00a07b0ea31d6a80298a2315ae958b4af7af3b
Bernhard Reiter <ockham@raz.or.at>
2017-05-11 14:42:22 +0200

    Selectors: Memoize findThemeFilterTerm

--- client/state/selectors/find-theme-filter-term.js
+++ client/state/selectors/find-theme-filter-term.js
@@ -6,6 +6,7 @@ import { filter, get } from 'lodash';
 /**
  * Internal dependencies
  */
+import createSelector from 'lib/create-selector';
 import { getThemeFilters, getThemeFilterTerm } from './';
 
 /**
@@ -15,21 +16,24 @@ import { getThemeFilters, getThemeFilterTerm } from './';
  * @param  {String}  search The term to search for
  * @return {Object}         A filter term object
  */
-export default function findThemeFilterTerm( state, search ) {
-	const [ left, right ] = search.split( ':' );
-	if ( right ) {
-		return getThemeFilterTerm( state, left, right );
-	}
+export default createSelector(
+	( state, search ) => {
+		const [ left, right ] = search.split( ':' );
+		if ( right ) {
+			return getThemeFilterTerm( state, left, right );
+		}
 
-	const filters = getThemeFilters( state );
+		const filters = getThemeFilters( state );
 
-	const results = filter( filters, ( terms ) => (
-		!! get( terms, left )
-	) );
+		const results = filter( filters, ( terms ) => (
+			!! get( terms, left )
+		) );
 
-	if ( results.length !== 1 ) {
-		// No or ambiguous results
-		return null;
-	}
-	return results[ 0 ][ left ];
-}
+		if ( results.length !== 1 ) {
+			// No or ambiguous results
+			return null;
+		}
+		return results[ 0 ][ left ];
+	},
+	( state ) => [ getThemeFilters( state ) ]
+);
