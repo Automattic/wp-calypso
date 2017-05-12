045893e2dc42ef91c90e3e1355cb1bafdec62c57
Chris R <chris@bluefuton.com>
2017-05-12 14:07:26 +0200

    Reader Following Intro: banner should be dismissed when user clicks Manage Following link (#13955)
    * Reader Following Intro: banner should be dismissed when user clicks Manage Following link
    
    * Prevent double firing of track

--- client/reader/following/intro.jsx
+++ client/reader/following/intro.jsx
@@ -33,15 +33,11 @@ class FollowingIntro extends React.Component {
 		}
 	};
 
-	recordManageLinkTrack = () => {
-		recordTrack( 'calypso_reader_following_intro_link_clicked' );
-	};
-
 	render() {
 		const { isNewReader, translate, dismiss } = this.props;
 		const linkElement = config.isEnabled( 'reader/following-manage-refresh' )
-			? <a onClick={ this.recordManageLinkTrack } href="/following/manage" />
-			: <a onClick={ this.recordManageLinkTrack } href="/following/edit" />;
+			? <a onClick={ this.props.handleManageLinkClick } href="/following/manage" />
+			: <a onClick={ this.props.handleManageLinkClick } href="/following/edit" />;
 
 		if ( ! isNewReader ) {
 			return null;
@@ -98,6 +94,10 @@ export default connect(
 			{
 				dismiss: () => {
 					recordTrack( 'calypso_reader_following_intro_dismiss' );
+					return savePreference( 'is_new_reader', false );
+				},
+				handleManageLinkClick: () => {
+					recordTrack( 'calypso_reader_following_intro_link_clicked' );
 					return savePreference( 'is_new_reader', false );
 				},
 			},
