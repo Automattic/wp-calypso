a7ae224b261317d88426e937666ad6467e31b04c
Gary Pendergast <gary@pento.net>
2017-05-12 16:08:06 +1000

    Show a message when 3rd party cookies are disabled

--- client/components/social-buttons/google.js
+++ client/components/social-buttons/google.js
@@ -19,6 +19,8 @@ class GoogleLoginButton extends Component {
 		fetchBasicProfile: true,
 	};
 
+	state = { error: false };
+
 	constructor( props ) {
 		super( props );
 
@@ -45,6 +47,10 @@ class GoogleLoginButton extends Component {
 			return this.initialized;
 		}
 
+		this.setState( { error: '' } );
+
+		const { translate } = this.props;
+
 		this.initialized = this.loadDependency()
 			.then( gapi => new Promise( resolve => gapi.load( 'client:auth2', resolve ) ).then( () => gapi ) )
 			.then( gapi => gapi.client.init( {
@@ -56,6 +62,14 @@ class GoogleLoginButton extends Component {
 			).catch( error => {
 				this.initialized = null;
 
+				if ( 'idpiframe_initialization_failed' === error.error ) {
+					// This error is caused by 3rd party cookies being blocked.
+					this.setState( { error: translate( 'Your privacy settings are blocking us from connecting to your Google account. ' +
+						'Please enable "third-party cookies" in your browser or operating system, or log in with an email address instead.'
+					) } );
+					return null;
+				}
+
 				return Promise.reject( error );
 			} );
 
@@ -64,6 +78,10 @@ class GoogleLoginButton extends Component {
 
 	handleClick( event ) {
 		event.preventDefault();
+
+		if ( this.state.error ) {
+			return;
+		}
 
 		const { responseHandler } = this.props;
 
@@ -75,26 +93,33 @@ class GoogleLoginButton extends Component {
 	}
 
 	render() {
+		let buttonDisabled = false;
+		if ( this.state.error ) {
+			buttonDisabled = true;
+		}
 		return (
-			<button className="button" onClick={ this.handleClick }>
-				<svg className="social-buttons__logo" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-					{ /* eslint-disable max-len */ }
-					<g fill="none" fillRule="evenodd">
-						<path d="M19.6 10.227c0-.709-.064-1.39-.182-2.045H10v3.868h5.382a4.6 4.6 0 0 1-1.996 3.018v2.51h3.232c1.891-1.742 2.982-4.305 2.982-7.35z" fill="#4285F4" />
-						<path d="M10 20c2.7 0 4.964-.895 6.618-2.423l-3.232-2.509c-.895.6-2.04.955-3.386.955-2.605 0-4.81-1.76-5.595-4.123H1.064v2.59A9.996 9.996 0 0 0 10 20z" fill="#34A853" />
-						<path d="M4.405 11.9c-.2-.6-.314-1.24-.314-1.9 0-.66.114-1.3.314-1.9V5.51H1.064A9.996 9.996 0 0 0 0 10c0 1.614.386 3.14 1.064 4.49l3.34-2.59z" fill="#FBBC05" />
-						<path d="M10 3.977c1.468 0 2.786.505 3.823 1.496l2.868-2.868C14.959.99 12.695 0 10 0 6.09 0 2.71 2.24 1.064 5.51l3.34 2.59C5.192 5.736 7.396 3.977 10 3.977z" fill="#EA4335" />
-					{ /* eslint-enable max-len */ }
-					</g>
-				</svg>
+			<div>
+				<button className="social-buttons__button button" onClick={ this.handleClick } disabled={ buttonDisabled }>
+					<svg className="social-buttons__logo" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+						{ /* eslint-disable max-len */ }
+						<g fill="none" fillRule="evenodd">
+							<path d="M19.6 10.227c0-.709-.064-1.39-.182-2.045H10v3.868h5.382a4.6 4.6 0 0 1-1.996 3.018v2.51h3.232c1.891-1.742 2.982-4.305 2.982-7.35z" fill="#4285F4" />
+							<path d="M10 20c2.7 0 4.964-.895 6.618-2.423l-3.232-2.509c-.895.6-2.04.955-3.386.955-2.605 0-4.81-1.76-5.595-4.123H1.064v2.59A9.996 9.996 0 0 0 10 20z" fill="#34A853" />
+							<path d="M4.405 11.9c-.2-.6-.314-1.24-.314-1.9 0-.66.114-1.3.314-1.9V5.51H1.064A9.996 9.996 0 0 0 0 10c0 1.614.386 3.14 1.064 4.49l3.34-2.59z" fill="#FBBC05" />
+							<path d="M10 3.977c1.468 0 2.786.505 3.823 1.496l2.868-2.868C14.959.99 12.695 0 10 0 6.09 0 2.71 2.24 1.064 5.51l3.34 2.59C5.192 5.736 7.396 3.977 10 3.977z" fill="#EA4335" />
+						{ /* eslint-enable max-len */ }
+						</g>
+					</svg>
 
-				<span className="social-buttons__service-name">
-					{ this.props.translate( 'Continue with %(service)s', {
-						args: { service: 'Google' },
-						comment: '%(service)s is the name of a Social Network, e.g. "Google", "Facebook", "Twitter" ...'
-					} ) }
-				</span>
-			</button>
+					<span className="social-buttons__service-name">
+						{ this.props.translate( 'Continue with %(service)s', {
+							args: { service: 'Google' },
+							comment: '%(service)s is the name of a Social Network, e.g. "Google", "Facebook", "Twitter" ...'
+						} ) }
+					</span>
+				</button>
+				<div className="social-buttons__service-error">{ this.state.error }</div>
+			</div>
 		);
 	}
 }
