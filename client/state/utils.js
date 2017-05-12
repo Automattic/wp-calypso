b6fad0598e7fbb3abdfe4d79eb43ee36aa1b942a
Dennis Snell <dennis.snell@automattic.com>
2017-05-12 13:45:37 -0500

    Add two-level persistence to keyedReducer
    Previously the `keyedReducer` was "eating" the SERIALIZE and DESERIALIZE
    actions because they never had the attached `key` value. This meant that
    the entire state was being persisted (I think)
    
    Now they pass through those actions to each item in the collection on
    SERIALIZE and return through an optional serializer and deserializer.

--- client/state/utils.js
+++ client/state/utils.js
@@ -2,7 +2,14 @@
  * External dependencies
  */
 import validator from 'is-my-json-valid';
-import { merge, flow, partialRight, reduce } from 'lodash';
+import {
+	flow,
+	identity,
+	mapValues,
+	merge,
+	partialRight,
+	reduce,
+} from 'lodash';
 import { combineReducers } from 'redux';
 
 /**
@@ -73,9 +80,11 @@ export function isValidStateWithSchema( state, schema ) {
  *
  * @param {string} keyName name of key in action referencing item in state map
  * @param {function} reducer applied to referenced item in state map
+ * @param {function} deserializer takes entire state of collection and deserializes
+ * @param {function} serializer takes entire state of collection and serializes
  * @return {function} super-reducer applying reducer over map of keyed items
  */
-export const keyedReducer = ( keyName, reducer ) => {
+export const keyedReducer = ( keyName, reducer, { deserializer, serializer } = { deserializer: identity, serializer: identity } ) => {
 	// some keys are invalid
 	if ( 'string' !== typeof keyName ) {
 		throw new TypeError( `Key name passed into ``keyedReducer`` must be a string but I detected a ${ typeof keyName }` );
@@ -92,6 +101,14 @@ export const keyedReducer = ( keyName, reducer ) => {
 	const initialState = reducer( undefined, { type: '@@calypso/INIT' } );
 
 	return ( state = {}, action ) => {
+		if ( DESERIALIZE === action.type ) {
+			return deserializer( state );
+		}
+
+		if ( SERIALIZE === action.type ) {
+			return serializer( mapValues( state, item => reducer( item, action ) ) );
+		}
+
 		// don't allow coercion of key name: null => 0
 		if ( ! action.hasOwnProperty( keyName ) ) {
 			return state;
