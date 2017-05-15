4fa01afebaeec53dd8a7278c60e93aa53aeb15c3
Marin Atanasov <tyxla@abv.bg>
2017-05-15 14:59:24 +0300

    Ads: Use localize HoC in earnings form

--- client/my-sites/ads/form-earnings.jsx
+++ client/my-sites/ads/form-earnings.jsx
@@ -4,6 +4,7 @@
 import React, { Component } from 'react';
 import notices from 'notices';
 import classNames from 'classnames';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -142,27 +143,29 @@ class AdsFormEarnings extends Component {
 	}
 
 	getStatus( status ) {
+		const { translate } = this.props;
 		var statuses = {
-			0: this.translate( 'Unpaid' ),
-			1: this.translate( 'Paid' ),
-			2: this.translate( 'a8c-only' ),
-			3: this.translate( 'Pending (Missing Tax Info)' ),
-			4: this.translate( 'Pending (Invalid PayPal)' )
+			0: translate( 'Unpaid' ),
+			1: translate( 'Paid' ),
+			2: translate( 'a8c-only' ),
+			3: translate( 'Pending (Missing Tax Info)' ),
+			4: translate( 'Pending (Invalid PayPal)' )
 		}
 
 		return statuses[ status ] ? statuses[ status ] : '?';
 	}
 
 	payoutNotice() {
+		const { translate } = this.props;
 		var owed = this.state.earnings && this.state.earnings.total_amount_owed ? this.state.earnings.total_amount_owed : '0.00',
-			notice = this.translate(
+			notice = translate(
 				'Outstanding amount of $%(amountOwed)s does not exceed the minimum $100 needed to make the payment. Payment will be made as soon as the total outstanding amount has reached $100.',
 				{
 					comment: 'Insufficient balance for payout.',
 					args: { amountOwed: owed }
 				}
 			),
-			payout = this.translate(
+			payout = translate(
 				'Outstanding amount of $%(amountOwed)s will be paid by the last business day of the month.',
 				{
 					comment: 'Payout will proceed.',
@@ -178,21 +181,23 @@ class AdsFormEarnings extends Component {
 	}
 
 	infoNotice() {
+		const { translate } = this.props;
+
 		return (
 			<div className="module-content-text module-content-text-info">
-				<p>{ this.translate( 'Payments can have the following statuses:' ) }</p>
+				<p>{ translate( 'Payments can have the following statuses:' ) }</p>
 				<ul className="earnings_history__statuses-list">
-					<li className="earnings_history__status"><strong>{ this.translate( 'Unpaid:' ) } </strong>
-						{ this.translate( 'Payment is on hold until the end of the current month.' ) }
+					<li className="earnings_history__status"><strong>{ translate( 'Unpaid:' ) } </strong>
+						{ translate( 'Payment is on hold until the end of the current month.' ) }
 					</li>
-					<li className="earnings_history__status"><strong>{ this.translate( 'Paid:' ) } </strong>
-						{ this.translate( 'Payment has been processed through PayPal.' ) }
+					<li className="earnings_history__status"><strong>{ translate( 'Paid:' ) } </strong>
+						{ translate( 'Payment has been processed through PayPal.' ) }
 					</li>
-					<li className="earnings_history__status"><strong>{ this.translate( 'Pending (Missing Tax Info):' ) } </strong>
-						{ this.translate( 'Payment is pending due to missing information. You can provide tax information in the settings screen.' ) }
+					<li className="earnings_history__status"><strong>{ translate( 'Pending (Missing Tax Info):' ) } </strong>
+						{ translate( 'Payment is pending due to missing information. You can provide tax information in the settings screen.' ) }
 					</li>
-					<li className="earnings_history__status"><strong>{ this.translate( 'Pending (Invalid PayPal):' ) } </strong>
-						{ this.translate( 'Payment processing has failed due to invalid PayPal address. You can correct the PayPal address in the settings screen.' ) }
+					<li className="earnings_history__status"><strong>{ translate( 'Pending (Invalid PayPal):' ) } </strong>
+						{ translate( 'Payment processing has failed due to invalid PayPal address. You can correct the PayPal address in the settings screen.' ) }
 					</li>
 				</ul>
 			</div>
@@ -200,6 +205,7 @@ class AdsFormEarnings extends Component {
 	}
 
 	earningsBreakdown() {
+		const { numberFormat, translate } = this.props;
 		var earnings = this.state.earnings && this.state.earnings.total_earnings ? Number( this.state.earnings.total_earnings ) : 0,
 			owed = this.state.earnings && this.state.earnings.total_amount_owed ? Number( this.state.earnings.total_amount_owed ) : 0,
 			paid = this.state.earnings && this.state.earnings.total_earnings && this.state.earnings.total_amount_owed ?
@@ -208,22 +214,23 @@ class AdsFormEarnings extends Component {
 		return (
 			<ul className="earnings_breakdown__list" >
 				<li className="earnings_breakdown__item">
-					<span className="earnings_breakdown__label">{ this.translate( 'Total earnings', { context: 'Sum of earnings' } ) }</span>
-					<span className="earnings_breakdown__value">${ this.numberFormat( earnings, 2 ) }</span>
+					<span className="earnings_breakdown__label">{ translate( 'Total earnings', { context: 'Sum of earnings' } ) }</span>
+					<span className="earnings_breakdown__value">${ numberFormat( earnings, 2 ) }</span>
 				</li>
 				<li className="earnings_breakdown__item">
-					<span className="earnings_breakdown__label">{ this.translate( 'Total paid', { context: 'Sum of earnings that have been distributed' } ) }</span>
-					<span className="earnings_breakdown__value">${ this.numberFormat( paid, 2 ) }</span>
+					<span className="earnings_breakdown__label">{ translate( 'Total paid', { context: 'Sum of earnings that have been distributed' } ) }</span>
+					<span className="earnings_breakdown__value">${ numberFormat( paid, 2 ) }</span>
 				</li>
 				<li className="earnings_breakdown__item">
-					<span className="earnings_breakdown__label">{ this.translate( 'Outstanding amount', { context: 'Sum earnings left unpaid' } ) }</span>
-					<span className="earnings_breakdown__value">${ this.numberFormat( owed, 2 ) }</span>
+					<span className="earnings_breakdown__label">{ translate( 'Outstanding amount', { context: 'Sum earnings left unpaid' } ) }</span>
+					<span className="earnings_breakdown__value">${ numberFormat( owed, 2 ) }</span>
 				</li>
 			</ul>
 		);
 	}
 
 	earningsTable( earnings, header_text, type ) {
+		const { numberFormat, translate } = this.props;
 		var period,
 			rows = [],
 			infoIcon = this.getInfoToggle( type ) ? 'info' : 'info-outline',
@@ -236,7 +243,7 @@ class AdsFormEarnings extends Component {
 				rows.push(
 					<tr key={ type + '-' + period }>
 						<td className="earnings-history__value">{ this.swapYearMonth( period ) }</td>
-						<td className="earnings-history__value">${ this.numberFormat( earnings[ period ].amount, 2 ) }</td>
+						<td className="earnings-history__value">${ numberFormat( earnings[ period ].amount, 2 ) }</td>
 						<td className="earnings-history__value">{ earnings[ period ].pageviews }</td>
 						<td className="earnings-history__value">{ this.getStatus( earnings[ period ].status ) }</td>
 					</tr>
@@ -252,8 +259,8 @@ class AdsFormEarnings extends Component {
 						<li className="module-header-action toggle-info">
 							<a href="#"
 								className="module-header-action-link"
-								aria-label={ this.translate( 'Show or hide panel information' ) }
-								title={ this.translate( 'Show or hide panel information' ) }
+								aria-label={ translate( 'Show or hide panel information' ) }
+								title={ translate( 'Show or hide panel information' ) }
 								onClick={ this.toggleInfo.bind( this, type ) }>
 								<Gridicon icon={ infoIcon } />
 							</a>
@@ -265,10 +272,10 @@ class AdsFormEarnings extends Component {
 					<table>
 						<thead>
 							<tr>
-								<th className="earnings-history__header">{ this.translate( 'Period' ) }</th>
-								<th className="earnings-history__header">{ this.translate( 'Earnings' ) }</th>
-								<th className="earnings-history__header">{ this.translate( 'Ad Impressions' ) }</th>
-								<th className="earnings-history__header">{ this.translate( 'Status' ) }</th>
+								<th className="earnings-history__header">{ translate( 'Period' ) }</th>
+								<th className="earnings-history__header">{ translate( 'Earnings' ) }</th>
+								<th className="earnings-history__header">{ translate( 'Ad Impressions' ) }</th>
+								<th className="earnings-history__header">{ translate( 'Status' ) }</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -281,6 +288,7 @@ class AdsFormEarnings extends Component {
 	}
 
 	render() {
+		const { translate } = this.props;
 		var infoIcon = this.state.showEarningsNotice ? 'info' : 'info-outline',
 			classes = classNames( 'earnings_breakdown', {
 				'is-showing-info': this.state.showEarningsNotice
@@ -290,13 +298,13 @@ class AdsFormEarnings extends Component {
 			<div>
 				<Card className={ classes }>
 					<div className="module-header">
-						<h1 className="module-header-title">{ this.translate( 'Totals' ) }</h1>
+						<h1 className="module-header-title">{ translate( 'Totals' ) }</h1>
 						<ul className="module-header-actions">
 							<li className="module-header-action toggle-info">
 								<a href="#"
 									className="module-header-action-link"
-									aria-label={ this.translate( 'Show or hide panel information' ) }
-									title={ this.translate( 'Show or hide panel information' ) }
+									aria-label={ translate( 'Show or hide panel information' ) }
+									title={ translate( 'Show or hide panel information' ) }
 									onClick={ this.toggleEarningsNotice } >
 									<Gridicon icon={ infoIcon } />
 								</a>
@@ -309,15 +317,15 @@ class AdsFormEarnings extends Component {
 					</div>
 				</Card>
 				{ this.state.earnings && this.checkSize( this.state.earnings.wordads ) ?
-					this.earningsTable( this.state.earnings.wordads, this.translate( 'Earnings History' ), 'wordads' ) :
+					this.earningsTable( this.state.earnings.wordads, translate( 'Earnings History' ), 'wordads' ) :
 					null
 				}
 				{ this.state.earnings && this.checkSize( this.state.earnings.sponsored ) ?
-					this.earningsTable( this.state.earnings.sponsored, this.translate( 'Sponsored Content History' ), 'sponsored' ) :
+					this.earningsTable( this.state.earnings.sponsored, translate( 'Sponsored Content History' ), 'sponsored' ) :
 					null
 				}
 				{ this.state.earnings && this.checkSize( this.state.earnings.adjustment ) ?
-					this.earningsTable( this.state.earnings.adjustment, this.translate( 'Adjustments History' ), 'adjustment' ) :
+					this.earningsTable( this.state.earnings.adjustment, translate( 'Adjustments History' ), 'adjustment' ) :
 					null
 				}
 			</div>
@@ -325,4 +333,4 @@ class AdsFormEarnings extends Component {
 	}
 }
 
-export default AdsFormEarnings;
+export default localize( AdsFormEarnings );
