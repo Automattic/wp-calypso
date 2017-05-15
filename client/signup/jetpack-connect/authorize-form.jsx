a77be07f78762262e8367f96daf9436f051b1aa3
Jon Surrell <jon.surrell@automattic.com>
2017-05-15 18:45:16 +0200

    Remove redundant bindActionCreators

--- client/signup/jetpack-connect/authorize-form.jsx
+++ client/signup/jetpack-connect/authorize-form.jsx
@@ -3,7 +3,6 @@
  */
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { localize } from 'i18n-calypso';
 import cookie from 'cookie';
 
@@ -172,7 +171,7 @@ export default connect(
 			jetpackSSOSessions: getSSOSessions( state ),
 		};
 	},
-	dispatch => bindActionCreators( {
+	{
 		authorize,
 		createAccount,
 		goBackToWpAdmin,
@@ -180,5 +179,5 @@ export default connect(
 		recordTracksEvent,
 		requestSites,
 		retryAuth,
-	}, dispatch )
+	}
 )( localize( JetpackConnectAuthorizeForm ) );
