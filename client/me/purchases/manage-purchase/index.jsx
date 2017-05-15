ab1bb0a0efada4b650c295c3e16320b605f1334a
Kelly Dwan <kelly.dwan@gmail.com>
2017-05-15 10:18:16 -0400

    Purchases: Remove the v2 feature flag

--- client/me/purchases/manage-purchase/index.jsx
+++ client/me/purchases/manage-purchase/index.jsx
@@ -180,10 +180,6 @@ class ManagePurchase extends Component {
 	}
 
 	renderPlanDetails() {
-		if ( ! config.isEnabled( 'me/purchases-v2' ) ) {
-			return null;
-		}
-
 		return (
 			<PurchasePlanDetails
 				selectedSite={ this.props.selectedSite }
