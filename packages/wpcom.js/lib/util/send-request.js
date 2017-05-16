9b8e2508251f5fdf2c6f507457b25680f2f40204
James Nylen <jnylen@gmail.com>
2017-05-16 10:31:20 -0400

    Merge pull request #213 from bperson/update/send-request-headers
    Send Request: Forward response headers with the response results

--- packages/wpcom.js/lib/util/send-request.js
+++ packages/wpcom.js/lib/util/send-request.js
@@ -78,9 +78,9 @@ export default function sendRequest( params, query, body, fn ) {
 	// if callback is provided, behave traditionally
 	if ( 'function' === typeof fn ) {
 		// request method
-		return this.request( params, function( err, res ) {
+		return this.request( params, function( err, res, headers ) {
 			debug_res( res );
-			fn( err, res );
+			fn( err, res, headers );
 		} );
 	}
 
