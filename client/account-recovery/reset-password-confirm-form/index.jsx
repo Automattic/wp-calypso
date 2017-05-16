752397769344c2983789b8e984cdfc747332f3a9
James Tien <southp0105@gmail.com>
2017-05-16 10:48:18 +0800

    Account Recovery: Redux plumbing for <ResetPasswordConfirmForm /> (#13364)
    * Add a substate tree `resetPassword` corresponding to
    `ACCOUNT_RECOVERY_RESET_PASSWORD_REQUEST{_SUCCESS|_ERROR}` actions, and
    the related selectors.
    
    * Finish the redux state plumbing for <ResetPasswordConfirm />
    
    * Add test suites for the newly-added selectors.
    
    * Add test suites for the substate tree, resetPassword.
    
    * Remove a debugging function that got committed accidentally.
    
    * Pipe in the react state value into `FormPasswordInput`
    
    * `accout-recovery/reset` data layer handler shouldn't block the action
    from passing down the redux data flow.
    
    * Remove the unnecessary `primary`
    
    * Pass the error object to the AccountRecoveryErrorMessage component
    
    * Show the error message from the backend directly in the case of
    `RestBadPasswordError`
    
    * Update the error message shown on receiving `RestBadPassword`.

--- client/account-recovery/reset-password-confirm-form/index.jsx
+++ client/account-recovery/reset-password-confirm-form/index.jsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import { identity } from 'lodash';
 
@@ -9,18 +10,55 @@ import { identity } from 'lodash';
  * Internal dependencies
  */
 import Card from 'components/card';
+import ErrorMessage from 'account-recovery/components/account-recovery-error-message';
 import FormPasswordInput from 'components/forms/form-password-input';
 import FormLabel from 'components/forms/form-label';
 import FormButton from 'components/forms/form-button';
 import { STRONG_PASSWORD } from 'lib/url/support';
+import {
+	getAccountRecoveryResetUserData,
+	getAccountRecoveryResetSelectedMethod,
+	getAccountRecoveryValidationKey,
+	getAccountRecoveryResetPasswordError,
+	isRequestingResetPassword,
+} from 'state/selectors';
+import { requestResetPassword } from 'state/account-recovery/reset/actions';
 
 class ResetPasswordConfirmForm extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			newPassword: '',
+		};
+	}
+
 	submitNewPassword = ( event ) => {
+		const {
+			userData,
+			selectedMethod,
+			validationKey,
+		} = this.props;
+
+		const { newPassword } = this.state;
+
+		this.props.requestResetPassword( userData, selectedMethod, validationKey, newPassword );
+
 		event.preventDefault();
 	}
 
+	updateNewPassword = ( event ) => {
+		this.setState( { newPassword: event.target.value } );
+	}
+
 	render() {
-		const { translate } = this.props;
+		const {
+			translate,
+			isRequesting,
+			error,
+		} = this.props;
+
+		const { newPassword } = this.state;
 
 		return (
 			<Card>
@@ -29,8 +67,18 @@ class ResetPasswordConfirmForm extends Component {
 					<FormLabel className="reset-password-confirm-form__text-input-label" htmlFor="password">
 						{ translate( 'New password' ) }
 					</FormLabel>
-					<FormPasswordInput className="reset-password-confirm-form__password-input-field" id="password" autoFocus />
-					<FormButton className="reset-password-confirm-form__button generate-password-button" isPrimary={ false }>
+					<FormPasswordInput
+						className="reset-password-confirm-form__password-input-field"
+						id="password"
+						onChange={ this.updateNewPassword }
+						value={ newPassword }
+						autoFocus
+					/>
+					<FormButton
+						className="reset-password-confirm-form__button generate-password-button"
+						type="button"
+						isPrimary={ false }
+					>
 						{ translate( 'Generate strong password' ) }
 					</FormButton>
 					<p className="reset-password-confirm-form__description">
@@ -48,7 +96,12 @@ class ResetPasswordConfirmForm extends Component {
 							}
 						) }
 					</p>
-					<FormButton className="reset-password-confirm-form__button submit" type="submit">
+					{ error && <ErrorMessage error={ error } /> }
+					<FormButton
+						className="reset-password-confirm-form__button submit"
+						type="submit"
+						disabled={ isRequesting || ! newPassword }
+					>
 						{ translate( 'Reset Password' ) }
 					</FormButton>
 				</form>
@@ -61,4 +114,15 @@ ResetPasswordConfirmForm.defaultProps = {
 	translate: identity,
 };
 
-export default localize( ResetPasswordConfirmForm );
+export default connect(
+	( state ) => ( {
+		userData: getAccountRecoveryResetUserData( state ),
+		selectedMethod: getAccountRecoveryResetSelectedMethod( state ),
+		validationKey: getAccountRecoveryValidationKey( state ),
+		isRequesting: isRequestingResetPassword( state ),
+		error: getAccountRecoveryResetPasswordError( state ),
+	} ),
+	{
+		requestResetPassword,
+	}
+)( localize( ResetPasswordConfirmForm ) );
