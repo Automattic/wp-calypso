0e1f6bbfaf07f173bebf070ea30f44cbcecebe23
bluefuton <chris@bluefuton.com>
2017-05-17 18:25:28 +0200

    Use to.be.true/to.be.false in tests

--- client/reader/lib/site-description-blacklist/test/index.js
+++ client/reader/lib/site-description-blacklist/test/index.js
@@ -11,11 +11,11 @@ import { isSiteDescriptionBlacklisted } from '../';
 describe( 'isSiteDescriptionBlacklisted', () => {
 	it( 'should return true if a site description is blacklisted', function() {
 		const blackListedDescription = 'Just another WordPress site';
-		expect( isSiteDescriptionBlacklisted( blackListedDescription ) ).to.equal( true );
+		expect( isSiteDescriptionBlacklisted( blackListedDescription ) ).to.be.true;
 	} );
 
 	it( 'should return false if a site description is not blacklisted', function() {
 		const unBlackListedDescription = 'My site is marvellous';
-		expect( isSiteDescriptionBlacklisted( unBlackListedDescription ) ).to.equal( false );
+		expect( isSiteDescriptionBlacklisted( unBlackListedDescription ) ).to.be.false;
 	} );
 } );
