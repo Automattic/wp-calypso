2f37d763bb8df8b104a8cbfa231daf33f6baeebe
bluefuton <chris@bluefuton.com>
2017-05-17 14:00:50 +0200

    Separate tests

--- client/reader/lib/site-description-blacklist/test/index.js
+++ client/reader/lib/site-description-blacklist/test/index.js
@@ -9,10 +9,13 @@ import { expect } from 'chai';
 import { isSiteDescriptionBlacklisted } from '../';
 
 describe( 'isSiteDescriptionBlacklisted', () => {
-	it( 'should return null if a site description is blacklisted', function() {
+	it( 'should return true if a site description is blacklisted', function() {
 		const blackListedDescription = 'Just another WordPress site';
-		const unBlackListedDescription = 'My site is marvellous';
 		expect( isSiteDescriptionBlacklisted( blackListedDescription ) ).to.equal( true );
+	} );
+
+	it( 'should return false if a site description is not blacklisted', function() {
+		const unBlackListedDescription = 'My site is marvellous';
 		expect( isSiteDescriptionBlacklisted( unBlackListedDescription ) ).to.equal( false );
 	} );
 } );
