b4d9f9fd287ce79e3f2648e8ec06d54748fda0c8
Kevin Killingsworth <kevin@redfenix.com>
2017-05-17 01:30:41 -0500

    WooCommerce: Move wc-api errors to site/status
    This moves the wc-api error state data to underneath state/site/status
    for an example of where to keep general top-level status information.

--- client/extensions/woocommerce/state/site/status/reducer.js
+++ client/extensions/woocommerce/state/site/status/reducer.js
@@ -2,10 +2,6 @@
  * Internal dependencies
  */
 import { combineReducers } from 'state/utils';
-
-/**
- * Internal dependencies
- */
 import wcApi from './wc-api/reducer';
 
 export default combineReducers( {
