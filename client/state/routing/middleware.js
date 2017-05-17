4532c3eaf3bff9d60f95c34ccb287daa77cc523f
Miguel Fonseca <miguelcsf@gmail.com>
2017-05-17 17:07:37 -0400

    restoreLastLocation: use localforage

--- client/state/routing/middleware.js
+++ client/state/routing/middleware.js
@@ -3,32 +3,33 @@
  */
 import debugFactory from 'debug';
 import page from 'page';
-import store from 'store';
 
 /**
  * Internal dependencies
  */
+import localforage from 'lib/localforage';
 import { ROUTE_SET } from 'state/action-types';
 
 const debug = debugFactory( 'calypso:restore-last-location' );
+const key = 'last_path';
 
 let hasInitialized = false;
 
 export const restoreLastLocation = () => ( next ) => ( action ) => {
 	if ( action.type === ROUTE_SET && action.path ) {
-		const lastPath = store.get( 'last_path' );
+		localforage.getItem( key ).then( ( lastPath ) => {
+			if ( ! hasInitialized && lastPath && lastPath !== '/' && action.path === '/' ) {
+				debug( 'redir to', lastPath );
+				page( lastPath );
+			} else if ( action.path !== lastPath ) {
+				debug( 'saving', action.path );
+				localforage.setItem( key, action.path );
+			}
 
-		if ( ! hasInitialized && lastPath && lastPath !== '/' && action.path === '/' ) {
-			debug( 'redir to', action.path );
-			page( lastPath );
-		} else {
-			debug( 'saving', action.path );
-			store.set( 'last_path', action.path );
-		}
-
-		if ( ! hasInitialized ) {
-			hasInitialized = true;
-		}
+			if ( ! hasInitialized ) {
+				hasInitialized = true;
+			}
+		} );
 	}
 
 	return next( action );
