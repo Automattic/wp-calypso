b80ac147920650a8557f7b39861df30fdcc41cb7
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-05-17 12:57:22 -0300

    Comments: adds parent-child relationship to get oldest and newest comment test cases

--- client/state/comments/test/selectors.js
+++ client/state/comments/test/selectors.js
@@ -17,7 +17,9 @@ const state = {
 		items: {
 			'1-1': [
 					{ ID: 1, parent: false, date: '2016-01-31T10:07:18-08:00', i_like: true, like_count: 5 },
-					{ ID: 2, parent: false, date: '2016-01-29T10:07:18-08:00', i_like: false, like_count: 456 }
+					{ ID: 2, parent: false, date: '2016-01-29T10:07:18-08:00', i_like: false, like_count: 456 },
+					{ ID: 3, parent: { ID: 1 }, date: '2017-01-31T10:07:18-08:00', i_like: false, like_count: 0 },
+					{ ID: 4, parent: { ID: 2 }, date: '2015-01-29T10:07:18-08:00', i_like: false, like_count: 0 }
 			]
 		}
 	}
@@ -25,7 +27,7 @@ const state = {
 
 describe( 'selectors', () => {
 	describe( '#getPostMostRecentCommentDate()', () => {
-		it( 'should get most recent date', () => {
+		it( 'should get most recent date, ignoring children', () => {
 			const res = getPostMostRecentCommentDate( state, 1, 1 );
 
 			expect( res ).to.be.eql( new Date( '2016-01-31T10:07:18-08:00' ) );
@@ -41,7 +43,7 @@ describe( 'selectors', () => {
 	} ); // end of getPostMostRecentCommentDate
 
 	describe( '#getPostOldestCommentDate()', () => {
-		it( 'should get earliest date', () => {
+		it( 'should get earliest date, ignoring children', () => {
 			const res = getPostOldestCommentDate( state, 1, 1 );
 
 			expect( res ).to.be.eql( new Date( '2016-01-29T10:07:18-08:00' ) );
