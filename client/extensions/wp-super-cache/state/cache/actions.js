c9026749dff89ba587e058dce3b6fcfc10d99c78
Donna Peplinskie <donnapep@gmail.com>
2017-05-18 15:16:14 -0400

    Extensions: WPSC - Move cache test functionality from HOC to EasyTab component (#13923)
    * Move cache test functionality from HOC to EasyTab component
    
    * Handle notices in the action instead of in the component
    
    * Remove defaultProps for props that have defaults returned by selectors
    
    * Store a boolean per siteId to indicate test status
    
    Remove obsolete selectors
    
    * Use success action for delivering cache test results
    
    This means the WP_SUPER_CACHE_RECEIVE_TEST_CACHE_RESULTS can safely be
    removed.
    
    * In getCacheState() selector, directly access substate

--- client/extensions/wp-super-cache/state/cache/actions.js
+++ client/extensions/wp-super-cache/state/cache/actions.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { translate } from 'i18n-calypso';
+
+/**
  * Internal dependencies
  */
 import wp from 'lib/wp';
@@ -6,49 +11,41 @@ import {
 	WP_SUPER_CACHE_DELETE_CACHE,
 	WP_SUPER_CACHE_DELETE_CACHE_FAILURE,
 	WP_SUPER_CACHE_DELETE_CACHE_SUCCESS,
-	WP_SUPER_CACHE_RECEIVE_TEST_CACHE_RESULTS,
 	WP_SUPER_CACHE_TEST_CACHE,
 	WP_SUPER_CACHE_TEST_CACHE_FAILURE,
 	WP_SUPER_CACHE_TEST_CACHE_SUCCESS,
 } from '../action-types';
-
-/**
- * Returns an action object to be used in signalling that cache test results have been received.
- *
- * @param  {Number} siteId Site ID
- * @param  {Object} results Cache test results object
- * @return {Object} Action object
- */
-export const receiveResults = ( siteId, results ) => ( { type: WP_SUPER_CACHE_RECEIVE_TEST_CACHE_RESULTS, siteId, results } );
+import { errorNotice, removeNotice, successNotice } from 'state/notices/actions';
 
 /*
  * Tests the cache for a site.
  *
  * @param  {Number} siteId Site ID
- * @returns {Function} Action thunk that tests the cache ror a given site
+ * @param  {String} siteTitle Site title
+ * @param  {Boolean} httpOnly Whether to send a non-secure request for the homepage
+ * @returns {Function} Action thunk that tests the cache for a given site
  */
-export const testCache = ( siteId, httpOnly ) => {
+export const testCache = ( siteId, siteTitle, httpOnly ) => {
 	return ( dispatch ) => {
-		dispatch( {
-			type: WP_SUPER_CACHE_TEST_CACHE,
-			siteId,
-		} );
+		dispatch( removeNotice( 'wpsc-test-cache' ) );
+		dispatch( { type: WP_SUPER_CACHE_TEST_CACHE, siteId } );
 
 		return wp.req.post(
 			{ path: `/jetpack-blogs/${ siteId }/rest-api/` },
 			{ path: '/wp-super-cache/v1/cache/test', body: JSON.stringify( { httponly: httpOnly } ), json: true } )
 			.then( ( { data } ) => {
-				dispatch( receiveResults( siteId, data ) );
-				dispatch( {
-					type: WP_SUPER_CACHE_TEST_CACHE_SUCCESS,
-					siteId,
-				} );
+				dispatch( successNotice(
+					translate( 'Cache test completed successfully on %(siteTitle)s.', { args: { siteTitle } } ),
+					{ id: 'wpsc-test-cache' }
+				) );
+				dispatch( { type: WP_SUPER_CACHE_TEST_CACHE_SUCCESS, siteId, data } );
 			} )
 			.catch( () => {
-				dispatch( {
-					type: WP_SUPER_CACHE_TEST_CACHE_FAILURE,
-					siteId,
-				} );
+				dispatch( errorNotice(
+					translate( 'There was a problem testing the cache. Please try again.' ),
+					{ id: 'wpsc-test-cache' }
+				) );
+				dispatch( { type: WP_SUPER_CACHE_TEST_CACHE_FAILURE, siteId } );
 			} );
 	};
 };
