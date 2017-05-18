ed636d57d29f1e0ab5d81a2ff2e1c07fc60edb4f
Jake F - @samouri <samouri@users.noreply.github.com>
2017-05-17 21:11:10 -0400

    hide the globe

--- client/blocks/reader-feed-header/index.jsx
+++ client/blocks/reader-feed-header/index.jsx
@@ -18,11 +18,11 @@ import SiteIcon from 'blocks/site-icon';
 
 const getBadgeForSite = site => {
 	/* eslint-disable wpcalypso/jsx-gridicon-size */
-	if ( site.is_private ) {
+	if ( site && site.is_private ) {
 		return <Gridicon icon="lock" size={ 14 } />;
-	} else if ( site.options && site.options.is_redirect ) {
+	} else if ( site && site.options && site.options.is_redirect ) {
 		return <Gridicon icon="block" size={ 14 } />;
-	} else if ( site.options && site.options.is_domain_only ) {
+	} else if ( site && site.options && site.options.is_domain_only ) {
 		return <Gridicon icon="domains" size={ 14 } />;
 	}
 	return null;
