9326178cc587025427779352a1c67773150e98ce
Dennis Snell <dennis.snell@automattic.com>
2017-05-18 15:13:16 -0400

    Updates

--- client/state/delayed-dispatch/actions.js
+++ client/state/delayed-dispatch/actions.js
@@ -3,12 +3,20 @@
  */
 import { DELAYED_DISPATCH } from '../action-types';
 
+/**
+ * Returns an action describing the intention to dispatch
+ * a given action after a given delay
+ *
+ * @param {Number} delay waiting time in ms before dispatching action
+ * @param {Object} payload action to dispatch
+ * @param {String} [message=''] optional message for debugging purposes
+ * @returns {Object} wrapped action indicating deferred dispatch intent
+ */
 export const delayDispatch = ( delay, payload, message = '' ) => Object.assign(
 	{
 		type: DELAYED_DISPATCH,
 		payload,
 		delay,
-		dispatchBy: Date.now() + delay,
 	},
 	message && { message }
 );
--- client/state/delayed-dispatch/test/actions.js
+++ client/state/delayed-dispatch/test/actions.js
@@ -6,24 +6,12 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import { useFakeTimers } from 'test/helpers/use-sinon';
 import { delayDispatch } from '../actions';
 
 const action = { type: 'SPIN' };
 const delay = 1337;
 
 describe( '#delayDispatch', () => {
-	// Initializes clock to 0
-	useFakeTimers();
-
-	it( 'should attach a `dispatchBy` value', () => {
-		expect( delayDispatch( delay, action ) ).to.have.property( 'dispatchBy' );
-	} );
-
-	it( 'should set `dispatchBy` to appropriate value', () => {
-		expect( delayDispatch( delay, action ).dispatchBy ).to.equal( delay );
-	} );
-
 	it( 'should include the optional message if given', () => {
 		expect( delayDispatch( delay, action, 'waiting…' ) ).to.have.property( 'message', 'waiting…' );
 	} );
