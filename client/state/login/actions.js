442bf54a4e8f30257265d03f8df0a53a36b43eec
Ben Dwyer <ben@scruffian.com>
2017-05-18 16:28:27 +0100

    Update the error messages inline with the recommendations in 14090

--- client/state/login/actions.js
+++ client/state/login/actions.js
@@ -24,14 +24,16 @@ import {
 } from 'state/action-types';
 
 const loginErrorMessages = {
-	empty_password: translate( 'Enter your password.' ),
-	empty_username: translate( 'Enter your username or email address.' ),
-	incorrect_password: translate( "That's not the right password." ),
-	invalid_two_step_code: translate( "That's not the right code." ),
-	invalid_email: translate( 'Invalid email address.' ),
-	invalid_username: translate( 'Invalid username' ),
-	unknown: translate( 'Invalid username or password.' ),
-	account_unactivated: translate( 'This account has not been activated. Please check your email for an activation link.' ),
+	empty_password: translate( 'Please be sure to enter your password.' ),
+	empty_username: translate( 'Please enter a username or email address.' ),
+	incorrect_password: translate( "Oops, looks like that's not the right password. Please try again!" ),
+	invalid_two_step_code: translate( "Hmm, that's not a valid verification code. Please double-check your app and try again." ),
+	invalid_email: translate( "Oops, looks like that's not the right address. Please try again!" ),
+	invalid_username: translate( "We don't seem to have an account with that name. Double-check the spelling and try again!" ),
+	unknown: translate( "Hmm, we can't find a WordPress.com account with this username and password combo. " +
+		'Please double check your information and try again.' ),
+	account_unactivated: translate( "This account hasn't been activated yet — check your email for a message from " +
+		"WordPress.com and click the activation link. You'll be able to log in after that." ),
 	sms_recovery_code_throttled: translate( 'You can only request a recovery code via SMS once per minute. Please wait and try again.' ),
 };
 
