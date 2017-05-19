f16fdf6ba6a6f873a05ca0b8aedd781531fd4e1f
Donna Peplinskie <donnapep@gmail.com>
2017-05-19 13:16:48 -0400

    Extensions: WPSC - Add support for deleting a cached file (#13818)
    * Add actions, reducers and selectors for deleting a cached file
    
    * Implement deleting a cached file
    
    The deleted file is removed from the list if the delete was successful.
    Otherwise, if an error was encountered, a notice is displayed.
    
    * Pass additional props to CacheStats component
    
    These additional props (isCached, isSupercache) get passed to the
    deleteFile action, which in turn passes them on to the reducer so that
    the correct elements of the state tree can be removed / updated.
    
    * Add tests for Redux state
    
    * Remove defaultProps for hasError and isDeleting
    
    Default values are returned by the selectors, so no need to explicitly
    set them again in defaultProps.
    
    * Handle notices in deleteFile action instead of in the component
    
    * Store a boolean per siteId to indicate delete status
    
    * Remove separate function for rendering cache stats and render inline instead
    
    * Use success action for indicating that a cached file has been removed
    
    This means the WP_SUPER_CACHE_REMOVE_FILE action type can safely be
    removed.
    
    * Make stats items reducer work with object-based API response
    
    * ES2016 magic
    
    See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Computed_object_property_names_and_destructuring
    
    * Fix indentation

--- client/extensions/wp-super-cache/state/stats/reducer.js
+++ client/extensions/wp-super-cache/state/stats/reducer.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { combineReducers } from 'redux';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies
@@ -9,6 +10,9 @@ import { combineReducers } from 'redux';
 import { createReducer } from 'state/utils';
 import { statsSchema } from './schema';
 import {
+	WP_SUPER_CACHE_DELETE_FILE,
+	WP_SUPER_CACHE_DELETE_FILE_FAILURE,
+	WP_SUPER_CACHE_DELETE_FILE_SUCCESS,
 	WP_SUPER_CACHE_RECEIVE_STATS,
 	WP_SUPER_CACHE_GENERATE_STATS,
 	WP_SUPER_CACHE_GENERATE_STATS_FAILURE,
@@ -48,6 +52,20 @@ const generateStatus = createReducer( {}, {
 } );
 
 /**
+ * Returns the updated deleting state after an action has been dispatched.
+ * Deleting state tracks whether a cached file for a site is currently being deleted.
+ *
+ * @param  {Object} state Current deleting state
+ * @param  {Object} action Action object
+ * @return {Object} Updated deleting state
+ */
+const deleting = createReducer( {}, {
+	[ WP_SUPER_CACHE_DELETE_FILE ]: ( state, { siteId } ) => ( { ...state, [ siteId ]: true } ),
+	[ WP_SUPER_CACHE_DELETE_FILE_FAILURE ]: ( state, { siteId } ) => ( { ...state, [ siteId ]: false } ),
+	[ WP_SUPER_CACHE_DELETE_FILE_SUCCESS ]: ( state, { siteId } ) => ( { ...state, [ siteId ]: false } )
+} );
+
+/**
  * Tracks the stats for a particular site.
  *
  * @param  {Object} state Current stats
@@ -56,9 +74,30 @@ const generateStatus = createReducer( {}, {
  */
 const items = createReducer( {}, {
 	[ WP_SUPER_CACHE_RECEIVE_STATS ]: ( state, { siteId, stats } ) => ( { ...state, [ siteId ]: stats } ),
+	[ WP_SUPER_CACHE_DELETE_FILE_SUCCESS ]: ( state, { siteId, url, isSupercache, isCached } ) => {
+		const cacheType = isSupercache ? 'supercache' : 'wpcache';
+		const listType = isCached ? 'cached_list' : 'expired_list';
+		const countType = isCached ? 'cached' : 'expired';
+		// Store the object whose key is given by `url` in the `file` var, and all other files in `remainingFiles`.
+		const { [ url ]: file, ...remainingFiles } = state[ siteId ][ cacheType ][ listType ];
+		const fileCount = get( file, 'files', 0 );
+
+		return ( {
+			...state,
+			[ siteId ]: {
+				...state[ siteId ],
+				[ cacheType ]: {
+					...state[ siteId ][ cacheType ],
+					[ countType ]: state[ siteId ][ cacheType ][ countType ] - fileCount,
+					[ listType ]: remainingFiles,
+				},
+			}
+		} );
+	},
 }, statsSchema );
 
 export default combineReducers( {
+	deleting,
 	generateStatus,
 	items,
 } );
