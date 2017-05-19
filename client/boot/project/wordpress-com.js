c510bc17365f9ba037a64f4b674596eafe0d253a
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-05-19 13:34:17 +0200

    Framework: Ensure make analyze-bundles work properly without special node flags

--- client/boot/project/wordpress-com.js
+++ client/boot/project/wordpress-com.js
@@ -200,6 +200,8 @@ export function setupMiddlewares( currentUser, reduxStore ) {
 		} );
 	}
 
+	require( 'my-sites/controller' );
+
 	if ( config.isEnabled( 'olark' ) ) {
 		asyncRequire( 'lib/olark', olark => olark.initialize( reduxStore.dispatch ) );
 	}
