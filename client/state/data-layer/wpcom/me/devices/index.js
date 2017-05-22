b33c87379b3f8f298803234e115fe01ba95232be
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-05-22 19:13:14 -0300

    Devices: simplifies the data layer and adds jsdoc comments

--- client/state/data-layer/wpcom/me/devices/index.js
+++ client/state/data-layer/wpcom/me/devices/index.js
@@ -15,23 +15,44 @@ import {
 import { userDevicesAdd } from 'state/user-devices/actions';
 import { errorNotice } from 'state/notices/actions';
 
-export const requestUserDevices = function( { dispatch } ) {
-	dispatch( http( {
-		apiVersion: '1.1',
-		method: 'GET',
-		path: '/notifications/devices',
-	} ) );
-};
+const devicesFromApi = devices => keyBy( devices.map(
+	( { device_id, device_name } ) => ( { id: device_id, name: device_name } )
+), 'id' );
 
-export const handleSuccess = ( { dispatch }, action, next, devices ) => {
-	dispatch( userDevicesAdd( {
-		devices: keyBy( devices.map( ( { device_id, device_name } ) => ( { id: device_id, name: device_name } ) ), 'id' )
-	} ) );
-};
+/**
+ * Dispatches a request to fetch all available WordPress.com plans
+ *
+ * @param   {Function} dispatch Redux dispatcher
+ * @returns {Object} dispatched http action
+ */
+export const requestUserDevices = ( { dispatch } ) => dispatch( http( {
+	apiVersion: '1.1',
+	method: 'GET',
+	path: '/notifications/devices',
+} ) );
 
-export const handleError = ( { dispatch } ) => {
-	dispatch( errorNotice( translate( 'We couldn\'t load your devices, please try again.' ) ) );
-};
+/**
+ * Dispatches a user devices add action then the request for user devices succeeded.
+ *
+ * @param   {Function} dispatch Redux dispatcher
+ * @param   {Object}   action   Redux action
+ * @param   {Function} next     data-layer-bypassing dispatcher
+ * @param   {Array}    devices  array of raw device data returned from the endpoint
+ * @returns {Object}            disparched user devices add action
+ */
+export const handleSuccess = ( { dispatch }, action, next, devices ) => dispatch( userDevicesAdd( {
+	devices: devicesFromApi( devices )
+} ) );
+
+/**
+ * Dispatches a error notice action when the request for user devices fails
+ *
+ * @param   {Function} dispatch Redux dispatcher
+ * @returns {Object}            dispatched error notice action
+ */
+export const handleError = ( { dispatch } ) => dispatch(
+	errorNotice( translate( 'We couldn\'t load your devices, please try again.' ) )
+);
 
 export default {
 	[ USER_DEVICES_REQUEST ]: [ dispatchRequest( requestUserDevices, handleSuccess, handleError ) ],
