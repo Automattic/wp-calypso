65b3b7d75516dd4bca8527b535060abd8e6379f1
Del Putnam <del@putnams.net>
2017-05-22 18:15:32 -0400

    WIP - broke primary component into multiple sub-components

--- client/my-sites/upgrades/domain-management/domain-connect/domain-connect-authorize.jsx
+++ client/my-sites/upgrades/domain-management/domain-connect/domain-connect-authorize.jsx
@@ -2,54 +2,62 @@
  * External dependencies
  */
 import React, { Component } from 'react';
-import Gridicon from 'gridicons';
 import { translate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-import Button from 'components/button';
 import Card from 'components/card/compact';
 import Main from 'components/main';
 import Notice from 'components/notice';
 import upgradesActions from 'lib/upgrades/actions';
-
-const actionType = {
-	READY_TO_SUBMIT: 'action-ready-to-submit',
-	SUBMITTING: 'action-submitting',
-	CLOSE: 'action-close'
-};
-
-const noticeType = {
-	ERROR: 'notice-error',
-	SUCCESS: 'notice-success'
-};
+import DomainConnectAuthorizeDescription from './domain-connect-authorize-description';
+import DomainConnectAuthorizeConflicts from './domain-connect-authorize-conflicts';
+import DomainConnectAuthorizeFooter from './domain-connect-authorize-footer';
+import { actionType, noticeType } from './constants';
 
 class DomainConnectAuthorize extends Component {
 	constructor( props ) {
 		super( props );
 		this.state = {
-			action: actionType.READY_TO_SUBMIT,
-			notice: null,
-			dnsTemplateConflicts: null,
+			action: null,
+			noticeType: null,
+			dnsTemplateConflictsRetrieved: false
 		};
 	}
 
 	componentDidMount() {
-		const { provider_id, params } = this.props,
+		const { providerId, params } = this.props,
 			{ domain } = params;
 
-		upgradesActions.getDnsTemplateConflicts( domain, provider_id, params, ( error, data ) => {
+		upgradesActions.getDnsTemplateConflicts( domain, providerId, params, ( error, data ) => {
 			if ( ! error ) {
 				this.setState( {
 					dnsTemplateConflicts: data.records,
+					dnsTemplateError: false,
+					action: actionType.READY_TO_SUBMIT
+				} );
+			} else {
+				const errorMessage = error.message ||
+					translate( 'We aren\'t able to set up the service with the information given. Please check ' +
+						'with your service provider to make sure they provided all the correct data.' );
+
+				this.setState( {
+					noticeType: noticeType.ERROR,
+					noticeMessage: errorMessage,
+					dnsTemplateError: true,
+					action: actionType.CLOSE
 				} );
 			}
+
+			this.setState( {
+				dnsTemplateConflictsRetrieved: true
+			} );
 		} );
 	}
 
 	handleClickConfirm = () => {
-		const { provider_id, params } = this.props,
+		const { providerId, params } = this.props,
 			{ domain } = params;
 
 		this.setState( {
@@ -57,151 +65,47 @@ class DomainConnectAuthorize extends Component {
 			notice: null
 		} );
 
-		upgradesActions.applyDnsTemplate( domain, provider_id, params, ( error ) => {
+		upgradesActions.applyDnsTemplate( domain, providerId, params, ( error ) => {
 			if ( error ) {
+
+				const errorMessage = error.message ||
+					translate( 'We weren\'t able to add the DNS records needed for this service. Please try again.' );
+
 				this.setState( {
 					action: actionType.READY_TO_SUBMIT,
-					notice: noticeType.ERROR,
-					errorMessage: error.message
+					noticeType: noticeType.ERROR,
+					noticeMessage: errorMessage
 				} );
 			} else {
 				this.setState( {
 					action: actionType.CLOSE,
-					notice: noticeType.SUCCESS
+					noticeType: noticeType.SUCCESS,
+					noticeMessage: translate( 'Horray! Your new service is now all set up.' )
 				} );
 			}
 		} );
 	}
 
-	handleClickCancel = () => {
+	handleClickClose = () => {
 		window.close();
 	}
 
-	renderConflict = () => {
-		if ( null !== this.state.dnsTemplateConflicts ) {
+	renderNotice = () => {
+		if ( this.state.noticeType ) {
 			return (
-				<div>
-					<p>
-						{ translate( 'The following DNS records will be replaced when you make this change:' ) }
-					</p>
-					<div className="domain-connect__dns-list">
-						<ul>
-							{
-								this.state.dnsTemplateConflicts.map( ( record, index ) => {
-									return (
-										<li key={ index }>
-											<div className="domain-connect__dns-list-type">
-												<label>{ record.type }</label>
-											</div>
-											<div className="domain-connect__dns-list-info">
-												<strong>{ record.name }</strong>
-												<em>{ record.data }</em>
-											</div>
-										</li>
-									);
-								} )
-							}
-						</ul>
-					</div>
-					<p>
-						{
-							translate( 'The services that these records were used for may no longer work if they ' +
-								'are removed. If you are trying to switch from one service provider to another ' +
-								'this is probably what you want to do.' )
-						}
-					</p>
-				</div>
+				<Notice
+					status={ this.state.noticeType }
+					showDismiss={ false }
+					text={ this.state.noticeMessage }>
+				</Notice>
 			);
 		}
-	}
 
-	renderNoticeSuccess = () => {
-		return (
-			<div>
-				<Notice
-					status="is-success"
-					showDismiss={ false }
-					text={ translate( 'Horray! Your service is now all set up.' ) }>
-				</Notice>
-			</div>
-		);
-	}
-
-	renderNoticeError = () => {
-		return (
-			<div>
-				<Notice
-					status="is-error"
-					showDismiss={ false }
-					text={
-						this.state.errorMessage ||
-						translate( 'We weren\'t able to add the DNS records needed for this service. Please try again.' ) }>
-				</Notice>
-			</div>
-		);
-	}
-
-	renderNotice = () => {
-		switch ( this.state.notice ) {
-			case noticeType.SUCCESS:
-				return this.renderNoticeSuccess();
-			case noticeType.ERROR:
-				return this.renderNoticeError();
-		}
-	}
-
-	renderActionConfirmCancel = () => {
-		return (
-			<div>
-				<Button
-					icon
-					className="domain-connect__button"
-					primary
-					onClick={ this.handleClickConfirm }
-					busy={ actionType.READY_TO_SUBMIT !== this.state.action }
-					disabled={ actionType.READY_TO_SUBMIT !== this.state.action }>
-					<Gridicon icon="checkmark" /> { translate( 'Confirm' ) }
-				</Button>
-				<Button
-					icon
-					className="domain-connect__button"
-					onClick={ this.handleClickCancel }
-					busy={ actionType.READY_TO_SUBMIT !== this.state.action }
-					disabled={ actionType.READY_TO_SUBMIT !== this.state.action }>
-					<Gridicon icon="cross" /> { translate( 'Cancel' ) }
-				</Button>
-			</div>
-		);
-	}
-
-	renderActionClose = () => {
-		return (
-			<div>
-				<Button
-					className="domain-connect__button"
-					onClick={ this.handleClickCancel }>
-					{ translate( 'Close' ) }
-				</Button>
-			</div>
-		);
-	}
-
-	renderAction = () => {
-		switch ( this.state.action ) {
-			case actionType.READY_TO_SUBMIT:
-			case actionType.SUBMITTING:
-				return this.renderActionConfirmCancel();
-			case actionType.CLOSE:
-				return this.renderActionClose();
-		}
+		return null;
 	}
 
 	render() {
 		const { domain } = this.props.params;
-
-		// TODO: Get this string from somewhere...based on the template that is being used.
-		// This is just an example of what the description may look like. I don't like that
-		const description = 'make your domain work with the Google G Suite email service.';
 
 		return (
 			<Main className="domain-connect__main">
@@ -216,26 +120,18 @@ class DomainConnectAuthorize extends Component {
 							)
 						}
 					</h2>
-					<p>
-						{
-							translate( 'Howdy! It looks like you want to %(description)s ' +
-								'This means that we\'ll be adding some new DNS records for you.',
-								{
-									args: { description: description },
-									comment: '%(description)s is a brief phrase that summarizes the changes that will be made.'
-								}
-							)
-						}
-					</p>
-					{ this.renderConflict() }
-					<p>
-						{
-							translate( 'When you\'re ready to proceed, click Confirm. If this isn\'t what you meant to do,' +
-								'click Cancel and we won\'t make any changes.' )
-						}
-					</p>
+					<DomainConnectAuthorizeDescription
+						isPlaceholder={ ! this.state.dnsTemplateConflictsRetrieved }
+						providerId={ this.props.providerId } />
+					<DomainConnectAuthorizeConflicts
+						isPlaceholder={ ! this.state.dnsTemplateConflictsRetrieved }
+						conflictingRecords= { this.state.dnsTemplateConflicts } />
 					{ this.renderNotice() }
-					{ this.renderAction() }
+					<DomainConnectAuthorizeFooter
+						isPlaceholder={ ! this.state.dnsTemplateConflictsRetrieved }
+						showAction={ this.state.action }
+						onConfirm={ this.handleClickConfirm }
+						onClose={ this.handleClickClose } />
 				</Card>
 			</Main>
 		);
