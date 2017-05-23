b6f87b4f36da23db66180cec0cbbec0b22dc2aa8
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-05-22 23:54:27 -0300

    Devices: adds jsdocs to action creators

--- client/state/user-devices/actions.js
+++ client/state/user-devices/actions.js
@@ -6,6 +6,16 @@ import {
 	USER_DEVICES_ADD,
 } from 'state/action-types';
 
+/**
+ * Returns an action object to signal the request of the user's devices.
+ * @returns {Object} Action object
+ */
 export const requestUserDevices = () => ( { type: USER_DEVICES_REQUEST } );
 
+/**
+ * Returns an action object signalling the adittion of devices to the store.
+ *
+ * @param  {Object} devices Object containing one or more devices, keyed by id.
+ * @return {Object}         Action object
+ */
 export const userDevicesAdd = ( { devices } ) => ( { type: USER_DEVICES_ADD, devices } );
