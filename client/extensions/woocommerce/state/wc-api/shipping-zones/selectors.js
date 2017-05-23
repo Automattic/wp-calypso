06a9463913c50ca2bcee763f6ea6a61cfdf3056d
Daniel Rey López <daniel.rey.lopez@gmail.com>
2017-05-23 22:54:51 +0100

    Fetch Shipping zones from the merchant host (#14279)
    Implemented actions & reducers to fetch shipping zones from the server.

---
+++ client/extensions/woocommerce/state/wc-api/shipping-zones/selectors.js
@@ -0,0 +1,32 @@
+/**
+ * External dependencies
+ */
+import { get, isArray } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { LOADING } from './reducer';
+
+const getRawShippingZones = ( state, siteId ) => {
+	return get( state, [ 'extensions', 'woocommerce', 'wcApi', siteId, 'shippingZones' ] );
+};
+
+/**
+ * @param {Object} state Whole Redux state tree
+ * @param {Number} [siteId] Site ID to check. If not provided, the Site ID selected in the UI will be used
+ * @return {boolean} Whether the shipping zones list has been successfully loaded from the server
+ */
+export const areShippingZonesLoaded = ( state, siteId = getSelectedSiteId( state ) ) => {
+	return isArray( getRawShippingZones( state, siteId ) );
+};
+
+/**
+ * @param {Object} state Whole Redux state tree
+ * @param {Number} [siteId] Site ID to check. If not provided, the Site ID selected in the UI will be used
+ * @return {boolean} Whether the shipping zones list is currently being retrieved from the server
+ */
+export const areShippingZonesLoading = ( state, siteId = getSelectedSiteId( state ) ) => {
+	return LOADING === getRawShippingZones( state, siteId );
+};
