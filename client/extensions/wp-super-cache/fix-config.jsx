0604c7d2aae25c96a9e5d59f630a58a1a8857582
Donna Peplinskie <donnapep@gmail.com>
2017-05-23 10:48:42 -0400

    Implement restoring the settings to defaults

--- client/extensions/wp-super-cache/fix-config.jsx
+++ client/extensions/wp-super-cache/fix-config.jsx
@@ -2,7 +2,9 @@
  * External dependencies
  */
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
+import { flowRight } from 'lodash';
 
 /**
  * Internal dependencies
@@ -10,20 +12,32 @@ import { localize } from 'i18n-calypso';
 import Button from 'components/button';
 import Card from 'components/card';
 import SectionHeader from 'components/section-header';
+import { restoreSettings } from './state/settings/actions';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { isRestoringSettings } from './state/settings/selectors';
 
 class FixConfig extends Component {
 	static propTypes = {
+		isRestoring: PropTypes.bool.isRequired,
+		restoreSettings: PropTypes.func.isRequired,
+		siteId: PropTypes.number,
 		translate: PropTypes.func.isRequired,
 	};
 
+	restoreSettings = () => this.props.restoreSettings( this.props.siteId );
+
 	render() {
-		const { translate } = this.props;
+		const { isRestoring, translate } = this.props;
 
 		return (
 			<div>
 				<SectionHeader label={ translate( 'Fix Configuration' ) } />
 				<Card>
-					<Button compact>
+					<Button
+						compact
+						busy={ isRestoring }
+						disabled={ isRestoring }
+						onClick={ this.restoreSettings }>
 						{ translate( 'Restore Default Configuration' ) }
 					</Button>
 				</Card>
@@ -32,4 +46,20 @@ class FixConfig extends Component {
 	}
 }
 
-export default localize( FixConfig );
+const connectComponent = connect(
+	( state ) => {
+		const siteId = getSelectedSiteId( state );
+		const isRestoring = isRestoringSettings( state, siteId );
+
+		return {
+			isRestoring,
+			siteId,
+		};
+	},
+	{ restoreSettings }
+);
+
+export default flowRight(
+	connectComponent,
+	localize,
+)( FixConfig );
