3821c91f12da278e3c9a3230bdbc064043b4bdbc
Donna Peplinskie <donnapep@gmail.com>
2017-05-23 10:45:21 -0400

    Add actions, reducers and selectors for restoring default config
    Success notice is shown if the settings were restored successfully;
    error notice is shown if the settings could not be restored.

--- client/extensions/wp-super-cache/state/settings/actions.js
+++ client/extensions/wp-super-cache/state/settings/actions.js
@@ -1,4 +1,9 @@
 /**
+ * External Dependencies
+ */
+import { translate } from 'i18n-calypso';
+
+/**
  * Internal dependencies
  */
 import wp from 'lib/wp';
@@ -7,12 +12,17 @@ import {
 	WP_SUPER_CACHE_REQUEST_SETTINGS,
 	WP_SUPER_CACHE_REQUEST_SETTINGS_FAILURE,
 	WP_SUPER_CACHE_REQUEST_SETTINGS_SUCCESS,
+	WP_SUPER_CACHE_RESTORE_SETTINGS,
+	WP_SUPER_CACHE_RESTORE_SETTINGS_FAILURE,
+	WP_SUPER_CACHE_RESTORE_SETTINGS_SUCCESS,
 	WP_SUPER_CACHE_SAVE_SETTINGS,
 	WP_SUPER_CACHE_SAVE_SETTINGS_FAILURE,
 	WP_SUPER_CACHE_SAVE_SETTINGS_SUCCESS,
 	WP_SUPER_CACHE_UPDATE_SETTINGS,
 } from '../action-types';
 import { normalizeSettings, sanitizeSettings } from './utils';
+import { errorNotice, removeNotice, successNotice } from 'state/notices/actions';
+import { getSiteTitle } from 'state/sites/selectors';
 
 /**
  * Returns an action object to be used in signalling that settings have been received.
@@ -93,6 +103,40 @@ export const saveSettings = ( siteId, settings ) => {
 					siteId,
 					error,
 				} );
+			} );
+	};
+};
+
+/**
+ * Restores settings for a site.
+ *
+ * @param  {Number} siteId Site ID
+ * @returns {Function} Action thunk that restores the settings for a given site
+ */
+export const restoreSettings = ( siteId ) => {
+	return ( dispatch, getState ) => {
+		dispatch( { type: WP_SUPER_CACHE_RESTORE_SETTINGS, siteId } );
+		dispatch( removeNotice( 'wpsc-restore-defaults' ) );
+
+		return wp.req.post(
+			{ path: `/jetpack-blogs/${ siteId }/rest-api/` },
+			{ path: '/wp-super-cache/v1/settings', body: JSON.stringify( { reset: true } ), json: true } )
+			.then( ( { data } ) => {
+				dispatch( receiveSettings( siteId, normalizeSettings( data ) || {} ) );
+				dispatch( { type: WP_SUPER_CACHE_RESTORE_SETTINGS_SUCCESS, siteId } );
+				dispatch( successNotice(
+					translate( 'Default configuration restored successfully on %(siteTitle)s.',
+						{ args: { siteTitle: getSiteTitle( getState(), siteId ) } }
+					),
+					{ id: 'wpsc-restore-defaults' }
+				) );
+			} )
+			.catch( () => {
+				dispatch( { type: WP_SUPER_CACHE_RESTORE_SETTINGS_FAILURE, siteId } );
+				dispatch( errorNotice(
+					translate( 'There was a problem restoring the default configuration. Please try again.' ),
+					{ id: 'wpsc-restore-defaults' }
+				) );
 			} );
 	};
 };
