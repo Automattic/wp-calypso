3a840a235aefccbcbd040e0e0b5ba7addb9b3e65
bluefuton <chris@bluefuton.com>
2017-05-24 13:18:31 +0200

    Add additional generic WordPress site descriptions to blacklist

--- client/reader/lib/site-description-blacklist/index.js
+++ client/reader/lib/site-description-blacklist/index.js
@@ -1,7 +1,13 @@
-const siteDescriptionBlacklist = new Set( [
-	'Just another WordPress.com site',
-	'Just another WordPress site',
-] );
+const siteDescriptionBlacklist = new Set(
+	[
+		'Just another WordPress.com site',
+		'Just another WordPress site',
+		"This WordPress.com site is the bee's knees",
+		"This WordPress site is the bee's knees",
+		'A topnotch WordPress.com site',
+		'A topnotch WordPress site',
+	]
+);
 
 /**
  * Is the provided site description name blacklisted?
