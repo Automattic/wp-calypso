0c92b78b923b274d6c0bcb5b9375221f61a95ca0
Jon Surrell <jon.surrell@automattic.com>
2017-05-24 09:50:17 +0200

    Add devdocs example for FormattedHeader

---
+++ client/components/formatted-header/docs/example.jsx
@@ -0,0 +1,18 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import FormattedHeader from '..';
+
+export default function FormattedHeaderExample() {
+	return (
+		<FormattedHeader
+			headerText="This is the header."
+			subHeaderText="This is the optional subheader."
+		/>
+	);
+}
--- client/devdocs/design/index.jsx
+++ client/devdocs/design/index.jsx
@@ -71,6 +71,7 @@ import VerticalMenu from 'components/vertical-menu/docs/example';
 import Banner from 'components/banner/docs/example';
 import EmojifyExample from 'components/emojify/docs/example';
 import LanguagePicker from 'components/language-picker/docs/example';
+import FormattedHeader from 'components/formatted-header/docs/example';
 
 let DesignAssets = React.createClass( {
 	displayName: 'DesignAssets',
@@ -135,6 +136,7 @@ let DesignAssets = React.createClass( {
 					<FeatureGate />
 					<FilePickers />
 					<FoldableCard />
+					<FormattedHeader />
 					<FormFields searchKeywords="input textbox textarea radio" />
 					<Gauge />
 					<GlobalNotices />
