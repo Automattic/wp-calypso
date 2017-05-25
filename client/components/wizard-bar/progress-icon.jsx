345321feca496f40997a28d99f3ee0d474064f32
Folletto <folletto@gmail.com>
2017-05-25 14:36:18 +0100

    WizardBar: simplified SVG, sharper 7px style

--- client/components/wizard-bar/progress-icon.jsx
+++ client/components/wizard-bar/progress-icon.jsx
@@ -4,14 +4,7 @@
 import React from 'react';
 
 export default () => (
-	<svg width="8px" height="8px" viewBox="0 0 7 7" version="1.1"
-		xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-		<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-			<g className="wizard-bar__indicator-icon" id="alt-7" transform="translate(-418.000000, -76.000000)">
-				<rect id="Rectangle-3"
-					transform="translate(421.500000, 79.500000) rotate(-315.000000) translate(-421.500000, -79.500000) "
-					x="419" y="77" width="5" height="5" />
-			</g>
-		</g>
+	<svg className="wizard-bar__indicator-icon" width="8px" height="8px" viewBox="0 0 8 8" version="1.1">
+		<path d="M 4,0 L 8,4 L 4,8 L 0,4 Z" />
 	</svg>
 );
