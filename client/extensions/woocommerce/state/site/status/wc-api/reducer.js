3f6b1c54b11623d9eae9fb44b3251ee1ea5e3f8b
Kevin Killingsworth <kevin@redfenix.com>
2017-05-26 14:08:54 -0500

    WooCommerce: system/product state, fix error dep
    This moves an error action dependency

--- client/extensions/woocommerce/state/site/status/wc-api/reducer.js
+++ client/extensions/woocommerce/state/site/status/wc-api/reducer.js
@@ -2,10 +2,6 @@
  * Internal dependencies
  */
 import { combineReducers } from 'state/utils';
-
-/**
- * Internal dependencies
- */
 import error from './error-reducer';
 
 export default combineReducers( {
