ed25007c51426e349cafca55a4925af2e4688db8
Dennis Snell <dennis.snell@automattic.com>
2017-05-25 23:47:08 -0400

    Add tests and documentation

--- client/state/data-layer/wpcom/sites/comments/index.js
+++ client/state/data-layer/wpcom/sites/comments/index.js
@@ -12,43 +12,52 @@ import { http } from 'state/data-layer/wpcom-http/actions';
 import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
 
 import { errorNotice } from 'state/notices/actions';
+import { getRawSite } from 'state/sites/selectors';
 
 // @see https://developer.wordpress.com/docs/api/1.1/get/sites/%24site/comments/
-const fetchCommentsList = ( { dispatch }, action ) => {
+export const fetchCommentsList = ( { dispatch }, action ) => {
 	const { query } = action;
-	const { listType } = query;
 
-	if ( 'site' === listType ) {
-		const {
-			siteId,
-			status = 'unapproved',
-			type = 'comment',
-		} = query;
-
-		return dispatch( http( {
-			method: 'GET',
-			path: `/sites/${ siteId }/comments`,
-			apiVersion: '1.1',
-			query: {
-				status,
-				type,
-			}
-		}, action ) );
+	if ( ! query || 'site' !== query.listType ) {
+		return;
 	}
+
+	const {
+		siteId,
+		status = 'unapproved',
+		type = 'comment',
+	} = query;
+
+	dispatch( http( {
+		method: 'GET',
+		path: `/sites/${ siteId }/comments`,
+		apiVersion: '1.1',
+		query: {
+			status,
+			type,
+		}
+	}, action ) );
 };
 
-const addComments = ( { dispatch }, { query: { siteId } }, next, { comments } ) => {
+export const addComments = ( { dispatch }, { query: { siteId } }, next, { comments } ) => {
 	const byPost = groupBy( comments, ( { post: { ID } } ) => ID );
 
 	forEach( byPost, ( postComments, postId ) => dispatch( {
 		type: COMMENTS_RECEIVE,
 		siteId,
-		postId,
+		postId: parseInt( postId, 10 ), // keyBy => object property names are strings
 		comments: postComments,
 	} ) );
 };
 
-const announceFailure = ( { dispatch } ) => dispatch( errorNotice( translate( 'Could not retrieve list of comments' ) ) );
+const announceFailure = ( { dispatch, getState }, { query: { siteId } } ) => {
+	const site = getRawSite( getState(), siteId );
+	const error = site && site.name
+		? translate( 'Failed to retrieve comments for site “%(siteName)s”', { args: { siteName: site.name } } )
+		: translate( 'Failed to retrieve comments for your site' );
+
+	dispatch( errorNotice( error ) );
+};
 
 export default {
 	[ COMMENTS_LIST_REQUEST ]: [ dispatchRequest( fetchCommentsList, addComments, announceFailure ) ]
