ae0de2a7ee6674b5addd55c840661a126a217a4b
Kevin Killingsworth <kevin@redfenix.com>
2017-05-26 15:09:39 -0500

    WooCommerce: Add selectors for last wcApi error. (#14302)
    * WooCommerce: Move wc-api errors to site/status
    
    This moves the wc-api error state data to underneath state/site/status
    for an example of where to keep general top-level status information.
    
    * WooCommerce: Add selectors for last wcApi error.
    
    This adds selectors for the error object, the code, and the message (if
    available).

--- client/extensions/woocommerce/state/site/reducer.js
+++ client/extensions/woocommerce/state/site/reducer.js
@@ -1,12 +1,7 @@
 /**
- * External dependencies
- */
-import { combineReducers } from 'state/utils';
-
-/**
  * Internal dependencies
  */
-import { keyedReducer } from 'state/utils';
+import { combineReducers, keyedReducer } from 'state/utils';
 import status from './status/reducer';
 
 const siteReducer = combineReducers( {
