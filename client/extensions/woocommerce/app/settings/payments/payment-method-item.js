7b57ac64f77bcd764a21a649b619659800e7767e
Jonathan Belcher <jonathan.belcher@automattic.com>
2017-05-26 10:11:43 -0400

    Adds list UI component to be reused (#13956)
    * Adds list UI to be reused
    
    * Add render methods function
    
    * Style the list rows
    
    * Change components to functional components
    
    * Make width param a percentage
    
    * Simplify a map function
    
    * Rename component for a better abstraction
    
    * set up button: make compact
    
    * list table header styling
    
    * list row vertical alignment
    
    * Added a style for the last column in the payments row
    
    * Adds payment method column
    
    * Change name to label for clarity
    
    * Change prop names for clarity
    
    * Insert styles using className not props
    
    * Rename lsit to table and fix errors
    
    * Fix jsDoc error
    
    * header: style to match /shipping
    
    * Tweak list styling to further match shipping
    
    * Change Table to List (ul)
    
    * Small formatting fixes

---
+++ client/extensions/woocommerce/app/settings/payments/payment-method-item.js
@@ -0,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Button from 'components/button';
+import ListItem from '../../../components/list/list-item';
+import ListItemField from '../../../components/list/list-item-field';
+
+const PaymentMethodItem = ( { method, translate } ) => {
+	return (
+		<ListItem>
+			<ListItemField>
+				{
+					method.isSuggested &&
+					(
+						<p className="payments__method-suggested">
+							{ translate( 'Suggested Method' ) }
+						</p>
+					)
+				}
+				<p className="payments__method-name">{ method.label }</p>
+			</ListItemField>
+			<ListItemField>
+				<p className="payments__method-information">{ method.fee }</p>
+				<p className="payments__method-information">
+					<a href={ method.information }>
+						{ translate( 'More Information' ) }
+					</a>
+				</p>
+			</ListItemField>
+			<ListItemField>
+				<Button compact>
+					{ translate( 'Set up' ) }
+				</Button>
+			</ListItemField>
+		</ListItem>
+	);
+};
+
+PaymentMethodItem.propTypes = {
+	method: PropTypes.shape( {
+		label: PropTypes.string.isRequired,
+		isSuggested: PropTypes.bool.isRequired,
+		fee: PropTypes.string.isRequired,
+		informationUrl: PropTypes.string.isRequired,
+	} ),
+};
+
+export default localize( PaymentMethodItem );
