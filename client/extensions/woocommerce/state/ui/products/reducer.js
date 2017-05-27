13aa0c16b302006fc66f845d1f2bf6b51cca2743
Kevin Killingsworth <kevin@redfenix.com>
2017-05-26 21:14:43 -0500

    WooCommerce: Add api plan for product edits. (#14236)
    * WooCommerce: Split out action header
    
    The action header with breadcrumbs will be used in a lot of places in
    the WooCommerce extension, so this splits that component out to its own
    component.
    
    * WooCommerce: Add api plan for product edits.
    
    This adds the beginning of the code to convert the product edits into
    sequential API calls.
    
    * WooCommerce product api plan: Updated comments
    
    Also removed old action-header from this PR.

--- client/extensions/woocommerce/state/ui/products/reducer.js
+++ client/extensions/woocommerce/state/ui/products/reducer.js
@@ -4,8 +4,10 @@
 import edits from './edits-reducer';
 import { combineReducers } from 'state/utils';
 import variations from './variations/reducer';
+import apiPlan from './api-plan/reducer';
 
 export default combineReducers( {
 	edits,
 	variations,
+	apiPlan,
 } );
