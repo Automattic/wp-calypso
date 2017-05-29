a0664b17ad7433eb94a078a8f5c5be051bccc127
Paul Sealock <psealock@gmail.com>
2017-05-29 12:18:40 +1200

    add woo stats topSellers endpoint

--- client/extensions/woocommerce/app/store-stats/index.js
+++ client/extensions/woocommerce/app/store-stats/index.js
@@ -14,19 +14,23 @@ import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
 import Chart from './store-stats-chart';
 import StatsPeriodNavigation from 'my-sites/stats/stats-period-navigation';
 import DatePicker from 'my-sites/stats/stats-date-picker';
-import { UNITS } from './constants';
+import Module from './store-stats-module';
+import List from './store-stats-list';
+import SectionHeader from 'components/section-header';
+import { topProducts, topCategories, topCoupons, UNITS } from 'woocommerce/app/store-stats/constants';
 
 class StoreStats extends Component {
 	static propTypes = {
 		path: PropTypes.string.isRequired,
 		queryDate: PropTypes.string,
+		querystring: PropTypes.string,
 		selectedDate: PropTypes.string,
 		siteId: PropTypes.number,
 		unit: PropTypes.string.isRequired,
 	};
 
 	render() {
-		const { path, queryDate, selectedDate, siteId, slug, unit } = this.props;
+		const { path, queryDate, selectedDate, siteId, slug, unit, querystring } = this.props;
 		const unitQueryDate = ( unit === 'week' )
 			? `${ moment( queryDate ).format( UNITS[ unit ].format ) }-W${ moment( queryDate ).isoWeek() }`
 			: moment( queryDate ).format( UNITS[ unit ].format );
@@ -38,6 +42,14 @@ class StoreStats extends Component {
 			date: queryDate,
 			quantity: UNITS[ unit ].quantity,
 		};
+		const topQuery = {
+			unit,
+			date: selectedDate,
+			limit: 10,
+		};
+		const widgets = [ topProducts, topCategories, topCoupons ];
+		const widgetPath = `/${ unit }/${ slug }${ querystring ? '?' : '' }${ querystring || '' }`;
+
 		return (
 			<Main className="store-stats woocommerce" wideLayout={ true }>
 				<Navigation unit={ unit } type="orders" slug={ slug } />
@@ -66,6 +78,33 @@ class StoreStats extends Component {
 						showQueryDate
 					/>
 				</StatsPeriodNavigation>
+				<div className="store-stats__widgets">
+				{ widgets.map( widget => {
+					const header = (
+						<SectionHeader href={ widget.basePath + widgetPath }>
+							{ widget.title }
+						</SectionHeader>
+					);
+					return (
+						<div className="store-stats__widgets-column" key={ widget.basePath }>
+							<Module
+								siteId={ siteId }
+								header={ header }
+								emptyMessage={ widget.empty }
+								query={ topQuery }
+								statType={ widget.statType }
+							>
+								<List
+									siteId={ siteId }
+									values={ widget.values }
+									query={ topQuery }
+									statType={ widget.statType }
+								/>
+							</Module>
+						</div>
+					);
+				} ) }
+				</div>
 			</Main>
 		);
 	}
