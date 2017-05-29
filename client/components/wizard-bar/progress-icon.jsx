d8356a710e27d2235ac7d91b081162ee56c6fd87
Folletto <folletto@gmail.com>
2017-05-29 11:45:12 +0100

    WizardBar: updated SVG to be an arrow

--- client/components/wizard-bar/progress-icon.jsx
+++ client/components/wizard-bar/progress-icon.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const ProgressIcon = () => (
 	<svg className="wizard-bar__indicator-icon" width="8px" height="8px" viewBox="0 0 8 8" version="1.1">
-		<path d="M 4,0 L 8,4 L 4,8 L 0,4 Z" />
+		<path d="M 4,0 L 8,4 L 4,8 Z" />
 	</svg>
 );
 
