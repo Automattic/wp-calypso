cf855ef4e1bd16f602ed455aea39dc81032e013c
Marin Atanasov <tyxla@abv.bg>
2017-05-30 13:32:24 +0300

    Site Settings: Remove post type toggle when theme supports it

--- client/my-sites/site-settings/custom-content-types/index.jsx
+++ client/my-sites/site-settings/custom-content-types/index.jsx
@@ -14,11 +14,13 @@ import FormFieldset from 'components/forms/form-fieldset';
 import CompactFormToggle from 'components/forms/form-toggle/compact';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { isJetpackModuleActive, isActivatingJetpackModule } from 'state/selectors';
+import { isPostTypeSupported } from 'state/post-types/selectors';
 import { activateModule } from 'state/jetpack/modules/actions';
 import { isJetpackSite } from 'state/sites/selectors';
 import FormSettingExplanation from 'components/forms/form-setting-explanation';
 import InfoPopover from 'components/info-popover';
 import ExternalLink from 'components/external-link';
+import QueryPostTypes from 'components/data/query-post-types';
 
 class CustomContentTypes extends Component {
 	componentDidUpdate() {
@@ -58,6 +60,14 @@ class CustomContentTypes extends Component {
 		return isRequestingSettings || isSavingSettings;
 	}
 
+	renderDescription( description ) {
+		return (
+			<FormSettingExplanation isIndented>
+				{ description }
+			</FormSettingExplanation>
+		);
+	}
+
 	renderToggle( name, label, description ) {
 		const {
 			activatingCustomContentTypesModule,
@@ -73,9 +83,19 @@ class CustomContentTypes extends Component {
 				>
 					{ label }
 				</CompactFormToggle>
-				<FormSettingExplanation isIndented>
-					{ description }
-				</FormSettingExplanation>
+				{ this.renderDescription( description ) }
+			</div>
+		);
+	}
+
+	renderSupportedContentType( fieldName, fieldLabel, fieldDescription ) {
+		return (
+			<div className="custom-content-types__module-settings">
+				<div className="custom-content-types__form-toggle-placeholder form-toggle__placeholder">
+					{ fieldLabel }
+				</div>
+
+				{ this.renderDescription( fieldDescription ) }
 			</div>
 		);
 	}
@@ -89,7 +109,10 @@ class CustomContentTypes extends Component {
 	}
 
 	renderTestimonialSettings() {
-		const { translate } = this.props;
+		const {
+			isTestimonialSupported,
+			translate
+		} = this.props;
 		const fieldLabel = translate( 'Testimonials' );
 		const fieldDescription = translate(
 			'Add, organize, and display {{link}}testimonials{{/link}}. If your theme doesn’t support testimonials yet, ' +
@@ -101,11 +124,18 @@ class CustomContentTypes extends Component {
 			}
 		);
 
+		if ( isTestimonialSupported ) {
+			return this.renderSupportedContentType( 'jetpack_testimonial', fieldLabel, fieldDescription );
+		}
+
 		return this.renderContentTypeSettings( 'jetpack_testimonial', fieldLabel, fieldDescription );
 	}
 
 	renderPortfolioSettings() {
-		const { translate } = this.props;
+		const {
+			isPortfolioSupported,
+			translate
+		} = this.props;
 		const fieldLabel = translate( 'Portfolios' );
 		const fieldDescription = translate(
 			'Add, organize, and display {{link}}portfolios{{/link}}. If your theme doesn’t support portfolios yet, ' +
@@ -117,13 +147,23 @@ class CustomContentTypes extends Component {
 			}
 		);
 
+		if ( isPortfolioSupported ) {
+			return this.renderSupportedContentType( 'jetpack_portfolio', fieldLabel, fieldDescription );
+		}
+
 		return this.renderContentTypeSettings( 'jetpack_portfolio', fieldLabel, fieldDescription );
 	}
 
 	render() {
-		const { translate } = this.props;
+		const {
+			siteId,
+			translate
+		} = this.props;
+
 		return (
 			<div>
+				{ siteId && <QueryPostTypes siteId={ siteId } /> }
+
 				<SectionHeader label={ translate( 'Custom content types' ) } />
 
 				<Card className="custom-content-types__card site-settings">
@@ -165,6 +205,8 @@ export default connect(
 		return {
 			siteId,
 			siteIsJetpack: isJetpackSite( state, siteId ),
+			isPortfolioSupported: isPostTypeSupported( state, siteId, 'jetpack-portfolio' ),
+			isTestimonialSupported: isPostTypeSupported( state, siteId, 'jetpack-testimonial' ),
 			customContentTypesModuleActive: isJetpackModuleActive( state, siteId, 'custom-content-types' ),
 			activatingCustomContentTypesModule: isActivatingJetpackModule( state, siteId, 'custom-content-types' ),
 		};
