0602bbf36bbec97150f71e6e191f0535a082f376
Kevin Killingsworth <kevin@redfenix.com>
2017-05-30 14:45:31 -0500

    WooCommerce: Make action-list general purpose
    This takes the api-plan code that was originally developed under
    state/ui/product and places it under state/api-plan. It also seeks to
    make more general use of the code and adds a README to explain its
    operation.
    
    This is really just the reducer/action code, there are still selectors
    to be written and more helper functions to be ported/written.
    
    Woo: Change API Plan to Action List
    
    This changes the name of the API Plan to Action List because "Plan"
    is an overloaded term within Calypso.
    
    Woo Action-List: Post-rebase cleanup
    
    This removes some files that lingered around after the rebase,
    and make a few small adjustments to code.
    
    Split action list end into success/failure
    
    This splits the action for ending a step into two separate actions for
    success and failure, respectively. This will line up with the data layer
    and the success/failure actions for request handling.
    
    Set Date.now() from the reducer.
    
    This changes the action-list code for each step to set the time to
    Date.now() in the reducer instead of in the action creator. This is due
    to the fact that the actions are usually created long before they're
    dispatched, so the timing ends up being inaccurate.
    
    Update action-list README
    
    Some things have changed in the system and become more clear.
    
    Woo Action-List: Fix artifact from rebase
    
    This removes the old `apiPlan` reducer variable that got left
    over from a rebase operation.

---
+++ client/extensions/woocommerce/state/action-list/actions.js
@@ -0,0 +1,82 @@
+
+/**
+ * Internal dependencies
+ */
+import {
+	WOOCOMMERCE_ACTION_LIST_CREATE,
+	WOOCOMMERCE_ACTION_LIST_CLEAR,
+	WOOCOMMERCE_ACTION_LIST_STEP_START,
+	WOOCOMMERCE_ACTION_LIST_STEP_SUCCESS,
+	WOOCOMMERCE_ACTION_LIST_STEP_FAILURE,
+} from '../action-types';
+
+/**
+ * Action Creator: Create new Action List.
+ *
+ * @param {Object} actionList The sequential actionList to be carried out
+ * @return {Object} action
+ */
+export function actionListCreate( actionList ) {
+	return {
+		type: WOOCOMMERCE_ACTION_LIST_CREATE,
+		actionList,
+	};
+}
+
+/**
+ * Action Creator: Clear the current Action List.
+ * @return {Object} action
+ */
+export function actionListClear() {
+	return {
+		type: WOOCOMMERCE_ACTION_LIST_CLEAR,
+	};
+}
+
+/**
+ * Action Creator: Start action list step.
+ *
+ * @param {Number} [stepIndex] The index of the step. Defaults to zero (0), first index.
+ * @param {Date} [time] Optional timestamp.
+ * @return {Object} action
+ */
+export function actionListStepStart( stepIndex = 0, time ) {
+	return {
+		type: WOOCOMMERCE_ACTION_LIST_STEP_START,
+		stepIndex,
+		time,
+	};
+}
+
+/**
+ * Action Creator: Mark step as successful.
+ *
+ * @param {Number} stepIndex The index of the step.
+ * @param {Date} [time] Optional timestamp.
+ * @return {Object} action
+ */
+export function actionListStepSuccess( stepIndex, time ) {
+	return {
+		type: WOOCOMMERCE_ACTION_LIST_STEP_SUCCESS,
+		stepIndex,
+		time,
+	};
+}
+
+/**
+ * Action Creator: Mark step as failed.
+ *
+ * @param {Number} stepIndex The index of the step.
+ * @param {Object} error The error from the failure.
+ * @param {Date} [time] Optional timestamp.
+ * @return {Object} action
+ */
+export function actionListStepFailure( stepIndex, error, time ) {
+	return {
+		type: WOOCOMMERCE_ACTION_LIST_STEP_FAILURE,
+		stepIndex,
+		error,
+		time,
+	};
+}
+
--- client/extensions/woocommerce/state/ui/products/reducer.js
+++ client/extensions/woocommerce/state/ui/products/reducer.js
@@ -4,10 +4,8 @@
 import edits from './edits-reducer';
 import { combineReducers, keyedReducer } from 'state/utils';
 import variations from './variations/reducer';
-import apiPlan from './api-plan/reducer';
 
 export default keyedReducer( 'siteId', combineReducers( {
 	edits,
 	variations,
-	apiPlan,
 } ) );
