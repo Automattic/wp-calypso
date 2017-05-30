e3e91fd30f519119213b5d6bfd7f0371ccb34685
Justin Shreve <justin.shreve@gmail.com>
2017-05-30 15:50:58 -0700

    Adds a SKU editor to the product form.

--- client/extensions/woocommerce/app/products/product-form-details-card.js
+++ client/extensions/woocommerce/app/products/product-form-details-card.js
@@ -3,12 +3,14 @@
  */
 import React, { Component, PropTypes } from 'react';
 import i18n from 'i18n-calypso';
+import { trim } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import Card from 'components/card';
 import CompactFormToggle from 'components/forms/form-toggle/compact';
+import FormClickToEditInput from 'woocommerce/components/form-click-to-edit-input';
 import FormFieldSet from 'components/forms/form-fieldset';
 import FormLabel from 'components/forms/form-label';
 import FormTextArea from 'components/forms/form-textarea';
@@ -29,6 +31,11 @@ export default class ProductFormDetailsCard extends Component {
 	constructor( props ) {
 		super( props );
 
+		const { product } = props;
+		this.state = {
+			updateSkuOnNameChange: ! product.name ? true : false,
+		};
+
 		this.setName = this.setName.bind( this );
 		this.setDescription = this.setDescription.bind( this );
 		this.toggleFeatured = this.toggleFeatured.bind( this );
@@ -38,7 +45,24 @@ export default class ProductFormDetailsCard extends Component {
 	// into a general purpose Higher Order Component
 	setName( e ) {
 		const { product, editProduct } = this.props;
-		editProduct( product, { name: e.target.value } );
+		const name = e.target.value;
+		editProduct( product, { name } );
+
+		if ( this.state.updateSkuOnNameChange ) {
+			const sku = trim( name ).toLowerCase().replace( /\s+/g, '-' );
+			editProduct( product, { sku } );
+		}
+	}
+
+	setSku = ( sku ) => {
+		const { product, editProduct } = this.props;
+		editProduct( product, { sku } );
+
+		if ( this.state.updateSkuOnNameChange ) {
+			this.setState( {
+				updateSkuOnNameChange: false,
+			} );
+		}
 	}
 
 	setDescription( e ) {
@@ -71,6 +95,7 @@ export default class ProductFormDetailsCard extends Component {
 		const { product } = this.props;
 		const images = product.images || [];
 		const __ = i18n.translate;
+
 		return (
 			<Card className="products__product-form-details">
 				<div className="products__product-form-details-featured">
@@ -89,9 +114,19 @@ export default class ProductFormDetailsCard extends Component {
 						onRemove={ this.onImageRemove }
 					/>
 					<div className="products__product-form-details-basic">
-						<FormFieldSet>
+						<FormFieldSet className="products__product-form-details-basic-name">
 							<FormLabel htmlFor="name">{ __( 'Product name' ) }</FormLabel>
 							<FormTextInput name="name" value={ product.name || '' } onChange={ this.setName } />
+						</FormFieldSet>
+						<FormFieldSet className="products__product-form-details-basic-sku">
+							<FormLabel htmlFor="sku">{ __( 'SKU:' ) }</FormLabel>
+							<FormClickToEditInput
+								name="sku"
+								value={ product.sku || '' }
+								placeholder="-"
+								onChange={ this.setSku }
+								disabled={ product.name || product.sku ? false : true }
+							/>
 						</FormFieldSet>
 						<FormFieldSet className="products__product-form-details-basic-description">
 							<FormLabel htmlFor="description">{ __( 'Description' ) }</FormLabel>
---
+++ client/extensions/woocommerce/components/form-click-to-edit-input/index.js
@@ -0,0 +1,114 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import Gridicon from 'gridicons';
+import { noop } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import Button from 'components/button';
+import FormTextInput from 'components/forms/form-text-input';
+
+class FormClickToEditInput extends Component {
+
+	static propTypes = {
+		onChange: PropTypes.func,
+		value: PropTypes.string,
+		placeholder: PropTypes.string,
+		disabled: PropTypes.bool,
+	};
+
+	static defaultProps = {
+		onChange: noop,
+		placeholder: '',
+		value: '',
+		disabled: false,
+	};
+
+	state = {
+		isEditing: false,
+		value: '',
+	};
+
+	editStart = () => {
+		const { value } = this.props;
+		this.setState( {
+			isEditing: true,
+			value
+		} );
+	}
+
+	editEnd = () => {
+		const { onChange } = this.props;
+		if ( this.props.value !== this.state.value ) {
+			onChange( this.state.value );
+		}
+
+		this.setState( {
+			isEditing: false,
+			value: '',
+		} );
+	}
+
+	onInputChange = ( e ) => {
+		this.setState( {
+			value: e.target.value
+		} );
+	}
+
+	renderInput() {
+		const props = { ...this.props,
+			onChange: this.onInputChange,
+			value: this.state.value,
+		};
+		return (
+			<span className="form-click-to-edit-input__wrapper editing">
+				<FormTextInput
+					{ ...props }
+					autoFocus
+					className="form-click-to-edit-input__input"
+				/>
+				<Button
+					borderless
+					onClick={ this.editEnd }
+				>
+					<Gridicon icon="checkmark" />
+				</Button>
+			</span>
+		);
+	}
+
+	renderText() {
+		const { value, placeholder, disabled } = this.props;
+		return (
+			<span className="form-click-to-edit-input__wrapper">
+				<span className="form-click-to-edit-input__text">
+					{ value || placeholder }
+				</span>
+
+				{ ! disabled && (
+					<Button
+						borderless
+						onClick={ this.editStart }
+					>
+						<Gridicon icon="pencil" />
+					</Button>
+				) }
+			</span>
+		);
+	}
+
+	render() {
+		const { isEditing } = this.state;
+
+		if ( isEditing ) {
+			return this.renderInput();
+		}
+
+		return this.renderText();
+	}
+}
+
+export default FormClickToEditInput;
