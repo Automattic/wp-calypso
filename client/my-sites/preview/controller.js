9fd198de589512d231162c4437ebd0f3259144e3
Marek Hrabe <marekhrabe@me.com>
2017-05-31 10:40:32 +0200

    very hacky embed, frontend communication draft

--- client/my-sites/preview/controller.js
+++ client/my-sites/preview/controller.js
@@ -3,15 +3,42 @@
  */
 // import ReactDom from 'react-dom';
 import React from 'react';
-// import page from 'page';
+import page from 'page';
 
 /**
  * Internal dependencies
  */
+// import WebPreview from 'components/web-preview';
+
+let initialLoad;
 
 export default {
 	preview: function( context, next ) {
-		context.primary = <span>Preview Everywhere™!</span>;
+		if ( initialLoad ) {
+			initialLoad = false;
+			window.addEventListener( 'message', ( e ) => {
+				try {
+					const data = JSON.parse( e.data );
+					if ( data.channel === 'preview' ) {
+						switch ( data.type ) {
+							case 'link':
+								page( data.payload.replace( 'https://wordpress.com', '' ) );
+								return;
+						}
+					}
+				} catch ( err ) {}
+			} );
+		}
+
+		context.primary = (
+			<div>
+				<iframe
+					width="100%"
+					height="600"
+					src={ `https://${ context.params.site }/?iframe=true&preview=true` }
+				/>
+			</div>
+		);
 		next();
 	},
 };
