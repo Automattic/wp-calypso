75c13bd69894f2173a08f87c6cc6b8c18114c00a
Miguel Fonseca <miguelcsf@gmail.com>
2017-05-31 11:24:49 +0100

    restoreLastLocation: use localforage, don't redir outside Calypso (#14196)
    * restoreLastLocation: use localforage
    
    * Address feedback + other improvements:
    
    - Don't save from or redirect to a path that isn't relative to Calypso.
    - Don't call `next` before `page`
    - Move `hasInitialized` state one function down to make it a store
      global instead of a module global
    
    * Call next() if localforage read fails

--- client/state/routing/middleware.js
+++ client/state/routing/middleware.js
@@ -3,35 +3,48 @@
  */
 import debugFactory from 'debug';
 import page from 'page';
-import store from 'store';
 
 /**
  * Internal dependencies
  */
+import localforage from 'lib/localforage';
+import { isOutsideCalypso } from 'lib/url';
 import { ROUTE_SET } from 'state/action-types';
 
 const debug = debugFactory( 'calypso:restore-last-location' );
+const LAST_PATH = 'last_path';
 
-let hasInitialized = false;
+export const restoreLastLocation = () => {
+	let hasInitialized = false;
 
-export const restoreLastLocation = () => ( next ) => ( action ) => {
-	if ( action.type === ROUTE_SET && action.path ) {
-		const lastPath = store.get( 'last_path' );
-
-		if ( ! hasInitialized && lastPath && lastPath !== '/' && action.path === '/' ) {
-			debug( 'redir to', action.path );
-			page( lastPath );
-		} else {
-			debug( 'saving', action.path );
-			store.set( 'last_path', action.path );
+	return ( next ) => ( action ) => {
+		if ( action.type !== ROUTE_SET || ! action.path ) {
+			return next( action );
 		}
 
-		if ( ! hasInitialized ) {
-			hasInitialized = true;
-		}
-	}
+		localforage.getItem( LAST_PATH ).then(
+			( lastPath ) => {
+				if ( ! hasInitialized &&
+						lastPath && lastPath !== '/' &&
+						action.path === '/' &&
+						! isOutsideCalypso( lastPath ) ) {
+					debug( 'redir to', lastPath );
+					page( lastPath );
+				} else if ( action.path !== lastPath &&
+						! isOutsideCalypso( action.path ) ) {
+					debug( 'saving', action.path );
+					localforage.setItem( LAST_PATH, action.path );
+				}
 
-	return next( action );
+				if ( ! hasInitialized ) {
+					hasInitialized = true;
+				}
+
+				return next( action );
+			},
+			() => next( action )
+		);
+	};
 };
 
 export default restoreLastLocation;
