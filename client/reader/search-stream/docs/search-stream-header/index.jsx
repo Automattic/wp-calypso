d572ee11589e29d28de4dcb2b357629ab05667b6
Jake F - @samouri <samouri@users.noreply.github.com>
2017-05-31 18:32:35 +0700

    update docs

--- client/reader/search-stream/docs/search-stream-header/index.jsx
+++ client/reader/search-stream/docs/search-stream-header/index.jsx
@@ -9,10 +9,23 @@ import { map } from 'lodash';
  */
 import Card from 'components/card';
 import SearchStreamHeader from '../../search-stream-header';
+import { POSTS, SITES } from '../../search-stream-header';
 
 export default class SearchStreamHeaderExample extends PureComponent {
 	static displayName = 'SearchStreamHeaderExample';
+
+	state = {
+		selected: POSTS,
+	};
+
 	render() {
-		return <Card><SearchStreamHeader /></Card>;
+		return (
+			<Card>
+				<SearchStreamHeader
+					selected={ this.state.selected }
+					onSelection={ selected => this.setState( { selected } ) }
+				/>
+			</Card>
+		);
 	}
 }
