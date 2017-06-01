166c2850e74ed219bc63f3cae335ccececaf8f57
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-06-01 11:00:30 -0700

    Sites: Update getSiteBySlug
    getSiteSlug needs an integer to be passed into it as siteId.
    lodashe's find was turning the object keys into string which in turn
    failed to mark site's as having a conflict or not and returned the
    wrong site.

--- client/state/sites/selectors.js
+++ client/state/sites/selectors.js
@@ -55,7 +55,8 @@ export const getRawSite = ( state, siteId ) => {
 export const getSiteBySlug = createSelector(
 	( state, siteSlug ) => (
 		find( state.sites.items, ( item, siteId ) => (
-			getSiteSlug( state, siteId ) === siteSlug
+			// find always passes the siteId as a string. We need it as a integer
+			getSiteSlug( state, parseInt( siteId, 10 ) ) === siteSlug
 		) ) || null
 	),
 	( state ) => state.sites.items
