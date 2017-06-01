71e76480eed9563f75875533fbb4bb33e5095c4f
Justin Shreve <justin.shreve@gmail.com>
2017-06-01 15:20:34 -0700

    Add SKU editor to the variation modal and generate default SKUs.

--- client/extensions/woocommerce/lib/generate-variations/test/index.js
+++ client/extensions/woocommerce/lib/generate-variations/test/index.js
@@ -37,23 +37,19 @@ describe( 'generateVariations', () => {
 		] };
 
 		const variations = generateVariations( product );
-		expect( variations[ 0 ] ).to.eql( {
-			attributes: [
-				{
-					name: 'Color',
-					option: 'Red',
-				},
-			]
-		} );
+		expect( variations[ 0 ].attributes ).to.eql( [
+			{
+				name: 'Color',
+				option: 'Red',
+			},
+		] );
 
-		expect( variations[ 1 ] ).to.eql( {
-			attributes: [
-				{
-					name: 'Color',
-					option: 'Blue',
-				},
-			]
-		} );
+		expect( variations[ 1 ].attributes ).to.eql( [
+			{
+				name: 'Color',
+				option: 'Blue',
+			},
+		] );
 	} );
 	it( 'generates a cartesian of variations when passed a product with multiple variation attributes', () => {
 		const product = { id: 1, attributes: [
@@ -73,31 +69,27 @@ describe( 'generateVariations', () => {
 
 		const variations = generateVariations( product );
 
-		expect( variations[ 0 ] ).to.eql( {
-			attributes: [
-				{
-					name: 'Color',
-					option: 'Red'
-				},
-				{
-					name: 'Size',
-					option: 'Small',
-				}
-			]
-		} );
+		expect( variations[ 0 ].attributes ).to.eql( [
+			{
+				name: 'Color',
+				option: 'Red'
+			},
+			{
+				name: 'Size',
+				option: 'Small',
+			}
+		] );
 
-		expect( variations[ 1 ] ).to.eql( {
-			attributes: [
-				{
-					name: 'Color',
-					option: 'Blue'
-				},
-				{
-					name: 'Size',
-					option: 'Small',
-				}
-			]
-		} );
+		expect( variations[ 1 ].attributes ).to.eql( [
+			{
+				name: 'Color',
+				option: 'Blue'
+			},
+			{
+				name: 'Size',
+				option: 'Small',
+			}
+		] );
 	} );
 	it( 'generates a complex cartesian of variations when passed a product with multiple variation attributes and multiple options', () => {
 		const product = { id: 1, attributes: [
@@ -124,21 +116,62 @@ describe( 'generateVariations', () => {
 		const variations = generateVariations( product );
 
 		expect( variations.length ).to.eql( 12 );
-		expect( variations[ 0 ] ).to.eql( {
-			attributes: [
-				{
-					name: 'Color',
-					option: 'Red',
-				},
-				{
-					name: 'Size',
-					option: 'Small',
-				},
-				{
-					name: 'Sleeves',
-					option: 'Short',
-				},
-			]
-		} );
+		expect( variations[ 0 ].attributes ).to.eql( [
+			{
+				name: 'Color',
+				option: 'Red',
+			},
+			{
+				name: 'Size',
+				option: 'Small',
+			},
+			{
+				name: 'Sleeves',
+				option: 'Short',
+			},
+		] );
+	} );
+
+	it( 'generates a default variation sku with product name', () => {
+		const product = { id: 1, name: 'Shirt', attributes: [
+			{
+				name: 'Color',
+				options: [ 'Red', 'Blue' ],
+				variation: true,
+				uid: 'edit_0',
+			},
+			{
+				name: 'Size',
+				options: [ 'Small' ],
+				variation: true,
+				uid: 'edit_1',
+			},
+		] };
+
+		const variations = generateVariations( product );
+
+		expect( variations[ 0 ].sku ).to.equal( 'shirt-red-small' );
+		expect( variations[ 1 ].sku ).to.equal( 'shirt-blue-small' );
+	} );
+
+	it( 'generates a default variation sku without a product name', () => {
+		const product = { id: 2, attributes: [
+			{
+				name: 'Color',
+				options: [ 'Red', 'Blue' ],
+				variation: true,
+				uid: 'edit_0',
+			},
+			{
+				name: 'Size',
+				options: [ 'Small' ],
+				variation: true,
+				uid: 'edit_1',
+			},
+		] };
+
+		const variations = generateVariations( product );
+		expect( variations[ 0 ].sku ).to.equal( 'red-small' );
+		expect( variations[ 1 ].sku ).to.equal( 'blue-small' );
 	} );
 } );
