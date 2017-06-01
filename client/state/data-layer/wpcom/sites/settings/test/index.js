1311521b98a6ca880afcdeb0da4a1fcfa5395ddd
oskosk <oskosk@gmail.com>
2017-05-31 21:49:44 -0300

    Add data-layer handlers for saving site settings

--- client/state/data-layer/wpcom/sites/settings/test/index.js
+++ client/state/data-layer/wpcom/sites/settings/test/index.js
@@ -7,10 +7,10 @@ import { spy } from 'sinon';
 /**
  * Internal dependencies
  */
-
 import {
-	replaceSiteSettings,
 	fetchSiteSettings,
+	replaceSiteSettings,
+	updateSiteSettings,
 } from '../';
 
 describe( '#replaceSiteSettings', () => {
@@ -33,6 +33,40 @@ describe( '#replaceSiteSettings', () => {
 } );
 
 describe( '#fetchSiteSettings', () => {
+	let dispatch;
+
+	beforeEach( () => dispatch = spy() );
+
+	it( 'should dispatch HTTP request for site settings', () => {
+		fetchSiteSettings( { dispatch }, { siteId: 123456 } );
+
+		expect( dispatch ).to.have.been.calledWithMatch( {
+			type: 'WPCOM_HTTP_REQUEST',
+			path: '/sites/123456/settings',
+		} );
+	} );
+} );
+
+describe( '#updateSiteSettings', () => {
+	let dispatch;
+	const updated = {
+		a: 1,
+	};
+
+	beforeEach( () => dispatch = spy() );
+
+	it( 'should dispatch action to update state with updated settings', () => {
+		updateSiteSettings( { dispatch }, { siteId: 123456 }, null, { updated } );
+		expect( dispatch ).to.have.been.calledTwice;
+		expect( dispatch.firstCall ).to.have.been.calledWith( {
+			type: 'SITE_SETTINGS_UPDATE',
+			siteId: 123456,
+			settings: updated,
+		} );
+	} );
+} );
+
+describe( '#saveSiteSettings', () => {
 	let dispatch;
 
 	beforeEach( () => dispatch = spy() );
