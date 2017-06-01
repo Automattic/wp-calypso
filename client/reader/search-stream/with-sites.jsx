fa6af6ca42bf09e31b5559463a2da57cda803dc5
Jake F - @samouri <samouri@users.noreply.github.com>
2017-06-01 16:30:50 +0700

    Reader: use feature flag for site search dev

---
+++ client/reader/search-stream/with-sites.jsx
@@ -0,0 +1,17 @@
+/**
+ * External Dependencies
+ */
+import React from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal Dependencies
+ */
+
+class SearchStream extends React.Component {
+	render() {
+		return <p> shiny search page with site search functionality </p>;
+	}
+}
+
+export default localize( SearchStream );
