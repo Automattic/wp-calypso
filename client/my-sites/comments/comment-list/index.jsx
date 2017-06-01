7d26e19b8e6b9c2d2a44e7f3377e054b1582cf22
Jacopo Tomasone <Copons@users.noreply.github.com>
2017-06-01 19:56:31 +0100

    Comments: Add actions transitions and notices (#14593)
    Introduce in the Comment Management lists both a quick fade out transition for disappearing comments and an undoable global notice in response to actions.
    
    ## Changes
    
    - Add `ReactCSSTransitionGroup` to handle a fade out transition on component unmount.
    Please note that `ReactCSSTransitionGroup` is deprecated as of React 15.0, and its replacement is tracked in https://github.com/Automattic/wp-calypso/issues/13321.
    
    - Add global notices in response to actions performed on comments.
      - All notices (except "Delete Permanently") feature an Undo button, that simply fires the `setCommentStatus` method with the previous comment status.
      - Undos don't trigger any notice, and are therefore not undoable directly in the notice (they can of course be undone by via comment action).
      - The notices are not stacked together, but performing multiple actions on the same comment (e.g. `approve -> unapprove -> approve -> etc.`) only displays one single notice containing the last performed action.
    In other words, we display one notice top for each actioned comment.
      - The notices are set to last 5 seconds before automatically disappearing.
      - The notices are persistent on route changes.
    This is to prevent them to disappear when changing Comment Management list.
    The side effect, though, is that undoing a notice while outside the Comment Management would trigger the `setCommentStatus` method of the `CommentList` component, which is not mounted anymore.

--- client/my-sites/comments/comment-list/index.jsx
+++ client/my-sites/comments/comment-list/index.jsx
@@ -3,11 +3,18 @@
  */
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { filter, map } from 'lodash';
+import { localize } from 'i18n-calypso';
+import { filter, get, keyBy, map, omit } from 'lodash';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 /**
  * Internal dependencies
  */
+import {
+	createNotice,
+	removeNotice,
+} from 'state/notices/actions';
+import { getNotices } from 'state/notices/selectors';
 import getSiteComments from 'state/selectors/get-site-comments';
 import CommentDetail from 'blocks/comment-detail';
 import CommentNavigation from '../comment-navigation';
@@ -27,53 +34,96 @@ export class CommentList extends Component {
 
 	componentWillReceiveProps( nextProps ) {
 		if ( ! this.props.comments.length ) {
-			this.setState( { comments: nextProps.comments } );
+			this.setState( { comments: keyBy( nextProps.comments, 'ID' ) } );
 		}
+	}
+
+	showNotice = ( commentId, newStatus, previousStatus ) => {
+		const { translate } = this.props;
+
+		const [ type, message ] = get( {
+			approved: [ 'is-success', translate( 'Comment approved.' ) ],
+			unapproved: [ 'is-info', translate( 'Comment unapproved.' ) ],
+			spam: [ 'is-warning', translate( 'Comment marked as spam.' ) ],
+			trash: [ 'is-error', translate( 'Comment moved to trash.' ) ],
+			'delete': [ 'is-error', translate( 'Comment deleted permanently.' ) ],
+		}, newStatus, [ null, null ] );
+
+		if ( ! type ) {
+			return;
+		}
+
+		const options = Object.assign(
+			{
+				duration: 5000,
+				id: `comment-notice-${ commentId }`,
+				isPersistent: true,
+			},
+			'delete' !== newStatus && {
+				button: translate( 'Undo' ),
+				onClick: () => this.setCommentStatus( commentId, previousStatus, { showNotice: false } ),
+			}
+		);
+
+		this.props.createNotice( type, message, options );
 	}
 
 	deleteForever = commentId => () => {
-		this.setState( {
-			comments: filter( this.state.comments, comment => commentId !== comment.ID ),
-		} );
+		this.props.removeNotice( `comment-notice-${ commentId }` );
+		this.showNotice( commentId, 'delete', 'trash' );
+
+		this.setState( { comments: omit( this.state.comments, commentId ) } );
 	}
 
 	toggleCommentLike = commentId => {
-		const comments = map( this.state.comments, comment => {
-			if ( commentId === comment.ID ) {
-				const newLike = ! comment.i_like;
-				// If like changes to true, also approve the comment
-				return {
-					...comment,
-					i_like: newLike,
-					status: newLike ? 'approved' : comment.status,
-				};
-			}
-			return comment;
-		} );
+		const comment = this.state.comments[ commentId ];
+		const newLikeValue = ! comment.i_like;
 
-		this.setState( { comments } );
-	};
-
-	setCommentStatus = ( commentId, status ) => {
-		let statusChanged = false;
-
-		const comments = map( this.state.comments, comment => {
-			if ( commentId === comment.ID && status !== comment.status ) {
-				statusChanged = true;
-				// If status changes to unapproved/spam/trash, also remove the like
-				return {
-					...comment,
-					i_like: 'approved' === status ? comment.i_like : false,
-					status,
-				};
-			}
-			return comment;
-		} );
-
-		if ( statusChanged ) {
-			this.setState( { comments } );
+		if ( 'unapproved' === comment.status ) {
+			this.props.removeNotice( `comment-notice-${ commentId }` );
+			this.showNotice( commentId, 'approved', 'unapproved' );
 		}
-	};
+
+		// If like changes to true, also approve the comment
+		this.setState( {
+			comments: {
+				...this.state.comments,
+				[ commentId ]: {
+					...comment,
+					i_like: newLikeValue,
+					status: newLikeValue ? 'approved' : comment.status,
+				},
+			},
+		} );
+	}
+
+	setCommentStatus = ( commentId, status, options = { showNotice: true } ) => {
+		const comment = this.state.comments[ commentId ];
+
+		if ( status === comment.status ) {
+			return;
+		}
+
+		// If the comment is not approved anymore, also remove the like, otherwise keep its previous value
+		const newLikeValue = 'approved' === status ? comment.i_like : false;
+
+		this.props.removeNotice( `comment-notice-${ commentId }` );
+
+		if ( options.showNotice ) {
+			this.showNotice( commentId, status, comment.status );
+		}
+
+		this.setState( {
+			comments: {
+				...this.state.comments,
+				[ commentId ]: {
+					...comment,
+					i_like: newLikeValue,
+					status,
+				},
+			},
+		} );
+	}
 
 	toggleBulkEdit = () => this.setState( { isBulkEdit: ! this.state.isBulkEdit } );
 
@@ -100,18 +150,24 @@ export class CommentList extends Component {
 					status,
 					toggleBulkEdit: this.toggleBulkEdit,
 				} } />
-				{ map( filteredComments, comment =>
-					<CommentDetail
-						commentId={ comment.ID }
-						deleteForever={ this.deleteForever( comment.ID ) }
-						isBulkEdit={ isBulkEdit }
-						key={ `comment-${ siteId }-${ comment.ID }` }
-						setCommentStatus={ this.setCommentStatus }
-						siteId={ siteId }
-						toggleCommentLike={ this.toggleCommentLike }
-						{ ...comment }
-					/>
-				) }
+				<ReactCSSTransitionGroup
+					transitionEnterTimeout={ 150 }
+					transitionLeaveTimeout={ 150 }
+					transitionName="comment-detail__transition"
+				>
+					{ map( filteredComments, comment =>
+						<CommentDetail
+							commentId={ comment.ID }
+							deleteForever={ this.deleteForever( comment.ID ) }
+							isBulkEdit={ isBulkEdit }
+							key={ `comment-${ siteId }-${ comment.ID }` }
+							setCommentStatus={ this.setCommentStatus }
+							siteId={ siteId }
+							toggleCommentLike={ this.toggleCommentLike }
+							{ ...comment }
+						/>
+					) }
+				</ReactCSSTransitionGroup>
 			</div>
 		);
 	}
@@ -121,8 +177,14 @@ const mapStateToProps = ( state, { siteId } ) => {
 	const comments = getSiteComments( state, siteId );
 	return {
 		comments,
+		notices: getNotices( state ),
 		siteId,
 	};
 };
 
-export default connect( mapStateToProps )( CommentList );
+const mapDispatchToProps = {
+	createNotice,
+	removeNotice,
+};
+
+export default connect( mapStateToProps, mapDispatchToProps )( localize( CommentList ) );
