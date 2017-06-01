53ebbe60722cf8fc53abc7f3abf79058cf8dbe73
Matías Ventura <mv@matiasventura.com>
2017-06-01 21:44:37 +0200

    Also filter sites response in Jetpack connect.

--- client/state/jetpack-connect/actions.js
+++ client/state/jetpack-connect/actions.js
@@ -356,7 +356,12 @@ export default {
 				} );
 				// Update the user now that we are fully connected.
 				userFactory().fetch();
-				return wpcom.me().sites( { site_visibility: 'all', include_domain_only: true } );
+				return wpcom.me().sites( {
+					site_visibility: 'all',
+					include_domain_only: true,
+					fields: 'ID,URL,name,capabilities,jetpack,visible,icon,plan,jetpack_modules,single_user_site,is_multisite,options',
+					options: 'is_mapped_domain,unmapped_url,admin_url,is_redirect,is_automated_transfer,allowed_file_types,show_on_front,main_network_site,jetpack_version,software_version,default_post_format,created_at,frame_nonce,publicize_permanently_disabled,page_on_front,page_for_posts,advanced_seo_front_page_description,advanced_seo_title_formats,verification_services_codes,podcasting_archive,is_domain_only'
+				} );
 			} )
 			.then( ( data ) => {
 				tracksEvent( dispatch, 'calypso_jpc_auth_sitesrefresh', {
