2dc26b76b584663e1b4377e92f9f48f9d99ca7ef
Donna Peplinskie <donnapep@gmail.com>
2017-06-01 11:11:20 -0400

    Add more detail to stats schema

--- client/extensions/wp-super-cache/state/stats/schema.js
+++ client/extensions/wp-super-cache/state/stats/schema.js
@@ -4,7 +4,29 @@ export const statsSchema = {
 	patternProperties: {
 		// Site Id
 		'^\\d+$': {
-			type: 'object'
+			type: 'object',
+			items: {
+				supercache: {
+					type: 'object',
+					items: {
+						cached: { type: 'integer' },
+						cached_list: { type: 'object' },
+						expired: { type: 'integer' },
+						expired_list: { type: 'object' },
+						fsize: { type: 'integer' },
+					}
+				},
+				wpcache: {
+					type: 'object',
+					items: {
+						cached: { type: 'integer' },
+						cached_list: { type: 'object' },
+						expired: { type: 'integer' },
+						expired_list: { type: 'object' },
+						fsize: { type: 'integer' },
+					}
+				}
+			}
 		}
 	}
 };
