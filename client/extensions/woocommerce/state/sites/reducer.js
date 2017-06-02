d20b3bf981b8927a7b0cfa73e628a02d73d67acf
Kelly Dwan <ryelle@users.noreply.github.com>
2017-06-02 15:47:41 -0400

    Update reducer to use `keyedReducer`

--- client/extensions/woocommerce/state/sites/reducer.js
+++ client/extensions/woocommerce/state/sites/reducer.js
@@ -1,37 +1,21 @@
 /**
  * Internal dependencies
  */
-import settingsGeneral from './settings/general/reducer';
+import { combineReducers, keyedReducer } from 'state/utils';
 import paymentMethods from './payment-methods/reducer';
 import productCategories from './product-categories/reducer';
-import shippingZones from './shipping-zones/reducer';
 import products from './products/reducer';
+import shippingZones from './shipping-zones/reducer';
+import settingsGeneral from './settings/general/reducer';
+import status from './status/reducer';
 
-const initialState = {};
+const reducer = combineReducers( {
+	paymentMethods,
+	productCategories,
+	products,
+	settingsGeneral,
+	shippingZones,
+	status,
+} );
 
-const handlers = {
-	...paymentMethods,
-	...productCategories,
-	...settingsGeneral,
-	...shippingZones,
-	...products,
-};
-
-export default function( state = initialState, action ) {
-	const { type, payload } = action;
-	const { siteId } = payload || {};
-	const handler = handlers[ type ];
-
-	if ( handler ) {
-		if ( ! siteId ) {
-			throw new TypeError( `Action ${ type } handled by reducer, but no siteId set on action.` );
-		}
-
-		const siteState = state[ siteId ] || {};
-		const newSiteState = handler( siteState, action );
-
-		return { ...state, [ siteId ]: newSiteState };
-	}
-
-	return state;
-}
+export default keyedReducer( 'siteId', reducer );
