1b9b38161cfada2c5e44c0e63f6dfae07934747c
Miguel Fonseca <miguelcsf@gmail.com>
2017-06-02 13:59:31 +0100

    Add handling of route /preview

--- client/my-sites/preview/index.js
+++ client/my-sites/preview/index.js
@@ -2,9 +2,10 @@
  * Internal dependencies
  */
 import { makeLayout } from 'controller';
-import { siteSelection, makeNavigation } from 'my-sites/controller';
+import { siteSelection, sites, makeNavigation } from 'my-sites/controller';
 import { preview } from './controller';
 
 export default function( router ) {
+	router( '/preview', siteSelection, sites );
 	router( '/preview/:site', siteSelection, makeNavigation, preview, makeLayout );
 }
