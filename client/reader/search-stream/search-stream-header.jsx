e43647f54da098c4efe4a90a15da7789dc49c770
Jake <samouri@users.noreply.github.com>
2017-06-02 20:06:17 +0700

    Reader: Add search-stream header for site-results-in-search (#14578)
    * Reader: Add in search-stream header for site-results-in-search
    
    * add in styles
    
    * added to devdocs
    
    * update docs
    
    * with width
    
    * typo
    
    * fix displayName issues
    
    * removed second componentDidMount -- only second was kept anyway
    
    * fix css namig
    
    * fix typo in docs
    
    * removed the docs and inserted the header to new with-sites search page

---
+++ client/reader/search-stream/search-stream-header.jsx
@@ -0,0 +1,70 @@
+/**
+ * External Dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { localize } from 'i18n-calypso';
+import { noop } from 'lodash';
+
+/**
+ * Internal Dependencies
+ */
+import NavTabs from 'components/section-nav/tabs';
+import SectionNav from 'components/section-nav';
+import NavItem from 'components/section-nav/item';
+
+export const POSTS = 'Posts';
+export const SITES = 'Sites';
+
+class SearchStreamHeader extends Component {
+	static propTypes = {
+		translate: PropTypes.func,
+		wideDisplay: PropTypes.bool,
+		selected: PropTypes.oneOf( [ POSTS, SITES ] ),
+		onSelection: PropTypes.func,
+	};
+	static defaultProps = {
+		onSelection: noop,
+		selected: POSTS,
+	}
+
+	handlePostsSelected = () => this.props.onSelection( POSTS );
+	handleSitesSelected = () => this.props.onSelection( SITES );
+
+	render() {
+		const { translate, wideDisplay, selected } = this.props;
+
+		if ( wideDisplay ) {
+			return (
+				<ul className="search-stream__headers">
+					<li className="search-stream__post-header">{ translate( 'Posts' ) }</li>
+					<li className="search-stream__site-header">{ translate( 'Sites' ) }</li>
+				</ul>
+			);
+		}
+
+		return (
+			<div className="search-stream__header">
+				<SectionNav>
+					<NavTabs>
+						<NavItem
+							key={ 'posts-nav' }
+							selected={ selected === POSTS }
+							onClick={ this.handlePostsSelected }
+						>
+							{ translate( 'Posts' ) }
+						</NavItem>
+						<NavItem
+							key={ 'sites-nav' }
+							selected={ selected === SITES }
+							onClick={ this.handleSitesSelected }
+						>
+							{ translate( 'Sites' ) }
+						</NavItem>
+					</NavTabs>
+				</SectionNav>
+			</div>
+		);
+	}
+}
+
+export default localize( SearchStreamHeader );
