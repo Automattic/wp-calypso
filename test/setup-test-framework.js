a2a4a1cffce401b98acef46c8fdc54d98039e3d5
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-06-04 14:29:33 +0200

    Tests: Remove unnecessary override for Jest assertion methods

--- test/setup-test-framework.js
+++ test/setup-test-framework.js
@@ -19,30 +19,7 @@ assert.expose( chai.assert, { prefix: '' } );
 disableNetConnect();
 enableNetConnect( 'localhost' );
 
-// Make sure chai and jasmine ".not" play nice together
-const originalNot = Object.getOwnPropertyDescriptor( chai.Assertion.prototype, 'not' ).get;
-
-Object.defineProperty( chai.Assertion.prototype, 'not', {
-	get() {
-		Object.assign( this, this.assignedNot );
-		return originalNot.apply( this );
-	},
-	set( newNot ) {
-		this.assignedNot = newNot;
-		return newNot;
-	},
-} );
-
-// Combine both jest and chai matchers on expect
-const originalExpect = global.expect;
-
-global.expect = actual => {
-	const originalMatchers = originalExpect( actual );
-	const chaiMatchers = chai.expect( actual );
-
-	return Object.assign( chaiMatchers, originalMatchers );
-};
-
 // Make sure we can share test helpers between Mocha and Jest
-global.before = global.beforeAll;
 global.after = global.afterAll;
+global.before = global.beforeAll;
+global.context = global.describe;
