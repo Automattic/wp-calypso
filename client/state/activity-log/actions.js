6b611483991ed2bece732a48e040b520bcf7f987
Steve Seear <steve.seear@gmail.com>
2017-06-05 13:53:31 +0100

    Activity log: Rewind activation actions and http

--- client/state/activity-log/actions.js
+++ client/state/activity-log/actions.js
@@ -2,10 +2,34 @@
  * Internal dependencies
  */
 import {
+	REWIND_ACTIVATE_FAILURE,
+	REWIND_ACTIVATE_REQUEST,
+	REWIND_ACTIVATE_SUCCESS,
 	REWIND_STATUS_ERROR,
 	REWIND_STATUS_REQUEST,
 	REWIND_STATUS_UPDATE,
 } from 'state/action-types';
+
+export function activateRewind( siteId ) {
+	return {
+		REWIND_ACTIVATE_REQUEST,
+		siteId,
+	};
+}
+
+export function rewindActivateSuccess( siteId ) {
+	return {
+		type: REWIND_ACTIVATE_SUCCESS,
+		siteId,
+	};
+}
+
+export function rewindActivateFailure( siteId ) {
+	return {
+		type: REWIND_ACTIVATE_FAILURE,
+		siteId,
+	};
+}
 
 export function getRewindStatus( siteId ) {
 	return {
--- client/state/data-layer/wpcom/activity-log/index.js
+++ client/state/data-layer/wpcom/activity-log/index.js
@@ -2,6 +2,10 @@
  * Internal dependencies
  */
 import { mergeHandlers } from 'state/action-watchers/utils';
+import activate from './activate';
 import rewind from './rewind';
 
-export default mergeHandlers( rewind );
+export default mergeHandlers(
+	activate,
+	rewind
+);
