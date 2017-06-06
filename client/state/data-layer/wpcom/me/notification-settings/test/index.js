1e120cbd6716247f3841e05267a54c9529c474f1
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-06-06 20:58:25 -0300

    Notification Settings: adds data layer squeleton for notification settings

---
+++ client/state/data-layer/wpcom/me/notification-settings/test/index.js
@@ -0,0 +1,65 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import { spy } from 'sinon';
+
+/**
+ * Internal dependencies
+ */
+import { http } from 'state/data-layer/wpcom-http/actions';
+import { NOTIFICATION_SETTINGS_RECEIVE, NOTICE_CREATE } from 'state/action-types';
+import {
+	requestNotificationSettings,
+	handleSuccess,
+	handleError,
+} from '../';
+
+describe( 'wpcom-api', () => {
+	describe( 'notification settings', () => {
+		describe( '#requestNotificationSettings()', () => {
+			it( 'should dispatch HTTP request to the user notification settings endpoint', () => {
+				const dispatch = spy();
+
+				requestNotificationSettings( { dispatch } );
+
+				expect( dispatch ).to.have.been.calledOnce;
+				expect( dispatch ).to.have.been.calledWith( http( {
+					apiVersion: '1.1',
+					method: 'GET',
+					path: '/me/notifications/settings'
+				} ) );
+			} );
+		} );
+
+		describe( '#handleSuccess', () => {
+			it( 'should dispatch notification settings', () => {
+				const dispatch = spy();
+
+				handleSuccess( { dispatch }, null, null, {} );
+
+				expect( dispatch ).to.have.been.calledOnce;
+				expect( dispatch ).to.have.been.calledWith( {
+					type: NOTIFICATION_SETTINGS_RECEIVE,
+					settings: {}
+				} );
+			} );
+		} );
+
+		describe( '#handleError', () => {
+			it( 'should dispatch error notice', () => {
+				const dispatch = spy();
+
+				handleError( { dispatch } );
+
+				expect( dispatch ).to.have.been.calledOnce;
+				expect( dispatch ).to.have.been.calledWithMatch( {
+					type: NOTICE_CREATE,
+					notice: {
+						status: 'is-error'
+					}
+				} );
+			} );
+		} );
+	} );
+} );
