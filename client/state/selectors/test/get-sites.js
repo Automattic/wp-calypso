6789e3145c4c3c5852edce6867512ba1b435a711
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-06-06 13:01:51 -0300

    Sites Selector: adds test for alphatical order

--- client/state/selectors/test/get-sites.js
+++ client/state/selectors/test/get-sites.js
@@ -95,7 +95,39 @@ describe( 'getSites()', () => {
 
 		expect( sites ).to.have.length( 3 );
 		expect( sites[ 0 ] ).to.have.property( 'ID', 2916288 );
-		expect( sites[ 1 ] ).to.have.property( 'ID', 2916287 );
-		expect( sites[ 2 ] ).to.have.property( 'ID', 2916289 );
+	} );
+
+	it( 'should returns sites in alphabetical order by name and url', () => {
+		const state = {
+			...currentUserState,
+			sites: {
+				items: {
+					2916287: { ID: 2916287, name: 'WordPress.com B Site', URL: '' },
+					2916288: { ID: 2916288, name: 'WordPress.com Z Site', URL: '' },
+					2916289: { ID: 2916289, name: 'WordPress.com A Site', URL: '' },
+					2916290: { ID: 2916290, name: 'WordPress.com C Site', URL: '' },
+					2916291: { ID: 2916291, name: 'WordPress.com 0 Site', URL: '' },
+					2916292: { ID: 2916292, name: '', URL: 'https://z-site-with-no-name.wordpress.com' },
+					2916293: { ID: 2916293, name: '', URL: 'https://0-site-with-no-name.wordpress.com' },
+					2916294: { ID: 2916294, name: 'WordPress.com B Site', URL: 'https://site-with-same-name-2.wordpress.com' },
+					2916295: { ID: 2916295, name: 'WordPress.com B Site', URL: 'https://site-with-same-name-1.wordpress.com' }
+				}
+			},
+			siteSettings: {
+				items: {},
+			},
+		};
+		const sites = getSites( state );
+
+		expect( sites ).to.have.length( 9 );
+		expect( sites[ 0 ] ).to.have.property( 'ID', 2916288 ); // WordPress.com Z Blog - Primary Site
+		expect( sites[ 1 ] ).to.have.property( 'ID', 2916293 ); // https://0-site-with-no-name.wordpress.com
+		expect( sites[ 2 ] ).to.have.property( 'ID', 2916292 ); // https://z-site-with-no-name.wordpress.com
+		expect( sites[ 3 ] ).to.have.property( 'ID', 2916291 ); // WordPress.com 0 Site
+		expect( sites[ 4 ] ).to.have.property( 'ID', 2916289 ); // WordPress.com A Site
+		expect( sites[ 5 ] ).to.have.property( 'ID', 2916287 ); // WordPress.com B Site
+		expect( sites[ 6 ] ).to.have.property( 'ID', 2916295 ); // WordPress.com B Site - https://site-with-same-name-1.wordpress.com
+		expect( sites[ 7 ] ).to.have.property( 'ID', 2916294 ); // WordPress.com B Site - https://site-with-same-name-2.wordpress.com
+		expect( sites[ 8 ] ).to.have.property( 'ID', 2916290 ); // WordPress.com C Site
 	} );
 } );
