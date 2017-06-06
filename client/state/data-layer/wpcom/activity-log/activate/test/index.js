9c92aef040080ec07d8e1ab18be2dc83e2c7f42b
Steve Seear <steve.seear@gmail.com>
2017-06-06 15:52:27 +0100

    Activity Logs: Activate/Deactivate actions and http handlers (#14762)
    Adds rewind activate/deactivate actions and http middleware.
    
    Part of splitting PR #10862 into merge-size chunks. Follows on from rewind status actions in #14660.
    
    This PR sticks closely to the original #10862, keeping the _SUCCESS and _FAILURE actions so that we can show UI feedback on the toggle button whilst the requests are in-flight.

---
+++ client/state/data-layer/wpcom/activity-log/activate/test/index.js
@@ -0,0 +1,51 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+/**
+ * Internal dependencies
+ */
+import {
+	activateSucceeded,
+	activateFailed,
+} from '../';
+import {
+	rewindActivateSuccess,
+	rewindActivateFailure,
+} from 'state/activity-log/actions';
+
+const siteId = 77203074;
+
+describe( 'activateSucceeded', () => {
+	it( 'should dispatch rewind activate success action', () => {
+		const dispatch = sinon.spy();
+		activateSucceeded( { dispatch }, { siteId } );
+		expect( dispatch ).to.have.been.calledWith(
+			rewindActivateSuccess( siteId )
+		);
+	} );
+} );
+
+describe( 'activateFailed', () => {
+	it( 'should dispatch rewind activate failed action', () => {
+		const dispatch = sinon.spy();
+		activateFailed( { dispatch }, { siteId }, null, { message: 'some problem' } );
+		expect( dispatch ).to.have.been.calledWith(
+			rewindActivateFailure( siteId )
+		);
+	} );
+
+	it( 'should dispatch an error notice', () => {
+		const dispatch = sinon.spy();
+		activateFailed( { dispatch }, { siteId }, null, { message: 'some problem' } );
+		expect( dispatch ).to.have.been.calledWith( sinon.match( {
+			notice: {
+				status: 'is-error',
+				text: 'Problem activating rewind: some problem'
+			}
+		} ) );
+	} );
+} );
+
--- client/state/data-layer/wpcom/activity-log/index.js
+++ client/state/data-layer/wpcom/activity-log/index.js
@@ -2,6 +2,12 @@
  * Internal dependencies
  */
 import { mergeHandlers } from 'state/action-watchers/utils';
+import activate from './activate';
+import deactivate from './deactivate';
 import rewind from './rewind';
 
-export default mergeHandlers( rewind );
+export default mergeHandlers(
+	activate,
+	deactivate,
+	rewind
+);
