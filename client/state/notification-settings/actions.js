ab14c77f9619f909c0a7ed33114add17693d9281
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-06-06 20:35:00 -0300

    Notification Settings: adds request and receive action creators

---
+++ client/state/notification-settings/actions.js
@@ -0,0 +1,24 @@
+/**
+ * Internal dependencies
+ */
+import {
+	NOTIFICATION_SETTINGS_RECEIVE,
+	NOTIFICATION_SETTINGS_REQUEST,
+} from 'state/action-types';
+
+/**
+ * Returns an action object to signal the request of the current user notification settings.
+ * @returns {Object} action object
+ */
+export const requestNotificationSettings = () => ( { type: NOTIFICATION_SETTINGS_REQUEST } );
+
+/**
+ * Returns an action object to signal the arrival of the requested notification settings.
+ *
+ * @param  {Object} settings User Notification Settings
+ * @return {Object}          action object
+ */
+export const receiveNotificationSettings = settings => ( {
+	type: NOTIFICATION_SETTINGS_RECEIVE,
+	settings
+} );
---
+++ client/state/notification-settings/test/actions.js
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import {
+	NOTIFICATION_SETTINGS_REQUEST,
+	NOTIFICATION_SETTINGS_RECEIVE,
+} from 'state/action-types';
+import {
+	requestNotificationSettings,
+	receiveNotificationSettings,
+} from '../actions';
+
+describe( 'actions', () => {
+	describe( '#requestNotificationSettings()', () => {
+		it( 'should return an action object', () => {
+			const action = requestNotificationSettings();
+
+			expect( action ).to.eql( { type: NOTIFICATION_SETTINGS_REQUEST } );
+		} );
+	} );
+
+	describe( '#receiveNotificationSettings()', () => {
+		it( 'should return an action object', () => {
+			const settings = {};
+			const action = receiveNotificationSettings( {} );
+
+			expect( action ).to.eql( { type: NOTIFICATION_SETTINGS_RECEIVE, settings } );
+		} );
+	} );
+} );
