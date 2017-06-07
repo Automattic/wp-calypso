ea45f98cc483ab23c22b6606d8ec374354397376
Marin Atanasov <tyxla@abv.bg>
2017-06-07 17:26:49 +0300

    Purchases: Embed the billing period switch in PurchasePlanDetails

--- client/me/purchases/manage-purchase/plan-details/index.jsx
+++ client/me/purchases/manage-purchase/plan-details/index.jsx
@@ -14,6 +14,7 @@ import FormFieldset from 'components/forms/form-fieldset';
 import FormLabel from 'components/forms/form-label';
 import QueryPluginKeys from 'components/data/query-plugin-keys';
 import SectionHeader from 'components/section-header';
+import PlanBillingPeriod from './billing-period';
 import { isRequestingSites } from 'state/sites/selectors';
 import { getByPurchaseId, hasLoadedUserPurchasesFromServer } from 'state/purchases/selectors';
 import { getPurchase, isDataLoading } from 'me/purchases/utils';
@@ -71,6 +72,8 @@ class PurchasePlanDetails extends Component {
 				<QueryPluginKeys siteId={ selectedSite.ID } />
 				<SectionHeader label={ headerText } />
 				<Card>
+					<PlanBillingPeriod purchase={ purchase } />
+
 					{ pluginList.map( ( plugin, i ) => {
 						return (
 							<FormFieldset key={ i }>
