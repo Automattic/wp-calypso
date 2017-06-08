d5d9a9765be92fd687aa767a5518b7c271cc60f0
Jon Surrell <jon.surrell@automattic.com>
2017-06-08 08:12:16 +0200

    Add/fix rewind-status tests

--- client/state/activity-log/rewind-status/test/reducer.js
+++ client/state/activity-log/rewind-status/test/reducer.js
@@ -8,73 +8,113 @@ import deepFreeze from 'deep-freeze';
  * Internal dependencies
  */
 import {
-	errors,
-	items,
+	rewindStatus,
+	rewindStatusError,
 } from '../reducer';
 import {
-	REWIND_STATUS_ERROR,
-	REWIND_STATUS_UPDATE,
-} from 'state/action-types';
+	rewindStatusError as rewindStatusErrorAction,
+	updateRewindStatus,
+} from '../../actions';
 
-describe( '#errors()', () => {
-	const siteId = 987;
+/**
+ * Constants
+ */
+const SITE_ID = 987;
+const STATUS_ACTION = deepFreeze( updateRewindStatus( SITE_ID, {
+	active: true,
+	firstBackupDate: '2017-04-19 12:00:00',
+	isPressable: false,
+	plan: 'jetpack-business',
+} ) );
 
-	const errorAction = deepFreeze( {
-		type: REWIND_STATUS_ERROR,
-		siteId,
-		error: {
-			error: 'unknown_blog',
-			message: 'Unknown blog'
-		},
+const ERROR_ACTION = deepFreeze( rewindStatusErrorAction( SITE_ID, {
+	error: 'unknown_blog',
+	message: 'Unknown blog'
+} ) );
+
+describe( '#rewindStatus()', () => {
+	it( 'should update site item', () => {
+		const state = rewindStatus( undefined, STATUS_ACTION );
+		expect( state[ SITE_ID ] ).to.deep.equal( STATUS_ACTION.status );
 	} );
 
-	it( 'should update site error', () => {
-		const state = errors( undefined, errorAction );
-		expect( state[ siteId ] ).to.deep.equal( errorAction.error );
-	} );
-
-	it( 'should preserve other site errors', () => {
+	it( 'should preserve other site\'s status', () => {
+		const otherSiteId = 123456;
 		const prevState = deepFreeze( {
-			123456: {
+			[ otherSiteId ]: {
+				active: false,
+				firstBackupDate: '',
+				isPressable: false,
+				plan: 'jetpack-free',
+			},
+		} );
+
+		const state = rewindStatus( prevState, STATUS_ACTION );
+		expect( state[ otherSiteId ] ).to.deep.equal( prevState[ otherSiteId ] );
+	} );
+
+	it( 'should remove status on error', () => {
+		const prevState = deepFreeze( {
+			[ SITE_ID ]: STATUS_ACTION.status,
+		} );
+		const state = rewindStatus( prevState, ERROR_ACTION );
+		expect( state ).to.not.have.ownProperty( SITE_ID );
+	} );
+
+	it( 'should preserve other site\'s status on error', () => {
+		const otherSiteId = 123456;
+		const prevState = deepFreeze( {
+			[ otherSiteId ]: {
+				active: false,
+				firstBackupDate: '',
+				isPressable: false,
+				plan: 'jetpack-free',
+			},
+		} );
+
+		const state = rewindStatus( prevState, ERROR_ACTION );
+		expect( state[ otherSiteId ] ).to.deep.equal( prevState[ otherSiteId ] );
+	} );
+} );
+
+describe( '#rewindStatusError()', () => {
+	it( 'should update site error', () => {
+		const state = rewindStatusError( undefined, ERROR_ACTION );
+		expect( state[ SITE_ID ] ).to.deep.equal( ERROR_ACTION.error );
+	} );
+
+	it( 'should preserve other site\'s error', () => {
+		const otherSiteId = 123456;
+		const prevState = deepFreeze( {
+			otherSiteId: {
 				error: 'an_error',
 				message: 'This is an error message.'
 			}
 		} );
 
-		const state = errors( prevState, errorAction );
-		expect( state[ 123456 ] ).to.deep.equal( prevState[ 123456 ] );
-	} );
-} );
-
-describe( '#items()', () => {
-	const siteId = 987;
-	const updateAction = deepFreeze( {
-		type: REWIND_STATUS_UPDATE,
-		siteId,
-		status: {
-			active: true,
-			firstBackupDate: '2017-04-19 12:00:00',
-			isPressable: false,
-			plan: 'jetpack-business',
-		},
+		const state = rewindStatusError( prevState, ERROR_ACTION );
+		expect( state[ otherSiteId ] ).to.deep.equal( prevState[ otherSiteId ] );
 	} );
 
-	it( 'should update site item', () => {
-		const state = items( undefined, updateAction );
-		expect( state[ siteId ] ).to.deep.equal( updateAction.status );
-	} );
-
-	it( 'should preserve other site items', () => {
+	it( 'should remove error on status', () => {
 		const prevState = deepFreeze( {
-			123456: {
-				active: false,
-				firstBackupDate: '',
-				isPressable: false,
-				plan: 'jetpack-free',
-			}
+			[ SITE_ID ]: ERROR_ACTION.error,
+		} );
+		const state = rewindStatusError( prevState, STATUS_ACTION );
+		expect( state ).to.not.have.ownProperty( SITE_ID );
+	} );
+
+	it( 'should preserve other site\'s error on status', () => {
+		const otherSiteId = 123456;
+		const prevState = deepFreeze( {
+			[ otherSiteId ]: {
+				error: 'an_error',
+				message: 'This is an error message.'
+			},
 		} );
 
-		const state = items( prevState, updateAction );
-		expect( state[ 123456 ] ).to.deep.equal( prevState[ 123456 ] );
+		const state = rewindStatusError( prevState, STATUS_ACTION );
+		expect( state[ otherSiteId ] ).to.deep.equal( prevState[ otherSiteId ] );
 	} );
 } );
+
