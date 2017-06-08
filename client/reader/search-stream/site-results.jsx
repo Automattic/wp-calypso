83a0e08a22544f23fd4b6b5c6b224e78bd8275a6
Jake F - @samouri <samouri@users.noreply.github.com>
2017-06-08 14:30:04 +0800

    compat for site-results

--- client/reader/search-stream/site-results.jsx
+++ client/reader/search-stream/site-results.jsx
@@ -9,21 +9,24 @@ import { connect } from 'react-redux';
 /**
  * Internal Dependencies
  */
-import { getReaderFeedsForQuery } from 'state/selectors';
+import { getReaderFeedsCountForQuery, getReaderFeedsForQuery } from 'state/selectors';
 import QueryReaderFeedsSearch from 'components/data/query-reader-feeds-search';
 import { requestFeedSearch } from 'state/reader/feed-searches/actions';
 import ReaderInfiniteStream from 'components/reader-infinite-stream';
 import { SORT_BY_RELEVANCE, SORT_BY_LAST_UPDATED } from 'state/reader/feed-searches/actions';
 import { siteRowRenderer } from 'components/reader-infinite-stream/row-renderers';
+import withWidth from 'lib/with-width';
 
 const pickSort = sort => ( sort === 'date' ? SORT_BY_LAST_UPDATED : SORT_BY_RELEVANCE );
 
-class SitesResults extends React.Component {
+class SiteResults extends React.Component {
 	static propTypes = {
 		query: PropTypes.string,
 		sort: PropTypes.oneOf( [ SORT_BY_LAST_UPDATED, SORT_BY_RELEVANCE ] ),
 		requestFeedSearch: PropTypes.func,
 		searchResults: PropTypes.array,
+		searchResultsCount: PropTypes.number,
+		width: PropTypes.number.isRequired,
 	};
 
 	fetchNextPage = offset => {
@@ -35,8 +38,10 @@ class SitesResults extends React.Component {
 		} );
 	};
 
+	hasNextPage = offset => offset < this.props.searchResultsCount;
+
 	render() {
-		const { query, searchResults } = this.props;
+		const { query, searchResults, width } = this.props;
 
 		return (
 			<div>
@@ -47,8 +52,10 @@ class SitesResults extends React.Component {
 				/>
 				<ReaderInfiniteStream
 					items={ searchResults || [ {}, {}, {}, {}, {} ] }
+					width={ width }
 					showLastUpdatedDate={ false }
 					fetchNextPage={ this.fetchNextPage }
+					hasNextPage={ this.hasNextPage }
 					rowRenderer={ siteRowRenderer }
 				/>
 			</div>
@@ -62,6 +69,7 @@ export default connect(
 			state,
 			{ query: ownProps.query, excludeFollowed: false, sort: pickSort( ownProps.sort ) },
 		),
+		searchResultsCount: getReaderFeedsCountForQuery( state, ownProps.query ),
 	} ),
 	{ requestFeedSearch },
-)( localize( SitesResults ) );
+)( localize( withWidth( SiteResults ) ) );
