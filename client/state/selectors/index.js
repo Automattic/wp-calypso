c758d85b46f196828ed9d688a799478f13a40d20
Steve Seear <steve.seear@gmail.com>
2017-06-08 18:31:27 +0100

    getRewindStartDate() selector and tests

--- client/state/selectors/index.js
+++ client/state/selectors/index.js
@@ -42,6 +42,7 @@ export getJetpackSettingsSaveError from './get-jetpack-settings-save-error';
 export getJetpackSettingsSaveRequestStatus from './get-jetpack-settings-save-request-status';
 export getReaderFollowsLastSyncTime from './get-reader-follows-last-sync-time';
 export getRewindStatusError from './get-rewind-status-error';
+export getRewindStartDate from './get-rewind-start-date';
 export getMagicLoginCurrentView from './get-magic-login-current-view';
 export getMagicLoginEmailAddressFormInput from './get-magic-login-email-address-form-input';
 export getMagicLoginEmailAddressFormInputIsValid from './get-magic-login-email-address-form-input-is-valid';
