8a26dd2e25b226f21f99a1b6a7a953585c70a60a
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-06-08 19:19:40 +0200

    Login: Extract locale suggestion list item to a new component

--- client/components/locale-suggestions/index.jsx
+++ client/components/locale-suggestions/index.jsx
@@ -1,17 +1,17 @@
 /**
  * External dependencies
  */
-import i18n from 'i18n-calypso';
 import React, { Component } from 'react';
-import { assign } from 'lodash';
+import { getLocaleSlug } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-import i18nUtils from 'lib/i18n-utils';
-import switchLocale from 'lib/i18n-utils/switch-locale';
+import { addLocaleToPath } from 'lib/i18n-utils';
+import LocaleSuggestionListItem from './list-item';
 import LocaleSuggestionStore from 'lib/locale-suggestions';
 import Notice from 'components/notice';
+import switchLocale from 'lib/i18n-utils/switch-locale';
 
 class LocaleSuggestions extends Component {
 	constructor( props ) {
@@ -46,27 +46,8 @@ class LocaleSuggestions extends Component {
 		this.setState( { dismissed: true } );
 	};
 
-	handleLocaleSuggestionClick( locale, event ) {
-		// TODO: record analytics event here
-		this.reloadIfLanguageDirectionChanges( locale, event );
-	}
-
-	hasLocaleDirectionChanged( locale ) {
-		const localeData = assign( {}, i18nUtils.getLanguage( locale ) ),
-			currentLocaleData = assign( {}, i18nUtils.getLanguage( i18n.getLocaleSlug() ) );
-
-		return localeData.rtl !== currentLocaleData.rtl;
-	}
-
-	reloadIfLanguageDirectionChanges( locale, event ) {
-		if ( this.hasLocaleDirectionChanged( locale ) ) {
-			event.preventDefault();
-			window.location = this.getPathWithLocale( locale );
-		}
-	}
-
 	getPathWithLocale = locale => {
-		return i18nUtils.addLocaleToPath( this.props.path, locale );
+		return addLocaleToPath( this.props.path, locale );
 	};
 
 	updateLocales = () => {
@@ -79,27 +60,22 @@ class LocaleSuggestions extends Component {
 		}
 
 		const usersOtherLocales = this.state.locales.filter( function( locale ) {
-			return locale.locale !== i18n.getLocaleSlug();
+			return locale.locale !== getLocaleSlug();
 		} );
 
 		if ( usersOtherLocales.length === 0 ) {
 			return null;
 		}
 
-		const localeMarkup = usersOtherLocales.map( function( locale, index ) {
+		const localeMarkup = usersOtherLocales.map( locale => {
 			return (
-				<div className="locale-suggestions__list-item" key={ 'locale-' + index } dir="auto">
-					{ locale.availability_text }
-					<a
-						href={ this.getPathWithLocale( locale.locale ) }
-						onClick={ this.handleLocaleSuggestionClick.bind( this, locale.locale ) }
-						className="locale-suggestions__locale-link"
-					>
-						{ locale.name }
-					</a>
-				</div>
+				<LocaleSuggestionListItem
+					key={ 'locale-' + locale.locale }
+					locale={ locale }
+					path={ this.getPathWithLocale( locale.locale ) }
+				/>
 			);
-		}, this );
+		} );
 
 		return (
 			<div className="locale-suggestions">
---
+++ client/components/locale-suggestions/list-item.jsx
@@ -0,0 +1,55 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { assign } from 'lodash';
+import { getLocaleSlug } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import { getLanguage } from 'lib/i18n-utils';
+
+class LocaleSuggestionsListItem extends Component {
+	static propTypes = {
+		locale: PropTypes.object.isRequired,
+		path: PropTypes.string.isRequired,
+	};
+
+	handleLocaleSuggestionClick = event => {
+		const { locale, path } = this.props;
+
+		if ( this.hasLocaleDirectionChanged( locale ) ) {
+			event.preventDefault();
+			window.location = path;
+		}
+
+		// TODO: record analytics event here
+	};
+
+	hasLocaleDirectionChanged( locale ) {
+		const localeData = assign( {}, getLanguage( locale ) ),
+			currentLocaleData = assign( {}, getLanguage( getLocaleSlug() ) );
+
+		return localeData.rtl !== currentLocaleData.rtl;
+	}
+
+	render() {
+		const { locale, path } = this.props;
+
+		return (
+			<div className="locale-suggestions__list-item" dir="auto">
+				{ locale.availability_text }
+				<a
+					href={ path }
+					onClick={ this.handleLocaleSuggestionClick }
+					className="locale-suggestions__locale-link"
+				>
+					{ locale.name }
+				</a>
+			</div>
+		);
+	}
+}
+
+export default LocaleSuggestionsListItem;
