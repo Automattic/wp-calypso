e90447d0f89a40b9b9354e1d08ac413be97f0a5c
Steve Seear <steve.seear@gmail.com>
2017-06-08 14:07:55 +0100

    isRewindActive() selector + tests

---
+++ client/state/selectors/is-rewind-active.js
@@ -0,0 +1,16 @@
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+/**
+ * Returns true if the Activity Log Rewind feature
+ * is enabled for a site.
+ *
+ * @param {Object} state Global state tree
+ * @param {number|string} siteId the site ID
+ * @return {boolean} true if rewind is enabled
+ */
+export default function isRewindActive( state, siteId ) {
+	return get( state.activityLog.rewindStatus, [ siteId, 'active' ], false );
+}
