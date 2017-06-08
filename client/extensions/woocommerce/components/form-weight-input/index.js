b4a1e836aaa0e9cad43fe55b64ba7d64fb2fb1e8
Justin Shreve <justin.shreve@gmail.com>
2017-06-08 09:31:35 -0700

    WooCommerce: Fix dimensions inputs by loading the correct unit and add actions, reducers, and selectors for fetching products settings (#14827)
    * Adds actions, reducers, and selectors for fetching products settings. Includes two selectors for getting weight and dimensions units.
    
    * Add README
    
    * Bump API version
    
    * Fix stray comma
    
    * Fix the weight and dimensions inputs by loading the correct unit. (#14834)

---
+++ client/extensions/woocommerce/components/form-weight-input/index.js
@@ -0,0 +1,88 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import classNames from 'classnames';
+import { noop } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import FormTextInputWithAffixes from 'components/forms/form-text-input-with-affixes';
+
+import { getSelectedSiteWithFallback } from 'woocommerce/state/sites/selectors';
+import { getWeightUnitSetting } from 'woocommerce/state/sites/settings/products/selectors';
+import { fetchSettingsProducts } from 'woocommerce/state/sites/settings/products/actions';
+
+class FormWeightInput extends Component {
+
+	static propTypes = {
+		className: PropTypes.string,
+		value: PropTypes.string.isRequired,
+		onChange: PropTypes.func.isRequired,
+		noWrap: PropTypes.bool,
+	};
+
+	static defaultProps = {
+		value: '',
+		className: '',
+		onChange: noop,
+		noWrap: false,
+	}
+
+	componentDidMount() {
+		const { siteId } = this.props;
+
+		if ( siteId ) {
+			this.props.fetchSettingsProducts( siteId );
+		}
+	}
+
+	componentWillReceiveProps( newProps ) {
+		if ( newProps.siteId !== this.props.siteId ) {
+			this.props.fetchSettingsProducts( newProps.siteId );
+		}
+	}
+
+	render() {
+		const { className, weightUnit, value, onChange, noWrap } = this.props;
+		const classes = classNames( 'form-weight-input', className, { 'no-wrap': noWrap } );
+
+		return (
+			<FormTextInputWithAffixes
+				name="weight"
+				min="0"
+				suffix={ weightUnit }
+				type="number"
+				value={ value || '' }
+				onChange={ onChange }
+				className={ classes }
+				size="4"
+			/>
+		);
+	}
+}
+
+function mapStateToProps( state ) {
+	const site = getSelectedSiteWithFallback( state );
+	const weightUnitSetting = site && getWeightUnitSetting( state, site.ID );
+	const weightUnit = weightUnitSetting && weightUnitSetting.value || 'lbs';
+
+	return {
+		siteId: site && site.ID,
+		weightUnit,
+	};
+}
+
+function mapDispatchToProps( dispatch ) {
+	return bindActionCreators(
+		{
+			fetchSettingsProducts,
+		},
+		dispatch
+	);
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )( FormWeightInput );
