7114157c31d4198ec06260d5f01c93d681d6fd8b
Jonathan Belcher <jonathan.belcher@automattic.com>
2017-06-08 17:10:50 -0400

    Adds toggle to be used with editing payment methods (#14858)
    * Adds toggle to be used with editing payment methods

---
+++ client/extensions/woocommerce/app/settings/payments/payment-method-edit-form-toggle.js
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import { omit } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import FormToggle from 'components/forms/form-toggle';
+
+const PaymentMethodEditFormToggle = ( props ) => {
+	const { checked, name, onChange } = props;
+	const onChangeHandler = () => {
+		const fakeEvent = {
+			target: {
+				name,
+				value: checked ? 'no' : 'yes'
+			}
+		};
+		onChange( fakeEvent );
+	};
+
+	return (
+		<FormToggle
+			{ ...omit( props, [ 'onChange' ] ) }
+			checked={ checked }
+			name={ name }
+			onChange={ onChangeHandler } />
+	);
+};
+
+PaymentMethodEditFormToggle.propTypes = {
+	checked: PropTypes.bool,
+	name: PropTypes.string,
+	onChange: PropTypes.func,
+};
+
+export default PaymentMethodEditFormToggle;
