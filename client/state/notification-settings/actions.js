42082fbf6f231dcd6c39bcc2e3390fe5ccf76100
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2017-06-09 15:14:41 -0300

    Notification Settings: data-layer skeleton (#14905)
    * Notification Settings: adds request and receive action types
    * Notification Settings: adds request and receive action creators
    * Notification Settings: adds data layer skeleton for notification settings

---
+++ client/state/notification-settings/actions.js
@@ -0,0 +1,24 @@
+/**
+ * Internal dependencies
+ */
+import {
+	NOTIFICATION_SETTINGS_REQUEST,
+	NOTIFICATION_SETTINGS_UPDATE,
+} from 'state/action-types';
+
+/**
+ * Returns an action object to signal the request of the current user notification settings.
+ * @returns {Object} action object
+ */
+export const requestNotificationSettings = () => ( { type: NOTIFICATION_SETTINGS_REQUEST } );
+
+/**
+ * Returns an action object to signal the arrival of the requested notification settings.
+ *
+ * @param  {Object} settings User Notification Settings
+ * @return {Object}          action object
+ */
+export const updateNotificationSettings = ( { settings } ) => ( {
+	type: NOTIFICATION_SETTINGS_UPDATE,
+	settings
+} );
