684f9bd8159e220064506e727a783a8d02891d34
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-06-09 14:58:49 -0300

    Notification Settings: removes redundant calls to describe

--- client/state/data-layer/wpcom/me/notification/settings/test/index.js
+++ client/state/data-layer/wpcom/me/notification/settings/test/index.js
@@ -15,51 +15,47 @@ import {
 	handleError,
 } from '../';
 
-describe( 'wpcom-api', () => {
-	describe( 'notification settings', () => {
-		describe( '#requestNotificationSettings()', () => {
-			it( 'should dispatch HTTP request to the user notification settings endpoint', () => {
-				const dispatch = spy();
+describe( '#requestNotificationSettings()', () => {
+	it( 'should dispatch HTTP request to the user notification settings endpoint', () => {
+		const dispatch = spy();
 
-				requestNotificationSettings( { dispatch } );
+		requestNotificationSettings( { dispatch } );
 
-				expect( dispatch ).to.have.been.calledOnce;
-				expect( dispatch ).to.have.been.calledWith( http( {
-					apiVersion: '1.1',
-					method: 'GET',
-					path: '/me/notifications/settings'
-				} ) );
-			} );
+		expect( dispatch ).to.have.been.calledOnce;
+		expect( dispatch ).to.have.been.calledWith( http( {
+			apiVersion: '1.1',
+			method: 'GET',
+			path: '/me/notifications/settings'
+		} ) );
+	} );
+} );
+
+describe( '#updateSettings()', () => {
+	it( 'should dispatch notification settings', () => {
+		const dispatch = spy();
+
+		updateSettings( { dispatch }, null, null, {} );
+
+		expect( dispatch ).to.have.been.calledOnce;
+		expect( dispatch ).to.have.been.calledWith( {
+			type: NOTIFICATION_SETTINGS_UPDATE,
+			settings: {}
 		} );
+	} );
+} );
 
-		describe( '#updateSettings', () => {
-			it( 'should dispatch notification settings', () => {
-				const dispatch = spy();
+describe( '#handleError()', () => {
+	it( 'should dispatch error notice', () => {
+		const dispatch = spy();
 
-				updateSettings( { dispatch }, null, null, {} );
+		handleError( { dispatch } );
 
-				expect( dispatch ).to.have.been.calledOnce;
-				expect( dispatch ).to.have.been.calledWith( {
-					type: NOTIFICATION_SETTINGS_UPDATE,
-					settings: {}
-				} );
-			} );
-		} );
-
-		describe( '#handleError', () => {
-			it( 'should dispatch error notice', () => {
-				const dispatch = spy();
-
-				handleError( { dispatch } );
-
-				expect( dispatch ).to.have.been.calledOnce;
-				expect( dispatch ).to.have.been.calledWithMatch( {
-					type: NOTICE_CREATE,
-					notice: {
-						status: 'is-error'
-					}
-				} );
-			} );
+		expect( dispatch ).to.have.been.calledOnce;
+		expect( dispatch ).to.have.been.calledWithMatch( {
+			type: NOTICE_CREATE,
+			notice: {
+				status: 'is-error'
+			}
 		} );
 	} );
 } );
