fec00f9538d9d7ac3ec4731dcc772ce6abfb7939
Kelly Dwan <ryelle@users.noreply.github.com>
2017-06-12 17:48:42 +0200

    Update build-jetpack command

---
+++ bin/copy-files.sh
@@ -0,0 +1,8 @@
+JETPACK_DIR="/Users/ryelle/Sites/woo/content/plugins/jetpack-ui"
+
+find ./public -iregex ".*manifest.*\.js" !  -iregex ".*\.m\.js" -exec mv {} $JETPACK_DIR/public/manifest.js \;
+find ./public -iregex ".*build.*\.js" !  -iregex ".*\.m\.js" -exec mv {} $JETPACK_DIR/public/build.js \;
+find ./public -iregex ".*vendor.*\.js" !  -iregex ".*\.m\.js" -exec mv {} $JETPACK_DIR/public/vendor.js \;
+cp ./public/style.css $JETPACK_DIR/public/
+
+# npm run -s build-server && NODE_PATH=$NODE_PATH:server:client:. CALYPSO_ENV=jetpack node server/bundler/bin/bundler.js && ./bin/copy-files.sh
