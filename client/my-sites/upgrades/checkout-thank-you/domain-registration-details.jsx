5bf6413490e9d28c211ab292ab48ef0893427ce0
Taegon Kim <gonom9@gmail.com>
2017-06-12 16:11:20 +0900

    Update the look of email verfication notification.

--- client/my-sites/upgrades/checkout-thank-you/domain-registration-details.jsx
+++ client/my-sites/upgrades/checkout-thank-you/domain-registration-details.jsx
@@ -1,12 +1,13 @@
 /**
  * External dependencies
  */
-import React from 'react';
-import i18n from 'i18n-calypso';
+import React, { Component, PropTypes } from 'react';
 
 /**
  * Internal dependencies
  */
+import { localize } from 'i18n-calypso';
+import Gauge from 'components/gauge';
 import { getDomainManagementUrl } from './utils';
 import GoogleAppsDetails from './google-apps-details';
 import { isGoogleApps } from 'lib/products-values';
@@ -14,70 +15,82 @@ import PurchaseDetail from 'components/purchase-detail';
 import supportUrls from 'lib/url/support';
 import { WaitTime, CheckEmailsDesktop } from './icons';
 
-const DomainRegistrationDetails = ( { selectedSite, domain, purchases } ) => {
-	const googleAppsWasPurchased = purchases.some( isGoogleApps ),
-		domainContactEmailVerified = purchases.some( purchase => purchase.isEmailVerified ),
-		hasOtherPrimaryDomain = selectedSite.options && selectedSite.options.is_mapped_domain && selectedSite.domain !== domain;
+export class DomainRegistrationDetails extends Component {
+	static propTypes = {
+		domain: PropTypes.string.isRequired,
+		purchases: PropTypes.array.isRequired,
+		selectedSite: PropTypes.oneOfType( [
+			PropTypes.bool,
+			PropTypes.object
+		] ).isRequired
+	};
 
-	return (
-		<div>
-			<div className="checkout-thank-you__domain-registration-details-compact">
-				{ ! domainContactEmailVerified && (
-					<PurchaseDetail
-						icon="mail"
-						svgIcon={ <CheckEmailsDesktop /> }
-						title={ i18n.translate( 'Verify your email address' ) }
-						description={ i18n.translate( 'We sent you an email with a request to verify your new domain. Unverified domains may be suspended.' ) }
-						buttonText={ i18n.translate( 'Learn more about verifying your domain' ) }
-						href={ supportUrls.EMAIL_VALIDATION_AND_VERIFICATION }
-						target="_blank"
-						rel="noopener noreferrer"
-						requiredText={ i18n.translate( 'Important! Your action is required.' ) }
-						isRequired />
-				) }
+	renderEmailVerfication() {
+		const { translate } = this.props;
 
-				{ googleAppsWasPurchased && <GoogleAppsDetails isRequired /> }
-			</div>
-
+		return [
+			<Gauge key="gauge" percentage={ 90 } metric={ translate( 'Almost Completed' ) } width={ 118 } />,
+			<em key="message" className="checkout-thank-you__action-required">
+				{ translate( 'Important! Your action is required.' ) }
+			</em>,
 			<PurchaseDetail
-				icon="time"
-				svgIcon={ <WaitTime /> }
-				title={ i18n.translate( 'When will it be ready?', { comment: '"it" refers to a domain' } ) }
-				description={ i18n.translate( 'Your domain should start working immediately, but may be unreliable during the first 72 hours.' ) }
-				buttonText={ i18n.translate( 'Learn more about your domain' ) }
-				href={ supportUrls.REGISTER_DOMAIN }
+				key="detail"
+				icon="mail"
+				svgIcon={ <CheckEmailsDesktop /> }
+				title={ translate( 'Verify your email address' ) }
+				description={ translate( 'We sent you an email with a request to verify your new domain. Unverified domains may be suspended.' ) } // eslint-disable-line max-len
+				buttonText={ translate( 'Learn more about verifying your domain' ) }
+				href={ supportUrls.EMAIL_VALIDATION_AND_VERIFICATION }
 				target="_blank"
-				rel="noopener noreferrer" />
+				rel="noopener noreferrer"
+				isRequired />
+		];
+	}
 
-			{ hasOtherPrimaryDomain && (
+	render() {
+		const { selectedSite, domain, purchases, translate } = this.props;
+		const googleAppsWasPurchased = purchases.some( isGoogleApps ),
+			domainContactEmailVerified = purchases.some( purchase => purchase.isEmailVerified ),
+			hasOtherPrimaryDomain = selectedSite.options && selectedSite.options.is_mapped_domain && selectedSite.domain !== domain;
+
+		return (
+			<div>
+				<div className="checkout-thank-you__domain-registration-details-compact">
+					{ domainContactEmailVerified && this.renderEmailVerfication() }
+					{ googleAppsWasPurchased && <GoogleAppsDetails isRequired /> }
+				</div>
+
 				<PurchaseDetail
-					icon="globe"
-					title={ i18n.translate( 'Your primary domain' ) }
-					description={
-						i18n.translate(
-							'Your existing domain, {{em}}%(domain)s{{/em}}, is the domain visitors see when they visit your site. ' +
-							'All other custom domains redirect to the primary domain.',
-							{
-								args: { domain: selectedSite.domain },
-								components: { em: <em /> }
-							}
-						)
-					}
-					buttonText={ i18n.translate( 'Change primary domain' ) }
-					href={ getDomainManagementUrl( selectedSite, domain ) } />
-			) }
-		</div>
-	);
-};
+					icon="time"
+					svgIcon={ <WaitTime /> }
+					title={ translate( 'When will it be ready?', { comment: '"it" refers to a domain' } ) }
+					description={ translate( 'Your domain should start working immediately, but may be unreliable during the first 72 hours.' ) } // eslint-disable-line max-len
+					buttonText={ translate( 'Learn more about your domain' ) }
+					href={ supportUrls.REGISTER_DOMAIN }
+					target="_blank"
+					rel="noopener noreferrer" />
 
-DomainRegistrationDetails.propTypes = {
-	domain: React.PropTypes.string.isRequired,
-	purchases: React.PropTypes.array.isRequired,
-	selectedSite: React.PropTypes.oneOfType( [
-		React.PropTypes.bool,
-		React.PropTypes.object
-	] ).isRequired
-};
+				{ hasOtherPrimaryDomain && (
+					<PurchaseDetail
+						icon="globe"
+						title={ translate( 'Your primary domain' ) }
+						description={
+							translate(
+								'Your existing domain, {{em}}%(domain)s{{/em}}, is the domain visitors see when they visit your site. ' +
+								'All other custom domains redirect to the primary domain.',
+								{
+									args: { domain: selectedSite.domain },
+									components: { em: <em /> }
+								}
+							)
+						}
+						buttonText={ translate( 'Change primary domain' ) }
+						href={ getDomainManagementUrl( selectedSite, domain ) } />
+				) }
+			</div>
+		);
+	}
+}
 
-export default DomainRegistrationDetails;
+export default localize( DomainRegistrationDetails );
 
