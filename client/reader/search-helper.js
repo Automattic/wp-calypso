14ebeb5d7cd56ab2a4e7d5e8cb31a8e5935cc713
Ben Lowery <ben.lowery@automattic.com>
2017-06-12 19:50:47 -0400

    limit to non production

--- client/reader/search-helper.js
+++ client/reader/search-helper.js
@@ -3,10 +3,17 @@
  */
 import querystring from 'querystring';
 
+/**
+ * Internal Dependencies
+ */
+import config from 'config';
+
 let algorithm;
 
 // try to pick the default algorithm up from the querystring
-if ( typeof window !== 'undefined' && window.location.search ) {
+if (
+	config( 'env_id' ) !== 'production' && typeof window !== 'undefined' && window.location.search
+) {
 	const query = querystring.parse( window.location.search.substring( 1 ) );
 	if ( query.algorithm ) {
 		algorithm = query.algorithm;
