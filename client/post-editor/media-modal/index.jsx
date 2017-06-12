016b6d14d3ac6490b29404d16f6f01ef493ba5b5
John Godley <johng75@gmail.com>
2017-06-12 16:24:26 +0100

    Changing media library source actions
    Change the library source when the dropdown is clicked

--- client/post-editor/media-modal/index.jsx
+++ client/post-editor/media-modal/index.jsx
@@ -302,7 +302,19 @@ export class EditorMediaModal extends Component {
 		}
 	};
 
-	onSourceChange = () => {
+	onSourceChange = ( source, cb ) => {
+		const { site } = this.props;
+
+		this.setState( {
+			source,
+			search: undefined,
+		}, () => {
+			MediaActions.changeSource( site.ID, source );
+
+			if ( cb ) {
+				cb();
+			}
+		} );
 	};
 
 	onClose = () => {
