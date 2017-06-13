99f814559413c95081c2bfb708be02bfee5214ca
Jon Surrell <jon.surrell@automattic.com>
2017-06-13 15:34:52 +0200

    Jetpack bundle: styles (#15002)
    * Build Jetpack styles
    
    * fix wrapper class

--- bin/copy-files.sh
+++ bin/copy-files.sh
@@ -3,6 +3,8 @@ JETPACK_DIR="/home/javi/work/jetpackSandbox/wp-content/plugins/jetpack-ui"
 find ./public -iregex ".*manifest.*\.js" !  -iregex ".*\.m\.js" -exec mv {} $JETPACK_DIR/public/manifest.js \;
 find ./public -iregex ".*build.*\.js" !  -iregex ".*\.m\.js" -exec mv {} $JETPACK_DIR/public/build.js \;
 find ./public -iregex ".*vendor.*\.js" !  -iregex ".*\.m\.js" -exec mv {} $JETPACK_DIR/public/vendor.js \;
-cp ./public/style.css $JETPACK_DIR/public/
+cp ./public/jetpack.css $JETPACK_DIR/public/
+cp ./public/jetpack-debug.css $JETPACK_DIR/public/
+cp ./public/jetpack-debug.css.map $JETPACK_DIR/public/
 
 # npm run -s build-server && NODE_PATH=$NODE_PATH:server:client:. CALYPSO_ENV=jetpack node server/bundler/bin/bundler.js && ./bin/copy-files.sh
