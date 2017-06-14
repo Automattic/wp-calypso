6df7417f91e8e70cd79923b7b5c2e6d71da9bc1a
Kevin Killingsworth <kevin@redfenix.com>
2017-06-14 11:54:48 -0500

    Move action thunk from ui/products to data-layer
    This moves the product ui action list  creation code that was relying
    on a thunk to get state/dispatch to the appropriate place as a
    handler in the data layer.

--- client/extensions/woocommerce/state/data-layer/index.js
+++ client/extensions/woocommerce/state/data-layer/index.js
@@ -6,10 +6,12 @@ import { addHandlers } from 'state/data-layer/extensions-middleware';
 import actionList from './action-list';
 import products from './products';
 import request from './request';
+import ui from './ui';
 import debugFactory from 'debug';
 
 const debug = debugFactory( 'woocommerce:errors' );
-const handlers = mergeHandlers( actionList, products, request );
+
+const handlers = mergeHandlers( actionList, products, request, ui );
 
 export default function installActionHandlers() {
 	const added = addHandlers( 'woocommerce', handlers );
---
+++ client/extensions/woocommerce/state/data-layer/ui/index.js
@@ -0,0 +1,8 @@
+/**
+ * Internal dependencies
+ */
+import { mergeHandlers } from 'state/action-watchers/utils';
+import products from './products';
+
+export default mergeHandlers( products );
+
