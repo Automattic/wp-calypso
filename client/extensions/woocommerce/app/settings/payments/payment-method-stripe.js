3443d28545de1635c19bb5b565ab7c96504b238b
Jonathan Belcher <belcherj@gmail.com>
2017-06-14 16:25:47 -0400

    More updates

--- client/extensions/woocommerce/app/settings/payments/payment-method-stripe.js
+++ client/extensions/woocommerce/app/settings/payments/payment-method-stripe.js
@@ -10,6 +10,8 @@ import { localize } from 'i18n-calypso';
 import Button from 'components/button';
 import FormFieldset from 'components/forms/form-fieldset';
 import FormLabel from 'components/forms/form-label';
+import FormLegend from 'components/forms/form-legend';
+import FormRadio from 'components/forms/form-radio';
 import FormTextInput from 'components/forms/form-text-input';
 import PaymentMethodEditFormToggle from './payment-method-edit-form-toggle';
 
@@ -56,18 +58,48 @@ class PaymentMethodStripe extends Component {
 	render() {
 		const { method, translate } = this.props;
 		return (
-			<div className="payments__method-edit-pane">
+			<div className="payments__method-edit-fields">
 				<FormFieldset className="payments__method-edit-field-container">
 					<FormLabel>Enabled</FormLabel>
 					{ this.renderEnabledField( method.settings.enabled.value ) }
 				</FormFieldset>
 				<FormFieldset className="payments__method-edit-field-container">
-					<FormLabel>Sort Code</FormLabel>
-					{ this.renderEnabledField( method.enabled ) }
+					<FormLegend>{ translate( 'Payment authorization' ) }</FormLegend>
+					<FormLabel>
+						<FormRadio
+							name="capture"
+							value="yes"
+							checked={ 'yes' === method.settings.capture.value }
+							onChange={ this.onEditFieldHandler } />
+						<span>{ translate( 'Authorize and charge the customers credit card' ) }</span>
+					</FormLabel>
+					<FormLabel>
+						<FormRadio
+							name="capture"
+							value="no"
+							checked={ 'no' === method.settings.capture.value }
+							onChange={ this.onEditFieldHandler } />
+						<span>{ translate( 'Only authorize the customers credit card' ) }</span>
+						<span>
+							{ translate(
+								'You will then have to manually capture the ' +
+								'payment'
+							) }
+						</span>
+					</FormLabel>
 				</FormFieldset>
 				<FormFieldset className="payments__method-edit-field-container">
-					<FormLabel>Account Number</FormLabel>
-					{ this.renderEnabledField( method.enabled ) }
+					<FormLabel>Use ApplePay</FormLabel>
+					<PaymentMethodEditFormToggle
+						checked={ method.settings.apple_pay.value === 'yes' ? true : false }
+						name="apple_pay"
+						onChange={ this.onEditFieldHandler } />
+					<span>
+						{ translate(
+							'By using ApplePay you aggree to Stripe and ' +
+							'Apple\'s terms of service'
+						) }
+					</span>
 				</FormFieldset>
 				<Button primary onClick={ this.onSaveHandler }>
 					{ translate( 'Save' ) }
