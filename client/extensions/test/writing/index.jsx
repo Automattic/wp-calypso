9288ae47a49a6e48d540051afb6270d1fa14f8d4
oskosk <oskosk@gmail.com>
2017-06-14 16:22:07 +0200

    Add MediaSettings card to  writing extension

--- client/extensions/test/writing/index.jsx
+++ client/extensions/test/writing/index.jsx
@@ -11,14 +11,26 @@ import Button from 'components/button';
 import Card from 'components/card';
 import Banner from 'components/banner';
 import FoldableCard from 'components/foldable-card';
+import MediaSettings from 'my-sites/site-settings/media-settings';
+import QueryJetpackModules from 'components/data/query-jetpack-modules';
+import wrapSettingsForms from 'my-sites/site-settings/wrap-settings-form';
 
-export default class JetpackWriting extends PureComponent {
+class JetpackWriting extends PureComponent {
 	render() {
+		const { fields, handleAutosavingToggle, isRequestingSettings, isSavingSettings, onChangeField, } = this.props;
 		return (
 			<Main className="writing jetpack-ui__writing">
 				<Card>
 					<h1>Writing</h1>
-					<p>Settings, or something, would go here.</p>
+					<QueryJetpackModules siteId={ 0 } />
+					<MediaSettings
+						handleAutosavingToggle={ handleAutosavingToggle }
+						onChangeField={ onChangeField }
+						isSavingSettings={ isSavingSettings }
+						isRequestingSettings={ isRequestingSettings }
+						fields={ fields }
+						siteId={ 0 }
+					/>
 					<Button onClick={ this.props.loadPage( '/test/discussion' ) }>Discussion</Button>
 				</Card>
 				<Banner title="WFT" description={ 'WTF!! A banner that works both in calypso and wp-admin?!?!?' } />
@@ -27,3 +39,5 @@ export default class JetpackWriting extends PureComponent {
 		);
 	}
 }
+
+export default wrapSettingsForms( () => ( {} ) )( JetpackWriting );
