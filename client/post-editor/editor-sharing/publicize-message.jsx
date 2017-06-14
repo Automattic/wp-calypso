8e9431a8189dbc2ec317682dd9b56c70a50c41cc
retrofox <rdsuarez@gmail.com>
2017-06-14 10:15:34 -0300

    editor-sharing: propagate `disabled` property

--- client/post-editor/editor-sharing/publicize-message.jsx
+++ client/post-editor/editor-sharing/publicize-message.jsx
@@ -17,6 +17,7 @@ export default React.createClass( {
 	displayName: 'PublicizeMessage',
 
 	propTypes: {
+		message: React.PropTypes.bool,
 		message: React.PropTypes.string,
 		preview: React.PropTypes.string,
 		acceptableLength: React.PropTypes.number,
@@ -26,6 +27,7 @@ export default React.createClass( {
 
 	getDefaultProps: function() {
 		return {
+			disabled: false,
 			message: '',
 			acceptableLength: 140,
 			requireCount: false,
@@ -66,6 +68,7 @@ export default React.createClass( {
 		if ( this.props.requireCount ) {
 			return (
 				<CountedTextarea
+					disabled={ this.props.disabled }
 					value={ this.props.message }
 					placeholder={ this.props.preview }
 					countPlaceholderLength={ true }
@@ -80,6 +83,7 @@ export default React.createClass( {
 		} else {
 			return (
 				<FormTextarea
+					disabled={ this.props.disabled }				
 					value={ this.props.message }
 					placeholder={ this.props.preview }
 					onChange={ this.onChange }
