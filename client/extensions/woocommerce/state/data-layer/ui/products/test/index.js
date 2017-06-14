486775a2959493259cfa3f164c4849ce23009afc
Kevin Killingsworth <kevin@redfenix.com>
2017-06-14 11:54:48 -0500

    Move action thunk from ui/products to data-layer
    This moves the product ui action list  creation code that was relying
    on a thunk to get state/dispatch to the appropriate place as a
    handler in the data layer.

--- client/extensions/woocommerce/state/data-layer/ui/products/test/index.js
+++ client/extensions/woocommerce/state/data-layer/ui/products/test/index.js
@@ -6,9 +6,7 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import {
-	makeProductActionList,
-} from '../actions';
+import { makeProductActionList, } from '../';
 import { actionListStepSuccess, actionListStepFailure } from 'woocommerce/state/action-list/actions';
 import { createProduct } from 'woocommerce/state/sites/products/actions';
 
--- client/extensions/woocommerce/state/ui/products/actions.js
+++ client/extensions/woocommerce/state/ui/products/actions.js
@@ -1,23 +1,10 @@
 /**
- * External dependencies
- */
-import { translate } from 'i18n-calypso';
-
-/**
  * Internal dependencies
  */
-// TODO: Remove this when product edits have siteIds.
-import { getSelectedSiteId } from 'state/ui/selectors';
-import { getAllProductEdits, getProductWithLocalEdits } from './selectors';
-import { createProduct } from 'woocommerce/state/sites/products/actions';
 import {
-	actionListCreate,
-	actionListStepSuccess,
-	actionListStepFailure
-} from 'woocommerce/state/action-list/actions';
-import {
-	WOOCOMMERCE_PRODUCT_EDIT,
+	WOOCOMMERCE_PRODUCT_ACTION_LIST_CREATE,
 	WOOCOMMERCE_PRODUCT_ATTRIBUTE_EDIT,
+	WOOCOMMERCE_PRODUCT_EDIT,
 } from 'woocommerce/state/action-types';
 
 export function editProduct( siteId, product, data ) {
@@ -43,70 +30,11 @@ export function editProductAttribute( siteId, product, attribute, data ) {
  * Creates an action list to save product-related edits.
  *
  * Saves products, variations, and product categories.
- * @return {Function} Action thunk to be dispatched.
+ * @return {Function} action
  */
 export function createProductActionList() {
-	return ( dispatch, getState ) => {
-		const rootState = getState();
-		const actionList = makeProductActionList( rootState );
-
-		return actionListCreate( actionList );
+	return {
+		type: WOOCOMMERCE_PRODUCT_ACTION_LIST_CREATE,
 	};
-}
-
-/**
- * Makes a product Action List object based on current product edits.
- *
- * For internal and testing use only.
- * @private
- * @param {Object} rootState The root calypso state.
- * @param {Number} [siteId=selected site] The siteId for the Action ListDO: Remove this when edits have siteIds.)
- * @param {Object} [productEdits=all edits] The product edits to be included in the Action List
- * @return {Object} An Action List object.
- */
-export function makeProductActionList(
-	rootState,
-	siteId = getSelectedSiteId( rootState ),
-	productEdits = getAllProductEdits( rootState )
-) {
-	const steps = [];
-
-	// TODO: sequentially go through edit state and create steps.
-	/* TODO: Add category API calls before product.
-	...categories.creates
-	...categories.updates
-	...categories.deletes
-	*/
-
-	if ( productEdits && productEdits.creates ) {
-		// Each create gets its own step.
-		// TODO: Consider making these parallel actions.
-		productEdits.creates.forEach( ( product ) => {
-			const stepIndex = steps.length;
-			const description = translate( 'Creating product: ' ) + product.name;
-
-			const action = createProduct(
-				siteId,
-				product,
-				actionListStepSuccess( stepIndex ),
-				actionListStepFailure( stepIndex, 'UNKNOWN' ) // error will be set by request failure.
-			);
-
-			steps.push( { description, action } );
-		} );
-	}
-
-	/* TODO: Add updates and deletes
-	...product.updates
-	...product.deletes
-	*/
-
-	/* TODO: Add variation API calls after product.
-	...variation.creates
-	...variation.updates
-	...variation.deletes
-	*/
-
-	return steps;
 }
 
