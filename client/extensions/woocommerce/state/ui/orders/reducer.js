0689e49f21ee422e787fdf0412371bebd6872ad2
Kelly Dwan <ryelle@users.noreply.github.com>
2017-06-14 12:43:48 +0200

    WooCommerce: Add orders ui state to track the current page of orders (#15042)
    * Add a reducer/action to track the current page of the orders list
    
    * Update the default currentPage state to 1, and update tests to match

---
+++ client/extensions/woocommerce/state/ui/orders/reducer.js
@@ -0,0 +1,27 @@
+/**
+ * External dependencies
+ */
+import { combineReducers, keyedReducer } from 'state/utils';
+import { WOOCOMMERCE_UI_ORDERS_SET_PAGE } from 'woocommerce/state/action-types';
+
+/**
+ * Tracks the current page of orders displayed for the current site.
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action payload
+ * @return {Object}        Updated state
+ */
+export function currentPage( state = 1, action ) {
+	switch ( action.type ) {
+		case WOOCOMMERCE_UI_ORDERS_SET_PAGE:
+			return action.page;
+		default:
+			return state;
+	}
+}
+
+const ordersReducer = combineReducers( {
+	currentPage
+} );
+
+export default keyedReducer( 'siteId', ordersReducer );
--- client/extensions/woocommerce/state/ui/reducer.js
+++ client/extensions/woocommerce/state/ui/reducer.js
@@ -1,12 +1,14 @@
 /**
  * Internal dependencies
  */
+import orders from './orders/reducer';
 import payments from './payments/reducer';
 import products from './products/reducer';
 import shipping from './shipping/reducer';
 import { combineReducers } from 'state/utils';
 
 export default combineReducers( {
+	orders,
 	payments,
 	products,
 	shipping,
