95c9033a63975f63912760c61c893ed05218e682
Miguel Fonseca <miguelcsf@gmail.com>
2017-06-15 18:17:37 +0100

    Add UpdatePostStatus HoC to replace updatePostStatus mixin

---
+++ client/components/update-post-status/index.js
@@ -0,0 +1,188 @@
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+import { translate } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import UpdateTemplate from './update-template';
+import PostActions from 'lib/posts/actions';
+import analytics from 'lib/analytics';
+
+// TODO(mcsf): use i18n#localize?
+const strings = {
+	page: {
+		deleteWarning: translate( 'Delete this page permanently?' ),
+		deleted: translate( 'Page Deleted' ),
+		deleting: translate( 'Deleting Page' ),
+		error: translate( 'Error' ),
+		restored: translate( 'Page Restored' ),
+		restoring: translate( 'Restoring Page' ),
+		trashed: translate( 'Moved to Trash' ),
+		trashing: translate( 'Trashing Page' ),
+		undo: translate( 'undo?' ),
+		updated: translate( 'Updated' ),
+		updating: translate( 'Updating Page' ),
+	},
+	post: {
+		deleteWarning: translate( 'Delete this post permanently?' ),
+		deleted: translate( 'Post Deleted' ),
+		deleting: translate( 'Deleting Post' ),
+		error: translate( 'Error' ),
+		restored: translate( 'Restored' ),
+		restoring: translate( 'Restoring' ),
+		trashed: translate( 'Moved to Trash' ),
+		trashing: translate( 'Trashing Post' ),
+		undo: translate( 'undo?' ),
+		updated: translate( 'Updated' ),
+		updating: translate( 'Updating Post' ),
+	},
+};
+
+const updatePostStatus = ( WrappedComponent ) =>
+	class UpdatePostStatus extends Component {
+		static displayName = `UpdatePostStatus(${
+			WrappedComponent.displayName || WrappedComponent.name || ''
+		})`;
+
+		state = {
+			updated: false,
+			updatedStatus: null,
+			previousStatus: null,
+			showMoreOptions: false,
+			showPageActions: false,
+		};
+
+		getType() {
+			return this.props.page ? 'page' : 'post';
+		}
+
+		buildUpdateTemplate = () => {
+			if ( ! this.state.updated ) {
+				return;
+			}
+
+			return <UpdateTemplate
+				post={ this.props.post || this.props.page }
+				previousStatus={ this.state.previousStatus }
+				resetToPreviousState={ this.resetToPreviousState }
+				status={ this.state.updatedStatus }
+				strings={ strings[ this.getType() ] }
+			/>;
+		}
+
+		updatePostStatus = ( status ) => {
+			const post = this.props.post || this.props.page;
+			let previousStatus = null;
+
+			const setNewStatus = ( error, resultPost ) => {
+				if ( error ) {
+					this.setErrorState();
+					return false;
+				}
+
+				this.setState( {
+					previousStatus,
+					updatedStatus: resultPost.status,
+					showMoreOptions: false,
+				} );
+				return true;
+			};
+
+			if ( status === 'delete' ) {
+				this.setState( {
+					showPageActions: false,
+					updatedStatus: 'deleting',
+					updated: true,
+				} );
+
+				const type = this.props.post ? 'post' : 'page';
+				if ( window.confirm( strings[ type ].deleteWarning ) ) { // eslint-disable-line no-alert
+					PostActions.trash( post, setNewStatus );
+				} else {
+					this.resetState();
+				}
+
+				return;
+			}
+
+			if ( status === 'trash' ) {
+				this.setState( {
+					showPageActions: false,
+					updatedStatus: 'trashing',
+					updated: true,
+				} );
+				previousStatus = post.status;
+				PostActions.trash( post, setNewStatus );
+				return;
+			}
+
+			if ( status === 'restore' ) {
+				this.setState( {
+					showPageActions: false,
+					updatedStatus: 'restoring',
+					updated: true,
+				} );
+				previousStatus = 'trash';
+				PostActions.restore( post, setNewStatus );
+				return;
+			}
+
+			this.setState( {
+				showPageActions: false,
+				updatedStatus: 'updating',
+				updated: true,
+			} );
+			PostActions.update( post, { status }, ( error, resultPost ) => {
+				if ( ! setNewStatus( error, resultPost ) ) {
+					return;
+				}
+				setTimeout( this.resetState, 1200 );
+			} );
+		}
+
+		resetState = () => {
+			this.setState( {
+				updatedStatus: null,
+				updated: false,
+				showMoreOptions: false,
+				showPageActions: false
+			} );
+		}
+
+		resetToPreviousState = () => {
+			const { group, eventName } = this.getType() === 'page'
+				? { group: 'Pages', eventName: 'Clicked Undo Trashed Page' }
+				: { group: 'Posts', eventName: 'Clicked Undo Trashed Post' };
+
+			analytics.ga.recordEvent( group, eventName );
+			if ( this.state.previousStatus ) {
+				this.updatePostStatus( this.state.previousStatus );
+			}
+		}
+
+		togglePageActions = () => {
+			this.setState( { showPageActions: ! this.state.showPageActions } );
+		}
+
+		setErrorState() {
+			this.setState( {
+				updated: true,
+				updatedStatus: 'error',
+			} );
+			setTimeout( this.resetState, 1200 );
+		}
+
+		render() {
+			return <WrappedComponent { ...this.props }
+				buildUpdateTemplate={ this.buildUpdateTemplate }
+				togglePageActions={ this.togglePageActions }
+				updatePostStatus={ this.updatePostStatus }
+				{ ...this.state } />;
+		}
+	};
+
+export default updatePostStatus;
+
---
+++ client/components/update-post-status/update-template.js
@@ -0,0 +1,72 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+export default function UpdateTemplate( {
+	post,
+	previousStatus,
+	resetToPreviousState,
+	status,
+	strings: s,
+} ) {
+	let updateClass = 'conf-alert';
+	let updateText, undoTemplate, undoClick, trashText;
+
+	switch ( status ) {
+		case 'trashing':
+		case 'deleting':
+			trashText = ( status === 'deleting' ) ? s.deleting : s.trashing;
+			updateText = (
+				<span>{ trashText } <span className="loading-dot">.</span><span className="loading-dot">.</span><span className="loading-dot">.</span></span>
+			);
+			updateClass += ' conf-alert--trashing';
+			break;
+
+		case 'trash':
+			undoClick = resetToPreviousState;
+			undoTemplate = (
+				<a className="undo" onClick={ undoClick }><span>{ s.undo }</span></a>
+			);
+			updateText = s.trashed;
+			updateClass += ' conf-alert--trashed';
+			break;
+
+		case 'deleted':
+			updateText = s.deleted;
+			updateClass += ' conf-alert--deleted';
+			break;
+
+		case 'updating':
+			updateText = (
+				<span>{ s.updating } <span className="loading-dot">.</span><span className="loading-dot">.</span><span className="loading-dot">.</span></span>
+			);
+			updateClass += ' conf-alert--updating';
+			break;
+
+		case 'error':
+			updateText = s.error;
+			updateClass += ' conf-alert--error';
+			break;
+
+		case 'restoring':
+			updateText = ( <span>{ s.restoring } <span className="loading-dot">.</span><span className="loading-dot">.</span><span className="loading-dot">.</span></span> );
+			updateClass += ' conf-alert--updating';
+			break;
+
+		default:
+			updateText = previousStatus === 'trash'
+				? s.restored
+				: s.updated;
+	}
+
+	return (
+		<div key={ post.global_ID + '-update' } className="updated-confirmation">
+			<div className={ updateClass }>
+				<div className="conf-alert_con">
+					<span className="conf-alert_title">{ updateText }</span>{ undoTemplate }
+				</div>
+			</div>
+		</div>
+	);
+}
