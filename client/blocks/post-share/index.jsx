49cca517e5669c57395858481dd17813baf1587a
retrofox <rdsuarez@gmail.com>
2017-06-15 17:34:43 -0300

    post-share: hide actions list under the flag

--- client/blocks/post-share/index.jsx
+++ client/blocks/post-share/index.jsx
@@ -465,7 +465,7 @@ class PostShare extends Component {
 					{ this.renderConnectionsSection() }
 				</div>
 
-				<ActionsList { ...this.props } />
+				{ isEnabled( 'publicize-scheduling' ) && <ActionsList { ...this.props } /> }
 			</div>
 		);
 	}
