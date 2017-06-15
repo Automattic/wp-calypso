cffaefdb941f13a4e6ff5e13d0a122c38266f893
Kevin Killingsworth <kevin@redfenix.com>
2017-06-15 11:41:44 -0500

    Woo: Add data layer requests (#14993)
    * Woo: Add data layer requests
    
    This adds a method of requesting WooCommerce API calls via the data
    layer in a declarative fashion
    
    * Woo Data Layer Requests: Add unique request id
    
    This adds a generated unique id for each request as it's created.
    This is to help track requests throughout their lifecycles in an
    asynchronous environment.
    
    * eslint fixes and review updates
    
    This fixes some minor formatting issues with eslint.
    It also replaces "bogus" with "placeholder" to be more obvious in
    meaning.
    It also removes the `body || {}` for post/put because `request` already
    does this.
    
    * Use WOOCOMMERCE_API_REQUEST instead
    
    This names WOOCOMMERCE_REQUEST_* action types to WOOCOMMERCE_API_REQUEST
    in order to be a bit more descriptive.
    
    * Woo: Add onSuccessAction and onFailureAction to request (#15071)
    
    * Update tests to use matchers
    
    This updates and simplifies the tests a bit by using matchers instead of
    matching on exact data structures.
    
    * Add onSuccessAction and onFailureAction to request
    
    This adds an optional onSuccessAction and onFailureAction to the request
    functions. This allows the calling code to add an optional action for
    each of these to be dispatched for an individual request. This will help
    track individual requests all the way through to the UI.
    
    * Simplify success/failure actions
    
    This removes the if checks for the success/failure actions.
    The resulting action will carry a couple of extra properties assigned to
    undefined, but it doesn't affect operation.
    
    This commit also simplifies the success/failure dispatch code.

--- client/extensions/woocommerce/state/data-layer/index.js
+++ client/extensions/woocommerce/state/data-layer/index.js
@@ -4,10 +4,11 @@
 import { mergeHandlers } from 'state/action-watchers/utils';
 import { addHandlers } from 'state/data-layer/extensions-middleware';
 import products from './products';
+import request from './request';
 import debugFactory from 'debug';
 
 const debug = debugFactory( 'woocommerce:errors' );
-const handlers = mergeHandlers( products );
+const handlers = mergeHandlers( products, request );
 
 export default function installActionHandlers() {
 	const added = addHandlers( 'woocommerce', handlers );
