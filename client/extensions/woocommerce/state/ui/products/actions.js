f08651e82adc51b820d54e61d89797b82fde40a4
Kevin Killingsworth <kevin@redfenix.com>
2017-06-15 14:37:14 -0500

    Woo: Product Create, add action-list success/fail
    This adds completion actions for notifications when the
    action list succeeds or fails.

--- client/extensions/woocommerce/state/ui/products/actions.js
+++ client/extensions/woocommerce/state/ui/products/actions.js
@@ -30,11 +30,15 @@ export function editProductAttribute( siteId, product, attribute, data ) {
  * Creates an action list to save product-related edits.
  *
  * Saves products, variations, and product categories.
+ * @param {Object} [successAction] Action to be dispatched upon successful completion.
+ * @param {Object} [failureAction] Action to be dispatched upon failure of execution.
  * @return {Function} action
  */
-export function createProductActionList() {
+export function createProductActionList( successAction, failureAction ) {
 	return {
 		type: WOOCOMMERCE_PRODUCT_ACTION_LIST_CREATE,
+		successAction,
+		failureAction,
 	};
 }
 
