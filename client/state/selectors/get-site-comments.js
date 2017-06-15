418ed9eaedfb8546f4e7e0ffb539e64fd58dfa38
Kerry Liu <gwwar@users.noreply.github.com>
2017-06-15 15:57:15 -0700

    Comments: memoize get site comments, update comment faker to at least show 20 comments (#15173)

--- client/state/selectors/get-site-comments.js
+++ client/state/selectors/get-site-comments.js
@@ -4,18 +4,26 @@
 import { get } from 'lodash';
 
 /**
+ * Internal dependencies
+ */
+import createSelector from 'lib/create-selector';
+
+/**
  * Returns list of loaded comments for a given site
  *
  * @param {Object} state Redux state
  * @param {Number} siteId site for whose comments to find
  * @returns {Array<Object>} available comments for site
  */
-export const getSiteComments = ( state, siteId ) => {
-	const comments = get( state, 'comments.items', {} );
+export const getSiteComments = createSelector(
+	( state, siteId ) => {
+		const comments = get( state, 'comments.items', {} );
 
-	return Object.keys( comments )
-		.filter( key => parseInt( key.split( '-', 1 ), 10 ) === siteId )
-		.reduce( ( list, key ) => [ ...list, ...comments[ key ] ], [] );
-};
+		return Object.keys( comments )
+			.filter( key => parseInt( key.split( '-', 1 ), 10 ) === siteId )
+			.reduce( ( list, key ) => [ ...list, ...comments[ key ] ], [] );
+	},
+	state => [ state.comments.items ]
+);
 
 export default getSiteComments;
