3c8a5fef80f4a47c9aec2eeedadacfd0dc3cd2d6
jonathansadowski <jonathansadowski@users.noreply.github.com>
2017-06-16 10:27:29 -0500

    Editor: Don't show preview on publish when private is selected (#15182)
    * Editor: Don't show preview on publish when private is selected from post-settings
    
    * Only enter the publishing dialog state from the dialog

--- client/post-editor/post-editor.jsx
+++ client/post-editor/post-editor.jsx
@@ -700,7 +700,7 @@ export const PostEditor = React.createClass( {
 			return;
 		}
 
-		if ( config.isEnabled( 'post-editor/delta-post-publish-flow' ) ) {
+		if ( config.isEnabled( 'post-editor/delta-post-publish-flow' ) && 'open' === this.state.confirmationSidebar ) {
 			this.setConfirmationSidebar( { status: 'publishing' } );
 		}
 
@@ -747,10 +747,6 @@ export const PostEditor = React.createClass( {
 			message = 'scheduled';
 		} else {
 			message = 'published';
-		}
-
-		if ( config.isEnabled( 'post-editor/delta-post-publish-flow' ) ) {
-			this.setConfirmationSidebar( { status: 'closed', context: 'publish_success' } );
 		}
 
 		this.onSaveSuccess( message, ( message === 'published' ? 'view' : 'preview' ), savedPost.URL );
@@ -820,6 +816,11 @@ export const PostEditor = React.createClass( {
 
 	onSaveSuccess: function( message, action, link ) {
 		const post = PostEditStore.get();
+		const isNotPrivateOrIsConfirmed = ( 'private' !== post.status ) || ( 'closed' !== this.state.confirmationSidebar );
+
+		if ( config.isEnabled( 'post-editor/delta-post-publish-flow' ) ) {
+			this.setConfirmationSidebar( { status: 'closed', context: 'publish_success' } );
+		}
 
 		if ( 'draft' === post.status ) {
 			this.props.setEditorLastDraft( post.site_ID, post.ID );
@@ -848,7 +849,11 @@ export const PostEditor = React.createClass( {
 
 			window.scrollTo( 0, 0 );
 
-			if ( config.isEnabled( 'post-editor/delta-post-publish-preview' ) && this.props.isSitePreviewable ) {
+			if (
+				config.isEnabled( 'post-editor/delta-post-publish-preview' ) &&
+				this.props.isSitePreviewable &&
+				isNotPrivateOrIsConfirmed
+			) {
 				this.setState( { isPostPublishPreview: true } );
 				this.iframePreview();
 			}
