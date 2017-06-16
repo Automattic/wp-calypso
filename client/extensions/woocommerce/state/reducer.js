b37cd1c1916feeccc92bf0b2f016b2bbe340606c
Kevin Killingsworth <kevin@redfenix.com>
2017-06-15 22:38:40 -0500

    Revert "WooCommerce: Make Action List general purpose"

--- client/extensions/woocommerce/state/reducer.js
+++ client/extensions/woocommerce/state/reducer.js
@@ -4,10 +4,8 @@
 import { combineReducers } from 'state/utils';
 import ui from './ui/reducer';
 import sites from './sites/reducer';
-import actionList from './action-list/reducer';
 
 export default combineReducers( {
 	ui,
 	sites,
-	actionList,
 } );
--- client/extensions/woocommerce/state/ui/products/reducer.js
+++ client/extensions/woocommerce/state/ui/products/reducer.js
@@ -4,6 +4,7 @@
 import edits from './edits-reducer';
 import { combineReducers, keyedReducer } from 'state/utils';
 import variations from './variations/reducer';
+import apiPlan from './api-plan/reducer';
 
 export default keyedReducer( 'siteId', combineReducers( {
 	edits,
