062fccd9eaa16c398345412fcfcb061ed2902811
Daniel Rey López <daniel.rey.lopez@gmail.com>
2017-06-18 09:09:33 +0200

    Added actions, reducers & selectors to fetch the locations from the WooCommerce host. (#15032)
    * Added actions, reducers & selectors to fetch the locations from the WooCommerce host.
    
    * Fixed PR comments.
    
    * Changed AM / America for NA / North America :facepalm:

--- client/extensions/woocommerce/state/sites/reducer.js
+++ client/extensions/woocommerce/state/sites/reducer.js
@@ -2,6 +2,7 @@
  * Internal dependencies
  */
 import { combineReducers, keyedReducer } from 'state/utils';
+import locations from './locations/reducer';
 import orders from './orders/reducer';
 import paymentMethods from './payment-methods/reducer';
 import productCategories from './product-categories/reducer';
@@ -15,6 +16,7 @@ import settings from './settings/reducer';
 import status from './status/reducer';
 
 const reducer = combineReducers( {
+	locations,
 	orders,
 	paymentMethods,
 	productCategories,
