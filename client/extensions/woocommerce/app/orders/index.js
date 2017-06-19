6b6770a90bb06226d980db9052a353565df29bc5
Kelly Dwan <ryelle@users.noreply.github.com>
2017-06-19 17:16:48 -0400

    Remove other filters, filtering orders list is out of v1 scope

--- client/extensions/woocommerce/app/orders/index.js
+++ client/extensions/woocommerce/app/orders/index.js
@@ -27,10 +27,6 @@ class Orders extends Component {
 					<SectionNav>
 						<NavTabs label={ translate( 'Status' ) } selectedText={ translate( 'All orders' ) }>
 							<NavItem path="/orders" selected={ true }>{ translate( 'All orders' ) }</NavItem>
-							<NavItem path="/orders/new" selected={ false }>{ translate( 'New' ) }</NavItem>
-							<NavItem path="/orders/pending" selected={ false }>{ translate( 'Pending' ) }</NavItem>
-							<NavItem path="/orders/processing" selected={ false }>{ translate( 'Processing' ) }</NavItem>
-							<NavItem path="/orders/failed" selected={ false }>{ translate( 'Failed' ) }</NavItem>
 						</NavTabs>
 					</SectionNav>
 
