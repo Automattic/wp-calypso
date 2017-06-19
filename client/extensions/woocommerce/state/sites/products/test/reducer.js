44675864a2a6b7f1767b253cf93f40632c973869
Justin Shreve <justin.shreve@gmail.com>
2017-06-19 15:30:17 -0700

    Add state for searching products.

--- client/extensions/woocommerce/state/sites/products/test/reducer.js
+++ client/extensions/woocommerce/state/sites/products/test/reducer.js
@@ -10,12 +10,20 @@ import {
 	productsRequest,
 	productsRequestSuccess,
 	productsRequestFailure,
+	productsSearchRequest,
+	productsSearchRequestFailure,
+	productsSearchRequestSuccess,
+	productsSearchClear,
 } from '../reducer';
 
 import {
 	WOOCOMMERCE_PRODUCTS_REQUEST,
 	WOOCOMMERCE_PRODUCTS_REQUEST_FAILURE,
 	WOOCOMMERCE_PRODUCTS_REQUEST_SUCCESS,
+	WOOCOMMERCE_PRODUCTS_SEARCH_CLEAR,
+	WOOCOMMERCE_PRODUCTS_SEARCH_REQUEST,
+	WOOCOMMERCE_PRODUCTS_SEARCH_REQUEST_SUCCESS,
+	WOOCOMMERCE_PRODUCTS_SEARCH_REQUEST_FAILURE,
 } from 'woocommerce/state/action-types';
 
 import products from './fixtures/products';
@@ -108,6 +116,111 @@ describe( 'reducer', () => {
 
 			const newState = productsRequestFailure( { isLoading: { 1: true } }, action );
 			expect( newState.isLoading ).to.eql( { 1: false } );
+		} );
+	} );
+	describe( 'productsSearchRequest', () => {
+		it( 'should store the currently loading page', () => {
+			const action = {
+				type: WOOCOMMERCE_PRODUCTS_SEARCH_REQUEST,
+				siteId: 123,
+				page: 1,
+				query: 'test',
+			};
+			const newState = productsSearchRequest( undefined, action );
+			expect( newState.search.isLoading ).to.eql( { 1: true } );
+		} );
+		it( 'should store the query', () => {
+			const action = {
+				type: WOOCOMMERCE_PRODUCTS_SEARCH_REQUEST,
+				siteId: 123,
+				page: 1,
+				query: 'testing',
+			};
+			const newState = productsSearchRequest( undefined, action );
+			expect( newState.search.query ).to.eql( 'testing' );
+		} );
+	} );
+	describe( 'productsSearchRequestSuccess', () => {
+		it( 'should should show that request is no longer loading', () => {
+			const action = {
+				type: WOOCOMMERCE_PRODUCTS_SEARCH_REQUEST_SUCCESS,
+				siteId: 123,
+				products,
+				page: 1,
+				totalProducts: 28,
+				query: 'testing',
+			};
+			const newState = productsSearchRequestSuccess( { search: { isLoading: { 1: true } } }, action );
+			expect( newState.search.isLoading ).to.eql( { 1: false } );
+		} );
+		it( 'should store the products in state', () => {
+			const action = {
+				type: WOOCOMMERCE_PRODUCTS_SEARCH_REQUEST_SUCCESS,
+				siteId: 123,
+				products,
+				page: 1,
+				totalProducts: 28,
+				query: 'testing',
+			};
+			const newState = productsSearchRequestSuccess( undefined, action );
+			expect( newState.products ).to.eql( products );
+		} );
+		it( 'should add new products onto the existing list', () => {
+			const additionalProducts = [ product ];
+			const action = {
+				type: WOOCOMMERCE_PRODUCTS_SEARCH_REQUEST_SUCCESS,
+				siteId: 123,
+				page: 2,
+				totalProducts: 28,
+				query: 'testing',
+				products: additionalProducts,
+			};
+			const originalState = {
+				products,
+				search: {
+					isLoading: { 1: false },
+					totalProducts: 28,
+				},
+			};
+			const newState = productsSearchRequestSuccess( originalState, action );
+			expect( newState.products ).to.eql( [ ...products, ...additionalProducts ] );
+		} );
+		it( 'should store the total number of products', () => {
+			const action = {
+				type: WOOCOMMERCE_PRODUCTS_SEARCH_REQUEST_SUCCESS,
+				siteId: 123,
+				page: 1,
+				totalProducts: 28,
+				query: 'testing',
+				products,
+			};
+			const newState = productsSearchRequestSuccess( undefined, action );
+			expect( newState.search.totalProducts ).to.eql( 28 );
+		} );
+	} );
+	describe( 'productsSearchRequestFailure', () => {
+		it( 'should should show that request has loaded on failure', () => {
+			const action = {
+				type: WOOCOMMERCE_PRODUCTS_SEARCH_REQUEST_FAILURE,
+				siteId: 123,
+				page: 1,
+				query: 'testing',
+				error: {}
+			};
+
+			const newState = productsSearchRequestFailure( { search: { isLoading: { 1: true } } }, action );
+			expect( newState.search.isLoading ).to.eql( { 1: false } );
+		} );
+	} );
+	describe( 'productsSearchClear', () => {
+		it( 'should reset search state', () => {
+			const action = {
+				type: WOOCOMMERCE_PRODUCTS_SEARCH_CLEAR,
+				siteId: 123,
+			};
+
+			const newState = productsSearchClear( { search: { isLoading: { 1: true } } }, action );
+			expect( newState.search ).to.eql( {} );
 		} );
 	} );
 } );
