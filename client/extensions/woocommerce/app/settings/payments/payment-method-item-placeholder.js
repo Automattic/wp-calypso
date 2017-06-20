d2fb7a8101d896d99fd9845cb0c5f09bc5b1c56c
Jonathan Belcher <belcherj@gmail.com>
2017-06-20 18:35:56 -0400

    Fix loading states on payment methods

--- client/extensions/woocommerce/app/settings/payments/payment-method-item-placeholder.js
+++ client/extensions/woocommerce/app/settings/payments/payment-method-item-placeholder.js
@@ -12,15 +12,18 @@ import ListItemField from 'woocommerce/components/list/list-item-field';
 const PaymentMethodItemPlaceHolder = () => {
 	return (
 		<ListItem>
-			<ListItemField>
+			<ListItemField className="payments__method-method-suggested-container">
 				<span className="payments__method-loading-suggested"></span>
 				<span className="payments__method-loading-title"></span>
 			</ListItemField>
-			<ListItemField>
+			<ListItemField className="payments__method-method-information-container">
 				<span className="payments__method-loading-fee"></span>
 				<span className="payments__method-loading-feelink"></span>
 			</ListItemField>
-			<ListItemField>
+			<ListItemField className="payments__method-enable-container">
+				<span className="payments__method-loading-enabled"></span>
+			</ListItemField>
+			<ListItemField className="payments__method-action-container">
 				<span className="payments__method-loading-settings"></span>
 			</ListItemField>
 		</ListItem>
