453b1aa238f83f0a5d28362615c97987b3023647
Kevin Killingsworth <kevin@redfenix.com>
2017-06-20 09:38:23 -0500

    Woo: Add Product Category selector
    This adds a "getProductCategory" selector to find a category by its id
    from the fetched API state.

--- client/extensions/woocommerce/state/sites/product-categories/test/selectors.js
+++ client/extensions/woocommerce/state/sites/product-categories/test/selectors.js
@@ -6,7 +6,7 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import { getProductCategories } from '../selectors';
+import { getProductCategories, getProductCategory } from '../selectors';
 import { LOADING } from 'woocommerce/state/constants';
 
 describe( 'selectors', () => {
@@ -65,6 +65,85 @@ describe( 'selectors', () => {
 
 			expect( getProductCategories( state, 123 ) ).to.equal( categories123 );
 			expect( getProductCategories( state, 345 ) ).to.equal( categories345 );
+		} );
+	} );
+
+	describe( '#getProductCategory', () => {
+		it( 'should return undefined if data is not available.', () => {
+			const state = {
+				extensions: {
+					woocommerce: {}
+				}
+			};
+
+			expect( getProductCategory( state, 1, 123 ) ).to.not.exist;
+		} );
+
+		it( 'should return undefined if data is still loading.', () => {
+			const state = {
+				extensions: {
+					woocommerce: {
+						sites: {
+							123: {
+								productCategories: LOADING,
+							}
+						}
+					}
+				}
+			};
+
+			expect( getProductCategory( state, 1, 123 ) ).to.not.exist;
+		} );
+
+		it( 'should return null if category is not found', () => {
+			const state = {
+				extensions: {
+					woocommerce: {
+						sites: {
+							123: {
+								productCategories: [
+									{ id: 1, name: 'Cat 1', slug: 'cat-1' },
+								]
+							}
+						}
+					}
+				}
+			};
+
+			expect( getProductCategory( state, 2, 123 ) ).to.equal( null );
+			expect( getProductCategory( state, { index: 0 }, 123 ) ).to.equal( null );
+		} );
+
+		it( 'should return categories that exist in fetched state', () => {
+			const categories123 = [
+				{ id: 1, name: 'Cat 1', slug: 'cat-1' },
+				{ id: 2, name: 'Cat 2', slug: 'cat-2' },
+			];
+
+			const categories345 = [
+				{ id: 3, name: 'Cat 3', slug: 'cat-3' },
+				{ id: 4, name: 'Cat 4', slug: 'cat-4' },
+			];
+
+			const state = {
+				extensions: {
+					woocommerce: {
+						sites: {
+							123: {
+								productCategories: categories123,
+							},
+							345: {
+								productCategories: categories345,
+							},
+						}
+					}
+				}
+			};
+
+			expect( getProductCategory( state, 1, 123 ) ).to.equal( categories123[ 0 ] );
+			expect( getProductCategory( state, 2, 123 ) ).to.equal( categories123[ 1 ] );
+			expect( getProductCategory( state, 3, 345 ) ).to.equal( categories345[ 0 ] );
+			expect( getProductCategory( state, 4, 345 ) ).to.equal( categories345[ 1 ] );
 		} );
 	} );
 } );
