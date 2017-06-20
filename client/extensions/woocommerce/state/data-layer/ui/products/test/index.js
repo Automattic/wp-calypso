3fc2677f2b7515db69d0bf8158a121a94e5a1091
Kevin Killingsworth <kevin@redfenix.com>
2017-06-20 17:04:41 -0500

    Woo: Product UI handlers: indent handlers test
    The previous code only had a single function at the top level `describe`
    block. This indents it under a top-level `handlers` block so other
    functions can be tested within it.

--- client/extensions/woocommerce/state/data-layer/ui/products/test/index.js
+++ client/extensions/woocommerce/state/data-layer/ui/products/test/index.js
@@ -10,128 +10,130 @@ import { makeProductActionList, } from '../';
 import { actionListStepSuccess, actionListStepFailure } from 'woocommerce/state/action-list/actions';
 import { createProduct } from 'woocommerce/state/sites/products/actions';
 
-describe( 'makeProductActionList', () => {
-	it( 'should return null when there are no edits', () => {
-		expect( makeProductActionList( null, 123, null ) ).to.equal.null;
-	} );
+describe( 'handlers', () => {
+	describe( '#makeProductActionList', () => {
+		it( 'should return null when there are no edits', () => {
+			expect( makeProductActionList( null, 123, null ) ).to.equal.null;
+		} );
 
-	it( 'should return a single product create request', () => {
-		const rootState = {
-			extensions: {
-				woocommerce: {
+		it( 'should return a single product create request', () => {
+			const rootState = {
+				extensions: {
+					woocommerce: {
+					}
 				}
-			}
-		};
+			};
 
-		const product1 = { id: { index: 0 }, name: 'Product #1' };
+			const product1 = { id: { index: 0 }, name: 'Product #1' };
 
-		const edits = {
-			creates: [
+			const edits = {
+				creates: [
+					product1,
+				]
+			};
+
+			const action1 = createProduct(
+				123,
 				product1,
-			]
-		};
+				actionListStepSuccess( 0 ),
+				actionListStepFailure( 0, 'UNKNOWN' )
+			);
 
-		const action1 = createProduct(
-			123,
-			product1,
-			actionListStepSuccess( 0 ),
-			actionListStepFailure( 0, 'UNKNOWN' )
-		);
+			const expectedActionList = {
+				steps: [
+					{ description: 'Creating product: Product #1', action: action1 },
+				],
+				successAction: undefined,
+				failureAction: undefined,
+				clearUponComplete: true,
+			};
 
-		const expectedActionList = {
-			steps: [
-				{ description: 'Creating product: Product #1', action: action1 },
-			],
-			successAction: undefined,
-			failureAction: undefined,
-			clearUponComplete: true,
-		};
+			expect( makeProductActionList( rootState, 123, edits ) ).to.eql( expectedActionList );
+		} );
 
-		expect( makeProductActionList( rootState, 123, edits ) ).to.eql( expectedActionList );
-	} );
-
-	it( 'should return multiple product create requests', () => {
-		const rootState = {
-			extensions: {
-				woocommerce: {
+		it( 'should return multiple product create requests', () => {
+			const rootState = {
+				extensions: {
+					woocommerce: {
+					}
 				}
-			}
-		};
+			};
 
-		const product1 = { id: { index: 0 }, name: 'Product #1' };
-		const product2 = { id: { index: 1 }, name: 'Product #2' };
+			const product1 = { id: { index: 0 }, name: 'Product #1' };
+			const product2 = { id: { index: 1 }, name: 'Product #2' };
 
-		const edits = {
-			creates: [
+			const edits = {
+				creates: [
+					product1,
+					product2,
+				]
+			};
+
+			const action1 = createProduct(
+				123,
 				product1,
+				actionListStepSuccess( 0 ),
+				actionListStepFailure( 0, 'UNKNOWN' )
+			);
+
+			const action2 = createProduct(
+				123,
 				product2,
-			]
-		};
+				actionListStepSuccess( 1 ),
+				actionListStepFailure( 1, 'UNKNOWN' )
+			);
 
-		const action1 = createProduct(
-			123,
-			product1,
-			actionListStepSuccess( 0 ),
-			actionListStepFailure( 0, 'UNKNOWN' )
-		);
+			const expectedActionList = {
+				steps: [
+					{ description: 'Creating product: Product #1', action: action1 },
+					{ description: 'Creating product: Product #2', action: action2 },
+				],
+				successAction: undefined,
+				failureAction: undefined,
+				clearUponComplete: true,
+			};
 
-		const action2 = createProduct(
-			123,
-			product2,
-			actionListStepSuccess( 1 ),
-			actionListStepFailure( 1, 'UNKNOWN' )
-		);
+			expect( makeProductActionList( rootState, 123, edits ) ).to.eql( expectedActionList );
+		} );
 
-		const expectedActionList = {
-			steps: [
-				{ description: 'Creating product: Product #1', action: action1 },
-				{ description: 'Creating product: Product #2', action: action2 },
-			],
-			successAction: undefined,
-			failureAction: undefined,
-			clearUponComplete: true,
-		};
-
-		expect( makeProductActionList( rootState, 123, edits ) ).to.eql( expectedActionList );
-	} );
-
-	it( 'should create an action list with success/failure actions', () => {
-		const rootState = {
-			extensions: {
-				woocommerce: {
+		it( 'should create an action list with success/failure actions', () => {
+			const rootState = {
+				extensions: {
+					woocommerce: {
+					}
 				}
-			}
-		};
+			};
 
-		const product1 = { id: { index: 0 }, name: 'Product #1' };
+			const product1 = { id: { index: 0 }, name: 'Product #1' };
 
-		const edits = {
-			creates: [
+			const edits = {
+				creates: [
+					product1,
+				]
+			};
+
+			const action1 = createProduct(
+				123,
 				product1,
-			]
-		};
+				actionListStepSuccess( 0 ),
+				actionListStepFailure( 0, 'UNKNOWN' )
+			);
 
-		const action1 = createProduct(
-			123,
-			product1,
-			actionListStepSuccess( 0 ),
-			actionListStepFailure( 0, 'UNKNOWN' )
-		);
+			const successAction = { type: '%%SUCCESS%%' };
+			const failureAction = { type: '%%FAILURE%%' };
 
-		const successAction = { type: '%%SUCCESS%%' };
-		const failureAction = { type: '%%FAILURE%%' };
+			const expectedActionList = {
+				steps: [
+					{ description: 'Creating product: Product #1', action: action1 },
+				],
+				successAction,
+				failureAction,
+				clearUponComplete: true,
+			};
 
-		const expectedActionList = {
-			steps: [
-				{ description: 'Creating product: Product #1', action: action1 },
-			],
-			successAction,
-			failureAction,
-			clearUponComplete: true,
-		};
-
-		const actionList = makeProductActionList( rootState, 123, edits, successAction, failureAction );
-		expect( actionList ).to.eql( expectedActionList );
+			const actionList = makeProductActionList( rootState, 123, edits, successAction, failureAction );
+			expect( actionList ).to.eql( expectedActionList );
+		} );
 	} );
 } );
 
