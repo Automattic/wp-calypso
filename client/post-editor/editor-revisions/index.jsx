6bb046bd4336d3317ac5bb8f21735856865b31c1
Benoit Person <benoit.person@gmail.com>
2017-06-21 21:31:57 +0200

    Editor Revisions: Move to functionnal component

--- client/post-editor/editor-revisions/index.jsx
+++ client/post-editor/editor-revisions/index.jsx
@@ -1,53 +1,41 @@
 /**
  * External dependencies
  */
-import React from 'react';
-import PureRenderMixin from 'react-pure-render/mixin';
 import Gridicon from 'gridicons';
+import { localize } from 'i18n-calypso';
+import React, { PropTypes } from 'react';
 
-export default React.createClass( {
-
-	displayName: 'EditorRevisions',
-
-	mixins: [ PureRenderMixin ],
-
-	propTypes: {
-		adminUrl: React.PropTypes.string,
-		revisions: React.PropTypes.array
-	},
-
-	getDefaultProps() {
-		return {
-			revisions: []
-		};
-	},
-
-	render() {
-		if ( ! this.props.revisions || ! this.props.revisions.length ) {
-			return null;
-		}
-
-		const lastRevision = this.props.revisions[ 0 ];
-		const revisionsLink = this.props.adminUrl + 'revision.php?revision=' + lastRevision;
-
-		return (
-			<a className="editor-revisions"
-				href={ revisionsLink }
-				target="_blank"
-				rel="noopener noreferrer"
-				aria-label={ this.translate( 'Open list of revisions' ) }
-			>
-				<Gridicon icon="history" size={ 18 } />
-				{ this.translate(
-					'%(revisions)d revision',
-					'%(revisions)d revisions', {
-						count: this.props.revisions.length,
-						args: {
-							revisions: this.props.revisions.length
-						}
-					}
-				) }
-			</a>
-		);
+function EditorRevisions( { adminUrl, revisions = [], translate } ) {
+	if ( ! revisions || ! revisions.length ) {
+		return null;
 	}
-} );
+
+	const lastRevision = revisions[ 0 ];
+	const revisionsLink = adminUrl + 'revision.php?revision=' + lastRevision;
+	return (
+		<a
+			className="editor-revisions"
+			href={ revisionsLink }
+			target="_blank"
+			rel="noopener noreferrer"
+			aria-label={ translate( 'Open list of revisions' ) }
+		>
+			<Gridicon icon="history" size={ 18 } />
+			{ translate(
+				'%(revisions)d revision',
+				'%(revisions)d revisions', {
+					count: revisions.length,
+					args: { revisions: revisions.length },
+				}
+			) }
+		</a>
+	);
+}
+
+EditorRevisions.propTypes = {
+	adminUrl: PropTypes.string,
+	revisions: PropTypes.array,
+	translate: PropTypes.func,
+};
+
+export default localize( EditorRevisions );
