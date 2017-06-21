1c66c0fe47182fca19f03dfe0664a4f4d0b17524
Donna Peplinskie <donnapep@gmail.com>
2017-06-21 13:49:09 -0400

    Remove use of combineReducers as there's currently only one

--- client/extensions/wp-job-manager/state/reducer.js
+++ client/extensions/wp-job-manager/state/reducer.js
@@ -1,9 +1,6 @@
 /**
  * Internal dependencies
  */
-import { combineReducers } from 'state/utils';
 import settings from './settings/reducer';
 
-export default combineReducers( {
-	settings,
-} );
+export default settings;
