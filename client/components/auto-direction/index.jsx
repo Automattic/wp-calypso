2e15f27ac5e709b92d728bf9a6a9ea6bc8cbcc5d
Yury Michurin <yury@automattic.com>
2017-06-21 16:45:07 -0400

    If we encounter some known inline node do parent
    If we encounter a node with a known class to be inline,
    such as emoji, apply the directionality style to
    it's parent instead

--- client/components/auto-direction/index.jsx
+++ client/components/auto-direction/index.jsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React, { PropTypes } from 'react';
+import get from 'lodash/get';
 
 /**
  * Internal dependencies
@@ -126,6 +127,30 @@ const getTextMainDirection = ( text ) => {
 	return rtlCount > ltrCount ? 'rtl' : 'ltr';
 };
 
+const getDirectionProps = ( child, direction ) => ( {
+	direction: direction,
+	style: Object.assign( {}, get( child, 'props.style', {} ), {
+		direction: direction,
+		textAlign: direction === 'rtl' ? 'right' : 'left'
+	} )
+} );
+
+const getChildDirection = child => {
+	const childContent = getContent( child );
+
+	if ( childContent ) {
+		const textMainDirection = getTextMainDirection( childContent );
+		const userDirection = user.isRTL() ? 'rtl' : 'ltr';
+
+		if ( textMainDirection !== userDirection ) {
+			return textMainDirection;
+		}
+	}
+
+	return null;
+};
+
+const inlineClasses = [ 'emojify' ];
 /***
  * Sets a react component child directionality according to it's text content
  * That function intended to be used recursively with React.Children.map
@@ -136,29 +161,28 @@ const getTextMainDirection = ( text ) => {
  * @returns {React.Element} transformed child
  */
 const setChildDirection = ( child ) => {
-	const childContent = getContent( child );
+	const childDirection = getChildDirection( child );
 
-	if ( childContent ) {
-		const textMainDirection = getTextMainDirection( childContent );
-		const userDirection = user.isRTL() ? 'rtl' : 'ltr';
-
-		if ( textMainDirection !== userDirection ) {
-			return React.cloneElement( child, {
-				direction: textMainDirection,
-				style: Object.assign( {}, child.props.style, {
-					direction: textMainDirection,
-					textAlign: textMainDirection === 'rtl' ? 'right' : 'left'
-				} )
-			} );
-		}
-
-		return child;
+	if ( childDirection ) {
+		return React.cloneElement( child, getDirectionProps( childDirection ) );
 	}
 
 	if ( child && child.props.children ) {
-		return React.cloneElement( child, {
-			children: React.Children.map( child.props.children, setChildDirection )
+		let innerChildDirection = null;
+		const children = React.Children.map( child.props.children, innerChild => {
+			if ( innerChildDirection ) {
+				return innerChild;
+			}
+
+			if ( inlineClasses.some( inlineClass => innerChild.props.className.indexOf( inlineClass ) > -1 ) ) {
+				innerChildDirection = getChildDirection( innerChild );
+				return innerChild;
+			}
+
+			return setChildDirection( innerChild );
 		} );
+
+		return React.cloneElement( child, innerChildDirection ? getDirectionProps( child, innerChildDirection ) : null, children );
 	}
 
 	return child;
