aa3db49da89b571b657c368be377e2ffa78a2c98
Donna Peplinskie <donnapep@gmail.com>
2017-06-22 14:02:33 -0400

    Convert JobListings to a functional component

--- client/extensions/wp-job-manager/components/settings/job-listings/index.jsx
+++ client/extensions/wp-job-manager/components/settings/job-listings/index.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -19,265 +19,271 @@ import FormToggle from 'components/forms/form-toggle/compact';
 import ListingsPerPage from './listings-per-page';
 import SectionHeader from 'components/section-header';
 
-class JobListings extends Component {
-	render() {
-		const {
-			isFetching,
-			onRadioChange,
-			onTextInputChange,
-			onToggleChange,
-			settings: {
-				job_manager_category_filter_type,
-				job_manager_date_format,
-				job_manager_enable_categories,
-				job_manager_enable_default_category_multiselect,
-				job_manager_enable_types,
-				job_manager_google_maps_api_key,
-				job_manager_hide_expired,
-				job_manager_hide_expired_content,
-				job_manager_hide_filled_positions,
-				job_manager_multi_job_type,
-				job_manager_per_page,
-			},
-			translate,
-		} = this.props;
-		const isDisabled = isFetching;
+const JobListings = ( {
+	isFetching,
+	onRadioChange,
+	onTextInputChange,
+	onToggleChange,
+	settings: {
+		job_manager_category_filter_type,
+		job_manager_date_format,
+		job_manager_enable_categories,
+		job_manager_enable_default_category_multiselect,
+		job_manager_enable_types,
+		job_manager_google_maps_api_key,
+		job_manager_hide_expired,
+		job_manager_hide_expired_content,
+		job_manager_hide_filled_positions,
+		job_manager_multi_job_type,
+		job_manager_per_page,
+	},
+	translate,
+} ) => {
+	const isDisabled = isFetching;
 
-		return (
-			<div>
-				<SectionHeader label={ translate( 'Listings' ) }>
-					<Button compact primary
-						disabled={ isDisabled }>
-						{ translate( 'Save Settings' ) }
-					</Button>
-				</SectionHeader>
-				<Card>
-					<form>
-						<p>
+	return (
+		<div>
+			<SectionHeader label={ translate( 'Listings' ) }>
+				<Button compact primary
+					disabled={ isDisabled }>
+					{ translate( 'Save Settings' ) }
+				</Button>
+			</SectionHeader>
+			<Card>
+				<form>
+					<p>
+						{ translate(
+							'Display {{listings /}} job listing per page',
+							'Display {{listings /}} job listings per page',
+							{
+								count: job_manager_per_page,
+								components: {
+									listings:
+										<ListingsPerPage
+											isDisabled={ isDisabled }
+											onChange={ onTextInputChange }
+											value={ job_manager_per_page } />
+								}
+							}
+						) }
+					</p>
+
+					<FormFieldset>
+						<FormToggle
+							checked={ job_manager_hide_filled_positions }
+							disabled={ isDisabled }
+							onChange={ onToggleChange( 'job_manager_hide_filled_positions' ) }>
+							{ translate( 'Hide filled positions' ) }
+						</FormToggle>
+						<FormSettingExplanation isIndented>
+							{ translate( 'Filled positions will not display in your archives.' ) }
+						</FormSettingExplanation>
+
+						<FormToggle
+							checked={ job_manager_hide_expired }
+							disabled={ isDisabled }
+							onChange={ onToggleChange( 'job_manager_hide_expired' ) }>
+							{ translate( 'Hide expired listings in job archives/search' ) }
+						</FormToggle>
+						<FormSettingExplanation isIndented>
+							{ translate( 'Expired job listings will not be searchable.' ) }
+						</FormSettingExplanation>
+
+						<FormToggle
+							checked={ job_manager_hide_expired_content }
+							disabled={ isDisabled }
+							onChange={ onToggleChange( 'job_manager_hide_expired_content' ) }>
+							{ translate( 'Hide content in expired single job listings' ) }
+						</FormToggle>
+						<FormSettingExplanation isIndented>
+							{ translate( 'Your site will display the titles of expired listings, but not the ' +
+								'content of the listings. Otherwise, expired listings display their full content ' +
+								'minus the application area.' ) }
+						</FormSettingExplanation>
+					</FormFieldset>
+				</form>
+			</Card>
+
+			<SectionHeader label={ translate( 'Categories' ) }>
+				<Button compact primary
+					disabled={ isDisabled }>
+					{ translate( 'Save Settings' ) }
+				</Button>
+			</SectionHeader>
+			<Card>
+				<form>
+					<FormFieldset>
+						<FormToggle
+							checked={ job_manager_enable_categories }
+							disabled={ isDisabled }
+							onChange={ onToggleChange( 'job_manager_enable_categories' ) }>
+							{ translate( 'Enable listing categories' ) }
+						</FormToggle>
+						<FormSettingExplanation isIndented>
+							{ translate( 'This lets users select from a list of categories when submitting a ' +
+								'job. Note!: an admin has to create categories before site users can select them.' ) }
+						</FormSettingExplanation>
+
+						<FormToggle
+							checked={ job_manager_enable_default_category_multiselect }
+							disabled={ isDisabled }
+							onChange={ onToggleChange( 'job_manager_enable_default_category_multiselect' ) }>
+							{ translate( 'Default to category multiselect' ) }
+						</FormToggle>
+						<FormSettingExplanation isIndented>
+							{ translate( 'The category selection box will default to allowing multiple ' +
+								'selections on the [jobs] shortcode. Without this, users will only be able to ' +
+								'select a single category when submitting jobs.' ) }
+						</FormSettingExplanation>
+					</FormFieldset>
+
+					<FormFieldset>
+						<FormLabel>
+							{ translate( 'Category Filter Type' ) }
+						</FormLabel>
+						<FormSettingExplanation>
+							{ translate( 'Determines the logic used to display jobs when selecting multiple categories.' ) }
+						</FormSettingExplanation>
+						<FormLabel>
+							<FormRadio
+								checked={ 'any' === job_manager_category_filter_type }
+								disabled={ isDisabled }
+								name="job_manager_category_filter_type"
+								onChange={ onRadioChange( 'job_manager_category_filter_type' ) }
+								value="any" />
+							<span>
+								{ translate( 'Jobs will be shown if within ANY selected category' ) }
+							</span>
+						</FormLabel>
+
+						<FormLabel>
+							<FormRadio
+								checked={ 'all' === job_manager_category_filter_type }
+								disabled={ isDisabled }
+								name="job_manager_category_filter_type"
+								onChange={ onRadioChange( 'job_manager_category_filter_type' ) }
+								value="all" />
+							<span>
+								{ translate( 'Jobs will be shown if within ALL selected categories' ) }
+							</span>
+						</FormLabel>
+					</FormFieldset>
+				</form>
+			</Card>
+
+			<SectionHeader label={ translate( 'Types' ) }></SectionHeader>
+			<Card>
+				<form>
+					<FormFieldset>
+						<FormToggle
+							checked={ job_manager_enable_types }
+							disabled={ isDisabled }
+							onChange={ onToggleChange( 'job_manager_enable_types' ) }>
+							{ translate( 'Enable listing types' ) }
+						</FormToggle>
+						<FormSettingExplanation isIndented>
+							{ translate( 'This lets users select from a list of types when submitting a job. ' +
+								'Note!: an admin has to create types before site users can select them.' ) }
+						</FormSettingExplanation>
+
+						<FormToggle
+							checked={ job_manager_multi_job_type }
+							disabled={ isDisabled }
+							onChange={ onToggleChange( 'job_manager_multi_job_type' ) }>
+							{ translate( 'Allow multiple types for listings' ) }
+						</FormToggle>
+						<FormSettingExplanation isIndented>
+							{ translate( 'This allows users to select more than one type when submitting a job. ' +
+								'The metabox on the post editor and the selection box on the front-end job ' +
+								'submission form will both reflect this.' ) }
+						</FormSettingExplanation>
+					</FormFieldset>
+				</form>
+			</Card>
+
+			<SectionHeader label={ translate( 'Date Format' ) }>
+				<Button compact primary
+					disabled={ isDisabled }>
+					{ translate( 'Save Settings' ) }
+				</Button>
+			</SectionHeader>
+			<Card>
+				<form>
+					<FormFieldset>
+						<FormSettingExplanation>
+							{ translate( 'Choose how you want the published date for jobs to be displayed on the front-end.' ) }
+						</FormSettingExplanation>
+						<FormLabel>
+							<FormRadio
+								checked={ 'relative' === job_manager_date_format }
+								disabled={ isDisabled }
+								name="job_manager_date_format"
+								onChange={ onRadioChange( 'job_manager_date_format' ) }
+								value="relative" />
+							<span>
+								{ translate( 'Relative to the current date (e.g., 1 day, 1 week, 1 month ago)' ) }
+							</span>
+						</FormLabel>
+
+						<FormLabel>
+							<FormRadio
+								checked={ 'default' === job_manager_date_format }
+								disabled={ isDisabled }
+								name="job_manager_date_format"
+								onChange={ onRadioChange( 'job_manager_date_format' ) }
+								value="default" />
+							<span>
+								{ translate( 'Default date format as defined in Settings' ) }
+							</span>
+						</FormLabel>
+					</FormFieldset>
+				</form>
+			</Card>
+
+			<SectionHeader label={ translate( 'Google Maps API Key' ) }>
+				<Button compact primary
+					disabled={ isDisabled }>
+					{ translate( 'Save Settings' ) }
+				</Button>
+			</SectionHeader>
+			<Card>
+				<form>
+					<FormFieldset>
+						<FormTextInput
+							disabled={ isDisabled }
+							onChange={ onTextInputChange( 'job_manager_google_maps_api_key' ) }
+							value={ job_manager_google_maps_api_key } />
+						<FormSettingExplanation>
 							{ translate(
-								'Display {{listings /}} job listing per page',
-								'Display {{listings /}} job listings per page',
+								'Google requires an API key to retrieve location information for job listings. ' +
+								'Acquire an API key from the {{a}}Google Maps API developer site{{/a}}.',
 								{
-									count: job_manager_per_page,
 									components: {
-										listings:
-											<ListingsPerPage
-												isDisabled={ isDisabled }
-												onChange={ onTextInputChange }
-												value={ job_manager_per_page } />
+										a: (
+											<ExternalLink
+												icon={ true }
+												target="_blank"
+												href="https://developers.google.com/maps/documentation/geocoding/get-api-key"
+											/>
+										),
 									}
 								}
 							) }
-						</p>
+						</FormSettingExplanation>
+					</FormFieldset>
+				</form>
+			</Card>
+		</div>
+	);
+};
 
-						<FormFieldset>
-							<FormToggle
-								checked={ job_manager_hide_filled_positions }
-								disabled={ isDisabled }
-								onChange={ onToggleChange( 'job_manager_hide_filled_positions' ) }>
-								{ translate( 'Hide filled positions' ) }
-							</FormToggle>
-							<FormSettingExplanation isIndented>
-								{ translate( 'Filled positions will not display in your archives.' ) }
-							</FormSettingExplanation>
-
-							<FormToggle
-								checked={ job_manager_hide_expired }
-								disabled={ isDisabled }
-								onChange={ onToggleChange( 'job_manager_hide_expired' ) }>
-								{ translate( 'Hide expired listings in job archives/search' ) }
-							</FormToggle>
-							<FormSettingExplanation isIndented>
-								{ translate( 'Expired job listings will not be searchable.' ) }
-							</FormSettingExplanation>
-
-							<FormToggle
-								checked={ job_manager_hide_expired_content }
-								disabled={ isDisabled }
-								onChange={ onToggleChange( 'job_manager_hide_expired_content' ) }>
-								{ translate( 'Hide content in expired single job listings' ) }
-							</FormToggle>
-							<FormSettingExplanation isIndented>
-								{ translate( 'Your site will display the titles of expired listings, but not the ' +
-									'content of the listings. Otherwise, expired listings display their full content ' +
-									'minus the application area.' ) }
-							</FormSettingExplanation>
-						</FormFieldset>
-					</form>
-				</Card>
-
-				<SectionHeader label={ translate( 'Categories' ) }>
-					<Button compact primary
-						disabled={ isDisabled }>
-						{ translate( 'Save Settings' ) }
-					</Button>
-				</SectionHeader>
-				<Card>
-					<form>
-						<FormFieldset>
-							<FormToggle
-								checked={ job_manager_enable_categories }
-								disabled={ isDisabled }
-								onChange={ onToggleChange( 'job_manager_enable_categories' ) }>
-								{ translate( 'Enable listing categories' ) }
-							</FormToggle>
-							<FormSettingExplanation isIndented>
-								{ translate( 'This lets users select from a list of categories when submitting a ' +
-									'job. Note!: an admin has to create categories before site users can select them.' ) }
-							</FormSettingExplanation>
-
-							<FormToggle
-								checked={ job_manager_enable_default_category_multiselect }
-								disabled={ isDisabled }
-								onChange={ onToggleChange( 'job_manager_enable_default_category_multiselect' ) }>
-								{ translate( 'Default to category multiselect' ) }
-							</FormToggle>
-							<FormSettingExplanation isIndented>
-								{ translate( 'The category selection box will default to allowing multiple ' +
-									'selections on the [jobs] shortcode. Without this, users will only be able to ' +
-									'select a single category when submitting jobs.' ) }
-							</FormSettingExplanation>
-						</FormFieldset>
-
-						<FormFieldset>
-							<FormLabel>
-								{ translate( 'Category Filter Type' ) }
-							</FormLabel>
-							<FormSettingExplanation>
-								{ translate( 'Determines the logic used to display jobs when selecting multiple categories.' ) }
-							</FormSettingExplanation>
-							<FormLabel>
-								<FormRadio
-									checked={ 'any' === job_manager_category_filter_type }
-									disabled={ isDisabled }
-									name="job_manager_category_filter_type"
-									onChange={ onRadioChange( 'job_manager_category_filter_type' ) }
-									value="any" />
-								<span>
-									{ translate( 'Jobs will be shown if within ANY selected category' ) }
-								</span>
-							</FormLabel>
-
-							<FormLabel>
-								<FormRadio
-									checked={ 'all' === job_manager_category_filter_type }
-									disabled={ isDisabled }
-									name="job_manager_category_filter_type"
-									onChange={ onRadioChange( 'job_manager_category_filter_type' ) }
-									value="all" />
-								<span>
-									{ translate( 'Jobs will be shown if within ALL selected categories' ) }
-								</span>
-							</FormLabel>
-						</FormFieldset>
-					</form>
-				</Card>
-
-				<SectionHeader label={ translate( 'Types' ) }></SectionHeader>
-				<Card>
-					<form>
-						<FormFieldset>
-							<FormToggle
-								checked={ job_manager_enable_types }
-								disabled={ isDisabled }
-								onChange={ onToggleChange( 'job_manager_enable_types' ) }>
-								{ translate( 'Enable listing types' ) }
-							</FormToggle>
-							<FormSettingExplanation isIndented>
-								{ translate( 'This lets users select from a list of types when submitting a job. ' +
-									'Note!: an admin has to create types before site users can select them.' ) }
-							</FormSettingExplanation>
-
-							<FormToggle
-								checked={ job_manager_multi_job_type }
-								disabled={ isDisabled }
-								onChange={ onToggleChange( 'job_manager_multi_job_type' ) }>
-								{ translate( 'Allow multiple types for listings' ) }
-							</FormToggle>
-							<FormSettingExplanation isIndented>
-								{ translate( 'This allows users to select more than one type when submitting a job. ' +
-									'The metabox on the post editor and the selection box on the front-end job ' +
-									'submission form will both reflect this.' ) }
-							</FormSettingExplanation>
-						</FormFieldset>
-					</form>
-				</Card>
-
-				<SectionHeader label={ translate( 'Date Format' ) }>
-					<Button compact primary
-						disabled={ isDisabled }>
-						{ translate( 'Save Settings' ) }
-					</Button>
-				</SectionHeader>
-				<Card>
-					<form>
-						<FormFieldset>
-							<FormSettingExplanation>
-								{ translate( 'Choose how you want the published date for jobs to be displayed on the front-end.' ) }
-							</FormSettingExplanation>
-							<FormLabel>
-								<FormRadio
-									checked={ 'relative' === job_manager_date_format }
-									disabled={ isDisabled }
-									name="job_manager_date_format"
-									onChange={ onRadioChange( 'job_manager_date_format' ) }
-									value="relative" />
-								<span>
-									{ translate( 'Relative to the current date (e.g., 1 day, 1 week, 1 month ago)' ) }
-								</span>
-							</FormLabel>
-
-							<FormLabel>
-								<FormRadio
-									checked={ 'default' === job_manager_date_format }
-									disabled={ isDisabled }
-									name="job_manager_date_format"
-									onChange={ onRadioChange( 'job_manager_date_format' ) }
-									value="default" />
-								<span>
-									{ translate( 'Default date format as defined in Settings' ) }
-								</span>
-							</FormLabel>
-						</FormFieldset>
-					</form>
-				</Card>
-
-				<SectionHeader label={ translate( 'Google Maps API Key' ) }>
-					<Button compact primary
-						disabled={ isDisabled }>
-						{ translate( 'Save Settings' ) }
-					</Button>
-				</SectionHeader>
-				<Card>
-					<form>
-						<FormFieldset>
-							<FormTextInput
-								disabled={ isDisabled }
-								onChange={ onTextInputChange( 'job_manager_google_maps_api_key' ) }
-								value={ job_manager_google_maps_api_key } />
-							<FormSettingExplanation>
-								{ translate(
-									'Google requires an API key to retrieve location information for job listings. ' +
-									'Acquire an API key from the {{a}}Google Maps API developer site{{/a}}.',
-									{
-										components: {
-											a: (
-												<ExternalLink
-													icon={ true }
-													target="_blank"
-													href="https://developers.google.com/maps/documentation/geocoding/get-api-key"
-												/>
-											),
-										}
-									}
-								) }
-							</FormSettingExplanation>
-						</FormFieldset>
-					</form>
-				</Card>
-			</div>
-		);
-	}
-}
+JobListings.propTypes = {
+	isFetching: PropTypes.bool,
+	onRadioChange: PropTypes.func,
+	onTextInputChange: PropTypes.func,
+	onToggleChange: PropTypes.func,
+	settings: PropTypes.object,
+	translate: PropTypes.func,
+};
 
 export default localize( JobListings );
