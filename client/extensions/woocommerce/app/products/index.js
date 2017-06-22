e4e3ffda43d69ab296f871c2242c87226be6e74d
Justin Shreve <justin.shreve@gmail.com>
2017-06-22 12:32:21 -0700

    WooCommerce: Add product search UI (#15306)
    * Add products UI state for searching.
    
    * Add UI for product searching and results display
    
    * Switch Search to SearchCard
    
    * Refactor search vs list code to clean up the main Products component.

--- client/extensions/woocommerce/app/products/index.js
+++ client/extensions/woocommerce/app/products/index.js
@@ -6,42 +6,35 @@ import { bindActionCreators } from 'redux';
 import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
+import { trim } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import ActionHeader from 'woocommerce/components/action-header';
 import Button from 'components/button';
-import EmptyContent from 'components/empty-content';
-import { fetchProducts } from 'woocommerce/state/sites/products/actions';
+import { fetchProducts, fetchProductSearchResults, clearProductSearch } from 'woocommerce/state/sites/products/actions';
 import { getLink } from 'woocommerce/lib/nav-utils';
-import { getTotalProducts, areProductsLoaded } from 'woocommerce/state/sites/products/selectors';
-import { getProductListCurrentPage, getProductListProducts, getProductListRequestedPage } from 'woocommerce/state/ui/products/selectors';
 import { getSelectedSiteWithFallback } from 'woocommerce/state/sites/selectors';
 import Main from 'components/main';
-import Pagination from 'my-sites/stats/pagination';
-import ProductsListTable from './products-list-table';
+import ProductsList from './products-list';
+import ProductsListSearchResults from './products-list-search-results';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
+import SearchCard from 'components/search-card';
 
 class Products extends Component {
 	static propTypes = {
 		site: PropTypes.shape( {
 			slug: PropTypes.string,
 		} ),
-		products: PropTypes.oneOfType( [
-			PropTypes.array,
-			PropTypes.bool,
-		] ),
-		currentPage: PropTypes.number,
-		currentPageLoaded: PropTypes.bool,
-		requestedPage: PropTypes.number,
-		requestedPageLoaded: PropTypes.bool,
-		totalProducts: PropTypes.number,
-		fetchProducts: PropTypes.func,
+		fetchProducts: PropTypes.func.isRequired,
+		fetchProductSearchResults: PropTypes.func.isRequired,
+		clearProductSearch: PropTypes.func.isRequired,
 	};
 
-	// Total number of results per page (the API returns 10)
-	perPage = 10;
+	state = {
+		query: '',
+	}
 
 	componentDidMount() {
 		const { site } = this.props;
@@ -55,75 +48,44 @@ class Products extends Component {
 		const newSiteId = newProps.site && newProps.site.ID || null;
 		const oldSiteId = site && site.ID || null;
 		if ( oldSiteId !== newSiteId ) {
+			this.setState( { query: '' } );
 			this.props.fetchProducts( newSiteId, 1 );
 		}
 	}
 
 	switchPage = ( page ) => {
 		const { site } = this.props;
-		this.props.fetchProducts( site.ID, page );
+		if ( trim( this.state.query ) !== '' ) {
+			this.props.fetchProductSearchResults( site.ID, page );
+		} else {
+			this.props.fetchProducts( site.ID, page );
+		}
 	}
 
-	pagination() {
-		const { site, currentPage, totalProducts, currentPageLoaded, requestedPage } = this.props;
+	onSearch = ( query ) => {
+		const { site } = this.props;
 
-		// If we know previously that all products fit on one page, don't show the placeholder
-		// since the Pagination component doesn't display anything for single page display.
-		if ( totalProducts && totalProducts < ( this.perPage + 1 ) ) {
-			return null;
+		if ( trim( query ) === '' ) {
+			this.setState( { query: '' } );
+			this.props.clearProductSearch( site.ID );
+			return;
 		}
 
-		if ( ! site || ! currentPageLoaded ) {
-			return ( <div className="products__list-placeholder pagination"></div> );
-		}
-
-		const page = requestedPage || currentPage;
-		return (
-			<Pagination
-				page={ page }
-				perPage={ this.perPage }
-				total={ totalProducts }
-				pageClick={ this.switchPage }
-			/>
-		);
-	}
-
-	renderEmptyContent() {
-		const { translate, site } = this.props;
-		const emptyContentAction = (
-			<Button href={ getLink( '/store/product/:site/', site ) }>
-				{ translate( 'Add your first product' ) }
-			</Button>
-		);
-		return <EmptyContent
-				title={ translate( 'You don\'t have any products yet.' ) }
-				action={ emptyContentAction }
-		/>;
-	}
-
-	renderList() {
-		const { site, products, totalProducts, currentPageLoaded, requestedPageLoaded, requestedPage } = this.props;
-
-		if ( currentPageLoaded === true && totalProducts === 0 ) {
-			return this.renderEmptyContent();
-		}
-
-		const isRequesting = ( requestedPage && ! requestedPageLoaded ) || ! products ? true : false;
-		return (
-			<div className="products__list-wrapper">
-				<ProductsListTable
-					site={ site }
-					products={ products }
-					isRequesting={ isRequesting }
-				/>
-				{ this.pagination() }
-			</div>
-		);
+		this.setState( { query } );
+		this.props.fetchProductSearchResults( site.ID, 1, query );
 	}
 
 	render() {
 		const { className, site, translate } = this.props;
 		const classes = classNames( 'products__list', className );
+
+		let productsDisplay;
+		if ( trim( this.state.query ) === '' ) {
+			productsDisplay = <ProductsList onSwitchPage={ this.switchPage } />;
+		} else {
+			productsDisplay = <ProductsListSearchResults onSwitchPage={ this.switchPage } />;
+		}
+
 		return (
 			<Main className={ classes }>
 				<SidebarNavigation />
@@ -132,7 +94,14 @@ class Products extends Component {
 						{ translate( 'Add a product' ) }
 					</Button>
 				</ActionHeader>
-				{ this.renderList() }
+				<SearchCard
+					onSearch={ this.onSearch }
+					delaySearch
+					delayTimeout={ 400 }
+					disabled={ ! site }
+					placeholder={ translate( 'Search products…' ) }
+				/>
+				{ productsDisplay }
 			</Main>
 		);
 	}
@@ -140,20 +109,8 @@ class Products extends Component {
 
 function mapStateToProps( state ) {
 	const site = getSelectedSiteWithFallback( state );
-	const currentPage = site && getProductListCurrentPage( state, site.ID );
-	const currentPageLoaded = site && currentPage && areProductsLoaded( state, currentPage, site.ID );
-	const requestedPage = site && getProductListRequestedPage( state, site.ID );
-	const requestedPageLoaded = site && requestedPage && areProductsLoaded( state, requestedPage, site.ID );
-	const products = site && getProductListProducts( state, site.ID );
-	const totalProducts = site && getTotalProducts( state, site.ID );
 	return {
 		site,
-		currentPage,
-		currentPageLoaded,
-		requestedPage,
-		requestedPageLoaded,
-		products,
-		totalProducts,
 	};
 }
 
@@ -161,6 +118,8 @@ function mapDispatchToProps( dispatch ) {
 	return bindActionCreators(
 		{
 			fetchProducts,
+			fetchProductSearchResults,
+			clearProductSearch,
 		},
 		dispatch
 	);
