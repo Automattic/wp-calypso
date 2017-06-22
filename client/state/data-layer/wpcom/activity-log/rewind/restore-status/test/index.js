8d0e9f54fe4cecb06e075e4b754c8ae84020b79e
Steve Seear <steve.seear@gmail.com>
2017-06-22 13:45:01 +0100

    Test for new data-layer handler

---
+++ client/state/data-layer/wpcom/activity-log/rewind/restore-status/test/index.js
@@ -0,0 +1,50 @@
+/**
+ * External dependencies
+ */
+import deepFreeze from 'deep-freeze';
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+/**
+ * Internal dependencies
+ */
+import {
+	receiveRestoreProgress,
+} from '../';
+import {
+	updateRewindRestoreProgress,
+} from 'state/activity-log/actions';
+
+const siteId = 77203074;
+const timestamp = 1496768464;
+const restoreId = 12345;
+
+const FINISHED_RESPONSE = deepFreeze( {
+	error: '',
+	ok: '',
+	restore_status: {
+		error_code: '',
+		failure_reason: '',
+		message: '',
+		percent: 100,
+		status: 'finished',
+	}
+} );
+
+describe( 'receiveRestoreProgress', () => {
+	it( 'should dispatch updateRewindRestoreProgress', () => {
+		const dispatch = sinon.spy();
+		receiveRestoreProgress( { dispatch }, { siteId, timestamp, restoreId }, null, FINISHED_RESPONSE );
+		expect( dispatch ).to.have.been.calledWith(
+			updateRewindRestoreProgress(
+				siteId, timestamp, restoreId, {
+					errorCode: '',
+					failureReason: '',
+					message: '',
+					percent: 100,
+					status: 'finished',
+				}
+			)
+		);
+	} );
+} );
