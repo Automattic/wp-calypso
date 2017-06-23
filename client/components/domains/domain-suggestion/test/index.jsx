001f2c120066e25df16d243d78180d918a03a71d
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-06-23 17:12:06 +0200

    Tests: Migrate a few client tests to work with Jest

---
+++ client/components/domains/domain-suggestion/test/index.jsx
@@ -0,0 +1,33 @@
+jest.mock( 'components/plans/premium-popover', () => require( 'test/helpers/react/empty-component' ) );
+
+/**
+ * External Dependencies
+ */
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import { identity, noop } from 'lodash';
+
+describe( 'Domain Suggestion', function() {
+	let DomainSuggestion;
+
+	before( () => {
+		DomainSuggestion = require( 'components/domains/domain-suggestion' );
+		DomainSuggestion.prototype.translate = identity;
+	} );
+
+	describe( 'has attributes', () => {
+		it( 'should have data-e2e-domain attribute for e2e testing', () => {
+			const domainSuggestion = shallow(
+				<DomainSuggestion
+					buttonContent="Click Me"
+					domain="example.com"
+					isAdded={ false }
+					onButtonClick={ noop }
+					priceRule="PRICE" />
+			);
+
+			expect( domainSuggestion.props()[ 'data-e2e-domain' ] ).to.equal( 'example.com' );
+		} );
+	} );
+} );
