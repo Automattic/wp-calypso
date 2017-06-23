ebc5c91225299eb79b71a8ecef9f583a6a6e9b37
Kirk Wight <kwight@kwight.ca>
2017-06-23 13:18:44 -0700

    Split out a DownloadCard component that uses `navigator.platform` for basic OS detection.

---
+++ client/me/get-apps/download-card.jsx
@@ -0,0 +1,77 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+import Button from 'components/button';
+
+const DownloadCard = ( { translate } ) => {
+	const trackAppDownloadClick = () => {};
+	const availableLinkTag = ( platformLink ) => {
+		return <a href={ platformLink } onClick={ trackAppDownloadClick } />;
+	};
+
+	const windowsLink = 'https://apps.wordpress.com/d/windows?ref=getapps';
+	const macLink = 'https://apps.wordpress.com/d/osx?ref=getapps';
+	const linuxTarLink = 'https://apps.wordpress.com/d/linux?ref=getapps';
+	const linuxDebLink = 'https://apps.wordpress.com/d/linux-deb?ref=getapps';
+
+	let buttonLink = windowsLink;
+	let cardTitle = translate( 'Desktop App for Windows' );
+	let requires = translate( 'Requires Windows Whatevs v12+. ' );
+	let translateComponents = {
+		firstAvailableLink: availableLinkTag( macLink ),
+		secondAvailableLink: availableLinkTag( linuxTarLink ),
+		thirdAvailableLink: availableLinkTag( linuxDebLink ),
+	};
+	let alsoAvailable = translate( 'Also available for: {{firstAvailableLink}}MacOS{{/firstAvailableLink}}, {{secondAvailableLink}}Linux (.tar.gz){{/secondAvailableLink}}, {{thirdAvailableLink}}Linux (.deb){{/thirdAvailableLink}}.', { components: translateComponents } );
+
+	if ( navigator.platform && navigator.platform.length > 0 ) {
+		switch ( navigator.platform ) {
+			case 'MacIntel':
+				buttonLink = macLink;
+				cardTitle = translate( 'Desktop App for MacOS' );
+				requires = translate( 'Requires Mac OS X 10.11+. ' );
+				translateComponents = {
+					firstAvailableLink: availableLinkTag( windowsLink ),
+					secondAvailableLink: availableLinkTag( linuxTarLink ),
+					thirdAvailableLink: availableLinkTag( linuxDebLink ),
+				};
+				alsoAvailable = translate( 'Also available for: {{firstAvailableLink}}Windows{{/firstAvailableLink}}, {{secondAvailableLink}}Linux (.tar.gz){{/secondAvailableLink}}, {{thirdAvailableLink}}Linux (.deb){{/thirdAvailableLink}}.', { components: translateComponents } );
+				break;
+			case 'Linux i686':
+			case 'Linux i686 on x86_64':
+				buttonLink = linuxTarLink;
+				cardTitle = translate( 'Desktop App for Linux' );
+				requires = translate( 'Requires Linux Whatevs v345. ' );
+				translateComponents = {
+					firstAvailableLink: availableLinkTag( linuxDebLink ),
+					secondAvailableLink: availableLinkTag( windowsLink ),
+					thirdAvailableLink: availableLinkTag( macLink ),
+				};
+				alsoAvailable = translate( 'Also available for: {{firstAvailableLink}}Linux (.deb){{/firstAvailableLink}}, {{secondAvailableLink}}Windows{{/secondAvailableLink}}, {{thirdAvailableLink}}Mac{{/thirdAvailableLink}}.', { components: translateComponents } );
+				break;
+			break;
+		}
+	}
+
+	return (
+		<Card className="get-apps__desktop">
+			<div className={ 'get-apps__card-text' }>
+				<h3>{ cardTitle }</h3>
+				<p>{ translate( 'A desktop app that gives WordPress a permanent home in your dock.' ) }</p>
+				<p className={ 'get-apps__also-available' }>
+					{ requires }
+					{ alsoAvailable }
+				</p>
+			</div>
+			<Button className={ 'get-apps__desktop-button' } href={ buttonLink }>{ translate( 'Download' ) }</Button>
+		</Card>
+	);
+};
+
+export default DownloadCard;
--- client/me/get-apps/index.jsx
+++ client/me/get-apps/index.jsx
@@ -10,10 +10,10 @@ import { localize } from 'i18n-calypso';
  */
 import MeSidebarNavigation from 'me/sidebar-navigation';
 import Main from 'components/main';
-import Button from 'components/button';
 import Card from 'components/card';
 import SectionHeader from 'components/section-header';
 import GetAppsIllustration from './illustration.jsx';
+import DownloadCard from './download-card.jsx';
 
 export const GetApps = ( { translate } ) => {
 	const trackAppDownloadClick = () => {};
@@ -22,41 +22,17 @@ export const GetApps = ( { translate } ) => {
 			<MeSidebarNavigation />
 			<GetAppsIllustration translate={ translate } />
 			<SectionHeader label={ translate( 'WordPress Apps' ) } />
-			<Card className="get-apps__desktop">
-				<div className={ 'get-apps__card-text' }>
-					<h3>{ translate( 'Desktop App for Mac' ) }</h3>
-					<p>{ translate( 'A desktop app that gives WordPress a permanent home in your dock.' ) }</p>
-					<p className={ 'get-apps__also-available' }>
-						{ translate(
-							'Requires Mac OS X 10.11+. Also available for: {{windowsLink}}Windows (7+){{/windowsLink}}, {{tarLink}}Linux (.tar.gz){{/tarLink}}, {{debLink}}Linux (.deb){{/debLink}}.',
-							{
-								components: {
-									windowsLink: (
-										<a href={ 'https://apps.wordpress.com/d/windows?ref=getapps' } onClick={ trackAppDownloadClick } />
-									),
-									tarLink: (
-										<a href={ 'https://apps.wordpress.com/d/linux?ref=getapps' } onClick={ trackAppDownloadClick } />
-									),
-									debLink: (
-										<a href={ 'https://apps.wordpress.com/d/linux-deb?ref=getapps' } onClick={ trackAppDownloadClick } />
-									)
-								}
-							}
-						) }
-					</p>
-				</div>
-				<Button className={ 'get-apps__desktop-button' } href={ 'https://apps.wordpress.com/d/osx?ref=getapps' }>Download</Button>
-			</Card>
+			<DownloadCard translate={ translate } />
 			<Card className="get-apps__mobile">
 				<div className={ 'get-apps__card-text' }>
 					<h3>{ translate( 'Mobile Apps' ) }</h3>
 					<p>{ translate( 'WordPress at your fingertips.' ) }</p>
 				</div>
 				<div className={ 'get-apps__badges' }>
-					<a href={ 'https://itunes.apple.com/us/app/wordpress/id335703880?mt=8' }>
+					<a href={ 'https://itunes.apple.com/us/app/wordpress/id335703880?mt=8' } onClick={ trackAppDownloadClick }>
 						<img src={ '/calypso/images/me/get-apps-app-store.png' } alt={ translate( 'Get on the iOS App Store' ) } />
 					</a>
-					<a href={ 'https://play.google.com/store/apps/details?id=org.wordpress.android' }>
+					<a href={ 'https://play.google.com/store/apps/details?id=org.wordpress.android' } onClick={ trackAppDownloadClick }>
 						<img src={ '/calypso/images/me/get-apps-google-play.png' } alt={ translate( 'Android' ) } />
 					</a>
 				</div>
