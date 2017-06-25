4a1f55fd59d7740e5e82bf594086d2cedb33d06e
Kevin Killingsworth <kevin@redfenix.com>
2017-06-25 15:57:28 -0500

    Woo: Update product create action-list
    This updates the product create action-list for use with the refactored
    action-list code.
    
    Woo: Request: Add support for dispatch functions.
    
    This adds support for passing a function to the request middleware
    handler, which allows for dispatching multiple functions in sequential
    order, with access to the data upon success and error upon failure.
    
    Woo: Product updated, func > completion action
    
    With the function support in request, it becomes easier to support a
    more versatile form of the completion action, where it's possible to
    pass on the data from the API call directly.
    
    Woo: Category updated, func > completion action
    
    With the function support in request, it becomes easier to support a
    more versatile form of the completion action, where it's possible to
    pass on the data from the API call directly.
    
    Woo: update category placeholder ids
    
    This adjusts existing code to update placeholder ids for categories
    during the product create/update process.

--- client/extensions/woocommerce/app/products/product-create.js
+++ client/extensions/woocommerce/app/products/product-create.js
@@ -15,7 +15,6 @@ import { successNotice, errorNotice } from 'state/notices/actions';
 import { editProduct, editProductAttribute, createProductActionList } from 'woocommerce/state/ui/products/actions';
 import { editProductCategory } from 'woocommerce/state/ui/product-categories/actions';
 import { getActionList } from 'woocommerce/state/action-list/selectors';
-import { actionListStepNext } from 'woocommerce/state/action-list/actions';
 import { getCurrentlyEditingProduct } from 'woocommerce/state/ui/products/selectors';
 import { getProductVariationsWithLocalEdits } from 'woocommerce/state/ui/products/variations/selectors';
 import { editProductVariation } from 'woocommerce/state/ui/products/variations/actions';
@@ -93,7 +92,6 @@ class ProductCreate extends React.Component {
 		);
 
 		this.props.createProductActionList( successAction, failureAction );
-		this.props.actionListStepNext();
 	}
 
 	isProductValid( product = this.props.product ) {
@@ -153,7 +151,6 @@ function mapStateToProps( state ) {
 function mapDispatchToProps( dispatch ) {
 	return bindActionCreators(
 		{
-			actionListStepNext,
 			createProduct,
 			createProductActionList,
 			editProduct,
