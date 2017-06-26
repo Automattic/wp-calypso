6de4074e726aa7053786c86a7e5b5f5be3799d0f
Jonathan Belcher <belcherj@gmail.com>
2017-06-26 12:22:53 -0400

    WIP

--- client/extensions/woocommerce/app/dashboard/required-plugins-install-view.js
+++ client/extensions/woocommerce/app/dashboard/required-plugins-install-view.js
@@ -1,29 +1,74 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
-import { localize } from 'i18n-calypso';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import debugFactor from 'debug';
+import { find, isEqual } from 'lodash';
+import { localize } from 'i18n-calypso';
+
+const debug = debugFactor( 'woocommerce:action-list' );
 
 /**
  * Internal dependencies
  */
-import SetupHeader from './setup-header';
+import { activatePlugin, installPlugin } from 'state/plugins/installed/actions';
+import { getPlugin } from 'state/plugins/wporg/selectors';
+import { getPlugins } from 'state/plugins/installed/selectors';
+import { getSelectedSiteWithFallback } from 'woocommerce/state/sites/selectors';
 import ProgressBar from 'components/progress-bar';
 import QueryJetpackPlugins from 'components/data/query-jetpack-plugins';
-import { getSelectedSiteWithFallback } from 'woocommerce/state/sites/selectors';
-import { getPlugins } from 'state/plugins/installed/selectors';
+import SetupHeader from './setup-header';
 
 const requiredPlugins = [
 	'woocommerce',
 	'wc-api-dev',
+	'woocommerce-gateway-stripe',
 	'woocommerce-services',
 ];
 
 class RequiredPluginsInstallView extends Component {
+	static propTypes = {
+		plugins: PropTypes.array,
+	};
+
+	componentDidMount = () => {
+		const { plugins } = this.props;
+
+		if ( plugins && plugins.length ) {
+			this.installPlugins( plugins );
+		}
+	}
+
+	componentWillReceiveProps = ( newProps ) => {
+		if ( isEqual( newProps.plugins, this.props.plugins ) ) {
+			return;
+		}
+		this.installPlugins( newProps.plugins );
+	}
+
+	installPlugins = ( plugins ) => {
+		const { siteId, wporg } = this.props;
+		requiredPlugins.map( function( slug ) {
+			const plugin = find( plugins, { slug } );
+			if ( slug === 'wc-api-dev' ) {
+				debug( 'wc-api-dev not installed since it is on on WordPress.org' );
+			}
+			if ( ! plugin ) {
+				const wporgPlugin = getPlugin( wporg, slug );
+				// debugger;
+				installPlugin( siteId, wporgPlugin );
+				return;
+			}
+			if ( plugin && ! plugin.active ) {
+				// debugger;
+				activatePlugin( siteId, plugin );
+			}
+		} );
+	}
+
 	render = () => {
 		const { translate, siteId, plugins } = this.props;
-
 		return (
 			<div className="card dashboard__setup-wrapper">
 				{ siteId && <QueryJetpackPlugins siteIds={ [ siteId ] } /> }
@@ -52,6 +97,7 @@ function mapStateToProps( state ) {
 	return {
 		siteId,
 		plugins: getPlugins( state, [ { ID: siteId } ] ),
+		wporg: state.plugins.wporg.items,
 	};
 }
 
