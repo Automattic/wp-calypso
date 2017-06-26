ad3d5633c4ef5cb135ad2fe89a04af78a0c10b23
Kirk Wight <kwight@kwight.ca>
2017-06-26 15:03:46 -0700

    Replace placeholder with the final illustration.

--- client/me/get-apps/illustration.jsx
+++ client/me/get-apps/illustration.jsx
@@ -6,7 +6,7 @@ import { localize } from 'i18n-calypso';
 
 const GetAppsIllustration = ( { translate } ) =>
 	<div className={ 'get-apps__illustration' }>
-		<img src={ '/calypso/images/illustrations/illustration-empty-results.svg' } />
+		<img src={ '/calypso/images/illustrations/get-apps-banner.svg' } />
 		<h2>{ translate( 'Inspiration strikes any time, anywhere.' ) }</h2>
 		<p>{ translate( 'Get WordPress apps for all your screens.' ) }</p>
 	</div>;
