fc0b190a29b07f286087e55656b81da094a1a339
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-06-27 06:06:59 +0100

    Added a helper to merge location edits with temporary ones

---
+++ client/extensions/woocommerce/state/ui/shipping/zones/locations/helpers.js
@@ -0,0 +1,40 @@
+/**
+ * External dependencies
+ */
+import { pullAll, union } from 'lodash';
+
+export const mergeLocationEdits = ( zoneLocationEdits, currentLocationEdits ) => {
+	if ( ! currentLocationEdits || currentLocationEdits.pristine ) {
+		return zoneLocationEdits;
+	}
+	const { journal, states } = zoneLocationEdits;
+	const {
+		journal: currentJournal,
+		states: currentStates,
+		postcode: currentPostcode,
+	} = currentLocationEdits;
+
+	let mergedStates = null;
+	if ( null !== currentStates ) {
+		if ( null === states ) {
+			mergedStates = currentStates;
+		} else {
+			mergedStates = {
+				add: currentStates.removeAll ? [] : [ ...states.add ],
+				remove: currentStates.removeAll ? [] : [ ...states.remove ],
+				removeAll: states.removeAll || currentStates.removeAll,
+			};
+			pullAll( mergedStates.add, currentStates.remove );
+			pullAll( mergedStates.remove, currentStates.add );
+			mergedStates.add = union( mergedStates.add, currentStates.add );
+			mergedStates.remove = union( mergedStates.remove, currentStates.remove );
+		}
+	}
+
+	return {
+		journal: [ ...journal, ...currentJournal ],
+		states: mergedStates,
+		postcode: currentPostcode,
+		pristine: zoneLocationEdits.pristine,
+	};
+};
