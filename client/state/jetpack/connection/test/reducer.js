cbc5ffa07656a40dafa4d093e2e4b8e74fa46ebe
Marin Atanasov <tyxla@abv.bg>
2017-06-27 21:03:08 +0300

    State: Introduce tests for Jetpack user connection data reducer

--- client/state/jetpack/connection/test/reducer.js
+++ client/state/jetpack/connection/test/reducer.js
@@ -15,17 +15,24 @@ import {
 	JETPACK_DISCONNECT_REQUEST,
 	JETPACK_DISCONNECT_REQUEST_FAILURE,
 	JETPACK_DISCONNECT_REQUEST_SUCCESS,
+	JETPACK_USER_CONNECTION_DATA_RECEIVE,
+	JETPACK_USER_CONNECTION_DATA_REQUEST,
+	JETPACK_USER_CONNECTION_DATA_REQUEST_SUCCESS,
+	JETPACK_USER_CONNECTION_DATA_REQUEST_FAILURE,
 	SERIALIZE,
 	DESERIALIZE
 } from 'state/action-types';
 import {
 	items as itemsReducer,
 	requests as requestsReducer,
+	dataItems as dataItemsReducer,
+	dataRequests as dataRequestsReducer,
 	disconnectRequests as disconnectRequestsReducer
 } from '../reducer';
 import {
 	items as ITEMS_FIXTURE,
-	requests as REQUESTS_FIXTURE
+	requests as REQUESTS_FIXTURE,
+	dataItems as DATA_ITEMS_FIXTURE,
 } from './fixture';
 
 describe( 'reducer', () => {
@@ -147,6 +154,128 @@ describe( 'reducer', () => {
 					type: DESERIALIZE
 				};
 			const stateOut = requestsReducer( deepFreeze( stateIn ), action );
+			expect( stateOut ).to.eql( {} );
+		} );
+	} );
+
+	describe( 'dataItems', () => {
+		it( 'state should default to empty object', () => {
+			const state = dataItemsReducer( undefined, {} );
+			expect( state ).to.eql( {} );
+		} );
+
+		it( 'should update user connection data in the items object', () => {
+			const stateIn = DATA_ITEMS_FIXTURE,
+				siteId = 12345678,
+				action = {
+					type: JETPACK_USER_CONNECTION_DATA_RECEIVE,
+					siteId,
+					data: DATA_ITEMS_FIXTURE[ 87654321 ]
+				};
+			const stateOut = dataItemsReducer( deepFreeze( stateIn ), action );
+			expect( stateOut ).to.eql( {
+				...DATA_ITEMS_FIXTURE,
+				[ siteId ]: DATA_ITEMS_FIXTURE[ 87654321 ],
+			} );
+		} );
+
+		it( 'should accummulate user connection data for separate sites in the items object', () => {
+			const stateIn = DATA_ITEMS_FIXTURE,
+				siteId = 11223344,
+				action = {
+					type: JETPACK_USER_CONNECTION_DATA_RECEIVE,
+					siteId,
+					data: DATA_ITEMS_FIXTURE[ 87654321 ]
+				};
+			const stateOut = dataItemsReducer( deepFreeze( stateIn ), action );
+			expect( stateOut ).to.eql( {
+				...DATA_ITEMS_FIXTURE,
+				[ siteId ]: DATA_ITEMS_FIXTURE[ 87654321 ]
+			} );
+		} );
+
+		it( 'should not persist state', () => {
+			const stateIn = DATA_ITEMS_FIXTURE,
+				action = {
+					type: SERIALIZE
+				};
+			const stateOut = dataItemsReducer( deepFreeze( stateIn ), action );
+			expect( stateOut ).to.eql( {} );
+		} );
+
+		it( 'should not load persisted state', () => {
+			const stateIn = DATA_ITEMS_FIXTURE,
+				action = {
+					type: DESERIALIZE
+				};
+			const stateOut = dataItemsReducer( deepFreeze( stateIn ), action );
+			expect( stateOut ).to.eql( {} );
+		} );
+	} );
+
+	describe( 'dataRequests', () => {
+		it( 'state should default to an empty object', () => {
+			const state = dataRequestsReducer( undefined, {} );
+			expect( state ).to.eql( {} );
+		} );
+
+		it( 'should set dataRequests to true for the specified site when data request starts', () => {
+			const stateIn = REQUESTS_FIXTURE,
+				siteId = 12345678,
+				action = {
+					type: JETPACK_USER_CONNECTION_DATA_REQUEST,
+					siteId
+				};
+			const stateOut = dataRequestsReducer( deepFreeze( stateIn ), action );
+			expect( stateOut ).to.eql( {
+				...REQUESTS_FIXTURE,
+				[ siteId ]: true
+			} );
+		} );
+
+		it( 'should set dataRequests to false for the specified site when data request completes successfully', () => {
+			const stateIn = REQUESTS_FIXTURE,
+				siteId = 87654321,
+				action = {
+					type: JETPACK_USER_CONNECTION_DATA_REQUEST_SUCCESS,
+					siteId
+				};
+			const stateOut = dataRequestsReducer( deepFreeze( stateIn ), action );
+			expect( stateOut ).to.eql( {
+				...REQUESTS_FIXTURE,
+				[ siteId ]: false
+			} );
+		} );
+
+		it( 'should set dataRequests to false for the specified site when data request completes unsuccessfully', () => {
+			const stateIn = REQUESTS_FIXTURE,
+				siteId = 87654321,
+				action = {
+					type: JETPACK_USER_CONNECTION_DATA_REQUEST_FAILURE,
+					siteId
+				};
+			const stateOut = dataRequestsReducer( deepFreeze( stateIn ), action );
+			expect( stateOut ).to.eql( {
+				...REQUESTS_FIXTURE,
+				[ siteId ]: false
+			} );
+		} );
+
+		it( 'should not persist state', () => {
+			const stateIn = REQUESTS_FIXTURE,
+				action = {
+					type: SERIALIZE
+				};
+			const stateOut = dataRequestsReducer( deepFreeze( stateIn ), action );
+			expect( stateOut ).to.eql( {} );
+		} );
+
+		it( 'should not load persisted state', () => {
+			const stateIn = REQUESTS_FIXTURE,
+				action = {
+					type: DESERIALIZE
+				};
+			const stateOut = dataRequestsReducer( deepFreeze( stateIn ), action );
 			expect( stateOut ).to.eql( {} );
 		} );
 	} );
