886f833e19d6d5cd57d66b2ca2544c567deccecb
Marin Atanasov <tyxla@abv.bg>
2017-06-27 21:02:28 +0300

    State: Introduce Jetpack user connection data reducer

--- client/state/jetpack/connection/reducer.js
+++ client/state/jetpack/connection/reducer.js
@@ -9,6 +9,10 @@ import {
 	JETPACK_DISCONNECT_REQUEST,
 	JETPACK_DISCONNECT_REQUEST_FAILURE,
 	JETPACK_DISCONNECT_REQUEST_SUCCESS,
+	JETPACK_USER_CONNECTION_DATA_RECEIVE,
+	JETPACK_USER_CONNECTION_DATA_REQUEST,
+	JETPACK_USER_CONNECTION_DATA_REQUEST_SUCCESS,
+	JETPACK_USER_CONNECTION_DATA_REQUEST_FAILURE,
 } from 'state/action-types';
 import { combineReducers, createReducer } from 'state/utils';
 
@@ -45,6 +49,32 @@ export const requests = createReducer( {}, {
 	[ JETPACK_CONNECTION_STATUS_REQUEST_SUCCESS ]: createRequestReducer( false )
 } );
 
+/**
+ * `Reducer` function which handles request/response actions
+ * concerning Jetpack user connection data
+ *
+ * @param  {Array}  state  Current state
+ * @param  {Object} action action
+ * @return {Array}         Updated state
+ */
+export const dataItems = createReducer( {}, {
+	[ JETPACK_USER_CONNECTION_DATA_RECEIVE ]: ( state, { siteId, data } ) => Object.assign( {}, state, { [ siteId ]: data } )
+} );
+
+/**
+ * `Reducer` function which handles request/response actions
+ * concerning Jetpack user connection data requests
+ *
+ * @param {Object} state - current state
+ * @param {Object} action - action
+ * @return {Object} updated state
+ */
+export const dataRequests = createReducer( {}, {
+	[ JETPACK_USER_CONNECTION_DATA_REQUEST ]: createRequestReducer( true ),
+	[ JETPACK_USER_CONNECTION_DATA_REQUEST_FAILURE ]: createRequestReducer( false ),
+	[ JETPACK_USER_CONNECTION_DATA_REQUEST_SUCCESS ]: createRequestReducer( false )
+} );
+
 export const disconnectRequests = createReducer( {}, {
 	[ JETPACK_DISCONNECT_REQUEST ]: createRequestReducer( true ),
 	[ JETPACK_DISCONNECT_REQUEST_FAILURE ]: createRequestReducer( false ),
@@ -54,5 +84,7 @@ export const disconnectRequests = createReducer( {}, {
 export const reducer = combineReducers( {
 	items,
 	requests,
+	dataItems,
+	dataRequests,
 	disconnectRequests
 } );
