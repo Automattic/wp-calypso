aee0e19a32dc1d81729e9ba1bcfe80fddabd870f
Jorge Costa <jorge.costa@developer.pt>
2017-06-28 23:29:25 +0100

    Framework: Created 3 new selectors (isMainSiteOf, getNetworkSites, isConnectedSecondaryNetworkSite).
    This selectors will be used as a replacement of current site-list methods.

---
+++ client/state/selectors/get-network-sites.js
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import createSelector from 'lib/create-selector';
+import { isMainSiteOf } from 'state/selectors';
+import {
+	getSite,
+	isJetpackSiteMainNetworkSite
+} from 'state/sites/selectors';
+
+/**
+ * Returns the secondary sites of a given site
+ * Returns null if main site is not found or if the site is not a main site
+ *
+ * @param  {Object}  state     Global state tree
+ * @param  {Number}  siteId    The ID of the main site for which we're retrieving the secondary sites
+ * @return {?Array}            Array of secondary sites
+ */
+export default createSelector(
+	( state, siteId ) => {
+		const isMainNetworkSite = isJetpackSiteMainNetworkSite( state, siteId );
+		if ( ! isMainNetworkSite ) {
+			return null;
+		}
+		const siteIds = Object.keys( get( state, 'sites.items', {} ) );
+		return siteIds.filter( secondarySiteId => isMainSiteOf( state, siteId, secondarySiteId ) )
+			.map( secondarySiteId => getSite( state, secondarySiteId ) );
+	},
+	( state ) => state.sites.items
+);
---
+++ client/state/selectors/test/is-main-site-of.js
@@ -0,0 +1,170 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { isMainSiteOf } from '../';
+
+describe( 'isMainSiteOf()', () => {
+	it( 'should return null if no sites exist in state', () => {
+		const state = {
+			sites: {
+				items: {}
+			}
+		};
+		expect( isMainSiteOf( state, 1, 2 ) ).be.null;
+	} );
+
+	it( 'should return null if no site with id equal to mainSiteId exists in state', () => {
+		const state = {
+			sites: {
+				items: {
+					2: {
+						ID: 2,
+						is_multisite: true,
+						jetpack: true,
+						options: {
+							unmapped_url: 'https://secondary.wordpress.com',
+							main_network_site: 'https://example.wordpress.com'
+						}
+					}
+				}
+			}
+		};
+		expect( isMainSiteOf( state, 1, 2 ) ).be.null;
+	} );
+
+	it( 'should return null if no site with id equal to secondarySiteId exists in state', () => {
+		const state = {
+			sites: {
+				items: {
+					1: {
+						ID: 1,
+						is_multisite: true,
+						jetpack: true,
+						options: {
+							unmapped_url: 'https://example.wordpress.com',
+							main_network_site: 'https://example.wordpress.com'
+						}
+					}
+				}
+			}
+		};
+		expect( isMainSiteOf( state, 1, 2 ) ).be.null;
+	} );
+
+	it( 'should return false if site mainSiteId is not a main site', () => {
+		const state = {
+			sites: {
+				items: {
+					1: {
+						ID: 1,
+						is_multisite: true,
+						jetpack: true,
+						options: {
+							unmapped_url: 'https://secondary.wordpress.com',
+							main_network_site: 'https://example.wordpress.com'
+						}
+					},
+					2: {
+						ID: 2,
+						is_multisite: true,
+						jetpack: true,
+						options: {
+							unmapped_url: 'https://secondary2.wordpress.com',
+							main_network_site: 'https://example.wordpress.com'
+						}
+					}
+				}
+			}
+		};
+		expect( isMainSiteOf( state, 1, 2 ) ).be.false;
+	} );
+
+	it( 'should return false if site secondarySiteId is not a secondary site', () => {
+		const state = {
+			sites: {
+				items: {
+					1: {
+						ID: 1,
+						is_multisite: true,
+						jetpack: true,
+						options: {
+							unmapped_url: 'https://example.wordpress.com',
+							main_network_site: 'https://example.wordpress.com'
+						}
+					},
+					2: {
+						ID: 2,
+						is_multisite: true,
+						jetpack: true,
+						options: {
+							unmapped_url: 'https://secondary.wordpress.com',
+							main_network_site: 'https://secondary.wordpress.com'
+						}
+					}
+				}
+			}
+		};
+		expect( isMainSiteOf( state, 1, 2 ) ).be.false;
+	} );
+
+	it( 'should return false if site mainSiteId is not the main site of site secondarySiteId', () => {
+		const state = {
+			sites: {
+				items: {
+					1: {
+						ID: 1,
+						is_multisite: true,
+						jetpack: true,
+						options: {
+							unmapped_url: 'https://example.wordpress.com',
+							main_network_site: 'https://example.wordpress.com'
+						}
+					},
+					2: {
+						ID: 2,
+						is_multisite: true,
+						jetpack: true,
+						options: {
+							unmapped_url: 'https://secondary.wordpress.com',
+							main_network_site: 'https://primary.wordpress.com'
+						}
+					}
+				}
+			}
+		};
+		expect( isMainSiteOf( state, 1, 2 ) ).be.false;
+	} );
+
+	it( 'should return true if site mainSiteId is the main site of site secondarySiteId', () => {
+		const state = {
+			sites: {
+				items: {
+					1: {
+						ID: 1,
+						is_multisite: true,
+						jetpack: true,
+						options: {
+							unmapped_url: 'https://example.wordpress.com',
+							main_network_site: 'https://example.wordpress.com'
+						}
+					},
+					2: {
+						ID: 2,
+						is_multisite: true,
+						jetpack: true,
+						options: {
+							unmapped_url: 'https://secondary.wordpress.com',
+							main_network_site: 'https://example.wordpress.com'
+						}
+					}
+				}
+			}
+		};
+		expect( isMainSiteOf( state, 1, 2 ) ).be.true;
+	} );
+} );
