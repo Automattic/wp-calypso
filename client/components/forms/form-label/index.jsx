9734126c0f6cbd6fd209bd4f076f8590c14634d6
James Koster <james@jameskoster.co.uk>
2017-06-28 17:05:01 +0100

    Add an optional 'required' prop to form labels

--- client/components/forms/form-label/index.jsx
+++ client/components/forms/form-label/index.jsx
@@ -9,10 +9,17 @@ module.exports = React.createClass( {
 
 	displayName: 'FormLabel',
 
+	propTypes: {
+		required: React.PropTypes.bool,
+	},
+
 	render: function() {
 		return (
 			<label { ...omit( this.props, 'className' ) } className={ classnames( this.props.className, 'form-label' ) } >
 				{ this.props.children }
+				{ this.props.required && (
+					<span className="form-label__required"> *</span>
+				) }
 			</label>
 		);
 	}
