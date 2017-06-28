f294f32ad0ec85d95a0c39b5593002564e5a97c9
Kevin Killingsworth <kevin@redfenix.com>
2017-06-28 15:55:53 -0500

    Woo: Add Product Variations API request
    This adds an API call to get all variations for a given product.

--- client/extensions/woocommerce/state/data-layer/product-variations/index.js
+++ client/extensions/woocommerce/state/data-layer/product-variations/index.js
@@ -7,16 +7,36 @@ import { isNumber } from 'lodash';
  * Internal dependencies
  */
 import { dispatchWithProps } from 'woocommerce/state/helpers';
-import { post } from 'woocommerce/state/data-layer/request/actions';
+import { get, post } from 'woocommerce/state/data-layer/request/actions';
 import { setError } from 'woocommerce/state/sites/status/wc-api/actions';
 import { productVariationUpdated } from 'woocommerce/state/sites/product-variations/actions';
 import {
 	WOOCOMMERCE_PRODUCT_VARIATION_CREATE,
+	WOOCOMMERCE_PRODUCT_VARIATIONS_REQUEST,
 } from 'woocommerce/state/action-types';
 
 export default {
 	[ WOOCOMMERCE_PRODUCT_VARIATION_CREATE ]: [ handleProductVariationCreate ],
+	[ WOOCOMMERCE_PRODUCT_VARIATIONS_REQUEST ]: [ handleProductVariationsRequest ],
 };
+
+export function handleProductVariationsRequest( store, action ) {
+	const { siteId, productId, successAction, failureAction } = action;
+
+	const updatedAction = ( dispatch, getState, { data } ) => {
+		data.map( ( variation ) => {
+			return dispatch( productVariationUpdated(
+				siteId, productId, variation, action
+			) );
+		} );
+
+		const props = { productId, receivedData: data };
+		dispatchWithProps( dispatch, getState, successAction, props );
+	};
+
+	const endpoint = 'products/' + productId + '/variations';
+	store.dispatch( get( siteId, endpoint, updatedAction, failureAction ) );
+}
 
 export function handleProductVariationCreate( store, action ) {
 	const { siteId, productId, variation, successAction, failureAction } = action;
