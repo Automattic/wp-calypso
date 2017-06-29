2e0e4f8d79ee7d79199742594e9b8c38f590254a
bluefuton <chris@bluefuton.com>
2017-06-29 09:21:40 -0400

    Change title to match existing translation

--- client/blocks/blog-stickers/list.jsx
+++ client/blocks/blog-stickers/list.jsx
@@ -8,7 +8,7 @@ import { localize } from 'i18n-calypso';
 const BlogStickersList = ( { stickers, translate } ) => {
 	return (
 		<div className="blog-stickers__list">
-			<h3 className="blog-stickers__list-title">{ translate( 'Blog stickers' ) }</h3>
+			<h3 className="blog-stickers__list-title">{ translate( 'Blog Stickers' ) }</h3>
 			<ul className="blog-stickers__list-ul">
 				{ map( stickers, sticker => {
 					return <li className="blog-stickers__list-item" key={ sticker }>{ sticker }</li>;
