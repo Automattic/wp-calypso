c891d39aab66c3ac43e1e905d86bdcdf768735ab
Benoit Person <benoit.person@gmail.com>
2017-06-29 14:27:39 +0200

    Editor Revisions: back to stateful component to deal with `react/jsx-no-bind`

--- client/post-editor/editor-revisions/index.jsx
+++ client/post-editor/editor-revisions/index.jsx
@@ -3,8 +3,7 @@
  */
 import Gridicon from 'gridicons';
 import { localize } from 'i18n-calypso';
-import { partial } from 'lodash';
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 /**
  * Internal dependencies
@@ -12,17 +11,46 @@ import React, { PropTypes } from 'react';
 import { isEnabled } from 'config';
 import { NESTED_SIDEBAR_REVISIONS } from 'post-editor/editor-sidebar/constants';
 
-function EditorRevisions( { adminUrl, revisions = [], translate, toggleNestedSidebar } ) {
-	if ( ! revisions || ! revisions.length ) {
-		return null;
+class EditorRevisions extends Component {
+	showRevisionsNestedSidebar = () => {
+		this.props.toggleNestedSidebar( NESTED_SIDEBAR_REVISIONS );
 	}
 
-	if ( isEnabled( 'post-editor/revisions' ) ) {
+	render() {
+		const { adminUrl, revisions, translate } = this.props;
+
+		if ( ! revisions || ! revisions.length ) {
+			return null;
+		}
+
+		if ( isEnabled( 'post-editor/revisions' ) ) {
+			return (
+				<button
+					className="editor-revisions"
+					title={ translate( 'Open list of revisions' ) }
+					onClick={ this.showRevisionsNestedSidebar }
+				>
+					<Gridicon icon="history" size={ 18 } />
+					{ translate(
+						'%(revisions)d revision',
+						'%(revisions)d revisions', {
+							count: revisions.length,
+							args: { revisions: revisions.length },
+						}
+					) }
+				</button>
+			);
+		}
+
+		const lastRevision = revisions[ 0 ];
+		const revisionsLink = adminUrl + 'revision.php?revision=' + lastRevision;
 		return (
-			<button
+			<a
 				className="editor-revisions"
-				title={ translate( 'Open list of revisions' ) }
-				onClick={ partial( toggleNestedSidebar, NESTED_SIDEBAR_REVISIONS ) }
+				href={ revisionsLink }
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label={ translate( 'Open list of revisions' ) }
 			>
 				<Gridicon icon="history" size={ 18 } />
 				{ translate(
@@ -32,30 +60,9 @@ function EditorRevisions( { adminUrl, revisions = [], translate, toggleNestedSid
 						args: { revisions: revisions.length },
 					}
 				) }
-			</button>
+			</a>
 		);
 	}
-
-	const lastRevision = revisions[ 0 ];
-	const revisionsLink = adminUrl + 'revision.php?revision=' + lastRevision;
-	return (
-		<a
-			className="editor-revisions"
-			href={ revisionsLink }
-			target="_blank"
-			rel="noopener noreferrer"
-			aria-label={ translate( 'Open list of revisions' ) }
-		>
-			<Gridicon icon="history" size={ 18 } />
-			{ translate(
-				'%(revisions)d revision',
-				'%(revisions)d revisions', {
-					count: revisions.length,
-					args: { revisions: revisions.length },
-				}
-			) }
-		</a>
-	);
 }
 
 EditorRevisions.propTypes = {
