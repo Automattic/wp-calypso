90f3d293ed35d0aba376c5cb7b8ff011a60e25ca
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-06-30 22:51:55 +0100

    Changed the shipping-zone-locations reducer code structure so it's easier to extend

--- client/extensions/woocommerce/state/sites/shipping-zone-locations/reducer.js
+++ client/extensions/woocommerce/state/sites/shipping-zone-locations/reducer.js
@@ -10,44 +10,46 @@ import {
 import { LOADING } from 'woocommerce/state/constants';
 import { createReducer } from 'state/utils';
 
-export default createReducer( {}, {
-	[ WOOCOMMERCE_SHIPPING_ZONE_LOCATIONS_REQUEST ]: ( state, { zoneId } ) => {
-		return { ...state,
-			[ zoneId ]: LOADING,
-		};
-	},
+const reducers = {};
 
-	[ WOOCOMMERCE_SHIPPING_ZONE_LOCATIONS_REQUEST_SUCCESS ]: ( state, { data, zoneId } ) => {
-		const locations = {
+reducers[ WOOCOMMERCE_SHIPPING_ZONE_LOCATIONS_REQUEST ] = ( state, { zoneId } ) => {
+	return { ...state,
+		[ zoneId ]: LOADING,
+	};
+};
+
+reducers[ WOOCOMMERCE_SHIPPING_ZONE_LOCATIONS_REQUEST_SUCCESS ] = ( state, { data, zoneId } ) => {
+	const locations = {
+		continent: [],
+		country: [],
+		state: [],
+		postcode: [],
+	};
+	data.forEach( ( { type, code } ) => locations[ type ].push( code ) );
+	return { ...state,
+		[ zoneId ]: locations,
+	};
+};
+
+reducers[ WOOCOMMERCE_SHIPPING_ZONE_UPDATED ] = ( state, { data, originatingAction: { zone } } ) => {
+	if ( 'number' === typeof zone.id ) {
+		return state;
+	}
+
+	return { ...state,
+		[ data.id ]: {
 			continent: [],
 			country: [],
 			state: [],
 			postcode: [],
-		};
-		data.forEach( ( { type, code } ) => locations[ type ].push( code ) );
-		return { ...state,
-			[ zoneId ]: locations,
-		};
-	},
+		},
+	};
+};
 
-	[ WOOCOMMERCE_SHIPPING_ZONE_UPDATED ]: ( state, { data, originatingAction: { zone } } ) => {
-		if ( 'number' === typeof zone.id ) {
-			return state;
-		}
+reducers[ WOOCOMMERCE_SHIPPING_ZONE_DELETED ] = ( state, { originatingAction: { zone } } ) => {
+	const newState = { ...state };
+	delete newState[ zone.id ];
+	return newState;
+};
 
-		return { ...state,
-			[ data.id ]: {
-				continent: [],
-				country: [],
-				state: [],
-				postcode: [],
-			},
-		};
-	},
-
-	[ WOOCOMMERCE_SHIPPING_ZONE_DELETED ]: ( state, { originatingAction: { zone } } ) => {
-		const newState = { ...state };
-		delete newState[ zone.id ];
-		return newState;
-	},
-} );
+export default createReducer( {}, reducers );
