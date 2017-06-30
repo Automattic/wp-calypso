ad75270e1266b2e32b7166bae11204b1dfb40008
Donna Peplinskie <donnapep@gmail.com>
2017-06-30 07:51:20 -0400

    Add actions, reducer and selectors for saving settings

--- client/extensions/wp-job-manager/state/settings/selectors.js
+++ client/extensions/wp-job-manager/state/settings/selectors.js
@@ -19,6 +19,17 @@ export function isFetchingSettings( state, siteId ) {
 }
 
 /**
+ * Returns true if we are saving settings for the specified site ID, false otherwise.
+ *
+ * @param  {Object}  state Global state tree
+ * @param  {Number}  siteId Site ID
+ * @return {Boolean} Whether settings are being saved
+ */
+export function isSavingSettings( state, siteId ) {
+	return get( getSettingsState( state ), [ 'saving', siteId ], false );
+}
+
+/**
  * Returns the settings for the specified site ID.
  *
  * @param  {Object} state Global state tree
