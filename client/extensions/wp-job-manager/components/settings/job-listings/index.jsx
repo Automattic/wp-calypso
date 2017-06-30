f0e310b18898246440d349166463af1492c96339
Donna Peplinskie <donnapep@gmail.com>
2017-06-30 11:58:02 -0400

    <form> element should wrap <FormButton> component as well
    To facilitate saving of partial form data, introduce <FormSection> components
    
    When a particular ‘Save Settings’ button is clicked, only the settings
    in that particular section should be saved. To facilitate this, the
    `redux-form` <FormSection> component can be used to differentiate
    between areas of the form.

--- client/extensions/wp-job-manager/components/settings/job-listings/index.jsx
+++ client/extensions/wp-job-manager/components/settings/job-listings/index.jsx
@@ -3,7 +3,7 @@
  */
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { formValueSelector, reduxForm } from 'redux-form';
+import { FormSection, formValueSelector, reduxForm } from 'redux-form';
 import { localize } from 'i18n-calypso';
 import { flowRight } from 'lodash';
 
@@ -24,196 +24,206 @@ import SectionHeader from 'components/section-header';
 const JobListings = ( { isSaving, perPage, translate } ) => {
 	return (
 		<div>
-			<SectionHeader label={ translate( 'Listings' ) }>
-				<FormButton compact
-					isSubmitting={ isSaving } />
-			</SectionHeader>
-			<Card>
-				<form>
-					<p>
-						{ translate(
-							'Display {{listings /}} job listing per page',
-							'Display {{listings /}} job listings per page',
-							{
-								count: perPage,
-								components: {
-									listings:
-										<ReduxFormTextInput
-											min="0"
-											name="job_manager_per_page"
-											step="1"
-											type="number" />
-								}
-							}
-						) }
-					</p>
-
-					<FormFieldset>
-						<ReduxFormToggle
-							name="job_manager_hide_filled_positions"
-							text={ translate( 'Hide filled positions' ) } />
-						<FormSettingExplanation isIndented>
-							{ translate( 'Filled positions will not display in your archives.' ) }
-						</FormSettingExplanation>
-
-						<ReduxFormToggle
-							name="job_manager_hide_expired"
-							text={ translate( 'Hide expired listings in job archives/search' ) } />
-						<FormSettingExplanation isIndented>
-							{ translate( 'Expired job listings will not be searchable.' ) }
-						</FormSettingExplanation>
-
-						<ReduxFormToggle
-							name="job_manager_hide_expired_content"
-							text={ translate( 'Hide content in expired single job listings' ) } />
-						<FormSettingExplanation isIndented>
-							{ translate( 'Your site will display the titles of expired listings, but not the ' +
-								'content of the listings. Otherwise, expired listings display their full content ' +
-								'minus the application area.' ) }
-						</FormSettingExplanation>
-					</FormFieldset>
-				</form>
-			</Card>
-
-			<SectionHeader label={ translate( 'Categories' ) }>
-				<FormButton compact
-					isSubmitting={ isSaving } />
-			</SectionHeader>
-			<Card>
-				<form>
-					<FormFieldset>
-						<ReduxFormToggle
-							name="job_manager_enable_categories"
-							text={ translate( 'Enable listing categories' ) } />
-						<FormSettingExplanation isIndented>
-							{ translate( 'This lets users select from a list of categories when submitting a ' +
-								'job. Note!: an admin has to create categories before site users can select them.' ) }
-						</FormSettingExplanation>
-
-						<ReduxFormToggle
-							name="job_manager_enable_default_category_multiselect"
-							text={ translate( 'Default to category multiselect' ) } />
-						<FormSettingExplanation isIndented>
-							{ translate( 'The category selection box will default to allowing multiple ' +
-								'selections on the [jobs] shortcode. Without this, users will only be able to ' +
-								'select a single category when submitting jobs.' ) }
-						</FormSettingExplanation>
-					</FormFieldset>
-
-					<FormFieldset>
-						<FormLabel>
-							{ translate( 'Category Filter Type' ) }
-						</FormLabel>
-						<FormSettingExplanation>
-							{ translate( 'Determines the logic used to display jobs when selecting multiple categories.' ) }
-						</FormSettingExplanation>
-						<FormLabel>
-							<ReduxFormRadio
-								name="job_manager_category_filter_type"
-								value="any" />
-							<span>
-								{ translate( 'Jobs will be shown if within ANY selected category' ) }
-							</span>
-						</FormLabel>
-
-						<FormLabel>
-							<ReduxFormRadio
-								name="job_manager_category_filter_type"
-								value="all" />
-							<span>
-								{ translate( 'Jobs will be shown if within ALL selected categories' ) }
-							</span>
-						</FormLabel>
-					</FormFieldset>
-				</form>
-			</Card>
-
-			<SectionHeader label={ translate( 'Types' ) }>
-				<FormButton compact
-					isSubmitting={ isSaving } />
-			</SectionHeader>
-			<Card>
-				<form>
-					<FormFieldset>
-						<ReduxFormToggle
-							name="job_manager_enable_types"
-							text={ translate( 'Enable listing types' ) } />
-						<FormSettingExplanation isIndented>
-							{ translate( 'This lets users select from a list of types when submitting a job. ' +
-								'Note!: an admin has to create types before site users can select them.' ) }
-						</FormSettingExplanation>
-
-						<ReduxFormToggle
-							name="job_manager_multi_job_type"
-							text={ translate( 'Allow multiple types for listings' ) } />
-						<FormSettingExplanation isIndented>
-							{ translate( 'This allows users to select more than one type when submitting a job. ' +
-								'The metabox on the post editor and the selection box on the front-end job ' +
-								'submission form will both reflect this.' ) }
-						</FormSettingExplanation>
-					</FormFieldset>
-				</form>
-			</Card>
-
-			<SectionHeader label={ translate( 'Date Format' ) }>
-				<FormButton compact
-					isSubmitting={ isSaving } />
-			</SectionHeader>
-			<Card>
-				<form>
-					<FormFieldset>
-						<FormSettingExplanation>
-							{ translate( 'Choose how you want the published date for jobs to be displayed on the front-end.' ) }
-						</FormSettingExplanation>
-						<FormLabel>
-							<ReduxFormRadio
-								name="job_manager_date_format"
-								value="relative" />
-							<span>
-								{ translate( 'Relative to the current date (e.g., 1 day, 1 week, 1 month ago)' ) }
-							</span>
-						</FormLabel>
-
-						<FormLabel>
-							<ReduxFormRadio
-								name="job_manager_date_format"
-								value="default" />
-							<span>
-								{ translate( 'Default date format as defined in Settings' ) }
-							</span>
-						</FormLabel>
-					</FormFieldset>
-				</form>
-			</Card>
-
-			<SectionHeader label={ translate( 'Google Maps API Key' ) }>
-				<FormButton compact
-					isSubmitting={ isSaving } />
-			</SectionHeader>
-			<Card>
-				<form>
-					<FormFieldset>
-						<ReduxFormTextInput name="job_manager_google_maps_api_key" />
-						<FormSettingExplanation>
+			<form>
+				<FormSection name="listings">
+					<SectionHeader label={ translate( 'Listings' ) }>
+						<FormButton compact
+							isSubmitting={ isSaving } />
+					</SectionHeader>
+					<Card>
+						<p>
 							{ translate(
-								'Google requires an API key to retrieve location information for job listings. ' +
-								'Acquire an API key from the {{a}}Google Maps API developer site{{/a}}.',
+								'Display {{listings /}} job listing per page',
+								'Display {{listings /}} job listings per page',
 								{
+									count: perPage,
 									components: {
-										a: (
-											<ExternalLink
-												icon={ true }
-												target="_blank"
-												href="https://developers.google.com/maps/documentation/geocoding/get-api-key"
-											/>
-										),
+										listings:
+											<ReduxFormTextInput
+												min="0"
+												name="job_manager_per_page"
+												step="1"
+												type="number" />
 									}
 								}
 							) }
-						</FormSettingExplanation>
-					</FormFieldset>
-				</form>
-			</Card>
+						</p>
+
+						<FormFieldset>
+							<ReduxFormToggle
+								name="job_manager_hide_filled_positions"
+								text={ translate( 'Hide filled positions' ) } />
+							<FormSettingExplanation isIndented>
+								{ translate( 'Filled positions will not display in your archives.' ) }
+							</FormSettingExplanation>
+
+							<ReduxFormToggle
+								name="job_manager_hide_expired"
+								text={ translate( 'Hide expired listings in job archives/search' ) } />
+							<FormSettingExplanation isIndented>
+								{ translate( 'Expired job listings will not be searchable.' ) }
+							</FormSettingExplanation>
+
+							<ReduxFormToggle
+								name="job_manager_hide_expired_content"
+								text={ translate( 'Hide content in expired single job listings' ) } />
+							<FormSettingExplanation isIndented>
+								{ translate( 'Your site will display the titles of expired listings, but not the ' +
+									'content of the listings. Otherwise, expired listings display their full content ' +
+									'minus the application area.' ) }
+							</FormSettingExplanation>
+						</FormFieldset>
+					</Card>
+				</FormSection>
+			</form>
+
+			<form>
+				<FormSection name="categories">
+					<SectionHeader label={ translate( 'Categories' ) }>
+						<FormButton compact
+							isSubmitting={ isSaving } />
+					</SectionHeader>
+					<Card>
+						<FormFieldset>
+							<ReduxFormToggle
+								name="job_manager_enable_categories"
+								text={ translate( 'Enable listing categories' ) } />
+							<FormSettingExplanation isIndented>
+								{ translate( 'This lets users select from a list of categories when submitting a ' +
+									'job. Note!: an admin has to create categories before site users can select them.' ) }
+							</FormSettingExplanation>
+
+							<ReduxFormToggle
+								name="job_manager_enable_default_category_multiselect"
+								text={ translate( 'Default to category multiselect' ) } />
+							<FormSettingExplanation isIndented>
+								{ translate( 'The category selection box will default to allowing multiple ' +
+									'selections on the [jobs] shortcode. Without this, users will only be able to ' +
+									'select a single category when submitting jobs.' ) }
+							</FormSettingExplanation>
+						</FormFieldset>
+
+						<FormFieldset>
+							<FormLabel>
+								{ translate( 'Category Filter Type' ) }
+							</FormLabel>
+							<FormSettingExplanation>
+								{ translate( 'Determines the logic used to display jobs when selecting multiple categories.' ) }
+							</FormSettingExplanation>
+							<FormLabel>
+								<ReduxFormRadio
+									name="job_manager_category_filter_type"
+									value="any" />
+								<span>
+									{ translate( 'Jobs will be shown if within ANY selected category' ) }
+								</span>
+							</FormLabel>
+
+							<FormLabel>
+								<ReduxFormRadio
+									name="job_manager_category_filter_type"
+									value="all" />
+								<span>
+									{ translate( 'Jobs will be shown if within ALL selected categories' ) }
+								</span>
+							</FormLabel>
+						</FormFieldset>
+					</Card>
+				</FormSection>
+			</form>
+
+			<form>
+				<FormSection name="types">
+					<SectionHeader label={ translate( 'Types' ) }>
+						<FormButton compact
+							isSubmitting={ isSaving } />
+					</SectionHeader>
+					<Card>
+						<FormFieldset>
+							<ReduxFormToggle
+								name="job_manager_enable_types"
+								text={ translate( 'Enable listing types' ) } />
+							<FormSettingExplanation isIndented>
+								{ translate( 'This lets users select from a list of types when submitting a job. ' +
+									'Note!: an admin has to create types before site users can select them.' ) }
+							</FormSettingExplanation>
+
+							<ReduxFormToggle
+								name="job_manager_multi_job_type"
+								text={ translate( 'Allow multiple types for listings' ) } />
+							<FormSettingExplanation isIndented>
+								{ translate( 'This allows users to select more than one type when submitting a job. ' +
+									'The metabox on the post editor and the selection box on the front-end job ' +
+									'submission form will both reflect this.' ) }
+							</FormSettingExplanation>
+						</FormFieldset>
+					</Card>
+				</FormSection>
+			</form>
+
+			<form>
+				<FormSection name="format">
+					<SectionHeader label={ translate( 'Date Format' ) }>
+						<FormButton compact
+							isSubmitting={ isSaving } />
+					</SectionHeader>
+					<Card>
+						<FormFieldset>
+							<FormSettingExplanation>
+								{ translate( 'Choose how you want the published date for jobs to be displayed on the front-end.' ) }
+							</FormSettingExplanation>
+							<FormLabel>
+								<ReduxFormRadio
+									name="job_manager_date_format"
+									value="relative" />
+								<span>
+									{ translate( 'Relative to the current date (e.g., 1 day, 1 week, 1 month ago)' ) }
+								</span>
+							</FormLabel>
+
+							<FormLabel>
+								<ReduxFormRadio
+									name="job_manager_date_format"
+									value="default" />
+								<span>
+									{ translate( 'Default date format as defined in Settings' ) }
+								</span>
+							</FormLabel>
+						</FormFieldset>
+					</Card>
+				</FormSection>
+			</form>
+
+			<form>
+				<FormSection name="apiKey">
+					<SectionHeader label={ translate( 'Google Maps API Key' ) }>
+						<FormButton compact
+							isSubmitting={ isSaving } />
+					</SectionHeader>
+					<Card>
+						<FormFieldset>
+							<ReduxFormTextInput name="job_manager_google_maps_api_key" />
+							<FormSettingExplanation>
+								{ translate(
+									'Google requires an API key to retrieve location information for job listings. ' +
+									'Acquire an API key from the {{a}}Google Maps API developer site{{/a}}.',
+									{
+										components: {
+											a: (
+												<ExternalLink
+													icon={ true }
+													target="_blank"
+													href="https://developers.google.com/maps/documentation/geocoding/get-api-key"
+												/>
+											),
+										}
+									}
+								) }
+							</FormSettingExplanation>
+						</FormFieldset>
+					</Card>
+				</FormSection>
+			</form>
 		</div>
 	);
 };
@@ -224,7 +234,7 @@ JobListings.propTypes = {
 
 const connectComponent = connect(
 	( state ) => {
-		const selector = formValueSelector( 'listings', () => state.extensions.wpJobManager.form );
+		const selector = formValueSelector( 'jobListings', () => state.extensions.wpJobManager.form );
 
 		return {
 			perPage: selector( state, 'job_manager_per_page' ),
@@ -234,7 +244,7 @@ const connectComponent = connect(
 
 const createReduxForm = reduxForm( {
 	enableReinitialize: true,
-	form: 'listings',
+	form: 'jobListings',
 	getFormState: state => state.extensions.wpJobManager.form,
 } );
 
