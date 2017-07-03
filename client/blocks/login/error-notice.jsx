Automattic/wp-calypso

commit c968da96895e10c59b3f6e3482f9d0a122442bac

Pass social account creation through redux (#14848)
Seperate concerns and display proper error message
    Also changed the error notice message and moved it
    to be inline instead of global notice

    Reset bearer token when user is logged in
    To help mitigate XSS by keeping it shortest time possible
    in the store

client/blocks/login/index.jsx
@@ -17,6 +17,8 @@ import {
 	getRequestNotice,
 	getTwoFactorAuthRequestError,
 	getTwoFactorNotificationSent,
+	getCreateSocialAccountError,
+	getRequestSocialAccountError,
 	isTwoFactorEnabled,
 } from 'state/login/selectors';
 import { recordTracksEvent } from 'state/analytics/actions';
@@ -90,9 +92,12 @@ class Login extends Component {
 	};
 
 	renderError() {
-		const error = this.props.requestError || this.props.twoFactorAuthRequestError;
+		const error = this.props.requestError ||
+			this.props.twoFactorAuthRequestError ||
+			this.props.requestAccountError ||
+			this.props.createAccountError && this.props.createAccountError.code !== 'unknown_user' ? this.props.createAccountError : null;
 
-		if ( ! error || error.field !== 'global' ) {
+		if ( ! error || ( error.field && error.field !== 'global' ) || ! error.message ) {
 			return null;
 		}
 
@@ -177,6 +182,8 @@ class Login extends Component {
 export default connect(
 	( state ) => ( {
 		redirectTo: getRedirectTo( state ),
+		createAccountError: getCreateSocialAccountError( state ),
+		requestAccountError: getRequestSocialAccountError( state ),
 		requestError: getRequestError( state ),
 		requestNotice: getRequestNotice( state ),
 		twoFactorAuthRequestError: getTwoFactorAuthRequestError( state ),

client/state/login/reducer.js
@@ -29,6 +29,7 @@ import {
 	TWO_FACTOR_AUTHENTICATION_SEND_SMS_CODE_REQUEST_FAILURE,
 	TWO_FACTOR_AUTHENTICATION_SEND_SMS_CODE_REQUEST_SUCCESS,
 	TWO_FACTOR_AUTHENTICATION_UPDATE_NONCE,
+	USER_RECEIVE,
 } from 'state/action-types';
 
 export const isRequesting = createReducer( false, {
@@ -124,6 +125,22 @@ export const twoFactorAuthPushPoll = createReducer( { inProgress: false, success
 	[ TWO_FACTOR_AUTHENTICATION_PUSH_POLL_COMPLETED ]: state => ( { ...state, inProgress: false, success: true } ),
 } );
 
+export const socialAccount = createReducer( { isCreating: false }, {
+	[ SOCIAL_CREATE_ACCOUNT_REQUEST ]: () => ( { isCreating: true } ),
+	[ SOCIAL_CREATE_ACCOUNT_REQUEST_FAILURE ]: ( state, { error } ) => ( { isCreating: false, createError: error } ),
+	[ SOCIAL_CREATE_ACCOUNT_REQUEST_SUCCESS ]: ( state, { data: { username, bearerToken } } ) => ( {
+		isCreating: false,
+		username,
+		bearerToken
+	} ),
+	[ SOCIAL_LOGIN_REQUEST_FAILURE ]: ( state, { error } ) => ( {
+		...state,
+		requestError: error
+	} ),
+	[ USER_RECEIVE ]: state => ( { ...state, bearerToken: null, username: null } ),
+	[ LOGIN_REQUEST ]: state => ( { ...state, createError: false } ),
+} );
+
 export default combineReducers( {
 	isRequesting,
 	isRequestingTwoFactorAuth,
@@ -136,4 +153,5 @@ export default combineReducers( {
 	twoFactorAuth,
 	twoFactorAuthRequestError,
 	twoFactorAuthPushPoll,
+	socialAccount,
 } );

client/state/login/test/reducer.js
@@ -41,6 +41,7 @@ describe( 'reducer', () => {
 			'requestError',
 			'requestNotice',
 			'requestSuccess',
+			'socialAccount',
 			'twoFactorAuth',
 			'isRequestingTwoFactorAuth',
 			'twoFactorAuthRequestError',

client/blocks/global-notice/index.js
@@ -1,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { infoNotice, removeNotice } from 'state/notices/actions';
+
+export class GlobalNotice extends Component {
+	static propTypes = {
+		displayNotice: PropTypes.func.isRequired,
+		removeNotice: PropTypes.func.isRequired,
+		text: PropTypes.string.isRequired,
+	};
+
+	componentWillMount() {
+		const { notice } = this.props.displayNotice( this.props.text, { isPersistent: true } );
+		this.notice = notice;
+	}
+
+	componentWillUnmount() {
+		if ( this.notice ) {
+			this.props.removeNotice( this.notice.noticeId );
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export const InfoNotice = connect(
+	null,
+	{
+		displayNotice: infoNotice,
+		removeNotice,
+	}
+)( GlobalNotice );

client/blocks/login/social.jsx
@@ -11,25 +11,24 @@ import { localize } from 'i18n-calypso';
  */
 import config from 'config';
 import { getCurrentQueryArguments } from 'state/ui/selectors';
-import { createSocialUser, loginSocialUser } from 'state/login/actions';
-import { errorNotice, infoNotice, removeNotice } from 'state/notices/actions';
+import { loginSocialUser, createSocialUser } from 'state/login/actions';
+import {
+	getCreatedSocialAccountUsername,
+	getCreatedSocialAccountBearerToken,
+	isSocialAccountCreating,
+} from 'state/login/selectors';
 import { recordTracksEvent } from 'state/analytics/actions';
 import WpcomLoginForm from 'signup/wpcom-login-form';
+import { InfoNotice } from 'blocks/global-notice';
 
 class SocialLoginForm extends Component {
 	static propTypes = {
-		errorNotice: PropTypes.func.isRequired,
-		infoNotice: PropTypes.func.isRequired,
+		createSocialUser: PropTypes.func.isRequired,
 		recordTracksEvent: PropTypes.func.isRequired,
 		redirectTo: PropTypes.string,
-		removeNotice: PropTypes.func.isRequired,
 		onSuccess: PropTypes.func.isRequired,
 		translate: PropTypes.func.isRequired,
-	};
-
-	state = {
-		username: null,
-		bearerToken: null,
+		loginSocialUser: PropTypes.func.isRequired,
 	};
 
 	handleGoogleResponse = ( response ) => {
@@ -39,46 +38,40 @@ class SocialLoginForm extends Component {
 			return;
 		}
 
-		this.props.loginSocialUser( 'google', response.Zi.id_token, redirectTo ).then( () => {
-			this.props.recordTracksEvent( 'calypso_login_social_login_success', {
-				social_account_type: 'google',
-			} );
+		this.props.loginSocialUser( 'google', response.Zi.id_token, redirectTo )
+			.then(
+				() => {
+					this.recordEvent( 'calypso_login_social_login_success' );
 
-			onSuccess();
-		} ).catch( error => {
-			if ( error.code === 'unknown_user' ) {
-				this.props.createSocialUser( 'google', response.Zi.id_token, 'login' ).then( wpcomResponse => {
-					this.props.recordTracksEvent( 'calypso_login_social_signup_success', {
-						social_account_type: 'google',
-					} );
+					onSuccess();
+				},
+				error => {
+					if ( error.code === 'unknown_user' ) {
+						return this.props.createSocialUser( 'google', response.Zi.id_token, 'login' )
+							.then(
+								() => this.recordEvent( 'calypso_login_social_signup_success' ),
+								createAccountError => this.recordEvent( 'calypso_login_social_signup_failure', {
+									error_code: createAccountError.code,
+									error_message: createAccountError.message
+								} )
+							);
+					}
 
-					this.setState( {
-						username: wpcomResponse.username,
-						bearerToken: wpcomResponse.bearer_token
+					this.recordEvent( 'calypso_login_social_login_failure', {
+						error_code: error.code,
+						error_message: error.message
 					} );
-				} ).catch( wpcomError => {
-					this.props.recordTracksEvent( 'calypso_login_social_signup_failure', {
-						social_account_type: 'google',
-						error_code: wpcomError.error,
-						error_message: wpcomError.message
-					} );
-				} );
-			} else {
-				this.props.recordTracksEvent( 'calypso_login_social_login_failure', {
-					social_account_type: 'google',
-					error_code: error.code,
-					error_message: error.message
-				} );
-
-				this.props.errorNotice( error.message );
-			}
-		} );
+				}
+			);
 	};
 
+	recordEvent = ( eventName, params ) => this.props.recordTracksEvent( eventName, {
+		social_account_type: 'google',
+		...params
+	} );
+
 	trackGoogleLogin = () => {
-		this.props.recordTracksEvent( 'calypso_login_social_button_click', {
-			social_account_type: 'google'
-		} );
+		this.recordEvent( 'calypso_login_social_button_click' );
 	};
 
 	render() {
@@ -95,10 +88,14 @@ class SocialLoginForm extends Component {
 						onClick={ this.trackGoogleLogin } />
 				</div>
 
-				{ this.state.bearerToken && (
+				{ this.props.isSocialAccountCreating &&
+					<InfoNotice text={ this.props.translate( 'Creating your account…' ) } />
+				}
+
+				{ this.props.bearerToken && (
 					<WpcomLoginForm
-						log={ this.state.username }
-						authorization={ 'Bearer ' + this.state.bearerToken }
+						log={ this.props.username }
+						authorization={ 'Bearer ' + this.props.bearerToken }
 						redirectTo="/start"
 					/>
 				) }
@@ -110,13 +107,13 @@ class SocialLoginForm extends Component {
 export default connect(
 	( state ) => ( {
 		redirectTo: getCurrentQueryArguments( state ).redirect_to,
+		isSocialAccountCreating: isSocialAccountCreating( state ),
+		bearerToken: getCreatedSocialAccountBearerToken( state ),
+		username: getCreatedSocialAccountUsername( state ),
 	} ),
 	{
-		errorNotice,
-		infoNotice,
-		removeNotice,
-		createSocialUser,
 		loginSocialUser,
+		createSocialUser,
 		recordTracksEvent,
 	}
 )( localize( SocialLoginForm ) );

client/state/login/actions.js
@@ -100,6 +100,24 @@ function getErrorFromHTTPError( httpError ) {
 	return { code, message, field };
 }
 
+const wpcomErrorMessages = {
+	user_exists: translate( 'Your Google email address is already in use WordPress.com. ' +
+		'Log in to your account using your email address or username, and your password. ' +
+		'To create a new WordPress.com account, use a different Google account.' )
+};
+
+/**
+ * Transforms WPCOM error to the error object we use for login purposes
+ *
+ * @param {Object} wpcomError HTTP error
+ * @returns {{message: string, field: string, code: string}} an error message and the id of the corresponding field
+ */
+const getErrorFromWPCOMError = ( wpcomError ) => ( {
+	message: wpcomErrorMessages[ wpcomError.error ] || wpcomError.message,
+	code: wpcomError.error,
+	field: 'global',
+} );
+
 /**
  * Attempt to login a user.
  *
@@ -245,10 +263,14 @@ export const createSocialUser = ( service, token, flowName ) => dispatch => {
 	} );
 
 	return wpcom.undocumented().usersSocialNew( service, token, flowName ).then( wpcomResponse => {
-		dispatch( { type: SOCIAL_CREATE_ACCOUNT_REQUEST_SUCCESS } );
-		return Promise.resolve( wpcomResponse );
-	} ).catch( wpcomError => {
-		const error = { ...wpcomError, field: 'global' };
+		const data = {
+			username: wpcomResponse.username,
+			bearerToken: wpcomResponse.bearer_token
+		};
+		dispatch( { type: SOCIAL_CREATE_ACCOUNT_REQUEST_SUCCESS, data } );
+		return data;
+	}, wpcomError => {
+		const error = getErrorFromWPCOMError( wpcomError );
 
 		dispatch( {
 			type: SOCIAL_CREATE_ACCOUNT_REQUEST_FAILURE,

client/state/login/selectors.js
@@ -166,3 +166,43 @@ export const getRedirectTo = ( state ) => {
 export const getRememberMe = ( state ) => {
 	return get( state, 'login.rememberMe', false );
 };
+
+/***
+ * Tells us if we're in a process of creating a social account
+ *
+ * @param  {Object}   state  Global state tree
+ * @return {?Boolean}         Error for the request.
+ */
+export const isSocialAccountCreating = ( state ) => get( state, 'login.socialAccount.isCreating', null );
+
+/***
+ * Gets Username of the created social account
+ *
+ * @param  {Object}   state  Global state tree
+ * @return {?String}         Username of the created social account
+ */
+export const getCreatedSocialAccountUsername = ( state ) => get( state, 'login.socialAccount.username', null );
+
+/***
+ * Gets Bearer token of the created social account
+ *
+ * @param  {Object}   state  Global state tree
+ * @return {?String}         Bearer token of the created social account
+ */
+export const getCreatedSocialAccountBearerToken = ( state ) => get( state, 'login.socialAccount.bearerToken', null );
+
+/***
+ * Gets error for the create social account request.
+ *
+ * @param  {Object}   state  Global state tree
+ * @return {?Object}         Error for the create social account request.
+ */
+export const getCreateSocialAccountError = ( state ) => get( state, 'login.socialAccount.createError', null );
+
+/***
+ * Gets error for the get social account request.
+ *
+ * @param  {Object}   state  Global state tree
+ * @return {?Object}         Error for the get social account request.
+ */
+export const getRequestSocialAccountError = ( state ) => get( state, 'login.socialAccount.requestError', null );

commit 30ae0328ac05c546dde751648f46fcb1e9116f8c

Login: Extract error notice logic into its own component
This also fixes two issues introduced in c968da96895e10c59b3f6e3482f9d0a122442bac in the process. First, it fixes 'global' notices that would not be displayed because the following condition is not taking into account operator precedence and would evaluate to false:

https://github.com/Automattic/wp-calypso/blob/c968da96895e10c59b3f6e3482f9d0a122442bac/client/blocks/login/index.jsx#L95-L98

Second, it makes sure that the window is scrolled back to the top for social account errors.

client/blocks/login/error-notice.jsx
@@ -1,0 +1,79 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import {
+	getRequestError,
+	getTwoFactorAuthRequestError,
+	getCreateSocialAccountError,
+	getRequestSocialAccountError,
+} from 'state/login/selectors';
+import Notice from 'components/notice';
+
+class ErrorNotice extends Component {
+	static propTypes = {
+		createAccountError: PropTypes.object,
+		requestAccountError: PropTypes.object,
+		requestError: PropTypes.object,
+		twoFactorAuthRequestError: PropTypes.object,
+	};
+
+	componentWillReceiveProps = ( nextProps ) => {
+		const receiveNewError = ( key ) => {
+			return this.props[ key ] !== nextProps[ key ];
+		};
+
+		if (
+			receiveNewError( 'createAccountError' ) ||
+			receiveNewError( 'requestAccountError' ) ||
+			receiveNewError( 'requestError' ) ||
+			receiveNewError( 'twoFactorAuthRequestError' )
+		) {
+			window.scrollTo( 0, 0 );
+		}
+	}
+
+	getCreateAccountError() {
+		const { createAccountError } = this.props;
+
+		if ( createAccountError && createAccountError.code !== 'unknown_user' ) {
+			return createAccountError;
+		}
+
+		return null;
+	}
+
+	getError() {
+		const { requestAccountError, requestError, twoFactorAuthRequestError } = this.props;
+
+		return requestError || twoFactorAuthRequestError || requestAccountError || this.getCreateAccountError();
+	}
+
+	render() {
+		const error = this.getError();
+
+		if ( ! error || ( error.field && error.field !== 'global' ) || ! error.message ) {
+			return null;
+		}
+
+		return (
+			<Notice status={ 'is-error' } showDismiss={ false }>
+				{ error.message }
+			</Notice>
+		);
+	}
+}
+
+export default connect(
+	( state ) => ( {
+		createAccountError: getCreateSocialAccountError( state ),
+		requestAccountError: getRequestSocialAccountError( state ),
+		requestError: getRequestError( state ),
+		twoFactorAuthRequestError: getTwoFactorAuthRequestError( state ),
+	} )
+)( ErrorNotice );

client/blocks/login/index.jsx
@@ -10,15 +10,12 @@ import page from 'page';
 /**
  * Internal dependencies
  */
+import ErrorNotice from './error-notice';
 import LoginForm from './login-form';
 import {
 	getRedirectTo,
-	getRequestError,
 	getRequestNotice,
-	getTwoFactorAuthRequestError,
 	getTwoFactorNotificationSent,
-	getCreateSocialAccountError,
-	getRequestSocialAccountError,
 	isTwoFactorEnabled,
 } from 'state/login/selectors';
 import { recordTracksEvent } from 'state/analytics/actions';
@@ -35,10 +32,8 @@ class Login extends Component {
 	static propTypes = {
 		recordTracksEvent: PropTypes.func.isRequired,
 		redirectTo: PropTypes.string,
-		requestError: PropTypes.object,
 		requestNotice: PropTypes.object,
 		twoFactorAuthType: PropTypes.string,
-		twoFactorAuthRequestError: PropTypes.object,
 		twoFactorEnabled: PropTypes.bool,
 		twoFactorNotificationSent: PropTypes.string,
 	};
@@ -51,12 +46,10 @@ class Login extends Component {
 	};
 
 	componentWillReceiveProps = ( nextProps ) => {
-		const hasLoginError = this.props.requestError !== nextProps.requestError;
-		const hasTwoFactorAuthError = this.props.twoFactorAuthRequestError !== nextProps.twoFactorAuthRequestError;
 		const hasNotice = this.props.requestNotice !== nextProps.requestNotice;
 		const isNewPage = this.props.twoFactorAuthType !== nextProps.twoFactorAuthType;
 
-		if ( isNewPage || hasLoginError || hasTwoFactorAuthError || hasNotice ) {
+		if ( isNewPage || hasNotice ) {
 			window.scrollTo( 0, 0 );
 		}
 	};
@@ -91,23 +84,6 @@ class Login extends Component {
 		window.location.href = url;
 	};
 
-	renderError() {
-		const error = this.props.requestError ||
-			this.props.twoFactorAuthRequestError ||
-			this.props.requestAccountError ||
-			this.props.createAccountError && this.props.createAccountError.code !== 'unknown_user' ? this.props.createAccountError : null;
-
-		if ( ! error || ( error.field && error.field !== 'global' ) || ! error.message ) {
-			return null;
-		}
-
-		return (
-			<Notice status={ 'is-error' } showDismiss={ false }>
-				{ error.message }
-			</Notice>
-		);
-	}
-
 	renderNotice() {
 		const { requestNotice } = this.props;
 
@@ -140,8 +116,7 @@ class Login extends Component {
 					{ poller }
 					<VerificationCodeForm
 						onSuccess={ this.rebootAfterLogin }
-						twoFactorAuthType={ twoFactorAuthType }
-					/>
+						twoFactorAuthType={ twoFactorAuthType } />
 				</div>
 			);
 		}
@@ -169,7 +144,7 @@ class Login extends Component {
 					{ twoStepNonce ? translate( 'Two-Step Authentication' ) : translate( 'Log in to your account.' ) }
 				</div>
 
-				{ this.renderError() }
+				<ErrorNotice />
 
 				{ this.renderNotice() }
 
@@ -182,11 +157,7 @@ class Login extends Component {
 export default connect(
 	( state ) => ( {
 		redirectTo: getRedirectTo( state ),
-		createAccountError: getCreateSocialAccountError( state ),
-		requestAccountError: getRequestSocialAccountError( state ),
-		requestError: getRequestError( state ),
 		requestNotice: getRequestNotice( state ),
-		twoFactorAuthRequestError: getTwoFactorAuthRequestError( state ),
 		twoFactorEnabled: isTwoFactorEnabled( state ),
 		twoFactorNotificationSent: getTwoFactorNotificationSent( state ),
 	} ), {

client/state/login/reducer.js
@@ -138,7 +138,7 @@ export const socialAccount = createReducer( { isCreating: false }, {
 		requestError: error
 	} ),
 	[ USER_RECEIVE ]: state => ( { ...state, bearerToken: null, username: null } ),
-	[ LOGIN_REQUEST ]: state => ( { ...state, createError: false } ),
+	[ LOGIN_REQUEST ]: state => ( { ...state, createError: null } ),
 } );
 
 export default combineReducers( {