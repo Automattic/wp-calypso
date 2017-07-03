cfadf8cf0d9ba395df367385a82a68cb9f9dfb12
Kirk Wight <kwight@kwight.ca>
2017-07-03 12:40:01 -0700

    Redesign the `/me/get-apps` page. (#15382)
    Redesign the `/me/get-apps` page.
    
    Present the user with a desktop download section tailored to their platform, with alternate platform downloads available as links. Have the mobile download badges above the desktop download card on smaller screen sizes (<480px).

---
+++ client/me/get-apps/mobile-download-card.jsx
@@ -0,0 +1,58 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import { identity, noop } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+import { recordTracksEvent } from 'state/analytics/actions';
+
+const MobileDownloadCard = ( { translate, trackIosClick, trackAndroidClick } ) => {
+	return (
+		<Card className="get-apps__mobile">
+			<div className="get-apps__card-text">
+				<h3 className="get-apps__card-title">{ translate( 'Mobile Apps' ) }</h3>
+				<p className="get-apps__description">{ translate( 'WordPress at your fingertips.' ) }</p>
+			</div>
+			<div className="get-apps__badges">
+				<a href={ 'https://itunes.apple.com/us/app/wordpress/id335703880?mt=8' } onClick={ trackIosClick }>
+					<img src={ '/calypso/images/me/get-apps-app-store.png' }
+						title={ translate( 'Download the WordPress iOS mobile app.' ) }
+						alt={ translate( 'Apple App Store download badge' ) } />
+				</a>
+				<a href={ 'https://play.google.com/store/apps/details?id=org.wordpress.android' } onClick={ trackAndroidClick }>
+					<img src={ '/calypso/images/me/get-apps-google-play.png' }
+						title={ translate( 'Download the WordPress Android mobile app.' ) }
+						alt={ translate( 'Google Play Store download badge' ) } />
+				</a>
+			</div>
+		</Card>
+	);
+};
+
+MobileDownloadCard.propTypes = {
+	translate: PropTypes.func,
+	trackIosClick: PropTypes.func,
+	trackAndroidClick: PropTypes.func,
+};
+
+MobileDownloadCard.defaultProps = {
+	translate: identity,
+	trackIosClick: noop,
+	trackAndroidClick: noop,
+};
+
+const mapDispatchToProps = {
+	trackIosClick: () => recordTracksEvent( 'calypso_app_download_ios_click' ),
+	trackAndroidClick: () => recordTracksEvent( 'calypso_app_download_android_click' ),
+};
+
+export default connect(
+	null,
+	mapDispatchToProps
+)( localize( MobileDownloadCard ) );
