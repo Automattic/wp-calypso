dd7c6150efd2da78aafdadc75bd22805553a60b1
Jake F - @samouri <samouri@users.noreply.github.com>
2017-07-03 20:11:54 -0400

    remove unused const

--- client/reader/follow-button/follow-sources.jsx
+++ client/reader/follow-button/follow-sources.jsx
@@ -5,7 +5,6 @@ export const SEARCH_RESULTS = 'search-results';
 export const SEARCH_RESULTS_SITES = 'search-results-sites';
 export const READER_SUBSCRIPTIONS = 'reader-subscriptions';
 export const READER_FEED_SEARCH = 'reader-feed-search-result';
-export const COMBINED_CARD = 'reader-combined-card';
 export const READER_FOLLOWING_MANAGE_URL_INPUT = 'reader-following-manage-url-input';
 export const READER_FOLLOWING_MANAGE_SEARCH_RESULT = 'reader-following-manage-search-result';
 export const READER_FOLLOWING_MANAGE_RECOMMENDATION = 'reader-following-manage-recommendation';
