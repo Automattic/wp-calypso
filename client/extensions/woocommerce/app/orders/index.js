e19bc87c95da7217ffb3a53da07b7429b03df615
Kelly Dwan <ryelle@users.noreply.github.com>
2017-07-04 14:26:09 -0400

    Update “Add Order” link to link to wp-admin (for now)

--- client/extensions/woocommerce/app/orders/index.js
+++ client/extensions/woocommerce/app/orders/index.js
@@ -10,19 +10,18 @@ import React, { Component } from 'react';
  */
 import ActionHeader from 'woocommerce/components/action-header';
 import Button from 'components/button';
-import { getLink } from 'woocommerce/lib/nav-utils';
+import { getSiteAdminUrl } from 'state/sites/selectors';
 import { getSelectedSiteWithFallback } from 'woocommerce/state/sites/selectors';
 import Main from 'components/main';
 import OrdersList from './orders-list';
 
 class Orders extends Component {
 	render() {
-		const { className, site, translate } = this.props;
-		const addLink = getLink( '/store/order/:site', site );
+		const { className, createOrderLink, translate } = this.props;
 		return (
 			<Main className={ className }>
 				<ActionHeader breadcrumbs={ ( <span>{ translate( 'Orders' ) }</span> ) }>
-					<Button primary href={ addLink }>{ translate( 'Add Order' ) }</Button>
+					<Button primary href={ createOrderLink }>{ translate( 'Add Order' ) }</Button>
 				</ActionHeader>
 				<OrdersList />
 			</Main>
@@ -33,8 +32,10 @@ class Orders extends Component {
 export default connect(
 	state => {
 		const site = getSelectedSiteWithFallback( state );
+		const createOrderLink = getSiteAdminUrl( state, site ? site.ID : null, 'post-new.php?post_type=shop_order' );
 
 		return {
+			createOrderLink,
 			site,
 		};
 	}
