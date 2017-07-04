bf530ba76b1400bb7303fce2d261c4fac6a146bb
Kelly Dwan <ryelle@users.noreply.github.com>
2017-07-04 14:12:47 -0400

    Add order status to SectionHeader

--- client/extensions/woocommerce/app/order/order-details.js
+++ client/extensions/woocommerce/app/order/order-details.js
@@ -1,15 +1,17 @@
 /**
  * External dependencies
  */
+import { find } from 'lodash';
+import Gridicon from 'gridicons';
 import { localize } from 'i18n-calypso';
 import React, { Component, PropTypes } from 'react';
-import Gridicon from 'gridicons';
 
 /**
  * Internal dependencies
  */
 import Button from 'components/button';
 import Card from 'components/card';
+import FormSelect from 'components/forms/form-select';
 import OrderDetailsTable from './order-details-table';
 import OrderRefundCard from './order-refund-card';
 import SectionHeader from 'components/section-header';
@@ -24,13 +26,55 @@ class OrderDetails extends Component {
 	}
 
 	state = {
-		showRefundDialog: false,
+		status: this.props.order.status || false,
 	}
 
-	toggleRefundDialog = () => {
-		this.setState( {
-			showRefundDialog: ! this.state.showRefundDialog,
-		} );
+	updateStatus = ( event ) => {
+		this.setState( { status: event.target.value } );
+	}
+
+	renderStatus = () => {
+		const { order, translate } = this.props;
+		const classes = `order__status is-${ order.status }`;
+		const statuses = [ {
+			value: 'pending',
+			name: translate( 'Pending payment' ),
+		}, {
+			value: 'processing',
+			name: translate( 'Processing' ),
+		}, {
+			value: 'on-hold',
+			name: translate( 'On Hold' ),
+		}, {
+			value: 'completed',
+			name: translate( 'Completed' ),
+		}, {
+			value: 'cancelled',
+			name: translate( 'Cancelled' ),
+		}, {
+			value: 'refunded',
+			name: translate( 'Refunded' ),
+		}, {
+			value: 'failed',
+			name: translate( 'Payment Failed' ),
+		} ];
+
+		if ( 'pending' === order.status || 'on-hold' === order.status ) {
+			return (
+				<FormSelect id="select" value={ this.state.status } onChange={ this.updateStatus }>
+					{ statuses.map( ( status, i ) => {
+						return (
+							<option key={ i } value={ status.value }>{ status.name }</option>
+						);
+					} ) }
+				</FormSelect>
+			);
+		}
+
+		const statusLabel = find( statuses, { value: order.status } );
+		return (
+			<span className={ classes }>{ statusLabel.name }</span>
+		);
 	}
 
 	render() {
@@ -41,7 +85,9 @@ class OrderDetails extends Component {
 
 		return (
 			<div className="order__details">
-				<SectionHeader label={ translate( 'Order Details' ) } />
+				<SectionHeader label={ translate( 'Order Details' ) }>
+					<span>{ this.renderStatus() }</span>
+				</SectionHeader>
 				<Card className="order__details-card">
 					<OrderDetailsTable order={ order } site={ site } />
 					<OrderRefundCard order={ order } site={ site } />
