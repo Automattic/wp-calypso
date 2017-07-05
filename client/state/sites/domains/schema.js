303c5750da3b809781ad76d2a304d73dd07fc39e
Igor Klimer <igor@klimer.eu>
2017-07-05 21:42:01 +0200

    Add the new field to the schema

--- client/state/sites/domains/schema.js
+++ client/state/sites/domains/schema.js
@@ -35,6 +35,7 @@ export const itemsSchema = {
 					pointsToWpcom: { type: 'boolean' },
 					registrar: { type: 'string' },
 					registrationDate: { type: 'string' },
+					subscriptionId: { type: 'string', optional: true },
 					type: { type: 'string' }
 				}
 			}
