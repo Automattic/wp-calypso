74f3e7b7a7359eaa012ab5a203f58ade36f944d0
Marin Atanasov <tyxla@abv.bg>
2017-07-06 18:12:25 +0300

    Site Settings: Introduce a basic loading placeholder

---
+++ client/my-sites/site-settings/placeholder.jsx
@@ -0,0 +1,8 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+export default () => (
+	<div className="site-settings__placeholder wpcom-site__logo noticon noticon-wordpress" />
+);
