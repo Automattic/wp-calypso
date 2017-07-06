a6bbfd32a639e7d1ce35695a3c29b053feef65dc
Donna Peplinskie <donnapep@gmail.com>
2017-07-06 13:05:36 -0400

    Add <FormSection> components
    - Also, map API field names to names used in the UI

--- client/extensions/wp-job-manager/components/settings/job-submission/index.jsx
+++ client/extensions/wp-job-manager/components/settings/job-submission/index.jsx
@@ -3,7 +3,7 @@
  */
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { formValueSelector, reduxForm } from 'redux-form';
+import { FormSection, formValueSelector, reduxForm } from 'redux-form';
 import { localize } from 'i18n-calypso';
 import { flowRight } from 'lodash';
 
@@ -21,165 +21,173 @@ import ReduxFormTextInput from 'components/redux-forms/redux-form-text-input';
 import ReduxFormToggle from 'components/redux-forms/redux-form-toggle';
 import SectionHeader from 'components/section-header';
 
-const JobSubmission = ( { isDisabled, duration, translate } ) => {
+const JobSubmission = ( { isDisabled, submissionDuration, translate } ) => {
 	return (
 		<div>
 			<form>
-				<SectionHeader label={ translate( 'Account' ) }>
-					<FormButton compact
-						disabled={ isDisabled } />
-				</SectionHeader>
-				<Card>
-					<FormFieldset>
-						<ReduxFormToggle
-							disabled={ isDisabled }
-							name="job_manager_user_requires_account"
-							text={ translate( 'Require an account to submit listings' ) } />
-						<FormSettingExplanation isIndented>
-							{ translate( 'Limits job listing submissions to registered, logged-in users.' ) }
-						</FormSettingExplanation>
+				<FormSection name="account">
+					<SectionHeader label={ translate( 'Account' ) }>
+						<FormButton compact
+							disabled={ isDisabled } />
+					</SectionHeader>
+					<Card>
+						<FormFieldset>
+							<ReduxFormToggle
+								disabled={ isDisabled }
+								name="isRequired"
+								text={ translate( 'Require an account to submit listings' ) } />
+							<FormSettingExplanation isIndented>
+								{ translate( 'Limits job listing submissions to registered, logged-in users.' ) }
+							</FormSettingExplanation>
 
-						<ReduxFormToggle
-							disabled={ isDisabled }
-							name="job_manager_enable_registration"
-							text={ translate( 'Enable account creation during submission' ) } />
-						<FormSettingExplanation isIndented>
-							{ translate( 'Includes account creation on the listing submission form, to allow ' +
-								'non-registered users to create an account and submit a job listing simultaneously.' ) }
-						</FormSettingExplanation>
+							<ReduxFormToggle
+								disabled={ isDisabled }
+								name="enableRegistration"
+								text={ translate( 'Enable account creation during submission' ) } />
+							<FormSettingExplanation isIndented>
+								{ translate( 'Includes account creation on the listing submission form, to allow ' +
+									'non-registered users to create an account and submit a job listing simultaneously.' ) }
+							</FormSettingExplanation>
 
-						<ReduxFormToggle
-							disabled={ isDisabled }
-							name="job_manager_generate_username_from_email"
-							text="Generate usernames from email addresses" />
-						<FormSettingExplanation isIndented>
-							{ translate( 'Automatically generates usernames for new accounts from the registrant\'s ' +
-								'email address. If this is not enabled, a "username" field will display instead.' ) }
-						</FormSettingExplanation>
-					</FormFieldset>
+							<ReduxFormToggle
+								disabled={ isDisabled }
+								name="generateUsername"
+								text="Generate usernames from email addresses" />
+							<FormSettingExplanation isIndented>
+								{ translate( 'Automatically generates usernames for new accounts from the registrant\'s ' +
+									'email address. If this is not enabled, a "username" field will display instead.' ) }
+							</FormSettingExplanation>
+						</FormFieldset>
 
-					<FormFieldset>
-						<FormLabel>
-							{ translate( 'Role' ) }
-						</FormLabel>
-						<ReduxFormSelect
-							disabled={ isDisabled }
-							name="job_manager_registration_role">
-							<option value="editor">{ translate( 'Editor' ) }</option>
-							<option value="author">{ translate( 'Author' ) }</option>
-							<option value="contributor">{ translate( 'Contributor' ) }</option>
-							<option value="subscriber">{ translate( 'Subscriber' ) }</option>
-							<option value="teacher">{ translate( 'Teacher' ) }</option>
-							<option value="employer">{ translate( 'Employer' ) }</option>
-							<option value="customer">{ translate( 'Customer' ) }</option>
-							<option value="shop_manager">{ translate( 'Shop manager' ) }</option>
-						</ReduxFormSelect>
-						<FormSettingExplanation>
-							{ translate( 'Any new accounts created during submission will have this role.' ) }
-						</FormSettingExplanation>
-					</FormFieldset>
-				</Card>
+						<FormFieldset>
+							<FormLabel>
+								{ translate( 'Role' ) }
+							</FormLabel>
+							<ReduxFormSelect
+								disabled={ isDisabled }
+								name="role">
+								<option value="editor">{ translate( 'Editor' ) }</option>
+								<option value="author">{ translate( 'Author' ) }</option>
+								<option value="contributor">{ translate( 'Contributor' ) }</option>
+								<option value="subscriber">{ translate( 'Subscriber' ) }</option>
+								<option value="teacher">{ translate( 'Teacher' ) }</option>
+								<option value="employer">{ translate( 'Employer' ) }</option>
+								<option value="customer">{ translate( 'Customer' ) }</option>
+								<option value="shop_manager">{ translate( 'Shop manager' ) }</option>
+							</ReduxFormSelect>
+							<FormSettingExplanation>
+								{ translate( 'Any new accounts created during submission will have this role.' ) }
+							</FormSettingExplanation>
+						</FormFieldset>
+					</Card>
+				</FormSection>
 			</form>
 
 			<form>
-				<SectionHeader label={ translate( 'Approval' ) }>
-					<FormButton compact
-						disabled={ isDisabled } />
-				</SectionHeader>
-				<Card>
-					<FormFieldset>
-						<ReduxFormToggle
-							disabled={ isDisabled }
-							name="job_manager_submission_requires_approval"
-							text={ translate( 'Require admin approval of all new listing submissions' ) } />
-						<FormSettingExplanation isIndented>
-							{ translate( 'Sets all new submissions to "pending." They will not appear on your ' +
-								'site until an admin approves them.' ) }
-						</FormSettingExplanation>
+				<FormSection name="approval">
+					<SectionHeader label={ translate( 'Approval' ) }>
+						<FormButton compact
+							disabled={ isDisabled } />
+					</SectionHeader>
+					<Card>
+						<FormFieldset>
+							<ReduxFormToggle
+								disabled={ isDisabled }
+								name="isRequired"
+								text={ translate( 'Require admin approval of all new listing submissions' ) } />
+							<FormSettingExplanation isIndented>
+								{ translate( 'Sets all new submissions to "pending." They will not appear on your ' +
+									'site until an admin approves them.' ) }
+							</FormSettingExplanation>
 
-						<ReduxFormToggle
-							disabled={ isDisabled }
-							name="job_manager_user_can_edit_pending_submissions"
-							text="Allow editing of pending listings" />
-						<FormSettingExplanation isIndented>
-							{ translate( 'Users can continue to edit pending listings until they are approved by an admin.' ) }
-						</FormSettingExplanation>
-					</FormFieldset>
-				</Card>
+							<ReduxFormToggle
+								disabled={ isDisabled }
+								name="canEdit"
+								text="Allow editing of pending listings" />
+							<FormSettingExplanation isIndented>
+								{ translate( 'Users can continue to edit pending listings until they are approved by an admin.' ) }
+							</FormSettingExplanation>
+						</FormFieldset>
+					</Card>
+				</FormSection>
 			</form>
 
 			<form>
-				<SectionHeader label={ translate( 'Listing Duration' ) }>
-					<FormButton compact
-						disabled={ isDisabled } />
-				</SectionHeader>
-				<Card>
-					<FormFieldset>
-						{ translate(
-							'Display listings for {{days /}} day',
-							'Display listings for {{days /}} days',
-							{
-								count: duration,
-								components: {
-									days:
-										<ReduxFormTextInput
-											disabled={ isDisabled }
-											min="0"
-											name="job_manager_submission_duration"
-											step="1"
-											type="number" />
+				<FormSection name="duration">
+					<SectionHeader label={ translate( 'Listing Duration' ) }>
+						<FormButton compact
+							disabled={ isDisabled } />
+					</SectionHeader>
+					<Card>
+						<FormFieldset>
+							{ translate(
+								'Display listings for {{days /}} day',
+								'Display listings for {{days /}} days',
+								{
+									count: submissionDuration,
+									components: {
+										days:
+											<ReduxFormTextInput
+												disabled={ isDisabled }
+												min="0"
+												name="submissionDuration"
+												step="1"
+												type="number" />
+									}
 								}
-							}
-						) }
-						<FormSettingExplanation>
-							{ translate( 'Listings will display for the set number of days, then expire. ' +
-								'Leave this field blank if you don\'t want listings to have an expiration date.' ) }
-						</FormSettingExplanation>
-					</FormFieldset>
-				</Card>
+							) }
+							<FormSettingExplanation>
+								{ translate( 'Listings will display for the set number of days, then expire. ' +
+									'Leave this field blank if you don\'t want listings to have an expiration date.' ) }
+							</FormSettingExplanation>
+						</FormFieldset>
+					</Card>
+				</FormSection>
 			</form>
 
 			<form>
-				<SectionHeader label={ translate( 'Application Method' ) }>
-					<FormButton compact
-						disabled={ isDisabled } />
-				</SectionHeader>
-				<Card>
-					<FormFieldset>
-						<FormSettingExplanation>
-							{ translate( 'Choose the contact method for listings.' ) }
-						</FormSettingExplanation>
-						<FormLabel>
-							<ReduxFormRadio
-								disabled={ isDisabled }
-								name="job_manager_allowed_application_method"
-								value="" />
-							<span>
-								{ translate( 'Email address or website URL' ) }
-							</span>
-						</FormLabel>
+				<FormSection name="method">
+					<SectionHeader label={ translate( 'Application Method' ) }>
+						<FormButton compact
+							disabled={ isDisabled } />
+					</SectionHeader>
+					<Card>
+						<FormFieldset>
+							<FormSettingExplanation>
+								{ translate( 'Choose the contact method for listings.' ) }
+							</FormSettingExplanation>
+							<FormLabel>
+								<ReduxFormRadio
+									disabled={ isDisabled }
+									name="applicationMethod"
+									value="" />
+								<span>
+									{ translate( 'Email address or website URL' ) }
+								</span>
+							</FormLabel>
 
-						<FormLabel>
-							<ReduxFormRadio
-								disabled={ isDisabled }
-								name="job_manager_allowed_application_method"
-								value="email" />
-							<span>
-								{ translate( 'Email addresses only' ) }
-							</span>
-						</FormLabel>
+							<FormLabel>
+								<ReduxFormRadio
+									disabled={ isDisabled }
+									name="applicationMethod"
+									value="email" />
+								<span>
+									{ translate( 'Email addresses only' ) }
+								</span>
+							</FormLabel>
 
-						<FormLabel>
-							<ReduxFormRadio
-								disabled={ isDisabled }
-								name="job_manager_allowed_application_method"
-								value="url" />
-							<span>
-								{ translate( 'Website URLs only' ) }
-							</span>
-						</FormLabel>
-					</FormFieldset>
-				</Card>
+							<FormLabel>
+								<ReduxFormRadio
+									disabled={ isDisabled }
+									name="applicationMethod"
+									value="url" />
+								<span>
+									{ translate( 'Website URLs only' ) }
+								</span>
+							</FormLabel>
+						</FormFieldset>
+					</Card>
+				</FormSection>
 			</form>
 		</div>
 	);
@@ -195,7 +203,7 @@ const connectComponent = connect(
 		const selector = formValueSelector( 'submission', () => state.extensions.wpJobManager.form );
 
 		return {
-			duration: selector( state, 'job_manager_submission_duration' ),
+			submissionDuration: selector( state, 'duration.submissionDuration' ),
 		};
 	}
 );
--- client/extensions/wp-job-manager/state/data-layer/settings/utils.js
+++ client/extensions/wp-job-manager/state/data-layer/settings/utils.js
@@ -1,24 +1,40 @@
 export const fromApi = data => ( {
-	listings: {
-		perPage: data.job_manager_per_page,
-		hideFilledPositions: data.job_manager_hide_filled_positions,
-		hideExpired: data.job_manager_hide_expired,
-		hideExpiredContent: data.job_manager_hide_expired_content,
+	account: {
+		enableRegistration: data.job_manager_enable_registration,
+		generateUsername: data.job_manager_generate_username_from_email,
+		isRequired: data.job_manager_user_requires_account,
+		role: data.job_manager_registration_role,
+	},
+	apiKey: {
+		googleMapsApiKey: data.job_manager_google_maps_api_key,
+	},
+	approval: {
+		canEdit: data.job_manager_user_can_edit_pending_submissions,
+		isRequired: data.job_manager_submission_requires_approval,
 	},
 	categories: {
 		enableCategories: data.job_manager_enable_categories,
 		enableDefaultCategory: data.job_manager_enable_default_category_multiselect,
 		categoryFilterType: data.job_manager_category_filter_type,
 	},
-	types: {
-		enableTypes: data.job_manager_enable_types,
-		multiJobType: data.job_manager_multi_job_type,
+	duration: {
+		submissionDuration: data.job_manager_submission_duration,
 	},
 	format: {
 		dateFormat: data.job_manager_date_format,
 	},
-	apiKey: {
-		googleMapsApiKey: data.job_manager_google_maps_api_key,
+	listings: {
+		perPage: data.job_manager_per_page,
+		hideFilledPositions: data.job_manager_hide_filled_positions,
+		hideExpired: data.job_manager_hide_expired,
+		hideExpiredContent: data.job_manager_hide_expired_content,
+	},
+	method: {
+		applicationMethod: data.job_manager_allowed_application_method,
+	},
+	types: {
+		enableTypes: data.job_manager_enable_types,
+		multiJobType: data.job_manager_multi_job_type,
 	},
 } );
 
