5120abf391b9d6f31434854ccfd5b71ad749415c
Donna Peplinskie <donnapep@gmail.com>
2017-07-06 07:25:06 -0400

    Omit props that ReduxForm components need but Form components don't

--- client/components/redux-forms/redux-form-radio/index.jsx
+++ client/components/redux-forms/redux-form-radio/index.jsx
@@ -3,6 +3,7 @@
  */
 import React, { Component, PropTypes } from 'react';
 import { Field } from 'redux-form';
+import { omit } from 'lodash';
 
 /**
  * Internal dependencies
@@ -15,19 +16,27 @@ class ReduxFormRadio extends Component {
 		value: PropTypes.string,
 	};
 
-	renderRadio = defaultValue => ( { input: { name, onChange, value } } ) => (
-		<FormRadio
-			{ ...this.props }
-			checked={ value === defaultValue }
-			name={ name }
-			onChange={ this.updateRadio( onChange ) }
-			value={ defaultValue } />
-	)
+	renderRadio = defaultValue => ( { input: { name, onChange, value } } ) => {
+		const otherProps = omit( this.props, [ 'name', 'value' ] );
+
+		return (
+			<FormRadio
+				{ ...otherProps }
+				checked={ value === defaultValue }
+				name={ name }
+				onChange={ this.updateRadio( onChange ) }
+				value={ defaultValue } />
+		);
+	}
 
 	updateRadio = onChange => event => onChange( event.target.value );
 
 	render() {
-		return <Field component={ this.renderRadio( this.props.value ) } name={ this.props.name } />;
+		return (
+			<Field
+				component={ this.renderRadio( this.props.value ) }
+				name={ this.props.name } />
+		);
 	}
 }
 
--- client/components/redux-forms/redux-form-text-input/index.jsx
+++ client/components/redux-forms/redux-form-text-input/index.jsx
@@ -3,6 +3,7 @@
  */
 import React, { Component, PropTypes } from 'react';
 import { Field } from 'redux-form';
+import { omit } from 'lodash';
 
 /**
  * Internal dependencies
@@ -14,12 +15,16 @@ class ReduxFormTextInput extends Component {
 		name: PropTypes.string,
 	};
 
-	renderTextInput = ( { input: { onChange, value } } ) => (
-		<FormTextInput
-			{ ...this.props }
-			onChange={ this.updateTextInput( onChange ) }
-			value={ value } />
-	)
+	renderTextInput = ( { input: { onChange, value } } ) => {
+		const otherProps = omit( this.props, 'name' );
+
+		return (
+			<FormTextInput
+				{ ...otherProps }
+				onChange={ this.updateTextInput( onChange ) }
+				value={ value } />
+		);
+	}
 
 	updateTextInput = onChange => event => onChange( event.target.value );
 
