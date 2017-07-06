60945489c41c873a765a3353f114ed2c2f8734c5
Copons <copons@users.noreply.github.com>
2017-07-06 11:25:24 +0100

    Replace <a> with <ExternalLink> where applicable

--- client/blocks/comment-detail/index.jsx
+++ client/blocks/comment-detail/index.jsx
@@ -200,6 +200,7 @@ export class CommentDetail extends Component {
 							commentContent={ commentContent }
 							commentDate={ commentDate }
 							commentStatus={ commentStatus }
+							postUrl={ postUrl }
 							repliedToComment={ repliedToComment }
 							siteId={ siteId }
 						/>
