caae71ebcfeed7839f9e65767d25c01304ff98bb
Kirk Wight <kwight@kwight.ca>
2017-07-07 15:03:34 -0700

    Improve the routing of Comments-based URLs.

--- client/my-sites/comments/controller.js
+++ client/my-sites/comments/controller.js
@@ -4,35 +4,55 @@
 import { renderWithReduxStore } from 'lib/react-helpers';
 import React from 'react';
 import page from 'page';
+import { includes } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import route from 'lib/route';
 import CommentsManagement from './main';
-import controller from 'my-sites/controller';
+
+const VALID_STATUSES = [ 'pending', 'approved', 'spam', 'trash', 'all' ];
+
+export const isValidStatus = status => includes( VALID_STATUSES, status );
+
+export const getRedirectUrl = ( status, siteFragment ) => {
+	const statusValidity = isValidStatus( status );
+	if ( status === siteFragment ) {
+		return `/comments/pending/${ siteFragment }`;
+	}
+	if ( ! statusValidity && ! siteFragment ) {
+		return '/comments/pending';
+	}
+	if ( ! statusValidity && siteFragment ) {
+		return `/comments/pending/${ siteFragment }`;
+	}
+	if ( statusValidity && ! siteFragment ) {
+		return `/comments/${ status }`;
+	}
+	return null;
+};
+
+export const redirect = function( context, next ) {
+	const { status } = context.params;
+	const siteFragment = route.getSiteFragment( context.path );
+	const redirectUrl = getRedirectUrl( status, siteFragment );
+	if ( redirectUrl && '/comments/' + status !== redirectUrl ) {
+		return page.redirect( redirectUrl );
+	}
+	next();
+};
 
 export const comments = function( context ) {
+	const { status } = context.params;
 	const siteFragment = route.getSiteFragment( context.path );
-	const status = context.params.status === 'pending' ? 'unapproved' : context.params.status;
-
 	renderWithReduxStore(
 		<CommentsManagement
 			basePath={ context.path }
 			siteFragment={ siteFragment }
-			status={ status }
+			status={ 'pending' === status ? 'unapproved' : status }
 		/>,
 		'primary',
 		context.store
 	);
-};
-
-export const sites = function( context ) {
-	const { status } = context.params;
-	const siteFragment = route.getSiteFragment( context.path );
-
-	if ( status === siteFragment ) {
-		return page.redirect( `/comments/pending/${ siteFragment }` );
-	}
-	controller.sites( context );
 };
