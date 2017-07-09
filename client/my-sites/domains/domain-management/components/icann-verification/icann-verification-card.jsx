246b77eea39998429a48c809e2f20b1b73e567e2
Igor Klimer <igor@klimer.eu>
2017-07-09 23:00:40 +0200

    Clean up code for the verification card and fix linter warnings in it

--- client/my-sites/domains/domain-management/components/icann-verification/icann-verification-card.jsx
+++ client/my-sites/domains/domain-management/components/icann-verification/icann-verification-card.jsx
@@ -3,30 +3,58 @@
  */
 import React from 'react';
 import Gridicon from 'gridicons';
+import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 /**
  * Internal dependencies
  */
-import IcannVerification from './icann-verification';
 import support from 'lib/url/support';
+import Button from 'components/button';
+import Card from 'components/card';
+import upgradesActions from 'lib/upgrades/actions';
+import { errorNotice, successNotice } from 'state/notices/actions';
 
-let IcannVerificationCard = React.createClass( {
-	propTypes: {
-		handleSubmit: React.PropTypes.func.isRequired,
-		submitting: React.PropTypes.bool.isRequired,
+class IcannVerificationCard extends React.Component {
+	static propTypes = {
+		explanationContext: React.PropTypes.string,
 		selectedDomainName: React.PropTypes.string.isRequired
-	},
+	};
+
+	state = {
+		submitting: false,
+	};
+
+	handleSubmit = ( event ) => {
+		event.preventDefault();
+
+		this.setState( { submitting: true } );
+
+		upgradesActions.resendIcannVerification( this.props.selectedDomainName, ( error ) => {
+			if ( error ) {
+				this.props.errorNotice( error.message );
+			} else {
+				this.props.successNotice( this.props.translate(
+					'Email sent to [registrant contact email]. Check your email.'
+				) );
+			}
+
+			this.setState( { submitting: false } );
+		} );
+	};
 
 	getExplanation() {
-		if ( this.props.explanationContext === 'name-servers' ) {
-			return this.translate(
+		const { translate, explanationContext } = this.props;
+		if ( explanationContext === 'name-servers' ) {
+			return translate(
 				'You have to verify the email address used to register this domain before you ' +
 				'are able to update the name servers for your domain. ' +
 				'Look for the verification message in your email inbox.'
 			);
 		}
 
-		return this.translate(
+		return translate(
 			'We need to verify the email address you provided for this domain to ensure we can contact ' +
 			'you concerning your domain. Please verify your email address or your domain may be suspended. ' +
 			'{{learnMoreLink}}Learn more.{{/learnMoreLink}}', {
@@ -38,11 +66,12 @@ let IcannVerificationCard = React.createClass( {
 				}
 			}
 		);
-	},
+	}
 
 	render() {
+		const { translate } = this.props;
 		return (
-			<div className="icann-verification is-warning card is-compact">
+			<Card compact highlight="warning" className="icann-verification__card">
 				<div className="icann-verification__explanation">
 					<h1 className="icann-verification__heading">Important: Verify Your Email Address</h1>
 					{ this.getExplanation() }
@@ -50,17 +79,21 @@ let IcannVerificationCard = React.createClass( {
 
 				<div className="icann-verification__status-container">
 					<div className="icann-verification__status waiting">
-						<Gridicon icon="notice-outline" size="36" />
-						{ this.translate(
+						<Gridicon icon="notice-outline" size={ 36 } />
+						{ translate(
 							'Check your email — we\'re waiting for you to verify.'
 						)}
 					</div>
 
-					<IcannVerification.Button submitting={ this.props.submitting }
-					onClick={ this.props.handleSubmit } />
+					<Button
+						primary
+						disabled={ this.state.submitting }
+						onClick={ this.handleSubmit }>
+						{ translate( 'Send Again' ) }
+					</Button>
 
 					<div className="icann-verification__sent-to">
-						{ this.translate(
+						{ translate(
 							'Sent to registrant@contactemail.com. ' +
 							'{{registrantEmail}}Change email address.{{/registrantEmail}}', {
 								components: {
@@ -70,12 +103,12 @@ let IcannVerificationCard = React.createClass( {
 						)}
 					</div>
 				</div>
-
-			</div>
+			</Card>
 		);
 	}
-} );
+}
 
-IcannVerificationCard = IcannVerification.createContainer( IcannVerificationCard );
-
-export default IcannVerificationCard;
+export default connect(
+	null,
+	dispatch => bindActionCreators( { errorNotice, successNotice }, dispatch )
+)( localize( IcannVerificationCard ) );
