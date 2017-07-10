a7259313cfe44bf55f1ce18d43e896d3d6f9b0a1
tellyworth <alex@automattic.com>
2017-07-10 15:51:18 +1000

    Instead of whitelisting entire posts, whitelist specific blocks of markup for galleries, Instagram, and Twitter embeds.

--- client/lib/post-normalizer/rule-content-remove-styles.js
+++ client/lib/post-normalizer/rule-content-remove-styles.js
@@ -8,28 +8,29 @@ export default function removeContentStyles( post, dom ) {
 		throw new Error( 'this transform must be used as part of withContentDOM' );
 	}
 
-	// if there are any specials in the post, skip it
-	if ( dom.querySelector( 'blockquote[class^="instagram-"], blockquote[class^="twitter-"]' ) ) {
-		return post;
-	}
+	// Whitelist the markup for galleries, Instagram, and Twitter. Styling will be allowed on elements that match this selector.
+	const whitelistSelector = '.gallery, .gallery *, .gallery-row, .gallery-row *, .gallery-group, .gallery-group *, ' +
+	'blockquote[class^="instagram-"], blockquote[class^="instagram-"] *, ' +
+	'blockquote[class^="twitter-"], blockquote[class^="twitter-"] *';
 
 	// remove most style attributes
 	const styled = dom.querySelectorAll( '[style]' );
 	forEach( styled, function( element ) {
-		// Leave inline styles intact for gallery markup
-		if ( ! element.matches( '.gallery-row, .gallery-group' ) ) {
+		if ( ! element.matches( whitelistSelector ) ) {
 			element.removeAttribute( 'style' );
 		}
 	} );
 
 	// remove all style elements
 	forEach( dom.querySelectorAll( 'style' ), function( element ) {
-		element.parentNode && element.parentNode.removeChild( element );
+		if ( ! element.matches( whitelistSelector ) ) {
+			element.parentNode && element.parentNode.removeChild( element );
+		}
 	} );
 
 	// remove align from non images
 	forEach( dom.querySelectorAll( '[align]' ), element => {
-		if ( element.tagName !== 'IMG' ) {
+		if ( element.tagName !== 'IMG' && !element.matches( whitelistSelector ) ) {
 			element.removeAttribute( 'align' );
 		}
 	} );
