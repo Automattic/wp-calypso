b60a1e64b9cf3f0ed25d600765b3976386384c35
Igor Klimer <klimeryk@users.noreply.github.com>
2017-07-11 17:38:29 +0200

    Google Apps: Properly format monthly price for G Suite (#15611)
    The previous approach was hacky and brittle - for example, it failed and
    burned for Japanese Yen (formatted as `5,800`).
    
    Fixes #15192

--- client/lib/google-apps/index.js
+++ client/lib/google-apps/index.js
@@ -1,26 +1,19 @@
 /**
- * External dependencies
+ * Internal dependencies
  */
-import { numberFormat } from 'i18n-calypso';
+import formatCurrency from 'lib/format-currency';
 
 /**
  * Constants
  */
 const GOOGLE_APPS_LINK_PREFIX = 'https://mail.google.com/a/';
-const PRICE_FORMAT = /(\d+[.,]?\d+)/;
 
-function getAnnualPrice( price ) {
-	return price && price.replace( PRICE_FORMAT, ( value ) => {
-		const number = parseFloat( value );
-		return number % 1 === 0 ? number : numberFormat( number, 2 );
-	} );
+function getAnnualPrice( cost, currencyCode ) {
+	return formatCurrency( cost, currencyCode );
 }
 
-function getMonthlyPrice( price ) {
-	return price && price.replace( PRICE_FORMAT, ( value ) => {
-		const number = ( Math.round( parseFloat( value ) / 10 * 100 ) / 100 );
-		return number % 1 === 0 ? number : numberFormat( number, 2 );
-	} );
+function getMonthlyPrice( cost, currencyCode ) {
+	return formatCurrency( cost / 10, currencyCode );
 }
 
 function googleAppsSettingsUrl( domainName ) {
