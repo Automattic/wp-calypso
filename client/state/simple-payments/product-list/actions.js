251e06cee4c1dad35c79fca4a5d8ee94140b3cad
Rastislav Lamos <lamos.rasto@gmail.com>
2017-07-11 15:00:44 +0200

    Add QuerySimplePayments (#16076)
    * Add functionality to get a single product
    
    * Add actions to request Simple Payments product(s)
    
    * Add QuerySimplePayments component
    
    * Fix README and requesting all products
    
    * Add smaller fixes, get rid of allProducts prop
    
    Props @jsnajdr for the suggestions.
    
    * Protect against productId 0
    
    Props @jsnajdr for the suggestion.

--- client/state/simple-payments/product-list/actions.js
+++ client/state/simple-payments/product-list/actions.js
@@ -2,16 +2,38 @@
  * Internal dependencies
  */
 import {
+	SIMPLE_PAYMENTS_PRODUCT_GET,
+	SIMPLE_PAYMENTS_PRODUCTS_LIST,
+	SIMPLE_PAYMENTS_PRODUCT_RECEIVE,
 	SIMPLE_PAYMENTS_PRODUCTS_LIST_RECEIVE,
 	SIMPLE_PAYMENTS_PRODUCTS_LIST_RECEIVE_UPDATE,
 	SIMPLE_PAYMENTS_PRODUCTS_LIST_RECEIVE_DELETE,
 } from 'state/action-types';
+
+export const requestProducts = ( siteId ) => ( {
+	siteId,
+	type: SIMPLE_PAYMENTS_PRODUCTS_LIST,
+} );
+
+export const requestProduct = ( siteId, productId ) => ( {
+	siteId,
+	productId,
+	type: SIMPLE_PAYMENTS_PRODUCT_GET,
+} );
 
 export function receiveProductsList( siteId, numOfProducts, posts ) {
 	return {
 		type: SIMPLE_PAYMENTS_PRODUCTS_LIST_RECEIVE,
 		siteId,
 		products: posts,
+	};
+}
+
+export function receiveProduct( siteId, product ) {
+	return {
+		siteId,
+		product,
+		type: SIMPLE_PAYMENTS_PRODUCT_RECEIVE,
 	};
 }
 
