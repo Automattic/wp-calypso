42099e17dd196fbcfe04d96aba364b6147d2fd28
Rastislav Lamos <lamos.rasto@gmail.com>
2017-07-12 10:36:02 +0200

    Add a fn to check if a product is a valid Simple Payment product (#16126)
    * Add a fn to check if a product is a valid Simple Payment product
    
    Helps to fix retrieving and storing standard posts as Simple Payments
    products.
    
    * Add product validation to retriever of all products
    
    Props @jsnajdr for the suggestion.
    
    * Remove "found" param -- not used

--- client/state/data-layer/wpcom/sites/simple-payments/index.js
+++ client/state/data-layer/wpcom/sites/simple-payments/index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { translate } from 'i18n-calypso';
+import { filter } from 'lodash';
 
 /**
  * Internal dependencies
@@ -24,6 +25,8 @@ import { http } from 'state/data-layer/wpcom-http/actions';
 import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
 import { getRawSite } from 'state/sites/selectors';
 import { errorNotice } from 'state/notices/actions';
+import { SIMPLE_PAYMENTS_PRODUCT_POST_TYPE } from 'lib/simple-payments/constants';
+import { isValidSimplePaymentsProduct } from 'lib/simple-payments/utils';
 
 /**
  * Reduce function for product attributes stored in post metadata
@@ -71,7 +74,7 @@ function productToCustomPost( product ) {
 			return payload;
 		},
 		{
-			type: 'jp_pay_product',
+			type: SIMPLE_PAYMENTS_PRODUCT_POST_TYPE,
 			metadata: [],
 			title: product.title,
 			content: product.description,
@@ -107,7 +110,7 @@ export function requestSimplePaymentsProducts( { dispatch }, action ) {
 		method: 'GET',
 		path: `/sites/${ siteId }/posts`,
 		query: {
-			type: 'jp_pay_product',
+			type: SIMPLE_PAYMENTS_PRODUCT_POST_TYPE,
 			status: 'publish'
 		},
 	}, action ) );
@@ -163,11 +166,19 @@ export const addProduct = ( { dispatch }, { siteId }, next, newProduct ) =>
 export const deleteProduct = ( { dispatch }, { siteId }, next, deletedProduct ) =>
 	dispatch( receiveDeleteProduct( siteId, deletedProduct.ID ) );
 
-export const listProduct = ( { dispatch }, { siteId }, next, product ) =>
-	dispatch( receiveProduct( siteId, customPostToProduct( product ) ) );
+export const listProduct = ( { dispatch }, { siteId }, next, product ) => {
+	if ( ! isValidSimplePaymentsProduct( product ) ) {
+		return;
+	}
 
-export const listProducts = ( { dispatch }, { siteId }, next, { found: numOfProducts, posts: products } ) =>
-	dispatch( receiveProductsList( siteId, numOfProducts, products.map( customPostToProduct ) ) );
+	dispatch( receiveProduct( siteId, customPostToProduct( product ) ) );
+};
+
+export const listProducts = ( { dispatch }, { siteId }, next, { posts: products } ) => {
+	const validProducts = filter( products, isValidSimplePaymentsProduct );
+
+	dispatch( receiveProductsList( siteId, validProducts.map( customPostToProduct ) ) );
+};
 
 const announceListingProductsFailure = ( { dispatch, getState }, { siteId } ) => {
 	const site = getRawSite( getState(), siteId );
