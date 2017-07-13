4077cf3411cd74adefd91f9882f78acd1e8094ca
bluefuton <chris@bluefuton.com>
2017-07-13 18:20:56 +0100

    Add to devdocs

---
+++ client/blocks/reader-email-settings/docs/example.jsx
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ */
+import React, { PureComponent } from 'react';
+
+/**
+ * Internal dependencies
+ */
+import ReaderEmailSettings from 'blocks/reader-email-settings';
+import Card from 'components/card';
+
+const exampleSiteId = 70135762; // Longreads
+
+export default class ReaderEmailSettingsExample extends React.Component {
+	static displayName = 'ReaderEmailSettings';
+
+	render() {
+		return (
+			<Card>
+				<ReaderEmailSettings siteId={ exampleSiteId } />
+			</Card>
+		);
+	}
+}
