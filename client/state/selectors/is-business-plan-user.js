d9a07e8f67c9b7c67feb09846e70ddfd43287eeb
Jeremey DuVall <jeremeylduvall@gmail.com>
2017-07-14 08:49:13 -0600

    Support: Add concierge offer to contact form (#16144)
    * Help: Add concierge offer to contact form
    
    This adds a screenshare offer to the contact form for Business users.
    For non-Business plan users, it adds a "Chat temporarily closed"
    message. These updates are for the all hands business onboarding
    experiment run in Happiness from 7/19-7/20. We'll reverse these changes
    afterwards.
    
    * Move the logic that checkes if the current user is a business user into
    a new selector.
    
    * Apply `isBusinessPlanUser` selector to `HelpContactForm` component.
    
    * Extract the `renderCalendlyOffer()` function and its related setup code
    as an individual component, `ChatBusinessConciergeNotice`.
    
    * Remove unnecessary div wrapper.
    
    * Add the test suite for `isBusinessPlanUser` selector.
    
    * Add from / to time properties to ChatBusinessConciergeNotice component.
    
    * Fix typos

---
+++ client/state/selectors/is-business-plan-user.js
@@ -0,0 +1,28 @@
+/**
+ * Internal dependencies
+ */
+import { getCurrentUserId } from 'state/current-user/selectors';
+import { getUserPurchases } from 'state/purchases/selectors';
+import { PLAN_BUSINESS } from 'lib/plans/constants';
+
+/**
+ * Returns a boolean flag indicating if the current user is a business plan user.
+ *
+ * @param {Object}   state Global state tree
+ * @return {Boolean} If the current user is a business plan user.
+ */
+export default ( state ) => {
+	const userId = getCurrentUserId( state );
+
+	if ( ! userId ) {
+		return false;
+	}
+
+	const purchases = getUserPurchases( state, userId );
+
+	if ( ! purchases || 0 === purchases.length ) {
+		return false;
+	}
+
+	return purchases.some( ( purchase ) => PLAN_BUSINESS === purchase.productSlug );
+};
