c3cac58182692b7ae46113aa941c4a304453bd0f
Kelly Dwan <ryelle@users.noreply.github.com>
2017-07-14 14:29:37 -0400

    Store: Rename KeydownCallback and add up/down arrow navigation

--- client/extensions/woocommerce/lib/get-keyboard-handler/index.js
+++ client/extensions/woocommerce/lib/get-keyboard-handler/index.js
@@ -1,5 +1,3 @@
-// Helper function for keyboard-equivalents of onClick events
-
 /**
  * Helper function which triggers a callback on a keydown event, only
  * if the key pressed is space or enter - to mirror button functionality.
@@ -10,7 +8,18 @@
 export default function( callback ) {
 	return ( event ) => {
 		if ( event.key === 'Enter' || event.key === ' ' ) {
+			event.preventDefault();
 			callback( event );
+		} else if ( event.key === 'ArrowDown' ) {
+			if ( event.target.nextSibling ) {
+				event.preventDefault();
+				event.target.nextSibling.focus();
+			}
+		} else if ( event.key === 'ArrowUp' ) {
+			if ( event.target.nextSibling ) {
+				event.preventDefault();
+				event.target.previousSibling.focus();
+			}
 		}
 	};
 }
