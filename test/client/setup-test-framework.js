5cc9580a0ec7f498aa1a5de14fd70fc5a94d859e
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-07-15 16:27:27 +0200

    Tests: Update setup files for test runners

--- test/client/setup-test-framework.js
+++ test/client/setup-test-framework.js
@@ -1,18 +1,51 @@
 /**
  * External dependencies
  */
-import { assert } from 'sinon';
 import { disableNetConnect } from 'nock';
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-import sinonChai from 'sinon-chai';
 
-chai.use( sinonChai );
-chai.use( chaiEnzyme() );
-assert.expose( chai.assert, { prefix: '' } );
-disableNetConnect();
-
-// Make sure we can share test helpers between Mocha and Jest
+// It allows to use Mocha API with Jest
 global.after = global.afterAll;
 global.before = global.beforeAll;
 global.context = global.describe;
+
+// It disables all network requests for all tests.
+disableNetConnect();
+
+// It "mocks" enzyme, so that we can delay loading of
+// the utility functions until enzyme is imported in tests.
+// Props to @gdborton for sharing this technique in his article:
+// https://medium.com/airbnb-engineering/unlocking-test-performance-migrating-from-mocha-to-jest-2796c508ec50.
+let mockEnzymeSetup = false;
+
+jest.mock( 'enzyme', () => {
+	const chai = require( 'chai' );
+
+	const actualEnzyme = require.requireActual( 'enzyme' );
+	if ( ! mockEnzymeSetup ) {
+		const chaiEnzyme = require( 'chai-enzyme' );
+		if ( typeof chaiEnzyme === 'function' && ! mockEnzymeSetup ) {
+			mockEnzymeSetup = true;
+			chai.use( chaiEnzyme() );
+		}
+	}
+	return actualEnzyme;
+} );
+
+// It "mocks" sinon, so that we can delay loading of
+// the utility functions until sinon is imported in tests.
+let mockSinonSetup = false;
+
+jest.mock( 'sinon', () => {
+	const chai = require( 'chai' );
+
+	const actualSinon = require.requireActual( 'sinon' );
+	if ( ! mockSinonSetup ) {
+		const sinonChai = require.requireActual( 'sinon-chai' );
+		if ( typeof sinonChai === 'function' && ! mockSinonSetup ) {
+			mockSinonSetup = true;
+			chai.use( sinonChai );
+			actualSinon.assert.expose( chai.assert, { prefix: '' } );
+		}
+	}
+	return actualSinon;
+} );
