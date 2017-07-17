8dc580bbb04cc4d82c68cb672fccc81f7e27399d
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-07-17 23:26:33 +0200

    Fix React Form's reducer

--- client/extensions/zoninator/state/reducer.js
+++ client/extensions/zoninator/state/reducer.js
@@ -3,4 +3,9 @@
  */
 import { reducer as form } from 'redux-form';
 
-export default form;
+/**
+ * Internal dependencies
+ */
+import { combineReducers } from 'state/utils';
+
+export default combineReducers( { form } );
