2ebf7fc58108491f5d480607bf597e5091c57b0c
Kelly Dwan <ryelle@users.noreply.github.com>
2017-07-18 18:37:18 -0400

    Store: keyboard accessibility cleanup (#16262)
    * Store: Accessibility cleanup based on results from jsx-a11y linter
    
    * Use `Button` for clickable text elements
    
    * Switch to keyboard-accessible events for toggles

--- client/extensions/woocommerce/app/settings/shipping/shipping-zone/shipping-zone-method-list.js
+++ client/extensions/woocommerce/app/settings/shipping/shipping-zone/shipping-zone-method-list.js
@@ -76,13 +76,7 @@ const ShippingZoneMethodList = ( {
 					{ getMethodSummary( method, currency ) }
 				</ListItemField>
 				<ListItemField className="shipping-zone__enable-container">
-					<span onClick={ onEnabledToggle }>
-						{ translate( 'Enabled {{toggle/}}', {
-							components: {
-								toggle: <FormToggle checked={ method.enabled } />
-							}
-						} ) }
-					</span>
+					<FormToggle checked={ method.enabled } onChange={ onEnabledToggle }>{ translate( 'Enabled' ) }</FormToggle>
 				</ListItemField>
 				<ListItemField className="shipping-zone__method-actions">
 					<Button compact onClick={ onEditClick }>{ translate( 'Edit' ) }</Button>
