dcabf59a0812b787394625b166df50f5493cc6c1
Steve Seear <steve.seear@gmail.com>
2017-07-18 14:30:45 +0100

    Plugins: Add upload selectors (#16285)
    Add selectors with unit tests for plugin upload state. Similar to the existing theme upload selectors.
    
    Part of #15665. No visual differences.

---
+++ client/state/selectors/test/get-plugin-upload-error.js
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { getPluginUploadError } from 'state/selectors';
+
+const siteId = 77203074;
+const error = {
+	error: 'folder_exists',
+	message: 'folder_exists',
+};
+
+describe( 'getPluginUploadError', () => {
+	it( 'should return null by default', () => {
+		const state = {
+			plugins: {
+				upload: {
+					uploadError: {}
+				}
+			}
+		};
+		expect( getPluginUploadError( state, siteId ) ).to.be.null;
+	} );
+
+	it( 'should return current value for site', () => {
+		const state = {
+			plugins: {
+				upload: {
+					uploadError: {
+						[ siteId ]: error,
+					},
+				},
+			},
+		};
+		expect( getPluginUploadError( state, siteId ) ).to.deep.equal( error );
+	} );
+} );
---
+++ client/state/selectors/test/is-plugin-upload-complete.js
@@ -0,0 +1,71 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { isPluginUploadComplete } from 'state/selectors';
+
+const siteId = 77203074;
+
+describe( 'isPluginUploadComplete', () => {
+	it( 'should return false by default', () => {
+		const state = {
+			plugins: {
+				upload: {
+					inProgress: {},
+					uploadedPluginId: {},
+				}
+			}
+		};
+		expect( isPluginUploadComplete( state, siteId ) ).to.be.false;
+	} );
+
+	it( 'should return false if no plugin id available', () => {
+		const state = {
+			plugins: {
+				upload: {
+					inProgress: {
+						[ siteId ]: false,
+					},
+					uploadedPluginId: {},
+				}
+			}
+		};
+		expect( isPluginUploadComplete( state, siteId ) ).to.be.false;
+	} );
+
+	it( 'should return false if upload still in progress', () => {
+		const state = {
+			plugins: {
+				upload: {
+					inProgress: {
+						[ siteId ]: true,
+					},
+					uploadedPluginId: {
+						[ siteId ]: 'hello-dolly',
+					},
+				}
+			}
+		};
+		expect( isPluginUploadComplete( state, siteId ) ).to.be.false;
+	} );
+
+	it( 'should return true if upload not in progress and plugin id present', () => {
+		const state = {
+			plugins: {
+				upload: {
+					inProgress: {
+						[ siteId ]: false,
+					},
+					uploadedPluginId: {
+						[ siteId ]: 'hello-dolly',
+					},
+				}
+			}
+		};
+		expect( isPluginUploadComplete( state, siteId ) ).to.be.true;
+	} );
+} );
