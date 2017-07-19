0d8c7fea16475c2350c330d03c7833bb91a4fd98
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-07-19 21:17:26 +0200

    Update Zone and ZoneCreator components

--- client/extensions/zoninator/components/settings/zone-creator/index.jsx
+++ client/extensions/zoninator/components/settings/zone-creator/index.jsx
@@ -1,5 +1,43 @@
-const ZoneCreator = () => {
-	return null;
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import page from 'page';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import { flowRight } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import HeaderCake from 'components/header-cake';
+import { getSelectedSiteSlug } from 'state/ui/selectors';
+
+const ZoneCreator = ( { siteSlug, translate } ) => {
+	const handleGoBack = () => {
+		page( `/extensions/zoninator/${ siteSlug }` );
+	};
+
+	return (
+		<div>
+			<HeaderCake onClick={ handleGoBack }>
+				{ translate( 'Add a zone' ) }
+			</HeaderCake>
+		</div>
+	);
 };
 
-export default ZoneCreator;
+ZoneCreator.propTypes = {
+	siteSlug: PropTypes.string,
+};
+
+const connectComponent = connect( state => {
+	return {
+		siteSlug: getSelectedSiteSlug( state ),
+	};
+} );
+
+export default flowRight(
+	connectComponent,
+	localize,
+)( ZoneCreator );
--- client/extensions/zoninator/components/settings/zone/index.jsx
+++ client/extensions/zoninator/components/settings/zone/index.jsx
@@ -1,5 +1,43 @@
-const Zone = () => {
-	return null;
+/**
+ * External dependencies
+ */
+import React, { PropTypes } from 'react';
+import page from 'page';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import { flowRight } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import HeaderCake from 'components/header-cake';
+import { getSelectedSiteSlug } from 'state/ui/selectors';
+
+const Zone = ( { siteSlug, translate } ) => {
+	const handleGoBack = () => {
+		page( `/extensions/zoninator/${ siteSlug }` );
+	};
+
+	return (
+		<div>
+			<HeaderCake onClick={ handleGoBack }>
+				{ translate( 'Edit zone' ) }
+			</HeaderCake>
+		</div>
+	);
 };
 
-export default Zone;
+Zone.propTypes = {
+	siteSlug: PropTypes.string,
+};
+
+const connectComponent = connect( state => {
+	return {
+		siteSlug: getSelectedSiteSlug( state ),
+	};
+} );
+
+export default flowRight(
+	connectComponent,
+	localize,
+)( Zone );
