8a88924b2f4a0a3714f066d0fe95d08e22b7c5e6
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-07-19 12:07:14 +0100

    Improved the <QuerySettingsGeneral> component logic so it can be mounted/unmounted harmlessly

--- client/extensions/woocommerce/components/query-settings-general/index.js
+++ client/extensions/woocommerce/components/query-settings-general/index.js
@@ -18,30 +18,41 @@ class QuerySettingsGeneral extends Component {
 		this.props.actions.fetchSettingsGeneral( siteId );
 	}
 
-	componentWillMount() {
-		const { siteId, loaded } = this.props;
+	showRetryNotice( siteId ) {
+		const noticeId = 'query-settings-general-retry';
+		this.props.actions.errorNotice( this.props.translate( 'Some of your site settings couldn\'t be retrieved.' ), {
+			id: noticeId,
+			showDismiss: false,
+			button: this.props.translate( 'Try again' ),
+			onClick: () => {
+				this.fetch( siteId );
+				this.props.actions.removeNotice( noticeId );
+			}
+		} );
+	}
 
-		if ( siteId && ! loaded ) {
-			this.fetch( siteId );
+	componentWillMount() {
+		const { siteId, loaded, error } = this.props;
+
+		if ( siteId ) {
+			if ( error ) {
+				this.showRetryNotice( siteId );
+			} else if ( ! loaded ) {
+				this.fetch( siteId );
+			}
 		}
 	}
 
-	componentWillReceiveProps( { siteId, loaded, error, translate } ) {
-		if ( siteId !== this.props.siteId && ! loaded ) {
+	componentWillReceiveProps( { siteId, loaded, error } ) {
+		if ( ! siteId ) {
+			return;
+		}
+		if ( ! this.props.error && error ) {
+			// just received an error fetching, show the notice to retry
+			this.showRetryNotice( siteId );
+		} else if ( siteId !== this.props.siteId && ! loaded ) {
 			// site ID changed, fetch new zones
 			this.fetch( siteId );
-		} else if ( ! this.props.error && error ) {
-			// just received an error fetching, show the notice to retry
-			const noticeId = 'query-settings-general-retry';
-			this.props.actions.errorNotice( translate( 'Some of your site settings couldn\'t be retrieved.' ), {
-				id: noticeId,
-				showDismiss: false,
-				button: translate( 'Try again' ),
-				onClick: () => {
-					this.fetch( siteId );
-					this.props.actions.removeNotice( noticeId );
-				}
-			} );
 		}
 	}
 
