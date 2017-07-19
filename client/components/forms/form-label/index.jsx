5d327b01f713fbd4ace51090b4585430bb9cb684
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-07-19 21:37:05 +0100

    Tweaked the <FormLabel> component so it works with "dangerouslySetInnerHTML"

--- client/components/forms/form-label/index.jsx
+++ client/components/forms/form-label/index.jsx
@@ -6,10 +6,14 @@ import { localize } from 'i18n-calypso';
 import React from 'react';
 
 const FormLabel = ( { children, required, translate, className, ...extraProps } ) => {
+	children = React.Children.toArray( children ) || [];
+	if ( required ) {
+		children.push( <small className="form-label__required">{ translate( 'Required' ) }</small> );
+	}
+
 	return (
 		<label { ...extraProps } className={ classnames( className, 'form-label' ) } >
-			{ children }
-			{ required && <small className="form-label__required">{ translate( 'Required' ) }</small> }
+			{ children.length ? children : null }
 		</label>
 	);
 };
