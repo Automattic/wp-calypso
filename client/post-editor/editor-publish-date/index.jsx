6ed6fa6e8a7de6070b23e33e49e782fdd083728e
Jonathan Sadowski <sadowski@automattic.com>
2017-07-19 08:01:05 -0500

    Add PostSchedule component

---
+++ client/post-editor/editor-publish-date/index.jsx
@@ -0,0 +1,66 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import classNames from 'classnames';
+import Gridicon from 'gridicons';
+
+/**
+ * Internal dependencies
+ */
+import PostSchedule from 'components/post-schedule';
+
+export class EditorPublishDate extends React.Component {
+
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			open: false,
+		};
+	}
+
+	toggleOpenState = () => {
+		this.setState( { open: ! this.state.open } );
+	}
+
+	renderHeader() {
+		const className = classNames( 'editor-publish-date__header', { 'is-open': this.state.open } );
+
+		return (
+			<div className={ className } onClick={ this.toggleOpenState }>
+				<Gridicon icon="calendar" size={ 18 } /> Publish Now
+			</div>
+		);
+	}
+
+	renderSchedule() {
+		const className = classNames( 'editor-publish-date__schedule', {} );
+		return (
+			<div className={ className }>
+				<PostSchedule />
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<div className="editor-publish-date">
+				<div className="editor-publish-date__wrapper">
+					{ this.renderHeader() }
+					{ this.state.open && this.renderSchedule() }
+				</div>
+			</div>
+		);
+	}
+
+}
+
+export default connect(
+	() => {
+		return {};
+	},
+	{}
+)( localize( EditorPublishDate ) );
