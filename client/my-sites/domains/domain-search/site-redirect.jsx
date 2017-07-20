7bf7f5001d2a13fbed08c949ed6b9d75b31649e0
Kamen Stanev <hambai@gmail.com>
2017-07-20 11:17:47 +0300

    Fixed the "Redirect a site" page to remove the target URL protocol (#16193)
    * Fixed a wrong prop type for `isSiteUpgradeable`
    
    * Refactored the code to extend the component class and to use redux for tracking events
    
    * Strip the protocol from redirect urls (we accept only http redirects)
    
    * Remove `isMounted` check when a product is added in the cart
    
    * Replaces vars with const and shortened the translate calls

--- client/my-sites/domains/domain-search/site-redirect.jsx
+++ client/my-sites/domains/domain-search/site-redirect.jsx
@@ -21,7 +21,7 @@ class SiteRedirect extends Component {
 		cart: PropTypes.object.isRequired,
 		selectedSite: PropTypes.object.isRequired,
 		selectedSiteSlug: PropTypes.string.isRequired,
-		isSiteUpgradeable: PropTypes.func.isRequired,
+		isSiteUpgradeable: PropTypes.bool.isRequired,
 		productsList: PropTypes.object.isRequired,
 		translate: PropTypes.func.isRequired,
 	};
