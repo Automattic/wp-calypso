a69c488779374491149098096db3732616691df0
Jake F - @samouri <samouri@users.noreply.github.com>
2017-07-20 13:18:21 -0400

    Check for @format within first docblock instead

--- bin/pre-commit-hook.js
+++ bin/pre-commit-hook.js
@@ -6,6 +6,7 @@ const chalk = require( 'chalk' );
 const fs = require( 'fs' );
 const prettier = require( 'prettier' );
 const path = require( 'path' );
+const _ = require( 'lodash' );
 
 console.log( '\nBy contributing to this project, you license the materials you contribute ' +
 	'under the GNU General Public License v2 (or later). All materials must have ' +
@@ -32,16 +33,27 @@ if ( lintResult.status ) {
 	process.exit( 1 );
 }
 
-// run prettier for any files in the commit
-// that have a line with the contents: '// pretty'
+
+// run prettier for any files in the commit that have @format within their first docblock
+const formatRegex = /@format/;
+const firstDocBlockStart = /\/\*\*/;
+const firstDocBlockEnd = /\*\//;
+
 files
 	.map( file => path.join( __dirname, '../', file ) )
 	.forEach( file => {
 		fs.readFile( file, 'utf8', ( err, data ) => {
-			if ( data.match( /^\/\/ pretty$/m ) ) {
+			const firstDocBlockStartIndex = _.get( firstDocBlockStart.exec( data ), 'index' );
+			const firstDocBlockEndIndex = _.get( firstDocBlockEnd.exec( data ), 'index' );
+			const firstDocBlockText =
+				_.isInteger( firstDocBlockStartIndex ) &&
+				_.isInteger( firstDocBlockEndIndex ) &&
+				data.substring( firstDocBlockStartIndex, firstDocBlockEndIndex + 2 );
+
+			if ( firstDocBlockText && formatRegex.test( firstDocBlockText ) ) {
 				const formattedData = prettier.format( data, {} );
 				fs.writeFileSync( file, formattedData );
 				execSync( `git add ${ file }` );
 			}
-		} )
+		} );
 } );
