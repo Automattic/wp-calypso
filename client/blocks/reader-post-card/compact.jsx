6641d2ba34002657d08e939cfc2960996e55c46e
Chris R <chris@bluefuton.com>
2017-07-21 10:32:33 +0100

    Reader: add compact post card (#16256)
    * Add compact post card
    
    * Hide post actions and avatar
    
    * Don't allow playing of video
    
    * Turn off the Discover bells and whistles for compact cards
    
    * Add back post options menu
    
    * Position post options menu
    
    * Style is-compact card
    
    * Add is-compact modifier
    
    * Move post byline inside compact post
    
    * Fix styling
    
    * Use sans-serif for byline
    
    * Use one fade only for byline and timestamp
    
    * Do not wrap timestamp

---
+++ client/blocks/reader-post-card/compact.jsx
@@ -0,0 +1,68 @@
+/**
+ * External Dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal Dependencies
+ */
+import AutoDirection from 'components/auto-direction';
+import Emojify from 'components/emojify';
+import ReaderFeaturedVideo from 'blocks/reader-featured-video';
+import ReaderFeaturedImage from 'blocks/reader-featured-image';
+import ReaderExcerpt from 'blocks/reader-excerpt';
+import ReaderPostOptionsMenu from 'blocks/reader-post-options-menu';
+
+const CompactPost = ( { post, postByline, children, isDiscover } ) => {
+	const canonicalMedia = post.canonical_media;
+	let featuredAsset;
+	if ( ! canonicalMedia ) {
+		featuredAsset = null;
+	} else if ( canonicalMedia.mediaType === 'video' ) {
+		featuredAsset = (
+			<ReaderFeaturedVideo
+				{ ...canonicalMedia }
+				videoEmbed={ canonicalMedia }
+				allowPlaying={ false }
+			/>
+		);
+	} else {
+		featuredAsset = <ReaderFeaturedImage imageUrl={ canonicalMedia.src } href={ post.URL } />;
+	}
+
+	/* eslint-disable wpcalypso/jsx-classname-namespace */
+	return (
+		<div className="reader-post-card__post">
+			{ featuredAsset }
+			<div className="reader-post-card__post-details">
+				{ postByline }
+				<ReaderPostOptionsMenu
+					className="ignore-click"
+					showFollow={ true }
+					post={ post }
+					position="bottom"
+				/>
+				<AutoDirection>
+					<h1 className="reader-post-card__title">
+						<a className="reader-post-card__title-link" href={ post.URL }>
+							<Emojify>
+								{ post.title }
+							</Emojify>
+						</a>
+					</h1>
+				</AutoDirection>
+				<ReaderExcerpt post={ post } isDiscover={ isDiscover } />
+				{ children }
+			</div>
+		</div>
+	);
+	/* eslint-enable wpcalypso/jsx-classname-namespace */
+};
+
+CompactPost.propTypes = {
+	post: React.PropTypes.object.isRequired,
+	postByline: React.PropTypes.string,
+	isDiscover: React.PropTypes.bool,
+};
+
+export default CompactPost;
