e2aae9e086743809196ecf7a08404056db78728a
Steve Seear <steve.seear@gmail.com>
2017-07-21 11:40:38 +0100

    Plugins: Add success and error notices for uploads

--- client/state/data-layer/wpcom/sites/plugins/new/index.js
+++ client/state/data-layer/wpcom/sites/plugins/new/index.js
@@ -1,4 +1,10 @@
 /**
+ * External dependencies
+ */
+import { translate } from 'i18n-calypso';
+import { find, includes, toLower } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import { PLUGIN_UPLOAD, PLUGIN_INSTALL_REQUEST_SUCCESS } from 'state/action-types';
@@ -9,6 +15,7 @@ import {
 } from 'state/plugins/upload/actions';
 import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
 import { http } from 'state/data-layer/wpcom-http/actions';
+import { successNotice, errorNotice } from 'state/notices/actions';
 
 export const uploadPlugin = ( { dispatch }, action ) => {
 	const { siteId, file } = action;
@@ -21,6 +28,38 @@ export const uploadPlugin = ( { dispatch }, action ) => {
 	}, action ) );
 };
 
+const showSuccessNotice = ( dispatch, { name } ) => {
+	dispatch( successNotice(
+		translate( 'Successfully uploaded plugin %(name)s', {
+			args: { name }
+		} ),
+		{ duration: 5000 }
+	) );
+};
+
+const showErrorNotice = ( dispatch, error ) => {
+	const knownErrors = {
+		exists: translate( 'Upload problem: Plugin already installed on site.' ),
+		'too large': translate( 'Upload problem: Plugin zip must be smaller than 10MB.' ),
+		incompatible: translate( 'Upload problem: Incompatible plugin.' ),
+		unsupported_mime_type: translate( 'Upload problem: Not a valid zip file' ),
+	};
+	const errorString = toLower( error.error + error.message );
+	const knownError = find( knownErrors, ( v, key ) => includes( errorString, key ) );
+
+	if ( knownError ) {
+		dispatch( errorNotice( knownError ) );
+		return;
+	}
+	if ( error.error ) {
+		dispatch( errorNotice( translate( 'Upload problem: %(error)s', {
+			args: { error: error.error }
+		} ) ) );
+		return;
+	}
+	dispatch( errorNotice( translate( 'Problem uploading plugin' ) ) );
+};
+
 export const uploadComplete = ( { dispatch }, { siteId }, next, data ) => {
 	const { slug: pluginId } = data;
 	dispatch( completePluginUpload( siteId, pluginId ) );
@@ -30,9 +69,12 @@ export const uploadComplete = ( { dispatch }, { siteId }, next, data ) => {
 		pluginId,
 		data
 	} );
+
+	showSuccessNotice( dispatch, data );
 };
 
 export const receiveError = ( { dispatch }, { siteId }, next, error ) => {
+	showErrorNotice( dispatch, error );
 	dispatch( pluginUploadError( siteId, error ) );
 };
 
