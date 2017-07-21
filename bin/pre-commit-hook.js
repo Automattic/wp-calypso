62f68ebf8461cde6fffbfe5a56421ce3d07c18c2
Jake F - @samouri <samouri@users.noreply.github.com>
2017-07-21 13:56:00 -0400

    extract out the shouldFormat check, add early returns, allow early block comments

--- bin/pre-commit-hook.js
+++ bin/pre-commit-hook.js
@@ -8,9 +8,11 @@ const prettier = require( 'prettier' );
 const path = require( 'path' );
 const _ = require( 'lodash' );
 
-console.log( '\nBy contributing to this project, you license the materials you contribute ' +
-	'under the GNU General Public License v2 (or later). All materials must have ' +
-	'GPLv2 compatible licenses — see .github/CONTRIBUTING.md for details.\n\n' );
+console.log(
+	'\nBy contributing to this project, you license the materials you contribute ' +
+		'under the GNU General Public License v2 (or later). All materials must have ' +
+		'GPLv2 compatible licenses — see .github/CONTRIBUTING.md for details.\n\n',
+);
 
 // Make quick pass over config files on every change
 require( '../server/config/validate-config-keys' );
@@ -18,8 +20,8 @@ require( '../server/config/validate-config-keys' );
 const files = execSync( 'git diff --cached --name-only' )
 	.toString()
 	.split( '\n' )
-	.map( ( name ) => name.trim() )
-	.filter( ( name ) => name.endsWith( '.js' ) || name.endsWith( '.jsx' ) );
+	.map( name => name.trim() )
+	.filter( name => name.endsWith( '.js' ) || name.endsWith( '.jsx' ) );
 
 const lintResult = spawnSync( 'eslint-eslines', [ ...files, '--', '--diff=index' ], {
 	shell: true,
@@ -27,33 +29,52 @@ const lintResult = spawnSync( 'eslint-eslines', [ ...files, '--', '--diff=index'
 } );
 
 if ( lintResult.status ) {
-	console.log( chalk.red( 'COMMIT ABORTED:' ), 'The linter reported some problems. ' +
-		'If you are aware of them and it is OK, ' +
-		'repeat the commit command with --no-verify to avoid this check.' );
+	console.log(
+		chalk.red( 'COMMIT ABORTED:' ),
+		'The linter reported some problems. ' +
+			'If you are aware of them and it is OK, ' +
+			'repeat the commit command with --no-verify to avoid this check.',
+	);
 	process.exit( 1 );
 }
 
+/**
+ * Returns true if the given text contains @format within its first docblock. False otherwise.
+ *
+ * @param {*} text text to scan for @format within the first docblock
+ */
+const shouldFormat = text => {
+	const firstDocBlockStart = /\/\*\*/;
+	const firstDocBlockEnd = /\*\//;
+
+	const firstDocBlockStartIndex = _.get( firstDocBlockStart.exec( text ), 'index' );
+
+	if ( ! firstDocBlockStartIndex ) {
+		return;
+	}
+
+	firstDocBlockEnd.lastIndex = firstDocBlockStartIndex; // docBlockEnd must come after the start
+	const firstDocBlockEndIndex = _.get( firstDocBlockEnd.exec( text ), 'index' );
+
+	if ( ! firstDocBlockEndIndex ) {
+		return;
+	}
+
+	const firstDocBlockText =
+		_.isInteger( firstDocBlockStartIndex ) &&
+		_.isInteger( firstDocBlockEndIndex ) &&
+		text.substring( firstDocBlockStartIndex, firstDocBlockEndIndex + 2 );
+
+	return firstDocBlockText && /@format/.test( firstDocBlockText );
+};
 
 // run prettier for any files in the commit that have @format within their first docblock
-const formatRegex = /@format/;
-const firstDocBlockStart = /\/\*\*/;
-const firstDocBlockEnd = /\*\//;
-
-files
-	.map( file => path.join( __dirname, '../', file ) )
-	.forEach( file => {
-		fs.readFile( file, 'utf8', ( err, data ) => {
-			const firstDocBlockStartIndex = _.get( firstDocBlockStart.exec( data ), 'index' );
-			const firstDocBlockEndIndex = _.get( firstDocBlockEnd.exec( data ), 'index' );
-			const firstDocBlockText =
-				_.isInteger( firstDocBlockStartIndex ) &&
-				_.isInteger( firstDocBlockEndIndex ) &&
-				data.substring( firstDocBlockStartIndex, firstDocBlockEndIndex + 2 );
-
-			if ( firstDocBlockText && formatRegex.test( firstDocBlockText ) ) {
-				const formattedData = prettier.format( data, {} );
-				fs.writeFileSync( file, formattedData );
-				execSync( `git add ${ file }` );
-			}
-		} );
+files.map( file => path.join( __dirname, '../', file ) ).forEach( file => {
+	fs.readFile( file, 'utf8', ( err, text ) => {
+		if ( shouldFormat( text ) ) {
+			const formattedText = prettier.format( text, {} );
+			fs.writeFileSync( file, formattedText );
+			execSync( `git add ${ file }` );
+		}
+	} );
 } );
