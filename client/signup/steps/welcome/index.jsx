dcd6b1895d82979cc828592faf18dda2284e8988
Mark Ryall <mark@ryall.name>
2017-07-23 18:46:30 -0700

    eliminate eslint warnings from WelcomeStep

--- client/signup/steps/welcome/index.jsx
+++ client/signup/steps/welcome/index.jsx
@@ -1,55 +1,64 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import StepWrapper from 'signup/step-wrapper';
 import SignupActions from 'lib/signup/actions';
-import wpcom from 'lib/wp';
 
-module.exports = React.createClass( {
-	getInitialState: function() {
-		return {
-			form: null,
-			submitting: false
-		};
-	},
+class WelcomeStep extends Component {
+	state = {
+		form: null,
+		submitting: false
+	};
 
-	handleSubmit: function( event ) {
+	handleSubmit = ( event ) => {
 		event.preventDefault();
 
 		//Create site here
 
 		SignupActions.submitSignupStep( {
 			stepName: this.props.stepName,
-		});
+		} );
 
 		this.props.goToNextStep();
-	},
+	}
 
-	renderContent: function() {
+	renderContent() {
+		const buttonClass = 'button is-primary';
 		return (
-			<div className="welcome-content">
-				<button className="button is-primary" onClick={ this.handleSubmit }>
-					{ this.translate( 'Create your account' ) }
+			<div className="welcome__content">
+				<button className={ buttonClass } onClick={ this.handleSubmit }>
+					{ this.props.translate( 'Create your account' ) }
 				</button>
 			</div>
-		)
-	},
+		);
+	}
 
-	render: function() {
+	render() {
+		const {
+			flowName,
+			positionInFlow,
+			signupProgress,
+			stepName,
+			translate,
+		} = this.props;
+
 		return (
 			<StepWrapper
-				flowName={ this.props.flowName }
-				stepName={ this.props.stepName }
-				positionInFlow={ this.props.positionInFlow }
-				headerText={ this.translate( 'Welcome to WordPress.com' ) }
-				subHeaderText={ this.translate( 'We\'ve partnered with Rebrand Cities to get businesses online. Get started today.' ) }
-				signupProgress={ this.props.signupProgress }
+				flowName={ flowName }
+				stepName={ stepName }
+				positionInFlow={ positionInFlow }
+				headerText={ translate( 'Welcome to WordPress.com' ) }
+				subHeaderText={ translate( 'We\'ve partnered with Rebrand Cities to get businesses online. Get started today.' ) }
+				signupProgress={ signupProgress }
 				stepContent={ this.renderContent() } />
 		);
 	}
-} );
+}
+
+export default localize( WelcomeStep );
