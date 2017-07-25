19b88d411a0f8d183e27094f37580bbb0d61c8d4
Andrija Vucinic <andrija.vucinic@gmail.com>
2017-07-26 00:52:34 +0200

    extract functions from domains controlloer

--- client/my-sites/domains/controller.jsx
+++ client/my-sites/domains/controller.jsx
@@ -30,171 +30,183 @@ import { getCurrentUser } from 'state/current-user/selectors';
  */
 const productsList = productsFactory();
 
-export default {
-	domainsAddHeader: function( context, next ) {
-		context.getSiteSelectionHeaderText = function() {
-			return i18n.translate( 'Select a site to add a domain' );
-		};
+export const domainsAddHeader = ( context, next ) => {
+	context.getSiteSelectionHeaderText = () => {
+		return i18n.translate( 'Select a site to add a domain' );
+	};
 
+	next();
+};
+
+export const domainsAddRedirectHeader = ( context, next ) => {
+	context.getSiteSelectionHeaderText = () => {
+		return i18n.translate( 'Select a site to add Site Redirect' );
+	};
+
+	next();
+};
+
+export const domainSearch = ( context ) => {
+	const CartData = require( 'components/data/cart' );
+	const DomainSearch = require( './domain-search' );
+	const basePath = route.sectionify( context.path );
+
+	// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
+	context.store.dispatch( setTitle( i18n.translate( 'Domain Search' ) ) );
+
+	analytics.pageView.record( basePath, 'Domain Search > Domain Registration' );
+
+	// Scroll to the top
+	if ( typeof window !== 'undefined' ) {
+		window.scrollTo( 0, 0 );
+	}
+
+	renderWithReduxStore(
+		(
+			<CartData>
+				<DomainSearch
+					basePath={ basePath }
+					context={ context }
+				/>
+			</CartData>
+		),
+		document.getElementById( 'primary' ),
+		context.store
+	);
+};
+
+export const siteRedirect = ( context ) => {
+	const CartData = require( 'components/data/cart' );
+	const SiteRedirect = require( './domain-search/site-redirect' );
+	const basePath = route.sectionify( context.path );
+
+	// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
+	context.store.dispatch( setTitle( i18n.translate( 'Redirect a Site' ) ) );
+
+	analytics.pageView.record( basePath, 'Domain Search > Site Redirect' );
+
+	renderWithReduxStore(
+		(
+			<CartData>
+				<SiteRedirect />
+			</CartData>
+		),
+		document.getElementById( 'primary' ),
+		context.store
+	);
+};
+
+export const mapDomain = ( context ) => {
+	const CartData = require( 'components/data/cart' );
+	const MapDomain = require( 'my-sites/domains/map-domain' ).default;
+	const basePath = route.sectionify( context.path );
+
+	// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
+	context.store.dispatch( setTitle( i18n.translate( 'Map a Domain' ) ) );
+
+	analytics.pageView.record( basePath, 'Domain Search > Domain Mapping' );
+	renderWithReduxStore(
+		(
+			<Main>
+				<CartData>
+					<MapDomain
+						initialQuery={ context.query.initialQuery }
+					/>
+				</CartData>
+			</Main>
+		),
+		document.getElementById( 'primary' ),
+		context.store
+	);
+};
+
+export const googleAppsWithRegistration = ( context ) => {
+	const CartData = require( 'components/data/cart' );
+	const GoogleApps = require( 'components/upgrades/google-apps' );
+
+	// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
+	context.store.dispatch( setTitle(
+		i18n.translate( 'Register %(domain)s', {
+			args: { domain: context.params.registerDomain }
+		} )
+	) );
+
+	const state = context.store.getState();
+	const siteSlug = getSelectedSiteSlug( state ) || '';
+
+	const handleAddGoogleApps = ( googleAppsCartItem ) => {
+		upgradesActions.addItem( googleAppsCartItem );
+		page( '/checkout/' + siteSlug );
+	};
+
+	const handleGoBack = () => {
+		page( '/domains/add/' + siteSlug );
+	};
+
+	const handleClickSkip = () => {
+		page( '/checkout/' + siteSlug );
+	};
+
+	analytics.pageView.record( '/domains/add/:site/google-apps', 'Domain Search > Domain Registration > Google Apps' );
+
+	renderWithReduxStore(
+		(
+			<Main>
+				<CartData>
+					<GoogleApps
+						productsList={ productsList }
+						domain={ context.params.registerDomain }
+						onGoBack={ handleGoBack }
+						onAddGoogleApps={ handleAddGoogleApps }
+						onClickSkip={ handleClickSkip }
+					/>
+				</CartData>
+			</Main>
+		),
+		document.getElementById( 'primary' ),
+		context.store
+	);
+};
+
+export const redirectIfNoSite = ( redirectTo ) => {
+	return ( context, next ) => {
+		const state = context.store.getState();
+		const siteId = getSelectedSiteId( state );
+		const userCanManageOptions = canCurrentUser( state, siteId, 'manage_options' );
+
+		if ( ! userCanManageOptions ) {
+			const user = getCurrentUser( state );
+			const visibleSiteCount = get( user, 'visible_site_count', 0 );
+			//if only one site navigate to stats to avoid redirect loop
+			const redirect = visibleSiteCount > 1 ? redirectTo : '/stats';
+			return page.redirect( redirect );
+		}
 		next();
-	},
+	};
+};
 
-	domainsAddRedirectHeader: function( context, next ) {
-		context.getSiteSelectionHeaderText = function() {
-			return i18n.translate( 'Select a site to add Site Redirect' );
-		};
+export const redirectToAddMappingIfVipSite = () => {
+	return ( context, next ) => {
+		const state = context.store.getState();
+		const selectedSite = getSelectedSite( state );
+		const domain = context.params.domain ? `/${ context.params.domain }` : '';
+		const query = qs.stringify( { initialQuery: context.params.suggestion } );
 
-		next();
-	},
-
-	domainSearch: function( context ) {
-		const CartData = require( 'components/data/cart' );
-		const DomainSearch = require( './domain-search' );
-		const basePath = route.sectionify( context.path );
-
-		// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
-		context.store.dispatch( setTitle( i18n.translate( 'Domain Search' ) ) );
-
-		analytics.pageView.record( basePath, 'Domain Search > Domain Registration' );
-
-		// Scroll to the top
-		if ( typeof window !== 'undefined' ) {
-			window.scrollTo( 0, 0 );
+		if ( selectedSite && selectedSite.is_vip ) {
+			return page.redirect( `/domains/add/mapping${ domain }?${ query }` );
 		}
 
-		renderWithReduxStore(
-			(
-				<CartData>
-					<DomainSearch
-						basePath={ basePath }
-						context={ context } />
-				</CartData>
-			),
-			document.getElementById( 'primary' ),
-			context.store
-		);
-	},
+		next();
+	};
+};
 
-	siteRedirect: function( context ) {
-		const CartData = require( 'components/data/cart' );
-		const SiteRedirect = require( './domain-search/site-redirect' );
-		const basePath = route.sectionify( context.path );
-
-		// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
-		context.store.dispatch( setTitle( i18n.translate( 'Redirect a Site' ) ) );
-
-		analytics.pageView.record( basePath, 'Domain Search > Site Redirect' );
-
-		renderWithReduxStore(
-			(
-				<CartData>
-					<SiteRedirect />
-				</CartData>
-			),
-			document.getElementById( 'primary' ),
-			context.store
-		);
-	},
-
-	mapDomain: function( context ) {
-		const CartData = require( 'components/data/cart' );
-		const MapDomain = require( 'my-sites/domains/map-domain' ).default;
-		const basePath = route.sectionify( context.path );
-
-		// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
-		context.store.dispatch( setTitle( i18n.translate( 'Map a Domain' ) ) );
-
-		analytics.pageView.record( basePath, 'Domain Search > Domain Mapping' );
-		renderWithReduxStore(
-			(
-				<Main>
-					<CartData>
-						<MapDomain
-							initialQuery={ context.query.initialQuery } />
-					</CartData>
-				</Main>
-			),
-			document.getElementById( 'primary' ),
-			context.store
-		);
-	},
-
-	googleAppsWithRegistration: function( context ) {
-		const CartData = require( 'components/data/cart' );
-		const GoogleApps = require( 'components/upgrades/google-apps' );
-
-		// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
-		context.store.dispatch( setTitle(
-			i18n.translate( 'Register %(domain)s', {
-				args: { domain: context.params.registerDomain }
-			} )
-		) );
-
-		const state = context.store.getState();
-		const siteSlug = getSelectedSiteSlug( state ) || '';
-
-		const handleAddGoogleApps = function( googleAppsCartItem ) {
-			upgradesActions.addItem( googleAppsCartItem );
-			page( '/checkout/' + siteSlug );
-		};
-
-		const handleGoBack = function() {
-			page( '/domains/add/' + siteSlug );
-		};
-
-		const handleClickSkip = function() {
-			page( '/checkout/' + siteSlug );
-		};
-
-		analytics.pageView.record( '/domains/add/:site/google-apps', 'Domain Search > Domain Registration > Google Apps' );
-
-		renderWithReduxStore(
-			(
-				<Main>
-					<CartData>
-						<GoogleApps
-							productsList={ productsList }
-							domain={ context.params.registerDomain }
-							onGoBack={ handleGoBack }
-							onAddGoogleApps={ handleAddGoogleApps }
-							onClickSkip={ handleClickSkip } />
-					</CartData>
-				</Main>
-			),
-			document.getElementById( 'primary' ),
-			context.store
-		);
-	},
-
-	redirectIfNoSite: function( redirectTo ) {
-		return function( context, next ) {
-			const state = context.store.getState();
-			const siteId = getSelectedSiteId( state );
-			const userCanManageOptions = canCurrentUser( state, siteId, 'manage_options' );
-
-			if ( ! userCanManageOptions ) {
-				const user = getCurrentUser( state );
-				const visibleSiteCount = get( user, 'visible_site_count', 0 );
-				//if only one site navigate to stats to avoid redirect loop
-				const redirect = visibleSiteCount > 1 ? redirectTo : '/stats';
-				return page.redirect( redirect );
-			}
-			next();
-		};
-	},
-
-	redirectToAddMappingIfVipSite: function() {
-		return function( context, next ) {
-			const state = context.store.getState();
-			const selectedSite = getSelectedSite( state );
-			const domain = context.params.domain ? `/${ context.params.domain }` : '';
-			const query = qs.stringify( { initialQuery: context.params.suggestion } );
-
-			if ( selectedSite && selectedSite.is_vip ) {
-				return page.redirect( `/domains/add/mapping${ domain }?${ query }` );
-			}
-
-			next();
-		};
-	}
+export default {
+	domainsAddHeader,
+	domainsAddRedirectHeader,
+	domainSearch,
+	siteRedirect,
+	mapDomain,
+	googleAppsWithRegistration,
+	redirectIfNoSite,
+	redirectToAddMappingIfVipSite,
 };
