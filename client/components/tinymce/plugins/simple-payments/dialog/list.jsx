6b1f1c93e93c284dfab4277df88d7188e1f25125
Jarda Snajdr <jsnajdr@gmail.com>
2017-07-25 17:09:44 +0200

    Simple Payments: add an EllipsisMenu to the product list item
    First, move the `ProductListItem` to a separate React component -- it's already complex enough to deserve one.
    Second, show an `EllipsisMenu` with "Edit" and "Trash" actions on every item. Clicking on them doesn't do anything yet, we're just adding the UI in this PR.

--- client/components/tinymce/plugins/simple-payments/dialog/list.jsx
+++ client/components/tinymce/plugins/simple-payments/dialog/list.jsx
@@ -4,58 +4,56 @@
  * External dependencies
  */
 import React, { Component, PropTypes } from 'react';
-import { localize } from 'i18n-calypso';
 import { noop } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import formatCurrency from 'lib/format-currency';
-import CompactCard from 'components/card/compact';
-import FormRadio from 'components/forms/form-radio';
+import ProductListItem from './list-item';
 
 class ProductList extends Component {
 	static propTypes = {
 		paymentButtons: PropTypes.array.isRequired,
 		selectedPaymentId: PropTypes.number,
 		onSelectedChange: PropTypes.func,
+		onEditClick: PropTypes.func,
+		onTrashClick: PropTypes.func,
 	};
 
 	static defaultProps = {
 		selectedPaymentId: null,
 		onSelectedChange: noop,
-	};
-
-	handleRadioChange = event => {
-		this.props.onSelectedChange( parseInt( event.target.value ) );
+		onEditClick: noop,
+		onTrashClick: noop,
 	};
 
 	render() {
-		const { paymentButtons, selectedPaymentId } = this.props;
+		const {
+			paymentButtons,
+			selectedPaymentId,
+			onSelectedChange,
+			onEditClick,
+			onTrashClick,
+		} = this.props;
 
 		return (
 			<div className="editor-simple-payments-modal__list">
-				{ paymentButtons.map( ( { ID: paymentId, title, price, currency } ) => {
-					const radioId = `simple-payments-list-item-radio-${ paymentId }`;
-
-					return (
-						<CompactCard className="editor-simple-payments-modal__list-item" key={ paymentId }>
-							<FormRadio
-								name="selection"
-								id={ radioId }
-								value={ paymentId }
-								checked={ selectedPaymentId === paymentId }
-								onChange={ this.handleRadioChange }
-							/>
-							<label className="editor-simple-payments-modal__list-label" htmlFor={ radioId }>
-								<div>{ title }</div><div>{ formatCurrency( price, currency ) }</div>
-							</label>
-						</CompactCard>
-					);
-				} ) }
+				{ paymentButtons.map( ( { ID: paymentId, title, price, currency } ) =>
+					<ProductListItem
+						key={ paymentId }
+						paymentId={ paymentId }
+						isSelected={ selectedPaymentId === paymentId }
+						title={ title }
+						price={ price }
+						currency={ currency }
+						onSelectedChange={ onSelectedChange }
+						onEditClick={ onEditClick }
+						onTrashClick={ onTrashClick }
+					/>,
+				) }
 			</div>
 		);
 	}
 }
 
-export default localize( ProductList );
+export default ProductList;
