226894e4abbc6b22b71b8d93902c68c929a0f7ac
Marin Atanasov <tyxla@abv.bg>
2017-07-25 15:46:14 +0300

    Sharing: Autoactivate likes module if necessary for Jetpack sites

--- client/my-sites/sharing/buttons/buttons.jsx
+++ client/my-sites/sharing/buttons/buttons.jsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React, { Component, PropTypes } from 'react';
-import { flowRight } from 'lodash';
+import { flowRight, get } from 'lodash';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
@@ -11,6 +11,7 @@ import { localize } from 'i18n-calypso';
  */
 import ButtonsAppearance from './appearance';
 import ButtonsOptions from './options';
+import QueryJetpackModules from 'components/data/query-jetpack-modules';
 import QuerySiteSettings from 'components/data/query-site-settings';
 import QuerySharingButtons from 'components/data/query-sharing-buttons';
 import { saveSiteSettings } from 'state/site-settings/actions';
@@ -18,8 +19,11 @@ import { saveSharingButtons } from 'state/sites/sharing-buttons/actions';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { getSiteSettings, isSavingSiteSettings, isSiteSettingsSaveSuccessful } from 'state/site-settings/selectors';
 import { getSharingButtons, isSavingSharingButtons, isSharingButtonsSaveSuccessful } from 'state/selectors';
+import { isJetpackSite } from 'state/sites/selectors';
+import { isJetpackModuleActive } from 'state/selectors';
 import { recordGoogleEvent } from 'state/analytics/actions';
 import { successNotice, errorNotice } from 'state/notices/actions';
+import { activateModule, fetchModuleList } from 'state/jetpack/modules/actions';
 import { protectForm } from 'lib/protect-form';
 
 class SharingButtons extends Component {
@@ -41,12 +45,32 @@ class SharingButtons extends Component {
 	};
 
 	saveChanges = event => {
+		const {
+			isJetpack,
+			isLikesModuleActive,
+			siteId
+		} = this.props;
+
 		event.preventDefault();
+
 		this.props.saveSiteSettings( this.props.siteId, this.state.values );
 		if ( this.state.buttonsPendingSave ) {
 			this.props.saveSharingButtons( this.props.siteId, this.state.buttonsPendingSave );
 		}
 		this.props.recordGoogleEvent( 'Sharing', 'Clicked Save Changes Button' );
+
+		if ( ! isJetpack || isLikesModuleActive !== false ) {
+			return;
+		}
+
+		const currentLikesSettingValue = get( this.props, [ 'settings', 'disabled_likes' ] );
+		const isLikesButtonEnabled = get( this.state, [ 'values', 'disabled_likes' ], currentLikesSettingValue ) === false;
+		if ( ! isLikesButtonEnabled ) {
+			return;
+		}
+
+		this.props.activateModule( siteId, 'likes', true )
+			.then( () => this.props.fetchModuleList( siteId ) );
 	};
 
 	handleChange = ( option, value ) => {
@@ -87,7 +111,13 @@ class SharingButtons extends Component {
 	}
 
 	render() {
-		const { buttons, isSaving, settings, siteId } = this.props;
+		const {
+			buttons,
+			isJetpack,
+			isSaving,
+			settings,
+			siteId
+		} = this.props;
 		const updatedSettings = Object.assign( {}, settings, this.state.values );
 		const updatedButtons = this.state.buttonsPendingSave || buttons;
 
@@ -95,6 +125,7 @@ class SharingButtons extends Component {
 			<form onSubmit={ this.saveChanges } id="sharing-buttons" className="sharing-settings sharing-buttons">
 				<QuerySiteSettings siteId={ siteId } />
 				<QuerySharingButtons siteId={ siteId } />
+				{ isJetpack && <QueryJetpackModules siteId={ siteId } /> }
 				<ButtonsAppearance
 					buttons={ updatedButtons }
 					values={ updatedSettings }
@@ -116,12 +147,16 @@ const connectComponent = connect(
 		const siteId = getSelectedSiteId( state );
 		const settings = getSiteSettings( state, siteId );
 		const buttons = getSharingButtons( state, siteId );
+		const isJetpack = isJetpackSite( state, siteId );
+		const isLikesModuleActive = isJetpackModuleActive( state, siteId, 'likes' );
 		const isSavingSettings = isSavingSiteSettings( state, siteId );
 		const isSavingButtons = isSavingSharingButtons( state, siteId );
 		const isSaveSettingsSuccessful = isSiteSettingsSaveSuccessful( state, siteId );
 		const isSaveButtonsSuccessful = isSharingButtonsSaveSuccessful( state, siteId );
 
 		return {
+			isJetpack,
+			isLikesModuleActive,
 			isSaving: isSavingSettings || isSavingButtons,
 			isSaveSettingsSuccessful,
 			isSaveButtonsSuccessful,
@@ -130,7 +165,15 @@ const connectComponent = connect(
 			siteId
 		};
 	},
-	{ errorNotice, recordGoogleEvent, saveSiteSettings, saveSharingButtons, successNotice }
+	{
+		activateModule,
+		errorNotice,
+		fetchModuleList,
+		recordGoogleEvent,
+		saveSharingButtons,
+		saveSiteSettings,
+		successNotice
+	}
 );
 
 export default flowRight(
