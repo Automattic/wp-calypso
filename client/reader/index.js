062f5102555aad41aa8b3ee3eb12a4ef5c648d0b
Miguel Fonseca <miguelcsf@gmail.com>
2017-07-26 18:21:18 +0100

    Reader: Inspect module- & route loading
    No functional change.

--- client/reader/index.js
+++ client/reader/index.js
@@ -27,9 +27,15 @@ function forceTeamA8C( context, next ) {
 	next();
 }
 
+function hello( context, next ) {
+	console.log( 'running reader page handling!' );
+	next();
+}
+
 export default function() {
+	console.log( 'loaded reader module!' );
 	if ( config.isEnabled( 'reader' ) ) {
-		page( '/', preloadReaderBundle, initAbTests, updateLastRoute, sidebar, following );
+		page( '/', hello, preloadReaderBundle, initAbTests, updateLastRoute, sidebar, following );
 
 		// Old and incomplete paths that should be redirected to /
 		page( '/read/following', '/' );
