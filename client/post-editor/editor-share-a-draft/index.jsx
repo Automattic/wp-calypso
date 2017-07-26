d3b639909e03288c65e39c080ea0cebdcdefed9c
Brandon Payton <brandon@happycode.net>
2017-07-26 02:23:29 -0700

    Add initial editor-share-a-draft component

---
+++ client/post-editor/editor-share-a-draft/index.jsx
@@ -0,0 +1,42 @@
+/**
+ * External dependencies
+ */
+import React, { PropTypes, Component } from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Accordion from 'components/accordion';
+import FormToggle from 'components/forms/form-toggle';
+import FormTextInputWithAction from 'components/forms/form-text-input-with-action';
+
+class EditorShareADraft extends Component {
+	static propTypes = {
+		translate: PropTypes.func.isRequired,
+	};
+
+	render() {
+		const { translate } = this.props;
+
+		return (
+			<Accordion className="editor-share-a-draft" title={ translate( 'Share a Draft' ) }>
+				<p>
+					{ translate(
+						'Enable draft sharing and we’ll create a public link to your draft, ' +
+							'so you can ask a trusted reader for feedback before you publish.',
+					) }
+				</p>
+				<FormToggle>
+					{ translate( 'Enable draft sharing' ) }
+				</FormToggle>
+				<FormTextInputWithAction
+					readonly
+					action={ translate( 'Copy', { context: 'verb: imperative' } ) }
+				/>
+			</Accordion>
+		);
+	}
+}
+
+export default localize( EditorShareADraft );
