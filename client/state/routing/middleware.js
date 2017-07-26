117a49fea89517fee88cdebdd7e11f04bab0e47e
Miguel Fonseca <miguelcsf@gmail.com>
2017-07-26 18:19:51 +0100

    Routing: Don't restore path in middleware
    But keep the middleware, as we'll still need to save the last path.

--- client/state/routing/middleware.js
+++ client/state/routing/middleware.js
@@ -3,44 +3,24 @@
  */
 import debugFactory from 'debug';
 import { isEmpty } from 'lodash';
-import page from 'page';
 
 /**
  * Internal dependencies
  */
 import {
-	getSavedPath,
 	savePath,
 } from 'lib/restore-last-path';
 import { ROUTE_SET } from 'state/action-types';
 
 const debug = debugFactory( 'calypso:restore-last-location' );
-let hasInitialized = false;
 
 export const routingMiddleware = () => {
 	return ( next ) => ( action ) => {
-		if ( action.type !== ROUTE_SET || ! action.path || ! action.query ) {
+		if ( action.type !== ROUTE_SET ||
+				! action.path ||
+				! action.query ||
+				! isEmpty( action.query ) ) {
 			return next( action );
-		}
-
-		if ( ! isEmpty( action.query ) ) {
-			return next( action );
-		}
-
-		const isFirstRun = ! hasInitialized;
-		hasInitialized = true;
-
-		if ( isFirstRun && action.path === '/' ) {
-			// Attempt to restore the last path on the first run
-			return getSavedPath()
-					.then( ( lastPath ) => {
-						debug( 'restoring: ' + lastPath );
-						page( lastPath );
-					} )
-					.catch( ( reason ) => {
-						debug( 'cannot restore', reason );
-						next( action );
-					} );
 		}
 
 		// Attempt to save the path so it might be restored in the future
