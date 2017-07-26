32a71bbc30d08b40af8c94d4640a26a6f7ece05c
Tugdual de Kerviler <dekervit@gmail.com>
2017-07-26 15:28:13 +0200

    Explain what we're doing with OAuth routes

--- client/auth/index.js
+++ client/auth/index.js
@@ -10,6 +10,7 @@ import config from 'config';
 import controller from './controller';
 
 export default () => {
+	// Always enable the /oauth-login route and redirect to /log-in if `oauth` is disabled
 	page( '/oauth-login', controller.oauthLogin );
 
 	if ( config.isEnabled( 'oauth' ) ) {
