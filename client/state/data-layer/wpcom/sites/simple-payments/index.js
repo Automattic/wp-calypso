7266e109ce9ef8341b8f57dbde6055f4bfa377cf
Rastislav Lamos <lamos.rasto@gmail.com>
2017-07-26 19:55:45 +0200

    Simple Payments: fix data-layer to not depend on non-SSR ready code
    This was causing tests to fail. Big props to @ockham for identifying the
    issue and helping me to fix it.

--- client/state/data-layer/wpcom/sites/simple-payments/index.js
+++ client/state/data-layer/wpcom/sites/simple-payments/index.js
@@ -25,7 +25,7 @@ import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
 import { SIMPLE_PAYMENTS_PRODUCT_POST_TYPE } from 'lib/simple-payments/constants';
 import { isValidSimplePaymentsProduct } from 'lib/simple-payments/utils';
 import formatCurrency from 'lib/format-currency';
-import { getFeaturedImageId } from 'lib/posts/utils';
+import { getFeaturedImageId } from 'lib/posts/utils-ssr-ready';
 
 /**
  * Reduce function for product attributes stored in post metadata
