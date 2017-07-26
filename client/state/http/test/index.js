27edf45c382207429df131f08284fafa0034bb71
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-07-26 12:05:17 +0200

    Tests: Migrate newly added state http test to use Jest

--- client/state/http/test/index.js
+++ client/state/http/test/index.js
@@ -1,3 +1,5 @@
+jest.mock( 'superagent', () => require( './superagent' ).handler );
+
 /**
  * External dependencies
  */
@@ -7,9 +9,10 @@ import sinon from 'sinon';
 /**
  * Internal dependencies
  */
-import useMockery from 'test/helpers/use-mockery';
 import { extendAction } from 'state/utils';
 import { failureMeta, successMeta } from 'state/data-layer/wpcom-http';
+import { httpHandler } from '../';
+import superagentMock from './superagent';
 
 const succeeder = { type: 'SUCCESS' };
 const failer = { type: 'FAIL' };
@@ -23,73 +26,8 @@ const getMe = {
 	onSuccess: succeeder,
 };
 
-class SuperAgentMock {
-	constructor() {
-		this._success = false;
-		this._data = new Error( 'Mock response is not set' );
-		this._lastRequest = null;
-	}
-
-	/***
-	 * Mocks superagent() call that returns request
-	 *
-	 * const request = superagent( method, url );
-	 *
-	 * @param {String} method HTTP method
-	 * @param {String} url URL
-	 * @returns {Object} superagent like request with sinon spies
-	 */
-	handler = ( method, url ) => {
-		const request = {
-			method,
-			url,
-			withCredentials: sinon.spy(),
-			set: sinon.spy(),
-			query: sinon.spy(),
-			accept: sinon.spy(),
-			send: sinon.spy(),
-			then: ( successHandler, failureHandler ) =>
-				this._success
-					? successHandler( { body: this._data } )
-					: failureHandler( { response: { body: this._data } } ),
-		};
-
-		this._lastRequest = request;
-
-		return request;
-	};
-
-	/***
-	 * Tells the mock how to act, successful response or failure
-	 *
-	 * @param {Boolean} success should the request call success handler or failure?
-	 * @param {Object} data the data that should be passed in body
-	 */
-	setResponse( success, data ) {
-		this._success = success;
-		this._data = data;
-	}
-
-	/***
-	 * Get last mocked request
-	 * @returns {Object} An object similar to superagent's request with additional `method` and `url` fields
-	 */
-	getLastRequest() {
-		return this._lastRequest;
-	}
-}
-
-const superagentMock = new SuperAgentMock();
-
 describe( '#httpHandler', () => {
 	let dispatch;
-	let httpHandler;
-
-	useMockery( mockery => mockery.registerMock( 'superagent', superagentMock.handler ) );
-
-	before( () => {
-		httpHandler = require( '../' ).httpHandler; // it's done here for mockery to work
-	} );
 
 	beforeEach( () => {
 		dispatch = sinon.spy();
