254778b3107b2dc5cd671e5b98241a47b4255311
Bernhard Reiter <ockham@raz.or.at>
2017-07-27 15:27:22 +0200

    Use enzyme

--- client/components/forms/form-phone-input/test/index.jsx
+++ client/components/forms/form-phone-input/test/index.jsx
@@ -2,11 +2,9 @@
  * External dependencies
  */
 import React from 'react';
-import ReactDom from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
 import { withContainer as useFakeDom } from 'test/helpers/use-fake-dom';
-import { getContainer } from 'test/helpers/use-fake-dom';
 
 /**
  * Internal dependencies
@@ -18,57 +16,42 @@ import mockCountriesListEmpty from './mocks/mock-countries-list-empty';
 const countries = mockCountriesList.get();
 
 describe( 'FormPhoneInput', function() {
-	var container;
-
 	useFakeDom();
-
-	before( function() {
-		container = getContainer();
-	} );
-
-	afterEach( function() {
-		ReactDom.unmountComponentAtNode( container );
-	} );
 
 	describe( 'getValue()', function() {
 		it( 'should set country from props', function() {
-			var phoneComponent = ReactDom.render( <FormPhoneInput countriesList={ mockCountriesList } initialCountryCode={ countries[ 1 ].code } />, container );
-
-			expect( phoneComponent.getValue().countryData ).to.deep.equal( countries[ 1 ] );
+			const phoneComponent = shallow( <FormPhoneInput countriesList={ mockCountriesList } initialCountryCode={ countries[ 1 ].code } /> );
+			expect( phoneComponent.instance().getValue().countryData ).to.deep.equal( countries[ 1 ] );
 		} );
 
 		it( 'should set country to first element when not specified', function() {
-			var phoneComponent = ReactDom.render( <FormPhoneInput countriesList={ mockCountriesList } />, container );
-
-			expect( phoneComponent.getValue().countryData ).to.deep.equal( countries[ 0 ] );
+			const phoneComponent = shallow( <FormPhoneInput countriesList={ mockCountriesList } /> );
+			expect( phoneComponent.instance().getValue().countryData ).to.deep.equal( countries[ 0 ] );
 		} );
 
 		it( 'should update country on change', function() {
-			var phoneComponent = ReactDom.render( <FormPhoneInput countriesList={ mockCountriesList } />, container ),
-				select = TestUtils.findRenderedDOMComponentWithTag( phoneComponent, 'select' );
-
-			TestUtils.Simulate.change( select, {
+			const phoneComponent = mount( <FormPhoneInput countriesList={ mockCountriesList } /> );
+			phoneComponent.find( 'select' ).simulate( 'change', {
 				target: {
 					value: countries[ 1 ].code
 				}
 			} );
 
-			expect( phoneComponent.getValue().countryData ).to.deep.equal( countries[ 1 ] );
+			expect( phoneComponent.instance().getValue().countryData ).to.deep.equal( countries[ 1 ] );
 		} );
 
 		it( 'should have no country with empty countryList', function() {
-			var phoneComponent = ReactDom.render( <FormPhoneInput countriesList={ mockCountriesListEmpty } />, container );
-
-			expect( phoneComponent.getValue().countryData ).to.equal( undefined );
+			const phoneComponent = shallow( <FormPhoneInput countriesList={ mockCountriesListEmpty } /> );
+			expect( phoneComponent.instance().getValue().countryData ).to.equal( undefined );
 		} );
 
 		it( 'should update country on countryList change', function() {
-			var phoneComponent = ReactDom.render( <FormPhoneInput countriesList={ mockCountriesListEmpty } />, container );
+			let phoneComponent = shallow( <FormPhoneInput countriesList={ mockCountriesListEmpty } /> );
 
 			// Render again with filled country list
-			phoneComponent = ReactDom.render( <FormPhoneInput countriesList={ mockCountriesList } />, container );
+			phoneComponent = shallow( <FormPhoneInput countriesList={ mockCountriesList } /> );
 
-			expect( phoneComponent.getValue().countryData ).to.deep.equal( countries[ 0 ] );
+			expect( phoneComponent.instance().getValue().countryData ).to.deep.equal( countries[ 0 ] );
 		} );
 	} );
 } );
