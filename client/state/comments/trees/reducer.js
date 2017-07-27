63b2a07f8448d98348d2b716d54d307da0055f92
Copons <copons@users.noreply.github.com>
2017-07-27 20:50:02 +0100

    Comment tree listens to the change status action

--- client/state/comments/trees/reducer.js
+++ client/state/comments/trees/reducer.js
@@ -1,12 +1,13 @@
 /**
  * External dependencies
  */
-import { reject, unionBy } from 'lodash';
+import { map, reject, unionBy } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import {
+	COMMENTS_CHANGE_STATUS,
 	COMMENTS_DELETE,
 	COMMENTS_TREE_SITE_ADD,
 } from 'state/action-types';
@@ -14,9 +15,18 @@ import { keyedReducer } from 'state/utils';
 
 const siteTree = ( state = [], action ) => {
 	switch ( action.type ) {
+		case COMMENTS_CHANGE_STATUS:
+			return map( state, comment => {
+				if ( comment.commentId === action.commentId ) {
+					return {
+						...comment,
+						status: action.status,
+					};
+				}
+				return comment;
+			} );
 		case COMMENTS_DELETE:
-			const { commentId } = action;
-			return reject( state, { commentId } );
+			return reject( state, { commentId: action.commentId } );
 		case COMMENTS_TREE_SITE_ADD:
 			return unionBy( action.tree, state, 'commentId' );
 	}
