076fe59aa3e97cc9e3d6e7d713e3a30e89d10048
Steve Seear <steve.seear@gmail.com>
2017-07-27 17:45:22 +0100

    Add some components

--- client/my-sites/demo/main.jsx
+++ client/my-sites/demo/main.jsx
@@ -7,9 +7,14 @@ import React from 'react';
  * Internal dependencies
  */
 import Main from 'components/main';
+import EmptyContent from 'components/empty-content';
+import HeaderCake from 'components/header-cake';
 
 export default function() {
 	return (
-		<Main>Hello World!</Main>
+		<Main>
+			<HeaderCake backHref="/">Demo Page</HeaderCake>
+			<EmptyContent title="Demo Page" line="Nothing to see here." />
+		</Main>
 	);
 }
