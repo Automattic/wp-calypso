578c6b776330658ed6948cc8eb0732bfe26d8568
Jacopo Tomasone <Copons@users.noreply.github.com>
2017-07-27 20:53:03 +0100

    Comment: Update CommentDetailPlaceholder style (#16636)
    This is a small update to the `CommentDetailPlaceholder` style in order to use it as a simple class (`.comment-detail__placeholder`) on a normal `CommentDetail`.
    
    It will be useful once we lazy-load comments.
    We know the number of comments to show, render them as placeholders, and gradually replace them with the real deal.
    
    This does not change the placeholder appearance in any way. It just does a better job at hiding the incomplete parts that might be shown while the comment is loading.

--- client/blocks/comment-detail/comment-detail-placeholder.jsx
+++ client/blocks/comment-detail/comment-detail-placeholder.jsx
@@ -9,9 +9,9 @@ import React from 'react';
 import Card from 'components/card';
 
 export const CommentDetailPlaceholder = () =>
-	<Card className="comment-detail comment-detail__placeholder is-expanded">
+	<Card className="comment-detail comment-detail__placeholder">
 		<div className="comment-detail__header is-preview">
-			<div className="comment-detail__author-info">
+			<div className="comment-detail__author-preview">
 				<div className="comment-detail__author-avatar gravatar" />
 			</div>
 			<div className="comment-detail__comment-preview" />
