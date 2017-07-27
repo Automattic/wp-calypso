213747bba4f1ee7f95c16623294e5a54a1415b97
Stéphane Thomas <email@stephanethomas.com>
2017-07-27 16:39:42 +0200

    Login: Improve readability of Private Site component
    This also removes an unnecessary prop type.

--- client/login/wp-login/private-site.jsx
+++ client/login/wp-login/private-site.jsx
@@ -12,21 +12,30 @@ import Card from 'components/card';
 
 class PrivateSite extends Component {
 	static propTypes = {
-		redirectTo: PropTypes.string,
 		translate: PropTypes.func.isRequired,
 	};
 
 	render() {
 		const { translate } = this.props;
+
 		return (
 			<Card>
 				<div className="wp-login__private-image">
 					<img src="/calypso/images/private/private.svg" />
 				</div>
-				<h2 className="wp-login__private-header">{ translate( 'This is a private WordPress.com site.' ) }</h2>
-				<p className="wp-login__private-text">{ translate( "Request an invitation to view it and we'll " +
-					'send your username to the site owner for their approval.' ) }</p>
-				<Button primary={ true } className="wp-login__private-button">{ translate( 'Request Invitation' ) }</Button>
+
+				<h2 className="wp-login__private-header">
+					{ translate( 'This is a private WordPress.com site.' ) }
+				</h2>
+
+				<p className="wp-login__private-text">
+					{ translate( "Request an invitation to view it and we'll " +
+						'send your username to the site owner for their approval.' ) }
+				</p>
+
+				<Button primary={ true } className="wp-login__private-button">
+					{ translate( 'Request Invitation' ) }
+				</Button>
 			</Card>
 		);
 	}
