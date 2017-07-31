760c6bd7e51c9034796ee24d1c763c49c96a9258
caydijay <j_kuk@outlook.com>
2017-07-31 02:38:58 -0700

    fix formatting issue in readme-viewer text

--- client/devdocs/docs-example/test/readme-viewer.jsx
+++ client/devdocs/docs-example/test/readme-viewer.jsx
@@ -15,16 +15,16 @@ describe( 'ReadmeViewer', () => {
 	it( 'should render README.md when given readmeFilePath', () => {
 		const getReadmeHTML = sinon.spy();
 		const readmeViewer = render( <ReadmeViewer readmeFilePath="foo" getReadmeHTML={ getReadmeHTML } /> );
-		expect( readmeViewer.find( '#docs-example__readme-present-header' ) ).to.have.length(1);
-		expect( readmeViewer.find( '#docs-example__readme-not-present-header' ) ).to.have.length(0);
+		expect( readmeViewer.find( '#docs-example__readme-present-header' ) ).to.have.length( 1 );
+		expect( readmeViewer.find( '#docs-example__readme-not-present-header' ) ).to.have.length( 0 );
 		assert.isTrue( getReadmeHTML.called );
 	} );
 
 	it( 'should not render a README.md when not given readmeFilePath', () => {
 		const getReadmeHTML = sinon.spy();
 		const readmeViewer = render( <ReadmeViewer getReadmeHTML={ getReadmeHTML } /> );
-		expect( readmeViewer.find( '#docs-example__readme-present-header' ) ).to.have.length(0);
-		expect( readmeViewer.find( '#docs-example__readme-not-present-header' ) ).to.have.length(1);
+		expect( readmeViewer.find( '#docs-example__readme-present-header' ) ).to.have.length( 0 );
+		expect( readmeViewer.find( '#docs-example__readme-not-present-header' ) ).to.have.length( 1 );
 		assert.isTrue( getReadmeHTML.notCalled );
 	} );
 } );
