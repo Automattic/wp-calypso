3c059a8ae4ee9a3d9525b3baa7fb23e6da9c2603
Jason Johnston <jasonjohnston76@gmail.com>
2017-07-31 17:04:05 -0700

    add product image to editor preview

--- client/components/tinymce/plugins/wpcom-view/views/simple-payments/index.jsx
+++ client/components/tinymce/plugins/wpcom-view/views/simple-payments/index.jsx
@@ -6,6 +6,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
+import { get } from 'lodash';
 
 /**
  * Internal dependecies
@@ -16,34 +17,36 @@ import { getSimplePayments } from 'state/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import formatCurrency from 'lib/format-currency';
 import QuerySimplePayments from 'components/data/query-simple-payments';
+import QueryMedia from 'components/data/query-media';
+import { getMediaItem } from 'state/selectors';
 
 class SimplePaymentsView extends Component {
 	render() {
-		const { productId, product, siteId } = this.props;
+		const { productId, product, siteId, productImage } = this.props;
 
 		if ( ! product ) {
 			return ( <QuerySimplePayments siteId={ siteId } productId={ productId } /> );
 		}
 
-		const { title, description, price, currency } = product;
-
-		// TODO: add from product.
-		const imageUrl = 'https://cldup.com/nKM0_KspYE.png';
+		const { title, description, price, currency, featuredImageId: productImageId } = product;
 
 		// TODO: make proper icon and store on some proper place.
 		const paypalButtonImageUrl = 'https://cldup.com/DoIAwrACBs.png';
 
 		return (
 			<div className="wpview-content wpview-type-simple-payments">
+				{ productImageId && <QueryMedia siteId={ siteId } mediaId={ productImageId } /> }
 				<div className="wpview-type-simple-payments__wrapper">
+				{ productImage &&
 					<div className="wpview-type-simple-payments__image-part">
 						<figure className="wpview-type-simple-payments__image-figure">
 							<img
 								className="wpview-type-simple-payments__image"
-								src={ imageUrl }
+								src={ productImage.URL }
 							/>
 						</figure>
 					</div>
+				}
 					<div className="wpview-type-simple-payments__text-part">
 						<div className="wpview-type-simple-payments__title">
 							{ title }
@@ -84,12 +87,14 @@ SimplePaymentsView = connect( ( state, props ) => {
 
 	const { id: productId = null } = shortcodeData;
 	const siteId = getSelectedSiteId( state );
+	const product = getSimplePayments( state, siteId, productId );
 
 	return {
 		shortcodeData,
 		productId,
 		siteId,
-		product: getSimplePayments( state, siteId, productId ),
+		product,
+		productImage: getMediaItem( state, siteId, get( product, 'featuredImageId' ) ),
 	};
 } )( localize( SimplePaymentsView ) );
 
