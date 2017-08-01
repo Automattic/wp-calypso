ec7c6c621b49af952c6915b9ae536122003b0770
Andrija Vucinic <andrija.vucinic@gmail.com>
2017-08-02 01:30:18 +0200

    Janitorial: Refactor Domains List component
    - ES6ify
    - Use localize
    - Use redux analytics actions

--- client/my-sites/domains/domain-management/list/domain-list-notice.jsx
+++ client/my-sites/domains/domain-management/list/domain-list-notice.jsx
@@ -1,12 +1,12 @@
 /**
- * External Dependencies
- **/
+ * External dependencies
+ */
 import React from 'react';
-import i18n from 'i18n-calypso';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal Dependencies
- **/
+ */
 import Notice from 'components/notice';
 import NoticeAction from 'components/notice/notice-action';
 import {
@@ -16,8 +16,8 @@ import {
 	PRIMARY_DOMAIN_REVERT_SUCCESS
 } from './constants';
 
-const DomainListNotice = ( { type, domainName, errorMessage, onUndoClick, onDismissClick } ) => {
-	let props = {
+const DomainListNotice = ( { type, domainName, errorMessage, onUndoClick, onDismissClick, translate } ) => {
+	const props = {
 		className: 'domain-management-list__notice',
 		showDismiss: false,
 		onDismissClick
@@ -26,30 +26,30 @@ const DomainListNotice = ( { type, domainName, errorMessage, onUndoClick, onDism
 	switch ( type ) {
 		case PRIMARY_DOMAIN_CHANGE_SUCCESS:
 			Object.assign( props, {
-				text: <span key="text">{ i18n.translate( 'Primary Domain changed: All domains will redirect to' +
-					' {{em}}%(domainName)s{{/em}}.', { args: { domainName }, components: { em: <em/> } } ) }</span>,
+				text: <span key="text">{ translate( 'Primary Domain changed: All domains will redirect to' +
+					' {{em}}%(domainName)s{{/em}}.', { args: { domainName }, components: { em: <em /> } } ) }</span>,
 				status: 'is-success',
 				children: (
 					<NoticeAction
 						icon="refresh"
 						key="undo-btn"
 						onClick={ onUndoClick }>
-						{ i18n.translate( 'Undo' ) }
+						{ translate( 'Undo' ) }
 					</NoticeAction>
 				)
 			} );
 			break;
 		case PRIMARY_DOMAIN_CHANGE_FAIL:
 			Object.assign( props, {
-				text: errorMessage || i18n.translate( 'Something went wrong and we couldn\'t change your Primary Domain.' ),
+				text: errorMessage || translate( 'Something went wrong and we couldn\'t change your Primary Domain.' ),
 				status: 'is-error',
 				showDismiss: true
 			} );
 			break;
 		case PRIMARY_DOMAIN_REVERT_SUCCESS:
 			Object.assign( props, {
-				text: <span key="text">{ i18n.translate( 'No worries, your Primary Domain has been set back to' +
-					' {{em}}%(domainName)s{{/em}}.', { args: { domainName }, components: { em: <em/> } } ) }</span>,
+				text: <span key="text">{ translate( 'No worries, your Primary Domain has been set back to' +
+					' {{em}}%(domainName)s{{/em}}.', { args: { domainName }, components: { em: <em /> } } ) }</span>,
 				status: 'is-success',
 				showDismiss: true,
 				duration: 10000
@@ -57,7 +57,7 @@ const DomainListNotice = ( { type, domainName, errorMessage, onUndoClick, onDism
 			break;
 		case PRIMARY_DOMAIN_REVERT_FAIL:
 			Object.assign( props, {
-				text: <span>{ errorMessage || i18n.translate( 'Something went wrong and we couldn\'t revert the changes' ) }</span>,
+				text: <span>{ errorMessage || translate( 'Something went wrong and we couldn\'t revert the changes' ) }</span>,
 				status: 'is-error',
 				showDismiss: true
 			} );
@@ -69,4 +69,4 @@ const DomainListNotice = ( { type, domainName, errorMessage, onUndoClick, onDism
 	);
 };
 
-export default DomainListNotice;
+export default localize( DomainListNotice );
