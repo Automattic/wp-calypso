14f2fdc9135bae1fe6e67e836be4ea0b0b56d0c7
Mark Ryall <mark@ryall.name>
2017-08-03 14:10:18 +1000

    fix lint error: let should be const

--- client/signup/flow-progress-indicator/index.jsx
+++ client/signup/flow-progress-indicator/index.jsx
@@ -6,8 +6,7 @@ import { localize } from 'i18n-calypso';
 
 const FlowProgressIndicator = ( { flowLength, positionInFlow, translate, flowName } ) => {
 	if ( flowLength > 1 ) {
-
-		let flowClassName = `flow-progress-indicator flow-progress-indicator--${ flowName }`;
+		const flowClassName = `flow-progress-indicator flow-progress-indicator--${ flowName }`;
 
 		return (
 			<div className={ flowClassName }>
