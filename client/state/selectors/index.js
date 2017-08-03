911f568597169f628b5f2d05bd25a4de89b03442
Jon Surrell <jon.surrell@automattic.com>
2017-08-03 11:46:20 +0200

    Add getMediaStorageUsed selector

--- client/state/selectors/index.js
+++ client/state/selectors/index.js
@@ -56,6 +56,7 @@ export getMagicLoginRequestEmailError from './get-magic-login-request-email-erro
 export getMedia from './get-media';
 export getMediaItem from './get-media-item';
 export getMediaStorageLimit from './get-media-storage-limit';
+export getMediaStorageUsed from './get-media-storage-used';
 export getMediaUrl from './get-media-url';
 export getMenuItemTypes from './get-menu-item-types';
 export getMenusUrl from './get-menus-url';
---
+++ client/state/selectors/test/get-media-storage-used.js
@@ -0,0 +1,57 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { getMediaStorageUsed } from '..';
+
+describe( 'getMediaStorageUsed()', () => {
+	it( 'should return null if the site is unknown', () => {
+		const state = {
+			sites: {
+				mediaStorage: {
+					items: {
+						456: { storage_used_bytes: 12345 },
+					},
+				},
+			},
+		};
+
+		expect( getMediaStorageUsed( state ) ).to.be.null;
+		expect( getMediaStorageUsed( state, 123 ) ).to.be.null;
+	} );
+
+	it( 'should return null if usage is unknown', () => {
+		const state = {
+			sites: {
+				mediaStorage: {
+					items: {
+						123: {},
+						456: { storage_used_bytes: 12345 },
+					},
+				},
+			},
+		};
+		expect( getMediaStorageUsed( state, 123 ) ).to.be.null;
+	} );
+
+	it( 'should return the storage used for a site', () => {
+		const storage_used_bytes = 1029384756;
+		const result = getMediaStorageUsed( {
+			sites: {
+				mediaStorage: {
+					items: {
+						123: {
+							storage_used_bytes,
+						},
+					},
+				},
+			},
+		}, 123 );
+
+		expect( result ).to.equal( storage_used_bytes );
+	} );
+} );
