479f1d54c5bb4ee9634e8ba2cbdc10c7d34d03e8
John Godley <johng75@gmail.com>
2017-08-03 08:39:48 +0100

    Sharing: add library to request external auth
    Uses the PopupMonitor to open a new window and request authorisation.

---
+++ client/lib/sharing/index.js
@@ -0,0 +1,16 @@
+/**
+ * Internal dependencies
+ */
+
+import PopupMonitor from 'lib/popup-monitor';
+
+const requestExternalAccess = ( url, cb ) => {
+	const popupMonitor = new PopupMonitor();
+
+	popupMonitor.open( url, null, 'toolbar=0,location=0,status=0,menubar=0,' +
+		popupMonitor.getScreenCenterSpecs( 780, 500 ) );
+
+	popupMonitor.once( 'close', cb );
+};
+
+export default requestExternalAccess;
