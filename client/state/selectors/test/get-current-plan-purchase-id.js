82143b2737fe5c207b4ff2942efb6362fdbc57cc
Jon Surrell <jon.surrell@automattic.com>
2017-08-07 09:53:20 +0200

    Add getCurrentPlanPurchaseId selector

---
+++ client/state/selectors/test/get-current-plan-purchase-id.js
@@ -0,0 +1,107 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { getCurrentPlanPurchaseId } from '..';
+
+describe( 'getCurrentPlanPurchaseId()', () => {
+	it( 'should return null if the site is unknown', () => {
+		const state = {
+			sites: {
+				plans: {
+					456: {
+						data: [
+							{
+								currentPlan: true,
+								id: 98765,
+							},
+						],
+					},
+				},
+				items: {
+					456: {},
+				},
+			},
+		};
+
+		expect( getCurrentPlanPurchaseId( state ) ).to.be.null;
+		expect( getCurrentPlanPurchaseId( state, 123 ) ).to.be.null;
+	} );
+
+	it( 'should return null if the current purchase ID is unknown', () => {
+		const siteId = 123;
+		const state = {
+			sites: {
+				plans: {
+					[ siteId ]: {
+						data: [
+							{
+								currentPlan: false,
+								id: 98765,
+							},
+							{
+								currentPlan: true,
+							},
+						],
+					},
+				},
+				items: {
+					[ siteId ]: {},
+				},
+			},
+		};
+
+		expect( getCurrentPlanPurchaseId( state, siteId ) ).to.be.null;
+	} );
+
+	it( 'should return null if there is no plans data, but there is site plan data', () => {
+		const siteId = 123;
+		const state = {
+			sites: {
+				plans: {
+					[ siteId ]: {
+						data: [],
+					},
+				},
+				items: {
+					[ siteId ]: {
+						plan: {},
+					},
+				},
+			},
+		};
+
+		expect( getCurrentPlanPurchaseId( state, siteId ) ).to.be.null;
+	} );
+
+	it( 'should return the purchase ID for a site', () => {
+		const siteId = 123;
+		const purchaseId = 123456;
+		const state = {
+			sites: {
+				plans: {
+					[ siteId ]: {
+						data: [
+							{
+								currentPlan: true,
+								id: purchaseId,
+							},
+						],
+					},
+				},
+				items: {
+					[ siteId ]: {
+						plan: { some: 'plan' },
+					},
+				},
+			},
+		};
+
+		expect( getCurrentPlanPurchaseId( state, siteId ) ).to.equal( purchaseId );
+	} );
+} );
