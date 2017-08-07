00ed7ec57165c38cacbc5d0e0db77a14155a2508
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-08-07 08:01:17 +0200

    Update docblocks

--- client/extensions/zoninator/state/zones/actions.js
+++ client/extensions/zoninator/state/zones/actions.js
@@ -7,8 +7,27 @@ import {
 	ZONINATOR_UPDATE_ZONES,
 } from '../action-types';
 
+/**
+ * Returns an action object to indicate that a request has been made to fetch the zones.
+ *
+ * @param  {Number} siteId Site ID
+ * @return {Action}        Action object
+ */
 export const requestZones = siteId => ( { type: ZONINATOR_REQUEST_ZONES, siteId } );
 
+/**
+ * Returns an action object to indicate that an error was received when fetching the zones.
+ *
+ * @param  {Number} siteId Site ID
+ * @return {Action}        Action object
+ */
 export const requestError = siteId => ( { type: ZONINATOR_REQUEST_ERROR, siteId } );
 
+/**
+ * Returns an action object to indicate that all the zones should be updated.
+ *
+ * @param  {Number} siteId Site ID
+ * @param  {Object} data   Zones
+ * @return {Object}        Action object
+ */
 export const updateZones = ( siteId, data ) => ( { type: ZONINATOR_UPDATE_ZONES, siteId, data } );
--- client/extensions/zoninator/state/zones/selectors.js
+++ client/extensions/zoninator/state/zones/selectors.js
@@ -5,8 +5,22 @@ import { get } from 'lodash';
 
 const getZonesState = ( state ) => state.extensions.zoninator.zones;
 
+/**
+ * Returns true if zones are being requested for the specified site ID.
+ *
+ * @param  {Object} state  Global state tree
+ * @param  {Number} siteId Site ID
+ * @return {Boolean}       Whether zones are being requested
+ */
 export const isRequestingZones = ( state, siteId ) =>
 	get( getZonesState( state ), [ 'requesting', siteId ], false );
 
+/**
+ * Returns the zones for the specified site ID.
+ *
+ * @param  {Object} state  Global state tree
+ * @param  {Number} siteId Site ID
+ * @return {Array}         Zones
+ */
 export const getZones = ( state, siteId ) =>
 	get( getZonesState( state ), [ 'items', siteId ], [] );
