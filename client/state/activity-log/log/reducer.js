af7221db4956719e9efb096d9df15f24cb811276
Jon Surrell <jon.surrell@automattic.com>
2017-08-07 17:51:14 +0200

    Use withSchemaValidation for persistence (fix tests)

--- client/state/activity-log/log/reducer.js
+++ client/state/activity-log/log/reducer.js
@@ -4,15 +4,16 @@
  */
 import { logItemsSchema } from './schema';
 import { ACTIVITY_LOG_UPDATE } from 'state/action-types';
-import { createReducer, keyedReducer } from 'state/utils';
+import { createReducer, keyedReducer, withSchemaValidation } from 'state/utils';
 
 // FIXME: No-op reducers
 export const logError = keyedReducer( 'siteId', state => state );
 
-export const logItems = keyedReducer(
+const logItemsReducer = keyedReducer(
 	'siteId',
 	createReducer( [], {
 		[ ACTIVITY_LOG_UPDATE ]: ( state, { data } ) => data,
 	} )
 );
-logItems.schema = logItemsSchema;
+
+export const logItems = withSchemaValidation( logItemsSchema, logItemsReducer );
