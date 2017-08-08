ed77bdb7113ccd69017c5e73bde1e23d04cd561e
Shaun Andrews <shaun@automattic.com>
2017-08-08 15:40:26 -0400

    Renaming Pages to Site Pages and moving it above Blog Posts in the publish menu.

--- client/my-sites/pages/controller.js
+++ client/my-sites/pages/controller.js
@@ -27,7 +27,7 @@ const controller = {
 			baseAnalyticsPath;
 
 		status = ( ! status || status === siteID ) ? '' : status;
-		context.store.dispatch( setTitle( i18n.translate( 'Pages', { textOnly: true } ) ) ); // FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
+		context.store.dispatch( setTitle( i18n.translate( 'Site Pages', { textOnly: true } ) ) ); // FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
 
 		if ( siteID ) {
 			baseAnalyticsPath = basePath + '/:site';
