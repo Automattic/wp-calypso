e478818829f546f511df26a2942753381efbaa02
bluefuton <chris@bluefuton.com>
2017-08-10 12:56:50 +0200

    Use prop-types

--- client/components/site-selector/index.jsx
+++ client/components/site-selector/index.jsx
@@ -2,7 +2,8 @@
 /**
  * External dependencies
  */
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ReactDom from 'react-dom';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
