94c701e8c48df14428c53853e5e9fcfe892c3475
Kelly Dwan <ryelle@users.noreply.github.com>
2017-08-10 18:29:58 -0400

    Update note display with data & style to match mockup

--- client/extensions/woocommerce/app/order/order-notes/note.js
+++ client/extensions/woocommerce/app/order/order-notes/note.js
@@ -2,13 +2,14 @@
  * External dependencies
  */
 import { localize } from 'i18n-calypso';
+import Gridicon from 'gridicons';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 /**
  * Internal dependencies
  */
-import { stripHTML } from 'lib/formatting';
+import { decodeEntities, stripHTML } from 'lib/formatting';
 
 class OrderNote extends Component {
 	static propTypes = {
@@ -20,12 +21,34 @@ class OrderNote extends Component {
 	}
 
 	render() {
-		const { note, translate } = this.props;
+		const {
+			customer_note,
+			date_created,
+			note,
+			moment,
+			translate
+		} = this.props;
+
+		// @todo Add comment author once we have that info
+		let icon = 'aside';
+		let note_type = translate( 'Internal note' );
+		if ( customer_note ) {
+			icon = 'mail';
+			note_type = translate( 'Note sent to customer' );
+		}
 
 		return (
 			<div className="order-notes__note">
-				<h4>{ translate( 'Note' ) }</h4>
-				{ stripHTML( note ) }
+				<div className="order-notes__note-meta">
+					<span className="order-notes__note-time">{ moment( date_created ).format( 'LT' ) }</span>
+					<Gridicon icon={ icon } size={ 24 } />
+				</div>
+				<div className="order-notes__note-body">
+					<div className="order-notes__note-type">{ note_type }</div>
+					<div className="order-notes__note-content">
+						{ decodeEntities( stripHTML( note ) ) }
+					</div>
+				</div>
 			</div>
 		);
 	}
