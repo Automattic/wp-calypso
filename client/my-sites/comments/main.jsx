608be2d55c37556858afe27c28f8fa203ce5edca
Kerry Liu <gwwar@users.noreply.github.com>
2017-08-10 14:24:26 -0700

    Comments: show permission error (#17115)

--- client/my-sites/comments/main.jsx
+++ client/my-sites/comments/main.jsx
@@ -14,11 +14,15 @@ import PageViewTracker from 'lib/analytics/page-view-tracker';
 import DocumentHead from 'components/data/document-head';
 import CommentList from './comment-list';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
+import { canCurrentUser } from 'state/selectors';
+import { preventWidows } from 'lib/formatting';
+import EmptyContent from 'components/empty-content';
 
 export class CommentsManagement extends Component {
 	static propTypes = {
 		basePath: PropTypes.string,
 		comments: PropTypes.array,
+		showPermissionError: PropTypes.bool,
 		siteId: PropTypes.number,
 		siteFragment: PropTypes.oneOfType( [
 			PropTypes.string,
@@ -30,6 +34,7 @@ export class CommentsManagement extends Component {
 
 	render() {
 		const {
+			showPermissionError,
 			basePath,
 			siteId,
 			siteFragment,
@@ -42,19 +47,30 @@ export class CommentsManagement extends Component {
 				<PageViewTracker path={ basePath } title="Comments" />
 				<DocumentHead title={ translate( 'Comments' ) } />
 				<SidebarNavigation />
-				<CommentList
+				{ showPermissionError && <EmptyContent
+					title={ preventWidows( translate( 'Oops! You don\'t have permission to manage comments.' ) ) }
+					line={ preventWidows( translate( 'If you think you should, contact this site\'s administrator.' ) ) }
+					illustration="/calypso/images/illustrations/illustration-500.svg" />
+				}
+				{ ! showPermissionError && <CommentList
 					siteId={ siteId }
 					siteFragment={ siteFragment }
 					status={ status }
 					order={ 'desc' }
-				/>
+				/> }
 			</Main>
 		);
 	}
 }
 
-const mapStateToProps = ( state, { siteFragment } ) => ( {
-	siteId: getSiteId( state, siteFragment ),
-} );
+const mapStateToProps = ( state, { siteFragment } ) => {
+	const siteId = getSiteId( state, siteFragment );
+	//update to moderate_comments when available
+	const canModerateComments = canCurrentUser( state, siteId, 'edit_others_posts' );
+	return {
+		siteId,
+		showPermissionError: canModerateComments === false,
+	};
+};
 
 export default connect( mapStateToProps )( localize( CommentsManagement ) );
