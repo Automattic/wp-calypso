2311668715df1bbad2d25b07454e4623b88fb0da
Jarda Snajdr <jsnajdr@gmail.com>
2017-08-11 15:25:54 +0200

    Components: support isValid/isError props for more form field types
    Added support for `isValid` and `isError` props to `FormCurrencyInput`,
    `FormTelInput` and `FormTextarea`.
    
    Moved is-valid/is-error styling to the %form-field placeholder selector and
    removed no longer needed styling from individual components.

--- client/components/forms/form-tel-input/index.jsx
+++ client/components/forms/form-tel-input/index.jsx
@@ -5,9 +5,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export default function FormTelInput( { className, isError, ...props } ) {
+export default function FormTelInput( { className, isError, isValid, ...props } ) {
 	const classes = classNames( 'form-tel-input', className, {
 		'is-error': isError,
+		'is-valid': isValid,
 	} );
 
 	return <input { ...props } type="tel" pattern="[0-9]*" className={ classes } />;
@@ -16,8 +17,10 @@ export default function FormTelInput( { className, isError, ...props } ) {
 FormTelInput.propTypes = {
 	className: PropTypes.string,
 	isError: PropTypes.bool,
+	isValid: PropTypes.bool,
 };
 
 FormTelInput.defaultProps = {
 	isError: false,
+	isValid: false,
 };
