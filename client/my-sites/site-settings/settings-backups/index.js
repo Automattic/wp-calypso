bbbb73148755a14e583e8e367de612fd7fbe5765
Richard Collier <rcollier@cortex.local>
2017-08-11 10:12:56 -0400

    Restrict credentials page and nav item to dev

--- client/my-sites/site-settings/settings-backups/index.js
+++ client/my-sites/site-settings/settings-backups/index.js
@@ -9,14 +9,17 @@ import page from 'page';
 import controller from './controller';
 import settingsController from 'my-sites/site-settings/settings-controller';
 import mySitesController from 'my-sites/controller';
+import config from 'config';
 
 export default function() {
-	page(
-		'/settings/backups/:site_id',
-		mySitesController.siteSelection,
-		mySitesController.navigation,
-		settingsController.setScroll,
-		settingsController.siteSettings,
-		controller.backups
-	);
+	if ( config.isEnabled( 'jetpack/activity-log' ) ) {
+		page(
+			'/settings/backups/:site_id',
+			mySitesController.siteSelection,
+			mySitesController.navigation,
+			settingsController.setScroll,
+			settingsController.siteSettings,
+			controller.backups
+		);
+	}
 }
