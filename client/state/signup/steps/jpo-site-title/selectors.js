3212c5138a7ecc72abcbed524e8fdf576405642d
Elio Rivero <eliorivero@gmail.com>
2017-08-11 16:04:14 -0300

    JPO: fill fields for title and description after user fills them

--- client/state/signup/steps/jpo-site-title/selectors.js
+++ client/state/signup/steps/jpo-site-title/selectors.js
@@ -1,8 +1,22 @@
 /**
  * External dependencies
  */
-import { get } from 'lodash';
+import get from 'lodash/get';
 
+/**
+ * Get stored site title as was last input by user.
+ * @param  {Object} state Global state tree
+ * @return {String} Site title in state tree.
+ */
 export function getJPOSiteTitle( state ) {
-	return get( state, 'signup.steps.jpoSiteTitle', '' );
+	return get( state, [ 'signup', 'dependencyStore', 'jpoSiteTitle', 'siteTitle' ], '' );
+}
+
+/**
+ * Get stored site description as was last input by user.
+ * @param  {Object} state Global state tree
+ * @return {String} Site title in state tree.
+ */
+export function getJPOSiteDescription( state ) {
+	return get( state, [ 'signup', 'dependencyStore', 'jpoSiteTitle', 'siteDescription' ], '' );
 }
