0d8b413866034caa475c20a62931fde0ee8b07ed
Rastislav Lamos <lamos.rasto@gmail.com>
2017-08-11 20:51:46 +0200

    Editor toolbar: change tooltip to "Insert special" on mouse over (v)
    Select tooltip in mouseenter event to prevent showing of "shadow" tooltip
    
    Move tooltip based on what was hovered with mouse
    
    Another try to move tooltip based on what button user hovers
    
    Update code styling by updating calypso-prettier

--- client/components/tinymce/plugins/insert-menu/plugin.jsx
+++ client/components/tinymce/plugins/insert-menu/plugin.jsx
@@ -1,3 +1,4 @@
+/** @format */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import tinymce from 'tinymce/tinymce';
@@ -15,7 +16,7 @@ const initialize = editor => {
 			cmd: item.cmd,
 			onPostRender() {
 				this.innerHtml( renderToString( item.item ) );
-			}
+			},
 		} )
 	);
 
@@ -26,11 +27,48 @@ const initialize = editor => {
 		cmd: menuItems[ 0 ].cmd,
 		menu: menuItems.map( ( { name } ) => editor.menuItems[ name ] ),
 		onPostRender() {
-			ReactDOM.render(
-				<Gridicon icon="add-outline" />,
-				this.$el[ 0 ].children[ 0 ]
-			);
-		}
+			ReactDOM.render( <Gridicon icon="add-outline" />, this.$el[ 0 ].children[ 0 ] );
+
+			// Listen to `mouseenter` events on the (+) part of the Inserter menu to show
+			// the "Insert content" tooltip.
+			const insertContentElm = this.$el[ 0 ].children[ 0 ];
+
+			insertContentElm.addEventListener( 'mouseenter', () => {
+				// We need to select the tooltip during the `mouseenter` event and not outside.
+				// Otherwise, Tinymce renders an empty tooltip somewhere in the editor.
+				const btnTooltip = this.tooltip();
+
+				btnTooltip.text( i18n.translate( 'Insert content' ) );
+
+				btnTooltip.moveBy( -10, 0 );
+			} );
+
+			insertContentElm.addEventListener( 'mouseleave', () => {
+				const btnTooltip = this.tooltip();
+
+				btnTooltip.moveBy( 10, 0 );
+			} );
+
+			// Listen to `mouseenter` events on the (v) part of the Inserter menu to show
+			// the "Insert special" tooltip.
+			const insertSpecialElm = this.$el[ 0 ].children[ 1 ];
+
+			insertSpecialElm.addEventListener( 'mouseenter', () => {
+				// We need to select the tooltip during the `mouseenter` event and not outside.
+				// Otherwise, Tinymce renders an empty tooltip somewhere in the editor.
+				const btnTooltip = this.tooltip();
+
+				btnTooltip.text( i18n.translate( 'Insert special' ) );
+
+				btnTooltip.moveBy( 24, 0 );
+			} );
+
+			insertSpecialElm.addEventListener( 'mouseleave', () => {
+				const btnTooltip = this.tooltip();
+
+				btnTooltip.moveBy( -24, 0 );
+			} );
+		},
 	} );
 };
 
