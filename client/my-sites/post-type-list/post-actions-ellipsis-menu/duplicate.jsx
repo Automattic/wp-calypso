2dd4cae3545f230fe1b7f3b942bd10c177fea8e1
Jonathan Sadowski <sadowski@automattic.com>
2017-08-11 14:52:00 -0500

    Post Actions Menu: add 'Duplicate' option

---
+++ client/my-sites/post-type-list/post-actions-ellipsis-menu/duplicate.jsx
@@ -0,0 +1,72 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import { get } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import PopoverMenuItem from 'components/popover/menu-item';
+import QueryPostTypes from 'components/data/query-post-types';
+import { mc } from 'lib/analytics';
+import { canCurrentUser } from 'state/selectors';
+import { getPost } from 'state/posts/selectors';
+import { getPostType } from 'state/post-types/selectors';
+import { getCurrentUserId, isValidCapability } from 'state/current-user/selectors';
+import { getEditorDuplicatePostPath } from 'state/ui/editor/selectors';
+import config from 'config';
+
+function PostActionsEllipsisMenuDuplicate( { translate, siteId, canEdit, duplicateUrl, isKnownType } ) {
+	if ( ! config.isEnabled( 'posts/post-type-list' ) || ! canEdit ) {
+		return null;
+	}
+
+	function bumpStat() {
+		mc.bumpStat( 'calypso_cpt_actions', 'duplicate' );
+	}
+
+	return (
+		<PopoverMenuItem href={ duplicateUrl } onClick={ bumpStat } icon="pages">
+			{ siteId && ! isKnownType && <QueryPostTypes siteId={ siteId } /> }
+			{ translate( 'Duplicate', { context: 'verb' } ) }
+		</PopoverMenuItem>
+	);
+}
+
+PostActionsEllipsisMenuDuplicate.propTypes = {
+	globalId: PropTypes.string,
+	translate: PropTypes.func.isRequired,
+	siteId: PropTypes.number,
+	canEdit: PropTypes.bool,
+	status: PropTypes.string,
+	duplicateUrl: PropTypes.string,
+	isKnownType: PropTypes.bool
+};
+
+export default connect( ( state, ownProps ) => {
+	const post = getPost( state, ownProps.globalId );
+	if ( ! post ) {
+		return {};
+	}
+
+	const type = getPostType( state, post.site_ID, post.type );
+	const userId = getCurrentUserId( state );
+	const isAuthor = get( post.author, 'ID' ) === userId;
+
+	let capability = isAuthor ? 'edit_posts' : 'edit_others_posts';
+	const typeCapability = get( type, [ 'capabilities', capability ] );
+	if ( isValidCapability( state, post.site_ID, typeCapability ) ) {
+		capability = typeCapability;
+	}
+
+	return {
+		siteId: post.site_ID,
+		canEdit: canCurrentUser( state, post.site_ID, capability ),
+		duplicateUrl: getEditorDuplicatePostPath( state, post.site_ID, post.ID ),
+		isKnownType: !! type
+	};
+} )( localize( PostActionsEllipsisMenuDuplicate ) );
