47812cf7289e163bc0905f80b4fd3ea58fa7f48b
Jon Surrell <jon.surrell@automattic.com>
2017-08-14 18:50:57 +0200

    Pass query to manager via selector
    (cherry picked from commit 0170d4c7130aa18ca07adf2bc30a2f52d3e97aef)

--- client/state/selectors/get-activity-logs.js
+++ client/state/selectors/get-activity-logs.js
@@ -7,25 +7,28 @@ import { get } from 'lodash';
 /**
  * Internal dependencies
  */
+import ActivityQueryManager from 'lib/query-manager/activity';
 import createSelector from 'lib/create-selector';
 
 /**
  * Returns list of Activity Log items.
  *
- * @param {Object} state Global state tree
- * @param {number|string} siteId the site ID
- * @return {?array} Activity log item objects. Null if no data.
+ * @param  {Object}        state  Global state tree
+ * @param  {number|string} siteId the site ID
+ * @param  {?Object}       query  Optional. Query object, passed to ActivityQueryManager.
+ * @return {?array}               Activity log item objects. Null if no data.
  */
 const getActivityLogs = createSelector(
-	( state, siteId ) => {
+	( state, siteId, query ) => {
 		const manager = get( state, [ 'activityLog', 'logItems', siteId ], null );
 		if ( ! manager ) {
 			return null;
 		}
 
-		return manager.getItems();
+		return manager.getItems( query );
 	},
-	( state, siteId ) => get( state, [ 'activityLog', 'logItems', siteId ] )
+	( state, siteId ) => get( state, [ 'activityLog', 'logItems', siteId ] ),
+	( state, siteId, query ) => ActivityQueryManager.QueryKey.stringify( query )
 );
 
 export default getActivityLogs;
