81c8c62217d7cd223bbcffeecb957191bd8f8557
Jon Surrell <jon.surrell@automattic.com>
2017-08-14 16:58:20 +0200

    Use prop-types module (fix lint error)

--- client/my-sites/stats/stats-period-navigation/index.jsx
+++ client/my-sites/stats/stats-period-navigation/index.jsx
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import React, { PropTypes, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { flowRight } from 'lodash';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
