0df6b70bf7e93e2dd011ffa65b49894326b4c6f9
Donna Peplinskie <donnapep@gmail.com>
2017-08-14 15:40:06 -0400

    Eliminate return statement from ProgressIndicator

--- client/components/wizard/progress-indicator.jsx
+++ client/components/wizard/progress-indicator.jsx
@@ -5,20 +5,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { localize } from 'i18n-calypso';
 
-const ProgressIndicator = ( { stepNumber, totalSteps, translate } ) => {
-	return (
-		<div className="wizard__progress-indicator">
-			{
-				translate( 'Step %(stepNumber)d of %(stepTotal)d', {
-					args: {
-						stepNumber: stepNumber + 1,
-						stepTotal: totalSteps,
-					}
-				} )
-			}
-		</div>
-	);
-};
+const ProgressIndicator = ( { stepNumber, totalSteps, translate } ) => (
+	<div className="wizard__progress-indicator">
+		{
+			translate( 'Step %(stepNumber)d of %(stepTotal)d', {
+				args: {
+					stepNumber: stepNumber + 1,
+					stepTotal: totalSteps,
+				}
+			} )
+		}
+	</div>
+);
 
 ProgressIndicator.propTypes = {
 	stepNumber: PropTypes.number.isRequired,
