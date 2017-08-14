9dfd73ebe03f267363b9a57e9d129f551367556c
Donna Peplinskie <donnapep@gmail.com>
2017-08-14 08:16:48 -0400

    Update from React.PropTypes to prop-types

--- client/components/wizard/index.jsx
+++ client/components/wizard/index.jsx
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { get, indexOf } from 'lodash';
 import page from 'page';
 
