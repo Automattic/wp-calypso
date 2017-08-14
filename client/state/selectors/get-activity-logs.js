179e586a04489654f5416ee8cd98ce31749dd769
Jon Surrell <jon.surrell@automattic.com>
2017-08-14 18:50:57 +0200

    Pass query to manager via selector

--- client/state/selectors/get-activity-logs.js
+++ client/state/selectors/get-activity-logs.js
@@ -12,18 +12,19 @@ import createSelector from 'lib/create-selector';
 /**
  * Returns list of Activity Log items.
  *
- * @param {Object} state Global state tree
- * @param {number|string} siteId the site ID
- * @return {?array} Activity log item objects. Null if no data.
+ * @param  {Object}        state  Global state tree
+ * @param  {number|string} siteId the site ID
+ * @param  {?Object}       query  Optional. Query object, passed to ActivityQueryManager.
+ * @return {?array}               Activity log item objects. Null if no data.
  */
 const getActivityLogs = createSelector(
-	( state, siteId ) => {
+	( state, siteId, query ) => {
 		const manager = get( state, [ 'activityLog', 'logItems', siteId ], null );
 		if ( ! manager ) {
 			return null;
 		}
 
-		return manager.getItems();
+		return manager.getItems( query );
 	},
 	( state, siteId ) => get( state, [ 'activityLog', 'logItems', siteId ] )
 );
