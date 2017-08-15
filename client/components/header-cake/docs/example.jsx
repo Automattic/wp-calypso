b48de4f5cf1a65d964de0f5ab6bc4ed96bccbec1
Jon Surrell <jon.surrell@automattic.com>
2017-08-15 23:48:34 +0200

    Apply codemod commonjs-exports and lint fix

--- client/components/header-cake/docs/example.jsx
+++ client/components/header-cake/docs/example.jsx
@@ -14,7 +14,7 @@ import HeaderCake from 'components/header-cake';
  */
 const noop = function() {};
 
-module.exports = React.createClass( {
+export default React.createClass( {
 	displayName: 'Headers',
 
 	mixins: [ PureRenderMixin ],
