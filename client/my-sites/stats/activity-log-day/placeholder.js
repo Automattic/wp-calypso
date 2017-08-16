375bc163f1995eb0f1641fade33ba642f5a3f6bf
Jon Surrell <jon.surrell@automattic.com>
2017-08-16 16:41:15 +0200

    Improve similarity to content

--- client/my-sites/stats/activity-log-day/placeholder.js
+++ client/my-sites/stats/activity-log-day/placeholder.js
@@ -3,29 +3,24 @@
  * External dependencies
  */
 import React from 'react';
-import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import FoldableCard from 'components/foldable-card';
 
-function ActivityLogDayPlaceholder( { translate } ) {
-	return (
-		<FoldableCard
-			className="activity-log-day__placeholder"
-			header={
-				<div>
-					<div className="activity-log-day__day">
-						{ translate( 'Loading…' ) }
-					</div>
-					<div className="activity-log-day__events">
-						{ translate( 'Loading…' ) }
-					</div>
-				</div>
-			}
-		/>
-	);
-}
+const component = (
+	<FoldableCard
+		className="activity-log-day__placeholder"
+		header={
+			<div>
+				<div className="activity-log-day__day" />
+				<div className="activity-log-day__events" />
+			</div>
+		}
+	/>
+);
 
-export default localize( ActivityLogDayPlaceholder );
+export default function ActivityLogDayPlaceholder() {
+	return component;
+}
