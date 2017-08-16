105e633b0680d1e7c30db4bea35b3a02ef800416
Leif Singer <git@singer.sh>
2017-08-16 18:10:09 +0200

    tweak indentation

--- client/layout/guided-tours/tours/editor-basics-tour.js
+++ client/layout/guided-tours/tours/editor-basics-tour.js
@@ -96,8 +96,10 @@ export const EditorBasicsTour = makeTour(
 		>
 			<p>
 				{
-					translate( 'The {{icon/}} lets you add other things, like a contact form. ' +
-						'If your site is on the Premium or Business plan, you can even add {{strong}}payment buttons{{/strong}}!', {
+					translate(
+						'The {{icon/}} lets you add other things, like a contact form. ' +
+						'If your site is on the Premium or Business plan, you can even add {{strong}}payment buttons{{/strong}}!',
+						{
 							components: {
 								strong: <strong />,
 								icon: <Gridicon icon="chevron-down" />,
