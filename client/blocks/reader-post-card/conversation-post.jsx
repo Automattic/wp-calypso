c4d84d49917201dea0aa36f12e8a48cd9463d92f
Ben Lowery <blowery@users.noreply.github.com>
2017-08-16 15:12:44 -0400

    Reader: Better updates for Conversations (#16849)
    Makes accepting updates to conversations stream work. Also fixed an old bug in how we dedupe new posts that come in via updates.

--- client/blocks/reader-post-card/conversation-post.jsx
+++ client/blocks/reader-post-card/conversation-post.jsx
@@ -4,46 +4,27 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { map, takeRight, filter } from 'lodash';
 
 /**
  * Internal Dependencies
  */
 import ConversationPostList from 'blocks/conversations/list';
 import CompactPostCard from 'blocks/reader-post-card/compact';
-import { getDateSortedPostComments } from 'state/comments/selectors';
 
 class ConversationPost extends React.Component {
 	static propTypes = {
 		post: PropTypes.object.isRequired,
-		comments: PropTypes.array.isRequired,
+		commentIds: PropTypes.array.isRequired,
 	};
 
 	render() {
-		const commentIdsToShow = map(
-			takeRight(
-				filter(
-					this.props.comments,
-					comment => comment.type !== 'trackback' && comment.type !== 'pingback'
-				),
-				3
-			),
-			'ID'
-		);
-
 		return (
 			<div className="reader-post-card__conversation-post">
 				<CompactPostCard { ...this.props } />
-				<ConversationPostList post={ this.props.post } commentIds={ commentIdsToShow } />
+				<ConversationPostList post={ this.props.post } commentIds={ this.props.commentIds } />
 			</div>
 		);
 	}
 }
 
-export default connect( ( state, ownProps ) => {
-	const { site_ID: siteId, ID: postId } = ownProps.post;
-	return {
-		comments: getDateSortedPostComments( state, siteId, postId ),
-	};
-} )( ConversationPost );
+export default ConversationPost;
