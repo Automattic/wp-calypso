59346a9a9f97f53d7384b47856ffd1e50ac7dff3
Copons <copons@users.noreply.github.com>
2017-08-16 17:46:53 +0100

    Don't dispatch `originalCommentData` on error

--- client/state/data-layer/wpcom/sites/comments/index.js
+++ client/state/data-layer/wpcom/sites/comments/index.js
@@ -229,7 +229,7 @@ const editComment = ( { dispatch, getState }, action ) => {
 const announceEditFailure = ( { dispatch, getState }, action ) => {
 	dispatch(
 		local( {
-			...action,
+			...omit( action, [ 'originalCommentData' ] ),
 			commentData: action.originalCommentData,
 		} )
 	);
