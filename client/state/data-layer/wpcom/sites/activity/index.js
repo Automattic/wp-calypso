5640c5a6d3a225eee7686459b3c6d1e2fc85564e
Jon Surrell <jon.surrell@automattic.com>
2017-08-16 14:49:58 +0200

    Use declarative mapping for Calypso -> API params

--- client/state/data-layer/wpcom/sites/activity/index.js
+++ client/state/data-layer/wpcom/sites/activity/index.js
@@ -2,7 +2,7 @@
 /**
  * External dependencies
  */
-import { get, pick } from 'lodash';
+import { get, includes, reduce } from 'lodash';
 
 /**
  * Internal dependencies
@@ -12,22 +12,34 @@ import { http } from 'state/data-layer/wpcom-http/actions';
 import { ACTIVITY_LOG_REQUEST } from 'state/action-types';
 import { activityLogError, activityLogUpdate } from 'state/activity-log/actions';
 
-const KNOWN_PARAMS = [ 'action', 'date_end', 'date_start', 'group', 'name', 'number' ];
+/**
+ * Module constants
+ */
+const CALYPSO_TO_API_PARAMS = {
+	dateEnd: 'date_end',
+	dateStart: 'date_start',
+};
+const KNOWN_API_PARAMS = [ 'action', 'date_end', 'date_start', 'group', 'name', 'number' ];
 
 export const handleActivityLogRequest = ( { dispatch }, action ) => {
 	const { siteId } = action;
 
-	const query = pick( action.params, KNOWN_PARAMS );
+	const query = reduce(
+		action.params,
+		( acc, value, param ) => {
+			const paramToStore = get( CALYPSO_TO_API_PARAMS, param, param );
 
-	const dateEnd = get( action.params, 'dateEnd' );
-	if ( dateEnd ) {
-		query.date_end = dateEnd;
-	}
+			if ( includes( KNOWN_API_PARAMS, paramToStore ) ) {
+				return {
+					...acc,
+					[ paramToStore ]: value,
+				};
+			}
 
-	const dateStart = get( action.params, 'dateStart' );
-	if ( dateStart ) {
-		query.date_start = dateStart;
-	}
+			return acc;
+		},
+		{}
+	);
 
 	dispatch(
 		http(
