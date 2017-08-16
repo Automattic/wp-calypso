db2bcc36ed3dbd3641d371d4d42138e78c980c6f
Copons <copons@users.noreply.github.com>
2017-08-16 16:53:58 +0100

    Fix PropTypes import deprecation

--- client/components/data/query-site-comments-list/index.jsx
+++ client/components/data/query-site-comments-list/index.jsx
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import { PureComponent, PropTypes } from 'react';
+import { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 /**
