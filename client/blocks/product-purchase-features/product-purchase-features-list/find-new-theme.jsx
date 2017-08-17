cb988cac93a22942ee70afb2b648e3d9e8f0f623
Marin Atanasov <tyxla@abv.bg>
2017-08-17 11:11:43 +0300

    Plans: Update theme upload feature copy in My Plan

--- client/blocks/product-purchase-features/product-purchase-features-list/find-new-theme.jsx
+++ client/blocks/product-purchase-features/product-purchase-features-list/find-new-theme.jsx
@@ -14,9 +14,11 @@ export default localize( ( { selectedSite, translate } ) => {
 		<div className="product-purchase-features-list__item">
 			<PurchaseDetail
 				icon="customize"
-				title={ translate( 'Find a new theme' ) }
-				description={ translate( 'All our premium themes are now available at no extra cost. Try them out now.' ) }
-				buttonText={ translate( 'Browse premium themes' ) }
+				title={ translate( 'Try a New Theme' ) }
+				description={ translate(
+					'You\'ve now got access to every premium theme, at no extra cost - that\'s hundreds of new options.'
+				) }
+				buttonText={ translate( 'Give one a try!' ) }
 				href={ '/themes/' + selectedSite.slug }
 			/>
 		</div>
