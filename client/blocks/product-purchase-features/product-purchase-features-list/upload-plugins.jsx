03f9e27aefc50dc29859d4ce1a0a43bce116fb40
Marin Atanasov <tyxla@abv.bg>
2017-08-17 11:09:09 +0300

    Update copy

--- client/blocks/product-purchase-features/product-purchase-features-list/upload-plugins.jsx
+++ client/blocks/product-purchase-features/product-purchase-features-list/upload-plugins.jsx
@@ -14,9 +14,10 @@ export default localize( ( { selectedSite, translate } ) => {
 		<div className="product-purchase-features-list__item">
 			<PurchaseDetail
 				icon="plugins"
-				title={ translate( 'Upload a Plugin' ) }
+				title={ translate( 'Add a Plugin' ) }
 				description={ translate(
-					'You can now upload your own plugins, or ones you\'ve downloaded, directly through a drag and drop interface.'
+					'Search and add plugins right from your dashboard, or upload a plugin ' +
+					'from your computer with a drag-and-drop interface.'
 				) }
 				buttonText={ translate( 'Upload a plugin now' ) }
 				href={ '/plugins/upload/' + selectedSite.slug }
