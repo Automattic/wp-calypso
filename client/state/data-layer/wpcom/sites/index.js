33099c2ff672b08b5184188dea6e8cce195b5865
Kerry Liu <kerryliu@gmail.com>
2017-08-18 15:04:20 -0700

    Comments: move comment-tree

--- client/state/data-layer/wpcom/sites/index.js
+++ client/state/data-layer/wpcom/sites/index.js
@@ -6,6 +6,7 @@ import activity from './activity';
 import automatedTransfer from './automated-transfer';
 import blogStickers from './blog-stickers';
 import comments from './comments';
+import commentsTree from './comments-tree';
 import media from './media';
 import plugins from './plugins';
 import posts from './posts';
@@ -16,6 +17,7 @@ export default mergeHandlers(
 	automatedTransfer,
 	blogStickers,
 	comments,
+	commentsTree,
 	media,
 	plugins,
 	posts,
