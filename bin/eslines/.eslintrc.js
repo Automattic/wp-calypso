641b68d3bd46182df416544be87a5b2765a2c5b7
Andrés Maneiro <andres.maneiro@automattic.com>
2017-08-18 18:50:59 +0200

    Remove unnecesary dep

--- bin/eslines/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  "extends": "wpcalypso",
-  "env": {
-    "node": true
-  }
-};
--- bin/eslines/src/processors/enforce.js
+++
@@ -1,9 +0,0 @@
-const fs = require( 'fs' );
-const enforce = require( '../lib/enforce' );
-
-const config = JSON.parse( fs.readFileSync( '.eslines.json', 'utf-8' ) );
-
-module.exports = function( report ) {
-	const rules = config.processors.enforce.rules || [ ];
-	return JSON.stringify( enforce( report, rules ) );
-};
