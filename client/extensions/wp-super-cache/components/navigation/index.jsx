667a465c8122236079aa4b8acbde9174efe71a37
Bernhard Reiter <ockham@raz.or.at>
2017-08-18 11:08:55 +0200

    WPSC: Simplify Tabs Routing

--- client/extensions/wp-super-cache/components/navigation/index.jsx
+++ client/extensions/wp-super-cache/components/navigation/index.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
+import { map } from 'lodash';
 
 /**
  * Internal dependencies
@@ -17,70 +18,26 @@ import { addSiteFragment } from 'lib/route/path';
 import { getSiteSlug } from 'state/sites/selectors';
 import { Tabs } from '../../app/constants';
 
-const Navigation = ( { activeTab, siteSlug, translate } ) => {
-	const getLabel = tab => {
-		switch ( tab ) {
-			case Tabs.EASY:
-				return translate( 'Easy' );
-			case Tabs.ADVANCED:
-				return translate( 'Advanced' );
-			case Tabs.CDN:
-				return translate( 'CDN' );
-			case Tabs.CONTENTS:
-				return translate( 'Contents' );
-			case Tabs.PRELOAD:
-				return translate( 'Preload' );
-			case Tabs.DEBUG:
-				return translate( 'Debug' );
-		}
-	};
-
-	const getTabs = () => {
-		const tabs = [];
-
-		for ( const key in Tabs ) {
-			if ( Tabs.hasOwnProperty( key ) ) {
-				tabs.push( Tabs[ key ] );
-			}
-		}
-
-		return tabs;
-	};
-
-	const renderTabItems = ( tabs ) => {
-		return tabs.map( tab => {
-			let path = '/extensions/wp-super-cache';
-
-			if ( tab !== Tabs.EASY ) {
-				path = `${ path }/${ tab }`;
-			}
-
-			return (
-				<SectionNavTabItem
-					key={ `wp-super-cache-${ tab }` }
-					path={ siteSlug && addSiteFragment( path, siteSlug ) }
-					selected={ ( activeTab || Tabs.EASY ) === tab }>
-					{ getLabel( tab ) }
-				</SectionNavTabItem>
-			);
-		} );
-	};
-
-	const pluginPath = '/plugins/wp-super-cache';
-	return (
-		<div>
-			<HeaderCake backText={ translate( 'Plugin Overview' ) }
-				backHref={ siteSlug && addSiteFragment( pluginPath, siteSlug ) }>
-				WP Super Cache
-			</HeaderCake>
-			<SectionNav selectedText="Settings">
-				<SectionNavTabs>
-					{ renderTabItems( getTabs() ) }
-				</SectionNavTabs>
-			</SectionNav>
-		</div>
-	);
-};
+const Navigation = ( { activeTab, siteSlug, translate } ) => (
+	<div>
+		<HeaderCake backText={ translate( 'Plugin Overview' ) }
+			backHref={ siteSlug && addSiteFragment( '/plugins/wp-super-cache', siteSlug ) }>
+			WP Super Cache
+		</HeaderCake>
+		<SectionNav selectedText="Settings">
+			<SectionNavTabs>
+				{ map( Tabs, ( { label, slug: tabSlug } ) => (
+					<SectionNavTabItem
+						key={ `wp-super-cache-${ tabSlug || 'easy' }` }
+						path={ siteSlug && addSiteFragment( '/extensions/wp-super-cache/' + tabSlug, siteSlug ) }
+						selected={ activeTab === tabSlug }>
+						{ label }
+					</SectionNavTabItem>
+				) ) }
+			</SectionNavTabs>
+		</SectionNav>
+	</div>
+);
 
 Navigation.propTypes = {
 	activeTab: PropTypes.string,
