9ad55d73cd7947f39c06e29435267fc9fd7e735e
Ben Lowery <ben.lowery@automattic.com>
2017-08-18 15:06:17 -0400

    Reader: Fix devdocs/blocks reader-post-card usage
    Currently breaking all of blocks.

--- client/blocks/reader-post-card/docs/example.jsx
+++ client/blocks/reader-post-card/docs/example.jsx
@@ -15,7 +15,11 @@ const ReaderPostCard = () =>
 			{ posts.map( item =>
 				<ReaderPostCardBlock key={ item.global_ID } post={ item } site={ site } />,
 			) }
-			<ReaderPostCardBlock compact key={ posts[ 0 ].global_ID } post={ posts[ 0 ] } site={ site } />
+			<ReaderPostCardBlock compact key={ posts[ 0 ].global_ID } post={ posts[ 0 ] } site={ site } postKey={ {
+				feedId: posts[0].feed_ID,
+				postId: posts[0].feed_item_ID,
+				comments: []
+			} } />
 		</div>
 	</div>;
 
