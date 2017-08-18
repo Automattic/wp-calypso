80fc6371b84567476bac4f225a95b714d50b5b99
Michael P. Pfeiffer <michael@cssconf.eu>
2017-08-18 15:29:14 +0200

    Move translate into component and address review suggestions

--- client/blocks/color-scheme-picker/constants.js
+++ client/blocks/color-scheme-picker/constants.js
@@ -1,29 +1,27 @@
 /** @format */
-/**
- * Internal dependencies
- */
-import { translate } from 'i18n-calypso';
 
-export const ColorSchemes = [
-	{
-		label: translate( 'Default' ),
-		value: 'default',
-		thumbnail: {
-			cssClass: 'is-default-theme',
+export default function( translate ) {
+	return [
+		{
+			label: translate( 'Default' ),
+			value: 'default',
+			thumbnail: {
+				cssClass: 'is-default-theme',
+			},
 		},
-	},
-	{
-		label: translate( 'Light' ),
-		value: 'light',
-		thumbnail: {
-			cssClass: 'is-light-theme',
+		{
+			label: translate( 'Light' ),
+			value: 'light',
+			thumbnail: {
+				cssClass: 'is-light-theme',
+			},
 		},
-	},
-	{
-		label: translate( 'Dark' ),
-		value: 'dark',
-		thumbnail: {
-			cssClass: 'is-dark-theme',
+		{
+			label: translate( 'Dark' ),
+			value: 'dark',
+			thumbnail: {
+				cssClass: 'is-dark-theme',
+			},
 		},
-	},
-];
+	];
+}
