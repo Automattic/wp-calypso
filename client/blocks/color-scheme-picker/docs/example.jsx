37a3b50e7068f6695cbd695e822b2f6a55c41594
Michael P. Pfeiffer <michael@cssconf.eu>
2017-08-21 20:00:09 +0200

    Remove Notice from Docs Example

--- client/blocks/color-scheme-picker/docs/example.jsx
+++ client/blocks/color-scheme-picker/docs/example.jsx
@@ -8,7 +8,6 @@ import React, { PureComponent } from 'react';
  * Internal dependencies
  */
 import Card from 'components/card';
-import Notice from 'components/notice';
 import ColorSchemePicker from 'blocks/color-scheme-picker';
 
 class ColorSchemePickerExample extends PureComponent {
@@ -26,12 +25,6 @@ class ColorSchemePickerExample extends PureComponent {
 		return (
 			<Card>
 				<ColorSchemePicker temporarySelection onSelection={ this.handleColorSchemeSelection } />
-
-				{ this.state.selectedColorScheme &&
-					<Notice
-						text={ `Selected color scheme: ${ this.state.selectedColorScheme }` }
-						showDismiss={ false }
-					/> }
 			</Card>
 		);
 	}
