9b1410927fcdf2c7380d8e63917e5d0dfca280be
Bernhard Reiter <ockham@raz.or.at>
2017-08-21 23:06:29 +0200

    WPSC: Generate Stats on Load
    Create a new query component, `<QueryStats />`, and use that to generate and fetch stats upon loading the 'Contents' tab.

---
+++ client/extensions/wp-super-cache/components/data/query-stats/index.jsx
@@ -0,0 +1,55 @@
+/**
+ * External dependencies
+ */
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { isGeneratingStats } from '../../../state/stats/selectors';
+import { generateStats } from '../../../state/stats/actions';
+
+class QueryStats extends Component {
+	componentWillMount() {
+		this.generateStats( this.props );
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		const { siteId } = this.props;
+
+		if ( ! nextProps.siteId || siteId === nextProps.siteId ) {
+			return;
+		}
+
+		this.generateStats( nextProps );
+	}
+
+	generateStats( props ) {
+		const { generatingStats, siteId } = props;
+
+		if ( ! generatingStats && siteId ) {
+			props.generateStats( siteId );
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
+
+QueryStats.propTypes = {
+	siteId: PropTypes.number,
+	generatingStats: PropTypes.bool,
+	generateStats: PropTypes.func,
+};
+
+export default connect(
+	( state, { siteId } ) => {
+		return {
+			generatingStats: isGeneratingStats( state, siteId ),
+		};
+	},
+	{ generateStats }
+)( QueryStats );
