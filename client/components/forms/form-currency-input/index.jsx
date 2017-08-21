d25f52d8486de4bb19742ae8102c0dc4b28d2470
Jarda Snajdr <jsnajdr@gmail.com>
2017-08-21 13:05:10 +0200

    FormCurrencyInput: make the currency value editable
    If the component has a `currencyList` prop (array of possible values), then make
    the currency affix editable. The `onCurrencyChange` prop is a handler that gets
    called when the currency is changed.
    
    Implemented by overlaying a transparent, absolutely positioned `<select>` over the
    affix. The same technique is already used to select the country code in the `PhoneInput`
    component.

--- client/components/forms/form-currency-input/index.jsx
+++ client/components/forms/form-currency-input/index.jsx
@@ -1,7 +1,9 @@
+/** @format */
 /**
  * External dependencies
  */
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 /**
@@ -9,22 +11,67 @@ import classNames from 'classnames';
  */
 import FormTextInputWithAffixes from 'components/forms/form-text-input-with-affixes';
 
-export default function FormCurrencyInput( {
+function renderAffix( currencyValue, onCurrencyChange, currencyList ) {
+	// If the currency value is not defined, don't render this affix at all
+	if ( ! currencyValue ) {
+		return null;
+	}
+
+	// If the currency is not editable, i.e., when `currencyList` is not defined, then just
+	// render the plain value.
+	if ( ! currencyList ) {
+		return currencyValue;
+	}
+
+	// For an editable currency, display a <select> overlay
+	return (
+		<span className="form-currency-input__affix">
+			{ currencyValue }
+			<select
+				className="form-currency-input__select"
+				value={ currencyValue }
+				onChange={ onCurrencyChange }
+			>
+				{ currencyList.map( code =>
+					<option key={ code } value={ code }>
+						{ code }
+					</option>
+				) }
+			</select>
+		</span>
+	);
+}
+
+function FormCurrencyInput( {
 	className,
 	currencySymbolPrefix,
 	currencySymbolSuffix,
+	onCurrencyChange,
+	currencyList,
+	placeholder = '0.00',
 	...props
 } ) {
 	const classes = classNames( 'form-currency-input', className );
+	const prefix = renderAffix( currencySymbolPrefix, onCurrencyChange, currencyList );
+	const suffix = renderAffix( currencySymbolSuffix, onCurrencyChange, currencyList );
 
 	return (
 		<FormTextInputWithAffixes
 			{ ...props }
 			type="number"
 			className={ classes }
-			prefix={ currencySymbolPrefix }
-			suffix={ currencySymbolSuffix }
-			placeholder="0.00"
+			prefix={ prefix }
+			suffix={ suffix }
+			placeholder={ placeholder }
 		/>
 	);
 }
+
+FormCurrencyInput.propTypes = {
+	currencySymbolPrefix: PropTypes.string,
+	currencySymbolSuffix: PropTypes.string,
+	onCurrencyChange: PropTypes.func,
+	currencyList: PropTypes.array,
+};
+
+export default FormCurrencyInput;
