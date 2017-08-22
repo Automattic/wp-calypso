cf390b1ba5f882e091dd615332d381931688dddf
Miklos Juhasz <juhasz.miklos.1@gmail.com>
2017-08-22 22:20:29 +0200

    Add Site Name to Post Item in the new post list (#17405)
    * Fix ESLint warning.
    
    * Use selector to get site title.
    
    * Add SiteIcon to the post item.
    
    * Only show site name and title in All Sites mode.
    
    * Add styling for site section on post item.
    
    * Use isEnabled from config.

--- client/blocks/post-item/index.jsx
+++ client/blocks/post-item/index.jsx
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
@@ -9,8 +10,12 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import { isEnabled } from 'config';
 import { getEditorPath } from 'state/ui/editor/selectors';
+import { getSelectedSiteId } from 'state/ui/selectors';
 import { getNormalizedPost } from 'state/posts/selectors';
+import { getSite, getSiteTitle } from 'state/sites/selectors';
+import SiteIcon from 'blocks/site-icon';
 import Card from 'components/card';
 import PostRelativeTime from 'blocks/post-relative-time';
 import PostStatus from 'blocks/post-status';
@@ -18,18 +23,29 @@ import PostTypeListPostThumbnail from 'my-sites/post-type-list/post-thumbnail';
 import PostActionsEllipsisMenu from 'my-sites/post-type-list/post-actions-ellipsis-menu';
 import PostTypePostAuthor from 'my-sites/post-type-list/post-type-post-author';
 
-function PostItem( { translate, globalId, post, editUrl, className, compact } ) {
+function PostItem( { translate, globalId, post, site, editUrl, siteTitle, isAllSitesModeSelected, className, compact } ) {
 	const title = post ? post.title : null;
-	const classes = classnames( 'post-item', className, {
+	const postItemClasses = classnames( 'post-item', className, {
 		'is-untitled': ! title,
 		'is-mini': compact,
 		'is-placeholder': ! globalId
 	} );
 
+	const isSiteVisible = isEnabled( 'posts/post-type-list' ) && isAllSitesModeSelected;
+	const titleMetaClasses = classnames( 'post-item__title-meta', { 'site-is-visible': isSiteVisible } );
+
 	return (
-		<Card compact className={ classes }>
+		<Card compact className={ postItemClasses }>
 			<div className="post-item__detail">
-				<div className="post-item__title-meta">
+				<div className={ titleMetaClasses }>
+					{ isSiteVisible &&
+						<div className="post-item__site">
+							<SiteIcon size={ 16 } site={ site } />
+							<div className="post-item__site-title">
+								{ siteTitle }
+							</div>
+						</div>
+					}
 					<h1 className="post-item__title">
 						<a href={ editUrl } className="post-item__title-link">
 							{ title || translate( 'Untitled' ) }
@@ -52,6 +68,9 @@ PostItem.propTypes = {
 	translate: PropTypes.func,
 	globalId: PropTypes.string,
 	post: PropTypes.object,
+	site: PropTypes.object,
+	siteTitle: PropTypes.string,
+	isAllSitesModeSelected: PropTypes.bool,
 	className: PropTypes.string,
 	compact: PropTypes.bool
 };
@@ -62,8 +81,13 @@ export default connect( ( state, ownProps ) => {
 		return {};
 	}
 
+	const siteId = post.site_ID;
+
 	return {
 		post,
-		editUrl: getEditorPath( state, post.site_ID, post.ID )
+		site: getSite( state, siteId ),
+		siteTitle: getSiteTitle( state, siteId ),
+		isAllSitesModeSelected: getSelectedSiteId( state ) === null,
+		editUrl: getEditorPath( state, siteId, post.ID ),
 	};
 } )( localize( PostItem ) );
