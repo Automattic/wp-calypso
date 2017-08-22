e74639c6b958e4acb8b77b3a6e269ce6463039f9
Kelly Dwan <ryelle@users.noreply.github.com>
2017-08-22 16:17:18 -0400

    Add indication that the note is saving

--- client/extensions/woocommerce/app/order/order-notes/new-note.js
+++ client/extensions/woocommerce/app/order/order-notes/new-note.js
@@ -10,12 +10,13 @@ import PropTypes from 'prop-types';
 /**
  * Internal dependencies
  */
-import { createNote } from 'woocommerce/state/sites/orders/notes/actions';
 import Button from 'components/button';
+import { createNote } from 'woocommerce/state/sites/orders/notes/actions';
 import FormFieldSet from 'components/forms/form-fieldset';
 import FormLabel from 'components/forms/form-label';
 import FormTextarea from 'components/forms/form-textarea';
 import FormSelect from 'components/forms/form-select';
+import { isOrderNoteSaving } from 'woocommerce/state/sites/orders/notes/selectors';
 
 class CreateOrderNote extends Component {
 	static propTypes = {
@@ -42,6 +43,9 @@ class CreateOrderNote extends Component {
 
 	saveNote = () => {
 		const { orderId, siteId } = this.props;
+		if ( ! this.state.note ) {
+			return;
+		}
 		const note = {
 			note: this.state.note,
 			customer_note: 'email' === this.state.type,
@@ -51,7 +55,7 @@ class CreateOrderNote extends Component {
 	}
 
 	render() {
-		const { translate } = this.props;
+		const { isNoteSaving, translate } = this.props;
 
 		return (
 			<div className="order-notes__new-note">
@@ -68,7 +72,13 @@ class CreateOrderNote extends Component {
 						<option value={ 'internal' }>{ translate( 'Private Note' ) }</option>
 						<option value={ 'email' }>{ translate( 'Send to Customer' ) }</option>
 					</FormSelect>
-					<Button primary onClick={ this.saveNote }>{ translate( 'Add Note' ) }</Button>
+					<Button
+						primary
+						onClick={ this.saveNote }
+						busy={ isNoteSaving }
+						disabled={ isNoteSaving }>
+						{ translate( 'Add Note' ) }
+					</Button>
 				</div>
 			</div>
 		);
@@ -76,6 +86,8 @@ class CreateOrderNote extends Component {
 }
 
 export default connect(
-	undefined,
+	( state, props ) => ( {
+		isNoteSaving: isOrderNoteSaving( state, props.orderId )
+	} ),
 	dispatch => bindActionCreators( { createNote }, dispatch )
 )( localize( CreateOrderNote ) );
