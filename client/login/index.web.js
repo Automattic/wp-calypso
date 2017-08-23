50fd2159cd86d164d949dc91e22202afbd248f4a
Ben Dwyer <ben@scruffian.com>
2017-08-23 11:29:38 +0100

    Simplify the redirect routing for /log-in

--- client/login/index.web.js
+++ client/login/index.web.js
@@ -1,10 +1,4 @@
 /**
- * External dependencies
- */
-
-import page from 'page';
-
-/**
  * Internal dependencies
  */
 import config from 'config';
@@ -35,11 +29,7 @@ export default router => {
 	}
 
 	if ( config.isEnabled( 'login/wp-login' ) ) {
-		router(
-			'/log-in/en', () => {
-				page.redirect( '/log-in' );
-			}
-		);
+		router( '/log-in/en', '/log-in' );
 		router(
 			[
 				'/log-in/:twoFactorAuthType(authenticator|backup|sms|push)/:lang?',
