008ec666e1dcb05caa733eec97d175a8d3fb4bf9
Don Park <donpark@docuverse.com>
2017-08-23 17:01:39 -0700

    cleanup for readability

--- client/lib/analytics/index.js
+++ client/lib/analytics/index.js
@@ -204,11 +204,12 @@ const analytics = {
 			}
 
 			if ( _superProps ) {
+				_dispatch && _dispatch( { type: ANALYTICS_SUPER_PROPS_UPDATE } );
 				// HACK: _selectedSite should only be used if path contains `:site`.
 				// This hack is enabled only for `calypso_page_view` event but may need to be expanded
 				// if other events require same special handling.
-				const useSelectedSite = eventName !== 'calypso_page_view' || eventProperties.path.indexOf( ':site' ) !== -1;
-				_dispatch && _dispatch( { type: ANALYTICS_SUPER_PROPS_UPDATE } );
+				const hasPathWithSite = eventProperties.path && eventProperties.path.indexOf( ':site' ) !== -1;
+				const useSelectedSite = hasPathWithSite || eventName !== 'calypso_page_view';
 				superProperties = _superProps.getAll( useSelectedSite ? _selectedSite : null, _siteCount );
 				eventProperties = assign( {}, eventProperties, superProperties ); // assign to a new object so we don't modify the argument
 			}
