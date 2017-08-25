67a7266effcb09030d49096fa4350aaa9885552f
Don Park <donpark@docuverse.com>
2017-08-25 09:15:02 -0700

    add delay to avoid stale `_dl`

--- client/lib/analytics/index.js
+++ client/lib/analytics/index.js
@@ -161,10 +161,13 @@ const analytics = {
 	// pageView is a wrapper for pageview events across Tracks and GA
 	pageView: {
 		record: function( urlPath, pageTitle ) {
-			mostRecentUrlPath = urlPath;
-			analytics.tracks.recordPageView( urlPath );
-			analytics.ga.recordPageView( urlPath, pageTitle );
-			analytics.emit( 'page-view', urlPath, pageTitle );
+			// add delay to avoid stale `_dl` (browserdocumentlocation) in recorded calypso_page_view event details
+			setTimeout( () => {
+				mostRecentUrlPath = urlPath;
+				analytics.tracks.recordPageView( urlPath );
+				analytics.ga.recordPageView( urlPath, pageTitle );
+				analytics.emit( 'page-view', urlPath, pageTitle );
+			}, 0 );
 		}
 	},
 
