d190840144863b9479bb65dda4c9b65123e93ec2
Mark Ryall <mark@ryall.name>
2017-08-25 16:36:06 +1000

    include designType in api request when creating new site.

--- client/signup/config/steps.js
+++ client/signup/config/steps.js
@@ -122,7 +122,7 @@ export default {
 		props: {
 			isDomainOnly: false
 		},
-		dependencies: [ 'themeSlugWithRepo' ],
+		dependencies: [ 'designType', 'themeSlugWithRepo' ],
 		delayApiRequestUntilComplete: true
 	},
 
