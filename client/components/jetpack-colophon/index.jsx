79b9ae9b31450fc33879d73a5598b3e4eabf71df
oskosk <oskosk@gmail.com>
2017-08-28 17:38:32 -0300

    refactor the jetpack-colophon into a stateless component

--- client/components/jetpack-colophon/index.jsx
+++ client/components/jetpack-colophon/index.jsx
@@ -2,28 +2,19 @@
  * External dependencies
  */
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 /**
  * Internal dependencies
  */
-import JetpackLogo from 'components/jetpack-logo'
+import JetpackLogo from 'components/jetpack-logo';
 
-export default React.createClass( {
-	displayName: 'JetpackColophon',
+const JetpackColophon = ( { className } ) => {
+	return (
+		<div className={ classNames( 'jetpack-colophon', className ) }>
+			<span className="jetpack-colophon__power">Powered by </span><JetpackLogo size={ 24 } full />
+		</div>
+	);
+};
 
-	propTypes: {
-		className: PropTypes.string,
-	},
-
-	render: function() {
-		const className = classNames( 'jetpack-colophon', this.props.className );
-
-		return (
-			<div className={ className }>
-				<span className="jetpack-colophon__power">Powered by </span><JetpackLogo size={ 24 } full />
-			</div>
-		);
-	}
-} );
+export default JetpackColophon;
