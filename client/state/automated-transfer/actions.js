3ad147595285282862d69f8e26a31e0c195aec06
Steve Seear <steve.seear@gmail.com>
2017-08-31 17:31:48 +0100

    AT: data-layer for requesting transfer status

--- client/state/automated-transfer/actions.js
+++ client/state/automated-transfer/actions.js
@@ -4,8 +4,20 @@
 import {
 	AUTOMATED_TRANSFER_ELIGIBILITY_REQUEST,
 	AUTOMATED_TRANSFER_ELIGIBILITY_UPDATE,
+	AUTOMATED_TRANSFER_STATUS_REQUEST,
 	AUTOMATED_TRANSFER_STATUS_SET,
 } from 'state/action-types';
+
+/**
+ * Query the automated transfer status of a given site.
+ *
+ * @param {number} siteId The id of the site to query.
+ * @returns {Object} An action object
+ */
+export const getAutomatedTransferStatus = ( siteId ) => ( {
+	type: AUTOMATED_TRANSFER_STATUS_REQUEST,
+	siteId,
+} );
 
 /**
  * Sets the status of an automated transfer for a particular site
@@ -14,12 +26,14 @@ import {
  *
  * @param {number} siteId The site id to which the status belongs
  * @param {string} status The new status of the automated transfer
+ * @param {string} uploadedPluginId Id of any uploaded plugin
  * @returns {Object} An action object
  */
-export const setAutomatedTransferStatus = ( siteId, status ) => ( {
+export const setAutomatedTransferStatus = ( siteId, status, uploadedPluginId ) => ( {
 	type: AUTOMATED_TRANSFER_STATUS_SET,
 	siteId,
 	status,
+	uploadedPluginId,
 } );
 
 /**
---
+++ client/state/data-layer/wpcom/sites/automated-transfer/status/index.js
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import { delay } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { AUTOMATED_TRANSFER_STATUS_REQUEST } from 'state/action-types';
+import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { http } from 'state/data-layer/wpcom-http/actions';
+import {
+	getAutomatedTransferStatus,
+	setAutomatedTransferStatus,
+} from 'state/automated-transfer/actions';
+
+export const requestStatus = ( { dispatch }, action ) => {
+	const { siteId } = action;
+
+	dispatch( http( {
+		method: 'GET',
+		path: `/sites/${ siteId }/automated-transfers/status`,
+		apiVersion: '1',
+	}, action ) );
+};
+
+export const receiveStatus = ( { dispatch }, { siteId }, next, { status, uploaded_plugin_slug } ) => {
+	dispatch( setAutomatedTransferStatus( siteId, status, uploaded_plugin_slug ) );
+	if ( status !== 'complete' ) {
+		delay( dispatch, 3000, getAutomatedTransferStatus( siteId ) );
+	}
+};
+
+export default {
+	[ AUTOMATED_TRANSFER_STATUS_REQUEST ]: [ dispatchRequest(
+		requestStatus,
+		receiveStatus
+	) ]
+};
