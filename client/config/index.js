b411bd21d84b13fc2b479d6d2e5925bbd8958608
James Nylen <jnylen@gmail.com>
2017-09-01 16:50:00 -0500

    Enable modifying feature flags based on URL query parameters

--- client/config/index.js
+++ client/config/index.js
@@ -11,5 +11,32 @@ if ( 'undefined' === typeof window || ! window.configData ) {
 	throw new ReferenceError( 'No configuration was found: please see client/config/README.md for more information' );
 }
 
-export default createConfig( window.configData );
+const configData = window.configData;
 
+if ( process.env.NODE_ENV === 'development' ) {
+	const match = (
+		document.location.search &&
+		document.location.search.match( /[?&]flags=([^&]+)(&|$)/ )
+	);
+	if ( match ) {
+		const flags = match[ 1 ].split( ',' );
+		flags.forEach( flagRaw => {
+			const flag = flagRaw.replace( /^[-+]/, '' );
+			if ( /^-/.test( flagRaw ) ) {
+				console.log( // eslint-disable-line no-console
+					'Config flag disabled via URL:',
+					flag
+				);
+				configData.features[ flag ] = false;
+			} else {
+				console.log( // eslint-disable-line no-console
+					'Config flag enabled via URL:',
+					flag
+				);
+				configData.features[ flag ] = true;
+			}
+		} );
+	}
+}
+
+export default createConfig( configData );
