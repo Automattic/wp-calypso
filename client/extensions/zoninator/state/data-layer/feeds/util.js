fefbe77e032cefa5a3f358d84e8aed8fe999501f
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-09-05 12:37:00 +0200

    Store post titles & urls for zone feeds instead of just the IDs

--- client/extensions/zoninator/state/data-layer/feeds/util.js
+++ client/extensions/zoninator/state/data-layer/feeds/util.js
@@ -1,3 +1,9 @@
-export const fromApi = data => data.map( post => post.ID );
+export const fromApi = posts => posts.map( post => ( {
+	ID: post.ID,
+	title: post.post_title,
+	URL: post.guid,
+} ) );
 
-export const toApi = postIds => ( { post_ids: postIds } );
+export const toApi = posts => ( {
+	post_ids: posts.map( post => post.ID ),
+} );
