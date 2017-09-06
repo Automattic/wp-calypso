00295dd71791126775bc0efec6c5d227c592fc15
Justin Shreve <justin.shreve@gmail.com>
2017-09-06 13:36:17 -0700

    Adds state for tracking the current page, product, and search term being viewed by the UI for the reviews list.

---
+++ client/extensions/woocommerce/state/ui/reviews/reducer.js
@@ -0,0 +1,13 @@
+/**
+ * External dependencies
+ */
+import { combineReducers, keyedReducer } from 'state/utils';
+
+/**
+ * Internal dependencies
+ */
+import list from './list/reducer';
+
+export default keyedReducer( 'siteId', combineReducers( {
+	list,
+} ) );
---
+++ client/extensions/woocommerce/state/ui/reviews/test/actions.js
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { updateCurrentReviewsQuery } from '../actions';
+import { WOOCOMMERCE_UI_REVIEWS_SET_QUERY } from 'woocommerce/state/action-types';
+
+describe( 'actions', () => {
+	describe( '#updateCurrentReviewsQuery()', () => {
+		const siteId = '123';
+		it( 'should return an action', () => {
+			const action = updateCurrentReviewsQuery( siteId, { page: 2, search: 'testing' } );
+			expect( action ).to.eql( {
+				type: WOOCOMMERCE_UI_REVIEWS_SET_QUERY,
+				siteId,
+				query: { page: 2, search: 'testing' }
+			} );
+		} );
+	} );
+} );
