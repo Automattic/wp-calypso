74171f62a8a57193ba6dc1660cdc9dd4808e3934
Ben Dwyer <ben@scruffian.com>
2017-09-07 12:42:10 +0100

    Remove the code to fetch an OAuth signup URL, since we no longer use this

--- client/state/oauth2-clients/actions.js
+++ client/state/oauth2-clients/actions.js
@@ -5,9 +5,6 @@ import {
 	OAUTH2_CLIENT_DATA_REQUEST,
 	OAUTH2_CLIENT_DATA_REQUEST_FAILURE,
 	OAUTH2_CLIENT_DATA_REQUEST_SUCCESS,
-	OAUTH2_CLIENT_SIGNUP_URL_REQUEST,
-	OAUTH2_CLIENT_SIGNUP_URL_REQUEST_SUCCESS,
-	OAUTH2_CLIENT_SIGNUP_URL_REQUEST_FAILURE,
 } from 'state/action-types';
 import wpcom from 'lib/wp';
 import { cachingActionCreatorFactory } from 'state/utils';
@@ -32,18 +29,3 @@ export const fetchOAuth2ClientData = cachingActionCreatorFactory(
 		return Promise.reject( error );
 	},
 );
-
-export const fetchOAuth2SignupUrl = redirectTo => dispatch => {
-	dispatch( { type: OAUTH2_CLIENT_SIGNUP_URL_REQUEST } );
-
-	return wpcom.undocumented().oauth2SignupUrl( redirectTo ).then(
-		data => dispatch( { type: OAUTH2_CLIENT_SIGNUP_URL_REQUEST_SUCCESS, signupUrl: data.signup_url } ),
-		wpcomError => {
-			const error = convertWpcomError( wpcomError );
-
-			dispatch( { type: OAUTH2_CLIENT_SIGNUP_URL_REQUEST_FAILURE, error } );
-
-			return Promise.reject( error );
-		}
-	);
-};
