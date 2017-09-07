f3e77f9b2992be3c0068ddde1dc8eb6819d60320
Kevin Killingsworth <kevin@redfenix.com>
2017-09-07 18:53:54 -0500

    Store: Get coupons via WooCommerce API
    This adds the data-layer http code necessary to fetch pages of coupons
    from the API.

---
+++ client/extensions/woocommerce/state/sites/coupons/actions.js
@@ -0,0 +1,16 @@
+/**
+ * Internal dependencies
+ */
+import {
+	WOOCOMMERCE_COUPONS_REQUEST_PAGE,
+	WOOCOMMERCE_COUPONS_PAGE_UPDATED,
+} from 'woocommerce/state/action-types';
+
+export function fetchCouponsPage( siteId, pageIndex = 1, perPage = 10 ) {
+	return { type: WOOCOMMERCE_COUPONS_REQUEST_PAGE, siteId, pageIndex, perPage };
+}
+
+export function couponsPageUpdated( siteId, pageIndex, coupons, totalPages, totalCoupons ) {
+	return { type: WOOCOMMERCE_COUPONS_PAGE_UPDATED, siteId, pageIndex, coupons, totalPages, totalCoupons };
+}
+
