631a14a364c12686168ecbcfcd4710198a278e25
Jeff Bowen <jblz@users.noreply.github.com>
2017-09-07 14:17:02 -0400

    Magic Login: Pull the query args out of the link consumption URL (#17667)
    * Store data from the query in the context.state & redirect to the path without the querystring.
    * Show the "expired or invalid" page when required props are missing.
    * Only do the basics for layout on the server-side. Without the separate server-site route, calls to `page.redirect` were throwing an error because `document` was undefined in that env.

--- client/login/index.node.js
+++ client/login/index.node.js
@@ -3,6 +3,7 @@
  */
 import config from 'config';
 import webRouter from './index.web';
+import { makeLayout, redirectLoggedIn, setUpLocale } from 'controller';
 
 export default router => {
 	if ( config.isEnabled( 'login/wp-login' ) ) {
@@ -10,6 +11,16 @@ export default router => {
 			'/log-in/en', ( { res } ) => {
 				res.redirect( 301, '/log-in' );
 			}
+		);
+	}
+
+	if ( config.isEnabled( 'login/magic-login' ) ) {
+		// Only do the basics for layout on the server-side
+		router(
+			'/log-in/link/use/:lang?',
+			setUpLocale,
+			redirectLoggedIn,
+			makeLayout,
 		);
 	}
 
