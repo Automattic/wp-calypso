3a26f0a28f4bcd0e31636284f2a6f8d78c276adb
Donna Peplinskie <donnapep@gmail.com>
2017-09-08 14:53:09 -0400

    Add validation to ReduxFormTextInput component

--- client/components/redux-forms/redux-form-text-input/index.jsx
+++ client/components/redux-forms/redux-form-text-input/index.jsx
@@ -9,11 +9,20 @@ import { Field } from 'redux-form';
 /**
  * Internal dependencies
  */
+import FormInputValidation from 'components/forms/form-input-validation';
 import FormTextInput from 'components/forms/form-text-input';
 
 // eslint-disable-next-line no-unused-vars
-const TextInputRenderer = ( { input, meta, ...props } ) =>
-	<FormTextInput { ...input } { ...props } />;
+const TextInputRenderer = ( { input, meta: { error, touched }, ...props } ) => {
+	const isError = !! ( touched && error );
+
+	return (
+		<span>
+			<FormTextInput isError={ isError } { ...input } { ...props } />
+			{ isError && <FormInputValidation isError text={ error } /> }
+		</span>
+	);
+};
 
 const ReduxFormTextInput = props => <Field component={ TextInputRenderer } { ...props } />;
 
