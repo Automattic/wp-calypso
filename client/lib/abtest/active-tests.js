5e65fc26839345bb777c5ba231c94a7a2aae2943
Del Putnam <del@putnams.net>
2017-09-09 17:58:27 -0400

    Recommend shortest domain name (before TLD) for A/B Test.

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -87,4 +87,14 @@ module.exports = {
 		allowExistingUsers: true,
 		localeTargets: 'any',
 	},
+	recommendShortestDomain: {
+		datestamp: '20170908', // placeholder
+		variations: {
+			shortest: 50,
+			original: 50,
+		},
+		defaultVariation: 'original',
+		allowExistingUsers: true,
+		localeTargets: 'any',
+	}
 };
