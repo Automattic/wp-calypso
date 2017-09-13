461b0a3d7e0ad572d39143851e8e5458d6cf8040
Andrés Maneiro <andres.maneiro@automattic.com>
2017-09-13 15:29:04 -0700

    Use wpcom-http instead of wpcom directly

--- client/state/data-layer/wpcom/account-recovery/validate/index.js
+++ client/state/data-layer/wpcom/account-recovery/validate/index.js
@@ -1,31 +1,43 @@
 /**
  * Internal dependencies
  */
-import wpcom from 'lib/wp';
 import { ACCOUNT_RECOVERY_RESET_VALIDATE_REQUEST } from 'state/action-types';
 import {
 	validateRequestSuccess,
 	validateRequestError,
 	setValidationKey,
 } from 'state/account-recovery/reset/actions';
+import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { http } from 'state/data-layer/wpcom-http/actions';
 
 export const handleValidateRequest = ( { dispatch }, action ) => {
 	const { userData, method, key } = action;
-	wpcom.req.post( {
-		body: {
-			...userData,
-			method,
-			key,
-		},
-		apiNamespace: 'wpcom/v2',
-		path: '/account-recovery/validate',
-	} ).then( () => {
-		dispatch( validateRequestSuccess() );
-		dispatch( setValidationKey( key ) );
-	} )
-	.catch( ( error ) => dispatch( validateRequestError( error ) ) );
+	dispatch(
+		http(
+			{
+				method: 'POST',
+				apiNamespace: 'wpcom/v2',
+				path: '/account-recovery/validate',
+				body: {
+					...userData,
+					method,
+					key
+				}
+			},
+			action
+		)
+	);
+};
+
+export const handleValidateRequestSuccess = ( { dispatch }, { key } ) => {
+	dispatch( validateRequestSuccess() );
+	dispatch( setValidationKey( key ) );
+};
+
+export const handleValidateRequestFailure = ( { dispatch }, action, response ) => {
+	dispatch( validateRequestError( response ) );
 };
 
 export default {
-	[ ACCOUNT_RECOVERY_RESET_VALIDATE_REQUEST ]: [ handleValidateRequest ],
+	[ ACCOUNT_RECOVERY_RESET_VALIDATE_REQUEST ]: [ dispatchRequest( handleValidateRequest, handleValidateRequestSuccess, handleValidateRequestFailure ) ], // eslint-disable-line max-len
 };
