2ce642f81af0b0af9f1f1f99a1c82d2faff820ba
Justin Shreve <justin.shreve@gmail.com>
2017-09-13 13:21:49 -0600

    Store: Add review listing screen and replies state (#17969, #17983, #17991)
    These were all reviewed separately.
    
    * Adds basic read only review listing, filtering, and searching.
    
    * style product reviews list view
    
    * Remove 'all' view.
    
    * Handle PR feedback / code cleanup: Comment about avatar size magic number, clean up filter nav labels for translation.
    
    * Fix loading indicators, so they don't show if data/a tab has been previously loaded.
    
    * Store: Add expanded card view for product reviews (#17983)
    
    * Adds expanded view for product reviews
    
    * Handle PR Feedback: Provide comment on difference between currentStatus and review.status, make sure rendered comment correctly displays, simplify star rating logic, clean up spam language, clean up verified buyer label.
    
    * Store: Add state for fetching admin replies associated with a review. (#17991)
    
    * Add state for fetching admin replies associated with a review.
    
    * Handle PR feedback

--- client/extensions/woocommerce/app/reviews/index.js
+++ client/extensions/woocommerce/app/reviews/index.js
@@ -4,6 +4,7 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
 
 /**
  * Internal dependencies
@@ -11,17 +12,25 @@ import { localize } from 'i18n-calypso';
 import ActionHeader from 'woocommerce/components/action-header';
 import Main from 'components/main';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
+import ReviewsList from './reviews-list';
 
 class Reviews extends Component {
+	static propTypes = {
+		params: PropTypes.shape( {
+			filter: PropTypes.string,
+		} ),
+		className: PropTypes.string,
+	};
 
 	render() {
-		const { className, translate } = this.props;
+		const { className, translate, params } = this.props;
 		const classes = classNames( 'reviews__list', className );
 
 		return (
 			<Main className={ classes }>
 				<SidebarNavigation />
 				<ActionHeader breadcrumbs={ ( <span>{ translate( 'Reviews' ) }</span> ) } />
+				<ReviewsList currentStatus={ params && params.filter } />
 			</Main>
 		);
 	}
---
+++ client/extensions/woocommerce/state/sites/review-replies/test/selectors.js
@@ -0,0 +1,93 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import {
+	getReviewReplies,
+	getReviewReply,
+} from '../selectors';
+import reviewReplies from './fixtures/review-replies';
+
+const preInitializedState = {
+	extensions: {
+		woocommerce: {},
+	},
+};
+const loadingState = {
+	extensions: {
+		woocommerce: {},
+	},
+};
+const loadedState = {
+	extensions: {
+		woocommerce: {
+			sites: {
+				123: {
+					reviewReplies: {
+						555: reviewReplies,
+					}
+				},
+				321: {
+					reviewReplies: {
+						556: [],
+					}
+				},
+			},
+		},
+	},
+};
+
+const loadedStateWithUi = { ...loadedState, ui: { selectedSiteId: 123 } };
+
+describe( 'selectors', () => {
+	describe( '#getReviewReplies', () => {
+		it( 'should be false when woocommerce state is not available.', () => {
+			expect( getReviewReplies( preInitializedState, 555, 123 ) ).to.be.false;
+		} );
+
+		it( 'should be false when review replies are loading.', () => {
+			expect( getReviewReplies( loadingState, 555, 123 ) ).to.be.empty;
+		} );
+
+		it( 'should be the list of replies if they are loaded.', () => {
+			expect( getReviewReplies( loadedState, 555, 123 ) ).to.eql( reviewReplies );
+		} );
+
+		it( 'should be the list of replies if they are loaded for a different site.', () => {
+			expect( getReviewReplies( loadedState, 556, 321 ) ).to.eql( [] );
+		} );
+
+		it( 'should be false when reviews are loaded only for a different site.', () => {
+			expect( getReviewReplies( loadedState, 555, 456 ) ).to.be.false;
+		} );
+
+		it( 'should get the siteId from the UI tree if not provided.', () => {
+			expect( getReviewReplies( loadedStateWithUi, 555 ) ).to.eql( reviewReplies );
+		} );
+	} );
+	describe( '#getReviewReply', () => {
+		it( 'should be false when woocommerce state is not available.', () => {
+			expect( getReviewReply( preInitializedState, 555, 556, 123 ) ).to.be.false;
+		} );
+
+		it( 'should be false when review replies are loading.', () => {
+			expect( getReviewReply( loadingState, 555, 556, 123 ) ).to.be.empty;
+		} );
+
+		it( 'should return reply content if replies are loaded.', () => {
+			expect( getReviewReply( loadedState, 555, 556, 123 ) ).to.eql( reviewReplies[ 0 ] );
+		} );
+
+		it( 'should be false when reviews are loaded only for a different site.', () => {
+			expect( getReviewReply( loadedState, 555, 556, 456 ) ).to.be.false;
+		} );
+
+		it( 'should get the siteId from the UI tree if not provided.', () => {
+			expect( getReviewReply( loadedStateWithUi, 555, 556 ) ).to.eql( reviewReplies[ 0 ] );
+		} );
+	} );
+} );
