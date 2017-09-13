11adb749d216d17ab58ce3367ac39d6eb80c071f
Jon Surrell <jon.surrell@automattic.com>
2017-09-13 15:38:40 -0700

    Add requestMediaItem success handler

--- client/state/data-layer/wpcom/sites/media/index.js
+++ client/state/data-layer/wpcom/sites/media/index.js
@@ -97,11 +97,15 @@ function handleMediaItemRequest( { dispatch, getState }, action ) {
 		)
 	);
 }
+function receiveMediaItem( { dispatch }, { mediaId, siteId }, media ) {
+	dispatch( receiveMedia( siteId, media ) );
+	dispatch( successMediaItemRequest( siteId, mediaId ) );
+}
 
 export default {
 	[ MEDIA_REQUEST ]: [ dispatchRequest( requestMedia, requestMediaSuccess, requestMediaError ) ],
 	[ MEDIA_ITEM_REQUEST ]: [
 		requestMediaItem,
-		dispatchRequest( handleMediaItemRequest, ()=>{}, () => {} ),
+		dispatchRequest( handleMediaItemRequest, receiveMediaItem, () => {} ),
 	],
 };
