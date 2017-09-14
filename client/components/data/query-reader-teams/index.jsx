119f516783d7bc3a328d5450f3c7361d33097504
Del Putnam <del@putnams.net>
2017-09-13 17:08:21 -0700

    Remove isRequesting props and requesting from state since the QueryReaderTeams component since these are no longer needed once handleTeamsRequest will use http data layer instead of wpcom.

--- client/components/data/query-reader-teams/index.jsx
+++ client/components/data/query-reader-teams/index.jsx
@@ -9,14 +9,9 @@ import { bindActionCreators } from 'redux';
  * Internal dependencies
  */
 import { requestTeams } from 'state/reader/teams/actions';
-import { isRequestingReaderTeams } from 'state/selectors';
 
 class QueryReaderTeams extends Component {
 	componentWillMount() {
-		if ( this.props.isRequesting ) {
-			return;
-		}
-
 		this.props.requestTeams();
 	}
 
@@ -26,18 +21,13 @@ class QueryReaderTeams extends Component {
 }
 
 QueryReaderTeams.propTypes = {
-	isRequesting: PropTypes.bool,
 	request: PropTypes.func
 };
-
-const mapStateToProps = state => ( {
-	requesting: isRequestingReaderTeams( state )
-} );
 
 const mapDispatchToProps = dispatch =>
 	bindActionCreators( { requestTeams }, dispatch );
 
 export default connect(
-	mapStateToProps,
+	null,
 	mapDispatchToProps,
 )( QueryReaderTeams );
