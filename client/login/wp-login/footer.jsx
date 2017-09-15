67431dd583ae66e8e37160350ac6d6514f5ba677
Jeremy Herve <jeremy@tagada.hu>
2017-09-15 16:55:01 -0700

    Add Prettier to new file.
    @see
    https://github.com/Automattic/wp-calypso/pull/18056/files#r139267438

--- client/login/wp-login/footer.jsx
+++ client/login/wp-login/footer.jsx
@@ -1,3 +1,4 @@
+/** @format */
 /**
  * External dependencies
  */
@@ -23,36 +24,34 @@ class LoginFooter extends Component {
 					'wp-login__footer--jetpack': ! isOauthLogin,
 				} ) }
 			>
-				{ isOauthLogin ? (
-					<div className="wp-login__footer-links">
-						<a
-							href="https://wordpress.com/about/"
-							rel="noopener noreferrer"
-							target="_blank"
-							title={ translate( 'About' ) }
-						>
-							{ translate( 'About' ) }
-						</a>
-						<a
-							href="https://automattic.com/privacy/"
-							rel="noopener noreferrer"
-							target="_blank"
-							title={ translate( 'Privacy' ) }
-						>
-							{ translate( 'Privacy' ) }
-						</a>
-						<a
-							href="https://wordpress.com/tos/"
-							rel="noopener noreferrer"
-							target="_blank"
-							title={ translate( 'Terms of Service' ) }
-						>
-							{ translate( 'Terms of Service' ) }
-						</a>
-					</div>
-				) : (
-					<img src="/calypso/images/jetpack/powered-by-jetpack.svg" alt="Powered by Jetpack" />
-				) }
+				{ isOauthLogin
+					? <div className="wp-login__footer-links">
+							<a
+								href="https://wordpress.com/about/"
+								rel="noopener noreferrer"
+								target="_blank"
+								title={ translate( 'About' ) }
+							>
+								{ translate( 'About' ) }
+							</a>
+							<a
+								href="https://automattic.com/privacy/"
+								rel="noopener noreferrer"
+								target="_blank"
+								title={ translate( 'Privacy' ) }
+							>
+								{ translate( 'Privacy' ) }
+							</a>
+							<a
+								href="https://wordpress.com/tos/"
+								rel="noopener noreferrer"
+								target="_blank"
+								title={ translate( 'Terms of Service' ) }
+							>
+								{ translate( 'Terms of Service' ) }
+							</a>
+						</div>
+					: <img src="/calypso/images/jetpack/powered-by-jetpack.svg" alt="Powered by Jetpack" /> }
 			</div>
 		);
 	}
@@ -63,8 +62,6 @@ LoginFooter.propTypes = {
 	translate: PropTypes.func.isRequired,
 };
 
-export default connect(
-	( state ) => ( {
-		oauth2Client: getCurrentOAuth2Client( state ),
-	} )
-)( localize( LoginFooter ) );
+export default connect( state => ( {
+	oauth2Client: getCurrentOAuth2Client( state ),
+} ) )( localize( LoginFooter ) );
