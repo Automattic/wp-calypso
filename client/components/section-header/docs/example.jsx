e5f2e85c0c2eaf31a9c7172c71745faff21e2999
Kelly Dwan <ryelle@users.noreply.github.com>
2017-09-15 13:01:01 -0700

    Fix SectionHeader docs example

--- client/components/section-header/docs/example.jsx
+++ client/components/section-header/docs/example.jsx
@@ -2,7 +2,6 @@
 * External dependencies
 */
 import React, { PureComponent } from 'react';
-import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -16,26 +15,22 @@ class SectionHeaderExample extends PureComponent {
 	render() {
 		return (
 			<div>
-				<SectionHeader label={ this.props.translate( 'Team' ) } count={ 10 }>
-					<Button compact primary>
-						{ this.props.translate( 'Primary Action' ) }
-					</Button>
-					<Button compact>
-						{ this.props.translate( 'Manage' ) }
-					</Button>
+				<SectionHeader label="Team" count={ 10 }>
+					<Button compact primary>Primary Action</Button>
+					<Button compact>Manage</Button>
 					<Button
 						compact
 						onClick={ function() {
 							alert( 'Clicked add button' );
 						} }
 					>
-						{ this.props.translate( 'Add' ) }
+						Add
 					</Button>
 				</SectionHeader>
 
 				<h3>Clickable SectionHeader</h3>
 
-				<SectionHeader label={ this.props.translate( 'Team' ) } count={ 10 } href="/devdocs/design/section-header">
+				<SectionHeader label="Team" count={ 10 } href="/devdocs/design/section-header">
 				</SectionHeader>
 
 				<h3>Empty SectionHeader</h3>
@@ -45,4 +40,4 @@ class SectionHeaderExample extends PureComponent {
 	}
 }
 
-export default localize( SectionHeader );
+export default SectionHeaderExample;
