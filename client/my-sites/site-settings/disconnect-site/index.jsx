46000364eadee514490192b4267c01c854eee7d5
AnnaMag <anna.mag.kedzierska@gmail.com>
2017-09-18 12:59:56 +0100

    Site Settings: add Disconnect Site page
    and styles.css file.

---
+++ client/my-sites/site-settings/disconnect-site/index.jsx
@@ -0,0 +1,48 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { flowRight } from 'lodash';
+import { localize } from 'i18n-calypso';
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+import DocumentHead from 'components/data/document-head';
+import FormattedHeader from 'components/formatted-header';
+import { getSelectedSite } from 'state/ui/selectors';
+import Main from 'components/main';
+import Placeholder from 'my-sites/site-settings/placeholder';
+import redirectNonJetpack from 'my-sites/site-settings/redirect-non-jetpack';
+
+class DisconnectSite extends Component {
+	render() {
+		const { site, translate } = this.props;
+
+		if ( ! site ) {
+			return <Placeholder />;
+		}
+		return (
+			<Main className="disconnect-site site-settings">
+				<DocumentHead title={ translate( 'Site Settings' ) } />
+				<FormattedHeader
+					headerText={ translate( 'Disconnect Site' ) }
+					subHeaderText={ translate(
+						'Tell us why you want to disconnect your site from WordPress.com.'
+					) }
+				/>
+				<Card className="disconnect-site__card"> </Card>
+			</Main>
+		);
+	}
+}
+
+const connectComponent = connect( state => {
+	return {
+		site: getSelectedSite( state ),
+	};
+} );
+
+export default flowRight( connectComponent, localize, redirectNonJetpack() )( DisconnectSite );
