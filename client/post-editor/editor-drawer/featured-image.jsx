187aafe39541637b9402518bbfcb56e7702eb971
Ian Dunn <ian@iandunn.name>
2017-09-19 16:41:26 -0700

    Editor: Migrate deprecated React.PropTypes to npm prop-types module.

--- client/post-editor/editor-drawer/featured-image.jsx
+++ client/post-editor/editor-drawer/featured-image.jsx
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
