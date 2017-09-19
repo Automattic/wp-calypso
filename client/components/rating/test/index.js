0ae76e5bb9c5bdaf77619fbe0f7ffa2ba058bf35
Raoul Wegat <raoul@raoulwegat.com>
2017-09-19 13:14:50 +1000

    Update test file

--- client/components/rating/test/index.js
+++ client/components/rating/test/index.js
@@ -12,57 +12,30 @@ import Rating from 'components/rating';
 
 describe( '<Rating />', function() {
 	describe( 'check props size', function() {
-		it( 'should fill in the parent component if no size', function() {
+		it( 'should be set to 24px if no size', function() {
 			const wrapper = shallow(
 				<Rating />
 			);
 
-			const component = wrapper.find( 'div' );
-			expect( component.props().style.width ).to.equal( '100%' );
+			const component = wrapper.find( 'div.rating' );
+			expect( component.props().style.width ).to.equal( '120px' ); // 24 * 5 = 120;
 		} );
 
 		it( 'should use size if passed', function() {
-			const size = 50,
+			const size = 48,
 				wrapper = shallow(
 					<Rating
 						size={ size }
 					/>
 				);
 
-			const component = wrapper.find( 'div' );
+			const component = wrapper.find( 'div.rating' );
 			expect( component.props().style.width ).to.equal( ( size * 5 ) + 'px' );
-		} );
-	} );
-
-	describe( 'check props rating', function() {
-		it( 'should render 5 spans without rating', function() {
-			const wrapper = shallow(
-					<Rating />
-				);
-
-			const component = wrapper.find( 'span' );
-			expect( component.nodes.length ).to.equal( 5 );
-			expect( component ).to.have.exactly( 5 ).descendants( '.noticon-rating-empty' );
-		} );
-
-		it( 'should render 1.5 stars with rating 30', function() {
-			const rating = 30,
-				wrapper = shallow(
-					<Rating
-						rating={ rating }
-					/>
-				);
-
-			const component = wrapper.find( 'span' );
-			expect( component.nodes.length ).to.equal( 5 );
-			expect( component ).to.have.exactly( 1 ).descendants( '.noticon-rating-full' );
-			expect( component ).to.have.exactly( 1 ).descendants( '.noticon-rating-half' );
-			expect( component ).to.have.exactly( 3 ).descendants( '.noticon-rating-empty' );
 		} );
 
 		it( 'should use size in each star', function() {
 			const rating = 30,
-				size = 10,
+				size = 48,
 				wrapper = shallow(
 					<Rating
 						rating={ rating }
@@ -70,27 +43,56 @@ describe( '<Rating />', function() {
 					/>
 				);
 
-			wrapper.find( 'span' ).forEach(
+			wrapper.find( 'svg' ).forEach(
 				( node ) => {
-					expect( node.props().style.fontSize ).to.equal( size + 'px' );
+					expect( node.props().style.width ).to.equal( size + 'px' );
 				}
 			);
 		} );
+	} );
 
-		it( 'if no size it should inherit', function() {
-			const rating = 30,
+	describe( 'check props rating', function() {
+		it( 'should render full width mask for no rating', function() {
+			const size = 24, // use default size
 				wrapper = shallow(
 					<Rating
-						rating={ rating
-						}
+						size={ size }
 					/>
 				);
 
-			wrapper.find( 'span' ).forEach(
-				( node ) => {
-					expect( node.props().style.fontSize ).to.equal( 'inherit' );
-				}
-			);
+			const component = wrapper.find( 'div.rating__overlay' );
+			expect( component.props().style.clipPath ).to.equal( 'inset(0 ' + ( size * 5 ) + 'px 0 0 )' );
+			expect( component.props().style.clip ).to.equal( 'rect(0, 0px, ' + size + 'px, 0)' );
+		} );
+
+		it( 'should render half width mask for rating 50', function() {
+			const rating = 50,
+				size = 24, // use default size
+				wrapper = shallow(
+					<Rating
+						rating={ rating }
+						size={ size }
+					/>
+				);
+
+			const component = wrapper.find( 'div.rating__overlay' );
+			expect( component.props().style.clipPath ).to.equal( 'inset(0 ' + ( ( size * 5 ) / 2 ) + 'px 0 0 )' );
+			expect( component.props().style.clip ).to.equal( 'rect(0, ' + ( ( size * 5 ) / 2 ) + 'px, ' + size + 'px, 0)' );
+		} );
+
+		it( 'should render no mask for full rating', function() {
+			const rating = 100,
+				size = 24, // use default size
+				wrapper = shallow(
+					<Rating
+						rating={ rating }
+						size={ size }
+					/>
+				);
+
+			const component = wrapper.find( 'div.rating__overlay' );
+			expect( component.props().style.clipPath ).to.equal( 'inset(0 0px 0 0 )' );
+			expect( component.props().style.clip ).to.equal( 'rect(0, ' + ( size * 5 ) + 'px, ' + size + 'px, 0)' );
 		} );
 	} );
 } );
