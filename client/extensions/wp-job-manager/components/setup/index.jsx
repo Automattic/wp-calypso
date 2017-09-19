5acdde1281882d795a210374f0978e16a941d5ad
Donna Peplinskie <donnapep@gmail.com>
2017-09-19 11:20:55 -0400

    Convert SetupWizard to ES6 class

--- client/extensions/wp-job-manager/components/setup/index.jsx
+++ client/extensions/wp-job-manager/components/setup/index.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
@@ -18,41 +18,44 @@ import PageSetup from './page-setup';
 import Wizard from 'components/wizard';
 import { getSelectedSiteSlug } from 'state/ui/selectors';
 
-const SetupWizard = ( {
-	slug,
-	stepName = Steps.INTRO,
-	translate,
-} ) => {
-	const steps = [ Steps.INTRO, Steps.PAGE_SETUP, Steps.CONFIRMATION ];
-	const components = {
-		[ Steps.INTRO ]: <Intro />,
-		[ Steps.PAGE_SETUP ]: <PageSetup />,
-		[ Steps.CONFIRMATION ]: <Confirmation />,
+class SetupWizard extends Component {
+	static propTypes = {
+		slug: PropTypes.string,
+		stepName: PropTypes.string,
+		translate: PropTypes.func.isRequired,
 	};
-	const mainClassName = 'wp-job-manager__setup';
 
-	return (
-		<Main className={ mainClassName }>
-			<DocumentHead title={ translate( 'Setup' ) } />
-			<Wizard
-				basePath={ `/extensions/wp-job-manager/setup/${ slug }` }
-				components={ components }
-				forwardText={ translate( 'Continue' ) }
-				hideNavigation={ true }
-				steps={ steps }
-				stepName={ stepName } />
-		</Main>
-	);
-};
+	render() {
+		const steps = [ Steps.INTRO, Steps.PAGE_SETUP, Steps.CONFIRMATION ];
+		const components = {
+			[ Steps.INTRO ]: <Intro />,
+			[ Steps.PAGE_SETUP ]: <PageSetup />,
+			[ Steps.CONFIRMATION ]: <Confirmation />,
+		};
+		const {
+			slug,
+			stepName = Steps.INTRO,
+			translate,
+		} = this.props;
+		const mainClassName = 'wp-job-manager__setup';
+
+		return (
+			<Main className={ mainClassName }>
+				<DocumentHead title={ translate( 'Setup' ) } />
+				<Wizard
+					basePath={ `/extensions/wp-job-manager/setup/${ slug }` }
+					components={ components }
+					forwardText={ translate( 'Continue' ) }
+					hideNavigation={ true }
+					steps={ steps }
+					stepName={ stepName } />
+			</Main>
+		);
+	}
+}
 
 const mapStateToProps = state => ( {
 	slug: getSelectedSiteSlug( state ),
 } );
-
-SetupWizard.propTypes = {
-	slug: PropTypes.string,
-	stepName: PropTypes.string,
-	translate: PropTypes.func.isRequired,
-};
 
 export default connect( mapStateToProps )( localize( SetupWizard ) );
