c67a6d220c7f60158e7513d601e7874dc24c2f7a
Justin Shreve <justin.shreve@gmail.com>
2017-09-19 10:30:16 -0700

    Store: Add UI for viewing review replies (#18003)
    * Add UI for review replies
    
    * Update comment textarea design. See https://github.com/Automattic/wp-calypso/pull/18003#issuecomment-329314913

---
+++ client/extensions/woocommerce/app/reviews/review-replies.js
@@ -0,0 +1,88 @@
+/**
+ * External depedencies
+ */
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
+
+/**
+ * Internal dependencies
+ */
+import { fetchReviewReplies } from 'woocommerce/state/sites/review-replies/actions';
+import { getSelectedSiteWithFallback } from 'woocommerce/state/sites/selectors';
+import { getReviewReplies } from 'woocommerce/state/sites/review-replies/selectors';
+import ReviewReply from './review-reply';
+
+class ReviewReplies extends Component {
+	static propTypes = {
+		siteId: PropTypes.number,
+		review: PropTypes.shape( {
+			status: PropTypes.string,
+			name: PropTypes.string,
+			id: PropTypes.number,
+		} ).isRequired,
+	};
+
+	componentDidMount() {
+		const { siteId, review } = this.props;
+		if ( siteId ) {
+			this.props.fetchReviewReplies( siteId, review.id );
+		}
+	}
+
+	componentWillReceiveProps( newProps ) {
+		const { siteId, review } = this.props;
+		const newSiteId = newProps.siteId || null;
+		const oldSiteId = siteId || null;
+		if ( oldSiteId !== newSiteId ) {
+			this.props.fetchReviewReplies( newSiteId, review.id );
+		}
+	}
+
+	renderReply = ( replyId, i ) => {
+		const { review } = this.props;
+		return <ReviewReply
+			key={ i }
+			reviewId={ review.id }
+			replyId={ replyId }
+		/>;
+	}
+
+	render() {
+		const { replyIds, review, translate } = this.props;
+		const repliesOutput = replyIds.length && replyIds.map( this.renderReply ) || null;
+
+		const textAreaPlaceholder = 'approved' === review.status
+			? translate( 'Reply to %(reviewAuthor)s…', { args: { reviewAuthor: review.name } } )
+			: translate( 'Approve and reply to %(reviewAuthor)s…', { args: { reviewAuthor: review.name } } );
+
+		return (
+			<div className="reviews__replies">
+				{ repliesOutput }
+
+				<form className="reviews__reply-textarea">
+					<textarea placeholder={ textAreaPlaceholder } />
+					<button className="reviews__reply-submit">
+						{ translate( 'Send' ) }
+					</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default connect(
+	( state, props ) => {
+		const site = getSelectedSiteWithFallback( state );
+		const siteId = site ? site.ID : false;
+		const replies = getReviewReplies( state, props.review.id );
+		const replyIds = replies && replies.map( reply => reply.id ) || [];
+		return {
+			siteId,
+			replyIds,
+		};
+	},
+	dispatch => bindActionCreators( { fetchReviewReplies }, dispatch )
+)( localize( ReviewReplies ) );
---
+++ client/extensions/woocommerce/app/reviews/review-reply.js
@@ -0,0 +1,80 @@
+/**
+ * External depedencies
+ */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Gridicon from 'gridicons';
+import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
+
+/**
+ * Internal dependencies
+ */
+import Button from 'components/button';
+import { getReviewReply } from 'woocommerce/state/sites/review-replies/selectors';
+import Gravatar from './gravatar';
+import humanDate from 'lib/human-date';
+
+class ReviewReply extends Component {
+	static propTypes = {
+		siteId: PropTypes.number,
+		reviewId: PropTypes.number.isRequired,
+		replyId: PropTypes.number.isRequired,
+	};
+
+	renderReplyActions = () => {
+		const { translate } = this.props;
+		return (
+			<div className="reviews__reply-actions">
+				<Button borderless className="reviews__reply-action-edit">
+					<Gridicon icon="pencil" />
+					<span>{ translate( 'Edit reply' ) }</span>
+				</Button>
+				<Button borderless className="reviews__reply-action-delete">
+					<Gridicon icon="trash" />
+					<span>{ translate( 'Delete reply' ) }</span>
+				</Button>
+			</div>
+		);
+	}
+
+	render() {
+		const { reply } = this.props;
+		const content = reply.content && reply.content.rendered || '';
+		return (
+			<div className="reviews__reply">
+				<div className="reviews__reply-bar">
+					<div className="reviews__author-gravatar">
+						<Gravatar
+							object={ reply }
+							forType="reply"
+						/>
+					</div>
+
+					<div className="reviews__info">
+						<div className="reviews__author-name">{ reply.author_name }</div>
+						<div className="reviews__date">{ humanDate( reply.date_gmt + 'Z' ) }</div>
+					</div>
+
+					{ this.renderReplyActions() }
+				</div>
+
+				<div className="reviews__reply-content"
+					dangerouslySetInnerHTML={ { __html: content } } //eslint-disable-line react/no-danger
+					// Sets the rendered comment HTML correctly for display.
+					// Also used for comments in `comment-detail/comment-detail-comment.jsx`
+				/>
+			</div>
+		);
+	}
+
+}
+
+export default connect(
+	( state, props ) => {
+		const reply = getReviewReply( state, props.reviewId, props.replyId );
+		return {
+			reply,
+		};
+	}
+)( localize( ReviewReply ) );
