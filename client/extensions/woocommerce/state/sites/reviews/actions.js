35ccfffcc538c82624c3ba4a03dadc749ba89877
Justin Shreve <justin.shreve@gmail.com>
2017-09-20 13:12:17 -0700

    Add the ability to update a reviews status or delete it.

--- client/extensions/woocommerce/state/sites/reviews/actions.js
+++ client/extensions/woocommerce/state/sites/reviews/actions.js
@@ -4,6 +4,8 @@
 import { getNormalizedReviewsQuery } from './utils';
 import {
 	WOOCOMMERCE_REVIEWS_REQUEST,
+	WOOCOMMERCE_REVIEW_DELETE,
+	WOOCOMMERCE_REVIEW_STATUS_CHANGE
 } from 'woocommerce/state/action-types';
 
 export function fetchReviews( siteId, query = {} ) {
@@ -12,5 +14,25 @@ export function fetchReviews( siteId, query = {} ) {
 		type: WOOCOMMERCE_REVIEWS_REQUEST,
 		siteId,
 		query: normalizedQuery,
+	};
+}
+
+export function deleteReview( siteId, productId, reviewId ) {
+	return {
+		type: WOOCOMMERCE_REVIEW_DELETE,
+		siteId,
+		productId,
+		reviewId,
+	};
+}
+
+export function changeReviewStatus( siteId, productId, reviewId, currentStatus, newStatus ) {
+	return {
+		type: WOOCOMMERCE_REVIEW_STATUS_CHANGE,
+		siteId,
+		productId,
+		reviewId,
+		currentStatus,
+		newStatus,
 	};
 }
--- client/extensions/woocommerce/state/sites/reviews/test/handlers.js
+++ client/extensions/woocommerce/state/sites/reviews/test/handlers.js
@@ -2,22 +2,34 @@
  * External dependencies
  */
 import { expect } from 'chai';
+import { keyBy } from 'lodash';
 import { spy, match } from 'sinon';
 
 /**
  * Internal dependencies
  */
 import {
+	changeReviewStatus,
+	deleteReview,
 	fetchReviews,
 } from 'woocommerce/state/sites/reviews/actions';
 import {
 	handleReviewsRequest,
 	handleReviewsRequestSuccess,
 	handleReviewsRequestError,
+	handleChangeReviewStatus,
+	handleChangeReviewStatusSuccess,
+	announceStatusChangeFailure,
+	handleDeleteReview,
+	announceDeleteSuccess,
+	announceDeleteFailure,
 } from '../handlers.js';
+import { NOTICE_CREATE } from 'state/action-types';
 import reviews from './fixtures/reviews';
 import {
 	WOOCOMMERCE_REVIEWS_RECEIVE,
+	WOOCOMMERCE_REVIEWS_REQUEST,
+	WOOCOMMERCE_REVIEW_STATUS_CHANGE,
 } from 'woocommerce/state/action-types';
 import { WPCOM_HTTP_REQUEST } from 'state/action-types';
 
@@ -106,6 +118,177 @@ describe( 'handlers', () => {
 				query: {},
 				error: 'rest_no_route',
 			} );
+		} );
+	} );
+	describe( '#handleChangeReviewStatus', () => {
+		it( 'should dispatch a request', () => {
+			const siteId = '123';
+			const productId = '544';
+			const reviewId = '105';
+			const currentStatus = 'pending';
+			const newStatus = 'approved';
+			const dispatch = spy();
+			const action = changeReviewStatus( siteId, productId, reviewId, currentStatus, newStatus );
+			handleChangeReviewStatus( { dispatch }, action );
+			expect( dispatch ).to.have.been.calledWith( match( {
+				type: WPCOM_HTTP_REQUEST,
+				method: 'POST',
+				path: `/jetpack-blogs/${ siteId }/rest-api/`,
+				query: {
+					json: true,
+					apiVersion: '1.1',
+				},
+				body: {
+					path: `/wp/v2/comments/${ reviewId }&_method=POST`,
+					body: JSON.stringify( { status: 'approved' } ),
+				}
+			} ) );
+		} );
+	} );
+	describe( '#handleChangeReviewStatusSuccess', () => {
+		const siteId = '123';
+		const productId = '544';
+		const reviewId = '105';
+		const currentStatus = 'pending';
+		const getState = () => {
+			return {
+				extensions: {
+					woocommerce: {
+						sites: {
+							123: {
+								reviews: {
+									items: keyBy( reviews, 'id' ),
+								}
+							},
+						},
+					},
+				},
+			};
+		};
+
+		it( 'should dispatch a fetch request for trash status updates', () => {
+			const store = {
+				dispatch: spy(),
+				getState,
+			};
+			const action = changeReviewStatus( siteId, productId, reviewId, currentStatus, 'trash' );
+			handleChangeReviewStatusSuccess( store, action );
+			expect( store.dispatch ).to.have.been.calledWith( match( {
+				type: WOOCOMMERCE_REVIEWS_REQUEST,
+			} ) );
+		} );
+
+		it( 'should dispatch a fetch request for spam status updates', () => {
+			const store = {
+				dispatch: spy(),
+				getState,
+			};
+			const action = changeReviewStatus( siteId, productId, reviewId, currentStatus, 'spam' );
+			handleChangeReviewStatusSuccess( store, action );
+			expect( store.dispatch ).to.have.been.calledWith( match( {
+				type: WOOCOMMERCE_REVIEWS_REQUEST,
+			} ) );
+		} );
+
+		it( 'should not dispatch a fetch request for other status updates', () => {
+			const store = {
+				dispatch: spy(),
+				getState,
+			};
+			const action = changeReviewStatus( siteId, productId, reviewId, currentStatus, 'approved' );
+			handleChangeReviewStatusSuccess( store, action );
+			expect( store.dispatch ).to.not.have.been.calledWith( match( {
+				type: WOOCOMMERCE_REVIEWS_REQUEST,
+			} ) );
+		} );
+	} );
+
+	describe( '#announceStatusChangeFailure', () => {
+		const siteId = '123';
+		const productId = '544';
+		const reviewId = '105';
+		const currentStatus = 'pending';
+		const dispatch = spy();
+
+		it( 'should reset the status and dispatch an error', () => {
+			const action = changeReviewStatus( siteId, productId, reviewId, currentStatus, 'approved' );
+			announceStatusChangeFailure( { dispatch }, action );
+			expect( dispatch ).to.have.been.calledWith( match( {
+				type: WOOCOMMERCE_REVIEW_STATUS_CHANGE,
+				newStatus: 'pending', // Status should change back to 'pending'
+			} ) );
+
+			expect( dispatch ).to.have.been.calledWith( match( {
+				type: NOTICE_CREATE,
+			} ) );
+		} );
+	} );
+
+	describe( '#handleDeleteReview', () => {
+		const siteId = '123';
+		const productId = '544';
+		const reviewId = '105';
+		it( 'should dispatch a request', () => {
+			const dispatch = spy();
+			const action = deleteReview( siteId, productId, reviewId );
+			handleDeleteReview( { dispatch }, action );
+			expect( dispatch ).to.have.been.calledWith( match( {
+				type: WPCOM_HTTP_REQUEST,
+				method: 'POST',
+				path: `/jetpack-blogs/${ siteId }/rest-api/`,
+				query: {
+					json: true,
+					apiVersion: '1.1',
+				},
+				body: {
+					path: `/wp/v2/comments/${ reviewId }&force=true&_method=DELETE`,
+				}
+			} ) );
+		} );
+	} );
+
+	describe( '#announceDeleteSuccess', () => {
+		const siteId = '123';
+		const getState = () => {
+			return {
+				extensions: {
+					woocommerce: {
+						sites: {
+							123: {
+								reviews: {
+									items: keyBy( reviews, 'id' ),
+								}
+							},
+						},
+					},
+				},
+			};
+		};
+
+		it( 'should dispatch a fetch request and success notice', () => {
+			const store = {
+				dispatch: spy(),
+				getState,
+			};
+			const action = deleteReview( siteId, 544, 105 );
+			announceDeleteSuccess( store, action );
+			expect( store.dispatch ).to.have.been.calledWith( match( {
+				type: WOOCOMMERCE_REVIEWS_REQUEST,
+			} ) );
+			expect( store.dispatch ).to.have.been.calledWith( match( {
+				type: NOTICE_CREATE,
+			} ) );
+		} );
+	} );
+	describe( '#announceDeleteFailure', () => {
+		const siteId = '123';
+		const dispatch = spy();
+		it( 'should dispatch an error', () => {
+			const action = deleteReview( siteId, 544, 105 );
+			announceDeleteFailure( { dispatch }, action );
+			expect( dispatch ).to.have.been.calledWith( match( {
+				type: NOTICE_CREATE,
+			} ) );
 		} );
 	} );
 } );
