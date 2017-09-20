3139506ec191e20ff0c492f17d5491f675e63dc2
Tugdual de Kerviler <dekervit@gmail.com>
2017-09-20 13:28:25 -0700

    Add TODO comment on context.user

--- server/pages/index.js
+++ server/pages/index.js
@@ -290,6 +290,8 @@ function setUpLoggedInRoute( req, res, next ) {
 			const end = new Date().getTime() - start;
 
 			debug( 'Rendering with bootstrapped user object. Fetched in %d ms', end );
+			// context.user is still used at in `render` to check if the user is bootstrapped
+			// TODO: remove this and update to using a redux selector
 			context.user = data;
 
 			context.store.dispatch( receiveUser( data ) );
