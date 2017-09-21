22827d3223c44fe6fd8727bbb0cf24ae6da49c4f
Kevin Killingsworth <kevin@redfenix.com>
2017-09-20 20:58:56 -0500

    Store: Add coupons API data reducer.
    This adds a reducer to store couponn API data from WooCommerce into the
    redux state in the appropriate place under the desired site id.

---
+++ client/extensions/woocommerce/state/sites/coupons/reducer.js
@@ -0,0 +1,27 @@
+/**
+ * Internal dependencies
+ */
+import { createReducer } from 'state/utils';
+import {
+	WOOCOMMERCE_COUPONS_PAGE_UPDATED,
+} from 'woocommerce/state/action-types';
+
+export default createReducer( {}, {
+	[ WOOCOMMERCE_COUPONS_PAGE_UPDATED ]: pageUpdated,
+} );
+
+function pageUpdated( state, action ) {
+	const { pageIndex, coupons, totalPages, totalCoupons } = action;
+
+	if ( coupons ) {
+		return {
+			coupons,
+			pageIndex,
+			totalPages,
+			totalCoupons,
+		};
+	}
+
+	return null;
+}
+
