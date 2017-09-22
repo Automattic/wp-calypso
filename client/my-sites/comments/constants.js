07586bd640a8c814e135e51f149fce1a7db9b594
Marko Andrijasevic <markoandrijasevic@gmail.com>
2017-09-22 15:01:31 +0200

    Fix typo in comments

--- client/my-sites/comments/constants.js
+++ client/my-sites/comments/constants.js
@@ -2,7 +2,7 @@
 export const COMMENTS_PER_PAGE = 20;
 
 // Allowed values for comments list sort order.
-// Currently these sorting is done by comparing the values of comment IDs,
+// Currently the sorting is done by comparing the values of comment IDs,
 // and relying on the fact that more recent comments have higher ID values.
 export const NEWEST_FIRST = 'desc';
 export const OLDEST_FIRST = 'asc';
