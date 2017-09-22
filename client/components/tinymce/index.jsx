0c5dcc16d38bd2702bf71c56364ae9c8573afdad
Stéphane Thomas <email@stephanethomas.com>
2017-09-22 13:49:19 -0700

    Font: Always load Google font from HTTPS
    This makes sure that we never load this font twice as depending on the protocol a slightly different stylesheet is served: the stylesheet indeed contains url() which includes the protocol in use.

--- client/components/tinymce/index.jsx
+++ client/components/tinymce/index.jsx
@@ -155,7 +155,7 @@ const CONTENT_CSS = [
 	window.app.staticUrls[ 'tinymce/skins/wordpress/wp-content.css' ],
 	'//s1.wp.com/wp-includes/css/dashicons.css?v=20150727',
 	window.app.staticUrls[ 'editor.css' ],
-	'//fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i&subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese',
+	'https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i&subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese',
 ];
 
 module.exports = React.createClass( {
