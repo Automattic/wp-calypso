ec44cbaa748167dddf7e71398a4f5903eb36a1d5
Jon Surrell <jon.surrell@automattic.com>
2017-09-22 12:27:24 +0200

    Replace require with import

--- client/state/signup/steps/jpo-connect/action.js
+++ client/state/signup/steps/jpo-connect/action.js
@@ -1,18 +1,24 @@
+/** @format */
 /**
  * External dependencies
  */
-const debug = require( 'debug' )( 'calypso:jetpack-jpo:actions' );
+import debugFactory from 'debug';
 
 /**
  * Internal dependencies
  */
 import { SIGNUP_STEPS_JPO_CONNECT_SET } from 'state/action-types';
 
+/**
+ * Module constants
+ */
+const debug = debugFactory( 'calypso:jetpack-jpo:actions' );
+
 export function setJpoConnect( connect ) {
-	debug( 'setJPOConnect: ', connect );
+	debug( 'setJpoConnect: ', connect );
 
 	return {
 		type: SIGNUP_STEPS_JPO_CONNECT_SET,
-		connect
+		connect,
 	};
 }
