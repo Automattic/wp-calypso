c9c21f792b0a0adb45b9a9ce102a9bf0fb69fe9f
Jason Moon <jason.moon@automattic.com>
2017-09-22 14:09:51 -0600

    Analytics: Add HotJar debug statements

--- client/lib/analytics/index.js
+++ client/lib/analytics/index.js
@@ -23,6 +23,7 @@ import { statsdTimingUrl } from 'lib/analytics/statsd';
  */
 const mcDebug = debug( 'calypso:analytics:mc' );
 const gaDebug = debug( 'calypso:analytics:ga' );
+const hotjarDebug = debug( 'calypso:analytics:hotjar' );
 const tracksDebug = debug( 'calypso:analytics:tracks' );
 
 let _superProps,
@@ -444,10 +445,12 @@ const analytics = {
 	hotjar: {
 		addHotJarScript: function() {
 			if ( ! config( 'hotjar_enabled' ) || doNotTrack() || isPiiUrl() ) {
+				hotjarDebug( 'Not loading HotJar script' );
 				return;
 			}
 
 			( function( h, o, t, j, a, r ) {
+				hotjarDebug( 'Loading HotJar script' );
 				h.hj = h.hj || function() {
 					( h.hj.q = h.hj.q || [] ).push( arguments );
 				};
