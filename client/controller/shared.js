9015ea50ce2c535b56789497309c0d4891b7462c
Yury Michurin <yury@automattic.com>
2017-09-25 15:34:15 +0300

    Change replacing stylesheet loading
    and fix RTL general stylesheet if we're loaded as LTR

--- client/controller/shared.js
+++ client/controller/shared.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import { noop } from 'lodash';
+import { noop, find } from 'lodash';
 
 /**
  * Internal dependencies
@@ -37,31 +37,60 @@ export function makeLayoutMiddleware( LayoutComponent ) {
 	};
 }
 
+/***
+ * Replaces stylesheet element gracefully
+ *
+ * If we set the `href` attribute of `link` directly, the browser will remove
+ * the previous stylesheet and will start loading the new one, but in the meantime
+ * we won't have all of the sizes the old one had.
+ *
+ * To prevent that, we first load a new stylesheet and then remove the old one
+ *
+ * @param {DOMElement} linkElement Link to change
+ * @param {String} newHref new url to load
+ */
+function replaceStlyesheet( linkElement, newHref ) {
+	const newLink = Object.assign( document.createElement( 'link' ), {
+		rel: 'stylesheet',
+		type: 'text/css',
+		href: newHref,
+	} );
+
+	if ( linkElement.id ) {
+		newLink.id = linkElement.id;
+	}
+
+	function deleteOldLink() {
+		linkElement.parentElement.removeChild( linkElement );
+		newLink.removeEventListener( 'load', deleteOldLink );
+	}
+
+	newLink.addEventListener( 'load', deleteOldLink );
+
+	linkElement.parentElement.appendChild( newLink );
+}
+
 function loadCSS( cssUrl, elementId = null ) {
 	if ( typeof document === 'undefined' ) {
 		return;
 	}
 
-	let link;
 	if ( elementId ) {
-		link = document.getElementById( elementId );
-	}
+		const link = document.getElementById( elementId );
 
-	if ( link ) {
 		if ( link.getAttribute( 'href' ) !== cssUrl ) {
-			link.href = cssUrl;
+			replaceStlyesheet( link, cssUrl );
 		}
-		return;
+	} else {
+		const link = Object.assign( document.createElement( 'link' ), {
+			rel: 'stylesheet',
+			type: 'text/css',
+			href: cssUrl,
+			... elementId && { id: elementId }
+		} );
+
+		document.head.appendChild( link );
 	}
-
-	link = Object.assign( document.createElement( 'link' ), {
-		rel: 'stylesheet',
-		type: 'text/css',
-		href: cssUrl,
-		... elementId && { id: elementId }
-	} );
-
-	document.head.appendChild( link );
 }
 
 export function setSection( section ) {
@@ -83,6 +112,28 @@ export function loadSectionCSS( context, next ) {
 	next();
 }
 
+function fixRtlStyles() {
+	const htmlElement = document.querySelectorAll( 'html' )[ 0 ];
+
+	// this will be already set to rtl if SSR properly rendered
+	if ( htmlElement.dir === 'rtl' ) {
+		return;
+	}
+
+	const linkElements = Array.prototype.slice.apply( document.querySelectorAll( 'link[rel=stylesheet]' ) );
+	// can be style.css and style-debug.css
+	const mainStyleLink = find( linkElements, el => el.href.match( /style[^.]+\.css/ ) );
+	const mainStyleLinkHash = mainStyleLink.href.match( /\?v=([A-F0-9]+)$/i )[ 1 ];
+	const rtlStyleHref = mainStyleLink.href.split( '/' )
+		.slice( 0, -1 )
+		.concat( [ 'style-rtl.css?v=' + mainStyleLinkHash ] )
+		.join( '/' );
+
+	replaceStlyesheet( mainStyleLink, rtlStyleHref );
+
+	htmlElement.dir = 'rtl';
+}
+
 export function setUpLocale( context, next ) {
 	const currentUser = getCurrentUser( context.store.getState() );
 
@@ -92,6 +143,10 @@ export function setUpLocale( context, next ) {
 	} else if ( currentUser ) {
 		context.lang = currentUser.localeSlug;
 		context.isRTL = isUserRtl( context.store.getState() );
+
+		if ( context.isRTL && ! config( 'rtl' ) ) {
+			fixRtlStyles();
+		}
 	} else {
 		context.lang = context.lang || config( 'i18n_default_locale_slug' );
 		context.isRTL = context.isRTL || Boolean( config( 'rtl' ) );
