16750c60980e514befa8cca72490c15cbe0e1494
Kelly Dwan <ryelle@users.noreply.github.com>
2017-09-25 14:12:31 -0400

    Use the AddressView component for editing addresses

--- client/extensions/woocommerce/app/order/order-create/customer-card.js
+++ client/extensions/woocommerce/app/order/order-create/customer-card.js
@@ -10,6 +10,7 @@ import PropTypes from 'prop-types';
 /**
  * Internal dependencies
  */
+import AddressView from 'woocommerce/components/address-view';
 import Card from 'components/card';
 import { getOrderWithEdits } from 'woocommerce/state/ui/orders/selectors';
 import FormCheckbox from 'components/forms/form-checkbox';
@@ -18,8 +19,6 @@ import FormLabel from 'components/forms/form-label';
 import FormLegend from 'components/forms/form-legend';
 import FormTelInput from 'components/forms/form-tel-input';
 import FormTextInput from 'components/forms/form-text-input';
-
-const AddressSearch = () => null;
 
 class OrderCustomerCard extends Component {
 	static propTypes = {
@@ -31,26 +30,78 @@ class OrderCustomerCard extends Component {
 		showShipping: false
 	}
 
+	getAddressViewFormat = ( address ) => {
+		return {
+			street: address.address_1 || '',
+			street2: address.address_2 || '',
+			city: address.city || '',
+			state: address.state || '',
+			country: address.country || '',
+			postcode: address.postcode || '',
+		};
+	}
+
+	onAddressChange = ( type = 'billing' ) => {
+		return ( event ) => {
+			this.updateOrder( `${ type }_${ event.target.name }`, event.target.value );
+		};
+	}
+
 	onChange = ( event ) => {
+		this.updateOrder( event.target.name, event.target.value );
+	}
+
+	updateOrder = ( name, value ) => {
 		let updateOrder;
-		switch ( event.target.name ) {
+		switch ( name ) {
 			case 'customerEmail':
-				updateOrder = { billing: { email: event.target.value } };
+				updateOrder = { billing: { email: value } };
 				break;
 			case 'firstName':
-				updateOrder = { billing: { first_name: event.target.value } };
+				updateOrder = { billing: { first_name: value } };
 				break;
 			case 'lastName':
-				updateOrder = { billing: { last_name: event.target.value } };
+				updateOrder = { billing: { last_name: value } };
 				break;
 			case 'phoneNumber':
-				updateOrder = { billing: { phone: event.target.value } };
+				updateOrder = { billing: { phone: value } };
 				break;
+			case 'billing_street':
+				updateOrder = { billing: { address_1: value } };
+				break;
+			case 'billing_street2':
+				updateOrder = { billing: { address_2: value } };
+				break;
+			case 'billing_city':
+			case 'billing_state':
+			case 'billing_country':
+			case 'billing_postcode': {
+				const keyName = name.replace( 'billing_', '' );
+				updateOrder = { billing: { [ keyName ]: value } };
+				break;
+			}
 			case 'shippingFirstName':
-				updateOrder = { shipping: { first_name: event.target.value } };
+				updateOrder = { shipping: { first_name: value } };
 				break;
 			case 'shippingLastName':
-				updateOrder = { shipping: { last_name: event.target.value } };
+				updateOrder = { shipping: { last_name: value } };
+				break;
+			case 'shipping_street':
+				updateOrder = { shipping: { address_1: value } };
+				break;
+			case 'shipping_street2':
+				updateOrder = { shipping: { address_2: value } };
+				break;
+			case 'shipping_city':
+			case 'shipping_state':
+			case 'shipping_country':
+			case 'shipping_postcode': {
+				const keyName = name.replace( 'shipping_', '' );
+				updateOrder = { shipping: { [ keyName ]: value } };
+				break;
+			}
+			default:
+				updateOrder = {};
 				break;
 		}
 		if ( this.props.order && this.props.order.id ) {
@@ -91,7 +142,10 @@ class OrderCustomerCard extends Component {
 							onChange={ this.onChange } />
 					</div>
 				</div>
-				<AddressSearch />
+				<AddressView
+					isEditable
+					onChange={ this.onAddressChange( 'shipping' ) }
+					address={ this.getAddressViewFormat( get( order, [ 'shipping' ], {} ) ) } />
 			</FormFieldset>
 		);
 	}
@@ -137,7 +191,10 @@ class OrderCustomerCard extends Component {
 								onChange={ this.onChange } />
 						</div>
 					</div>
-					<AddressSearch />
+					<AddressView
+						isEditable
+						onChange={ this.onAddressChange( 'billing' ) }
+						address={ this.getAddressViewFormat( get( order, [ 'billing' ], {} ) ) } />
 				</FormFieldset>
 				<FormFieldset>
 					<FormLegend>{ translate( 'Shipping Details' ) }</FormLegend>
