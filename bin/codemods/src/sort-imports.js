3a9c6c1aa1a58e957d8a2ede6ae944159412ab87
Jake <samouri@users.noreply.github.com>
2017-09-25 22:29:42 -0400

    Sort Imports: account for nodejs libs (#18244)
    * Sort Imports: account for nodejs libs
    
    * modify style to conform better

--- bin/codemods/src/sort-imports.js
+++ bin/codemods/src/sort-imports.js
@@ -11,8 +11,10 @@ const _ = require( 'lodash' );
 /**
  * Gather all of the external deps and throw them in a set
  */
+const nodeJsDeps = require( 'repl' )._builtinLibs;
 const packageJson = JSON.parse( fs.readFileSync( './package.json', 'utf8' ) );
 const packageJsonDeps = []
+	.concat( nodeJsDeps )
 	.concat( Object.keys( packageJson.dependencies ) )
 	.concat( Object.keys( packageJson.devDependencies ) );
 
