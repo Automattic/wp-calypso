e69c9f09062b7734df145f802420140882fa59ab
Kevin Killingsworth <kevin@redfenix.com>
2017-09-26 16:26:59 -0500

    Store: Add promotions state
    This adds action/reducer/handler code for promotions which are comprised
    of "on-sale" products and coupons at this time. It fetches all product
    and coupon data and compiles it into promotions objects.

--- client/extensions/woocommerce/state/ui/reducer.js
+++ client/extensions/woocommerce/state/ui/reducer.js
@@ -5,6 +5,7 @@ import orders from './orders/reducer';
 import payments from './payments/reducer';
 import products from './products/reducer';
 import productCategories from './product-categories/reducer';
+import promotions from './promotions/reducer';
 import reviews from './reviews/reducer';
 import reviewReplies from './review-replies/reducer';
 import shipping from './shipping/reducer';
@@ -15,6 +16,7 @@ export default combineReducers( {
 	payments,
 	products,
 	productCategories,
+	promotions,
 	reviews,
 	reviewReplies,
 	shipping,
