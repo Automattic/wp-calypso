f29bdcb36bf7694d4774f527d153ab1574ac17bc
Tugdual de Kerviler <dekervit@gmail.com>
2017-09-25 18:24:36 -0700

    Fix loading CSS section

--- client/controller/shared.js
+++ client/controller/shared.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import { noop, find } from 'lodash';
+import { noop } from 'lodash';
 
 /**
  * Internal dependencies
@@ -37,101 +37,64 @@ export function makeLayoutMiddleware( LayoutComponent ) {
 	};
 }
 
-/***
- * Replaces stylesheet element gracefully
- *
- * If we set the `href` attribute of `link` directly, the browser will remove
- * the previous stylesheet and will start loading the new one, but in the meantime
- * we won't have all of the sizes the old one had.
- *
- * To prevent that, we first load a new stylesheet and then remove the old one
- *
- * @param {DOMElement} linkElement Link to change
- * @param {String} newHref new url to load
+/**
+ * Loads a a css stylesheet into the page
+ * @param {string} cssUrl - a url to a css resource to be inserted into the page
+ * @param {Function} callback - a callback function to be called when the CSS has been loaded (after 500ms have passed).
  */
-function replaceStlyesheet( linkElement, newHref ) {
-	const newLink = Object.assign( document.createElement( 'link' ), {
+function loadCSS( cssUrl, callback = noop ) {
+	const link = Object.assign( document.createElement( 'link' ), {
 		rel: 'stylesheet',
 		type: 'text/css',
-		href: newHref,
+		href: cssUrl,
 	} );
 
-	if ( linkElement.id ) {
-		newLink.id = linkElement.id;
-	}
-
-	function deleteOldLink() {
-		linkElement.parentElement.removeChild( linkElement );
-		newLink.removeEventListener( 'load', deleteOldLink );
-	}
-
-	newLink.addEventListener( 'load', deleteOldLink );
-
-	linkElement.parentElement.appendChild( newLink );
-}
-
-function loadCSS( cssUrl, elementId = null ) {
-	if ( typeof document === 'undefined' ) {
-		return;
-	}
-
-	if ( elementId ) {
-		const link = document.getElementById( elementId );
-
-		if ( link.getAttribute( 'href' ) !== cssUrl ) {
-			replaceStlyesheet( link, cssUrl );
+	const onload = () => {
+		if ( 'onload' in link ) {
+			link.onload = null;
 		}
-	} else {
-		const link = Object.assign( document.createElement( 'link' ), {
-			rel: 'stylesheet',
-			type: 'text/css',
-			href: cssUrl,
-			... elementId && { id: elementId }
-		} );
+		callback( null, link );
+	};
 
-		document.head.appendChild( link );
+	if ( 'onload' in link ) {
+		link.onload = onload;
+	} else {
+		setTimeout( onload, 500 );
 	}
+
+	document.head.appendChild( link );
 }
 
 export function setSection( section ) {
 	return ( context, next = noop ) => {
 		context.store.dispatch( setSectionAction( section ) );
 
-		next();
+		loadSectionCSS( context, next );
 	};
 }
 
 export function loadSectionCSS( context, next ) {
 	const section = getSection( context.store.getState() );
 
-	if ( section.cssUrls ) {
+	if ( section.cssUrls && typeof document !== 'undefined' ) {
 		const cssUrl = context.isRTL ? section.cssUrls.rtl : section.cssUrls.ltr;
-		loadCSS( cssUrl, 'section-css' );
-	}
 
-	next();
-}
+		// TODO: handle adding styles in `state.documentHead.meta` instead (currently only supports setting all meta at once)
+		loadCSS( cssUrl, ( err, newLink ) => {
+			const currentLink = document.getElementById( 'section-css' );
+			if ( currentLink ) {
+				currentLink.parentElement.removeChild( currentLink );
+			}
 
-function fixRtlStyles() {
-	const htmlElement = document.querySelectorAll( 'html' )[ 0 ];
+			newLink.id = 'section-css';
 
-	// this will be already set to rtl if SSR properly rendered
-	if ( htmlElement.dir === 'rtl' ) {
+			next();
+		} );
+
 		return;
 	}
 
-	const linkElements = Array.prototype.slice.apply( document.querySelectorAll( 'link[rel=stylesheet]' ) );
-	// can be style.css and style-debug.css
-	const mainStyleLink = find( linkElements, el => el.href.match( /style[^.]+\.css/ ) );
-	const mainStyleLinkHash = mainStyleLink.href.match( /\?v=([A-F0-9]+)$/i )[ 1 ];
-	const rtlStyleHref = mainStyleLink.href.split( '/' )
-		.slice( 0, -1 )
-		.concat( [ 'style-rtl.css?v=' + mainStyleLinkHash ] )
-		.join( '/' );
-
-	replaceStlyesheet( mainStyleLink, rtlStyleHref );
-
-	htmlElement.dir = 'rtl';
+	next();
 }
 
 export function setUpLocale( context, next ) {
@@ -143,10 +106,6 @@ export function setUpLocale( context, next ) {
 	} else if ( currentUser ) {
 		context.lang = currentUser.localeSlug;
 		context.isRTL = isUserRtl( context.store.getState() );
-
-		if ( context.isRTL && ! config( 'rtl' ) ) {
-			fixRtlStyles();
-		}
 	} else {
 		context.lang = context.lang || config( 'i18n_default_locale_slug' );
 		context.isRTL = context.isRTL || Boolean( config( 'rtl' ) );
