072c56b6d825d22c9ec5e87946f6911929b2ba59
Kamen Stanev <hambai@gmail.com>
2017-09-26 18:01:02 +0300

    Created unit tests for the data layer functions

---
+++ client/state/data-layer/wpcom/meta/sms-country-codes/test/index.js
@@ -0,0 +1,71 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+import { spy } from 'sinon';
+
+/**
+ * Internal dependencies
+ */
+import { http } from 'state/data-layer/wpcom-http/actions';
+import { COUNTRIES_SMS_UPDATED, NOTICE_CREATE } from 'state/action-types';
+import { fetchCountries, handleSuccess, handleError } from '../';
+
+describe( 'wpcom-api', () => {
+	describe( 'meta sms-country-codes', () => {
+		describe( '#fetchCountries', () => {
+			it( 'should dispatch HTTP request to plans endpoint', () => {
+				const action = { type: 'DUMMY' };
+				const dispatch = spy();
+
+				fetchCountries( { dispatch }, action );
+
+				expect( dispatch ).to.have.been.calledOnce;
+				expect( dispatch ).to.have.been.calledWith(
+					http(
+						{
+							apiVersion: '1.1',
+							method: 'GET',
+							path: '/meta/sms-country-codes/',
+						},
+						action
+					)
+				);
+			} );
+		} );
+
+		describe( '#handleSuccess', () => {
+			it( 'should dispatch updated action', () => {
+				const action = { type: 'DUMMY' };
+				const dispatch = spy();
+				const data = [ 'BG', 'US', 'UK' ];
+
+				handleSuccess( { dispatch }, action, data );
+
+				expect( dispatch ).to.have.been.calledOnce;
+				expect( dispatch ).to.have.been.calledWith( {
+					type: COUNTRIES_SMS_UPDATED,
+					countries: data,
+				} );
+			} );
+		} );
+
+		describe( '#handleError', () => {
+			it( 'should dispatch error notice', () => {
+				const dispatch = spy();
+
+				handleError( { dispatch } );
+
+				expect( dispatch ).to.have.been.calledOnce;
+				expect( dispatch ).to.have.been.calledWithMatch( {
+					type: NOTICE_CREATE,
+					notice: {
+						status: 'is-error',
+						text: "We couldn't load the countries list.",
+					},
+				} );
+			} );
+		} );
+	} );
+} );
