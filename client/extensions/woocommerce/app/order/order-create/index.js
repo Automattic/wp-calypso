038f5c44939baf068e31b669c94bac6e9c6f2f78
Kelly Dwan <ryelle@users.noreply.github.com>
2017-09-26 15:28:12 -0400

    Store Orders: Add Customer Details to order creation (#18246)
    * Store Orders: Add components for create order form
    
    * Add the actions/selectors to enable edit/save functionality
    
    * Toggle shipping details
    
    * Fix React error with “uncontrolled checkbox"
    
    * Use the AddressView component for editing addresses
    
    * Use prettier to format new files
    
    * Add documentation for the update helper
    
    * style order create page: headers, spacing, colors
    
    * Switch to the auto-formatting phone number input component

--- client/extensions/woocommerce/app/order/order-create/index.js
+++ client/extensions/woocommerce/app/order/order-create/index.js
@@ -1,7 +1,9 @@
+/** @format */
 /**
  * External dependencies
  */
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
@@ -10,25 +12,51 @@ import { localize } from 'i18n-calypso';
  */
 import ActionHeader from 'woocommerce/components/action-header';
 import Button from 'components/button';
+import Card from 'components/card';
+import { editOrder } from 'woocommerce/state/ui/orders/actions';
 import { getSelectedSiteWithFallback } from 'woocommerce/state/sites/selectors';
 import { getLink } from 'woocommerce/lib/nav-utils';
 import Main from 'components/main';
+import OrderCustomerCard from './customer-card';
+import { ProtectFormGuard } from 'lib/protect-form';
+import SectionHeader from 'components/section-header';
 
 class OrderCreate extends Component {
+	editOrder = order => {
+		const { site } = this.props;
+		if ( site && site.ID ) {
+			this.props.editOrder( site.ID, order );
+		}
+	};
 
 	render() {
 		const { className, site, translate } = this.props;
 		const breadcrumbs = [
-			( <a href={ getLink( '/store/orders/:site/', site ) }>{ translate( 'Orders' ) }</a> ),
-			( <span>{ translate( 'New Order' ) }</span> ),
+			<a href={ getLink( '/store/orders/:site/', site ) }>{ translate( 'Orders' ) }</a>,
+			<span>{ translate( 'New Order' ) }</span>,
 		];
 		return (
 			<Main className={ className }>
 				<ActionHeader breadcrumbs={ breadcrumbs }>
 					<Button primary>{ translate( 'Save Order' ) }</Button>
 				</ActionHeader>
+				<ProtectFormGuard isChanged={ false } />
 
-				<div className="order-create__container"></div>
+				<div className="order-create__container">
+					<SectionHeader label={ translate( 'Customer Details' ) } />
+					<OrderCustomerCard siteId={ site && site.ID } editOrder={ this.editOrder } />
+
+					<SectionHeader label={ translate( 'Add products to the order' ) } />
+					<Card className="order-create__card" />
+
+					<SectionHeader label={ translate( 'How will these products be shipped?' ) } />
+					<Card className="order-create__card" />
+
+					<SectionHeader label={ translate( 'How will the customer pay for the order?' ) }>
+						<span>{ translate( 'Add coupon' ) }</span>
+					</SectionHeader>
+					<Card className="order-create__card" />
+				</div>
 			</Main>
 		);
 	}
@@ -37,11 +65,10 @@ class OrderCreate extends Component {
 export default connect(
 	state => {
 		const site = getSelectedSiteWithFallback( state );
-		const siteId = site ? site.ID : false;
 
 		return {
 			site,
-			siteId,
 		};
-	}
+	},
+	dispatch => bindActionCreators( { editOrder }, dispatch )
 )( localize( OrderCreate ) );
