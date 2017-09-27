eeae042d553b1e207e58177f81611528ecc86cdf
Bernhard Reiter <ockham@raz.or.at>
2017-09-27 15:44:36 +0200

    WPSC: Remove plugin_list from settings schema

--- client/extensions/wp-super-cache/state/settings/schema.js
+++ client/extensions/wp-super-cache/state/settings/schema.js
@@ -66,16 +66,6 @@ export const itemsSchema = {
 						tag: { type: 'boolean' }
 					}
 				},
-				plugin_list: {
-					type: 'object',
-					items: {
-						desc: { type: 'string' },
-						enabled: { type: 'boolean' },
-						key: { type: 'string' },
-						title: { type: 'string' },
-						url: { type: 'string' },
-					}
-				},
 				post_count: { type: 'string' },
 				preload_email_volume: { type: 'string' },
 				preload_interval: { type: 'integer' },
