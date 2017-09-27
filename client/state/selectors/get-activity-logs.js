e5a1af1c6486b46887ec2ddeec896b4b90be0344
Jon Surrell <jon.surrell@automattic.com>
2017-09-27 13:18:56 +0200

    Remove memoization from GetActivityLogs selector
    This appears to hold little benefit and adds complexity and memory
    footprint.

--- client/state/selectors/get-activity-logs.js
+++ client/state/selectors/get-activity-logs.js
@@ -5,12 +5,6 @@
 import { get } from 'lodash';
 
 /**
- * Internal dependencies
- */
-import ActivityQueryManager from 'lib/query-manager/activity';
-import createSelector from 'lib/create-selector';
-
-/**
  * Returns list of Activity Log items.
  *
  * @param  {Object}        state  Global state tree
@@ -18,17 +12,11 @@ import createSelector from 'lib/create-selector';
  * @param  {?Object}       query  Optional. Query object, passed to ActivityQueryManager.
  * @return {?array}               Activity log item objects. Null if no data.
  */
-const getActivityLogs = createSelector(
-	( state, siteId, query ) => {
-		const manager = get( state, [ 'activityLog', 'logItems', siteId ], null );
-		if ( ! manager ) {
-			return null;
-		}
+export default function getActivityLogs( state, siteId, query ) {
+	const manager = get( state, [ 'activityLog', 'logItems', siteId ], null );
+	if ( ! manager ) {
+		return null;
+	}
 
-		return manager.getItems( query );
-	},
-	( state, siteId ) => get( state, [ 'activityLog', 'logItems', siteId ] ),
-	( state, siteId, query ) => ActivityQueryManager.QueryKey.stringify( query )
-);
-
-export default getActivityLogs;
+	return manager.getItems( query );
+}
