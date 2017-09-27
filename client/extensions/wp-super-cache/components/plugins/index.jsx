dc8296e1fe03d264c35701366357457144282cb8
Bernhard Reiter <ockham@raz.or.at>
2017-09-27 18:16:17 +0200

    WPSC: Fix plugin toggling

--- client/extensions/wp-super-cache/components/plugins/index.jsx
+++ client/extensions/wp-super-cache/components/plugins/index.jsx
@@ -3,7 +3,8 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { map, pick } from 'lodash';
+import { connect } from 'react-redux';
+import { flowRight, map, pick } from 'lodash';
 
 /**
  * Internal dependencies
@@ -14,10 +15,14 @@ import SectionHeader from 'components/section-header';
 import FormSettingExplanation from 'components/forms/form-setting-explanation';
 import FormToggle from 'components/forms/form-toggle/compact';
 import WrapSettingsForm from '../wrap-settings-form';
+import QueryPlugins from '../data/query-plugins';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { togglePlugin } from '../../state/plugins/actions';
+import { isRequestingPlugins, getPlugins } from '../../state/plugins/selectors';
 
 class PluginsTab extends Component {
 	static propTypes = {
-		fields: PropTypes.object,
+		plugins: PropTypes.object,
 		translate: PropTypes.func.isRequired,
 	};
 
@@ -25,29 +30,32 @@ class PluginsTab extends Component {
 		fields: {},
 	};
 
+	togglePlugin = ( plugin, newState ) => () => {
+		this.props.togglePlugin( this.props.siteId, plugin, newState );
+	}
+
 	render() {
 		const {
-			fields: {
-				plugin_list: plugins
-			},
-			handleAutosavingToggle,
+			plugins,
 			isRequesting,
-			isSaving,
+			siteId,
 			translate,
 		} = this.props;
 
 		return (
 			<div className="wp-super-cache__plugins-tab">
 				<SectionHeader label={ translate( 'Plugins' ) } />
+				<QueryPlugins siteId={ siteId } />
 				<Card>
 					{ map( plugins, ( { desc, enabled, key, title, url } ) => {
 						return (
 							<div key={ key }>
 								<FormToggle
 									checked={ !! enabled }
-									disabled={ isRequesting || isSaving }
-									onChange={ handleAutosavingToggle( 'plugin_list' ) /* FIXME */ }>
-									<span>{ title } { url }</span>
+									data-plugin={ key }
+									disabled={ isRequesting }
+									onChange={ this.togglePlugin( key, ! enabled ) }>
+									<span>{ title }</span>
 								</FormToggle>
 								<FormSettingExplanation>
 									{ desc + ' ' }
@@ -64,10 +72,27 @@ class PluginsTab extends Component {
 	}
 }
 
+const connectComponent = connect(
+	( state ) => {
+		const siteId = getSelectedSiteId( state );
+		const plugins = getPlugins( state, siteId );
+		const isRequesting = isRequestingPlugins( state, siteId );
+
+		return {
+			isRequesting,
+			plugins
+		};
+	},
+	{ togglePlugin },
+);
+
 const getFormSettings = settings => {
 	return pick( settings, [
 		'plugin_list'
 	] );
 };
 
-export default WrapSettingsForm( getFormSettings )( PluginsTab );
+export default flowRight(
+	connectComponent,
+	WrapSettingsForm( getFormSettings )
+)( PluginsTab );
