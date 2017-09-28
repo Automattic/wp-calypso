65e1fa3a36c273296d6211f3189638997ed08f72
bluefuton <chris@bluefuton.com>
2017-09-28 15:24:00 +0100

    Prettify client/state/data-layer/wpcom/sites/utils.js

--- client/state/data-layer/wpcom/sites/utils.js
+++ client/state/data-layer/wpcom/sites/utils.js
@@ -1,3 +1,4 @@
+/** @format */
 /**
  * External dependencies
  */
@@ -6,11 +7,7 @@ import { translate } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
-import {
-	COMMENTS_DELETE,
-	COMMENTS_RECEIVE,
-	COMMENTS_COUNT_INCREMENT,
-} from 'state/action-types';
+import { COMMENTS_DELETE, COMMENTS_RECEIVE, COMMENTS_COUNT_INCREMENT } from 'state/action-types';
 import { bypassDataLayer } from 'state/data-layer/utils';
 import { http } from 'state/data-layer/wpcom-http/actions';
 import { getSitePost } from 'state/posts/selectors';
@@ -27,15 +24,15 @@ import { errorNotice } from 'state/notices/actions';
  * @returns {Object}                           comment placeholder
  */
 export const createPlaceholderComment = ( commentText, postId, parentCommentId ) => ( {
-	ID: 'placeholder-' + ( new Date().getTime() ),
+	ID: 'placeholder-' + new Date().getTime(),
 	parent: parentCommentId ? { ID: parentCommentId } : false,
-	date: ( new Date() ).toISOString(),
+	date: new Date().toISOString(),
 	content: commentText,
 	status: 'pending',
 	type: 'comment',
 	post: { ID: postId },
 	isPlaceholder: true,
-	placeholderState: 'PENDING'
+	placeholderState: 'PENDING',
 } );
 
 /**
@@ -57,22 +54,24 @@ export const dispatchNewCommentRequest = ( dispatch, action, path ) => {
 		siteId,
 		postId,
 		comments: [ placeholder ],
-		skipSort: !! parentCommentId
+		skipSort: !! parentCommentId,
 	} );
 
-	dispatch( http( {
-		method: 'POST',
-		apiVersion: '1.1',
-		path,
-		body: {
-			content: commentText
-		},
-		onSuccess: {
-			...action,
-			placeholderId: placeholder.ID
-		},
-		onFailure: action
-	} ) );
+	dispatch(
+		http( {
+			method: 'POST',
+			apiVersion: '1.1',
+			path,
+			body: {
+				content: commentText,
+			},
+			onSuccess: {
+				...action,
+				placeholderId: placeholder.ID,
+			},
+			onFailure: action,
+		} )
+	);
 };
 
 /**
@@ -82,11 +81,23 @@ export const dispatchNewCommentRequest = ( dispatch, action, path ) => {
  * @param {Object}   action   redux action
  * @param {Object}   comment  updated comment from the request response
  */
-export const updatePlaceholderComment = ( { dispatch }, { siteId, postId, parentCommentId, placeholderId }, comment ) => {
+export const updatePlaceholderComment = (
+	{ dispatch },
+	{ siteId, postId, parentCommentId, placeholderId },
+	comment
+) => {
 	// remove placeholder from state
-	dispatch( bypassDataLayer( { type: COMMENTS_DELETE, siteId, postId, commentId: placeholderId } ) );
+	dispatch(
+		bypassDataLayer( { type: COMMENTS_DELETE, siteId, postId, commentId: placeholderId } )
+	);
 	// add new comment to state with updated values from server
-	dispatch( { type: COMMENTS_RECEIVE, siteId, postId, comments: [ comment ], skipSort: !! parentCommentId } );
+	dispatch( {
+		type: COMMENTS_RECEIVE,
+		siteId,
+		postId,
+		comments: [ comment ],
+		skipSort: !! parentCommentId,
+	} );
 	// increment comments count
 	dispatch( { type: COMMENTS_COUNT_INCREMENT, siteId, postId } );
 };
@@ -101,7 +112,14 @@ export const updatePlaceholderComment = ( { dispatch }, { siteId, postId, parent
  */
 export const handleWriteCommentFailure = ( { dispatch, getState }, { siteId, postId } ) => {
 	const post = getSitePost( getState(), siteId, postId );
-	const postTitle = post && post.title && post.title.trim().slice( 0, 20 ).trim().concat( '…' );
+	const postTitle =
+		post &&
+		post.title &&
+		post.title
+			.trim()
+			.slice( 0, 20 )
+			.trim()
+			.concat( '…' );
 	const error = postTitle
 		? translate( 'Could not add a reply to “%(postTitle)s”', { args: { postTitle } } )
 		: translate( 'Could not add a reply to this post' );
