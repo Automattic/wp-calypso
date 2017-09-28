910b6fe3fd9b46a74e85a08d9ab06488439a0759
Mark Ryall <mark@ryall.name>
2017-09-28 14:56:22 +1000

    Move refer.wordpress.com request into third-party folder.

--- client/state/data-layer/third-party/index.js
+++ client/state/data-layer/third-party/index.js
@@ -2,10 +2,12 @@
  * Internal dependencies
  */
 import { mergeHandlers } from 'state/action-watchers/utils';
+import refer from './refer';
 import directly from './directly';
 
 export const handlers = mergeHandlers(
 	directly,
+	refer,
 );
 
 export default handlers;
