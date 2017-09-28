0da3012616d853824cbabfcd68d21ffb333fae98
Kelly Dwan <ryelle@users.noreply.github.com>
2017-09-28 17:46:51 -0400

    Update customer info to use `AddressView` component

--- client/extensions/woocommerce/app/order/order-customer/index.js
+++ client/extensions/woocommerce/app/order/order-customer/index.js
@@ -1,3 +1,4 @@
+/** @format */
 /**
  * External dependencies
  */
@@ -8,6 +9,7 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import AddressView from 'woocommerce/components/address-view';
 import Card from 'components/card';
 import SectionHeader from 'components/section-header';
 
@@ -17,7 +19,18 @@ class OrderCustomerInfo extends Component {
 			billing: PropTypes.object.isRequired,
 			shipping: PropTypes.object.isRequired,
 		} ),
-	}
+	};
+
+	getAddressViewFormat = address => {
+		return {
+			street: address.address_1 || '',
+			street2: address.address_2 || '',
+			city: address.city || '',
+			state: address.state || '',
+			country: address.country || '',
+			postcode: address.postcode || '',
+		};
+	};
 
 	render() {
 		const { order, translate } = this.props;
@@ -31,37 +44,35 @@ class OrderCustomerInfo extends Component {
 			<div className="order-customer">
 				<SectionHeader label={ translate( 'Customer Information' ) } />
 				<Card>
-				<div className="order-customer__container">
-					<div className="order-customer__billing">
-					<h3 className="order-customer__billing-details">{ translate( 'Billing Details' ) }</h3>
-						<h4>{ translate( 'Address' ) }</h4>
-						<div className="order-customer__billing-address">
-							<p>{ `${ billing.first_name } ${ billing.last_name }` }</p>
-							<p>{ billing.address_1 }</p>
-							<p>{ billing.address_2 }</p>
-							<p>{ `${ billing.city }, ${ billing.state } ${ billing.postcode }` }</p>
-							<p>{ billing.country }</p>
+					<div className="order-customer__container">
+						<div className="order-customer__billing">
+							<h3 className="order-customer__billing-details">
+								{ translate( 'Billing Details' ) }
+							</h3>
+							<h4>{ translate( 'Address' ) }</h4>
+							<div className="order-customer__billing-address">
+								<p>{ `${ billing.first_name } ${ billing.last_name }` }</p>
+								<AddressView address={ this.getAddressViewFormat( billing ) } />
+							</div>
+
+							<h4>{ translate( 'Email' ) }</h4>
+							<p>{ billing.email }</p>
+
+							<h4>{ translate( 'Phone' ) }</h4>
+							<span>{ billing.phone }</span>
 						</div>
 
-						<h4>{ translate( 'Email' ) }</h4>
-						<p>{ billing.email }</p>
-
-						<h4>{ translate( 'Phone' ) }</h4>
-						<span>{ billing.phone }</span>
-					</div>
-
-					<div className="order-customer__shipping">
-						<h3 className="order-customer__shipping-details">{ translate( 'Shipping Details' ) }</h3>
-						<h4>{ translate( 'Address' ) }</h4>
-						<div className="order-customer__shipping-address">
-							<p>{ `${ shipping.first_name } ${ shipping.last_name }` }</p>
-							<p>{ shipping.address_1 }</p>
-							<p>{ shipping.address_2 }</p>
-							<p>{ `${ shipping.city }, ${ shipping.state } ${ shipping.postcode }` }</p>
-							<p>{ shipping.country }</p>
+						<div className="order-customer__shipping">
+							<h3 className="order-customer__shipping-details">
+								{ translate( 'Shipping Details' ) }
+							</h3>
+							<h4>{ translate( 'Address' ) }</h4>
+							<div className="order-customer__shipping-address">
+								<p>{ `${ shipping.first_name } ${ shipping.last_name }` }</p>
+								<AddressView address={ this.getAddressViewFormat( shipping ) } />
+							</div>
 						</div>
 					</div>
-				</div>
 				</Card>
 			</div>
 		);
