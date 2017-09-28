cfc4b904e27b3ed1b43b34c2d279d031d05575c4
Grzegorz (Greg) Ziółkowski <grzegorz@gziolo.pl>
2017-09-28 12:13:34 +0200

    Testing: Update documentation to reflect migration to Jest (#16943)
    * Testing: Update documentation to reflect migration to Jest
    
    * Testing: Add testing overview documentation

--- test/test/helpers/use-nock/index.js
+++ test/test/helpers/use-nock/index.js
@@ -6,6 +6,10 @@ export { nock };
 
 const log = debug( 'calypso:test:use-nock' );
 
+/**
+ * @param {Function} setupCallback Function executed before all tests are run.
+ * @deprecated Use nock directly instead.
+ */
 export const useNock = ( setupCallback ) => {
 	if ( setupCallback ) {
 		before( partial( setupCallback, nock ) );
