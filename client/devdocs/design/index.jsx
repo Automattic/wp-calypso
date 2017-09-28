9cb0c3d1284e35483c390a4c0759a0e22d391830
Marin Atanasov <tyxla@users.noreply.github.com>
2017-09-28 12:06:22 +0300

    DevDocs: Set page title in all of the sections (#17939)
    * DevDocs: Use renderWithReduxStore for main sections
    
    * DevDocs: Set page title in all main sections

--- client/devdocs/design/index.jsx
+++ client/devdocs/design/index.jsx
@@ -14,6 +14,7 @@ import { trim } from 'lodash';
  * Internal dependencies
  */
 import config from 'config';
+import DocumentHead from 'components/data/document-head';
 import fetchComponentsUsageStats from 'state/components-usage-stats/actions';
 import HeaderCake from 'components/header-cake';
 import Main from 'components/main';
@@ -109,6 +110,8 @@ class DesignAssets extends React.Component {
 
 		return (
 			<Main className="design">
+				<DocumentHead title="UI Components" />
+
 				{ component ? (
 					<HeaderCake onClick={ this.backToComponents } backText="All Components">
 						{ slugToCamelCase( component ) }
