2e3e5f933fc68ed6fb4806a1727ed9edfbe53014
Allen Snook <allen@allendav.com>
2017-09-29 16:39:40 -0700

    Remove debug scaffolding for google analytics

--- client/my-sites/site-settings/form-analytics.jsx
+++ client/my-sites/site-settings/form-analytics.jsx
@@ -7,9 +7,6 @@ import config from 'config';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { find, flowRight, partialRight, pick, overSome } from 'lodash';
-import debugFactory from 'debug';
-
-const debug = debugFactory( 'calypso:google-analytics' );
 
 /**
  * Internal dependencies
@@ -125,12 +122,6 @@ class GoogleAnalyticsForm extends Component {
 			config.isEnabled( 'jetpack/google-analytics-anonymize-ip' ) &&
 			siteIsJetpack &&
 			siteSupportsIPAnonymization;
-
-		debug( 'siteIsJetpack', siteIsJetpack );
-		debug( 'siteSupportsGoogleAnalyticsBasicEcommerceTracking', siteSupportsBasicEcommerceTracking );
-		debug( 'siteSupportsGoogleAnalyticsIPAnonymization', siteSupportsIPAnonymization );
-		debug( 'showAnalyticsForStores', showAnalyticsForStores );
-		debug( 'showAnonymizeIP', showAnonymizeIP );
 
 		return (
 			<form id="site-settings" onSubmit={ handleSubmitForm }>
