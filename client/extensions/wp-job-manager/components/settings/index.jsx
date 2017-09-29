faeb4395b01eec94b0e2767c63a14fc598d4d854
Donna Peplinskie <donnapep@gmail.com>
2017-09-29 10:14:17 -0400

    Redirect from settings to plugin details page if active or version checks fail

--- client/extensions/wp-job-manager/components/settings/index.jsx
+++ client/extensions/wp-job-manager/components/settings/index.jsx
@@ -14,6 +14,7 @@ import { flowRight } from 'lodash';
  * Internal dependencies
  */
 import DocumentHead from 'components/data/document-head';
+import ExtensionRedirect from 'blocks/extension-redirect';
 import Main from 'components/main';
 import Navigation from '../navigation';
 import QuerySettings from '../data/query-settings';
@@ -40,6 +41,10 @@ class Settings extends Component {
 
 		return (
 			<Main className={ mainClassName }>
+				<ExtensionRedirect
+					minimumVersion="1.28.0"
+					pluginId="wp-job-manager"
+					siteId={ siteId } />
 				<SetupRedirect siteId={ siteId } />
 				<QuerySettings siteId={ siteId } />
 				<DocumentHead title={ translate( 'WP Job Manager' ) } />
