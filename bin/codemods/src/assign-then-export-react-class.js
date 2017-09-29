af2116cb2b2a51133c284c130fbe7d9af6cf5e79
spen <spen_@hotmail.co.uk>
2017-09-29 13:35:42 +0100

    Codemods: Update assign-then-export-react-class formatting - to be squashed

--- bin/codemods/src/assign-then-export-react-class.js
+++ bin/codemods/src/assign-then-export-react-class.js
@@ -1,52 +1,56 @@
+/**
+ * External dependencies
+ */
 const { get } = require( 'lodash' );
 
-const classIdentifier = {
-  type: 'CallExpression',
-  callee: {
-    type: 'MemberExpression',
-    object: {
-      name: 'React',
+/**
+ * Module variables
+ */
+const createClassIdentifier = {
+    type: 'CallExpression',
+    callee: {
+        type: 'MemberExpression',
+        object: {
+            name: 'React',
+        },
+        property: {
+            name: 'createClass',
+        }
     },
-    property: {
-      name: 'createClass',
-    }
-  },
 };
 
 const displayNamePropertyIdentifier = {
-  key: {
-    name: 'displayName',
-  },
+    key: {
+        name: 'displayName',
+    },
 };
 
 const DEFAULT_EXPORT_NAME = '__CHANGE_ME__';
 
 export default function transformer( file, api ) {
-  const j = api.jscodeshift;
+    const j = api.jscodeshift;
 
-  const exportDefaultDeclaration = j( file.source )
-    .find( j.ExportDefaultDeclaration, {
-	   declaration: classIdentifier,
+    const exportDefaultDeclaration = j( file.source )
+        .find( j.ExportDefaultDeclaration, {
+	         declaration: createClassIdentifier,
+        } );
+
+    const displayNamePaths = exportDefaultDeclaration.find( j.Property, displayNamePropertyIdentifier );
+    const displayNameValue = displayNamePaths.length && displayNamePaths.at( 0 ).get( 'value' );
+    const displayName = get( displayNameValue, 'value.value', DEFAULT_EXPORT_NAME );
+
+    exportDefaultDeclaration.replaceWith( node => (
+        j.variableDeclaration( 'const', [
+            j.variableDeclarator(
+                j.identifier( displayName ),
+                node.get( 'declaration' ).value
+            )
+        ] )
+    ) );
+
+    exportDefaultDeclaration.insertAfter( 'export default ' + displayName + ';' );
+
+    return exportDefaultDeclaration.toSource( {
+        useTabs: true,
     } );
-
-  const displayNamePaths = exportDefaultDeclaration.find( j.Property, displayNamePropertyIdentifier );
-  const displayNameValue = displayNamePaths.length && displayNamePaths.at( 0 ).get( 'value' );
-  const displayName = get( displayNameValue, 'value.value', DEFAULT_EXPORT_NAME );
-
-  exportDefaultDeclaration.replaceWith( node => {
-    const declaration = node.get( 'declaration' );
-
-    return j.variableDeclaration( 'const', [
-      j.variableDeclarator(
-        j.identifier( displayName ),
-        declaration.value
-      )
-    ] );
-  } );
-
-  exportDefaultDeclaration.insertAfter( 'export default ' + displayName + ';' );
-
-  return exportDefaultDeclaration.toSource( {
-    useTabs: true,
-  } );
 }
