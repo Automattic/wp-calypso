6abbe230272198b825894c886a6d65d170337957
Kevin Killingsworth <kevin@redfenix.com>
2017-09-29 18:00:37 -0500

    Store Promotions: Move list state up a level
    This moves the list page states up a level to make room for edit states.

--- client/extensions/woocommerce/state/ui/promotions/test/list-reducer.js
+++ client/extensions/woocommerce/state/ui/promotions/test/list-reducer.js
@@ -24,35 +24,11 @@ describe( 'reducer', () => {
 		const action = {
 			type: WOOCOMMERCE_PROMOTIONS_PAGE_SET,
 			currentPage: 6,
-			perPage: 30,
+			perPage: 10,
 		};
 		const state = reducer( undefined, action );
 
 		expect( state.currentPage ).to.equal( 6 );
-		expect( state.perPage ).to.equal( 30 );
-	} );
-
-	it( 'should ignore an invalid page number', () => {
-		const action = {
-			type: WOOCOMMERCE_PROMOTIONS_PAGE_SET,
-			currentPage: 0,
-			perPage: 30,
-		};
-		const state = reducer( undefined, action );
-
-		expect( state.currentPage ).to.equal( 1 );
-		expect( state.perPage ).to.equal( 30 );
-	} );
-
-	it( 'should ignore an invalid perPage', () => {
-		const action = {
-			type: WOOCOMMERCE_PROMOTIONS_PAGE_SET,
-			currentPage: 4,
-			perPage: 0,
-		};
-		const state = reducer( undefined, action );
-
-		expect( state.currentPage ).to.equal( 4 );
 		expect( state.perPage ).to.equal( 10 );
 	} );
 } );
