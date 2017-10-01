e7afac5ebdab649114da532b450abb6251615732
Yury Michurin <yury@automattic.com>
2017-10-01 12:03:55 +0300

    Suggestion to refactor bundle loader

---
+++ server/bundler/loader_template.js
@@ -0,0 +1,94 @@
+var config = require( 'config' ),
+	page = require( 'page' ),
+	React = require( 'react' ),
+	activateNextLayoutFocus = require( 'state/ui/layout-focus/actions' ).activateNextLayoutFocus,
+	LoadingError = require( 'layout/error' ),
+	controller = require( 'controller' ),
+	restoreLastSession = require( 'lib/restore-last-path' ).restoreLastSession,
+	preloadHub = require( 'sections-preload' ).hub;
+
+var sections = ___DEFINITION___;
+var _loadedSections = {};
+
+function preload( sectionName ) {
+	console.log( sectionName );
+
+	switch ( sectionName ) {
+		___LOADERS___
+	}
+}
+
+preloadHub.on( 'preload', preload );
+
+function pathToRegExp( path ) {
+	// Prevents root level double dash urls from being validated.
+	if ( path === '/' ) {
+		return path;
+	}
+	return new RegExp( '^' + path + '(/.*)?$' );
+}
+
+function getPathRegex( pathString ) {
+	if ( pathString === '/' ) {
+		return pathString;
+	}
+	const regex = pathToRegExp( pathString );
+	return '/' + regex.toString().slice( 1, -1 ) + '/';
+}
+
+function createPageDefinition( path, sectionDefinition ) {
+	page( getPathRegex( path ), function( context, next ) {
+		const envId = sectionDefinition.envId;
+
+		if ( envId && envId.indexOf( config( "env_id" ) ) === -1 ) {
+			return next();
+		}
+
+		if ( _loadedSections[ sectionDefinition.module ] ) {
+			controller.setSection( sectionDefinition )( context );
+			context.store.dispatch( activateNextLayoutFocus() );
+			return next();
+		}
+
+		if ( config.isEnabled( "restore-last-location" ) && restoreLastSession( context.path ) ) {
+			return;
+		}
+
+		context.store.dispatch( { type: "SECTION_SET", isLoading: true } );
+
+		preload( sectionDefinition.name ).then( module => {
+			context.store.dispatch( { type: "SECTION_SET", isLoading: false } );
+			controller.setSection( sectionDefinition )( context );
+
+			if ( ! _loadedSections[ sectionDefinition.module ] ) {
+				module( controller.clientRouter );
+				_loadedSections[ sectionDefinition.module ] = true;
+			}
+
+			context.store.dispatch( activateNextLayoutFocus() );
+
+			next();
+		}, error => {
+			if ( ! LoadingError.isRetry() ) {
+				LoadingError.retry( sectionDefinition.name );
+			} else {
+				console.error( error );
+				context.store.dispatch( { type: "SECTION_SET", isLoading: false } );
+				LoadingError.show( sectionDefinition.name );
+			}
+
+			return Promise.reject( error );
+		} );
+	} );
+}
+
+module.exports = {
+	get: function() {
+		return sections;
+	},
+	load: function() {
+		sections.forEach( sectionDefinition => {
+			sectionDefinition.paths.forEach( path => createPageDefinition( path, sectionDefinition ) );
+		} );
+	}
+};
