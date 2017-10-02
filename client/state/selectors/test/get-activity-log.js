38c987f272a84f993a4a2562d87de72c77ad2fc3
Jon Surrell <jon.surrell@automattic.com>
2017-10-02 11:59:23 +0200

    Add get-activity-log selector

---
+++ client/state/selectors/test/get-activity-log.js
@@ -0,0 +1,69 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { getActivityLog } from '..';
+import ActivityQueryManager from 'lib/query-manager/activity';
+
+const SITE_ID = 1234;
+
+describe( 'getActivityLog()', () => {
+	it( 'should return null if there is no data', () => {
+		const result = getActivityLog(
+			{
+				activityLog: {
+					logItems: {},
+				},
+			},
+			SITE_ID
+		);
+
+		expect( result ).to.be.null;
+	} );
+
+	it( 'should return null if the activityId is not found', () => {
+		const activity = { activityId: 'fooBarBaz', activityTs: 1 };
+
+		const result = getActivityLog(
+			{
+				activityLog: {
+					logItems: {
+						[ SITE_ID ]: new ActivityQueryManager().receive( [ activity ], {
+							found: 1,
+						} ),
+					},
+				},
+			},
+			'nonExistentId'
+		);
+
+		expect( result ).to.be.null;
+	} );
+
+	it( 'should return log by id', () => {
+		const activityId = 'fooBarBaz';
+		const activity = { activityId: 'a', activityTs: 1 };
+		const items = [ { activityId: 'anotherActivity', activityTs: 2 }, activity ];
+
+		const result = getActivityLog(
+			{
+				activityLog: {
+					logItems: {
+						[ SITE_ID ]: new ActivityQueryManager().receive( items, {
+							found: items.length,
+						} ),
+					},
+				},
+			},
+			SITE_ID,
+			activityId
+		);
+
+		expect( result ).to.deep.equal( activity );
+	} );
+} );
