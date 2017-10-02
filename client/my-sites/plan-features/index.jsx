33943478893ec4034a98b489fe2c3a37ec147ba7
Rastislav Lamos <lamos.rasto@gmail.com>
2017-10-02 20:47:28 +0200

    PlanFeatures: plan btn primary if it's the only shown plan

--- client/my-sites/plan-features/index.jsx
+++ client/my-sites/plan-features/index.jsx
@@ -610,8 +610,9 @@ export default connect(
 					primaryUpgrade: (
 						( currentPlan === PLAN_PERSONAL && plan === PLAN_PREMIUM ) ||
 						( currentPlan === PLAN_PREMIUM && plan === PLAN_BUSINESS ) ||
-						popular,
-						newPlan
+						popular ||
+						newPlan ||
+						plans.length === 1
 					),
 					rawPrice: getPlanRawPrice( state, planProductId, showMonthlyPrice ),
 					relatedMonthlyPlan: relatedMonthlyPlan
