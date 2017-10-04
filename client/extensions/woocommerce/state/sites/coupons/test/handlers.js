e603624e86c0b0aba99d8501ed86cc4644e7a22b
Kevin Killingsworth <kevin@redfenix.com>
2017-10-04 06:18:32 -0500

    Store Coupons: Add create/update/delete handlers
    This adds data-layer handler code to create/update/delete coupons.

--- client/extensions/woocommerce/state/sites/coupons/test/handlers.js
+++ client/extensions/woocommerce/state/sites/coupons/test/handlers.js
@@ -9,12 +9,30 @@ import { spy, match } from 'sinon';
 /**
  * Internal dependencies
  */
-import { fetchCoupons, couponsUpdated } from '../actions';
-import { requestCoupons, requestCouponsSuccess } from '../handlers';
 import { WPCOM_HTTP_REQUEST } from 'state/action-types';
+import {
+	fetchCoupons,
+	couponsUpdated,
+	createCoupon,
+	updateCoupon,
+	deleteCoupon,
+} from '../actions';
+import {
+	requestCoupons,
+	requestCouponsSuccess,
+	couponCreate,
+	couponCreateSuccess,
+	couponUpdate,
+	couponUpdateSuccess,
+	couponDelete,
+	couponDeleteSuccess,
+} from '../handlers';
 
 describe( 'handlers', () => {
 	const siteId = 123;
+
+	const successAction = { type: '%%SUCCESS%%' };
+	const failureAction = { type: '%%FAILURE%%' };
 
 	describe( '#requestCoupons', () => {
 		test( 'should dispatch a request', () => {
@@ -94,6 +112,116 @@ describe( 'handlers', () => {
 			response.data.body = body4;
 			requestCouponsSuccess( store, action, response );
 			expect( store.dispatch ).to.not.have.been.called;
+		} );
+	} );
+
+	describe( '#createCoupon', () => {
+		it( 'should dispatch a request', () => {
+			const store = {
+				dispatch: spy(),
+			};
+
+			const coupon = { id: { placeholder: 'coupon:5' }, code: '10off', amount: '10', discount_type: 'percent' };
+			const action = createCoupon( siteId, coupon, successAction, failureAction );
+
+			couponCreate( store, action );
+
+			expect( store.dispatch ).to.have.been.calledWith( match( {
+				type: WPCOM_HTTP_REQUEST,
+				body: {
+					path: '/wc/v3/coupons&_method=POST',
+					body: JSON.stringify( coupon ),
+				}
+			} ) );
+		} );
+	} );
+
+	describe( '#couponCreateSuccess', () => {
+		it( 'should dispatch a success action', () => {
+			const store = {
+				dispatch: spy(),
+			};
+
+			const coupon = { id: { placeholder: 'coupon:5' }, code: '10off', amount: '10', discount_type: 'percent' };
+			const action = createCoupon( siteId, coupon, successAction, failureAction );
+
+			couponCreateSuccess( store, action, { data: { ...coupon, id: 12 } } );
+			expect( store.dispatch ).to.have.been.calledWith( match( {
+				type: successAction.type,
+			} ) );
+		} );
+	} );
+
+	describe( '#updateCoupon', () => {
+		it( 'should dispatch a request', () => {
+			const store = {
+				dispatch: spy(),
+			};
+
+			const coupon = { id: 5, code: '15off', amount: '15', discount_type: 'percent' };
+			const action = updateCoupon( siteId, coupon, successAction, failureAction );
+
+			couponUpdate( store, action );
+
+			expect( store.dispatch ).to.have.been.calledWith( match( {
+				type: WPCOM_HTTP_REQUEST,
+				body: {
+					path: '/wc/v3/coupons/5&_method=PUT',
+					body: JSON.stringify( coupon ),
+				}
+			} ) );
+		} );
+	} );
+
+	describe( '#couponUpdateSuccess', () => {
+		it( 'should dispatch a success action', () => {
+			const store = {
+				dispatch: spy(),
+			};
+
+			const coupon = { id: 5, code: '15off', amount: '15', discount_type: 'percent' };
+			const action = updateCoupon( siteId, coupon, successAction, failureAction );
+
+			couponUpdateSuccess( store, action, { data: { ...coupon, id: 12 } } );
+			expect( store.dispatch ).to.have.been.calledWith( match( {
+				type: successAction.type,
+			} ) );
+		} );
+	} );
+
+	describe( '#deleteCoupon', () => {
+		it( 'should dispatch a request', () => {
+			const store = {
+				dispatch: spy(),
+			};
+
+			const couponId = 15;
+			const action = deleteCoupon( siteId, couponId, successAction, failureAction );
+
+			couponDelete( store, action );
+
+			expect( store.dispatch ).to.have.been.calledWith( match( {
+				type: WPCOM_HTTP_REQUEST,
+				body: {
+					path: '/wc/v3/coupons/15&_method=DELETE',
+				}
+			} ) );
+		} );
+	} );
+
+	describe( '#couponDeleteSuccess', () => {
+		it( 'should dispatch a success action', () => {
+			const store = {
+				dispatch: spy(),
+			};
+
+			const couponId = 15;
+			const action = deleteCoupon( siteId, couponId, successAction, failureAction );
+
+			couponDeleteSuccess( store, action );
+			expect( store.dispatch ).to.have.been.calledWith( match( {
+				type: successAction.type,
+			} ) );
 		} );
 	} );
 } );
