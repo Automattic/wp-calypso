72e515b65e349b6903d7d978a480d05c1541b77d
Kevin Killingsworth <kevin@redfenix.com>
2017-10-04 06:18:32 -0500

    Store Coupons: Add create/update/delete handlers
    This adds data-layer handler code to create/update/delete coupons.

--- client/extensions/woocommerce/state/sites/coupons/actions.js
+++ client/extensions/woocommerce/state/sites/coupons/actions.js
@@ -3,8 +3,10 @@
  *
  * @format
  */
-
 import {
+	WOOCOMMERCE_COUPON_CREATE,
+	WOOCOMMERCE_COUPON_DELETE,
+	WOOCOMMERCE_COUPON_UPDATE,
 	WOOCOMMERCE_COUPONS_REQUEST,
 	WOOCOMMERCE_COUPONS_UPDATED,
 } from 'woocommerce/state/action-types';
@@ -19,3 +21,34 @@ export function fetchCoupons( siteId, params ) {
 export function couponsUpdated( siteId, params, coupons, totalPages, totalCoupons ) {
 	return { type: WOOCOMMERCE_COUPONS_UPDATED, siteId, params, coupons, totalPages, totalCoupons };
 }
+
+export function createCoupon( siteId, coupon, successAction, failureAction ) {
+	return {
+		type: WOOCOMMERCE_COUPON_CREATE,
+		siteId,
+		coupon,
+		successAction,
+		failureAction,
+	};
+}
+
+export function updateCoupon( siteId, coupon, successAction, failureAction ) {
+	return {
+		type: WOOCOMMERCE_COUPON_UPDATE,
+		siteId,
+		coupon,
+		successAction,
+		failureAction,
+	};
+}
+
+export function deleteCoupon( siteId, couponId, successAction, failureAction ) {
+	return {
+		type: WOOCOMMERCE_COUPON_DELETE,
+		siteId,
+		couponId,
+		successAction,
+		failureAction,
+	};
+}
+
