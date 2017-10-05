Automattic/wp-calypso

commit 112c7f3c62e8c089dd2756fe40de99fb90e50b4e

Framework: Run 'imports' codemod on `client/` (#18359)
Plus subsequent fixes.

client/my-sites/checkout/checkout/pay-button.jsx
@@ -1,17 +1,18 @@
 /**
  * External dependencies
  */
-var React = require( 'react' );
+import React from 'react';
 
 /**
  * Internal dependencies
  */
-var cartValues = require( 'lib/cart-values' ),
-	cartItems = cartValues.cartItems,
-	hasFreeTrial = cartItems.hasFreeTrial,
-	isPaidForFullyInCredits = cartValues.isPaidForFullyInCredits,
-	SubscriptionText = require( './subscription-text' ),
-	transactionStepTypes = require( 'lib/store-transactions/step-types' );
+import {
+	cartItems,
+	hasFreeTrial,
+	isPaidForFullyInCredits,
+} from 'lib/cart-values';
+import SubscriptionText from './subscription-text';
+import transactionStepTypes from 'lib/store-transactions/step-types';
 
 var PayButton = React.createClass( {
 	buttonState: function() {

commit 40fe515a2c91c57cacdfdccd2cfa5710132b1143

Checkout: Fix hasFreeTrial import in pay-button.jsx (#18571)
Broken by #18359 (https://github.com/Automattic/wp-calypso/pull/18359/files#diff-5dca9d3f8f0e140509c204503492fbbbL11), reported by @lamosty in Slack.

client/my-sites/checkout/checkout/pay-button.jsx
@@ -8,7 +8,6 @@ import React from 'react';
  */
 import {
 	cartItems,
-	hasFreeTrial,
 	isPaidForFullyInCredits,
 } from 'lib/cart-values';
 import SubscriptionText from './subscription-text';
@@ -123,7 +122,7 @@ var PayButton = React.createClass( {
 
 	completing: function() {
 		var text;
-		if ( hasFreeTrial( this.props.cart ) ) {
+		if ( cartItems.hasFreeTrial( this.props.cart ) ) {
 			text = this.translate( 'Starting your free trial…', { context: 'Loading state on /checkout' } )
 		} else {
 			text = this.translate( 'Completing your purchase', { context: 'Loading state on /checkout' } )