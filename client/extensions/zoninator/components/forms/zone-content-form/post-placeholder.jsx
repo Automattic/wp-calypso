008c22d1f69fa86e945cc10eae2a437e8668c6d1
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-10-05 18:03:31 +0200

    Improve loading experience on 'Edit Zone' view

---
+++ client/extensions/zoninator/components/forms/zone-content-form/post-placeholder.jsx
@@ -0,0 +1,17 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import SectionHeader from 'components/section-header';
+
+const PostPlaceholder = () => {
+	const className = 'zoninator__zone-list-item is-placeholder';
+
+	return <SectionHeader className={ className } />;
+};
+
+export default PostPlaceholder;
