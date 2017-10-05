c46ec99d0ef1031139ad7dbecb5787b33971581f
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-10-05 17:59:18 +0200

    Add 'requesting' state for zone feeds

--- client/extensions/zoninator/state/feeds/test/reducer.js
+++ client/extensions/zoninator/state/feeds/test/reducer.js
@@ -8,11 +8,66 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import reducer, { items } from '../reducer';
+import {
+	ZONINATOR_REQUEST_FEED,
+	ZONINATOR_REQUEST_FEED_ERROR,
+	ZONINATOR_UPDATE_FEED,
+} from '../../action-types';
+import reducer, { items, requesting } from '../reducer';
 
 describe( 'reducer', () => {
-	test( 'should export expected reducer keys', () => {
-		expect( reducer( undefined, {} ) ).to.have.keys( [ 'items' ] );
+	it( 'should export expected reducer keys', () => {
+		expect( reducer( undefined, {} ) ).to.have.keys( [ 'requesting', 'items' ] );
+	} );
+
+	describe( 'requesting()', () => {
+		it( 'should default to an empty object', () => {
+			const state = requesting( undefined, {} );
+
+			expect( state ).to.deep.equal( {} );
+		} );
+
+		it( 'should set state to true if a feed is being requested', () => {
+			const state = requesting( undefined, {
+				type: ZONINATOR_REQUEST_FEED,
+				siteId: 123,
+				zoneId: 456,
+			} );
+
+			expect( state ).to.deep.equal( {
+				[ 123 ]: {
+					[ 456 ]: true,
+				},
+			} );
+		} );
+
+		it( 'should set state to false if updating a feed', () => {
+			const state = requesting( undefined, {
+				type: ZONINATOR_UPDATE_FEED,
+				siteId: 123,
+				zoneId: 456,
+			} );
+
+			expect( state ).to.deep.equal( {
+				[ 123 ]: {
+					[ 456 ]: false,
+				},
+			} );
+		} );
+
+		it( 'should set state to false if a feed could not be fetched', () => {
+			const state = requesting( undefined, {
+				type: ZONINATOR_REQUEST_FEED_ERROR,
+				siteId: 123,
+				zoneId: 456,
+			} );
+
+			expect( state ).to.deep.equal( {
+				[ 123 ]: {
+					[ 456 ]: false,
+				},
+			} );
+		} );
 	} );
 
 	describe( 'items()', () => {
