a5aec0bab9e78058a1bb52af1b21db1b26ffe992
Bernhard Reiter <ockham@raz.or.at>
2017-10-06 12:18:50 +0200

    TinyMCE: ContactForm View: Prettify

--- client/components/tinymce/plugins/wpcom-view/views/contact-form/index.jsx
+++ client/components/tinymce/plugins/wpcom-view/views/contact-form/index.jsx
@@ -14,41 +14,43 @@ import { deserialize } from 'components/tinymce/plugins/contact-form/shortcode-u
 import shortcodeUtils from 'lib/shortcode';
 import renderField from './preview-fields';
 
-export default localize(React.createClass({
-	displayName: 'ContactForm',
+export default localize(
+	React.createClass( {
+		displayName: 'ContactForm',
 
-	statics: {
-		match( content ) {
-			const match = shortcodeUtils.next( 'contact-form', content );
+		statics: {
+			match( content ) {
+				const match = shortcodeUtils.next( 'contact-form', content );
 
-			if ( match ) {
-				return {
-					index: match.index,
-					content: match.content,
-					options: {
-						shortcode: match.shortcode,
-					},
-				};
-			}
+				if ( match ) {
+					return {
+						index: match.index,
+						content: match.content,
+						options: {
+							shortcode: match.shortcode,
+						},
+					};
+				}
+			},
+
+			serialize( content ) {
+				return encodeURIComponent( content );
+			},
+
+			edit( editor, content ) {
+				editor.execCommand( 'wpcomContactForm', content );
+			},
 		},
 
-		serialize( content ) {
-			return encodeURIComponent( content );
+		render() {
+			const { fields } = deserialize( this.props.content );
+
+			return (
+				<div className="wpview-content wpview-type-contact-form">
+					{ [].concat( fields ).map( renderField ) }
+					<button disabled>{ this.props.translate( 'Submit' ) }</button>
+				</div>
+			);
 		},
-
-		edit( editor, content ) {
-			editor.execCommand( 'wpcomContactForm', content );
-		},
-	},
-
-	render() {
-		const { fields } = deserialize( this.props.content );
-
-		return (
-            <div className="wpview-content wpview-type-contact-form">
-				{ [].concat( fields ).map( renderField ) }
-				<button disabled>{ this.props.translate( 'Submit' ) }</button>
-			</div>
-        );
-	},
-}));
+	} )
+);
