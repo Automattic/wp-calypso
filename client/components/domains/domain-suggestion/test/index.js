aee4f9ef7bcdd99623d5a2ef42e1b45ff0f56e17
Bernhard Reiter <ockham@raz.or.at>
2017-10-06 11:30:03 +0200

    DomainSuggestion: Remove translate stub from test

--- client/components/domains/domain-suggestion/test/index.js
+++ client/components/domains/domain-suggestion/test/index.js
@@ -8,19 +8,15 @@
  */
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import { identity, noop } from 'lodash';
+import { noop } from 'lodash';
 import React from 'react';
 
-jest.mock( 'components/plans/premium-popover', () => require( 'components/empty-component' ) );
+/**
+ * Internal dependencies
+ */
+import DomainSuggestion from 'components/domains/domain-suggestion';
 
 describe( 'Domain Suggestion', function() {
-	let DomainSuggestion;
-
-	before( () => {
-		DomainSuggestion = require( 'components/domains/domain-suggestion' );
-		DomainSuggestion.prototype.translate = identity;
-	} );
-
 	describe( 'has attributes', () => {
 		it( 'should have data-e2e-domain attribute for e2e testing', () => {
 			const domainSuggestion = shallow(
