eff8820bb3f174df535079237f79a5c8abe14187
Steve Seear <steve.seear@gmail.com>
2017-10-06 13:00:34 +0100

    Jetpack plans: Make the free plan button clearer (#18494)
    Fixes #18357.
    
    Make the free plan option more obvious with clearer wording and button instead of link styling.

--- client/jetpack-connect/plans-skip-button.jsx
+++ client/jetpack-connect/plans-skip-button.jsx
@@ -9,11 +9,13 @@ import { localize } from 'i18n-calypso';
  * Internal dependencies
  */
 import Button from 'components/button';
+import Gridicon from 'gridicons';
 
-const PlansSkipButton = ( { onClick, translate } ) => (
+const PlansSkipButton = ( { onClick, isRtl, translate } ) => (
 	<div className="jetpack-connect__plans-nav-buttons">
-		<Button onClick={ onClick } compact borderless>
-			{ translate( 'Skip' ) }
+		<Button onClick={ onClick }>
+			{ translate( 'Start with free' ) }
+			<Gridicon icon={ isRtl ? 'arrow-left' : 'arrow-right' } />
 		</Button>
 	</div>
 );
