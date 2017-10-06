54239b91ef48f1320571dd56ad302a7b791bb37c
Bernhard Reiter <ockham@raz.or.at>
2017-10-06 12:43:55 +0200

    Placate ESLint

--- client/components/tinymce/plugins/wpcom-view/views.js
+++ client/components/tinymce/plugins/wpcom-view/views.js
@@ -20,7 +20,7 @@ import { isEnabled } from 'config';
 /**
  * Module variables
  */
-let views = {
+const views = {
 	gallery: GalleryView,
 	embed: new EmbedViewManager(),
 	contactForm: ContactFormView,
@@ -52,7 +52,7 @@ export default {
 	 * @return {String} The string with markers.
 	 */
 	setMarkers( content ) {
-		var pieces = [ { content: content } ],
+		let pieces = [ { content: content } ],
 			current;
 
 		forEach( views, function( view, type ) {
@@ -60,7 +60,7 @@ export default {
 			pieces = [];
 
 			forEach( current, function( piece ) {
-				var remaining = piece.content,
+				let remaining = piece.content,
 					result;
 
 				// Ignore processed pieces, but retain their location.
