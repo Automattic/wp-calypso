66d1d4c1c9a0291feb9f42747bdb77b8b51b3f7d
James Nylen <jnylen@gmail.com>
2017-10-06 16:18:08 -0500

    Add debug info that's helpful for tracking down `lastPage` issues

--- client/lib/query-manager/index.js
+++ client/lib/query-manager/index.js
@@ -223,6 +223,7 @@ export default class QueryManager {
 	 *                                             same instance otherwise
 	 */
 	receive( items = [], options = {} ) {
+		console.warn( 'receive', items, options );
 		// Coerce received single item to array
 		if ( ! Array.isArray( items ) ) {
 			items = [ items ];
