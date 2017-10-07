3f23b6e339693c4ba9a540addab372a1700d9c3a
Andrés Maneiro <andres.maneiro@automattic.com>
2017-10-07 03:03:36 +0200

    State: blur and focus actions moved to happychat/ui

--- client/state/happychat/actions.js
+++ client/state/happychat/actions.js
@@ -3,12 +3,9 @@
 /**
  * Internal dependencies
  */
-import { HAPPYCHAT_BLUR, HAPPYCHAT_FOCUS, HAPPYCHAT_SET_CHAT_STATUS } from 'state/action-types';
+import { HAPPYCHAT_SET_CHAT_STATUS } from 'state/action-types';
 
 export const setHappychatChatStatus = status => ( {
 	type: HAPPYCHAT_SET_CHAT_STATUS,
 	status,
 } );
-
-export const blur = () => ( { type: HAPPYCHAT_BLUR } );
-export const focus = () => ( { type: HAPPYCHAT_FOCUS } );
