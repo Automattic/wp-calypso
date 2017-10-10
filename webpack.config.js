177f6ed767f94ab8e3e8915a1b923f076e0bc131
Jake F - @samouri <samouri@users.noreply.github.com>
2017-10-10 03:21:24 -0400

    remove wpcom that was causing issues bc autoddl wont accept anyting using node built-ins (fs)

--- webpack.config.js
+++ webpack.config.js
@@ -186,7 +186,6 @@ const vendorLibs = [
 	'i18n-calypso',
 	'lodash',
 	'moment',
-	'fs',
 	'page',
 	'react',
 	'create-react-class',
@@ -197,7 +196,7 @@ const vendorLibs = [
 	'redux-thunk',
 	'store',
 	'superagent',
-	'wpcom',
+	// 'wpcom',
 ];
 
 if ( calypsoEnv === 'desktop' ) {
@@ -213,7 +212,7 @@ if ( calypsoEnv === 'desktop' ) {
 	// it extracts the "runtime" so that the frequently changing mapping doesn't break caching of the entry chunks
 	// NOTE: order matters. vendor must be before manifest.
 	webpackConfig.plugins = webpackConfig.plugins.concat( [
-		new webpack.optimize.CommonsChunkPlugin( { name: 'vendor', minChunks: Infinity } ),
+		// new webpack.optimize.CommonsChunkPlugin( { name: 'vendor', minChunks: Infinity } ),
 		new webpack.optimize.CommonsChunkPlugin( { name: 'manifest' } )
 	] );
 
