ea1f3cbe40b7905a274c827da40e5fb209abe5ac
Jon Surrell <jon.surrell@automattic.com>
2017-10-10 14:00:21 +0200

    Move constant into constants file

--- client/me/help/help-courses/index.jsx
+++ client/me/help/help-courses/index.jsx
@@ -4,9 +4,9 @@
  * @format
  */
 import React, { Component } from 'react';
-import i18n, { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
 import { find } from 'lodash';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -17,6 +17,7 @@ import Main from 'components/main';
 import QueryUserPurchases from 'components/data/query-user-purchases';
 import { getCurrentUserId } from 'state/current-user/selectors';
 import { getHelpCourses } from 'state/help/courses/selectors';
+import { helpCourses } from './constants';
 import { PLAN_BUSINESS } from 'lib/plans/constants';
 import { receiveHelpCourses } from 'state/help/courses/actions';
 import {
@@ -62,43 +63,12 @@ class Courses extends Component {
 	}
 }
 
-function getCourses() {
-	return [
-		{
-			title: i18n.translate(
-				'Everything You Need to Know About SEO and Marketing for Your Website'
-			),
-			description: i18n.translate(
-				'You will leave this 60-minute session with a plan for optimizing your site for search engines ' +
-					'and an overview of basic tactics for increasing your traffic. Our Happiness Engineers will provide a ' +
-					"foundation for making sure that your site meets current SEO standards. We'll also discuss how to " +
-					'maximize tools like Google Analytics and Webmaster Tools.'
-			),
-			video: {
-				youtubeId: 'FU7uxbngrq4',
-			},
-		},
-		{
-			title: i18n.translate( 'How to Make a Business Site on WordPress.com' ),
-			description: i18n.translate(
-				'A 60-minute overview course with two of our Happiness Engineers. In this live group session, ' +
-					'we provide an overview of WordPress.com, discuss features of the WordPress.com Business ' +
-					'plan, provide a basic setup overview to help you get started with your site, and show you ' +
-					'where to find additional resources and help in the future.'
-			),
-			video: {
-				youtubeId: 'S2h_mV0OAcU',
-			},
-		},
-	];
-}
-
 function mapDispatchToProps( dispatch ) {
 	// This function only adds a way of dispatching courses because we don't have another mechanism yet.
 	// Once the courses make it into the API this function should go away in preference for
 	// something linke <QueryHelpCourses />
 	return {
-		fetchCourses: () => dispatch( receiveHelpCourses( getCourses() ) ),
+		fetchCourses: () => dispatch( receiveHelpCourses( helpCourses ) ),
 	};
 }
 
