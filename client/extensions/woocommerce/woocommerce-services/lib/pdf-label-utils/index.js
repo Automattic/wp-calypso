61738f0fd6800350e01bf729bb27367d0dc94462
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-10-10 14:41:13 +0100

    Remove the "preview PDF URL" logic since it's not used in Calypso (and will be retired from WCS)

--- client/extensions/woocommerce/woocommerce-services/lib/pdf-label-utils/index.js
+++ client/extensions/woocommerce/woocommerce-services/lib/pdf-label-utils/index.js
@@ -9,7 +9,6 @@ import { includes, reduce, filter, map } from 'lodash';
  * Internal dependencies
  */
 import * as api from '../../api';
-import getPDFSupport from '../utils/pdf-support';
 
 const PAPER_SIZES = {
 	a4: {
@@ -36,25 +35,15 @@ export const getPaperSizes = ( country ) => (
 	}, {} )
 );
 
-const _getPDFURL = ( paperSize, labels, test = false ) => {
+export const getPrintURL = ( siteId, paperSize, labels ) => {
 	if ( ! PAPER_SIZES[ paperSize ] ) {
 		throw new Error( `Invalid paper size: ${ paperSize }` );
 	}
 	const params = {
 		paper_size: paperSize,
-		//send params as a CSV to avoid conflicts with some plugins out there (#1111)
+		// send params as a CSV to avoid conflicts with some plugins out there (woocommerce-services #1111)
 		label_id_csv: filter( map( labels, 'labelId' ) ).join( ',' ),
 		caption_csv: filter( map( labels, ( l ) => ( l.caption ? encodeURIComponent( l.caption ) : null ) ) ).join( ',' ),
 	};
-	const urlBase = test ? api.url.labelTestPrint() : api.url.labelsPrint();
-
-	return api.createGetUrlWithNonce( urlBase, querystring.stringify( params ) );
-};
-
-export const getPrintURL = ( paperSize, labels ) => {
-	return _getPDFURL( paperSize, labels );
-};
-
-export const getPreviewURL = ( paperSize, labels ) => {
-	return getPDFSupport() ? _getPDFURL( paperSize, labels, true ) : null;
+	return api.url.labelsPrint() + '?' + querystring.stringify( params );
 };
