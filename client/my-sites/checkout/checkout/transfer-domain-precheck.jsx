a68bfbbcf50574c3bc1b457467cdb53b723c543f
Andrija Vucinic <andrija.vucinic@gmail.com>
2017-10-11 02:49:16 +0200

    add text for multiple transfers :)

--- client/my-sites/checkout/checkout/transfer-domain-precheck.jsx
+++ client/my-sites/checkout/checkout/transfer-domain-precheck.jsx
@@ -47,23 +47,19 @@ class TransferDomainPrecheck extends React.PureComponent {
 	};
 
 	refreshStatus = () => {
-		this.setState(
-			{ loading: true }
-		);
+		this.setState( { loading: true } );
 
 		checkInboundTransferStatus( this.props.domain, ( error, result ) => {
 			if ( ! isEmpty( error ) ) {
 				return;
 			}
 
-			this.setState(
-				{
-					email: result.admin_email,
-					privacy: false,
-					unlocked: result.unlocked,
-					loading: false,
-				}
-			);
+			this.setState( {
+				email: result.admin_email,
+				privacy: false,
+				unlocked: result.unlocked,
+				loading: false,
+			} );
 		} );
 	};
 
@@ -71,9 +67,7 @@ class TransferDomainPrecheck extends React.PureComponent {
 		return (
 			<p>
 				{ icon }
-				<span>
-					{ heading }
-				</span>
+				<span>{ heading }</span>
 				{ message }
 			</p>
 		);
@@ -83,12 +77,8 @@ class TransferDomainPrecheck extends React.PureComponent {
 		const { translate } = this.props;
 		const { status } = this.state;
 
-		const icon = status
-			? <Gridicon icon="checkmark-circle" />
-			: <Gridicon icon="notice-outline" />;
-		const heading = status
-			? translate( 'Domain is unlocked.' )
-			: translate( 'Unlock the domain.' );
+		const icon = status ? <Gridicon icon="checkmark-circle" /> : <Gridicon icon="notice-outline" />;
+		const heading = status ? translate( 'Domain is unlocked.' ) : translate( 'Unlock the domain.' );
 		const message = status
 			? translate( 'Your domain is unlocked at your current registrar.' )
 			: translate( 'Please unlock the domain at your current registrar so you can transfer it.' );
@@ -100,28 +90,30 @@ class TransferDomainPrecheck extends React.PureComponent {
 		const { translate } = this.props;
 		const { email, privacy } = this.state;
 
-		const icon = privacy
-			? <Gridicon icon="checkmark-circle" />
-			: <Gridicon icon="notice-outline" />;
+		const icon = privacy ? (
+			<Gridicon icon="checkmark-circle" />
+		) : (
+			<Gridicon icon="notice-outline" />
+		);
 		const heading = privacy
 			? translate( 'Whois privacy is disabled.' )
 			: translate( 'Disable Whois privacy.' );
 		const message = privacy
 			? translate(
-				'We\'ll send an important email to %(email)s to start the domain transfer. If you don\'t recognize' +
-				'this email address you might need to disable Whois privacy to make sure you receive it. After the transfer' +
-				'is complete you can make your registration information private again.',
-				{
-					args: { email }
-				}
-			)
+					"We'll send an important email to %(email)s to start the domain transfer. If you don't recognize" +
+						'this email address you might need to disable Whois privacy to make sure you receive it. After the transfer' +
+						'is complete you can make your registration information private again.',
+					{
+						args: { email },
+					}
+				)
 			: translate(
-				'We\'ll send an important email to %(email)s to start the domain transfer. After the transfer' +
-				'is complete you can enable privacy to hide your registration information again.',
-				{
-					args: { email }
-				}
-			);
+					"We'll send an important email to %(email)s to start the domain transfer. After the transfer" +
+						'is complete you can enable privacy to hide your registration information again.',
+					{
+						args: { email },
+					}
+				);
 
 		return this.getSection( icon, heading, message );
 	}
@@ -133,18 +125,29 @@ class TransferDomainPrecheck extends React.PureComponent {
 		const heading = translate( 'Get domain authorization code.' );
 		const message = translate(
 			'Get an authorization code from your current registrar. This is sometimes ' +
-			'called a secret code, EPP code, or auth code. You will need it to initiate the transfer.'
+				'called a secret code, EPP code, or auth code. You will need it to initiate the transfer.'
 		);
 
 		return this.getSection( icon, heading, message );
 	}
 
 	render() {
-		const { translate } = this.props;
+		const { current, domain, total, translate } = this.props;
+
+		let headerLabel = translate( 'Prepare Domain' );
+		if ( total > 1 ) {
+			headerLabel = translate( 'Prepare Domain %(current)s of %(total)s: %(domain)s', {
+				args: {
+					current: current + 1,
+					total,
+					domain,
+				},
+			} );
+		}
 
 		return (
 			<div className="checkout__transfer-domain-precheck">
-				<SectionHeader label={ translate( 'Prepare Domain' ) }>
+				<SectionHeader label={ headerLabel }>
 					<Button disabled={ this.state.loading } onClick={ this.refreshStatus }>
 						{ translate( 'Refresh Status' ) }
 					</Button>
@@ -153,11 +156,11 @@ class TransferDomainPrecheck extends React.PureComponent {
 					<p>
 						{ translate(
 							'Log in to your current registrar and complete these steps to prepare your domain for transfer. ' +
-							'Changes can take up to X minutes to update. {{a}}Need Help?{{/a}}',
+								'Changes can take up to X minutes to update. {{a}}Need Help?{{/a}}',
 							{
 								components: {
 									a: <a href="#" />,
-								}
+								},
 							}
 						) }
 					</p>
