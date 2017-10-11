1b359d53704986914b6b2e9367fba3544cbfed1b
Kelly Dwan <ryelle@users.noreply.github.com>
2017-10-11 17:24:48 -0400

    Store Orders: Edit customer info on an order (#18579)
    * Connect the customer info component, and add “edit” links on the address details
    
    * Add dialogs to handle editing & saving the customer info
    
    * Enable copy-to-shipping for billing changes
    
    * Switch button style to primary
    
    * Fix label for shipping dialog
    
    * Update proptypes & defaults
    
    * Remove site from the connected props, only grab siteId
    
    * Address edit button styling
    
    * Address modal fieldset margins

--- client/extensions/woocommerce/app/order/order-customer/index.js
+++ client/extensions/woocommerce/app/order/order-customer/index.js
@@ -4,26 +4,82 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import AddressView from 'woocommerce/components/address-view';
+import Button from 'components/button';
 import Card from 'components/card';
+import CustomerAddressDialog from './dialog';
+import { editOrder } from 'woocommerce/state/ui/orders/actions';
+import { isCurrentlyEditingOrder, getOrderWithEdits } from 'woocommerce/state/ui/orders/selectors';
 import getAddressViewFormat from 'woocommerce/lib/get-address-view-format';
+import { getOrder } from 'woocommerce/state/sites/orders/selectors';
+import { getSelectedSiteId } from 'state/ui/selectors';
 import SectionHeader from 'components/section-header';
 
 class OrderCustomerInfo extends Component {
 	static propTypes = {
+		editOrder: PropTypes.func.isRequired,
+		isEditing: PropTypes.bool,
+		orderId: PropTypes.number.isRequired,
 		order: PropTypes.shape( {
 			billing: PropTypes.object.isRequired,
 			shipping: PropTypes.object.isRequired,
 		} ),
+		siteId: PropTypes.number.isRequired,
+	};
+
+	state = {
+		showDialog: false,
+	};
+
+	updateAddress = ( type = 'billing' ) => {
+		const { siteId, order } = this.props;
+		return address => {
+			const { copyToShipping = false, ...newAddress } = address;
+			if ( siteId ) {
+				this.props.editOrder( siteId, { id: order.id, [ type ]: newAddress } );
+				if ( copyToShipping && 'billing' === type ) {
+					this.props.editOrder( siteId, { id: order.id, shipping: newAddress } );
+				}
+			}
+		};
+	};
+
+	toggleDialog = type => {
+		return () => {
+			this.setState( { showDialog: type } );
+		};
+	};
+
+	renderDialogs = () => {
+		const { billing, shipping } = this.props.order;
+		return [
+			<CustomerAddressDialog
+				key="dialog-billing"
+				address={ billing }
+				closeDialog={ this.toggleDialog( false ) }
+				isBilling
+				isVisible={ 'billing' === this.state.showDialog }
+				updateAddress={ this.updateAddress( 'billing' ) }
+			/>,
+			<CustomerAddressDialog
+				key="dialog-shipping"
+				address={ shipping }
+				closeDialog={ this.toggleDialog( false ) }
+				isVisible={ 'shipping' === this.state.showDialog }
+				updateAddress={ this.updateAddress( 'shipping' ) }
+			/>,
+		];
 	};
 
 	render() {
-		const { order, translate } = this.props;
+		const { isEditing, order, translate } = this.props;
 		if ( ! order ) {
 			return null;
 		}
@@ -38,6 +94,16 @@ class OrderCustomerInfo extends Component {
 						<div className="order-customer__billing">
 							<h3 className="order-customer__billing-details">
 								{ translate( 'Billing Details' ) }
+								{ isEditing ? (
+									<Button
+										compact
+										className="order-customer__edit-link"
+										onClick={ this.toggleDialog( 'billing' ) }
+										borderless
+									>
+										{ translate( 'Edit' ) }
+									</Button>
+								) : null }
 							</h3>
 							<h4>{ translate( 'Address' ) }</h4>
 							<div className="order-customer__billing-address">
@@ -55,6 +121,16 @@ class OrderCustomerInfo extends Component {
 						<div className="order-customer__shipping">
 							<h3 className="order-customer__shipping-details">
 								{ translate( 'Shipping Details' ) }
+								{ isEditing ? (
+									<Button
+										compact
+										className="order-customer__edit-link"
+										onClick={ this.toggleDialog( 'shipping' ) }
+										borderless
+									>
+										{ translate( 'Edit' ) }
+									</Button>
+								) : null }
 							</h3>
 							<h4>{ translate( 'Address' ) }</h4>
 							<div className="order-customer__shipping-address">
@@ -64,9 +140,23 @@ class OrderCustomerInfo extends Component {
 						</div>
 					</div>
 				</Card>
+				{ isEditing && this.renderDialogs() }
 			</div>
 		);
 	}
 }
 
-export default localize( OrderCustomerInfo );
+export default connect(
+	( state, props ) => {
+		const siteId = getSelectedSiteId( state );
+		const isEditing = isCurrentlyEditingOrder( state );
+		const order = isEditing ? getOrderWithEdits( state ) : getOrder( state, props.orderId );
+
+		return {
+			isEditing,
+			order,
+			siteId,
+		};
+	},
+	dispatch => bindActionCreators( { editOrder }, dispatch )
+)( localize( OrderCustomerInfo ) );
