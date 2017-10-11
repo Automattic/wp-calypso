05c51fb7ca366c8fbf39fe7c881de0188e270b42
Bernhard Reiter <ockham@raz.or.at>
2017-10-11 23:19:25 +0200

    Simplify a little

--- client/my-sites/site-settings/confirm-disconnection/index.jsx
+++ client/my-sites/site-settings/confirm-disconnection/index.jsx
@@ -2,7 +2,7 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { flowRight } from 'lodash';
 import { localize } from 'i18n-calypso';
@@ -15,36 +15,30 @@ import FormattedHeader from 'components/formatted-header';
 import Main from 'components/main';
 import Placeholder from 'my-sites/site-settings/placeholder';
 import redirectNonJetpack from 'my-sites/site-settings/redirect-non-jetpack';
-import { getSelectedSite } from 'state/ui/selectors';
+import { getSelectedSiteId } from 'state/ui/selectors';
 
-class ConfirmDisconnection extends Component {
-	render() {
-		const { site, translate } = this.props;
-
-		if ( ! site ) {
-			return <Placeholder />;
-		}
-
-		return (
-			<Main className="confirm-disconnection site-settings">
-				<DocumentHead title={ translate( 'Site Settings' ) } />
-				<FormattedHeader
-					headerText={ translate( 'Confirm Disconnection' ) }
-					subHeaderText={ translate(
-						'Confirm that you want to disconnect your site from WordPress.com.'
-					) }
-				/>
-				<Card className="confirm-disconnection__card" />
-			</Main>
-		);
+const ConfirmDisconnection = ( { siteId, translate } ) => {
+	if ( ! siteId ) {
+		return <Placeholder />;
 	}
-}
 
-const connectComponent = connect( state => {
-	return {
-		site: getSelectedSite( state ),
-	};
-} );
+	return (
+		<Main className="confirm-disconnection site-settings">
+			<DocumentHead title={ translate( 'Site Settings' ) } />
+			<FormattedHeader
+				headerText={ translate( 'Confirm Disconnection' ) }
+				subHeaderText={ translate(
+					'Confirm that you want to disconnect your site from WordPress.com.'
+				) }
+			/>
+			<Card className="confirm-disconnection__card" />
+		</Main>
+	);
+};
+
+const connectComponent = connect( state => ( {
+	siteId: getSelectedSiteId( state ),
+} ) );
 
 export default flowRight( connectComponent, localize, redirectNonJetpack() )(
 	ConfirmDisconnection
