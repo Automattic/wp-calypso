89c761d62403f4d70380f3f680fe302045b3563f
Rastislav Lamos <lamos.rasto@gmail.com>
2017-10-11 16:00:23 +0200

    Atomic Store: add special checkout-thank-you page for store signup flow (#18715)

--- client/my-sites/checkout/checkout-thank-you/index.jsx
+++ client/my-sites/checkout/checkout-thank-you/index.jsx
@@ -6,7 +6,7 @@
 
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import { find } from 'lodash';
+import { find, get } from 'lodash';
 import page from 'page';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -36,6 +36,7 @@ import HappinessSupport from 'components/happiness-support';
 import HeaderCake from 'components/header-cake';
 import PlanThankYouCard from 'blocks/plan-thank-you-card';
 import JetpackThankYouCard from './jetpack-thank-you-card';
+import AtomicStoreThankYouCard from './atomic-store-thank-you-card';
 import {
 	isChargeback,
 	isDomainMapping,
@@ -75,6 +76,7 @@ import {
 	PLAN_JETPACK_BUSINESS,
 	PLAN_JETPACK_BUSINESS_MONTHLY,
 } from 'lib/plans/constants';
+import { getSiteOptions } from 'state/selectors';
 
 function getPurchases( props ) {
 	return ( props.receipt.data && props.receipt.data.purchases ) || [];
@@ -264,6 +266,8 @@ const CheckoutThankYou = React.createClass( {
 			return <RebrandCitiesThankYou receipt={ this.props.receipt } />;
 		}
 
+		const { signupIsStore } = this.props;
+
 		// streamlined paid NUX thanks page
 		if ( this.isNewUser() && wasDotcomPlanPurchased ) {
 			return (
@@ -277,6 +281,13 @@ const CheckoutThankYou = React.createClass( {
 				<Main className="checkout-thank-you">
 					{ this.renderConfirmationNotice() }
 					<JetpackThankYouCard siteId={ this.props.selectedSite.ID } />
+				</Main>
+			);
+		} else if ( wasDotcomPlanPurchased && signupIsStore ) {
+			return (
+				<Main className="checkout-thank-you">
+					{ this.renderConfirmationNotice() }
+					<AtomicStoreThankYouCard siteId={ this.props.selectedSite.ID } />
 				</Main>
 			);
 		}
@@ -432,6 +443,7 @@ export default connect(
 	( state, props ) => {
 		const siteId = getSelectedSiteId( state );
 		const planSlug = getSitePlanSlug( state, siteId );
+		const siteOptions = getSiteOptions( state, siteId );
 
 		return {
 			planSlug,
@@ -439,6 +451,7 @@ export default connect(
 			sitePlans: getPlansBySite( state, props.selectedSite ),
 			user: getCurrentUser( state ),
 			userDate: getCurrentUserDate( state ),
+			signupIsStore: get( siteOptions, 'signup_is_store', false ),
 		};
 	},
 	dispatch => {
