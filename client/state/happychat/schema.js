8e45f5fce026c1fe96e7295dfff124e0152d7d29
Andrés Maneiro <andres.maneiro@automattic.com>
2017-10-11 11:40:16 +0200

    State: extract happychat.chat reducer from happychat

--- client/state/happychat/schema.js
+++ client/state/happychat/schema.js
@@ -1,22 +1,5 @@
 /** @format */
 
-export const eventSchema = {
-	type: 'object',
-	additionalProperties: false,
-	required: [ 'id', 'source', 'message', 'timestamp', 'user_id', 'type' ],
-	properties: {
-		id: { type: 'string' },
-		source: { type: 'string' },
-		message: { type: 'string' },
-		name: { type: 'string' },
-		image: { type: 'string' },
-		timestamp: { type: [ 'number', 'string' ] },
-		user_id: { type: [ 'number', 'string' ] },
-		type: { type: 'string' },
-		links: { type: 'array' },
-	},
-};
-
 export const geoLocationSchema = {
 	type: [ 'object', 'null' ],
 	properties: {
@@ -25,10 +8,4 @@ export const geoLocationSchema = {
 		country_short: { type: 'string' },
 		region: { type: 'string' },
 	},
-};
-
-export const timelineSchema = {
-	type: 'array',
-	additionalProperties: false,
-	items: eventSchema,
 };
--- client/state/happychat/test/reducer.js
+++ client/state/happychat/test/reducer.js
@@ -8,13 +8,10 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import { lastActivityTimestamp, lostFocusAt, message, geoLocation } from '../reducer';
+import { lostFocusAt, geoLocation } from '../reducer';
 import {
-	HAPPYCHAT_RECEIVE_EVENT,
 	HAPPYCHAT_BLUR,
 	HAPPYCHAT_FOCUS,
-	HAPPYCHAT_SEND_MESSAGE,
-	HAPPYCHAT_SET_MESSAGE,
 	SERIALIZE,
 	DESERIALIZE,
 	HAPPYCHAT_CONNECTED,
@@ -25,27 +22,6 @@ import { useSandbox } from 'test/helpers/use-sinon';
 const NOW = 1488173100125;
 
 describe( 'reducers', () => {
-	describe( '#lastActivityTimestamp', () => {
-		useSandbox( sandbox => {
-			sandbox.stub( Date, 'now' ).returns( NOW );
-		} );
-
-		test( 'defaults to null', () => {
-			const result = lastActivityTimestamp( undefined, {} );
-			expect( result ).to.be.null;
-		} );
-
-		test( 'should update on certain activity-specific actions', () => {
-			let result;
-
-			result = lastActivityTimestamp( null, { type: HAPPYCHAT_RECEIVE_EVENT } );
-			expect( result ).to.equal( NOW );
-
-			result = lastActivityTimestamp( null, { type: HAPPYCHAT_SEND_MESSAGE } );
-			expect( result ).to.equal( NOW );
-		} );
-	} );
-
 	describe( '#lostFocusAt', () => {
 		useSandbox( sandbox => {
 			sandbox.stub( Date, 'now' ).returns( NOW );
@@ -65,23 +41,6 @@ describe( 'reducers', () => {
 
 		test( 'returns null on HAPPYCHAT_FOCUS actions', () => {
 			expect( lostFocusAt( 12345, { type: HAPPYCHAT_FOCUS } ) ).to.be.null;
-		} );
-	} );
-
-	describe( '#message()', () => {
-		test( 'defaults to an empty string', () => {
-			const result = message( undefined, {} );
-			expect( result ).to.eql( '' );
-		} );
-		test( 'saves messages passed from HAPPYCHAT_SET_MESSAGE', () => {
-			const action = { type: HAPPYCHAT_SET_MESSAGE, message: 'abcd' };
-			const result = message( 'abc', action );
-			expect( result ).to.eql( 'abcd' );
-		} );
-		test( 'resets to empty string on HAPPYCHAT_SEND_MESSAGE', () => {
-			const action = { type: HAPPYCHAT_SEND_MESSAGE, message: 'abcd' };
-			const result = message( 'abcd', action );
-			expect( result ).to.eql( '' );
 		} );
 	} );
 
