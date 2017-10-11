f8f04b473a1cc3f2045e8ebc8956240b9f8cca0d
Andrés Maneiro <andres.maneiro@automattic.com>
2017-10-11 17:02:49 +0200

    State: extract ui.happychat selectors to happychat/selectors

--- client/state/happychat/selectors/is-happychat-open.js
+++ client/state/happychat/selectors/is-happychat-open.js
@@ -1,27 +1,18 @@
+/** @format */
+
 /**
  * Internal dependencies
- *
- * @format
  */
-
 import createSelector from 'lib/create-selector';
 import { getSectionName } from 'state/ui/selectors';
 
 /**
  * Returns wether the docked happychat client UI should be displayed
  * The docked UI should not be displayed when viewing the happychat section
- *
  * @param {Object} state - global redux state
  * @returns {Boolean}
  */
 export const isHappychatOpen = createSelector(
-	state => state.ui.happychat.open && getSectionName( state ) !== 'happychat',
-	state => [ state.ui.happychat.open, getSectionName( state ) ]
+	state => state.happychat.ui.isOpen && getSectionName( state ) !== 'happychat',
+	state => [ state.happychat.ui.isOpen, getSectionName( state ) ]
 );
-
-/**
- * Gets the current happychat minimizing status
- * @param {Object} state - global redux state
- * @return {String} current state value
- */
-export const isHappychatMinimizing = createSelector( state => state.ui.happychat.isMinimizing );
