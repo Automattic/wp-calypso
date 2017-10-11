9e87709157a1f1b0139e268545e671cb384a0c5b
Albert Manya <albert.manya@schibsted.com>
2017-10-11 23:03:47 +0200

    Use template literals instead of handlebars

--- server/bundler/loader.js
+++ server/bundler/loader.js
@@ -1,23 +1,61 @@
 const config = require( 'config' ),
 	fs = require( 'fs' ),
-	utils = require( './utils' ),
-	Handlebars = require( 'handlebars' );
-
-Handlebars.registerHelper( 'json', function( context ) {
-	return context && JSON.stringify( context ) || 'undefined';
-} );
+	utils = require( './utils' );
 
 function getSectionsModule( sections ) {
-	let templateName;
+	let templateName,
+		sectionLoaders;
+
 	if ( config.isEnabled( 'code-splitting' ) ) {
-		templateName = 'loader-code-splitting.js.mst';
+		templateName = 'loader-code-splitting.js.template';
+		sectionLoaders = getSectionLoadersCodeSplitting( sections );
 	} else {
-		templateName = 'loader.js.mst';
+		templateName = 'loader.js.template';
+		sectionLoaders = getSectionLoaders( sections );
 	}
 
-	const source = fs.readFileSync( __dirname + '/' + templateName, 'utf8' );
-	const template = Handlebars.compile( source.toString() );
-	return template( { sections: sections } );
+	stringSections = JSON.stringify( sections );
+
+	// Load the template and convert to a template literal using eval
+	return eval( '`' + fs.readFileSync( __dirname + '/' + templateName, 'utf8' ).toString() + '`' );
+}
+
+function getSectionLoadersCodeSplitting( sections ) {
+	return sections.map( section => {
+		return [
+			'case "' + section.name + '":',
+			'   return import( /* webpackChunkName: "' + section.name + '" */ "' + section.module + '" );',
+			'   break;'
+		].join( '\n' );
+	} ).join( '\n' );
+}
+
+function getSectionLoaders( sections ) {
+	let content = '';
+
+	sections.forEach( function( section ) {
+		content += requireTemplate( section );
+	} );
+
+	return content;
+}
+
+function requireTemplate( section ) {
+	const result = section.paths.reduce( function( acc, path ) {
+		return acc.concat( [
+			'page( getPathRegExp( ' + JSON.stringify( path ) + ', function( context, next ) {',
+			'	var envId = ' + JSON.stringify( section.envId ) + ';',
+			'	if ( envId && envId.indexOf( config( "env_id" ) ) === -1 ) {',
+			'		return next();',
+			'	}',
+			'	controller.setSection( ' + JSON.stringify( section ) + ' )( context );',
+			'	require( ' + JSON.stringify( section.module ) + ' )( controller.clientRouter );',
+			'	next();',
+			'} );\n'
+		] );
+	}, [] );
+
+	return result.join( '\n' );
 }
 
 function sectionsWithCSSUrls( sections ) {
