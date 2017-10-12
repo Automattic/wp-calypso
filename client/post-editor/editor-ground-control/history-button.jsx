7dea5f9669cf1d3e04162310ba65eaceb806cd8d
Jeff Bowen <jeff@automattic.com>
2017-10-12 11:38:27 -0400

    Toggle the showing state of the sidebar when clicking `History`

--- client/post-editor/editor-ground-control/history-button.jsx
+++ client/post-editor/editor-ground-control/history-button.jsx
@@ -2,26 +2,56 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-import { NESTED_SIDEBAR_REVISIONS } from 'post-editor/editor-sidebar/constants';
+import {
+	NESTED_SIDEBAR_NONE,
+	NESTED_SIDEBAR_REVISIONS,
+} from 'post-editor/editor-sidebar/constants';
 
-const HistoryButton = ( { selectRevision, setNestedSidebar, translate } ) => (
-	<div className="editor-ground-control__history">
-		<button
-			className="editor-ground-control__save button is-link"
-			onClick={ function() {
-				selectRevision( null );
-				setNestedSidebar( NESTED_SIDEBAR_REVISIONS );
-			} }
-		>
-			{ translate( 'History' ) }
-		</button>
-	</div>
-);
+class HistoryButton extends PureComponent {
+	state = {
+		showingHistory: false,
+	};
+
+	toggleShowing = () => {
+		this.setState( {
+			showingHistory: ! this.state.showingHistory,
+		} );
+	};
+
+	componentWillUpdate( nextProps, nextState ) {
+		if ( nextState.showingHistory === this.state.showingHistory ) {
+			return;
+		}
+
+		const { selectRevision, setNestedSidebar } = this.props;
+
+		selectRevision( null );
+
+		if ( nextState.showingHistory ) {
+			setNestedSidebar( NESTED_SIDEBAR_REVISIONS );
+			return;
+		}
+		setNestedSidebar( NESTED_SIDEBAR_NONE );
+	}
+
+	render() {
+		return (
+			<div className="editor-ground-control__history">
+				<button
+					className="editor-ground-control__save button is-link"
+					onClick={ this.toggleShowing }
+				>
+					{ this.props.translate( 'History' ) }
+				</button>
+			</div>
+		);
+	}
+}
 
 export default localize( HistoryButton );
