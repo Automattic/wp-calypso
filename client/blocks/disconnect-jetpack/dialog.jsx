16e30de2402c207418660dfc8208c4077e34d3d4
Bernhard Reiter <ockham@raz.or.at>
2017-10-12 23:21:20 +0200

    Restructure props

--- client/blocks/disconnect-jetpack/dialog.jsx
+++ client/blocks/disconnect-jetpack/dialog.jsx
@@ -12,27 +12,30 @@ import PropTypes from 'prop-types';
 import Dialog from 'components/dialog';
 import DisconnectJetpack from '.';
 
-const DisconnectJetpackDialog = ( { isBroken, isVisible, onClose, redirect, siteId } ) => (
+const DisconnectJetpackDialog = ( { isBroken, isVisible, onClose, disconnectHref, siteId } ) => (
 	<Dialog
 		isVisible={ isVisible }
 		additionalClassNames="disconnect-jetpack-dialog"
 		onClose={ onClose }
 	>
 		<DisconnectJetpack
+			disconnectHref={ disconnectHref }
 			isBroken={ isBroken }
-			onClose={ onClose }
-			redirect={ redirect }
+			onDisconnect={ onClose }
+			onStayConnected={ onClose }
 			siteId={ siteId }
 		/>
 	</Dialog>
 );
 
 DisconnectJetpackDialog.propTypes = {
+	disconnectHref: PropTypes.string,
 	isBroken: PropTypes.bool,
 	isVisible: PropTypes.bool,
 	onClose: PropTypes.func,
 	redirect: PropTypes.string,
 	siteId: PropTypes.number,
+	stayConnectedHref: PropTypes.string,
 };
 
 export default DisconnectJetpackDialog;
