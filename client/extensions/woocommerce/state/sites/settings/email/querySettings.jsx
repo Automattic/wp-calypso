f065b8f2cdd1c266a39471a03ddc78c4286a9945
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2017-10-13 14:58:51 -0700

    MailChimp integration query components. (#18751)

---
+++ client/extensions/woocommerce/state/sites/settings/email/querySettings.jsx
@@ -0,0 +1,48 @@
+/**
+ * External dependencies
+ */
+import { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { requestSettings } from './actions';
+import { isRequestingSettings } from './selectors';
+
+class QueryMailChimpSettings extends Component {
+	static propTypes = {
+		siteId:	PropTypes.number.isRequired,
+		isRequesting: PropTypes.bool.isRequired,
+		requestSettings: PropTypes.func.isRequired,
+	}
+
+	componentDidMount() {
+		this.request( this.props );
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		if ( this.props.siteId === nextProps.siteId ) {
+			return;
+		}
+		this.request( nextProps );
+	}
+
+	request( props ) {
+		if ( ! props.isRequesting && props.siteId ) {
+			props.requestSettings( props.siteId );
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default connect(
+	( state, { siteId } ) => ( {
+		isRequesting: isRequestingSettings( state, siteId ),
+	} ),
+	{ requestSettings }
+)( QueryMailChimpSettings );
