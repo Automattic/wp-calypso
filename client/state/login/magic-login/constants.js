8f77ac4981240b4dde6056f6592b9fcc7447adfb
Jake F - @samouri <samouri@users.noreply.github.com>
2017-10-13 17:15:31 -0400

    Magic Logic: interstitial page constant missing

--- client/state/login/magic-login/constants.js
+++ client/state/login/magic-login/constants.js
@@ -3,3 +3,4 @@ export const AUTHENTICATE_URL = 'https://wordpress.com/wp-login.php?action=magic
 export const CHECK_YOUR_EMAIL_PAGE = 'CHECK_YOUR_EMAIL_PAGE';
 export const LINK_EXPIRED_PAGE = 'LINK_EXPIRED_PAGE';
 export const REQUEST_FORM = 'REQUEST_FORM';
+export const INTERSTITIAL_PAGE = 'INTERSTITIAL_PAGE';
