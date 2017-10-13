02c7cdfc34e9b0c6e9774484091b09929428c00d
Bartosz <bartosz.budzanowski@gmail.com>
2017-10-13 09:17:46 -0700

    Address review.

--- client/extensions/woocommerce/app/settings/email/mailchimp/getting-started.js
+++ client/extensions/woocommerce/app/settings/email/mailchimp/getting-started.js
@@ -1,8 +1,9 @@
 /**
  * External dependencies
  */
-import React from 'react';
 import Gridicon from 'gridicons';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 /**
  * Internal dependencies
@@ -11,54 +12,51 @@ import Button from 'components/button';
 import Card from 'components/card';
 import { localize } from 'i18n-calypso';
 
-class MailChimpGettingStarted extends React.Component {
+const GettingStarted = localize( ( { translate, onClick } ) => {
+	const allow = translate( 'Allow customers to subscribe to your Email list' );
+	const send = translate( 'Send abandon cart emails' );
+	const create = translate( 'Create purchase-based segments for targeted campaigns' );
+	const list = [ allow, send, create ];
 
-	render() {
-		const { translate, onClick } = this.props;
-		const infoText = translate( 'Allow your customers subscribe to your MailChimp email list' );
+	return (
+		<div>
+			<Card className="mailchimp__getting-started-title">
+				<div className="mailchimp__getting-started-title-text">MailChimp</div>
+				<div className="mailchimp__getting-started-subtitle-text">
+					{ translate( 'Allow your customers subscribe to your MailChimp email list' ) }
+				</div>
+			</Card>
+			<Card className="mailchimp__getting-started-content">
+				<span>
+					<img
+						src={ '/calypso/images/illustrations/illustration-layout.svg' }
+						width={ 220 }
+						className="mailchimp__getting-started-illustration"
+					/>
+				</span>
+				<span>
+					<h3 className="mailchimp__getting-started-list-header">
+						{ translate( 'Connect with your customers through MailChimp' ) }
+					</h3>
+					<ul className="mailchimp__getting-started-list">
+						{ list.map( ( item, key ) =>
+							<li key={ key }>
+								<Gridicon icon="checkmark" size={ 18 } />
+								{ item }
+							</li>
+						) }
+					</ul>
+					<Button className="mailchimp__getting-started-button" onClick={ onClick }>
+						{ translate( 'Get started with MailChimp' ) }
+					</Button>
+				</span>
+			</Card>
+		</div>
+	);
+} );
 
-		const illustration =
-			<img
-				src={ '/calypso/images/illustrations/illustration-layout.svg' }
-				width={ 220 }
-				className="mailchimp__getting-started-illustration"
-			/>;
+GettingStarted.propTypes = {
+	onClick: PropTypes.func.isRequired,
+};
 
-		const connect = translate( 'Connect with your customers through MailChimp' );
-		const allow = translate( 'Allow customers to subscribe to your Email list' );
-		const send = translate( 'Send abandon cart emails' );
-		const create = translate( 'Create purchase-based segments for targeted campaigns' );
-		const gettingStarted = translate( 'Get started with MailChimp' );
-		const list = [ allow, send, create ];
-
-		return (
-			<div>
-				<Card className="mailchimp__getting-started-title">
-					<div className="mailchimp__getting-started-title-text">MailChimp</div>
-					<div className="mailchimp__getting-started-subtitle-text">{ infoText }</div>
-				</Card>
-				<Card className="mailchimp__getting-started-content">
-					<span>{ illustration }</span>
-					<span>
-						<h3 className="mailchimp__getting-started-list-header">
-							{ connect }
-						</h3>
-						<ul className="mailchimp__getting-started-list">
-							{ list.map( ( item, key ) =>
-								<li key={ key }>
-									<Gridicon icon="checkmark" size={ 18 } />
-									{ item }
-								</li>
-							) }
-						</ul>
-						<Button className="mailchimp__getting-started-button" onClick={ onClick }>
-							{ gettingStarted }
-						</Button>
-					</span>
-				</Card>
-			</div>
-		);
-	}
-}
-
-export default localize( MailChimpGettingStarted );
+export default GettingStarted;
