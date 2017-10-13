fae06062e115cab13e5a69877e7a30cebddb157e
Jake F - @samouri <samouri@users.noreply.github.com>
2017-10-13 17:37:31 -0400

    Reader Tags: remove recs

--- client/reader/tag-stream/index.js
+++ client/reader/tag-stream/index.js
@@ -7,12 +7,10 @@ import page from 'page';
 /**
  * Internal dependencies
  */
-import { recommendedTags, tagListing } from './controller';
+import { tagListing } from './controller';
 import { initAbTests, preloadReaderBundle, sidebar, updateLastRoute } from 'reader/controller';
 
 export default function() {
 	page( '/tag/*', preloadReaderBundle, initAbTests );
 	page( '/tag/:tag', updateLastRoute, sidebar, tagListing );
-
-	page( '/tags', initAbTests, updateLastRoute, sidebar, recommendedTags );
 }
