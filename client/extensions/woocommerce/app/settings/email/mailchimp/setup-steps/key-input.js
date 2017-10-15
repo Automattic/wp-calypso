e933bc46b28c437bd145374662dcfc1887900911
Bartosz <bartosz.budzanowski@gmail.com>
2017-10-15 01:27:40 -0700

    Address review.

--- client/extensions/woocommerce/app/settings/email/mailchimp/setup-steps/key-input.js
+++ client/extensions/woocommerce/app/settings/email/mailchimp/setup-steps/key-input.js
@@ -1,28 +1,29 @@
 /**
  * External dependencies
  */
+import PropTypes from 'prop-types';
 import React from 'react';
 
 /**
  * Internal dependencies
  */
 import FormFieldset from 'components/forms/form-fieldset';
-import FormInputValidation from 'components/forms/form-input-validation';
 import FormLabel from 'components/forms/form-label';
 import FormTextInput from 'components/forms/form-text-input';
+import FormInputValidation from 'components/forms/form-input-validation';
 import { localize } from 'i18n-calypso';
 
-export default localize( ( { translate, onChange, apiKey, isKeyCorrect } ) => (
+const KeyInputStep = localize( ( { translate, onChange, apiKey, isKeyCorrect } ) => (
 	<FormFieldset className="setup-steps__mailchimp-key-input">
 		<div className="setup-steps__mailchimp-api-intro-notice">
 			{ translate( 'Now that you\'re signed in to MailChimp, you need an API key to start the connection process' ) }
 		</div>
 		<div className="setup-steps__mailchimp-api-directions" >
-			<span>{ translate( 'To find your Mailchimp API key, go to ' ) }</span>
+			<span>{ translate( 'To find your Mailchimp API key ' ) }</span>
 			<span className="setup-steps__mailchimp-api-directions-bold">
-				{ translate( 'Settting > Extras > API keys.' ) }
+				{ translate( 'click your profile picture, select \'Account\', and go to Extras > API keys.' ) }
 			</span>
-			<div>{ translate( 'From there, grab an existing key or generate a new on for your store.' ) } </div>
+			<div>{ translate( 'From there, grab an existing key or generate a new one for your store.' ) } </div>
 		</div>
 		<FormLabel required={ ! isKeyCorrect }>
 			{ translate( 'Mailchimp API Key:' ) }
@@ -37,3 +38,11 @@ export default localize( ( { translate, onChange, apiKey, isKeyCorrect } ) => (
 		{ ! isKeyCorrect && <FormInputValidation isError text="Key appears to be invalid" /> }
 	</FormFieldset>
 ) );
+
+KeyInputStep.propTypes = {
+	apiKey: PropTypes.string,
+	isKeyCorrect: PropTypes.bool,
+	onChange: PropTypes.func.isRequired,
+};
+
+export default KeyInputStep;
