38862182132c5aec1abed3691c90dc4f8c154950
Bartosz <bartosz.budzanowski@gmail.com>
2017-10-16 03:41:21 -0700

    Address review.

--- client/extensions/woocommerce/app/settings/email/mailchimp/setup-steps/newsletter-settings.js
+++ client/extensions/woocommerce/app/settings/email/mailchimp/setup-steps/newsletter-settings.js
@@ -17,12 +17,12 @@ import FormLabel from 'components/forms/form-label';
 import FormSelect from 'components/forms/form-select';
 import QueryMailChimpLists from 'woocommerce/state/sites/settings/email/queryLists';
 
-const NewsletterSettings = ( { storeData, onChange, siteId, isRequesting, translate } ) => {
+const NewsletterSettings = ( { storeData = {}, onChange, siteId, isRequesting, translate } ) => {
 	return (
 		<FormFieldset className="setup-steps__store-info-field">
 			<QueryMailChimpLists siteId={ siteId } />
-			<div>{ translate( 'Pick a list, you will not able to change it for now so pick carefully' ) }</div>
-			<div>{ translate( 'Create your list at mailchimp.com if you have not done it aready' ) }</div>
+			<div>{ translate( 'Pick a list, you will not able to change it for now so pick carefully.' ) }</div>
+			<div>{ translate( 'Create your list at mailchimp.com if you have not done it aready.' ) }</div>
 			<FormLabel>
 				{ translate( 'Newsletter' ) }
 			</FormLabel>
@@ -43,9 +43,9 @@ const NewsletterSettings = ( { storeData, onChange, siteId, isRequesting, transl
 
 NewsletterSettings.propTypes = {
 	onChange: PropTypes.func.isRequired,
-	storeData: PropTypes.object,
+	storeData: PropTypes.object.isRequired,
 	siteId: PropTypes.number.isRequired,
-	isRequesting: PropTypes.bool
+	isRequesting: PropTypes.bool,
 };
 
 const NewsletterSettingsConnected = connect(
@@ -53,7 +53,7 @@ const NewsletterSettingsConnected = connect(
 		const siteId = getSelectedSiteId( state );
 		return {
 			siteId,
-			isRequesting: isRequestingLists( state, siteId )
+			isRequesting: isRequestingLists( state, siteId ),
 		};
 	}
 )( NewsletterSettings );
