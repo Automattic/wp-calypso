93f5571b3dd75d097c2027508ef8cbae709ec819
Mark Ryall <mark@ryall.name>
2017-10-16 12:54:49 +1000

    add a/b test

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -99,4 +99,13 @@ export default {
 		allowExistingUsers: true,
 		localeTargets: 'any',
 	},
+	unlimitedThemeNudge: {
+		datestamp: '20171016',
+		variations: {
+			hide: 50,
+			show: 50,
+		},
+		defaultVariation: 'hide',
+		allowExistingUsers: true,
+	},
 };
