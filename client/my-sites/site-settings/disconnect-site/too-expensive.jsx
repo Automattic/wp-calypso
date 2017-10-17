5be2bcddea823f1e9023365355c72ade59cea662
Bernhard Reiter <ockham@raz.or.at>
2017-10-17 15:32:07 +0200

    Add link to purchase page

--- client/my-sites/site-settings/disconnect-site/too-expensive.jsx
+++ client/my-sites/site-settings/disconnect-site/too-expensive.jsx
@@ -3,6 +3,7 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -10,19 +11,42 @@ import { localize } from 'i18n-calypso';
  */
 import Button from 'components/button';
 import Card from 'components/card';
+import Placeholder from 'my-sites/site-settings/placeholder';
+import QuerySitePlans from 'components/data/query-site-plans';
+import { getCurrentPlanPurchaseId } from 'state/selectors';
+import { getSiteSlug } from 'state/sites/selectors';
+import { getSelectedSiteId } from 'state/ui/selectors';
 
-const TooExpensive = ( { confirmHref, translate } ) => (
-	<div>
+const TooExpensive = ( { confirmHref, planPurchaseId, siteId, siteSlug, translate } ) => {
+	if ( ! siteSlug || ! planPurchaseId ) {
+		return (
+			<div>
+				<QuerySitePlans siteId={ siteId } />
+				<Placeholder />
+			</div>
+		);
+	}
+	return (
 		<Card className="disconnect-site__question">
+			<QuerySitePlans siteId={ siteId } />
 			<p>{ translate( 'Would you like to downgrade your plan?' ) }</p>
 			<div>
-				<Button compact>Yes</Button>
+				<Button compact href={ `/me/purchases/${ siteSlug }/${ planPurchaseId }` }>
+					Yes
+				</Button>
 				<Button compact href={ confirmHref }>
 					No
 				</Button>
 			</div>
 		</Card>
-	</div>
-);
+	);
+};
 
-export default localize( TooExpensive );
+export default connect( state => {
+	const siteId = getSelectedSiteId( state );
+	return {
+		planPurchaseId: getCurrentPlanPurchaseId( state, siteId ),
+		siteId,
+		siteSlug: getSiteSlug( state, siteId ),
+	};
+} )( localize( TooExpensive ) );
