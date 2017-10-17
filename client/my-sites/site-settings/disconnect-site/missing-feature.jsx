9663daca253334442303edec394521b2fbaddf9e
Bernhard Reiter <ockham@raz.or.at>
2017-10-17 15:31:50 +0200

    Remove obsolete div

--- client/my-sites/site-settings/disconnect-site/missing-feature.jsx
+++ client/my-sites/site-settings/disconnect-site/missing-feature.jsx
@@ -12,14 +12,12 @@ import Card from 'components/card';
 import FormTextInput from 'components/forms/form-text-input';
 
 const MissingFeature = ( { translate } ) => (
-	<div>
-		<Card className="disconnect-site__question">
-			<p>{ translate( 'Which feature where you looking for?' ) }</p>
-			<div>
-				<FormTextInput />
-			</div>
-		</Card>
-	</div>
+	<Card className="disconnect-site__question">
+		<p>{ translate( 'Which feature where you looking for?' ) }</p>
+		<div>
+			<FormTextInput />
+		</div>
+	</Card>
 );
 
 export default localize( MissingFeature );
