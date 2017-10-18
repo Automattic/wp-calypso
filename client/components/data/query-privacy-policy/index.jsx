28c840d135e43775a021c819ac88d9b32ae9faac
retrofox <rdsuarez@gmail.com>
2017-10-18 15:53:49 -0300

    privacy-policy-tos: add query component

---
+++ client/components/data/query-privacy-policy/index.jsx
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ *
+ * @format
+ */
+
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { requestPrivacyPolicy } from 'state/privacy-policy/actions';
+
+export class QueryPrivacyPolicy extends Component {
+	static propTypes = {
+		requestPrivacyPolicy: PropTypes.func,
+	};
+
+	componentDidMount() {
+		this.props.requestPrivacyPolicy();
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default connect( null, { requestPrivacyPolicy } )( QueryPrivacyPolicy );
