0035957e1adab83e63d03808907157924edf42c9
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-10-18 21:39:58 +0100

    Put react-hot-loader behind a feature flag

--- webpack.config.js
+++ webpack.config.js
@@ -145,7 +145,7 @@ const webpackConfig = {
 		new CopyWebpackPlugin( [ { from: 'node_modules/flag-icon-css/flags/4x3', to: 'images/flags' } ] ),
 		new HappyPack( {
 			loaders: _.compact( [
-				calypsoEnv === 'development' && 'react-hot-loader',
+				calypsoEnv === 'development' && config.isEnabled( 'webpack/hot-loader' ) && 'react-hot-loader',
 				babelLoader
 			] )
 		} ),
