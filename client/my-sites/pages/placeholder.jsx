57b38cabb96faeaebfb1f286ee972df92689799f
spen <spen_@hotmail.co.uk>
2017-10-18 22:46:29 +0100

    Placeholders: Unwrap components from localize HoCs

--- client/my-sites/pages/placeholder.jsx
+++ client/my-sites/pages/placeholder.jsx
@@ -3,9 +3,7 @@
  *
  * @format
  */
-
 import React from 'react';
-
 import { localize } from 'i18n-calypso';
 
 /**
@@ -14,43 +12,43 @@ import { localize } from 'i18n-calypso';
 import CompactCard from 'components/card/compact';
 import SiteIcon from 'blocks/site-icon';
 
+const PagePlaceholder = React.createClass( {
+	displayName: 'PagePlaceholder',
+
+	render: function() {
+		return (
+			<CompactCard className="page is-placeholder">
+				{ this.props.multisite ? <SiteIcon size={ 34 } /> : null }
+				<a className="page__title">
+					<span className="placeholder-text">
+						{ this.props.translate( 'Loading a page of Pages…' ) }
+					</span>
+				</a>
+				{ this.props.multisite ? (
+					<span className="page__site-url ">
+						<span className="placeholder-text">
+							{ this.props.translate( 'A domain, quite soon…' ) }
+						</span>
+					</span>
+				) : null }
+			</CompactCard>
+		);
+	},
+} );
+
+const MarkerPlaceholder = React.createClass( {
+	displayName: 'MarkerPlaceholder',
+
+	render: function() {
+		return (
+			<div className="pages__page-list-header is-placeholder">
+				<span>&nbsp;</span>
+			</div>
+		);
+	},
+} );
+
 export default {
-	Page: localize(
-		React.createClass( {
-			displayName: 'PagePlaceholder',
-
-			render: function() {
-				return (
-					<CompactCard className="page is-placeholder">
-						{ this.props.multisite ? <SiteIcon size={ 34 } /> : null }
-						<a className="page__title">
-							<span className="placeholder-text">
-								{ this.props.translate( 'Loading a page of Pages…' ) }
-							</span>
-						</a>
-						{ this.props.multisite ? (
-							<span className="page__site-url ">
-								<span className="placeholder-text">
-									{ this.props.translate( 'A domain, quite soon…' ) }
-								</span>
-							</span>
-						) : null }
-					</CompactCard>
-				);
-			},
-		} )
-	),
-	Marker: localize(
-		React.createClass( {
-			displayName: 'MarkerPlaceholder',
-
-			render: function() {
-				return (
-					<div className="pages__page-list-header is-placeholder">
-						<span>&nbsp;</span>
-					</div>
-				);
-			},
-		} )
-	),
+	Page: localize( PagePlaceholder ),
+	Marker: localize( MarkerPlaceholder ),
 };
