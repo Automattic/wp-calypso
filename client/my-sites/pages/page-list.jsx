98bba7626a63b5df11faf6060597cc237325ab1f
spen <spen_@hotmail.co.uk>
2017-10-18 22:55:20 +0100

    PageList:Page: Unwrap components from localize HoCs

--- client/my-sites/pages/page-list.jsx
+++ client/my-sites/pages/page-list.jsx
@@ -80,274 +80,270 @@ export default class PageList extends Component {
 	}
 }
 
-const Pages = localize(
-	React.createClass( {
-		displayName: 'Pages',
+const Pages = React.createClass( {
+	displayName: 'Pages',
 
-		mixins: [ infiniteScroll( 'fetchPages' ) ],
+	mixins: [ infiniteScroll( 'fetchPages' ) ],
 
-		propTypes: {
-			incrementPage: PropTypes.func.isRequired,
-			lastPage: PropTypes.bool,
-			loading: PropTypes.bool.isRequired,
-			page: PropTypes.number.isRequired,
-			pages: PropTypes.array.isRequired,
-			search: PropTypes.string,
-			site: PropTypes.object,
-			siteId: PropTypes.any,
-			hasSites: PropTypes.bool.isRequired,
-			trackScrollPage: PropTypes.func.isRequired,
-		},
+	propTypes: {
+		incrementPage: PropTypes.func.isRequired,
+		lastPage: PropTypes.bool,
+		loading: PropTypes.bool.isRequired,
+		page: PropTypes.number.isRequired,
+		pages: PropTypes.array.isRequired,
+		search: PropTypes.string,
+		site: PropTypes.object,
+		siteId: PropTypes.any,
+		hasSites: PropTypes.bool.isRequired,
+		trackScrollPage: PropTypes.func.isRequired,
+	},
 
-		getDefaultProps() {
-			return {
-				perPage: 100,
-				loading: false,
-				lastPage: false,
-				page: 0,
-				pages: [],
-				trackScrollPage: function() {},
-			};
-		},
+	getDefaultProps() {
+		return {
+			perPage: 100,
+			loading: false,
+			lastPage: false,
+			page: 0,
+			pages: [],
+			trackScrollPage: function() {},
+		};
+	},
 
-		fetchPages( options ) {
-			if ( this.props.loading || this.props.lastPage ) {
-				return;
+	fetchPages( options ) {
+		if ( this.props.loading || this.props.lastPage ) {
+			return;
+		}
+		if ( options.triggeredByScroll ) {
+			this.props.trackScrollPage( this.props.page + 1 );
+		}
+		this.props.incrementPage();
+	},
+
+	_insertTimeMarkers( pages ) {
+		const markedPages = [],
+			now = this.props.moment();
+		let lastMarker;
+
+		const buildMarker = function( pageDate ) {
+			pageDate = this.props.moment( pageDate );
+			const days = now.diff( pageDate, 'days' );
+			if ( days <= 0 ) {
+				return this.props.translate( 'Today' );
 			}
-			if ( options.triggeredByScroll ) {
-				this.props.trackScrollPage( this.props.page + 1 );
+			if ( days === 1 ) {
+				return this.props.translate( 'Yesterday' );
 			}
-			this.props.incrementPage();
-		},
+			return pageDate.from( now );
+		}.bind( this );
 
-		_insertTimeMarkers( pages ) {
-			const markedPages = [],
-				now = this.props.moment();
-			let lastMarker;
-
-			const buildMarker = function( pageDate ) {
-				pageDate = this.props.moment( pageDate );
-				const days = now.diff( pageDate, 'days' );
-				if ( days <= 0 ) {
-					return this.props.translate( 'Today' );
-				}
-				if ( days === 1 ) {
-					return this.props.translate( 'Yesterday' );
-				}
-				return pageDate.from( now );
-			}.bind( this );
-
-			pages.forEach( function( page ) {
-				const date = this.props.moment( page.date ),
-					marker = buildMarker( date );
-				if ( lastMarker !== marker ) {
-					markedPages.push(
-						<div key={ 'marker-' + date.unix() } className="pages__page-list-header">
-							<Gridicon icon="time" size={ 12 } /> { marker }
-						</div>
-					);
-				}
-				lastMarker = marker;
-				markedPages.push( page );
-			}, this );
-
-			return markedPages;
-		},
-
-		getNoContentMessage() {
-			const { search, translate } = this.props;
-
-			if ( search ) {
-				return (
-					<NoResults
-						image="/calypso/images/pages/illustration-pages.svg"
-						text={ translate( 'No pages match your search for {{searchTerm/}}.', {
-							components: {
-								searchTerm: <em>{ search }</em>,
-							},
-						} ) }
-					/>
+		pages.forEach( function( page ) {
+			const date = this.props.moment( page.date ),
+				marker = buildMarker( date );
+			if ( lastMarker !== marker ) {
+				markedPages.push(
+					<div key={ 'marker-' + date.unix() } className="pages__page-list-header">
+						<Gridicon icon="time" size={ 12 } /> { marker }
+					</div>
 				);
 			}
+			lastMarker = marker;
+			markedPages.push( page );
+		}, this );
 
-			const { site, siteId, status = 'published' } = this.props;
-			const sitePart = ( site && site.slug ) || siteId;
-			const newPageLink = this.props.siteId ? '/page/' + sitePart : '/page';
-			let attributes;
+		return markedPages;
+	},
 
-			switch ( status ) {
-				case 'drafts':
-					attributes = {
-						title: translate( "You don't have any drafts." ),
-						line: translate( 'Would you like to create one?' ),
-						action: translate( 'Start a Page' ),
-						actionURL: newPageLink,
-					};
-					break;
-				case 'scheduled':
-					attributes = {
-						title: translate( "You don't have any scheduled pages yet." ),
-						line: translate( 'Would you like to create one?' ),
-						action: translate( 'Start a Page' ),
-						actionURL: newPageLink,
-					};
-					break;
-				case 'trashed':
-					attributes = {
-						title: translate( "You don't have any pages in your trash folder." ),
-						line: translate( 'Everything you write is solid gold.' ),
-					};
-					break;
-				default:
-					attributes = {
-						title: translate( "You haven't published any pages yet." ),
-						line: translate( 'Would you like to publish your first page?' ),
-						action: translate( 'Start a Page' ),
-						actionURL: newPageLink,
-					};
-			}
+	getNoContentMessage() {
+		const { search, translate } = this.props;
 
-			attributes.illustration = '/calypso/images/pages/illustration-pages.svg';
-			attributes.illustrationWidth = 150;
-
+		if ( search ) {
 			return (
-				<EmptyContent
-					title={ attributes.title }
-					line={ attributes.line }
-					action={ attributes.action }
-					actionURL={ attributes.actionURL }
-					illustration={ attributes.illustration }
-					illustrationWidth={ attributes.illustrationWidth }
+				<NoResults
+					image="/calypso/images/pages/illustration-pages.svg"
+					text={ translate( 'No pages match your search for {{searchTerm/}}.', {
+						components: {
+							searchTerm: <em>{ search }</em>,
+						},
+					} ) }
 				/>
 			);
-		},
+		}
 
-		addLoadingRows( rows, count ) {
-			for ( let i = 0; i < count; i++ ) {
-				if ( i % 4 === 0 ) {
-					rows.push( <Placeholder.Marker key={ 'placeholder-marker-' + i } /> );
-				}
-				rows.push(
-					<Placeholder.Page key={ 'placeholder-page-' + i } multisite={ ! this.props.site } />
-				);
+		const { site, siteId, status = 'published' } = this.props;
+		const sitePart = ( site && site.slug ) || siteId;
+		const newPageLink = this.props.siteId ? '/page/' + sitePart : '/page';
+		let attributes;
+
+		switch ( status ) {
+			case 'drafts':
+				attributes = {
+					title: translate( "You don't have any drafts." ),
+					line: translate( 'Would you like to create one?' ),
+					action: translate( 'Start a Page' ),
+					actionURL: newPageLink,
+				};
+				break;
+			case 'scheduled':
+				attributes = {
+					title: translate( "You don't have any scheduled pages yet." ),
+					line: translate( 'Would you like to create one?' ),
+					action: translate( 'Start a Page' ),
+					actionURL: newPageLink,
+				};
+				break;
+			case 'trashed':
+				attributes = {
+					title: translate( "You don't have any pages in your trash folder." ),
+					line: translate( 'Everything you write is solid gold.' ),
+				};
+				break;
+			default:
+				attributes = {
+					title: translate( "You haven't published any pages yet." ),
+					line: translate( 'Would you like to publish your first page?' ),
+					action: translate( 'Start a Page' ),
+					actionURL: newPageLink,
+				};
+		}
+
+		attributes.illustration = '/calypso/images/pages/illustration-pages.svg';
+		attributes.illustrationWidth = 150;
+
+		return (
+			<EmptyContent
+				title={ attributes.title }
+				line={ attributes.line }
+				action={ attributes.action }
+				actionURL={ attributes.actionURL }
+				illustration={ attributes.illustration }
+				illustrationWidth={ attributes.illustrationWidth }
+			/>
+		);
+	},
+
+	addLoadingRows( rows, count ) {
+		for ( let i = 0; i < count; i++ ) {
+			if ( i % 4 === 0 ) {
+				rows.push( <Placeholder.Marker key={ 'placeholder-marker-' + i } /> );
 			}
-		},
-
-		renderLoading() {
-			const rows = [];
-			this.addLoadingRows( rows, 1 );
-
-			return (
-				<div id="pages" className="pages__page-list">
-					{ rows }
-				</div>
+			rows.push(
+				<Placeholder.Page key={ 'placeholder-page-' + i } multisite={ ! this.props.site } />
 			);
-		},
+		}
+	},
 
-		renderPagesList( { pages } ) {
-			const { site } = this.props;
-			const status = this.props.status || 'published';
+	renderLoading() {
+		const rows = [];
+		this.addLoadingRows( rows, 1 );
 
-			// Pages only display hierarchically for published pages on single-sites when
-			// there are 100 or fewer pages and no more pages to load (last page).
-			// Pages are not displayed hierarchically for search.
-			if (
-				site &&
-				status === 'published' &&
-				this.props.lastPage &&
-				pages.length <= 100 &&
-				! this.props.search
-			) {
-				return this.renderHierarchical( { pages, site } );
-			}
+		return (
+			<div id="pages" className="pages__page-list">
+				{ rows }
+			</div>
+		);
+	},
 
-			return this.renderChronological( { pages, site } );
-		},
+	renderPagesList( { pages } ) {
+		const { site } = this.props;
+		const status = this.props.status || 'published';
 
-		renderHierarchical( { pages, site } ) {
-			pages = sortPagesHierarchically( pages );
-			const rows = pages.map( function( page ) {
-				return (
-					<Page
-						key={ 'page-' + page.global_ID }
-						page={ page }
-						site={ site }
-						multisite={ false }
-						hierarchical={ true }
-						hierarchyLevel={ page.indentLevel || 0 }
-					/>
-				);
-			}, this );
+		// Pages only display hierarchically for published pages on single-sites when
+		// there are 100 or fewer pages and no more pages to load (last page).
+		// Pages are not displayed hierarchically for search.
+		if (
+			site &&
+			status === 'published' &&
+			this.props.lastPage &&
+			pages.length <= 100 &&
+			! this.props.search
+		) {
+			return this.renderHierarchical( { pages, site } );
+		}
 
+		return this.renderChronological( { pages, site } );
+	},
+
+	renderHierarchical( { pages, site } ) {
+		pages = sortPagesHierarchically( pages );
+		const rows = pages.map( function( page ) {
 			return (
-				<div id="pages" className="pages__page-list">
-					<BlogPostsPage key="blog-posts-page" site={ site } pages={ pages } />
-					{ rows }
-				</div>
+				<Page
+					key={ 'page-' + page.global_ID }
+					page={ page }
+					site={ site }
+					multisite={ false }
+					hierarchical={ true }
+					hierarchyLevel={ page.indentLevel || 0 }
+				/>
 			);
-		},
+		}, this );
 
-		renderChronological( { pages, site } ) {
-			if ( ! this.props.search ) {
-				// we're listing in reverse chrono. use the markers.
-				pages = this._insertTimeMarkers( pages );
-			}
-			const rows = pages.map( function( page ) {
-				if ( ! ( 'site_ID' in page ) ) {
-					return page;
-				}
-
-				// Render each page
-				return (
-					<Page
-						key={ 'page-' + page.global_ID }
-						page={ page }
-						multisite={ this.props.siteId === null }
-					/>
-				);
-			}, this );
-
-			if ( this.props.loading ) {
-				this.addLoadingRows( rows, 1 );
-			}
-
-			const blogPostsPage = site &&
-			status === 'published' && (
+		return (
+			<div id="pages" className="pages__page-list">
 				<BlogPostsPage key="blog-posts-page" site={ site } pages={ pages } />
-			);
+				{ rows }
+			</div>
+		);
+	},
 
-			return (
-				<div id="pages" className="pages__page-list">
-					{ blogPostsPage }
-					{ rows }
-					{ this.props.lastPage && pages.length ? <ListEnd /> : null }
-				</div>
-			);
-		},
-
-		renderNoContent() {
-			return (
-				<div id="pages" className="pages__page-list">
-					<div key="page-list-no-results">{ this.getNoContentMessage() }</div>
-				</div>
-			);
-		},
-
-		render() {
-			const { hasSites, loading, pages } = this.props;
-
-			if ( pages.length && hasSites ) {
-				return this.renderPagesList( { pages } );
+	renderChronological( { pages, site } ) {
+		if ( ! this.props.search ) {
+			// we're listing in reverse chrono. use the markers.
+			pages = this._insertTimeMarkers( pages );
+		}
+		const rows = pages.map( function( page ) {
+			if ( ! ( 'site_ID' in page ) ) {
+				return page;
 			}
 
-			if ( ! loading && hasSites ) {
-				return this.renderNoContent();
-			}
+			// Render each page
+			return (
+				<Page
+					key={ 'page-' + page.global_ID }
+					page={ page }
+					multisite={ this.props.siteId === null }
+				/>
+			);
+		}, this );
 
-			return this.renderLoading();
-		},
-	} )
-);
+		if ( this.props.loading ) {
+			this.addLoadingRows( rows, 1 );
+		}
+
+		const blogPostsPage = site &&
+		status === 'published' && <BlogPostsPage key="blog-posts-page" site={ site } pages={ pages } />;
+
+		return (
+			<div id="pages" className="pages__page-list">
+				{ blogPostsPage }
+				{ rows }
+				{ this.props.lastPage && pages.length ? <ListEnd /> : null }
+			</div>
+		);
+	},
+
+	renderNoContent() {
+		return (
+			<div id="pages" className="pages__page-list">
+				<div key="page-list-no-results">{ this.getNoContentMessage() }</div>
+			</div>
+		);
+	},
+
+	render() {
+		const { hasSites, loading, pages } = this.props;
+
+		if ( pages.length && hasSites ) {
+			return this.renderPagesList( { pages } );
+		}
+
+		if ( ! loading && hasSites ) {
+			return this.renderNoContent();
+		}
+
+		return this.renderLoading();
+	},
+} );
 
 const mapState = ( state, { query, siteId } ) => ( {
 	hasSites: hasInitializedSites( state ),
@@ -357,4 +353,4 @@ const mapState = ( state, { query, siteId } ) => ( {
 	site: getSite( state, siteId ),
 } );
 
-const ConnectedPages = connect( mapState )( Pages );
+const ConnectedPages = flow( localize, connect( mapState ) )( Pages );
