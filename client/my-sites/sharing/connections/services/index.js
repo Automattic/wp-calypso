e87b1de5b3ba296d4735cf99e6d9ca33af026ab2
Jake <samouri@users.noreply.github.com>
2017-10-19 13:24:39 -0400

    MySites Sharing: give services a shimmed hasOwnProperty (#18845)

--- client/my-sites/sharing/connections/services/index.js
+++ client/my-sites/sharing/connections/services/index.js
@@ -2,3 +2,6 @@
 export eventbrite from './eventbrite';
 export instagram from './instagram';
 export google_photos from './google-photos';
+
+const services = new Set( [ 'eventbrite', 'instagram', 'google_photos' ] );
+export const hasOwnProperty = name => services.has( name );
