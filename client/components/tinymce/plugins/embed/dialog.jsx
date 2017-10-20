d3b7d9da6941a44822c2d3c9e7f9b3df8151d855
Biser Perchinkov <biser@bisko.be>
2017-10-20 18:43:48 +0300

    Add a loader/spinner while the embeds are loading from the API

--- client/components/tinymce/plugins/embed/dialog.jsx
+++ client/components/tinymce/plugins/embed/dialog.jsx
@@ -19,6 +19,7 @@ import EmbedContainer from 'components/embed-container';
 import FormTextInput from 'components/forms/form-text-input';
 import wpcom from 'lib/wp';
 import { getSelectedSiteId } from 'state/ui/selectors';
+import Spinner from 'components/spinner';
 
 /*
  * Shows the URL and preview of an embed, and allows it to be edited.
@@ -45,9 +46,9 @@ export class EmbedDialog extends React.Component {
 	state = {
 		embedUrl: this.props.embedUrl,
 		previewUrl: this.props.embedUrl,
-			// might be simpler to get rid of previewUrl and just use embedUrl
-			// but i think that'd introduce extra unnecessary render() calls
-			// might not to revisit that now that using previewMarkup, though
+		// might be simpler to get rid of previewUrl and just use embedUrl
+		// but i think that'd introduce extra unnecessary render() calls
+		// might not to revisit that now that using previewMarkup, though
 		previewMarkup: [],
 	};
 
@@ -73,11 +74,16 @@ export class EmbedDialog extends React.Component {
 				return;
 			}
 
+			this.setState( { isLoading: true } );
+
 			// Fetch fresh data from the API
-			wpcom.undocumented().site( this.props.siteId ).embeds(
-				{ embed_url: this.state.embedUrl },
-				( error, data ) => {
+			wpcom
+				.undocumented()
+				.site( this.props.siteId )
+				.embeds( { embed_url: this.state.embedUrl }, ( error, data ) => {
 					const { previewMarkup, embedUrl } = this.state;
+
+					this.setState( { isLoading: false } );
 
 					let cachedMarkup;
 
@@ -85,35 +91,31 @@ export class EmbedDialog extends React.Component {
 						cachedMarkup = data.result;
 						// todo need to do more to check that data.result is valid before using it?
 					} else {
-						console.log('lookup error:', error);
-							// todo add details? or just generic error message
+						console.log( 'lookup error:', error );
+						// todo add details? or just generic error message
 
 						cachedMarkup = 'error foo';
 
 						// todo handle errors
-							// xhr errors in `error` var
-							// and also application layer errors in `data.error` or however wpcom.js signals a error to the caller
-							// add unit tests for those. mock the xhr
+						// xhr errors in `error` var
+						// and also application layer errors in `data.error` or however wpcom.js signals a error to the caller
+						// add unit tests for those. mock the xhr
 
 						// todo show an error in the preview box if fetching the embed failed / it's an invalid URL
-							// right now it just continues showing the last valid preview
-							// don't wanna do if they're still typing though. debounce might be enough to fix that, but still could be annoying.
-							// need to play with
-							// how to detect from the markup if it was an error? it'll be dependent on the service etc, right? or maybe wpcom.js normalizes it into a standard error format?
+						// right now it just continues showing the last valid preview
+						// don't wanna do if they're still typing though. debounce might be enough to fix that, but still could be annoying.
+						// need to play with
+						// how to detect from the markup if it was an error? it'll be dependent on the service etc, right? or maybe wpcom.js normalizes it into a standard error format?
 					}
 
 					this.setState( {
 						// SECURITY WARNING: The value of previewMarkup is later used with
 						// dangerouslySetInnerHtml, so it must never be changed to include
 						// untrusted data.
-						previewMarkup: assign(
-							previewMarkup,
-							{ [ embedUrl ]: cachedMarkup }
-						),
+						previewMarkup: assign( previewMarkup, { [ embedUrl ]: cachedMarkup } ),
 						// todo merge() or other function would be better than assign() ^^^?
 					} );
-				}
-			);
+				} );
 		}, 500 );
 		// todo this doesn't need to be inside compwillmount? it can just be regular function below?
 		// that'd be cleaner and more consistent, but i was thinking it had to be declared in here for some reason. not sure about that anymore.
@@ -135,21 +137,24 @@ export class EmbedDialog extends React.Component {
 	 * @param {object} nextProps The properties that will be received.
 	 */
 	componentWillReceiveProps = nextProps => {
-		this.setState( {
-			embedUrl: nextProps.embedUrl,
-			previewUrl: nextProps.embedUrl,
-		}, () => {
-			// Refresh the preview
-			this.debouncedUpdateEmbedPreview();
+		this.setState(
+			{
+				embedUrl: nextProps.embedUrl,
+				previewUrl: nextProps.embedUrl,
+			},
+			() => {
+				// Refresh the preview
+				this.debouncedUpdateEmbedPreview();
 				// todo should probably call immediately w/ .flush() instead of waiting for debounce
 				// todo maybe pass new value directly instead of waiting for setstate to completely asyncronously?
 				// todo should probably only call if props.isvisible? otherwise calling when closing the dialog, which is wasteful and unnecessary
-		} );
+			}
+		);
 
 		// todo this whole flow is getting a little complicated.
-			// state is updated here, and also updated in the debounced function, and maybe other places.
-			// and the functions are dependent on whether or not the state has been updated, etc.
-			// try to simplify everything
+		// state is updated here, and also updated in the debounced function, and maybe other places.
+		// and the functions are dependent on whether or not the state has been updated, etc.
+		// try to simplify everything
 	};
 
 	onChangeEmbedUrl = event => {
@@ -161,9 +166,9 @@ export class EmbedDialog extends React.Component {
 		} );
 
 		// todo when you click Update, the focus is jumping back to the start of the editor, instead of the position it was in before opening the dialog
-			// doesn't happen when click Cancel, though
-			// might not be impactful enough to spend time fixing
-			// might be easy to fix with wpcom-view::select() or editor.selection.set() or something, though
+		// doesn't happen when click Cancel, though
+		// might not be impactful enough to spend time fixing
+		// might be easy to fix with wpcom-view::select() or editor.selection.set() or something, though
 	};
 
 	onUpdate = () => {
@@ -207,9 +212,13 @@ export class EmbedDialog extends React.Component {
 					onKeyDown={ this.onKeyDownEmbedUrl }
 				/>
 
+				{ this.state.isLoading && (
+					<div className="embed__loading">
+						<Spinner className="embed__loading-spinner" size={ 20 } />
+					</div>
+				) }
 
-
-				{/* todo another approach would be to use an iframe instead of a div
+				{ /* todo another approach would be to use an iframe instead of a div
 				<iframe className="embed__preview" srcDoc={ this.state.previewMarkup[ this.state.previewUrl ] } />
 
 				that feels a bit safer because we can sandbox it to improve security, but maybe it's not needed because the markup
@@ -222,19 +231,27 @@ export class EmbedDialog extends React.Component {
 				need to use ResizeableIframe to deal w/ unknown dimensions of various embeds?
 				maybe try again to just use EmbedView, even if you have to modify it to accept a url prop or something
 					use it directly, though, don't mess with EmbedViewManager
-				*/}
+				*/ }
 
-				{/* This is safe because previewMarkup comes from our API endpoint */}
-					{/* are you sure that makes it safe? get security review */}
-				{ this.state.previewMarkup[ this.state.previewUrl ] &&
+				{ /* This is safe because previewMarkup comes from our API endpoint */ }
+				{ /* are you sure that makes it safe? get security review */ }
+
+				{ ! this.state.isLoading &&
+				this.state.previewMarkup[ this.state.previewUrl ] && (
 					<EmbedContainer>
-						<div className="embed__preview" dangerouslySetInnerHTML={ { '__html' : this.state.previewMarkup[ this.state.previewUrl ] } } />
+						<div
+							className="embed__preview"
+							dangerouslySetInnerHTML={ {
+								__html: this.state.previewMarkup[ this.state.previewUrl ],
+							} }
+						/>
 					</EmbedContainer>
-				}
+				) }
 			</Dialog>
 		);
 
-		{/* todo
+		{
+			/* todo
 
 			test various embed services, both whitelisted and generic oembed
 				not embedding correctly in editor, before preview, but maybe these aren't supposed to be handled by wpcom-view/embed?
@@ -252,17 +269,18 @@ export class EmbedDialog extends React.Component {
 			chrome console warning: "Refused to display 'https://www.facebook.com/xti.php?xt=AZW4yx3ElFj8wS2zZ0awYZYeCHQX9GKfcaP5HUVhjVUJe2rhI0dScOBaE4IALljF-1-71pVsHv4FYfcrV6ozzCPdIBMFedrUFTypnEMwzlYEJ9YvnQhpZG4rWmmMFM2YjhtMLSx-PiBt9vIW1iFuPVuadBkqQffHDRynmqZNDMSykA&isv=1&cts=1508425830&csp' in a frame because it set 'X-Frame-Options' to 'sameorigin'."
 				it happens intermittently, though, and the embed still works, so not sure if it's an issue
 
-		*/}
+		*/
+		}
 	}
 }
 
 const connectedEmbedDialog = connect( ( state, { siteId } ) => {
 	return {
-		siteId: siteId ? siteId : getSelectedSiteId( state )
-	}
+		siteId: siteId ? siteId : getSelectedSiteId( state ),
+	};
 } )( EmbedDialog );
 
 export default localize( connectedEmbedDialog );
-	// todo should localize() and then connect()? for consistency in dev tools element tree?
-	// the code reads cleaning having it this way, though, since localize() is concise enough to fit in the export statement,
-	// but connect() needs several lines and extra logic, so it's nicer to have it as a separate piece
+// todo should localize() and then connect()? for consistency in dev tools element tree?
+// the code reads cleaning having it this way, though, since localize() is concise enough to fit in the export statement,
+// but connect() needs several lines and extra logic, so it's nicer to have it as a separate piece
