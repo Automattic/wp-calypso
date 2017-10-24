cab57bce807391786fcc30fefe2d9c341f9f037b
Jon Surrell <jon.surrell@automattic.com>
2017-10-24 09:14:32 +0200

    Pre-commit: Do not prettier + add dirty files (#18868)
    * Disable auto prettier and commit for dirty files
    * Unify git output parsing logic, simplify file match
    * Remove redundant path operation, use relative path

--- bin/pre-commit-hook.js
+++ bin/pre-commit-hook.js
@@ -12,7 +12,6 @@ const spawnSync = require( 'child_process' ).spawnSync;
 const chalk = require( 'chalk' );
 const fs = require( 'fs' );
 const prettier = require( 'prettier' );
-const path = require( 'path' );
 
 /**
  * Internal dependencies
@@ -28,24 +27,48 @@ console.log(
 // Make quick pass over config files on every change
 require( '../server/config/validate-config-keys' );
 
-const files = execSync( 'git diff --cached --name-only --diff-filter=ACM' )
-	.toString()
-	.split( '\n' )
-	.map( name => name.trim() )
-	.filter( name => name.endsWith( '.js' ) || name.endsWith( '.jsx' ) );
+/**
+ * Parses the output of a git diff command into javascript file paths.
+ *
+ * @param   {String} command Command to run. Expects output like `git diff --name-only […]`
+ * @returns {Array}          Paths output from git command
+ */
+function parseGitDiffToPathArray( command ) {
+	return execSync( command )
+		.toString()
+		.split( '\n' )
+		.map( name => name.trim() )
+		.filter( name => name.endsWith( '.js' ) || name.endsWith( '.jsx' ) );
+}
+
+const dirtyFiles = new Set( parseGitDiffToPathArray( 'git diff --name-only --diff-filter=ACM' ) );
+
+const files = parseGitDiffToPathArray( 'git diff --cached --name-only --diff-filter=ACM' );
 
 // run prettier for any files in the commit that have @format within their first docblock
-files
-	.map( file => path.join( __dirname, '../', file ) )
-	.forEach( file => {
-		const text = fs.readFileSync( file, 'utf8' );
-		if ( shouldFormat( text ) ) {
-			console.log( `Prettier formatting file: ${ file } because it contains the @format flag` );
-			const formattedText = prettier.format( text, {} );
-			fs.writeFileSync( file, formattedText );
-			execSync( `git add ${ file }` );
+files.forEach( file => {
+	const text = fs.readFileSync( file, 'utf8' );
+	if ( shouldFormat( text ) ) {
+		// File has unstaged changes. It's a bad idea to modify and add it before commit.
+		if ( dirtyFiles.has( file ) ) {
+			console.log(
+				chalk.red( `${ file } will not be auto-formatted because it has unstaged changes.` )
+			);
+			return;
 		}
-	} );
+
+		const formattedText = prettier.format( text, {} );
+
+		// No change required.
+		if ( text === formattedText ) {
+			return;
+		}
+
+		fs.writeFileSync( file, formattedText );
+		console.log( `Prettier formatting file: ${ file } because it contains the @format flag` );
+		execSync( `git add ${ file }` );
+	}
+} );
 
 // linting should happen after formatting
 const lintResult = spawnSync( 'eslint-eslines', [ ...files, '--', '--diff=index' ], {
