289522702053d4761904e0facdffb2b513ec8469
Dennis Snell <dennis.snell@automattic.com>
2017-10-24 16:13:46 -0400

    Rewind: Update existing state, refactor
    This is a preparatory PR for work on the **Activity Log**.
    
    @see #18944 for prior work
    
    In this PR we're simplifying some reducer code and trying to simplify
    thr structure to make it easier to see what's occurring in state while
    the app is running and loading in remote data.

--- client/state/activity-log/log/reducer.js
+++ client/state/activity-log/log/reducer.js
@@ -1,41 +1,29 @@
 /** @format */
 /**
- * External dependencies
- */
-import { mapValues } from 'lodash';
-
-/**
  * Internal dependencies
  */
 import ActivityQueryManager from 'lib/query-manager/activity';
 import { ACTIVITY_LOG_UPDATE, DESERIALIZE, SERIALIZE } from 'state/action-types';
-import { createReducer, isValidStateWithSchema } from 'state/utils';
+import { isValidStateWithSchema, keyedReducer } from 'state/utils';
 import { logItemsSchema } from './schema';
 
-export const logItems = createReducer(
-	{},
-	{
-		[ ACTIVITY_LOG_UPDATE ]: ( state, { data, found, query, siteId } ) => {
-			return state[ siteId ]
-				? {
-						...state,
-						[ siteId ]: state[ siteId ].receive( data, { found, query } ),
-					}
-				: {
-						...state,
-						[ siteId ]: new ActivityQueryManager().receive( data, { found, query } ),
-					};
-		},
-		[ DESERIALIZE ]: state => {
-			if ( ! isValidStateWithSchema( state, logItemsSchema ) ) {
-				return {};
-			}
-			return mapValues( state, ( { data, options } ) => {
-				return new ActivityQueryManager( data, options );
-			} );
-		},
-		[ SERIALIZE ]: state => {
-			return mapValues( state, ( { data, options } ) => ( { data, options } ) );
-		},
+export const logItem = ( state = undefined, { type, data, found, query } ) => {
+	switch ( type ) {
+		case ACTIVITY_LOG_UPDATE:
+			return ( state || new ActivityQueryManager() ).receive( data, { found, query } );
+
+		case DESERIALIZE:
+			return isValidStateWithSchema( state, logItemsSchema )
+				? new ActivityQueryManager( state.data, state.options )
+				: undefined;
+
+		case SERIALIZE:
+			return { data: state.data, options: state.options };
+
+		default:
+			return state;
 	}
-);
+};
+
+export const logItems = keyedReducer( 'siteId', logItem, [ DESERIALIZE, SERIALIZE ] );
+logItems.hasCustomPersistence = true;
