a1de018142fcd9cac43378c25d0672c7627f593a
Kirk Wight <kwight@kwight.ca>
2017-10-24 16:50:55 -0700

    Update the CommentButton block to be able to accept a link and target.

--- client/blocks/comment-button/index.jsx
+++ client/blocks/comment-button/index.jsx
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import { noop } from 'lodash';
+import { isNull, noop, omitBy } from 'lodash';
 
 /**
  * Internal dependencies
@@ -18,29 +18,46 @@ import { getPostTotalCommentsCount } from 'state/comments/selectors';
 
 class CommentButton extends Component {
 	static propTypes = {
-		onClick: PropTypes.func,
-		tagName: PropTypes.string,
 		commentCount: PropTypes.number,
+		link: PropTypes.string,
+		onClick: PropTypes.func,
 		showLabel: PropTypes.bool,
+		tagName: PropTypes.string,
+		target: PropTypes.string,
 	};
 
 	static defaultProps = {
-		onClick: noop,
-		tagName: 'li',
-		size: 24,
 		commentCount: 0,
+		link: null,
+		onClick: noop,
 		showLabel: true,
+		size: 24,
+		tagName: 'li',
+		target: null,
 	};
 
 	render() {
-		const { translate, commentCount, onClick, showLabel, tagName: containerTag } = this.props;
+		const {
+			commentCount,
+			link,
+			onClick,
+			showLabel,
+			tagName: containerTag,
+			target,
+			translate,
+		} = this.props;
 
 		return React.createElement(
 			containerTag,
-			{
-				className: 'comment-button',
-				onClick,
-			},
+			omitBy(
+				{
+					className: 'comment-button',
+					href: link,
+					onClick,
+					target,
+				},
+				isNull
+			),
 			<Gridicon icon="comment" size={ this.props.size } className="comment-button__icon" />,
 			<span className="comment-button__label">
 				{ commentCount > 0 && (
