3c02fe2789f4236d123cc3576c7c8915364fe7b9
Kevin Killingsworth <kevin@redfenix.com>
2017-10-24 15:49:19 -0500

    Promotion edit: clarify model
    This clarifies certain elements of the promotion models and field
    models. It adds JSDoc comments and more descriptive variable names.

--- client/extensions/woocommerce/app/promotions/promotion-models.js
+++ client/extensions/woocommerce/app/promotions/promotion-models.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { translate as moduleTranslate } from 'i18n-calypso';
+import { translate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -10,69 +10,82 @@ import CurrencyField from './fields/currency-field';
 import PercentField from './fields/percent-field';
 import TextField from './fields/text-field';
 
-export function createModels( translate ) {
-	// -------------------------
-	// Reusable Promotion Fields
-	// -------------------------
-	const couponCode = {
-		component: TextField,
-		labelText: translate( 'Coupon code' ),
-		explanationText: translate(
-			'Only apply this promotion when the customer supplies the coupon code'
-		),
-		placeholderText: translate( 'Enter coupon code' ),
-		isRequired: true,
-	};
+/**
+ * Field reused for all coupon promotion types.
+ */
+const couponCodeField = {
+	component: TextField,
+	labelText: translate( 'Coupon code' ),
+	explanationText: translate(
+		'Only apply this promotion when the customer supplies the coupon code'
+	),
+	placeholderText: translate( 'Enter coupon code' ),
+	isRequired: true,
+};
 
-	const fixedDiscount = {
+/**
+ * Field reused for fixed currency-based coupons.
+ */
+const fixedDiscountField = {
+	labelText: translate( 'Discount', { context: 'noun' } ),
+	component: CurrencyField,
+	isRequired: true,
+};
+
+/**
+ * Promotion Type: Product Sale (e.g. $5 off the "I <3 Robots" t-shirt)
+ */
+const productSaleModel = {
+	salePrice: {
 		component: CurrencyField,
+		labelText: translate( 'Product Sale Price' ),
 		isRequired: true,
-	};
+	},
+};
 
-	// ---------------
-	// Promotion types
-	// ---------------
-	const productSaleModel = {
-		salePrice: {
-			component: CurrencyField,
-			labelText: translate( 'Product Sale Price' ),
-			isRequired: true,
-		},
-	};
+/**
+ * Promotion Type: Fixed Product Discount (e.g. $5 off any t-shirt)
+ */
+const fixedProductModel = {
+	couponCode: couponCodeField,
+	fixedDiscount: {
+		...fixedDiscountField,
+		labelText: translate( 'Product Discount', { context: 'noun' } )
+	},
+};
 
-	const fixedProductModel = {
-		couponCode,
-		fixedDiscount: {
-			...fixedDiscount,
-			labelText: translate( 'Product Discount', { context: 'noun' } )
-		},
-	};
+/**
+ * Promotion Type: Fixed Cart Discount (e.g. $10 off my cart)
+ */
+const fixedCartModel = {
+	couponCode: couponCodeField,
+	fixedDiscount: {
+		...fixedDiscountField,
+		labelText: translate( 'Cart Discount', { context: 'noun' } ),
+	},
+};
 
-	const fixedCartModel = {
-		couponCode,
-		fixedDiscount: {
-			...fixedDiscount,
-			labelText: translate( 'Cart Discount', { context: 'noun' } ),
-		},
-	};
+/**
+ * Promotion Type: Percentage Cart Discount (e.g. 10% off my cart)
+ */
+const percentCartModel = {
+	couponCode: couponCodeField,
+	percentDiscount: {
+		component: PercentField,
+		labelText: translate( 'Percent Cart Discount', { context: 'noun' } ),
+		isRequired: true,
+	},
+};
 
-	const percentCartModel = {
-		couponCode,
-		percentDiscount: {
-			component: PercentField,
-			labelText: translate( 'Percent Cart Discount', { context: 'noun' } ),
-			isRequired: true,
-		},
-	};
-
-	// Master list of models by promotion type.
-	return {
-		[ 'product_sale' ]: productSaleModel,
-		[ 'fixed_product' ]: fixedProductModel,
-		[ 'fixed_cart' ]: fixedCartModel,
-		[ 'percent' ]: percentCartModel,
-	};
-}
-
-export default createModels( moduleTranslate );
+/**
+ * Master list of models by promotion type.
+ *
+ * Note: The keys of this object correspond with promotion.type
+ */
+export default {
+	product_sale: productSaleModel,
+	fixed_product: fixedProductModel,
+	fixed_cart: fixedCartModel,
+	percent: percentCartModel,
+};
 
