5809f90cb5eb5eda2b5d8fe0831c9b54996143b2
Jacopo Tomasone <Copons@users.noreply.github.com>
2017-10-25 16:35:31 +0100

    Selectors: Add isEmailBlacklisted selector (#19136)
    Add a new `isEmailBlacklisted` selector that checks if a site blacklist contains an email address.

---
+++ client/state/selectors/is-email-blacklisted.js
@@ -0,0 +1,25 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { includes } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { getSiteSetting } from 'state/selectors';
+
+/**
+ * Check if a site blacklist contains an email address.
+ *
+ * @param {Object} state Global state tree
+ * @param {Number} siteId Site ID
+ * @param {String} email An email address.
+ * @returns {Boolean} If the blacklist contains the email address.
+ */
+export const isEmailBlacklisted = ( state, siteId, email = '' ) => {
+	const blacklist = getSiteSetting( state, siteId, 'blacklist_keys' ) || '';
+	return includes( blacklist.split( '\n' ), email );
+};
+
+export default isEmailBlacklisted;
