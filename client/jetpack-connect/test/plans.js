54af9bb61d04d4d129a6fa63182c0a47a117e56b
Jon Surrell <jon.surrell@automattic.com>
2017-10-25 17:44:06 +0200

    Jetpack connect: Add test for hasPlan update redirect (#19145)
    * Unmount test mount
    
      See http://airbnb.io/enzyme/docs/api/mount.html#full-rendering-api-mount
    
    * Add and export DEFAULT_PROPS fixture
    * Add redirect test for hasPlan

--- client/jetpack-connect/test/plans.js
+++ client/jetpack-connect/test/plans.js
@@ -11,7 +11,13 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import PlansWrapper, { getSitePlans, SELECTED_SITE, SITE_PLAN_PRO } from './lib/plans';
+import PlansWrapper, {
+	DEFAULT_PROPS,
+	getSitePlans,
+	SELECTED_SITE,
+	SITE_PLAN_PRO,
+} from './lib/plans';
+import { PlansTestComponent as Plans } from '../plans';
 import { PLAN_JETPACK_BUSINESS } from 'lib/plans/constants';
 
 jest.mock( 'components/data/query-plans', () => 'components--data--query-plans' );
@@ -37,10 +43,25 @@ describe( 'Plans', () => {
 		expect( wrapper ).toMatchSnapshot();
 	} );
 
+	test( 'should redirect on update from free to paid plan', () => {
+		const wrapper = mount( <Plans { ...DEFAULT_PROPS } /> );
+
+		const redirect = ( wrapper.instance().redirect = jest.fn() );
+
+		wrapper.setProps( {
+			selectedSite: { ...SELECTED_SITE, plan: SITE_PLAN_PRO },
+			sitePlans: getSitePlans( PLAN_JETPACK_BUSINESS ),
+		} );
+
+		expect( redirect.mock.calls.length ).toBe( 1 );
+
+		wrapper.unmount();
+	} );
+
 	test( 'should redirect if Atomic', () => {
 		const goBackToWpAdmin = jest.fn();
 
-		mount(
+		const wrapper = mount(
 			<PlansWrapper
 				goBackToWpAdmin={ goBackToWpAdmin }
 				isAutomatedTransfer={ true }
@@ -54,5 +75,7 @@ describe( 'Plans', () => {
 		);
 
 		expect( goBackToWpAdmin.mock.calls.length ).toBe( 1 );
+
+		wrapper.unmount();
 	} );
 } );
