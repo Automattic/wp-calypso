9d53aa76b6d238275d968a9014179fefe67d21bc
James Nylen <jnylen@gmail.com>
2017-10-25 19:14:18 +0800

    Better fix for calling `getEditorPath` without a site ID
    Calling this selector without a post ID is still valid.  Calling it
    without a site ID is not really valid, but happens anyway while
    `ResumeEditing` is loading.

--- client/state/ui/editor/selectors.js
+++ client/state/ui/editor/selectors.js
@@ -89,7 +89,10 @@ export function getEditorNewPostPath( state, siteId, type = 'post' ) {
  * @return {String}             Editor URL path
  */
 export function getEditorPath( state, siteId, postId, defaultType = 'post' ) {
-	const editedPost = siteId && postId && getEditedPost( state, siteId, postId );
+	if ( ! siteId ) {
+		return '/post';
+	}
+	const editedPost = getEditedPost( state, siteId, postId );
 	const type = get( editedPost, 'type', defaultType );
 	let path = getEditorNewPostPath( state, siteId, type );
 
