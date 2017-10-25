4b72acb4b2dbbfa257f69fc5fa2616f82a370ef4
Ben Lowery <ben.lowery@automattic.com>
2017-10-25 11:57:08 -0400

    add tests

--- client/lib/feed-post-store/post-fetcher.js
+++ client/lib/feed-post-store/post-fetcher.js
@@ -9,7 +9,7 @@ import { assign, noop, pick, pickBy } from 'lodash';
 /**
  * Internal Dependencies
  */
-import FeedPostStore from './';
+import FeedPostStore from 'lib/feed-post-store';
 
 function PostFetcher( options ) {
 	assign(
