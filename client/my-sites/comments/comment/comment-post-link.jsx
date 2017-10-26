aac0bcd76dca034b0a685e06a5908d3403cb9725
Copons <copons@users.noreply.github.com>
2017-10-26 15:31:08 +0100

    Properly style all the things

--- client/my-sites/comments/comment/comment-post-link.jsx
+++ client/my-sites/comments/comment/comment-post-link.jsx
@@ -11,13 +11,25 @@ import { get } from 'lodash';
 /**
  * Internal dependencies
  */
-import { decodeEntities } from 'lib/formatting';
+import QueryPosts from 'components/data/query-posts';
+import { decodeEntities, stripHTML } from 'lib/formatting';
 import { getSiteComment } from 'state/selectors';
+import { getSitePost } from 'state/posts/selectors';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
 
-const CommentPostLink = ( { siteSlug, postId, postTitle, translate } ) => (
+const CommentPostLink = ( {
+	isPostTitleLoaded,
+	postId,
+	postTitle,
+	siteId,
+	siteSlug,
+	translate,
+} ) => (
 	<div className="comment__post-link">
+		{ ! isPostTitleLoaded && <QueryPosts siteId={ siteId } postId={ postId } /> }
+
 		<Gridicon icon="chevron-right" size={ 18 } />
+
 		<a href={ `/comments/all/${ siteSlug }/${ postId }` }>
 			{ postTitle || translate( 'Untitled' ) }
 		</a>
@@ -26,12 +38,21 @@ const CommentPostLink = ( { siteSlug, postId, postTitle, translate } ) => (
 
 const mapStateToProps = ( state, { commentId } ) => {
 	const siteId = getSelectedSiteId( state );
+	const siteSlug = getSelectedSiteSlug( state );
+
 	const comment = getSiteComment( state, siteId, commentId );
 
+	const postId = get( comment, 'post.ID' );
+	const post = getSitePost( state, siteId, postId );
+	const postTitle =
+		decodeEntities( get( comment, 'post.title' ) ) ||
+		decodeEntities( stripHTML( get( post, 'excerpt' ) ) );
+
 	return {
-		postId: get( comment, 'post.ID' ),
-		postTitle: decodeEntities( get( comment, 'post.title' ) ),
-		siteSlug: getSelectedSiteSlug( state ),
+		isPostTitleLoaded: !! postTitle || !! post,
+		postId,
+		postTitle,
+		siteSlug,
 	};
 };
 
