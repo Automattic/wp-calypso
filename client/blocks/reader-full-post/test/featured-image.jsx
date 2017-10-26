3e33ba692aab2a1001c1df01db8db80809d8712c
bluefuton <chris@bluefuton.com>
2017-10-26 17:53:56 +0100

    Fix test typo

--- client/blocks/reader-full-post/test/featured-image.jsx
+++ client/blocks/reader-full-post/test/featured-image.jsx
@@ -13,8 +13,8 @@ import FeaturedImage from '../featured-image';
 
 describe( 'FeaturedImage', () => {
 	test( 'sets the source to an empty string if the image fails to load', () => {
-		const nonExistantImage = 'http://sketchy-feed.com/missing-image-2.jpg';
-		const wrapper = shallow( <FeaturedImage src={ nonExistantImage } /> );
+		const nonExistentImage = 'http://sketchy-feed.com/missing-image-2.jpg';
+		const wrapper = shallow( <FeaturedImage src={ nonExistentImage } /> );
 		wrapper.instance().handleImageError();
 		assert.equal( '', wrapper.state( 'src' ) );
 	} );
