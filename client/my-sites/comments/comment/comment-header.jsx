7ff7edadff3ddc2456cd9e2efb5cca3fd4f7f905
Jacopo Tomasone <Copons@users.noreply.github.com>
2017-10-26 11:19:24 +0100

    Comments: Redesign the component (Part 1) (#19082)
    This PR is the first part of a full refactor (HTML, CSS, Redux) of `CommentDetail` for the new visual redesign.

---
+++ client/my-sites/comments/comment/comment-header.jsx
@@ -0,0 +1,49 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import React from 'react';
+import Gridicon from 'gridicons';
+import noop from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import Button from 'components/button';
+import CommentAuthor from 'my-sites/comments/comment/comment-author';
+import CommentAuthorMoreInfo from 'my-sites/comments/comment/comment-author-more-info';
+import FormCheckbox from 'components/forms/form-checkbox';
+
+export const CommentHeader = ( {
+	commentId,
+	isBulkMode,
+	isEditMode,
+	isExpanded,
+	isSelected,
+	toggleExpanded,
+} ) => (
+	<div className="comment__header">
+		{ isBulkMode && (
+			<label className="comment__bulk-select">
+				<FormCheckbox checked={ isSelected } onChange={ noop } />
+			</label>
+		) }
+
+		<CommentAuthor { ...{ commentId, isExpanded } } />
+
+		{ isExpanded && <CommentAuthorMoreInfo /> }
+
+		{ ! isBulkMode && (
+			<Button
+				borderless
+				className="comment__toggle-expanded"
+				disabled={ isEditMode }
+				onClick={ toggleExpanded }
+			>
+				<Gridicon icon="chevron-down" />
+			</Button>
+		) }
+	</div>
+);
+
+export default CommentHeader;
