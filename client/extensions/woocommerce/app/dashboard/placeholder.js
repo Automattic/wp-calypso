d3c46630d2a48a3e38d36daa1b627d3097d8782e
Timmy Crawford <timmydcrawford@gmail.com>
2017-10-26 15:41:13 -0700

    Store: Add loading placeholder to dashboard.

---
+++ client/extensions/woocommerce/app/dashboard/placeholder.js
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ *
+ * @format
+ */
+
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+
+const DashboardPlaceholder = () => {
+	return (
+		<div className="dashboard__placeholder">
+			<Card />
+			<Card />
+			<Card />
+		</div>
+	);
+};
+
+export default DashboardPlaceholder;
