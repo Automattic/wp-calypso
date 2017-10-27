80c3ce75603a78c16b12c255a85a10b809a4066a
Timmy Crawford <timmydcrawford@gmail.com>
2017-10-27 11:33:48 -0700

    Updates per feedback

--- client/extensions/woocommerce/app/dashboard/placeholder.js
+++ client/extensions/woocommerce/app/dashboard/placeholder.js
@@ -9,14 +9,19 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import Card from 'components/card';
+import BasicWidget from 'woocommerce/components/basic-widget';
+import WidgetGroup from 'woocommerce/components/widget-group';
 
 const DashboardPlaceholder = () => {
+	const loading = <BasicWidget title="…" className="dashboard__placeholder-small" />;
+
 	return (
 		<div className="dashboard__placeholder">
-			<Card />
-			<Card />
-			<Card />
+			<BasicWidget className="dashboard__placeholder-large card" />
+			<WidgetGroup>
+				{ loading }
+				{ loading }
+			</WidgetGroup>
 		</div>
 	);
 };
