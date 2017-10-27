0f61c3896246a3625332d070e510e93930e31d21
Paul Dechov <dechovsky@gmail.com>
2017-10-27 10:00:16 -0400

    Store: Label details modal in order activity log, and remove details after refund request (#19095)

---
+++ client/extensions/woocommerce/woocommerce-services/views/shipping-label/label-details-modal.js
@@ -0,0 +1,77 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Dialog from 'components/dialog';
+import ActionButtons from 'woocommerce/woocommerce-services/components/action-buttons';
+import FormSectionHeading from 'components/forms/form-section-heading';
+import { closeDetailsDialog } from 'woocommerce/woocommerce-services/state/shipping-label/actions';
+import { isLoaded, getShippingLabel } from 'woocommerce/woocommerce-services/state/shipping-label/selectors';
+
+const DetailsDialog = ( props ) => {
+	const { orderId, siteId, isVisible, labelIndex, serviceName, packageName, productNames, translate } = props;
+
+	const onClose = () => props.closeDetailsDialog( orderId, siteId );
+	return (
+		<Dialog
+			additionalClassNames="label-details-modal woocommerce"
+			isVisible={ isVisible }
+			onClose={ onClose }>
+			<FormSectionHeading>
+				{ translate( 'Label #%(labelIndex)s details', { args: { labelIndex: labelIndex + 1 } } ) }
+			</FormSectionHeading>
+			<dl>
+				<dt>{ translate( 'Service' ) }</dt>
+				<dd>{ serviceName }</dd>
+
+				<dt>{ translate( 'Package' ) }</dt>
+				<dd>{ packageName }</dd>
+
+				<dt>{ translate( 'Items' ) }</dt>
+				<dd>
+					<ul>
+						{ productNames.map( productName => <li>{ productName }</li> ) }
+					</ul>
+				</dd>
+			</dl>
+			<ActionButtons buttons={ [
+				{
+					onClick: onClose,
+					label: translate( 'Close' ),
+				},
+			] } />
+		</Dialog>
+	);
+};
+
+DetailsDialog.propTypes = {
+	siteId: PropTypes.number.isRequired,
+	orderId: PropTypes.number.isRequired,
+	isVisible: PropTypes.bool,
+	serviceName: PropTypes.string,
+	packageName: PropTypes.string,
+	productNames: PropTypes.array,
+	closeDetailsDialog: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = ( state, { orderId, siteId, labelId } ) => {
+	const loaded = isLoaded( state, orderId, siteId );
+	const { detailsDialog } = getShippingLabel( state, orderId, siteId );
+	return {
+		isVisible: Boolean( loaded && detailsDialog && detailsDialog.labelId === labelId ),
+	};
+};
+
+const mapDispatchToProps = ( dispatch ) => {
+	return bindActionCreators( { closeDetailsDialog }, dispatch );
+};
+
+export default connect( mapStateToProps, mapDispatchToProps )( localize( DetailsDialog ) );
