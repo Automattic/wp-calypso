a03e4e4b38cc92da341591b5f96c13b79b7db01e
Jon Surrell <jon.surrell@automattic.com>
2017-10-27 11:37:38 +0200

    Separate jetpackConnectAuthorize reducer and tests

--- client/state/jetpack-connect/reducer/index.js
+++ client/state/jetpack-connect/reducer/index.js
@@ -1,136 +1,14 @@
 /** @format */
 /**
- * External dependencis
- */
-import { isEmpty, omit } from 'lodash';
-
-/**
  * Internal dependencies
  */
 import jetpackAuthAttempts from './jetpack-auth-attempts';
+import jetpackConnectAuthorize from './jetpack-connect-authorize';
 import jetpackConnectSelectedPlans from './jetpack-connect-selected-plans';
 import jetpackConnectSessions from './jetpack-connect-site';
 import jetpackConnectSite from './jetpack-connect-site';
 import jetpackSSO from './jetpack-sso';
-import {
-	JETPACK_CONNECT_COMPLETE_FLOW,
-	JETPACK_CONNECT_QUERY_SET,
-	JETPACK_CONNECT_AUTHORIZE,
-	JETPACK_CONNECT_AUTHORIZE_LOGIN_COMPLETE,
-	JETPACK_CONNECT_AUTHORIZE_RECEIVE,
-	JETPACK_CONNECT_AUTHORIZE_RECEIVE_SITE_LIST,
-	JETPACK_CONNECT_CREATE_ACCOUNT,
-	JETPACK_CONNECT_CREATE_ACCOUNT_RECEIVE,
-	JETPACK_CONNECT_REDIRECT_WP_ADMIN,
-	JETPACK_CONNECT_REDIRECT_XMLRPC_ERROR_FALLBACK_URL,
-	JETPACK_CONNECT_USER_ALREADY_CONNECTED,
-	SITE_REQUEST_FAILURE,
-	SERIALIZE,
-	DESERIALIZE,
-} from 'state/action-types';
 import { combineReducers } from 'state/utils';
-import { isStale } from '../utils';
-import { JETPACK_CONNECT_AUTHORIZE_TTL } from '../constants';
-
-function buildDefaultAuthorizeState() {
-	return {
-		queryObject: {},
-		isAuthorizing: false,
-		authorizeSuccess: false,
-		authorizeError: false,
-		timestamp: Date.now(),
-		userAlreadyConnected: false,
-	};
-}
-
-export function jetpackConnectAuthorize( state = {}, action ) {
-	switch ( action.type ) {
-		case JETPACK_CONNECT_AUTHORIZE:
-			return Object.assign( {}, omit( state, 'userData', 'bearerToken' ), {
-				isAuthorizing: true,
-				authorizeSuccess: false,
-				authorizeError: false,
-				isRedirectingToWpAdmin: false,
-				autoAuthorize: false,
-			} );
-		case JETPACK_CONNECT_AUTHORIZE_RECEIVE:
-			if ( isEmpty( action.error ) && action.data ) {
-				const { plans_url } = action.data;
-				return Object.assign( {}, state, {
-					authorizeError: false,
-					authorizeSuccess: true,
-					autoAuthorize: false,
-					plansUrl: plans_url,
-					siteReceived: false,
-				} );
-			}
-			return Object.assign( {}, state, {
-				isAuthorizing: false,
-				authorizeError: action.error,
-				authorizeSuccess: false,
-				autoAuthorize: false,
-			} );
-		case JETPACK_CONNECT_AUTHORIZE_LOGIN_COMPLETE:
-			return Object.assign( {}, state, { authorizationCode: action.data.code } );
-		case JETPACK_CONNECT_AUTHORIZE_RECEIVE_SITE_LIST:
-			const updateQueryObject = omit( state.queryObject, '_wp_nonce', 'secret', 'scope' );
-			return Object.assign( {}, omit( state, 'queryObject' ), {
-				siteReceived: true,
-				isAuthorizing: false,
-				queryObject: updateQueryObject,
-			} );
-		case JETPACK_CONNECT_QUERY_SET:
-			const queryObject = Object.assign( {}, action.queryObject );
-			return Object.assign( {}, buildDefaultAuthorizeState(), { queryObject: queryObject } );
-		case JETPACK_CONNECT_CREATE_ACCOUNT:
-			return Object.assign( {}, state, {
-				isAuthorizing: true,
-				authorizeSuccess: false,
-				authorizeError: false,
-				autoAuthorize: true,
-			} );
-		case JETPACK_CONNECT_CREATE_ACCOUNT_RECEIVE:
-			if ( ! isEmpty( action.error ) ) {
-				return Object.assign( {}, state, {
-					isAuthorizing: false,
-					authorizeSuccess: false,
-					authorizeError: true,
-					autoAuthorize: false,
-				} );
-			}
-			return Object.assign( {}, state, {
-				isAuthorizing: true,
-				authorizeSuccess: false,
-				authorizeError: false,
-				autoAuthorize: true,
-				userData: action.userData,
-				bearerToken: action.data.bearer_token,
-			} );
-		case SITE_REQUEST_FAILURE:
-			if (
-				state.queryObject &&
-				state.queryObject.client_id &&
-				parseInt( state.queryObject.client_id ) === action.siteId
-			) {
-				return Object.assign( {}, state, { clientNotResponding: true } );
-			}
-			return state;
-		case JETPACK_CONNECT_USER_ALREADY_CONNECTED:
-			return Object.assign( {}, state, { userAlreadyConnected: true } );
-		case JETPACK_CONNECT_REDIRECT_XMLRPC_ERROR_FALLBACK_URL:
-			return Object.assign( {}, state, { isRedirectingToWpAdmin: true } );
-		case JETPACK_CONNECT_REDIRECT_WP_ADMIN:
-			return Object.assign( {}, state, { isRedirectingToWpAdmin: true } );
-		case JETPACK_CONNECT_COMPLETE_FLOW:
-			return {};
-		case DESERIALIZE:
-			return ! isStale( state.timestamp, JETPACK_CONNECT_AUTHORIZE_TTL ) ? state : {};
-		case SERIALIZE:
-			return state;
-	}
-	return state;
-}
-jetpackConnectAuthorize.hasCustomPersistence = true;
 
 export default combineReducers( {
 	jetpackConnectSite,
