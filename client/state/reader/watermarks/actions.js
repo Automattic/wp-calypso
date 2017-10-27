56470c91d6ff31dea79bf624d777951f6315068c
Jake <samouri@users.noreply.github.com>
2017-10-27 11:16:13 -0500

    Reader: add high watermark redux bits (#18769)
    * Reader: add high watermark
    
    * add config flag for hwm
    
    * add basic reducers/selector/tests/actions
    
    * hacustompersistence
    
    * teency cleanup of comment
        "ajv": "5.2.1",
    
    * add tests

---
+++ client/state/reader/watermarks/actions.js
@@ -0,0 +1,23 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import { READER_VIEW_STREAM } from 'state/action-types';
+
+/**
+ * this is a relatively generic action type for something very specific (marking up the watermark)
+ * My hope is that we'll be able to reuse this same action-type for many other functionalities.
+ * i.e. unexpanding all photos/videos when opening a stream.
+ *
+ * @param {Date} mark  - date last viewed
+ * @param {String} streamId - stream being viewed
+ * @returns {Object} action object for dispatch
+ */
+export const viewStream = ( { mark, streamId } ) => {
+	return {
+		type: READER_VIEW_STREAM,
+		mark,
+		streamId,
+	};
+};
