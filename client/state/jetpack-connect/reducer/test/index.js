8ae8d1d52696ba75e9e72f9a68334dfc02b8e9b3
Jon Surrell <jon.surrell@automattic.com>
2017-10-27 11:45:02 +0200

    Migrate simple chai expect to jest

--- client/state/jetpack-connect/reducer/test/index.js
+++ client/state/jetpack-connect/reducer/test/index.js
@@ -1,27 +1,20 @@
 /** @format */
 /**
- * External dependencies
- */
-import { expect } from 'chai';
-
-/**
  * Internal dependencies
  */
 import reducer from '..';
 
-const jestExpect = global.expect;
-
 describe( 'reducer', () => {
 	test( 'should export expected reducer keys', () => {
 		const initialState = reducer( undefined, {} );
-		expect( initialState ).to.have.keys( [
-			'jetpackConnectSite',
-			'jetpackConnectAuthorize',
-			'jetpackConnectSessions',
-			'jetpackSSO',
-			'jetpackConnectSelectedPlans',
-			'jetpackAuthAttempts',
-		] );
-		jestExpect( initialState ).toMatchSnapshot();
+		expect( initialState ).toMatchObject( {
+			jetpackConnectSite: expect.anything(),
+			jetpackConnectAuthorize: expect.anything(),
+			jetpackConnectSessions: expect.anything(),
+			jetpackSSO: expect.anything(),
+			jetpackConnectSelectedPlans: expect.anything(),
+			jetpackAuthAttempts: expect.anything(),
+		} );
+		expect( initialState ).toMatchSnapshot();
 	} );
 } );
