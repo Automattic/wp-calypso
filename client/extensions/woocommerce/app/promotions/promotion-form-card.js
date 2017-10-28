d374d1f360faba63037e346d0cb335f10aa0ad88
Kevin Killingsworth <kevin@redfenix.com>
2017-10-27 23:05:56 -0500

    Promotion edit: Add cards to model
    This extends the promotion models by adding cards to the model instead
    of hard-coding them in the promotions-form-card. This will allow an
    additional card for constraints to be added via the model as a next
    step.

--- client/extensions/woocommerce/app/promotions/promotion-form-card.js
+++ client/extensions/woocommerce/app/promotions/promotion-form-card.js
@@ -3,14 +3,11 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { localize } from 'i18n-calypso';
-import warn from 'lib/warn';
 
 /**
  * Internal dependencies
  */
 import Card from 'components/card';
-import promotionModels from './promotion-models';
 import { renderField } from './fields';
 import SectionHeader from 'components/section-header';
 
@@ -22,50 +19,24 @@ const promotionFieldEdit = ( siteId, promotion, editPromotion ) => ( fieldName, 
 	editPromotion( siteId, promotion, newPromotion );
 };
 
-function renderFields( promotion, edit, translate, currency ) {
-	const model = promotionModels[ promotion.type ];
-
-	if ( ! model ) {
-		warn( 'No model found for promotion type: ' + promotion.type );
-		return null;
-	}
-
-	return Object.keys( model ).map( ( fieldName ) => {
-		const fieldModel = model[ fieldName ];
-		return renderField( fieldName, fieldModel, promotion, edit, currency );
-	} );
-}
-
-function getHeaderText( promotionType, translate ) {
-	switch ( promotionType ) {
-		case 'product_sale':
-			return translate( 'Product & Sale Price' );
-		case 'fixed_product':
-		case 'fixed_cart':
-		case 'percent':
-			return translate( 'Coupon Code & Discount' );
-	}
-}
-
 const PromotionFormCard = ( {
 	siteId,
 	currency,
 	promotion,
+	cardModel,
 	editPromotion,
-	translate
 } ) => {
 	const edit = promotionFieldEdit( siteId, promotion, editPromotion );
+	const fields = Object.keys( cardModel.fields ).map( ( fieldName ) => {
+		const fieldModel = cardModel.fields[ fieldName ];
+		return renderField( fieldName, fieldModel, promotion, edit, currency );
+	} );
 
 	return (
 		<div>
-			<SectionHeader label={ getHeaderText( promotion.type, translate ) } />
+			<SectionHeader label={ cardModel.labelText } />
 			<Card className="promotions__promotion-form-card">
-				{ renderFields(
-					promotion,
-					edit,
-					translate,
-					currency
-				) }
+				{ fields }
 			</Card>
 		</div>
 	);
@@ -79,7 +50,11 @@ PromotionFormCard.PropTypes = {
 		type: PropTypes.string.isRequired,
 	} ),
 	editPromotion: PropTypes.func.isRequired,
+	cardModel: PropTypes.shape( {
+		labelText: PropTypes.string.isRequired,
+		fields: PropTypes.object.isRequired,
+	} ).isRequired,
 };
 
-export default localize( PromotionFormCard );
+export default PromotionFormCard;
 
