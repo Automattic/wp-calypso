8fb26aee9d6eec918399e1145087ad1afb6894c3
bluefuton <chris@bluefuton.com>
2017-10-30 15:54:37 +0000

    Change selector to isFollowingReaderConversation

---
+++ client/state/selectors/is-following-reader-conversation.js
@@ -0,0 +1,29 @@
+/*
+ * @format
+ */
+
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { key } from 'state/reader/conversations/utils';
+import { CONVERSATION_FOLLOW_STATUS_FOLLOWING } from 'state/reader/conversations/follow-status';
+
+/*
+ * Get the conversation following status for a given post
+ *
+ * @param  {Object}  state  Global state tree
+ * @param  {Number} siteId
+ * @param  {Number} postId
+ * @return {Boolean} Is the user following this conversation?
+ */
+export default function isFollowingReaderConversation( state, { siteId, postId } ) {
+	return (
+		get( state, [ 'reader', 'conversations', 'items', key( siteId, postId ) ] ) ===
+		CONVERSATION_FOLLOW_STATUS_FOLLOWING
+	);
+}
