469cd545ea6cdb137f6091cd123792dae7e93390
Timmy Crawford <timmyc@users.noreply.github.com>
2017-10-30 14:57:49 -0700

    Store: Add loading placeholder to dashboard. (#19203)
    * Store: Add loading placeholder to dashboard.
    
    * Updates per feedback

--- client/extensions/woocommerce/components/action-header/index.js
+++ client/extensions/woocommerce/components/action-header/index.js
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { isArray } from 'lodash';
 
 /**
@@ -15,7 +16,7 @@ import Card from 'components/card';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import StickyPanel from 'components/sticky-panel';
 
-const ActionHeader = ( { children, breadcrumbs } ) => {
+const ActionHeader = ( { children, breadcrumbs, isLoading } ) => {
 	// TODO: Implement proper breadcrumbs component.
 	// For v1, we will just pass in a prop from each page.
 	let breadcrumbsOutput = breadcrumbs;
@@ -34,11 +35,13 @@ const ActionHeader = ( { children, breadcrumbs } ) => {
 		} );
 	}
 
+	const breadcrumbClasses = classNames( 'action-header__breadcrumbs', { 'is-loading': isLoading } );
+
 	return (
 		<StickyPanel>
 			<SidebarNavigation />
 			<Card className="action-header__header">
-				<span className="action-header__breadcrumbs">{ breadcrumbsOutput }</span>
+				<span className={ breadcrumbClasses }>{ breadcrumbsOutput }</span>
 				<div className="action-header__actions">{ children }</div>
 			</Card>
 		</StickyPanel>
