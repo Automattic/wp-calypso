a9fb756f9ffe731bb0039d32376a6a74fdcde40c
Marko Andrijasevic <marko.andrijasevic@automattic.com>
2017-10-31 22:35:43 +0100

    Comments: add Jetpack update screen (#19206)
    Force users to update to Jetpack 5.5 in order to use the comments section.
    Until Jetpack 5.5 is released this will be gated behind a feature flag
    and turned off in all environments.

--- client/my-sites/comments/main.jsx
+++ client/my-sites/comments/main.jsx
@@ -4,13 +4,17 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import config from 'config';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
+import { find } from 'lodash';
 
 /**
  * Internal dependencies
  */
+import EmptyContent from 'components/empty-content';
 import getSiteId from 'state/selectors/get-site-id';
+import { isJetpackSite, isJetpackMinimumVersion } from 'state/sites/selectors';
 import Main from 'components/main';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
 import DocumentHead from 'components/data/document-head';
@@ -18,7 +22,10 @@ import CommentList from './comment-list';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import { canCurrentUser } from 'state/selectors';
 import { preventWidows } from 'lib/formatting';
-import EmptyContent from 'components/empty-content';
+import QueryJetpackPlugins from 'components/data/query-jetpack-plugins';
+import { updatePlugin } from 'state/plugins/installed/actions';
+import { getPlugins } from 'state/plugins/installed/selectors';
+import { infoNotice } from 'state/notices/actions';
 
 export class CommentsManagement extends Component {
 	static propTypes = {
@@ -37,9 +44,17 @@ export class CommentsManagement extends Component {
 		status: 'all',
 	};
 
+	updateJetpackHandler = () => {
+		const { siteId, translate, jetpackPlugin } = this.props;
+
+		this.props.infoNotice( translate( 'Please wait while we update your Jetpack plugin.' ) );
+		this.props.updatePlugin( siteId, jetpackPlugin );
+	};
+
 	render() {
 		const {
 			changePage,
+			showJetpackUpdateScreen,
 			page,
 			postId,
 			showPermissionError,
@@ -51,10 +66,23 @@ export class CommentsManagement extends Component {
 
 		return (
 			<Main className="comments" wideLayout>
+				{ showJetpackUpdateScreen && <QueryJetpackPlugins siteIds={ [ siteId ] } /> }
 				<PageViewTracker path="/comments/:status/:site" title="Comments" />
 				<DocumentHead title={ translate( 'Comments' ) } />
-				<SidebarNavigation />
-				{ showPermissionError && (
+				{ showJetpackUpdateScreen && (
+					<EmptyContent
+						title={ preventWidows( translate( "Looking to manage this site's comments?" ) ) }
+						line={ preventWidows(
+							translate( 'Please update to the latest version of Jetpack first' )
+						) }
+						illustration="/calypso/images/illustrations/illustration-404.svg"
+						action={ translate( 'Update Jetpack' ) }
+						actionCallback={ this.updateJetpackHandler }
+					/>
+				) }
+				{ ! showJetpackUpdateScreen && <SidebarNavigation /> }
+				{ ! showJetpackUpdateScreen &&
+				showPermissionError && (
 					<EmptyContent
 						title={ preventWidows(
 							translate( "Oops! You don't have permission to manage comments." )
@@ -65,7 +93,8 @@ export class CommentsManagement extends Component {
 						illustration="/calypso/images/illustrations/illustration-500.svg"
 					/>
 				) }
-				{ ! showPermissionError && (
+				{ ! showJetpackUpdateScreen &&
+				! showPermissionError && (
 					<CommentList
 						changePage={ changePage }
 						order={ 'desc' }
@@ -83,11 +112,27 @@ export class CommentsManagement extends Component {
 
 const mapStateToProps = ( state, { siteFragment } ) => {
 	const siteId = getSiteId( state, siteFragment );
+	const isJetpack = isJetpackSite( state, siteId );
 	const canModerateComments = canCurrentUser( state, siteId, 'moderate_comments' );
+	const showJetpackUpdateScreen =
+		isJetpack &&
+		! isJetpackMinimumVersion( state, siteId, '5.5' ) &&
+		config.isEnabled( 'comments/management/jetpack-5.5' );
+
+	const sitePlugins = getPlugins( state, [ siteId ] );
+	const jetpackPlugin = find( sitePlugins, { slug: 'jetpack' } );
+
 	return {
 		siteId,
+		jetpackPlugin,
+		showJetpackUpdateScreen,
 		showPermissionError: canModerateComments === false,
 	};
 };
 
-export default connect( mapStateToProps )( localize( CommentsManagement ) );
+const mapDispatchToProps = {
+	updatePlugin,
+	infoNotice,
+};
+
+export default connect( mapStateToProps, mapDispatchToProps )( localize( CommentsManagement ) );
