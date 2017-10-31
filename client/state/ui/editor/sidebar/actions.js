4764b8751e04ac5cf8ac071a22c5f6e71302c8b5
spen <spen_@hotmail.co.uk>
2017-10-31 13:42:59 +0000

    Editor: Move sidebar tracking in to redux actions

--- client/state/ui/editor/sidebar/actions.js
+++ client/state/ui/editor/sidebar/actions.js
@@ -2,22 +2,36 @@
 /**
  * Internal dependencies
  */
-import { setLayoutFocus } from 'state/ui/layout-focus/actions';
+import {
+	bumpStat,
+	composeAnalytics,
+	recordGoogleEvent,
+	withAnalytics,
+} from 'state/analytics/actions';
 import { EDITOR_NESTED_SIDEBAR_SET } from 'state/action-types';
+import { setLayoutFocus } from 'state/ui/layout-focus/actions';
 
-export const openEditorSidebar = () => dispatch => {
-	// dispatch( {
-	// 	type: POST_EDITOR_SIDEBAR_OPEN,
-	// } );
-	dispatch( setLayoutFocus( 'sidebar' ) );
-};
+export const openEditorSidebar = () => dispatch =>
+	dispatch(
+		withAnalytics(
+			composeAnalytics(
+				recordGoogleEvent( 'Editor', 'Sidebar Toggle', 'open' ),
+				bumpStat( 'editor_actions', 'open-sidebar' )
+			),
+			setLayoutFocus( 'sidebar' )
+		)
+	);
 
-export const closeEditorSidebar = () => dispatch => {
-	// dispatch( {
-	// 	type: POST_EDITOR_SIDEBAR_CLOSE,
-	// } );
-	dispatch( setLayoutFocus( 'content' ) );
-};
+export const closeEditorSidebar = () => dispatch =>
+	dispatch(
+		withAnalytics(
+			composeAnalytics(
+				recordGoogleEvent( 'Editor', 'Sidebar Toggle', 'close' ),
+				bumpStat( 'editor_actions', 'close-sidebar' )
+			),
+			setLayoutFocus( 'content' )
+		)
+	);
 
 export const setNestedSidebar = target => ( {
 	type: EDITOR_NESTED_SIDEBAR_SET,
