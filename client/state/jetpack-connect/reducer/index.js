a7babb6c6cf7342d1972e8c15f2004b653f12d71
Steve Seear <steve.seear@gmail.com>
2017-10-31 10:22:27 +0000

    Jetpack connect: Fix missing auto-authorizations (#19305)
    Bad import was pulling in same reducer twice.

--- client/state/jetpack-connect/reducer/index.js
+++ client/state/jetpack-connect/reducer/index.js
@@ -5,7 +5,7 @@
 import jetpackAuthAttempts from './jetpack-auth-attempts';
 import jetpackConnectAuthorize from './jetpack-connect-authorize';
 import jetpackConnectSelectedPlans from './jetpack-connect-selected-plans';
-import jetpackConnectSessions from './jetpack-connect-site';
+import jetpackConnectSessions from './jetpack-connect-sessions';
 import jetpackConnectSite from './jetpack-connect-site';
 import jetpackSSO from './jetpack-sso';
 import { combineReducers } from 'state/utils';
