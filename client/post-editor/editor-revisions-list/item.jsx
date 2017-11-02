dca1dbadabc5a5ac29e5289498b42c17433d819a
Shaun Andrews <shaun@automattic.com>
2017-11-02 13:42:13 -0400

    Updating the CSS to match the design

--- client/post-editor/editor-revisions-list/item.jsx
+++ client/post-editor/editor-revisions-list/item.jsx
@@ -31,17 +31,17 @@ class EditorRevisionsListItem extends PureComponent {
 				<span className="editor-revisions-list__date">
 					<PostTime date={ this.props.revision.date } />
 				</span>
-				&nbsp;
 				<span className="editor-revisions-list__author">
 					{ isObject( this.props.revision.author ) &&
-						this.props.translate( 'by %(author)s', {
+						this.props.translate( '%(author)s', {
 							args: { author: this.props.revision.author.display_name },
 						} ) }
 				</span>
 				<div className="editor-revisions-list__changes">
 					{ this.props.revision.changes.added > 0 && (
 						<span className="editor-revisions-list__additions">
-							{ this.props.translate( '%(changes)d word added', '%(changes)d words added', {
+							+
+							{ this.props.translate( '%(changes)d word', '%(changes)d words', {
 								args: { changes: this.props.revision.changes.added },
 								count: this.props.revision.changes.added,
 							} ) }
@@ -50,11 +50,12 @@ class EditorRevisionsListItem extends PureComponent {
 
 					{ this.props.revision.changes.added > 0 &&
 						this.props.revision.changes.removed > 0 &&
-						', ' }
+						'  ' }
 
 					{ this.props.revision.changes.removed > 0 && (
 						<span className="editor-revisions-list__deletions">
-							{ this.props.translate( '%(changes)d word removed', '%(changes)d words removed', {
+							-
+							{ this.props.translate( '%(changes)d word', '%(changes)d words', {
 								args: { changes: this.props.revision.changes.removed },
 								count: this.props.revision.changes.removed,
 							} ) }
