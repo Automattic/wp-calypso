d8c46a590c1672d1fc44c917a52f50c221d8be36
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-11-02 08:42:40 +0100

    Tests: Update react-pass-to-children to avoid spaces around children

--- client/lib/react-pass-to-children/index.js
+++ client/lib/react-pass-to-children/index.js
@@ -22,7 +22,7 @@ export default function( element, additionalProps ) {
 			return React.cloneElement( child, props );
 		} );
 
-		return <div> { childElements } </div>;
+		return <div>{ childElements }</div>;
 	}
 
 	return React.cloneElement( element.props.children, props );
