77f50237b222cc69be63fe19fa46589747237c20
Jeremy Smith <jeremy@jeremysawesome.com>
2017-11-01 19:20:02 -0700

    initial add of invites data-layer

---
+++ client/state/data-layer/wpcom/sites/invites/index.js
@@ -0,0 +1,20 @@
+import { INVITES_REQUEST } from 'state/action-types';
+import { http } from 'state/data-layer/wpcom-http/actions';
+import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+
+const fetchInvites = ( { dispatch, getState }, action ) => {
+	const { siteId } = action;
+
+	dispatch( http(
+		{
+			method: 'GET',
+			path: `/sites/${ siteId }/invites`,
+			apiVersion: '1.1'
+		},
+		action
+	) );
+};
+
+export default {
+	[ INVITES_REQUEST ]: [ dispatchRequest( fetchInvites ) ]
+};
