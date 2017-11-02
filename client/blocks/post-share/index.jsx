6f537b36c00eebfe0e964070c51e9bc7ccf9bb50
Jonathan Sadowski <sadowski@automattic.com>
2017-11-02 15:20:45 -0500

    Query site plans from post-share component

--- client/blocks/post-share/index.jsx
+++ client/blocks/post-share/index.jsx
@@ -19,6 +19,7 @@ import Gridicon from 'gridicons';
 import QueryPostTypes from 'components/data/query-post-types';
 import QueryPosts from 'components/data/query-posts';
 import QueryPublicizeConnections from 'components/data/query-publicize-connections';
+import QuerySitePlans from 'components/data/query-site-plans';
 import Button from 'components/button';
 import ButtonGroup from 'components/button-group';
 import NoticeAction from 'components/notice/notice-action';
@@ -559,6 +560,7 @@ class PostShare extends Component {
 				<TrackComponentView eventName="calypso_publicize_post_share_view" />
 				<QueryPostTypes siteId={ siteId } />
 				<QueryPublicizeConnections siteId={ siteId } />
+				<QuerySitePlans siteId={ siteId } />
 
 				<div className={ classes }>
 					<div className="post-share__head">
