f72c4d3afeb2830b319cf5a2b6aab88c750efc69
Andrés Maneiro <andres.maneiro@automattic.com>
2017-11-02 12:54:40 +0100

    Moar tests

--- client/state/happychat/test/middleware.js
+++ client/state/happychat/test/middleware.js
@@ -15,11 +15,22 @@ import {
 	sendTyping,
 	sendNotTyping,
 } from 'state/happychat/connection/actions';
-import { blur } from 'state/happychat/ui/actions';
+import { blur, focus } from 'state/happychat/ui/actions';
 import {
+	HAPPYCHAT_CHAT_STATUS_ABANDONED,
 	HAPPYCHAT_CHAT_STATUS_ASSIGNED,
+	HAPPYCHAT_CHAT_STATUS_ASSIGNING,
+	HAPPYCHAT_CHAT_STATUS_BLOCKED,
+	HAPPYCHAT_CHAT_STATUS_CLOSED,
+	HAPPYCHAT_CHAT_STATUS_DEFAULT,
+	HAPPYCHAT_CHAT_STATUS_MISSED,
 	HAPPYCHAT_CHAT_STATUS_NEW,
+	HAPPYCHAT_CHAT_STATUS_PENDING,
 	HAPPYCHAT_CONNECTION_STATUS_CONNECTED,
+	HAPPYCHAT_CONNECTION_STATUS_CONNECTING,
+	HAPPYCHAT_CONNECTION_STATUS_DISCONNECTED,
+	HAPPYCHAT_CONNECTION_STATUS_RECONNECTING,
+	HAPPYCHAT_CONNECTION_STATUS_UNAUTHORIZED,
 	HAPPYCHAT_CONNECTION_STATUS_UNINITIALIZED,
 } from 'state/happychat/constants';
 
@@ -101,71 +112,81 @@ describe( 'middleware', () => {
 	} );
 
 	describe( 'eventMessage', () => {
-		test( 'is dispatched if client is connected, chat is assigned, and there is a message for the action', () => {
-			const state = {
-				happychat: {
-					connection: {
-						status: HAPPYCHAT_CONNECTION_STATUS_CONNECTED,
-					},
-					chat: {
-						status: HAPPYCHAT_CHAT_STATUS_ASSIGNED,
-					},
+		const state = {
+			happychat: {
+				connection: {
+					status: HAPPYCHAT_CONNECTION_STATUS_CONNECTED,
 				},
-			};
-			store.getState.mockReturnValue( state );
-			const action = blur();
-			actionMiddleware( action );
-			expect( store.dispatch.mock.calls[ 0 ][ 0 ].event ).toBe( 'message' );
-			expect( store.dispatch.mock.calls[ 0 ][ 0 ].payload.text ).toBe(
-				'Stopped looking at Happychat'
-			);
+				chat: {
+					status: HAPPYCHAT_CHAT_STATUS_ASSIGNED,
+				},
+			},
+		};
+
+		describe( 'is dispatched if client is connected, chat is assigned, and there is a message for the action', () => {
+			test( 'for HAPPYCHAT_BLUR', () => {
+				store.getState.mockReturnValue( state );
+				const action = blur();
+				actionMiddleware( action );
+
+				expect( store.dispatch.mock.calls[ 0 ][ 0 ].event ).toBe( 'message' );
+				expect( store.dispatch.mock.calls[ 0 ][ 0 ].payload.text ).toBe(
+					'Stopped looking at Happychat'
+				);
+			} );
+
+			test( 'for HAPPYCHAT_FOCUS', () => {
+				store.getState.mockReturnValue( state );
+				const action = focus();
+				actionMiddleware( action );
+
+				expect( store.dispatch.mock.calls[ 0 ][ 0 ].event ).toBe( 'message' );
+				expect( store.dispatch.mock.calls[ 0 ][ 0 ].payload.text ).toBe(
+					'Started looking at Happychat'
+				);
+			} );
 		} );
 
 		test( 'is not dispatched if client is not connected', () => {
-			const state = {
-				happychat: {
-					connection: {
-						status: HAPPYCHAT_CONNECTION_STATUS_UNINITIALIZED,
-					},
-					chat: {
-						status: HAPPYCHAT_CHAT_STATUS_ASSIGNED,
-					},
-				},
-			};
-			store.getState.mockReturnValue( state );
-			const action = blur();
-			actionMiddleware( action );
-			expect( store.dispatch ).not.toHaveBeenCalled();
+			[
+				HAPPYCHAT_CONNECTION_STATUS_CONNECTING,
+				HAPPYCHAT_CONNECTION_STATUS_DISCONNECTED,
+				HAPPYCHAT_CONNECTION_STATUS_RECONNECTING,
+				HAPPYCHAT_CONNECTION_STATUS_UNAUTHORIZED,
+				HAPPYCHAT_CONNECTION_STATUS_UNINITIALIZED,
+			].forEach( connectionStatus => {
+				store.getState.mockReturnValue(
+					Object.assign( state, { happychat: { connection: { status: connectionStatus } } } )
+				);
+				const action = blur();
+				actionMiddleware( action );
+
+				expect( store.dispatch ).not.toHaveBeenCalled();
+			} );
 		} );
 
 		test( 'is not dispatched if chat is not assigned', () => {
-			const state = {
-				happychat: {
-					connection: {
-						status: HAPPYCHAT_CONNECTION_STATUS_CONNECTED,
-					},
-					chat: {
-						status: HAPPYCHAT_CHAT_STATUS_NEW,
-					},
-				},
-			};
-			store.getState.mockReturnValue( state );
-			const action = blur();
-			actionMiddleware( action );
-			expect( store.dispatch ).not.toHaveBeenCalled();
+			[
+				HAPPYCHAT_CHAT_STATUS_ABANDONED,
+				HAPPYCHAT_CHAT_STATUS_ASSIGNING,
+				HAPPYCHAT_CHAT_STATUS_BLOCKED,
+				HAPPYCHAT_CHAT_STATUS_CLOSED,
+				HAPPYCHAT_CHAT_STATUS_DEFAULT,
+				HAPPYCHAT_CHAT_STATUS_NEW,
+				HAPPYCHAT_CHAT_STATUS_MISSED,
+				HAPPYCHAT_CHAT_STATUS_PENDING,
+			].forEach( chatStatus => {
+				store.getState.mockReturnValue(
+					Object.assign( state, { happychat: { chat: { status: chatStatus } } } )
+				);
+				const action = blur();
+				actionMiddleware( action );
+
+				expect( store.dispatch ).not.toHaveBeenCalled();
+			} );
 		} );
 
 		test( 'is not dispatched if there is no message defined', () => {
-			const state = {
-				happychat: {
-					connection: {
-						status: HAPPYCHAT_CONNECTION_STATUS_CONNECTED,
-					},
-					chat: {
-						status: HAPPYCHAT_CHAT_STATUS_ASSIGNED,
-					},
-				},
-			};
 			store.getState.mockReturnValue( state );
 			const action = { type: 'HAPPYCHAT_ACTION_WITHOUT_EVENT_MESSAGE_DEFINED' };
 			actionMiddleware( action );
