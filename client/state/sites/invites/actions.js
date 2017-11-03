b18ef91435c1bd36891d1e8466534e11a82399f3
Jeremy Smith <jeremy@jeremysawesome.com>
2017-11-03 16:40:46 -0700

    add invitesReceiveAction creator

--- client/state/sites/invites/actions.js
+++ client/state/sites/invites/actions.js
@@ -1,6 +1,14 @@
 import {
 	INVITES_REQUEST,
+	INVITES_RECEIVE,
 } from 'state/action-types';
+
+export const invitesReceiveAction = invites => {
+	return {
+		type: INVITES_RECEIVE,
+		invites
+	};
+};
 
 export const requestInvites = siteId => ( {
 	type: INVITES_REQUEST,
