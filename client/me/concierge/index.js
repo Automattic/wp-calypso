460ae242db20f8b4955523389e41f04690d49862
James Tien <southp0105@gmail.com>
2017-11-04 00:01:11 -0500

    Add `/me/concierge/` section scaffolding code. (#18915)
    * Add `/me/concierge/` section scaffolding code.
    
    * Feature-guarded the business concierge section.
    
    * Remove the redundant slash in the path.
    
    * Add `ConciergeMain` component and fill in with a placeholder div.
    
    * Update terminology from "business concierge" to "concierge chats".
    
    * Remove the me sidebar for now. For the current MVP version, we don't
    plan to let user traversing back and forth anywhere from the concierge
    page.

---
+++ client/me/concierge/index.js
@@ -0,0 +1,19 @@
+/**
+ * External dependencies
+ *
+ * @format
+ */
+
+import page from 'page';
+
+/**
+ * Internal dependencies
+ */
+import config from 'config';
+import controller from './controller';
+
+export default () => {
+	if ( config.isEnabled( 'concierge-chats' ) ) {
+		page( '/me/concierge', controller.concierge );
+	}
+};
---
+++ client/me/concierge/main.js
@@ -0,0 +1,13 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { localize } from 'i18n-calypso';
+
+const ConciergeMain = () => {
+	return (
+		<div>{ 'Concierge chats page placeholder.' }</div>
+	);
+};
+
+export default localize( ConciergeMain );
