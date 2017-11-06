744f54bac9d8297e58a3917833f0b3d1310a0245
Bernie Reiter <ockham@raz.or.at>
2017-11-06 16:45:54 +0100

    Jetpack Disconnect Survey: Final Touches (#19517)
    * Jetpack Disconnect Survey: Fix Punctuation, reword 'Other' to 'Another reason'
    * Jetpack Disconnect Survey: Simplify Whitelist

--- client/my-sites/site-settings/disconnect-site/disconnect-survey.jsx
+++ client/my-sites/site-settings/disconnect-site/disconnect-survey.jsx
@@ -28,20 +28,22 @@ const DisconnectSurvey = ( { confirmHref, isPaidPlan, siteId, siteSlug, translat
 			) }
 		/>
 		<CompactCard href={ `/settings/disconnect-site/too-difficult/${ siteSlug }` }>
-			{ translate( 'It was too hard to configure Jetpack' ) }
+			{ translate( 'It was too hard to configure Jetpack.' ) }
 		</CompactCard>
 		<CompactCard href={ `/settings/disconnect-site/missing-feature/${ siteSlug }` }>
 			{ translate( 'A feature I need was missing.' ) }
 		</CompactCard>
 		{ isPaidPlan && (
 			<CompactCard href={ `/settings/disconnect-site/too-expensive/${ siteSlug }` }>
-				{ translate( 'This plan is too expensive' ) }
+				{ translate( 'This plan is too expensive.' ) }
 			</CompactCard>
 		) }
 		<CompactCard href={ confirmHref + '?reason=troubleshooting' }>
-			{ translate( "Troubleshooting — I'll be reconnecting afterwards" ) }
+			{ translate( "Troubleshooting — I'll be reconnecting afterwards." ) }
 		</CompactCard>
-		<CompactCard href={ confirmHref + '?reason=other' }>{ translate( 'Other' ) }</CompactCard>
+		<CompactCard href={ confirmHref + '?reason=other' }>
+			{ translate( 'Another reason…' ) }
+		</CompactCard>
 	</div>
 );
 
