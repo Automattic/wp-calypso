c8a6b4b8847331b8ea1697f12aaa77427afef112
James Tien <southp0105@gmail.com>
2017-11-07 15:15:04 +0800

    Update the code accordingly due to rebasing.

--- client/state/concierge/shifts/reducer.js
+++ client/state/concierge/shifts/reducer.js
@@ -4,10 +4,10 @@
  * Internal dependencies
  */
 import { createReducer } from 'state/utils';
-import { CONCIERGE_SHIFTS_FETCH, CONCIERGE_SHIFTS_UPDATE } from 'state/action-types';
+import { CONCIERGE_SHIFTS_REQUEST, CONCIERGE_SHIFTS_UPDATE } from 'state/action-types';
 
 export const shifts = createReducer( null, {
-	[ CONCIERGE_SHIFTS_FETCH ]: () => null,
+	[ CONCIERGE_SHIFTS_REQUEST ]: () => null,
 	[ CONCIERGE_SHIFTS_UPDATE ]: ( state, { shifts } ) => shifts,
 } );
 
