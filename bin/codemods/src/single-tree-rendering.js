c6e64b32e4ef8d7659b61092c18dabe10a470d88
Mikael Korpela <mikael@ihminen.org>
2017-11-07 19:20:42 +0200

    Single-tree-render codemod: transform ReactDom.render()
    - Adds transformation of `ReactDom.render()`
    - Refactors codemod to by more DRY
    - Adds comments for clarity

--- bin/codemods/src/single-tree-rendering.js
+++ bin/codemods/src/single-tree-rendering.js
@@ -1,5 +1,21 @@
 /** @format */
 
+/**
+ * Single Tree Rendering Codemod
+ *
+ * Transforms `ReactDom.render()` to `context.primary/secondary`.
+ *
+ * Transforms `renderWithReduxStore()` to `context.primary/secondary`.
+ *
+ * Adds `context` to params in middlewares when using `ReactDom.render()`.
+ *
+ * Adds `next` to params and `next()` to body in middlewares when using
+ *   `ReactDom.render()` or `renderWithReduxStore()`.
+ *
+ * Adds `makeLayout` and `clientRender` to `page()` route definitions and
+ *   accompanying import statement.
+ */
+
 const config = require( './config' );
 
 export default function transformer( file, api ) {
@@ -15,8 +31,25 @@ export default function transformer( file, api ) {
 		return str.replace( /(import.*\n)\n+(import)/g, '$1$2' );
 	}
 
+	/**
+	 * Ensure `context` is among parameters
+	 */
+	function ensureContextMiddleware( path ) {
+		// `context` param is already in
+		if ( path.value.params.indexOf( 'context' ) !== -1 ) {
+			return path.value;
+		}
+		let ret = path.value;
+		ret.params = [ j.identifier( 'context' ), ...ret.params ];
+
+		return ret;
+	}
+
+	/**
+	 * Adds `next` to params and `next()` to body
+	 */
 	function addNextMiddleware( path ) {
-		if ( path.value.params.length !== 1 ) {
+		if ( path.value.params.length > 1 ) {
 			// More than just a context arg, possibly not a middleware
 			return path.value;
 		}
@@ -40,16 +73,79 @@ export default function transformer( file, api ) {
 		}
 	}
 
+	/**
+	 * Input
+	 * ```
+	 * renderWithReduxStore(
+	 * 	 <Example />,
+	 * 	 'primary',
+	 * 	 context.store
+	 * );
+	 * ```
+	 *
+	 * Output:
+	 * ```
+	 * context.primary = <Example />;
+	 * ```
+	 */
 	function transformRenderWithReduxStore( path ) {
+		const expressionCallee = {
+			name: 'renderWithReduxStore',
+		};
+
+		return transformToContextLayout( path, expressionCallee );
+	}
+
+	/**
+	 * Input
+	 * ```
+	 * ReactDom.render(
+	 * 	 <Example />,
+	 * 	 document.getElementById( 'primary' )
+	 * );
+	 * ```
+	 *
+	 * Output:
+	 * ```
+	 * context.primary = <Example />;
+	 * ```
+	 */
+	function transformReactDomRender( path ) {
+		const expressionCallee = {
+			type: 'MemberExpression',
+			object: {
+				name: 'ReactDom',
+			},
+			property: {
+				name: 'render',
+			},
+		};
+
+		return transformToContextLayout( path, expressionCallee );
+	}
+
+	/**
+	 * Input
+	 * ```
+	 * DefinedByExpressionCallee(
+	 * 	 <Example />,
+	 * 	 document.getElementById( 'primary' )
+	 * );
+	 * ```
+	 *
+	 * Output:
+	 * ```
+	 * context.primary = <Example />;
+	 * ```
+	 */
+	function transformToContextLayout( path, expressionCallee ) {
 		if ( path.value.params.length !== 2 ) {
 			// More than just context and next args, possibly not a middleware
 			return path.value;
 		}
 		return j( path )
 			.find( j.CallExpression, {
-				callee: {
-					name: 'renderWithReduxStore',
-				},
+				callee: expressionCallee,
 			} )
 			.replaceWith( p => {
 				const contextArg = path.value.params[ 0 ];
@@ -60,8 +156,29 @@ export default function transformer( file, api ) {
 					p.value.arguments[ 0 ]
 				);
 			} );
+
+		return j( node );
 	}
 
+	// Transform `ReactDom.render()` to `context.primary/secondary`
+	root
+		.find( j.CallExpression, {
+			callee: {
+				type: 'MemberExpression',
+				object: {
+					name: 'ReactDom',
+				},
+				property: {
+					name: 'render',
+				},
+			},
+		} )
+		.closest( j.FunctionExpression )
+		.replaceWith( ensureContextMiddleware )
+		.replaceWith( addNextMiddleware )
+		.forEach( transformReactDomRender );
+
+	// Transform `renderWithReduxStore()` to `context.primary/secondary`
 	root
 		.find( j.CallExpression, {
 			callee: {
