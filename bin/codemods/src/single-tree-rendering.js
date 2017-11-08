9b2beb2470cb06acf8c1bb095863b175f93b685c
Mikael Korpela <mikael@ihminen.org>
2017-11-08 19:13:57 +0200

    Don't forEach, replaceWith instead

--- bin/codemods/src/single-tree-rendering.js
+++ bin/codemods/src/single-tree-rendering.js
@@ -108,8 +108,9 @@ export default function transformer( file, api ) {
 					return importValue.indexOf( '/' ) > -1;
 				} )
 				.at( 0 )
-				.forEach( p => {
+				.replaceWith( p => {
 					p.value.comments = [ comment ];
+					return p.value;
 				} );
 		}
 	}
