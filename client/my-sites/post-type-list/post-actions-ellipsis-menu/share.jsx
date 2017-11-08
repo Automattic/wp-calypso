09aa58af3754c4b704fafbf25ebf3daf0775eefa
Miklos Juhasz <juhasz.miklos.1@gmail.com>
2017-11-08 23:48:41 +0100

    Hide Share action for Custom Post Types (#19489)
    * Hide Share action for CPTs.
    
    This is not yet supported. The UI explicitly references posts.
    
    * Use post.type as opposed to getPostType selector

--- client/my-sites/post-type-list/post-actions-ellipsis-menu/share.jsx
+++ client/my-sites/post-type-list/post-actions-ellipsis-menu/share.jsx
@@ -17,7 +17,6 @@ import PopoverMenuItem from 'components/popover/menu-item';
 import { bumpStat as bumpAnalyticsStat } from 'state/analytics/actions';
 import { bumpStatGenerator } from './utils';
 import { getPost } from 'state/posts/selectors';
-import { getPostType } from 'state/post-types/selectors';
 import { toggleSharePanel } from 'state/ui/post-type-list/actions';
 import { isPublicizeEnabled } from 'state/selectors';
 import config from 'config';
@@ -44,11 +43,12 @@ class PostActionsEllipsisMenuShare extends Component {
 	}
 
 	render() {
-		const { translate, status, isPublicizeEnabled: isPublicizeEnabledForSite } = this.props;
+		const { translate, status, type, isPublicizeEnabled: isPublicizeEnabledForSite } = this.props;
 		if (
 			! config.isEnabled( 'posts/post-type-list' ) ||
 			! includes( [ 'publish' ], status ) ||
-			! isPublicizeEnabledForSite
+			! isPublicizeEnabledForSite ||
+			'post' !== type
 		) {
 			return null;
 		}
@@ -70,7 +70,7 @@ const mapStateToProps = ( state, { globalId } ) => {
 	return {
 		status: post.status,
 		isPublicizeEnabled: isPublicizeEnabled( state, post.site_ID, post.type ),
-		type: getPostType( state, post.site_ID, post.type ),
+		type: post.type,
 	};
 };
 
