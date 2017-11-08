916b387f14b89419ba1944199a3cdcfd6561765b
Jon Surrell <jon.surrell@automattic.com>
2017-11-08 14:11:12 +0100

    Remove bad code
    This block was originally removed in #16995 but had to be reverted in #17438

--- client/jetpack-connect/authorize-form.jsx
+++ client/jetpack-connect/authorize-form.jsx
@@ -115,10 +115,6 @@ class JetpackConnectAuthorizeForm extends Component {
 			return this.renderNoQueryArgsError();
 		}
 
-		if ( queryObject && queryObject.already_authorized && ! this.props.isAlreadyOnSitesList ) {
-			this.renderForm();
-		}
-
 		return (
 			<MainWrapper>
 				<div className="jetpack-connect__authorize-form">{ this.renderForm() }</div>
