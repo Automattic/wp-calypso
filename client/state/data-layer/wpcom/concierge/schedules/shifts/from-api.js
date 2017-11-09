8f06bf7412ac458f9fe224a9f94ef7225f73a80a
James Tien <southp0105@gmail.com>
2017-11-09 10:37:01 +0800

    Add the api response transformer and the validator for `/concierge/schedules/{id}/shifts`

---
+++ client/state/data-layer/wpcom/concierge/schedules/shifts/from-api.js
@@ -0,0 +1,23 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+/**
+ * Internal dependenceis
+ */
+import { makeParser } from 'state/data-layer/wpcom-http/utils';
+import responseSchema from './schema';
+
+export const transformShift = shift => ( {
+	beginTimestamp: get( shift, 'begin_timestamp' ),
+	endTimestamp: get( shift, 'end_timestamp' ),
+	scheduleId: get( shift, 'schedule_id' ),
+	description: get( shift, 'description' ),
+} );
+
+export const transform = response => response.map( transformShift );
+
+export default makeParser( responseSchema, {}, transform );
