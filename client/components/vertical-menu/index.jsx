47aa24bece32e7bfbe7ea49ac96edc7fe9c5a714
Jason Johnston <jasonjohnston76@gmail.com>
2017-11-09 09:12:38 -0800

    fix lint error

--- client/components/vertical-menu/index.jsx
+++ client/components/vertical-menu/index.jsx
@@ -9,19 +9,16 @@ import PropTypes from 'prop-types';
 import { identity, partial } from 'lodash';
 
 export class VerticalMenu extends PureComponent {
-
 	static propTypes = {
 		onClick: PropTypes.func,
 		initalItemIndex: PropTypes.number,
-		children: PropTypes.arrayOf(
-			PropTypes.element
-		)
-	}
+		children: PropTypes.arrayOf( PropTypes.element ),
+	};
 
 	static defaultProps = {
 		initialItemIndex: 0,
-		onClick: identity
-	}
+		onClick: identity,
+	};
 
 	constructor( props ) {
 		super( props );
