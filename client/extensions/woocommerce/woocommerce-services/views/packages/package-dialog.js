d58b97b32f4c2caf131d5859442a41fcda2181f4
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-11-09 11:22:08 -0500

    Added "wcc-root" class name to the Packages dialog

--- client/extensions/woocommerce/woocommerce-services/views/packages/package-dialog.js
+++ client/extensions/woocommerce/woocommerce-services/views/packages/package-dialog.js
@@ -129,7 +129,7 @@ const AddPackageDialog = props => {
 	return (
 		<Dialog
 			isVisible={ showModal }
-			additionalClassNames="packages__add-edit-dialog woocommerce"
+			additionalClassNames="packages__add-edit-dialog woocommerce wcc-root"
 			onClose={ onClose }
 			buttons={ buttons }
 		>
