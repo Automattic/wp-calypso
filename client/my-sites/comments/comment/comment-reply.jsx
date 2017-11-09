6cc70ce25604b3e2b9f509f67e46a1b43e7e5f0a
Jacopo Tomasone <Copons@users.noreply.github.com>
2017-11-09 11:46:43 +0000

    Comments Redesign: Add the reply action (#19607)
    Add the Reply action button and introduce some new logics with it:
    
    - The reply textarea focus has been moved entirely in the `CommentReply` component, as it should have been all along, considering it's entirely its responsibility to handle it.
    
    - Introduced a new `isReplyVisible` state and `toggleReply` method in the parent `Comment` component. They take care of toggling the visibility of `CommentReply`.
    
    - `CommentReply` is now always rendered when the comment `isExpanded` and not `isBulkMode`, regardless of its visibility state.
    This way, as long as a comment is expanded, the reply textarea content won't disappear.
    
    - Whenever `isReplyVisible` turns into `true`, `CommentReply` appears and the textarea gets focus.

--- client/my-sites/comments/comment/comment-reply.jsx
+++ client/my-sites/comments/comment/comment-reply.jsx
@@ -34,23 +34,30 @@ const TEXTAREA_VERTICAL_BORDER = 2;
 export class CommentReply extends Component {
 	static propTypes = {
 		commentId: PropTypes.number,
-		blurReply: PropTypes.func,
-		focusReply: PropTypes.func,
-		hasReplyFocus: PropTypes.bool,
+		isReplyVisible: PropTypes.bool,
 	};
 
 	state = {
+		hasReplyFocus: false,
 		replyContent: '',
 		textareaHeight: TEXTAREA_HEIGHT_COLLAPSED,
 	};
 
 	componentDidMount() {
-		if ( this.props.hasReplyFocus ) {
+		this.textarea.focus();
+	}
+
+	componentDidUpdate( prevProps ) {
+		if ( ! prevProps.isReplyVisible && this.props.isReplyVisible ) {
 			this.textarea.focus();
 		}
 	}
 
 	storeTextareaRef = textarea => ( this.textarea = textarea );
+
+	blurReply = () => this.setState( { hasReplyFocus: false } );
+
+	focusReply = () => this.setState( { hasReplyFocus: true } );
 
 	calculateTextareaHeight = () => {
 		const textareaHeight = Math.min(
@@ -81,14 +88,14 @@ export class CommentReply extends Component {
 	};
 
 	submitReply = () => {
-		const { approveComment, blurReply, commentStatus, postId, replyToComment, siteId } = this.props;
+		const { approveComment, commentStatus, postId, replyToComment, siteId } = this.props;
 		const { replyContent } = this.state;
 
 		const alsoApprove = 'approved' !== commentStatus;
 
 		replyToComment( replyContent, siteId, postId, { alsoApprove } );
 		this.setState( { replyContent: '' } );
-		blurReply();
+		this.blurReply();
 
 		if ( alsoApprove ) {
 			approveComment( siteId, postId, { previousStatus: commentStatus } );
@@ -102,8 +109,8 @@ export class CommentReply extends Component {
 	};
 
 	render() {
-		const { blurReply, currentUser, focusReply, hasReplyFocus, translate } = this.props;
-		const { replyContent, textareaHeight } = this.state;
+		const { currentUser, translate } = this.props;
+		const { hasReplyFocus, replyContent, textareaHeight } = this.state;
 
 		const hasReplyContent = replyContent.trim().length > 0;
 		// Only show the scrollbar if the textarea content exceeds the max height
@@ -138,9 +145,9 @@ export class CommentReply extends Component {
 					<AutoDirection>
 						<textarea
 							className={ textareaClasses }
-							onBlur={ blurReply }
+							onBlur={ this.blurReply }
 							onChange={ this.updateTextarea }
-							onFocus={ focusReply }
+							onFocus={ this.focusReply }
 							onKeyDown={ this.keyDownHandler }
 							placeholder={ this.getPlaceholder() }
 							ref={ this.storeTextareaRef }
