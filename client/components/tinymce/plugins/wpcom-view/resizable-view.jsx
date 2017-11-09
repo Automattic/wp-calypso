bfce565e3e727834d9d010830c22cd99c8734117
Bernie Reiter <ockham@raz.or.at>
2017-11-09 14:29:22 +0100

    TinyMCE: Use React.PureComponent for the WPcom View Plugin (#19628)
    This was the only occurrence of `react-pure-render/component` I could find in our codebase.

--- client/components/tinymce/plugins/wpcom-view/resizable-view.jsx
+++ client/components/tinymce/plugins/wpcom-view/resizable-view.jsx
@@ -1,12 +1,9 @@
+/** @format */
 /**
  * External dependencies
- *
- * @format
  */
-
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
-import PureComponent from 'react-pure-render/component';
 
 export default function( Component ) {
 	const componentName = Component.displayName || Component.name || '';
