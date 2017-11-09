c12feb2737b80b533f0241b1455fcd693fc64963
James Tien <southp0105@gmail.com>
2017-11-09 08:50:36 +0800

    move /concierge/shifts to /concierge/schedules/shifts to match the API route.

--- client/state/data-layer/wpcom/concierge/index.js
+++ client/state/data-layer/wpcom/concierge/index.js
@@ -4,6 +4,6 @@
  * Internal dependencies
  */
 import { mergeHandlers } from 'state/action-watchers/utils';
-import shifts from './shifts';
+import schedules from './schedules';
 
-export default mergeHandlers( shifts );
+export default mergeHandlers( schedules );
