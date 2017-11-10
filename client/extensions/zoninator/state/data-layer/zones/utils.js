a84c005a125174cbce487a69acff274c26281331
Kuba Birecki <kuba.birecki@crystalplanet.io>
2017-11-10 19:28:43 +0100

    Unescape html entities in zone description

--- client/extensions/zoninator/state/data-layer/zones/utils.js
+++ client/extensions/zoninator/state/data-layer/zones/utils.js
@@ -1,12 +1,10 @@
 /**
  * External dependencies
- *
- * @format
  */
 import { unescape } from 'lodash';
 
 export const fromApi = ( { description, name, slug, term_id } ) => ( {
-	description,
+	description: unescape( description ),
 	id: term_id,
 	name: unescape( name ),
 	slug,
