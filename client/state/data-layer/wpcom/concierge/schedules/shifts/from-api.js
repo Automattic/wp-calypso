cadc1ee8de392d6362d3871636ac56ae6eb604d0
James Tien <southp0105@gmail.com>
2017-11-10 10:50:38 +0800

    Remove the use of `lodash.get`, since it's not necessary here.

--- client/state/data-layer/wpcom/concierge/schedules/shifts/from-api.js
+++ client/state/data-layer/wpcom/concierge/schedules/shifts/from-api.js
@@ -1,21 +1,16 @@
 /** @format */
 
 /**
- * External dependencies
- */
-import { get } from 'lodash';
-
-/**
  * Internal dependenceis
  */
 import { makeParser } from 'state/data-layer/wpcom-http/utils';
 import responseSchema from './schema';
 
 const transformShift = shift => ( {
-	beginTimestamp: get( shift, 'begin_timestamp' ),
-	endTimestamp: get( shift, 'end_timestamp' ),
-	scheduleId: get( shift, 'schedule_id' ),
-	description: get( shift, 'description' ),
+	beginTimestamp: shift.begin_timestamp,
+	endTimestamp: shift.end_timestamp,
+	scheduleId: shift.schedule_id,
+	description: shift.description,
 } );
 
 export const transform = response => response.map( transformShift );
