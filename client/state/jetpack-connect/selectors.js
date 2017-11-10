388dff80a8b0ce9b1161d6392449fdfc1ef8fc2c
Jon Surrell <jon.surrell@automattic.com>
2017-11-10 15:26:31 +0100

    Restore detailed description

--- client/state/jetpack-connect/selectors.js
+++ client/state/jetpack-connect/selectors.js
@@ -104,6 +104,9 @@ const getUserAlreadyConnected = state => {
 /**
  * Returns true if there is an XMLRPC error.
  *
+ * XMLRPC errors can be identified by the presence of an error message, the presence of an
+ * authorization code, and if the error message contains the string 'error'.
+ *
  * @param  {object}  state Global state tree
  * @return {Boolean}       True if there's an xmlrpc error otherwise false
  */
