8ba3c6d489ad1cb99a7af432bfbdbc3a89374ff7
Mikael Korpela <mikael@ihminen.org>
2017-11-13 12:44:20 +0200

    Single-tree-render codemod: transform ReactDom.render() (#19567)
    - Adds transformation of `ReactDom.render()`
    - Refactors codemod to be more DRY
    - Adds comments for clarity
    - Checks if `next` and `context` params are already set and sets them only when they're missing

--- bin/codemods/src/single-tree-rendering.js
+++ bin/codemods/src/single-tree-rendering.js
@@ -1,6 +1,22 @@
 /** @format */
 
 /**
+ * Single Tree Rendering Codemod
+ *
+ * Transforms `ReactDom.render()` to `context.primary/secondary`.
+ *
+ * Transforms `renderWithReduxStore()` to `context.primary/secondary`.
+ *
+ * Adds `context` to params in middlewares when using `ReactDom.render()`.
+ *
+ * Adds `next` to params and `next()` to body in middlewares when using
+ *   `ReactDom.render()` or `renderWithReduxStore()`.
+ *
+ * Adds `makeLayout` and `clientRender` to `page()` route definitions and
+ *   accompanying import statement.
+ */
+
+/**
  * External dependencies
  */
 const _ = require( 'lodash' );
@@ -8,8 +24,8 @@ const fs = require( 'fs' );
 const repl = require( 'repl' );
 
 /**
-* Internal dependencies
-*/
+ * Internal dependencies
+ */
 const config = require( './config' );
 
 export default function transformer( file, api ) {
@@ -39,13 +55,57 @@ export default function transformer( file, api ) {
 	 * Removes the extra newlines between two import statements
  	 * caused by `insertAfter()`:
 	 * @link https://github.com/benjamn/recast/issues/371
+	 *
+	 * @param {string} str
+	 * @returns {string}
 	 */
 	function removeExtraNewlines( str ) {
 		return str.replace( /(import.*\n)\n+(import)/g, '$1$2' );
 	}
 
-	function addNextMiddleware( path ) {
-		if ( path.value.params.length !== 1 ) {
+	/**
+	 * Check if `parameters` has `param` either as a string or as a name of
+	 * an object, which could be e.g. an `Identifier`.
+	 *
+	 * @param {array} parameters Parameters to look from. Could be an array of strings or Identifier objects.
+	 * @param {string} parameter Parameter name
+	 * @returns {boolean}
+	 */
+	function hasParam( params = [], paramValue ) {
+		return _.some( params, param => {
+			return ( param.name || param ) === paramValue;
+		} );
+	}
+
+	/**
+	 * Ensure `context` is among params
+	 *
+	 * @param {object} path Path object that wraps a single node
+	 * @returns {object} Single node object
+	 */
+	function ensureContextMiddleware( path ) {
+		// `context` param is already in
+		if ( hasParam( path.value.params, 'context' ) ) {
+			return path.value;
+		}
+		let ret = path.value;
+		ret.params = [ j.identifier( 'context' ), ...ret.params ];
+
+		return ret;
+	}
+
+	/**
+	 * Ensure `next` is among params and `next()` is in the block's body
+	 *
+	 * @param {object} path Path object that wraps a single node
+	 * @returns {object} Single node object
+	 */
+	function ensureNextMiddleware( path ) {
+		// `next` param is already in
+		if ( hasParam( path.value.params, 'next' ) ) {
+			return path.value;
+		}
+		if ( path.value.params.length > 1 ) {
 			// More than just a context arg, possibly not a middleware
 			return path.value;
 		}
@@ -69,16 +129,100 @@ export default function transformer( file, api ) {
 		}
 	}
 
+	/**
+	 * Transform `renderWithReduxStore()` CallExpressions.
+	 *
+	 * @example
+	 * Input
+	 * ```
+	 * renderWithReduxStore(
+	 * 	 <Example />,
+	 * 	 'primary',
+	 * 	 context.store
+	 * );
+	 * ```
+	 *
+	 * Output:
+	 * ```
+	 * context.primary = <Example />;
+	 * ```
+	 *
+	 * @param {object} path Path object that wraps a single node
+	 * @returns {object} Single node object
+	 */
 	function transformRenderWithReduxStore( path ) {
+		const expressionCallee = {
+			name: 'renderWithReduxStore',
+		};
+
+		return transformToContextLayout( path, expressionCallee );
+	}
+
+	/**
+	 * Transform `ReactDom.render()` CallExpressions.
+	 *
+	 * @example
+	 * Input
+	 * ```
+	 * ReactDom.render(
+	 * 	 <Example />,
+	 * 	 document.getElementById( 'primary' )
+	 * );
+	 * ```
+	 *
+	 * Output:
+	 * ```
+	 * context.primary = <Example />;
+	 * ```
+	 *
+	 * @param {object} path Path object that wraps a single node
+	 * @returns {object} Single node object
+	 */
+	function transformReactDomRender( path ) {
+		const expressionCallee = {
+			type: 'MemberExpression',
+			object: {
+				name: 'ReactDom',
+			},
+			property: {
+				name: 'render',
+			},
+		};
+
+		return transformToContextLayout( path, expressionCallee );
+	}
+
+	/**
+	 * Transform CallExpressions.
+	 * What kind of CallExpressions this replaces depends on `expressionCallee`
+	 * parameter.
+	 *
+	 * @example
+	 * Input
+	 * ```
+	 * DefinedByExpressionCallee(
+	 * 	 <Example />,
+	 * 	 document.getElementById( 'primary' )
+	 * );
+	 * ```
+	 *
+	 * Output:
+	 * ```
+	 * context.primary = <Example />;
+	 * ```
+	 *
+	 * @param {object} path Path object that wraps a single node
+	 * @param {object} expressionCallee `callee` parameter for finding `CallExpression` nodes.
+	 * @returns {object} Single node object
+	 */
+	function transformToContextLayout( path, expressionCallee ) {
 		if ( path.value.params.length !== 2 ) {
 			// More than just context and next args, possibly not a middleware
 			return path.value;
 		}
 		return j( path )
 			.find( j.CallExpression, {
-				callee: {
-					name: 'renderWithReduxStore',
-				},
+				callee: expressionCallee,
 			} )
 			.replaceWith( p => {
 				const contextArg = path.value.params[ 0 ];
@@ -89,8 +233,30 @@ export default function transformer( file, api ) {
 					p.value.arguments[ 0 ]
 				);
 			} );
+
+		return j( node );
 	}
 
+	// Transform `ReactDom.render()` to `context.primary/secondary`
+	root
+		.find( j.CallExpression, {
+			callee: {
+				type: 'MemberExpression',
+				object: {
+					name: 'ReactDom',
+				},
+				property: {
+					name: 'render',
+				},
+			},
+		} )
+		.closest( j.Function )
+		.replaceWith( ensureContextMiddleware )
+		// Receives already transformed object from `replaceWith()` above
+		.replaceWith( ensureNextMiddleware )
+		.forEach( transformReactDomRender );
+
+	// Transform `renderWithReduxStore()` to `context.primary/secondary`
 	root
 		.find( j.CallExpression, {
 			callee: {
@@ -98,7 +264,7 @@ export default function transformer( file, api ) {
 			},
 		} )
 		.closestScope()
-		.replaceWith( addNextMiddleware )
+		.replaceWith( ensureNextMiddleware )
 		.forEach( transformRenderWithReduxStore );
 
 	// Remove `renderWithReduxStore` from `import { a, renderWithReduxStore, b } from 'lib/react-helpers'`
