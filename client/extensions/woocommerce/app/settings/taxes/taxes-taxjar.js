580b51864456f3d12e4db6aad6999fbe58e8065d
Allen Snook <allen@allendav.com>
2017-11-14 10:03:59 -0800

    Tweak the wording we present to the merchant when TaxJar is active

--- client/extensions/woocommerce/app/settings/taxes/taxes-taxjar.js
+++ client/extensions/woocommerce/app/settings/taxes/taxes-taxjar.js
@@ -14,7 +14,6 @@ import { localize } from 'i18n-calypso';
  */
 import ActionHeader from 'woocommerce/components/action-header';
 import Card from 'components/card';
-import ExtendedHeader from 'woocommerce/components/extended-header';
 import ExternalLink from 'components/external-link';
 import { getLink } from 'woocommerce/lib/nav-utils';
 import Main from 'components/main';
@@ -48,23 +47,37 @@ class SettingsTaxesTaxJar extends Component {
 				<ActionHeader breadcrumbs={ breadcrumbs } />
 				<SettingsNavigation activeSection="taxes" />
 				<div className="taxes__tax-jar">
-					<ExtendedHeader
-						label={ translate( 'Tax Plugin Detected' ) }
-						description={ translate( "A third-party plugin is managing your store's taxes." ) }
-					/>
 					<Card>
 						<p>
-							{ translate( "Your store's taxes are being managed by a third-party plugin. " ) }
+							{ translate(
+								"Your store's taxes are being managed by the {{b}}TaxJar - Sales Tax Automation " +
+									'for WooCommerce plugin{{/b}}. This optional plugin adds automated reporting, filing and multi-nexus ' +
+									'support for your store.',
+								{
+									components: {
+										b: <strong />,
+									},
+								}
+							) }
+						</p>
+						<p>
+							{ translate(
+								'You can manage settings, including your personal TaxJar API key, in the plugin settings. '
+							) }
 							<ExternalLink icon href={ adminUrl } rel="noopener noreferrer">
 								{ translate( 'Plugin settings' ) }
 							</ExternalLink>
 						</p>
 						<p>
-							{ translate( 'Or, if you prefer, you can {{a}}manage or remove this plugin{{/a}}. ', {
-								components: {
-									a: <a href={ pluginUrl } className="taxes__tax-jar-deactivate" />,
-								},
-							} ) }
+							{ translate(
+								"Or, if you don't require those features, you can {{a}}remove this plugin{{/a}} and " +
+									"we'll automatically provide sales tax calculations for your store through WooCommerce Services.",
+								{
+									components: {
+										a: <a href={ pluginUrl } className="taxes__tax-jar-deactivate" />,
+									},
+								}
+							) }
 						</p>
 					</Card>
 				</div>
