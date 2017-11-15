3fdf295bb31b49ff69aad730e6be690f257a89a6
Jeff Bowen <jblz@users.noreply.github.com>
2017-11-15 10:45:38 -0500

    Post Revisions: Pull the wp-admin link out into a component (#19803)
    & fall back to using the list of revision IDs from the post object in the editor

--- client/post-editor/editor-revisions/index.jsx
+++ client/post-editor/editor-revisions/index.jsx
@@ -4,10 +4,9 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Gridicon from 'gridicons';
 import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
-import { flow, get } from 'lodash';
+import { flow } from 'lodash';
 
 /**
  * Internal dependencies
@@ -26,40 +25,7 @@ import QueryUsers from 'components/data/query-users';
 
 class EditorRevisions extends Component {
 	render = () => {
-		const {
-			adminUrl,
-			authorsIds,
-			postId,
-			revisions,
-			selectedRevisionId,
-			siteId,
-			translate,
-		} = this.props;
-
-		if ( adminUrl ) {
-			// This is what gets rendered in the sidebar
-			// @TODO take it out (& adminUrl too) when the feature flag is enabled
-			const lastRevisionId = get( revisions, [ 0, 'id' ], 0 );
-			const revisionsLink = adminUrl + 'revision.php?revision=' + lastRevisionId;
-
-			return (
-				<a
-					className="editor-revisions__wpadmin-link"
-					href={ revisionsLink }
-					target="_blank"
-					rel="noopener noreferrer"
-					aria-label={ translate( 'Open list of revisions' ) }
-				>
-					<QueryPostRevisions postId={ postId } siteId={ siteId } />
-					<QueryUsers siteId={ siteId } userIds={ authorsIds } />
-					<Gridicon icon="history" size={ 18 } />
-					{ translate( '%(revisions)d revision', '%(revisions)d revisions', {
-						count: revisions.length,
-						args: { revisions: revisions.length },
-					} ) }
-				</a>
-			);
-		}
+		const { authorsIds, postId, revisions, selectedRevisionId, siteId } = this.props;
 
 		return (
 			<div className="editor-revisions__wrapper">
@@ -81,9 +47,6 @@ class EditorRevisions extends Component {
 }
 
 EditorRevisions.propTypes = {
-	// @TODO adminUrl exists for sidebar fallback UI -- remove when that's taken out
-	adminUrl: PropTypes.string,
-
 	// connected
 	authorsIds: PropTypes.array.isRequired,
 	postId: PropTypes.number.isRequired,
