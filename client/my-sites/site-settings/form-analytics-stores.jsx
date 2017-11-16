669a30d139eba8408fb896bfe0bd68dd1ba2f354
Allen Snook <allen@allendav.com>
2017-11-15 17:03:40 -0800

    Store: Add support for universal analytics for stores

--- client/my-sites/site-settings/form-analytics-stores.jsx
+++ client/my-sites/site-settings/form-analytics-stores.jsx
@@ -21,6 +21,8 @@ class FormAnalyticsStores extends Component {
 	renderSettings = ( settings, disableAll ) => {
 		const { fields } = this.props;
 
+		// TODO - disable changes to a (child) setting if a required (parent) setting is not enabled
+
 		return (
 			<div className="site-settings__analytics-stores-basic-settings">
 				{ settings.map( setting => (
@@ -53,13 +55,57 @@ class FormAnalyticsStores extends Component {
 		return this.renderSettings( settings, disableAll );
 	};
 
+	renderAdvancedSettings = disableAll => {
+		const { translate } = this.props;
+		const settings = [
+			{
+				key: 'enh_ec_tracking',
+				label: translate( 'Enable Enhanced eCommerce' ),
+				explanation: translate(
+					'Before enabling, turn on enhanced eCommerce in your Google Analytics dashboard.'
+				),
+			},
+			{
+				key: 'enh_ec_track_remove_from_cart',
+				label: translate( 'Remove from Cart events' ),
+				requires: 'enh_ec_tracking',
+			},
+			{
+				key: 'enh_ec_track_prod_impression',
+				label: translate( 'Product impressions from listing pages' ),
+				requires: 'enh_ec_tracking',
+			},
+			{
+				key: 'enh_ec_track_prod_click',
+				label: translate( 'Product clicks from listing pages' ),
+				requires: 'enh_ec_tracking',
+			},
+			{
+				key: 'enh_ec_track_prod_detail_view',
+				label: translate( 'Product detail views' ),
+				requires: 'enh_ec_tracking',
+			},
+			{
+				key: 'enh_ec_track_checkout_started',
+				label: translate( 'Checkout process initiated' ),
+				requires: 'enh_ec_tracking',
+			},
+		];
+
+		return this.renderSettings( settings, disableAll );
+	};
+
 	render = () => {
 		const { disabled, translate } = this.props;
+
+		// TODO config flag it
 
 		return (
 			<div>
 				<FormLegend>{ translate( 'Basic store analytics' ) }</FormLegend>
 				{ this.renderBasicSettings( disabled ) }
+				<FormLegend>{ translate( 'Advanced store analytics' ) }</FormLegend>
+				{ this.renderAdvancedSettings( disabled ) }
 			</div>
 		);
 	};
