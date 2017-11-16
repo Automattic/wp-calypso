024e4ab5dae913204867eb6f9a1924ce8ce45f15
Mark Ryall <mark@ryall.name>
2017-11-16 17:44:37 +1000

    destructure imports from my-sites/controller

--- client/my-sites/tasks/index.js
+++ client/my-sites/tasks/index.js
@@ -9,9 +9,9 @@ import page from 'page';
 /**
  * Internal dependencies
  */
-import controller from 'my-sites/controller';
+import { navigation, siteSelection } from 'my-sites/controller';
 import { show } from './controller';
 
 export default function() {
-	page( '/tasks/:domain?', controller.siteSelection, controller.navigation, show );
+	page( '/tasks/:domain?', siteSelection, navigation, show );
 }
