c27c3481235a4278081aae7ba40ce329d8eef268
Jarda Snajdr <jsnajdr@gmail.com>
2017-11-20 12:03:44 +0100

    Data Layer Tests: wait for Nock request to finish before checking its results
    Fixes intermittent failures in tests where the result of the request would be asserted
    before nock triggered the response.

--- client/state/data-layer/wpcom-http/test/index.js
+++ client/state/data-layer/wpcom-http/test/index.js
@@ -4,7 +4,6 @@
  * External dependencies
  */
 import { expect } from 'chai';
-import { spy } from 'sinon';
 
 /**
  * Internal dependencies
@@ -35,13 +34,7 @@ const getMe = {
 };
 
 describe( '#queueRequest', () => {
-	let dispatch;
-
 	useNock();
-
-	beforeEach( () => {
-		dispatch = spy();
-	} );
 
 	test( 'should call `onSuccess` when a response returns with data', done => {
 		const data = { value: 1 };
@@ -49,13 +42,12 @@ describe( '#queueRequest', () => {
 			.get( '/rest/v1.1/me' )
 			.reply( 200, data );
 
-		http( { dispatch }, getMe );
-
-		setTimeout( () => {
-			expect( dispatch ).to.have.been.calledOnce;
-			expect( dispatch ).to.have.been.calledWith( extendAction( succeeder, successMeta( data ) ) );
+		const dispatch = action => {
+			expect( action ).to.be.eql( extendAction( succeeder, successMeta( data ) ) );
 			done();
-		}, 10 );
+		};
+
+		http( { dispatch }, getMe );
 	} );
 
 	test( 'should call `onFailure` when a response returns with an error', done => {
@@ -64,12 +56,11 @@ describe( '#queueRequest', () => {
 			.get( '/rest/v1.1/me' )
 			.replyWithError( error );
 
-		http( { dispatch }, getMe );
-
-		setTimeout( () => {
-			expect( dispatch ).to.have.been.calledOnce;
-			expect( dispatch ).to.have.been.calledWith( extendAction( failer, failureMeta( error ) ) );
+		const dispatch = action => {
+			expect( action ).to.be.eql( extendAction( failer, failureMeta( error ) ) );
 			done();
-		}, 10 );
+		};
+
+		http( { dispatch }, getMe );
 	} );
 } );
