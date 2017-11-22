4d204e89db2e81bd3515e72753120b2b054be6ac
Steve Seear <steve.seear@gmail.com>
2017-11-22 15:37:45 +0000

    Add some doc

--- client/jetpack-connect/cookies.js
+++ client/jetpack-connect/cookies.js
@@ -4,6 +4,12 @@
  */
 import cookie from 'cookie';
 
+/**
+ * Utilities for storing jetpack connect state that needs to persist across
+ * logins and redirects. Cookies work well for this, since redux
+ * state is not guaranteed to be persisted in these scenarios.
+ */
+
 export const storePlan = planSlug => {
 	const options = { path: '/' };
 	document.cookie = cookie.serialize( 'jetpack_connect_selected_plan', planSlug, options );
