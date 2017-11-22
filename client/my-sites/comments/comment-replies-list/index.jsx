51a2a12f03d550364f3dc26d77bb0df60f44d75a
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2017-11-22 12:56:39 -0300

    Comments: adds load more replies when a comment has more than five replies

--- client/my-sites/comments/comment-replies-list/index.jsx
+++ client/my-sites/comments/comment-replies-list/index.jsx
@@ -7,23 +7,36 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 import { localize } from 'i18n-calypso';
-import { map } from 'lodash';
+import { map, take } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import Comment from 'my-sites/comments/comment';
+import Notice from 'components/notice';
+import NoticeAction from 'components/notice/notice-action';
 import { getSiteCommentParentDepth, getSiteCommentRepliesTree } from 'state/selectors';
 
 export class CommentRepliesList extends Component {
+	constructor( props ) {
+		super( props );
+		this.state = { showAllReplies: false };
+	}
+
+	toggleShowAllRepliesReplies = () =>
+		this.setState( ( { showAllReplies } ) => ( { showAllReplies: ! showAllReplies } ) );
+
 	render() {
-		const { siteId, commentParentId, depth, replies } = this.props;
+		const { siteId, commentParentId, depth, replies, translate } = this.props;
+		const { showAllReplies } = this.state;
+
+		const repliesToShow = showAllReplies ? replies : take( replies, 5 );
 
 		const classes = classNames( { 'comment-replies-list': depth < 2 } );
 
 		return (
 			<div className={ classes }>
-				{ map( replies, ( { commentId } ) => (
+				{ map( repliesToShow, ( { commentId } ) => (
 					<Comment
 						commentId={ commentId }
 						key={ `comment-${ siteId }-${ commentParentId }-${ commentId }` }
@@ -31,6 +44,20 @@ export class CommentRepliesList extends Component {
 						isPostView={ true }
 					/>
 				) ) }
+				{ ! showAllReplies &&
+					replies.length > 5 && (
+						<Notice
+							icon="chevron-down"
+							showDismiss={ false }
+							text={ translate( 'Show %(remainingReplies)d more comments', {
+								args: { remainingReplies: replies.length - 5 },
+							} ) }
+						>
+							<NoticeAction onClick={ this.toggleShowAllReplies }>
+								{ translate( 'Show' ) }
+							</NoticeAction>
+						</Notice>
+					) }
 			</div>
 		);
 	}
