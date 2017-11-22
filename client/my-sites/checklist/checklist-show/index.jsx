ff9c944fc3e060f4efcfde7e4741b6df3f5fa060
Mark Ryall <mark@ryall.name>
2017-11-22 15:51:14 +1000

    Add Checlist component (initially in loading state).

--- client/my-sites/checklist/checklist-show/index.jsx
+++ client/my-sites/checklist/checklist-show/index.jsx
@@ -6,7 +6,6 @@
 
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import { forEach } from 'lodash';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -14,6 +13,7 @@ import { localize } from 'i18n-calypso';
  */
 
 import Card from 'components/card';
+import Checklist from 'components/checklist';
 import Main from 'components/main';
 import DocumentHead from 'components/data/document-head';
 import { getSelectedSiteId } from 'state/ui/selectors';
@@ -22,20 +22,7 @@ import QuerySiteChecklist from 'components/data/query-site-checklist';
 
 class ChecklistShow extends PureComponent {
 	render() {
-		const { siteId, siteChecklist, translate } = this.props;
-		let completedTaskCount = 0;
-		let taskCount = 0;
-		let text = '';
-
-		if ( siteChecklist && siteChecklist.tasks ) {
-			forEach( siteChecklist.tasks, value => {
-				if ( value ) {
-					completedTaskCount += 1;
-				}
-				taskCount += 1;
-			} );
-			text = `${ completedTaskCount } of ${ taskCount } tasks completed`;
-		}
+		const { siteId, translate } = this.props;
 
 		return (
 			<Main>
@@ -44,9 +31,9 @@ class ChecklistShow extends PureComponent {
 				<Card>
 					<h1 className="checklist-show__header-heading">{ translate( 'Welcome back!' ) }</h1>
 					<h2 className="checklist-show__header-text">
-						{ translate( 'Let\'s get your site ready for its debut with a few quick setup steps' ) }
+						{ translate( "Let's get your site ready for its debut with a few quick setup steps" ) }
 					</h2>
-					{ text }
+					<Checklist placeholderCount={ 5 } />
 				</Card>
 			</Main>
 		);
