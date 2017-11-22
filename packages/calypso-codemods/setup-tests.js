407660d8fc15c1297edacd01291093fa6480fc02
@samouri <@samouri>
2017-11-22 13:47:37 -0800

    fix broken deps so tests can pass. do not use yarn

--- packages/calypso-codemods/setup-tests.js
+++ packages/calypso-codemods/setup-tests.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-const api = require.requireActual('./api');
+const fs = require("fs");
+const path = require("path");
+const api = require.requireActual("./api");
 
 function test_folder(dir) {
-	const testFiles = fs.readdirSync(dir).filter(f => !f.endsWith('.spec.js') && f.endsWith('.js'));
+	const testFiles = fs.readdirSync(dir).filter(f => !f.endsWith(".spec.js") && f.endsWith(".js"));
 
 	testFiles.forEach(filename => {
 		const filepath = path.join(dir, filename);
 
 		test(filename, () => {
-      const result = api.runCodemodDry(path.basename(dir), filepath);
+			const result = api.runCodemodDry(path.basename(dir), filepath);
 			expect(result).toMatchSnapshot();
 		});
 	});
