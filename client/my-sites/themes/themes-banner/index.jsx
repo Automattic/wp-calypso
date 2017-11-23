f61a93e4e04dc55cc28c58b24021d9529e122ca2
Ernesto Méndez <mendezcode@gmail.com>
2017-11-23 16:29:07 -0400

    Initial banner implementation.

---
+++ client/my-sites/themes/themes-banner/index.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+
+export class ThemesBanner extends Component {
+	render() {
+		return <div className="themes-banner">{ this.props.children }</div>;
+	}
+}
