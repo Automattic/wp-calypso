812c2c305ff68cc7f93a3bed2b6e8b4a9f88f37b
fditrapani <filippo.di.trapani@automattic.com>
2017-11-24 13:55:20 -0500

    Add experience field

---
+++ client/state/signup/steps/user-experience/selectors.js
@@ -0,0 +1,11 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import { get } from 'lodash';
+
+export function getUserExperience( state ) {
+	return get( state, 'signup.steps.userExperience', '' );
+}
---
+++ client/state/signup/steps/user-experience/test/reducer.js
@@ -0,0 +1,26 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import reducer from '../reducer';
+import { SIGNUP_STEPS_SITE_GOALS_SET } from 'state/action-types';
+
+describe( 'reducer', () => {
+	test( 'should update the site goals', () => {
+		expect(
+			reducer(
+				{},
+				{
+					type: SIGNUP_STEPS_SITE_GOALS_SET,
+					siteGoals: 'Showcase creative work',
+				}
+			)
+		).to.be.eql( 'Showcase creative work' );
+	} );
+} );
