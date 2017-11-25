5bab644196740a2cbdd7c6c8821aa742d093dd56
Mikael Korpela <mikael@ihminen.org>
2017-11-25 21:38:41 +0200

    Remove ReduxProvider from customizer controller

--- client/my-sites/customize/controller.js
+++ client/my-sites/customize/controller.js
@@ -6,7 +6,6 @@
 
 import i18n from 'i18n-calypso';
 import React from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
 
 /**
  * Internal Dependencies
@@ -24,16 +23,13 @@ export function customize( context, next ) {
 	// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
 	context.store.dispatch( setTitle( i18n.translate( 'Customizer', { textOnly: true } ) ) );
 
-	context.primary = React.createElement(
-		ReduxProvider,
-		{ store: context.store },
-		React.createElement( CustomizeComponent, {
-			domain: context.params.domain || '',
-			pathname: context.pathname,
-			prevPath: context.prevPath || '',
-			query: context.query,
-			panel: context.params.panel,
-		} )
-	);
+	context.primary = React.createElement( CustomizeComponent, {
+		domain: context.params.domain || '',
+		pathname: context.pathname,
+		prevPath: context.prevPath || '',
+		query: context.query,
+		panel: context.params.panel,
+	} );
+
 	next();
 }
