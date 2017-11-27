56e7d68dd6556b1c7809ed40f32171b5f434e158
Marin Atanasov <tyxla@abv.bg>
2017-11-27 02:00:44 +0200

    State: Add tests for user profile links addition data layer

--- client/state/data-layer/wpcom/me/user-profile-links/test/index.js
+++ client/state/data-layer/wpcom/me/user-profile-links/test/index.js
@@ -3,14 +3,41 @@
 /**
  * Internal dependencies
  */
-import { requestUserProfileLinks, handleRequestSuccess } from '../';
+import {
+	addUserProfileLinks,
+	handleAddError,
+	handleAddSuccess,
+	handleRequestSuccess,
+	requestUserProfileLinks,
+} from '../';
+import {
+	addUserProfileLinks as addUserProfileLinksAction,
+	addUserProfileLinksDuplicate,
+	addUserProfileLinksError,
+	addUserProfileLinksMalformed,
+	addUserProfileLinksSuccess,
+	receiveUserProfileLinks,
+} from 'state/user-profile-links/actions';
 import { USER_PROFILE_LINKS_RECEIVE } from 'state/action-types';
 import { http } from 'state/data-layer/wpcom-http/actions';
 
 describe( 'wpcom-api', () => {
 	describe( 'user profile links', () => {
+		const profileLinks = [
+			{
+				link_slug: 'wordpress-org',
+				title: 'WordPress.org',
+				value: 'https://wordpress.org/',
+			},
+			{
+				link_slug: 'wordpress-com',
+				title: 'WordPress.com',
+				value: 'https://wordpress.com/',
+			},
+		];
+
 		describe( 'requestUserProfileLinks()', () => {
-			test( 'should dispatch HTTP request to the users profile links endpoint', () => {
+			test( 'should dispatch HTTP request to fetch the users profile links endpoint', () => {
 				const dispatch = jest.fn();
 
 				requestUserProfileLinks( { dispatch } );
@@ -27,20 +54,8 @@ describe( 'wpcom-api', () => {
 		} );
 
 		describe( 'handleRequestSuccess()', () => {
-			test( 'should dispatch user profile links updates', () => {
+			test( 'should dispatch user profile links receive action', () => {
 				const dispatch = jest.fn();
-				const profileLinks = [
-					{
-						link_slug: 'wordpress-org',
-						title: 'WordPress.org',
-						value: 'https://wordpress.org/',
-					},
-					{
-						link_slug: 'wordpress-com',
-						title: 'WordPress.com',
-						value: 'https://wordpress.com/',
-					},
-				];
 
 				handleRequestSuccess( { dispatch }, null, { profile_links: profileLinks } );
 
@@ -49,6 +64,87 @@ describe( 'wpcom-api', () => {
 					type: USER_PROFILE_LINKS_RECEIVE,
 					profileLinks,
 				} );
+			} );
+		} );
+
+		describe( 'addUserProfileLinks()', () => {
+			test( 'should dispatch a POST HTTP request to the users new profile links endpoint', () => {
+				const dispatch = jest.fn();
+				const action = addUserProfileLinksAction( profileLinks );
+
+				addUserProfileLinks( { dispatch }, action );
+
+				expect( dispatch ).toHaveBeenCalledTimes( 1 );
+				expect( dispatch ).toHaveBeenCalledWith(
+					http(
+						{
+							apiVersion: '1.2',
+							method: 'POST',
+							path: '/me/settings/profile-links/new',
+							body: {
+								links: action.profileLinks,
+							},
+						},
+						action
+					)
+				);
+			} );
+		} );
+
+		describe( 'handleAddSuccess()', () => {
+			test( 'should dispatch user profile links add success and receive actions', () => {
+				const dispatch = jest.fn();
+				const data = { profile_links: profileLinks };
+
+				handleAddSuccess( { dispatch }, null, data );
+
+				expect( dispatch ).toHaveBeenCalledTimes( 2 );
+				expect( dispatch ).toHaveBeenCalledWith( addUserProfileLinksSuccess( profileLinks ) );
+				expect( dispatch ).toHaveBeenCalledWith( receiveUserProfileLinks( profileLinks ) );
+			} );
+
+			test( 'should dispatch user profile links add success and duplicate actions', () => {
+				const dispatch = jest.fn();
+				const data = {
+					profile_links: [ profileLinks[ 0 ] ],
+					duplicate: [ profileLinks[ 1 ] ],
+				};
+
+				handleAddSuccess( { dispatch }, null, data );
+
+				expect( dispatch ).toHaveBeenCalledTimes( 2 );
+				expect( dispatch ).toHaveBeenCalledWith( addUserProfileLinksSuccess( data.profile_links ) );
+				expect( dispatch ).toHaveBeenCalledWith( addUserProfileLinksDuplicate( data.duplicate ) );
+			} );
+
+			test( 'should dispatch user profile links add success and malformed actions', () => {
+				const dispatch = jest.fn();
+				const data = {
+					profile_links: [ profileLinks[ 0 ] ],
+					malformed: [ profileLinks[ 1 ] ],
+				};
+
+				handleAddSuccess( { dispatch }, null, data );
+
+				expect( dispatch ).toHaveBeenCalledTimes( 2 );
+				expect( dispatch ).toHaveBeenCalledWith( addUserProfileLinksSuccess( data.profile_links ) );
+				expect( dispatch ).toHaveBeenCalledWith( addUserProfileLinksMalformed( data.malformed ) );
+			} );
+		} );
+
+		describe( 'handleAddError()', () => {
+			test( 'should dispatch user profile links add error action', () => {
+				const dispatch = jest.fn();
+				const error = {
+					status: 403,
+					message:
+						'An active access token must be used to query information about the current user.',
+				};
+
+				handleAddError( { dispatch }, { profileLinks }, error );
+
+				expect( dispatch ).toHaveBeenCalledTimes( 1 );
+				expect( dispatch ).toHaveBeenCalledWith( addUserProfileLinksError( profileLinks, error ) );
 			} );
 		} );
 	} );
