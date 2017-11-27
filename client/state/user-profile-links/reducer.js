9fec0eb7ea41f527a1edb934453cc5b9eb2d31cd
Marin Atanasov <tyxla@abv.bg>
2017-11-27 15:17:43 +0200

    State: Add reducer for user profile links deletion

--- client/state/user-profile-links/reducer.js
+++ client/state/user-profile-links/reducer.js
@@ -1,6 +1,11 @@
 /** @format */
 
 /**
+ * External dependencies
+ */
+import { reject } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import { combineReducers, createReducer } from 'state/utils';
@@ -9,12 +14,16 @@ import {
 	USER_PROFILE_LINKS_ADD_FAILURE,
 	USER_PROFILE_LINKS_ADD_MALFORMED,
 	USER_PROFILE_LINKS_ADD_SUCCESS,
+	USER_PROFILE_LINKS_DELETE_FAILURE,
+	USER_PROFILE_LINKS_DELETE_SUCCESS,
 	USER_PROFILE_LINKS_RECEIVE,
 	USER_PROFILE_LINKS_RESET_ERRORS,
 } from 'state/action-types';
 
 export const items = createReducer( [], {
 	[ USER_PROFILE_LINKS_RECEIVE ]: ( state, { profileLinks } ) => profileLinks,
+	[ USER_PROFILE_LINKS_DELETE_SUCCESS ]: ( state, { linkSlug } ) =>
+		reject( state, { link_slug: linkSlug } ),
 } );
 
 export const errors = createReducer(
@@ -28,6 +37,8 @@ export const errors = createReducer(
 			malformed: profileLinks,
 		} ),
 		[ USER_PROFILE_LINKS_ADD_FAILURE ]: ( state, { error } ) => ( { error } ),
+		[ USER_PROFILE_LINKS_DELETE_SUCCESS ]: () => ( {} ),
+		[ USER_PROFILE_LINKS_DELETE_FAILURE ]: ( state, { error } ) => ( { error } ),
 		[ USER_PROFILE_LINKS_RESET_ERRORS ]: () => ( {} ),
 	}
 );
