5803e72e461af3ccc4abda229610ccf35d823cca
Marin Atanasov <tyxla@abv.bg>
2017-11-27 02:06:58 +0200

    State: Add user profile links reducer for addition and errors

--- client/state/user-profile-links/reducer.js
+++ client/state/user-profile-links/reducer.js
@@ -4,12 +4,35 @@
  * Internal dependencies
  */
 import { combineReducers, createReducer } from 'state/utils';
-import { USER_PROFILE_LINKS_RECEIVE } from 'state/action-types';
+import {
+	USER_PROFILE_LINKS_ADD_DUPLICATE,
+	USER_PROFILE_LINKS_ADD_FAILURE,
+	USER_PROFILE_LINKS_ADD_MALFORMED,
+	USER_PROFILE_LINKS_ADD_SUCCESS,
+	USER_PROFILE_LINKS_RECEIVE,
+	USER_PROFILE_LINKS_RESET_ERRORS,
+} from 'state/action-types';
 
 export const items = createReducer( [], {
 	[ USER_PROFILE_LINKS_RECEIVE ]: ( state, { profileLinks } ) => profileLinks,
 } );
 
+export const errors = createReducer(
+	{},
+	{
+		[ USER_PROFILE_LINKS_ADD_SUCCESS ]: () => ( {} ),
+		[ USER_PROFILE_LINKS_ADD_DUPLICATE ]: ( state, { profileLinks } ) => ( {
+			duplicate: profileLinks,
+		} ),
+		[ USER_PROFILE_LINKS_ADD_MALFORMED ]: ( state, { profileLinks } ) => ( {
+			malformed: profileLinks,
+		} ),
+		[ USER_PROFILE_LINKS_ADD_FAILURE ]: ( state, { error } ) => ( { error } ),
+		[ USER_PROFILE_LINKS_RESET_ERRORS ]: () => ( {} ),
+	}
+);
+
 export default combineReducers( {
 	items,
+	errors,
 } );
