2f1fd7326ac8febdbf1b579dd03e9703bd5eff93
Bernhard Reiter <ockham@raz.or.at>
2017-11-28 20:17:24 +0100

    Layout: Modernize logged-out layout test

--- client/layout/test/index.js
+++ client/layout/test/index.js
@@ -3,32 +3,29 @@
  * External dependencies
  */
 import React from 'react';
-import ReactDomServer from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 
 /**
  * Internal dependencies
  */
-import LayoutLoggedOut from 'layout/logged-out';
-
-jest.mock( 'lib/analytics', () => {} );
+import LayoutLoggedOut from '../logged-out';
 
 describe( 'index', () => {
 	describe( 'when trying to renderToString() LayoutLoggedOut ', () => {
 		test( "doesn't throw an exception", () => {
-			const LayoutLoggedOutFactory = React.createFactory( LayoutLoggedOut );
-			const props = {
-				store: {
-					dispatch: () => {},
-					getState: () => ( {
-						ui: {},
-					} ),
-					subscribe: () => {},
-				},
-			};
-
-			expect(
-				ReactDomServer.renderToString.bind( ReactDomServer, LayoutLoggedOutFactory( props ) )
-			).not.toThrow();
+			expect( () => {
+				renderToString(
+					<LayoutLoggedOut
+						store={ {
+							dispatch: () => {},
+							getState: () => ( {
+								ui: {},
+							} ),
+							subscribe: () => {},
+						} }
+					/>
+				);
+			} ).not.toThrow();
 		} );
 	} );
 } );
