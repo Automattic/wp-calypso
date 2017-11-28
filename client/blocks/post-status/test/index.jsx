c1a9f17bfe8c568d2b8a509558b9f394cdda2677
Jonathan Sadowski <sadowski@automattic.com>
2017-11-28 13:30:57 -0600

    Update/add tests

--- client/blocks/post-status/test/index.jsx
+++ client/blocks/post-status/test/index.jsx
@@ -59,36 +59,138 @@ describe( 'PostStatus', () => {
 			} );
 
 			describe( 'future', () => {
-				test( 'should render the primary components', () => {
-					const PostStatusComponent = (
-						<PostStatus post={ { sticky: false, status: 'future' } } translate={ identity } />
-					);
-					const wrapper = shallow( PostStatusComponent );
+				describe( 'showAll', () => {
+					test( 'should render the primary components', () => {
+						const PostStatusComponent = (
+							<PostStatus
+								post={ { sticky: false, status: 'future' } }
+								showAll
+								translate={ identity }
+							/>
+						);
+						const wrapper = shallow( PostStatusComponent );
 
-					expect( wrapper ).to.have.descendants( 'span' );
-					expect( wrapper ).to.have.className( 'is-scheduled' );
-					expect( wrapper.childAt( 0 ).is( Gridicon ) ).to.be.true;
-					expect( wrapper.childAt( 0 ) ).to.have.prop( 'icon', 'calendar' );
-					expect( wrapper.childAt( 1 ) )
-						.to.have.tagName( 'span' )
-						.to.have.text( 'Scheduled' );
+						expect( wrapper ).to.have.descendants( 'span' );
+						expect( wrapper ).to.have.className( 'is-scheduled' );
+						expect( wrapper.childAt( 0 ).is( Gridicon ) ).to.be.true;
+						expect( wrapper.childAt( 0 ) ).to.have.prop( 'icon', 'calendar' );
+						expect( wrapper.childAt( 1 ) )
+							.to.have.tagName( 'span' )
+							.to.have.text( 'Scheduled' );
+					} );
+				} );
+
+				describe( 'not showAll', () => {
+					test( 'should be empty', () => {
+						const PostStatusComponent = (
+							<PostStatus post={ { sticky: false, status: 'future' } } translate={ identity } />
+						);
+						const wrapper = shallow( PostStatusComponent );
+
+						expect( wrapper ).to.be.empty;
+					} );
 				} );
 			} );
 
 			describe( 'trash', () => {
-				test( 'should render the primary components', () => {
-					const PostStatusComponent = (
-						<PostStatus post={ { sticky: false, status: 'trash' } } translate={ identity } />
-					);
-					const wrapper = shallow( PostStatusComponent );
+				describe( 'showAll', () => {
+					test( 'should render the primary components', () => {
+						const PostStatusComponent = (
+							<PostStatus
+								post={ { sticky: false, status: 'trash' } }
+								showAll
+								translate={ identity }
+							/>
+						);
+						const wrapper = shallow( PostStatusComponent );
 
-					expect( wrapper ).to.have.descendants( 'span' );
-					expect( wrapper ).to.have.className( 'is-trash' );
-					expect( wrapper.childAt( 0 ).is( Gridicon ) ).to.be.true;
-					expect( wrapper.childAt( 0 ) ).to.have.prop( 'icon', 'trash' );
-					expect( wrapper.childAt( 1 ) )
-						.to.have.tagName( 'span' )
-						.to.have.text( 'Trashed' );
+						expect( wrapper ).to.have.descendants( 'span' );
+						expect( wrapper ).to.have.className( 'is-trash' );
+						expect( wrapper.childAt( 0 ).is( Gridicon ) ).to.be.true;
+						expect( wrapper.childAt( 0 ) ).to.have.prop( 'icon', 'trash' );
+						expect( wrapper.childAt( 1 ) )
+							.to.have.tagName( 'span' )
+							.to.have.text( 'Trashed' );
+					} );
+				} );
+
+				describe( 'not showAll', () => {
+					test( 'should be empty', () => {
+						const PostStatusComponent = (
+							<PostStatus post={ { sticky: false, status: 'trash' } } translate={ identity } />
+						);
+						const wrapper = shallow( PostStatusComponent );
+
+						expect( wrapper ).to.be.empty;
+					} );
+				} );
+			} );
+
+			describe( 'draft', () => {
+				describe( 'showAll', () => {
+					test( 'should render the primary components', () => {
+						const PostStatusComponent = (
+							<PostStatus
+								post={ { sticky: false, status: 'draft' } }
+								showAll
+								translate={ identity }
+							/>
+						);
+						const wrapper = shallow( PostStatusComponent );
+
+						expect( wrapper ).to.have.descendants( 'span' );
+						expect( wrapper ).to.have.className( 'is-draft' );
+						expect( wrapper.childAt( 0 ).is( Gridicon ) ).to.be.true;
+						expect( wrapper.childAt( 0 ) ).to.have.prop( 'icon', 'aside' );
+						expect( wrapper.childAt( 1 ) )
+							.to.have.tagName( 'span' )
+							.to.have.text( 'Draft' );
+					} );
+				} );
+
+				describe( 'not showAll', () => {
+					test( 'should be empty', () => {
+						const PostStatusComponent = (
+							<PostStatus post={ { sticky: false, status: 'draft' } } translate={ identity } />
+						);
+						const wrapper = shallow( PostStatusComponent );
+
+						expect( wrapper ).to.be.empty;
+					} );
+				} );
+			} );
+
+			describe( 'publish', () => {
+				describe( 'showAll', () => {
+					test( 'should render the primary components', () => {
+						const PostStatusComponent = (
+							<PostStatus
+								post={ { sticky: false, status: 'publish' } }
+								showAll
+								translate={ identity }
+							/>
+						);
+						const wrapper = shallow( PostStatusComponent );
+
+						expect( wrapper ).to.have.descendants( 'span' );
+						expect( wrapper ).to.have.className( 'is-published' );
+						expect( wrapper.childAt( 0 ).is( Gridicon ) ).to.be.true;
+						expect( wrapper.childAt( 0 ) ).to.have.prop( 'icon', 'aside' );
+						expect( wrapper.childAt( 1 ) )
+							.to.have.tagName( 'span' )
+							.to.have.text( 'Published' );
+					} );
+				} );
+
+				describe( 'not showAll', () => {
+					test( 'should be empty', () => {
+						const PostStatusComponent = (
+							<PostStatus post={ { sticky: false, status: 'publish' } } translate={ identity } />
+						);
+						const wrapper = shallow( PostStatusComponent );
+
+						expect( wrapper ).to.be.empty;
+					} );
 				} );
 			} );
 
