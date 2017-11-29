b036574a4246eaf4692077fca911063035cde22f
Dan Hauk <hauk.dan@gmail.com>
2017-11-28 20:23:30 -0500

    Update copy and cleanup styles on incoming transfer failed info

--- client/my-sites/domains/domain-management/edit/transfer.jsx
+++ client/my-sites/domains/domain-management/edit/transfer.jsx
@@ -80,12 +80,13 @@ class Transfer extends React.PureComponent {
 		return (
 			<Card>
 				<div>
-					<p className="edit__transfer-text-fail">{ translate( 'Domain transfer failed' ) }</p>
+					<h2 className="edit__transfer-text-fail">{ translate( 'Domain transfer failed' ) }</h2>
 					<p>
 						{ translate(
-							'We were unable to verify the transfer of {{strong}}%(domain)s{{/strong}}. The domain ' +
-								'authorization code from you current registrar was not provided to initiate the transfer. ' +
-								'{{a}}Learn More{{/a}}.',
+							'We were unable to complete the transfer of {{strong}}%(domain)s{{/strong}}. It could be ' +
+								'a number of things that caused the transfer to fail like an invalid or missing authorization code, ' +
+								'the domain is still locked, or your current domain provider denied the transfer. ' +
+								'{{a}}Visit our support article{{/a}} for more detailed information about why it may have failed.',
 							{
 								components: {
 									strong: <strong />,
@@ -97,10 +98,13 @@ class Transfer extends React.PureComponent {
 					</p>
 				</div>
 				<div>
-					<Button onClick={ this.restartTransfer }>
+					<Button className="edit__transfer-button-fail" onClick={ this.restartTransfer }>
 						{ this.props.translate( 'Start Transfer Again' ) }
 					</Button>
-					<Button className="edit__transfer-button-margin" href={ support.CALYPSO_CONTACT }>
+					<Button
+						className="edit__transfer-button-fail edit__transfer-button-fail-margin"
+						href={ support.CALYPSO_CONTACT }
+					>
 						{ this.props.translate( 'Contact Support' ) }
 					</Button>
 				</div>
