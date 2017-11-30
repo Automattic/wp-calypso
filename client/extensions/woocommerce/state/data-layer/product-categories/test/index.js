c913b00bb052fefa54a6cdbc461b7891500d90da
Justin Shreve <justin.shreve@gmail.com>
2017-11-30 12:48:20 -0800

    Store: Update product categories fetching to use the data-layer (#20340)

--- client/extensions/woocommerce/state/data-layer/product-categories/test/index.js
+++ client/extensions/woocommerce/state/data-layer/product-categories/test/index.js
@@ -9,11 +9,121 @@ import { spy, match } from 'sinon';
 /**
  * Internal dependencies
  */
-import { handleProductCategoryCreate } from '../';
-import { WOOCOMMERCE_API_REQUEST } from 'woocommerce/state/action-types';
+import {
+	handleProductCategoryCreate,
+	handleProductCategoriesRequest,
+	handleProductCategoriesSuccess,
+	handleProductCategoriesError,
+} from '../';
+import {
+	WOOCOMMERCE_API_REQUEST,
+	WOOCOMMERCE_ERROR_SET,
+	WOOCOMMERCE_PRODUCT_CATEGORIES_REQUEST,
+	WOOCOMMERCE_PRODUCT_CATEGORIES_REQUEST_SUCCESS,
+} from 'woocommerce/state/action-types';
 import { createProductCategory } from 'woocommerce/state/sites/product-categories/actions';
+import { WPCOM_HTTP_REQUEST } from 'state/action-types';
 
 describe( 'handlers', () => {
+	describe( '#handleProductCategoriesRequest()', () => {
+		test( 'should dispatch a get action', () => {
+			const siteId = '123';
+			const dispatch = spy();
+			const action = {
+				type: WOOCOMMERCE_PRODUCT_CATEGORIES_REQUEST,
+				siteId,
+			};
+			handleProductCategoriesRequest( { dispatch }, action );
+			expect( dispatch ).to.have.been.calledWith(
+				match( {
+					type: WPCOM_HTTP_REQUEST,
+					method: 'GET',
+					path: `/jetpack-blogs/${ siteId }/rest-api/`,
+					query: {
+						path: '/wc/v3/products/categories&_method=GET',
+						json: true,
+						apiVersion: '1.1',
+					},
+				} )
+			);
+		} );
+	} );
+
+	describe( '#handleProductCategoriesSuccess()', () => {
+		test( 'should dispatch a success action with product category information when request completes', () => {
+			const siteId = '123';
+			const store = {
+				dispatch: spy(),
+			};
+
+			const response = [
+				{
+					id: 10,
+					name: 'Tops',
+					slug: 'tops',
+					description: '',
+					display: 'default',
+				},
+			];
+
+			const action = {
+				type: WOOCOMMERCE_PRODUCT_CATEGORIES_REQUEST,
+				siteId,
+			};
+			handleProductCategoriesSuccess( store, action, { data: response } );
+
+			expect( store.dispatch ).calledWith( {
+				type: WOOCOMMERCE_PRODUCT_CATEGORIES_REQUEST_SUCCESS,
+				siteId,
+				data: response,
+			} );
+		} );
+		test( 'should dispatch with an error if the response is not valid', () => {
+			const siteId = '123';
+			const store = {
+				dispatch: spy(),
+			};
+			const response = [ { bogus: 'test' } ];
+
+			const action = {
+				type: WOOCOMMERCE_PRODUCT_CATEGORIES_REQUEST,
+				siteId,
+			};
+			handleProductCategoriesSuccess( store, action, { data: response } );
+
+			expect( store.dispatch ).to.have.been.calledWith(
+				match( {
+					type: WOOCOMMERCE_ERROR_SET,
+					siteId,
+					originalAction: action,
+				} )
+			);
+		} );
+	} );
+
+	describe( '#handleProductCategoriesError()', () => {
+		test( 'should dispatch error', () => {
+			const siteId = '123';
+			const store = {
+				dispatch: spy(),
+			};
+
+			const action = {
+				type: WOOCOMMERCE_PRODUCT_CATEGORIES_REQUEST,
+				siteId,
+			};
+			handleProductCategoriesError( store, action, 'error' );
+
+			expect( store.dispatch ).to.have.been.calledWith(
+				match( {
+					type: WOOCOMMERCE_ERROR_SET,
+					siteId,
+					originalAction: action,
+				} )
+			);
+		} );
+	} );
+
 	describe( '#handleProductCategoryCreate', () => {
 		test( 'should dispatch a post action', () => {
 			const store = {
