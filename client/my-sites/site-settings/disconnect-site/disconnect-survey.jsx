d57c5c06cb815d4afdcd629379a8b511bb4e56da
Bernie Reiter <ockham@raz.or.at>
2017-11-30 20:29:50 +0100

    JP Disconnect Survey: Add Free-form Text Input for 'Another Reason' (#20286)
    Add a dedicated step for the 'Another Reason' option. Background: p7rd6c-19A-p2
    
    This necessitated some awkward focus handling in the `DisconnectSurvey` component since the `CompactCard` would otherwise jump to the next step upon interaction with the text field.

--- client/my-sites/site-settings/disconnect-site/disconnect-survey.jsx
+++ client/my-sites/site-settings/disconnect-site/disconnect-survey.jsx
@@ -2,7 +2,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
@@ -12,40 +12,77 @@ import { localize } from 'i18n-calypso';
  */
 import CompactCard from 'components/card/compact';
 import QuerySitePlans from 'components/data/query-site-plans';
+import FormTextInput from 'components/forms/form-text-input';
 import SectionHeader from 'components/section-header';
+import { addQueryArgs } from 'lib/url';
 import { isSiteOnPaidPlan } from 'state/selectors';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
 
-const DisconnectSurvey = ( { confirmHref, isPaidPlan, siteId, siteSlug, translate } ) => (
-	<div className="disconnect-site__survey main">
-		<QuerySitePlans siteId={ siteId } />
-		<SectionHeader
-			label={ translate(
-				'Would you mind sharing why you want to disconnect %(siteName)s from WordPress.com?',
-				{
-					args: { siteName: siteSlug },
-				}
-			) }
-		/>
-		<CompactCard href={ `/settings/disconnect-site/too-difficult/${ siteSlug }` }>
-			{ translate( 'It was too hard to configure Jetpack.' ) }
-		</CompactCard>
-		<CompactCard href={ `/settings/disconnect-site/missing-feature/${ siteSlug }` }>
-			{ translate( 'A feature I need was missing.' ) }
-		</CompactCard>
-		{ isPaidPlan && (
-			<CompactCard href={ `/settings/disconnect-site/too-expensive/${ siteSlug }` }>
-				{ translate( 'This plan is too expensive.' ) }
-			</CompactCard>
-		) }
-		<CompactCard href={ confirmHref + '?reason=troubleshooting' }>
-			{ translate( "Troubleshooting — I'll be reconnecting afterwards." ) }
-		</CompactCard>
-		<CompactCard href={ confirmHref + '?reason=other' }>
-			{ translate( 'Another reason…' ) }
-		</CompactCard>
-	</div>
-);
+class DisconnectSurvey extends PureComponent {
+	state = {
+		otherReason: '',
+		hasOtherReasonFocus: false,
+	};
+
+	interceptOtherReasonClick = event => {
+		if ( ! this.state.otherReason || this.state.hasOtherReasonFocus ) {
+			event.preventDefault();
+		}
+	};
+
+	setOtherReason = event => {
+		this.setState( { otherReason: event.target.value } );
+	};
+
+	setOtherReasonFocus = focus => () => {
+		this.setState( { hasOtherReasonFocus: focus } );
+	};
+
+	render() {
+		const { confirmHref, isPaidPlan, siteId, siteSlug, translate } = this.props;
+		return (
+			<div className="disconnect-site__survey main">
+				<QuerySitePlans siteId={ siteId } />
+				<SectionHeader
+					label={ translate(
+						'Would you mind sharing why you want to disconnect %(siteName)s from WordPress.com?',
+						{
+							args: { siteName: siteSlug },
+						}
+					) }
+				/>
+				<CompactCard href={ `/settings/disconnect-site/too-difficult/${ siteSlug }` }>
+					{ translate( 'It was too hard to configure Jetpack.' ) }
+				</CompactCard>
+				<CompactCard href={ `/settings/disconnect-site/missing-feature/${ siteSlug }` }>
+					{ translate( 'A feature I need was missing.' ) }
+				</CompactCard>
+				{ isPaidPlan && (
+					<CompactCard href={ `/settings/disconnect-site/too-expensive/${ siteSlug }` }>
+						{ translate( 'This plan is too expensive.' ) }
+					</CompactCard>
+				) }
+				<CompactCard href={ confirmHref + '?reason=troubleshooting' }>
+					{ translate( "Troubleshooting — I'll be reconnecting afterwards." ) }
+				</CompactCard>
+				<CompactCard
+					className="disconnect-site__survey-other-option"
+					href={ addQueryArgs( { reason: 'other', text: this.state.otherReason }, confirmHref ) }
+					onClick={ this.interceptOtherReasonClick }
+				>
+					{ translate( 'Another reason:' ) }
+					<FormTextInput
+						onBlur={ this.setOtherReasonFocus( false ) }
+						onChange={ this.setOtherReason }
+						onFocus={ this.setOtherReasonFocus( true ) }
+						placeholder={ translate( 'share your experience…' ) }
+						value={ this.state.otherReason }
+					/>
+				</CompactCard>
+			</div>
+		);
+	}
+}
 
 DisconnectSurvey.propTypes = {
 	confirmHref: PropTypes.string,
