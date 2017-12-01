ba55d95b3c271ef1a969c3f02d8e3c3437e71fd7
Tugdual de Kerviler <dekervit@gmail.com>
2017-12-01 14:04:20 +0100

    Add security to prevent other sections to be accessed without the X-Frame-Options' header

--- client/iframe/index.js
+++ client/iframe/index.js
@@ -11,6 +11,13 @@ import { login } from '../login/controller';
 import { makeLayout, setUpLocale } from 'controller';
 
 export default router => {
+	router( '*', context => {
+		// if we're changing section force a reload of the app
+		if ( context.path.indexOf( '/iframe' ) !== 0 ) {
+			window.location = context.canonicalPath;
+		}
+	} );
+
 	router(
 		[
 			'/log-in/:twoFactorAuthType(authenticator|backup|sms|push)/:lang?',
