7e7f245519f873a7f4e01973d4e4ae0d624df761
Marin Atanasov <tyxla@abv.bg>
2017-12-01 11:36:08 +0200

    Jetpack Onboarding: Add empty site title and site type steps

---
+++ client/jetpack-onboarding/steps/site-title.jsx
@@ -0,0 +1,26 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import FormattedHeader from 'components/formatted-header';
+
+class JetpackOnboardingSiteTitleStep extends React.Component {
+	render() {
+		const { translate } = this.props;
+		const headerText = translate( "Let's get started." );
+		const subHeaderText = translate(
+			'First up, what would you like to name your site and have as its public description?'
+		);
+
+		return <FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />;
+	}
+}
+
+export default localize( JetpackOnboardingSiteTitleStep );
