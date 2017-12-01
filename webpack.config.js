f16fb19d31ef0df754f0ca6041b283e363c90c37
Jake F - @samouri <samouri@users.noreply.github.com>
2017-12-01 13:19:39 -0600

    disable cjs

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,12 @@ const shouldMinify = process.env.hasOwnProperty( 'MINIFY_JS' )
 	? process.env.MINIFY_JS === 'true'
 	: ! isDevelopment;
 
+const babelConfig = JSON.parse( fs.readFileSync( './.babelrc', { encoding: 'utf8' } ) );
+
+babelConfig.presets[ 0 ][ 1 ].modules = false;
+babelConfig.plugins = _.without( babelConfig.plugins, 'add-module-exports' );
+
+
 /**
  * This function scans the /client/extensions directory in order to generate a map that looks like this:
  * {
@@ -62,22 +68,19 @@ function getAliasesForExtensions() {
 
 const babelLoader = {
 	loader: 'babel-loader',
-	options: {
-		cacheDirectory: path.join( __dirname, 'build', '.babel-client-cache' ),
-		cacheIdentifier: cacheIdentifier,
-		plugins: [
-			[
-				path.join(
-					__dirname,
-					'server',
-					'bundler',
-					'babel',
-					'babel-plugin-transform-wpcalypso-async'
-				),
-				{ async: config.isEnabled( 'code-splitting' ) },
-			],
-		],
-	},
+	options: Object.assign(
+		{},
+		babelConfig,
+		{
+			babelrc: false,
+			cacheDirectory: path.join( __dirname, 'build', '.babel-client-cache' ),
+			cacheIdentifier: cacheIdentifier,
+			plugins: [ [
+				path.join( __dirname, 'server', 'bundler', 'babel', 'babel-plugin-transform-wpcalypso-async' ),
+				{ async: config.isEnabled( 'code-splitting' ) }
+			] ].concat( babelConfig.plugins )
+		}
+	)
 };
 
 const webpackConfig = {
