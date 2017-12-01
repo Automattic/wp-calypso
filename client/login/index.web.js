7d75a692555743b59020f61bc4c356e4d9d78e39
Bernhard Reiter <ockham@raz.or.at>
2017-12-01 18:51:48 +0100

    Login: Add Languages Whitelist to Route Definition

--- client/login/index.web.js
+++ client/login/index.web.js
@@ -1,27 +1,39 @@
 /** @format */
 
 /**
+ * External dependencies
+ */
+import { map } from 'lodash';
+
+/**
  * Internal dependencies
  */
-
 import config from 'config';
 import { login, magicLogin, magicLoginUse, redirectDefaultLocale } from './controller';
 import { makeLayout, redirectLoggedIn, setUpLocale } from 'controller';
 
+export const lang = `:lang(${ map( config( 'languages' ), 'langSlug' ).join( '|' ) })?`;
+
 export default router => {
 	if ( config.isEnabled( 'login/magic-login' ) ) {
-		router( '/log-in/link/use/:lang?', setUpLocale, redirectLoggedIn, magicLoginUse, makeLayout );
+		router(
+			`/log-in/link/use/${ lang }`,
+			setUpLocale,
+			redirectLoggedIn,
+			magicLoginUse,
+			makeLayout
+		);
 
-		router( '/log-in/link/:lang?', setUpLocale, redirectLoggedIn, magicLogin, makeLayout );
+		router( `/log-in/link/${ lang }`, setUpLocale, redirectLoggedIn, magicLogin, makeLayout );
 	}
 
 	if ( config.isEnabled( 'login/wp-login' ) ) {
 		router(
 			[
-				'/log-in/:twoFactorAuthType(authenticator|backup|sms|push)/:lang?',
-				'/log-in/:flow(social-connect|private-site)/:lang?',
-				'/log-in/:socialService(google)/callback/:lang?',
-				'/log-in/:lang?',
+				`/log-in/:twoFactorAuthType(authenticator|backup|sms|push)/${ lang }`,
+				`/log-in/:flow(social-connect|private-site)/${ lang }`,
+				`/log-in/:socialService(google)/callback/${ lang }`,
+				`/log-in/${ lang }`,
 			],
 			redirectDefaultLocale,
 			setUpLocale,
