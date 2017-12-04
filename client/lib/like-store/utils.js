0fdf61aeeb6067a045035ecb8387bb229da5ffb7
Dennis Snell <dennis.snell@automattic.com>
2017-12-04 09:24:14 -0600

    Update: Use proper exports for like store (#20389)
    @see #18838
    
    In this PR the like store is updated to use proper module export and
    import syntax.
    
    There should be no functional or visual changes with this change.

--- client/lib/like-store/utils.js
+++ client/lib/like-store/utils.js
@@ -1,9 +1,8 @@
 /** @format */
-export default {
-	key: function( siteId, postId ) {
-		if ( ! siteId || ! postId ) {
-			throw new Error( 'siteId and postId must be non-empty' );
-		}
-		return siteId + '-' + postId;
-	},
-};
+
+export function key( siteId, postId ) {
+	if ( ! siteId || ! postId ) {
+		throw new Error( 'siteId and postId must be non-empty' );
+	}
+	return siteId + '-' + postId;
+}
