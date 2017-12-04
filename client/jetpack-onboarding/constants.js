9ccd637cabc27995f59f03b621d2608fe26de34f
Marin Atanasov <tyxla@abv.bg>
2017-12-04 16:07:27 +0200

    Jetpack Onboarding: Add business address step

--- client/jetpack-onboarding/constants.js
+++ client/jetpack-onboarding/constants.js
@@ -2,6 +2,7 @@
  * Internal Dependencies
  */
 import React from 'react';
+import JetpackOnboardingBusinessAddressStep from './steps/business-address';
 import JetpackOnboardingContactFormStep from './steps/contact-form';
 import JetpackOnboardingHomepageStep from './steps/homepage';
 import JetpackOnboardingSiteTitleStep from './steps/site-title';
@@ -14,6 +15,7 @@ export const JETPACK_ONBOARDING_STEPS = {
 	SITE_TYPE: 'site-type',
 	HOMEPAGE: 'homepage',
 	CONTACT_FORM: 'contact-form',
+	BUSINESS_ADDRESS: 'business-address',
 	WOOCOMMERCE: 'woocommerce',
 	SUMMARY: 'summary',
 };
@@ -23,6 +25,7 @@ export const JETPACK_ONBOARDING_COMPONENTS = {
 	[ JETPACK_ONBOARDING_STEPS.SITE_TYPE ]: <JetpackOnboardingSiteTypeStep />,
 	[ JETPACK_ONBOARDING_STEPS.HOMEPAGE ]: <JetpackOnboardingHomepageStep />,
 	[ JETPACK_ONBOARDING_STEPS.CONTACT_FORM ]: <JetpackOnboardingContactFormStep />,
+	[ JETPACK_ONBOARDING_STEPS.BUSINESS_ADDRESS ]: <JetpackOnboardingBusinessAddressStep />,
 	[ JETPACK_ONBOARDING_STEPS.WOOCOMMERCE ]: <JetpackOnboardingWoocommerceStep />,
 	[ JETPACK_ONBOARDING_STEPS.SUMMARY ]: <JetpackOnboardingSummaryStep />,
 };
