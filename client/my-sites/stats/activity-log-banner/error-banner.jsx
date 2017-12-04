99e4e9320ef40a2082583cbb036183ec696a45d3
Elio Rivero <eliorivero@gmail.com>
2017-12-04 11:03:19 -0600

    Solve issue that caused restarting a failed backup to start a restore

--- client/my-sites/stats/activity-log-banner/error-banner.jsx
+++ client/my-sites/stats/activity-log-banner/error-banner.jsx
@@ -6,7 +6,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import { isEmpty } from 'lodash';
+import { isUndefined } from 'lodash';
 
 /**
  * Internal dependencies
@@ -50,34 +50,28 @@ class ErrorBanner extends PureComponent {
 			rewindRestore,
 			createBackup,
 		} = this.props;
-		if ( requestedRestoreActivityId ) {
-			rewindRestore( siteId, requestedRestoreActivityId );
-		} else if ( downloadId ) {
+		if ( downloadId ) {
 			createBackup( siteId, downloadId );
+		} else if ( requestedRestoreActivityId ) {
+			rewindRestore( siteId, requestedRestoreActivityId );
 		}
 	};
 
 	handleDismiss = () =>
-		isEmpty( this.props.downloadId )
+		isUndefined( this.props.downloadId )
 			? this.props.closeDialog( 'restore' )
 			: this.props.closeDialog( 'backup' );
 
 	render() {
 		const { errorCode, failureReason, timestamp, translate, downloadId } = this.props;
-		const strings = isEmpty( downloadId )
+		const strings = isUndefined( downloadId )
 			? {
 					title: translate( 'Problem restoring your site' ),
 					details: translate( 'We came across a problem while trying to restore your site.' ),
 				}
 			: {
 					title: translate( 'Problem creating a backup' ),
-					details: (
-						<span>
-							{ translate( 'We came across a problem creating a backup for your site.' ) }
-							<br />
-							<code>{ errorCode }</code>
-						</span>
-					),
+					details: translate( 'We came across a problem creating a backup for your site.' ),
 				};
 
 		return (
@@ -90,7 +84,7 @@ class ErrorBanner extends PureComponent {
 				<TrackComponentView
 					eventName="calypso_activitylog_errorbanner_impression"
 					eventProperties={
-						isEmpty( downloadId )
+						isUndefined( downloadId )
 							? {
 									error_code: errorCode,
 									failure_reason: failureReason,
