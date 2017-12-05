b1a4badb02f4866cc945aa7dd3a9a6b77d45fac5
Mark Ryall <mark@ryall.name>
2017-12-05 15:54:14 +1000

    add simplified site title tour

--- client/layout/guided-tours/tours/site-title-tour.js
+++ client/layout/guided-tours/tours/site-title-tour.js
@@ -15,57 +15,24 @@ import Gridicon from 'gridicons';
 import {
 	ButtonRow,
 	Continue,
-	Link,
 	makeTour,
 	Next,
 	Quit,
 	Step,
 	Tour,
 } from 'layout/guided-tours/config-elements';
-import {
-	hasSelectedSiteDefaultSiteTitle,
-	isUserOlderThan,
-	isEnabled,
-	canUserEditSettingsOfSelectedSite,
-	isAbTestInVariant,
-} from 'state/ui/guided-tours/contexts';
+import { canUserEditSettingsOfSelectedSite } from 'state/ui/guided-tours/contexts';
 import { isDesktop } from 'lib/viewport';
-
-const TWO_DAYS_IN_MILLISECONDS = 2 * 1000 * 3600 * 24;
 
 export const SiteTitleTour = makeTour(
 	<Tour
 		name="siteTitle"
-		version="20161207"
+		version="20171205"
 		path="/stats"
-		when={ and(
-			isEnabled( 'guided-tours/site-title' ),
-			isDesktop,
-			hasSelectedSiteDefaultSiteTitle,
-			canUserEditSettingsOfSelectedSite,
-			isUserOlderThan( TWO_DAYS_IN_MILLISECONDS ),
-			isAbTestInVariant( 'siteTitleTour', 'enabled' )
-		) }
+		when={ and( isDesktop, canUserEditSettingsOfSelectedSite ) }
 	>
-		<Step name="init" placement="right" next="click-settings">
-			<p>
-				{ translate(
-					"Hey there! We noticed you haven't changed the title of your site yet. Want to change it?"
-				) }
-			</p>
-			<p>
-				{ translate(
-					'The site title appears in places like the top of your web browser and in search results.'
-				) }
-			</p>
-			<ButtonRow>
-				<Next step="click-settings">{ translate( 'Yes, please!' ) }</Next>
-				<Quit>{ translate( 'No, thanks.' ) }</Quit>
-			</ButtonRow>
-		</Step>
-
 		<Step
-			name="click-settings"
+			name="init"
 			target="settings"
 			arrow="left-top"
 			placement="beside"
@@ -73,7 +40,7 @@ export const SiteTitleTour = makeTour(
 			shouldScrollTo
 		>
 			<Continue target="settings" step="site-title-input" click>
-				{ translate( 'Click {{strong}}{{icon/}} Settings{{/strong}} to continue.', {
+				{ translate( 'Click {{strong}}{{icon/}} Settings{{/strong}} to get started.', {
 					components: {
 						icon: <Gridicon icon="cog" />,
 						strong: <strong />,
@@ -89,20 +56,7 @@ export const SiteTitleTour = makeTour(
 				) }
 			</p>
 			<ButtonRow>
-				<Next step="site-tagline-input">{ translate( 'Looks Good!' ) }</Next>
-				<Quit>{ translate( 'Cancel' ) }</Quit>
-			</ButtonRow>
-		</Step>
-
-		<Step name="site-tagline-input" target="site-tagline-input" arrow="top-left" placement="below">
-			<p>
-				{ translate(
-					"While you're at it, why not add a tagline? It should explain what your site is about in a few words. " +
-						'It usually appears right below your site title.'
-				) }
-			</p>
-			<ButtonRow>
-				<Next step="click-save">{ translate( 'Great!' ) }</Next>
+				<Next step="click-save">{ translate( 'Looks Good!' ) }</Next>
 				<Quit>{ translate( 'Cancel' ) }</Quit>
 			</ButtonRow>
 		</Step>
@@ -127,9 +81,6 @@ export const SiteTitleTour = makeTour(
 			<ButtonRow>
 				<Quit primary>{ translate( "We're all done!" ) }</Quit>
 			</ButtonRow>
-			<Link href="https://en.support.wordpress.com/start">
-				{ translate( 'Learn more about WordPress.com' ) }
-			</Link>
 		</Step>
 	</Tour>
 );
