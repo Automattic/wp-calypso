3d8df078e7295e70a935695dacf0e9eaeb10e5bd
Marin Atanasov <tyxla@users.noreply.github.com>
2017-12-05 18:09:04 +0200

    Components: Introduce TileGrid and Tile components (#20426)
    * Components: Introduce TileGrid component
    
    * Components: Introduce Tile component
    
    * Components: Introduce TileGrid and Tile stylesheet
    
    * DevDocs: Add TileGrid example
    
    * Components: Introduce TileGrid and Tile README
    
    * Components: Implement button class name in Tile component
    
    * Components: Remove isHidden prop from TileGrid
    
    * Components: Oxford comma in TileGrid README
    
    * Components: Fix a punctuation error in TileGrid README
    
    * Components: Use PureComponent in TileGrid example
    
    * Components: Make onClick in TileGrid example more helpful
    
    * Components: Update Tile to use Card directly
    
    * Components: Bring back the link indicator in Tile for mobile

---
+++ client/components/tile-grid/README.md
@@ -0,0 +1,49 @@
+TileGrid (jsx)
+==========
+
+Component used to declare an area for displaying Tiles — it's the main wrapper that takes care of tile alignment and positioning.
+
+#### Props
+
+* `className`: Add your own class to the grid.
+
+----------
+
+Tile (jsx)
+==========
+
+Component used to display a clickable tile with an image, call to action, and description.
+
+#### Props
+
+* `buttonClassName`: Add your own class to the tile button.
+* `buttonLabel`: Text of the button.
+* `className`: Add your own class to the tile.
+* `description`: Description text.
+* `href`: URL that the item leads to upon click.
+* `image`: URL of the image.
+* `onClick`: Function, executed when the user clicks the tile.
+
+----------
+
+#### How to use:
+
+```js
+import Tile from 'components/tile-grid/tile';
+import TileGrid from 'components/tile-grid';
+
+class MyExampleComponent extends PureComponent {
+	render() {
+		return (
+			<TileGrid>
+				<Tile
+					buttonLabel={ 'Start with a blog' }
+					description={ 'To share your ideas, stories, and photographs with your followers.' }
+					image={ '/calypso/images/illustrations/type-blog.svg' }
+					href={ 'https://wordpress.com/start' }
+				/>
+			</TileGrid>
+		);
+	}
+}
+```
---
+++ client/components/tile-grid/tile.jsx
@@ -0,0 +1,57 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import Button from 'components/button';
+import Card from 'components/card';
+
+export default class extends React.PureComponent {
+	static propTypes = {
+		buttonClassName: PropTypes.string,
+		buttonLabel: PropTypes.string,
+		className: PropTypes.string,
+		description: PropTypes.string,
+		href: PropTypes.string,
+		image: PropTypes.string,
+		onClick: PropTypes.func,
+	};
+
+	render() {
+		const {
+			buttonClassName,
+			buttonLabel,
+			className,
+			description,
+			href,
+			image,
+			onClick,
+		} = this.props;
+		const tileClassName = classNames( 'tile-grid__item', className );
+
+		return (
+			<Card className={ tileClassName } href={ href } onClick={ onClick }>
+				{ image && (
+					<div className="tile-grid__image">
+						<img src={ image } />
+					</div>
+				) }
+				<div className="tile-grid__item-copy">
+					{ buttonLabel && (
+						<Button className={ classNames( 'tile-grid__cta', buttonClassName ) } compact>
+							{ buttonLabel }
+						</Button>
+					) }
+					{ description && <p className="tile-grid__item-description">{ description }</p> }
+				</div>
+			</Card>
+		);
+	}
+}
