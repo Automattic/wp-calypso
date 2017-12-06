05c62dab83c62e73d6178f54dde20d962ffb2d8b
Bernhard Reiter <ockham@raz.or.at>
2017-12-06 16:29:41 +0100

    JP Onboarding: Add Site Title Step

--- client/jetpack-onboarding/steps/site-title.jsx
+++ client/jetpack-onboarding/steps/site-title.jsx
@@ -9,9 +9,29 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import Button from 'components/button';
+import Card from 'components/card';
 import FormattedHeader from 'components/formatted-header';
+import FormFieldset from 'components/forms/form-fieldset';
+import FormLabel from 'components/forms/form-label';
+import FormTextInput from 'components/forms/form-text-input';
+import FormTextarea from 'components/forms/form-textarea';
+import Main from 'components/main';
 
 class JetpackOnboardingSiteTitleStep extends React.PureComponent {
+	state = {
+		description: '',
+		title: '',
+	};
+
+	setDescription = event => {
+		this.setState( { description: event.target.value } );
+	};
+
+	setTitle = event => {
+		this.setState( { title: event.target.value } );
+	};
+
 	render() {
 		const { translate } = this.props;
 		const headerText = translate( "Let's get started." );
@@ -19,7 +39,27 @@ class JetpackOnboardingSiteTitleStep extends React.PureComponent {
 			'First up, what would you like to name your site and have as its public description?'
 		);
 
-		return <FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />;
+		return (
+			<Main>
+				<FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />
+
+				<Card>
+					<form>
+						<FormFieldset>
+							<FormLabel>{ translate( 'Site Title' ) }</FormLabel>
+							<FormTextInput onChange={ this.setTitle } value={ this.state.title } />
+
+							<FormLabel>{ translate( 'Site Description' ) }</FormLabel>
+							<FormTextarea onChange={ this.setDescription } value={ this.state.description } />
+
+							<Button href={ this.props.getForwardUrl() } primary>
+								{ translate( 'Next Step' ) }
+							</Button>
+						</FormFieldset>
+					</form>
+				</Card>
+			</Main>
+		);
 	}
 }
 
