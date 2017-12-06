85f0bd2ef88dc5b6d9cd716d682a17814e7ca243
Copons <copons@users.noreply.github.com>
2017-12-06 12:14:11 +0000

    Scroll back to top when clicking on a post link

--- client/my-sites/comments/comment/comment-post-link.jsx
+++ client/my-sites/comments/comment/comment-post-link.jsx
@@ -2,7 +2,8 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import Gridicon from 'gridicons';
@@ -17,26 +18,46 @@ import { getSiteComment } from 'state/selectors';
 import { getSitePost } from 'state/posts/selectors';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
 
-const CommentPostLink = ( {
-	isBulkMode,
-	isPostTitleLoaded,
-	postId,
-	postTitle,
-	siteId,
-	siteSlug,
-	status,
-	translate,
-} ) => (
-	<div className="comment__post-link">
-		{ ! isPostTitleLoaded && <QueryPosts siteId={ siteId } postId={ postId } /> }
+export class CommentPostLink extends PureComponent {
+	static propTypes = {
+		commentId: PropTypes.number,
+		isBulkMode: PropTypes.bool,
+	};
 
-		<Gridicon icon={ isBulkMode ? 'chevron-right' : 'posts' } size={ 18 } />
+	handleClick = () => {
+		if ( window ) {
+			window.scrollTo( 0, 0 );
+		}
+	};
 
-		<a href={ `/comments/${ status }/${ siteSlug }/${ postId }` } tabIndex={ isBulkMode ? -1 : 0 }>
-			{ postTitle.trim() || translate( 'Untitled' ) }
-		</a>
-	</div>
-);
+	render() {
+		const {
+			isBulkMode,
+			isPostTitleLoaded,
+			postId,
+			postTitle,
+			siteId,
+			siteSlug,
+			status,
+			translate,
+		} = this.props;
+		return (
+			<div className="comment__post-link">
+				{ ! isPostTitleLoaded && <QueryPosts siteId={ siteId } postId={ postId } /> }
+
+				<Gridicon icon={ isBulkMode ? 'chevron-right' : 'posts' } size={ 18 } />
+
+				<a
+					onClick={ this.handleClick }
+					href={ `/comments/${ status }/${ siteSlug }/${ postId }` }
+					tabIndex={ isBulkMode ? -1 : 0 }
+				>
+					{ postTitle.trim() || translate( 'Untitled' ) }
+				</a>
+			</div>
+		);
+	}
+}
 
 const mapStateToProps = ( state, { commentId } ) => {
 	const siteId = getSelectedSiteId( state );
