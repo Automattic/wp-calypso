28e08ae18e23e454c232b9fb80a036a0b472f87a
Marin Atanasov <tyxla@users.noreply.github.com>
2017-12-07 15:58:57 +0200

    Jetpack Onboarding: Add site type step tiles (#20592)
    * Jetpack Onboarding: Add site type step tiles
    
    * Jetpack Onboarding: Make site type document title more fine-grained
    
    * Jetpack Onboarding: Use translate blocks in site type tiles

--- client/jetpack-onboarding/steps/site-type.jsx
+++ client/jetpack-onboarding/steps/site-type.jsx
@@ -9,7 +9,11 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
+import Main from 'components/main';
+import Tile from 'components/tile-grid/tile';
+import TileGrid from 'components/tile-grid';
 
 class JetpackOnboardingSiteTypeStep extends React.PureComponent {
 	render() {
@@ -17,7 +21,30 @@ class JetpackOnboardingSiteTypeStep extends React.PureComponent {
 		const headerText = translate( "Let's shape your new site." );
 		const subHeaderText = translate( 'What kind of site do you need? Choose an option below:' );
 
-		return <FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />;
+		return (
+			<Main>
+				<DocumentHead title={ translate( 'Site Type ‹ Jetpack Onboarding' ) } />
+
+				<FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />
+
+				<TileGrid>
+					<Tile
+						buttonLabel={ translate( 'Personal site' ) }
+						description={ translate(
+							'To share your ideas, stories, photographs, or creative projects with your followers.'
+						) }
+						image={ '/calypso/images/illustrations/type-personal.svg' }
+					/>
+					<Tile
+						buttonLabel={ translate( 'Business site' ) }
+						description={ translate(
+							'To promote your business, organization, or brand, sell products or services, or connect with your audience.'
+						) }
+						image={ '/calypso/images/illustrations/type-business.svg' }
+					/>
+				</TileGrid>
+			</Main>
+		);
 	}
 }
 
