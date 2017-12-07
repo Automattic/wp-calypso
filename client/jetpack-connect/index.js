be92758f47823d6b50ba530d3f3528f00e332ff4
Steve Seear <steve.seear@gmail.com>
2017-12-07 16:35:25 +0000

    Combine routes

--- client/jetpack-connect/index.js
+++ client/jetpack-connect/index.js
@@ -40,13 +40,18 @@ export default function() {
 
 	page( '/jetpack/connect/store/:interval(yearly|monthly)?', controller.plansLanding );
 
-	page( '/jetpack/connect/:from(akismet|vaultpress)/:interval(yearly|monthly)?', ( { params } ) =>
-		page.redirect( `/jetpack/connect/store${ params.interval ? '/' + params.interval : '' }` )
+	page(
+		'/jetpack/connect/:_(akismet|plans|vaultpress)/:interval(yearly|monthly)?',
+		( { params } ) =>
+			page.redirect( `/jetpack/connect/store${ params.interval ? '/' + params.interval : '' }` )
 	);
 
-	page( '/jetpack/connect/plans', '/jetpack/connect/store' );
 	page( '/jetpack/connect/plans/:site', siteSelection, controller.plansSelection );
-	page( '/jetpack/connect/plans/:interval/:site', siteSelection, controller.plansSelection );
+	page(
+		'/jetpack/connect/plans/:interval(yearly|monthly)?/:site',
+		siteSelection,
+		controller.plansSelection
+	);
 
 	page(
 		'/jetpack/connect/:locale?',
