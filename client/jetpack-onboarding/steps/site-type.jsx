4a3e6e672c2957907d6ee88b07e84e6d9cb390f5
Marin Atanasov <tyxla@abv.bg>
2017-12-07 13:19:20 +0200

    Jetpack Onboarding: Add site type step tiles

--- client/jetpack-onboarding/steps/site-type.jsx
+++ client/jetpack-onboarding/steps/site-type.jsx
@@ -9,7 +9,11 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
+import Main from 'components/main';
+import Tile from 'components/tile-grid/tile';
+import TileGrid from 'components/tile-grid';
 
 class JetpackOnboardingSiteTypeStep extends React.PureComponent {
 	render() {
@@ -17,7 +21,30 @@ class JetpackOnboardingSiteTypeStep extends React.PureComponent {
 		const headerText = translate( "Let's shape your new site." );
 		const subHeaderText = translate( 'What kind of site do you need? Choose an option below:' );
 
-		return <FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />;
+		return (
+			<Main>
+				<DocumentHead title={ translate( 'Jetpack Onboarding' ) } />
+
+				<FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />
+
+				<TileGrid>
+					<Tile
+						buttonLabel={ 'Personal site' }
+						description={
+							'To share your ideas, stories, photographs, or creative projects with your followers.'
+						}
+						image={ '/calypso/images/illustrations/type-personal.svg' }
+					/>
+					<Tile
+						buttonLabel={ 'Business site' }
+						description={
+							'To promote your business, organization, or brand, sell products or services, or connect with your audience.'
+						}
+						image={ '/calypso/images/illustrations/type-business.svg' }
+					/>
+				</TileGrid>
+			</Main>
+		);
 	}
 }
 
