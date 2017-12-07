829deec8c40e000ce9576bf590f5b3b5a94804a7
Jonathan Sadowski <sadowski@automattic.com>
2017-12-07 13:28:52 -0600

    only display counts for enabled features

--- client/my-sites/post-type-list/post-action-counts/index.jsx
+++ client/my-sites/post-type-list/post-action-counts/index.jsx
@@ -15,7 +15,8 @@ import { get } from 'lodash';
 import QueryPostStats from 'components/data/query-post-stats';
 import { getNormalizedPost } from 'state/posts/selectors';
 import { getPostStat } from 'state/stats/posts/selectors';
-import { getSiteSlug } from 'state/sites/selectors';
+import { canCurrentUser } from 'state/selectors';
+import { getSiteSlug, isJetpackModuleActive, isJetpackSite } from 'state/sites/selectors';
 
 class PostActionCounts extends PureComponent {
 	static propTypes = {
@@ -23,7 +24,11 @@ class PostActionCounts extends PureComponent {
 	};
 
 	renderCommentCount() {
-		const { commentCount: count, numberFormat, postId, siteSlug, translate } = this.props;
+		const { commentCount: count, numberFormat, postId, showComments, siteSlug, translate } = this.props;
+
+		if ( count < 1 || ! showComments ) {
+			return null;
+		}
 
 		return (
 			<li>
@@ -42,7 +47,11 @@ class PostActionCounts extends PureComponent {
 	}
 
 	renderLikeCount() {
-		const { likeCount: count, numberFormat, postId, siteSlug, translate } = this.props;
+		const { likeCount: count, numberFormat, postId, showLikes, siteSlug, translate } = this.props;
+
+		if ( count < 1 || ! showLikes ) {
+			return null;
+		}
 
 		return (
 			<li>
@@ -61,7 +70,11 @@ class PostActionCounts extends PureComponent {
 	}
 
 	renderViewCount() {
-		const { viewCount: count, numberFormat, postId, siteSlug, translate } = this.props;
+		const { viewCount: count, numberFormat, postId, showViews, siteSlug, translate } = this.props;
+
+		if ( count < 1 || ! showViews ) {
+			return null;
+		}
 
 		return (
 			<li>
@@ -80,14 +93,14 @@ class PostActionCounts extends PureComponent {
 	}
 
 	render() {
-		const { commentCount, likeCount, postId, siteId, viewCount } = this.props;
+		const { postId, siteId } = this.props;
 
 		return (
 			<ul className="post-action-counts">
 				{ siteId && <QueryPostStats siteId={ siteId } postId={ postId } fields={ [ 'views' ] } /> }
-				{ viewCount > 0 && this.renderViewCount() }
-				{ likeCount > 0 && this.renderLikeCount() }
-				{ commentCount > 0 && this.renderCommentCount() }
+				{ this.renderViewCount() }
+				{ this.renderLikeCount() }
+				{ this.renderCommentCount() }
 			</ul>
 		);
 	}
@@ -98,10 +111,25 @@ export default connect( ( state, { globalId } ) => {
 	const postId = post && post.ID;
 	const siteId = post && post.site_ID;
 
+	const isJetpack = isJetpackSite( state, siteId );
+
+	const showComments =
+		( ! isJetpack || isJetpackModuleActive( state, siteId, 'comments' ) ) &&
+		post &&
+		post.discussion &&
+		post.discussion.comments_open;
+	const showLikes = ! isJetpack || isJetpackModuleActive( state, siteId, 'likes' );
+	const showViews =
+		canCurrentUser( state, siteId, 'view_stats' ) &&
+		( ! isJetpack || isJetpackModuleActive( state, siteId, 'stats' ) );
+
 	return {
 		commentCount: get( post, 'discussion.comment_count', null ),
 		likeCount: get( post, 'like_count', null ),
 		postId,
+		showComments,
+		showLikes,
+		showViews,
 		siteId,
 		siteSlug: getSiteSlug( state, siteId ),
 		viewCount: getPostStat( state, siteId, postId, 'views' ),
