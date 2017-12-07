68fdeafbaa870c9f79ac2a7d7e9fe72951e68903
Marin Atanasov <tyxla@users.noreply.github.com>
2017-12-07 15:58:42 +0200

    Jetpack Onboarding: Add homepage step tiles (#20593)
    * Jetpack Onboarding: Add homepage step tiles
    
    * Jetpack Onboarding: Use translate blocks in homepage tiles

--- client/jetpack-onboarding/steps/homepage.jsx
+++ client/jetpack-onboarding/steps/homepage.jsx
@@ -9,7 +9,11 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
+import Main from 'components/main';
+import Tile from 'components/tile-grid/tile';
+import TileGrid from 'components/tile-grid';
 
 class JetpackOnboardingHomepageStep extends React.PureComponent {
 	render() {
@@ -17,7 +21,28 @@ class JetpackOnboardingHomepageStep extends React.PureComponent {
 		const headerText = translate( "Let's shape your new site." );
 		const subHeaderText = translate( 'What should visitors see on your homepage?' );
 
-		return <FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />;
+		return (
+			<Main>
+				<DocumentHead title={ translate( 'Homepage ‹ Jetpack Onboarding' ) } />
+
+				<FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />
+
+				<TileGrid>
+					<Tile
+						buttonLabel={ translate( 'Recent news or updates' ) }
+						description={ translate(
+							'We can pull the latest information into your homepage for you.'
+						) }
+						image={ '/calypso/images/illustrations/homepage-news.svg' }
+					/>
+					<Tile
+						buttonLabel={ translate( 'A static welcome page' ) }
+						description={ translate( 'Have your homepage stay the same as time goes on.' ) }
+						image={ '/calypso/images/illustrations/homepage-static.svg' }
+					/>
+				</TileGrid>
+			</Main>
+		);
 	}
 }
 
