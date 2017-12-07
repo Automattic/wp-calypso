59f2e8a9cfc9bd804300bd2f4fbf2885f99c2095
Bernie Reiter <ockham@raz.or.at>
2017-12-07 14:02:18 +0100

    JP Onboarding: Add Site Title Step (#20566)
    This doesn't really do anything yet, i.e. it doesn't store the changes made anywhere, nor does it propagate them to the site.

--- client/jetpack-onboarding/steps/site-title.jsx
+++ client/jetpack-onboarding/steps/site-title.jsx
@@ -9,9 +9,30 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import Button from 'components/button';
+import Card from 'components/card';
+import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
+import FormFieldset from 'components/forms/form-fieldset';
+import FormLabel from 'components/forms/form-label';
+import FormTextarea from 'components/forms/form-textarea';
+import FormTextInput from 'components/forms/form-text-input';
+import Main from 'components/main';
 
 class JetpackOnboardingSiteTitleStep extends React.PureComponent {
+	state = {
+		description: '',
+		title: '',
+	};
+
+	setDescription = event => {
+		this.setState( { description: event.target.value } );
+	};
+
+	setTitle = event => {
+		this.setState( { title: event.target.value } );
+	};
+
 	render() {
 		const { translate } = this.props;
 		const headerText = translate( "Let's get started." );
@@ -19,7 +40,34 @@ class JetpackOnboardingSiteTitleStep extends React.PureComponent {
 			'First up, what would you like to name your site and have as its public description?'
 		);
 
-		return <FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />;
+		return (
+			<Main>
+				<DocumentHead title={ translate( 'Site Title ‹ Jetpack Onboarding' ) } />
+				<FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />
+
+				<Card>
+					<form>
+						<FormFieldset>
+							<FormLabel htmlFor="title">{ translate( 'Site Title' ) }</FormLabel>
+							<FormTextInput id="title" onChange={ this.setTitle } value={ this.state.title } />
+						</FormFieldset>
+
+						<FormFieldset>
+							<FormLabel htmlFor="description">{ translate( 'Site Description' ) }</FormLabel>
+							<FormTextarea
+								id="description"
+								onChange={ this.setDescription }
+								value={ this.state.description }
+							/>
+						</FormFieldset>
+
+						<Button href={ this.props.getForwardUrl() } primary>
+							{ translate( 'Next Step' ) }
+						</Button>
+					</form>
+				</Card>
+			</Main>
+		);
 	}
 }
 
