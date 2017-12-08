e481ebb76550dc69dbf1637e6eaacb57807d0d8a
Greg Ichneumon Brown <gbrown5878@gmail.com>
2017-12-08 16:44:47 -0700

    My Plan: add Jetpack Search (Beta)
    Also rearrange the feature ordering to put features that need configuration at the top.

---
+++ client/blocks/product-purchase-features/product-purchase-features-list/jetpack-search.jsx
@@ -0,0 +1,29 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import PurchaseDetail from 'components/purchase-detail';
+
+export default localize( ( { selectedSite, translate } ) => {
+	return (
+		<div className="product-purchase-features-list__item">
+			<PurchaseDetail
+				icon="search"
+				title={ translate( 'Search (Beta)' ) }
+				description={ translate(
+					'Replace the default WordPress search with better results that will help your users find what they are looking for.'
+				) }
+				buttonText={ translate( 'Configure Search' ) }
+				href={ '/settings/traffic/' + selectedSite.slug }
+			/>
+		</div>
+	);
+} );
