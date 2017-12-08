6387871dbf8c6b10608fc0bb77ae2b0d5947fc3a
Jeff Bowen <jeff@automattic.com>
2017-12-08 01:46:01 -0500

    Working inline diffs from the server

--- client/post-editor/editor-diff-viewer/changes.jsx
+++ client/post-editor/editor-diff-viewer/changes.jsx
@@ -6,31 +6,61 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { map } from 'lodash';
+import { isArray, isEmpty, map } from 'lodash';
 
-const EditorDiffChanges = ( { changes } ) =>
-	map( changes, ( change, changeIndex ) => {
-		const changeClassNames = classNames( {
-			'editor-diff-viewer__additions': change.added,
-			'editor-diff-viewer__deletions': change.removed,
-		} );
-		return (
-			<span className={ changeClassNames } key={ changeIndex }>
-				{ change.value }
-			</span>
-		);
-	} );
+const addLinesToChanges = changes => {
+	if ( ! isArray( changes ) || isEmpty( changes ) ) {
+		return changes;
+	}
+	return changes.join( '\n\n' );
+};
+
+const renderChange = ( change, changeIndex, splitLines ) => {
+	switch ( change.op ) {
+		case 'add': {
+			const content = change.final || change.value;
+			return (
+				<span className="editor-diff-viewer__additions" key={ changeIndex }>
+					{ splitLines ? addLinesToChanges( content ) : content }
+				</span>
+			);
+		}
+		case 'change': {
+			return (
+				<span className="editor-diff-viewer__changes" key={ changeIndex }>
+					{ map( change.inline, ( changeList, inlineIndex ) =>
+						map( changeList, ( inlineChanges, inlineIndex1 ) =>
+							renderChange( inlineChanges, `${ changeIndex }:${ inlineIndex }:${ inlineIndex1 }` )
+						)
+					) }
+				</span>
+			);
+		}
+		case 'copy': {
+			const content = change.final || change.value;
+			return (
+				<span className="editor-diff-viewer__context" key={ changeIndex }>
+					{ splitLines ? addLinesToChanges( content ) : content }
+				</span>
+			);
+		}
+		case 'del': {
+			const content = change.orig || change.value;
+			return (
+				<span className="editor-diff-viewer__deletions" key={ changeIndex }>
+					{ splitLines ? addLinesToChanges( content ) : content }
+				</span>
+			);
+		}
+	}
+};
+
+const EditorDiffChanges = ( { changes, splitLines } ) =>
+	map( changes, ( change, changeIndex ) => renderChange( change, changeIndex, splitLines ) );
 
 EditorDiffChanges.propTypes = {
-	changes: PropTypes.arrayOf(
-		PropTypes.shape( {
-			value: PropTypes.string.isRequired,
-			added: PropTypes.boolean,
-			removed: PropTypes.removed,
-			count: PropTypes.number,
-		} ).isRequired
-	),
+	changes: PropTypes.array,
+	splitLines: PropTypes.bool,
 };
 
 export default EditorDiffChanges;
--- client/state/selectors/get-post-revisions-diff.js
+++ client/state/selectors/get-post-revisions-diff.js
@@ -11,6 +11,6 @@ export default function getPostRevisionsDiff(
 	fromRevisionId,
 	toRevisionId
 ) {
-	const key = `${ fromRevisionId }:${ toRevisionId }`;
+	const key = `${ fromRevisionId || 0 }:${ toRevisionId || 0 }`;
 	return get( state.posts.revisions.diffs, [ siteId, postId, key, 'diff' ], {} );
 }
