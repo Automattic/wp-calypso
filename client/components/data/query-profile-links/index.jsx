f1dcb625421b3768a8b579ec4100333dc62242c7
Marin Atanasov <tyxla@abv.bg>
2017-12-08 15:55:07 +0200

    Components: Add QueryProfileLinks component

---
+++ client/components/data/query-profile-links/index.jsx
@@ -0,0 +1,29 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { requestUserProfileLinks } from 'state/profile-links/actions';
+
+class QueryProfileLinks extends Component {
+	static propTypes = {
+		requestUserProfileLinks: PropTypes.func,
+	};
+
+	componentDidMount() {
+		this.props.requestUserProfileLinks();
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default connect( null, { requestUserProfileLinks } )( QueryProfileLinks );
