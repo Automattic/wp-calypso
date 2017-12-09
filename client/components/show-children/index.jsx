3764c24084e192a466d2466ec98f2656de1c7001
Nicola Heald <nicola@notnowlewis.com>
2017-12-09 15:26:14 +0000

    Test for disabling sources in MediaLibraryDataSource

---
+++ client/components/show-children/index.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+
+export default class ShowChildren extends Component {
+	render() {
+		return <div>{ this.props.children }</div>;
+	}
+}
