Automattic/wp-calypso

commit ed11cd0462e97223dfce9bb8735a007f3179c4ed

Framework: Removed sites-list dependency from lib/posts/actions.js (#20328)
* Framework: Removed sites-list dependency prom lib/posts/actions.js;

Make required actions receive site object to allow future remove of sites-list from lib/posts, update the required components to pass the site object.

client/post-editor/controller.js
@@ -26,6 +26,7 @@ import { decodeEntities } from 'lib/formatting';
 import PostEditor from './post-editor';
 import { startEditingPost, stopEditingPost } from 'state/ui/editor/actions';
 import { getSelectedSiteId } from 'state/ui/selectors';
+import { getSite } from 'state/sites/selectors';
 import { getEditorPostId, getEditorPath } from 'state/ui/editor/selectors';
 import { editPost } from 'state/posts/actions';
 import wpcom from 'lib/wp';
@@ -210,6 +211,7 @@ export default {
 		const postToCopyId = context.query.copy;
 
 		function startEditing( siteId ) {
+			const site = getSite( context.store.getState(), siteId );
 			const isCopy = context.query.copy ? true : false;
 			context.store.dispatch( startEditingPost( siteId, isCopy ? null : postID, postType ) );
 
@@ -237,7 +239,7 @@ export default {
 				analytics.pageView.record( '/' + postType, gaTitle + ' > New' );
 			} else if ( postID ) {
 				// TODO: REDUX - remove flux actions when whole post-editor is reduxified
-				actions.startEditingExisting( siteId, postID );
+				actions.startEditingExisting( site, postID );
 				analytics.pageView.record( '/' + postType + '/:blogid/:postid', gaTitle + ' > Edit' );
 			} else {
 				const postOptions = { type: postType };
@@ -253,7 +255,7 @@ export default {
 				}
 
 				// TODO: REDUX - remove flux actions when whole post-editor is reduxified
-				actions.startEditingNew( siteId, postOptions );
+				actions.startEditingNew( site, postOptions );
 				analytics.pageView.record( '/' + postType, gaTitle + ' > New' );
 			}
 		}

client/components/update-post-status/index.js
@@ -8,11 +8,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import { flow, once } from 'lodash';
+import { flow, get, once } from 'lodash';
 
 /**
  * Internal dependencies
  */
+import { getSite } from 'state/sites/selectors';
 import UpdateTemplate from './update-template';
 import PostActions from 'lib/posts/actions';
 import { recordGoogleEvent } from 'state/analytics/actions';
@@ -48,7 +49,15 @@ const getStrings = once( translate => ( {
 	},
 } ) );
 
-const enhance = flow( localize, connect( null, { recordGoogleEvent } ) );
+const enhance = flow(
+	localize,
+	connect(
+		( state, props ) => ( {
+			site: getSite( state, get( props, [ props.page ? 'page' : 'post', 'site_ID' ] ) ),
+		} ),
+		{ recordGoogleEvent }
+	)
+);
 
 const updatePostStatus = WrappedComponent =>
 	enhance(
@@ -95,6 +104,7 @@ const updatePostStatus = WrappedComponent =>
 			};
 
 			updatePostStatus = status => {
+				const { site } = this.props;
 				const post = this.props.post || this.props.page;
 				let previousStatus = null;
 
@@ -125,7 +135,7 @@ const updatePostStatus = WrappedComponent =>
 
 						if ( typeof window === 'object' && window.confirm( strings[ type ].deleteWarning ) ) {
 							// eslint-disable-line no-alert
-							PostActions.trash( post, setNewStatus );
+							PostActions.trash( site, post, setNewStatus );
 						} else {
 							this.resetState();
 						}
@@ -138,7 +148,7 @@ const updatePostStatus = WrappedComponent =>
 							updated: true,
 						} );
 						previousStatus = post.status;
-						PostActions.trash( post, setNewStatus );
+						PostActions.trash( site, post, setNewStatus );
 						return;
 
 					case 'restore':
@@ -148,7 +158,7 @@ const updatePostStatus = WrappedComponent =>
 							updated: true,
 						} );
 						previousStatus = 'trash';
-						PostActions.restore( post, setNewStatus );
+						PostActions.restore( site, post, setNewStatus );
 						return;
 
 					default:
@@ -157,7 +167,7 @@ const updatePostStatus = WrappedComponent =>
 							updatedStatus: 'updating',
 							updated: true,
 						} );
-						PostActions.update( post, { status }, ( error, resultPost ) => {
+						PostActions.update( site, post, { status }, ( error, resultPost ) => {
 							if ( ! setNewStatus( error, resultPost ) ) {
 								return;
 							}

client/lib/posts/actions.js
@@ -16,14 +16,12 @@ import PostsStore from './posts-store';
 import PostEditStore from './post-edit-store';
 import postListStoreFactory from './post-list-store-factory';
 import PreferencesStore from 'lib/preferences/store';
-import sitesFactory from 'lib/sites-list';
 import utils from './utils';
 import versionCompare from 'lib/version-compare';
 import Dispatcher from 'dispatcher';
 import { recordSaveEvent } from './stats';
 import { normalizeTermsForApi } from 'state/posts/utils';
 
-const sites = sitesFactory();
 const debug = debugFactory( 'calypso:posts' );
 
 var PostActions;
@@ -100,19 +98,20 @@ PostActions = {
 	/**
 	 * Start keeping track of edits to a new post
 	 *
-	 * @param {Number} siteId  Site ID
+	 * @param {Object} site    Site object
 	 * @param {Object} options Edit options
 	 */
-	startEditingNew: function( siteId, options ) {
+	startEditingNew: function( site, options ) {
 		var args;
 		options = options || {};
 
 		args = {
 			type: 'DRAFT_NEW_POST',
-			siteId: siteId,
+			siteId: site.ID,
 			postType: options.type || 'post',
 			title: options.title,
 			content: options.content,
+			site,
 		};
 
 		Dispatcher.handleViewAction( args );
@@ -121,34 +120,35 @@ PostActions = {
 	/**
 	 * Load an existing post and keep track of edits to it
 	 *
-	 * @param {Number} siteId Site ID to load post from
+	 * @param {Object} site   Site object
 	 * @param {Number} postId Post ID to load
 	 */
-	startEditingExisting: function( siteId, postId ) {
+	startEditingExisting: function( site, postId ) {
 		var currentPost = PostEditStore.get(),
 			postHandle;
 
-		if ( ! siteId ) {
+		if ( ! site || ! site.ID ) {
 			return;
 		}
 
-		if ( currentPost && currentPost.site_ID === siteId && currentPost.ID === postId ) {
+		if ( currentPost && currentPost.site_ID === site.ID && currentPost.ID === postId ) {
 			return; // already editing same post
 		}
 
 		Dispatcher.handleViewAction( {
 			type: 'START_EDITING_POST',
-			siteId: siteId,
+			siteId: site.ID,
 			postId: postId,
 		} );
 
-		postHandle = wpcom.site( siteId ).post( postId );
+		postHandle = wpcom.site( site.ID ).post( postId );
 
 		postHandle.get( { context: 'edit', meta: 'autosave' }, function( error, data ) {
 			Dispatcher.handleServerAction( {
 				type: 'RECEIVE_POST_TO_EDIT',
 				error: error,
 				post: data,
+				site,
 			} );
 		} );
 	},
@@ -162,11 +162,10 @@ PostActions = {
 		} );
 	},
 
-	autosave: function( callback ) {
+	autosave: function( site, callback ) {
 		var post = PostEditStore.get(),
 			savedPost = PostEditStore.getSavedPost(),
-			siteHandle = wpcom.undocumented().site( post.site_ID ),
-			site;
+			siteHandle = wpcom.undocumented().site( post.site_ID );
 
 		callback = callback || function() {};
 
@@ -178,8 +177,6 @@ PostActions = {
 
 		// TODO: incorporate post locking
 		if ( utils.isPublished( savedPost ) || utils.isPublished( post ) ) {
-			site = sites.getSite( post.site_ID );
-
 			if (
 				! post.ID ||
 				! site ||
@@ -205,13 +202,17 @@ PostActions = {
 						type: 'RECEIVE_POST_AUTOSAVE',
 						error: error,
 						autosave: data,
+						site,
 					} );
 
 					callback( error, data );
 				}
 			);
 		} else {
-			PostActions.saveEdited( null, null, callback, { recordSaveEvent: false, autosave: true } );
+			PostActions.saveEdited( site, null, null, callback, {
+				recordSaveEvent: false,
+				autosave: true,
+			} );
 		}
 	},
 
@@ -294,12 +295,13 @@ PostActions = {
 	/**
 	 * Calls out to API to save a Post object
 	 *
+	 * @param {Object} site Site object
 	 * @param {object} attributes post attributes to change before saving
 	 * @param {object} context additional properties for recording the save event
 	 * @param {function} callback receives ( err, post ) arguments
 	 * @param {object} options object with optional recordSaveEvent property. True if you want to record the save event.
 	 */
-	saveEdited: function( attributes, context, callback, options ) {
+	saveEdited: function( site, attributes, context, callback, options ) {
 		var post, postHandle, query, changedAttributes, rawContent, mode, isNew;
 
 		Dispatcher.handleViewAction( {
@@ -352,7 +354,7 @@ PostActions = {
 		}
 
 		if ( ! options || options.recordSaveEvent !== false ) {
-			recordSaveEvent( context ); // do this before changing status from 'future'
+			recordSaveEvent( context, site ); // do this before changing status from 'future'
 		}
 
 		if (
@@ -392,6 +394,7 @@ PostActions = {
 				isNew: isNew,
 				original: original,
 				post: data,
+				site,
 			} );
 
 			callback( error, data );
@@ -401,27 +404,30 @@ PostActions = {
 	/**
 	 * Calls out to API to update Post object with any changed attributes
 	 *
+	 * @param {Object} site Site object
 	 * @param {object} post to be changed
 	 * @param {object} attributes only send the attributes to be changed
 	 * @param {function} callback callback receives ( err, post ) arguments
+
 	 */
-	update: function( post, attributes, callback ) {
+	update: function( site, post, attributes, callback ) {
 		var postHandle = wpcom.site( post.site_ID ).post( post.ID );
 
-		postHandle.update( attributes, PostActions.receiveUpdate.bind( null, callback ) );
+		postHandle.update( attributes, PostActions.receiveUpdate.bind( null, site, callback ) );
 	},
 
 	/**
 	 * Sends `delete` request to the API. The first request
 	 * updates status to `trash`, the second request deletes the post.
 	 *
+	 * @param {Object} site Site object
 	 * @param {object} post to be trashed
 	 * @param {function} callback that receives ( err, post ) arguments
 	 */
-	trash: function( post, callback ) {
+	trash: function( site, post, callback ) {
 		var postHandle = wpcom.site( post.site_ID ).post( post.ID );
 
-		postHandle.delete( PostActions.receiveUpdate.bind( null, callback ) );
+		postHandle.delete( PostActions.receiveUpdate.bind( null, site, callback ) );
 	},
 
 	/**
@@ -429,11 +435,12 @@ PostActions = {
 	 *
 	 * @param {object} post to be trashed
 	 * @param {function} callback that receives ( err, post ) arguments
+	 * @param {Object} site Site object
 	 */
-	restore: function( post, callback ) {
+	restore: function( site, post, callback ) {
 		var postHandle = wpcom.site( post.site_ID ).post( post.ID );
 
-		postHandle.restore( PostActions.receiveUpdate.bind( null, callback ) );
+		postHandle.restore( PostActions.receiveUpdate.bind( null, site, callback ) );
 	},
 
 	queryPosts: function( options, postListStoreId = 'default' ) {
@@ -526,7 +533,7 @@ PostActions = {
 		} );
 	},
 
-	receiveUpdate: function( callback, error, data ) {
+	receiveUpdate: function( site, callback, error, data ) {
 		var original;
 
 		if ( ! error ) {
@@ -538,6 +545,7 @@ PostActions = {
 			error: error,
 			original: original,
 			post: data,
+			site,
 		} );
 		callback( error, data );
 	},

client/lib/posts/test/actions.js
@@ -19,7 +19,12 @@ import Dispatcher from 'dispatcher';
 
 jest.mock( 'lib/localforage', () => require( 'lib/localforage/localforage-bypass' ) );
 jest.mock( 'lib/wp', () => require( './mocks/lib/wp' ) );
-
+const sampleSite = {
+	ID: 123,
+	jetpack: false,
+	slug: 'example.wordpress.com',
+	URL: 'https://example.wordpress.com',
+};
 describe( 'actions', () => {
 	let sandbox;
 
@@ -176,7 +181,7 @@ describe( 'actions', () => {
 			const spy = sandbox.spy();
 			sandbox.stub( PostEditStore, 'hasContent' ).returns( false );
 
-			PostActions.saveEdited( null, {}, spy );
+			PostActions.saveEdited( sampleSite, null, {}, spy );
 
 			defer( () => {
 				expect( spy ).to.have.been.calledOnce;
@@ -192,7 +197,7 @@ describe( 'actions', () => {
 			sandbox.stub( PostEditStore, 'hasContent' ).returns( true );
 			sandbox.stub( PostEditStore, 'getChangedAttributes' ).returns( {} );
 
-			PostActions.saveEdited( null, {}, spy );
+			PostActions.saveEdited( sampleSite, null, {}, spy );
 
 			defer( () => {
 				expect( spy ).to.have.been.calledOnce;
@@ -224,7 +229,7 @@ describe( 'actions', () => {
 			};
 			sandbox.stub( PostEditStore, 'getChangedAttributes' ).returns( changedAttributes );
 
-			PostActions.saveEdited( null, {}, ( error, data ) => {
+			PostActions.saveEdited( sampleSite, null, {}, ( error, data ) => {
 				const normalizedAttributes = {
 					ID: 777,
 					site_ID: 123,

client/my-sites/draft/index.jsx
@@ -89,7 +89,7 @@ class Draft extends Component {
 		}.bind( this );
 
 		if ( utils.userCan( 'delete_post', this.props.post ) ) {
-			actions.trash( this.props.post, updateStatus );
+			actions.trash( this.props.site, this.props.post, updateStatus );
 		}
 	};
 
@@ -115,7 +115,7 @@ class Draft extends Component {
 		}.bind( this );
 
 		if ( utils.userCan( 'delete_post', this.props.post ) ) {
-			actions.restore( this.props.post, updateStatus );
+			actions.restore( this.props.site, this.props.post, updateStatus );
 		}
 	};
 

client/post-editor/editor-delete-post/index.jsx
@@ -8,6 +8,7 @@ import PropTypes from 'prop-types';
 import { localize } from 'i18n-calypso';
 import React from 'react';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
 import Gridicon from 'gridicons';
 
 /**
@@ -17,6 +18,7 @@ import actions from 'lib/posts/actions';
 import accept from 'lib/accept';
 import utils from 'lib/posts/utils';
 import Button from 'components/button';
+import { getSelectedSite } from 'state/ui/selectors';
 
 class EditorDeletePost extends React.Component {
 	static displayName = 'EditorDeletePost';
@@ -39,7 +41,7 @@ class EditorDeletePost extends React.Component {
 		this.setState( { isTrashing: true } );
 
 		// TODO: REDUX - remove flux actions when whole post-editor is reduxified
-		actions.trash( this.props.post, error => {
+		actions.trash( this.props.site, this.props.post, error => {
 			this.setState( { isTrashing: false } );
 
 			if ( this.props.onTrashingPost ) {
@@ -101,4 +103,6 @@ class EditorDeletePost extends React.Component {
 	}
 }
 
-export default localize( EditorDeletePost );
+export default connect( state => ( {
+	site: getSelectedSite( state ),
+} ) )( localize( EditorDeletePost ) );

client/post-editor/post-editor.jsx
@@ -629,7 +629,7 @@ export const PostEditor = createReactClass( {
 		}
 
 		// TODO: REDUX - remove flux actions when whole post-editor is reduxified
-		actions.autosave( callback );
+		actions.autosave( this.props.selectedSite, callback );
 
 		// If debounced / throttled autosave was pending, consider it flushed
 		this.throttledAutosave.cancel();
@@ -715,6 +715,7 @@ export const PostEditor = createReactClass( {
 
 		// TODO: REDUX - remove flux actions when whole post-editor is reduxified
 		actions.saveEdited(
+			this.props.selectedSite,
 			edits,
 			{ isConfirmationSidebarEnabled: this.props.isConfirmationSidebarEnabled },
 			function( error ) {
@@ -787,7 +788,7 @@ export const PostEditor = createReactClass( {
 		if ( status === 'publish' ) {
 			// TODO: REDUX - remove flux actions when whole post-editor is reduxified
 			actions.edit( { content: this.editor.getContent() } );
-			actions.autosave( previewPost );
+			actions.autosave( this.props.selectedSite, previewPost );
 		} else {
 			this.onSave( null, previewPost );
 		}
@@ -884,6 +885,7 @@ export const PostEditor = createReactClass( {
 		edits.content = this.editor.getContent();
 
 		actions.saveEdited(
+			this.props.selectedSite,
 			edits,
 			{ isConfirmationSidebarEnabled: this.props.isConfirmationSidebarEnabled },
 			function( error ) {

commit 2b7670718ed125ef78331d8ec845af1327e3e90c

Posts: Fix saving and publishing drafts copies (#20707)
Fix a regression introduced with #20328 and caused by not replacing `siteId` with `site` in the  `startEditingNew` call fired when creating a copy of a post.

client/post-editor/controller.js
@@ -130,9 +130,9 @@ function getPressThisContent( query ) {
 // - (Flux) startEditingNew: to set the editor content;
 // - (Redux) editPost: to set every other attribute (in particular, to update the Category Selector, terms can only be set via Redux);
 // - (Flux) edit: to reliably show the updated post attributes before (auto)saving.
-function startEditingPostCopy( siteId, postToCopyId, context ) {
+function startEditingPostCopy( site, postToCopyId, context ) {
 	wpcom
-		.site( siteId )
+		.site( site.ID )
 		.post( postToCopyId )
 		.get( { context: 'edit' } )
 		.then( postToCopy => {
@@ -166,12 +166,12 @@ function startEditingPostCopy( siteId, postToCopyId, context ) {
 				title: postAttributes.title,
 			};
 
-			actions.startEditingNew( siteId, {
+			actions.startEditingNew( site, {
 				content: postToCopy.content,
 				title: postToCopy.title,
 				type: postToCopy.type,
 			} );
-			context.store.dispatch( editPost( siteId, null, reduxPostAttributes ) );
+			context.store.dispatch( editPost( site.ID, null, reduxPostAttributes ) );
 			actions.edit( postAttributes );
 
 			/**
@@ -235,7 +235,7 @@ export default {
 			// so kick it off here to minimize time spent waiting for it to load
 			// in the view components
 			if ( postToCopyId ) {
-				startEditingPostCopy( siteId, postToCopyId, context );
+				startEditingPostCopy( site, postToCopyId, context );
 				analytics.pageView.record( '/' + postType, gaTitle + ' > New' );
 			} else if ( postID ) {
 				// TODO: REDUX - remove flux actions when whole post-editor is reduxified