43cb45296431f0809182a2f9705d86399909b575
AnnaMag <anna.mag.kedzierska@gmail.com>
2017-12-11 23:53:29 +0000

    Jetpack onboarding: set up initial Summary step structure in the flow
    - add document head
    - add follow-up redirect Button, its temporary redirect href
      and adapt its positioning.

--- client/jetpack-onboarding/steps/summary.jsx
+++ client/jetpack-onboarding/steps/summary.jsx
@@ -3,12 +3,14 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Fragment } from 'react';
 import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
+import Button from 'components/button';
+import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
 
 class JetpackOnboardingSummaryStep extends React.PureComponent {
@@ -19,7 +21,20 @@ class JetpackOnboardingSummaryStep extends React.PureComponent {
 			'You enabled Jetpack and unlocked dozens of website-bolstering features. Continue preparing your site below.'
 		);
 
-		return <FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />;
+		// TODO: adapt when we have more info
+		const buttonRedirectHref = '#';
+
+		return (
+			<Fragment>
+				<DocumentHead title={ translate( 'Summary ‹ Jetpack Onboarding' ) } />
+				<FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />
+				<div className="steps__button-group">
+					<Button href={ buttonRedirectHref } primary>
+						{ translate( 'Visit your site' ) }
+					</Button>
+				</div>
+			</Fragment>
+		);
 	}
 }
 
