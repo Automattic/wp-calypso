e61b63cf89c0f5efa81701a5161cb245ed1c2134
Jeff Bowen <jeff@automattic.com>
2017-12-11 16:19:06 -0500

    Preliminary wiring of revision for load button

--- client/state/selectors/get-post-revision.js
+++ client/state/selectors/get-post-revision.js
@@ -3,17 +3,19 @@
 /**
  * External dependencies
  */
-
-import { get } from 'lodash';
+import { find } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import createSelector from 'lib/create-selector';
+import { getPostRevisions } from 'state/selectors';
 
 const getPostRevision = createSelector(
-	( state, siteId, postId, revisionId ) =>
-		get( state.posts.revisions.diffs, [ siteId, postId, 'revisions', revisionId ], null ),
+	( state, siteId, postId, revisionId ) => {
+		const revisions = getPostRevisions( state, siteId, postId );
+		return find( revisions, { id: revisionId } ) || null;
+	},
 	state => [ state.posts.revisions.diffs ]
 );
 
