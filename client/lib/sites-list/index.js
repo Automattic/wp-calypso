805aec7594898c95e45c0a81b6c6f38c223e6fe1
Jarda Snajdr <jsnajdr@gmail.com>
2017-12-12 11:49:27 +0100

    Stop dispatching JETPACK_CONNECT_AUTHORIZE_RECEIVE_SITE_LIST to SitesList Flux store
    The action updates the sites list with the newly connected Jetpack site by calling
    `SitesList.sync()`. That's already covered in Redux by dispatching `SITES_RECEIVE` with
    the same data. We no longer need to update the Flux store.

--- client/lib/sites-list/index.js
+++ client/lib/sites-list/index.js
@@ -5,7 +5,6 @@
  */
 
 import { action as InvitesActionTypes } from 'lib/invites/constants';
-import { JETPACK_CONNECT_AUTHORIZE_RECEIVE_SITE_LIST } from 'state/action-types';
 import SitesList from './list';
 import PollerPool from 'lib/data-poller';
 import Dispatcher from 'dispatcher';
@@ -27,9 +26,6 @@ export default function() {
 					if ( [ 'follower', 'viewer' ].indexOf( action.invite.role ) === -1 ) {
 						_sites.sync( action.data );
 					}
-					break;
-				case JETPACK_CONNECT_AUTHORIZE_RECEIVE_SITE_LIST:
-					_sites.sync( action.data );
 					break;
 				case 'FETCH_SITES':
 					_sites.fetch(); // refetch the sites from .com
