6a8f3ea4ff4b79ae1f11693a8f1354dc57b1071a
Hew <github@mhs.fastmail.com>
2017-12-12 16:19:11 -0500

    Add UK ccTLDs to Calypso
    Props @deBhal, for all the heavy lifting. This feature remains gated server-side until we complete testing.

---
+++ client/components/domains/registrant-extra-info/test/uk-form.js
@@ -0,0 +1,114 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { shallow } from 'enzyme';
+import { identity } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { RegistrantExtraInfoUkForm } from '../uk-form';
+import FormInputValidation from 'components/forms/form-input-validation';
+
+const mockProps = {
+	step: 'uk',
+	translate: identity,
+	updateContactDetailsCache: identity,
+};
+
+describe( 'uk-form', () => {
+	describe( 'Validation Errors', () => {
+		test( 'should render the correct registation errors', () => {
+			const testProps = {
+				...mockProps,
+				contactDetails: { extra: { registrantType: 'LLP' } },
+				validationErrors: {
+					extra: {
+						registrationNumber: [ 'testErrorCode' ],
+					},
+				},
+			};
+
+			const wrapper = shallow( <RegistrantExtraInfoUkForm { ...testProps } /> );
+			const error = wrapper.find( FormInputValidation );
+			expect( error.props() ).toHaveProperty( 'text', 'There was a problem with this field.' );
+		} );
+
+		test( 'should render multiple registation errors', () => {
+			const testProps = {
+				...mockProps,
+				contactDetails: { extra: { registrantType: 'LLP' } },
+				validationErrors: {
+					extra: {
+						registrationNumber: [ 'testErrorCode', 'testErrorCode' ],
+						tradingName: [ 'testErrorCode' ],
+					},
+				},
+			};
+
+			const wrapper = shallow( <RegistrantExtraInfoUkForm { ...testProps } /> );
+			const error = wrapper.find( FormInputValidation );
+			expect( error ).toHaveProperty( 'length', 3 );
+		} );
+
+		test( 'should convert error code to user-facing string', () => {
+			const testProps = {
+				...mockProps,
+				contactDetails: { extra: { registrantType: 'LLP' } },
+				validationErrors: {
+					extra: {
+						registrationNumber: [ 'dotukRegistrationNumberFormat' ],
+					},
+				},
+			};
+
+			const wrapper = shallow( <RegistrantExtraInfoUkForm { ...testProps } /> );
+			const error = wrapper.find( FormInputValidation );
+			expect( error.props() ).toHaveProperty(
+				'text',
+				'A Company Registration Number is 8 numerals, or 2 letters followed by 6 numerals (e.g. AB123456 or 12345678).'
+			);
+		} );
+
+		test( 'Should disable submit button with validation errors', () => {
+			const testProps = {
+				...mockProps,
+				contactDetails: { extra: { registrantType: 'LLP' } },
+				validationErrors: {
+					extra: {
+						registrationNumber: [ 'dotukRegistrationNumberFormat' ],
+					},
+				},
+			};
+
+			const wrapper = shallow(
+				<RegistrantExtraInfoUkForm { ...testProps }>
+					<button className="test__hush-eslint .submit-button" />
+				</RegistrantExtraInfoUkForm>
+			);
+
+			expect( wrapper.find( 'button' ).prop( 'disabled' ) ).toBe( true );
+		} );
+
+		test( 'Should not disable submit button with irrelevant validation errors', () => {
+			const testProps = {
+				...mockProps,
+				contactDetails: { extra: { registrantType: 'IND' } },
+				validationErrors: {
+					extra: {
+						registrationNumber: [ 'dotukRegistrationNumberFormat' ],
+					},
+				},
+			};
+
+			const wrapper = shallow(
+				<RegistrantExtraInfoUkForm { ...testProps }>
+					<button className="test__hush-eslint .submit-button" />
+				</RegistrantExtraInfoUkForm>
+			);
+			expect( wrapper.find( 'button' ).prop( 'disabled' ) ).toBe( undefined );
+		} );
+	} );
+} );
---
+++ client/components/domains/registrant-extra-info/test/with-contact-details-validation.js
@@ -0,0 +1,172 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { shallow } from 'enzyme';
+import { difference, identity, set } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { ValidatedRegistrantExtraInfoUkForm } from '../uk-form';
+
+const mockProps = {
+	translate: identity,
+	updateContactDetailsCache: identity,
+};
+
+describe( 'uk-form validation', () => {
+	const validTradingName = 'A valid trading name';
+	const validRegistrationNumber = 'AB123456';
+
+	const allRegistrantTypes = [
+		'CRC',
+		'FCORP',
+		'FIND',
+		'FOTHER',
+		'GOV',
+		'IND',
+		'IP',
+		'LLP',
+		'LTD',
+		'OTHER',
+		'PLC',
+		'PTNR',
+		'RCHAR',
+		'SCH',
+		'STAT',
+		'STRA',
+	];
+
+	// see http://domains.opensrs.guide/docs/tld#section-uk
+	const needsRegistrationNumber = [ 'LTD', 'PLC', 'LLP', 'IP', 'SCH', 'RCHAR' ];
+
+	const needsTradingName = [
+		'LTD',
+		'PLC',
+		'LLP',
+		'IP',
+		'RCHAR',
+		'FCORP',
+		'OTHER',
+		'FOTHER',
+		'STRA',
+	];
+
+	describe( 'registrationNumber field', () => {
+		test( 'should be required for some values of registrantType', () => {
+			const testContactDetails = {
+				extra: {
+					registrantType: 'LLP',
+					tradingName: validTradingName,
+				},
+			};
+
+			needsRegistrationNumber.forEach( registrantType => {
+				const wrapper = shallow(
+					<ValidatedRegistrantExtraInfoUkForm
+						{ ...mockProps }
+						contactDetails={ set( testContactDetails, 'extra.registrantType', registrantType ) }
+					/>
+				);
+
+				expect( wrapper.props() ).toHaveProperty( 'validationErrors', {
+					extra: { registrationNumber: [ 'dotukRegistrantTypeRequiresRegistrationNumber' ] },
+				} );
+			} );
+		} );
+
+		test( 'should not be required for other values of registrantType', () => {
+			const testContactDetails = {
+				extra: {
+					registrantType: 'LLP',
+					tradingName: validTradingName,
+				},
+			};
+
+			difference( allRegistrantTypes, needsRegistrationNumber ).forEach( registrantType => {
+				const wrapper = shallow(
+					<ValidatedRegistrantExtraInfoUkForm
+						{ ...mockProps }
+						contactDetails={ set( testContactDetails, 'extra.registrantType', registrantType ) }
+					/>
+				);
+
+				expect( wrapper.props() ).toHaveProperty( 'validationErrors', {} );
+			} );
+		} );
+
+		test( 'should reject bad formats', () => {
+			const testContactDetails = {
+				extra: {
+					registrantType: 'LLP',
+					tradingName: validTradingName,
+				},
+			};
+
+			const badFormats = [ '124', 'OC38599', '066566879', 'OCABCDEF', '054025OC' ];
+
+			badFormats.forEach( registrationNumber => {
+				const wrapper = shallow(
+					<ValidatedRegistrantExtraInfoUkForm
+						{ ...mockProps }
+						contactDetails={ set(
+							testContactDetails,
+							'extra.registrationNumber',
+							registrationNumber
+						) }
+					/>
+				);
+
+				expect( wrapper.props() ).toHaveProperty( 'validationErrors.extra.registrationNumber' );
+			} );
+		} );
+	} );
+
+	describe( 'tradingName', () => {
+		test( 'should be required for some values of registrantType', () => {
+			const testContactDetails = {
+				extra: {
+					registrantType: 'LLP',
+					registrationNumber: validRegistrationNumber,
+					tradingName: '',
+				},
+			};
+
+			needsTradingName.forEach( registrantType => {
+				const wrapper = shallow(
+					<ValidatedRegistrantExtraInfoUkForm
+						{ ...mockProps }
+						contactDetails={ set( testContactDetails, 'extra.registrantType', registrantType ) }
+					/>
+				);
+
+				expect( wrapper.props() ).toHaveProperty( 'validationErrors', {
+					extra: { tradingName: [ 'dotukRegistrantTypeRequiresTradingName' ] },
+				} );
+			} );
+		} );
+
+		test( 'should not be required for other values of registrantType', () => {
+			difference( allRegistrantTypes, needsTradingName ).forEach( registrantType => {
+				const testContactDetails = {
+					extra: {
+						registrantType,
+						registrationNumber: validRegistrationNumber,
+						tradingName: '',
+					},
+				};
+
+				const wrapper = shallow(
+					<ValidatedRegistrantExtraInfoUkForm
+						{ ...mockProps }
+						contactDetails={ testContactDetails }
+					/>
+				);
+
+				expect( wrapper.props() ).toHaveProperty( 'validationErrors', {} );
+			} );
+		} );
+	} );
+} );
