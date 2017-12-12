22dd2d33ec827b4ac7cc1e99e836d54d4090148e
Marin Atanasov <tyxla@users.noreply.github.com>
2017-12-12 09:45:44 +0200

    Me: Update ApplicationPasswordsItem to use Redux analytics and React.Component (#20680)
    * Ме:Me: Update ApplicationPasswordsItem to use Redux analytics instead of eventRecorder
    
    * Me: Remove debug code in ApplicationPasswordsItem
    
    * Me: Refactor ApplicationPasswordsItem to use React.Component
    
    * Me: Cleanup bindActionCreators in ApplicationPasswordsItem
    
    * Me: Sort some imports in ApplicationPasswordsItem
    
    * Me: Use arrow functions in ApplicationPasswordsItem
    
    * Me: Use const instead of var in ApplicationPasswordsItem

--- client/me/application-password-item/index.jsx
+++ client/me/application-password-item/index.jsx
@@ -3,62 +3,46 @@
 /**
  * External dependencies
  */
-
 import React from 'react';
-import createReactClass from 'create-react-class';
-import { localize } from 'i18n-calypso';
-import debugFactory from 'debug';
-const debug = debugFactory( 'calypso:application-password-item' );
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
 import Gridicon from 'gridicons';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-import eventRecorder from 'me/event-recorder';
-import { errorNotice } from 'state/notices/actions';
 import Button from 'components/button';
+import { errorNotice } from 'state/notices/actions';
+import { recordGoogleEvent } from 'state/analytics/actions';
 
-const ApplicationPasswordsItem = createReactClass( {
-	displayName: 'ApplicationPasswordsItem',
+class ApplicationPasswordsItem extends React.Component {
+	state = {
+		removingPassword: false,
+	};
 
-	mixins: [ eventRecorder ],
+	handleRemovePasswordButtonClick = () => {
+		this.props.recordGoogleEvent( 'Me', 'Clicked on Remove Application Password Button' );
+		this.removeApplicationPassword();
+	};
 
-	componentDidMount: function() {
-		debug( this.displayName + ' React component is mounted.' );
-	},
-
-	componentWillUnmount: function() {
-		debug( this.displayName + ' React component is unmounting.' );
-	},
-
-	getInitialState: function() {
-		return {
-			removingPassword: false,
-		};
-	},
-
-	removeApplicationPassword: function() {
+	removeApplicationPassword() {
 		this.setState( { removingPassword: true } );
 
-		this.props.appPasswordsData.revoke(
-			parseInt( this.props.password.ID, 10 ),
-			function( error ) {
-				if ( error && 'unknown_application_password' !== error.error ) {
-					this.setState( { removingPassword: false } );
-					this.props.errorNotice(
-						this.props.translate(
-							'The application password was not successfully deleted. Please try again.'
-						)
-					);
-				}
-			}.bind( this )
-		);
-	},
+		this.props.appPasswordsData.revoke( parseInt( this.props.password.ID, 10 ), error => {
+			if ( error && 'unknown_application_password' !== error.error ) {
+				this.setState( { removingPassword: false } );
+				this.props.errorNotice(
+					this.props.translate(
+						'The application password was not successfully deleted. Please try again.'
+					)
+				);
+			}
+		} );
+	}
 
-	render: function() {
-		var password = this.props.password;
+	render() {
+		const password = this.props.password;
+
 		return (
 			<li className="application-password-item__password" key={ password.ID }>
 				<div className="application-password-item__details">
@@ -72,18 +56,16 @@ const ApplicationPasswordsItem = createReactClass( {
 				<Button
 					borderless
 					className="application-password-item__revoke"
-					onClick={ this.recordClickEvent(
-						'Remove Application Password Button',
-						this.removeApplicationPassword
-					) }
+					onClick={ this.handleRemovePasswordButtonClick }
 				>
 					<Gridicon icon="cross" />
 				</Button>
 			</li>
 		);
-	},
-} );
+	}
+}
 
-export default connect( null, dispatch => bindActionCreators( { errorNotice }, dispatch ) )(
-	localize( ApplicationPasswordsItem )
-);
+export default connect( null, {
+	errorNotice,
+	recordGoogleEvent,
+} )( localize( ApplicationPasswordsItem ) );
