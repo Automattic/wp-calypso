ff4910268b49c9c46aef41195fbc31fca588e5c8
Kirk Wight <kwight@kwight.ca>
2017-12-13 14:17:44 -0800

    Comments: Add selectors for `getSiteCommentCounts` with tests.

---
+++ client/state/selectors/test/get-site-comment-counts.js
@@ -0,0 +1,64 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+/**
+ * Internal dependencies
+ */
+import { getSiteCommentCounts } from 'state/selectors';
+
+describe( 'getSiteCommentCounts()', () => {
+	const siteId = 2916284;
+
+	test( 'should return site counts given a site ID', () => {
+		const state = {
+			comments: {
+				counts: {
+					[ siteId ]: {
+						site: {
+							all: 5,
+							approved: 2,
+							pending: 3,
+						},
+					},
+				},
+			},
+		};
+		const output = getSiteCommentCounts( state, siteId );
+		expect( output ).toEqual( {
+			all: 5,
+			approved: 2,
+			pending: 3,
+		} );
+	} );
+
+	test( 'should return site counts given a site ID and a post ID', () => {
+		const state = {
+			comments: {
+				counts: {
+					[ siteId ]: {
+						12345: {
+							all: 5,
+							approved: 2,
+							pending: 3,
+						},
+					},
+				},
+			},
+		};
+		const output = getSiteCommentCounts( state, siteId, 12345 );
+		expect( output ).toEqual( {
+			all: 5,
+			approved: 2,
+			pending: 3,
+		} );
+	} );
+
+	test( 'should return null when counts are not available', () => {
+		const state = {};
+		const output = getSiteCommentCounts( state, siteId, 12345 );
+		expect( output ).toEqual( null );
+	} );
+} );
