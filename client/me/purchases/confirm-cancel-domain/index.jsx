a30f57c516f5821b14814c0b6d6fa460223bfd6f
Andrija Vucinic <andrija.vucinic@gmail.com>
2017-12-13 20:26:44 +0000

    Purchases: Error when no purchases on ConfirmCancelDomain
    If purchases are empty, redirect doesn't fire before render
    and since there is no purchase an error is thrown before page()
    picks it up.

--- client/me/purchases/confirm-cancel-domain/index.jsx
+++ client/me/purchases/confirm-cancel-domain/index.jsx
@@ -267,9 +267,12 @@ class ConfirmCancelDomain extends React.Component {
 			);
 		}
 
-		const purchase = getPurchase( this.props ),
-			domain = getDomainName( purchase ),
-			selectedReason = this.state.selectedReason;
+		const purchase = getPurchase( this.props );
+		if ( ! purchase ) {
+			return null;
+		}
+		const domain = getDomainName( purchase );
+		const selectedReason = this.state.selectedReason;
 
 		return (
 			<Main className="confirm-cancel-domain">
