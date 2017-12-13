01304c1a899c15669d9729514de87c7202420b67
Matt Wondra <matt.wondra@automattic.com>
2017-12-13 13:34:24 -0600

    Remove id field from concierge data, since it will be removed from the API response

--- client/state/data-layer/wpcom/concierge/schedules/shifts/from-api.js
+++ client/state/data-layer/wpcom/concierge/schedules/shifts/from-api.js
@@ -7,7 +7,6 @@ import { makeParser } from 'state/data-layer/wpcom-http/utils';
 import responseSchema from './schema';
 
 export const transformShift = shift => ( {
-	id: shift.id,
 	beginTimestamp: shift.begin_timestamp,
 	endTimestamp: shift.end_timestamp,
 } );
