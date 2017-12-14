88894f942bcaf485df281c90306a0b7213cba4ed
Mark Ryall <mark@ryall.name>
2017-12-14 11:01:28 +1100

    move site-title-tour to checklist-site-title-tour

--- client/layout/guided-tours/tours/site-title-tour.js
+++ client/layout/guided-tours/tours/site-title-tour.js
@@ -15,60 +15,121 @@ import Gridicon from 'gridicons';
 import {
 	ButtonRow,
 	Continue,
+	Link,
 	makeTour,
 	Next,
-	SiteLink,
+	Quit,
 	Step,
 	Tour,
 } from 'layout/guided-tours/config-elements';
-import { canUserEditSettingsOfSelectedSite } from 'state/ui/guided-tours/contexts';
+import {
+	hasSelectedSiteDefaultSiteTitle,
+	isUserOlderThan,
+	isEnabled,
+	canUserEditSettingsOfSelectedSite,
+	isAbTestInVariant,
+} from 'state/ui/guided-tours/contexts';
+import { isDesktop } from 'lib/viewport';
+
+const TWO_DAYS_IN_MILLISECONDS = 2 * 1000 * 3600 * 24;
 
 export const SiteTitleTour = makeTour(
 	<Tour
 		name="siteTitle"
-		version="20171205"
-		path="/non-existent-route"
-		when={ and( canUserEditSettingsOfSelectedSite ) }
+		version="20161207"
+		path="/stats"
+		when={ and(
+			isEnabled( 'guided-tours/site-title' ),
+			isDesktop,
+			hasSelectedSiteDefaultSiteTitle,
+			canUserEditSettingsOfSelectedSite,
+			isUserOlderThan( TWO_DAYS_IN_MILLISECONDS ),
+			isAbTestInVariant( 'siteTitleTour', 'enabled' )
+		) }
 	>
-		<Step name="init" target="site-title-input" arrow="top-left" placement="below">
+		<Step name="init" placement="right" next="click-settings">
 			<p>
 				{ translate(
-					"Update the {{b}}Site Title{{/b}} field with a descriptive name to let your visitors know which site they're visiting.",
+					"Hey there! We noticed you haven't changed the title of your site yet. Want to change it?"
+				) }
+			</p>
+			<p>
+				{ translate(
+					'The site title appears in places like the top of your web browser and in search results.'
+				) }
+			</p>
+			<ButtonRow>
+				<Next step="click-settings">{ translate( 'Yes, please!' ) }</Next>
+				<Quit>{ translate( 'No, thanks.' ) }</Quit>
+			</ButtonRow>
+		</Step>
+
+		<Step
+			name="click-settings"
+			target="settings"
+			arrow="left-top"
+			placement="beside"
+			scrollContainer=".sidebar__region"
+			shouldScrollTo
+		>
+			<Continue target="settings" step="site-title-input" click>
+				{ translate( 'Click {{strong}}{{icon/}} Settings{{/strong}} to continue.', {
+					components: {
+						icon: <Gridicon icon="cog" />,
+						strong: <strong />,
+					},
+				} ) }
+			</Continue>
+		</Step>
+
+		<Step name="site-title-input" target="site-title-input" arrow="top-left" placement="below">
+			<p>
+				{ translate(
+					'You can change the site title here. A good title can help others find your site.'
+				) }
+			</p>
+			<ButtonRow>
+				<Next step="site-tagline-input">{ translate( 'Looks Good!' ) }</Next>
+				<Quit>{ translate( 'Cancel' ) }</Quit>
+			</ButtonRow>
+		</Step>
+
+		<Step name="site-tagline-input" target="site-tagline-input" arrow="top-left" placement="below">
+			<p>
+				{ translate(
+					"While you're at it, why not add a tagline? It should explain what your site is about in a few words. " +
+						'It usually appears right below your site title.'
+				) }
+			</p>
+			<ButtonRow>
+				<Next step="click-save">{ translate( 'Great!' ) }</Next>
+				<Quit>{ translate( 'Cancel' ) }</Quit>
+			</ButtonRow>
+		</Step>
+
+		<Step name="click-save" target="settings-site-profile-save" arrow="top-right" placement="below">
+			<Continue target="settings-site-profile-save" step="finish" click>
+				{ translate( "Don't forget to save your changes." ) }
+			</Continue>
+		</Step>
+
+		<Step name="finish" placement="center">
+			<p>
+				{ translate(
+					"{{strong}}That's it!{{/strong}} Your visitors can now easily identify your website by its title.",
 					{
-						components: { b: <strong /> },
+						components: {
+							strong: <strong />,
+						},
 					}
 				) }
 			</p>
 			<ButtonRow>
-				<Next step="click-save">{ translate( 'All done, continue' ) }</Next>
-				<SiteLink href="/checklist/:site">{ translate( 'Return to the checklist' ) }</SiteLink>
+				<Quit primary>{ translate( "We're all done!" ) }</Quit>
 			</ButtonRow>
-		</Step>
-
-		<Step name="click-save" target="settings-site-profile-save" arrow="top-left" placement="below">
-			<Continue target="settings-site-profile-save" step="finish" click>
-				{ translate(
-					'Almost done — every time you make a change, it needs to be saved. ' +
-						"Let's save your changes and then see what's next on our list."
-				) }
-			</Continue>
-		</Step>
-
-		<Step name="finish" placement="right">
-			<h1 className="tours__title">
-				<span className="tours__completed-icon-wrapper">
-					<Gridicon icon="checkmark" className="tours__completed-icon" />
-				</span>
-				{ translate( 'Good job, looks great!' ) }
-			</h1>
-			<p>
-				{ translate(
-					"Your changes have been saved. Let's move on and see what's next on our checklist."
-				) }
-			</p>
-			<SiteLink isButton="true" href={ '/checklist/:site' }>
-				{ translate( 'Return to the checklist' ) }
-			</SiteLink>
+			<Link href="https://en.support.wordpress.com/start">
+				{ translate( 'Learn more about WordPress.com' ) }
+			</Link>
 		</Step>
 	</Tour>
 );
