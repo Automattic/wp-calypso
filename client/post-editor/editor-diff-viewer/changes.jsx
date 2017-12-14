96de03f5cf8830efbb646d7a27a9a1a64df138b7
spen <spen_@hotmail.co.uk>
2017-12-14 12:34:06 +0000

    DiffViewer: Refactor existing changes file

--- client/post-editor/editor-diff-viewer/changes.jsx
+++ client/post-editor/editor-diff-viewer/changes.jsx
@@ -3,10 +3,10 @@
 /**
  * External dependencies
  */
-
 import React from 'react';
 import PropTypes from 'prop-types';
-import { isArray, isEmpty, map } from 'lodash';
+import classNames from 'classnames';
+import { isArray, isEmpty, map, partialRight } from 'lodash';
 
 const addLinesToChanges = changes => {
 	if ( ! isArray( changes ) || isEmpty( changes ) ) {
@@ -16,40 +16,35 @@ const addLinesToChanges = changes => {
 };
 
 const renderChange = ( change, changeIndex, splitLines ) => {
-	switch ( change.op ) {
-		case 'add': {
-			const content = change.final || change.value;
-			return (
-				<span className="editor-diff-viewer__additions" key={ changeIndex }>
-					{ splitLines ? addLinesToChanges( content ) : content }
-				</span>
-			);
-		}
-		case 'copy': {
-			const content = change.final || change.value;
-			return (
-				<span className="editor-diff-viewer__context" key={ changeIndex }>
-					{ splitLines ? addLinesToChanges( content ) : content }
-				</span>
-			);
-		}
-		case 'del': {
-			const content = change.orig || change.value;
-			return (
-				<span className="editor-diff-viewer__deletions" key={ changeIndex }>
-					{ splitLines ? addLinesToChanges( content ) : content }
-				</span>
-			);
-		}
-	}
+	const { orig, final, value, op } = change;
+	const content = orig || final || value;
+
+	const classnames = classNames( {
+		'editor-diff-viewer__additions': op === 'add',
+		'editor-diff-viewer__context': op === 'copy',
+		'editor-diff-viewer__deletions': op === 'del',
+	} );
+
+	return (
+		<span className={ classnames } key={ changeIndex }>
+			{ splitLines ? addLinesToChanges( content ) : content }
+		</span>
+	);
 };
 
-const EditorDiffChanges = ( { changes, splitLines } ) =>
-	map( changes, ( change, changeIndex ) => renderChange( change, changeIndex, splitLines ) );
+const renderSplitLineChange = partialRight( renderChange, true );
 
-EditorDiffChanges.propTypes = {
-	changes: PropTypes.array,
+const DiffChanges = ( { changes, splitLines } ) =>
+	map( changes, splitLines ? renderSplitLineChange : renderChange );
+
+DiffChanges.propTypes = {
+	changes: PropTypes.arrayOf(
+		PropTypes.shape( {
+			op: PropTypes.oneOf( [ 'add', 'copy', 'del' ] ),
+			value: PropTypes.string.isRequired,
+		} )
+	),
 	splitLines: PropTypes.bool,
 };
 
-export default EditorDiffChanges;
+export default DiffChanges;
