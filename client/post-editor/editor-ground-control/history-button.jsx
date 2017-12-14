05dafc2d4ba1b5c4ba2823ebb52a53475035cce2
Aaron Yan <aaronyan815@gmail.com>
2017-12-14 14:52:59 -0800

    created selectHistory to replace onClick

--- client/post-editor/editor-ground-control/history-button.jsx
+++ client/post-editor/editor-ground-control/history-button.jsx
@@ -12,22 +12,17 @@ import { flow } from 'lodash';
  * Internal dependencies
  */
 import { openPostRevisionsDialog } from 'state/posts/revisions/actions';
-import { recordTracksEvent } from 'state/analytics/actions';
+import { withAnalytics, recordTracksEvent } from 'state/analytics/actions';
 import EditorRevisionsDialog from 'post-editor/editor-revisions/dialog';
 
 class HistoryButton extends Component {
-	onClick = () => {
-		this.props.recordTracksEvent( 'calypso_editor_history_button_click' );
-		this.props.openDialog();
-	};
-
 	render() {
 		const { loadRevision, postId, siteId, translate } = this.props;
 		return (
 			<div className="editor-ground-control__history">
 				<button
 					className="editor-ground-control__history-button button is-link"
-					onClick={ this.onClick }
+					onClick={ this.props.selectHistory }
 				>
 					{ translate( 'History' ) }
 				</button>
@@ -46,10 +41,18 @@ HistoryButton.propTypes = {
 	translate: PropTypes.func,
 };
 
+const selectHistory = () => dispatch => {
+	dispatch(
+		withAnalytics(
+			recordTracksEvent( 'calypso_editor_history_button_click' ),
+			openPostRevisionsDialog()
+		)
+	);
+};
+
 export default flow(
 	localize,
 	connect( null, {
-		openDialog: openPostRevisionsDialog,
-		recordTracksEvent,
+		selectHistory,
 	} )
 )( HistoryButton );
