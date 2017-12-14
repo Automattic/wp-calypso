68a70810bb9150fd9357945dde06e8b090bc01e6
jonathansadowski <jonathansadowski@users.noreply.github.com>
2017-12-14 16:15:51 -0500

    PostItem: record mc stats for title and image clicks (#20831)

--- client/my-sites/post-type-list/post-thumbnail.jsx
+++ client/my-sites/post-type-list/post-thumbnail.jsx
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
-import { get } from 'lodash';
+import { get, noop } from 'lodash';
 
 /**
  * Internal dependencies
@@ -17,7 +17,7 @@ import resizeImageUrl from 'lib/resize-image-url';
 import safeImageUrl from 'lib/safe-image-url';
 import { getNormalizedPost } from 'state/posts/selectors';
 
-function PostTypeListPostThumbnail( { thumbnail } ) {
+function PostTypeListPostThumbnail( { onClick, thumbnail } ) {
 	const classes = classnames( 'post-type-list__post-thumbnail-wrapper', {
 		'has-image': !! thumbnail,
 	} );
@@ -28,6 +28,7 @@ function PostTypeListPostThumbnail( { thumbnail } ) {
 				<img
 					src={ resizeImageUrl( safeImageUrl( thumbnail ), { h: 80 } ) }
 					className="post-type-list__post-thumbnail"
+					onClick={ onClick }
 				/>
 			) }
 		</div>
@@ -36,7 +37,12 @@ function PostTypeListPostThumbnail( { thumbnail } ) {
 
 PostTypeListPostThumbnail.propTypes = {
 	globalId: PropTypes.string,
+	onClick: PropTypes.func,
 	thumbnail: PropTypes.string,
+};
+
+PostTypeListPostThumbnail.defaultProps = {
+	onClick: noop,
 };
 
 export default connect( ( state, ownProps ) => {
