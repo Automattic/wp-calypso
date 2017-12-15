b378b2a09c9d4e44622c5d904756d5abfd968fd2
Marin Atanasov <tyxla@abv.bg>
2017-12-15 14:58:12 +0200

    Jetpack Onboarding: Set onboarding site ID from siteSlug

--- client/jetpack-onboarding/controller.js
+++ client/jetpack-onboarding/controller.js
@@ -3,12 +3,15 @@
  * External Dependencies
  */
 import React from 'react';
+import { findKey } from 'lodash';
 
 /**
  * Internal Dependencies
  */
 import JetpackOnboardingMain from './main';
+import { setJetpackOnboardingSiteId } from 'state/ui/actions';
 import { setSection } from 'state/ui/actions';
+import { urlToSlug } from 'lib/url';
 
 const removeSidebar = context => {
 	context.store.dispatch( setSection( null, { hasSidebar: false } ) );
@@ -18,5 +21,21 @@ export const onboarding = ( context, next ) => {
 	removeSidebar( context );
 
 	context.primary = <JetpackOnboardingMain stepName={ context.params.stepName } />;
+	next();
+};
+
+export const siteSelection = ( context, next ) => {
+	const state = context.store.getState();
+
+	// TODO: move to a separate selector
+	const siteId = findKey( state.jetpackOnboarding.credentials, credentials => {
+		const siteSlug = urlToSlug( credentials.siteUrl );
+		return siteSlug === context.params.site;
+	} );
+
+	if ( siteId ) {
+		context.store.dispatch( setJetpackOnboardingSiteId( parseInt( siteId, 10 ) ) );
+	}
+
 	next();
 };
