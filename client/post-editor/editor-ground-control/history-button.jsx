6508049f525487b9aa6c3ffb7532cd0081fc3a05
Aaron Yan <aaronyan815@gmail.com>
2017-12-14 17:48:31 -0800

    Tightened up HistoryButton to be function component

--- client/post-editor/editor-ground-control/history-button.jsx
+++ client/post-editor/editor-ground-control/history-button.jsx
@@ -2,7 +2,7 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { localize } from 'i18n-calypso';
@@ -14,22 +14,17 @@ import { openPostRevisionsDialog } from 'state/posts/revisions/actions';
 import { withAnalytics, recordTracksEvent } from 'state/analytics/actions';
 import EditorRevisionsDialog from 'post-editor/editor-revisions/dialog';
 
-class HistoryButton extends Component {
-	render() {
-		const { loadRevision, postId, siteId, translate } = this.props;
-		return (
-			<div className="editor-ground-control__history">
-				<button
-					className="editor-ground-control__history-button button is-link"
-					onClick={ this.props.selectHistory }
-				>
-					{ translate( 'History' ) }
-				</button>
-				<EditorRevisionsDialog loadRevision={ loadRevision } postId={ postId } siteId={ siteId } />
-			</div>
-		);
-	}
-}
+const HistoryButton = ( { loadRevision, postId, siteId, translate, selectHistory } ) => (
+	<div className="editor-ground-control__history">
+		<button
+			className="editor-ground-control__history-button button is-link"
+			onClick={ selectHistory }
+		>
+			{ translate( 'History' ) }
+		</button>
+		<EditorRevisionsDialog loadRevision={ loadRevision } postId={ postId } siteId={ siteId } />
+	</div>
+);
 
 HistoryButton.propTypes = {
 	// localize
