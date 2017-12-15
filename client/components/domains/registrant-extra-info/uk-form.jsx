78a00857877a1895a81f31076df1f52d6750a182
Hew <github@mhs.fastmail.com>
2017-12-15 14:59:56 -0500

    Add dynamic trading name and registrant number fields

--- client/components/domains/registrant-extra-info/uk-form.jsx
+++ client/components/domains/registrant-extra-info/uk-form.jsx
@@ -8,17 +8,18 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import { camelCase, difference, isEmpty, keys, map, pick } from 'lodash';
+import { camelCase, difference, includes, isEmpty, keys, map, pick } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import { getContactDetailsExtraCache } from 'state/selectors';
+import { getContactDetailsCache } from 'state/selectors';
 import { getCurrentUserLocale } from 'state/current-user/selectors';
 import { updateContactDetailsCache } from 'state/domains/management/actions';
 import FormFieldset from 'components/forms/form-fieldset';
 import FormLabel from 'components/forms/form-label';
 import FormSelect from 'components/forms/form-select';
+import FormTextInput from 'components/forms/form-text-input';
 
 const defaultValues = {
 	registrantType: 'IND',
@@ -26,7 +27,7 @@ const defaultValues = {
 
 class RegistrantExtraInfoUkForm extends React.PureComponent {
 	static propTypes = {
-		contactDetailsExtra: PropTypes.object.isRequired,
+		contactDetails: PropTypes.object.isRequired,
 		translate: PropTypes.func.isRequired,
 	};
 
@@ -88,12 +89,72 @@ class RegistrantExtraInfoUkForm extends React.PureComponent {
 		} );
 	};
 
+	isTradingNameRequired( registrantType ) {
+		return includes(
+			[ 'LTD', 'PLC', 'LLP', 'IP', 'RCHAR', 'FCORP', 'OTHER', 'FOTHER', 'STRA' ],
+			registrantType
+		);
+	}
+
+	isRegistrationNumberRequired( registrantType ) {
+		return includes( [ 'LTD', 'PLC', 'LLP', 'IP', 'SCH', 'RCHAR' ], registrantType );
+	}
+
+	renderTradingNameField() {
+		const { contactDetails, translate } = this.props;
+		const { tradingName } = contactDetails.extra;
+		return (
+			<div>
+				<FormFieldset>
+					<FormLabel className="registrant-extra-info__trading-name" htmlFor="trading-name">
+						{ translate( 'Trading Name' ) }
+					</FormLabel>
+					<FormTextInput
+						id="trading-name"
+						value={ tradingName }
+						autoCapitalize="off"
+						autoComplete="off"
+						autoCorrect="off"
+						placeholder={ '' }
+						onChange={ this.handleChangeEvent }
+					/>
+				</FormFieldset>
+			</div>
+		);
+	}
+
+	renderRegistrationNumberField() {
+		const { contactDetails, translate } = this.props;
+		const { registrationNumber } = contactDetails.extra;
+		return (
+			<div>
+				<FormFieldset>
+					<FormLabel
+						className="registrant-extra-info__registration-number"
+						htmlFor="registration-number"
+					>
+						{ translate( 'Registration Number' ) }
+					</FormLabel>
+					<FormTextInput
+						id="registration-number"
+						value={ registrationNumber }
+						autoCapitalize="off"
+						autoComplete="off"
+						autoCorrect="off"
+						placeholder={ '' }
+						onChange={ this.handleChangeEvent }
+					/>
+				</FormFieldset>
+			</div>
+		);
+	}
+
 	render() {
 		const { translate } = this.props;
 		const { registrantTypeOptions } = this.state;
 		const { registrantType } = {
 			...defaultValues,
-			...this.props.contactDetailsExtra,
+			...this.props.contactDetails.extra,
 		};
 
 		return (
@@ -117,6 +178,11 @@ class RegistrantExtraInfoUkForm extends React.PureComponent {
 					</FormSelect>
 				</FormFieldset>
 
+				{ this.isTradingNameRequired( registrantType ) && this.renderTradingNameField() }
+
+				{ this.isRegistrationNumberRequired( registrantType ) &&
+					this.renderRegistrationNumberField() }
+
 				{ this.props.children }
 			</form>
 		);
@@ -125,7 +191,7 @@ class RegistrantExtraInfoUkForm extends React.PureComponent {
 
 export default connect(
 	state => ( {
-		contactDetailsExtra: getContactDetailsExtraCache( state ),
+		contactDetails: getContactDetailsCache( state ),
 		userWpcomLang: getCurrentUserLocale( state ),
 	} ),
 	{ updateContactDetailsCache }
