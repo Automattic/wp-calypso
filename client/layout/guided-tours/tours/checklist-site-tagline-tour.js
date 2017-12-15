d3b4dbd4b884ad3986c66dd2096d15518fa6178b
Mark Ryall <mark.ryall@a8c.com>
2017-12-15 17:53:55 +1100

    Add site tagline guided tour for checklist (#20767)
    * add simplified site title tour
    
    * change edit site title action to use guided tour
    
    * Use redux action to trigger guided tour from checklist
    
    * Show both tour and visit url for tasks.
    
    * Changes to site title tourfor new design.
    
    * Design cleanup
    
    * use non existant route to ensure tour is never displayed automatically
    
    * add site tagline tour
    
    * remove line missed in merge
    
    * revert all changes to site-title-tour
    
    * Change animation delay on site tagline tour.

---
+++ client/layout/guided-tours/tours/checklist-site-tagline-tour.js
@@ -0,0 +1,76 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React from 'react';
+import { translate } from 'i18n-calypso';
+import { noop } from 'lodash';
+import Gridicon from 'gridicons';
+
+/**
+ * Internal dependencies
+ */
+import {
+	ButtonRow,
+	Continue,
+	makeTour,
+	Next,
+	SiteLink,
+	Step,
+	Tour,
+} from 'layout/guided-tours/config-elements';
+
+export const ChecklistSiteTaglineTour = makeTour(
+	<Tour name="checklistSiteTagline" version="20171205" path="/non-existent-route" when={ noop }>
+		<Step
+			name="init"
+			target="site-tagline-input"
+			arrow="top-left"
+			placement="below"
+			style={ {
+				animationDelay: '0.7s',
+			} }
+		>
+			<p>
+				{ translate(
+					'First impressions last - a tagline tells visitors what your site is all about without ' +
+						'having to read all your content. Enter a short descriptive phrase about your site into ' +
+						'the {{b}}Site Tagline{{/b}} field.',
+					{ components: { b: <strong /> } }
+				) }
+			</p>
+			<ButtonRow>
+				<Next step="click-save">{ translate( 'All done, continue' ) }</Next>
+				<SiteLink href="/checklist/:site">{ translate( 'Return to the checklist' ) }</SiteLink>
+			</ButtonRow>
+		</Step>
+
+		<Step name="click-save" target="settings-site-profile-save" arrow="top-left" placement="below">
+			<Continue target="settings-site-profile-save" step="finish" click>
+				{ translate(
+					"Almost done — press {{b}}Save Settings{{/b}} to save your changes and then see what's next on our list.",
+					{ components: { b: <strong /> } }
+				) }
+			</Continue>
+		</Step>
+
+		<Step name="finish" placement="right">
+			<h1 className="tours__title">
+				<span className="tours__completed-icon-wrapper">
+					<Gridicon icon="checkmark" className="tours__completed-icon" />
+				</span>
+				{ translate( 'Way to go!' ) }
+			</h1>
+			<p>
+				{ translate(
+					"Your tagline has been saved! Let's move on and see what's next on our checklist."
+				) }
+			</p>
+			<SiteLink isButton="true" href={ '/checklist/:site' }>
+				{ translate( 'Return to the checklist' ) }
+			</SiteLink>
+		</Step>
+	</Tour>
+);
