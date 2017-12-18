cd11314c0dd3e01923da946f979ea6e1acb92730
Jake F - @samouri <samouri@users.noreply.github.com>
2017-12-17 19:57:55 -0500

    fix conflict issues

--- client/devdocs/docs-example/readme-viewer.jsx
+++ client/devdocs/docs-example/readme-viewer.jsx
@@ -1,8 +1,13 @@
+/** @format */
+
 /**
  * External Dependencies
  */
-import React, { Component, PropTypes } from 'react';
+
+import React, { Component } from 'react';
 import { Parser } from 'html-to-react';
+import PropTypes from 'prop-types';
+
 const htmlToReactParser = new Parser();
 
 /**
@@ -13,26 +18,27 @@ import FoldableCard from 'components/foldable-card';
 class ReadmeViewer extends Component {
 	static propTypes = {
 		getReadme: PropTypes.func,
-		readmeFilePath: PropTypes.string
+		readmeFilePath: PropTypes.string,
 	};
 
 	static defaultProps = {
 		getReadme: readmeFilePath => {
-			return htmlToReactParser.parse(
-				require( `../../components/${ readmeFilePath }/README.md` ) );
-		}
+			return htmlToReactParser.parse( require( `../../components/${ readmeFilePath }/README.md` ) );
+		},
 	};
 
 	render() {
 		const readmeFilePath = this.props.readmeFilePath;
 		const readme = readmeFilePath && this.props.getReadme( readmeFilePath );
-		const editLink = <div className="docs-example__readme-viewer-edit-link">
-			<a
-				href={ `https://github.com/Automattic/wp-calypso/edit/master/client/components/${ readmeFilePath }/README.md` }
-			>
-				Improve this document on GitHub
-			</a>
-		</div>;
+		const editLink = (
+			<div className="docs-example__readme-viewer-edit-link">
+				<a
+					href={ `https://github.com/Automattic/wp-calypso/edit/master/client/components/${ readmeFilePath }/README.md` }
+				>
+					Improve this document on GitHub
+				</a>
+			</div>
+		);
 		return (
 			<div className="docs-example__readme-viewer">
 				<hr className="docs-example__readme-viewer-hr" />
@@ -45,7 +51,11 @@ class ReadmeViewer extends Component {
 					disabled={ ! readme }
 				>
 					{ readme && editLink }
-					{ readme || <div className="docs-example__readme-viewer-not-available">README.md is not available.</div> }
+					{ readme || (
+						<div className="docs-example__readme-viewer-not-available">
+							README.md is not available.
+						</div>
+					) }
 				</FoldableCard>
 			</div>
 		);
