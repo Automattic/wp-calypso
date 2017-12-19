2a7d8fe070e826868581b85e42b5a21a17b79da4
James Nylen <jnylen@gmail.com>
2017-12-18 22:15:40 -0500

    Refactor `PostLikes` as a class

--- client/blocks/post-likes/index.jsx
+++ client/blocks/post-likes/index.jsx
@@ -3,10 +3,8 @@
 /**
  * External dependencies
  */
-
 import React from 'react';
 import { connect } from 'react-redux';
-import { flowRight } from 'lodash';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -17,73 +15,96 @@ import QueryPostLikes from 'components/data/query-post-likes';
 import { isRequestingPostLikes, getPostLikes, countPostLikes } from 'state/selectors';
 import { recordGoogleEvent } from 'state/analytics/actions';
 
-export const PostLikes = props => {
-	const { countLikes, isRequesting, likes, postId, postType, siteId, translate } = props;
-	const trackLikeClick = () => props.recordGoogleEvent( 'Post Likes', 'Clicked on Gravatar' );
-	const getLikeUrl = like => {
-		return like.URL ? like.URL : `https://gravatar.com/${ like.login }`;
+class PostLikes extends React.PureComponent {
+	static defaultProps = {
+		postType: 'post',
 	};
 
-	let noLikesLabel;
-
-	if ( postType === 'page' ) {
-		noLikesLabel = translate( 'There are no likes on this page yet.' );
-	} else {
-		noLikesLabel = translate( 'There are no likes on this post yet.' );
+	getLikeUrl = like => {
+		return like.URL ? like.URL : `https://gravatar.com/${ like.login }`;
 	}
 
-	return (
-		<div className="post-likes">
-			<QueryPostLikes siteId={ siteId } postId={ postId } />
-			{ ! likes &&
-				isRequesting && (
+	trackLikeClick = () => {
+		this.props.recordGoogleEvent( 'Post Likes', 'Clicked on Gravatar' );
+	}
+
+	renderLike = like => {
+		return (
+			<a
+				key={ like.ID }
+				href={ this.getLikeUrl( like ) }
+				rel="noopener noreferrer"
+				target="_blank"
+				className="post-likes__item"
+				title={ like.login }
+				onClick={ this.trackLikeClick }
+			>
+				<Gravatar user={ like } size={ 24 } />
+			</a>
+		);
+	}
+
+	renderExtraCount() {
+		const { likes, likeCount } = this.props;
+
+		if ( ! likes || likeCount <= likes.length ) {
+			return null;
+		}
+
+		return (
+			<span key="placeholder" className="post-likes__count">
+				{ `+ ${ likeCount - likes.length }` }
+			</span>
+		);
+	}
+
+	render() {
+		const {
+			likeCount,
+			isRequesting,
+			likes,
+			postId,
+			postType,
+			siteId,
+			translate,
+		} = this.props;
+
+		let noLikesLabel;
+
+		if ( postType === 'page' ) {
+			noLikesLabel = translate( 'There are no likes on this page yet.' );
+		} else {
+			noLikesLabel = translate( 'There are no likes on this post yet.' );
+		}
+
+		const isLoading = ! likes && isRequesting;
+
+		return (
+			<div className="post-likes">
+				<QueryPostLikes siteId={ siteId } postId={ postId } />
+				{ isLoading && (
 					<span key="placeholder" className="post-likes__count is-loading">
 						…
 					</span>
 				) }
-			{ likes &&
-				likes
-					.map( like => (
-						<a
-							key={ like.ID }
-							href={ getLikeUrl( like ) }
-							rel="noopener noreferrer"
-							target="_blank"
-							className="post-likes__item"
-							title={ like.login }
-							onClick={ trackLikeClick }
-						>
-							<Gravatar user={ like } size={ 24 } />
-						</a>
-					) )
-					.concat(
-						countLikes > likes.length && (
-							<span key="placeholder" className="post-likes__count">
-								{ `+ ${ countLikes - likes.length }` }
-							</span>
-						)
-					) }
-			{ countLikes === 0 && ! isRequesting && noLikesLabel }
-		</div>
-	);
-};
+				{ likes && likes.map( this.renderLike ) }
+				{ this.renderExtraCount() }
+				{ likeCount === 0 && ! isRequesting && noLikesLabel }
+			</div>
+		);
+	}
+}
 
-PostLikes.defaultProps = {
-	postType: 'post',
-};
-
-const connectComponent = connect(
+export default connect(
 	( state, { siteId, postId } ) => {
 		const isRequesting = isRequestingPostLikes( state, siteId, postId );
-		const countLikes = countPostLikes( state, siteId, postId );
+		const likeCount = countPostLikes( state, siteId, postId );
 		const likes = getPostLikes( state, siteId, postId );
 		return {
-			countLikes,
+			likeCount,
 			isRequesting,
 			likes,
 		};
 	},
 	{ recordGoogleEvent }
-);
-
-export default flowRight( connectComponent, localize )( PostLikes );
+)( localize( PostLikes ) );
