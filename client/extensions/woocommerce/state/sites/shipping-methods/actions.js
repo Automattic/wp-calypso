9e7e37d9e92fb5a1a48734aa029afc5d5d553448
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-12-19 23:52:23 +0000

    Fetch the WooCommerce Services shipping methods' schemas when the shipping methods are fetched.

--- client/extensions/woocommerce/state/sites/shipping-methods/actions.js
+++ client/extensions/woocommerce/state/sites/shipping-methods/actions.js
@@ -1,9 +1,12 @@
 /** @format */
+/**
+ * External dependencies
+ */
+import { startsWith } from 'lodash';
 
 /**
  * Internal dependencies
  */
-
 import request from 'woocommerce/state/sites/request';
 import { setError } from '../status/wc-api/actions';
 import {
@@ -11,6 +14,8 @@ import {
 	WOOCOMMERCE_SHIPPING_METHODS_REQUEST_SUCCESS,
 } from 'woocommerce/state/action-types';
 import { areShippingMethodsLoaded, areShippingMethodsLoading } from './selectors';
+import { isWcsEnabled } from 'woocommerce/state/selectors/plugins';
+import { fetchShippingSchema } from 'woocommerce/woocommerce-services/state/shipping-schemas/actions';
 
 export const fetchShippingMethodsSuccess = ( siteId, data ) => {
 	return {
@@ -39,6 +44,13 @@ export const fetchShippingMethods = siteId => ( dispatch, getState ) => {
 		.get( 'shipping_methods' )
 		.then( data => {
 			dispatch( fetchShippingMethodsSuccess( siteId, data ) );
+			return data;
+		} )
+		.then( data => {
+			const wcsMethods = isWcsEnabled( getState(), siteId )
+				? data.filter( ( { id } ) => startsWith( id, 'wc_services' ) )
+				: [];
+			wcsMethods.forEach( ( { id } ) => dispatch( fetchShippingSchema( siteId, id ) ) );
 		} )
 		.catch( err => {
 			dispatch( setError( siteId, getAction, err ) );
