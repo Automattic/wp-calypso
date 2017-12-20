7540749cd7aa06b2891a8d600eca739fd8cda036
Copons <copons@users.noreply.github.com>
2017-12-20 19:39:20 +0100

    Make all query properties optional

--- client/state/selectors/get-comments-page.js
+++ client/state/selectors/get-comments-page.js
@@ -11,10 +11,10 @@ import { get } from 'lodash';
  * @param {Object} state Redux state.
  * @param {Number} siteId Site identifier.
  * @param {Object} query Filter parameters.
- * @param {Number} query.page Requested page.
+ * @param {Number} [query.page] Requested page.
  * @param {Number} [query.postId] Post identifier.
  * @param {String} [query.search] Search query.
- * @param {String} query.status Comments status.
+ * @param {String} [query.status] Comments status.
  * @returns {Array} List of comment IDs for the requested page and filters.
  */
 export const getCommentsPage = ( state, siteId, { page = 1, postId, search, status = 'all' } ) => {
