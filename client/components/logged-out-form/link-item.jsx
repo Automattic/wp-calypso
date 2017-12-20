202b20a26380b75ea23d7e50d04bc78679ba8ad0
Jon Surrell <jon.surrell@automattic.com>
2017-12-20 15:36:30 +0100

    Component: Fix LoggedOutLinkFormItem props (#20987)
    * Add LoggedOutFormLinkItem tests
    
    * Use named function over class with displayName
    
    * Remove redundant omission
    
    This omission was never applied because `classNames` is not the expected
    prop. The way this component is structured, the `className` will be
    overwritten without requiring manual omission.

--- client/components/logged-out-form/link-item.jsx
+++ client/components/logged-out-form/link-item.jsx
@@ -3,27 +3,15 @@
 /**
  * External dependencies
  */
-
 import PropTypes from 'prop-types';
 import React from 'react';
 import classnames from 'classnames';
-import { omit } from 'lodash';
 
-export default class extends React.Component {
-	static displayName = 'LoggedOutFormLinkItem';
-
-	static propTypes = {
-		className: PropTypes.string,
-	};
-
-	render() {
-		return (
-			<a
-				{ ...omit( this.props, 'classNames' ) }
-				className={ classnames( 'logged-out-form__link-item', this.props.className ) }
-			>
-				{ this.props.children }
-			</a>
-		);
-	}
+export default function LoggedOutFormLinkItem( props ) {
+	return (
+		<a { ...props } className={ classnames( 'logged-out-form__link-item', props.className ) }>
+			{ props.children }
+		</a>
+	);
 }
+LoggedOutFormLinkItem.propTypes = { className: PropTypes.string };
