900f3f833fcdb6a3d67aebebb66c76815fab2c6c
James Nylen <jnylen@gmail.com>
2017-12-20 15:21:37 -0500

    Add site_ID to likes state

--- client/state/posts/likes/reducer.js
+++ client/state/posts/likes/reducer.js
@@ -66,7 +66,9 @@ export const items = createReducer(
 				[ siteId ]: {
 					...state[ siteId ],
 					[ postId ]: {
-						likes: likes.map( like => pick( like, 'ID', 'avatar_URL', 'URL', 'login', 'name' ) ),
+						likes: likes.map( like =>
+							pick( like, 'ID', 'avatar_URL', 'URL', 'login', 'name', 'site_ID' )
+						),
 						iLike,
 						found,
 					},
