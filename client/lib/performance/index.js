3ab4fb9602dfac3c8de627dec28d35b37e86da61
Jake F - @samouri <samouri@users.noreply.github.com>
2017-12-20 16:19:21 -0500

    Framework: HoC for measuring wasted rerenders

---
+++ client/lib/performance/index.js
@@ -0,0 +1,7 @@
+/** @format */
+/**
+ * Internal dependencies
+ */
+import measure from './measured-component';
+
+export { measure };
---
+++ client/lib/performance/measured-component.js
@@ -0,0 +1,60 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import * as React from 'react';
+import { isEqual } from 'lodash';
+
+const wastedRenders = {};
+window.wastedRenders = wastedRenders;
+
+function getDisplayName( WrappedComponent ) {
+	return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
+const RENDER_COUNT = 'renderCount';
+const WASTED_COUNT = 'wastedRenderCount';
+const BAD_PROPS = 'bad_props';
+
+const measuredComponent = Component => {
+	return class Measured extends React.Component {
+		displayName = getDisplayName( Component );
+
+		constructor( props ) {
+			super( props );
+			const stats = ( wastedRenders[ this.displayName ] = {} );
+
+			stats[ RENDER_COUNT ] = 0;
+			stats[ WASTED_COUNT ] = 0;
+			stats[ BAD_PROPS ] = {};
+		}
+
+		componentWillReceiveProps( nextProps ) {
+			const keys = Object.keys( nextProps );
+			const stats = wastedRenders[ this.displayName ];
+			const badProps = stats[ BAD_PROPS ];
+			let hasWaste = false;
+
+			keys.forEach( key => {
+				const deepEqual = isEqual( this.props[ key ], nextProps[ key ] );
+				const shallowEqual = this.props[ key ] === nextProps[ key ];
+				if ( ! shallowEqual && deepEqual ) {
+					badProps[ key ] = ( badProps[ key ] || 0 ) + 1;
+					hasWaste = true;
+				}
+			} );
+
+			if ( hasWaste ) {
+				stats[ WASTED_COUNT ] += 1;
+			}
+		}
+
+		render() {
+			const stats = wastedRenders[ this.displayName ];
+			stats[ RENDER_COUNT ] += 1;
+			return <Component { ...this.props } />;
+		}
+	};
+};
+
+export default measuredComponent;
