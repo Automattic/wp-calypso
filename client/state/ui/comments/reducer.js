8e7d1c8e63b11c64e1bef5f9dfcaf453fbd18dda
Copons <copons@users.noreply.github.com>
2017-12-20 13:04:37 +0100

    Create new actions and reducer

---
+++ client/state/ui/comments/reducer.js
@@ -0,0 +1,48 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { get, isUndefined, map } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { COMMENTS_PER_PAGE } from 'my-sites/comments/constants';
+import { COMMENTS_QUERY_UPDATE } from 'state/action-types';
+import { combineReducers, keyedReducer } from 'state/utils';
+
+const deepUpdateComments = ( state, comments, { offset = 0, postId, search, status = 'all' } ) => {
+	const parent = postId || 'site';
+	const filter = !! search ? `${ status }?s=${ search }` : status;
+	const pageNumber = offset / COMMENTS_PER_PAGE + 1;
+	const commentIds = map( comments, 'ID' );
+
+	const parentObject = get( state, parent, {} );
+	const filterObject = get( parentObject, filter, {} );
+
+	return {
+		...state,
+		[ parent ]: {
+			...parentObject,
+			[ status ]: {
+				...filterObject,
+				[ pageNumber ]: commentIds,
+			},
+		},
+	};
+};
+
+const queries = ( state = {}, action ) => {
+	if ( isUndefined( get( action, 'query.offset' ) ) ) {
+		return state;
+	}
+	const { comments, type, query } = action;
+	switch ( type ) {
+		case COMMENTS_QUERY_UPDATE:
+			return deepUpdateComments( state, comments, query );
+		default:
+			return state;
+	}
+};
+
+export default combineReducers( { queries: keyedReducer( 'siteId', queries ) } );
