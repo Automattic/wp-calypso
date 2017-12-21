0f69e72c8a8ae5afd13118663bf0480f7ce1b1cb
James Nylen <jnylen@gmail.com>
2017-12-21 14:22:14 -0500

    Fix state validation error for locale switching (#21020)
    * Failures to deserialize initial state should fail the tests
    
    * Fix state validation error for locale switching

--- client/state/test/persistence.js
+++ client/state/test/persistence.js
@@ -6,12 +6,15 @@ import { DESERIALIZE, SERIALIZE } from 'state/action-types';
 import { createReduxStore, reducer } from 'state';
 
 describe( 'persistence', () => {
-	test( 'initial state should serialize and deserialize without errors', () => {
-		const consoleSpy = jest.spyOn( global.console, 'error' ).mockImplementation( () => () => {} );
+	test( 'initial state should serialize and deserialize without errors or warnings', () => {
+		const consoleErrorSpy = jest.spyOn( global.console, 'error' ).mockImplementation( () => () => {} );
+		const consoleWarnSpy = jest.spyOn( global.console, 'warn' ).mockImplementation( () => () => {} );
+
 		const initialState = createReduxStore().getState();
 
 		reducer( reducer( initialState, { type: SERIALIZE } ), { type: DESERIALIZE } );
 
-		expect( consoleSpy.mock.calls ).toHaveLength( 0 );
+		expect( consoleErrorSpy ).not.toHaveBeenCalled();
+		expect( consoleWarnSpy ).not.toHaveBeenCalled();
 	} );
 } );
--- client/state/ui/language/schema.js
+++ client/state/ui/language/schema.js
@@ -1,4 +1,4 @@
 /** @format */
 export const localeSlugSchema = {
-	type: 'string',
+	anyOf: [ { type: 'string' }, { enum: [ false ] } ],
 };
