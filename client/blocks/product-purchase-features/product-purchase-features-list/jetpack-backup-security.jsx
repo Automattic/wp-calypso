7cc7229ca815eeeb5dc54ac68f3ec778c1999a80
Elio Rivero <eliorivero@gmail.com>
2017-12-21 13:46:09 -0300

    Reduced one rewindActive condition checking and combined into an object. Removed redundant var in connect. Eliminated var with typo.

--- client/blocks/product-purchase-features/product-purchase-features-list/jetpack-backup-security.jsx
+++ client/blocks/product-purchase-features/product-purchase-features-list/jetpack-backup-security.jsx
@@ -17,13 +17,15 @@ import { isRewindActive } from 'state/selectors';
 import { getSelectedSite, getSelectedSiteId } from 'state/ui/selectors';
 
 const JetpackBackupSecurity = ( { rewindActive, site, siteId, translate } ) => {
-	const securlty_url = rewindActive
-		? '/stats/activity/' + site.slug
-		: 'https://dashboard.vaultpress.com/';
-
-	const button_text = rewindActive
-		? translate( 'View Activity Log' )
-		: translate( 'Visit security dashboard' );
+	const buttonProps = rewindActive
+		? {
+				url: `/stats/activity/${ site.slug }`,
+				label: translate( 'View Activity Log' ),
+			}
+		: {
+				url: 'https://dashboard.vaultpress.com/',
+				label: translate( 'Visit security dashboard' ),
+			};
 	return (
 		<div className="product-purchase-features-list__item">
 			<QueryRewindStatus siteId={ siteId } />
@@ -33,20 +35,18 @@ const JetpackBackupSecurity = ( { rewindActive, site, siteId, translate } ) => {
 				description={ translate(
 					'Your site is being securely backed up and scanned with real-time sync.'
 				) }
-				buttonText={ button_text }
-				href={ securlty_url }
+				buttonText={ buttonProps.label }
+				href={ buttonProps.url }
 			/>
 		</div>
 	);
 };
 
 export default connect( state => {
-	const site = getSelectedSite( state );
 	const siteId = getSelectedSiteId( state );
-
 	return {
 		rewindActive: isRewindActive( state, siteId ),
-		site,
+		site: getSelectedSite( state ),
 		siteId,
 	};
 } )( localize( JetpackBackupSecurity ) );
