3a4b41bc2321388131cd69d6e399861234071705
Mark Ryall <mark@ryall.name>
2017-12-21 13:29:13 +1000

    Remove restriction on free to paid nudge according to user registration age

--- client/state/selectors/is-eligible-for-free-to-paid-upsell.js
+++ client/state/selectors/is-eligible-for-free-to-paid-upsell.js
@@ -4,36 +4,22 @@
  * Internal dependencies
  */
 
-import {
-	canCurrentUser,
-	isMappedDomainSite,
-	isSiteOnFreePlan,
-	isUserRegistrationDaysWithinRange,
-	isVipSite,
-} from 'state/selectors';
+import { canCurrentUser, isMappedDomainSite, isSiteOnFreePlan, isVipSite } from 'state/selectors';
 
 /**
  * Returns true if the current user is eligible to participate in the free to paid plan upsell for the site
  *
  * @param {Object} state Global state tree
  * @param {Number} siteId Site ID
- * @param {Object} moment Current moment for determination of elapsed days since registration
  * @return {?Boolean} True if the user can participate in the free to paid upsell
  */
-const isEligibleForFreeToPaidUpsell = ( state, siteId, moment ) => {
+const isEligibleForFreeToPaidUpsell = ( state, siteId ) => {
 	const userCanManageOptions = canCurrentUser( state, siteId, 'manage_options' );
 	const siteHasMappedDomain = isMappedDomainSite( state, siteId );
 	const siteIsOnFreePlan = isSiteOnFreePlan( state, siteId );
-	const registrationDaysIsWithinRange = isUserRegistrationDaysWithinRange( state, moment, 0, 180 );
 	const siteIsVipSite = isVipSite( state, siteId );
 
-	return (
-		userCanManageOptions &&
-		! siteHasMappedDomain &&
-		siteIsOnFreePlan &&
-		! siteIsVipSite &&
-		registrationDaysIsWithinRange
-	);
+	return userCanManageOptions && ! siteHasMappedDomain && siteIsOnFreePlan && ! siteIsVipSite;
 };
 
 export default isEligibleForFreeToPaidUpsell;
