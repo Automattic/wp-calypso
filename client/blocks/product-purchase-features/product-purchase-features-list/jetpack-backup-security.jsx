d349dea5b51ccf65df531fd46d92ae32bc67cd27
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-12-20 19:42:32 -0800

    Plans Page: Fix url for to take users to the activity log instead of vaultpress

--- client/blocks/product-purchase-features/product-purchase-features-list/jetpack-backup-security.jsx
+++ client/blocks/product-purchase-features/product-purchase-features-list/jetpack-backup-security.jsx
@@ -6,15 +6,23 @@
 
 import React from 'react';
 import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import PurchaseDetail from 'components/purchase-detail';
+import QueryRewindStatus from 'components/data/query-rewind-status';
+import { isRewindActive } from 'state/selectors';
+import { getSelectedSite, getSelectedSiteId } from 'state/ui/selectors';
 
-export default localize( ( { translate } ) => {
+const JetpackBackupSecurity = ( { rewindActive, site, siteId, translate } ) => {
+	const securlty_url = rewindActive
+		? '/stats/activity/' + site.slug
+		: 'https://dashboard.vaultpress.com/';
 	return (
 		<div className="product-purchase-features-list__item">
+			<QueryRewindStatus siteId={ siteId } />
 			<PurchaseDetail
 				icon="flag"
 				title={ translate( 'Site Security' ) }
@@ -22,8 +30,19 @@ export default localize( ( { translate } ) => {
 					'Your site is being securely backed up and scanned with real-time sync.'
 				) }
 				buttonText={ translate( 'Visit security dashboard' ) }
-				href="https://dashboard.vaultpress.com/"
+				href={ securlty_url }
 			/>
 		</div>
 	);
-} );
+};
+
+export default connect( state => {
+	const site = getSelectedSite( state );
+	const siteId = getSelectedSiteId( state );
+
+	return {
+		rewindActive: isRewindActive( state, siteId ),
+		site,
+		siteId,
+	};
+} )( localize( JetpackBackupSecurity ) );
