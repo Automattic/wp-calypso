40168ebe53beda5be782159441e06f7339e16f62
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-12-21 09:52:19 -0800

    Plans Page: Fix url for to take users to the activity log instead of VP Dashboard (#21023)
    * Plans Page: Fix url for to take users to the activity log instead of vaultpress
    * Update the button text
    * Update Visit to view since it is already translated else where
    * Reduced one rewindActive condition checking and combined into an object. Removed redundant var in connect. Eliminated var with typo.

--- client/blocks/product-purchase-features/product-purchase-features-list/jetpack-backup-security.jsx
+++ client/blocks/product-purchase-features/product-purchase-features-list/jetpack-backup-security.jsx
@@ -6,24 +6,47 @@
 
 import React from 'react';
 import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import PurchaseDetail from 'components/purchase-detail';
+import QueryRewindStatus from 'components/data/query-rewind-status';
+import { isRewindActive } from 'state/selectors';
+import { getSelectedSite, getSelectedSiteId } from 'state/ui/selectors';
 
-export default localize( ( { translate } ) => {
+const JetpackBackupSecurity = ( { rewindActive, site, siteId, translate } ) => {
+	const buttonProps = rewindActive
+		? {
+				url: `/stats/activity/${ site.slug }`,
+				label: translate( 'View Activity Log' ),
+			}
+		: {
+				url: 'https://dashboard.vaultpress.com/',
+				label: translate( 'Visit security dashboard' ),
+			};
 	return (
 		<div className="product-purchase-features-list__item">
+			<QueryRewindStatus siteId={ siteId } />
 			<PurchaseDetail
 				icon="flag"
 				title={ translate( 'Site Security' ) }
 				description={ translate(
 					'Your site is being securely backed up and scanned with real-time sync.'
 				) }
-				buttonText={ translate( 'Visit security dashboard' ) }
-				href="https://dashboard.vaultpress.com/"
+				buttonText={ buttonProps.label }
+				href={ buttonProps.url }
 			/>
 		</div>
 	);
-} );
+};
+
+export default connect( state => {
+	const siteId = getSelectedSiteId( state );
+	return {
+		rewindActive: isRewindActive( state, siteId ),
+		site: getSelectedSite( state ),
+		siteId,
+	};
+} )( localize( JetpackBackupSecurity ) );
