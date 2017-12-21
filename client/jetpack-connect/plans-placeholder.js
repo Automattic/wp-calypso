88f73a573c2f0db1d08d1283718876b1e5b9d330
Marin Atanasov <tyxla@users.noreply.github.com>
2017-12-21 15:42:47 +0200

    Jetpack Connect: Add more detailed placeholder for plans (#21031)
    * Jetpack Connect: Add more detailed placeholder for plans
    
    * Add a tbody wrapper
    
    * Add some spacing

--- client/jetpack-connect/plans-placeholder.js
+++ client/jetpack-connect/plans-placeholder.js
@@ -4,16 +4,16 @@
  * External dependencies
  */
 import React from 'react';
+import { times, random } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import Card from 'components/card';
 import Main from 'components/main';
 
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 const placeholderContent = (
-	<Main wideLayout>
+	<Main className="jetpack-connect__hide-plan-icons" wideLayout>
 		<div className="jetpack-connect__plans placeholder">
 			<header className="formatted-header">
 				<h1 className="formatted-header__title">
@@ -23,9 +23,49 @@ const placeholderContent = (
 					<span className="placeholder-text">Now pick a plan that's right for you.</span>
 				</p>
 			</header>
-			<Card>
-				<div className="jetpack-connect-plans-placeholder__plans-content" />
-			</Card>
+
+			<div className="plans-wrapper">
+				<div className="plan-features plan-features--signup">
+					<table className="plan-features__table">
+						<tbody>
+							<tr className="plan-features__row">
+								{ times( 3, cellKey => (
+									<td className="plan-features__table-item has-border-top" key={ cellKey }>
+										<div className="plan-features__header-wrapper">
+											<header className="plan-features__header">
+												<div className="plan-features__header-text">
+													<h4 className="plan-features__header-title">
+														<span className="placeholder-text">Premium</span>
+													</h4>
+													<span className="placeholder-text">Best for small businesses</span>
+												</div>
+											</header>
+											<div className="plan-features__pricing">
+												<span className="plan-price is-original placeholder-text">$108,00</span>
+												<span className="plan-price placeholder-text">$99,00</span>
+											</div>
+										</div>
+										<div className="plan-features__actions">
+											<div className="plan-features__actions-buttons">
+												<div className="placeholder-text">
+													Upgrade<br />Your Plan
+												</div>
+											</div>
+										</div>
+										{ times( 5, featureKey => (
+											<div className="plan-features__item" key={ featureKey }>
+												<span className="placeholder-text">
+													Test feature { random( 1, Number.MAX_SAFE_INTEGER ) }
+												</span>
+											</div>
+										) ) }
+									</td>
+								) ) }
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</div>
 	</Main>
 );
