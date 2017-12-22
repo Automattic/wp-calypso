dcde9293c2de373a8c515a2a29a7512b8c3b835f
AnnaMag <AnnaMag@users.noreply.github.com>
2017-12-22 12:23:55 +0000

    Jetpack Onboarding: add Summary site content (#20740)
    * Jetpack onboarding: set up initial Summary step structure in the flow
    
    - add document head
    - add follow-up redirect Button, its temporary redirect href
      and adapt its positioning.
    
    * JPO: Add Summary Site content (hardcoded)
    
    * Address review feedback
    
    mostly around styling.
    
    * Capitalize the title in one of the steps

--- client/jetpack-onboarding/steps/summary.jsx
+++ client/jetpack-onboarding/steps/summary.jsx
@@ -3,15 +3,56 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Fragment } from 'react';
+import Gridicon from 'gridicons';
 import { localize } from 'i18n-calypso';
+import { map } from 'lodash';
 
 /**
  * Internal dependencies
  */
+import Button from 'components/button';
+import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
 
 class JetpackOnboardingSummaryStep extends React.PureComponent {
+	renderCompleted = () => {
+		const { translate } = this.props;
+		const stepsCompleted = [
+			translate( 'Site Title & Description' ),
+			translate( 'Type of Site' ),
+			translate( 'Type of Homepage' ),
+			translate( 'Contact Us Form' ),
+			translate( 'Jetpack Connection' ),
+		];
+
+		return map( stepsCompleted, ( fieldLabel, fieldIndex ) => (
+			<div key={ fieldIndex } className="steps__summary-entry completed">
+				<Gridicon icon="checkmark" size={ 18 } />
+				{ fieldLabel }
+			</div>
+		) );
+	};
+
+	renderTodo = () => {
+		const { translate } = this.props;
+		const stepsTodo = [
+			translate( 'Choose a Theme' ),
+			translate( 'Add a Site Address' ),
+			translate( 'Add a Store' ),
+			translate( 'Start a Blog' ),
+		];
+
+		// TODO: adapt when we have more info + it will differ for different steps
+		const siteRedirectHref = '#';
+
+		return map( stepsTodo, ( fieldLabel, fieldIndex ) => (
+			<div key={ fieldIndex } className="steps__summary-entry todo">
+				<a href={ siteRedirectHref }>{ fieldLabel }</a>
+			</div>
+		) );
+	};
+
 	render() {
 		const { translate } = this.props;
 		const headerText = translate( 'Congratulations! Your site is on its way.' );
@@ -19,7 +60,31 @@ class JetpackOnboardingSummaryStep extends React.PureComponent {
 			'You enabled Jetpack and unlocked dozens of website-bolstering features. Continue preparing your site below.'
 		);
 
-		return <FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />;
+		// TODO: adapt when we have more info
+		const buttonRedirectHref = '#';
+
+		return (
+			<Fragment>
+				<DocumentHead title={ translate( 'Summary ‹ Jetpack Onboarding' ) } />
+				<FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />
+
+				<div className="steps__summary-columns">
+					<div className="steps__summary-column">
+						<h3 className="steps__summary-heading">{ translate( "Steps you've completed:" ) }</h3>
+						{ this.renderCompleted() }
+					</div>
+					<div className="steps__summary-column">
+						<h3 className="steps__summary-heading">{ translate( 'Continue your site setup:' ) }</h3>
+						{ this.renderTodo() }
+					</div>
+				</div>
+				<div className="steps__button-group">
+					<Button href={ buttonRedirectHref } primary>
+						{ translate( 'Visit your site' ) }
+					</Button>
+				</div>
+			</Fragment>
+		);
 	}
 }
 
