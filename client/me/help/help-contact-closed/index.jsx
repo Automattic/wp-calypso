a406d29b9e1f5e0d29a3b137c7335569e9e55efc
Matt Wondra <matt.wondra@automattic.com>
2017-12-22 11:11:56 -0600

    Update copy based on feedback

--- client/me/help/help-contact-closed/index.jsx
+++ client/me/help/help-contact-closed/index.jsx
@@ -24,30 +24,28 @@ const HelpContactClosed = ( { translate } ) => {
 	if ( currentDate >= newYear2018ClosureStartsAt ) {
 		closureHeading = translate( "Limited Support New Year's Day" );
 		closureMessage = translate(
-			"Live chat is closed today for the New Year's Day holiday. If you need to get in touch with us, " +
-				'you can submit a support request below and we will get to it as fast as we can. Live chat will ' +
-				'reopen on January 2nd. Thank you!'
+			"Live chat is closed today for the New Year's Day holiday. If you need to get in touch with us, submit " +
+				"a support request below and we'll respond by email. Live chat will reopen on January 2nd. Thank you!"
 		);
 	} else if ( currentDate >= newYear2018NoticeStartsAt ) {
 		closureHeading = translate( "Limited Support New Year's Day" );
 		closureMessage = translate(
 			"Live chat will be closed on Monday, January 1, 2018 for the New Year's Day holiday. If you need " +
-				'to get in touch with us, you’ll be able to submit a support request from this page and we will ' +
-				'get to it as fast as we can. Live chat will reopen on January 2nd. Thank you!'
+				"to get in touch with us, you’ll be able to submit a support request from this page and we'll " +
+				'respond by email. Live chat will reopen on January 2nd. Thank you!'
 		);
 	} else if ( currentDate >= christmas2017ClosureStartsAt ) {
 		closureHeading = translate( 'Limited Support over Christmas' );
 		closureMessage = translate(
-			'Live chat is closed today for the Christmas holiday. If you need to get in touch with us, ' +
-				'you can submit a support request below and we will get to it as fast as we can. Live chat will ' +
-				'reopen on December 26. Thank you!'
+			'Live chat is closed today for the Christmas holiday. If you need to get in touch with us, submit ' +
+				"a support request below and we'll respond by email. Live chat will reopen on December 26. Thank you!"
 		);
 	} else {
 		closureHeading = translate( 'Limited Support over Christmas' );
 		closureMessage = translate(
 			'Live chat will be closed on Sunday, December 24th and Monday, December 25th for the Christmas holiday. ' +
 				'If you need to get in touch with us, you’ll be able to submit a support request from this page and ' +
-				'we will get to it as fast as we can. Live chat will reopen on December 26th. Thank you!'
+				"we'll respond by email. Live chat will reopen on December 26th. Thank you!"
 		);
 	}
 
