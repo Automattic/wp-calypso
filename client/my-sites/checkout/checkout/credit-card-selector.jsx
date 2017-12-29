bc3f3f1fc6e0eb78898c75e660399c552909f2cc
Dennis Snell <dennis.snell@automattic.com>
2017-12-29 13:29:58 -0700

    esmodules: Upgrade lib/store-transactions
    @see: https://github.com/Automattic/wp-calypso/milestone/224
    
    Previously we were exporting a default object of multiple methods and
    importing those through the non-spec-compliant Babel destructuring
    import statements.
    
    This patch makes those methods proper named exports in the work of
    turning off CommonJS compilation.

--- client/my-sites/checkout/checkout/credit-card-selector.jsx
+++ client/my-sites/checkout/checkout/credit-card-selector.jsx
@@ -14,7 +14,7 @@ import { find } from 'lodash';
 import analytics from 'lib/analytics';
 import StoredCard from './stored-card';
 import NewCardForm from './new-card-form';
-import storeTransactions from 'lib/store-transactions';
+import { newCardPayment, storedCardPayment } from 'lib/store-transactions';
 import upgradesActions from 'lib/upgrades/actions';
 
 class CreditCardSelector extends React.Component {
@@ -83,9 +83,9 @@ class CreditCardSelector extends React.Component {
 
 		if ( 'new-card' === section ) {
 			analytics.ga.recordEvent( 'Upgrades', 'Clicked Use a New Credit/Debit Card Link' );
-			newPayment = storeTransactions.newCardPayment( this.props.transaction.newCardFormFields );
+			newPayment = newCardPayment( this.props.transaction.newCardFormFields );
 		} else {
-			newPayment = storeTransactions.storedCardPayment( this.getStoredCardDetails( section ) );
+			newPayment = storedCardPayment( this.getStoredCardDetails( section ) );
 		}
 
 		upgradesActions.setPayment( newPayment );
