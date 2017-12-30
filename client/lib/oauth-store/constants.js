ce2b6e1c0ada6191a1411a60576d70b5bd5f1c57
Dennis Snell <dennis.snell@automattic.com>
2017-12-29 18:17:22 -0800

    esmodules: Update lib/oauth-store/constants
    @see: https://github.com/Automattic/wp-calypso/milestone/224
    
    Previously we were exporting a default object of multiple methods and
    importing those through the non-spec-compliant Babel destructuring
    import statements.
    
    This patch makes those methods proper named exports in the work of
    turning off CommonJS compilation.

--- client/lib/oauth-store/constants.js
+++ client/lib/oauth-store/constants.js
@@ -1,19 +1,12 @@
 /** @format */
 
-/**
- * External dependencies
- */
+export const actions = {
+	AUTH_LOGIN: 'AUTH_LOGIN',
+	RECEIVE_AUTH_LOGIN: 'RECEIVE_AUTH_LOGIN',
+	AUTH_RESET: 'AUTH_RESET',
+};
 
-import keyMirror from 'key-mirror';
-
-export default {
-	actions: keyMirror( {
-		AUTH_LOGIN: null,
-		RECEIVE_AUTH_LOGIN: null,
-		AUTH_RESET: null,
-	} ),
-	errors: {
-		ERROR_REQUIRES_2FA: 'needs_2fa', // From WP.com API
-		ERROR_INVALID_OTP: 'invalid_otp', // From WP.com API
-	},
+export const errors = {
+	ERROR_REQUIRES_2FA: 'needs_2fa', // From WP.com API
+	ERROR_INVALID_OTP: 'invalid_otp', // From WP.com API
 };
