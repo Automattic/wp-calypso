3bef3efa895023ef9d09adf5da46bcf27f5f5269
Dennis Snell <dennis.snell@automattic.com>
2018-01-02 18:19:10 -0700

    esmodules: Update lib/credit-card-details (#21173)
    * esmodules: Update lib/credit-card-details
    
    @see: https://github.com/Automattic/wp-calypso/milestone/224
    
    Previously we were exporting a default object of multiple methods and
    importing those through the non-spec-compliant Babel destructuring
    import statements.
    
    This patch makes those methods proper named exports in the work of
    turning off CommonJS compilation.
    
    * use `../` over `../index`

--- client/lib/credit-card-details/index.js
+++ client/lib/credit-card-details/index.js
@@ -1,15 +1,6 @@
 /** @format */
-
 /**
  * Internal dependencies
  */
-
-import masking from './masking';
-import validation from './validation';
-
-export default {
-	getCreditCardType: validation.getCreditCardType,
-	maskField: masking.maskField,
-	unmaskField: masking.unmaskField,
-	validateCardDetails: validation.validateCardDetails,
-};
+export { maskField, unmaskField } from './masking';
+export { getCreditCardType, validateCardDetails } from './validation';
