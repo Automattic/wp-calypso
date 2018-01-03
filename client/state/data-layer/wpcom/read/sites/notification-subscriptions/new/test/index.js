94507bdc7dfe760d8642189748f30e19fc51a2f7
bluefuton <chris@bluefuton.com>
2018-01-03 18:36:29 +0000

    Add test for requestNotificationSubscription

--- client/state/data-layer/wpcom/read/sites/notification-subscriptions/new/test/index.js
+++ client/state/data-layer/wpcom/read/sites/notification-subscriptions/new/test/index.js
@@ -1,104 +1,27 @@
 /** @format */
 
 /**
- * External dependencies
- */
-import { merge } from 'lodash';
-
-/**
  * Internal dependencies
  */
-import { requestConversationFollow, receiveConversationFollow } from '../';
-import { bypassDataLayer } from 'state/data-layer/utils';
+import { requestNotificationSubscription } from '../';
+//import { bypassDataLayer } from 'state/data-layer/utils';
 import { http } from 'state/data-layer/wpcom-http/actions';
-import {
-	followConversation,
-	updateConversationFollowStatus,
-} from 'state/reader/conversations/actions';
-import { CONVERSATION_FOLLOW_STATUS } from 'state/reader/conversations/follow-status';
+import { subscribeToNewPostNotifications } from 'state/reader/follows/actions';
 
-describe( 'conversation-follow', () => {
-	describe( 'requestConversationFollow', () => {
+describe( 'notification-subscriptions-new', () => {
+	describe( 'requestNotificationSubscription', () => {
 		test( 'should dispatch an http request', () => {
-			const dispatch = jest.fn();
-			const action = followConversation( { siteId: 123, postId: 456 } );
-			const actionWithRevert = merge( {}, action, {
-				meta: {
-					previousState: CONVERSATION_FOLLOW_STATUS.muting,
-				},
-			} );
-			const getState = () => {
-				return {
-					reader: {
-						conversations: {
-							items: {
-								'123-456': 'M',
-							},
-						},
-					},
-				};
-			};
-			requestConversationFollow( { dispatch, getState }, action );
-			expect( dispatch ).toHaveBeenCalledWith(
+			const blogId = 123;
+			const action = subscribeToNewPostNotifications( blogId );
+
+			expect( requestNotificationSubscription( action ) ).toEqual(
 				http(
 					{
-						method: 'POST',
-						path: '/read/sites/123/posts/456/follow',
-						body: {},
 						apiNamespace: 'wpcom/v2',
+						method: 'POST',
+						path: `/read/sites/${ blogId }/notification-subscriptions/new`,
 					},
-					actionWithRevert
-				)
-			);
-		} );
-	} );
-
-	describe( 'receiveConversationFollow', () => {
-		test( 'should dispatch a success notice', () => {
-			const dispatch = jest.fn();
-			receiveConversationFollow(
-				{ dispatch },
-				{
-					payload: { siteId: 123, postId: 456 },
-					meta: { previousState: CONVERSATION_FOLLOW_STATUS.muting },
-				},
-				{ success: true }
-			);
-			expect( dispatch ).toHaveBeenCalledWith(
-				expect.objectContaining( {
-					notice: expect.objectContaining( {
-						status: 'is-success',
-					} ),
-				} )
-			);
-		} );
-
-		test( 'should revert to the previous follow state if it fails', () => {
-			const dispatch = jest.fn();
-			receiveConversationFollow(
-				{ dispatch },
-				{
-					payload: { siteId: 123, postId: 456 },
-					meta: { previousState: CONVERSATION_FOLLOW_STATUS.muting },
-				},
-				{
-					success: false,
-				}
-			);
-			expect( dispatch ).toHaveBeenCalledWith(
-				expect.objectContaining( {
-					notice: expect.objectContaining( {
-						status: 'is-error',
-					} ),
-				} )
-			);
-			expect( dispatch ).toHaveBeenCalledWith(
-				bypassDataLayer(
-					updateConversationFollowStatus( {
-						siteId: 123,
-						postId: 456,
-						followStatus: CONVERSATION_FOLLOW_STATUS.muting,
-					} )
+					action
 				)
 			);
 		} );
