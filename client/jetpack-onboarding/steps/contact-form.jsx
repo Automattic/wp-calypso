8e87bb62ba91157560b341572449d89b0308094d
Marin Atanasov <tyxla@users.noreply.github.com>
2018-01-03 16:45:49 +0200

    Jetpack Onboarding: Make contact form step create a form (#21065)

--- client/jetpack-onboarding/steps/contact-form.jsx
+++ client/jetpack-onboarding/steps/contact-form.jsx
@@ -17,14 +17,21 @@ import Tile from 'components/tile-grid/tile';
 import TileGrid from 'components/tile-grid';
 import { JETPACK_ONBOARDING_STEPS as STEPS } from '../constants';
 import { recordTracksEvent } from 'state/analytics/actions';
+import { saveJetpackOnboardingSettings } from 'state/jetpack-onboarding/actions';
 
 class JetpackOnboardingContactFormStep extends React.PureComponent {
-	clickAddContactForm = () => {
+	handleAddContactForm = () => {
+		const { siteId } = this.props;
+
 		this.props.recordTracksEvent( 'calypso_jpo_contact_form_clicked' );
+
+		this.props.saveJetpackOnboardingSettings( siteId, {
+			addContactForm: true,
+		} );
 	};
 
 	render() {
-		const { translate } = this.props;
+		const { getForwardUrl, translate } = this.props;
 		const headerText = translate( "Let's shape your new site." );
 		const subHeaderText = translate( 'Would you like to get started with a Contact Us page?' );
 
@@ -45,7 +52,8 @@ class JetpackOnboardingContactFormStep extends React.PureComponent {
 							'Not sure? You can skip this step and add a contact form later.'
 						) }
 						image={ '/calypso/images/illustrations/contact-us.svg' }
-						onClick={ this.clickAddContactForm }
+						onClick={ this.handleAddContactForm }
+						href={ getForwardUrl() }
 					/>
 				</TileGrid>
 			</div>
@@ -53,6 +61,6 @@ class JetpackOnboardingContactFormStep extends React.PureComponent {
 	}
 }
 
-export default connect( null, { recordTracksEvent } )(
+export default connect( null, { recordTracksEvent, saveJetpackOnboardingSettings } )(
 	localize( JetpackOnboardingContactFormStep )
 );
