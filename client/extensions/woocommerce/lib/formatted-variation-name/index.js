75d1d2b685a45cf45b603f8ae2f0bd20d1fc1a2b
Timmy Crawford <timmyc@users.noreply.github.com>
2018-01-02 16:39:14 -0800

    Store: Refactor formatted-variation-name safegaurd for corrupt variant attributes. (#21086)

--- client/extensions/woocommerce/lib/formatted-variation-name/index.js
+++ client/extensions/woocommerce/lib/formatted-variation-name/index.js
@@ -1,20 +1,19 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { map } from 'lodash';
+
 /**
  * Returns a formatted variation name for display based on attributes.
  * 
  *
- * @format
+ * @param {Object} variant the product variant option which includes the attributes key.
  * @param {String} fallbackName Fallback name to use if no attributes are passed.
  * @return {String} Formatted variation name.
  */
 
 export default function formattedVariationName( { attributes }, fallbackName = '' ) {
-	return (
-		( Array.isArray( attributes ) &&
-			attributes
-				.map( function( attribute ) {
-					return attribute.option;
-				} )
-				.join( ' - ' ) ) ||
-		fallbackName
-	);
+	return map( attributes, 'option' ).join( ' - ' ) || fallbackName;
 }
