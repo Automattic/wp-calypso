ae97eab0e3394d8d2b74f4dc66d7242dec5354e1
Kelly Dwan <ryelle@users.noreply.github.com>
2018-01-04 12:53:46 -0500

    Move styles to the component itself, and only apply reset button styles if there is a reset button

--- client/extensions/woocommerce/components/price-input/index.js
+++ client/extensions/woocommerce/components/price-input/index.js
@@ -6,6 +6,7 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { connect } from 'react-redux';
 import Gridicon from 'gridicons';
 import { localize } from 'i18n-calypso';
@@ -84,8 +85,10 @@ class PriceInput extends Component {
 				</Button>
 			);
 		}
+
+		const classes = classNames( 'price-input', { 'has-reset': !! initialValue } );
 		return (
-			<div className="price-input">
+			<div className={ classes }>
 				<QuerySettingsGeneral siteId={ siteId } />
 				{ currencyObject ? (
 					<FormCurrencyInput
