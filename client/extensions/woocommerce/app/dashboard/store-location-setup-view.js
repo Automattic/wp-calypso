8a84cd38668942a551fd6199496e9ae3d621f5c2
Allen Snook <allen@allendav.com>
2018-01-04 13:49:46 -0800

    Add email verify notices and VerifyEmailDialog to set store location view

--- client/extensions/woocommerce/app/dashboard/store-location-setup-view.js
+++ client/extensions/woocommerce/app/dashboard/store-location-setup-view.js
@@ -23,12 +23,15 @@ import BasicWidget from 'woocommerce/components/basic-widget';
 import { errorNotice } from 'state/notices/actions';
 import { getContactDetailsCache } from 'state/selectors';
 import { getCountryData, getCountries } from 'woocommerce/lib/countries';
+import { isCurrentUserEmailVerified } from 'state/current-user/selectors';
 import { setSetStoreAddressDuringInitialSetup } from 'woocommerce/state/sites/setup-choices/actions';
 import SetupFooter from './setup-footer';
 import SetupHeader from './setup-header';
+import SetupNotices from './setup-notices';
 import { doInitialSetup } from 'woocommerce/state/sites/settings/actions';
 import QueryContactDetailsCache from 'components/data/query-contact-details-cache';
 import QuerySettingsGeneral from 'woocommerce/components/query-settings-general';
+import VerifyEmailDialog from 'components/email-verification/email-verification-dialog';
 
 class StoreLocationSetupView extends Component {
 	constructor( props ) {
@@ -36,6 +39,7 @@ class StoreLocationSetupView extends Component {
 		this.state = {
 			address: {},
 			isSaving: false,
+			showEmailVerificationDialog: false,
 			userBeganEditing: false,
 		};
 	}
@@ -123,8 +127,14 @@ class StoreLocationSetupView extends Component {
 	};
 
 	onNext = event => {
-		const { siteId, translate } = this.props;
+		const { currentUserEmailVerified, siteId, translate } = this.props;
 		event.preventDefault();
+
+		if ( ! currentUserEmailVerified ) {
+			this.setState( { showEmailVerificationDialog: true } );
+			return;
+		}
+
 		this.setState( { isSaving: true } );
 
 		// TODO before attempting to set the address, make sure all required
@@ -211,20 +221,30 @@ class StoreLocationSetupView extends Component {
 		);
 	};
 
+	closeVerifyEmailDialog = () => {
+		this.setState( { showEmailVerificationDialog: false } );
+	};
+
 	render = () => {
 		const { siteId, translate } = this.props;
 
 		return (
-			<div className="card dashboard__setup-wrapper dashboard__location">
-				<SetupHeader
-					imageSource={ '/calypso/images/extensions/woocommerce/woocommerce-setup.svg' }
-					imageWidth={ 160 }
-					title={ translate( 'Howdy! Ready to start selling?' ) }
-					subtitle={ translate( 'First we need to know where you are in the world.' ) }
-				/>
-				{ this.renderForm() }
-				<QuerySettingsGeneral siteId={ siteId } />
-				<QueryContactDetailsCache />
+			<div className="dashboard__setup-wrapper">
+				<SetupNotices />
+				{ this.state.showEmailVerificationDialog && (
+					<VerifyEmailDialog onClose={ this.closeVerifyEmailDialog } />
+				) }
+				<div className="card dashboard__location">
+					<SetupHeader
+						imageSource={ '/calypso/images/extensions/woocommerce/woocommerce-setup.svg' }
+						imageWidth={ 160 }
+						title={ translate( 'Howdy! Ready to start selling?' ) }
+						subtitle={ translate( 'First we need to know where you are in the world.' ) }
+					/>
+					{ this.renderForm() }
+					<QuerySettingsGeneral siteId={ siteId } />
+					<QueryContactDetailsCache />
+				</div>
 			</div>
 		);
 	};
@@ -234,11 +254,13 @@ function mapStateToProps( state, ownProps ) {
 	const { siteId } = ownProps;
 
 	const contactDetails = getContactDetailsCache( state );
+	const currentUserEmailVerified = isCurrentUserEmailVerified( state );
 	const settingsGeneralLoaded = areSettingsGeneralLoaded( state, siteId );
 	const storeLocation = getStoreLocation( state, siteId );
 
 	return {
 		contactDetails,
+		currentUserEmailVerified,
 		settingsGeneralLoaded,
 		storeLocation,
 	};
