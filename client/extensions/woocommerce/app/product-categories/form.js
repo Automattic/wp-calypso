5c7b1c9af8d2b6144c14ddbc4e081ef521492c35
Justin Shreve <justin.shreve@gmail.com>
2018-01-05 09:28:33 -0800

    Add TinyMCE powered description field

--- client/extensions/woocommerce/app/product-categories/form.js
+++ client/extensions/woocommerce/app/product-categories/form.js
@@ -8,12 +8,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { localize } from 'i18n-calypso';
-import { isNumber } from 'lodash';
+import { debounce, isNumber } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import Card from 'components/card';
+import CompactTinyMCE from 'woocommerce/components/compact-tinymce';
 import FormFieldSet from 'components/forms/form-fieldset';
 import FormLabel from 'components/forms/form-label';
 import FormTextInput from 'components/forms/form-text-input';
@@ -28,10 +29,21 @@ class ProductCategoryForm extends Component {
 		editProductCategory: PropTypes.func.isRequired,
 	};
 
+	constructor( props ) {
+		super( props );
+
+		this.debouncedSetDescription = debounce( this.setDescription, 200 );
+	}
+
 	setName = e => {
 		const { siteId, category, editProductCategory } = this.props;
 		const name = e.target.value;
 		editProductCategory( siteId, category, { name } );
+	};
+
+	setDescription = description => {
+		const { siteId, category, editProductCategory } = this.props;
+		editProductCategory( siteId, category, { description } );
 	};
 
 	renderPlaceholder() {
@@ -40,6 +52,24 @@ class ProductCategoryForm extends Component {
 			<div className={ classNames( 'product-categories__form', 'is-placeholder', className ) }>
 				<div />
 			</div>
+		);
+	}
+
+	renderTinyMCE() {
+		const { category } = this.props;
+
+		if (
+			( isNumber( category.id ) && 'undefined' === typeof category.description ) ||
+			'undefined' === typeof category.id
+		) {
+			return <div className="product-categories__form-tinymce-placeholder" />;
+		}
+
+		return (
+			<CompactTinyMCE
+				initialValue={ category.description || '' }
+				onContentsChange={ this.debouncedSetDescription }
+			/>
 		);
 	}
 
@@ -57,6 +87,10 @@ class ProductCategoryForm extends Component {
 					<FormFieldSet>
 						<FormLabel htmlFor="name">{ translate( 'Category name' ) }</FormLabel>
 						<FormTextInput id="name" value={ category.name || '' } onChange={ this.setName } />
+					</FormFieldSet>
+					<FormFieldSet>
+						<FormLabel htmlFor="description">{ translate( 'Description' ) }</FormLabel>
+						{ this.renderTinyMCE() }
 					</FormFieldSet>
 				</Card>
 			</div>
