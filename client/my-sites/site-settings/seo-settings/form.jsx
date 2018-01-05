15bc4a2f89581124fecf47fe8a233c211fe5ad65
Rastislav Lamos <lamos.rasto@gmail.com>
2018-01-05 11:06:24 +0100

    Framework: Async load all Web Previews – 2nd try (#21278)
    * Framework: Async load all Web Previews
    
    * Don't use WebPreviewContent outside of WebPreview
    
    * Don't call lifecycle methods of WebPreview if isContentOnly
    
    * Remove HOC from URL and Design preview components
    
    * Hide AsyncLoad from WebPreview component
    
    * Separate WebPreview from WebPreviewContent
    
    * Remove PureComponent from WebPreviewContent
    
    * Add safeguards against passing bad props to WebPreview

--- client/my-sites/site-settings/seo-settings/form.jsx
+++ client/my-sites/site-settings/seo-settings/form.jsx
@@ -48,7 +48,6 @@ import { getSelectedSite, getSelectedSiteId } from 'state/ui/selectors';
 import { isJetpackModuleActive, isHiddenSite, isPrivateSite } from 'state/selectors';
 import { toApi as seoTitleToApi } from 'components/seo/meta-title-editor/mappings';
 import { recordTracksEvent } from 'state/analytics/actions';
-import WebPreview from 'components/web-preview';
 import { requestSite } from 'state/sites/actions';
 import { activateModule } from 'state/jetpack/modules/actions';
 import { isBusiness, isEnterprise, isJetpackBusiness } from 'lib/products-values';
@@ -64,6 +63,7 @@ import QueryJetpackModules from 'components/data/query-jetpack-modules';
 import QueryJetpackPlugins from 'components/data/query-jetpack-plugins';
 import QuerySiteSettings from 'components/data/query-site-settings';
 import { requestSiteSettings, saveSiteSettings } from 'state/site-settings/actions';
+import WebPreview from 'components/web-preview';
 
 // Basic matching for HTML tags
 // Not perfect but meets the needs of this component well
