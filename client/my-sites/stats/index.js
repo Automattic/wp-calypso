295b79ca2d45fde1346f3870503a986a997cb1bb
Jarda Snajdr <jsnajdr@gmail.com>
2018-01-08 16:59:50 +0100

    Refactor the perfmon library and use it for tracking /stats/day/:site_id

--- client/my-sites/stats/index.js
+++ client/my-sites/stats/index.js
@@ -11,11 +11,8 @@ import { navigation, siteSelection, sites } from 'my-sites/controller';
 import statsController from './controller';
 import config from 'config';
 import { makeLayout, render as clientRender } from 'controller';
-import { enablePerfmonForRoute } from 'lib/perfmon';
 
 export default function() {
-	page( '/stats/*', enablePerfmonForRoute );
-
 	page(
 		'/stats/activity/:site_id',
 		siteSelection,
