641d33c999dde9315e8e5ca19d71ddf892de3f7e
Marin Atanasov <tyxla@users.noreply.github.com>
2018-01-10 16:51:00 +0200

    Jetpack Onboarding: Bring back Business Address animations (#21384)

--- client/jetpack-onboarding/steps/business-address.jsx
+++ client/jetpack-onboarding/steps/business-address.jsx
@@ -3,7 +3,7 @@
 /**
  * External dependencies
  */
-import React, { Fragment } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import page from 'page';
@@ -64,7 +64,7 @@ class JetpackOnboardingBusinessAddressStep extends React.PureComponent {
 		);
 
 		return (
-			<Fragment>
+			<div className="steps__main">
 				<DocumentHead title={ translate( 'Business Address ‹ Jetpack Onboarding' ) } />
 				<PageViewTracker
 					path={ '/jetpack/onboarding/' + STEPS.BUSINESS_ADDRESS + '/:site' }
@@ -92,7 +92,7 @@ class JetpackOnboardingBusinessAddressStep extends React.PureComponent {
 						</Button>
 					</form>
 				</Card>
-			</Fragment>
+			</div>
 		);
 	}
 }
