478ace0d4d29dcc1c1feb0361a46e1641dda50fb
Kirk Wight <kwight@kwight.ca>
2018-01-10 12:16:39 -0800

    Chat: Correct typo warning for ChatClosureNotice.

--- client/me/help/chat-closure-notice/index.jsx
+++ client/me/help/chat-closure-notice/index.jsx
@@ -67,7 +67,7 @@ export default class ChatClosureNotice extends Component {
 	}
 }
 
-ChatClosureNotice.PropTypes = {
+ChatClosureNotice.propTypes = {
 	from: PropTypes.string.isRequired,
 	to: PropTypes.string.isRequired,
 };
