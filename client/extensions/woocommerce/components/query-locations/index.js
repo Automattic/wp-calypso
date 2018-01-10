78c3a10905e3645d480c9aef42325c65308f9552
Allen Snook <allen@allendav.com>
2018-01-10 10:13:46 -0800

    Simplify QueryLocations component to align with best practices

--- client/extensions/woocommerce/components/query-locations/index.js
+++ client/extensions/woocommerce/components/query-locations/index.js
@@ -7,37 +7,24 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 
 /**
  * Internal dependencies
  */
 import { fetchLocations } from 'woocommerce/state/sites/locations/actions';
-import { areLocationsLoaded } from 'woocommerce/state/sites/locations/selectors';
 
 class QueryLocations extends Component {
 	static propTypes = {
 		fetchLocations: PropTypes.func,
-		loaded: PropTypes.bool,
 		siteId: PropTypes.number.isRequired,
 	};
 
-	componentWillMount = () => {
-		const { siteId, loaded } = this.props;
-
-		if ( siteId && ! loaded ) {
-			this.props.fetchLocations( siteId );
-		}
+	componentDidMount = () => {
+		this.props.fetchLocations( this.props.siteId );
 	};
 
-	componentWillReceiveProps = ( { siteId, loaded } ) => {
-		if ( ! siteId ) {
-			return;
-		}
-
-		if ( siteId !== this.props.siteId && ! loaded ) {
-			this.props.fetchLocations( siteId );
-		}
+	componentDidUpdate = () => {
+		this.props.fetchLocations( this.props.siteId );
 	};
 
 	render = () => {
@@ -45,21 +32,4 @@ class QueryLocations extends Component {
 	};
 }
 
-const mapStateToProps = ( state, ownProps ) => {
-	const loaded = areLocationsLoaded( state, ownProps.siteId );
-
-	return {
-		loaded,
-	};
-};
-
-const mapDispatchToProps = dispatch => {
-	return bindActionCreators(
-		{
-			fetchLocations,
-		},
-		dispatch
-	);
-};
-
-export default connect( mapStateToProps, mapDispatchToProps )( QueryLocations );
+export default connect( null, { fetchLocations } )( QueryLocations );
