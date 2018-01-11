8bf5487b12577905b6bbc521b72e1d106cef8370
Marin Atanasov <tyxla@abv.bg>
2018-01-11 14:02:57 +0200

    State: Introduce a isJetpackOnboardingStepCompleted selector

---
+++ client/state/selectors/is-jetpack-onboarding-step-completed.js
@@ -0,0 +1,48 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+import { translate } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import createSelector from 'lib/create-selector';
+import { getJetpackOnboardingSettings } from 'state/selectors';
+import { JETPACK_ONBOARDING_STEPS as STEPS } from 'jetpack-onboarding/constants';
+
+export const isJetpackOnboardingStepCompleted = createSelector(
+	( state, siteId, stepName ) => {
+		const settings = getJetpackOnboardingSettings( state, siteId );
+
+		if ( ! settings ) {
+			return null;
+		}
+
+		switch ( stepName ) {
+			case STEPS.SITE_TITLE:
+				const titleModified = get( settings, 'siteTitle', '' ) !== '';
+				const defaultDescription = translate( 'Just another WordPress site' );
+				const descriptionModified =
+					get( settings, 'siteDescription', defaultDescription ) !== defaultDescription;
+				return titleModified || descriptionModified;
+			case STEPS.SITE_TYPE:
+				return !! get( settings, 'siteType' );
+			case STEPS.HOMEPAGE:
+				return !! get( settings, 'homepageFormat' );
+			case STEPS.CONTACT_FORM:
+				return !! get( settings, 'contactForm' );
+			case STEPS.BUSINESS_ADDRESS:
+				return !! get( settings, 'businessAddress' );
+			case STEPS.WOOCOMMERCE:
+				return !! get( settings, 'woocommerce' );
+		}
+
+		return null;
+	},
+	state => [ state.jetpackOnboarding.settings ]
+);
+
+export default isJetpackOnboardingStepCompleted;
