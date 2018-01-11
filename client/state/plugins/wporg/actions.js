36d55363ae92f15763a28d5fc56fca285b3bbf93
Dennis Snell <dennis.snell@automattic.com>
2018-01-11 12:27:55 -0500

    esmodules: Update state/plugins/wporg/actions (#21191)
    @see: https://github.com/Automattic/wp-calypso/milestone/224
    
    Previously we were exporting a default object of multiple methods and
    importing those through the non-spec-compliant Babel destructuring
    import statements.
    
    This patch makes those methods proper named exports in the work of
    turning off CommonJS compilation.

--- client/state/plugins/wporg/actions.js
+++ client/state/plugins/wporg/actions.js
@@ -1,11 +1,8 @@
 /** @format */
-
 /**
  * External dependencies
  */
-
 import debugFactory from 'debug';
-
 const debug = debugFactory( 'calypso:wporg-data:actions' );
 
 /**
@@ -18,34 +15,32 @@ import { WPORG_PLUGIN_DATA_RECEIVE, FETCH_WPORG_PLUGIN_DATA } from 'state/action
 /**
  *  Local variables;
  */
-let _fetching = {};
+const _fetching = {};
 
-export default {
-	fetchPluginData: function( pluginSlug ) {
-		return dispatch => {
-			if ( _fetching[ pluginSlug ] ) {
-				return;
-			}
-			_fetching[ pluginSlug ] = true;
+export function fetchPluginData( pluginSlug ) {
+	return dispatch => {
+		if ( _fetching[ pluginSlug ] ) {
+			return;
+		}
+		_fetching[ pluginSlug ] = true;
 
-			setTimeout( () => {
-				dispatch( {
-					type: FETCH_WPORG_PLUGIN_DATA,
-					pluginSlug: pluginSlug,
-				} );
-			}, 1 );
-
-			wporg.fetchPluginInformation( pluginSlug, function( error, data ) {
-				_fetching[ pluginSlug ] = null;
-				debug( 'plugin details fetched from .org', pluginSlug, error, data );
-
-				dispatch( {
-					type: WPORG_PLUGIN_DATA_RECEIVE,
-					pluginSlug: pluginSlug,
-					data: data ? utils.normalizePluginData( { detailsFetched: Date.now() }, data ) : null,
-					error: error,
-				} );
+		setTimeout( () => {
+			dispatch( {
+				type: FETCH_WPORG_PLUGIN_DATA,
+				pluginSlug: pluginSlug,
 			} );
-		};
-	},
-};
+		}, 1 );
+
+		wporg.fetchPluginInformation( pluginSlug, function( error, data ) {
+			_fetching[ pluginSlug ] = null;
+			debug( 'plugin details fetched from .org', pluginSlug, error, data );
+
+			dispatch( {
+				type: WPORG_PLUGIN_DATA_RECEIVE,
+				pluginSlug: pluginSlug,
+				data: data ? utils.normalizePluginData( { detailsFetched: Date.now() }, data ) : null,
+				error: error,
+			} );
+		} );
+	};
+}
