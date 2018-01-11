1672c88ee14c66bf2554076ad6f951fa530811dd
Markus Dobmann <markus@mpnjs.com>
2018-01-11 11:10:55 +0100

    Head: remove preload/-connect switches
    According to @jsnmoon these switches were tries to improve loading performance but didn't have any relevant impact

--- client/document/index.jsx
+++ client/document/index.jsx
@@ -6,7 +6,7 @@
 
 import React, { Fragment } from 'react';
 import classNames from 'classnames';
-import get from 'lodash/get';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies
@@ -15,7 +15,6 @@ import { jsonStringifyForHtml } from '../../server/sanitize';
 import Head from '../components/head';
 import Masterbar from '../layout/masterbar/masterbar.jsx';
 import getStylesheetUrl from './utils/stylesheet';
-// import Badge from 'components/badge';
 
 class Document extends React.Component {
 	render() {
@@ -37,13 +36,6 @@ class Document extends React.Component {
 			sectionGroup,
 			config,
 			isFluidWidth,
-			shouldUseSingleCDN,
-			shouldUsePreconnect,
-			shouldUsePreconnectGoogle,
-			shouldUseStylePreloadExternal,
-			shouldUseStylePreloadSection,
-			shouldUseStylePreloadCommon,
-			shouldUseScriptPreload,
 			sectionCss,
 			env,
 			isDebug,
@@ -62,95 +54,23 @@ class Document extends React.Component {
 				dir={ isRTL ? 'rtl' : 'ltr' }
 				className={ classNames( { 'is-fluid-width': isFluidWidth } ) }
 			>
-				<Head
-					title={ head.title }
-					faviconURL={ faviconURL }
-					cdn={ shouldUseSingleCDN ? '//s0.wp.com' : '//s1.wp.com' }
-				>
-					{ head.metas.map( ( { name, property, content } ) => (
-						<meta name={ name } property={ property } content={ content } key={ 'meta-' + name } />
-					) ) }
-
-					{ head.links.map( ( { rel, href } ) => (
-						<link rel={ rel } href={ href } key={ 'link-' + rel + href } />
-					) ) }
+				<Head title={ head.title } faviconURL={ faviconURL } cdn={ '//s1.wp.com' }>
+					{ head.metas.map( props => <meta { ...props } /> ) }
+					{ head.links.map( props => <link { ...props } /> ) }
 
 					<link
 						rel="stylesheet"
 						id="main-css"
 						href={ urls[ getStylesheetUrl( { isRTL, isDebug, env } ) ] }
+						type="text/css"
 					/>
 					{ sectionCss && (
 						<link
 							rel="stylesheet"
 							id={ 'section-css-' + sectionCss.id }
 							href={ get( sectionCss, `urls.${ isRTL ? 'rtl' : 'ltr' }` ) }
+							type="text/css"
 						/>
-					) }
-
-					{ shouldUsePreconnect && (
-						<Fragment>
-							{ /*- Preconnect to our CDN hosts */ }
-							{ shouldUseSingleCDN ? (
-								<link rel="preconnect" href="//s0.wp.com" />
-							) : (
-								<link rel="preconnect" href="//s1.wp.com" />
-							) }
-
-							{ /*- Preconnect to our API */ }
-							<link rel="preconnect" href="https://public-api.wordpress.com" />
-
-							{ /*- Preconnect for analytics scripts. */ }
-							<link rel="preconnect" href="//stats.wp.com" />
-							<link rel="preconnect" href="https://www.google-analytics.com" />
-
-							{ /*- Preconnect for Google domains - especially useful for SSO. */ }
-							{ shouldUsePreconnectGoogle && [
-								<link rel="preconnect" href="https://google.com" />,
-								<link rel="preconnect" href="https://ssl.gstatic.com" />,
-								<link rel="preconnect" href="https://apis.google.com" />,
-								<link rel="preconnect" href="https://accounts.google.com" />,
-								<link rel="preconnect" href="https://stats.g.doubleclick.net" />,
-							] }
-						</Fragment>
-					) }
-
-					{ shouldUseStylePreloadExternal && (
-						<Fragment>
-							<link
-								rel="preload"
-								as="style"
-								/* eslint-disable max-len */
-								href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i&subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
-								/* eslint-enable max0-len */
-							/>
-							<link
-								rel="preload"
-								as="style"
-								href={ `//s${
-									shouldUseSingleCDN ? '0' : '1'
-								}.wp.com/i/noticons/noticons.css?v=20150727` }
-							/>
-						</Fragment>
-					) }
-
-					{ shouldUseStylePreloadSection &&
-						sectionCss && (
-							<link rel="preload" as="style" href={ sectionCss.urls[ isRTL ? 'rtl' : 'ltr' ] } />
-						) }
-
-					{ shouldUseStylePreloadCommon && (
-						<link rel="preload" as="style" href={ this.getStylesheetUrl() } />
-					) }
-
-					{ shouldUseScriptPreload && (
-						<Fragment>
-							{ i18nLocaleScript && <link rel="preload" as="script" href={ i18nLocaleScript } /> }
-							<link rel="preload" as="script" href={ urls.manifest } />
-							<link rel="preload" as="script" href={ urls.vendor } />
-							<link rel="preload" as="script" href={ urls[ jsFile ] } />
-							{ chunk && <link rel="preload" as="script" href={ urls[ chunk ] } /> }
-						</Fragment>
 					) }
 				</Head>
 				<body className={ classNames( { rtl: isRTL } ) }>
