e88080314c755645ae2dff6a65342babff2b7d5f
Allen Snook <allen@allendav.com>
2018-01-11 15:08:44 -0800

    Track external manage clicks

--- client/extensions/woocommerce/app/dashboard/manage-external-view.js
+++ client/extensions/woocommerce/app/dashboard/manage-external-view.js
@@ -12,12 +12,17 @@ import { localize } from 'i18n-calypso';
  * Internal dependencies
  */
 import EmptyContent from 'components/empty-content';
+import { recordTrack } from 'woocommerce/lib/analytics';
 
 class ManageExternalView extends Component {
 	static propTypes = {
 		site: PropTypes.shape( {
 			URL: PropTypes.string.isRequired,
 		} ),
+	};
+
+	recordAction = () => {
+		recordTrack( 'calypso_woocommerce_manage_external_clicked' );
 	};
 
 	render = () => {
@@ -28,14 +33,26 @@ class ManageExternalView extends Component {
 			'Stores in your country are managed directly on your site. ' +
 				'We will let you know when you can manage your store here.'
 		);
+
 		const actionURL = site.URL + '/wp-admin/edit.php?post_type=shop_order';
+		const action = (
+				<a
+					className="dashboard__empty-action button is-primary"
+					onClick={ this.recordAction }
+					href={ actionURL }
+				>
+					{ translate( 'OK, take me to my site' ) }
+				</a>
+			),
+			secondaryAction = null;
+
 		return (
 			<div className="dashboard__manage-externally">
 				<EmptyContent
 					title={ title }
 					line={ line }
-					action={ translate( 'OK, take me to my site' ) }
-					actionURL={ actionURL }
+					action={ action }
+					secondaryAction={ secondaryAction }
 				/>
 			</div>
 		);
