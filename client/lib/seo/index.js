11744376d66674fd0e4c37b2c47d3735b15c111a
Marko Andrijasevic <markoandrijasevic@gmail.com>
2018-01-12 21:15:07 +0100

    Refactor code

--- client/lib/seo/index.js
+++ client/lib/seo/index.js
@@ -4,15 +4,14 @@
  */
 import { includes } from 'lodash';
 
-export const getConflictingSeoPlugins = activePlugins => {
-	const conflictingSeoPlugins = [
-		'Yoast SEO',
-		'Yoast SEO Premium',
-		'All In One SEO Pack',
-		'All in One SEO Pack Pro',
-	];
+const conflictingSeoPlugins = [
+	'Yoast SEO',
+	'Yoast SEO Premium',
+	'All In One SEO Pack',
+	'All in One SEO Pack Pro',
+];
 
-	return activePlugins
+export const getConflictingSeoPlugins = activePlugins =>
+	activePlugins
 		.filter( ( { name } ) => includes( conflictingSeoPlugins, name ) )
 		.map( ( { name, slug } ) => ( { name, slug } ) );
-};
