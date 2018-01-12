f559514054791f91da04b3aca10766b7173ea24b
James Tien <southp0105@gmail.com>
2018-01-12 12:03:40 +0800

    Make WPCOM_BUSINESS_CONCIERGE_SCHEDULE_ID configurable via configuration files.

--- client/me/concierge/constants.js
+++ client/me/concierge/constants.js
@@ -1,4 +1,9 @@
-export const WPCOM_CONCIERGE_SCHEDULE_ID = 1;
+/**
+ * Internal dependencies
+ */
+import config from 'config';
+
+export const WPCOM_CONCIERGE_SCHEDULE_ID = config( 'wpcom_concierge_schedule_id' ) || 1;
 
 // booking status
 export const CONCIERGE_STATUS_BOOKED = 'booked';
