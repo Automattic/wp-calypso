0710edb5bfe784c4820b2e985583bbb7b8970ea5
Marin Atanasov <tyxla@abv.bg>
2018-01-12 17:28:50 +0200

    State: Remove getJetpackOnboardingProgress steps fallback

--- client/state/selectors/get-jetpack-onboarding-progress.js
+++ client/state/selectors/get-jetpack-onboarding-progress.js
@@ -3,19 +3,14 @@
 /**
  * External dependencies
  */
-import { reduce, values } from 'lodash';
+import { reduce } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { isJetpackOnboardingStepCompleted } from 'state/selectors';
-import { JETPACK_ONBOARDING_STEPS as STEPS } from 'jetpack-onboarding/constants';
 
 export default function getJetpackOnboardingProgress( state, siteId, steps ) {
-	if ( ! steps ) {
-		steps = values( STEPS );
-	}
-
 	return reduce(
 		steps,
 		( result, stepName ) => {
--- client/state/selectors/test/get-jetpack-onboarding-progress.js
+++ client/state/selectors/test/get-jetpack-onboarding-progress.js
@@ -1,42 +1,27 @@
 /** @format */
 
 /**
- * External dependencies
- */
-import { invert, mapValues } from 'lodash';
-
-/**
  * Internal dependencies
  */
 import { JETPACK_ONBOARDING_STEPS as STEPS } from 'jetpack-onboarding/constants';
 import { getJetpackOnboardingProgress, isJetpackOnboardingStepCompleted } from 'state/selectors';
 
 describe( 'getJetpackOnboardingProgress()', () => {
-	const siteId = 2916284;
-	const state = {
-		jetpackOnboarding: {
-			settings: {
-				[ siteId ]: {},
-			},
-		},
-	};
-
-	test( 'should return progress of all steps if no steps are specified', () => {
-		const steps = invert( STEPS );
-		const expected = mapValues( steps, stepName =>
-			isJetpackOnboardingStepCompleted( state, siteId, stepName )
-		);
-		const progress = getJetpackOnboardingProgress( state, 2916284 );
-		expect( progress ).toEqual( expected );
-	} );
-
 	test( 'should return progress of concrete steps if such are specified', () => {
+		const siteId = 2916284;
+		const state = {
+			jetpackOnboarding: {
+				settings: {
+					[ siteId ]: {},
+				},
+			},
+		};
 		const steps = [ [ STEPS.SITE_TITLE ], [ STEPS.SITE_TYPE ] ];
 		const expected = {
 			[ STEPS.SITE_TITLE ]: isJetpackOnboardingStepCompleted( state, siteId, STEPS.SITE_TITLE ),
 			[ STEPS.SITE_TYPE ]: isJetpackOnboardingStepCompleted( state, siteId, STEPS.SITE_TYPE ),
 		};
-		const progress = getJetpackOnboardingProgress( state, 2916284, steps );
+		const progress = getJetpackOnboardingProgress( state, siteId, steps );
 		expect( progress ).toEqual( expected );
 	} );
 } );
