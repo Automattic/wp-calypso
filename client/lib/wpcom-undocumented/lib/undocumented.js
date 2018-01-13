569ff34b33d9af7b17b5814a6ad733aa189c08c8
Marin Atanasov <tyxla@abv.bg>
2018-01-13 13:03:32 +0200

    Undocumented: Remove deleteWPCOMFollower

--- client/lib/wpcom-undocumented/lib/undocumented.js
+++ client/lib/wpcom-undocumented/lib/undocumented.js
@@ -2031,11 +2031,6 @@ Undocumented.prototype.googleAppsFilterBySiteId = function( siteId, fn ) {
 	return this.wpcom.req.get( { path: '/sites/' + siteId + '/google-apps' }, fn );
 };
 
-Undocumented.prototype.deleteWPCOMFollower = function( siteId, followerId, fn ) {
-	debug( '/site/:site_id/follower/:follower_id/delete' );
-	return this.wpcom.req.post( { path: '/sites/%s/follower/%d/delete' }, fn );
-};
-
 Undocumented.prototype.deleteEmailFollower = function( siteId, followerId, email, fn ) {
 	debug( '/site/:site_id/follower/:follower_id/delete' );
 	return this.wpcom.req.post(
