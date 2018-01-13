594c101f5106f8bc4f8e319a9c0bf76bf7932854
Marin Atanasov <tyxla@abv.bg>
2018-01-13 13:06:02 +0200

    Undocumented: Remove storeJetpackConnectUrl

--- client/lib/wpcom-undocumented/lib/undocumented.js
+++ client/lib/wpcom-undocumented/lib/undocumented.js
@@ -2290,23 +2290,6 @@ Undocumented.prototype.getSiteConnectInfo = function( targetUrl, filters ) {
 };
 
 /**
- * Post an url to be stored under user's settings,
- * so we can know that they have started a jetpack-connect flow for that site
- *
- * @param {String} targetUrl          The url of the site to store
- * @returns {Promise} Promise
- */
-Undocumented.prototype.storeJetpackConnectUrl = function( targetUrl ) {
-	return this.wpcom.req.post(
-		{ path: '/me/settings' },
-		{},
-		{
-			jetpack_connect: targetUrl,
-		}
-	);
-};
-
-/**
  * Exports the user's Reader feed as an OPML XML file.
  * A JSON object is returned with the XML given as a String
  * in the `opml` field.
