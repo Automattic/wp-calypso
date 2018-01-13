d5288c9f30852899f53327cdb9ccb8d4bdc27f59
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2018-01-13 21:40:28 +0000

    Load each WCS shipping method settings when loading the shipping zones

---
+++ client/extensions/woocommerce/woocommerce-services/state/shipping-zone-method-settings/reducer.js
@@ -0,0 +1,24 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+
+import { createReducer, keyedReducer } from 'state/utils';
+import {
+	WOOCOMMERCE_SERVICES_SHIPPING_ZONE_METHOD_SETTINGS_REQUEST,
+	WOOCOMMERCE_SERVICES_SHIPPING_ZONE_METHOD_SETTINGS_REQUEST_SUCCESS,
+} from 'woocommerce/woocommerce-services/state/action-types';
+import { LOADING } from 'woocommerce/state/constants';
+
+const reducer = createReducer( null, {
+	[ WOOCOMMERCE_SERVICES_SHIPPING_ZONE_METHOD_SETTINGS_REQUEST ]: () => {
+		return LOADING;
+	},
+
+	[ WOOCOMMERCE_SERVICES_SHIPPING_ZONE_METHOD_SETTINGS_REQUEST_SUCCESS ]: ( state, { data } ) => {
+		return data;
+	},
+} );
+
+export default keyedReducer( 'instanceId', reducer );
