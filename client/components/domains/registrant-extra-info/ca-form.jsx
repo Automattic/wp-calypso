89be733367b80f7383c79eb39bb0e121f3d7e121
Julian de Bhal <julian.debhal@automattic.com>
2018-01-15 09:49:12 +1000

    Handle not having children

--- client/components/domains/registrant-extra-info/ca-form.jsx
+++ client/components/domains/registrant-extra-info/ca-form.jsx
@@ -22,6 +22,7 @@ import FormSelect from 'components/forms/form-select';
 import FormCheckbox from 'components/forms/form-checkbox';
 import FormInputValidation from 'components/forms/form-input-validation';
 import { Input } from 'my-sites/domains/components/form';
+import { disableSubmitButton } from './with-contact-details-validation';
 
 const ciraAgreementUrl = 'https://services.cira.ca/agree/agreement/agreementVersion2.0.jsp';
 const defaultValues = {
@@ -180,10 +181,9 @@ export class RegistrantExtraInfoCaForm extends React.PureComponent {
 			doesntNeedOrganizationField || ! organizationFieldProps.isError;
 
 		const formIsValid = ciraAgreementAccepted && organizationFieldIsValid;
-
 		const validatingSubmitButton = formIsValid
 			? this.props.children
-			: React.cloneElement( this.props.children, { disabled: true } );
+			: disableSubmitButton( this.props.children );
 
 		return (
 			<form className="registrant-extra-info__form">
