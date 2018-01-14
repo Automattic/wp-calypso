495bcf12cb2e0de4aa0e31a09a2f8a6a6d2319fb
Dennis Snell <dennis.snell@automattic.com>
2018-01-13 19:27:33 -0500

    Rewind Credentials: Shuffle props around
    This is part of a mini-project to untangle data flows within the
    Activity Log and Rewind system so that each component operates more
    independently of its parents and can be worked on in isolation.
    
    Many of the props and callbacks relevant to Jetpack Rewind credentials
    get passed down through multiple chains of React component props. In
    this patch we're disconnecting some of those parent-child relationships.
    
    This is preparatory work further to deprecate and eventually remove code
    dependent on the older and also-deprecated Rewind APIs, preferring
    instead to move to the specified and centralized "state machine" API

--- client/my-sites/site-settings/jetpack-credentials/credentials-setup-flow/setup-form.jsx
+++ client/my-sites/site-settings/jetpack-credentials/credentials-setup-flow/setup-form.jsx
@@ -1,31 +1,36 @@
+/** @format */
 /**
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import CompactCard from 'components/card/compact';
 import CredentialsForm from '../credentials-form/index';
+import { updateCredentials } from 'state/jetpack/credentials/actions';
 
-const SetupForm = ( { formIsSubmitting, reset, siteId, updateCredentials } ) => (
+const SetupForm = ( { formIsSubmitting, reset, siteId } ) => (
 	<CompactCard>
-		<CredentialsForm { ...{
-			formIsSubmitting,
-			protocol: 'ssh',
-			host: '',
-			port: '22',
-			user: '',
-			pass: '',
-			abspath: '',
-			kpri: '',
-			onCancel: reset,
-			siteId,
-			updateCredentials,
-			showCancelButton: true
-		} } />
+		<CredentialsForm
+			{ ...{
+				formIsSubmitting,
+				protocol: 'ssh',
+				host: '',
+				port: '22',
+				user: '',
+				pass: '',
+				abspath: '',
+				kpri: '',
+				onCancel: reset,
+				siteId,
+				updateCredentials: this.props.updateCredentials,
+				showCancelButton: true,
+			} }
+		/>
 	</CompactCard>
 );
 
-export default SetupForm;
+export default connect( null, { updateCredentials } )( SetupForm );
--- client/my-sites/site-settings/jetpack-credentials/index.jsx
+++ client/my-sites/site-settings/jetpack-credentials/index.jsx
@@ -1,6 +1,9 @@
 /**
  * External dependencies
+ *
+ * @format
  */
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { localize } from 'i18n-calypso';
@@ -13,99 +16,101 @@ import CompactCard from 'components/card/compact';
 import CredentialsSetupFlow from './credentials-setup-flow/index';
 import CredentialsConfigured from './credentials-configured/index';
 import Gridicon from 'gridicons';
+import QueryRewindState from 'components/data/query-rewind-state';
 import QueryRewindStatus from 'components/data/query-rewind-status';
 import QueryJetpackCredentials from 'components/data/query-jetpack-credentials';
-import { isRewindActive } from 'state/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import {
 	getJetpackCredentials,
-	isUpdatingJetpackCredentials,
+	getRewindState,
 	hasMainCredentials,
-	isSitePressable,
-	getCredentialsAutoConfigStatus
+	isRewindActive,
+	isUpdatingJetpackCredentials,
 } from 'state/selectors';
-import {
-	updateCredentials,
-	autoConfigCredentials,
-	deleteCredentials,
-} from 'state/jetpack/credentials/actions';
 
 class Backups extends Component {
 	static propTypes = {
-		autoConfigStatus: PropTypes.string,
 		formIsSubmitting: PropTypes.bool,
 		hasMainCredentials: PropTypes.bool,
 		mainCredentials: PropTypes.object,
 		isPressable: PropTypes.bool,
 		isRewindActive: PropTypes.bool,
-		siteId: PropTypes.number.isRequired
+		siteId: PropTypes.number.isRequired,
 	};
 
 	render() {
 		const {
-			autoConfigStatus,
-			hasMainCredentials, // eslint-disable-line no-shadow
-			isPressable,
-			isRewindActive, // eslint-disable-line no-shadow
-			translate,
 			formIsSubmitting,
-			updateCredentials,
+			hasMainCredentials, // eslint-disable-line no-shadow
+			isRewindActive, // eslint-disable-line no-shadow
+			mainCredentials,
+			rewindState,
 			siteId,
-			autoConfigCredentials
+			translate,
 		} = this.props;
+
+		const canAutoconfigure =
+			!! rewindState.canAutoconfigure ||
+			( rewindState.credentials || [] ).some( cred => cred.type === 'auto' );
 
 		return (
 			<div className="jetpack-credentials">
+				<QueryRewindState siteId={ this.props.siteId } />
 				<QueryRewindStatus siteId={ this.props.siteId } />
 				<QueryJetpackCredentials siteId={ this.props.siteId } />
 				{ isRewindActive && (
 					<CompactCard className="jetpack-credentials__header">
 						<span>{ translate( 'Backups and security scans' ) }</span>
-							{ hasMainCredentials && (
-								<span className="jetpack-credentials__connected">
-									<Gridicon icon="checkmark" size={ 18 } className="jetpack-credentials__connected-checkmark" />
-									{ translate( 'Connected' ) }
-								</span>
-							) }
+						{ hasMainCredentials && (
+							<span className="jetpack-credentials__connected">
+								<Gridicon
+									icon="checkmark"
+									size={ 18 }
+									className="jetpack-credentials__connected-checkmark"
+								/>
+								{ translate( 'Connected' ) }
+							</span>
+						) }
 					</CompactCard>
 				) }
 
-				{ isRewindActive && ! hasMainCredentials && (
-					<CredentialsSetupFlow { ...{
-						isPressable,
-						formIsSubmitting,
-						siteId,
-						updateCredentials,
-						autoConfigCredentials,
-						autoConfigStatus
-					} } />
-				) }
+				{ isRewindActive &&
+					! hasMainCredentials && (
+						<CredentialsSetupFlow
+							{ ...{
+								canAutoconfigure,
+								formIsSubmitting,
+								siteId,
+							} }
+						/>
+					) }
 
-				{ isRewindActive && hasMainCredentials && (
-					<CredentialsConfigured { ...this.props } />
-				) }
+				{ isRewindActive &&
+					hasMainCredentials && (
+						<CredentialsConfigured
+							{ ...{
+								canAutoconfigure,
+								mainCredentials,
+								formIsSubmitting,
+								siteId,
+							} }
+						/>
+					) }
 			</div>
 		);
 	}
 }
 
-export default connect(
-	( state ) => {
-		const siteId = getSelectedSiteId( state );
-		const credentials = getJetpackCredentials( state, siteId, 'main' );
+export default connect( state => {
+	const siteId = getSelectedSiteId( state );
+	const credentials = getJetpackCredentials( state, siteId, 'main' );
 
-		return {
-			autoConfigStatus: getCredentialsAutoConfigStatus( state, siteId ),
-			formIsSubmitting: isUpdatingJetpackCredentials( state, siteId ),
-			hasMainCredentials: hasMainCredentials( state, siteId ),
-			mainCredentials: credentials,
-			isPressable: isSitePressable( state, siteId ),
-			isRewindActive: isRewindActive( state, siteId ),
-			siteId,
-		};
-	}, {
-		autoConfigCredentials,
-		updateCredentials,
-		deleteCredentials,
-	}
-)( localize( Backups ) );
+	return {
+		formIsSubmitting: isUpdatingJetpackCredentials( state, siteId ),
+		hasMainCredentials: hasMainCredentials( state, siteId ),
+		mainCredentials: credentials,
+		isRewindActive: isRewindActive( state, siteId ),
+		rewindState: getRewindState( state, siteId ),
+		siteId,
+	};
+} )( localize( Backups ) );
