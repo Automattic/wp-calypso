be9b2d8848da17fc6566f4b921ea6b654bc8334e
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2018-01-15 11:19:45 +0000

    remove reducer/actions unused code (doesn't apply to the Calypso flow)

--- client/extensions/woocommerce/woocommerce-services/state/service-settings/actions.js
+++ client/extensions/woocommerce/woocommerce-services/state/service-settings/actions.js
@@ -1,23 +1,9 @@
 /**
- * External dependencies
- */
-import { translate } from 'i18n-calypso';
-
-/**
  * Internal dependencies
  */
-import * as api from 'woocommerce/woocommerce-services/api';
-import * as NoticeActions from 'state/notices/actions';
 export const INIT_FORM = 'INIT_FORM';
 export const SET_FORM_PROPERTY = 'SET_FORM_PROPERTY';
 export const SET_ALL_PRISTINE = 'SET_ALL_PRISTINE';
-
-const initForm = ( form ) => {
-	return {
-		type: INIT_FORM,
-		...form,
-	};
-};
 
 export const setFormProperty = ( field, value ) => {
 	return {
@@ -25,21 +11,6 @@ export const setFormProperty = ( field, value ) => {
 		field,
 		value,
 	};
-};
-
-export const fetchForm = () => ( dispatch, getState, { methodId, instanceId } ) => {
-	dispatch( setFormProperty( 'isFetching', true ) );
-	return api.get( api.url.serviceSettings( methodId, instanceId ) )
-		.then( ( response ) => {
-			dispatch( initForm( response ) );
-		} )
-		.catch( () => {
-			NoticeActions.errorNotice( translate( 'Error while loading the settings. Please refresh the page to try again.' ) );
-			dispatch( setFormProperty( 'fetchError', false ) );
-		} )
-		.then( () => {
-			dispatch( setFormProperty( 'isFetching', false ) );
-		} );
 };
 
 export const setAllPristine = ( pristineValue ) => ( {
