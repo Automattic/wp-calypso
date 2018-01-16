3d9221e61e119fb92a685ad8176f80f035aa7714
Dennis Snell <dennis.snell@automattic.com>
2018-01-15 19:04:40 -0500

    move `updateCredentials` to credentials form

--- client/my-sites/site-settings/jetpack-credentials/credentials-setup-flow/setup-form.jsx
+++ client/my-sites/site-settings/jetpack-credentials/credentials-setup-flow/setup-form.jsx
@@ -3,14 +3,12 @@
  * External dependencies
  */
 import React from 'react';
-import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import CompactCard from 'components/card/compact';
 import CredentialsForm from '../credentials-form/index';
-import { updateCredentials } from 'state/jetpack/credentials/actions';
 
 const SetupForm = ( { formIsSubmitting, reset, siteId } ) => (
 	<CompactCard>
@@ -26,11 +24,10 @@ const SetupForm = ( { formIsSubmitting, reset, siteId } ) => (
 				kpri: '',
 				onCancel: reset,
 				siteId,
-				updateCredentials: this.props.updateCredentials,
 				showCancelButton: true,
 			} }
 		/>
 	</CompactCard>
 );
 
-export default connect( null, { updateCredentials } )( SetupForm );
+export default SetupForm;
