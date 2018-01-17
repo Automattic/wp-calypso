44e702752d596ef08f8a5b15f589098ad3503255
James Nylen <jnylen@gmail.com>
2018-01-16 19:02:54 -0500

    Refine invite rendering; include gravatar

--- client/my-sites/people/people-invites/index.jsx
+++ client/my-sites/people/people-invites/index.jsx
@@ -8,6 +8,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
+import { pick } from 'lodash';
 
 /**
  * Internal dependencies
@@ -17,12 +18,38 @@ import Main from 'components/main';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import PeopleSectionNav from 'my-sites/people/people-section-nav';
 import Card from 'components/card';
+import Gravatar from 'components/gravatar';
 import { requestInvites } from 'state/invites/actions';
 
 class PeopleInvites extends React.PureComponent {
 	static propTypes = {
 		site: PropTypes.object.isRequired,
 	};
+
+	renderInvite( invite ) {
+		// If an invite was sent to a WP.com user, the invite object will have
+		// either a display name (if set) or the WP.com username.  Invites can
+		// also be sent to any email address, in which case the other details
+		// will not be set (but the server will still set `avatar_URL` based on
+		// the email).
+		const user = invite.user;
+		const gravatarUser = pick( user, 'ID', 'display_name', 'avatar_URL' );
+		const userNameOrEmail = user.name || user.login || user.email;
+
+		return (
+			<Card key={ invite.invite_key }>
+				Invited
+				{ ' ' }
+				<Gravatar user={ gravatarUser } />
+				{ ' ' }
+				<strong>{ userNameOrEmail }</strong>
+				{ ' ' }
+				as
+				{ ' ' }
+				<strong>{ invite.role }</strong>
+			</Card>
+		);
+	}
 
 	render() {
 		const { site, requesting, invites } = this.props;
@@ -39,11 +66,7 @@ class PeopleInvites extends React.PureComponent {
 					<PeopleSectionNav filter="invites" site={ site } />
 					{ ! requesting && ! invites && this.props.requestInvites( site.ID ) }
 					{ requesting && <Card>Loading invites...</Card> }
-					{ ( invites || [] ).map( invite => (
-						<Card key={ invite.invite_key } style={ { wordWrap: 'break-word' } }>
-							{ JSON.stringify( invite ) }
-						</Card>
-					) ) }
+					{ ( invites || [] ).map( this.renderInvite ) }
 				</div>
 			</Main>
 		);
