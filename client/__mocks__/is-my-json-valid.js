db29d39a17279ffa76ed52409f21634c8999db30
Jon Surrell <jon.surrell@automattic.com>
2018-01-18 10:19:10 +0100

    Print title/description if provided, fallback schema

--- client/__mocks__/is-my-json-valid.js
+++ client/__mocks__/is-my-json-valid.js
@@ -11,7 +11,23 @@ const validateSchema = imjv( jsonSchemaDraft04, { verbose: true, greedy: true } 
 
 function throwOnInvalidSchema( schema ) {
 	if ( ! validateSchema( schema ) ) {
-		const msg = [ 'Invalid schema received', '', JSON.stringify( schema, undefined, 2 ), '' ];
+		const msg = [ 'Invalid schema received', '' ];
+
+		// Build a "header" which allows the schema to be easily identified
+		// from the error message. Use title and/or description if provided.
+		// Otherwise, print the entire schema.
+		if ( schema.title || schema.description ) {
+			if ( schema.title ) {
+				msg.push( 'Title: ' + schema.title );
+			}
+			if ( schema.description ) {
+				msg.push( 'Description: ' + schema.description );
+			}
+			msg.push( '' );
+		} else {
+			msg.push( JSON.stringify( schema, undefined, 2 ), '' );
+		}
+
 		forEach( validateSchema.errors, ( { field, message, schemaPath, value } ) => {
 			// data.myField is required
 			msg.push( `${ field } ${ message }` );
