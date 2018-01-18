da8569d8a29aea75017dc741c21179afbc84ea86
Kerry Liu <kerryliu@gmail.com>
2018-01-17 17:20:54 -0800

    Plugins: fix incorrect import

--- client/my-sites/plugins/utils.js
+++ client/my-sites/plugins/utils.js
@@ -10,11 +10,11 @@ import { find, get, includes } from 'lodash';
  * Internal dependencies
  */
 import config from 'config';
-import sectionsModule from 'sections-middleware';
+import { getSections } from 'sections-middleware';
 
 export function getExtensionSettingsPath( plugin ) {
 	const pluginSlug = get( plugin, 'slug', '' );
-	const sections = sectionsModule.getSections();
+	const sections = getSections();
 	const section = find( sections, value => value.name === pluginSlug );
 	const env = get( section, 'envId', [] );
 
