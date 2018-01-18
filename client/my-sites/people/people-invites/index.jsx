2b8e8d501c100f4b16e9c63a81d9dcbd27ab57d8
Jonathan Sadowski <sadowski@automattic.com>
2018-01-18 09:21:47 -0600

    Handle loading and empty states

--- client/my-sites/people/people-invites/index.jsx
+++ client/my-sites/people/people-invites/index.jsx
@@ -13,7 +13,9 @@ import { pick } from 'lodash';
  * Internal dependencies
  */
 import Main from 'components/main';
+import EmptyContent from 'components/empty-content';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
+import PeopleListSectionHeader from 'my-sites/people/people-list-section-header';
 import PeopleSectionNav from 'my-sites/people/people-section-nav';
 import PeopleListItem from 'my-sites/people/people-list-item';
 import Card from 'components/card';
@@ -57,14 +59,32 @@ class PeopleInvites extends React.PureComponent {
 		);
 	};
 
+	renderEmptyContent() {
+		const { translate } = this.props;
+
+		return <EmptyContent title={ translate( "You haven't sent any invites yet." ) } />;
+	}
+
+	renderPlaceholder() {
+		return (
+			<Card>
+				<PeopleListItem key="people-list-item-placeholder" />
+			</Card>
+		);
+	}
+
 	render() {
-		const { site, requesting, invites } = this.props;
+		const { invites, requesting, site, translate } = this.props;
 
 		if ( ! site || ! site.ID ) {
 			return null;
 		}
 
 		const hasInvites = !! ( invites && invites.length );
+		const count = ( ! requesting && hasInvites && invites.length ) || null;
+		const showPlaceholder = requesting && ! hasInvites;
+		const showEmptyContent = ! requesting && ! hasInvites;
+		const showSectionHeader = requesting || hasInvites;
 
 		return (
 			<Main className="people-invites">
@@ -73,8 +93,17 @@ class PeopleInvites extends React.PureComponent {
 
 				<div>
 					<PeopleSectionNav filter="invites" site={ site } />
-					{ requesting && ! hasInvites && <Card>Loading invites...</Card> }
-					{ hasInvites && invites.map( this.renderInvite ) }
+
+					{ showSectionHeader && (
+						<PeopleListSectionHeader
+							label={ translate( 'Invites' ) }
+							site={ site }
+							count={ count }
+						/>
+					) }
+					{ showPlaceholder && this.renderPlaceholder() }
+					{ showEmptyContent && this.renderEmptyContent() }
+					{ hasInvites && <Card>{ invites.map( this.renderInvite ) }</Card> }
 				</div>
 			</Main>
 		);
