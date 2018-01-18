ef2cafd60baab5e4a295b34de2e9bb3d42cea38c
Steve Seear <steve.seear@gmail.com>
2018-01-18 11:51:32 +0000

    Jetpack Connect: Record mobile app flow to tracks (#21612)
    Record whether or not a connection is being made from the mobile apps by adding an is_mobile_app_flow property to the major connection auth events.
    
    This will allow segmenting jetpack connection funnels by connections made via mobile app.

--- client/jetpack-connect/authorize.js
+++ client/jetpack-connect/authorize.js
@@ -101,6 +101,8 @@ export class JetpackAuthorize extends Component {
 		const { recordTracksEvent } = this.props;
 
 		const tracksProperties = pick( this.props.authQuery, 'from' );
+		tracksProperties.is_mobile_app_flow = this.props.isMobileAppFlow;
+
 		recordTracksEvent( 'calypso_jpc_authorize_form_view', tracksProperties );
 		recordTracksEvent( 'calypso_jpc_auth_view', tracksProperties );
 
