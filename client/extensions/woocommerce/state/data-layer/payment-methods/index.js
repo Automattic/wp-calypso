d3b08f35bf8ecd61df051923cec47520ed1b9d76
Justin Shreve <justin.shreve@gmail.com>
2018-01-18 22:36:05 -0800

    Make sure we don't pass back empty values for payment_request options.

--- client/extensions/woocommerce/state/data-layer/payment-methods/index.js
+++ client/extensions/woocommerce/state/data-layer/payment-methods/index.js
@@ -26,11 +26,22 @@ export default {
 				settings[ settingKey ] = method.settings[ settingKey ].value;
 			} );
 
-			// Temporary Fix. See https://github.com/woocommerce/woocommerce-gateway-stripe/issues/459
-			// The API default when no choice has been chosen previously is a string 'default'
-			// If that gets passed back to the API, the entire request fails.
+			// Temporary Fix.
+			// If blank values get passed back to the API (see https://github.com/Automattic/wp-calypso/pull/21618#issuecomment-358844061)
+			// the API call will fail. We can remove this hack when we stop the payment method saving code from passing up all fields, and
+			// only pass edited fields. See https://github.com/Automattic/wp-calypso/issues/21670
 			if ( 'stripe' === method.id ) {
-				settings.payment_request_button_theme = 'dark';
+				if ( ! settings.payment_request_button_theme ) {
+					settings.payment_request_button_theme = 'dark';
+				}
+
+				if ( ! settings.payment_request_button_type ) {
+					settings.payment_request_button_type = 'buy';
+				}
+
+				if ( ! settings.payment_request_button_height ) {
+					settings.payment_request_button_height = '44';
+				}
 			}
 
 			const payload = {
