da084a25ec200029761414260622d8e41122ae1e
Dennis Snell <dennis.snell@automattic.com>
2018-01-19 18:04:10 -0500

    Rewind Credentials: Shuffle props around (Take 2) (#21707)
    See #21516 where I tried and failed.
    
    This is part of a mini-project to untangle data flows within the
    Activity Log and Rewind system so that each component operates more
    independently of its parents and can be worked on in isolation.
    
    Many of the props and callbacks relevant to Jetpack Rewind credentials
    get passed down through multiple chains of React component props. In
    this patch we're disconnecting some of those parent-child relationships.
    
    This is preparatory work further to deprecate and eventually remove code
    dependent on the older and also-deprecated Rewind APIs, preferring
    instead to move to the specified and centralized "state machine" API

--- client/my-sites/site-settings/jetpack-credentials/index.jsx
+++ client/my-sites/site-settings/jetpack-credentials/index.jsx
@@ -1,3 +1,4 @@
+/** @format */
 /**
  * External dependencies
  */
@@ -10,48 +11,27 @@ import { connect } from 'react-redux';
  * Internal dependencies
  */
 import CompactCard from 'components/card/compact';
-import CredentialsSetupFlow from './credentials-setup-flow/index';
-import CredentialsConfigured from './credentials-configured/index';
+import CredentialsSetupFlow from './credentials-setup-flow';
+import CredentialsConfigured from './credentials-configured';
 import Gridicon from 'gridicons';
 import QueryRewindStatus from 'components/data/query-rewind-status';
 import QueryJetpackCredentials from 'components/data/query-jetpack-credentials';
-import { isRewindActive } from 'state/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
-import {
-	getJetpackCredentials,
-	isUpdatingJetpackCredentials,
-	hasMainCredentials,
-	isSitePressable,
-	getCredentialsAutoConfigStatus
-} from 'state/selectors';
-import {
-	updateCredentials,
-	autoConfigCredentials,
-	deleteCredentials,
-} from 'state/jetpack/credentials/actions';
+import { hasMainCredentials, isRewindActive } from 'state/selectors';
 
 class Backups extends Component {
 	static propTypes = {
-		autoConfigStatus: PropTypes.string,
-		formIsSubmitting: PropTypes.bool,
 		hasMainCredentials: PropTypes.bool,
-		mainCredentials: PropTypes.object,
-		isPressable: PropTypes.bool,
 		isRewindActive: PropTypes.bool,
-		siteId: PropTypes.number.isRequired
+		siteId: PropTypes.number.isRequired,
 	};
 
 	render() {
 		const {
-			autoConfigStatus,
 			hasMainCredentials, // eslint-disable-line no-shadow
-			isPressable,
 			isRewindActive, // eslint-disable-line no-shadow
 			translate,
-			formIsSubmitting,
-			updateCredentials,
 			siteId,
-			autoConfigCredentials
 		} = this.props;
 
 		return (
@@ -61,51 +41,31 @@ class Backups extends Component {
 				{ isRewindActive && (
 					<CompactCard className="jetpack-credentials__header">
 						<span>{ translate( 'Backups and security scans' ) }</span>
-							{ hasMainCredentials && (
-								<span className="jetpack-credentials__connected">
-									<Gridicon icon="checkmark" size={ 18 } className="jetpack-credentials__connected-checkmark" />
-									{ translate( 'Connected' ) }
-								</span>
-							) }
+						{ hasMainCredentials && (
+							<span className="jetpack-credentials__connected">
+								<Gridicon
+									icon="checkmark"
+									size={ 18 }
+									className="jetpack-credentials__connected-checkmark"
+								/>
+								{ translate( 'Connected' ) }
+							</span>
+						) }
 					</CompactCard>
 				) }
-
-				{ isRewindActive && ! hasMainCredentials && (
-					<CredentialsSetupFlow { ...{
-						isPressable,
-						formIsSubmitting,
-						siteId,
-						updateCredentials,
-						autoConfigCredentials,
-						autoConfigStatus
-					} } />
-				) }
-
-				{ isRewindActive && hasMainCredentials && (
-					<CredentialsConfigured { ...this.props } />
-				) }
+				{ isRewindActive && ! hasMainCredentials && <CredentialsSetupFlow siteId={ siteId } /> }
+				{ isRewindActive && hasMainCredentials && <CredentialsConfigured siteId={ siteId } /> }
 			</div>
 		);
 	}
 }
 
-export default connect(
-	( state ) => {
-		const siteId = getSelectedSiteId( state );
-		const credentials = getJetpackCredentials( state, siteId, 'main' );
+export default connect( state => {
+	const siteId = getSelectedSiteId( state );
 
-		return {
-			autoConfigStatus: getCredentialsAutoConfigStatus( state, siteId ),
-			formIsSubmitting: isUpdatingJetpackCredentials( state, siteId ),
-			hasMainCredentials: hasMainCredentials( state, siteId ),
-			mainCredentials: credentials,
-			isPressable: isSitePressable( state, siteId ),
-			isRewindActive: isRewindActive( state, siteId ),
-			siteId,
-		};
-	}, {
-		autoConfigCredentials,
-		updateCredentials,
-		deleteCredentials,
-	}
-)( localize( Backups ) );
+	return {
+		hasMainCredentials: hasMainCredentials( state, siteId ),
+		isRewindActive: isRewindActive( state, siteId ),
+		siteId,
+	};
+} )( localize( Backups ) );
