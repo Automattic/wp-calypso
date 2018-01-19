3af8e6660d808f24e83d2f5e543eedc536a7ae44
Dennis Snell <dennis.snell@automattic.com>
2018-01-19 16:44:39 -0500

    Rewind: Update rewind state when changing credentials (#21643)
    The credentials banner and credentials form are still dependent on old
    API calls we have decided to stop relying on. Their logic is replicated
    in here in an almost-identical way as it is in the server, but it's not
    entirely the same, plus it's duplicated.
    
    In D9399 we're returning the `rewind_state` as an additional property on
    calls to the credentials endpoints so that we can start gradually
    replacing the old API calls with the centralized `/rewind` API.
    
    This patch incorporates our ability to read and inject that response
    into Calypso. It shouldn't have any effect until the server starts
    sending those updates.

--- client/state/data-layer/wpcom/activity-log/rewind/activate/index.js
+++ client/state/data-layer/wpcom/activity-log/rewind/activate/index.js
@@ -11,8 +11,13 @@ import i18n from 'i18n-calypso';
  */
 import { http } from 'state/data-layer/wpcom-http/actions';
 import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
-import { JETPACK_CREDENTIALS_AUTOCONFIGURE, JETPACK_CREDENTIALS_STORE } from 'state/action-types';
+import {
+	JETPACK_CREDENTIALS_AUTOCONFIGURE,
+	JETPACK_CREDENTIALS_STORE,
+	REWIND_STATE_UPDATE,
+} from 'state/action-types';
 import { successNotice, errorNotice } from 'state/notices/actions';
+import { transformApi } from 'state/data-layer/wpcom/sites/rewind/api-transformer';
 
 export const fetch = ( { dispatch }, action ) => {
 	const notice = successNotice( i18n.translate( 'Obtaining your credentials…' ) );
@@ -32,7 +37,7 @@ export const fetch = ( { dispatch }, action ) => {
 	);
 };
 
-export const storeAndAnnounce = ( { dispatch }, { siteId, noticeId } ) => {
+export const storeAndAnnounce = ( { dispatch }, { siteId, noticeId }, { rewind_state } ) => {
 	dispatch( {
 		type: JETPACK_CREDENTIALS_STORE,
 		credentials: { main: { type: 'auto' } }, // fake for now until data actually comes through
@@ -45,6 +50,17 @@ export const storeAndAnnounce = ( { dispatch }, { siteId, noticeId } ) => {
 			id: noticeId,
 		} )
 	);
+
+	// the API transform could fail and the rewind data might
+	// be unavailable so if that's the case just let it go
+	// for now. we'll improve our rigor as time goes by.
+	try {
+		dispatch( {
+			type: REWIND_STATE_UPDATE,
+			siteId,
+			data: transformApi( rewind_state ),
+		} );
+	} catch ( e ) {}
 };
 
 export const announceFailure = ( { dispatch }, { noticeId } ) => {
