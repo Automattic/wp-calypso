75c34cc0311154f703842e07add2b316dd7c2df0
Jon Surrell <jon.surrell@automattic.com>
2018-01-19 13:52:27 +0100

    Remove test covered by keyedReducer

--- client/state/jetpack-connect/reducer/test/jetpack-auth-attempts.js
+++ client/state/jetpack-connect/reducer/test/jetpack-auth-attempts.js
@@ -6,11 +6,6 @@ import jetpackAuthAttempts from '../jetpack-auth-attempts';
 import { JETPACK_CONNECT_COMPLETE_FLOW, JETPACK_CONNECT_RETRY_AUTH } from 'state/action-types';
 
 describe( '#jetpackAuthAttempts()', () => {
-	test( 'should default to an empty object', () => {
-		const state = jetpackAuthAttempts( undefined, {} );
-		expect( state ).toEqual( {} );
-	} );
-
 	test( 'should update the timestamp when adding an existent slug with stale timestamp', () => {
 		const state = jetpackAuthAttempts(
 			{ 'example.com': { timestamp: 1, attempt: 1 } },
