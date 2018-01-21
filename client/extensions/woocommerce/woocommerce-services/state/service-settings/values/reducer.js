0c54601fb4c44ddbb0baca60cb77b053e9ea86c0
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2018-01-21 19:14:39 +0000

    Remove dead code and tests

--- client/extensions/woocommerce/woocommerce-services/state/service-settings/values/reducer.js
+++ client/extensions/woocommerce/woocommerce-services/state/service-settings/values/reducer.js
@@ -6,24 +6,12 @@ import objectPath from 'object-path-immutable';
 /**
  * Internal dependencies
  */
-import {
-	UPDATE_FIELD,
-	REMOVE_FIELD,
-	ADD_ARRAY_FIELD_ITEM,
-} from './actions';
+import { UPDATE_FIELD } from './actions';
 
 const reducers = {};
 
 reducers[ UPDATE_FIELD ] = ( state, action ) => {
 	return objectPath.set( state, action.path, action.value );
-};
-
-reducers[ REMOVE_FIELD ] = ( state, action ) => {
-	return objectPath.del( state, action.path, action.value );
-};
-
-reducers[ ADD_ARRAY_FIELD_ITEM ] = ( state, action ) => {
-	return objectPath.push( state, action.path, action.item );
 };
 
 const settings = ( state = {}, action ) => {
