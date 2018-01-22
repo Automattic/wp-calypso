a3622413a05b9256b2e0405d9a282773bc075dfc
Bernhard Reiter <ockham@raz.or.at>
2018-01-22 14:43:12 +0100

    Add wp-admin link targets

--- client/jetpack-onboarding/steps/summary.jsx
+++ client/jetpack-onboarding/steps/summary.jsx
@@ -52,18 +52,20 @@ class JetpackOnboardingSummaryStep extends React.PureComponent {
 	};
 
 	renderTodo = () => {
+		const { siteUrl } = this.props;
 		const stepsTodo = [
 			SUMMARY_STEPS.JETPACK_CONNECTION,
 			SUMMARY_STEPS.THEME,
 			SUMMARY_STEPS.PAGES,
 			SUMMARY_STEPS.BLOG,
 		];
+		// If we're not connected, we cannot use selectors from `sites/selectors`.
 		const stepLinks = [
-			'/jetpack/connect?url=' + this.props.siteUrl,
+			'/jetpack/connect?url=' + siteUrl,
 			// TODO: update the following with relevant links
-			'#',
-			'#',
-			'#',
+			siteUrl + '/wp-admin/themes.php',
+			siteUrl + '/wp-admin/post-new.php?post_type=page',
+			siteUrl + '/wp-admin/post-new.php',
 		];
 
 		return map( stepsTodo, ( fieldLabel, fieldIndex ) => (
