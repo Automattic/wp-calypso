fa409a359e847feb532fa1c0cecc3f307d37f204
andfinally <andfinally@users.noreply.github.com>
2018-01-23 21:36:50 +0000

    PostRevisions. Split navigation buttons into a component contained in a ButtonGroup.
    Replaces the editor-revisions-list__navigation div with a ButtonGroup.
    Splits out the revisions nav buttons into a separate component, EditorRevisionsListNavigation, which is included from EditorRevisionsList. This will make it easier to reposition the buttons if needed. Changes the button container to a ButtonGroup.

--- client/post-editor/editor-revisions-list/header.jsx
+++ client/post-editor/editor-revisions-list/header.jsx
@@ -7,48 +7,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { localize } from 'i18n-calypso';
-import Gridicon from 'gridicons';
 
-/**
- * Internal dependencies
- */
-import Button from 'components/button';
-
-const EditorRevisionsListHeader = ( {
-	numRevisions,
-	translate,
-	selectNextRevision,
-	selectPreviousRevision,
-} ) => {
+const EditorRevisionsListHeader = ( { numRevisions, translate } ) => {
 	return (
-		<div>
-			<div className="editor-revisions-list__header">
-				{ !! numRevisions &&
-					translate( '%(revisions)d revision', '%(revisions)d revisions', {
-						count: numRevisions,
-						args: { revisions: numRevisions },
-					} ) }
-			</div>
-			<div className="editor-revisions-list__navigation">
-				<Button
-					compact
-					borderless
-					className="editor-revisions-list__prev-button"
-					type="button"
-					onClick={ selectPreviousRevision }
-				>
-					<Gridicon icon="chevron-down" />
-				</Button>
-				<Button
-					compact
-					borderless
-					className="editor-revisions-list__next-button"
-					type="button"
-					onClick={ selectNextRevision }
-				>
-					<Gridicon icon="chevron-up" />
-				</Button>
-			</div>
+		<div className="editor-revisions-list__header">
+			{ !! numRevisions &&
+				translate( '%(revisions)d revision', '%(revisions)d revisions', {
+					count: numRevisions,
+					args: { revisions: numRevisions },
+				} ) }
 		</div>
 	);
 };
