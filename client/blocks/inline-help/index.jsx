be3414390af2d21f88ae0231308a5eb04d170f1a
Leif Singer <git@singer.sh>
2018-01-23 15:55:28 +0100

    add keyboard navigation

--- client/blocks/inline-help/index.jsx
+++ client/blocks/inline-help/index.jsx
@@ -1,12 +1,10 @@
-/** @format */
-
 /**
  * External dependencies
  */
 
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { identity, isEmpty } from 'lodash';
+import { identity, includes, isEmpty } from 'lodash';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import classNames from 'classnames';
@@ -15,12 +13,19 @@ import Gridicon from 'gridicons';
 /**
  * Internal Dependencies
  */
+import config from 'config';
 import Button from 'components/button';
 import Popover from 'components/popover';
 import SearchCard from 'components/search-card';
 import HelpSearchStore from 'lib/help-search/store';
 import HelpSearchActions from 'lib/help-search/actions';
 import { decodeEntities, preventWidows } from 'lib/formatting';
+
+/**
+ * Module variables
+ */
+const globalKeyBoardShortcutsEnabled = config.isEnabled( 'keyboard-shortcuts' );
+const globalKeyboardShortcuts = globalKeyBoardShortcutsEnabled ? require( 'lib/keyboard-shortcuts/global' )() : null;
 
 class InlineHelp extends Component {
 	static propTypes = {
@@ -31,14 +36,52 @@ class InlineHelp extends Component {
 		translate: identity,
 	};
 
+	// going without context for now -- let's just provide the top x recommended links
+	// copied from client/me/help/main for now
+	helpfulResults = [
+		{
+			link: 'https://en.support.wordpress.com/com-vs-org/',
+			title: this.props.translate( 'Uploading custom plugins and themes' ),
+			description: this.props.translate(
+				'Learn more about installing a custom theme or plugin using the Business plan.'
+			),
+		},
+		{
+			link: 'https://en.support.wordpress.com/all-about-domains/',
+			title: this.props.translate( 'All About Domains' ),
+			description: this.props.translate(
+				'Set up your domain whether it’s registered with WordPress.com or elsewhere.'
+			),
+		},
+		{
+			link: 'https://en.support.wordpress.com/start/',
+			title: this.props.translate( 'Get Started' ),
+			description: this.props.translate(
+				'No matter what kind of site you want to build, our five-step checklists will get you set up and ready to publish.'
+			),
+		},
+		{
+			link: 'https://en.support.wordpress.com/settings/privacy-settings/',
+			title: this.props.translate( 'Privacy Settings' ),
+			description: this.props.translate(
+				'Limit your site’s visibility or make it completely private.'
+			),
+		},
+	];
+
 	state = {
 		helpLinks: [],
+		resultCount: this.helpfulResults.length,
+		selectedResult: -1,
 		searchQuery: '',
 		showInlineHelp: false,
 	};
 
 	componentDidMount() {
 		HelpSearchStore.on( 'change', this.refreshHelpLinks );
+		if ( globalKeyboardShortcuts ) {
+			globalKeyboardShortcuts.showInlineHelp = this.showInlineHelp.bind( this );
+		}
 	}
 
 	componentWillUnmount() {
@@ -46,8 +89,25 @@ class InlineHelp extends Component {
 	}
 
 	refreshHelpLinks = () => {
-		this.setState( { helpLinks: HelpSearchStore.getHelpLinks() } );
+		const helpLinks = HelpSearchStore.getHelpLinks().wordpress_support_links;
+		this.setState( {
+			helpLinks: helpLinks,
+			selectedResult: -1,
+			resultCount: helpLinks ? helpLinks.length : this.helpfulResults.length,
+		} );
 	};
+
+	selectedUrl = () => {
+		if ( this.state.selectedResult === -1 ) {
+			return false;
+		}
+		const links = this.state.helpLinks && this.state.helpLinks.length > 0 ? this.state.helpLinks : this.helpfulResults;
+		const selectedLink = links[ this.state.selectedResult ];
+		if ( ! selectedLink || ! selectedLink.link ) {
+			return false;
+		}
+		return selectedLink.link;
+	}
 
 	toggleInlineHelp = () => {
 		const { showInlineHelp } = this.state;
@@ -73,6 +133,50 @@ class InlineHelp extends Component {
 		HelpSearchActions.fetch( searchQuery );
 	};
 
+	onKeyDown = event => {
+		// ignore keyboard access when manipulating a text selection in input
+		if ( event.getModifierState() ) {
+			return;
+		}
+		// take over control if and only if it's one of our keys
+		if ( includes( [ 'ArrowUp', 'ArrowDown', 'Enter' ], event.key ) ) {
+			event.preventDefault();
+		} else {
+			return;
+		}
+
+		const { resultCount, selectedResult } = this.state;
+		let nextIndex = null;
+
+		switch ( event.key ) {
+			case 'ArrowUp':
+				nextIndex = selectedResult - 1;
+				if ( nextIndex < -1 ) {
+					nextIndex = resultCount - 1;
+				}
+				break;
+			case 'ArrowDown':
+				nextIndex = selectedResult + 1;
+				if ( nextIndex >= resultCount ) {
+					nextIndex = -1;
+				}
+				break;
+			case 'Enter':
+				const selectedUrl = this.selectedUrl();
+				if ( selectedUrl ) {
+					window.location = selectedUrl;
+					return;
+				}
+				break;
+		}
+
+		if ( nextIndex !== null ) {
+			this.setState( {
+				selectedResult: nextIndex,
+			} );
+		}
+	};
+
 	inlineHelpToggleRef = node => {
 		this.inlineHelpToggle = node;
 	};
@@ -95,7 +199,7 @@ class InlineHelp extends Component {
 			);
 		}
 
-		if ( isEmpty( this.state.helpLinks.wordpress_support_links ) ) {
+		if ( isEmpty( this.state.helpLinks ) ) {
 			// search done, but nothing found
 			return (
 				<div>
@@ -106,57 +210,28 @@ class InlineHelp extends Component {
 		}
 
 		// found something in helpLinks.wordpress_support_links!
-		const links = this.state.helpLinks.wordpress_support_links;
+		const links = this.state.helpLinks;
 		return (
 			<ul className="inline-help__results-list">{ links && links.map( this.renderHelpLink ) }</ul>
 		);
 	}
 
 	renderContextHelp() {
-		// going without context for now -- let's just provide the top x recommended links
-		// copied from client/me/help/main for now
-
-		const helpfulResults = [
-			{
-				link: 'https://en.support.wordpress.com/com-vs-org/',
-				title: this.props.translate( 'Uploading custom plugins and themes' ),
-				description: this.props.translate(
-					'Learn more about installing a custom theme or plugin using the Business plan.'
-				),
-			},
-			{
-				link: 'https://en.support.wordpress.com/all-about-domains/',
-				title: this.props.translate( 'All About Domains' ),
-				description: this.props.translate(
-					'Set up your domain whether it’s registered with WordPress.com or elsewhere.'
-				),
-			},
-			{
-				link: 'https://en.support.wordpress.com/start/',
-				title: this.props.translate( 'Get Started' ),
-				description: this.props.translate(
-					'No matter what kind of site you want to build, our five-step checklists will get you set up and ready to publish.'
-				),
-			},
-			{
-				link: 'https://en.support.wordpress.com/settings/privacy-settings/',
-				title: this.props.translate( 'Privacy Settings' ),
-				description: this.props.translate(
-					'Limit your site’s visibility or make it completely private.'
-				),
-			},
-		];
-
 		return (
 			<ul className="inline-help__results-list">
-				{ helpfulResults && helpfulResults.map( this.renderHelpLink ) }
+				{ this.helpfulResults && this.helpfulResults.map( this.renderHelpLink ) }
 			</ul>
 		);
 	}
 
-	renderHelpLink( link ) {
+	renderHelpLink = ( link, index ) => {
+		const { selectedResult } = this.state;
+		const classes = [
+			'inline-help__results-item',
+			selectedResult === index && 'is-selected',
+		].filter( Boolean );
 		return (
-			<li key={ link.link } className="inline-help__results-item">
+			<li key={ link.link } className={ classNames( ...classes ) }>
 				<a href={ link.link } title={ decodeEntities( link.description ) }>
 					{ preventWidows( decodeEntities( link.title ) ) }
 				</a>
@@ -166,9 +241,10 @@ class InlineHelp extends Component {
 
 	render() {
 		const { translate } = this.props;
-		const buttonClasses = [ 'inline-help', this.state.showInlineHelp && 'is-active' ].filter(
-			Boolean
-		);
+		const buttonClasses = [
+			'inline-help',
+			this.state.showInlineHelp && 'is-active',
+		].filter( Boolean );
 		return (
 			<Button
 				className={ classNames( ...buttonClasses ) }
@@ -189,6 +265,7 @@ class InlineHelp extends Component {
 						<SearchCard
 							placeholder={ translate( 'Search for help…' ) }
 							onSearch={ this.onSearch }
+							onKeyDown={ this.onKeyDown }
 							autoFocus
 							delaySearch={ true }
 						/>
