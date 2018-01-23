121b9e839ab599f73fc120d6d44cb0f5558cdb8d
Ramon <ramonjd@users.noreply.github.com>
2018-01-23 15:45:49 +1100

    Language picker: Use localized language names for search and tooltips (#20694)
    * Added module with English language names
    
    config/_shared.json knows only the language antonyms, this new module
    adds English names, too, with support for translations.
    
    Useful for searching in language list by name.
    
    * Use English language names for search and mouseover titles
    
    Search in LanguagePickerModal now searched by autonym name, English (or localized)
    name, or by the language two or three-letter code.
    
    The English name is also displayed as element `title` on mouseover.
    
    * Plugged in new endpoint and rejigged search to accept English translations
    
    Moved i18n/language-names into data layer
    Added i18n.languageNames to redux store
    
    Updated fixtures and unit tests
    Enabled search as a permanent feature in the header
    
    Preparing for updating the  endpoint from v1.1 to v2
    
    * Updated code to support data-layer + query data component, removed useless actions
    
    * Bumping to api v2

---
+++ client/state/data-layer/wpcom/i18n/language-names/index.js
@@ -0,0 +1,52 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { noop } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { http } from 'state/data-layer/wpcom-http/actions';
+import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
+import { I18N_LANGUAGE_NAMES_REQUEST } from 'state/action-types';
+import { receiveLanguageNames } from 'state/i18n/language-names/actions';
+
+/**
+ * @module state/data-layer/wpcom/i18n/language-names
+ */
+
+/**
+ * Dispatches a request to fetch localized language names
+ *
+ * @param {Object} action Redux action
+ * @returns {Object} original action
+ */
+export const fetchLanguageNames = action =>
+	http(
+		{
+			method: 'GET',
+			apiNamespace: 'wpcom/v2',
+			path: '/i18n/language-names',
+		},
+		action
+	);
+/**
+ * Dispatches returned localized language names data
+ *
+ * @param {Object} action Redux action
+ * @param {Array} data raw data from i18n/language-names
+ * @returns {Array<Object>} Redux actions
+ */
+export const addLanguageNames = ( action, data ) => [ receiveLanguageNames( data ) ];
+
+export const dispatchPlansRequest = dispatchRequestEx( {
+	fetch: fetchLanguageNames,
+	onSuccess: addLanguageNames,
+	onError: noop,
+} );
+
+export default {
+	[ I18N_LANGUAGE_NAMES_REQUEST ]: [ dispatchPlansRequest ],
+};
