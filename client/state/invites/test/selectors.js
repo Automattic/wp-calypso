e64de73ccc3c13378f399a84d6c63b94d1898153
James Nylen <jnylen@gmail.com>
2018-01-25 15:02:14 -0600

    Simplify invites selector test

--- client/state/invites/test/selectors.js
+++ client/state/invites/test/selectors.js
@@ -36,10 +36,7 @@ describe( 'selectors', () => {
 		test( 'should return false when resend has not been requested', () => {
 			const state = {
 				invites: {
-					requestingResend: {
-						12345: { '123456asdf789': false },
-						67890: { '789lkjh123456': true },
-					},
+					requestingResend: {},
 				},
 			};
 			expect( isRequestingResend( state, 12345, '9876asdf54321' ) ).to.equal( false );
