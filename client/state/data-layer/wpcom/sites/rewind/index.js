de9d94cb6f8573b2291c1e7c9d849d126fafb2e2
Dennis Snell <dennis.snell@automattic.com>
2018-01-25 18:35:15 -0500

    extract logic for clarity

--- client/state/data-layer/wpcom/sites/rewind/index.js
+++ client/state/data-layer/wpcom/sites/rewind/index.js
@@ -25,25 +25,31 @@ const fetchRewindState = action =>
 		action
 	);
 
-const updateRewindState = ( { siteId }, data ) =>
-	[
-		{
-			type: REWIND_STATE_UPDATE,
-			siteId,
-			data,
-		},
-		data.rewind &&
-			data.rewind.status === 'running' &&
-			( dispatch =>
-				setTimeout(
-					() =>
-						dispatch( {
-							type: REWIND_STATE_REQUEST,
-							siteId,
-						} ),
-					3000
-				) ),
-	].filter( Boolean );
+const updateRewindState = ( { siteId }, data ) => {
+	const stateUpdate = {
+		type: REWIND_STATE_UPDATE,
+		siteId,
+		data,
+	};
+
+	const hasRunningRewind = data.rewind && data.rewind.status === 'running';
+
+	if ( ! hasRunningRewind ) {
+		return stateUpdate;
+	}
+
+	const delayedStateRequest = dispatch =>
+		setTimeout(
+			() =>
+				dispatch( {
+					type: REWIND_STATE_REQUEST,
+					siteId,
+				} ),
+			3000
+		);
+
+	return [ stateUpdate, delayedStateRequest ];
+};
 
 const setUnknownState = ( { siteId }, error ) =>
 	withAnalytics(
