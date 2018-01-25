9179704256b42ec0eac379c7e42f4227c84d4028
Michael Cain <michael@michaeldcain.com>
2018-01-24 21:22:47 -0500

    More reducer tests.

--- client/state/invites/test/reducer.js
+++ client/state/invites/test/reducer.js
@@ -4,6 +4,7 @@
  * External dependencies
  */
 import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
 
 /**
  * Internal dependencies
@@ -24,6 +25,29 @@ describe( 'reducer', () => {
 			);
 			expect( state ).to.eql( {
 				12345: { '123456asdf789': true },
+			} );
+		} );
+		test( 'should accumulate invites', () => {
+			const original = deepFreeze( { 12345: { '123456asdf789': false } } );
+			const state = requestingResend( original, {
+				type: INVITE_RESEND_REQUEST,
+				siteId: 12345,
+				inviteId: '789lkjh123456',
+			} );
+			expect( state ).to.eql( {
+				12345: { '123456asdf789': false, '789lkjh123456': true },
+			} );
+		} );
+		test( 'should accumulate sites', () => {
+			const original = deepFreeze( { 12345: { '123456asdf789': false } } );
+			const state = requestingResend( original, {
+				type: INVITE_RESEND_REQUEST,
+				siteId: 67890,
+				inviteId: '789lkjh123456',
+			} );
+			expect( state ).to.eql( {
+				12345: { '123456asdf789': false },
+				67890: { '789lkjh123456': true },
 			} );
 		} );
 	} );
