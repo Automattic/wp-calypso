c9305ae68d4d93d997101c12c2075cce70f3897c
andfinally <andfinally@users.noreply.github.com>
2018-01-27 16:23:01 +0000

    PostRevisions. Disable the navigation buttons when they are unable to move the selection further.
    Add methods to EditorRevisionsList to check if selected revision is the first or last in the revisions array. Pass selectedRevisionOrder prop to EditorRevisionsListNavigation which disables the previous or next button when appropriate.
    Style buttons below the 660px breakpoint. Override button group borders, background and colour. Rotate .gridicon in buttons and reverse order of buttons on small viewports.
    Switch side border on next and previous buttons at the 660 breakpoint, so the border is always in the middle.
    Condense revision order logic into a single method to avoid repetition.

--- client/post-editor/editor-revisions-list/navigation.jsx
+++ client/post-editor/editor-revisions-list/navigation.jsx
@@ -13,24 +13,28 @@ import Gridicon from 'gridicons';
 import Button from 'components/button';
 import ButtonGroup from 'components/button-group';
 
-const EditorRevisionsListNavigation = ( { selectNextRevision, selectPreviousRevision } ) => {
+const EditorRevisionsListNavigation = ( {
+	selectNextRevision,
+	selectPreviousRevision,
+	selectedRevisionOrder,
+} ) => {
 	return (
 		<ButtonGroup className="editor-revisions-list__navigation">
 			<Button
 				compact
-				borderless
 				className="editor-revisions-list__prev-button"
 				type="button"
 				onClick={ selectPreviousRevision }
+				disabled={ selectedRevisionOrder === 'earliest' }
 			>
 				<Gridicon icon="chevron-down" />
 			</Button>
 			<Button
 				compact
-				borderless
 				className="editor-revisions-list__next-button"
 				type="button"
 				onClick={ selectNextRevision }
+				disabled={ selectedRevisionOrder === 'latest' }
 			>
 				<Gridicon icon="chevron-up" />
 			</Button>
@@ -39,6 +43,7 @@ const EditorRevisionsListNavigation = ( { selectNextRevision, selectPreviousRevi
 };
 
 EditorRevisionsListNavigation.propTypes = {
+	selectedRevisionOrder: PropTypes.string,
 	selectNextRevision: PropTypes.func.isRequired,
 	selectPreviousRevision: PropTypes.func.isRequired,
 };
