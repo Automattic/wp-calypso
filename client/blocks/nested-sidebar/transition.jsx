b5118c3cc9fc8779abed00bc1ee037dc7ab0a2b2
spen <spen_@hotmail.co.uk>
2018-01-28 12:15:04 +0100

    WIP

--- client/blocks/nested-sidebar/transition.jsx
+++ client/blocks/nested-sidebar/transition.jsx
@@ -16,51 +16,6 @@ export class Transitioner extends Component {
 		direction: 'right',
 	}
 
-	state = {
-		animate: false,
-		direction: 'right',
-	};
-
-	componentDidMount() {
-		const { direction, children } = this.props;
-		// console.log( 'componentDidMount', direction )
-
-		// if (true) {}
-
-		setTimeout( () => {
-			this.setState( {
-				transitioning: true,
-			} );
-		}, 1 )
-
-		// isnt being repeated
-	}
-
-	componentWillUpdate( { direction, tcomp } ) {
-		// console.log( {
-		// 	tcomp,
-		// 	pC: this.props.tcomp,
-		// 	direction,
-		// 	pD: this.props.direction
-		// } );
-
-		// console.log(
-		// 	! this.props.direction && direction
-		// 		? 'transitioning:' + direction
-		// 		: 'no anim'
-		// )
-
-
-
-		if ( ! this.props.direction && direction ) {
-			this.setState( {
-				transitioning: false,
-				direction,
-			} )
-		}
-
-	}
-
 	render() {
 		const {
 			direction,
@@ -68,32 +23,17 @@ export class Transitioner extends Component {
 			Comp,
 			TComp
 		} = this.props;
-		const {
-			transitioning,
-			// direction
-		} = this.state;
 
 		const wrapperClasses = classNames( 'nested-sidebar__transitioner', {
-			// 'nested-sidebar__transition-right': true,
 			'nested-sidebar__transition-right': direction === 'right',
 			'nested-sidebar__transition-left': direction === 'left',
-			'transitioning': transitioning,
 		} );
-
-		const transitioningClasses = classNames( 'transitioning-component', {
-			// 'transitioning': transitioning,
-		} );
-
-		// console.log( {
-		// 	direction,
-		// 	transitioning,
-		// } );
 
 		return (
 			<div className={ wrapperClasses }>
 				{ children }
 				{ TComp && (
-					<span className={ transitioningClasses }>
+					<span className="transitioning-component">
 						<TComp />
 					</span>
 				) }
