9e808359afe294072f8b2e6225be98c992679778
Mark Ryall <mark@ryall.name>
2018-01-31 21:33:03 +1000

    Show blank placeholder for 'another reason' questions.

--- client/components/marketing-survey/cancel-purchase-form/index.jsx
+++ client/components/marketing-survey/cancel-purchase-form/index.jsx
@@ -177,11 +177,7 @@ class CancelPurchaseForm extends React.Component {
 			translate( 'I’m going to use my domain with WordPress.com without transferring it.' )
 		);
 
-		appendRadioOption(
-			'anotherReasonOne',
-			translate( 'Another reason…' ),
-			translate( 'How can we improve?' )
-		);
+		appendRadioOption( 'anotherReasonOne', translate( 'Another reason…' ), ' ' );
 
 		return (
 			<div>
@@ -243,11 +239,7 @@ class CancelPurchaseForm extends React.Component {
 			translate( 'Any particular reason(s)?' )
 		);
 
-		appendRadioOption(
-			'anotherReasonTwo',
-			translate( 'Another reason…' ),
-			translate( 'How can we improve?' )
-		);
+		appendRadioOption( 'anotherReasonTwo', translate( 'Another reason…' ), ' ' );
 
 		return (
 			<div>
