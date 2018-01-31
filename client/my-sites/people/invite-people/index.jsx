2e03e53012d63510e905f6d5acd0a91f58a62502
Sheri Bigelow <sheri@designsimply.com>
2018-01-31 16:13:50 -0700

    People: clarify the invite copy
    Fix line length.

--- client/my-sites/people/invite-people/index.jsx
+++ client/my-sites/people/invite-people/index.jsx
@@ -328,7 +328,8 @@ class InvitePeople extends React.Component {
 							<FormSettingExplanation>
 								{ translate(
 									'Want to invite new users to your site? The more the merrier! ' +
-										'Invite as many as you want, up to 10 at a time, by adding their email addresses or WordPress.com usernames.'
+										'Invite as many as you want, up to 10 at a time, by adding ' +
+										'their email addresses or WordPress.com usernames.'
 								) }
 							</FormSettingExplanation>
 						</div>
