efc4e95f3d840815fe475343ef612bb349ed9a80
Rastislav Lamos <lamos.rasto@gmail.com>
2018-01-31 16:30:48 +0100

    isRtl selector: update code and tests

--- client/state/selectors/test/is-rtl.js
+++ client/state/selectors/test/is-rtl.js
@@ -11,33 +11,33 @@ import { expect } from 'chai';
 import { isRtl } from 'state/selectors';
 
 describe( 'isRtl()', () => {
-	test( 'should return null if the value is not known', () => {
+	test( 'should return false if the value is not known', () => {
 		const result = isRtl( { ui: { language: {} } } );
 
-		expect( result ).to.be.null;
+		expect( result ).to.be.false;
 	} );
 
-	test( 'should return true if the localeSlug is RTL language', () => {
+	test( 'should return false if the isRtl reducer is false', () => {
 		const result = isRtl( {
 			ui: {
 				language: {
-					localeSlug: 'he',
-				},
-			},
-		} );
-
-		expect( result ).to.be.true;
-	} );
-
-	test( 'should return true if the localeSlug is LTR language', () => {
-		const result = isRtl( {
-			ui: {
-				language: {
-					localeSlug: 'fr',
+					isRtl: false,
 				},
 			},
 		} );
 
 		expect( result ).to.be.false;
+	} );
+
+	test( 'should return true if the isRtl reducer is true', () => {
+		const result = isRtl( {
+			ui: {
+				language: {
+					isRtl: true,
+				},
+			},
+		} );
+
+		expect( result ).to.be.true;
 	} );
 } );
