e53799438485ee88497ad5d382b4063453b90696
Rich Collier <rich.collier@automattic.com>
2018-01-31 14:14:35 -0500

    Change  to  as the default selector value

--- client/state/selectors/get-jetpack-credentials-update-status.js
+++ client/state/selectors/get-jetpack-credentials-update-status.js
@@ -4,5 +4,5 @@
 import { get } from 'lodash';
 
 export default function getJetpackCredentialsUpdateStatus( state, siteId ) {
-	return get( state, [ 'jetpack', 'credentials', 'requestStatus', siteId ], 'unknown' );
+	return get( state, [ 'jetpack', 'credentials', 'requestStatus', siteId ], 'unsubmitted' );
 }
