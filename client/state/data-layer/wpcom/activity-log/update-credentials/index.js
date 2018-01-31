c6475c3acbb47e6b15981787481a1532c4c1c380
Dennis Snell <dennis.snell@automattic.com>
2018-01-31 15:07:44 -0500

    Rewind Credentials: Report more specific errors on credentials updates (#21949)
    * Rewind Credentials: Report more specific errors on credentials updates
    
    Previously we had one generic message to indicate that we failed to
    update a site's credentials for Rewind.
    
    In this patch we're adding specific messages when the server reports a
    specific kind of failure condition.
    
    * Update text and add action buttons to notices
    
    * Update from PR feedback
    
    * Separate Happychat priming logic from rqeuest fetcher
    
    * Open help screen in new tab/session
    
    * Make navigation more robust against failure

--- client/state/data-layer/wpcom/activity-log/update-credentials/index.js
+++ client/state/data-layer/wpcom/activity-log/update-credentials/index.js
@@ -1,14 +1,20 @@
 /** @format */
-
 /**
  * External dependencies
  */
-
 import i18n from 'i18n-calypso';
+import page from 'page';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies
  */
+import { getHappychatAuth } from 'state/happychat/utils';
+import hasActiveHappychatSession from 'state/happychat/selectors/has-active-happychat-session';
+import isHappychatAvailable from 'state/happychat/selectors/is-happychat-available';
+import isHappychatConnectionUninitialized from 'state/happychat/selectors/is-happychat-connection-uninitialized';
+import { initConnection } from 'state/happychat/connection/actions';
+import { openChat } from 'state/happychat/ui/actions';
 import { http } from 'state/data-layer/wpcom-http/actions';
 import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
 import {
@@ -21,8 +27,27 @@ import {
 import { successNotice, errorNotice } from 'state/notices/actions';
 import { transformApi } from 'state/data-layer/wpcom/sites/rewind/api-transformer';
 
+const navigateTo =
+	undefined !== typeof window ? path => window.open( path, '_blank' ) : path => page( path );
+
+/**
+ * Makes sure that we can initialize a connection
+ * to HappyChat. We'll need this on the API response
+ *
+ * @param {function} dispatch Redux dispatcher
+ * @param {function} getState Redux getState
+ */
+export const primeHappychat = ( { dispatch, getState } ) => {
+	const state = getState();
+	const getAuth = getHappychatAuth( state );
+
+	if ( isHappychatConnectionUninitialized( state ) ) {
+		dispatch( initConnection( getAuth() ) );
+	}
+};
+
 export const request = ( { dispatch }, action ) => {
-	const notice = successNotice( i18n.translate( 'Testing connection…' ), { duration: 4000 } );
+	const notice = successNotice( i18n.translate( 'Testing connection…' ), { duration: 30000 } );
 	const { notice: { noticeId } } = notice;
 
 	dispatch( notice );
@@ -76,21 +101,89 @@ export const success = ( { dispatch }, action, { rewind_state } ) => {
 	} catch ( e ) {}
 };
 
-export const failure = ( { dispatch }, action, error ) => {
+export const failure = ( { dispatch, getState }, action, error ) => {
 	dispatch( {
 		type: JETPACK_CREDENTIALS_UPDATE_FAILURE,
 		error,
 		siteId: action.siteId,
 	} );
 
+	const getHelp = () => {
+		const state = getState();
+		const canChat = isHappychatAvailable( state ) || hasActiveHappychatSession( state );
+
+		return canChat ? dispatch( openChat() ) : navigateTo( '/help' );
+	};
+
+	const { translate } = i18n;
+	const [ errorMessage, extraOptions ] = get(
+		{
+			service_unavailable: () => [
+				translate(
+					"Our service isn't working at the moment. " +
+						"We'll get it up and running as fast as we can, so please try again later."
+				),
+				{
+					button: translate( 'Try again' ),
+					onClick: () => dispatch( action ),
+				},
+			],
+			missing_args: () => [
+				translate( 'Something seems to be missing — please fill out all the required fields.' ),
+				null,
+			],
+			invalid_args: () => [
+				translate(
+					'The information you entered seems to be incorrect. ' +
+						"Let's take another look to ensure everything is in the right place."
+				),
+				null,
+			],
+			invalid_credentials: () => [
+				translate(
+					"Oops! We couldn't connect to your site with these credentials " +
+						"— let's give it another try."
+				),
+				null,
+			],
+			invalid_wordpress_path: () => [
+				translate(
+					'We looked for `wp-config.php` in the WordPress installation path you provided ' +
+						"but couldn't find it."
+				),
+				{
+					button: translate( 'Get help' ),
+					onClick: getHelp,
+				},
+			],
+			read_only_install: () => [
+				translate(
+					'It looks like your server is read-only. ' +
+						'To create backups and rewind your site, we need permission to write to your server.'
+				),
+				{
+					button: translate( 'Get help' ),
+					onClick: getHelp,
+				},
+			],
+			unreachable_path: () => [
+				translate(
+					'We tried to access your WordPress installation through its publicly available URL, ' +
+						"but it didn't work. Please make sure the directory is accessible and try again."
+				),
+				null,
+			],
+		},
+		error.error,
+		() => [ translate( 'Error saving. Please check your credentials and try again.' ), null ]
+	)();
+
+	const baseOptions = { duration: 4000, id: action.noticeId };
 	dispatch(
-		errorNotice( i18n.translate( 'Error saving. Please check your credentials and try again.' ), {
-			duration: 4000,
-			id: action.noticeId,
-		} )
+		errorNotice( errorMessage, extraOptions ? { ...baseOptions, ...extraOptions } : baseOptions )
 	);
 };
 
 export default {
-	[ JETPACK_CREDENTIALS_UPDATE ]: [ dispatchRequest( request, success, failure ) ],
+	[ JETPACK_CREDENTIALS_UPDATE ]: [ primeHappychat, dispatchRequest( request, success, failure ) ],
 };
