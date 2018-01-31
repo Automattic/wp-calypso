5fab2689827c8c8978b23217969195c73ad3d486
Julian de Bhal <julian.debhal@automattic.com>
2018-01-31 10:53:45 +1000

    fix validation schemas initialization

--- client/state/domains/management/validation-schemas/reducer.js
+++ client/state/domains/management/validation-schemas/reducer.js
@@ -5,7 +5,7 @@
  */
 import { DOMAIN_MANAGEMENT_VALIDATION_SCHEMA_ADD } from 'state/action-types';
 
-export const validationSchemas = ( state = null, action ) => {
+export const validationSchemas = ( state = {}, action ) => {
 	switch ( action.type ) {
 		case DOMAIN_MANAGEMENT_VALIDATION_SCHEMA_ADD:
 			return { ...state, ...action.schemas };
