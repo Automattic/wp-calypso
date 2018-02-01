09f2eb3693805e2fe460a0752799f8afd4d398ab
Jonathan Sadowski <sadowski@automattic.com>
2018-01-31 19:40:35 -0600

    Add tests for didResendSucceed selector

--- client/state/invites/test/reducer.js
+++ client/state/invites/test/reducer.js
@@ -291,32 +291,32 @@ describe( 'reducer', () => {
 				}
 			);
 			expect( state ).to.eql( {
-				12345: { '123456asdf789': true },
+				12345: { '123456asdf789': 'requesting' },
 			} );
 		} );
 
 		test( 'should accumulate invites', () => {
-			const original = deepFreeze( { 12345: { '123456asdf789': false } } );
+			const original = deepFreeze( { 12345: { '123456asdf789': 'success' } } );
 			const state = requestingResend( original, {
 				type: INVITE_RESEND_REQUEST,
 				siteId: 12345,
 				inviteId: '789lkjh123456',
 			} );
 			expect( state ).to.eql( {
-				12345: { '123456asdf789': false, '789lkjh123456': true },
+				12345: { '123456asdf789': 'success', '789lkjh123456': 'requesting' },
 			} );
 		} );
 
 		test( 'should accumulate sites', () => {
-			const original = deepFreeze( { 12345: { '123456asdf789': false } } );
+			const original = deepFreeze( { 12345: { '123456asdf789': 'success' } } );
 			const state = requestingResend( original, {
 				type: INVITE_RESEND_REQUEST,
 				siteId: 67890,
 				inviteId: '789lkjh123456',
 			} );
 			expect( state ).to.eql( {
-				12345: { '123456asdf789': false },
-				67890: { '789lkjh123456': true },
+				12345: { '123456asdf789': 'success' },
+				67890: { '789lkjh123456': 'requesting' },
 			} );
 		} );
 	} );
