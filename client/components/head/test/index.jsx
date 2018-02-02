01e2cd033d1dd2afe2203dd6d43946db63c9849f
Markus Dobmann <markus@mpnjs.com>
2018-02-02 15:06:58 +0100

    Head: use jest assertions

--- client/components/head/test/index.jsx
+++ client/components/head/test/index.jsx
@@ -6,7 +6,6 @@
 /**
  * External dependencies
  */
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import React from 'react';
 import merge from 'lodash/merge';
@@ -31,14 +30,16 @@ describe( 'Head', () => {
 		const props = createProps();
 		const wrapper = shallow( <Head { ...props } /> );
 
-		expect( wrapper.find( 'title' ) ).to.have.text( 'WordPress.com' );
+		expect( wrapper.find( 'title' ).text() ).toBe( 'WordPress.com' );
+		expect( wrapper ).toMatchSnapshot();
 	} );
 
 	test( 'should render custom title', () => {
 		const props = createProps( { title: 'Arbitrary Custom Title' } );
 		const wrapper = shallow( <Head { ...props } /> );
 
-		expect( wrapper.find( 'title' ) ).to.have.text( 'Arbitrary Custom Title' );
+		expect( wrapper.find( 'title' ).text() ).toBe( 'Arbitrary Custom Title' );
+		expect( wrapper ).toMatchSnapshot();
 	} );
 
 	test( "render use 'faviconURL' for relevant <link /> tags ", () => {
@@ -47,11 +48,11 @@ describe( 'Head', () => {
 
 		expect(
 			wrapper.find( { rel: 'shortcut icon', type: 'image/vnd.microsoft.icon' } ).prop( 'href' )
-		).to.equal( props.faviconURL );
-		expect(
-			wrapper.find( { rel: 'shortcut icon', type: 'image/x-icon' } ).prop( 'href' )
-		).to.equal( props.faviconURL );
-		expect( wrapper.find( { rel: 'icon', type: 'image/x-icon' } ).prop( 'href' ) ).to.equal(
+		).toEqual( props.faviconURL );
+		expect( wrapper.find( { rel: 'shortcut icon', type: 'image/x-icon' } ).prop( 'href' ) ).toEqual(
+			props.faviconURL
+		);
+		expect( wrapper.find( { rel: 'icon', type: 'image/x-icon' } ).prop( 'href' ) ).toEqual(
 			props.faviconURL
 		);
 	} );
@@ -61,12 +62,12 @@ describe( 'Head', () => {
 		const wrapper = shallow( <Head { ...props } /> );
 
 		wrapper.find( { rel: 'icon', type: 'image/png' } ).map( link => {
-			expect( link.prop( 'href' ) ).to.match( /^\/\/s1.wp.com/ );
+			expect( link.prop( 'href' ) ).toMatch( /^\/\/s1.wp.com/ );
 		} );
 		wrapper.find( { rel: 'apple-touch-icon' } ).map( link => {
-			expect( link.prop( 'href' ) ).to.match( /^\/\/s1.wp.com/ );
+			expect( link.prop( 'href' ) ).toMatch( /^\/\/s1.wp.com/ );
 		} );
 
-		expect( wrapper.find( 'link[id="noticons-css"]' ).prop( 'href' ) ).to.match( /^\/\/s1.wp.com/ );
+		expect( wrapper.find( 'link[id="noticons-css"]' ).prop( 'href' ) ).toMatch( /^\/\/s1.wp.com/ );
 	} );
 } );
