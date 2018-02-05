fd8135f704a8681269ee48876c185e2fbbb85ad8
Andrija Vucinic <andrija.vucinic@gmail.com>
2018-02-05 22:35:51 +0100

    Checkout: Thank You page for delayed transfers
    We want to update thank you pages for delayed transfers. They have different messaging and links.
    - add thank you for only NUX transfer purchase
    - differenty copy on the nux plan page when transfer bought

--- client/lib/products-values/index.js
+++ client/lib/products-values/index.js
@@ -262,6 +262,10 @@ export function isDomainTransferPrivacy( product ) {
 	return product.product_slug === domainProductSlugs.TRANSFER_IN_PRIVACY;
 }
 
+export function isDelayedDomainTransfer( product ) {
+	return isDomainTransfer( product ) && product.delayedProvisioning;
+}
+
 export function isBundled( product ) {
 	product = formatProduct( product );
 	assertValidProduct( product );
@@ -405,6 +409,7 @@ export default {
 	isCredits,
 	isCustomDesign,
 	isDependentProduct,
+	isDelayedDomainTransfer,
 	isDomainMapping,
 	isDomainProduct,
 	isDomainRedemption,
