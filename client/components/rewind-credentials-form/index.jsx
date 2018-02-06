e80201fc18ee8c6e4c5c9a63b5c43d2ad83a4a8a
Michael Arestad <marestad@gmail.com>
2018-02-06 15:28:05 -0700

    RewindCredentialsForm - updated advanced settings spacing and added explanation text (#21948)
    * RewindCredentialsForm - updated advanced settings spacing and added explanation text
    
    * RewindCredentialsForm - put help text in correct spot this time. D'oh!

--- client/components/rewind-credentials-form/index.jsx
+++ client/components/rewind-credentials-form/index.jsx
@@ -205,15 +205,16 @@ export class RewindCredentialsForm extends Component {
 						disabled={ formIsSubmitting }
 						onClick={ this.toggleAdvancedSettings }
 						borderless={ true }
+						primary={ true }
 						className="rewind-credentials-form__advanced-button"
 					>
 						{ translate( 'Advanced settings' ) }
 					</Button>
 					{ showAdvancedSettings && (
-						<div>
+						<div className="rewind-credentials-form__advanced-settings">
 							<FormFieldset className="rewind-credentials-form__path">
 								<FormLabel htmlFor="wordpress-path">
-									{ translate( 'WordPress Installation Path' ) }
+									{ translate( 'WordPress installation path' ) }
 								</FormLabel>
 								<FormTextInput
 									name="path"
@@ -236,6 +237,9 @@ export class RewindCredentialsForm extends Component {
 									disabled={ formIsSubmitting }
 									className="rewind-credentials-form__private-key"
 								/>
+								<p className="form-setting-explanation">
+									{ translate( 'Only non-encrypted private keys are supported.' ) }
+								</p>
 							</FormFieldset>
 						</div>
 					) }
