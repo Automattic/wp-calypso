d12dc6358cfd61ba0b379046379b2e21c77819f1
Markus Dobmann <markus@mpnjs.com>
2018-02-06 17:15:18 +0100

    Render 'desktop' template with JSX

--- server/render/index.js
+++ server/render/index.js
@@ -160,7 +160,7 @@ export function serverRender( req, res ) {
 	context.config = config.ssrConfig;
 
 	if ( config.isEnabled( 'desktop' ) ) {
-		res.render( 'desktop', context );
+		res.send( renderJsx( 'desktop', context ) );
 		return;
 	}
 
