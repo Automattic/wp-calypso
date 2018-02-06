04714117147d5ddf8a0d23946c9d1c91ac3ce59b
Bernhard Reiter <ockham@raz.or.at>
2018-02-06 13:57:06 +0000

    Pass steps, flat

--- client/jetpack-onboarding/steps/summary-todo-links.jsx
+++ client/jetpack-onboarding/steps/summary-todo-links.jsx
@@ -12,7 +12,7 @@ import { map } from 'lodash';
  */
 import { getEditorNewPostPath } from 'state/ui/editor/selectors';
 
-const TodoLinks = ( { steps } ) =>
+const TodoLinks = steps =>
 	map( steps, ( { label, url }, stepName ) => (
 		<div key={ stepName } className="steps__summary-entry todo">
 			<a href={ url }>{ label }</a>
@@ -20,39 +20,35 @@ const TodoLinks = ( { steps } ) =>
 	) );
 
 export const TodoLinksConnected = connect( ( state, { siteId, siteSlug, translate } ) => ( {
-	steps: {
-		THEME: {
-			label: translate( 'Choose a Theme' ),
-			url: '/themes/' + siteSlug,
-		},
-		PAGES: {
-			label: translate( 'Add additional pages' ),
-			url: getEditorNewPostPath( state, siteId, 'page' ),
-		},
-		BLOG: {
-			label: translate( 'Write your first blog post' ),
-			url: getEditorNewPostPath( state, siteId, 'post' ),
-		},
+	THEME: {
+		label: translate( 'Choose a Theme' ),
+		url: '/themes/' + siteSlug,
+	},
+	PAGES: {
+		label: translate( 'Add additional pages' ),
+		url: getEditorNewPostPath( state, siteId, 'page' ),
+	},
+	BLOG: {
+		label: translate( 'Write your first blog post' ),
+		url: getEditorNewPostPath( state, siteId, 'post' ),
 	},
 } ) )( TodoLinks );
 
 export const TodoLinksUnconnected = connect( ( state, { siteUrl, translate } ) => ( {
-	steps: {
-		JETPACK_CONNECTION: {
-			label: translate( 'Connect to WordPress.com' ),
-			url: '/jetpack/connect?url=' + siteUrl,
-		},
-		THEME: {
-			label: translate( 'Choose a Theme' ),
-			url: siteUrl + '/wp-admin/theme-install.php?browse=featured',
-		},
-		PAGES: {
-			label: translate( 'Add additional pages' ),
-			url: siteUrl + '/wp-admin/post-new.php?post_type=page',
-		},
-		BLOG: {
-			label: translate( 'Write your first blog post' ),
-			url: siteUrl + '/wp-admin/post-new.php',
-		},
+	JETPACK_CONNECTION: {
+		label: translate( 'Connect to WordPress.com' ),
+		url: '/jetpack/connect?url=' + siteUrl,
+	},
+	THEME: {
+		label: translate( 'Choose a Theme' ),
+		url: siteUrl + '/wp-admin/theme-install.php?browse=featured',
+	},
+	PAGES: {
+		label: translate( 'Add additional pages' ),
+		url: siteUrl + '/wp-admin/post-new.php?post_type=page',
+	},
+	BLOG: {
+		label: translate( 'Write your first blog post' ),
+		url: siteUrl + '/wp-admin/post-new.php',
 	},
 } ) )( TodoLinks );
