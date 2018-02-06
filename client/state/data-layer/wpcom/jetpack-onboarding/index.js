baf9ca00995fdd7fb03b8bf0146f5d06e756b50b
Marin Atanasov <tyxla@abv.bg>
2018-02-06 15:27:18 +0200

    State: Add a handleSaveSuccess clarification

--- client/state/data-layer/wpcom/jetpack-onboarding/index.js
+++ client/state/data-layer/wpcom/jetpack-onboarding/index.js
@@ -109,6 +109,10 @@ export const saveJetpackOnboardingSettings = ( { dispatch, getState }, action ) 
 	);
 };
 
+// Although we don't use the save success action in any of the reducers,
+// we need to dispatch some action in order to signal to the data layer that
+// the save request has finished. Tracking those requests is necessary for
+// displaying an up to date progress for some steps.
 export const handleSaveSuccess = ( { dispatch }, { siteId, settings } ) =>
 	dispatch( saveJetpackOnboardingSettingsSuccess( siteId, settings ) );
 
