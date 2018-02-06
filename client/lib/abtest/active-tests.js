346a02215e98755cdbd23ab7c928bf73a2274cea
Matt Husby <matthusby@gmail.com>
2018-02-06 12:42:05 -0500

    Add group 3 to the exact match filter and move where the vendor gets set.

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -92,12 +92,12 @@ export default {
 		allowExistingUsers: true,
 	},
 	domainSuggestionTestV5: {
-		datestamp: '20180105',
+		datestamp: '20180204',
 		variations: {
 			group_0: 1, // Default group
-			group_1: 1000, // Domainsbot control
-			group_2: 1000, // Domainsbot test
-			group_3: 1000, // Verisign
+			group_1: 1000,
+			group_2: 1000,
+			group_3: 1000,
 		},
 		defaultVariation: 'group_0',
 		allowExistingUsers: true,
