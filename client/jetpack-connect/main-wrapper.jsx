1506d3816e4a17afcf65d7af589ba059235e0e30
Jon Surrell <jon.surrell@automattic.com>
2018-02-06 19:17:52 +0100

    Start branding the authorization page

--- client/jetpack-connect/main-wrapper.jsx
+++ client/jetpack-connect/main-wrapper.jsx
@@ -9,15 +9,28 @@ import classNames from 'classnames';
 /**
  * Internal dependencies
  */
+import JetpackLogo from 'components/jetpack-logo';
 import Main from 'components/main';
+import { isEnabled } from 'config';
 import { retrieveMobileRedirect } from './persistence-utils';
 
 const JetpackConnectMainWrapper = ( { isWide, className, children } ) => {
+	const jetpackBranded = isEnabled( 'jetpack/connection-rebranding' );
 	const wrapperClassName = classNames( 'jetpack-connect__main', {
 		'is-wide': isWide,
 		'is-mobile-app-flow': !! retrieveMobileRedirect(),
+		'jetpack-branded': jetpackBranded,
 	} );
-	return <Main className={ classNames( className, wrapperClassName ) }>{ children }</Main>;
+	return (
+		<Main className={ classNames( className, wrapperClassName ) }>
+			{ jetpackBranded && (
+				<div className="jetpack-connect__main-logo">
+					<JetpackLogo full size={ 100 /* @todo get real size from designs */ } />
+				</div>
+			) }
+			{ children }
+		</Main>
+	);
 };
 
 JetpackConnectMainWrapper.propTypes = {
