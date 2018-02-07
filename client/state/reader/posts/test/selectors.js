b7a33d2e51ab9771eb01ebbc2bb5a513568d4418
Jake F - @samouri <samouri@users.noreply.github.com>
2018-02-07 16:46:23 -0500

    remove outdated test

--- client/state/reader/posts/test/selectors.js
+++ client/state/reader/posts/test/selectors.js
@@ -1,29 +1,10 @@
 /** @format */
 /**
- * External dependencies
- */
-import { expect } from 'chai';
-
-/**
  * Internal dependencies
  */
-import { getPost } from '../selectors';
 
 describe( 'selectors', () => {
-	describe( '#getPost()', () => {
-		test( 'should return undefined if there is no match', () => {
-			const post = getPost(
-				{
-					reader: {
-						posts: {
-							items: {},
-						},
-					},
-				},
-				'nope'
-			);
-
-			expect( post ).to.eql( undefined );
-		} );
-	} );
+	// TODO: write these
+	describe( '#getPostById()', () => {} );
+	describe( '#getPostByKey()', () => {} );
 } );
