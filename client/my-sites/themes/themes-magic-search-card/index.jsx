bff85a76c4ee77214cd3b658601699fc71138f8c
Mark Uraine <uraine@gmail.com>
2018-02-07 13:22:35 -0800

    Show search filters by default
    Show search filters by default. Fixes #22228
    
    Removed top margin of filters
    
    There was a `margin:5px` that was added when the search field had focus. I've removed that so the filters don't jump down 5px when there is focus as suggested by @mattsherman.
    
    Removed styles that hid and showed keyed-suggestions
    
    Removed mobile arg when showing clear input icon. This way the clear input icon shows for mobile as well.

--- client/my-sites/themes/themes-magic-search-card/index.jsx
+++ client/my-sites/themes/themes-magic-search-card/index.jsx
@@ -304,19 +304,18 @@ class ThemesMagicSearchCard extends React.Component {
 						onClick={ this.handleClickInside }
 					>
 						{ searchField }
-						{ ! isMobile() &&
-							this.state.searchInput !== '' && (
-								<div className="themes-magic-search-card__icon">
-									<Gridicon
-										icon="cross"
-										className="themes-magic-search-card__icon-close"
-										tabIndex="0"
-										onClick={ this.clearSearch }
-										aria-controls={ 'search-component-magic-search' }
-										aria-label={ translate( 'Clear Search' ) }
-									/>
-								</div>
-							) }
+						{ this.state.searchInput !== '' && (
+							<div className="themes-magic-search-card__icon">
+								<Gridicon
+									icon="cross"
+									className="themes-magic-search-card__icon-close"
+									tabIndex="0"
+									onClick={ this.clearSearch }
+									aria-controls={ 'search-component-magic-search' }
+									aria-label={ translate( 'Clear Search' ) }
+								/>
+							</div>
+						) }
 						{ isPremiumThemesEnabled &&
 							showTierThemesControl && (
 								<SegmentedControl
