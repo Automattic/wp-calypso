558de26c36954391f72404b743b290899fb6f403
Jon Surrell <jon.surrell@automattic.com>
2018-02-07 21:16:17 +0100

    Don't import from tests for client code

--- client/blocks/daily-post-button/docs/example.jsx
+++ client/blocks/daily-post-button/docs/example.jsx
@@ -10,12 +10,23 @@ import React from 'react';
  * Internal dependencies
  */
 import DailyPostButton from 'blocks/daily-post-button';
-import { dailyPromptPost } from 'blocks/daily-post-button/test/fixtures';
-
 const DailyPostButtonExample = () => {
 	return (
 		<div className="design-assets__group">
-			<DailyPostButton post={ dailyPromptPost } />
+			<DailyPostButton
+				post={ {
+					site_ID: 489937,
+					tags: {
+						'daily prompts': {
+							slug: 'daily-prompts-2',
+						},
+					},
+					type: 'dp_prompt',
+					title: 'Crisis',
+					URL: 'https://dailypost.wordpress.com/2016/07/27/crisis/',
+					short_url: 'http://wp.me/p23sd-12Mf',
+				} }
+			/>
 		</div>
 	);
 };
