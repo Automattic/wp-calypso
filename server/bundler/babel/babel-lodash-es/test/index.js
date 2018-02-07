1bfd0a023f2909de5f9cbdf5b4ce26ec425b3028
Jarda Snajdr <jsnajdr@gmail.com>
2018-02-07 22:26:26 +0100

    Rewrite tests to not use snapshots

--- server/bundler/babel/babel-lodash-es/test/index.js
+++ server/bundler/babel/babel-lodash-es/test/index.js
@@ -13,27 +13,27 @@ describe( 'babel-lodash-es', () => {
 	}
 
 	test( 'should transform named import from top-level package', () => {
-		const code = "import { get } from 'lodash-es'";
-		expect( transform( code ) ).toMatchSnapshot();
+		const code = transform( "import { get } from 'lodash-es';" );
+		expect( code ).toBe( "import { get } from 'lodash';" );
 	} );
 
 	test( 'should transform default import from top-level package', () => {
-		const code = "import _ from 'lodash-es'";
-		expect( transform( code ) ).toMatchSnapshot();
+		const code = transform( "import _ from 'lodash-es';" );
+		expect( code ).toBe( "import _ from 'lodash';" );
 	} );
 
 	test( 'should transform default import from a submodule', () => {
-		const code = "import get from 'lodash-es/get'";
-		expect( transform( code ) ).toMatchSnapshot();
+		const code = transform( "import get from 'lodash-es/get'" );
+		expect( code ).toBe( "import { get } from 'lodash';" );
 	} );
 
 	test( 'should transform aliased default import from a submodule', () => {
-		const code = "import theGet from 'lodash-es/get'";
-		expect( transform( code ) ).toMatchSnapshot();
+		const code = transform( "import theGet from 'lodash-es/get';" );
+		expect( code ).toBe( "import { get as theGet } from 'lodash';" );
 	} );
 
 	test( 'should report error on a non-default import from a submodule', () => {
 		const code = "import { get } from 'lodash-es/get'";
-		expect( () => transform( code ) ).toThrowErrorMatchingSnapshot();
+		expect( () => transform( code ) ).toThrow( /Could not transform a non-default import/ );
 	} );
 } );
