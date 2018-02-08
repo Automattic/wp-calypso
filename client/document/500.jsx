01d8515969d037600478a9c1413e12f73ba8829b
Markus Dobmann <markus@mpnjs.com>
2018-02-08 20:01:15 +0100

    500: cleanup / use EmptyContent component

--- client/document/500.jsx
+++ client/document/500.jsx
@@ -4,46 +4,38 @@
  * @format
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 
 /**
  * Internal dependencies
  */
-import Head from '../components/head';
+import Head from 'components/head';
+import EmptyContent from 'components/empty-content';
 
-class ServerError extends Component {
-	render() {
-		const { urls, faviconURL } = this.props;
-		return (
-			<html lang="en">
-				<Head faviconURL={ faviconURL } cdn={ '//s1.wp.com' }>
-					<link rel="stylesheet" id="main-css" href={ urls[ 'style.css' ] } type="text/css" />
-				</Head>
-				<body>
-					{ /* eslint-disable wpcalypso/jsx-classname-namespace*/ }
-					<div id="wpcom" className="wpcom-site">
-						<div className="wp has-no-sidebar">
-							<div className="layout__content" id="content">
-								<div className="layout__primary" id="primary">
-									<div className="empty-content">
-										<img
-											src="/calypso/images/illustrations/illustration-500.svg"
-											alt="500 - Server error"
-											className="empty-content__illustration"
-										/>
-										<h2 className="empty-content__title">
-											We're sorry, but an unexpected error has occurred
-										</h2>
-									</div>
-								</div>
+function ServerError( { urls, faviconURL } ) {
+	return (
+		<html lang="en">
+			<Head faviconURL={ faviconURL } cdn={ '//s1.wp.com' }>
+				<link rel="stylesheet" id="main-css" href={ urls[ 'style.css' ] } type="text/css" />
+			</Head>
+			<body>
+				{ /* eslint-disable wpcalypso/jsx-classname-namespace*/ }
+				<div id="wpcom" className="wpcom-site">
+					<div className="wp has-no-sidebar">
+						<div className="layout__content" id="content">
+							<div className="layout__primary" id="primary">
+								<EmptyContent
+									illustration="/calypso/images/illustrations/illustration-500.svg"
+									title="We're sorry, but an unexpected error has occurred"
+								/>
 							</div>
 						</div>
 					</div>
-					{ /* eslint-enable wpcalypso/jsx-classname-namespace*/ }
-				</body>
-			</html>
-		);
-	}
+				</div>
+				{ /* eslint-enable wpcalypso/jsx-classname-namespace*/ }
+			</body>
+		</html>
+	);
 }
 
 export default ServerError;
