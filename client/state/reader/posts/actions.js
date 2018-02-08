821689546d8da74eced90ff35388da0a35b3dd84
Jake F - @samouri <samouri@users.noreply.github.com>
2018-02-07 20:12:21 -0500

    cleanup some of reader/posts/actions

--- client/state/reader/posts/actions.js
+++ client/state/reader/posts/actions.js
@@ -2,7 +2,7 @@
 /**
  * External Dependencies
  */
-import { filter, forEach, has, isUndefined, map, omit, partition, reject } from 'lodash';
+import { filter, forEach, compact } from 'lodash';
 
 /**
  * Internal dependencies
@@ -10,33 +10,11 @@ import { filter, forEach, has, isUndefined, map, omit, partition, reject } from 
 import { READER_POSTS_RECEIVE } from 'state/action-types';
 import analytics from 'lib/analytics';
 import { runFastRules, runSlowRules } from './normalization-rules';
-import Dispatcher from 'dispatcher';
-import { action } from 'lib/feed-post-store/constants';
 import wpcom from 'lib/wp';
+import { keyForPost } from 'lib/feed-stream-store/post-key';
 
 function trackRailcarRender( post ) {
 	analytics.tracks.recordEvent( 'calypso_traintracks_render', post.railcar );
-}
-
-export function postToKey( post ) {
-	if ( post && post.feed_ID && post.feed_item_ID ) {
-		return {
-			feedId: post.feed_ID,
-			postId: post.feed_item_ID,
-		};
-	}
-
-	if ( post.is_external ) {
-		return {
-			feedId: post.feed_ID,
-			postId: post.ID,
-		};
-	}
-
-	return {
-		postId: post.ID,
-		blogId: post.site_ID,
-	};
 }
 
 function fetchForKey( postKey ) {
@@ -49,93 +27,69 @@ function fetchForKey( postKey ) {
 	return wpcom.undocumented().readFeedPost( postKey );
 }
 
-export function fetchPost( postKey ) {
-	return dispatch =>
-		fetchForKey( postKey ).then(
-			data => dispatch( receivePosts( [ data ] ) ),
-			err =>
-				dispatch( {
-					type: READER_POSTS_RECEIVE,
-					posts: [
-						{
-							feed_ID: postKey.feedId,
-							ID: postKey.postId,
-							site_ID: postKey.blogId,
-							is_external: ! postKey.blogId,
-							is_error: true,
-							global_ID: `${ postKey.feedId || 'na' }-${ postKey.blogId || 'na' }-${
-								postKey.postId
-							}`,
-							error: err,
-						},
-					],
-				} )
-		);
-}
-
-export function reloadPost( post ) {
-	return dispatch => {
-		// keep track of any railcars we might have
-		const railcar = post.railcar;
-		return fetchForKey( postToKey( post ) ).then( data => {
-			data.railcar = railcar;
-			return dispatch( receivePosts( [ data ] ) );
-		} );
-	};
-}
-
 /**
  * Returns an action object to signal that post objects have been received.
  *
  * @param  {Array}  posts Posts received
  * @return {Object} Action object
  */
-export function receivePosts( posts ) {
-	return function( dispatch ) {
-		if ( ! posts ) {
-			return Promise.resolve( [] );
-		}
+export const receivePosts = posts => dispatch => {
+	if ( ! posts ) {
+		return Promise.resolve( [] );
+	}
 
-		const withoutUndefined = reject( posts, isUndefined );
-		const normalizedPosts = map( withoutUndefined, runFastRules );
+	const normalizedPosts = compact( posts ).map( runFastRules );
 
-		const [ postsToReload, postsToProcess ] = partition( normalizedPosts, '_should_reload' );
-		forEach( postsToReload, post => {
-			delete post._should_reload;
-			dispatch( reloadPost( post ) );
-		} );
+	// save the posts after running the fast rules
+	dispatch( {
+		type: READER_POSTS_RECEIVE,
+		posts: normalizedPosts,
+	} );
 
-		forEach( map( postsToProcess, runSlowRules ), slowPromise => {
-			slowPromise.then( post => {
-				dispatch( {
-					type: READER_POSTS_RECEIVE,
-					posts: [ post ],
-				} );
-
-				// keep the old feed post store in sync
-				Dispatcher.handleServerAction( {
-					data: post,
-					type: action.RECEIVE_NORMALIZED_FEED_POST,
-				} );
-			} );
-		} );
-
+	// also save them after running the slow rules
+	Promise.all( normalizedPosts.map( runSlowRules ) ).then( processedPosts => {
 		dispatch( {
 			type: READER_POSTS_RECEIVE,
-			posts: normalizedPosts,
+			posts: [ processedPosts ],
 		} );
+	} );
 
-		// keep the old feed post store in sync
-		forEach( normalizedPosts, post => {
-			const postForFlux = has( post, '_should_reload' ) ? omit( post, '_should_reload' ) : post;
-			Dispatcher.handleServerAction( {
-				data: postForFlux,
-				type: action.RECEIVE_NORMALIZED_FEED_POST,
-			} );
+	forEach( filter( normalizedPosts, 'railcar' ), trackRailcarRender );
+
+	// TODO: resolve weird dependency between related-posts and the return here
+	return Promise.resolve( normalizedPosts );
+};
+
+export const fetchPost = postKey => dispatch => {
+	return fetchForKey( postKey )
+		.then( data => dispatch( receivePosts( [ data ] ) ) )
+		.catch( error => dispatch( receiveErrorForPostKey( error, postKey ) ) );
+};
+
+export function reloadPost( post ) {
+	return dispatch => {
+		// keep track of any railcars we might have
+		const railcar = post.railcar;
+		return fetchForKey( keyForPost( post ) ).then( data => {
+			data.railcar = railcar;
+			return dispatch( receivePosts( [ data ] ) );
 		} );
+	};
+}
 
-		forEach( filter( postsToProcess, 'railcar' ), trackRailcarRender );
-
-		return Promise.resolve( normalizedPosts );
+function receiveErrorForPostKey( error, postKey ) {
+	return {
+		type: READER_POSTS_RECEIVE,
+		posts: [
+			{
+				feed_ID: postKey.feedId,
+				ID: postKey.postId,
+				site_ID: postKey.blogId,
+				is_external: ! postKey.blogId,
+				global_ID: `${ postKey.feedId || 'na' }-${ postKey.blogId || 'na' }-${ postKey.postId }`,
+				is_error: true,
+				error,
+			},
+		],
 	};
 }
--- client/state/reader/posts/test/actions.js
+++ client/state/reader/posts/test/actions.js
@@ -47,42 +47,6 @@ describe( 'actions', () => {
 		readSiteStub.reset();
 	} );
 
-	describe( '#postToKey', () => {
-		test( 'should return a feed key for an external post', () => {
-			expect(
-				actions.postToKey( {
-					feed_ID: 1,
-					site_ID: 1,
-					ID: 3,
-					feed_item_ID: 3,
-					is_external: true,
-				} )
-			).to.deep.equal( { feedId: 1, postId: 3 } );
-		} );
-
-		test( 'should return an blog id key for an internal post', () => {
-			expect(
-				actions.postToKey( {
-					site_ID: 2,
-					ID: 4,
-					is_external: false,
-				} )
-			).to.deep.equal( { blogId: 2, postId: 4 } );
-		} );
-
-		test( 'should return a feed key for a post with both a feed id and a site id', () => {
-			expect(
-				actions.postToKey( {
-					feed_ID: 1,
-					site_ID: 2,
-					ID: 4,
-					feed_item_ID: 3,
-					is_external: false,
-				} )
-			).to.deep.equal( { feedId: 1, postId: 3 } );
-		} );
-	} );
-
 	describe( '#receivePosts()', () => {
 		test( 'should return an action object and dispatch posts receive', () => {
 			const posts = [];
@@ -107,21 +71,6 @@ describe( 'actions', () => {
 			];
 			actions.receivePosts( posts )( dispatchSpy );
 			expect( trackingSpy ).to.have.been.calledWith( 'calypso_traintracks_render', 'foo' );
-		} );
-
-		test( 'should try to reload posts marked with should_reload', () => {
-			const posts = [
-				{
-					ID: 1,
-					site_ID: 1,
-					global_ID: 1,
-					railcar: '1234',
-					_should_reload: true,
-				},
-			];
-
-			actions.receivePosts( posts )( dispatchSpy );
-			expect( dispatchSpy ).to.have.been.calledWith( sinon.match.func );
 		} );
 	} );
 
