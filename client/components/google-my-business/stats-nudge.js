8340b2d2ffb537bbe06de30242fa4a4b4583187d
Caleb Bauermeister <cbauerman@gmail.com>
2018-02-08 15:10:19 -0800

    Refine Nudge content and add all device styling.

--- client/components/google-my-business/stats-nudge.js
+++ client/components/google-my-business/stats-nudge.js
@@ -9,25 +9,42 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal Dependencies
  */
-import Card from 'components/card';
 import Button from 'components/button';
+import Card from 'components/card';
+import SectionHeader from 'components/section-header';
 
 export const StatsNudge = ( { translate } ) => {
 	return (
-		<Card className="google-my-business__stats-nudge">
-			<h2 className="google-my-business__stats-nudge-heading">
-				{ translate( 'Is your business listed on Google?' ) }
-			</h2>
-
-			<p className="google-my-business__stats-nudge-description">
-				{ translate( 'Google my business etc.' ) }
-			</p>
-
-			<Button className="google-my-business__stats-nudge-button">
-				{ translate( 'Drive More Traffic' ) }
-			</Button>
-			<Button className="google-my-business__stats-nudge-button">{ translate( 'Not Now' ) }</Button>
-		</Card>
+		<div>
+			<SectionHeader
+				className="google-my-business__stats-nudge-header"
+				label={ translate( 'We recommend Google My Business' ) }
+				href="#"
+			/>
+			<Card className="google-my-business__stats-nudge">
+				<div className="google-my-business__stats-nudge-image-wrapper">
+					<img
+						className="google-my-business__stats-nudge-image"
+						src="/calypso/images/google-my-business/phone-screenshot.png"
+						alt="Your business with Google My Business"
+					/>
+				</div>
+				<div className="google-my-business__stats-nudge-info">
+					<h1 className="google-my-business__stats-nudge-heading">
+						{ translate( 'Is your business listed on Google?' ) }
+					</h1>
+					<h2 className="google-my-business__stats-nudge-description">
+						{ translate(
+							'Google My Business is a fast and free way to drive traffic to your local business.'
+						) }
+					</h2>
+					<div className="google-my-business__stats-nudge-button-row">
+						<Button primary>{ translate( 'Drive More Traffic' ) }</Button>
+						<Button>{ translate( 'Not Now' ) }</Button>
+					</div>
+				</div>
+			</Card>
+		</div>
 	);
 };
 
