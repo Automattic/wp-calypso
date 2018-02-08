e545880f484a6b67f94dbc3cd88c3762ae52c26f
Caleb Bauermeister <cbauerman@gmail.com>
2018-02-08 10:49:33 -0800

    Add basic nudge content and rough styling

--- client/components/google-my-business/stats-nudge.js
+++ client/components/google-my-business/stats-nudge.js
@@ -4,14 +4,31 @@
  * External dependencies
  */
 import React from 'react';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal Dependencies
  */
 import Card from 'components/card';
+import Button from 'components/button';
 
-export const StatsNudge = ( {} ) => {
-	return <Card>Try Google My Business</Card>;
+export const StatsNudge = ( { translate } ) => {
+	return (
+		<Card className="google-my-business__stats-nudge">
+			<h2 className="google-my-business__stats-nudge-heading">
+				{ translate( 'Is your business listed on Google?' ) }
+			</h2>
+
+			<p className="google-my-business__stats-nudge-description">
+				{ translate( 'Google my business etc.' ) }
+			</p>
+
+			<Button className="google-my-business__stats-nudge-button">
+				{ translate( 'Drive More Traffic' ) }
+			</Button>
+			<Button className="google-my-business__stats-nudge-button">{ translate( 'Not Now' ) }</Button>
+		</Card>
+	);
 };
 
-export default StatsNudge;
+export default localize( StatsNudge );
