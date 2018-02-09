05e14754d81c902686af620910dd99a9d4beab82
Copons <copons@users.noreply.github.com>
2018-02-09 12:21:49 +0000

    Ad createElement logic

--- client/my-sites/comments/comment/comment-html-editor.jsx
+++ client/my-sites/comments/comment/comment-html-editor.jsx
@@ -6,7 +6,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { localize } from 'i18n-calypso';
 import classNames from 'classnames';
-import { noop } from 'lodash';
+import { each } from 'lodash';
 
 /**
  * Internal dependencies
@@ -28,7 +28,56 @@ export class CommentHtmlEditor extends Component {
 
 	storeTextareaRef = textarea => ( this.textarea = textarea );
 
-	isTagOpen = tag => ! tag;
+	isTagOpen = tag => {
+		const { commentContent } = this.props;
+		const openers = commentContent.match( new RegExp( `<${ tag }.*?>`, 'gi' ) ) || [];
+		const closers = commentContent.match( new RegExp( `<\/${ tag }>`, 'gi' ) ) || [];
+		return openers.length > closers.length;
+	};
+
+	setCursorPosition = ( selectionEnd, insertedContentLength ) => {
+		this.textarea.selectionEnd = this.textarea.selectionStart =
+			selectionEnd + insertedContentLength;
+	};
+
+	splitSelectedContent = () => {
+		const { selectionEnd, selectionStart, value } = this.textarea;
+		return {
+			before: value.substring( 0, selectionStart ),
+			inner: value.substring( selectionStart, selectionEnd ),
+			after: value.substring( selectionEnd, value.length ),
+		};
+	};
+
+	insertHtml = ( tag, attributes ) => {
+		const element = document.createElement( tag );
+		const isOpen = this.isTagOpen( tag );
+		const { inner } = this.splitSelectedContent();
+
+		if ( ! isOpen ) {
+			each( attributes, ( value, key ) => element.setAttribute( key, value ) );
+		}
+
+		if ( inner.length ) {
+			element.innerHTML = inner;
+			return this.insertContent( element.outerHTML );
+		}
+
+		element.innerHTML = '<!---->';
+		const [ opener, closer ] = element.outerHTML.split( '<!---->' );
+
+		if ( isOpen ) {
+			return this.insertContent( closer );
+		}
+		return this.insertContent( opener );
+	};
+
+	insertContent = content => {
+		this.textarea.focus();
+		document.execCommand( 'insertText', false, content );
+	};
+
+	insertStrongTag = () => this.insertHtml( 'strong' );
 
 	render() {
 		const { commentContent, disabled, onChange } = this.props;
@@ -44,7 +93,7 @@ export class CommentHtmlEditor extends Component {
 						compact
 						disabled={ disabled }
 						key="strong"
-						onClick={ noop }
+						onClick={ this.insertStrongTag }
 					>
 						b
 					</Button>
