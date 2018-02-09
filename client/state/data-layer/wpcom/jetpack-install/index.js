262d3af82ea5fd40503d582203f190eade341ba8
Steve Seear <steve.seear@gmail.com>
2018-02-09 22:12:39 +0000

    Data-layer handlers for jetpack-install api request

---
+++ client/state/data-layer/wpcom/jetpack-install/index.js
@@ -0,0 +1,46 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { http } from 'state/data-layer/wpcom-http/actions';
+import {
+	updateJetpackRemoteInstallError,
+	jetpackRemoteInstallComplete,
+} from 'state/jetpack-remote-install/actions';
+import { JETPACK_REMOTE_INSTALL } from 'state/action-types';
+
+const requestRemoteInstall = ( { dispatch }, action ) => {
+	const { url, user, password } = action;
+	return dispatch(
+		http(
+			{
+				method: 'POST',
+				path: '/jetpack-install/' + url,
+				query: {
+					user,
+					password,
+				},
+			},
+			action
+		)
+	);
+};
+
+const handleResponse = ( { dispatch }, { url }, data ) => {
+	if ( data.status ) {
+		return dispatch( jetpackRemoteInstallComplete( url ) );
+	}
+	dispatch( updateJetpackRemoteInstallError( url, data.error ) );
+};
+
+const handleError = ( { dispatch }, { url } ) => {
+	dispatch( updateJetpackRemoteInstallError( url, 'API_ERROR' ) );
+};
+
+export default {
+	[ JETPACK_REMOTE_INSTALL ]: [
+		dispatchRequest( requestRemoteInstall, handleResponse, handleError ),
+	],
+};
