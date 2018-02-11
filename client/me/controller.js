97ef61f5e47f73e7f041ffa7a6d3d363210c4216
Yury Michurin <yury.michurin@gmail.com>
2018-02-11 14:24:31 +0200

    Please the linter gods

--- client/me/controller.js
+++ client/me/controller.js
@@ -38,7 +38,7 @@ export default {
 		context.store.dispatch( setTitle( i18n.translate( 'My Profile', { textOnly: true } ) ) ); // FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
 
 		analytics.pageView.record( basePath, ANALYTICS_PAGE_TITLE + ' > My Profile' );
-		
+
 		const ProfileComponent = require( 'me/profile' );
 
 		context.primary = React.createElement( ProfileComponent, {
