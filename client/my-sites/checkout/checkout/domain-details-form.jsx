312a5b0f1dd652f2c898f784a303f32b1e53d2f8
Del Putnam <del@putnams.net>
2018-02-12 14:26:54 -0500

    Only show message if contact details are auto-filled.

--- client/my-sites/checkout/checkout/domain-details-form.jsx
+++ client/my-sites/checkout/checkout/domain-details-form.jsx
@@ -89,6 +89,7 @@ export class DomainDetailsForm extends PureComponent {
 
 		this.state = {
 			form: null,
+			hasAutoFilledContactDetails: !! this.props.contactDetails,
 			submissionCount: 0,
 			phoneCountryCode: 'US',
 			steps,
@@ -603,11 +604,12 @@ export class DomainDetailsForm extends PureComponent {
 			title = this.props.translate( 'G Suite Account Information' );
 		} else {
 			title = this.props.translate( 'Domain Contact Information' );
-			message = this.props.translate(
-				'For your convenience, we have pre-filled your WordPress.com ' +
-					"contact information. Please review this to be sure it's the correct information you want " +
-					'to use for this domain.'
-			);
+			message =
+				this.state.hasAutoFilledContactDetails &&
+				this.props.translate(
+					'For your convenience, we have pre-filled your WordPress.com contact information. Please ' +
+						"review this to be sure it's the correct information you want to use for this domain."
+				);
 		}
 
 		const renderPrivacy =
