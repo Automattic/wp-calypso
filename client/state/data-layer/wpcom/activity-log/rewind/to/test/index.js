974e0a36a241f77c708dc951d138f5453d874412
Dennis Snell <dennis.snell@automattic.com>
2018-02-12 14:08:53 -0500

    fix broken test

--- client/state/data-layer/wpcom/activity-log/rewind/to/test/index.js
+++ client/state/data-layer/wpcom/activity-log/rewind/to/test/index.js
@@ -1,12 +1,5 @@
 /** @format */
 /**
- * External dependencies
- */
-import { expect } from 'chai';
-import deepFreeze from 'deep-freeze';
-import sinon from 'sinon';
-
-/**
  * Internal dependencies
  */
 import { receiveRestoreSuccess } from '../';
@@ -16,16 +9,10 @@ const siteId = 77203074;
 const timestamp = 1496768464;
 const restoreId = 12345;
 
-const SUCCESS_RESPONSE = deepFreeze( {
-	ok: true,
-	error: '',
-	restore_id: restoreId,
-} );
-
 describe( 'receiveRestoreSuccess', () => {
 	test( 'should dispatch get restore progress on success', () => {
-		const dispatch = sinon.spy();
-		receiveRestoreSuccess( { dispatch }, { siteId, timestamp }, SUCCESS_RESPONSE );
-		expect( dispatch ).to.have.been.calledWith( getRewindRestoreProgress( siteId, restoreId ) );
+		expect( receiveRestoreSuccess( { siteId, timestamp }, restoreId ) ).toEqual(
+			getRewindRestoreProgress( siteId, restoreId )
+		);
 	} );
 } );
