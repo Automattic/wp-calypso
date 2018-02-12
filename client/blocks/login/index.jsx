50d4854a0ce714a9ceb6c620da728fb6a34f3560
Jon Surrell <jon.surrell@automattic.com>
2018-02-12 17:31:32 +0100

    Gate Jetpack branding behind feature flag

--- client/blocks/login/index.jsx
+++ client/blocks/login/index.jsx
@@ -37,6 +37,7 @@ import PushNotificationApprovalPoller from './two-factor-authentication/push-not
 import userFactory from 'lib/user';
 import SocialConnectPrompt from './social-connect-prompt';
 import JetpackLogo from 'components/jetpack-logo';
+import { isEnabled } from 'config';
 
 const user = userFactory();
 
@@ -188,7 +189,7 @@ class Login extends Component {
 					</p>
 				);
 			}
-		} else if ( isJetpack ) {
+		} else if ( isJetpack && isEnabled( 'jetpack/connection-rebranding' ) ) {
 			headerText = translate( 'Log in to your WordPress.com account to set up Jetpack.' );
 			preHeader = (
 				<div>
@@ -272,8 +273,13 @@ class Login extends Component {
 	}
 
 	render() {
+		const { isJetpack } = this.props;
 		return (
-			<div className={ classNames( 'login', { 'is-jetpack': this.props.isJetpack } ) }>
+			<div
+				className={ classNames( 'login', {
+					'is-jetpack': isJetpack && isEnabled( 'jetpack/connection-rebranding' ),
+				} ) }
+			>
 				{ this.renderHeader() }
 
 				<ErrorNotice />
