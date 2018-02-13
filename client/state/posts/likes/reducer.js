cdf4beb0e0cc66e4dec309f7a79e69cb3f4d3cc7
Ben Lowery <ben.lowery@automattic.com>
2018-02-13 15:45:35 -0500

    progress

--- client/state/posts/likes/reducer.js
+++ client/state/posts/likes/reducer.js
@@ -3,14 +3,21 @@
 /**
  * External dependencies
  */
-import { pick } from 'lodash';
+
+import { get, pick } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import itemsSchema from './schema';
 import { combineReducers, createReducer } from 'state/utils';
-import { POST_LIKES_RECEIVE } from 'state/action-types';
+import {
+	POST_LIKES_ADD_LIKER,
+	POST_LIKES_RECEIVE,
+	POST_LIKES_REMOVE_LIKER,
+	POST_LIKE,
+	POST_UNLIKE,
+} from 'state/action-types';
 
 /**
  * Returns the updated items state after an action has been dispatched. The
@@ -38,6 +45,32 @@ export const items = createReducer(
 				},
 			};
 		},
+		[ POST_LIKE ]: ( state, { siteId, postId } ) => {
+			const currentLike = get( state, [ siteId, postId ], {
+				likes: undefined,
+				iLike: false,
+				found: 0,
+			} );
+
+			if ( currentLike.iLike ) {
+				return state;
+			}
+
+			return {
+				...state,
+				[ siteId ]: {
+					...state[ siteId ],
+					[ postId ]: {
+						likes: undefined,
+						iLike: true,
+						found: currentLike.found + 1,
+					},
+				},
+			};
+		},
+		[ POST_UNLIKE ]: ( state, { siteId, postId } ) => {},
+		[ POST_LIKES_ADD_LIKER ]: ( state, { siteId, postId, likeCount, liker } ) => {},
+		[ POST_LIKES_REMOVE_LIKER ]: ( state, { siteId, postId, likeCount, liker } ) => {},
 	},
 	itemsSchema
 );
