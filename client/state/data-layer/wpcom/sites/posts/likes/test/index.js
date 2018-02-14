c470f0d27261dc2f37fbe2a1925b3646c0f179b4
Ben Lowery <blowery@users.noreply.github.com>
2018-02-14 13:36:29 -0500

    Post Likes: Create a data-layer for post likes (#22024)
    * Likes: Add in data layer for likes, supporting like, unlike, fetching current likes
    * Remove the old thunk based like fetcher, use the data layer and also clean up a bunch of cruft we don't need any more.

---
+++ client/state/data-layer/wpcom/sites/posts/likes/test/index.js
@@ -0,0 +1,58 @@
+/** @format */
+/**
+ * External Dependencies
+ */
+
+/**
+ * Internal Dependencies
+ */
+import { fetch, fromApi, onSuccess } from '../';
+import { POST_LIKES_REQUEST } from 'state//action-types';
+import { receiveLikes } from 'state/posts/likes/actions';
+
+describe( 'fromApi', () => {
+	test( 'transforms to standard output', () => {
+		expect(
+			fromApi( {
+				found: 45,
+				likes: [],
+			} )
+		).toEqual( {
+			found: 45,
+			likes: [],
+			iLike: false,
+		} );
+
+		expect(
+			fromApi( {
+				found: '45',
+				likes: [],
+				i_like: true,
+			} )
+		).toEqual( {
+			found: 45,
+			likes: [],
+			iLike: true,
+		} );
+	} );
+} );
+
+describe( 'fetch', () => {
+	it( 'should return an http action with the proper path', () => {
+		const action = fetch( {
+			type: POST_LIKES_REQUEST,
+			siteId: 1,
+			postId: 1,
+		} );
+		expect( action ).toHaveProperty( 'method', 'GET' );
+		expect( action ).toHaveProperty( 'path', '/sites/1/posts/1/likes' );
+		expect( action ).toHaveProperty( 'query.apiVersion', '1.1' );
+	} );
+} );
+
+describe( 'onSuccess', () => {
+	it( 'should return a receiveLikes action with the data', () => {
+		const data = {};
+		expect( onSuccess( { siteId: 1, postId: 1 }, data ) ).toEqual( receiveLikes( 1, 1, data ) );
+	} );
+} );
