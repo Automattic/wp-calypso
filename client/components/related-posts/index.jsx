31cdad0a5248aebed1108e77f2fd7914db48a611
Jan Cavan Boulas <janmcavan@gmail.com>
2018-02-14 09:08:32 -0800

    Reader: Rename reader-related-card-v2 to reader-related-card (#22406)
    * Reader: Rename reader-related-card-v2 to reader-related-card
    
    * Add eslint ignore, remove remaining v2 instance

--- client/components/related-posts/index.jsx
+++ client/components/related-posts/index.jsx
@@ -14,7 +14,7 @@ import classnames from 'classnames';
  */
 import { relatedPostsForPost } from 'state/reader/related-posts/selectors';
 import { SCOPE_SAME, SCOPE_OTHER } from 'state/reader/related-posts/utils';
-import RelatedPost from 'blocks/reader-related-card-v2';
+import RelatedPost from 'blocks/reader-related-card';
 import QueryReaderRelatedPosts from 'components/data/query-reader-related-posts';
 
 function RelatedPosts( {
@@ -33,9 +33,11 @@ function RelatedPosts( {
 		// Placeholders
 		listItems = times( 2, i => {
 			return (
-				<li className="reader-related-card-v2__list-item" key={ 'related-post-placeholder-' + i }>
+				/* eslint-disable */
+				<li className="reader-related-card__list-item" key={ 'related-post-placeholder-' + i }>
 					<RelatedPost post={ null } />
 				</li>
+				/* eslint-enable */
 			);
 		} );
 	} else if ( posts.length === 0 ) {
@@ -43,19 +45,23 @@ function RelatedPosts( {
 	} else {
 		listItems = posts.map( post_id => {
 			return (
-				<li key={ post_id } className="reader-related-card-v2__list-item">
+				/* eslint-disable */
+				<li key={ post_id } className="reader-related-card__list-item">
 					<RelatedPost post={ post_id } onPostClick={ onPostClick } onSiteClick={ onSiteClick } />
 				</li>
+				/* eslint-enable */
 			);
 		} );
 	}
 
 	return (
-		<div className={ classnames( 'reader-related-card-v2__blocks', className ) }>
+		/* eslint-disable */
+		<div className={ classnames( 'reader-related-card__blocks', className ) }>
 			{ ! posts && <QueryReaderRelatedPosts siteId={ siteId } postId={ postId } scope={ scope } /> }
-			<h1 className="reader-related-card-v2__heading">{ title }</h1>
-			<ul className="reader-related-card-v2__list">{ listItems }</ul>
+			<h1 className="reader-related-card__heading">{ title }</h1>
+			<ul className="reader-related-card__list">{ listItems }</ul>
 		</div>
+		/* eslint-enable */
 	);
 }
 
