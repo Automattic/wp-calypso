163f30c7f7cfa5a8b8302ea536db385076ee93f6
Caleb Bauermeister <cbauerman@gmail.com>
2018-02-15 12:28:40 -0800

    Refactor GMBStatsNudge as class.

--- client/components/google-my-business/stats-nudge.js
+++ client/components/google-my-business/stats-nudge.js
@@ -3,8 +3,9 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
 import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
 
 /**
  * Internal Dependencies
@@ -13,42 +14,48 @@ import Button from 'components/button';
 import DismissibleCard from 'blocks/dismissible-card';
 import SectionHeader from 'components/section-header';
 
-const GMBStatsNudge = ( { translate } ) => {
-	return (
-		<DismissibleCard
-			className="google-my-business__stats-nudge"
-			preferenceName="show-google-my-business-nudge"
-			temporary
-		>
-			<SectionHeader
-				className="google-my-business__stats-nudge-header"
-				label={ translate( 'Recommendation from WordPress.com' ) }
-			/>
-			<div className="google-my-business__stats-nudge-body">
-				<div className="google-my-business__stats-nudge-image-wrapper">
-					<img
-						className="google-my-business__stats-nudge-image"
-						src="/calypso/images/google-my-business/phone-screenshot.png"
-						alt={ translate( 'Your business with Google My Business' ) }
-					/>
-				</div>
-				<div className="google-my-business__stats-nudge-info">
-					<h1 className="google-my-business__stats-nudge-title">
-						{ translate( 'Reach more customers with Google My Business' ) }
-					</h1>
-					<h2 className="google-my-business__stats-nudge-description">
-						{ translate(
-							'Show up when customers search for businesses like yours on Google Search and Maps.'
-						) }
-					</h2>
-					<div className="google-my-business__stats-nudge-button-row">
-						<Button primary>{ translate( 'Start Now' ) }</Button>
-						<Button>{ translate( "I've Already Listed" ) }</Button>
+class GMBStatsNudge extends Component {
+	static propTypes = {
+		translate: PropTypes.func.isRequired,
+	};
+
+	render() {
+		return (
+			<DismissibleCard
+				className="google-my-business__stats-nudge"
+				preferenceName="show-google-my-business-nudge"
+				temporary
+			>
+				<SectionHeader
+					className="google-my-business__stats-nudge-header"
+					label={ this.props.translate( 'Recommendation from WordPress.com' ) }
+				/>
+				<div className="google-my-business__stats-nudge-body">
+					<div className="google-my-business__stats-nudge-image-wrapper">
+						<img
+							className="google-my-business__stats-nudge-image"
+							src="/calypso/images/google-my-business/phone-screenshot.png"
+							alt={ this.props.translate( 'Your business with Google My Business' ) }
+						/>
+					</div>
+					<div className="google-my-business__stats-nudge-info">
+						<h1 className="google-my-business__stats-nudge-title">
+							{ this.props.translate( 'Reach more customers with Google My Business' ) }
+						</h1>
+						<h2 className="google-my-business__stats-nudge-description">
+							{ this.props.translate(
+								'Show up when customers search for businesses like yours on Google Search and Maps.'
+							) }
+						</h2>
+						<div className="google-my-business__stats-nudge-button-row">
+							<Button primary>{ this.props.translate( 'Start Now' ) }</Button>
+							<Button>{ this.props.translate( "I've Already Listed" ) }</Button>
+						</div>
 					</div>
 				</div>
-			</div>
-		</DismissibleCard>
-	);
-};
+			</DismissibleCard>
+		);
+	}
+}
 
 export default localize( GMBStatsNudge );
