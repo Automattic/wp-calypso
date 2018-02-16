e6442d76a8723ff8a1ab6b37550dbeb5ea151551
Shaun Andrews <shaun@automattic.com>
2018-02-16 14:40:57 -0500

    Editor: Default the editor’s preview to desktop (#22456)

--- client/post-editor/post-editor.jsx
+++ client/post-editor/post-editor.jsx
@@ -442,7 +442,6 @@ export const PostEditor = createReactClass( {
 							postId={ this.props.postId }
 							externalUrl={ this.getExternalUrl() }
 							editUrl={ this.props.editPath }
-							defaultViewportDevice={ this.state.isPostPublishPreview ? 'computer' : 'tablet' }
 							revision={ get( this.state, 'post.revisions.length', 0 ) }
 						/>
 					) : null }
