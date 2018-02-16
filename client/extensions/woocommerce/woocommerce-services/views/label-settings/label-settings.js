da6466e3d218773e0ab263ca906de21faabd3441
Paul Dechov <dechovsky@gmail.com>
2018-02-16 10:20:46 -0500

    Show reloading placeholder for entire credit card list

--- client/extensions/woocommerce/woocommerce-services/views/label-settings/label-settings.js
+++ client/extensions/woocommerce/woocommerce-services/views/label-settings/label-settings.js
@@ -233,10 +233,13 @@ class ShippingLabels extends Component {
 				{ this.renderPaymentPermissionNotice() }
 				<p className="label-settings__credit-card-description">{ description }</p>
 
-				{ ! isReloading ? null : (
+				{ isReloading ? (
 					<div className="label-settings__placeholder">
 						<PaymentMethod selected={ false } isLoading={ true } />
+						<PaymentMethod selected={ false } isLoading={ true } />
 					</div>
+				) : (
+					paymentMethods.map( renderPaymentMethod )
 				) }
 				<QueryStoredCards />
 				{ paymentMethods.map( renderPaymentMethod ) }
