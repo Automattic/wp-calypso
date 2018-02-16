d86612ffd1658a68978ab02dcd235f14bdb1c7f2
Markus Dobmann <markus@mpnjs.com>
2018-02-16 20:13:21 +0100

    SupportUser: drop pug in favor of jsx (#22296)
    * SupportUser: drop pug in favor of jsx
    
    * Eliminate spaces, use tabs instead
    
    * extract 'supportUserFn' to improve dx
    
    * Use window.JSON.stringify explicitly
    
    * SupportUser: sanitize user provided input
    
    * SupportUser: ensure jsx for any response

--- server/pages/index.js
+++ server/pages/index.js
@@ -520,17 +520,17 @@ module.exports = function() {
 		} );
 
 		if ( calypsoEnv === 'development' ) {
-			return res.render( 'support-user', {
-				authorized: true,
-				supportUser: req.query.support_user,
-				supportToken: req.query._support_token,
-			} );
+			return res.send(
+				renderJsx( 'support-user', {
+					authorized: true,
+					supportUser: req.query.support_user,
+					supportToken: req.query._support_token,
+				} )
+			);
 		}
 
 		if ( ! config.isEnabled( 'wpcom-user-bootstrap' ) || ! req.cookies.wordpress_logged_in ) {
-			return res.render( 'support-user', {
-				authorized: false,
-			} );
+			return res.send( renderJsx( 'support-user' ) );
 		}
 
 		// Maybe not logged in, note that you need docker to test this properly
@@ -546,26 +546,24 @@ module.exports = function() {
 					domain: '.wordpress.com',
 				} );
 
-				return res.render( 'support-user', {
-					authorized: false,
-				} );
+				return res.send( renderJsx( 'support-user' ) );
 			}
 
 			const activeFlags = get( data, 'meta.data.flags.active_flags', [] );
 
 			// A8C check
 			if ( ! includes( activeFlags, 'calypso_support_user' ) ) {
-				return res.render( 'support-user', {
-					authorized: false,
-				} );
+				return res.send( renderJsx( 'support-user' ) );
 			}
 
 			// Passed all checks, prepare support user session
-			return res.render( 'support-user', {
-				authorized: true,
-				supportUser: req.query.support_user,
-				supportToken: req.query._support_token,
-			} );
+			return res.send(
+				renderJsx( 'support-user', {
+					authorized: true,
+					supportUser: req.query.support_user,
+					supportToken: req.query._support_token,
+				} )
+			);
 		} );
 	} );
 
