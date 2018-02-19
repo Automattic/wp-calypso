3d501b1659f31d51c3865bc129970ce9dc0f2b44
Steve Seear <steve.seear@gmail.com>
2018-02-19 17:53:48 +0000

    Tweak jsdoc

--- client/state/selectors/get-jetpack-remote-install-error.js
+++ client/state/selectors/get-jetpack-remote-install-error.js
@@ -10,9 +10,9 @@ import { get } from 'lodash';
  * a remote install of the jetpack plugin on the .org
  * site at the given url.
  *
- * @param {Object} state - Global state tree
- * @param {String} url - .org site URL
- * @return {?String} - Error code, if any
+ * @param {Object} state Global state tree
+ * @param {String} url .org site URL
+ * @return {?String} Error code, if any
  */
 export default function getJetpackRemoteInstallError( state, url ) {
 	return get( state.jetpackRemoteInstall.error, url, null );
