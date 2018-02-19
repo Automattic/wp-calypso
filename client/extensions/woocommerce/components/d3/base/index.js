cc5935d1e47717ef6d715963908c17c056021023
Paul Sealock <psealock@gmail.com>
2018-02-19 23:47:46 +1300

    Stats: Add d3js base class

---
+++ client/extensions/woocommerce/components/d3/base/index.js
@@ -0,0 +1,78 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { select as d3Select } from 'd3-selection';
+
+export default class D3Base extends Component {
+	static propTypes = {
+		className: PropTypes.string,
+		drawChart: PropTypes.func.isRequired,
+		getParams: PropTypes.func.isRequired,
+	};
+
+	constructor( props ) {
+		super( props );
+		this.updateParams = this.updateParams.bind( this );
+	}
+
+	state = {};
+
+	componentDidMount() {
+		window.addEventListener( 'resize', this.updateParams );
+		this.updateParams();
+	}
+
+	componentWillReceiveProps() {
+		this.updateParams();
+	}
+
+	componentDidUpdate() {
+		this.draw();
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener( 'resize', this.updateParams );
+		delete this.node;
+	}
+
+	updateParams() {
+		const { getParams } = this.props;
+		this.setState( getParams( this.node ), this.draw );
+	}
+
+	draw() {
+		this.props.drawChart( this.createNewContext(), this.state );
+	}
+
+	createNewContext() {
+		const { className } = this.props;
+		const { width, height } = this.state;
+
+		d3Select( this.node )
+			.selectAll( 'svg' )
+			.remove();
+		const newNode = d3Select( this.node )
+			.append( 'svg' )
+			.attr( 'class', `${ className }__viewbox` )
+			.attr( 'viewBox', `0 0 ${ width } ${ height }` )
+			.attr( 'preserveAspectRatio', 'xMidYMid meet' )
+			.append( 'g' );
+		return newNode;
+	}
+
+	setNodeRef = node => {
+		this.node = node;
+	};
+
+	render() {
+		return (
+			<div className={ classNames( 'd3-base', this.props.className ) } ref={ this.setNodeRef } />
+		);
+	}
+}
