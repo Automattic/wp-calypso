ea2385e653aacdea7865af3c0398815c54743a60
Yury Michurin <yury.michurin@gmail.com>
2018-02-20 10:13:56 +0200

    Add proptypes

--- client/my-sites/google-my-business/select-business-type/cta-card.js
+++ client/my-sites/google-my-business/select-business-type/cta-card.js
@@ -5,6 +5,7 @@
  */
 import React from 'react';
 import Gridicon from 'gridicons';
+import PropTypes from 'prop-types';
 
 /**
  * Internal dependencies
@@ -12,18 +13,26 @@ import Gridicon from 'gridicons';
 import Card from 'components/card';
 import Button from 'components/button';
 
-const CTACard = props => (
+const CTACard = ( { headerText, mainText, buttonPrimary, buttonText, buttonIcon } ) => (
 	<Card className="select-business-type__cta-card">
 		<div className="select-business-type__cta-card-main">
-			<h2>{ props.headerText }</h2>
-			<p>{ props.mainText }</p>
+			<h2>{ headerText }</h2>
+			<p>{ mainText }</p>
 		</div>
 		<div className="select-business-type__cta-card-button-container">
-			<Button primary={ props.buttonPrimary }>
-				{ props.buttonText } <Gridicon icon={ props.buttonIcon } />
+			<Button primary={ buttonPrimary }>
+				{ buttonText } <Gridicon icon={ buttonIcon } />
 			</Button>
 		</div>
 	</Card>
 );
+
+CTACard.propTypes = {
+	headerText: PropTypes.string.isRequired,
+	mainText: PropTypes.string.isRequired,
+	buttonPrimary: PropTypes.bool,
+	buttonText: PropTypes.string.isRequired,
+	buttonIcon: PropTypes.string.isRequired,
+};
 
 export default CTACard;
