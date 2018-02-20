1ce90dfc492b52362eccc3b9dfb4221145e88e98
Marin Atanasov <tyxla@abv.bg>
2018-02-20 17:10:39 +0200

    Jetpack Onboarding: Add empty stats step

---
+++ client/jetpack-onboarding/steps/stats.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { localize } from 'i18n-calypso';
+
+class JetpackOnboardingStatsStep extends React.Component {
+	render() {
+		return <div className="steps__main" />;
+	}
+}
+
+export default localize( JetpackOnboardingStatsStep );
