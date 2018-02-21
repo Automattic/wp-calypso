22c6dd5c358d53a827227d38f705e6cd844793cb
Ernesto Méndez <mendezcode@gmail.com>
2018-02-21 14:13:32 -0400

    Add theme showcase banners. (#21983)
    Implement the `ThemesBanner` component, as well as the soon to be used Photo Blog and Small Business banners.

--- client/my-sites/themes/themes-banner/index.jsx
+++ client/my-sites/themes/themes-banner/index.jsx
@@ -3,34 +3,100 @@
 /**
  * External dependencies
  */
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import cssSafeUrl from 'lib/css-safe-url';
-import Button from 'components/button';
 
-export class ThemesBanner extends PureComponent {
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+
+import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
+import Button from 'components/button';
+import safeImageUrl from 'lib/safe-image-url';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { recordTracksEvent } from 'state/analytics/actions';
+import { getActiveTheme, getThemeDetailsUrl } from 'state/themes/selectors';
+
+class ThemesBanner extends PureComponent {
 	static propTypes = {
-		title: PropTypes.string.isRequired,
-		description: PropTypes.string.isRequired,
-		action: PropTypes.func,
-		actionLabel: PropTypes.string.isRequired,
-		backgroundImage: PropTypes.string,
-		href: PropTypes.string,
+		themeId: PropTypes.string,
+		themeName: PropTypes.string,
+		title: PropTypes.node.isRequired,
+		description: PropTypes.node.isRequired,
+		backgroundColor: PropTypes.string,
+		image: PropTypes.string,
+		imageWidth: PropTypes.number,
+		imageTransform: PropTypes.string,
+
+		// Connected props
+		siteId: PropTypes.number,
+		themeUrl: PropTypes.string,
+		activeThemeId: PropTypes.string,
+	};
+
+	recordEvent = () => {
+		const { siteId, themeId, activeThemeId } = this.props;
+		const tracksData = {
+			site_id: siteId,
+			theme: themeId,
+			active_theme: activeThemeId,
+		};
+		recordTracksEvent( 'calypso_showcase_banner_click', tracksData );
 	};
 
 	render() {
-		const { title, description, actionLabel, action, backgroundImage, href } = this.props;
-		const backgroundStyle = backgroundImage
-			? { backgroundImage: `url( ${ cssSafeUrl( backgroundImage ) } )` }
-			: null;
+		const {
+			title,
+			description,
+			backgroundColor,
+			image,
+			imageWidth,
+			imageTransform = 'auto',
+			themeName,
+			themeUrl,
+			translate,
+		} = this.props;
+		const backgroundStyle = backgroundColor ? { backgroundColor } : {};
 		return (
-			<div className="themes-banner" style={ backgroundStyle }>
-				<h1>{ title }</h1>
-				<p>{ description }</p>
-				<Button compact primary onClick={ action } href={ href }>
-					{ actionLabel }
+			<a
+				className="themes-banner"
+				role="button"
+				style={ backgroundStyle }
+				onClick={ this.recordEvent }
+				href={ themeUrl }
+			>
+				<h1 className="themes-banner__title">{ title }</h1>
+				<p className="themes-banner__description">{ description }</p>
+				<Button className="themes-banner__cta" compact primary>
+					{ translate( 'See the theme' ) }
 				</Button>
-			</div>
+				{ image && (
+					<img
+						alt={ translate( '%(themeName)s Theme', {
+							args: { themeName },
+						} ) }
+						width={ imageWidth }
+						className="themes-banner__image"
+						src={ safeImageUrl( image ) }
+						style={ { transform: imageTransform } }
+					/>
+				) }
+			</a>
 		);
 	}
 }
+
+const mapStateToProps = ( state, { themeId } ) => {
+	const siteId = getSelectedSiteId( state );
+	const themeUrl = getThemeDetailsUrl( state, themeId, siteId );
+	const activeThemeId = getActiveTheme( state, siteId );
+	return {
+		siteId,
+		themeUrl,
+		activeThemeId,
+	};
+};
+
+export default connect( mapStateToProps )( localize( ThemesBanner ) );
