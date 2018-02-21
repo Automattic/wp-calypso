b611cec3d63f7b332c8887364a0264ba59b72aa8
Paul Sealock <psealock@gmail.com>
2018-02-21 22:28:17 +1300

    Revert "Stats: Add d3js base class"
    This reverts commit cc5935d1e47717ef6d715963908c17c056021023.

--- client/extensions/woocommerce/components/sparkline/test/index.jsx
+++ client/extensions/woocommerce/components/sparkline/test/index.jsx
@@ -16,8 +16,8 @@ describe( 'Sparkline', () => {
 	const shallowWithoutLifecycle = arg => shallow( arg, { disableLifecycleMethods: true } );
 
 	test( 'should allow data to be set.', () => {
-		const sparkline = <Sparkline data={ [ 1, 2, 3, 4, 5 ] } />;
-		assert.deepEqual( [ 1, 2, 3, 4, 5 ], sparkline.props.data );
+		const sparkline = shallowWithoutLifecycle( <Sparkline data={ [ 1, 2, 3, 4, 5 ] } /> );
+		assert.deepEqual( [ 1, 2, 3, 4, 5 ], sparkline.instance().props.data );
 	} );
 
 	test( 'should have sparkline class', () => {
@@ -34,44 +34,52 @@ describe( 'Sparkline', () => {
 	} );
 
 	test( 'should have a default aspectRatio of 4.5', () => {
-		const sparkline = <Sparkline data={ [ 1, 2, 3, 4, 5 ] } />;
-		assert.equal( '4.5', sparkline.props.aspectRatio );
+		const sparkline = shallowWithoutLifecycle( <Sparkline data={ [ 1, 2, 3, 4, 5 ] } /> );
+		assert.equal( '4.5', sparkline.instance().props.aspectRatio );
 	} );
 
 	test( 'should have a defined aspectRatio if set', () => {
-		const sparkline = <Sparkline data={ [ 1, 2, 3, 4, 5 ] } aspectRatio={ 10 } />;
-		assert.equal( '10', sparkline.props.aspectRatio );
+		const sparkline = shallowWithoutLifecycle(
+			<Sparkline data={ [ 1, 2, 3, 4, 5 ] } aspectRatio={ 10 } />
+		);
+		assert.equal( '10', sparkline.instance().props.aspectRatio );
 	} );
 
 	test( 'should have a default highlightRadius of 3.5', () => {
-		const sparkline = <Sparkline data={ [ 1, 2, 3, 4, 5 ] } />;
-		assert.equal( '3.5', sparkline.props.highlightRadius );
+		const sparkline = shallowWithoutLifecycle( <Sparkline data={ [ 1, 2, 3, 4, 5 ] } /> );
+		assert.equal( '3.5', sparkline.instance().props.highlightRadius );
 	} );
 
 	test( 'should have a defined highlightRadius if set', () => {
-		const sparkline = <Sparkline data={ [ 1, 2, 3, 4, 5 ] } highlightRadius={ 10 } />;
-		assert.equal( '10', sparkline.props.highlightRadius );
+		const sparkline = shallowWithoutLifecycle(
+			<Sparkline data={ [ 1, 2, 3, 4, 5 ] } highlightRadius={ 10 } />
+		);
+		assert.equal( '10', sparkline.instance().props.highlightRadius );
 	} );
 
 	test( 'should have a default margin', () => {
-		const sparkline = <Sparkline data={ [ 1, 2, 3, 4, 5 ] } />;
-		assert.deepEqual( { top: 4, right: 4, bottom: 4, left: 4 }, sparkline.props.margin );
+		const sparkline = shallowWithoutLifecycle( <Sparkline data={ [ 1, 2, 3, 4, 5 ] } /> );
+		assert.deepEqual( { top: 4, right: 4, bottom: 4, left: 4 }, sparkline.instance().props.margin );
 	} );
 
 	test( 'should have a defined margin if set', () => {
-		const sparkline = (
+		const sparkline = shallowWithoutLifecycle(
 			<Sparkline data={ [ 1, 2, 3, 4, 5 ] } margin={ { top: 1, right: 1, bottom: 1, left: 1 } } />
 		);
-		assert.deepEqual( { top: 1, right: 1, bottom: 1, left: 1 }, sparkline.props.margin );
+		assert.deepEqual( { top: 1, right: 1, bottom: 1, left: 1 }, sparkline.instance().props.margin );
 	} );
 
 	test( 'should allow maxHeight to be defined.', () => {
-		const sparkline = <Sparkline data={ [ 1, 2, 3, 4, 5 ] } maxHeight={ 10 } />;
-		assert.equal( '10', sparkline.props.maxHeight );
+		const sparkline = shallowWithoutLifecycle(
+			<Sparkline data={ [ 1, 2, 3, 4, 5 ] } maxHeight={ 10 } />
+		);
+		assert.equal( '10', sparkline.instance().props.maxHeight );
 	} );
 
 	test( 'should allow highlightIndex to be defined.', () => {
-		const sparkline = <Sparkline data={ [ 1, 2, 3, 4, 5 ] } highlightIndex={ 10 } />;
-		assert.equal( '10', sparkline.props.highlightIndex );
+		const sparkline = shallowWithoutLifecycle(
+			<Sparkline data={ [ 1, 2, 3, 4, 5 ] } highlightIndex={ 10 } />
+		);
+		assert.equal( '10', sparkline.instance().props.highlightIndex );
 	} );
 } );
