1714c6d3ff8b338d5fc065e48c2e9228e14cb545
Ernesto Méndez <mendezcode@gmail.com>
2018-02-22 12:05:58 -0400

    Refactor to use componentDidMount()

--- client/my-sites/themes/themes-banner/random-themes-banner.jsx
+++ client/my-sites/themes/themes-banner/random-themes-banner.jsx
@@ -7,10 +7,6 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { sample } from 'lodash';
 
-const isClient = () => {
-	return !! global.window;
-};
-
 class RandomThemesBanner extends PureComponent {
 	static propTypes = {
 		banners: PropTypes.array.isRequired,
@@ -21,13 +17,29 @@ class RandomThemesBanner extends PureComponent {
 		const { banners } = props;
 		this.state = {
 			BannerComponent: sample( banners ),
+			isClient: false,
 		};
 	}
 
+	componentDidMount() {
+		// In this particular case, we need to disable the ESLint check
+		// to disable using `setState()` on `componentDidMount()`, since
+		// it needs to be done this way to properly render the component
+		// both on client-side and server-side.
+		const state = this.state;
+
+		// eslint-disable-next-line react/no-did-mount-set-state
+		this.setState( {
+			...state,
+			isClient: true,
+		} );
+	}
+
 	render() {
+		const { isClient, BannerComponent } = this.state;
+
 		// Client-side.
-		if ( isClient() ) {
-			const { BannerComponent } = this.state;
+		if ( isClient ) {
 			return <BannerComponent />;
 		}
 
