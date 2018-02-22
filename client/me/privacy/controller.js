cab497447149baa3bcfcfb6fce117989ec60fcb1
Benoit Person <benoit.person@gmail.com>
2018-02-22 12:22:44 +0100

    Remove `formBase` / `protectForm`

--- client/me/privacy/controller.js
+++ client/me/privacy/controller.js
@@ -8,12 +8,9 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import userSettings from 'lib/user-settings';
 import PrivacyComponent from 'me/privacy/main';
 
 export default function privacyController( context, next ) {
-	context.primary = React.createElement( PrivacyComponent, {
-		userSettings: userSettings,
-	} );
+	context.primary = React.createElement( PrivacyComponent );
 	next();
 }
--- client/me/privacy/main.jsx
+++ client/me/privacy/main.jsx
@@ -3,31 +3,17 @@
 /**
  * External dependencies
  */
-import PropTypes from 'prop-types';
-import React from 'react';
-import createReactClass from 'create-react-class';
+import React, { Component } from 'react';
 import { localize } from 'i18n-calypso';
 import { flowRight as compose } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import { protectForm } from 'lib/protect-form';
-import formBase from 'me/form-base';
 import DocumentHead from 'components/data/document-head';
 import Main from 'components/main';
 
-// NOTE: disabling it since we need to use the `formBase` mixin
-// eslint-disable-next-line react/prefer-es6-class
-const Privacy = createReactClass( {
-	displayName: 'Privacy',
-
-	mixins: [ formBase ],
-
-	propTypes: {
-		userSettings: PropTypes.object.isRequired,
-	},
-
+class Privacy extends Component {
 	render() {
 		return (
 			<Main className="privacy">
@@ -35,7 +21,7 @@ const Privacy = createReactClass( {
 				Privacy form incoming
 			</Main>
 		);
-	},
-} );
+	}
+}
 
-export default compose( localize, protectForm )( Privacy );
+export default compose( localize )( Privacy );
