48f22c2ca5795b60a8e1609d2cb483be6df1d7ad
Benoit Person <benoit.person@gmail.com>
2018-02-22 12:33:46 +0100

    Redirect `/me/privacy` to `/me` if feature flag is disabled

--- client/me/privacy/controller.js
+++ client/me/privacy/controller.js
@@ -4,13 +4,20 @@
  * External dependencies
  */
 import React from 'react';
+import page from 'page';
 
 /**
  * Internal dependencies
  */
+import config from 'config';
 import PrivacyComponent from 'me/privacy/main';
 
 export default function privacyController( context, next ) {
-	context.primary = React.createElement( PrivacyComponent );
+	if ( ! config.isEnabled( 'me/privacy' ) ) {
+		page.redirect( '/me' );
+	} else {
+		context.primary = React.createElement( PrivacyComponent );
+	}
+
 	next();
 }
