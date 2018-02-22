25f773fbb7b3b73f69103f48f2ebee39776b3ea4
Jarda Snajdr <jsnajdr@gmail.com>
2018-02-22 13:40:33 +0100

    Remove the isRequestingSiteUpdates selector

--- client/state/sites/updates/selectors.js
+++ client/state/sites/updates/selectors.js
@@ -1,9 +1,5 @@
 /** @format */
 
-export const isRequestingSiteUpdates = ( state, siteId ) => {
-	return state.sites.updates.requesting[ siteId ] || false;
-};
-
 export const getUpdatesBySiteId = ( state, siteId ) => {
 	return state.sites.updates.items[ siteId ] || null;
 };
--- client/state/sites/updates/test/selectors.js
+++ client/state/sites/updates/test/selectors.js
@@ -8,60 +8,9 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import { isRequestingSiteUpdates, getUpdatesBySiteId } from '../selectors';
+import { getUpdatesBySiteId } from '../selectors';
 
 describe( 'selectors', () => {
-	describe( '#isRequestingSiteUpdates()', () => {
-		test( 'should return false if site updates have not been fetched yet', () => {
-			const isRequesting = isRequestingSiteUpdates(
-				{
-					sites: {
-						updates: {
-							requesting: {},
-						},
-					},
-				},
-				12345678
-			);
-
-			expect( isRequesting ).to.be.false;
-		} );
-
-		test( 'should return false if the site updates are not being requested', () => {
-			const isRequesting = isRequestingSiteUpdates(
-				{
-					sites: {
-						updates: {
-							requesting: {
-								12345678: false,
-							},
-						},
-					},
-				},
-				12345678
-			);
-
-			expect( isRequesting ).to.be.false;
-		} );
-
-		test( 'should return true if the site updates are being requested', () => {
-			const isRequesting = isRequestingSiteUpdates(
-				{
-					sites: {
-						updates: {
-							requesting: {
-								12345678: true,
-							},
-						},
-					},
-				},
-				12345678
-			);
-
-			expect( isRequesting ).to.be.true;
-		} );
-	} );
-
 	describe( '#getUpdatesBySiteId()', () => {
 		test( 'should return null if site updates have not been fetched yet', () => {
 			const updates = getUpdatesBySiteId(
