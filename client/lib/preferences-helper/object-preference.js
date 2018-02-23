25b6d0f7db41b7056cec25a91a65f528f9934ae3
Caleb Bauermeister <cbauerman@gmail.com>
2018-02-22 17:36:09 -0800

    Add sub-preference classes for basic types.

---
+++ client/lib/preferences-helper/object-preference.js
@@ -0,0 +1,30 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
+
+class ObjectPreference extends Component {
+	static propTypes = {
+		name: PropTypes.string.isRequired,
+		value: PropTypes.object.isRequired,
+	};
+
+	render() {
+		const { value } = this.props;
+
+		return (
+			<ul className="preferences-helper__list">
+				{ Object.keys( value ).map( property => (
+					<li key={ property }>{ property + ': ' + value[ property ].toString() }</li>
+				) ) }
+			</ul>
+		);
+	}
+}
+
+export default connect( null, null )( localize( ObjectPreference ) );
--- client/lib/preferences-helper/preference.js
+++ client/lib/preferences-helper/preference.js
@@ -6,16 +6,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import { partial, isArray } from 'lodash';
+import { partial, isArray, isPlainObject } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { savePreference } from 'state/preferences/actions';
+import ArrayPreference from './array-preference';
+import ObjectPreference from './object-preference';
+import BooleanPreference from './boolean-preference';
+import StringPreference from './string-preference';
+import NumberPreference from './number-preference';
 
 class Preference extends Component {
 	render() {
 		const { name, value, translate, unsetPreference } = this.props;
+
+		let preferenceHandler = (
+			<ul>
+				<li key={ name }>{ value.toString() } </li>
+			</ul>
+		);
+
+		if ( isArray( value ) ) {
+			preferenceHandler = <ArrayPreference name={ name } value={ value } />;
+		} else if ( isPlainObject( value ) ) {
+			preferenceHandler = <ObjectPreference name={ name } value={ value } />;
+		} else if ( 'string' === typeof value ) {
+			preferenceHandler = <StringPreference name={ name } value={ value } />;
+		} else if ( 'boolean' === typeof value ) {
+			preferenceHandler = <BooleanPreference name={ name } value={ value } />;
+		} else if ( 'number' === typeof value ) {
+			preferenceHandler = <NumberPreference name={ name } value={ value } />;
+		}
+
 		return (
 			<div>
 				<div className="preferences-helper__preference-header">
@@ -28,13 +52,7 @@ class Preference extends Component {
 					</button>
 					<span>{ name }</span>
 				</div>
-				<ul className="preferences-helper__list">
-					{ isArray( value ) ? (
-						value.map( ( preference, index ) => <li key={ index }>{ preference }</li> )
-					) : (
-						<li key={ 0 }>{ value.toString() }</li>
-					) }
-				</ul>
+				{ preferenceHandler }
 			</div>
 		);
 	}
