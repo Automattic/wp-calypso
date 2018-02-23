34a60036110f81b34902e9f0b0a3f02f08b6be74
Jarda Snajdr <jsnajdr@gmail.com>
2018-02-23 21:13:30 +0100

    Cleanup the EditorGroundControl component (#22734)
    * EditorGroundControl: use more concise notation for mapDispatchToProps
    
    * EditorGroundControl: refactor the getCloseButtonPath method
    
    Avoid things like allocating a filtered array or calling `last` three times.
    
    * EditorGroundControl: fix determining post type when recording Preview button click
    
    The action creator was treating the `page` module import (the router) as a `post` object
    and checked its `type` property. Certainly not intended by the original author.
    
    * EditorGroundControl: import only used functions from lib/posts/utils
    
    * EditorGroundControl: remove the unused 'showAdvanceStatus' state field
    
    * EditorGroundControl: get user email verification status from Redux
    
    Removes the need for complicated checking of a property update of the
    `user` object.
    
    * EditorGroundControl: no need for 'user' and 'userUtils' props
    
    All user info is retrieved from Redux now. `PostEditor` doesn't need these props either.
    
    * Remove needsVerificationForSite function from lib/user/utils
    
    Not needed any more, replaced with Redux selectors.
    
    * EditorGroundControl: use props.translate for verification notice label
    
    Avoid using the global `i18n.translate` function. Also, don't translate all
    possible label on each call just to choose one of them at the end. Translate
    only the one that's needed.
    
    * EditorGroundControl doesn't need to be a PureComponent
    
    It's connected to Redux, and `connect` already implements the equivalent
    `shouldComponentUpdate` functionality for its wrapped components. Declaring
    the component as pure is redundant.

--- client/post-editor/controller.js
+++ client/post-editor/controller.js
@@ -18,7 +18,6 @@ import { map, pick, reduce, startsWith } from 'lodash';
 import actions from 'lib/posts/actions';
 import { addSiteFragment } from 'lib/route';
 import User from 'lib/user';
-import userUtils from 'lib/user/utils';
 import analytics from 'lib/analytics';
 import { decodeEntities } from 'lib/formatting';
 import PostEditor from './post-editor';
@@ -55,10 +54,7 @@ function determinePostType( context ) {
 }
 
 function renderEditor( context ) {
-	context.primary = React.createElement( PostEditor, {
-		user: user,
-		userUtils: userUtils,
-	} );
+	context.primary = React.createElement( PostEditor );
 }
 
 function maybeRedirect( context ) {
