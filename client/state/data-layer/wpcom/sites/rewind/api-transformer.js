f53280c38013b1a8415eaa390f2a179f33399367
Dennis Snell <dennis.snell@automattic.com>
2018-02-25 15:54:56 -0700

    Rewind: Start collecting status from state machine API (#22786)
    See #21855
    
    In the long process of deprecating the old Rewind status API we have to
    get information about recent Rewind operations from the "state machine"
    API instead of `/rewind/status`.
    
    In this patch we're starting to poll both endpoints whenever a Rewind is
    in progress or is started. In further PRs we'll slowly move
    functionality from the old data into the new data.
    
    In the meantime we will have some duplication of data and network
    activity but this gradual approach will allow us to make small and
    incremental changes without risking too much breakage.
    
    This patch only exist to make sure the necessary data is loaded into
    Calypso; it does not attempt to change the behavior of the UI.

--- client/state/data-layer/wpcom/sites/rewind/api-transformer.js
+++ client/state/data-layer/wpcom/sites/rewind/api-transformer.js
@@ -4,6 +4,12 @@
  */
 import { camelCase } from 'lodash';
 
+/**
+ * Internal dependencies
+ */
+import { http } from 'state/data-layer/wpcom-http/actions';
+import { requestRewindState } from 'state/rewind/actions';
+
 const transformCredential = data =>
 	Object.assign(
 		{
@@ -28,6 +34,15 @@ const transformDownload = data =>
 		data.validUntil && { validUntil: new Date( data.validUntil * 1000 ) }
 	);
 
+const makeRewindDismisser = data =>
+	http( {
+		apiVersion: data.apiVersion,
+		method: data.method,
+		path: data.path,
+		onSuccess: requestRewindState( data.site_id ),
+		onFailure: requestRewindState( data.site_id ),
+	} );
+
 const transformRewind = data =>
 	Object.assign(
 		{
@@ -36,7 +51,8 @@ const transformRewind = data =>
 			status: data.status,
 		},
 		data.progress && { progress: data.progress },
-		data.reason && { reason: data.reason }
+		data.reason && { reason: data.reason },
+		data.links && data.links.dismiss && { dismiss: makeRewindDismisser( data.links.dismiss ) }
 	);
 
 export const transformApi = data =>
