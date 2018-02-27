6acdb8aa9e0632abf054b1acd4938230d91608f6
Marin Atanasov <tyxla@abv.bg>
2018-02-27 17:25:07 +0200

    Jetpack Onboarding: Use ConnectIntro in contact form step

--- client/jetpack-onboarding/steps/contact-form.jsx
+++ client/jetpack-onboarding/steps/contact-form.jsx
@@ -11,16 +11,14 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import ConnectIntro from '../connect-intro';
 import ConnectSuccess from '../connect-success';
 import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
 import JetpackLogo from 'components/jetpack-logo';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
 import QuerySites from 'components/data/query-sites';
-import Tile from 'components/tile-grid/tile';
-import TileGrid from 'components/tile-grid';
-import { addQueryArgs } from 'lib/route';
-import { getJetpackOnboardingPendingSteps, getUnconnectedSiteUrl } from 'state/selectors';
+import { getJetpackOnboardingPendingSteps } from 'state/selectors';
 import { isJetpackSite } from 'state/sites/selectors';
 import { JETPACK_ONBOARDING_STEPS as STEPS } from '../constants';
 
@@ -65,45 +63,32 @@ class JetpackOnboardingContactFormStep extends React.PureComponent {
 	}
 
 	renderActionTile() {
-		const { hasContactForm, isConnected, siteUrl, translate } = this.props;
-		const headerText = translate( "Let's grow your audience with Jetpack." );
-		const subHeaderText = (
-			<Fragment>
-				{ translate(
-					'A great first step is adding a Contact Us page that includes a contact form.'
-				) }
-				<br />
-				{ translate( 'Create a Jetpack account to unlock this and dozens of other features.' ) }
-			</Fragment>
+		const { hasContactForm, siteId, translate } = this.props;
+		const header = (
+			<FormattedHeader
+				headerText={ translate( "Let's grow your audience with Jetpack." ) }
+				subHeaderText={
+					<Fragment>
+						{ translate(
+							'A great first step is adding a Contact Us page that includes a contact form.'
+						) }
+						<br />
+						{ translate( 'Create a Jetpack account to unlock this and dozens of other features.' ) }
+					</Fragment>
+				}
+			/>
 		);
-		const connectUrl = addQueryArgs(
-			{
-				url: siteUrl,
-				// TODO: add a parameter to the JPC to redirect back to this step after completion
-				// and in the redirect URL include the ?action=add_contact_form parameter
-				// to actually trigger the page and form creation action after getting back to JPO
-			},
-			'/jetpack/connect'
-		);
-		const href = ! isConnected ? connectUrl : null;
 
 		return (
-			<Fragment>
-				<FormattedHeader headerText={ headerText } subHeaderText={ subHeaderText } />
-
-				<TileGrid>
-					<Tile
-						buttonLabel={ ! hasContactForm ? translate( 'Add a contact form' ) : null }
-						description={
-							hasContactForm ? translate( 'Your contact form has been created.' ) : null
-						}
-						image={ '/calypso/images/illustrations/contact-us.svg' }
-						onClick={ this.handleAddContactForm }
-						href={ href }
-						e2eType={ 'contact-form' }
-					/>
-				</TileGrid>
-			</Fragment>
+			<ConnectIntro
+				buttonLabel={ ! hasContactForm ? translate( 'Add a contact form' ) : null }
+				description={ hasContactForm ? translate( 'Your contact form has been created.' ) : null }
+				e2eType="contact-form"
+				header={ header }
+				illustration="/calypso/images/illustrations/contact-us.svg"
+				onClick={ this.handleAddContactForm }
+				siteId={ siteId }
+			/>
 		);
 	}
 
@@ -139,6 +124,5 @@ class JetpackOnboardingContactFormStep extends React.PureComponent {
 export default connect( ( state, { settings, siteId, steps } ) => ( {
 	hasContactForm: !! get( settings, 'addContactForm' ),
 	isConnected: isJetpackSite( state, siteId ),
-	siteUrl: getUnconnectedSiteUrl( state, siteId ),
 	stepsPending: getJetpackOnboardingPendingSteps( state, siteId, steps ),
 } ) )( localize( JetpackOnboardingContactFormStep ) );
