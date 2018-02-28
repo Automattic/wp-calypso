67e4fc97e865cda060be317ceefd0867f689dda9
Alister Scott <alister.scott@gmail.com>
2018-02-28 15:25:10 +1000

    Revert "Layout: Add Skip Navigation link to sidebar (#3155)"

--- client/layout/index.jsx
+++ client/layout/index.jsx
@@ -175,11 +175,11 @@ const Layout = createReactClass( {
 						forcePinned={ 'post' === this.props.section.name }
 					/>
 
-					<div id="secondary" className="layout__secondary">
-						{ this.props.secondary }
-					</div>
 					<div id="primary" className="layout__primary">
 						{ this.props.primary }
+					</div>
+					<div id="secondary" className="layout__secondary">
+						{ this.props.secondary }
 					</div>
 				</div>
 				<TranslatorLauncher
