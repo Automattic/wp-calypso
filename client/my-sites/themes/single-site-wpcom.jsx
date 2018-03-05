b7e5537679843b00e98a9c91464a6d9ad21982a1
Bernhard Reiter <ockham@raz.or.at>
2018-03-05 13:07:26 +0000

    Themes: Use Selectors to determine if Plan has Unlimited Themes

--- client/my-sites/themes/single-site-wpcom.jsx
+++ client/my-sites/themes/single-site-wpcom.jsx
@@ -16,33 +16,34 @@ import SidebarNavigation from 'my-sites/sidebar-navigation';
 import ThanksModal from 'my-sites/themes/thanks-modal';
 import { connectOptions } from './theme-options';
 import Banner from 'components/banner';
-import { PLAN_FREE, PLAN_PERSONAL, PLAN_PREMIUM } from 'lib/plans/constants';
-import { getCurrentPlan } from 'state/sites/plans/selectors';
+import { FEATURE_UNLIMITED_PREMIUM_THEMES, PLAN_PREMIUM } from 'lib/plans/constants';
+import { getCurrentPlan, hasFeature, isRequestingSitePlans } from 'state/sites/plans/selectors';
 import QuerySitePlans from 'components/data/query-site-plans';
 import QuerySitePurchases from 'components/data/query-site-purchases';
 import ThemeShowcase from './theme-showcase';
 import { getSiteSlug } from 'state/sites/selectors';
 
 const ConnectedSingleSiteWpcom = connectOptions( props => {
-	const { siteId, siteSlug, currentPlanSlug, translate } = props;
+	const { hasUnlimitedPremiumThemes, requestingSitePlans, siteId, siteSlug, translate } = props;
 
 	const upsellUrl = `/plans/${ siteSlug }`;
 	return (
 		<div>
 			<SidebarNavigation />
 			<CurrentTheme siteId={ siteId } />
-			{ ( currentPlanSlug === PLAN_FREE || currentPlanSlug === PLAN_PERSONAL ) && (
-				<Banner
-					plan={ PLAN_PREMIUM }
-					title={ translate(
-						'Access all our premium themes with our Premium and Business plans!'
-					) }
-					description={ translate(
-						'Get advanced customization, more storage space, and video support along with all your new themes.'
-					) }
-					event="themes_plans_free_personal"
-				/>
-			) }
+			{ ! requestingSitePlans &&
+				! hasUnlimitedPremiumThemes && (
+					<Banner
+						plan={ PLAN_PREMIUM }
+						title={ translate(
+							'Access all our premium themes with our Premium and Business plans!'
+						) }
+						description={ translate(
+							'Get advanced customization, more storage space, and video support along with all your new themes.'
+						) }
+						event="themes_plans_free_personal"
+					/>
+				) }
 
 			<ThemeShowcase { ...props } upsellUrl={ upsellUrl } siteId={ siteId }>
 				{ siteId && <QuerySitePlans siteId={ siteId } /> }
@@ -58,5 +59,7 @@ export default connect( ( state, { siteId } ) => {
 	return {
 		siteSlug: getSiteSlug( state, siteId ),
 		currentPlanSlug: get( currentPlan, 'productSlug', null ),
+		hasUnlimitedPremiumThemes: hasFeature( state, siteId, FEATURE_UNLIMITED_PREMIUM_THEMES ),
+		requestingSitePlans: isRequestingSitePlans( state, siteId ),
 	};
 } )( ConnectedSingleSiteWpcom );
