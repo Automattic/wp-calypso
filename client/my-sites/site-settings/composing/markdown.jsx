2a8ee42b7b7287163b216f0967fc9812d7f9a159
Bernhard Reiter <ockham@raz.or.at>
2018-03-06 17:25:08 +0000

    Make stateless

--- client/my-sites/site-settings/composing/markdown.jsx
+++ client/my-sites/site-settings/composing/markdown.jsx
@@ -3,8 +3,8 @@
 /**
  * External dependencies
  */
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -15,32 +15,26 @@ import FormSettingExplanation from 'components/forms/form-setting-explanation';
 import ExternalLink from 'components/external-link';
 import JetpackModuleToggle from '../jetpack-module-toggle';
 
-class Markdown extends PureComponent {
-	static propTypes = {
-		disabled: PropTypes.bool,
-		siteId: PropTypes.number,
-		translate: PropTypes.func,
-	};
+const Markdown = ( { disabled, siteId, translate } ) => (
+	<FormFieldset className="composing__markdown has-divider is-top-only">
+		<JetpackModuleToggle
+			disabled={ disabled }
+			label={ translate( 'Write posts or pages in plain-text Markdown syntax' ) }
+			moduleSlug="markdown"
+			siteId={ siteId }
+		/>
+		<FormSettingExplanation isIndented>
+			<ExternalLink href="https://jetpack.com/support/markdown/">
+				{ translate( 'Learn more' ) }
+			</ExternalLink>
+		</FormSettingExplanation>
+	</FormFieldset>
+);
 
-	render() {
-		const { disabled, siteId, translate } = this.props;
-
-		return (
-			<FormFieldset className="composing__markdown has-divider is-top-only">
-				<JetpackModuleToggle
-					disabled={ disabled }
-					label={ translate( 'Write posts or pages in plain-text Markdown syntax' ) }
-					moduleSlug="markdown"
-					siteId={ siteId }
-				/>
-				<FormSettingExplanation isIndented>
-					<ExternalLink href="https://jetpack.com/support/markdown/">
-						{ translate( 'Learn more' ) }
-					</ExternalLink>
-				</FormSettingExplanation>
-			</FormFieldset>
-		);
-	}
-}
+Markdown.propTypes = {
+	disabled: PropTypes.bool,
+	siteId: PropTypes.number,
+	translate: PropTypes.func,
+};
 
 export default localize( Markdown );
