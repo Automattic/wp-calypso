df0fc6b298a95cfa5045810c1b6890abdcf1deef
Rastislav Lamos <lamos.rasto@gmail.com>
2018-03-07 15:22:22 -0800

    Remove from Domain Management

--- client/components/data/domain-management/index.jsx
+++ client/components/data/domain-management/index.jsx
@@ -3,8 +3,7 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
-import React from 'react';
-import createReactClass from 'create-react-class';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 
 /**
@@ -13,13 +12,14 @@ import { connect } from 'react-redux';
 import StoreConnection from 'components/data/store-connection';
 import DomainsStore from 'lib/domains/store';
 import CartStore from 'lib/cart/store';
-import observe from 'lib/mixins/data-observe';
 import { fetchDomains } from 'lib/upgrades/actions';
 import QuerySitePlans from 'components/data/query-site-plans';
 import QueryContactDetailsCache from 'components/data/query-contact-details-cache';
 import { getPlansBySite } from 'state/sites/plans/selectors';
 import { getSelectedSite } from 'state/ui/selectors';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
+import { getProductsList } from 'state/products-list/selectors';
+import QueryProducts from 'components/data/query-products-list';
 
 const stores = [ DomainsStore, CartStore ];
 
@@ -35,59 +35,54 @@ function getStateFromStores( props ) {
 	};
 }
 
-const DomainManagementData = createReactClass( {
-	displayName: 'DomainManagementData',
-
-	propTypes: {
+class DomainManagementData extends React.Component {
+	static propTypes = {
 		analyticsPath: PropTypes.string,
 		analyticsTitle: PropTypes.string,
 		context: PropTypes.object.isRequired,
-		productsList: PropTypes.object.isRequired,
 		selectedDomainName: PropTypes.string,
 		selectedSite: PropTypes.object,
 		sitePlans: PropTypes.object.isRequired,
-	},
+	};
 
-	mixins: [ observe( 'productsList' ) ],
-
-	componentWillMount: function() {
+	componentWillMount() {
 		const { selectedSite } = this.props;
 
 		if ( selectedSite ) {
 			fetchDomains( selectedSite.ID );
 		}
-	},
+	}
 
-	componentWillUpdate: function( nextProps ) {
+	componentWillUpdate( nextProps ) {
 		const { selectedSite: prevSite } = this.props;
 		const { selectedSite: nextSite } = nextProps;
 
 		if ( nextSite && nextSite !== prevSite ) {
 			fetchDomains( nextSite.ID );
 		}
-	},
+	}
 
-	render: function() {
+	render() {
 		return (
-			<div>
+			<Fragment>
 				<PageViewTracker path={ this.props.analyticsPath } title={ this.props.analyticsTitle } />
+				<QueryProducts />
+				{ this.props.selectedSite && <QuerySitePlans siteId={ this.props.selectedSite.ID } /> }
+				<QueryContactDetailsCache />
 				<StoreConnection
 					component={ this.props.component }
 					stores={ stores }
 					getStateFromStores={ getStateFromStores }
-					products={ this.props.productsList.get() }
+					products={ this.props.productsList }
 					selectedDomainName={ this.props.selectedDomainName }
 					selectedSite={ this.props.selectedSite }
 					sitePlans={ this.props.sitePlans }
 					context={ this.props.context }
 				/>
-				{ this.props.selectedSite && <QuerySitePlans siteId={ this.props.selectedSite.ID } /> && (
-						<QueryContactDetailsCache />
-					) }
-			</div>
+			</Fragment>
 		);
-	},
-} );
+	}
+}
 
 const mapStateToProps = state => {
 	const selectedSite = getSelectedSite( state );
@@ -95,6 +90,7 @@ const mapStateToProps = state => {
 	return {
 		sitePlans: getPlansBySite( state, selectedSite ),
 		selectedSite,
+		productsList: getProductsList( state ),
 	};
 };
 
