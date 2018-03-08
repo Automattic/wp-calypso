648a6ce3bce9c89225e5e954ff2e1d263ff3c7ff
Adam Zieliński <adam@adamziel.com>
2018-03-08 12:42:08 +0100

    Simplify usage of plans metadata

--- client/lib/plans/constants.js
+++ client/lib/plans/constants.js
@@ -154,8 +154,8 @@ export const TYPE_PREMIUM = 'TYPE_PREMIUM';
 export const TYPE_BUSINESS = 'TYPE_BUSINESS';
 
 const getPlanPersonalDetails = () => ( {
-	getGroup: () => GROUP_WPCOM,
-	getType: () => TYPE_PERSONAL,
+	group: GROUP_WPCOM,
+	type: TYPE_PERSONAL,
 	getTitle: () => i18n.translate( 'Personal' ),
 	getAudience: () => i18n.translate( 'Best for hobbyists' ),
 	getBlogAudience: () => i18n.translate( 'Best for hobbyists' ),
@@ -209,8 +209,8 @@ const getPlanPersonalDetails = () => ( {
 } );
 
 const getPlanPremiumDetails = () => ( {
-	getGroup: () => GROUP_WPCOM,
-	getType: () => TYPE_PREMIUM,
+	group: GROUP_WPCOM,
+	type: TYPE_PREMIUM,
 	getTitle: () => i18n.translate( 'Premium' ),
 	getAudience: () => i18n.translate( 'Best for entrepreneurs' ),
 	getBlogAudience: () => i18n.translate( 'Best for professionals' ),
@@ -276,8 +276,8 @@ const getPlanPremiumDetails = () => ( {
 } );
 
 const getPlanBusinessDetails = () => ( {
-	getGroup: () => GROUP_WPCOM,
-	getType: () => TYPE_BUSINESS,
+	group: GROUP_WPCOM,
+	type: TYPE_BUSINESS,
 	getTitle: () => i18n.translate( 'Business' ),
 	getAudience: () => i18n.translate( 'Best for small businesses' ),
 	getBlogAudience: () => i18n.translate( 'Best for brands' ),
@@ -373,9 +373,9 @@ const getPlanBusinessDetails = () => ( {
 // DO NOT import. Use `getPlan` from `lib/plans` instead.
 export const PLANS_LIST = {
 	[ PLAN_FREE ]: {
-		getGroup: () => GROUP_WPCOM,
-		getType: () => TYPE_FREE,
-		getTerm: () => TERM_ANNUALLY,
+		group: GROUP_WPCOM,
+		type: TYPE_FREE,
+		term: TERM_ANNUALLY,
 		getTitle: () => i18n.translate( 'Free' ),
 		getAudience: () => i18n.translate( 'Best for students' ),
 		getBlogAudience: () => i18n.translate( 'Best for students' ),
@@ -419,7 +419,7 @@ export const PLANS_LIST = {
 
 	[ PLAN_PERSONAL ]: {
 		...getPlanPersonalDetails(),
-		getTerm: () => TERM_ANNUALLY,
+		term: TERM_ANNUALLY,
 		availableFor: plan => includes( [ PLAN_FREE ], plan ),
 		getProductId: () => 1009,
 		getStoreSlug: () => PLAN_PERSONAL,
@@ -428,7 +428,7 @@ export const PLANS_LIST = {
 
 	[ PLAN_PERSONAL_2_YEARS ]: {
 		...getPlanPersonalDetails(),
-		getTerm: () => TERM_BIENNIALLY,
+		term: TERM_BIENNIALLY,
 		availableFor: plan => includes( [ PLAN_FREE, PLAN_PERSONAL ], plan ),
 		getProductId: () => 1029,
 		getStoreSlug: () => PLAN_PERSONAL_2_YEARS,
@@ -437,7 +437,7 @@ export const PLANS_LIST = {
 
 	[ PLAN_PREMIUM ]: {
 		...getPlanPremiumDetails(),
-		getTerm: () => TERM_ANNUALLY,
+		term: TERM_ANNUALLY,
 		availableFor: plan => includes( [ PLAN_FREE, PLAN_PERSONAL, PLAN_PERSONAL_2_YEARS ], plan ),
 		getProductId: () => 1003,
 		getStoreSlug: () => PLAN_PREMIUM,
@@ -447,7 +447,7 @@ export const PLANS_LIST = {
 
 	[ PLAN_PREMIUM_2_YEARS ]: {
 		...getPlanPremiumDetails(),
-		getTerm: () => TERM_BIENNIALLY,
+		term: TERM_BIENNIALLY,
 		availableFor: plan =>
 			includes( [ PLAN_FREE, PLAN_PERSONAL, PLAN_PERSONAL_2_YEARS, PLAN_PREMIUM ], plan ),
 		getProductId: () => 1023,
@@ -458,7 +458,7 @@ export const PLANS_LIST = {
 
 	[ PLAN_BUSINESS ]: {
 		...getPlanBusinessDetails(),
-		getTerm: () => TERM_ANNUALLY,
+		term: TERM_ANNUALLY,
 		availableFor: plan =>
 			includes(
 				[ PLAN_FREE, PLAN_PERSONAL, PLAN_PERSONAL_2_YEARS, PLAN_PREMIUM, PLAN_PREMIUM_2_YEARS ],
@@ -472,7 +472,7 @@ export const PLANS_LIST = {
 
 	[ PLAN_BUSINESS_2_YEARS ]: {
 		...getPlanBusinessDetails(),
-		getTerm: () => TERM_BIENNIALLY,
+		term: TERM_BIENNIALLY,
 		availableFor: plan =>
 			includes(
 				[
@@ -492,9 +492,9 @@ export const PLANS_LIST = {
 	},
 
 	[ PLAN_JETPACK_FREE ]: {
-		getTerm: () => TERM_ANNUALLY,
-		getGroup: () => GROUP_JETPACK,
-		getType: () => TYPE_FREE,
+		term: TERM_ANNUALLY,
+		group: GROUP_JETPACK,
+		type: TYPE_FREE,
 		getTitle: () => i18n.translate( 'Free' ),
 		getAudience: () => i18n.translate( 'Best for students' ),
 		getProductId: () => 2002,
@@ -527,9 +527,9 @@ export const PLANS_LIST = {
 	},
 
 	[ PLAN_JETPACK_PREMIUM ]: {
-		getGroup: () => GROUP_JETPACK,
-		getType: () => TYPE_PREMIUM,
-		getTerm: () => TERM_ANNUALLY,
+		group: GROUP_JETPACK,
+		type: TYPE_PREMIUM,
+		term: TERM_ANNUALLY,
 		getTitle: () => i18n.translate( 'Premium' ),
 		getAudience: () => i18n.translate( 'Best for small businesses' ),
 		getSubtitle: () => i18n.translate( 'Protection, speed, and revenue.' ),
@@ -588,9 +588,9 @@ export const PLANS_LIST = {
 	},
 
 	[ PLAN_JETPACK_PREMIUM_MONTHLY ]: {
-		getGroup: () => GROUP_JETPACK,
-		getType: () => TYPE_PREMIUM,
-		getTerm: () => TERM_MONTHLY,
+		group: GROUP_JETPACK,
+		type: TYPE_PREMIUM,
+		term: TERM_MONTHLY,
 		getTitle: () => i18n.translate( 'Premium' ),
 		getAudience: () => i18n.translate( 'Best for small businesses' ),
 		getProductId: () => 2003,
@@ -646,9 +646,9 @@ export const PLANS_LIST = {
 	},
 
 	[ PLAN_JETPACK_PERSONAL ]: {
-		getGroup: () => GROUP_JETPACK,
-		getType: () => TYPE_PERSONAL,
-		getTerm: () => TERM_ANNUALLY,
+		group: GROUP_JETPACK,
+		type: TYPE_PERSONAL,
+		term: TERM_ANNUALLY,
 		getTitle: () => i18n.translate( 'Personal' ),
 		getAudience: () => i18n.translate( 'Best for hobbyists' ),
 		getProductId: () => 2005,
@@ -685,9 +685,9 @@ export const PLANS_LIST = {
 	},
 
 	[ PLAN_JETPACK_PERSONAL_MONTHLY ]: {
-		getGroup: () => GROUP_JETPACK,
-		getType: () => TYPE_PERSONAL,
-		getTerm: () => TERM_MONTHLY,
+		group: GROUP_JETPACK,
+		type: TYPE_PERSONAL,
+		term: TERM_MONTHLY,
 		getTitle: () => i18n.translate( 'Personal' ),
 		getAudience: () => i18n.translate( 'Best for hobbyists' ),
 		getStoreSlug: () => PLAN_JETPACK_PERSONAL_MONTHLY,
@@ -730,9 +730,9 @@ export const PLANS_LIST = {
 	},
 
 	[ PLAN_JETPACK_BUSINESS ]: {
-		getGroup: () => GROUP_JETPACK,
-		getType: () => TYPE_BUSINESS,
-		getTerm: () => TERM_ANNUALLY,
+		group: GROUP_JETPACK,
+		type: TYPE_BUSINESS,
+		term: TERM_ANNUALLY,
 		getTitle: () => i18n.translate( 'Professional' ),
 		getAudience: () => i18n.translate( 'Best for organizations' ),
 		getProductId: () => 2001,
@@ -791,9 +791,9 @@ export const PLANS_LIST = {
 	},
 
 	[ PLAN_JETPACK_BUSINESS_MONTHLY ]: {
-		getGroup: () => GROUP_JETPACK,
-		getType: () => TYPE_BUSINESS,
-		getTerm: () => TERM_MONTHLY,
+		group: GROUP_JETPACK,
+		type: TYPE_BUSINESS,
+		term: TERM_MONTHLY,
 		getTitle: () => i18n.translate( 'Professional' ),
 		getAudience: () => i18n.translate( 'Best for organizations' ),
 		getSubtitle: () => i18n.translate( 'Ultimate security and traffic tools.' ),
--- client/lib/plans/test/plan-lookups.js
+++ client/lib/plans/test/plan-lookups.js
@@ -8,173 +8,82 @@ import { expect } from 'chai';
  * Internal dependencies
  */
 import {
-	GROUP_JETPACK,
-	GROUP_WPCOM,
 	PLAN_BUSINESS,
 	PLAN_BUSINESS_2_YEARS,
 	PLAN_FREE,
+	PLAN_JETPACK_BUSINESS,
+	PLAN_JETPACK_BUSINESS_MONTHLY,
+	PLAN_JETPACK_FREE,
+	PLAN_JETPACK_PERSONAL,
+	PLAN_JETPACK_PERSONAL_MONTHLY,
+	PLAN_JETPACK_PREMIUM,
+	PLAN_JETPACK_PREMIUM_MONTHLY,
 	PLAN_PERSONAL,
 	PLAN_PERSONAL_2_YEARS,
 	PLAN_PREMIUM,
 	PLAN_PREMIUM_2_YEARS,
-	TERM_ANNUALLY,
-	TERM_BIENNIALLY,
-	TERM_MONTHLY,
-	TYPE_BUSINESS,
-	TYPE_FREE,
-	TYPE_PERSONAL,
-	TYPE_PREMIUM,
 } from '../constants';
-import { getPlan, __internal__planMatches } from '../index';
+import { isBusinessPlan, isPersonalPlan, isPremiumPlan } from '../index';
 
-describe( '__internal__planMatches', () => {
-	test( 'should return true for queries matching plans', () => {
-		[
-			[
-				PLAN_FREE,
-				[
-					{ group: GROUP_WPCOM },
-					{ type: TYPE_FREE },
-					{ term: TERM_ANNUALLY },
-					{ group: GROUP_WPCOM, type: TYPE_FREE },
-					{ group: GROUP_WPCOM, term: TERM_ANNUALLY },
-					{ term: TERM_ANNUALLY, type: TYPE_FREE },
-					{ group: GROUP_WPCOM, term: TERM_ANNUALLY, type: TYPE_FREE },
-				],
-				PLAN_BUSINESS,
-				[
-					{ group: GROUP_WPCOM },
-					{ type: TYPE_BUSINESS },
-					{ term: TERM_ANNUALLY },
-					{ group: GROUP_WPCOM, type: TYPE_BUSINESS },
-					{ group: GROUP_WPCOM, term: TERM_ANNUALLY },
-					{ term: TERM_ANNUALLY, type: TYPE_BUSINESS },
-					{ group: GROUP_WPCOM, term: TERM_ANNUALLY, type: TYPE_BUSINESS },
-				],
-				PLAN_BUSINESS_2_YEARS,
-				[
-					{ group: GROUP_WPCOM },
-					{ type: TYPE_BUSINESS },
-					{ term: TERM_BIENNIALLY },
-					{ group: GROUP_WPCOM, type: TYPE_BUSINESS },
-					{ group: GROUP_WPCOM, term: TERM_BIENNIALLY },
-					{ term: TERM_BIENNIALLY, type: TYPE_BUSINESS },
-					{ group: GROUP_WPCOM, term: TERM_BIENNIALLY, type: TYPE_BUSINESS },
-				],
-				PLAN_PREMIUM,
-				[
-					{ group: GROUP_WPCOM },
-					{ type: TYPE_PREMIUM },
-					{ term: TERM_ANNUALLY },
-					{ group: GROUP_WPCOM, type: TYPE_PREMIUM },
-					{ group: GROUP_WPCOM, term: TERM_ANNUALLY },
-					{ term: TERM_ANNUALLY, type: TYPE_PREMIUM },
-					{ group: GROUP_WPCOM, term: TERM_ANNUALLY, type: TYPE_PREMIUM },
-				],
-				PLAN_PREMIUM_2_YEARS,
-				[
-					{ group: GROUP_WPCOM },
-					{ type: TYPE_PREMIUM },
-					{ term: TERM_BIENNIALLY },
-					{ group: GROUP_WPCOM, type: TYPE_PREMIUM },
-					{ group: GROUP_WPCOM, term: TERM_BIENNIALLY },
-					{ term: TERM_BIENNIALLY, type: TYPE_PREMIUM },
-					{ group: GROUP_WPCOM, term: TERM_BIENNIALLY, type: TYPE_PREMIUM },
-				],
-				PLAN_PERSONAL,
-				[
-					{ group: GROUP_WPCOM },
-					{ type: TYPE_PERSONAL },
-					{ term: TERM_ANNUALLY },
-					{ group: GROUP_WPCOM, type: TYPE_PERSONAL },
-					{ group: GROUP_WPCOM, term: TERM_ANNUALLY },
-					{ term: TERM_ANNUALLY, type: TYPE_PERSONAL },
-					{ group: GROUP_WPCOM, term: TERM_ANNUALLY, type: TYPE_PERSONAL },
-				],
-				PLAN_PERSONAL_2_YEARS,
-				[
-					{ group: GROUP_WPCOM },
-					{ type: TYPE_PERSONAL },
-					{ term: TERM_BIENNIALLY },
-					{ group: GROUP_WPCOM, type: TYPE_PERSONAL },
-					{ group: GROUP_WPCOM, term: TERM_BIENNIALLY },
-					{ term: TERM_BIENNIALLY, type: TYPE_PERSONAL },
-					{ group: GROUP_WPCOM, term: TERM_BIENNIALLY, type: TYPE_PERSONAL },
-				],
-			],
-		].forEach( ( [ plan, queries ] ) =>
-			queries.forEach( query => {
-				const result = __internal__planMatches( getPlan( plan ), query );
-				expect( result ).to.be.true;
-			} )
-		);
+describe( 'isBusinessPlan', () => {
+	test( 'should return true for all business plans', () => {
+		expect( isBusinessPlan( PLAN_BUSINESS ) ).to.be.true;
+		expect( isBusinessPlan( PLAN_BUSINESS_2_YEARS ) ).to.be.true;
+		expect( isBusinessPlan( PLAN_JETPACK_BUSINESS ) ).to.be.true;
+		expect( isBusinessPlan( PLAN_JETPACK_BUSINESS_MONTHLY ) ).to.be.true;
 	} );
+	test( 'should return true for all non-business plans', () => {
+		expect( isBusinessPlan( PLAN_FREE ) ).to.be.false;
+		expect( isBusinessPlan( PLAN_PERSONAL ) ).to.be.false;
+		expect( isBusinessPlan( PLAN_PERSONAL_2_YEARS ) ).to.be.false;
+		expect( isBusinessPlan( PLAN_PREMIUM ) ).to.be.false;
+		expect( isBusinessPlan( PLAN_PREMIUM_2_YEARS ) ).to.be.false;
+		expect( isBusinessPlan( PLAN_JETPACK_FREE ) ).to.be.false;
+		expect( isBusinessPlan( PLAN_JETPACK_PERSONAL ) ).to.be.false;
+		expect( isBusinessPlan( PLAN_JETPACK_PERSONAL_MONTHLY ) ).to.be.false;
+		expect( isBusinessPlan( PLAN_JETPACK_PREMIUM ) ).to.be.false;
+		expect( isBusinessPlan( PLAN_JETPACK_PREMIUM_MONTHLY ) ).to.be.false;
+	} );
+} );
 
-	test( 'should return false for queries not matching plans', () => {
-		[
-			[
-				PLAN_FREE,
-				[
-					{ group: GROUP_JETPACK },
-					{ type: TYPE_PERSONAL },
-					{ type: TYPE_PREMIUM },
-					{ term: TERM_MONTHLY },
-					{ term: TERM_BIENNIALLY },
-				],
-				PLAN_BUSINESS,
-				[
-					{ group: GROUP_JETPACK },
-					{ type: TYPE_PERSONAL },
-					{ type: TYPE_PREMIUM },
-					{ term: TERM_MONTHLY },
-					{ term: TERM_BIENNIALLY },
-				],
-				PLAN_BUSINESS_2_YEARS,
-				[
-					{ group: GROUP_JETPACK },
-					{ type: TYPE_PERSONAL },
-					{ type: TYPE_PREMIUM },
-					{ term: TERM_ANNUALLY },
-					{ term: TERM_MONTHLY },
-				],
-				PLAN_PREMIUM,
-				[
-					{ group: GROUP_JETPACK },
-					{ type: TYPE_PERSONAL },
-					{ type: TYPE_BUSINESS },
-					{ term: TERM_MONTHLY },
-					{ term: TERM_BIENNIALLY },
-				],
-				PLAN_PREMIUM_2_YEARS,
-				[
-					{ group: GROUP_JETPACK },
-					{ type: TYPE_PERSONAL },
-					{ type: TYPE_BUSINESS },
-					{ term: TERM_ANNUALLY },
-					{ term: TERM_MONTHLY },
-				],
-				PLAN_PERSONAL,
-				[
-					{ group: GROUP_JETPACK },
-					{ type: TYPE_PREMIUM },
-					{ type: TYPE_BUSINESS },
-					{ term: TERM_MONTHLY },
-					{ term: TERM_BIENNIALLY },
-				],
-				PLAN_PERSONAL_2_YEARS,
-				[
-					{ group: GROUP_JETPACK },
-					{ type: TYPE_PREMIUM },
-					{ type: TYPE_BUSINESS },
-					{ term: TERM_ANNUALLY },
-					{ term: TERM_MONTHLY },
-				],
-			],
-		].forEach( ( [ plan, queries ] ) =>
-			queries.forEach( query => {
-				const result = __internal__planMatches( getPlan( plan ), query );
-				expect( result ).to.be.false;
-			} )
-		);
+describe( 'isPremiumPlan', () => {
+	test( 'should return true for all premium plans', () => {
+		expect( isPremiumPlan( PLAN_PREMIUM ) ).to.be.true;
+		expect( isPremiumPlan( PLAN_PREMIUM_2_YEARS ) ).to.be.true;
+		expect( isPremiumPlan( PLAN_JETPACK_PREMIUM ) ).to.be.true;
+		expect( isPremiumPlan( PLAN_JETPACK_PREMIUM_MONTHLY ) ).to.be.true;
+	} );
+	test( 'should return true for all non-premium plans', () => {
+		expect( isPremiumPlan( PLAN_FREE ) ).to.be.false;
+		expect( isPremiumPlan( PLAN_BUSINESS ) ).to.be.false;
+		expect( isPremiumPlan( PLAN_BUSINESS_2_YEARS ) ).to.be.false;
+		expect( isPremiumPlan( PLAN_PERSONAL ) ).to.be.false;
+		expect( isPremiumPlan( PLAN_PERSONAL_2_YEARS ) ).to.be.false;
+		expect( isPremiumPlan( PLAN_JETPACK_FREE ) ).to.be.false;
+		expect( isPremiumPlan( PLAN_JETPACK_PERSONAL ) ).to.be.false;
+		expect( isPremiumPlan( PLAN_JETPACK_PERSONAL_MONTHLY ) ).to.be.false;
+		expect( isPremiumPlan( PLAN_JETPACK_BUSINESS ) ).to.be.false;
+		expect( isPremiumPlan( PLAN_JETPACK_BUSINESS_MONTHLY ) ).to.be.false;
+	} );
+} );
+
+describe( 'isPersonalPlan', () => {
+	test( 'should return true for all personal plans', () => {
+		expect( isPersonalPlan( PLAN_PERSONAL ) ).to.be.true;
+		expect( isPersonalPlan( PLAN_PERSONAL_2_YEARS ) ).to.be.true;
+		expect( isPersonalPlan( PLAN_JETPACK_PERSONAL ) ).to.be.true;
+		expect( isPersonalPlan( PLAN_JETPACK_PERSONAL_MONTHLY ) ).to.be.true;
+	} );
+	test( 'should return false for all non-personal plans', () => {
+		expect( isPersonalPlan( PLAN_FREE ) ).to.be.false;
+		expect( isPersonalPlan( PLAN_JETPACK_FREE ) ).to.be.false;
+		expect( isPersonalPlan( PLAN_PREMIUM ) ).to.be.false;
+		expect( isPersonalPlan( PLAN_PREMIUM_2_YEARS ) ).to.be.false;
+		expect( isPersonalPlan( PLAN_BUSINESS ) ).to.be.false;
+		expect( isPersonalPlan( PLAN_BUSINESS_2_YEARS ) ).to.be.false;
+		expect( isPersonalPlan( PLAN_JETPACK_PREMIUM ) ).to.be.false;
+		expect( isPersonalPlan( PLAN_JETPACK_PREMIUM_MONTHLY ) ).to.be.false;
+		expect( isPersonalPlan( PLAN_JETPACK_BUSINESS ) ).to.be.false;
+		expect( isPersonalPlan( PLAN_JETPACK_BUSINESS_MONTHLY ) ).to.be.false;
 	} );
 } );
