711e1736c1536ed548eb3434e94d6b7b199b9083
Chris R <chris@bluefuton.com>
2018-03-08 12:03:10 +0000

    Reader: consume site block status from /read/sites/:site endpoint (#23046)
    * Reader: consume site block status from /read/sites/:site endpoint
    
    * Add is_blocked to schema

--- client/state/reader/site-blocks/reducer.js
+++ client/state/reader/site-blocks/reducer.js
@@ -1,22 +1,46 @@
 /** @format */
 /**
- * Internal dependencies
+ * External dependencies
  */
-import { READER_SITE_BLOCK, READER_SITE_UNBLOCK } from 'state/action-types';
-import { combineReducers, createReducer, keyedReducer } from 'state/utils';
+import { omit } from 'lodash';
 
 /**
- * Tracks all known site block statuses, indexed by site ID.
+ * Internal dependencies
  */
-export const items = keyedReducer(
-	'payload.siteId',
-	createReducer(
-		{},
-		{
-			[ READER_SITE_BLOCK ]: () => true,
-			[ READER_SITE_UNBLOCK ]: () => false,
-		}
-	)
+import {
+	READER_SITE_BLOCK,
+	READER_SITE_UNBLOCK,
+	READER_SITE_REQUEST_SUCCESS,
+} from 'state/action-types';
+import { combineReducers, createReducer } from 'state/utils';
+
+export const items = createReducer(
+	{},
+	{
+		[ READER_SITE_BLOCK ]: ( state, action ) => {
+			return {
+				...state,
+				[ action.payload.siteId ]: true,
+			};
+		},
+		[ READER_SITE_UNBLOCK ]: ( state, action ) => {
+			return omit( state, action.payload.siteId );
+		},
+		[ READER_SITE_REQUEST_SUCCESS ]: ( state, action ) => {
+			if ( ! action.payload.is_blocked ) {
+				if ( ! state[ action.payload.ID ] ) {
+					return state;
+				}
+
+				return omit( state, action.payload.ID );
+			}
+
+			return {
+				...state,
+				[ action.payload.ID ]: true,
+			};
+		},
+	}
 );
 
 export default combineReducers( {
