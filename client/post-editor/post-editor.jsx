293e19981ead9f1085efb2d17b1af69fad8c4e4e
Jonathan Sadowski <sadowski@automattic.com>
2018-03-12 17:07:47 -0500

    Editor: Hide notice when clicking edit from preview

--- client/post-editor/post-editor.jsx
+++ client/post-editor/post-editor.jsx
@@ -826,6 +826,8 @@ export const PostEditor = createReactClass( {
 			this.props.setNextLayoutFocus( 'sidebar' );
 		}
 
+		this.hideNotice();
+
 		this.setState( {
 			showPreview: false,
 			isPostPublishPreview: false,
