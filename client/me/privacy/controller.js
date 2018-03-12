7619a7b57be3d78058eb4b72f21e4736b3f610ce
Benoit Person <benoit.person@gmail.com>
2018-03-12 15:28:33 +0100

    Privacy settings: Add toggle for Tracks opt-out

--- client/me/privacy/controller.js
+++ client/me/privacy/controller.js
@@ -10,12 +10,15 @@ import page from 'page';
  * Internal dependencies
  */
 import config from 'config';
+import userSettings from 'lib/user-settings';
 import PrivacyComponent from 'me/privacy/main';
 
 export default function privacyController( context, next ) {
 	if ( ! config.isEnabled( 'me/privacy' ) ) {
 		return page.redirect( '/me' );
 	}
-	context.primary = React.createElement( PrivacyComponent );
+	context.primary = React.createElement( PrivacyComponent, {
+		userSettings: userSettings,
+	} );
 	next();
 }
