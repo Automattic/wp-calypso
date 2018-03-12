389b713be20ccf61280f8ed98ebe4949f1b4bf8c
James Tien <southp0105@gmail.com>
2018-03-12 16:02:38 +0800

    Add `getCurrentUserLocaleVariant()` selector.

--- client/state/current-user/selectors.js
+++ client/state/current-user/selectors.js
@@ -56,6 +56,13 @@ export const createCurrentUserSelector = ( path, otherwise = null ) => state => 
 export const getCurrentUserLocale = createCurrentUserSelector( 'localeSlug' );
 
 /**
+ * Returns the locale variant slug for the current user.
+ * @param  {Object}  state  Global state tree
+ * @return {?String}        Current user locale variant
+ */
+export const getCurrentUserLocaleVariant = createCurrentUserSelector( 'localeVariant' );
+
+/**
  * Returns the country code for the current user.
  *
  * @param  {Object}  state  Global state tree
