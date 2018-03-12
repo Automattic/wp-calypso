9e170e4c0e1dcf82b1a855d96a89b2d785491c04
Bernhard Reiter <ockham@raz.or.at>
2018-03-12 17:30:07 +0100

    JP Onboarding: Hide Stats Step when Connected

--- client/jetpack-onboarding/main.jsx
+++ client/jetpack-onboarding/main.jsx
@@ -152,7 +152,7 @@ class JetpackOnboardingMain extends React.PureComponent {
 	}
 }
 export default connect(
-	( state, { siteSlug } ) => {
+	( state, { action, siteSlug } ) => {
 		let siteId = getUnconnectedSiteIdBySlug( state, siteSlug );
 		if ( ! siteId ) {
 			// We rely on the fact that all sites are being requested automatically early in <Layout />.
@@ -186,6 +186,13 @@ export default connect(
 		).isLoading;
 
 		const userIdHashed = getUnconnectedSiteUserHash( state, siteId );
+
+		// Only show the Stats Step either if we aren't connected to WP.com yet,
+		// or if we're just being redirected back to JP Onboarding right after
+		// going through JP Connect, in which case the `action` query arg will be
+		// set to `activate_stats`.
+		const showStatsStep = ! isConnected || action === 'activate_stats';
+
 		const steps = compact( [
 			STEPS.SITE_TITLE,
 			STEPS.SITE_TYPE,
@@ -193,7 +200,7 @@ export default connect(
 			STEPS.CONTACT_FORM,
 			isBusiness && STEPS.BUSINESS_ADDRESS,
 			isBusiness && STEPS.WOOCOMMERCE,
-			STEPS.STATS,
+			showStatsStep && STEPS.STATS,
 			STEPS.SUMMARY,
 		] );
 		const stepsCompleted = getJetpackOnboardingCompletedSteps( state, siteId, steps );
