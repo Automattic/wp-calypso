73c4dadc47ae0680b178216d0821fce761ba662f
Dave Whitley <drw158@gmail.com>
2018-03-14 15:34:50 -0500

    Replace noticon with gridicon
    This removes the plus noticon from the G Suite registration flow for
    new domains.

--- client/components/upgrades/google-apps/google-apps-dialog/users.jsx
+++ client/components/upgrades/google-apps/google-apps-dialog/users.jsx
@@ -8,6 +8,7 @@ import React from 'react';
 import { clone } from 'lodash';
 import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
+import Gridicon from 'gridicons';
 
 /**
  * Internal dependencies
@@ -45,6 +46,7 @@ class GoogleAppsUsers extends React.Component {
 				{ allUserInputs }
 
 				<button className="google-apps-dialog__add-another-user-button" onClick={ this.addUser }>
+					<Gridicon icon="plus" />
 					{ translate( 'Add Another User' ) }
 				</button>
 			</div>
