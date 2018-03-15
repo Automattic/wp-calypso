6d2402bd83d2d1ea3106bc888b98d2582e0ceabc
Shaun Andrews <shaun@automattic.com>
2018-03-15 13:39:45 -0400

    Adding a wrapper in a wrapper. Its stupid, but it lets use center component examples.

--- client/devdocs/docs-example/wrapper.jsx
+++ client/devdocs/docs-example/wrapper.jsx
@@ -33,7 +33,9 @@ class DocsExampleWrapper extends Component {
 				} ) }
 			>
 				<div className="docs-example__wrapper-header">{ renderTitle( unique, name, url ) }</div>
-				<div className="docs-example__wrapper-content">{ children }</div>
+				<div className="docs-example__wrapper-content">
+					<span className="docs-example__wrapper-content-wrap">{ children }</span>
+				</div>
 			</div>
 		);
 	}
