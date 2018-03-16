92376d80f47974cc4944073d70ba7c9b8ebb38df
Jon Surrell <jon.surrell@automattic.com>
2018-03-16 11:12:27 +0100

    Update theme query key tests for new stringify

--- client/lib/query-manager/theme/test/key.js
+++ client/lib/query-manager/theme/test/key.js
@@ -14,43 +14,43 @@ describe( 'ThemeQueryKey', () => {
 		test( 'should return a JSON string of the object', () => {
 			const key = ThemeQueryKey.stringify( { ok: true } );
 
-			expect( key ).to.equal( '[["ok",true]]' );
+			expect( key ).to.equal( '{"ok":true}' );
 		} );
 
 		test( 'should omit default theme query parameters', () => {
 			const key = ThemeQueryKey.stringify( { ok: true, tier: '' } );
 
-			expect( key ).to.equal( '[["ok",true]]' );
+			expect( key ).to.equal( '{"ok":true}' );
 		} );
 
 		test( 'should omit null query values', () => {
 			const key = ThemeQueryKey.stringify( { ok: true, search: null } );
 
-			expect( key ).to.equal( '[["ok",true]]' );
+			expect( key ).to.equal( '{"ok":true}' );
 		} );
 
 		test( 'should omit undefined query values', () => {
 			const key = ThemeQueryKey.stringify( { ok: true, search: undefined } );
 
-			expect( key ).to.equal( '[["ok",true]]' );
+			expect( key ).to.equal( '{"ok":true}' );
 		} );
 	} );
 
 	describe( '.parse()', () => {
 		test( 'should return an object of the JSON string', () => {
-			const query = ThemeQueryKey.parse( '[["ok",true]]' );
+			const query = ThemeQueryKey.parse( '{"ok":true}' );
 
 			expect( query ).to.eql( { ok: true } );
 		} );
 
 		test( 'should omit default theme query parameters', () => {
-			const query = ThemeQueryKey.parse( '[["ok",true],["tier",""]]' );
+			const query = ThemeQueryKey.parse( '{"ok":true,"tier":""}' );
 
 			expect( query ).to.eql( { ok: true } );
 		} );
 
 		test( 'should omit null query values', () => {
-			const query = ThemeQueryKey.parse( '[["ok",true],["search",null]]' );
+			const query = ThemeQueryKey.parse( '{"ok":true,"search":null}' );
 
 			expect( query ).to.eql( { ok: true } );
 		} );
