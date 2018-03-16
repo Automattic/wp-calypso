169066b04946ad3e3c6068079f6f6075e7545b42
Jon Surrell <jon.surrell@automattic.com>
2018-03-16 11:00:04 +0100

    Update paginated tests to reflect stringify changes

--- client/lib/query-manager/paginated/test/key.js
+++ client/lib/query-manager/paginated/test/key.js
@@ -14,25 +14,25 @@ describe( 'PaginatedQueryKey', () => {
 		test( 'should return a JSON string of the object', () => {
 			const key = PaginatedQueryKey.stringify( { ok: true } );
 
-			expect( key ).to.equal( '[["ok",true]]' );
+			expect( key ).to.equal( '{"ok":true}' );
 		} );
 
 		test( 'should omit pagination query parameters', () => {
 			const key = PaginatedQueryKey.stringify( { ok: true, page: 2 } );
 
-			expect( key ).to.equal( '[["ok",true]]' );
+			expect( key ).to.equal( '{"ok":true}' );
 		} );
 	} );
 
 	describe( '.parse()', () => {
 		test( 'should return an object of the JSON string', () => {
-			const query = PaginatedQueryKey.parse( '[["ok",true]]' );
+			const query = PaginatedQueryKey.parse( '{"ok":true}' );
 
 			expect( query ).to.eql( { ok: true } );
 		} );
 
 		test( 'should omit pagination query parameters', () => {
-			const query = PaginatedQueryKey.parse( '[["ok",true],["page",2]]' );
+			const query = PaginatedQueryKey.parse( '{"ok":true,"page":2}' );
 
 			expect( query ).to.eql( { ok: true } );
 		} );
