3308705f4153decea5f5f8a1bb9bdc6ce25ec1e6
Jarda Snajdr <jsnajdr@gmail.com>
2018-03-16 11:18:42 +0100

    Use wpcom.site().post().delete() to trash a post
    Trashing post is almost equivalent to saving the post with status field set to `trash`
    and the action behaves like it was doing exactly that -- it dispatches the `POST_SAVE_*`
    actions with the right properties.
    
    But what we really do is to call the `wpcom.site().post().delete()` method, i.e., sending
    a `POST /sites/:site/posts/:post/delete` request. The difference is that an explicit delete
    will set a `_wp_trash_meta_status` meta property on the post and a later `restore` call
    can restore the original post status, i.e., `publish`. Without this, the post will be always
    recovered as `draft`.

--- client/state/posts/actions.js
+++ client/state/posts/actions.js
@@ -256,7 +256,46 @@ export function savePost( siteId, postId = null, post ) {
  * @return {Function}        Action thunk
  */
 export function trashPost( siteId, postId ) {
-	return savePost( siteId, postId, { status: 'trash' } );
+	return dispatch => {
+		// Trashing post is almost equivalent to saving the post with status field set to `trash`
+		// and the action behaves like it was doing exactly that -- it dispatches the `POST_SAVE_*`
+		// actions with the right properties.
+		//
+		// But what we really do is to call the `wpcom.site().post().delete()` method, i.e., sending
+		// a `POST /sites/:site/posts/:post/delete` request. The difference is that an explicit delete
+		// will set a `_wp_trash_meta_status` meta property on the post and a later `restore` call
+		// can restore the original post status, i.e., `publish`. Without this, the post will be always
+		// recovered as `draft`.
+		const post = { status: 'trash' };
+
+		dispatch( {
+			type: POST_SAVE,
+			siteId,
+			postId,
+			post,
+		} );
+
+		const trashResult = wpcom
+			.site( siteId )
+			.post( postId )
+			.delete();
+
+		trashResult
+			.then( savedPost => {
+				dispatch( savePostSuccess( siteId, postId, savedPost, post ) );
+				dispatch( receivePost( savedPost ) );
+			} )
+			.catch( error => {
+				dispatch( {
+					type: POST_SAVE_FAILURE,
+					siteId,
+					postId,
+					error,
+				} );
+			} );
+
+		return trashResult;
+	};
 }
 
 /**
