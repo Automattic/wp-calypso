34cb069f8ae8a209f77a44a01ff59332964ea58c
Paul Sealock <psealock@gmail.com>
2018-03-16 13:44:05 +1300

    Store: Stats: Add Referrer Page

--- client/extensions/woocommerce/app/store-stats/controller.js
+++ client/extensions/woocommerce/app/store-stats/controller.js
@@ -13,16 +13,21 @@ import { moment, translate } from 'i18n-calypso';
 import AsyncLoad from 'components/async-load';
 import StatsPagePlaceholder from 'my-sites/stats/stats-page-placeholder';
 import { setDocumentHeadTitle as setTitle } from 'state/document-head/actions';
-import { getQueryDate } from './utils';
+import { getQueryDate, getUnitPeriod } from './utils';
 import analytics from 'lib/analytics';
 import titlecase from 'to-title-case';
 import { recordTrack } from 'woocommerce/lib/analytics';
+import { UNITS } from './constants';
+import config from 'config';
 
 function isValidParameters( context ) {
 	const validParameters = {
 		type: [ 'orders', 'products', 'categories', 'coupons' ],
 		unit: [ 'day', 'week', 'month', 'year' ],
 	};
+	if ( config.isEnabled( 'woocommerce/extension-referrers' ) ) {
+		validParameters.type.push( 'referrers' );
+	}
 	return Object.keys( validParameters ).every( param =>
 		includes( validParameters[ param ], context.params[ param ] )
 	);
@@ -66,6 +71,9 @@ export default function StatsController( context, next ) {
 		case 'coupons':
 			tracksEvent = 'calypso_woocommerce_stats_coupons_page';
 			break;
+		case 'referrers':
+			tracksEvent = 'calypso_woocommerce_stats_referrers_page';
+			break;
 	}
 	if ( tracksEvent ) {
 		recordTrack( tracksEvent, {
@@ -75,24 +83,47 @@ export default function StatsController( context, next ) {
 		} );
 	}
 
-	const asyncComponent =
-		props.type === 'orders' ? (
-			<AsyncLoad
-				/* eslint-disable wpcalypso/jsx-classname-namespace */
-				placeholder={ <StatsPagePlaceholder className="woocommerce" /> }
-				/* eslint-enable wpcalypso/jsx-classname-namespace */
-				require="extensions/woocommerce/app/store-stats"
-				{ ...props }
-			/>
-		) : (
-			<AsyncLoad
-				/* eslint-disable wpcalypso/jsx-classname-namespace */
-				placeholder={ <StatsPagePlaceholder className="woocommerce" /> }
-				/* eslint-enable wpcalypso/jsx-classname-namespace */
-				require="extensions/woocommerce/app/store-stats/listview"
-				{ ...props }
-			/>
-		);
+	let asyncComponent;
+	/* eslint-disable wpcalypso/jsx-classname-namespace */
+	const placeholder = <StatsPagePlaceholder className="woocommerce" />;
+	/* eslint-enable wpcalypso/jsx-classname-namespace */
+
+	switch ( props.type ) {
+		case 'orders':
+			asyncComponent = (
+				<AsyncLoad
+					placeholder={ placeholder }
+					require="extensions/woocommerce/app/store-stats"
+					{ ...props }
+				/>
+			);
+			break;
+		case 'referrers':
+			const referrersQuery = {
+				unit: props.unit,
+				date: getUnitPeriod( props.queryDate, props.unit ),
+				quantity: UNITS[ props.unit ].quantity,
+			};
+			asyncComponent = (
+				<AsyncLoad
+					placeholder={ placeholder }
+					require="extensions/woocommerce/app/store-stats/referrers"
+					query={ referrersQuery }
+					{ ...props }
+				/>
+			);
+			break;
+		default:
+			asyncComponent = (
+				<AsyncLoad
+					placeholder={ placeholder }
+					require="extensions/woocommerce/app/store-stats/listview"
+					{ ...props }
+				/>
+			);
+			break;
+	}
+
 	context.primary = asyncComponent;
 	next();
 }
