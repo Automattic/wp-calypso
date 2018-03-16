94a1803e0d608e96d0a8a19054270cc99a437bbb
Jon Surrell <jon.surrell@automattic.com>
2018-03-16 12:39:18 +0100

    Use unified queryManagerSchema for posts

--- client/state/posts/schema.js
+++ client/state/posts/schema.js
@@ -1,4 +1,10 @@
 /** @format */
+
+/**
+ * Internal dependencies
+ */
+import queryManagerSchema from 'lib/query-manager/schema';
+
 export const itemsSchema = {
 	type: 'object',
 	patternProperties: {
@@ -14,48 +20,6 @@ export const itemsSchema = {
 	additionalProperties: false,
 };
 
-const queryManagerSchema = {
-	type: 'object',
-	properties: {
-		data: {
-			type: 'object',
-			required: [ 'items', 'queries' ],
-			properties: {
-				items: {
-					type: 'object',
-				},
-				queries: {
-					patternProperties: {
-						// Query key pairs
-						'^\\[.*\\]$': {
-							type: 'object',
-							required: [ 'itemKeys' ],
-							properties: {
-								itemKeys: {
-									type: 'array',
-								},
-								found: {
-									type: 'number',
-								},
-							},
-						},
-					},
-					additionalProperties: false,
-				},
-			},
-		},
-		options: {
-			type: 'object',
-			required: [ 'itemKey' ],
-			properties: {
-				itemKey: {
-					type: 'string',
-				},
-			},
-		},
-	},
-};
-
 export const queriesSchema = {
 	type: 'object',
 	patternProperties: {
@@ -65,4 +29,4 @@ export const queriesSchema = {
 	additionalProperties: false,
 };
 
-export const allSitesQueriesSchema = queryManagerSchema;
+export { queryManagerSchema as allSitesQueriesSchema };
