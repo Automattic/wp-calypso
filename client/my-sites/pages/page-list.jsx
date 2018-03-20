292be609744aae91372160a37b2ddd73ecb21c23
Jarda Snajdr <jsnajdr@gmail.com>
2018-03-20 11:45:05 +0100

    Page List: read 'status' property from the 'query' object and check its value correctly
    The `status` prop is not set directly on the `Pages` component, but is a property of the
    `query` prop.
    
    The valid values don't include `published`. The right one is `publish,private` -- the output
    of a `mapPostStatus( 'published' )` call.

--- client/my-sites/pages/page-list.jsx
+++ client/my-sites/pages/page-list.jsx
@@ -199,8 +199,8 @@ class Pages extends Component {
 		);
 
 	getNoContentMessage() {
-		const { query = {}, translate, site, siteId } = this.props;
-		const { search, status = 'published' } = query;
+		const { query, translate, site, siteId } = this.props;
+		const { search, status } = query;
 
 		if ( search ) {
 			return (
@@ -291,16 +291,20 @@ class Pages extends Component {
 
 	renderPagesList( { pages } ) {
 		const { site, lastPage, query } = this.props;
-		const status = this.props.status || 'published';
 
 		// Pages only display hierarchically for published pages on single-sites when
 		// there are 100 or fewer pages and no more pages to load (last page).
 		// Pages are not displayed hierarchically for search.
-		if ( site && status === 'published' && lastPage && pages.length <= 100 && ! query.search ) {
-			return this.renderHierarchical( { pages, site } );
-		}
+		const showHierarchical =
+			site &&
+			query.status === 'publish,private' &&
+			lastPage &&
+			pages.length <= 100 &&
+			! query.search;
 
-		return this.renderChronological( { pages, site } );
+		return showHierarchical
+			? this.renderHierarchical( { pages, site } )
+			: this.renderChronological( { pages, site } );
 	}
 
 	renderHierarchical( { pages, site } ) {
@@ -354,7 +358,7 @@ class Pages extends Component {
 		}
 
 		const blogPostsPage = site &&
-			status === 'published' && (
+			this.props.query.status === 'publish,private' && (
 				<BlogPostsPage key="blog-posts-page" site={ site } pages={ pages } />
 			);
 
