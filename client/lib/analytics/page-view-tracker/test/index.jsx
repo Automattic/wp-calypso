cca476fcc782daa857a8921e3b86aaa003bae1c0
Copons <copons@users.noreply.github.com>
2018-03-20 17:36:25 +0000

    Fix failing tests

--- client/lib/analytics/page-view-tracker/test/index.jsx
+++ client/lib/analytics/page-view-tracker/test/index.jsx
@@ -27,7 +27,15 @@ describe( 'PageViewTracker', () => {
 	test( 'should immediately fire off event when given no delay', () => {
 		const recorder = spy();
 
-		mount( <PageViewTracker path="/test" title="test" recorder={ recorder } /> );
+		mount(
+			<PageViewTracker
+				path="/test"
+				title="test"
+				recorder={ recorder }
+				selectSiteId={ 12345678 }
+				sitesLoaded
+			/>
+		);
 
 		expect( recorder ).to.have.been.calledOnce;
 	} );
@@ -35,7 +43,16 @@ describe( 'PageViewTracker', () => {
 	test( 'should wait for the delay before firing off the event', () => {
 		const recorder = spy();
 
-		mount( <PageViewTracker delay={ 500 } path="/test" title="test" recorder={ recorder } /> );
+		mount(
+			<PageViewTracker
+				delay={ 500 }
+				path="/test"
+				title="test"
+				recorder={ recorder }
+				selectSiteId={ 12345678 }
+				sitesLoaded
+			/>
+		);
 
 		expect( recorder ).to.not.have.been.called;
 
@@ -47,7 +64,15 @@ describe( 'PageViewTracker', () => {
 	test( 'should pass the appropriate event information', () => {
 		const recorder = spy();
 
-		mount( <PageViewTracker path="/test" title="test" recorder={ recorder } /> );
+		mount(
+			<PageViewTracker
+				path="/test"
+				title="test"
+				recorder={ recorder }
+				selectSiteId={ 12345678 }
+				sitesLoaded
+			/>
+		);
 
 		expect( recorder ).to.have.been.calledWith( '/test', 'test' );
 	} );
