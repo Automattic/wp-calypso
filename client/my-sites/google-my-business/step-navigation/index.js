d8d26bc1e18fc1a045327ac7bc5ae5e625d0b9cb
Ben Dwyer <ben@scruffian.com>
2018-03-21 14:25:39 +0000

    desktop tweaks

--- client/my-sites/google-my-business/step-navigation/index.js
+++ client/my-sites/google-my-business/step-navigation/index.js
@@ -11,25 +11,28 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import Button from 'components/button';
 import Card from 'components/card';
 import ProgressBar from 'components/progress-bar';
 
 const StepNavigation = ( { backHref, nextHref, total, value, translate } ) => (
 	<Card className="step-navigation">
 		{ backHref && (
-			<a href={ backHref }>
+			<a href={ backHref } className="step-navigation__back">
 				<Gridicon icon="arrow-left" size={ 24 } className="step-navigation__left-arrow" />
 				{ translate( 'Back' ) }
 			</a>
 		) }
 		<ProgressBar value={ value } total={ total } />
 		{ value === total ? (
-			<a href={ nextHref }>{ translate( 'Done' ) }</a>
+			<Button primary href={ nextHref }>
+				{ translate( 'Done' ) }
+			</Button>
 		) : (
-			<a href={ nextHref }>
+			<Button primary href={ nextHref }>
 				{ translate( 'Next' ) }
 				<Gridicon icon="arrow-right" size={ 24 } className="step-navigation__right-arrow" />
-			</a>
+			</Button>
 		) }
 	</Card>
 );
