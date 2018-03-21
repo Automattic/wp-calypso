e5f86f6d09ee15fde6c864aa4bad2c795277e758
Bernhard Reiter <ockham@raz.or.at>
2018-03-21 17:20:54 +0100

    State: Allow siteId string (slug) as isRequestingSite arg

--- client/state/sites/selectors.js
+++ client/state/sites/selectors.js
@@ -365,9 +365,9 @@ export function isRequestingSites( state ) {
  * Returns true if a network request is in progress to fetch the specified, or
  * false otherwise.
  *
- * @param  {Object}  state  Global state tree
- * @param  {Number}  siteId Site ID
- * @return {Boolean}        Whether request is in progress
+ * @param  {Object}           state  Global state tree
+ * @param  {(Number|String)}  siteId Site ID or slug
+ * @return {Boolean}          Whether request is in progress
  */
 export function isRequestingSite( state, siteId ) {
 	return !! state.sites.requesting[ siteId ];
