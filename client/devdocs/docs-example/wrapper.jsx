59d43c4cd8a55db49ff6b95b9d949516fa2170a7
Chris R <chris@bluefuton.com>
2018-03-23 11:32:33 +1300

    Add error boundaries to Devdocs (#20252)

--- client/devdocs/docs-example/wrapper.jsx
+++ client/devdocs/docs-example/wrapper.jsx
@@ -7,6 +7,11 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import classNames from 'classnames';
 
+/**
+ * Internal dependencies
+ */
+import DocsExampleError from 'devdocs/docs-example/error';
+
 const renderTitle = ( unique, name, url ) =>
 	unique ? (
 		<h2 className="docs-example__wrapper-header-title">{ name }</h2>
@@ -23,6 +28,14 @@ class DocsExampleWrapper extends Component {
 		url: PropTypes.string.isRequired,
 	};
 
+	state = {
+		hasError: false,
+	};
+
+	componentDidCatch() {
+		this.setState( { hasError: true } );
+	}
+
 	render() {
 		const { children, name, unique, url } = this.props;
 
@@ -34,7 +47,9 @@ class DocsExampleWrapper extends Component {
 			>
 				<div className="docs-example__wrapper-header">{ renderTitle( unique, name, url ) }</div>
 				<div className="docs-example__wrapper-content">
-					<span className="docs-example__wrapper-content-centering">{ children }</span>
+					<span className="docs-example__wrapper-content-centering">
+						{ this.state.hasError ? <DocsExampleError /> : children }
+					</span>
 				</div>
 			</div>
 		);
