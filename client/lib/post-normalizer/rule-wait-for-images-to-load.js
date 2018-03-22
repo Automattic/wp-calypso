0e1043dc48709aa6910f600d50384c336bc4397a
bluefuton <chris@bluefuton.com>
2018-03-22 18:20:40 +1300

    Check 10 images, not 15

--- client/lib/post-normalizer/rule-wait-for-images-to-load.js
+++ client/lib/post-normalizer/rule-wait-for-images-to-load.js
@@ -103,7 +103,7 @@ export default function waitForImagesToLoad( post ) {
 
 		// convert to image objects to start the load process
 		// only check the first x images
-		const NUMBER_OF_IMAGES_TO_CHECK = 15;
+		const NUMBER_OF_IMAGES_TO_CHECK = 10;
 		let promises = map( take( imagesToCheck, NUMBER_OF_IMAGES_TO_CHECK ), imageUrl => {
 			if ( imageUrl in knownImages ) {
 				return Promise.resolve( knownImages[ imageUrl ] );
