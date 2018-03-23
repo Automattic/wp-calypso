dd0e90e0579e087e761c8b0b59b9b614c5beeeb0
Paul Sealock <psealock@gmail.com>
2018-03-23 23:02:55 +1300

    Store: Stats: Referrer: Add ability to search

--- client/extensions/woocommerce/app/store-stats/store-stats-referrer-widget/index.js
+++ client/extensions/woocommerce/app/store-stats/store-stats-referrer-widget/index.js
@@ -7,22 +7,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { find } from 'lodash';
-import { max as d3Max } from 'd3-array';
 import { localize } from 'i18n-calypso';
+import classnames from 'classnames';
 
 /**
  * Internal dependencies
  */
-import { getSiteStatsNormalizedData } from 'state/stats/lists/selectors';
 import Table from 'woocommerce/components/table';
 import TableRow from 'woocommerce/components/table/table-row';
 import TableItem from 'woocommerce/components/table/table-item';
-import HorizontalBar from 'woocommerce/components/d3/horizontal-bar';
 import Card from 'components/card';
 import ErrorPanel from 'my-sites/stats/stats-error';
-import { sortBySales } from 'woocommerce/app/store-stats/referrers/helpers';
-import { getWidgetPath } from 'woocommerce/app/store-stats/utils';
+import { getWidgetPath, formatValue } from 'woocommerce/app/store-stats/utils';
+import Pagination from 'components/pagination';
+import { getStoreReferrers } from 'state/selectors';
 
 class StoreStatsReferrerWidget extends Component {
 	static propTypes = {
@@ -30,29 +28,37 @@ class StoreStatsReferrerWidget extends Component {
 		query: PropTypes.object.isRequired,
 		siteId: PropTypes.number,
 		statType: PropTypes.string.isRequired,
-		selectedDate: PropTypes.string.isRequired,
+		unitSelectedDate: PropTypes.string.isRequired,
 		unit: PropTypes.string.isRequired,
 		queryParams: PropTypes.object.isRequired,
-		slug: PropTypes.string,
+		slug: PropTypes.string.isRequired,
+		pageType: PropTypes.string.isRequired,
+		paginate: PropTypes.bool,
+		selectedIndex: PropTypes.number,
+		selectedReferrer: PropTypes.string,
 	};
 
-	isPreCollection( selectedData ) {
+	state = {
+		page: 1,
+	};
+
+	isPreCollection( date ) {
 		const { moment } = this.props;
-		return moment( selectedData.date ).isBefore( moment( '2018-02-01' ) );
+		return moment( date ).isBefore( moment( '2018-02-01' ) );
 	}
 
-	hasNosaraJobRun( selectedData ) {
+	hasNosaraJobRun( date ) {
 		const { moment } = this.props;
 		const nowUtc = moment().utc();
 		const daysOffsetFromUtc = nowUtc.hour() >= 10 ? 1 : 2;
 		const lastValidDay = nowUtc.subtract( daysOffsetFromUtc, 'days' );
-		return lastValidDay.isAfter( moment( selectedData.date ) );
+		return lastValidDay.isAfter( moment( date ) );
 	}
 
-	getEmptyDataMessage( selectedData ) {
-		const { translate, slug, queryParams } = this.props;
-		if ( ! this.hasNosaraJobRun( selectedData ) ) {
-			const href = `/store/stats/orders${ getWidgetPath( 'week', slug, queryParams ) }`;
+	getEmptyDataMessage( date ) {
+		const { translate, slug, queryParams, pageType } = this.props;
+		if ( ! this.hasNosaraJobRun( date ) ) {
+			const href = `/store/stats/${ pageType }${ getWidgetPath( 'week', slug, queryParams ) }`;
 			const primary = translate( 'Data is being processed – check back soon' );
 			const secondary = translate(
 				'Expand to a {{a}}wider{{/a}} view to see your latest referrers',
@@ -64,25 +70,67 @@ class StoreStatsReferrerWidget extends Component {
 			);
 			return [ primary, <p key="link">{ secondary }</p> ];
 		}
-		return this.isPreCollection( selectedData )
+		return this.isPreCollection( date )
 			? [ translate( 'Referral data isn’t available before Jetpack v5.9 (March 2018)' ) ]
 			: [ translate( 'No referral activity on this date' ) ];
 	}
 
+	paginate = data => {
+		const { paginate, limit } = this.props;
+		if ( ! paginate ) {
+			return data.slice( 0, limit || data.length );
+		}
+		const { page } = this.state;
+		const start = ( page - 1 ) * limit;
+		const end = start + limit;
+		return data.slice( start, end );
+	};
+
+	onPageClick = pageNumber => {
+		this.setState( {
+			page: pageNumber,
+		} );
+	};
+
+	setPage( { selectedIndex, limit } ) {
+		if ( this.props.paginate ) {
+			this.setState( {
+				page: Math.floor( selectedIndex / limit ) + 1,
+			} );
+		}
+	}
+
+	componentWillMount() {
+		this.setPage( this.props );
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		this.setPage( nextProps );
+	}
+
 	render() {
-		const { data, selectedDate, translate, unit, slug, queryParams } = this.props;
+		const {
+			data,
+			unitSelectedDate,
+			translate,
+			unit,
+			slug,
+			queryParams,
+			limit,
+			paginate,
+			selectedReferrer,
+		} = this.props;
+		const { page } = this.state;
 		const basePath = '/store/stats/referrers';
-		const selectedData = find( data, d => d.date === selectedDate ) || { data: [] };
-		if ( selectedData.data.length === 0 ) {
-			const messages = this.getEmptyDataMessage( selectedData );
+		if ( data.length === 0 ) {
+			const messages = this.getEmptyDataMessage( unitSelectedDate );
 			return (
 				<Card className="store-stats-referrer-widget stats-module is-showing-error has-no-data">
 					<ErrorPanel message={ messages.shift() }>{ messages }</ErrorPanel>
 				</Card>
 			);
 		}
-		const sortedAndTrimmedData = sortBySales( selectedData.data, 5 );
-		const extent = [ 0, d3Max( sortedAndTrimmedData.map( d => d.sales ) ) ];
+		const paginatedData = this.paginate( data );
 		const header = (
 			<TableRow isHeader>
 				<TableItem isHeader isTitle>
@@ -92,37 +140,46 @@ class StoreStatsReferrerWidget extends Component {
 			</TableRow>
 		);
 		return (
-			<Table className="store-stats-referrer-widget" header={ header } compact>
-				{ sortedAndTrimmedData.map( d => {
-					const widgetPath = getWidgetPath(
-						unit,
-						slug,
-						Object.assign( {}, { referrer: d.referrer }, queryParams )
-					);
-					const href = `${ basePath }${ widgetPath }`;
-					return (
-						<TableRow key={ d.referrer }>
-							<TableItem isTitle>
-								<a href={ href }>{ d.referrer }</a>
-							</TableItem>
-							<TableItem>
-								<HorizontalBar
-									extent={ extent }
-									data={ d.sales }
-									currency={ d.currency }
-									height={ 20 }
-								/>
-							</TableItem>
-						</TableRow>
-					);
-				} ) }
-			</Table>
+			<Card className="store-stats-referrer-widget">
+				<Table header={ header } compact>
+					{ paginatedData.map( d => {
+						const widgetPath = getWidgetPath(
+							unit,
+							slug,
+							Object.assign( {}, queryParams, { referrer: d.referrer } )
+						);
+						const href = `${ basePath }${ widgetPath }`;
+						return (
+							<TableRow
+								key={ d.referrer }
+								href={ href }
+								className={ classnames( {
+									'is-selected': selectedReferrer === d.referrer,
+								} ) }
+							>
+								<TableItem isTitle>{ d.referrer }</TableItem>
+								<TableItem>{ formatValue( d.sales, 'currency', d.currency ) }</TableItem>
+							</TableRow>
+						);
+					} ) }
+				</Table>
+				{ paginate && (
+					<Pagination
+						compact
+						page={ page }
+						perPage={ limit }
+						total={ data.length }
+						pageClick={ this.onPageClick }
+					/>
+				) }
+			</Card>
 		);
 	}
 }
 
-export default connect( ( state, { siteId, statType, query } ) => {
+export default connect( ( state, ownProps ) => {
+	const { fetchedData } = ownProps;
 	return {
-		data: getSiteStatsNormalizedData( state, siteId, statType, query ),
+		data: fetchedData || getStoreReferrers( state, ownProps ),
 	};
 } )( localize( StoreStatsReferrerWidget ) );
