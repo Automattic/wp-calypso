e860df8a1402d595616d5fa8df2de905eab915cc
Bernhard Reiter <ockham@raz.or.at>
2018-03-26 16:32:46 +0200

    Data Layer: Add retry policy for POST request

--- client/state/data-layer/wpcom-http/pipeline/retry-on-failure/policies.js
+++ client/state/data-layer/wpcom-http/pipeline/retry-on-failure/policies.js
@@ -3,6 +3,18 @@ export const exponentialBackoff = ( { delay = 1000, maxAttempts = 3 } = {} ) => 
 	name: 'EXPONENTIAL_BACKOFF',
 	delay: Math.max( 500, delay ),
 	maxAttempts: Math.min( 5, maxAttempts ),
+	allowedMethods: [ 'GET' ],
+} );
+
+export const explicitPost = () => ( {
+	name: 'EXPLICITY_POST',
+	allowedMethods: [ 'POST' ],
+} );
+
+export const exponentialBackoffExplicitPost = ( { delay, maxAttempts } = {} ) => ( {
+	...exponentialBackoff( delay, maxAttempts ),
+	...explicitPost(),
+	name: 'EXPONENTIAL_BACKOFF_EXPLICIT_POST',
 } );
 
 export const noRetry = () => ( {
