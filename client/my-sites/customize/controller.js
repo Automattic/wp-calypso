a2b62703208d159e6c5f037f05dd54112cf61f9f
Jacopo Tomasone <Copons@users.noreply.github.com>
2018-03-27 18:51:27 +0100

    Analytics: Track /customize page views with normalized paths (#23584)
    Replace a direct `analytics.pageView.record` call with the `PageViewTracker` component which is aware of the selected site (no more incorrect `blog_id: undefined` reports).
    
    Update it to only track the `/customize/:site` path, which is the only possible path for this section.

--- client/my-sites/customize/controller.js
+++ client/my-sites/customize/controller.js
@@ -8,16 +8,10 @@ import React from 'react';
 /**
  * Internal Dependencies
  */
-import { sectionify } from 'lib/route';
-import analytics from 'lib/analytics';
 import { setDocumentHeadTitle as setTitle } from 'state/document-head/actions';
 import CustomizeComponent from 'my-sites/customize/main';
 
 export function customize( context, next ) {
-	const basePath = sectionify( context.path );
-
-	analytics.pageView.record( basePath, 'Customizer' );
-
 	// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
 	context.store.dispatch( setTitle( i18n.translate( 'Customizer', { textOnly: true } ) ) );
 
