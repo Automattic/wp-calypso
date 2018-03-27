0256bb8917fa061def72a2396cd0d5596edcaa84
Ben Lowery <ben.lowery@automattic.com>
2018-03-27 14:12:39 -0400

    Remove tween.js and use a home-rolled animator instead

--- client/lib/scroll-to/index.js
+++ client/lib/scroll-to/index.js
@@ -1,11 +1,6 @@
 /** @format */
 
-/**
- * External dependencies
- */
-
-import TWEEN from 'tween.js';
-import { defer } from 'lodash';
+const scrollers = new Map();
 
 function getCurrentScroll( container ) {
 	if ( container && container.scrollTop !== undefined ) {
@@ -17,34 +12,112 @@ function getCurrentScroll( container ) {
 
 	const x = window.pageXOffset || document.documentElement.scrollLeft,
 		y = window.pageYOffset || document.documentElement.scrollTop;
-	return { x: x, y: y };
+	return { x, y };
 }
 
 function makeScrollUpdater( container ) {
 	container = container && container.scrollTop !== undefined ? container : window;
 
-	return function updateScroll() {
+	return function updateScroll( x, y ) {
 		if ( container === window ) {
-			container.scrollTo( this.x, this.y );
+			container.scrollTo( x, y );
 		} else {
-			container.scrollTop = this.y;
-			container.scrollLeft = this.x;
+			container.scrollTop = y;
+			container.scrollLeft = x;
 		}
 	};
 }
 
-function animate() {
-	if ( ! TWEEN.getAll().length ) {
-		return;
+class Stepper {
+	constructor( options ) {
+		this.x = options.x;
+		this.y = options.y;
+		this.startTime = null;
+		this.duration = options.duration;
+		this.start = options.start;
+		this.end = options.end;
+		this.onStart = options.onStart;
+		this.onComplete = options.onComplete;
+		this.updater = options.updater;
+		this.easing = options.easing;
+		this.container = options.container;
 	}
 
-	if ( 'undefined' !== typeof window && window.requestAnimationFrame ) {
-		window.requestAnimationFrame( animate );
-	} else {
-		defer( animate );
+	animate() {
+		this.nextFrame = requestAnimationFrame( this.step );
 	}
 
-	TWEEN.update();
+	cancel() {
+		if ( this.nextFrame ) {
+			cancelAnimationFrame( this.nextFrame );
+			this.nextFrame = null;
+		}
+		if ( this.finishTimeout ) {
+			clearTimeout( this.finishTimeout );
+			this.finishTimeout = null;
+		}
+	}
+
+	jumpTo( x, y ) {
+		this.cancel();
+		this.end = {
+			x,
+			y,
+		};
+		this.updater( x, y );
+		this.finishTimeout = setTimeout( this.finish, this.duration );
+	}
+
+	finish = () => {
+		this.updater( this.end.x, this.end.y );
+		if ( this.onComplete ) {
+			this.onComplete();
+		}
+		scrollers.delete( this.container );
+	};
+
+	step = ts => {
+		this.nextFrame = null;
+		if ( ! this.startTime ) {
+			this.startTime = ts;
+			this.animate();
+			return;
+		}
+
+		// are we at or past our target duration?
+		// go to the last coordinate and finish up
+		if ( ts - this.startTime >= this.duration ) {
+			this.finish();
+			return;
+		}
+
+		// how far along are we as a percentage?
+		const progress = ( ts - this.startTime ) / this.duration;
+
+		// ease the progress
+		const easedProgress = this.easing( progress );
+
+		// figure out the new values
+		const newX = Math.round( this.start.x + ( this.end.x - this.start.x ) * easedProgress );
+		const newY = Math.round( this.start.y + ( this.end.y - this.start.y ) * easedProgress );
+
+		if ( newX !== this.x || newY !== this.y ) {
+			this.x = newX;
+			this.y = newY;
+			this.updater( newX, newY );
+		}
+
+		// set up the next turn
+		this.animate();
+	};
+}
+
+function circularInOutEasing( val ) {
+	if ( ( val *= 2 ) < 1 ) {
+		return -0.5 * ( Math.sqrt( 1 - val * val ) - 1 );
+	}
+
+	return 0.5 * ( Math.sqrt( 1 - ( val -= 2 ) * val ) + 1 );
 }
 
 /**
@@ -59,22 +132,35 @@ function animate() {
  * @param {HTMLElement} options.container - the container to scroll instead of window, if any
  */
 export default function scrollTo( options ) {
-	const currentScroll = getCurrentScroll( options.container ),
-		tween = new TWEEN.Tween( currentScroll )
-			.easing( options.easing || TWEEN.Easing.Circular.Out )
-			.to( { x: options.x, y: options.y }, options.duration || 500 )
-			.onUpdate( makeScrollUpdater( options.container ) );
+	const container = options.container || window;
+	if ( scrollers.has( container ) ) {
+		const scroller = scrollers.get( container );
+		scroller.jumpTo( options.x, options.y );
 
+		return;
+	}
+
+	const currentScroll = getCurrentScroll( container );
+
+	const stepper = new Stepper( {
+		start: currentScroll,
+		end: {
+			x: options.x,
+			y: options.y,
+		},
+		container,
+		easing: options.easing || circularInOutEasing,
+		duration: options.duration || 500,
+		updater: makeScrollUpdater( container ),
+		x: currentScroll.x,
+		y: currentScroll.y,
+		onStart: options.onStart,
+		onComplete: options.onComplete,
+	} );
+	scrollers.set( container, stepper );
+	stepper.animate();
 	if ( options.onStart ) {
-		tween.onStart( options.onStart );
+		options.onStart();
 	}
-	if ( options.onComplete ) {
-		tween.onComplete( options.onComplete );
-	}
-	if ( TWEEN.getAll().length === 0 ) {
-		tween.start();
-		animate();
-	} else {
-		tween.start();
-	}
+	return stepper;
 }
