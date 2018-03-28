cd4a5c8284d150ff366d764e2e99a0b739ba911e
James Tien <southp0105@gmail.com>
2018-03-28 14:48:12 +0800

    Implement the polling functionality of `QuerySourcePaymentTransactionDetail`

--- client/components/data/query-source-payment-transaction-detail/index.jsx
+++ client/components/data/query-source-payment-transaction-detail/index.jsx
@@ -4,19 +4,53 @@
  * External dependencies
  */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import { fetchSourcePaymentTransactionDetail } from 'state/transaction-detail/actions';
+import { getSourcePaymentTransactionDetail } from 'state/selectors';
 
 class QuerySourcePaymentTransactionDetail extends React.Component {
+	static propTypes = {
+		pollIntervalMs: PropTypes.int,
+	};
+
+	static defaultProps = {
+		pollIntervalMs: 0,
+	};
+
 	componentDidMount() {
-		this.props.fetchSourcePaymentTransactionDetail( this.props.orderId );
+		const { pollIntervalMs, orderId, transactionDetail, fetchTransactionDetail } = this.props;
+
+		if ( pollIntervalMs ) {
+			this.timer = setInterval( () => {
+				// no need to fetch if it's there.
+				if ( null !== transactionDetail ) {
+					return;
+				}
+				fetchTransactionDetail( orderId );
+			}, pollIntervalMs );
+			return;
+		}
+
+		this.props.fetchTransactionDetail( orderId );
+	}
+
+	componentWillUnmount() {
+		if ( this.timer ) {
+			clearInterval( this.timer );
+		}
 	}
 }
 
-export default connect( () => {}, {
-	fetchSourcePaymentTransactionDetail,
-} )( QuerySourcePaymentTransactionDetail );
+export default connect(
+	( state, props ) => ( {
+		transactionDetail: getSourcePaymentTransactionDetail( state, props.orderId ),
+	} ),
+	{
+		fetchTransactionDetail: fetchSourcePaymentTransactionDetail,
+	}
+)( QuerySourcePaymentTransactionDetail );
