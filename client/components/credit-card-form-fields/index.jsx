0fc32ce2dd41ae40b9176e6a3f7a557af11099e9
Kat Hagan <kat@codebykat.com>
2018-03-28 03:09:34 -0700

    remove extraneous translation context from expiry

--- client/components/credit-card-form-fields/index.jsx
+++ client/components/credit-card-form-fields/index.jsx
@@ -217,9 +217,7 @@ export class CreditCardFormFields extends React.Component {
 				<div className={ creditCardFormFieldsExtrasClassNames }>
 					{ this.createField( 'expiration-date', Input, {
 						inputMode: 'numeric',
-						label: translate( 'Expiry: MM/YY', {
-							context: 'Expiry label on credit card form',
-						} ),
+						label: translate( 'Expiry: MM/YY' ),
 					} ) }
 
 					{ this.createField( 'cvv', Input, {
