2b983edee0707bf3f9aa8773f10474ce27b5aa2b
Yury Michurin <yury.michurin@gmail.com>
2018-03-28 17:04:43 +0300

    Change url of GMB business type select page

--- client/my-sites/google-my-business/index.js
+++ client/my-sites/google-my-business/index.js
@@ -15,7 +15,7 @@ import { makeLayout, render as clientRender } from 'controller';
 export default function() {
 	page( '/google-my-business', siteSelection, sites, makeLayout, clientRender );
 	page(
-		'/google-my-business/:site_id/',
+		'/google-my-business/:site_id/select-business-type',
 		siteSelection,
 		navigation,
 		selectBusinessType,
