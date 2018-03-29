617e652e4d27dea679de59b80c6bb32ab7f183fb
Copons <copons@users.noreply.github.com>
2018-03-29 19:51:48 +0100

    Replace Chai with Jest

--- client/lib/analytics/test/utils.js
+++ client/lib/analytics/test/utils.js
@@ -1,34 +1,28 @@
 /** @format */
 /**
- * External dependencies
- */
-
-import { expect } from 'chai';
-
-/**
  * Internal dependencies
  */
 import { shouldReportOmitBlogId } from '../utils';
 
 describe( '#shouldReportOmitBlogId', () => {
 	test( 'should allow blog_id reporting for site-specific paths', () => {
-		expect( shouldReportOmitBlogId( '/stats/day/example.wordpress.com' ) ).to.be.false;
-		expect( shouldReportOmitBlogId( '/pages/example.wordpress.com' ) ).to.be.false;
-		expect( shouldReportOmitBlogId( '/posts/example.wordpress.com' ) ).to.be.false;
-		expect( shouldReportOmitBlogId( '/media/example.wordpress.com' ) ).to.be.false;
-		expect( shouldReportOmitBlogId( '/comments/all/example.wordpress.com' ) ).to.be.false;
-		expect( shouldReportOmitBlogId( '/plugins/example.wordpress.com' ) ).to.be.false;
-		expect( shouldReportOmitBlogId( '/domains/manage/example.wordpress.com' ) ).to.be.false;
-		expect( shouldReportOmitBlogId( '/settings/general/example.wordpress.com' ) ).to.be.false;
+		expect( shouldReportOmitBlogId( '/stats/day/example.wordpress.com' ) ).toBe.false;
+		expect( shouldReportOmitBlogId( '/pages/example.wordpress.com' ) ).toBe.false;
+		expect( shouldReportOmitBlogId( '/posts/example.wordpress.com' ) ).toBe.false;
+		expect( shouldReportOmitBlogId( '/media/example.wordpress.com' ) ).toBe.false;
+		expect( shouldReportOmitBlogId( '/comments/all/example.wordpress.com' ) ).toBe.false;
+		expect( shouldReportOmitBlogId( '/plugins/example.wordpress.com' ) ).toBe.false;
+		expect( shouldReportOmitBlogId( '/domains/manage/example.wordpress.com' ) ).toBe.false;
+		expect( shouldReportOmitBlogId( '/settings/general/example.wordpress.com' ) ).toBe.false;
 	} );
 	test( 'should not allow blog_id reporting for general administration paths', () => {
-		expect( shouldReportOmitBlogId( '/' ) ).to.be.true;
-		expect( shouldReportOmitBlogId( '/me' ) ).to.be.true;
-		expect( shouldReportOmitBlogId( '/help' ) ).to.be.true;
-		expect( shouldReportOmitBlogId( '/read' ) ).to.be.true;
-		expect( shouldReportOmitBlogId( '/following' ) ).to.be.true;
-		expect( shouldReportOmitBlogId( '/discover' ) ).to.be.true;
-		expect( shouldReportOmitBlogId( '/activities' ) ).to.be.true;
-		expect( shouldReportOmitBlogId( '/tag' ) ).to.be.true;
+		expect( shouldReportOmitBlogId( '/' ) ).toBe.true;
+		expect( shouldReportOmitBlogId( '/me' ) ).toBe.true;
+		expect( shouldReportOmitBlogId( '/help' ) ).toBe.true;
+		expect( shouldReportOmitBlogId( '/read' ) ).toBe.true;
+		expect( shouldReportOmitBlogId( '/following' ) ).toBe.true;
+		expect( shouldReportOmitBlogId( '/discover' ) ).toBe.true;
+		expect( shouldReportOmitBlogId( '/activities' ) ).toBe.true;
+		expect( shouldReportOmitBlogId( '/tag' ) ).toBe.true;
 	} );
 } );
