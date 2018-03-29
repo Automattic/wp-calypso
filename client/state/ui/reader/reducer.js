83feafe258f8516805a105260244d8c94df86b21
Jake F - @samouri <samouri@users.noreply.github.com>
2018-03-29 15:07:24 -0400

    fix duplicated reducer

--- client/state/ui/reader/reducer.js
+++ client/state/ui/reader/reducer.js
@@ -8,16 +8,6 @@ import { READER_VIEW_STREAM } from 'state/action-types';
 import sidebar from './sidebar/reducer';
 import { combineReducers } from 'state/utils';
 import cardExpansions from './card-expansions/reducer';
-import { READER_VIEW_STREAM } from 'state/action-types';
-
-/**
- * Holds the viewed touched stream for the purposes of keyboard navigation
- *
- * @param {*} state
- * @param {*} action
- */
-export const currentStream = ( state = null, action ) =>
-	action.type === READER_VIEW_STREAM ? action.payload.streamKey : state;
 
 /*
  * Holds the last viewed stream for the purposes of keyboard navigation
