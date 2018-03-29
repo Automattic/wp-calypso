bd81a99146c1b01991366ae32ba4b5a84e9b9c05
Jake F - @samouri <samouri@users.noreply.github.com>
2018-03-28 22:36:52 -0400

    fix tests w ugly mocky mock

--- client/state/reader/streams/test/selectors.js
+++ client/state/reader/streams/test/selectors.js
@@ -9,6 +9,8 @@ import freeze from 'deep-freeze';
  */
 import { getOffsetItem, getTransformedStreamItems, shouldRequestRecs } from '../selectors';
 
+jest.mock( 'lib/user/utils', () => ( { getLocaleSlug: () => 'en' } ) );
+
 const postKey1 = { postId: 1, feedId: 1 };
 const postKey2 = { postId: 1, blogId: 1 };
 const postKey3 = { postId: 2, feedId: 1 };
@@ -17,13 +19,14 @@ const currentStream = { ui: { reader: { currentStream: 'following' } } };
 
 describe( 'getOffsetItem', () => {
 	test( 'should return null when not given a currentItem', () => {
-		const state = freeze( { reader: { streams: {} } }, ...currentStream );
+		const state = freeze( { reader: { streams: {} }, ...currentStream } );
 		expect( getOffsetItem( state ) ).toBe( null );
 	} );
 
 	test( 'should return null for out of bounds', () => {
 		const state = freeze( {
 			reader: { streams: { following: { items: [ postKey1 ] } } },
+			...currentStream,
 		} );
 		expect( getOffsetItem( state, postKey1, 1 ) ).toBe( null );
 		expect( getOffsetItem( state, postKey1, -1 ) ).toBe( null );
@@ -32,6 +35,7 @@ describe( 'getOffsetItem', () => {
 	test( 'should return items offset by one in either direction', () => {
 		const state = freeze( {
 			reader: { streams: { following: { items: [ postKey1, postKey2, postKey3 ] } } },
+			...currentStream,
 		} );
 		expect( getOffsetItem( state, postKey2, 1 ) ).toBe( postKey3 );
 		expect( getOffsetItem( state, postKey2, -1 ) ).toBe( postKey1 );
