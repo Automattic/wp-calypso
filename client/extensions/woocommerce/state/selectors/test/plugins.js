f48924eebecb287ab0d7710919912328420012d8
Kelly Dwan <ryelle@users.noreply.github.com>
2018-04-02 17:21:43 -0400

    Store State: Add tests for required plugins selector

--- client/extensions/woocommerce/state/selectors/test/plugins.js
+++ client/extensions/woocommerce/state/selectors/test/plugins.js
@@ -2,66 +2,60 @@
  * External dependencies
  */
 import { expect } from 'chai';
+import deepFreeze from 'deep-freeze';
 import sinon from 'sinon';
 
 /**
  * Internal dependencies
  */
+import { areAllRequiredPluginsActive, isWcsEnabled } from '../plugins';
 import config from 'config';
-import {
-	isWcsEnabled,
-} from '../plugins';
+import plugins from './fixtures/plugins.js';
 
-const siteId = 123;
-
-const getState = ( loaded = false, installed = false, active = false ) => {
-	const pluginArray = [];
-	if ( loaded && installed ) {
-		pluginArray.push( {
-			id: 'woocommerce-services/woocommerce-services',
-			slug: 'woocommerce-services',
-			active,
-		} );
-	}
-
-	return {
-		plugins: {
-			installed: {
-				plugins: {
-					[ siteId ]: pluginArray,
-				},
-				isRequesting: {
-					[ siteId ]: ! loaded,
-				},
-			},
-		},
-	};
-};
+const state = deepFreeze( { plugins } );
 
 describe( 'plugins', () => {
+	describe( '#areAllRequiredPluginsActive', () => {
+		it( 'should be null if the plugin list is being requested', () => {
+			expect( areAllRequiredPluginsActive( state, 'site.four' ) ).to.be.null;
+		} );
+
+		it( 'should be false if a required plugin is not installed', () => {
+			expect( areAllRequiredPluginsActive( state, 'site.three' ) ).to.be.false;
+		} );
+
+		it( 'should be false if a required plugin is not active', () => {
+			expect( areAllRequiredPluginsActive( state, 'site.two' ) ).to.be.false;
+		} );
+
+		it( 'should be true if the plugin is installed and active', () => {
+			expect( areAllRequiredPluginsActive( state, 'site.one' ) ).to.be.true;
+		} );
+	} );
+
 	describe( '#isWcsEnabled', () => {
 		it( 'should be false if WCS is disabled in the config', () => {
 			const configStub = sinon.stub( config, 'isEnabled' );
 			configStub.withArgs( 'woocommerce/extension-wcservices' ).returns( false );
 
-			expect( isWcsEnabled( getState(), siteId ) ).to.be.false;
+			expect( isWcsEnabled( state, 'site.one' ) ).to.be.false;
 			configStub.restore();
 		} );
 
 		it( 'should be null if the plugin list is being requested', () => {
-			expect( isWcsEnabled( getState( false ), siteId ) ).to.be.null;
+			expect( isWcsEnabled( state, 'site.four' ) ).to.be.null;
 		} );
 
 		it( 'should be false if the plugin is not installed', () => {
-			expect( isWcsEnabled( getState( true, false ), siteId ) ).to.be.false;
+			expect( isWcsEnabled( state, 'site.three' ) ).to.be.false;
 		} );
 
 		it( 'should be false if the plugin is not active', () => {
-			expect( isWcsEnabled( getState( true, true, false ), siteId ) ).to.be.false;
+			expect( isWcsEnabled( state, 'site.two' ) ).to.be.false;
 		} );
 
 		it( 'should be true if the plugin is installed and active', () => {
-			expect( isWcsEnabled( getState( true, true, true ), siteId ) ).to.be.true;
+			expect( isWcsEnabled( state, 'site.one' ) ).to.be.true;
 		} );
 	} );
 } );
