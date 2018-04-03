f4ab837218f5a677bf43428b0890792e82f0f733
Bernhard Reiter <ockham@raz.or.at>
2018-04-03 16:59:39 +0200

    Remove one nested testing layer

--- client/state/jetpack/onboarding/test/reducer.js
+++ client/state/jetpack/onboarding/test/reducer.js
@@ -18,117 +18,115 @@ import {
 import { useSandbox } from 'test/helpers/use-sinon';
 
 describe( 'reducer', () => {
-	describe( 'credentials', () => {
-		useSandbox( sandbox => {
-			sandbox.stub( console, 'warn' );
+	useSandbox( sandbox => {
+		sandbox.stub( console, 'warn' );
+	} );
+
+	const siteCredentials = {
+		token: 'abcd1234',
+		siteUrl: 'http://yourgroovydomain.com/',
+		userEmail: 'somebody@yourgroovydomain.com',
+	};
+
+	test( 'should default to an empty object', () => {
+		const state = reducer( undefined, {} );
+		expect( state ).toEqual( {} );
+	} );
+
+	test( 'should index credentials by siteId', () => {
+		const siteId = 12345678;
+		const initialState = deepFreeze( {} );
+		const state = reducer( initialState, {
+			type: JETPACK_ONBOARDING_CREDENTIALS_RECEIVE,
+			siteId,
+			credentials: siteCredentials,
 		} );
 
-		const siteCredentials = {
-			token: 'abcd1234',
-			siteUrl: 'http://yourgroovydomain.com/',
-			userEmail: 'somebody@yourgroovydomain.com',
+		expect( state ).toEqual( {
+			[ siteId ]: siteCredentials,
+		} );
+	} );
+
+	test( 'should store credentials for new sites', () => {
+		const siteId = 87654321;
+		const initialState = deepFreeze( {
+			[ 12345678 ]: siteCredentials,
+		} );
+		const state = reducer( initialState, {
+			type: JETPACK_ONBOARDING_CREDENTIALS_RECEIVE,
+			siteId,
+			credentials: siteCredentials,
+		} );
+
+		expect( state ).toEqual( {
+			...initialState,
+			[ siteId ]: siteCredentials,
+		} );
+	} );
+
+	test( 'should update credentials for the particular site', () => {
+		const siteId = 12345678;
+		const newCredentials = {
+			...siteCredentials,
+			token: 'efgh5678',
 		};
-
-		test( 'should default to an empty object', () => {
-			const state = reducer( undefined, {} );
-			expect( state ).toEqual( {} );
+		const initialState = deepFreeze( {
+			[ siteId ]: siteCredentials,
+			[ 87654321 ]: siteCredentials,
+		} );
+		const state = reducer( initialState, {
+			type: JETPACK_ONBOARDING_CREDENTIALS_RECEIVE,
+			siteId,
+			credentials: newCredentials,
 		} );
 
-		test( 'should index credentials by siteId', () => {
-			const siteId = 12345678;
-			const initialState = deepFreeze( {} );
-			const state = reducer( initialState, {
-				type: JETPACK_ONBOARDING_CREDENTIALS_RECEIVE,
-				siteId,
-				credentials: siteCredentials,
-			} );
+		expect( state ).toEqual( {
+			...initialState,
+			[ siteId ]: newCredentials,
+		} );
+	} );
 
-			expect( state ).toEqual( {
-				[ siteId ]: siteCredentials,
-			} );
+	test( 'should remove credentials when site connects successfully', () => {
+		const siteId = 12345678;
+		const original = deepFreeze( {
+			[ siteId ]: siteCredentials,
+		} );
+		const state = reducer( original, {
+			type: JETPACK_CONNECT_AUTHORIZE_RECEIVE,
+			siteId,
 		} );
 
-		test( 'should store credentials for new sites', () => {
-			const siteId = 87654321;
-			const initialState = deepFreeze( {
-				[ 12345678 ]: siteCredentials,
-			} );
-			const state = reducer( initialState, {
-				type: JETPACK_ONBOARDING_CREDENTIALS_RECEIVE,
-				siteId,
-				credentials: siteCredentials,
-			} );
+		expect( state ).toEqual( {} );
+	} );
 
-			expect( state ).toEqual( {
-				...initialState,
-				[ siteId ]: siteCredentials,
-			} );
+	test( 'should persist state', () => {
+		const original = deepFreeze( {
+			[ 12345678 ]: siteCredentials,
+		} );
+		const state = reducer( original, { type: SERIALIZE } );
+
+		expect( state ).toEqual( original );
+	} );
+
+	test( 'should load valid persisted state', () => {
+		const original = deepFreeze( {
+			[ 12345678 ]: siteCredentials,
 		} );
 
-		test( 'should update credentials for the particular site', () => {
-			const siteId = 12345678;
-			const newCredentials = {
+		const state = reducer( original, { type: DESERIALIZE } );
+
+		expect( state ).toEqual( original );
+	} );
+
+	test( 'should not load invalid persisted state', () => {
+		const original = deepFreeze( {
+			[ 12345678 ]: {
 				...siteCredentials,
-				token: 'efgh5678',
-			};
-			const initialState = deepFreeze( {
-				[ siteId ]: siteCredentials,
-				[ 87654321 ]: siteCredentials,
-			} );
-			const state = reducer( initialState, {
-				type: JETPACK_ONBOARDING_CREDENTIALS_RECEIVE,
-				siteId,
-				credentials: newCredentials,
-			} );
-
-			expect( state ).toEqual( {
-				...initialState,
-				[ siteId ]: newCredentials,
-			} );
+				token: {},
+			},
 		} );
+		const state = reducer( original, { type: DESERIALIZE } );
 
-		test( 'should remove credentials when site connects successfully', () => {
-			const siteId = 12345678;
-			const original = deepFreeze( {
-				[ siteId ]: siteCredentials,
-			} );
-			const state = reducer( original, {
-				type: JETPACK_CONNECT_AUTHORIZE_RECEIVE,
-				siteId,
-			} );
-
-			expect( state ).toEqual( {} );
-		} );
-
-		test( 'should persist state', () => {
-			const original = deepFreeze( {
-				[ 12345678 ]: siteCredentials,
-			} );
-			const state = reducer( original, { type: SERIALIZE } );
-
-			expect( state ).toEqual( original );
-		} );
-
-		test( 'should load valid persisted state', () => {
-			const original = deepFreeze( {
-				[ 12345678 ]: siteCredentials,
-			} );
-
-			const state = reducer( original, { type: DESERIALIZE } );
-
-			expect( state ).toEqual( original );
-		} );
-
-		test( 'should not load invalid persisted state', () => {
-			const original = deepFreeze( {
-				[ 12345678 ]: {
-					...siteCredentials,
-					token: {},
-				},
-			} );
-			const state = reducer( original, { type: DESERIALIZE } );
-
-			expect( state ).toEqual( {} );
-		} );
+		expect( state ).toEqual( {} );
 	} );
 } );
