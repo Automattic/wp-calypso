e7b2b037d2f2ab6539280d663e3123b4a89cca8a
Bernie Reiter <ockham@raz.or.at>
2018-04-03 18:01:38 +0200

    JP Settings (data-layer based): Keep Track of Module Activation State (#23855)
    This part of state isn't used by selectors (yet). However, the reducer does listen to those action types.
    
    Reducer and tests basically duplicated from `state/jetpack/settings`, with some minor modifications (to work with `keyedReducer` and Jest). This is the same strategy as with #23774 -- duplicate temporarily so stuff is readily available when switching over.

--- client/state/jetpack-onboarding/reducer.js
+++ client/state/jetpack-onboarding/reducer.js
@@ -3,14 +3,18 @@
 /**
  * External dependencies
  */
-import { merge } from 'lodash';
+import { mapValues, merge } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { createReducer, combineReducers, keyedReducer } from 'state/utils';
 import { jetpackOnboardingCredentialsSchema, jetpackSettingsSchema } from './schema';
+import { normalizeSettings } from 'state/jetpack/settings/utils';
 import {
+	JETPACK_MODULE_ACTIVATE_SUCCESS,
+	JETPACK_MODULE_DEACTIVATE_SUCCESS,
+	JETPACK_MODULES_RECEIVE,
 	JETPACK_CONNECT_AUTHORIZE_RECEIVE,
 	JETPACK_ONBOARDING_CREDENTIALS_RECEIVE,
 	JETPACK_ONBOARDING_SETTINGS_SAVE_SUCCESS,
@@ -35,6 +39,31 @@ export const settingsReducer = keyedReducer(
 	createReducer(
 		{},
 		{
+			[ JETPACK_MODULE_ACTIVATE_SUCCESS ]: ( state, { moduleSlug } ) => ( {
+				...state,
+				[ moduleSlug ]: true,
+			} ),
+			[ JETPACK_MODULE_DEACTIVATE_SUCCESS ]: ( state, { moduleSlug } ) => ( {
+				...state,
+				[ moduleSlug ]: false,
+			} ),
+			[ JETPACK_MODULES_RECEIVE ]: ( state, { modules } ) => {
+				const modulesActivationState = mapValues( modules, module => module.active );
+				// The need for flattening module options into this moduleSettings is temporary.
+				// Once https://github.com/Automattic/jetpack/pull/6002 is released,
+				// the flattening will be done on the server side for the /jetpack/v4/settings/ endpoint
+				const moduleSettings = Object.keys( modules ).reduce( ( allTheSettings, slug ) => {
+					return {
+						...allTheSettings,
+						...mapValues( modules[ slug ].options, option => option.current_value ),
+					};
+				}, {} );
+				return {
+					...state,
+					...modulesActivationState,
+					...normalizeSettings( moduleSettings ),
+				};
+			},
 			[ JETPACK_ONBOARDING_SETTINGS_SAVE_SUCCESS ]: (
 				state,
 				{ settings: { post_by_email_address } }
