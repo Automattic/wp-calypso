4e8a10ffdc5dc8b4bf6860f6a833f63411711805
bluefuton <chris@bluefuton.com>
2018-04-05 15:41:27 +1200

    Address review feedback

--- client/lib/posts/utils.js
+++ client/lib/posts/utils.js
@@ -15,12 +15,13 @@ import postNormalizer from 'lib/post-normalizer';
 
 export const getEditURL = function( post, site ) {
 	let basePath = '';
+	const postType = post.type || 'post';
 
-	if ( post.type && ! includes( [ 'post', 'page' ], post.type ) ) {
+	if ( ! includes( [ 'post', 'page' ], postType ) ) {
 		basePath = '/edit';
 	}
 
-	return `${ basePath }/${ post.type || 'post' }/${ site.slug }/${ post.ID }`;
+	return `${ basePath }/${ postType || 'post' }/${ site.slug }/${ post.ID }`;
 };
 
 export const getPreviewURL = function( site, post ) {
