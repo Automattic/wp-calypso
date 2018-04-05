ab88016edecc88c63a82378e11e99c0aa7cd8cda
Andrija Vucinic <andrija.vucinic@gmail.com>
2018-04-05 21:57:20 +0100

    Domains: Update postal code sanitizer
    Try to improve the postal code sanitizer. We try
    to strip all delimeters users might input into postal codes
    and then add the delimeters exactly where they should be.t

--- client/lib/postal-code/test/index.jsx
+++ client/lib/postal-code/test/index.jsx
@@ -15,11 +15,19 @@ describe( 'Postal Code Utils', () => {
 		assert.equal( tryToGuessPostalCodeFormat( 'WC1R4PF', 'GB' ), 'WC1R 4PF' );
 	} );
 
-	test( 'should format valid GB code, length 6', () => {
+	test( 'should format valid GB code, length 5', () => {
 		assert.equal( tryToGuessPostalCodeFormat( 'M11AA', 'GB' ), 'M1 1AA' );
 	} );
 
-	test( 'should format valid GB code, length 5', () => {
+	test( 'should format valid GB code, length 6', () => {
 		assert.equal( tryToGuessPostalCodeFormat( 'B338TH', 'GB' ), 'B33 8TH' );
+	} );
+
+	test( 'should format valid GB code, spaces wrong, length 6', () => {
+		assert.equal( tryToGuessPostalCodeFormat( 'B 338T H', 'GB' ), 'B33 8TH' );
+	} );
+
+	test( 'should format valid GB code, wrong delimeter, length 6', () => {
+		assert.equal( tryToGuessPostalCodeFormat( 'B33-8TH', 'GB' ), 'B33 8TH' );
 	} );
 } );
