d379f776b78c7c80f992ec5a03a21e2bf6fc327e
Jason Moon <jason.moon@automattic.com>
2018-04-06 14:32:41 -0600

    [Domains] Reset filters on search input clear

--- client/components/domains/register-domain-step/index.jsx
+++ client/components/domains/register-domain-step/index.jsx
@@ -413,17 +413,22 @@ class RegisterDomainStep extends React.Component {
 	};
 
 	repeatSearch = ( stateOverride = {}, { shouldQuerySubdomains = true } = {} ) => {
+		this.save();
+
+		const { lastQuery } = this.state;
+		const loadingResults = Boolean( getFixedDomainSearch( lastQuery ) );
+
 		const nextState = {
 			exactMatchDomain: null,
 			lastDomainSearched: null,
-			loadingResults: true,
-			loadingSubdomainResults: true,
+			loadingResults,
+			loadingSubdomainResults: loadingResults,
 			notice: null,
 			...stateOverride,
 		};
 		debug( 'Repeating a search with the following input for setState', nextState );
 		this.setState( nextState, () => {
-			this.onSearch( this.state.lastQuery, { shouldQuerySubdomains } );
+			loadingResults && this.onSearch( lastQuery, { shouldQuerySubdomains } );
 		} );
 	};
 
@@ -448,7 +453,9 @@ class RegisterDomainStep extends React.Component {
 			{
 				filters: this.getInitialFiltersState(),
 			},
-			this.repeatSearch
+			() => {
+				this.repeatSearch();
+			}
 		);
 	};
 
@@ -474,6 +481,7 @@ class RegisterDomainStep extends React.Component {
 				pageNumber: 1,
 				searchResults: null,
 				subdomainSearchResults: null,
+				...( loadingResults ? { filters: this.getInitialFiltersState() } : {} ),
 			},
 			callback
 		);
