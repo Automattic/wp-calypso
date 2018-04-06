9fc2380e13908e859755c8bd8bfc11557b3fc13a
Caleb Bauermeister <cbauerman@gmail.com>
2018-04-05 19:17:52 -0700

    Add Search Data section with placeholder data.

--- client/my-sites/google-my-business/stats/index.js
+++ client/my-sites/google-my-business/stats/index.js
@@ -15,19 +15,24 @@ import Main from 'components/main';
 import StatsFirstView from 'my-sites/stats/stats-first-view';
 import StatsNavigation from 'blocks/stats-navigation';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
-import EmptyContent from 'components/empty-content';
 import { getSelectedSiteSlug, getSelectedSiteId } from 'state/ui/selectors';
 import DocumentHead from 'components/data/document-head';
+import Card from 'components/card';
+import SectionHeader from 'components/section-header';
+import PieChart from 'components/pie-chart';
+import SearchDataType from './search-data-type';
 
 class GoogleMyBusinessStats extends Component {
 	static propTypes = {
 		translate: PropTypes.func.isRequired,
 		siteSlug: PropTypes.string.isRequired,
 		siteId: PropTypes.number.isRequired,
+		searchData: PropTypes.arrayOf( SearchDataType ).isRequired,
+		searchDataTotal: PropTypes.number.isRequired,
 	};
 
 	render() {
-		const { translate, siteSlug, siteId } = this.props;
+		const { translate, siteSlug, siteId, searchData, searchDataTotal } = this.props;
 
 		return (
 			<Main wideLayout>
@@ -35,13 +40,44 @@ class GoogleMyBusinessStats extends Component {
 				<StatsFirstView />
 				<SidebarNavigation />
 				<StatsNavigation selectedItem={ 'googleMyBusiness' } siteId={ siteId } slug={ siteSlug } />
-				<EmptyContent title={ translate( 'Google My Business Statistics' ) } />
+
+				<div>
+					<SectionHeader label={ translate( 'How customers search for your business' ) } />
+					<Card>
+						<PieChart
+							data={ searchData }
+							title={ translate( '%(searchDataTotal)d Total Searches', {
+								args: { searchDataTotal },
+							} ) }
+						/>
+					</Card>
+				</div>
 			</Main>
 		);
 	}
 }
 
-export default connect( state => ( {
-	siteSlug: getSelectedSiteSlug( state ),
-	siteId: getSelectedSiteId( state ),
-} ) )( localize( GoogleMyBusinessStats ) );
+const dataSummer = ( prevResult, datum ) => prevResult + datum.value;
+
+export default connect( state => {
+	const searchData = [
+		{
+			name: 'Direct',
+			description: 'Customers who find your listing searching for you business name or address',
+			value: 362,
+		},
+		{
+			name: 'Discovery',
+			description: 'Customers who findyour listing searching for a category, product, or service',
+			value: 189,
+		},
+	];
+	const searchDataTotal = searchData.reduce( dataSummer, 0 );
+
+	return {
+		siteSlug: getSelectedSiteSlug( state ),
+		siteId: getSelectedSiteId( state ),
+		searchData,
+		searchDataTotal,
+	};
+} )( localize( GoogleMyBusinessStats ) );
