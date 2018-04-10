8fe62fa99665a5efcee368e89001d4d06431380a
undemian <undemian@gmail.com>
2018-04-11 00:48:09 +0300

    Add cleanup tag

--- client/me/chat/main.jsx
+++ client/me/chat/main.jsx
@@ -12,7 +12,7 @@ import { localize } from 'i18n-calypso';
  * Internal dependencies
  */
 
-// TODO: this is not used anymore, instead we use blocks/happychat/page, we keep it to make sure we
+// TODO: HAPPYCHAT_CLEANUP this is not used anymore, instead we use blocks/happychat/page, we keep it to make sure we
 // have happychat-client parity, it should be removed when doing happychat cleanup.
 
 import config from 'config';
