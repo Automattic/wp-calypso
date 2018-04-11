ff0fbea7211023e4b7f74479b439c51372bc4c3e
Jason Moon <jason.moon@automattic.com>
2018-04-11 16:07:35 -0600

    [Domains] Adjust styling for featured suggestions
    - Add content responsive font sizing
    - Handle missing primary or secondary featured suggestions

--- client/components/domains/featured-domain-suggestions/index.jsx
+++ client/components/domains/featured-domain-suggestions/index.jsx
@@ -33,8 +33,48 @@ export class FeaturedDomainSuggestions extends Component {
 		return pick( this.props, childKeys );
 	}
 
+	getMaxTitleLength() {
+		const { primarySuggestion = {}, secondarySuggestion = {} } = this.props;
+		const { domain_name: primaryDomainName = '' } = primarySuggestion;
+		const { domain_name: secondaryDomainName = '' } = secondarySuggestion;
+		const longestDomainName =
+			primaryDomainName.length >= secondaryDomainName ? primaryDomainName : secondaryDomainName;
+		return longestDomainName.length;
+	}
+
+	getTextSizeClass() {
+		const length = this.getMaxTitleLength();
+		const classNamePrefix = 'featured-domain-suggestions--title-in';
+		if ( length <= 18 ) {
+			return `${ classNamePrefix }-20em`;
+		}
+		if ( length <= 19 ) {
+			return `${ classNamePrefix }-18em`;
+		}
+		if ( length <= 22 ) {
+			return `${ classNamePrefix }-16em`;
+		}
+		if ( length <= 25 ) {
+			return `${ classNamePrefix }-14em`;
+		}
+		if ( length <= 27 ) {
+			return `${ classNamePrefix }-12em`;
+		}
+		if ( length <= 33 ) {
+			return `${ classNamePrefix }-10em`;
+		}
+
+		return 'featured-domain-suggestions--title-cases-overflow';
+	}
+
+	getClassNames() {
+		return classNames( 'featured-domain-suggestions', this.getTextSizeClass(), {
+			'featured-domain-suggestions--has-match-reasons': this.hasMatchReasons(),
+		} );
+	}
+
 	hasMatchReasons() {
-		const { primarySuggestion, secondarySuggestion } = this.props;
+		const { primarySuggestion = {}, secondarySuggestion = {} } = this.props;
 		return (
 			Array.isArray( primarySuggestion.matchReasons ) ||
 			Array.isArray( secondarySuggestion.matchReasons )
@@ -44,12 +84,9 @@ export class FeaturedDomainSuggestions extends Component {
 	render() {
 		const { primarySuggestion, secondarySuggestion } = this.props;
 		const childProps = this.getChildProps();
-		const className = classNames( 'featured-domain-suggestions', {
-			'featured-domain-suggestions--has-match-reasons': this.hasMatchReasons(),
-		} );
 
 		return (
-			<div className={ className }>
+			<div className={ this.getClassNames() }>
 				{ primarySuggestion && (
 					<DomainRegistrationSuggestion
 						suggestion={ primarySuggestion }
