883db818f9ea57a600e734424ad78244ddb0952f
Matt Wiebe <wiebe@automattic.com>
2018-04-13 11:49:25 -0500

    style banner with pseudo-elements
    better semantically, and likely better perf, too, since the svg isn't creating extra dom/react elements

--- client/blocks/plan-footer/index.jsx
+++ client/blocks/plan-footer/index.jsx
@@ -20,19 +20,6 @@ class PlanFooter extends Component {
 		};
 	}
 
-	banner( side ) {
-		return (
-			<svg className={ 'plan-footer__banner is-' + side } width="50" height="35" xmlns="http://www.w3.org/2000/svg">
-				<g fill="none" fill-rule="evenodd">
-					<g stroke-width="2">
-						<path stroke="#86A6BC" fill="#86A6BC" d="M41.522 2.488L4.093 12.518l15.47 6.207-8.631 12.665 36.975-9.907z" />
-						<path stroke="#151E26" fill="#151E26" d="M27.882 10.606l19.199 9.832-3.883-14.49z" />
-					</g>
-				</g>
-			</svg>
-		);
-	}
-
 	render() {
 		if ( ! this.props.isInSignup || ! this.state.showMoneyBackGuarantee ) {
 			return (
@@ -42,9 +29,7 @@ class PlanFooter extends Component {
 		return (
 			<div className="plan-footer">
 				<div className="plan-footer__guarantee">
-					{ this.banner( 'left' ) }
 					<div className="plan-footer__text">30 day money back guarantee</div>
-					{ this.banner( 'right' ) }
 				</div>
 				<div className="plan-footer__fineprint">
 					* Domains purchased with a plan are only refundable for 48 hours.
