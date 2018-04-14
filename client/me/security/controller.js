b988c693ab6a751a4945a3bfffba36dfbfea5f92
Marin Atanasov <tyxla@abv.bg>
2018-04-14 03:29:43 +0300

    Me: Remove ConnectedApplications usage from controller

--- client/me/security/controller.js
+++ client/me/security/controller.js
@@ -17,7 +17,6 @@ import PasswordComponent from 'me/security/main';
 import accountPasswordData from 'lib/account-password-data';
 import SocialLoginComponent from 'me/social-login';
 import ConnectedAppsComponent from 'me/connected-applications';
-import connectedAppsData from 'lib/connected-applications-data';
 import AccountRecoveryComponent from 'me/security-account-recovery';
 
 export default {
@@ -52,7 +51,6 @@ export default {
 		context.primary = React.createElement( ConnectedAppsComponent, {
 			userSettings: userSettings,
 			path: context.path,
-			connectedAppsData: connectedAppsData,
 		} );
 		next();
 	},
