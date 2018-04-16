30149b70f1836a4563d5d32e19e3129d53225f4d
Marin Atanasov <tyxla@abv.bg>
2018-04-16 19:49:03 +0300

    Jetpack Connect: Less scary illustration for NoDirectAccessError

--- client/jetpack-connect/no-direct-access-error.js
+++ client/jetpack-connect/no-direct-access-error.js
@@ -31,7 +31,7 @@ class NoDirectAccessError extends PureComponent {
 		return (
 			<Main className="jetpack-connect__main-error">
 				<EmptyContent
-					illustration="/calypso/images/illustrations/whoops.svg"
+					illustration="/calypso/images/illustrations/illustration-500.svg"
 					title={ translate( 'Oops, this URL should not be accessed directly' ) }
 					action={ translate( 'Get back to Jetpack Connect screen' ) }
 					actionURL="/jetpack/connect"
