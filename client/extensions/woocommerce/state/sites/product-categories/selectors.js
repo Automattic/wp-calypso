4e625a00d3ea3dc323eef23e2f98af12ec43ddc0
Kelly Dwan <ryelle@users.noreply.github.com>
2018-04-16 17:42:24 -0400

    Clean up product category selectors

--- client/extensions/woocommerce/state/sites/product-categories/selectors.js
+++ client/extensions/woocommerce/state/sites/product-categories/selectors.js
@@ -1,9 +1,7 @@
 /** @format */
-
 /**
  * External dependencies
  */
-
 import { get, isArray, omit, range } from 'lodash';
 
 /**
@@ -14,31 +12,30 @@ import { getSerializedProductCategoriesQuery } from './utils';
 
 /**
  * @param {Object} state Whole Redux state tree
+ * @param {Number} [siteId] Site ID to check. If not provided, the Site ID selected in the UI will be used
+ * @return {Object} State tree local to product categories reducer
+ */
+function getRawCategoryState( state, siteId = getSelectedSiteId( state ) ) {
+	return get( state, [ 'extensions', 'woocommerce', 'sites', siteId, 'productCategories' ], {} );
+}
+
+/**
+ * @param {Object} state Whole Redux state tree
  * @param {Object} [query] Query used to fetch product categories. If not provided, API defaults are used.
  * @param {Number} [siteId] Site ID to check. If not provided, the Site ID selected in the UI will be used
  * @return {boolean} Whether product categories have been successfully loaded from the server
  */
-export const areProductCategoriesLoaded = (
+export function areProductCategoriesLoaded(
 	state,
 	query = {},
 	siteId = getSelectedSiteId( state )
-) => {
+) {
 	const serializedQuery = getSerializedProductCategoriesQuery( query );
-	const cats = get(
-		state,
-		[
-			'extensions',
-			'woocommerce',
-			'sites',
-			siteId,
-			'productCategories',
-			'queries',
-			serializedQuery,
-		],
-		false
-	);
+	const categoryState = getRawCategoryState( state, siteId );
+	const cats = categoryState.queries && categoryState.queries[ serializedQuery ];
+
 	return isArray( cats );
-};
+}
 
 /**
  * @param {Object} state Whole Redux state tree
@@ -46,66 +43,41 @@ export const areProductCategoriesLoaded = (
  * @param {Number} [siteId] Site ID to check. If not provided, the Site ID selected in the UI will be used
  * @return {boolean} Whether product categories are currently being retrieved from the server
  */
-export const areProductCategoriesLoading = (
+export function areProductCategoriesLoading(
 	state,
 	query = {},
 	siteId = getSelectedSiteId( state )
-) => {
+) {
 	const serializedQuery = getSerializedProductCategoriesQuery( query );
-	const isLoading = get( state, [
-		'extensions',
-		'woocommerce',
-		'sites',
-		siteId,
-		'productCategories',
-		'isQueryLoading',
-		serializedQuery,
-	] );
+	const categoryState = getRawCategoryState( state, siteId );
+	const isLoading = categoryState.isQueryLoading && categoryState.isQueryLoading[ serializedQuery ];
 	// Strict check because it could also be undefined.
 	return true === isLoading;
-};
+}
 
 /**
  * Gets product categories from API data.
  *
- * @param {Object} rootState Global state tree
+ * @param {Object} state Global state tree
  * @param {Object} [query] Query used to fetch product categories. If not provided, API defaults are used.
  * @param {Number} [siteId] wpcom site id, if not provided, uses the selected site id.
  * @return {Array} List of product categories
  */
-export const getProductCategories = (
-	rootState,
-	query = {},
-	siteId = getSelectedSiteId( rootState )
-) => {
-	if ( ! areProductCategoriesLoaded( rootState, query, siteId ) ) {
+export function getProductCategories( state, query = {}, siteId = getSelectedSiteId( state ) ) {
+	if ( ! areProductCategoriesLoaded( state, query, siteId ) ) {
 		return [];
 	}
 	const serializedQuery = getSerializedProductCategoriesQuery( query );
-	const cats = get(
-		rootState,
-		[ 'extensions', 'woocommerce', 'sites', siteId, 'productCategories', 'items' ],
-		{}
-	);
-	const idsForQuery = get(
-		rootState,
-		[
-			'extensions',
-			'woocommerce',
-			'sites',
-			siteId,
-			'productCategories',
-			'queries',
-			serializedQuery,
-		],
-		[]
-	);
+	const categoryState = getRawCategoryState( state, siteId );
+	const allCategories = categoryState.items || {};
+	const idsForQuery = categoryState.queries && categoryState.queries[ serializedQuery ];
 
 	if ( idsForQuery.length ) {
-		return idsForQuery.map( id => cats[ id ] );
+		return idsForQuery.map( id => allCategories[ id ] );
 	}
+
 	return [];
-};
+}
 
 /**
  * @param {Object} state Whole Redux state tree
@@ -113,37 +85,27 @@ export const getProductCategories = (
  * @param {Number} [siteId] Site ID to check. If not provided, the Site ID selected in the UI will be used
  * @return {Number} Total number of product categories available for a query, or 0 if not loaded yet.
  */
-export const getTotalProductCategories = (
+export function getTotalProductCategories(
 	state,
 	query = {},
 	siteId = getSelectedSiteId( state )
-) => {
+) {
 	const serializedQuery = getSerializedProductCategoriesQuery( omit( query, 'page' ) );
-	return get(
-		state,
-		[ 'extensions', 'woocommerce', 'sites', siteId, 'productCategories', 'total', serializedQuery ],
-		0
-	);
-};
+	const categoryState = getRawCategoryState( state, siteId );
+	return ( categoryState.total && categoryState.total[ serializedQuery ] ) || 0;
+}
 
 /**
  * Gets product category fetched from API data.
  *
- * @param {Object} rootState Global state tree
+ * @param {Object} state Global state tree
  * @param {Number} categoryId The id of the category sought
  * @param {Number} siteId wpcom site id
  * @return {Object|null} Product category if found, otherwise null.
  */
-export function getProductCategory(
-	rootState,
-	categoryId,
-	siteId = getSelectedSiteId( rootState )
-) {
-	return get(
-		rootState,
-		[ 'extensions', 'woocommerce', 'sites', siteId, 'productCategories', 'items', categoryId ],
-		null
-	);
+export function getProductCategory( state, categoryId, siteId = getSelectedSiteId( state ) ) {
+	const categoryState = getRawCategoryState( state, siteId );
+	return ( categoryState.items && categoryState.items[ categoryId ] ) || null;
 }
 
 /**
@@ -152,26 +114,15 @@ export function getProductCategory(
  * @param {Number} [siteId] Site ID to check. If not provided, the Site ID selected in the UI will be used
  * @return {Number|Null} Total number of pages available for a query, or null if not loaded yet.
  */
-export const getProductCategoriesLastPage = (
+export function getProductCategoriesLastPage(
 	state,
 	query = {},
 	siteId = getSelectedSiteId( state )
-) => {
+) {
 	const serializedQuery = getSerializedProductCategoriesQuery( omit( query, 'page' ) );
-	return get(
-		state,
-		[
-			'extensions',
-			'woocommerce',
-			'sites',
-			siteId,
-			'productCategories',
-			'totalPages',
-			serializedQuery,
-		],
-		null
-	);
-};
+	const categoryState = getRawCategoryState( state, siteId );
+	return ( categoryState.totalPages && categoryState.totalPages[ serializedQuery ] ) || null;
+}
 
 /**
  * Returns true if currently requesting product categories for a query, excluding all known
@@ -201,51 +152,34 @@ export function areProductCategoriesLoadingIgnoringPage(
 /**
  * Gets all product categories from API data for a query, ignoring pages.
  *
- * @param {Object} rootState Global state tree
+ * @param {Object} state Global state tree
  * @param {Object} [query] Query used to fetch product categories. If not provided, API defaults are used.
  * @param {Number} [siteId] wpcom site id, if not provided, uses the selected site id.
  * @return {Array} List of product categories
  */
-export const getProductCategoriesIgnoringPage = (
-	rootState,
+export function getProductCategoriesIgnoringPage(
+	state,
 	query = {},
-	siteId = getSelectedSiteId( rootState )
-) => {
-	const lastPage = getProductCategoriesLastPage( rootState, query, siteId );
+	siteId = getSelectedSiteId( state )
+) {
+	const lastPage = getProductCategoriesLastPage( state, query, siteId );
 	if ( null === lastPage ) {
 		return [];
 	}
 
-	const cats = get(
-		rootState,
-		[ 'extensions', 'woocommerce', 'sites', siteId, 'productCategories', 'items' ],
-		{}
-	);
-
+	const categoryState = getRawCategoryState( state, siteId );
+	const categories = categoryState.items || {};
 	const result = [];
 
 	range( 1, lastPage + 1 ).some( page => {
 		const catQuery = { ...query, page };
 		const serializedQuery = getSerializedProductCategoriesQuery( catQuery );
-
-		const idsForQuery = get(
-			rootState,
-			[
-				'extensions',
-				'woocommerce',
-				'sites',
-				siteId,
-				'productCategories',
-				'queries',
-				serializedQuery,
-			],
-			[]
-		);
+		const idsForQuery = ( categoryState.queries && categoryState.queries[ serializedQuery ] ) || [];
 
 		if ( idsForQuery.length ) {
-			idsForQuery.forEach( id => result.push( cats[ id ] ) );
+			idsForQuery.forEach( id => result.push( categories[ id ] ) );
 		}
 	} );
 
 	return result;
-};
+}
