aa6f35de07db08ad0041cf9dd82f89fac8ad1987
Jon Surrell <jon.surrell@automattic.com>
2018-04-16 18:22:37 +0200

    Add tests

--- server/isomorphic-routing/test/index.js
+++ server/isomorphic-routing/test/index.js
@@ -14,26 +14,45 @@ describe( 'getNormalizedPath', () => {
 	} );
 
 	test( 'should return cacheKey for a known query param', () => {
-		const pathname = '/my/path';
+		const pathname = '/';
 		const query = { cache_me: '1' };
 
-		expect( getNormalizedPath( pathname, query ) ).toBe( '/my/path?cache_me=1' );
+		expect( getNormalizedPath( pathname, query ) ).toBe( '/?cache_me=1' );
 	} );
 
 	test( 'should return cacheKey for multiple known query params', () => {
-		const pathname = '/my/path';
+		const pathname = '/';
 		const query = { cache_me: '1', and_me: '2', me_too: '3' };
 
-		expect( getNormalizedPath( pathname, query ) ).toBe( '/my/path?and_me=2&cache_me=1&me_too=3' );
+		expect( getNormalizedPath( pathname, query ) ).toBe( '/?and_me=2&cache_me=1&me_too=3' );
 	} );
 
 	test( 'should return a stable key pathname', () => {
-		const pathname = '/my/path';
+		const pathname = '/';
 		const query = { a: '1', b: '2' };
 		const querySwapped = { b: '2', a: '1' };
 
-		expect( getNormalizedPath( pathname, query ) ).toEqual(
+		expect( getNormalizedPath( pathname, query ) ).toBe(
 			getNormalizedPath( pathname, querySwapped )
+		);
+	} );
+
+	test( 'should handle "empty" query params coherently', () => {
+		expect( getNormalizedPath( '/', { empty: '' } ) ).toBe(
+			getNormalizedPath( '/', { empty: null } )
+		);
+		expect( getNormalizedPath( '/', { empty: '' } ) ).toBe(
+			getNormalizedPath( '/', { empty: undefined } )
+		);
+	} );
+
+	test( 'should produce unique results', () => {
+		expect( getNormalizedPath( '/?a=query', {} ) ).not.toBe(
+			getNormalizedPath( '/', { a: 'query' } )
+		);
+
+		expect( getNormalizedPath( '/', { key: 'val' } ) ).not.toBe(
+			getNormalizedPath( '/', { 'key=val': '' } )
 		);
 	} );
 } );
