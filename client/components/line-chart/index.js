fca0a509de6c83a7099a8f4bd931544d9fe4ab1c
Caleb Bauermeister <cbauerman@gmail.com>
2018-04-17 08:23:30 -0700

    Copy D3 Base to core and create a line chart stub

---
+++ client/components/line-chart/index.js
@@ -0,0 +1,15 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React, { Component } from 'react';
+
+class LineChart extends Component {
+	render() {
+		return <div />;
+	}
+}
+
+export default LineChart;
