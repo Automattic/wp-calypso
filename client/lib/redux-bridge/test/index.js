f6ce637f4672ba21f3d75e7835cbcc0659808cb0
Ben Lowery <ben.lowery@automattic.com>
2018-04-17 16:57:10 -0400

    rename fake to mock

--- client/lib/redux-bridge/test/index.js
+++ client/lib/redux-bridge/test/index.js
@@ -11,13 +11,13 @@ import Dispatcher from 'dispatcher';
 import { setReduxStore, registerActionForward, clearActionForwards } from '../';
 
 describe( 'redux-bridge', () => {
-	let fakeStore;
+	let mockReduxStore;
 	beforeEach( () => {
-		fakeStore = {
+		mockReduxStore = {
 			dispatch: jest.fn(),
 			getState: jest.fn(),
 		};
-		setReduxStore( fakeStore );
+		setReduxStore( mockReduxStore );
 	} );
 
 	afterEach( () => {
@@ -27,16 +27,16 @@ describe( 'redux-bridge', () => {
 	test( 'flux actions that match the dispatch map get forwarded to the redux store', () => {
 		registerActionForward( 'MY_ACTION' );
 		Dispatcher.handleServerAction( { type: 'MY_ACTION' } );
-		expect( fakeStore.dispatch ).toHaveBeenLastCalledWith( { type: 'FLUX_MY_ACTION' } );
+		expect( mockReduxStore.dispatch ).toHaveBeenLastCalledWith( { type: 'FLUX_MY_ACTION' } );
 	} );
 
 	test( 'flux actions that DO NOT match the dispatch map DO NOT get forwarded to the redux store', () => {
 		registerActionForward( 'MY_OTHER_ACTION' );
 		Dispatcher.handleServerAction( { type: 'MY_ACTION' } );
-		expect( fakeStore.dispatch ).not.toHaveBeenCalled();
+		expect( mockReduxStore.dispatch ).not.toHaveBeenCalled();
 	} );
 
 	test( 'if nothing is registered nothing breaks', () => {
-		expect( fakeStore.dispatch ).not.toHaveBeenCalled();
+		expect( mockReduxStore.dispatch ).not.toHaveBeenCalled();
 	} );
 } );
