0696495782c2b07055a8a144ca42ff9e0a425ea9
Brandon Kraft <public@brandonkraft.com>
2018-04-18 17:08:20 -0500

    Coding standards

--- client/me/help/main.jsx
+++ client/me/help/main.jsx
@@ -242,7 +242,9 @@ class Help extends React.PureComponent {
 				<MeSidebarNavigation />
 				<Notice status="is-error" showDismiss={ false }>
 					{ translate(
-						'Some WordPress.com Business Sites are having connectivity problems due to an issue with an infrastructure partner. They are actively working to resolve the problem. Please visit {{a}}automatticstatus.com{{/a}} for updates.',
+						'Some WordPress.com Business Sites are having connectivity problems due to an issue with an infrastructure '
+						+ 'partner. They are actively working to resolve the problem. Please visit {{a}}automatticstatus.com{{/a}} '
+						+ 'for updates.',
 						{
 						components: {
 							a: (
