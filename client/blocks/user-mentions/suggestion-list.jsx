db3f4d5a2a26168b23ffbd5352c27f07be4528ac
bluefuton <chris@bluefuton.com>
2018-04-18 19:02:15 +1200

    Rename HoC
    Add textarea-caret package
    
    User mentions changes
    
    Update shrinkwrap
    
    Add styles back to _components

--- client/blocks/user-mentions/suggestion-list.jsx
+++ client/blocks/user-mentions/suggestion-list.jsx
@@ -5,7 +5,7 @@
  */
 
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import { bind, noop } from 'lodash';
 
 /**
@@ -15,39 +15,46 @@ import PopoverMenu from 'components/popover/menu';
 import PopoverMenuItem from 'components/popover/menu-item';
 import UserMentionSuggestion from './suggestion';
 
-const UserMentionSuggestionList = ( {
-	onClick,
-	onClose,
-	popoverContext,
-	query,
-	selectedSuggestionId,
-	suggestions,
-} ) => (
-	<PopoverMenu
-		className="user-mentions__suggestions"
-		context={ popoverContext }
-		isVisible={ true }
-		autoPosition={ false }
-		position="bottom right"
-		onClose={ onClose }
-	>
-		{ suggestions.map( suggestion => (
-			<PopoverMenuItem
-				className="user-mentions__suggestion"
-				key={ suggestion.ID }
-				isSelected={ suggestion.ID === selectedSuggestionId }
-				onClick={ bind( onClick, null, suggestion ) }
+class UserMentionSuggestionList extends Component {
+	render() {
+		const {
+			onClick,
+			onClose,
+			popoverContext,
+			query,
+			selectedSuggestionId,
+			suggestions,
+		} = this.props;
+		return (
+			<PopoverMenu
+				className="user-mentions__suggestions"
+				context={ popoverContext }
+				isVisible={ true }
+				autoPosition={ false }
+				position="bottom right"
+				onClose={ onClose }
+				popoverPositionLeft={ this.props.popoverPositionLeft }
+				popoverPositionTop={ this.props.popoverPositionTop }
 			>
-				<UserMentionSuggestion
-					avatarUrl={ suggestion.image_URL }
-					username={ suggestion.user_login }
-					fullName={ suggestion.display_name }
-					query={ query }
-				/>
-			</PopoverMenuItem>
-		) ) }
-	</PopoverMenu>
-);
+				{ suggestions.map( suggestion => (
+					<PopoverMenuItem
+						className="user-mentions__suggestion"
+						key={ suggestion.ID }
+						isSelected={ suggestion.ID === selectedSuggestionId }
+						onClick={ bind( onClick, null, suggestion ) }
+					>
+						<UserMentionSuggestion
+							avatarUrl={ suggestion.image_URL }
+							username={ suggestion.user_login }
+							fullName={ suggestion.display_name }
+							query={ query }
+						/>
+					</PopoverMenuItem>
+				) ) }
+			</PopoverMenu>
+		);
+	}
+}
 
 UserMentionSuggestionList.propTypes = {
 	onClick: PropTypes.func,
@@ -62,7 +69,7 @@ UserMentionSuggestionList.defaultProps = {
 	onClick: noop,
 	onClose: noop,
 	query: '',
-	popoverContext: {},
+	popoverContext: null,
 	selectedSuggestionId: 0,
 	suggestions: [],
 };
--- client/blocks/user-mentions/with-user-mentions.jsx
+++ client/blocks/user-mentions/with-user-mentions.jsx
@@ -3,11 +3,15 @@
  * External dependencies
  */
 import React from 'react';
+import getCaretCoordinates from 'textarea-caret';
+import { escapeRegExp, findIndex, get, head, includes, throttle } from 'lodash';
 
 /**
  * Internal dependencies
  */
-//import UserMentionSuggestionList from './suggestion-list';
+import UserMentionSuggestionList from './suggestion-list';
+
+const keys = { enter: 13, esc: 27, spaceBar: 32, upArrow: 38, downArrow: 40 };
 
 /**
  * withUserMentionSuggestions is a higher-order component that adds user mention support to whatever input it wraps.
@@ -18,33 +22,261 @@ import React from 'react';
  * @returns {object} the enhanced component
  */
 export default EnhancedComponent =>
-	class withUserMentionSuggestions extends React.Component {
-		static displayName = `withUserMentionSuggestions( ${ EnhancedComponent.displayName ||
+	class withUserMentions extends React.Component {
+		matchingSuggestions = [];
+
+		static displayName = `withUserMentions( ${ EnhancedComponent.displayName ||
 			EnhancedComponent.name } )`;
 		static propTypes = {};
 
 		state = {
 			showPopover: false,
+			popoverContext: null,
+			query: '',
 		};
 
-		handleKeyPress = e => {
-			if ( e.target.value[ e.target.value.length - 1 ] === '@' ) {
-				console.log( 'found @something' ); // eslint-disable-line no-console
-				this.setState( { showPopover: true } );
+		constructor( props ) {
+			super( props );
+			// create a ref to store the textarea DOM element
+			this.textInput = React.createRef();
+			this.popoverStyles = {};
+		}
+
+		componentDidMount() {
+			const { left, top, height } = this.getPosition();
+
+			this.left = left;
+			this.top = top;
+			this.height = height;
+
+			if ( typeof window !== 'undefined' ) {
+				window.addEventListener( 'resize', this.throttledUpdatePosition );
 			}
+		}
+
+		componentWillUpdate( nextProps, nextState ) {
+			// Update position of popover if going from invisible to visible state.
+			if ( ! this.state.showPopover && nextState.showPopover ) {
+				this.updatePosition( nextState );
+				return;
+			}
+
+			// Update position of popover if cursor has moved to a new line.
+			if ( nextState.showPopover ) {
+				const { top, left } = this.getPosition();
+				const isLineBefore = this.top > top && this.left < left;
+				const isLineAfter = this.top < top && this.left > left;
+
+				if ( isLineBefore || isLineAfter ) {
+					this.updatePosition( nextState, { top, left } );
+				}
+			}
+		}
+
+		componentWillUnmount() {
+			if ( typeof window !== 'undefined' ) {
+				window.removeEventListener( 'resize', this.throttledUpdatePosition );
+			}
+		}
+
+		handleKeyDown = event => {
+			const selectedIndex = this.getSelectedSuggestionIndex();
+
+			if ( ! includes( [ keys.upArrow, keys.downArrow ], event.keyCode ) || -1 === selectedIndex ) {
+				return;
+			}
+
+			let nextIndex;
+
+			// Cancel the cursor move.
+			event.preventDefault();
+
+			if ( event.keyCode === keys.downArrow ) {
+				nextIndex = ( selectedIndex + 1 ) % this.matchingSuggestions.length;
+			} else {
+				nextIndex = selectedIndex - 1;
+
+				if ( nextIndex < 0 ) {
+					nextIndex = this.matchingSuggestions.length - 1;
+				}
+			}
+
+			this.setState( { selectedSuggestionId: this.matchingSuggestions[ nextIndex ].ID } );
 		};
+
+		handleKeyUp = event => {
+			if ( includes( [ keys.downArrow, keys.upArrow ], event.keyCode ) ) {
+				return;
+			}
+
+			if ( includes( [ keys.spaceBar, keys.esc ], event.keyCode ) ) {
+				return this.hidePopover();
+			}
+
+			if ( includes( [ keys.enter ], event.keyCode ) ) {
+				if ( ! this.state.showPopover || this.matchingSuggestions.length === 0 ) {
+					return;
+				}
+
+				event.preventDefault();
+
+				const suggestion = this.getSuggestion();
+
+				if ( suggestion ) {
+					this.insertSuggestion( suggestion );
+				}
+
+				return this.hidePopover();
+			}
+
+			const query = this.getQueryText();
+
+			this.setState( {
+				showPopover: query !== null,
+				selectedSuggestionId: null,
+				query,
+			} );
+		};
+
+		getQueryText() {
+			const node = this.textInput.current;
+			// (?:^|\\s) means start of input or whitespace
+			// ([A-Za-z0-9_\+\-]*) means 0 or more instances of: A-Z a-z 0-9 _ + -
+			const matcher = new RegExp( '(?:^|\\s)@([A-Za-z0-9_+-]*)$', 'gi' );
+			const textBeforeCaret = node.value.slice( 0, node.selectionEnd );
+			const match = matcher.exec( textBeforeCaret );
+
+			return match && match.length > 1 ? match[ 1 ] : null;
+		}
+
+		getPosition() {
+			const node = this.textInput.current;
+			const nodeRect = node.getBoundingClientRect();
+			const caretPosition = getCaretCoordinates( node, node.selectionEnd );
+			const position = {
+				left: nodeRect.left + caretPosition.left + 8,
+				top: nodeRect.top + caretPosition.top + 10,
+			};
+
+			console.log( position ); // eslint-disable-line no-console
+
+			return position;
+		}
+
+		getSuggestion() {
+			const index = this.getSelectedSuggestionIndex();
+
+			return index > -1 ? this.matchingSuggestions[ index ] : null;
+		}
+
+		getSelectedSuggestionIndex() {
+			if ( ! this.state.selectedSuggestionId ) {
+				return 0;
+			}
+
+			return findIndex(
+				this.matchingSuggestions,
+				( { ID: id } ) => id === this.state.selectedSuggestionId
+			);
+		}
+
+		getMatchingSuggestions( suggestions, query ) {
+			if ( query ) {
+				query = escapeRegExp( query );
+				const matcher = new RegExp( `^${ query }|\\s${ query }`, 'ig' ); // Start of string or preceded by a space.
+
+				suggestions = suggestions.filter( ( { user_login: login, display_name: name } ) =>
+					matcher.test( `${ login } ${ name }` )
+				);
+			}
+
+			return suggestions.slice( 0, 10 );
+		}
+
+		insertSuggestion = ( { user_login: userLogin } ) => {
+			if ( ! userLogin ) {
+				return;
+			}
+
+			const node = this.textInput.current;
+			const textBeforeCaret = node.value.slice( 0, node.selectionEnd );
+			const lastAtSymbolPosition = textBeforeCaret.lastIndexOf( '@' );
+			const textBeforeAtSymbol = node.value.slice( 0, lastAtSymbolPosition );
+			const textAfterSelectionEnd = node.value.slice( node.selectionEnd, node.value.length + 1 );
+
+			node.value = textBeforeAtSymbol + '@' + userLogin + textAfterSelectionEnd;
+		};
+
+		updatePosition = ( state = this.state, newPosition ) => {
+			if ( ! newPosition ) {
+				newPosition = this.getPosition( state );
+			}
+
+			//console.log( 'updatePosition' );
+			//console.log( this.state );
+
+			const { left, top, height } = newPosition;
+
+			this.left = left;
+			this.top = top;
+			this.height = height;
+
+			this.popoverPositionLeft = `${ this.left }px`;
+			// 10 is the top position of .popover__inner, which hasn't rendered yet.
+			this.popoverPositionTop = `${ this.top }px`;
+		};
+
+		throttledUpdatePosition = throttle( this.updatePosition, 100 );
+
+		hidePopover = () => this.setState( { showPopover: false } );
 
 		render() {
-			// const suggestions = [
-			// 	{
-			// 		ID: 1,
-			// 		user_login: 'testuser',
-			// 	},
-			// ];
+			//const { siteId } = this.props;
+			const { query, showPopover } = this.state;
+			const suggestions = [
+				{
+					ID: 1,
+					user_login: 'bungle',
+				},
+				{
+					ID: 2,
+					user_login: 'george',
+				},
+				{
+					ID: 3,
+					user_login: 'zippy',
+				},
+				{
+					ID: 4,
+					user_login: 'geoffrey',
+				},
+			];
+
+			this.matchingSuggestions = this.getMatchingSuggestions( suggestions, query );
+			const selectedSuggestionId =
+				this.state.selectedSuggestionId || get( head( this.matchingSuggestions ), 'ID' );
 
 			return (
 				<div>
-					<EnhancedComponent { ...this.props } onKeyPress={ this.handleKeyPress } />
+					<EnhancedComponent
+						{ ...this.props }
+						onKeyUp={ this.handleKeyUp }
+						onKeyDown={ this.handleKeyDown }
+						ref={ this.textInput }
+					/>
+
+					{ showPopover &&
+						this.matchingSuggestions.length > 0 && (
+							<UserMentionSuggestionList
+								suggestions={ this.matchingSuggestions }
+								selectedSuggestionId={ selectedSuggestionId }
+								popoverContext={ this.textInput.current }
+								popoverPositionLeft={ this.popoverPositionLeft }
+								popoverPositionTop={ this.popoverPositionTop }
+								onClick={ this.insertSuggestion }
+								onClose={ this.hidePopover }
+							/>
+						) }
 				</div>
 			);
 		}
