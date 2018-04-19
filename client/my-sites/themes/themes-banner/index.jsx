944464f8d67d046ca12d8d90c521230ff2a58d20
Ernesto Méndez <mendezcode@gmail.com>
2018-04-19 14:08:22 -0400

    Remove eslint  override from source.

--- client/my-sites/themes/themes-banner/index.jsx
+++ client/my-sites/themes/themes-banner/index.jsx
@@ -51,7 +51,6 @@ class ThemesBanner extends PureComponent {
 		recordTracksEvent( 'calypso_showcase_banner_click', tracksData );
 	};
 
-	// eslint-disable-next-line no-undef
 	handleBannerClose = e => {
 		this.props.hideThemesBanner();
 		e.preventDefault();
