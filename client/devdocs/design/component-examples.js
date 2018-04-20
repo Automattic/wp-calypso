253226eae7bf37c78ed912f6b215c99d766faa59
Ben Dwyer <ben@scruffian.com>
2018-04-20 10:32:28 +0100

    adding missing file

---
+++ client/devdocs/design/component-examples.js
@@ -0,0 +1,154 @@
+/**
+ * Internal dependencies
+ */
+import ActionCard from 'components/action-card/docs/example';
+import Accordions from 'components/accordion/docs/example';
+import BackButton from 'components/back-button/docs/example';
+import Badge from 'components/badge/docs/example';
+import Banner from 'components/banner/docs/example';
+import BulkSelect from 'components/bulk-select/docs/example';
+import ButtonGroups from 'components/button-group/docs/example';
+import Buttons from 'components/button/docs/example';
+import Cards from 'components/card/docs/example';
+import CardHeading from 'components/card-heading/docs/example';
+import Checklist from 'components/checklist/docs/example';
+import ClipboardButtonInput from 'components/clipboard-button-input/docs/example';
+import ClipboardButtons from 'components/forms/clipboard-button/docs/example';
+import Collection from 'devdocs/design/search-collection';
+import Count from 'components/count/docs/example';
+import CountedTextareas from 'components/forms/counted-textarea/docs/example';
+import DatePicker from 'components/date-picker/docs/example';
+import DropZones from 'components/drop-zone/docs/example';
+import EllipsisMenu from 'components/ellipsis-menu/docs/example';
+import EmbedDialog from 'components/tinymce/plugins/embed/docs/example';
+import EmojifyExample from 'components/emojify/docs/example';
+import EmptyContent from 'components/empty-content/docs/example';
+import ExternalLink from 'components/external-link/docs/example';
+import FAQ from 'components/faq/docs/example';
+import FeatureGate from 'components/feature-example/docs/example';
+import FilePickers from 'components/file-picker/docs/example';
+import FocusableExample from 'components/focusable/docs/example';
+import FoldableCard from 'components/foldable-card/docs/example';
+import FormattedHeader from 'components/formatted-header/docs/example';
+import FormFields from 'components/forms/docs/example';
+import Gauge from 'components/gauge/docs/example';
+import GlobalNotices from 'components/global-notices/docs/example';
+import Gravatar from 'components/gravatar/docs/example';
+import HeaderButton from 'components/header-button/docs/example';
+import Headers from 'components/header-cake/docs/example';
+import ImagePreloader from 'components/image-preloader/docs/example';
+import InfoPopover from 'components/info-popover/docs/example';
+import InputChrono from 'components/input-chrono/docs/example';
+import JetpackColophonExample from 'components/jetpack-colophon/docs/example';
+import JetpackLogoExample from 'components/jetpack-logo/docs/example';
+import LanguagePicker from 'components/language-picker/docs/example';
+import ListEnd from 'components/list-end/docs/example';
+import Notices from 'components/notice/docs/example';
+import PaginationExample from 'components/pagination/docs/example';
+import PaymentLogo from 'components/payment-logo/docs/example';
+import PieChart from 'components/pie-chart/docs/example';
+import Popovers from 'components/popover/docs/example';
+import ProgressBar from 'components/progress-bar/docs/example';
+import Ranges from 'components/forms/range/docs/example';
+import Rating from 'components/rating/docs/example';
+import Ribbon from 'components/ribbon/docs/example';
+import ScreenReaderTextExample from 'components/screen-reader-text/docs/example';
+import SearchDemo from 'components/search/docs/example';
+import SectionHeader from 'components/section-header/docs/example';
+import SectionNav from 'components/section-nav/docs/example';
+import SegmentedControl from 'components/segmented-control/docs/example';
+import SelectDropdown from 'components/select-dropdown/docs/example';
+import ShareButton from 'components/share-button/docs/example';
+import SiteTitleControl from 'components/site-title/docs/example';
+import SocialLogos from 'social-logos/example';
+import Spinner from 'components/spinner/docs/example';
+import SpinnerButton from 'components/spinner-button/docs/example';
+import SpinnerLine from 'components/spinner-line/docs/example';
+import SplitButton from 'components/split-button/docs/example';
+import Suggestions from 'components/suggestions/docs/example';
+import TextDiff from 'components/text-diff/docs/example';
+import TileGrid from 'components/tile-grid/docs/example';
+import TimeSince from 'components/time-since/docs/example';
+import Timezone from 'components/timezone/docs/example';
+import TokenFields from 'components/token-field/docs/example';
+import Tooltip from 'components/tooltip/docs/example';
+import Version from 'components/version/docs/example';
+import VerticalMenu from 'components/vertical-menu/docs/example';
+import Wizard from 'components/wizard/docs/example';
+
+export default {
+	ActionCard,
+	Accordions,
+	BackButton,
+	Badge,
+	Banner,
+	BulkSelect,
+	ButtonGroups,
+	Buttons,
+	Cards,
+	CardHeading,
+	Checklist,
+	ClipboardButtonInput,
+	ClipboardButtons,
+	Collection,
+	Count,
+	CountedTextareas,
+	DatePicker,
+	DropZones,
+	EllipsisMenu,
+	EmbedDialog,
+	EmojifyExample,
+	EmptyContent,
+	ExternalLink,
+	FAQ,
+	FeatureGate,
+	FilePickers,
+	FocusableExample,
+	FoldableCard,
+	FormattedHeader,
+	FormFields,
+	Gauge,
+	GlobalNotices,
+	Gravatar,
+	HeaderButton,
+	Headers,
+	ImagePreloader,
+	InfoPopover,
+	InputChrono,
+	JetpackColophonExample,
+	JetpackLogoExample,
+	LanguagePicker,
+	ListEnd,
+	Notices,
+	PaginationExample,
+	PaymentLogo,
+	PieChart,
+	Popovers,
+	ProgressBar,
+	Ranges,
+	Rating,
+	Ribbon,
+	ScreenReaderTextExample,
+	SearchDemo,
+	SectionHeader,
+	SectionNav,
+	SegmentedControl,
+	SelectDropdown,
+	ShareButton,
+	SiteTitleControl,
+	SocialLogos,
+	Spinner,
+	SpinnerButton,
+	SpinnerLine,
+	SplitButton,
+	Suggestions,
+	TextDiff,
+	TileGrid,
+	TimeSince,
+	Timezone,
+	TokenFields,
+	Tooltip,
+	Version,
+	VerticalMenu,
+	Wizard,
+};
