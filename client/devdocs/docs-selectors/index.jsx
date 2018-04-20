4c215dbf9fa43d0d9846ac6d0df5971979e44253
Ben Dwyer <ben@scruffian.com>
2018-04-20 15:05:13 +0100

    Add more information about State Selectors

--- client/devdocs/docs-selectors/index.jsx
+++ client/devdocs/docs-selectors/index.jsx
@@ -10,6 +10,8 @@ import React, { PureComponent } from 'react';
 /**
  * Internal dependencies
  */
+import Card from 'components/card';
+import CardHeading from 'components/card-heading';
 import Main from 'components/main';
 import DocsSelectorsSingle from './single';
 import DocsSelectorsSearch from './search';
@@ -27,6 +29,22 @@ export default class DocsSelectors extends PureComponent {
 		return (
 			<Main className="devdocs docs-selectors">
 				<DocumentHead title="State Selectors" />
+				<Card>
+					<CardHeading>State Selectors</CardHeading>
+
+					<p>
+						A selector is simply a convenience function for retrieving data out of the global state
+						tree. This page contains all available state selectors, which can be used as a helper in
+						retrieving derived data from the global state tree.
+					</p>
+
+					<p>
+						To learn more about selectors, refer to the{' '}
+						<a href="https://github.com/Automattic/wp-calypso/blob/master/docs/our-approach-to-data.md#selectors">
+							Our Approach to Data" document
+						</a>.
+					</p>
+				</Card>
 				{ selector && <DocsSelectorsSingle { ...{ selector, search } } /> }
 				{ ! selector && <DocsSelectorsSearch search={ search } /> }
 			</Main>
