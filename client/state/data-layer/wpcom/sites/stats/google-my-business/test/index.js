ad6cb2ca5a6a4b8029efa878c86a0098fe7ff3a2
Stéphane Thomas <email@stephanethomas.com>
2018-04-20 12:05:37 +0200

    Google My Business: Fix inconsistency issues in unit tests

--- client/state/data-layer/wpcom/sites/stats/google-my-business/test/index.js
+++ client/state/data-layer/wpcom/sites/stats/google-my-business/test/index.js
@@ -9,21 +9,17 @@ import sinon from 'sinon';
 /**
  * Internal dependencies
  */
-import { GOOGLE_MY_BUSINESS_STATS_REQUEST } from 'state/action-types';
 import { fetchGoogleMyBusinessStats, receiveStats } from '../';
 import { receiveGoogleMyBusinessStats } from 'state/google-my-business/actions';
 import { http } from 'state/data-layer/wpcom-http/actions';
 
 describe( '#fetchGoogleMyBusinessStatsSearch', () => {
 	test( 'should dispatch HTTP request to Google My Business stats endpoint', () => {
-		const action = {
-			type: GOOGLE_MY_BUSINESS_STATS_REQUEST,
-			interval: 'week',
-			aggregation: 'total',
-			siteId: 12345,
-			statType: 'searches',
-		};
 		const dispatch = sinon.spy();
+		const action = {
+			siteId: 12345,
+			statType: 'queries',
+		};
 
 		fetchGoogleMyBusinessStats( { dispatch }, action );
 
@@ -31,7 +27,7 @@ describe( '#fetchGoogleMyBusinessStatsSearch', () => {
 		expect( dispatch ).to.have.been.calledWith(
 			http(
 				{
-					path: '/sites/12345/stats/google-my-business/searches',
+					path: '/sites/12345/stats/google-my-business/queries',
 					method: 'GET',
 					query: {
 						interval: 'week',
@@ -48,13 +44,40 @@ describe( '#receiveStats', () => {
 	test( 'should dispatch receive stats action', () => {
 		const dispatch = sinon.spy();
 		const action = {
-			type: GOOGLE_MY_BUSINESS_STATS_REQUEST,
-			interval: 'week',
+			interval: 'month',
 			aggregation: 'total',
 			siteId: 12345,
-			statType: 'searches',
+			statType: 'views',
 		};
-		receiveStats( { dispatch }, action, { hello: 'world' } );
+
+		receiveStats( { dispatch }, action, {
+			time_zone: 'Europe/London',
+			metric_values: [ {
+				metric: 'QUERIES_DIRECT',
+				total_value: {
+					time_dimension: {
+						timeRange: {
+							endTime: '2018-04-19T23:59:59.900Z',
+							startTime: '2018-04-13T00:00:00Z',
+						},
+					},
+					metric_option: 'AGGREGATED_TOTAL',
+					value: 0,
+				},
+			}, {
+				metric: 'QUERIES_INDIRECT',
+				total_value: {
+					time_dimension: {
+						timeRange: {
+							endTime: '2018-04-19T23:59:59.900Z',
+							startTime: '2018-04-13T00:00:00Z',
+						},
+					},
+					metric_option: 'AGGREGATED_TOTAL',
+					value: 1,
+				},
+			} ],
+		} );
 
 		expect( dispatch ).to.have.been.calledOnce;
 		expect( dispatch ).to.have.been.calledWith(
@@ -64,7 +87,32 @@ describe( '#receiveStats', () => {
 				action.interval,
 				action.aggregation,
 				{
-					hello: 'world',
+					timeZone: 'Europe/London',
+					metricValues: [ {
+						metric: 'QUERIES_DIRECT',
+						total_value: {
+							time_dimension: {
+								timeRange: {
+									endTime: '2018-04-19T23:59:59.900Z',
+									startTime: '2018-04-13T00:00:00Z',
+								},
+							},
+							metric_option: 'AGGREGATED_TOTAL',
+							value: 0,
+						},
+					}, {
+						metric: 'QUERIES_INDIRECT',
+						total_value: {
+							time_dimension: {
+								timeRange: {
+									endTime: '2018-04-19T23:59:59.900Z',
+									startTime: '2018-04-13T00:00:00Z',
+								},
+							},
+							metric_option: 'AGGREGATED_TOTAL',
+							value: 1,
+						},
+					} ],
 				}
 			)
 		);
@@ -73,12 +121,12 @@ describe( '#receiveStats', () => {
 	test( 'should transform data snake_case to camelCase', () => {
 		const dispatch = sinon.spy();
 		const action = {
-			type: GOOGLE_MY_BUSINESS_STATS_REQUEST,
-			interval: 'week',
-			aggregation: 'total',
+			interval: 'quarter',
+			aggregation: 'daily',
 			siteId: 12345,
-			statType: 'searches',
+			statType: 'actions',
 		};
+
 		receiveStats( { dispatch }, action, { hello_world: 'hello' } );
 
 		expect( dispatch ).to.have.been.calledOnce;
