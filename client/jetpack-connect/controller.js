1d5c1a227b7590da14cc4f8700de076c080b786e
Marin Atanasov <tyxla@abv.bg>
2018-04-20 16:11:31 +0300

    Temporarily re-add interval handling in signupForm

--- client/jetpack-connect/controller.js
+++ client/jetpack-connect/controller.js
@@ -204,12 +204,26 @@ export function signupForm( context, next ) {
 
 	const { query } = context;
 	const transformedQuery = parseAuthorizationQuery( query );
+
 	if ( transformedQuery ) {
 		context.store.dispatch( startAuthorizeStep( transformedQuery.clientId ) );
 
-		const { locale } = context.params;
+		let interval = context.params.interval;
+		let locale = context.params.locale;
+		if ( context.params.localeOrInterval ) {
+			if ( [ 'monthly', 'yearly' ].indexOf( context.params.localeOrInterval ) >= 0 ) {
+				interval = context.params.localeOrInterval;
+			} else {
+				locale = context.params.localeOrInterval;
+			}
+		}
 		context.primary = (
-			<JetpackSignup path={ context.path } locale={ locale } authQuery={ transformedQuery } />
+			<JetpackSignup
+				path={ context.path }
+				interval={ interval }
+				locale={ locale }
+				authQuery={ transformedQuery }
+			/>
 		);
 	} else {
 		context.primary = <NoDirectAccessError />;
@@ -229,6 +243,8 @@ export function authorizeForm( context, next ) {
 
 	const { query } = context;
 	const transformedQuery = parseAuthorizationQuery( query );
+	const interval = context.params.localeOrInterval;
+
 	if ( transformedQuery ) {
 		context.store.dispatch( startAuthorizeStep( transformedQuery.clientId ) );
 		context.primary = <JetpackAuthorize authQuery={ transformedQuery } interval={ interval } />;
