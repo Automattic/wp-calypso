e7a89d6da1619263a8613a036d0672fe3371316e
Bernie Reiter <ockham@raz.or.at>
2018-04-20 14:39:59 +0200

    Use named import

--- client/components/plans/plans-skip-button/index.jsx
+++ client/components/plans/plans-skip-button/index.jsx
@@ -12,7 +12,7 @@ import { localize } from 'i18n-calypso';
  */
 import Button from 'components/button';
 import Gridicon from 'gridicons';
-import isRtlSelector from 'state/selectors/is-rtl';
+import { isRtl as isRtlSelector } from 'state/selectors';
 
 export const PlansSkipButton = ( { onClick, isRtl, translate = identity } ) => (
 	<div className="plans-skip-button">
