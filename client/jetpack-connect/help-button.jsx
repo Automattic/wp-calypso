e557dc1ce682c2807a7f458e58a9ef6eb0facf54
Jon Surrell <jon.surrell@automattic.com>
2018-04-23 16:28:06 +0200

    Record tracks prop form HelpButton

--- client/jetpack-connect/help-button.jsx
+++ client/jetpack-connect/help-button.jsx
@@ -6,19 +6,19 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Gridicon from 'gridicons';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import LoggedOutFormLinkItem from 'components/logged-out-form/link-item';
 import { localize } from 'i18n-calypso';
+import { recordTracksEvent } from 'state/analytics/actions';
 
 export class JetpackConnectHelpButton extends PureComponent {
-	static propTypes = {
-		onClick: PropTypes.func,
-		label: PropTypes.string,
-	};
+	static propTypes = { label: PropTypes.string };
 
+	recordClick = () => void this.props.recordTracksEvent( 'calypso_jpc_help_link_click' );
 
 	render() {
 		const { label, translate } = this.props;
@@ -28,7 +28,7 @@ export class JetpackConnectHelpButton extends PureComponent {
 				href="https://jetpack.com/contact-support"
 				target="_blank"
 				rel="noopener noreferrer"
-				onClick={ this.props.onClick }
+				onClick={ this.recordClick }
 			>
 				<Gridicon icon="help-outline" size={ 18 } />{' '}
 				{ label || translate( 'Get help setting up Jetpack' ) }
@@ -37,4 +37,4 @@ export class JetpackConnectHelpButton extends PureComponent {
 	}
 }
 
-export default localize( JetpackConnectHelpButton );
+export default connect( null, { recordTracksEvent } )( localize( JetpackConnectHelpButton ) );
