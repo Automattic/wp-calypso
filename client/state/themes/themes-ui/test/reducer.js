c8e3cc4a438d7bfc999e07e51902232abe01c56d
Bernie Reiter <ockham@raz.or.at>
2018-04-23 17:54:41 +0200

    Themes UI State: Add (de)serialization tests

--- client/state/themes/themes-ui/test/reducer.js
+++ client/state/themes/themes-ui/test/reducer.js
@@ -4,6 +4,7 @@
  * Internal dependencies
  */
 import reducer, { backPath, themesBannerVisible } from '../reducer';
+import { SERIALIZE, DESERIALIZE } from 'state/action-types';
 
 describe( 'reducer', () => {
 	test( 'should include expected keys in return value', () => {
@@ -22,9 +23,49 @@ describe( '#backPath', () => {
 } );
 
 describe( '#themesBannerVisible', () => {
-	const state = themesBannerVisible( undefined, {} );
-
 	test( 'should default to a themesBannerVisible of true', () => {
+		const state = themesBannerVisible( undefined, {} );
+
 		expect( state ).toBe( true );
+	} );
+
+	test( 'persists visible state', () => {
+		const state = themesBannerVisible( true, {
+			type: SERIALIZE,
+		} );
+
+		expect( state ).toBe( true );
+	} );
+
+	test( 'persists invisible state', () => {
+		const state = themesBannerVisible( false, {
+			type: SERIALIZE,
+		} );
+
+		expect( state ).toBe( false );
+	} );
+
+	test( 'loads persisted visible state', () => {
+		const state = themesBannerVisible( true, {
+			type: DESERIALIZE,
+		} );
+
+		expect( state ).toBe( true );
+	} );
+
+	test( 'loads persisted invisible state', () => {
+		const state = themesBannerVisible( false, {
+			type: DESERIALIZE,
+		} );
+
+		expect( state ).toBe( false );
+	} );
+
+	test( "doesn't load invalid persisted state", () => {
+		const state = themesBannerVisible( 'wtf', {
+			type: DESERIALIZE,
+		} );
+
+		expect( state ).toBe( true ); // Falls back to `initialState`, which is `true`.
 	} );
 } );
