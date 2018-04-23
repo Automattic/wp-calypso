39cd150caf405365887ad9186cd94003a1799564
Bernie Reiter <ockham@raz.or.at>
2018-04-23 17:42:47 +0200

    Themes UI State: Check for presence of reducer keys

--- client/state/themes/themes-ui/test/reducer.js
+++ client/state/themes/themes-ui/test/reducer.js
@@ -8,6 +8,12 @@ import reducer from '../reducer';
 describe( 'reducer', () => {
 	const state = reducer( undefined, {} );
 
+	test( 'should include expected keys in return value', () => {
+		expect( Object.keys( reducer( undefined, {} ) ) ).toEqual(
+			expect.arrayContaining( [ 'backPath', 'themesBannerVisible' ] )
+		);
+	} );
+
 	test( 'should default to a backPath of /themes', () => {
 		expect( state.backPath ).toBe( '/themes' );
 	} );
