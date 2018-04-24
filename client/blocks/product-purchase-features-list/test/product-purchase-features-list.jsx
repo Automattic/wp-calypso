73e592c2b2b3a09fef6491895ef222dd8a29bab6
AnnaMag <AnnaMag@users.noreply.github.com>
2018-04-24 15:04:00 +0100

    Component: remove redundant definitions in the tests of the ProductPurchaseFeaturesList. (#24430)
    * which are also causing linter and console warnings regarding returned object type
    and object defintion.

--- client/blocks/product-purchase-features-list/test/product-purchase-features-list.jsx
+++ client/blocks/product-purchase-features-list/test/product-purchase-features-list.jsx
@@ -1,26 +1,6 @@
 /** @format */
-
-jest.mock( 'lib/abtest', () => ( {
-	abtest: () => '',
-} ) );
-
-jest.mock( '../google-vouchers', () => ( {} ) );
-jest.mock( '../video-audio-posts', () => {
-	const React = require( 'react' );
-	return class VideoAudioPosts extends React.Component {};
-} );
-
-jest.mock( 'i18n-calypso', () => ( {
-	localize: Comp => props => (
-		<Comp
-			{ ...props }
-			translate={ function( x ) {
-				return x;
-			} }
-		/>
-	),
-	numberFormat: x => x,
-} ) );
+jest.mock( '../google-vouchers', () => 'GoogleVouchers' );
+jest.mock( '../video-audio-posts', () => 'VideoAudioPosts' );
 
 /**
  * External dependencies
