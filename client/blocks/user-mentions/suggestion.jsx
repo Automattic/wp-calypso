e61ab7b28d049476bec99aa40601a7729c434f68
Chris R <chris@bluefuton.com>
2018-04-26 10:36:12 +1200

    Rename user-mention-suggestions block to user-mentions (#24418)
    * Rename block to user-mentions
    
    * Add suggestion list and styles from TinyMCE plugin
    
    * Remove popup for now

---
+++ client/blocks/user-mentions/suggestion.jsx
@@ -0,0 +1,67 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const UserMentionSuggestion = ( { avatarUrl, fullName, query, username } ) => {
+	const highlight = ( content, type ) => {
+		const expressions = {
+			username: `(^${ query })(\\w*)\\s*`,
+			fullName: `(^.*?)(\\b${ query })(.*)`,
+		};
+
+		const matches = new RegExp( expressions[ type ], 'ig' ).exec( content );
+
+		if ( ! matches ) {
+			return [ content ];
+		}
+
+		return matches.map( ( item, index ) => {
+			if ( index === 0 ) {
+				return '';
+			}
+
+			if ( query.toLowerCase() === item.toLowerCase() ) {
+				return (
+					<mark className="user-mentions__highlight" key={ index }>
+						{ item }
+					</mark>
+				);
+			}
+
+			return item;
+		} );
+	};
+
+	const highlightedUsername = highlight( username, 'username' );
+	const highlightedFullName = highlight( fullName, 'fullName' );
+
+	highlightedUsername.unshift( '@' );
+
+	return (
+		<div>
+			<img className="user-mentions__avatar" src={ avatarUrl } alt="" />
+			<span className="user-mentions__username">{ highlightedUsername }</span>
+			<small className="user-mentions__fullname">{ highlightedFullName }</small>
+		</div>
+	);
+};
+
+UserMentionSuggestion.propTypes = {
+	avatarUrl: PropTypes.string,
+	fullName: PropTypes.string,
+	query: PropTypes.string,
+	username: PropTypes.string,
+};
+
+UserMentionSuggestion.defaultProps = {
+	avatarUrl: '',
+	fullName: '',
+	query: '',
+	username: '',
+};
+
+export default UserMentionSuggestion;
