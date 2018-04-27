a51d1fc4213072b230b184d1e4e4f800af011929
AnnaMag <anna.mag.kedzierska@gmail.com>
2018-04-27 13:05:32 +0100

    Plans: create mobile apps feature card.
    This is a new card that will be included with the My Plan feature cards
    for all .com and JP plans.

---
+++ client/blocks/product-purchase-features-list/mobile-apps.jsx
@@ -0,0 +1,30 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import PurchaseDetail from 'components/purchase-detail';
+
+export default localize( ( { translate } ) => {
+	return (
+		<div className="product-purchase-features-list__item">
+			<PurchaseDetail
+				icon={ <img alt="" src="/calypso/images/illustrations/jetpack-apps.svg" /> }
+				title={ translate( 'Download our mobile apps' ) }
+				description={ translate(
+					'Manage all your sites from a single dashboard: publish content, ' +
+						'track stats, moderate comments, and more.'
+				) }
+				buttonText={ translate( 'Get the apps' ) }
+				href={ '/themes/' }
+			/>
+		</div>
+	);
+} );
