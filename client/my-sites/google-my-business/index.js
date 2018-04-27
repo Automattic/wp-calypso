3d403846149e03d7c2be73317a77d404ddd4195e
Caleb Bauermeister <cbauerman@gmail.com>
2018-04-27 16:16:30 -0700

    add conditions to index to avoid rendering empty stats pafe on non-GMB sites.

--- client/my-sites/google-my-business/index.js
+++ client/my-sites/google-my-business/index.js
@@ -14,9 +14,22 @@ import { navigation, sites, siteSelection } from 'my-sites/controller';
 import { newAccount, selectBusinessType, selectLocation, stats } from './controller';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { getKeyringConnectionsByName } from 'state/sharing/keyring/selectors';
-import { isGoogleMyBusinessLocationConnected, getGoogleMyBusinessLocations } from 'state/selectors';
+import {
+	isGoogleMyBusinessLocationConnected,
+	getGoogleMyBusinessLocations,
+	isSiteGoogleMyBusinessEligible,
+} from 'state/selectors';
 import { requestSiteSettings } from 'state/site-settings/actions';
 import { requestKeyringConnections } from 'state/sharing/keyring/actions';
+
+const loadKeyringAndSettings = ( context, next ) => {
+	const state = context.store.getState();
+	const siteId = getSelectedSiteId( state );
+	Promise.all( [
+		context.store.dispatch( requestKeyringConnections() ),
+		context.store.dispatch( requestSiteSettings( siteId ) ),
+	] ).then( next );
+};
 
 export default function( router ) {
 	router( '/google-my-business', siteSelection, sites, navigation, makeLayout );
@@ -56,6 +69,27 @@ export default function( router ) {
 			'/google-my-business/stats/:site',
 			redirectLoggedOut,
 			siteSelection,
+			loadKeyringAndSettings,
+			( context, next ) => {
+				const state = context.store.getState();
+				const siteId = getSelectedSiteId( state );
+				const siteIsGMBEligible = isSiteGoogleMyBusinessEligible( state, siteId );
+				const hasConnectedLocation = isGoogleMyBusinessLocationConnected( state, siteId );
+				const hasLocationsAvailable = getGoogleMyBusinessLocations( state, siteId ).length > 0;
+
+				if ( ! config.isEnabled( 'google-my-business' ) ) {
+					page.redirect( `/google-my-business/select-business-type/${ context.params.site }` );
+					return;
+				}
+
+				if ( hasConnectedLocation ) {
+					next();
+				} else if ( hasLocationsAvailable && siteIsGMBEligible ) {
+					page.redirect( `/google-my-business/select-location/${ context.params.site }` );
+				} else {
+					page.redirect( `/stats/${ context.params.site }` );
+				}
+			},
 			stats,
 			navigation,
 			makeLayout
@@ -71,39 +105,26 @@ export default function( router ) {
 		makeLayout
 	);
 
-	router(
-		'/google-my-business/:site',
-		siteSelection,
-		( context, next ) => {
-			const state = context.store.getState();
-			const siteId = getSelectedSiteId( state );
-			Promise.all( [
-				context.store.dispatch( requestKeyringConnections() ),
-				context.store.dispatch( requestSiteSettings( siteId ) ),
-			] ).then( next );
-		},
-		context => {
-			const state = context.store.getState();
-			const siteId = getSelectedSiteId( state );
-			const hasConnectedLocation = isGoogleMyBusinessLocationConnected( state, siteId );
-			const hasLocationsAvailable = getGoogleMyBusinessLocations( state, siteId ).length > 0;
-			const hasAuthenticated =
-				getKeyringConnectionsByName( state, 'google-my-business' ).length > 0;
+	router( '/google-my-business/:site', siteSelection, loadKeyringAndSettings, context => {
+		const state = context.store.getState();
+		const siteId = getSelectedSiteId( state );
+		const hasConnectedLocation = isGoogleMyBusinessLocationConnected( state, siteId );
+		const hasLocationsAvailable = getGoogleMyBusinessLocations( state, siteId ).length > 0;
+		const hasAuthenticated = getKeyringConnectionsByName( state, 'google-my-business' ).length > 0;
 
-			if ( ! config.isEnabled( 'google-my-business' ) ) {
-				page.redirect( `/google-my-business/select-business-type/${ context.params.site }` );
-				return;
-			}
-
-			if ( hasConnectedLocation ) {
-				page.redirect( `/google-my-business/stats/${ context.params.site }` );
-			} else if ( hasLocationsAvailable ) {
-				page.redirect( `/google-my-business/select-location/${ context.params.site }` );
-			} else if ( hasAuthenticated ) {
-				page.redirect( `/google-my-business/new/${ context.params.site }` );
-			} else {
-				page.redirect( `/google-my-business/select-business-type/${ context.params.site }` );
-			}
+		if ( ! config.isEnabled( 'google-my-business' ) ) {
+			page.redirect( `/google-my-business/select-business-type/${ context.params.site }` );
+			return;
 		}
-	);
+
+		if ( hasConnectedLocation ) {
+			page.redirect( `/google-my-business/stats/${ context.params.site }` );
+		} else if ( hasLocationsAvailable ) {
+			page.redirect( `/google-my-business/select-location/${ context.params.site }` );
+		} else if ( hasAuthenticated ) {
+			page.redirect( `/google-my-business/new/${ context.params.site }` );
+		} else {
+			page.redirect( `/google-my-business/select-business-type/${ context.params.site }` );
+		}
+	} );
 }
