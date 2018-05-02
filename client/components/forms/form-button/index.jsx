853854f728c9df810a3a5488f3363ea489cce862
Filipe Varela <keoshi@keoshi.com>
2018-05-02 13:55:26 +0100

    FormButton: revert changes since props are already being passed

--- client/components/forms/form-button/index.jsx
+++ client/components/forms/form-button/index.jsx
@@ -18,7 +18,6 @@ class FormButton extends React.Component {
 	static defaultProps = {
 		isSubmitting: false,
 		isPrimary: true,
-		isScary: false,
 		type: 'submit',
 	};
 
@@ -29,14 +28,13 @@ class FormButton extends React.Component {
 	};
 
 	render() {
-		const { children, className, isPrimary, isScary, ...props } = this.props,
+		const { children, className, isPrimary, ...props } = this.props,
 			buttonClasses = classNames( className, 'form-button' );
 
 		return (
 			<Button
 				{ ...omit( props, [ 'isSubmitting', 'moment', 'numberFormat', 'translate' ] ) }
 				primary={ isPrimary }
-				scary={ isScary }
 				className={ buttonClasses }
 			>
 				{ Children.count( children ) ? children : this.getDefaultButtonAction() }
