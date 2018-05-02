34fee37f17e3884338040a17f6f886a3cad00b53
Ben Lowery <ben.lowery@automattic.com>
2018-05-02 15:10:39 -0400

    add comments explaining the config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,32 +7,63 @@ module.exports = {
 	env: {
 		browser: true,
 		'jest/globals': true,
+		// mocha is only still on because we have not finished porting all of our tests to jest's syntax
 		mocha: true,
 		node: true,
 	},
 	globals: {
+		// this is our custom function that's transformed by babel into either a dynamic import or a normal require
 		asyncRequire: true,
+		// this is the name of the project from the build config. Injected at boot in a script tag.
 		PROJECT_NAME: true,
+		// this is the SHA of the current commit. Injected at boot in a script tag.
 		COMMIT_SHA: true,
 	},
 	plugins: [ 'jest', 'jsx-a11y', 'import' ],
 	rules: {
-		camelcase: 0, // REST API objects include underscores
+		// REST API objects include underscores
+		camelcase: 0,
+
+		// TODO: why did we turn this off?
 		'jest/valid-expect': 0,
-		'jsx-a11y/anchor-has-content': 0, // i18n-calypso translate triggers false failures
+
+		// i18n-calypso translate triggers false failures
+		'jsx-a11y/anchor-has-content': 0,
+
+		// error if any given line is over 140 characters long.
+		// Ideally this would be even lower(120, 80?), but that's a big step.
 		'max-len': [ 2, { code: 140 } ],
+
+		// error if any module depends on the data-observe mixin, which is deprecated
 		'no-restricted-imports': [ 2, 'lib/mixins/data-observe' ],
 		'no-restricted-modules': [ 2, 'lib/mixins/data-observe' ],
-		'no-unused-expressions': 0, // Allows Chai `expect` expressions
+
+		// Allows Chai `expect` expressions. Now that we're on jest, hopefully we can remove this one.
+		'no-unused-expressions': 0,
+
+		// enforce our classname namespacing rules
 		'wpcalypso/jsx-classname-namespace': [
 			2,
 			{
 				rootFiles: [ 'index.js', 'index.jsx', 'main.js', 'main.jsx' ],
 			},
 		],
+
+		// Force folks to use our custom combineReducers function instead of the plain redux one
+		// This allows us to control serialization for every reducer.
 		'wpcalypso/import-no-redux-combine-reducers': 2,
+
+		// Disallow importing of native node modules, with some exceptions
+		// - url because we use it all over the place to parse and build urls
+		// - events because we use it for some event emitters
+		// - path because we use it quite a bit
 		'import/no-nodejs-modules': [ 'error', { allow: [ 'url', 'events', 'path' ] } ],
+
+		// Disallow importing or requiring packages that are not listed in package.json
+		// This prevents us from depending on transitive dependencies, which could break in unexpected ways.
 		'import/no-extraneous-dependencies': [ 'error', { packageDir: './' } ],
+
+		// turn off indenting rules since we let prettier handle it
 		indent: 0,
 	},
 };
