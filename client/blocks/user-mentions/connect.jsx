c6e344d5a830e89a5a148a46313f31d537cadffa
bluefuton <chris@bluefuton.com>
2018-05-03 12:01:29 +1200

    Add component docs

--- client/blocks/user-mentions/connect.jsx
+++ client/blocks/user-mentions/connect.jsx
@@ -8,6 +8,14 @@ import QueryUsersSuggestions from 'components/data/query-users-suggestions';
 import { getUserSuggestions } from 'state/users/suggestions/selectors';
 import PropTypes from 'prop-types';
 
+/**
+ * connectUserMentions is a higher-order component that connects the child component to user suggestions from the API.
+ *
+ * @example: connectUserMentions( Component )
+ *
+ * @param {object} WrappedComponent - React component to wrap
+ * @returns {object} the enhanced component
+ */
 const connectUserMentions = WrappedComponent => {
 	class connectUserMentionsFetcher extends React.PureComponent {
 		static propTypes = {
