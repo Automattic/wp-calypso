46d2e84bc73ccf26331b96e4aa3fb4f3ca6db745
Dennis Snell <dennis.snell@automattic.com>
2018-05-03 18:00:00 +0200

    Make `fromApi` lazy and skip parser creation

--- client/state/data-layer/http-data/getters.js
+++ client/state/data-layer/http-data/getters.js
@@ -2,9 +2,8 @@
 /**
  * Internal dependencies
  */
-import makeJsonSchemaParser from 'lib/make-json-schema-parser';
 import { http as rawHttp } from 'state/http/actions';
-import { requestHttpData } from 'state/data-layer/http-data/common';
+import { requestHttpData } from 'state/data-layer/http-data';
 
 export const requestGeoLocation = () =>
 	requestHttpData(
@@ -13,26 +12,5 @@ export const requestGeoLocation = () =>
 			method: 'GET',
 			url: 'https://public-api.wordpress.com/geo/',
 		} ),
-		{
-			fromApi: makeJsonSchemaParser(
-				{
-					type: 'object',
-					properties: {
-						body: {
-							type: [ 'object', 'null' ],
-							properties: {
-								latitude: { type: 'string' },
-								longitude: { type: 'string' },
-								country_short: { type: 'string' },
-								country_long: { type: 'string' },
-								region: { type: 'string' },
-								city: { type: 'string' },
-							},
-						},
-					},
-				},
-				// we only use the short code currently
-				( { body: { country_short } } ) => [ [ 'geo', country_short ] ]
-			),
-		}
+		{ fromApi: ( { body: { country_short } } ) => [ [ 'geo', parseInt( country_short, 10 ) ] ] }
 	);
