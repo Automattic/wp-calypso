8f6d2f45896e1ca1eacb68b6f97d42ca300d9123
Bernie Reiter <ockham@raz.or.at>
2018-05-03 11:05:25 +0300

    JPC: Dispatch notice is user is already connected

--- client/state/notices/middleware.js
+++ client/state/notices/middleware.js
@@ -33,6 +33,7 @@ import {
 	INVITE_RESEND_REQUEST_FAILURE,
 	INVITES_DELETE_REQUEST_SUCCESS,
 	INVITES_DELETE_REQUEST_FAILURE,
+	JETPACK_CONNECT_USER_ALREADY_CONNECTED,
 	JETPACK_MODULE_ACTIVATE_SUCCESS,
 	JETPACK_MODULE_DEACTIVATE_SUCCESS,
 	JETPACK_MODULE_ACTIVATE_FAILURE,
@@ -319,6 +320,15 @@ export const onBillingTransactionRequestFailure = ( { transactionId, error } ) =
 	} );
 };
 
+export const onJetpackConnectUserAlreadyConnected = () => {
+	return errorNotice(
+		translate(
+			'This WordPress.com account is already connected to another user on this site. ' +
+				'Please login to another WordPress.com account to complete the connection.'
+		)
+	);
+};
+
 /**
  * Handler action type mapping
  */
@@ -342,6 +352,7 @@ export const handlers = {
 	[ INVITES_DELETE_REQUEST_SUCCESS ]: onDeleteInvitesSuccess,
 	[ INVITES_DELETE_REQUEST_FAILURE ]: onDeleteInvitesFailure,
 	[ INVITE_RESEND_REQUEST_FAILURE ]: onInviteResendRequestFailure,
+	[ JETPACK_CONNECT_USER_ALREADY_CONNECTED ]: onJetpackConnectUserAlreadyConnected,
 	[ JETPACK_MODULE_ACTIVATE_SUCCESS ]: onJetpackModuleActivationActionMessage,
 	[ JETPACK_MODULE_DEACTIVATE_SUCCESS ]: onJetpackModuleActivationActionMessage,
 	[ JETPACK_MODULE_ACTIVATE_FAILURE ]: onJetpackModuleActivationActionMessage,
