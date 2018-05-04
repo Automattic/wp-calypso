Automattic/wp-calypso

commit c91ce4f4ac455b41162383e7bcaa61c143951147

Enable the /checkout/:domain/:product route for logged-out users (#22993)
Fixes #22857

Currently, logged-out users arriving at wordpress.com/checkout/:domain/:product see a blank screen. This PR will show a login-form instead.

Adds a common middleware for diverting to Calypso /log-in form, before redirecting to the original requested route, meaning customers must log in before completing the purchase.

All other checkout routes remain logged-in only for now.

NOTE: This is a temporary solution for #22857 in lieu of a general fix in #23785.

client/my-sites/checkout/index.js
@@ -7,13 +7,30 @@ import page from 'page';
 /**
  * Internal dependencies
  */
-import { noSite, siteSelection } from 'my-sites/controller';
 import checkoutController from './controller';
 import SiftScience from 'lib/siftscience';
-import { makeLayout, render as clientRender } from 'controller';
+import userFactory from 'lib/user';
+import { makeLayout, redirectLoggedOut, render as clientRender } from 'controller';
+import { noSite, siteSelection } from 'my-sites/controller';
 
 export default function() {
+	const user = userFactory();
+	const isLoggedOut = ! user.get();
+
 	SiftScience.recordUser();
+
+	// TODO (seear): Temporary logged-out handling. Remove when a general solution arrives in #23785.
+	if ( isLoggedOut ) {
+		page(
+			'/checkout/:domain/:product?',
+			redirectLoggedOut,
+			siteSelection,
+			checkoutController.checkout,
+			makeLayout,
+			clientRender
+		);
+		return;
+	}
 
 	page(
 		'/checkout/thank-you/no-site/:receiptId?',

client/controller/index.web.js
@@ -3,7 +3,6 @@
 /**
  * External dependencies
  */
-
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
@@ -12,8 +11,10 @@ import page from 'page';
 /**
  * Internal Dependencies
  */
+import config from 'config';
 import Layout from 'layout';
 import LayoutLoggedOut from 'layout/logged-out';
+import { login } from 'lib/paths';
 import { makeLayoutMiddleware } from './shared.js';
 import { getCurrentUser } from 'state/current-user/selectors';
 import userFactory from 'lib/user';
@@ -63,6 +64,20 @@ export function redirectLoggedIn( context, next ) {
 		return;
 	}
 
+	next();
+}
+
+export function redirectLoggedOut( context, next ) {
+	const currentUser = getCurrentUser( context.store.getState() );
+
+	if ( ! currentUser ) {
+		return page.redirect(
+			login( {
+				isNative: config.isEnabled( 'login/native-login-links' ),
+				redirectTo: context.path,
+			} )
+		);
+	}
 	next();
 }
 

client/wordpress-com.js
@@ -231,6 +231,7 @@ const sections = [
 		module: 'my-sites/checkout',
 		secondary: true,
 		group: 'sites',
+		enableLoggedOut: true,
 	},
 	{
 		name: 'plans',

commit 9f0b20898ac6e348c56c8d0a7fffc8014decbab1

Checkout: redirect logged-out users to login page
When logged-out, a blank screen is displayed on the all checkout pages but `/checkout/:domain/:product?`.
This trouble is caused by #22993 which fixed a bug filed in #22857 (see #23785 for the general problem).
For example, renewal email was also affected by this bug (see p9jf6J-rI-p2).

This PR fixes an empty page displayed to logged-out users when they try
to access any `/checkout` pages.

client/my-sites/checkout/index.js
@@ -9,31 +9,15 @@ import page from 'page';
  */
 import checkoutController from './controller';
 import SiftScience from 'lib/siftscience';
-import userFactory from 'lib/user';
 import { makeLayout, redirectLoggedOut, render as clientRender } from 'controller';
 import { noSite, siteSelection } from 'my-sites/controller';
 
 export default function() {
-	const user = userFactory();
-	const isLoggedOut = ! user.get();
-
 	SiftScience.recordUser();
-
-	// TODO (seear): Temporary logged-out handling. Remove when a general solution arrives in #23785.
-	if ( isLoggedOut ) {
-		page(
-			'/checkout/:domain/:product?',
-			redirectLoggedOut,
-			siteSelection,
-			checkoutController.checkout,
-			makeLayout,
-			clientRender
-		);
-		return;
-	}
 
 	page(
 		'/checkout/thank-you/no-site/pending/:orderId',
+		redirectLoggedOut,
 		siteSelection,
 		checkoutController.checkoutPending,
 		makeLayout,
@@ -42,6 +26,7 @@ export default function() {
 
 	page(
 		'/checkout/thank-you/no-site/:receiptId?',
+		redirectLoggedOut,
 		noSite,
 		checkoutController.checkoutThankYou,
 		makeLayout,
@@ -50,6 +35,7 @@ export default function() {
 
 	page(
 		'/checkout/thank-you/:site/pending/:orderId',
+		redirectLoggedOut,
 		siteSelection,
 		checkoutController.checkoutPending,
 		makeLayout,
@@ -58,6 +44,7 @@ export default function() {
 
 	page(
 		'/checkout/thank-you/:site/:receiptId?',
+		redirectLoggedOut,
 		siteSelection,
 		checkoutController.checkoutThankYou,
 		makeLayout,
@@ -66,6 +53,7 @@ export default function() {
 
 	page(
 		'/checkout/thank-you/:site/:receiptId/with-gsuite/:gsuiteReceiptId',
+		redirectLoggedOut,
 		siteSelection,
 		checkoutController.checkoutThankYou,
 		makeLayout,
@@ -74,6 +62,7 @@ export default function() {
 
 	page(
 		'/checkout/thank-you/features/:feature/:site/:receiptId?',
+		redirectLoggedOut,
 		siteSelection,
 		checkoutController.checkoutThankYou,
 		makeLayout,
@@ -82,6 +71,7 @@ export default function() {
 
 	page(
 		'/checkout/no-site',
+		redirectLoggedOut,
 		noSite,
 		checkoutController.sitelessCheckout,
 		makeLayout,
@@ -90,6 +80,7 @@ export default function() {
 
 	page(
 		'/checkout/features/:feature/:domain/:plan_name?',
+		redirectLoggedOut,
 		siteSelection,
 		checkoutController.checkout,
 		makeLayout,
@@ -98,6 +89,7 @@ export default function() {
 
 	page(
 		'/checkout/:domain/:product?',
+		redirectLoggedOut,
 		siteSelection,
 		checkoutController.checkout,
 		makeLayout,
@@ -106,6 +98,7 @@ export default function() {
 
 	page(
 		'/checkout/:product/renew/:purchaseId/:domain',
+		redirectLoggedOut,
 		siteSelection,
 		checkoutController.checkout,
 		makeLayout,
@@ -114,6 +107,7 @@ export default function() {
 
 	page(
 		'/checkout/:site/with-gsuite/:domain/:receiptId?',
+		redirectLoggedOut,
 		siteSelection,
 		checkoutController.gsuiteNudge,
 		makeLayout,