be9ef44216e89a6f7af2f48bcb15af954dcc1112
spen <spen_@hotmail.co.uk>
2018-05-04 12:22:55 +0100

    InlineHelp: Add Forum Flow

--- client/blocks/inline-help/constants.js
+++ client/blocks/inline-help/constants.js
@@ -8,3 +8,4 @@ export const RESULT_TOUR = 'tour';
 export const RESULT_VIDEO = 'video';
 export const VIEW_CONTACT = 'contact';
 export const VIEW_RICH_RESULT = 'richresult';
+export const VIEW_FORUM = 'forums';
