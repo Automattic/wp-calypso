5986d876937abe453843c8cf5124b2a68acc4b0d
Chris R <chris@bluefuton.com>
2018-05-09 11:07:13 +1200

    Editor mentions: share suggestion components and styles with user-mentions block (#24673)
    * Editor mentions: share suggestion components and styles with user-mentions block
    
    * Move back to a stateless functional component for SuggestionList
    
    * Reflect class name change in TinyMCE plugin

--- client/blocks/user-mentions/suggestion-list.jsx
+++ client/blocks/user-mentions/suggestion-list.jsx
@@ -5,72 +5,74 @@
  */
 
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { bind, noop } from 'lodash';
+import React from 'react';
+import { bind } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import PopoverMenu from 'components/popover/menu';
 import PopoverMenuItem from 'components/popover/menu-item';
-import UserMentionSuggestion from './suggestion';
+import UserMentionsSuggestion from 'blocks/user-mentions/suggestion';
 
-class UserMentionSuggestionList extends Component {
-	render() {
-		const {
-			onClick,
-			onClose,
-			popoverContext,
-			query,
-			selectedSuggestionId,
-			suggestions,
-		} = this.props;
-		return (
-			<PopoverMenu
-				className="user-mentions__suggestions"
-				context={ popoverContext }
-				isVisible={ true }
-				autoPosition={ false }
-				position="bottom right"
-				onClose={ onClose }
-				customPosition={ this.props.popoverPosition }
+const UserMentionsSuggestionList = ( {
+	onClick,
+	onClose,
+	popoverContext,
+	popoverPosition,
+	query,
+	selectedSuggestionId,
+	suggestions,
+} ) => (
+	<PopoverMenu
+		className="user-mentions__suggestions"
+		context={ popoverContext }
+		isVisible={ true }
+		autoPosition={ false }
+		position="bottom right"
+		onClose={ onClose }
+		customPosition={ popoverPosition }
+	>
+		{ suggestions.map( suggestion => (
+			<PopoverMenuItem
+				className="user-mentions__suggestion"
+				key={ suggestion.ID }
+				isSelected={ suggestion.ID === selectedSuggestionId }
+				onClick={ bind( onClick, null, suggestion ) }
 			>
-				{ suggestions.map( suggestion => (
-					<PopoverMenuItem
-						className="user-mentions__suggestion"
-						key={ suggestion.ID }
-						isSelected={ suggestion.ID === selectedSuggestionId }
-						onClick={ bind( onClick, null, suggestion ) }
-					>
-						<UserMentionSuggestion
-							avatarUrl={ suggestion.image_URL }
-							username={ suggestion.user_login }
-							fullName={ suggestion.display_name }
-							query={ query }
-						/>
-					</PopoverMenuItem>
-				) ) }
-			</PopoverMenu>
-		);
-	}
-}
+				<UserMentionsSuggestion
+					avatarUrl={ suggestion.image_URL }
+					username={ suggestion.user_login }
+					fullName={ suggestion.display_name }
+					query={ query }
+				/>
+			</PopoverMenuItem>
+		) ) }
+	</PopoverMenu>
+);
 
-UserMentionSuggestionList.propTypes = {
+UserMentionsSuggestionList.propTypes = {
 	onClick: PropTypes.func,
 	onClose: PropTypes.func,
 	query: PropTypes.string,
 	popoverContext: PropTypes.object,
+	popoverPosition: PropTypes.shape( {
+		top: PropTypes.number,
+		left: PropTypes.number,
+		positionClass: PropTypes.oneOf( [ 'top', 'right', 'bottom', 'left' ] ),
+	} ),
 	selectedSuggestionId: PropTypes.number,
 	suggestions: PropTypes.array,
 };
 
-UserMentionSuggestionList.defaultProps = {
-	onClick: noop,
-	onClose: noop,
+UserMentionsSuggestionList.defaultProps = {
+	onClick: () => {},
+	onClose: () => {},
 	query: '',
-	popoverContext: null,
+	popoverContext: {},
+	popoverPosition: null,
 	selectedSuggestionId: 0,
 	suggestions: [],
 };
 
-export default UserMentionSuggestionList;
+export default UserMentionsSuggestionList;
