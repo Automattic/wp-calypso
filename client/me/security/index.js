36016a243ddec6ac910a1c2ffb490a446607b487
Bernie Reiter <ockham@raz.or.at>
2018-05-08 17:11:17 +0200

    Me: Import me/security controllers individually

--- client/me/security/index.js
+++ client/me/security/index.js
@@ -8,33 +8,33 @@ import page from 'page';
 /**
  * Internal dependencies
  */
-import * as controller from './controller';
+import {
+	accountRecovery,
+	connectedApplications,
+	password,
+	socialLogin,
+	twoStep,
+} from './controller';
 import config from 'config';
 import { makeLayout, render as clientRender } from 'controller';
 import { sidebar } from 'me/controller';
 
 export default function() {
-	page( '/me/security', sidebar, controller.password, makeLayout, clientRender );
+	page( '/me/security', sidebar, password, makeLayout, clientRender );
 
 	if ( config.isEnabled( 'signup/social-management' ) ) {
-		page( '/me/security/social-login', sidebar, controller.socialLogin, makeLayout, clientRender );
+		page( '/me/security/social-login', sidebar, socialLogin, makeLayout, clientRender );
 	}
 
-	page( '/me/security/two-step', sidebar, controller.twoStep, makeLayout, clientRender );
+	page( '/me/security/two-step', sidebar, twoStep, makeLayout, clientRender );
 
 	page(
 		'/me/security/connected-applications',
 		sidebar,
-		controller.connectedApplications,
+		connectedApplications,
 		makeLayout,
 		clientRender
 	);
 
-	page(
-		'/me/security/account-recovery',
-		sidebar,
-		controller.accountRecovery,
-		makeLayout,
-		clientRender
-	);
+	page( '/me/security/account-recovery', sidebar, accountRecovery, makeLayout, clientRender );
 }
