2c13cf65f4d695ee178d25f30b78179bca1eb43b
James Nylen <jnylen@gmail.com>
2018-05-08 12:36:08 -0500

    Reader: Show `PostLikesPopover` when hovering over post likes (#24612)

--- client/reader/like-button/index.jsx
+++ client/reader/like-button/index.jsx
@@ -2,7 +2,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 
 /**
@@ -10,11 +10,27 @@ import { connect } from 'react-redux';
  */
 import { reduxGetState } from 'lib/redux-bridge';
 import LikeButtonContainer from 'blocks/like-button';
+import PostLikesPopover from 'blocks/post-likes/popover';
 import { markPostSeen } from 'state/reader/posts/actions';
 import { recordAction, recordGaEvent, recordTrackForPost } from 'reader/stats';
 import { getPostByKey } from 'state/reader/posts/selectors';
+import { isEnabled } from 'config';
 
 class ReaderLikeButton extends React.Component {
+	constructor( props ) {
+		super( props );
+
+		this.hidePopoverTimeout = null;
+		this.state = {
+			showLikesPopover: false,
+			likesPopoverContext: null,
+		};
+	}
+
+	componentWillUnmount() {
+		clearTimeout( this.hidePopoverTimeout );
+	}
+
 	recordLikeToggle = liked => {
 		const post =
 			this.props.post ||
@@ -35,13 +51,55 @@ class ReaderLikeButton extends React.Component {
 		}
 	};
 
+	setLikesPopoverContext = element => {
+		this.setState( { likesPopoverContext: element } );
+	};
+
+	maybeShowLikesPopover = () => {
+		if ( ! isEnabled( 'reader/likes-hover' ) ) {
+			return;
+		}
+		clearTimeout( this.hidePopoverTimeout );
+		this.setState( { showLikesPopover: true } );
+	};
+
+	maybeHideLikesPopover = () => {
+		if ( ! isEnabled( 'reader/likes-hover' ) ) {
+			return;
+		}
+		this.hidePopoverTimeout = setTimeout( () => {
+			this.setState( { showLikesPopover: false } );
+		}, 500 );
+	};
+
 	render() {
+		const { siteId, postId } = this.props;
+		const { showLikesPopover, likesPopoverContext } = this.state;
+
 		return (
-			<LikeButtonContainer
-				{ ...this.props }
-				onLikeToggle={ this.recordLikeToggle }
-				likeSource="reader"
-			/>
+			<Fragment>
+				<LikeButtonContainer
+					{ ...this.props }
+					ref={ this.setLikesPopoverContext }
+					onMouseEnter={ this.maybeShowLikesPopover }
+					onMouseLeave={ this.maybeHideLikesPopover }
+					onLikeToggle={ this.recordLikeToggle }
+					likeSource="reader"
+				/>
+				{ showLikesPopover &&
+					siteId &&
+					postId && (
+						<PostLikesPopover
+							className="reader-likes-popover" // eslint-disable-line
+							onMouseEnter={ this.maybeShowLikesPopover }
+							onMouseLeave={ this.maybeHideLikesPopover }
+							siteId={ siteId }
+							postId={ postId }
+							showDisplayNames={ true }
+							context={ likesPopoverContext }
+						/>
+					) }
+			</Fragment>
 		);
 	}
 }
