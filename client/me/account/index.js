7ffa5278591e32b28c0e8a8c1f72ed10fa26c521
Jon Surrell <jon.surrell@automattic.com>
2018-05-08 12:38:49 +0200

    Use named {im,ex}port from me account controller

--- client/me/account/index.js
+++ client/me/account/index.js
@@ -10,9 +10,9 @@ import page from 'page';
  * Internal dependencies
  */
 import meController from 'me/controller';
-import controller from './controller';
+import { account } from './controller';
 import { makeLayout, render as clientRender } from 'controller';
 
 export default function() {
-	page( '/me/account', meController.sidebar, controller.account, makeLayout, clientRender );
+	page( '/me/account', meController.sidebar, account, makeLayout, clientRender );
 }
