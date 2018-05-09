0e124e9084ef083e2276ec725ff446828bb22f4c
Jui-Shan Liang <jsliang.tw@gmail.com>
2018-05-10 00:46:48 +0800

    PaymentLogo: do not require `altText` anymore, and fall back to a default `altText` if the prop is not passed from parent

--- client/components/payment-logo/index.jsx
+++ client/components/payment-logo/index.jsx
@@ -7,20 +7,33 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { keys } from 'lodash';
+
+const ALT_TEXT = {
+	amex: 'American Express',
+	discover: 'Discover',
+	mastercard: 'MasterCard',
+	visa: 'VISA',
+	paypal: 'PayPal',
+};
+
+const POSSIBLE_TYPES = keys( ALT_TEXT );
 
 class PaymentLogo extends React.Component {
 	static propTypes = {
-		type: PropTypes.oneOf( [ 'amex', 'discover', 'mastercard', 'visa', 'paypal' ] ),
-		altText: PropTypes.string.isRequired,
+		type: PropTypes.oneOf( POSSIBLE_TYPES ),
+		altText: PropTypes.string,
 		isCompact: PropTypes.bool,
 	};
 
 	render() {
-		const classes = classNames( 'payment-logo', `is-${ this.props.type }`, {
-			'is-compact': this.props.isCompact,
+		const { altText, isCompact, type } = this.props;
+
+		const classes = classNames( 'payment-logo', `is-${ type }`, {
+			'is-compact': isCompact,
 		} );
 
-		return <div className={ classes } aria-label={ this.props.altText } />;
+		return <div className={ classes } aria-label={ altText || ALT_TEXT[ type ] || '' } />;
 	}
 }
 
