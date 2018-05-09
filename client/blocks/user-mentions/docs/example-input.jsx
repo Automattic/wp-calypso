27b43b276e27079e0f0a01fc6370d7846c9b8677
bluefuton <chris@bluefuton.com>
2018-05-09 13:49:08 +1200

    User mentions: add README

--- client/blocks/user-mentions/docs/example-input.jsx
+++ client/blocks/user-mentions/docs/example-input.jsx
@@ -11,7 +11,6 @@ import React, { Component } from 'react';
 import ExampleInput from './example-input';
 import addUserMentions from '../add';
 
-// @todo Move ref forwarding to the HOC
 const UserMentionsExampleInput = React.forwardRef( ( props, ref ) => (
 	<textarea ref={ ref } onKeyUp={ props.onKeyUp } onKeyDown={ props.onKeyDown } />
 ) );
