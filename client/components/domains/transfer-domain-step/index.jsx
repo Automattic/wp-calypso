ab828128c92733acee1c6494a87648ac4b06edb6
Andrija Vucinic <andrija.vucinic@gmail.com>
2018-05-10 00:32:25 +0100

    Domains: Show when a transfer is free / included in plan
    It's confusing when we only display the transfer price
    but then end up adding a plan to the cart. Show "Included in Plan"
    in this case, and showw "free with your plan" when bundle credit
    still not used.

--- client/components/domains/transfer-domain-step/index.jsx
+++ client/components/domains/transfer-domain-step/index.jsx
@@ -43,6 +43,8 @@ import { fetchDomains } from 'lib/upgrades/actions';
 import { domainManagementTransferIn } from 'my-sites/domains/paths';
 import { errorNotice } from 'state/notices/actions';
 import QueryProducts from 'components/data/query-products-list';
+import { isPlan } from 'lib/products-values';
+import { isNextDomainFree } from 'lib/cart-values/cart-items';
 
 class TransferDomainStep extends React.Component {
 	static propTypes = {
@@ -132,15 +134,22 @@ class TransferDomainStep extends React.Component {
 	};
 
 	addTransfer() {
-		const { translate } = this.props;
+		const { cart, domainsWithPlansOnly, selectedSite, translate } = this.props;
 		const { searchQuery, submittingAvailability, submittingWhois } = this.state;
 		const submitting = submittingAvailability || submittingWhois;
 		const productSlug = getDomainProductSlug( searchQuery );
-		const domainProductPrice = getDomainPrice(
+
+		let domainProductPrice = getDomainPrice(
 			productSlug,
 			this.props.productsList,
 			this.props.currencyCode
 		);
+
+		if ( isNextDomainFree( cart ) ) {
+			domainProductPrice = translate( 'Free with your plan' );
+		} else if ( domainsWithPlansOnly && ! isPlan( selectedSite.plan ) ) {
+			domainProductPrice = translate( 'Included in paid plans' );
+		}
 
 		return (
 			<div>
