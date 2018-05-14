8d05486c2446a8806939a8b914c323f28682f41e
Dennis Snell <dennis.snell@automattic.com>
2018-05-14 21:23:18 +0200

    Activity Log: Fetch site alert data
    In #24729 we have largely built out the UI for showing
    malicious threats on a site but up until now we have
    had no way of loading this data into Calypso.
    
    In this patch we're using the `getHttpData()` system to
    fetch that data from the new and developing API on
    WordPress.com and hooking it into the React component.

--- client/state/data-getters/index.js
+++ client/state/data-getters/index.js
@@ -3,6 +3,7 @@
  * Internal dependencies
  */
 import { http as rawHttp } from 'state/http/actions';
+import { http } from 'state/data-layer/wpcom-http/actions';
 import { requestHttpData } from 'state/data-layer/http-data';
 
 export const requestGeoLocation = () =>
@@ -14,3 +15,49 @@ export const requestGeoLocation = () =>
 		} ),
 		{ fromApi: () => ( { body: { country_short } } ) => [ [ 'geo', country_short ] ] }
 	);
+
+export const requestSiteAlerts = siteId => {
+	const id = `site-alerts-${ siteId }`;
+
+	return requestHttpData(
+		id,
+		http(
+			{
+				method: 'GET',
+				path: `/sites/${ siteId }/alerts`,
+				apiNamespace: 'wpcom/v2',
+			},
+			{}
+		),
+		{
+			freshness: 5 * 60 * 1000,
+			fromApi: () => ( { suggestions, threats, warnings } ) => [
+				[
+					id,
+					{
+						suggestions,
+						threats: threats.map( threat => ( {
+							id: threat.id,
+							signature: threat.signature,
+							description: threat.description,
+							firstDetected: Date.parse( threat.first_detected ),
+							...( threat.context ? { filename: threat.filename, context: threat.context } : {} ),
+							...( threat.diff ? { filename: threat.filename, diff: threat.diff } : {} ),
+							...( threat.extension
+								? {
+										extension: {
+											type: threat.extension.type,
+											name: threat.extension.name,
+											slug: threat.extension.slug,
+											version: threat.extension.version,
+										},
+									}
+								: {} ),
+						} ) ),
+						warnings,
+					},
+				],
+			],
+		}
+	);
+};
