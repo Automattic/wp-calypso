39e6c446223501cae4438093d4a65641727656cc
Jon Surrell <jon.surrell@automattic.com>
2018-05-15 15:22:55 +0200

    Use identity for test translate

--- client/components/jetpack-header/test/index.js
+++ client/components/jetpack-header/test/index.js
@@ -6,9 +6,9 @@
 /**
  * External dependencies
  */
-import { shallow } from 'enzyme';
-import { noop } from 'lodash';
 import React from 'react';
+import { identity } from 'lodash';
+import { shallow } from 'enzyme';
 
 /**
  * Internal dependencies
@@ -18,22 +18,22 @@ import JetpackLogo from 'components/jetpack-logo';
 
 describe( 'JetpackHeader', () => {
 	test( 'renders Jetpack logo when no partnerSlug prop', () => {
-		const wrapper = shallow( <JetpackHeader /> );
+		const wrapper = shallow( <JetpackHeader translate={ identity } /> );
 		expect( wrapper.find( JetpackLogo ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should render Jetpack logo when passed empty string for partnerSlug prop', () => {
-		const wrapper = shallow( <JetpackHeader partnerSlug="" /> );
+		const wrapper = shallow( <JetpackHeader partnerSlug="" translate={ identity } /> );
 		expect( wrapper.find( JetpackLogo ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should render JetpackLogo when passed an unknown partner slug', () => {
-		const wrapper = shallow( <JetpackHeader partnerSlug="nonexistent" /> );
+		const wrapper = shallow( <JetpackHeader partnerSlug="nonexistent" translate={ identity } /> );
 		expect( wrapper.find( JetpackLogo ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should render a co-branded logo when passed a known partner slug', () => {
-		const wrapper = shallow( <JetpackHeader partnerSlug="dreamhost" translate={ noop } /> );
+		const wrapper = shallow( <JetpackHeader partnerSlug="dreamhost" translate={ identity } /> );
 		expect( wrapper.find( '.jetpack-connect-header-logo__cobranded-logo' ) ).toHaveLength( 1 );
 	} );
 } );
