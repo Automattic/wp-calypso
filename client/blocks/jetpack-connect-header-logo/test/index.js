70fd50121f5cb3ae4477824fa6c6b49c7f2048f1
Jon Surrell <jon.surrell@automattic.com>
2018-05-15 13:04:25 +0200

    Use jest expect

--- client/blocks/jetpack-connect-header-logo/test/index.js
+++ client/blocks/jetpack-connect-header-logo/test/index.js
@@ -6,7 +6,6 @@
 /**
  * External dependencies
  */
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import { noop } from 'lodash';
 import React from 'react';
@@ -19,31 +18,31 @@ import { JetpackConnectHeaderLogo } from '..';
 describe( 'JetpackConnectHeaderLogo', () => {
 	test( 'renders Jetpack logo when no partnerSlug prop', () => {
 		const wrapper = shallow( <JetpackConnectHeaderLogo /> );
-		expect( wrapper.find( 'JetpackLogo' ) ).to.have.length( 1 );
+		expect( wrapper.find( 'JetpackLogo' ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should render Jetpack logo when passed empty string for partnerSlug prop', () => {
 		const wrapper = shallow( <JetpackConnectHeaderLogo partnerSlug="" /> );
-		expect( wrapper.find( 'JetpackLogo' ) ).to.have.length( 1 );
+		expect( wrapper.find( 'JetpackLogo' ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should render JetpackLogo when passed an unknown partner slug', () => {
 		const wrapper = shallow( <JetpackConnectHeaderLogo partnerSlug="nonexistent" /> );
-		expect( wrapper.find( 'JetpackLogo' ) ).to.have.length( 1 );
+		expect( wrapper.find( 'JetpackLogo' ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should render a co-branded logo when passed a known partner slug', () => {
 		const wrapper = shallow(
 			<JetpackConnectHeaderLogo partnerSlug="dreamhost" translate={ noop } />
 		);
-		expect( wrapper.find( '.jetpack-connect-header-logo__cobranded-logo' ) ).to.have.length( 1 );
+		expect( wrapper.find( '.jetpack-connect-header-logo__cobranded-logo' ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should fall back to using partnerSlugFromQuery prop when partnerSlug prop is empty', () => {
 		const wrapper = shallow(
 			<JetpackConnectHeaderLogo partnerSlugFromQuery="dreamhost" translate={ noop } />
 		);
-		expect( wrapper.find( '.jetpack-connect-header-logo__cobranded-logo' ) ).to.have.length( 1 );
+		expect( wrapper.find( '.jetpack-connect-header-logo__cobranded-logo' ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should prefer partnerSlug prop over partnerSlugFromQuery to allow overriding', () => {
@@ -56,9 +55,7 @@ describe( 'JetpackConnectHeaderLogo', () => {
 		);
 
 		const logo = wrapper.find( '.jetpack-connect-header-logo__cobranded-logo' );
-		expect( logo ).to.have.length( 1 );
-		expect( logo.props().src ).to.equal(
-			'/calypso/images/jetpack/jetpack-milesweb-connection.png'
-		);
+		expect( logo ).toHaveLength( 1 );
+		expect( logo.prop( 'src' ) ).toBe( '/calypso/images/jetpack/jetpack-milesweb-connection.png' );
 	} );
 } );
