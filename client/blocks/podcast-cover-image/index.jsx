e69aadcce132444a51921149abe8e2f98cb2e9ae
Jonathan Sadowski <sadowski@automattic.com>
2018-05-17 13:49:31 -0500

    Convert PodcastCoverImage to class

--- client/blocks/podcast-cover-image/index.jsx
+++ client/blocks/podcast-cover-image/index.jsx
@@ -5,7 +5,7 @@
  */
 
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 import Gridicon from 'gridicons';
@@ -24,44 +24,47 @@ import {
 } from 'state/selectors/get-podcasting-cover-image';
 import resizeImageUrl from 'lib/resize-image-url';
 
-function PodcastCoverImage( { siteId, coverImageUrl, size, isTransientIcon } ) {
-	const imageSrc = resizeImageUrl( coverImageUrl, size );
-
-	const classes = classNames( 'podcast-cover-image', {
-		'is-blank': ! imageSrc,
-		'is-transient': isTransientIcon,
-	} );
-
-	const style = {
-		height: size,
-		width: size,
-		lineHeight: size + 'px',
-		fontSize: size + 'px',
+class PodcastCoverImage extends PureComponent {
+	static propTypes = {
+		siteId: PropTypes.number,
+		coverImageUrl: PropTypes.string,
+		size: PropTypes.number,
+		isTransientImage: PropTypes.bool,
 	};
 
-	return (
-		<div className={ classes } style={ style }>
-			{ siteId > 0 && <QuerySites siteId={ siteId } /> }
-			{ imageSrc ? (
-				<Image className="podcast-cover-image__img" src={ imageSrc } alt="" />
-			) : (
-				<Gridicon icon="globe" size={ Math.round( size / 1.3 ) } />
-			) }
-			{ isTransientIcon && <Spinner /> }
-		</div>
-	);
+	static defaultProps = {
+		size: 32,
+	};
+
+	render() {
+		const { siteId, coverImageUrl, size, isTransientIcon } = this.props;
+		const imageSrc = resizeImageUrl( coverImageUrl, size );
+
+		const classes = classNames( 'podcast-cover-image', {
+			'is-blank': ! imageSrc,
+			'is-transient': isTransientIcon,
+		} );
+
+		const style = {
+			height: size,
+			width: size,
+			lineHeight: size + 'px',
+			fontSize: size + 'px',
+		};
+
+		return (
+			<div className={ classes } style={ style }>
+				{ siteId > 0 && <QuerySites siteId={ siteId } /> }
+				{ imageSrc ? (
+					<Image className="podcast-cover-image__img" src={ imageSrc } alt="" />
+				) : (
+					<Gridicon icon="globe" size={ Math.round( size / 1.3 ) } />
+				) }
+				{ isTransientIcon && <Spinner /> }
+			</div>
+		);
+	}
 }
-
-PodcastCoverImage.propTypes = {
-	siteId: PropTypes.number,
-	coverImageUrl: PropTypes.string,
-	size: PropTypes.number,
-	isTransientImage: PropTypes.bool,
-};
-
-PodcastCoverImage.defaultProps = {
-	size: 32,
-};
 
 export default connect( state => {
 	const siteId = getSelectedSiteId( state );
