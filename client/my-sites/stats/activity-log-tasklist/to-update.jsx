e64d0baa6d649ea4d4597ad8fbd6e1d52f3c39cf
Elio Rivero <eliorivero@gmail.com>
2018-05-18 15:45:40 -0300

    Use generic requestSiteAlerts extending it to introduce the theme updates data

--- client/my-sites/stats/activity-log-tasklist/to-update.jsx
+++ client/my-sites/stats/activity-log-tasklist/to-update.jsx
@@ -12,22 +12,7 @@ import { get, unionBy, extend } from 'lodash';
  * Internal dependencies
  */
 import { getPluginsWithUpdates } from 'state/plugins/installed/selectors';
-import { requestHttpData, getHttpData } from 'state/data-layer/http-data';
-import { http } from 'state/data-layer/wpcom-http/actions';
-
-const getThemesWithUpdates = siteId =>
-	requestHttpData(
-		`theme-update-${ siteId }-data`,
-		http( {
-			method: 'GET',
-			apiNamespace: 'wpcom/v2',
-			path: `/sites/${ siteId }/alerts`,
-		} ),
-		{ fromApi: () => ( { updates: { themes: { id } } } ) => [ [ id, true ] ] }
-	);
-
-const themesWithUpdates = siteId =>
-	get( getHttpData( `theme-update-${ siteId }-data` ), 'data.updates.themes', [] );
+import { requestSiteAlerts } from 'state/data-getters';
 
 const mapUpdateNewVersionToVersion = plugin =>
 	extend( plugin, {
@@ -43,7 +28,6 @@ export default WrappedComponent => {
 			// Connected
 			plugins: PropTypes.arrayOf( PropTypes.object ),
 			themes: PropTypes.arrayOf( PropTypes.object ),
-			getThemesWithUpdates: PropTypes.func,
 		};
 
 		state = {
@@ -62,10 +46,6 @@ export default WrappedComponent => {
 			};
 		}
 
-		componentDidMount() {
-			this.props.getThemesWithUpdates();
-		}
-
 		render() {
 			return (
 				<WrappedComponent
@@ -77,13 +57,10 @@ export default WrappedComponent => {
 			);
 		}
 	}
-	return connect(
-		( state, { siteId } ) => ( {
+	return connect( ( state, { siteId } ) => {
+		return {
 			plugins: getPluginsWithUpdates( state, [ siteId ] ).map( mapUpdateNewVersionToVersion ),
-			themes: themesWithUpdates( siteId ),
-		} ),
-		( dispatch, { siteId } ) => ( {
-			getThemesWithUpdates: () => getThemesWithUpdates( siteId ),
-		} )
-	)( ToUpdate );
+			themes: get( requestSiteAlerts( siteId ), 'data.updates.themes', [] ),
+		};
+	} )( ToUpdate );
 };
--- client/state/data-getters/index.js
+++ client/state/data-getters/index.js
@@ -31,7 +31,7 @@ export const requestSiteAlerts = siteId => {
 		),
 		{
 			freshness: 5 * 60 * 1000,
-			fromApi: () => ( { suggestions, threats, warnings } ) => [
+			fromApi: () => ( { suggestions, threats, warnings, updates } ) => [
 				[
 					id,
 					{
@@ -55,6 +55,7 @@ export const requestSiteAlerts = siteId => {
 								: {} ),
 						} ) ),
 						warnings,
+						updates,
 					},
 				],
 			],
