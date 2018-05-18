f7b9886338eac71f280f18452fbc31f6f4019cfd
Eric Binnion <ericbinnion@gmail.com>
2018-05-18 12:31:45 -0500

    Components: JetpackHeader: Fix tests; Simplify Dreamhost logo

--- client/components/jetpack-header/test/index.js
+++ client/components/jetpack-header/test/index.js
@@ -8,32 +8,40 @@
  */
 import React from 'react';
 import { shallow } from 'enzyme';
+import { noop } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import JetpackHeader from '..';
-import JetpackLogo from 'components/jetpack-logo';
-import JetpackDreamhostLogo from 'components/jetpack-header/dreamhost';
+import { JetpackHeader } from '..';
 
 describe( 'JetpackHeader', () => {
-	test( 'renders Jetpack logo when no partnerSlug prop', () => {
+	test( 'renders default Jetpack logo when no partnerSlug prop', () => {
 		const wrapper = shallow( <JetpackHeader /> );
-		expect( wrapper.find( JetpackLogo ) ).toHaveLength( 1 );
+		expect( wrapper.children() ).toHaveLength( 1 );
+		expect( wrapper.children().name() ).toEqual( 'JetpackLogo' );
+		expect( wrapper.find( 'JetpackLogo' ).props().size ).toEqual( 45 );
 	} );
 
-	test( 'should render Jetpack logo when passed empty string for partnerSlug prop', () => {
+	test( 'should render default Jetpack logo when passed empty string for partnerSlug prop', () => {
 		const wrapper = shallow( <JetpackHeader partnerSlug="" /> );
-		expect( wrapper.find( JetpackLogo ) ).toHaveLength( 1 );
+		expect( wrapper.children() ).toHaveLength( 1 );
+		expect( wrapper.children().name() ).toEqual( 'JetpackLogo' );
+		expect( wrapper.find( 'JetpackLogo' ).props().size ).toEqual( 45 );
 	} );
 
 	test( 'should render JetpackLogo when passed an unknown partner slug', () => {
 		const wrapper = shallow( <JetpackHeader partnerSlug="nonexistent" /> );
-		expect( wrapper.find( JetpackLogo ) ).toHaveLength( 1 );
+		expect( wrapper.children() ).toHaveLength( 1 );
+		expect( wrapper.children().name() ).toEqual( 'JetpackLogo' );
+		expect( wrapper.find( 'JetpackLogo' ).props().size ).toEqual( 45 );
 	} );
 
 	test( 'should render a co-branded logo when passed a known partner slug', () => {
-		const wrapper = shallow( <JetpackHeader partnerSlug="dreamhost" /> );
-		expect( wrapper.find( JetpackDreamhostLogo ) ).toHaveLength( 1 );
+		const wrapper = shallow( <JetpackHeader partnerSlug="dreamhost" translate={ noop } /> );
+		expect( wrapper.find( 'JetpackDreamhostLogo' ) ).toHaveLength( 1 );
+		expect( wrapper.find( 'JetpackLogo' ) ).toHaveLength( 1 );
+		expect( wrapper.find( 'JetpackLogo' ).props().size ).toEqual( 150 );
+		expect( wrapper.find( 't' ).props().icon ).toEqual( 'plus-small' );
 	} );
 } );
