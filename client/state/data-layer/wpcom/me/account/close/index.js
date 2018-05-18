256d00a67765ff10ec0096d21cfa01723f004ee9
bluefuton <chris@bluefuton.com>
2018-05-18 13:58:17 +1200

    Account closure: add data layer and actions

---
+++ client/state/data-layer/wpcom/me/account/close/index.js
@@ -0,0 +1,52 @@
+/**
+ * @format
+ */
+
+/**
+ * External Dependencies
+ */
+import { translate } from 'i18n-calypso';
+import { noop } from 'lodash';
+
+/**
+ * Internal Dependencies
+ */
+import { ACCOUNT_CLOSE } from 'state/action-types';
+import { http } from 'state/data-layer/wpcom-http/actions';
+import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
+import { errorNotice } from 'state/notices/actions';
+
+export function requestAccountClose( action ) {
+	return http(
+		{
+			method: 'POST',
+			apiVersion: '1.1',
+			path: `/me/account/close`,
+			body: {}, // have to have an empty body to make wpcom-http happy
+		},
+		action
+	);
+}
+
+export function fromApi( response ) {
+	// don't need to check for existence of response because errors are handled
+	if ( ! response.success ) {
+		throw new Error( 'Account closure was unsuccessful', response );
+	}
+	return response;
+}
+
+export function receiveAccountCloseError() {
+	return errorNotice( translate( 'Sorry, there was a problem closing your account.' ) );
+}
+
+export default {
+	[ ACCOUNT_CLOSE ]: [
+		dispatchRequestEx( {
+			fetch: requestAccountClose,
+			onSuccess: noop,
+			onError: receiveAccountCloseError,
+			fromApi,
+		} ),
+	],
+};
---
+++ client/state/data-layer/wpcom/me/account/close/test/index.js
@@ -0,0 +1,52 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import { requestAccountClose, receiveAccountCloseError, fromApi } from '../';
+import { http } from 'state/data-layer/wpcom-http/actions';
+import { closeAccount } from 'state/account/close/actions';
+
+describe( 'account-close', () => {
+	describe( 'requestAccountClose', () => {
+		test( 'should dispatch a HTTP request', () => {
+			const action = closeAccount();
+			expect( requestAccountClose( action ) ).toEqual(
+				http(
+					{
+						method: 'POST',
+						path: '/me/account/close',
+						body: {},
+						apiVersion: '1.1',
+					},
+					action
+				)
+			);
+		} );
+	} );
+
+	describe( 'fromApi', () => {
+		it( 'should throw an error for an unsuccessful closure', () => {
+			expect( () => fromApi( { success: false } ) ).toThrow();
+		} );
+		it( 'should return original response for an successful closure', () => {
+			expect( fromApi( { success: true } ) ).toEqual( { success: true } );
+		} );
+	} );
+
+	describe( 'receiveAccountCloseError', () => {
+		test( 'should fire an error notice', () => {
+			const result = receiveAccountCloseError( closeAccount(), {
+				success: false,
+			} );
+
+			expect( result ).toEqual(
+				expect.objectContaining( {
+					notice: expect.objectContaining( {
+						status: 'is-error',
+					} ),
+				} )
+			);
+		} );
+	} );
+} );
