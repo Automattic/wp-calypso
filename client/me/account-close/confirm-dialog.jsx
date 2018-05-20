4f30249b6a0939ab8181eaba9f14c0efe4de6c2d
bluefuton <chris@bluefuton.com>
2018-05-21 11:25:49 +1200

    Account closure confirm dialog: use real username and activate button when usernames match

--- client/me/account-close/confirm-dialog.jsx
+++ client/me/account-close/confirm-dialog.jsx
@@ -6,6 +6,7 @@
 import React from 'react';
 import { localize } from 'i18n-calypso';
 import { noop } from 'lodash';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
@@ -13,61 +14,76 @@ import { noop } from 'lodash';
 import Dialog from 'components/dialog';
 import FormLabel from 'components/forms/form-label';
 import Button from 'components/button';
+import { getCurrentUser } from 'state/current-user/selectors';
 
-const AccountCloseConfirmDialog = ( {
-	isVisible,
-	username,
-	onConfirm,
-	translate,
-	closeConfirmDialog,
-	deleteAccount,
-} ) => {
-	const deleteButtons = [
-		<Button onClick={ closeConfirmDialog }>{ translate( 'Cancel' ) }</Button>,
-		<Button primary scary disabled onClick={ deleteAccount }>
-			{ translate( 'Close your account' ) }
-		</Button>,
-	];
+class AccountCloseConfirmDialog extends React.Component {
+	state = {
+		inputValue: '',
+	};
 
-	return (
-		<Dialog
-			isVisible={ isVisible }
-			buttons={ deleteButtons }
-			className="account-close__confirm-dialog"
-		>
-			<h1 className="account-close__confirm-dialog-header">
-				{ translate( 'Confirm account closure' ) }
-			</h1>
-			<FormLabel htmlFor="confirmAccountCloseInput" className="account-close__confirm-dialog-label">
-				{ translate(
-					'Please type {{warn}}%(username)s{{/warn}} in the field below to confirm. ' +
-						'Your account will then be gone forever.',
-					{
-						components: {
-							warn: <span className="account-close__confirm-dialog-target-username" />,
-						},
-						args: {
-							username: username,
-						},
-					}
-				) }
-			</FormLabel>
+	handleInputChange = event => {
+		this.setState( { inputValue: event.target.value.toLowerCase() } );
+	};
 
-			<input
-				autoCapitalize="off"
-				className="account-close__confirm-dialog-confirm-input"
-				type="text"
-				onChange={ onConfirm }
-				aria-required="true"
-				id="confirmAccountCloseInput"
-			/>
-		</Dialog>
-	);
-};
+	render() {
+		const { isVisible, currentUsername, onConfirm, translate, closeConfirmDialog } = this.props;
+		const isButtonDisabled = this.state.inputValue !== currentUsername;
+		const deleteButtons = [
+			<Button onClick={ closeConfirmDialog }>{ translate( 'Cancel' ) }</Button>,
+			<Button primary scary disabled={ isButtonDisabled } onClick={ onConfirm }>
+				{ translate( 'Close your account' ) }
+			</Button>,
+		];
+
+		return (
+			<Dialog
+				isVisible={ isVisible }
+				buttons={ deleteButtons }
+				className="account-close__confirm-dialog"
+			>
+				<h1 className="account-close__confirm-dialog-header">
+					{ translate( 'Confirm account closure' ) }
+				</h1>
+				<FormLabel
+					htmlFor="confirmAccountCloseInput"
+					className="account-close__confirm-dialog-label"
+				>
+					{ translate(
+						'Please type {{warn}}%(currentUsername)s{{/warn}} in the field below to confirm. ' +
+							'Your account will then be gone forever.',
+						{
+							components: {
+								warn: <span className="account-close__confirm-dialog-target-username" />,
+							},
+							args: {
+								currentUsername,
+							},
+						}
+					) }
+				</FormLabel>
+
+				<input
+					autoCapitalize="off"
+					className="account-close__confirm-dialog-confirm-input"
+					type="text"
+					onChange={ this.handleInputChange }
+					value={ this.state.inputValue }
+					aria-required="true"
+					id="confirmAccountCloseInput"
+				/>
+			</Dialog>
+		);
+	}
+}
 
 AccountCloseConfirmDialog.defaultProps = {
-	deleteAccount: noop,
-	username: 'yourusername', // @todo connect this to get real username
+	onConfirm: noop,
 };
 
-export default localize( AccountCloseConfirmDialog );
+export default connect( state => {
+	const user = getCurrentUser( state );
+
+	return {
+		currentUsername: user && user.username,
+	};
+} )( localize( AccountCloseConfirmDialog ) );
