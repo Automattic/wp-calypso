a1ed411edb8a386b23195617fcc35286cf27939d
Chris R <chris@bluefuton.com>
2018-05-22 09:29:41 +1200

    Account closure: fetch purchases and sites, and display appropriate message to user (#24963)
    * Fetch purchases and sites
    
    * Load purchases
    
    * Add loading placeholders
    
    * Update copy for the 3 different scenarios
    
    * Fix purchases check
    
    * Fix Atomic sites copy

--- client/me/account-close/main.jsx
+++ client/me/account-close/main.jsx
@@ -3,10 +3,12 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import page from 'page';
 import Gridicon from 'gridicons';
 import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
 
 /**
  * Internal dependencies
@@ -23,6 +25,12 @@ import ActionPanelLink from 'components/action-panel/link';
 import ActionPanelFooter from 'components/action-panel/footer';
 import Button from 'components/button';
 import AccountCloseConfirmDialog from './confirm-dialog';
+import QueryUserPurchases from 'components/data/query-user-purchases';
+import QuerySites from 'components/data/query-sites';
+import { getCurrentUser } from 'state/current-user/selectors';
+import hasLoadedSites from 'state/selectors/has-loaded-sites';
+import userHasAnyAtomicSites from 'state/selectors/user-has-any-atomic-sites';
+import { hasLoadedUserPurchasesFromServer, getUserPurchases } from 'state/purchases/selectors';
 
 class AccountSettingsClose extends Component {
 	state = {
@@ -36,7 +44,10 @@ class AccountSettingsClose extends Component {
 	handleDeleteClick = event => {
 		event.preventDefault();
 
-		// @todo check if purchases and sites have loaded
+		// Check if purchases and sites have loaded
+		if ( this.props.isLoading ) {
+			return false;
+		}
 
 		this.setState( { showConfirmDialog: true } );
 	};
@@ -46,11 +57,16 @@ class AccountSettingsClose extends Component {
 	};
 
 	render() {
-		const { translate } = this.props;
-		const hasAtomicSites = false;
+		const { translate, currentUserId, hasAtomicSites, hasPurchases, isLoading } = this.props;
+		const isDeletePossible = ! isLoading && ! hasAtomicSites && ! hasPurchases;
+		const containerClasses = classnames( 'account-close', 'main', {
+			'is-loading': isLoading,
+		} );
 
 		return (
-			<div className="account-close main" role="main">
+			<div className={ containerClasses } role="main">
+				{ currentUserId && <QueryUserPurchases userId={ currentUserId } /> }
+				<QuerySites allSites />
 				<HeaderCake onClick={ this.goBack }>
 					<h1>{ translate( 'Close account' ) }</h1>
 				</HeaderCake>
@@ -59,38 +75,74 @@ class AccountSettingsClose extends Component {
 						{ translate( 'Close account' ) }
 					</ActionPanelTitle>
 					<ActionPanelBody>
-						<ActionPanelFigure>
-							<ActionPanelFigureHeader>
-								{ translate( 'These items will be deleted' ) }
-							</ActionPanelFigureHeader>
-							<ActionPanelFigureList>
-								<ActionPanelFigureListItem>
-									{ translate( 'Personal details' ) }
-								</ActionPanelFigureListItem>
-								<ActionPanelFigureListItem>{ translate( 'Sites' ) }</ActionPanelFigureListItem>
-								<ActionPanelFigureListItem>{ translate( 'Posts' ) }</ActionPanelFigureListItem>
-								<ActionPanelFigureListItem>{ translate( 'Pages' ) }</ActionPanelFigureListItem>
-								<ActionPanelFigureListItem>{ translate( 'Media' ) }</ActionPanelFigureListItem>
-								<ActionPanelFigureListItem>{ translate( 'Domains' ) }</ActionPanelFigureListItem>
-							</ActionPanelFigureList>
-						</ActionPanelFigure>
-						{ ! hasAtomicSites && (
-							<div>
-								<p>
+						{ isDeletePossible && (
+							<ActionPanelFigure>
+								<ActionPanelFigureHeader>
+									{ translate( 'These items will be deleted' ) }
+								</ActionPanelFigureHeader>
+								<ActionPanelFigureList>
+									<ActionPanelFigureListItem>
+										{ translate( 'Personal details' ) }
+									</ActionPanelFigureListItem>
+									<ActionPanelFigureListItem>{ translate( 'Sites' ) }</ActionPanelFigureListItem>
+									<ActionPanelFigureListItem>{ translate( 'Posts' ) }</ActionPanelFigureListItem>
+									<ActionPanelFigureListItem>{ translate( 'Pages' ) }</ActionPanelFigureListItem>
+									<ActionPanelFigureListItem>{ translate( 'Media' ) }</ActionPanelFigureListItem>
+									<ActionPanelFigureListItem>{ translate( 'Domains' ) }</ActionPanelFigureListItem>
+								</ActionPanelFigureList>
+							</ActionPanelFigure>
+						) }
+						{ ! isLoading &&
+							hasAtomicSites && (
+								<Fragment>
+									<p className="account-close__body-copy">
+										{ translate(
+											'Account closure cannot be undone and will remove all sites and content.'
+										) }
+									</p>
+									<p className="account-close__body-copy">
+										{ translate(
+											"To close your account, you'll need to {{a}}contact our support team{{/a}}.",
+											{
+												components: {
+													a: <ActionPanelLink href="/help/contact" />,
+												},
+											}
+										) }
+									</p>
+								</Fragment>
+							) }
+						{ ! isLoading &&
+							hasPurchases &&
+							! hasAtomicSites && (
+								<Fragment>
+									<p className="account-close__body-copy">
+										{ translate( 'You still have active purchases on your account.' ) }
+									</p>
+									<p className="account-close__body-copy">
+										{ translate(
+											"To delete your account, you'll need to cancel any active purchases " +
+												'in {{a}}Manage Purchases{{/a}} before proceeding.',
+											{
+												components: {
+													a: <ActionPanelLink href="/me/purchases" />,
+												},
+											}
+										) }
+									</p>
+								</Fragment>
+							) }
+						{ ( isLoading || isDeletePossible ) && (
+							<Fragment>
+								<p className="account-close__body-copy">
 									{ translate(
-										'Account closure {{strong}}cannot{{/strong}} be undone, ' +
-											'and will remove all sites and content.',
-										{
-											components: {
-												strong: <strong />,
-											},
-										}
+										'Account closure cannot be undone and will remove all sites and content.'
 									) }
 								</p>
-								<p>
+								<p className="account-close__body-copy">
 									{ translate(
 										"If you're unsure about what account closure means or have any other questions, " +
-											'please {{a}}chat with someone from our support team{{/a}} before proceeding.',
+											'{{a}}chat with someone from our support team{{/a}} before going ahead.',
 										{
 											components: {
 												a: <ActionPanelLink href="/help/contact" />,
@@ -98,37 +150,27 @@ class AccountSettingsClose extends Component {
 										}
 									) }
 								</p>
-							</div>
-						) }
-						{ hasAtomicSites && (
-							<div>
-								<p>
-									{ translate(
-										"To close your account, you'll need to contact our support team. Account closure cannot be undone " +
-											'and will remove all sites and content.'
-									) }
-								</p>
-								<p>
-									{ translate(
-										"If you're unsure about what account closure means or have any other questions, " +
-											"you'll have a chance to chat with someone from our support team before anything happens."
-									) }
-								</p>
-							</div>
+							</Fragment>
 						) }
 					</ActionPanelBody>
 					<ActionPanelFooter>
-						{ ! hasAtomicSites && (
+						{ ( isLoading || isDeletePossible ) && (
 							<Button scary onClick={ this.handleDeleteClick }>
 								<Gridicon icon="trash" />
-								{ translate( 'Close account' ) }
+								{ translate( 'Close Account' ) }
 							</Button>
 						) }
 						{ hasAtomicSites && (
 							<Button primary href="/help/contact">
-								{ translate( 'Contact support' ) }
+								{ translate( 'Contact Support' ) }
 							</Button>
 						) }
+						{ hasPurchases &&
+							! hasAtomicSites && (
+								<Button primary href="/me/purchases">
+									{ translate( 'Manage Purchases' ) }
+								</Button>
+							) }
 					</ActionPanelFooter>
 					<AccountCloseConfirmDialog
 						isVisible={ this.state.showConfirmDialog }
@@ -140,4 +182,17 @@ class AccountSettingsClose extends Component {
 	}
 }
 
-export default localize( AccountSettingsClose );
+export default connect( state => {
+	const user = getCurrentUser( state );
+	const currentUserId = user && user.ID;
+	const purchases = getUserPurchases( state, currentUserId );
+	const isLoading =
+		! purchases || ! hasLoadedSites( state ) || ! hasLoadedUserPurchasesFromServer( state );
+
+	return {
+		currentUserId: user && user.ID,
+		isLoading,
+		hasPurchases: purchases && purchases.length > 0,
+		hasAtomicSites: userHasAnyAtomicSites( state ),
+	};
+} )( localize( AccountSettingsClose ) );
