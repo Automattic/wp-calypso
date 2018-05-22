7fcf6aee1a333b609cdc9dfdff79ac940cacb959
Rich Collier <rcoll@users.noreply.github.com>
2018-05-22 11:04:33 -0400

    Activity Log: Replace Rewind buttons with Clone buttons when enableClone is set (#24854)

--- client/my-sites/stats/activity-log-item/index.jsx
+++ client/my-sites/stats/activity-log-item/index.jsx
@@ -212,6 +212,7 @@ class ActivityLogItem extends Component {
 
 	renderItemAction() {
 		const {
+			enableClone,
 			hideRestore,
 			activity: { activityIsRewindable, activityName, activityMeta },
 			plugin,
@@ -254,7 +255,27 @@ class ActivityLogItem extends Component {
 		if ( ! hideRestore && activityIsRewindable ) {
 			return this.renderRewindAction();
 		}
+
+		if ( enableClone && activityIsRewindable ) {
+			return this.renderCloneAction();
+		}
 	}
+
+	renderCloneAction = () => {
+		const { translate } = this.props;
+
+		return (
+			<Button
+				className="activity-log-item__clone-action"
+				primary
+				onClick={ this.performCloneAction }
+			>
+				{ translate( 'Clone from here' ) }
+			</Button>
+		);
+	};
+
+	performCloneAction = () => this.props.cloneOnClick( this.props.activity.activityTs );
 
 	renderRewindAction() {
 		const { createBackup, createRewind, disableRestore, disableBackup, translate } = this.props;
