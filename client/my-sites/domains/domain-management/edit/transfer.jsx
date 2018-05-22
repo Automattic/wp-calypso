4984a382e814735fd17bba3d37eaf9070d2af999
Andrija Vucinic <andrija.vucinic@gmail.com>
2018-05-22 19:21:22 +0200

    remove resend email copy, as it's on the losing registrar side

--- client/my-sites/domains/domain-management/edit/transfer.jsx
+++ client/my-sites/domains/domain-management/edit/transfer.jsx
@@ -27,8 +27,6 @@ import VerticalNav from 'components/vertical-nav';
 import VerticalNavItem from 'components/vertical-nav/item';
 import { cancelPurchase as cancelPurchaseLink } from 'me/purchases/paths';
 import { Notice } from 'components/notice';
-import { get } from 'lodash';
-import InboundTransferEmailVerificationCard from 'my-sites/domains/domain-management/components/inbound-transfer-verification';
 import { domainManagementTransferInPrecheck } from 'my-sites/domains/paths';
 
 class Transfer extends React.PureComponent {
@@ -110,7 +108,6 @@ class Transfer extends React.PureComponent {
 
 		return (
 			<div className="edit__domain-details-card">
-				{ this.renderInboundTransferEmailNotice() }
 				{ transferNotice }
 				<Header domain={ domain } />
 				{ content }
@@ -118,22 +115,6 @@ class Transfer extends React.PureComponent {
 			</div>
 		);
 	}
-
-	renderInboundTransferEmailNotice = () => {
-		const domain = this.props.domain;
-		const isPendingVerification = transferStatus.PENDING_OWNER === get( domain, 'transferStatus' );
-
-		if ( ! isPendingVerification ) {
-			return null;
-		}
-
-		return (
-			<InboundTransferEmailVerificationCard
-				domain={ domain }
-				selectedSiteSlug={ this.props.selectedSite.slug }
-			/>
-		);
-	};
 
 	handlePaymentSettingsClick = () => {
 		this.props.paymentSettingsClick( this.props.domain );
