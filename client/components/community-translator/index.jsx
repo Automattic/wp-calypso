791ec89d8f5c9b01f2c7074d6a8f4a0aff847392
James Tien <southp0105@gmail.com>
2018-05-23 15:37:20 +0800

    Update the function signature of `wrapTranslation` so it fits the callback directly and we don't need to wrap it with one more lambda function.

--- client/components/community-translator/index.jsx
+++ client/components/community-translator/index.jsx
@@ -31,9 +31,7 @@ class CommunityTranslator extends Component {
 		this.setLanguage();
 
 		// wrap translations from i18n
-		i18n.registerTranslateHook( ( translation, options ) =>
-			this.wrapTranslation( options.original, translation, options )
-		);
+		i18n.registerTranslateHook( this.wrapTranslation );
 
 		// callback when translated component changes.
 		// the callback is overwritten by the translator on load/unload, so we're returning it within an anonymous function.
@@ -49,21 +47,20 @@ class CommunityTranslator extends Component {
 		userSettings.off( 'change', this.setLanguage );
 	}
 
-	setLanguage() {
+	setLanguage = () => {
 		this.languageJson = i18n.getLocale() || { '': {} };
 		const { localeSlug, localeVariant } = this.languageJson[ '' ];
 		this.localeCode = localeVariant || localeSlug;
 		this.currentLocale = find( languages, lang => lang.langSlug === this.localeCode );
-	}
+	};
 
 	/**
 	 * Wraps translation in a DOM object and attaches `toString()` method in case in can't be rendered
-	 * @param { String } originalFromPage - original string
 	 * @param { String } displayedTranslationFromPage - translated string
 	 * @param  { Object } optionsFromPage - i18n.translate options
 	 * @returns {Object} DOM object
 	 */
-	wrapTranslation( originalFromPage, displayedTranslationFromPage, optionsFromPage ) {
+	wrapTranslation = ( displayedTranslationFromPage, optionsFromPage ) => {
 		if ( ! isCommunityTranslatorEnabled() ) {
 			return displayedTranslationFromPage;
 		}
@@ -71,6 +68,8 @@ class CommunityTranslator extends Component {
 		if ( 'object' !== typeof optionsFromPage ) {
 			optionsFromPage = {};
 		}
+
+		const originalFromPage = optionsFromPage.original;
 
 		if ( 'string' !== typeof originalFromPage ) {
 			debug( 'unknown original format' );
@@ -124,7 +123,7 @@ class CommunityTranslator extends Component {
 		Object.freeze( translatableElement );
 
 		return translatableElement;
-	}
+	};
 
 	render() {
 		return null;
