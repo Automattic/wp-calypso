e88c27515ac869f9b6d0273f7b5eabe1ad942332
Yury Michurin <yury.michurin@gmail.com>
2018-05-23 09:26:12 +0300

    Adjust JSDOC for getGoogleMyBusinessStatsError

--- client/state/selectors/get-google-my-business-stats-error.js
+++ client/state/selectors/get-google-my-business-stats-error.js
@@ -6,14 +6,14 @@
 import { get } from 'lodash';
 
 /**
- * Retrieves Google My Business stats for the specified criteria.
+ * Retrieves Google My Business stats error for the specified criteria.
  *
  * @param {Object} state - Global state tree
  * @param {Number} siteId - Id of the site
  * @param {String} statType - Type of metrics (e.g. 'queries')
  * @param {String} interval - Time period (e.g. 'month')
  * @param {String} aggregation - Type of aggregation (e.g. 'daily')
- * @return {Number} the corresponding stats, or null if not found
+ * @return {Object} the corresponding stats error, or null if there is no error
  */
 export default function getGoogleMyBusinessStatsError(
 	state,
