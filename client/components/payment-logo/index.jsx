fe65a331ca3aed5bfc3b48cc376fa4caf1254b5e
Jui-Shan Liang (Jenny) <jenny@jsliang.com>
2018-05-24 04:52:37 +0800

    PaymentLogo: Improve documentation and auto-generate altText property (#24769)
    This change improves the documentation for the `PaymentLogo` component. It also adds auto-generation of its `altText` property derived from the `type` property.

--- client/components/payment-logo/index.jsx
+++ client/components/payment-logo/index.jsx
@@ -7,20 +7,47 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { keys } from 'lodash';
+
+const ALT_TEXT = {
+	alipay: 'Alipay',
+	amex: 'American Express',
+	bancontact: 'Bancontact',
+	diners: 'Diners Club',
+	discover: 'Discover',
+	eps: 'eps',
+	giropay: 'Giropay',
+	ideal: 'iDEAL',
+	jcb: 'JCB',
+	mastercard: 'MasterCard',
+	p24: 'Przelewy24',
+	paypal: 'PayPal',
+	placeholder: '',
+	unionpay: 'UnionPay',
+	visa: 'VISA',
+};
+
+export const POSSIBLE_TYPES = keys( ALT_TEXT );
 
 class PaymentLogo extends React.Component {
 	static propTypes = {
-		type: PropTypes.string.isRequired,
-		altText: PropTypes.string.isRequired,
+		className: PropTypes.string,
+		type: PropTypes.oneOf( POSSIBLE_TYPES ),
+		altText: PropTypes.string,
 		isCompact: PropTypes.bool,
 	};
 
 	render() {
-		const classes = classNames( 'payment-logo', `is-${ this.props.type }`, {
-			'is-compact': this.props.isCompact,
-		} );
+		const { altText, className, isCompact, type } = this.props;
 
-		return <div className={ classes } aria-label={ this.props.altText } />;
+		const classes = classNames(
+			'payment-logo',
+			`is-${ type }`,
+			{ 'is-compact': isCompact },
+			className
+		);
+
+		return <div className={ classes } aria-label={ altText || ALT_TEXT[ type ] || '' } />;
 	}
 }
 
