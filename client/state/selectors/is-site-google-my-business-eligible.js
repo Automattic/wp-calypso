1c43aa4a4edb065fe28d9dd1746900483f856641
Tugdual de Kerviler <dekervit@gmail.com>
2018-05-25 14:38:35 -0500

    Deploy Google My Business "Connect your Location"

--- client/state/selectors/is-site-google-my-business-eligible.js
+++ client/state/selectors/is-site-google-my-business-eligible.js
@@ -3,31 +3,14 @@
 /**
  * External dependencies
  */
-import config from 'config';
 
 /**
  * Internal dependencies
  */
 import createSelector from 'lib/create-selector';
-import { getSiteOption, getSitePlanSlug } from 'state/sites/selectors';
+import { getSitePlanSlug } from 'state/sites/selectors';
 import { planMatches } from 'lib/plans';
 import { TYPE_BUSINESS, GROUP_WPCOM, GROUP_JETPACK, TYPE_PREMIUM } from 'lib/plans/constants';
-
-/**
- * Returns true if site has promote goal set
- *
- * @param  {Object}  state  Global state tree
- * @param  {String}  siteId The Site ID
- * @return {Boolean} True if site has "promote" goal
- */
-const siteHasPromoteGoal = createSelector(
-	( state, siteId ) => {
-		const siteGoals = ( getSiteOption( state, siteId, 'site_goals' ) || '' ).split( ',' );
-
-		return siteGoals.indexOf( 'promote' ) !== -1;
-	},
-	( state, siteId ) => [ getSiteOption( state, siteId, 'site_goals' ) ]
-);
 
 /**
  * Returns true if site has business plan
@@ -74,13 +57,5 @@ export const siteHasEligibleJetpackPlan = createSelector(
  * @return {Boolean} True if we should show the nudge
  */
 export default function isSiteGoogleMyBusinessEligible( state, siteId ) {
-	// call-for-testing condition, remove on launch
-	if ( config.isEnabled( 'google-my-business' ) ) {
-		return siteHasBusinessPlan( state, siteId ) || siteHasEligibleJetpackPlan( state, siteId );
-	}
-
-	return (
-		( siteHasBusinessPlan( state, siteId ) && siteHasPromoteGoal( state, siteId ) ) ||
-		siteHasEligibleJetpackPlan( state, siteId )
-	);
+	return siteHasBusinessPlan( state, siteId ) || siteHasEligibleJetpackPlan( state, siteId );
 }
