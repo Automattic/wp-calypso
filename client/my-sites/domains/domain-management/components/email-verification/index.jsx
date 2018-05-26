1ebcac9a1e983b984640b3b86837a564c5923912
Igor Klimer <igor@klimer.eu>
2018-05-26 03:22:12 +0200

    Domains: Show a maintenance notice for verification emails
    They are currently disabled on Tucows side, so let's show some
    semi-helpful message to users.

--- client/my-sites/domains/domain-management/components/email-verification/index.jsx
+++ client/my-sites/domains/domain-management/components/email-verification/index.jsx
@@ -72,10 +72,13 @@ class EmailVerificationCard extends React.Component {
 			waiting: ! emailSent,
 			sent: emailSent,
 		} );
+		const hideButtons = true;
 		let statusIcon = 'notice-outline';
-		let statusText = translate( 'Check your email — instructions sent to %(email)s.', {
-			args: { email: contactEmail },
-		} );
+		let statusText = translate(
+			'Verification services are temporarily interrupted. ' +
+				'We hope to be able to send the email within 48 hours. ' +
+				'Please review and act upon receipt.'
+		);
 
 		if ( emailSent ) {
 			statusIcon = 'mail';
@@ -90,24 +93,25 @@ class EmailVerificationCard extends React.Component {
 					<Gridicon icon={ statusIcon } size={ 36 } />
 					{ statusText }
 
-					{ ! emailSent && (
-						<div>
-							<Button
-								compact
-								busy={ submitting }
-								disabled={ submitting }
-								onClick={ this.handleSubmit }
-							>
-								{ submitting ? translate( 'Sending…' ) : translate( 'Send Again' ) }
-							</Button>
-
-							{ changeEmailHref && (
-								<Button compact href={ changeEmailHref } onClick={ this.props.onClick }>
-									{ this.props.translate( 'Change Email Address' ) }
+					{ ! emailSent &&
+						! hideButtons && (
+							<div>
+								<Button
+									compact
+									busy={ submitting }
+									disabled={ submitting }
+									onClick={ this.handleSubmit }
+								>
+									{ submitting ? translate( 'Sending…' ) : translate( 'Send Again' ) }
 								</Button>
-							) }
-						</div>
-					) }
+
+								{ changeEmailHref && (
+									<Button compact href={ changeEmailHref } onClick={ this.props.onClick }>
+										{ this.props.translate( 'Change Email Address' ) }
+									</Button>
+								) }
+							</div>
+						) }
 				</div>
 			</div>
 		);
