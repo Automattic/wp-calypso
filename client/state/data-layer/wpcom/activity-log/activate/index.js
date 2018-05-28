9a60d09f6a9b10ae2590210b26a52ae43144a7d6
Dennis Snell <dennis.snell@automattic.com>
2018-05-28 19:14:53 +0100

    Framework: `dispatchRequest` update (activity log activate)
    See #25121
    
    In this patch we're replacing the use of dispatchRequest()
    in the data layer handler to use the newer API exposed
    as dispatchRequestEx() This should have no change in
    actual effect or interaction.

--- client/state/data-layer/wpcom/activity-log/activate/index.js
+++ client/state/data-layer/wpcom/activity-log/activate/index.js
@@ -12,44 +12,54 @@ import { get } from 'lodash';
  */
 import { REWIND_ACTIVATE_REQUEST, REWIND_STATE_UPDATE } from 'state/action-types';
 import { rewindActivateFailure, rewindActivateSuccess } from 'state/activity-log/actions';
-import { dispatchRequest, getData } from 'state/data-layer/wpcom-http/utils';
+import { dispatchRequestEx, getData } from 'state/data-layer/wpcom-http/utils';
 import { http } from 'state/data-layer/wpcom-http/actions';
 import { errorNotice } from 'state/notices/actions';
 import { transformApi } from 'state/data-layer/wpcom/sites/rewind/api-transformer';
 
-const activateRewind = ( { dispatch }, action ) => {
+const activateRewind = action => {
 	const params = {
 		method: 'POST',
 		path: `/activity-log/${ action.siteId }/rewind/activate`,
 		apiVersion: '1',
 	};
+
 	if ( action.isVpMigrate ) {
 		params.body = { rewindOptIn: true };
 	}
-	dispatch( http( params, action ) );
+
+	return http( params, action );
 };
 
-export const activateSucceeded = ( { dispatch }, action ) => {
-	dispatch( rewindActivateSuccess( action.siteId ) );
+export const activateSucceeded = action => {
+	const successNotifier = rewindActivateSuccess( action.siteId );
 	const rawData = getData( action );
-	if ( undefined !== get( rawData, 'rewind_state', undefined ) ) {
-		dispatch( {
+
+	if ( undefined === get( rawData, 'rewind_state', undefined ) ) {
+		return successNotifier;
+	}
+
+	return [
+		successNotifier,
+		{
 			type: REWIND_STATE_UPDATE,
 			siteId: action.siteId,
 			data: transformApi( rawData.rewind_state ),
-		} );
-	}
+		},
+	];
 };
 
-export const activateFailed = ( { dispatch }, { siteId }, { message } ) => {
-	dispatch(
-		errorNotice( translate( 'Problem activating rewind: %(message)s', { args: { message } } ) )
-	);
-	dispatch( rewindActivateFailure( siteId ) );
-};
+export const activateFailed = ( { siteId }, { message } ) => [
+	errorNotice( translate( 'Problem activating rewind: %(message)s', { args: { message } } ) ),
+	rewindActivateFailure( siteId ),
+];
 
 export default {
 	[ REWIND_ACTIVATE_REQUEST ]: [
-		dispatchRequest( activateRewind, activateSucceeded, activateFailed ),
+		dispatchRequestEx( {
+			fetch: activateRewind,
+			onSuccess: activateSucceeded,
+			onError: activateFailed,
+		} ),
 	],
 };
