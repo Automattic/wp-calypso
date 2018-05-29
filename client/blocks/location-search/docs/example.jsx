24d43c8bb6a12b4dc455c4afc920d122916032fa
Stéphane Thomas <email@stephanethomas.com>
2018-05-29 13:45:47 +0200

    Location Search: Display notice when location result is clicked

--- client/blocks/location-search/docs/example.jsx
+++ client/blocks/location-search/docs/example.jsx
@@ -3,15 +3,35 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import LocationSearch from 'blocks/location-search';
+import { createNotice } from 'state/notices/actions';
 
-const LocationSearchExample = () => <LocationSearch />;
+class LocationSearchExample extends Component {
+	propTypes = {
+		createNotice: PropTypes.func.isRequired,
+	};
 
-LocationSearchExample.displayName = 'LocationSearch';
+	handlePredictionClick = ( prediction ) => {
+		this.props.createNotice(
+			'is-info',
+			`You clicked the '${ prediction.structured_formatting.main_text }' location`
+		);
+	};
 
-export default LocationSearchExample;
+	render() {
+		return (
+			<LocationSearch onPredictionClick={ this.handlePredictionClick } />
+		);
+	}
+}
+
+const ConnectedLocationSearchExample = connect( null, { createNotice } )( LocationSearchExample );
+ConnectedLocationSearchExample.displayName = 'LocationSearch';
+export default ConnectedLocationSearchExample;
