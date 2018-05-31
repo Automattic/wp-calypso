52b32d6db60c0681683f3dfe8f6e27b043551b77
Stéphane Thomas <email@stephanethomas.com>
2018-05-31 18:00:54 +0200

    Form Country Select: Add unit tests

--- client/components/forms/form-country-select/index.jsx
+++ client/components/forms/form-country-select/index.jsx
@@ -11,55 +11,55 @@ import observe from 'lib/mixins/data-observe';
 import { isEmpty, omit } from 'lodash';
 import { localize } from 'i18n-calypso';
 
-export default localize(
-	createReactClass( {
-		displayName: 'FormCountrySelect',
+export const FormCountrySelect = createReactClass( {
+	displayName: 'FormCountrySelect',
 
-		mixins: [ observe( 'countriesList' ) ],
+	mixins: [ observe( 'countriesList' ) ],
 
-		getOptions( countriesList ) {
-			if ( isEmpty( countriesList ) ) {
-				return [
-					{
-						key: '',
-						label: this.props.translate( 'Loading…' ),
-					},
-				];
-			}
-			return countriesList.map( ( { code, name }, idx ) => ( {
-				key: idx,
-				label: name,
-				code,
-				disabled: ! code,
-			} ) );
-		},
+	getOptions( countriesList ) {
+		if ( isEmpty( countriesList ) ) {
+			return [
+				{
+					key: '',
+					label: this.props.translate( 'Loading…' ),
+				},
+			];
+		}
+		return countriesList.map( ( { code, name }, idx ) => ( {
+			key: idx,
+			label: name,
+			code,
+			disabled: !code,
+		} ) );
+	},
 
-		render() {
-			const countriesList = this.props.countriesList.get(),
-				options = this.getOptions( countriesList );
+	render() {
+		const countriesList = this.props.countriesList.get(),
+			options = this.getOptions( countriesList );
 
-			return (
-				<select
-					{ ...omit( this.props, [
-						'className',
-						'countriesList',
-						'translate',
-						'moment',
-						'numberFormat',
-					] ) }
-					className={ classnames( this.props.className, 'form-country-select' ) }
-					onChange={ this.props.onChange }
-					disabled={ this.props.disabled }
-				>
-					{ options.map( function( option ) {
-						return (
-							<option key={ option.key } value={ option.code } disabled={ option.disabled }>
-								{ option.label }
-							</option>
-						);
-					} ) }
-				</select>
-			);
-		},
-	} )
-);
+		return (
+			<select
+				{ ...omit( this.props, [
+					'className',
+					'countriesList',
+					'translate',
+					'moment',
+					'numberFormat',
+				] ) }
+				className={ classnames( this.props.className, 'form-country-select' ) }
+				onChange={ this.props.onChange }
+				disabled={ this.props.disabled }
+			>
+				{ options.map( function( option ) {
+					return (
+						<option key={ option.key } value={ option.code } disabled={ option.disabled }>
+							{ option.label }
+						</option>
+					);
+				} ) }
+			</select>
+		);
+	},
+} );
+
+export default localize( FormCountrySelect );
