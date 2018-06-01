86afec270a02c6265ecd3e16d503803fe2a7191d
Jon Surrell <jon.surrell@automattic.com>
2018-06-01 11:05:27 +0200

    Explicit path params managePurchase

--- client/me/purchases/index.js
+++ client/me/purchases/index.js
@@ -59,7 +59,7 @@ export default function( router ) {
 	);
 
 	router(
-		paths.managePurchase(),
+		paths.managePurchase( ':site', ':purchaseId' ),
 		redirectLoggedOut,
 		sidebar,
 		siteSelection,
