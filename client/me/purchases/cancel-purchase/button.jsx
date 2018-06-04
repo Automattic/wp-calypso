3b2a6fed12c5229915d5d116b015924a88503110
Jon Surrell <jon.surrell@automattic.com>
2018-06-04 17:19:06 +0200

    No selectedSite requirement for CancelPurchaseButton

--- client/me/purchases/cancel-purchase/button.jsx
+++ client/me/purchases/cancel-purchase/button.jsx
@@ -45,7 +45,7 @@ import isPrecancellationChatAvailable from 'state/happychat/selectors/is-precanc
 class CancelPurchaseButton extends Component {
 	static propTypes = {
 		purchase: PropTypes.object.isRequired,
-		selectedSite: PropTypes.object.isRequired,
+		selectedSite: PropTypes.object,
 		cancelBundledDomain: PropTypes.bool.isRequired,
 		includedDomainPurchase: PropTypes.object,
 		disabled: PropTypes.bool,
@@ -191,9 +191,7 @@ class CancelPurchaseButton extends Component {
 	};
 
 	goToCancelConfirmation = () => {
-		const { id } = this.props.purchase,
-			{ slug } = this.props.selectedSite;
-
+		const { id } = this.props.purchase;
 		page( confirmCancelDomain( id ) );
 	};
 
@@ -295,7 +293,7 @@ class CancelPurchaseButton extends Component {
 
 			submitSurvey(
 				'calypso-remove-purchase',
-				this.props.selectedSite.ID,
+				purchase.siteId,
 				enrichedSurveyData( surveyData, moment(), selectedSite, purchase )
 			);
 		}
