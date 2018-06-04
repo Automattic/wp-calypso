901faf5c49fabd2ece89a72329db7e417706eaf1
James Nylen <jnylen@gmail.com>
2018-06-04 11:11:59 -0500

    Do not show podcast indicators for private sites
    We hide and disable the settings screen, and we should do the same for
    any other indications that podcasting is enabled..

--- client/state/selectors/get-podcasting-category-id.js
+++ client/state/selectors/get-podcasting-category-id.js
@@ -4,6 +4,7 @@
  * Internal dependencies
  */
 import { get } from 'lodash';
+import isPrivateSite from 'state/selectors/is-private-site';
 
 /**
  * Returns the Podcasting category ID for a given site ID.
@@ -13,5 +14,11 @@ import { get } from 'lodash';
  * @return {Number}          Category ID or null if not found
  */
 export default function getPodcastingCategoryId( state, siteId ) {
+	if ( isPrivateSite( state, siteId ) ) {
+		// Podcasting relies on the RSS feed being public, so it is disabled on
+		// private sites.
+		return null;
+	}
+
 	return get( state.siteSettings.items, [ siteId, 'podcasting_category_id' ], null );
 }
