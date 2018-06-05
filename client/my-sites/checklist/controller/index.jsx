6a229a2628c0de9f920ed244e94adb1a9b1639b3
Jeff Bowen <jeff@automattic.com>
2018-06-05 01:06:56 -0400

    Checklist: Redirect if the current user has insufficient capabilities

--- client/my-sites/checklist/controller/index.jsx
+++ client/my-sites/checklist/controller/index.jsx
@@ -5,15 +5,25 @@
  */
 
 import React from 'react';
+import page from 'page';
 import { get } from 'lodash';
 
 /**
  * Internal Dependencies
  */
-
+import { getSelectedSiteId } from 'state/ui/selectors';
+import canCurrentUser from 'state/selectors/can-current-user';
 import ChecklistShow from '../checklist-show';
 
 export function show( context, next ) {
+	const state = context.store.getState();
+	const siteId = getSelectedSiteId( state );
+
+	//@TODO This doesn't work until the state gets hydrated....hrmmm
+	if ( ! canCurrentUser( state, siteId, 'manage_options' ) ) {
+		return page.redirect( '/view/' + get( context, 'params.site_id' ) );
+	}
+
 	const displayMode = get( context, 'query.d' );
 	context.primary = <ChecklistShow displayMode={ displayMode } />;
 	next();
