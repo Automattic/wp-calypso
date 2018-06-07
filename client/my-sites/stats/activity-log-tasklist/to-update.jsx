7ac0a8e621089c11d570250012578e3b40b72bb6
Elio Rivero <eliorivero@gmail.com>
2018-06-07 16:06:24 -0300

    Activity Log: make theme updates available to all users

--- client/my-sites/stats/activity-log-tasklist/to-update.jsx
+++ client/my-sites/stats/activity-log-tasklist/to-update.jsx
@@ -13,7 +13,6 @@ import { get, unionBy } from 'lodash';
  */
 import { getPluginsWithUpdates } from 'state/plugins/installed/selectors';
 import { requestSiteAlerts } from 'state/data-getters';
-import config from 'config';
 
 const emptyList = [];
 
@@ -57,9 +56,7 @@ export default WrappedComponent => {
 	return connect( ( state, { siteId } ) => {
 		return {
 			plugins: getPluginsWithUpdates( state, [ siteId ] ),
-			themes: config.isEnabled( 'activity-log-theme-update' )
-				? get( requestSiteAlerts( siteId ), 'data.updates.themes', emptyList )
-				: emptyList,
+			themes: get( requestSiteAlerts( siteId ), 'data.updates.themes', emptyList ),
 		};
 	} )( ToUpdate );
 };
