e3b4347e8478635b07ee79495ad6e1f380a71695
Jon Surrell <jon.surrell@automattic.com>
2018-06-08 20:52:49 +0200

    Simplify QueryUserPurchases with data-layer deduping

---
+++ client/components/data/query-user-purchases/index.js
@@ -0,0 +1,36 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { PureComponent } from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { fetchUserPurchases } from 'state/purchases/actions';
+
+class QueryUserPurchases extends PureComponent {
+	static propTypes = {
+		fetchUserPurchases: PropTypes.func.isRequired,
+		userId: PropTypes.number.isRequired,
+	};
+
+	componentDidMount() {
+		this.props.fetchUserPurchases( this.props.userId );
+	}
+
+	componentDidUpdate( prevProps ) {
+		if ( prevProps.userId !== this.props.userId ) {
+			this.props.fetchUserPurchases( this.props.userId );
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default connect( null, { fetchUserPurchases } )( QueryUserPurchases );
