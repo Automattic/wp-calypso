de7d364c7d40b2af44b6ed456b8c8cacd7ebf465
Jonathan Sadowski <sadowski@automattic.com>
2018-06-11 12:51:09 -0500

    EditorPublishDate: Fix issue that prevented cancelation of scheduling

--- client/post-editor/post-editor.jsx
+++ client/post-editor/post-editor.jsx
@@ -787,7 +787,7 @@ export class PostEditor extends React.Component {
 
 	setPostDate = date => {
 		const { siteId, postId } = this.props;
-		const dateValue = date ? date.format() : null;
+		const dateValue = date ? date.format() : false;
 
 		this.props.editPost( siteId, postId, { date: dateValue } );
 
