6dc3e5fb64ed5d09d91b1e64b73be72550986a57
Dominik Porada <dominik@porada.co>
2018-06-12 00:21:43 +0200

    Inject text styles into Sketch

--- packages/color-studio/foundations.js
+++ packages/color-studio/foundations.js
@@ -40,5 +40,74 @@ module.exports = {
       name: 'Celadon',
       value: '#1abc97'
     }
+  ],
+
+  textPresets: [
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 12,
+      lineHeight: 17,
+      letterSpacing: 0
+    },
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 13,
+      lineHeight: 19,
+      letterSpacing: 0
+    },
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 14,
+      lineHeight: 22,
+      letterSpacing: 0
+    },
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 16,
+      lineHeight: 26,
+      letterSpacing: 0
+    },
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 19,
+      lineHeight: 30,
+      letterSpacing: 0
+    },
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 23,
+      lineHeight: 35,
+      letterSpacing: -0.35
+    },
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 30,
+      lineHeight: 45,
+      letterSpacing: -0.5
+    },
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 36,
+      lineHeight: 52,
+      letterSpacing: -0.65
+    },
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 43,
+      lineHeight: 59,
+      letterSpacing: -0.85
+    },
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 51,
+      lineHeight: 68,
+      letterSpacing: -1.15
+    },
+    {
+      fontFamily: 'Noto Sans',
+      fontSize: 61,
+      lineHeight: 78,
+      letterSpacing: -1.5
+    }
   ]
 }
