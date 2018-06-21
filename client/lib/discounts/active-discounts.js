fdb7478f9d141e6778ede721a89de47f441ff0ca
Mark Ryall <mark@ryall.name>
2018-06-21 14:52:10 +1000

    Add June 2018 promotion

--- client/lib/discounts/active-discounts.js
+++ client/lib/discounts/active-discounts.js
@@ -10,13 +10,12 @@ import { TYPE_FREE } from 'lib/plans/constants';
  */
 export default [
 	{
-		name: 'mothers_day_2018',
-		startsAt: new Date( 2018, 4, 9, 0, 0, 0 ),
-		endsAt: new Date( 2018, 4, 13, 23, 59, 59 ),
-		nudgeText: '30% Off All Plans',
-		nudgeEndsTodayText: '30% Off (Ends Today)',
-		plansPageNoticeText:
-			'Enter coupon code “MOTHERSDAY30” during checkout to claim your 30% discount',
+		name: 'june_2018',
+		startsAt: new Date( 2018, 4, 26, 0, 0, 0 ),
+		endsAt: new Date( 2018, 5, 30, 23, 59, 59 ),
+		nudgeText: '20% Off All Plans',
+		nudgeEndsTodayText: '20% Off (Ends Today)',
+		plansPageNoticeText: 'Enter coupon code “JUNE20” during checkout to claim your 20% discount',
 		targetPlan: { type: TYPE_FREE },
 	},
 ];
