fafa22cd9b04725e95198b543eb98aa8f13f94ff
Jon Surrell <jon.surrell@automattic.com>
2018-06-21 09:48:28 +0200

    Improve comments

--- client/components/global-notices/index.jsx
+++ client/components/global-notices/index.jsx
@@ -87,12 +87,12 @@ export const NoticesList = createReactClass( {
 			);
 		}, this );
 
-		//This is an interim solution for displaying both notices from redux store
-		//and from the old component. When all notices are moved to redux store, this component
-		//needs to be updated.
+		// This is an interim solution for displaying both notices from redux store and from the old
+		// component. When all notices are moved to redux store, this component needs to be updated.
 		noticesList = noticesList.concat(
 			this.props.storeNotices.map(
-				// Collect `<Notice />` props in rest, extract other expected props
+				// We'll rest/spread props to notice so arbitrary props can be passed to `Notice`.
+				// Be sure to destructure any props that aren't for at `Notice`, e.g. `button`.
 				function( { button, href, noticeId, onClick, onDismissClick, ...notice } ) {
 					return (
 						<Notice
