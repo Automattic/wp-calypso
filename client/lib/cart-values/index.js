Automattic/wp-calypso

commit ac187dd32d38f573bec8683dcc184bf0cc611452

Add support for `?coupon=[code]` in Calypso. (#25433)
* Add support for `?coupon=[code]` at checkout.

client/lib/cart-values/index.js
@@ -3,7 +3,7 @@
 /**
  * External dependencies
  */
-
+import url from 'url';
 import { extend, isArray } from 'lodash';
 import update from 'immutability-helper';
 import i18n from 'i18n-calypso';
@@ -15,6 +15,53 @@ import config from 'config';
 import cartItems from './cart-items';
 import productsValues from 'lib/products-values';
 import { requestGeoLocation } from 'state/data-getters';
+
+/**
+ * Preprocesses cart for server.
+ *
+ * @param {Object} cart Cart object.
+ * @returns {Object} A new cart object.
+ */
+function preprocessCartForServer( {
+	coupon,
+	is_coupon_applied,
+	is_coupon_removed,
+	currency,
+	temporary,
+	extra,
+	products,
+} ) {
+	const needsUrlCoupon = ! (
+		coupon ||
+		is_coupon_applied ||
+		is_coupon_removed ||
+		typeof document === 'undefined'
+	);
+	const urlCoupon = needsUrlCoupon ? url.parse( document.URL, true ).query.coupon : '';
+
+	return Object.assign(
+		{
+			coupon,
+			is_coupon_applied,
+			is_coupon_removed,
+			currency,
+			temporary,
+			extra,
+			products: products.map( ( { product_id, meta, free_trial, volume, _extra } ) => ( {
+				product_id,
+				meta,
+				free_trial,
+				volume,
+				extra: _extra,
+			} ) ),
+		},
+		needsUrlCoupon &&
+			urlCoupon && {
+				coupon: urlCoupon,
+				is_coupon_applied: false,
+			}
+	);
+}
 
 /**
  * Create a new empty cart.
@@ -37,6 +84,17 @@ function applyCoupon( coupon ) {
 		return update( cart, {
 			coupon: { $set: coupon },
 			is_coupon_applied: { $set: false },
+			$unset: [ 'is_coupon_removed' ],
+		} );
+	};
+}
+
+function removeCoupon() {
+	return function( cart ) {
+		return update( cart, {
+			coupon: { $set: '' },
+			is_coupon_applied: { $set: false },
+			$merge: { is_coupon_removed: true },
 		} );
 	};
 }
@@ -237,9 +295,11 @@ function getLocationOrigin( l ) {
 
 export {
 	applyCoupon,
+	removeCoupon,
 	canRemoveFromCart,
 	cartItems,
 	emptyCart,
+	preprocessCartForServer,
 	fillInAllCartItemAttributes,
 	fillInSingleCartItemAttributes,
 	getNewMessages,
@@ -254,6 +314,7 @@ export {
 
 export default {
 	applyCoupon,
+	removeCoupon,
 	cartItems,
 	emptyCart,
 	isPaymentMethodEnabled,

client/lib/cart/store/index.js
@@ -15,7 +15,7 @@ import { recordEvents } from './cart-analytics';
 import productsListFactory from 'lib/products-list';
 const productsList = productsListFactory();
 import Dispatcher from 'dispatcher';
-import { applyCoupon, cartItems, fillInAllCartItemAttributes } from 'lib/cart-values';
+import { applyCoupon, removeCoupon, cartItems, fillInAllCartItemAttributes } from 'lib/cart-values';
 import wp from 'lib/wp';
 
 const wpcom = wp.undocumented();
@@ -122,6 +122,10 @@ CartStore.dispatchToken = Dispatcher.register( payload => {
 
 		case UpgradesActionTypes.CART_COUPON_APPLY:
 			update( applyCoupon( action.coupon ) );
+			break;
+
+		case UpgradesActionTypes.CART_COUPON_REMOVE:
+			update( removeCoupon() );
 			break;
 
 		case UpgradesActionTypes.CART_ITEM_REMOVE:

client/lib/upgrades/actions/index.js
@@ -13,6 +13,7 @@ export {
 	addItems,
 	addPrivacyToAllDomains,
 	applyCoupon,
+	removeCoupon,
 	closeCartPopup,
 	disableCart,
 	openCartPopup,

client/lib/cart/store/cart-synchronizer.js
@@ -3,8 +3,7 @@
 /**
  * External dependencies
  */
-
-import { assign, flowRight, pick } from 'lodash';
+import { assign, flowRight } from 'lodash';
 import i18n from 'i18n-calypso';
 import Dispatcher from 'dispatcher';
 import { action as upgradesActionTypes } from 'lib/upgrades/constants';
@@ -14,6 +13,7 @@ import debugFactory from 'debug';
  * Internal dependencies
  */
 import Emitter from 'lib/mixins/emitter';
+import { preprocessCartForServer } from 'lib/cart-values';
 
 /**
  * Internal dependencies
@@ -42,27 +42,6 @@ function castProductIDsToNumbers( cartItems ) {
 	return cartItems.map( function( item ) {
 		return assign( {}, item, { product_id: parseInt( item.product_id, 10 ) } );
 	} );
-}
-
-function preprocessCartForServer( cart ) {
-	let newCart;
-
-	newCart = pick(
-		cart,
-		'products',
-		'coupon',
-		'is_coupon_applied',
-		'currency',
-		'temporary',
-		'extra'
-	);
-
-	const newCartItems = cart.products.map( function( cartItem ) {
-		return pick( cartItem, 'product_id', 'meta', 'free_trial', 'volume', 'extra' );
-	} );
-	newCart = assign( {}, newCart, { products: newCartItems } );
-
-	return newCart;
 }
 
 function CartSynchronizer( cartKey, wpcom ) {

client/lib/signup/cart.js
@@ -3,7 +3,6 @@
 /**
  * External dependencies
  */
-
 import { forEach } from 'lodash';
 
 /**
@@ -12,7 +11,7 @@ import { forEach } from 'lodash';
 import wpcom from 'lib/wp';
 import productsListFactory from 'lib/products-list';
 const productsList = productsListFactory();
-import { cartItems, fillInAllCartItemAttributes } from 'lib/cart-values';
+import { cartItems, preprocessCartForServer, fillInAllCartItemAttributes } from 'lib/cart-values';
 
 function addProductsToCart( cart, newCartItems ) {
 	forEach( newCartItems, function( cartItem ) {
@@ -36,6 +35,7 @@ export default {
 		};
 
 		newCart = addProductsToCart( newCart, newCartItems );
+		newCart = preprocessCartForServer( newCart );
 
 		wpcom.undocumented().setCart( cartKey, newCart, function( postError ) {
 			callback( postError );
@@ -51,7 +51,8 @@ export default {
 				newCartItems = [ newCartItems ];
 			}
 
-			const newCart = addProductsToCart( data, newCartItems );
+			let newCart = addProductsToCart( data, newCartItems );
+			newCart = preprocessCartForServer( newCart );
 
 			wpcom.undocumented().setCart( cartKey, newCart, callback );
 		} );

client/lib/upgrades/actions/cart.js
@@ -116,3 +116,9 @@ export function applyCoupon( coupon ) {
 		coupon,
 	} );
 }
+
+export function removeCoupon() {
+	Dispatcher.handleViewAction( {
+		type: ActionTypes.CART_COUPON_REMOVE,
+	} );
+}

client/lib/upgrades/constants.js
@@ -2,6 +2,7 @@
 
 export const action = {
 	CART_COUPON_APPLY: 'CART_COUPON_APPLY',
+	CART_COUPON_REMOVE: 'CART_COUPON_REMOVE',
 	CART_DISABLE: 'CART_DISABLE',
 	CART_ITEMS_ADD: 'CART_ITEMS_ADD',
 	CART_ITEM_REMOVE: 'CART_ITEM_REMOVE',

client/my-sites/checkout/cart/cart-coupon.jsx
@@ -6,14 +6,15 @@
 
 import React from 'react';
 import { isEmpty, trim } from 'lodash';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import Button from 'components/button';
-import analytics from 'lib/analytics';
-import { applyCoupon } from 'lib/upgrades/actions';
+import { recordTracksEvent, recordGoogleEvent } from 'state/analytics/actions';
+import { applyCoupon, removeCoupon } from 'lib/upgrades/actions';
 
 export class CartCoupon extends React.Component {
 	static displayName = 'CartCoupon';
@@ -115,9 +116,9 @@ export class CartCoupon extends React.Component {
 		this.setState( { isCouponFormShowing: ! this.state.isCouponFormShowing } );
 
 		if ( this.state.isCouponFormShowing ) {
-			analytics.ga.recordEvent( 'Upgrades', 'Clicked Hide Coupon Code Link' );
+			this.props.recordGoogleEvent( 'Upgrades', 'Clicked Hide Coupon Code Link' );
 		} else {
-			analytics.ga.recordEvent( 'Upgrades', 'Clicked Show Coupon Code Link' );
+			this.props.recordGoogleEvent( 'Upgrades', 'Clicked Show Coupon Code Link' );
 		}
 	};
 
@@ -130,22 +131,29 @@ export class CartCoupon extends React.Component {
 				isCouponFormShowing: false,
 			},
 			() => {
-				this.applyCoupon( event );
+				this.removeCoupon( event );
 			}
 		);
 	};
 
 	applyCoupon = event => {
 		event.preventDefault();
+
 		if ( this.isSubmitting ) {
 			return;
 		}
 
-		analytics.tracks.recordEvent( 'calypso_checkout_coupon_submit', {
-			coupon_code: this.state.couponInputValue,
-		} );
+		this.props.applyCoupon( this.state.couponInputValue );
+	};
 
-		applyCoupon( this.state.couponInputValue );
+	removeCoupon = event => {
+		event.preventDefault();
+
+		if ( this.isSubmitting ) {
+			return;
+		}
+
+		this.props.removeCoupon();
 	};
 
 	handleCouponInputChange = event => {
@@ -156,4 +164,19 @@ export class CartCoupon extends React.Component {
 	};
 }
 
-export default localize( CartCoupon );
+const mapDispatchToProps = dispatch => ( {
+	recordGoogleEvent,
+	applyCoupon: coupon_code => {
+		dispatch( recordTracksEvent( 'calypso_checkout_coupon_submit', { coupon_code } ) );
+		applyCoupon( coupon_code );
+	},
+	removeCoupon: () => {
+		dispatch( recordTracksEvent( 'calypso_checkout_coupon_submit', { coupon_code: '' } ) );
+		removeCoupon();
+	},
+} );
+
+export default connect(
+	null,
+	mapDispatchToProps
+)( localize( CartCoupon ) );

commit b59d4136656358512076e7f636fab46e9d92b9fb

Shopping Cart: Fix not passing the correct extra for the product
Regression introduced in #25433

Destructuring was not done properly and a non-exisiting `_extra` property was
used.

client/lib/cart-values/index.js
@@ -47,13 +47,15 @@ function preprocessCartForServer( {
 			currency,
 			temporary,
 			extra,
-			products: products.map( ( { product_id, meta, free_trial, volume, _extra } ) => ( {
-				product_id,
-				meta,
-				free_trial,
-				volume,
-				extra: _extra,
-			} ) ),
+			products: products.map(
+				( { product_id, meta, free_trial, volume, extra: productExtra } ) => ( {
+					product_id,
+					meta,
+					free_trial,
+					volume,
+					extra: productExtra,
+				} )
+			),
 		},
 		needsUrlCoupon &&
 			urlCoupon && {