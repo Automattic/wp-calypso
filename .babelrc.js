8a28f4dc605c04b1a7fa9463ac4f5dced28b9216
Ben Lowery <ben.lowery@automattic.com>
2018-06-22 11:03:31 -0400

    Build: Remove last 2 versions
    See https://jamie.build/last-2-versions

--- .babelrc.js
+++ .babelrc.js
@@ -16,7 +16,7 @@ const config = {
 			{
 				modules,
 				targets: {
-					browsers: [ 'last 2 versions', 'Safari >= 10', 'iOS >= 10', 'ie >= 11' ],
+					browsers: [ '>0.25%', 'Safari >= 10', 'iOS >= 10', 'ie >= 11' ],
 				},
 				exclude: [ 'transform-classes', 'transform-template-literals' ], // transform-classes is added manually later.
 				useBuiltIns: 'entry',
