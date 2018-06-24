e51af32858d0df6ba9e325da6065ccbdf75dd3e3
jakejones1984 <40353716+jakejones1984@users.noreply.github.com>
2018-06-24 17:17:51 +0100

    Fixes DatePicker Nav Bar to account for whether prev/next buttons should be displayed
    Previously props which determine whether the previous and next nav buttons should be displayed were ignored. Minor modification to take account of this prior to rendering the buttons

--- client/components/date-picker/nav-bar.jsx
+++ client/components/date-picker/nav-bar.jsx
@@ -21,32 +21,39 @@ export const DatePickerNavBar = ( {
 	onNextClick,
 	className,
 	localeUtils,
+	showPreviousButton,
+	showNextButton,
 } ) => {
 	const classes = classNames( 'date-picker__nav-bar', {
 		[ className ]: !! className,
 	} );
 	return (
 		<div className={ classes }>
-			<button
-				className="date-picker__previous-month button"
-				type="button"
-				aria-label={ translate( 'Previous month ', {
-					comment: 'Aria label for date picker controls',
-				} ) }
-				onClick={ handleMonthClick( onPreviousClick ) }
-			>
-				{ localeUtils.formatMonthShort( previousMonth ) }
-			</button>
-			<button
-				className="date-picker__next-month button"
-				type="button"
-				aria-label={ translate( 'Next month ', {
-					comment: 'Aria label for date picker controls',
-				} ) }
-				onClick={ handleMonthClick( onNextClick ) }
-			>
-				{ localeUtils.formatMonthShort( nextMonth ) }
-			</button>
+			{ showPreviousButton && (
+				<button
+					className="date-picker__previous-month button"
+					type="button"
+					aria-label={ translate( 'Previous month ', {
+						comment: 'Aria label for date picker controls',
+					} ) }
+					onClick={ handleMonthClick( onPreviousClick ) }
+				>
+					{ localeUtils.formatMonthShort( previousMonth ) }
+				</button>
+			) }
+
+			{ showNextButton && (
+				<button
+					className="date-picker__next-month button"
+					type="button"
+					aria-label={ translate( 'Next month ', {
+						comment: 'Aria label for date picker controls',
+					} ) }
+					onClick={ handleMonthClick( onNextClick ) }
+				>
+					{ localeUtils.formatMonthShort( nextMonth ) }
+				</button>
+			) }
 		</div>
 	);
 };
