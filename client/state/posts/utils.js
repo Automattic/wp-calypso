bf3090550f379beb32d0e68eaa513c2ead713645
Jarda Snajdr <jsnajdr@gmail.com>
2018-06-25 15:44:11 +0200

    Post Editor: improve how post edits are merged after publish date reset
    This PR iterates on #25422 by improving the way how local date edits are
    removed after receiving updated post from server.
    
    Many post attributes already have nontrivial logic to compare incoming post
    update with local edits and decide if the edits are applied or not. For most
    attributes it's a simple `===` comparison, but for many it's not.
    
    for `POST_SAVE_SUCCESS` action. This PR moves it to the reducer for `POSTS_RECEIVE`
    action, where all the other comparisons are done.
    
    There should be no change in functionality.

--- client/state/posts/utils.js
+++ client/state/posts/utils.js
@@ -383,6 +383,13 @@ export function isAuthorEqual( localAuthorEdit, savedAuthor ) {
 }
 
 export function isDateEqual( localDateEdit, savedDate ) {
+	// if the local date edit is false, it means we are asking the server to reset
+	// the scheduled date to "now". In that case, we accept the date value returned
+	// by the server and consider the edit saved.
+	if ( localDateEdit === false ) {
+		return true;
+	}
+
 	return localDateEdit && moment( localDateEdit ).isSame( savedDate );
 }
 
