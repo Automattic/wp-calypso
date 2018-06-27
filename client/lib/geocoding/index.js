daff34a8a2852647adf1ac57cd4a76bb6a3cced1
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-06-27 16:28:52 -0700

    Add Google Maps API key to the editor

--- client/lib/geocoding/index.js
+++ client/lib/geocoding/index.js
@@ -7,9 +7,15 @@
 import request from 'superagent';
 
 /**
++ * Internal dependencies
++ */
+import config from 'config';
+
+/**
  * Module variables
  */
 const GOOGLE_MAPS_API_BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+const GOOGLE_MAPS_API_KEY = config( 'google_maps_and_places_api_key' );
 
 function queryGoogleMapsApi( queryParams ) {
 	return new Promise( ( resolve, reject ) => {
@@ -27,7 +33,7 @@ function queryGoogleMapsApi( queryParams ) {
 }
 
 export function geocode( address ) {
-	return queryGoogleMapsApi( { address } );
+	return queryGoogleMapsApi( { address, key: GOOGLE_MAPS_API_KEY } );
 }
 
 export function reverseGeocode( latitude, longitude ) {
