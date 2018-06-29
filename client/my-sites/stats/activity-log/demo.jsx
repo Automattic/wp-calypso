dad9b44e491b9022adfce4e6e91da9dc4b98c051
Mikael Korpela <mikael@ihminen.org>
2018-06-29 14:52:26 +0300

    Updates
    - simplify
    - add translations
    - style fixes

--- client/my-sites/stats/activity-log/demo.jsx
+++ client/my-sites/stats/activity-log/demo.jsx
@@ -5,7 +5,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import { defaults } from 'lodash';
 
 /**
  * Internal dependencies
@@ -15,58 +14,63 @@ import FeatureExample from 'components/feature-example';
 
 class ActivityLogDemo extends Component {
 	render() {
-		const demoContent = [
+		const { translate, siteId } = this.props;
+
+		const demoContents = [
 			{
-				activityId: 1,
-				activityDescription: [ 'My journey through Asia' ],
+				activityTs: new Date( '2018-06-29T18:38:00.000Z' ).getTime(),
+				activityDescription: [ translate( 'My journey through Asia' ) ],
 				activityIcon: 'posts',
 				activityStatus: 'success',
-				activityTitle: 'Post published',
+				activityTitle: translate( 'Post published' ),
 				actorName: 'Maria',
+				actorRole: 'administrator',
 			},
 			{
-				activityId: 2,
-				activityDescription: [ 'Lovely summer photos' ],
+				activityTs: new Date( '2018-06-28T00:20:00.000Z' ).getTime(),
+				activityDescription: [ translate( 'Lovely summer photos' ) ],
 				activityIcon: 'comment',
 				activityStatus: 'warning',
-				activityTitle: 'Comment waiting approval',
+				activityTitle: translate( 'Comment waiting approval' ),
 				actorName: 'Filippa',
 			},
 			{
-				activityId: 3,
-				activityDescription: [ 'My journey through Asia' ],
+				activityTs: new Date( '2018-06-27T15:10:00.000Z' ).getTime(),
+				activityDescription: [ translate( 'My journey through Asia' ) ],
 				activityIcon: 'posts',
-				activityTitle: 'Post draft modified',
+				activityTitle: translate( 'Post draft modified' ),
 				actorName: 'Vincent',
+				actorRole: 'administrator',
 			},
 		];
 
-		const demoItems = demoContent.map( content => {
-			return defaults( content, {
-				activityDate: new Date().toISOString(),
-				activityMeta: {},
-				activityStatus: null,
-				activityTs: Date.now(),
-				actorAvatarUrl: 'https://www.gravatar.com/avatar/0',
-				actorRemoteId: 0,
-				actorRole: 'administrator',
-				actorType: 'Person',
-			} );
+		const demoItems = demoContents.map( content => {
+			return Object.assign(
+				{
+					activityMeta: {},
+					activityStatus: null,
+					actorRole: '',
+					actorType: 'Person',
+				},
+				content
+			);
 		} );
 
 		return (
-			<FeatureExample>
-				{ demoItems.map( log => (
-					<ActivityLogItem
-						key={ log.activityId }
-						activity={ log }
-						disableRestore={ true }
-						disableBackup={ true }
-						hideRestore={ true }
-						siteId={ this.props.siteId }
-					/>
-				) ) }
-			</FeatureExample>
+			<div className="activity-log__demo">
+				<FeatureExample>
+					{ demoItems.map( log => (
+						<ActivityLogItem
+							key={ `activity-log-demo-${ log.activityTs }` }
+							activity={ log }
+							disableRestore={ true }
+							disableBackup={ true }
+							hideRestore={ true }
+							siteId={ siteId }
+						/>
+					) ) }
+				</FeatureExample>
+			</div>
 		);
 	}
 }
