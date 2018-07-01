ab7550ebd6ae56fc3650c2ac1c259c1c3f970695
Dennis Snell <dennis.snell@automattic.com>
2018-07-01 17:33:45 +0200

    Framework: `dispatchRequest` update (account recovery reset) (#25730)
    See #25121
    
    In this patch we're replacing the use of dispatchRequest()
    in the data layer handler to use the newer API exposed
    as dispatchRequestEx() This should have no change in
    actual effect or interaction.

--- client/state/data-layer/wpcom/account-recovery/reset/index.js
+++ client/state/data-layer/wpcom/account-recovery/reset/index.js
@@ -1,59 +1,43 @@
 /** @format */
-
 /**
  * Internal dependencies
  */
-
 import {
 	ACCOUNT_RECOVERY_RESET_PASSWORD_REQUEST,
 	ACCOUNT_RECOVERY_RESET_PASSWORD_REQUEST_SUCCESS,
 	ACCOUNT_RECOVERY_RESET_PASSWORD_REQUEST_ERROR,
 } from 'state/action-types';
 
-import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
 import { http } from 'state/data-layer/wpcom-http/actions';
 
-export const resetPassword = ( { dispatch }, action ) => {
-	const {
-		userData, // userData can be either { user } or { firstname, lastname, url }
-		method,
-		key,
-		password,
-	} = action;
-
-	dispatch(
-		http(
-			{
-				method: 'POST',
-				apiNamespace: 'wpcom/v2',
-				path: '/account-recovery/reset',
-				body: {
-					...userData,
-					method,
-					key,
-					password,
-				},
+export const fetch = action =>
+	http(
+		{
+			method: 'POST',
+			apiNamespace: 'wpcom/v2',
+			path: '/account-recovery/reset',
+			body: {
+				...action.userData,
+				method: action.method,
+				key: action.key,
+				password: action.password,
 			},
-			action
-		)
+		},
+		action
 	);
-};
 
-export const handleError = ( { dispatch }, action, rawError ) => {
-	dispatch( {
-		type: ACCOUNT_RECOVERY_RESET_PASSWORD_REQUEST_ERROR,
-		error: rawError.message,
-	} );
-};
+export const onError = ( action, rawError ) => ( {
+	type: ACCOUNT_RECOVERY_RESET_PASSWORD_REQUEST_ERROR,
+	error: rawError.message,
+} );
 
-export const handleSuccess = ( { dispatch } ) => {
-	dispatch( {
-		type: ACCOUNT_RECOVERY_RESET_PASSWORD_REQUEST_SUCCESS,
-	} );
-};
+export const onSuccess = () => ( {
+	type: ACCOUNT_RECOVERY_RESET_PASSWORD_REQUEST_SUCCESS,
+} );
 
 export default {
 	[ ACCOUNT_RECOVERY_RESET_PASSWORD_REQUEST ]: [
-		dispatchRequest( resetPassword, handleSuccess, handleError ),
+		dispatchRequestEx( { fetch, onSuccess, onError } ),
 	],
 };
