a63dbee5109ccd0322872812af8017ee73a50a89
Mário Valney <mario@vizir.com.br>
2018-07-02 22:02:17 -0300

    Fix: jetpack filter to domains/

--- client/my-sites/sites/index.jsx
+++ client/my-sites/sites/index.jsx
@@ -36,7 +36,7 @@ export const Sites = createReactClass( {
 
 		// Filter out sites with no upgrades on particular routes
 		if ( /^\/domains/.test( path ) || /^\/plans/.test( this.props.basePath ) ) {
-			return ! site.isJetpack || site.isSiteUpgradable;
+			return ! site.jetpack || site.isSiteUpgradable;
 		}
 
 		return site;
