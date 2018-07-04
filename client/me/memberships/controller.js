c37370bf637886a22a00022e67b35269afd7dac8
artpi <artur.piszek@gmail.com>
2018-07-04 17:13:14 +0200

    remove junk

--- client/me/memberships/controller.js
+++ client/me/memberships/controller.js
@@ -11,12 +11,3 @@ export function myMemberships( context, next ) {
 	context.primary = React.createElement( BillingHistoryComponent );
 	next();
 }
-
-// export function subscription( context, next ) {
-// 	const subscriptionId = context.params.subscriptionId;
-//
-// 	if ( subscriptionId ) {
-// 		context.primary = React.createElement( Receipt, { subscriptionId } );
-// 	}
-// 	next();
-// }
