3b9a1364cd27d666fb9de40180b73b8b4ca75201
artpi <artur.piszek@gmail.com>
2018-07-04 16:15:43 +0200

    Trim css

--- client/me/memberships/main.jsx
+++ client/me/memberships/main.jsx
@@ -21,39 +21,36 @@ import formatCurrency from 'lib/format-currency';
 import SectionHeader from 'components/section-header';
 
 const MembershipsHistory = ( { translate, subscriptions, moment } ) => (
-	<Main className="billing-history">
+	<Main className="memberships">
 		<DocumentHead title={ translate( 'My Memberships' ) } />
 		<PageViewTracker path="/me/purchases/memberships" title="Me > My Memberships" />
 		<MeSidebarNavigation />
 		<QueryMembershipsSubscriptions />
 		<PurchasesHeader section={ 'memberships' } />
 		<SectionHeader label={ translate( 'Active Membership plans' ) } />
-		<Card className="billing-history__receipts">
-			<table className="billing-history__transactions">
+		<Card className="memberships__receipts">
+			<table className="memberships__transactions">
 				<tbody>
 					{ subscriptions &&
 						subscriptions.map( subscription => {
 							const endDate = moment( subscription.end_date ).format( 'll' );
 
 							return (
-								<tr key={ subscription.ID } className="billing-history__transaction">
+								<tr key={ subscription.ID } className="memberships__transaction">
 									<td>{ endDate }</td>
-									<td className="billing-history__trans-app">
-										<div className="billing-history__trans-wrap">
-											<div className="billing-history__service-description">
-												<div className="billing-history__service-name">{ subscription.title }</div>
-												<div className="billing-history__transaction-links">
-													<a
-														className="billing-history__view-receipt"
-														href={ subscription.site_url }
-													>
+									<td className="memberships__trans-app">
+										<div className="memberships__trans-wrap">
+											<div className="memberships__service-description">
+												<div className="memberships__service-name">{ subscription.title }</div>
+												<div className="memberships__transaction-links">
+													<a className="memberships__view-receipt" href={ subscription.site_url }>
 														on { subscription.site_title } &raquo;
 													</a>
 												</div>
 											</div>
 										</div>
 									</td>
-									<td className="billing-history__amount">
+									<td className="memberships__amount">
 										{ formatCurrency( subscription.renewal_price, subscription.currency ) }
 									</td>
 								</tr>
