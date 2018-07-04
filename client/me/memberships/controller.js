6971da37e42da2bc661fabb3cc68574a3682c7f6
artpi <artur.piszek@gmail.com>
2018-07-04 16:02:19 +0200

    Remove junk

--- client/me/memberships/controller.js
+++ client/me/memberships/controller.js
@@ -6,18 +6,18 @@
 
 import React from 'react';
 import BillingHistoryComponent from './main';
-import Receipt from './receipt';
+// import Receipt from './receipt';
 
 export function myMemberships( context, next ) {
 	context.primary = React.createElement( BillingHistoryComponent );
 	next();
 }
 
-export function transaction( context, next ) {
-	const receiptId = context.params.receiptId;
-
-	if ( receiptId ) {
-		context.primary = React.createElement( Receipt, { transactionId: receiptId } );
-	}
-	next();
-}
+// export function transaction( context, next ) {
+// 	const receiptId = context.params.receiptId;
+//
+// 	if ( receiptId ) {
+// 		context.primary = React.createElement( Receipt, { transactionId: receiptId } );
+// 	}
+// 	next();
+// }
