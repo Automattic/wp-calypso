dac12b1be2b151755ff63ab0e59dcd038fd55f98
Artur Piszek <artur.piszek@gmail.com>
2018-07-05 16:33:40 +0200

    Memberships: Single subscription view + cancel option (#25884)
    This introduces a UI for viewing membership and maybe cancelling it.

--- client/me/memberships/controller.js
+++ client/me/memberships/controller.js
@@ -6,8 +6,18 @@
 
 import React from 'react';
 import BillingHistoryComponent from './main';
+import Subscription from './subscription';
 
 export function myMemberships( context, next ) {
 	context.primary = React.createElement( BillingHistoryComponent );
+	next();
+}
+
+export function subscription( context, next ) {
+	const subscriptionId = context.params.subscriptionId;
+
+	if ( subscriptionId ) {
+		context.primary = React.createElement( Subscription, { subscriptionId: subscriptionId } );
+	}
 	next();
 }
