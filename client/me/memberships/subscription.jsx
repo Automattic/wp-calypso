b536064275e9be468e1d16325342febb75a7fe07
artpi <artur.piszek@gmail.com>
2018-07-05 15:25:54 +0200

    make stateful component

--- client/me/memberships/subscription.jsx
+++ client/me/memberships/subscription.jsx
@@ -20,60 +20,74 @@ import formatCurrency from 'lib/format-currency';
 import HeaderCake from 'components/header-cake';
 import { purchasesRoot } from '../purchases/paths';
 import Site from 'blocks/site';
+import Gridicon from 'gridicons';
+import CompactCard from 'components/card/compact';
 
-const Subscription = ( { translate, subscription, moment } ) => (
-	<Main className="memberships__subscription">
-		<DocumentHead title={ translate( 'My Memberships' ) } />
-		<MeSidebarNavigation />
-		<QueryMembershipsSubscriptions />
-		<PurchasesHeader section={ 'memberships' } />
-		{ subscription && (
-			<div>
-				<HeaderCake backHref={ purchasesRoot + '/memberships' }>{ subscription.title }</HeaderCake>
-				<Card className="memberships__subscription-meta">
-					<Site siteId={ subscription.site_id } href={ subscription.site_url } />
-					<div className="memberships__subscription-title">{ subscription.title }</div>
-					<Fragment>
-						<ul className="memberships__subscription-inner-meta">
-							<li>
-								<em className="memberships__subscription-inner-detail-label">
-									{ translate( 'Price' ) }
-								</em>
-								<span className="memberships__subscription-inner-detail">
-									{ formatCurrency( subscription.renewal_price, subscription.currency ) }
-								</span>
-							</li>
-							<li>
-								<em className="memberships__subscription-inner-detail-label">
-									{ translate( 'Renew interval' ) }
-								</em>
-								<span className="memberships__subscription-inner-detail">
-									{ subscription.renew_interval }
-								</span>
-							</li>
-							<li>
-								<em className="memberships__subscription-inner-detail-label">
-									{ translate( 'Subscribed on' ) }
-								</em>
-								<span className="memberships__subscription-inner-detail">
-									{ moment( subscription.start_date ).format( 'll' ) }
-								</span>
-							</li>
-							<li>
-								<em className="memberships__subscription-inner-detail-label">
-									{ translate( 'Renews on' ) }
-								</em>
-								<span className="memberships__subscription-inner-detail">
-									{ moment( subscription.end_date ).format( 'll' ) }
-								</span>
-							</li>
-						</ul>
-					</Fragment>
-				</Card>
-			</div>
-		) }
-	</Main>
-);
+class Subscription extends React.Component {
+	render() {
+		const { translate, subscription, moment } = this.props;
+		return (
+			<Main className="memberships__subscription">
+				<DocumentHead title={ translate( 'My Memberships' ) } />
+				<MeSidebarNavigation />
+				<QueryMembershipsSubscriptions />
+				<PurchasesHeader section={ 'memberships' } />
+				{ subscription && (
+					<div>
+						<HeaderCake backHref={ purchasesRoot + '/memberships' }>
+							{ subscription.title }
+						</HeaderCake>
+						<Card className="memberships__subscription-meta">
+							<Site siteId={ subscription.site_id } href={ subscription.site_url } />
+							<div className="memberships__subscription-title">{ subscription.title }</div>
+							<Fragment>
+								<ul className="memberships__subscription-inner-meta">
+									<li>
+										<em className="memberships__subscription-inner-detail-label">
+											{ translate( 'Price' ) }
+										</em>
+										<span className="memberships__subscription-inner-detail">
+											{ formatCurrency( subscription.renewal_price, subscription.currency ) }
+										</span>
+									</li>
+									<li>
+										<em className="memberships__subscription-inner-detail-label">
+											{ translate( 'Renew interval' ) }
+										</em>
+										<span className="memberships__subscription-inner-detail">
+											{ subscription.renew_interval }
+										</span>
+									</li>
+									<li>
+										<em className="memberships__subscription-inner-detail-label">
+											{ translate( 'Subscribed on' ) }
+										</em>
+										<span className="memberships__subscription-inner-detail">
+											{ moment( subscription.start_date ).format( 'll' ) }
+										</span>
+									</li>
+									<li>
+										<em className="memberships__subscription-inner-detail-label">
+											{ translate( 'Renews on' ) }
+										</em>
+										<span className="memberships__subscription-inner-detail">
+											{ moment( subscription.end_date ).format( 'll' ) }
+										</span>
+									</li>
+								</ul>
+							</Fragment>
+						</Card>
+						<CompactCard tagName="button" className="memberships__subscription-remove">
+							<Gridicon icon="trash" />
+							{ translate( 'Stop %s subscription.', { args: subscription.title } ) }
+						</CompactCard>
+					</div>
+				) }
+			</Main>
+		);
+	}
+}
+
 const getSubscription = ( state, subscriptionId ) =>
 	get( state, 'memberships.subscriptions.items', [] )
 		.filter( sub => sub.ID === subscriptionId )
