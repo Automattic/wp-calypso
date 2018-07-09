e2bbfd18e07dd5c8aff74d0f446a82700d3eaa55
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-07-09 09:38:43 -0700

    Remove the debug statements

--- client/lib/geocoding/index.js
+++ client/lib/geocoding/index.js
@@ -20,15 +20,12 @@ const GOOGLE_MAPS_API_KEY = config( 'google_maps_and_places_api_key' );
 let geocoder;
 
 function queryGoogleMapsApi( queryParams ) {
-	console.log( 'called queryGoogleMapsApi' );
 	return new Promise( ( resolve, reject ) => {
 		if ( geocoder ) {
-			console.log( 'Howdy!' );
 			return queryGeocoder( queryParams, resolve, reject );
 		}
 
 		loadScript( GOOGLE_MAPS_API_BASE_URL + '?key=' + GOOGLE_MAPS_API_KEY, function() {
-			console.log( 'load script' );
 			// eslint-disable-next-line no-undef
 			geocoder = new google.maps.Geocoder();
 			return queryGeocoder( queryParams, resolve, reject );
@@ -37,13 +34,10 @@ function queryGoogleMapsApi( queryParams ) {
 }
 
 function queryGeocoder( queryParams, resolve, reject ) {
-	console.log( 'queryGeocoder', queryParams );
 	geocoder.geocode( queryParams, function( results, status ) {
 		if ( status === 'OK' ) {
-			console.log( 'WORKED', results, status );
 			return resolve( results );
 		}
-		console.log( 'FAILED', results, status );
 		reject( status );
 	} );
 }
