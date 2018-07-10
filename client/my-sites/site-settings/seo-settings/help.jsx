a9044205fc498769cf011308581727eaf1df6fe9
johnmaeda <jm@creativeleadership.com>
2018-07-09 21:51:56 -0400

    Update SEO help link to the new master link

--- client/my-sites/site-settings/seo-settings/help.jsx
+++ client/my-sites/site-settings/seo-settings/help.jsx
@@ -29,7 +29,7 @@ const SeoSettingsHelpCard = ( {
 } ) => {
 	const seoHelpLink = siteIsJetpack
 		? 'https://jetpack.com/support/seo-tools/'
-		: 'https://en.blog.wordpress.com/2013/03/22/seo-on-wordpress-com/';
+		: 'https://wpbizseo.wordpress.com/';
 
 	return (
 		<div id="seo">
