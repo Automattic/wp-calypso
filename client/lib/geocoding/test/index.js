8fea5c42005733733d13a3790d6b8d86852b9ae9
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-07-09 23:06:19 -0700

    Make the tests pass

--- client/lib/geocoding/test/index.js
+++ client/lib/geocoding/test/index.js
@@ -8,8 +8,8 @@ import { expect } from 'chai';
  * Internal dependencies
  */
 import { geocode, reverseGeocode } from '../';
-import { useNock } from 'test/helpers/use-nock';
 
+jest.mock( 'lib/load-script', () => require( './mocks/load-script' ) );
 /**
  * Module variables
  */
@@ -21,14 +21,6 @@ const TEST_LONGITUDE = '-87.629798';
 
 describe( 'geocoding', () => {
 	describe( 'when converting a search string to location results', () => {
-		useNock( nock => {
-			nock( 'https://maps.googleapis.com' )
-				.persist()
-				.get( '/maps/api/geocode/json' )
-				.query( { address: TEST_ADDRESS } )
-				.reply( 200, { results: [ 1, 2, 3 ], status: 'OK' } );
-		} );
-
 		describe( '#geocode()', () => {
 			test( 'should return a promise', () => {
 				expect( geocode( TEST_ADDRESS ) ).to.be.an.instanceof( Promise );
@@ -46,14 +38,6 @@ describe( 'geocoding', () => {
 	} );
 
 	describe( 'when converting from coordinates to address labels', () => {
-		useNock( nock => {
-			nock( 'https://maps.googleapis.com', { encodedQueryParams: true } )
-				.persist()
-				.get( '/maps/api/geocode/json' )
-				.query( { latlng: TEST_LATITUDE + '%2C' + TEST_LONGITUDE } )
-				.reply( 200, { results: [ 1, 2, 3 ], status: 'OK' } );
-		} );
-
 		describe( '#reverseGeocode()', () => {
 			test( 'should return a promise', () => {
 				expect( reverseGeocode( TEST_LATITUDE, TEST_LONGITUDE ) ).to.be.an.instanceof( Promise );
---
+++ client/lib/geocoding/test/mocks/load-script/index.js
@@ -0,0 +1,30 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import { defer } from 'lodash';
+
+function fakeLoader( url, callback ) {
+	// eslint-disable-next-line no-undef
+	google = {
+		maps: {
+			Geocoder: function() {
+				return {
+					geocode: function( queryParams, result_callback ) {
+						defer( result_callback, [ 1, 2, 3 ], 'OK' ); // fake the results
+					},
+				};
+			},
+		},
+	};
+	fakeLoader.urlsLoaded.push( url );
+	if ( callback ) {
+		defer( callback );
+	}
+}
+
+fakeLoader.urlsLoaded = [];
+
+export default { loadScript: fakeLoader };
