b31a79c6735bb153de00778c28ed951f20871b9a
Jarda Snajdr <jsnajdr@gmail.com>
2018-07-10 17:25:24 +0200

    Gutenberg: experimental adds needed packages

--- client/gutenberg/editor/main.jsx
+++ client/gutenberg/editor/main.jsx
@@ -1,12 +1,43 @@
 /** @format */
+
+/** @format */
 /**
  * External dependencies
  */
 import React, { Component } from 'react';
 
+/**
+ * Internal dependencies
+ */
+import '@wordpress/data';
+import '@wordpress/core-data';
+import { registerCoreBlocks } from '@wordpress/core-blocks';
+import { EditorProvider, WritingFlow, ObserveTyping, BlockList } from '@wordpress/editor';
+
+const editorSettings = {};
+const wpApiSettings = {
+	schema: {
+		routes: {},
+	},
+};
+const post = {
+	type: 'post',
+	content: {},
+};
+
+registerCoreBlocks();
+
 class GutenbergEditor extends Component {
 	render() {
-		return <div className="editor">Hello, Gutenberg!</div>;
+		return (
+			<EditorProvider settings={ editorSettings } wpApiSettings={ wpApiSettings } post={ post }>
+				<WritingFlow>
+					<ObserveTyping>
+						<BlockList />
+					</ObserveTyping>
+				</WritingFlow>
+			</EditorProvider>
+		);
 	}
 }
 
