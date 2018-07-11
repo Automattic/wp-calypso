06eee62db8ecb406fea0ff5785e19f13e53f76f2
Daniel Rey López <daniel.rey.lopez@a8c.com>
2018-07-11 16:03:06 +0100

    Update the ITN field RegExp to account for "NOEEI 30.XX(X)"-type exemptions.
    See https://github.com/Automattic/woocommerce-connect-server/pull/1118 for more context.

--- client/extensions/woocommerce/woocommerce-services/state/shipping-label/selectors.js
+++ client/extensions/woocommerce/woocommerce-services/state/shipping-label/selectors.js
@@ -341,7 +341,7 @@ export const getCustomsErrors = (
 			} );
 
 			if ( pckg.itn ) {
-				if ( ! /^AES X\d{14}$/.test( pckg.itn ) ) {
+				if ( ! /^(AES X\d{14})|(NOEEI 30\.\d{2}(\([a-z]\))?)$/.test( pckg.itn ) ) {
 					errors.itn = translate( 'Invalid format' );
 				}
 			} else if ( 'CA' !== destinationCountryCode ) {
