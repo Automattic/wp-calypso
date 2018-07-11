8b1f5b19c1f85c5715bd9c29146b282c2ed255ac
Jon Surrell <jon.surrell@automattic.com>
2018-07-11 14:16:46 +0200

    Export data as desired, don't build on module init

--- client/my-sites/checklist/jetpack-checklist.js
+++ client/my-sites/checklist/jetpack-checklist.js
@@ -4,69 +4,64 @@
  */
 import { translate } from 'i18n-calypso';
 
-const unorderedTasks = {
-	jetpack_brute_force: {
+export const tasks = [
+	{
+		id: 'jetpack_brute_force',
 		completedTitle: translate(
 			"We've automatically protected you from brute force login attacks."
 		),
 		completed: true,
 	},
-	jetpack_spam_filtering: {
+	{
+		id: 'jetpack_spam_filtering',
 		completedTitle: translate( "We've automatically turned on spam filtering." ),
 		completed: true,
 	},
-	jetpack_backups: {
+	{
+		id: 'jetpack_backups',
 		title: translate( 'Backups & Scanning' ),
 		description: translate(
 			"Connect your site's server to Jetpack to perform backups, rewinds, and security scans."
 		),
 		completed: true,
 		completedTitle: translate( 'You turned on backups and scanning.' ),
-		completedButtonText: 'Change',
+		completedButtonText: translate( 'Change' ),
 		duration: translate( '2 min' ),
 		url: '/stats/activity/$siteSlug',
 	},
-	jetpack_monitor: {
+	{
+		id: 'jetpack_monitor',
 		title: translate( 'Jetpack Monitor' ),
 		description: translate(
 			"Monitor your site's uptime and alert you the moment downtime is detected with instant notifications."
 		),
 		completedTitle: translate( 'You turned on Jetpack Monitor.' ),
-		completedButtonText: 'Change',
+		completedButtonText: translate( 'Change' ),
 		duration: translate( '3 min' ),
 		tour: 'jetpackMonitoring',
 		url: '/settings/security/$siteSlug',
 	},
-	jetpack_plugin_updates: {
+	{
+		id: 'jetpack_plugin_updates',
 		title: translate( 'Automatic Plugin Updates' ),
 		description: translate(
 			'Choose which WordPress plugins you want to keep automatically updated.'
 		),
 		completedTitle: translate( 'You turned on automatic plugin updates.' ),
-		completedButtonText: 'Change',
+		completedButtonText: translate( 'Change' ),
 		duration: translate( '3 min' ),
 		url: '/plugins/manage/$siteSlug',
 	},
-	jetpack_sign_in: {
+	{
+		id: 'jetpack_sign_in',
 		title: translate( 'WordPress.com sign in' ),
 		description: translate(
 			'Manage your log in preferences and two-factor authentication settings.'
 		),
 		completedTitle: translate( 'You completed your sign in preferences.' ),
-		completedButtonText: 'Change',
+		completedButtonText: translate( 'Change' ),
 		duration: translate( '3 min' ),
 		tour: 'jetpackSignIn',
 		url: '/settings/security/$siteSlug',
 	},
-};
-
-const sequence = [
-	'jetpack_brute_force',
-	'jetpack_spam_filtering',
-	'jetpack_backups',
-	'jetpack_monitor',
-	'jetpack_plugin_updates',
-	'jetpack_sign_in',
 ];
-
-export const tasks = sequence.map( id => ( { id, ...unorderedTasks[ id ] } ) );
