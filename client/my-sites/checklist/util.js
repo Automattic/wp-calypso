f91a6c35430f74d660eef9427568ab862d4485d4
Jon Surrell <jon.surrell@automattic.com>
2018-07-11 13:52:03 +0200

    No need to make all new objects

--- client/my-sites/checklist/util.js
+++ client/my-sites/checklist/util.js
@@ -1,17 +1,26 @@
 /** @format */
 
 /**
- * Merge object into array
+ * Update an array of objects with an id property with an object of objects keyed by id.
  *
- * Given an array of objects and an object of objects, merge the object's values
- * into array items, using the array's objects' `id` values to determine which
- * key to use for merging.
+ * Given an array of objects with an id property, update and add properties from the provided object
+ * keyed by id.
  *
- * @param   {Array}   arr  An array of objects, each of which must contain an `id` key
- * @param   {Object}  obj  An object
- * @returns {Array}        The merged array
+ * @example
+ *   mergeObjectIntoArrayById(
+ *     [ { id: 'a', prop: 'stale', keep: 'keep' } ],
+ *     {
+ *       a: { prop: 'fresh', add: 'new' }
+ *     }
+ *   )
+ *   // results in…
+ *   [ { id: 'a', prop: 'fresh', keep: 'keep', add: 'new' } ],
+ *
+ * @param  {Array<Object>}   arr  An array of objects, each of which must contain an `id` property
+ * @param  {Object}          obj  An object whose keys match ids and values are objects or properties to update
+ * @return {Array<Object>}        A new array with updated objects
  *
  */
 export function mergeObjectIntoArrayById( arr, obj ) {
-	return arr.map( item => ( { ...item, ...obj[ item.id ] } ) );
+	return arr.map( item => ( obj[ item.id ] ? { ...item, ...obj[ item.id ] } : item ) );
 }
