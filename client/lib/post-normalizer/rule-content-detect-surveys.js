1f8e46fa7c7642a1adf0f2d258f720140d1d88e1
bluefuton <chris@bluefuton.com>
2018-07-12 14:41:05 +1200

    Add rule

--- client/lib/post-normalizer/rule-content-detect-surveys.js
+++ client/lib/post-normalizer/rule-content-detect-surveys.js
@@ -3,18 +3,43 @@
 /**
  * External dependencies
  */
-// import { forEach } from 'lodash';
-// import i18n from 'i18n-calypso';
-
-/**
- * Internal dependencies
- */
-//import { domForHtml } from './utils';
+import { forEach } from 'lodash';
+import i18n from 'i18n-calypso';
 
 export default function detectSurveys( post, dom ) {
 	if ( ! dom ) {
 		throw new Error( 'this transform must be used as part of withContentDOM' );
 	}
+
+	const surveys = dom.querySelectorAll( '.pd-embed' );
+
+	if ( ! surveys ) {
+		return post;
+	}
+
+	forEach( surveys, survey => {
+		// Get survey details
+		const { domain: surveyDomain, id: surveySlug } = JSON.parse(
+			survey.getAttribute( 'data-settings' )
+		);
+
+		if ( ! surveyDomain || ! surveySlug ) {
+			return;
+		}
+
+		// Construct a survey link
+		const p = document.createElement( 'p' );
+		p.innerHTML =
+			'<a target="_blank" rel="external noopener noreferrer" href="https://' +
+			surveyDomain +
+			surveySlug +
+			'">' +
+			i18n.translate( 'Take our survey' ) +
+			'</a>';
+
+		// Replace the .pd-embed div with the new paragraph
+		survey.parentNode.replaceChild( p, survey );
+	} );
 
 	return post;
 }
