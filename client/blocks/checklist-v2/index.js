dda355637b8b571d495e3dd6f55f28315cde2e70
Jon Surrell <jon.surrell@automattic.com>
2018-07-13 01:07:33 +0200

    Add header and collapsing

--- client/blocks/checklist-v2/index.js
+++ client/blocks/checklist-v2/index.js
@@ -2,20 +2,37 @@
 /**
  * External dependencies
  */
-import React, { PureComponent } from 'react';
+import React, { Children, PureComponent } from 'react';
 import classNames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import ChecklistHeader from 'blocks/checklist/checklist-header';
 
 export default class Checklist extends PureComponent {
 	state = { hideCompleted: false };
 
+	toggleCompleted = () =>
+		this.setState( ( { hideCompleted } ) => ( { hideCompleted: ! hideCompleted } ) );
+
 	render() {
+		const { children } = this.props;
+
 		return (
 			<div
 				className={ classNames( 'checklist-v2', 'checklist', {
 					'is-expanded': ! this.state.hideCompleted,
+					'hide-completed': this.state.hideCompleted,
 				} ) }
 			>
-				<div className="checklist-v2__items">{ this.props.children }</div>
+				<ChecklistHeader
+					total={ Children.count( children ) }
+					completed={ /* @TODO get count */ 1 }
+					hideCompleted={ this.state.hideCompleted }
+					onClick={ this.toggleCompleted }
+				/>
+				<div className="checklist-v2__items">{ children }</div>
 			</div>
 		);
 	}
