2ff06d97b54a74df08cb9410200cc6077c54220d
Miguel Torres <miguelmariatorresrojas@gmail.com>
2018-07-14 17:04:03 +0200

    Devdocs: tests covering the new section for Gutenberg blocks

--- client/devdocs/design/gutenberg.jsx
+++ client/devdocs/design/gutenberg.jsx
@@ -14,12 +14,12 @@ import ReadmeViewer from 'components/readme-viewer';
 
 export default class Gutenberg extends React.Component {
 	render() {
-		const className = classnames('devdocs', 'devdocs__gutenberg');
+		const className = classnames( 'devdocs', 'devdocs__gutenberg' );
 
 		return (
 			<Main className={ className }>
 				<DocumentHead title="Gutenberg Blocks"/>
-				<ReadmeViewer readmeFilePath="/client/devdocs/gutenberg/README.md" />
+				<ReadmeViewer readmeFilePath="/client/devdocs/gutenberg/README.md"/>
 			</Main>
 		);
 	}
---
+++ client/devdocs/design/test/gutenberg.js
@@ -0,0 +1,45 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import {shallow} from 'enzyme';
+
+/**
+ * Internal dependencies
+ */
+import DocumentHead from 'components/data/document-head';
+import Gutenberg from '../gutenberg';
+import Main from 'components/main';
+import ReadmeViewer from 'components/readme-viewer';
+
+describe('Gutenberg', () => {
+	test('should render', () => {
+		const wrapper = shallow(<Gutenberg/>);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	test('should have a main component', () => {
+		const wrapper = shallow(<Gutenberg/>);
+		const main = wrapper.find(Main);
+
+		expect(main).toHaveLength(1);
+		expect(main.hasClass('devdocs devdocs__gutenberg')).toBe(true);
+	});
+
+	test('should have a document head component', () => {
+		const wrapper = shallow(<Gutenberg/>);
+		const documentHead = wrapper.find(DocumentHead);
+
+		expect(documentHead).toHaveLength(1);
+		expect(documentHead.props().title).toBe('Gutenberg Blocks');
+	});
+
+	test('should have a readme viewer component', () => {
+		const wrapper = shallow(<Gutenberg/>);
+		const readmeViewer = wrapper.find(ReadmeViewer);
+
+		expect(readmeViewer).toHaveLength(1);
+		expect(readmeViewer.props().readmeFilePath).toBe('/client/devdocs/gutenberg/README.md');
+	});
+});
