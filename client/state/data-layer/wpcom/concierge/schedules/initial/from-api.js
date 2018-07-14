fda7f2a1e0378b93197d5cc1f46bc574242bf178
Dan Gates <dan.gates@pentangle.co.uk>
2018-07-14 14:45:13 +0100

    Rename transformation method to better describe what it does.

--- client/state/data-layer/wpcom/concierge/schedules/initial/from-api.js
+++ client/state/data-layer/wpcom/concierge/schedules/initial/from-api.js
@@ -7,14 +7,14 @@ import makeJsonSchemaParser from 'lib/make-json-schema-parser';
 import responseSchema from './schema';
 import { transform as appointmentTransformer } from 'state/data-layer/wpcom/concierge/schedules/appointments/detail/from-api';
 
-export const convertToDate = timestampInSeconds => timestampInSeconds * 1000;
+export const convertToMilliseconds = timestampInSeconds => timestampInSeconds * 1000;
 
 export const transform = response => {
 	const nextAppointment =
 		response.next_appointment === null ? null : appointmentTransformer( response.next_appointment );
 
 	return {
-		availableTimes: response.available_times.map( convertToDate ),
+		availableTimes: response.available_times.map( convertToMilliseconds ),
 		nextAppointment: nextAppointment,
 	};
 };
