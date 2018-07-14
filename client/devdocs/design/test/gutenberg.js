9bd2254c72bb379cb0a9095ce8e84f04dcdfab8e
Miguel Torres <miguelmariatorresrojas@gmail.com>
2018-07-14 17:08:14 +0200

    Applying guidelines to tests covering the Gutenberg blocks in DevDocs

--- client/devdocs/design/test/gutenberg.js
+++ client/devdocs/design/test/gutenberg.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 
 /**
  * Internal dependencies
@@ -12,34 +12,34 @@ import Gutenberg from '../gutenberg';
 import Main from 'components/main';
 import ReadmeViewer from 'components/readme-viewer';
 
-describe('Gutenberg', () => {
-	test('should render', () => {
-		const wrapper = shallow(<Gutenberg/>);
+describe( 'Gutenberg', () => {
+	test( 'should render', () => {
+		const wrapper = shallow( <Gutenberg/> );
 
-		expect(wrapper).toMatchSnapshot();
-	});
+		expect( wrapper ).toMatchSnapshot();
+	} );
 
-	test('should have a main component', () => {
-		const wrapper = shallow(<Gutenberg/>);
-		const main = wrapper.find(Main);
+	test( 'should have a main component', () => {
+		const wrapper = shallow( <Gutenberg/> );
+		const main = wrapper.find( Main );
 
-		expect(main).toHaveLength(1);
-		expect(main.hasClass('devdocs devdocs__gutenberg')).toBe(true);
-	});
+		expect( main ).toHaveLength( 1 );
+		expect( main.hasClass( 'devdocs devdocs__gutenberg' ) ).toBe( true );
+	} );
 
-	test('should have a document head component', () => {
-		const wrapper = shallow(<Gutenberg/>);
-		const documentHead = wrapper.find(DocumentHead);
+	test( 'should have a document head component', () => {
+		const wrapper = shallow( <Gutenberg/> );
+		const documentHead = wrapper.find( DocumentHead );
 
-		expect(documentHead).toHaveLength(1);
-		expect(documentHead.props().title).toBe('Gutenberg Blocks');
-	});
+		expect( documentHead ).toHaveLength( 1 );
+		expect( documentHead.props().title ).toBe( 'Gutenberg Blocks' );
+	} );
 
-	test('should have a readme viewer component', () => {
-		const wrapper = shallow(<Gutenberg/>);
-		const readmeViewer = wrapper.find(ReadmeViewer);
+	test( 'should have a readme viewer component', () => {
+		const wrapper = shallow( <Gutenberg/> );
+		const readmeViewer = wrapper.find( ReadmeViewer );
 
-		expect(readmeViewer).toHaveLength(1);
-		expect(readmeViewer.props().readmeFilePath).toBe('/client/devdocs/gutenberg/README.md');
-	});
-});
+		expect( readmeViewer ).toHaveLength( 1 );
+		expect( readmeViewer.props().readmeFilePath ).toBe( '/client/devdocs/gutenberg/README.md' );
+	} );
+} );
