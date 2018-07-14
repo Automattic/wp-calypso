8b4f8981caee776c0f6a1793812621f54884969c
Dan Gates <dan.gates@pentangle.co.uk>
2018-07-14 14:45:13 +0100

    Remove the usage of the available_times endpoint.

--- client/state/data-layer/wpcom/concierge/schedules/index.js
+++ client/state/data-layer/wpcom/concierge/schedules/index.js
@@ -4,8 +4,7 @@
  * Internal dependencies
  */
 import { mergeHandlers } from 'state/action-watchers/utils';
-import availableTimes from './available-times';
 import appointments from './appointments';
 import initial from './initial';
 
-export default mergeHandlers( availableTimes, appointments, initial );
+export default mergeHandlers( appointments, initial );
