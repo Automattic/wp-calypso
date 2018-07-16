7445da02bd82691d778e6a6c69ff77c6028104d7
Jon Surrell <jon.surrell@automattic.com>
2018-07-16 11:51:08 +0200

    Checklists: Redirect Jetpack checklists to "My Plan" (#26042)
    * Redirect Jetpack checklists to /plans/my-plan/:site

--- client/my-sites/checklist/main.jsx
+++ client/my-sites/checklist/main.jsx
@@ -1,12 +1,12 @@
 /** @format */
-
 /**
  * External dependencies
  */
+import page from 'page';
 import React, { Fragment, PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { find, some } from 'lodash';
 import { localize } from 'i18n-calypso';
-import { find } from 'lodash';
 
 /**
  * Internal dependencies
@@ -28,6 +28,39 @@ import { isJetpackSite, getSiteSlug } from 'state/sites/selectors';
 import { recordTracksEvent } from 'state/analytics/actions';
 
 class ChecklistMain extends PureComponent {
+	componentDidMount() {
+		this.maybeRedirectJetpack();
+	}
+
+	componentDidUpdate( prevProps ) {
+		this.maybeRedirectJetpack( prevProps );
+	}
+
+	/**
+	 * Redirect Jetpack checklists to /plans/my-plan/:siteSlug
+	 *
+	 * Guard for relevant props updated and correct conditions
+	 *
+	 * @param {Object}   prevProps           Optional. Previous props from componentDidUpdate.
+	 * @param {?boolean} prevProps.isAtomic  Previous isAtomic
+	 * @param {?boolean} prevProps.isJetpack Previous isJetpack
+	 * @param {?string}  prevProps.siteSlug  Previous siteSlug
+	 */
+	maybeRedirectJetpack( prevProps = {} ) {
+		if (
+			this.props.siteSlug &&
+			false === this.props.isAtomic &&
+			this.props.isJetpack &&
+			some( [
+				this.props.siteSlug !== prevProps.siteSlug,
+				this.props.isAtomic !== prevProps.isAtomic,
+				this.props.isJetpack !== prevProps.isJetpack,
+			] )
+		) {
+			page.redirect( `/plans/my-plan/${ this.props.siteSlug }` );
+		}
+	}
+
 	getHeaderTitle( displayMode ) {
 		const { translate } = this.props;
 
@@ -161,6 +194,8 @@ const mapStateToProps = state => {
 	const isJetpack = isJetpackSite( state, siteId );
 	return {
 		checklistAvailable: ! isAtomic && ( config.isEnabled( 'jetpack/checklist' ) || ! isJetpack ),
+		isAtomic,
+		isJetpack,
 		siteId,
 		siteSlug,
 		user: getCurrentUser( state ),
