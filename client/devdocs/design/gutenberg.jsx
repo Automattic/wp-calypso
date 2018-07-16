684a3dca8ddbb31fb399ff8dec4c415c128faf36
Miguel Torres <miguelmariatorresrojas@gmail.com>
2018-07-16 17:29:41 +0200

    Proof of concept: render a Gutenberg component in Calypso DevDocs

--- client/devdocs/design/gutenberg.jsx
+++ client/devdocs/design/gutenberg.jsx
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import classnames from 'classnames';
+import { Button } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -20,7 +21,10 @@ export default class Gutenberg extends React.Component {
 			<Main className={ className }>
 				<DocumentHead title="Gutenberg Blocks"/>
 				<ReadmeViewer readmeFilePath="/client/devdocs/gutenberg/README.md"/>
+
+				<Button>My Button</Button>
 			</Main>
 		);
 	}
+
 }
