6c3fbf0b41208dedea14ad28330d2d5b142fae99
Bernie Reiter <ockham@raz.or.at>
2018-07-16 23:36:28 +0200

    Framework: Move @babel/polyfill import to webpack config

--- client/boot/polyfills.js
+++ client/boot/polyfills.js
@@ -1,13 +1,8 @@
 /** @format */
-/**
- * External dependencies
- */
-import '@babel/polyfill';
 
 /**
  * Internal dependencies
  */
-
 import localStoragePolyfill from 'lib/local-storage';
 
 localStoragePolyfill();
