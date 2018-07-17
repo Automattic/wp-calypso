4d858f0f59ee3390073af1e4e817e35d1fffd0d9
Miguel Torres <miguelmariatorresrojas@gmail.com>
2018-07-17 23:07:15 +0200

    adding @format so prettier handles whitespace formatting

--- client/devdocs/design/gutenberg-blocks.jsx
+++ client/devdocs/design/gutenberg-blocks.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 /**
  * External dependencies
  */
@@ -11,15 +13,14 @@ import DocumentHead from 'components/data/document-head';
 import Main from 'components/main';
 import ReadmeViewer from 'components/readme-viewer';
 
-
 export default class GutenbergBlocks extends React.Component {
 	render() {
 		const className = classnames( 'devdocs', 'devdocs__gutenberg-blocks' );
 
 		return (
 			<Main className={ className }>
-				<DocumentHead title="Gutenberg Blocks"/>
-				<ReadmeViewer readmeFilePath="/client/devdocs/gutenberg-blocks/README.md"/>
+				<DocumentHead title="Gutenberg Blocks" />
+				<ReadmeViewer readmeFilePath="/client/devdocs/gutenberg-blocks/README.md" />
 			</Main>
 		);
 	}
