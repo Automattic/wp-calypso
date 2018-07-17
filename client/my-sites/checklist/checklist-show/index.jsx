74919f84ae5b6d74761e80e87293adf4b5978504
Jon Surrell <jon.surrell@automattic.com>
2018-07-17 13:48:39 +0200

    Declarative onboarding checklist (breaks banner)

--- client/my-sites/checklist/checklist-show/index.jsx
+++ client/my-sites/checklist/checklist-show/index.jsx
@@ -4,94 +4,45 @@
  * External dependencies
  */
 import React, { Fragment, PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { localize } from 'i18n-calypso';
-import { find, get } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import Checklist from 'blocks/checklist';
 import JetpackChecklist from '../jetpack-checklist';
-import { requestSiteChecklistTaskUpdate } from 'state/checklist/actions';
 import { getSelectedSiteId } from 'state/ui/selectors';
-import getSiteChecklist from 'state/selectors/get-site-checklist';
-import { isJetpackSite, getSiteSlug } from 'state/sites/selectors';
+import { isJetpackSite } from 'state/sites/selectors';
 import QuerySiteChecklist from 'components/data/query-site-checklist';
-import { launchTask, tasks as wpcomTasks } from '../onboardingChecklist';
-import { mergeObjectIntoArrayById } from '../util';
-import { recordTracksEvent } from 'state/analytics/actions';
-import { createNotice } from 'state/notices/actions';
-import { requestGuidedTour } from 'state/ui/guided-tours/actions';
+import OnboardingChecklist from '../onboardingChecklist';
 
 class ChecklistShow extends PureComponent {
-	handleAction = id => {
-		const { requestTour, siteSlug, tasks, track } = this.props;
-		const task = find( tasks, { id } );
-
-		launchTask( {
-			task,
-			location: 'checklist_show',
-			requestTour,
-			siteSlug,
-			track,
-		} );
-	};
-
-	handleToggle = id => {
-		const { notify, siteId, tasks, update } = this.props;
-		const task = find( tasks, { id } );
-
-		if ( task && ! task.completed ) {
-			notify( 'is-success', 'You completed a task!' );
-			update( siteId, id );
-		}
+	static propTypes = {
+		isJetpack: PropTypes.bool,
+		siteId: PropTypes.number,
 	};
 
 	render() {
-		const { isJetpack, siteId, tasks } = this.props;
+		const { isJetpack, siteId } = this.props;
+
+		const Checklist = isJetpack ? JetpackChecklist : OnboardingChecklist;
 
 		return (
 			<Fragment>
 				{ siteId && <QuerySiteChecklist siteId={ siteId } /> }
-				{ isJetpack ? (
-					<JetpackChecklist siteId={ siteId } />
-				) : (
-					<Checklist
-						isLoading={ ! tasks }
-						tasks={ tasks }
-						onAction={ this.handleAction }
-						onToggle={ this.handleToggle }
-					/>
+				{ siteId && (
+					<Checklist key={ siteId /* force remount on siteId change */ } siteId={ siteId } />
 				) }
 			</Fragment>
 		);
 	}
 }
-
-const mapStateToProps = state => {
+function mapStateToProps( state ) {
 	const siteId = getSelectedSiteId( state );
-	const siteSlug = getSiteSlug( state, siteId );
-	const isJetpack = isJetpackSite( state, siteId );
-	const tasks = isJetpack ? null : wpcomTasks;
-	const tasksFromServer = isJetpack ? null : get( getSiteChecklist( state, siteId ), [ 'tasks' ] );
-
 	return {
-		isJetpack,
+		isJetpack: isJetpackSite( state, siteId ),
 		siteId,
-		siteSlug,
-		tasks: tasksFromServer ? mergeObjectIntoArrayById( tasks, tasksFromServer ) : null,
 	};
-};
+}
 
-const mapDispatchToProps = {
-	track: recordTracksEvent,
-	notify: createNotice,
-	requestTour: requestGuidedTour,
-	update: requestSiteChecklistTaskUpdate,
-};
-
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)( localize( ChecklistShow ) );
+export default connect( mapStateToProps )( ChecklistShow );
--- client/my-sites/checklist/onboardingChecklist.js
+++ client/my-sites/checklist/onboardingChecklist.js
@@ -3,92 +3,150 @@
  * External dependencies
  */
 import page from 'page';
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { get, reduce } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import Checklist from 'components/checklist';
+import ConnectedItem from 'blocks/checklist/item';
+import getSiteChecklist from 'state/selectors/get-site-checklist';
 import { isDesktop } from 'lib/viewport';
 
-export const tasks = [
-	{
-		id: 'site_created',
-		title: 'Create your site',
-		description: 'This is where your adventure begins.',
-		completedTitle: 'You created your site',
-		completed: true,
-	},
-	{
-		id: 'domain_selected',
-		title: 'Pick a website address',
-		description: 'Choose an address so people can find you on the internet.',
-		completedTitle: 'You picked a website address',
-		completed: true,
-		image: '/calypso/images/stats/tasks/domains.svg',
-	},
-	{
-		id: 'blogname_set',
-		title: 'Give your site a name',
-		description: 'Give your site a descriptive name to entice visitors.',
-		duration: '1 min',
-		completedTitle: 'You updated your site title',
-		completedButtonText: 'Edit',
-		url: '/settings/general/$siteSlug',
-		image: '/calypso/images/stats/tasks/personalize-your-site.svg',
-		tour: 'checklistSiteTitle',
-	},
-	{
-		id: 'site_icon_set',
-		title: 'Upload a site icon',
-		description: 'Help people recognize your site in browser tabs — just like the WordPress.com W!',
-		duration: '1 min',
-		completedTitle: 'You uploaded a site icon',
-		completedButtonText: 'Change',
-		url: '/settings/general/$siteSlug',
-		image: '/calypso/images/stats/tasks/upload-icon.svg',
-		tour: 'checklistSiteIcon',
-	},
-	{
-		id: 'blogdescription_set',
-		title: 'Create a tagline',
-		description: 'Pique readers’ interest with a little more detail about your site.',
-		duration: '2 mins',
-		completedTitle: 'You created a tagline',
-		completedButtonText: 'Change',
-		url: '/settings/general/$siteSlug',
-		image: '/calypso/images/stats/tasks/create-tagline.svg',
-		tour: 'checklistSiteTagline',
-	},
-	{
-		id: 'avatar_uploaded',
-		title: 'Upload your profile picture',
-		description:
-			'Who’s the person behind the site? Personalize your posts and comments with a custom profile picture.',
-		duration: '2 mins',
-		completedTitle: 'You uploaded a profile picture',
-		completedButtonText: 'Change',
-		url: '/me',
-		image: '/calypso/images/stats/tasks/upload-profile-picture.svg',
-		tour: 'checklistUserAvatar',
-	},
-	{
-		id: 'contact_page_updated',
-		title: 'Personalize your Contact page',
-		description: 'Encourage visitors to get in touch — a website is for connecting with people.',
-		duration: '2 mins',
-		completedTitle: 'You updated your Contact page',
-		completedButtonText: 'Edit',
-		image: '/calypso/images/stats/tasks/contact.svg',
-		url: '/post/$siteSlug/2',
-		tour: 'checklistContactPage',
-	},
-	{
-		id: 'post_published',
-		title: 'Publish your first blog post',
-		description: 'Introduce yourself to the world! That’s why you’re here.',
-		duration: '10 mins',
-		completedTitle: 'You published your first blog post',
-		completedButtonText: 'Edit',
-		url: '/post/$siteSlug',
-		image: '/calypso/images/stats/tasks/first-post.svg',
-		tour: 'checklistPublishPost',
-	},
-];
+class OnboardingChecklist extends PureComponent {
+	static propTypes = {
+		checklistTasks: PropTypes.shape( {
+			completed: PropTypes.bool,
+		} ),
+		siteId: PropTypes.number.isRequired,
+		translate: PropTypes.func.isRequired,
+	};
+
+	static taskIds = new Set( [
+		'site_created',
+		'domain_selected',
+		'blogname_set',
+		'site_icon_set',
+		'blogdescription_set',
+		'avatar_uploaded',
+		'contact_page_updated',
+		'post_published',
+		'jetpack_backups',
+	] );
+
+	render() {
+		const { siteId, checklistTasks } = this.props;
+
+		const completedCount = reduce(
+			checklistTasks,
+			( count, { completed = false }, taskId ) =>
+				completed && OnboardingChecklist.taskIds.has( taskId ) ? count + 1 : count,
+
+			2 // 2 tasks are statically completed
+		);
+
+		return (
+			<Checklist completedCount={ completedCount }>
+				<ConnectedItem
+					siteId={ siteId }
+					taskId="site_created"
+					title="Create your site"
+					description="This is where your adventure begins."
+					completedTitle="You created your site"
+					completed
+				/>
+				<ConnectedItem
+					siteId={ siteId }
+					taskId="domain_selected"
+					title="Pick a website address"
+					description="Choose an address so people can find you on the internet."
+					completedTitle="You picked a website address"
+					completed
+					image="/calypso/images/stats/tasks/domains.svg"
+				/>
+				<ConnectedItem
+					siteId={ siteId }
+					taskId="blogname_set"
+					title="Give your site a name"
+					description="Give your site a descriptive name to entice visitors."
+					duration="1 min"
+					completedTitle="You updated your site title"
+					completedButtonText="Edit"
+					tourUrl="/settings/general/$siteSlug"
+					image="/calypso/images/stats/tasks/personalize-your-site.svg"
+					tourId="checklistSiteTitle"
+				/>
+				<ConnectedItem
+					siteId={ siteId }
+					taskId="site_icon_set"
+					title="Upload a site icon"
+					description="Help people recognize your site in browser tabs — just like the WordPress.com W!"
+					duration="1 min"
+					completedTitle="You uploaded a site icon"
+					completedButtonText="Change"
+					tourUrl="/settings/general/$siteSlug"
+					image="/calypso/images/stats/tasks/upload-icon.svg"
+					tourId="checklistSiteIcon"
+				/>
+				<ConnectedItem
+					siteId={ siteId }
+					taskId="blogdescription_set"
+					title="Create a tagline"
+					description="Pique readers’ interest with a little more detail about your site."
+					duration="2 mins"
+					completedTitle="You created a tagline"
+					completedButtonText="Change"
+					tourUrl="/settings/general/$siteSlug"
+					image="/calypso/images/stats/tasks/create-tagline.svg"
+					tourId="checklistSiteTagline"
+				/>
+				<ConnectedItem
+					siteId={ siteId }
+					taskId="avatar_uploaded"
+					title="Upload your profile picture"
+					description="Who’s the person behind the site? Personalize your posts and comments with a custom profile picture."
+					duration="2 mins"
+					completedTitle="You uploaded a profile picture"
+					completedButtonText="Change"
+					tourUrl="/me"
+					image="/calypso/images/stats/tasks/upload-profile-picture.svg"
+					tourId="checklistUserAvatar"
+				/>
+				<ConnectedItem
+					siteId={ siteId }
+					taskId="contact_page_updated"
+					title="Personalize your Contact page"
+					description="Encourage visitors to get in touch — a website is for connecting with people."
+					duration="2 mins"
+					completedTitle="You updated your Contact page"
+					completedButtonText="Edit"
+					image="/calypso/images/stats/tasks/contact.svg"
+					tourUrl="/post/$siteSlug/2"
+					tourId="checklistContactPage"
+				/>
+				<ConnectedItem
+					siteId={ siteId }
+					taskId="post_published"
+					title="Publish your first blog post"
+					description="Introduce yourself to the world! That’s why you’re here."
+					duration="10 mins"
+					completedTitle="You published your first blog post"
+					completedButtonText="Edit"
+					tourUrl="/post/$siteSlug"
+					image="/calypso/images/stats/tasks/first-post.svg"
+					tourId="checklistPublishPost"
+				/>
+			</Checklist>
+		);
+	}
+}
+
+export default connect( ( state, { siteId } ) => ( {
+	checklistTasks: get( getSiteChecklist( state, siteId ), [ 'tasks' ] ),
+} ) )( OnboardingChecklist );
 
 export function launchTask( { task, location, requestTour, siteSlug, track } ) {
 	const checklist_name = 'new_blog';
