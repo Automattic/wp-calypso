c7646b8759cf50ff30c876b2d0da07a593ec997c
Adam Zieliński <adam@adamziel.com>
2018-07-17 15:58:16 +0200

    Update tests to consider canUserUpgradeSite property

--- client/my-sites/sidebar/test/sidebar.js
+++ client/my-sites/sidebar/test/sidebar.js
@@ -183,10 +183,11 @@ describe( 'MySitesSidebar', () => {
 			expect( wrapper.html() ).toEqual( null );
 		} );
 
-		test( "Should return upsell menu item if user can't use ads on this site but can manage options", () => {
+		test( "Should return upsell menu item if user can't use ads on this site but can manage options and upgrade site", () => {
 			const Sidebar = new MySitesSidebar( {
 				canUserUseAds: false,
 				canUserManageOptions: true,
+				canUserUpgradeSite: true,
 				...defaultProps,
 			} );
 			const Ads = () => Sidebar.ads();
@@ -195,10 +196,37 @@ describe( 'MySitesSidebar', () => {
 			expect( wrapper.props().link ).toEqual( '/feature/ads/mysite.com' );
 		} );
 
-		test( "Should return null if user can't use ads on this site and can't manage options", () => {
+		test( "Should return null if user can't use ads on this site and can't manage options or upgrade site", () => {
 			const Sidebar = new MySitesSidebar( {
 				canUserUseAds: false,
 				canUserManageOptions: false,
+				canUserUpgradeSite: false,
+				...defaultProps,
+			} );
+			const Ads = () => Sidebar.ads();
+
+			const wrapper = shallow( <Ads /> );
+			expect( wrapper.html() ).toEqual( null );
+		} );
+
+		test( "Should return null if user can't use ads on this site and can manage options but can't upgrade site", () => {
+			const Sidebar = new MySitesSidebar( {
+				canUserUseAds: false,
+				canUserManageOptions: true,
+				canUserUpgradeSite: false,
+				...defaultProps,
+			} );
+			const Ads = () => Sidebar.ads();
+
+			const wrapper = shallow( <Ads /> );
+			expect( wrapper.html() ).toEqual( null );
+		} );
+
+		test( "Should return null if user can't use ads on this site and can't manage options but can upgrade site", () => {
+			const Sidebar = new MySitesSidebar( {
+				canUserUseAds: false,
+				canUserManageOptions: false,
+				canUserUpgradeSite: true,
 				...defaultProps,
 			} );
 			const Ads = () => Sidebar.ads();
