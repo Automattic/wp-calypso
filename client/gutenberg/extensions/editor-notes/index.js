c59b1a9a0fb3d10fe9fdb5a3df0c7acb6d926ce4
Dennis Snell <dennis.snell@automattic.com>
2018-07-18 20:19:23 +0200

    SDK: Add "editor notes" block
    This patch adds a new Gutenberg block whose purpose is to provide
    an area to write notes about a post while writing it while making
    sure that those notes are never published in the open.
    
    The motivation for adding a block here is to test building a block
    from Calypso and then including it in another project. That is,
    the output will be used in an external PHP plugin for WordPress
    which loads the block itself.

---
+++ client/gutenberg/extensions/editor-notes/index.js
@@ -0,0 +1,50 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import wp from 'wp';
+const { RichText } = wp.editor;
+
+const attributes = {
+	notes: {
+		type: 'array',
+	},
+};
+
+const edit = ( { attributes: { notes }, className, isSelected, setAttributes } ) => (
+	<div
+		style={ {
+			border: '2px solid gray',
+			position: 'relative',
+			padding: '6px',
+			paddingTop: isSelected ? '6px' : '32px',
+			backgroundColor: '#c0e7ff',
+		} }
+	>
+		{ ! isSelected && (
+			<span style={ { position: 'absolute', top: 0, left: '8px', fontStyle: 'italic' } }>
+				<span role="img" aria-label="notebook">
+					📔
+				</span>
+				Editor's Notes: hidden from rendered page
+			</span>
+		) }
+		<RichText
+			tagName="p"
+			className={ className }
+			value={ notes }
+			onChange={ newNotes => setAttributes( { notes: newNotes } ) }
+		/>
+	</div>
+);
+
+const save = () => null;
+
+wp.blocks.registerBlockType( 'a8c/editor-notes', {
+	title: 'Editor Notes',
+	icon: 'welcome-write-blog',
+	category: 'common',
+	attributes,
+	edit,
+	save,
+} );
