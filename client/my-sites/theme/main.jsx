bcc7bddaf53557de403cd1e0a9ab0094d4358b49
Adam Zieliński <adam@adamziel.com>
2018-07-18 14:53:16 +0200

    Bugfix: Check isPremium instead of props.price

--- client/my-sites/theme/main.jsx
+++ client/my-sites/theme/main.jsx
@@ -579,7 +579,7 @@ class ThemeSheet extends React.Component {
 
 	renderSheet = () => {
 		const section = this.validateSection( this.props.section );
-		const { id, siteId, retired, hasUnlimitedPremiumThemes } = this.props;
+		const { id, siteId, retired, isPremium, hasUnlimitedPremiumThemes } = this.props;
 
 		const analyticsPath = `/theme/${ id }${ section ? '/' + section : '' }${
 			siteId ? '/:site' : ''
@@ -620,7 +620,7 @@ class ThemeSheet extends React.Component {
 
 		let pageUpsellBanner, previewUpsellBanner;
 		const hasUpsellBanner =
-			this.props.price > 0 &&
+			isPremium &&
 			! hasUnlimitedPremiumThemes &&
 			config.isEnabled( 'upsell/nudge-a-palooza' ) &&
 			abtest( 'nudgeAPalooza' ) === 'themesUpsells';
