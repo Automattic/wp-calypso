8b4d5c579ab04f5522ba77bc87ed5cbae2c0baa6
Adam Zieliński <adam@adamziel.com>
2018-07-18 14:33:52 +0200

    Copy updates as suggested by the editorial team

--- client/my-sites/feature-upsell/ads-upsell.jsx
+++ client/my-sites/feature-upsell/ads-upsell.jsx
@@ -64,7 +64,7 @@ class WordAdsUpsellComponent extends Component {
 					<h1 className="feature-upsell__header-title">Start making money with this site!</h1>
 					<p className="feature-upsell__header-subtitle">
 						Welcome to WordAds: the advertising platform where internet’s top ad suppliers bid
-						against each other to deliver their ads to your site, maximizing your revenue.
+						against each other to get their ads to your site.
 					</p>
 				</header>
 
@@ -87,7 +87,7 @@ class WordAdsUpsellComponent extends Component {
 
 				{ this.renderCTA() }
 
-				<h2 className="feature-upsell__section-header">As simple as 1, 2, 3</h2>
+				<h2 className="feature-upsell__section-header">We do the work, you make the money</h2>
 
 				<ul className="feature-upsell__benefits-list">
 					<li className="feature-upsell__benefits-list-item">
@@ -95,10 +95,10 @@ class WordAdsUpsellComponent extends Component {
 						<div className="feature-upsell__benefits-list-item-content">
 							<div className="feature-upsell__benefits-list-name">Enable WordAds</div>
 							<div className="feature-upsell__benefits-list-description">
-								After you upgrade, you can install WordAds on your site in just a few clicks.
-								WordAds places the ads automatically, optimizes their placement in your page layout
-								to give you the best return. No need to approve individual ads. Just turn it on and
-								WordAds does the rest.
+								Once you upgrade, install WordAds on your site (we'll help!) in just a few clicks.
+								WordAds places ads automatically, optimizing their placement on your site to give
+								you the best return. No need to approve individual ads - turn WordAds on, and it
+								does the rest.
 							</div>
 						</div>
 						<div className="feature-upsell__benefits-list-image-wrapper">
@@ -113,15 +113,14 @@ class WordAdsUpsellComponent extends Component {
 						<div className="feature-upsell__benefits-list-marker">2.</div>
 						<div className="feature-upsell__benefits-list-item-content">
 							<div className="feature-upsell__benefits-list-name">
-								High quality Ads start to show on your site
+								Display high quality ads on your site
 							</div>
 							<div className="feature-upsell__benefits-list-description">
-								We only partner with advertisers that display family-friendly ads, and our fraud
-								prevention team proactively identifies malicious ads. That means WordAds won't place
-								an advertisement that contains nudity, promotes gambling, or makes fraudulent claims
-								on your site, and you don't have to worry what visitors will see. If you have any
-								questions, Automattic’s global team of happiness engineers are always available to
-								address any questions and concerns.
+								We only partner with advertisers that offer family-friendly ads, and our fraud
+								prevention team proactively identifies malicious ads. That means WordAds won't show
+								ads that contains nudity, promotes gambling, or makes fraudulent claims, and you
+								don't have to worry what visitors will see. If you ever do have a question or
+								concern, Automattic’s global team of Happiness Engineers are always available.
 							</div>
 						</div>
 						<div className="feature-upsell__benefits-list-image-wrapper">
@@ -135,13 +134,12 @@ class WordAdsUpsellComponent extends Component {
 					<li className="feature-upsell__benefits-list-item">
 						<div className="feature-upsell__benefits-list-marker">3.</div>
 						<div className="feature-upsell__benefits-list-item-content">
-							<div className="feature-upsell__benefits-list-name">Collect the payout</div>
+							<div className="feature-upsell__benefits-list-name">Collect your payout</div>
 							<div className="feature-upsell__benefits-list-description">
-								You’re paid when the ad is seen by a visitor, not by click. That means the more
-								visits you get, and the more each visitor uses your site, the more you’ll earn.
-								WordAds pays monthly via PayPal. Payments are sent around the last day of the
-								following month. If you earned less than $100 in a given month, your earnings will
-								carry over to the next month instead.
+								You’re paid whenever the ad is seen by a visitor, whether they click on it or not,
+								so the more visits you get, and the more pages each visitor visits, the more you’ll
+								earn. WordAds pays monthly via PayPal (if you earn less than $100 in a month, your
+								earnings will carry over to the next month instead).
 							</div>
 						</div>
 						<div className="feature-upsell__benefits-list-image-wrapper">
@@ -164,7 +162,7 @@ class WordAdsUpsellComponent extends Component {
 							icon={ <img alt="" src="/calypso/images/illustrations/ads-removed.svg" /> }
 							title={ 'Unlimited access to premium themes' }
 							description={
-								'Make your site perfect in just a few clicks with beautiful premium themes we prepared.'
+								'You don’t have to be a designer to make a beautiful site. Choose from a range of layouts created by pros.'
 							}
 						/>
 					</div>
@@ -174,7 +172,7 @@ class WordAdsUpsellComponent extends Component {
 								icon={ <img alt="" src="/calypso/images/illustrations/jetpack-apps.svg" /> }
 								title={ 'Custom site address' }
 								description={
-									".com, .shop, or any other dot - it's on us. You choose an address and we pay for it."
+									'Make your site memorable and professional - choose a .com, .shop, or any other dot.'
 								}
 							/>
 						</div>
@@ -184,7 +182,8 @@ class WordAdsUpsellComponent extends Component {
 							icon={ <img alt="" src="/calypso/images/illustrations/jetpack-dashboard.svg" /> }
 							title={ 'Advanced design customizations' }
 							description={
-								'Make your site look perfect with additional customization features - extended color schemes, background designs, and complete control over website CSS.'
+								'Take creative control with additional customization features - like color schemes and, background designs, or add completely ' +
+								'personal touches with your CSS code.'
 							}
 						/>
 					</div>
@@ -193,8 +192,8 @@ class WordAdsUpsellComponent extends Component {
 							icon={ <img alt="" src="/calypso/images/illustrations/jetpack-wordads.svg" /> }
 							title={ 'Simple payments' }
 							description={
-								'Accept credit card payments right on your site with the click of a button! Whether you sell products, memberships, ' +
-								'or subscriptions - we got you covered.'
+								'Accept credit card payments on your site with the click of a button! Sell products, take donations, ' +
+								'sell tickets - add payment buttons to any page right from the WordPress editor'
 							}
 						/>
 					</div>
@@ -203,17 +202,7 @@ class WordAdsUpsellComponent extends Component {
 							icon={ <img alt="" src="/calypso/images/illustrations/jetpack-support.svg" /> }
 							title={ 'Priority support' }
 							description={
-								'Unlimited access to our world-class live chat and email support - no matter how complicated your question, our team ' +
-								'will find you an answer, guaranteed.'
-							}
-						/>
-					</div>
-					<div className="product-purchase-features-list__item">
-						<PurchaseDetail
-							icon={ <img alt="" src="/calypso/images/illustrations/jetpack-updates.svg" /> }
-							title={ 'Even more!' }
-							description={
-								'Need help? A Happiness Engineer can answer any question you may have about your ads and your account.'
+								'Unlimited access to our world-class live chat and email support. No question is too big or too small!'
 							}
 						/>
 					</div>
@@ -235,7 +224,8 @@ class WordAdsUpsellComponent extends Component {
 							onClick={ this.handleUpgradeButtonClick }
 							className="button is-primary feature-upsell__cta-button"
 						>
-							Upgrade for { this.renderPrice() } and get started
+							Upgrade to a Premium plan for { this.renderPrice() } and start earning revenue from
+							your site
 						</button>
 						<span className="feature-upsell__cta-guarantee">30-day money back guarantee</span>
 					</React.Fragment>
