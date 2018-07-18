ba15e9700d910c7f47df172df6e6048fa3a45973
Ben Lowery <ben.lowery@automattic.com>
2018-07-18 16:35:47 -0400

    use async require instead of dynamic import

--- client/state/i18n/middleware.js
+++ client/state/i18n/middleware.js
@@ -11,8 +11,8 @@ import { LOCALE_SET } from 'state/action-types';
 function updateMoment( action ) {
 	if ( action && action.type === LOCALE_SET && action.localeSlug !== 'en' ) {
 		// load moment and update the locale
-		import( 'moment' ).then( moment => {
-			import( 'moment/locale/' + action.localeSlug ).then( () => {
+		asyncRequire( 'moment', moment => {
+			asyncRequire( 'moment/locale/' + action.localeSlug, () => {
 				moment.locale( action.localeSlug );
 			} );
 		} );
