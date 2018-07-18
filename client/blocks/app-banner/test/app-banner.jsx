9dc9335a7476c8013512682f71722cd987c068cc
James Frost <james.frost@automattic.com>
2018-07-18 11:55:55 +0100

    AppBanner: Remove chai dependency in tests

--- client/blocks/app-banner/test/app-banner.jsx
+++ client/blocks/app-banner/test/app-banner.jsx
@@ -6,7 +6,6 @@
 /**
  * External dependencies
  */
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import React from 'react';
 
@@ -24,49 +23,49 @@ describe( 'AppBanner', () => {
 	} );
 
 	test( 'iOS deep links return correct URIs for EDITOR', () => {
-		expect( wrapper.instance().getiOSDeepLink( '/post', EDITOR ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( '/post', EDITOR ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fpost'
 		);
-		expect( wrapper.instance().getiOSDeepLink( '/post/discover.wordpress.com', EDITOR ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( '/post/discover.wordpress.com', EDITOR ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fpost'
 		);
-		expect( wrapper.instance().getiOSDeepLink( null, EDITOR ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( null, EDITOR ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fpost'
 		);
 	} );
 
 	test( 'iOS deep links return correct URIs for NOTES', () => {
-		expect( wrapper.instance().getiOSDeepLink( '/notifications', NOTES ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( '/notifications', NOTES ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fnotifications'
 		);
-		expect( wrapper.instance().getiOSDeepLink( '/notifications/12345', NOTES ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( '/notifications/12345', NOTES ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fnotifications'
 		);
-		expect( wrapper.instance().getiOSDeepLink( null, NOTES ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( null, NOTES ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fnotifications'
 		);
 	} );
 
 	test( 'iOS deep links return correct URIs for READER', () => {
-		expect( wrapper.instance().getiOSDeepLink( '/', READER ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( '/', READER ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fread'
 		);
-		expect( wrapper.instance().getiOSDeepLink( '/read/feeds/12345/posts/6789', READER ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( '/read/feeds/12345/posts/6789', READER ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fread%2Ffeeds%2F12345%2Fposts%2F6789'
 		);
-		expect( wrapper.instance().getiOSDeepLink( null, READER ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( null, READER ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fread'
 		);
 	} );
 
 	test( 'iOS deep links return correct URIs for STATS', () => {
-		expect( wrapper.instance().getiOSDeepLink( '/stats', STATS ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( '/stats', STATS ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fstats'
 		);
-		expect( wrapper.instance().getiOSDeepLink( '/stats/day/discover.wordpress.com', STATS ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( '/stats/day/discover.wordpress.com', STATS ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fstats%2Fday%2Fdiscover.wordpress.com'
 		);
-		expect( wrapper.instance().getiOSDeepLink( null, STATS ) ).equal(
+		expect( wrapper.instance().getiOSDeepLink( null, STATS ) ).toBe(
 			'https://apps.wordpress.com/get#%2Fstats'
 		);
 	} );
