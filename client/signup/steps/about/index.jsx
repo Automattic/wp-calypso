43187ee92ea1103621f838aa3c45793cd46901d1
Dan Hauk <hauk.dan@gmail.com>
2018-07-20 16:45:09 -0400

    Use info popovers to show the helper text

--- client/signup/steps/about/index.jsx
+++ client/signup/steps/about/index.jsx
@@ -35,7 +35,7 @@ import { isUserLoggedIn } from 'state/current-user/selectors';
 import Card from 'components/card';
 import Button from 'components/button';
 import FormTextInput from 'components/forms/form-text-input';
-import FormSettingExplanation from 'components/forms/form-setting-explanation';
+import InfoPopover from 'components/info-popover';
 import FormLabel from 'components/forms/form-label';
 import FormLegend from 'components/forms/form-legend';
 import FormFieldset from 'components/forms/form-fieldset';
@@ -541,6 +541,12 @@ class AboutStep extends Component {
 							<FormFieldset>
 								<FormLabel htmlFor="siteTitle">
 									{ translate( 'What would you like to name your site?' ) }
+									<InfoPopover className="about__info-popover" position="right">
+										{ translate(
+											"We'll use this as your site title. " +
+												"Don't worry, you can change this later."
+										) }
+									</InfoPopover>
 								</FormLabel>
 								<FormTextInput
 									id="siteTitle"
@@ -549,14 +555,14 @@ class AboutStep extends Component {
 									defaultValue={ siteTitle }
 									onChange={ this.handleChangeEvent }
 								/>
-								<FormSettingExplanation>
-									{ translate( "Don't worry, you can change this anytime." ) }
-								</FormSettingExplanation>
 							</FormFieldset>
 
 							<FormFieldset>
 								<FormLabel htmlFor="siteTopic">
 									{ translate( 'What will your site be about?' ) }
+									<InfoPopover className="about__info-popover" position="right">
+										{ translate( "We'll use this to personalize your site and experience." ) }
+									</InfoPopover>
 								</FormLabel>
 								<FormTextInput
 									id="siteTopic"
@@ -574,9 +580,6 @@ class AboutStep extends Component {
 									suggestions={ this.getSuggestions() }
 									suggest={ this.handleSuggestionMouseDown }
 								/>
-								<FormSettingExplanation>
-									{ translate( "We'll start you off with a theme designed for you." ) }
-								</FormSettingExplanation>
 							</FormFieldset>
 
 							<FormFieldset>
