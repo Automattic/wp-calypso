396b9df91373400e514d20f4ed8eca833fb79c3f
Jon Surrell <jon.surrell@automattic.com>
2018-07-20 15:17:52 +0200

    Use declarative checklists

--- client/my-sites/checklist/checklist-show/index.jsx
+++ client/my-sites/checklist/checklist-show/index.jsx
@@ -3,63 +3,49 @@
 /**
  * External dependencies
  */
+import PropTypes from 'prop-types';
 import React, { Fragment, PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { get, reduce } from 'lodash';
 import { localize } from 'i18n-calypso';
-import { find, get } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import Checklist from 'blocks/checklist';
-import { tasks as jetpackTasks } from '../jetpack-checklist';
-import { requestSiteChecklistTaskUpdate } from 'state/checklist/actions';
-import { getSelectedSiteId } from 'state/ui/selectors';
+import Checklist from 'components/checklist';
 import getSiteChecklist from 'state/selectors/get-site-checklist';
-import { isJetpackSite, getSiteSlug } from 'state/sites/selectors';
+import isAtomicSite from 'state/selectors/is-site-automated-transfer';
+import Item from 'blocks/checklist/item';
 import QuerySiteChecklist from 'components/data/query-site-checklist';
-import { launchTask, tasks as wpcomTasks } from '../onboardingChecklist';
-import { mergeObjectIntoArrayById } from '../util';
-import { recordTracksEvent } from 'state/analytics/actions';
-import { createNotice } from 'state/notices/actions';
-import { requestGuidedTour } from 'state/ui/guided-tours/actions';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { isJetpackSite, getSiteSlug } from 'state/sites/selectors';
+import { tasks as jetpackTasks } from '../jetpack-checklist';
+import { tasks as wpcomTasks } from '../onboardingChecklist';
 
 class ChecklistShow extends PureComponent {
-	handleAction = id => {
-		const { requestTour, siteSlug, tasks, track } = this.props;
-		const task = find( tasks, { id } );
-
-		launchTask( {
-			task,
-			location: 'checklist_show',
-			requestTour,
-			siteSlug,
-			track,
-		} );
-	};
-
-	handleToggle = id => {
-		const { notify, siteId, tasks, update } = this.props;
-		const task = find( tasks, { id } );
-
-		if ( task && ! task.completed ) {
-			notify( 'is-success', 'You completed a task!' );
-			update( siteId, id );
-		}
+	static propTypes = {
+		siteId: PropTypes.number,
+		siteSlug: PropTypes.string,
+		useJetpackChecklist: PropTypes.bool,
 	};
 
 	render() {
-		const { siteId, tasks } = this.props;
-
+		const { useJetpackChecklist, siteId, siteSlug, taskStatuses } = this.props;
+		const tasks = useJetpackChecklist ? jetpackTasks : wpcomTasks;
+		const completedCount = reduce(
+			tasks,
+			( count, { id, completed } ) =>
+				completed || get( taskStatuses, [ id, 'completed' ] ) ? count + 1 : count,
+			0
+		);
 		return (
 			<Fragment>
 				{ siteId && <QuerySiteChecklist siteId={ siteId } /> }
-				<Checklist
-					isLoading={ ! tasks }
-					tasks={ tasks }
-					onAction={ this.handleAction }
-					onToggle={ this.handleToggle }
-				/>
+				<Checklist completedCount={ completedCount }>
+					{ tasks.map( taskProps => (
+						<Item { ...taskProps } key={ taskProps.id } siteId={ siteId } siteSlug={ siteSlug } />
+					) ) }
+				</Checklist>
 			</Fragment>
 		);
 	}
@@ -68,26 +54,15 @@ class ChecklistShow extends PureComponent {
 const mapStateToProps = state => {
 	const siteId = getSelectedSiteId( state );
 	const siteSlug = getSiteSlug( state, siteId );
-	const siteChecklist = getSiteChecklist( state, siteId );
-	const isJetpack = isJetpackSite( state, siteId );
-	const tasks = isJetpack ? jetpackTasks : wpcomTasks;
-	const tasksFromServer = get( siteChecklist, [ 'tasks' ] );
+	const useJetpackChecklist = isJetpackSite( state, siteId ) && ! isAtomicSite( state, siteId );
+	const taskStatuses = get( getSiteChecklist( state, siteId ), [ 'tasks' ] );
 
 	return {
 		siteId,
 		siteSlug,
-		tasks: tasksFromServer ? mergeObjectIntoArrayById( tasks, tasksFromServer ) : null,
+		taskStatuses,
+		useJetpackChecklist,
 	};
 };
 
-const mapDispatchToProps = {
-	track: recordTracksEvent,
-	notify: createNotice,
-	requestTour: requestGuidedTour,
-	update: requestSiteChecklistTaskUpdate,
-};
-
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)( localize( ChecklistShow ) );
+export default connect( mapStateToProps )( localize( ChecklistShow ) );
