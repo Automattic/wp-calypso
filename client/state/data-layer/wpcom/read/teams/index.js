919993a400b958a7b04a37d665a49cb51c89baf2
Marin Atanasov <tyxla@abv.bg>
2018-07-20 17:27:07 +0300

    State: Use dispatchRequestEx for read/teams

--- client/state/data-layer/wpcom/read/teams/index.js
+++ client/state/data-layer/wpcom/read/teams/index.js
@@ -1,41 +1,39 @@
 /** @format */
+
 /**
  * Internal dependencies
  */
 import { READER_TEAMS_REQUEST, READER_TEAMS_RECEIVE } from 'state/action-types';
-import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
 import { http } from 'state/data-layer/wpcom-http/actions';
 
-export const handleTeamsRequest = ( { dispatch }, action ) => {
-	dispatch(
-		http(
-			{
-				method: 'GET',
-				path: '/read/teams',
-				apiVersion: '1.2',
-			},
-			action
-		)
+export const handleTeamsRequest = action =>
+	http(
+		{
+			method: 'GET',
+			path: '/read/teams',
+			apiVersion: '1.2',
+		},
+		action
 	);
-};
 
-export const teamRequestReceived = ( { dispatch }, action, apiResponse ) => {
-	dispatch( {
-		type: READER_TEAMS_RECEIVE,
-		payload: apiResponse,
-	} );
-};
+export const teamRequestReceived = ( action, apiResponse ) => ( {
+	type: READER_TEAMS_RECEIVE,
+	payload: apiResponse,
+} );
 
-export const teamRequestFailure = ( { dispatch }, error ) => {
-	dispatch( {
-		type: READER_TEAMS_RECEIVE,
-		payload: error,
-		error: true,
-	} );
-};
+export const teamRequestFailure = error => ( {
+	type: READER_TEAMS_RECEIVE,
+	payload: error,
+	error: true,
+} );
 
 export default {
 	[ READER_TEAMS_REQUEST ]: [
-		dispatchRequest( handleTeamsRequest, teamRequestReceived, teamRequestFailure ),
+		dispatchRequestEx( {
+			fetch: handleTeamsRequest,
+			onSuccess: teamRequestReceived,
+			onError: teamRequestFailure,
+		} ),
 	],
 };
