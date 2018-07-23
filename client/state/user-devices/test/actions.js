a9f9338e4677a99a149b0e769d49cd53aa40f14f
Marin Atanasov <tyxla@users.noreply.github.com>
2018-07-23 17:50:21 +0300

    Framework: Use dispatchRequestEx for user devices (#26214)
    * State: Update me/devices data layer to use dispatchRequestEx
    
    * State: Update me/devices data layer tests
    
    * State: Simplify userDevicesAdd action creator
    
    * State: Use simple userDevicesAdd in data layer and fix jsdoc
    
    * State: Update user devices action tests to use jest
    
    * State: Update user devices data layer tests to use the right format
    
    * Framework: Fix docs in me/devices data layer

--- client/state/user-devices/test/actions.js
+++ client/state/user-devices/test/actions.js
@@ -1,39 +1,32 @@
 /** @format */
 
 /**
- * External dependencies
- */
-import { expect } from 'chai';
-
-/**
  * Internal dependencies
  */
 import { requestUserDevices, userDevicesAdd } from '../actions';
 import { USER_DEVICES_REQUEST, USER_DEVICES_ADD } from 'state/action-types';
 
-describe( 'actions', () => {
-	describe( '#requestUserDevices()', () => {
-		test( 'should return an action object', () => {
-			const action = requestUserDevices();
+describe( 'requestUserDevices()', () => {
+	test( 'should return an action object', () => {
+		const action = requestUserDevices();
 
-			expect( action ).to.eql( {
-				type: USER_DEVICES_REQUEST,
-			} );
+		expect( action ).toEqual( {
+			type: USER_DEVICES_REQUEST,
 		} );
 	} );
+} );
 
-	describe( '#userDevicesRequestSuccess()', () => {
-		test( 'should return an action object', () => {
-			const devices = {
-				1: { id: 1, name: 'Mobile Phone' },
-				2: { id: 2, name: 'Tablet' },
-			};
-			const action = userDevicesAdd( { devices } );
+describe( 'userDevicesAdd()', () => {
+	test( 'should return an action object', () => {
+		const devices = {
+			1: { id: 1, name: 'Mobile Phone' },
+			2: { id: 2, name: 'Tablet' },
+		};
+		const action = userDevicesAdd( devices );
 
-			expect( action ).to.eql( {
-				type: USER_DEVICES_ADD,
-				devices,
-			} );
+		expect( action ).toEqual( {
+			type: USER_DEVICES_ADD,
+			devices,
 		} );
 	} );
 } );
