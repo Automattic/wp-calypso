f3203807519452394e4218d12b990772f13e8ec0
Marin Atanasov <tyxla@abv.bg>
2018-07-23 16:48:20 +0300

    Gutenberg SDK: Add npm script for block building

--- bin/create-scripts/block.js
+++ bin/create-scripts/block.js
@@ -3,7 +3,7 @@ const path = require( 'path' );
 const webpack = require( 'webpack' );
 
 const __rootDir = path.resolve( __dirname, '../../' );
-const entryPath = path.resolve( process.argv[ 2 ] );
+const entryPath = path.resolve( 'client/gutenberg/extensions/', process.argv[ 2 ] );
 const sourceDir = path.dirname( entryPath );
 const outputDir = path.join( sourceDir, 'build' );
 const blockName = path.basename( path.dirname( entryPath ) );
