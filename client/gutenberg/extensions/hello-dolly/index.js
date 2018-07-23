b93f5118774c1d50c49eddcad50084d79a954415
Marin Atanasov <tyxla@abv.bg>
2018-07-23 15:42:51 +0300

    Gutenberg: Add basic stylesheet to Hello Dolly block

--- client/gutenberg/extensions/hello-dolly/index.js
+++ client/gutenberg/extensions/hello-dolly/index.js
@@ -8,6 +8,14 @@ wp.blocks.registerBlockType( 'calypsoberg/hello-dolly', {
 	title: 'Hello Dolly',
 	icon: 'format-audio',
 	category: 'layout',
-	edit: ( { isSelected } ) => <p>{ isSelected ? 'Editing Hello Dolly' : 'Viewing Hello Dolly' }</p>,
-	save: () => <p>Saving Hello Dolly</p>,
+	edit: ( { isSelected } ) => (
+		<div className="hello-dolly__block">
+			<p>{ isSelected ? 'Editing Hello Dolly' : 'Viewing Hello Dolly' }</p>
+		</div>
+	),
+	save: () => (
+		<div className="hello-dolly__block">
+			<p>Saving Hello Dolly</p>
+		</div>
+	),
 } );
