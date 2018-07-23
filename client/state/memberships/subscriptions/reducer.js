99b2a64a61d571476a1d0a70e1610f9b55dfbab4
Artur Piszek <artur.piszek@gmail.com>
2018-07-23 12:46:12 +0200

    Memberships: move subscription list to data layer (#26023)
    This moves subscription list action introduced in #25866 to data layer.
    This removes fair amount of code and makes everything better.
    
    No functional changes.
    
    
    * Move subscriptions list to data layer
    
    * Trim more extra reducers
    
    * MEMBERSHIPS_SUBSCRIPTIONS_LIST_REQUEST marked as request
    
    * move handleSubscribedMembershipsList to a dir

--- client/state/memberships/subscriptions/reducer.js
+++ client/state/memberships/subscriptions/reducer.js
@@ -5,10 +5,7 @@
  */
 
 import {
-	MEMBERSHIPS_SUBSCRIPTIONS_LIST,
 	MEMBERSHIPS_SUBSCRIPTIONS_RECEIVE,
-	MEMBERSHIPS_SUBSCRIPTIONS_LIST_SUCCESS,
-	MEMBERSHIPS_SUBSCRIPTIONS_LIST_FAILURE,
 	MEMBERSHIPS_SUBSCRIPTION_STOP,
 	MEMBERSHIPS_SUBSCRIPTION_STOP_SUCCESS,
 	MEMBERSHIPS_SUBSCRIPTION_STOP_FAILURE,
@@ -29,20 +26,6 @@ export const items = createReducer( [], {
 		state.filter( sub => sub.ID !== subscriptionId ),
 } );
 
-/**
- * Returns the updated requests state after an action has been dispatched.
- * The state contains whether a request for billing transactions is in progress.
- *
- * @param  {Object} state  Current state
- * @param  {Object} action Action payload
- * @return {Object}        Updated state
- */
-export const requesting = createReducer( false, {
-	[ MEMBERSHIPS_SUBSCRIPTIONS_LIST ]: () => true,
-	[ MEMBERSHIPS_SUBSCRIPTIONS_LIST_SUCCESS ]: () => false,
-	[ MEMBERSHIPS_SUBSCRIPTIONS_LIST_FAILURE ]: () => false,
-} );
-
 export const stoppingSubscription = createReducer( [], {
 	[ MEMBERSHIPS_SUBSCRIPTION_STOP ]: ( state, { subscriptionId } ) => ( {
 		...state,
@@ -60,6 +43,5 @@ export const stoppingSubscription = createReducer( [], {
 
 export default combineReducers( {
 	items,
-	requesting,
 	stoppingSubscription,
 } );
