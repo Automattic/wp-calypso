2e283ad5553856f04d1527d5b3a63e00982f4f06
Caroline Moore <calobee@gmail.com>
2018-07-23 16:03:20 -0400

    Domains: Update subheading to match whether transferring or mapping an existing domain name. (#26266)

--- client/signup/steps/domains/index.jsx
+++ client/signup/steps/domains/index.jsx
@@ -374,7 +374,7 @@ class DomainsStep extends React.Component {
 
 	getSubHeaderText() {
 		const { translate } = this.props;
-		return 'transfer' === this.props.stepSectionName
+		return 'transfer' === this.props.stepSectionName || 'mapping' === this.props.stepSectionName
 			? translate( 'Use a domain you already own with your new WordPress.com site.' )
 			: translate(
 					"Enter your site's name, or some keywords that describe it - " +
