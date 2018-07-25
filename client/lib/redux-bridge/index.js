0a56a94db2967c6fe2be5ad0550ef3344da3355f
Jarda Snajdr <jsnajdr@gmail.com>
2018-07-25 11:45:36 +0200

    Redux Bridge: add 'getReduxStore' fn to return a Promise with initialized store
    Returns a promise of initialized store, so that clients can wait for Redux store to be
    initialized and then perform some task on it.

--- client/lib/redux-bridge/index.js
+++ client/lib/redux-bridge/index.js
@@ -7,8 +7,23 @@ import Dispatcher from 'dispatcher';
 
 let reduxStore = null;
 
+let resolveReduxStorePromise;
+const reduxStorePromise = new Promise( resolve => {
+	resolveReduxStorePromise = resolve;
+} );
+
 export function setReduxStore( store ) {
 	reduxStore = store;
+	resolveReduxStorePromise( store );
+}
+
+/**
+ * Asynchronously get the current Redux store. Returns a Promise that gets resolved only
+ * after the store is set by `setReduxStore`.
+ * @returns {Promise<ReduxStore>} Promise of the Redux store object.
+ */
+export function getReduxStore() {
+	return reduxStorePromise;
 }
 
 /**
