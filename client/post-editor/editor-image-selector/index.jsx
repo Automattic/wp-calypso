5167aa51bb54ce424341781bcf7f5146561382aa
Joshua Flowers <joshuatf@gmail.com>
2018-07-25 18:10:08 +0800

    Add post editor image selector
    This will act as a reusable component to allow for multiple images to be selected as featured images or for various post meta options using the media modal for image selection and uploads.
    
    The existing editor-featured-image does this well for a single image, but does not allow multiple images to be selected and also handles data persistence directly within the component.
    
    This new component will be in many ways similar to the featured image, but will allow multiple images and also handle data changes within a prop to allow for easy extensibility.

---
+++ client/post-editor/editor-image-selector/index.jsx
@@ -0,0 +1,163 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { noop } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import EditorImageSelectorPreview from './preview';
+import ImageSelectorDropZone from './dropzone';
+import isDropZoneVisible from 'state/selectors/is-drop-zone-visible';
+import MediaLibrarySelectedData from 'components/data/media-library-selected-data';
+import MediaActions from 'lib/media/actions';
+import MediaModal from 'post-editor/media-modal';
+import MediaStore from 'lib/media/store';
+import QueryMedia from 'components/data/query-media';
+import { localize } from 'i18n-calypso';
+import { getSelectedSiteId } from 'state/ui/selectors';
+
+export class EditorImageSelector extends Component {
+	static propTypes = {
+		hasDropZone: PropTypes.bool,
+		imageIds: PropTypes.array,
+		isDropZoneVisible: PropTypes.bool,
+		showEditIcon: PropTypes.bool,
+		maxWidth: PropTypes.number,
+		selecting: PropTypes.bool,
+		translate: PropTypes.func,
+		onImageChange: PropTypes.func,
+		onImageSelected: PropTypes.func,
+		onRemoveImage: PropTypes.func,
+	};
+
+	static defaultProps = {
+		imageIds: [],
+		hasDropZone: false,
+		isDropZoneVisible: false,
+		maxWidth: 450,
+		onImageSelected: noop,
+		onImageChange: noop,
+		onRemoveImage: noop,
+	};
+
+	state = {
+		isSelecting: false,
+	};
+
+	showMediaModal = () => {
+		const { siteId, imageIds } = this.props;
+
+		if ( imageIds ) {
+			const images = imageIds.map( imageId => MediaStore.get( siteId, imageId ) );
+			MediaActions.setLibrarySelectedItems( siteId, images );
+		}
+
+		this.setState( {
+			isSelecting: true,
+		} );
+	};
+
+	hideMediaModal = () => {
+		this.setState( {
+			isSelecting: false,
+		} );
+	};
+
+	setImage = value => {
+		this.hideMediaModal();
+		this.props.onImageSelected( value );
+	};
+
+	removeImage = id => {
+		this.props.onRemoveImage( id );
+	};
+
+	addImage = id => {
+		this.props.onAddImage( id );
+	};
+
+	// called when media library item transitions from temporary ID to a permanent ID, e.g.,
+	// after creating an item by uploading or selecting from Google library.
+	onImageChange = imageIds => {
+		if ( imageIds !== this.props.imageIds ) {
+			this.props.onImageChange( imageIds );
+		}
+	};
+
+	renderMediaModal() {
+		if ( ! this.props.siteId ) {
+			return;
+		}
+
+		return (
+			<MediaLibrarySelectedData siteId={ this.props.siteId }>
+				<MediaModal
+					visible={ this.props.selecting || this.state.isSelecting }
+					onClose={ this.setImage }
+					siteId={ this.props.siteId }
+					labels={ { confirm: this.props.translate( 'Set Images' ) } }
+					enabledFilters={ [ 'images' ] }
+					galleryViewEnabled={ false }
+					{ ...! this.props.multiple && { single: true } }
+				/>
+			</MediaLibrarySelectedData>
+		);
+	}
+
+	renderSelectedImages() {
+		const { siteId, imageIds, maxWidth, multiple, showEditIcon } = this.props;
+
+		if ( ! imageIds ) {
+			return;
+		}
+
+		return (
+			<EditorImageSelectorPreview
+				siteId={ siteId }
+				itemIds={ imageIds }
+				maxWidth={ maxWidth }
+				onImageChange={ this.onImageChange }
+				onImageClick={ this.showMediaModal }
+				onRemoveImage={ this.removeImage }
+				multiple={ multiple }
+				compact={ imageIds && imageIds.length > 0 }
+				showEditIcon={ showEditIcon }
+			/>
+		);
+	}
+
+	render() {
+		const { siteId, imageIds } = this.props;
+		const classes = classnames( 'editor-image-selector', {
+			'is-assigned': !! imageIds,
+			'has-active-drop-zone': this.props.hasDropZone && this.props.isDropZoneVisible,
+		} );
+
+		return (
+			<div className={ classes }>
+				{ imageIds && <QueryMedia siteId={ siteId } mediaId={ imageIds[ 0 ] } /> }
+				{ this.renderMediaModal() }
+				<div className="editor-image-selector__inner-content">{ this.renderSelectedImages() }</div>
+
+				{ this.props.hasDropZone && <ImageSelectorDropZone onDroppedImage={ this.addImage } /> }
+			</div>
+		);
+	}
+}
+
+export default connect( state => {
+	const siteId = getSelectedSiteId( state );
+
+	return {
+		siteId,
+		isDropZoneVisible: isDropZoneVisible( state, 'imageSelector' ),
+	};
+} )( localize( EditorImageSelector ) );
---
+++ client/post-editor/editor-image-selector/preview.jsx
@@ -0,0 +1,216 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { defer } from 'lodash';
+import classNames from 'classnames';
+import Gridicon from 'gridicons';
+
+/**
+ * Internal dependencies
+ */
+import Button from 'components/button';
+import ImagePreloader from 'components/image-preloader';
+import { localize } from 'i18n-calypso';
+import MediaActions from 'lib/media/actions';
+import MediaStore from 'lib/media/store';
+import Spinner from 'components/spinner';
+import { url } from 'lib/media/utils';
+
+export class EditorImageSelectorPreview extends Component {
+	static propTypes = {
+		siteId: PropTypes.number,
+		itemIds: PropTypes.array,
+		multiple: PropTypes.bool,
+		maxWidth: PropTypes.number,
+		onImageChange: PropTypes.func,
+		showEditIcon: PropTypes.bool,
+		translate: PropTypes.func,
+	};
+
+	state = {
+		images: [],
+	};
+
+	componentDidMount() {
+		this.fetchImages();
+		MediaStore.on( 'change', this.updateImageState );
+	}
+
+	componentDidUpdate( prevProps ) {
+		const { siteId, itemIds } = this.props;
+		if ( siteId !== prevProps.siteId || itemIds !== prevProps.itemIds ) {
+			this.fetchImages();
+		}
+	}
+
+	componentWillUnmount() {
+		MediaStore.off( 'change', this.updateImageState );
+	}
+
+	fetchImages = () => {
+		// We may not necessarily need to trigger a network request if we
+		// already have the data for the media item, so first update the state
+		this.updateImageState( () => {
+			if ( this.state.images ) {
+				return;
+			}
+
+			defer( () => {
+				this.props.itemIds.map( id => {
+					id = parseInt( id, 10 );
+					MediaActions.fetch( this.props.siteId, id );
+				} );
+			} );
+		} );
+	};
+
+	updateImageState = callback => {
+		const images = this.props.itemIds
+			.map( id => {
+				const media = MediaStore.get( this.props.siteId, id );
+				if ( ! media ) {
+					MediaActions.fetch( this.props.siteId, id );
+				}
+				return media;
+			} )
+			.filter( function( e ) {
+				return e;
+			} );
+
+		this.setState( { images }, () => {
+			if ( 'function' === typeof callback ) {
+				callback();
+			}
+		} );
+
+		defer( () => {
+			if ( this.props.onImageChange && images ) {
+				this.props.onImageChange( images );
+			}
+		} );
+	};
+
+	src = image => {
+		return url( image, {
+			maxWidth: this.props.maxWidth,
+			size: 'post-thumbnail',
+		} );
+	};
+
+	renderPlaceholder = image => {
+		const { placeholder } = image;
+		return (
+			<figure>
+				<img src={ placeholder || '' } alt="" />
+				<Spinner />
+			</figure>
+		);
+	};
+
+	renderUploaded = ( { URL, placeholder } ) => {
+		return (
+			<figure>
+				<ImagePreloader
+					src={ URL }
+					placeholder={ placeholder ? <img src={ placeholder } alt="" /> : <span /> }
+				/>
+			</figure>
+		);
+	};
+
+	renderImage = image => {
+		const src = this.src( image );
+		const { showEditIcon, translate } = this.props;
+		const id = image.ID || image.transientId;
+
+		const removeImage = () => {
+			this.props.onRemoveImage( id );
+		};
+
+		const classes = classNames( 'editor-image-selector__item', {
+			preview: null === src,
+		} );
+
+		return (
+			<div className={ classes } key={ id }>
+				<Button
+					className="editor-image-selector__image"
+					onClick={ this.props.onImageClick }
+					borderless
+					compact
+					data-tip-target="editor-image-selector-image"
+				>
+					{ src ? this.renderUploaded( image ) : this.renderPlaceholder( image ) }
+					{ showEditIcon && (
+						<Gridicon icon="pencil" className="editor-image-selector__edit-icon" />
+					) }
+				</Button>
+				<Button
+					onClick={ removeImage }
+					compact
+					aria-label={ translate( 'Remove image' ) }
+					className="editor-image-selector__remove"
+				>
+					<Gridicon icon="cross-small" size={ 24 } className="editor-image-selector__remove-icon" />
+				</Button>
+			</div>
+		);
+	};
+
+	renderUploadPlaceholder() {
+		const { translate, compact, multiple } = this.props;
+
+		const classes = classNames( 'editor-image-selector__uploader-wrapper', {
+			compact,
+		} );
+
+		const addString = multiple ? translate( 'Add images' ) : translate( 'Add image' );
+		const iconSize = compact ? 24 : 36;
+
+		return (
+			<Button
+				className={ classes }
+				onClick={ this.props.onImageClick }
+				borderless
+				compact
+				data-tip-target="editor-image-selector-image"
+			>
+				<div className="editor-image-selector__uploader-picker">
+					<div className="editor-image-selector__uploader-label">
+						<div>
+							<Gridicon icon="add-image" size={ iconSize } />
+							<p>{ ! compact && addString }</p>
+						</div>
+					</div>
+				</div>
+			</Button>
+		);
+	}
+
+	render() {
+		const images = [ ...this.state.images ];
+		const featuredImage = ( images && images.shift() ) || null;
+
+		return (
+			<div className="editor-image-selector__images-wrapper">
+				<div className="editor-image-selector__images">
+					<div className="editor-image-selector__images-featured">
+						{ featuredImage && this.renderImage( featuredImage ) }
+						{ ! featuredImage && this.renderUploadPlaceholder() }
+					</div>
+					<div className="editor-image-selector__images-thumbs">
+						{ images && images.map( image => this.renderImage( image ) ) }
+						{ featuredImage && this.props.multiple && this.renderUploadPlaceholder() }
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default localize( EditorImageSelectorPreview );
