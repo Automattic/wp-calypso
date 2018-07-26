0904e6f69ad555a01a1aceeb4770d42651b11fbe
Marin Atanasov <tyxla@abv.bg>
2018-07-26 18:23:45 +0300

    Fix main block file

--- client/gutenberg/extensions/markdown/jetpack-markdown-block.js
+++ client/gutenberg/extensions/markdown/jetpack-markdown-block.js
@@ -1,64 +1,54 @@
-'use strict';
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-var _jetpackMarkdownBlockEditor = require('./jetpack-markdown-block-editor');
-
-var _jetpackMarkdownBlockEditor2 = _interopRequireDefault(_jetpackMarkdownBlockEditor);
-
-var _jetpackMarkdownBlockSave = require('./jetpack-markdown-block-save');
-
-var _jetpackMarkdownBlockSave2 = _interopRequireDefault(_jetpackMarkdownBlockSave);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+/**
+ * External dependencies
+ */
+import React from 'react';
 
 /**
  * Internal dependencies
  */
-var __ = window.wp.i18n.__; /**
-                             * External dependencies
-                             */
+import JetpackMarkdownBlockEditor from './jetpack-markdown-block-editor';
+import JetpackMarkdownBlockSave from './jetpack-markdown-block-save';
 
-var registerBlockType = window.wp.blocks.registerBlockType;
+const { __ } = window.wp.i18n;
 
+const {
+	registerBlockType,
+} = window.wp.blocks;
 
-registerBlockType('jetpack/markdown-block', {
+registerBlockType( 'jetpack/markdown-block', {
 
-	title: __('Markdown'),
+	title: __( 'Markdown' ),
 
-	description: [__('Write your content in plain-text Markdown syntax.'), wp.element.createElement(
-		'p',
-		null,
-		wp.element.createElement(
-			'a',
-			{ href: 'https://en.support.wordpress.com/markdown-quick-reference/' },
-			'Support Reference'
+	description: [
+		__( 'Write your content in plain-text Markdown syntax.' ),
+		(
+		<p>
+			<a href="https://en.support.wordpress.com/markdown-quick-reference/">
+			Support Reference
+			</a>
+		</p>
 		)
-	)],
+	],
 
-	icon: wp.element.createElement(
-		'svg',
-		{
-			xmlns: 'http://www.w3.org/2000/svg',
-			'class': 'dashicon',
-			width: '20',
-			height: '20',
-			viewBox: '0 0 208 128',
-			stroke: 'currentColor'
-		},
-		wp.element.createElement('rect', {
-			width: '198',
-			height: '118',
-			x: '5',
-			y: '5',
-			ry: '10',
-			'stroke-width': '10',
-			fill: 'none'
-		}),
-		wp.element.createElement('path', { d: 'M30 98v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39zM155 98l-30-33h20v-35h20v35h20z' })
-	),
+	icon: <svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="dashicon"
+			width="20"
+			height="20"
+			viewBox="0 0 208 128"
+			stroke="currentColor"
+		>
+			<rect
+				width="198"
+				height="118"
+				x="5"
+				y="5"
+				ry="10"
+				stroke-width="10"
+				fill="none"
+			/>
+			<path d="M30 98v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39zM155 98l-30-33h20v-35h20v35h20z" />
+		</svg>,
 
 	category: 'formatting',
 
@@ -67,8 +57,8 @@ registerBlockType('jetpack/markdown-block', {
 		source: { type: 'string' }
 	},
 
-	edit: _jetpackMarkdownBlockEditor2['default'],
+	edit: JetpackMarkdownBlockEditor,
 
-	save: _jetpackMarkdownBlockSave2['default']
+	save: JetpackMarkdownBlockSave,
 
-});
+} );
