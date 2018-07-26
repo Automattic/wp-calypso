18acb3ba7f39bd41789bc188239f048ba20a2b02
bluefuton <chris@bluefuton.com>
2018-07-26 12:59:03 +1200

    Implement polling

--- client/blocks/comments/index.jsx
+++ client/blocks/comments/index.jsx
@@ -1,8 +1,36 @@
 /** @format */
 
 /**
+ * External dependencies
+ */
+import React, { Fragment } from 'react';
+
+/**
  * Internal dependencies
  */
-import PostCommentsList from './post-comment-list.jsx';
+import PostCommentsList from './post-comment-list';
+import Interval, { EVERY_MINUTE } from 'lib/interval';
 
-export default PostCommentsList;
+class PostComments extends React.Component {
+	static defaultProps = {
+		shouldPollForNewComments: true,
+	};
+
+	pollForNewComments = () => {
+		alert( 'poll ' );
+		// request page of comments
+	};
+
+	render() {
+		return (
+			<Fragment>
+				{ this.props.shouldPollForNewComments && (
+					<Interval onTick={ this.pollForNewComments } period={ EVERY_MINUTE } />
+				) }
+				<PostCommentsList { ...this.props } />
+			</Fragment>
+		);
+	}
+}
+
+export default PostComments;
