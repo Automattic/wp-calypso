db61239d1dfe51cb5fd48ef21f15b06be670aa64
Michael Cain <michael@michaeldcain.com>
2018-07-27 11:17:52 -0400

    Add ExternalLink fallback support.

--- client/components/inline-support-link/README.md
+++ client/components/inline-support-link/README.md
@@ -1,7 +1,7 @@
 Inline Support Link
 ===================
 
-This component displays a link and icon. When clicked, it opens a SupportArticleDialog with an ExternalLink to the article if supplied.
+This component displays a link and icon. If `props.supportPostId` is supplied, when the link is clicked a SupportArticleDialog is opened with an `ExternalLink` to the `props.supportLink`. If no `props.supportPostId` is supplied, an `ExternalLink` to the `props.supportLink` is rendered.
 
 ## Example Usage:
 
@@ -10,7 +10,6 @@ import InlineSupportLink from 'components/inline-support-link';
 
 render() {
 	const inlineSupportProps = {
-		text: 'Learn more about Podcasting',
 		supportLink: 'https://en.support.wordpress.com/audio/podcasting/',
 		supportPostId: 38147,
 	};
@@ -23,7 +22,7 @@ render() {
 ## Props
 
 - `supportPostId` - (number) The postId of the support document.
-- `supportLink` - *optional* (string) The URL of the support document. If left out, no ExternalLink will be given.
+- `supportLink` - (string) The URL of the support document. If left out, no ExternalLink will be given.
 - `text` - *optional* (string) The link text. Default `Learn more`
 - `showText` - *optional* (bool) Whether or not to display the link text. Default `true`.
 - `showIcon` - *optional* (bool) Whether or not to display the "help-outline" Gridicon. Default `true`.
--- client/components/inline-support-link/index.jsx
+++ client/components/inline-support-link/index.jsx
@@ -12,11 +12,12 @@ import Gridicon from 'gridicons';
 /**
  * Internal dependencies
  */
+import ExternalLink from 'components/external-link';
 import { openSupportArticleDialog } from 'state/inline-support-article/actions';
 
 class InlineSupportLink extends Component {
 	static propTypes = {
-		supportPostId: PropTypes.number.isRequired,
+		supportPostId: PropTypes.number,
 		supportLink: PropTypes.string,
 		showText: PropTypes.bool,
 		text: PropTypes.string,
@@ -36,6 +37,10 @@ class InlineSupportLink extends Component {
 	handleClick = event => {
 		const { supportPostId, supportLink } = this.props;
 
+		if ( ! supportPostId ) {
+			return null;
+		}
+
 		event.preventDefault();
 		this.props.openSupportArticleDialog( { postId: supportPostId, postUrl: supportLink } );
 	};
@@ -51,21 +56,28 @@ class InlineSupportLink extends Component {
 			translate,
 		} = this.props;
 
-		if ( ! supportPostId ) {
+		if ( ! supportPostId && ! supportLink ) {
 			return null;
 		}
 
+		const LinkComponent = supportPostId ? 'a' : ExternalLink;
+		const externalLinkProps = ! supportPostId && {
+			icon: showIcon,
+			iconSize,
+		};
+
 		return (
-			<a
+			<LinkComponent
 				className="inline-support-link"
 				href={ supportLink }
 				onClick={ this.handleClick }
 				target="_blank"
 				rel="noopener noreferrer"
+				{ ...externalLinkProps }
 			>
 				{ showText && ( text || translate( 'Learn more' ) ) }
-				{ showIcon && <Gridicon icon="help-outline" size={ iconSize } /> }
-			</a>
+				{ supportPostId && showIcon && <Gridicon icon="help-outline" size={ iconSize } /> }
+			</LinkComponent>
 		);
 	}
 }
