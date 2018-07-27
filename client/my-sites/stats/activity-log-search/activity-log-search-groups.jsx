f7370a5b59183f8e7bdf9277862b27523d0f4f69
Rocco Tripaldi <tripaldir@gmail.com>
2018-07-27 16:21:22 -0400

    Activity Log: add basic functionality to group filters

--- client/my-sites/stats/activity-log-search/activity-log-search-groups.jsx
+++ client/my-sites/stats/activity-log-search/activity-log-search-groups.jsx
@@ -3,15 +3,18 @@
 /**
  * External dependencies
  */
+import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
+import { pull, union } from 'lodash';
 import React, { Component } from 'react';
 
 /**
  * Internal dependencies
  */
-import { updateFilter } from 'state/activity-log/actions';
+import getActivityLogFilter from 'state/selectors/get-activity-log-filter';
 import Gridicon from 'gridicons';
+import { updateFilter } from 'state/activity-log/actions';
 
 // Note: this data-set will eventually come from the API
 const groups = [
@@ -24,16 +27,69 @@ const groups = [
 ];
 
 class ActivityLogSearchGroups extends Component {
+	onGroupClick = event => {
+		const { filter, siteId } = this.props;
+		const currentState = event.target.getAttribute( 'class' );
+		const group = event.target.getAttribute( 'data-group' );
+		event.preventDefault;
+
+		if ( 'included' === currentState ) {
+			this.props.updateFilter(
+				siteId,
+				Object.assign(
+					{},
+					filter,
+					filter.group.length > 1 ? { group: pull( filter.group, group ) } : { group: null },
+					filter.notGroup
+						? { notGroup: union( filter.notGroup, [ group ] ) }
+						: { notGroup: [ group ] }
+				)
+			);
+			return;
+		}
+
+		if ( 'excluded' === currentState ) {
+			this.props.updateFilter(
+				siteId,
+				Object.assign(
+					{},
+					filter,
+					filter.notGroup.length > 1
+						? { notGroup: pull( filter.notGroup, group ) }
+						: { notGroup: null }
+				)
+			);
+			return;
+		}
+
+		this.props.updateFilter(
+			siteId,
+			Object.assign(
+				{},
+				filter,
+				filter.group ? { group: union( filter.group, [ group ] ) } : { group: [ group ] }
+			)
+		);
+	};
+
 	render() {
+		const { filter } = this.props;
 		return (
 			<div className="activity-log-search__filters-groups">
 				{ groups.map( group => (
-					<button key={ group.slug }>
+					<button
+						data-group={ group.slug }
+						onClick={ this.onGroupClick }
+						className={ classNames( {
+							included: filter.group && filter.group.includes( group.slug ),
+							excluded: filter.notGroup && filter.notGroup.includes( group.slug ),
+						} ) }
+						key={ group.slug }
+					>
 						<Gridicon
 							icon={ group.icon }
 							className="activity-log-search__filter-group-icon"
 							size={ 18 }
-							data-group={ group.slug }
 						/>
 						<span>{ group.title }</span>
 					</button>
@@ -44,6 +100,10 @@ class ActivityLogSearchGroups extends Component {
 }
 
 export default connect(
-	null,
+	( state, ownProps ) => {
+		return {
+			filter: getActivityLogFilter( state, ownProps.siteId ),
+		};
+	},
 	{ updateFilter }
 )( localize( ActivityLogSearchGroups ) );
