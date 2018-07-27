f9aeb823a5a3f9f24a86582c11afda2ee1620191
Miguel Torres <miguelmariatorresrojas@gmail.com>
2018-07-27 23:51:51 +0200

    DevDocs: Gutenberg Components - ClipboardButton example

---
+++ client/gutenberg-components/clipboard-button/docs/example.jsx
@@ -0,0 +1,29 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { ClipboardButton } from '@wordpress/components';
+
+ClipboardButton.displayName = 'ClipboardButton';
+
+export default class extends React.Component {
+	static displayName = 'ClipboardButton';
+
+	state = {
+		hasCopied: false,
+	};
+
+	render() {
+		return (
+			<ClipboardButton
+				text="WordPress"
+				onCopy={ () => this.setState( { hasCopied: true } ) }
+				onFinishCopy={ () => this.setState( { hasCopied: false } ) }
+			>
+				{ this.state.hasCopied ? 'Copied!' : 'Copy Text' }
+			</ClipboardButton>
+		);
+	}
+}
