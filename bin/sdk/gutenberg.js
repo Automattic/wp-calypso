1be7405d099fedf40d1cfd9ebee18eaecee5071c
Dennis Snell <dennis.snell@automattic.com>
2018-07-27 15:25:10 +0200

    SDK: Rename some things for better goal alignment (#26329)
    Rename some things for better goal alignment.
    
    Renames `bin` and `npm` scripts:
      - `npx calypso-gutenberg-sdk build-block` → `npx calypso-sdk gutenberg`
      - `npm run build-block` → `npm run sdk:gutenberg`

--- bin/sdk/gutenberg.js
+++ bin/sdk/gutenberg.js
@@ -4,12 +4,14 @@
 const path = require( 'path' );
 const webpack = require( 'webpack' );
 
+const [ /* node */, /* this-script */, givenEditorScript, givenOutputDir ] = process.argv;
+
 const __rootDir = path.resolve( __dirname, '../../' );
 const CopyWebpackPlugin = require( path.resolve( __rootDir, 'server/bundler/copy-webpack-plugin' ) );
-const entryPath = path.resolve( __rootDir, process.argv[ 2 ] );
-const sourceDir = path.dirname( entryPath );
-const outputDir = process.argv[ 3 ] ? process.argv[ 3 ] : path.join( sourceDir, 'build' );
-const blockName = path.basename( path.dirname( entryPath ) );
+const editorScript = path.resolve( __rootDir, givenEditorScript );
+const sourceDir = path.dirname( editorScript );
+const name = path.basename( sourceDir.replace( /\/$/, '' ) );
+const outputDir = givenOutputDir ? givenOutputDir : path.join( sourceDir, 'build' );
 
 const baseConfig = require( path.join( __rootDir, 'webpack.config.js' ) );
 
@@ -18,7 +20,9 @@ const config = {
 	...{
 		context: __rootDir,
 		mode: 'production',
-		entry: entryPath,
+		entry: {
+			[ `${ name }-editor-script` ]: editorScript,
+		},
 		externals: {
 			...baseConfig.externals,
 			wp: 'wp',
@@ -28,9 +32,9 @@ const config = {
 		},
 		output: {
 			path: outputDir,
-			filename: `blocks-${ blockName }.js`,
+			filename: `${ name }-editor.js`,
 			libraryTarget: 'window',
-			library: `blocks-${ blockName }`,
+			library: `plugins-${ name }-editor`,
 		},
 		plugins: [
 			...baseConfig.plugins.filter( plugin => ! ( plugin instanceof CopyWebpackPlugin ) ),
