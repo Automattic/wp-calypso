0381dc931b5625b102d5a27ed36aad5d0c6c93ec
Mikael Korpela <mikael@ihminen.org>
2018-07-27 21:41:01 +0300

    SDK: Add tiled gallery block (#26223)
    Add tiled gallery Gutenberg block
    
    Based on @haszari's work in:
    https://github.com/Automattic/jetpack/pull/9903 &
    https://github.com/Automattic/jetpack/pull/9434
    
    Contains some modifications on top of their work to make this function with Gutenberg v3.3 and with Calypso's linters.
    
    Co-authored-by: haszari <haszari@cartoonbeats.com>

---
+++ client/gutenberg/extensions/tiled-gallery/save.js
@@ -0,0 +1,12 @@
+/**
+ * Internal dependencies
+ */
+import TiledGalleryLayoutSquare from './components/tiled-gallery-layout-square.js';
+
+function JetpackGalleryBlockSave( { attributes } ) {
+	return (
+		<TiledGalleryLayoutSquare { ...attributes } />
+	);
+}
+
+export default JetpackGalleryBlockSave;
