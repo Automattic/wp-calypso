4df965d66e7717da54a75445a46492c49d4f0e28
Radoslav Georgiev <rageorgiev@gmail.com>
2018-07-30 17:02:21 +0300

    Adding tests for the WooCommerce Services shipping-classes actions.

---
+++ client/extensions/woocommerce/woocommerce-services/state/shipping-classes/test/actions.js
@@ -0,0 +1,88 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import {
+	WOOCOMMERCE_SERVICES_SHIPPING_CLASSES_REQUEST,
+	WOOCOMMERCE_SERVICES_SHIPPING_CLASSES_REQUEST_SUCCESS,
+} from '../../action-types';
+import {
+	fetchShippingClassesSuccess,
+	fetchShippingClassesFailure,
+	fetchShippingClassesIfNotLoaded,
+	fetchShippingClasses,
+} from '../actions';
+import initialShippingClasses from './data/initial-state';
+
+const siteId = 123;
+
+const dispatchFn = action => action;
+const getState = ( shippingClasses = initialShippingClasses ) => () => ( {
+	extensions: {
+		woocommerce: {
+			woocommerceServices: {
+				[ siteId ]: {
+					shippingClasses,
+				},
+			},
+		},
+	},
+} );
+
+describe( 'Shipping classes state actions', () => {
+	/**
+	 * fetchShippingClassesSuccess
+	 */
+
+	test( '#fetchShippingClassesSuccess', () => {
+		expect( fetchShippingClassesSuccess( siteId, initialShippingClasses ) ).to.eql( {
+			type: WOOCOMMERCE_SERVICES_SHIPPING_CLASSES_REQUEST_SUCCESS,
+			siteId,
+			data: initialShippingClasses,
+		} );
+	} );
+
+	/**
+	 * fetchShippingClassesFailure
+	 */
+
+	test( '#fetchShippingClassesFailure', () => {
+		const result = fetchShippingClassesFailure( siteId );
+
+		expect( result ).to.be.an( 'object' );
+		expect( result.type ).to.equal( 'NOTICE_CREATE' );
+		expect( result.notice ).to.be.an( 'object' );
+	} );
+
+	/**
+	 * fetchShippingClassesIfNotLoaded
+	 */
+
+	test( '#fetchShippingClassesIfNotLoaded', () => {
+		expect( fetchShippingClassesIfNotLoaded( siteId ) ).to.eql( {
+			type: WOOCOMMERCE_SERVICES_SHIPPING_CLASSES_REQUEST,
+			siteId,
+		} );
+	} );
+
+	/**
+	 * fetchShippingClasses
+	 */
+
+	test( '#fetchShippingClasses', () => {
+		expect( fetchShippingClasses( siteId )( dispatchFn, getState( false ) ) ).to.eql( {
+			type: WOOCOMMERCE_SERVICES_SHIPPING_CLASSES_REQUEST,
+			siteId,
+		} );
+	} );
+
+	test( '#fetchShippingClasses when classes have been already loaded', () => {
+		expect( fetchShippingClasses( siteId )( dispatchFn, getState() ) ).to.equal( undefined );
+	} );
+} );
