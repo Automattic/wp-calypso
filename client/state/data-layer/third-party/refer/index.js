2bc57fbcfe1dd4cc3cac824f3c71415ba87ff339
Jarda Snajdr <jsnajdr@gmail.com>
2018-07-31 16:11:29 +0200

    Migrate 'third-party' data layer handlers (refer and directly)
    This PR does a few related migrations:
    
    The `refer` data layer handler is migrated to `dispatchRequestEx`. It records a referral
    to `refer.wordpress.com` when signup URL has `aff` and `cid` parameters.
    
    The `refer` and `directly` handlers are both migrated to `registerHandlers`, so that they are moved
    from `build` chunk to the chunks that use them: Signup and Inline Help.

--- client/state/data-layer/third-party/refer/index.js
+++ client/state/data-layer/third-party/refer/index.js
@@ -3,54 +3,46 @@
 /**
  * External dependencies
  */
-
-import debug from 'debug';
-import noop from 'lodash';
+import { noop } from 'lodash';
 
 /**
  * Internal dependencies
  */
+import { registerHandlers } from 'state/data-layer/handler-registry';
+import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
 import { http } from 'state/http/actions';
-import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
-
-const aDebug = debug( 'calypso:analytics:affiliate' );
-
 import { AFFILIATE_REFERRAL } from 'state/action-types';
 
-const trackAffiliatePageLoad = ( { dispatch }, action ) => {
+const trackAffiliatePageLoad = action => {
 	const { affiliateId, campaignId, urlPath } = action;
 
 	if ( ! affiliateId || isNaN( affiliateId ) ) {
-		return;
+		return null;
 	}
 
-	aDebug( 'affiliate referrer request', action );
-
-	dispatch(
-		http(
-			{
-				method: 'POST',
-				url: 'https://refer.wordpress.com/clicks/67402',
-				headers: [ [ 'content-type', 'application/x-www-form-urlencoded; charset=UTF-8' ] ],
-				body: {
-					affiliate_id: affiliateId,
-					campaign_id: campaignId || '',
-					referrer: urlPath,
-				},
-				// Needed to check and set the 'wp-affiliate-tracker' cookie
-				withCredentials: true,
+	return http(
+		{
+			method: 'POST',
+			url: 'https://refer.wordpress.com/clicks/67402',
+			headers: [ [ 'content-type', 'application/x-www-form-urlencoded; charset=UTF-8' ] ],
+			body: {
+				affiliate_id: affiliateId,
+				campaign_id: campaignId || '',
+				referrer: urlPath,
 			},
-			action
-		)
+			// Needed to check and set the 'wp-affiliate-tracker' cookie
+			withCredentials: true,
+		},
+		action
 	);
 };
 
-const trackAffiliatePageLoadSuccess = ( store, action, rData ) => {
-	aDebug( 'affiliate referrer request successful', rData );
-};
-
-export default {
+registerHandlers( 'state/data-layer/third-party/refer', {
 	[ AFFILIATE_REFERRAL ]: [
-		dispatchRequest( trackAffiliatePageLoad, trackAffiliatePageLoadSuccess, noop ),
+		dispatchRequestEx( {
+			fetch: trackAffiliatePageLoad,
+			onSuccess: noop,
+			onError: noop,
+		} ),
 	],
-};
+} );
