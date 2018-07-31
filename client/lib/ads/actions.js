aef3bc057de488b5b45c06325f7ec49f9ca7fe06
Ben Lowery <blowery@users.noreply.github.com>
2018-07-31 12:22:54 -0400

    Remove the TOS Store (#26130)
    It's apparently unused. Remove the store, associated tests, and README references.

--- client/lib/ads/actions.js
+++ client/lib/ads/actions.js
@@ -61,50 +61,6 @@ const WordadsActions = {
 				} );
 		}
 	},
-
-	fetchTos: site => {
-		debug( 'WordAds Actions fetchTos', site );
-		Dispatcher.handleViewAction( {
-			type: 'FETCHING_WORDADS_TOS',
-			site: site,
-		} );
-
-		wpcom
-			.site( site.ID )
-			.wordAds()
-			.tos()
-			.get( ( error, data ) => {
-				Dispatcher.handleServerAction( {
-					type: 'RECEIVE_WORDADS_TOS',
-					site: site,
-					error: error,
-					data: data,
-				} );
-			} );
-	},
-
-	signTos: site => {
-		debug( 'WordAds Actions signTos', site );
-		if ( site.capabilities && site.capabilities.manage_options ) {
-			Dispatcher.handleViewAction( {
-				type: 'SIGNING_WORDADS_TOS',
-				site: site,
-			} );
-
-			wpcom
-				.site( site.ID )
-				.wordAds()
-				.tos()
-				.sign( ( error, data ) => {
-					Dispatcher.handleServerAction( {
-						type: 'SIGN_WORDADS_TOS',
-						site: site,
-						error: error,
-						data: data,
-					} );
-				} );
-		}
-	},
 };
 
 export default WordadsActions;
