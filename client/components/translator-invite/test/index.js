eca48212fc1372d2753882accdef9a342df03a28
Ramon <ramonjd@users.noreply.github.com>
2018-08-02 10:51:01 +1000

    i18n: Invite users to contribute to WordPress.com translations (#25268)
    * Initial commit. This component, used for now on login, invites users whose locales are non-default, to contribute to WordPress translations.
    
    * Removing translate call for and leaving in English.
    
    * Removing extra space in render block
    Removing useless object assignment
    
    * Moving translator invite below the form
    
    * Moving the nudge to underneath the login page and only showing on main login view. Updated tests. Translating nudge content.
    
    * Moving getLocale utility function into a utils file
    Correcting `isLoginView` condition to return true when non-landing page detected
    Adding .com to WordPress :)
    Updating tests
    
    * Updated README to reflect change in locale prop. We check the ui locale in the component now in order to run a non-EN check over it, better for portability.
    
    * Relocated module var comment.

---
+++ client/components/translator-invite/test/index.js
@@ -0,0 +1,53 @@
+/**
+ * @format
+ * @jest-environment jsdom
+ */
+
+/**
+ * External dependencies
+ */
+import { shallow } from 'enzyme';
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { TranslatorInvite } from '../';
+
+describe( 'TranslatorInvite', () => {
+	const defaultProps = {
+		translate: x => x,
+		localizedLanguageNames: {
+			'en-gb': {
+				localized: 'British English',
+				name: 'English (UK)',
+				en: 'British English',
+			},
+			mt: {
+				localized: 'Maltese',
+				name: 'Malti',
+				en: 'Maltese',
+			},
+			uk: {
+				localized: 'Ukrainian',
+				name: 'Українська',
+				en: 'Ukrainian',
+			},
+			tl: {
+				localized: 'Filipino',
+				name: 'Tagalog',
+				en: 'Filipino',
+			},
+		},
+	};
+
+	test( 'should not render when no locale information present', () => {
+		const wrapper = shallow( <TranslatorInvite { ...defaultProps } /> );
+		expect( wrapper.find( '.translator-invite__content' ) ).toHaveLength( 0 );
+	} );
+
+	test( 'should render when no locale information present', () => {
+		const wrapper = shallow( <TranslatorInvite { ...defaultProps } locale="tl" /> );
+		expect( wrapper.find( '.translator-invite__content' ) ).toHaveLength( 1 );
+	} );
+} );
---
+++ client/components/translator-invite/utils.js
@@ -0,0 +1,65 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { startsWith } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import config from 'config';
+import { getLanguage, getLocaleFromPath } from 'lib/i18n-utils';
+
+/**
+ * Module variables
+ */
+const defaultLanguageSlug = config( 'i18n_default_locale_slug' );
+
+/**
+ * Checks a locale against the default. Returns true if the
+ * locale's prefix matches the default locale. E.g., a locale of en-AU
+ * starts with 'en', the defaultLanguageSlug.
+ *
+ * @param {String} locale An language slug
+ * @returns {Boolean} The locale slug of the language, if any found.
+ */
+export function isDefaultLocale( locale ) {
+	return startsWith( locale, defaultLanguageSlug );
+}
+
+/**
+ * Checks incoming locale then urlPath for a valid non-default locale.
+ * If no matching locale exists, we look in the user's browser preferences.
+ *
+ * @param {String} locale The language slug of an existing locale
+ * @param {String} urlPath Current path
+ * @returns {String|Null} The locale slug of the language, if any found.
+ */
+export function getCurrentNonDefaultLocale( locale, urlPath ) {
+	// First try the locale passed as props.
+	let languageLocale = ! isDefaultLocale( locale ) ? locale : null;
+
+	// Then the locale in the path, if any.
+	if ( ! languageLocale && urlPath ) {
+		languageLocale = getLocaleFromPath( urlPath );
+		languageLocale = ! isDefaultLocale( languageLocale ) ? languageLocale : null;
+	}
+
+	// Then navigator.languages.
+	if ( ! languageLocale && typeof navigator === 'object' && 'languages' in navigator ) {
+		for ( const langSlug of navigator.languages ) {
+			const language = getLanguage( langSlug.toLowerCase() );
+			if ( language && ! isDefaultLocale( language.langSlug ) ) {
+				languageLocale = language.langSlug;
+				break;
+			}
+		}
+	}
+
+	if ( languageLocale ) {
+		return languageLocale;
+	}
+
+	return null;
+}
