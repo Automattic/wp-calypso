9647de812baa2edc177f213b3a3cb25b3372fb2a
Ben Lowery <ben.lowery@automattic.com>
2018-08-02 09:11:41 -0400

    use the [request] placeholder to give moment locales better names

--- client/state/i18n/middleware.js
+++ client/state/i18n/middleware.js
@@ -12,7 +12,7 @@ function updateMoment( action ) {
 	if ( action && action.type === LOCALE_SET && action.localeSlug !== 'en' ) {
 		// load moment and update the locale
 		import( 'moment' ).then( moment => {
-			import( /* webpackChunkName: "moment-locale-" */ /* webpackInclude: /\.js$/ */
+			import( /* webpackChunkName: "moment-locale-[request]", webpackInclude: /\.js$/ */
 			`moment/locale/${ action.localeSlug }` ).then( () => {
 				moment.locale( action.localeSlug );
 			} );
