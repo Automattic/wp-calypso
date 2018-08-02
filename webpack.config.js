53b53f447261f20df02226b426fe6d393b138354
Miguel Torres <miguel.torres@ebury.com>
2018-08-02 12:32:18 +0200

    Update @wordpress/components. jQuery is no longer needed in the build

--- webpack.config.js
+++ webpack.config.js
@@ -262,8 +262,7 @@ if ( calypsoEnv === 'desktop' ) {
 } else {
 	// jquery is only needed in the build for the desktop app
 	// see electron bug: https://github.com/atom/electron/issues/254
-	// uncomment the next line once @wordpress/api-fetch doesn't require jQuery
-	// webpackConfig.externals.push( 'jquery' );
+	webpackConfig.externals.push( 'jquery' );
 }
 
 if ( isDevelopment ) {
