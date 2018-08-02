598db14b2df9ee4aded6467c053f48480fbb9cb4
Marko Andrijasevic <marko.andrijasevic@automattic.com>
2018-08-02 20:33:30 +0200

    Gutenberg: include @wordpress/api-fetch package (#26426)
    Import @wordpress/api-fetch package and carry over existing tests from Gutenberg repo.

--- client/gutenberg/editor/test/index.js
+++ client/gutenberg/editor/test/index.js
@@ -21,6 +21,8 @@ import {
 
 import { createBlock, registerBlockType } from '@wordpress/blocks';
 
+import apiFetch from '@wordpress/api-fetch';
+
 describe( 'Gutenberg @wordress/data package', () => {
 	describe( 'registerStore', () => {
 		it( 'should be shorthand for reducer, actions, selectors registration', () => {
@@ -145,6 +147,117 @@ describe( 'Gutenberg @wordress/blocks package', () => {
 			expect( block.innerBlocks ).toHaveLength( 1 );
 			expect( block.innerBlocks[ 0 ].name ).toBe( 'core/test-block' );
 			expect( typeof block.clientId ).toBe( 'string' );
+		} );
+	} );
+} );
+
+describe( 'Gutenberg @wordress/api-fetch package', () => {
+	// Try tests from https://github.com/WordPress/gutenberg/blob/54990c0f77294e6603cc4e0aa957c0679c1bf242/packages/api-fetch/src/test/index.js
+	describe( 'apiFetch', () => {
+		const originalFetch = window.fetch;
+		beforeAll( () => {
+			window.fetch = jest.fn();
+		} );
+
+		afterAll( () => {
+			window.fetch = originalFetch;
+		} );
+
+		it( 'should call the API propertly', () => {
+			window.fetch.mockReturnValue(
+				Promise.resolve( {
+					status: 200,
+					json() {
+						return Promise.resolve( { message: 'ok' } );
+					},
+				} )
+			);
+
+			return apiFetch( { path: '/random' } ).then( body => {
+				expect( body ).toEqual( { message: 'ok' } );
+			} );
+		} );
+
+		it( 'should return the error message properly', () => {
+			window.fetch.mockReturnValue(
+				Promise.resolve( {
+					status: 400,
+					json() {
+						return Promise.resolve( {
+							code: 'bad_request',
+							message: 'Bad Request',
+						} );
+					},
+				} )
+			);
+
+			return apiFetch( { path: '/random' } ).catch( body => {
+				expect( body ).toEqual( {
+					code: 'bad_request',
+					message: 'Bad Request',
+				} );
+			} );
+		} );
+
+		it( 'should return invalid JSON error if no json response', () => {
+			window.fetch.mockReturnValue(
+				Promise.resolve( {
+					status: 200,
+				} )
+			);
+
+			return apiFetch( { path: '/random' } ).catch( body => {
+				expect( body ).toEqual( {
+					code: 'invalid_json',
+					message: 'The response is not a valid JSON response.',
+				} );
+			} );
+		} );
+
+		it( 'should return invalid JSON error if response is not valid', () => {
+			window.fetch.mockReturnValue(
+				Promise.resolve( {
+					status: 200,
+					json() {
+						return Promise.reject();
+					},
+				} )
+			);
+
+			return apiFetch( { path: '/random' } ).catch( body => {
+				expect( body ).toEqual( {
+					code: 'invalid_json',
+					message: 'The response is not a valid JSON response.',
+				} );
+			} );
+		} );
+
+		it( 'should not try to parse the response', () => {
+			window.fetch.mockReturnValue(
+				Promise.resolve( {
+					status: 200,
+				} )
+			);
+
+			return apiFetch( { path: '/random', parse: false } ).then( response => {
+				expect( response ).toEqual( {
+					status: 200,
+				} );
+			} );
+		} );
+
+		it( 'should not try to parse the error', () => {
+			window.fetch.mockReturnValue(
+				Promise.resolve( {
+					status: 400,
+				} )
+			);
+
+			return apiFetch( { path: '/random', parse: false } ).catch( response => {
+				expect( response ).toEqual( {
+					status: 400,
+				} );
+			} );
 		} );
 	} );
 } );
