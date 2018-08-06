8bd5942687764d6a4d8b6d9b162aac3bde865d0f
Jeff Bowen <jeff@automattic.com>
2018-08-06 02:13:05 -0400

    exit to checklist

--- client/signup/config/flows-pure.js
+++ client/signup/config/flows-pure.js
@@ -274,7 +274,7 @@ export function generateFlows( { getSiteDestination = noop, getPostsDestination 
 	if ( config.isEnabled( 'signup/import-landing-handler' ) ) {
 		flows[ 'from-site' ] = {
 			steps: [ 'import-from-url' ],
-			destination: getSiteDestination,
+			destination: () => '/checklist',
 			description: 'A flow to kick off an import during signup',
 			disallowResume: true,
 			allowContinue: false,
