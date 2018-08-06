478bc8295ff651a8a340dc899833ca413dbd418d
Bernie Reiter <ockham@raz.or.at>
2018-08-06 17:28:10 +0200

    Remove Idioticon

--- client/gutenberg/extensions/presets/a8c/editor.js
+++ client/gutenberg/extensions/presets/a8c/editor.js
@@ -3,7 +3,6 @@
 /**
  * Internal dependencies
  */
-import 'gutenberg/extensions/idioticon/index.js';
 import 'gutenberg/extensions/prev-next/block.js';
 import 'gutenberg/extensions/todo/block.js';
 import 'gutenberg/extensions/trials/block.js';
