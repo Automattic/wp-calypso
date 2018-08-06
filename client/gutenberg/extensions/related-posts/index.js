57715413906c08e4f4fb934a7e69a4bfe9889e76
Marin Atanasov <tyxla@abv.bg>
2018-08-06 15:19:45 +0300

    Specify registerBlockType as module variable

--- client/gutenberg/extensions/related-posts/index.js
+++ client/gutenberg/extensions/related-posts/index.js
@@ -1,10 +1,16 @@
 /** @format */
+
 /**
  * External dependencies
  */
 import wp from 'wp';
 
-wp.blocks.registerBlockType( 'jetpack/related-posts', {
+/**
+ * Module variables
+ */
+const { registerBlockType } = wp.blocks;
+
+registerBlockType( 'jetpack/related-posts', {
 	title: 'Related Posts',
 	icon: 'admin-post',
 	category: 'layout',
