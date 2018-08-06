Automattic/wp-calypso

commit 72318e15e09f5196c249b04de5b05743cff6660d

Use memoization to avoid re-renders in ReaderCombinedCardComponent. (#26499)
* Use memoization to avoid re-renders in ReaderCombinedCardComponent.

Considerable time is being spent re-rendering instances of this
component both when loading a page that uses them, as well as
when navigating away from one.

This PR uses memoization in order to ensure that the existing
post keys are kept, rather than re-generated every time. This
means that we avoid unnecessary re-renders for this component and
everything below it in the render tree.

This PR, when tested in a new account, improves responsiveness
when navigating from "followed sites" to "search" by around 300ms
(before: 857, after: 538) on my machine.

* Make ReaderCombinedCardComponent impure.

Redux already generates a pure component for us with `connect`.

* Simplify recordRenderTrack in ReaderCombinedCardComponent.

* Remove custom memoization in `ReaderCombinedCardComponent`.

Replaced with `createSelector` instead.

client/blocks/reader-combined-card/index.jsx
@@ -25,6 +25,7 @@ import FollowButton from 'reader/follow-button';
 import { getPostsByKeys } from 'state/reader/posts/selectors';
 import ReaderPostOptionsMenu from 'blocks/reader-post-options-menu';
 import PostBlocked from 'blocks/reader-post-card/blocked';
+import createSelector from 'lib/create-selector';
 
 class ReaderCombinedCardComponent extends React.Component {
 	static propTypes = {
@@ -50,18 +51,18 @@ class ReaderCombinedCardComponent extends React.Component {
 		this.recordRenderTrack();
 	}
 
-	componentWillReceiveProps( nextProps ) {
+	componentDidUpdate( prevProps ) {
 		if (
-			this.props.postKey.feedId !== nextProps.postKey.feedId ||
-			this.props.postKey.blogId !== nextProps.postKey.blogId ||
-			size( this.props.posts ) !== size( nextProps.posts )
+			this.props.postKey.feedId !== prevProps.postKey.feedId ||
+			this.props.postKey.blogId !== prevProps.postKey.blogId ||
+			size( this.props.posts ) !== size( prevProps.posts )
 		) {
-			this.recordRenderTrack( nextProps );
+			this.recordRenderTrack();
 		}
 	}
 
-	recordRenderTrack = ( props = this.props ) => {
-		const { postKey, posts } = props;
+	recordRenderTrack = () => {
+		const { postKey, posts } = this.props;
 
 		recordTrack( 'calypso_reader_combined_card_render', {
 			blog_id: postKey.blogId,
@@ -168,18 +169,24 @@ export function combinedCardPostKeyToKeys( postKey ) {
 		return [];
 	}
 
-	const feedId = postKey.feedId;
-	const blogId = postKey.blogId;
+	const { feedId, blogId } = postKey;
+
 	return postKey.postIds.map( postId => ( { feedId, blogId, postId } ) );
 }
 
 export const ReaderCombinedCard = localize( ReaderCombinedCardComponent );
 
-export default connect( ( state, ownProps ) => {
-	const postKeys = combinedCardPostKeyToKeys( ownProps.postKey );
+const getPostData = createSelector(
+	( state, postKey ) => {
+		const postKeys = combinedCardPostKeyToKeys( postKey );
+		return {
+			posts: getPostsByKeys( state, postKeys ),
+			postKeys,
+		};
+	},
+	state => [ state.posts ]
+);
 
-	return {
-		posts: getPostsByKeys( state, postKeys ),
-		postKeys,
-	};
-} )( ReaderCombinedCard );
+export default connect( ( state, { siteId } ) => getPostData( state, siteId ) )(
+	ReaderCombinedCard
+);

commit 3a2b8502faf5b487b6a75512adccc2f3853b39b3

Fix a logic error introduced in #26499.

client/blocks/reader-combined-card/index.jsx
@@ -187,6 +187,6 @@ const getPostData = createSelector(
 	state => [ state.posts ]
 );
 
-export default connect( ( state, { siteId } ) => getPostData( state, siteId ) )(
+export default connect( ( state, { postKey } ) => getPostData( state, postKey ) )(
 	ReaderCombinedCard
 );