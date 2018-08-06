bae20241b0d65b4402baedb1aca4be0588b551d7
Marin Atanasov <tyxla@abv.bg>
2018-08-06 15:57:43 +0300

    Add a number of posts inspector control

--- client/gutenberg/extensions/related-posts/index.js
+++ client/gutenberg/extensions/related-posts/index.js
@@ -6,14 +6,53 @@
 import wp from 'wp';
 
 /**
+ * WordPress dependencies
+ */
+const { __ } = wp.i18n;
+const { Fragment } = wp.element;
+const { InspectorControls } = wp.editor;
+const { PanelBody, RangeControl } = wp.components;
+const { registerBlockType } = wp.blocks;
+
+/**
  * Module variables
  */
-const { registerBlockType } = wp.blocks;
+const MAX_POSTS_TO_SHOW = 3;
 
 registerBlockType( 'jetpack/related-posts', {
 	title: 'Related Posts',
+
 	icon: 'admin-post',
+
 	category: 'layout',
-	edit: () => <p>Editing related posts</p>,
-	save: () => <p>Saving related posts</p>,
+
+	attributes: {
+		postsToShow: {
+			type: 'number',
+			default: 3,
+		},
+	},
+
+	edit: ( { attributes, setAttributes } ) => {
+		const { postsToShow } = attributes;
+
+		return (
+			<Fragment>
+				<InspectorControls>
+					<PanelBody title={ __( 'Related Posts Settings' ) }>
+						<RangeControl
+							label={ __( 'Number of posts' ) }
+							value={ postsToShow }
+							onChange={ value => setAttributes( { postsToShow: value } ) }
+							min={ 1 }
+							max={ MAX_POSTS_TO_SHOW }
+						/>
+					</PanelBody>
+				</InspectorControls>
+
+				<div>List of posts here</div>
+			</Fragment>
+		);
+	},
+	save: () => null,
 } );
