c4085900f8cb31ff9df86ad5664a829ebca8f56b
Jon Surrell <jon.surrell@automattic.com>
2018-08-07 11:07:07 -0400

    Signup: Eslint fixes (componentWillMount, a11y) (#26516)
    * Use instance property for initial state
    
    Removes deprecated `componentWillMount` usage.
    
    * Add empty alt for non-semantic, decorative img

--- client/signup/processing-screen/index.jsx
+++ client/signup/processing-screen/index.jsx
@@ -33,12 +33,10 @@ export class SignupProcessingScreen extends Component {
 		useOAuth2Layout: PropTypes.bool.isRequired,
 	};
 
-	componentWillMount() {
-		this.setState( {
-			siteSlug: '',
-			hasPaidSubscription: false,
-		} );
-	}
+	state = {
+		siteSlug: '',
+		hasPaidSubscription: false,
+	};
 
 	componentWillReceiveProps( nextProps ) {
 		const dependencies = nextProps.signupDependencies;
@@ -259,6 +257,7 @@ export class SignupProcessingScreen extends Component {
 
 				<div className="signup-processing__content">
 					<img
+						alt=""
 						src="/calypso/images/signup/confetti.svg"
 						className="signup-process-screen__confetti"
 					/>
