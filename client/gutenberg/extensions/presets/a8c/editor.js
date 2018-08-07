cddc2e637b176232820f914d4418ff34f5bfa24c
Bernie Reiter <ockham@raz.or.at>
2018-08-07 11:43:53 +0200

    Move blocks into p2 preset

--- client/gutenberg/extensions/presets/a8c/editor.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-/**
- * Internal dependencies
- */
-import 'gutenberg/extensions/prev-next/block.js';
-import 'gutenberg/extensions/todo/block.js';
-import 'gutenberg/extensions/trials/block.js';
-import 'gutenberg/extensions/trial/block.js';
--- client/gutenberg/extensions/presets/p2/editor.js
+++ client/gutenberg/extensions/presets/p2/editor.js
@@ -4,3 +4,7 @@
  * Internal dependencies
  */
 import 'gutenberg/extensions/editor-notes/index.js';
+import 'gutenberg/extensions/prev-next/block.js';
+import 'gutenberg/extensions/todo/block.js';
+import 'gutenberg/extensions/trials/block.js';
+import 'gutenberg/extensions/trial/block.js';
