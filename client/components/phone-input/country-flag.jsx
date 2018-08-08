3b3d7b86192ffa84ec41840e381e132b2c6fadaa
Dennis Snell <dennis.snell@automattic.com>
2018-08-08 19:06:02 +0200

    feedback updates

--- client/components/phone-input/country-flag.jsx
+++ client/components/phone-input/country-flag.jsx
@@ -22,28 +22,35 @@ export default class extends Component {
 	};
 
 	componentDidMount() {
-		this.updateFlag( null, this.props.countryCode );
+		this.updateFlag();
 	}
 
 	componentDidUpdate( prevProps ) {
-		this.updateFlag( prevProps.countryCode, this.props.countryCode );
+		if ( prevProps.countryCode !== this.props.countryCode ) {
+			this.updateFlag();
+		}
 	}
 
-	updateFlag = ( prevCode, nextCode ) => {
-		if ( prevCode !== nextCode ) {
-			// don't confusingly split this import statement with its long string
+	updateFlag = () => {
+		this.setState( { flagSvg: null }, () => {
+			// don't confusingly split the import statement with its long string
+			// note that webpack will parse this code at compile-time and auto-generate
+			// a context for loading the SVGs. it will infer the file glob for all SVG
+			// files in the given path and should make a separate bundle for each one
+			//
+			// also, the "noquotes" part of the loader tells it that we're consuming
+			// the SVG data-uri as a JavaScript value and not as a string
+			// see the docs for `svg-url-loader` for more information
+			//
 			// prettier-ignore
 			import(
 				/* webpackChunkName: "svg-flag-[request]" */
-				'svg-url-loader?noquotes!../../../node_modules/flag-icon-css/flags/4x3/' + nextCode + '.svg'
+				'svg-url-loader?noquotes!../../../node_modules/flag-icon-css/flags/4x3/' + this.props.countryCode + '.svg'
 			).then(
 				( { default: flagSvg } ) => this.setState( { flagSvg } ),
-				() => this.setState( { flagSvg: false } ),
-			);
-
-			// called synchronously before the dynamic import loads
-			this.setState( { flagSvg: null } );
-		}
+				() => this.setState( { flagSvg: false } )
+			)
+		} );
 	};
 
 	render() {
