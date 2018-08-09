e96b74d91ea747cdb1bcb1192fbf6816ad025334
Jon Surrell <jon.surrell@automattic.com>
2018-08-09 07:46:12 -0400

    Alphabetize imports

--- client/my-sites/checklist/main.jsx
+++ client/my-sites/checklist/main.jsx
@@ -13,19 +13,19 @@ import { localize } from 'i18n-calypso';
  */
 import ChecklistShow from './checklist-show';
 import ChecklistShowShare from './share';
-import { isEnabled } from 'config';
 import DocumentHead from 'components/data/document-head';
 import EmptyContent from 'components/empty-content';
 import FormattedHeader from 'components/formatted-header';
 import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
+import isSiteOnFreePlan from 'state/selectors/is-site-on-free-plan';
 import Main from 'components/main';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
 import QuerySiteChecklist from 'components/data/query-site-checklist';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import { getCurrentUser } from 'state/current-user/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
-import { isJetpackSite, isNewSite, getSiteSlug } from 'state/sites/selectors';
-import isSiteOnFreePlan from 'state/selectors/is-site-on-free-plan';
+import { getSiteSlug, isJetpackSite, isNewSite } from 'state/sites/selectors';
+import { isEnabled } from 'config';
 
 /**
  * Included to fix regression.
