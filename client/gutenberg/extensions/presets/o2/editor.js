a9821b71b2016f7886a8ec4dffaa271698f03a98
Bernie Reiter <ockham@raz.or.at>
2018-08-09 18:49:59 +0200

    Gutenberg: Add a8c-blocks (#26513)
    * Add a8c-blocks (excluding idioticon, which needs a separate audit) to the `p2` preset.
    * Fix `wp` imports and code style.
    * Rename p2 preset to o2 (for style namespacing).
    * Remove empty style.scss file.
    * Add view script files.

---
+++ client/gutenberg/extensions/presets/o2/editor.js
@@ -0,0 +1,10 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import 'gutenberg/extensions/editor-notes/index.js';
+import 'gutenberg/extensions/prev-next/block.js';
+import 'gutenberg/extensions/todo/block.js';
+import 'gutenberg/extensions/trials/block.js';
+import 'gutenberg/extensions/trial/block.js';
