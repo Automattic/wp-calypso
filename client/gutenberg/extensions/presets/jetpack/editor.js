a896d8d384bc05f2580aca92555829cabc791eb6
Mikael Korpela <mikael@ihminen.org>
2018-08-09 13:36:29 +0300

    ABC 🙃

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -3,6 +3,6 @@
 /**
  * Internal dependencies
  */
-import 'gutenberg/extensions/tiled-gallery/tiled-gallery.jsx';
 import 'gutenberg/extensions/markdown/jetpack-markdown-block.js';
 import 'gutenberg/extensions/publicize/publicize-gutenberg-plugin.jsx';
+import 'gutenberg/extensions/tiled-gallery/tiled-gallery.jsx';
