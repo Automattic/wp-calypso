0282847ec6500f685236b0e6037dfad6caeb70c1
Marin Atanasov <tyxla@users.noreply.github.com>
2018-08-09 16:03:12 +0300

    SDK: Update Publicize extension to be rendered as a plugin extension (#26533)
    * Render Publicize UI in plugin sidebar
    
    * Add a simple Jetpack logo
    
    * Update docs
    
    * Update docs

--- client/gutenberg/extensions/publicize/publicize-gutenberg-plugin.jsx
+++ client/gutenberg/extensions/publicize/publicize-gutenberg-plugin.jsx
@@ -1,9 +1,9 @@
 /**
  * Top-level Publicize plugin for Gutenberg editor.
  *
- * Hooks into Gutenberg's PluginPrePublishPanel
- * to display Jetpack's Publicize UI in the pre-
- * publish flow.
+ * Hooks into Gutenberg's PluginPrePublishPanel and PluginSidebar
+ * to display Jetpack's Publicize UI in the pre-publish flow and
+ * as a regular plugin extension.
  *
  * @since  5.9.1
  */
@@ -19,19 +19,41 @@ import wp from 'wp';
  */
 import PublicizePanel from './publicize-panel';
 import publicizeStore from './publicize-gutenberg-store';
+import JetpackLogo from 'components/jetpack-logo';
 
 /**
  * Module variables
  */
 const { data } = wp;
 const { registerStore } = data;
-const { PluginPrePublishPanel } = wp.editPost;
+const {
+	PluginPrePublishPanel,
+	PluginSidebar,
+	PluginSidebarMoreMenuItem,
+} = wp.editPost;
 const { registerPlugin } = wp.plugins;
+const { __ } = wp.i18n;
+const { Fragment } = wp.element;
 
 const PluginRender = () => (
-	<PluginPrePublishPanel>
-		<PublicizePanel />
-	</PluginPrePublishPanel>
+	<Fragment>
+		<PluginSidebarMoreMenuItem
+			target="jetpack"
+			icon={ <JetpackLogo size={ 24 } /> }
+		>
+			{ __( 'Jetpack' ) }
+		</PluginSidebarMoreMenuItem>
+		<PluginSidebar
+			name="jetpack"
+			title={ __( 'Jetpack' ) }
+			icon={ <JetpackLogo size={ 24 } /> }
+		>
+			<PublicizePanel />
+		</PluginSidebar>
+		<PluginPrePublishPanel>
+			<PublicizePanel />
+		</PluginPrePublishPanel>
+	</Fragment>
 );
 
 registerPlugin( 'jetpack-publicize', {
