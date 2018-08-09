bfdb59a41f16dc1a1874fcd71082ee002f99d43b
Mikael Korpela <mikael@ihminen.org>
2018-08-09 15:40:05 +0300

    SDK: add Jetpack blocks preset (#26561)

---
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -0,0 +1,8 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import 'gutenberg/extensions/markdown/jetpack-markdown-block.jsx';
+import 'gutenberg/extensions/publicize/publicize-gutenberg-plugin.jsx';
+import 'gutenberg/extensions/tiled-gallery/tiled-gallery.jsx';
