40fcb1fa2f693d4b7f4f1be0aee8727d3ee6d6b3
Mikael Korpela <mikael@ihminen.org>
2018-08-10 17:56:35 +0300

    SDK: remove Publicize from Jetpack bundle
    Removing for now because it fails the bundle. Introducing it back once it's fixed.
    
    https://github.com/Automattic/jetpack/pull/10008#pullrequestreview-145180764
    
    >it errors out; seems to be missing the gutenberg_publicize_setup global variable, which is specifically introduced for this extension. It will also be missing some more things too - see https://github.com/Automattic/jetpack/pull/9963 for details.

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -4,5 +4,4 @@
  * Internal dependencies
  */
 import 'gutenberg/extensions/markdown/jetpack-markdown-block.jsx';
-import 'gutenberg/extensions/publicize/publicize-gutenberg-plugin.jsx';
 import 'gutenberg/extensions/tiled-gallery/tiled-gallery.jsx';
