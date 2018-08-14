713129cddf9b0db1bf35d0f10cb7b1196475ccc1
Markus Dobmann <markus@mpnjs.com>
2018-08-14 16:00:07 +0200

    store: alias import to 'store/dist/store.modern'

--- webpack.config.js
+++ webpack.config.js
@@ -244,6 +244,7 @@ function getWebpackConfig( { cssFilename, externalizeWordPressPackages = false }
 					'react-virtualized': 'react-virtualized/dist/commonjs',
 					'social-logos/example': 'social-logos/build/example',
 					debug: path.resolve( __dirname, 'node_modules/debug' ),
+					store: 'store/dist/store.modern',
 				},
 				getAliasesForExtensions()
 			),
