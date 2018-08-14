4ca080d76b40d05cab6e56d0880b06179b1507c7
Jon Surrell <jon.surrell@automattic.com>
2018-08-14 14:58:45 -0400

    Add text

--- client/my-sites/plans/current-plan/plan-setup-header.js
+++ client/my-sites/plans/current-plan/plan-setup-header.js
@@ -19,6 +19,26 @@ export class PlanSetupHeader extends Component {
 		return (
 			<Card>
 				<img alt="" aria-hidden="true" src="/calypso/images/illustrations/fireworks.svg" />
+				<h1>{ translate( 'Thank you for your purchase!' ) }</h1>
+				<p>
+					{ translate(
+						'Your website is on a %(planName)s plan for $(duration)s. Let’s walk through a short checklist of essential security features for safeguarding your website.',
+						{
+							args: {
+								/**
+								 * @TODO (sirreal) real props
+								 */
+								planName: 'Jetpack Premium',
+								duration: '1 month',
+							},
+						}
+					) }
+				</p>
+				<p>
+					{ translate(
+						'We’ve taken the liberty of starting the first two items, since they’re key to your site’s safety: we’re configuring spam filtering and backups for you now. Once that’s done, we can work through the rest of the checklist.'
+					) }
+				</p>
 				<ProgressBar isPlusing total={ 100 } value={ 10 } />
 				<div>
 					<a href={ /* @TODO (sirreal) fix this */ document.location.pathname }>
