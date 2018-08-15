4503ad9d9865bddaa9a55cf87a267c02bdd1e8bf
Matt Sherman <matt@jam123.com>
2018-08-15 14:15:00 -0400

    Remove querying of post view counts from Blog Posts list

--- client/my-sites/post-type-list/post-action-counts/index.jsx
+++ client/my-sites/post-type-list/post-action-counts/index.jsx
@@ -12,10 +12,8 @@ import { get } from 'lodash';
 /**
  * Internal dependencies
  */
-import QueryPostStats from 'components/data/query-post-stats';
 import PostLikesPopover from 'blocks/post-likes/popover';
 import { getNormalizedPost } from 'state/posts/selectors';
-import { getPostStat } from 'state/stats/posts/selectors';
 import canCurrentUser from 'state/selectors/can-current-user';
 import { getSiteSlug, isJetpackModuleActive, isJetpackSite } from 'state/sites/selectors';
 import { recordTracksEvent } from 'state/analytics/actions';
@@ -120,11 +118,8 @@ class PostActionCounts extends PureComponent {
 	}
 
 	render() {
-		const { postId, siteId } = this.props;
-
 		return (
 			<ul className="post-action-counts">
-				{ siteId && <QueryPostStats siteId={ siteId } postId={ postId } fields={ [ 'views' ] } /> }
 				{ this.renderLikeCount() }
 				{ this.renderCommentCount() }
 			</ul>
@@ -160,7 +155,6 @@ export default connect(
 			siteId,
 			siteSlug: getSiteSlug( state, siteId ),
 			type: get( post, 'type', 'unknown' ),
-			viewCount: getPostStat( state, siteId, postId, 'views' ),
 			isCurrentLikesPopoverOpen: isLikesPopoverOpen( state, globalId ),
 		};
 	},
