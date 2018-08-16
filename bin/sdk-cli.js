648f51f6ae133ec7eca325db493c1d3b5497b960
Dennis Snell <dennis.snell@automattic.com>
2018-08-16 12:25:39 +0100

    update config script

--- bin/sdk-cli.js
+++ bin/sdk-cli.js
@@ -44,11 +44,11 @@ const getBaseConfig = ( options = {} ) => {
 };
 
 const build = ( target, argv ) => {
-	const config = target.config( { argv, getBaseConfig } );
+	const config = target.config( { argv, getBaseConfig, __rootDir } );
 	const compiler = webpack( config );
 
 	// watch takes an additional argument, adjust accordingly
-	const runner = f => argv.watch ? compiler.watch( {}, f ) : compiler.run( f );
+	const runner = f => ( argv.watch ? compiler.watch( {}, f ) : compiler.run( f ) );
 
 	runner( ( error, stats ) => {
 		if ( error ) {
@@ -120,24 +120,18 @@ yargs
 	.command( {
 		command: 'notifications',
 		desc: 'Build the standalone notifications client',
-		builder: yargs => yargs.options( {
-			'mode': {
-				alias: 'm',
-				description: 'Choose the way how assets are optimized.',
-				type: 'string',
-				choices: [ 'production', 'development' ],
-				default: 'production',
-				requiresArg: true,
-			},
-			'output-dir': {
-				alias: 'o',
-				description: 'Output directory for the built assets. Intermediate directories are created as required.',
-				type: 'string',
-				coerce: path.resolve,
-				requiresArg: true,
-			}
-		} ),
-		handler: argv => notifications.compile( argv )
+		builder: yargs =>
+			yargs.options( {
+				'output-dir': {
+					alias: 'o',
+					description:
+						'Output directory for the built assets. Intermediate directories are created as required.',
+					type: 'string',
+					coerce: path.resolve,
+					requiresArg: true,
+				},
+			} ),
+		handler: argv => build( notifications, argv ),
 	} )
 	.demandCommand( 1, chalk.red( 'You must provide a valid command!' ) )
 	.alias( 'help', 'h' )
--- bin/sdk/notifications.js
+++ bin/sdk/notifications.js
@@ -4,70 +4,36 @@
  * External dependencies
  */
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
-const chalk = require( 'chalk' );
 const path = require( 'path' );
 const spawnSync = require( 'child_process' ).spawnSync;
-const webpack = require( 'webpack' );
 
-const __rootDir = path.resolve( __dirname, '../../' );
-const CopyWebpackPlugin = require( path.resolve(
-	__rootDir,
-	'server/bundler/copy-webpack-plugin'
-) );
-const getBaseConfig = require( path.join( __rootDir, 'webpack.config.js' ) );
-
-exports.compile = options => {
+exports.config = ( { argv: { outputDir }, getBaseConfig, __rootDir } ) => {
 	const baseConfig = getBaseConfig();
-	console.log( options.mode );
 
-	const config = {
+	return {
 		...baseConfig,
 		...{
-			context: __rootDir,
-			mode: options.mode,
-			devtool: 'source-map',
 			entry: path.join( __rootDir, 'client', 'notifications', 'src', 'standalone' ),
 			node: {
 				fs: 'empty',
 			},
-			optimization: {
-				splitChunks: false,
-			},
 			output: {
-				path: options.outputDir,
+				path: outputDir,
 				filename: 'build.min.js',
 			},
 			plugins: [
-				...baseConfig.plugins.filter( plugin => ! ( plugin instanceof CopyWebpackPlugin ) ),
+				...baseConfig,
 				new HtmlWebpackPlugin( {
-					filename: path.join( options.outputDir, 'root.html' ),
-					gitDescribe: spawnSync( 'git', [ 'describe', '--always', '--dirty', '--long' ], { encoding: 'utf8' } ).stdout.replace( '\n', '' ),
+					filename: path.join( outputDir, 'root.html' ),
+					gitDescribe: spawnSync( 'git', [ 'describe', '--always', '--dirty', '--long' ], {
+						encoding: 'utf8',
+					} ).stdout.replace( '\n', '' ),
 					hash: true,
 					nodePlatform: process.platform,
 					nodeVersion: process.version,
-					template: path.join( __rootDir, 'client', 'notifications', 'src', 'index.ejs' )
+					template: path.join( __rootDir, 'client', 'notifications', 'src', 'index.ejs' ),
 				} ),
 			],
 		},
 	};
-
-	const compiler = webpack( config );
-
-	compiler.run( ( error, stats ) => {
-		if ( error ) {
-			console.error( error );
-			console.log( chalk.red( 'Failed to build notifications' ) );
-			process.exit( 1 );
-		}
-
-		console.log( stats.toString() );
-
-		if ( stats.hasErrors() ) {
-			console.log( chalk.red( 'Finished building notifications but with errors.' ) );
-		} else if ( stats.hasWarnings() ) {
-			console.log( chalk.yellow( 'Successfully built notifications but with warnings.' ) );
-		} else {
-			console.log( chalk.green( 'Successfully built notifications' ) );
-		}
-	} );
 };
