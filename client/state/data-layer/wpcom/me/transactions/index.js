e500db7841b22e2b1e6afa452542f40068095bfa
Louis Laugesen <louis.laugesen@gmail.com>
2018-08-20 16:50:52 +1000

    Use dispatchRequestEx per documentation

--- client/state/data-layer/wpcom/me/transactions/index.js
+++ client/state/data-layer/wpcom/me/transactions/index.js
@@ -8,34 +8,38 @@
  * Internal Dependencies
  */
 import { http } from 'state/data-layer/wpcom-http/actions';
-import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
-import { convertToCamelCase } from 'state/data-layer/utils';
+import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
+import { convertToCamelCase as fromApi } from 'state/data-layer/utils';
 import { TRANSACTION_CREATE, TRANSACTION_CREATE_SUCCESS, TRANSACTION_CREATE_FAILURE } from 'state/action-types';
 
-export const createTransaction = function( { dispatch }, action ) {
-	const request = {
-		path: `/me/transactions`,
+export const createTransaction = ( action ) => http(
+	{
+		path: '/me/transactions',
 		method: 'POST',
 		apiNamespace: 'rest/v1',
-		query: Object.assign( {}, {	http_envelope: 1 }, action ),
-	};
+		query: Object.assign( {}, {	http_envelope: 1 }, action )
+	},
+	action
+);
 
-	dispatch( http( request, action ) );
-};
-
-export const handleError = ( { dispatch }, action, error ) => dispatch( {
+export const createError = ( action, error ) => ( {
 	type: TRANSACTION_CREATE_FAILURE,
 	message: error.message
 } );
 
 
-export const handleSuccess = ( { dispatch }, action, response ) => dispatch( {
+export const createSuccess = ( action, response ) => ( {
 	type: TRANSACTION_CREATE_SUCCESS,
 	...response
 } );
 
 export default {
 	[ TRANSACTION_CREATE ]: [
-		dispatchRequest( createTransaction, handleSuccess, handleError, { fromApi: convertToCamelCase } )
-	],
+		dispatchRequestEx( {
+			fetch: createTransaction,
+			onSuccess: createSuccess,
+			onError: createError,
+			fromApi
+		} )
+	]
 };
--- client/state/data-layer/wpcom/me/transactions/test/index.js
+++ client/state/data-layer/wpcom/me/transactions/test/index.js
@@ -4,28 +4,23 @@
  * External dependencies
  */
 import { expect } from 'chai';
-import { spy } from 'sinon';
 
 /**
  * Internal dependencies
  */
-import { createTransaction, handleSuccess, handleError } from '../';
+import { createTransaction, createSuccess, createError } from '../';
 import { http } from 'state/data-layer/wpcom-http/actions';
 import { TRANSACTION_CREATE, TRANSACTION_CREATE_SUCCESS, TRANSACTION_CREATE_FAILURE } from 'state/action-types';
 
 describe( 'data-layer/me/transactions', () => {
 	describe( '#createTransaction', () => {
-		const dispatch = spy();
 		const action = { type: TRANSACTION_CREATE };
 
-		createTransaction( { dispatch }, action );
-
-		test( 'should dispatch HTTP request to /me/transactions endpoint', () => {
-			expect( dispatch ).to.have.been.calledOnce;
-			expect( dispatch ).to.have.been.calledWith(
+		test( 'should create HTTP request to /me/transactions endpoint', () => {
+			expect( createTransaction( action ) ).to.deep.equal(
 				http(
 					{
-						path: `/me/transactions`,
+						path: '/me/transactions',
 						method: 'POST',
 						apiNamespace: 'rest/v1',
 						query: Object.assign( {}, {	http_envelope: 1 }, action ),
@@ -36,28 +31,24 @@ describe( 'data-layer/me/transactions', () => {
 		} );
 	} );
 
-	describe( '#handleError', () => {
-		const dispatch = spy();
+	describe( '#onError', () => {
 		const message = 'This is an error message.';
-		const error = Error( message );
 
-		handleError( { dispatch }, null, error );
-
-		test( 'should dispatch error action with error message', () => {
-			expect( dispatch ).to.have.been.calledWith( {
+		test( 'should create failure action', () => {
+			expect( createError( null, Error( message ) ) ).to.deep.equal( {
 				type: TRANSACTION_CREATE_FAILURE,
-				message,
+				message
 			} );
 		} );
 	} );
 
-	describe( '#handleSuccess', () => {
-		const dispatch = spy();
-
-		handleSuccess( { dispatch } );
-
-		test( 'should dispatch response action with response', () => {
-			expect( dispatch ).to.have.been.calledWith( { type: TRANSACTION_CREATE_SUCCESS } );
+	describe( '#onSuccess', () => {
+		test( 'should create success action with response', () => {
+			const response = { arbitrary: 'response' };
+			expect( createSuccess( null, response ) ).to.deep.equal( {
+				type: TRANSACTION_CREATE_SUCCESS,
+				...response
+			} );
 		} );
 	} );
 } );
