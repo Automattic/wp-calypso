770f85c942568d9c154a1cecc45681ddb5b7ee1a
Copons <copons@users.noreply.github.com>
2018-08-21 17:00:37 +0100

    Add Close button, Site, and Drafts to the Gutenberg top bar
    Style the whole thing
    
    Remove unnecessary fade
    
    Simplify route checking and revert unnecessary changes
    
    Use the navigate and withAnalytics middlewares
    
    Drop unnecessary changes

--- client/gutenberg/editor/edit-post/components/header/header-toolbar/index.js
+++ client/gutenberg/editor/edit-post/components/header/header-toolbar/index.js
@@ -1,7 +1,12 @@
 /**
  * External dependencies
+ *
+ * @format
  */
 import React from 'react';
+import { findLast } from 'lodash';
+import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
 
 /**
  * WordPress dependencies
@@ -23,30 +28,126 @@ import {
 	NavigableToolbar,
 } from '@wordpress/editor';
 
+/**
+ * Internal dependencies
+ */
+import Button from 'components/button';
+import Drafts from 'layout/masterbar/drafts';
+import Site from 'blocks/site';
+import { addSiteFragment } from 'lib/route';
+import { recordTracksEvent, withAnalytics } from 'state/analytics/actions';
+import { navigate } from 'state/ui/actions';
+import { getSelectedSite } from 'state/ui/selectors';
+import { getRouteHistory } from 'state/ui/action-log/selectors';
+
 /* eslint-disable wpcalypso/jsx-classname-namespace */
-function HeaderToolbar( { hasFixedToolbar, isLargeViewport } ) {
+function HeaderToolbar( {
+	closeEditor,
+	hasFixedToolbar,
+	isLargeViewport,
+	recordSiteButtonClick,
+	site,
+	translate,
+} ) {
+	const onCloseButtonClick = () => closeEditor();
+
 	return (
-		<NavigableToolbar
-			className="edit-post-header-toolbar"
-			aria-label={ __( 'Editor Toolbar' ) }
-		>
+		<NavigableToolbar className="edit-post-header-toolbar" aria-label={ __( 'Editor Toolbar' ) }>
+			<Button
+				borderless
+				className="edit-post-header-toolbar__back"
+				onClick={ onCloseButtonClick }
+				aria-label={ translate( 'Close' ) }
+			>
+				{ translate( 'Close' ) }
+			</Button>
+			<Site compact site={ site } indicator={ false } onSelect={ recordSiteButtonClick } />
+			<Drafts />
 			<Inserter position="bottom right" />
 			<EditorHistoryUndo />
 			<EditorHistoryRedo />
 			<TableOfContents />
-			{ hasFixedToolbar && isLargeViewport && (
-				<div className="edit-post-header-toolbar__block-toolbar">
-					<BlockToolbar />
-				</div>
-			) }
+			{ hasFixedToolbar &&
+				isLargeViewport && (
+					<div className="edit-post-header-toolbar__block-toolbar">
+						<BlockToolbar />
+					</div>
+				) }
 		</NavigableToolbar>
 	);
 }
 /* eslint-enable wpcalypso/jsx-classname-namespace */
 
+function getCloseButtonPath( routeHistory, site ) {
+	const editorPathRegex = /^\/gutenberg\/(post|page|(edit\/[^\/]+))\/[^\/]+(\/\d+)?$/i;
+	const lastEditorPath = routeHistory[ routeHistory.length - 1 ].path;
+
+	// @see post-editor/editor-ground-control/index.jsx
+	const lastNonEditorPath = findLast(
+		routeHistory,
+		action => ! action.path.match( editorPathRegex )
+	);
+	if ( lastNonEditorPath ) {
+		return lastNonEditorPath.path;
+	}
+
+	const editorPostType = lastEditorPath.match( editorPathRegex )[ 1 ];
+	let path;
+
+	// @see post-editor/post-editor.jsx
+	if ( 'post' === editorPostType ) {
+		path = '/posts';
+	} else if ( 'page' === editorPostType ) {
+		path = '/pages';
+	} else {
+		path = `/types/${ editorPostType.split( '/' )[ 1 ] }`;
+	}
+	if ( 'post' === editorPostType && site && ! site.jetpack && ! site.single_user_site ) {
+		path += '/my';
+	}
+	if ( site ) {
+		path = addSiteFragment( path, site.slug );
+	}
+	return path;
+}
+
+const mapStateToProps = state => ( {
+	routeHistory: getRouteHistory( state ),
+	site: getSelectedSite( state ),
+} );
+
+const mapDispatchToProps = dispatch => {
+	return {
+		closeEditor: ( { routeHistory, site } ) =>
+			dispatch(
+				withAnalytics(
+					recordTracksEvent( 'calypso_gutenberg_editor_close_button_click' ),
+					navigate( getCloseButtonPath( routeHistory, site ) )
+				)
+			),
+		recordSiteButtonClick: () =>
+			dispatch( recordTracksEvent( 'calypso_gutenberg_editor_site_button_click' ) ),
+	};
+};
+
+const mergeProps = ( stateProps, dispatchProps, ownProps ) => {
+	return {
+		...ownProps,
+		...stateProps,
+		...dispatchProps,
+		closeEditor: () => dispatchProps.closeEditor( stateProps ),
+	};
+};
+
 export default compose( [
-	withSelect( ( select ) => ( {
+	withSelect( select => ( {
 		hasFixedToolbar: select( 'core/edit-post' ).isFeatureActive( 'fixedToolbar' ),
 	} ) ),
 	withViewportMatch( { isLargeViewport: 'medium' } ),
-] )( HeaderToolbar );
+] )(
+	connect(
+		mapStateToProps,
+		mapDispatchToProps,
+		mergeProps
+	)( localize( HeaderToolbar ) )
+);
