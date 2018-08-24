83f2d3c11b9c2df4b8d7643fffdde71f6a898642
Jeff Bowen <jeff@automattic.com>
2018-08-23 22:19:34 -0400

    Account: Periodic Health Check

---
+++ client/components/account-periodic-health-check/index.jsx
@@ -0,0 +1,37 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { getCurrentUser } from 'state/current-user/selectors';
+import {
+	getAccountRecoveryEmail,
+	getAccountRecoveryPhone,
+} from 'state/account-recovery/settings/selectors';
+import getApplicationPasswords from 'state/selectors/get-application-passwords';
+import getConnectedApplications from 'state/selectors/get-connected-applications';
+import debugFactory from 'debug';
+
+const debug = debugFactory( 'calypso:account-periodic-health-check' );
+
+const AccountPeriodicHealthCheck = () => {
+	debug( this.props );
+	return 'ohai';
+};
+
+export default connect( state => {
+	const currentUser = getCurrentUser( state );
+
+	return {
+		accountRecoveryEmail: getAccountRecoveryEmail( state ),
+		accountRecoveryPhone: getAccountRecoveryPhone( state ),
+		applicationPasswordCount: getApplicationPasswords( state ).length,
+		connectedApplicationCount: getConnectedApplications( state ),
+		displayName: currentUser.display_name,
+		email: currentUser.email,
+	};
+} )( AccountPeriodicHealthCheck );
