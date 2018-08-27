8c0d2337ce28590e274a26522da3f4626397c773
Bernie Reiter <ockham@raz.or.at>
2018-08-27 19:09:46 +0200

    Fix preferences selectors

--- client/state/preferences/selectors.js
+++ client/state/preferences/selectors.js
@@ -11,22 +11,21 @@ import { get, find, has } from 'lodash';
  */
 import { DEFAULT_PREFERENCE_VALUES } from './constants';
 
-export const isFetchingPreferences = state => !! state.preferences.fetching;
+export const isFetchingPreferences = state => !! state.fetching;
 
 /**
  * Returns the preference value associated with the specified key. Attempts to
  * find in local and remote preferences, then any applicable default value,
  * otherwise returning null.
  *
- * @param  {Object} state Global state tree
+ * @param  {Object} state Preferences state tree
  * @param  {String} key   Preference key
  * @return {*}            Preference value
  */
 export function getPreference( state, key ) {
 	return get(
-		find(
-			[ state.preferences.localValues, state.preferences.remoteValues, DEFAULT_PREFERENCE_VALUES ],
-			source => has( source, key )
+		find( [ state.localValues, state.remoteValues, DEFAULT_PREFERENCE_VALUES ], source =>
+			has( source, key )
 		),
 		key,
 		null
@@ -38,22 +37,22 @@ export function getPreference( state, key ) {
  * of the object are each preference key and the values are the preference
  * values.
  *
- * @param  {Object} state Global state tree
+ * @param  {Object} state Preferences state tree
  * @return {Object}       Preference value
  */
 export function getAllRemotePreferences( state ) {
-	return state.preferences.remoteValues;
+	return state.remoteValues;
 }
 
-export const preferencesLastFetchedTimestamp = state => state.preferences.lastFetchedTimestamp;
+export const preferencesLastFetchedTimestamp = state => state.lastFetchedTimestamp;
 
 /**
  * Returns true if preferences have been received from the remote source, or
  * false otherwise.
  *
- * @param  {Object}  state Global state tree
+ * @param  {Object}  state Preferences state tree
  * @return {Boolean}       Whether preferences have been received
  */
 export function hasReceivedRemotePreferences( state ) {
-	return !! state.preferences.remoteValues;
+	return !! state.remoteValues;
 }
