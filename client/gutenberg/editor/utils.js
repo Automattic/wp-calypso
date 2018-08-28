031e59db101b3458d62d2ccb167535da40549dd9
Marin Atanasov <tyxla@abv.bg>
2018-08-28 14:23:26 +0300

    Gutenberg: Proxy API requests

--- client/gutenberg/editor/utils.js
+++ client/gutenberg/editor/utils.js
@@ -3,29 +3,26 @@
  * External dependencies
  */
 import apiFetch from '@wordpress/api-fetch';
-import { noop } from 'lodash';
+import wpcomProxyRequest from 'wpcom-proxy-request';
 
-/**
- * Internal dependencies
- */
-import debugFactory from 'debug';
-
-const debug = debugFactory( 'calypso:gutenberg' );
-
-export const overrideAPIPaths = siteSlug => {
-	// TODO: no API support for now. We'll also need to handle authorization here.
-	apiFetch.use( () => noop );
+export const overrideAPIPaths = () => {
+	apiFetch.use( options => {
+		return new Promise( ( resolve, reject ) => {
+			wpcomProxyRequest(
+				{
+					...options,
+					apiNamespace: 'rest/v1.1',
+				},
+				( error, body ) => {
+					if ( error ) {
+						return reject( error );
+					}
+					return resolve( body );
+				}
+			);
+		} );
+	} );
 
 	const rootURL = 'https://public-api.wordpress.com/';
 	apiFetch.use( apiFetch.createRootURLMiddleware( rootURL ) );
-
-	// rewrite default API paths to match WP.com equivalents
-	// Example: /wp/v2/posts -> /wp/v2/sites/{siteSlug}/posts
-	apiFetch.use( ( options, next ) => {
-		const wpcomPath = `/wp/v2/sites/${ siteSlug }/` + options.path.replace( '/wp/v2/', '' );
-
-		debug( 'sending API request to: ', wpcomPath );
-
-		return next( { ...options, path: wpcomPath } );
-	} );
 };
