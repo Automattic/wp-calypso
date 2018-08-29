fe1391800796a819f0ff98f94650345a0debe6ff
Marin Atanasov <tyxla@abv.bg>
2018-08-29 18:20:17 +0300

    Bring back previously removed apiFetch middleware

--- client/gutenberg/editor/utils.js
+++ client/gutenberg/editor/utils.js
@@ -4,26 +4,36 @@
  */
 import apiFetch from '@wordpress/api-fetch';
 import wpcomProxyRequest from 'wpcom-proxy-request';
+import { noop } from 'lodash';
 
-export const overrideAPIPaths = () => {
-	// TODO: fix for /gutenberg in Calypso
-	apiFetch.use( options => {
-		return new Promise( ( resolve, reject ) => {
-			wpcomProxyRequest(
-				{
-					...options,
-					apiNamespace: 'rest/v1.1',
-				},
-				( error, body ) => {
-					if ( error ) {
-						return reject( error );
-					}
-					return resolve( body );
-				}
-			);
-		} );
-	} );
-
+export const overrideAPIPaths = siteSlug => {
 	const rootURL = 'https://public-api.wordpress.com/';
 	apiFetch.use( apiFetch.createRootURLMiddleware( rootURL ) );
+
+	apiFetch.use( ( options, next ) => {
+		if ( options.path.indexOf( 'wp/v2/sites/' ) >= 0 ) {
+			// rewrite default API paths to match WP.com equivalents
+			// Example: /wp/v2/posts -> /wp/v2/sites/{siteSlug}/posts
+			const wpcomPath = `/wp/v2/sites/${ siteSlug }/` + options.path.replace( '/wp/v2/', '' );
+			return next( { ...options, path: wpcomPath } );
+		} else if ( options.path.indexOf( '/me' ) >= 0 ) {
+			// rewrite the namespace of /me endpoints
+			// example: /wp/v2/me/settings -> /rest/v1.1/me/settings
+			return new Promise( ( resolve, reject ) => {
+				wpcomProxyRequest(
+					{
+						...options,
+						apiNamespace: 'rest/v1.1',
+					},
+					( error, body ) => {
+						if ( error ) {
+							return reject( error );
+						}
+						return resolve( body );
+					}
+				);
+			} );
+		}
+		return noop;
+	} );
 };
