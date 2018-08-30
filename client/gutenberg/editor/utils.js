69233a3251c728c2ae8406ee5147de61c2cd765f
Marin Atanasov <tyxla@abv.bg>
2018-08-30 11:28:45 +0300

    Use startsWith instead of indexOf

--- client/gutenberg/editor/utils.js
+++ client/gutenberg/editor/utils.js
@@ -4,19 +4,19 @@
  */
 import apiFetch from '@wordpress/api-fetch';
 import wpcomProxyRequest from 'wpcom-proxy-request';
-import { noop } from 'lodash';
+import { noop, startsWith } from 'lodash';
 
 export const overrideAPIPaths = siteSlug => {
 	const rootURL = 'https://public-api.wordpress.com/';
 	apiFetch.use( apiFetch.createRootURLMiddleware( rootURL ) );
 
 	apiFetch.use( ( options, next ) => {
-		if ( options.path.indexOf( 'wp/v2/sites/' ) >= 0 ) {
+		if ( startsWith( options.path, '/wp/v2/sites/' ) ) {
 			// rewrite default API paths to match WP.com equivalents
 			// Example: /wp/v2/posts -> /wp/v2/sites/{siteSlug}/posts
 			const wpcomPath = `/wp/v2/sites/${ siteSlug }/` + options.path.replace( '/wp/v2/', '' );
 			return next( { ...options, path: wpcomPath } );
-		} else if ( options.path.indexOf( '/me' ) >= 0 ) {
+		} else if ( startsWith( options.path, '/me' ) ) {
 			// rewrite the namespace of /me endpoints
 			// example: /wp/v2/me/settings -> /rest/v1.1/me/settings
 			return new Promise( ( resolve, reject ) => {
