7cb11f9efd411af5eb9035bef50f7674002ca5a9
Louis Laugesen <louis.laugesen@gmail.com>
2018-08-31 18:46:19 +1000

    Add convertKeysTo, convertToSnakeCase
    This replaces convertToCamelCase with a generic function
    `convertKeysTo`. This is so it can be used similarly to the
    undocumented.js function `mapKeysRecursively` in `convertToSnakeCase`
    
    I've left existing convertToCamelCase tests in place to ensure forward
    compatability. These can be removed later. The new tests do omit testing
    bracket and dot keys but that looks to be lodash behaviour we _probably_
    dont need to test for.
    
    https://github.com/Automattic/wp-calypso/blob/master/client/lib/wpcom-undocumented/lib/undocumented.js#L674

--- client/state/data-layer/utils.js
+++ client/state/data-layer/utils.js
@@ -3,7 +3,16 @@
 /**
  * External dependencies
  */
-import { camelCase, isArray, isObjectLike, isPlainObject, map, reduce, set } from 'lodash';
+import {
+	camelCase,
+	isArray,
+	isObjectLike,
+	isPlainObject,
+	map,
+	reduce,
+	set,
+	snakeCase,
+} from 'lodash';
 
 /**
  * Internal dependencies
@@ -21,22 +30,23 @@ const doBypassDataLayer = {
 export const bypassDataLayer = action => extendAction( action, doBypassDataLayer );
 
 /**
- * Deeply converts keys from the specified object to camelCase notation.
+ * Deeply converts keys of an object using provided function.
  *
  * @param {Object} obj object to convert
- * @returns {Object}   a new object with all keys converted
+ * @param  {Function} fn function to apply to each key of the object
+ * @returns {Object} a new object with all keys converted
  */
-export function convertToCamelCase( obj ) {
+export function convertKeysBy( obj, fn ) {
 	if ( isArray( obj ) ) {
-		return map( obj, convertToCamelCase );
+		return map( obj, v => convertKeysBy( v, fn ) );
 	}
 
 	if ( isPlainObject( obj ) ) {
 		return reduce(
 			obj,
 			( result, value, key ) => {
-				const newKey = camelCase( key );
-				const newValue = isObjectLike( value ) ? convertToCamelCase( value ) : value;
+				const newKey = fn( key );
+				const newValue = isObjectLike( value ) ? convertKeysBy( value, fn ) : value;
 				return set( result, [ newKey ], newValue );
 			},
 			{}
@@ -45,3 +55,19 @@ export function convertToCamelCase( obj ) {
 
 	return obj;
 }
+
+/**
+ * Deeply converts keys from the specified object to camelCase notation.
+ *
+ * @param {Object} obj object to convert
+ * @returns {Object} a new object with all keys converted
+ */
+export const convertToCamelCase = obj => convertKeysBy( obj, camelCase );
+
+/**
+ * Deeply convert keys of an object to snake_case.
+ *
+ * @param {Object} obj Object to convert
+ * @return {Object} a new object with snake_cased keys
+ */
+export const convertToSnakeCase = obj => convertKeysBy( obj, snakeCase );
