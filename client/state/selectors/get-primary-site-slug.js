06f76e639bada341bfdd66ebc4aaad58144a35f3
Jarda Snajdr <jsnajdr@gmail.com>
2018-09-03 15:42:45 +0200

    Masterbar Publish Button: remove dependency on lib/user  (#26985)
    * Create new getPrimarySiteSlug selector
    
    Creates a new selector and migrates one existing instance where a module defined
    it privately.
    
    * Simplify the editorPathFromSite helper function
    
    Removes a dead branch that used to make sense when there was a `post-editor` feature flag,
    but is no longer needed today.
    
    * Masterbar Publish Button: remove dependency on lib/user
    
    Use Redux selectors to get the primary site slug and visible site count and remove
    dependencly on the `user` prop that's a `lib/user` instance.
    
    * Masterbar Publish Button: convert ref to React.createRef
    
    Use the React.createRef helper to maintain the button ref.
    
    * Masterbar Publish Button: clarify the handler for site selection
    
    Make it clear that the handler returns `false` to signal that the event should not be considered as
    handled and that the `SiteSelector` should continue handling it by navigating to `/post/:site`.

---
+++ client/state/selectors/get-primary-site-slug.js
@@ -0,0 +1,18 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import getPrimarySiteId from 'state/selectors/get-primary-site-id';
+import { getSiteSlug } from 'state/sites/selectors';
+
+/**
+ * Returns the current user's primary site's slug.
+ *
+ * @param  {Object}  state Global state tree
+ * @return {?String}       The current user's primary site's slug
+ */
+export default function getPrimarySiteSlug( state ) {
+	const primarySiteId = getPrimarySiteId( state );
+	return getSiteSlug( state, primarySiteId );
+}
