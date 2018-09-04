17da5672a57b9f3ae4f75e4d793fa4cac0df3b6d
Copons <copons@users.noreply.github.com>
2018-09-04 13:43:54 +0100

    Add unsupported post type

--- client/gutenberg/editor/controller.js
+++ client/gutenberg/editor/controller.js
@@ -3,14 +3,28 @@
  * External dependencies
  */
 import React from 'react';
+import { startsWith } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import GutenbergEditor from 'gutenberg/editor/main';
 
+function determinePostType( context ) {
+	if ( startsWith( context.path, '/gutenberg/post' ) ) {
+		return 'post';
+	}
+
+	if ( startsWith( context.path, '/gutenberg/page' ) ) {
+		return 'page';
+	}
+
+	return context.params.type;
+}
+
 export const post = ( context, next ) => {
+	const postType = determinePostType( context );
 	//reserved space for adding the post to context see post-editor/controller.js
-	context.primary = <GutenbergEditor />;
+	context.primary = <GutenbergEditor postType={ postType } />;
 	next();
 };
--- client/gutenberg/editor/main.jsx
+++ client/gutenberg/editor/main.jsx
@@ -13,13 +13,15 @@ import { registerCoreBlocks } from '@wordpress/block-library';
  * Internal dependencies
  */
 import Editor from './edit-post/editor.js';
+import EditorPostTypeUnsupported from 'post-editor/editor-post-type-unsupported';
+import QueryPostTypes from 'components/data/query-post-types';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { getSiteSlug } from 'state/sites/selectors';
 import { WithAPIMiddleware } from './api-middleware/utils';
 
 const editorSettings = {};
 
-const post = {
+const mockPost = {
 	type: 'post',
 	content: { raw: 'test content' },
 };
@@ -32,10 +34,13 @@ class GutenbergEditor extends Component {
 	}
 
 	render() {
-		const { siteSlug } = this.props;
+		const { postType, siteId, siteSlug } = this.props;
+		const post = { ...mockPost, type: postType };
 
 		return (
 			<WithAPIMiddleware siteSlug={ siteSlug }>
+				<QueryPostTypes siteId={ siteId } />
+				<EditorPostTypeUnsupported type={ postType } />
 				<Editor
 					settings={ editorSettings }
 					hasFixedToolbar={ true }
@@ -51,6 +56,7 @@ const mapStateToProps = state => {
 	const siteId = getSelectedSiteId( state );
 
 	return {
+		siteId,
 		siteSlug: getSiteSlug( state, siteId ),
 	};
 };
