0c6c3a995ba16215aeee0426cef12b7a0196cfff
spen <spen_@hotmail.co.uk>
2018-09-04 16:44:52 +0100

    ImporterPreview: Skip some redundant state keeping. Directly use props instead

--- client/my-sites/importer/site-importer/site-importer-site-preview.jsx
+++ client/my-sites/importer/site-importer/site-importer-site-preview.jsx
@@ -14,7 +14,6 @@ import classNames from 'classnames';
 import Spinner from 'components/spinner';
 import Button from 'components/forms/form-button';
 import MiniSitePreview from 'components/mini-site-preview';
-import ErrorPane from 'my-sites/importer/error-pane';
 import { recordTracksEvent } from 'state/analytics/actions';
 import ImportableContent from 'my-sites/importer/site-importer/site-importer-importable-content';
 
@@ -28,73 +27,54 @@ class SiteImporterSitePreview extends React.Component {
 		site: PropTypes.object,
 	};
 
-	state = {
-		siteURL: this.props.siteURL,
-	};
-
 	trackSitePreviewSuccess = ( { time_taken_ms } ) =>
 		this.props.recordTracksEvent( 'calypso_site_importer_site_preview_success', {
 			blog_id: this.props.site.ID,
-			site_url: this.state.siteURL,
+			site_url: this.props.siteURL,
 			time_taken_ms,
 		} );
 
 	trackSitePreviewFailure = ( { time_taken_ms } ) =>
 		this.props.recordTracksEvent( 'calypso_site_importer_site_preview_fail', {
 			blog_id: this.props.site.ID,
-			site_url: this.state.siteURL,
+			site_url: this.props.siteURL,
 			time_taken_ms,
 		} );
 
 	render = () => {
-		const { isLoading } = this.props;
-		const isError = this.state.sitePreviewFailed;
+		const { isLoading, siteURL } = this.props;
 
 		const containerClass = classNames( 'site-importer__site-preview-overlay-container', {
 			isLoading,
 		} );
 
-		return ! isError ? (
+		return ! isLoading ? (
 			<div>
-				{ ! isLoading && (
-					<div>
-						<div className="site-importer__site-importer-confirm-site-pane-container">
-							<div className="site-importer__site-importer-confirm-site-label">
-								{ this.props.translate( 'Is this your site?' ) }
-							</div>
-							<Button disabled={ isLoading } onClick={ this.props.startImport }>
-								{ this.props.translate( 'Yes! Start import' ) }
-							</Button>
-							<Button disabled={ isLoading } isPrimary={ false } onClick={ this.props.resetImport }>
-								{ this.props.translate( 'No' ) }
-							</Button>
-						</div>
-						<div className={ containerClass }>
-							<div className="site-importer__site-preview-column-container">
-								<MiniSitePreview
-									siteURL={ this.state.siteURL }
-									onFetchSuccess={ this.trackSitePreviewSuccess }
-									onFetchError={ this.trackSitePreviewFailure }
-								/>
-								<ImportableContent importData={ this.props.importData } />
-							</div>
-						</div>
+				<div className="site-importer__site-importer-confirm-site-pane-container">
+					<div className="site-importer__site-importer-confirm-site-label">
+						{ this.props.translate( 'Is this your site?' ) }
 					</div>
-				) }
-				{ isLoading && (
-					<div className="site-importer__site-preview-loading-overlay">
-						<Spinner />
+					<Button disabled={ isLoading } onClick={ this.props.startImport }>
+						{ this.props.translate( 'Yes! Start import' ) }
+					</Button>
+					<Button disabled={ isLoading } isPrimary={ false } onClick={ this.props.resetImport }>
+						{ this.props.translate( 'No' ) }
+					</Button>
+				</div>
+				<div className={ containerClass }>
+					<div className="site-importer__site-preview-column-container">
+						<MiniSitePreview
+							siteURL={ siteURL }
+							onFetchSuccess={ this.trackSitePreviewSuccess }
+							onFetchError={ this.trackSitePreviewFailure }
+						/>
+						<ImportableContent importData={ this.props.importData } />
 					</div>
-				) }
+				</div>
 			</div>
 		) : (
-			<div className="site-importer__site-preview-error">
-				<ErrorPane
-					type="importError"
-					description={ this.props.translate(
-						'Unable to load site preview. Please try again later.'
-					) }
-				/>
+			<div className="site-importer__site-preview-loading-overlay">
+				<Spinner />
 			</div>
 		);
 	};
