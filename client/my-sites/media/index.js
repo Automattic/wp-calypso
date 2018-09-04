e241a4883bab55b89373b202b7dea18b63ae48c9
Jarda Snajdr <jsnajdr@gmail.com>
2018-09-04 14:50:29 +0200

    Remove the manage/media feature flag (#26956)

--- client/my-sites/media/index.js
+++ client/my-sites/media/index.js
@@ -11,31 +11,28 @@ import page from 'page';
  */
 import { navigation, siteSelection, sites } from 'my-sites/controller';
 import mediaController from './controller';
-import config from 'config';
 import { makeLayout, render as clientRender } from 'controller';
 import { getSiteFragment } from 'lib/route';
 
 export default function() {
-	if ( config.isEnabled( 'manage/media' ) ) {
-		page( '/media', siteSelection, sites, makeLayout, clientRender );
+	page( '/media', siteSelection, sites, makeLayout, clientRender );
 
-		page(
-			'/media/:filter(this-post|images|documents|videos|audio)?/:domain',
-			siteSelection,
-			navigation,
-			mediaController.media,
-			makeLayout,
-			clientRender
-		);
+	page(
+		'/media/:filter(this-post|images|documents|videos|audio)?/:domain',
+		siteSelection,
+		navigation,
+		mediaController.media,
+		makeLayout,
+		clientRender
+	);
 
-		page( '/media/*', ( { path } ) => {
-			const siteFragment = getSiteFragment( path );
+	page( '/media/*', ( { path } ) => {
+		const siteFragment = getSiteFragment( path );
 
-			if ( siteFragment ) {
-				return page.redirect( `/media/${ siteFragment }` );
-			}
+		if ( siteFragment ) {
+			return page.redirect( `/media/${ siteFragment }` );
+		}
 
-			return page.redirect( '/media' );
-		} );
-	}
+		return page.redirect( '/media' );
+	} );
 }
