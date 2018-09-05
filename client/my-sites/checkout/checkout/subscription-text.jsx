62f14c96fee30793aa414a6f829c22ce919301ac
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-09-05 15:47:16 +0200

    fix(my-sites/checkout) Update informative text according to plan.
    The information text was “renews annually”, which is only true for
    yearly plan. This patch adds two other information texts: “renews
    biennially” and “renews monthly”.
    
    Fix #26847.

--- client/my-sites/checkout/checkout/subscription-text.jsx
+++ client/my-sites/checkout/checkout/subscription-text.jsx
@@ -7,6 +7,7 @@
 import React from 'react';
 
 import { localize } from 'i18n-calypso';
+import { isMonthly, isYearly, isBiennially } from 'lib/products-values';
 
 /**
  * Internal dependencies
@@ -16,9 +17,20 @@ import { cartItems } from 'lib/cart-values';
 class SubscriptionText extends React.Component {
 	render() {
 		if ( cartItems.hasRenewalItem( this.props.cart ) ) {
+			const product = this.props.cart.products[ 0 ];
+			let aside_text = '';
+
+			if ( isBiennially( product ) ) {
+				aside_text = 'renews biennially';
+			} else if ( isYearly( product ) ) {
+				aside_text = 'renews annually';
+			} else if ( isMonthly( product ) ) {
+				aside_text = 'renews monthly';
+			}
+
 			return (
 				<span className="subscription-text">
-					{ this.props.translate( 'renews annually', {
+					{ this.props.translate( aside_text, {
 						context: 'Informative text for renewals in /checkout',
 					} ) }
 				</span>
