eee83307ec2ceb99daccad73213ed4444c30cc71
Marin Atanasov <tyxla@abv.bg>
2018-09-05 14:21:30 +0300

    State: Introduce help links reducer

--- client/state/help/reducer.js
+++ client/state/help/reducer.js
@@ -3,8 +3,7 @@
 /**
  * Internal dependencies
  */
-
-import { HELP_CONTACT_FORM_SITE_SELECT } from 'state/action-types';
+import { HELP_CONTACT_FORM_SITE_SELECT, HELP_LINKS_RECEIVE } from 'state/action-types';
 import courses from './courses/reducer';
 import { combineReducers, createReducer } from 'state/utils';
 import directly from './directly/reducer';
@@ -21,9 +20,21 @@ export const selectedSiteId = createReducer( null, {
 	[ HELP_CONTACT_FORM_SITE_SELECT ]: ( state, action ) => action.siteId,
 } );
 
+/**
+ * Responsible for the help search results links
+ *
+ * @param  {Object} state  Current state
+ * @param  {Object} action Action payload
+ * @return {Object}        Updated state
+ */
+export const links = createReducer( [], {
+	[ HELP_LINKS_RECEIVE ]: ( state, action ) => action.helpLinks,
+} );
+
 export default combineReducers( {
 	courses,
 	directly,
+	links,
 	ticket,
 	selectedSiteId,
 } );
