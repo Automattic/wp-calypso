6e9239cff018a01526c4b2fe161a05a9ecbc2b4e
Marin Atanasov <tyxla@abv.bg>
2018-09-05 14:23:28 +0300

    State: Introduce getHelpLinks selector

---
+++ client/state/selectors/get-help-links.js
@@ -0,0 +1,9 @@
+/** @format */
+
+/**
+ * Returns the help links that user received as a result of their last search query
+ *
+ * @param  {Object} state Global state tree
+ * @return {Object}       Help links
+ */
+export default state => state.help.links;
