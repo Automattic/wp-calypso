2586dbc4e568a846d0e63c6b08c5351e790d25ee
Marko Andrijasevic <markoandrijasevic@gmail.com>
2018-09-05 13:35:25 +0200

    Move API override logic to wrapper component

--- client/gutenberg/editor/main.jsx
+++ client/gutenberg/editor/main.jsx
@@ -4,7 +4,7 @@
  */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { isEmpty, noop } from 'lodash';
+import { noop } from 'lodash';
 import { dispatch } from '@wordpress/data';
 import '@wordpress/core-data'; // Initializes core data store
 import { registerCoreBlocks } from '@wordpress/block-library';
@@ -15,7 +15,7 @@ import { registerCoreBlocks } from '@wordpress/block-library';
 import Editor from './edit-post/editor.js';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { getSiteSlug } from 'state/sites/selectors';
-import { applyAPIMiddlewares } from './utils';
+import { WithAPIMiddleware } from './utils';
 
 const editorSettings = {};
 
@@ -25,37 +25,24 @@ const post = {
 };
 
 class GutenbergEditor extends Component {
-	state = {
-		isAPIOverrideReady: false,
-	};
-
 	componentDidMount() {
 		registerCoreBlocks();
 		// Prevent Guided tour from showing when editor loads.
 		dispatch( 'core/nux' ).disableTips();
 	}
 
-	static getDerivedStateFromProps( props, state ) {
-		// Execute API override only once when siteSlug is not null.
-		// We can't do this in componentDidMount because it wouldn't work with clean state.
-		// siteSlug would be null and API requests would get executed before the override.
-		if ( ! isEmpty( props.siteSlug ) && ! state.isAPIOverrideReady ) {
-			overrideAPIPaths( props.siteSlug );
-			return { isAPIOverrideReady: true };
-		}
-
-		return null;
-	}
-
 	render() {
-		if ( ! this.state.isAPIOverrideReady ) {
-			return null;
-		}
-
-		applyAPIMiddlewares( this.props.siteSlug );
+		const { siteSlug } = this.props;
 
 		return (
-			<Editor settings={ editorSettings } hasFixedToolbar={ true } post={ post } onError={ noop } />
+			<WithAPIMiddleware siteSlug={ siteSlug }>
+				<Editor
+					settings={ editorSettings }
+					hasFixedToolbar={ true }
+					post={ post }
+					onError={ noop }
+				/>
+			</WithAPIMiddleware>
 		);
 	}
 }
