94cf51ed5567b3d9880091549342faab5666a1aa
Dennis Snell <dennis.snell@automattic.com>
2018-09-06 17:52:10 -0500

    SDK: Use newly-merged notifications panel code in Calypso (#26760)
    * SDK: Use newly-merged notifications panel code in Calypso
    
    See #26524 and #26757
    
    After importing the notifications panel code into Calypso this patch
    makes the changes necessary for us to start importing that code directly
    instead of through the `node_modules` import.

--- webpack.config.js
+++ webpack.config.js
@@ -159,7 +159,7 @@ function getWebpackConfig( { cssFilename, externalizeWordPressPackages = false }
 			rules: [
 				{
 					test: /\.jsx?$/,
-					exclude: /node_modules[\/\\](?!notifications-panel)/,
+					exclude: /node_modules\//,
 					use: [
 						{
 							loader: 'thread-loader',
