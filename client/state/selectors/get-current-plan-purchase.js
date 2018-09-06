af89126ac481ba24cc1d1ea2cfff418c57dd231a
Jon Surrell <jon.surrell@automattic.com>
2018-09-06 11:05:04 +0200

    Add getCurrentPlanPurchase selector

---
+++ client/state/selectors/get-current-plan-purchase.js
@@ -0,0 +1,19 @@
+/** @format */
+/**
+ * Internal dependencies
+ */
+import getCurrentPlanPurchaseId from 'state/selectors/get-current-plan-purchase-id';
+import { getByPurchaseId } from 'state/purchases/selectors';
+
+/**
+ * Return the purchase object for a site's current plan or null if not found.
+ *
+ * @param  {Object}  state  Global state tree
+ * @param  {Number}  siteId Site ID
+ * @return {?Object}        Purchase object or null if not found
+ */
+export default function getCurrentPlanPurchase( state, siteId ) {
+	const result = getByPurchaseId( state, getCurrentPlanPurchaseId( state, siteId ) );
+	// getByPurchaseId may return `undefined`. Ensure our return is [ purchase object | null ]
+	return 'undefined' !== typeof result ? result : null;
+}
