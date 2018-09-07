a22b59ef17c1f0d2a4f33afb351dc2ce7b079fb6
Jason Moon <jason.moon@automattic.com>
2018-09-07 15:22:30 -0600

    DomainTip: Simplify with explicit bool properties

--- client/my-sites/domain-tip/index.jsx
+++ client/my-sites/domain-tip/index.jsx
@@ -3,11 +3,9 @@
 /**
  * External dependencies
  */
-
 import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import { noop } from 'lodash';
 import PropTypes from 'prop-types';
 import React from 'react';
 
@@ -15,6 +13,7 @@ import React from 'react';
  * Internal dependencies
  */
 import { getSite, getSiteSlug } from 'state/sites/selectors';
+import { hasDomainCredit } from 'state/sites/plans/selectors';
 import { getDomainsSuggestions } from 'state/domains/suggestions/selectors';
 import { currentUserHasFlag } from 'state/current-user/selectors';
 import QueryDomainsSuggestions from 'components/data/query-domains-suggestions';
@@ -38,79 +37,65 @@ class DomainTip extends React.Component {
 	static propTypes = {
 		className: PropTypes.string,
 		event: PropTypes.string.isRequired,
+		hasDomainCredit: PropTypes.bool,
+		isIneligible: PropTypes.bool,
+		shouldNudgePlanUpgrade: PropTypes.bool,
 		siteId: PropTypes.number.isRequired,
-		domainsWithPlansOnly: PropTypes.bool.isRequired,
+		queryObject: PropTypes.shape( {
+			query: PropTypes.string,
+			quantity: PropTypes.number,
+			vendor: PropTypes.string,
+		} ),
 	};
 
-	static defaultProps = {
-		quantity: noop,
-	};
-
-	noticeShouldDisplay = () => {
-		// bypass some of the upgrade nudge display logic
-		return true;
-	};
-
-	domainUpgradeNudge() {
-		const { event, translate } = this.props;
-
+	renderPlanUpgradeNudge() {
 		return (
-			<UpgradeNudge
-				title={ translate( 'Get a free Custom Domain' ) }
-				message={ translate(
-					'Custom domains are free when you upgrade to a Premium or Business plan.'
-				) }
-				feature={ FEATURE_CUSTOM_DOMAIN }
-				event={ event }
-			/>
+			<div className={ classNames( 'domain-tip', this.props.className ) }>
+				<UpgradeNudge
+					event={ `domain_tip_${ this.props.event }` }
+					feature={ FEATURE_CUSTOM_DOMAIN }
+					shouldDisplay
+					message={ this.props.translate( 'Upgrade your plan to register a custom domain.' ) }
+					title={ this.props.translate( 'Get a custom domain' ) }
+				/>
+			</div>
 		);
 	}
 
 	render() {
-		const {
-			className,
-			domainsWithPlansOnly,
-			event,
-			site,
-			siteSlug,
-			suggestions,
-			translate,
-		} = this.props;
-
-		if (
-			! site ||
-			site.jetpack ||
-			! siteSlug ||
-			domainsWithPlansOnly ||
-			! isFreePlan( site.plan )
-		) {
-			return this.domainUpgradeNudge();
+		if ( this.props.isIneligible ) {
+			return null;
 		}
 
-		const classes = classNames( className, 'domain-tip' );
-		const { query, quantity, vendor } = getQueryObject( site, siteSlug );
-		const suggestion = suggestions ? suggestions[ 0 ] : null;
+		if ( this.props.shouldNudgePlanUpgrade ) {
+			return this.renderPlanUpgradeNudge();
+		}
 
+		const suggestion = Array.isArray( this.props.suggestions ) ? this.props.suggestions[ 0 ] : null;
+		let title = this.props.translate( 'Get a custom domain' );
+		if ( suggestion ) {
+			title = this.props.translate( '{{span}}%(domain)s{{/span}} is available!', {
+				args: { domain: suggestion.domain_name },
+				components: { span: <span className="domain-tip__suggestion" /> },
+			} );
+		}
+
+		const { query, quantity, vendor } = this.props.queryObject;
 		return (
-			<div className={ classes }>
+			<div className={ classNames( 'domain-tip', this.props.className ) }>
 				<QueryDomainsSuggestions query={ query } quantity={ quantity } vendor={ vendor } />
-				{ suggestion ? (
-					<UpgradeNudge
-						event={ `domain_tip_${ event }` }
-						shouldDisplay={ this.noticeShouldDisplay }
-						feature={ FEATURE_CUSTOM_DOMAIN }
-						title={ translate( '{{span}}%(domain)s{{/span}} is available!', {
-							args: { domain: suggestion.domain_name },
-							components: {
-								span: <span className="domain-tip__suggestion" />,
-							},
-						} ) }
-						message={ translate( 'Upgrade your plan to register a domain.' ) }
-						href={ `/domains/add/${ siteSlug }` }
-					/>
-				) : (
-					this.domainUpgradeNudge()
-				) }
+				<UpgradeNudge
+					event={ `domain_tip_${ this.props.event }` }
+					feature={ FEATURE_CUSTOM_DOMAIN }
+					href={ `/domains/add/${ this.props.siteSlug }` }
+					message={
+						this.props.hasDomainCredit
+							? this.props.translate( 'Your plan includes a free custom domain. Grab this one!' )
+							: this.props.translate( 'Purchase a custom domain for your site.' )
+					}
+					shouldDisplay
+					title={ title }
+				/>
 			</div>
 		);
 	}
@@ -120,11 +105,18 @@ export default connect( ( state, ownProps ) => {
 	const site = getSite( state, ownProps.siteId );
 	const siteSlug = getSiteSlug( state, ownProps.siteId );
 	const queryObject = getQueryObject( site, siteSlug );
+	const domainsWithPlansOnly = currentUserHasFlag( state, DOMAINS_WITH_PLANS_ONLY );
+	const isPaidWithoutDomainCredit =
+		site && siteSlug && ! isFreePlan( site.plan ) && ! hasDomainCredit( state, ownProps.siteId );
+	const isIneligible = ! site || ! siteSlug || site.jetpack || isPaidWithoutDomainCredit;
 
 	return {
+		hasDomainCredit: hasDomainCredit( state, ownProps.siteId ),
+		isIneligible,
+		queryObject,
+		shouldNudgePlanUpgrade: ! isIneligible && isFreePlan( site.plan ) && domainsWithPlansOnly,
+		site,
+		siteSlug,
 		suggestions: queryObject && getDomainsSuggestions( state, queryObject ),
-		domainsWithPlansOnly: currentUserHasFlag( state, DOMAINS_WITH_PLANS_ONLY ),
-		site: site,
-		siteSlug: siteSlug,
 	};
 } )( localize( DomainTip ) );
