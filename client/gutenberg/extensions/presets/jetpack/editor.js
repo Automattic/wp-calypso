83e78686e160e7301911950baf5b16a2cb55b5f8
Marin Atanasov <tyxla@abv.bg>
2018-09-11 12:55:07 +0300

    Rename Markdown block main entry point file

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -3,5 +3,5 @@
 /**
  * Internal dependencies
  */
-import 'gutenberg/extensions/markdown/jetpack-markdown-block.jsx';
+import 'gutenberg/extensions/markdown/index.jsx';
 import 'gutenberg/extensions/tiled-gallery/tiled-gallery.jsx';
