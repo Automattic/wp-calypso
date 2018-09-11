7ea953fb3d1896e102f88094a44d19d149fde35e
Marin Atanasov <tyxla@abv.bg>
2018-09-11 13:20:39 +0300

    Simplify renderer

--- client/gutenberg/extensions/markdown/renderer.jsx
+++ client/gutenberg/extensions/markdown/renderer.jsx
@@ -11,16 +11,8 @@ import { RawHTML } from '@wordpress/element';
  */
 const markdownConverter = new MarkdownIt();
 
-const MarkdownRenderer = function( props ) {
-	const { className, source } = props;
-
-	let content = '';
-
-	if ( source ) {
-		// converts the markdown source to HTML
-		content = markdownConverter.render( source );
-	}
-	return <RawHTML className={ className }>{ content }</RawHTML>;
-};
-
-export default MarkdownRenderer;
+export default ( { className, source } ) => (
+	<RawHTML className={ className }>
+		{ source.length ? markdownConverter.render( source ) : '' }
+	</RawHTML>
+);
