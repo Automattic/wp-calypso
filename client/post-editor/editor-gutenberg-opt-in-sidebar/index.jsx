8f4f89f43318f25834389a78aa078e0380d6a719
Kirk Wight <kwight@kwight.ca>
2018-09-11 13:27:47 -0700

    Make entire Gutenberg opt-in visual clickable.

--- client/post-editor/editor-gutenberg-opt-in-sidebar/index.jsx
+++ client/post-editor/editor-gutenberg-opt-in-sidebar/index.jsx
@@ -5,7 +5,7 @@
  */
 
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 
 /**
@@ -15,16 +15,35 @@ import { localize } from 'i18n-calypso';
 import Button from 'components/button';
 import { showGutenbergOptInDialog } from 'state/ui/gutenberg-opt-in-dialog/actions';
 
-function EditorGutenbergOptInSidebar( { translate, showDialog } ) {
-	return (
-		<div className="editor-gutenberg-opt-in-sidebar">
-			<img src="/calypso/images/illustrations/gutenberg-mini.svg" alt="" />
-			<p>{ translate( 'Try our new editor and level-up your layout.' ) }</p>
-			<Button onClick={ showDialog } action="show">
-				{ translate( 'Learn more' ) }
-			</Button>
-		</div>
-	);
+class EditorGutenbergOptInSidebar extends PureComponent {
+	static propTypes = {
+		translate: PropTypes.func,
+		showDialog: PropTypes.func,
+	};
+
+	handleKeyPress = event => {
+		if ( event.key === 'Enter' || event.key === ' ' ) {
+			event.preventDefault();
+			this.props.showDialog();
+		}
+	};
+
+	render() {
+		const { showDialog, translate } = this.props;
+		return (
+			<div
+				tabIndex="0"
+				role="button"
+				className="editor-gutenberg-opt-in-sidebar"
+				onClick={ showDialog }
+				onKeyPress={ this.handleKeyPress }
+			>
+				<img src="/calypso/images/illustrations/gutenberg-mini.svg" alt="" />
+				<p>{ translate( 'Try our new editor and level-up your layout.' ) }</p>
+				<Button tabIndex="-1">{ translate( 'Learn more' ) }</Button>
+			</div>
+		);
+	}
 }
 
 EditorGutenbergOptInSidebar.propTypes = {
