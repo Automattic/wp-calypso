1c5d51bfabf917804444cff831bf8efec2efec04
Bernie Reiter <ockham@raz.or.at>
2018-09-11 11:59:53 +0200

    Framework: Pull in i18n-calypso

---
+++ packages/i18n/test/data/index.js
@@ -0,0 +1,189 @@
+/*eslint-disable quote-props */
+var locale = {
+	'': {
+		localeSlug: 'de',
+		'Plural-Forms': 'nplurals=2; plural=n != 1;',
+		'MIME-Version': '1.0',
+		'X-Generator': 'GlotPress/0.1',
+		'Content-Type': 'text/plain; charset=UTF-8',
+		'PO-Revision-Date': '2014-08-11 23:50:52+0000',
+		'Content-Transfer-Encoding': '8bit',
+		'Project-Id-Version': 'WordPress.com'
+	},
+	'test1': [
+		null,
+		'translation1'
+	],
+	'test2': null,
+	'thecontext\u0004test3': [
+		null,
+		'translation3'
+	],
+	'test4': [
+		null,
+		'translation4'
+	],
+	'test-will-overwrite': [
+		null,
+		'translation1'
+	],
+	'Featured': [
+		null,
+		'translation-without-context'
+	],
+	'plural-test': [
+		'plural-test',
+		'plural-test singular translation',
+		'plural-test multiple translation'
+	],
+	'plural-test new syntax': [
+		'plural-test new syntax',
+		'plural-test new syntax translated, single',
+		'plural-test new syntax translated, plural'
+	],
+	'%(numberSelected)s site selected': [
+		'%(numberSelected)s sites selected',
+		'translation singular',
+		'translation plural'
+	],
+	'Navigation item\u0004Menu': null,
+	'Section title\u0004Reader': null,
+	'Section title\u0004My Sites': null,
+	'Section title\u0004Stats': null,
+	'Navigation item\u0004Settings': null,
+	'Navigation item\u0004Help & Support': null,
+	'Navigation item\u0004Find Friends': null,
+	'Navigation item\u0004Sign Out': null,
+	'general site settings\u0004General': null,
+	'Menu item\u0004Plugins': null,
+	'Menu item\u0004Jetpack Features': null,
+	'verb\u0004Update': null,
+	'Author byline\u0004By': null,
+	'Visit Plugin Site': [
+		null,
+		''
+	],
+	'Activate': [
+		null,
+		'Aktivieren'
+	],
+	'Search...': [
+		null,
+		'Suchen...'
+	],
+	'Select None': [
+		null,
+		''
+	],
+	'Select All': [
+		null,
+		'Alle auswählen'
+	],
+	'All sites selected': [
+		null,
+		''
+	],
+	'%(numberTotal)s site total': [
+		'%(numberTotal)s sites total',
+		'',
+		''
+	],
+	'No sites found': [
+		null,
+		'Keine Websites gefunden'
+	],
+	'Pages': [
+		null,
+		'Seiten'
+	],
+	'Stats': [
+		null,
+		'Statistik'
+	],
+	'Sharing': [
+		null,
+		'Sharing'
+	],
+	'Settings': [
+		null,
+		'Einstellungen'
+	],
+	'Manage': [
+		null,
+		'Verwalten'
+	],
+	'Customize': [
+		null,
+		'Anpassen'
+	],
+	'Domains': [
+		null,
+		'Domains'
+	],
+	'Upgrade': [
+		null,
+		'Aktualisierung'
+	],
+	'Posts': [
+		null,
+		'Beiträge'
+	],
+	'Theme': [
+		null,
+		'Theme'
+	],
+	'Open site in a new tab': [
+		null,
+		''
+	],
+	'Filter Label\u0004All Sites': null,
+	'Filter label for site list\u0004Public': null,
+	'Filter label for site list\u0004Private': null,
+	'future time\u0004in %s': null,
+	'a few seconds': [
+		null,
+		'einige Sekunden'
+	],
+	'a minute': [
+		null,
+		'eine Minute'
+	],
+	'%d minutes': [
+		null,
+		'%d Minuten'
+	],
+	'%d hours': [
+		null,
+		'%d Stunden'
+	],
+	'%d days': [
+		null,
+		'%d Tage'
+	],
+	'a month': [
+		null,
+		'ein Monat'
+	],
+	'%d months': [
+		null,
+		'%d Monaten'
+	],
+	'a year': [
+		null,
+		'ein Jahr'
+	],
+	'%d years': [
+		null,
+		'%d Jahre'
+	],
+	'number_format_thousands_sep': [
+		null,
+		'.'
+	],
+	'number_format_decimals': [
+		null,
+		','
+	]
+};
+
+exports.locale = locale;
