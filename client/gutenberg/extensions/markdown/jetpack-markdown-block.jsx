e8148651b7b00ce3354fa212cec157c77f4f6423
Marin Atanasov <tyxla@abv.bg>
2018-09-11 11:02:26 +0300

    Use a Fragment for block description

--- client/gutenberg/extensions/markdown/jetpack-markdown-block.jsx
+++ client/gutenberg/extensions/markdown/jetpack-markdown-block.jsx
@@ -4,6 +4,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Fragment } from '@wordpress/element';
 import { registerBlockType } from '@wordpress/blocks';
 
 /**
@@ -16,12 +17,14 @@ import JetpackMarkdownBlockSave from './jetpack-markdown-block-save';
 registerBlockType( 'a8c/markdown', {
 	title: __( 'Markdown' ),
 
-	description: [
-		__( 'Write your content in plain-text Markdown syntax.' ),
-		<p>
-			<a href="https://en.support.wordpress.com/markdown-quick-reference/">Support Reference</a>
-		</p>,
-	],
+	description: (
+		<Fragment>
+			{ __( 'Write your content in plain-text Markdown syntax.' ) }
+			<p>
+				<a href="https://en.support.wordpress.com/markdown-quick-reference/">Support Reference</a>
+			</p>
+		</Fragment>
+	),
 
 	icon: (
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 208 128">
