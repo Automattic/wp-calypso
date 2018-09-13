8ed9ba5444eeb4888ce2a319457164c1c0aacb16
Jarda Snajdr <jsnajdr@gmail.com>
2018-09-13 14:59:35 +0200

    Guided Tours: check for onTargetDisappear on current step on every DOM mutation (#27178)
    * Guided Tour Step: check for onTargetDisappear on current step on every DOM mutation
    
    This patch fixes a crash where the Mutation Observer tries to call `onTargetDisappear`
    on a `Step` that doesn't have the prop. One of previous steps had it, that's why the
    Mutation Observer was initialized, but a following step doesn't need to.
    
    The observer is removed only at the end of the tour, after all steps are executed.
    
    * Simple Payment Email Guided Tour: remove noop onTargetDisappear
    
    It was a hack to prevent crashing. After the bug was fixed in the previous commit, the hack
    is no longer necessary.

--- client/layout/guided-tours/config-elements/step.js
+++ client/layout/guided-tours/config-elements/step.js
@@ -178,9 +178,15 @@ export default class Step extends Component {
 
 		if ( ! this.observer ) {
 			this.observer = new MutationObserver( () => {
-				const target = document.querySelector( `[data-tip-target="${ this.props.target }"]` );
-				if ( ! target ) {
-					this.props.onTargetDisappear( {
+				const { target, onTargetDisappear } = this.props;
+
+				if ( ! target || ! onTargetDisappear ) {
+					return;
+				}
+
+				const targetEl = document.querySelector( `[data-tip-target="${ target }"]` );
+				if ( ! targetEl ) {
+					onTargetDisappear( {
 						quit: () => this.context.quit( this.context ),
 						next: () => this.skipToNext( this.props, this.context ),
 					} );
