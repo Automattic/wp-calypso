84f516ee80ae174444245d9541b4290094d61fb2
Jon Surrell <jon.surrell@automattic.com>
2018-09-13 13:25:02 +0200

    Use strokeWidth over stroke-width for SVG
    React expects camelCased svg property names and shows a warning with
    stroke-width.

--- client/gutenberg/extensions/markdown/editor.jsx
+++ client/gutenberg/extensions/markdown/editor.jsx
@@ -36,7 +36,7 @@ registerBlockType( 'a8c/markdown', {
 				y="5"
 				ry="10"
 				stroke="#000"
-				stroke-width="10"
+				strokeWidth="10"
 				fill="none"
 			/>
 			<path d="M30 98v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39zM155 98l-30-33h20v-35h20v35h20z" />
