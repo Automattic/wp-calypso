2d10db55c361070dc3462715e10078742983cc58
Kirk Wight <kwight@kwight.ca>
2018-09-13 13:19:17 -0700

    Remove `GUTENBERG_OPT_IN` action creators.

--- client/state/ui/gutenberg-opt-in-dialog/actions.js
+++ client/state/ui/gutenberg-opt-in-dialog/actions.js
@@ -3,7 +3,7 @@
 /**
  * Internal dependencies
  */
-import { GUTENBERG_OPT_IN, GUTENBERG_OPT_IN_DIALOG_IS_SHOWING } from 'state/action-types';
+import { GUTENBERG_OPT_IN_DIALOG_IS_SHOWING } from 'state/action-types';
 
 export function showGutenbergOptInDialog() {
 	return {
@@ -16,19 +16,5 @@ export function hideGutenbergOptInDialog() {
 	return {
 		type: GUTENBERG_OPT_IN_DIALOG_IS_SHOWING,
 		isShowing: false,
-	};
-}
-
-export function optInToGutenberg() {
-	return {
-		type: GUTENBERG_OPT_IN,
-		hasOptedIn: true,
-	};
-}
-
-export function optOutOfGutenberg() {
-	return {
-		type: GUTENBERG_OPT_IN,
-		hasOptedIn: false,
 	};
 }
