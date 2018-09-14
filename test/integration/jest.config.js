cd0458efc515fb7b54b5ff42ddef4338173642df
Jon Surrell <jon.surrell@automattic.com>
2018-09-14 18:04:49 +0200

    Persist Jest caches between builds

--- test/integration/jest.config.js
+++ test/integration/jest.config.js
@@ -1,19 +1,22 @@
 /** @format */
 
-module.exports = {
-	modulePaths: [
-		'<rootDir>/test/',
-		'<rootDir>/server/',
-		'<rootDir>/client/',
-		'<rootDir>/client/extensions/',
-	],
-	rootDir: './../../',
-	testEnvironment: 'node',
-	testMatch: [
-		'<rootDir>/bin/**/integration/*.js',
-		'<rootDir>/client/**/integration/*.js',
-		'<rootDir>/server/**/integration/*.js',
-		'<rootDir>/test/test/helpers/**/integration/*.js',
-	],
-	verbose: false,
-};
+module.exports = Object.assign(
+	{
+		modulePaths: [
+			'<rootDir>/test/',
+			'<rootDir>/server/',
+			'<rootDir>/client/',
+			'<rootDir>/client/extensions/',
+		],
+		rootDir: './../../',
+		testEnvironment: 'node',
+		testMatch: [
+			'<rootDir>/bin/**/integration/*.js',
+			'<rootDir>/client/**/integration/*.js',
+			'<rootDir>/server/**/integration/*.js',
+			'<rootDir>/test/test/helpers/**/integration/*.js',
+		],
+		verbose: false,
+	},
+	process.env.CIRCLECI && process.env.HOME && { cacheDirectory: `${ process.env.HOME }/jest-cache` }
+);
--- test/server/jest.config.js
+++ test/server/jest.config.js
@@ -1,20 +1,23 @@
 /** @format */
 
-module.exports = {
-	collectCoverageFrom: [ 'server/**/*.js?(x)' ],
-	coveragePathIgnorePatterns: [ '<rootDir>/server/devdocs/search-index.js' ],
-	modulePaths: [
-		'<rootDir>/test/',
-		'<rootDir>/server/',
-		'<rootDir>/client/',
-		'<rootDir>/client/extensions/',
-	],
-	rootDir: './../../',
-	roots: [ '<rootDir>/server/' ],
-	testEnvironment: 'node',
-	transformIgnorePatterns: [ 'node_modules[\\/\\\\](?!redux-form)' ],
-	testMatch: [ '<rootDir>/server/**/test/*.js?(x)' ],
-	timers: 'fake',
-	setupTestFrameworkScriptFile: '<rootDir>/test/server/setup-test-framework.js',
-	verbose: false,
-};
+module.exports = Object.assign(
+	{
+		collectCoverageFrom: [ 'server/**/*.js?(x)' ],
+		coveragePathIgnorePatterns: [ '<rootDir>/server/devdocs/search-index.js' ],
+		modulePaths: [
+			'<rootDir>/test/',
+			'<rootDir>/server/',
+			'<rootDir>/client/',
+			'<rootDir>/client/extensions/',
+		],
+		rootDir: './../../',
+		roots: [ '<rootDir>/server/' ],
+		testEnvironment: 'node',
+		transformIgnorePatterns: [ 'node_modules[\\/\\\\](?!redux-form)' ],
+		testMatch: [ '<rootDir>/server/**/test/*.js?(x)' ],
+		timers: 'fake',
+		setupTestFrameworkScriptFile: '<rootDir>/test/server/setup-test-framework.js',
+		verbose: false,
+	},
+	process.env.CIRCLECI && process.env.HOME && { cacheDirectory: `${ process.env.HOME }/jest-cache` }
+);
