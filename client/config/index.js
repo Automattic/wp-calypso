a048b6877992ea03173db6b8bcbe9035cdd9afbe
Jarda Snajdr <jsnajdr@gmail.com>
2018-09-15 10:46:12 +0200

    Allow flags=... query params for calypso.live subdomains
    Another piece of code that needs to be updated for calypso.live subdomains: the test whether
    to allow `flags=...` query params or to ignore them.
    
    Drive-by fix: misplaced `eslint-ignore-next-line` broken by Prettier formatting

--- client/config/index.js
+++ client/config/index.js
@@ -18,10 +18,16 @@ if ( 'undefined' === typeof window || ! window.configData ) {
 
 const configData = window.configData;
 
+// https://calypso.live matches
+// https://calypso.live/hello matches
+// https://hash-abcd1234.calypso.live matches
+// https://calypso.live.com doesn't match
+const CALYPSO_LIVE_REGEX = /^https:\/\/([a-zA-Z0-9-]+\.)?calypso.live($|\/)/;
+
 if (
 	process.env.NODE_ENV === 'development' ||
 	configData.env_id === 'stage' ||
-	( window && window.location.href.indexOf( 'https://calypso.live' ) === 0 )
+	( window && CALYPSO_LIVE_REGEX.test( window.location.href ) )
 ) {
 	const match =
 		document.location.search && document.location.search.match( /[?&]flags=([^&]+)(&|$)/ );
@@ -31,8 +37,8 @@ if (
 			const flag = flagRaw.replace( /^[-+]/, '' );
 			const enabled = ! /^-/.test( flagRaw );
 			configData.features[ flag ] = enabled;
+			// eslint-disable-next-line no-console
 			console.log(
-				// eslint-disable-line no-console
 				'%cConfig flag %s via URL: %s',
 				'font-weight: bold;',
 				enabled ? 'enabled' : 'disabled',
