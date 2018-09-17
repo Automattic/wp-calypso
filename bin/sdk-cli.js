d39175877a5f94acee4383a078d554b372758a6d
Bernie Reiter <ockham@raz.or.at>
2018-09-17 21:58:01 +0200

    Remove output file name options

--- bin/sdk-cli.js
+++ bin/sdk-cli.js
@@ -91,16 +91,6 @@ yargs
 					coerce: path.resolve,
 					requiresArg: true,
 				},
-				'output-editor-file': {
-					description: 'Name of the built editor script output file (without the file extension).',
-					type: 'string',
-					requiresArg: true,
-				},
-				'output-view-file': {
-					description: 'Name of the built view script output file (without the file extension).',
-					type: 'string',
-					requiresArg: true,
-				},
 				watch: {
 					alias: 'w',
 					description: 'Whether to watch for changes and automatically rebuild.',
