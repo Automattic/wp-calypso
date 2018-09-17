8dd7d980de8800a832a54b54163e0c66bb70ce11
Jon Surrell <jon.surrell@automattic.com>
2018-09-17 10:09:58 +0200

    Move cacheDirectory option out of config

--- test/client/jest.config.js
+++ test/client/jest.config.js
@@ -1,26 +1,23 @@
 /** @format */
 
-module.exports = Object.assign(
-	{
-		moduleNameMapper: {
-			'^config$': '<rootDir>/server/config/index.js',
-		},
-		transform: {
-			'^.+\\.jsx?$': 'babel-jest',
-			'\\.(svg|(sc|sa|c)ss)$': '<rootDir>/test/test/helpers/assets/transform.js',
-		},
-		modulePaths: [ '<rootDir>/test/', '<rootDir>/client/', '<rootDir>/client/extensions/' ],
-		rootDir: './../../',
-		roots: [ '<rootDir>/client/' ],
-		testEnvironment: 'node',
-		transformIgnorePatterns: [ 'node_modules[\\/\\\\](?!flag-icon-css|redux-form)' ],
-		testMatch: [ '<rootDir>/client/**/test/*.js?(x)' ],
-		testURL: 'https://example.com',
-		setupTestFrameworkScriptFile: '<rootDir>/test/client/setup-test-framework.js',
-		verbose: false,
-		globals: {
-			google: {},
-		},
+module.exports = {
+	moduleNameMapper: {
+		'^config$': '<rootDir>/server/config/index.js',
 	},
-	process.env.CIRCLECI && process.env.HOME && { cacheDirectory: `${ process.env.HOME }/jest-cache` }
-);
+	transform: {
+		'^.+\\.jsx?$': 'babel-jest',
+		'\\.(svg|(sc|sa|c)ss)$': '<rootDir>/test/test/helpers/assets/transform.js',
+	},
+	modulePaths: [ '<rootDir>/test/', '<rootDir>/client/', '<rootDir>/client/extensions/' ],
+	rootDir: './../../',
+	roots: [ '<rootDir>/client/' ],
+	testEnvironment: 'node',
+	transformIgnorePatterns: [ 'node_modules[\\/\\\\](?!flag-icon-css|redux-form)' ],
+	testMatch: [ '<rootDir>/client/**/test/*.js?(x)' ],
+	testURL: 'https://example.com',
+	setupTestFrameworkScriptFile: '<rootDir>/test/client/setup-test-framework.js',
+	verbose: false,
+	globals: {
+		google: {},
+	},
+};
--- test/integration/jest.config.js
+++ test/integration/jest.config.js
@@ -1,22 +1,19 @@
 /** @format */
 
-module.exports = Object.assign(
-	{
-		modulePaths: [
-			'<rootDir>/test/',
-			'<rootDir>/server/',
-			'<rootDir>/client/',
-			'<rootDir>/client/extensions/',
-		],
-		rootDir: './../../',
-		testEnvironment: 'node',
-		testMatch: [
-			'<rootDir>/bin/**/integration/*.js',
-			'<rootDir>/client/**/integration/*.js',
-			'<rootDir>/server/**/integration/*.js',
-			'<rootDir>/test/test/helpers/**/integration/*.js',
-		],
-		verbose: false,
-	},
-	process.env.CIRCLECI && process.env.HOME && { cacheDirectory: `${ process.env.HOME }/jest-cache` }
-);
+module.exports = {
+	modulePaths: [
+		'<rootDir>/test/',
+		'<rootDir>/server/',
+		'<rootDir>/client/',
+		'<rootDir>/client/extensions/',
+	],
+	rootDir: './../../',
+	testEnvironment: 'node',
+	testMatch: [
+		'<rootDir>/bin/**/integration/*.js',
+		'<rootDir>/client/**/integration/*.js',
+		'<rootDir>/server/**/integration/*.js',
+		'<rootDir>/test/test/helpers/**/integration/*.js',
+	],
+	verbose: false,
+};
