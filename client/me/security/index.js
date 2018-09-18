c1751ce70c4dfab9082b60bc25b8fa61a99e9317
bluefuton <chris@bluefuton.com>
2018-09-18 14:36:31 +1200

    Me: add logged-out redirect for me/security/*

--- client/me/security/index.js
+++ client/me/security/index.js
@@ -9,7 +9,7 @@ import page from 'page';
  * Internal dependencies
  */
 import config from 'config';
-import { makeLayout, render as clientRender } from 'controller';
+import { makeLayout, redirectLoggedOut, render as clientRender } from 'controller';
 import { sidebar } from 'me/controller';
 import {
 	accountRecovery,
@@ -20,21 +20,36 @@ import {
 } from './controller';
 
 export default function() {
-	page( '/me/security', sidebar, password, makeLayout, clientRender );
+	page( '/me/security', redirectLoggedOut, sidebar, password, makeLayout, clientRender );
 
 	if ( config.isEnabled( 'signup/social-management' ) ) {
-		page( '/me/security/social-login', sidebar, socialLogin, makeLayout, clientRender );
+		page(
+			'/me/security/social-login',
+			redirectLoggedOut,
+			sidebar,
+			socialLogin,
+			makeLayout,
+			clientRender
+		);
 	}
 
-	page( '/me/security/two-step', sidebar, twoStep, makeLayout, clientRender );
+	page( '/me/security/two-step', redirectLoggedOut, sidebar, twoStep, makeLayout, clientRender );
 
 	page(
 		'/me/security/connected-applications',
+		redirectLoggedOut,
 		sidebar,
 		connectedApplications,
 		makeLayout,
 		clientRender
 	);
 
-	page( '/me/security/account-recovery', sidebar, accountRecovery, makeLayout, clientRender );
+	page(
+		'/me/security/account-recovery',
+		redirectLoggedOut,
+		sidebar,
+		accountRecovery,
+		makeLayout,
+		clientRender
+	);
 }
