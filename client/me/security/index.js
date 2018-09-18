e51a95fc98365cc9df2fb72cc37361a4770f33db
bluefuton <chris@bluefuton.com>
2018-09-19 11:05:16 +1200

    Reduce duplication of redirectLoggedOut

--- client/me/security/index.js
+++ client/me/security/index.js
@@ -21,35 +21,21 @@ import {
 
 export default function() {
 	page( '/me/security', redirectLoggedOut, sidebar, password, makeLayout, clientRender );
+	page( '/me/security/*', redirectLoggedOut );
 
 	if ( config.isEnabled( 'signup/social-management' ) ) {
-		page(
-			'/me/security/social-login',
-			redirectLoggedOut,
-			sidebar,
-			socialLogin,
-			makeLayout,
-			clientRender
-		);
+		page( '/me/security/social-login', sidebar, socialLogin, makeLayout, clientRender );
 	}
 
-	page( '/me/security/two-step', redirectLoggedOut, sidebar, twoStep, makeLayout, clientRender );
+	page( '/me/security/two-step', sidebar, twoStep, makeLayout, clientRender );
 
 	page(
 		'/me/security/connected-applications',
-		redirectLoggedOut,
 		sidebar,
 		connectedApplications,
 		makeLayout,
 		clientRender
 	);
 
-	page(
-		'/me/security/account-recovery',
-		redirectLoggedOut,
-		sidebar,
-		accountRecovery,
-		makeLayout,
-		clientRender
-	);
+	page( '/me/security/account-recovery', sidebar, accountRecovery, makeLayout, clientRender );
 }
