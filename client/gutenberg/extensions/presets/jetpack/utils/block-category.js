45770052723961331488d57d26cd7196055f7fc3
Marin Atanasov <tyxla@abv.bg>
2018-09-18 13:07:58 +0300

    Omit localization from Jetpack term

--- client/gutenberg/extensions/presets/jetpack/utils/block-category.js
+++ client/gutenberg/extensions/presets/jetpack/utils/block-category.js
@@ -3,11 +3,10 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { getCategories, setCategories } from '@wordpress/blocks';
 
 setCategories( [
 	// Add a Jetpack block category
-	{ slug: 'jetpack', title: __( 'Jetpack' ) },
+	{ slug: 'jetpack', title: 'Jetpack' },
 	...getCategories().filter( ( { slug } ) => slug !== 'jetpack' ),
 ] );
