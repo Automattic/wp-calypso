1e6bdcd1005d0b66ed86bd0ef47b40478d3a6d54
edwinfrc <edwinfrc@github.com>
2018-09-18 12:14:48 -0600

    3

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-/**
- * External dependencies.
- */
 var boot = require( 'boot' ),
-	http = require( 'http' );
-
-/**
- * Internal dependencies
- */
-var pkg = require( './package.json' ),
-	config = require( 'config' );
-
-var start = Date.now(),
+	http = require( 'http' ),
+	pkg = require( './package.json' ),
+	config = require( 'config' ),
+	start = Date.now(),
 	port = process.env.PORT || 3000,
 	app = boot(),
 	server,
 	hotReloader;
-
-console.log( '%s booted in %dms - port: %s', pkg.name, ( Date.now() ) - start, port );
-server = http.createServer( app );
-server.listen( port );
-
+//console.log( '%s booted in %dms - port: %s', pkg.name, ( Date.now() ) - start, port );
+server = http.createServer(app);
+server.listen(port);
 // Enable hot reloader in development
 if ( config( 'env' ) === 'development' ) {
 	hotReloader = require( 'bundler/hot-reloader' );
