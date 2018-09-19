a64658b41b903284082641166f741db8aa7b3170
Jason Moon <jason.moon@automattic.com>
2018-09-19 17:33:19 -0600

    Analytics: Add build_timestamp to Tracks page view

--- client/lib/analytics/index.js
+++ client/lib/analytics/index.js
@@ -370,8 +370,9 @@ const analytics = {
 
 		recordPageView: function( urlPath, params ) {
 			let eventProperties = {
-				path: urlPath,
+				build_timestamp: BUILD_TIMESTAMP,
 				do_not_track: doNotTrack() ? 1 : 0,
+				path: urlPath,
 			};
 
 			// add optional path params
