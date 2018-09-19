b6c5be23e124dcd87530a964c4c5f8ffe0b37e9b
Rocco Tripaldi <tripaldir@gmail.com>
2018-09-19 15:41:19 -0400

    Activity Log: apply button (#27303)
    * This a clean-up PR that moves all of the logic date filter and type filter
    out of index and into their respective components.
    
    It allowed us to have more control of the type filter, and add an apply button.
    
    * Fix Make the selection more intuitive.

---
+++ client/state/data-layer/wpcom/sites/activity-types/from-api.js
@@ -0,0 +1,31 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { get, forOwn } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import apiResponseSchema from './schema';
+import makeJsonSchemaParser from 'lib/make-json-schema-parser';
+
+/**
+ * Transforms API response into array of activities
+ *
+ * @param  {object} apiResponse API response body
+ * @return {object}             Object with an entry for proccessed item objects and another for oldest item timestamp
+ */
+export function transformer( apiResponse ) {
+	const groups = [];
+	forOwn( get( apiResponse, [ 'groups' ], {} ), ( group, slug ) => {
+		groups.push( {
+			key: slug,
+			name: group.name,
+			count: group.count,
+		} );
+	} );
+	return groups;
+}
+// fromApi default export
+export default makeJsonSchemaParser( apiResponseSchema, transformer );
