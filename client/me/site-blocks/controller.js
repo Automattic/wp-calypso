020b06b56404dc274fadfed6592c974cc08019b5
bluefuton <chris@bluefuton.com>
2018-09-20 16:36:50 +1200

    Add route, controller and sidebar item

---
+++ client/me/site-blocks/controller.js
@@ -0,0 +1,16 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import SiteBlockListComponent from 'me/site-blocks/main';
+
+export function siteBlockList( context, next ) {
+	context.primary = React.createElement( SiteBlockListComponent );
+	next();
+}
