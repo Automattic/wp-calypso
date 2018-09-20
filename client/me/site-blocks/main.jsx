37ee644d109d6b8550d26f01ebda8b2070799d4a
bluefuton <chris@bluefuton.com>
2018-09-20 16:46:11 +1200

    Add basic components

--- client/me/site-blocks/main.jsx
+++ client/me/site-blocks/main.jsx
@@ -10,18 +10,34 @@ import classnames from 'classnames';
 /**
  * Internal dependencies
  */
+import Card from 'components/card';
+import DocumentHead from 'components/data/document-head';
+import Main from 'components/main';
+import SectionHeader from 'components/section-header';
+import MeSidebarNavigation from 'me/sidebar-navigation';
+import PageViewTracker from 'lib/analytics/page-view-tracker';
 
 class SiteBlockList extends Component {
 	render() {
-		const { isLoading } = this.props;
+		const { translate, isLoading } = this.props;
 		const containerClasses = classnames( 'site-block-list', 'main', {
 			'is-loading': isLoading,
 		} );
 
 		return (
-			<div className={ containerClasses } role="main">
-				Site block list
-			</div>
+			<Main className={ containerClasses }>
+				<PageViewTracker path="/me/site-blocks" title="Me > Blocked Sites" />
+				<DocumentHead title={ translate( 'Blocked Sites' ) } />
+				<MeSidebarNavigation />
+				<SectionHeader label={ translate( 'Blocked Sites' ) } />
+				<Card className="site-blocks__intro">
+					<p>
+						{ translate(
+							'Blocked sites will not appear in your Reader and will not be recommended to you.'
+						) }
+					</p>
+				</Card>
+			</Main>
 		);
 	}
 }
