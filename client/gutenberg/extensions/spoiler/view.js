f4457e62c33a87364a295f732da7f8ddf283337b
Kerry Liu <kerryliu@gmail.com>
2018-09-22 14:29:45 -0700

    do not blur in editor when user is typing. Also update from hover effect to click handler

--- client/gutenberg/extensions/spoiler/view.js
+++ client/gutenberg/extensions/spoiler/view.js
@@ -4,3 +4,14 @@
  * Internal dependencies
  */
 import './style.scss';
+
+function toggleSpoiler( event ) {
+	if ( event.target.matches( '.wp-block-a8c-spoiler' ) ) {
+		event.target.classList.toggle( 'is-visible' );
+	}
+}
+
+document.addEventListener( 'DOMContentLoaded', () => {
+	//add click handler on body to check click events for any spoiler tags
+	document.body.addEventListener( 'click', toggleSpoiler );
+} );
