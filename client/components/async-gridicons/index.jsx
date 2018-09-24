4f108f7eff22587f739d7b8c329b6bf57aa58541
Markus Dobmann <markus@mpnjs.com>
2018-09-24 17:33:50 +0200

    Revert "AsyncGridicon: provide 'empty' fallback icon"
    This reverts commit 2fba28335f02fee1e058db7ee1b04fb3704a205e.

--- client/components/async-gridicons/index.jsx
+++ client/components/async-gridicons/index.jsx
@@ -9,18 +9,14 @@ import React from 'react';
  * Internal dependencies
  */
 import AsyncLoad from 'components/async-load';
-import FallbackIcon from './fallback';
 
 function AsyncGridicon( { icon = '', ...rest } ) {
 	return (
 		<AsyncLoad
 			require={ function( callback ) {
-				import( /* webpackChunkName: "gridicons-[request]" */ `gridicons/dist/${ icon }` )
-					.then( g => callback( g.default ) )
-					.catch( err => {
-						console.warn( `Error loading icon '${ icon }':`, err.message ); // eslint-disable-line no-console
-						callback( FallbackIcon );
-					} );
+				import( /* webpackChunkName: "gridicons-[request]" */ `gridicons/dist/${ icon }` ).then(
+					g => callback( g.default )
+				);
 			} }
 			placeholder={ null }
 			{ ...rest }
