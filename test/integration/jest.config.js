41c85554fbb123a71585f8ec1898abb53b440199
Jon Surrell <jon.surrell@automattic.com>
2018-09-25 11:31:33 +0200

    CI: Overhaul for speed 🚀 (#27180)
    * Use a parallel CI workflow
    
    * Restructure CI config with references
    
    * Inline CircleCI test runners
    
    * Report eslint results
    
    * Use js for jest config
    
    * Persist Jest caches between builds
    
    * Better git caching
    
    * Cache node_modules
    
    * Reenable tests and further split
    
    * Run integration nightly
    
    * Remove CI splitting for non-parallel jobs
    
    * Test results by directory not filename
    
    * Try timings again with cache-bust
    
    * Add 2m timeout to Jest
    
    I've observed a hung test suite that waited the default
    10 minutes. Let's ensure that doesn't happen again.
    
    * Add docs describing the cache mechanisms
    
    * Replace CircleCI v1 notify with explicit curl

---
+++ test/integration/jest.config.js
@@ -0,0 +1,19 @@
+/** @format */
+
+module.exports = {
+	modulePaths: [
+		'<rootDir>/test/',
+		'<rootDir>/server/',
+		'<rootDir>/client/',
+		'<rootDir>/client/extensions/',
+	],
+	rootDir: './../../',
+	testEnvironment: 'node',
+	testMatch: [
+		'<rootDir>/bin/**/integration/*.js',
+		'<rootDir>/client/**/integration/*.js',
+		'<rootDir>/server/**/integration/*.js',
+		'<rootDir>/test/test/helpers/**/integration/*.js',
+	],
+	verbose: false,
+};
