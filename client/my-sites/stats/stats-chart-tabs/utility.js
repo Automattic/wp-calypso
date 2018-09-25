e84e831a507141312a74a8f6cd155ad412945b94
Jason Moon <jason.moon@automattic.com>
2018-09-24 18:12:53 -0600

    ChartTabs: Move date format function

--- client/my-sites/stats/stats-chart-tabs/utility.js
+++ client/my-sites/stats/stats-chart-tabs/utility.js
@@ -10,6 +10,23 @@ import moment from 'moment';
  */
 import { rangeOfPeriod } from 'state/stats/lists/utils';
 
+export function formatDate( date, period ) {
+	// NOTE: Consider localizing the dates, especially for the 'week' case.
+	const momentizedDate = moment( date );
+	switch ( period ) {
+		case 'day':
+			return momentizedDate.format( 'LL' );
+		case 'week':
+			return momentizedDate.format( 'L' ) + ' - ' + momentizedDate.add( 6, 'days' ).format( 'L' );
+		case 'month':
+			return momentizedDate.format( 'MMMM YYYY' );
+		case 'year':
+			return momentizedDate.format( 'YYYY' );
+		default:
+			return null;
+	}
+}
+
 export function getQueryDate( queryDate, timezoneOffset, period, quantity ) {
 	const momentSiteZone = moment().utcOffset( timezoneOffset );
 	const endOfPeriodDate = rangeOfPeriod( period, momentSiteZone.locale( 'en' ) ).endOf;
