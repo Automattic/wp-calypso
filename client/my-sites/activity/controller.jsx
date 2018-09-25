99653e296b359e44960c4632adb7b65cc3d14242
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-09-25 07:35:15 -0700

    Activity Log: Add new calypso_activitylog_view record (#27424)

--- client/my-sites/activity/controller.jsx
+++ client/my-sites/activity/controller.jsx
@@ -15,6 +15,44 @@ import getActivityLogFilter from 'state/selectors/get-activity-log-filter';
 import ActivityLog from 'my-sites/activity/activity-log';
 import { setFilter } from 'state/activity-log/actions';
 import { queryToFilterState } from 'state/activity-log/utils';
+import { recordTrack } from 'reader/stats';
+
+function queryFilterToStats( filter ) {
+	// These values are hardcoded so that the attributes that we collect via stats are not unbound
+	const possibleGroups = [
+		'attachment',
+		'comment',
+		'core',
+		'feedback',
+		'jetpack',
+		'menu',
+		'module',
+		'monitor',
+		'pingback',
+		'plan',
+		'plugin',
+		'post',
+		'protect',
+		'rewind',
+		'setting',
+		'theme',
+		'user',
+		'widget',
+	];
+
+	const groupStats = {};
+	possibleGroups.forEach( groupSlug => {
+		groupStats[ 'filter_group_' + groupSlug ] = !! (
+			filter.group && filter.group.indexOf( groupSlug ) >= 0
+		);
+	} );
+
+	return {
+		...groupStats,
+		filter_date_on: !! filter.on || !! filter.before || !! filter.after,
+		page: filter.page ? parseInt( filter.page ) : 1,
+	};
+}
 
 export function activity( context, next ) {
 	const state = context.store.getState();
@@ -33,6 +71,7 @@ export function activity( context, next ) {
 		} );
 	}
 
+	recordTrack( 'calypso_activitylog_view', queryFilterToStats( queryFilter ) );
 	context.primary = <ActivityLog siteId={ siteId } context={ context } startDate={ startDate } />;
 
 	next();
