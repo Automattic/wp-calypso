ca94cf59ad8e16cfc8c0da711be3753b426b975d
Jason Moon <jason.moon@automattic.com>
2018-09-24 18:03:36 -0600

    ChartTabs: Refactor query date determination

---
+++ client/my-sites/stats/stats-chart-tabs/utility.js
@@ -0,0 +1,23 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import moment from 'moment';
+
+/**
+ * Internal dependencies
+ */
+import { rangeOfPeriod } from 'state/stats/lists/utils';
+
+export function getQueryDate( queryDate, timezoneOffset, period, quantity ) {
+	const momentSiteZone = moment().utcOffset( timezoneOffset );
+	const endOfPeriodDate = rangeOfPeriod( period, momentSiteZone.locale( 'en' ) ).endOf;
+	const periodDifference = moment( endOfPeriodDate ).diff( moment( queryDate ), period );
+	if ( periodDifference >= quantity ) {
+		return moment( endOfPeriodDate )
+			.subtract( Math.floor( periodDifference / quantity ) * quantity, period )
+			.format( 'YYYY-MM-DD' );
+	}
+	return endOfPeriodDate;
+}
