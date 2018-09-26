eaf455ad3c45cc127f9856dba234f59ddd003314
Ben Lowery <ben.lowery@automattic.com>
2018-09-26 16:20:56 -0400

    a different take on the loader. AsyncLoad isn't really meant to be passed a function. It works mostly by chance, not intent. Instead, use import directly and force a re-render when the icon is available, or fails to load.

--- client/components/async-gridicons/index.jsx
+++ client/components/async-gridicons/index.jsx
@@ -3,29 +3,61 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
 
 /**
  * Internal dependencies
  */
-import AsyncLoad from 'components/async-load';
 import FallbackIcon from './fallback';
 
-function AsyncGridicon( { icon = '', ...rest } ) {
-	return (
-		<AsyncLoad
-			require={ function( callback ) {
-				import( /* webpackChunkName: "gridicons-[request]" */ `gridicons/dist/${ icon }` )
-					.then( g => callback( g.default ) )
-					.catch( err => {
-						console.warn( `Error loading icon '${ icon }':`, err.message ); // eslint-disable-line no-console
-						callback( FallbackIcon );
-					} );
-			} }
-			placeholder={ <FallbackIcon /> }
-			{ ...rest }
-		/>
+const loadedIcons = new Map();
+
+function loadIcon( icon ) {
+	return import( /* webpackChunkName: "gridicons-[request]" */
+	`gridicons/dist/${ icon }` ).then(
+		g => {
+			loadedIcons.set( icon, g.default );
+			return g.default;
+		},
+		err => {
+			loadedIcons.set( icon, false );
+			console.warn( `Error loading icon '${ icon }':`, err.message ); // eslint-disable-line no-console
+		}
 	);
+}
+
+class AsyncGridicon extends Component {
+	constructor( props ) {
+		super( props );
+	}
+	checkAndLoad() {
+		if ( ! loadedIcons.has( this.props.icon ) ) {
+			loadIcon( this.props.icon ).then( () => this.update() );
+		}
+	}
+
+	componentDidMount() {
+		this.checkAndLoad();
+	}
+	componentDidUpdate() {
+		this.checkAndLoad();
+	}
+
+	update = () => this.forceUpdate();
+
+	componentWillUnmount() {
+		this.update = () => {};
+	}
+
+	render() {
+		const { icon = '', ...rest } = this.props;
+		if ( loadedIcons.get( icon ) ) {
+			const Icon = loadedIcons.get( icon );
+			return <Icon { ...rest } />;
+		}
+
+		return <FallbackIcon { ...rest } />;
+	}
 }
 
 export default AsyncGridicon;
