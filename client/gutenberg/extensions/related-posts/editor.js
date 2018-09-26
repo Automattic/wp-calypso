b4806e1b49a8ccc1419eedb509dc70fd8ebf0dd2
Marin Atanasov <tyxla@abv.bg>
2018-09-26 15:31:33 +0300

    Gutenberg: Update Related Posts icon to use the Latest Posts one

--- client/gutenberg/extensions/related-posts/editor.js
+++ client/gutenberg/extensions/related-posts/editor.js
@@ -17,7 +17,18 @@ import { ALIGNMENT_OPTIONS, MAX_POSTS_TO_SHOW } from './constants';
 registerBlockType( 'a8c/related-posts', {
 	title: __( 'Related Posts' ),
 
-	icon: 'admin-page',
+	icon: (
+		<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+			<path d="M0,0h24v24H0V0z" fill="none" />
+			<rect x="11" y="7" width="6" height="2" />
+			<rect x="11" y="11" width="6" height="2" />
+			<rect x="11" y="15" width="6" height="2" />
+			<rect x="7" y="7" width="2" height="2" />
+			<rect x="7" y="11" width="2" height="2" />
+			<rect x="7" y="15" width="2" height="2" />
+			<path d="M20.1,3H3.9C3.4,3,3,3.4,3,3.9v16.2C3,20.5,3.4,21,3.9,21h16.2c0.4,0,0.9-0.5,0.9-0.9V3.9C21,3.4,20.5,3,20.1,3z M19,19H5V5h14V19z" />
+		</svg>
+	),
 
 	category: 'layout',
 
