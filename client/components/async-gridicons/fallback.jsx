2ff22e3f45e5fe662ab0e737961a1466a3d7b371
Ben Lowery <ben.lowery@automattic.com>
2018-09-26 16:19:45 -0400

    make this a proper function so it has a name in dev tools. fix usage of class -> className

--- client/components/async-gridicons/fallback.jsx
+++ client/components/async-gridicons/fallback.jsx
@@ -9,12 +9,15 @@ import React from 'react';
  * Internal dependencies
  */
 
-export default () => (
-	<svg
-		class="gridicon"
-		height="24"
-		width="24"
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 24 24"
-	/>
-);
+export default function FallbackIcon() {
+	/* eslint-disable wpcalypso/jsx-classname-namespace */
+	return (
+		<svg
+			className="gridicon"
+			height="24"
+			width="24"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+		/>
+	);
+}
