817296f7536bd3c59acdc5f64119535f4dbba04f
Copons <copons@users.noreply.github.com>
2018-09-26 16:38:41 +0100

    Improve new/edit logic

--- client/gutenberg/editor/editor-document-head/index.jsx
+++ client/gutenberg/editor/editor-document-head/index.jsx
@@ -2,7 +2,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { localize } from 'i18n-calypso';
 import { withSelect } from '@wordpress/data';
@@ -13,29 +13,55 @@ import { withSelect } from '@wordpress/data';
 import DocumentHead from 'components/data/document-head';
 
 //TODO: CPT support once https://github.com/Automattic/wp-calypso/issues/27430 is fixed
-export const EditorDocumentHead = ( { isNewPost, postType, translate } ) => {
-	let title = '';
-	if ( 'post' === postType ) {
-		title = isNewPost ? translate( 'New Post' ) : translate( 'Edit Post' );
-	} else if ( 'page' === postType ) {
-		title = isNewPost ? translate( 'New Page' ) : translate( 'Edit Page' );
-	} else {
-		title = isNewPost ? translate( 'New' ) : translate( 'Edit' );
+export class EditorDocumentHead extends Component {
+	static propTypes = {
+		postStatus: PropTypes.string,
+		postType: PropTypes.string,
+	};
+
+	shouldComponentUpdate( nextProps ) {
+		if ( 'auto-draft' === this.props.postStatus && 'draft' === nextProps.postStatus ) {
+			return false;
+		}
+		return this.props.postStatus !== nextProps.postStatus;
 	}
 
-	return <DocumentHead title={ title } />;
-};
+	isNewPost = () => 'auto-draft' === this.props.postStatus;
 
-EditorDocumentHead.propTypes = {
-	isNewPost: PropTypes.bool,
-	postType: PropTypes.string,
-};
+	getTitle = () => {
+		const { postStatus, postType, translate } = this.props;
+		if ( ! postStatus ) {
+			return '';
+		}
+		const isNewPost = this.isNewPost();
+		if ( 'post' === postType && isNewPost ) {
+			return translate( 'New Post' );
+		}
+		if ( 'post' === postType && ! isNewPost ) {
+			return translate( 'Edit Post' );
+		}
+		if ( 'page' === postType && isNewPost ) {
+			return translate( 'New Page' );
+		}
+		if ( 'page' === postType && ! isNewPost ) {
+			return translate( 'Edit Page' );
+		}
+		if ( isNewPost ) {
+			return translate( 'New ' );
+		}
+		return translate( 'Edit' );
+	};
+
+	render() {
+		return <DocumentHead title={ this.getTitle() } />;
+	}
+}
 
 export default withSelect( select => {
 	const { getCurrentPost } = select( 'core/editor' );
 	const { status } = getCurrentPost();
 
 	return {
-		isNewPost: status === 'auto-draft',
+		postStatus: status,
 	};
 } )( localize( EditorDocumentHead ) );
