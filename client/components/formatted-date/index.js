82246a4cdb3526b60b33b1acb70b87af02c37b65
Ben Lowery <ben.lowery@automattic.com>
2018-09-28 16:17:20 -0400

    extract a small tool to convert a moment from any locale to the current locale

--- client/components/formatted-date/index.js
+++ client/components/formatted-date/index.js
@@ -1,20 +1,28 @@
+/** @format */
 /**
  * External dependencies
+ *
  */
+
 import React, { Component } from 'react';
 
 /**
  * Internal dependencies
  */
 import withMoment from 'components/with-localized-moment';
+import toCurrentLocale from './to-current-locale';
 
 class FormattedDate extends Component {
-	static displayName = "FormattedDate";
+	static displayName = 'FormattedDate';
 	render() {
 		let date = this.props.date;
-		if ( ! this.props.moment.isMoment( date ) ) {
+		const moment = this.props.moment;
+		if ( ! moment.isMoment( date ) ) {
 			// only make a new moment if we were passed something else
-			date = this.props.moment( date );
+			date = moment( date );
+		} else {
+			// make sure the date is in the current locale
+			date = toCurrentLocale( date );
 		}
 		return <time dateTime={ date.toISOString( true ) }>{ date.format( this.props.format ) }</time>;
 	}
