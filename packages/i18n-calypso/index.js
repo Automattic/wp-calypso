428140e124820bb6b63aa4f5e675ef72f2f890b1
Bernie Reiter <ockham@raz.or.at>
2018-09-29 19:40:26 +0200

    i18n: Post-codemod cleanup

--- packages/i18n-calypso/index.js
+++ packages/i18n-calypso/index.js
@@ -1,55 +1,52 @@
+/** @format */
 /**
  * Internal dependencies
  */
 import I18N from './lib';
 
-/**
- * Internal dependencies
- */
-let i18n = new I18N();
+const i18n = new I18N();
 
 const exported = {
-    moment: i18n.moment,
-    numberFormat: i18n.numberFormat.bind( i18n ),
-    translate: i18n.translate.bind( i18n ),
-    configure: i18n.configure.bind( i18n ),
-    setLocale: i18n.setLocale.bind( i18n ),
-    getLocale: i18n.getLocale.bind( i18n ),
-    getLocaleSlug: i18n.getLocaleSlug.bind( i18n ),
-    addTranslations: i18n.addTranslations.bind( i18n ),
-    reRenderTranslations: i18n.reRenderTranslations.bind( i18n ),
-    registerComponentUpdateHook: i18n.registerComponentUpdateHook.bind( i18n ),
-    registerTranslateHook: i18n.registerTranslateHook.bind( i18n ),
-    state: i18n.state,
-    stateObserver: i18n.stateObserver,
-    on: i18n.stateObserver.on.bind(i18n.stateObserver),
-    off: i18n.stateObserver.removeListener.bind(i18n.stateObserver),
-    emit: i18n.stateObserver.emit.bind(i18n.stateObserver),
-    localize: require( './lib/localize' )( i18n ),
-    $this: i18n,
-    I18N: I18N
+	moment: i18n.moment,
+	numberFormat: i18n.numberFormat.bind( i18n ),
+	translate: i18n.translate.bind( i18n ),
+	configure: i18n.configure.bind( i18n ),
+	setLocale: i18n.setLocale.bind( i18n ),
+	getLocale: i18n.getLocale.bind( i18n ),
+	getLocaleSlug: i18n.getLocaleSlug.bind( i18n ),
+	addTranslations: i18n.addTranslations.bind( i18n ),
+	reRenderTranslations: i18n.reRenderTranslations.bind( i18n ),
+	registerComponentUpdateHook: i18n.registerComponentUpdateHook.bind( i18n ),
+	registerTranslateHook: i18n.registerTranslateHook.bind( i18n ),
+	state: i18n.state,
+	stateObserver: i18n.stateObserver,
+	on: i18n.stateObserver.on.bind( i18n.stateObserver ),
+	off: i18n.stateObserver.removeListener.bind( i18n.stateObserver ),
+	emit: i18n.stateObserver.emit.bind( i18n.stateObserver ),
+	localize: require( './lib/localize' )( i18n ),
+	$this: i18n,
+	I18N: I18N,
 };
 
 export default exported;
 export { i18n as $this, I18N };
 
 export const {
-    moment,
-    numberFormat,
-    translate,
-    configure,
-    setLocale,
-    getLocale,
-    getLocaleSlug,
-    addTranslations,
-    reRenderTranslations,
-    registerComponentUpdateHook,
-    registerTranslateHook,
-    state,
-    stateObserver,
-    on,
-    off,
-    emit,
-    localize
+	moment,
+	numberFormat,
+	translate,
+	configure,
+	setLocale,
+	getLocale,
+	getLocaleSlug,
+	addTranslations,
+	reRenderTranslations,
+	registerComponentUpdateHook,
+	registerTranslateHook,
+	state,
+	stateObserver,
+	on,
+	off,
+	emit,
+	localize,
 } = exported;
-
