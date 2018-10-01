13fb892a02b3db8e3c9dc10e0d23b6fcaf5fe6ec
Markus Dobmann <markus@mpnjs.com>
2018-10-01 18:07:11 -0400

    PulsingDot: add 'timeout' property

--- client/components/pulsing-dot/index.jsx
+++ client/components/pulsing-dot/index.jsx
@@ -5,9 +5,49 @@
  */
 
 import React from 'react';
+import { number } from 'prop-types';
 import classnames from 'classnames';
 
-export default function PulsingDot( { active } ) {
-	const className = classnames( 'pulsing-dot', { 'is-active': active } );
-	return <div className={ className } />;
+class PulsingDot extends React.Component {
+	constructor( props ) {
+		super( props );
+
+		this.timeout = null;
+		this.state = {
+			show: false,
+		};
+	}
+
+	componentDidMount() {
+		const { timeout } = this.props;
+		this.timeout = setTimeout( () => {
+			this.setState( { show: true } );
+		}, timeout );
+	}
+
+	componentWillUnmount() {
+		if ( this.timeout ) {
+			clearTimeout( this.timeout );
+		}
+	}
+
+	render() {
+		const { active } = this.props;
+		const { show } = this.state;
+
+		const className = classnames( 'pulsing-dot', { 'is-active': active } );
+		return show ? <div className={ className } /> : null;
+	}
 }
+
+PulsingDot.propTypes = {
+	timeout: number.isRequired,
+};
+
+PulsingDot.defaultProps = {
+	timeout: 0,
+};
+
+export default PulsingDot;
+
+// export default function PulsingDot( { active, timeout = 0 } ) {}
