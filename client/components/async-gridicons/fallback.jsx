a13fa22d4cf2c91be52ec13705acd76f9f2c5a93
Markus Dobmann <markus@mpnjs.com>
2018-10-02 15:47:11 -0400

    Async load Gridicons (#27527)
    * Async load gridicons (#27158)
    
    Push them all into one chunk and load that chunk async
    
    * Editor: use single Gridicon imports for TinyMCE
    
    * refactor: GridiconButtonEx -> GridiconButton

---
+++ client/components/async-gridicons/fallback.jsx
@@ -0,0 +1,23 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+
+export default function FallbackIcon() {
+	/* eslint-disable wpcalypso/jsx-classname-namespace */
+	return (
+		<svg
+			className="gridicon"
+			height="24"
+			width="24"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 24 24"
+		/>
+	);
+}
