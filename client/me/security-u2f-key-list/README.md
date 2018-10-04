1c06f9f51ab9fba9f9bf6cf2187cb3379251d083
Jonathan Belcher <belcherj@gmail.com>
2018-10-04 14:53:06 -0400

    Move files into main u2f folder

--- client/me/security-u2f-key-list/README.md
+++
@@ -1,5 +0,0 @@
-<!-- @format -->
-
-# SecurityU2fKeyList
-
-Used by SecurityU2fKey, this displays the u2f keys list on the 2fa page
---
+++ client/me/security-u2f-key/add.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React, { Fragment } from 'react';
+
+class SecurityU2fKeyAdd extends React.Component {
+	render() {
+		return (
+			<Fragment>
+				<p>Baaaaaaar</p>
+			</Fragment>
+		);
+	}
+}
+
+export default SecurityU2fKeyAdd;
