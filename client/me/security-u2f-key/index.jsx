ccd293bb48bf73bedd9543e037e1336e2e6a70d9
Biser Perchinkov <biser@bisko.be>
2018-10-04 17:53:27 -0400

    Add delete button and update list styling

--- client/me/security-u2f-key/index.jsx
+++ client/me/security-u2f-key/index.jsx
@@ -16,6 +16,7 @@ import Card from 'components/card';
 import getU2fKeys from 'state/selectors/get-u2f-keys';
 import getNewU2fKey from 'state/selectors/get-new-u2f-key';
 import SectionHeader from 'components/section-header';
+
 import SecurityU2fKeyAdd from './add';
 import SecurityU2fKeyList from './list';
 import { recordGoogleEvent } from 'state/analytics/actions';
@@ -64,10 +65,12 @@ class SecurityU2fKey extends React.Component {
 							</Button>
 						) }
 				</SectionHeader>
-				{ !! u2fKeys.length && <SecurityU2fKeyList /> }
-				{ addingKey && <SecurityU2fKeyAdd /> }
-				{ ! addingKey &&
-					! u2fKeys.length && <Card>Use a Universal 2nd Factor security key to sign in.</Card> }
+				<Card>
+					{ !! u2fKeys.length && <SecurityU2fKeyList keys={ this.props.u2fKeys } /> }
+					{ addingKey && <SecurityU2fKeyAdd /> }
+					{ ! addingKey &&
+						! u2fKeys.length && <p>Use a Universal 2nd Factor security key to sign in.</p> }
+				</Card>
 			</Fragment>
 		);
 	}
