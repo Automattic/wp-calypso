30756556c09b5a2ea8bcfbe12a0ebfd8a1e20ae8
Jonathan Belcher <belcherj@gmail.com>
2018-10-04 14:49:08 -0400

    Added more UI

---
+++ client/me/security-u2f-key-list/index.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React, { Fragment } from 'react';
+
+class SecurityU2fKeyList extends React.Component {
+	render() {
+		return (
+			<Fragment>
+				<p>Fooooooo</p>
+			</Fragment>
+		);
+	}
+}
+
+export default SecurityU2fKeyList;
--- client/me/security-u2f-key/index.jsx
+++ client/me/security-u2f-key/index.jsx
@@ -5,6 +5,7 @@
  */
 import Gridicon from 'gridicons';
 import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -12,8 +13,19 @@ import { localize } from 'i18n-calypso';
  */
 import Button from 'components/button';
 import Card from 'components/card';
+import getU2fKeys from 'state/selectors/get-u2f-keys';
+import getNewU2fKey from 'state/selectors/get-new-u2f-key';
 import SectionHeader from 'components/section-header';
+import SecurityU2fKeyList from 'me/security-u2f-key-list';
+import { recordGoogleEvent } from 'state/analytics/actions';
+
 class SecurityU2fKey extends React.Component {
+	static initialState = Object.freeze( {
+		addingKey: false,
+	} );
+
+	state = this.constructor.initialState;
+
 	getClickHandler = ( action, callback ) => {
 		return event => {
 			this.props.recordGoogleEvent( 'Me', 'Clicked on ' + action );
@@ -24,28 +36,48 @@ class SecurityU2fKey extends React.Component {
 		};
 	};
 
+	toggleAddNewU2fKey = event => {
+		event.preventDefault();
+		this.setState( { addingKey: ! this.state.addingKey } );
+	};
+
 	render() {
-		const { translate } = this.props;
-		const { newAppPassword } = true;
+		const { translate, u2fKeys } = this.props;
+		const { addingKey } = this.state;
 		return (
 			<Fragment>
 				<SectionHeader label={ translate( 'Security Key' ) }>
-					{ ! newAppPassword && (
-						<Button
-							compact
-							onClick={ this.getClickHandler( 'Register New Key Button', this.toggleNewPassword ) }
-						>
-							{ /* eslint-disable wpcalypso/jsx-gridicon-size */ }
-							<Gridicon icon="plus-small" size={ 16 } />
-							{ /* eslint-enable wpcalypso/jsx-gridicon-size */ }
-							{ translate( 'Register Key' ) }
-						</Button>
-					) }
+					{ ! addingKey &&
+						! u2fKeys.length && (
+							<Button
+								compact
+								onClick={ this.getClickHandler(
+									'Register New Key Button',
+									this.toggleAddNewU2fKey
+								) }
+							>
+								{ /* eslint-disable wpcalypso/jsx-gridicon-size */ }
+								<Gridicon icon="plus-small" size={ 16 } />
+								{ /* eslint-enable wpcalypso/jsx-gridicon-size */ }
+								{ translate( 'Register Key' ) }
+							</Button>
+						) }
 				</SectionHeader>
-				<Card>Use a Universal 2nd Factor security key to sign in.</Card>
+				{ !! u2fKeys.length && <SecurityU2fKeyList /> }
+				{ addingKey && <SecurityU2fKeyList /> }
+				{ ! addingKey &&
+					! u2fKeys.length && <Card>Use a Universal 2nd Factor security key to sign in.</Card> }
 			</Fragment>
 		);
 	}
 }
 
-export default localize( SecurityU2fKey );
+export default connect(
+	state => ( {
+		u2fKeys: getU2fKeys( state ),
+		newU2fKey: getNewU2fKey( state ),
+	} ),
+	{
+		recordGoogleEvent,
+	}
+)( localize( SecurityU2fKey ) );
