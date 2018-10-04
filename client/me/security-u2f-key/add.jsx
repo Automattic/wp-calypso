5fbb0613ea6e8156f5a7df54354d3190c8ca1f36
Jonathan Belcher <belcherj@gmail.com>
2018-10-04 15:50:23 -0400

    Add u2f code

--- client/me/security-u2f-key/add.jsx
+++ client/me/security-u2f-key/add.jsx
@@ -3,15 +3,41 @@
 /**
  * External dependencies
  */
+import Card from 'components/card';
 import React, { Fragment } from 'react';
 
+/**
+ * Internal dependencies
+ */
+import u2f from './u2f-api';
+
 class SecurityU2fKeyAdd extends React.Component {
+	componentDidMount() {
+		this.registerKey();
+	}
+
+	registerKey() {
+		const registerRequests = [
+			{ version: 'U2F_V2', challenge: this.createChallenge(), attestation: 'direct' },
+		];
+		u2f.register( 'https://wordpress.com', registerRequests, [], this.keyRegistered );
+	}
+
+	keyRegistered( data ) {
+		console.log( JSON.stringify( data ) ); // eslint-disable-line
+	}
+
+	createChallenge() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace( /[xy]/g, function( c ) {
+			const r = ( Math.random() * 16 ) | 0,
+				v = c === 'x' ? r : ( r & 0x3 ) | 0x8;
+			return v.toString( 16 );
+		} );
+	}
+
 	render() {
-		return (
-			<Fragment>
-				<p>Baaaaaaar</p>
-			</Fragment>
-		);
+		window.u2f = u2f;
+		return <Fragment>{ !! u2f && <Card>Aaaaaaaaddddddd</Card> }</Fragment>;
 	}
 }
 
