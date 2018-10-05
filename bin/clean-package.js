faebd70b0a3da1c8cd1da81f07fad0a4a34bb0f6
Ben Lowery <ben.lowery@automattic.com>
2018-10-04 20:55:13 -0400

    add a clean script

---
+++ bin/clean-package.js
@@ -0,0 +1,7 @@
+const rimraf = require( 'rimraf' );
+const process = require('process');
+
+console.log( `cleaning ${ process.cwd() }` );
+rimraf( 'dist/**/*', err => {
+	console.error( err );
+}  );
