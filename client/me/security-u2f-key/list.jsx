2415d407caa8c38a4419f84407b40654c1a58f5a
Biser Perchinkov <biser@bisko.be>
2018-10-04 20:45:13 -0400

    Improvements to data structure and rendering

--- client/me/security-u2f-key/list.jsx
+++ client/me/security-u2f-key/list.jsx
@@ -6,6 +6,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
 
 /**
  * Internal dependencies
@@ -17,13 +18,17 @@ import U2FItem from './item';
 import './style.scss';
 
 class SecurityU2fKeyList extends React.Component {
+	static propTypes = {
+		securityKeys: PropTypes.array.isRequired,
+	};
+
 	render() {
 		return (
 			<div className="security-u2f-key__active-keys">
 				<FormSectionHeading>{ this.props.translate( 'Active keys' ) }</FormSectionHeading>
 				<ul className="security-u2f-key__list">
 					{ this.props.securityKeys.map( securityKey => (
-						<li key={ securityKey } className="security-u2f-key__list-item">
+						<li key={ securityKey.id } className="security-u2f-key__list-item">
 							<U2FItem securityKey={ securityKey } />
 						</li>
 					) ) }
