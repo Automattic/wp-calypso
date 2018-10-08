92a7d4dc2e278113c073b0ee2de52df33ca07c7f
Veselin Nikolov <veselin@automattic.com>
2018-10-08 16:52:48 +0300

    Update posts revisions to dispatchRequestEx

--- client/state/data-layer/wpcom/posts/revisions/index.js
+++ client/state/data-layer/wpcom/posts/revisions/index.js
@@ -4,7 +4,7 @@
  * Internal dependencies
  */
 import { POST_REVISIONS_REQUEST } from 'state/action-types';
-import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
 import { http } from 'state/data-layer/wpcom-http/actions';
 import {
 	receivePostRevisions,
@@ -18,56 +18,57 @@ import { registerHandlers } from 'state/data-layer/handler-registry';
  * Dispatches returned error from post revisions request
  *
  * @param {Function} dispatch Redux dispatcher
- * @param {Object} action Redux action
  * @param {String} action.siteId of the revisions
  * @param {String} action.postId of the revisions
  * @param {Object} rawError from HTTP request
- * @returns {Object} the dispatched action
+ *
+ * @return {Object} the dispatched action
  */
-export const receiveError = ( { dispatch }, { siteId, postId }, rawError ) =>
-	dispatch( receivePostRevisionsFailure( siteId, postId, rawError ) );
+export const receiveError = ( { siteId, postId }, rawError ) =>
+	receivePostRevisionsFailure( siteId, postId, rawError );
 
 /**
  * Dispatches returned post revisions
  *
- * @param {Function} dispatch Redux dispatcher
  * @param {Object} action Redux action
  * @param {String} action.siteId of the revisions
  * @param {String} action.postId of the revisions
  * @param {Object} response from the server
  * @param {Object} response.diffs raw data containing a set of diffs for the site & post
+ *
+ * @return {Array} An array of Redux actions
  */
-export const receiveSuccess = ( { dispatch }, { siteId, postId }, response ) => {
-	dispatch( receivePostRevisionsSuccess( siteId, postId ) );
-	dispatch( receivePostRevisions( { siteId, postId, ...response } ) );
+export const receiveSuccess = ( { siteId, postId }, response ) => {
+	return [
+		receivePostRevisionsSuccess( siteId, postId ),
+		receivePostRevisions( { siteId, postId, ...response } ),
+	];
 };
 
 /**
  * Dispatches a request to fetch post revisions
  *
- * @param {Function} dispatch Redux dispatcher
  * @param {Object} action Redux action
+ * @return {Object} Redux action
  */
-export const fetchPostRevisionsDiffs = ( { dispatch }, action ) => {
+export const fetchPostRevisionsDiffs = action => {
 	const { siteId, postId, postType, comparisons } = action;
-	dispatch(
-		http(
-			{
-				apiVersion: '1.1',
-				path: `/sites/${ siteId }/${ postType }/${ postId }/diffs`,
-				method: 'GET',
-				query: { comparisons },
-			},
-			action
-		)
+	return http(
+		{
+			apiVersion: '1.1',
+			path: `/sites/${ siteId }/${ postType }/${ postId }/diffs`,
+			method: 'GET',
+			query: { comparisons },
+		},
+		action
 	);
 };
 
-const dispatchPostRevisionsDiffsRequest = dispatchRequest(
-	fetchPostRevisionsDiffs,
-	receiveSuccess,
-	receiveError
-);
+const dispatchPostRevisionsDiffsRequest = dispatchRequestEx( {
+	fetch: fetchPostRevisionsDiffs,
+	onSuccess: receiveSuccess,
+	onError: receiveError,
+} );
 
 registerHandlers( 'state/data-layer/wpcom/posts/revisions/index.js', {
 	[ POST_REVISIONS_REQUEST ]: [ dispatchPostRevisionsDiffsRequest ],
