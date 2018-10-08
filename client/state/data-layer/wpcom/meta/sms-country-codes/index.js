8e8594b8a7f50ab003c1716e59b1ff255a55d087
Veselin Nikolov <veselin@automattic.com>
2018-10-08 16:18:31 +0300

    Use dispatchRequestEx instead of dispatchRequest

--- client/state/data-layer/wpcom/meta/sms-country-codes/index.js
+++ client/state/data-layer/wpcom/meta/sms-country-codes/index.js
@@ -8,7 +8,7 @@ import { translate } from 'i18n-calypso';
  * Internal dependencies
  */
 import { http } from 'state/data-layer/wpcom-http/actions';
-import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
 import { COUNTRIES_SMS_FETCH, COUNTRIES_SMS_UPDATED } from 'state/action-types';
 import { errorNotice } from 'state/notices/actions';
 
@@ -17,48 +17,46 @@ import { registerHandlers } from 'state/data-layer/handler-registry';
 /**
  * Dispatches a request to fetch all available WordPress.com countries
  *
- * @param   {Function} dispatch Redux dispatcher
- * @param 	{String} action The action to dispatch next
+ * @param 	{Object} action The action to dispatch next
  * @returns {Object} dispatched http action
  */
-export const fetchCountriesSms = ( { dispatch }, action ) =>
-	dispatch(
-		http(
-			{
-				apiVersion: '1.1',
-				method: 'GET',
-				path: '/meta/sms-country-codes/',
-			},
-			action
-		)
+export const fetchCountriesSms = action =>
+	http(
+		{
+			apiVersion: '1.1',
+			method: 'GET',
+			path: '/meta/sms-country-codes/',
+		},
+		action
 	);
 
 /**
  * Dispatches a countries updated action then the request for countries succeeded.
  *
- * @param   {Function} dispatch Redux dispatcher
  * @param   {Object}   action   Redux action
  * @param   {Array}    countries  array of raw device data returned from the endpoint
  * @returns {Object}            disparched user devices add action
  */
-export const updateCountriesSms = ( { dispatch }, action, countries ) =>
-	dispatch( {
-		type: COUNTRIES_SMS_UPDATED,
-		countries,
-	} );
+export const updateCountriesSms = ( action, countries ) => ( {
+	type: COUNTRIES_SMS_UPDATED,
+	countries,
+} );
 
 /**
  * Dispatches a error notice action when the request for the supported countries list fails.
  *
- * @param   {Function} dispatch Redux dispatcher
  * @returns {Object}            dispatched error notice action
  */
-export const showCountriesSmsLoadingError = ( { dispatch } ) =>
-	dispatch( errorNotice( translate( "We couldn't load the countries list." ) ) );
+export const showCountriesSmsLoadingError = () =>
+	errorNotice( translate( "We couldn't load the countries list." ) );
 
 registerHandlers( 'state/data-layer/wpcom/meta/sms-country-codes/index.js', {
 	[ COUNTRIES_SMS_FETCH ]: [
-		dispatchRequest( fetchCountriesSms, updateCountriesSms, showCountriesSmsLoadingError ),
+		dispatchRequestEx( {
+			fetch: fetchCountriesSms,
+			onSuccess: updateCountriesSms,
+			onError: showCountriesSmsLoadingError,
+		} ),
 	],
 } );
 
