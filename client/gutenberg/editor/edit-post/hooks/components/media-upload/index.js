5eb2cc8e58c28d9d2ac45a4c10cf065d5c817408
Copons <copons@users.noreply.github.com>
2018-10-10 17:09:14 +0100

    Add media type filters and selected items

--- client/gutenberg/editor/edit-post/hooks/components/media-upload/index.js
+++ client/gutenberg/editor/edit-post/hooks/components/media-upload/index.js
@@ -4,12 +4,14 @@
  */
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { isArray, map } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import MediaLibrarySelectedData from 'components/data/media-library-selected-data';
 import MediaModal from 'post-editor/media-modal';
+import MediaActions from 'lib/media/actions';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { mediaCalypsoToGutenberg } from './utils';
 
@@ -17,6 +19,16 @@ export class MediaUpload extends Component {
 	state = {
 		isModalVisible: false,
 	};
+
+	componentDidMount() {
+		MediaActions.setLibrarySelectedItems( this.props.siteId, this.getSelectedItems() );
+	}
+
+	componentDidUpdate( prevProps ) {
+		if ( prevProps.value !== this.props.value ) {
+			MediaActions.setLibrarySelectedItems( this.props.siteId, this.getSelectedItems() );
+		}
+	}
 
 	closeModal = () => this.setState( { isModalVisible: false } );
 
@@ -35,15 +47,44 @@ export class MediaUpload extends Component {
 		this.closeModal();
 	};
 
+	getFilter = () => {
+		// TODO: Replace with `allowedTypes` (array) after updating Gutenberg
+		const { type } = this.props;
+		if ( 'image' === type ) {
+			return 'images';
+		}
+		if ( 'video' === type ) {
+			return 'videos';
+		}
+		return type;
+	};
+
+	getSelectedItems = () => {
+		const { value } = this.props;
+		if ( ! value ) {
+			return [];
+		}
+		if ( isArray( value ) ) {
+			return map( this.props.value, item => ( { ID: parseInt( item, 10 ) } ) );
+		}
+		return [ { ID: parseInt( value, 10 ) } ];
+	};
+
 	render() {
-		const { render, siteId } = this.props;
+		const { multiple, render, siteId } = this.props;
 		const { isModalVisible } = this.state;
 
 		return (
 			<Fragment>
 				{ render( { open: this.openModal } ) }
 				<MediaLibrarySelectedData siteId={ siteId }>
-					<MediaModal onClose={ this.onCloseModal } visible={ isModalVisible } source="" />
+					<MediaModal
+						enabledFilters={ [ this.getFilter() ] }
+						onClose={ this.onCloseModal }
+						single={ ! multiple }
+						source=""
+						visible={ isModalVisible }
+					/>
 				</MediaLibrarySelectedData>
 			</Fragment>
 		);
