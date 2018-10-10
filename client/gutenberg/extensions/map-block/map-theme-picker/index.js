99b4af1c5dc42ef1f0f403747f88881ab3297133
Jefferson Rabb <jeff@atavist.net>
2018-10-10 12:54:55 -0400

    Missing map theme component.

---
+++ client/gutenberg/extensions/map-block/map-theme-picker/index.js
@@ -0,0 +1,67 @@
+/**
+ * Wordpress dependencies
+ */
+
+import { __ } from '@wordpress/i18n';
+
+import {
+	Component,
+	Fragment
+} from '@wordpress/element';
+
+import {
+	Button,
+	ButtonGroup
+} from '@wordpress/components';
+
+/**
+ * External dependencies
+ */
+
+ import classnames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+
+import './style.scss';
+
+export class MapThemePicker extends Component {
+
+	render() {
+		const { options, value, onChange, label } = this.props;
+		const buttons = options.map( ( option ) => {
+			const classes = classnames(
+				'component_map-theme-picker__button',
+				option.value,
+				option.value === value ? 'isSelected' : ''
+			);
+			return (
+				<Button
+					className={ classes }
+					onClick={ ( value ) => onChange( option.value ) }
+				>
+					{ option.label }
+				</Button>
+			);
+		} );
+		return (
+			<Fragment>
+				<label className='components-base-control__label'>{ label }</label>
+				<ButtonGroup>
+					{ buttons }
+				</ButtonGroup>
+			</Fragment>
+		);
+	}
+
+}
+
+MapThemePicker.defaultProps = {
+	label: '',
+	options: [],
+	value: null,
+	onChange: () => {}
+}
+
+export default MapThemePicker;
