a83648e7a6299085f8c9ab3d67c038f0cbd8c60f
Jon Surrell <jon.surrell@automattic.com>
2018-10-10 16:11:29 +0200

    Gutenpack: Add dynamic Webpack publicPath (#27274)

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -3,6 +3,7 @@
 /**
  * Internal dependencies
  */
+import './utils/public-path';
 import './utils/block-category'; // Register the Jetpack category
 import 'gutenberg/extensions/markdown/editor';
 import 'gutenberg/extensions/related-posts/editor';
