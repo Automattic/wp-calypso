011f316253f9bafa080ff39b377eb697f8787248
Jefferson Rabb <jeff@atavist.net>
2018-10-10 15:37:10 -0400

    Refactoring the frontend initialization, moving code into each block's view.js, to simplify the presets.

--- client/gutenberg/extensions/map-block/view.js
+++ client/gutenberg/extensions/map-block/view.js
@@ -7,8 +7,20 @@
 import './style.scss';
 import component from './map-component.js';
 import { CONFIG } from './config.js';
+import FrontendManagement from 'gutenberg/extensions/shared/atavist/frontend-management.js';
 
-export const map = {
-	component,
-	CONFIG,
-};
+window.addEventListener( 'load', function() {
+	const frontendManagement = new FrontendManagement();
+	frontendManagement.blockIterator(
+		document,
+		[
+			{
+				component: component,
+				options: {
+					config: CONFIG,
+					selector: '.map__map-container'
+				}
+			}
+		]
+	);
+} );
--- client/gutenberg/extensions/presets/atavist/view.js
+++ client/gutenberg/extensions/presets/atavist/view.js
@@ -2,25 +2,6 @@
  * Internal dependencies
  */
 
-import FrontendManagement from 'gutenberg/extensions/shared/atavist/frontend-management.js';
-import { map } from 'gutenberg/extensions/map-block/view';
+import 'gutenberg/extensions/map-block/view';
 
-const navigationTypes = [];
-const blocks = [
-	{
-		component: map.component,
-		options: {
-			config: map.CONFIG,
-			selector: '.map__map-container'
-		}
-	}
-];
 
-const init = function() {
-	const frontendManagement = new FrontendManagement();
-	document.querySelector('body').classList.add('webcomponentsready');
-	frontendManagement.blockIterator( document, navigationTypes );
-	frontendManagement.blockIterator( document, blocks );
-};
-
-window.addEventListener( 'load', init );
