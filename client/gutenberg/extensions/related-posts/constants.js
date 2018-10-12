0ee36e4b4fd610eb641b02d7ef680750b9ea71ed
Marin Atanasov <tyxla@abv.bg>
2018-10-12 10:31:29 +0300

    Fix image schema in example posts

--- client/gutenberg/extensions/related-posts/constants.js
+++ client/gutenberg/extensions/related-posts/constants.js
@@ -5,19 +5,25 @@ export const MAX_POSTS_TO_SHOW = 3;
 export const DEFAULT_POSTS = [
 	{
 		title: 'Big iPhone/iPad Update Now Available',
-		image: 'https://wordpress.com/calypso/images/related-posts/cat-blog.png',
+		img: {
+			src: 'https://wordpress.com/calypso/images/related-posts/cat-blog.png',
+		},
 		date: 'August 3, 2018',
 		context: 'In "Mobile"',
 	},
 	{
 		title: 'The WordPress for Android App Gets a Big Facelift',
-		image: 'https://wordpress.com/calypso/images/related-posts/devices.jpg',
+		img: {
+			src: 'https://wordpress.com/calypso/images/related-posts/devices.jpg',
+		},
 		date: 'August 2, 2018',
 		context: 'In "Mobile"',
 	},
 	{
 		title: 'Upgrade Focus: VideoPress For Weddings',
-		image: 'https://wordpress.com/calypso/images/related-posts/mobile-wedding.jpg',
+		img: {
+			src: 'https://wordpress.com/calypso/images/related-posts/mobile-wedding.jpg',
+		},
 		date: 'August 5, 2018',
 		context: 'In "Upgrade"',
 	},
