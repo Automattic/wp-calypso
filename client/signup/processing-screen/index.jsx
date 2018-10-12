b96fc375cbbdc3b130e74401438eb5362f0402ef
Caroline Moore <calobee@gmail.com>
2018-10-12 12:56:24 -0400

    Remove Gridicon floaties; they're too busy with the new background.

--- client/signup/processing-screen/index.jsx
+++ client/signup/processing-screen/index.jsx
@@ -95,44 +95,6 @@ export class SignupProcessingScreen extends Component {
 		);
 	}
 
-	renderFloaties() {
-		// Non standard gridicon sizes are used here because we display giant, floating icons on the page with an animation
-		/* eslint-disable wpcalypso/jsx-gridicon-size, wpcalypso/jsx-classname-namespace */
-		return (
-			<div className="signup-processing-screen__floaties">
-				<Gridicon icon="add" size={ 64 } />
-				<Gridicon icon="aside" size={ 64 } />
-				<Gridicon icon="attachment" size={ 64 } />
-				<Gridicon icon="audio" size={ 64 } />
-				<Gridicon icon="bell" size={ 64 } />
-				<Gridicon icon="book" size={ 64 } />
-				<Gridicon icon="camera" size={ 64 } />
-				<Gridicon icon="comment" size={ 64 } />
-				<Gridicon icon="globe" size={ 64 } />
-				<Gridicon icon="pencil" size={ 64 } />
-				<Gridicon icon="phone" size={ 64 } />
-				<Gridicon icon="reader" size={ 64 } />
-				<Gridicon icon="star" size={ 64 } />
-				<Gridicon icon="video" size={ 64 } />
-				<Gridicon icon="align-image-right" size={ 64 } />
-				<Gridicon icon="bookmark" size={ 64 } />
-				<Gridicon icon="briefcase" size={ 64 } />
-				<Gridicon icon="calendar" size={ 64 } />
-				<Gridicon icon="clipboard" size={ 64 } />
-				<Gridicon icon="cloud-upload" size={ 64 } />
-				<Gridicon icon="cog" size={ 64 } />
-				<Gridicon icon="customize" size={ 64 } />
-				<Gridicon icon="help" size={ 64 } />
-				<Gridicon icon="link" size={ 64 } />
-				<Gridicon icon="lock" size={ 64 } />
-				<Gridicon icon="pages" size={ 64 } />
-				<Gridicon icon="share" size={ 64 } />
-				<Gridicon icon="stats" size={ 64 } />
-			</div>
-		);
-		/* eslint-enable wpcalypso/jsx-gridicon-size, wpcalypso/jsx-classname-namespace */
-	}
-
 	getTitle() {
 		const { loginHandler } = this.props;
 
@@ -258,8 +220,6 @@ export class SignupProcessingScreen extends Component {
 		/* eslint-disable wpcalypso/jsx-classname-namespace */
 		return (
 			<div>
-				{ this.renderFloaties() }
-
 				<div className="signup-processing__content">
 					<img
 						alt=""
@@ -346,8 +306,6 @@ export class SignupProcessingScreen extends Component {
 		/* eslint-disable wpcalypso/jsx-classname-namespace */
 		return (
 			<div>
-				{ this.renderFloaties() }
-
 				<div className="signup-processing__content">
 					<svg
 						className="signup-process-screen__wpcom-logo"
