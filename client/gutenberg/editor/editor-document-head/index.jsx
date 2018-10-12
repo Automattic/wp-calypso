f1216483e82ac9eda8c4bc042a2ba2f94d314f97
Copons <copons@users.noreply.github.com>
2018-10-12 16:14:07 +0100

    Add support for CPT

--- client/gutenberg/editor/editor-document-head/index.jsx
+++ client/gutenberg/editor/editor-document-head/index.jsx
@@ -4,15 +4,18 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import { withSelect } from '@wordpress/data';
+import { flowRight, get } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import DocumentHead from 'components/data/document-head';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { getPostType } from 'state/post-types/selectors';
 
-//TODO: CPT support once https://github.com/Automattic/wp-calypso/issues/27430 is fixed
 export class EditorDocumentHead extends Component {
 	static propTypes = {
 		postStatus: PropTypes.string,
@@ -29,7 +32,7 @@ export class EditorDocumentHead extends Component {
 	isNewPost = () => 'auto-draft' === this.props.postStatus;
 
 	getTitle = () => {
-		const { postStatus, postType, translate } = this.props;
+		const { postStatus, postType, postTypeObject, translate } = this.props;
 		if ( ! postStatus ) {
 			return '';
 		}
@@ -47,9 +50,9 @@ export class EditorDocumentHead extends Component {
 			return translate( 'Edit Page' );
 		}
 		if ( isNewPost ) {
-			return translate( 'New ' );
+			return get( postTypeObject, 'labels.new_item' ) || translate( 'New ' );
 		}
-		return translate( 'Edit' );
+		return get( postTypeObject, 'labels.edit_item' ) || translate( 'Edit' );
 	};
 
 	render() {
@@ -57,11 +60,19 @@ export class EditorDocumentHead extends Component {
 	}
 }
 
-export default withSelect( select => {
-	const { getCurrentPost } = select( 'core/editor' );
-	const { status } = getCurrentPost();
+export default flowRight(
+	withSelect( select => {
+		const { getCurrentPost } = select( 'core/editor' );
+		const { status } = getCurrentPost();
 
-	return {
-		postStatus: status,
-	};
-} )( localize( EditorDocumentHead ) );
+		return {
+			postStatus: status,
+		};
+	} ),
+	connect( ( state, { postType } ) => {
+		const siteId = getSelectedSiteId( state );
+		return {
+			postTypeObject: getPostType( state, siteId, postType ),
+		};
+	} )
+)( localize( EditorDocumentHead ) );
