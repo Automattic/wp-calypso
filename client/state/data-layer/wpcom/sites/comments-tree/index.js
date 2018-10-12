4fa2d7b2e32948ad14db166ce19d08f2a8da1dc9
Markus Dobmann <markus@mpnjs.com>
2018-10-12 20:11:51 +0200

    Update fetchCommentsTreeForSite to use dispatchRequestEx (#27716)
    * refactor: update fetchCommentsTreeForSite to use dispatchRequestEx
    
    * tests: use jest exclusively

--- client/state/data-layer/wpcom/sites/comments-tree/index.js
+++ client/state/data-layer/wpcom/sites/comments-tree/index.js
@@ -12,27 +12,25 @@ import { flatMap, flatten, isArray, map } from 'lodash';
  */
 import { COMMENTS_TREE_SITE_ADD, COMMENTS_TREE_SITE_REQUEST } from 'state/action-types';
 import { http } from 'state/data-layer/wpcom-http/actions';
-import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
 import { errorNotice } from 'state/notices/actions';
 import getRawSite from 'state/selectors/get-raw-site';
 
 import { registerHandlers } from 'state/data-layer/handler-registry';
 
-export const fetchCommentsTreeForSite = ( { dispatch }, action ) => {
+export const fetchCommentsTreeForSite = action => {
 	const { siteId, status = 'unapproved' } = action.query;
 
-	dispatch(
-		http(
-			{
-				method: 'GET',
-				path: `/sites/${ siteId }/comments-tree`,
-				apiVersion: '1.1',
-				query: {
-					status: 'unapproved' === status ? 'pending' : status,
-				},
+	return http(
+		{
+			method: 'GET',
+			path: `/sites/${ siteId }/comments-tree`,
+			apiVersion: '1.1',
+			query: {
+				status: 'unapproved' === status ? 'pending' : status,
 			},
-			action
-		)
+		},
+		action
 	);
 };
 
@@ -56,7 +54,7 @@ const mapTree = ( tree, status, type ) => {
 	} ) );
 };
 
-export const addCommentsTree = ( { dispatch }, { query }, data ) => {
+export const addCommentsTree = ( { query }, data ) => {
 	const { siteId, status } = query;
 
 	const tree = [
@@ -65,15 +63,15 @@ export const addCommentsTree = ( { dispatch }, { query }, data ) => {
 		...mapTree( data.trackbacks_tree, status, 'trackback' ),
 	];
 
-	dispatch( {
+	return {
 		type: COMMENTS_TREE_SITE_ADD,
 		siteId,
 		status,
 		tree,
-	} );
+	};
 };
 
-export const announceFailure = ( { dispatch, getState }, { query } ) => {
+export const announceFailure = ( { query } ) => ( dispatch, getState ) => {
 	const { siteId } = query;
 	const site = getRawSite( getState(), siteId );
 
@@ -89,6 +87,10 @@ export const announceFailure = ( { dispatch, getState }, { query } ) => {
 
 registerHandlers( 'state/data-layer/wpcom/sites/comments-tree/index.js', {
 	[ COMMENTS_TREE_SITE_REQUEST ]: [
-		dispatchRequest( fetchCommentsTreeForSite, addCommentsTree, announceFailure ),
+		dispatchRequestEx( {
+			fetch: fetchCommentsTreeForSite,
+			onSuccess: addCommentsTree,
+			onError: announceFailure,
+		} ),
 	],
 } );
