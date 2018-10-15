13f525bc086d998988ed227bb3c66ff7a9dbc716
Jon Surrell <jon.surrell@automattic.com>
2018-10-15 12:15:12 +0200

    Use wp.element.createElement for JSX transform

--- bin/sdk/gutenberg.js
+++ bin/sdk/gutenberg.js
@@ -5,6 +5,14 @@
 const fs = require( 'fs' );
 const path = require( 'path' );
 
+const wpElementPragmaBabelPlugin = [
+	'@babel/plugin-transform-react-jsx',
+	{
+		pragma: 'wp.element.createElement',
+		pragmaFrag: 'wp.element.Fragment',
+	},
+];
+
 exports.config = ( { argv: { inputDir, outputDir }, getBaseConfig } ) => {
 	const baseConfig = getBaseConfig( {
 		cssFilename: '[name].css',
@@ -12,6 +20,21 @@ exports.config = ( { argv: { inputDir, outputDir }, getBaseConfig } ) => {
 	} );
 	const editorScript = path.join( inputDir, 'editor.js' );
 	const viewScript = path.join( inputDir, 'view.js' );
+
+	baseConfig.module.rules[ 0 ].use = baseConfig.module.rules[ 0 ].use.map( rule => {
+		const { loader, options } = rule;
+		if ( loader === 'babel-loader' ) {
+			const plugins = ( options && options.plugins ) || [];
+			return {
+				...rule,
+				options: {
+					...options,
+					plugins: [ ...plugins, wpElementPragmaBabelPlugin ],
+				},
+			};
+		}
+		return rule;
+	} );
 
 	return {
 		...baseConfig,
