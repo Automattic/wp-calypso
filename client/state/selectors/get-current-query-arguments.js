e6bb34023665c3df8dfe1ab238163152ce68fab9
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2018-10-15 17:04:42 +0200

    fix(state) Use a constant empty object to avoid re-rendering.

--- client/state/selectors/get-current-query-arguments.js
+++ client/state/selectors/get-current-query-arguments.js
@@ -10,11 +10,13 @@ import { get } from 'lodash';
  * Internal dependencies
  */
 
+const empty_object = {};
+
 /**
  * Gets the last query parameters set by a ROUTE_SET action
  * @param {Object} state - global redux state
  * @return {Object} current state value
  */
-export const getCurrentQueryArguments = state => get( state, 'ui.route.query.current', {} );
+export const getCurrentQueryArguments = state => get( state, 'ui.route.query.current', empty_object );
 
 export default getCurrentQueryArguments;
