41f3b29507d62f54830c0a0f72c57943c731a8a0
Jason Caldwell <jaswrks@o5p.me>
2018-10-16 11:14:27 -0800

    Add Tracks event: `calypso_refer_visit_response`

--- client/state/data-layer/third-party/refer/index.js
+++ client/state/data-layer/third-party/refer/index.js
@@ -3,15 +3,32 @@
 /**
  * External dependencies
  */
-import { noop } from 'lodash';
+import { pick } from 'lodash';
+import debugFactory from 'debug';
 
 /**
  * Internal dependencies
  */
+import analytics from 'lib/analytics';
 import { registerHandlers } from 'state/data-layer/handler-registry';
 import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
 import { http } from 'state/http/actions';
 import { AFFILIATE_REFERRAL } from 'state/action-types';
+
+const debug = debugFactory( 'calypso:analytics:aff-tracking' );
+
+const whitelistedEventProps = [
+	'status',
+	'success',
+	'duplicate',
+	'description',
+	'cookie_id',
+	'vendor_id',
+	'affiliate_id',
+	'campaign_id',
+	'sub_id',
+	'referrer',
+];
 
 const trackAffiliatePageLoad = action => {
 	const { affiliateId, campaignId, subId, urlPath } = action;
@@ -38,12 +55,55 @@ const trackAffiliatePageLoad = action => {
 	);
 };
 
+const onTrackAffiliatePageLoadSuccess = ( action, response ) => {
+	if (
+		'object' !== typeof response ||
+		'object' !== typeof response.body ||
+		'object' !== typeof response.body.data
+	) {
+		debug( 'Unexpected referral response: ', response );
+		return; // Not possible.
+	}
+
+	const responseBody = response.body;
+	const responseData = responseBody.data;
+	const eventProps = pick( responseData, whitelistedEventProps );
+
+	eventProps.status = response.status || '';
+	eventProps.success = responseBody.success || '';
+	eventProps.description = responseBody.message || 'success';
+
+	analytics.tracks.recordEvent( 'calypso_refer_visit_response', eventProps );
+};
+
+const onTrackAffiliatePageLoadError = ( action, error ) => {
+	if (
+		'object' !== typeof error ||
+		'object' !== typeof error.response ||
+		'string' !== typeof error.response.text
+	) {
+		debug( 'Unexpected referral error: ', error );
+		return; // Not possible.
+	}
+
+	const response = error.response;
+	let responseBody = JSON.parse( response.text );
+	responseBody = 'object' === typeof responseBody ? responseBody : {};
+	const eventProps = pick( responseBody, whitelistedEventProps );
+
+	eventProps.status = response.status || '';
+	eventProps.success = responseBody.success || '';
+	eventProps.description = responseBody.message || 'error';
+
+	analytics.tracks.recordEvent( 'calypso_refer_visit_response', eventProps );
+};
+
 registerHandlers( 'state/data-layer/third-party/refer', {
 	[ AFFILIATE_REFERRAL ]: [
 		dispatchRequestEx( {
 			fetch: trackAffiliatePageLoad,
-			onSuccess: noop,
-			onError: noop,
+			onSuccess: onTrackAffiliatePageLoadSuccess,
+			onError: onTrackAffiliatePageLoadError,
 		} ),
 	],
 } );
