b4e5f3742edb1d0ebd10bdfa69212caf9f5fa46f
Michael Turk <turk.michael@gmail.com>
2018-10-15 20:34:40 -0400

    Adding tracking

--- client/my-sites/activity/activity-log-item/aggregated.jsx
+++ client/my-sites/activity/activity-log-item/aggregated.jsx
@@ -24,6 +24,7 @@ import { filterStateToQuery } from 'state/activity-log/utils';
 import { addQueryArgs } from 'lib/url';
 import ActivityActor from './activity-actor';
 import ActivityMedia from './activity-media';
+import analytics from 'lib/analytics';
 
 const MAX_STREAM_ITEMS_IN_AGGREGATE = 10;
 
@@ -49,6 +50,14 @@ class ActivityLogAggregatedItem extends Component {
 
 		return addQueryArgs( query, window.location.pathname + window.location.hash );
 	}
+	trackContentLinkClick = ( {
+		target: {
+			dataset: { activity, section, intent },
+		},
+	} ) => {
+		const params = { activity, section, intent };
+		analytics.tracks.recordEvent( 'calypso_activitylog_item_click', params );
+	};
 
 	renderHeader() {
 		const { activity } = this.props;
@@ -124,7 +133,11 @@ class ActivityLogAggregatedItem extends Component {
 					</div>
 					<ActivityIcon activityIcon={ activityIcon } activityStatus={ activityStatus } />
 				</div>
-				<FoldableCard className="activity-log-item__card" header={ this.renderHeader() }>
+				<FoldableCard
+					className="activity-log-item__card"
+					header={ this.renderHeader() }
+					onClick={ this.trackContentLinkClick }
+				>
 					{ activity.streams.map( log => (
 						<Fragment key={ log.activityId }>
 							<ActivityLogItem
