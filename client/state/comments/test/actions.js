bee8efc3139e7f236436b7fa75a6c770d3095d1d
Markus Dobmann <markus@mpnjs.com>
2018-10-16 11:32:41 +0200

    tests: check if 'previousStatus' is set for changeCommentStatus and deleteComment

--- client/state/comments/test/actions.js
+++ client/state/comments/test/actions.js
@@ -10,6 +10,7 @@ import {
 	COMMENTS_WRITE,
 	COMMENTS_REPLY_WRITE,
 	COMMENTS_SET_ACTIVE_REPLY,
+	COMMENTS_CHANGE_STATUS,
 } from '../../action-types';
 import {
 	requestPostComments,
@@ -19,6 +20,7 @@ import {
 	likeComment,
 	unlikeComment,
 	setActiveReply,
+	changeCommentStatus,
 } from '../actions';
 import { NUMBER_OF_COMMENTS_PER_FETCH } from '../constants';
 import { setFeatureFlag } from 'test/helpers/config';
@@ -104,8 +106,81 @@ describe( 'actions', () => {
 			const dispatch = jest.fn();
 			deleteComment( SITE_ID, POST_ID, 'placeholder-123' )( dispatch, () => ( { comments: [] } ) );
 
-			expect( dispatch.mock.calls[ 0 ][ 0 ].type ).toEqual( COMMENTS_DELETE );
-			expect( dispatch.mock.calls[ 0 ][ 0 ].commentId ).toEqual( 'placeholder-123' );
+			expect( dispatch ).toHaveBeenCalledWith(
+				expect.objectContaining( {
+					type: COMMENTS_DELETE,
+					commentId: 'placeholder-123',
+				} )
+			);
+		} );
+
+		test( "should dispatch remove including the 'previousStatus'", () => {
+			const dispatch = jest.fn();
+			const getState = () => ( {
+				comments: {
+					items: {
+						[ SITE_ID ]: [
+							{
+								ID: 1,
+								status: 'trash',
+								post: {
+									ID: POST_ID,
+								},
+							},
+						],
+					},
+				},
+			} );
+			deleteComment( SITE_ID, POST_ID, 1 )( dispatch, getState );
+
+			expect( dispatch ).toHaveBeenCalledWith(
+				expect.objectContaining( {
+					type: COMMENTS_DELETE,
+					commentId: 1,
+					meta: expect.objectContaining( {
+						comment: expect.objectContaining( {
+							previousStatus: 'trash',
+						} ),
+					} ),
+				} )
+			);
+		} );
+	} );
+
+	describe( '#changeCommentStatus', () => {
+		test( "should dispatch including the 'previousStatus'", () => {
+			const dispatch = jest.fn();
+			const getState = () => ( {
+				comments: {
+					items: {
+						[ SITE_ID ]: [
+							{
+								ID: 1,
+								status: 'approved',
+								post: {
+									ID: POST_ID,
+								},
+							},
+						],
+					},
+				},
+			} );
+			changeCommentStatus( SITE_ID, POST_ID, 1, 'trash' )( dispatch, getState );
+
+			expect( dispatch ).toHaveBeenCalledWith(
+				expect.objectContaining( {
+					type: COMMENTS_CHANGE_STATUS,
+					siteId: SITE_ID,
+					postId: POST_ID,
+					commentId: 1,
+					status: 'trash',
+					meta: expect.objectContaining( {
+						comment: expect.objectContaining( {
+							previousStatus: 'approved',
+						} ),
+					} ),
+				} )
+			);
 		} );
 	} );
 
