654c76df11e31ff16a8204cdcdeaea77f1a39fec
Marin Atanasov <tyxla@abv.bg>
2018-10-16 16:26:08 +0300

    Gutenberg: Add icon to Jetpack block category

--- client/gutenberg/extensions/presets/jetpack/utils/block-category.js
+++ client/gutenberg/extensions/presets/jetpack/utils/block-category.js
@@ -5,8 +5,17 @@
  */
 import { getCategories, setCategories } from '@wordpress/blocks';
 
+/**
+ * Internal dependencies
+ */
+import JetpackLogo from 'components/jetpack-logo';
+
 setCategories( [
 	// Add a Jetpack block category
-	{ slug: 'jetpack', title: 'Jetpack' },
+	{
+		slug: 'jetpack',
+		title: 'Jetpack',
+		icon: <JetpackLogo />,
+	},
 	...getCategories().filter( ( { slug } ) => slug !== 'jetpack' ),
 ] );
