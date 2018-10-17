d36c027bfa7f07af4b52ca62230203dc0de59029
Bernie Reiter <ockham@raz.or.at>
2018-10-17 12:19:19 +0200

    Calypsofy imports

--- client/gutenberg/extensions/vr/editor.js
+++ client/gutenberg/extensions/vr/editor.js
@@ -1,101 +1,106 @@
-/* global wp */
-/* eslint react/react-in-jsx-scope: 0 */
+/** @format */
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Placeholder, SelectControl, TextControl } from '@wordpress/components';
+import { registerBlockType } from '@wordpress/blocks';
 
-( function( blocks, components, i18n ) {
-	const { registerBlockType } = blocks;
-	const {
-		Placeholder,
-		SelectControl,
-		TextControl
-	} = components;
-	const { __ } = i18n;
-
-	registerBlockType( 'jetpack/vr', {
-		title: __( 'VR Image', 'jetpack' ),
-		description: __( 'Embed 360° photos and Virtual Reality (VR) Content', 'jetpack' ),
-		icon: 'embed-photo',
-		category: 'embed',
-		support: {
-			html: false
+registerBlockType( 'jetpack/vr', {
+	title: __( 'VR Image', 'jetpack' ),
+	description: __( 'Embed 360° photos and Virtual Reality (VR) Content', 'jetpack' ),
+	icon: 'embed-photo',
+	category: 'embed',
+	support: {
+		html: false,
+	},
+	attributes: {
+		url: {
+			type: 'string',
+			default: '',
 		},
-		attributes: {
-			url: {
-				type: 'string',
-				'default': '',
-			},
-			view: {
-				type: 'string',
-				'default': '',
-			}
+		view: {
+			type: 'string',
+			default: '',
 		},
+	},
 
-		edit: props => {
-			const { attributes, setAttributes } = props;
+	edit: props => {
+		const { attributes, setAttributes } = props;
 
-			const onChangeUrl = value => setAttributes( { url: value.trim() } );
-			const onChangeView = value => setAttributes( { view: value } );
+		const onChangeUrl = value => setAttributes( { url: value.trim() } );
+		const onChangeView = value => setAttributes( { view: value } );
 
-			const renderEdit = () => {
-				if ( attributes.url && attributes.view ) {
-					return (
-						<div className={ props.className }>
-							<iframe
-								title={ __( 'VR Image', 'jetpack' ) }
-								allowFullScreen="true"
-								frameBorder="0"
-								width="100%"
-								height="300"
-								src={ 'https://vr.me.sh/view/?view=' + encodeURIComponent( attributes.view ) + '&url=' + encodeURIComponent( attributes.url ) }
-							/>
-						</div>
-					);
-				}
+		const renderEdit = () => {
+			if ( attributes.url && attributes.view ) {
 				return (
-					<div>
-						<Placeholder
-							key="placeholder"
-							icon="format-image"
-							label={ __( 'VR Image', 'jetpack' ) }
-							className={ props.className }
-						>
-							<TextControl
-								type="url"
-								style={ { flex: '1 1 auto' } }
-								label={ __( 'Enter URL to VR image', 'jetpack' ) }
-								value={ attributes.url }
-								onChange={ onChangeUrl }
-							/>
-							<SelectControl
-								label={ __( 'View Type', 'jetpack' ) }
-								disabled={ ! attributes.url }
-								value={ attributes.view }
-								onChange={ onChangeView }
-								options={ [
-									{ label: '', value: '' },
-									{ label: __( '360°', 'jetpack' ), value: '360' },
-									{ label: __( 'Cinema', 'jetpack' ), value: 'cinema' },
-								] }
-							/>
-						</Placeholder>
+					<div className={ props.className }>
+						<iframe
+							title={ __( 'VR Image', 'jetpack' ) }
+							allowFullScreen="true"
+							frameBorder="0"
+							width="100%"
+							height="300"
+							src={
+								'https://vr.me.sh/view/?view=' +
+								encodeURIComponent( attributes.view ) +
+								'&url=' +
+								encodeURIComponent( attributes.url )
+							}
+						/>
 					</div>
 				);
-			};
-
-			return renderEdit();
-		},
-		save: props => {
+			}
 			return (
-				<div className={ props.className }>
-					<iframe
-						title={ __( 'VR Image', 'jetpack' ) }
-						allowFullScreen="true"
-						frameBorder="0"
-						width="100%"
-						height="300"
-						src={ 'https://vr.me.sh/view/?view=' + encodeURIComponent( props.attributes.view ) + '&url=' + encodeURIComponent( props.attributes.url ) }
-					/>
+				<div>
+					<Placeholder
+						key="placeholder"
+						icon="format-image"
+						label={ __( 'VR Image', 'jetpack' ) }
+						className={ props.className }
+					>
+						<TextControl
+							type="url"
+							style={ { flex: '1 1 auto' } }
+							label={ __( 'Enter URL to VR image', 'jetpack' ) }
+							value={ attributes.url }
+							onChange={ onChangeUrl }
+						/>
+						<SelectControl
+							label={ __( 'View Type', 'jetpack' ) }
+							disabled={ ! attributes.url }
+							value={ attributes.view }
+							onChange={ onChangeView }
+							options={ [
+								{ label: '', value: '' },
+								{ label: __( '360°', 'jetpack' ), value: '360' },
+								{ label: __( 'Cinema', 'jetpack' ), value: 'cinema' },
+							] }
+						/>
+					</Placeholder>
 				</div>
 			);
-		}
-	} );
-} )( wp.blocks, wp.components, wp.i18n );
+		};
+
+		return renderEdit();
+	},
+	save: props => {
+		return (
+			<div className={ props.className }>
+				<iframe
+					title={ __( 'VR Image', 'jetpack' ) }
+					allowFullScreen="true"
+					frameBorder="0"
+					width="100%"
+					height="300"
+					src={
+						'https://vr.me.sh/view/?view=' +
+						encodeURIComponent( props.attributes.view ) +
+						'&url=' +
+						encodeURIComponent( props.attributes.url )
+					}
+				/>
+			</div>
+		);
+	},
+} );
