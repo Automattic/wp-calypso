91e63a930b3eaf5f84c35da2979ccefd2e22049b
Chris R <chris@bluefuton.com>
2018-10-18 10:35:39 +1300

    Account closure: allow account to be closed if user has only purchased premium themes (#27411)
    * Add new selectors and add theme messaging
    
    * Style premium theme list
    
    * Move theme selector to the correct spot
    
    * Move cancelable purchases selector
    
    * Formatting
    
    * deepFreeze all state in selector tests
    
    * Handle refundable themes

---
+++ client/state/selectors/test/get-user-purchased-premium-themes.js
@@ -0,0 +1,78 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import deepFreeze from 'deep-freeze';
+
+/**
+ * Internal dependencies
+ */
+import getUserPurchasedPremiumThemes from 'state/selectors/get-user-purchased-premium-themes';
+
+describe( 'getUserPurchasedPremiumThemes', () => {
+	const targetUserId = 123;
+	const examplePurchases = deepFreeze( [
+		{ ID: 1, product_name: 'domain registration', blog_id: 1337, user_id: targetUserId },
+		{ ID: 2, product_name: 'premium plan', blog_id: 1337, user_id: targetUserId },
+		{
+			ID: 3,
+			product_name: 'premium theme',
+			product_slug: 'premium_theme',
+			blog_id: 1337,
+			user_id: targetUserId,
+		},
+	] );
+
+	test( 'should return an empty array because there are no purchases', () => {
+		const state = {
+			purchases: {
+				data: [],
+				error: null,
+				isFetchingSitePurchases: false,
+				isFetchingUserPurchases: false,
+				hasLoadedSitePurchasesFromServer: false,
+				hasLoadedUserPurchasesFromServer: true,
+			},
+		};
+
+		expect( getUserPurchasedPremiumThemes( state, targetUserId ) ).toEqual( [] );
+	} );
+
+	test( 'should return false because the data is not ready', () => {
+		const state = {
+			purchases: {
+				data: examplePurchases,
+				error: null,
+				isFetchingSitePurchases: false,
+				isFetchingUserPurchases: false,
+				hasLoadedSitePurchasesFromServer: false,
+				hasLoadedUserPurchasesFromServer: false,
+			},
+		};
+
+		expect( getUserPurchasedPremiumThemes( state, targetUserId ) ).toBe( false );
+	} );
+
+	test( 'should return an array of themes because there is a theme purchase for the specified user', () => {
+		const state = {
+			purchases: {
+				data: examplePurchases,
+				error: null,
+				isFetchingSitePurchases: false,
+				isFetchingUserPurchases: false,
+				hasLoadedSitePurchasesFromServer: false,
+				hasLoadedUserPurchasesFromServer: true,
+			},
+		};
+
+		const purchasedPremiumThemes = getUserPurchasedPremiumThemes( state, targetUserId );
+		expect( purchasedPremiumThemes.length ).toBe( 1 );
+		expect( purchasedPremiumThemes[ 0 ] ).toMatchObject( {
+			id: 3,
+			productName: 'premium theme',
+			productSlug: 'premium_theme',
+			userId: targetUserId,
+		} );
+	} );
+} );
---
+++ client/state/selectors/test/has-cancelable-user-purchases.js
@@ -0,0 +1,134 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import deepFreeze from 'deep-freeze';
+
+/**
+ * Internal dependencies
+ */
+import hasCancelableUserPurchases from 'state/selectors/has-cancelable-user-purchases';
+
+describe( 'hasCancelableUserPurchases', () => {
+	const targetUserId = 123;
+	const examplePurchases = [
+		{ ID: 1, product_name: 'domain registration', blog_id: 1337, user_id: targetUserId },
+		{ ID: 2, product_name: 'premium plan', blog_id: 1337, user_id: targetUserId },
+		{
+			ID: 3,
+			product_name: 'premium theme',
+			product_slug: 'premium_theme',
+			blog_id: 1337,
+			user_id: targetUserId,
+		},
+	];
+
+	test( 'should return false because there are no purchases', () => {
+		const state = deepFreeze( {
+			purchases: {
+				data: [],
+				error: null,
+				isFetchingSitePurchases: false,
+				isFetchingUserPurchases: false,
+				hasLoadedSitePurchasesFromServer: false,
+				hasLoadedUserPurchasesFromServer: true,
+			},
+		} );
+
+		expect( hasCancelableUserPurchases( state, targetUserId ) ).toBe( false );
+	} );
+
+	test( 'should return true because there are purchases from the target user', () => {
+		const state = deepFreeze( {
+			purchases: {
+				data: examplePurchases,
+				error: null,
+				isFetchingSitePurchases: false,
+				isFetchingUserPurchases: false,
+				hasLoadedSitePurchasesFromServer: false,
+				hasLoadedUserPurchasesFromServer: true,
+			},
+		} );
+
+		expect( hasCancelableUserPurchases( state, targetUserId ) ).toBe( true );
+	} );
+
+	test( 'should return false because there are no purchases from this user', () => {
+		const state = deepFreeze( {
+			purchases: {
+				data: examplePurchases,
+				error: null,
+				isFetchingSitePurchases: false,
+				isFetchingUserPurchases: false,
+				hasLoadedSitePurchasesFromServer: false,
+				hasLoadedUserPurchasesFromServer: true,
+			},
+		} );
+
+		expect( hasCancelableUserPurchases( state, 65535 ) ).toBe( false );
+	} );
+
+	test( 'should return false because the data is not ready', () => {
+		const state = deepFreeze( {
+			purchases: {
+				data: examplePurchases,
+				error: null,
+				isFetchingSitePurchases: false,
+				isFetchingUserPurchases: false,
+				hasLoadedSitePurchasesFromServer: false,
+				hasLoadedUserPurchasesFromServer: false,
+			},
+		} );
+
+		expect( hasCancelableUserPurchases( state, targetUserId ) ).toBe( false );
+	} );
+
+	test( 'should return false because the only purchase is a non-refundable theme', () => {
+		const state = deepFreeze( {
+			purchases: {
+				data: [
+					{
+						ID: 3,
+						product_name: 'premium theme',
+						product_slug: 'premium_theme',
+						blog_id: 1337,
+						user_id: targetUserId,
+						is_refundable: false,
+					},
+				],
+				error: null,
+				isFetchingSitePurchases: false,
+				isFetchingUserPurchases: false,
+				hasLoadedSitePurchasesFromServer: false,
+				hasLoadedUserPurchasesFromServer: true,
+			},
+		} );
+
+		expect( hasCancelableUserPurchases( state, targetUserId ) ).toBe( false );
+	} );
+
+	test( 'should return true because one of the purchases is a refundable theme', () => {
+		const state = deepFreeze( {
+			purchases: {
+				data: [
+					{
+						ID: 3,
+						product_name: 'premium theme',
+						product_slug: 'premium_theme',
+						blog_id: 1337,
+						user_id: targetUserId,
+						is_refundable: true,
+					},
+				],
+				error: null,
+				isFetchingSitePurchases: false,
+				isFetchingUserPurchases: false,
+				hasLoadedSitePurchasesFromServer: false,
+				hasLoadedUserPurchasesFromServer: true,
+			},
+		} );
+
+		expect( hasCancelableUserPurchases( state, targetUserId ) ).toBe( true );
+	} );
+} );
