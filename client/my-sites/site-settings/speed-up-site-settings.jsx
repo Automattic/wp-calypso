ca7fc449e539b708009f92943f63590e8779651a
Jeremy Herve <jeremy@jeremy.hu>
2018-10-18 23:14:09 +0200

    Site Accelerator: first pass - add new field in Writing settings
    Companion PR: https://github.com/Automattic/jetpack/pull/10311/
    
    This feature is only available when running Jetpack 6.6+
    
    The updated UI has 3 toggles:
    - A main toggle that allows one to turn on "Site Accelerator", aka 2 different modules in the background
    (Photon and Asset CDN).
    - 2 sub-toggles, regular module toggles allowing one to select which of the 2 modules they need.

--- client/my-sites/site-settings/speed-up-site-settings.jsx
+++ client/my-sites/site-settings/speed-up-site-settings.jsx
@@ -14,8 +14,11 @@ import { connect } from 'react-redux';
 import Card from 'components/card';
 import JetpackModuleToggle from 'my-sites/site-settings/jetpack-module-toggle';
 import FormFieldset from 'components/forms/form-fieldset';
+import CompactFormToggle from 'components/forms/form-toggle/compact';
 import isJetpackModuleUnavailableInDevelopmentMode from 'state/selectors/is-jetpack-module-unavailable-in-development-mode';
 import isJetpackSiteInDevelopmentMode from 'state/selectors/is-jetpack-site-in-development-mode';
+import { activateModule, deactivateModule } from 'state/jetpack/modules/actions';
+import isJetpackModuleActive from 'state/selectors/is-jetpack-module-active';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { getSiteSlug } from 'state/sites/selectors';
 import SupportInfo from 'components/support-info';
@@ -26,11 +29,38 @@ class SpeedUpSiteSettings extends Component {
 		isRequestingSettings: PropTypes.bool,
 		isSavingSettings: PropTypes.bool,
 		jetpackVersionSupportsLazyImages: PropTypes.bool,
+		siteAcceleratorSupported: PropTypes.bool,
+		activateModule: PropTypes.func,
+		deactivateModule: PropTypes.func,
 
 		// Connected props
 		photonModuleUnavailable: PropTypes.bool,
 		selectedSiteId: PropTypes.number,
 		siteSlug: PropTypes.string,
+	};
+
+	handleCdnChange = () => {
+		const { photonModuleActive, assetCdnModuleActive, selectedSiteId } = this.props;
+
+		// Check if any of the CDN options are on.
+		const cdnStatus = photonModuleActive || assetCdnModuleActive;
+
+		// If one of them is on, we turn everything off.
+		if ( true === cdnStatus ) {
+			if ( false === ! photonModuleActive ) {
+				this.props.deactivateModule( selectedSiteId, 'photon' );
+			}
+			if ( false === ! assetCdnModuleActive ) {
+				this.props.deactivateModule( selectedSiteId, 'photon-cdn' );
+			}
+		} else {
+			if ( false === photonModuleActive ) {
+				this.props.activateModule( selectedSiteId, 'photon' );
+			}
+			if ( false === assetCdnModuleActive ) {
+				this.props.activateModule( selectedSiteId, 'photon-cdn' );
+			}
+		}
 	};
 
 	render() {
@@ -41,28 +71,50 @@ class SpeedUpSiteSettings extends Component {
 			isSavingSettings,
 			translate,
 			jetpackVersionSupportsLazyImages,
+			siteAcceleratorSupported,
+			photonModuleActive,
+			assetCdnModuleActive,
 		} = this.props;
 		const isRequestingOrSaving = isRequestingSettings || isSavingSettings;
+		const cdnStatus = photonModuleActive || assetCdnModuleActive;
 
 		return (
 			<div className="site-settings__module-settings site-settings__speed-up-site-settings">
 				<Card>
 					<FormFieldset className="site-settings__formfieldset">
 						<SupportInfo
-							text={ translate( 'Hosts your image files on the global WordPress.com servers.' ) }
-							link="https://jetpack.com/support/photon/"
-						/>
-						<JetpackModuleToggle
-							siteId={ selectedSiteId }
-							moduleSlug="photon"
-							label={ translate( 'Serve images from our servers' ) }
-							description={ translate(
-								'Jetpack will optimize your images and serve them from the server ' +
-									'location nearest to your visitors. Using our global content delivery ' +
-									'network will boost the loading speed of your site.'
+							text={ translate(
+								"Jetpack's global Content Delivery Network (CDN) optimizes " +
+									'files and images so your visitors enjoy ' +
+									'the fastest experience regardless of device or location.'
 							) }
-							disabled={ isRequestingOrSaving || photonModuleUnavailable }
+							link="https://jetpack.com/support/image-cdn/"
 						/>
+						<CompactFormToggle
+							checked={ cdnStatus }
+							disabled={
+								isRequestingOrSaving || photonModuleUnavailable || ! siteAcceleratorSupported
+							}
+							onChange={ this.handleCdnChange }
+						>
+							{ translate( 'Enable site accelerator' ) }
+						</CompactFormToggle>
+						<div className="site-settings__child-settings">
+							<JetpackModuleToggle
+								siteId={ selectedSiteId }
+								moduleSlug="photon"
+								label={ translate( 'Speed up images' ) }
+								disabled={ isRequestingOrSaving || photonModuleUnavailable }
+							/>
+							<JetpackModuleToggle
+								siteId={ selectedSiteId }
+								moduleSlug="photon-cdn"
+								label={ translate(
+									'Speed up all static files (CSS and JavaScript) for WordPress, WooCommerce, and Jetpack'
+								) }
+								disabled={ isRequestingOrSaving || ! siteAcceleratorSupported }
+							/>
+						</div>
 					</FormFieldset>
 
 					{ jetpackVersionSupportsLazyImages && (
@@ -92,18 +144,26 @@ class SpeedUpSiteSettings extends Component {
 	}
 }
 
-export default connect( state => {
-	const selectedSiteId = getSelectedSiteId( state );
-	const siteInDevMode = isJetpackSiteInDevelopmentMode( state, selectedSiteId );
-	const moduleUnavailableInDevMode = isJetpackModuleUnavailableInDevelopmentMode(
-		state,
-		selectedSiteId,
-		'photon'
-	);
+export default connect(
+	state => {
+		const selectedSiteId = getSelectedSiteId( state );
+		const siteInDevMode = isJetpackSiteInDevelopmentMode( state, selectedSiteId );
+		const moduleUnavailableInDevMode = isJetpackModuleUnavailableInDevelopmentMode(
+			state,
+			selectedSiteId,
+			'photon'
+		);
 
-	return {
-		photonModuleUnavailable: siteInDevMode && moduleUnavailableInDevMode,
-		selectedSiteId,
-		siteSlug: getSiteSlug( state, selectedSiteId ),
-	};
-} )( localize( SpeedUpSiteSettings ) );
+		return {
+			photonModuleUnavailable: siteInDevMode && moduleUnavailableInDevMode,
+			selectedSiteId,
+			siteSlug: getSiteSlug( state, selectedSiteId ),
+			photonModuleActive: isJetpackModuleActive( state, selectedSiteId, 'photon' ),
+			assetCdnModuleActive: isJetpackModuleActive( state, selectedSiteId, 'photon-cdn' ),
+		};
+	},
+	{
+		activateModule,
+		deactivateModule,
+	}
+)( localize( SpeedUpSiteSettings ) );
