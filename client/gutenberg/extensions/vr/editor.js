4837e66dd0f5dd4ff3817f4418b71e046e27857b
Bernie Reiter <ockham@raz.or.at>
2018-10-18 13:40:45 +0200

    Gutenblocks: Add VR block (#27873)
    [Moved](https://github.com/Automattic/jetpack/blob/ff229729399161b6ed0ff8b19b05e30a55928b2d/modules/shortcodes/js/blocks/vr-block.jsx) from the Jetpack repo. Some modifications to be consistent with Gutenblocks in Calypso. Includes some modifications to adapt to Calypso's code style.
    
    I'm not yet adding this to the `jetpack` preset, since it's not in good enough shape yet (and was previously broken).
    
    * Calypsofy imports
    * Import stylesheet
    * Remove weird `renderEdit()` wrapper
    * Change block type to `a8c/vr`
    * Componentize `save` and `edit`
    * Remove obsolete wrapping `div`
    * Change `category` to `jetpack`
    * Add some keywords
    * Remove obsolete defaults
    * Lowercase `content`
    * Fix SCSS
    * Remove obsolete styling

---
+++ client/gutenberg/extensions/vr/editor.js
@@ -0,0 +1,34 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { registerBlockType } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies
+ */
+import './editor.scss';
+import VRImageEdit from './edit';
+import VRImageSave from './save';
+
+registerBlockType( 'a8c/vr', {
+	title: __( 'VR Image', 'jetpack' ),
+	description: __( 'Embed 360° photos and Virtual Reality (VR) content', 'jetpack' ),
+	icon: 'embed-photo',
+	category: 'jetpack',
+	keywords: [ __( 'vr', 'jetpack' ), __( 'panorama', 'jetpack' ), __( '360', 'jetpack' ) ],
+	support: {
+		html: false,
+	},
+	attributes: {
+		url: {
+			type: 'string',
+		},
+		view: {
+			type: 'string',
+		},
+	},
+	edit: VRImageEdit,
+	save: VRImageSave,
+} );
