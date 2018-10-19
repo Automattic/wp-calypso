c6b917fb9e82def6fbe6254a00ac9de52d4dcbc1
Jarda Snajdr <jsnajdr@gmail.com>
2018-10-19 15:36:43 +0200

    Section Loader: create separate load and preload functions, avoid double module init
    Fixes a bug where loading a webpack chunk that's shared by two sections (e.g., `posts-pages`)
    leads to double initialization of the module for the first section.
    
    Also splits the `preload` function into two separate ones (`load` and `preload`) as their
    semantics are very different.

--- client/sections-helper.js
+++ client/sections-helper.js
@@ -14,7 +14,7 @@
 /**
  * External dependencies
  */
-import { filter, isEmpty, find } from 'lodash';
+import { find } from 'lodash';
 
 /**
  * Internal dependencies
@@ -50,9 +50,26 @@ function maybeLoadCSS( sectionName ) {
 
 export function preload( sectionName ) {
 	maybeLoadCSS( sectionName );
-	const filteredSections = filter( sections, { name: sectionName } );
-	if ( isEmpty( filteredSections ) ) {
-		return Promise.reject( `Attempting to load non-existent section: ${ sectionName }` );
+
+	const section = find( sections, { name: sectionName } );
+
+	if ( section ) {
+		section.load();
 	}
-	return Promise.all( filteredSections.map( section => section.load() ) );
+}
+
+export function load( sectionName, moduleName ) {
+	maybeLoadCSS( sectionName );
+
+	const section = find( sections, { name: sectionName, module: moduleName } );
+
+	if ( ! section ) {
+		return Promise.reject(
+			`Attempting to load non-existent section: ${ sectionName } (module=${ moduleName })`
+		);
+	}
+
+	// section.load() loads the module synchronously (using require()) in environments without
+	// code splitting. The return value must be explicitly resolved to Promise.
+	return Promise.resolve( section.load() );
 }
