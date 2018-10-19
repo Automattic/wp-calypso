fa7042115116546230fed6fa470c9aa6762ef542
Marko Andrijasevic <marko.andrijasevic@automattic.com>
2018-10-19 17:28:55 +0200

    Gutenberg: fix dynamics block rendering in editor (#27945)
    Dynamic blocks requests for rendered content were not properly mapped
    to their WP.com endpoint paths equivalent. This was caused by not having
    any handling for mapping the gutenberg/v1 namespace.

--- client/gutenberg/editor/api-middleware/test/index.js
+++ client/gutenberg/editor/api-middleware/test/index.js
@@ -6,55 +6,45 @@
 /**
  * Internal dependencies
  */
-import { pathRewriteMiddleware, urlRewriteMiddleware } from '../index';
+import { wpcomPathMappingMiddleware } from '../index';
 
-const rootUrl = 'https://public-api.wordpress.com';
 const testSiteSlug = 'example.wordpress.com';
 
-const pathReturner = path => {
+const optionsReturner = options => {
 	let output;
 
-	pathRewriteMiddleware( { path: path }, nextOutput => ( output = nextOutput ), testSiteSlug );
+	wpcomPathMappingMiddleware( options, nextOutput => ( output = nextOutput ), testSiteSlug );
 
-	return output.path;
+	return output;
 };
 
-it( 'Adds the site slug to the path', () => {
-	[
-		[ '/', '/' ],
-		[ '/wp/v2/', `/sites/${ testSiteSlug }/` ],
+describe( 'wpcomPathMappingMiddleware', () => {
+	it( 'Adds the site slug to the path', () => {
 		[
-			'/wp/v2/users/?who=authors&per_page=10',
-			`/sites/${ testSiteSlug }/users/?who=authors&per_page=10`,
-		],
-		[ '/wp/v2/types/post?context=edit', `/sites/${ testSiteSlug }/types/post?context=edit` ],
-		[ '/wp/v2/taxonomies?context=edit', `/sites/${ testSiteSlug }/taxonomies?context=edit` ],
-	].forEach( ( [ input, output ] ) => expect( pathReturner( input ) ).toEqual( output ) );
-} );
+			[ '/', '/' ],
+			[ '/wp/v2/', `/sites/${ testSiteSlug }/` ],
+			[
+				'/wp/v2/users/?who=authors&per_page=10',
+				`/sites/${ testSiteSlug }/users/?who=authors&per_page=10`,
+			],
+			[ '/wp/v2/types/post?context=edit', `/sites/${ testSiteSlug }/types/post?context=edit` ],
+			[ '/wp/v2/taxonomies?context=edit', `/sites/${ testSiteSlug }/taxonomies?context=edit` ],
+		].forEach( ( [ input, output ] ) =>
+			expect( optionsReturner( { path: input } ).path ).toEqual( output )
+		);
+	} );
 
-const urlReturner = url => {
-	let output;
-
-	urlRewriteMiddleware( { url: url }, nextOutput => ( output = nextOutput ), testSiteSlug );
-
-	return output.url;
-};
-
-it( 'Adds the site slug to the url', () => {
-	[
-		[ rootUrl, rootUrl ],
-		[ `${ rootUrl }/wp/v2/`, `${ rootUrl }/sites/${ testSiteSlug }/` ],
+	it( 'Sets correct namespace based on path', () => {
 		[
-			`${ rootUrl }/wp/v2/users/?who=authors&per_page=10`,
-			`${ rootUrl }/sites/${ testSiteSlug }/users/?who=authors&per_page=10`,
-		],
-		[
-			`${ rootUrl }/wp/v2/types/post?context=edit`,
-			`${ rootUrl }/sites/${ testSiteSlug }/types/post?context=edit`,
-		],
-		[
-			`${ rootUrl }/wp/v2/taxonomies?context=edit`,
-			`${ rootUrl }/sites/${ testSiteSlug }/taxonomies?context=edit`,
-		],
-	].forEach( ( [ input, output ] ) => expect( urlReturner( input ) ).toEqual( output ) );
+			[ '/', undefined ],
+			[ '/wp/v2/', `wp/v2` ],
+			[ '/wp/v2/users/?who=authors&per_page=10', 'wp/v2' ],
+			[ '/wp/v2/types/post?context=edit', 'wp/v2' ],
+			[ '/gutenberg/v1/block-renderer/core/latest-comments', 'gutenberg/v1' ],
+			[ '/oembed/1.0/proxy?url=example.wordpress.com', 'oembed/1.0' ],
+			[ '/oembed/110/proxy?url=example.wordpress.com', undefined ],
+		].forEach( ( [ input, output ] ) =>
+			expect( optionsReturner( { path: input } ).apiNamespace ).toEqual( output )
+		);
+	} );
 } );
