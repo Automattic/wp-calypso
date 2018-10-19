36b483ecff81c97009942479b2fcbe1141d31ff9
Markus Dobmann <markus@mpnjs.com>
2018-10-19 08:37:55 +0200

    danger: remove ✅ flag

--- dangerfile.js
+++ dangerfile.js
@@ -49,6 +49,4 @@ if ( results.warnings.length > 0 || results.fails.length > 0 ) {
 	markdown(
 		"This is an automated check which relies on [`PULL_REQUEST_TEMPLATE`](https://github.com/Automattic/wp-calypso/blob/master/.github/PULL_REQUEST_TEMPLATE.md). We encourage you to follow that template as it helps Calypso maintainers do their job. If you think 'Testing instructions' are not needed for your PR - please explain why you think so. Thanks for cooperation :robot:"
 	);
-} else {
-	markdown( "That's a great PR description, thank you so much for your effort!" );
 }
