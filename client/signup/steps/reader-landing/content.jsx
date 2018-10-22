e4c8a7622cca16be310c971ba7b106d43ef35fc3
bluefuton <chris@bluefuton.com>
2018-10-23 12:15:29 +1300

    Add copy

--- client/signup/steps/reader-landing/content.jsx
+++ client/signup/steps/reader-landing/content.jsx
@@ -24,32 +24,48 @@ class ReaderLandingStepContent extends PureComponent {
 					<div className="reader-landing__feature">
 						<div className="reader-landing__feature-image-placeholder" />
 						<div className="reader-landing__feature-detail">
-							<h2>Discover new content</h2>
-							<p>blah</p>
+							<h2>{ translate( 'Explore the best content on WordPress' ) }</h2>
+							<p>
+								{ translate(
+									'Head to the Reader’s Discover section to browse recommended sites, editors’ picks, and site-building resources.'
+								) }
+							</p>
 						</div>
 					</div>
 
 					<div className="reader-landing__feature">
 						<div className="reader-landing__feature-image-placeholder" />
 						<div className="reader-landing__feature-detail">
-							<h2>Discover new content</h2>
-							<p>blah</p>
+							<h2>{ translate( 'Never miss a post from your favorite sites' ) }</h2>
+							<p>
+								{ translate(
+									'Whether you’re on your laptop, tablet, or smartphone, set up notifications to get word when a new post appears on a site you follow.'
+								) }
+							</p>
 						</div>
 					</div>
 
 					<div className="reader-landing__feature">
 						<div className="reader-landing__feature-image-placeholder" />
 						<div className="reader-landing__feature-detail">
-							<h2>Discover new content</h2>
-							<p>blah</p>
+							<h2>{ translate( 'Dive into your reading from any mobile device' ) }</h2>
+							<p>
+								{ translate(
+									'Access the Reader on the go with the WordPress app, available for iOS or Android devices.'
+								) }
+							</p>
 						</div>
 					</div>
 
 					<div className="reader-landing__feature">
 						<div className="reader-landing__feature-image-placeholder" />
 						<div className="reader-landing__feature-detail">
-							<h2>Discover new content</h2>
-							<p>blah</p>
+							<h2>{ translate( 'Lively conversations made easy' ) }</h2>
+							<p>
+								{ translate(
+									'When a post generates an active discussion, you can stay up-to-date on the latest comments — or leave a few yourself.'
+								) }
+							</p>
 						</div>
 					</div>
 				</section>
