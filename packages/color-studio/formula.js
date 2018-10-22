02dfd13688ed519de9b4058a1c630004462dbc63
Dominik Porada <dominik@porada.co>
2018-10-22 23:30:16 +0200

    nail_care:

--- packages/color-studio/formula.js
+++ packages/color-studio/formula.js
@@ -1,6 +1,9 @@
 const chroma = require('chroma-js')
 
+/* eslint-disable no-unused-vars */
 const COLOR_WHITE = '#fff'
+const COLOR_BLACK = '#000'
+/* eslint-enable no-unused-vars */
 
 module.exports = createPaletteColors
 
@@ -28,15 +31,15 @@ function createPaletteColors(baseColor, baseColorName = '') {
 }
 
 function createBrightShades(baseColor) {
-  const shade100 = chroma.mix(baseColor, COLOR_WHITE, 0.85, 'lch')
-  const shade50 = chroma.mix(shade100, COLOR_WHITE, 0.65)
+  const shade50 = chroma.mix(baseColor, COLOR_WHITE, 0.9, 'lch').desaturate(0.05)
+  const shade100 = chroma.mix(baseColor, COLOR_WHITE, 0.8, 'lch')
   const colors = chroma.scale([shade100, baseColor]).mode('lch').colors(5)
   colors.unshift(shade50.hex())
   return colors
 }
 
 function createDarkShades(baseColor) {
-  const shade900 = chroma(baseColor).darken(1.85).desaturate(1)
+  const shade900 = chroma(baseColor).darken(1.7).desaturate(1)
   const colors = chroma.scale([baseColor, shade900]).mode('lch').colors(5)
   return colors
 }
