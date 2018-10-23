3653809ec3989d0afdef6a566afab38fce3b9926
Jon Surrell <jon.surrell@automattic.com>
2018-10-23 17:08:40 +0200

    Adds lookup for simple payments

--- client/gutenberg/extensions/simple-payments/edit.js
+++ client/gutenberg/extensions/simple-payments/edit.js
@@ -5,6 +5,12 @@
  */
 import { Component, Fragment } from '@wordpress/element';
 import { withInstanceId } from '@wordpress/compose';
+import { withSelect } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { SIMPLE_PAYMENTS_PRODUCT_POST_TYPE } from 'lib/simple-payments/constants';
 
 class Edit extends Component {
 	handleChange = event => {
@@ -28,9 +34,19 @@ class Edit extends Component {
 					type="number"
 					value={ attributes.paymentId || '' }
 				/>
+				<pre>
+					{ JSON.stringify( this.props.simplePayments, undefined, 2 ) }
+				</pre>
 			</Fragment>
 		);
 	}
 }
 
-export default withInstanceId( Edit );
+export default withSelect( select => {
+	const { getEntityRecords } = select( 'core' );
+	return {
+		simplePayments: getEntityRecords( 'postType', SIMPLE_PAYMENTS_PRODUCT_POST_TYPE, {
+			status: 'publish',
+		} ),
+	};
+} )( withInstanceId( Edit ) );
