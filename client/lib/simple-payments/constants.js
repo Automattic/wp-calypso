a2dfed75ec86b1c4a05365873abe6bbf19d67012
Jon Surrell <jon.surrell@automattic.com>
2018-10-24 21:20:21 +0200

    Share SUPPORTED_CURRENCY_LIST

--- client/lib/simple-payments/constants.js
+++ client/lib/simple-payments/constants.js
@@ -1,2 +1,30 @@
 /** @format */
 export const SIMPLE_PAYMENTS_PRODUCT_POST_TYPE = 'jp_pay_product';
+
+// https://developer.paypal.com/docs/integration/direct/rest/currency-codes/
+export const SUPPORTED_CURRENCY_LIST = [
+	'USD',
+	'EUR',
+	'AUD',
+	'BRL',
+	'CAD',
+	'CZK',
+	'DKK',
+	'HKD',
+	'HUF',
+	'ILS',
+	'JPY',
+	'MYR',
+	'MXN',
+	'TWD',
+	'NZD',
+	'NOK',
+	'PHP',
+	'PLN',
+	'GBP',
+	'RUB',
+	'SGD',
+	'SEK',
+	'CHF',
+	'THB',
+];
