56fe9a397177565bc3e6dd44bd77def7b6aead93
Ramon <ramonjd@users.noreply.github.com>
2018-10-24 17:40:31 +1100

    To test an upcoming new signup journey step (Site Type) we're adding redux actions, reducer and selectors to store the current site type. (#28041)

---
+++ client/state/signup/steps/site-type/actions.js
@@ -0,0 +1,14 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+
+import { SIGNUP_STEPS_SITE_TYPE_SET } from 'state/action-types';
+
+export function setSiteType( siteType ) {
+	return {
+		type: SIGNUP_STEPS_SITE_TYPE_SET,
+		siteType,
+	};
+}
---
+++ client/state/signup/steps/site-type/test/reducer.js
@@ -0,0 +1,21 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import signupDependencyStore from '../reducer';
+import { SIGNUP_STEPS_SITE_TYPE_SET } from 'state/action-types';
+
+describe( 'reducer', () => {
+	test( 'should update the site type', () => {
+		expect(
+			signupDependencyStore(
+				{},
+				{
+					type: SIGNUP_STEPS_SITE_TYPE_SET,
+					siteType: 'mushroom-sandwich',
+				}
+			)
+		).toEqual( 'mushroom-sandwich' );
+	} );
+} );
