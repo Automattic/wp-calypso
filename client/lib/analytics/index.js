e71b7c38202fd2e94c7554ae89e65708739a7a30
Leif Singer <git@singer.sh>
2018-10-24 13:01:44 +0200

    Analytics: add `clientId` custom dimension

--- client/lib/analytics/index.js
+++ client/lib/analytics/index.js
@@ -477,8 +477,11 @@ const analytics = {
 				if ( _user && _user.get() ) {
 					parameters.userId = hashPii( _user.get().ID );
 				}
-
 				window.ga( 'create', config( 'google_analytics_key' ), 'auto', parameters );
+				window.ga( function( tracker ) {
+					const clientId = tracker.get( 'clientId' );
+					window.ga( 'set', 'dimension3', clientId );
+				} );
 				window.ga( 'set', 'anonymizeIp', true );
 				analytics.ga.initialized = true;
 			}
