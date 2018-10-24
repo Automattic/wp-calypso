14fe43d247d5182b5b0d821e0674a41e4c0c3588
bluefuton <chris@bluefuton.com>
2018-10-24 15:15:31 +1300

    Add remove button

--- client/me/site-blocks/list-item.jsx
+++ client/me/site-blocks/list-item.jsx
@@ -12,10 +12,11 @@ import { connect } from 'react-redux';
  */
 import { getSite } from 'state/reader/sites/selectors';
 import ExternalLink from 'components/external-link';
+import Button from 'components/button';
 
 class SiteBlockListItem extends Component {
 	render() {
-		const { site } = this.props;
+		const { site, translate } = this.props;
 
 		if ( ! site ) {
 			return null;
@@ -24,6 +25,14 @@ class SiteBlockListItem extends Component {
 		return (
 			<div className="site-blocks__list-item">
 				<ExternalLink href={ site.URL }>{ site.name }</ExternalLink>
+				<Button
+					scary
+					borderless
+					className="site-blocks__remove-button"
+					title={ translate( 'Remove site block' ) }
+				>
+					<span>{ translate( 'Remove' ) }</span>
+				</Button>
 			</div>
 		);
 	}
