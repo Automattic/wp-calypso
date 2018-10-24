716a1e69244ab86546698396b1332403d86ed4f5
Ramon <ramonjd@users.noreply.github.com>
2018-10-25 09:57:23 +1100

    Checklist Site Icon: providing a way to pause/hide Guided Tour modals (#27895)
    * Adding a `isPaused` property to `state.ui.guidedTour` to flag a temporary cessation of a guided tour in the case of media notifications.
    
    Extended reducer tests
    Some minor destructuring
    
    Added explanatory comments in relation to the use of setTimeout. Flux complains about dispatching during a dispatch. I didn't see much point in spending time working out how to defer the dispatch in the Flux implementation since there's a present effort to reduxify all these stores.
    
    * Rather than a `setTimeout` let's use the redux bridge to dispatch redux actions.

--- client/state/ui/guided-tours/reducer.js
+++ client/state/ui/guided-tours/reducer.js
@@ -9,11 +9,13 @@ import { omit } from 'lodash';
 /**
  * Internal dependencies
  */
-import { GUIDED_TOUR_UPDATE } from 'state/action-types';
+import { GUIDED_TOUR_UPDATE, GUIDED_TOUR_PAUSE, GUIDED_TOUR_RESUME } from 'state/action-types';
 
 export function guidedTour( state = {}, action ) {
 	switch ( action.type ) {
 		case GUIDED_TOUR_UPDATE:
+		case GUIDED_TOUR_PAUSE:
+		case GUIDED_TOUR_RESUME:
 			return Object.assign( {}, state, omit( action, 'type' ) );
 	}
 	return state;
--- client/state/ui/guided-tours/test/reducer.js
+++ client/state/ui/guided-tours/test/reducer.js
@@ -1,21 +1,33 @@
 /** @format */
 
 /**
- * External dependencies
- */
-import { expect } from 'chai';
-
-/**
  * Internal dependencies
  */
 import { guidedTour } from '../reducer';
+import { GUIDED_TOUR_UPDATE } from 'state/action-types';
 
 describe( 'reducer', () => {
 	describe( '#guidedTour()', () => {
 		test( 'should default to an empty object', () => {
 			const state = guidedTour( undefined, {} );
+			expect( state ).toEqual( {} );
+		} );
 
-			expect( state ).to.be.empty;
+		test( 'should return expected state', () => {
+			const state = guidedTour(
+				{},
+				{
+					type: GUIDED_TOUR_UPDATE,
+					tour: 'testTour',
+					stepName: 'testStepName',
+					isPaused: false,
+				}
+			);
+			expect( state ).toEqual( {
+				tour: 'testTour',
+				stepName: 'testStepName',
+				isPaused: false,
+			} );
 		} );
 	} );
 } );
