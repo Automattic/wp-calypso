7df93ed0fc2c54d959e66ec6b3b2cb363a3e28a9
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-10-25 11:46:20 -0700

    Use a new index.json preset format to generate individual blocks

--- bin/sdk/gutenberg.js
+++ bin/sdk/gutenberg.js
@@ -4,21 +4,32 @@
  */
 const fs = require( 'fs' );
 const path = require( 'path' );
+const reduce = require( 'lodash/reduce' );
+const pickBy = require( 'lodash/pickBy' );
 
 exports.config = ( { argv: { inputDir, outputDir }, getBaseConfig } ) => {
 	const baseConfig = getBaseConfig( {
 		cssFilename: '[name].css',
 		externalizeWordPressPackages: true,
 	} );
+
+	const presetPath = path.join( inputDir, 'index.json' );
+	const utilsPath = path.join( inputDir, 'utils' );
+
 	const editorScript = path.join( inputDir, 'editor.js' );
 	const viewScript = path.join( inputDir, 'view.js' );
 
+	let viewBlocksPoints;
+	if ( fs.existsSync( presetPath ) ) {
+		const preset = require( presetPath );
+		viewBlocksPoints = pickBy( reduce( preset, (obj, block) => ( { [ block + '/view'] : path.join( inputDir, '../../'+ block +'/view.js' ) } )  ), fs.existsSync );
+	}
 	return {
 		...baseConfig,
 		entry: {
 			editor: editorScript,
-			...( fs.existsSync( viewScript ) ? { view: viewScript } : {} ),
-			...( fs.existsSync( path.join( inputDir, 'view/tiled-gallery.js' ) ) ? { 'tiled-gallery/view' : path.join( inputDir, 'view/tiled-gallery.js' ) } : {} ),
+			...(fs.existsSync( viewScript ) ? { view: viewScript } : {} ),
+			...viewBlocksPoints,
 		},
 		output: {
 			path: outputDir || path.join( inputDir, 'build' ),
--- client/gutenberg/extensions/presets/jetpack/view/tiled-gallery.js
+++
@@ -1,2 +0,0 @@
-import './../utils/public-path';
-import 'gutenberg/extensions/tiled-gallery/view';
