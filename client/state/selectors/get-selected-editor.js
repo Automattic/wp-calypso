a4fb92ce810061e9187f495ac421e68af2f8e6df
Kirk Wight <kwight@kwight.ca>
2018-10-25 09:13:23 -0700

    Explain why we might return `null` from our selector.

--- client/state/selectors/get-selected-editor.js
+++ client/state/selectors/get-selected-editor.js
@@ -13,7 +13,7 @@ import { get } from 'lodash';
  * Returns the editor of the selected site
  * @param {Object} state Global state tree
  * @param {Number} siteId Site ID
- * @return {String} "gutenberg" or "classic" or null
+ * @return {String} "gutenberg" or "classic", or null if we have no data yet
  */
 export const getSelectedEditor = ( state, siteId ) =>
 	get( state, [ 'selectedEditor', siteId ], null );
