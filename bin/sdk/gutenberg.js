3bc62c54b8f9648ce35c3720dac280ab57b1e793
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-10-25 12:49:49 -0700

    Remove the need for view.js and edtor.js
    We are not combining all the files based on the index.json file.

--- bin/sdk/gutenberg.js
+++ bin/sdk/gutenberg.js
@@ -6,6 +6,7 @@ const fs = require( 'fs' );
 const path = require( 'path' );
 const reduce = require( 'lodash/reduce' );
 const pickBy = require( 'lodash/pickBy' );
+const isEmpty = require( 'lodash/isEmpty' );
 
 exports.config = ( { argv: { inputDir, outputDir }, getBaseConfig } ) => {
 	const baseConfig = getBaseConfig( {
@@ -16,19 +17,37 @@ exports.config = ( { argv: { inputDir, outputDir }, getBaseConfig } ) => {
 	const presetPath = path.join( inputDir, 'index.json' );
 	const utilsPath = path.join( inputDir, 'utils' );
 
-	const editorScript = path.join( inputDir, 'editor.js' );
-	const viewScript = path.join( inputDir, 'view.js' );
+
 
 	let viewBlocksPoints;
+	let viewScriptEntry;
 	if ( fs.existsSync( presetPath ) ) {
+
 		const preset = require( presetPath );
 		viewBlocksPoints = pickBy( reduce( preset, (obj, block) => ( { [ block + '/view'] : path.join( inputDir, '../../'+ block +'/view.js' ) } )  ), fs.existsSync );
+
+		utilScripts = fs.readdirSync( utilsPath ).map( utilFile => path.join( utilsPath, utilFile ) ).filter( fs.existsSync );
+		editorScripts = preset.map( block => path.join( inputDir, '../../'+ block +'/editor.js' ) ).filter( fs.existsSync );
+		viewScripts = preset.map( block => path.join( inputDir, '../../'+ block +'/view.js' ) ).filter( fs.existsSync );
+		// Combines all the different blocks into one
+		editorScript = utilScripts.concat( editorScripts ).concat( viewScripts );
+
+		// create a viewScrpript
+		if ( ! isEmpty( viewScripts ) ) {
+			viewScriptEntry = { view: viewScripts };
+		}
+
+	} else {
+		const editorScript = path.join( inputDir, 'editor.js' );
+		const viewScript = path.join( inputDir, 'view.js' );
+		viewScriptEntry = (fs.existsSync( viewScript ) ? { view: viewScript } : {} )
 	}
+
 	return {
 		...baseConfig,
 		entry: {
 			editor: editorScript,
-			...(fs.existsSync( viewScript ) ? { view: viewScript } : {} ),
+			...viewScriptEntry,
 			...viewBlocksPoints,
 		},
 		output: {
