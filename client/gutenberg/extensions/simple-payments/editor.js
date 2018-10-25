42f8cee7d5ae44d9913be14d64846173e69da0c9
Mikael Korpela <mikael@ihminen.org>
2018-10-25 19:10:55 +0300

    Remove `jetpack` textdomain

--- client/gutenberg/extensions/simple-payments/editor.js
+++ client/gutenberg/extensions/simple-payments/editor.js
@@ -24,7 +24,7 @@ registerBlockType( 'jetpack/simple-payments', {
 
 	category: 'jetpack',
 
-	keywords: [ __( 'simple payments', 'jetpack' ), __( 'PayPal', 'jetpack' ) ],
+	keywords: [ __( 'simple payments' ), __( 'PayPal' ) ],
 
 	attributes: {
 		cta: {
