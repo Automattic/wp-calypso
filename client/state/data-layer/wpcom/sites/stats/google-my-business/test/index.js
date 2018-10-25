4d73e0d73dd14db8c6f92c65818ce4fc0379fedf
Markus Dobmann <markus@mpnjs.com>
2018-10-25 15:16:48 +0200

    Use dispatchRequestEx for google my business stats

--- client/state/data-layer/wpcom/sites/stats/google-my-business/test/index.js
+++ client/state/data-layer/wpcom/sites/stats/google-my-business/test/index.js
@@ -4,27 +4,24 @@
  * External dependencies
  */
 import { expect } from 'chai';
-import sinon from 'sinon';
 
 /**
  * Internal dependencies
  */
-import { fetchStats, receiveStats } from '..';
+import { fetchStats, receiveStats, fromApi } from '..';
 import { receiveGoogleMyBusinessStats } from 'state/google-my-business/actions';
 import { http } from 'state/data-layer/wpcom-http/actions';
 
 describe( '#fetchStats', () => {
 	test( 'should dispatch HTTP request to Google My Business stats endpoint', () => {
-		const dispatch = sinon.spy();
 		const action = {
 			siteId: 12345,
 			statType: 'queries',
 		};
 
-		fetchStats( { dispatch }, action );
+		const result = fetchStats( action );
 
-		expect( dispatch ).to.have.been.calledOnce;
-		expect( dispatch ).to.have.been.calledWith(
+		expect( result ).to.eql(
 			http(
 				{
 					path: '/sites/12345/stats/google-my-business/queries',
@@ -42,7 +39,6 @@ describe( '#fetchStats', () => {
 
 describe( '#receiveStats', () => {
 	test( 'should dispatch receive stats action', () => {
-		const dispatch = sinon.spy();
 		const action = {
 			interval: 'month',
 			aggregation: 'total',
@@ -50,40 +46,41 @@ describe( '#receiveStats', () => {
 			statType: 'views',
 		};
 
-		receiveStats( { dispatch }, action, {
-			time_zone: 'Europe/London',
-			metric_values: [
-				{
-					metric: 'QUERIES_DIRECT',
-					total_value: {
-						time_dimension: {
-							timeRange: {
-								endTime: '2018-04-19T23:59:59.900Z',
-								startTime: '2018-04-13T00:00:00Z',
+		const result = fromApi(
+			receiveStats( action, {
+				time_zone: 'Europe/London',
+				metric_values: [
+					{
+						metric: 'QUERIES_DIRECT',
+						total_value: {
+							time_dimension: {
+								timeRange: {
+									endTime: '2018-04-19T23:59:59.900Z',
+									startTime: '2018-04-13T00:00:00Z',
+								},
 							},
+							metric_option: 'AGGREGATED_TOTAL',
+							value: 0,
 						},
-						metric_option: 'AGGREGATED_TOTAL',
-						value: 0,
 					},
-				},
-				{
-					metric: 'QUERIES_INDIRECT',
-					total_value: {
-						time_dimension: {
-							timeRange: {
-								endTime: '2018-04-19T23:59:59.900Z',
-								startTime: '2018-04-13T00:00:00Z',
+					{
+						metric: 'QUERIES_INDIRECT',
+						total_value: {
+							time_dimension: {
+								timeRange: {
+									endTime: '2018-04-19T23:59:59.900Z',
+									startTime: '2018-04-13T00:00:00Z',
+								},
 							},
+							metric_option: 'AGGREGATED_TOTAL',
+							value: 1,
 						},
-						metric_option: 'AGGREGATED_TOTAL',
-						value: 1,
 					},
-				},
-			],
-		} );
+				],
+			} )
+		);
 
-		expect( dispatch ).to.have.been.calledOnce;
-		expect( dispatch ).to.have.been.calledWith(
+		expect( result ).to.eql(
 			receiveGoogleMyBusinessStats(
 				action.siteId,
 				action.statType,
@@ -125,7 +122,6 @@ describe( '#receiveStats', () => {
 	} );
 
 	test( 'should transform data snake_case to camelCase', () => {
-		const dispatch = sinon.spy();
 		const action = {
 			interval: 'quarter',
 			aggregation: 'daily',
@@ -133,10 +129,9 @@ describe( '#receiveStats', () => {
 			statType: 'actions',
 		};
 
-		receiveStats( { dispatch }, action, { hello_world: 'hello' } );
+		const result = fromApi( receiveStats( action, { hello_world: 'hello' } ) );
 
-		expect( dispatch ).to.have.been.calledOnce;
-		expect( dispatch ).to.have.been.calledWith(
+		expect( result ).to.eql(
 			receiveGoogleMyBusinessStats(
 				action.siteId,
 				action.statType,
