b62576d21b97d80f896060fd4b3f0f21cf0284fa
Jeremy Massel <jeremy.massel@gmail.com>
2018-10-26 13:35:27 -0600

    Add support for using the 2FA phone number, too
    Now automatically pulls in the user’s 2FA number, if it’s specified

--- client/me/get-apps/mobile-download-card.jsx
+++ client/me/get-apps/mobile-download-card.jsx
@@ -9,7 +9,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { localize } from 'i18n-calypso';
-import { isEmpty } from 'lodash';
 
 /**
  * Internal dependencies
@@ -19,16 +18,18 @@ import Card from 'components/card';
 import Button from 'components/button';
 import QuerySmsCountries from 'components/data/query-countries/sms';
 import QueryAccountRecoverySettings from 'components/data/query-account-recovery-settings';
+import QueryUserSettings from 'components/data/query-user-settings';
 import FormPhoneInput from 'components/forms/form-phone-input';
 import getCountries from 'state/selectors/get-countries';
 import { successNotice, errorNotice } from 'state/notices/actions';
 import {
 	getAccountRecoveryPhone,
 	isAccountRecoverySettingsReady,
-	isAccountRecoveryPhoneValidated,
 } from 'state/account-recovery/settings/selectors';
-
+import getUserSettings from 'state/selectors/get-user-settings';
+import hasUserSettings from 'state/selectors/has-user-settings';
 import { sendSMS } from 'state/mobile-download-sms/actions';
+import phoneValidation from 'lib/phone-validation';
 import config from 'config';
 
 class MobileDownloadCard extends React.Component {
@@ -43,6 +44,7 @@ class MobileDownloadCard extends React.Component {
 			number: PropTypes.string,
 			numberFull: PropTypes.string,
 		} ),
+		// userSettings: PropTypes.array.isRequired,
 		hasLoadedStoredPhone: PropTypes.bool,
 		successNotice: PropTypes.func,
 		errorNotice: PropTypes.func,
@@ -52,15 +54,85 @@ class MobileDownloadCard extends React.Component {
 		phoneNumber: null,
 	};
 
+	getPreferredNumber = ( data_has_been_fetched = true ) => {
+		const noPreferredNumber = {
+			countryCode: null,
+			number: null,
+			isValid: false,
+		};
+
+		if ( ! data_has_been_fetched ) {
+			return noPreferredNumber;
+		}
+
+		const tfa_number =
+			this.props.userSettings != null ? this.props.userSettings.two_step_sms_phone_number : null;
+
+		const tfa_country_code =
+			this.props.userSettings != null ? this.props.userSettings.two_step_sms_country : null;
+
+		const tfa_sms_enabled =
+			this.props.userSettings != null ? this.props.userSettings.two_step_sms_enabled : null;
+
+		const account_recovery_number = this.props.accountRecoveryPhone;
+
+		// If the user has typed their own phone number,
+		// that's the most preferred.
+		if ( this.state.phoneNumber !== null ) {
+			return this.state.phoneNumber;
+		}
+
+		// We proritize TFA over the account recovery number.
+		// Also, if we have their TFA phone number, but they're not using
+		// it for TFA, we won't show it to them, to avoid creeping them out.
+		if ( tfa_number !== null && tfa_country_code !== null && tfa_sms_enabled ) {
+			const country_code = this.numericCountryCodeForCountryCode( tfa_country_code );
+			const full_number = country_code + tfa_number;
+
+			return {
+				countryCode: tfa_country_code,
+				countryNumericCode: country_code,
+				number: tfa_number,
+				numberFull: full_number,
+				isValid: this.phoneNumberIsValid( full_number ),
+			};
+		}
+
+		// Account recovery number already has the keys formatted in the
+		// way we want, so we can just return it directly.
+		if ( account_recovery_number !== null ) {
+			const isValid = this.phoneNumberIsValid( account_recovery_number.numberFull );
+			account_recovery_number.isValid = isValid;
+
+			return account_recovery_number;
+		}
+
+		// Fallback if we didn't match anything
+		return noPreferredNumber;
+	};
+
+	phoneNumberIsValid( number ) {
+		return ! phoneValidation( number ).error;
+	}
+
+	numericCountryCodeForCountryCode( code ) {
+		const element = this.props.countriesList.find( item => {
+			return item.code === code;
+		} );
+
+		if ( element !== undefined ) {
+			return element.numeric_code;
+		}
+
+		return null;
+	}
+
 	render() {
 		const translate = this.props.translate;
-		const hasStoredPhone = ! isEmpty( this.props.storedPhone );
-		const hasLoadedStoredPhone = this.props.hasLoadedStoredPhone;
 
-		const phoneNumberIsValid =
-			this.state.phoneNumber == null ? hasStoredPhone : this.state.phoneNumberIsValid;
-
-		const mobile_sms_is_enabled = config.isEnabled( 'get-apps-sms' );
+		const has_all_data = this.props.hasUserSettings && this.props.hasLoadedAccountRecoveryPhone;
+		const { countryCode, number, isValid } = this.getPreferredNumber( has_all_data );
+		const feature_is_enabled = config.isEnabled( 'get-apps-sms' );
 
 		return (
 			<Card className="get-apps__mobile">
@@ -86,14 +158,15 @@ class MobileDownloadCard extends React.Component {
 						/>
 					</div>
 				</div>
-				{ mobile_sms_is_enabled ? (
+
+				{ feature_is_enabled ? (
 					<div className="get-apps__sms-subpanel">
 						<div className="get-apps__sms-field-wrapper">
-							{ hasLoadedStoredPhone ? (
+							{ has_all_data ? (
 								<FormPhoneInput
 									countriesList={ this.props.countriesList }
-									initialCountryCode={ hasStoredPhone ? this.props.storedPhone.countryCode : null }
-									initialPhoneNumber={ hasStoredPhone ? this.props.storedPhone.number : null }
+									initialCountryCode={ countryCode }
+									initialPhoneNumber={ number }
 									phoneInputProps={ {
 										onKeyUp: this.onKeyUp,
 									} }
@@ -103,6 +176,7 @@ class MobileDownloadCard extends React.Component {
 								<>
 									<QuerySmsCountries />
 									<QueryAccountRecoverySettings />
+									<QueryUserSettings />
 
 									<FormPhoneInput countriesList={ this.props.countriesList } isDisabled={ true } />
 								</>
@@ -114,7 +188,7 @@ class MobileDownloadCard extends React.Component {
 							<Button
 								className="get-apps__sms-button"
 								onClick={ this.onSubmit }
-								disabled={ ! phoneNumberIsValid }
+								disabled={ ! isValid }
 							>
 								{ translate( 'Text me a link' ) }
 							</Button>
@@ -130,7 +204,6 @@ class MobileDownloadCard extends React.Component {
 	onChange = phoneNumber => {
 		this.setState( {
 			phoneNumber,
-			phoneNumberIsValid: phoneNumber.isValid,
 		} );
 	};
 
@@ -143,12 +216,7 @@ class MobileDownloadCard extends React.Component {
 	onSubmit = () => {
 		const { translate } = this.props;
 
-		const phoneNumber =
-			this.state.phoneNumber !== null
-				? // The user has typed their own phone number
-				  this.state.phoneNumber.phoneNumberFull
-				: // The user is sending a message to their stored number
-				  this.props.storedPhone.numberFull;
+		const phoneNumber = this.getPreferredNumber();
 
 		sendSMS( phoneNumber )
 			.then( (/* response */) => {
@@ -166,9 +234,11 @@ export default connect(
 	state => {
 		return {
 			countriesList: getCountries( state, 'sms' ),
-			storedPhone: getAccountRecoveryPhone( state ),
-			hasLoadedStoredPhone: isAccountRecoverySettingsReady( state ),
-			storedPhoneIsValid: isAccountRecoveryPhoneValidated( state ),
+			accountRecoveryPhone: getAccountRecoveryPhone( state ),
+			hasLoadedAccountRecoveryPhone: isAccountRecoverySettingsReady( state ),
+
+			userSettings: getUserSettings( state ),
+			hasUserSettings: hasUserSettings( state ),
 		};
 	},
 	dispatch => bindActionCreators( { successNotice, errorNotice }, dispatch )
