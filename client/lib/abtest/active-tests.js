36512f7038cc98066cd7ac8986c6aebec5699805
James Tien <southp0105@gmail.com>
2018-10-26 10:02:19 +0800

    Take 2: Create a AB-test branch for the improved onboarding sign-up flow (#28046)
    * Add a new abtest: `improvedSignupFlow`
    
    Duplicate the current `main` flow as the `onboarding` one.
    
    Use `abtest()` to alter the main signup flow between the original `main`
    and the new `onboarding`.
    
    Open the signup flow test for all locales.
    
    * Alternate the default flow according to `improvedOnboarding` ab-test
    value.

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -1,5 +1,14 @@
 /** @format */
 export default {
+	improvedOnboarding: {
+		datestamp: '20181023',
+		variations: {
+			main: 90,
+			onboarding: 10,
+		},
+		defaultVariation: 'main',
+		localeTargets: 'any',
+	},
 	cartNudgeUpdateToPremium: {
 		datestamp: '20180917',
 		variations: {
