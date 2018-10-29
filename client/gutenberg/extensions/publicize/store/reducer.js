ff6ac7ba58b07ac7906e5d7683577182a45af3b0
Marin Atanasov <tyxla@abv.bg>
2018-10-29 13:10:32 +0200

    Split reducer to subreducers

--- client/gutenberg/extensions/publicize/store/reducer.js
+++ client/gutenberg/extensions/publicize/store/reducer.js
@@ -1,42 +1,48 @@
 /**
- * Default Publicize reducer state
+ * External dependencies
  */
-const DEFAULT_STATE = {
-	connections: [],
-	services: null,
-};
+import { combineReducers } from '@wordpress/data';
 
 /**
- * Reducer managing Publicize extension state.
+ * Reducer managing Publicize extension connections.
  *
  * @param {Object} state  Current state.
  * @param {Object} action Dispatched action.
  *
  * @return {Object} Updated state.
  */
-const reducer = ( state = DEFAULT_STATE, action ) => {
+export function connections( state = [], action ) {
 	switch ( action.type ) {
 		case 'SET_CONNECTIONS':
 			return {
 				...state,
-				connections: {
-					...state.connections,
-					[ action.postId ]: action.connections,
-				},
+				[ action.postId ]: action.connections,
 			};
 		case 'REFRESH_CONNECTIONS':
-			return {
-				...state,
-				connections: DEFAULT_STATE.connections,
-			};
-		case 'SET_SERVICES':
-			return {
-				...state,
-				services: action.services,
-			};
+			return [];
 	}
 
 	return state;
 }
 
-export default reducer;
+/**
+ * Reducer managing Publicize extension services available for connection.
+ *
+ * @param {Object} state  Current state.
+ * @param {Object} action Dispatched action.
+ *
+ * @return {Object} Updated state.
+ */
+export function services( state = null, action ) {
+	switch ( action.type ) {
+		case 'SET_SERVICES':
+			return action.services;
+	}
+
+	return state;
+}
+
+export default combineReducers( {
+	connections,
+	services,
+} );
