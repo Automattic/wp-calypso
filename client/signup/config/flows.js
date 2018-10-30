802dc0f23fb3c469bf748353fb2a5345ee5d8d88
ramonjd <ramonjd@gmail.com>
2018-10-30 13:02:21 +1100

    Disable the new signup flow in the desktop app for now until we can perform further testing.

--- client/signup/config/flows.js
+++ client/signup/config/flows.js
@@ -130,7 +130,9 @@ const Flows = {
 	filterFlowName,
 	filterDestination,
 
-	defaultFlowName: abtest( 'improvedOnboarding' ) || 'main',
+	defaultFlowName: config.isEnabled( 'signup/atomic-store-flow' )
+		? abtest( 'improvedOnboarding' )
+		: 'main',
 	resumingFlow: false,
 
 	/**
