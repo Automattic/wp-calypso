b510dea1c05e5490aacf1735c4af3888befaef4c
Miguel Lezama <miguel@automattic.com>
2018-10-30 12:59:18 -0300

    don't register if jetpackData isn't available

--- client/gutenberg/extensions/presets/jetpack/utils/jetpack-block-type.js
+++ client/gutenberg/extensions/presets/jetpack/utils/jetpack-block-type.js
@@ -2,7 +2,9 @@
  * External dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-import { has, get, pickBy } from 'lodash';
+import has from 'lodash/has';
+import get from 'lodash/get';
+import pickBy from 'lodash/pickBy';
 
 /**
  * Where Jetpack data can be found in WP Admin
@@ -13,25 +15,22 @@ export default class JetpackBlockType {
 	constructor( name, config ) {
 		this.name = name;
 		this.config = config;
-		// TODO: how do we access Jetpack Data from calypso?
 		this.jetpackData = get(
 			'object' === typeof window ? window : null,
 			JETPACK_DATA_PATH,
 			null
 		);
-		return this;
 	}
 
 	hasRequiredModule = () => {
-		const activeModules = pickBy( get( this.jetpackData, 'getModules' ), ( module ) => { return module.activated && module.override !== 'inactive' } );
+		const activeModules = pickBy( get( this.jetpackData, 'getModules' ), module => module.activated && module.override !== 'inactive' );
 		return has( activeModules, this.name )
 	};
 
 	register() {
-		if ( this.jetpackData && ! this.hasRequiredModule() ) {
-			return this;
+		if ( ! this.jetpackData || ! this.hasRequiredModule() ) {
+			return;
 		}
-
 		registerBlockType( 'jetpack/' + this.name, this.config );
 	}
 }
