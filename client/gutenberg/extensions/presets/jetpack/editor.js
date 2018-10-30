8fea2b693b45854db212029c1ad97b9c009da4cf
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-10-29 20:09:21 -0700

    Fix editor.js includes

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -3,8 +3,8 @@
 /**
  * Internal dependencies
  */
-import './utils/public-path';
-import './utils/block-category'; // Register the Jetpack category
+import './shared/public-path';
+import './editor-shared/block-category'; // Register the Jetpack category
 import 'gutenberg/extensions/markdown/editor';
 import 'gutenberg/extensions/publicize/editor';
 import 'gutenberg/extensions/related-posts/editor';
