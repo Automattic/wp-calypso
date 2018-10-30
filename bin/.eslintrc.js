f80e1889cf7de63401d1ddad54d44cde76dff5f1
Mikael Korpela <mikael@ihminen.org>
2018-10-30 13:26:51 +0200

    Bin: enable eslint for files in `/bin/`
    We relax rules a bit to make more sense for CLI scripts.

---
+++ bin/.eslintrc.js
@@ -0,0 +1,12 @@
+/** @format */
+
+module.exports = {
+	extends: '../.eslintrc.js',
+	rules: {
+		'import/no-extraneous-dependencies': 0,
+		'import/no-nodejs-modules': 0,
+		'no-console': 0,
+		'no-process-exit': 0,
+		'valid-jsdoc': 0,
+	},
+};
