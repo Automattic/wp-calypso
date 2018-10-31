c2009e1ad0a80bb5e5830286aa1daf0da32a3bd2
Jefferson Rabb <jeff@atavist.net>
2018-10-31 16:57:18 -0400

    Switch to Mapbox from Google Maps.

--- client/gutenberg/extensions/map-block/add-point/index.js
+++ client/gutenberg/extensions/map-block/add-point/index.js
@@ -16,7 +16,7 @@ import LocationSearch from '../location-search';
 import './style.scss';
 export class AddPoint extends Component {
 	render() {
-		const { onClose, onAddPoint } = this.props;
+		const { onClose, onAddPoint, api_key } = this.props;
 		return (
 			<Button className="component__add-point">
 				{ __( 'Add marker', 'jetpack' ) }
@@ -24,7 +24,11 @@ export class AddPoint extends Component {
 					<Button className="component__add-point__close" onClick={ onClose }>
 						<Dashicon icon="no" />
 					</Button>
-					<LocationSearch onAddPoint={ onAddPoint } label={ __( 'Add a location', 'jetpack' ) } />
+					<LocationSearch
+						onAddPoint={ onAddPoint }
+						label={ __( 'Add a location', 'jetpack' ) }
+						api_key={ api_key }
+					/>
 				</Popover>
 			</Button>
 		);
---
+++ client/gutenberg/extensions/map-block/mapbox-map-formatter/index.js
@@ -0,0 +1,25 @@
+/** @format */
+
+/* TODO: Replace with styles created in A8C-owned account */
+export function mapboxMapFormatter( map_style, map_details ) {
+	const style_urls = {
+		default: {
+			details: 'mapbox://styles/rabberson/cjnxjme2m054v2rs2gf3nggx6',
+			no_details: 'mapbox://styles/rabberson/cjnxl6xqp55kt2sk55ivay8r0',
+		},
+		black_and_white: {
+			details: 'mapbox://styles/rabberson/cjnxjofik08ax2rmx47rfzhji',
+			no_details: 'mapbox://styles/rabberson/cjnxjvlk108kq2slmjnd9beni',
+		},
+		satellite: {
+			details: 'mapbox://styles/mapbox/satellite-streets-v10',
+			no_details: 'mapbox://styles/mapbox/satellite-v9',
+		},
+		terrain: {
+			details: 'mapbox://styles/rabberson/cjnxl0upd52ik2srtr0zh216f',
+			no_details: 'mapbox://styles/rabberson/cjnxl28cc54rl2roaamgb621y',
+		},
+	};
+	const style_url = style_urls[ map_style ][ map_details ? 'details' : 'no_details' ];
+	return style_url;
+}
