48ca4181378b180ed8c565469f3f21947a970b9d
Markus Dobmann <markus@mpnjs.com>
2018-10-31 10:40:03 +0100

    Use dispatchRequestEx for requestFollow (#27919)
    * Use dispatchRequestEx for read following mine new
    
    * tests: drop chai in favor of jest

--- client/state/data-layer/wpcom/read/following/mine/new/index.js
+++ client/state/data-layer/wpcom/read/following/mine/new/index.js
@@ -3,13 +3,14 @@
  * External Dependencies
  */
 import { translate } from 'i18n-calypso';
+import { get } from 'lodash';
 
 /**
  * Internal Dependencies
  */
 import config from 'config';
 import { READER_FOLLOW } from 'state/action-types';
-import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
 import { http } from 'state/data-layer/wpcom-http/actions';
 import { errorNotice } from 'state/notices/actions';
 import { follow, unfollow, recordFollowError } from 'state/reader/follows/actions';
@@ -18,13 +19,11 @@ import { bypassDataLayer } from 'state/data-layer/utils';
 
 import { registerHandlers } from 'state/data-layer/handler-registry';
 
-export function requestFollow( { dispatch }, action ) {
-	const {
-		payload: { feedUrl },
-	} = action;
+export function requestFollow( action ) {
+	const feedUrl = get( action, 'payload.feedUrl' );
 
-	dispatch(
-		http( {
+	return http(
+		{
 			method: 'POST',
 			path: '/read/following/mine/new',
 			apiVersion: '1.1',
@@ -32,38 +31,40 @@ export function requestFollow( { dispatch }, action ) {
 				url: feedUrl,
 				source: config( 'readerFollowingSource' ),
 			},
-			onSuccess: action,
-			onFailure: action,
-		} )
+		},
+		action
 	);
 }
 
-export function receiveFollow( store, action, response ) {
+export function receiveFollow( action, response ) {
 	if ( response && response.subscribed ) {
 		const subscription = subscriptionFromApi( response.subscription );
-		store.dispatch( bypassDataLayer( follow( action.payload.feedUrl, subscription ) ) );
-	} else {
-		followError( store, action, response );
+		return bypassDataLayer( follow( action.payload.feedUrl, subscription ) );
 	}
+	return followError( action, response );
 }
 
-export function followError( { dispatch }, action, response ) {
-	dispatch(
+export function followError( action, response ) {
+	const actions = [
 		errorNotice(
 			translate( 'Sorry, there was a problem following %(url)s. Please try again.', {
 				args: { url: action.payload.feedUrl },
 			} ),
 			{ duration: 5000 }
-		)
-	);
+		),
+	];
 
 	if ( response && response.info ) {
-		dispatch( recordFollowError( action.payload.feedUrl, response.info ) );
+		actions.push( recordFollowError( action.payload.feedUrl, response.info ) );
 	}
 
-	dispatch( bypassDataLayer( unfollow( action.payload.feedUrl ) ) );
+	actions.push( bypassDataLayer( unfollow( action.payload.feedUrl ) ) );
+
+	return actions;
 }
 
 registerHandlers( 'state/data-layer/wpcom/read/following/mine/new/index.js', {
-	[ READER_FOLLOW ]: [ dispatchRequest( requestFollow, receiveFollow, followError ) ],
+	[ READER_FOLLOW ]: [
+		dispatchRequestEx( { fetch: requestFollow, onSuccess: receiveFollow, onError: followError } ),
+	],
 } );
