06f388f47304ad1c63eaf58430265a1fd8ae7432
Marin Atanasov <tyxla@abv.bg>
2018-10-31 13:52:31 +0200

    Gutenberg: Gracefully handle any Publicize API request failures

--- client/gutenberg/extensions/publicize/connection-verify.jsx
+++ client/gutenberg/extensions/publicize/connection-verify.jsx
@@ -47,14 +47,23 @@ class PublicizeConnectionVerify extends Component {
 	};
 
 	/**
+	 * Callback for connection test request failure
+	 */
+	connectionTestRequestFailed = () => {
+		this.setState( {
+			isLoading: false,
+		} );
+	};
+
+	/**
 	 * Starts request to check connections
 	 *
 	 * Checks connections with using the '/publicize/connections' endpoint
 	 */
 	connectionTestStart = () => {
-		apiFetch( {
-			path: '/jetpack/v4/publicize/connections',
-		} ).then( () => this.connectionTestComplete );
+		apiFetch( { path: '/jetpack/v4/publicize/connections' } )
+			.then( () => this.connectionTestComplete )
+			.catch( () => this.connectionTestRequestFailed );
 	};
 
 	/**
