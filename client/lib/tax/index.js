e1fefd091c4a786159949c1c576af4b15ba850c0
Julian de Bhal <julian.debhal@automattic.com>
2018-10-31 10:12:01 +1000

    tidy up comments

--- client/lib/tax/index.js
+++ client/lib/tax/index.js
@@ -1,13 +1,5 @@
 /** @format */
 
-/**
- * External dependencies
- */
-
-/**
- * Internal dependencies
- */
-
 export function dummyTaxRate( postalCode, countryCode ) {
 	if ( countryCode !== 'US' ) {
 		return null;
