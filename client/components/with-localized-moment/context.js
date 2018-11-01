579e010c6563958a45318fd392bb1ccb3a1e7020
Ben Lowery <ben.lowery@automattic.com>
2018-11-01 12:23:30 -0400

    Pass the current lang slug to the child to break purity

--- client/components/with-localized-moment/context.js
+++ client/components/with-localized-moment/context.js
@@ -7,16 +7,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
+import debugFactory from 'debug';
 
 /**
  * Internal dependencies
  */
 import getCurrentLocaleSlug from 'state/selectors/get-current-locale-slug';
 
+const debug = debugFactory( 'calypso:with-localized-moment' );
+
 const { Provider, Consumer } = React.createContext( moment );
 
 class MomentProvider extends React.Component {
-	state = { moment };
+	state = { moment, locale: 'en' };
 
 	async checkAndLoad() {
 		const { currentLocale } = this.props;
@@ -27,11 +30,21 @@ class MomentProvider extends React.Component {
 		}
 
 		if ( currentLocale !== 'en' ) {
-			await import( /* webpackChunkName: "moment-locale-[request]", webpackInclude: /\.js$/ */ `moment/locale/${ currentLocale }` );
+			debug( 'Loading moment locale for %s', currentLocale );
+			try {
+				await import( /* webpackChunkName: "moment-locale-[request]", webpackInclude: /\.js$/ */ `moment/locale/${ currentLocale }` );
+			} catch ( e ) {
+				debug( 'Failed to load moment locale for %s', currentLocale );
+				return;
+			}
+			debug( 'Loaded moment locale for %s', currentLocale );
 		}
 
 		moment.locale( currentLocale );
-		this.setState( {} );
+		this.setState( {
+			moment,
+			locale: currentLocale,
+		} );
 	}
 
 	componentDidMount() {
--- client/components/with-localized-moment/index.js
+++ client/components/with-localized-moment/index.js
@@ -16,7 +16,7 @@ export default createHigherOrderComponent( Wrapped => {
 	return function WithLocalizedMoment( props ) {
 		return (
 			<MomentConsumer>
-				{ momentState => <Wrapped { ...props } moment={ momentState.moment } /> }
+				{ momentState => <Wrapped { ...props } { ...momentState } /> }
 			</MomentConsumer>
 		);
 	};
