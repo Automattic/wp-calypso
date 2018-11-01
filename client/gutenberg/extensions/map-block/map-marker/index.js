72d4906613b2854a117c11dda4dd44e90a60acd0
Jefferson Rabb <jeff@atavist.net>
2018-11-01 16:57:08 -0400

    Fix to the opacity of marker pins - they are now 90% opaque.

--- client/gutenberg/extensions/map-block/map-marker/index.js
+++ client/gutenberg/extensions/map-block/map-marker/index.js
@@ -49,7 +49,7 @@ export class MapMarker extends Component {
 			this.marker.getElement().addEventListener( 'click', handleClick );
 		}
 		el.innerHTML =
-			'<?xml version="1.0" encoding="UTF-8"?><svg version="1.1" viewBox="0 0 32 38" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g fill-rule="evenodd"><mask id="c" fill="white"><use xlink:href="#d"/></mask><path id="d" d="m16 38s16-11.308 16-22-7.1634-16-16-16-16 5.3076-16 16 16 22 16 22z" fill="' +
+			'<?xml version="1.0" encoding="UTF-8"?><svg version="1.1" viewBox="0 0 32 38" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g fill-rule="evenodd"><path id="d" d="m16 38s16-11.308 16-22-7.1634-16-16-16-16 5.3076-16 16 16 22 16 22z" fill="' +
 			marker_color +
 			'" mask="url(#c)"/></g></svg>';
 	}
