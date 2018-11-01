dca38cc17c4e694b8f0ed25654068de82b42675b
Ben Lowery <ben.lowery@automattic.com>
2018-11-01 12:54:58 -0400

    move to a functional component for FormattedDate

--- client/components/formatted-date/index.js
+++ client/components/formatted-date/index.js
@@ -4,7 +4,7 @@
  *
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 
 /**
  * Internal dependencies
@@ -12,20 +12,17 @@ import React, { Component } from 'react';
 import withMoment from 'components/with-localized-moment';
 import toCurrentLocale from './to-current-locale';
 
-class FormattedDate extends Component {
-	static displayName = 'FormattedDate';
-	render() {
-		let date = this.props.date;
-		const moment = this.props.moment;
-		if ( ! moment.isMoment( date ) ) {
-			// only make a new moment if we were passed something else
-			date = moment( date );
-		} else {
-			// make sure the date is in the current locale
-			date = toCurrentLocale( date );
-		}
-		return <time dateTime={ date.toISOString( true ) }>{ date.format( this.props.format ) }</time>;
+function FormattedDate( { date, moment, format } ) {
+	if ( ! moment.isMoment( date ) ) {
+		// only make a new moment if we were passed something else
+		date = moment( date );
+	} else {
+		// make sure the date is in the current locale
+		date = toCurrentLocale( date );
 	}
+	return <time dateTime={ date.toISOString( true ) }>{ date.format( format ) }</time>;
 }
+
+FormattedDate.displayName = 'FormattedDate';
 
 export default withMoment( FormattedDate );
