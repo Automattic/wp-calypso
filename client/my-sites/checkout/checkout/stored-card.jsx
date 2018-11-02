217d0debfd3e9ed50caa324fa8fe13a3a19cd373
Alister Scott <alister.scott@gmail.com>
2018-11-02 11:13:38 +1000

    Add support for 'american express' being returned as card type (#28229)
    Card type is being returned by the API as either 'american express' or 'amex' when we only expect 'amex'
    
    This transforms 'american express' into 'amex'
    
    Question: should we fix this on the API side?

--- client/my-sites/checkout/checkout/stored-card.jsx
+++ client/my-sites/checkout/checkout/stored-card.jsx
@@ -12,18 +12,20 @@ class StoredCard extends React.Component {
 	static displayName = 'StoredCard';
 
 	render() {
-		let card = this.props.card,
+		const card = this.props.card,
 			expirationDate = this.props.moment( card.expiry ).format( 'MM/YY' ),
-			cardClasses = 'stored-card ' + card.card_type.toLowerCase();
+			cardClasses = `stored-card ${
+				card.card_type === 'american express' ? 'amex' : card.card_type.toLowerCase()
+			}`;
 
 		return (
 			<div className={ cardClasses }>
-				<span className="stored-card__number">
+				<span className="checkout__stored-card-number">
 					{ card.card_type } ****
 					{ card.card }
 				</span>
-				<span className="stored-card__name">{ card.name }</span>
-				<span className="stored-card__expiration-date">
+				<span className="checkout__stored-card-name">{ card.name }</span>
+				<span className="checkout__stored-card-expiration-date">
 					{ this.props.translate( 'Expires %(date)s', {
 						args: { date: expirationDate },
 						context: 'date is of the form MM/YY',
