afc8a9f1d83456b527e81b12130ee731a79d07cf
Jarda Snajdr <jsnajdr@gmail.com>
2018-11-05 12:39:25 +0100

    Refactor sections route handler to support async init functions
    This patch adds support for async initialization functions for sections. If the
    function is async, we wait until it's complete and only then continue handling
    the route.
    
    The handler also gets a rewrite that seeks to avoid race conditions. If a section is
    already being loaded, don't start loading it again, but wait for the existing load to
    finish. The section initialization function shouldn't ever be called more than once.
    
    The async init of sections will be useful for dynamic reducers (although not strictly
    necessary) and gets us one small step closer towards making sections into powerful
    modules -- their initialization just got more flexible.

--- client/sections-middleware.js
+++ client/sections-middleware.js
@@ -18,61 +18,82 @@ import { receiveSections, load } from './sections-helper';
 import sections from './sections';
 receiveSections( sections );
 
-const _loadedSections = {};
-
-function activateSection( sectionDefinition, context, next ) {
-	const dispatch = context.store.dispatch;
-
+function activateSection( sectionDefinition, context ) {
 	controller.setSection( sectionDefinition )( context );
-	dispatch( { type: 'SECTION_SET', isLoading: false } );
-	dispatch( activateNextLayoutFocus() );
-	next();
+	context.store.dispatch( activateNextLayoutFocus() );
 }
 
+async function loadSection( context, sectionDefinition ) {
+	context.store.dispatch( { type: 'SECTION_SET', isLoading: true } );
+
+	// If the section chunk is not loaded within 400ms, report it to analytics
+	const loadReportTimeout = setTimeout( () => {
+		context.store.dispatch( bumpStat( 'calypso_chunk_waiting', sectionDefinition.name ) );
+	}, 400 );
+
+	try {
+		// load the section module, i.e., its webpack chunk
+		const requiredModule = await load( sectionDefinition.name, sectionDefinition.module );
+		// call the module initialization function (possibly async, registers page.js handlers etc.)
+		await requiredModule.default( controller.clientRouter );
+	} finally {
+		context.store.dispatch( { type: 'SECTION_SET', isLoading: false } );
+
+		// If the load was faster than the timeout, this will cancel the analytics reporting
+		clearTimeout( loadReportTimeout );
+	}
+}
+
+/**
+ * Cache of already loaded or loading section modules. Every section module is in one of
+ * three states regarding the cache:
+ * - no record in the cache: not loaded or not currently loading
+ * - record value is `true`: already loaded and initialized
+ * - record value is a `Promise`: is currently loading, the promise will fulfill when done.
+ *   Don't start a second load with `loadSection` but rather wait for the existing promise.
+ */
+const _loadedSections = {};
+
 function createPageDefinition( path, sectionDefinition ) {
+	// skip this section if it's not enabled in current enviroment
+	const { envId } = sectionDefinition;
+	if ( envId && ! envId.includes( config( 'env_id' ) ) ) {
+		return;
+	}
+
 	const pathRegex = pathToRegExp( path );
-
-	page( pathRegex, function( context, next ) {
-		const envId = sectionDefinition.envId;
-		const dispatch = context.store.dispatch;
-
-		if ( envId && envId.indexOf( config( 'env_id' ) ) === -1 ) {
-			return next();
-		}
-
-		if ( _loadedSections[ sectionDefinition.module ] === true ) {
-			return activateSection( sectionDefinition, context, next );
-		}
-
-		dispatch( { type: 'SECTION_SET', isLoading: true } );
-
-		// If the section chunk is not loaded within 400ms, report it to analytics
-		const loadReportTimeout = setTimeout(
-			() => dispatch( bumpStat( 'calypso_chunk_waiting', sectionDefinition.name ) ),
-			400
-		);
-
-		load( sectionDefinition.name, sectionDefinition.module )
-			.then( requiredModule => {
-				if ( ! _loadedSections[ sectionDefinition.module ] ) {
-					requiredModule.default( controller.clientRouter );
-					_loadedSections[ sectionDefinition.module ] = true;
+	page( pathRegex, async function( context, next ) {
+		try {
+			const loadedSection = _loadedSections[ sectionDefinition.module ];
+			if ( loadedSection ) {
+				// wait for the promise if loading, do nothing when already loaded
+				if ( loadedSection !== true ) {
+					await loadedSection;
 				}
-				return activateSection( sectionDefinition, context, next );
-			} )
-			.catch( error => {
-				console.error( error ); // eslint-disable-line
-				if ( ! LoadingError.isRetry() && process.env.NODE_ENV !== 'development' ) {
-					LoadingError.retry( sectionDefinition.name );
-				} else {
-					dispatch( { type: 'SECTION_SET', isLoading: false } );
-					LoadingError.show( context, sectionDefinition.name );
-				}
-			} )
-			.then( () => {
-				// If the load was faster than the timeout, this will cancel the analytics reporting
-				clearTimeout( loadReportTimeout );
-			} );
+			} else {
+				// start loading the section and record the `Promise` in a map
+				const loadingSection = loadSection( context, sectionDefinition );
+				_loadedSections[ sectionDefinition.module ] = loadingSection;
+
+				// wait until the section module is loaded and the set the map record to `true`
+				await loadingSection;
+				_loadedSections[ sectionDefinition.module ] = true;
+			}
+
+			// activate the section after ensuring it's fully loaded
+			activateSection( sectionDefinition, context );
+			next();
+		} catch ( error ) {
+			// delete the cache record on failure; next attempt to load will start from scratch
+			delete _loadedSections[ sectionDefinition.module ];
+
+			console.error( error ); // eslint-disable-line
+			if ( ! LoadingError.isRetry() && process.env.NODE_ENV !== 'development' ) {
+				LoadingError.retry( sectionDefinition.name );
+			} else {
+				LoadingError.show( context, sectionDefinition.name );
+			}
+		}
 	} );
 }
 
