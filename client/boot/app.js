042c8bed47741ef8218a0c093160afd0395abc07
Jarda Snajdr <jsnajdr@gmail.com>
2018-11-05 08:37:34 +0100

    Revert "Boot: Don't render empty Layout (#26944)" (#28273)
    This reverts commit 178da208837235fca81f5e7575745f3cffeb924c.

--- client/boot/app.js
+++ client/boot/app.js
@@ -33,7 +33,7 @@ const boot = currentUser => {
 		setupMiddlewares( currentUser, reduxStore );
 		invoke( project, 'setupMiddlewares', currentUser, reduxStore );
 		detectHistoryNavigation.start();
-		page.start( { click: false, decodeURLComponents: false } );
+		page.start( { decodeURLComponents: false } );
 	} );
 };
 
