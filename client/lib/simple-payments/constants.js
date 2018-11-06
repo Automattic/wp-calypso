52119e98d0d078630b20594b1203d2cf5b9e74a9
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2018-11-06 17:47:19 -0300

    Simple Payments: adds a little more context to the supported list of currencies on Simple Payments

--- client/lib/simple-payments/constants.js
+++ client/lib/simple-payments/constants.js
@@ -6,6 +6,12 @@ export const DEFAULT_CURRENCY = 'USD';
 // https://developer.paypal.com/docs/integration/direct/rest/currency-codes/
 // If this list changes, Simple Payments in Jetpack must be updated as well.
 // See https://github.com/Automattic/jetpack/blob/master/modules/simple-payments/simple-payments.php
+
+/**
+ * Indian Rupee not supported because at the time of the creation of this file
+ * because it's limited to in-country PayPal India accounts only.
+ * Discussion: https://github.com/Automattic/wp-calypso/pull/28236
+ */
 export const SUPPORTED_CURRENCY_LIST = [
 	DEFAULT_CURRENCY,
 	'EUR',
