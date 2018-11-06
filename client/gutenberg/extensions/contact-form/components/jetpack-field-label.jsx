dc45e3e30b3d9c79adbff08e5b52720a1fbaf332
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-11-06 15:09:10 -0500

    Refactor jetpack fieldf label
    This was done for the field form to be simpler
    as well as address
    - improved padding and remove borders of the label

--- client/gutenberg/extensions/contact-form/components/jetpack-field-label.jsx
+++ client/gutenberg/extensions/contact-form/components/jetpack-field-label.jsx
@@ -3,38 +3,27 @@
 /**
  * External dependencies
  */
+import { Fragment } from '@wordpress/element';
+import { PlainText } from '@wordpress/editor';
 
-import { Component, Fragment } from '@wordpress/element';
-
-
-class JetpackFieldLabel extends Component {
-	constructor( ...args ) {
-		super( ...args );
-		this.onChangeLabel = this.onChangeLabel.bind( this );
-	}
-
-	onChangeLabel( event ) {
-		this.props.setAttributes( { label: event.target.value } );
-	}
 /**
  * Internal dependencies
  */
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 
-	render() {
-		return (
-			<Fragment>
-				<input
-					type="text"
-					value={ this.props.label }
-					className="jetpack-field-label"
-					onChange={ this.onChangeLabel }
-				/>
-						placeholder={ __( 'Type label…' ) }
-				{ props.required && <span className="required">{ __( '(required)' ) }</span> }
-			</Fragment>
-		);
-	}
-}
+const JetpackFieldLabel = props => {
+	const { setAttributes } = props;
+	return (
+		<Fragment>
+			<PlainText
+				value={ props.label }
+				className="jetpack-field-label"
+				onChange={ label => setAttributes( { label } ) }
+				placeholder={ __( 'Type label…' ) }
+			/>
+			{ props.required && <span className="required">{ __( '(required)' ) }</span> }
+		</Fragment>
+	);
+};
 
 export default JetpackFieldLabel;
