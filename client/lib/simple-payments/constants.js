74af169a743d6262c4ab77903051f65856a0fcf1
Jon Surrell <jon.surrell@automattic.com>
2018-11-06 11:54:47 +0100

    Add comment about syncing currencies with Jetpack

--- client/lib/simple-payments/constants.js
+++ client/lib/simple-payments/constants.js
@@ -4,6 +4,8 @@ export const SIMPLE_PAYMENTS_PRODUCT_POST_TYPE = 'jp_pay_product';
 export const DEFAULT_CURRENCY = 'USD';
 
 // https://developer.paypal.com/docs/integration/direct/rest/currency-codes/
+// If this list changes, Simple Payments in Jetpack must be updated as well.
+// See https://github.com/Automattic/jetpack/blob/master/modules/simple-payments/simple-payments.php
 export const SUPPORTED_CURRENCY_LIST = [
 	DEFAULT_CURRENCY,
 	'EUR',
