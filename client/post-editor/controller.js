13c955ddadd52ff7e55d233e343fe1077201d228
mmtr <miguel.torres@automattic.com>
2018-11-07 10:49:47 +0100

    Import missing get from lodash

--- client/post-editor/controller.js
+++ client/post-editor/controller.js
@@ -10,7 +10,7 @@ import i18n from 'i18n-calypso';
 import page from 'page';
 import { stringify } from 'qs';
 import { isWebUri as isValidUrl } from 'valid-url';
-import { has, startsWith } from 'lodash';
+import { get, has, startsWith } from 'lodash';
 
 /**
  * Internal dependencies
