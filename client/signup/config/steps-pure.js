0879742258ca284dfba047e82fccb3db2a201441
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2018-11-07 18:23:19 +0530

    add site information to the config

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -132,11 +132,6 @@ export function generateSteps( {
 			},
 		},
 
-		'site-information': {
-			stepName: 'site-information',
-			providesDependencies: [ 'siteTitle', 'address', 'email', 'phone' ],
-		},
-
 		'site-title': {
 			stepName: 'site-title',
 			providesDependencies: [ 'siteTitle' ],
