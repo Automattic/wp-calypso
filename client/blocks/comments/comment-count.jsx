fd001af060417c7da1884afbb1e9ab8a3cfc631e
bluefuton <chris@bluefuton.com>
2018-11-08 10:33:22 +1300

    Move span out of translated phrase when there are comments

--- client/blocks/comments/comment-count.jsx
+++ client/blocks/comments/comment-count.jsx
@@ -14,18 +14,15 @@ import { localize } from 'i18n-calypso';
 const CommentCount = ( { count, translate } ) => {
 	let countPhrase;
 	if ( count > 0 ) {
-		countPhrase = translate(
-			'{{span}}%(commentCount)d comment{{/span}}',
-			'{{span}}%(commentCount)d comments{{/span}}',
-			{
-				count,
-				args: {
-					commentCount: count,
-				},
-				components: {
-					span: <span className="comments__comment-count-phrase" />,
-				},
-			}
+		countPhrase = (
+			<span className="comments__comment-count-phrase">
+				{ translate( '%(commentCount)d comment', '%(commentCount)d comments', {
+					count,
+					args: {
+						commentCount: count,
+					},
+				} ) }
+			</span>
 		);
 	} else {
 		countPhrase = translate( '{{span}}No comments{{/span}} - add the first!', {
