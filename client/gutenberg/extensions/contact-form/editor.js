dbd4a5c28179f436744dcd3dc74d6028bba66442
Michael Arestad <marestad@gmail.com>
2018-11-07 22:34:24 -0500

    Contact form: Updated labels * Checkbox Multiple is now Checkbox group * Updated prefilled label copy to be more intuitive

--- client/gutenberg/extensions/contact-form/editor.js
+++ client/gutenberg/extensions/contact-form/editor.js
@@ -344,12 +344,19 @@ registerBlockType(
 registerBlockType(
 	'jetpack/field-checkbox-multiple',
 	Object.assign( {}, FieldDefaults, {
-		title: __( 'Checkbox Multiple' ),
+		title: __( 'Checkbox group' ),
 		description: __( 'People love options. Add several checkbox items.' ),
 		icon: renderSVG(
 			<Path d="M13 7.5h5v2h-5zm0 7h5v2h-5zM19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM11 6H6v5h5V6zm-1 4H7V7h3v3zm1 3H6v5h5v-5zm-1 4H7v-3h3v3z" />
 		),
 		edit: editMultiField( 'checkbox' ),
+		attributes: {
+			...FieldDefaults.attributes,
+			label: {
+				type: 'string',
+				default: 'Choose several',
+			},
+		},
 	} )
 );
 
@@ -367,6 +374,13 @@ registerBlockType(
 			</Fragment>
 		),
 		edit: editMultiField( 'radio' ),
+		attributes: {
+			...FieldDefaults.attributes,
+			label: {
+				type: 'string',
+				default: 'Choose one',
+			},
+		},
 	} )
 );
 
