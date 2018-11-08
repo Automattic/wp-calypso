15ca370a6a5fb4e47dc0705580dd851c37a64cf1
Filipe Varela <keoshi@keoshi.com>
2018-11-08 14:28:43 +0000

    Simplify return statement

--- client/blocks/product-purchase-features-list/jetpack-site-accelerator.jsx
+++ client/blocks/product-purchase-features-list/jetpack-site-accelerator.jsx
@@ -12,19 +12,17 @@ import { localize } from 'i18n-calypso';
  */
 import PurchaseDetail from 'components/purchase-detail';
 
-export default localize( ( { selectedSite, translate } ) => {
-	return (
-		<div className="product-purchase-features-list__item">
-			<PurchaseDetail
-				icon={ <img alt="" src="/calypso/images/illustrations/jetpack-speed.svg" /> }
-				title={ translate( 'Site accelerator' ) }
-				description={ translate(
-					'Load pages faster, and serve your images and ' +
-						'static files from our global network of servers.'
-				) }
-				buttonText={ translate( 'View settings' ) }
-				href={ '/settings/writing/' + selectedSite.slug }
-			/>
-		</div>
-	);
-} );
+export default localize( ( { selectedSite, translate } ) => (
+	<div className="product-purchase-features-list__item">
+		<PurchaseDetail
+			icon={ <img alt="" src="/calypso/images/illustrations/jetpack-speed.svg" /> }
+			title={ translate( 'Site accelerator' ) }
+			description={ translate(
+				'Load pages faster, and serve your images and ' +
+					'static files from our global network of servers.'
+			) }
+			buttonText={ translate( 'View settings' ) }
+			href={ '/settings/writing/' + selectedSite.slug }
+		/>
+	</div>
+) );
