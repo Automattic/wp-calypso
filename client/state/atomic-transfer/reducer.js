573b286839213aaab0b4142ae3dab885572fbb9f
Jason Johnston <jasonjohnston76@gmail.com>
2018-11-08 21:56:37 -0800

    remove initalization bits
    we're not there yet ;)

--- client/state/atomic-transfer/reducer.js
+++ client/state/atomic-transfer/reducer.js
@@ -7,8 +7,6 @@ import { combineReducers, keyedReducer, withSchemaValidation } from 'state/utils
 import { transferStates } from './constants';
 import { atomicTransfer as schema } from './schema';
 import {
-	ATOMIC_TRANSFER_INITIATE_REQUEST as INITIATE,
-	ATOMIC_TRANSFER_INITIATE_REQUEST_FAILURE as INITIATE_FAILURE,
 	ATOMIC_TRANSFER_REQUEST as TRANSFER_REQUEST,
 	ATOMIC_TRANSFER_REQUEST_FAILURE as TRANSFER_REQUEST_FAILURE,
 	ATOMIC_TRANSFER_RECEIVE as TRANSFER_UPDATE,
@@ -17,10 +15,6 @@ import {
 
 export const status = ( state = null, action ) => {
 	switch ( action.type ) {
-		case INITIATE:
-			return transferStates.PENDING;
-		case INITIATE_FAILURE:
-			return transferStates.ERROR;
 		case SET_TRANSFER:
 			return action.status;
 		case TRANSFER_UPDATE:
