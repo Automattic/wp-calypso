0e35ddcced6546e29a3ee4dc5b26ee82777447a6
Rocco Tripaldi <tripaldir@gmail.com>
2018-11-09 15:20:21 -0500

    deconstruct this.props on required toggle component

--- client/gutenberg/extensions/contact-form/components/jetpack-field-required-toggle.jsx
+++ client/gutenberg/extensions/contact-form/components/jetpack-field-required-toggle.jsx
@@ -10,14 +10,8 @@ import { ToggleControl } from '@wordpress/components';
  */
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 
-const JetpackFieldRequiredToggle = props => {
-	return (
-		<ToggleControl
-			label={ __( 'Required' ) }
-			checked={ props.required }
-			onChange={ props.onChange }
-		/>
-	);
+const JetpackFieldRequiredToggle = ( { required, onChange } ) => {
+	return <ToggleControl label={ __( 'Required' ) } checked={ required } onChange={ onChange } />;
 };
 
 export default JetpackFieldRequiredToggle;
