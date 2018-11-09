c415671011c0efe383539058b69af07718159843
Jason Johnston <jasonjohnston76@gmail.com>
2018-11-09 00:35:39 -0800

    cleanup reducer

--- client/state/atomic-transfer/reducer.js
+++ client/state/atomic-transfer/reducer.js
@@ -3,46 +3,30 @@
 /**
  * Internal dependencies
  */
-import { combineReducers, keyedReducer, withSchemaValidation } from 'state/utils';
-import { transferStates } from './constants';
+import { combineReducers, keyedReducer, createReducer } from 'state/utils';
 import { atomicTransfer as schema } from './schema';
 import {
 	ATOMIC_TRANSFER_REQUEST as TRANSFER_REQUEST,
 	ATOMIC_TRANSFER_REQUEST_FAILURE as TRANSFER_REQUEST_FAILURE,
-	ATOMIC_TRANSFER_RECEIVE as TRANSFER_UPDATE,
 	ATOMIC_TRANSFER_SET as SET_TRANSFER,
 } from 'state/action-types';
 
-export const status = ( state = null, action ) => {
-	switch ( action.type ) {
-		case SET_TRANSFER:
-			return action.status;
-		case TRANSFER_UPDATE:
-			return 'completed' === action.status ? transferStates.COMPLETED : state;
-	}
+export const atomicTransfer = createReducer(
+	{},
+	{
+		[ SET_TRANSFER ]: ( state, { transfer } ) => ( { ...state, ...transfer } ),
+	},
+	schema
+);
 
-	return state;
-};
-status.hasCustomPersistence = true;
+export const fetchingTransfer = createReducer( false, {
+	[ TRANSFER_REQUEST ]: () => true,
+	[ TRANSFER_REQUEST_FAILURE ]: () => false,
+} );
 
-export const fetchingTransfer = ( state = false, action ) => {
-	switch ( action.type ) {
-		case TRANSFER_REQUEST:
-			return true;
-
-		case TRANSFER_REQUEST_FAILURE:
-			return false;
-
-		default:
-			return state;
-	}
-};
-
-export const siteReducer = combineReducers( {
-	status,
+export const atomicTransferReducers = combineReducers( {
+	atomicTransfer,
 	fetchingTransfer,
 } );
 
-// state is a map of transfer sub-states
-// keyed by the associated site id
-export default withSchemaValidation( schema, keyedReducer( 'siteId', siteReducer ) );
+export default keyedReducer( 'siteId', atomicTransferReducers );
