df32bd0d95faa8ae1280ac6213481aeaa7cb1fdb
mmtr <miguel.torres@automattic.com>
2018-11-09 13:17:12 +0100

    Switch to Gutenberg action

--- client/post-editor/editor-gutenberg-blocks-warning-dialog/index.jsx
+++ client/post-editor/editor-gutenberg-blocks-warning-dialog/index.jsx
@@ -3,29 +3,37 @@
 /**
  * External dependencies
  */
-
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Gridicon from 'gridicons';
+import { identity, noop } from 'lodash';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
-import { getEditorRawContent } from 'state/ui/editor/selectors';
-import { localize } from 'i18n-calypso';
+import { getEditorRawContent, getEditorPostId } from 'state/ui/editor/selectors';
 import Dialog from 'components/dialog';
-import { identity } from 'lodash';
+import { setSelectedEditor } from 'state/selected-editor/actions';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { getEditedPostValue } from 'state/posts/selectors';
+import getGutenbergEditorUrl from 'state/selectors/get-gutenberg-editor-url';
 
 class EditorGutenbergBlocksWarningDialog extends Component {
 	static propTypes = {
 		translate: PropTypes.func,
 		postContent: PropTypes.string,
+		siteId: PropTypes.number,
+		gutenbergUrl: PropTypes.string,
+		onSwitchToGutenberg: PropTypes.func,
 	};
 
 	static defaultProps = {
 		translate: identity,
-		postContent: '',
+		postContent: null,
+		siteId: null,
+		gutenbergUrl: null,
+		onSwitchToGutenberg: noop,
 	};
 
 	state = {
@@ -56,7 +64,10 @@ class EditorGutenbergBlocksWarningDialog extends Component {
 		} );
 	};
 
-	switchToGutenberg = () => {};
+	switchToGutenberg = () => {
+		const { onSwitchToGutenberg, siteId, gutenbergUrl } = this.props;
+		onSwitchToGutenberg( siteId, gutenbergUrl );
+	};
 
 	render() {
 		const { translate } = this.props;
@@ -75,24 +86,10 @@ class EditorGutenbergBlocksWarningDialog extends Component {
 			},
 		];
 		return (
-			<Dialog
-				additionalClassNames="editor-gutenberg-blocks-warning-dialog"
-				isVisible={ isDialogVisible }
-				buttons={ buttons }
-				onClose={ this.useClassic }
-			>
-				<header>
-					<button
-						onClick={ this.useClassic }
-						className="editor-gutenberg-blocks-warning-dialog__close"
-					>
-						<Gridicon icon="cross" />
-					</button>
-				</header>
-
+			<Dialog isVisible={ isDialogVisible } buttons={ buttons } onClose={ this.useClassic }>
 				<h1>{ translate( 'This post uses blocks from the new editor' ) }</h1>
 
-				<p className="editor-gutenberg-blocks-warning-dialog__subhead">
+				<p>
 					{ translate(
 						'You can continue to edit this post in the Classic Editor, but you may lose some data and formatting.'
 					) }
@@ -109,8 +106,22 @@ class EditorGutenbergBlocksWarningDialog extends Component {
 }
 
 export default connect(
-	state => ( {
-		postContent: getEditorRawContent( state ),
-	} ),
-	{}
+	state => {
+		const postContent = getEditorRawContent( state );
+		const siteId = getSelectedSiteId( state );
+		const postId = getEditorPostId( state );
+		const postType = getEditedPostValue( state, siteId, postId, 'type' );
+		const gutenbergUrl = getGutenbergEditorUrl( state, siteId, postId, postType );
+
+		return {
+			postContent,
+			siteId,
+			gutenbergUrl,
+		};
+	},
+	dispatch => ( {
+		onSwitchToGutenberg: ( siteId, gutenbergUrl ) => {
+			return dispatch( setSelectedEditor( siteId, 'gutenberg', gutenbergUrl ) );
+		},
+	} )
 )( localize( EditorGutenbergBlocksWarningDialog ) );
