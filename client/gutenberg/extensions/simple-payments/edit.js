405198a5cd02e6562d76daa203b926ee82604a62
Mikael Korpela <mikael@ihminen.org>
2018-11-09 22:17:36 +0200

    Update paypal text

--- client/gutenberg/extensions/simple-payments/edit.js
+++ client/gutenberg/extensions/simple-payments/edit.js
@@ -461,11 +461,10 @@ class SimplePaymentsEdit extends Component {
 					</HelpMessage>
 					<HelpMessage id={ `${ instanceId }-email-help` }>
 						{ __(
-							"This is where PayPal will send your money. To claim a payment, you'll " +
-								'need a PayPal account connected to a bank account.'
+							'Enter the email address associated with your PayPal account. Don’t have an account?'
 						) + ' ' }
 						<ExternalLink href="https://www.paypal.com/">
-							{ __( 'Create an account at PayPal' ) }
+							{ __( 'Create one on PayPal' ) }
 						</ExternalLink>
 					</HelpMessage>
 				</Fragment>
