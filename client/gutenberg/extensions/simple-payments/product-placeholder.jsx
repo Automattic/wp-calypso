6f9f3ec98872ab556489d6567d0923a93526534c
Jon Surrell <jon.surrell@automattic.com>
2018-11-09 10:03:24 +0100

    Build SDK Gutenpack: Use file-loader to include images (#28378)
    * Add png, jpg, and gif handling to file loader
    
    * Use file-loader for Simple Payments block images
    
    * Use file-loader for Related Posts block images
    
    * Use file-loader for Related Posts settings images
    
    * Add clean script to jetpack-blocks, run on publish
    
    * Add hash to file-loader names
    
    * Copy shared file-loader images into `client/assets/images`

--- client/gutenberg/extensions/simple-payments/product-placeholder.jsx
+++ client/gutenberg/extensions/simple-payments/product-placeholder.jsx
@@ -9,53 +9,48 @@ import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
  * Internal dependencies
  */
 import './product-placeholder.scss';
+import paypalImage from './paypal-button.png';
+import paypalImage2x from './paypal-button@2x.png';
 
-export default ( { title = '', content = '', formattedPrice = '', multiple = false } ) => {
-	const assetUrl =
-		typeof window !== undefined && window.Jetpack_Block_Assets_Base_Url
-			? window.Jetpack_Block_Assets_Base_Url
-			: '/wp-content/plugins/jetpack/_inc/blocks';
-
-	return (
-		<div className="jetpack-simple-payments-wrapper">
-			<div className="jetpack-simple-payments-product">
-				<div className="jetpack-simple-payments-details">
-					{ title && (
-						<div className="jetpack-simple-payments-title">
-							<p>{ title }</p>
-						</div>
-					) }
-					{ content && (
-						<div className="jetpack-simple-payments-description">
-							<p>{ content }</p>
-						</div>
-					) }
-					{ formattedPrice && (
-						<div className="jetpack-simple-payments-price">
-							<p>{ formattedPrice }</p>
-						</div>
-					) }
-					<div className="jetpack-simple-payments-purchase-box">
-						{ multiple && (
-							<div className="jetpack-simple-payments-items">
-								<input
-									className="jetpack-simple-payments-items-number"
-									readOnly
-									type="number"
-									value="1"
-								/>
-							</div>
-						) }
-						<div className="jetpack-simple-payments-button">
-							<img
-								alt={ __( 'Pay with PayPal' ) }
-								src={ `${ assetUrl }paypal-button.png` }
-								srcSet={ `${ assetUrl }paypal-button@2x.png 2x` }
+export default ( { title = '', content = '', formattedPrice = '', multiple = false } ) => (
+	<div className="jetpack-simple-payments-wrapper">
+		<div className="jetpack-simple-payments-product">
+			<div className="jetpack-simple-payments-details">
+				{ title && (
+					<div className="jetpack-simple-payments-title">
+						<p>{ title }</p>
+					</div>
+				) }
+				{ content && (
+					<div className="jetpack-simple-payments-description">
+						<p>{ content }</p>
+					</div>
+				) }
+				{ formattedPrice && (
+					<div className="jetpack-simple-payments-price">
+						<p>{ formattedPrice }</p>
+					</div>
+				) }
+				<div className="jetpack-simple-payments-purchase-box">
+					{ multiple && (
+						<div className="jetpack-simple-payments-items">
+							<input
+								className="jetpack-simple-payments-items-number"
+								readOnly
+								type="number"
+								value="1"
 							/>
 						</div>
+					) }
+					<div className="jetpack-simple-payments-button">
+						<img
+							alt={ __( 'Pay with PayPal' ) }
+							src={ paypalImage }
+							srcSet={ `${ paypalImage2x } 2x` }
+						/>
 					</div>
 				</div>
 			</div>
 		</div>
-	);
-};
+	</div>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -286,11 +286,14 @@ function getWebpackConfig( { cssFilename, externalizeWordPressPackages = false }
 					loader: 'html-loader',
 				},
 				{
-					test: /\.(svg)$/,
+					test: /\.(?:gif|jpg|jpeg|png|svg)$/i,
 					use: [
 						{
 							loader: 'file-loader',
-							options: { name: '[name].[ext]', outputPath: 'images/' },
+							options: {
+								name: '[name]-[hash].[ext]',
+								outputPath: 'images/',
+							},
 						},
 					],
 				},
