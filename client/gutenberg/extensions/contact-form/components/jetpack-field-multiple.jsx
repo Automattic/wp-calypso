ff123f5e1873edeab6b30ffc26230a162cfc602a
Miguel Lezama <miguel@automattic.com>
2018-11-09 15:18:01 -0500

    destructuring all of the things

--- client/gutenberg/extensions/contact-form/components/jetpack-field-multiple.jsx
+++ client/gutenberg/extensions/contact-form/components/jetpack-field-multiple.jsx
@@ -54,10 +54,10 @@ class JetpackFieldMultiple extends Component {
 	}
 
 	render() {
-		this.type = this.props.type;
-		let options = this.props.options;
-		let inFocus = this.state.inFocus;
-		if ( ! this.props.options.length ) {
+		const { type, instanceId, required, label, setAttributes, isSelected } = this.props;
+		let { options } = this.props;
+		let { inFocus } = this.state;
+		if ( ! options.length ) {
 			options = [ '' ];
 			inFocus = 0;
 		}
@@ -65,48 +65,45 @@ class JetpackFieldMultiple extends Component {
 		return (
 			<Fragment>
 				<BaseControl
-					id={ `jetpack-field-multiple-${ this.props.instanceId }` }
+					id={ `jetpack-field-multiple-${ instanceId }` }
 					className="jetpack-field jetpack-field-multiple"
 					label={
 						<JetpackFieldLabel
-							required={ this.props.required }
-							label={ this.props.label }
-							setAttributes={ this.props.setAttributes }
-							isSelected={ this.props.isSelected }
-							resetFocus={ () => {
-								this.setState( { inFocus: null } );
-							} }
+							required={ required }
+							label={ label }
+							setAttributes={ setAttributes }
+							isSelected={ isSelected }
+							resetFocus={ () => this.setState( { inFocus: null } ) }
 						/>
 					}
 				>
 					<ol
 						className="jetpack-field-multiple__list"
-						id={ `jetpack-field-multiple-${ this.props.instanceId }` }
+						id={ `jetpack-field-multiple-${ instanceId }` }
 					>
 						{ options.map( ( option, index ) => (
 							<JetpackOption
-								type={ this.type }
+								type={ type }
 								key={ index }
 								option={ option }
 								index={ index }
 								onChangeOption={ this.onChangeOption }
-								isSelected={ this.props.isSelected }
 								onAddOption={ this.addNewOption }
-								isInFocus={ index === inFocus && this.props.isSelected }
+								isInFocus={ index === inFocus && isSelected }
+								isSelected={ isSelected }
 							/>
 						) ) }
 					</ol>
-				{ this.props.isSelected && (
-					<IconButton
-						className="jetpack-field-multiple__add-option"
-						icon="insert"
-						label={ __( 'Insert option' ) }
-						onClick={ this.addNewOption }
-					>
-						{' '}
-						{ __( 'Add option' ) }
-					</IconButton>
-				) }
+					{ isSelected && (
+						<IconButton
+							className="jetpack-field-multiple__add-option"
+							icon="insert"
+							label={ __( 'Insert option' ) }
+							onClick={ this.addNewOption }
+						>
+							{ __( 'Add option' ) }
+						</IconButton>
+					) }
 				</BaseControl>
 			</Fragment>
 		);
