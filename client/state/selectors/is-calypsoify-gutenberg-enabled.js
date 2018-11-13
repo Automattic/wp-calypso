4dc88e7c261dc9a301fd94063099856a9a77dd8e
Marko Andrijasevic <marko.andrijasevic@automattic.com>
2018-11-13 22:51:31 +0100

    Gutenberg: release calypsoify opt-in to all users (#28508)
    Releases calypsoify opt-in to all WordPress.com Simple site users

--- client/state/selectors/is-calypsoify-gutenberg-enabled.js
+++ client/state/selectors/is-calypsoify-gutenberg-enabled.js
@@ -5,14 +5,12 @@
 import { isEnabled } from 'config';
 import isVipSite from 'state/selectors/is-vip-site';
 import { isJetpackSite } from 'state/sites/selectors';
-import { abtest } from 'lib/abtest';
 
 export const isCalypsoifyGutenbergEnabled = ( state, siteId ) =>
 	siteId
 		? isEnabled( 'calypsoify/gutenberg' ) &&
 		  ! isJetpackSite( state, siteId ) &&
-		  ! isVipSite( state, siteId ) &&
-		  'yes' === abtest( 'calypsoifyGutenberg' )
+		  ! isVipSite( state, siteId )
 		: false;
 
 export default isCalypsoifyGutenbergEnabled;
