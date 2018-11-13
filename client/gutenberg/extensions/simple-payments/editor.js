4b162d1f8de147d6ae24ca9e4703c688dcb34ba3
Thomas Guillot <thomasguillot@users.noreply.github.com>
2018-11-13 16:36:11 +0000

    Simple Payments block: Update the icon used for the block with Material

--- client/gutenberg/extensions/simple-payments/editor.js
+++ client/gutenberg/extensions/simple-payments/editor.js
@@ -4,7 +4,6 @@
  * External dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-import GridiconMoney from 'gridicons/dist/money';
 
 /**
  * Internal dependencies
@@ -26,7 +25,8 @@ registerBlockType( 'jetpack/simple-payments', {
 		'Lets you create and embed credit and debit card payment buttons with minimal setup.'
 	),
 
-	icon: <GridiconMoney />,
+	icon:
+		'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/></svg>',
 
 	category: 'jetpack',
 
