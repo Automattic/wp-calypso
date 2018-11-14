70d34aeda7ed952d929269bf60b9cf60e6dad5db
Thomas Guillot <thomasguillot@users.noreply.github.com>
2018-11-14 11:51:06 +0000

    Simple Payments block: Update svg icon to use `import { Path, SVG }`

--- client/gutenberg/extensions/simple-payments/editor.js
+++ client/gutenberg/extensions/simple-payments/editor.js
@@ -4,6 +4,7 @@
  * External dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
+import { Path, SVG } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -25,8 +26,12 @@ registerBlockType( 'jetpack/simple-payments', {
 		'Lets you create and embed credit and debit card payment buttons with minimal setup.'
 	),
 
-	icon:
-		'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/></svg>',
+	icon: (
+		<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+			<Path fill="none" d="M0 0h24v24H0V0z" />
+			<Path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z" />
+		</SVG>
+	),
 
 	category: 'jetpack',
 
