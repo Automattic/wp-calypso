14582cf76dd6441eadf86c350e5e415026571dac
Julian de Bhal <julian.debhal@automattic.com>
2018-11-14 13:29:45 +1000

    Add test for myFormatCurrency() option pass-through behaviour

--- client/blocks/subscription-length-picker/test/index.js
+++ client/blocks/subscription-length-picker/test/index.js
@@ -79,6 +79,12 @@ describe( 'SubscriptionLengthPicker basic tests', () => {
 } );
 
 describe( 'myFormatCurrency', () => {
+	test( 'Should pass through additional options', () => {
+		expect( myFormatCurrency( 1, 'USD' ) ).toBe( '$1' );
+		expect( myFormatCurrency( 1, 'USD', { symbol: '' } ) ).toBe( '1' );
+		expect( myFormatCurrency( 1, 'USD', { symbol: '?' } ) ).toBe( '?1' );
+	} );
+
 	describe( 'USD - precision 2', () => {
 		const code = 'USD';
 		const symbol = '$';
