7643e55a125ba8f08cdfe94a41e787283cef7909
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-11-15 15:11:32 -0800

    Update the icon to ballot

--- client/gutenberg/extensions/contact-form/components/jetpack-contact-form.jsx
+++ client/gutenberg/extensions/contact-form/components/jetpack-contact-form.jsx
@@ -11,7 +11,7 @@ import { Component, Fragment } from '@wordpress/element';
  * Internal dependencies
  */
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
-import GridiconMention from 'gridicons/dist/mention';
+import renderSVG from 'gutenberg/extensions/presets/jetpack/editor-shared/render-svg';
 
 class JetpackContactForm extends Component {
 	constructor( ...args ) {
@@ -87,7 +87,12 @@ class JetpackContactForm extends Component {
 				</InspectorControls>
 				<div className={ formClassnames }>
 					{ ! has_form_settings_set && (
-						<Placeholder label={ __( 'Contact Form' ) } icon={ <GridiconMention /> }>
+						<Placeholder
+							label={ __( 'Contact Form' ) }
+							icon={ renderSVG(
+								<path d="M13 7.5h5v2h-5zm0 7h5v2h-5zM19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM11 6H6v5h5V6zm-1 4H7V7h3v3zm1 3H6v5h5v-5zm-1 4H7v-3h3v3z" />
+							) }
+						>
 							<form onSubmit={ this.onFormSettingsSet }>
 								<p className="jetpack-contact-form__intro-message">{ this.getIntroMessage() }</p>
 								<TextControl
