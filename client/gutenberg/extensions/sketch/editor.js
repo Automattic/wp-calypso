4052ef3aa289c1e46f8a7270d22145ee3960a4b0
Dennis Snell <dennis.snell@automattic.com>
2018-11-15 12:43:13 -0500

    shrink file

--- client/gutenberg/extensions/sketch/editor.js
+++ client/gutenberg/extensions/sketch/editor.js
@@ -1,97 +1,71 @@
 /** @format */
-
 /**
  * External dependencies
  */
 import React from 'react';
 import apiFetch from '@wordpress/api-fetch';
 import { registerBlockType } from '@wordpress/blocks';
-import { Component } from '@wordpress/element';
-import { InspectorControls, PanelColorSettings, mediaUpload } from '@wordpress/editor';
+import { Component, Fragment } from '@wordpress/element';
+import { InspectorControls, mediaUpload, PanelColorSettings } from '@wordpress/editor';
 
 function dataURItoBlob( dataURI ) {
 	const byteString = atob( dataURI.split( ',' )[ 1 ] );
-	const mimeString = dataURI
-		.split( ',' )[ 0 ]
-		.split( ':' )[ 1 ]
-		.split( ';' )[ 0 ];
 	const ab = new ArrayBuffer( byteString.length );
 	const ia = new Uint8Array( ab );
 	for ( let i = 0; i < byteString.length; i++ ) {
 		ia[ i ] = byteString.charCodeAt( i );
 	}
-	return new Blob( [ ab ], { type: mimeString } );
+	return new Blob( [ ab ], { type: 'image/png' } );
 }
+
+const save = ( { attributes, className } ) => (
+	<img className={ className } src={ attributes.url } alt="Doodle" />
+);
 
 class Doodleboard extends Component {
 	constructor( ...args ) {
 		super( ...args );
 
-		this.xs = [];
-		this.ys = [];
-		this.cs = [];
-		this.rs = [];
+		this.points = [];
 		this.canvas = React.createRef();
 	}
 
-	addClick = ( x, y, c ) => {
-		this.xs.push( x );
-		this.ys.push( y );
-		this.cs.push( c );
-		this.rs.push( this.props.attributes.color || '#df4b26' );
-	};
+	addClick = ( x, y, c ) =>
+		this.points.push( [ x, y, c, this.props.attributes.color || '#0087be' ] );
 
 	draw = () => {
 		const c = this.canvas.current.getContext( '2d' );
 
 		c.clearRect( 0, 0, c.canvas.width, c.canvas.height );
-
 		c.lineJoin = 'round';
 		c.lineWidth = 5;
 
-		const xs = this.xs;
-		const ys = this.ys;
-		const cs = this.cs;
-		const rs = this.rs;
+		for ( let i = 0; i < this.points.length; i++ ) {
+			const next = this.points[ i ];
+			const prev = this.points[ i - 1 ];
 
-		for ( let i = 0; i < xs.length; i++ ) {
 			c.beginPath();
-			if ( cs[ i ] && i ) {
-				c.moveTo( xs[ i - 1 ], ys[ i - 1 ] );
+			if ( next[ 2 ] && i ) {
+				c.moveTo( prev[ 0 ], prev[ 1 ] );
 			} else {
-				c.moveTo( xs[ i ] - 1, ys[ i ] );
+				c.moveTo( next[ 0 ] - 1, next[ 1 ] );
 			}
-			c.lineTo( xs[ i ], ys[ i ] );
+			c.lineTo( next[ 0 ], next[ 1 ] );
 			c.closePath();
-			c.strokeStyle = rs[ i ];
+			c.strokeStyle = next[ 3 ];
 			c.stroke();
 		}
 	};
 
-	mouseDown = ( { clientX, clientY } ) => {
-		const c = this.canvas.current;
-		const { left, top } = c.getBoundingClientRect();
-		const x = clientX - left;
-		const y = clientY - top;
-
+	mouseDown = ( { clientX, clientY, isConnected = false } ) => {
+		const { left, top } = this.canvas.current.getBoundingClientRect();
 		this.isDrawing = true;
-		this.addClick( x, y );
+		this.addClick( clientX - left, clientY - top, isConnected );
 		this.draw();
 	};
 
-	mouseMove = ( { clientX, clientY } ) => {
-		if ( ! this.isDrawing ) {
-			return;
-		}
-
-		const c = this.canvas.current;
-		const { left, top } = c.getBoundingClientRect();
-		const x = clientX - left;
-		const y = clientY - top;
-
-		this.addClick( x, y, true );
-		this.draw();
-	};
+	mouseMove = ( { clientX, clientY } ) =>
+		this.isDrawing && this.mouseDown( { clientX, clientY, isConnected: true } );
 
 	stopDrawing = () => {
 		this.isDrawing = false;
@@ -105,32 +79,27 @@ class Doodleboard extends Component {
 			this.props.setAttributes( { mediaId: undefined } );
 		}
 
-		this.props.setAttributes( { isUploading: true } );
 		mediaUpload( {
 			filesList: [ dataURItoBlob( data ) ],
 			allowedTypes: [ 'image' ],
 			onFileChange: ( [ image ] ) => {
-				this.props.setAttributes( { isUploading: false, mediaId: image.id, url: image.url } );
+				this.props.setAttributes( { mediaId: image.id, url: image.url } );
 			},
 		} );
 	};
 
 	render() {
-		const { attributes, className, isSelected } = this.props;
-
-		if ( ! isSelected || attributes.isUploading ) {
-			return <img className={ className } src={ attributes.url } alt="Doodle" />;
-		}
-
-		return (
-			<div>
+		return ! this.props.isSelected ? (
+			save( this.props )
+		) : (
+			<Fragment>
 				<InspectorControls>
 					<PanelColorSettings
 						title={ 'Brush Settings' }
 						initialOpen={ false }
 						colorSettings={ [
 							{
-								value: attributes.color || '#df4b26',
+								value: this.props.attributes.color || '#0087be',
 								onChange: color => this.props.setAttributes( { color } ),
 								label: 'Color',
 							},
@@ -139,7 +108,8 @@ class Doodleboard extends Component {
 				</InspectorControls>
 				<canvas
 					ref={ this.canvas }
-					className={ className }
+					className={ this.props.className }
+					style={ { border: '2px solid #0087be' } }
 					width="400"
 					height="400"
 					onMouseDown={ this.mouseDown }
@@ -147,31 +117,20 @@ class Doodleboard extends Component {
 					onMouseUp={ this.stopDrawing }
 					onMouseLeave={ this.stopDrawing }
 				/>
-			</div>
+			</Fragment>
 		);
 	}
 }
 
 const edit = Doodleboard;
 
-const save = ( { attributes, className } ) => (
-	<img className={ className } src={ attributes.url } alt="Doodle" />
-);
-
 registerBlockType( 'a8c/sketch', {
 	title: 'Sketch a doodle',
 	icon: 'admin-customizer',
 	category: 'common',
 	attributes: {
-		mediaId: {
-			type: 'number',
-		},
-		url: {
-			type: 'string',
-			source: 'attribute',
-			selector: 'img',
-			attribute: 'src',
-		},
+		mediaId: { type: 'number' },
+		url: { type: 'string', source: 'attribute', selector: 'img', attribute: 'src' },
 	},
 	edit,
 	save,
