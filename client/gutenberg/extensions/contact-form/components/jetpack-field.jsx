46928ed8441a37ab5a762979a41d21eb5cb0a7a1
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-11-15 22:36:04 -0800

    Add placeholder and default value toggles

--- client/gutenberg/extensions/contact-form/components/jetpack-field.jsx
+++ client/gutenberg/extensions/contact-form/components/jetpack-field.jsx
@@ -4,9 +4,9 @@
  * External dependencies
  */
 import { TextControl, PanelBody } from '@wordpress/components';
-import { Fragment } from '@wordpress/element';
+import { Fragment, Component } from '@wordpress/element';
 import classNames from 'classnames';
-import { InspectorControls } from '@wordpress/editor';
+import { BlockControls, InspectorControls } from '@wordpress/editor';
 
 /**
  * Internal dependencies
@@ -14,56 +14,98 @@ import { InspectorControls } from '@wordpress/editor';
 import JetpackFieldLabel from './jetpack-field-label';
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 
-function JetpackField( {
-	isSelected,
-	type,
-	required,
-	label,
-	setAttributes,
-	defaultValue,
-	placeholder,
-	id,
-} ) {
-	return (
-		<Fragment>
-			<div className={ classNames( 'jetpack-field', { 'is-selected': isSelected } ) }>
-				<TextControl
-					type={ type }
-					label={
-						<JetpackFieldLabel
-							required={ required }
-							label={ label }
-							setAttributes={ setAttributes }
-							isSelected={ isSelected }
-						/>
-					}
-					placeholder={ placeholder }
-					value={ defaultValue }
-					disabled
-				/>
-			</div>
-			<InspectorControls>
-				<PanelBody title={ __( 'Field Settings' ) }>
-					<TextControl
-						label={ __( 'Default Value' ) }
-						value={ defaultValue }
-						onChange={ value => setAttributes( { defaultValue: value } ) }
-					/>
-					<TextControl
-						label={ __( 'Placeholder' ) }
-						value={ placeholder }
-						onChange={ value => setAttributes( { placeholder: value } ) }
-					/>
+/**
+ * Module variables
+ */
+const PANEL_PLACEHOLDER = 'placeholder';
+const PANEL_VALUE = 'value';
 
+class JetpackField extends Component {
+	state = {
+		activePanel: PANEL_PLACEHOLDER,
+	};
+
+	toggleMode = mode => () => this.setState( { activePanel: mode } );
+
+	renderToolbarButton( mode, label ) {
+		const { activePanel } = this.state;
+
+		return (
+			<button
+				className={ `components-tab-button ${ activePanel === mode ? 'is-active' : '' }` }
+				onClick={ this.toggleMode( mode ) }
+			>
+				<span>{ label }</span>
+			</button>
+		);
+	}
+
+	render() {
+		const {
+			isSelected,
+			type,
+			required,
+			label,
+			setAttributes,
+			defaultValue,
+			placeholder,
+			id,
+		} = this.props;
+
+		const { activePanel } = this.state;
+
+		const inputValue = activePanel === PANEL_PLACEHOLDER ? placeholder : defaultValue;
+		return (
+			<Fragment>
+				<div className={ classNames( 'jetpack-field', { 'is-selected': isSelected } ) }>
+					<BlockControls>
+						<div className="components-toolbar">
+							{ this.renderToolbarButton( PANEL_PLACEHOLDER, __( 'Placeholder' ) ) }
+							{ this.renderToolbarButton( PANEL_VALUE, __( 'Default Value' ) ) }
+						</div>
+					</BlockControls>
 					<TextControl
-						label={ __( 'ID' ) }
-						value={ id }
-						onChange={ value => setAttributes( { id: value } ) }
+						type={ type }
+						label={
+							<JetpackFieldLabel
+								required={ required }
+								label={ label }
+								setAttributes={ setAttributes }
+								isSelected={ isSelected }
+							/>
+						}
+						onChange={ value => {
+							if ( activePanel === PANEL_PLACEHOLDER ) {
+								setAttributes( { placeholder: value } );
+								return;
+							}
+							setAttributes( { defaultValue: value } );
+						} }
+						value={ inputValue }
 					/>
-				</PanelBody>
-			</InspectorControls>
-		</Fragment>
-	);
+				</div>
+				<InspectorControls>
+					<PanelBody title={ __( 'Field Settings' ) }>
+						<TextControl
+							label={ __( 'Default Value' ) }
+							value={ defaultValue }
+							onChange={ value => setAttributes( { defaultValue: value } ) }
+						/>
+						<TextControl
+							label={ __( 'Placeholder' ) }
+							value={ placeholder }
+							onChange={ value => setAttributes( { placeholder: value } ) }
+						/>
+						<TextControl
+							label={ __( 'ID' ) }
+							value={ id }
+							onChange={ value => setAttributes( { id: value } ) }
+						/>
+					</PanelBody>
+				</InspectorControls>
+			</Fragment>
+		);
+	}
 }
 
 export default JetpackField;
