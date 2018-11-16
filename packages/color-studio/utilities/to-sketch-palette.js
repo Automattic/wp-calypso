99a4006c784649a97113cd01642c94705e2c2e43
Dominik Porada <dominik@porada.co>
2018-11-16 01:16:10 +0100

    Format generated Sketch palettes

--- packages/color-studio/utilities/to-sketch-palette.js
+++ packages/color-studio/utilities/to-sketch-palette.js
@@ -1,6 +1,9 @@
 const chroma = require('chroma-js')
 const flatten = require('lodash/flatten')
 
+const SKETCH_PALETTE_ROW_COUNT = 8
+const SKETCH_PALETTE_ROW_FILLER = 'white'
+
 module.exports = (colorArray, customProperties = {}) => {
   const sketchPalette = Object.assign(customProperties, {
     compatibleVersion: '2',
@@ -10,11 +13,34 @@ module.exports = (colorArray, customProperties = {}) => {
     images: []
   })
 
-  flatten(colorArray).map(formatColor).forEach(rgba => {
+  formatPalette(colorArray).map(formatColor).forEach(rgba => {
     sketchPalette.colors.push(rgba)
   })
 
   return JSON.stringify(sketchPalette, null, 2)
+}
+
+function formatPalette(colorArray) {
+  if (!isArrayOfArrays(colorArray)) {
+    return colorArray
+  }
+
+  const rows = colorArray.map(fillOutPaletteRow)
+  return flatten(rows)
+}
+
+function isArrayOfArrays(collection) {
+  return Array.isArray(collection) && Array.isArray(collection[0])
+}
+
+function fillOutPaletteRow(colorArray) {
+  const row = [].concat(colorArray)
+
+  while (row.length % SKETCH_PALETTE_ROW_COUNT > 0) {
+    row.push(SKETCH_PALETTE_ROW_FILLER)
+  }
+
+  return row
 }
 
 function formatColor(colorValue) {
