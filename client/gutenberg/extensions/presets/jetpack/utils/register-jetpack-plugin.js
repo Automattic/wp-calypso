d79b1728e97c583ac15c5a956e2b7c046ec3018b
Bernie Reiter <ockham@raz.or.at>
2018-11-16 12:57:26 +0100

    Gutenblocks: Add registerJetpackPlugin() helper

---
+++ client/gutenberg/extensions/presets/jetpack/utils/register-jetpack-plugin.js
@@ -0,0 +1,22 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+import { registerPlugin } from '@wordpress/plugins';
+
+/**
+ * Internal dependencies
+ */
+import getJetpackData from './get-jetpack-data';
+
+export default function registerJetpackPlugins( name, settings ) {
+	const data = getJetpackData();
+	const available = get( data, [ 'available_blocks', name, 'available' ], false );
+	if ( data && ! available ) {
+		// TODO: check 'unavailable_reason' and respond accordingly
+		return false;
+	}
+
+	return registerPlugin( `jetpack-${ name }`, settings );
+}
