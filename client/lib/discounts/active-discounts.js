2f9f9f0f99d9460e6a3102f160ec9c6b547a4d51
Travis Walter <travis@automattic.com>
2018-11-16 07:35:58 -0800

    fixed indentation after merging a conflict

--- client/lib/discounts/active-discounts.js
+++ client/lib/discounts/active-discounts.js
@@ -94,7 +94,7 @@ export default [
 		startsAt: new Date( 2018, 9, 25, 0, 0, 1 ),
 		endsAt: new Date( 2120, 9, 26, 0, 0, 0 ),
 	},
-  {
+	{
 		name: 'default_plans_tab_business',
 		startsAt: new Date( 2018, 9, 25, 0, 0, 1 ),
 		endsAt: new Date( 2120, 9, 26, 0, 0, 0 ),
@@ -117,5 +117,5 @@ export default [
 		plansPageNoticeText:
 			'Hurry! Cyber Monday 30% Off Sale Is Almost Over. Enter CYBER30 At Checkout',
 		targetPlans: [ { type: TYPE_FREE }, { type: TYPE_PERSONAL }, { type: TYPE_PREMIUM } ],
-  },
+	},
 ];
