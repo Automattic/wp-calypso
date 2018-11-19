96cb83f57985a5a181b771cc6749828d2937c065
Radoslav Georgiev <rageorgiev@gmail.com>
2018-11-19 11:09:18 +0100

    Improving the `isEqualCaseInsensitive` function.
    - Adding JSDoc in order to explain the function.
    - Renaming some variables inside in order to make the function easier to read

--- client/extensions/woocommerce/woocommerce-services/lib/utils/is-equal-case-insensitive.js
+++ client/extensions/woocommerce/woocommerce-services/lib/utils/is-equal-case-insensitive.js
@@ -5,10 +5,22 @@
  */
 import { isString, isEqualWith } from 'lodash';
 
+/**
+ * Performs a deep comparison between two values to determine if they are equivalent.
+ *
+ * This function will transform strings to lowercase before comparison in order
+ * to ignore capitalisation. This way `Street` and `STREET` are considered equal.
+ *
+ * All other data types will be treated the same way as in `isEqual`.
+ *
+ * @param   {*} value The value/object/array to to.
+ * @param   {*} other The value/object/array to with.
+ * @returns {boolean} An indicator whether both values are deeply equal.
+ */
 export default function isEqualCaseInsensitive( value, other ) {
-	return isEqualWith( value, other, ( main, additional ) => {
-		if ( isString( main ) && isString( additional ) ) {
-			return main.toLowerCase() === additional.toLowerCase();
+	return isEqualWith( value, other, ( left, right ) => {
+		if ( isString( left ) && isString( right ) ) {
+			return left.toLowerCase() === right.toLowerCase();
 		}
 	} );
 }
