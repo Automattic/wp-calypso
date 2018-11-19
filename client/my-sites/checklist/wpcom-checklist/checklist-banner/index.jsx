422b4014f26edbc6102c766be7668a73e4cad263
Sérgio Gomes <sergiomdgomes@gmail.com>
2018-11-19 10:47:10 +0000

    Improve WpcomChecklist: reduce re-renders and load dynamically in stats (#27926)
    * Add memoization to taskUrls in WpcomChecklist.
    
    This prop wasn't being memoized in the Redux selector, which was causing
    large numbers of unnecessary re-renders.
    
    * Extract never show banner status handling to a separate module
    
    * Split WpcomChecklist into a loader and a loaded component.
    
    WpcomChecklist loads a sizeable amount of code, but it's often only
    needed in sites that qualify and haven't completed the checklist.
    
    Instead of statically including and instancing the component every
    time, this PR instead extracts some minimal exports to determine
    whether it makes sense to display the WpcomChecklist content. Only
    if these checks are passed does WpcomChecklistComponent then get
    asynchronously loaded and instanced.
    
    * Simplify render/no-render logic and move it to WpcomChecklist.
    
    This commit also removes the "CheckListNotification" class, moving that
    functionality to the main WpcomChecklist class, and moves propTypes and
    defaultProps to the WpcomChecklist class.
    
    * Add missing  designType, which was lost in a merge.

--- client/my-sites/checklist/wpcom-checklist/checklist-banner/index.jsx
+++ client/my-sites/checklist/wpcom-checklist/checklist-banner/index.jsx
@@ -5,9 +5,7 @@
 import Gridicon from 'gridicons';
 import PropTypes from 'prop-types';
 import React, { Children, Component } from 'react';
-import store from 'store';
 import { connect } from 'react-redux';
-import { get } from 'lodash';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -18,12 +16,10 @@ import Card from 'components/card';
 import ChecklistBannerTask from './task';
 import ChecklistShowShare from 'my-sites/checklist/share';
 import Gauge from 'components/gauge';
-import isEligibleForDotcomChecklist from 'state/selectors/is-eligible-for-dotcom-checklist';
 import ProgressBar from 'components/progress-bar';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { getSiteSlug } from 'state/sites/selectors';
-
-const storeKeyForNeverShow = 'sitesNeverShowChecklistBanner';
+import { setNeverShowBannerStatus } from './never-show';
 
 export class ChecklistBanner extends Component {
 	static propTypes = {
@@ -36,36 +32,19 @@ export class ChecklistBanner extends Component {
 
 	handleClose = () => {
 		const { siteId } = this.props;
-		const sitesNeverShowBanner = store.get( storeKeyForNeverShow ) || {};
-		sitesNeverShowBanner[ `${ siteId }` ] = true;
-		store.set( storeKeyForNeverShow, sitesNeverShowBanner );
+		setNeverShowBannerStatus( siteId, true );
 
 		this.setState( { closed: true } );
 
-		this.props.track( 'calypso_checklist_banner_close', {
-			site_id: siteId,
-		} );
+		if ( this.props.track ) {
+			this.props.track( 'calypso_checklist_banner_close', {
+				site_id: siteId,
+			} );
+		}
 	};
 
-	canShow() {
-		if ( ! this.props.isEligibleForDotcomChecklist ) {
-			return false;
-		}
-
-		if ( this.state.closed ) {
-			return false;
-		}
-
-		const sitesNeverShowBanner = store.get( storeKeyForNeverShow );
-		if ( get( sitesNeverShowBanner, String( this.props.siteId ) ) === true ) {
-			return false;
-		}
-
-		return true;
-	}
-
 	render() {
-		if ( ! this.canShow() ) {
+		if ( this.state.closed ) {
 			return null;
 		}
 
@@ -143,7 +122,6 @@ export class ChecklistBanner extends Component {
 export default connect( state => {
 	const siteId = getSelectedSiteId( state );
 	return {
-		isEligibleForDotcomChecklist: isEligibleForDotcomChecklist( state, siteId ),
 		siteId,
 		siteSlug: getSiteSlug( state, siteId ),
 	};
