55da5da11d3fade09dfc9f97e2a0d7b2259e6fa7
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-11-19 15:21:07 -0800

    Update the checkbox icon to be different from the form block

--- client/gutenberg/extensions/contact-form/editor.js
+++ client/gutenberg/extensions/contact-form/editor.js
@@ -305,7 +305,7 @@ registerBlockType( 'jetpack/field-checkbox-multiple', {
 	keywords: [ __( 'Choose Multiple' ), __( 'Option' ) ],
 	description: __( 'People love options. Add several checkbox items.' ),
 	icon: renderMaterialIcon(
-		<path d="M13 7.5h5v2h-5zm0 7h5v2h-5zM19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM11 6H6v5h5V6zm-1 4H7V7h3v3zm1 3H6v5h5v-5zm-1 4H7v-3h3v3z" />
+		<path d="M18 7l-1.41-1.41-6.34 6.34 1.41 1.41L18 7zm4.24-1.41L11.66 16.17 7.48 12l-1.41 1.41L11.66 19l12-12-1.42-1.41zM.41 13.41L6 19l1.41-1.41L1.83 12 .41 13.41z" />
 	),
 	edit: editMultiField( 'checkbox' ),
 	attributes: {
