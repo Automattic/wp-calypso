00201590017af071e27d015429f2f0b00c17b91f
Andrija Vucinic <andrija.vucinic@gmail.com>
2018-11-20 22:49:35 +0100

    Domains: Show the transfer away eligibility date in the UI
    - Add the date to the assembler
    - Fix tests
    - Add new UI element when transfer lock exists
    - Update copy.

---
+++ client/my-sites/domains/domain-management/transfer/transfer-out/transfer-lock.jsx
@@ -0,0 +1,50 @@
+/**
+ *  External Dependencies
+ *
+ * @format
+ */
+import React from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+import SectionHeader from 'components/section-header';
+import { UPDATE_CONTACT_INFORMATION_EMAIL_OR_NAME_CHANGES } from 'lib/url/support';
+import { getSelectedDomain } from 'lib/domains';
+
+const TransferLock = props => {
+	const { translate } = props;
+	const { transferAwayEligibleAtMoment } = getSelectedDomain( props );
+
+	return (
+		<div>
+			<SectionHeader label={ translate( 'Transfer Domain' ) } />
+			<Card className="transfer-card">
+				{ translate(
+					'Due to recent contact information updates, ' +
+						'this domain has a transfer lock that expires at {{strong}}%(date)s.{{/strong}}. ' +
+						'{{learnMoreLink}}Learn more.{{/learnMoreLink}}',
+					{
+						args: {
+							date: transferAwayEligibleAtMoment.format( 'LL' ),
+						},
+						components: {
+							strong: <strong />,
+							learnMoreLink: (
+								<a
+									href={ UPDATE_CONTACT_INFORMATION_EMAIL_OR_NAME_CHANGES }
+									target="_blank"
+									rel="noopener noreferrer"
+								/>
+							),
+						},
+					}
+				) }
+			</Card>
+		</div>
+	);
+};
+
+export default localize( TransferLock );
