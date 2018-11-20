79c8d4feee4b9c76b2e76351774e6ccab6cee6c0
Marko Andrijasevic <markoandrijasevic@gmail.com>
2018-11-20 18:19:41 +0100

    Fix PostPublishPanelToggle deprecation warning

--- client/gutenberg/editor/edit-post/components/header/post-publish-button-or-toggle.js
+++ client/gutenberg/editor/edit-post/components/header/post-publish-button-or-toggle.js
@@ -1,4 +1,3 @@
-/** @format */
 /**
  * External Dependencies
  */
@@ -8,72 +7,77 @@ import { get } from 'lodash';
 /**
  * WordPress dependencies.
  */
-import { PostPublishPanelToggle, PostPublishButton } from '@wordpress/editor';
 import { compose } from '@wordpress/compose';
 import { withDispatch, withSelect } from '@wordpress/data';
+import { PostPublishButton } from '@wordpress/editor';
 import { withViewportMatch } from '@wordpress/viewport';
 
 export function PostPublishButtonOrToggle( {
-	forceIsDirty,
-	forceIsSaving,
-	hasPublishAction,
-	isBeingScheduled,
-	isLessThanMediumViewport,
-	isPending,
-	isPublished,
-	isPublishSidebarEnabled,
-	isPublishSidebarOpened,
-	isScheduled,
-	togglePublishSidebar,
-} ) {
-	const button = (
-		<PostPublishButton forceIsDirty={ forceIsDirty } forceIsSaving={ forceIsSaving } />
-	);
-	const toggle = (
-		<PostPublishPanelToggle
-			isOpen={ isPublishSidebarOpened }
-			onToggle={ togglePublishSidebar }
-			forceIsSaving={ forceIsSaving }
-		/>
-	);
+											   forceIsDirty,
+											   forceIsSaving,
+											   hasPublishAction,
+											   isBeingScheduled,
+											   isLessThanMediumViewport,
+											   isPending,
+											   isPublished,
+											   isPublishSidebarEnabled,
+											   isPublishSidebarOpened,
+											   isScheduled,
+											   togglePublishSidebar,
+										   } ) {
+	const IS_TOGGLE = 'toggle';
+	const IS_BUTTON = 'button';
+	let component;
 
 	/**
-	 * We want to show a BUTTON when the post status is at the _final stage_
+	 * Conditions to show a BUTTON (publish directly) or a TOGGLE (open publish sidebar):
+	 *
+	 * 1) We want to show a BUTTON when the post status is at the _final stage_
 	 * for a particular role (see https://codex.wordpress.org/Post_Status):
 	 *
 	 * - is published
 	 * - is scheduled to be published
-	 * - is pending and can't be published (but only for viewports >= medium)
+	 * - is pending and can't be published (but only for viewports >= medium).
+	 * 	 Originally, we considered showing a button for pending posts that couldn't be published
+	 * 	 (for example, for an author with the contributor role). Some languages can have
+	 * 	 long translations for "Submit for review", so given the lack of UI real estate available
+	 * 	 we decided to take into account the viewport in that case.
+	 *  	 See: https://github.com/WordPress/gutenberg/issues/10475
 	 *
-	 * Originally we considered showing a button for pending posts
-	 * that couldn't be published (for ex, for a contributor role).
-	 * Some languages can have really long translations for "Submit for review",
-	 * so given the lack of UI real state we decided to take into account the viewport
-	 * in that particular case.
+	 * 2) Then, in small viewports, we'll show a TOGGLE.
+	 *
+	 * 3) Finally, we'll use the publish sidebar status to decide:
+	 *
+	 * - if it is enabled, we show a TOGGLE
+	 * - if it is disabled, we show a BUTTON
 	 */
 	if (
 		isPublished ||
 		( isScheduled && isBeingScheduled ) ||
 		( isPending && ! hasPublishAction && ! isLessThanMediumViewport )
 	) {
-		return button;
+		component = IS_BUTTON;
+	} else if ( isLessThanMediumViewport ) {
+		component = IS_TOGGLE;
+	} else if ( isPublishSidebarEnabled ) {
+		component = IS_TOGGLE;
+	} else {
+		component = IS_BUTTON;
 	}
 
-	/**
-	 * Then, we take other things into account:
-	 *
-	 * - Show TOGGLE if it is small viewport.
-	 * - Otherwise, use publish sidebar status to decide - TOGGLE if enabled, BUTTON if not.
-	 */
-	if ( isLessThanMediumViewport ) {
-		return toggle;
-	}
-
-	return isPublishSidebarEnabled ? toggle : button;
+	return (
+		<PostPublishButton
+			forceIsDirty={ forceIsDirty }
+			forceIsSaving={ forceIsSaving }
+			isOpen={ isPublishSidebarOpened }
+			isToggle={ component === IS_TOGGLE }
+			onToggle={ togglePublishSidebar }
+		/>
+	);
 }
 
 export default compose(
-	withSelect( select => ( {
+	withSelect( ( select ) => ( {
 		hasPublishAction: get(
 			select( 'core/editor' ).getCurrentPost(),
 			[ '_links', 'wp:action-publish' ],
@@ -86,11 +90,11 @@ export default compose(
 		isPublishSidebarOpened: select( 'core/edit-post' ).isPublishSidebarOpened(),
 		isScheduled: select( 'core/editor' ).isCurrentPostScheduled(),
 	} ) ),
-	withDispatch( dispatch => {
+	withDispatch( ( dispatch ) => {
 		const { togglePublishSidebar } = dispatch( 'core/edit-post' );
 		return {
 			togglePublishSidebar,
 		};
 	} ),
-	withViewportMatch( { isLessThanMediumViewport: '< medium' } )
+	withViewportMatch( { isLessThanMediumViewport: '< medium' } ),
 )( PostPublishButtonOrToggle );
