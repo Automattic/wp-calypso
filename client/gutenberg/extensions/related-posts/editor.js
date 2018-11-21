f0661358f8de2fadb56dd2ad3bec7a5deca228f8
Mikael Korpela <mikael@ihminen.org>
2018-11-21 14:27:19 +0200

    Gutenberg blocks: use SVG primitives to add accessibility attributes
    - Move `render-material-icon` utility from preset to shared folder
    
    - Use SVG primitives to add accessibility attributes, namely:
        ```
        role: 'img',
        'aria-hidden': 'true',
        focusable: 'false',
        ```
         https://github.com/WordPress/gutenberg/tree/ec1a4cbe126d860f0feb0dda8c0c8f8c1d38419b/packages/components/src/primitives/svg#readme

--- client/gutenberg/extensions/related-posts/editor.js
+++ client/gutenberg/extensions/related-posts/editor.js
@@ -4,6 +4,7 @@
  * External dependencies
  */
 import { includes } from 'lodash';
+import { G, Path, SVG } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -20,22 +21,22 @@ export const settings = {
 	title: __( 'Related Posts' ),
 
 	icon: (
-		<svg xmlns="http://www.w3.org/2000/svg">
+		<SVG xmlns="http://www.w3.org/2000/svg">
 			<defs>
-				<path
+				<Path
 					id="a"
 					d="M4 5v14h17V5H4zm4 2v2H6V7h2zm-2 6v-2h2v2H6zm0 2h2v2H6v-2zm13 2h-9v-2h9v2zm0-4h-9v-2h9v2zm0-4h-9V7h9v2z"
 				/>
 			</defs>
-			<g fill="none" fillRule="evenodd">
+			<G fill="none" fillRule="evenodd">
 				<mask id="b" fill="#fff">
 					<use xlinkHref="#a" />
 				</mask>
-				<g fill="#555D66" mask="url(#b)">
-					<path d="M0 0h24v24H0z" />
-				</g>
-			</g>
-		</svg>
+				<G fill="#555D66" mask="url(#b)">
+					<Path d="M0 0h24v24H0z" />
+				</G>
+			</G>
+		</SVG>
 	),
 
 	category: 'jetpack',
--- client/gutenberg/extensions/shared/README.md
+++ client/gutenberg/extensions/shared/README.md
@@ -1,1 +1,11 @@
-The shared directory is meant to contain components, scripts, and stylesheets that are used in multiple blocks. At present it contains only the FrontendManagement component, which manages the process rendering React components into the wrappers of certain Gutenberg blocks, extracting blcok attributes from the containers data-attributes, and injecting the children.  
+# Shared Components for Gutenberg extensions
+
+The shared directory is meant to contain components, scripts, and stylesheets that are used in multiple blocks.
+
+## FrontendManagement component
+
+Manages the process rendering React components into the wrappers of certain Gutenberg blocks, extracting block attributes from the containers data-attributes, and injecting the children.  
+
+## Render Material icon component
+
+Utility to render 24x24 SVG icons.
