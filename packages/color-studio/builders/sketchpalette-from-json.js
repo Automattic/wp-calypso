2cd2677336981d994d1b2b42651582ff426bc71c
Dominik Porada <dominik@porada.co>
2018-11-21 12:04:47 +0100

    nail_care:

--- packages/color-studio/builders/sketchpalette-from-json.js
+++ packages/color-studio/builders/sketchpalette-from-json.js
@@ -3,7 +3,8 @@ const PALETTE = require('../dist/colors.json')
 const toSketchPalette = require('../utilities/to-sketch-palette')
 
 const colors = PALETTE.colors.map(colorArray => {
-  return colorArray.map(colorObject => colorObject.value)
+  const values = colorArray.map(colorObject => colorObject.value)
+  return ['white'].concat(values)
 })
 
 const palette = toSketchPalette(colors, {
