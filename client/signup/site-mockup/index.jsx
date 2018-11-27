17dd5baf4695f4dc3a4f01130399478953fe5e91
Matt Wiebe <wiebe@automattic.com>
2018-11-27 12:20:07 -0600

    Move data into a mock file to prepare for API

--- client/signup/site-mockup/index.jsx
+++ client/signup/site-mockup/index.jsx
@@ -6,13 +6,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { find } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { getSiteTitle } from 'state/signup/steps/site-title/selectors';
 import getSiteTopic from 'state/selectors/get-signup-steps-site-topic';
+import { getVerticalData } from './mock-data';
 
 class SiteMockup extends Component {
 	static propTypes = {
@@ -51,63 +51,10 @@ class SiteMockup extends Component {
 		);
 	}
 
-	defaultVerticalData = {
-		vertical_name: 'Default',
-		vertical_id: 'a8c.0',
-		preview: {
-			title: 'My awesome WordPress site',
-			cover_image:
-				'https://images.unsplash.com/photo-1542325823-53124d9c5cbe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5441d8034187fec24a8d9ea0d5e634e6&auto=format&fit=crop&w=3134&q=80',
-			cover_image_text: 'Imagine the Synergy',
-			feature_image:
-				'https://images.unsplash.com/photo-1542332213-31f87348057f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=30f348fe853a2447c6413bea9aeb859e&auto=format&fit=crop&w=200&h=200&q=50',
-			headline: 'About Us',
-			text: 'This site will blow your mind.',
-		},
-	};
-
-	verticalList = [
-		{
-			vertical_name: 'Mexican Restaurant',
-			vertical_id: 'a8c.3.0.4.1',
-			parent: 'a8c.3.0.4',
-			preview: {
-				title: 'Very Cool Mexican Restaurant',
-				cover_image:
-					'https://images.unsplash.com/photo-1507048331197-7d4ac70811cf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=cc14d05af9963bb8ee521c3c4ea6df55&auto=format&fit=crop&w=3134&q=80',
-				cover_image_text: 'Fresh and authentic Mexican food.',
-				feature_image:
-					'https://images.unsplash.com/photo-1504544750208-dc0358e63f7f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=be3aa106f944edc77c68fcd567c22bbb&auto=format&fit=crop&w=200&h=200&q=50',
-				headline: 'About Us',
-				text: 'Enjoy a unique mix of classic Mexican dishes and innovative house specialties.',
-			},
-		},
-	];
-
-	normalizeVerticalName( name ) {
-		return name
-			.trim()
-			.toLowerCase()
-			.replace( /\s/g, '-' );
-	}
-
-	getVerticalData() {
-		let { vertical } = this.props;
-		if ( ! vertical ) {
-			return this.defaultVerticalData.preview;
-		}
-		vertical = this.normalizeVerticalName( vertical );
-		// this probably needs to be memoized
-		const verticalData = find( this.verticalList, v => {
-			return this.normalizeVerticalName( v.vertical_name ) === vertical;
-		} );
-		// todo deal with children that have no preview, use the parent preview
-		return verticalData ? verticalData.preview : this.defaultVerticalData.preview;
-	}
-
 	renderMockup( size = 'desktop' ) {
 		const classes = classNames( 'site-mockup__viewport', size );
-		const data = this.getVerticalData();
+		const data = this.props.verticalData;
+		/* eslint-disable react/no-danger */
 		return (
 			<div className={ classes }>
 				{ size === 'mobile' ? this.getMockupChromeMobile() : this.getMockupChromeDesktop() }
@@ -123,15 +70,7 @@ class SiteMockup extends Component {
 						>
 							<span>{ data.cover_image_text }</span>
 						</div>
-						<div className="site-mockup__h2">Heading 2</div>
-						<div className="site-mockup__image-text left">
-							<img src={ data.feature_image } alt="" />
-							<span>{ data.text }</span>
-						</div>
-						<div className="site-mockup__image-text right">
-							<img src={ data.feature_image } alt="" />
-							<span>{ data.text }</span>
-						</div>
+                        <div dangerouslySetInnerHTML={ { __html: data.content } } />
 						<div className="site-mockup__hr" />
 						<div className="site-mockup__h2">Send Us a Message</div>
 						<div className="site-mockup__contact-form">
@@ -147,6 +86,7 @@ class SiteMockup extends Component {
 				</div>
 			</div>
 		);
+		/* eslint-enable react/no-danger */
 	}
 
 	render() {
@@ -164,8 +104,11 @@ class SiteMockup extends Component {
 }
 
 export default connect( state => {
+	const vertical = getSiteTopic( state );
+	const verticalData = getVerticalData( vertical );
 	return {
 		title: getSiteTitle( state ),
-		vertical: getSiteTopic( state ),
+		vertical,
+		verticalData,
 	};
 } )( SiteMockup );
---
+++ client/signup/site-mockup/mock-data.js
@@ -0,0 +1,58 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { find } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+
+function normalizeVerticalName( name ) {
+	return name
+		.trim()
+		.toLowerCase()
+		.replace( /\s/g, '-' );
+}
+
+const defaultVerticalData = {
+	vertical_name: 'Default',
+	vertical_id: 'a8c.0',
+	preview: {
+		title: 'My awesome WordPress site',
+		cover_image:
+			'https://images.unsplash.com/photo-1542325823-53124d9c5cbe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5441d8034187fec24a8d9ea0d5e634e6&auto=format&fit=crop&w=3134&q=80',
+		cover_image_text: 'Imagine the Synergy',
+		content:
+			'<img class="featured-image" src="https://images.unsplash.com/photo-1543270915-a8381a52e201?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ac686dc4d32458c9afcf6ae578501b6e&auto=format&fit=crop&w=200&q=60" /><h2>The Journey Begins</h2>Here is some content that will make you change your life.',
+	},
+};
+
+const verticalList = [
+	{
+		vertical_name: 'Mexican Restaurant',
+		vertical_id: 'a8c.3.0.4.1',
+		parent: 'a8c.3.0.4',
+		preview: {
+			title: 'Very Cool Mexican Restaurant',
+			cover_image:
+				'https://images.unsplash.com/photo-1507048331197-7d4ac70811cf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=cc14d05af9963bb8ee521c3c4ea6df55&auto=format&fit=crop&w=3134&q=80',
+			cover_image_text: 'Fresh and authentic Mexican food.',
+			content:
+				'<img class="featured-image" src="https://images.unsplash.com/photo-1526350593220-3a1d7d3c8677?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4300b572345adea9ed6c828d041a6a28&auto=format&fit=crop&w=200&q=60" /><h2>Dedicated to quality</h2><div class="entry-content">Enjoy a unique mix of classic Mexican dishes and innovative house specialties.</div>',
+		},
+	},
+];
+
+export function getVerticalData( vertical ) {
+	if ( ! vertical ) {
+		return defaultVerticalData.preview;
+	}
+	vertical = normalizeVerticalName( vertical );
+	// this probably needs to be memoized
+	const verticalData = find( verticalList, v => {
+		return normalizeVerticalName( v.vertical_name ) === vertical;
+	} );
+	// todo deal with children that have no preview, use the parent preview
+	return verticalData ? verticalData.preview : defaultVerticalData.preview;
+}
