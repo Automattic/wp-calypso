a1c383e5cdee1ed2525fd06cc22769d2a612578d
Konstantin Obenland <obenland@gmx.de>
2018-11-28 08:17:28 -0800

    Remove ecommerce from plan linup

--- client/my-sites/plans-features-main/index.jsx
+++ client/my-sites/plans-features-main/index.jsx
@@ -169,13 +169,7 @@ export class PlansFeaturesMain extends Component {
 
 		if ( ! withWPPlanTabs ) {
 			return plans.filter( plan =>
-				isPlanOneOfType( plan, [
-					TYPE_FREE,
-					TYPE_PERSONAL,
-					TYPE_PREMIUM,
-					TYPE_BUSINESS,
-					TYPE_ECOMMERCE,
-				] )
+				isPlanOneOfType( plan, [ TYPE_FREE, TYPE_PERSONAL, TYPE_PREMIUM, TYPE_BUSINESS ] )
 			);
 		}
 
