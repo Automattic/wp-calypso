bb10e2dd8e2c90af5f9fa7b2c2f9f3c370ae61a9
Ben Lowery <blowery@users.noreply.github.com>
2018-11-29 13:38:26 -0500

    Tests: Clean up memory leaks during test runs (#28875)
    * Tests: Clean up memory leak from nock
    
    We were setting up nock's disableNetConnect globally, but never cleaning it up. Use a afterAll hook to clean up the interceptors and restore the normal functionality and a beforeAll hook to activate nock.
    
    The disableNetConnect call _cannot_ be inside the beforeAll hook due to some kind of race between superagent and nock. Moving it there allows some superagent requests to escape nock's sandbox and randomly break the test runner.
    
    * free up memory in the server tests too

--- test/client/setup-test-framework.js
+++ test/client/setup-test-framework.js
@@ -2,10 +2,23 @@
 /**
  * External dependencies
  */
-import { disableNetConnect } from 'nock';
+const nock = require( 'nock' );
 
-// It disables all network requests for all tests.
-disableNetConnect();
+// Disables all network requests for all tests.
+nock.disableNetConnect();
+
+beforeAll( () => {
+	// reactivate nock on test start
+	if ( ! nock.isActive() ) {
+		nock.activate();
+	}
+} );
+
+afterAll( () => {
+	// helps clean up nock after each test run and avoid memory leaks
+	nock.restore();
+	nock.cleanAll();
+} );
 
 // It "mocks" enzyme, so that we can delay loading of
 // the utility functions until enzyme is imported in tests.
