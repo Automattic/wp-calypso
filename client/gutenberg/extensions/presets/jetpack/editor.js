fcc89ce60daef85e15bbd0fcd9d3ba9beb4dfc33
Mikael Korpela <mikael@ihminen.org>
2018-11-29 17:30:51 +0200

    Tiled galleries block

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -10,3 +10,4 @@ import 'gutenberg/extensions/markdown/editor';
 import 'gutenberg/extensions/publicize/editor';
 import 'gutenberg/extensions/related-posts/editor';
 import 'gutenberg/extensions/simple-payments/editor';
+import 'gutenberg/extensions/tiled-gallery/editor';
