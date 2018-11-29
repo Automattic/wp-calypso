3a151cca56672de1c7809cc83847e3c3e1c05e58
Mikael Korpela <mikael@ihminen.org>
2018-11-29 15:55:25 +0200

    Tiled Gallery block: rewrite (#27458)

--- client/gutenberg/extensions/tiled-gallery/edit.jsx
+++ client/gutenberg/extensions/tiled-gallery/edit.jsx
@@ -3,44 +3,53 @@
 /**
  * External Dependencies
  */
-import { pick } from 'lodash';
-
-/**
- * WordPress dependencies
- */
+import { filter, pick } from 'lodash';
 import { Component, Fragment } from '@wordpress/element';
 import {
-	BlockControls,
-	InspectorControls,
-	MediaPlaceholder,
-	MediaUpload,
-	mediaUpload,
-} from '@wordpress/editor';
-import {
 	DropZone,
+	FormFileUpload,
 	IconButton,
 	PanelBody,
 	RangeControl,
 	SelectControl,
+	ToggleControl,
 	Toolbar,
 	withNotices,
 } from '@wordpress/components';
+import {
+	BlockControls,
+	InspectorControls,
+	MediaPlaceholder,
+	mediaUpload,
+	MediaUpload,
+} from '@wordpress/editor';
+import { create } from '@wordpress/rich-text';
 
 /**
  * Internal dependencies
  */
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
-import TiledGallerySave from './save.jsx';
+import { ALLOWED_MEDIA_TYPES, MAX_COLUMNS, DEFAULT_COLUMNS } from './constants';
+import GalleryGrid from './gallery-grid';
+import GalleryImage from './gallery-image';
+import { getActiveStyleName } from './layouts';
 
-/**
- * Module variables
- */
-const MAX_COLUMNS = 8;
-const linkOptions = [
-	{ value: 'attachment', label: __( 'Attachment Page' ) },
-	{ value: 'media', label: __( 'Media File' ) },
-	{ value: 'none', label: __( 'None' ) },
-];
+export function defaultColumnsNumber( attributes ) {
+	return Math.min( DEFAULT_COLUMNS, attributes.images.length );
+}
+
+const pickRelevantMediaFiles = image => {
+	let { caption } = image;
+
+	if ( typeof caption !== 'object' ) {
+		caption = create( { html: caption } );
+	}
+
+	return {
+		...pick( image, [ 'alt', 'id', 'link', 'url' ] ),
+		caption,
+	};
+};
 
 class TiledGalleryEdit extends Component {
 	constructor() {
@@ -48,13 +57,18 @@ class TiledGalleryEdit extends Component {
 
 		this.onSelectImage = this.onSelectImage.bind( this );
 		this.onSelectImages = this.onSelectImages.bind( this );
+		this.setLayout = this.setLayout.bind( this );
 		this.setLinkTo = this.setLinkTo.bind( this );
 		this.setColumnsNumber = this.setColumnsNumber.bind( this );
+		this.toggleImageCrop = this.toggleImageCrop.bind( this );
+		this.onRemoveImage = this.onRemoveImage.bind( this );
 		this.setImageAttributes = this.setImageAttributes.bind( this );
 		this.addFiles = this.addFiles.bind( this );
+		this.uploadFromFiles = this.uploadFromFiles.bind( this );
 
 		this.state = {
 			selectedImage: null,
+			layout: getActiveStyleName( arguments[ 0 ].className ),
 		};
 	}
 
@@ -68,18 +82,42 @@ class TiledGalleryEdit extends Component {
 		};
 	}
 
+	onRemoveImage( index ) {
+		return () => {
+			const images = filter( this.props.attributes.images, ( img, i ) => index !== i );
+			const { columns } = this.props.attributes;
+			this.setState( { selectedImage: null } );
+			this.props.setAttributes( {
+				images,
+				columns: columns ? Math.min( images.length, columns ) : columns,
+			} );
+		};
+	}
+
 	onSelectImages( images ) {
 		this.props.setAttributes( {
-			images: images.map( image => pick( image, [ 'alt', 'caption', 'id', 'url', 'link' ] ) ),
+			images: images.map( image => pickRelevantMediaFiles( image ) ),
 		} );
 	}
 
-	setLinkTo( value ) {
-		this.props.setAttributes( { linkTo: value } );
+	setLayout( layout ) {
+		this.setState( { layout } );
 	}
 
-	setColumnsNumber( value ) {
-		this.props.setAttributes( { columns: value } );
+	setLinkTo( linkTo ) {
+		this.props.setAttributes( { linkTo } );
+	}
+
+	setColumnsNumber( columns ) {
+		this.props.setAttributes( { columns } );
+	}
+
+	toggleImageCrop() {
+		this.props.setAttributes( { imageCrop: ! this.props.attributes.imageCrop } );
+	}
+
+	getImageCropHelp( checked ) {
+		return checked ? __( 'Thumbnails are cropped to align.' ) : __( 'Thumbnails are not cropped.' );
 	}
 
 	setImageAttributes( index, attributes ) {
@@ -102,15 +140,20 @@ class TiledGalleryEdit extends Component {
 		} );
 	}
 
+	uploadFromFiles( event ) {
+		this.addFiles( event.target.files );
+	}
+
 	addFiles( files ) {
 		const currentImages = this.props.attributes.images || [];
 		const { noticeOperations, setAttributes } = this.props;
 		mediaUpload( {
-			allowedType: 'image',
+			allowedTypes: ALLOWED_MEDIA_TYPES,
 			filesList: files,
 			onFileChange: images => {
+				const imagesNormalized = images.map( image => pickRelevantMediaFiles( image ) );
 				setAttributes( {
-					images: currentImages.concat( images ),
+					images: currentImages.concat( imagesNormalized ),
 				} );
 			},
 			onError: noticeOperations.createErrorNotice,
@@ -120,28 +163,46 @@ class TiledGalleryEdit extends Component {
 	componentDidUpdate( prevProps ) {
 		// Deselect images when deselecting the block
 		if ( ! this.props.isSelected && prevProps.isSelected ) {
-			// @TODO refactor
-			// eslint-disable-next-line react/no-did-update-set-state
+			//eslint-disable-next-line
 			this.setState( {
 				selectedImage: null,
 				captionSelected: false,
 			} );
 		}
+
+		if ( this.props.className !== prevProps.className ) {
+			const activeStyleName = getActiveStyleName( this.props.className );
+
+			if ( activeStyleName !== this.state.layout ) {
+				this.setLayout( activeStyleName );
+			}
+		}
 	}
 
 	render() {
-		const { attributes, className, isSelected, noticeOperations, noticeUI } = this.props;
-		const { images, columns, linkTo } = attributes;
+		const { selectedImage } = this.state;
 
-		const dropZone = <DropZone key="item-dropzone" onFilesDrop={ this.addFiles } />;
+		const { attributes, isSelected, className, noticeOperations, noticeUI } = this.props;
 
-		const controls = isSelected && (
-			<BlockControls key="controls">
+		const {
+			images,
+			columns = defaultColumnsNumber( attributes ),
+			align,
+			imageCrop,
+			linkTo,
+		} = attributes;
+
+		const layoutsSupportingColumns = [ 'square', 'circle' ];
+
+		const dropZone = <DropZone onFilesDrop={ this.addFiles } />;
+
+		const controls = (
+			<BlockControls>
 				{ !! images.length && (
 					<Toolbar>
 						<MediaUpload
 							onSelect={ this.onSelectImages }
-							type="image"
+							allowedTypes={ ALLOWED_MEDIA_TYPES }
 							multiple
 							gallery
 							value={ images.map( img => img.id ) }
@@ -163,65 +224,102 @@ class TiledGalleryEdit extends Component {
 			return (
 				<Fragment>
 					{ controls }
-					{ noticeUI }
 					<MediaPlaceholder
-						key="gallery-placeholder"
-						className={ className }
 						icon="format-gallery"
+						className={ className }
 						labels={ {
-							title: __( 'Tiled Gallery' ),
+							title: __( 'Tiled gallery' ),
 							name: __( 'images' ),
 						} }
 						onSelect={ this.onSelectImages }
+						accept="image/*"
+						allowedTypes={ ALLOWED_MEDIA_TYPES }
+						multiple
 						notices={ noticeUI }
 						onError={ noticeOperations.createErrorNotice }
-						accept="image/*"
-						type="image"
-						multiple
 					/>
 				</Fragment>
 			);
 		}
 
-		// To avoid users accidentally navigating out of Gutenberg by clicking an image, we disable linkTo in the editor view here by forcing 'none'.
-		const imageTiles = (
-			<TiledGallerySave
-				attributes={ {
-					className,
-					images,
-					columns,
-					linkTo: 'none',
-				} }
-			/>
-		);
+		const renderGalleryImage = index => {
+			if ( ! images[ index ] ) {
+				return null;
+			}
+
+			const image = images[ index ];
+
+			return (
+				<GalleryImage
+					alt={ image.alt }
+					caption={ image.caption }
+					id={ image.id }
+					isSelected={ isSelected && selectedImage === index }
+					onRemove={ this.onRemoveImage( index ) }
+					onSelect={ this.onSelectImage( index ) }
+					setAttributes={ attrs => this.setImageAttributes( index, attrs ) }
+					url={ image.url }
+				/>
+			);
+		};
 
 		return (
 			<Fragment>
 				{ controls }
-				{ isSelected && (
-					<InspectorControls key="inspector">
-						<PanelBody title={ __( 'Tiled Gallery Settings' ) }>
-							{ images.length > 1 && (
-								<RangeControl
-									label={ __( 'Columns' ) }
-									value={ columns }
-									onChange={ this.setColumnsNumber }
-									min={ 1 }
-									max={ Math.min( MAX_COLUMNS, images.length ) }
-								/>
-							) }
-							<SelectControl
-								label={ __( 'Link to' ) }
-								value={ linkTo }
-								onChange={ this.setLinkTo }
-								options={ linkOptions }
+				<InspectorControls>
+					<PanelBody title={ __( 'Tiled gallery settings' ) }>
+						{ images.length > 1 && (
+							<RangeControl
+								label={ __( 'Columns' ) }
+								value={ columns }
+								onChange={ this.setColumnsNumber }
+								min={ 1 }
+								disabled={ layoutsSupportingColumns.indexOf( this.state.layout ) === -1 }
+								max={ Math.min( MAX_COLUMNS, images.length ) }
 							/>
-						</PanelBody>
-					</InspectorControls>
-				) }
-				{ dropZone }
+						) }
+						<ToggleControl
+							label={ __( 'Crop images' ) }
+							checked={ !! imageCrop }
+							onChange={ this.toggleImageCrop }
+							help={ this.getImageCropHelp }
+						/>
+						<SelectControl
+							label={ __( 'Link to' ) }
+							value={ linkTo }
+							onChange={ this.setLinkTo }
+							options={ [
+								{ value: 'attachment', label: __( 'Attachment page' ) },
+								{ value: 'media', label: __( 'Media file' ) },
+								{ value: 'none', label: __( 'None' ) },
+							] }
+						/>
+					</PanelBody>
+				</InspectorControls>
 				{ noticeUI }
-				{ imageTiles }
+				{ dropZone }
+				<GalleryGrid
+					align={ align }
+					className={ className }
+					columns={ columns }
+					imageCrop={ imageCrop }
+					images={ images }
+					layout={ this.state.layout }
+					renderGalleryImage={ renderGalleryImage }
+				>
+					{ isSelected && (
+						<FormFileUpload
+							multiple
+							isLarge
+							className="block-library-gallery-add-item-button"
+							onChange={ this.uploadFromFiles }
+							accept="image/*"
+							icon="insert"
+						>
+							{ __( 'Upload an image' ) }
+						</FormFileUpload>
+					) }
+				</GalleryGrid>
 			</Fragment>
 		);
 	}
--- client/gutenberg/extensions/tiled-gallery/save.jsx
+++ client/gutenberg/extensions/tiled-gallery/save.jsx
@@ -1,12 +1,81 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { RichText } from '@wordpress/editor';
+import classnames from 'classnames';
+
 /**
  * Internal dependencies
  */
-import TiledGalleryLayoutSquare from './layout-square.jsx';
+import { defaultColumnsNumber } from './edit';
+import GalleryGrid from './gallery-grid';
+import { getActiveStyleName } from './layouts';
 
-function TiledGallerySave( { attributes } ) {
+export default ( { attributes } ) => {
+	const {
+		align,
+		className,
+		columns = defaultColumnsNumber( attributes ),
+		imageCrop,
+		images,
+		linkTo,
+	} = attributes;
+
+	const layout = getActiveStyleName( className );
+
+	const renderGalleryImage = index => {
+		if ( ! images[ index ] ) {
+			return null;
+		}
+
+		const image = images[ index ];
+
+		let href;
+
+		switch ( linkTo ) {
+			case 'media':
+				href = image.url;
+				break;
+			case 'attachment':
+				href = image.link;
+				break;
+		}
+
+		const img = (
+			<img
+				alt={ image.alt }
+				className={ classnames( {
+					[ `wp-image-${ image.id }` ]: image.id,
+				} ) }
+				data-id={ image.id }
+				data-link={ image.link }
+				src={ image.url }
+			/>
+		);
+
+		return (
+			<figure>
+				{ href ? <a href={ href }>{ img }</a> : img }
+				{ layout !== 'circle' &&
+					image.caption &&
+					image.caption.length > 0 && (
+						<RichText.Content tagName="figcaption" value={ image.caption } />
+					) }
+			</figure>
+		);
+	};
+
 	return (
-		<TiledGalleryLayoutSquare { ...attributes } />
+		<GalleryGrid
+			align={ align }
+			className={ `wp-block-jetpack-tiled-gallery ${ className }` }
+			columns={ columns }
+			imageCrop={ imageCrop }
+			images={ images }
+			layout={ layout }
+			renderGalleryImage={ renderGalleryImage }
+		/>
 	);
-}
-
-export default TiledGallerySave;
+};
