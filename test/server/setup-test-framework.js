6d6a06c38660a3b092bd13d38c308b3d2d878d3a
Ben Lowery <ben.lowery@automattic.com>
2018-11-29 11:35:15 -0500

    free up memory in the server tests too

--- test/server/setup-test-framework.js
+++ test/server/setup-test-framework.js
@@ -2,9 +2,24 @@
 /**
  * External dependencies
  */
-import { disableNetConnect } from 'nock';
+import * as nock from 'nock';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
 
 chai.use( sinonChai );
-disableNetConnect();
+
+// Disables all network requests for all tests.
+nock.disableNetConnect();
+
+beforeAll( () => {
+	// reactivate nock on test start
+	if ( ! nock.isActive() ) {
+		nock.activate();
+	}
+} );
+
+afterAll( () => {
+	// helps clean up nock after each test run and avoid memory leaks
+	nock.restore();
+	nock.cleanAll();
+} );
