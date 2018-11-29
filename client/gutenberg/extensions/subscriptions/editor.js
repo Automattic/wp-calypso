a03851f7d6e237e97d33caaf60c7c67800e1876e
Alda Vigdís Skarphéðinsdóttir <alda.vigdis@a8c.com>
2018-11-29 22:17:37 +0100

    Removing comment referencing markdown and adding subscriber count as an attribute

--- client/gutenberg/extensions/subscriptions/editor.js
+++ client/gutenberg/extensions/subscriptions/editor.js
@@ -20,16 +20,18 @@ export const settings = {
 	description: (
 		<Fragment>
 			<p>
-				{ __( 'A form enabling readers to get notifications when new posts are published from this site.' ) }
+				{ __(
+					'A form enabling readers to get notifications when new posts are published from this site.'
+				) }
 			</p>
 		</Fragment>
 	),
 
 	icon: (
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-			<rect x="0" fill="none" width="24" height="24"/>
+			<rect x="0" fill="none" width="24" height="24" />
 			<g>
-				<path d="M23 16v2h-3v3h-2v-3h-3v-2h3v-3h2v3h3zM20 2v9h-4v3h-3v4H4c-1.1 0-2-.9-2-2V2h18zM8 13v-1H4v1h4zm3-3H4v1h7v-1zm0-2H4v1h7V8zm7-4H4v2h14V4z"/>
+				<path d="M23 16v2h-3v3h-2v-3h-3v-2h3v-3h2v3h3zM20 2v9h-4v3h-3v4H4c-1.1 0-2-.9-2-2V2h18zM8 13v-1H4v1h4zm3-3H4v1h7v-1zm0-2H4v1h7V8zm7-4H4v2h14V4z" />
 			</g>
 		</svg>
 	),
@@ -39,7 +41,7 @@ export const settings = {
 	keywords: [ __( 'subscribe' ), __( 'subscription' ), __( 'follow' ) ],
 
 	attributes: {
-		//The Markdown source is saved in the block content comments delimiter
+		subscriber_count: { type: 'string', default: '0' },
 		subscribe_placeholder: { type: 'string', default: 'Email Address' },
 		subscribe_button: { type: 'string', default: 'Subscribe' },
 		show_subscribers_total: { type: 'boolean', default: false },
