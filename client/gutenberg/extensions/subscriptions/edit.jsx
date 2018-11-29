6d5770f467b3214303c7287d26e97ec5cf0defa0
Michael Turk <turk.michael@gmail.com>
2018-11-29 17:57:59 -0500

    Added back string construction and attempting to use new endpoint

--- client/gutenberg/extensions/subscriptions/edit.jsx
+++ client/gutenberg/extensions/subscriptions/edit.jsx
@@ -10,11 +10,13 @@ import { TextControl, Button, ToggleControl } from '@wordpress/components';
  * Internal dependencies
  */
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
+import { sprintf } from '@wordpress/i18n/build/index';
+import apiFetch from '@wordpress/api-fetch';
 
 class SubscriptionEdit extends Component {
 	render() {
 		const { attributes, className, isSelected, setAttributes } = this.props;
-		const { subscribe_placeholder, show_subscribers_total, subscriber_count } = attributes;
+		const { subscribe_placeholder, show_subscribers_total } = attributes;
 
 		if ( isSelected ) {
 			return (
@@ -34,13 +36,23 @@ class SubscriptionEdit extends Component {
 			);
 		}
 
+		let subscriberCount = null;
+		let subscriberCountString = null;
+
+		if ( show_subscribers_total ) {
+			apiFetch( { path: '/wpcom/v2/subscribers/count' } ).then( count => {
+				subscriberCount = count;
+			} );
+
+			if ( 1 === subscriberCount ) {
+				subscriberCountString = sprintf( __( 'Join %s other subscriber' ), subscriberCount );
+			} else {
+				subscriberCountString = sprintf( __( 'Join %s other subscribers' ), subscriberCount );
+			}
+		}
 		return (
 			<div className={ className }>
-				{ show_subscribers_total === true && (
-					<p>
-						{ subscriber_count } { 'subscribers' }
-					</p>
-				) }
+				<p>{ subscriberCountString }</p>
 				<TextControl placeholder={ subscribe_placeholder } />
 				<Button type="button" isDefault>
 					{ __( 'Subscribe' ) }
