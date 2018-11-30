8b36eff286d08d2b4d985cfe7791af72ea01ea5b
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2018-11-30 14:23:59 +0200

    Gutenberg: Introduce a reusable JetpackPluginSidebar (#28913)
    * Gutenberg: Introduce JetpackPluginSidebar
    
    * Gutenberg: Style JetpackPluginSidebar icons
    
    * Cleanup JetpackLogo indirection
    
    * Use variables for colors
    
    * Remove unnecessary fill
    
    * Remove * rule in favor of specific selectors
    
    * Use white instead of muriel-white

---
+++ client/gutenberg/extensions/presets/jetpack/editor-shared/jetpack-plugin-sidebar.js
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import { createSlotFill } from '@wordpress/components';
+import { Fragment } from '@wordpress/element';
+import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
+import { registerPlugin } from '@wordpress/plugins';
+
+/**
+ * Internal dependencies
+ */
+import './jetpack-plugin-sidebar.scss';
+import JetpackLogo from 'components/jetpack-logo';
+import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
+
+const { Fill, Slot } = createSlotFill( 'JetpackPluginSidebar' );
+
+const JetpackPluginSidebar = ( { children } ) => (
+	<Fill>
+		{ children }
+	</Fill>
+);
+
+JetpackPluginSidebar.Slot = () => (
+	<Fragment>
+		<PluginSidebarMoreMenuItem target="jetpack" icon={ <JetpackLogo /> }>
+			{ __( 'Jetpack' ) }
+		</PluginSidebarMoreMenuItem>
+		<PluginSidebar name="jetpack" title={ __( 'Jetpack' ) } icon={ <JetpackLogo /> }>
+			<Slot />
+		</PluginSidebar>
+	</Fragment>
+);
+
+registerPlugin( 'jetpack-sidebar', {
+	render: () => <JetpackPluginSidebar.Slot />
+} );
+
+export default JetpackPluginSidebar;
