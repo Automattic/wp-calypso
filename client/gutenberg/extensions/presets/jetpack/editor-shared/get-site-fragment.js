7ab762fc557df18347ee823414e637147fdd972c
Marin Atanasov <tyxla@abv.bg>
2018-11-30 17:20:20 +0200

    Rename

--- client/gutenberg/extensions/presets/jetpack/editor-shared/get-site-fragment.js
+++ client/gutenberg/extensions/presets/jetpack/editor-shared/get-site-fragment.js
@@ -8,14 +8,14 @@ import { includes } from 'lodash';
 /**
  * Internal dependencies
  */
-import { getSiteFragment } from 'lib/route/path';
+import { getSiteFragment as getCalypsoSiteFragment } from 'lib/route/path';
 
 /***
  * Returns the site fragment in the environment we're running Gutenberg in.
  *
  * @returns {?Number|String} Site fragment (ID or slug); null for WP.org wp-admin.
  */
-export default function getGutenbergSiteFragment() {
+export default function getSiteFragment() {
 	// WP.com wp-admin exposes the site ID in window._currentSiteId
 	if ( window && window._currentSiteId ) {
 		return window._currentSiteId;
@@ -23,7 +23,7 @@ export default function getGutenbergSiteFragment() {
 
 	// Calypso will contain a site slug or ID in the site fragment.
 	// WP.org will contain either `post` or `post-new.php`.
-	const siteFragment = getSiteFragment( window.location.pathname );
+	const siteFragment = getCalypsoSiteFragment( window.location.pathname );
 	if ( ! includes( [ 'post.php', 'post-new.php' ], siteFragment ) ) {
 		return siteFragment || null;
 	}
--- client/gutenberg/extensions/presets/jetpack/editor-shared/test/get-site-fragment.js
+++ client/gutenberg/extensions/presets/jetpack/editor-shared/test/get-site-fragment.js
@@ -3,9 +3,9 @@
 /**
  * Internal dependencies
  */
-import getGutenbergSiteFragment from '../get-gutenberg-site-fragment';
+import getSiteFragment from '../get-site-fragment';
 
-describe( 'getGutenbergSiteFragment()', () => {
+describe( 'getSiteFragment()', () => {
 	let beforeWindow;
 
 	beforeAll( () => {
@@ -21,32 +21,32 @@ describe( 'getGutenbergSiteFragment()', () => {
 
 	test( 'should return null by default', () => {
 		window.location.pathname = '/';
-		expect( getGutenbergSiteFragment() ).toBeNull();
+		expect( getSiteFragment() ).toBeNull();
 	} );
 
 	test( 'should return null when editing a post in wp-admin', () => {
 		window.location.pathname = '/wp-admin/post.php';
-		expect( getGutenbergSiteFragment() ).toBeNull();
+		expect( getSiteFragment() ).toBeNull();
 	} );
 
 	test( 'should return null when starting a new post in wp-admin', () => {
 		window.location.pathname = '/wp-admin/post-new.php';
-		expect( getGutenbergSiteFragment() ).toBeNull();
+		expect( getSiteFragment() ).toBeNull();
 	} );
 
 	test( 'should return site fragment when starting a post in calypso', () => {
 		window.location.pathname = '/gutenberg/post/yourjetpack.blog';
-		expect( getGutenbergSiteFragment() ).toBe( 'yourjetpack.blog' );
+		expect( getSiteFragment() ).toBe( 'yourjetpack.blog' );
 	} );
 
 	test( 'should return site fragment when editing a post in calypso', () => {
 		window.location.pathname = '/gutenberg/post/yourjetpack.blog/123';
-		expect( getGutenbergSiteFragment() ).toBe( 'yourjetpack.blog' );
+		expect( getSiteFragment() ).toBe( 'yourjetpack.blog' );
 	} );
 
 	test( 'should return site ID when _currentSiteId is exposed', () => {
 		window.location.pathname = '/gutenberg/post/yourjetpack.blog/123';
 		window._currentSiteId = 12345678;
-		expect( getGutenbergSiteFragment() ).toBe( 12345678 );
+		expect( getSiteFragment() ).toBe( 12345678 );
 	} );
 } );
