6ead2e4bb72f65816212f3ddb6d4abbd9285e968
niranjan-uma-shankar <niranjan.u@gmail.com>
2018-12-03 18:09:35 +0530

    Adding the actions.js tests (#29022)

---
+++ client/state/signup/steps/site-type/test/actions.js
@@ -0,0 +1,18 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import { setSiteType } from '../actions';
+import { SIGNUP_STEPS_SITE_TYPE_SET } from 'state/action-types';
+
+describe( 'setSiteType()', () => {
+	test( 'should return the expected action object', () => {
+		const siteType = 'Blog';
+
+		expect( setSiteType( siteType ) ).toEqual( {
+			type: SIGNUP_STEPS_SITE_TYPE_SET,
+			siteType,
+		} );
+	} );
+} );
