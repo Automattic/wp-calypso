d4ef2edf3f3063b350a88524dd4eb702550eeae6
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-12-03 07:34:04 -0800

    Gutenlypso: Add/map block to calypso gutenberg (#29014)
    And fix some styling issues by increasing specificity.

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -7,6 +7,7 @@ import './shared/public-path';
 import './editor-shared/block-category'; // Register the Jetpack category
 import 'gutenberg/extensions/contact-form/editor';
 import 'gutenberg/extensions/markdown/editor';
+import 'gutenberg/extensions/map/editor';
 import 'gutenberg/extensions/publicize/editor';
 import 'gutenberg/extensions/related-posts/editor';
 import 'gutenberg/extensions/simple-payments/editor';
