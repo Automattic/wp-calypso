4339e4aee5fe7682ae3f842caebd8a94c3b0e18e
Alex Kirk <akirk@users.noreply.github.com>
2018-12-04 14:58:14 +0100

    Typo

--- client/gutenberg/extensions/contact-form/editor.js
+++ client/gutenberg/extensions/contact-form/editor.js
@@ -238,7 +238,7 @@ registerJetpackBlock( 'field-url', {
 registerJetpackBlock( 'field-date', {
 	...FieldDefaults,
 	title: __( 'Date Picker' ),
-	keywords: [ __( 'Calendar' ), __( 'day month year', 'block search term' ) ],
+	keywords: [ __( 'Calendar' ), _x( 'day month year', 'block search term' ) ],
 	description: __( 'The best way to set a date. Add a date picker.' ),
 	icon: renderMaterialIcon(
 		<path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V9h14v10zm0-12H5V5h14v2zM7 11h5v5H7z" />
