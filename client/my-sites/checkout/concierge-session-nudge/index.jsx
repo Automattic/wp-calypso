5d5ead8d0425265224aa43ecc4f63a8c13435f72
Adam Zieliński <adam@adamziel.com>
2018-12-04 15:13:26 +0100

    Design for Concierge Session upsell page

--- client/my-sites/checkout/concierge-session-nudge/index.jsx
+++ client/my-sites/checkout/concierge-session-nudge/index.jsx
@@ -6,6 +6,7 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Gridicon from 'gridicons';
 import page from 'page';
 
 /**
@@ -23,6 +24,10 @@ import isEligibleForDotcomChecklist from 'state/selectors/is-eligible-for-dotcom
 import { getSiteSlug } from 'state/sites/selectors';
 import { recordTracksEvent } from 'state/analytics/actions';
 
+/*
+ * There are no translate() calls in this file because it's launching only to
+ * EN audience
+ */
 export class ConciergeSessionNudge extends React.Component {
 	static propTypes = {
 		receiptId: PropTypes.number.isRequired,
@@ -49,13 +54,76 @@ export class ConciergeSessionNudge extends React.Component {
 	header() {
 		return (
 			<header className="concierge-session-nudge__header">
-				<h2 className="concierge-session-nudge__title">Offer header</h2>
+				<h2 className="concierge-session-nudge__title">
+					Congratulations, Your site is being upgraded.
+				</h2>
 			</header>
 		);
 	}
 
 	body() {
-		return <Fragment>Offer content</Fragment>;
+		return (
+			<Fragment>
+				<h4 className="concierge-session-nudge__sub-header">
+					Want to create a site that looks great, gets traffic, and makes money?
+				</h4>
+
+				<p>
+					<b>
+						Reserve a 45-minute one-on-one call with a website expert to help you get started on the
+						right foot.
+					</b>
+				</p>
+
+				<p>What our team of experts can help you with:</p>
+
+				<p>
+					<ul className="concierge-session-nudge__checklist">
+						<li className="concierge-session-nudge__checklist-item">
+							<Gridicon icon="checkmark" className="concierge-session-nudge__checklist-item-icon" />
+							<span className="concierge-session-nudge__checklist-item-text">
+								<b>Design:</b> Which template to choose.
+							</span>
+						</li>
+						<li className="concierge-session-nudge__checklist-item">
+							<Gridicon icon="checkmark" className="concierge-session-nudge__checklist-item-icon" />
+							<span className="concierge-session-nudge__checklist-item-text">
+								<b>Traffic:</b> How to get free search engine traffic with SEO tools.
+							</span>
+						</li>
+						<li className="concierge-session-nudge__checklist-item">
+							<Gridicon icon="checkmark" className="concierge-session-nudge__checklist-item-icon" />
+							<span className="concierge-session-nudge__checklist-item-text">
+								<b>Site building tools:</b> Learn how to create a site you're proud to share.
+							</span>
+						</li>
+						<li className="concierge-session-nudge__checklist-item">
+							<Gridicon icon="checkmark" className="concierge-session-nudge__checklist-item-icon" />
+							<span className="concierge-session-nudge__checklist-item-text">
+								<b>Content:</b> What information to include and where it should go.
+							</span>
+						</li>
+						<li className="concierge-session-nudge__checklist-item">
+							<Gridicon icon="checkmark" className="concierge-session-nudge__checklist-item-icon" />
+							<span className="concierge-session-nudge__checklist-item-text">
+								<b>And more:</b> Tell our experts what you'd like to cover.
+							</span>
+						</li>
+					</ul>
+				</p>
+
+				<h4 className="concierge-session-nudge__sub-header">
+					Reserve a 45-minute "Quick Start" appointment, and save $50 if you sign up today.
+				</h4>
+
+				<p>
+					<b>
+						Book your call today for just <del>$149</del> $99.
+					</b>{' '}
+					Click the button below to confirm your purchase.
+				</p>
+			</Fragment>
+		);
 	}
 
 	footer() {
@@ -65,14 +133,14 @@ export class ConciergeSessionNudge extends React.Component {
 					className="concierge-session-nudge__decline-offer-button"
 					onClick={ this.handleClickDecline }
 				>
-					No, thank you.
+					Skip
 				</Button>
 				<Button
 					primary
 					className="concierge-session-nudge__accept-offer-button"
 					onClick={ this.handleClickAccept }
 				>
-					Yes, please.
+					Reserve a call for $99
 				</Button>
 			</footer>
 		);
