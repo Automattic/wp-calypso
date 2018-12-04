9c68f9585f2d4c0a5c6f523cf7d5692fe5164a8a
Jarda Snajdr <jsnajdr@gmail.com>
2018-12-04 18:50:47 +0000

    Simplify the fetching of countries list data
    When fetching the various flavors of countries list (domains, payments, SMS codes), we
    don't need the `isFetching` flag to avoid sending duplicate request. The data layer's
    deduplication feature does that for us. In this PR, I'm removing the `isFetching` flag
    and all the related code: reducer, extra code in `QueryCountries` component, the
    `areCountriesFetching` selector.
    
    The state structure changes from `state.countries.domains.{items,isFetching}` to just
    `state.countries.domains`. We don't need extra nesting, the list is now the only thing
    we track now. Affects the reducer and the `getCountries` selector.

--- client/components/data/query-countries/payments.jsx
+++ client/components/data/query-countries/payments.jsx
@@ -8,15 +8,10 @@ import { connect } from 'react-redux';
 /**
  * Internal dependencies
  */
-import areCountriesFetching from 'state/selectors/are-countries-fetching';
 import QueryCountries from 'components/data/query-countries';
 import { fetchPaymentCountries } from 'state/countries/actions';
 
 export default connect(
-	state => ( {
-		isRequesting: areCountriesFetching( state, 'payments' ),
-	} ),
-	{
-		requestCountries: fetchPaymentCountries,
-	}
+	null,
+	{ requestCountries: fetchPaymentCountries }
 )( QueryCountries );
