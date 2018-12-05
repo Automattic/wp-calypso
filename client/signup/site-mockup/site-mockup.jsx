6b6d3a92ad0eb41c1437f069cf5a471febc1cc7e
Matt Wiebe <wiebe@automattic.com>
2018-12-04 19:12:48 -0600

    show outlines before a valid vertical is selected

--- client/signup/site-mockup/site-mockup.jsx
+++ client/signup/site-mockup/site-mockup.jsx
@@ -4,6 +4,7 @@
  */
 import React from 'react';
 import classNames from 'classnames';
+import { isEmpty } from 'lodash';
 
 /**
  * Internal dependencies
@@ -42,40 +43,70 @@ function MockupChromeDesktop() {
 	);
 }
 
+function SiteMockupContent( { data, title, tagline } ) {
+	/* eslint-disable react/no-danger */
+	return (
+		<>
+			<div className="site-mockup__title">{ title }</div>
+			<div className="site-mockup__tagline">{ tagline }</div>
+			<div
+				className="site-mockup__cover-image"
+				style={ { backgroundImage: `url("${ data.cover_image }")` } }
+			>
+				<span>{ data.cover_image_text }</span>
+			</div>
+			<div
+				className="site-mockup__entry-content"
+				dangerouslySetInnerHTML={ { __html: data.content } }
+			/>
+			<div className="site-mockup__hr" />
+			<div className="site-mockup__h2">Send Us a Message</div>
+			<div className="site-mockup__contact-form">
+				<div className="site-mockup__label">Name</div>
+				<div className="site-mockup__input" />
+				<div className="site-mockup__label">Email</div>
+				<div className="site-mockup__input" />
+				<div className="site-mockup__label">Your Message</div>
+				<div className="site-mockup__textarea" />
+				<div className="site-mockup__button">Send</div>
+			</div>
+		</>
+	);
+	/* eslint-enable react/no-danger */
+}
+
+function SiteMockupOutlines() {
+	return (
+		<>
+			<div className="site-mockup__outline is-title" />
+			<div className="site-mockup__outline" />
+			<div className="site-mockup__columns">
+				<div className="site-mockup__column">
+					<div className="site-mockup__outline" />
+				</div>
+				<div className="site-mockup__column">
+					<div className="site-mockup__outline" />
+				</div>
+			</div>
+			<div className="site-mockup__outline" />
+		</>
+	);
+}
+
 export default function SiteMockup( { size, data, title, tagline } ) {
 	const classes = classNames( 'site-mockup__viewport', `is-${ size }` );
-	/* eslint-disable react/no-danger */
 	return (
 		<div className={ classes }>
 			{ size === 'mobile' ? <MockupChromeMobile /> : <MockupChromeDesktop /> }
 			<div className="site-mockup__body">
 				<div className="site-mockup__content">
-					<div className="site-mockup__title">{ title }</div>
-					<div className="site-mockup__tagline">{ tagline }</div>
-					<div
-						className="site-mockup__cover-image"
-						style={ { backgroundImage: `url("${ data.cover_image }")` } }
-					>
-						<span>{ data.cover_image_text }</span>
-					</div>
-					<div
-						className="site-mockup__entry-content"
-						dangerouslySetInnerHTML={ { __html: data.content } }
-					/>
-					<div className="site-mockup__hr" />
-					<div className="site-mockup__h2">Send Us a Message</div>
-					<div className="site-mockup__contact-form">
-						<div className="site-mockup__label">Name</div>
-						<div className="site-mockup__input" />
-						<div className="site-mockup__label">Email</div>
-						<div className="site-mockup__input" />
-						<div className="site-mockup__label">Your Message</div>
-						<div className="site-mockup__textarea" />
-						<div className="site-mockup__button">Send</div>
-					</div>
+					{ isEmpty( data ) ? (
+						<SiteMockupOutlines />
+					) : (
+						<SiteMockupContent { ...{ data, title, tagline } } />
+					) }
 				</div>
 			</div>
 		</div>
 	);
-	/* eslint-enable react/no-danger */
 }
