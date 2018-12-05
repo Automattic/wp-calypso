7575a31b2affd08335f83e8302fa8954b2659f23
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2018-12-05 11:42:50 -0300

    Including missing constant export on get jetpack data

--- client/gutenberg/extensions/presets/jetpack/utils/get-jetpack-data.js
+++ client/gutenberg/extensions/presets/jetpack/utils/get-jetpack-data.js
@@ -4,7 +4,7 @@
  */
 import { get } from 'lodash';
 
-const JETPACK_DATA_PATH = [ 'Jetpack_Editor_Initial_State' ];
+export const JETPACK_DATA_PATH = [ 'Jetpack_Editor_Initial_State' ];
 
 export default function getJetpackData() {
 	return get( 'object' === typeof window ? window : null, JETPACK_DATA_PATH, null );
