79da38baa463601df171d61823d7a83aaa97e9c5
Dennis Snell <dennis.snell@automattic.com>
2018-12-06 10:22:27 -0700

    Editor: Wrap editor component in new async loader (#29166)
    We've been adding asynchronous tasks into the `page.js` middleware chain
    when loading the new block editor to make sure that things are in place
    before it renders. One example is translation data, another will soon be
    the Jetpack block availability data.
    
    I'm finding this flow a bit confusing and at least a little buggy as
    we're showing the editor in partial states somehow.
    
    In this PR I'm creating a new async wrapper that we can use to move
    these tasks into the component and out of the middleware. This should
    provide a side benefit of making sure that all flows into the editor
    behave in the same way and gives us more control over things like
    loading screen and failure screens.

--- client/gutenberg/editor/controller.js
+++ client/gutenberg/editor/controller.js
@@ -14,12 +14,12 @@ import { setLocaleData } from '@wordpress/i18n';
  */
 import getCurrentLocaleSlug from 'state/selectors/get-current-locale-slug';
 import { getCurrentUserId } from 'state/current-user/selectors';
-import { setAllSitesSelected } from 'state/ui/actions';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
 import { EDITOR_START } from 'state/action-types';
-import { initGutenberg } from './init';
 import { requestFromUrl } from 'state/data-getters';
 import { waitForData } from 'state/data-layer/http-data';
+import { asyncLoader } from './async-loader';
+import { Placeholder } from './placeholder';
 
 function determinePostType( context ) {
 	if ( context.path.startsWith( '/block-editor/post/' ) ) {
@@ -42,26 +42,20 @@ function getPostID( context ) {
 	return parseInt( context.params.post, 10 );
 }
 
-export const loadTranslations = ( context, next ) => {
-	const domains = [
-		{
-			name: 'default',
-			url: 'gutenberg',
-		},
-	];
-	if ( isEnabled( 'gutenberg/block/jetpack-preset' ) ) {
-		domains.push( {
-			name: 'jetpack',
-			url: 'jetpack-gutenberg-blocks',
-		} );
-	}
+export const loadTranslations = store => {
+	const domainDefault = { name: 'default', url: 'gutenberg' };
+	const domainJetpack = isEnabled( 'gutenberg/block/jetpack-preset' ) && {
+		name: 'jetpack',
+		url: 'jetpack-gutenberg-blocks',
+	};
+	const domains = domainJetpack ? [ domainDefault, domainJetpack ] : [ domainDefault ];
 
-	const state = context.store.getState();
+	const state = store.getState();
 	const localeSlug = getCurrentLocaleSlug( state );
 
 	// We don't need to localize English
 	if ( ! localeSlug || localeSlug === config( 'i18n_default_locale_slug' ) ) {
-		return next();
+		return Promise.resolve();
 	}
 
 	const query = domains.reduce( ( currentQuery, domain ) => {
@@ -73,12 +67,13 @@ export const loadTranslations = ( context, next ) => {
 		};
 	}, {} );
 
-	waitForData( query ).then( responses => {
+	return waitForData( query ).then( responses => {
 		Object.entries( responses ).forEach( ( [ domain, { state: requestState, data } ] ) => {
 			if ( requestState === 'failure' ) {
 				debug(
 					`Encountered an error loading locale file for domain ${ domain } and locale ${ localeSlug }. Falling back to English.`
 				);
+				return Promise.reject();
 			} else if ( data ) {
 				const localeData = {
 					'': {
@@ -90,26 +85,8 @@ export const loadTranslations = ( context, next ) => {
 				setLocaleData( localeData, domain );
 			}
 		} );
-
-		next();
 	} );
 };
-
-function waitForSelectedSiteId( context ) {
-	return new Promise( resolve => {
-		const unsubscribe = context.store.subscribe( () => {
-			const state = context.store.getState();
-			const siteId = getSelectedSiteId( state );
-			if ( ! siteId ) {
-				return;
-			}
-			unsubscribe();
-			resolve( siteId );
-		} );
-		// Trigger a `store.subscribe()` callback
-		context.store.dispatch( setAllSitesSelected() );
-	} );
-}
 
 export const post = async ( context, next ) => {
 	//see post-editor/controller.js for reference
@@ -119,23 +96,33 @@ export const post = async ( context, next ) => {
 	const postType = determinePostType( context );
 	const isDemoContent = ! postId && has( context.query, 'gutenberg-demo' );
 
-	let state = context.store.getState();
-	let siteId = getSelectedSiteId( state );
-	if ( ! siteId ) {
-		siteId = await waitForSelectedSiteId( context );
-		state = context.store.getState();
-	}
-	const siteSlug = getSelectedSiteSlug( state );
-	const userId = getCurrentUserId( state );
+	const makeEditor = import( './init' ).then( ( { initGutenberg } ) => {
+		const state = context.store.getState();
+		const siteId = getSelectedSiteId( state );
+		const siteSlug = getSelectedSiteSlug( state );
+		const userId = getCurrentUserId( state );
 
-	//set postId on state.ui.editor.postId, so components like editor revisions can read from it
-	context.store.dispatch( { type: EDITOR_START, siteId, postId } );
+		//set postId on state.ui.editor.postId, so components like editor revisions can read from it
+		context.store.dispatch( { type: EDITOR_START, siteId, postId } );
 
-	const GutenbergEditor = initGutenberg( userId, siteSlug );
+		const Editor = initGutenberg( userId, siteSlug );
 
-	context.primary = (
-		<GutenbergEditor { ...{ siteId, postId, postType, uniqueDraftKey, isDemoContent } } />
-	);
+		return props => (
+			<Editor { ...{ siteId, postId, postType, uniqueDraftKey, isDemoContent, ...props } } />
+		);
+	} );
+
+	const EditorLoader = asyncLoader( {
+		promises: {
+			Editor: makeEditor,
+			translations: loadTranslations( context.store ),
+		},
+		loading: () => <Placeholder />,
+		success: ( { Editor } ) => <Editor />,
+		failure: () => <div>Couldn't load everything - try hitting reload in your browser…</div>,
+	} );
+
+	context.primary = <EditorLoader />;
 
 	next();
 };
