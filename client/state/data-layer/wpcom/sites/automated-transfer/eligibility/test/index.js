62a25981e2b1aaa4feb920387537b927e0cd31e1
Markus Dobmann <markus@mpnjs.com>
2018-12-06 17:32:12 +0000

    Use dispatchRequestEx for AT eligibility (#28097)
    * Use dispatchRequestEx for AT eligibility
    
    * drop chai in favor of jest
    
    * replace 'requestThrowError' with noop

--- client/state/data-layer/wpcom/sites/automated-transfer/eligibility/test/index.js
+++ client/state/data-layer/wpcom/sites/automated-transfer/eligibility/test/index.js
@@ -1,10 +1,7 @@
 /** @format */
-
 /**
  * External dependencies
  */
-import { expect } from 'chai';
-import sinon from 'sinon';
 
 /**
  * Internal dependencies
@@ -12,38 +9,33 @@ import sinon from 'sinon';
 import {
 	requestAutomatedTransferEligibility,
 	updateAutomatedTransferEligibility,
-	throwRequestError,
 } from 'state/data-layer/wpcom/sites/automated-transfer/eligibility';
+import { http } from 'state/data-layer/wpcom-http/actions';
 
 describe( 'requestAutomatedTransferEligibility', () => {
 	test( 'should dispatch an http request', () => {
-		const dispatch = sinon.spy();
 		const siteId = 2916284;
-		requestAutomatedTransferEligibility( { dispatch }, { siteId } );
-		expect( dispatch ).to.have.been.calledWithMatch( {
-			method: 'GET',
-			path: `/sites/${ siteId }/automated-transfers/eligibility`,
-		} );
+		const result = requestAutomatedTransferEligibility( { siteId } );
+		expect( result ).toEqual(
+			http(
+				{
+					method: 'GET',
+					path: `/sites/${ siteId }/automated-transfers/eligibility`,
+					apiVersion: '1',
+				},
+				{ siteId }
+			)
+		);
 	} );
 } );
 
 describe( 'updateAutomatedTransferEligibility', () => {
 	test( 'should dispatch an update eligibility action ', () => {
-		const dispatch = sinon.spy();
 		const action = { type: 'AUTOMATED_TRANSFER_ELIGIBILITY_REQUEST', siteId: 2916284 };
-		updateAutomatedTransferEligibility( { dispatch }, action, { warnings: {}, errors: [] } );
-		expect( dispatch ).to.have.been.calledWith(
-			sinon.match( { type: 'AUTOMATED_TRANSFER_ELIGIBILITY_UPDATE', siteId: 2916284 } )
-		);
-	} );
-} );
-
-// TODO: Find out why we're throwing
-describe( 'throwRequestError', () => {
-	test( 'should throw an error', () => {
-		const testError = () => {
-			throwRequestError( {}, {}, {} );
-		};
-		expect( testError ).to.throw();
+		const result = updateAutomatedTransferEligibility( action, { warnings: {}, errors: [] } );
+		expect( result ).toMatchObject( {
+			type: 'AUTOMATED_TRANSFER_ELIGIBILITY_UPDATE',
+			siteId: 2916284,
+		} );
 	} );
 } );
