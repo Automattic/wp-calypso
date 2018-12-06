d96c4b6759f4b460173639becc55bcc2403433a9
Adam Zieliński <adam@adamziel.com>
2018-12-06 13:26:42 +0100

    Add doodle illustration to the upsell message

--- client/my-sites/checkout/concierge-session-nudge/index.jsx
+++ client/my-sites/checkout/concierge-session-nudge/index.jsx
@@ -64,65 +64,91 @@ export class ConciergeSessionNudge extends React.Component {
 	body() {
 		return (
 			<Fragment>
-				<h4 className="concierge-session-nudge__sub-header">
-					Want to create a site that looks great, gets traffic, and makes money?
-				</h4>
+				<div className="concierge-session-nudge__column-pane">
+					<div className="concierge-session-nudge__column-content">
+						<h4 className="concierge-session-nudge__sub-header">
+							Want to create a site that looks great, gets traffic, and makes money?
+						</h4>
 
-				<p>
-					<b>
-						Reserve a 45-minute one-on-one call with a website expert to help you get started on the
-						right foot.
-					</b>
-				</p>
+						<p>
+							<b>
+								Reserve a 45-minute one-on-one call with a website expert to help you get started on
+								the right foot.
+							</b>
+						</p>
 
-				<p>What our team of experts can help you with:</p>
+						<p>What our team of experts can help you with:</p>
 
-				<p>
-					<ul className="concierge-session-nudge__checklist">
-						<li className="concierge-session-nudge__checklist-item">
-							<Gridicon icon="checkmark" className="concierge-session-nudge__checklist-item-icon" />
-							<span className="concierge-session-nudge__checklist-item-text">
-								<b>Design:</b> Which template to choose.
-							</span>
-						</li>
-						<li className="concierge-session-nudge__checklist-item">
-							<Gridicon icon="checkmark" className="concierge-session-nudge__checklist-item-icon" />
-							<span className="concierge-session-nudge__checklist-item-text">
-								<b>Traffic:</b> How to get free search engine traffic with SEO tools.
-							</span>
-						</li>
-						<li className="concierge-session-nudge__checklist-item">
-							<Gridicon icon="checkmark" className="concierge-session-nudge__checklist-item-icon" />
-							<span className="concierge-session-nudge__checklist-item-text">
-								<b>Site building tools:</b> Learn how to create a site you're proud to share.
-							</span>
-						</li>
-						<li className="concierge-session-nudge__checklist-item">
-							<Gridicon icon="checkmark" className="concierge-session-nudge__checklist-item-icon" />
-							<span className="concierge-session-nudge__checklist-item-text">
-								<b>Content:</b> What information to include and where it should go.
-							</span>
-						</li>
-						<li className="concierge-session-nudge__checklist-item">
-							<Gridicon icon="checkmark" className="concierge-session-nudge__checklist-item-icon" />
-							<span className="concierge-session-nudge__checklist-item-text">
-								<b>And more:</b> Tell our experts what you'd like to cover.
-							</span>
-						</li>
-					</ul>
-				</p>
+						<p>
+							<ul className="concierge-session-nudge__checklist">
+								<li className="concierge-session-nudge__checklist-item">
+									<Gridicon
+										icon="checkmark"
+										className="concierge-session-nudge__checklist-item-icon"
+									/>
+									<span className="concierge-session-nudge__checklist-item-text">
+										<b>Design:</b> Which template to choose.
+									</span>
+								</li>
+								<li className="concierge-session-nudge__checklist-item">
+									<Gridicon
+										icon="checkmark"
+										className="concierge-session-nudge__checklist-item-icon"
+									/>
+									<span className="concierge-session-nudge__checklist-item-text">
+										<b>Traffic:</b> How to get free search engine traffic with SEO tools.
+									</span>
+								</li>
+								<li className="concierge-session-nudge__checklist-item">
+									<Gridicon
+										icon="checkmark"
+										className="concierge-session-nudge__checklist-item-icon"
+									/>
+									<span className="concierge-session-nudge__checklist-item-text">
+										<b>Site building tools:</b> Learn how to create a site you're proud to share.
+									</span>
+								</li>
+								<li className="concierge-session-nudge__checklist-item">
+									<Gridicon
+										icon="checkmark"
+										className="concierge-session-nudge__checklist-item-icon"
+									/>
+									<span className="concierge-session-nudge__checklist-item-text">
+										<b>Content:</b> What information to include and where it should go.
+									</span>
+								</li>
+								<li className="concierge-session-nudge__checklist-item">
+									<Gridicon
+										icon="checkmark"
+										className="concierge-session-nudge__checklist-item-icon"
+									/>
+									<span className="concierge-session-nudge__checklist-item-text">
+										<b>And more:</b> Tell our experts what you'd like to cover.
+									</span>
+								</li>
+							</ul>
+						</p>
 
-				<h4 className="concierge-session-nudge__sub-header">
-					Reserve a 45-minute &ldquo;Quick Start&rdquo; appointment, and save $50 if you sign up
-					today.
-				</h4>
+						<h4 className="concierge-session-nudge__sub-header">
+							Reserve a 45-minute &ldquo;Quick Start&rdquo; appointment, and save $50 if you sign up
+							today.
+						</h4>
 
-				<p>
-					<b>
-						Book your call today for just <del>$149</del> $99.
-					</b>{' '}
-					Click the button below to confirm your purchase.
-				</p>
+						<p>
+							<b>
+								Book your call today for just <del>$149</del> $99.
+							</b>{' '}
+							Click the button below to confirm your purchase.
+						</p>
+					</div>
+					<div className="concierge-session-nudge__column-doodle">
+						<img
+							className="concierge-session-nudge__doodle"
+							alt=""
+							src="/calypso/images/illustrations/support.svg"
+						/>
+					</div>
+				</div>
 			</Fragment>
 		);
 	}
