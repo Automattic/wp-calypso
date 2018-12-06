1cf98770fa00979d1696001daa8562211ba4a6e7
Andrija Vucinic <andrija.vucinic@gmail.com>
2018-12-06 20:30:03 +0100

    Update active-tests.js

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -82,7 +82,7 @@ export default {
 		defaultVariation: 'public',
 	},
 	krackenM5NonEnDomainSuggestions: {
-		datestamp: '20181210',
+		datestamp: '20181207',
 		variations: {
 			domainsbot_front: 50,
 			variation2_front: 50,
