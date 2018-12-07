eba9259f9ead04f63d86cc98c63a9054a778d575
ramonjd <ramonjd@gmail.com>
2018-12-07 14:51:11 +1100

    Added reducer to bundle. Fixed translate function typo. Added default theme slug to component state.

--- client/state/signup/steps/reducer.js
+++ client/state/signup/steps/reducer.js
@@ -10,17 +10,19 @@ import siteTopic from './site-topic/reducer';
 import siteInformation from './site-information/reducer';
 import siteGoals from './site-goals/reducer';
 import userExperience from './user-experience/reducer';
+import siteStyle from './site-style/reducer';
 import siteType from './site-type/reducer';
 import { combineReducers } from 'state/utils';
 import survey from './survey/reducer';
 
 export default combineReducers( {
 	designType,
-	siteTitle,
-	siteTopic,
 	siteInformation,
 	siteGoals,
-	userExperience,
+	siteStyle,
+	siteTitle,
+	siteTopic,
 	siteType,
 	survey,
+	userExperience,
 } );
