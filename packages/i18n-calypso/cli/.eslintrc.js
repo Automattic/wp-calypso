7bfea7875cce4455fcb189cfee5ce9445976bcf6
Jon Surrell <jon.surrell@automattic.com>
2018-12-10 12:21:08 +0100

    Drop extraneous eslint extends

--- packages/i18n-calypso/cli/.eslintrc.js
+++ packages/i18n-calypso/cli/.eslintrc.js
@@ -1,7 +1,6 @@
 /** @format */
 
 module.exports = {
-	extends: '../.eslintrc.js',
 	rules: {
 		'import/no-nodejs-modules': 0,
 	},
--- packages/tree-select/.eslintrc.js
+++ packages/tree-select/.eslintrc.js
@@ -1,7 +1,6 @@
 /** @format */
 
 module.exports = {
-	extends: '../../.eslintrc.js',
 	rules: {
 		'import/no-extraneous-dependencies': [ 'error', { packageDir: __dirname } ],
 	},
