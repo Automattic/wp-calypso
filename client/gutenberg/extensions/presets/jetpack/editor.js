3d8b5b152c63001d84d74fa6eac72daf50b5b7f3
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2018-12-11 17:15:40 +0200

    Gutenberg: Load Jetpack beta blocks under a feature flag (#29308)
    * Config: Add feature flag for Jetpack beta blocks
    
    * Gutenberg: Load Jetpack beta blocks under a feature flag
    
    * Properly handle false flags

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -12,7 +12,11 @@ import * as ContactFormBlock from 'gutenberg/extensions/contact-form';
 import * as MarkdownBlock from 'gutenberg/extensions/markdown';
 import * as MapBlock from 'gutenberg/extensions/map';
 import * as PublicizeBlock from 'gutenberg/extensions/publicize';
+import * as RelatedPostsBlock from 'gutenberg/extensions/related-posts';
 import * as SimplePaymentsBlock from 'gutenberg/extensions/simple-payments';
+import * as TiledGalleryBlock from 'gutenberg/extensions/tiled-gallery';
+import * as VRBlock from 'gutenberg/extensions/vr';
+import { isEnabled } from 'config';
 
 export default [
 	{ name: ContactFormBlock.name, settings: ContactFormBlock.settings },
@@ -21,4 +25,7 @@ export default [
 	MapBlock,
 	PublicizeBlock,
 	SimplePaymentsBlock,
+	...( isEnabled( 'jetpack/blocks/beta' )
+		? [ RelatedPostsBlock, TiledGalleryBlock, VRBlock ]
+		: [] ),
 ];
