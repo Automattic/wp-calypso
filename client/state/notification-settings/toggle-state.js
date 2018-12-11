f746081f20b237424bf95b9c2a54c125e727a1a5
Spen <spen_@hotmail.co.uk>
2018-12-11 13:21:52 +0000

    NotificationsSettings: add and use replaceAtIndex util

--- client/state/notification-settings/toggle-state.js
+++ client/state/notification-settings/toggle-state.js
@@ -2,7 +2,14 @@
 /**
  * Internal dependencies
  */
-import { find, get, without } from 'lodash';
+import { find, get, findIndex } from 'lodash';
+
+const replaceAtIndex = ( array, index, item ) => {
+	const newArray = array.slice();
+	newArray.splice( index, 1, item );
+
+	return newArray;
+};
 
 const toggleInStream = ( streamName, stream, setting ) => ( {
 	[ streamName ]: {
@@ -13,16 +20,14 @@ const toggleInStream = ( streamName, stream, setting ) => ( {
 
 const toggleInDevice = ( devices, deviceId, setting ) => {
 	const device = find( devices, { device_id: parseInt( deviceId, 10 ) } );
+	const deviceIndex = findIndex( devices, device );
 	const deviceSetting = get( device, setting );
 
 	return {
-		devices: [
-			...without( devices, device ),
-			{
-				...device,
-				[ setting ]: ! deviceSetting,
-			},
-		],
+		devices: replaceAtIndex( devices, deviceIndex, {
+			...device,
+			[ setting ]: ! deviceSetting,
+		} ),
 	};
 };
 
@@ -47,18 +52,16 @@ export default {
 	blog( state, source, stream, setting ) {
 		const blogs = get( state, 'dirty.blogs' );
 		const blog = find( blogs, { blog_id: parseInt( source, 10 ) } );
+		const blogIndex = findIndex( blogs, blog );
 		const devices = get( blog, 'devices', [] );
 
 		return {
-			blogs: [
-				...without( blogs, blog ),
-				{
-					...blog,
-					...( isNaN( stream )
-						? toggleInStream( stream, get( blog, stream ), setting )
-						: toggleInDevice( devices, stream, setting ) ),
-				},
-			],
+			blogs: replaceAtIndex( blogs, blogIndex, {
+				...blog,
+				...( isNaN( stream )
+					? toggleInStream( stream, get( blog, stream ), setting )
+					: toggleInDevice( devices, stream, setting ) ),
+			} ),
 		};
 	},
 };
