af061994c8eb927ccdd0e12ff00867b4d70c77c7
Bernie Reiter <ockham@raz.or.at>
2018-12-11 12:39:08 +0100

    Functions args typo

--- client/gutenberg/extensions/contact-form/editor.js
+++ client/gutenberg/extensions/contact-form/editor.js
@@ -7,4 +7,4 @@ import * as form from './index';
 import registerJetpackBlock from 'gutenberg/extensions/presets/jetpack/utils/register-jetpack-block';
 
 registerJetpackBlock( form.name, form.settings );
-form.fields.map( field => registerJetpackBlock( field.name.field.settings ) );
+form.fields.map( field => registerJetpackBlock( field.name, field.settings ) );
