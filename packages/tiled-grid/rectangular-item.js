5c6639b76a3922f353461af72ce1346a73ec97fd
Mikael Korpela <mikael@ihminen.org>
2018-12-12 19:40:39 +0200

    Add more to Item class

--- packages/tiled-grid/rectangular-item.js
+++ packages/tiled-grid/rectangular-item.js
@@ -1,6 +1,13 @@
 /** @format */
-/* eslint-disable */
 
-export class Jetpack_Tiled_Gallery_Rectangular_Item {
-	constructor( image, needs_attachment_link, grayscale ) {}
+/**
+ * Internal dependencies
+ */
+import { Jetpack_Tiled_Gallery_Item } from './item';
+
+export class Jetpack_Tiled_Gallery_Rectangular_Item extends Jetpack_Tiled_Gallery_Item {
+	// eslint-disable-next-line no-unused-vars
+	constructor( image, needs_attachment_link, grayscale ) {
+		super();
+	}
 }
