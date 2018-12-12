8a76d8731f6a42293065ea18a5de7adb8d44be13
Mikael Korpela <mikael@ihminen.org>
2018-12-12 22:58:13 +0200

    Add `size` to `Jetpack_Tiled_Gallery_Rectangular_Item`

--- packages/tiled-grid/item.js
+++ packages/tiled-grid/item.js
@@ -2,5 +2,7 @@
 
 export class Jetpack_Tiled_Gallery_Item {
 	// eslint-disable-next-line no-unused-vars
-	constructor( attachment_image, needs_attachment_link, grayscale ) {}
+	constructor( attachment_image, needs_attachment_link, grayscale ) {
+		this.image = attachment_image;
+	}
 }
