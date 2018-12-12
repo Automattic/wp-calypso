65f2986e6f3fff09bc0cf005fbc47e95f42e59b0
Mikael Korpela <mikael@ihminen.org>
2018-12-12 19:13:37 +0200

    Add deeper object logging

--- packages/tiled-grid/index.js
+++ packages/tiled-grid/index.js
@@ -1,21 +1,29 @@
 /** @format */
-/* eslint-disable */
 
 /*
  * Mosaic layout
  *
- * Implements `Jetpack_Tiled_Gallery_Layout_Rectangular`
- *
- * from modules/tiled-gallery/tiled-gallery/tiled-gallery-rectangular.php
- *
+ * Implements `Jetpack_Tiled_Gallery_Layout_Rectangular` from:
+ * https://github.com/Automattic/jetpack/blob/0d837791212dcfab0e75145a21857cc507e4c9d3/modules/tiled-gallery/tiled-gallery/tiled-gallery-rectangular.php
  */
 
+/**
+ * External dependencies
+ */
+// eslint-disable-next-line import/no-nodejs-modules
+import { inspect } from 'util';
+
+/**
+ * Internal dependencies
+ */
 import { attachments } from './attachments';
 import { Jetpack_Tiled_Gallery_Grouper } from './grouper';
-import { Jetpack_Tiled_Gallery_Shape } from './shapes/super';
 
 const grouper = new Jetpack_Tiled_Gallery_Grouper( attachments );
 
 //Jetpack_Tiled_Gallery_Shape::reset_last_shape();
 
-console.log( 'Rows:', grouper.grouped_images );
+const output = inspect( grouper.grouped_images, { showHidden: false, depth: null } );
+
+// eslint-disable-next-line no-console
+console.log( 'Rows:', output );
