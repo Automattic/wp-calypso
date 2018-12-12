23ca0fe03a6105b7657a5f0abfb858089ed2c729
Mikael Korpela <mikael@ihminen.org>
2018-12-12 13:15:44 +0200

    More code & fixes

--- packages/tiled-grid/grouper.js
+++ packages/tiled-grid/grouper.js
@@ -41,8 +41,42 @@ export class Jetpack_Tiled_Gallery_Grouper {
 		}
 	}
 
+	get_current_row_size = () => {
+		/*
+		$images_left = count( $this->images );
+		if ( $images_left < 3 ) {
+			return array_fill( 0, $images_left, 1 );
+		}
+
+		foreach ( $this->shapes as $shape_name ) {
+			$class_name = "Jetpack_Tiled_Gallery_$shape_name";
+			$shape      = new $class_name( $this->images );
+			if ( $shape->is_possible() ) {
+				Jetpack_Tiled_Gallery_Shape::set_last_shape( $class_name );
+				return $shape->shape;
+			}
+		}
+
+		Jetpack_Tiled_Gallery_Shape::set_last_shape( 'Two' );
+		return array( 1, 1 );
+		*/
+		return [ 1, 1 ];
+	}
+
 	get_images_with_sizes = ( attachments ) => {
-		return attachments;
+		let images_with_sizes = [];
+
+		for ( const image of attachments ) {
+			// Attachments now already include all the meta we need
+			// meta = wp_get_attachment_metadata( $image->ID );
+			image.width_orig   = image.width && image.width > 0 ? image.width : 1;
+			image.height_orig  = image.height && image.height > 0 ? image.height : 1;
+			image.ratio        = image.width_orig / image.height_orig;
+			image.ratio        = image.ratio ? image.ratio : 1;
+			images_with_sizes.push( image );
+		}
+
+		return images_with_sizes;
 	}
 
 
@@ -50,7 +84,7 @@ export class Jetpack_Tiled_Gallery_Grouper {
 		const vector = this.get_current_row_size();
 
 		let row = [];
-		for ( group_size of vector ) {
+		for ( const group_size of vector ) {
 			row.push(
 				new Jetpack_Tiled_Gallery_Group(
 					this.images.slice( 0, group_size )
@@ -63,16 +97,18 @@ export class Jetpack_Tiled_Gallery_Grouper {
 
 	get_grouped_images = () => {
 		let grouped_images = [];
-		while ( ! this.images.length ) {
+		// @todo change to while() if we're reducing `images` somewhere
+		this.images.forEach( () => {
+		// while ( ! empty( $this->images ) ) {
 			grouped_images.push(
 				new Jetpack_Tiled_Gallery_Row( this.read_row() )
 			);
-		}
+		} );
 		return grouped_images;
 	}
 
 	apply_content_width = ( width ) => {
-		for ( row of this.grouped_images ) {
+		for ( const row of this.grouped_images ) {
 			row.width      = width;
 			row.raw_height = 1 / row.ratio * ( width - this.margin * ( row.groups.length - row.weighted_ratio ) );
 			row.height     = Math.round( row.raw_height );
@@ -84,44 +120,42 @@ export class Jetpack_Tiled_Gallery_Grouper {
 	calculate_group_sizes = ( row ) => {
 		// Storing the calculated group heights in an array for rounding them later while preserving their sum
 		// This fixes the rounding error that can lead to a few ugly pixels sticking out in the gallery
-
 		let group_widths_array = [];
-		for ( group of row.groups ) {
+		for ( const group of row.groups ) {
 			group.height = row.height;
 			// Storing the raw calculations in a separate property to prevent
 			// rounding errors from cascading down and for diagnostics
 			group.raw_width = ( row.raw_height - this.margin * group.images.length ) * group.ratio + this.margin;
 			group_widths_array.push( group.raw_width );
 		}
-
 		let rounded_group_widths_array = get_rounded_constrained_array( group_widths_array, row.width );
 
-		/*
-		foreach ( $row->groups as $group ) {
-			$group->width = array_shift( $rounded_group_widths_array );
-			$this->calculate_image_sizes( $group );
+		for ( const group of row.groups ) {
+			// @TODO check what happens to `rounded_image_heights_array`
+			// in PHP `array_shift([])` vs JS `[].shift()` and if it has affects here
+			group.width = rounded_group_widths_array.shift();
+			this.calculate_image_sizes( group );
 		}
-		*/
 	}
 
 	calculate_image_sizes = ( group ) => {
-		/*
 		// Storing the calculated image heights in an array for rounding them later while preserving their sum
 		// This fixes the rounding error that can lead to a few ugly pixels sticking out in the gallery
-		$image_heights_array = array();
-		foreach ( $group->images as $image ) {
-			$image->width = $group->width - $this->margin;
+		let image_heights_array = [];
+		for ( const image of group.images ) {
+			image.width = group.width - this.margin;
 			// Storing the raw calculations in a separate property for diagnostics
-			$image->raw_height     = ( $group->raw_width - $this->margin ) / $image->ratio;
-			$image_heights_array[] = $image->raw_height;
+			image.raw_height = ( group.raw_width - this.margin ) / image.ratio;
+			image_heights_array.push( image.raw_height );
 		}
 
-		$image_height_sum            = $group->height - count( $image_heights_array ) * $this->margin;
-		$rounded_image_heights_array = get_rounded_constrained_array( $image_heights_array, $image_height_sum );
+		const image_height_sum = group.height - image_heights_array.length * this.margin;
+		let rounded_image_heights_array = get_rounded_constrained_array( image_heights_array, image_height_sum );
 
-		foreach ( $group->images as $image ) {
-			$image->height = array_shift( $rounded_image_heights_array );
+		for ( const image of group.images ) {
+			// @TODO check what happens to `rounded_image_heights_array`
+			// in PHP `array_shift([])` vs JS `[].shift()` and if it has affects here
+			image.height = rounded_image_heights_array.shift();
 		}
-		*/
 	}
 };
