51bed961432753c434b0a31efc65033843c201b2
Michael Turk <turk.michael@gmail.com>
2018-12-13 15:19:11 -0500

    Adding shortcode attribute to suppress parts of subscription shortcode that aren't in design

--- client/gutenberg/extensions/subscriptions/save.jsx
+++ client/gutenberg/extensions/subscriptions/save.jsx
@@ -9,6 +9,6 @@ export default function Save( { attributes } ) {
 	const { show_subscribers_total } = attributes;
 	return (
 		<RawHTML
-		>{ `[jetpack_subscription_form show_subscribers_total="${ show_subscribers_total }"]` }</RawHTML>
+		>{ `[jetpack_subscription_form show_subscribers_total="${ show_subscribers_total }" show_only_email_and_button="true"]` }</RawHTML>
 	);
 }
