bd06df0699aa6e32f2789cbfc5d9bec2820b5733
Jon Surrell <jon.surrell@automattic.com>
2018-12-13 11:38:44 +0100

    Add comments, fix tests

--- packages/tiled-grid/constrained-array-rounding/test/index.js
+++ packages/tiled-grid/constrained-array-rounding/test/index.js
@@ -3,16 +3,16 @@
 /**
  * Internal dependencies
  */
-import { getRoundedConstrainedArray } from '../constrained-array-rounding';
+import { getRoundedConstrainedArray } from '..';
 
 describe( 'getRoundedConstrainedArray', () => {
 	test( 'Round floats to integers that fit the row', () => {
 		const rounded = getRoundedConstrainedArray( [ 427.33333333333337, 212.66666666666669 ], 640 );
-		expect( rounded ).toBe( [ 427, 213 ] );
+		expect( rounded ).toEqual( [ 427, 213 ] );
 	} );
 
 	test( 'No changes in integers that fit the row', () => {
 		const rounded = getRoundedConstrainedArray( [ 320, 320 ], 640 );
-		expect( rounded ).toBe( [ 320, 320 ] );
+		expect( rounded ).toEqual( [ 320, 320 ] );
 	} );
 } );
