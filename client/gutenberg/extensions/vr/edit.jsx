3b094495b490ee9585570ca613955b510cc3fece
Marin Atanasov <tyxla@abv.bg>
2018-12-13 11:54:11 +0200

    Gutenberg: Make VR block editable - take 2

--- client/gutenberg/extensions/vr/edit.jsx
+++ client/gutenberg/extensions/vr/edit.jsx
@@ -2,8 +2,17 @@
 /**
  * External dependencies
  */
-import { Component } from '@wordpress/element';
-import { Placeholder, SelectControl, TextControl } from '@wordpress/components';
+import { BlockControls } from '@wordpress/editor';
+import { Component, Fragment } from '@wordpress/element';
+import {
+	BaseControl,
+	Button,
+	IconButton,
+	Placeholder,
+	SelectControl,
+	TextControl,
+	Toolbar,
+} from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -12,41 +21,87 @@ import VRImageSave from './save';
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 
 export default class VRImageEdit extends Component {
-	onChangeUrl = value => void this.props.setAttributes( { url: value.trim() } );
-	onChangeView = value => void this.props.setAttributes( { view: value } );
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			isEditing: ! ( props.attributes.url && props.attributes.view ),
+		};
+	}
+
+	onEditClick = () => {
+		this.setState( { isEditing: true } );
+	};
+
+	onPreviewClick = () => {
+		this.setState( { isEditing: false } );
+	};
+
+	onChangeUrl = value => {
+		this.props.setAttributes( { url: value.trim() } );
+	};
+
+	onChangeView = value => {
+		this.props.setAttributes( { view: value } );
+	};
 
 	render() {
 		const { attributes, className } = this.props;
-
-		if ( attributes.url && attributes.view ) {
-			return <VRImageSave attributes={ attributes } className={ className } />;
-		}
+		const { isEditing } = this.state;
 
 		return (
-			<Placeholder
-				key="placeholder"
-				icon="format-image"
-				label={ __( 'VR Image' ) }
-				className={ className }
-			>
-				<TextControl
-					type="url"
-					label={ __( 'Enter URL to VR image' ) }
-					value={ attributes.url }
-					onChange={ this.onChangeUrl }
-				/>
-				<SelectControl
-					label={ __( 'View Type' ) }
-					disabled={ ! attributes.url }
-					value={ attributes.view }
-					onChange={ this.onChangeView }
-					options={ [
-						{ label: '', value: '' },
-						{ label: __( '360°' ), value: '360' },
-						{ label: __( 'Cinema' ), value: 'cinema' },
-					] }
-				/>
-			</Placeholder>
+			<Fragment>
+				<BlockControls>
+					<Toolbar>
+						{ ! isEditing && (
+							<IconButton
+								className="components-toolbar__control"
+								label={ __( 'Edit URL' ) }
+								icon="edit"
+								onClick={ this.onEditClick }
+							/>
+						) }
+					</Toolbar>
+				</BlockControls>
+
+				{ attributes.url && attributes.view && isEditing === false ? (
+					<VRImageSave attributes={ attributes } className={ className } />
+				) : (
+					<Placeholder
+						key="placeholder"
+						icon="format-image"
+						label={ __( 'VR Image' ) }
+						className={ className }
+					>
+						<TextControl
+							type="url"
+							label={ __( 'Enter URL to VR image' ) }
+							value={ attributes.url }
+							onChange={ this.onChangeUrl }
+						/>
+						<SelectControl
+							label={ __( 'View Type' ) }
+							value={ attributes.view }
+							onChange={ this.onChangeView }
+							options={ [
+								{ label: '', value: '' },
+								{ label: __( '360°' ), value: '360' },
+								{ label: __( 'Cinema' ), value: 'cinema' },
+							] }
+						/>
+						<BaseControl>
+							<Button
+								disabled={ ! attributes.url || ! attributes.view }
+								isLarge
+								onClick={ this.onPreviewClick }
+								type="submit"
+							>
+								{ __( 'Preview' ) }
+							</Button>
+						</BaseControl>
+					</Placeholder>
+				) }
+			</Fragment>
 		);
 	}
 }
