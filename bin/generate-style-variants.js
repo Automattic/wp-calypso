ad3684533523ab292b67ed0850096be9adbbba3d
Ben Lowery <ben.lowery@automattic.com>
2018-12-14 13:01:01 -0500

    fix generator and colors

--- bin/generate-style-variants.js
+++ bin/generate-style-variants.js
@@ -5,11 +5,11 @@ const [ basename, murielname ] = argv._;
 
 const steps = [ 0, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900 ];
 const variants = _.map( steps, variant => {
-	return `--${ basename }-${ variant }: #{ \$muriel-color-${ murielname }-${ variant } };`;
+	return `--${ basename }-${ variant }: #{ \$muriel-${ murielname }-${ variant } };`;
 } );
 
-variants.unshift( `--${ basename }-light: #{ \$muriel-color-${ murielname }-300 };` );
-variants.unshift( `--${ basename }-dark: #{ \$muriel-color-${ murielname }-700 };` );
-variants.unshift( `--${ basename }: #{ \$muriel-color-${ murielname }-500 };` );
+variants.unshift( `--${ basename }-light: #{ \$muriel-${ murielname }-300 };` );
+variants.unshift( `--${ basename }-dark: #{ \$muriel-${ murielname }-700 };` );
+variants.unshift( `--${ basename }: #{ \$muriel-${ murielname }-500 };` );
 
 variants.forEach( n => console.log( n ) );
