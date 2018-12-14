a5aadff2373cb7079c396f1ae7daebeae577fab2
Bernie Reiter <ockham@raz.or.at>
2018-12-14 14:51:03 +0100

    Gutenberg Jetpack Preset: Add async getExtensions() function

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -5,6 +5,7 @@
  */
 import './shared/public-path';
 import './editor-shared/block-category'; // Register the Jetpack category
+import extensionSlugs from './index.json';
 
 // TODO: Generate dyanmically from index.json
 // Appending `Block` to the names to keep `Map` from colliding with JS' Map
@@ -18,6 +19,14 @@ import * as SubscriptionsBlock from 'gutenberg/extensions/subscriptions';
 import * as TiledGalleryBlock from 'gutenberg/extensions/tiled-gallery';
 import * as VRBlock from 'gutenberg/extensions/vr';
 import { isEnabled } from 'config';
+
+export async function getExtensions() {
+	const promises = extensionSlugs.production.map( slug =>
+		import( '../../' + slug ).then( ( { name, settings } ) => ( { name, settings } ) )
+	);
+
+	return await Promise.all( promises ); // Y U NO RETURN ARRAY BUT PROMISE?
+}
 
 export default [
 	{ name: ContactFormBlock.name, settings: ContactFormBlock.settings },
