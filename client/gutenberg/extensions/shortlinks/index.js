6ea42c76ec98b2a058c472dbce85a2e813d337fb
Marin Atanasov <tyxla@abv.bg>
2018-12-15 14:46:56 +0200

    Gutenberg: Add WP.com shortlinks

---
+++ client/gutenberg/extensions/shortlinks/index.js
@@ -0,0 +1,17 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import JetpackPluginSidebar from 'gutenberg/extensions/presets/jetpack/editor-shared/jetpack-plugin-sidebar';
+import ShortlinksPanel from './panel';
+
+export const name = 'shortlinks';
+
+export const settings = {
+	render: () => (
+		<JetpackPluginSidebar>
+			<ShortlinksPanel />
+		</JetpackPluginSidebar>
+	),
+};
