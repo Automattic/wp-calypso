8207c63ad0462d29e3e1794e2c27946e8adc46cb
Jon Surrell <jon.surrell@automattic.com>
2018-12-17 15:25:32 +0100

    Comment debugging output

--- packages/tiled-grid/index.js
+++ packages/tiled-grid/index.js
@@ -7,17 +7,13 @@
  * https://github.com/Automattic/jetpack/blob/0d837791212dcfab0e75145a21857cc507e4c9d3/modules/tiled-gallery/tiled-gallery/tiled-gallery-rectangular.php
  */
 
-/**
- * External dependencies
- */
-// eslint-disable-next-line import/no-nodejs-modules
-import { inspect } from 'util';
-import { cloneDeep } from 'lodash';
+// import { inspect } from 'util';
+// import { cloneDeep } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import { attachments } from './test/fixtures/attachments';
+// import { attachments } from './test/fixtures/attachments';
 import { Jetpack_Tiled_Gallery_Grouper } from './grouper';
 import { Jetpack_Tiled_Gallery_Shape } from './shapes/jetpack-tiled-gallery-shape';
 import { TILE_MARGIN, CONTENT_WIDTH } from './constants.js';
@@ -37,6 +33,6 @@ export const rectangularLayout = ( {
 };
 
 // For debugging while building this
-const layout = rectangularLayout( { images: cloneDeep( attachments ) } );
+// const layout = rectangularLayout( { images: cloneDeep( attachments ) } );
 // eslint-disable-next-line no-console
-console.log( inspect( layout, { showHidden: false, depth: null } ) );
+// console.log( inspect( layout, { showHidden: false, depth: null } ) );
