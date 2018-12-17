0e27d40302c4dd44902654a609cc8eedd170ad45
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-12-17 09:48:35 -0800

    Update the icons

--- client/gutenberg/extensions/address/index.js
+++ client/gutenberg/extensions/address/index.js
@@ -3,7 +3,8 @@
  * External dependencies
  */
 import { sprintf } from '@wordpress/i18n';
-import { Path } from '@wordpress/components';
+import { Path, Circle } from '@wordpress/components';
+import { Fragment } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -58,7 +59,10 @@ export const name = 'address';
 export const settings = {
 	title: 'Address',
 	icon: renderMaterialIcon(
-		<Path d="M22 3H2C.9 3 0 3.9 0 5v14c0 1.1.9 2 2 2h20c1.1 0 1.99-.9 1.99-2L24 5c0-1.1-.9-2-2-2zm0 16H2V5h20v14zm-2.99-1.01L21 16l-1.51-2h-1.64c-.22-.63-.35-1.3-.35-2s.13-1.37.35-2h1.64L21 8l-1.99-1.99c-1.31.98-2.28 2.37-2.73 3.99-.18.64-.28 1.31-.28 2s.1 1.36.28 2c.45 1.61 1.42 3.01 2.73 3.99zM9 12c1.65 0 3-1.35 3-3s-1.35-3-3-3-3 1.35-3 3 1.35 3 3 3zm0-4c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm6 8.59c0-2.5-3.97-3.58-6-3.58s-6 1.08-6 3.58V18h12v-1.41zM5.48 16c.74-.5 2.22-1 3.52-1s2.77.49 3.52 1H5.48z" />
+		<Fragment>
+			<Path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zM7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 2.88-2.88 7.19-5 9.88C9.92 16.21 7 11.85 7 9z" />
+			<Circle cx="12" cy="9" r="2.5" />
+		</Fragment>
 	),
 	category: 'jetpack',
 	attributes,
