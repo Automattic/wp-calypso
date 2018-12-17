269b31c4c30825e6c6f289d203e4260712b8a565
Rocco Tripaldi <tripaldir@gmail.com>
2018-12-17 17:37:53 -0500

    removing an unneeded file/method

--- client/gutenberg/extensions/presets/jetpack/utils/get-localized-week.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-/**
- * External Dependencies
- */
-import apiFetch from '@wordpress/api-fetch';
-
-export default function getLocalizedWeek() {
-	return apiFetch( { path: '/wpcom/v2/business-hours/localized-week' } );
-}
