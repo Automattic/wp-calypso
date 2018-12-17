e6863f1b784dc1383a77dda204cbe9549e76d5f4
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-12-17 11:43:32 -0800

    Prevern Enter Key from creating new lines

--- client/gutenberg/extensions/address/edit.js
+++ client/gutenberg/extensions/address/edit.js
@@ -15,6 +15,15 @@ import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 class AddressEdit extends Component {
 	constructor( ...args ) {
 		super( ...args );
+
+		this.preventEnterKey = this.preventEnterKey.bind( this );
+	}
+
+	preventEnterKey( event ) {
+		if ( event.key === 'Enter' ) {
+			event.preventDefault();
+			return;
+		}
 	}
 
 	render() {
@@ -54,36 +63,43 @@ class AddressEdit extends Component {
 							value={ address }
 							placeholder={ __( 'Street Address' ) }
 							onChange={ newAddress => setAttributes( { address: newAddress } ) }
+							onKeyDown={ this.preventEnterKey }
 						/>
 						<PlainText
 							value={ address_line2 }
 							placeholder={ __( 'Address Line 2' ) }
 							onChange={ newAddressLine2 => setAttributes( { address_line2: newAddressLine2 } ) }
+							onKeyDown={ this.preventEnterKey }
 						/>
 						<PlainText
 							value={ address_line3 }
 							placeholder={ __( 'Address Line 3' ) }
 							onChange={ newAddressLine3 => setAttributes( { address_line3: newAddressLine3 } ) }
+							onKeyDown={ this.preventEnterKey }
 						/>
 						<PlainText
 							value={ city }
 							placeholder={ __( 'City' ) }
 							onChange={ newCity => setAttributes( { city: newCity } ) }
+							onKeyDown={ this.preventEnterKey }
 						/>
 						<PlainText
 							value={ region }
 							placeholder={ __( 'State/Province/Region' ) }
 							onChange={ newRegion => setAttributes( { region: newRegion } ) }
+							onKeyDown={ this.preventEnterKey }
 						/>
 						<PlainText
 							value={ postal }
 							placeholder={ __( 'Postal/Zip Code' ) }
 							onChange={ newPostal => setAttributes( { postal: newPostal } ) }
+							onKeyDown={ this.preventEnterKey }
 						/>
 						<PlainText
 							value={ country }
 							placeholder={ __( 'Country' ) }
 							onChange={ newCountry => setAttributes( { country: newCountry } ) }
+							onKeyDown={ this.preventEnterKey }
 						/>
 					</Fragment>
 				) }
