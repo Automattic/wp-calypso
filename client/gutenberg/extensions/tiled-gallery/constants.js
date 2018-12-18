d96bdaed775615195f20b91ad3a401213fa0fc3f
Jon Surrell <jon.surrell@automattic.com>
2018-12-18 22:42:19 +0100

    Sort layout styles

--- client/gutenberg/extensions/tiled-gallery/constants.js
+++ client/gutenberg/extensions/tiled-gallery/constants.js
@@ -11,21 +11,21 @@ export const DEFAULT_GALLERY_WIDTH = 580;
 export const DEFAULT_LAYOUT = 'rectangular';
 export const LAYOUT_STYLES = [
 	{
+		isDefault: true,
 		label: _x( 'Tiled mosaic', 'Tiled gallery layout' ),
 		name: 'rectangular',
-		isDefault: true,
 	},
 	{
-		label: _x( 'Tiled columns', 'Tiled gallery layout' ),
-		name: 'columns',
+		label: _x( 'Circles', 'Tiled gallery layout' ),
+		name: 'circle',
 	},
 	{
 		label: _x( 'Square tiles', 'Tiled gallery layout' ),
 		name: 'square',
 	},
 	{
-		label: _x( 'Circles', 'Tiled gallery layout' ),
-		name: 'circle',
+		label: _x( 'Tiled columns', 'Tiled gallery layout' ),
+		name: 'columns',
 	},
 ];
 export const LAYOUTS = LAYOUT_STYLES.map( ( { name } ) => name );
