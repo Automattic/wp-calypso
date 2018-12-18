5eaf8e0713d36dc6c7ef9df10a694520c5b6347d
Mikael Korpela <mikael@ihminen.org>
2018-12-18 17:35:48 +0200

    Moar fixing

--- client/gutenberg/extensions/tiled-gallery/constants.js
+++ client/gutenberg/extensions/tiled-gallery/constants.js
@@ -30,4 +30,4 @@ export const LAYOUT_STYLES = [
 ];
 export const LAYOUTS = [ 'rectangular', 'columns', 'square', 'circle' ];
 export const MAX_COLUMNS = 20;
-export const TILE_MARGIN = 2;
+export const TILE_MARGIN = 4;
