e61e341b5f03aefde7a56265911f226dcae6226a
Filipe Varela <keoshi@keoshi.com>
2018-12-18 12:49:19 +0000

    Reorder external dependencies
    See: https://github.com/Automattic/wp-calypso/pull/28910#discussion_r242464706

--- client/my-sites/checkout/cart/secondary-cart.jsx
+++ client/my-sites/checkout/cart/secondary-cart.jsx
@@ -3,10 +3,10 @@
 /**
  * External dependencies
  */
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import Dispatcher from 'dispatcher';
 import classNames from 'classnames';
