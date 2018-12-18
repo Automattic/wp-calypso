0dd4169ab459677f0bd3bf465e8276b36f12168c
Jon Surrell <jon.surrell@automattic.com>
2018-12-18 10:17:26 +0100

    Eslint: Use Gutenberg svg wrappers for extensions (#29531)
    Add an eslint rule for Gutenberg SVG wrappers. Example:
    
    > error  <path> is forbidden, use <Path> from @wordpress/components

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,22 @@ module.exports = {
 		{
 			files: [ 'client/gutenberg/extensions/**/*' ],
 			rules: {
+				'react/forbid-elements': [
+					'error',
+					{
+						forbid: [
+							[ 'circle', 'Circle' ],
+							[ 'g', 'G' ],
+							[ 'path', 'Path' ],
+							[ 'polygon', 'Polygon' ],
+							[ 'rect', 'Rect' ],
+							[ 'svg', 'SVG' ],
+						].map( ( [ element, componentName ] ) => ( {
+							element,
+							message: `use <${ componentName }> from @wordpress/components`,
+						} ) ),
+					},
+				],
 				'react/react-in-jsx-scope': 0,
 				'wpcalypso/jsx-classname-namespace': 0,
 			},
