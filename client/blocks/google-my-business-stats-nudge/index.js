1e3e3444cdb2575f00ee78e90083c1e2c7ba1561
Jan Cavan Boulas <mj12982@gmail.com>
2018-12-18 15:19:08 -0800

    Stats: Add Upwork referral banner test

--- client/blocks/google-my-business-stats-nudge/index.js
+++ client/blocks/google-my-business-stats-nudge/index.js
@@ -21,6 +21,7 @@ import { dismissNudge } from './actions';
 import { enhanceWithDismissCount } from 'my-sites/google-my-business/utils';
 import { enhanceWithSiteType, recordTracksEvent } from 'state/analytics/actions';
 import { withEnhancers } from 'state/utils';
+import { abtest } from 'lib/abtest';
 
 class GoogleMyBusinessStatsNudge extends Component {
 	static propTypes = {
@@ -70,54 +71,60 @@ class GoogleMyBusinessStatsNudge extends Component {
 			return null;
 		}
 
-		return (
-			<Card className="google-my-business-stats-nudge">
-				<QueryPreferences />
+		if ( abtest( 'builderReferralStatsNudge' ) === 'googleMyBusinessBanner' ) {
+			return (
+				<Card className="google-my-business-stats-nudge">
+					<QueryPreferences />
 
-				<Gridicon
-					icon="cross"
-					className="google-my-business-stats-nudge__close-icon"
-					onClick={ this.onDismissClick }
-				/>
+					<Gridicon
+						icon="cross"
+						className="google-my-business-stats-nudge__close-icon"
+						onClick={ this.onDismissClick }
+					/>
 
-				<SectionHeader
-					className="google-my-business-stats-nudge__header"
-					label={ this.props.translate( 'Recommendations from WordPress.com' ) }
-				/>
+					<SectionHeader
+						className="google-my-business-stats-nudge__header"
+						label={ this.props.translate( 'Recommendations from WordPress.com' ) }
+					/>
 
-				<div className="google-my-business-stats-nudge__body">
-					<div className="google-my-business-stats-nudge__image-wrapper">
-						<img
-							className="google-my-business-stats-nudge__image"
-							src="/calypso/images/google-my-business/phone-screenshot-cropped.png"
-							alt={ this.props.translate( 'Your business with Google My Business' ) }
-						/>
-					</div>
+					<div className="google-my-business-stats-nudge__body">
+						<div className="google-my-business-stats-nudge__image-wrapper">
+							<img
+								className="google-my-business-stats-nudge__image"
+								src="/calypso/images/google-my-business/phone-screenshot-cropped.png"
+								alt={ this.props.translate( 'Your business with Google My Business' ) }
+							/>
+						</div>
 
-					<div className="google-my-business-stats-nudge__info">
-						<h1 className="google-my-business-stats-nudge__title">
-							{ this.props.translate( 'Can your customers find you on Google?' ) }
-						</h1>
+						<div className="google-my-business-stats-nudge__info">
+							<h1 className="google-my-business-stats-nudge__title">
+								{ this.props.translate( 'Can your customers find you on Google?' ) }
+							</h1>
 
-						<h2 className="google-my-business-stats-nudge__description">
-							{ this.props.translate(
-								'Be there when customers search businesses like yours on Google Search and Maps.'
-							) }
-						</h2>
+							<h2 className="google-my-business-stats-nudge__description">
+								{ this.props.translate(
+									'Be there when customers search businesses like yours on Google Search and Maps.'
+								) }
+							</h2>
 
-						<div className="google-my-business-stats-nudge__button-row">
-							<Button
-								href={ `/google-my-business/${ this.props.siteSlug }` }
-								primary
-								onClick={ this.onStartNowClick }
-							>
-								{ this.props.translate( 'Start Now' ) }
-							</Button>
+							<div className="google-my-business-stats-nudge__button-row">
+								<Button
+									href={ `/google-my-business/${ this.props.siteSlug }` }
+									primary
+									onClick={ this.onStartNowClick }
+								>
+									{ this.props.translate( 'Start Now' ) }
+								</Button>
+							</div>
 						</div>
 					</div>
-				</div>
-			</Card>
-		);
+				</Card>
+			);
+		}
+
+		{
+			return <Card className="google-my-business-stats-nudge" />;
+		}
 	}
 }
 
