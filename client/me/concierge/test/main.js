a19084f615e6fd8d7b8a8eb49ee5b2b98497587a
James Tien <southp0105@gmail.com>
2018-12-20 11:32:31 +0800

    Update the concierge/main tests

--- client/me/concierge/test/main.js
+++ client/me/concierge/test/main.js
@@ -29,27 +29,6 @@ import React from 'react';
  * Internal dependencies
  */
 import { ConciergeMain } from '../main';
-import {
-	PLAN_FREE,
-	PLAN_ECOMMERCE,
-	PLAN_ECOMMERCE_2_YEARS,
-	PLAN_BUSINESS_MONTHLY,
-	PLAN_BUSINESS,
-	PLAN_BUSINESS_2_YEARS,
-	PLAN_PREMIUM,
-	PLAN_PREMIUM_2_YEARS,
-	PLAN_PERSONAL,
-	PLAN_PERSONAL_2_YEARS,
-	PLAN_BLOGGER,
-	PLAN_BLOGGER_2_YEARS,
-	PLAN_JETPACK_FREE,
-	PLAN_JETPACK_PERSONAL,
-	PLAN_JETPACK_PERSONAL_MONTHLY,
-	PLAN_JETPACK_PREMIUM,
-	PLAN_JETPACK_PREMIUM_MONTHLY,
-	PLAN_JETPACK_BUSINESS,
-	PLAN_JETPACK_BUSINESS_MONTHLY,
-} from 'lib/plans/constants';
 
 const props = {
 	steps: [ 'Step1' ],
@@ -93,52 +72,29 @@ describe( 'ConciergeMain basic tests', () => {
 } );
 
 describe( 'ConciergeMain.render()', () => {
-	[
-		PLAN_FREE,
-		PLAN_JETPACK_FREE,
-		PLAN_BLOGGER,
-		PLAN_BLOGGER_2_YEARS,
-		PLAN_PERSONAL,
-		PLAN_PERSONAL_2_YEARS,
-		PLAN_JETPACK_PERSONAL,
-		PLAN_JETPACK_PERSONAL_MONTHLY,
-		PLAN_PREMIUM,
-		PLAN_PREMIUM_2_YEARS,
-		PLAN_JETPACK_PREMIUM,
-		PLAN_JETPACK_PREMIUM_MONTHLY,
-		PLAN_JETPACK_BUSINESS,
-		PLAN_JETPACK_BUSINESS_MONTHLY,
-	].forEach( product_slug => {
-		test( `Should render upsell for non-business plans (${ product_slug })`, () => {
-			const comp = shallow( <ConciergeMain { ...props } site={ { plan: { product_slug } } } /> );
-			expect( comp.find( 'Upsell' ) ).toHaveLength( 1 );
-			expect( comp.find( 'Step1' ) ).toHaveLength( 0 );
-		} );
+	test( 'Should render upsell for non-eligible users', () => {
+		const comp = shallow( <ConciergeMain { ...props } scheduleId={ 0 } /> );
+		expect( comp.find( 'Upsell' ) ).toHaveLength( 1 );
+		expect( comp.find( 'Step1' ) ).toHaveLength( 0 );
 	} );
 
-	[ PLAN_BUSINESS, PLAN_BUSINESS_2_YEARS, PLAN_ECOMMERCE, PLAN_ECOMMERCE_2_YEARS ].forEach(
-		product_slug => {
-			test( `Should render NoAvailableTimes if no times are available (${ product_slug })`, () => {
-				const propsWithoutAvailableTimes = { ...props, availableTimes: [] };
-				const comp = shallow(
-					<ConciergeMain { ...propsWithoutAvailableTimes } site={ { plan: { product_slug } } } />
-				);
-				expect( comp.find( 'NoAvailableTimes' ) ).toHaveLength( 1 );
-			} );
-		}
-	);
+	test( 'Should render NoAvailableTimes if no times are available.', () => {
+		const propsWithoutAvailableTimes = { ...props, availableTimes: [] };
+		const comp = shallow(
+			<ConciergeMain
+				{ ...propsWithoutAvailableTimes }
+				scheduleId={ 1 }
+				site={ { plan: { product_slug: 'a-plan' } } }
+			/>
+		);
+		expect( comp.find( 'NoAvailableTimes' ) ).toHaveLength( 1 );
+	} );
 
-	[
-		PLAN_BUSINESS_MONTHLY,
-		PLAN_BUSINESS,
-		PLAN_BUSINESS_2_YEARS,
-		PLAN_ECOMMERCE,
-		PLAN_ECOMMERCE_2_YEARS,
-	].forEach( product_slug => {
-		test( `Should render CurrentStep for business plans (${ product_slug })`, () => {
-			const comp = shallow( <ConciergeMain { ...props } site={ { plan: { product_slug } } } /> );
-			expect( comp.find( 'Upsell' ) ).toHaveLength( 0 );
-			expect( comp.find( 'Step1' ) ).toHaveLength( 1 );
-		} );
+	test( 'Should render CurrentStep for eligible users', () => {
+		const comp = shallow(
+			<ConciergeMain { ...props } scheduleId={ 1 } site={ { plan: { product_slug: 'a-plan' } } } />
+		);
+		expect( comp.find( 'Upsell' ) ).toHaveLength( 0 );
+		expect( comp.find( 'Step1' ) ).toHaveLength( 1 );
 	} );
 } );
