26449ab42940d901d615272c5bd6466a9ad1b803
Ben Lowery <ben.lowery@automattic.com>
2018-12-19 19:25:22 -0500

    add more variants, including rgb variants

--- bin/generate-style-variants.js
+++ bin/generate-style-variants.js
@@ -4,12 +4,18 @@ const _ = require( 'lodash' );
 const [ basename, murielname ] = argv._;
 
 const steps = [ 0, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900 ];
-const variants = _.map( steps, variant => {
-	return `--${ basename }-${ variant }: #{ \$muriel-${ murielname }-${ variant } };`;
+const variants = _.flatMap( steps, variant => {
+	return [
+		`--${ basename }-${ variant }: #{ \$muriel-${ murielname }-${ variant } };`,
+		`--${ basename }-${ variant }-rgb: #{ hex-to-rgb( \$muriel-${ murielname }-${ variant } ) };`,
+	];
 } );
 
+variants.unshift( `--${ basename }-light-rgb: #{ hex-to-rgb( \$muriel-${ murielname }-300 ) };` );
 variants.unshift( `--${ basename }-light: #{ \$muriel-${ murielname }-300 };` );
+variants.unshift( `--${ basename }-dark-rgb: #{ hex-to-rgb( \$muriel-${ murielname }-700 ) };` );
 variants.unshift( `--${ basename }-dark: #{ \$muriel-${ murielname }-700 };` );
+variants.unshift( `--${ basename }-rgb: #{ hex-to-rgb( \$muriel-${ murielname }-500 ) };` );
 variants.unshift( `--${ basename }: #{ \$muriel-${ murielname }-500 };` );
 
 variants.forEach( n => console.log( n ) );
