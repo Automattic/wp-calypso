d9b75b668761f66daf8211e008c446f905d4cad3
Thomas Guillot <thomasguillot@users.noreply.github.com>
2018-12-21 15:27:27 +0000

    Contact Info Block: Update description and keywords

--- client/gutenberg/extensions/email/index.js
+++ client/gutenberg/extensions/email/index.js
@@ -2,6 +2,7 @@
 /**
  * External dependencies
  */
+import { Fragment } from '@wordpress/element';
 import { Path } from '@wordpress/components';
 
 /**
@@ -10,7 +11,7 @@ import { Path } from '@wordpress/components';
 import edit from './edit';
 import save from './save';
 import renderMaterialIcon from 'gutenberg/extensions/presets/jetpack/utils/render-material-icon';
-import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
+import { __, _x } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 
 const attributes = {
 	email: {
@@ -23,7 +24,20 @@ export const name = 'email';
 
 export const settings = {
 	title: __( 'Email Address' ),
-	keywords: [ 'e-mail', 'email' ], // these are not tranlatable on purpose
+	description: (
+		<Fragment>
+			<p>
+				{ __(
+					'Lets you add an email address with an automatically generated click-to-email link.'
+				) }
+			</p>
+		</Fragment>
+	),
+	keywords: [
+		'e-mail', // not translatable on purpose
+		'email', // not translatable on purpose
+		_x( 'message', 'block search term' ),
+	],
 	icon: renderMaterialIcon(
 		<Path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z" />
 	),
