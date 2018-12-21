d22353a06aba0d1472b7a2fc9e39a638f46ec8fa
Enej Bajgoric <enej.bajgoric@gmail.com>
2018-12-21 08:44:36 -0800

    Contact Form Block: Update to use Path and Circle

--- client/gutenberg/extensions/contact-form/components/jetpack-contact-form.jsx
+++ client/gutenberg/extensions/contact-form/components/jetpack-contact-form.jsx
@@ -4,7 +4,7 @@
  * External dependencies
  */
 import classnames from 'classnames';
-import { Button, PanelBody, Placeholder, TextControl } from '@wordpress/components';
+import { Button, PanelBody, Placeholder, TextControl, Path } from '@wordpress/components';
 import { InnerBlocks, InspectorControls } from '@wordpress/editor';
 import { Component, Fragment } from '@wordpress/element';
 import { sprintf } from '@wordpress/i18n';
@@ -220,7 +220,7 @@ class JetpackContactForm extends Component {
 						<Placeholder
 							label={ __( 'Form' ) }
 							icon={ renderMaterialIcon(
-								<path d="M13 7.5h5v2h-5zm0 7h5v2h-5zM19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM11 6H6v5h5V6zm-1 4H7V7h3v3zm1 3H6v5h5v-5zm-1 4H7v-3h3v3z" />
+								<Path d="M13 7.5h5v2h-5zm0 7h5v2h-5zM19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM11 6H6v5h5V6zm-1 4H7V7h3v3zm1 3H6v5h5v-5zm-1 4H7v-3h3v3z" />
 							) }
 						>
 							<form onSubmit={ this.onFormSettingsSet }>
