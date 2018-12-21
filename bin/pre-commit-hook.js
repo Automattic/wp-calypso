69e9cfa0f667d05452d298e9c393d796adff7738
Ben Lowery <ben.lowery@automattic.com>
2018-12-20 20:13:18 -0500

    Swap stylelint for prettier for SCSS files
    We have been using prettier recently to format SCSS files. Unfortunately, prettier has a number of outstanding bugs with comments and formatting that can easily break code.
    
    This patch turns off prettier for sass files and uses stylelint instead. This has the added bonus of enforcing (or at least reporting) our stylelint rules, something that has been sorely missing until now.

--- bin/pre-commit-hook.js
+++ bin/pre-commit-hook.js
@@ -6,6 +6,7 @@
 const execSync = require( 'child_process' ).execSync;
 const spawnSync = require( 'child_process' ).spawnSync;
 const chalk = require( 'chalk' );
+const _ = require( 'lodash' );
 
 console.log(
 	'\nBy contributing to this project, you license the materials you contribute ' +
@@ -29,18 +30,31 @@ function parseGitDiffToPathArray( command ) {
 		.filter( name => /\.(jsx?|scss)$/.test( name ) );
 }
 
-const dirtyFiles = new Set( parseGitDiffToPathArray( 'git diff --name-only --diff-filter=ACM' ) );
+// grab a list of all the files staged to commit
 const files = parseGitDiffToPathArray( 'git diff --cached --name-only --diff-filter=ACM' );
 
+// grab a list of all the files with changes in the working copy.
+// This list may have overlaps with the staged list if files are
+// partially staged...
+const dirtyFiles = new Set( parseGitDiffToPathArray( 'git diff --name-only --diff-filter=ACM' ) );
+
+// we don't want to format any files that are partially staged or unstaged
 dirtyFiles.forEach( file =>
 	console.log(
 		chalk.red( `${ file } will not be auto-formatted because it has unstaged changes.` )
 	)
 );
 
-const toPrettify = files.filter( file => ! dirtyFiles.has( file ) );
-toPrettify.forEach( file => console.log( `Prettier formatting staged file: ${ file }` ) );
+// Remove all the dirty files from the set to format
+const toFormat = files.filter( file => ! dirtyFiles.has( file ) );
 
+// Split the set to format into things to format with stylelint and things to format with prettier.
+// We avoid prettier on sass files because of outstanding bugs in how prettier handles
+// single line comments.
+const [ toStylelintfix, toPrettify ] = _.partition( toFormat, file => file.endsWith( '.scss' ) );
+
+// Format the JS and JSX files with prettier, then re-stage them. Swallow the output.
+toPrettify.forEach( file => console.log( `Prettier formatting staged file: ${ file }` ) );
 if ( toPrettify.length ) {
 	execSync(
 		`./node_modules/.bin/prettier --ignore-path .eslintignore --write ${ toPrettify.join( ' ' ) }`
@@ -48,11 +62,36 @@ if ( toPrettify.length ) {
 	execSync( `git add ${ toPrettify.join( ' ' ) }` );
 }
 
-// linting should happen after formatting
-const toLint = files.filter( file => ! file.endsWith( '.scss' ) );
+// Format the sass files with stylelint and then re-stage them. Swallow the output.
+toStylelintfix.forEach( file => console.log( `stylelint formatting staged file: ${ file }` ) );
+if ( toStylelintfix.length ) {
+	spawnSync( `./node_modules/.bin/stylelint --fix ${ toStylelintfix.join( ' ' ) }` );
+	execSync( `git add ${ toStylelintfix.join( ' ' ) }` );
+}
 
-if ( toLint.length ) {
-	const lintResult = spawnSync( './node_modules/.bin/eslint', [ '--quiet', ...toLint ], {
+// Now run the linters over everything staged to commit, even if they are partially staged
+const [ toStylelint, toEslint ] = _.partition( files, file => file.endsWith( '.scss' ) );
+
+// first stylelint
+if ( toStylelint.length ) {
+	const lintResult = spawnSync( './node_modules/.bin/stylelint', [ ...toStylelint ], {
+		shell: true,
+		stdio: 'inherit',
+	} );
+
+	if ( lintResult.status ) {
+		console.log(
+			chalk.yellow( 'STYLELINT:' ),
+			'The linter reported some problems. Please consider fixing them.'
+		);
+		//process.exit( 1 );
+		// do not abort the commit for now. Maybe in the future...
+	}
+}
+
+// then eslint
+if ( toEslint.length ) {
+	const lintResult = spawnSync( './node_modules/.bin/eslint', [ '--quiet', ...toEslint ], {
 		shell: true,
 		stdio: 'inherit',
 	} );
