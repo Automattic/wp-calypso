ca7c95400cc8e15e1cd7651a765e7d3ea692ad45
James Tien <southp0105@gmail.com>
2018-12-24 16:56:12 +0800

    Remove the hard-coded session length from the time picker. (#29702)
    * Add `appointmentTimespan` reducer
    
    * Add a new selector: `getConciergeAppointmentTimespan`
    
    * Add a test suite for `appointmentTimespan` reducer.
    
    * Update the `concierge/initial` data-layer handler according to the
    response changes.
    
    * Replace all the hard-coded "30mins" session length.

---
+++ client/state/concierge/appointment-timespan/test/reducer.js
@@ -0,0 +1,34 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import { appointmentTimespan } from '../reducer';
+import { CONCIERGE_INITIAL_REQUEST, CONCIERGE_INITIAL_UPDATE } from 'state/action-types';
+
+describe( 'concierge/appointmentTimespan/reducer', () => {
+	test( 'should be defaulted as null.', () => {
+		expect( appointmentTimespan( undefined, {} ) ).toBeNull();
+	} );
+
+	test( 'should be null on receiving the request action.', () => {
+		expect(
+			appointmentTimespan( 123, {
+				type: CONCIERGE_INITIAL_REQUEST,
+			} )
+		).toBeNull();
+	} );
+
+	test( 'should be the received data on receiving the update action.', () => {
+		const expectedAppointmentTimespan = 1984;
+
+		expect(
+			appointmentTimespan( undefined, {
+				type: CONCIERGE_INITIAL_UPDATE,
+				initial: {
+					appointmentTimespan: expectedAppointmentTimespan,
+				},
+			} )
+		).toEqual( expectedAppointmentTimespan );
+	} );
+} );
