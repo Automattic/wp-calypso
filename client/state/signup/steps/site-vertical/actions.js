c2b691691aaa43f75b99cc7022513a04acd0f112
ramonjd <ramonjd@gmail.com>
2018-12-24 12:06:59 +1100

    Simplify function return syntax

--- client/state/signup/steps/site-vertical/actions.js
+++ client/state/signup/steps/site-vertical/actions.js
@@ -18,9 +18,7 @@ export function setSiteVertical( { name, slug } ) {
 // It's a thunk since there is still Flux involved, so it can't be a plain object yet.
 // If the signup state is fully reduxified, we can just keep setSiteVertical() and
 // keep all the dependency filling and progress filling in a middleware.
-export const submitSiteVertical = ( { name, slug } ) => {
-	return dispatch => {
-		dispatch( setSiteVertical( { name, slug } ) );
-		SignupActions.submitSignupStep( { stepName: 'site-topic' }, [], { name } );
-	};
+export const submitSiteVertical = ( { name, slug } ) => dispatch => {
+	dispatch( setSiteVertical( { name, slug } ) );
+	SignupActions.submitSignupStep( { stepName: 'site-topic' }, [], { siteTopic: name } );
 };
