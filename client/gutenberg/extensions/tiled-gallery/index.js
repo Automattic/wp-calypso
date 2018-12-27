b3a63d3cd7cf4b56b6af024aaa8204acc8dc259e
Mikael Korpela <mikael@ihminen.org>
2018-12-27 16:50:36 +0200

    Tiled gallery block: update icon

--- client/gutenberg/extensions/tiled-gallery/index.js
+++ client/gutenberg/extensions/tiled-gallery/index.js
@@ -3,7 +3,7 @@
  */
 import { createBlock } from '@wordpress/blocks';
 import { filter } from 'lodash';
-import { Rect, SVG } from '@wordpress/components';
+import { Path, SVG } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -95,12 +95,9 @@ export const settings = {
 	category: 'jetpack',
 	description: __( 'Display multiple images in an elegantly organized tiled layout.' ),
 	icon: (
-		<SVG viewBox="0 0 20 20">
-			<Rect x="8" y="11" width="9" height="6" />
-			<Rect x="3" y="11" width="4" height="6" />
-			<Rect x="13" y="7" width="4" height="3" />
-			<Rect x="13" y="3" width="4" height="3" />
-			<Rect x="3" y="3" width="9" height="7" />
+		<SVG viewBox="0 0 24 24">
+			<Path fill="none" d="M0 0h24v24H0V0z" />
+			<Path d="M19 5v2h-4V5h4M9 5v6H5V5h4m10 8v6h-4v-6h4M9 17v2H5v-2h4M21 3h-8v6h8V3zM11 3H3v10h8V3zm10 8h-8v10h8V11zm-10 4H3v6h8v-6z" />
 		</SVG>
 	),
 	keywords: [
