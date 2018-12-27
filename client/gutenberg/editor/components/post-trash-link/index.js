e513ff652193de41a78745e9d30fb41b9c1ba59d
Copons <copons@users.noreply.github.com>
2018-12-27 15:51:10 +0000

    Use Calypso trash logic

--- client/gutenberg/editor/components/post-trash-link/index.js
+++ client/gutenberg/editor/components/post-trash-link/index.js
@@ -11,51 +11,25 @@ import { connect } from 'react-redux';
  */
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
-import { withSelect, withDispatch } from '@wordpress/data';
-import { compose } from '@wordpress/compose';
+import { withSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
-import { removeNotice, successNotice } from 'state/notices/actions';
-import { restorePost } from 'state/posts/actions';
+import { restorePost, trashPost } from 'state/posts/actions';
 import { navigate } from 'state/ui/actions';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import getPostTypeTrashUrl from 'state/selectors/get-post-type-trash-url';
 
 export class PostTrashLink extends Component {
-	state = {
-		isTrashing: false,
-	};
-
-	componentDidUpdate( prevProps ) {
-		const { postId, postStatus, siteId, translate, trashUrl } = this.props;
-
-		if ( 'trash' !== prevProps.postStatus && 'trash' === postStatus ) {
-			const noticeId = `trash_${ siteId }_${ postId }`;
-			this.props.successNotice( translate( 'Post successfully moved to trash.' ), {
-				button: translate( 'Undo' ),
-				id: noticeId,
-				isPersistent: true,
-				onClick: () => {
-					this.props.removeNotice( noticeId );
-					this.props.restorePost( siteId, postId );
-				},
-			} );
-
-			this.props.navigate( trashUrl );
-		}
-	}
-
 	onClick = () => {
-		const { postId, postType, trashPost } = this.props;
-		trashPost( postId, postType );
-		this.setState( { isTrashing: true } );
+		const { postId, siteId, trashUrl } = this.props;
+		this.props.trashPost( siteId, postId );
+		this.props.navigate( trashUrl );
 	};
 
 	render() {
 		const { isNew, postId } = this.props;
-		const { isTrashing } = this.state;
 
 		if ( isNew || ! postId ) {
 			return null;
@@ -64,8 +38,6 @@ export class PostTrashLink extends Component {
 		return (
 			<Button
 				className="editor-post-trash button-link-delete"
-				disabled={ isTrashing }
-				isBusy={ isTrashing }
 				isDefault
 				isLarge
 				onClick={ this.onClick }
@@ -76,25 +48,17 @@ export class PostTrashLink extends Component {
 	}
 }
 
-export default compose( [
-	withSelect( select => {
-		const {
-			getCurrentPostId,
-			getCurrentPostType,
-			getEditedPostAttribute,
-			isEditedPostNew,
-		} = select( 'core/editor' );
-		return {
-			isNew: isEditedPostNew(),
-			postId: getCurrentPostId(),
-			postStatus: getEditedPostAttribute( 'status' ),
-			postType: getCurrentPostType(),
-		};
-	} ),
-	withDispatch( dispatch => ( {
-		trashPost: dispatch( 'core/editor' ).trashPost,
-	} ) ),
-] )(
+export default withSelect( select => {
+	const { getCurrentPostId, getCurrentPostType, getEditedPostAttribute, isEditedPostNew } = select(
+		'core/editor'
+	);
+	return {
+		isNew: isEditedPostNew(),
+		postId: getCurrentPostId(),
+		postStatus: getEditedPostAttribute( 'status' ),
+		postType: getCurrentPostType(),
+	};
+} )(
 	connect(
 		( state, { postType } ) => ( {
 			siteId: getSelectedSiteId( state ),
@@ -102,9 +66,8 @@ export default compose( [
 		} ),
 		{
 			navigate,
-			removeNotice,
 			restorePost,
-			successNotice,
+			trashPost,
 		}
 	)( localize( PostTrashLink ) )
 );
