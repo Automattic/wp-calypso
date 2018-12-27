078e6d3ac1a160e800a3f10f46eba9a06ac37719
Bernie Reiter <ockham@raz.or.at>
2018-12-27 21:49:18 +0100

    Related Posts Block: Use Invariable className for Preview

--- client/gutenberg/extensions/related-posts/edit.jsx
+++ client/gutenberg/extensions/related-posts/edit.jsx
@@ -38,6 +38,7 @@ class RelatedPostsEdit extends Component {
 
 		const postsToDisplay = posts.length ? posts : DEFAULT_POSTS;
 		const displayPosts = postsToDisplay.slice( 0, postsToShow );
+		const previewClassName = 'related-posts__preview';
 
 		return (
 			<Fragment>
@@ -80,28 +81,28 @@ class RelatedPostsEdit extends Component {
 						[ `columns-${ postsToShow }` ]: postLayout === 'grid',
 					} ) }
 				>
-					<div className={ `${ className }__preview-items` }>
+					<div className={ previewClassName }>
 						{ displayPosts.map( ( post, i ) => (
-							<div className={ `${ className }__preview-post` } key={ i }>
+							<div className={ `${ previewClassName }-post` } key={ i }>
 								{ displayThumbnails &&
 									post.img &&
 									post.img.src && (
-										<Button className={ `${ className }__preview-post-link` } isLink>
+										<Button className={ `${ previewClassName }-post-link` } isLink>
 											<img src={ post.img.src } alt={ post.title } />
 										</Button>
 									) }
 								<h4>
-									<Button className={ `${ className }__preview-post-link` } isLink>
+									<Button className={ `${ previewClassName }-post-link` } isLink>
 										{ post.title }
 									</Button>
 								</h4>
 								{ displayDate && (
-									<span className={ `${ className }__preview-post-date has-small-font-size` }>
+									<span className={ `${ previewClassName }-post-date has-small-font-size` }>
 										{ post.date }
 									</span>
 								) }
 								{ displayContext && (
-									<p className={ `${ className }__preview-post-context` }>{ post.context }</p>
+									<p className={ `${ previewClassName }-post-context` }>{ post.context }</p>
 								) }
 							</div>
 						) ) }
