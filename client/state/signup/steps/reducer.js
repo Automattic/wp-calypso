f1822838b984651bfc71ac26ee29ed13fcc86353
Ramon <ramonjd@users.noreply.github.com>
2018-12-27 11:18:29 +1100

    NUX: Refactor step site topic state (#29736)
    * Adding siteVertical to the state tree.
    
    * Granularizing selectors to fetch individual properties.
    Migrating submit action from site topic actions.
    
    * Replacing siteTopic state usage with siteVertical state
    
    * Simplify function return syntax
    
    * Remove `siteTopic` state tree files.
    
    * Update client/signup/site-mockup/index.jsx
    
    Updating selector name to correct one: `getSiteVerticalName`
    
    Co-Authored-By: ramonjd <ramonjd@users.noreply.github.com>
    
    * Update client/signup/site-mockup/index.jsx
    
    Updating selector name to the correct one: `getSiteVerticalName`
    
    Co-Authored-By: ramonjd <ramonjd@users.noreply.github.com>

--- client/state/signup/steps/reducer.js
+++ client/state/signup/steps/reducer.js
@@ -6,21 +6,21 @@
 
 import designType from './design-type/reducer';
 import siteTitle from './site-title/reducer';
-import siteTopic from './site-topic/reducer';
 import siteInformation from './site-information/reducer';
 import siteGoals from './site-goals/reducer';
 import userExperience from './user-experience/reducer';
 import siteType from './site-type/reducer';
+import siteVertical from './site-vertical/reducer';
 import { combineReducers } from 'state/utils';
 import survey from './survey/reducer';
 
 export default combineReducers( {
 	designType,
 	siteTitle,
-	siteTopic,
 	siteInformation,
 	siteGoals,
 	userExperience,
 	siteType,
+	siteVertical,
 	survey,
 } );
