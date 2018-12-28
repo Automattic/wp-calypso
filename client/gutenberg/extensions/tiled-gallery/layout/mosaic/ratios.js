e628f29aae90478351149823eafd02c7e3a9ea14
Jon Surrell <jon.surrell@automattic.com>
2018-12-28 17:21:00 +0100

    Revert "Use accumulated ratio over local ratio"
    This reverts commit 47e089ff7ec451fae8bcf3d168ba5c2f9e4cfecd.

--- client/gutenberg/extensions/tiled-gallery/layout/mosaic/ratios.js
+++ client/gutenberg/extensions/tiled-gallery/layout/mosaic/ratios.js
@@ -43,14 +43,14 @@ export function ratiosToColumns( ratios ) {
 
 	const row = [];
 	let toProcess = ratios;
-	let accumulatedRatio = 0;
 
 	// We skip the last column in the loop and add rest later
 	for ( let i = 0; i <= columnCount - 1; i++ ) {
+		let colRatio = 0;
 		const colSize = takeWhile( toProcess, ratio => {
-			const shouldTake = accumulatedRatio <= ( i + 1 ) * targetColRatio;
+			const shouldTake = colRatio <= targetColRatio;
 			if ( shouldTake ) {
-				accumulatedRatio += ratio;
+				colRatio += ratio;
 			}
 			return shouldTake;
 		} ).length;
