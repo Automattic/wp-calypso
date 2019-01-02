053ff8a7c54a9e30b134f89d38d19e5a8b156c4f
Kerry Liu <kerryliu@gmail.com>
2019-01-01 16:08:55 -0800

    Gutenlypso: update calypso classic block to update block content even if the editor instance is marked for removal

--- client/gutenberg/extensions/classic-block/edit.jsx
+++ client/gutenberg/extensions/classic-block/edit.jsx
@@ -4,7 +4,7 @@
  */
 import React from 'react';
 import { Component } from '@wordpress/element';
-import { debounce, noop } from 'lodash';
+import { noop } from 'lodash';
 import TinyMCE from 'components/tinymce';
 
 /**
@@ -23,14 +23,48 @@ export class ClassicEdit extends Component {
 	}
 
 	storeEditor = ref => {
+		//By the time editor lifecycle events like PostProcess fire, this editor instance is already marked for removal
+		//so any content manipulation doesn't get reflected in this block.
+		//
+		//For now, when we detect an unmount, get content from the editor marked to be destroyed and update block attributes.
+		//https://github.com/Automattic/wp-calypso/issues/29643
+		if ( ! ref && this.editor ) {
+			const content = this.editor._editor.getContent();
+			this.props.setAttributes( { content } );
+		}
 		this.editor = ref;
 	};
 
-	debouncedOnContentChange = debounce( () => {
-		const rawContent = this.editor.getContent( { format: 'raw' } );
+	componentDidUpdate( prevProps ) {
+		const {
+			attributes: { content },
+		} = this.props;
 
-		this.props.setAttributes( { content: rawContent } );
-	}, 300 );
+		if ( this.editor && prevProps.attributes.content !== content ) {
+			this.editor.setEditorContent( content || '' );
+		}
+	}
+
+	updateBlockContentAndBookmark = () => {
+		if ( ! this.editor ) {
+			return;
+		}
+		const editor = this.editor._editor;
+
+		this.bookmark = editor.selection.getBookmark( 2, true );
+
+		this.props.setAttributes( {
+			content: editor.getContent(),
+		} );
+
+		editor.once( 'focus', () => {
+			if ( this.bookmark ) {
+				editor.selection.moveToBookmark( this.bookmark );
+			}
+		} );
+
+		return false;
+	};
 
 	render() {
 		const { isSelected, setSelected } = this.props;
@@ -38,7 +72,7 @@ export class ClassicEdit extends Component {
 			<TinyMCE
 				isGutenbergClassicBlock
 				mode="tinymce"
-				onChange={ this.debouncedOnContentChange }
+				onBlur={ this.updateBlockContentAndBookmark }
 				onClick={ isSelected ? noop : setSelected }
 				onKeyUp={ this.debouncedOnContentChange }
 				onSetContent={ this.debouncedOnContentChange }
