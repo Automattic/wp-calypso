7b5a367cea1d1f34d0fb9426bdd8e3ebedc938f1
Bernie Reiter <ockham@raz.or.at>
2019-01-03 23:10:34 +0100

    Gutenberg Jetpack Preset: Register Child Blocks if their parent is available

--- client/gutenberg/extensions/presets/jetpack/utils/register-jetpack-block.js
+++ client/gutenberg/extensions/presets/jetpack/utils/register-jetpack-block.js
@@ -14,13 +14,22 @@ import isJetpackExtensionAvailable from './is-jetpack-extension-available';
  *
  * @param {string} name The block's name.
  * @param {object} settings The block's settings.
+ * @param {object} childBlocks The block's child blocks.
  * @returns {object|false} Either false if the block is not available, or the results of `registerBlockType`
  */
-export default function registerJetpackBlock( name, settings ) {
+export default function registerJetpackBlock( name, settings, childBlocks = [] ) {
 	if ( ! isJetpackExtensionAvailable( name ) ) {
 		// TODO: check 'unavailable_reason' and respond accordingly
 		return false;
 	}
 
-	return registerBlockType( `jetpack/${ name }`, settings );
+	const result = registerBlockType( `jetpack/${ name }`, settings );
+
+	// Register child blocks. Using `registerBlockType()` directly avoids availability checks -- if
+	// their parent is available, we register them all, without checking for their individual availability.
+	childBlocks.forEach( childBlock =>
+		registerBlockType( `jetpack/${ childBlock.name }`, childBlock.settings )
+	);
+
+	return result;
 }
