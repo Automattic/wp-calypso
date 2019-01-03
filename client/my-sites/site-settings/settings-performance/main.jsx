f3fff9a9fd43e14cda7c57baa2e0edc41a95db39
Filipe Varela <keoshi@keoshi.com>
2019-01-03 14:35:09 +0000

    Get rid of FormPerformance
    Modules are now included in the main Performance component

--- client/my-sites/site-settings/settings-performance/main.jsx
+++ client/my-sites/site-settings/settings-performance/main.jsx
@@ -4,7 +4,7 @@
  * External dependencies
  */
 
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import { flowRight, partialRight, pick } from 'lodash';
@@ -14,72 +14,136 @@ import { flowRight, partialRight, pick } from 'lodash';
  */
 import AmpJetpack from 'my-sites/site-settings/amp/jetpack';
 import AmpWpcom from 'my-sites/site-settings/amp/wpcom';
+import Button from 'components/button';
 import DocumentHead from 'components/data/document-head';
-import FormPerformance from 'my-sites/site-settings/form-performance';
 import JetpackDevModeNotice from 'my-sites/site-settings/jetpack-dev-mode-notice';
 import Main from 'components/main';
-import Placeholder from 'my-sites/site-settings/placeholder';
+import MediaSettingsPerformance from 'my-sites/site-settings/media-settings-performance';
+import QueryJetpackModules from 'components/data/query-jetpack-modules';
 import Search from 'my-sites/site-settings/search';
+import SectionHeader from 'components/section-header';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import SiteSettingsNavigation from 'my-sites/site-settings/navigation';
+import SpeedUpYourSite from 'my-sites/site-settings/speed-up-site-settings';
 import wrapSettingsForm from 'my-sites/site-settings/wrap-settings-form';
 import { getSelectedSite, getSelectedSiteId } from 'state/ui/selectors';
-import { isJetpackSite } from 'state/sites/selectors';
+import {
+	isJetpackSite,
+	isJetpackMinimumVersion,
+	siteSupportsJetpackSettingsUi,
+} from 'state/sites/selectors';
 
-const SiteSettingsPerformance = ( {
-	fields,
-	handleAutosavingToggle,
-	isJetpack,
-	isRequestingSettings,
-	isSavingSettings,
-	site,
-	submitForm,
-	trackEvent,
-	translate,
-	updateFields,
-} ) => {
-	if ( ! site ) {
-		return <Placeholder />;
+class SiteSettingsPerformance extends Component {
+	renderSectionHeader( title, showButton = true ) {
+		const { handleSubmitForm, isRequestingSettings, isSavingSettings, translate } = this.props;
+		return (
+			<SectionHeader label={ title }>
+				{ showButton && (
+					<Button
+						compact
+						primary
+						onClick={ handleSubmitForm }
+						disabled={ isRequestingSettings || isSavingSettings }
+					>
+						{ isSavingSettings ? translate( 'Saving…' ) : translate( 'Save Settings' ) }
+					</Button>
+				) }
+			</SectionHeader>
+		);
 	}
 
-	return (
-		<Main className="settings-performance site-settings">
-			<DocumentHead title={ translate( 'Site Settings' ) } />
-			<JetpackDevModeNotice />
-			<SidebarNavigation />
-			<SiteSettingsNavigation site={ site } section="performance" />
-			<FormPerformance />
-			<Search
-				handleAutosavingToggle={ handleAutosavingToggle }
-				isSavingSettings={ isSavingSettings }
-				isRequestingSettings={ isRequestingSettings }
-				fields={ fields }
-			/>
+	render() {
+		const {
+			fields,
+			handleAutosavingToggle,
+			isRequestingSettings,
+			isSavingSettings,
+			jetpackSettingsUI,
+			jetpackVersionSupportsLazyImages,
+			onChangeField,
+			site,
+			siteId,
+			siteIsJetpack,
+			submitForm,
+			translate,
+			trackEvent,
+			updateFields,
+		} = this.props;
 
-			{ isJetpack ? (
-				<AmpJetpack />
-			) : (
-				<AmpWpcom
-					submitForm={ submitForm }
-					trackEvent={ trackEvent }
-					updateFields={ updateFields }
+		return (
+			<Main className="settings-performance site-settings">
+				<DocumentHead title={ translate( 'Site Settings' ) } />
+				<JetpackDevModeNotice />
+				<SidebarNavigation />
+				<SiteSettingsNavigation site={ site } section="performance" />
+
+				{ siteIsJetpack && <QueryJetpackModules siteId={ siteId } /> }
+
+				{ jetpackSettingsUI &&
+					jetpackVersionSupportsLazyImages && (
+						<Fragment>
+							{ this.renderSectionHeader( translate( 'Performance & speed' ), false ) }
+							<SpeedUpYourSite
+								isSavingSettings={ isSavingSettings }
+								isRequestingSettings={ isRequestingSettings }
+								jetpackVersionSupportsLazyImages={ jetpackVersionSupportsLazyImages }
+								submitForm={ submitForm }
+								updateFields={ updateFields }
+							/>
+						</Fragment>
+					) }
+
+				{ jetpackSettingsUI && (
+					<Fragment>
+						{ this.renderSectionHeader( translate( 'Media' ) ) }
+						<MediaSettingsPerformance
+							siteId={ siteId }
+							handleAutosavingToggle={ handleAutosavingToggle }
+							onChangeField={ onChangeField }
+							isSavingSettings={ isSavingSettings }
+							isRequestingSettings={ isRequestingSettings }
+							fields={ fields }
+							jetpackVersionSupportsLazyImages={ jetpackVersionSupportsLazyImages }
+						/>
+					</Fragment>
+				) }
+
+				<Search
+					handleAutosavingToggle={ handleAutosavingToggle }
 					isSavingSettings={ isSavingSettings }
 					isRequestingSettings={ isRequestingSettings }
 					fields={ fields }
 				/>
-			) }
-		</Main>
-	);
-};
+
+				{ siteIsJetpack ? (
+					<AmpJetpack />
+				) : (
+					<AmpWpcom
+						submitForm={ submitForm }
+						trackEvent={ trackEvent }
+						updateFields={ updateFields }
+						isSavingSettings={ isSavingSettings }
+						isRequestingSettings={ isRequestingSettings }
+						fields={ fields }
+					/>
+				) }
+			</Main>
+		);
+	}
+}
 
 const connectComponent = connect( state => {
 	const site = getSelectedSite( state );
 	const siteId = getSelectedSiteId( state );
-	const isJetpack = isJetpackSite( state, siteId );
+	const siteIsJetpack = isJetpackSite( state, siteId );
+	const jetpackSettingsUiSupported = siteSupportsJetpackSettingsUi( state, siteId );
+	const jetpackVersionSupportsLazyImages = isJetpackMinimumVersion( state, siteId, '5.8-alpha' );
 
 	return {
 		site,
-		isJetpack,
+		siteIsJetpack,
+		jetpackVersionSupportsLazyImages,
+		jetpackSettingsUI: siteIsJetpack && jetpackSettingsUiSupported,
 	};
 } );
 
@@ -88,6 +152,9 @@ const getFormSettings = partialRight( pick, [
 	'amp_is_supported',
 	'jetpack_search_enabled',
 	'jetpack_search_supported',
+	'lazy-images',
+	'photon',
+	'photon-cdn',
 ] );
 
 export default flowRight(
