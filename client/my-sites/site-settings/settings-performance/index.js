31e43753294c51702520c5b972197140afc90f76
Filipe Varela <keoshi@keoshi.com>
2019-01-03 12:54:25 +0000

    Revert settingsController import
    See: https://github.com/Automattic/wp-calypso/pull/29597/files#r244987287

--- client/my-sites/site-settings/settings-performance/index.js
+++ client/my-sites/site-settings/settings-performance/index.js
@@ -10,14 +10,14 @@ import page from 'page';
 import { makeLayout, render as clientRender } from 'controller';
 import { navigation, siteSelection } from 'my-sites/controller';
 import { performance } from './controller';
-import { siteSettings } from 'my-sites/site-settings/settings-controller';
+import settingsController from 'my-sites/site-settings/settings-controller';
 
 export default function() {
 	page(
 		'/settings/performance/:site_id',
 		siteSelection,
 		navigation,
-		siteSettings,
+		settingsController.siteSettings,
 		performance,
 		makeLayout,
 		clientRender
