2a26ea87803d345240a6c651fe8042aac1b4f362
mmtr <miguel.torres@automattic.com>
2019-01-04 16:37:24 +0100

    Gutenpack: New VideoPress block

---
+++ client/gutenberg/extensions/videopress/index.js
@@ -0,0 +1,23 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { getBlockType } from '@wordpress/blocks';
+
+const name = 'videopress';
+
+const coreVideoBlock = getBlockType( 'core/video' );
+
+const settings = {
+	...coreVideoBlock,
+	category: 'jetpack',
+};
+
+// Since we're building a block using the settings of a registered block, they
+// already include a name which will override our custom name, so we need to
+// delete it.
+// See https://github.com/WordPress/gutenberg/blob/95edac1e42cb10ed7da9f406696cdc85d6e476d5/packages/blocks/src/api/registration.js#L67-L71
+delete settings.name;
+
+export { name, settings };
