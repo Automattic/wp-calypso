dea30a0bbd105e31f3e9a229f05d4d728565746f
Travis Walter <travis@automattic.com>
2019-01-04 11:01:25 -0800

    remove changes from products-values - they were already implemented in #29524

--- client/lib/products-values/index.js
+++ client/lib/products-values/index.js
@@ -413,13 +413,6 @@ export function isTheme( product ) {
 	return 'premium_theme' === product.product_slug;
 }
 
-export function isConciergeSession( product ) {
-	product = formatProduct( product );
-	assertValidProduct( product );
-
-	return 'concierge-session' === product.product_slug;
-}
-
 export function isCustomDesign( product ) {
 	product = formatProduct( product );
 	assertValidProduct( product );
@@ -520,7 +513,6 @@ export default {
 	isSiteRedirect,
 	isSpaceUpgrade,
 	isTheme,
-	isConciergeSession,
 	isUnlimitedSpace,
 	isUnlimitedThemes,
 	isVideoPress,
