356bcfcc236caa7c1672cce4d80815e23c130278
ramonjd <ramonjd@gmail.com>
2019-01-05 11:59:48 +1100

    Anonymizing information in the submit tracking call.

--- client/lib/signup/actions.js
+++ client/lib/signup/actions.js
@@ -4,7 +4,7 @@
  * External dependencies
  */
 
-import { defer, keys, snakeCase } from 'lodash';
+import { defer, includes, keys, snakeCase } from 'lodash';
 
 /**
  * Internal dependencies
@@ -27,10 +27,18 @@ const SignupActions = {
 	submitSignupStep( step, errors, providedDependencies ) {
 		// Transform the keys since tracks events only accept snaked prop names.
 		const inputs = keys( providedDependencies ).reduce( ( props, name ) => {
-			const propName = snakeCase( name );
+			let propName = snakeCase( name );
+			let propValue = providedDependencies[ name ];
+
+			// Ensure we don't capture identifiable user data we don't need.
+			if ( includes( [ 'email', 'address', 'phone' ], propName ) ) {
+				propName = `user_entered_${ propName }`;
+				propValue = !! propValue;
+			}
+
 			return {
 				...props,
-				[ propName ]: providedDependencies[ name ],
+				[ propName ]: propValue,
 			};
 		}, {} );
 
