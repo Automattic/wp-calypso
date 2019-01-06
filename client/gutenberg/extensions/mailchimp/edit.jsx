9e578da71edc7df09d6f5c877e051f0b6e8dec65
Jefferson Rabb <jeff@atavist.net>
2019-01-06 12:43:00 -0500

    Refactor of Mailchimp block without Server Side Rendering.

--- client/gutenberg/extensions/mailchimp/edit.jsx
+++ client/gutenberg/extensions/mailchimp/edit.jsx
@@ -3,42 +3,211 @@
 /**
  * External dependencies
  */
-import { ServerSideRender, TextControl } from '@wordpress/components';
-import { InspectorControls } from '@wordpress/editor';
+import apiFetch from '@wordpress/api-fetch';
+import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
+import {
+	Button,
+	ExternalLink,
+	PanelBody,
+	Placeholder,
+	Spinner,
+	TextControl,
+	withNotices,
+} from '@wordpress/components';
+import { InspectorControls, RichText } from '@wordpress/editor';
 import { Fragment, Component } from '@wordpress/element';
-/**
- * Internal dependencies
- */
 
-import { fields } from '.';
+const API_STATE_LOADING = 0;
+const API_STATE_CONNECTED = 1;
+const API_STATE_NOTCONNECTED = 2;
+
+const NOTIFICATION_PROCESSING = 'processing';
+const NOTIFICATION_SUCCESS = 'success';
+const NOTIFICATION_ERROR = 'error';
 
 class MailchimpSubscribeEdit extends Component {
-	constructor( ...args ) {
-		super( ...args );
-		this.fields = fields.map( field => {
-			field.set = value => this.props.setAttributes( { [ field.id ]: value } );
-			return field;
-		} );
+	constructor() {
+		super( ...arguments );
+		this.state = {
+			audition: null,
+			connected: API_STATE_LOADING,
+			connectURL: null,
+		};
+		this.timeout = null;
 	}
-
-	render() {
-		const { attributes } = this.props;
+	componentDidMount = () => {
+		this.apiCall();
+	};
+	apiCall = () => {
+		const { noticeOperations } = this.props;
+		const path = '/wpcom/v2/mailchimp';
+		const method = 'GET';
+		const fetch = { path, method };
+		apiFetch( fetch ).then(
+			result => {
+				const connected =
+					result.code === 'connected' ? API_STATE_CONNECTED : API_STATE_NOTCONNECTED;
+				const connectURL = result.connect_url;
+				this.setState( { connected, connectURL } );
+			},
+			result => {
+				noticeOperations.removeAllNotices();
+				noticeOperations.createErrorNotice( result.message );
+			}
+		);
+	};
+	auditionNotification = notification => {
+		this.setState( { audition: notification } );
+		if ( this.timeout ) {
+			clearTimeout( this.timeout );
+		}
+		this.timeout = setTimeout( this.clearAudition, 3000 );
+	};
+	clearAudition = () => {
+		this.setState( { audition: null } );
+	};
+	updateProcessingText = processingLabel => {
+		const { setAttributes } = this.props;
+		setAttributes( { processingLabel } );
+		this.auditionNotification( NOTIFICATION_PROCESSING );
+	};
+	updateSuccessText = successLabel => {
+		const { setAttributes } = this.props;
+		setAttributes( { successLabel } );
+		this.auditionNotification( NOTIFICATION_SUCCESS );
+	};
+	updateErrorText = errorLabel => {
+		const { setAttributes } = this.props;
+		setAttributes( { errorLabel } );
+		this.auditionNotification( NOTIFICATION_ERROR );
+	};
+	updateEmailPlaceholder = emailPlaceholder => {
+		const { setAttributes } = this.props;
+		setAttributes( { emailPlaceholder } );
+		this.clearAudition();
+	};
+	updateSubmitLabel = submitLabel => {
+		const { setAttributes } = this.props;
+		setAttributes( { submitLabel } );
+		this.clearAudition();
+	};
+	render = () => {
+		const { attributes, className, notices, noticeUI, setAttributes } = this.props;
+		const { audition, connected } = this.state;
+		const {
+			emailPlaceholder,
+			title,
+			submitLabel,
+			consentText,
+			processingLabel,
+			successLabel,
+			errorLabel,
+		} = attributes;
+		const classPrefix = 'wp-block-jetpack-mailchimp-';
+		const waiting = (
+			<Placeholder icon="email" notices={ notices }>
+				<Spinner />
+			</Placeholder>
+		);
+		const placeholder = (
+			<Placeholder icon="email" label={ __( 'Mailchimp' ) } notices={ notices }>
+				<div className="components-placeholder__instructions">
+					{ __(
+						'You need to connect your MailChimp account and choose a list in order to start collecting Email subscribers.'
+					) }
+					<br />
+					<ExternalLink href="https://wordpress.com/sharing/">
+						{ __( 'Set up MailChimp form' ) }
+					</ExternalLink>
+				</div>
+			</Placeholder>
+		);
+		const inspectorControls = (
+			<InspectorControls>
+				<PanelBody title={ __( 'Text Elements' ) }>
+					<TextControl
+						label={ __( 'Email Placeholder' ) }
+						value={ emailPlaceholder }
+						onChange={ this.updateEmailPlaceholder }
+					/>
+					<TextControl
+						label={ __( 'Submit button label' ) }
+						value={ submitLabel }
+						onChange={ this.updateSubmitLabel }
+					/>
+				</PanelBody>
+				<PanelBody title={ __( 'Notifications' ) }>
+					<TextControl
+						label={ __( 'Processing text' ) }
+						value={ processingLabel }
+						onChange={ this.updateProcessingText }
+					/>
+					<TextControl
+						label={ __( 'Success text' ) }
+						value={ successLabel }
+						onChange={ this.updateSuccessText }
+					/>
+					<TextControl
+						label={ __( 'Error text' ) }
+						value={ errorLabel }
+						onChange={ this.updateErrorText }
+					/>
+				</PanelBody>
+			</InspectorControls>
+		);
+		const blockContent = (
+			<div className={ className }>
+				<RichText
+					tagName="h3"
+					placeholder={ __( 'Write title' ) }
+					value={ title }
+					onChange={ value => setAttributes( { title: value } ) }
+					inlineToolbar
+				/>
+				{ ! audition && (
+					<form ref={ this.formRef }>
+						<TextControl placeholder={ emailPlaceholder } />
+						<Button isPrimary>{ submitLabel }</Button>
+						<RichText
+							tagName="figcaption"
+							placeholder={ __( 'Write consent text' ) }
+							value={ consentText }
+							onChange={ value => setAttributes( { consentText: value } ) }
+							inlineToolbar
+						/>
+					</form>
+				) }
+				{ audition === NOTIFICATION_PROCESSING && (
+					<div
+						className={ `${ classPrefix }notification ${ classPrefix }${ NOTIFICATION_PROCESSING }` }
+					>
+						{ processingLabel }
+					</div>
+				) }
+				{ audition === NOTIFICATION_SUCCESS && (
+					<div
+						className={ `${ classPrefix }notification ${ classPrefix }${ NOTIFICATION_SUCCESS }` }
+					>
+						{ successLabel }
+					</div>
+				) }
+				{ audition === NOTIFICATION_ERROR && (
+					<div className={ `${ classPrefix }notification ${ classPrefix }${ NOTIFICATION_ERROR }` }>
+						{ errorLabel }
+					</div>
+				) }
+			</div>
+		);
 		return (
 			<Fragment>
-				<InspectorControls>
-					{ this.fields.map( field => (
-						<TextControl
-							label={ field.label }
-							key={ field.id }
-							value={ attributes[ field.id ] }
-							onChange={ field.set }
-						/>
-					) ) }
-				</InspectorControls>
-				<ServerSideRender block="jetpack/mailchimp" attributes={ attributes } />
+				{ noticeUI }
+				{ connected === API_STATE_LOADING && waiting }
+				{ connected === API_STATE_NOTCONNECTED && placeholder }
+				{ connected === API_STATE_CONNECTED && inspectorControls }
+				{ connected === API_STATE_CONNECTED && blockContent }
 			</Fragment>
 		);
-	}
+	};
 }
 
-export default MailchimpSubscribeEdit;
+export default withNotices( MailchimpSubscribeEdit );
--- client/gutenberg/extensions/mailchimp/index.js
+++ client/gutenberg/extensions/mailchimp/index.js
@@ -5,36 +5,50 @@
  */
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 import edit from './edit';
+import save from './save';
 import './editor.scss';
 
 export const name = 'mailchimp';
-
-export const fields = [
-	{ id: 'title', label: __( 'Title' ) },
-	{ id: 'email_placeholder', label: __( 'Placeholder' ) },
-	{ id: 'submit_label', label: __( 'Submit button label' ) },
-	{ id: 'consent_text', label: __( 'Consent text' ) },
-	{ id: 'processing_label', label: __( '"Processing" status message' ) },
-	{ id: 'success_label', label: __( 'Success status message' ) },
-	{ id: 'error_label', label: __( 'Error status message' ) },
-];
 
 export const settings = {
 	title: __( 'MailChimp' ),
 	icon: 'email',
 	category: 'jetpack',
-	keywords: [ __( 'email' ), __( 'mailchimp' ), 'jetpack' ],
-
-	edit,
-	attributes: fields.reduce( ( attrs, field ) => {
-		attrs[ field.id ] = {
+	keywords: [ __( 'email' ), __( 'mailchimp' ) ],
+	attributes: {
+		title: {
 			type: 'string',
-			default: '',
-		};
-		return attrs;
-	}, {} ),
-
-	save: function() {
-		return null;
+			default: __( 'Join my email list' ),
+		},
+		emailPlaceholder: {
+			type: 'string',
+			default: __( 'Enter your email' ),
+		},
+		submitLabel: {
+			type: 'string',
+			default: __( 'Join My Email List' ),
+		},
+		consentText: {
+			type: 'string',
+			default: __(
+				'By clicking submit, you agree to share your email address with the site owner and MailChimp to receive marketing, updates, and other emails from the site owner. Use the unsubscribe link in those emails to opt out at any time.'
+			),
+		},
+		processingLabel: {
+			type: 'string',
+			default: __( 'Processing...' ),
+		},
+		successLabel: {
+			type: 'string',
+			default: __( "Success! You've been added to the list." ),
+		},
+		errorLabel: {
+			type: 'string',
+			default: __(
+				'Oh no! Unfortunately there was an error. Please try reloading this page and adding your email once more.'
+			),
+		},
 	},
+	edit,
+	save,
 };
