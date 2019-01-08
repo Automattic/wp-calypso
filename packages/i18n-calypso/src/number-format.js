40330d22ae2af15e45a0e17554039e095dbc9ae8
Jon Surrell <jon.surrell@automattic.com>
2019-01-08 14:42:45 +0100

    Prettier

--- packages/i18n-calypso/src/number-format.js
+++ packages/i18n-calypso/src/number-format.js
@@ -10,29 +10,24 @@
  */
 
 export default function number_format( number, decimals, dec_point, thousands_sep ) {
-  number = ( number + '' )
-    .replace( /[^0-9+\-Ee.]/g, '' );
-  let n = ! isFinite( +number ) ? 0 : +number,
-    prec = ! isFinite( +decimals ) ? 0 : Math.abs( decimals ),
-    sep = ( typeof thousands_sep === 'undefined' ) ? ',' : thousands_sep,
-    dec = ( typeof dec_point === 'undefined' ) ? '.' : dec_point,
-    s = '',
-    toFixedFix = function( n, prec ) {
-      const k = Math.pow( 10, prec );
-      return '' + ( Math.round( n * k ) / k )
-          .toFixed( prec );
-    };
-  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-  s = ( prec ? toFixedFix( n, prec ) : '' + Math.round( n ) )
-    .split( '.' );
-  if ( s[ 0 ].length > 3 ) {
-    s[ 0 ] = s[ 0 ].replace( /\B(?=(?:\d{3})+(?!\d))/g, sep );
-  }
-  if ( ( s[ 1 ] || '' )
-        .length < prec ) {
-    s[ 1 ] = s[ 1 ] || '';
-    s[ 1 ] += new Array( prec - s[ 1 ].length + 1 )
-      .join( '0' );
-  }
-  return s.join( dec );
+	number = ( number + '' ).replace( /[^0-9+\-Ee.]/g, '' );
+	let n = ! isFinite( +number ) ? 0 : +number,
+		prec = ! isFinite( +decimals ) ? 0 : Math.abs( decimals ),
+		sep = typeof thousands_sep === 'undefined' ? ',' : thousands_sep,
+		dec = typeof dec_point === 'undefined' ? '.' : dec_point,
+		s = '',
+		toFixedFix = function( n, prec ) {
+			const k = Math.pow( 10, prec );
+			return '' + ( Math.round( n * k ) / k ).toFixed( prec );
+		};
+	// Fix for IE parseFloat(0.55).toFixed(0) = 0;
+	s = ( prec ? toFixedFix( n, prec ) : '' + Math.round( n ) ).split( '.' );
+	if ( s[ 0 ].length > 3 ) {
+		s[ 0 ] = s[ 0 ].replace( /\B(?=(?:\d{3})+(?!\d))/g, sep );
+	}
+	if ( ( s[ 1 ] || '' ).length < prec ) {
+		s[ 1 ] = s[ 1 ] || '';
+		s[ 1 ] += new Array( prec - s[ 1 ].length + 1 ).join( '0' );
+	}
+	return s.join( dec );
 }
--- packages/i18n-calypso/test/data/index.js
+++ packages/i18n-calypso/test/data/index.js
@@ -8,36 +8,23 @@ const locale = {
 		'Content-Type': 'text/plain; charset=UTF-8',
 		'PO-Revision-Date': '2014-08-11 23:50:52+0000',
 		'Content-Transfer-Encoding': '8bit',
-		'Project-Id-Version': 'WordPress.com'
+		'Project-Id-Version': 'WordPress.com',
 	},
-	'test1': [
-		'translation1'
-	],
-	'test2': null,
-	'thecontext\u0004test3': [
-		'translation3'
-	],
-	'test4': [
-		'translation4'
-	],
-	'test-will-overwrite': [
-		'translation1'
-	],
-	'Featured': [
-		'translation-without-context'
-	],
-	'plural-test': [
-		'plural-test singular translation',
-		'plural-test multiple translation'
-	],
+	test1: [ 'translation1' ],
+	test2: null,
+	'thecontext\u0004test3': [ 'translation3' ],
+	test4: [ 'translation4' ],
+	'test-will-overwrite': [ 'translation1' ],
+	Featured: [ 'translation-without-context' ],
+	'plural-test': [ 'plural-test singular translation', 'plural-test multiple translation' ],
 	'plural-test new syntax': [
 		'plural-test new syntax translated, single',
-		'plural-test new syntax translated, plural'
+		'plural-test new syntax translated, plural',
 	],
 	'%(numberSelected)s site selected': [
 		'%(numberSelected)s sites selected',
 		'translation singular',
-		'translation plural'
+		'translation plural',
 	],
 	'Navigation item\u0004Menu': null,
 	'Section title\u0004Reader': null,
@@ -52,102 +39,40 @@ const locale = {
 	'Menu item\u0004Jetpack Features': null,
 	'verb\u0004Update': null,
 	'Author byline\u0004By': null,
-	'Visit Plugin Site': [
-		''
-	],
-	'Activate': [
-		'Aktivieren'
-	],
-	'Search...': [
-		'Suchen...'
-	],
-	'Select None': [
-		''
-	],
-	'Select All': [
-		'Alle auswählen'
-	],
-	'All sites selected': [
-		''
-	],
-	'%(numberTotal)s site total': [
-		'%(numberTotal)s sites total',
-		'',
-		''
-	],
-	'No sites found': [
-		'Keine Websites gefunden'
-	],
-	'Pages': [
-		'Seiten'
-	],
-	'Stats': [
-		'Statistik'
-	],
-	'Sharing': [
-		'Sharing'
-	],
-	'Settings': [
-		'Einstellungen'
-	],
-	'Manage': [
-		'Verwalten'
-	],
-	'Customize': [
-		'Anpassen'
-	],
-	'Domains': [
-		'Domains'
-	],
-	'Upgrade': [
-		'Aktualisierung'
-	],
-	'Posts': [
-		'Beiträge'
-	],
-	'Theme': [
-		'Theme'
-	],
-	'Open site in a new tab': [
-		''
-	],
+	'Visit Plugin Site': [ '' ],
+	Activate: [ 'Aktivieren' ],
+	'Search...': [ 'Suchen...' ],
+	'Select None': [ '' ],
+	'Select All': [ 'Alle auswählen' ],
+	'All sites selected': [ '' ],
+	'%(numberTotal)s site total': [ '%(numberTotal)s sites total', '', '' ],
+	'No sites found': [ 'Keine Websites gefunden' ],
+	Pages: [ 'Seiten' ],
+	Stats: [ 'Statistik' ],
+	Sharing: [ 'Sharing' ],
+	Settings: [ 'Einstellungen' ],
+	Manage: [ 'Verwalten' ],
+	Customize: [ 'Anpassen' ],
+	Domains: [ 'Domains' ],
+	Upgrade: [ 'Aktualisierung' ],
+	Posts: [ 'Beiträge' ],
+	Theme: [ 'Theme' ],
+	'Open site in a new tab': [ '' ],
 	'Filter Label\u0004All Sites': null,
 	'Filter label for site list\u0004Public': null,
 	'Filter label for site list\u0004Private': null,
 	'future time\u0004in %s': null,
-	'a few seconds': [
-		'einige Sekunden'
-	],
-	'a minute': [
-		'eine Minute'
-	],
-	'%d minutes': [
-		'%d Minuten'
-	],
-	'%d hours': [
-		'%d Stunden'
-	],
-	'%d days': [
-		'%d Tage'
-	],
-	'a month': [
-		'ein Monat'
-	],
-	'%d months': [
-		'%d Monaten'
-	],
-	'a year': [
-		'ein Jahr'
-	],
-	'%d years': [
-		'%d Jahre'
-	],
-	'number_format_thousands_sep': [
-		'.'
-	],
-	'number_format_decimals': [
-		','
-	]
+	'a few seconds': [ 'einige Sekunden' ],
+	'a minute': [ 'eine Minute' ],
+	'%d minutes': [ '%d Minuten' ],
+	'%d hours': [ '%d Stunden' ],
+	'%d days': [ '%d Tage' ],
+	'a month': [ 'ein Monat' ],
+	'%d months': [ '%d Monaten' ],
+	'a year': [ 'ein Jahr' ],
+	'%d years': [ '%d Jahre' ],
+	number_format_thousands_sep: [ '.' ],
+	number_format_decimals: [ ',' ],
 };
 
 exports.locale = locale;
