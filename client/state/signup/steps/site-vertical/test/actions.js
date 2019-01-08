e5ea9494bfec53f772cfc113567113878ecdced0
Ramon <ramonjd@users.noreply.github.com>
2019-01-08 11:39:16 +1100

    NUX Onboarding: update site-topic step to use verticals endpoint (#29172)
    * First commit with some experimentation only.
    
    * Creating custom site vertical search component
    
    * Added component in about field in the main flow
    Added basic component test
    Updated spelling for SuggestionSearch README
    Removed duplicate const assignment: siteTitleInput
    
    * Adding snapshot!
    
    * We are now passing the full, matching object from the vertical api collection.
    Removing hints data js
    Updating site topic step to use vertical slug for tracking where available.
    Updating tests and readme.
    
    * Debouncing the requestSiteVerticals method for more predictable behavious.
    We don't need to fetch results if the search term already exists in the cached results.
    Updated tests
    
    * Remove console.log!
    
    * This is to ensure that the free text search doesn't bork with any regex lookup of the suggestion list.
    
    * Removing unused topicLabel argument
    
    * In items.jsx, ensuring that we're comparing lowercase to lowercase when highlighting partial search results. Updating the element to be a button for accessibility.
    In site-verticals-suggestion-search, checking the verticals results to ensure we don't fire off another API call if the user input matches a valid vertical.
    
    * Plugging in the site vertical state tree selectors and actions.
    
    * Correcting the properties sent to formStateController.
    Only setting `N/A` value for the tracking call.
    Adding the corresponding tracking call to the change topic action.
    Removing the onboarding-dev condition around setting the site topic
    
    Bumping the debounce wait time to allow for more staccato-type typing
    
    Ensuring that we cancel debounced invocations should a user quickly delete the characters to a length below the trigger count.
    Removing the state from the site topic component because we really don't need it and it causes extra redraws.
    
    Added characters to trigger search value to the props object. Updated tests and README accordingly
    
    * Reverting the addition of `calypso_signup_actions_select_site_topic` tracking event.
    Now tracking whether the input is user defined. Updated tests to reflect this change.
    
    * Addinf `isUserInput` to the state so that we can reliably pass the user's selected value on step submit, even when the value hasn't been modified.

--- client/state/signup/steps/site-vertical/test/actions.js
+++ client/state/signup/steps/site-vertical/test/actions.js
@@ -9,6 +9,7 @@ import { SIGNUP_STEPS_SITE_VERTICAL_SET } from 'state/action-types';
 describe( 'setSiteVertical()', () => {
 	test( 'should return the expected action object', () => {
 		const siteVertical = {
+			isUserInput: false,
 			name: 'heureux',
 			slug: 'happy',
 		};
