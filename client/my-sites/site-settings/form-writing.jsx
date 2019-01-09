ae3fb2e98381b7f8f2edb66cf31e4dfffe1bd725
Marin Atanasov <tyxla@abv.bg>
2019-01-09 16:34:52 +0200

    Settings: Remove last isJetpackMinimumVersion check from writing settings

--- client/my-sites/site-settings/form-writing.jsx
+++ client/my-sites/site-settings/form-writing.jsx
@@ -16,7 +16,7 @@ import config from 'config';
 import PressThis from './press-this';
 import QueryTaxonomies from 'components/data/query-taxonomies';
 import TaxonomyCard from './taxonomies/taxonomy-card';
-import { isJetpackSite, isJetpackMinimumVersion } from 'state/sites/selectors';
+import { isJetpackSite } from 'state/sites/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { requestPostTypes } from 'state/post-types/actions';
 import Composing from './composing';
@@ -195,7 +195,6 @@ const connectComponent = connect(
 			siteId,
 			isMasterbarSectionVisible:
 				siteIsJetpack &&
-				isJetpackMinimumVersion( state, siteId, '4.8' ) &&
 				// Masterbar can't be turned off on Atomic sites - don't show the toggle in that case
 				! siteIsAutomatedTransfer,
 			isPodcastingSupported,
