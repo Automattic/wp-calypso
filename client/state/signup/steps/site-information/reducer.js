1b63433391c3dfd00352f0d66519c25ce4733bc3
ramonjd <ramonjd@gmail.com>
2019-01-11 18:27:20 +1100

    Adding default state for site information, and now submitting the entire state object. This is because we have a provided dependencies check that will try to find title, address and phone values for this step, even though they might not exist in the UI.

--- client/state/signup/steps/site-information/reducer.js
+++ client/state/signup/steps/site-information/reducer.js
@@ -9,8 +9,14 @@ import { SIGNUP_COMPLETE_RESET, SIGNUP_STEPS_SITE_INFORMATION_SET } from 'state/
 import { createReducer } from 'state/utils';
 import { siteInformationSchema } from './schema';
 
+const initialState = {
+	address: '',
+	phone: '',
+	title: '',
+};
+
 export default createReducer(
-	{},
+	initialState,
 	{
 		[ SIGNUP_STEPS_SITE_INFORMATION_SET ]: ( state, { data } ) => {
 			return {
