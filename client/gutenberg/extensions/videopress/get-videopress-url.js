8a056b6e8473d1132c85892ad58870134f8e8e5a
mmtr <miguel.torres@automattic.com>
2019-01-11 15:45:55 +0100

    Check that the videos are converted

--- client/gutenberg/extensions/videopress/get-videopress-url.js
+++ client/gutenberg/extensions/videopress/get-videopress-url.js
@@ -23,17 +23,25 @@ const getVideoPressUrl = async videoId => {
 		addSiteSlug: false,
 	} );
 
+	const {
+		files_status: filesStatus,
+		file_url_base: { https: fileUrlBase },
+		files,
+	} = videoPressData;
+
 	const bestResolution = [ 'hd', 'dvd', 'std' ].find( resolution => {
-		return !! videoPressData.files_status[ resolution ];
+		return !! filesStatus[ resolution ];
 	} );
 
 	const bestFormat = [ 'mp4', 'ogv' ].find( format => {
-		return !! videoPressData.files_status[ bestResolution ][ format ];
+		return 'DONE' === filesStatus[ bestResolution ][ format ];
 	} );
 
-	return `${ videoPressData.file_url_base.https }${
-		videoPressData.files[ bestResolution ][ bestFormat ]
-	}`;
+	if ( ! bestResolution || ! bestFormat ) {
+		return null;
+	}
+
+	return `${ fileUrlBase }${ files[ bestResolution ][ bestFormat ] }`;
 };
 
 export default getVideoPressUrl;
