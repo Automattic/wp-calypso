017007cbbd95303039f0f419b11c79789f567892
belcherj <belcherj@gmail.com>
2019-01-15 16:47:01 -0500

    Remove unintended change

--- client/components/upgrades/gsuite/gsuite-dialog/users.jsx
+++ client/components/upgrades/gsuite/gsuite-dialog/users.jsx
@@ -116,14 +116,14 @@ class GoogleAppsUsers extends React.Component {
 
 const recordAddUserClick = section =>
 	composeAnalytics(
-		recordTracksEvent( 'calypso_gsuite_add_user_button_click', { section } ),
-		recordGoogleEvent( 'Domain Search', 'Clicked "Add User" Button in G Suite Dialog' )
+		recordTracksEvent( 'calypso_google_apps_add_user_button_click', { section } ),
+		recordGoogleEvent( 'Domain Search', 'Clicked "Add User" Button in Google Apps Dialog' )
 	);
 
 const recordInputFocus = ( userIndex, fieldName, inputValue ) =>
 	recordGoogleEvent(
 		'Domain Search',
-		`Focused On "${ fieldName }" Input for User #${ userIndex } in G Suite Dialog`,
+		`Focused On "${ fieldName }" Input for User #${ userIndex } in Google Apps Dialog`,
 		'Input Value',
 		inputValue
 	);
