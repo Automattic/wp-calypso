57a58526e73b9d3a402052c0604c40d8547ffbd9
Jon Surrell <jon.surrell@automattic.com>
2019-01-16 13:38:54 +0100

    Fix

--- client/gutenberg/extensions/demo-breakage/editor.js
+++ client/gutenberg/extensions/demo-breakage/editor.js
@@ -3,8 +3,6 @@ import paypalImage2x from '../simple-payments/paypal-button@2x.png';
 window.onload = function() {
 	const img = document.createElement( 'img' );
 	img.src = paypalImage2x;
-	// img.setAttribute( 'sizes', `${ paypalImage2x } 100px` );
-	img.setAttribute( 'srcset', `${ paypalImage2x } 100px` );
 	document.body.appendChild( img );
 	console.log( 'Appended src `$o` in img: %o', paypalImage2x, img );
 };
