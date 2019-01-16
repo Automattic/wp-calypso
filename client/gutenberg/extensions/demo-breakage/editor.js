d1f1c96f5b21ab0cba96e1ab02fe430274222715
Jon Surrell <jon.surrell@automattic.com>
2019-01-16 13:25:16 +0100

    Demo issue

--- client/gutenberg/extensions/demo-breakage/editor.js
+++ client/gutenberg/extensions/demo-breakage/editor.js
@@ -1,8 +1,10 @@
-import paypalImage2x from './paypal-button@2x.png';
+import paypalImage2x from '../simple-payments/paypal-button@2x.png';
 
 window.onload = function() {
 	const img = document.createElement( 'img' );
 	img.src = paypalImage2x;
-	document.appendChild( img );
+	// img.setAttribute( 'sizes', `${ paypalImage2x } 100px` );
+	img.setAttribute( 'srcset', `${ paypalImage2x } 100px` );
+	document.body.appendChild( img );
 	console.log( 'Appended src `$o` in img: %o', paypalImage2x, img );
 };
