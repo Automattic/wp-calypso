7e71edd1c8cae0ed77d4eb79010679bddf847ace
mmtr <miguel.torres@automattic.com>
2019-01-17 13:11:53 +0100

    Refactor how the Jetpack Gutenberg extensions are registered.
    - Add a new utility for registering extensions that are neither blocks
      nor plugins.
    - Remove `refreshRegistrations` in favor of using the `editor.js` files
      so there is no duplicated logic.

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -3,7 +3,7 @@
 /**
  * External dependencies
  */
-import { sprintf } from '@wordpress/i18n';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies
@@ -11,45 +11,20 @@ import { sprintf } from '@wordpress/i18n';
 import './shared/public-path';
 import './editor-shared/block-category'; // Register the Jetpack category
 import extensionSlugsJson from './index.json';
-import { _x } from './utils/i18n';
 import { isEnabled } from 'config';
+import getJetpackData from './utils/get-jetpack-data';
 
 const extensionSlugs = [
 	...extensionSlugsJson.production,
 	...( isEnabled( 'jetpack/blocks/beta' ) ? extensionSlugsJson.beta : [] ),
 ];
 
-export async function getExtensions() {
-	const promises = extensionSlugs.map( slug =>
-		/**
-		 * Dynamically pull in extensions
-		 *
-		 * At build time, webpack needs to compile modules that will be dynamically imported at
-		 * runtime.
-		 *
-		 * Because the import path is dynamic (it includes a variable), webpack does not know at
-		 * build time what might be imported at runtime. Therefore, webpack will attempt to find any
-		 * import that could be reached by completing the string and build the modules.
-		 *
-		 * By fixing parts of the path with literal strings, we can limit what webpack needs bundle
-		 * here. However, any number of path parts could be inserted in the variable. Therefore, we
-		 * must also include a `webpackInclude` comment to fix our string further and ensure webpack
-		 * does not attempt to build problematic targets and instead only parses our
-		 * intended modules.
-		 */
-		import( /* webpackMode: "eager" */
-		/* webpackInclude: /\/gutenberg\/extensions\/[a-zA-Z0-9_-]+\/index.js$/ */
-		`../../${ slug }/index.js` ).then( ( { childBlocks, name, settings } ) => ( {
-			childBlocks,
-			name,
-			settings: extensionSlugsJson.beta.includes( slug )
-				? {
-						...settings,
-						title: sprintf( _x( '%s (beta)', 'Gutenberg Block in beta stage' ), settings.title ),
-				  }
-				: settings,
-		} ) )
-	);
+export function registerExtensions() {
+	const extensionAvailability = get( getJetpackData(), [ 'available_blocks' ] );
 
-	return await Promise.all( promises );
+	if ( ! extensionAvailability ) {
+		return;
+	}
+
+	extensionSlugs.forEach( slug => require( `../../${ slug }/editor.js` ) );
 }
--- client/gutenberg/extensions/presets/jetpack/utils/register-jetpack-block.js
+++ client/gutenberg/extensions/presets/jetpack/utils/register-jetpack-block.js
@@ -2,7 +2,8 @@
 /**
  * External dependencies
  */
-import { registerBlockType } from '@wordpress/blocks';
+import { getBlockType, registerBlockType, unregisterBlockType } from '@wordpress/blocks';
+import { without } from 'lodash';
 
 /**
  * Internal dependencies
@@ -18,18 +19,40 @@ import isJetpackExtensionAvailable from './is-jetpack-extension-available';
  * @returns {object|false} Either false if the block is not available, or the results of `registerBlockType`
  */
 export default function registerJetpackBlock( name, settings, childBlocks = [] ) {
-	if ( ! isJetpackExtensionAvailable( name ) ) {
+	const available = isJetpackExtensionAvailable( name );
+	const blockName = `jetpack/${ name }`;
+	const registered = getBlockType( blockName );
+
+	if ( available && ! registered ) {
+		const result = registerBlockType( blockName, settings );
+
+		if ( childBlocks ) {
+			childBlocks.forEach( ( { name: childName, settings: childSettings } ) => {
+				// This might have been registered by another parent before
+				if ( ! getBlockType( `jetpack/${ childName }` ) ) {
+					registerBlockType( `jetpack/${ childName }`, childSettings );
+				}
+			} );
+		}
+
+		return result;
+	} else if ( ! available ) {
+		if ( registered ) {
+			if ( childBlocks ) {
+				childBlocks.forEach( ( { name: childName } ) => {
+					const childBlock = getBlockType( `jetpack/${ childName }` );
+					const otherParents = without( childBlock.parent, blockName );
+
+					// Are any of the other parents currently registered?
+					if ( ! otherParents.some( getBlockType ) ) {
+						unregisterBlockType( `jetpack/${ childName }` );
+					}
+				} );
+			}
+			unregisterBlockType( blockName );
+		}
+
 		// TODO: check 'unavailable_reason' and respond accordingly
 		return false;
 	}
-
-	const result = registerBlockType( `jetpack/${ name }`, settings );
-
-	// Register child blocks. Using `registerBlockType()` directly avoids availability checks -- if
-	// their parent is available, we register them all, without checking for their individual availability.
-	childBlocks.forEach( childBlock =>
-		registerBlockType( `jetpack/${ childBlock.name }`, childBlock.settings )
-	);
-
-	return result;
 }
