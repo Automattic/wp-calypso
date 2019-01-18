cd307ae4c6460422bda8ce1c2c2babf7937b58c8
mmtr <miguel.torres@automattic.com>
2019-01-18 16:27:19 +0100

    Register extension with Jetpack preset utility

--- client/gutenberg/extensions/videopress/editor.js
+++ client/gutenberg/extensions/videopress/editor.js
@@ -8,9 +8,14 @@ import { addFilter } from '@wordpress/hooks';
 /**
  * Internal dependencies
  */
+import registerJetpackExtension from 'gutenberg/extensions/presets/jetpack/utils/register-jetpack-extension';
 import withVideoPressEdit from './edit';
 
-const addVideoPressSupport = ( settings, name ) =>
-	'core/video' === name ? { ...settings, edit: withVideoPressEdit( settings.edit ) } : settings;
+const initVideoPressExtension = () => {
+	const addVideoPressSupport = ( settings, name ) =>
+		'core/video' === name ? { ...settings, edit: withVideoPressEdit( settings.edit ) } : settings;
 
-addFilter( 'blocks.registerBlockType', 'gutenberg/extensions/videopress', addVideoPressSupport );
+	addFilter( 'blocks.registerBlockType', 'gutenberg/extensions/videopress', addVideoPressSupport );
+};
+
+registerJetpackExtension( 'videopress', initVideoPressExtension );
