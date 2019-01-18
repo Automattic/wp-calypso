b00765e49daa19f79cf73e79c63c35d8586306c2
Thomas Guillot <thomasguillot@users.noreply.github.com>
2019-01-18 15:43:23 +0000

    Related Posts block: Introduce a rect SVG to replicate image

--- client/gutenberg/extensions/related-posts/edit.jsx
+++ client/gutenberg/extensions/related-posts/edit.jsx
@@ -36,6 +36,14 @@ function PlaceholderPostEdit( props ) {
 						className={ `${ previewClassName }-post-image-placeholder-icon` }
 						aria-label={ __( 'Placeholder image' ) }
 					>
+						<SVG
+							xmlns="http://www.w3.org/2000/svg"
+							width="100%"
+							height="100%"
+							viewBox="0 0 350 200"
+						>
+							<Path d="M0 0h350v200H0z" fill="#8B8B96" fill-opacity=".1" />
+						</SVG>
 						<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 							<Path fill="none" d="M0 0h24v24H0V0z" />
 							<Path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-4.86 8.86l-3 3.87L9 13.14 6 17h12l-3.86-5.14z" />
