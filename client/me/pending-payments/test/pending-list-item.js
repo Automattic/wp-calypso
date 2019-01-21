7349987f2154679d2baf4d141e70d4fc55324673
Louis Laugesen <louis.laugesen@gmail.com>
2019-01-21 18:30:56 +1100

    Test fixes

--- client/me/pending-payments/test/pending-list-item.js
+++ client/me/pending-payments/test/pending-list-item.js
@@ -25,10 +25,9 @@ describe( 'PendingListItem', () => {
 		// Check nesting
 		'Card.pending-payments__list-item .pending-payments__list-item-wrapper .pending-payments__list-item-details',
 		'.pending-payments__list-item-details .pending-payments__list-item-title',
-		'.pending-payments__list-item-details .pending-payments__list-item-purchase-type',
-		'.pending-payments__list-item-details .pending-payments__list-item-purchase-cost',
+		'.pending-payments__list-item-details .pending-payments__list-item-payment',
 		'.pending-payments__list-item-details .pending-payments__list-item-actions',
-		'.pending-payments__list-item-actions Button[href="/help/contact"][primary=true]',
+		'.pending-payments__list-item-actions Button[href="/help/contact"]',
 	];
 
 	assertions.forEach( rule => {
