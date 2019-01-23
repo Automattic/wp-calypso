2d71c762fbd7081c6ebf7885e221eda83f49b5ed
Bernie Reiter <ockham@raz.or.at>
2019-01-23 23:54:30 +0100

    Encapsulate Slideshow component

--- client/gutenberg/extensions/slideshow/save.js
+++ client/gutenberg/extensions/slideshow/save.js
@@ -1,51 +1,10 @@
 /** @format */
 
 /**
- * External dependencies
+ * Internal dependencies
  */
-import { Component } from '@wordpress/element';
-import classnames from 'classnames';
+import Slideshow from './slideshow';
 
-class SlideshowSave extends Component {
-	render() {
-		const { attributes, className } = this.props;
-		const { align, effect, images } = attributes;
-		const alignClassName = align ? `align${ align }` : null;
-		const classes = classnames( className, alignClassName );
-		const swiperClassNames = classnames( className, 'swiper-container' );
-
-		return (
-			<div className={ classes } effect={ effect }>
-				<div className={ swiperClassNames }>
-					<div className="swiper-wrapper">
-						{ images.map( ( image, index ) => {
-							const { alt, caption, height, id, url, width } = image;
-
-							//const caption = figcaption ? figcaption.props.children : null;
-							const style = {
-								backgroundImage: `url(${ url })`,
-								height: 400, // TODO
-							};
-							return (
-								<div className="swiper-slide">
-									<div className="slide-background atavist-cover-background-color" />
-									<div
-										className="wp-block-slideshow-image-container"
-										style={ style }
-										title={ alt }
-									/>
-									{ caption && <p className="slideshow-slide-caption">{ caption }</p> }
-								</div>
-							);
-						} ) }
-					</div>
-					<div className="swiper-pagination swiper-pagination-white" />
-					<div className="swiper-button-prev swiper-button-white" />
-					<div className="swiper-button-next swiper-button-white" />
-				</div>
-			</div>
-		);
-	}
-}
-
-export default SlideshowSave;
+export default ( { attributes: { align, effect, images }, className } ) => (
+	<Slideshow align={ align } className={ className } effect={ effect } images={ images } />
+);
---
+++ client/gutenberg/extensions/slideshow/slideshow.js
@@ -0,0 +1,49 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { Component } from '@wordpress/element';
+import classnames from 'classnames';
+
+class Slideshow extends Component {
+	render() {
+		const { className, align, effect, images } = this.props;
+		const alignClassName = align ? `align${ align }` : null;
+		const classes = classnames( className, alignClassName );
+		const swiperClassNames = classnames( className, 'swiper-container' );
+
+		return (
+			<div className={ classes } effect={ effect }>
+				<div className={ swiperClassNames }>
+					<div className="swiper-wrapper">
+						{ images.map( ( image, index ) => {
+							const { alt, caption, height, id, url, width } = image;
+
+							const style = {
+								backgroundImage: `url(${ url })`,
+								height: 400, // TODO
+							};
+							return (
+								<div className="swiper-slide">
+									<div className="slide-background atavist-cover-background-color" />
+									<div
+										className="wp-block-slideshow-image-container"
+										style={ style }
+										title={ alt }
+									/>
+									{ caption && <p className="slideshow-slide-caption">{ caption }</p> }
+								</div>
+							);
+						} ) }
+					</div>
+					<div className="swiper-pagination swiper-pagination-white" />
+					<div className="swiper-button-prev swiper-button-white" />
+					<div className="swiper-button-next swiper-button-white" />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Slideshow;
