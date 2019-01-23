18bfe654910c0c54ecb79d2dad2f23c19ac80674
Bernie Reiter <ockham@raz.or.at>
2019-01-24 00:58:42 +0100

    No need to put swiperInstance in state

--- client/gutenberg/extensions/slideshow/component.js
+++ client/gutenberg/extensions/slideshow/component.js
@@ -14,13 +14,8 @@ import createSwiper from './create-swiper';
 import SlideshowComponent from './slideshow';
 
 export class Slideshow extends Component {
-	state = {
-		imageHeight: 400,
-	};
-
 	render() {
 		const { align, className, effect, images } = this.props;
-		const { imageHeight } = this.state;
 
 		return (
 			<SlideshowComponent
@@ -33,48 +28,33 @@ export class Slideshow extends Component {
 	}
 
 	componentDidMount() {
-		this.buildSwiper();
+		const { effect } = this.props;
+		this.swiperInstance = createSwiper( { effect } );
 	}
 
 	componentDidUpdate( prevProps ) {
-		const { swiperInstance } = this.state;
 		const { align, effect, images } = this.props;
 		if ( ! isEqual( images, prevProps.images ) ) {
 			this.sizeSlideshow();
 		}
 		/* A change in alignment or images only needs an update */
 		if ( align !== prevProps.align || ! isEqual( images, prevProps.images ) ) {
-			swiperInstance.update();
+			this.swiperInstance.update();
 		}
 		/* A change in effect requires a full rebuild */
 		if ( effect !== prevProps.effect ) {
-			const activeIndex = swiperInstance.activeIndex;
-			swiperInstance.destroy( true, true );
-			this.buildSwiper( activeIndex );
+			const { activeIndex } = this.swiperInstance;
+			this.swiperInstance.destroy( true, true );
+			this.swiperInstance = createSwiper( { effect, initialSlide: activeIndex } );
 		}
 	}
 
-	buildSwiper = ( initialSlide = 0 ) => {
-		const { effect } = this.props;
-		const swiperInstance = createSwiper( { effect, init: false, initialSlide } );
-		this.setState(
-			{
-				swiperInstance,
-			},
-			() => {
-				swiperInstance.init();
-				this.sizeSlideshow();
-			}
-		);
-	};
-
-	sizeSlideshow = () => {
-		const { swiperInstance } = this.state;
-		const ratio = Math.max( Math.min( this.props.images[ 0 ].ratio, 16 / 9 ), 1 );
-		const sanityHeight = window.innerHeight * 0.8;
-		const imageHeight = Math.min( swiperInstance.width / ratio, sanityHeight );
-		this.setState( { imageHeight } );
-	};
+	// sizeSlideshow = () => {
+	// 	const ratio = Math.max( Math.min( this.props.images[ 0 ].ratio, 16 / 9 ), 1 );
+	// 	const sanityHeight = window.innerHeight * 0.8;
+	// 	const imageHeight = Math.min( this.swiperInstance.width / ratio, sanityHeight );
+	// 	this.setState( { imageHeight } );
+	// };
 }
 
 Slideshow.defaultProps = {
