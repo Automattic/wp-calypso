e7c0446b23952899bff594b5cf7b6a37832201ab
Bernie Reiter <ockham@raz.or.at>
2019-01-25 01:16:08 +0100

    Use correct effect in view.js

--- client/gutenberg/extensions/slideshow/view.js
+++ client/gutenberg/extensions/slideshow/view.js
@@ -1,14 +1,23 @@
 /** @format */
 
 /**
+ * External dependencies
+ */
+import { forEach } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import createSwiper from './create-swiper';
 import './style.scss';
 
-// TODO: Use actual attributes
-
 typeof window !== 'undefined' &&
 	window.addEventListener( 'load', function() {
-		createSwiper( { effect: 'slide', init: true, initialSlide: 0 } );
+		const slideshowBlocks = document.getElementsByClassName( 'wp-block-jetpack-slideshow' );
+		forEach( slideshowBlocks, slideshowBlock => {
+			const { effect } = slideshowBlock.dataset;
+			const slideshowContainer = slideshowBlock.getElementsByClassName( 'swiper-container' )[ 0 ];
+
+			createSwiper( slideshowContainer, { effect, init: true, initialSlide: 0 } );
+		} );
 	} );
