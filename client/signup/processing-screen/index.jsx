7ea16245a4d21c9f0453c7f283f844a4b33d7765
ramonjd <ramonjd@gmail.com>
2019-01-29 12:00:17 +1100

    Adding flow to destination check in `showPreviewAfterLogin`. See: #30407

--- client/signup/processing-screen/index.jsx
+++ client/signup/processing-screen/index.jsx
@@ -137,7 +137,9 @@ export class SignupProcessingScreen extends Component {
 	}
 
 	showPreviewAfterLogin = () => {
-		const destination = [ 'onboarding', 'onboarding-dev' ].includes( this.props.flowName )
+		const destination = [ 'onboarding', 'onboarding-dev', 'onboarding-for-business' ].includes(
+			this.props.flowName
+		)
 			? 'view'
 			: 'checklist';
 
