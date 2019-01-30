856a34181a5f921f4640763316412868ed0e6724
Jason Moon <jason.moon@automattic.com>
2019-01-30 13:24:59 -0700

    Gutenberg Visited: Add keywords for block discovery

--- client/gutenberg/extensions/visited/index.js
+++ client/gutenberg/extensions/visited/index.js
@@ -2,7 +2,7 @@
 /**
  * Internal dependencies
  */
-import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
+import { __, _x } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 import edit from './components/edit';
 import save from './components/save';
 import { CRITERIA_AFTER, DEFAULT_THRESHOLD } from './constants';
@@ -26,6 +26,11 @@ export const settings = {
 		"Control block visibility depending on how many times they've visited the page."
 	),
 	icon: 'universal-access-alt',
+	keywords: [
+		_x( 'traffic', 'block search term' ),
+		_x( 'visitors', 'block search term' ),
+		_x( 'visibility', 'block search term' ),
+	],
 	supports: { html: false },
 	title: __( 'Visited' ),
 	edit,
