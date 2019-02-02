a1fc4424989e83cf91207c7b22016690a8c8f781
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-02-01 16:23:14 -0800

    Add new icon

--- client/gutenberg/extensions/wordads/index.js
+++ client/gutenberg/extensions/wordads/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { ExternalLink, Rect, SVG } from '@wordpress/components';
+import { ExternalLink, SVG, Path } from '@wordpress/components';
 import { Fragment } from '@wordpress/element';
 
 /**
@@ -11,6 +11,12 @@ import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 import edit from './edit';
 
 export const name = 'wordads';
+export const icon = (
+	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<Path d="M0 0h24v24H0z" fill="none"/>
+		<Path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7 12h-2v-2h2v2zm0-4h-2V6h2v4z"/>
+	</SVG>
+)
 
 export const settings = {
 	title: __( 'Ad' ),
@@ -22,12 +28,7 @@ export const settings = {
 		</Fragment>
 	),
 
-	/* @TODO icon */
-	icon: (
-		<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-			<Rect width="24" height="24" x="0" y="0" fill="#f00" />
-		</SVG>
-	),
+	icon: icon,
 
 	category: 'jetpack',
 
