76aadc854c75d5394ba366d15f065792ccca65d0
Sérgio Gomes <mail@sgomes.com>
2019-02-04 12:33:44 +0000

    Reimplement lib/viewport using matchMedia.
    This should reduce layout recalcs (matchMedia doesn't trigger layout)
    while maintaining compatibility with all target browsers. The public
    interface remains unchanged.

--- client/lib/viewport/index.js
+++ client/lib/viewport/index.js
@@ -37,36 +37,69 @@
 // [1] https://github.com/Automattic/wp-calypso/blob/master/docs/coding-guidelines/css.md#media-queries
 //
 
-export function isWithinBreakpoint( breakpoint ) {
-	const screenWidth = getWindowInnerWidth(),
-		breakpoints = {
-			'<480px': () => screenWidth <= 480,
-			'<660px': () => screenWidth <= 660,
-			'<800px': () => screenWidth <= 800,
-			'<960px': () => screenWidth <= 960,
-			'<1040px': () => screenWidth <= 1040,
-			'<1280px': () => screenWidth <= 1280,
-			'<1400px': () => screenWidth <= 1400,
-			'>480px': () => screenWidth > 480,
-			'>660px': () => screenWidth > 660,
-			'>800px': () => screenWidth > 800,
-			'>960px': () => screenWidth > 960,
-			'>1040px': () => screenWidth > 1040,
-			'>1280px': () => screenWidth > 1280,
-			'>1400px': () => screenWidth > 1400,
-			'480px-660px': () => screenWidth > 480 && screenWidth <= 660,
-			'660px-960px': () => screenWidth > 660 && screenWidth <= 960,
-			'480px-960px': () => screenWidth > 480 && screenWidth <= 960,
-		};
+// FIXME: We can't detect window size on the server, so until we have more intelligent detection,
+// use 769, which is just above the general maximum mobile screen width.
+const SERVER_WIDTH = 769;
 
-	if ( ! breakpoints.hasOwnProperty( breakpoint ) ) {
+const isServer = typeof window === 'undefined' || ! window.matchMedia;
+
+function createMediaQueryList( { min, max } = {} ) {
+	if ( min !== undefined && max !== undefined ) {
+		return isServer
+			? { matches: SERVER_WIDTH > min && SERVER_WIDTH <= max }
+			: window.matchMedia( `(min-width: ${ min + 1 }px) and (max-width: ${ max }px)` );
+	}
+
+	if ( min !== undefined ) {
+		return isServer
+			? { matches: SERVER_WIDTH > min }
+			: window.matchMedia( `(min-width: ${ min + 1 }px)` );
+	}
+
+	if ( max !== undefined ) {
+		return isServer
+			? { matches: SERVER_WIDTH <= max }
+			: window.matchMedia( `(max-width: ${ max }px)` );
+	}
+
+	return false;
+}
+
+const mediaQueryLists = {
+	'<480px': createMediaQueryList( { max: 480 } ),
+	'<660px': createMediaQueryList( { max: 660 } ),
+	'<800px': createMediaQueryList( { max: 800 } ),
+	'<960px': createMediaQueryList( { max: 960 } ),
+	'<1040px': createMediaQueryList( { max: 1040 } ),
+	'<1280px': createMediaQueryList( { max: 1280 } ),
+	'<1400px': createMediaQueryList( { max: 1400 } ),
+	'>480px': createMediaQueryList( { min: 480 } ),
+	'>660px': createMediaQueryList( { min: 660 } ),
+	'>800px': createMediaQueryList( { min: 800 } ),
+	'>960px': createMediaQueryList( { min: 960 } ),
+	'>1040px': createMediaQueryList( { min: 1040 } ),
+	'>1280px': createMediaQueryList( { min: 1280 } ),
+	'>1400px': createMediaQueryList( { min: 1400 } ),
+	'480px-660px': createMediaQueryList( { min: 480, max: 660 } ),
+	'660px-960px': createMediaQueryList( { min: 660, max: 960 } ),
+	'480px-960px': createMediaQueryList( { min: 480, max: 960 } ),
+};
+
+function getMediaQueryList( breakpoint ) {
+	if ( ! mediaQueryLists.hasOwnProperty( breakpoint ) ) {
 		try {
 			// eslint-disable-next-line no-console
 			console.warn( 'Undefined breakpoint used in `mobile-first-breakpoint`', breakpoint );
 		} catch ( e ) {}
 		return undefined;
 	}
-	return breakpoints[ breakpoint ]();
+
+	return mediaQueryLists[ breakpoint ];
+}
+
+export function isWithinBreakpoint( breakpoint ) {
+	const mediaQueryList = getMediaQueryList( breakpoint );
+	return mediaQueryList ? mediaQueryList.matches : undefined;
 }
 
 export function isMobile() {
@@ -77,8 +110,6 @@ export function isDesktop() {
 	return isWithinBreakpoint( '>960px' );
 }
 
-// FIXME: We can't detect window size on the server, so until we have more intelligent detection,
-// use 769, which is just above the general maximum mobile screen width.
 export function getWindowInnerWidth() {
-	return typeof window !== 'undefined' ? window.innerWidth : 769;
+	return isServer ? SERVER_WIDTH : window.innerWidth;
 }
