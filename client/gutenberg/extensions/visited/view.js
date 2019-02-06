b43a0d2ad62f9871148f173d2af7a77a2fe94b38
Jason Moon <jason.moon@automattic.com>
2019-02-06 15:15:48 -0700

    Gutenberg Visited: Remove view.js debug calls

--- client/gutenberg/extensions/visited/view.js
+++ client/gutenberg/extensions/visited/view.js
@@ -3,25 +3,19 @@
  * External dependencies
  */
 import cookie from 'cookie';
-import debugFactory from 'debug';
 
 /**
  * Internal dependencies
  */
 import { COOKIE_NAME, MAX_COOKIE_AGE } from './constants';
 
-// TODO: Remove debug capability
-const debug = debugFactory( 'wp-gutenberg:visited-block' );
-
 function getViewCount() {
 	const cookies = cookie.parse( document.cookie );
 	const value = cookies[ COOKIE_NAME ] || 0;
-	debug( 'cookie value', +value );
 	return +value;
 }
 
 function setViewCount( value ) {
-	debug( 'new cookie value', value );
 	document.cookie = cookie.serialize( COOKIE_NAME, value, {
 		path: window.location.pathname,
 		maxAge: MAX_COOKIE_AGE,
