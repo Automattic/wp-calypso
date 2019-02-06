78fd901427ef4c3350a584f47a2646c0854afaa2
Jason Moon <jason.moon@automattic.com>
2019-02-06 15:52:03 -0700

    Gutenberg Visited: Update cookie name

--- client/gutenberg/extensions/visited/constants.js
+++ client/gutenberg/extensions/visited/constants.js
@@ -1,5 +1,5 @@
 export const CRITERIA_AFTER = 'after-visits';
 export const CRITERIA_BEFORE = 'before-visits';
 export const DEFAULT_THRESHOLD = 3;
-export const COOKIE_NAME = 'wp-visit-tracking';
+export const COOKIE_NAME = 'jp-visit-counter';
 export const MAX_COOKIE_AGE = 6 * 30 * 24 * 60 * 60; // 6 months
