40d27c43b7228cfe71110bc142857a92b55aebe2
Jefferson Rabb <j.max.rabb@gmail.com>
2019-02-07 08:14:26 -0500

    Mailchimp: Client-Side Rendering In Editor (#30275)
    * Refactor of Mailchimp block without Server Side Rendering.
    
    * Use WPCOM API for checking whether blog has Mailchimp connection. UI for editing the connection, refreshing block after connection has been made in another browser window.
    
    * If email is already subscribed, display success message even though API returns an error.
    
    * Determine Mailchimp connection status using Jetpack API ( /wpcom/v2/mailchimp ) rather than making direct public API call.
    
    * Switch to server-side rendering in the view, to circumvent WPCOM tag stripping.
    
    * Title styling.
    
    * Use ellipsis over ...
    
    * Remove extraneous comments
    
    * Remove redundant @format pragma
    
    * Material icon, block description, re-styling of Re-check Connection button to match typical Media Upload buttons.
    
    * Add newsletter keyword
    
    * Add space around methods
    
    * Drop unused argument
    
    * Replace custom email validation with lib
    
    * Fix URL encoding
    
    * Add non-production error message
    
    * Replace object with flat functions
    
    * Abort submission on validation failure
    
    * fixup! Abort submission on validation failure
    
    * Add an onChange handler to the placeholder TextControl, to avoid Javascript errors in the editor.

--- client/gutenberg/extensions/mailchimp/index.js
+++ client/gutenberg/extensions/mailchimp/index.js
@@ -1,4 +1,7 @@
-/** @format */
+/**
+ * External dependencies
+ */
+import { Path, SVG } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -9,32 +12,53 @@ import './editor.scss';
 
 export const name = 'mailchimp';
 
-export const fields = [
-	{ id: 'title', label: __( 'Title' ) },
-	{ id: 'email_placeholder', label: __( 'Placeholder' ) },
-	{ id: 'submit_label', label: __( 'Submit button label' ) },
-	{ id: 'consent_text', label: __( 'Consent text' ) },
-	{ id: 'processing_label', label: __( '"Processing" status message' ) },
-	{ id: 'success_label', label: __( 'Success status message' ) },
-	{ id: 'error_label', label: __( 'Error status message' ) },
-];
+export const icon = (
+	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<Path fill="none" d="M0 0h24v24H0V0z" />
+		<Path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z" />
+	</SVG>
+);
 
 export const settings = {
 	title: __( 'MailChimp' ),
-	icon: 'email',
+	icon,
+	description: __( 'A form enabling readers to join a Mailchimp list.' ),
 	category: 'jetpack',
-	keywords: [ __( 'email' ), __( 'mailchimp' ), 'jetpack' ],
-
-	edit,
-	attributes: fields.reduce( ( attrs, field ) => {
-		attrs[ field.id ] = {
+	keywords: [ __( 'email' ), __( 'mailchimp' ), __( 'newsletter' ) ],
+	attributes: {
+		title: {
 			type: 'string',
-			default: '',
-		};
-		return attrs;
-	}, {} ),
-
-	save: function() {
-		return null;
+			default: __( 'Join my email list' ),
+		},
+		emailPlaceholder: {
+			type: 'string',
+			default: __( 'Enter your email' ),
+		},
+		submitLabel: {
+			type: 'string',
+			default: __( 'Join My Email List' ),
+		},
+		consentText: {
+			type: 'string',
+			default: __(
+				'By clicking submit, you agree to share your email address with the site owner and MailChimp to receive marketing, updates, and other emails from the site owner. Use the unsubscribe link in those emails to opt out at any time.'
+			),
+		},
+		processingLabel: {
+			type: 'string',
+			default: __( 'Processing…' ),
+		},
+		successLabel: {
+			type: 'string',
+			default: __( "Success! You've been added to the list." ),
+		},
+		errorLabel: {
+			type: 'string',
+			default: __(
+				'Oh no! Unfortunately there was an error. Please try reloading this page and adding your email once more.'
+			),
+		},
 	},
+	edit,
+	save: () => null,
 };
