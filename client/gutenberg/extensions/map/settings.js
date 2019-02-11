0ec684465215d3714851d66413546c8a60efb7cf
Jon Surrell <jon.surrell@automattic.com>
2019-02-11 16:58:58 +0100

    Disable SVG rule for map, add a11y props
    @wordpress/component dependency is undesireable for view script. Disable
    eslint rule.
    
    Add a11y properties from Gutenberg:
    https://github.com/WordPress/gutenberg/blob/52e8384cff48748a30546467aa22a4b0958cd751/packages/components/src/primitives/svg/index.js#L15-L17

--- client/gutenberg/extensions/map/settings.js
+++ client/gutenberg/extensions/map/settings.js
@@ -1,9 +1,8 @@
-/** @format */
-
+// Disable forbidden <svg> etc. so that frontend component does not depend on @wordpress/component
+/* eslint-disable react/forbid-elements */
 /**
  * External dependencies
  */
-
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 
 export const settings = {
@@ -11,7 +10,15 @@ export const settings = {
 	prefix: 'jetpack',
 	title: __( 'Map' ),
 	icon: (
-		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			role="img"
+			aria-hidden="true"
+			focusable="false"
+		>
 			<path fill="none" d="M0 0h24v24H0V0z" />
 			<path d="M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM10 5.47l4 1.4v11.66l-4-1.4V5.47zm-5 .99l3-1.01v11.7l-3 1.16V6.46zm14 11.08l-3 1.01V6.86l3-1.16v11.84z" />
 		</svg>
