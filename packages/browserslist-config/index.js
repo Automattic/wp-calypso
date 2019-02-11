9a83543d8eddc2507ad1979178cb5a83584396c2
Mikael Korpela <mikael@ihminen.org>
2019-02-11 14:00:10 +0200

    Add `prettier-ignore` to keep lines

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -1,1 +1,8 @@
-module.exports = [ '> 1%', 'iOS >= 10', 'last 2 versions', 'not ie <= 10', 'Safari >= 10' ];
+// prettier-ignore
+module.exports = [
+	'> 1%',
+	'iOS >= 10',
+	'last 2 versions',
+	'not ie <= 10',
+	'Safari >= 10'
+];
