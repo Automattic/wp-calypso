190ab7dc76185c0363f773d3815b64afb20561f5
Marin Atanasov <tyxla@abv.bg>
2019-02-12 14:55:46 +0200

    Jetpack Connect: Adapt site type to use the jetpackOnly HoC

--- client/jetpack-connect/site-type.js
+++ client/jetpack-connect/site-type.js
@@ -2,36 +2,22 @@
  * External dependencies
  */
 import React, { Component } from 'react';
-import page from 'page';
 import { connect } from 'react-redux';
+import { flowRight } from 'lodash';
 import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import FormattedHeader from 'components/formatted-header';
+import jetpackOnly from './jetpack-only';
 import MainWrapper from './main-wrapper';
 import SiteTypeForm from 'signup/steps/site-type/form';
 import { loadTrackingTool } from 'state/analytics/actions';
-import { isJetpackSite } from 'state/sites/selectors';
-import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
 
 class JetpackSiteType extends Component {
 	componentDidMount() {
-		this.verifyJetpackSite();
-
 		this.props.loadTrackingTool( 'HotJar' );
-	}
-
-	verifyJetpackSite() {
-		const { notJetpack, siteId, siteSlug } = this.props;
-		if ( notJetpack ) {
-			// Redirect to /plans/:site if this is not a Jetpack site
-			page.redirect( `/plans/${ siteSlug }` );
-		} else if ( ! siteId ) {
-			// Redirect to /jetpack/connect if this is not a valid connected site
-			page.redirect( `/jetpack/connect` );
-		}
 	}
 
 	handleSubmit() {
@@ -39,11 +25,7 @@ class JetpackSiteType extends Component {
 	}
 
 	render() {
-		const { notJetpack, siteId, translate } = this.props;
-
-		if ( notJetpack || ! siteId ) {
-			return null;
-		}
+		const { translate } = this.props;
 
 		return (
 			<MainWrapper isWide>
@@ -66,17 +48,15 @@ class JetpackSiteType extends Component {
 	}
 }
 
-export default connect(
-	state => {
-		const siteId = getSelectedSiteId( state );
-
-		return {
-			notJetpack: siteId && ! isJetpackSite( state, siteId ),
-			siteId,
-			siteSlug: getSelectedSiteSlug( state ),
-		};
-	},
+const connectComponent = connect(
+	null,
 	{
 		loadTrackingTool,
 	}
-)( localize( JetpackSiteType ) );
+);
+
+export default flowRight(
+	connectComponent,
+	jetpackOnly,
+	localize
+)( JetpackSiteType );
