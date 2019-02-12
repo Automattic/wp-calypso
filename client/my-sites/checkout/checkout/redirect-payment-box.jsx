794eb704e50dc8648c54f1987220c38c1ab5e5f1
Louis Laugesen <louis.laugesen@gmail.com>
2019-02-12 17:58:01 +1100

    Partial refactor on payment-button classes

--- client/my-sites/checkout/checkout/redirect-payment-box.jsx
+++ client/my-sites/checkout/checkout/redirect-payment-box.jsx
@@ -302,27 +302,32 @@ export class RedirectPaymentBox extends PureComponent {
 					<DomainRegistrationRefundPolicy cart={ this.props.cart } />
 
 					<div className="checkout__payment-box-actions">
-						<div className="checkout__pay-button">
-							<button
-								type="submit"
-								className="checkout__button-pay button is-primary "
-								disabled={ this.state.formDisabled }
-							>
-								{ this.renderButtonText() }
-							</button>
-							<SubscriptionText cart={ this.props.cart } />
-						</div>
+						<div className="checkout__payment-box-payment-buttons">
+							<span className="checkout__pay-button">
+								<button
+									type="submit"
+									className="checkout__pay-button-button button is-primary "
+									disabled={ this.state.formDisabled }
+								>
+									{ this.renderButtonText() }
+								</button>
+								<SubscriptionText cart={ this.props.cart } />
+							</span>
 
-						<div className="checkout__secure-payment">
-							<div className="checkout__secure-payment-content">
-								<Gridicon icon="lock" />
-								{ translate( 'Secure Payment' ) }
+							<div className="checkout__secure-payment">
+								<div className="checkout__secure-payment-content">
+									<Gridicon icon="lock" />
+									{ translate( 'Secure Payment' ) }
+								</div>
 							</div>
-						</div>
 
-						{ showPaymentChatButton && (
-							<PaymentChatButton paymentType={ this.props.paymentType } cart={ this.props.cart } />
-						) }
+							{ showPaymentChatButton && (
+								<PaymentChatButton
+									paymentType={ this.props.paymentType }
+									cart={ this.props.cart }
+								/>
+							) }
+						</div>
 					</div>
 				</form>
 				<CartCoupon cart={ this.props.cart } />
