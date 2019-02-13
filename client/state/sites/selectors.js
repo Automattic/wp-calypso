d59aee58f97f8970b7b074355ab3e7f987c00003
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2019-02-13 14:09:58 +0200

    State: Remove unnecessary Analytics supports selectors (#30755)

--- client/state/sites/selectors.js
+++ client/state/sites/selectors.js
@@ -1122,42 +1122,6 @@ export const siteSupportsJetpackSettingsUi = ( state, siteId ) => {
 };
 
 /**
- * Returns true if the version of Jetpack on the site supports Google Analytics IP Anonymization.
- * False otherwise.
- *
- * @param {Object} state  Global state tree
- * @param {Object} siteId Site ID
- * @return {?Boolean}     Whether site supports the setting.
- */
-export const siteSupportsGoogleAnalyticsIPAnonymization = ( state, siteId ) => {
-	return isJetpackMinimumVersion( state, siteId, '5.4-beta3' );
-};
-
-/**
- * Returns true if the version of Jetpack on the site supports Google Analytics Basic eCommerce Tracking.
- * False otherwise.
- *
- * @param {Object} state  Global state tree
- * @param {Object} siteId Site ID
- * @return {?Boolean}     Whether site supports the settings.
- */
-export const siteSupportsGoogleAnalyticsBasicEcommerceTracking = ( state, siteId ) => {
-	return isJetpackMinimumVersion( state, siteId, '5.4-beta3' );
-};
-
-/**
- * Returns true if the version of Jetpack on the site supports Google Analytics Enhanced eCommerce Tracking.
- * False otherwise.
- *
- * @param {Object} state  Global state tree
- * @param {Object} siteId Site ID
- * @return {?Boolean}     Whether site supports the settings.
- */
-export const siteSupportsGoogleAnalyticsEnhancedEcommerceTracking = ( state, siteId ) => {
-	return isJetpackMinimumVersion( state, siteId, '5.6-beta2' );
-};
-
-/**
  * Returns true if the site is created less than 30 mins ago.
  * False otherwise.
  *
