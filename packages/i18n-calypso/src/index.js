9c0964838ce1e89bb70a070964b521cf5a86fe1f
Jarda Snajdr <jsnajdr@gmail.com>
2019-02-13 14:27:59 +0100

    Convert the module exports to ESM

--- packages/i18n-calypso/src/index.js
+++ packages/i18n-calypso/src/index.js
@@ -7,25 +7,22 @@ import I18N from './i18n';
 import localizeFactory from './localize';
 
 const i18n = new I18N();
-
-module.exports = {
-	moment: i18n.moment,
-	numberFormat: i18n.numberFormat.bind( i18n ),
-	translate: i18n.translate.bind( i18n ),
-	configure: i18n.configure.bind( i18n ),
-	setLocale: i18n.setLocale.bind( i18n ),
-	getLocale: i18n.getLocale.bind( i18n ),
-	getLocaleSlug: i18n.getLocaleSlug.bind( i18n ),
-	addTranslations: i18n.addTranslations.bind( i18n ),
-	reRenderTranslations: i18n.reRenderTranslations.bind( i18n ),
-	registerComponentUpdateHook: i18n.registerComponentUpdateHook.bind( i18n ),
-	registerTranslateHook: i18n.registerTranslateHook.bind( i18n ),
-	state: i18n.state,
-	stateObserver: i18n.stateObserver,
-	on: i18n.stateObserver.on.bind( i18n.stateObserver ),
-	off: i18n.stateObserver.removeListener.bind( i18n.stateObserver ),
-	emit: i18n.stateObserver.emit.bind( i18n.stateObserver ),
-	localize: localizeFactory( i18n ),
-	$this: i18n,
-	I18N,
-};
+export { I18N };
+export default i18n;
+export const moment = i18n.moment;
+export const numberFormat = i18n.numberFormat.bind( i18n );
+export const translate = i18n.translate.bind( i18n );
+export const configure = i18n.configure.bind( i18n );
+export const setLocale = i18n.setLocale.bind( i18n );
+export const getLocale = i18n.getLocale.bind( i18n );
+export const getLocaleSlug = i18n.getLocaleSlug.bind( i18n );
+export const addTranslations = i18n.addTranslations.bind( i18n );
+export const reRenderTranslations = i18n.reRenderTranslations.bind( i18n );
+export const registerComponentUpdateHook = i18n.registerComponentUpdateHook.bind( i18n );
+export const registerTranslateHook = i18n.registerTranslateHook.bind( i18n );
+export const state = i18n.state;
+export const stateObserver = i18n.stateObserver;
+export const on = i18n.on.bind( i18n );
+export const off = i18n.off.bind( i18n );
+export const emit = i18n.emit.bind( i18n );
+export const localize = localizeFactory( i18n );
