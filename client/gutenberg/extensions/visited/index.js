7f8a800a8d2d5bac299798c7170fa1a3bccea832
Jason Moon <jason.moon@automattic.com>
2019-02-13 16:18:51 -0700

    Use 'history' icon, take 2

--- client/gutenberg/extensions/visited/index.js
+++ client/gutenberg/extensions/visited/index.js
@@ -1,8 +1,14 @@
 /** @format */
 /**
+ * External dependencies
+ */
+import { Path } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
 import { __, _x } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
+import renderMaterialIcon from 'gutenberg/extensions/presets/jetpack/utils/render-material-icon';
 import edit from './components/edit';
 import save from './components/save';
 import { CRITERIA_AFTER, DEFAULT_THRESHOLD } from './constants';
@@ -22,7 +28,9 @@ export const settings = {
 	},
 	category: 'jetpack',
 	description: __( 'Control block visibility based on how often a visitor has viewed the page.' ),
-	icon: 'history',
+	icon: renderMaterialIcon(
+		<Path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z" />
+	),
 	keywords: [
 		_x( 'traffic', 'block search term' ),
 		_x( 'visitors', 'block search term' ),
