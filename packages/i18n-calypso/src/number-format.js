1ec912c53a5f08b4c1e9d0598b48ac10ed242b30
Jarda Snajdr <jsnajdr@gmail.com>
2019-02-13 14:36:23 +0100

    Update number-format source to contemporary standards

--- packages/i18n-calypso/src/number-format.js
+++ packages/i18n-calypso/src/number-format.js
@@ -1,25 +1,22 @@
-/** @TODO prettier, eslint fix, test, refactor+update */
-/* eslint-disable prefer-const, no-shadow */
-
-/**
+/*
  * Exposes number format capability
  *
  * @copyright Copyright (c) 2013 Kevin van Zonneveld (http://kvz.io) and Contributors (http://phpjs.org/authors).
  * @license See CREDITS.md
  * @see https://github.com/kvz/phpjs/blob/ffe1356af23a6f2512c84c954dd4e828e92579fa/functions/strings/number_format.js
  */
+function toFixedFix( n, prec ) {
+	const k = Math.pow( 10, prec );
+	return '' + ( Math.round( n * k ) / k ).toFixed( prec );
+}
 
 export default function number_format( number, decimals, dec_point, thousands_sep ) {
 	number = ( number + '' ).replace( /[^0-9+\-Ee.]/g, '' );
-	let n = ! isFinite( +number ) ? 0 : +number,
-		prec = ! isFinite( +decimals ) ? 0 : Math.abs( decimals ),
-		sep = typeof thousands_sep === 'undefined' ? ',' : thousands_sep,
-		dec = typeof dec_point === 'undefined' ? '.' : dec_point,
-		s = '',
-		toFixedFix = function( n, prec ) {
-			const k = Math.pow( 10, prec );
-			return '' + ( Math.round( n * k ) / k ).toFixed( prec );
-		};
+	const n = ! isFinite( +number ) ? 0 : +number;
+	const prec = ! isFinite( +decimals ) ? 0 : Math.abs( decimals );
+	const sep = typeof thousands_sep === 'undefined' ? ',' : thousands_sep;
+	const dec = typeof dec_point === 'undefined' ? '.' : dec_point;
+	let s = '';
 	// Fix for IE parseFloat(0.55).toFixed(0) = 0;
 	s = ( prec ? toFixedFix( n, prec ) : '' + Math.round( n ) ).split( '.' );
 	if ( s[ 0 ].length > 3 ) {
