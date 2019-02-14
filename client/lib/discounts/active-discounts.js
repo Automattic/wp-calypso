73e02c068f5b2a1ef2bece7c69fe1e9a7c4945a2
Travis Walter <travis@automattic.com>
2019-02-14 15:38:15 -0800

    Adjust start and end dates for wpcom sale

--- client/lib/discounts/active-discounts.js
+++ client/lib/discounts/active-discounts.js
@@ -57,8 +57,8 @@ export default [
 	},
 	{
 		name: 'sale_feb_2019',
-		startsAt: new Date( 2019, 1, 25, 0, 0, 0 ),
-		endsAt: new Date( 2019, 2, 1, 0, 0, 0 ),
+		startsAt: new Date( 2019, 2, 4, 0, 0, 0 ),
+		endsAt: new Date( 2019, 2, 8, 0, 0, 0 ),
 		nudgeText: 'Sale! 20% off all plans',
 		ctaText: 'Upgrade',
 		plansPageNoticeText: 'Enter coupon code “FEBRUARY20” at checkout to claim your 20% discount',
