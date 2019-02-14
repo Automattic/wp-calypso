20a2f852d3938b055178136df5758b5735f1e6ee
Ben Lowery <ben.lowery@automattic.com>
2019-02-14 15:50:28 -0500

    add a comment to the lint rules for the i18n cli to explain why

--- packages/i18n-calypso/cli/.eslintrc.js
+++ packages/i18n-calypso/cli/.eslintrc.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	parserOptions: {
-		sourceType: 'script',
+		sourceType: 'script', // force the cli to use require instead of import, which it should be to node compatible
 	},
 	rules: {
 		'import/no-nodejs-modules': 0,
