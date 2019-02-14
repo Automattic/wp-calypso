b32c7f41bba9830061518297dc0fc28e3da2b1c8
mmtr <miguel.torres@automattic.com>
2019-02-14 16:27:58 +0100

    Return the element produced by the render in the fallback to core.
    This is to ensure that the block class is added to the element cloned
    by Gutenberg when getting the extra props to be rendered.

--- client/gutenberg/extensions/videopress/save.js
+++ client/gutenberg/extensions/videopress/save.js
@@ -8,7 +8,16 @@ const VideoPressSave = CoreVideoSave => props => {
 	const { attributes: { caption, guid } = {} } = props;
 
 	if ( ! guid ) {
-		return <CoreVideoSave { ...props } />;
+		/**
+		 * We return the element produced by the render so Gutenberg can add the block class when cloning the element.
+		 * This is due to the fact that `React.cloneElement` ignores the class name when we clone a component to be
+		 * rendered (i.e. `React.cloneElement( <CoreVideoSave { ...props } />, { className: 'wp-block-video' } )`).
+		 *
+		 * @see https://github.com/WordPress/gutenberg/blob/3f1324b53cc8bb45d08d12d5321d6f88510bed09/packages/blocks/src/api/serializer.js#L78-L96
+		 * @see https://github.com/WordPress/gutenberg/blob/c5f9bd88125282a0c35f887cc8d835f065893112/packages/editor/src/hooks/generated-class-name.js#L42
+		 * @see https://github.com/Automattic/wp-calypso/pull/30546#issuecomment-463637946
+		 */
+		return CoreVideoSave( props );
 	}
 
 	const url = `https://videopress.com/v/${ guid }`;
