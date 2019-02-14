b1dcab1218f50b948e53aee1a92d490dc20ba0d5
Jason Moon <jason.moon@automattic.com>
2019-02-14 14:02:20 -0700

    Hide form controls when unselected, fix i18n calls

--- client/gutenberg/extensions/visited/components/edit.jsx
+++ client/gutenberg/extensions/visited/components/edit.jsx
@@ -2,7 +2,7 @@
 /**
  * External dependencies
  */
-import { Notice, PanelBody, RadioControl, TextControl } from '@wordpress/components';
+import { Notice, TextControl, SelectControl } from '@wordpress/components';
 import { Component, Fragment } from '@wordpress/element';
 import { InnerBlocks } from '@wordpress/editor';
 import { withSelect } from '@wordpress/data';
@@ -14,6 +14,11 @@ import interpolateComponents from 'interpolate-components';
 import { sprintf } from '@wordpress/i18n';
 import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 import { CRITERIA_AFTER, CRITERIA_BEFORE } from '../constants';
+
+const SELECT_OPTIONS = [
+	{ value: CRITERIA_AFTER, label: __( 'at least' ) },
+	{ value: CRITERIA_BEFORE, label: __( 'less than' ) },
+];
 
 class VisitedEdit extends Component {
 	setCriteria = criteria => this.props.setAttributes( { criteria } );
@@ -29,31 +34,42 @@ class VisitedEdit extends Component {
 			<Fragment>
 				<div className={ this.props.className }>
 					<div className="wp-block-jetpack-visited-inner-block">
-						{ this.props.isSelected && (
+						{
 							<Notice status="info" isDismissible={ false }>
-								{ interpolateComponents( {
-									mixedString: __(
-										'This block will only appear to people who have previously visited this page {{select /}} {{input/}} times.'
-									),
-									components: {
-										input: (
-											<TextControl
-												onChange={ this.setThreshold }
-												type="number"
-												min="1"
-												defaultValue={ this.props.attributes.threshold }
-											/>
-										),
-										select: (
-											<select>
-												<option value={ this.props.attributes.criteria }>at least</option>
-												<option value={ this.props.attributes.criteria }>less than</option>
-											</select>
-										),
-									},
-								} ) }
+								{ this.props.isSelected
+									? interpolateComponents( {
+											mixedString: __(
+												'This block will only appear to people who have previously visited this page {{atLeastOrLessThanSelectBox/}} {{numberInputField/}} times.'
+											),
+											components: {
+												numberInputField: (
+													<TextControl
+														defaultValue={ this.props.attributes.threshold }
+														min="1"
+														onChange={ this.setThreshold }
+														type="number"
+													/>
+												),
+												atLeastOrLessThanSelectBox: (
+													<SelectControl
+														value={ this.props.attributes.criteria }
+														onChange={ this.setCriteria }
+														options={ SELECT_OPTIONS }
+													/>
+												),
+											},
+									  } )
+									: sprintf(
+											__(
+												'This block will only appear to people who have previously visited this page %s %d times.'
+											),
+											this.props.attributes.criteria === CRITERIA_AFTER
+												? SELECT_OPTIONS[ 0 ].label
+												: SELECT_OPTIONS[ 1 ].label,
+											this.props.attributes.threshold
+									  ) }
 							</Notice>
-						) }
+						}
 						<InnerBlocks />
 					</div>
 				</div>
