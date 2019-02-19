7ba243d382e3ee1c9198d678912c61d940a78de9
Jarda Snajdr <jsnajdr@gmail.com>
2019-02-19 11:13:57 +0100

    SearchCard: use React.forwardRef to forward ref to inner Search component
    Instead of defining wrapper methods for `focus` and `clear`, just forward the ref
    to the inner `Search` component using `React.forwardRef`.
    
    Defining `propTypes` is not necessary, as we pass-thru all props to `Search` anyway
    and the two prop lists are duplicate and have not been in sync for some time.

--- client/components/search-card/index.jsx
+++ client/components/search-card/index.jsx
@@ -3,7 +3,6 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 /**
@@ -17,40 +16,10 @@ import Search from 'components/search';
  */
 import './style.scss';
 
-class SearchCard extends React.Component {
-	static propTypes = {
-		additionalClasses: PropTypes.string,
-		initialValue: PropTypes.string,
-		placeholder: PropTypes.string,
-		delaySearch: PropTypes.bool,
-		onSearch: PropTypes.func.isRequired,
-		onSearchChange: PropTypes.func,
-		analyticsGroup: PropTypes.string,
-		autoFocus: PropTypes.bool,
-		disabled: PropTypes.bool,
-		dir: PropTypes.string,
-		maxLength: PropTypes.number,
-		hideOpenIcon: PropTypes.bool,
-		disableAutocorrect: PropTypes.bool,
-	};
+const SearchCard = ( { className, ...props }, ref ) => (
+	<Card className={ classnames( 'search-card', className ) }>
+		<Search ref={ ref } { ...props } />
+	</Card>
+);
 
-	render() {
-		const cardClasses = classnames( 'search-card', this.props.className );
-
-		return (
-			<Card className={ cardClasses }>
-				<Search ref="search" { ...this.props } />
-			</Card>
-		);
-	}
-
-	focus = () => {
-		this.refs.search.focus();
-	};
-
-	clear = () => {
-		this.refs.search.clear();
-	};
-}
-
-export default SearchCard;
+export default React.forwardRef( SearchCard );
