7674a305b10707c03b07ff94fea7bf80a3f010ba
mmtr <miguel.torres@automattic.com>
2019-02-19 13:04:49 +0100

    Keep rendering the VideoPress player after a downgrade

--- client/gutenberg/extensions/videopress/editor.js
+++ client/gutenberg/extensions/videopress/editor.js
@@ -15,73 +15,56 @@ const addVideoPressSupport = ( settings, name ) => {
 		return settings;
 	}
 
-	const attributes = {
-		autoplay: {
-			type: 'boolean',
-		},
-		caption: {
-			type: 'string',
-			source: 'html',
-			selector: 'figcaption',
-		},
-		controls: {
-			type: 'boolean',
-			default: true,
-		},
-		guid: {
-			type: 'string',
-		},
-		id: {
-			type: 'number',
-		},
-		loop: {
-			type: 'boolean',
-		},
-		muted: {
-			type: 'boolean',
-		},
-		poster: {
-			type: 'string',
-		},
-		preload: {
-			type: 'string',
-			default: 'metadata',
-		},
-		src: {
-			type: 'string',
-		},
-	};
-
-	const edit = withVideoPressEdit( settings.edit );
-	const save = withVideoPressSave( settings.save );
 	const { available, unavailableReason } = getJetpackExtensionAvailability( 'videopress' );
 
-	if ( available ) {
+	// We customize the video block even if VideoPress it not available so we can support videos that were uploaded to
+	// VideoPress if it was available in the past (i.e. before a plan downgrade).
+	if ( available || [ 'missing_plan', 'missing_module' ].includes( unavailableReason ) ) {
 		return {
 			...settings,
-			attributes,
-			edit,
-			save,
+			attributes: {
+				autoplay: {
+					type: 'boolean',
+				},
+				caption: {
+					type: 'string',
+					source: 'html',
+					selector: 'figcaption',
+				},
+				controls: {
+					type: 'boolean',
+					default: true,
+				},
+				guid: {
+					type: 'string',
+				},
+				id: {
+					type: 'number',
+				},
+				loop: {
+					type: 'boolean',
+				},
+				muted: {
+					type: 'boolean',
+				},
+				poster: {
+					type: 'string',
+				},
+				preload: {
+					type: 'string',
+					default: 'metadata',
+				},
+				src: {
+					type: 'string',
+				},
+			},
+			edit: withVideoPressEdit( settings.edit ),
+			save: withVideoPressSave( settings.save ),
 			deprecated: [
 				{
 					attributes: settings.attributes,
 					save: settings.save,
 					isEligible: attrs => ! attrs.guid,
-				},
-			],
-		};
-	}
-
-	if ( [ 'missing_plan', 'missing_module' ].includes( unavailableReason ) ) {
-		// If the user downgraded the plan or disabled the module, we mark as deprecated the VideoPress-enhanced video
-		// block so the block is migrated to the default core video block.
-		return {
-			...settings,
-			attributes,
-			deprecated: [
-				{
-					attributes,
-					save,
 				},
 			],
 		};
