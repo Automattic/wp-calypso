f871ec6a83434388dfba5522beb9c5ce4bc7a222
Caleb Bauermeister <cbauerman@gmail.com>
2019-02-19 14:15:57 -0800

    fix a few css class names

--- client/my-sites/email/email-forwarding/email-forwarding-add-new.jsx
+++ client/my-sites/email/email-forwarding/email-forwarding-add-new.jsx
@@ -195,7 +195,7 @@ const EmailForwardingAddNew = createReactClass( {
 			{ mailbox, destination } = formState.getAllFieldValues( this.state.fields );
 
 		return (
-			<div className="form-content">
+			<div className="email-forwarding__form-content">
 				<FormFieldset>
 					<FormLabel>{ this.props.translate( 'Emails Sent To' ) }</FormLabel>
 					<FormTextInputWithAffixes
