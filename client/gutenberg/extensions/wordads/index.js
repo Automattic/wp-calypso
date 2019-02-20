6b8a9bb4b4f8b5d0ee95187f14eb66b1b6f268f5
Jon Surrell <jon.surrell@automattic.com>
2019-02-20 17:15:11 +0100

    WordAds block: Use outline icon (#30902)

--- client/gutenberg/extensions/wordads/index.js
+++ client/gutenberg/extensions/wordads/index.js
@@ -16,8 +16,8 @@ export const title = __( 'Ad' );
 
 export const icon = (
 	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-		<Path d="M0 0h24v24H0z" fill="none" />
-		<Path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7 12h-2v-2h2v2zm0-4h-2V6h2v4z" />
+		<Path fill="none" d="M0 0h24v24H0V0z" />
+		<Path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.17L4 17.17V4h16v12zM11 5h2v6h-2zm0 8h2v2h-2z" />
 	</SVG>
 );
 
