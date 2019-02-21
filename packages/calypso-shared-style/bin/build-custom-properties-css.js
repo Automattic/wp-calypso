f8bd08353086b7a968044802620a2c0f5162bbf4
Bernie Reiter <ockham@raz.or.at>
2019-02-21 14:40:22 +0100

    Fix paths

--- packages/calypso-shared-style/bin/build-custom-properties-css.js
+++ packages/calypso-shared-style/bin/build-custom-properties-css.js
@@ -2,8 +2,8 @@ const { writeFileSync } = require( 'fs' );
 const { renderSync } = require( 'node-sass' );
 const packageImporter = require( 'node-sass-package-importer' );
 
-const INPUT_FILE = 'assets/stylesheets/custom-properties.scss';
-const OUTPUT_FILE = 'public/custom-properties.css';
+const INPUT_FILE = 'src/custom-properties.scss';
+const OUTPUT_FILE = 'dist/custom-properties.css';
 
 const output = renderSync( { file: INPUT_FILE, importer: packageImporter() } );
 writeFileSync( OUTPUT_FILE, output.css );
