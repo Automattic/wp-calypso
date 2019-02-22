9f4db3900ebe2f282497036e3efa1e3614b7c60e
Jefferson Rabb <jeff@atavist.net>
2019-02-22 08:46:33 -0500

    a11y: Shorter timeout (.5s) before shifting focus to current slide after bullet click.

--- client/gutenberg/extensions/slideshow/swiper-callbacks.js
+++ client/gutenberg/extensions/slideshow/swiper-callbacks.js
@@ -55,7 +55,7 @@ function swiperPaginationRender( swiper ) {
 			const currentSlide = swiper.slides[ swiper.realIndex ];
 			setTimeout( () => {
 				currentSlide.focus();
-			}, 1000 );
+			}, 500 );
 		} );
 	} );
 }
