a640c8826abdbe20d71bfbdee1e8dae7d8a568e7
Marin Atanasov <tyxla@abv.bg>
2019-02-25 13:06:36 +0200

    Remove description again

--- client/jetpack-connect/site-topic.js
+++ client/jetpack-connect/site-topic.js
@@ -35,10 +35,7 @@ class JetpackSiteTopic extends Component {
 		return (
 			<MainWrapper isWide>
 				<div className="jetpack-connect__step">
-					<FormattedHeader
-						headerText={ translate( 'Tell us about your website' ) }
-						subHeaderText={ translate( 'What topic or category does your site cover?' ) }
-					/>
+					<FormattedHeader headerText={ translate( 'Tell us about your website' ) } />
 
 					<SiteTopicForm submitForm={ this.handleSubmit } />
 
