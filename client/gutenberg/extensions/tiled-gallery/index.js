2243bc9293cccada7a9c5ca4637c4ec9ef0ed775
Jon Surrell <jon.surrell@automattic.com>
2019-02-25 18:23:01 +0100

    Remove deprecation

--- client/gutenberg/extensions/tiled-gallery/index.js
+++ client/gutenberg/extensions/tiled-gallery/index.js
@@ -20,11 +20,6 @@ import {
 } from './constants';
 import './editor.scss';
 
-/**
- * Deprecated versions for migrations
- */
-import * as deprecatedV1 from './deprecated/v1';
-
 // Style names are translated. Avoid introducing an i18n dependency elsewhere (view)
 // by only including the labels here, the only place they're needed.
 //
@@ -182,5 +177,4 @@ export const settings = {
 	},
 	edit,
 	save,
-	deprecated: [ deprecatedV1 ],
 };
