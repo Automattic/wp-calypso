012298ef9575d3b34a1bf51c2d078513370cbad6
Caleb Bauermeister <cbauerman@gmail.com>
2019-02-26 08:51:51 -0800

    Fill out reducer tests

--- client/state/email-forwarding/test/index.js
+++ client/state/email-forwarding/test/index.js
@@ -9,22 +9,97 @@ import { expect } from 'chai';
  * Internal dependencies
  */
 import emailForwardsReducer from '../reducer';
-import { EMAIL_FORWARDING_RECEIVE } from 'state/action-types';
+import { EMAIL_FORWARDING_RECEIVE, EMAIL_FORWARDING_REQUEST } from 'state/action-types';
 
-describe( 'reducer', () => {
+const TEST_MAILBOX_EXAMPLE_DOT_COM = {
+	email: 'test@example.com',
+	mailbox: 'test',
+	domain: 'example.com',
+	forward_address: 'test@forward.com',
+	active: true,
+	created: 1551136603,
+};
+
+const TEST_MAILBOX_TEST_DOT_COM = {
+	email: 'example@test.com',
+	mailbox: 'example',
+	domain: 'test.com',
+	forward_address: 'test@forward.com',
+	active: true,
+	created: 1551136603,
+};
+
+describe( 'emailForwardsReducer', () => {
 	test( 'should save data', () => {
 		const state = emailForwardsReducer( undefined, {
 			type: EMAIL_FORWARDING_RECEIVE,
-			domainName: 'test.com',
+			domainName: 'example.com',
 			data: {
-				hello: 'world',
+				forwards: [ TEST_MAILBOX_EXAMPLE_DOT_COM ],
 			},
 		} );
+
 		expect( state ).to.eql( {
-			'test.com': {
-				forwards: { hello: 'world' },
-				error: null,
+			'example.com': {
+				forwards: [ TEST_MAILBOX_EXAMPLE_DOT_COM ],
 				isRequesting: false,
+				forwardsError: null,
+			},
+		} );
+	} );
+
+	test( 'should reset data on request to server', () => {
+		const prevState = {
+			'example.com': {
+				forwards: [ TEST_MAILBOX_EXAMPLE_DOT_COM ],
+				isRequesting: false,
+				forwardsError: null,
+			},
+		};
+
+		const nextState = emailForwardsReducer( prevState, {
+			type: EMAIL_FORWARDING_REQUEST,
+			domainName: 'example.com',
+		} );
+
+		expect( nextState ).to.eql( {
+			'example.com': {
+				forwards: null,
+				isRequesting: true,
+				forwardsError: null,
+			},
+		} );
+	} );
+
+	test( 'should reset data only for specific domainName on request', () => {
+		const prevState = {
+			'example.com': {
+				forwards: [ TEST_MAILBOX_EXAMPLE_DOT_COM ],
+				isRequesting: false,
+				forwardsError: null,
+			},
+			'test.com': {
+				forwards: [ TEST_MAILBOX_TEST_DOT_COM ],
+				isRequesting: false,
+				forwardsError: null,
+			},
+		};
+
+		const nextState = emailForwardsReducer( prevState, {
+			type: EMAIL_FORWARDING_REQUEST,
+			domainName: 'example.com',
+		} );
+
+		expect( nextState ).to.eql( {
+			'example.com': {
+				forwards: null,
+				isRequesting: true,
+				forwardsError: null,
+			},
+			'test.com': {
+				forwards: [ TEST_MAILBOX_TEST_DOT_COM ],
+				isRequesting: false,
+				forwardsError: null,
 			},
 		} );
 	} );
