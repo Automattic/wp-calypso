e15700ed1f83d8027537cec21cded6dcdc68beec
Louis Laugesen <louis.laugesen@gmail.com>
2019-02-26 21:29:33 +1100

    No need for class

--- client/blocks/payment-methods/index.jsx
+++ client/blocks/payment-methods/index.jsx
@@ -4,7 +4,7 @@
  * External dependencies
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import { localize } from 'i18n-calypso';
 import Gridicon from 'gridicons';
 import { intersection } from 'lodash';
@@ -15,48 +15,37 @@ import { intersection } from 'lodash';
 import PaymentLogo, { POSSIBLE_TYPES } from 'components/payment-logo';
 import { getEnabledPaymentMethods } from 'lib/cart-values';
 
-class PaymentMethods extends Component {
-	renderPaymentMethods = methods => {
-		if ( methods.includes( 'credit-card' ) ) {
-			methods.splice(
-				methods.indexOf( 'credit-card' ),
-				1,
-				'mastercard',
-				'visa',
-				'amex',
-				'discover'
-			);
-		}
-
-		methods = intersection( methods, POSSIBLE_TYPES );
-
-		return methods.map( method => <PaymentLogo type={ method } key={ method } /> );
-	};
-
-	render() {
-		const { translate } = this.props;
-		if ( ! this.props.cart.hasLoadedFromServer ) {
-			return false;
-		}
-
-		return (
-			<div className="payment-methods">
-				<Gridicon
-					icon="lock"
-					size={ 18 }
-					aria-label={ translate( 'Lock icon' ) }
-					className="payment-methods__icon"
-				/>
-				{ translate( 'Secure payment using:', {
-					comment: 'Followed by a graphical list of payment methods available to the user',
-				} ) }
-
-				<div className="payment-methods__methods">
-					{ this.renderPaymentMethods( getEnabledPaymentMethods( this.props.cart ) ) }
-				</div>
-			</div>
-		);
+function PaymentMethods( { translate, cart } ) {
+	if ( ! cart.hasLoadedFromServer ) {
+		return false;
 	}
-}
 
+	let methods = getEnabledPaymentMethods( cart );
+
+	if ( methods.includes( 'credit-card' ) ) {
+		methods.splice( methods.indexOf( 'credit-card' ), 1, 'mastercard', 'visa', 'amex', 'discover' );
+	}
+
+	methods = intersection( methods, POSSIBLE_TYPES );
+
+	return (
+		<div className="payment-methods">
+			<Gridicon
+				icon="lock"
+				size={ 18 }
+				aria-label={ translate( 'Lock icon' ) }
+				className="payment-methods__icon"
+			/>
+			{ translate( 'Secure payment using:', {
+				comment: 'Followed by a graphical list of payment methods available to the user',
+			} ) }
+
+			<div className="payment-methods__methods">
+				{ methods.map( method => (
+					<PaymentLogo type={ method } key={ method } />
+				) ) }
+			</div>
+		</div>
+	);
+}
 export default localize( PaymentMethods );
