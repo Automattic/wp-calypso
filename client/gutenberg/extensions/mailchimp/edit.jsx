Automattic/wp-calypso

commit ac0660a7a27debace40aed6e97d422c0d2aab12c

Remove p elements to avoid divs inside p
Fixes warning: "validateDOMNesting(...): <div> cannot appear as a descendant of <p>."

client/gutenberg/extensions/mailchimp/edit.jsx
@@ -190,19 +190,15 @@ class MailchimpSubscribeEdit extends Component {
 		);
 		const blockContent = (
 			<div className={ classes }>
-				<p>
-					<TextControl
-						aria-label={ emailPlaceholder }
-						disabled
-						onChange={ () => false }
-						placeholder={ emailPlaceholder }
-						title={ __( 'You can edit the email placeholder in the sidebar.' ) }
-						type="email"
-					/>
-				</p>
-				<p>
-					<SubmitButton { ...this.props } />
-				</p>
+				<TextControl
+					aria-label={ emailPlaceholder }
+					disabled
+					onChange={ () => false }
+					placeholder={ emailPlaceholder }
+					title={ __( 'You can edit the email placeholder in the sidebar.' ) }
+					type="email"
+				/>
+				<SubmitButton { ...this.props } />
 				<RichText
 					tagName="p"
 					placeholder={ __( 'Write consent text' ) }

commit 21818816cc63ea5166e2d64ed120322a83f9e83a

Fix hiding notification when modifying notification labels
Caused by https://github.com/Automattic/wp-calypso/commit/ac0660a7a27debace40aed6e97d422c0d2aab12c

client/gutenberg/extensions/mailchimp/edit.jsx
@@ -184,12 +184,11 @@ class MailchimpSubscribeEdit extends Component {
 				</PanelBody>
 			</InspectorControls>
 		);
-		const classes = classnames(
-			className,
-			audition && 'wp-block-jetpack-mailchimp_notication-audition'
-		);
+		const blockClasses = classnames( className, {
+			[ `${ classPrefix }notication-audition` ]: audition,
+		} );
 		const blockContent = (
-			<div className={ classes }>
+			<div className={ blockClasses }>
 				<TextControl
 					aria-label={ emailPlaceholder }
 					disabled

client/gutenberg/extensions/mailchimp/editor.scss
@@ -13,7 +13,8 @@
 		}
 	}
 
-	&.wp-block-jetpack-mailchimp_notication-audition p {
+	// Hide everything else except notification when modifying notification labels
+	&.wp-block-jetpack-mailchimp_notication-audition > *:not( .wp-block-jetpack-mailchimp_notification ) {
 		display: none;
 	}
 