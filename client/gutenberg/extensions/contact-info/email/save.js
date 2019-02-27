5e806bb4e24b89f9b38d6e8b0bde96bceb42c286
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-02-27 09:14:25 -0800

    Contact Info Block: Improve email validation (#31033)
    * Contact Info Block: Improve the email validation
    
    By using the emailValidator that is use in more places we can provide better matching and of email addresses.
    
    * bug fix. take into account any number of punctuation.

--- client/gutenberg/extensions/contact-info/email/save.js
+++ client/gutenberg/extensions/contact-info/email/save.js
@@ -1,18 +1,33 @@
 /**
- * Internal dependencies
+ * External dependencies
  */
-import textMatchReplace from 'gutenberg/extensions/presets/jetpack/utils/text-match-replace';
+import emailValidator from 'email-validator';
+import { Fragment } from '@wordpress/element';
 
 const renderEmail = inputText => {
-	return textMatchReplace(
-		inputText,
-		/((?:[a-z|0-9+_](?:\.|_\+)*)+[a-z|0-9]\@(?:[a-z|0-9])+(?:(?:\.){0,1}[a-z|0-9]){2}\.[a-z]{2,22})/gim,
-		( email, i ) => (
-			<a href={ `mailto:${ email }` } key={ i }>
-				{ email }
-			</a>
-		)
-	);
+	const explodedInput = inputText.split( /(\s+)/ ).map( ( email, i ) => {
+		// Remove and punctuation from the end of the email address.
+		const emailToValidate = email.replace( /([.,\/#!$%\^&\*;:{}=\-_`~()\]\[])+$/g, '' );
+		if ( email.indexOf( '@' ) && emailValidator.validate( emailToValidate ) ) {
+			return email === emailToValidate ? (
+				// Email.
+				<a href={ `mailto:${ email }` } key={ i }>
+					{ email }
+				</a>
+			) : (
+				// Email with punctionation.
+				<Fragment key={ i }>
+					<a href={ `mailto:${ email }` } key={ i }>
+						{ emailToValidate }
+					</a>
+					<Fragment>{ email.slice( -( email.length - emailToValidate.length ) ) }</Fragment>
+				</Fragment>
+			);
+		}
+		// Just a plain string.
+		return <Fragment key={ i }>{ email }</Fragment>;
+	} );
+	return explodedInput;
 };
 
 const save = ( { attributes: { email }, className } ) =>
