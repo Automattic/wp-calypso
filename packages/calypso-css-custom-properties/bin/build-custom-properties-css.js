2cac29a917e878412492be1f4f969211ec3890cd
Bernie Reiter <ockham@raz.or.at>
2019-02-28 18:53:10 +0100

    Make sure output dir exists

--- packages/calypso-css-custom-properties/bin/build-custom-properties-css.js
+++ packages/calypso-css-custom-properties/bin/build-custom-properties-css.js
@@ -1,4 +1,5 @@
-const { writeFileSync } = require( 'fs' );
+const { dirname } = require( 'path' );
+const { mkdirSync, writeFileSync } = require( 'fs' );
 const { renderSync } = require( 'node-sass' );
 const packageImporter = require( 'node-sass-package-importer' );
 
@@ -6,4 +7,5 @@ const INPUT_FILE = 'src/custom-properties.scss';
 const OUTPUT_FILE = 'dist/custom-properties.css';
 
 const output = renderSync( { file: INPUT_FILE, importer: packageImporter() } );
+mkdirSync( dirname( OUTPUT_FILE ), { recursive: true } ); // Make sure output dir exists
 writeFileSync( OUTPUT_FILE, output.css );
