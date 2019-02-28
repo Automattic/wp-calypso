b96f49ebaeadc4f43f2af76a6626b6e0745acd33
Jeff Bowen <jeff@automattic.com>
2019-02-28 13:33:27 -0500

    Revert change to description text per feedback

--- client/my-sites/site-settings/settings-import/descriptive-header.jsx
+++ client/my-sites/site-settings/settings-import/descriptive-header.jsx
@@ -20,7 +20,7 @@ class DescriptiveHeader extends PureComponent {
 
 		const title = siteTitle.length ? siteTitle : slug;
 		const description = translate(
-			'Import site content into ' +
+			'Import content from another site into ' +
 				'{{strong}}%(title)s{{/strong}}. Learn more about ' +
 				'the import process in our {{a}}support documentation{{/a}}. ' +
 				'Once you start importing, you can visit ' +
