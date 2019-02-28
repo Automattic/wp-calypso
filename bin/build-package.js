9abe31d5b3623060770ce55145c5c42c097efbfb
Bernie Reiter <ockham@raz.or.at>
2019-02-28 16:50:34 +0100

    Use npx babel

--- bin/build-package.js
+++ bin/build-package.js
@@ -12,11 +12,11 @@ const outputDirEsm = path.join( dir, 'dist', 'esm' );
 const outputDirCommon = path.join( dir, 'dist', 'cjs' );
 
 console.log( 'Building %s', dir );
-execSync( `node_modules/.bin/babel -d ${ outputDirEsm } ${ inputDir }`, {
+execSync( `npx babel -d ${ outputDirEsm } ${ inputDir }`, {
 	env: Object.assign( {}, process.env, { CALYPSO_CLIENT: 'true' } ),
 	cwd: root,
 } );
-execSync( `node_modules/.bin/babel -d ${ outputDirCommon } ${ inputDir }`, {
+execSync( `npx babel -d ${ outputDirCommon } ${ inputDir }`, {
 	env: Object.assign( {}, process.env, { CALYPSO_CLIENT: 'false' } ),
 	cwd: root,
 } );
