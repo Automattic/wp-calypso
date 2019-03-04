4d52d48c0f537fb802bd98c10cd9b8e17d2c0cca
Brent Sessions <brent.sessions@automattic.com>
2019-03-04 10:59:33 -0700

    Merging in latest test updates

--- test/e2e/specs/wp-signup-spec.js
+++ test/e2e/specs/wp-signup-spec.js
@@ -721,7 +721,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		} );
 	} );
 
-	describe( 'Sign up for a domain only purchase coming in from wordpress.com/domains in EUR currency @parallel', function() {
+	xdescribe( 'Sign up for a domain only purchase coming in from wordpress.com/domains in EUR currency @parallel', function() {
 		const siteName = dataHelper.getNewBlogName();
 		const expectedDomainName = `${ siteName }.live`;
 		const emailAddress = dataHelper.getEmailAddress( siteName, signupInboxId );
