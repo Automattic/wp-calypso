90904da0ced017dcb4ff0fe42d57317e1691cb63
Jon Surrell <jon.surrell@automattic.com>
2019-03-06 14:34:30 +0100

    Drop calypso site fragment

--- packages/jetpack-blocks/preset/editor-shared/get-site-fragment.js
+++ packages/jetpack-blocks/preset/editor-shared/get-site-fragment.js
@@ -1,13 +1,3 @@
-/**
- * External dependencies
- */
-import { includes } from 'lodash';
-
-/**
- * Internal dependencies
- */
-import { getSiteFragment as getCalypsoSiteFragment } from 'lib/route/path';
-
 /***
  * Returns the site fragment in the environment we're running Gutenberg in.
  *
@@ -17,13 +7,6 @@ export default function getSiteFragment() {
 	// WP.com wp-admin exposes the site ID in window._currentSiteId
 	if ( window && window._currentSiteId ) {
 		return window._currentSiteId;
-	}
-
-	// Calypso will contain a site slug or ID in the site fragment.
-	// WP.org will contain either `post` or `post-new.php`.
-	const siteFragment = getCalypsoSiteFragment( window.location.pathname );
-	if ( ! includes( [ 'post.php', 'post-new.php' ], siteFragment ) ) {
-		return siteFragment || null;
 	}
 
 	// Gutenberg in Jetpack adds a site fragment in the initial state
