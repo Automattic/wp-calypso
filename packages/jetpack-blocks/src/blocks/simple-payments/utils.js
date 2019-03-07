537db9b6fc5ddc9a1f298d1b4dec000869baf719
Jon Surrell <jon.surrell@automattic.com>
2019-03-07 13:45:18 +0100

    DANGER! Import lib/format-currency outside package
    This is not intended to be merged, but is a workaround until #31229
    lands.
    
    It should be replaced by '@automattic/format-currency' at that point.

--- packages/jetpack-blocks/src/blocks/simple-payments/utils.js
+++ packages/jetpack-blocks/src/blocks/simple-payments/utils.js
@@ -6,7 +6,7 @@ import { trimEnd } from 'lodash';
 /**
  * Internal dependencies
  */
-import { getCurrencyDefaults } from 'lib/format-currency/currencies';
+import { getCurrencyDefaults } from '../../../../../client/lib/format-currency/currencies';
 import { SIMPLE_PAYMENTS_PRODUCT_POST_TYPE } from './constants';
 
 export const isValidSimplePaymentsProduct = product =>
