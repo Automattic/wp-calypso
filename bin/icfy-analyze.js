4bfcccc96025fd60733260216284246102ba9f41
Jarda Snajdr <jsnajdr@gmail.com>
2019-03-07 12:00:16 +0100

    icfy-analyze: get the sizes of public/style.css file and report it
    The stylesheet is built outside webpack and therefore not a part of reported stats.
    This patch adds code to analyze the file and report the stats as a separate artifact
    file, `style.json`.
    
    Enables us to start showing an ICFY chart of `style.css` getting smaller.

--- bin/icfy-analyze.js
+++ bin/icfy-analyze.js
@@ -1,7 +1,8 @@
 /** @format */
 
 const { getViewerData, readStatsFromFile } = require( 'webpack-bundle-analyzer/lib/analyzer' );
-const { writeFileSync } = require( 'fs' );
+const { statSync, writeFileSync } = require( 'fs' );
+const gzip = require( 'gzip-size' );
 
 analyzeBundle();
 
@@ -12,5 +13,14 @@ function analyzeBundle() {
 	const chart = getViewerData( stats, './public' );
 	console.log( 'Analyze: writing chart.json file' );
 	writeFileSync( 'chart.json', JSON.stringify( chart, null, 2 ) );
+	console.log( 'Analyze: analyzing the style.css file' );
+	analyzeStylesheet( './public/style.css', 'style.json' );
 	console.log( 'Analyze: finished' );
+}
+
+function analyzeStylesheet( inputCSSFile, outputJSONFile ) {
+	const parsedSize = statSync( inputCSSFile ).size;
+	const gzipSize = gzip.fileSync( inputCSSFile );
+	const styleStats = { statSize: parsedSize, parsedSize, gzipSize };
+	writeFileSync( outputJSONFile, JSON.stringify( styleStats, null, 2 ) );
 }
