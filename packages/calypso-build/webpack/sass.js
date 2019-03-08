5eb6799d1ea7ac3d7375809f8186e4b8f11583c2
Bernie Reiter <ockham@raz.or.at>
2019-03-08 13:16:18 +0100

    Add JSDoc

--- packages/calypso-build/webpack/sass.js
+++ packages/calypso-build/webpack/sass.js
@@ -4,6 +4,15 @@
 const MiniCssExtractPluginWithRTL = require( 'mini-css-extract-plugin-with-rtl' );
 const WebpackRTLPlugin = require( 'webpack-rtl-plugin' );
 
+/**
+ * Return a webpack loader object containing our styling (Sass -> CSS) stack.
+ *
+ * @param {boolean}   preserveCssCustomProperties  whether Custom Properties and properties using them should be preserved in their original form
+ * @param {string[]}  includePaths                 Sass files lookup paths
+ * @param {string}    prelude                      String to prepend to each Sass file
+ *
+ * @return {Object}                                webpack loader object
+ */
 module.exports.loader = ( { preserveCssCustomProperties, includePaths, prelude } ) => ( {
 	test: /\.(sc|sa|c)ss$/,
 	use: [
@@ -34,6 +43,14 @@ module.exports.loader = ( { preserveCssCustomProperties, includePaths, prelude }
 	],
 } );
 
+/**
+ * Return an array of styling relevant webpack plugin objects.
+ *
+ * @param {String}   cssFilename  filename pattern to use for CSS files
+ * @param {Boolean}  minify       whether to minify CSS
+ *
+ * @return {Object[]}             styling relevant webpack plugin objects
+ */
 module.exports.plugins = ( { cssFilename, minify } ) => [
 	new MiniCssExtractPluginWithRTL( {
 		filename: cssFilename,
