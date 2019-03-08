cbf0d6d87361041bb89073a88cd560f672ccfdcc
Dave Smith <getdavemail@gmail.com>
2019-03-08 10:21:44 +0000

    Updates button wording to better convey action in context
    Designer research indicates that Cancel is a better word to use in the context of “cancel this Import”.
    
    See https://github.com/Automattic/wp-calypso/issues/30564

--- client/my-sites/importer/importer-header/close-button.jsx
+++ client/my-sites/importer/importer-header/close-button.jsx
@@ -66,7 +66,7 @@ export class CloseButton extends React.PureComponent {
 				isSecondary={ true }
 				onClick={ this.handleClick }
 			>
-				{ translate( 'Close', { context: 'verb, to Close a dialog' } ) }
+				{ translate( 'Cancel', { context: 'verb, to Close a dialog' } ) }
 			</FormButton>
 		);
 	}
