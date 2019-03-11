61eaa9098e297f4a940f231e83352898b119fa25
Jason Moon <jason.moon@automattic.com>
2019-03-11 17:16:16 -0600

    Add selectors for recommended plugins

---
+++ client/state/selectors/get-recommended-plugins.js
@@ -0,0 +1,18 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+/**
+ * Returns a list of recommended plugins for the given site.
+ * TODO: Update documentation here
+ * Returns null if the site is unknown, or monitor settings haven't been received yet.
+ *
+ * @param  {Object}    state   Global state tree
+ * @param  {Number}    siteId  The ID of the site we're querying
+ * @return {?Object}           The monitor settings of that site
+ */
+export default function getRecommendedPlugins( state, siteId ) {
+	return get( state, [ 'plugins', 'recommended', siteId, 'items' ], null );
+}
