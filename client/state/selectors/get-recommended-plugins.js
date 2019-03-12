8558a65365353e24a926e06f4d6c3daf5c2ffcee
Jason Moon <jason.moon@automattic.com>
2019-03-12 15:34:22 -0600

    Update docstrings for selectors

--- client/state/selectors/get-recommended-plugins.js
+++ client/state/selectors/get-recommended-plugins.js
@@ -6,12 +6,10 @@ import { get } from 'lodash';
 
 /**
  * Returns a list of recommended plugins for the given site.
- * TODO: Update documentation here
- * Returns null if the site is unknown, or monitor settings haven't been received yet.
  *
- * @param  {Object}    state   Global state tree
- * @param  {Number}    siteId  The ID of the site we're querying
- * @return {?Object}           The monitor settings of that site
+ * @param  {Object}    state 		Global state tree
+ * @param  {Number}    siteId 	The ID of the site we're querying
+ * @return {?Array<Object>} 		Array of plugin objects
  */
 export default function getRecommendedPlugins( state, siteId ) {
 	return get( state, [ 'plugins', 'recommended', siteId, 'items' ], null );
--- client/state/selectors/is-requesting-recommended-plugins.js
+++ client/state/selectors/is-requesting-recommended-plugins.js
@@ -5,14 +5,12 @@
 import { get } from 'lodash';
 
 /**
- * Returns a list of recommended plugins for the given site.
- * TODO: Update documentation here
- * Returns null if the site is unknown, or monitor settings haven't been received yet.
+ * Returns the requesting status of the recommended plugins request for the given site.
  *
  * @param  {Object}    state   Global state tree
  * @param  {Number}    siteId  The ID of the site we're querying
- * @return {?Object}           The monitor settings of that site
+ * @return {Boolean}           True if requesting and false otherwise
  */
 export default function isRequestingRecommendedPlugins( state, siteId ) {
-	return get( state, [ 'plugins', 'recommended', siteId, 'isRequesting' ], null );
+	return get( state, [ 'plugins', 'recommended', siteId, 'isRequesting' ], false );
 }
