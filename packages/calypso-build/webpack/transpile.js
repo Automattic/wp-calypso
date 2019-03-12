d78694b008e4be89d7e339f13c5c14f3641385a8
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-03-12 11:04:16 -0700

    Provide Console log errors when not sone parrameters are ommited.

--- packages/calypso-build/webpack/transpile.js
+++ packages/calypso-build/webpack/transpile.js
@@ -10,24 +10,44 @@
  *
  * @return {Object}   webpack loader object
  */
-module.exports.loader = ( { workerCount, configFile, cacheDirectory, cacheIdentifier } ) => ( {
-	test: /\.jsx?$/,
-	exclude: /node_modules\//,
-	use: [
-		{
-			loader: require.resolve( 'thread-loader' ),
-			options: {
-				workers: workerCount,
+module.exports.loader = ( { workerCount, configFile, cacheDirectory, cacheIdentifier } ) => {
+	if ( ! workerCount ) {
+		workerCount = 1;
+	}
+	if ( ! configFile ) {
+		console.log( 'Please specifiy the bable.js configFile' ); // eslint-disable-line no-console
+		return {};
+	}
+
+	if ( ! cacheDirectory ) {
+		console.log( 'Please specifiy the bable cacheDirectory' ); // eslint-disable-line no-console
+		return {};
+	}
+
+	if ( ! cacheIdentifier ) {
+		console.log( 'Please specifiy the bable cacheIdentifier' ); // eslint-disable-line no-console
+		return {};
+	}
+
+	return {
+		test: /\.jsx?$/,
+		exclude: /node_modules\//,
+		use: [
+			{
+				loader: require.resolve( 'thread-loader' ),
+				options: {
+					workers: workerCount,
+				},
 			},
-		},
-		{
-			loader: require.resolve( 'babel-loader' ),
-			options: {
-				configFile,
-				babelrc: false,
-				cacheDirectory,
-				cacheIdentifier,
+			{
+				loader: require.resolve( 'babel-loader' ),
+				options: {
+					configFile,
+					babelrc: false,
+					cacheDirectory,
+					cacheIdentifier,
+				},
 			},
-		},
-	],
-} );
+		],
+	};
+};
