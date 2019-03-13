339352da2877c5c014ccec94498ec12da765fa29
Jonathan Belcher <belcherj@gmail.com>
2019-03-13 14:41:17 -0400

    Update Source params (#31434)

--- client/my-sites/themes/theme-showcase.jsx
+++ client/my-sites/themes/theme-showcase.jsx
@@ -241,7 +241,7 @@ class ThemeShowcase extends React.Component {
 						<RandomThemesBanner banners={ themeBanners } />
 					) }
 					{ showBanners && abtest( 'builderReferralThemesBanner' ) === 'builderReferralBanner' && (
-						<UpworkBanner location={ 'themes' } />
+						<UpworkBanner location={ 'theme-banner' } />
 					) }
 					<ThemesSearchCard
 						onSearch={ this.doSearch }
