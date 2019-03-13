a8abe37186f6fa581109615b7c1e7b65142ea3ba
Jon Surrell <jon.surrell@automattic.com>
2019-03-13 21:46:53 +0100

    Add test build

---
+++ packages/jetpack-blocks/test/build-script.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-nodejs-modules */
+/**
+ * External dependencies
+ */
+import { exec } from 'child_process';
+import { join } from 'path';
+import { readFile } from 'fs';
+
+describe( 'jetpack-blocks-build', () => {
+	test(
+		'builds-as-expected',
+		() =>
+			new Promise( ( resolve, reject ) => {
+				exec(
+					`SOURCEMAP='none' node ${ join( __dirname, '..', 'bin', 'build.js' ) } --test-build`,
+					buildError => {
+						if ( buildError ) {
+							return reject( buildError );
+						}
+						readFile(
+							join( __dirname, '..', 'dist', 'test.js' ),
+							'utf8',
+							( readError, contents ) => {
+								if ( readError ) {
+									return reject( readError );
+								}
+								expect( contents ).toMatchSnapshot();
+								resolve();
+							}
+						);
+					}
+				);
+			} ),
+		30000
+	);
+} );
---
+++ packages/jetpack-blocks/test/fixtures/test-build.js
@@ -0,0 +1,10 @@
+export const { destructured: destructuredAs = 'default' } = {
+	[ 'destructured' ]: 'destructured',
+};
+
+export class SomeClass {
+	instanceProp = null;
+	arrowMethod = () => {};
+	method() {}
+	static thing = null;
+}
