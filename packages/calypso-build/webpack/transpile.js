05c541c23756f1ee60b61e6e57edf281d6e909da
Bernie Reiter <ockham@raz.or.at>
2019-03-13 15:26:27 +0100

    Minor JSDoc change (just Babel, not loader)

--- packages/calypso-build/webpack/transpile.js
+++ packages/calypso-build/webpack/transpile.js
@@ -3,9 +3,9 @@
  *
  * @param {Object} _                 Options
  * @param {number} _.workerCount     Number of workers that are being used by the thread-loader
- * @param {string} _.configFile      Babel loader config file
- * @param {string} _.cacheDirectory  Babel loader cache directory
- * @param {string} _.cacheIdentifier Babel loader cache identifier
+ * @param {string} _.configFile      Babel config file
+ * @param {string} _.cacheDirectory  Babel cache directory
+ * @param {string} _.cacheIdentifier Babel cache identifier
  * @param {RegExp} _.exclude         Directories to exclude when looking for files to transpile
  *
  * @return {Object} Webpack loader object
