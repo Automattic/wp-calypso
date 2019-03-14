d8d71fa6ded87d7ace1983549ed67b8c9b2154ab
Jarda Snajdr <jsnajdr@gmail.com>
2019-03-14 14:03:57 +0100

    ClipboardButtonInput: use hooks

--- client/components/clipboard-button-input/index.jsx
+++ client/components/clipboard-button-input/index.jsx
@@ -7,8 +7,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import { omit } from 'lodash';
-import { localize } from 'i18n-calypso';
+import { useTranslate } from 'i18n-calypso';
 import { connect } from 'react-redux';
 
 /**
@@ -24,85 +23,47 @@ import { recordTracksEvent } from 'state/analytics/actions';
  */
 import './style.scss';
 
-class ClipboardButtonInputExport extends React.Component {
-	constructor( props ) {
-		super( props );
-		this.state = {
-			isCopied: false,
-			disabled: false,
-		};
-	}
-	static propTypes = {
-		value: PropTypes.string,
-		disabled: PropTypes.bool,
-		className: PropTypes.string,
-		hideHttp: PropTypes.bool,
-		moment: PropTypes.func,
-		numberFormat: PropTypes.func,
-		translate: PropTypes.func,
+function ClipboardButtonInput( { value = '', className, disabled, hideHttp, dispatch, ...rest } ) {
+	const translate = useTranslate();
+
+	const [ isCopied, setCopied ] = React.useState( false );
+
+	React.useEffect(() => {
+		if ( isCopied ) {
+			const confirmationTimeout = setTimeout( () => setCopied( false ), 4000 );
+			return () => clearTimeout( confirmationTimeout );
+		}
+	}, [ isCopied ]);
+
+	const showConfirmation = () => {
+		setCopied( true );
+		dispatch( recordTracksEvent( 'calypso_editor_clipboard_url_button_click' ) );
 	};
 
-	static defaultProps = {
-		value: '',
-	};
+	const classes = classnames( 'clipboard-button-input', className );
 
-	componentWillUnmount() {
-		clearTimeout( this.confirmationTimeout );
-		delete this.confirmationTimeout;
-	}
-
-	showConfirmation = () => {
-		this.setState( {
-			isCopied: true,
-		} );
-
-		this.confirmationTimeout = setTimeout( () => {
-			this.setState( {
-				isCopied: false,
-			} );
-		}, 4000 );
-		this.props.recordTracksEvent( 'calypso_editor_clipboard_url_button_click' );
-	};
-
-	render() {
-		const { value, className, disabled, hideHttp, translate } = this.props;
-		const classes = classnames( 'clipboard-button-input', className );
-
-		return (
-			<span className={ classes }>
-				<FormTextInput
-					{ ...omit(
-						this.props,
-						'className',
-						'hideHttp',
-						'moment',
-						'numberFormat',
-						'translate',
-						'recordTracksEvent'
-					) }
-					value={ hideHttp ? withoutHttp( value ) : value }
-					type="text"
-					selectOnFocus
-					readOnly
-				/>
-				<ClipboardButton
-					text={ value }
-					onCopy={ this.showConfirmation }
-					disabled={ disabled }
-					compact
-				>
-					{ this.state.isCopied
-						? translate( 'Copied!' )
-						: translate( 'Copy', { context: 'verb' } ) }
-				</ClipboardButton>
-			</span>
-		);
-	}
+	return (
+		<span className={ classes }>
+			<FormTextInput
+				{ ...rest }
+				disabled={ disabled }
+				value={ hideHttp ? withoutHttp( value ) : value }
+				type="text"
+				selectOnFocus
+				readOnly
+			/>
+			<ClipboardButton text={ value } onCopy={ showConfirmation } disabled={ disabled } compact>
+				{ isCopied ? translate( 'Copied!' ) : translate( 'Copy', { context: 'verb' } ) }
+			</ClipboardButton>
+		</span>
+	);
 }
 
-export default connect(
-	null,
-	{
-		recordTracksEvent,
-	}
-)( localize( ClipboardButtonInputExport ) );
+ClipboardButtonInput.propTypes = {
+	value: PropTypes.string,
+	disabled: PropTypes.bool,
+	className: PropTypes.string,
+	hideHttp: PropTypes.bool,
+};
+
+export default connect()( ClipboardButtonInput );
