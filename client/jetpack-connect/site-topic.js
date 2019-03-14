4add1d9853b843daed870112b67b6106499578c2
AnnaMag <anna.mag.kedzierska@gmail.com>
2019-03-14 01:42:53 +0100

    Redirect to the user type step from the site vertical one.
    We hook up the new screen to the already merged NUX flow.

--- client/jetpack-connect/site-topic.js
+++ client/jetpack-connect/site-topic.js
@@ -26,7 +26,7 @@ class JetpackSiteTopic extends Component {
 
 		this.props.saveSiteVertical( siteId, siteVertical );
 
-		page( `/jetpack/connect/plans/${ siteSlug }` );
+		page.redirect( `/jetpack/connect/user-type/${ siteSlug }` );
 	};
 
 	render() {
