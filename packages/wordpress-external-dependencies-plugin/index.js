3ab42614306361c9accaf6606c134b3ac9c43d2a
Jon Surrell <jon.surrell@automattic.com>
2019-03-18 15:50:42 +0100

    First working version

--- packages/wordpress-external-dependencies-plugin/index.js
+++ packages/wordpress-external-dependencies-plugin/index.js
@@ -1,23 +1,62 @@
+/**
+ * External dependencies
+ */
 const { ExternalsPlugin } = require( 'webpack' );
+const { RawSource } = require( 'webpack-sources' );
 
-class WordPressExternalDependenciesPlugin extends ExternalsPlugin {
+/**
+ * Internal dependencies
+ */
+const wpModules = require( './wp-modules' );
+
+class WordPressExternalDependenciesPlugin {
 	constructor() {
-		super( 'global', [ wpExternals ] );
+		this.externalsPlugin = new ExternalsPlugin( 'global', [ wpExternals ] );
+
+		this.entryDeps = new Map();
+	}
+
+	apply( compiler ) {
+		this.externalsPlugin.apply( compiler );
+
+		compiler.hooks.compilation.tap( this.constructor.name, compilation => {
+			compilation.hooks.additionalChunkAssets.tap( this.constructor.name, (/* chunks */) => {
+				// Get entry points
+				compilation.chunkGroups.forEach( chunkGroup => {
+					if ( ! chunkGroup.isInitial() ) {
+						return;
+					}
+
+					dependencyIteration: for ( const [ wpModule, { dependency } ] of wpModules.entries() ) {
+						for ( const chunk of chunkGroup.chunks ) {
+							for ( const chunkModule of chunk.modulesIterable ) {
+								if ( chunkModule.userRequest === wpModule ) {
+									this.entryDeps.has( chunkGroup )
+										? this.entryDeps.get( chunkGroup ).add( dependency )
+										: this.entryDeps.set( chunkGroup, new Set( [ dependency ] ) );
+									continue dependencyIteration;
+								}
+							}
+						}
+					}
+				} );
+				for ( const [ entry, deps ] of this.entryDeps.entries() ) {
+					const entryFiles = entry.getFiles();
+					if ( entryFiles.length ) {
+						const depsFile = entryFiles[ 0 ].replace( '.js', '.deps.json' );
+						compilation.assets[ depsFile ] = new RawSource( JSON.stringify( Array.from( deps ) ) );
+					}
+				}
+			} );
+		} );
 	}
 }
 
-function wordpressRequire( request ) {
-	// @wordpress/components -> [ @wordpress, components ]
-	const [ , name ] = request.split( '/' );
-
-	// components -> wp.components
-	return `wp.${ name.replace( /-([a-z])/g, ( match, letter ) => letter.toUpperCase() ) }`;
-}
-
 function wpExternals( context, request, callback ) {
-	return /^@wordpress\//.test( request )
-		? callback( null, `root ${ wordpressRequire( request ) }` )
-		: callback();
+	if ( wpModules.has( request ) ) {
+		return callback( null, `root ${ wpModules.get( request ).globalName }` );
+	}
+	return callback();
 }
 
 module.exports = WordPressExternalDependenciesPlugin;
