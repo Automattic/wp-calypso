2eeb65ba06be7712c5c61d4fbbb139b023aaa471
Jason Moon <jason.moon@automattic.com>
2019-03-18 15:40:00 -0600

    Align jsdoc fragments

--- client/state/selectors/get-recommended-plugins.js
+++ client/state/selectors/get-recommended-plugins.js
@@ -7,9 +7,9 @@ import { get } from 'lodash';
 /**
  * Returns a list of recommended plugins for the given site.
  *
- * @param  {Object}    state 		Global state tree
- * @param  {Number}    siteId 	The ID of the site we're querying
- * @return {?Array<Object>} 		Array of plugin objects
+ * @param  {Object}         state   Global state tree
+ * @param  {Number}         siteId  The ID of the site we're querying
+ * @return {Array<Object>}          Array of plugin objects
  */
 export default function getRecommendedPlugins( state, siteId ) {
 	return get( state, [ 'plugins', 'recommended', 'items', siteId ], [] );
