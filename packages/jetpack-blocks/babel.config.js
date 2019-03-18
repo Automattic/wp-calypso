fb24d89d93e3264a88e09a1760def7d4685fff62
Miguel Lezama <miguel@automattic.com>
2019-03-18 16:04:04 -0300

    Add internal bable.config.js

---
+++ packages/jetpack-blocks/babel.config.js
@@ -0,0 +1,89 @@
+/** @format */
+const _ = require( 'lodash' );
+
+const targets = { browsers: [ 'last 2 versions', 'Safari >= 10', 'iOS >= 10', 'ie >= 11' ] };
+
+const config = {
+	presets: [
+		[
+			'@babel/env',
+			{
+				targets,
+				useBuiltIns: 'entry',
+				// allows es7 features like Promise.prototype.finally
+				shippedProposals: true,
+				// Exclude transforms that make all code slower, see https://github.com/facebook/create-react-app/pull/5278
+				exclude: [ 'transform-typeof-symbol' ],
+			},
+		],
+		'@babel/react',
+	],
+	plugins: _.compact( [
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-proposal-export-default-from',
+		'@babel/plugin-proposal-export-namespace-from',
+		'@babel/plugin-syntax-dynamic-import',
+		[
+			'@babel/transform-runtime',
+			{
+				corejs: false, // we polyfill so we don't need core-js
+				helpers: true,
+				regenerator: false,
+				useESModules: false,
+			},
+		],
+	] ),
+	overrides: [
+		{
+			test: [ './client/gutenberg/extensions', './packages/jetpack-blocks' ],
+			plugins: [
+				[
+					'@wordpress/import-jsx-pragma',
+					{
+						scopeVariable: 'createElement',
+						source: '@wordpress/element',
+						isDefault: false,
+					},
+				],
+				[
+					'@babel/transform-react-jsx',
+					{
+						pragma: 'createElement',
+					},
+				],
+			],
+		},
+	],
+	env: {
+		build_pot: {
+			plugins: [
+				[
+					'@wordpress/babel-plugin-makepot',
+					{
+						output: 'build/i18n-calypso/gutenberg-strings.pot',
+						headers: {
+							'content-type': 'text/plain; charset=UTF-8',
+							'x-generator': 'calypso',
+						},
+					},
+				],
+				[
+					'@automattic/babel-plugin-i18n-calypso',
+					{
+						dir: 'build/i18n-calypso/',
+						headers: {
+							'content-type': 'text/plain; charset=UTF-8',
+							'x-generator': 'calypso',
+						},
+					},
+				],
+			],
+		},
+		test: {
+			presets: [ [ '@babel/env', { targets: { node: 'current' } } ] ],
+			plugins: [ 'add-module-exports', 'babel-plugin-dynamic-import-node' ],
+		},
+	},
+};
+
+module.exports = config;
--- packages/jetpack-blocks/webpack.config.js
+++ packages/jetpack-blocks/webpack.config.js
@@ -112,7 +112,7 @@ function getWebpackConfig() {
 			rules: [
 				TranspileConfig.loader( {
 					workerCount,
-					configFile: path.join( __dirname, '..', '..', 'babel.config.js' ), // FIX ME
+					configFile: path.join( __dirname, 'babel.config.js' ),
 					cacheDirectory: path.join( __dirname, '.cache' ),
 					cacheIdentifier,
 					exclude: /node_modules\//,
