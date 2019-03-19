fe375e0af4c634a516c5ee0de9f8b526bbd0959b
Jason Moon <jason.moon@automattic.com>
2019-03-19 15:30:49 -0600

    Remove Tracks calls from the data layer

--- client/state/data-layer/wpcom/sites/plugins/recommended/index.js
+++ client/state/data-layer/wpcom/sites/plugins/recommended/index.js
@@ -9,14 +9,12 @@ import {
 } from 'state/plugins/recommended/actions';
 import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
 import { http } from 'state/data-layer/wpcom-http/actions';
-import { recordTracksEvent } from 'state/analytics/actions';
 import { registerHandlers } from 'state/data-layer/handler-registry';
 
 export const fetch = action => {
 	const { siteId, limit = 6 } = action;
 
 	return [
-		recordTracksEvent( 'calypso_recommended_plugins_requested', { site_id: siteId, limit } ),
 		http(
 			{
 				apiNamespace: 'wpcom/v2',
@@ -29,18 +27,12 @@ export const fetch = action => {
 	];
 };
 
-export const onSuccess = ( { siteId, limit }, data ) => {
-	return [
-		recordTracksEvent( 'calypso_recommended_plugins_received', { site_id: siteId, limit } ),
-		receiveRecommendedPlugins( siteId, normalizePluginsList( data ) ),
-	];
+export const onSuccess = ( { siteId }, data ) => {
+	return [ receiveRecommendedPlugins( siteId, normalizePluginsList( data ) ) ];
 };
 
-export const onError = ( { siteId, limit } ) => {
-	return [
-		recordTracksEvent( 'calypso_recommended_plugins_error', { site_id: siteId, limit } ),
-		dispatchRecommendPluginsRequestFailure(),
-	];
+export const onError = () => {
+	return [ dispatchRecommendPluginsRequestFailure() ];
 };
 
 registerHandlers( 'state/data-layer/wpcom/sites/plugins/recommended/index.js', {
