a168ca511f637daa25c4cbc1cee236a2302b82fe
Jason Moon <jason.moon@automattic.com>
2019-03-19 13:12:50 -0600

    Align jsdoc comments and collapse objects

--- client/state/plugins/recommended/actions.js
+++ client/state/plugins/recommended/actions.js
@@ -10,30 +10,22 @@ import 'state/data-layer/wpcom/sites/plugins/recommended';
  * Returns an action object that's bound to the data layer;
  * fetches a list of recommended plugins for the given siteId.
  *
- * @param  {Number}  siteId        Site ID
- * @param  {Number}  limit         Number of desired plugin recommendations
- * @return {Object}  Action object
+ * @param  {Number}  siteId  Site ID
+ * @param  {Number}  limit   Number of desired plugin recommendations
+ * @return {Object}          Action object
  */
 export function fetchRecommendedPlugins( siteId, limit = 6 ) {
-	return {
-		limit,
-		siteId,
-		type: PLUGINS_RECOMMENDED_REQUEST,
-	};
+	return { limit, siteId, type: PLUGINS_RECOMMENDED_REQUEST };
 }
 
 /**
  * Returns an action object to signal that a list of recommended plugins
  * has been received.
  *
- * @param  {Number}  siteId         Site ID
+ * @param  {Number}         siteId  Site ID
  * @param  {Array<Object>}  data    List of recommended plugins
- * @return {Object}  Action object
+ * @return {Object}                 Action object
  */
 export function receiveRecommendedPlugins( siteId, data ) {
-	return {
-		data,
-		siteId,
-		type: PLUGINS_RECOMMENDED_RECEIVE,
-	};
+	return { data, siteId, type: PLUGINS_RECOMMENDED_RECEIVE };
 }
