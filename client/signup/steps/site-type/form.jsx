e64001e08c5c7d24be02021b4609e171a84ab2ae
Shaun Andrews <shaun@automattic.com>
2019-03-19 15:34:06 -0400

    Signup: Remove radio's and button from `site-type` step (#31400)
    Using clickable cards to replace radios and the button.

--- client/signup/steps/site-type/form.jsx
+++ client/signup/steps/site-type/form.jsx
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import React, { Component } from 'react';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
@@ -10,11 +9,7 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
-import Button from 'components/button';
 import Card from 'components/card';
-import FormFieldset from 'components/forms/form-fieldset';
-import FormLabel from 'components/forms/form-label';
-import FormRadio from 'components/forms/form-radio';
 import { getAllSiteTypes } from 'lib/signup/site-type';
 import { recordTracksEvent } from 'state/analytics/actions';
 
@@ -39,54 +34,33 @@ class SiteTypeForm extends Component {
 		};
 	}
 
-	handleRadioChange = event => this.setState( { siteType: event.currentTarget.value } );
-
-	handleSubmit = event => {
-		const { siteType } = this.state;
-
-		event.preventDefault();
-
+	handleSubmit = type => {
 		this.props.recordTracksEvent( 'calypso_signup_actions_submit_site_type', {
-			value: siteType,
+			value: type,
 		} );
 
-		this.props.submitForm( siteType );
+		this.setState( { siteType: type } );
+
+		this.props.submitForm( type );
 	};
 
-	renderRadioOptions() {
+	renderSegmentOptions() {
 		return getAllSiteTypes().map( siteTypeProperties => (
-			<FormLabel
-				className={ classNames( 'site-type__option', {
-					'is-selected': siteTypeProperties.slug === this.state.siteType,
-				} ) }
+			<Card
+				className="site-type__option"
 				key={ siteTypeProperties.id }
+				displayAsLink
+				tagName="button"
+				onClick={ this.handleSubmit.bind( this, siteTypeProperties.slug ) }
 			>
-				<FormRadio
-					value={ siteTypeProperties.slug }
-					checked={ siteTypeProperties.slug === this.state.siteType }
-					onChange={ this.handleRadioChange }
-				/>
 				<strong className="site-type__option-label">{ siteTypeProperties.label }</strong>
 				<span className="site-type__option-description">{ siteTypeProperties.description }</span>
-			</FormLabel>
+			</Card>
 		) );
 	}
 
 	render() {
-		const { translate } = this.props;
-
-		return (
-			<div className="site-type__wrapper">
-				<form onSubmit={ this.handleSubmit }>
-					<Card>
-						<FormFieldset>{ this.renderRadioOptions() }</FormFieldset>
-						<Button primary={ true } type="submit" disabled={ ! this.state.siteType }>
-							{ translate( 'Continue' ) }
-						</Button>
-					</Card>
-				</form>
-			</div>
-		);
+		return <Card className="site-type__wrapper">{ this.renderSegmentOptions() }</Card>;
 	}
 }
 
