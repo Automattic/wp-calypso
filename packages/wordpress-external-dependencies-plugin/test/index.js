8d66a2f5896487d635d7211853b8f895ad28a65b
Jon Surrell <jon.surrell@automattic.com>
2019-03-20 23:51:49 +0100

    Starting test work

---
+++ packages/wordpress-external-dependencies-plugin/test/index.js
@@ -0,0 +1,42 @@
+const webpack = require( 'webpack' );
+const WordPressExternalDependenciesPlugin = require( '..' );
+const { join } = require( 'path' );
+const { readFile } = require( 'fs' );
+const rimraf = require( 'rimraf' ).sync;
+
+afterEach( () => {
+	rimraf( join( __dirname, 'result' ) );
+} );
+
+describe( 'wordpress-external-dependencies-plugin', () => {
+	test(
+		'produces no output when no relevant deps are found',
+		() =>
+			new Promise( ( resolve, reject ) =>
+				webpack(
+					{
+						entry: {
+							'no-deps': join( __dirname, 'test-source', 'no-deps' ),
+						},
+						output: {
+							path: join( __dirname, 'result' ),
+						},
+						plugins: [ new WordPressExternalDependenciesPlugin() ],
+					},
+					webpackErr => {
+						if ( webpackErr ) {
+							reject( webpackErr );
+						}
+						readFile( join( __dirname, 'result', 'no-deps.js' ), ( readErr, contents ) => {
+							if ( readErr ) {
+								reject( readErr );
+							}
+							expect( contents ).toMatchSnapshot();
+							resolve();
+						} );
+					}
+				)
+			),
+		10000
+	);
+} );
