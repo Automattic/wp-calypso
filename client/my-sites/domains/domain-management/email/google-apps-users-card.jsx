68c2df85bd7bf6ec1ed0eda38403f5ca4e47f316
Jonathan Belcher <belcherj@gmail.com>
2019-03-21 12:26:43 -0400

    Refactors GSuiteUserItem (#31613)

--- client/my-sites/domains/domain-management/email/google-apps-users-card.jsx
+++ client/my-sites/domains/domain-management/email/google-apps-users-card.jsx
@@ -19,7 +19,7 @@ import { composeAnalytics, recordGoogleEvent, recordTracksEvent } from 'state/an
 import { domainManagementAddGSuiteUsers } from 'my-sites/domains/paths';
 import { hasPendingGSuiteUsers } from 'lib/domains/gsuite';
 import { getSelectedDomain } from 'lib/domains';
-import GoogleAppsUserItem from './google-apps-user-item';
+import GSuiteUserItem from 'my-sites/domains/domain-management/email/gsuite-user-item';
 import Notice from 'components/notice';
 import PendingGSuiteTosNotice from 'my-sites/domains/components/domain-warnings/pending-gsuite-tos-notice';
 import SectionHeader from 'components/section-header';
@@ -47,7 +47,7 @@ class GoogleAppsUsers extends React.Component {
 
 	generateClickHandler( user ) {
 		return () => {
-			this.props.manageClick( this.props.selectedDomainName, user );
+			this.props.manageClick( user.domain, user.email );
 		};
 	}
 
@@ -114,7 +114,7 @@ class GoogleAppsUsers extends React.Component {
 		}
 
 		return (
-			<GoogleAppsUserItem
+			<GSuiteUserItem
 				key={ `google-apps-user-${ user.domain }-${ index }` }
 				user={ user }
 				onClick={ this.generateClickHandler( user ) }
---
+++ client/my-sites/domains/domain-management/email/gsuite-user-item/test/index.js
@@ -0,0 +1,37 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+/**
+ * Internal dependencies
+ */
+import GSuiteUserItem from '../';
+
+const noop = () => {};
+
+describe( 'GSuiteUserItem', () => {
+	test( 'renders correctly', () => {
+		const tree = renderer
+			.create(
+				<GSuiteUserItem onClick={ noop } user={ { email: 'foo@bar.buzz', domain: 'bar.buzz' } } />
+			)
+			.toJSON();
+		expect( tree ).toMatchSnapshot();
+	} );
+
+	test( 'should call onClick function when manage is clicked', done => {
+		const callback = jest.fn( () => {
+			done();
+		} );
+		const instance = renderer.create(
+			<GSuiteUserItem onClick={ callback } user={ { email: 'foo@bar.buzz', domain: 'bar.buzz' } } />
+		);
+		const link = instance.root.findByProps( { className: 'gsuite-user-item__manage-link' } );
+		// trigger the onClick
+		link.props.onClick( 'buzz' );
+		expect( callback ).toHaveBeenCalledWith( 'buzz' );
+	} );
+} );
