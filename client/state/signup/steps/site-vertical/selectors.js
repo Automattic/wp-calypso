3650e5477217be8f78ced39b7e2192f222c98c5c
James Tien <southp0105@gmail.com>
2019-03-22 18:50:50 +0800

    Draft: making the site vertical data a derived data from selector, rather than a vanilla state tree.

--- client/state/signup/steps/site-vertical/selectors.js
+++ client/state/signup/steps/site-vertical/selectors.js
@@ -4,7 +4,12 @@
  * External dependencies
  */
 
-import { get } from 'lodash';
+import { get, find } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { getVerticals } from 'state/signup/verticals/selectors';
 
 export function getSiteVerticalName( state ) {
 	return get( state, 'signup.steps.siteVertical.name', '' );
@@ -26,6 +31,34 @@ export function getSiteVerticalIsUserInput( state ) {
 	return get( state, 'signup.steps.siteVertical.isUserInput', true );
 }
 
+export function getSiteVerticalData( state ) {
+	const verticalName = get( state, 'signup.steps.siteVertical.name', '' );
+
+	const verticals = getVerticals( state, verticalName );
+
+	// TODO: this can be optimized
+	const match = find(
+		verticals,
+		item => item.verticalName.toLowerCase() === verticalName.toLowerCase()
+	);
+
+	if ( match ) {
+		return match;
+	}
+
+	// TODO: ARRRGHHH. I'm running out of time ...
+	const defaultVerticalData = get( verticals, 'business[0]', {} );
+
+	return {
+		isUserInputVertical: true,
+		parent: '',
+		preview: defaultVerticalData.preview,
+		verticalId: '',
+		verticalName,
+		verticalSlug: verticalName,
+	};
+}
+
 export function getSiteVerticalPreview( state ) {
-	return get( state, 'signup.steps.siteVertical.preview', '' );
+	return get( getSiteVerticalData( state ), 'preview', '' );
 }
