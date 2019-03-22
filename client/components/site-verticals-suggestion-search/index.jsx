cafd9ebba4f5175b7757747ca055c8517d107c65
James Tien <southp0105@gmail.com>
2019-03-22 17:59:47 +0800

    Plumb the underlying `signup.verticals` state tree onto `SiteVerticalsSuggestionSearch` component

--- client/components/site-verticals-suggestion-search/index.jsx
+++ client/components/site-verticals-suggestion-search/index.jsx
@@ -6,7 +6,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { find, get, noop, startsWith, trim, uniq } from 'lodash';
+import { find, get, noop, startsWith, uniq } from 'lodash';
 import { localize } from 'i18n-calypso';
 import { v4 as uuid } from 'uuid';
 
@@ -14,10 +14,9 @@ import { v4 as uuid } from 'uuid';
  * Internal dependencies
  */
 import SuggestionSearch from 'components/suggestion-search';
-import { getHttpData, requestHttpData } from 'state/data-layer/http-data';
-import { http } from 'state/data-layer/wpcom-http/actions';
-import { convertToCamelCase } from 'state/data-layer/utils';
 import PopularTopics from 'components/site-verticals-suggestion-search/popular-topics';
+import QueryVerticals from 'components/data/query-verticals';
+import { getVerticals } from 'state/signup/verticals/selectors';
 
 /**
  * Style dependencies
@@ -31,8 +30,6 @@ export class SiteVerticalsSuggestionSearch extends Component {
 		lastUpdated: PropTypes.number,
 		onChange: PropTypes.func,
 		placeholder: PropTypes.string,
-		requestDefaultVertical: PropTypes.func,
-		requestVerticals: PropTypes.func,
 		shouldShowPopularTopics: PropTypes.func,
 		searchResultsLimit: PropTypes.number,
 		verticals: PropTypes.array,
@@ -44,8 +41,6 @@ export class SiteVerticalsSuggestionSearch extends Component {
 		initialValue: '',
 		onChange: noop,
 		placeholder: '',
-		requestDefaultVertical: noop,
-		requestVerticals: noop,
 		shouldShowPopularTopics: noop,
 		searchResultsLimit: 5,
 		verticals: [],
@@ -55,26 +50,8 @@ export class SiteVerticalsSuggestionSearch extends Component {
 	constructor( props ) {
 		super( props );
 		this.state = {
-			searchValue: props.initialValue,
 			railcar: this.getNewRailcar(),
 		};
-		props.requestDefaultVertical();
-	}
-
-	componentDidMount() {
-		// If we have a stored vertical, grab the preview
-		this.props.initialValue && this.props.requestVerticals( this.props.initialValue, 1 );
-	}
-
-	componentDidUpdate( prevProps ) {
-		// Check if there's a direct match for any subsequent
-		// HTTP requests
-		if ( prevProps.lastUpdated !== this.props.lastUpdated ) {
-			this.updateVerticalData(
-				this.searchForVerticalMatches( this.state.searchValue ),
-				this.state.searchValue
-			);
-		}
 	}
 
 	getNewRailcar() {
@@ -117,16 +94,13 @@ export class SiteVerticalsSuggestionSearch extends Component {
 			// Don't trigger a search if there's already an exact, non-user-defined match from the API
 			! result
 		) {
-			this.props.requestVerticals( value, this.props.searchResultsLimit );
 			this.setState( { railcar: this.getNewRailcar() } );
 		}
 
-		this.setState( { searchValue: value } );
 		this.updateVerticalData( result, value );
 	};
 
 	onPopularTopicSelect = value => {
-		this.props.requestVerticals( value, 1 );
 		this.setState( { searchValue: value } );
 	};
 
@@ -158,71 +132,35 @@ export class SiteVerticalsSuggestionSearch extends Component {
 
 	render() {
 		const { translate, placeholder, autoFocus, shouldShowPopularTopics } = this.props;
-		const showPopularTopics = shouldShowPopularTopics( this.state.searchValue );
+		const showPopularTopics = shouldShowPopularTopics( this.props.searchValue );
 		return (
 			<>
+				<QueryVerticals searchTerm={ this.props.searchValue } />
+				<QueryVerticals searchTerm={ 'business' } limit={ 1 } />
 				<SuggestionSearch
 					id="siteTopic"
 					placeholder={ placeholder || translate( 'Enter a keyword or select one from below.' ) }
 					onChange={ this.onSiteTopicChange }
 					suggestions={ this.getSuggestions() }
-					value={ this.state.searchValue }
+					value={ this.props.searchValue }
 					sortResults={ this.sortSearchResults }
 					autoFocus={ autoFocus } // eslint-disable-line jsx-a11y/no-autofocus
 					railcar={ this.state.railcar }
 				/>
-				{ showPopularTopics && <PopularTopics onSelect={ this.onPopularTopicSelect } /> }
+				{ showPopularTopics && <PopularTopics onSelect={ this.onSiteTopicChange } /> }
 			</>
 		);
 	}
 }
 
-const SITE_VERTICALS_REQUEST_ID = 'site-verticals-search-results';
-const DEFAULT_SITE_VERTICAL_REQUEST_ID = 'default-site-verticals-search-results';
-
-const requestSiteVerticalHttpData = ( searchTerm, limit = 7, id = SITE_VERTICALS_REQUEST_ID ) => {
-	searchTerm = trim( searchTerm );
-	requestHttpData(
-		id,
-		http( {
-			apiNamespace: 'wpcom/v2',
-			method: 'GET',
-			path: '/verticals',
-			query: {
-				search: searchTerm,
-				limit,
-				include_preview: true,
-			},
-		} ),
-		{
-			fromApi: () => data => [ [ id, convertToCamelCase( data ) ] ],
-			freshness: -Infinity,
-		}
-	);
-};
-
-export const isVerticalSearchPending = () =>
-	'pending' === get( getHttpData( SITE_VERTICALS_REQUEST_ID ), 'state', false );
-
 export default localize(
 	connect(
-		() => {
-			const siteVerticalHttpData = getHttpData( SITE_VERTICALS_REQUEST_ID );
-			const defaultVerticalHttpData = getHttpData( DEFAULT_SITE_VERTICAL_REQUEST_ID );
-			return {
-				lastUpdated: get( siteVerticalHttpData, 'lastUpdated', 0 ),
-				verticals: get( siteVerticalHttpData, 'data', [] ),
-				defaultVertical: get( defaultVerticalHttpData, 'data[0]', {} ),
-			};
-		},
+		( state, ownProps ) => ( {
+			verticals: getVerticals( state, ownProps.searchValue ) || [],
+			defaultVertical: get( getVerticals( state, 'business' ), '0', {} ),
+		} ),
 		( dispatch, ownProps ) => ( {
 			shouldShowPopularTopics: searchValue => ! searchValue && ownProps.showPopular,
-			requestVerticals: requestSiteVerticalHttpData,
-			requestDefaultVertical: ( searchTerm = 'business' ) => {
-				if ( ! get( getHttpData( DEFAULT_SITE_VERTICAL_REQUEST_ID ), 'data', null ) ) {
-					requestSiteVerticalHttpData( searchTerm, 1, DEFAULT_SITE_VERTICAL_REQUEST_ID );
-				}
-			},
 		} )
 	)( SiteVerticalsSuggestionSearch )
 );
