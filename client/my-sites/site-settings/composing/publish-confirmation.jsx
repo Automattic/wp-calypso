e1b1702d7244efc32fae5e115ec3e72aef8859f7
retrofox <rdsuarez@gmail.com>
2019-03-22 18:07:04 -0300

    site-setting: enable/disable publish confirmation It hides the toggle of the Publish confirmation option if the current editor of the site is the Block Editor, showing a message in order to let to the customer know about this change.

--- client/my-sites/site-settings/composing/publish-confirmation.jsx
+++ client/my-sites/site-settings/composing/publish-confirmation.jsx
@@ -15,12 +15,15 @@ import { bindActionCreators } from 'redux';
  */
 import CompactFormToggle from 'components/forms/form-toggle/compact';
 import FormFieldset from 'components/forms/form-fieldset';
+import FormLabel from 'components/forms/form-label';
 import FormSettingExplanation from 'components/forms/form-setting-explanation';
 import QueryPreferences from 'components/data/query-preferences';
 import { isFetchingPreferences } from 'state/preferences/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { isConfirmationSidebarEnabled } from 'state/ui/editor/selectors';
 import { saveConfirmationSidebarPreference } from 'state/ui/editor/actions';
+import { isGutenbergEnabled } from 'state/selectors/is-gutenberg-enabled';
+import { getSelectedEditor } from 'state/selectors/get-selected-editor';
 
 class PublishConfirmation extends Component {
 	constructor( props ) {
@@ -43,7 +46,24 @@ class PublishConfirmation extends Component {
 	}
 
 	render() {
-		const { fetchingPreferences, translate } = this.props;
+		const { fetchingPreferences, translate, showPublishFlow } = this.props;
+
+		if ( showPublishFlow ) {
+			return (
+				<FormFieldset>
+					<FormLabel>{ translate( 'Show publish confirmation' ) }</FormLabel>
+					<FormSettingExplanation isIndented>
+						{ translate(
+							'The confirmation step for double-checking your content before publishing ' +
+								'now is handled by the new block editor.' +
+								'To show the publish confirmation with the block editor, ' +
+								'click the editor menu, then "Options", ' +
+								'and check "Enable Pre-publish Checks".'
+						) }
+					</FormSettingExplanation>
+				</FormFieldset>
+			);
+		}
 
 		return (
 			<FormFieldset className="composing__publish-confirmation has-divider is-bottom-only">
@@ -87,6 +107,8 @@ export default connect(
 			siteId,
 			fetchingPreferences: isFetchingPreferences( state ),
 			publishConfirmationEnabled: isConfirmationSidebarEnabled( state, siteId ),
+			showPublishFlow:
+				isGutenbergEnabled( state, siteId ) && getSelectedEditor( state, siteId ) === 'gutenberg',
 		};
 	},
 	dispatch => {
