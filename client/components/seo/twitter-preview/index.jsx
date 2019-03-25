27ef960ffeab1363416e1cfe77494e9b664d91bf
Arun Sathiya <arun@sathiya.me>
2019-03-25 23:33:03 +0530

    Lint fix: twitter-preview: Follow CSS namespace guidelines (#31666)
    * Follow CSS namespace guidelines for twitter-preview
    
    * Update classnames inside template strings to follow CSS namespace guidelines

--- client/components/seo/twitter-preview/index.jsx
+++ client/components/seo/twitter-preview/index.jsx
@@ -21,13 +21,13 @@ export class TwitterPreview extends PureComponent {
 		};
 
 		return (
-			<div className="twitter-card-preview__container">
-				<div className={ `twitter-card-preview twitter-card-preview__${ type }` }>
-					{ image && <div className="twitter-card-preview__image" style={ previewImageStyle } /> }
-					<div className="twitter-card-preview__body">
-						<div className="twitter-card-preview__title">{ title }</div>
-						<div className="twitter-card-preview__description">{ description }</div>
-						<div className="twitter-card-preview__url">{ baseDomain( url ) }</div>
+			<div className="twitter-preview">
+				<div className={ `twitter-preview__${ type }` }>
+					{ image && <div className="twitter-preview__image" style={ previewImageStyle } /> }
+					<div className="twitter-preview__body">
+						<div className="twitter-preview__title">{ title }</div>
+						<div className="twitter-preview__description">{ description }</div>
+						<div className="twitter-preview__url">{ baseDomain( url ) }</div>
 					</div>
 				</div>
 			</div>
