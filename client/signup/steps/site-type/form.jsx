c14ddc334b4b7a57e887f20ff2e194e37a58867e
AnnaMag <anna.mag.kedzierska@gmail.com>
2019-03-26 15:07:54 +0100

    Extend the NUX SiteType component to include an extre free form text option.
    The change is temporary and is meant to serve the purpose of gathering
    user data.

--- client/signup/steps/site-type/form.jsx
+++ client/signup/steps/site-type/form.jsx
@@ -10,6 +10,10 @@ import { localize } from 'i18n-calypso';
  * Internal dependencies
  */
 import Card from 'components/card';
+import Button from 'components/forms/form-button';
+import FormTextInput from 'components/forms/form-text-input';
+import LoggedOutFormLinks from 'components/logged-out-form/links';
+import LoggedOutFormLinkItem from 'components/logged-out-form/link-item';
 import { getAllSiteTypes } from 'lib/signup/site-type';
 import { recordTracksEvent } from 'state/analytics/actions';
 
@@ -20,19 +24,23 @@ import './style.scss';
 
 class SiteTypeForm extends Component {
 	static propTypes = {
-		siteType: PropTypes.string,
 		submitForm: PropTypes.func.isRequired,
 
 		// from localize() HoC
 		translate: PropTypes.func.isRequired,
 	};
 
-	constructor( props ) {
-		super( props );
-		this.state = {
-			siteType: props.siteType,
-		};
-	}
+	state = {
+		otherValue: '',
+		siteType: '',
+		hasOtherReasonFocus: false,
+	};
+
+	onOtherCatChange = event => {
+		this.setState( {
+			otherValue: event.target.value,
+		} );
+	};
 
 	handleSubmit = type => {
 		this.props.recordTracksEvent( 'calypso_signup_actions_submit_site_type', {
@@ -44,24 +52,60 @@ class SiteTypeForm extends Component {
 		this.props.submitForm( type );
 	};
 
-	renderSegmentOptions() {
-		return getAllSiteTypes().map( siteTypeProperties => (
-			<Card
-				className="site-type__option"
-				key={ siteTypeProperties.id }
-				displayAsLink
-				data-e2e-title={ siteTypeProperties.slug }
-				tagName="button"
-				onClick={ this.handleSubmit.bind( this, siteTypeProperties.slug ) }
-			>
-				<strong className="site-type__option-label">{ siteTypeProperties.label }</strong>
-				<span className="site-type__option-description">{ siteTypeProperties.description }</span>
-			</Card>
-		) );
-	}
+	handleSubmitOther = () => {
+		if ( ! this.state.otherValue || this.state.hasOtherReasonFocus ) {
+			return;
+		}
+		this.handleSubmit( 'other—' + this.state.otherValue );
+	};
+
+	setOtherReasonFocus = focus => () => {
+		this.setState( { hasOtherReasonFocus: focus } );
+	};
 
 	render() {
-		return <Card className="site-type__wrapper">{ this.renderSegmentOptions() }</Card>;
+		const { translate } = this.props;
+		return (
+			<div>
+				<Card className="site-type__wrapper">
+					{ getAllSiteTypes().map( siteTypeProperties => (
+						<Card
+							className="site-type__option"
+							key={ siteTypeProperties.id }
+							displayAsLink
+							data-e2e-title={ siteTypeProperties.slug }
+							onClick={ this.handleSubmit.bind( this, siteTypeProperties.slug ) }
+						>
+							<strong className="site-type__option-label">{ siteTypeProperties.label }</strong>
+						</Card>
+					) ) }
+				</Card>
+				<LoggedOutFormLinks className="site-type__links">
+					<LoggedOutFormLinkItem className="site-type__text">
+						Or type your own
+					</LoggedOutFormLinkItem>
+				</LoggedOutFormLinks>
+
+				<div className="site-type__other-link-item-form">
+					<FormTextInput
+						className="site-type__option-other"
+						type="text"
+						selectOnFocus
+						placeholder={ translate( 'Site profile' ) }
+						onChange={ this.onOtherCatChange }
+						value={ this.state.otherValue }
+					/>
+					<Button
+						className="site-type__other-submit"
+						disabled={ false }
+						onClick={ this.handleSubmitOther }
+						compact
+					>
+						{ translate( 'Continue' ) }
+					</Button>
+				</div>
+			</div>
+		);
 	}
 }
 
