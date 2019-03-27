338cb24728148a3889ad1240da46dd8ad535490a
Bernie Reiter <ockham@raz.or.at>
2019-03-27 23:17:37 +0100

    Update notes

--- packages/calypso-build/bin/build-package.js
+++ packages/calypso-build/bin/build-package.js
@@ -8,6 +8,7 @@ const { execSync } = require( 'child_process' );
 
 const dir = process.cwd();
 const root = path.dirname( __dirname );
+// TODO: const babelConfigFile = require.resolve( '@automattic/calypso-build/babel.config.js' );
 const babelConfigFile = path.join( root, '..', '..', 'babel.config.js' );
 
 const inputDir = path.join( dir, 'src' );
@@ -16,13 +17,11 @@ const outputDirCommon = path.join( dir, 'dist', 'cjs' );
 
 console.log( 'Building %s', dir );
 
-// TODO: Drop `--config-file ${ babelConfigFile }` once we've moved `babel.config.js` to `packages/calypso-build`.
 execSync( `npx babel --config-file ${ babelConfigFile } -d ${ outputDirEsm } ${ inputDir }`, {
 	env: Object.assign( {}, process.env, { CALYPSO_CLIENT: 'true' } ),
 	cwd: root,
 } );
 
-// TODO: Drop `--config-file ${ babelConfigFile }` once we've moved `babel.config.js` to `packages/calypso-build`.
 execSync( `npx babel --config-file ${ babelConfigFile } -d ${ outputDirCommon } ${ inputDir }`, {
 	env: Object.assign( {}, process.env, { CALYPSO_CLIENT: 'false' } ),
 	cwd: root,
