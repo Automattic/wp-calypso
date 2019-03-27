c52a89c91709771163c43f70b3e0715e1f4b81c9
James Tien <southp0105@gmail.com>
2019-03-27 12:46:51 +0800

    Add: verticals substate tree (#31716)
    * Add the new `verticals` reducer
    
    * Add `getVerticals` selector.
    
    * Add a test suite for `signup/verticals/selectors`
    
    * Add a test suite for the signup/verticals/reducer

--- client/state/signup/reducer.js
+++ client/state/signup/reducer.js
@@ -9,6 +9,7 @@ import progress from './progress/reducer';
 import optionalDependencies from './optional-dependencies/reducer';
 import steps from './steps/reducer';
 import flow from './flow/reducer';
+import verticals from './verticals/reducer';
 
 export default combineReducers( {
 	dependencyStore,
@@ -16,4 +17,5 @@ export default combineReducers( {
 	progress,
 	steps,
 	flow,
+	verticals,
 } );
