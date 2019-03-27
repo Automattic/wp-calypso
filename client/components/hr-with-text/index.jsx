54456fb6843b1b58f0d44814422b1bd42b7bfa96
Marin Atanasov <tyxla@abv.bg>
2019-03-27 10:56:55 +0200

    Components: Migrate HrWithText CSS to Webpack

--- client/components/hr-with-text/index.jsx
+++ client/components/hr-with-text/index.jsx
@@ -4,6 +4,11 @@
  */
 import React from 'react';
 
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
 const HrWithText = ( { children } ) => (
 	<div className="hr-with-text">
 		<div>{ children }</div>
