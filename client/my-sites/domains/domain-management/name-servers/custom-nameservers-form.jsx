8cff48a5aaf8489965ab0a83b548d354bc7dd7ed
Jarda Snajdr <jsnajdr@gmail.com>
2019-03-28 12:39:44 +0100

    Domains: migrate nameservers UI CSS styles to webpack

--- client/my-sites/domains/domain-management/name-servers/custom-nameservers-form.jsx
+++ client/my-sites/domains/domain-management/name-servers/custom-nameservers-form.jsx
@@ -6,7 +6,6 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import classnames from 'classnames';
 import { dropRightWhile, negate, identity } from 'lodash';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
@@ -14,6 +13,7 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import Card from 'components/card';
 import FormButton from 'components/forms/form-button';
 import FormFooter from 'my-sites/domains/domain-management/components/form-footer';
 import CustomNameserversRow from './custom-nameservers-row';
@@ -63,7 +63,7 @@ class CustomNameserversForm extends React.PureComponent {
 		const { translate } = this.props;
 
 		return (
-			<div className="custom-nameservers-form__explanation">
+			<div className="name-servers__custom-nameservers-form-explanation">
 				{ translate( 'Not sure what name servers to use?' ) }{' '}
 				<a
 					href={ CHANGE_NAME_SERVERS_FINDING_OUT_NEW_NS }
@@ -127,16 +127,13 @@ class CustomNameserversForm extends React.PureComponent {
 
 	render() {
 		const { translate } = this.props;
-		const classes = classnames( 'button is-primary is-full-width', {
-			disabled: this.props.submitDisabled,
-		} );
 
 		if ( ! this.props.nameservers ) {
 			return null;
 		}
 
 		return (
-			<div className="custom-nameservers-form is-compact card">
+			<Card compact className="name-servers__custom-nameservers-form">
 				<span>{ translate( 'Use Custom Name Servers:' ) }</span>
 
 				{ this.warning() }
@@ -147,8 +144,8 @@ class CustomNameserversForm extends React.PureComponent {
 
 					<FormFooter>
 						<FormButton
+							isPrimary
 							onClick={ this.handleSubmit }
-							className={ classes }
 							disabled={ this.props.submitDisabled }
 						>
 							{ translate( 'Save Custom Name Servers' ) }
@@ -159,7 +156,7 @@ class CustomNameserversForm extends React.PureComponent {
 						</FormButton>
 					</FormFooter>
 				</form>
-			</div>
+			</Card>
 		);
 	}
 
