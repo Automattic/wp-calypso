f909774cb17782df04c7cd9e60c3e26c644cc606
Marin Atanasov <tyxla@abv.bg>
2019-03-28 12:35:56 +0200

    Plans: Keep JSX in CurrentPlanThankYouCard in render()

--- client/my-sites/plans/current-plan/current-plan-thank-you-card/index.js
+++ client/my-sites/plans/current-plan/current-plan-thank-you-card/index.js
@@ -22,64 +22,8 @@ export class CurrentPlanThankYouCard extends Component {
 		return `/plans/my-plan/${ siteSlug }`;
 	}
 
-	renderSetup() {
-		const { progressComplete, translate } = this.props;
-
-		return (
-			<Fragment>
-				<img
-					className="current-plan-thank-you-card__illustration"
-					alt=""
-					aria-hidden="true"
-					src="/calypso/images/illustrations/fireworks.svg"
-				/>
-				<h1 className="current-plan-thank-you-card__title">
-					{ translate( 'Thank you for your purchase!' ) }
-				</h1>
-				<p>{ translate( "Now let's make sure your site is protected." ) }</p>
-				<p>
-					{ translate(
-						"We're setting up spam filters and site backups for you first. Once that's done, our security checklist will guide you through the next steps."
-					) }
-				</p>
-
-				<ProgressBar isPulsing total={ 100 } value={ progressComplete || 0 } />
-
-				<p>
-					<a href={ this.getMyPlanRoute() }>{ translate( 'Skip setup. I’ll do this later.' ) }</a>
-				</p>
-			</Fragment>
-		);
-	}
-
-	renderSuccess() {
-		const { translate } = this.props;
-
-		return (
-			<Fragment>
-				<img
-					className="current-plan-thank-you-card__illustration"
-					alt=""
-					aria-hidden="true"
-					src="/calypso/images/illustrations/security.svg"
-				/>
-				<h1 className="current-plan-thank-you-card__title">
-					{ translate( 'So long spam, hello backups!' ) }
-				</h1>
-				<p>
-					{ translate( 'We’ve finished setting up spam filtering and backups for you.' ) }
-					<br />
-					{ translate( "You're now ready to finish the rest of the checklist." ) }
-				</p>
-				<Button primary href={ this.getMyPlanRoute() }>
-					{ translate( 'Continue' ) }
-				</Button>
-			</Fragment>
-		);
-	}
-
 	render() {
-		const { progressComplete } = this.props;
+		const { progressComplete, translate } = this.props;
 
 		return (
 			<Fragment>
@@ -88,7 +32,53 @@ export class CurrentPlanThankYouCard extends Component {
 				{ progressComplete !== null && (
 					<Card className="current-plan-thank-you-card__main">
 						<div className="current-plan-thank-you-card__content">
-							{ progressComplete === 100 ? this.renderSuccess() : this.renderSetup() }
+							{ progressComplete === 100 ? (
+								<Fragment>
+									<img
+										className="current-plan-thank-you-card__illustration"
+										alt=""
+										aria-hidden="true"
+										src="/calypso/images/illustrations/security.svg"
+									/>
+									<h1 className="current-plan-thank-you-card__title">
+										{ translate( 'So long spam, hello backups!' ) }
+									</h1>
+									<p>
+										{ translate( 'We’ve finished setting up spam filtering and backups for you.' ) }
+										<br />
+										{ translate( "You're now ready to finish the rest of the checklist." ) }
+									</p>
+									<Button primary href={ this.getMyPlanRoute() }>
+										{ translate( 'Continue' ) }
+									</Button>
+								</Fragment>
+							) : (
+								<Fragment>
+									<img
+										className="current-plan-thank-you-card__illustration"
+										alt=""
+										aria-hidden="true"
+										src="/calypso/images/illustrations/fireworks.svg"
+									/>
+									<h1 className="current-plan-thank-you-card__title">
+										{ translate( 'Thank you for your purchase!' ) }
+									</h1>
+									<p>{ translate( "Now let's make sure your site is protected." ) }</p>
+									<p>
+										{ translate(
+											"We're setting up spam filters and site backups for you first. Once that's done, our security checklist will guide you through the next steps."
+										) }
+									</p>
+
+									<ProgressBar isPulsing total={ 100 } value={ progressComplete || 0 } />
+
+									<p>
+										<a href={ this.getMyPlanRoute() }>
+											{ translate( 'Skip setup. I’ll do this later.' ) }
+										</a>
+									</p>
+								</Fragment>
+							) }
 						</div>
 					</Card>
 				) }
