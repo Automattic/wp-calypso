7bea25ce8afbc6c7e3667e5fcc9d2a203387ae5e
Marin Atanasov <tyxla@abv.bg>
2019-03-28 12:33:14 +0200

    Plans: Shorten CurrentPlanThankYouCard copy

--- client/my-sites/plans/current-plan/current-plan-thank-you-card/index.js
+++ client/my-sites/plans/current-plan/current-plan-thank-you-card/index.js
@@ -3,7 +3,6 @@
  */
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { get, invoke } from 'lodash';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -11,14 +10,10 @@ import { localize } from 'i18n-calypso';
  */
 import Button from 'components/button';
 import Card from 'components/card';
-import getCurrentPlanPurchase from 'state/selectors/get-current-plan-purchase';
 import getJetpackProductInstallProgress from 'state/selectors/get-jetpack-product-install-progress';
 import JetpackProductInstall from 'my-sites/plans/current-plan/jetpack-product-install';
 import ProgressBar from 'components/progress-bar';
-import QuerySitePurchases from 'components/data/query-site-purchases';
-import { getPlan } from 'lib/plans';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
-import { getSitePlanSlug } from 'state/sites/selectors';
 
 export class CurrentPlanThankYouCard extends Component {
 	getMyPlanRoute() {
@@ -28,10 +23,7 @@ export class CurrentPlanThankYouCard extends Component {
 	}
 
 	renderSetup() {
-		const { moment, planName, progressComplete, purchaseExpiryDate, translate } = this.props;
-		const duration = purchaseExpiryDate
-			? moment.duration( moment().diff( purchaseExpiryDate ) ).humanize()
-			: null;
+		const { progressComplete, translate } = this.props;
 
 		return (
 			<Fragment>
@@ -44,19 +36,10 @@ export class CurrentPlanThankYouCard extends Component {
 				<h1 className="current-plan-thank-you-card__title">
 					{ translate( 'Thank you for your purchase!' ) }
 				</h1>
-				<p>
-					{ duration &&
-						planName &&
-						translate(
-							'Your website is on a %(planName)s plan for %(duration)s. That means it has lots of useful security tools — let’s walk through a short checklist of the essentials so Jetpack can start monitoring things for you.',
-							{
-								args: { duration, planName },
-							}
-						) }
-				</p>
+				<p>{ translate( "Now let's make sure your site is protected." ) }</p>
 				<p>
 					{ translate(
-						'We’ve taken the liberty of starting the first two items, since they’re key to your site’s safety: we’re configuring spam filtering and backups for you now. Once that’s done, we can work through the rest of the checklist.'
+						"We're setting up spam filters and site backups for you first. Once that's done, our security checklist will guide you through the next steps."
 					) }
 				</p>
 
@@ -96,11 +79,10 @@ export class CurrentPlanThankYouCard extends Component {
 	}
 
 	render() {
-		const { progressComplete, siteId } = this.props;
+		const { progressComplete } = this.props;
 
 		return (
 			<Fragment>
-				<QuerySitePurchases siteId={ siteId } />
 				<JetpackProductInstall />
 
 				{ progressComplete !== null && (
@@ -117,12 +99,9 @@ export class CurrentPlanThankYouCard extends Component {
 
 export default connect( state => {
 	const siteId = getSelectedSiteId( state );
-	const planSlug = getSitePlanSlug( state, siteId );
 
 	return {
-		planName: invoke( getPlan( planSlug ), [ 'getTitle' ] ),
 		progressComplete: getJetpackProductInstallProgress( state, siteId ),
-		purchaseExpiryDate: get( getCurrentPlanPurchase( state, siteId ), [ 'expiryDate' ] ),
 		siteId,
 		siteSlug: getSelectedSiteSlug( state ),
 	};
