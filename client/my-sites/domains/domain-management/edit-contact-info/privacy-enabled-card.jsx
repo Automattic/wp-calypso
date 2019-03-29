c230f76d9f4856afc0c3b09f6e04e55515510c95
Andrija Vucinic <andrija.vucinic@gmail.com>
2019-03-29 21:17:25 +0100

    Domains: Fix bad rebase. Switch to functional component.
    Broke the component with a bad rebase. Not sure if functional
    components are preferred, but I like the hooks. :)

--- client/my-sites/domains/domain-management/edit-contact-info/privacy-enabled-card.jsx
+++ client/my-sites/domains/domain-management/edit-contact-info/privacy-enabled-card.jsx
@@ -18,35 +18,34 @@ import PropTypes from 'prop-types';
  */
 import './privacy-enabled-card.scss';
 
-export default class EditContactInfoPrivacyEnabledCard extends React.PureComponent {
-	static propTypes = {
-		selectedDomainName: PropTypes.string.isRequired,
-		selectedSite: PropTypes.oneOfType( [ PropTypes.object, PropTypes.bool ] ).isRequired,
-	};
+function EditContactInfoPrivacyEnabledCard( { selectedDomainName, selectedSiteSlug } ) {
+	const translate = useTranslate();
 
-	render() {
-		const { selectedDomainName, selectedSite } = this.props;
-		const translate = useTranslate();
-
-		return (
-			<Card className="edit-contact-info__privacy-enabled-card">
-				<p className="edit-contact-info__privacy-enabled-card-settings-explanation">
-					{ translate(
-						'This domain is currently using Privacy Protection to keep your information from showing up in public record searches. ' +
-							"If you need to make a change to your domain's contact info, please {{a}}turn privacy off{{/a}} first.",
-						{
-							components: {
-								a: (
-									<a
-										href={ domainManagementEdit( selectedSite.slug, selectedDomainName ) }
-										rel="noopener noreferrer"
-									/>
-								),
-							},
-						}
-					) }
-				</p>
-			</Card>
-		);
-	}
+	return (
+		<Card className="edit-contact-info__privacy-enabled-card">
+			<p className="edit-contact-info__privacy-enabled-card-settings-explanation">
+				{ translate(
+					'This domain is currently using Privacy Protection to keep your information from showing up in public record searches. ' +
+						"If you need to make a change to your domain's contact info, please {{a}}turn privacy off{{/a}} first.",
+					{
+						components: {
+							a: (
+								<a
+									href={ domainManagementEdit( selectedSiteSlug, selectedDomainName ) }
+									rel="noopener noreferrer"
+								/>
+							),
+						},
+					}
+				) }
+			</p>
+		</Card>
+	);
 }
+
+EditContactInfoPrivacyEnabledCard.propTypes = {
+	selectedDomainName: PropTypes.string.isRequired,
+	selectedSiteSlug: PropTypes.string.isRequired,
+};
+
+export default EditContactInfoPrivacyEnabledCard;
