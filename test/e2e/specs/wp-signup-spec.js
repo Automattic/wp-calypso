b50557a71f18f9abfc67d829b9173474131eceb1
Brent Sessions <brent.sessions@automattic.com>
2019-04-01 08:46:56 -0600

    just a change

--- test/e2e/specs/wp-signup-spec.js
+++ test/e2e/specs/wp-signup-spec.js
@@ -1726,7 +1726,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		} );
 	} );
 
-	describe( 'Sign up for an account only (no site) then add a site via new onboarding flow @parallel', () => {
+	xdescribe( 'Sign up for an account only (no site) then add a site via new onboarding flow @parallel', () => {
 		const userName = dataHelper.getNewBlogName();
 		const blogName = dataHelper.getNewBlogName();
 
