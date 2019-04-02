3bf806c828b802603d09d557899acfdfc5dc8b63
Arun Sathiya <arun@sathiya.me>
2019-04-02 15:36:14 +0000

    Migrate ListEnd styles to JS imports (#31887)
    * Migrate ListEnd styles to JS imports
    
    * Move Reader-specific styles to Reader stylesheet
    
    Co-authored-by: Ben Lowery <blowery@users.noreply.github.com>

--- client/components/list-end/index.js
+++ client/components/list-end/index.js
@@ -7,6 +7,11 @@
 import React from 'react';
 import Gridicon from 'gridicons';
 
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
 export default function ListEnd() {
 	return (
 		<div className="list-end">
