9cab7af78888c61b2b5bb1afdaabbe876a2fdc2f
Bernie Reiter <ockham@raz.or.at>
2019-04-02 14:27:16 +0200

    Fix JSDoc

--- packages/calypso-build/webpack.config.js
+++ packages/calypso-build/webpack.config.js
@@ -28,7 +28,19 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 /**
  * Return a webpack config object
  *
- * @return {object} webpack config
+ * Arguments to this function replicate webpack's so this config can be used on the command line,
+ * with individual options overridden by command line args.
+ *
+ * @see {@link https://webpack.js.org/configuration/configuration-types/#exporting-a-function}
+ * @see {@link https://webpack.js.org/api/cli/}
+ *
+ * @param  {object}  env                           environment options
+ * @param  {object}  argv                          options map
+ * @param  {object}  argv.entry                    Entry point(s)
+ * @param  {string}  argv.'output-path'            Output path
+ * @param  {string}  argv.'output-filename'        Output filename pattern
+ * @param  {string}  argv.'output-library-target'  Output library target
+ * @return {object}                                webpack config
  */
 function getWebpackConfig(
 	env,
