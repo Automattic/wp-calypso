81291820eea8ed7402a128b3096ec5fb3e94ceba
Matt Wiebe <wiebe@automattic.com>
2019-04-02 17:28:55 -0500

    enable page builder mvp test

--- client/lib/signup/page-builder.js
+++ client/lib/signup/page-builder.js
@@ -1,24 +1,15 @@
 /**
  * Internal dependencies
  */
-import config from 'config';
+import { abtest, getABTestVariation } from 'lib/abtest';
 import { getLocaleSlug } from 'lib/i18n-utils';
 
-// temp
-let inTest = false;
-
 export function isInPageBuilderTest() {
-	// This will check an already-set abtest value with
-	// `getABTestVariation` when ready to launch
-	return inTest;
+	return 'test' === getABTestVariation( 'pageBuilderMVP' );
 }
 
 export function shouldEnterPageBuilder() {
-	// This will become an abtest when we are ready to launch
-	inTest = config.isEnabled( 'signup/page-builder' );
-	// using `inTest` this way ensures that `isInPageBuilderTest` will only
-	// return true after this, like assigning the test and later checking `getABTestVariation`
-	return inTest;
+	return 'test' === abtest( 'pageBuilderMVP' );
 }
 
 export function isEligibleForPageBuilder( segment, flowName ) {
