15292a3c4b7bbdd32d63f9d7f81bfd13f8eab2dd
Matt Wiebe <matt@somadesign.ca>
2019-04-03 15:56:58 -0500

    Enable page builder mvp test (#31981)

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -174,4 +174,12 @@ export default {
 		},
 		defaultVariation: 'original',
 	},
+	pageBuilderMVP: {
+		datestamp: '20190402',
+		variations: {
+			control: 50,
+			test: 50,
+		},
+		defaultVariation: 'control',
+	},
 };
