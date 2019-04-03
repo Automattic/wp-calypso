d5d15cebbe17dedf28b470fd36709f4b94f145df
Marin Atanasov <tyxla@abv.bg>
2019-04-03 12:43:53 +0300

    Devdocs: Update JetpackHeader example to support dark color scheme

--- client/components/jetpack-header/docs/example.js
+++ client/components/jetpack-header/docs/example.js
@@ -1,18 +1,16 @@
-/** @format */
-
 /**
  * External dependencies
  */
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import JetpackHeader from '..';
+import { getPreference } from 'state/preferences/selectors';
 
-export default class JetpackHeaderExample extends PureComponent {
-	static displayName = 'JetpackHeaderExample';
-
+class JetpackHeaderExample extends PureComponent {
 	static defaultProps = {
 		exampleCode: (
 			<div>
@@ -28,6 +26,26 @@ export default class JetpackHeaderExample extends PureComponent {
 	};
 
 	render() {
-		return this.props.exampleCode;
+		const darkColorScheme = this.props.colorSchemePreference === 'laser-black';
+
+		return (
+			<div>
+				<JetpackHeader />
+				<JetpackHeader partnerSlug="dreamhost" darkColorScheme={ darkColorScheme } />
+				<JetpackHeader partnerSlug="pressable" darkColorScheme={ darkColorScheme } />
+				<JetpackHeader partnerSlug="milesweb" darkColorScheme={ darkColorScheme } />
+				<JetpackHeader partnerSlug="bluehost" darkColorScheme={ darkColorScheme } />
+				<JetpackHeader partnerSlug="inmotion" darkColorScheme={ darkColorScheme } />
+				<JetpackHeader partnerSlug="liquidweb" darkColorScheme={ darkColorScheme } />
+			</div>
+		);
 	}
 }
+
+const ConnectedJetpackHeaderExample = connect( state => ( {
+	colorSchemePreference: getPreference( state, 'colorScheme' ),
+} ) )( JetpackHeaderExample );
+
+ConnectedJetpackHeaderExample.displayName = 'JetpackHeaderExample';
+
+export default ConnectedJetpackHeaderExample;
