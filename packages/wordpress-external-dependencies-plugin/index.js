fab524152fda6994a4be755a61c91de389d7d857
Jon Surrell <jon.surrell@automattic.com>
2019-04-04 09:50:09 +0200

    Rework plugin
    - No static deps mapping
    - Better deps file generation

--- packages/wordpress-external-dependencies-plugin/index.js
+++ packages/wordpress-external-dependencies-plugin/index.js
@@ -1,90 +1,115 @@
 /**
  * External dependencies
  */
-const { createHash } = require( 'crypto' );
 const { ExternalsPlugin } = require( 'webpack' );
 const { RawSource } = require( 'webpack-sources' );
+const { camelCaseDash } = require( '@wordpress/scripts/utils' );
+const { createHash } = require( 'crypto' );
 
-/**
- * Internal dependencies
- */
-const wpModules = require( './wp-modules' );
+const WORDPRESS_NAMESPACE = '@wordpress/';
 
 class WordPressExternalDependenciesPlugin {
 	constructor() {
-		this.externalsPlugin = new ExternalsPlugin( 'global', [ wpExternals ] );
+		this.externalizedDeps = new Set();
+		this.externalsPlugin = new ExternalsPlugin( 'global', [ this.externalizeWpDeps.bind( this ) ] );
+	}
 
-		this.entryDeps = new Map();
+	externalizeWpDeps( context, request, callback ) {
+		let externRootRequest;
+
+		switch ( request ) {
+			case 'lodash':
+			case 'moment':
+				externRootRequest = request;
+				break;
+
+			case 'jquery':
+				externRootRequest = 'jQuery';
+				break;
+
+			case 'react':
+				externRootRequest = 'React';
+				break;
+			case 'react-dom':
+				externRootRequest = 'ReactDOM';
+				break;
+
+			default:
+				if ( request.startsWith( WORDPRESS_NAMESPACE ) ) {
+					// @wordpress/api-fetch -> wp.apiFetch
+					// @wordpress/i18n -> wp.i18n
+					externRootRequest = `wp.${ camelCaseDash(
+						request.substring( WORDPRESS_NAMESPACE.length )
+					) }`;
+				}
+				break;
+		}
+
+		if ( externRootRequest ) {
+			this.externalizedDeps.add( request );
+			return callback( null, `root ${ externRootRequest }` );
+		}
+
+		return callback();
 	}
 
 	apply( compiler ) {
 		this.externalsPlugin.apply( compiler );
 
-		compiler.hooks.emit.tapAsync( this.constructor.name, ( compilation, callback ) => {
-			for ( const [ , entrypoint ] of compilation.entrypoints.entries() ) {
-				// labelled loop to continue when nested dep is found
-				dependencyIteration: for ( const [ wpModule, { dependency } ] of wpModules.entries() ) {
-					for ( const chunk of entrypoint.chunks ) {
-						for ( const chunkModule of chunk.modulesIterable ) {
-							if ( chunkModule.userRequest === wpModule ) {
-								this.entryDeps.has( entrypoint )
-									? this.entryDeps.get( entrypoint ).add( dependency )
-									: this.entryDeps.set( entrypoint, new Set( [ dependency ] ) );
-								continue dependencyIteration;
-							}
+		const { output } = compiler.options;
+		const { /*path: outputPath,*/ filename: outputFilename } = output;
+
+		compiler.hooks.emit.tap( this.constructor.name, compilation => {
+			// const stats = compilation.getStats();
+			for ( const [ entrypointName, entrypoint ] of compilation.entrypoints.entries() ) {
+				const entrypointExternalizedWpDeps = new Set();
+
+				for ( const c of entrypoint.chunks ) {
+					for ( const chunkModule of c.modulesIterable ) {
+						const { userRequest } = chunkModule;
+
+						if ( this.externalizedDeps.has( userRequest ) ) {
+							// Pass externalized deps
+							// Transform @wordpress deps:
+							//   @wordpress/i18n -> wp-i18n
+							//   @wordpress/escape-html -> wp-escape-html
+							entrypointExternalizedWpDeps.add(
+								userRequest.startsWith( WORDPRESS_NAMESPACE )
+									? 'wp-' + userRequest.substring( WORDPRESS_NAMESPACE.length )
+									: userRequest
+							);
 						}
 					}
 				}
+
+				const depsArray = Array.from( entrypointExternalizedWpDeps );
+				depsArray.sort();
+
+				const depsString = JSON.stringify( depsArray );
+
+				const [ filename, query ] = entrypointName.split( '?', 2 );
+				const depsFile = compilation.getPath( outputFilename.replace( /\.js$/i, '.deps.json' ), {
+					chunk: entrypoint.getRuntimeChunk(),
+					filename,
+					query,
+					basename: basename( filename ),
+					contentHash: createHash( 'md4' )
+						.update( depsString )
+						.digest( 'hex' ),
+				} );
+
+				compilation.assets[ depsFile ] = new RawSource( depsString );
+				entrypoint.getRuntimeChunk().files.push( depsFile );
 			}
-
-			const filename = compilation.outputOptions.filename.replace( /.js$/, '.deps.json' );
-			for ( const [ entry, deps ] of this.entryDeps.entries() ) {
-				const stableDeps = Array.from( deps );
-				stableDeps.sort();
-				const chunkId = `depschunk_${ entry.id }`;
-				const moduleId = `depsmodule_${ entry.id }`;
-
-				// Act like a chunk or module for paths
-				const data = {
-					contentHashType: 'json',
-					chunk: {
-						entryModule: entry.getRuntimeChunk().entryModule,
-						id: chunkId,
-						hash: createHash( 'sha256' )
-							.update( chunkId )
-							.digest( 'hex' ),
-						contentHash: {
-							json: createHash( 'sha256' )
-								.update( JSON.stringify( stableDeps ) )
-								.digest( 'hex' ),
-						},
-					},
-					module: {
-						id: moduleId,
-						hash: createHash( 'sha256' )
-							.update( moduleId )
-							.digest( 'hex' ),
-					},
-				};
-				data.chunk.hashWithLength = length => data.chunk.hash.substr( 0, length );
-				data.chunk.contentHashWithLength = length =>
-					data.chunk.contentHash[ data.chunk.contentHashType ].substr( 0, length );
-				data.module.hashWithLength = length => data.module.hash.substr( 0, length );
-
-				const assetPath = compilation.getPath( filename, data );
-				compilation.assets[ assetPath ] = new RawSource( JSON.stringify( Array.from( deps ) ) );
-			}
-
-			callback();
 		} );
 	}
 }
 
-function wpExternals( context, request, callback ) {
-	if ( wpModules.has( request ) ) {
-		return callback( null, `root ${ wpModules.get( request ).globalName }` );
+function basename( name ) {
+	if ( ! name.includes( '/' ) ) {
+		return name;
 	}
-	return callback();
+	return name.substr( name.lastIndexOf( '/' ) + 1 );
 }
 
 module.exports = WordPressExternalDependenciesPlugin;
