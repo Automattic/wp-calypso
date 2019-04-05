e42acd12fe14322ea56275f465a5c552405082f7
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2019-04-05 13:26:16 +0300

    Jetpack Checklist: Introduce header component (#32027)

---
+++ client/my-sites/plans/current-plan/jetpack-checklist/header.js
@@ -0,0 +1,32 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Card from 'components/card';
+import CardHeading from 'components/card-heading';
+
+const JetpackChecklistHeader = ( { translate } ) => (
+	<Card compact className="jetpack-checklist__header">
+		<img
+			className="jetpack-checklist__header-illustration"
+			alt=""
+			aria-hidden="true"
+			src="/calypso/images/illustrations/security.svg"
+		/>
+		<div className="jetpack-checklist__header-content">
+			<CardHeading>
+				{ translate( "Let's start by securing your site with a few essential security features" ) }
+			</CardHeading>
+			<p>
+				{ translate( 'These security features ensure that your site is secured and backed up.' ) }
+			</p>
+		</div>
+	</Card>
+);
+
+export default localize( JetpackChecklistHeader );
