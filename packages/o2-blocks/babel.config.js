5cb13f06c7d723f95552a987a9eaaeb02d39b8b0
Bernie Reiter <ockham@raz.or.at>
2019-04-05 14:04:36 +0200

    Remove some args from @wordpress/babel-plugin-makepot since defaults are fine here

--- packages/o2-blocks/babel.config.js
+++ packages/o2-blocks/babel.config.js
@@ -3,17 +3,7 @@ module.exports = {
 	presets: [ require( '@automattic/calypso-build/babel/wordpress-element' ) ],
 	env: {
 		build_pot: {
-			plugins: [
-				[
-					'@wordpress/babel-plugin-makepot',
-					{
-						headers: {
-							'content-type': 'text/plain; charset=UTF-8',
-							'x-generator': 'calypso',
-						},
-					},
-				],
-			],
+			plugins: [ '@wordpress/babel-plugin-makepot' ],
 		},
 	},
 };
