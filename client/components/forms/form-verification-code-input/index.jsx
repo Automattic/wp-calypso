2a077a4fa106914344d76529c622837335ea4b0d
torres126 <torres126@clubpenguinmountains.com>
2019-04-06 23:21:46 +0100

    Display Six Digits in Placeholder

--- client/components/forms/form-verification-code-input/index.jsx
+++ client/components/forms/form-verification-code-input/index.jsx
@@ -46,7 +46,7 @@ export default class FormVerificationCodeInput extends React.Component {
 		if ( method === 'backup' ) {
 			placeholder = constants.eightDigitBackupCodePlaceholder;
 		} else if ( method === 'sms' ) {
-			placeholder = constants.sevenDigit2faPlaceholder;
+			placeholder = constants.sixDigit2faPlaceholder;
 		}
 
 		return (
