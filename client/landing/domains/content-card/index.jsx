45d4b0d3d92f6b02446c86f70593f5ff2b282066
Del Putnam <del@putnams.net>
2019-04-07 11:59:15 -0400

    Set required and default props on DomainsLandingContentCard

--- client/landing/domains/content-card/index.jsx
+++ client/landing/domains/content-card/index.jsx
@@ -19,12 +19,16 @@ import './style.scss';
 class DomainsLandingContentCard extends Component {
 	static propTypes = {
 		illustration: PropTypes.string,
-		title: PropTypes.oneOfType( [ PropTypes.string, PropTypes.node ] ),
+		title: PropTypes.oneOfType( [ PropTypes.string, PropTypes.node ] ).isRequired,
 		message: PropTypes.oneOfType( [ PropTypes.string, PropTypes.node ] ),
 		actionTitle: PropTypes.oneOfType( [ PropTypes.string, PropTypes.node ] ),
 		actionCallback: PropTypes.func,
 		footer: PropTypes.oneOfType( [ PropTypes.string, PropTypes.node ] ),
 		isLoading: PropTypes.bool,
+	};
+
+	static defaultProps = {
+		isLoading: false,
 	};
 
 	renderPlaceholder = () => {
@@ -59,7 +63,7 @@ class DomainsLandingContentCard extends Component {
 		return (
 			<CompactCard className="content-card">
 				{ illustration && <img src={ illustration } alt="" /> }
-				{ title && <h2 className="content-card__title">{ title }</h2> }
+				{ <h2 className="content-card__title">{ title }</h2> }
 				{ message && <h3 className="content-card__message">{ message }</h3> }
 				{ actionTitle && (
 					<Button className="content-card__action-button" primary onClick={ actionCallback }>
