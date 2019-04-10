a8f7634de3712817346d92b147d9904d0f7ba271
Rares Crisan <rarescrisan@Raress-MacBook-Pro.local>
2019-04-10 15:03:55 -0400

    add explore and editor renders

---
+++ client/my-sites/looka/editor/index.jsx
@@ -0,0 +1,21 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+
+export default class Editor extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Editor</h1>
+			</div>
+		);
+	}
+}
---
+++ client/my-sites/looka/explore/index.jsx
@@ -0,0 +1,21 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+
+export default class Explore extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Explore</h1>
+			</div>
+		);
+	}
+}
