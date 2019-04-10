7be9a1e8535e6f5f2679f05dea5fe3d05681e0d7
mmtr <miguel.torres@automattic.com>
2019-04-10 14:38:54 +0900

    Rename package to `@automattic/wpcom-block-editor`

--- apps/wpcom-block-editor/src/iframe-bridge-server.js
+++ apps/wpcom-block-editor/src/iframe-bridge-server.js
@@ -74,8 +74,11 @@ function overrideRevisions( calypsoPort ) {
 		const payload = get( message, 'data.payload', null );
 
 		if ( action === 'loadRevision' && payload ) {
-			const blocks = parse( payload.content );
-			dispatch( 'core/editor' ).editPost( payload );
+			const currentPost = select( 'core/editor' ).getCurrentPost();
+			const postRevision = { ...currentPost, ...payload };
+			const blocks = parse( postRevision.content );
+
+			dispatch( 'core/editor' ).updatePost( postRevision );
 			dispatch( 'core/editor' ).resetBlocks( blocks );
 
 			calypsoPort.removeEventListener( 'message', onLoadRevision, false );
