677ea9705979d909e4b239a4fa675ff00c907707
Bernie Reiter <ockham@raz.or.at>
2019-04-11 14:01:04 +0200

    Use cssNameFromFilename helper

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ const Minify = require( '@automattic/calypso-build/webpack/minify' );
 const SassConfig = require( '@automattic/calypso-build/webpack/sass' );
 const TranspileConfig = require( '@automattic/calypso-build/webpack/transpile' );
 const WordPressExternalDependenciesPlugin = require( '@automattic/wordpress-external-dependencies-plugin' );
+const { cssNameFromFilename } = require( './webpack/util' );
 
 /**
  * Internal dependencies
@@ -162,8 +163,8 @@ function getWebpackConfig( {
 		outputChunkFilename = '[name].js';
 	}
 
-	const cssFilename = path.parse( outputFilename ).name + '.css';
-	const cssChunkFilename = path.parse( outputChunkFilename ).name + '.css';
+	const cssFilename = cssNameFromFilename( outputFilename );
+	const cssChunkFilename = cssNameFromFilename( outputChunkFilename );
 
 	const webpackConfig = {
 		bail: ! isDevelopment,
