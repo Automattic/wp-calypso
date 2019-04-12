4b986e8fee92b6d1689a5ae4866a80b2455f6ad8
AnnaMag <AnnaMag@users.noreply.github.com>
2019-04-12 16:06:58 +0200

    NUX: extend the NUX SiteType UI component to include a free form text option. (#32125)
    * Extend the NUX SiteType component to include an extre free form text option.
    
    The change is temporary and is meant to serve the purpose of gathering
    user data.
    
    * Restrict the Other section to be used with Jetpack sites only.
    
    * Include missing translate on the user facing text.
    
    * Moving a few things around and cleaning up the design. (first draft)
    
    * More tweaks.
    
    * Add feedback from reviews: cosmetics.
    
    * remove obsolete state property
    
    * Revise the return value for the other field, using other– as a data separator.
    
    This is included for easier data processing.
    
    * Add sending missing tracks for user input events.
    
    * Add review feedback:
    - remove double track firing
    - edit to the render logic
    
    * Add condtiton to remove hyphen from the defined categories.

--- client/signup/steps/site-type/form.jsx
+++ client/signup/steps/site-type/form.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
@@ -9,8 +9,12 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import Button from 'components/forms/form-button';
 import Card from 'components/card';
+import FormTextInput from 'components/forms/form-text-input';
 import { getAllSiteTypes } from 'lib/signup/site-type';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { isJetpackSite } from 'state/sites/selectors';
 import { recordTracksEvent } from 'state/analytics/actions';
 
 /**
@@ -30,43 +34,96 @@ class SiteTypeForm extends Component {
 	constructor( props ) {
 		super( props );
 		this.state = {
+			otherValue: '',
 			siteType: props.siteType,
 		};
 	}
 
+	onOtherCatChange = event => {
+		this.setState( {
+			otherValue: event.target.value,
+		} );
+	};
+
 	handleSubmit = type => {
 		this.props.recordTracksEvent( 'calypso_signup_actions_submit_site_type', {
 			value: type,
+			user_input_site_type: this.state.otherValue || null,
 		} );
 
 		this.setState( { siteType: type } );
 
-		this.props.submitForm( type );
+		this.props.submitForm( type === 'other' ? type + '-' + this.state.otherValue : type );
 	};
 
-	renderSegmentOptions() {
-		return getAllSiteTypes().map( siteTypeProperties => (
-			<Card
-				className="site-type__option"
-				key={ siteTypeProperties.id }
-				displayAsLink
-				data-e2e-title={ siteTypeProperties.slug }
-				tagName="button"
-				onClick={ this.handleSubmit.bind( this, siteTypeProperties.slug ) }
-			>
-				<strong className="site-type__option-label">{ siteTypeProperties.label }</strong>
-				<span className="site-type__option-description">{ siteTypeProperties.description }</span>
+	handleSubmitOther = () => this.handleSubmit( 'other' );
+
+	renderBasicCard = () => {
+		return (
+			<Card className="site-type__wrapper">
+				{ getAllSiteTypes().map( siteTypeProperties => (
+					<Card
+						className="site-type__option"
+						key={ siteTypeProperties.id }
+						tagName="button"
+						displayAsLink
+						data-e2e-title={ siteTypeProperties.slug }
+						onClick={ this.handleSubmit.bind( this, siteTypeProperties.slug ) }
+					>
+						<strong className="site-type__option-label">{ siteTypeProperties.label }</strong>
+						<span className="site-type__option-description">
+							{ siteTypeProperties.description }
+						</span>
+					</Card>
+				) ) }
 			</Card>
-		) );
-	}
+		);
+	};
+
+	renderOtherInfo = () => {
+		const { translate } = this.props;
+
+		return (
+			<div className="site-type__other-option">
+				<p className="site-type__other-label">{ translate( 'Or type your own' ) }</p>
+
+				<div className="site-type__other-form">
+					<FormTextInput
+						className="site-type__other-input"
+						selectOnFocus
+						placeholder={ translate( 'Other' ) }
+						onChange={ this.onOtherCatChange }
+						value={ this.state.otherValue }
+					/>
+
+					<Button
+						className="site-type__other-submit"
+						disabled={ false }
+						onClick={ this.handleSubmitOther }
+					>
+						{ translate( 'Continue' ) }
+					</Button>
+				</div>
+			</div>
+		);
+	};
 
 	render() {
-		return <Card className="site-type__wrapper">{ this.renderSegmentOptions() }</Card>;
+		const { isJetpack } = this.props;
+
+		return (
+			<Fragment>
+				{ this.renderBasicCard() }
+				{ isJetpack && this.renderOtherInfo() }
+			</Fragment>
+		);
 	}
 }
 
 export default connect(
-	null,
+	state => ( {
+		isJetpack: isJetpackSite( state, getSelectedSiteId( state ) ),
+	} ),
 	{
 		recordTracksEvent,
 	}
