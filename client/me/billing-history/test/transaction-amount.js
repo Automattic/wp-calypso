46dc90a5c5a525203c39c3db20252a8887602da6
Louis Laugesen <louis.laugesen@gmail.com>
2019-04-15 14:31:32 +1000

    scaffold

--- client/me/billing-history/test/transaction-amount.js
+++ client/me/billing-history/test/transaction-amount.js
@@ -10,11 +10,24 @@ import { mount } from 'enzyme';
 /**
  * Internal dependencies
  */
-import { TransactionAmount } from '../transaction-amount';
+import TransactionAmount from '../transaction-amount';
 
-const translate = x => x;
+// const translate = x => x;
 
 describe( 'TransactionAmount', () => {
+	const transaction = {
+		subtotal: '$36.00',
+		tax: '$2.48',
+		amount: '$38.48',
+		items: [
+			{
+				raw_tax: 2.48,
+			},
+		],
+	};
+
+	const upcoming = { amount: '€38.48' };
+
 	test( 'amount', () => {} );
 	test( 'tax exempt', () => {} );
 	test( 'tax inclusive', () => {} );
