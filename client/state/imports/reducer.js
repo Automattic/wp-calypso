52a3f666ed70d5b29d6bf093a09d3812cb1f19ce
Jeff Bowen <jeff@automattic.com>
2019-04-16 09:39:10 -0400

    clear state when a site is selected

--- client/state/imports/reducer.js
+++ client/state/imports/reducer.js
@@ -5,9 +5,10 @@
 import { combineReducers, createReducer } from 'state/utils';
 import uploadsReducer from 'state/imports/uploads/reducer';
 import siteImporterReducer from 'state/imports/site-importer/reducer';
-import { IMPORTS_FETCH_COMPLETED } from 'state/action-types';
+import { IMPORTS_FETCH_COMPLETED, SELECTED_SITE_SET } from 'state/action-types';
 
 const isHydrated = createReducer( false, {
+	[ SELECTED_SITE_SET ]: () => false,
 	[ IMPORTS_FETCH_COMPLETED ]: () => true,
 } );
 
