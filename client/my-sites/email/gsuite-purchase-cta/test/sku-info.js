c4ca717636991504c15e64bfa71a7850b897aa93
Caleb Bauermeister <cbauerman@gmail.com>
2019-04-16 16:48:12 -0700

    Update Prices

--- client/my-sites/email/gsuite-purchase-cta/test/sku-info.js
+++ client/my-sites/email/gsuite-purchase-cta/test/sku-info.js
@@ -2,7 +2,6 @@
 /**
  * External dependencies
  */
-import { noop } from 'lodash';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
@@ -15,21 +14,10 @@ jest.mock( 'components/forms/form-button', () => 'Button' );
 jest.mock( 'components/info-popover', () => 'InfoPopover' );
 
 describe( 'GSuitePurchaseCta', () => {
-	test( 'it renders GSuitePurchaseCtaSkuInfo with no props', () => {
-		const tree = renderer.create( <GSuitePurchaseCtaSkuInfo /> ).toJSON();
-		expect( tree ).toMatchSnapshot();
-	} );
-
 	test( 'it renders GSuitePurchaseCtaSkuInfo with all props', () => {
 		const tree = renderer
 			.create(
 				<GSuitePurchaseCtaSkuInfo
-					buttonText={ 'Add G Suite Business' }
-					currencyCode={ 'USD' }
-					onButtonClick={ noop }
-					showButton={ true }
-					showMonthlyPrice={ false }
-					skuCost={ 144 }
 					skuName={ 'G Suite Business' }
 					storageText={ 'Unlimited Storage' }
 					storageNoticeText={ 'Accounts with fewer than 5 users have 1 TB per user' }
@@ -39,16 +27,10 @@ describe( 'GSuitePurchaseCta', () => {
 		expect( tree ).toMatchSnapshot();
 	} );
 
-	test( 'it renders GSuitePurchaseCtaSkuInfo with monthly prices', () => {
+	test( 'it renders GSuitePurchaseCtaSkuInfo with all required props', () => {
 		const tree = renderer
 			.create(
 				<GSuitePurchaseCtaSkuInfo
-					buttonText={ 'Add G Suite Business' }
-					currencyCode={ 'USD' }
-					onButtonClick={ noop }
-					showButton={ true }
-					showMonthlyPrice={ true }
-					skuCost={ 144 }
 					skuName={ 'G Suite Business' }
 					storageText={ 'Unlimited Storage' }
 				/>
