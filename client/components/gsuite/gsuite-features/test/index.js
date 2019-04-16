9bb5329ab4c05fc0ab1f22e9265e2d25b368edd5
Caleb Bauermeister <cbauerman@gmail.com>
2019-04-16 15:17:49 -0700

    update tests

--- client/components/gsuite/gsuite-features/test/index.js
+++ client/components/gsuite/gsuite-features/test/index.js
@@ -2,60 +2,44 @@
 /**
  * External dependencies
  */
-import { Provider } from 'react-redux';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 /**
  * Internal dependencies
  */
-import { createReduxStore } from 'state';
 import GSuiteFeatures from '../';
 
 describe( 'GSuiteFeatures', () => {
 	test( 'it renders GSuiteFeatures with basic plan', () => {
-		const store = createReduxStore();
 		const tree = renderer
-			.create(
-				<Provider store={ store }>
-					<GSuiteFeatures domainName={ 'testing123.com' } productSlug={ 'gapps' } />
-				</Provider>
-			)
+			.create( <GSuiteFeatures domainName={ 'testing123.com' } productSlug={ 'gapps' } /> )
 			.toJSON();
 		expect( tree ).toMatchSnapshot();
 	} );
 
 	test( 'it renders GSuiteFeatures with business plan', () => {
-		const store = createReduxStore();
 		const tree = renderer
 			.create(
-				<Provider store={ store }>
-					<GSuiteFeatures domainName={ 'testing123.com' } productSlug={ 'gapps_unlimited' } />
-				</Provider>
+				<GSuiteFeatures domainName={ 'testing123.com' } productSlug={ 'gapps_unlimited' } />
 			)
 			.toJSON();
 		expect( tree ).toMatchSnapshot();
 	} );
 
 	test( 'it renders GSuiteFeatures in a grid', () => {
-		const store = createReduxStore();
 		const tree = renderer
 			.create(
-				<Provider store={ store }>
-					<GSuiteFeatures domainName={ 'testing123.com' } productSlug={ 'gapps' } type={ 'grid' } />
-				</Provider>
+				<GSuiteFeatures domainName={ 'testing123.com' } productSlug={ 'gapps' } type={ 'grid' } />
 			)
 			.toJSON();
 		expect( tree ).toMatchSnapshot();
 	} );
 
 	test( 'it renders GSuiteFeatures in a list', () => {
-		const store = createReduxStore();
 		const tree = renderer
 			.create(
-				<Provider store={ store }>
-					<GSuiteFeatures domainName={ 'testing123.com' } productSlug={ 'gapps' } type={ 'list' } />
-				</Provider>
+				<GSuiteFeatures domainName={ 'testing123.com' } productSlug={ 'gapps' } type={ 'list' } />
 			)
 			.toJSON();
 		expect( tree ).toMatchSnapshot();
---
+++ client/components/gsuite/gsuite-features/test/single-feature.js
@@ -0,0 +1,28 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+/**
+ * Internal dependencies
+ */
+import GSuiteSingleFeature from '../single-feature';
+
+describe( 'GSuiteSingleFeature', () => {
+	test( 'it renders GSuiteSingleFeature', () => {
+		const tree = renderer
+			.create(
+				<GSuiteSingleFeature
+					title={ 'title' }
+					description={ 'description' }
+					imagePath={ '/test/image/path.svg' }
+					imageAlt={ 'image alt' }
+					compact={ false }
+				/>
+			)
+			.toJSON();
+		expect( tree ).toMatchSnapshot();
+	} );
+} );
