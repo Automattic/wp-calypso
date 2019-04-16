c76b8f552dbbfb7b92f14bd70bd96018dc08c3a9
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2019-04-16 16:44:49 +0300

    Guided Tours: Fix JetpackBackupsRewindTour positioning (#32271)
    * Guided Tours: Fix JetpackBackupsRewindTour positioning
    
    * Guided Tours: Cleanup positioning in JetpackBackupsRewindTour

--- client/layout/guided-tours/tours/jetpack-backups-rewind-tour/index.js
+++ client/layout/guided-tours/tours/jetpack-backups-rewind-tour/index.js
@@ -57,7 +57,7 @@ const ConnectedContinueToLastStep = connect( state => ( {
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 export const JetpackBackupsRewindTour = makeTour(
 	<Tour { ...meta }>
-		<Step name="init" target=".credentials-setup-flow" placement="beside" arrow="right-top">
+		<Step name="init" target=".credentials-setup-flow" placement="below" arrow="top-left">
 			{ ( { translate } ) => (
 				<Fragment>
 					<p>
@@ -126,8 +126,6 @@ export const JetpackBackupsRewindTour = makeTour(
 		<Step
 			name="credentials"
 			target=".rewind-credentials-form"
-			placement="beside"
-			arrow="right-top"
 			style={ {
 				display: 'none',
 			} }
