073abff711bceecb9d39be709b78d8ed60a2e72c
Ramon <ramonjd@users.noreply.github.com>
2019-04-19 08:53:51 +1000

    Signup: create site preview iframe component (#32135)
    * Initial commit
    
    * Created iframe and update methods
    
    Loading fonts based on the style definitions
    
    Avoid rerender of site mockup
    
    Abstracting utils, removing (maybe) unnecessary files...
    
    added tracking to desktop
    
    * Passing next props to ensure the latest changes are coming through to the get iframe source method
    
    Ensuring that the iframe wrapper can resize to the content height of the iframe
    
    Smaller loading spinner for phone
    
    Adding a height resize to the desktop iframe on window resize
    The desktop iframe is now the default at smaller screen widths because we're already calculating the height
    
    * Some defensive element checking for good measure
    
    * Honour the resize prop when setting the container height
    
    * Removed justify content because it causes the form to push right. `.site-style__form` has a margin value of `auto` which centres the content anyway.
    
    * Abstracting `URL.revokeObjectURL` to the utils file to keep references to the Web API `URL.*` in one place.

---
+++ client/components/signup-site-preview/component.jsx
@@ -0,0 +1,131 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classNames from 'classnames';
+import { localize, translate } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import SignupSitePreviewIframe from 'components/signup-site-preview/iframe';
+import Spinner from 'components/spinner';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
+function MockupChromeMobile() {
+	return (
+		<div className="signup-site-preview__chrome-mobile">
+			<span className="signup-site-preview__chrome-label">
+				{ translate( 'Phone', {
+					comment: 'Label for a phone-sized preview of a website',
+				} ) }
+			</span>
+		</div>
+	);
+}
+
+function MockupChromeDesktop() {
+	return (
+		<div className="signup-site-preview__chrome-desktop">
+			<svg width="38" height="10">
+				<g>
+					<rect width="10" height="10" rx="5" />
+					<rect x="14" width="10" height="10" rx="5" />
+					<rect x="28" width="10" height="10" rx="5" />
+				</g>
+			</svg>
+			<span className="signup-site-preview__chrome-label">{ translate( 'Website Preview' ) }</span>
+		</div>
+	);
+}
+
+export class SignupSitePreview extends Component {
+	static propTypes = {
+		className: PropTypes.string,
+		cssUrl: PropTypes.string,
+		// The viewport device to show initially
+		defaultViewportDevice: PropTypes.oneOf( [ 'desktop', 'phone' ] ),
+		fontUrl: PropTypes.string,
+		isRtl: PropTypes.bool,
+		langSlug: PropTypes.string,
+		// Iframe body content
+		content: PropTypes.object,
+		onPreviewClick: PropTypes.func,
+		resize: PropTypes.bool,
+		scrolling: PropTypes.bool,
+	};
+
+	static defaultProps = {
+		className: '',
+		defaultViewportDevice: 'desktop',
+		isRtl: false,
+		langSlug: 'en',
+		content: {},
+		onPreviewClick: () => {},
+		resize: false,
+		scrolling: true,
+	};
+
+	constructor( props ) {
+		super( props );
+		this.state = {
+			isLoaded: false,
+			wrapperHeight: 800,
+		};
+	}
+
+	componentDidUpdate( prevProps ) {
+		if (
+			this.props.cssUrl !== prevProps.cssUrl ||
+			this.props.fontUrl !== prevProps.fontUrl ||
+			this.props.langSlug !== prevProps.langSlug
+		) {
+			this.setIsLoaded( false );
+		}
+	}
+
+	setIsLoaded = isLoaded => this.setState( { isLoaded } );
+	setWrapperHeight = wrapperHeight => this.setState( { wrapperHeight } );
+
+	render() {
+		const { isDesktop, isPhone } = this.props;
+		const className = classNames( this.props.className, 'signup-site-preview__wrapper', {
+			'is-desktop': isDesktop,
+			'is-phone': isPhone,
+		} );
+		const wrapperHeightStyle = {
+			height: this.state.wrapperHeight,
+		};
+
+		return (
+			<div className={ className } style={ this.props.resize ? wrapperHeightStyle : null }>
+				<div className="signup-site-preview__iframe-wrapper">
+					{ isPhone ? <MockupChromeMobile /> : <MockupChromeDesktop /> }
+					{ ! this.state.isLoaded && <Spinner size={ isPhone ? 20 : 40 } /> }
+					<SignupSitePreviewIframe
+						{ ...this.props }
+						setIsLoaded={ this.setIsLoaded }
+						setWrapperHeight={ this.setWrapperHeight }
+					/>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default connect(
+	( state, ownProps ) => ( {
+		isDesktop: 'desktop' === ownProps.defaultViewportDevice,
+		isPhone: 'phone' === ownProps.defaultViewportDevice,
+	} ),
+	null
+)( localize( SignupSitePreview ) );
---
+++ client/components/signup-site-preview/utils.js
@@ -0,0 +1,149 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+
+// Detect IE 11 and below
+export function isIE() {
+	const ua = window.navigator.userAgent;
+	return -1 !== ua.indexOf( 'MSIE' ) || -1 !== ua.indexOf( 'Trident/' );
+}
+
+/**
+ * Returns entry content HTML
+ *
+ * @param  {Object} content Object containing `title`, `tagline` and `body` strings
+ * @return {String}         The HTML source.
+ */
+export function getIframePageContent( { body, title, tagline } ) {
+	return `
+		<div class="entry-content">
+			<div class="site-builder__header">
+				<h1 class="site-builder__title">${ title }</h1>
+				<h2 class="site-builder__description">${ tagline }</h2>
+			</div>
+			${ body }
+		</div>`;
+}
+
+/**
+ * Returns CSS link HTML
+ *
+ * @param  {String} url 	The css file path
+ * @return {String}         The HTML source or an empty string if `url` is absent.
+ */
+export function getCSSLinkHtml( url ) {
+	return url ? `<link type="text/css" media="all" rel="stylesheet" href="${ url }" />` : '';
+}
+
+/**
+ * Releases an existing object URL to let the browser know not to keep the reference to the file any longer.
+ * For memory management: https://developer.mozilla.org/en-US/docs/Web/API/URL/revokeObjectURL
+ *
+ * @param  {String} objectUrl A DOMString containing a URL representing an object
+ */
+export function revokeObjectURL( objectUrl ) {
+	URL.revokeObjectURL( objectUrl );
+}
+
+/**
+ * Returns a WordPress page shell HTML
+ *
+ * @param  {Object}  content   Object containing `title`, `tagline` and `body` strings
+ * @param  {String}  cssUrl    A URL to the theme CSS file
+ * @param  {String}  fontUrl   A URL to the font CSS file
+ * @param  {Boolean} isRtl     If the current locale is a right-to-left language
+ * @param  {String}  langSlug  The slug of the current locale
+ * @param  {Boolean} scrolling Whether to allow scrolling on the body
+ * @return {String}            The HTML source.
+ */
+export function getIframeSource(
+	content,
+	cssUrl,
+	fontUrl,
+	isRtl = false,
+	langSlug = 'en',
+	scrolling = true
+) {
+	const source = `
+		<html lang="${ langSlug }" dir="${ isRtl ? 'rtl' : 'ltr' }">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="dns-prefetch" href="//s0.wp.com">
+			<link rel="dns-prefetch" href="//fonts.googleapis.com">
+			<title>${ content.title } – ${ content.tagline }</title>
+			<link type="text/css" media="all" rel="stylesheet" href="https://s0.wp.com/wp-content/plugins/gutenberg-core/build/block-library/style.css" />
+			${ getCSSLinkHtml( cssUrl ) }
+			${ getCSSLinkHtml( fontUrl ) }
+			<style type="text/css">
+				body {
+					padding-bottom: 25px;
+				}
+				
+				.no-scrolling {
+					overflow: hidden;
+				}
+				
+				.is-loading {
+					position: relative;
+					background: white;
+				}
+				.is-loading .site {
+					opacity: 0;
+				}
+				
+				.site {
+					opacity: 1;
+					transition: opacity 1s ease-in;
+				}	
+
+				@media only screen and (min-width: 768px) {
+					/*
+						Some of the themes use js to dynamically set the height of the banner
+						Let's set a fixed max-height.
+					*/
+					.entry .entry-content .wp-block-cover-image, 
+					.entry .entry-content .wp-block-cover {
+						min-height: 500px !important;
+					}
+				}
+
+				
+				.is-loading .wp-block-cover,
+				.is-loading img {
+					animation: loading-animation 1.5s infinite;
+					background-color: rgba( 0, 0, 0, 0.1 ) !important;
+				}
+				
+				@keyframes loading-animation {
+					0%   { background-color: rgba( 0, 0, 0, 0.7 ); }
+					50%  { background-color: rgba( 0, 0, 0, 1 ); }
+					100% { background-color: rgba( 0, 0, 0, 0.7 ); }
+				}
+			</style>
+		</head>
+		<body class="home page-template-default page logged-in is-loading ${
+			scrolling ? '' : 'no-scrolling'
+		}">
+			<div id="page" class="site">
+				<div id="content" class="site-content">
+					<section id="primary" class="content-area">
+						<main id="main" class="site-main">
+							<article class="page type-page status-publish hentry entry">
+								${ getIframePageContent( content ) }
+							</article>
+						</div>
+					</section>
+				</div>
+			</div>
+		</body>
+	</html>`;
+
+	if ( isIE() ) {
+		return source;
+	}
+
+	return URL.createObjectURL( new Blob( [ source ], { type: 'text/html' } ) );
+}
