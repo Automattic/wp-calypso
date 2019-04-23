bc702fcbe3d2b364602eaccb971c1ef32c3b2d41
Rodrigo Iloro <rodrigo.iloro@gmail.com>
2019-04-23 20:43:34 -0300

    Site Settings: removes After the Deadline from Writing Settings(#31880)

--- client/my-sites/site-settings/form-writing.jsx
+++ client/my-sites/site-settings/form-writing.jsx
@@ -232,7 +232,6 @@ const getFormSettings = settings => {
 		'wp_mobile_featured_images',
 		'wp_mobile_app_promos',
 		'post_by_email_address',
-		'after-the-deadline',
 		'onpublish',
 		'onupdate',
 		'guess_lang',
