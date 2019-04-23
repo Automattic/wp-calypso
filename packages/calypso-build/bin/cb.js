e4fee9ba459e3c884de2b6710fa9e746e2d690ac
Bernie Reiter <ockham@raz.or.at>
2019-04-23 13:02:07 +0600

    Ugh wrong dir

--- packages/calypso-build/bin/cb.js
+++ packages/calypso-build/bin/cb.js
@@ -14,7 +14,7 @@ const path = require( 'path' );
 if ( ! process.argv.some( arg => arg.startsWith( '--config' ) ) ) {
 	let webpackConfig = path.join( process.cwd(), 'webpack.config.js' );
 	if ( ! fs.existsSync( webpackConfig ) ) {
-		webpackConfig = path.join( __dirname, 'webpack.config.js' ); // Default to this package's Webpack config
+		webpackConfig = path.join( __dirname, '..', 'webpack.config.js' ); // Default to this package's Webpack config
 	}
 
 	process.argv.push( '--config', webpackConfig );
