b80d1d09400d51db9da110d92f061f3b129128f0
Bernie Reiter <ockham@raz.or.at>
2019-04-26 17:47:33 +0600

    Add back a minimal babel.config.js

---
+++ packages/calypso-build/babel.config.js
@@ -0,0 +1,13 @@
+/*
+ * Only for `bin/build-packages`.
+ * Don't use this in your projects -- use `@automattic/calypso-build/babel/default instead.
+ */
+
+/**
+ * External dependencies
+ */
+const path = require( 'path' );
+
+module.exports = {
+	presets: [ path.join( __dirname, 'babel', 'default' ) ],
+};
