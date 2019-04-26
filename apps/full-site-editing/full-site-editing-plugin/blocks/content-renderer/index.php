3cf411a13b3a7618461005caf48f2a55a6fd7658
Copons <copons@users.noreply.github.com>
2019-04-26 12:52:33 +0100

    Fix the render function

--- apps/full-site-editing/full-site-editing-plugin/blocks/content-renderer/index.php
+++ apps/full-site-editing/full-site-editing-plugin/blocks/content-renderer/index.php
@@ -1,13 +1,17 @@
 <?php
 
 function render_content_renderer_block( $attributes ) {
-	if ( ! $attributes['selectedPostId'] || ! is_int( $attributes['selectedPostId'] ) ) {
+	if ( ! isset( $attributes['selectedPostId'] ) || ! is_int( $attributes['selectedPostId'] ) ) {
 		return;
 	}
 	$align = $attributes['align'] ? ' align' . $attributes['selectedPostId'] : '';
 	$post = get_post( $attributes['selectedPostId'] );
 	setup_postdata( $post );
-	$content = '<div class="a8c-content-renderer'. $align . '">' . get_the_content() . '</div>';
+
+	$content = '<div class="a8c-content-renderer'. $align . '">'
+		. apply_filters( 'the_content', get_the_content() )
+		. '</div>';
+
 	wp_reset_postdata();
 	return $content;
 }
