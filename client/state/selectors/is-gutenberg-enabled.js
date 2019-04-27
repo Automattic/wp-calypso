f75c9a33ddb4cfc9e665da20b14396a0b8057294
Miguel Torres <miguelmariatorresrojas@gmail.com>
2019-04-27 15:10:44 +0900

    Revert "Gutenberg: Avoid Gutenframe flows on JP sites without SSL certs (#32628)" (#32633)
    This reverts commit 33bcbe58e0ef082523ecbd6af9c6bce32c58cb43.

--- client/state/selectors/is-gutenberg-enabled.js
+++ client/state/selectors/is-gutenberg-enabled.js
@@ -1,13 +1,19 @@
 /** @format */
 
 /**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import { isEnabled } from 'config';
+import getSiteOptions from 'state/selectors/get-site-options';
 import isCalypsoifyGutenbergEnabled from 'state/selectors/is-calypsoify-gutenberg-enabled';
 import isVipSite from 'state/selectors/is-vip-site';
-import { isJetpackSite, getSiteAdminUrl, isJetpackMinimumVersion } from 'state/sites/selectors';
-import { isHttps } from 'lib/url';
+import { isJetpackSite } from 'state/sites/selectors';
+import versionCompare from 'lib/version-compare';
 
 export const isGutenbergEnabled = ( state, siteId ) => {
 	if ( ! siteId ) {
@@ -18,15 +24,16 @@ export const isGutenbergEnabled = ( state, siteId ) => {
 		return true;
 	}
 
-	// We do want Gutenframe flows for JP/AT sites that have been updated to Jetpack 7.3 or greater since it will
-	// handle the required token verification. But only if the site has a SSL cert since the browser cannot embed
-	// insecure content in a resource loaded over a secure HTTPS connection.
-	if (
-		isEnabled( 'jetpack/gutenframe' ) &&
-		isJetpackMinimumVersion( state, siteId, '7.3-alpha' ) &&
-		isHttps( getSiteAdminUrl( state, siteId ) )
-	) {
-		return isEnabled( 'gutenberg' ) && ! isVipSite( state, siteId );
+	if ( isEnabled( 'jetpack/gutenframe' ) ) {
+		if (
+			versionCompare(
+				get( getSiteOptions( state, siteId ), 'jetpack_version', 0 ),
+				'7.3-alpha',
+				'>='
+			)
+		) {
+			return isEnabled( 'gutenberg' ) && ! isVipSite( state, siteId );
+		}
 	}
 
 	return (
