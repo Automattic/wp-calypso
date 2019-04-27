c78482ffb2a0a924f28807a044bdce48a6c8955e
mmtr <miguel.torres@automattic.com>
2019-04-27 11:13:18 +0900

    Gutenberg: Use Calypso Classic on JP sites without a SSL cert

--- client/state/selectors/is-calypsoify-gutenberg-enabled.js
+++ client/state/selectors/is-calypsoify-gutenberg-enabled.js
@@ -17,19 +17,19 @@ export const isCalypsoifyGutenbergEnabled = ( state, siteId ) => {
 		return false;
 	}
 
-	if ( isJetpackSite( state, siteId ) || isSiteAutomatedTransfer( state, siteId ) ) {
-		// We do want Gutenframe flows for JP/AT sites that have been updated to Jetpack 7.3 or greater since it will
-		// handle the required token verification. But only if the site has a SSL cert since the browser cannot embed
-		// insecure content in a resource loaded over a secure HTTPS connection.
-		if (
-			isEnabled( 'jetpack/gutenframe' ) &&
-			isJetpackMinimumVersion( state, siteId, '7.3-alpha' ) &&
-			isHttps( getSiteAdminUrl( state, siteId ) )
-		) {
-			return false;
-		}
+	// We do want Gutenframe flows for JP/AT sites that have been updated to Jetpack 7.3 or greater since it will
+	// handle the required token verification. But only if the site has a SSL cert since the browser cannot embed
+	// insecure content in a resource loaded over a secure HTTPS connection.
+	if (
+		isEnabled( 'jetpack/gutenframe' ) &&
+		isJetpackMinimumVersion( state, siteId, '7.3-alpha' ) &&
+		isHttps( getSiteAdminUrl( state, siteId ) )
+	) {
+		return false;
+	}
 
-		// Otherwise, we use Calypsoify flows
+	// We do want Calypsoify flows for Atomic sites
+	if ( isSiteAutomatedTransfer( state, siteId ) ) {
 		const wpVersion = getWordPressVersion( state, siteId );
 
 		// But not if they activated Classic editor plugin (effectively opting out of Gutenberg)
@@ -53,7 +53,7 @@ export const isCalypsoifyGutenbergEnabled = ( state, siteId ) => {
 	}
 
 	// Not ready yet.
-	if ( isVipSite( state, siteId ) ) {
+	if ( isJetpackSite( state, siteId ) || isVipSite( state, siteId ) ) {
 		return false;
 	}
 
