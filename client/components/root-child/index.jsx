98085626dee0aecacfcdf600d575c7ed0926fa3a
Jarda Snajdr <jsnajdr@gmail.com>
2019-04-29 23:06:30 +0200

    Implement RootChild using React portal

--- client/components/root-child/index.jsx
+++ client/components/root-child/index.jsx
@@ -1,67 +1,21 @@
-/** @format */
-
 /**
  * External dependencies
  */
-
-import ReactDom from 'react-dom';
-import PropTypes from 'prop-types';
 import React from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-
-/**
- * Internal dependencies
- */
-import { MomentProvider } from 'components/localized-moment/context';
+import ReactDOM from 'react-dom';
 
 export default class RootChild extends React.Component {
-	static contextTypes = {
-		store: PropTypes.object,
-	};
+	container = document.createElement( 'div' );
 
 	componentDidMount() {
-		this.container = document.createElement( 'div' );
 		document.body.appendChild( this.container );
-		this.renderChildren();
-	}
-
-	componentDidUpdate() {
-		this.renderChildren();
 	}
 
 	componentWillUnmount() {
-		if ( ! this.container ) {
-			return;
-		}
-
-		ReactDom.unmountComponentAtNode( this.container );
 		document.body.removeChild( this.container );
-		delete this.container;
 	}
 
-	renderChildren = () => {
-		let content;
-
-		if ( this.props && ( Object.keys( this.props ).length > 1 || ! this.props.children ) ) {
-			content = <div { ...this.props }>{ this.props.children }</div>;
-		} else {
-			content = this.props.children;
-		}
-
-		// Context is lost when creating a new render hierarchy, so ensure that
-		// we preserve the context that we care about
-		if ( this.context.store ) {
-			content = (
-				<ReduxProvider store={ this.context.store }>
-					<MomentProvider>{ content }</MomentProvider>
-				</ReduxProvider>
-			);
-		}
-
-		ReactDom.render( content, this.container );
-	};
-
 	render() {
-		return null;
+		return ReactDOM.createPortal( this.props.children, this.container );
 	}
 }
