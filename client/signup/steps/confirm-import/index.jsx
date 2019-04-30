5688f1b99a120003159e4faf648bf3f3c59d86f2
Grant Kinney <grant.kinney@automattic.com>
2019-04-30 14:51:39 -0500

    Fixes issues after rebasing from master
    - Signup dependency prop changed from `importUrl` to `importSiteUrl`

--- client/signup/steps/confirm-import/index.jsx
+++ client/signup/steps/confirm-import/index.jsx
@@ -42,7 +42,7 @@ class ImportURLStepComponent extends Component {
 	handleCancelClick = () => this.props.goToStep( 'import-url' );
 
 	renderContent = () => {
-		const { translate, signupDependencies: { importUrl } = {} } = this.props;
+		const { translate, signupDependencies: { importSiteUrl } = {} } = this.props;
 		const previewImageSrc = get( this.props, 'signupDependencies.sitePreviewImageBlob' );
 		const siteMockupClasses = classnames( 'confirm-import__site-mockup', {
 			[ 'is-hidden' ]: ! this.state.showSitePreview,
@@ -62,7 +62,7 @@ class ImportURLStepComponent extends Component {
 					{ previewImageSrc && (
 						<img
 							src={ previewImageSrc }
-							alt={ `Preview Screenshot of site: ${ withoutHttp( importUrl ) }` }
+							alt={ `Preview Screenshot of site: ${ withoutHttp( importSiteUrl ) }` }
 						/>
 					) }
 				</SiteMockup>
@@ -74,16 +74,16 @@ class ImportURLStepComponent extends Component {
 		const {
 			flowName,
 			positionInFlow,
-			signupDependencies: { importUrl } = {},
+			signupDependencies: { importSiteUrl } = {},
 			signupProgress,
 			stepName,
 			translate,
 		} = this.props;
 		const headerText = translate( 'Is this your site?' );
 		const subHeaderText = translate(
-			"Here's what we found at {{strong}}%(importUrl)s{{/strong}}, please take a moment to confirm that this is your site.",
+			"Here's what we found at {{strong}}%(importSiteUrl)s{{/strong}}, please take a moment to confirm that this is your site.",
 			{
-				args: { importUrl: withoutHttp( importUrl ) },
+				args: { importSiteUrl: withoutHttp( importSiteUrl ) },
 				components: { strong: <strong /> },
 			}
 		);
