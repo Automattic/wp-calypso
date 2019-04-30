979869bfdf2961eebff0ddd32ed809926ef561e6
Jarda Snajdr <jsnajdr@gmail.com>
2019-04-30 08:40:31 +0200

    Dialog: remove unused onClosed property
    Used to be used a long time ago (removed in #8969) in Reader Full Post dialog
    to call a callback when dialog is unmounted.

--- client/components/dialog/index.jsx
+++ client/components/dialog/index.jsx
@@ -4,7 +4,6 @@
  */
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { defer, noop } from 'lodash';
 
 /**
  * Internal dependencies
@@ -18,31 +17,18 @@ class Dialog extends Component {
 		baseClassName: PropTypes.string,
 		leaveTimeout: PropTypes.number,
 		onClose: PropTypes.func,
-		onClosed: PropTypes.func,
 		shouldCloseOnEsc: PropTypes.bool,
 	};
 
 	static defaultProps = {
 		isVisible: false,
 		leaveTimeout: 200,
-		onClosed: noop,
-	};
-
-	checkOnClosed = ref => {
-		if ( null === ref ) {
-			defer( this.props.onClosed );
-		}
 	};
 
 	render() {
 		return (
 			<RootChild>
-				<DialogBase
-					{ ...this.props }
-					ref={ this.checkOnClosed }
-					key="dialog"
-					onDialogClose={ this.onDialogClose }
-				/>
+				<DialogBase { ...this.props } onDialogClose={ this.onDialogClose } />
 			</RootChild>
 		);
 	}
