7aedfecba09ca87bda63836ddb0239cd9120379e
mmtr <miguel.torres@automattic.com>
2019-05-01 08:48:55 +0900

    Rebase

--- client/state/selectors/is-gutenframe-enabled.js
+++ client/state/selectors/is-gutenframe-enabled.js
@@ -4,31 +4,48 @@
  */
 import { isEnabled } from 'config';
 import isVipSite from 'state/selectors/is-vip-site';
-import { isJetpackSite } from 'state/sites/selectors';
-import versionCompare from '../../lib/version-compare';
-import { get } from 'lodash';
-import getSiteOptions from './get-site-options';
+import { getSiteAdminUrl, isJetpackMinimumVersion, isJetpackSite } from 'state/sites/selectors';
+import { isHttps } from 'lib/url';
+import versionCompare from 'lib/version-compare';
+import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
+import getWordPressVersion from 'state/selectors/get-wordpress-version';
+import isPluginActive from 'state/selectors/is-plugin-active';
 
 export const isGutenframeEnabled = ( state, siteId ) => {
 	if ( ! siteId ) {
 		return false;
 	}
 
-	if ( isEnabled( 'jetpack/gutenframe' ) ) {
-		if (
-			versionCompare(
-				get( getSiteOptions( state, siteId ), 'jetpack_version', 0 ),
-				'7.3-alpha',
-				'>='
-			)
-		) {
-			return isEnabled( 'gutenberg' ) && ! isVipSite( state, siteId );
+	// We do want Gutenframe flows for Jetpack and Atomic sites.
+	if ( isJetpackSite( state, siteId ) || isSiteAutomatedTransfer( state, siteId ) ) {
+		// But only once they have been updated to WordPress version 5.0 or greater since it will provide Gutenberg
+		// editor by default.
+		const wpVersion = getWordPressVersion( state, siteId );
+		if ( versionCompare( wpVersion, '5.0', '<' ) ) {
+			return false;
 		}
+
+		// But not if they activated the Classic Editor plugin (effectively opting out of Gutenberg).
+		if ( isPluginActive( state, siteId, 'classic-editor' ) ) {
+			return false;
+		}
+
+		// And also only if they have been updated to Jetpack 7.3 or greater since it will provide a way to handle the
+		// frame nonces verification.
+		if ( ! isJetpackMinimumVersion( state, siteId, '7.3-alpha' ) ) {
+			return false;
+		}
+
+		// But only if the site has a SSL cert since the browser cannot embed insecure content in a resource loaded
+		// over a secure HTTPS connection.
+		if ( ! isHttps( getSiteAdminUrl( state, siteId ) ) ) {
+			return false;
+		}
+
+		return isEnabled( 'jetpack/gutenframe' ) && isEnabled( 'gutenberg' );
 	}
 
-	return (
-		isEnabled( 'gutenberg' ) && ! isJetpackSite( state, siteId ) && ! isVipSite( state, siteId )
-	);
+	return isEnabled( 'gutenberg' ) && ! isVipSite( state, siteId );
 };
 
 export default isGutenframeEnabled;
