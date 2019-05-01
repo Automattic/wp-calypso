b8935c5047ac5215ad2e9dd04e9d8f7f4b37286c
Ran <ran@a8c.com>
2019-05-01 15:40:23 +0300

    Rephrase confirmation message to clarify refollowing

--- client/my-sites/people/followers-list/index.jsx
+++ client/my-sites/people/followers-list/index.jsx
@@ -78,10 +78,9 @@ const Followers = localize(
 				<div>
 					<p>
 						{ this.props.translate(
-							'If removed, this follower will stop receiving notifications about this site, unless they re-follow.'
+							'Removing this follower will make them stop receiving updates from your site. If they choose to, they can still visit your site, and follow it again.'
 						) }
 					</p>
-					<p>{ this.props.translate( 'Would you still like to remove this follower?' ) }</p>
 				</div>,
 				accepted => {
 					if ( accepted ) {
