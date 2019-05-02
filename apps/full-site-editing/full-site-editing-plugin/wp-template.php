63f5373c514d3f9bd578f854bb199cbc9bbdc81c
Matt Wiebe <wiebe@automattic.com>
2019-05-02 16:38:47 -0500

    Full Site Editing: make `wp_template` not `public` by default (#32702)

--- apps/full-site-editing/full-site-editing-plugin/wp-template.php
+++ apps/full-site-editing/full-site-editing-plugin/wp-template.php
@@ -32,7 +32,7 @@ function fse_register_wp_template() {
 				'item_scheduled'           => __( 'Template scheduled.' ),
 				'item_updated'             => __( 'Template updated.' ),
 			),
-			'public'                => true, // temp to support the search endpoint
+			'public'                => false,
 			'show_ui'               => true,
 			'show_in_menu'          => true,
 			'rewrite'               => false,
@@ -46,10 +46,12 @@ function fse_register_wp_template() {
 				// You need to be able to customize, in order to create templates.
 				'create_posts'           => 'edit_theme_options',
 				'edit_posts'             => 'edit_theme_options',
+				'delete_posts'           => 'edit_theme_options',
 				'edit_published_posts'   => 'edit_theme_options',
 				'delete_published_posts' => 'edit_theme_options',
 				'edit_others_posts'      => 'edit_theme_options',
 				'delete_others_posts'    => 'edit_theme_options',
+				'publish_posts'          => 'edit_theme_options'
 			),
 			'map_meta_cap'          => true,
 			'supports'              => array(
