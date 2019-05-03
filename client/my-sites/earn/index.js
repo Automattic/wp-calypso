dc27280c84c1155f37f529b68ed168e61a78bb88
Collyn Philleo <collyn.philleo@automattic.com>
2019-05-02 17:36:27 -0700

    Redirect /ads path to /earn after section rename

--- client/my-sites/earn/index.js
+++ client/my-sites/earn/index.js
@@ -16,10 +16,13 @@ export default function() {
 	page( '/earn/memberships', siteSelection, sites, makeLayout, clientRender );
 	page( '/earn/ads-settings', siteSelection, sites, makeLayout, clientRender );
 	page( '/earn/ads-earnings', siteSelection, sites, makeLayout, clientRender );
-	page( '/earn/:site_id', earnController.redirect, makeLayout, clientRender );
-	// These 2 are legacy URLs to redirect if they are present anywhere on the web.
-	page( '/ads/earnings/:site_id', earnController.redirect, makeLayout, clientRender );
+	page( '/earn/:site_id', earnController.redirectToAdsEarnings, makeLayout, clientRender );
+	// These are legacy URLs to redirect if they are present anywhere on the web.
+	page( '/ads/earnings/:site_id', earnController.redirectToAdsEarnings, makeLayout, clientRender );
 	page( '/ads/settings/:site_id', earnController.redirectToAdsSettings, makeLayout, clientRender );
+	page( '/ads/:site_id', earnController.redirectToAdsEarnings, makeLayout, clientRender );
+	page( '/ads', '/earn' );
+	page( '/ads/*', '/earn' );
 
 	page(
 		'/earn/:section/:site_id',
