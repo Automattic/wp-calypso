02afcb9a42919360782e08cce0bf543c93130f7f
Jon Surrell <jon.surrell@automattic.com>
2019-05-07 21:54:16 +0200

    Remove babel plugin export-namespace-from

--- packages/calypso-build/babel/default.js
+++ packages/calypso-build/babel/default.js
@@ -14,7 +14,6 @@ module.exports = () => ( {
 	],
 	plugins: [
 		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-proposal-export-namespace-from',
 		'@babel/plugin-syntax-dynamic-import',
 		[
 			'@babel/transform-runtime',
