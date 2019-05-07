ce37a0aaabb45024d18eecd63a3576e3d81f2953
Sérgio Gomes <mail@sgomes.com>
2019-05-07 10:38:35 +0100

    Allow multiple sections to add a reducer. (#32830)
    * Allow multiple sections to add a reducer.
    
    `addReducerToStore` now runs a single initialization per key, allowing
    for multiple sections to register the same async reducer without causing
    race conditions.
    
    * Remove async from addReducerToStore.
    
    * Throw error on mismatched reducers on same key.
    
    * Simplify key normalization logic.
    
    * Fix initial state tests to use array-based keys.
    
    * Rewrite addReducerToStore in TypeScript.

---
+++ client/state/add-reducer.ts
@@ -0,0 +1,58 @@
+/**
+ * Internal Dependencies
+ */
+import { APPLY_STORED_STATE } from 'state/action-types';
+import { getStateFromLocalStorage } from 'state/initial-state';
+
+const initializations = new Map< string, Promise >();
+const reducers = new Map< string, ( state: object, action: object ) => object >();
+
+function normalizeKey( key: string[] ): string {
+	return key.join( '.' );
+}
+
+async function initializeState(
+	store: object,
+	storageKey: string,
+	reducer: ( state: object, action: object ) => object
+) {
+	const storedState = await getStateFromLocalStorage( reducer, storageKey );
+
+	if ( storedState ) {
+		store.dispatch( { type: APPLY_STORED_STATE, storageKey, storedState } );
+	}
+}
+
+// For a given store, creates a function that adds a new reducer to the store,
+// and loads (asynchronously) and applies the persisted state for it.
+export const addReducerToStore = ( store: object ) => (
+	key: string,
+	reducer: ( state: object, action: object ) => object
+): Promise => {
+	const storageKey: string = reducer.storageKey;
+	const normalizedKey = normalizeKey( key );
+
+	const previousReducer = reducers.get( normalizedKey );
+	let init = initializations.get( normalizedKey );
+
+	if ( previousReducer && reducer !== previousReducer ) {
+		throw new Error(
+			`Different reducers on multiple calls to \`addReducerToStore\` for key: ${ normalizedKey }`
+		);
+	}
+
+	if ( ! init ) {
+		store.addReducer( key, reducer );
+
+		if ( storageKey ) {
+			init = initializeState( store, storageKey, reducer );
+		} else {
+			init = Promise.resolve();
+		}
+
+		initializations.set( normalizedKey, init );
+		reducers.set( normalizedKey, reducer );
+	}
+
+	return init;
+};
