51e2756d273d34a011d337c6824712ebb97f4ccd
Jon Surrell <jon.surrell@automattic.com>
2019-05-07 21:47:55 +0200

    Remove export-default-from plugin

--- packages/calypso-build/babel/default.js
+++ packages/calypso-build/babel/default.js
@@ -14,7 +14,6 @@ module.exports = () => ( {
 	],
 	plugins: [
 		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-proposal-export-default-from',
 		'@babel/plugin-proposal-export-namespace-from',
 		'@babel/plugin-syntax-dynamic-import',
 		[
