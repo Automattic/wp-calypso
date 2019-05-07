8935696f74c7ba4c6b64b49eddf91e62f891f3eb
Ben Lowery <ben.lowery@automattic.com>
2019-05-07 15:49:09 -0400

    Try: migrate dialog. requires migrating root-child and with-localized-moment...

--- packages/calypso-ui/src/dialog/index.js
+++ packages/calypso-ui/src/dialog/index.js
@@ -1,15 +1,57 @@
+/** @format */
 /**
  * External dependencies
  */
-import * as React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { defer, noop } from 'lodash';
 
-import './style.scss';
+/**
+ * Internal dependencies
+ */
+import RootChild from 'components/root-child';
+import DialogBase from './dialog-base';
 
-export default function Dialog( props ) {
-	return (
-		<>
-			<h1>A Dialog</h1>
-			{ props.children }
-		</>
-	);
+class Dialog extends Component {
+	static propTypes = {
+		isVisible: PropTypes.bool,
+		baseClassName: PropTypes.string,
+		leaveTimeout: PropTypes.number,
+		onClose: PropTypes.func,
+		onClosed: PropTypes.func,
+		shouldCloseOnEsc: PropTypes.bool,
+	};
+
+	static defaultProps = {
+		isVisible: false,
+		leaveTimeout: 200,
+		onClosed: noop,
+	};
+
+	checkOnClosed = ref => {
+		if ( null === ref ) {
+			defer( this.props.onClosed );
+		}
+	};
+
+	render() {
+		return (
+			<RootChild>
+				<DialogBase
+					{ ...this.props }
+					ref={ this.checkOnClosed }
+					key="dialog"
+					onDialogClose={ this.onDialogClose }
+				/>
+			</RootChild>
+		);
+	}
+
+	onDialogClose = action => {
+		if ( this.props.onClose ) {
+			this.props.onClose( action );
+		}
+	};
 }
+
+export default Dialog;
