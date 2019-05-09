0071c0e0c7015065f758eeb90ca372a7fd32c581
artpi <artur.piszek@gmail.com>
2019-05-09 17:48:30 +0200

    Product list route with display is ready

--- client/my-sites/earn/memberships/products.jsx
+++ client/my-sites/earn/memberships/products.jsx
@@ -7,20 +7,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import HeaderCake from 'components/header-cake';
+import { get } from 'lodash';
+import formatCurrency from '@automattic/format-currency';
 /**
  * Internal dependencies
  */
 import './style.scss';
 import { getSelectedSite, getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
+import HeaderCake from 'components/header-cake';
+import SectionHeader from 'components/section-header';
+import Button from 'components/button';
+import CompactCard from 'components/card/compact';
+import QueryMembershipProducts from 'components/data/query-memberships';
+import EllipsisMenu from 'components/ellipsis-menu';
+import PopoverMenuItem from 'components/popover/menu-item';
 
 class MembershipsProductsSection extends Component {
+	renderEllipsisMenu() {
+		return (
+			<EllipsisMenu position="bottom left">
+				<PopoverMenuItem>{ this.props.translate( 'Delete' ) }</PopoverMenuItem>
+			</EllipsisMenu>
+		);
+	}
 	render() {
 		return (
 			<div>
+				<QueryMembershipProducts siteId={ this.props.siteId } />
 				<HeaderCake backHref={ '/earn/memberships/' + this.props.siteSlug }>
 					{ this.props.translate( 'Membership Amounts' ) }
 				</HeaderCake>
+
+				<SectionHeader>
+					<Button primary compact>
+						{ this.props.translate( 'Add new amount' ) }
+					</Button>
+				</SectionHeader>
+				{ this.props.products.map( product => (
+					<CompactCard className="memberships__products-product-card" key={ product.ID }>
+						<div className="memberships__products-product-details">
+							<div className="memberships__products-product-price">
+								{ formatCurrency( product.price, product.currency ) }
+							</div>
+							<div className="memberships__products-product-title">{ product.title }</div>
+						</div>
+
+						{ this.renderEllipsisMenu( product.ID ) }
+					</CompactCard>
+				) ) }
 			</div>
 		);
 	}
@@ -34,6 +68,7 @@ export default connect(
 			site,
 			siteId,
 			siteSlug: getSelectedSiteSlug( state ),
+			products: get( state, [ 'memberships', 'productList', 'items', siteId ], [] ),
 		};
 	},
 	{}
