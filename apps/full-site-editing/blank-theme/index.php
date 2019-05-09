7404dfcf53ce71d66343dd4abb0d43678f9e28b9
Copons <copons@users.noreply.github.com>
2019-05-09 15:22:32 +0100

    Add loop support

--- apps/full-site-editing/blank-theme/index.php
+++ apps/full-site-editing/blank-theme/index.php
@@ -2,14 +2,18 @@
 
 get_header();
 
-$post_id = get_the_ID();
-$template_id = get_post_meta( $post_id, 'wp_template_id', true );
-$template = get_post( $template_id );
+while ( have_posts() ) {
+	the_post();
 
-if ( $template_id !== $post_id && $template ) {
-	echo apply_filters( 'the_content', $template->post_content );
-} else {
-	the_content();
+	$post_id = get_the_ID();
+	$template_id = get_post_meta( $post_id, 'wp_template_id', true );
+	$template = get_post( $template_id );
+
+	if ( $template_id !== $post_id && $template ) {
+		echo apply_filters( 'the_content', $template->post_content );
+	} else {
+		the_content();
+	}
 }
 
 get_footer();
--- apps/full-site-editing/full-site-editing-plugin/blocks/post-content/index.php
+++ apps/full-site-editing/full-site-editing-plugin/blocks/post-content/index.php
@@ -2,10 +2,11 @@
 
 function render_post_content_block( $attributes, $content ) {
 	$post_id = get_the_ID();
+	$post_type = get_post_type();
 	$template_id = get_post_meta( $post_id, 'wp_template_id', true );
 
 	// Early return to avoid the infinite loop of a template rendering itself.
-	if ( $template_id === $post_id ) {
+	if ( 'wp_template' === $post_type || $template_id === $post_id ) {
 		return $content;
 	}
 
