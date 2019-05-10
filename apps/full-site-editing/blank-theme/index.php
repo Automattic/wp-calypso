075502dce13d3373b5e49e1ac0e88920a026cdaf
Copons <copons@users.noreply.github.com>
2019-05-10 16:47:57 +0100

    Enable a rough template hierarchy

--- apps/full-site-editing/blank-theme/index.php
+++ apps/full-site-editing/blank-theme/index.php
@@ -2,17 +2,48 @@
 
 get_header();
 
-while ( have_posts() ) {
+$template_hierarchy = json_decode( get_option( 'template_hierarchy' ) );
+
+if ( is_singular() && have_posts() ) {
 	the_post();
 
 	$post_id = get_the_ID();
 	$template_id = get_post_meta( $post_id, 'wp_template_id', true );
+
+	if ( ! $template_id ) {
+		if ( isset( $template_hierarchy->singular ) ) {
+			$template_id = $template_hierarchy->singular;
+		} else if ( isset( $template_hierarchy->index ) ) {
+			$template_id = $template_hierarchy->index;
+		}
+	}
+
 	$template = get_post( $template_id );
 
 	if ( $template_id !== $post_id && $template ) {
 		echo apply_filters( 'the_content', $template->post_content );
 	} else {
 		the_content();
+	}
+} else {
+	$post_type = get_post_type();
+
+	if ( isset( $template_hierarchy->$post_type ) ) {
+		$template_id = $template_hierarchy->$post_type;
+	} else if ( isset( $template_hierarchy->index ) ) {
+		$template_id = $template_hierarchy->index;
+	}
+
+	$template = get_post( $template_id );
+
+	if ( $template ) {
+		echo apply_filters( 'the_content', $template->post_content );
+	} else {
+		while( have_posts() ) {
+			the_post(); ?>
+			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+			<div><?php the_content(); ?></div>
+		<?php }
 	}
 }
 
--- apps/full-site-editing/full-site-editing-plugin/blocks/post-content/index.php
+++ apps/full-site-editing/full-site-editing-plugin/blocks/post-content/index.php
@@ -5,20 +5,32 @@ function render_post_content_block( $attributes, $content ) {
 		return $content;
 	}
 
-	$post_id = get_the_ID();
-	$post_type = get_post_type();
-	$template_id = get_post_meta( $post_id, 'wp_template_id', true );
-
-	// Early return to avoid the infinite loop of a template rendering itself.
-	if ( 'wp_template' === $post_type || $template_id === $post_id ) {
-		return $content;
-	}
-
 	$align = isset( $attributes['align'] ) ? ' align' . $attributes['align'] : '';
 
-	$content = '<div class="post-content'. $align . '">'
-		. apply_filters( 'the_content', get_the_content() )
-		. '</div>';
+	if ( is_singular() ) {
+		$post_id = get_the_ID();
+		$post_type = get_post_type();
+		$template_id = get_post_meta( $post_id, 'wp_template_id', true );
 
-	return $content;
+		// Early return to avoid the infinite loop of a template rendering itself.
+		if ( 'wp_template' === $post_type || $template_id === $post_id ) {
+			return $content;
+		}
+
+		$content = '<div class="post-content'. $align . '">'
+			. apply_filters( 'the_content', get_the_content() )
+			. '</div>';
+
+		return $content;
+	} else {
+		$content = '<div class="post-content'. $align . '">';
+		while( have_posts() ) {
+			the_post();
+			$content .= '<h2><a href="' . get_the_permalink(). '">' . apply_filters( 'the_title', get_the_title() ) . '</a></h2>';
+			$content .= '<div>' . apply_filters( 'the_content', get_the_content() ) . '</div>';
+		}
+		$content .= '</div>';
+
+		return $content;
+	}
 }
