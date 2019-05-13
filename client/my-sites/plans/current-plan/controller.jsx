08e6aa09f781fbcec38f554d3c3c26cee40d3a9f
Jon Surrell <jon.surrell@automattic.com>
2019-05-13 16:47:58 +0200

    Hide the sidebar

--- client/my-sites/plans/current-plan/controller.jsx
+++ client/my-sites/plans/current-plan/controller.jsx
@@ -1,9 +1,6 @@
-/** @format */
-
 /**
  * External dependencies
  */
-
 import React from 'react';
 import page from 'page';
 
@@ -11,8 +8,9 @@ import page from 'page';
  * Internal Dependencies
  */
 import CurrentPlan from './';
-import { isFreePlan } from 'lib/products-values';
 import { getSelectedSite } from 'state/ui/selectors';
+import { isFreePlan } from 'lib/products-values';
+import { setSection } from 'state/ui/actions';
 
 export function currentPlan( context, next ) {
 	const state = context.store.getState();
@@ -36,6 +34,7 @@ export function currentPlan( context, next ) {
 	context.primary = <CurrentPlan path={ context.path } requestThankYou={ requestThankYou } />;
 
 	if ( requestThankYou ) {
+		context.store.dispatch( setSection( null, { hasSidebar: false } ) );
 		context.secondary = null;
 	}
 
