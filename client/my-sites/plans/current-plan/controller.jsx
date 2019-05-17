655094db79923fa06e28c0b4aeb2fdfd97e21354
Mikael Korpela <mikael@ihminen.org>
2019-05-17 13:56:25 +0300

    Jetpack checklist: remove navigation placeholders (#33100)

--- client/my-sites/plans/current-plan/controller.jsx
+++ client/my-sites/plans/current-plan/controller.jsx
@@ -10,6 +10,7 @@ import page from 'page';
 import CurrentPlan from './';
 import { getSelectedSite } from 'state/ui/selectors';
 import { isFreePlan } from 'lib/products-values';
+import { setSection } from 'state/ui/actions';
 
 export function currentPlan( context, next ) {
 	const state = context.store.getState();
@@ -33,6 +34,7 @@ export function currentPlan( context, next ) {
 	context.primary = <CurrentPlan path={ context.path } requestThankYou={ requestThankYou } />;
 
 	if ( requestThankYou ) {
+		context.store.dispatch( setSection( null, { hasSidebar: false } ) );
 		context.secondary = null;
 	}
 
