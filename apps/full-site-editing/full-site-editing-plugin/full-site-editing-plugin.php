3d566bedc770570c8cae52edb8021d1805882cbd
Matt Wiebe <wiebe@automattic.com>
2019-05-17 16:24:17 -0500

    first pass at server side template filtering

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing-plugin.php
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing-plugin.php
@@ -19,6 +19,50 @@ class A8C_Full_Site_Editing {
 		add_action( 'init', array( $this, 'register_wp_template' ) );
 		add_action( 'rest_api_init', array( $this, 'allow_searching_for_templates' ) );
 		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_script_and_style' ), 100 );
+		add_filter( 'rest_request_after_callbacks', array( $this, 'filter_posts_to_include_template' ), 10, 3 );
+	}
+
+	/**
+	 * Filters outbound REST API requests to combine `post_content` in the context of its template
+	 *
+	 * @param WP_Rest_Response $response REST API response
+	 * @return WP_Rest_Response A possibly modified response
+	 */
+	function filter_posts_to_include_template( $response, $handler, $request ) {
+		if ( ! $this->is_a_page_route( $request ) ) {
+			return $response;
+		}
+		return $this->wrap_page_with_template( $response );
+	}
+
+	private function is_a_page_route( $request ) {
+		$route = $request->get_route();
+		return 0 === strpos( $route, '/wp/v2/pages/' )
+			&& 0 === preg_match( '#/autosaves$#', $route );
+	}
+
+	private function wrap_page_with_template( $response ) {
+		$data = $response->get_data();
+		$template = $this->get_template_for_post( $data['id'] );
+		$post_content = $data['content']['raw'];
+		$data['content']['raw'] = $this->merge_content_and_template( $post_content, $template );
+		$response->set_data( $data );
+		return $response;
+	}
+
+	private function merge_content_and_template( $content, $template ) {
+		$wrapped = sprintf( '<!-- wp:a8c/post-content -->%s<!-- /wp:a8c/post-content -->', $content );
+		$all = str_replace( '<!-- wp:a8c/post-content /-->', $wrapped, $template );
+		return $all;
+	}
+
+	private function get_template_for_post( $id ) {
+		// we'll do this via post meta and such later but let's just hardcode for now
+		return '<!-- wp:heading -->
+<h2>Pretend This is a Title</h2>
+<!-- /wp:heading -->
+
+<!-- wp:a8c/post-content /-->';
 	}
 
 	function register_wp_template() {
