bb8f9fbfa6300fb4a984c4ab2db36d2d48a7ccdc
Copons <copons@users.noreply.github.com>
2019-05-20 16:03:24 +0100

    Add some proper styling

--- apps/full-site-editing/blank-theme/functions.php
+++ apps/full-site-editing/blank-theme/functions.php
@@ -16,5 +16,15 @@ function fse_blank_theme_enqueue_script_and_style() {
 		wp_get_theme()->parent()->get( 'Version' ),
 		'print'
 	);
+
+	$style_file = is_rtl()
+		? 'blank-theme.rtl.css'
+		: 'blank-theme.css';
+	wp_enqueue_style(
+		'a8c-blank-theme-style',
+		get_stylesheet_directory_uri() . '/dist/' . $style_file,
+		array(),
+		wp_get_theme()->get( 'Version' )
+	);
 }
 add_action( 'wp_enqueue_scripts', 'fse_blank_theme_enqueue_script_and_style' );
--- apps/full-site-editing/blank-theme/page.php
+++ apps/full-site-editing/blank-theme/page.php
@@ -1,28 +1,3 @@
-<?php /* ?>
-
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-			<?php
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php
-get_footer( 'blank' );
-*/ ?>
 <?php
 
 get_header( 'blank' );
@@ -33,24 +8,36 @@ if ( have_posts() ) {
 	$post_id = get_the_ID();
 	$template_id = get_post_meta( $post_id, '_wp_template_id', true );
 
-	$template = $template_id !== $post_id ? get_post( $template_id ) : null;
+	$template = $template_id && $template_id !== $post_id ? get_post( $template_id ) : null;
 
-	if ( isset( $template ) ) {
-		echo apply_filters( 'the_content', $template->post_content );
-	} else { ?>
+	if ( isset( $template ) ) { ?>
+
 		<div id="content" class="site-content">
 			<section id="primary" class="content-area">
 				<main id="main" class="site-main">
-					<?php
-						get_template_part( 'template-parts/content/content', 'page' );
-
-						if ( comments_open() || get_comments_number() ) {
-							comments_template();
-						}
-					?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<div class="entry-content">
+							<?php echo apply_filters( 'the_content', $template->post_content ); ?>
+						</div><!-- .entry-content -->
+					</article><!-- #post-<?php the_ID(); ?> -->
 				</main><!-- #main -->
 			</section><!-- #primary -->
 		</div><!-- #content -->
+
+	<?php } else { ?>
+
+		<section id="primary" class="content-area">
+			<main id="main" class="site-main">
+				<?php
+					get_template_part( 'template-parts/content/content', 'page' );
+
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				?>
+			</main><!-- #main -->
+		</section><!-- #primary -->
+
 	<?php }
 }
 
