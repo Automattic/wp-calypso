002193fea7f9401a3d4a254e67c19c0b57b43989
Copons <copons@users.noreply.github.com>
2019-05-21 11:46:31 +0100

    Add wp_body_open backward compatibility

--- apps/full-site-editing/blank-theme/functions.php
+++ apps/full-site-editing/blank-theme/functions.php
@@ -28,3 +28,18 @@ function fse_blank_theme_enqueue_script_and_style() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'fse_blank_theme_enqueue_script_and_style' );
+
+if ( ! function_exists( 'wp_body_open' ) ) :
+	/**
+	 * Fire the wp_body_open action.
+	 *
+	 * Added for backwards compatibility to support pre 5.2.0 WordPress versions
+	 * and pre 1.4 Twenty Nineteen versions.
+	 */
+	function wp_body_open() {
+		/**
+		 * Triggered after the opening <body> tag.
+		 */
+		do_action( 'wp_body_open' );
+	}
+endif;
