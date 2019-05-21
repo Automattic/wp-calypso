87bfbc9a3860f94c21604c0d7b3834eae401fb0d
Markus Dobmann <markus@mpnjs.com>
2019-05-21 09:55:44 +0200

    move progress bar story into component directory

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
-const req = require.context( '../stories', true, /\.stories\.js$/ );
+const req = require.context( '../', true, /_story\.js$/ );
 function loadStories() {
 	req.keys().forEach( filename => req( filename ) );
 }
--- packages/calypso-ui/src/progress-bar/_story.js
+++ packages/calypso-ui/src/progress-bar/_story.js
@@ -7,10 +7,11 @@ import { storiesOf } from '@storybook/react';
 /**
  * Internal dependencies
  */
-import { ProgressBar } from '../packages/calypso-ui';
+import ProgressBar from './';
 
 storiesOf( 'ProgressBar', module )
 	.add( '0% percent', () => <ProgressBar value={ 0 } title="0% complete" /> )
 	.add( '50% done', () => <ProgressBar value={ 50 } total={ 100 } /> )
 	.add( 'custom color', () => <ProgressBar value={ 100 } color="#1BABDA" /> )
 	.add( 'pulsing', () => <ProgressBar value={ 100 } isPulsing /> );
+
