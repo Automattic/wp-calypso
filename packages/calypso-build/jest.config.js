91f22d05c3c7200b6fe453a13e7e056c2bc93df5
Bernie Reiter <ockham@raz.or.at>
2019-05-21 13:08:30 +0600

    Calypso Build: Properly mock File and Style imports

--- packages/calypso-build/jest.config.js
+++ packages/calypso-build/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
 	testEnvironment: 'node',
 	testMatch: [ '<rootDir>/**/test/*.js?(x)', '!**/.eslintrc.*' ],
-	transform: { '^.+\\.jsx?$': 'babel-jest' },
+	transform: {
+		'^.+\\.[jt]sx?$': 'babel-jest',
+		'\\.(gif|jpg|jpeg|png|svg|scss|sass|css)$': __dirname + '/jest/util/assets/transform.js',
+	},
 	verbose: false,
 };
