3b2b025374acfb87685a029cb3dcbd18acee0913
Copons <copons@users.noreply.github.com>
2019-05-23 13:25:54 +0100

    Pluralize the file names

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing-plugin.php
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing-plugin.php
@@ -19,7 +19,7 @@ function a8c_load_full_site_editing() {
 	require_once __DIR__ . '/full-site-editing/blocks/template/index.php';
 	require_once __DIR__ . '/full-site-editing/class-a8c-rest-templates-controller.php';
 	require_once __DIR__ . '/full-site-editing/class-full-site-editing.php';
-	require_once __DIR__ . '/lib/feature-flag/feature-flag.php';
+	require_once __DIR__ . '/lib/feature-flags/feature-flags.php';
 
 	Full_Site_Editing::get_instance();
 }
