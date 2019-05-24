f78dd11ff4685cad183012dd30bf2a19697c2216
Adam Zieliński <adam@adamziel.com>
2019-05-24 10:25:37 +0200

    Replace empty object with {} to reduce reader's confusion

--- client/state/signup/verticals/reducer.js
+++ client/state/signup/verticals/reducer.js
@@ -9,7 +9,7 @@ import { SIGNUP_VERTICALS_SET } from 'state/action-types';
 export default createReducer( null, {
 	[ SIGNUP_VERTICALS_SET ]: ( state, action ) => {
 		const siteType = action.siteType;
-		const previousData = state ? state[ siteType ] : {};
+		const previousData = state ? state[ siteType ] : null;
 		return {
 			...state,
 			[ siteType ]: {
