9704750d306565b1b3ebfd313c9128d62ac4482e
Caleb Bauermeister <cbauerman@gmail.com>
2019-05-24 16:16:21 -0700

    import changes from previous PR

--- client/devdocs/design/index.jsx
+++ client/devdocs/design/index.jsx
@@ -10,6 +10,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { slugToCamelCase } from 'devdocs/docs-example/util';
 import { trim } from 'lodash';
+import Gridicons from 'gridicons/example';
 
 /**
  * Internal dependencies
@@ -67,7 +68,7 @@ import Gauge from 'components/gauge/docs/example';
 import GlobalNotices from 'components/global-notices/docs/example';
 import Gravatar from 'components/gravatar/docs/example';
 import GravatarCaterpillar from 'components/gravatar-caterpillar/docs/example';
-import Gridicon from 'components/gridicon/docs/example';
+import GSuiteExamples from 'components/gsuite/docs/example';
 import HeaderButton from 'components/header-button/docs/example';
 import Headers from 'components/header-cake/docs/example';
 import ImagePreloader from 'components/image-preloader/docs/example';
@@ -226,7 +227,8 @@ class DesignAssets extends React.Component {
 					<GlobalNotices readmeFilePath="global-notices" />
 					<Gravatar readmeFilePath="gravatar" />
 					<GravatarCaterpillar readmeFilePath="gravatar-caterpillar" />
-					<Gridicon />
+					<Gridicons />
+					<GSuiteExamples readmeFilePath="gsuite" />
 					<HeaderButton readmeFilePath="header-button" />
 					<Headers readmeFilePath="header-cake" />
 					<ImagePreloader readmeFilePath="image-preloader" />
---
+++ client/lib/gsuite/new-users.ts
@@ -0,0 +1,172 @@
+/**
+ * External dependencies
+ */
+import { cartItems } from 'lib/cart-values';
+import emailValidator from 'email-validator';
+import i18n from 'i18n-calypso';
+import { find, includes, groupBy, map, mapValues } from 'lodash';
+import { hasGSuite } from '.';
+
+// exporting these in the big export below causes trouble
+export interface GSuiteNewUserField {
+	value: string;
+	error: string | null;
+}
+
+export interface GSuiteNewUser {
+	domain: GSuiteNewUserField;
+	mailBox: GSuiteNewUserField;
+	firstName: GSuiteNewUserField;
+	lastName: GSuiteNewUserField;
+}
+
+export interface GSuiteProductUser {
+	firstname: string;
+	lastname: string;
+	email: string;
+}
+
+const removePreviousErrors = ( { value }: GSuiteNewUserField ): GSuiteNewUserField => ( {
+	value,
+	error: null,
+} );
+
+const requiredField = ( { value, error }: GSuiteNewUserField ): GSuiteNewUserField => ( {
+	value,
+	error:
+		! error && ( ! value || '' === value ) ? i18n.translate( 'This field is required.' ) : error,
+} );
+
+const sixtyCharacterField = ( { value, error }: GSuiteNewUserField ): GSuiteNewUserField => ( {
+	value,
+	error:
+		! error && 60 < value.length
+			? i18n.translate( "This field can't be longer than 60 characters." )
+			: error,
+} );
+
+const validEmailCharacterField = ( { value, error }: GSuiteNewUserField ): GSuiteNewUserField => ( {
+	value,
+	error:
+		! error && ! /^[0-9a-z_'-](\.?[0-9a-z_'-])*$/i.test( value )
+			? i18n.translate(
+					'Only number, letters, dashes, underscores, apostrophes and periods are allowed.'
+			  )
+			: error,
+} );
+
+const validateOverallEmail = (
+	{ value: mailBox, error: mailBoxError }: GSuiteNewUserField,
+	{ value: domain }: GSuiteNewUserField
+): GSuiteNewUserField => ( {
+	value: mailBox,
+	error:
+		! mailBoxError && ! emailValidator.validate( `${ mailBox }@${ domain }` )
+			? i18n.translate( 'Please provide a valid email address.' )
+			: mailBoxError,
+} );
+
+const validateOverallEmailAgainstExistingEmails = (
+	{ value: mailBox, error: mailBoxError }: GSuiteNewUserField,
+	{ value: domain }: GSuiteNewUserField,
+	existingGSuiteUsers: any[]
+): GSuiteNewUserField => ( {
+	value: mailBox,
+	error:
+		! mailBoxError &&
+		includes( mapValues( existingGSuiteUsers, 'email' ), `${ mailBox }@${ domain }` )
+			? i18n.translate( 'You already have this email address.' )
+			: mailBoxError,
+} );
+
+const validateUser = ( user: GSuiteNewUser ): GSuiteNewUser => {
+	// every field is required. Also scrubs previous errors.
+	const { domain, mailBox, firstName, lastName } = mapValues( user, field =>
+		requiredField( removePreviousErrors( field ) )
+	);
+
+	return {
+		domain,
+		mailBox: validateOverallEmail( validEmailCharacterField( mailBox ), domain ),
+		firstName: sixtyCharacterField( firstName ),
+		lastName: sixtyCharacterField( lastName ),
+	};
+};
+
+const validateAgainstExistingUsers = (
+	{ domain, mailBox, firstName, lastName }: GSuiteNewUser,
+	existingGSuiteUsers: any[]
+) => ( {
+	firstName,
+	lastName,
+	domain,
+	mailBox: validateOverallEmailAgainstExistingEmails( mailBox, domain, existingGSuiteUsers ),
+} );
+
+const newField = ( value: string = '' ): GSuiteNewUserField => ( {
+	value,
+	error: null,
+} );
+
+const newUser = ( domain: string = '' ): GSuiteNewUser => {
+	return {
+		firstName: newField(),
+		lastName: newField(),
+		mailBox: newField(),
+		domain: newField( domain ),
+	};
+};
+
+const newUsers = ( domain: string ): GSuiteNewUser[] => {
+	return [ newUser( domain ) ];
+};
+
+const isUserComplete = ( user: GSuiteNewUser ): boolean => {
+	return Object.values( user ).every( ( { value } ) => '' !== value );
+};
+
+const doesUserHaveError = ( user: GSuiteNewUser ): boolean => {
+	return Object.values( user ).some( ( { error } ) => null !== error );
+};
+
+const userIsReady = ( user: GSuiteNewUser ): boolean =>
+	isUserComplete( user ) && ! doesUserHaveError( user );
+
+const transformUser = ( {
+	firstName: { value: firstname },
+	lastName: { value: lastname },
+	domain: { value: domain },
+	mailBox: { value: mailBox },
+}: GSuiteNewUser ): GSuiteProductUser => ( {
+	email: `${ mailBox }@${ domain }`.toLowerCase(),
+	firstname,
+	lastname,
+} );
+
+const getItemsForCart = (
+	domains: { name: string },
+	productSlug: string,
+	users: GSuiteNewUser[]
+) => {
+	const groups = mapValues( groupBy( users, 'domain.value' ), groupedUsers =>
+		groupedUsers.map( transformUser )
+	);
+
+	return map( groups, ( groupedUsers, domain ) => {
+		const domainInfo = find( domains, [ 'name', domain ] );
+		return domainInfo && hasGSuite( domainInfo )
+			? cartItems.gsuiteExtraLicenses( domain, groupedUsers )
+			: cartItems.gsuite( domain, groupedUsers, productSlug );
+	} );
+};
+
+export {
+	doesUserHaveError,
+	getItemsForCart,
+	isUserComplete,
+	newUser,
+	newUsers,
+	userIsReady,
+	validateAgainstExistingUsers,
+	validateUser,
+};
