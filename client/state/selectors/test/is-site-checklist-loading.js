f2ecb1b6f338c2982e9aaf421bc5ef7cabbe7458
Bernie Reiter <ockham@raz.or.at>
2019-05-25 14:54:19 +0600

    Fix test

--- client/state/selectors/test/is-site-checklist-loading.js
+++ client/state/selectors/test/is-site-checklist-loading.js
@@ -4,22 +4,30 @@
  * Internal dependencies
  */
 import isSiteChecklistLoading from 'state/selectors/is-site-checklist-loading';
+import { getRequestKey } from 'state/data-layer/wpcom-http/utils';
+import { requestSiteChecklist } from 'state/checklist/actions';
 
 describe( 'isSiteChecklistLoading()', () => {
 	test( 'should return `false` by default', () => {
-		const isLoading = isSiteChecklistLoading( {}, 1234567 );
+		const state = {
+			dataRequests: {},
+		};
+		const isLoading = isSiteChecklistLoading( state, 1234567 );
 		expect( isLoading ).toEqual( false );
 	} );
 
 	test( 'should return isLoading value', () => {
+		const siteId = 1234567;
+		const action = requestSiteChecklist( siteId );
 		const state = {
-			checklist: {
-				1234567: {
-					isLoading: true,
+			dataRequests: {
+				[ getRequestKey( action ) ]: {
+					status: 'pending',
 				},
 			},
 		};
-		const isLoading = isSiteChecklistLoading( state, 1234567 );
+
+		const isLoading = isSiteChecklistLoading( state, siteId );
 		expect( isLoading ).toEqual( true );
 	} );
 } );
