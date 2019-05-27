116f7afae404a6092b59d4f037eacb6a6f2bd45c
Leif Singer <git@singer.sh>
2019-05-27 16:24:00 +0200

    Signup / Checkout: remember coupons passed into signup via a query argument with a lifetime of a week and apply the most recent one in checkout

--- client/signup/main.jsx
+++ client/signup/main.jsx
@@ -209,6 +209,7 @@ class Signup extends React.Component {
 
 	componentDidMount() {
 		debug( 'Signup component mounted' );
+		this.saveCouponQueryArgument();
 		this.startTrackingForBusinessSite();
 		this.recordSignupStart();
 		this.preloadNextStep();
@@ -231,6 +232,35 @@ class Signup extends React.Component {
 		const filteredDestination = getDestination( destination, dependencies, this.props.flowName );
 		return this.handleFlowComplete( dependencies, filteredDestination );
 	};
+
+	saveCouponQueryArgument() {
+		// read coupon query argument, return early if there is none
+		const parsedUrl = urlParseAmpCompatible( location.href );
+		const couponCode = parsedUrl.query.coupon;
+		if ( ! couponCode ) {
+			return;
+		}
+
+		// read coupon list from localStorage, create new if it's not there yet, refresh existing
+		const couponsJson = localStorage.getItem( 'marketing-coupons' );
+		const coupons = JSON.parse( couponsJson ) || {};
+		const ONE_WEEK_MILLISECONDS = 7 * 24 * 60 * 60 * 1000;
+		const now = Date.now();
+		debug( 'Found coupons in localStorage: ', coupons );
+
+		coupons[ couponCode ] = now;
+
+		// delete coupons if they're older than a week
+		Object.keys( coupons ).forEach( key => {
+			if ( now > coupons[ key ] + ONE_WEEK_MILLISECONDS ) {
+				delete coupons[ key ];
+			}
+		} );
+
+		// write remembered coupons back to localStorage
+		debug( 'Storing coupons in localStorage: ', coupons );
+		localStorage.setItem( 'marketing-coupons', JSON.stringify( coupons ) );
+	}
 
 	startTrackingForBusinessSite() {
 		const siteType = get( this.props.signupDependencies, 'siteType' );
