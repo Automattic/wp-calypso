e8fbfaeec76837546f8d4e648452e1e266cf63e2
Daniel Dudzic <daniel@woothemes.com>
2019-05-27 19:06:52 +0200

    Billing: Moving the Upcoming Charges table to a new tab (#32764)

--- client/me/billing-history/upcoming-charges.jsx
+++ client/me/billing-history/upcoming-charges.jsx
@@ -10,10 +10,8 @@ import { localize } from 'i18n-calypso';
  */
 import Card from 'components/card';
 import MeSidebarNavigation from 'me/sidebar-navigation';
-import config from 'config';
-import CreditCards from 'me/purchases/credit-cards';
 import PurchasesHeader from '../purchases/purchases-list/header';
-import BillingHistoryTable from './billing-history-table';
+import UpcomingChargesTable from './upcoming-charges-table';
 import Main from 'components/main';
 import DocumentHead from 'components/data/document-head';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
@@ -24,18 +22,17 @@ import QueryBillingTransactions from 'components/data/query-billing-transactions
  */
 import './style.scss';
 
-const BillingHistory = ( { translate } ) => (
-	<Main className="billing-history">
-		<DocumentHead title={ translate( 'Billing History' ) } />
-		<PageViewTracker path="/me/purchases/billing" title="Me > Billing History" />
+const UpcomingCharges = ( { translate } ) => (
+	<Main>
+		<DocumentHead title={ translate( 'Upcoming Charges' ) } />
+		<PageViewTracker path="/me/purchases/upcoming" title="Me > Upcoming Charges" />
 		<MeSidebarNavigation />
 		<QueryBillingTransactions />
-		<PurchasesHeader section={ 'billing' } />
-		<Card className="billing-history__receipts">
-			<BillingHistoryTable />
+		<PurchasesHeader section={ 'upcoming' } />
+		<Card className="billing-history__upcoming-charges">
+			<UpcomingChargesTable />
 		</Card>
-		{ config.isEnabled( 'upgrades/credit-cards' ) && <CreditCards /> }
 	</Main>
 );
 
-export default localize( BillingHistory );
+export default localize( UpcomingCharges );
