9c1790b30b39992d67ec252d1c0107c4ab3b3216
bluefuton <chris@bluefuton.com>
2019-05-28 09:39:19 +1200

    Refactor to remove lodash dependency

--- client/reader/lib/author-name-blacklist/index.ts
+++ client/reader/lib/author-name-blacklist/index.ts
@@ -1,20 +1,8 @@
 /**
- * External dependencies
- */
-import { includes } from 'lodash';
-
-const authorNameBlacklist = [ 'admin' ];
-
-/**
  * Is the provided author name blacklisted?
  *
  * @param {string} authorName Author name
  * @returns {boolean} True if blacklisted
  */
-export const isAuthorNameBlacklisted = ( authorName: string ): boolean => {
-	if ( ! authorName ) {
-		return false;
-	}
-
-	return includes( authorNameBlacklist, authorName.toLowerCase() );
-};
+export const isAuthorNameBlacklisted = ( authorName: string ) =>
+	!! authorName && 'admin' === authorName.toLowerCase();
