fc7258322a85bb6072e66d73959e688fae36d35e
Bernie Reiter <ockham@raz.or.at>
2019-05-28 11:32:36 +0600

    Guided Tour: Wait until target appears

--- client/layout/guided-tours/config-elements/step.tsx
+++ client/layout/guided-tours/config-elements/step.tsx
@@ -168,7 +168,24 @@ export default class Step extends Component< Props, State > {
 			}
 		}
 
-		return Promise.resolve();
+		// From https://gist.github.com/jwilson8767/db379026efcbd932f64382db4b02853e
+		return new Promise( resolve => {
+			if ( document.querySelector( props.target ) ) {
+				return resolve;
+			}
+			new MutationObserver( ( mutationRecords, observer ) => {
+				// Query for elements matching the specified selector
+				Array.from( document.querySelectorAll( props.target ) ).forEach( () => {
+					resolve();
+					//Once we have resolved we don't need the observer anymore.
+					observer.disconnect();
+					return true;
+				} );
+			} ).observe( document.documentElement, {
+				childList: true,
+				subtree: true,
+			} );
+		} );
 	}
 
 	safeSetState( state: State ) {
