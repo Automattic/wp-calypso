78516fd0618a957d0a2fac9aeec2ddb897509c02
Bernie Reiter <ockham@raz.or.at>
2019-05-28 18:01:31 +0600

    Revert "Use path.join()"
    This reverts commit b4b91913abd48f4e0dcab6a54ebfbd9796dcda4b.

--- packages/calypso-build/jest.config.js
+++ packages/calypso-build/jest.config.js
@@ -1,18 +1,10 @@
-/**
- * External dependencies
- */
-import path from 'path';
-
 module.exports = {
-	setupFilesAfterEnv: [ path.join( __dirname, '/jest/setup.js' ) ],
+	setupFilesAfterEnv: [ __dirname + '/jest/setup.js' ],
 	testEnvironment: 'node',
 	testMatch: [ '<rootDir>/**/test/*.js?(x)', '!**/.eslintrc.*' ],
 	transform: {
 		'^.+\\.[jt]sx?$': 'babel-jest',
-		'\\.(gif|jpg|jpeg|png|svg|scss|sass|css)$': path.join(
-			__dirname,
-			'/jest/util/assets/transform.js'
-		),
+		'\\.(gif|jpg|jpeg|png|svg|scss|sass|css)$': __dirname + '/jest/util/assets/transform.js',
 	},
 	verbose: false,
 };
