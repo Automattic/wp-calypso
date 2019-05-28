9a9ac879580fc5f07a6f943b0e4078cea403c450
Bernie Reiter <ockham@raz.or.at>
2019-05-28 18:03:32 +0600

    Use `path.join`
    Co-Authored-By: Jon Surrell <jon.surrell@automattic.com>

--- packages/calypso-build/jest.config.js
+++ packages/calypso-build/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	testMatch: [ '<rootDir>/**/test/*.js?(x)', '!**/.eslintrc.*' ],
 	transform: {
 		'^.+\\.[jt]sx?$': 'babel-jest',
-		'\\.(gif|jpg|jpeg|png|svg|scss|sass|css)$': __dirname + '/jest/util/assets/transform.js',
+		'\\.(gif|jpg|jpeg|png|svg|scss|sass|css)$': path.join( __dirname, 'jest', 'util', 'assets', 'transform.js' ),
 	},
 	verbose: false,
 };
