4cd689f1582dba96ef2bc5e8919e1e35c6505f5d
Jeff Bowen <jblz@users.noreply.github.com>
2019-05-28 14:06:55 -0400

    alphabetize an import

--- client/my-sites/plans-features-main/index.jsx
+++ client/my-sites/plans-features-main/index.jsx
@@ -35,12 +35,12 @@ import QueryPlans from 'components/data/query-plans';
 import QuerySitePlans from 'components/data/query-site-plans';
 import { isEnabled } from 'config';
 import {
-	plansLink,
-	planMatches,
 	findPlansKeys,
 	getPlan,
 	getPopularPlanType,
 	isFreePlan,
+	planMatches,
+	plansLink,
 } from 'lib/plans';
 import Button from 'components/button';
 import SegmentedControl from 'components/segmented-control';
