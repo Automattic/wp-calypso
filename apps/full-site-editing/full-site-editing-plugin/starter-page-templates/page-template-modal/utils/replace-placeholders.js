ac0e02c5add5bfbfa70438e0a37de0d84783cfc0
Marek Hrabe <marekhrabe@me.com>
2019-05-29 12:36:32 +0200

    add vertical placeholder handling

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/utils/replace-placeholders.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/utils/replace-placeholders.js
@@ -4,12 +4,14 @@ const PLACEHOLDER_DEFAULTS = {
 	Address: '123 Main St',
 	Phone: '555-555-5555',
 	CompanyName: __( 'Your Company Name' ),
+	Vertical: __( 'Business' ),
 };
 
 const KEY_MAP = {
 	CompanyName: 'title',
 	Address: 'address',
 	Phone: 'phone',
+	Vertical: 'vertical',
 };
 
 const replacePlaceholders = ( pageContent, siteInformation = {} ) => {
