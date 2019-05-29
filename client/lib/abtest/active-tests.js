735c03dc95e05ad37d52526d83a38adaf7a7d86a
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2019-05-29 16:42:26 +0530

    Add the abtest

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -135,4 +135,13 @@ export default {
 		defaultVariation: 'notSkippable',
 		allowExistingUsers: true,
 	},
+	showPlanUpsellNudge: {
+		datestamp: '20190529',
+		variations: {
+			variantShowNudge: 50,
+			control: 50,
+		},
+		defaultVariation: 'control',
+		allowExistingUsers: true,
+	},
 };
