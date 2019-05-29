8eda52d20d99ddeea2dd5682d9843d8a30a552b5
Marek Hrabe <marekhrabe@me.com>
2019-05-29 16:31:22 +0200

    Starer Page Templates: Add translation calls (#33422)
    * translate placeholders
    
    * add script translations
    
    * include vertical too
    
    * use imports instead of reading window.wp, add translation calls
    
    * add domains
    
    * customiZe

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/index.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/index.js
@@ -1,10 +1,20 @@
 /**
+ * External dependencies
+ */
+import { keyBy, map, has } from 'lodash';
+import { __ } from '@wordpress/i18n';
+import { withState } from '@wordpress/compose';
+import { Modal } from '@wordpress/components';
+import { registerPlugin } from '@wordpress/plugins';
+import { dispatch } from '@wordpress/data';
+import { parse as parseBlocks } from '@wordpress/blocks';
+
+/**
  * Internal dependencies
  */
 import replacePlaceholders from './utils/replace-placeholders';
 import './styles/starter-page-templates-editor.scss';
 import TemplateSelectorControl from './components/template-selector-control';
-import { keyBy, map, has } from 'lodash';
 
 // TODO: remove once we have proper previews from API
 if ( window.starterPageTemplatesConfig ) {
@@ -33,72 +43,76 @@ if ( window.starterPageTemplatesConfig ) {
 	);
 }
 
-( function( wp, config = {} ) {
-	const registerPlugin = wp.plugins.registerPlugin;
-	const { Modal } = wp.components;
-	const { withState } = wp.compose;
+const { siteInformation = {}, templates = [] } = window.starterPageTemplatesConfig;
+const editorDispatcher = dispatch( 'core/editor' );
 
-	const { siteInformation = {}, templates = [] } = config;
+const insertTemplate = template => {
+	// Skip inserting if there's nothing to insert.
+	if ( ! has( template, 'content' ) ) {
+		return;
+	}
 
-	const insertTemplate = template => {
-		// Skip inserting if there's nothing to insert.
-		if ( ! has( template, 'content' ) ) {
-			return;
-		}
+	// set title
+	editorDispatcher.editPost( { title: replacePlaceholders( template.title, siteInformation ) } );
 
-		// set title
-		wp.data
-			.dispatch( 'core/editor' )
-			.editPost( { title: replacePlaceholders( template.title, siteInformation ) } );
+	// load content
+	const templateString = replacePlaceholders( template.content, siteInformation );
+	const blocks = parseBlocks( templateString );
+	editorDispatcher.insertBlocks( blocks );
+};
 
-		// load content
-		const templateString = replacePlaceholders( template.content, siteInformation );
-		const blocks = wp.blocks.parse( templateString );
-		wp.data.dispatch( 'core/editor' ).insertBlocks( blocks );
-	};
+const PageTemplateModal = withState( {
+	isOpen: true,
+	isLoading: false,
+	verticalTemplates: keyBy( templates, 'slug' ),
+} )( ( { isOpen, verticalTemplates, setState } ) => (
+	<div>
+		{ isOpen && (
+			<Modal
+				title={ __( 'Select Page Template', 'full-site-editing' ) }
+				onRequestClose={ () => setState( { isOpen: false } ) }
+				className="page-template-modal"
+			>
+				<div className="page-template-modal__inner">
+					<form className="page-template-modal__form">
+						<fieldset className="page-template-modal__list">
+							<legend className="page-template-modal__intro">
+								<p>
+									{ __(
+										'Pick a Template that matches the purpose of your page.',
+										'full-site-editing'
+									) }
+								</p>
+								<p>
+									{ __(
+										'You can customize each Template to meet your needs.',
+										'full-site-editing'
+									) }
+								</p>
+							</legend>
+							<TemplateSelectorControl
+								label={ __( 'Template', 'full-site-editing' ) }
+								templates={ Object.values( verticalTemplates ).map( template => ( {
+									label: template.title,
+									value: template.slug,
+									preview: template.preview && template.preview.src,
+									previewAlt: template.preview && template.preview.alt,
+								} ) ) }
+								onClick={ newTemplate => {
+									setState( { isOpen: false } );
+									insertTemplate( verticalTemplates[ newTemplate ] );
+								} }
+							/>
+						</fieldset>
+					</form>
+				</div>
+			</Modal>
+		) }
+	</div>
+) );
 
-	const PageTemplateModal = withState( {
-		isOpen: true,
-		isLoading: false,
-		verticalTemplates: keyBy( templates, 'slug' ),
-	} )( ( { isOpen, verticalTemplates, setState } ) => (
-		<div>
-			{ isOpen && (
-				<Modal
-					title="Select Page Template"
-					onRequestClose={ () => setState( { isOpen: false } ) }
-					className="page-template-modal"
-				>
-					<div className="page-template-modal__inner">
-						<form className="page-template-modal__form">
-							<fieldset className="page-template-modal__list">
-								<legend className="page-template-modal__intro">
-									<p>Pick a Template that matches the purpose of your page.</p>
-									<p>You can customise each Template to meet your needs.</p>
-								</legend>
-								<TemplateSelectorControl
-									label="Template"
-									templates={ Object.values( verticalTemplates ).map( template => ( {
-										label: template.title,
-										value: template.slug,
-										preview: template.preview && template.preview.src,
-										previewAlt: template.preview && template.preview.alt,
-									} ) ) }
-									onClick={ newTemplate => {
-										setState( { isOpen: false } );
-										insertTemplate( verticalTemplates[ newTemplate ] );
-									} }
-								/>
-							</fieldset>
-						</form>
-					</div>
-				</Modal>
-			) }
-		</div>
-	) );
-	registerPlugin( 'page-templates', {
-		render: function() {
-			return <PageTemplateModal />;
-		},
-	} );
-} )( window.wp, window.starterPageTemplatesConfig );
+registerPlugin( 'page-templates', {
+	render: function() {
+		return <PageTemplateModal />;
+	},
+} );
