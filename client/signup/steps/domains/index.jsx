10f80d7166312fc98aded76d26f784043a956b99
Spen <spen_@hotmail.co.uk>
2019-05-30 18:09:17 +0100

    Onboarding/Domains: Use segment specific copy for transfer/mapping subheader

--- client/signup/steps/domains/index.jsx
+++ client/signup/steps/domains/index.jsx
@@ -518,17 +518,11 @@ class DomainsStep extends React.Component {
 	};
 
 	getSubHeaderText() {
-		const { siteType, translate } = this.props;
-		const onboardingSubHeaderCopy =
-			siteType && getSiteTypePropertyValue( 'slug', siteType, 'domainsStepSubheader' );
+		const { siteType, stepSectionName } = this.props;
 
-		if ( onboardingSubHeaderCopy ) {
-			return onboardingSubHeaderCopy;
-		}
-
-		return 'transfer' === this.props.stepSectionName || 'mapping' === this.props.stepSectionName
-			? translate( 'Use a domain you already own with your new WordPress.com site.' )
-			: translate( "Enter your site's name or some keywords that describe it to get started." );
+		return 'transfer' === stepSectionName || 'mapping' === stepSectionName
+			? getSiteTypePropertyValue( 'slug', siteType, 'domainsStepTransferringSubheader' )
+			: getSiteTypePropertyValue( 'slug', siteType, 'domainsStepSubheader' );
 	}
 
 	getHeaderText() {
