8f81a6fd7d4c18d4965bde45a99f945383d38084
Mikael Korpela <mikael@ihminen.org>
2019-05-30 14:25:57 +0300

    Turn ThankYou back to class component for `requestGuidedTour`

--- client/my-sites/plans/current-plan/current-plan-thank-you/thank-you.js
+++ client/my-sites/plans/current-plan/current-plan-thank-you/thank-you.js
@@ -4,7 +4,7 @@
 import { connect } from 'react-redux';
 import { getSelectedSiteSlug } from 'state/ui/selectors';
 import { localize } from 'i18n-calypso';
-import React from 'react';
+import React, { Component } from 'react';
 
 /**
  * Internal dependencies
@@ -16,64 +16,67 @@ import Button from 'components/button';
 
 import './style.scss';
 
-export function ThankYouCard( {
-	children,
-	illustration,
-	showCalypsoIntro,
-	showContinueButton,
-	showHideMessage,
-	siteSlug,
-	title,
-	translate,
-} ) {
-
-	const startChecklistTour = () => {
+export class ThankYouCard extends Component {
+	startChecklistTour = () => {
 		if ( isDesktop() ) {
 			this.props.requestGuidedTour( 'jetpackChecklistTour' );
 		}
 	};
 
-	return (
-		<div className="current-plan-thank-you">
-			{ illustration && (
-				<img
-					alt=""
-					aria-hidden="true"
-					className="current-plan-thank-you__illustration"
-					src={ illustration }
-				/>
-			) }
-			{ title && <h1 className="current-plan-thank-you__title">{ title }</h1> }
-			{ children }
-			{ showCalypsoIntro && (
-				<p>
-					{ preventWidows(
-						translate(
-							'This is your new WordPress.com dashboard. You can manage your site ' +
-								'here, or return to your self-hosted WordPress dashboard using the ' +
-								'link at the bottom of your checklist.'
-						)
-					) }
-				</p>
-			) }
-			{ showContinueButton && (
-				<Button
-					href={ `/plans/my-plan/${ siteSlug }` }
-					onClick={ startChecklistTour }
-					primary
-				>
-					{ translate( 'Continue' ) }
-				</Button>
-			) }
-			{ showHideMessage && (
-				<p>
-					<a href={ `/plans/my-plan/${ siteSlug }` } onClick={ startChecklistTour }>
-						{ translate( 'Hide message' ) }
-					</a>
-				</p>
-			) }
-		</div>
-	);
+	render() {
+		const {
+			children,
+			illustration,
+			showCalypsoIntro,
+			showContinueButton,
+			showHideMessage,
+			siteSlug,
+			title,
+			translate,
+		} = this.props;
+
+		return (
+			<div className="current-plan-thank-you">
+				{ illustration && (
+					<img
+						alt=""
+						aria-hidden="true"
+						className="current-plan-thank-you__illustration"
+						src={ illustration }
+					/>
+				) }
+				{ title && <h1 className="current-plan-thank-you__title">{ title }</h1> }
+				{ children }
+				{ showCalypsoIntro && (
+					<p>
+						{ preventWidows(
+							translate(
+								'This is your new WordPress.com dashboard. You can manage your site ' +
+									'here, or return to your self-hosted WordPress dashboard using the ' +
+									'link at the bottom of your checklist.'
+							)
+						) }
+					</p>
+				) }
+				{ showContinueButton && (
+					<Button
+						href={ `/plans/my-plan/${ siteSlug }` }
+						onClick={ this.startChecklistTour }
+						primary
+					>
+						{ translate( 'Continue' ) }
+					</Button>
+				) }
+				{ showHideMessage && (
+					<p>
+						<a href={ `/plans/my-plan/${ siteSlug }` } onClick={ this.startChecklistTour }>
+							{ translate( 'Hide message' ) }
+						</a>
+					</p>
+				) }
+			</div>
+		);
+	}
 }
 
 export default connect(
