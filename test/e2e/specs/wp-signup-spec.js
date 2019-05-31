bce4a4c530d41caebdfa656d2392617014fff074
Ivan Stojadinov <ivanstojadinov@gmail.com>
2019-05-31 21:12:26 +0200

    Skip "Sign up for free subdomain site" test (#33517)
    Temporarily skip "Sign up for free subdomain site @parallel".
    
    The test is failing after https://github.com/Automattic/wp-calypso/pull/33470 was merged.

--- test/e2e/specs/wp-signup-spec.js
+++ test/e2e/specs/wp-signup-spec.js
@@ -1274,7 +1274,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		} );
 	} );
 
-	describe( 'Sign up for free subdomain site @parallel', function() {
+	describe.skip( 'Sign up for free subdomain site @parallel', function() {
 		const blogName = dataHelper.getNewBlogName();
 		const expectedDomainName = `${ blogName }.art.blog`;
 
