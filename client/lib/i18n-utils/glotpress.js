4608b908a65990ffe4c3b3238ff685bd0ee6f8f3
Julian de Bhal <julian.debhal@automattic.com>
2019-06-03 21:36:03 +1000

    PHPDoc fixes

--- client/lib/i18n-utils/glotpress.js
+++ client/lib/i18n-utils/glotpress.js
@@ -11,7 +11,7 @@ import debugFactory from 'debug';
  */
 import { GP_BASE_URL } from './constants';
 
-const debug = debugFactory( 'calypso:i18n-utils:glotpress' )
+const debug = debugFactory( 'calypso:i18n-utils:glotpress' );
 
 /**
  * Sends the POST request
@@ -20,16 +20,14 @@ const debug = debugFactory( 'calypso:i18n-utils:glotpress' )
  * @returns {Object} request object
  */
 export function postRequest( glotPressUrl, postFormData ) {
-	return (
-		request
-			.post( glotPressUrl )
-			.withCredentials()
-			.send( postFormData )
-			.then( response => response.body )
-			.catch( error => {
-				throw error; // pass on the error so the call sites can handle it accordingly.
-			} )
-	);
+	return request
+		.post( glotPressUrl )
+		.withCredentials()
+		.send( postFormData )
+		.then( response => response.body )
+		.catch( error => {
+			throw error; // pass on the error so the call sites can handle it accordingly.
+		} );
 }
 
 export function encodeOriginalKey( { original, context } ) {
@@ -38,19 +36,16 @@ export function encodeOriginalKey( { original, context } ) {
 
 /**
  * Sends originals to translate.wordpress.com to be recorded
- * @param {[String]} Array of original keys to record
- * @param {String} context to record
+ * @param {[String]} originalKeys Array of original keys to record
+ * @param {String} recordId fallback recordId to pass to the backend
  * @param {Function} post see postRequest()
  * @returns {Object} request object
  */
 export function recordOriginals( originalKeys, recordId, post = postRequest ) {
 	const glotPressUrl = `${ GP_BASE_URL }/api/translations/-record-originals`;
-	const recordIdQueryFragment = recordId
-		? `record_id=${ encodeURIComponent( recordId ) }&`
-		: '';
-	const postFormData = recordIdQueryFragment +
-		`originals=${ encodeURIComponent( JSON.stringify( originalKeys ) ) }`;
+	const recordIdQueryFragment = recordId ? `record_id=${ encodeURIComponent( recordId ) }&` : '';
+	const postFormData =
+		recordIdQueryFragment + `originals=${ encodeURIComponent( JSON.stringify( originalKeys ) ) }`;
 
-	return post( glotPressUrl, postFormData )
-		.catch( ( err ) => debug( 'recordOriginals failed:', err ) );
+	return post( glotPressUrl, postFormData ).catch( err => debug( 'recordOriginals failed:', err ) );
 }
