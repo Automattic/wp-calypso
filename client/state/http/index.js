ba0b76ff544a4c41e3a333b331a0077f149dafd4
Tugdual de Kerviler <dekervit@gmail.com>
2019-06-03 18:50:01 +0200

    Handle both json and urlencoded data for fetch

--- client/state/http/index.js
+++ client/state/http/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { fromPairs } from 'lodash';
+import { fromPairs, identity, toPairs } from 'lodash';
 
 /***
  * Internal dependencies
@@ -9,6 +9,15 @@ import { fromPairs } from 'lodash';
 import { extendAction } from 'state/utils';
 import { HTTP_REQUEST } from 'state/action-types';
 import { failureMeta, successMeta } from 'state/data-layer/wpcom-http';
+
+const encodeQueryParameters = queryParams => {
+	return queryParams
+		.map(
+			( [ queryKey, queryValue ] ) =>
+				encodeURIComponent( queryKey ) + '=' + encodeURIComponent( queryValue )
+		)
+		.join( '&' );
+};
 
 const isAllHeadersValid = headers =>
 	headers.every(
@@ -54,19 +63,27 @@ export const httpHandler = async ( { dispatch }, action ) => {
 	const fetchHeaders = fromPairs( headers );
 	fetchHeaders.Accept = 'application/json';
 
-	const queryString = queryParams
-		.map(
-			( [ queryKey, queryValue ] ) =>
-				encodeURIComponent( queryKey ) + '=' + encodeURIComponent( queryValue )
-		)
-		.join( '&' );
+	const contentType = ( fetchHeaders[ 'Content-Type' ] || '' ).split( ';' )[ 0 ];
+
+	let serialize;
+
+	if ( contentType === 'application/x-www-form-urlencoded' ) {
+		serialize = data => encodeQueryParameters( toPairs( data ) );
+	} else if ( contentType === 'application/json' || typeof body !== 'string' ) {
+		serialize = JSON.stringify.bind( JSON );
+	} else {
+		// assume body is already serialized
+		serialize = identity;
+	}
+
+	const queryString = encodeQueryParameters( queryParams );
 
 	let response, json;
 	try {
 		response = await fetch( queryString.length ? `${ url }?${ queryString }` : url, {
 			method,
 			headers: fetchHeaders,
-			body: typeof body === 'object' ? JSON.stringify( body ) : body,
+			body: serialize( body ),
 			credentials: withCredentials ? 'include' : 'same-origin',
 		} );
 		json = await response.json();
