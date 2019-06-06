9cee38242fcdb917bd693bd061c442dab07ac00d
Marek Hrabe <marekhrabe@me.com>
2019-06-06 18:00:00 +0200

    add auth_callback for the meta field

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/class-starter-page-templates.php
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/class-starter-page-templates.php
@@ -62,8 +62,18 @@ class Starter_Page_Templates {
 			'single'         => true,
 			'show_in_rest'   => true,
 			'object_subtype' => 'page',
+			'auth_callback'  => array( $this, 'meta_field_auth_callback' ),
 		);
 		register_meta( 'post', '_starter_page_template', $args );
+	}
+
+	/**
+	 * Allow meta field updates for users able to edit posts.
+	 *
+	 * @return boolean
+	 */
+	public function meta_field_auth_callback() {
+		return current_user_can( 'edit_posts' );
 	}
 
 	/**
