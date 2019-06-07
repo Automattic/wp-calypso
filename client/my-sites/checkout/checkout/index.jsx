ceb7092f5878973f4a2f904ef40b79e0e2239bd4
Jon Surrell <jon.surrell@automattic.com>
2019-06-07 13:14:39 +0200

    Remove isEnabled( 'jetpack/checklist' ) check

--- client/my-sites/checkout/checkout/index.jsx
+++ client/my-sites/checkout/checkout/index.jsx
@@ -452,7 +452,7 @@ export class Checkout extends React.Component {
 			return `/${ destination }/${ selectedSiteSlug }${ queryParam }`;
 		}
 
-		if ( this.props.isJetpackNotAtomic && config.isEnabled( 'jetpack/checklist' ) ) {
+		if ( this.props.isJetpackNotAtomic ) {
 			return `/plans/my-plan/${ selectedSiteSlug }?thank-you`;
 		}
 
