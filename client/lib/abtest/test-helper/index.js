e9a2c9351998d68f1dcf9044ce41b0e1a38eea98
Ben Lowery <blowery@users.noreply.github.com>
2019-06-10 08:58:45 -0400

    Migrate CSS, modernize abtest-helper (#33684)

--- client/lib/abtest/test-helper/index.js
+++ client/lib/abtest/test-helper/index.js
@@ -1,4 +1,6 @@
-/** @format */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 
 /**
  * External dependencies
@@ -7,14 +9,70 @@
 import ReactDom from 'react-dom';
 import React from 'react';
 import Debug from 'debug';
+import classNames from 'classnames';
 /**
  * Internal dependencies
  */
-import TestList from './TestList';
+import Card from 'components/card';
 import { getAllTests } from 'lib/abtest';
 import { ABTEST_LOCALSTORAGE_KEY } from 'lib/abtest/utility';
 
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
 const debug = Debug( 'calypso:abtests:helper' );
+
+class Test extends React.Component {
+	static displayName = 'Test';
+
+	changeVariant = variation => {
+		this.props.onChangeVariant( this.props.test, variation );
+	};
+
+	render() {
+		const currentVariation = this.props.test.getVariation();
+		return (
+			<div>
+				<h5 className="test-helper__test-header">{ this.props.test.name }</h5>
+				<ul className="test-helper__list">
+					{ this.props.test.variationNames.map( variation => (
+						<li onClick={ this.changeVariant.bind( this, variation ) } key={ variation }>
+							<a
+								className={ classNames( {
+									'test-helper__variation': true,
+									'test-helper__current-variation': variation === currentVariation,
+								} ) }
+							>
+								<input
+									className="test-helper__choice-indicator"
+									type="radio"
+									checked={ variation === currentVariation }
+									readOnly
+								/>
+								{ variation }
+							</a>
+						</li>
+					) ) }
+				</ul>
+			</div>
+		);
+	}
+}
+
+const TestList = ( { tests, onChangeVariant } ) => (
+	<div>
+		<a href={ '/devdocs/client/lib/abtest/README.md' } title="ABTests">
+			ABTests
+		</a>
+		<Card className="test-helper__active-tests">
+			{ tests.map( test => (
+				<Test key={ test.name } test={ test } onChangeVariant={ onChangeVariant } />
+			) ) }
+		</Card>
+	</div>
+);
 
 export default function injectTestHelper( element ) {
 	ReactDom.render(
