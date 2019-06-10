2037f9388bc090fa9542bc58615c749423ed1cf0
Jarda Snajdr <jsnajdr@gmail.com>
2019-06-10 10:37:10 +0200

    Posts: remove warning notice about minimum Jetpack version

--- client/my-sites/posts/main.jsx
+++ client/my-sites/posts/main.jsx
@@ -16,16 +16,9 @@ import SidebarNavigation from 'my-sites/sidebar-navigation';
 import PostTypeList from 'my-sites/post-type-list';
 import PostTypeBulkEditBar from 'my-sites/post-type-list/bulk-edit-bar';
 import titlecase from 'to-title-case';
-import config from 'config';
 import Main from 'components/main';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { mapPostStatus as mapStatus } from 'lib/route';
-import { warningNotice } from 'state/notices/actions';
-import {
-	getSiteAdminUrl,
-	isJetpackSite,
-	siteHasMinimumJetpackVersion,
-} from 'state/sites/selectors';
 
 /**
  * Style dependencies
@@ -33,19 +26,6 @@ import {
 import './style.scss';
 
 class PostsMain extends React.Component {
-	UNSAFE_componentWillMount() {
-		this.setWarning( this.props );
-	}
-
-	UNSAFE_componentWillReceiveProps( nextProps ) {
-		if (
-			nextProps.siteId !== this.props.siteId ||
-			nextProps.hasMinimumJetpackVersion !== this.props.hasMinimumJetpackVersion
-		) {
-			this.setWarning( nextProps );
-		}
-	}
-
 	getAnalyticsPath() {
 		const { siteId, statusSlug, author } = this.props;
 		let analyticsPath = '/posts';
@@ -106,39 +86,8 @@ class PostsMain extends React.Component {
 			</Main>
 		);
 	}
-
-	setWarning = ( { adminUrl, hasMinimumJetpackVersion, isJetpack, siteId } ) => {
-		if ( siteId && isJetpack && false === hasMinimumJetpackVersion ) {
-			this.props.warningNotice(
-				this.props.translate(
-					'Jetpack %(version)s is required to take full advantage of all post editing features.',
-					{
-						args: { version: config( 'jetpack_min_version' ) },
-					}
-				),
-				{
-					button: this.props.translate( 'Update now' ),
-					href: adminUrl,
-				}
-			);
-		}
-	};
 }
 
-function mapStateToProps( state ) {
-	const siteId = getSelectedSiteId( state );
-
-	return {
-		adminUrl: getSiteAdminUrl( state, siteId, 'plugins.php?plugin_status=upgrade' ),
-		hasMinimumJetpackVersion: siteHasMinimumJetpackVersion( state, siteId ),
-		isJetpack: isJetpackSite( state, siteId ),
-		siteId,
-	};
-}
-
-export default connect(
-	mapStateToProps,
-	{
-		warningNotice,
-	}
-)( localize( PostsMain ) );
+export default connect( state => ( {
+	siteId: getSelectedSiteId( state ),
+} ) )( localize( PostsMain ) );
