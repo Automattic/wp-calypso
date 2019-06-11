428fa759d9ae2ef93f33b75250d36cccf3afebc7
Marko Andrijasevic <markoandrijasevic@gmail.com>
2019-06-11 17:25:28 +0200

    Remove template selector sidebar

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/index.js
@@ -7,6 +7,5 @@ import './blocks/site-description';
 import './blocks/site-title';
 import './blocks/template';
 import './blocks/site-logo';
-import './plugins/template-selector-sidebar';
 import './plugins/close-button-override';
 import './plugins/template-update-confirmation';
