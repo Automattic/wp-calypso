db6d0fb6bb8f2cd1069c13dafca41bf661e9e475
Joshua Wold <joshua@joshuawold.com>
2019-06-12 14:11:19 -0700

    Removing logic that dumps customers to WP admin
    Currently there's logic that will send non US/Canada business plan customers directly to the WP-admin when they begin onboarding. They're sent without a warning that they'll be in a new context.
    
    However, currently US/Canada customres get an appropriate notice letting them know what's about to happen.
    
    This fix removes the logic and makes it so that all customers (regardless of location) are presented with the notice and told about the switch the WP-admin.
    
    In order to pass pre-commit hook I needed to comment out other lines of code as well. I can't tell if they are causing conflicts elsewhere so I absolutely would welcome a review on this. Thank you!

--- client/extensions/woocommerce/app/dashboard/setup/store-location.js
+++ client/extensions/woocommerce/app/dashboard/setup/store-location.js
@@ -22,7 +22,9 @@ import {
 import { bumpStat } from 'woocommerce/lib/analytics';
 import { errorNotice } from 'state/notices/actions';
 import getContactDetailsCache from 'state/selectors/get-contact-details-cache';
-import { isStoreManagementSupportedInCalypsoForCountry } from 'woocommerce/lib/countries';
+// Commenting out for now because country logic for dumping to wp-admin is being removoed
+// remove this code if it doesn't cause conflicts.
+//import { isStoreManagementSupportedInCalypsoForCountry } from 'woocommerce/lib/countries';
 import {
 	areLocationsLoaded,
 	getAllCountries,
@@ -153,10 +155,14 @@ class StoreLocationSetupView extends Component {
 
 	onNext = event => {
 		const {
-			adminURL,
+			// Commenting out for now, remove if no conflict with removing logic
+			// elsewhere in file
+			//adminURL,
 			countries,
 			currentUserEmailVerified,
-			onRequestRedirect,
+			// Commenting out for now, remove if no conflict with removing logic
+			// elsewhere in file
+			//onRequestRedirect,
 			pushDefaultsForCountry,
 			siteId,
 			translate,
@@ -186,14 +192,17 @@ class StoreLocationSetupView extends Component {
 			// mc stat 32 char max :P
 			this.props.bumpStat( 'calypso_woo_store_setup_country', this.state.address.country );
 
-			// If we don't support a calypso experience yet for this country, let
+			// Original: If we don't support a calypso experience yet for this country, let
 			// them complete setup with the wp-admin WooCommerce wizard
+			// Revised: Removing functionality due to no longer being needed. Commenting out for now
+			// to ensure that the dependencies in the same file are ok to also be commented out.
+			/*
 			if ( ! isStoreManagementSupportedInCalypsoForCountry( this.state.address.country ) ) {
 				const storeSetupURL =
 					adminURL + 'admin.php?page=wc-setup&step=store_setup&activate_error=false&from=calypso';
 				onRequestRedirect( storeSetupURL );
 			}
-
+			*/
 			return setSetStoreAddressDuringInitialSetup( siteId, true );
 		};
 
