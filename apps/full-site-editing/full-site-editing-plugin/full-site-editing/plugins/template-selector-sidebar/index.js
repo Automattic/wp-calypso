12f155eb89e49538a122d364cb8765cde123f820
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2019-06-12 19:32:43 -0300

    Adds effect to the template selector sidebar to react to template changes

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/template-selector-sidebar/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/template-selector-sidebar/index.js
@@ -2,11 +2,13 @@
 /**
  * External dependencies
  */
+import { useEffect, useState } from 'react';
 import { get } from 'lodash';
 
 /**
  * WordPress dependencies
  */
+import { createBlock, parse } from '@wordpress/blocks';
 import { PanelBody } from '@wordpress/components';
 import { compose } from '@wordpress/compose';
 import { withDispatch, withSelect } from '@wordpress/data';
@@ -24,45 +26,83 @@ const TemplateSelectorSidebar = compose(
 	withDispatch( dispatch => ( {
 		setTemplateId: templateId =>
 			dispatch( 'core/editor' ).editPost( { meta: { _wp_template_id: templateId } } ),
+		resetBlocks: dispatch( 'core/block-editor' ).resetBlocks,
 	} ) ),
 	withSelect( select => {
 		const { canUser, getEntityRecord } = select( 'core' );
-		const templateId = get(
-			select( 'core/editor' ).getEditedPostAttribute( 'meta' ),
-			'_wp_template_id'
-		);
+		const { getBlocks, getEditedPostAttribute } = select( 'core/editor' );
+		const templateId = get( getEditedPostAttribute( 'meta' ), '_wp_template_id' );
+
 		return {
 			canUserUpdateSettings: canUser( 'update', 'settings' ),
+			contentBlocks: getBlocks(),
 			selectedTemplate: templateId && getEntityRecord( 'postType', 'wp_template', templateId ),
+			templateId,
 		};
 	} )
-)( ( { canUserUpdateSettings, setTemplateId, selectedTemplate } ) => {
-	if ( ! canUserUpdateSettings ) {
-		return null;
+)(
+	( {
+		canUserUpdateSettings,
+		contentBlocks,
+		resetBlocks,
+		setTemplateId,
+		selectedTemplate,
+		templateId,
+	} ) => {
+		if ( ! canUserUpdateSettings ) {
+			return null;
+		}
+
+		useEffect( () => {
+			const [ templateMerged, setTemplateMerged ] = useState( false );
+
+			if ( ! selectedTemplate || templateMerged ) {
+				return;
+			}
+
+			const templateBlocks = parse( get( selectedTemplate, [ 'content', 'raw' ] ) );
+
+			const mergedBlocks = templateBlocks.map( ( { name, attributes } ) => {
+				if ( 'a8c/post-content' === name ) {
+					return createBlock(
+						'a8c/post-content', //for clarity
+						attributes,
+						contentBlocks.map( contentBlock =>
+							createBlock( contentBlock.name, contentBlock.attributes )
+						)
+					);
+				}
+
+				return createBlock( name, attributes );
+			} );
+
+			resetBlocks( mergedBlocks );
+			setTemplateMerged( true );
+		}, [ templateId ] );
+
+		const onSelectTemplate = ( { id } ) => {
+			setTemplateId( parseInt( id, 10 ) );
+		};
+
+		return (
+			<Fragment>
+				<PluginSidebarMoreMenuItem target="fse-template-sidebar" icon="layout">
+					{ __( 'Template' ) }
+				</PluginSidebarMoreMenuItem>
+				<PluginSidebar icon="layout" name="fse-template-sidebar" title={ __( 'Template' ) }>
+					<PanelBody>
+						{ __( 'Select a template' ) }
+						<PostAutocomplete
+							initialValue={ get( selectedTemplate, [ 'title', 'rendered' ] ) }
+							onSelectPost={ onSelectTemplate }
+							postType="wp_template"
+						/>
+					</PanelBody>
+				</PluginSidebar>
+			</Fragment>
+		);
 	}
-
-	const onSelectTemplate = ( { id } ) => {
-		setTemplateId( parseInt( id, 10 ) );
-	};
-
-	return (
-		<Fragment>
-			<PluginSidebarMoreMenuItem target="fse-template-sidebar" icon="layout">
-				{ __( 'Template' ) }
-			</PluginSidebarMoreMenuItem>
-			<PluginSidebar icon="layout" name="fse-template-sidebar" title={ __( 'Template' ) }>
-				<PanelBody>
-					{ __( 'Select a template' ) }
-					<PostAutocomplete
-						initialValue={ get( selectedTemplate, [ 'title', 'rendered' ] ) }
-						onSelectPost={ onSelectTemplate }
-						postType="wp_template"
-					/>
-				</PanelBody>
-			</PluginSidebar>
-		</Fragment>
-	);
-} );
+);
 
 if ( 'page' === fullSiteEditing.editorPostType ) {
 	registerPlugin( 'fse-template-selector-sidebar', {
