023b592bc034f42981f016657b7cc4167bae53e8
Marek Hrabe <marekhrabe@me.com>
2019-06-13 11:28:06 +0200

    FSE: Add tracks identity (#33877)
    * js handling of identity
    
    * load tracks script, prepare identity object
    
    * pass identity to js
    
    * phpcbf
    
    * fix nbsp

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/utils/tracking.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/utils/tracking.js
@@ -1,26 +1,52 @@
 // Ensure Tracks Library
 window._tkq = window._tkq || [];
 
+let tracksIdentity = null;
+
+export const initializeWithIdentity = identity => {
+	tracksIdentity = identity;
+	window._tkq.push( [ 'identifyUser', identity.userid, identity.username ] );
+};
+
 export const trackView = vertical_id => {
+	if ( ! tracksIdentity ) {
+		return;
+	}
 	window._tkq.push( [
 		'recordEvent',
 		'a8c_test_full_site_editing_template_selector_view',
-		{ vertical_id },
+		{
+			blog_id: tracksIdentity.blogid,
+			vertical_id,
+		},
 	] );
 };
 
 export const trackDismiss = vertical_id => {
+	if ( ! tracksIdentity ) {
+		return;
+	}
 	window._tkq.push( [
 		'recordEvent',
 		'a8c_test_full_site_editing_template_selector_dismiss',
-		{ vertical_id },
+		{
+			blog_id: tracksIdentity.blogid,
+			vertical_id,
+		},
 	] );
 };
 
 export const trackSelection = ( vertical_id, template ) => {
+	if ( ! tracksIdentity ) {
+		return;
+	}
 	window._tkq.push( [
 		'recordEvent',
 		'a8c_test_full_site_editing_template_selector_template_selected',
-		{ vertical_id, template },
+		{
+			blog_id: tracksIdentity.blogid,
+			vertical_id,
+			template,
+		},
 	] );
 };
