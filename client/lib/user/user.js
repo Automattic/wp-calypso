0a048489ea9adc879e13d3b3913a1ed035c6ff30
Sérgio Gomes <mail@sgomes.com>
2019-06-13 14:28:19 +0100

    Fix typo in user.js

--- client/lib/user/user.js
+++ client/lib/user/user.js
@@ -260,7 +260,7 @@ User.prototype.clear = function( onClear ) {
 	delete this.settings;
 	store.clearAll();
 	if ( config.isEnabled( 'persist-redux' ) ) {
-		clearStorage.then( onClear );
+		clearStorage().then( onClear );
 	} else if ( onClear ) {
 		onClear();
 	}
