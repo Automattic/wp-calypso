9b2dbef70864da67d4ffb5733239922c5e25f417
Marko Andrijasevic <markoandrijasevic@gmail.com>
2019-06-14 18:15:41 +0200

    Full Site Editing: override close button URL for Template Part CPTs

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/index.js
@@ -5,3 +5,4 @@ import './blocks/navigation-menu';
 import './blocks/post-content';
 import './blocks/template';
 import './plugins/template-selector-sidebar';
+import './plugins/close-button-override';
