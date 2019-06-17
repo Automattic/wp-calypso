0c2e96a69240f9b39262402820c980c07524416d
Jarda Snajdr <jsnajdr@gmail.com>
2019-06-17 10:07:46 +0200

    Posts: remove legacy and unused code from the main component (#33788)
    * Remove unused StatUpdateIndicator and PostTotalViews components
    
    * Posts: remove warning notice about minimum Jetpack version
    
    * Remove localize HOC from PostsMain: nothing to translate there
    
    * Posts: remove the unneeded posts__primary div
    
    * Posts: remove unused extra CSS classes from main.posts
    
    * Posts: import mapPostStatus with original name

--- client/my-sites/posts/main.jsx
+++ client/my-sites/posts/main.jsx
@@ -4,8 +4,6 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-import classnames from 'classnames';
-import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -16,16 +14,9 @@ import SidebarNavigation from 'my-sites/sidebar-navigation';
 import PostTypeList from 'my-sites/post-type-list';
 import PostTypeBulkEditBar from 'my-sites/post-type-list/bulk-edit-bar';
 import titlecase from 'to-title-case';
-import config from 'config';
 import Main from 'components/main';
 import { getSelectedSiteId } from 'state/ui/selectors';
-import { mapPostStatus as mapStatus } from 'lib/route';
-import { warningNotice } from 'state/notices/actions';
-import {
-	getSiteAdminUrl,
-	isJetpackSite,
-	siteHasMinimumJetpackVersion,
-} from 'state/sites/selectors';
+import { mapPostStatus } from 'lib/route';
 
 /**
  * Style dependencies
@@ -33,19 +24,6 @@ import {
 import './style.scss';
 
 class PostsMain extends React.Component {
-	UNSAFE_componentWillMount() {
-		this.setWarning( this.props );
-	}
-
-	UNSAFE_componentWillReceiveProps( nextProps ) {
-		if (
-			nextProps.siteId !== this.props.siteId ||
-			nextProps.hasMinimumJetpackVersion !== this.props.hasMinimumJetpackVersion
-		) {
-			this.setWarning( nextProps );
-		}
-	}
-
 	getAnalyticsPath() {
 		const { siteId, statusSlug, author } = this.props;
 		let analyticsPath = '/posts';
@@ -77,11 +55,7 @@ class PostsMain extends React.Component {
 
 	render() {
 		const { author, category, search, siteId, statusSlug, tag } = this.props;
-		const classes = classnames( 'posts', {
-			'is-multisite': ! this.props.siteId,
-			'is-single-site': this.props.siteId,
-		} );
-		const status = mapStatus( statusSlug );
+		const status = mapPostStatus( statusSlug );
 		const query = {
 			author,
 			category,
@@ -95,50 +69,17 @@ class PostsMain extends React.Component {
 		};
 
 		return (
-			<Main className={ classes }>
+			<Main className="posts">
 				<PageViewTracker path={ this.getAnalyticsPath() } title={ this.getAnalyticsTitle() } />
 				<SidebarNavigation />
-				<div className="posts__primary">
-					<PostTypeFilter query={ query } siteId={ siteId } statusSlug={ statusSlug } />
-					{ siteId && <PostTypeBulkEditBar /> }
-					<PostTypeList query={ query } scrollContainer={ document.body } />
-				</div>
+				<PostTypeFilter query={ query } siteId={ siteId } statusSlug={ statusSlug } />
+				{ siteId && <PostTypeBulkEditBar /> }
+				<PostTypeList query={ query } scrollContainer={ document.body } />
 			</Main>
 		);
 	}
-
-	setWarning = ( { adminUrl, hasMinimumJetpackVersion, isJetpack, siteId } ) => {
-		if ( siteId && isJetpack && false === hasMinimumJetpackVersion ) {
-			this.props.warningNotice(
-				this.props.translate(
-					'Jetpack %(version)s is required to take full advantage of all post editing features.',
-					{
-						args: { version: config( 'jetpack_min_version' ) },
-					}
-				),
-				{
-					button: this.props.translate( 'Update now' ),
-					href: adminUrl,
-				}
-			);
-		}
-	};
 }
 
-function mapStateToProps( state ) {
-	const siteId = getSelectedSiteId( state );
-
-	return {
-		adminUrl: getSiteAdminUrl( state, siteId, 'plugins.php?plugin_status=upgrade' ),
-		hasMinimumJetpackVersion: siteHasMinimumJetpackVersion( state, siteId ),
-		isJetpack: isJetpackSite( state, siteId ),
-		siteId,
-	};
-}
-
-export default connect(
-	mapStateToProps,
-	{
-		warningNotice,
-	}
-)( localize( PostsMain ) );
+export default connect( state => ( {
+	siteId: getSelectedSiteId( state ),
+} ) )( PostsMain );
