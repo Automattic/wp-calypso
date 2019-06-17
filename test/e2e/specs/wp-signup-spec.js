9d2583a04cfcaf1467c0e3fe9bd50017ab32ccb1
Grant Kinney <creativecoder@users.noreply.github.com>
2019-06-17 11:08:28 -0500

    Enables import signup e2e test, now that import system is working again (#34042)

--- test/e2e/specs/wp-signup-spec.js
+++ test/e2e/specs/wp-signup-spec.js
@@ -1513,8 +1513,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		} );
 	} );
 
-	describe.skip( 'Import a site while signing up @parallel', function() {
-		// Temporarily skip https://github.com/Automattic/wp-calypso/pull/34021#issuecomment-502487953
+	describe( 'Import a site while signing up @parallel', function() {
 		// Currently must use a Wix site to be importable through this flow.
 		const siteURL = 'https://hi6822.wixsite.com/eat-here-its-good';
 		const userName = dataHelper.getNewBlogName();
