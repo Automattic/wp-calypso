75e3b39feb54be24733f10639f03d265a262ef8d
Jon Surrell <jon.surrell@automattic.com>
2019-06-20 11:40:37 +0200

    Use named exports/imports from plans controller

--- client/my-sites/plans/controller.jsx
+++ client/my-sites/plans/controller.jsx
@@ -12,49 +12,47 @@ import CheckoutData from 'components/data/checkout';
 import Plans from './plans';
 import { isValidFeatureKey } from 'lib/plans/features-list';
 
-export default {
-	plans( context, next ) {
-		context.primary = (
-			<CheckoutData>
-				<Plans
-					context={ context }
-					intervalType={ context.params.intervalType }
-					customerType={ context.query.customerType }
-					selectedFeature={ context.query.feature }
-					selectedPlan={ context.query.plan }
-					withDiscount={ context.query.discount }
-					discountEndDate={ context.query.ts }
-				/>
-			</CheckoutData>
-		);
-		next();
-	},
+export function plans( context, next ) {
+	context.primary = (
+		<CheckoutData>
+			<Plans
+				context={ context }
+				intervalType={ context.params.intervalType }
+				customerType={ context.query.customerType }
+				selectedFeature={ context.query.feature }
+				selectedPlan={ context.query.plan }
+				withDiscount={ context.query.discount }
+				discountEndDate={ context.query.ts }
+			/>
+		</CheckoutData>
+	);
+	next();
+}
 
-	features( context ) {
-		const domain = context.params.domain;
-		const feature = get( context, 'params.feature' );
-		let comparePath = domain ? `/plans/${ domain }` : '/plans/';
+export function features( context ) {
+	const domain = context.params.domain;
+	const feature = get( context, 'params.feature' );
+	let comparePath = domain ? `/plans/${ domain }` : '/plans/';
 
-		if ( isValidFeatureKey( feature ) ) {
-			comparePath += '?feature=' + feature;
-		}
+	if ( isValidFeatureKey( feature ) ) {
+		comparePath += '?feature=' + feature;
+	}
 
-		// otherwise redirect to the compare page if not found
-		page.redirect( comparePath );
-	},
+	// otherwise redirect to the compare page if not found
+	page.redirect( comparePath );
+}
 
-	redirectToCheckout( context ) {
-		// this route is deprecated, use `/checkout/:site/:plan` to link to plan checkout
-		page.redirect( `/checkout/${ context.params.domain }/${ context.params.plan }` );
-	},
+export function redirectToCheckout( context ) {
+	// this route is deprecated, use `/checkout/:site/:plan` to link to plan checkout
+	page.redirect( `/checkout/${ context.params.domain }/${ context.params.plan }` );
+}
 
-	redirectToPlans( context ) {
-		const siteDomain = context.params.domain;
+export function redirectToPlans( context ) {
+	const siteDomain = context.params.domain;
 
-		if ( siteDomain ) {
-			return page.redirect( `/plans/${ siteDomain }` );
-		}
+	if ( siteDomain ) {
+		return page.redirect( `/plans/${ siteDomain }` );
+	}
 
-		return page.redirect( '/plans' );
-	},
-};
+	return page.redirect( '/plans' );
+}
