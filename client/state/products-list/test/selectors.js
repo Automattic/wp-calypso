e411535e8b09f86efdfc62740c6872bdd9158d12
Veselin Nikolov <veselin@automattic.com>
2019-06-21 11:14:07 +0300

    Add tests for the selectors

--- client/state/products-list/test/selectors.js
+++ client/state/products-list/test/selectors.js
@@ -146,6 +146,16 @@ describe( 'selectors', () => {
 				priceMonthly: 10,
 			} );
 		} );
+
+		test( 'Should return proper priceFinal if couponDiscounts are provided', () => {
+			const plan = { getStoreSlug: () => 'abc', getProductId: () => 'def' };
+			expect( computeFullAndMonthlyPricesForPlan( {}, 1, plan, 0, { def: 60 } ) ).toEqual( {
+				priceFullBeforeDiscount: 150,
+				priceFull: 120,
+				priceFinal: 60,
+				priceMonthly: 10, // The monthly price is without discounts applied
+			} );
+		} );
 	} );
 
 	describe( '#computeProductsWithPrices()', () => {
@@ -205,6 +215,40 @@ describe( 'selectors', () => {
 					priceFullBeforeDiscount: 150,
 					priceFull: 240,
 					priceFinal: 240,
+					priceMonthly: 20,
+				},
+			] );
+		} );
+
+		test( 'couponDiscount should discount priceFinal', () => {
+			const state = {
+				productsList: {
+					items: {
+						plan1: { available: true },
+						plan2: { available: true },
+					},
+				},
+			};
+
+			expect(
+				computeProductsWithPrices( state, 10, [ 'plan1', 'plan2' ], 0, { def: 60, mno: 120 } )
+			).toEqual( [
+				{
+					planSlug: 'plan1',
+					plan: testPlans.plan1,
+					product: state.productsList.items.plan1,
+					priceFullBeforeDiscount: 150,
+					priceFull: 120,
+					priceFinal: 60,
+					priceMonthly: 10,
+				},
+				{
+					planSlug: 'plan2',
+					plan: testPlans.plan2,
+					product: state.productsList.items.plan2,
+					priceFullBeforeDiscount: 150,
+					priceFull: 240,
+					priceFinal: 120,
 					priceMonthly: 20,
 				},
 			] );
