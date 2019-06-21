69ca5cb4dc8abbedc4d710cd7c07ca9118a3caed
Jon Surrell <jon.surrell@automattic.com>
2019-06-21 09:49:53 +0200

    Fix getCurrentQueryArguments selector types

--- client/state/selectors/get-current-query-arguments.js
+++ client/state/selectors/get-current-query-arguments.js
@@ -1,20 +1,18 @@
-/** @format */
-
 /**
  * External dependencies
  */
-
 import { get } from 'lodash';
 
 /**
- * Internal dependencies
+ * @typedef {import('querystring').ParsedUrlQuery} Query
  */
 
 /**
  * Gets the last query parameters set by a ROUTE_SET action
  * @param {Object} state - global redux state
- * @return {Object} current state value
+ * @return {(Query|false)} Initial state is false, otherwise this should be a query object
  */
-export const getCurrentQueryArguments = state => get( state, 'ui.route.query.current', null );
+export const getCurrentQueryArguments = state =>
+	get( state, [ 'ui', 'route', 'query', 'current' ] );
 
 export default getCurrentQueryArguments;
