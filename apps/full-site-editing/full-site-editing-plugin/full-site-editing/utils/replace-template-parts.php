d2956ec97c48347633e6720c21bd306d22d726cf
mmtr <miguel.torres@automattic.com>
2019-06-21 13:57:52 +0900

    FSE: Render dynamic blocks on page areas replaced by template parts

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/utils/replace-template-parts.php
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/utils/replace-template-parts.php
@@ -25,12 +25,12 @@ function a8c_fse_replace_template_parts( $html ) {
 		'header' => [
 			// Query the first <header> element that contains the 'site-header' class.
 			// Note that this is not always the direct descendant of <body> since it's sometimes wrapped in a <div>.
-			'xpath_query' => "(/html/body//header[@class='site-header'])[1]",
+			'xpath_query' => "(//header[@class='site-header'])[1]",
 			'fse_content' => $page_template->get_header_content(),
 		],
 		'footer' => [
 			// Query the first <footer> element that contains the 'site-footer' class.
-			'xpath_query' => "(/html/body//footer[@class='site-footer'])[1]",
+			'xpath_query' => "(//footer[@class='site-footer'])[1]",
 			'fse_content' => $page_template->get_footer_content(),
 		],
 	];
@@ -39,7 +39,14 @@ function a8c_fse_replace_template_parts( $html ) {
 	// phpcs:disable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 	$doc->preserveWhiteSpace = false;
 	// phpcs:disable WordPress.PHP.NoSilencedErrors.Discouraged
-	@$doc->loadHTML( mb_convert_encoding( $html, 'HTML-ENTITIES', 'UTF-8' ) );
+
+	$encoded_html = mb_convert_encoding( $html, 'HTML-ENTITIES', 'UTF-8' );
+
+	// We wrap the given html in a div, since LibXML requires a root node.
+	$wrapped_html = '<div>' . $encoded_html . '</div>';
+
+	// LIBXML_HTML_NOIMPLIED keeps the HTML intact, without removing the blocks comments markup.
+	@$doc->loadHTML( $wrapped_html, LIBXML_HTML_NOIMPLIED );
 
 	$temp_doc                     = new DOMDocument();
 	$temp_doc->preserveWhiteSpace = false;
@@ -58,11 +65,27 @@ function a8c_fse_replace_template_parts( $html ) {
 
 		$node_to_replace = $candidate_nodes[0];
 
-		@$temp_doc->loadHTML( mb_convert_encoding( $replacement['fse_content'], 'HTML-ENTITIES', 'UTF-8' ) );
+		$encoded_fse_content = mb_convert_encoding( $replacement['fse_content'], 'HTML-ENTITIES', 'UTF-8' );
+
+		// We wrap the FSE html content in a div, since LibXML requires a root node.
+		$wrapped_fse_content = '<div>' . $encoded_fse_content . '</div>';
+
+		// LIBXML_HTML_NOIMPLIED keeps the HTML intact, without adding html, body or other unnecessary tags in a single node.
+		@$temp_doc->loadHTML( $wrapped_fse_content, LIBXML_HTML_NOIMPLIED );
 
 		$fse_node = $doc->importNode( $temp_doc->documentElement, true );
-		$node_to_replace->parentNode->replaceChild( $fse_node, $node_to_replace );
+
+		// Remove all the children of the element to be replaced in order to keep using the same container for the FSE content.
+		while ( $node_to_replace->hasChildNodes() ) {
+			$node_to_replace->removeChild( $node_to_replace->firstChild );
+		}
+
+		// Rather than appending the FSE content node, we add its child nodes so the extra wrapping div added above is not rendered.
+		for ( $i = 0; $i < $fse_node->childNodes->length; $i++ ) {
+			$fse_child_node = $fse_node->childNodes->item( $i );
+			$node_to_replace->appendChild( $fse_child_node );
+		}
 	}
 
-	return $doc->saveHTML();
+	return do_blocks( $doc->saveHTML() );
 }
