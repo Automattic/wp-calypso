f7baa6ec961bd29728594fcb9b550a0121c44dd7
Sérgio Gomes <mail@sgomes.com>
2019-06-24 18:38:19 +0100

    Detect broken IndexedDB implementations.
    Falls back to localStorage if one is detected.

--- client/lib/browser-storage/index.ts
+++ client/lib/browser-storage/index.ts
@@ -15,18 +15,99 @@ const DB_NAME = 'calypso';
 const DB_VERSION = 2; // Match versioning of the previous localforage-based implementation.
 const STORE_NAME = 'calypso_store';
 
-const supportsIDB = typeof window !== 'undefined' && window.indexedDB;
+const SANITY_TEST_KEY = 'browser-storage-sanity-test';
 
 const getDB = memoizeLast( () => {
 	const request = indexedDB.open( DB_NAME, DB_VERSION );
 	return new Promise< IDBDatabase >( ( resolve, reject ) => {
-		if ( request ) {
-			request.onerror = () => reject( request.error );
-			request.onsuccess = () => resolve( request.result );
-			request.onupgradeneeded = () => request.result.createObjectStore( STORE_NAME );
+		try {
+			if ( request ) {
+				request.onerror = event => {
+					// InvalidStateError is special in Firefox.
+					// We need to `preventDefault` to stop it from reaching the console.
+					if ( request.error && request.error.name === 'InvalidStateError' ) {
+						event.preventDefault();
+					}
+					reject( request.error );
+				};
+				request.onsuccess = () => resolve( request.result );
+				request.onupgradeneeded = () => request.result.createObjectStore( STORE_NAME );
+			}
+		} catch ( error ) {
+			reject( error );
 		}
 	} );
 } ) as () => Promise< IDBDatabase >;
+
+const supportsIDB = memoizeLast( async () => {
+	if ( typeof window === 'undefined' || ! window.indexedDB ) {
+		return Promise.resolve( false );
+	}
+
+	try {
+		const testValue = Date.now().toString();
+		await idbSet( SANITY_TEST_KEY, testValue );
+		await idbGet( SANITY_TEST_KEY );
+		return true;
+	} catch ( error ) {
+		// IDB sanity test failed. Fall back to alternative method.
+		return false;
+	}
+} ) as () => Promise< boolean >;
+
+function idbGet< T >( key: string ): Promise< T | undefined > {
+	return new Promise( ( resolve, reject ) => {
+		getDB()
+			.then( db => {
+				const transaction = db.transaction( STORE_NAME, 'readonly' );
+				const get = transaction.objectStore( STORE_NAME ).get( key );
+
+				const success = () => resolve( get.result );
+				const error = () => reject( transaction.error );
+
+				transaction.oncomplete = success;
+				transaction.onabort = error;
+				transaction.onerror = error;
+			} )
+			.catch( err => reject( err ) );
+	} );
+}
+
+function idbSet< T >( key: string, value: T ): Promise< void > {
+	return new Promise( ( resolve, reject ) => {
+		getDB()
+			.then( db => {
+				const transaction = db.transaction( STORE_NAME, 'readwrite' );
+				transaction.objectStore( STORE_NAME ).put( value, key );
+
+				const success = () => resolve();
+				const error = () => reject( transaction.error );
+
+				transaction.oncomplete = success;
+				transaction.onabort = error;
+				transaction.onerror = error;
+			} )
+			.catch( err => reject( err ) );
+	} );
+}
+
+function idbClear(): Promise< void > {
+	return new Promise( ( resolve, reject ) => {
+		getDB()
+			.then( db => {
+				const transaction = db.transaction( STORE_NAME, 'readwrite' );
+				transaction.objectStore( STORE_NAME ).clear();
+
+				const success = () => resolve();
+				const error = () => reject( transaction.error );
+
+				transaction.oncomplete = success;
+				transaction.onabort = error;
+				transaction.onerror = error;
+			} )
+			.catch( err => reject( err ) );
+	} );
+}
 
 /**
  * Whether persistent storage should be bypassed, using a memory store instead.
@@ -47,37 +128,22 @@ export function bypassPersistentStorage( shouldBypassPersistentStorage: boolean 
  * @param key The stored item key.
  * @return A promise with the stored value. `undefined` if missing.
  */
-export function getStoredItem< T >( key: string ): Promise< T | undefined > {
+export async function getStoredItem< T >( key: string ): Promise< T | undefined > {
 	if ( shouldBypass ) {
-		return bypassGet( key );
+		return await bypassGet( key );
 	}
 
-	if ( ! supportsIDB ) {
+	const idbSupported = await supportsIDB();
+	if ( ! idbSupported ) {
 		const valueString = localStorage.getItem( key );
-		try {
-			if ( valueString === undefined || valueString === null ) {
-				return Promise.resolve( undefined );
-			}
-
-			return Promise.resolve( JSON.parse( valueString ) );
-		} catch ( error ) {
-			return Promise.reject( error );
+		if ( valueString === undefined || valueString === null ) {
+			return undefined;
 		}
+
+		return JSON.parse( valueString );
 	}
 
-	return new Promise( ( resolve, reject ) => {
-		getDB().then( db => {
-			const transaction = db.transaction( STORE_NAME, 'readonly' );
-			const get = transaction.objectStore( STORE_NAME ).get( key );
-
-			const success = () => resolve( get.result );
-			const error = () => reject( transaction.error );
-
-			transaction.oncomplete = success;
-			transaction.onabort = error;
-			transaction.onerror = error;
-		} );
-	} );
+	return await idbGet( key );
 }
 
 /**
@@ -87,29 +153,18 @@ export function getStoredItem< T >( key: string ): Promise< T | undefined > {
  * @param value The value of the item to be stored.
  * @return A promise that gets resolved when the item is successfully stored.
  */
-export function setStoredItem< T >( key: string, value: T ): Promise< void > {
+export async function setStoredItem< T >( key: string, value: T ): Promise< void > {
 	if ( shouldBypass ) {
-		return bypassSet( key, value );
+		return await bypassSet( key, value );
 	}
 
-	if ( ! supportsIDB ) {
+	const idbSupported = await supportsIDB();
+	if ( ! idbSupported ) {
 		localStorage.setItem( key, JSON.stringify( value ) );
-		return Promise.resolve();
+		return;
 	}
 
-	return new Promise( ( resolve, reject ) => {
-		getDB().then( db => {
-			const transaction = db.transaction( STORE_NAME, 'readwrite' );
-			transaction.objectStore( STORE_NAME ).put( value, key );
-
-			const success = () => resolve();
-			const error = () => reject( transaction.error );
-
-			transaction.oncomplete = success;
-			transaction.onabort = error;
-			transaction.onerror = error;
-		} );
-	} );
+	return await idbSet( key, value );
 }
 
 /**
@@ -117,27 +172,16 @@ export function setStoredItem< T >( key: string, value: T ): Promise< void > {
  *
  * @return A promise that gets resolved when all items are successfully cleared.
  */
-export function clearStorage(): Promise< void > {
+export async function clearStorage(): Promise< void > {
 	if ( shouldBypass ) {
-		return bypassClear();
+		return await bypassClear();
 	}
 
-	if ( ! supportsIDB ) {
+	const idbSupported = await supportsIDB();
+	if ( ! idbSupported ) {
 		localStorage.clear();
-		return Promise.resolve();
+		return;
 	}
 
-	return new Promise( ( resolve, reject ) => {
-		getDB().then( db => {
-			const transaction = db.transaction( STORE_NAME, 'readwrite' );
-			transaction.objectStore( STORE_NAME ).clear();
-
-			const success = () => resolve();
-			const error = () => reject( transaction.error );
-
-			transaction.oncomplete = success;
-			transaction.onabort = error;
-			transaction.onerror = error;
-		} );
-	} );
+	return await idbClear();
 }
