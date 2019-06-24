19cd66562ed91f031e3dc892a9a58a30a1d2762c
Kirk Wight <kwight@kwight.ca>
2019-06-24 12:06:52 -0700

    FSE: Site Description block improvements.

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/site-description/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/site-description/index.js
@@ -13,7 +13,12 @@ import './style.scss';
 registerBlockType( 'a8c/site-description', {
 	title: __( 'Site Description' ),
 	description: __( 'Site description, also known as the tagline.' ),
-	icon: 'layout',
+	icon: (
+		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+			<path fill="none" d="M0 0h24v24H0z" />
+			<path d="M4 9h16v2H4V9zm0 4h10v2H4v-2z" />
+		</svg>
+	),
 	category: 'layout',
 	supports: {
 		html: false,
