03ee37824b6ff712765f112133f988705f833fd9
Jon Surrell <jon.surrell@automattic.com>
2019-06-25 09:46:46 +0200

    Drop unnecessary state changes
    Extracted to #34180

--- client/state/selectors/get-current-query-arguments.js
+++ client/state/selectors/get-current-query-arguments.js
@@ -1,18 +1,20 @@
+/** @format */
+
 /**
  * External dependencies
  */
+
 import { get } from 'lodash';
 
 /**
- * @typedef {import('querystring').ParsedUrlQuery} Query
+ * Internal dependencies
  */
 
 /**
  * Gets the last query parameters set by a ROUTE_SET action
  * @param {Object} state - global redux state
- * @return {(Query|false)} Initial state is false, otherwise this should be a query object
+ * @return {Object} current state value
  */
-export const getCurrentQueryArguments = state =>
-	get( state, [ 'ui', 'route', 'query', 'current' ] );
+export const getCurrentQueryArguments = state => get( state, 'ui.route.query.current', null );
 
 export default getCurrentQueryArguments;
