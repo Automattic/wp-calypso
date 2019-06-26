d1c128c3c97e81421f8137c138a2a1b112d44c8b
Sérgio Gomes <mail@sgomes.com>
2019-06-26 12:58:42 +0100

    Add `once` method to memoizeLast. (#34304)
    * Add `once` method to memoizeLast.
    
    This is effectively an alias of `memoizeLast`, albeit with stricter
    typing, which ensures that the function to be wrapped doesn't have
    any arguments. There's a runtime check too, for non-typed scenarios.
    
    This guarantees that the provided method will only be run once (through
    the wrapper), and we can thus take advantage of the special semantics
    with some clearer naming.
    
    * Do not perform arity check in production.

--- client/lib/memoize-last/index.ts
+++ client/lib/memoize-last/index.ts
@@ -24,3 +24,21 @@ export default function memoizeLast< T extends ( ...args: any[] ) => any >( fn: 
 		return lastResult;
 	} ) as T;
 }
+
+/**
+ * A stricter-typed alias of `memoizeLast`, for functions without arguments.
+ * Since it only accepts functions without arguments, it effectively guarantees
+ * that the provided function will only be run once, as the check for whether
+ * the arguments have not changed will always pass.
+ *
+ * @param fn The function to be wrapped.
+ *
+ * @returns The wrapped function.
+ */
+export function once< T extends () => any >( fn: T ) {
+	// Runtime validation. Useful when static validation is unavailable.
+	if ( process.env.NODE_ENV !== 'production' && fn.length !== 0 ) {
+		throw new Error( 'memoize-last: The `once` method expects a function with no arguments.' );
+	}
+	return memoizeLast( fn );
+}
