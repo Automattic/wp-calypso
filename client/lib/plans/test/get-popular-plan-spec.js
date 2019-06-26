13c09765ad85992522a4b076971a508f1848f745
Jeff Bowen <jblz@users.noreply.github.com>
2019-06-26 16:51:47 -0400

    tests++

--- client/lib/plans/test/get-popular-plan-spec.js
+++ client/lib/plans/test/get-popular-plan-spec.js
@@ -3,21 +3,67 @@
  * External dependencies
  */
 import { expect } from 'chai';
+import { noop } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { getPopularPlanSpec } from '..';
-import { TYPE_BUSINESS } from '../constants';
+import { GROUP_WPCOM, TYPE_BUSINESS, TYPE_PERSONAL, TYPE_PREMIUM } from '../constants';
+
+const abtest = noop;
 
 describe( 'getPopularPlanSpec()', () => {
+	test( 'Should return biz for empty customer type', () => {
+		expect( getPopularPlanSpec( {} ) ).to.deep.equal( {
+			type: TYPE_BUSINESS,
+			group: GROUP_WPCOM,
+		} );
+	} );
+
+	test( 'Should return premium for personal customer type', () => {
+		expect(
+			getPopularPlanSpec( {
+				customerType: 'personal',
+			} )
+		).to.deep.equal( {
+			type: TYPE_PREMIUM,
+			group: GROUP_WPCOM,
+		} );
+	} );
+
+	test( 'Should return personal for blog site type', () => {
+		expect(
+			getPopularPlanSpec( {
+				siteType: 'blog',
+				abtest,
+			} )
+		).to.deep.equal( {
+			type: TYPE_PERSONAL,
+			group: GROUP_WPCOM,
+		} );
+	} );
+
+	test( 'Should return premium for professional site type', () => {
+		expect(
+			getPopularPlanSpec( {
+				siteType: 'professional',
+				abtest,
+			} )
+		).to.deep.equal( {
+			type: TYPE_PREMIUM,
+			group: GROUP_WPCOM,
+		} );
+	} );
+
 	test( 'Should return biz for biz customer type', () => {
 		expect(
 			getPopularPlanSpec( {
 				customerType: 'business',
 			} )
-		).to.deep.equal( { type: TYPE_BUSINESS, group: 'GROUP_WPCOM' } );
+		).to.deep.equal( { type: TYPE_BUSINESS, group: GROUP_WPCOM } );
 	} );
+
 	test( 'Should return false when isJetpack is true', () => {
 		expect(
 			getPopularPlanSpec( {
