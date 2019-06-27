910bc424c4f586911483007d28a23bd5dd2df76e
Glen Davies <glen.davies@a8c.com>
2019-06-28 11:57:57 +1200

    Replace FSE block save buttons with temporary fake setAttribute call to force editor dirty state

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/site-title/edit.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/site-title/edit.js
@@ -7,7 +7,7 @@ import classNames from 'classnames';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { withNotices, Button } from '@wordpress/components';
+import { withNotices } from '@wordpress/components';
 import { PlainText } from '@wordpress/editor';
 import { withSelect } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
@@ -24,17 +24,19 @@ function SiteTitleEdit( {
 	noticeOperations,
 	shouldUpdateSiteOption,
 	isSelected,
+	setAttributes,
 } ) {
 	const inititalTitle = __( 'Site title loading…' );
-	const { onSave, siteOptions, setSiteOptions } = useSiteOptions(
+	const { siteOptions, handleChange } = useSiteOptions(
 		'title',
 		inititalTitle,
 		noticeOperations,
 		isSelected,
-		shouldUpdateSiteOption
+		shouldUpdateSiteOption,
+		setAttributes
 	);
 
-	const { option, isDirty, isSaving } = siteOptions;
+	const { option } = siteOptions;
 
 	return (
 		<Fragment>
@@ -42,21 +44,10 @@ function SiteTitleEdit( {
 			<PlainText
 				className={ classNames( 'site-title', className ) }
 				value={ option }
-				onChange={ value => setSiteOptions( { ...siteOptions, option: value, isDirty: true } ) }
+				onChange={ value => handleChange( value ) }
 				placeholder={ __( 'Site Title' ) }
 				aria-label={ __( 'Site Title' ) }
 			/>
-			{ isDirty && (
-				<Button
-					isLarge
-					className="site-title__save-button"
-					disabled={ isSaving }
-					isBusy={ isSaving }
-					onClick={ onSave }
-				>
-					{ __( 'Save' ) }
-				</Button>
-			) }
 		</Fragment>
 	);
 }
