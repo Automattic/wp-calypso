b6c0da085762d67a16aa195128c89d2243c5d250
Jon Surrell <jon.surrell@automattic.com>
2019-06-27 22:41:46 +0200

    Isolate cachingActionCreatorFactory

--- client/state/utils/index.js
+++ client/state/utils/index.js
@@ -4,7 +4,7 @@
 import validator from 'is-my-json-valid';
 import { forEach, get, isEmpty, isEqual, mapValues, merge, reduce, reduceRight } from 'lodash';
 import { combineReducers as combine } from 'redux'; // eslint-disable-line wpcalypso/import-no-redux-combine-reducers
-import LRU from 'lru';
+import { cachingActionCreatorFactory } from './caching-action-creator-factory';
 import { keyedReducer } from './keyed-reducer';
 
 /**
@@ -14,7 +14,7 @@ import { APPLY_STORED_STATE, DESERIALIZE, SERIALIZE } from 'state/action-types';
 import { SerializationResult } from 'state/serialization-result';
 import warn from 'lib/warn';
 
-export { keyedReducer };
+export { cachingActionCreatorFactory, keyedReducer };
 
 export function isValidStateWithSchema( state, schema, debugInfo ) {
 	const validate = validator( schema, {
@@ -515,67 +515,3 @@ function createCombinedReducer( reducers ) {
 
 	return combinedReducer;
 }
-
-/**
- * Creates a caching action creator
- *
- * @example Here's a caching action creator:
- * export const fetchOAuth2ClientData = cachingActionCreatorFactory(
- *	clientId => wpcom.undocumented().oauth2ClientId( clientId ),
- *	dispatch => clientId => dispatch( { type: OAUTH2_CLIENT_DATA_REQUEST, clientId, } ),
- *	dispatch => wpcomResponse => dispatch( { type: OAUTH2_CLIENT_DATA_REQUEST_SUCCESS, data: wpcomResponse } ),
- *	dispatch => wpcomError => {
- *		const error = {
- *			message: wpcomError.message,
- *			code: wpcomError.error,
- *		};
- *
- *		dispatch( {
- *			type: OAUTH2_CLIENT_DATA_REQUEST_FAILURE,
- *			error,
- *		} );
- *
- *		return Promise.reject( error );
- *	},
- *);
- *
- * @param {Function} worker a worker function that returns the promise ( param1, param2, ... ) => Promise
- * @param {Function} loadingActionCreator an action creator for before the work is performed of the following signature:
- * 					dispatch => ( param1, param2, ... ) => dispatch( ... ),
- * @param {Function} successActionCreator an action creator for the success case of the work performed of the following signature:
- * 					dispatch => ( param1, param2, ... ) => dispatch( ... ),
- * @param {Function} failureActionCreator an action creator for the failure case of the work performed of the following signature:
- * 					dispatch => ( param1, param2, ... ) => dispatch( ... ),
- * @param {Function} parametersHashFunction a hash function for params, default is just array's join
- * @param {Object} cacheOptions options that passed to LRU cache constructor
- *
- * @return {Function} a function that can be used as an action creator of the following signature:
- * 					( param1, param2, ... ) => dispatch => Promise
- */
-export const cachingActionCreatorFactory = (
-	worker,
-	loadingActionCreator,
-	successActionCreator,
-	failureActionCreator,
-	parametersHashFunction = params => params.join( '' ),
-	cacheOptions = {
-		// those are passed to LRU ctor directly
-		max: 100,
-		maxAge: 2 * 60 * 60, // 2 hours
-	}
-) => {
-	const cache = new LRU( cacheOptions );
-
-	return ( ...params ) => dispatch => {
-		loadingActionCreator( dispatch )( ...params );
-
-		const cacheKey = parametersHashFunction( params );
-		const cachedValue = cache.get( cacheKey );
-		const resultPromise = cachedValue ? Promise.resolve( cachedValue ) : worker( ...params );
-
-		return resultPromise.then( result => {
-			cache.set( cacheKey, result );
-			return successActionCreator( dispatch )( result );
-		}, failureActionCreator( dispatch ) ); // we don't cache failures
-	};
-};
