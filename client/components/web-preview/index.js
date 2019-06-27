d59a1b59db4fe750a6e84af369fccf7d0797ee31
Joshua Flowers <joshuatf@gmail.com>
2019-06-27 15:36:23 +0800

    Wrap WebPreview UI component with connected state for Calypso

---
+++ client/components/web-preview/index.js
@@ -0,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import { WebPreview as WebPreviewComponent } from '@automattic/calypso-ui';
+
+/**
+ * Internal dependencies
+ */
+import { isInlineHelpPopoverVisible } from 'state/inline-help/selectors';
+import { recordTracksEvent } from 'state/analytics/actions';
+import { setPreviewShowing } from 'state/ui/actions';
+
+export class WebPreview extends Component {
+	constructor() {
+		super( ...arguments );
+		this.onSetDeviceViewport = this.onSetDeviceViewport.bind( this );
+	}
+
+	onSetDeviceViewport( device ) {
+		this.props.recordTracksEvent( 'calypso_web_preview_select_viewport_device', { device } );
+	}
+
+	render() {
+		return (
+			<WebPreviewComponent
+				{ ...this.props }
+				onPreviewShowChange={ this.onPreviewShowChange }
+				onSetDeviceViewport={ this.onSetDeviceViewport }
+			/>
+		);
+	}
+}
+
+const mapState = state => ( {
+	disableFocus: isInlineHelpPopoverVisible( state ),
+} );
+
+export default connect(
+	mapState,
+	{ recordTracksEvent, setPreviewShowing }
+)( WebPreview );
