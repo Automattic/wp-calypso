203d386ddcca0c6788e4c4a085a615bc5a832a1d
Jeff Bowen <jblz@users.noreply.github.com>
2019-06-27 16:11:18 -0400

    Fix jetpack e2e test w/ appropriate selector

--- test/e2e/lib/pages/plans-page.js
+++ test/e2e/lib/pages/plans-page.js
@@ -41,12 +41,18 @@ export default class PlansPage extends AsyncBaseContainer {
 	}
 
 	async onePrimaryButtonShown() {
-		const selector =
-			currentScreenSize() === 'mobile'
-				? '.plan-features__mobile .plan-features__actions-button.is-primary'
-				: '.plan-features__table-item.is-top-buttons button.plan-features__actions-button.is-primary';
-		const count = await driverHelper.getElementCount( this.driver, by.css( selector ) );
+		if ( host === 'WPCOM' ) {
+			const selector =
+				currentScreenSize() === 'mobile'
+					? '.plan-features__mobile .plan-features__actions-button.is-primary'
+					: '.plan-features__table-item.is-top-buttons button.plan-features__actions-button.is-primary';
+			const count = await driverHelper.getElementCount( this.driver, by.css( selector ) );
+			return count === 1;
+		}
 
+		// Jetpack non-atomic (inside wp-admin)
+		const selector = '.plan-features__table-item.is-top-buttons a.dops-button.is-primary';
+		const count = await driverHelper.getElementCount( this.driver, by.css( selector ) );
 		return count === 1;
 	}
 
