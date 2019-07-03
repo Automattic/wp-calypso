Automattic/wp-calypso

commit f0acae7f9ba6921ec85f947496df3bbaa3fd4d9c

Update canResume to only match against the current flowName (#34356)
* Update signup canResume's dependencies to only match against steps from the same flow as the current flowName

This makes `resumeProgress` less eager to resume the progress. In this change, progress should only resume if we're certain that the user is resuming the same flow that they were last on. Otherwise, they will start from the beginning of the flow again.

* Add test coverage for `canResumeFlow`

client/signup/utils.js
@@ -188,7 +188,10 @@ export function getDesignTypeForSiteGoals( siteGoals, flow ) {
 
 export function getFilteredSteps( flowName, progress ) {
 	const flow = flows.getFlow( flowName );
-	return filter( progress, step => includes( flow.steps, step.stepName ) );
+	return filter(
+		progress,
+		step => includes( flow.steps, step.stepName ) && step.lastKnownFlow === flowName
+	);
 }
 
 export function getFirstInvalidStep( flowName, progress ) {

client/signup/test/fixtures/flows.js
@@ -19,4 +19,31 @@ export default {
 		steps: [ 'user', 'site' ],
 		destination: '/',
 	},
+
+	onboarding: {
+		steps: [
+			'user',
+			'site-type',
+			'site-topic-with-preview',
+			'site-title-with-preview',
+			'site-style-with-preview',
+			'domains-with-preview',
+			'plans',
+		],
+		destination: '/',
+	},
+
+	'disallow-resume': {
+		steps: [
+			'user',
+			'site-type',
+			'site-topic-with-preview',
+			'site-title-with-preview',
+			'site-style-with-preview',
+			'domains-with-preview',
+			'plans',
+		],
+		destination: '/',
+		disallowResume: true,
+	},
 };

client/signup/test/utils.js
@@ -13,6 +13,7 @@ import sinon from 'sinon';
  * Internal dependencies
  */
 import {
+	canResumeFlow,
 	getValueFromProgressStore,
 	getValidPath,
 	getStepName,
@@ -214,6 +215,36 @@ describe( 'utils', () => {
 		test( 'should return null if the field is not present', () => {
 			delete signupProgress[ 1 ].site;
 			assert.equal( getValueFromProgressStore( config ), null );
+		} );
+	} );
+
+	describe( 'canResumeFlow', () => {
+		test( 'should return true when given flow matches progress state', () => {
+			const signupProgress = [ { stepName: 'site-type', lastKnownFlow: 'onboarding' } ];
+			const canResume = canResumeFlow( 'onboarding', signupProgress );
+
+			expect( canResume ).toBe( true );
+		} );
+
+		test( 'should return false when given flow does not match progress state', () => {
+			const signupProgress = [ { stepName: 'site-type', lastKnownFlow: 'onboarding' } ];
+			const canResume = canResumeFlow( 'other', signupProgress );
+
+			expect( canResume ).toBe( false );
+		} );
+
+		test( 'should return false when flow sets disallowResume', () => {
+			const signupProgress = [ { stepName: 'site-type', lastKnownFlow: 'disallow-resume' } ];
+			const canResume = canResumeFlow( 'disallow-resume', signupProgress );
+
+			expect( canResume ).toBe( false );
+		} );
+
+		test( 'should return false when progress state is empty', () => {
+			const signupProgress = [];
+			const canResume = canResumeFlow( 'onboarding', signupProgress );
+
+			expect( canResume ).toBe( false );
 		} );
 	} );
 } );

commit 06f05d522df08dc96d21416e7ab37c2b21f49c4a

Update signup canResume's dependencies to only match steps against the same flowName
This is the second attempt at making `resumeProgress` less eager to resume the progress. In this change, progress should only resume if we're certain that the user is resuming the same flow that they were last on. Otherwise, they will start from the beginning of the flow.

Additionally, `getCompletedSteps`, which gets called by the main Signup's `isEveryStepSubmitted` method should _not_ only match against the same flow. Tests are included to confirm this, and fixes a regression in #34356 where the loading screen would not display after submitting the final step, and instead it would show `user` step before redirecting to the checkout page. That issue was logged in #34403.

client/signup/utils.js
@@ -195,12 +195,20 @@ export function getFirstInvalidStep( flowName, progress ) {
 	return find( getFilteredSteps( flowName, progress ), { status: 'invalid' } );
 }
 
-export function getCompletedSteps( flowName, progress ) {
+export function getCompletedSteps( flowName, progress, options = {} ) {
+	if ( options.shouldMatchFlowName ) {
+		return filter(
+			getFilteredSteps( flowName, progress ),
+			step => 'in-progress' !== step.status && step.lastKnownFlow === flowName
+		);
+	}
 	return filter( getFilteredSteps( flowName, progress ), step => 'in-progress' !== step.status );
 }
 
 export function canResumeFlow( flowName, progress ) {
 	const flow = flows.getFlow( flowName );
-	const flowStepsInProgressStore = getCompletedSteps( flowName, progress );
+	const flowStepsInProgressStore = getCompletedSteps( flowName, progress, {
+		shouldMatchFlowName: true,
+	} );
 	return flowStepsInProgressStore.length > 0 && ! flow.disallowResume;
 }

client/signup/test/fixtures/flows.js
@@ -33,6 +33,11 @@ export default {
 		destination: '/',
 	},
 
+	'onboarding-blog': {
+		steps: [ 'user', 'site-type', 'site-topic', 'site-title', 'domains', 'plans' ],
+		destination: '/',
+	},
+
 	'disallow-resume': {
 		steps: [
 			'user',

client/signup/test/utils.js
@@ -13,6 +13,8 @@ import sinon from 'sinon';
  * Internal dependencies
  */
 import {
+	canResumeFlow,
+	getCompletedSteps,
 	getValueFromProgressStore,
 	getValidPath,
 	getStepName,
@@ -214,6 +216,86 @@ describe( 'utils', () => {
 		test( 'should return null if the field is not present', () => {
 			delete signupProgress[ 1 ].site;
 			assert.equal( getValueFromProgressStore( config ), null );
+		} );
+	} );
+
+	describe( 'getCompletedSteps', () => {
+		const mixedFlowsSignupProgress = [
+			{ stepName: 'user', lastKnownFlow: 'onboarding', status: 'completed' },
+			{ stepName: 'site-type', lastKnownFlow: 'onboarding', status: 'completed' },
+			{ stepName: 'site-topic', lastKnownFlow: 'onboarding-blog', status: 'completed' },
+			{ stepName: 'site-title', lastKnownFlow: 'onboarding-blog', status: 'completed' },
+			{ stepName: 'domains', lastKnownFlow: 'onboarding-blog', status: 'pending' },
+			{ stepName: 'plans', lastKnownFlow: 'onboarding-blog', status: 'pending' },
+		];
+		const singleFlowSignupProgress = [
+			{ stepName: 'user', lastKnownFlow: 'onboarding', status: 'completed' },
+			{ stepName: 'site-type', lastKnownFlow: 'onboarding', status: 'completed' },
+			{ stepName: 'site-topic-with-preview', lastKnownFlow: 'onboarding', status: 'completed' },
+			{ stepName: 'site-title-with-preview', lastKnownFlow: 'onboarding', status: 'completed' },
+			{ stepName: 'site-style-with-preview', lastKnownFlow: 'onboarding', status: 'completed' },
+			{ stepName: 'domains-with-preview', lastKnownFlow: 'onboarding', status: 'pending' },
+			{ stepName: 'plans', lastKnownFlow: 'onboarding', status: 'pending' },
+		];
+
+		test( 'step names should match steps of a particular flow given progress with mixed flows', () => {
+			const completedSteps = getCompletedSteps( 'onboarding-blog', mixedFlowsSignupProgress );
+			const stepNames = completedSteps.map( step => step.stepName );
+
+			expect( stepNames ).toStrictEqual( flows.getFlow( 'onboarding-blog' ).steps );
+		} );
+
+		test( 'should not match steps of a flow given progress with mixed flows and `shouldMatchFlowName` flag', () => {
+			const completedSteps = getCompletedSteps( 'onboarding-blog', mixedFlowsSignupProgress, {
+				shouldMatchFlowName: true,
+			} );
+			const filteredOnboardingBlogSteps = mixedFlowsSignupProgress.filter(
+				step => step.lastKnownFlow === 'onboarding-blog'
+			);
+			const stepNames = completedSteps.map( step => step.stepName );
+
+			expect( stepNames ).not.toStrictEqual( flows.getFlow( 'onboarding-blog' ).steps );
+			expect( completedSteps ).toStrictEqual( filteredOnboardingBlogSteps );
+		} );
+
+		test( 'should match steps of a flow given progress with single flow and `shouldMatchFlowName` flag', () => {
+			const completedSteps = getCompletedSteps( 'onboarding', singleFlowSignupProgress, {
+				shouldMatchFlowName: true,
+			} );
+			const stepNames = completedSteps.map( step => step.stepName );
+
+			expect( stepNames ).toStrictEqual( flows.getFlow( 'onboarding' ).steps );
+			expect( completedSteps ).toStrictEqual( singleFlowSignupProgress );
+		} );
+	} );
+
+	describe( 'canResumeFlow', () => {
+		test( 'should return true when given flow matches progress state', () => {
+			const signupProgress = [ { stepName: 'site-type', lastKnownFlow: 'onboarding' } ];
+			const canResume = canResumeFlow( 'onboarding', signupProgress );
+
+			expect( canResume ).toBe( true );
+		} );
+
+		test( 'should return false when given flow does not match progress state', () => {
+			const signupProgress = [ { stepName: 'site-type', lastKnownFlow: 'onboarding' } ];
+			const canResume = canResumeFlow( 'onboarding-blog', signupProgress );
+
+			expect( canResume ).toBe( false );
+		} );
+
+		test( 'should return false when flow sets disallowResume', () => {
+			const signupProgress = [ { stepName: 'site-type', lastKnownFlow: 'disallow-resume' } ];
+			const canResume = canResumeFlow( 'disallow-resume', signupProgress );
+
+			expect( canResume ).toBe( false );
+		} );
+
+		test( 'should return false when progress state is empty', () => {
+			const signupProgress = [];
+			const canResume = canResumeFlow( 'onboarding', signupProgress );
+
+			expect( canResume ).toBe( false );
 		} );
 	} );
 } );