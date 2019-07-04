f5125c54e7b591f00a0a54fd726e81e73ca585c4
Don Park <donpark@docuverse.com>
2019-07-04 00:28:57 -0700

    People: Fix team list and follower list freezing while scrolling (#33705)
    * do not synchronously call updateScroll from componentDidUpdate
    
    * update ref use
    
    * use _isMounted

--- client/components/infinite-list/index.jsx
+++ client/components/infinite-list/index.jsx
@@ -52,6 +52,11 @@ export default class InfiniteList extends React.Component {
 	_isMounted = false;
 	smartSetState = smartSetState;
 
+	namedRefs = {
+		topPlaceholder: React.createRef(),
+		bottomPlaceholder: React.createRef(),
+	};
+
 	componentWillMount() {
 		const url = page.current;
 		let newState, scrollTop;
@@ -154,9 +159,14 @@ export default class InfiniteList extends React.Component {
 		// we may have guessed item heights wrong - now we have real heights
 		if ( ! this.isScrolling ) {
 			this.cancelAnimationFrame();
-			this.updateScroll( {
-				triggeredByScroll: false,
-			} );
+			// updateScroll misbehaves when it's called syncronously from componentDidUpdate.
+			// Promise.resolve() is not used here intentionally to avoid the orignal problem which
+			// I suspect is a race condition introduced by changes in React fiber implementation.
+			setTimeout( () => {
+				if ( this._isMounted ) {
+					this.updateScroll( { triggeredByScroll: false } );
+				}
+			}, 0 );
 		}
 	}
 
@@ -293,8 +303,9 @@ export default class InfiniteList extends React.Component {
 	}
 
 	boundsForRef = ref => {
-		if ( ref in this.refs ) {
-			return ReactDom.findDOMNode( this.refs[ ref ] ).getBoundingClientRect();
+		if ( ref in this.namedRefs ) {
+			const node = this.namedRefs[ ref ].current;
+			return node ? node.getBoundingClientRect() : null;
 		}
 		return null;
 	};
@@ -376,14 +387,14 @@ export default class InfiniteList extends React.Component {
 		return (
 			<div { ...propsToTransfer }>
 				<div
-					ref="topPlaceholder"
+					ref={ this.namedRefs.topPlaceholder }
 					className={ spacerClassName }
 					style={ { height: this.state.topPlaceholderHeight } }
 				/>
 				{ itemsToRender }
 				{ this.props.renderTrailingItems() }
 				<div
-					ref="bottomPlaceholder"
+					ref={ this.namedRefs.bottomPlaceholder }
 					className={ spacerClassName }
 					style={ { height: this.state.bottomPlaceholderHeight } }
 				/>
