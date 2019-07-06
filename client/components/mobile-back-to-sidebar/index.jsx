15bf095dfe5c47a35a8b744ae818826a9b2168de
Aurorum <torres126@clubpenguinmountains.com>
2019-07-06 11:47:06 +0100

    Import Styling

--- client/components/mobile-back-to-sidebar/index.jsx
+++ client/components/mobile-back-to-sidebar/index.jsx
@@ -1,5 +1,3 @@
-/** @format */
-
 /**
  * External dependencies
  */
@@ -11,6 +9,11 @@ import { connect } from 'react-redux';
  * Internal Dependencies
  */
 import { setLayoutFocus } from 'state/ui/layout-focus/actions';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
 
 class MobileBackToSidebar extends React.Component {
 	toggleSidebar = event => {
