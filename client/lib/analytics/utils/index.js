bb3742181a316a9f979a2ee06857a14f85c2b299
Sérgio Gomes <mail@sgomes.com>
2019-07-09 18:00:04 +0100

    Split client/lib/analytics into multiple modules.
    Instead of one monolithic file that can't easily be tree-shaken, this
    change splits `client/lib/analytics` into individual modules.

---
+++ client/lib/analytics/utils/index.js
@@ -0,0 +1,14 @@
+/**
+ * Re-exports
+ */
+export { default as MARKETING_COUPONS_KEY } from './marketing-coupons-key';
+export { default as costToUSD } from './cost-to-usd';
+export { default as doNotTrack } from './do-not-track';
+export { default as hashPii } from './hash-pii';
+export { default as isPiiUrl } from './is-pii-url';
+export { default as isUrlBlacklistedForPerformance } from './is-url-blacklisted-for-performance';
+export { default as isAdTrackingAllowed } from './is-ad-tracking-allowed';
+export { default as mayWeTrackCurrentUserGdpr } from './may-we-track-current-user-gdpr';
+export { default as isCurrentUserMaybeInGdprZone } from './is-current-user-maybe-in-gdpr-zone';
+export { default as urlParseAmpCompatible } from './url-parse-amp-compatible';
+export { default as shouldReportOmitBlogId } from './should-report-omit-blog-id';
