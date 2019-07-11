4c29e62ef3e632ac61759cf11cd5c71f2dad6969
Aurorum <torres126@clubpenguinmountains.com>
2019-07-11 20:05:40 +0100

    Hide the "Upgrade" CTA itself for non-admins

--- client/components/banner/index.jsx
+++ client/components/banner/index.jsx
@@ -1,5 +1,3 @@
-/** @format */
-
 /**
  * External dependencies
  */
@@ -25,7 +23,8 @@ import {
 import { GROUP_JETPACK, GROUP_WPCOM } from 'lib/plans/constants';
 import { addQueryArgs } from 'lib/url';
 import { recordTracksEvent } from 'state/analytics/actions';
-import { getSelectedSiteSlug } from 'state/ui/selectors';
+import { getSelectedSite, getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
+import canCurrentUser from 'state/selectors/can-current-user';
 import Button from 'components/button';
 import Card from 'components/card';
 import DismissibleCard from 'blocks/dismissible-card';
@@ -145,6 +144,7 @@ export class Banner extends Component {
 			description,
 			event,
 			feature,
+			isAdmin,
 			list,
 			price,
 			title,
@@ -179,7 +179,7 @@ export class Banner extends Component {
 						</ul>
 					) }
 				</div>
-				{ ( callToAction || price ) && (
+				{ isAdmin && ( callToAction || price ) && (
 					<div className="banner__action">
 						{ size( prices ) === 1 && <PlanPrice rawPrice={ prices[ 0 ] } /> }
 						{ size( prices ) === 2 && (
@@ -264,6 +264,7 @@ export class Banner extends Component {
 
 const mapStateToProps = ( state, ownProps ) => ( {
 	siteSlug: ownProps.disableHref ? null : getSelectedSiteSlug( state ),
+	isAdmin: canCurrentUser( state, getSelectedSiteId( state ), 'manage_options' ),
 } );
 
 export default connect(
