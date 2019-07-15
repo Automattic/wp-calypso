d22bea9a7cc3c7729a79a2b9c69f1f518fc1ea3f
Sérgio Gomes <mail@sgomes.com>
2019-07-15 17:26:32 +0100

    Replace string refs in FormTextInput.
    String refs are deprecated and can easily be replaced in this component.

--- client/components/forms/form-text-input/index.jsx
+++ client/components/forms/form-text-input/index.jsx
@@ -1,9 +1,6 @@
-/** @format */
-
 /**
  * External dependencies
  */
-
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -17,24 +14,28 @@ export default class FormTextInput extends PureComponent {
 		className: PropTypes.string,
 	};
 
-	constructor() {
-		super( ...arguments );
+	currentTextField = undefined;
 
-		this.selectOnFocus = this.selectOnFocus.bind( this );
-	}
+	textFieldRef = element => {
+		if ( element && this.props.inputRef ) {
+			this.props.inputRef.current = element;
+		}
+		this.currentTextField = element;
+	};
 
 	focus() {
-		this.refs.textField.focus();
-	}
-
-	selectOnFocus( event ) {
-		if ( this.props.selectOnFocus ) {
-			event.target.select();
+		if ( this.currentTextField ) {
+			this.currentTextField.focus();
 		}
 	}
 
+	selectOnFocus = event => {
+		if ( this.props.selectOnFocus ) {
+			event.target.select();
+		}
+	};
+
 	render() {
-		const { inputRef } = this.props;
 		const props = omit( this.props, 'isError', 'isValid', 'selectOnFocus', 'inputRef' );
 
 		const classes = classNames( 'form-text-input', this.props.className, {
@@ -46,7 +47,7 @@ export default class FormTextInput extends PureComponent {
 			<input
 				type="text"
 				{ ...props }
-				ref={ inputRef || 'textField' }
+				ref={ this.textFieldRef }
 				className={ classes }
 				onClick={ this.selectOnFocus }
 			/>
