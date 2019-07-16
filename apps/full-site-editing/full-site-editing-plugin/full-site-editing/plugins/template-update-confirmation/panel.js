f30327338b7fb8eecebf7a6fe8fb3a004ed07004
Glen Davies <glen.davies@a8c.com>
2019-07-17 11:49:48 +1200

    Add icon, and template name to the warning template save warning

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/template-update-confirmation/panel.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/template-update-confirmation/panel.js
@@ -2,39 +2,61 @@
 /**
  * External dependencies
  */
-import { Component } from '@wordpress/element';
-import { Button, IconButton } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
+import { Button, IconButton, Icon } from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
 
-export default class Panel extends Component {
-	render() {
-		const { onSave, onClose, isBusy, disabled } = this.props;
-		return (
-			<div className="edit-post-layout">
-				<div className="editor-post-publish-panel">
-					<div className="editor-post-publish-panel__header">
-						<div className="editor-post-publish-panel__header-publish-button">
-							<Button isPrimary isLarge isBusy={ isBusy } onClick={ onSave } disabled={ disabled }>
-								{ __( 'Publish' ) }
-							</Button>
-							<span className="editor-post-publish-panel__spacer" />
-						</div>
+/**
+ * Internal dependencies
+ */
+import './style.scss';
 
-						<IconButton
-							aria-expanded={ true }
-							onClick={ onClose }
-							icon="no-alt"
-							label={ __( 'Close panel' ) }
-						/>
+export default ( { onSave, onClose, isBusy, disabled } ) => {
+	const { title } = useSelect( select => {
+		return select( 'core/editor' ).getCurrentPost();
+	} );
+	return (
+		<div className="edit-post-layout">
+			<div className="editor-post-publish-panel">
+				<div className="editor-post-publish-panel__header">
+					<div className="editor-post-publish-panel__header-publish-button">
+						<Button isPrimary isLarge isBusy={ isBusy } onClick={ onSave } disabled={ disabled }>
+							{ __( 'Publish' ) }
+						</Button>
+						<span className="editor-post-publish-panel__spacer" />
 					</div>
-					<div className="editor-post-publish-panel__prepublish">
-						<p>
-							<strong>{ __( 'Are you ready to publish?' ) }</strong>
-						</p>
-						<p>{ __( 'Changes you publish will update all pages with this template part.' ) }</p>
+
+					<IconButton
+						aria-expanded={ true }
+						onClick={ onClose }
+						icon="no-alt"
+						label={ __( 'Close panel' ) }
+					/>
+				</div>
+				<div className="editor-post-publish-panel__prepublish">
+					<p>
+						<strong>{ __( 'Are you ready to publish?' ) }</strong>
+					</p>
+					<div className="warning">
+						<Icon
+							icon={
+								<svg
+									className="icon"
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+								>
+									<path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z" />
+								</svg>
+							}
+						/>
+						<div className="message">
+							{ sprintf( __( 'Changes published will update all pages using this %s.' ), title ) }
+						</div>
 					</div>
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
