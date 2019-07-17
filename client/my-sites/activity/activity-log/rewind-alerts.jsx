972de181f7e8ca7aa001839a1350a708d11e18bd
pidgeonman <2166135+aeryx@users.noreply.github.com>
2019-07-17 19:18:23 -0300

    First commit. Unstable. WIP.

--- client/my-sites/activity/activity-log/rewind-alerts.jsx
+++ client/my-sites/activity/activity-log/rewind-alerts.jsx
@@ -2,7 +2,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
 
@@ -11,34 +11,36 @@ import { useTranslate } from 'i18n-calypso';
  */
 import Card from 'components/card';
 import ThreatAlert from './threat-alert';
-import { requestSiteAlerts } from 'state/data-getters';
+import { getSiteThreats } from 'state/selectors/get-site-threats';
 
 /**
  * Style dependencies
  */
 import './rewind-alerts.scss';
 
-function RewindAlerts( { alerts } ) {
-	const translate = useTranslate();
+export class RewindAlerts extends Component {
+	getRewindAlerts = alerts => {
+		const translate = useTranslate();
 
-	if ( ! alerts || ! alerts.threats || alerts.threats.length === 0 ) {
-		return null;
-	}
+		if ( ! alerts || ! alerts.threats || alerts.threats.length === 0 ) {
+			return null;
+		}
 
-	return (
-		<Card className="activity-log__threats" highlight="error">
-			<div className="activity-log__threats-heading">
-				{ translate( 'These items require your immediate attention' ) }
-			</div>
-			{ alerts.threats.map( threat => (
-				<ThreatAlert key={ threat.id } threat={ threat } />
-			) ) }
-		</Card>
-	);
+		return (
+			<Card className="activity-log__threats" highlight="error">
+				<div className="activity-log__threats-heading">
+					{ translate( 'These items require your immediate attention' ) }
+				</div>
+				{ alerts.threats.map( threat => (
+					<ThreatAlert key={ threat.id } threat={ threat } />
+				) ) }
+			</Card>
+		);
+	};
 }
 
 const mapStateToProps = ( state, { siteId } ) => ( {
-	alerts: requestSiteAlerts( siteId ).data,
+	alerts: getSiteThreats( state, siteId ),
 } );
 
 export default connect( mapStateToProps )( RewindAlerts );
