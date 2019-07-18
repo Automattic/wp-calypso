319b9fa3a3a6a59605e47e439837ae8e4f4e007b
Glen Davies <glen.davies@a8c.com>
2019-07-18 14:58:06 +1200

    Replace icon with notice component

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/template-update-confirmation/panel.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/template-update-confirmation/panel.js
@@ -2,7 +2,7 @@
 /**
  * External dependencies
  */
-import { Button, IconButton, Icon } from '@wordpress/components';
+import { Button, IconButton, Notice } from '@wordpress/components';
 import { __, sprintf } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 
@@ -37,25 +37,9 @@ export default ( { onSave, onClose, isBusy, disabled } ) => {
 					<p>
 						<strong>{ __( 'Are you ready to publish?' ) }</strong>
 					</p>
-					<div className="warning">
-						{ /* TODO:  if we want to keep the icon it should be added to @automattic/material-design-icons */ }
-						<Icon
-							icon={
-								<svg
-									className="icon"
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-								>
-									<path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z" />
-								</svg>
-							}
-						/>
-						<div className="message">
-							{ sprintf( __( 'Changes published will update all pages using this %s.' ), title ) }
-						</div>
-					</div>
+					<Notice status="warning" isDismissible={ false }>
+						{ sprintf( __( 'Changes published will update all pages using this %s.' ), title ) }
+					</Notice>
 				</div>
 			</div>
 		</div>
