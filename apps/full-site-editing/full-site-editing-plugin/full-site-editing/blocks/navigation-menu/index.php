93e2762f061447e304b9984847fa3093877fcc57
Glen Davies <glen.davies@a8c.com>
2019-07-19 09:02:28 +1200

    Add specific message for missing menu in different locations

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/navigation-menu/index.php
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/navigation-menu/index.php
@@ -74,6 +74,28 @@ function get_wrapper_attributes_by_theme_location( $location ) {
 }
 
 /**
+ * Determines the message to display if a menu is not set for the given location.
+ *
+ * @param string $location Theme location.
+ * @return string
+ */
+function get_add_menu_message( $location ) {
+	switch ( $location ) {
+		case 'footer':
+			$message = __( 'Add a menu to the Footer Menu display location', 'full-site-editing' );
+			break;
+		case 'social':
+			$message = __( 'Add a menu to the Social Links Menu display location', 'full-site-editing' );
+			break;
+		case 'main-1':
+		default:
+			$message = __( 'Add a menu to the Primary display location', 'full-site-editing' );
+			break;
+	}
+	return $message;
+}
+
+/**
  * Renders post content.
  *
  * @param array $attributes Block attributes.
@@ -82,22 +104,22 @@ function get_wrapper_attributes_by_theme_location( $location ) {
 function render_navigation_menu_block( $attributes ) {
 	$location     = ! empty( $attributes['themeLocation'] ) ? $attributes['themeLocation'] : null;
 	$wrapper_attr = get_wrapper_attributes_by_theme_location( $location );
+	$class_name   = ! empty( $attributes['className'] ) ? ' ' . $attributes['className'] : '';
+	$params       = get_menu_params_by_theme_location( $location );
 
-	$params = get_menu_params_by_theme_location( $location );
 	if ( ! array_key_exists( $params['theme_location'], get_nav_menu_locations() ) ) {
-		return ' ';
-	}
-	ob_start();
-	if ( array_key_exists( $params['theme_location'], get_nav_menu_locations() ) ) {
+		return get_add_menu_message( $location );
+	} else {
+		ob_start();
 		// phpcs:disable WordPress.WP.I18n.NonSingularStringLiteralText
 		?>
-		<nav class="<?php echo esc_attr( $wrapper_attr['class'] . ' wp-block-a8c-navigation-menu' ); ?>" aria-label="<?php esc_attr_e( $wrapper_attr['label'], 'twentynineteen' ); ?>">
+		<nav class="<?php echo esc_attr( $wrapper_attr['class'] . ' wp-block-a8c-navigation-menu' . $class_name ); ?>" aria-label="<?php esc_attr_e( $wrapper_attr['label'], 'twentynineteen' ); ?>">
 			<?php
 				wp_nav_menu( $params );
 			?>
 		</nav>
 		<!-- #site-navigation -->
 		<?php
+		return ob_get_clean();
 	}
-	return ob_get_clean();
 }
