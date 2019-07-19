7042a2d8e830888f18737ea263fed763239e22e7
retrofox <rdsuarez@gmail.com>
2019-07-19 12:02:44 -0300

    Adds tracking lib skeleton for Block Editor

--- apps/wpcom-block-editor/src/common/tracking/track-record-event.js
+++ apps/wpcom-block-editor/src/common/tracking/track-record-event.js
@@ -1,12 +1,12 @@
 /**
  * External dependencies
  */
-import { registerPlugin } from '@wordpress/plugins';
 import { isObjectLike, isUndefined, omit } from 'lodash';
 import debug from 'debug';
+
 const tracksDebug = debug( 'wpcom-block-editor:analytics:tracks' );
 
-// In case Tracks hasn't loaded
+// In case Tracks hasn't loaded.
 if ( typeof window !== 'undefined' ) {
 	window._tkq = window._tkq || [];
 }
@@ -14,7 +14,12 @@ if ( typeof window !== 'undefined' ) {
 // Adapted from the analytics lib :(
 // Because this is happening outside of the Calypso app we can't reuse the same lib
 // This means we don't have any extra props like user
-const tracksRecordEvent = function( eventName, eventProperties ) {
+
+export default ( eventName, eventProperties ) => {
+	// Required by Tracks when added manually
+	const blog_id = window._currentSiteId;
+	const site_type = window._currentSiteType;
+
 	eventProperties = eventProperties || {};
 
 	if ( process.env.NODE_ENV !== 'production' && typeof console !== 'undefined' ) {
@@ -43,44 +48,11 @@ const tracksRecordEvent = function( eventName, eventProperties ) {
 	// This allows a caller to easily remove properties from the recorded set by setting them to undefined
 	eventProperties = omit( eventProperties, isUndefined );
 
+	// Populate custom properties.
+	eventProperties = { ...eventProperties, blog_id, site_type };
+
 	tracksDebug( 'Recording event "%s" with actual props %o', eventName, eventProperties );
 
-	if ( 'undefined' !== typeof window ) {
-		window._tkq.push( [ 'recordEvent', eventName, eventProperties ] );
-	}
+	// window._tkq.push( [ 'identifyUser', userId, userUsername ] );
+	window._tkq.push( [ 'recordEvent', eventName, eventProperties ] );
 };
-
-const trackGutenbergSearch = event => {
-	const searchTerm = event.target.value;
-	const hasResults =
-		document.getElementsByClassName( 'block-editor-inserter__no-results' ).length === 0;
-
-	if ( searchTerm.length < 3 ) {
-		return;
-	}
-
-	tracksRecordEvent( 'wpcom_block_picker_search_term', {
-		search_term: searchTerm,
-	} );
-
-	if ( hasResults ) {
-		return;
-	}
-
-	// Create a separate event for search with no results to make it easier to filter by them
-	tracksRecordEvent( 'wpcom_block_picker_no_results', {
-		search_term: searchTerm,
-	} );
-};
-
-registerPlugin( 'track-no-search-results', {
-	render: () => {
-		document.onkeyup = event => {
-			if ( event.target.id.indexOf( 'block-editor-inserter__search' ) === 0 ) {
-				trackGutenbergSearch( event );
-			}
-		};
-
-		return null;
-	},
-} );
