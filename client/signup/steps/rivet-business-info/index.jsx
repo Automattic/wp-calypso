d344e0e0667ecdd8184bc4d8fc1ad342cf690647
Jonathan Sadowski <sadowski@automattic.com>
2019-07-22 14:40:54 -0500

    Add new files

---
+++ client/signup/steps/rivet-business-info/index.jsx
@@ -0,0 +1,43 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import StepWrapper from 'signup/step-wrapper';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
+class RivetBusinessInfo extends React.Component {
+	renderBusinessInfoForm = () => {
+		return <div>Gather business info here!</div>;
+	};
+
+	render() {
+		return (
+			<StepWrapper
+				flowName={ this.props.flowName }
+				stepName={ this.props.stepName }
+				positionInFlow={ this.props.positionInFlow }
+				fallbackHeaderText={ this.props.translate( 'Create your site.' ) }
+				signupProgress={ this.props.signupProgress }
+				stepContent={ this.renderBusinessInfoForm() }
+			/>
+		);
+	}
+}
+
+export default connect(
+	null,
+	null
+)( localize( RivetBusinessInfo ) );
