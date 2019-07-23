54764a32395e62b601cb38e57da39a821ad0f8f5
Payton Swick <payton@foolord.com>
2019-07-23 18:21:59 -0400

    StripeElementsPaymentBox: Use withStripe HOC

--- client/my-sites/checkout/checkout/stripe-elements-payment-box.jsx
+++ client/my-sites/checkout/checkout/stripe-elements-payment-box.jsx
@@ -5,13 +5,14 @@
  */
 import React from 'react';
 import { localize } from 'i18n-calypso';
-import { StripeProvider, Elements } from 'react-stripe-elements';
 
 /**
  * Internal dependencies
  */
 import CreditCardPaymentBox from './credit-card-payment-box';
-import { useStripeConfiguration, useStripeJs } from 'lib/stripe';
+import { withStripe } from 'lib/stripe';
+
+const CreditCardPaymentBoxWithStripe = withStripe( CreditCardPaymentBox );
 
 export function StripeElementsPaymentBox( {
 	translate,
@@ -25,29 +26,21 @@ export function StripeElementsPaymentBox( {
 	presaleChatAvailable,
 	cards,
 } ) {
-	// TODO: send the country to useStripeConfiguration
-	const stripeConfiguration = useStripeConfiguration();
-	const stripeJs = useStripeJs( stripeConfiguration );
 	return (
-		<StripeProvider stripe={ stripeJs }>
-			<Elements>
-				<CreditCardPaymentBox
-					translate={ translate }
-					cards={ cards }
-					transaction={ transaction }
-					cart={ cart }
-					countriesList={ countriesList }
-					initialCard={ initialCard }
-					selectedSite={ selectedSite }
-					onSubmit={ onSubmit }
-					transactionStep={ transaction.step }
-					presaleChatAvailable={ presaleChatAvailable }
-					stripeConfiguration={ stripeConfiguration }
-				>
-					{ children }
-				</CreditCardPaymentBox>
-			</Elements>
-		</StripeProvider>
+		<CreditCardPaymentBoxWithStripe
+			translate={ translate }
+			cards={ cards }
+			transaction={ transaction }
+			cart={ cart }
+			countriesList={ countriesList }
+			initialCard={ initialCard }
+			selectedSite={ selectedSite }
+			onSubmit={ onSubmit }
+			transactionStep={ transaction.step }
+			presaleChatAvailable={ presaleChatAvailable }
+		>
+			{ children }
+		</CreditCardPaymentBoxWithStripe>
 	);
 }
 export default localize( StripeElementsPaymentBox );
