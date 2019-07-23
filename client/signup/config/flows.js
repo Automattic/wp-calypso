40e5e0f1e86c7b2a3eb9aee007b4969d19e4ed20
ramonjd <ramonjd@gmail.com>
2019-07-23 18:01:42 +1000

    Removing methods we don't use anywhere in the code base Removing sinon from the tests

--- client/signup/config/flows.js
+++ client/signup/config/flows.js
@@ -12,7 +12,7 @@ import config from 'config';
 import stepConfig from './steps';
 import userFactory from 'lib/user';
 import { abtest } from 'lib/abtest';
-import { generateFlows } from './flows-pure';
+import { generateFlows } from 'signup/config/flows-pure';
 
 const user = userFactory();
 
@@ -74,29 +74,6 @@ function removeUserStepFromFlow( flow ) {
 	} );
 }
 
-/**
- * Properly filter the current flow.
- *
- * Called by `getFlowName` in 'signup/utils.js' to allow conditional filtering of the current
- * flow for AB tests.
- *
- * @example
- * function filterFlowName( flowName ) {
- *   const defaultFlows = [ 'main', 'website' ];
- *   if ( ! user.get() && includes( defaultFlows, flowName ) ) {
- *     return 'filtered-flow-name';
- *   }
- *   return flowName;
- * }
- * // If user is logged out and the current flow is 'main' or 'website' switch to 'filtered-flow-name' flow.
- *
- * @param  {string} flowName Current flow name.
- * @return {string}          New flow name.
- */
-function filterFlowName( flowName ) {
-	return flowName;
-}
-
 function filterDestination( destination, dependencies ) {
 	if ( dependenciesContainCartItem( dependencies ) ) {
 		return getCheckoutUrl( dependencies );
@@ -106,7 +83,6 @@ function filterDestination( destination, dependencies ) {
 }
 
 const Flows = {
-	filterFlowName,
 	filterDestination,
 
 	defaultFlowName: config.isEnabled( 'signup/onboarding-flow' )
@@ -179,19 +155,6 @@ const Flows = {
 
 	getFlows() {
 		return flows;
-	},
-
-	isValidFlow( flowName ) {
-		return Boolean( Flows.getFlows()[ flowName ] );
-	},
-
-	removeStepFromFlow( stepName, flow ) {
-		return {
-			...flow,
-			steps: flow.steps.filter( step => {
-				return step !== stepName;
-			} ),
-		};
 	},
 };
 
