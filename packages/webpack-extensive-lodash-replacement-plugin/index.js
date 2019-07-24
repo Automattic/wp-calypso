f0b68fc07bae3e754934e6b8125e2520b6a59322
Sérgio Gomes <mail@sgomes.com>
2019-07-24 12:57:28 +0100

    Fix potential race conditions and reorganize code.

--- packages/webpack-extensive-lodash-replacement-plugin/index.js
+++ packages/webpack-extensive-lodash-replacement-plugin/index.js
@@ -359,12 +359,6 @@ class ExtensiveLodashReplacementPlugin {
 	}
 
 	async initBaseLodashData() {
-		if ( this.hasInit ) {
-			return;
-		}
-
-		this.hasInit = true;
-
 		try {
 			this.baseLodashContext = await getModuleForPath(
 				this.moduleResolver,
@@ -398,10 +392,10 @@ class ExtensiveLodashReplacementPlugin {
 			path.dirname( file ),
 			packageName
 		);
-		const baseLodashContext = await this.baseLodashContext;
+		const baseLodashContext = this.baseLodashContext;
 
 		if ( foundContext.path === baseLodashContext.path ) {
-			return await this.baseLodashESVersion;
+			return this.baseLodashESVersion;
 		}
 
 		return (
@@ -413,19 +407,20 @@ class ExtensiveLodashReplacementPlugin {
 	// It takes the importer's version and the global lodash-es version into account.
 	async canBeReplaced( file, packageName ) {
 		const importVersion = await this.findRequestedVersion( file, packageName );
-		const baseLodashESVersion = await this.baseLodashESVersion;
 		const isVersionMatch =
 			importVersion &&
-			baseLodashESVersion &&
-			semver.major( baseLodashESVersion ) === semver.major( importVersion ) &&
-			semver.gte( baseLodashESVersion, importVersion );
+			this.baseLodashESVersion &&
+			semver.major( this.baseLodashESVersion ) === semver.major( importVersion ) &&
+			semver.gte( this.baseLodashESVersion, importVersion );
 
 		if ( ! isVersionMatch ) {
 			const relativePath = path.relative( this.baseDir, file );
 			// Output compilation warning.
 			this.compilation.warnings.push(
 				new Error(
-					`${ relativePath }\n  ${ packageName } version ${ importVersion } cannot be replaced by lodash-es version ${ baseLodashESVersion }`
+					`${ relativePath }\n  ${ packageName } version ${ importVersion } cannot be replaced by lodash-es version ${
+						this.baseLodashESVersion
+					}`
 				)
 			);
 		}
@@ -477,6 +472,55 @@ class ExtensiveLodashReplacementPlugin {
 		return request;
 	}
 
+	async modifyResult( result ) {
+		if ( ! result || ! result.contextInfo || ! result.contextInfo.issuer ) {
+			return;
+		}
+
+		// Wait for initialization, if it's still running.
+		await this.init;
+
+		const { request } = result;
+
+		// Replace plain 'lodash' with 'lodash-es'.
+		if ( /^lodash$/.test( request ) ) {
+			if ( await this.canBeReplaced( result.contextInfo.issuer, 'lodash' ) ) {
+				result.request = 'lodash-es';
+				return;
+			}
+		}
+
+		// Replace 'lodash/foo' with 'lodash-es/foo'.
+		if ( /^lodash\/(.*)$/.test( request ) ) {
+			if ( await this.canBeReplaced( result.contextInfo.issuer, 'lodash' ) ) {
+				result.request = request.replace( 'lodash/', 'lodash-es/' );
+				return;
+			}
+		}
+
+		// Replace 'lodash.foo' with 'lodash-es/foo'.
+		if ( /^lodash\.(.*)$/.test( request ) ) {
+			if ( await this.canBeReplaced( result.contextInfo.issuer, request ) ) {
+				const match = /^lodash\.(.*)$/.exec( request );
+				let subModule = match[ 1 ];
+
+				// Normalize module names.
+				// This avoids code duplication due to module name case differences
+				// (e.g. 'camelcase' vs 'camelCase').
+				LODASH_MODULE_NAMES.forEach( casedModule => {
+					if ( subModule === casedModule.toLowerCase() ) {
+						subModule = casedModule;
+					}
+				} );
+
+				result.request = `lodash-es/${ subModule }`;
+				return;
+			}
+		}
+
+		return;
+	}
+
 	apply( compiler ) {
 		compiler.hooks.thisCompilation.tap( 'LodashReplacementPlugin', compilation => {
 			this.compilation = compilation;
@@ -484,18 +528,16 @@ class ExtensiveLodashReplacementPlugin {
 
 		compiler.hooks.normalModuleFactory.tap( 'LodashReplacementPlugin', nmf => {
 			this.moduleResolver = this.moduleResolver || nmf.getResolver( 'normal' );
-			this.initBaseLodashData();
+			this.init = this.init || this.initBaseLodashData();
 
-			nmf.hooks.beforeResolve.tapPromise( 'LodashReplacementPlugin', async result => {
-				if ( ! result ) return;
-				result.request = await this.getModifiedRequest( result );
-				return result;
-			} );
-			nmf.hooks.afterResolve.tapPromise( 'LodashReplacementPlugin', async result => {
-				if ( ! result ) return;
-				result.request = await this.getModifiedRequest( result );
-				return result;
-			} );
+			nmf.hooks.beforeResolve.tapPromise(
+				'LodashReplacementPlugin',
+				this.modifyResult.bind( this )
+			);
+			nmf.hooks.afterResolve.tapPromise(
+				'LodashReplacementPlugin',
+				this.modifyResult.bind( this )
+			);
 		} );
 	}
 }
