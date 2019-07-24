2a446778fd80c75625490ce43be02dffb9640497
Miguel Torres <miguel.torres@automattic.com>
2019-07-24 13:37:33 +0900

    Gutenberg: Enable Jetpack Gutenframe in the desktop app (#34833)
    This PR enables Gutenframe for Jetpack/Atomic sites in the desktop app by removing the `jetpack/gutenframe` feature toggle (since all environments will have it enabled now).
    
    In order to keep the user in the app, it also fallbacks to the classic editor when the site is not eligible for Gutenframe (i.e. because a denied plugin is active) rather than opening WP Admin in the browser. This was previously controlled by the `calypsoify/gutenberg` feature flag which has been renamed to `gutenberg/redirect` in order to align the name with the value of the selected editor.

--- client/state/selectors/get-selected-editor.js
+++ client/state/selectors/get-selected-editor.js
@@ -4,8 +4,6 @@
  * External dependencies
  */
 import { get } from 'lodash';
-import { isJetpackSite } from 'state/sites/selectors';
-import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
 import { isEnabled } from 'config';
 
 /**
@@ -28,20 +26,16 @@ export const getSelectedEditor = ( state, siteId ) => {
 		'gutenberg-redirect-and-style',
 		'classic',
 	];
-	if ( validEditors.indexOf( selectedEditor ) === -1 ) {
+	if ( ! validEditors.includes( selectedEditor ) ) {
 		return null;
 	}
 
-	if ( isJetpackSite( state, siteId ) ) {
-		if ( 'gutenberg-iframe' === selectedEditor && ! isEnabled( 'jetpack/gutenframe' ) ) {
-			// Redirect to Calypsoified WP Admin block editor on Atomic sites when the feature flag is disabled.
-			if ( isSiteAutomatedTransfer( state, siteId ) ) {
-				return isEnabled( 'calypsoify/gutenberg' ) ? 'gutenberg-redirect-and-style' : 'classic';
-			}
-
-			// Redirect to WP Admin block editor on Jetpack sites when the feature flag is disabled.
-			return 'gutenberg-redirect';
-		}
+	// Fallback to classic editor if we cannot redirect to WP Admin
+	if (
+		! isEnabled( 'gutenberg/redirect' ) &&
+		[ 'gutenberg-redirect', 'gutenberg-redirect-and-style' ].includes( selectedEditor )
+	) {
+		return 'classic';
 	}
 
 	return selectedEditor;
