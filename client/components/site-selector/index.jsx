7a05c7a8a721618405c04cfa20959adf13d7277e
Sérgio Gomes <mail@sgomes.com>
2019-07-29 12:31:33 +0100

    Replace dom-scroll-into-view with native methods. (#34909)
    * Replace dom-scroll-into-view with native methods.
    
    The usage of `dom-scroll-into-view` in Calypso is all achievable through
    native methods on the web platform.
    
    * Rewrite lib/scroll-into-viewport.
    
    It now matches the Element.scrollIntoView() API, including the proposed
    `scrollMode` option.
    
    * Fix refs in form input.
    
    * Fix refs in StateSelect.

--- client/components/site-selector/index.jsx
+++ client/components/site-selector/index.jsx
@@ -9,7 +9,6 @@ import { localize } from 'i18n-calypso';
 import page from 'page';
 import classNames from 'classnames';
 import { filter, find, flow, get, includes, isEmpty, noop } from 'lodash';
-import scrollIntoView from 'dom-scroll-into-view';
 import debugFactory from 'debug';
 
 /**
@@ -29,6 +28,7 @@ import SitePlaceholder from 'blocks/site/placeholder';
 import Search from 'components/search';
 import SiteSelectorAddSite from './add-site';
 import searchSites from 'components/search-sites';
+import scrollIntoViewport from 'lib/scroll-into-viewport';
 
 /**
  * Style dependencies
@@ -121,8 +121,9 @@ class SiteSelector extends Component {
 			return;
 		}
 
-		scrollIntoView( highlightedSiteElem, selectorElement, {
-			onlyScrollIfNeeded: true,
+		scrollIntoViewport( highlightedSiteElem, {
+			block: 'nearest',
+			scrollMode: 'if-needed',
 		} );
 	}
 
