a0dac9d79690366ceccd3a0bb845099e723b1a14
Jeff Bowen <jblz@users.noreply.github.com>
2019-07-30 19:05:38 -0400

    Private-by-Default: AB Test the launch 90% selected 10% not

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -134,4 +134,11 @@ export default {
 		defaultVariation: 'yearlyPlan',
 		allowExistingUsers: true,
 	},
+	privateByDefault: {
+		datestamp: '20190730',
+		variations: {
+			selected: 90,
+			control: 10,
+		},
+	},
 };
--- client/lib/signup/step-actions.js
+++ client/lib/signup/step-actions.js
@@ -15,7 +15,7 @@ import { parse as parseURL } from 'url';
 import wpcom from 'lib/wp';
 /* eslint-enable no-restricted-imports */
 import userFactory from 'lib/user';
-import { getSavedVariations } from 'lib/abtest';
+import { abtest, getSavedVariations } from 'lib/abtest';
 import analytics from 'lib/analytics';
 import {
 	updatePrivacyForDomain,
@@ -167,7 +167,7 @@ export function createSiteWithCart(
 				title: siteTitle,
 			},
 		},
-		public: -1,
+		public: abtest( 'privateByDefault' ) === 'selected' ? -1 : 1,
 		validate: false,
 	};
 
