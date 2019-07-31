80c7bd8b63b5c56ea1a4d187c0a4d0f21a6ba017
mmtr <miguel.torres@automattic.com>
2019-07-31 10:41:22 +0900

    Ensure VIP sites cannot opt in

--- client/state/selectors/is-gutenberg-opt-in-enabled.js
+++ client/state/selectors/is-gutenberg-opt-in-enabled.js
@@ -2,10 +2,15 @@
  * Internal dependencies
  */
 import { isEnabled } from 'config';
-import { isGutenbergSupported } from 'state/selectors/is-gutenberg-supported';
+import isGutenbergSupported from 'state/selectors/is-gutenberg-supported';
+import isVipSite from 'state/selectors/is-vip-site';
 
 export const isGutenbergOptInEnabled = ( state, siteId ) => {
-	return isGutenbergSupported( state, siteId ) && isEnabled( 'gutenberg/opt-in' );
+	return (
+		isGutenbergSupported( state, siteId ) &&
+		! isVipSite( state, siteId ) &&
+		isEnabled( 'gutenberg/opt-in' )
+	);
 };
 
 export default isGutenbergOptInEnabled;
