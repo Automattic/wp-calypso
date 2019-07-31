aaf216777776753cd74d9fd27bdf2ff1452db13e
retrofox <rdsuarez@gmail.com>
2019-07-31 11:37:45 -0300

    starter-page-templates: refact -> replace function by class

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-control.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-control.js
@@ -9,58 +9,56 @@ import classnames from 'classnames';
  */
 import { withInstanceId } from '@wordpress/compose';
 import { BaseControl } from '@wordpress/components';
+import { Component } from '@wordpress/element';
 
-function TemplateSelectorControl( {
-	label,
-	className,
-	help,
-	instanceId,
-	onClick,
-	templates = [],
-} ) {
-	const id = `template-selector-control-${ instanceId }`;
-	const handleButtonClick = event => onClick( event.target.value );
-
-	if ( isEmpty( templates ) ) {
-		return null;
-	}
-
-	const renderTemplatePreview = ( { preview, previewAlt = '' } ) => {
+class TemplateSelectorControl extends Component {
+	renderTemplatePreview( { preview, previewAlt } ) {
 		if ( ! preview ) {
 			return null;
 		}
 
 		return <img className="template-selector-control__media" src={ preview } alt={ previewAlt } />;
-	};
+	}
 
-	return (
-		<BaseControl
-			label={ label }
-			id={ id }
-			help={ help }
-			className={ classnames( className, 'template-selector-control' ) }
-		>
-			<ul className="template-selector-control__options">
-				{ templates.map( ( option, index ) => (
-					<li key={ `${ id }-${ index }` } className="template-selector-control__option">
-						<button
-							type="button"
-							id={ `${ id }-${ index }` }
-							className="template-selector-control__label"
-							value={ option.value }
-							onClick={ handleButtonClick }
-							aria-describedby={ help ? `${ id }__help` : undefined }
-						>
-							<div className="template-selector-control__media-wrap">
-								{ renderTemplatePreview( option ) }
-							</div>
-							{ option.label }
-						</button>
-					</li>
-				) ) }
-			</ul>
-		</BaseControl>
-	);
+	render() {
+		const { label, className, help, instanceId, onClick, templates = [] } = this.props;
+
+		if ( isEmpty( templates ) ) {
+			return null;
+		}
+
+		const id = `template-selector-control-${ instanceId }`;
+		const handleButtonClick = event => onClick( event.target.value );
+
+		return (
+			<BaseControl
+				label={ label }
+				id={ id }
+				help={ help }
+				className={ classnames( className, 'template-selector-control' ) }
+			>
+				<ul className="template-selector-control__options">
+					{ templates.map( ( option, index ) => (
+						<li key={ `${ id }-${ index }` } className="template-selector-control__option">
+							<button
+								type="button"
+								id={ `${ id }-${ index }` }
+								className="template-selector-control__label"
+								value={ option.value }
+								onClick={ handleButtonClick }
+								aria-describedby={ help ? `${ id }__help` : undefined }
+							>
+								<div className="template-selector-control__media-wrap">
+									{ this.renderTemplatePreview( option ) }
+								</div>
+								{ option.label }
+							</button>
+						</li>
+					) ) }
+				</ul>
+			</BaseControl>
+		);
+	}
 }
 
 export default withInstanceId( TemplateSelectorControl );
