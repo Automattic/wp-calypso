eeca1d634c1f505e475ad506a189504ebe198dc8
Glen Davies <glen.davies@a8c.com>
2019-08-02 09:46:27 +1200

    Switch to dispatching a notice instead of pushing notice node into the dom

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/template-update-notice/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/template-update-notice/index.js
@@ -3,26 +3,18 @@
  * External dependencies
  */
 import domReady from '@wordpress/dom-ready';
-import { render } from '@wordpress/element';
-import { Notice } from '@wordpress/components';
+import { dispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-
-/**
- * Internal dependencies
- */
-import './style.scss';
 
 domReady( () => {
 	if ( 'wp_template_part' !== fullSiteEditing.editorPostType ) {
 		return;
 	}
-	const element = document.createElement( 'div' );
-	element.classList.add( 'wp-block-a8c-template-edit-warning' );
-	document.getElementsByClassName( 'edit-post-layout__content' )[ 0 ].prepend( element );
-	render(
-		<Notice isDismissible={ false }>
-			{ __( 'Updates to this block will affect multiple pages on your site' ) }
-		</Notice>,
-		element
+	dispatch( 'core/notices' ).createNotice(
+		'info',
+		__( 'Updates to this template will affect all pages on your site.' ),
+		{
+			isDismissible: false,
+		}
 	);
 } );
