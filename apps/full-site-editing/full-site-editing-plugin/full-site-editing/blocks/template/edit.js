87aedc0eaaf6c558c787c06eccb83f717eb0ac32
Glen Davies <glen.davies@a8c.com>
2019-08-01 15:24:12 +1200

    Minor tidy ups

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/template/edit.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/template/edit.js
@@ -99,8 +99,8 @@ const TemplateEdit = compose(
 			if ( ! isDirty ) {
 				return;
 			}
-			setNavigateToTemplate( true );
 			event.preventDefault();
+			setNavigateToTemplate( true );
 			savePost();
 		};
 
