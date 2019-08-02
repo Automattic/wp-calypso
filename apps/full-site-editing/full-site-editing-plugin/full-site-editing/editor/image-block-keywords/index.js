4f12e77962d146a870bceb1cb11a07ef0c758129
apeatling <apeatling@gmail.com>
2019-08-01 20:07:48 -0700

    Remove global reference to lodash and add global wp comment.

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/editor/image-block-keywords/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/editor/image-block-keywords/index.js
@@ -1,13 +1,24 @@
+/* global wp */
+
+/**
+ * External dependencies
+ */
+import { assign } from 'lodash';
+
 const additionalKeywords = [ 'logo', 'brand', 'emblem', 'hallmark' ];
 
-wp.hooks.addFilter( 'blocks.registerBlockType', 'core/image', ( settings, name ) => {
-	if ( name !== 'core/image' ) {
+wp.hooks.addFilter(
+	'blocks.registerBlockType',
+	'full-site-editing/editor/image-block-keywords',
+	( settings, name ) => {
+		if ( name !== 'core/image' ) {
+			return settings;
+		}
+
+		settings = assign( {}, settings, {
+			keywords: settings.keywords.concat( additionalKeywords ),
+		} );
+
 		return settings;
 	}
-
-	settings = lodash.assign( {}, settings, {
-		keywords: settings.keywords.concat( additionalKeywords ),
-	} );
-
-	return settings;
-} );
+);
