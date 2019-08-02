e5c7c0d21eebef1622cb6f0b36e010a1c386f021
Marko Andrijasevic <markoandrijasevic@gmail.com>
2019-08-02 04:06:06 +0200

    Full Site Editing: remove obsolete wp_template CPT (#35075)
    With the new themes approach we don't really use wp_template CPT,
    and unnecessarily complicates a lot of our code. I'm also removing some
    functionality that was tied to it like sidebar selector and separate handling 
    in blocks for this page. All of this will be unreachable once we hide the
    template CPTs.
    
    This separation made sense when we were going for the Blank theme
    approach, or full page hijacking in the plugin. Now this layout is defined
    by theme template files (header.php, footer.php) so having this stored
    in the template becomes kind of pointless since it can't be manipulated.
    Even if we do it, it wouldn't be reflected on the page.
    
    Follow up to this will be to rename wp_template_part to wp_template.

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/post-content/index.php
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/post-content/index.php
@@ -18,15 +18,6 @@ function render_post_content_block( $attributes, $content ) {
 		return $content;
 	}
 
-	$post_id     = get_the_ID();
-	$post_type   = get_post_type();
-	$template_id = get_post_meta( $post_id, '_wp_template_id', true );
-
-	// Early return to avoid the infinite loop of a template rendering itself.
-	if ( 'wp_template' === $post_type || $template_id === $post_id ) {
-		return $content;
-	}
-
 	$align = isset( $attributes['align'] ) ? ' align' . $attributes['align'] : '';
 
 	ob_start();
--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/index.js
@@ -6,7 +6,6 @@ import './blocks/post-content';
 import './blocks/site-description';
 import './blocks/site-title';
 import './blocks/template';
-import './plugins/template-selector-sidebar';
 import './plugins/close-button-override';
 import './plugins/template-update-confirmation';
 import './plugins/editor-template-classes';
