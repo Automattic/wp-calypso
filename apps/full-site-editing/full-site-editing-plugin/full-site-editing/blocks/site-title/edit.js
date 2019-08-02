b7cd3e65c791b41db5ec6b2ada9089da20932632
Copons <copons@users.noreply.github.com>
2019-08-02 14:44:10 +0100

    Prevent newline in Site Title block

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/site-title/edit.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/site-title/edit.js
@@ -11,6 +11,7 @@ import { PlainText } from '@wordpress/editor';
 import { withSelect, withDispatch } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
 import { Fragment } from '@wordpress/element';
+import { ENTER } from '@wordpress/keycodes';
 
 /**
  * Internal dependencies
@@ -23,6 +24,7 @@ function SiteTitleEdit( {
 	shouldUpdateSiteOption,
 	isSelected,
 	setAttributes,
+	insertDefaultBlock,
 } ) {
 	const inititalTitle = __( 'Site title loading…' );
 	const { siteOptions, handleChange } = useSiteOptions(
@@ -36,12 +38,21 @@ function SiteTitleEdit( {
 
 	const { option } = siteOptions;
 
+	const onKeyDown = event => {
+		if ( event.keyCode !== ENTER ) {
+			return;
+		}
+		event.preventDefault();
+		insertDefaultBlock();
+	};
+
 	return (
 		<Fragment>
 			<PlainText
 				className={ classNames( 'site-title', className ) }
 				value={ option }
 				onChange={ value => handleChange( value ) }
+				onKeyDown={ onKeyDown }
 				placeholder={ __( 'Site Title' ) }
 				aria-label={ __( 'Site Title' ) }
 			/>
@@ -50,17 +61,25 @@ function SiteTitleEdit( {
 }
 
 export default compose( [
-	withDispatch( dispatch => ( {
-		createErrorNotice: dispatch( 'core/notices' ).createErrorNotice,
-	} ) ),
-	withSelect( select => {
+	withSelect( ( select, { clientId } ) => {
 		const { isSavingPost, isPublishingPost, isAutosavingPost, isCurrentPostPublished } = select(
 			'core/editor'
 		);
+		const { getBlockIndex, getBlockRootClientId } = select( 'core/block-editor' );
+		const rootClientId = getBlockRootClientId( clientId );
+		const blockIndex = getBlockIndex( clientId, rootClientId );
+
 		return {
+			blockIndex,
+			rootClientId,
 			shouldUpdateSiteOption:
 				( ( isSavingPost() && isCurrentPostPublished() ) || isPublishingPost() ) &&
 				! isAutosavingPost(),
 		};
 	} ),
+	withDispatch( ( dispatch, { blockIndex, rootClientId } ) => ( {
+		createErrorNotice: dispatch( 'core/notices' ).createErrorNotice,
+		insertDefaultBlock: () =>
+			dispatch( 'core/block-editor' ).insertDefaultBlock( {}, rootClientId, blockIndex + 1 ),
+	} ) ),
 ] )( SiteTitleEdit );
