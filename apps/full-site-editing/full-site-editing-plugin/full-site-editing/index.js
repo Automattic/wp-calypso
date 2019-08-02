ed7a17215a58f337addde46f219885c5b8cf950f
Glen Davies <glendaviesnz@users.noreply.github.com>
2019-08-02 15:47:13 +1200

    FSE: Remove custom publish button and panel, and replace with info notice (#35006)
    Remove the custom publish button and warning panel for FSE template part updating
    Add a fixed info notice to warn users of the implications of template part update

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/index.js
@@ -7,6 +7,6 @@ import './blocks/site-description';
 import './blocks/site-title';
 import './blocks/template';
 import './plugins/close-button-override';
-import './plugins/template-update-confirmation';
 import './plugins/editor-template-classes';
+import './plugins/template-update-notice';
 import './editor';
