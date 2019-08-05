4a859034f5a5a3c41895921b8d9c0bb8b9021cec
Copons <copons@users.noreply.github.com>
2019-08-05 18:07:46 +0100

    Do the same for the posts page

--- client/state/sites/selectors/get-site-posts-page.js
+++ client/state/sites/selectors/get-site-posts-page.js
@@ -1,7 +1,7 @@
 /**
  * Internal dependencies
  */
-import getSiteOption from './get-site-option';
+import getSiteSetting from 'state/selectors/get-site-setting';
 
 /**
  * Returns the ID of the static page set as the page for posts, or 0 if a static page is not set.
@@ -11,5 +11,5 @@ import getSiteOption from './get-site-option';
  * @return {Number} ID of the static page set as page for posts, or 0 if a static page is not set
  */
 export default function getSitePostsPage( state, siteId ) {
-	return getSiteOption( state, siteId, 'page_for_posts' );
+	return getSiteSetting( state, siteId, 'page_for_posts' );
 }
