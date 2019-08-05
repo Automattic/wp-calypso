eaffc0b5f7837371bdfecce0a5dd2c53f4df53f1
Noah Allen <noahtallen@gmail.com>
2019-08-05 09:14:30 -0700

    Use a more general name for the content slot
    Since users can see this in a few places, we want to make
    sure they aren't confused when it does pop up.

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/post-content/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/post-content/index.js
@@ -15,8 +15,8 @@ import save from './save';
 import './style.scss';
 
 registerBlockType( 'a8c/post-content', {
-	title: __( 'Content Slot' ),
-	description: __( 'Placeholder for a post or a page.' ),
+	title: __( 'Content' ),
+	description: __( 'The page content.' ),
 	icon: 'layout',
 	category: 'layout',
 	supports: {
