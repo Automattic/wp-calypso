f8830eac8461f724053a3e5011046b34b45f608d
retrofox <rdsuarez@gmail.com>
2019-08-06 17:29:25 -0300

    useMemo (wip)

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-control.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-control.js
@@ -9,62 +9,68 @@ import classnames from 'classnames';
  */
 import { withInstanceId } from '@wordpress/compose';
 import { BaseControl } from '@wordpress/components';
-import { Component } from '@wordpress/element';
-import { parse as parseBlocks } from '@wordpress/blocks';
+import { useMemo, useState } from '@wordpress/element';
+import { parse } from '@wordpress/blocks';
 import { BlockPreview } from '@wordpress/block-editor';
 
-class TemplateSelectorControl extends Component {
-	renderTemplatePreview( { content, preview, previewAlt } ) {
+function TemplateSelectorControl( {
+	label,
+	className,
+	help,
+	instanceId,
+	onClick,
+	templates = [],
+} ) {
+	const [ templateIndex, setTemplateIndex ] = useState( 0 );
+	const template = templates[ templateIndex ];
+
+	const renderTemplatePreview = ( { content, preview, previewAlt } ) => {
 		if ( ! content ) {
+			setTemplateIndex( templateIndex + 1 );
 			return null;
 		}
 
 		return (
-			<BlockPreview blocks={ parseBlocks( content ) } /> || (
+			<BlockPreview blocks={ parse( content ) } /> || (
 				<img className="template-selector-control__media" src={ preview } alt={ previewAlt } />
 			)
 		);
+	};
+
+	const renderBlocks = useMemo( () => renderTemplatePreview( template ), templates );
+
+	if ( isEmpty( templates ) ) {
+		return null;
 	}
 
-	render() {
-		const { label, className, help, instanceId, onClick, templates = [] } = this.props;
+	const id = `template-selector-control-${ instanceId }`;
+	const handleButtonClick = event => onClick( event.target.value );
 
-		if ( isEmpty( templates ) ) {
-			return null;
-		}
-
-		const id = `template-selector-control-${ instanceId }`;
-		const handleButtonClick = event => onClick( event.target.value );
-
-		return (
-			<BaseControl
-				label={ label }
-				id={ id }
-				help={ help }
-				className={ classnames( className, 'template-selector-control' ) }
-			>
-				<ul className="template-selector-control__options">
-					{ templates.map( ( option, index ) => (
-						<li key={ `${ id }-${ index }` } className="template-selector-control__option">
-							<button
-								type="button"
-								id={ `${ id }-${ index }` }
-								className="template-selector-control__label"
-								value={ option.value }
-								onClick={ handleButtonClick }
-								aria-describedby={ help ? `${ id }__help` : undefined }
-							>
-								<div className="template-selector-control__media-wrap">
-									{ this.renderTemplatePreview( option ) }
-								</div>
-								{ option.label }
-							</button>
-						</li>
-					) ) }
-				</ul>
-			</BaseControl>
-		);
-	}
+	return (
+		<BaseControl
+			label={ label }
+			id={ id }
+			help={ help }
+			className={ classnames( className, 'template-selector-control' ) }
+		>
+			<ul className="template-selector-control__options">
+				{ templates.map( ( option, index ) => (
+					<li key={ `${ id }-${ index }` } className="template-selector-control__option">
+						<button
+							type="button"
+							id={ `${ id }-${ index }` }
+							className="template-selector-control__label"
+							value={ option.value }
+							onClick={ handleButtonClick }
+							aria-describedby={ help ? `${ id }__help` : undefined }
+						>
+							<div className="template-selector-control__media-wrap">{ renderBlocks }</div>
+						</button>
+					</li>
+				) ) }
+			</ul>
+		</BaseControl>
+	);
 }
 
 export default withInstanceId( TemplateSelectorControl );
