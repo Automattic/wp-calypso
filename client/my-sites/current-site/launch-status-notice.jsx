d8eea18ee95cfea6741a65a30c9e8837584ba64f
Caroline Moore <calobee@gmail.com>
2019-08-07 14:38:22 -0400

    Simplify site privacy notice in sidebar by removing action, cleaning up styles.

--- client/my-sites/current-site/launch-status-notice.jsx
+++ client/my-sites/current-site/launch-status-notice.jsx
@@ -3,44 +3,33 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-
-import { recordTracksEvent } from 'state/analytics/actions';
-import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
-import isUnlaunchedSite from 'state/selectors/is-unlaunched-site';
-import { isCurrentUserEmailVerified } from 'state/current-user/selectors';
-import { isJetpackSite, isCurrentPlanPaid } from 'state/sites/selectors';
-import { launchSite } from 'state/sites/launch/actions';
 
 /**
  * Internal dependencies
  */
 import Notice from 'components/notice';
-import NoticeAction from 'components/notice/notice-action';
-import { connect } from 'react-redux';
+import { recordTracksEvent } from 'state/analytics/actions';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import isUnlaunchedSite from 'state/selectors/is-unlaunched-site';
+import { isCurrentUserEmailVerified } from 'state/current-user/selectors';
+import { isJetpackSite, isCurrentPlanPaid } from 'state/sites/selectors';
+import { launchSite } from 'state/sites/launch/actions';
 
-export const LaunchStatusNotice = ( { translate, siteSlug, siteIsLaunched, siteIsJetpack } ) => {
+export const LaunchStatusNotice = ( { translate, siteIsLaunched, siteIsJetpack } ) => {
 	if ( siteIsLaunched || siteIsJetpack ) {
 		return null;
 	}
-
-	// TODO: if user has paid plan && domain, this should just dispatch the launchSite action directly
-	// similar logic is in my-sites/site-settings/form-general.jsx#L405
-	const noticeAction = `/start/launch-site?siteSlug=${ siteSlug }`;
 
 	return (
 		<Notice
 			icon="info-outline"
 			isCompact
-			status="is-info"
-			text={ translate( 'Your site is private and only visible to you.', {
+			text={ translate( 'Your site is only visible to you.', {
 				comment: 'Sidebar notice to user about whether or not their site is public.',
 			} ) }
-		>
-			<NoticeAction href={ noticeAction }>
-				{ translate( 'Launch', { context: 'verb' } ) }
-			</NoticeAction>
-		</Notice>
+		/>
 	);
 };
 
@@ -49,7 +38,6 @@ export default connect(
 		const siteId = getSelectedSiteId( state );
 
 		return {
-			siteSlug: getSelectedSiteSlug( state ),
 			siteIsJetpack: isJetpackSite( state, siteId ),
 			siteIsLaunched: ! isUnlaunchedSite( state, siteId ),
 			needsVerification: ! isCurrentUserEmailVerified( state ),
