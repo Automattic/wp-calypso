39c440e3a56b05501e455602b3a653859a5b9182
retrofox <rdsuarez@gmail.com>
2019-08-07 10:37:50 -0300

    no wait to the block parsering to render the preview

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-control.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-control.js
@@ -11,6 +11,30 @@ import { withInstanceId } from '@wordpress/compose';
 import { BaseControl } from '@wordpress/components';
 import { BlockPreview } from '@wordpress/block-editor';
 
+/**
+ * Renders the block preview content for the template.
+ * It the templates blocks are not ready yet or not exist,
+ * it tries to render a static image, or simply return null.
+ *
+ * @param {string} preview     Image URL for the static preview.
+ * @param {string} previewAlt  Alt text to for the <img />
+ * @param {array}  blocks      Parsed blocks to dynamic preview.
+ * @return {null|*}            Preview block component.
+ */
+function renderBlockPreview( { preview, previewAlt, blocks } ) {
+	if ( ! blocks || ! blocks.length ) {
+		if ( ! preview ) {
+			return null;
+		}
+
+		return (
+			<img className="template-selector-control__media" src={ preview } alt={ previewAlt || '' } />
+		);
+	}
+
+	return <BlockPreview blocks={ blocks } viewportWidth={ 1024 } />;
+}
+
 function TemplateSelectorControl( {
 	label,
 	className,
@@ -45,7 +69,7 @@ function TemplateSelectorControl( {
 							aria-describedby={ help ? `${ id }__help` : undefined }
 						>
 							<div className="template-selector-control__media-wrap">
-								<BlockPreview blocks={ option.blocks } />
+								{ renderBlockPreview( option ) }
 							</div>
 							{ option.label }
 						</button>
