3454b7462a14544cf02a38398bb8977a836375c5
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2019-08-08 21:00:12 -0300

    Disables move to trash for templates on Gutenberg

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/editor/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/editor/index.js
@@ -2,6 +2,7 @@
  * Internal dependencies
  */
 import './block-inserter';
+import './disable-trash-template';
 import './template-validity-override';
 import './image-block-keywords';
 import './style.scss';
