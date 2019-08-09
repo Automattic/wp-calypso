068307bafa452bcc83f4e863e1c82622c9a85e78
Jon Surrell <jon.surrell@automattic.com>
2019-08-09 17:12:18 +0200

    Move logToLogstash action to TypeScript

--- client/state/logstash/actions.ts
+++ client/state/logstash/actions.ts
@@ -1,20 +1,25 @@
-/** @format */
-
 /**
  * Internal dependencies
  */
 import { LOGSTASH } from 'state/action-types';
-
 import 'state/data-layer/wpcom/logstash';
+
+interface LogToLogstashParams {
+	feature: string;
+	message: string;
+	extra?: any;
+	site_id?: number;
+	[key: string]: any;
+}
 
 /**
  * Log to logstash. This method is inefficient because
  * the data goes over the REST API, so use sparingly.
  *
- * @param {Object} params same as wpcom log2logstash params arg
- * @returns {Object} Action object
+ * @param params wpcom log2logstash params. @see PCYsg-5T4-p2
+ * @returns      Action object
  */
-export function logToLogstash( params ) {
+export function logToLogstash( params: LogToLogstashParams ) {
 	return {
 		type: LOGSTASH,
 		params: { params: JSON.stringify( params ) },
