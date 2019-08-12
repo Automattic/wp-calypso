cacd7b0be4999957a6099b0c550567a22c90c770
Jon Surrell <jon.surrell@automattic.com>
2019-08-12 16:41:13 +0200

    More strict feature

--- client/state/logstash/actions.ts
+++ client/state/logstash/actions.ts
@@ -4,8 +4,18 @@
 import { LOGSTASH } from 'state/action-types';
 import 'state/data-layer/wpcom/logstash';
 
+/**
+ * Parameters sent to logstash endpoint.
+ *
+ * @see PCYsg-5T4-p2
+ */
 interface LogToLogstashParams {
-	feature: string;
+	/**
+	 * Feature name.
+	 *
+	 * Should be whitelisted. @see D31385-code
+	 */
+	feature: 'calypso_ssr' | 'calypso_client';
 	message: string;
 	extra?: any;
 	site_id?: number;
@@ -16,7 +26,7 @@ interface LogToLogstashParams {
  * Log to logstash. This method is inefficient because
  * the data goes over the REST API, so use sparingly.
  *
- * @param params wpcom log2logstash params. @see PCYsg-5T4-p2
+ * @param params wpcom log2logstash params.
  * @returns      Action object
  */
 export function logToLogstash( params: LogToLogstashParams ) {
