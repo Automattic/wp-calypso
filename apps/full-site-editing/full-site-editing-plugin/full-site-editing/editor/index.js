a5cea6b77166fc59577ef8da014a4208f8b59cad
Rodrigo Iloro <rodrigo.iloro@automattic.com>
2019-08-12 12:48:21 -0300

    Removes JS magic in favor of css to hide the move to trash button.

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/editor/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/editor/index.js
@@ -2,7 +2,6 @@
  * Internal dependencies
  */
 import './block-inserter';
-import './disable-trash-template';
 import './template-validity-override';
 import './image-block-keywords';
 import './style.scss';
