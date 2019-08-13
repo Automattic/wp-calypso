62d5bd55212e6e075675a95ee55d516e7b9eebfe
Sérgio Gomes <mail@sgomes.com>
2019-08-13 16:00:54 +0100

    Don't mutate args in setSection.

--- client/state/ui/section/actions.js
+++ client/state/ui/section/actions.js
@@ -4,10 +4,14 @@
 import { SECTION_SET } from 'state/action-types';
 
 export function setSection( section, options = {} ) {
-	options.type = SECTION_SET;
+	const action = {
+		...options,
+		type: SECTION_SET,
+		hasSidebar: options.hasSidebar === false ? false : true,
+	};
 	if ( section ) {
-		options.section = section;
+		action.section = section;
 	}
-	options.hasSidebar = options.hasSidebar === false ? false : true;
-	return options;
+
+	return action;
 }
