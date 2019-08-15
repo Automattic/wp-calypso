f26e1677a22315cc9a124129213f42986983270b
Dave Smith <getdavemail@gmail.com>
2019-08-15 15:50:43 +0100

    Adds loading overlay to obscure janky large preview render
    Previously when the large preview was rendered there was layout jank and rejigging which looked ugly. We obscure this to avoid the user seeing the preview until it is ready.

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-preview.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-preview.js
@@ -3,6 +3,7 @@
  */
 import { isEmpty } from 'lodash';
 import { __ } from '@wordpress/i18n';
+import { useState, useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -10,6 +11,25 @@ import { __ } from '@wordpress/i18n';
 import BlockPreview from './block-template-preview';
 
 const TemplateSelectorPreview = ( { blocks, viewportWidth } ) => {
+	// Used to hide the `BlockPreview` until we're confident
+	// it's completed rendering. Ideally there would a way to detect
+	// this but there isn't.
+	const artificialLoadingDelay = 800;
+
+	const [ isLoading, setIsLoading ] = useState( false );
+
+	useEffect( () => {
+		setIsLoading( true );
+		const timer = setTimeout( () => {
+			setIsLoading( false );
+		}, artificialLoadingDelay );
+
+		return () => {
+			setIsLoading( false );
+			clearTimeout( timer );
+		};
+	}, [ blocks, viewportWidth ] );
+
 	return (
 		<div className="template-selector-preview">
 			{ isEmpty( blocks ) ? (
@@ -17,7 +37,12 @@ const TemplateSelectorPreview = ( { blocks, viewportWidth } ) => {
 					{ __( 'Select a page template to preview.', 'full-site-editing' ) }
 				</div>
 			) : (
-				<BlockPreview blocks={ blocks } viewportWidth={ viewportWidth } />
+				<>
+					{ isLoading && (
+						<div className="template-selector-preview__loading">Loading preview...</div>
+					) }
+					<BlockPreview blocks={ blocks } viewportWidth={ viewportWidth } />
+				</>
 			) }
 		</div>
 	);
