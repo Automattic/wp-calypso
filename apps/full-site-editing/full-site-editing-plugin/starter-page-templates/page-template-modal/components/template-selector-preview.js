2667cccf1d7821ee05f83e3bc87b150a8b6ed320
Michael P. Pfeiffer <michael@cssconf.eu>
2019-08-15 14:30:20 +0200

    Add placeholder text on empty preview

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-preview.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-preview.js
@@ -1,12 +1,24 @@
 /**
  * External dependencies
  */
+import { isEmpty } from 'lodash';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
 import BlockPreview from './block-template-preview';
 
 const TemplateSelectorPreview = ( { blocks, viewportWidth } ) => {
 	return (
 		<div className="template-selector-preview">
-			<BlockPreview blocks={ blocks } viewportWidth={ viewportWidth } />
+			{ isEmpty( blocks ) ? (
+				<div className="template-selector-preview__placeholder">
+					{ __( 'Select a page template to preview.', 'full-site-editing' ) }
+				</div>
+			) : (
+				<BlockPreview blocks={ blocks } viewportWidth={ viewportWidth } />
+			) }
 		</div>
 	);
 };
