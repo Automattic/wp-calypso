886c5b2d73a16a3a27191a4510ba41d7f1ee00c0
Jon Surrell <jon.surrell@automattic.com>
2019-08-16 16:10:33 +0200

    Move test to ts and update

--- client/lib/interval/test/interval.tsx
+++ client/lib/interval/test/interval.tsx
@@ -1,18 +1,17 @@
 /**
- * @format
  * @jest-environment jsdom
  */
 
 /**
  * External dependencies
  */
-import { mount } from 'enzyme';
 import React from 'react';
+import { mount } from 'enzyme';
 
 /**
  * Internal dependencies
  */
-import Interval, { EVERY_SECOND, EVERY_MINUTE } from '../index';
+import { Interval, EVERY_SECOND, EVERY_MINUTE } from '../index';
 
 jest.useFakeTimers();
 
@@ -21,42 +20,43 @@ describe( 'Interval', () => {
 		test( 'Does not run onTick() on mount', () => {
 			const spy = jest.fn();
 			mount( <Interval onTick={ spy } period={ EVERY_SECOND } /> );
-			expect( spy.mock.calls.length ).toBe( 0 );
+			expect( spy ).not.toHaveBeenCalled();
 		} );
 
 		test( 'Runs onTick() on the intervals', () => {
 			const spy = jest.fn();
 			mount( <Interval onTick={ spy } period={ EVERY_SECOND } /> );
 			jest.runTimersToTime( 1000 );
-			expect( spy.mock.calls.length ).toBe( 1 );
+			expect( spy ).toHaveBeenCalledTimes( 1 );
 		} );
 
 		test( 'Respects the period passed', () => {
 			const spy = jest.fn();
 			mount( <Interval onTick={ spy } period={ EVERY_MINUTE } /> );
 			jest.runTimersToTime( 1000 * 60 );
-			expect( spy.mock.calls.length ).toBe( 1 );
+			expect( spy ).toHaveBeenCalledTimes( 1 );
 		} );
 
 		test( 'Stops running the interval on unmount', () => {
 			const spy = jest.fn();
 			const wrapper = mount( <Interval onTick={ spy } period={ EVERY_SECOND } /> );
 			jest.runTimersToTime( 1000 );
-			expect( spy.mock.calls.length ).toBe( 1 );
+			expect( spy ).toHaveBeenCalledTimes( 1 );
 			wrapper.unmount();
 			jest.runTimersToTime( 2000 );
-			expect( spy.mock.calls.length ).toBe( 1 );
+			expect( spy ).toHaveBeenCalledTimes( 1 );
 		} );
 
 		test( 'Changes the interval when the period changes', () => {
 			const spy = jest.fn();
 			const wrapper = mount( <Interval onTick={ spy } period={ EVERY_SECOND } /> );
 			jest.runTimersToTime( 1000 );
-			expect( spy.mock.calls.length ).toBe( 1 );
+			expect( spy ).toHaveBeenCalledTimes( 1 );
 			wrapper.setProps( { onTick: spy, period: EVERY_MINUTE } );
-			expect( spy.mock.calls.length ).toBe( 1 );
+			jest.runTimersToTime( 1000 );
+			expect( spy ).toHaveBeenCalledTimes( 1 );
 			jest.runTimersToTime( 60 * 1000 );
-			expect( spy.mock.calls.length ).toBe( 2 );
+			expect( spy ).toHaveBeenCalledTimes( 2 );
 		} );
 
 		test( 'Picks up a new ticker even if the period does not change', () => {
@@ -66,14 +66,14 @@ describe( 'Interval', () => {
 			const wrapper = mount( <Interval onTick={ spy } period={ EVERY_SECOND } /> );
 
 			jest.runTimersToTime( 1000 );
-			expect( spy.mock.calls.length ).toBe( 1 );
+			expect( spy ).toHaveBeenCalledTimes( 1 );
 
 			wrapper.setProps( { onTick: otherSpy, period: EVERY_SECOND } );
-			expect( spy.mock.calls.length ).toBe( 1 );
-			expect( otherSpy.mock.calls.length ).toBe( 0 );
+			expect( spy ).toHaveBeenCalledTimes( 1 );
+			expect( otherSpy ).not.toHaveBeenCalled();
 
 			jest.runTimersToTime( 1000 );
-			expect( spy.mock.calls.length ).toBe( 1 );
+			expect( spy ).toHaveBeenCalledTimes( 1 );
 		} );
 	} );
 } );
