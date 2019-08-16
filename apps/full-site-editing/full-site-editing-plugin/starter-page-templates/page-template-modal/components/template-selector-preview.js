4e470aac032209903a5db940910d1f58cf861191
Dave Smith <getdavemail@gmail.com>
2019-08-16 11:24:11 +0100

    Fix “missing”/hidden preview loading state bug
    Previously if the large preview was scrolled down then when a new template was loaded due to the scroll position the loading element was not visible.
    
    Resetting the scrollTop on the preview container before setting the loading state ensures the loading state is always visible.

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-preview.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-preview.js
@@ -8,7 +8,7 @@ import { isEmpty } from 'lodash';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useState, useEffect } from '@wordpress/element';
+import { useState, useEffect, useRef } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -21,9 +21,13 @@ const TemplateSelectorPreview = ( { blocks, viewportWidth } ) => {
 	// this but there isn't.
 	const artificialLoadingDelay = 800;
 
+	const previewContainerRef = useRef();
+
 	const [ isLoading, setIsLoading ] = useState( false );
 
 	useEffect( () => {
+		// Reset scroll first to avoid flicker
+		previewContainerRef.current.scrollTop = 0;
 		setIsLoading( true );
 		const timer = setTimeout( () => {
 			setIsLoading( false );
@@ -45,7 +49,7 @@ const TemplateSelectorPreview = ( { blocks, viewportWidth } ) => {
 
 	if ( isEmpty( blocks ) ) {
 		return (
-			<div className="template-selector-preview">
+			<div ref={ previewContainerRef } className="template-selector-preview">
 				<div className="template-selector-preview__placeholder">
 					{ __( 'Select a page template to preview.', 'full-site-editing' ) }
 				</div>
@@ -54,7 +58,7 @@ const TemplateSelectorPreview = ( { blocks, viewportWidth } ) => {
 	}
 
 	return (
-		<div className={ previewElClasses }>
+		<div ref={ previewContainerRef } className={ previewElClasses }>
 			<div aria-hidden={ ! isLoading } className={ loadingElClasses }>
 				Loading preview...
 			</div>
