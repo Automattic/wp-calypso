804bde71c8e9affd5b3336a72e0ebc3934c9719d
bluefuton <chris@bluefuton.com>
2019-08-19 14:57:51 +1200

    Move file download stats to more prominent spot in site stats

--- client/my-sites/stats/site.jsx
+++ client/my-sites/stats/site.jsx
@@ -1,5 +1,3 @@
-/** @format */
-
 /**
  * External dependencies
  */
@@ -13,7 +11,6 @@ import { find } from 'lodash';
 /**
  * Internal dependencies
  */
-
 import DocumentHead from 'components/data/document-head';
 import StatsPeriodNavigation from './stats-period-navigation';
 import Main from 'components/main';
@@ -226,6 +223,7 @@ class StatsSite extends Component {
 								statType="statsSearchTerms"
 								showSummaryLink
 							/>
+							{ fileDownloadList }
 							{ videoList }
 						</div>
 						<div className="stats__module-column">
@@ -262,7 +260,6 @@ class StatsSite extends Component {
 								className="stats__author-views"
 								showSummaryLink
 							/>
-							{ fileDownloadList }
 						</div>
 					</div>
 				</div>
