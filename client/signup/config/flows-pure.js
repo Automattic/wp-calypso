6658079e8237c23290178d59f0c0363216594502
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2019-08-20 18:01:32 +0530

    Updating modified date for the with-theme flow definition

--- client/signup/config/flows-pure.js
+++ client/signup/config/flows-pure.js
@@ -114,7 +114,7 @@ export function generateFlows( {
 			steps: [ 'domains-theme-preselected', 'plans', 'user' ],
 			destination: getChecklistThemeDestination,
 			description: 'Preselect a theme to activate/buy from an external source',
-			lastModified: '2016-01-27',
+			lastModified: '2019-08-20',
 		},
 
 		main: {
