e48609a121820c3be1eb5367f5cb97ea160913e3
Aurorum <torres126@clubpenguinmountains.com>
2019-08-20 09:40:21 +0100

    Linter

--- client/blocks/plan-storage/bar.jsx
+++ client/blocks/plan-storage/bar.jsx
@@ -3,7 +3,6 @@
  */
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import classNames from 'classnames';
 import filesize from 'filesize';
