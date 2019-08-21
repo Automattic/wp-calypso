7e6294d9b94007155abef2ea1b3eb6572172c071
Noah Allen <noahtallen@gmail.com>
2019-08-21 11:58:09 -0700

    Hide PostStatus panel with CSS
    Much simpler than adding a classname programatically :)

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/editor/remove-editor-panels/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/editor/remove-editor-panels/index.js
@@ -3,7 +3,7 @@
 /**
  * External dependencies
  */
-import { dispatch, select, subscribe } from '@wordpress/data';
+import { dispatch, subscribe } from '@wordpress/data';
 
 /**
  * Disables specific sidebar editor panels in the FSE context.
@@ -15,31 +15,17 @@ import { dispatch, select, subscribe } from '@wordpress/data';
  * in the redux state.
  */
 const unsubscribe = subscribe( () => {
-	const { getActiveGeneralSidebarName } = select( 'core/edit-post' );
 	const { removeEditorPanel } = dispatch( 'core/edit-post' );
-
 	if ( 'page' === fullSiteEditing.editorPostType ) {
 		removeEditorPanel( 'featured-image' );
-		return unsubscribe();
 	}
 
+	// @TODO Since the post status component doesn't check to see if it is removed, the
+	// removeEditorPanel action won't have the desired effect. See:
+	// https://github.com/WordPress/gutenberg/pull/17117
+	// When support is added, we should remove the CSS hack at '../style.scss'
 	if ( 'wp_template' === fullSiteEditing.editorPostType ) {
-		// @TODO Since the post status component doesn't check to see if it is removed, the
-		// removeEditorPanel action won't have the desired effect. See:
-		// https://github.com/WordPress/gutenberg/pull/17117
-		// When support is added, we should add this line back in and remove the DOM manipulation hack:
-		// removeEditorPanel( 'post-status' );
-
-		// Only run the hack when the document sidebar is opened:
-		if ( 'edit-post/document' === getActiveGeneralSidebarName() ) {
-			const impendingElement = setInterval( () => {
-				const postStatusElement = document.querySelector(
-					'.components-panel__body.edit-post-post-status'
-				);
-				postStatusElement && postStatusElement.classList.add( 'hidden' );
-				clearInterval( impendingElement );
-				// We cannot unsubscribe since the element can be recreated by React.
-			} );
-		}
+		removeEditorPanel( 'post-status' );
 	}
+	return unsubscribe();
 } );
