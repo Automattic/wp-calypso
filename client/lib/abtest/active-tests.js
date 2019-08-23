5073569ebb94008048c16a1dbbde456107dec6c6
Victor Espigares <espigares@gmail.com>
2019-08-23 12:55:08 +0000

    Removing test definition

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -124,16 +124,6 @@ export default {
 		defaultVariation: 'control',
 		allowExistingUsers: true,
 	},
-	checkoutSealsCopyBundle: {
-		datestamp: '20190613',
-		variations: {
-			variant: 50,
-			control: 50,
-		},
-		defaultVariation: 'control',
-		allowExistingUsers: true,
-		//localeTargets: 'any',
-	},
 	showImportFlowInSiteTypeStep: {
 		datestamp: '20190820',
 		variations: {
