22b5a6a782781747c0ca1d36af5aced6c77edc64
Dave Smith <getdavemail@gmail.com>
2019-08-23 16:40:47 +0100

    Adds WP eslint rules

--- apps/full-site-editing/.eslintrc.js
+++ apps/full-site-editing/.eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
 	rules: {
 		'import/no-extraneous-dependencies': [ 'error', { packageDir: __dirname } ],
 		'react/react-in-jsx-scope': 0,
