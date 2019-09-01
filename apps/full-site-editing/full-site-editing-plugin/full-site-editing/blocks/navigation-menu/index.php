fa2fbae6900bbeab6a555f099550e0d8be4ded0c
Glen Davies <glen.davies@a8c.com>
2019-09-02 11:17:28 +1200

    Try moving menu markup from plugin to theme

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/navigation-menu/index.php
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/navigation-menu/index.php
@@ -13,7 +13,7 @@ namespace A8C\FSE;
  * @return string
  */
 function render_navigation_menu_block() {
-	$menu = wp_nav_menu(
+	$menu                     = wp_nav_menu(
 		[
 			'echo'           => false,
 			'fallback_cb'    => 'get_fallback_navigation_menu',
@@ -22,25 +22,10 @@ function render_navigation_menu_block() {
 			'theme_location' => 'menu-1',
 		]
 	);
-
+	$navigation_block_support = get_theme_support( 'navigation-menu-block' );
+	require_once $navigation_block_support[0]['template_path'];
 	ob_start();
-	// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
-	?>
-	<nav class="main-navigation wp-block-a8c-navigation-menu">
-		<input type="checkbox" role="button" aria-haspopup="true" id="toggle" class="hide-visually">
-		<label for="toggle" id="toggle-menu" class="button">
-			<?php esc_html_e( 'Menu', 'full-site-editing' ); ?>
-			<span class="dropdown-icon open">+</span>
-			<span class="dropdown-icon close">&times;</span>
-			<span class="hide-visually expanded-text"><?php esc_html_e( 'expanded', 'full-site-editing' ); ?></span>
-			<span class="hide-visually collapsed-text"><?php esc_html_e( 'collapsed', 'full-site-editing' ); ?></span>
-		</label>
-		<div class="menu-primary-container">
-			<?php echo $menu ? $menu : get_fallback_navigation_menu(); ?>
-		</div>
-	</nav>
-	<!-- #site-navigation -->
-	<?php
+	$navigation_block_support[0]['method']($menu ? $menu : get_fallback_navigation_menu());
 	// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
 	return ob_get_clean();
 }
