d6aceb5d0f59f557c95cb3f38cce93cc58b02869
Jarda Snajdr <jsnajdr@gmail.com>
2019-09-02 15:24:11 +0200

    Move HMR connection code to separate function

--- client/components/webpack-build-monitor/index.tsx
+++ client/components/webpack-build-monitor/index.tsx
@@ -32,95 +32,112 @@ enum BuildState {
 	ERROR,
 }
 
+interface HotServerConnectParams {
+	setIsConnected: ( isConnected: boolean ) => void;
+	setBuildState: ( buildState: BuildState ) => void;
+	setLastHash: ( lastHash: string ) => void;
+}
+
+function connectToHotServer( {
+	setIsConnected,
+	setBuildState,
+	setLastHash,
+}: HotServerConnectParams ) {
+	if ( typeof EventSource === 'undefined' ) {
+		if ( process.env.NODE_ENV !== 'production' ) {
+			// eslint-disable-next-line no-console
+			console.warn( 'Webpack build monitor disabled. No `EventSource`.' );
+		}
+		return;
+	}
+
+	let lastActivity: TimestampMS;
+	let connectionTimer: IntervalHandle;
+	let source: EventSource;
+
+	const connect = () => {
+		debug( 'Webpack HMR connecting' );
+		source = new EventSource( '/__webpack_hmr' );
+
+		source.onopen = () => {
+			debug( 'Webpack HMR connected' );
+			setIsConnected( true );
+			lastActivity = Date.now();
+			connectionTimer = setInterval( () => {
+				if ( Date.now() - lastActivity > CONNECTION_TIMEOUT ) {
+					debug( 'Webpack HMR connection timeout. Reconnecting in %o…', CONNECTION_TIMEOUT );
+					setIsConnected( false );
+					clearInterval( connectionTimer );
+					source.close();
+					setTimeout( connect, CONNECTION_TIMEOUT );
+				}
+			}, CONNECTION_TIMEOUT / 2 );
+		};
+
+		source.onmessage = ( m: MessageEvent ) => {
+			lastActivity = Date.now();
+
+			if ( m.data === '💓' ) {
+				return;
+			}
+
+			let action: string | undefined;
+			let nextErrors;
+			let nextWarnings;
+
+			try {
+				const parsedData = JSON.parse( m.data );
+				debug( 'Webpack HMR message: %o', parsedData );
+				action = parsedData.action;
+				nextErrors = parsedData.errors;
+				nextWarnings = parsedData.warnings;
+				if ( parsedData.hash ) {
+					setLastHash( parsedData.hash );
+				}
+			} catch ( err ) {
+				debug( 'Could not parse HMR message.data %o', m.data );
+			}
+
+			switch ( action ) {
+				case 'building':
+					setBuildState( BuildState.BUILDING );
+					break;
+
+				case 'built':
+					if ( nextErrors.length ) {
+						setBuildState( BuildState.ERROR );
+					} else if ( nextWarnings.length ) {
+						setBuildState( BuildState.BUILT_WITH_WARNINGS );
+					} else {
+						setBuildState( BuildState.IDLE );
+					}
+					break;
+
+				case 'sync':
+					if ( nextErrors.length ) {
+						setBuildState( BuildState.ERROR );
+					}
+					break;
+			}
+		};
+	};
+
+	connect();
+
+	return () => {
+		if ( source ) {
+			source.close();
+		}
+		clearInterval( connectionTimer );
+	};
+}
+
 const WebpackBuildMonitor: FunctionComponent = () => {
 	const [ lastHash, setLastHash ] = useState( __webpack_hash__ );
 	const [ isConnected, setIsConnected ] = useState( false );
 	const [ buildState, setBuildState ] = useState( BuildState.UNKNOWN );
 
-	useEffect( () => {
-		if ( typeof EventSource === 'undefined' ) {
-			if ( process.env.NODE_ENV !== 'production' ) {
-				// eslint-disable-next-line no-console
-				console.warn( 'Webpack build monitor disabled. No `EventSource`.' );
-			}
-		} else {
-			let lastActivity: TimestampMS;
-			let connectionTimer: IntervalHandle;
-			let source: EventSource;
-			const connect = () => {
-				debug( 'Webpack HMR connecting' );
-				source = new EventSource( '/__webpack_hmr' );
-
-				source.onopen = () => {
-					debug( 'Webpack HMR connected' );
-					setIsConnected( true );
-					lastActivity = Date.now();
-					connectionTimer = setInterval( () => {
-						if ( Date.now() - lastActivity > CONNECTION_TIMEOUT ) {
-							debug( 'Webpack HMR connection timeout. Reconnecting in %o…', CONNECTION_TIMEOUT );
-							setIsConnected( false );
-							clearInterval( connectionTimer );
-							source.close();
-							setTimeout( connect, CONNECTION_TIMEOUT );
-						}
-					}, CONNECTION_TIMEOUT / 2 );
-				};
-
-				source.onmessage = ( m: MessageEvent ) => {
-					lastActivity = Date.now();
-					if ( m.data !== '💓' ) {
-						let action: string | undefined;
-						let nextErrors;
-						let nextWarnings;
-
-						try {
-							const parsedData = JSON.parse( m.data );
-							debug( 'Webpack HMR message: %o', parsedData );
-							action = parsedData.action;
-							nextErrors = parsedData.errors;
-							nextWarnings = parsedData.warnings;
-							if ( parsedData.hash ) {
-								setLastHash( parsedData.hash );
-							}
-						} catch ( err ) {
-							debug( 'Could not parse HMR message.data %o', m.data );
-						}
-
-						switch ( action ) {
-							case 'building':
-								setBuildState( BuildState.BUILDING );
-								break;
-
-							case 'built':
-								if ( nextErrors.length ) {
-									setBuildState( BuildState.ERROR );
-								} else if ( nextWarnings.length ) {
-									setBuildState( BuildState.BUILT_WITH_WARNINGS );
-								} else {
-									setBuildState( BuildState.IDLE );
-								}
-								break;
-
-							case 'sync':
-								if ( nextErrors.length ) {
-									setBuildState( BuildState.ERROR );
-								}
-								break;
-						}
-					}
-				};
-			};
-
-			connect();
-
-			return () => {
-				if ( source ) {
-					source.close();
-				}
-				clearInterval( connectionTimer );
-			};
-		}
-	}, [] );
+	useEffect( () => connectToHotServer( { setIsConnected, setBuildState, setLastHash } ), [] );
 
 	const needsReload = lastHash !== __webpack_hash__;
 
