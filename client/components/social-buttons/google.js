Automattic/wp-calypso

commit 208f4fcbcd9dfeb244237a387cf80df43e5a03fe

Use platform.js for Google sign-in. (#35019)
Not only is this the recommended way of loading the auth API, it
loads less JS than the current api.js alternative (53KB vs 103KB).

See https://developers.google.com/identity/sign-in/web/

client/components/social-buttons/google.js
@@ -1,9 +1,6 @@
-/** @format */
-
 /**
  * External dependencies
  */
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -68,7 +65,7 @@ class GoogleLoginButton extends Component {
 
 	async loadDependency() {
 		if ( ! window.gapi ) {
-			await loadScript( 'https://apis.google.com/js/api.js' );
+			await loadScript( 'https://apis.google.com/js/platform.js' );
 		}
 
 		return window.gapi;
@@ -84,36 +81,29 @@ class GoogleLoginButton extends Component {
 		const { translate } = this.props;
 
 		this.initialized = this.loadDependency()
+			.then( gapi => new Promise( resolve => gapi.load( 'auth2', resolve ) ).then( () => gapi ) )
 			.then( gapi =>
-				new Promise( resolve => gapi.load( 'client:auth2', resolve ) ).then( () => gapi )
-			)
-			.then( gapi =>
-				gapi.client
+				gapi.auth2
 					.init( {
 						fetch_basic_profile: this.props.fetchBasicProfile,
+						client_id: this.props.clientId,
+						scope: this.props.scope,
 						ux_mode: this.props.uxMode,
 						redirect_uri: this.props.redirectUri,
 					} )
-					.then( () =>
-						gapi.auth2
-							.init( {
-								client_id: this.props.clientId,
-								scope: this.props.scope,
-							} )
-							.then( () => {
-								this.setState( { isDisabled: false } );
+					.then( () => {
+						this.setState( { isDisabled: false } );
 
-								const googleAuth = gapi.auth2.getAuthInstance();
-								const currentUser = googleAuth.currentUser.get();
+						const googleAuth = gapi.auth2.getAuthInstance();
+						const currentUser = googleAuth.currentUser.get();
 
-								// handle social authentication response from a redirect-based oauth2 flow
-								if ( currentUser && this.props.uxMode === 'redirect' ) {
-									this.props.responseHandler( currentUser, false );
-								}
+						// handle social authentication response from a redirect-based oauth2 flow
+						if ( currentUser && this.props.uxMode === 'redirect' ) {
+							this.props.responseHandler( currentUser, false );
+						}
 
-								return gapi; // don't try to return googleAuth here, it's a thenable but not a valid promise
-							} )
-					)
+						return gapi; // don't try to return googleAuth here, it's a thenable but not a valid promise
+					} )
 			)
 			.catch( error => {
 				this.initialized = null;
@@ -208,7 +198,9 @@ class GoogleLoginButton extends Component {
 					<button
 						className={ classNames( 'social-buttons__button button', { disabled: isDisabled } ) }
 						onMouseOver={ this.showError }
+						onFocus={ this.showError }
 						onMouseOut={ this.hideError }
+						onBlur={ this.hideError }
 						onClick={ this.handleClick }
 					>
 						<GoogleIcon isDisabled={ isDisabled } />

commit 0f55516e92e97e23242b965247da53c3ecc2dcc1

Google login: ensure auth2 init only happens once.
Fixes issue introduced in #35019 and discussed there.

client/components/social-buttons/google.js
@@ -18,6 +18,8 @@ import { preventWidows } from 'lib/formatting';
 import { recordTracksEventWithClientId as recordTracksEvent } from 'state/analytics/actions';
 import { isFormDisabled } from 'state/login/selectors';
 
+let auth2InitDone = false;
+
 /**
  * Style dependencies
  */
@@ -71,6 +73,21 @@ class GoogleLoginButton extends Component {
 		return window.gapi;
 	}
 
+	async initializeAuth2( gapi ) {
+		if ( auth2InitDone ) {
+			return;
+		}
+
+		await gapi.auth2.init( {
+			fetch_basic_profile: this.props.fetchBasicProfile,
+			client_id: this.props.clientId,
+			scope: this.props.scope,
+			ux_mode: this.props.uxMode,
+			redirect_uri: this.props.redirectUri,
+		} );
+		auth2InitDone = true;
+	}
+
 	initialize() {
 		if ( this.initialized ) {
 			return this.initialized;
@@ -83,27 +100,19 @@ class GoogleLoginButton extends Component {
 		this.initialized = this.loadDependency()
 			.then( gapi => new Promise( resolve => gapi.load( 'auth2', resolve ) ).then( () => gapi ) )
 			.then( gapi =>
-				gapi.auth2
-					.init( {
-						fetch_basic_profile: this.props.fetchBasicProfile,
-						client_id: this.props.clientId,
-						scope: this.props.scope,
-						ux_mode: this.props.uxMode,
-						redirect_uri: this.props.redirectUri,
-					} )
-					.then( () => {
-						this.setState( { isDisabled: false } );
+				this.initializeAuth2( gapi ).then( () => {
+					this.setState( { isDisabled: false } );
 
-						const googleAuth = gapi.auth2.getAuthInstance();
-						const currentUser = googleAuth.currentUser.get();
+					const googleAuth = gapi.auth2.getAuthInstance();
+					const currentUser = googleAuth.currentUser.get();
 
-						// handle social authentication response from a redirect-based oauth2 flow
-						if ( currentUser && this.props.uxMode === 'redirect' ) {
-							this.props.responseHandler( currentUser, false );
-						}
+					// handle social authentication response from a redirect-based oauth2 flow
+					if ( currentUser && this.props.uxMode === 'redirect' ) {
+						this.props.responseHandler( currentUser, false );
+					}
 
-						return gapi; // don't try to return googleAuth here, it's a thenable but not a valid promise
-					} )
+					return gapi; // don't try to return googleAuth here, it's a thenable but not a valid promise
+				} )
 			)
 			.catch( error => {
 				this.initialized = null;