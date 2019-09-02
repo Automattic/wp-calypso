9ce4a4204dee3840022762ed12a3c69329634047
Sérgio Gomes <mail@sgomes.com>
2019-09-02 12:39:04 +0100

    Add "continue as user" option to login. (#35309)
    * Add "continue as user" option to login.
    
    This displays a link for the user to continue as the currently logged-in
    user as part of the login screen. Only displayed if the user is already
    logged in, of course.
    
    This change was originally part of #33642.
    
    * Force logged-out layout for login pages.
    
    * Export makeForcedLoggedOutLayout from server-side controller, too
    
    * Rename makeForcedLoggedOutLayout.
    
    Renamed to makeLoggedOutLayout.
    
    * Add support for redirect validation API endpoint.
    
    * Move redirect validation inside `ContinueAsUser`.
    
    * Read current query from Redux.
    
    Also simplifies and clarifies effect logic.
    
    * Get redirectUrl from query as a prop instead of passing whole query
    
    Also rename state variable to `validatedRedirectUrl`
    
    * Remove check for currentUser, is guaranteed to be valid
    
    * Send redirect_url using a query param, escapes things correctly
    
    * Extract validateUrl to separate function
    
    Makes it similar to a Redux action and simplifies the effect hook call.
    
    * Add MomentProvider to ReduxWrappedLoggedOutLayout, fixes 2FA screens
    
    * Show ContinueAsUser only on the initial, plain login page
    
    Don't show it on other screens, like 2FA, or on customized logins to Jetpack,
    WooCommerce, or other OAuth-connected apps like Crowdsignal.
    
    * Check for continue link and click it if it shows up.

--- client/blocks/login/index.jsx
+++ client/blocks/login/index.jsx
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-
 import PropTypes from 'prop-types';
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
@@ -15,8 +14,6 @@ import classNames from 'classnames';
  * Internal dependencies
  */
 import config from 'config';
-import ErrorNotice from './error-notice';
-import LoginForm from './login-form';
 import {
 	getRedirectToSanitized,
 	getRequestNotice,
@@ -25,23 +22,27 @@ import {
 	getSocialAccountIsLinking,
 	getSocialAccountLinkService,
 } from 'state/login/selectors';
+import { getCurrentUser } from 'state/current-user/selectors';
 import { wasManualRenewalImmediateLoginAttempted } from 'state/immediate-login/selectors';
 import { getCurrentOAuth2Client } from 'state/ui/oauth2-clients/selectors';
 import getCurrentQueryArguments from 'state/selectors/get-current-query-arguments';
 import getPartnerSlugFromQuery from 'state/selectors/get-partner-slug-from-query';
 import { setResumeAfterLogin } from 'state/signup/progress/actions';
 import { getSignupProgress } from 'state/signup/progress/selectors';
-import { isCrowdsignalOAuth2Client, isWooOAuth2Client } from 'lib/oauth2-clients';
 import { recordTracksEventWithClientId as recordTracksEvent } from 'state/analytics/actions';
-import VerificationCodeForm from './two-factor-authentication/verification-code-form';
-import WaitingTwoFactorNotificationApproval from './two-factor-authentication/waiting-notification-approval';
+import { isCrowdsignalOAuth2Client, isWooOAuth2Client } from 'lib/oauth2-clients';
 import { login } from 'lib/paths';
-import Notice from 'components/notice';
-import PushNotificationApprovalPoller from './two-factor-authentication/push-notification-approval-poller';
 import userFactory from 'lib/user';
+import Notice from 'components/notice';
 import AsyncLoad from 'components/async-load';
 import VisitSite from 'blocks/visit-site';
 import WooCommerceConnectCartHeader from 'extensions/woocommerce/components/woocommerce-connect-cart-header';
+import ContinueAsUser from './continue-as-user';
+import ErrorNotice from './error-notice';
+import LoginForm from './login-form';
+import VerificationCodeForm from './two-factor-authentication/verification-code-form';
+import WaitingTwoFactorNotificationApproval from './two-factor-authentication/waiting-notification-approval';
+import PushNotificationApprovalPoller from './two-factor-authentication/push-notification-approval-poller';
 
 /**
  * Style dependencies
@@ -170,6 +171,8 @@ class Login extends Component {
 			translate,
 			twoStepNonce,
 			fromSite,
+			currentUser,
+			twoFactorEnabled,
 		} = this.props;
 
 		let headerText = translate( 'Log in to your account' );
@@ -302,6 +305,9 @@ class Login extends Component {
 		} else if ( fromSite ) {
 			// if redirected from Calypso URL with a site slug, offer a link to that site's frontend
 			postHeader = <VisitSite siteSlug={ fromSite } />;
+		} else if ( currentUser && ! twoFactorEnabled ) {
+			// someone is already logged in, offer to proceed to the app without a new login
+			postHeader = <ContinueAsUser />;
 		}
 
 		return (
@@ -405,6 +411,7 @@ class Login extends Component {
 
 export default connect(
 	state => ( {
+		currentUser: getCurrentUser( state ),
 		redirectTo: getRedirectToSanitized( state ),
 		requestNotice: getRequestNotice( state ),
 		twoFactorEnabled: isTwoFactorEnabled( state ),
