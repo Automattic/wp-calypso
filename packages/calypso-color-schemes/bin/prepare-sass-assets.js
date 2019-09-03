f297ac320e1724cfe5bb8b978f33e0d1eae21061
Dominik Porada <dominik@porada.co>
2019-09-03 17:19:58 +0200

    Phase out SCSS color variables

--- packages/calypso-color-schemes/bin/prepare-sass-assets.js
+++ packages/calypso-color-schemes/bin/prepare-sass-assets.js
@@ -4,7 +4,6 @@ const { copyFileSync, existsSync, mkdirSync } = require( 'fs' );
 
 copyAsset( '@automattic/color-studio/dist/color-properties.css' );
 copyAsset( '@automattic/color-studio/dist/color-properties-rgb.css' );
-copyAsset( '@automattic/color-studio/dist/color-variables.scss' );
 
 function copyAsset( assetPath, targetName ) {
 	if ( ! targetName ) {
