cb7e62ae9d6f99fd1fdc2833338533d881eff5e0
Filipe Varela <keoshi@keoshi.com>
2019-09-03 16:20:38 +0100

    Revert "Jetpack Guided Tours: update Gridicon import reference"
    This reverts commit 51a6e93ff2b2f84027e60ac8c7233cdaa21e6d8a.

--- client/layout/guided-tours/tours/jetpack-monitoring-tour/index.js
+++ client/layout/guided-tours/tours/jetpack-monitoring-tour/index.js
@@ -4,7 +4,7 @@
  * External dependencies
  */
 import React, { Fragment } from 'react';
-import Gridicon from 'components/gridicon';
+import Gridicon from 'gridicons';
 
 /**
  * Internal dependencies
