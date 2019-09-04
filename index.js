Automattic/wp-calypso

commit 3ab4ef6a5ac98d53ba6fdfdc23967986c39aa96e

Remove legacy CSS hot reloader (#35934)
* Server: remove legacy CSS hot reloader

* Fix ESLint errors in server script

* Client: remove legacy CSS hot reloader

* Upgrade mini-css-extract-plugin-with-rtl fork to latest version

* Remove chokidar dependency

index.js
@@ -1,28 +1,26 @@
 /* eslint-disable no-console */
+/* eslint-disable import/no-nodejs-modules */
 
 require( '@babel/polyfill' );
 
 /**
  * External dependencies.
  */
-var boot = require( 'boot' ),
-	chalk = require( 'chalk' ),
-	fs = require( 'fs' );
+const boot = require( 'boot' );
+const chalk = require( 'chalk' );
+const fs = require( 'fs' );
 
 /**
  * Internal dependencies
  */
-var pkg = require( './package.json' ),
-	config = require( 'config' );
+const pkg = require( './package.json' );
+const config = require( 'config' );
 
-var start = Date.now(),
-	protocol = process.env.PROTOCOL || config( 'protocol' ),
-	port = process.env.PORT || config( 'port' ),
-	host = process.env.HOST || config( 'hostname' ),
-	app = boot(),
-	server,
-	compiler,
-	hotReloader;
+const start = Date.now();
+const protocol = process.env.PROTOCOL || config( 'protocol' );
+const port = process.env.PORT || config( 'port' );
+const host = process.env.HOST || config( 'hostname' );
+const app = boot();
 
 function sendBootStatus( status ) {
 	// don't send anything if we're not running in a fork
@@ -41,8 +39,7 @@ console.log(
 	port
 );
 
-// Start a development HTTPS server.
-if ( protocol === 'https' ) {
+function loadSslCert() {
 	const { execSync } = require( 'child_process' );
 	const execOptions = { encoding: 'utf-8', windowsHide: true };
 	let key = './config/server/key.pem';
@@ -65,42 +62,26 @@ if ( protocol === 'https' ) {
 		}
 	}
 
-	const options = {
+	return {
 		key: fs.readFileSync( key ),
 		cert: fs.readFileSync( certificate ),
 	};
-	server = require( 'https' ).createServer( options, app );
-} else {
-	server = require( 'http' ).createServer( app );
 }
 
-// The desktop app runs Calypso in a fork.
-// Let non-forks listen on any host.
-if ( ! process.env.CALYPSO_IS_FORK ) {
-	host = null;
+// Start a development HTTPS server.
+function createServer() {
+	if ( protocol === 'https' ) {
+		return require( 'https' ).createServer( loadSslCert(), app );
+	}
+
+	return require( 'http' ).createServer( app );
 }
 
-server.listen( { port, host }, function() {
+const server = createServer();
+
+// The desktop app runs Calypso in a fork. Let non-forks listen on any host.
+
+server.listen( { port, host: process.env.CALYPSO_IS_FORK ? null : host }, function() {
 	// Tell the parent process that Calypso has booted.
 	sendBootStatus( 'ready' );
 } );
-
-// Enable hot reloader in development
-if ( process.env.NODE_ENV === 'development' ) {
-	console.info( chalk.cyan( '\nGetting bundles ready, hold on...' ) );
-
-	hotReloader = require( 'bundler/hot-reloader' );
-	compiler = app.get( 'compiler' );
-
-	compiler.plugin( 'compile', function() {
-		sendBootStatus( 'compiler compiling' );
-	} );
-	compiler.plugin( 'invalid', function() {
-		sendBootStatus( 'compiler invalid' );
-	} );
-	compiler.plugin( 'done', function() {
-		sendBootStatus( 'compiler done' );
-	} );
-
-	hotReloader.listen( server, compiler );
-}

client/lib/css-hot-reload/index.js
@@ -1,85 +1,0 @@
-/**
- * /* eslint-disable no-console
- *
- * @format
- */
-
-/**
- * External dependencies
- */
-import io from 'socket.io-client';
-
-// https://developer.mozilla.org/en/docs/Web/HTML/Element/link
-const standardAttributes = [
-	'crossorigin',
-	'href',
-	'hreflang',
-	'media',
-	'rel',
-	'sizes',
-	'title',
-	'type',
-];
-
-/**
- * Replaces the old hash parameter with the current timestamp if `name` is `href`.
- * @return {Object}
- */
-function bustHashForHrefs( { name, oldValue } ) {
-	// http://some.site.com/and/a/path?with=a&query -> http://some.site.com/and/a/path?v=13508135781
-	const value =
-		'href' === name ? `${ oldValue.split( '?' ).shift() }?v=${ new Date().getTime() }` : oldValue;
-
-	return { name, value };
-}
-
-/**
- * @return {Boolean}
- */
-function isChanged( href, changedFiles ) {
-	// "/calypso/style-debug.css?v=5a1db7fee7" -> "style-debug.css"
-	const path = href
-		.split( '?' )
-		.shift()
-		.split( '/' )
-		.pop();
-
-	return changedFiles.some( file => file === path );
-}
-
-/**
- * Initialize client CSS hot-reload handler
- */
-export default function() {
-	const namespace = location.protocol + '//' + location.host + '/css-hot-reload';
-	const socket = io.connect( namespace );
-
-	socket.on( 'css-hot-reload', function( data ) {
-		switch ( data.status ) {
-			case 'reload':
-				// Turn HTMLCollection to standard list
-				const elems = [].slice.call( document.head.getElementsByTagName( 'link' ) );
-				elems.forEach( function( oldLink ) {
-					if ( 'href' in oldLink && isChanged( oldLink.href, data.changedFiles ) ) {
-						console.log( 'Reloading CSS: ', oldLink );
-						// Remove old .css and insert new one in the same spot
-						const newLink = document.createElement( 'link' );
-						// Copy <link> standard attributes
-						standardAttributes
-							.filter( attr => oldLink.hasAttribute( attr ) )
-							.map( attr => ( { name: attr, oldValue: oldLink.getAttribute( attr ) } ) )
-							.map( bustHashForHrefs )
-							.forEach( ( { name, value } ) => newLink.setAttribute( name, value ) );
-						oldLink.parentNode.replaceChild( newLink, oldLink );
-					}
-				} );
-				break;
-			case 'building':
-				console.log( 'Building CSS…' );
-				break;
-			case 'build-failed':
-				console.error( 'CSS build failed.\n', data.error );
-				break;
-		}
-	} );
-}

client/boot/common.js
@@ -201,10 +201,6 @@ export const locales = ( currentUser, reduxStore ) => {
 export const utils = () => {
 	debug( 'Executing Calypso utils.' );
 
-	if ( process.env.NODE_ENV === 'development' ) {
-		require( './dev-modules' ).default();
-	}
-
 	// Infer touch screen by checking if device supports touch events
 	// See touch-detect/README.md
 	if ( hasTouch() ) {

client/boot/dev-modules.js
@@ -1,3 +1,0 @@
-export default function() {
-	asyncRequire( 'lib/css-hot-reload', cssHotReload => cssHotReload() );
-}

server/bundler/css-hot-reload.js
@@ -1,150 +1,0 @@
-/** @format */
-/**
- * Module dependencies
- */
-
-const fs = require( 'fs' ),
-	path = require( 'path' ),
-	spawn = require( 'child_process' ).spawn,
-	debug = require( 'debug' )( 'calypso:bundler:css-hot-reload' ),
-	chalk = require( 'chalk' ),
-	chokidar = require( 'chokidar' ),
-	md5File = require( 'md5-file' );
-
-/**
- * Initialize server CSS hot-reloading logic
- * @param {Object} io socket io socket
- */
-function setup( io ) {
-	const SCSS_PATHS = [ path.join( '.', 'assets', 'stylesheets' ), path.join( '.', 'client' ) ];
-	const ROOT_DIR = path.resolve( __dirname, '..', '..' );
-	const PUBLIC_DIR = path.join( ROOT_DIR, 'public' );
-
-	let cssMake = null,
-		errors = '',
-		scheduleBuild = false;
-	const publicCssFiles = {}; // { path, hash } dictionary
-
-	function spawnMake() {
-		// If a build is already in progress schedule another build for later
-		if ( cssMake ) {
-			scheduleBuild = true;
-			return;
-		}
-
-		io.of( '/css-hot-reload' ).emit( 'css-hot-reload', { status: 'building' } );
-
-		debug( 'spawning %o', 'npm run build-css' );
-		cssMake = spawn( 'npm', [ 'run', 'build-css' ], {
-			shell: true,
-			cwd: ROOT_DIR,
-			stdio: [ 'ignore', 'pipe', 'pipe' ],
-		} );
-
-		errors = '';
-		cssMake.once( 'exit', onexit );
-		cssMake.stdout.setEncoding( 'utf8' );
-		cssMake.stderr.setEncoding( 'utf8' );
-		cssMake.stdout.on( 'data', onstdout );
-		cssMake.stderr.on( 'data', onstderr );
-	}
-
-	function onstdout( d ) {
-		debug( 'stdout %o', d.trim() );
-	}
-
-	function onstderr( stderr ) {
-		process.stderr.write( stderr );
-		// Note:
-		// Because we use make --jobs the error message is usually duplicated.
-		errors += stderr;
-	}
-
-	/**
-	 * Handle 'make build-css' success or failure
-	 * @param {Number} code The exit code
-	 */
-	function onexit( code ) {
-		let changedFiles;
-
-		cssMake.stderr.removeListener( 'data', onstderr );
-		cssMake.stdout.removeListener( 'data', onstdout );
-		cssMake = null;
-
-		if ( scheduleBuild ) {
-			// Handle new css build request
-			scheduleBuild = false;
-			spawnMake();
-		} else if ( 0 === code ) {
-			// 'make build-css' success
-			changedFiles = updateChangedCssFiles();
-			if ( 0 !== changedFiles.length ) {
-				debug( chalk.green( 'css reload' ) );
-				io.of( '/css-hot-reload' ).emit( 'css-hot-reload', {
-					status: 'reload',
-					changedFiles: changedFiles,
-				} );
-			} else {
-				debug( chalk.green( 'css up to date' ) );
-				io.of( '/css-hot-reload' ).emit( 'css-hot-reload', { status: 'up-to-date' } );
-			}
-		} else {
-			// 'make build-css' failed
-			debug( chalk.red( 'css build failed' ) );
-			io.of( '/css-hot-reload' ).emit( 'css-hot-reload', {
-				status: 'build-failed',
-				error: errors,
-			} );
-		}
-	}
-
-	/**
-	 * Updates the MD5 hash of the public CSS files and returns the changed ones
-	 * in a list.
-	 * @returns {Array} an array of changed files (string)
-	 */
-	function updateChangedCssFiles() {
-		let hash, filePath;
-		const changedFiles = [];
-		for ( filePath in publicCssFiles ) {
-			hash = md5File.sync( filePath );
-			if ( hash !== publicCssFiles[ filePath ] ) {
-				publicCssFiles[ filePath ] = hash;
-				changedFiles.push( path.basename( filePath ) );
-			}
-		}
-
-		return changedFiles;
-	}
-
-	// Initialize publicCssFiles
-
-	fs.readdirSync( PUBLIC_DIR ).forEach( function( file ) {
-		if ( '.css' === file.slice( -4 ) ) {
-			const fullPath = path.join( PUBLIC_DIR, file );
-			publicCssFiles[ fullPath ] = md5File.sync( fullPath );
-		}
-	} );
-
-	// Watch .scss files
-
-	const watcher = chokidar.watch( SCSS_PATHS, {
-		ignored: /^.*\.(js[x]?|md|json|unison\.tmp)$/,
-		usePolling: false,
-		persistent: true,
-	} );
-
-	// The add/addDir events are fired during initialization generating an
-	// avalanche of refreshes on the client so we stick to the change events
-	// only for now until we can exclude the initial add/addDir events.
-	watcher.on( 'all', function( event, filepath ) {
-		if ( 'change' === event && filepath.match( /^.*\.scss$/ ) ) {
-			spawnMake();
-		}
-	} );
-}
-
-/**
- * Module exports
- */
-module.exports = setup;

server/bundler/hot-reloader.js
@@ -1,88 +1,0 @@
-/** @format */
-/*
- *  WARNING: ES5 code only here. Not transpiled! ****
- */
-
-/**
- * External dependencies
- */
-const socketio = require( 'socket.io' );
-const debug = require( 'debug' )( 'calypso:bundler:hot-reloader' );
-const { memoize } = require( 'lodash' );
-
-/**
- * Internal dependencies
- */
-const cssHotReloader = require( './css-hot-reload' );
-
-let io = null,
-	_stats = null;
-
-function invalidPlugin() {
-	if ( io ) {
-		io.emit( 'invalid' );
-	}
-}
-
-const getStats = memoize( stats => stats.toJson() );
-getStats.cache = new WeakMap(); // play a bit nicer with the GC than the default lodash cache
-
-function sendStats( socket, stats, force ) {
-	function emitted( asset ) {
-		return ! asset.emitted;
-	}
-
-	if ( ! force && stats && stats.assets && stats.assets.every( emitted ) ) {
-		return socket.emit( 'still-ok' );
-	}
-
-	socket.emit( 'hash', stats.hash );
-
-	if ( stats.errors.length > 0 ) {
-		socket.emit( 'errors', stats.errors );
-	} else if ( stats.warnings.length > 0 ) {
-		socket.emit( 'warnings', stats.warnings );
-	} else {
-		socket.emit( 'ok' );
-	}
-}
-
-const hotReloader = {
-	listen: function( server, webpackCompiler ) {
-		io = socketio.listen( server, { 'log level': 1 } );
-		io.sockets.on( 'connection', function( socket ) {
-			socket.emit( 'hot' );
-			if ( ! _stats ) {
-				return;
-			}
-			sendStats( socket, getStats( _stats ), true );
-		} );
-
-		webpackCompiler.plugin( 'compile', invalidPlugin );
-		webpackCompiler.plugin( 'invalid', invalidPlugin );
-		webpackCompiler.plugin( 'done', function( stats ) {
-			if ( ! io ) {
-				return;
-			}
-			sendStats( io.sockets, getStats( stats ) );
-			_stats = stats;
-		} );
-
-		// CSS hot reloading logic
-
-		io.of( '/css-hot-reload' ).on( 'connection', function() {
-			debug( '/css-hot-reload new connection' );
-		} );
-
-		cssHotReloader( io );
-	},
-
-	close: function() {
-		if ( io ) {
-			io.close();
-			io = null;
-		}
-	},
-};
-
-module.exports = hotReloader;

commit 912a71fc26f8fc37415b3346970d4eb9a6eb2e29

Fixup host parameter in server.listen call
Broken in ESLint cleanup in #35934.

index.js
@@ -81,7 +81,7 @@ const server = createServer();
 
 // The desktop app runs Calypso in a fork. Let non-forks listen on any host.
 
-server.listen( { port, host: process.env.CALYPSO_IS_FORK ? null : host }, function() {
+server.listen( { port, host: process.env.CALYPSO_IS_FORK ? host : null }, function() {
 	// Tell the parent process that Calypso has booted.
 	sendBootStatus( 'ready' );
 } );