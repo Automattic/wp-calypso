308690527a5d320eea0b4f5570c9d1986af1fddc
Aurorum <torres126@clubpenguinmountains.com>
2019-09-04 22:32:50 +0100

    Push some changes

--- client/me/help/help-flow/index.jsx
+++ client/me/help/help-flow/index.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import { get } from 'lodash';
@@ -11,6 +11,7 @@ import page from 'page';
 /**
  * Internal dependencies
  */
+import Accordion from 'components/accordion';
 import Card from 'components/card';
 import Dialog from 'components/dialog';
 import Button from 'components/button';
@@ -23,7 +24,6 @@ import MeSidebarNavigation from 'me/sidebar-navigation';
 import hasJetpackSites from 'state/selectors/has-jetpack-sites';
 import userHasAnyAtomicSites from 'state/selectors/user-has-any-atomic-sites';
 import { getCurrentUserSiteCount } from 'state/current-user/selectors';
-import userHasAnyPaidPlans from 'state/selectors/user-has-any-paid-plans';
 import getJetpackSites from 'state/selectors/get-jetpack-sites';
 
 /**
@@ -41,7 +41,6 @@ class HelpFlow extends Component {
 			displayDialog: false,
 			blogIssueDialog: false,
 			jetpackDialog: false,
-			wooDialog: false,
 		} );
 	};
 
@@ -49,12 +48,8 @@ class HelpFlow extends Component {
 		this.setState( { displayDialog: true, jetpackDialog: true } );
 	};
 
-	handleWooCommerceButtonClick = () => {
-		this.setState( { displayDialog: true, wooDialog: true } );
-	};
-
-	handleBlogIssueButtonClick = () => {
-		this.setState( { displayDialog: true, blogIssueDialog: true } );
+	handleOtherClick = () => {
+		this.setState( { displayDialog: true, otherDialog: true } );
 	};
 
 	siteFilter = site => {
@@ -68,15 +63,9 @@ class HelpFlow extends Component {
 	};
 
 	render() {
-		const {
-			siteCount,
-			jetpackSiteCount,
-			hasAtomicSites,
-			hasPaidPlans,
-			translate,
-		} = this.props;
+		const { siteCount, jetpackSiteCount, hasAtomicSites, translate } = this.props;
 
-		const { displayDialog, jetpackDialog, wooDialog, blogIssueDialog } = this.state;
+		const { displayDialog, jetpackDialog, otherDialog } = this.state;
 
 		const hasWpComSites = siteCount - jetpackSiteCount > 0;
 
@@ -84,42 +73,70 @@ class HelpFlow extends Component {
 			page( `/help/contact/form` );
 		}
 
-		const dialogClassNames = classnames( 'help-flow__dialog', {
-			'has-site-selector': blogIssueDialog && hasJetpackSites && hasWpComSites,
+		const helpFlowClassNames = classnames( 'help-flow', {
+			'has-site-selector': hasJetpackSites && hasWpComSites,
 		} );
 
-		let dialogText;
-		let buttonLink;
-		let buttonText;
+		let dialogContent;
 
 		if ( jetpackDialog ) {
-			buttonLink = 'https://jetpack.com/contact-support/';
-			buttonText = translate( 'Contact Jetpack support' );
-			dialogText = translate(
-				'Please contact the Jetpack support team directly for issues regarding the plugin, ' +
-					'or explore the Jetpack support documentation.'
+			dialogContent = (
+				<p>
+					{ translate(
+						'For issues with the Jetpack plugin, contact the Jetpack support team directly or {{a}}take a look at the Jetpack support documentation{{/a}}.',
+						{
+							components: {
+								a: <a href="https://jetpack.com/support/" />,
+							},
+						}
+					) }
+				</p>
 			);
-		} else if ( wooDialog ) {
-			buttonLink = 'https://woocommerce.com/contact-us/';
-			buttonText = translate( 'Contact WooCommerce support' );
-			dialogText = translate(
-				'Please contact the WooCommerce support team directly for issues regarding the Store service or the plugin.'
-			);
-		} else if ( blogIssueDialog && ! hasWpComSites ) {
-			buttonLink = 'https://wordpress.org/support/';
-			buttonText = translate( 'Explore self-hosted site support' );
-			dialogText = translate(
-				"You don't have a site hosted at WordPress.com! " +
-					"If you're having an issue with a particular theme or plugin, contact its developer. " +
-					'Otherwise, explore the self-hosted forums for community help, or contact your hosting provider.'
-			);
-		} else if ( blogIssueDialog && hasWpComSites ) {
-			buttonLink = 'https://wordpress.org/support/';
-			buttonText = translate( 'Explore self-hosted site support' );
-			dialogText = translate(
-				'Are you experencing an issue with one of the sites listed? ' +
-					'If so, as your site is not hosted on WordPress.com, you should instead visit the self-hosted forums, ' +
-					'or alternatively contact your hosting provider.'
+		} else if ( otherDialog ) {
+			dialogContent = (
+				<Fragment>
+					<Accordion title={ translate( 'I have an issue with WooCommerce' ) }>
+						{ translate(
+							'For Store or WooCommerce questions, {{a}}contact the Woo support team directly{{/a}}.',
+							{
+								components: {
+									a: <a href="https://woocommerce.com/contact-us/" />,
+								},
+							}
+						) }
+					</Accordion>
+
+					<Accordion title={ translate( 'I have an issue with a custom theme/plugin' ) }>
+						{ translate(
+							'For issues with a custom theme or a custom plugin, contact that developer directly.'
+						) }
+					</Accordion>
+
+					<Accordion title={ translate( 'I have an issue with the WordPress app' ) }>
+						{ translate(
+							'For issues with the WordPress apps, use the in-help support located under "Help & Support" to provide additional information necessary for debugging the issue.'
+						) }
+					</Accordion>
+
+					<Accordion title={ translate( 'I have an issue WordAds' ) }>
+						{ translate( 'For issues with WordAds, contact us {{a}}here{{/a}}.', {
+							components: {
+								a: <a href="/help/contact/form" />,
+							},
+						} ) }
+					</Accordion>
+
+					<Accordion title={ translate( 'I have an issue with my dashboard on WordPress.com' ) }>
+						{ translate(
+							'For issues with your WordPress.com dashboard, contact us {{a}}here{{/a}}.',
+							{
+								components: {
+									a: <a href="/help/contact/form" />,
+								},
+							}
+						) }
+					</Accordion>
+				</Fragment>
 			);
 		}
 
@@ -128,9 +145,9 @@ class HelpFlow extends Component {
 				<DocumentHead title={ translate( 'Help' ) } />
 				<MeSidebarNavigation />
 				<HeaderCake onClick={ this.backToHelp } isCompact>
-					{ this.props.translate( 'Contact Us' ) }
+					{ translate( 'Contact Us' ) }
 				</HeaderCake>
-				<Card className="help-flow">
+				<Card className={ helpFlowClassNames }>
 					<div className="help-flow__wrapper">
 						<img
 							className="help-flow__illustration"
@@ -157,57 +174,60 @@ class HelpFlow extends Component {
 						</div>
 					</div>
 					<div className="help-flow__button-wrapper">
-						<Button className="help-flow__button" href="/help/contact/form">
-							WordPress.com
-						</Button>
+						{ hasWpComSites && (
+							<Button className="help-flow__button is-wpcom" href="/help/contact/form">
+								{ translate( 'My WordPress.com site' ) }
+							</Button>
+						) }
 						{ hasJetpackSites && (
-							<Button className="help-flow__button" onClick={ this.handleJetpackButtonClick }>
-								Jetpack
+							<Button
+								className="help-flow__button is-jetpack"
+								primary
+								onClick={ this.handleJetpackButtonClick }
+							>
+								{ translate( 'My Jetpack-connected site' ) }
 							</Button>
 						) }
-						{ hasPaidPlans && (
-							<Button className="help-flow__button" href="/help/contact/form">
-								WordAds
-							</Button>
-						) }
-						{ ( hasJetpackSites || hasAtomicSites ) && (
-							<Button className="help-flow__button" onClick={ this.handleWooCommerceButtonClick }>
-								WooCommerce
-							</Button>
-						) }
-						<Button className="help-flow__button" onClick={ this.handleBlogIssueButtonClick }>
-							{ translate( 'An issue with my blog' ) }
+						<Button className="help-flow__button is-link" onClick={ this.handleOtherClick }>
+							{ translate( 'Other' ) }
 						</Button>
 					</div>
+					{ hasJetpackSites && hasWpComSites && (
+						<div className="help-flow__site-selector">
+							<p className="help-flow__site-selector-subtitle">
+								{ translate( 'Your Jetpack-connected sites:' ) }
+							</p>
+							<SiteSelector filter={ this.siteFilter } displaySearch={ false } />
+						</div>
+					) }
 				</Card>
 
 				{ displayDialog && (
-					<Dialog isVisible additionalClassNames={ dialogClassNames }>
+					<Dialog isVisible additionalClassNames="help-flow__dialog">
 						<div className="help-flow__dialog-text">
 							<h1 className="help-flow__dialog-heading">
-								{ blogIssueDialog && hasWpComSites
-									? translate( 'Which blog are you having issues with?' )
-									: translate( 'We wish we could help more…' ) }
+								{ ! jetpackDialog
+									? translate( 'How can we help?' )
+									: translate( 'We think we could provide more help elsewhere…' ) }
 							</h1>
-							<p>{ dialogText }</p>
+							{ dialogContent }
 						</div>
 						<div className="help-flow__dialog-buttons">
+							<Button className="is-link" href="/help/contact/form">
+								{ ! jetpackDialog
+									? translate( "I'd like to discuss something else and speak to a human" )
+									: translate( "I'd like to speak with a human anyway" ) }
+							</Button>
 							<Button className="help-flow__close-dialog" onClick={ this.handleDialogClosure }>
 								{ translate( 'Close' ) }
 							</Button>
-							<Button primary href={ buttonLink }>
-								<span>{ buttonText }</span>
-								<Gridicon icon="external" />
-							</Button>
-							{ blogIssueDialog && hasWpComSites && (
-								<Button className="help-flow__not-listed-button is-link" href="/help/contact/form">
-									{ translate( "The site I'm having issues with isn't listed" ) }
+							{ jetpackDialog && (
+								<Button primary onClick={ this.handleDialogClosure }>
+									{ translate( 'Contact Jetpack Support' ) }
+									<Gridicon icon="external" />
 								</Button>
 							) }
 						</div>
-						{ hasJetpackSites && hasWpComSites && blogIssueDialog && (
-							<SiteSelector filter={ this.siteFilter } displaySearch={ false } />
-						) }
 					</Dialog>
 				) }
 			</Main>
@@ -218,7 +238,6 @@ class HelpFlow extends Component {
 export default connect( state => ( {
 	hasJetpackSites: hasJetpackSites( state ),
 	hasAtomicSites: userHasAnyAtomicSites( state ),
-	hasPaidPlans: userHasAnyPaidPlans( state ),
 	siteCount: getCurrentUserSiteCount( state ),
 	jetpackSiteCount: getJetpackSites( state ).length,
 } ) )( localize( HelpFlow ) );
