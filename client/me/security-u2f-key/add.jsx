e956bf7a39f693da65e8e51d14a38ff04786588f
Xiao Yu <me@xyu.io>
2019-09-12 17:04:48 +0000

    Remove old U2F code

--- client/me/security-u2f-key/add.jsx
+++ client/me/security-u2f-key/add.jsx
@@ -12,7 +12,6 @@ import { localize } from 'i18n-calypso';
  */
 import Button from 'components/button';
 import Card from 'components/card';
-import u2f from './u2f-api';
 import webauthn from 'lib/webauthn';
 import Spinner from 'components/spinner';
 
@@ -42,34 +41,21 @@ class SecurityU2fKeyAdd extends React.Component {
 		this.props.onRegister();
 	};
 
-	createChallenge() {
-		return 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'.replace( /[xy]/g, function( c ) {
-			const r = ( Math.random() * 16 ) | 0,
-				v = c === 'x' ? r : ( r & 0x3 ) | 0x8;
-			return v.toString( 16 );
-		} );
-	}
-
 	render() {
-		window.u2f = u2f;
 		return (
 			<Card>
-				{ !! u2f && (
-					<Fragment>
-						<div className="security-u2f-key__add-wait-for-key">
-							<Spinner />
-							<p>{ this.props.translate( 'Insert your USB key into your USB port.' ) }</p>
-							<p>
-								{ this.props.translate(
-									'Then tap the button or gold disk on the security device'
-								) }
-							</p>
-						</div>
-						<div className="security-u2f-key__add-button-container">
-							<Button onClick={ this.props.onCancel }>Cancel</Button>
-						</div>
-					</Fragment>
-				) }
+				<Fragment>
+					<div className="security-u2f-key__add-wait-for-key">
+						<Spinner />
+						<p>{ this.props.translate( 'Insert your USB key into your USB port.' ) }</p>
+						<p>
+							{ this.props.translate( 'Then tap the button or gold disk on the security device' ) }
+						</p>
+					</div>
+					<div className="security-u2f-key__add-button-container">
+						<Button onClick={ this.props.onCancel }>Cancel</Button>
+					</div>
+				</Fragment>
 			</Card>
 		);
 	}
