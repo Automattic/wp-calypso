7fdd936efb058add1ca5f9f06da2478b9245b02c
Jon Surrell <jon.surrell@automattic.com>
2019-09-12 01:08:15 -0400

    Add ignore pattern

--- test/packages/jest.config.js
+++ test/packages/jest.config.js
@@ -1,6 +1,9 @@
+const { defaults } = require( 'jest-config' );
+
 module.exports = {
 	preset: '@automattic/calypso-build',
 	// run tests for all packages that have a Jest config file
 	projects: [ '<rootDir>/packages/*/jest.config.js' ],
 	rootDir: './../../',
+	testPathIgnorePatterns: [ ...defaults.testPathIgnorePatterns, '/^<rootDir>/packages/dist//' ],
 };
