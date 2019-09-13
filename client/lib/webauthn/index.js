9f94753025822e93f31e1cad4fd6f8881f473e5b
Jeff Bowen <jblz@users.noreply.github.com>
2019-09-13 10:41:45 -0400

    Code style & lint fixes: * Format webauthn/index.js with prettier * remove unused vars * let instead of var

--- client/lib/webauthn/index.js
+++ client/lib/webauthn/index.js
@@ -1,39 +1,43 @@
+/**
+ * Internal dependencies
+ */
 import wpcom from 'lib/wp';
-var _backend;
+
+let _backend;
 
 const POST = 'POST';
 
-function strToBin(str) {
-	str = str.replace(/[-_]/g, function(m) { return m[0] === '-' ? '+' : '/'; });
-	return Uint8Array.from(atob(str), c => c.charCodeAt(0));
+function strToBin( str ) {
+	str = str.replace( /[-_]/g, function( m ) {
+		return m[ 0 ] === '-' ? '+' : '/';
+	} );
+	return Uint8Array.from( atob( str ), c => c.charCodeAt( 0 ) );
 }
 
-function binToStr(bin) {
-	return btoa(new Uint8Array(bin).reduce(
-		(s, byte) => s + String.fromCharCode(byte), ''
-	));
+function binToStr( bin ) {
+	return btoa( new Uint8Array( bin ).reduce( ( s, byte ) => s + String.fromCharCode( byte ), '' ) );
 }
 
 function isBrowser() {
 	try {
-		if (!window) return false;
-	} catch (err) {
+		if ( ! window ) return false;
+	} catch ( err ) {
 		return false;
 	}
 	return true;
 }
 
-function credentialListConversion(list) {
-	return list.map(item => {
+function credentialListConversion( list ) {
+	return list.map( item => {
 		const cred = {
 			type: item.type,
-			id: strToBin(item.id)
+			id: strToBin( item.id ),
 		};
-		if ('transports' in item && item.transports.length > 0) {
+		if ( 'transports' in item && item.transports.length > 0 ) {
 			cred.transports = list.transports;
 		}
 		return cred;
-	});
+	} );
 }
 
 function wpcomRequestPromise( path, _data, method ) {
@@ -42,7 +46,7 @@ function wpcomRequestPromise( path, _data, method ) {
 		data.hostname = window.location.hostname;
 	}
 
-	return new Promise( function ( resolve, reject ) {
+	return new Promise( function( resolve, reject ) {
 		const promise = function( err, result ) {
 			if ( err ) {
 				reject( err );
@@ -59,83 +63,91 @@ function wpcomRequestPromise( path, _data, method ) {
 }
 
 function isSupported() {
-	if ( !_backend ) {
-		_backend = new Promise( function( resolve, reject ) {
+	if ( ! _backend ) {
+		_backend = new Promise( function( resolve ) {
 			function notSupported() {
 				resolve( { webauthn: null } );
 			}
-			if ( !isBrowser() ) {
+			if ( ! isBrowser() ) {
 				return notSupported();
 			}
-			if ( !window.isSecureContext ) {
+			if ( ! window.isSecureContext ) {
 				return notSupported();
 			}
-			if ( window.PublicKeyCredential === undefined ||
-			    typeof window.PublicKeyCredential !== "function" ||
-			    typeof window.PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable !== "function" ) {
-				    return notSupported();
-			    }
-			    resolve( { webauthn: true } );
+			if (
+				window.PublicKeyCredential === undefined ||
+				typeof window.PublicKeyCredential !== 'function' ||
+				typeof window.PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable !==
+					'function'
+			) {
+				return notSupported();
+			}
+			resolve( { webauthn: true } );
 		} );
 	}
-	return _backend.then( backend => !!backend.webauthn );
+	return _backend.then( backend => !! backend.webauthn );
 }
 
-function register( reactComponent ) {
+function register() {
 	return wpcomRequestPromise( '/me/two-step/security-key/registration_challenge' )
-	.then( (options) => {
-		const makeCredentialOptions = {};
-		makeCredentialOptions.rp = options.rp;
-		makeCredentialOptions.user = options.user;
-		makeCredentialOptions.user.id = strToBin(options.user.id);
-		makeCredentialOptions.challenge = strToBin(options.challenge);
-		makeCredentialOptions.pubKeyCredParams = options.pubKeyCredParams;
+		.then( options => {
+			const makeCredentialOptions = {};
+			makeCredentialOptions.rp = options.rp;
+			makeCredentialOptions.user = options.user;
+			makeCredentialOptions.user.id = strToBin( options.user.id );
+			makeCredentialOptions.challenge = strToBin( options.challenge );
+			makeCredentialOptions.pubKeyCredParams = options.pubKeyCredParams;
 
-		if ( 'timeout' in options ) {
-			makeCredentialOptions.timeout = options.timeout;
-		}
-		if ('excludeCredentials' in options) {
-			makeCredentialOptions.excludeCredentials = credentialListConversion(options.excludeCredentials);
-		}
-		if ('authenticatorSelection' in options) {
-			makeCredentialOptions.authenticatorSelection = options.authenticatorSelection;
-		}
-		if ('attestation' in options) {
-			makeCredentialOptions.attestation = options.attestation;
-		}
-		if ('extensions' in options) {
-			makeCredentialOptions.extensions = options.extensions;
-		}
-		return navigator.credentials.create({
-			"publicKey": makeCredentialOptions
-		});
-	}).then(attestation => {
-		const publicKeyCredential = {};
-		if ('id' in attestation) {
-			publicKeyCredential.id = attestation.id;
-		}
-		if ('type' in attestation) {
-			publicKeyCredential.type = attestation.type;
-		}
-		if ('rawId' in attestation) {
-			publicKeyCredential.rawId = binToStr(attestation.rawId);
-		}
-		if (!attestation.response) {
-			Promise.reject( 'response lacking "response" attribute' );
-		}
-		const response = {};
-		response.clientDataJSON = binToStr(attestation.response.clientDataJSON);
-		response.attestationObject = binToStr(attestation.response.attestationObject);
-		publicKeyCredential.response = response;
+			if ( 'timeout' in options ) {
+				makeCredentialOptions.timeout = options.timeout;
+			}
+			if ( 'excludeCredentials' in options ) {
+				makeCredentialOptions.excludeCredentials = credentialListConversion(
+					options.excludeCredentials
+				);
+			}
+			if ( 'authenticatorSelection' in options ) {
+				makeCredentialOptions.authenticatorSelection = options.authenticatorSelection;
+			}
+			if ( 'attestation' in options ) {
+				makeCredentialOptions.attestation = options.attestation;
+			}
+			if ( 'extensions' in options ) {
+				makeCredentialOptions.extensions = options.extensions;
+			}
+			return navigator.credentials.create( {
+				publicKey: makeCredentialOptions,
+			} );
+		} )
+		.then( attestation => {
+			const publicKeyCredential = {};
+			if ( 'id' in attestation ) {
+				publicKeyCredential.id = attestation.id;
+			}
+			if ( 'type' in attestation ) {
+				publicKeyCredential.type = attestation.type;
+			}
+			if ( 'rawId' in attestation ) {
+				publicKeyCredential.rawId = binToStr( attestation.rawId );
+			}
+			if ( ! attestation.response ) {
+				Promise.reject( 'response lacking "response" attribute' );
+			}
+			const response = {};
+			response.clientDataJSON = binToStr( attestation.response.clientDataJSON );
+			response.attestationObject = binToStr( attestation.response.attestationObject );
+			publicKeyCredential.response = response;
 
-		return wpcomRequestPromise( '/me/two-step/security-key/registration_validate', {
-			data: JSON.stringify(publicKeyCredential)
-		}, POST);
-	});
+			return wpcomRequestPromise(
+				'/me/two-step/security-key/registration_validate',
+				{
+					data: JSON.stringify( publicKeyCredential ),
+				},
+				POST
+			);
+		} );
 }
 
-function authenticate() {
+function authenticate() {}
 
-}
-
-export default { isSupported, register, authenticate }
+export default { isSupported, register, authenticate };
