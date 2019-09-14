7ad3747ac90405a9bba00964f200a9c6acecc358
Alexander Concha <alex@buayacorp.com>
2019-09-14 15:21:43 -0400

    Update the data attribute to avoid (potentially) breaking e2e tests.

--- client/blocks/login/two-factor-authentication/two-factor-actions.jsx
+++ client/blocks/login/two-factor-authentication/two-factor-actions.jsx
@@ -80,7 +80,7 @@ class TwoFactorActions extends Component {
 		return (
 			<Card className="two-factor-authentication__actions">
 				{ isSecurityKeyAvailable && (
-					<Button n data-e2e-link="2fa-sms-link" onClick={ this.recordSecurityKey }>
+					<Button n data-e2e-link="2fa-security-key-link" onClick={ this.recordSecurityKey }>
 						{ translate( 'Continue with your security\u00A0key' ) }
 					</Button>
 				) }
