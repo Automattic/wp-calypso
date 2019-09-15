0b8bb4e6e768f92a1b456ba17aa19c104aa1b792
James Tien <southp0105@gmail.com>
2019-09-15 10:45:30 -0400

    <SidebarMenu> should use <ul> instead of <li>

--- client/layout/sidebar/menu.jsx
+++ client/layout/sidebar/menu.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 import classNames from 'classnames';
 
 const SidebarMenu = ( { children, className } ) => (
-	<li className={ classNames( 'sidebar__menu', className ) }>{ children }</li>
+	<ul className={ classNames( 'sidebar__menu', className ) }>{ children }</ul>
 );
 
 export default SidebarMenu;
