a3cbaac3af8ed546bfbba96091620b128804ce1d
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-09-15 12:37:54 -0400

    Fix the Upgrade nudge banner to now show for VIP sites.

--- client/blocks/upgrade-nudge/index.jsx
+++ client/blocks/upgrade-nudge/index.jsx
@@ -21,6 +21,7 @@ import { isFreePlan } from 'lib/products-values';
 import TrackComponentView from 'lib/analytics/track-component-view';
 import { recordTracksEvent } from 'state/analytics/actions';
 import canCurrentUser from 'state/selectors/can-current-user';
+import isVipSite from 'state/selectors/is-vip-site';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { getSite } from 'state/sites/selectors';
 
@@ -87,9 +88,11 @@ export class UpgradeNudge extends React.Component {
 			site,
 			title,
 			translate,
+			isVip,
 		} = this.props;
 
 		const shouldNotDisplay =
+			isVip ||
 			! canManageSite ||
 			( ! site || typeof site !== 'object' || typeof site.jetpack !== 'boolean' ) ||
 			( feature && planHasFeature ) ||
@@ -155,6 +158,7 @@ export default connect(
 			site: getSite( state, siteId ),
 			planHasFeature: hasFeature( state, siteId, ownProps.feature ),
 			canManageSite: canCurrentUser( state, siteId, 'manage_options' ),
+			isVip: isVipSite( state, siteId ),
 		};
 	},
 	{ recordTracksEvent }
