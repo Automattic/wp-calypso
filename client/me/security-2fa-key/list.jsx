ff1837da4ebc417e1706a6b7057f8509d1ec0d16
Alexander Concha <alex@buayacorp.com>
2019-09-25 12:51:29 +0200

    Convert to function compontents.

--- client/me/security-2fa-key/list.jsx
+++ client/me/security-2fa-key/list.jsx
@@ -17,29 +17,27 @@ import Security2faKeyItem from './item';
  */
 import './style.scss';
 
-class Security2faKeyList extends React.Component {
-	static propTypes = {
-		securityKeys: PropTypes.array.isRequired,
-		onDelete: PropTypes.func.isRequired,
-	};
-
-	render() {
-		return (
-			<div className="security-2fa-key__active-keys">
-				<ul className="security-2fa-key__list">
-					{ this.props.securityKeys.map( securityKey => (
-						<li key={ securityKey.id } className="security-2fa-key__list-item">
-							<Security2faKeyItem securityKey={ securityKey } onDelete={ this.props.onDelete } />
-						</li>
-					) ) }
-				</ul>
-			</div>
-		);
-	}
+function Security2faKeyList( props ) {
+	return (
+		<div className="security-2fa-key__active-keys">
+			<ul className="security-2fa-key__list">
+				{ props.securityKeys.map( securityKey => (
+					<li key={ securityKey.id } className="security-2fa-key__list-item">
+						<Security2faKeyItem securityKey={ securityKey } onDelete={ props.onDelete } />
+					</li>
+				) ) }
+			</ul>
+		</div>
+	);
 }
 
+Security2faKeyList.propTypes = {
+	securityKeys: PropTypes.array.isRequired,
+	onDelete: PropTypes.func.isRequired,
+};
+
 export default connect(
-	state => ( { state } ),
+	null,
 	{
 		recordGoogleEvent,
 	}
