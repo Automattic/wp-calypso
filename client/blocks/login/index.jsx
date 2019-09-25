8505c260c6ece095662fc9ac52d1e92b67186cf2
Alex Concha <xknown@users.noreply.github.com>
2019-09-25 05:53:21 -0400

    Update client/blocks/login/index.jsx
    Remove unnecessary curly brackets
    
    Co-Authored-By: Jon Surrell <jon.surrell@automattic.com>

--- client/blocks/login/index.jsx
+++ client/blocks/login/index.jsx
@@ -367,7 +367,7 @@ class Login extends Component {
 		if ( twoFactorEnabled && twoFactorAuthType === 'webauthn' && this.state.isBrowserSupported ) {
 			return (
 				<div>
-					<SecurityKeyForm twoFactorAuthType={ 'webauthn' } onSuccess={ this.handleValid2FACode } />
+					<SecurityKeyForm twoFactorAuthType="webauthn" onSuccess={ this.handleValid2FACode } />
 				</div>
 			);
 		}
