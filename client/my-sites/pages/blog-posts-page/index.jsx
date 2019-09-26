85a141c5ee24f71012b68878e064ab68ce8ca128
Caroline Moore <calobee@gmail.com>
2019-09-25 20:51:20 -0400

    Further simplify the component, removing duplicated checks for homepage set as posts page, and add event tracking on the homepage callout click

--- client/my-sites/pages/blog-posts-page/index.jsx
+++ client/my-sites/pages/blog-posts-page/index.jsx
@@ -5,7 +5,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { identity } from 'lodash';
+import { identity, noop } from 'lodash';
 import { localize } from 'i18n-calypso';
 import Gridicon from 'components/gridicon';
 import classNames from 'classnames';
@@ -16,6 +16,7 @@ import classNames from 'classnames';
 import Card from 'components/card';
 import { getSiteFrontPageType, getSitePostsPage, getSiteFrontPage } from 'state/sites/selectors';
 import isSiteUsingFullSiteEditing from 'state/selectors/is-site-using-full-site-editing';
+import { recordTracksEvent } from 'state/analytics/actions';
 
 /**
  * Style dependencies
@@ -25,40 +26,32 @@ import './style.scss';
 class BlogPostsPage extends React.Component {
 	static propTypes = {
 		site: PropTypes.object,
-		pages: PropTypes.array,
+		recordCalloutClick: PropTypes.func,
 	};
 
 	static defaultProps = {
 		translate: identity,
+		recordCalloutClick: noop,
 	};
 
-	getPageProperty( { pageId, property } ) {
-		return this.props.pages
-			.filter( page => page.ID === pageId )
-			.map( page => page[ property ] )
-			.shift();
-	}
-
-	getPostsPageLink( { isCurrentlySetAsHomepage } ) {
-		if ( ! isCurrentlySetAsHomepage ) {
-			return this.getPageProperty( { pageId: this.props.postsPage, property: 'URL' } );
-		}
-
+	getPostsPageLink() {
 		return this.props.site.URL;
 	}
 
-	renderPostsPageInfo( { isCurrentlySetAsHomepage } ) {
+	renderPostsPageInfo() {
 		const { translate } = this.props;
 
-		if ( isCurrentlySetAsHomepage ) {
-			return (
-				<span>
-					<Gridicon size={ 12 } icon="house" className="blog-posts-page__front-page-icon" />
-					{ translate( 'The homepage is showing your latest posts.' ) }
-				</span>
-			);
-		}
+		return (
+			<span>
+				<Gridicon size={ 12 } icon="house" className="blog-posts-page__front-page-icon" />
+				{ translate( 'The homepage is showing your latest posts.' ) }
+			</span>
+		);
 	}
+
+	recordCalloutClick = () => {
+		this.props.recordCalloutClick( this.props.site.ID );
+	};
 
 	render() {
 		const { isFullSiteEditing } = this.props;
@@ -75,12 +68,11 @@ class BlogPostsPage extends React.Component {
 
 		return (
 			<Card
-				href={ this.getPostsPageLink( {
-					isCurrentlySetAsHomepage,
-				} ) }
+				href={ this.getPostsPageLink() }
 				target="_blank"
 				rel="noopener noreferrer"
 				className="blog-posts-page"
+				onClick={ this.recordCalloutClick }
 			>
 				<div className="blog-posts-page__details">
 					<div
@@ -88,9 +80,7 @@ class BlogPostsPage extends React.Component {
 							'blog-posts-page__info': true,
 						} ) }
 					>
-						{ this.renderPostsPageInfo( {
-							isCurrentlySetAsHomepage,
-						} ) }
+						{ this.renderPostsPageInfo() }
 					</div>
 				</div>
 			</Card>
@@ -98,12 +88,21 @@ class BlogPostsPage extends React.Component {
 	}
 }
 
-export default connect( ( state, props ) => {
-	return {
-		frontPageType: getSiteFrontPageType( state, props.site.ID ),
-		isFrontPage: getSiteFrontPageType( state, props.site.ID ) === 'posts',
-		postsPage: getSitePostsPage( state, props.site.ID ),
-		frontPage: getSiteFrontPage( state, props.site.ID ),
-		isFullSiteEditing: isSiteUsingFullSiteEditing( state, props.site.ID ),
-	};
-} )( localize( BlogPostsPage ) );
+const mapDispatchToProps = dispatch => ( {
+	recordCalloutClick: siteId => {
+		dispatch( recordTracksEvent( 'calypso_pages_blog_posts_callout_click', { blog_id: siteId } ) );
+	},
+} );
+
+export default connect(
+	( state, props ) => {
+		return {
+			frontPageType: getSiteFrontPageType( state, props.site.ID ),
+			isFrontPage: getSiteFrontPageType( state, props.site.ID ) === 'posts',
+			postsPage: getSitePostsPage( state, props.site.ID ),
+			frontPage: getSiteFrontPage( state, props.site.ID ),
+			isFullSiteEditing: isSiteUsingFullSiteEditing( state, props.site.ID ),
+		};
+	},
+	mapDispatchToProps
+)( localize( BlogPostsPage ) );
