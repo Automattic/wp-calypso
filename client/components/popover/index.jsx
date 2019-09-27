Automattic/wp-calypso

commit 1c709492178eafb26e99ba31fd25558495495913

Popover: remove the rootClassName prop (#36297)
* Popover: remove the rootClassName prop

Lot of code that passes it to nested components, but no actual usage.

* Popover: improve tabIndex value, remove isFocusOnShow prop

To focus a non-interactive element and make VoiceOver announce it, `tabIndex="-1"`
is the right thing to do, rather than `tabIndex="0"`.

The `isFocusOnShow` prop can be removed, as having `tabIndex="-1"` is always a good
thing to do, no downside.

client/components/popover/index.jsx
@@ -45,7 +45,6 @@ class Popover extends Component {
 		closeOnEsc: PropTypes.bool,
 		id: PropTypes.string,
 		ignoreContext: PropTypes.shape( { getDOMNode: PropTypes.function } ),
-		isFocusOnShow: PropTypes.bool,
 		isRtl: PropTypes.bool,
 		isVisible: PropTypes.bool,
 		position: PropTypes.oneOf( [
@@ -58,7 +57,6 @@ class Popover extends Component {
 			'left',
 			'top left',
 		] ),
-		rootClassName: PropTypes.string,
 		showDelay: PropTypes.number,
 		onClose: PropTypes.func,
 		onShow: PropTypes.func,
@@ -76,7 +74,6 @@ class Popover extends Component {
 		autoRtl: true,
 		className: '',
 		closeOnEsc: true,
-		isFocusOnShow: false,
 		isRtl: false,
 		isVisible: false,
 		position: 'top',
@@ -284,7 +281,7 @@ class Popover extends Component {
 	}
 
 	focusPopover() {
-		if ( ! this.props.isFocusOnShow || ! this.popoverNode ) {
+		if ( ! this.popoverNode ) {
 			return null;
 		}
 
@@ -502,14 +499,15 @@ class Popover extends Component {
 		this.debug( 'rendering ...' );
 
 		return (
-			<RootChild
-				aria-label={ this.props[ 'aria-label' ] }
-				className={ this.props.rootClassName }
-				id={ this.id }
-				role="tooltip"
-				tabIndex={ this.props.isFocusOnShow ? 0 : null }
-			>
-				<div style={ this.getStylePosition() } className={ classes }>
+			<RootChild>
+				<div
+					aria-label={ this.props[ 'aria-label' ] }
+					id={ this.id }
+					role="tooltip"
+					tabIndex="-1"
+					style={ this.getStylePosition() }
+					className={ classes }
+				>
 					<div className="popover__arrow" />
 					<div ref={ this.setDOMBehavior } className="popover__inner">
 						{ this.props.children }

client/blocks/blog-stickers/index.jsx
@@ -34,7 +34,7 @@ const BlogStickers = ( { blogId, teams, stickers } ) => {
 		<div className="blog-stickers">
 			<QueryBlogStickers blogId={ blogId } />
 			{ isTeamMember && stickers && stickers.length > 0 && (
-				<InfoPopover rootClassName="blog-stickers__popover">
+				<InfoPopover>
 					<BlogStickersList stickers={ stickers } />
 				</InfoPopover>
 			) }

client/blocks/calendar-button/index.jsx
@@ -26,7 +26,6 @@ class CalendarButton extends Component {
 		events: PropTypes.array,
 		ignoreContext: PropTypes.shape( { getDOMNode: PropTypes.function } ),
 		isVisible: PropTypes.bool,
-		rootClassName: PropTypes.string,
 		selectedDay: PropTypes.object,
 		showDelay: PropTypes.number,
 		siteId: PropTypes.number,
@@ -85,7 +84,6 @@ class CalendarButton extends Component {
 			'ignoreContext',
 			'isVisible',
 			'modifiers',
-			'rootClassName',
 			'selectedDay',
 			'showDelay',
 			'siteId',

client/blocks/calendar-popover/index.jsx
@@ -34,7 +34,6 @@ class CalendarPopover extends Component {
 		ignoreContext: PropTypes.shape( { getDOMNode: PropTypes.function } ),
 		isVisible: PropTypes.bool,
 		position: PropTypes.string,
-		rootClassName: PropTypes.string,
 		showDelay: PropTypes.number,
 		onClose: PropTypes.func,
 		onShow: PropTypes.func,
@@ -99,7 +98,6 @@ class CalendarPopover extends Component {
 			'ignoreContext',
 			'isVisible',
 			'position',
-			'rootClassName',
 			'showDelay',
 			'onClose',
 			'onShow',

client/components/info-popover/index.jsx
@@ -42,7 +42,6 @@ export default class InfoPopover extends Component {
 			'left',
 			'top left',
 		] ),
-		rootClassName: PropTypes.string,
 	};
 
 	static defaultProps = {
@@ -83,11 +82,9 @@ export default class InfoPopover extends Component {
 					aria-label={ translate( 'More information' ) }
 					onClick={ this.handleClick }
 					ref={ this.iconRef }
-					className={ classNames(
-						'info-popover',
-						{ 'is-active': this.state.showPopover },
-						this.props.className
-					) }
+					className={ classNames( 'info-popover', this.props.className, {
+						'is-active': this.state.showPopover,
+					} ) }
 				>
 					<Gridicon icon={ this.props.icon } size={ this.props.iconSize } />
 				</button>
@@ -101,7 +98,6 @@ export default class InfoPopover extends Component {
 						position={ this.props.position }
 						onClose={ this.handleClose }
 						className={ classNames( 'info-popover__tooltip', this.props.className ) }
-						rootClassName={ this.props.rootClassName }
 					>
 						{ this.props.children }
 					</Popover>

client/components/tooltip/index.jsx
@@ -37,7 +37,6 @@ function Tooltip( props ) {
 		<Popover
 			autoPosition={ props.autoPosition }
 			className={ classes }
-			rootClassName={ props.rootClassName }
 			context={ props.context }
 			id={ props.id }
 			isVisible={ props.isVisible }
@@ -55,7 +54,6 @@ Tooltip.propTypes = {
 	id: PropTypes.string,
 	isVisible: PropTypes.bool,
 	position: PropTypes.string,
-	rootClassName: PropTypes.string,
 	status: PropTypes.string,
 	showDelay: PropTypes.number,
 	showOnMobile: PropTypes.bool,

client/components/domains/search-filters/dropdown-filters.jsx
@@ -185,7 +185,6 @@ export class DropdownFilters extends Component {
 				className="search-filters__popover"
 				context={ this.button.current }
 				id={ popoverId }
-				isFocusOnShow
 				isVisible={ this.state.showPopover }
 				onClose={ this.handleFiltersSubmit }
 				position="bottom left"

client/components/popover/menu.jsx
@@ -21,7 +21,6 @@ class PopoverMenu extends Component {
 		onClose: PropTypes.func.isRequired,
 		position: PropTypes.string,
 		className: PropTypes.string,
-		rootClassName: PropTypes.string,
 		popoverComponent: PropTypes.func,
 		popoverTitle: PropTypes.string, // used by ReaderPopover
 		customPosition: PropTypes.object,
@@ -50,7 +49,6 @@ class PopoverMenu extends Component {
 			isVisible,
 			popoverTitle,
 			position,
-			rootClassName,
 		} = this.props;
 
 		return (
@@ -64,7 +62,6 @@ class PopoverMenu extends Component {
 				isVisible={ isVisible }
 				popoverTitle={ popoverTitle }
 				position={ position }
-				rootClassName={ rootClassName }
 			>
 				<div
 					ref={ this.menu }

commit febf7815ccc4125ba360645fc88d7902955ff447

Fix a bug where Popover is focused too early, before it's fully rendered
Fixes a regression introduced in #36297 where we started focusing all
`Popover` elements right after they are rendered.

The focusing, however, happens in a ref callback, at a time when the element
is not yet fully rendered (I don't understand the low-level details ATM) and
focusing it scrolls to the top of the page.

A quick fix uses a `defer` call. That's in this patch. A correct patch would
move the focus call to a lifecycle method (`componentDid{Mount,Update}`), but
that's too much to ask on Friday evening.

client/components/popover/index.jsx
@@ -10,7 +10,7 @@ import { connect } from 'react-redux';
 import debugFactory from 'debug';
 import classNames from 'classnames';
 import clickOutside from 'click-outside';
-import { uniqueId } from 'lodash';
+import { defer, uniqueId } from 'lodash';
 
 /**
  * Internal dependencies
@@ -308,7 +308,7 @@ class Popover extends Component {
 
 		this.setPosition();
 
-		this.focusPopover();
+		defer( () => this.focusPopover() );
 	}
 
 	getPositionClass( position = this.props.position ) {