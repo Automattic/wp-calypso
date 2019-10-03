3dd447833ef3620027476a21f52b614af5ac1c4c
Jarda Snajdr <jsnajdr@gmail.com>
2019-10-03 11:48:51 +0200

    Replace ui.language.isRtl Redux state with HOC that depends on i18n-calypso (#36443)
    * Add HOC/hook providers for current locale's RTL flag
    
    * Replace usages of isRtl selector with useRtl/withRtl
    
    * Remove the ui.language.rtl Redux state
    
    * Chart Bar: remove code that flips position of tooltip in RTL mode
    
    The `Popover` class already does that flipping internally (`adjustRtlPosition`)
    when `autoRtl` prop is enabled, which by default it is.
    
    * useRtl: rewrite the hook using the useSubscription library

--- client/components/chart/index.jsx
+++ client/components/chart/index.jsx
@@ -6,7 +6,6 @@ import React, { useState, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { localize } from 'i18n-calypso';
 import { noop } from 'lodash';
-import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
@@ -15,7 +14,7 @@ import { hasTouch } from 'lib/touch-detect';
 import { useWindowResizeCallback } from 'lib/track-element-size';
 import Tooltip from 'components/tooltip';
 import Notice from 'components/notice';
-import isRtlSelector from 'state/selectors/is-rtl';
+import { withRtl } from 'components/rtl';
 import BarContainer from './bar-container';
 
 /**
@@ -196,6 +195,4 @@ Chart.defaultProps = {
 	minTouchBarWidth: 42,
 };
 
-export default connect( state => ( {
-	isRtl: isRtlSelector( state ),
-} ) )( localize( Chart ) );
+export default withRtl( localize( Chart ) );
