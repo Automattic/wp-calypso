Automattic/wp-calypso

commit 1e53e418a9e6e0c6465395c810312cc4cf447f0b

Add login entrypoint (#33642)
* Add login entrypoint.

This change extracts login into its own entrypoint, with the minimal
required set of reducers and dependencies. This greatly reduces the
amount of JS required to boot `/log-in`.

* Login: fix CSS specificity of visited links

Explicitly specify color of `a:visited` link, otherwise it can be overridden by base styles.

* Don't use hydrate to render, init router inside user callback

* Render dev helpers (abtests and preferences) also in the Login page

Requires adding `preferences` reducer. That's not ideal, but I don't see a good way
around that.

* Disable waitTillFocused() for login form

client/landing/login/common.js
@@ -1,0 +1,97 @@
+/**
+ * External dependencies
+ */
+import { parse } from 'qs';
+import page from 'page';
+import url from 'url';
+import debugFactory from 'debug';
+
+/**
+ * Internal dependencies
+ */
+import config from 'config';
+import { setCurrentUser } from 'state/current-user/actions';
+
+const debug = debugFactory( 'calypso' );
+
+export function setupContextMiddleware() {
+	page( '*', ( context, next ) => {
+		// page.js url parsing is broken so we had to disable it with `decodeURLComponents: false`
+		const parsed = url.parse( context.canonicalPath, true );
+		context.prevPath = parsed.path === context.path ? false : parsed.path;
+		context.query = parsed.query;
+
+		context.hashstring = ( parsed.hash && parsed.hash.substring( 1 ) ) || '';
+		// set `context.hash` (we have to parse manually)
+		if ( context.hashstring ) {
+			try {
+				context.hash = parse( context.hashstring );
+			} catch ( e ) {
+				debug( 'failed to query-string parse `location.hash`', e );
+				context.hash = {};
+			}
+		} else {
+			context.hash = {};
+		}
+
+		// client version of the isomorphic method for redirecting to another page
+		context.redirect = ( httpCode, newUrl = null ) => {
+			if ( isNaN( httpCode ) && ! newUrl ) {
+				newUrl = httpCode;
+			}
+
+			return page.replace( newUrl, context.state, false, false );
+		};
+
+		// Break routing and do full load for logout link in /me
+		if ( context.pathname === '/wp-login.php' ) {
+			window.location.href = context.path;
+			return;
+		}
+
+		next();
+	} );
+}
+
+function renderDevHelpers( reduxStore ) {
+	if ( config.isEnabled( 'dev/test-helper' ) ) {
+		const testHelperEl = document.querySelector( '.environment.is-tests' );
+		if ( testHelperEl ) {
+			asyncRequire( 'lib/abtest/test-helper', testHelper => {
+				testHelper( testHelperEl );
+			} );
+		}
+	}
+
+	if ( config.isEnabled( 'dev/preferences-helper' ) ) {
+		const prefHelperEl = document.querySelector( '.environment.is-prefs' );
+		if ( prefHelperEl ) {
+			asyncRequire( 'lib/preferences-helper', prefHelper => {
+				prefHelper( prefHelperEl, reduxStore );
+			} );
+		}
+	}
+}
+
+export const configureReduxStore = ( currentUser, reduxStore ) => {
+	debug( 'Executing Calypso configure Redux store.' );
+
+	if ( currentUser.get() ) {
+		// Set current user in Redux store
+		reduxStore.dispatch( setCurrentUser( currentUser.get() ) );
+		currentUser.on( 'change', () => {
+			reduxStore.dispatch( setCurrentUser( currentUser.get() ) );
+		} );
+	}
+
+	if ( config.isEnabled( 'network-connection' ) ) {
+		asyncRequire( 'lib/network-connection', networkConnection =>
+			networkConnection.init( reduxStore )
+		);
+	}
+};
+
+export function setupMiddlewares( currentUser, reduxStore ) {
+	setupContextMiddleware( reduxStore );
+	renderDevHelpers( reduxStore );
+}

client/landing/login/index.js
@@ -1,0 +1,57 @@
+/**
+ * Global polyfills
+ */
+import 'boot/polyfills';
+import { render } from 'controller/web-util';
+
+/**
+ * External dependencies
+ */
+import debugFactory from 'debug';
+import page from 'page';
+
+/**
+ * Internal dependencies
+ */
+import createStore from './store';
+import { setupMiddlewares, configureReduxStore } from './common';
+import initLoginSection from 'login';
+import userFactory from 'lib/user';
+
+const debug = debugFactory( 'calypso' );
+
+import 'assets/stylesheets/style.scss';
+// goofy import for environment badge, which is SSR'd
+import 'components/environment-badge/style.scss';
+
+// Create Redux store
+const store = createStore();
+
+const boot = currentUser => {
+	debug( "Starting Calypso. Let's do this." );
+
+	configureReduxStore( currentUser, store );
+	setupMiddlewares( currentUser, store );
+
+	page( '*', ( context, next ) => {
+		context.store = store;
+		next();
+	} );
+
+	page.exit( '*', ( context, next ) => {
+		context.store = store;
+		next();
+	} );
+
+	initLoginSection( ( route, ...handlers ) => page( route, ...handlers, render ) );
+	page.start( { decodeURLComponents: false } );
+};
+
+window.AppBoot = () => {
+	const user = userFactory();
+	if ( user.initialized ) {
+		boot( user );
+	} else {
+		user.once( 'change', () => boot( user ) );
+	}
+};

server/pages/index.js
@@ -1,4 +1,3 @@
-/** @format */
 /**
  * External dependencies
  */
@@ -34,6 +33,7 @@ import sanitize from 'sanitize';
 import utils from 'bundler/utils';
 import { pathToRegExp } from '../../client/utils';
 import sections from '../../client/sections';
+import loginRouter, { LOGIN_SECTION_DEFINITION } from '../../client/login';
 import { serverRouter, getNormalizedPath } from 'isomorphic-routing';
 import { serverRender, renderJsx, attachBuildTimestamp, attachHead, attachI18n } from 'render';
 import stateCache from 'state-cache';
@@ -757,36 +757,38 @@ module.exports = function() {
 		res.send( pageHtml );
 	} );
 
+	function handleSectionPath( section, sectionPath, isEntrypoint = false ) {
+		const pathRegex = pathToRegExp( sectionPath );
+
+		app.get( pathRegex, function( req, res, next ) {
+			req.context = Object.assign( {}, req.context, { sectionName: section.name } );
+
+			if ( ! isEntrypoint && config.isEnabled( 'code-splitting' ) ) {
+				req.context.chunkFiles = getFilesForChunk( section.name, req );
+			} else {
+				req.context.chunkFiles = EMPTY_ASSETS;
+			}
+
+			if ( section.secondary && req.context ) {
+				req.context.hasSecondary = true;
+			}
+
+			if ( section.group && req.context ) {
+				req.context.sectionGroup = section.group;
+			}
+
+			next();
+		} );
+
+		if ( ! section.isomorphic ) {
+			app.get( pathRegex, setUpRoute, serverRender );
+		}
+	}
+
 	sections
 		.filter( section => ! section.envId || section.envId.indexOf( config( 'env_id' ) ) > -1 )
 		.forEach( section => {
-			section.paths.forEach( sectionPath => {
-				const pathRegex = pathToRegExp( sectionPath );
-
-				app.get( pathRegex, function( req, res, next ) {
-					req.context = Object.assign( {}, req.context, { sectionName: section.name } );
-
-					if ( config.isEnabled( 'code-splitting' ) ) {
-						req.context.chunkFiles = getFilesForChunk( section.name, req );
-					} else {
-						req.context.chunkFiles = EMPTY_ASSETS;
-					}
-
-					if ( section.secondary && req.context ) {
-						req.context.hasSecondary = true;
-					}
-
-					if ( section.group && req.context ) {
-						req.context.sectionGroup = section.group;
-					}
-
-					next();
-				} );
-
-				if ( ! section.isomorphic ) {
-					app.get( pathRegex, setUpRoute, serverRender );
-				}
-			} );
+			section.paths.forEach( sectionPath => handleSectionPath( section, sectionPath ) );
 
 			if ( section.isomorphic ) {
 				// section.load() uses require on the server side so we also need to access the
@@ -797,6 +799,21 @@ module.exports = function() {
 				section.load().default( serverRouter( app, setUpRoute, section ) );
 			}
 		} );
+
+	function loginRouteSetup( req, res, next ) {
+		req.context = getDefaultContext( req );
+		const target = getBuildTargetFromRequest( req );
+		req.context.entrypoint = getFilesForEntrypoint( target, 'entry-login' );
+		setUpCSP( req, res, () =>
+			req.context.isLoggedIn
+				? setUpLoggedInRoute( req, res, next )
+				: setUpLoggedOutRoute( req, res, next )
+		);
+	}
+
+	// Set up login routing.
+	handleSectionPath( LOGIN_SECTION_DEFINITION, '/log-in', true );
+	loginRouter( serverRouter( app, loginRouteSetup, null ) );
 
 	// This is used to log to tracks Content Security Policy violation reports sent by browsers
 	app.post(

client/controller/index.node.js
@@ -1,9 +1,6 @@
-/** @format */
-
 /**
  * External dependencies
  */
-
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 
@@ -32,7 +29,6 @@ const ReduxWrappedLoggedOutLayout = ( { store, primary, secondary, redirectUri }
  * `context.primary` and `context.secondary` to populate it.
  */
 export const makeLayout = makeLayoutMiddleware( ReduxWrappedLoggedOutLayout );
-export const makeLoggedOutLayout = makeLayout;
 
 export function redirectLoggedIn( { isLoggedIn, res }, next ) {
 	// TODO: Make it work also for development env

client/controller/index.web.js
@@ -44,16 +44,6 @@ export const ReduxWrappedLayout = ( { store, primary, secondary, redirectUri } )
 
 export const makeLayout = makeLayoutMiddleware( ReduxWrappedLayout );
 
-const ReduxWrappedLoggedOutLayout = ( { store, primary, secondary, redirectUri } ) => (
-	<ReduxProvider store={ store }>
-		<MomentProvider>
-			<LayoutLoggedOut primary={ primary } secondary={ secondary } redirectUri={ redirectUri } />
-		</MomentProvider>
-	</ReduxProvider>
-);
-
-export const makeLoggedOutLayout = makeLayoutMiddleware( ReduxWrappedLoggedOutLayout );
-
 /**
  * Isomorphic routing helper, client side
  *

client/landing/login/store.js
@@ -1,0 +1,51 @@
+/**
+ * External dependencies
+ */
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+/**
+ * Internal dependencies
+ */
+import wpcomApiMiddleware from 'state/data-layer/wpcom-api-middleware';
+import { reducer as httpData, enhancer as httpDataEnhancer } from 'state/data-layer/http-data';
+import { combineReducers } from 'state/utils';
+import application from 'state/application/reducer';
+import documentHead from 'state/document-head/reducer';
+import login from 'state/login/reducer';
+import language from 'state/ui/language/reducer';
+import route from 'state/ui/route/reducer';
+import masterbarVisibility from 'state/ui/masterbar-visibility/reducer';
+import section from 'state/ui/section/reducer';
+import notices from 'state/notices/reducer';
+import i18n from 'state/i18n/reducer';
+import users from 'state/users/reducer';
+import currentUser from 'state/current-user/reducer';
+import preferences from 'state/preferences/reducer';
+
+// Create Redux store
+const reducer = combineReducers( {
+	application,
+	documentHead,
+	httpData,
+	login,
+	notices,
+	i18n,
+	users,
+	currentUser,
+	preferences,
+	ui: combineReducers( {
+		language,
+		route,
+		masterbarVisibility,
+		section,
+	} ),
+} );
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () =>
+	createStore(
+		reducer,
+		composeEnhancers( httpDataEnhancer, applyMiddleware( thunkMiddleware, wpcomApiMiddleware ) )
+	);

client/layout/logged-out.jsx
@@ -1,9 +1,6 @@
-/** @format */
-
 /**
  * External dependencies
  */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -25,6 +22,11 @@ import getCurrentQueryArguments from 'state/selectors/get-current-query-argument
 import { getSection, masterbarIsVisible } from 'state/ui/selectors';
 import BodySectionCssClass from './body-section-css-class';
 import GdprBanner from 'blocks/gdpr-banner';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
 
 // Returns true if given section should display sidebar for logged out users.
 const hasSidebar = section => {

client/login/index.node.js
@@ -1,13 +1,15 @@
-/** @format */
-
 /**
  * Internal dependencies
  */
-
 import config from 'config';
 import webRouter from './index.web';
 import { lang } from './controller';
 import { makeLayout, redirectLoggedIn, setUpLocale } from 'controller';
+
+/**
+ * Re-exports
+ */
+export { LOGIN_SECTION_DEFINITION } from './index.web';
 
 export default router => {
 	if ( config.isEnabled( 'login/magic-login' ) ) {

commit 4a982f1320168f35f7b1efbe725693fb76bc887c

Fix redirect when logging in.
The `redirect_to` URL parameter was being ignored during login, due to a
bug introduced by #33642, where an important setup action was no longer
being dispatched.

This PR fixes that, while moving the action to its separate file, in
order to keep bundle sizes small.

Fixes #36518.

client/landing/login/common.js
@@ -11,6 +11,7 @@ import debugFactory from 'debug';
  */
 import config from 'config';
 import { setCurrentUser } from 'state/current-user/actions';
+import setRouteAction from 'state/ui/actions/set-route';
 
 const debug = debugFactory( 'calypso' );
 
@@ -91,7 +92,16 @@ export const configureReduxStore = ( currentUser, reduxStore ) => {
 	}
 };
 
+const setRouteMiddleware = reduxStore => {
+	page( '*', ( context, next ) => {
+		reduxStore.dispatch( setRouteAction( context.pathname, context.query ) );
+
+		next();
+	} );
+};
+
 export function setupMiddlewares( currentUser, reduxStore ) {
 	setupContextMiddleware( reduxStore );
+	setRouteMiddleware( reduxStore );
 	renderDevHelpers( reduxStore );
 }

client/state/ui/actions/index.js
@@ -3,7 +3,6 @@
  */
 import {
 	SELECTED_SITE_SET,
-	ROUTE_SET,
 	PREVIEW_IS_SHOWING,
 	NOTIFICATIONS_PANEL_TOGGLE,
 	NAVIGATE,
@@ -15,8 +14,9 @@ import 'state/data-layer/wpcom/sites/jitm';
 /**
  * Re-exports
  */
-export { setSection } from './section/actions';
-export { showMasterbar, hideMasterbar } from './masterbar-visibility/actions';
+export { default as setRoute } from './set-route';
+export { setSection } from '../section/actions';
+export { showMasterbar, hideMasterbar } from '../masterbar-visibility/actions';
 
 /**
  * Returns an action object to be used in signalling that a site has been set
@@ -42,21 +42,6 @@ export function setAllSitesSelected() {
 	return {
 		type: SELECTED_SITE_SET,
 		siteId: null,
-	};
-}
-
-/**
- * Returns an action object signalling that the current route is to be changed
- *
- * @param  {String} path    Route path
- * @param  {Object} [query] Query arguments
- * @return {Object}         Action object
- */
-export function setRoute( path, query = {} ) {
-	return {
-		type: ROUTE_SET,
-		path,
-		query,
 	};
 }
 

client/state/ui/actions/set-route.js
@@ -1,0 +1,19 @@
+/**
+ * Internal dependencies
+ */
+import { ROUTE_SET } from 'state/action-types';
+
+/**
+ * Returns an action object signalling that the current route is to be changed
+ *
+ * @param  {String} path    Route path
+ * @param  {Object} [query] Query arguments
+ * @return {Object}         Action object
+ */
+export default function setRoute( path, query = {} ) {
+	return {
+		type: ROUTE_SET,
+		path,
+		query,
+	};
+}