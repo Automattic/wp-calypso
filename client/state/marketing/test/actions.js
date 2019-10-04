10ba34f9a6010757117b32dfef802bb91dacd210
James Tien <southp0105@gmail.com>
2019-10-04 14:32:33 +0800

    Update `/marketing/test/actions.js` accordingly.

--- client/state/marketing/test/actions.js
+++ client/state/marketing/test/actions.js
@@ -7,9 +7,11 @@ import { MARKETING_CLICK_UPGRADE_NUDGE } from 'state/action-types';
 describe( 'clickUpgradeNudge()', () => {
 	test( 'should return the expected action object', () => {
 		const nudgeName = 'profit!';
+		const siteId = 123;
 
-		expect( clickUpgradeNudge( nudgeName ) ).toEqual( {
+		expect( clickUpgradeNudge( siteId, nudgeName ) ).toEqual( {
 			type: MARKETING_CLICK_UPGRADE_NUDGE,
+			siteId,
 			nudgeName,
 		} );
 	} );
