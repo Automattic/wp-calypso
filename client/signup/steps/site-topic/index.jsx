93682fbb7942feb4b75046198155492a833cb6d4
Philip Jackson <p-jackson@live.com>
2019-10-08 17:11:07 +1300

    Create new sites with themes suggested by vertical

--- client/signup/steps/site-topic/index.jsx
+++ client/signup/steps/site-topic/index.jsx
@@ -39,9 +39,9 @@ class SiteTopicStep extends Component {
 		this.props.saveSignupStep( { stepName: this.props.stepName } );
 	}
 
-	submitSiteTopic = ( { isUserInput, name, slug } ) => {
+	submitSiteTopic = ( { isUserInput, name, slug, suggestedTheme } ) => {
 		const { flowName, stepName } = this.props;
-		this.props.submitSiteVertical( { isUserInput, name, slug }, stepName );
+		this.props.submitSiteVertical( { isUserInput, name, slug }, stepName, suggestedTheme );
 		this.props.goToNextStep( flowName );
 	};
 
