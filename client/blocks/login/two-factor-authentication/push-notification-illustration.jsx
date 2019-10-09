23254ad73270933ecbc4f4fc50ffef42969b5391
Sérgio Gomes <mail@sgomes.com>
2019-10-09 12:03:09 +0100

    Replace illustration time with hardcoded string.

--- client/blocks/login/two-factor-authentication/push-notification-illustration.jsx
+++ client/blocks/login/two-factor-authentication/push-notification-illustration.jsx
@@ -67,14 +67,6 @@ function NotificationSvg() {
 }
 
 export default function PushNotificationIllustration() {
-	const now = new Date();
-	const hours = now.getHours() || 12;
-	const minutes = now.getMinutes();
-
-	const currentTime = `${ hours > 12 ? hours - 12 : hours }:${
-		minutes < 10 ? '0' + minutes : minutes
-	}`;
-
 	// Inlining two stacked SVGs because they’re a part of an animated image.
 	// By not loading them externally, we’re making sure the animation will
 	// get fired right away with all of its elements in place.
@@ -82,7 +74,7 @@ export default function PushNotificationIllustration() {
 	return (
 		<div className="two-factor-authentication__illustration" aria-hidden="true">
 			<DeviceSvg />
-			<div className="two-factor-authentication__illustration-screen">{ currentTime }</div>
+			<div className="two-factor-authentication__illustration-screen">10:07</div>
 			<div className="two-factor-authentication__illustration-notification-container">
 				<NotificationSvg />
 			</div>
