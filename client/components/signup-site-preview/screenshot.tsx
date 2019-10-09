3ecb2cd71739c181f9dff55e343dbdb160ac816d
Philip Jackson <p-jackson@live.com>
2019-10-09 21:17:03 +1300

    Fix preview click track event

--- client/components/signup-site-preview/screenshot.tsx
+++ client/components/signup-site-preview/screenshot.tsx
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { noop } from 'lodash';
 import React from 'react';
 
 /**
@@ -11,29 +12,34 @@ import Spinner from 'components/spinner';
 import classNames from 'classnames';
 
 interface Props {
-	isPhone: boolean;
+	defaultViewportDevice: string;
 	setWrapperHeight: ( height: number ) => void;
 	screenshotUrl: string;
 	scrolling: boolean;
+	onPreviewClick: ( viewportSize: string ) => void;
 	translate: typeof import('i18n-calypso').translate;
 }
 
 export default function SignupSitePreviewScreenshot( {
-	isPhone,
+	defaultViewportDevice,
 	setWrapperHeight,
 	scrolling,
 	screenshotUrl,
+	onPreviewClick = noop,
 	translate,
 }: Props ) {
 	const className = classNames( {
 		'signup-site-preview__scrolling-screenshot': scrolling,
 	} );
 
+	const isPhone = defaultViewportDevice === 'phone';
+
 	return (
 		<div className={ className }>
 			<ImagePreloader
 				src={ screenshotUrl }
 				placeholder={ <Spinner size={ isPhone ? 20 : 40 } /> }
+				onClick={ () => onPreviewClick( defaultViewportDevice ) }
 				onLoad={ ( e: any ) => setWrapperHeight( e.target.height ) }
 				alt={
 					isPhone
