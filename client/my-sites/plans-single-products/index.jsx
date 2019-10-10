9cc45c79ff6e96b84d3de9a79de177a280a96976
Piotr Delawski <piotr.delawski@xwp.co>
2019-10-10 15:55:24 +0200

    Fetch and pass product properties in the parent component

--- client/my-sites/plans-single-products/index.jsx
+++ client/my-sites/plans-single-products/index.jsx
@@ -2,28 +2,69 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import { isEnabled } from 'config';
 import Product from './product';
+import { getCurrentUserCurrencyCode } from 'state/current-user/selectors';
 
 /**
  * Style dependencies
  */
 import './style.scss';
 
-const PlansSingleProducts = () => {
+const PlansSingleProducts = props => {
+	const { billingTimeFrame, currencyCode, isPlaceholder, productProperties } = props;
 	const displayBackup = isEnabled( 'plans/jetpack-backup' );
 	const displayScan = isEnabled( 'plans/jetpack-scan' );
 
 	return (
 		<div className="plans-single-products">
-			{ displayScan && <Product productSlug="jetpack-scan" /> }
-			{ displayBackup && <Product productSlug="jetpack-backup" /> }
+			{ displayScan && (
+				<Product
+					billingTimeFrame={ billingTimeFrame }
+					currencyCode={ currencyCode }
+					isPlaceholder={ isPlaceholder }
+					{ ...productProperties.jetpackScan }
+				/>
+			) }
+			{ displayBackup && (
+				<Product
+					billingTimeFrame={ billingTimeFrame }
+					currencyCode={ currencyCode }
+					isPlaceholder={ isPlaceholder }
+					{ ...productProperties.jetpackBackup }
+				/>
+			) }
 		</div>
 	);
 };
 
-export default PlansSingleProducts;
+export default connect( state => {
+	return {
+		billingTimeFrame: 'per year',
+		currencyCode: getCurrentUserCurrencyCode( state ),
+		isPlaceholder: false,
+		productProperties: {
+			jetpackScan: {
+				discountedPrice: 10,
+				fullPrice: 16,
+				moreInfoLabel: 'More info',
+				productDescription:
+					'Automatic scanning and one-click fixes keep your site one step ahead of security threats.',
+				title: 'Jetpack Scan',
+			},
+			jetpackBackup: {
+				discountedPrice: 16,
+				fullPrice: 25,
+				moreInfoLabel: 'Which one do I need?',
+				productDescription:
+					'Always-on backups ensure you never lose your site. Choose from real-time or daily backups.',
+				title: 'Jetpack Backup',
+			},
+		},
+	};
+} )( PlansSingleProducts );
--- client/my-sites/plans-single-products/product.jsx
+++ client/my-sites/plans-single-products/product.jsx
@@ -3,15 +3,12 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { localize } from 'i18n-calypso';
 import classNames from 'classnames';
 
 /**
  * Internal dependencies
  */
 import PlanPrice from 'my-sites/plan-price';
-import { getCurrentUserCurrencyCode } from 'state/current-user/selectors';
 
 class PlansSingleProduct extends Component {
 	static propTypes = {
@@ -88,30 +85,4 @@ class PlansSingleProduct extends Component {
 	}
 }
 
-export default connect( ( state, { productSlug } ) => {
-	const productProperties = {
-		'jetpack-scan': {
-			discountedPrice: 10,
-			fullPrice: 16,
-			moreInfoLabel: 'More info',
-			productDescription:
-				'Automatic scanning and one-click fixes keep your site one step ahead of security threats.',
-			title: 'Jetpack Scan',
-		},
-		'jetpack-backup': {
-			discountedPrice: 16,
-			fullPrice: 25,
-			moreInfoLabel: 'Which one do I need?',
-			productDescription:
-				'Always-on backups ensure you never lose your site. Choose from real-time or daily backups.',
-			title: 'Jetpack Backup',
-		},
-	};
-
-	return {
-		billingTimeFrame: 'per year',
-		currencyCode: getCurrentUserCurrencyCode( state ),
-		isPlaceholder: false,
-		...productProperties[ productSlug ],
-	};
-} )( localize( PlansSingleProduct ) );
+export default PlansSingleProduct;
