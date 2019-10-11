6ae708b3f81f2b0b357d730a4b206684ae5e7410
Copons <copons@users.noreply.github.com>
2019-10-11 15:23:50 +0100

    Add attributes to the Navigation Menu block

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/navigation-menu/index.php
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/navigation-menu/index.php
@@ -14,6 +14,49 @@ namespace A8C\FSE;
  * @return string
  */
 function render_navigation_menu_block( $attributes ) {
+	$styles = '';
+
+	$class = 'menu-primary-container';
+	if ( isset( $attributes['className'] ) ) {
+		$class .= ' ' . $attributes['className'];
+	}
+
+	$align = ' alignwide';
+	if ( isset( $attributes['align'] ) ) {
+		$align = empty( $attributes['align'] ) ? '' : ' align' . $attributes['align'];
+	}
+	$class .= $align;
+
+	if ( isset( $attributes['textAlign'] ) ) {
+		$class .= ' has-text-align-' . $attributes['textAlign'];
+	} else {
+		$class .= ' has-text-align-center';
+	}
+
+	if ( isset( $attributes['textColor'] ) ) {
+		$class .= ' has-text-color';
+		$class .= ' has-' . $attributes['textColor'] . '-color';
+	} elseif ( isset( $attributes['customTextColor'] ) ) {
+		$class  .= ' has-text-color';
+		$styles .= ' color: ' . $attributes['customTextColor'] . ';';
+	}
+
+	if ( isset( $attributes['backgroundColor'] ) ) {
+		$class .= ' has-background';
+		$class .= ' has-' . $attributes['backgroundColor'] . '-background-color';
+	} elseif ( isset( $attributes['customBackgroundColor'] ) ) {
+		$class  .= ' has-background';
+		$styles .= ' background-color: ' . $attributes['customBackgroundColor'] . ';';
+	}
+
+	if ( isset( $attributes['fontSize'] ) ) {
+		$class .= ' has-' . $attributes['fontSize'] . '-font-size';
+	} elseif ( isset( $attributes['customFontSize'] ) ) {
+		$styles .= ' font-size: ' . $attributes['customFontSize'] . 'px;';
+	} else {
+		$class .= ' has-small-font-size';
+	}
+
 	$menu = wp_nav_menu(
 		[
 			'echo'           => false,
@@ -21,13 +64,9 @@ function render_navigation_menu_block( $attributes ) {
 			'items_wrap'     => '<ul id="%1$s" class="%2$s" aria-label="submenu">%3$s</ul>',
 			'menu_class'     => 'main-menu footer-menu',
 			'theme_location' => 'menu-1',
+			'container'      => '',
 		]
 	);
-
-	$align = ' alignwide';
-	if ( isset( $attributes['align'] ) ) {
-		$align = empty( $attributes['align'] ) ? '' : ' align' . $attributes['align'];
-	}
 
 	ob_start();
 	// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
@@ -41,7 +80,9 @@ function render_navigation_menu_block( $attributes ) {
 			<span class="hide-visually expanded-text"><?php esc_html_e( 'expanded', 'full-site-editing' ); ?></span>
 			<span class="hide-visually collapsed-text"><?php esc_html_e( 'collapsed', 'full-site-editing' ); ?></span>
 		</label>
-		<?php echo $menu ? $menu : get_fallback_navigation_menu(); ?>
+		<div class="<?php echo esc_attr( $class ); ?>" style="<?php echo esc_attr( $styles ); ?>">
+			<?php echo $menu ? $menu : get_fallback_navigation_menu(); ?>
+		</div>
 	</nav>
 	<!-- #site-navigation -->
 	<?php
@@ -75,7 +116,5 @@ function get_fallback_navigation_menu() {
 	$original_classes    = [ 'children', 'page_item_has_sub-menu' ];
 	$replacement_classes = [ 'sub-menu', 'menu-item-has-children' ];
 
-	return '<div class="menu-primary-container">'
-		. str_replace( $original_classes, $replacement_classes, $menu )
-		. '</div>';
+	return str_replace( $original_classes, $replacement_classes, $menu );
 }
