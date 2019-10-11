668cffba7a0ec52ce9552defb6722865805c3c34
Enej Bajgoric <enej.bajgoric@automattic.com>
2019-10-11 11:34:39 +0200

    Price Plan: Add support for range of prices (#36651)
    * Update to support a price range.
    
    * Better checking for not zero.
    
    * Update the spacing in the ' - '.
    
    * Updated the example text to be more clear and add % in the tax.
    
    * Incerase spacing on the tax.
    
    * Remove Fragment.
    
    * Make the price range option more translatable.
    
    * Updated example to be more clear and have space.

--- client/my-sites/plan-price/index.jsx
+++ client/my-sites/plan-price/index.jsx
@@ -25,6 +25,7 @@ export class PlanPrice extends Component {
 		const {
 			currencyCode,
 			rawPrice,
+			rawPriceRange,
 			original,
 			discounted,
 			className,
@@ -33,36 +34,94 @@ export class PlanPrice extends Component {
 			taxText,
 			translate,
 		} = this.props;
-		if ( ! currencyCode || ( rawPrice !== 0 && ! rawPrice ) ) {
+
+		let priceRange = false;
+		if ( rawPrice && rawPrice !== 0 ) {
+			priceRange = [
+				{
+					price: getCurrencyObject( rawPrice, currencyCode ),
+					raw: rawPrice,
+				},
+			];
+		}
+
+		if (
+			rawPriceRange &&
+			rawPriceRange[ 0 ] &&
+			rawPriceRange[ 1 ] &&
+			rawPriceRange[ 0 ] !== 0 &&
+			rawPriceRange[ 1 ] !== 0
+		) {
+			priceRange = [
+				{
+					price: getCurrencyObject( rawPriceRange[ 0 ], currencyCode ),
+					raw: rawPriceRange[ 0 ],
+				},
+				{
+					price: getCurrencyObject( rawPriceRange[ 1 ], currencyCode ),
+					raw: rawPriceRange[ 1 ],
+				},
+			];
+		}
+
+		if ( ! currencyCode || ! priceRange ) {
 			return null;
 		}
-		const price = getCurrencyObject( rawPrice, currencyCode );
+
 		const classes = classNames( 'plan-price', className, {
 			'is-original': original,
 			'is-discounted': discounted,
 		} );
 
+		const renderPrice = priceObj => {
+			const fraction = priceObj.raw - priceObj.price.integer > 0 && priceObj.price.fraction;
+			return `${ priceObj.price.integer }${ fraction }`;
+		};
+
 		if ( isInSignup ) {
+			const smallerPrice = renderPrice( priceRange[ 0 ] );
+			const higherPrice = priceRange[ 1 ] && renderPrice( priceRange[ 1 ] );
+
 			return (
 				<span className={ classes }>
-					{ price.symbol }
-					{ price.integer }
-					{ rawPrice - price.integer > 0 && price.fraction }
+					{ priceRange[ 0 ].price.symbol }
+					{ ! higherPrice && renderPrice( priceRange[ 0 ] ) }
+					{ higherPrice &&
+						translate( '%(smallerPrice)s-%(higherPrice)s', {
+							args: { smallerPrice, higherPrice },
+							comment: 'The price range for a particular product',
+						} ) }
 				</span>
 			);
 		}
+
+		const renderPriceHtml = priceObj => {
+			return (
+				<>
+					<span className="plan-price__integer">{ priceObj.price.integer }</span>
+					<sup className="plan-price__fraction">
+						{ priceObj.raw - priceObj.price.integer > 0 && priceObj.price.fraction }
+					</sup>
+				</>
+			);
+		};
 
 		const saleBadgeText = translate( 'Sale', {
 			comment: 'Shown next to a domain that has a special discounted sale price',
 		} );
 
+		const smallerPriceHtml = renderPriceHtml( priceRange[ 0 ] );
+		const higherPriceHtml = priceRange[ 1 ] && renderPriceHtml( priceRange[ 1 ] );
+
 		return (
 			<h4 className={ classes }>
-				<sup className="plan-price__currency-symbol">{ price.symbol }</sup>
-				<span className="plan-price__integer">{ price.integer }</span>
-				<sup className="plan-price__fraction">
-					{ rawPrice - price.integer > 0 && price.fraction }
-				</sup>
+				<sup className="plan-price__currency-symbol">{ priceRange[ 0 ].price.symbol }</sup>
+				{ ! higherPriceHtml && renderPriceHtml( priceRange[ 0 ] ) }
+				{ higherPriceHtml &&
+					translate( '{{smallerPrice/}}-{{higherPrice/}}', {
+						components: { smallerPrice: smallerPriceHtml, higherPrice: higherPriceHtml },
+						comment: 'The price range for a particular product',
+					} ) }
 				{ taxText && (
 					<sup className="plan-price__tax-amount">
 						{ translate( '(+%(taxText)s tax)', { args: { taxText } } ) }
@@ -78,6 +137,7 @@ export default localize( PlanPrice );
 
 PlanPrice.propTypes = {
 	rawPrice: PropTypes.number,
+	rawPriceRange: PropTypes.array,
 	original: PropTypes.bool,
 	discounted: PropTypes.bool,
 	currencyCode: PropTypes.string,
