aedda38e32636c67f5ef7dd2c60a017e1b7a9ffe
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-10-11 10:58:54 +0200

    Updated example to be more clear and have space

--- client/my-sites/plan-price/docs/example.jsx
+++ client/my-sites/plan-price/docs/example.jsx
@@ -17,7 +17,7 @@ function PlanPriceExample() {
 			<h3>Plan with standard price</h3>
 			<PlanPrice rawPrice={ 99 } />
 			<PlanPrice rawPriceRange={ [ 99, 140 ] } />
-
+			<br />
 			<h3>Plan with discounted price</h3>
 			<span style={ { display: 'flex' } }>
 				<PlanPrice rawPrice={ 8.25 } original />
@@ -25,14 +25,16 @@ function PlanPriceExample() {
 				<PlanPrice rawPriceRange={ [ 8.25, 20.23 ] } original />
 				<PlanPrice rawPriceRange={ [ 2.25, 3.5 ] } discounted />
 			</span>
-			<h3>Plan with discounted price with tax</h3>
+			<br />
+			<h3>Plan with discounted price and tax</h3>
 			<span style={ { display: 'flex' } }>
 				<PlanPrice rawPrice={ 8.25 } original taxText="10%" />
 				<PlanPrice rawPrice={ 2.25 } discounted taxText="10%" />
 				<PlanPrice rawPriceRange={ [ 8.25, 20.23 ] } original taxText="10%" />
 				<PlanPrice rawPriceRange={ [ 2.25, 3.5 ] } discounted taxText="10%" />
 			</span>
-			<h3>Plan with discounted price with tax simple version</h3>
+			<br />
+			<h3>Simple View (isInSignup) - Plan with discounted price and tax </h3>
 			<span style={ { display: 'flex' } }>
 				<PlanPrice rawPrice={ 8.25 } original taxText="10%" isInSignup={ true } />
 				<PlanPrice rawPrice={ 2.25 } discounted taxText="10%" isInSignup={ true } />
