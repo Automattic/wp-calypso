877d237811ad8e415b605fcd416ee7c53404c0d6
Piotr Delawski <delawski@users.noreply.github.com>
2019-10-11 11:13:37 +0200

    Plans Single Products: Add remaining UI elements and styles of the backup card (#36647)
    * Address code review feedback
    
    * Add single product UI elements (mobile, non-responsive for now)
    
    * Fetch and pass product properties in the parent component
    
    * Add Jetpack Backup product options along with styles. Refine CSS.

--- client/my-sites/plans-single-products/index.jsx
+++ client/my-sites/plans-single-products/index.jsx
@@ -2,33 +2,103 @@
  * External dependencies
  */
 import React from 'react';
-import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import { isEnabled } from 'config';
 import Product from './product';
+import { getCurrentUserCurrencyCode } from 'state/current-user/selectors';
 
 /**
  * Style dependencies
  */
 import './style.scss';
 
-const PlansSingleProducts = ( { translate } ) => {
+const PlansSingleProducts = props => {
+	const {
+		billingTimeFrame,
+		currencyCode,
+		isPlaceholder,
+		onProductSelect,
+		productProperties,
+	} = props;
 	const displayBackup = isEnabled( 'plans/jetpack-backup' );
 	const displayScan = isEnabled( 'plans/jetpack-scan' );
 
-	if ( ! displayBackup && ! displayScan ) {
-		return null;
-	}
-
 	return (
 		<div className="plans-single-products">
-			{ displayScan && <Product heading={ translate( 'Jetpack Scan' ) } /> }
-			{ displayBackup && <Product heading={ translate( 'Jetpack Backup' ) } /> }
+			{ displayScan && (
+				<Product
+					billingTimeFrame={ billingTimeFrame }
+					currencyCode={ currencyCode }
+					isPlaceholder={ isPlaceholder }
+					onSelect={ onProductSelect }
+					{ ...productProperties.jetpackScan }
+				/>
+			) }
+			{ displayBackup && (
+				<Product
+					billingTimeFrame={ billingTimeFrame }
+					currencyCode={ currencyCode }
+					isPlaceholder={ isPlaceholder }
+					onSelect={ onProductSelect }
+					{ ...productProperties.jetpackBackup }
+				/>
+			) }
 		</div>
 	);
 };
 
-export default localize( PlansSingleProducts );
+export default connect(
+	state => {
+		return {
+			billingTimeFrame: 'per year',
+			currencyCode: getCurrentUserCurrencyCode( state ),
+			isPlaceholder: false,
+			productProperties: {
+				jetpackScan: {
+					discountedPrice: 10,
+					fullPrice: 16,
+					moreInfoLabel: 'More info',
+					productDescription:
+						'Automatic scanning and one-click fixes keep your site one step ahead of security threats.',
+					slug: 'jetpack-scan',
+					title: 'Jetpack Scan',
+				},
+				jetpackBackup: {
+					discountedPrice: 16,
+					fullPrice: 25,
+					moreInfoLabel: 'Which one do I need?',
+					options: [
+						{
+							discountedPrice: 12,
+							fullPrice: 14,
+							slug: 'jetpack_backup_daily',
+							title: 'Daily Backups',
+						},
+						{
+							discountedPrice: 16,
+							fullPrice: 25,
+							slug: 'jetpack_backup_realtime',
+							title: 'Real-Time Backups',
+						},
+					],
+					optionsHeading: 'Backup Options:',
+					productDescription:
+						'Always-on backups ensure you never lose your site. Choose from real-time or daily backups.',
+					slug: 'jetpack-backup',
+					title: 'Jetpack Backup',
+				},
+			},
+		};
+	},
+	() => {
+		return {
+			onProductSelect: () => {
+				return null;
+			},
+		};
+	}
+)( PlansSingleProducts );
--- client/my-sites/plans-single-products/product.jsx
+++ client/my-sites/plans-single-products/product.jsx
@@ -3,22 +3,119 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { localize } from 'i18n-calypso';
+import classNames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import PlanPrice from 'my-sites/plan-price';
+import ProductOption from './product-option';
 
 class PlansSingleProduct extends Component {
 	static propTypes = {
-		heading: PropTypes.string.isRequired,
+		billingTimeFrame: PropTypes.string,
+		currencyCode: PropTypes.string,
+		discountedPrice: PropTypes.number,
+		fullPrice: PropTypes.number,
+		isPlaceholder: PropTypes.bool,
+		moreInfoLabel: PropTypes.string,
+		onSelect: PropTypes.func,
+		options: PropTypes.array,
+		optionsHeading: PropTypes.string,
+		productDescription: PropTypes.string,
+		slug: PropTypes.string,
+		title: PropTypes.string,
 	};
 
+	static defaultProps = {
+		isPlaceholder: false,
+	};
+
+	renderPriceGroup() {
+		const { currencyCode, discountedPrice, fullPrice, isPlaceholder } = this.props;
+		const isDiscounted = !! discountedPrice;
+
+		const priceGroupClasses = classNames( 'plans-single-products__price-group', {
+			'is-discounted': isDiscounted,
+			'is-placeholder': isPlaceholder,
+		} );
+
+		return (
+			<div className={ priceGroupClasses }>
+				<PlanPrice currencyCode={ currencyCode } rawPrice={ fullPrice } original />
+				<PlanPrice currencyCode={ currencyCode } rawPrice={ discountedPrice } discounted />
+			</div>
+		);
+	}
+
+	renderBillingTimeFrame() {
+		const { billingTimeFrame, isPlaceholder } = this.props;
+
+		const timeFrameClasses = classNames( 'plans-single-products__billing-timeframe', {
+			'is-placeholder': isPlaceholder,
+		} );
+
+		return <p className={ timeFrameClasses }>{ billingTimeFrame }</p>;
+	}
+
+	renderProductDescription() {
+		const { moreInfoLabel, isPlaceholder, productDescription } = this.props;
+
+		const productDescriptionClasses = classNames( 'plans-single-products__product-description', {
+			'is-placeholder': isPlaceholder,
+		} );
+
+		const moreInfo = moreInfoLabel ? <a href="/">{ moreInfoLabel }</a> : null;
+
+		return (
+			<p className={ productDescriptionClasses }>
+				{ productDescription } { moreInfo }
+			</p>
+		);
+	}
+
+	renderOptions() {
+		const { billingTimeFrame, currencyCode, onSelect, options, optionsHeading } = this.props;
+
+		if ( ! options ) {
+			return null;
+		}
+
+		return (
+			<div className="plans-single-products__options">
+				{ optionsHeading && (
+					<h4 className="plans-single-products__options-heading">{ optionsHeading }</h4>
+				) }
+				{ options.map( option => (
+					<ProductOption
+						key={ option.slug }
+						billingTimeFrame={ billingTimeFrame }
+						currencyCode={ currencyCode }
+						onSelect={ onSelect }
+						{ ...option }
+					/>
+				) ) }
+			</div>
+		);
+	}
+
 	render() {
-		const { heading } = this.props;
+		const { title } = this.props;
 
 		return (
 			<div className="plans-single-products__card">
-				<h3 className="plans-single-products__heading">{ heading }</h3>
+				<div className="plans-single-products__card-header">
+					<h3 className="plans-single-products__product-name">{ title }</h3>
+					{ this.renderPriceGroup() }
+					{ this.renderBillingTimeFrame() }
+				</div>
+				<div className="plans-single-products__card-content">
+					{ this.renderProductDescription() }
+					{ this.renderOptions() }
+				</div>
 			</div>
 		);
 	}
 }
 
-export default localize( PlansSingleProduct );
+export default PlansSingleProduct;
