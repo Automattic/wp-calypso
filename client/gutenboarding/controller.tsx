0426f67c56a569900f2dfe4753e329e8c6725566
Jon Surrell <jon.surrell@automattic.com>
2019-10-11 14:09:19 +0200

    Use PageJS.Callback types

--- client/gutenboarding/controller.tsx
+++ client/gutenboarding/controller.tsx
@@ -11,20 +11,20 @@ import page from 'page';
 import { Gutenboard } from './gutenboard';
 import { hideMasterbar as hideMasterbarAction } from 'state/ui/actions';
 
-export function redirectIfNotEnabled( context: PageJS.Context, next ) {
+export const redirectIfNotEnabled: PageJS.Callback = ( context, next ) => {
 	if ( ! config.isEnabled( 'gutenboarding' ) ) {
 		page.redirect( '/' );
 		return;
 	}
 	next();
-}
+};
 
-export function hideMasterbar( context: PageJS.Context, next ) {
+export const hideMasterbar: PageJS.Callback = ( context, next ) => {
 	context.store.dispatch( hideMasterbarAction() );
 	next();
-}
+};
 
-export function main( context: PageJS.Context, next ) {
+export const main: PageJS.Callback = ( context, next ) => {
 	context.primary = <Gutenboard />;
 	next();
-}
+};
