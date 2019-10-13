04fa03a5a1447ce66087c850d60b664ddfcb0734
Aurorum <torres126@clubpenguinmountains.com>
2019-10-13 13:53:12 +0100

    Fix linter

--- client/my-sites/people/people-list-section-header/index.jsx
+++ client/my-sites/people/people-list-section-header/index.jsx
@@ -14,7 +14,6 @@ import { localize } from 'i18n-calypso';
  * Internal dependencies
  */
 import SectionHeader from 'components/section-header';
-import InfoPopover from 'components/info-popover';
 import Button from 'components/button';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import getCurrentRoute from 'state/selectors/get-current-route';
