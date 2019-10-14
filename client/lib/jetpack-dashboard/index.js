77d620aad88c3a57559d0bd21e69a689f4e400d5
Marin Atanasov <tyxla@abv.bg>
2019-10-14 14:20:51 +0300

    Jetpack Dashboard: Update domains

--- client/lib/jetpack-dashboard/index.js
+++ client/lib/jetpack-dashboard/index.js
@@ -1,6 +1,9 @@
+const jetpackDashboardDomains = [ 'cloud.jetpack.com', 'my.jetpack.com' ];
+
 export function isJetpackDashboard() {
 	return !! (
 		process.env.JETPACK_DASHBOARD ||
-		( typeof window !== 'undefined' && 'dashboard.jetpack.com' === window.location.host )
+		( typeof window !== 'undefined' &&
+			jetpackDashboardDomains.indexOf( window.location.host ) >= 0 )
 	);
 }
