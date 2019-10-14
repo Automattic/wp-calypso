Automattic/wp-calypso

commit 8e015e356b6721840062a06361d1221127f8d26c

Add domain upsell nudge for plan owners with domain (#36208)
* Update functions that retrieve domain prices

Add new functions that return unformatted domain prices, and
update older functions to format the values returned from the
newly added functions.

* Add domain upsell nudge

Also modify render method to display domain upsell nudge only if
there are no other notices displayed.

client/lib/domains/index.js
@@ -234,17 +234,27 @@ function getDomainProductSlug( domain ) {
 	return `dot${ tldSlug }_domain`;
 }
 
-function getDomainPrice( slug, productsList, currencyCode ) {
+function getUnformattedDomainPrice( slug, productsList ) {
 	let price = get( productsList, [ slug, 'cost' ], null );
+
 	if ( price ) {
 		price += get( productsList, [ 'domain_map', 'cost' ], 0 );
+	}
+
+	return price;
+}
+
+function getDomainPrice( slug, productsList, currencyCode ) {
+	let price = getUnformattedDomainPrice( slug, productsList );
+
+	if ( price ) {
 		price = formatCurrency( price, currencyCode );
 	}
 
 	return price;
 }
 
-function getDomainSalePrice( slug, productsList, currencyCode ) {
+function getUnformattedDomainSalePrice( slug, productsList ) {
 	const saleCost = get( productsList, [ slug, 'sale_cost' ], null );
 	const couponValidForNewDomainPurchase = get(
 		productsList,
@@ -256,7 +266,17 @@ function getDomainSalePrice( slug, productsList, currencyCode ) {
 		return null;
 	}
 
-	return formatCurrency( saleCost, currencyCode );
+	return saleCost;
+}
+
+function getDomainSalePrice( slug, productsList, currencyCode ) {
+	let saleCost = getUnformattedDomainSalePrice( slug, productsList );
+
+	if ( saleCost ) {
+		saleCost = formatCurrency( saleCost, currencyCode );
+	}
+
+	return saleCost;
 }
 
 function getDomainTransferSalePrice( slug, productsList, currencyCode ) {
@@ -342,6 +362,8 @@ export {
 	getSelectedDomain,
 	getTld,
 	getTopLevelOfTld,
+	getUnformattedDomainPrice,
+	getUnformattedDomainSalePrice,
 	hasMappedDomain,
 	isHstsRequired,
 	isMappedDomain,

client/lib/domains/constants.js
@@ -1,6 +1,7 @@
 /** @format */
 
 export const type = {
+	ATOMIC_STAGING: 'ATOMIC_STAGING',
 	MAPPED: 'MAPPED',
 	REGISTERED: 'REGISTERED',
 	SITE_REDIRECT: 'SITE_REDIRECT',

client/lib/domains/utils.js
@@ -27,6 +27,10 @@ export function getDomainType( domainFromApi ) {
 		return domainTypes.REGISTERED;
 	}
 
+	if ( get( domainFromApi, 'domain', '' ).endsWith( '.wpcomstaging.com' ) ) {
+		return domainTypes.ATOMIC_STAGING;
+	}
+
 	return domainTypes.MAPPED;
 }
 

client/my-sites/current-site/notice.jsx
@@ -9,6 +9,7 @@ import moment from 'moment';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import config from 'config';
+import { get, reject, transform } from 'lodash';
 
 /**
  * Internal dependencies
@@ -18,7 +19,7 @@ import Notice from 'components/notice';
 import NoticeAction from 'components/notice/notice-action';
 import getActiveDiscount from 'state/selectors/get-active-discount';
 import { domainManagementList } from 'my-sites/domains/paths';
-import { hasDomainCredit } from 'state/sites/plans/selectors';
+import { hasDomainCredit, isCurrentUserCurrentPlanOwner } from 'state/sites/plans/selectors';
 import canCurrentUser from 'state/selectors/can-current-user';
 import isDomainOnlySite from 'state/selectors/is-domain-only-site';
 import isEligibleForFreeToPaidUpsell from 'state/selectors/is-eligible-for-free-to-paid-upsell';
@@ -33,6 +34,14 @@ import CartData from 'components/data/cart';
 import TrackComponentView from 'lib/analytics/track-component-view';
 import DomainToPaidPlanNotice from './domain-to-paid-plan-notice';
 import PendingPaymentNotice from './pending-payment-notice';
+import { getDomainsBySiteId } from 'state/sites/domains/selectors';
+import { getProductsList } from 'state/products-list/selectors';
+import QueryProductsList from 'components/data/query-products-list';
+import { getCurrentUserCurrencyCode } from 'state/current-user/selectors';
+import { getUnformattedDomainPrice, getUnformattedDomainSalePrice } from 'lib/domains';
+import formatCurrency from '@automattic/format-currency/src';
+import { getDomainType } from 'lib/domains/utils';
+import { type as domainTypes } from 'lib/domains/constants';
 
 export class SiteNotice extends React.Component {
 	static propTypes = {
@@ -92,6 +101,81 @@ export class SiteNotice extends React.Component {
 					<TrackComponentView eventName={ eventName } eventProperties={ eventProperties } />
 				</NoticeAction>
 			</Notice>
+		);
+	}
+
+	domainUpsellNudge() {
+		if ( ! this.props.isPlanOwner ) {
+			return null;
+		}
+
+		const nonWPCOMDomains = reject( this.props.domains, domain => {
+			const domainType = getDomainType( domain );
+			return domainType === domainTypes.WPCOM || domainType === domainTypes.ATOMIC_STAGING;
+		} );
+
+		if ( nonWPCOMDomains.length < 1 || nonWPCOMDomains.length > 2 ) {
+			return null;
+		}
+
+		const { site, currencyCode, productsList, translate } = this.props;
+
+		const priceAndSaleInfo = transform(
+			productsList,
+			function( result, value, key ) {
+				if ( value.is_domain_registration && value.available ) {
+					const regularPrice = getUnformattedDomainPrice( key, productsList );
+					const minRegularPrice = get( result, 'minRegularPrice', regularPrice );
+					result.minRegularPrice = Math.min( minRegularPrice, regularPrice );
+
+					const salePrice = getUnformattedDomainSalePrice( key, productsList );
+					if ( salePrice ) {
+						const minSalePrice = get( result, 'minSalePrice', salePrice );
+						result.minSalePrice = Math.min( minSalePrice, salePrice );
+						result.saleTlds.push( value.tld );
+					}
+				}
+			},
+			{ saleTlds: [] }
+		);
+
+		if ( ! priceAndSaleInfo.minSalePrice && ! priceAndSaleInfo.minRegularPrice ) {
+			return null;
+		}
+
+		let noticeText;
+
+		if ( priceAndSaleInfo.minSalePrice ) {
+			if ( get( priceAndSaleInfo, 'saleTlds.length', 0 ) === 1 ) {
+				noticeText = translate( 'Get a %(tld)s domain for just %(salePrice)s for a limited time', {
+					args: {
+						tld: priceAndSaleInfo.saleTlds[ 0 ],
+						salePrice: formatCurrency( priceAndSaleInfo.minSalePrice, currencyCode ),
+					},
+				} );
+			} else {
+				noticeText = translate( 'Domains on sale starting at %(minSalePrice)s', {
+					args: {
+						minSalePrice: formatCurrency( priceAndSaleInfo.minSalePrice, currencyCode ),
+					},
+				} );
+			}
+		} else {
+			noticeText = translate( 'Get a domain starting at %(minDomainPrice)s', {
+				args: {
+					minDomainPrice: formatCurrency( priceAndSaleInfo.minRegularPrice, currencyCode ),
+				},
+			} );
+		}
+
+		return (
+			<SidebarBanner
+				ctaName="domain-upsell-nudge"
+				ctaText={ translate( 'Go' ) }
+				href={ '/domains/add/' + site.slug }
+				icon="info-outline"
+				text={ noticeText }
+			/>
 		);
 	}
 
@@ -188,18 +272,26 @@ export class SiteNotice extends React.Component {
 	render() {
 		const { site } = this.props;
 		if ( ! site ) {
-			return <div className="site__notices" />;
+			return <div className="current-site__notices" />;
 		}
 
+		const discountOrFreeToPaid = this.activeDiscountNotice() || this.freeToPaidPlanNotice();
+		const siteRedirectNotice = this.getSiteRedirectNotice( site );
+		const domainCreditNotice = this.domainCreditNotice();
+		const jetpackPluginsSetupNotice = this.jetpackPluginsSetupNotice();
+
 		return (
-			<div className="site__notices">
+			<div className="current-site__notices">
+				<QueryProductsList />
 				<QueryActivePromotions />
-				{ this.activeDiscountNotice() || this.freeToPaidPlanNotice() || <DomainToPaidPlanNotice /> }
-				{ this.getSiteRedirectNotice( site ) }
+				{ discountOrFreeToPaid || <DomainToPaidPlanNotice /> }
+				{ siteRedirectNotice }
 				<QuerySitePlans siteId={ site.ID } />
 				{ this.pendingPaymentNotice() }
-				{ this.domainCreditNotice() }
-				{ this.jetpackPluginsSetupNotice() }
+				{ domainCreditNotice }
+				{ jetpackPluginsSetupNotice }
+				{ ! ( discountOrFreeToPaid || domainCreditNotice || jetpackPluginsSetupNotice ) &&
+					this.domainUpsellNudge() }
 			</div>
 		);
 	}
@@ -216,6 +308,10 @@ export default connect(
 			canManageOptions: canCurrentUser( state, siteId, 'manage_options' ),
 			pausedJetpackPluginsSetup:
 				isJetpackPluginsStarted( state, siteId ) && ! isJetpackPluginsFinished( state, siteId ),
+			productsList: getProductsList( state ),
+			domains: getDomainsBySiteId( state, siteId ),
+			isPlanOwner: isCurrentUserCurrentPlanOwner( state, siteId ),
+			currencyCode: getCurrentUserCurrencyCode( state ),
 		};
 	},
 	dispatch => {

test/e2e/lib/components/sidebar-component.js
@@ -139,7 +139,10 @@ export default class SidebarComponent extends AsyncBaseContainer {
 
 	async _scrollToAndClickMenuItem( target, { clickButton = false } = {} ) {
 		const selector = SidebarComponent._getSidebarSelector( target, { getButton: clickButton } );
-		await driverHelper.waitTillPresentAndDisplayed( this.driver, By.css( '.site__notices' ) );
+		await driverHelper.waitTillPresentAndDisplayed(
+			this.driver,
+			By.css( '.current-site__notices' )
+		);
 
 		if ( ! ( await driverHelper.isEventuallyPresentAndDisplayed( this.driver, selector, 500 ) ) ) {
 			const settingsSelector = SidebarComponent._getSidebarSelector( 'settings' );

commit 3aa38389651b969cce0da96bb5046d88ef5b16fe

Fix handling of *.wpcomstaging.com domains
Addresses an issue introduced in PR #36208 that resulted in an unhandled domain type.

Fixes issue #36727

client/lib/domains/index.js
@@ -159,7 +159,7 @@ function isHstsRequired( productSlug, productsList ) {
 }
 
 function isMappedDomain( domain ) {
-	return domain.type === domainTypes.MAPPED;
+	return domain.type === domainTypes.MAPPED || domain.type === domainTypes.ATOMIC_STAGING;
 }
 
 function getSelectedDomain( { domains, selectedDomainName, isTransfer } ) {

client/my-sites/domains/domain-management/edit/index.jsx
@@ -54,6 +54,7 @@ class Edit extends React.Component {
 	getDetailsForType = type => {
 		switch ( type ) {
 			case domainTypes.MAPPED:
+			case domainTypes.ATOMIC_STAGING:
 				return MappedDomain;
 
 			case domainTypes.REGISTERED: