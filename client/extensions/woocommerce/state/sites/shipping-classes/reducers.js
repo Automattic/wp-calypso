a7b9d4e4aad8db7b6c94110f81b5fd25d278a409
Jon Surrell <jon.surrell@automattic.com>
2019-10-14 19:38:17 +0200

    Remove createReducer from shipping-classes

--- client/extensions/woocommerce/state/sites/shipping-classes/reducers.js
+++ client/extensions/woocommerce/state/sites/shipping-classes/reducers.js
@@ -1,23 +1,18 @@
-/** @format */
-
 /**
  * Internal dependencies
  */
-import { createReducer } from 'state/utils';
 import {
 	WOOCOMMERCE_SHIPPING_CLASSES_REQUEST,
 	WOOCOMMERCE_SHIPPING_CLASSES_REQUEST_SUCCESS,
 } from 'woocommerce/state/action-types';
 import { LOADING } from 'woocommerce/state/constants';
 
-const reducers = {};
-
-reducers[ WOOCOMMERCE_SHIPPING_CLASSES_REQUEST ] = () => {
-	return LOADING;
-};
-
-reducers[ WOOCOMMERCE_SHIPPING_CLASSES_REQUEST_SUCCESS ] = ( state, { data } ) => {
-	return data;
-};
-
-export default createReducer( {}, reducers );
+export default function( state = {}, action ) {
+	switch ( action.type ) {
+		case WOOCOMMERCE_SHIPPING_CLASSES_REQUEST:
+			return LOADING;
+		case WOOCOMMERCE_SHIPPING_CLASSES_REQUEST_SUCCESS:
+			return action.data;
+	}
+	return state;
+}
