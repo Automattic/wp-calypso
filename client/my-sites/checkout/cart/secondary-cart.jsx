4cdc47a3af80105c009b5a4a2720aa64e03cd56b
Jarda Snajdr <jsnajdr@gmail.com>
2019-10-15 10:18:17 +0200

    Checkout: rewrite SecondaryCart mobile toggling from Flux to Redux

--- client/my-sites/checkout/cart/secondary-cart.jsx
+++ client/my-sites/checkout/cart/secondary-cart.jsx
@@ -3,26 +3,24 @@
  */
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import ReactDom from 'react-dom';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import Dispatcher from 'dispatcher';
 import classNames from 'classnames';
 
 /**
  * Internal dependencies
  */
-import CartBody from 'my-sites/checkout/cart/cart-body';
+import CartBody from './cart-body';
 import CartMessages from './cart-messages';
-import CartSummaryBar from 'my-sites/checkout/cart/cart-summary-bar';
+import CartSummaryBar from './cart-summary-bar';
 import CartPlanAdTheme from './cart-plan-ad-theme';
 import CartPlanDiscountAd from './cart-plan-discount-ad';
-import CartBodyLoadingPlaceholder from 'my-sites/checkout/cart/cart-body/loading-placeholder';
-import { CART_ON_MOBILE_SHOW } from 'lib/upgrades/action-types';
+import CartBodyLoadingPlaceholder from './cart-body/loading-placeholder';
 import scrollIntoViewport from 'lib/scroll-into-viewport';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import { isJetpackSite } from 'state/sites/selectors';
 import isAtomicSite from 'state/selectors/is-site-automated-transfer';
+import { isShowingCartOnMobile } from 'state/ui/checkout/selectors';
 import JetpackLogo from 'components/jetpack-logo';
 
 /**
@@ -36,43 +34,22 @@ class SecondaryCart extends Component {
 		selectedSite: PropTypes.oneOfType( [ PropTypes.bool, PropTypes.object ] ),
 	};
 
-	state = {
-		cartVisible: false,
-	};
+	cartBodyRef = React.createRef();
 
-	UNSAFE_componentWillMount() {
-		this.dispatchToken = Dispatcher.register(
-			function( payload ) {
-				if ( payload.action.type === CART_ON_MOBILE_SHOW ) {
-					this.setState( { cartVisible: payload.action.show } );
-				}
-			}.bind( this )
-		);
-	}
-
-	componentWillUnmount() {
-		Dispatcher.unregister( this.dispatchToken );
-	}
-
-	componentDidUpdate( prevProps, prevState ) {
-		if ( ! prevState.cartVisible && this.state.cartVisible ) {
-			const node = ReactDom.findDOMNode( this.cartBodyRef );
-			scrollIntoViewport( node, {
+	componentDidUpdate( prevProps ) {
+		if ( ! prevProps.isShowingOnMobile && this.props.isShowingOnMobile ) {
+			scrollIntoViewport( this.cartBodyRef.current, {
 				behavior: 'smooth',
 				scrollMode: 'if-needed',
 			} );
 		}
 	}
 
-	setCartBodyRef = cartBody => {
-		this.cartBodyRef = cartBody;
-	};
-
 	render() {
-		const { cart, selectedSite, isJetpackNotAtomic } = this.props;
-		const cartClasses = classNames( {
-			'secondary-cart': true,
-			'secondary-cart__hidden': ! this.state.cartVisible,
+		const { cart, selectedSite, isJetpackNotAtomic, isShowingOnMobile } = this.props;
+
+		const cartClasses = classNames( 'secondary-cart', {
+			'secondary-cart__hidden': ! isShowingOnMobile,
 		} );
 
 		if ( ! cart.hasLoadedFromServer ) {
@@ -91,7 +68,7 @@ class SecondaryCart extends Component {
 				<CartSummaryBar additionalClasses="cart-header" />
 				<CartPlanAdTheme selectedSite={ selectedSite } cart={ cart } />
 				<CartBody
-					ref={ this.setCartBodyRef }
+					ref={ this.cartBodyRef }
 					cart={ cart }
 					selectedSite={ selectedSite }
 					showCoupon={ true }
@@ -110,5 +87,6 @@ export default connect( state => {
 	return {
 		isJetpackNotAtomic:
 			isJetpackSite( state, selectedSiteId ) && ! isAtomicSite( state, selectedSiteId ),
+		isShowingOnMobile: isShowingCartOnMobile( state ),
 	};
 } )( localize( SecondaryCart ) );
