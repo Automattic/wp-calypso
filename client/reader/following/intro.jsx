beb1c762726a673fc003207c51c16e60793fa200
Aurorum <torres126@clubpenguinmountains.com>
2019-10-15 23:20:53 +0100

    Stick with background-image for the header

--- client/reader/following/intro.jsx
+++ client/reader/following/intro.jsx
@@ -16,11 +16,13 @@ import { savePreference } from 'state/preferences/actions';
 import { getPreference } from 'state/preferences/selectors';
 import { recordTrack } from 'reader/stats';
 import { isUserNewerThan, WEEK_IN_MILLISECONDS } from 'state/ui/guided-tours/contexts';
+import cssSafeUrl from 'lib/css-safe-url';
 
 /**
  * Image dependencies
  */
 import readerImage from 'assets/images/reader/reader-intro-character.svg';
+import readerBackground from 'assets/images/reader/reader-intro-background.svg';
 
 class FollowingIntro extends React.Component {
 	componentDidMount() {
@@ -49,7 +51,10 @@ class FollowingIntro extends React.Component {
 		const linkElement = <a onClick={ this.props.handleManageLinkClick } href="/following/manage" />;
 
 		return (
-			<header className="following__intro">
+			<header
+				className="following__intro"
+				style={ { backgroundImage: 'url(' + cssSafeUrl( readerBackground ) + ')' } }
+			>
 				<QueryPreferences />
 				<div className="following__intro-header">
 					<div className="following__intro-copy">
