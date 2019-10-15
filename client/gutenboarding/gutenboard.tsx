b0d8f204eccaf59ead7a81b2dcceab568cc5ad3a
Jon Surrell <jon.surrell@automattic.com>
2019-10-15 08:47:40 +0200

    Add Gutenberg playground (#36494)
    Exposes a Gutenberg playground at `/gutenboarding`.
    
    Only available in `development` and `wpcalypso` environments.

---
+++ client/gutenboarding/gutenboard.tsx
@@ -0,0 +1,52 @@
+/**
+ * WordPress dependencies
+ */
+import '@wordpress/editor'; // This shouldn't be necessary
+
+import React, { useState } from 'react';
+import {
+	BlockEditorKeyboardShortcuts,
+	BlockEditorProvider,
+	BlockList,
+	WritingFlow,
+	ObserveTyping,
+} from '@wordpress/block-editor';
+import { Popover, SlotFillProvider, DropZoneProvider } from '@wordpress/components';
+import { registerCoreBlocks } from '@wordpress/block-library';
+import '@wordpress/format-library';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+import '@wordpress/components/build-style/style.css';
+import '@wordpress/block-editor/build-style/style.css';
+import '@wordpress/block-library/build-style/style.css';
+import '@wordpress/block-library/build-style/editor.css';
+import '@wordpress/block-library/build-style/theme.css';
+import '@wordpress/format-library/build-style/style.css';
+
+export function Gutenboard() {
+	const [ blocks, updateBlocks ] = useState( [] );
+
+	return (
+		<SlotFillProvider>
+			<DropZoneProvider>
+				<BlockEditorProvider value={ blocks } onInput={ updateBlocks } onChange={ updateBlocks }>
+					<div>
+						<BlockEditorKeyboardShortcuts />
+						<WritingFlow>
+							<ObserveTyping>
+								<BlockList />
+							</ObserveTyping>
+						</WritingFlow>
+					</div>
+					<Popover.Slot />
+				</BlockEditorProvider>
+			</DropZoneProvider>
+		</SlotFillProvider>
+	);
+}
+
+registerCoreBlocks();
