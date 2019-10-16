ba665da6a543a899a39bfe0a1c4314aacdffc9b6
Grant Kinney <grant.kinney@automattic.com>
2019-10-16 11:19:25 -0500

    Updates logic to generate back destination

--- client/extensions/woocommerce/store-sidebar/store-ground-control.js
+++ client/extensions/woocommerce/store-sidebar/store-ground-control.js
@@ -8,6 +8,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies
