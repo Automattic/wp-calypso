Automattic/wp-calypso

commit 235c755d4a5987cc354bd27f75eed1ab34c74722

Site Selector: Use text rather than icons for flags in the sidebar for clarity (#35882)

client/blocks/site/style.scss
@@ -92,7 +92,7 @@
 	height: 30px;
 	width: 30px;
 	overflow: hidden;
-	align-self: center;
+	align-self: flex-start;
 	margin-right: 8px;
 	flex: 0 0 auto;
 }
@@ -108,7 +108,7 @@
 	display: block;
 	font-size: 13px;
 	font-weight: 400;
-	line-height: 1.4;
+	line-height: 1.3;
 }
 
 .site__domain {
@@ -117,6 +117,7 @@
 	max-width: 95%;
 	font-size: 11px;
 	line-height: 1.4;
+	margin-top: 2px;
 }
 
 .site__title,
@@ -143,7 +144,7 @@
 	transform: translate3d( 0, 0, 0 );
 	position: absolute;
 	left: 16px;
-	top: 17px;
+	top: 16px;
 
 	.gridicon {
 		margin-top: 5px;
@@ -165,7 +166,24 @@
 }
 
 .site__badge {
-	color: var( --color-neutral );
-	margin-right: 4px;
-	vertical-align: middle;
+	background: var( --color-sidebar-menu-hover-background );
+	color: var( --color-sidebar-menu-hover-text );
+	font-size: 12px;
+	border-radius: 12px;
+	clear: both;
+	display: inline-block;
+	margin-top: 6px;
+	margin-right: 3px;
+	padding: 1px 10px;
+}
+
+.site__content:hover .site__badge {
+	background: var( --color-sidebar-background );
+	color: var( --color-sidebar-text );
+}
+
+.current-site .site__content:hover .site__badge,
+.purchases-site .site__content:hover .site__badge {
+	background: var( --color-sidebar-menu-hover-background );
+	color: var( --color-sidebar-menu-hover-text );
 }

client/layout/style.scss
@@ -160,11 +160,7 @@
 	.site__domain::after {
 		@include long-content-fade( $color: var( --color-sidebar-background-rgb ) );
 	}
-	
-	.site__badge {
-		color: var( --color-sidebar-gridicon-fill );
-	}
-	
+
 	.site-selector {
 		&.is-large .site-selector__sites {
 			border-color: var( --color-sidebar-border );

client/blocks/site/index.jsx
@@ -144,23 +144,6 @@ class Site extends React.Component {
 					<SiteIcon site={ site } size={ this.props.compact ? 24 : 32 } />
 					<div className="site__info">
 						<div className="site__title">
-							{ /* eslint-disable wpcalypso/jsx-gridicon-size */ }
-							{ this.props.site.is_private && (
-								<span className="site__badge">
-									<Gridicon icon="lock" size={ 14 } />
-								</span>
-							) }
-							{ site.options && site.options.is_redirect && (
-								<span className="site__badge">
-									<Gridicon icon="block" size={ 14 } />
-								</span>
-							) }
-							{ site.options && site.options.is_domain_only && (
-								<span className="site__badge">
-									<Gridicon icon="domains" size={ 14 } />
-								</span>
-							) }
-							{ /* eslint-enable wpcalypso/jsx-gridicon-size */ }
 							{ site.title }
 						</div>
 						<div className="site__domain">
@@ -170,6 +153,21 @@ class Site extends React.Component {
 								  } )
 								: site.domain }
 						</div>
+						{ /* eslint-disable wpcalypso/jsx-gridicon-size */ }
+							{ this.props.site.is_private && (
+								<span className="site__badge site__badge-private">{ translate( 'Private' ) }</span>
+							) }
+							{ site.options && site.options.is_redirect && (
+								<span className="site__badge site__badge-redirect">
+									{ translate( 'Redirect' ) }
+								</span>
+							) }
+							{ site.options && site.options.is_domain_only && (
+								<span className="site__badge site__badge-domain-only">
+									{ translate( 'Domain' ) }
+								</span>
+							) }
+						{ /* eslint-enable wpcalypso/jsx-gridicon-size */ }
 					</div>
 					{ this.props.homeLink && this.props.showHomeIcon && (
 						<span className="site__home">

commit b985a27081c8b827fe3bd95c27773f071dd159f2

Fixup site icon positioning in compact version of Site component
Regressed in #35882. This patch adds centering style to `.site.is-compact .site-icon`
and unifies `.site.is-compact .site__home` styles into one rule.

client/blocks/site/docs/example.jsx
@@ -16,7 +16,7 @@ import { getSite } from 'state/sites/selectors';
 
 const SiteExample = ( { site } ) => (
 	<Card style={ { padding: 0 } }>
-		<Site site={ site } />
+		<Site site={ site } homeLink />
 		<Site compact site={ site } homeLink={ true } />
 	</Card>
 );

client/blocks/site/style.scss
@@ -39,6 +39,10 @@
 	}
 
 	&.is-compact {
+		.site-icon {
+			align-self: center;
+		}
+
 		.site__content {
 			padding: 0 16px;
 		}
@@ -54,6 +58,8 @@
 		.site__home {
 			top: 11px;
 			line-height: 18px;
+			width: 24px;
+			height: 24px;
 		}
 
 		.site__home .gridicon {
@@ -149,11 +155,6 @@
 	.gridicon {
 		margin-top: 5px;
 		vertical-align: middle;
-	}
-
-	.site.is-compact & {
-		width: 24px;
-		height: 24px;
 	}
 
 	@include breakpoint( '<660px' ) {