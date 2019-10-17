Automattic/wp-calypso

commit 4c5ecec819c5e7d0072b01ddf304af95ead178f8

Move Transaction Flux Store actions to lib/transaction (#36764)
* CartSynchronizer: rewrite Flux dispatcher fn with switch statement

* Move transaction actions to lib/transaction

* Update imports of transaction actions and action-types

client/lib/cart/store/cart-synchronizer.js
@@ -3,7 +3,7 @@
  */
 import { assign, flowRight, get } from 'lodash';
 import Dispatcher from 'dispatcher';
-import { TRANSACTION_STEP_SET } from 'lib/upgrades/action-types';
+import { TRANSACTION_STEP_SET } from 'lib/transaction/action-types';
 import debugFactory from 'debug';
 
 /**
@@ -70,23 +70,16 @@ function CartSynchronizer( cartKey, wpcom ) {
 
 Emitter( CartSynchronizer.prototype );
 
-CartSynchronizer.prototype.handleDispatch = function( payload ) {
-	const { action } = payload;
+CartSynchronizer.prototype.handleDispatch = ( { action } ) => {
+	switch ( action.type ) {
+		case TRANSACTION_STEP_SET:
+			if ( action.step.first && ! action.step.last ) {
+				this.pause();
+			}
 
-	if ( action.type !== TRANSACTION_STEP_SET ) {
-		return;
-	}
-
-	const { step } = action;
-
-	if ( step.first && step.last ) {
-		return;
-	}
-
-	if ( step.first ) {
-		this.pause();
-	} else if ( step.last ) {
-		this.resume();
+			if ( action.step.last && ! action.step.first ) {
+				this.resume();
+			}
 	}
 };
 

client/lib/cart/store/index.js
@@ -20,9 +20,11 @@ import {
 	CART_TAX_COUNTRY_CODE_SET,
 	CART_TAX_POSTAL_CODE_SET,
 	GOOGLE_APPS_REGISTRATION_DATA_ADD,
+} from 'lib/upgrades/action-types';
+import {
 	TRANSACTION_NEW_CREDIT_CARD_DETAILS_SET,
 	TRANSACTION_PAYMENT_SET,
-} from 'lib/upgrades/action-types';
+} from 'lib/transaction/action-types';
 import emitter from 'lib/mixins/emitter';
 import cartSynchronizer from './cart-synchronizer';
 import PollerPool from 'lib/data-poller';

client/lib/transaction/action-types.js
@@ -1,0 +1,6 @@
+export const TRANSACTION_DOMAIN_DETAILS_SET = 'TRANSACTION_DOMAIN_DETAILS_SET';
+export const TRANSACTION_NEW_CREDIT_CARD_DETAILS_SET = 'TRANSACTION_NEW_CREDIT_CARD_DETAILS_SET';
+export const TRANSACTION_PAYMENT_SET = 'TRANSACTION_PAYMENT_SET';
+export const TRANSACTION_RESET = 'TRANSACTION_RESET';
+export const TRANSACTION_STEP_SET = 'TRANSACTION_STEP_SET';
+export const TRANSACTION_STRIPE_SET = 'TRANSACTION_STRIPE_SET';

client/lib/transaction/actions.js
@@ -1,0 +1,57 @@
+/**
+ * Internal dependencies
+ */
+import {
+	TRANSACTION_DOMAIN_DETAILS_SET,
+	TRANSACTION_NEW_CREDIT_CARD_DETAILS_SET,
+	TRANSACTION_PAYMENT_SET,
+	TRANSACTION_RESET,
+	TRANSACTION_STEP_SET,
+	TRANSACTION_STRIPE_SET,
+} from './action-types';
+import Dispatcher from 'dispatcher';
+
+export function setDomainDetails( domainDetails ) {
+	Dispatcher.handleViewAction( {
+		type: TRANSACTION_DOMAIN_DETAILS_SET,
+		domainDetails,
+	} );
+}
+
+export function setPayment( payment ) {
+	Dispatcher.handleViewAction( {
+		type: TRANSACTION_PAYMENT_SET,
+		payment,
+	} );
+}
+
+export function setStripeObject( stripe, stripeConfiguration ) {
+	Dispatcher.handleViewAction( {
+		type: TRANSACTION_STRIPE_SET,
+		stripe,
+		stripeConfiguration,
+	} );
+}
+
+export function setNewCreditCardDetails( options ) {
+	const { rawDetails, maskedDetails } = options;
+
+	Dispatcher.handleViewAction( {
+		type: TRANSACTION_NEW_CREDIT_CARD_DETAILS_SET,
+		rawDetails,
+		maskedDetails,
+	} );
+}
+
+export function setTransactionStep( step ) {
+	Dispatcher.handleViewAction( {
+		type: TRANSACTION_STEP_SET,
+		step,
+	} );
+}
+
+export function resetTransaction() {
+	Dispatcher.handleViewAction( {
+		type: TRANSACTION_RESET,
+	} );
+}

client/lib/transaction/store.js
@@ -10,15 +10,15 @@ import update from 'immutability-helper';
 /**
  * Internal dependencies
  */
+import { CART_ITEM_REMOVE } from 'lib/upgrades/action-types';
 import {
-	CART_ITEM_REMOVE,
 	TRANSACTION_DOMAIN_DETAILS_SET,
 	TRANSACTION_NEW_CREDIT_CARD_DETAILS_SET,
 	TRANSACTION_PAYMENT_SET,
 	TRANSACTION_RESET,
 	TRANSACTION_STEP_SET,
 	TRANSACTION_STRIPE_SET,
-} from 'lib/upgrades/action-types';
+} from './action-types';
 import { hasDomainRegistration } from 'lib/cart-values/cart-items';
 import CartStore from 'lib/cart/store';
 import Emitter from 'lib/mixins/emitter';

client/lib/upgrades/action-types.js
@@ -44,12 +44,6 @@ export const SITE_REDIRECT_NOTICE_CLOSE = 'SITE_REDIRECT_NOTICE_CLOSE';
 export const SITE_REDIRECT_UPDATE = 'SITE_REDIRECT_UPDATE';
 export const SITE_REDIRECT_UPDATE_COMPLETED = 'SITE_REDIRECT_UPDATE_COMPLETED';
 export const SITE_REDIRECT_UPDATE_FAILED = 'SITE_REDIRECT_UPDATE_FAILED';
-export const TRANSACTION_DOMAIN_DETAILS_SET = 'TRANSACTION_DOMAIN_DETAILS_SET';
-export const TRANSACTION_NEW_CREDIT_CARD_DETAILS_SET = 'TRANSACTION_NEW_CREDIT_CARD_DETAILS_SET';
-export const TRANSACTION_PAYMENT_SET = 'TRANSACTION_PAYMENT_SET';
-export const TRANSACTION_RESET = 'TRANSACTION_RESET';
-export const TRANSACTION_STEP_SET = 'TRANSACTION_STEP_SET';
-export const TRANSACTION_STRIPE_SET = 'TRANSACTION_STRIPE_SET';
 export const WAPI_DOMAIN_INFO_FETCH = 'WAPI_DOMAIN_INFO_FETCH';
 export const WAPI_DOMAIN_INFO_FETCH_COMPLETED = 'WAPI_DOMAIN_INFO_FETCH_COMPLETED';
 export const WAPI_DOMAIN_INFO_FETCH_FAILED = 'WAPI_DOMAIN_INFO_FETCH_FAILED';

client/lib/upgrades/actions/checkout.js
@@ -1,4 +1,3 @@
-/** @format */
 /**
  * External dependencies
  */
@@ -7,53 +6,13 @@ import { defer } from 'lodash';
 /**
  * Internal dependencies
  */
-import {
-	TRANSACTION_DOMAIN_DETAILS_SET,
-	TRANSACTION_NEW_CREDIT_CARD_DETAILS_SET,
-	TRANSACTION_PAYMENT_SET,
-	TRANSACTION_RESET,
-	TRANSACTION_STEP_SET,
-	TRANSACTION_STRIPE_SET,
-} from 'lib/upgrades/action-types';
-import Dispatcher from 'dispatcher';
+import { setTransactionStep } from 'lib/transaction/actions';
 import { submit } from 'lib/store-transactions';
-
-export function setDomainDetails( domainDetails ) {
-	Dispatcher.handleViewAction( {
-		type: TRANSACTION_DOMAIN_DETAILS_SET,
-		domainDetails,
-	} );
-}
-
-export function setPayment( payment ) {
-	Dispatcher.handleViewAction( {
-		type: TRANSACTION_PAYMENT_SET,
-		payment,
-	} );
-}
-
-export function setStripeObject( stripe, stripeConfiguration ) {
-	Dispatcher.handleViewAction( {
-		type: TRANSACTION_STRIPE_SET,
-		stripe,
-		stripeConfiguration,
-	} );
-}
-
-export function setNewCreditCardDetails( options ) {
-	const { rawDetails, maskedDetails } = options;
-
-	Dispatcher.handleViewAction( {
-		type: TRANSACTION_NEW_CREDIT_CARD_DETAILS_SET,
-		rawDetails,
-		maskedDetails,
-	} );
-}
 
 export function submitTransaction( { cart, transaction, successUrl, cancelUrl } ) {
 	submit(
 		{
-			cart: cart,
+			cart,
 			payment: transaction.payment,
 			domainDetails: transaction.domainDetails,
 			successUrl,
@@ -64,18 +23,6 @@ export function submitTransaction( { cart, transaction, successUrl, cancelUrl } 
 		// Execute every step handler in its own event loop tick, so that a complete React
 		// rendering cycle happens on each step and `componentWillReceiveProps` of objects
 		// like the `TransactionStepsMixin` are called with every step.
-		step =>
-			defer( () => {
-				Dispatcher.handleViewAction( {
-					type: TRANSACTION_STEP_SET,
-					step,
-				} );
-			} )
+		step => defer( () => setTransactionStep( step ) )
 	);
-}
-
-export function resetTransaction() {
-	Dispatcher.handleViewAction( {
-		type: TRANSACTION_RESET,
-	} );
 }

client/lib/upgrades/actions/index.js
@@ -23,14 +23,7 @@ export {
 	replaceItem,
 } from './cart';
 
-export {
-	resetTransaction,
-	setDomainDetails,
-	setNewCreditCardDetails,
-	setPayment,
-	setStripeObject,
-	submitTransaction,
-} from './checkout';
+export { submitTransaction } from './checkout';
 
 export {
 	acceptTransfer,

commit d9e515434a8d40ea2a9795a9477daf6adb3546a1

Cart Synchronizer: rewrite handleDispatch method to classic function
Arrow function has the wrong `this` and throws exception when trying to call
`this.pause()` or `this.resume()`. Regressed in #36764.

client/lib/cart/store/cart-synchronizer.js
@@ -70,7 +70,7 @@ function CartSynchronizer( cartKey, wpcom ) {
 
 Emitter( CartSynchronizer.prototype );
 
-CartSynchronizer.prototype.handleDispatch = ( { action } ) => {
+CartSynchronizer.prototype.handleDispatch = function( { action } ) {
 	switch ( action.type ) {
 		case TRANSACTION_STEP_SET:
 			if ( action.step.first && ! action.step.last ) {