c3f3749f5ea52bb93bf5bd6d744a2ec1dcf2cc31
Philip Jackson <p-jackson@live.com>
2019-10-21 19:55:01 +1300

    Use photon to improve screenshot preview performance
    https://developer.wordpress.com/docs/photon/
    
    Use the query parameters provided by photon to scale the image to the
    appropriate size, as determined by the client using the `srcset`
    attribute.
    
    No longer using `<ImagePreloader>` as it doesn't support the `srcset`
    attribute.

--- client/components/signup-site-preview/screenshot.tsx
+++ client/components/signup-site-preview/screenshot.tsx
@@ -2,12 +2,11 @@
  * External dependencies
  */
 import { noop } from 'lodash';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 /**
  * Internal dependencies
  */
-import ImagePreloader from 'components/image-preloader';
 import Spinner from 'components/spinner';
 import classNames from 'classnames';
 
@@ -28,21 +27,42 @@ export default function SignupSitePreviewScreenshot( {
 	onPreviewClick = noop,
 	translate,
 }: Props ) {
+	const [ isLoading, setIsLoading ] = useState( true );
+
+	useEffect( () => {
+		setIsLoading( true );
+	}, [ screenshotUrl ] );
+
 	const className = classNames( {
 		'signup-site-preview__scrolling-screenshot': scrolling,
 	} );
 
 	const isPhone = defaultViewportDevice === 'phone';
 
+	const { hostname, pathname } = new URL( screenshotUrl );
+	const zoom = window.devicePixelRatio;
+	const w = isPhone ? 280 : 904;
+	const src = `https://i0.wp.com/${ hostname }${ pathname }?w=${ w }`;
+	const srcset = `https://i0.wp.com/${ hostname }${ pathname }?w=${ w }&zoom=${ zoom } ${ zoom }x`;
+
+	const onLoad = ( event: React.SyntheticEvent< HTMLImageElement > ) => {
+		setWrapperHeight( event.currentTarget.height );
+		setIsLoading( false );
+	};
+
 	return (
 		<div className={ className }>
-			<ImagePreloader
-				src={ screenshotUrl }
-				placeholder={
-					<Spinner className="signup-site-preview__screenshot-spinner" size={ isPhone ? 20 : 40 } />
-				}
+			{ isLoading && (
+				<Spinner className="signup-site-preview__screenshot-spinner" size={ isPhone ? 20 : 40 } />
+			) }
+			{ /* The onClick is being used for analytics purposes, there's no user interaction implemented with this click handler */ }
+			{ /* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */ }
+			<img
+				style={ isLoading ? { display: 'none' } : undefined }
+				src={ src }
+				srcSet={ srcset }
 				onClick={ () => onPreviewClick( defaultViewportDevice ) }
-				onLoad={ ( e: any ) => setWrapperHeight( e.target.height ) }
+				onLoad={ onLoad }
 				alt={
 					isPhone
 						? translate( 'Preview of site with phone layout', {
