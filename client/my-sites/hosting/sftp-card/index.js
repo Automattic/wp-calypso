11dcffed5058897ca1fb2b066efff353fcee6fa9
Olivia Wolski <olivia.wolski@automattic.com>
2019-10-22 13:02:06 -0400

    Hosting Panel: Update UI for cards and add clipboard button (#36864)

--- client/my-sites/hosting/sftp-card/index.js
+++ client/my-sites/hosting/sftp-card/index.js
@@ -7,7 +7,6 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import { get, map } from 'lodash';
-import classNames from 'classnames';
 
 /**
  * Internal dependencies
@@ -16,10 +15,17 @@ import Card from 'components/card';
 import CardHeading from 'components/card-heading';
 import MaterialIcon from 'components/material-icon';
 import Button from 'components/button';
+import ClipboardButton from 'components/forms/clipboard-button';
+import Spinner from 'components/spinner';
 import { getSelectedSiteId } from 'state/ui/selectors';
 
-// @TODO derive API request details from props when API is merged & remove component state
+// @TODO derive API request details from props when API is merged & remove component state for dummy data
 const SFTPCard = ( { translate, siteId } ) => {
+	// State for clipboard copy button for both username and password data
+	const [ isCopied, setIsCopied ] = useState( false );
+	const usernameIsCopied = isCopied === 'username';
+	const passwordIsCopied = isCopied === 'password';
+
 	// Begin dummy API data/methods
 	const [ dummyApiRequest, setDummyApiRequest ] = useState( {
 		status: 'error',
@@ -31,13 +37,16 @@ const SFTPCard = ( { translate, siteId } ) => {
 	const username = get( dummyApiRequest, 'data.username', null );
 	const password = get( dummyApiRequest, 'data.password', null );
 	const loading = dummyApiRequest.status === 'pending';
-	const noSftpUser = get( dummyApiRequest, 'error.status', 0 ) === 404;
+	const noSftpUser = dummyApiRequest.error.status === 404;
 
 	const createAtomicSFTPUser = () => {
 		setDummyApiRequest( {
 			status: 'success',
 			data: {
 				username: 'test_user_testsite.wordpress.com_1234',
+			},
+			error: {
+				status: 0,
 			},
 		} );
 	};
@@ -49,6 +58,9 @@ const SFTPCard = ( { translate, siteId } ) => {
 				username: 'test_user_testsite.wordpress.com_1234',
 				password: 'a.reset.p.a.s.s.word',
 			},
+			error: {
+				status: 0,
+			},
 		} );
 	};
 	// End of dummy API data/methods
@@ -56,64 +68,97 @@ const SFTPCard = ( { translate, siteId } ) => {
 	const sftpData = {
 		[ translate( 'URL' ) ]: 'sftp1.wordpress.com',
 		[ translate( 'Port' ) ]: 22,
-		[ translate( 'Username' ) ]: username,
 	};
 
 	return (
-		<Card>
-			<div className="sftp-card__icon-col">
+		<Card className="sftp-card">
+			<div className="sftp-card__icon">
 				<MaterialIcon icon="cloud" size={ 32 } />
 			</div>
-			<div>
+			<div className="sftp-card__body">
 				<CardHeading>{ translate( 'SFTP Information' ) }</CardHeading>
-				<p>{ translate( "Access and edit your website's files directly using an FTP client." ) }</p>
-				{ password && (
-					<div className="sftp-card__callout-box">
-						<p>{ translate( 'Your new password for your sftp user is:' ) }</p>
+				{ noSftpUser ? (
+					<>
 						<p>
-							<code>{ password }</code>
-						</p>
-						<strong>
 							{ translate(
-								'Make sure to save this password in a safe place! You will need to reset this password if you lose it.'
+								"Enable SFTP access to generate a username and password so you can access your website's files."
 							) }
-						</strong>
-					</div>
+						</p>
+						<Button onClick={ () => createAtomicSFTPUser( siteId ) } primary>
+							{ translate( 'Enable SFTP' ) }
+						</Button>
+					</>
+				) : (
+					<p>
+						{ translate( "Access and edit your website's files directly using an FTP client." ) }
+					</p>
 				) }
-				{ noSftpUser && (
-					<Button onClick={ () => createAtomicSFTPUser( siteId ) } primary>
-						Create SFTP User
-					</Button>
-				) }
-				<table
-					className={ classNames( 'sftp-card__info-table', {
-						[ 'is-placeholder' ]: loading || noSftpUser,
-					} ) }
-				>
+			</div>
+			{ username && (
+				<table className="sftp-card__info-table">
 					<tbody>
 						{ map( sftpData, ( data, title ) => (
 							<tr key={ title }>
 								<th>{ title }:</th>
 								<td>
-									<span>{ ! loading && ! noSftpUser && data }</span>
+									<span>{ data }</span>
 								</td>
 							</tr>
 						) ) }
 						<tr>
+							<th>{ translate( 'Username' ) }:</th>
+							<td>
+								<p className="sftp-card__hidden-overflow">{ username }</p>
+								<ClipboardButton
+									text={ username }
+									onCopy={ () => setIsCopied( 'username' ) }
+									compact
+								>
+									{ usernameIsCopied
+										? translate( 'Copied!' )
+										: translate( 'Copy', { context: 'verb' } ) }
+								</ClipboardButton>
+							</td>
+						</tr>
+						<tr>
 							<th>{ translate( 'Password' ) }:</th>
 							<td>
-								<Button
-									onClick={ () => resetAtomicSFTPUserPassword( siteId ) }
-									disabled={ loading || noSftpUser }
-									busy={ loading && ! noSftpUser }
-								>
-									{ translate( 'Reset Password' ) }
-								</Button>
+								{ password ? (
+									<>
+										<p className="sftp-card__hidden-overflow">{ password }</p>
+										<ClipboardButton
+											text={ password }
+											onCopy={ () => setIsCopied( 'password' ) }
+											compact
+										>
+											{ passwordIsCopied
+												? translate( 'Copied!' )
+												: translate( 'Copy', { context: 'verb' } ) }
+										</ClipboardButton>
+										<p className="sftp-card__password-warning">
+											{ translate(
+												"Be sure to save your password somewhere safe. You won't be able to view it again without resetting."
+											) }
+										</p>
+									</>
+								) : (
+									<>
+										<p>{ translate( 'You must reset your password to view it.' ) }</p>
+										<Button
+											onClick={ () => resetAtomicSFTPUserPassword( siteId ) }
+											disabled={ loading }
+											compact
+										>
+											{ translate( 'Reset Password' ) }
+										</Button>
+									</>
+								) }
 							</td>
 						</tr>
 					</tbody>
 				</table>
-			</div>
+			) }
+			{ loading && ! noSftpUser && <Spinner /> }
 		</Card>
 	);
 };
