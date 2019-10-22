bf971124690397c15b486c523a4da690067ecd6a
Bernie Reiter <ockham@raz.or.at>
2019-10-22 13:46:09 +0600

    Add header

--- client/gutenboarding/onboarding-block/edit.tsx
+++ client/gutenboarding/onboarding-block/edit.tsx
@@ -2,7 +2,13 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import React from 'react';
 
 export default function OnboardingEdit() {
-	return __( 'I want to create' );
+	return (
+		<>
+			<h1>{ __( "Let's set up your website -- it takes only a moment" ) }</h1>
+			<p>{ __( 'I want to create' ) }</p>
+		</>
+	);
 }
