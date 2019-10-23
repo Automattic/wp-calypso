fbb5c13a481cce9af5f006622035c74f30575a9b
Jon Surrell <jon.surrell@automattic.com>
2019-10-23 23:24:10 +0200

    Type form-checkbox

--- client/components/forms/form-checkbox/index.tsx
+++ client/components/forms/form-checkbox/index.tsx
@@ -1,11 +1,12 @@
-/** @format */
 /**
  * External dependencies
  */
 import React from 'react';
 import classnames from 'classnames';
 
-const FormInputCheckbox = ( { className, ...otherProps } ) => (
+type Props = Omit< React.ComponentPropsWithoutRef< 'input' >, 'type' >;
+
+const FormInputCheckbox: React.FunctionComponent< Props > = ( { className, ...otherProps } ) => (
 	<input { ...otherProps } type="checkbox" className={ classnames( className, 'form-checkbox' ) } />
 );
 
