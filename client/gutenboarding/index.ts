3c5a7b51ad4891fa163ce878333229e4987c05e1
Jon Surrell <jon.surrell@automattic.com>
2019-10-23 12:29:47 +0200

    Add wp-data debug to gutenboarding

--- client/gutenboarding/index.ts
+++ client/gutenboarding/index.ts
@@ -9,6 +9,29 @@ import page from 'page';
 import { hideMasterbar, main, redirectIfNotEnabled } from './controller';
 import { makeLayout, render as clientRender } from 'controller';
 
+// Don't complain about window.wp.data types in our debug function
+declare const window: any;
+
 export default function() {
-	page( '/gutenboarding', redirectIfNotEnabled, hideMasterbar, main, makeLayout, clientRender );
+	page(
+		'/gutenboarding',
+		redirectIfNotEnabled,
+		( context, next ) => {
+			if ( process.env.NODE_ENV !== 'production' ) {
+				if ( typeof window === 'object' ) {
+					if ( ! window.wp ) {
+						window.wp = {};
+					}
+					if ( ! window.wp.data ) {
+						window.wp.data = require( '@wordpress/data' );
+					}
+				}
+			}
+			next();
+		},
+		hideMasterbar,
+		main,
+		makeLayout,
+		clientRender
+	);
 }
