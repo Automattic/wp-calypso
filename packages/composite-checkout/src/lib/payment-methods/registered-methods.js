fcf11c3b1411c310aa65091227a1cd08038f90b2
Filippo Di Trapani <fditrapani@users.noreply.github.com>
2019-10-24 12:41:23 -0400

    Add payment icons (#37035)

--- packages/composite-checkout/src/lib/payment-methods/registered-methods.js
+++ packages/composite-checkout/src/lib/payment-methods/registered-methods.js
@@ -8,6 +8,8 @@ import React from 'react';
  */
 import { registerPaymentMethod } from '../../lib/payment-methods';
 import { ApplePayBillingForm, ApplePaySubmitButton, ApplePayLabel } from './apple-pay';
+import { PaypalLabel, PaypalSubmitButton } from './paypal';
+import { CreditCardLabel, CreditCardSubmitButton } from './credit-card';
 import CreditCardFields from '../../components/credit-card-fields';
 
 export default function loadPaymentMethods() {
@@ -21,17 +23,17 @@ export default function loadPaymentMethods() {
 
 	registerPaymentMethod( {
 		id: 'card',
-		LabelComponent: () => <span>Credit Card</span>,
+		LabelComponent: CreditCardLabel,
 		PaymentMethodComponent: ( { isActive } ) => ( isActive ? <CreditCardFields /> : null ),
 		BillingContactComponent: ApplePayBillingForm, // TODO: replace this
-		SubmitButtonComponent: () => <button>Pay</button>,
+		SubmitButtonComponent: CreditCardSubmitButton,
 	} );
 
 	registerPaymentMethod( {
 		id: 'paypal',
-		LabelComponent: () => <span>PayPal</span>,
+		LabelComponent: PaypalLabel,
 		PaymentMethodComponent: () => null,
 		BillingContactComponent: ApplePayBillingForm, // TODO: replace this
-		SubmitButtonComponent: () => <button>Pay</button>,
+		SubmitButtonComponent: PaypalSubmitButton,
 	} );
 }
