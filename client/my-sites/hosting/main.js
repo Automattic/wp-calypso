787dd6848bc3667b73f917479f28df76161d9188
Olivia Wolski <olivia.wolski@automattic.com>
2019-10-28 12:56:59 -0400

    Hosting Panel: Display sidebar link & panel for non-Atomic Business sites (#37091)

--- client/my-sites/hosting/main.js
+++ client/my-sites/hosting/main.js
@@ -11,10 +11,12 @@ import { localize } from 'i18n-calypso';
  * Internal dependencies
  */
 import Main from 'components/main';
+import Banner from 'components/banner';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
 import DocumentHead from 'components/data/document-head';
-import { getSelectedSite, getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
 import SFTPCard from './sftp-card';
 import PhpMyAdminCard from './phpmyadmin-card';
 
@@ -23,24 +25,34 @@ import PhpMyAdminCard from './phpmyadmin-card';
  */
 import './style.scss';
 
-const Hosting = ( { translate } ) => {
+const Hosting = ( { translate, isDisabled } ) => {
 	return (
 		<Main className="hosting is-wide-layout">
 			<PageViewTracker path="hosting/:site" title="Hosting" />
 			<DocumentHead title={ translate( 'Hosting' ) } />
 			<SidebarNavigation />
+			{ isDisabled && (
+				<Banner
+					title={ translate(
+						'Please active SFTP and PHPMyAdmin access to begin using these features.'
+					) }
+					icon="info"
+					callToAction={ translate( 'Activate' ) }
+					disableHref
+				/>
+			) }
 			<div className="hosting__cards">
-				<SFTPCard />
-				<PhpMyAdminCard />
+				<SFTPCard disabled={ isDisabled } />
+				<PhpMyAdminCard disabled={ isDisabled } />
 			</div>
 		</Main>
 	);
 };
 
 export default connect( state => {
+	const siteId = getSelectedSiteId( state );
+
 	return {
-		site: getSelectedSite( state ),
-		siteId: getSelectedSiteId( state ),
-		siteSlug: getSelectedSiteSlug( state ),
+		isDisabled: ! isSiteAutomatedTransfer( state, siteId ),
 	};
 } )( localize( Hosting ) );
