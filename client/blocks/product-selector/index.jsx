b4b12a5e696139791dad3f022d6cea0e20ffcaba
Marin Atanasov <tyxla@abv.bg>
2019-10-29 15:51:58 +0200

    Blocks: Set ProductSelector to loading state when purchases are fetching

--- client/blocks/product-selector/index.jsx
+++ client/blocks/product-selector/index.jsx
@@ -20,7 +20,7 @@ import { extractProductSlugs, filterByProductSlugs } from './utils';
 import { getAvailableProductsList } from 'state/products-list/selectors';
 import { getCurrentUserCurrencyCode } from 'state/current-user/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
-import { getSitePurchases } from 'state/purchases/selectors';
+import { getSitePurchases, isFetchingSitePurchases } from 'state/purchases/selectors';
 import { getSiteSlug } from 'state/sites/selectors';
 import { withLocalizedMoment } from 'components/localized-moment';
 
@@ -206,9 +206,15 @@ export class ProductSelector extends Component {
 	}
 
 	renderProducts() {
-		const { currencyCode, intervalType, products, storeProducts } = this.props;
+		const {
+			currencyCode,
+			fetchingSitePurchases,
+			intervalType,
+			products,
+			storeProducts,
+		} = this.props;
 
-		if ( isEmpty( storeProducts ) ) {
+		if ( isEmpty( storeProducts ) || fetchingSitePurchases ) {
 			return map( products, product => {
 				return (
 					<ProductCard
@@ -291,6 +297,7 @@ ProductSelector.propTypes = {
 
 	// Connected props
 	currencyCode: PropTypes.string,
+	fetchingSitePurchases: PropTypes.bool,
 	productSlugs: PropTypes.arrayOf( PropTypes.string ),
 	purchases: PropTypes.array,
 	selectedSiteId: PropTypes.number,
@@ -314,6 +321,7 @@ const connectComponent = connect( ( state, { products, siteId } ) => {
 
 	return {
 		currencyCode: getCurrentUserCurrencyCode( state ),
+		fetchingSitePurchases: isFetchingSitePurchases( state ),
 		productSlugs,
 		purchases: getSitePurchases( state, selectedSiteId ),
 		selectedSiteId,
