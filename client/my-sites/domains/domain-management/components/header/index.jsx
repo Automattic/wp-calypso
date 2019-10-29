8c7b73858e717ca81d0e67f0dd933d554eafc0cc
sixhours <calobee@gmail.com>
2019-10-29 16:01:30 -0400

    Consider removing domain name from HeaderCake title. It's repetitive (the domain name is almost always repeated elsewhere in the page contents) and obscures the more important section title on mobile devices and small screens.

--- client/my-sites/domains/domain-management/components/header/index.jsx
+++ client/my-sites/domains/domain-management/components/header/index.jsx
@@ -18,13 +18,12 @@ import DocumentHead from 'components/data/document-head';
 import './style.scss';
 
 export default function DomainManagementHeader( props ) {
-	const { onClick, backHref, selectedDomainName, children } = props;
+	const { onClick, backHref, children } = props;
 
 	/* eslint-disable wpcalypso/jsx-classname-namespace */
 	return (
 		<HeaderCake className="domain-management-header" onClick={ onClick } backHref={ backHref }>
 			<div className="domain-management-header__children">
-				{ selectedDomainName && <span>{ selectedDomainName }: </span> }
 				<span className="domain-management-header__title">{ children }</span>
 			</div>
 			<DocumentHead title={ children } />
