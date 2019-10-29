1b24bfddbec00a9c50a15dba44f009a01a515f09
Brent Sessions <brent.sessions@automattic.com>
2019-10-29 08:34:49 -0600

    make a change to the signup dir

--- test/e2e/specs/wp-signup-spec.js
+++ test/e2e/specs/wp-signup-spec.js
@@ -1156,7 +1156,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		} );
 	} );
 
-	describe( 'Basic sign up for a free site @parallel @email @ie11canary', function() {
+	describe( 'Basic sign up for a free site @signup @email @ie11canary', function() {
 		const blogName = dataHelper.getNewBlogName();
 
 		before( async function() {
