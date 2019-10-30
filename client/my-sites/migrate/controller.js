52ea4406b65bb3f0dfda307f37098adb2b3b9658
Jonathan Sadowski <sadowski@automattic.com>
2019-10-30 11:06:55 -0500

    Prevent access to /migrate when feature-flag is not enabled

--- client/my-sites/migrate/controller.js
+++ client/my-sites/migrate/controller.js
@@ -3,13 +3,19 @@
  * External dependencies
  */
 import React from 'react';
+import page from 'page';
 
 /**
  * Internal Dependencies
  */
 import SectionMigrate from 'my-sites/migrate/section-migrate';
+import { isEnabled } from 'config';
 
 export function migrateSite( context, next ) {
-	context.primary = <SectionMigrate />;
-	next();
+	if ( isEnabled( 'tools/migrate' ) ) {
+		context.primary = <SectionMigrate />;
+		return next();
+	}
+
+	page.redirect( '/' );
 }
