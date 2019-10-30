9de2f8aa98121b30cba88f604d88a6dd44c82670
Jon Surrell <jon.surrell@automattic.com>
2019-10-30 16:01:34 +0100

    Add apiFetch fixme

--- client/gutenboarding/store/resolvers.ts
+++ client/gutenboarding/store/resolvers.ts
@@ -14,6 +14,8 @@ export function* getVertical(
 	search: TailParameters< typeof import('./selectors').getVertical >[ 0 ]
 ) {
 	const url = addQueryArgs( 'https://public-api.wordpress.com/wpcom/v2/verticals', { search } );
+
+	// @FIXME use generic fetch?
 	const verticals = yield apiFetch( { url } );
 
 	// @FIXME Why doesn't `yield receiveVertical( search, verticals );` work?
