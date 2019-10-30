09deae426443a55f1f785fbb0fe003ef37ae9d08
Jon Surrell <jon.surrell@automattic.com>
2019-10-30 15:40:43 +0100

    Add TailParameters docblock

--- client/gutenboarding/store/types.ts
+++ client/gutenboarding/store/types.ts
@@ -35,6 +35,12 @@ export interface ApiVertical {
 	preview_styles_url: string;
 }
 
+/**
+ * Parameters type of a function, excluding the first parameter.
+ *
+ * This is useful for typing some @wordpres/data functions that make a leading
+ * `state` argument implicit.
+ */
 export type TailParameters< F extends ( head: any, ...tail: any[] ) => any > = F extends (
 	head: any,
 	...tail: infer PS
