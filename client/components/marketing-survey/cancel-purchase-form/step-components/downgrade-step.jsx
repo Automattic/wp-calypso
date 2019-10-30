b3f14ec4e920ffd2312bdefbd488f7c602e42b43
Veselin Nikolov <veselin@automattic.com>
2019-10-30 13:00:36 +0200

    Update copy while maintaining the old copy for non-EN locales

--- client/components/marketing-survey/cancel-purchase-form/step-components/downgrade-step.jsx
+++ client/components/marketing-survey/cancel-purchase-form/step-components/downgrade-step.jsx
@@ -17,6 +17,7 @@ import { getSelectedSite } from 'state/ui/selectors';
 import { recordTracksEvent } from 'state/analytics/actions';
 import FormSectionHeading from 'components/forms/form-section-heading';
 import FormFieldset from 'components/forms/form-fieldset';
+import userUtils from 'lib/user/utils';
 
 export class DowngradeStep extends Component {
 	static propTypes = {
@@ -38,25 +39,46 @@ export class DowngradeStep extends Component {
 		const { translate, refundAmount, planCost, currencySymbol } = this.props;
 		const canRefund = !! parseFloat( refundAmount );
 		const amount = currencySymbol + ( canRefund ? refundAmount : planCost );
-
+		const isEnglishLocale = 'en' === userUtils.getLocaleSlug();
+		let refundDetails, refundTitle, refundReason;
+		if ( isEnglishLocale ) {
+			refundTitle = translate( 'Would you rather switch to a more affordable plan?' );
+			refundReason = (
+				<p>
+					{ translate(
+						'WordPress.com Personal still gives you access to email and chat support, ' +
+							'removal of ads, and more — and for 50% of the cost of your current plan.'
+					) }
+				</p>
+			);
+			refundDetails = canRefund
+				? translate(
+						'You can downgrade and get a partial refund of %(amount)s or ' +
+							'continue to the next step and cancel the plan.',
+						{ args: { amount } }
+				  )
+				: translate( 'You can downgrade and the new plan will renew at only %(amount)s.', {
+						args: { amount },
+				  } );
+		} else {
+			refundTitle = translate( 'Would you rather switch to a lower-tier plan?' );
+			refundDetails = canRefund
+				? translate(
+						'You can downgrade to Personal and get a partial refund of %(amount)s or ' +
+							'continue to the next step and cancel the plan.',
+						{ args: { amount } }
+				  )
+				: translate(
+						'You can downgrade to Personal and the new plan will renew at only %(amount)s.',
+						{ args: { amount } }
+				  );
+		}
 		return (
 			<div>
-				<FormSectionHeading>
-					{ translate( 'Would you rather switch to a lower-tier plan?' ) }
-				</FormSectionHeading>
+				<FormSectionHeading>{ refundTitle }</FormSectionHeading>
 				<FormFieldset>
-					<p>
-						{ canRefund
-							? translate(
-									'You can downgrade to Personal and get a partial refund of %(amount)s or ' +
-										'continue to the next step and cancel the plan.',
-									{ args: { amount } }
-							  )
-							: translate(
-									'You can downgrade to Personal and the new plan will renew at only %(amount)s.',
-									{ args: { amount } }
-							  ) }
-					</p>
+					{ refundReason }
+					<p>{ refundDetails }</p>
 				</FormFieldset>
 			</div>
 		);
