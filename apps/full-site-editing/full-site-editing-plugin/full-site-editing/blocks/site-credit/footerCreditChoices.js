6956f2359379c9acc11e5631f4324de96a6ad564
Noah Allen <noahtallen@gmail.com>
2019-10-29 22:52:43 -0700

    Add another TODO comment

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/site-credit/footerCreditChoices.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/site-credit/footerCreditChoices.js
@@ -8,6 +8,7 @@ import { Icon } from '@wordpress/components';
 const CreditTextRenderer = ( { label } ) => <span>{ label }</span>;
 
 // @TODO: Figure out i18n.
+// @TODO: Figure out how to extend this so they can be overridden for WordPress.com
 export const creditOptions = [
 	{ label: 'Proudly powered by WordPress', value: 'default', onRender: CreditTextRenderer },
 	{ label: 'WordPress Icon', value: 'svg', onRender: () => <Icon icon="wordpress" /> },
