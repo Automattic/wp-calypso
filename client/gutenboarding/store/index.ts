8adc8a46314786d3b0304764c6d421f8cf2e13a7
Jon Surrell <jon.surrell@automattic.com>
2019-10-31 09:23:48 +0100

    Gutenboarding: Add verticals API  (#37118)
    * Add @types/wordpress__url
    
    * Add @wordpress/data-controls
    
    * Add verticals search via API in onboarding store

--- client/gutenboarding/store/index.ts
+++ client/gutenboarding/store/index.ts
@@ -1,30 +1,51 @@
 /**
  * External dependencies
  */
-import { registerStore, useSelect, useDispatch } from '@wordpress/data';
+import { controls } from '@wordpress/data-controls';
+import { registerStore, useDispatch, useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
+import { STORE_KEY } from './constants';
 import reducer, { State } from './reducer';
 import * as actions from './actions';
 import * as selectors from './selectors';
-
-export const STORE_KEY = 'automattic/onboard';
+import * as resolvers from './resolvers';
+import { TailParameters } from './types';
 
 registerStore< State >( STORE_KEY, {
-	reducer,
 	actions,
+	controls,
+	reducer,
+	resolvers,
 	selectors,
 	// persist: [],
 } );
 
-export function useOnboardingState(): State {
-	return useSelect( select => select( STORE_KEY ).getState() );
+type Select = {
+	[ selector in keyof typeof selectors ]: (
+		...args: TailParameters< typeof selectors[ selector ] >
+	) => ReturnType< typeof selectors[ selector ] >;
+};
+
+type Dispatch = {
+	[ A in keyof typeof actions ]: ( ...args: Parameters< typeof actions[ A ] > ) => void;
+};
+
+declare module '@wordpress/data' {
+	function dispatch( key: typeof STORE_KEY ): Dispatch;
+	function select( key: typeof STORE_KEY ): Select;
+}
+
+export function useOnboardingSelect() {
+	return useSelect( select => select( STORE_KEY ) );
+}
+
+export function useOnboardingState() {
+	return useOnboardingSelect().getState();
 }
 
 export function useOnboardingDispatch() {
-	return useDispatch( STORE_KEY ) as {
-		[ D in keyof typeof actions ]: ( ...args: Parameters< typeof actions[ D ] > ) => void;
-	};
+	return useDispatch( STORE_KEY );
 }
