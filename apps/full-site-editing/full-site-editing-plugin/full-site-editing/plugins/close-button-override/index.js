582f0fd58006a9b2e5d5568d29f13e37377198b8
Addison-Stavlo <stavz01@gmail.com>
2019-10-30 20:16:50 -0400

    windowed back button for template part

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/close-button-override/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/close-button-override/index.js
@@ -11,10 +11,24 @@ domReady( () => {
 	const editPostHeaderInception = setInterval( () => {
 		const closeButton = document.querySelector( '.edit-post-fullscreen-mode-close__toolbar a' );
 
+		// console.log( closeButton );
+		// console.log( editorPostType + ' ' + closeButtonUrl );
+		// console.log(closeButtonUrl)
+
 		if ( ! closeButton ) {
+			// no close Button in window mode, lets create one
+			const toolbar = document.querySelector( '.edit-post-header-toolbar' );
+			// console.log( toolbar );
+			const closeButtonWrapper = document.createElement( 'a' );
+			closeButtonWrapper.className = 'edit-post-fullscreen-mode-close__toolbar';
+			const placeholderButton = document.createElement( 'a' );
+			closeButtonWrapper.prepend( placeholderButton );
+			toolbar.prepend( closeButtonWrapper );
 			return;
 		}
 		clearInterval( editPostHeaderInception );
+
+		//
 
 		// When closing Template CPT (e.g. header) to navigate back to parent page.
 		if ( 'wp_template_part' === editorPostType && closeButtonUrl ) {
@@ -26,6 +40,8 @@ domReady( () => {
 
 			const parentContainer = document.querySelector( '.edit-post-fullscreen-mode-close__toolbar' );
 			parentContainer.replaceChild( newCloseButton, closeButton );
+		} else if ( 'page' === editorPostType ) {
+			// console.log( 'its a page' );
 		}
-	} );
+	}, 1000 );
 } );
