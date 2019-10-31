bf96d7a37ff93437a86d130e47666d3a19fd8c28
Addison-Stavlo <stavz01@gmail.com>
2019-10-30 20:52:30 -0400

    windowed back button for pages

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/close-button-override/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/close-button-override/index.js
@@ -8,40 +8,48 @@ import domReady from '@wordpress/dom-ready';
 domReady( () => {
 	const { closeButtonLabel, closeButtonUrl, editorPostType } = fullSiteEditing;
 
-	const editPostHeaderInception = setInterval( () => {
-		const closeButton = document.querySelector( '.edit-post-fullscreen-mode-close__toolbar a' );
+	const editPostHeaderInception = setInterval(
+		() => {
+			const closeButton = document.querySelector( '.edit-post-fullscreen-mode-close__toolbar a' );
 
-		// console.log( closeButton );
-		// console.log( editorPostType + ' ' + closeButtonUrl );
-		// console.log(closeButtonUrl)
+			if ( ! closeButton ) {
+				// no close Button in window mode, lets create one
+				const toolbar = document.querySelector( '.edit-post-header-toolbar' );
+				const buttonWrapper = document.createElement( 'a' );
+				buttonWrapper.className = 'edit-post-fullscreen-mode-close__toolbar';
+				const placeholderButton = document.createElement( 'a' );
+				buttonWrapper.prepend( placeholderButton );
+				toolbar.prepend( buttonWrapper );
+				return;
+			}
+			clearInterval( editPostHeaderInception );
 
-		if ( ! closeButton ) {
-			// no close Button in window mode, lets create one
-			const toolbar = document.querySelector( '.edit-post-header-toolbar' );
-			// console.log( toolbar );
-			const closeButtonWrapper = document.createElement( 'a' );
-			closeButtonWrapper.className = 'edit-post-fullscreen-mode-close__toolbar';
-			const placeholderButton = document.createElement( 'a' );
-			closeButtonWrapper.prepend( placeholderButton );
-			toolbar.prepend( closeButtonWrapper );
-			return;
-		}
-		clearInterval( editPostHeaderInception );
+			// When closing Template CPT (e.g. header) to navigate back to parent page.
+			if ( 'wp_template_part' === editorPostType && closeButtonUrl ) {
+				const newCloseButton = document.createElement( 'a' );
+				newCloseButton.href = closeButtonUrl;
+				newCloseButton.innerHTML = closeButtonLabel;
+				newCloseButton.className = 'components-button components-icon-button is-button is-default';
+				newCloseButton.setAttribute( 'aria-label', closeButtonLabel );
 
-		//
+				const parentContainer = document.querySelector(
+					'.edit-post-fullscreen-mode-close__toolbar'
+				);
+				parentContainer.replaceChild( newCloseButton, closeButton );
+			} else if ( 'page' === editorPostType ) {
+				const newCloseButton = document.createElement( 'a' );
+				newCloseButton.href = 'edit.php?post_type=page';
+				newCloseButton.innerHTML = 'Back to Pages';
+				newCloseButton.className = 'components-button components-icon-button is-button is-default';
+				newCloseButton.setAttribute( 'aria-label', closeButtonLabel );
 
-		// When closing Template CPT (e.g. header) to navigate back to parent page.
-		if ( 'wp_template_part' === editorPostType && closeButtonUrl ) {
-			const newCloseButton = document.createElement( 'a' );
-			newCloseButton.href = closeButtonUrl;
-			newCloseButton.innerHTML = closeButtonLabel;
-			newCloseButton.className = 'components-button components-icon-button is-button is-default';
-			newCloseButton.setAttribute( 'aria-label', closeButtonLabel );
-
-			const parentContainer = document.querySelector( '.edit-post-fullscreen-mode-close__toolbar' );
-			parentContainer.replaceChild( newCloseButton, closeButton );
-		} else if ( 'page' === editorPostType ) {
-			// console.log( 'its a page' );
-		}
-	}, 1000 );
+				const parentContainer = document.querySelector(
+					'.edit-post-fullscreen-mode-close__toolbar'
+				);
+				parentContainer.replaceChild( newCloseButton, closeButton );
+			}
+		},
+		// set short delay to ensure back button added after + add block button (can we listen for this?)
+		50
+	);
 } );
