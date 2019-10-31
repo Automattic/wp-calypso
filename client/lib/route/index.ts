285d43ae48a1054ac9ee10147d82b618e1207690
Jon Surrell <jon.surrell@automattic.com>
2019-10-31 16:07:34 +0100

    fixup! Remove redundant implementations

--- client/lib/route/index.ts
+++ client/lib/route/index.ts
@@ -2,5 +2,5 @@ export * from './path';
 export { default as addQueryArgs } from './add-query-args';
 export { default as normalize } from './normalize';
 export { default as trailingslashit } from './trailingslashit';
-export { default as untrailingslashit } from './untrailintslashit';
+export { default as untrailingslashit } from './untrailingslashit';
 export { default as redirect } from './redirect';
