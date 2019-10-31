3a1f1146a99a4ddbab56290367de249d5d76afc9
Payton Swick <payton@foolord.com>
2019-10-30 20:04:22 -0400

    Move CheckoutProvider outside Checkout

--- packages/composite-checkout/src/components/checkout-provider.js
+++ packages/composite-checkout/src/components/checkout-provider.js
@@ -1,35 +1,39 @@
 /**
  * External dependencies
  */
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
 
 /**
  * Internal dependencies
  */
 import CheckoutContext from '../lib/checkout-context';
 import { getPaymentMethods } from '../lib/payment-methods';
+import localizeFactory from '../lib/localize';
+import defaultTheme from '../theme';
 
 export const CheckoutProvider = ( {
-	dispatchPaymentAction,
-	paymentData,
+	locale,
 	total,
 	items,
-	localize,
 	onSuccess,
 	onFailure,
 	successRedirectUrl,
 	failureRedirectUrl,
+	eventHandler,
+	theme,
 	children,
 } ) => {
+	const localize = localizeFactory( locale );
 	const paymentMethods = getPaymentMethods();
+	const [ paymentData, dispatchPaymentAction ] = usePaymentState( eventHandler );
 	const [ paymentMethodId, setPaymentMethodId ] = useState( paymentMethods[ 0 ].id );
 	const paymentMethod = getPaymentMethods().find( ( { id } ) => id === paymentMethodId );
 	if (
-		! paymentData ||
+		! locale ||
 		! total ||
 		! items ||
-		! localize ||
 		! onSuccess ||
 		! onFailure ||
 		! successRedirectUrl ||
@@ -50,15 +54,27 @@ export const CheckoutProvider = ( {
 		successRedirectUrl,
 		failureRedirectUrl,
 	};
-	if ( paymentMethod && paymentMethod.CheckoutWrapper ) {
-		const { CheckoutWrapper } = paymentMethod;
-		return (
+	const { CheckoutWrapper = React.Fragment } = paymentMethod || {};
+	return (
+		<ThemeProvider theme={ theme || defaultTheme }>
 			<CheckoutContext.Provider value={ value }>
 				<CheckoutWrapper>{ children }</CheckoutWrapper>
 			</CheckoutContext.Provider>
-		);
-	}
-	return <CheckoutContext.Provider value={ value }>{ children }</CheckoutContext.Provider>;
+		</ThemeProvider>
+	);
+};
+
+CheckoutProvider.propTypes = {
+	theme: PropTypes.object,
+	eventHandler: PropTypes.func,
+	locale: PropTypes.string.isRequired,
+	total: PropTypes.object.isRequired,
+	items: PropTypes.arrayOf( PropTypes.object ).isRequired,
+	paymentMethodId: PropTypes.string,
+	onSuccess: PropTypes.func.isRequired,
+	onFailure: PropTypes.func.isRequired,
+	successRedirectUrl: PropTypes.string.isRequired,
+	failureRedirectUrl: PropTypes.string.isRequired,
 };
 
 export const useCheckoutLineItems = () => {
@@ -76,15 +92,69 @@ export const useCheckoutRedirects = () => {
 	return { successRedirectUrl, failureRedirectUrl };
 };
 
-CheckoutProvider.propTypes = {
-	dispatchPaymentAction: PropTypes.func.isRequired,
-	paymentData: PropTypes.object.isRequired,
-	total: PropTypes.object.isRequired,
-	items: PropTypes.arrayOf( PropTypes.object ).isRequired,
-	localize: PropTypes.func.isRequired,
-	paymentMethodId: PropTypes.string,
-	onSuccess: PropTypes.func.isRequired,
-	onFailure: PropTypes.func.isRequired,
-	successRedirectUrl: PropTypes.string.isRequired,
-	failureRedirectUrl: PropTypes.string.isRequired,
-};
+function usePaymentState( hostHandler ) {
+	const [ paymentData, setPaymentData ] = useState( {} );
+	const dispatch = useCallback(
+		action => {
+			console.log( 'dispatch', action ); // eslint-disable-line no-console
+			const next = () => paymentStateHandler( action, dispatch, setPaymentData );
+			if ( ! hostHandler ) {
+				next();
+			}
+			if ( hostHandler ) {
+				hostHandler( action, dispatch, next );
+			}
+		},
+		[ hostHandler ]
+	);
+	return [ paymentData, dispatch ];
+}
+
+function paymentStateHandler( { type, payload }, dispatch, setPaymentData ) {
+	switch ( type ) {
+		case 'STEP_CHANGED':
+			// noop
+			return;
+		case 'PAYMENT_DATA_UPDATE':
+			setPaymentData( currentData => {
+				const newState = { ...currentData, ...payload };
+				console.log( 'new state', newState ); // eslint-disable-line no-console
+				return newState;
+			} );
+			return;
+		case 'STRIPE_CONFIGURATION_SET':
+			dispatch( { type: 'PAYMENT_DATA_UPDATE', payload: { stripeConfiguration: payload } } );
+			return;
+		case 'STRIPE_TRANSACTION_END':
+			dispatch( { type: 'PAYMENT_DATA_UPDATE', payload: { stripeTransactionStatus: 'complete' } } );
+			return;
+		case 'STRIPE_TRANSACTION_AUTH':
+			dispatch( { type: 'PAYMENT_DATA_UPDATE', payload: { stripeTransactionStatus: 'auth' } } );
+			return;
+		case 'STRIPE_TRANSACTION_REDIRECT':
+			dispatch( { type: 'PAYMENT_DATA_UPDATE', payload: { stripeTransactionStatus: 'redirect' } } );
+			return;
+		case 'STRIPE_TRANSACTION_ERROR':
+			dispatch( {
+				type: 'PAYMENT_DATA_UPDATE',
+				payload: { stripeTransactionStatus: 'error', stripeTransactionError: payload },
+			} );
+			return;
+		case 'STRIPE_TRANSACTION_RESPONSE':
+			if ( payload && payload.message && payload.message.payment_intent_client_secret ) {
+				dispatch( { type: 'STRIPE_TRANSACTION_AUTH', payload } );
+				return;
+			}
+			if ( payload && payload.redirect_url ) {
+				dispatch( { type: 'STRIPE_TRANSACTION_REDIRECT', payload } );
+				return;
+			}
+			dispatch( { type: 'STRIPE_TRANSACTION_END', payload } );
+			return;
+		case 'STRIPE_CONFIGURATION_FETCH':
+		case 'STRIPE_TRANSACTION_BEGIN':
+			throw new Error( `The action '${ type }' must be handled by the host page` );
+		default:
+			throw new Error( `Unknown action type '${ type }'` );
+	}
+}
