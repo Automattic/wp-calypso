cc9bc526e5106e515e7948c6f29de2e16e84644b
Brent Sessions <brent.sessions@automattic.com>
2019-11-01 07:33:28 -0600

    Extend wait times for calypso.live

--- test/e2e/scripts/wait-for-running-branch.sh
+++ test/e2e/scripts/wait-for-running-branch.sh
@@ -3,8 +3,8 @@
 sha=$CIRCLE_SHA1
 
 COUNT=0
-RESETCOUNT=120 # 5sec retry = Reset the branch after 5 minutes
-MAXCOUNT=240  # 5sec retry = Cancel after 10 minutes
+RESETCOUNT=240 # 5sec retry = Reset the branch after 20 minutes
+MAXCOUNT=480  # 5sec retry = Cancel after 40 minutes
 SITEWAITCOUNT=30 # 5sec retry = Stop waiting after 2.5 minutes
 
 STATUS=$(curl https://hash-$sha.calypso.live/status 2>/dev/null)
