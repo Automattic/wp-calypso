37b6ecf1fbd53fd97fb589c02730b940520ac01c
Glen Davies <glendaviesnz@users.noreply.github.com>
2019-11-04 12:58:27 -0800

    Add atomic hosting eligibility check to limit rollout to new sites (#37241)
    * Add selector to limit Atomic hosting site feature visibility

--- client/my-sites/hosting/controller.js
+++ client/my-sites/hosting/controller.js
@@ -4,33 +4,24 @@
  * External dependencies
  */
 import React from 'react';
-import config from 'config';
 import page from 'page';
-import { get } from 'lodash';
 
 /**
  * Internal Dependencies
  */
 import Hosting from './main';
-import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
-import { getSelectedSiteId, getSelectedSite } from 'state/ui/selectors';
-import { isBusinessPlan } from 'lib/plans';
+import canSiteViewAtomicHosting from 'state/selectors/can-site-view-atomic-hosting';
 
 export function handleHostingPanelRedirect( context, next ) {
 	const { store } = context;
 	const state = store.getState();
-	const isAtomic = isSiteAutomatedTransfer( state, getSelectedSiteId( state ) );
-	const isBusinessSite = isBusinessPlan( get( getSelectedSite( state ), 'plan.product_slug' ) );
 
-	if (
-		config.isEnabled( 'hosting' ) &&
-		( isAtomic || ( config.isEnabled( 'hosting/non-atomic-support' ) && isBusinessSite ) )
-	) {
+	if ( canSiteViewAtomicHosting( state ) ) {
 		next();
 		return;
 	}
 
-	page.redirect( '/' );
+	page.redirect( `/stats/day/${ context.params.siteId || '' }` );
 }
 
 export function layout( context, next ) {
