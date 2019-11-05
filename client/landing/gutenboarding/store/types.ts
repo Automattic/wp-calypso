16307e17844824f506a3cc7e2b48d28a09647907
Jon Surrell <jon.surrell@automattic.com>
2019-11-05 14:34:25 +0100

    fixup! Add and use hasValue helper

--- client/landing/gutenboarding/store/types.ts
+++ client/landing/gutenboarding/store/types.ts
@@ -15,7 +15,7 @@ export enum SiteType {
 
 export const EMPTY_FORM_VALUE = Object.freeze( {} );
 export type FormValue< T > = T | typeof EMPTY_FORM_VALUE;
-export function hasValue< T >( value: FormValue< T > ): value is T {
+export function isFilledFormValue< T >( value: FormValue< T > ): value is T {
 	return value !== EMPTY_FORM_VALUE;
 }
 
