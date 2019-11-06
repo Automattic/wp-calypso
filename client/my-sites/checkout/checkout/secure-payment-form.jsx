90358475edfb854be1ba9d68236db45f6408d963
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2019-11-06 19:05:25 +0530

    Move the site setting to Public to post purchase success handler

--- client/my-sites/checkout/checkout/secure-payment-form.jsx
+++ client/my-sites/checkout/checkout/secure-payment-form.jsx
@@ -182,14 +182,6 @@ export class SecurePaymentForm extends Component {
 			transaction.payment.paymentMethod = 'WPCOM_Billing_Ebanx';
 		}
 
-		try {
-			await this.maybeSetSiteToPublic( { cart } );
-		} catch ( e ) {
-			debug( 'Error setting site to public', e );
-			displayError();
-			return;
-		}
-
 		submit(
 			{
 				cart,
@@ -256,8 +248,16 @@ export class SecurePaymentForm extends Component {
 		}
 	}
 
-	finishIfLastStep( cart, selectedSite, step ) {
+	async finishIfLastStep( cart, selectedSite, step ) {
 		if ( ! step.last || step.error ) {
+			return;
+		}
+
+		try {
+			await this.maybeSetSiteToPublic( { cart } );
+		} catch ( e ) {
+			debug( 'Error setting site to public', e );
+			displayError();
 			return;
 		}
 
