9a48dcd2bec917e5b4701ae515ef840b2c7a0a2d
Dan Hauk <hauk.dan@gmail.com>
2019-11-06 14:20:11 -0500

    Media page heading

--- client/my-sites/media/controller.js
+++ client/my-sites/media/controller.js
@@ -4,14 +4,12 @@
  * External dependencies
  */
 import React from 'react';
-import i18n from 'i18n-calypso';
 import page from 'page';
 
 /**
  * Internal Dependencies
  */
 import MediaComponent from 'my-sites/media/main';
-import { setDocumentHeadTitle as setTitle } from 'state/document-head/actions';
 import { getSiteFragment } from 'lib/route';
 
 export default {
@@ -19,10 +17,6 @@ export default {
 		if ( ! getSiteFragment( context.path ) ) {
 			return page.redirect( '/media' );
 		}
-
-		// Page Title
-		// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
-		context.store.dispatch( setTitle( i18n.translate( 'Media', { textOnly: true } ) ) );
 
 		const mediaId = context.params.mediaId ? parseInt( context.params.mediaId ) : null;
 		// Render
