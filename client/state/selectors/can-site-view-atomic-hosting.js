5e47c9cacb953bfe47d863cc4305fb78d109b1db
Kerry Liu <kerryliu@gmail.com>
2019-11-06 14:47:34 -0800

    Update canSiteViewAtomicHosting to include user capabilities check

--- client/state/selectors/can-site-view-atomic-hosting.js
+++ client/state/selectors/can-site-view-atomic-hosting.js
@@ -11,7 +11,8 @@ import { get } from 'lodash';
 import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
 import { getSelectedSiteId, getSelectedSite } from 'state/ui/selectors';
 import { isEnabled } from 'config';
-import { isBusinessPlan } from 'lib/plans';
+import { isBusinessPlan, isEcommercePlan } from 'lib/plans';
+import canCurrentUser from 'state/selectors/can-current-user';
 
 /**
  * TODO: this selector should be backed by an API response instead
@@ -29,13 +30,21 @@ export default function canSiteViewAtomicHosting( state ) {
 	// This is also enforced on the server, please remove client checks later.
 	// ID of site added 31 Oct 2019, so only sites newer currently eligible
 	if ( siteId > 168768859 ) {
-		const isAtomicSite = !! isSiteAutomatedTransfer( state, siteId );
+		const canManageOptions = canCurrentUser( state, siteId, 'manage_options' );
 
-		if ( isAtomicSite ) {
+		if ( ! canManageOptions ) {
+			return false;
+		}
+
+		const isAtomicSite = !! isSiteAutomatedTransfer( state, siteId );
+		const planSlug = get( getSelectedSite( state ), [ 'plan', 'product_slug' ] );
+		const hasEligablePlan = isBusinessPlan( planSlug ) || isEcommercePlan( planSlug );
+
+		if ( isAtomicSite && hasEligablePlan ) {
 			return true;
 		}
 
-		if ( isEnabled( 'hosting/non-atomic-support' ) ) {
+		if ( ! isAtomicSite && hasEligablePlan && isEnabled( 'hosting/non-atomic-support' ) ) {
 			return isBusinessPlan( get( getSelectedSite( state ), [ 'plan', 'product_slug' ] ) );
 		}
 	}
