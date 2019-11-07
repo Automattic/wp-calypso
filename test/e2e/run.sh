4590fbe8fba73826194abdb9288d664cf9af9bb9
Brent Sessions <brent.sessions@automattic.com>
2019-11-07 14:43:15 -0700

    Don't need this bit

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -31,7 +31,6 @@ function joinStr { local IFS="$1"; shift; echo "$*"; }
 I18N_CONFIG="\"browser\":\"chrome\",\"proxy\":\"system\",\"saveAllScreenshots\":true"
 IE11_CONFIG="\"sauce\":\"true\",\"sauceConfig\":\"win-ie11\""
 SAFARI_CONFIG="\"sauce\":\"true\",\"sauceConfig\":\"osx-safari\""
-CHROME_CONFIG="\"sauce\":\"true\",\"sauceConfig\":\"osx-chrome\""
 
 declare -a MAGELLAN_CONFIGS
 
