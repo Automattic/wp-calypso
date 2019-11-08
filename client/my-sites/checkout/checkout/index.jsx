b3093dc369753ad7f448d7b7596716b7f90a8f1f
niranjan-uma-shankar <niranjan.u@gmail.com>
2019-11-08 20:33:24 +0530

    Fix plan bump test errors (#37349)
    * Check if the previous step is complete
    
    * Update a/bt test slug

--- client/my-sites/checkout/checkout/index.jsx
+++ client/my-sites/checkout/checkout/index.jsx
@@ -446,7 +446,7 @@ export class Checkout extends React.Component {
 		return;
 	}
 
-	maybeRedirectToConciergeNudge( pendingOrReceiptId ) {
+	maybeRedirectToConciergeNudge( pendingOrReceiptId, stepResult ) {
 		const { cart, selectedSiteSlug, previousRoute } = this.props;
 
 		// For a user purchasing a qualifying plan, show either a plan upgrade upsell or concierge upsell.
@@ -455,6 +455,8 @@ export class Checkout extends React.Component {
 		// then skip this section so that we do not show further upsells.
 		if (
 			config.isEnabled( 'upsell/concierge-session' ) &&
+			stepResult &&
+			isEmpty( stepResult.failed_purchases ) &&
 			! hasConciergeSession( cart ) &&
 			! hasJetpackPlan( cart ) &&
 			( hasBloggerPlan( cart ) || hasPersonalPlan( cart ) || hasPremiumPlan( cart ) ) &&
@@ -584,7 +586,10 @@ export class Checkout extends React.Component {
 			return redirectPathForGSuiteUpsell;
 		}
 
-		const redirectPathForConciergeUpsell = this.maybeRedirectToConciergeNudge( pendingOrReceiptId );
+		const redirectPathForConciergeUpsell = this.maybeRedirectToConciergeNudge(
+			pendingOrReceiptId,
+			stepResult
+		);
 		if ( redirectPathForConciergeUpsell ) {
 			return redirectPathForConciergeUpsell;
 		}
