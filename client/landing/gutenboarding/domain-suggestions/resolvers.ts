b01592588b332c806354cf7233a40b4e3b88139f
Bernie Reiter <ockham@raz.or.at>
2019-11-12 12:47:42 +0600

    Remove comment about using generic fetch

--- client/landing/gutenboarding/domain-suggestions/resolvers.ts
+++ client/landing/gutenboarding/domain-suggestions/resolvers.ts
@@ -13,7 +13,6 @@ import { receiveDomainSuggestions } from './actions';
 export function* getDomainSuggestions( queryObject: DomainSuggestionQuery ) {
 	const url = 'https://public-api.wordpress.com/rest/v1.1/domains/suggestions';
 
-	// @FIXME use generic fetch?
 	const suggestions = yield apiFetch( {
 		credentials: 'same-origin',
 		mode: 'cors',
