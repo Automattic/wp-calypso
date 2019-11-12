6abbbe0c42e28e41e0f805a753d84fdb66474b62
Bernie Reiter <ockham@raz.or.at>
2019-11-12 12:14:27 +0600

    CORS

--- client/landing/gutenboarding/domain-suggestions/resolvers.ts
+++ client/landing/gutenboarding/domain-suggestions/resolvers.ts
@@ -14,7 +14,11 @@ export function* getDomainSuggestions( queryObject: DomainSuggestionQuery ) {
 	const url = 'https://public-api.wordpress.com/rest/v1.1/domains/suggestions';
 
 	// @FIXME use generic fetch?
-	const suggestions = yield apiFetch( { url: addQueryArgs( url, queryObject ) } );
+	const suggestions = yield apiFetch( {
+		credentials: 'same-origin',
+		mode: 'cors',
+		url: addQueryArgs( url, queryObject ),
+	} );
 
 	return receiveDomainSuggestions( queryObject, suggestions );
 }
