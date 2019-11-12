59ffa11ec5b442bd226e537f2254dcefd75bee03
Bernie Reiter <ockham@raz.or.at>
2019-11-12 17:58:52 +0600

    Extract TailParameters into stores/type-helpers.ts

---
+++ client/landing/gutenboarding/stores/type-helpers.ts
@@ -0,0 +1,12 @@
+/**
+ * Parameters type of a function, excluding the first parameter.
+ *
+ * This is useful for typing some @wordpres/data functions that make a leading
+ * `state` argument implicit.
+ */
+export type TailParameters< F extends ( head: any, ...tail: any[] ) => any > = F extends (
+	head: any,
+	...tail: infer PS
+) => any
+	? PS
+	: never;
