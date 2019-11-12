abaeb1a6f3743cef573d9419a8e6db919a8bfd8f
Bernie Reiter <ockham@raz.or.at>
2019-11-12 12:09:04 +0600

    Typo in API route :rolleyes:

--- client/landing/gutenboarding/domain-suggestions/resolvers.ts
+++ client/landing/gutenboarding/domain-suggestions/resolvers.ts
@@ -11,7 +11,7 @@ import { DomainSuggestionQuery } from './types';
 import { receiveDomainSuggestions } from './actions';
 
 export function* getDomainSuggestions( queryObject: DomainSuggestionQuery ) {
-	const url = 'https://public-api.wordpress.com/rest/v1.1/domains/sugestions';
+	const url = 'https://public-api.wordpress.com/rest/v1.1/domains/suggestions';
 
 	// @FIXME use generic fetch?
 	const suggestions = yield apiFetch( { url: addQueryArgs( url, queryObject ) } );
