53984f246eb1a4d53ba270e910c64b72ee046b79
Payton Swick <payton@foolord.com>
2019-11-11 22:05:34 -0500

    Add CompositeCheckoutContainer
    This is a host page for the checkout provider and component. Currently
    it mostly uses mock data.

---
+++ client/my-sites/checkout/checkout/composite-checkout-container.js
@@ -0,0 +1,153 @@
+/**
+ * External dependencies
+ */
+import React, { useState, useEffect, useMemo } from 'react';
+import {
+	Checkout,
+	CheckoutProvider,
+	createRegistry,
+	createStripeMethod,
+	createApplePayMethod,
+	createPayPalMethod,
+	WPCheckoutOrderSummary,
+	WPCheckoutOrderReview,
+} from '@automattic/composite-checkout';
+import wp from 'lib/wp';
+
+const initialItems = [
+	{
+		label: 'WordPress.com Personal Plan',
+		id: 'wpcom-personal',
+		type: 'plan',
+		amount: { currency: 'USD', value: 6000, displayValue: '$60' },
+	},
+	{
+		label: 'Domain registration',
+		subLabel: 'example.com',
+		id: 'wpcom-domain',
+		type: 'domain',
+		amount: { currency: 'USD', value: 0, displayValue: '~~$17~~ 0' },
+	},
+];
+
+// These are used only for non-redirect payment methods
+const onSuccess = () => window.alert( 'Payment succeeded!' );
+const onFailure = error => window.alert( 'There was a problem with your payment: ' + error );
+
+// These are used only for redirect payment methods
+const successRedirectUrl = window.location.href;
+const failureRedirectUrl = window.location.href;
+
+const wpcom = wp.undocumented();
+
+async function fetchStripeConfiguration( requestArgs ) {
+	return wpcom.stripeConfiguration( requestArgs );
+}
+
+async function sendStripeTransaction() {
+	// return await wpcom.req.post( '/me/transactions', transaction );
+	return {
+		success: true,
+	};
+}
+
+async function makePayPalExpressRequest() {
+	// return this.wpcom.req.post( '/me/paypal-express-url', data );
+	return window.location.href;
+}
+
+const registry = createRegistry();
+const { registerStore, select, subscribe } = registry;
+
+const stripeMethod = createStripeMethod( {
+	registerStore,
+	fetchStripeConfiguration,
+	sendStripeTransaction,
+} );
+
+const applePayMethod = isApplePayAvailable()
+	? createApplePayMethod( {
+			registerStore,
+			fetchStripeConfiguration,
+	  } )
+	: null;
+
+const paypalMethod = createPayPalMethod( { registerStore, makePayPalExpressRequest } );
+
+function isApplePayAvailable() {
+	// Our Apple Pay implementation uses the Payment Request API, so check that first.
+	if ( ! window.PaymentRequest ) {
+		return false;
+	}
+
+	// Check if Apple Pay is available. This can be very expensive on certain
+	// Safari versions due to a bug (https://trac.webkit.org/changeset/243447/webkit),
+	// and there is no way it can change during a page request, so cache the
+	// result.
+	if ( typeof isApplePayAvailable.canMakePayments === 'undefined' ) {
+		try {
+			isApplePayAvailable.canMakePayments = Boolean(
+				window.ApplePaySession && window.ApplePaySession.canMakePayments()
+			);
+		} catch ( error ) {
+			console.error( error ); // eslint-disable-line no-console
+			return false;
+		}
+	}
+	return isApplePayAvailable.canMakePayments;
+}
+
+const handleEvent = setItems => () => {
+	const cardholderName = select( 'stripe' ).getCardholderName();
+	if ( cardholderName === 'admin' ) {
+		setItems( items =>
+			items.map( item => ( { ...item, amount: { ...item.amount, value: 0, displayValue: '0' } } ) )
+		);
+	}
+};
+
+const getTotal = items => {
+	const lineItemTotal = items.reduce( ( sum, item ) => sum + item.amount.value, 0 );
+	const currency = items.reduce( ( lastCurrency, item ) => item.amount.currency, 'USD' );
+	return {
+		label: 'Total',
+		amount: {
+			currency,
+			value: lineItemTotal,
+			displayValue: formatValueForCurrency( currency, lineItemTotal ),
+		},
+	};
+};
+
+// This is the parent component which would be included on a host page
+export default function CompositeCheckoutContainer() {
+	const [ items, setItems ] = useState( initialItems );
+	useEffect( () => {
+		subscribe( handleEvent( setItems ) );
+	}, [] );
+	const total = useMemo( () => getTotal( items ), [ items ] );
+
+	return (
+		<CheckoutProvider
+			locale={ 'US' }
+			items={ items }
+			total={ total }
+			onSuccess={ onSuccess }
+			onFailure={ onFailure }
+			successRedirectUrl={ successRedirectUrl }
+			failureRedirectUrl={ failureRedirectUrl }
+			registry={ registry }
+			paymentMethods={ [ applePayMethod, stripeMethod, paypalMethod ].filter( Boolean ) }
+		>
+			<Checkout OrderSummary={ WPCheckoutOrderSummary } ReviewContent={ WPCheckoutOrderReview } />
+		</CheckoutProvider>
+	);
+}
+
+function formatValueForCurrency( currency, value ) {
+	if ( currency !== 'USD' ) {
+		throw new Error( `Unsupported currency ${ currency }'` );
+	}
+	const floatValue = value / 100;
+	return '$' + floatValue.toString();
+}
