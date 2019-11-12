40c515366ccac1628996a353a3e6859362d5ce55
Bernie Reiter <ockham@raz.or.at>
2019-11-12 12:48:51 +0600

    Add comment about credentials and mode

--- client/landing/gutenboarding/domain-suggestions/resolvers.ts
+++ client/landing/gutenboarding/domain-suggestions/resolvers.ts
@@ -13,6 +13,8 @@ import { receiveDomainSuggestions } from './actions';
 export function* getDomainSuggestions( queryObject: DomainSuggestionQuery ) {
 	const url = 'https://public-api.wordpress.com/rest/v1.1/domains/suggestions';
 
+	// `credentials` and `mode` args are needed since we're accessing the WP.com REST API
+	// (rather than the WP Core REST API)
 	const suggestions = yield apiFetch( {
 		credentials: 'same-origin',
 		mode: 'cors',
