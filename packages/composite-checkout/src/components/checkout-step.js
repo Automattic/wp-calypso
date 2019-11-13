b059b9aa4a3ec4f5d8b9ef322418c66325032fe2
fditrapani <filippo.di.trapani@automattic.com>
2019-11-13 09:33:47 -0500

    Composite checkout: Fix style issue for step border

--- packages/composite-checkout/src/components/checkout-step.js
+++ packages/composite-checkout/src/components/checkout-step.js
@@ -131,12 +131,12 @@ Stepper.propTypes = {
 };
 
 const StepWrapper = styled.div`
-	padding-bottom: ${ props => ( props.finalStep ? '0' : '32px' ) };
+	padding-bottom: ${props => ( props.finalStep ? '0' : '32px' )};
 	position: relative;
-	border-bottom: 1px solid ${ props => props.theme.colors.borderColorLight }
+	border-bottom: 1px solid ${props => props.theme.colors.borderColorLight};
 	padding: 16px;
 
-	@media ( ${ props => props.theme.breakpoints.tabletUp } ) {
+	@media ( ${props => props.theme.breakpoints.tabletUp} ) {
 		padding: 24px;
 	}
 `;
