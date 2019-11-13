e13853dd451a00e65614148c3da490a862310c50
Marin Atanasov <tyxla@abv.bg>
2019-11-13 12:23:47 +0200

    Split translation to multiple lines

--- client/my-sites/plans/current-plan/current-plan-thank-you/backup-thank-you.js
+++ client/my-sites/plans/current-plan/current-plan-thank-you/backup-thank-you.js
@@ -18,7 +18,9 @@ const BackupProductThankYou = ( { translate } ) => (
 		<p>{ translate( 'We just finished setting up backups for you.' ) }</p>
 		<p>
 			{ translate(
-				'Next, we’ll take a look at your new WordPress.com dashboard. You can manage your backups under “Tools > Activity” in the sidebar. There’s also a checklist to help you get the most out of your Jetpack plan.'
+				'Next, we’ll take a look at your new WordPress.com dashboard. ' +
+					'You can manage your backups under “Tools > Activity” in the sidebar. ' +
+					'There’s also a checklist to help you get the most out of your Jetpack plan.'
 			) }
 		</p>
 		<p>
