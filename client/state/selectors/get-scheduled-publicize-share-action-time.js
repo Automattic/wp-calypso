3ff6dbc2c0f77be4038bf550bda1de66ec87192e
Sérgio Gomes <mail@sgomes.com>
2019-11-14 14:39:22 +0000

    getScheduledPublicizeShareActionTime: no moment.

--- client/state/selectors/get-scheduled-publicize-share-action-time.js
+++ client/state/selectors/get-scheduled-publicize-share-action-time.js
@@ -1,33 +1,17 @@
 /**
- * External dependencies
- */
-import { get } from 'lodash';
-import { getLocaleSlug } from 'i18n-calypso';
-import moment from 'moment';
-
-/**
  * @param {Object} state Global state tree
  * @param {Number} siteId Site ID
  * @param {Number} postId Post ID
  * @return {String|false} time when newly scheduled share action will be published
  */
 export default function getScheduledPublicizeShareActionTime( state, siteId, postId ) {
-	const localeSlug = getLocaleSlug();
-	const date = get(
-		state,
-		[
-			'sharing',
-			'publicize',
-			'sharePostActions',
-			'schedulingSharePostActionStatus',
-			siteId,
-			postId,
-			'shareDate',
-		],
-		false
-	);
+	const date =
+		state?.sharing?.publicize?.sharePostActions?.schedulingSharePostActionStatus?.[ siteId ]?.[
+			postId
+		]?.shareDate ?? false;
+
 	if ( date ) {
-		return moment( new Date( date * 1000 ) ).locale( localeSlug );
+		return new Date( date * 1000 );
 	}
 	return false;
 }
