bb64ac0932c60851c54ea07d40da7ef895bc35fb
Ben Dwyer <ben@scruffian.com>
2019-11-15 11:16:25 +0000

    Add a comment to explain the previous commit

--- client/my-sites/marketing/connections/service.jsx
+++ client/my-sites/marketing/connections/service.jsx
@@ -266,6 +266,9 @@ export class SharingService extends Component {
 	refresh = ( connections = this.props.brokenConnections ) => {
 		this.getConnections( connections ).map( connection => {
 			const keyringConnection = find( this.props.keyringConnections, {
+				// Publicize connections store the keyring ID in keyring_connection_ID
+				// Since keyring connections don't have a keyring_connection_ID property,
+				// we need to check both the keyring_connection_ID and the ID
 				ID: connection.keyring_connection_ID || connection.ID,
 			} );
 
