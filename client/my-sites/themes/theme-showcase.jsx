47b398af47a98dee85346ae219bd7c1ebb56e0c0
Addison-Stavlo <stavz01@gmail.com>
2019-11-18 21:04:20 -0500

    reorganized existing components and classNames to better fit original layout

--- client/my-sites/themes/theme-showcase.jsx
+++ client/my-sites/themes/theme-showcase.jsx
@@ -217,60 +217,66 @@ class ThemeShowcase extends React.Component {
 		const { isShowcaseOpen } = this.state;
 		// FIXME: Logged-in title should only be 'Themes'
 		return (
-			<div className="themes__content">
-				{ this.showUploadButton() && (
-					<Button
-						className="themes__upload-button"
-						compact
-						onClick={ this.onUploadClick }
-						href={ siteSlug ? `/themes/upload/${ siteSlug }` : '/themes/upload' }
-					>
-						<Gridicon icon="cloud-upload" />
-						{ translate( 'Install Theme' ) }
-					</Button>
-				) }
-				<RecommendedThemes
-					upsellUrl={ this.props.upsellUrl }
-					search={ search }
-					tier={ this.props.tier }
-					filter={ filter }
-					vertical={ this.props.vertical }
-					siteId={ this.props.siteId }
-					listLabel={ this.props.listLabel }
-					defaultOption={ this.props.defaultOption }
-					secondaryOption={ this.props.secondaryOption }
-					placeholderCount={ this.props.placeholderCount }
-					getScreenshotUrl={ function( theme ) {
-						if ( ! getScreenshotOption( theme ).getUrl ) {
-							return null;
-						}
-						return getScreenshotOption( theme ).getUrl( theme );
-					} }
-					onScreenshotClick={ function( themeId ) {
-						if ( ! getScreenshotOption( themeId ).action ) {
-							return;
-						}
-						getScreenshotOption( themeId ).action( themeId );
-					} }
-					getActionLabel={ function( theme ) {
-						return getScreenshotOption( theme ).label;
-					} }
-					getOptions={ function( theme ) {
-						return pickBy(
-							addTracking( options ),
-							option => ! ( option.hideForTheme && option.hideForTheme( theme, siteId ) )
-						);
-					} }
-					trackScrollPage={ this.props.trackScrollPage }
-					emptyContent={ this.props.emptyContent }
+			<div>
+				<DocumentHead title={ title } meta={ metas } link={ links } />
+				<PageViewTracker
+					path={ this.props.analyticsPath }
+					title={ this.props.analyticsPageTitle }
 				/>
+				<div className="themes__content">
+					{ this.showUploadButton() && (
+						<Button
+							className="themes__upload-button"
+							compact
+							onClick={ this.onUploadClick }
+							href={ siteSlug ? `/themes/upload/${ siteSlug }` : '/themes/upload' }
+						>
+							<Gridicon icon="cloud-upload" />
+							{ translate( 'Install Theme' ) }
+						</Button>
+					) }
+					<RecommendedThemes
+						upsellUrl={ this.props.upsellUrl }
+						search={ search }
+						tier={ this.props.tier }
+						filter={ filter }
+						vertical={ this.props.vertical }
+						siteId={ this.props.siteId }
+						listLabel={ this.props.listLabel }
+						defaultOption={ this.props.defaultOption }
+						secondaryOption={ this.props.secondaryOption }
+						placeholderCount={ this.props.placeholderCount }
+						getScreenshotUrl={ function( theme ) {
+							if ( ! getScreenshotOption( theme ).getUrl ) {
+								return null;
+							}
+							return getScreenshotOption( theme ).getUrl( theme );
+						} }
+						onScreenshotClick={ function( themeId ) {
+							if ( ! getScreenshotOption( themeId ).action ) {
+								return;
+							}
+							getScreenshotOption( themeId ).action( themeId );
+						} }
+						getActionLabel={ function( theme ) {
+							return getScreenshotOption( theme ).label;
+						} }
+						getOptions={ function( theme ) {
+							return pickBy(
+								addTracking( options ),
+								option => ! ( option.hideForTheme && option.hideForTheme( theme, siteId ) )
+							);
+						} }
+						trackScrollPage={ this.props.trackScrollPage }
+						emptyContent={ this.props.emptyContent }
+					/>
+				</div>
 				<Button onClick={ this.toggleShowcase }>
 					{ isShowcaseOpen ? 'Hide Extra Themes' : 'Show All Themes' }
 				</Button>
 
 				{ this.state.isShowcaseOpen && (
 					<div>
-						<DocumentHead title={ title } meta={ metas } link={ links } />
 						{ ! isLoggedIn && (
 							<SubMasterbarNav
 								options={ headerIcons }
@@ -278,64 +284,64 @@ class ThemeShowcase extends React.Component {
 								uri={ this.constructUrl() }
 							/>
 						) }
-						<PageViewTracker
-							path={ this.props.analyticsPath }
-							title={ this.props.analyticsPageTitle }
-						/>
-						<QueryThemeFilters />
-						{ showBanners &&
-							abtest &&
-							abtest( 'builderReferralThemesBanner' ) === 'builderReferralBanner' && (
-								<UpworkBanner location={ 'theme-banner' } />
-							) }
-						<ThemesSearchCard
-							onSearch={ this.doSearch }
-							search={ filterString + search }
-							tier={ tier }
-							showTierThemesControl={ ! isMultisite }
-							select={ this.onTierSelect }
-						/>
-						{ this.props.upsellBanner }
+						<div className="themes__content">
+							<QueryThemeFilters />
+							{ showBanners &&
+								abtest &&
+								abtest( 'builderReferralThemesBanner' ) === 'builderReferralBanner' && (
+									<UpworkBanner location={ 'theme-banner' } />
+								) }
+							<ThemesSearchCard
+								onSearch={ this.doSearch }
+								search={ filterString + search }
+								tier={ tier }
+								showTierThemesControl={ ! isMultisite }
+								select={ this.onTierSelect }
+							/>
+							{ this.props.upsellBanner }
 
-						<ThemesSelection
-							upsellUrl={ this.props.upsellUrl }
-							search={ search }
-							tier={ this.props.tier }
-							filter={ filter }
-							vertical={ this.props.vertical }
-							siteId={ this.props.siteId }
-							listLabel={ this.props.listLabel }
-							defaultOption={ this.props.defaultOption }
-							secondaryOption={ this.props.secondaryOption }
-							placeholderCount={ this.props.placeholderCount }
-							getScreenshotUrl={ function( theme ) {
-								if ( ! getScreenshotOption( theme ).getUrl ) {
-									return null;
-								}
-								return getScreenshotOption( theme ).getUrl( theme );
-							} }
-							onScreenshotClick={ function( themeId ) {
-								if ( ! getScreenshotOption( themeId ).action ) {
-									return;
-								}
-								getScreenshotOption( themeId ).action( themeId );
-							} }
-							getActionLabel={ function( theme ) {
-								return getScreenshotOption( theme ).label;
-							} }
-							getOptions={ function( theme ) {
-								return pickBy(
-									addTracking( options ),
-									option => ! ( option.hideForTheme && option.hideForTheme( theme, siteId ) )
-								);
-							} }
-							trackScrollPage={ this.props.trackScrollPage }
-							emptyContent={ this.props.emptyContent }
-						/>
+							<ThemesSelection
+								upsellUrl={ this.props.upsellUrl }
+								search={ search }
+								tier={ this.props.tier }
+								filter={ filter }
+								vertical={ this.props.vertical }
+								siteId={ this.props.siteId }
+								listLabel={ this.props.listLabel }
+								defaultOption={ this.props.defaultOption }
+								secondaryOption={ this.props.secondaryOption }
+								placeholderCount={ this.props.placeholderCount }
+								getScreenshotUrl={ function( theme ) {
+									if ( ! getScreenshotOption( theme ).getUrl ) {
+										return null;
+									}
+									return getScreenshotOption( theme ).getUrl( theme );
+								} }
+								onScreenshotClick={ function( themeId ) {
+									if ( ! getScreenshotOption( themeId ).action ) {
+										return;
+									}
+									getScreenshotOption( themeId ).action( themeId );
+								} }
+								getActionLabel={ function( theme ) {
+									return getScreenshotOption( theme ).label;
+								} }
+								getOptions={ function( theme ) {
+									return pickBy(
+										addTracking( options ),
+										option => ! ( option.hideForTheme && option.hideForTheme( theme, siteId ) )
+									);
+								} }
+								trackScrollPage={ this.props.trackScrollPage }
+								emptyContent={ this.props.emptyContent }
+							/>
+						</div>
 					</div>
 				) }
-				<ThemePreview />
-				{ this.props.children }
+				<div className="themes__content">
+					<ThemePreview />
+					{ this.props.children }
+				</div>
 			</div>
 		);
 	}
