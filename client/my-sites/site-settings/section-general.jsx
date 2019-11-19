ade3d10fb69c6fc2e8aacd9ce058a72bd4a7eaf3
Steve Seear <steve.seear@gmail.com>
2019-11-19 21:20:19 +0000

    Add banner to general settings page.

--- client/my-sites/site-settings/section-general.jsx
+++ client/my-sites/site-settings/section-general.jsx
@@ -8,11 +8,13 @@ import { connect } from 'react-redux';
  * Internal dependencies
  */
 import GeneralForm from 'my-sites/site-settings/form-general';
+import JetpackBackupCredsBanner from 'blocks/jetpack-backup-creds-banner';
 import SiteTools from './site-tools';
 import { getSelectedSite } from 'state/ui/selectors';
 
 const SiteSettingsGeneral = ( { site } ) => (
 	<div className="site-settings__main general-settings">
+		<JetpackBackupCredsBanner location={ 'settings' } />
 		<GeneralForm site={ site } />
 		<SiteTools />
 	</div>
