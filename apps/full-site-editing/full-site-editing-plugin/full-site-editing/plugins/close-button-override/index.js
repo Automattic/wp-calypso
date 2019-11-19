660a702234d91dc04fb1b1d7b5c85eba3470dcc4
Addison-Stavlo <stavz01@gmail.com>
2019-11-19 11:51:19 -0500

    restructured reusable sections, added fallback href/label for template_part

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/close-button-override/index.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/plugins/close-button-override/index.js
@@ -17,8 +17,9 @@ import './style.scss';
 
 domReady( () => {
 	const { closeButtonLabel, closeButtonUrl, editorPostType } = fullSiteEditing;
+	let newLabel;
 
-	// Only alter for the page and template part editors.
+	// Only alter for the page, post, and template part editors.
 	if (
 		'wp_template_part' !== editorPostType &&
 		'page' !== editorPostType &&
@@ -45,12 +46,13 @@ domReady( () => {
 		// Create custom close button and append to components toolbar.
 		const newCloseButton = document.createElement( 'a' );
 		// When closing Template CPT (e.g. header) to navigate back to parent page.
-		if ( 'wp_template_part' === editorPostType && closeButtonUrl ) {
-			newCloseButton.href = closeButtonUrl;
-			newCloseButton.setAttribute( 'aria-label', closeButtonLabel );
+		if ( 'wp_template_part' === editorPostType ) {
+			newCloseButton.href = closeButtonUrl || 'edit.php?post_type=page';
+			const backupLabel = __( 'Go Back' );
+			newCloseButton.setAttribute( 'aria-label', closeButtonLabel || backupLabel );
 			newCloseButton.className = 'components-button components-icon-button is-button is-default';
 			const wideContent = document.createElement( 'div' );
-			wideContent.innerHTML = closeButtonLabel;
+			wideContent.innerHTML = closeButtonLabel || backupLabel;
 			wideContent.className = 'close-button-override-wide';
 			newCloseButton.prepend( wideContent );
 			const thinContent = document.createElement( 'div' );
@@ -60,19 +62,14 @@ domReady( () => {
 			newCloseButton.prepend( thinContent );
 		} else if ( 'page' === editorPostType ) {
 			newCloseButton.href = 'edit.php?post_type=page';
-			const newLabel = __( 'Pages' );
-			newCloseButton.setAttribute( 'aria-label', newLabel );
-
-			ReactDOM.render(
-				<Button className="components-button components-icon-button">
-					<Dashicon icon="arrow-left-alt2" />
-					<div className="close-button-override__label">{ newLabel }</div>
-				</Button>,
-				newCloseButton
-			);
+			newLabel = __( 'Pages' );
 		} else if ( 'post' === editorPostType ) {
 			newCloseButton.href = 'edit.php?post_type=post';
-			const newLabel = __( 'Posts' );
+			newLabel = __( 'Posts' );
+			newCloseButton.setAttribute( 'aria-label', newLabel );
+		}
+
+		if ( 'page' === editorPostType || 'post' === editorPostType ) {
 			newCloseButton.setAttribute( 'aria-label', newLabel );
 
 			ReactDOM.render(
