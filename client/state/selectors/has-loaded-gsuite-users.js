4be3c047823591a1e6d22f906cdfe0dacf13b64f
Stéphane Thomas <email@stephanethomas.com>
2019-11-19 18:06:47 +0100

    Fix linting errors in G Suite and Domains selectors

--- client/state/selectors/has-loaded-gsuite-users.js
+++ client/state/selectors/has-loaded-gsuite-users.js
@@ -6,9 +6,9 @@ import { get } from 'lodash';
 /**
  * Determines whether the list of G Suite users for the specified site has loaded.
  *
- * @param {Object} state - global state tree
- * @param {Number} siteId - identifier of the site
- * @return {Boolean} true if the list of G Suite users has loaded, false otherwise
+ * @param {object} state - global state tree
+ * @param {number} siteId - identifier of the site
+ * @returns {boolean} true if the list of G Suite users has loaded, false otherwise
  */
 export default function hasLoadedGSuiteUsers( state, siteId ) {
 	return get( state, [ 'gsuiteUsers', siteId, 'users' ], null ) !== null;
