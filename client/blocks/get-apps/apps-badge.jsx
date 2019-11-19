334d2c07c9549a10468fc72bb86a84844feed881
sixhours <calobee@gmail.com>
2019-11-19 16:27:57 -0500

    Load AppsBadge styles with webpack.

--- client/blocks/get-apps/apps-badge.jsx
+++ client/blocks/get-apps/apps-badge.jsx
@@ -16,6 +16,11 @@ import { getLocaleSlug } from 'lib/i18n-utils';
 import { recordTracksEvent } from 'state/analytics/actions';
 import TranslatableString from 'components/translatable/proptype';
 
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
 // the locale slugs for each stores' image paths follow different rules
 // therefore we have to perform some trickery in getLocaleSlug()
 const APP_STORE_BADGE_URLS = {
