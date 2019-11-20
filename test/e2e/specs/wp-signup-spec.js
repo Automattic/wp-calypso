65ef6a94def02581478455f39f71727a47b01958
Adam Zieliński <adam@adamziel.com>
2019-11-20 12:46:42 +0100

    Remove unrelated test skip change

--- test/e2e/specs/wp-signup-spec.js
+++ test/e2e/specs/wp-signup-spec.js
@@ -1761,9 +1761,7 @@ describe( `[${ host }] Sign Up  (${ screenSize }, ${ locale })`, function() {
 		} );
 	} );
 
-	// Disable test while Passwordless functionality is completely switched off
-	// https://github.com/Automattic/wp-calypso/pull/37054
-	describe.skip( 'Passwordless signup @parallel', function() {
+	describe( 'Passwordless signup @parallel', function() {
 		const blogName = dataHelper.getNewBlogName();
 		const emailAddress = dataHelper.getEmailAddress( blogName, signupInboxId );
 		const expectedBlogAddresses = dataHelper.getExpectedFreeAddresses( blogName );
