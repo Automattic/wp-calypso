7f2cb2a3e165ac67c3029b29bb24fd22a6d61a7c
Sérgio Gomes <mail@sgomes.com>
2019-11-21 17:49:43 +0000

    Use button again, with CSS fixes.

--- client/my-sites/domains/domain-management/list/item.jsx
+++ client/my-sites/domains/domain-management/list/item.jsx
@@ -49,8 +49,7 @@ class ListItem extends React.PureComponent {
 
 	content() {
 		return (
-			// eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
-			<div className="domain-management-list-item__link" onClick={ this.handleClick }>
+			<button className="domain-management-list-item__link" onClick={ this.handleClick }>
 				{ this.icon() }
 				<div className="domain-management-list-item__title">{ this.props.domain.name }</div>
 				<span className="domain-management-list-item__meta">
@@ -62,7 +61,7 @@ class ListItem extends React.PureComponent {
 					<DomainTransferFlag domain={ this.props.domain } />
 				</span>
 				{ this.busyMessage() }
-			</div>
+			</button>
 		);
 	}
 
