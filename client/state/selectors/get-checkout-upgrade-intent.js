d929497d5c4246aeba3a9204c775beafcee3bee4
Jeff Bowen <jblz@users.noreply.github.com>
2019-11-21 14:27:57 -0500

    Checkout: Enable returning customers to pre-upgrade-intended activity
    Revert "Put selector behind a config flag"
    
    This reverts commit d2aa822949982d85b17d2adaf209908314219c9e.

--- client/state/selectors/get-checkout-upgrade-intent.js
+++ client/state/selectors/get-checkout-upgrade-intent.js
@@ -1,15 +1,9 @@
 /**
- * Internal dependencies
- */
-import { isEnabled } from 'config';
-
-/**
  * Retrieve the "intent" that the client implied prior to upgrading so we can send them to the appropriate route after checkout
  *
  * @param {object} state  Global state tree
  * @returns {string} The intent signaled by the customer for upgrade purposes
  */
 export default function getCheckoutUpgradeIntent( state ) {
-	// This is gated by a config flag while we wait for translations
-	return ( isEnabled( 'checkout-upgrade-intent' ) && state?.ui?.checkout?.upgradeIntent ) || '';
+	return state?.ui?.checkout?.upgradeIntent || '';
 }
