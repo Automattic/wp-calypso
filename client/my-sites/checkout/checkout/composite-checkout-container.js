175d6136b61fc72df9bb9176943bec7fa578f44b
Payton Swick <payton@foolord.com>
2019-11-22 14:16:29 -0500

    Add remaining payment methods to CompositeCheckoutContainer

--- client/my-sites/checkout/checkout/composite-checkout-container.js
+++ client/my-sites/checkout/checkout/composite-checkout-container.js
@@ -2,7 +2,12 @@
  * External dependencies
  */
 import React from 'react';
-import { createRegistry, createPayPalMethod } from '@automattic/composite-checkout';
+import {
+	createRegistry,
+	createPayPalMethod,
+	createStripeMethod,
+	createApplePayMethod,
+} from '@automattic/composite-checkout';
 import {
 	WPCOMCheckout,
 	makeShoppingCartHook,
@@ -61,16 +66,65 @@ const useShoppingCart = makeShoppingCartHook(
 	initialCart
 );
 
+async function fetchStripeConfiguration( requestArgs ) {
+	return wpcom.stripeConfiguration( requestArgs );
+}
+
+async function sendStripeTransaction() {
+	// return await wpcom.req.post( '/me/transactions', transaction );
+	return {
+		success: true,
+	};
+}
+
+const stripeMethod = createStripeMethod( {
+	registerStore,
+	fetchStripeConfiguration,
+	sendStripeTransaction,
+} );
+
+const paypalMethod = createPayPalMethod( {
+	registerStore: registerStore,
+	makePayPalExpressRequest: mockPayPalExpressRequest,
+} );
+
+const applePayMethod = isApplePayAvailable()
+	? createApplePayMethod( {
+			registerStore,
+			fetchStripeConfiguration,
+	  } )
+	: null;
+
+export function isApplePayAvailable() {
+	// Our Apple Pay implementation uses the Payment Request API, so check that first.
+	if ( ! window.PaymentRequest ) {
+		return false;
+	}
+
+	// Check if Apple Pay is available. This can be very expensive on certain
+	// Safari versions due to a bug (https://trac.webkit.org/changeset/243447/webkit),
+	// and there is no way it can change during a page request, so cache the
+	// result.
+	if ( typeof isApplePayAvailable.canMakePayments === 'undefined' ) {
+		try {
+			isApplePayAvailable.canMakePayments = Boolean(
+				window.ApplePaySession && window.ApplePaySession.canMakePayments()
+			);
+		} catch ( error ) {
+			console.error( error ); // eslint-disable-line no-console
+			return false;
+		}
+	}
+	return isApplePayAvailable.canMakePayments;
+}
+
+const availablePaymentMethods = [ applePayMethod, stripeMethod, paypalMethod ].filter( Boolean );
+
 export default function CompositeCheckoutContainer() {
 	return (
 		<WPCOMCheckout
 			useShoppingCart={ useShoppingCart }
-			availablePaymentMethods={ [
-				createPayPalMethod( {
-					registerStore: registerStore,
-					makePayPalExpressRequest: mockPayPalExpressRequest,
-				} ),
-			] }
+			availablePaymentMethods={ availablePaymentMethods }
 			registry={ registry }
 		/>
 	);
