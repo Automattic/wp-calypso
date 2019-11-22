732c7626e40ea404c71bb9a4ebec3687cdce2562
Stéphane Thomas <email@stephanethomas.com>
2019-11-22 16:00:00 +0100

    G Suite: Refine selectors that handle G Suite users

--- client/state/selectors/has-loaded-gsuite-users.js
+++ client/state/selectors/has-loaded-gsuite-users.js
@@ -1,15 +1,15 @@
 /**
- * External dependencies
+ * Internal dependencies
  */
-import { get } from 'lodash';
+import getGSuiteUsers from 'state/selectors/get-gsuite-users';
 
 /**
  * Determines whether the list of G Suite users for the specified site has loaded.
  *
  * @param {object} state - global state tree
  * @param {number} siteId - identifier of the site
- * @returns {boolean} true if the list of G Suite users has loaded, false otherwise
+ * @returns {?boolean} true if the list of G Suite users has loaded, false otherwise
  */
 export default function hasLoadedGSuiteUsers( state, siteId ) {
-	return get( state, [ 'gsuiteUsers', siteId, 'users' ], null ) !== null;
+	return getGSuiteUsers( state, siteId ) !== null;
 }
