f325c1bc46f5586cc72665daab6513a28fecde09
Jeff Bowen <jblz@users.noreply.github.com>
2019-11-25 10:01:16 -0500

    Layout: More closely tie is-iframe classname to the state (#37902)
    * Layout: More closely tie is-iframe classname to the state

---
+++ client/state/selectors/is-iframe-for-html-element.js
@@ -0,0 +1,9 @@
+/**
+ * Internal dependencies
+ */
+import getSelectedSiteId from 'state/ui/selectors/get-selected-site-id';
+import getSectionName from 'state/ui/selectors/get-section-name';
+
+const isIframeForHtmlElement = state =>
+	!! ( getSelectedSiteId( state ) && getSectionName( state ) === 'gutenberg-editor' );
+export default isIframeForHtmlElement;
