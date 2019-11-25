7b65550e5375d48c61dcc93f46e23c95e6d23799
Jeff Bowen <jblz@users.noreply.github.com>
2019-11-25 15:38:11 -0500

    Enforce `isUnlaunchedSite`'s return to boolean & fix up JSDoc annotation

--- client/state/selectors/is-unlaunched-site.js
+++ client/state/selectors/is-unlaunched-site.js
@@ -7,12 +7,12 @@ import getRawSite from 'state/selectors/get-raw-site';
 /**
  * Returns true if the site is unlaunched
  *
- * @param {Object} state Global state tree
- * @param {Object} siteId Site ID
- * @return {Boolean} True if site is unlaunched
+ * @param {object} state Global state tree
+ * @param {number | null} siteId Site ID
+ * @returns {boolean} True if site is unlaunched
  */
 export default function isUnlaunchedSite( state, siteId ) {
 	const site = getRawSite( state, siteId );
 
-	return site && site.launch_status && site.launch_status === 'unlaunched';
+	return site?.launch_status === 'unlaunched';
 }
