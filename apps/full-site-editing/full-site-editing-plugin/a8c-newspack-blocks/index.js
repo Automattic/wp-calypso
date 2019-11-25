8b5291a48a756a5e37a65bb9b86e6d1958d77bc0
retrofox <rdsuarez@gmail.com>
2019-11-25 18:09:29 -0300

    simplify

--- apps/full-site-editing/full-site-editing-plugin/a8c-newspack-blocks/index.js
+++ apps/full-site-editing/full-site-editing-plugin/a8c-newspack-blocks/index.js
@@ -6,15 +6,13 @@ import { addFilter } from '@wordpress/hooks';
 import { __ } from '@wordpress/i18n';
 
 // Change the block name through of the `registerBlockType` hook.
-addFilter( 'blocks.registerBlockType', 'a8c/renaming-newspack-blocks', ( settings, name ) => {
-	if ( 'newspack-blocks/homepage-articles' !== name ) {
-		return settings;
-	}
-
-	return {
-		...settings,
-		name: 'a8c/homepage-posts',
-		title: __( 'Homepage Posts' ),
-		category: 'layout',
-	};
-} );
+addFilter( 'blocks.registerBlockType', 'a8c/renaming-newspack-blocks', ( settings, name ) =>
+	'newspack-blocks/homepage-articles' !== name
+		? settings
+		: {
+				...settings,
+				name: 'a8c/homepage-posts',
+				title: __( 'Homepage Posts' ),
+				category: 'layout',
+		  }
+);
