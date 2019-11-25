2e9bb45cbbfa7415331bfa3417634ef7a23758a7
Jon Surrell <jon.surrell@automattic.com>
2019-11-25 17:33:04 +0100

    Drop redundant cors args

--- client/landing/gutenboarding/stores/fse-templates/resolvers.ts
+++ client/landing/gutenboarding/stores/fse-templates/resolvers.ts
@@ -12,11 +12,7 @@ export function* getTemplates(
 	// Resolver has the same signature as corresponding selector without the initial state argument
 	verticalId: Parameters< typeof import('./selectors')[ 'getTemplates' ] >[ 1 ]
 ) {
-	// `credentials` and `mode` args are needed since we're accessing the WP.com REST API
-	// (rather than the WP Core REST API)
 	const templates = yield apiFetch( {
-		// credentials: 'same-origin',
-		// mode: 'cors',
 		url: `https://public-api.wordpress.com/wpcom/v2/verticals/${ verticalId }/templates`,
 	} );
 
