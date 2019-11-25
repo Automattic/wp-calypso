6b0fed788f2ca3d80a37b5af6e8a82807abff106
Jeff Bowen <jblz@users.noreply.github.com>
2019-11-25 09:45:01 -0500

    pull out debugs

--- client/layout/html-is-iframe-classname.tsx
+++ client/layout/html-is-iframe-classname.tsx
@@ -3,14 +3,11 @@
  */
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import debugFactory from 'debug';
 
 /**
  * Internal dependencies
  */
 import isIframeForHtmlElement from 'state/selectors/is-iframe-for-html-element';
-
-const debug = debugFactory( 'calypso:layout:html-is-iframe-classname' );
 
 const HtmlIsIframeClassname = () => {
 	const isIframe = useSelector( isIframeForHtmlElement );
@@ -19,16 +16,14 @@ const HtmlIsIframeClassname = () => {
 		const htmlNode = document.querySelector( 'html' );
 
 		if ( ! htmlNode ) {
-			debug( 'no html node' );
 			return;
 		}
 
 		if ( isIframe ) {
-			debug( 'adding is-iframe' );
 			htmlNode.classList.add( 'is-iframe' );
 			return () => htmlNode.classList.remove( 'is-iframe' );
 		}
-		debug( 'removing is-iframe' );
+
 		htmlNode.classList.remove( 'is-iframe' );
 	}, [ isIframe ] );
 
