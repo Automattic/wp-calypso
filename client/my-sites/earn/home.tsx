83efd121d1cc98ab1732a803f2cf4498bc39c5a5
artpi <artur.piszek@gmail.com>
2019-11-25 13:55:59 +0100

    Small fix for the untranslated 'new'

--- client/my-sites/earn/home.tsx
+++ client/my-sites/earn/home.tsx
@@ -142,7 +142,7 @@ const Home: FunctionComponent< ConnectedProps > = ( {
 		return {
 			title,
 			body,
-			badge: 'New',
+			badge: translate( 'New' ),
 			image: {
 				path: '/calypso/images/earn/recurring.svg',
 			},
