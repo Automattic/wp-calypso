0921a47dd5d320ab8c59fc4dd92e79b8643a697f
Jon Surrell <jon.surrell@automattic.com>
2019-11-25 17:52:20 +0100

    Revert "Add fixed `en` locale"
    This reverts commit 8154813d90967673a478bce80801f2f8071d50eb.

--- client/landing/gutenboarding/stores/fse-templates/resolvers.ts
+++ client/landing/gutenboarding/stores/fse-templates/resolvers.ts
@@ -13,8 +13,7 @@ export function* getTemplates(
 	verticalId: Parameters< typeof import('./selectors')[ 'getTemplates' ] >[ 1 ]
 ) {
 	const templates = yield apiFetch( {
-		// FIXME: Remove fixed `_locale=en` when endpoint supports _local=user
-		url: `https://public-api.wordpress.com/wpcom/v2/verticals/${ verticalId }/templates?_locale=en`,
+		url: `https://public-api.wordpress.com/wpcom/v2/verticals/${ verticalId }/templates`,
 	} );
 
 	return receiveTemplates( verticalId, templates );
