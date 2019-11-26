996848e9166937ddd8c7d0d4ee2a602be7d02d88
Aurorum <torres126@clubpenguinmountains.com>
2019-11-26 02:50:38 +0000

    SEO: Migrate Image to Webpack (#37654)
    * Move file
    
    * Import and use image

--- client/my-sites/site-settings/seo-settings/form.jsx
+++ client/my-sites/site-settings/seo-settings/form.jsx
@@ -64,6 +64,11 @@ import { getFirstConflictingPlugin } from 'lib/seo';
  */
 import './style.scss';
 
+/**
+ * Image dependencies
+ */
+import pageTitleImage from 'assets/images/illustrations/seo-page-title.svg';
+
 // Basic matching for HTML tags
 // Not perfect but meets the needs of this component well
 const anyHtmlTag = /<\/?[a-z][a-z0-9]*\b[^>]*>/i;
@@ -378,7 +383,7 @@ export class SeoForm extends React.Component {
 							<Card compact className="seo-settings__page-title-header">
 								<img
 									className="seo-settings__page-title-header-image"
-									src="/calypso/images/seo/page-title.svg"
+									src={ pageTitleImage }
 									alt=""
 								/>
 								<p className="seo-settings__page-title-header-text">
