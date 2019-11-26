55337576d71ec10ee230bba41f5570bc4e63ce83
Jon Surrell <jon.surrell@automattic.com>
2019-11-26 10:21:55 +0100

    Add docblock to selector

--- client/landing/gutenboarding/stores/verticals-templates/selectors.ts
+++ client/landing/gutenboarding/stores/verticals-templates/selectors.ts
@@ -5,6 +5,11 @@ import { State } from './reducer';
 
 export const getState = ( state: State ) => state;
 
+/**
+ *
+ * @param state Store state
+ * @param verticalId Vertical ID, e.g. `"p13v1"`
+ */
 export const getTemplates = ( state: State, verticalId: string ) => {
 	return state.templates[ verticalId ];
 };
