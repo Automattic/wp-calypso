7426063ee60c117ec2527db4959701468a03c184
Jon Surrell <jon.surrell@automattic.com>
2019-11-26 11:07:39 +0100

    Extract templates from response

--- client/landing/gutenboarding/stores/verticals-templates/resolvers.ts
+++ client/landing/gutenboarding/stores/verticals-templates/resolvers.ts
@@ -12,9 +12,9 @@ export function* getTemplates(
 	// Resolver has the same signature as corresponding selector without the initial state argument
 	verticalId: Parameters< typeof import('./selectors')[ 'getTemplates' ] >[ 1 ]
 ) {
-	const templates = yield apiFetch( {
+	const resp = yield apiFetch( {
 		url: `https://public-api.wordpress.com/wpcom/v2/verticals/${ verticalId }/templates`,
 	} );
 
-	return receiveTemplates( verticalId, templates );
+	return receiveTemplates( verticalId, resp.templates );
 }
