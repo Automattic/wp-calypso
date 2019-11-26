9f3ba96d14abaa7214c38d3707fc12f1b5319571
Enej Bajgoric <enej.bajgoric@gmail.com>
2019-11-26 13:53:52 +0100

    My Plans: Rename Products => Solutions

--- client/my-sites/plans/current-plan/purchases-listing.jsx
+++ client/my-sites/plans/current-plan/purchases-listing.jsx
@@ -218,7 +218,7 @@ class PurchasesListing extends Component {
 		return (
 			<Fragment>
 				<Card compact>
-					<strong>{ translate( 'My Products' ) }</strong>
+					<strong>{ translate( 'My Solutions' ) }</strong>
 				</Card>
 				{ productPurchases.map( purchase => (
 					<MyPlanCard
