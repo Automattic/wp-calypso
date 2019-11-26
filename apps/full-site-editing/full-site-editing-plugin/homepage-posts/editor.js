f1b8e05c70932dfb315975287392dedbde993372
Marek Hrabe <marekhrabe@me.com>
2019-11-26 17:06:35 +0100

    add loaders bridging fse-nha

---
+++ apps/full-site-editing/full-site-editing-plugin/homepage-posts/editor.js
@@ -0,0 +1,1 @@
+import './src/blocks/homepage-articles/editor';
---
+++ apps/full-site-editing/full-site-editing-plugin/homepage-posts/index.php
@@ -0,0 +1,9 @@
+<?php
+
+//define( 'NEWSPACK_BLOCKS__BLOCKS_DIRECTORY', __DIR__ );
+//define( 'NEWSPACK_BLOCKS__PLUGIN_DIR', __DIR__ );
+
+require_once __DIR__ . '/class-newspack-blocks.php';
+require_once __DIR__ . '/class-newspack-blocks-api.php';
+
+require_once __DIR__ . '/src/blocks/homepage-articles/view.php';
