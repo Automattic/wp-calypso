d2dba1103456c1a11328733ebc6d25b0094d5540
Jon Surrell <jon.surrell@automattic.com>
2019-11-26 13:21:57 +0100

    SPIKE: Share component from FSE
    Extract the component and satisfy its data requirements with our store.

--- client/landing/gutenboarding/gutenboard.tsx
+++ client/landing/gutenboarding/gutenboard.tsx
@@ -20,7 +20,14 @@ import { rawShortcut, displayShortcut, shortcutAriaLabel } from '@wordpress/keyc
 import '@wordpress/format-library';
 import classnames from 'classnames';
 import React, { useState } from 'react';
+import { parse as parseBlocks } from '@wordpress/blocks';
 import '@wordpress/components/build-style/style.css';
+
+/**
+ * FSE dependencies ???
+ */
+import TemplateSelectorControl from '../../../apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-control';
+import replacePlaceholders from '../../../apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/utils/replace-placeholders';
 
 /**
  * Internal dependencies
@@ -33,6 +40,7 @@ import './stores/domain-suggestions';
 import './stores/onboard';
 import './stores/verticals-templates';
 import './style.scss';
+import { useSelect } from '@wordpress/data';
 
 // Copied from https://github.com/WordPress/gutenberg/blob/c7d00c64a4c74236a4aab528b3987811ab928deb/packages/edit-post/src/keyboard-shortcuts.js#L11-L15
 // to be consistent with Gutenberg's shortcuts, and in order to avoid pulling in all of `@wordpress/edit-post`.
@@ -51,50 +59,32 @@ export function Gutenboard() {
 
 	const toggleGeneralSidebar = () => updateIsEditorSidebarOpened( isOpen => ! isOpen );
 
+	const templates = useSelect( select =>
+		select( 'automattic/verticals/templates' ).getTemplates( 'p13v1' )
+	);
+
+	// Parse templates blocks and store them into the state.
+	const blocksByTemplateSlug = templates?.reduce( ( prev, { slug, content } ) => {
+		prev[ slug ] = content ? parseBlocks( replacePlaceholders( content ) ) : [];
+		return prev;
+	}, {} );
+
+	const [ previewedTemplate, setPreviewedTemplate ] = useState< string | null >( null );
+
 	/* eslint-disable wpcalypso/jsx-classname-namespace */
 	return (
 		<div className="block-editor__container">
-			<SlotFillProvider>
-				<DropZoneProvider>
-					<div
-						className={ classnames( 'edit-post-layout', {
-							'is-sidebar-opened': isEditorSidebarOpened,
-						} ) }
-					>
-						<KeyboardShortcuts
-							bindGlobal
-							shortcuts={ {
-								[ toggleSidebarShortcut.raw ]: toggleGeneralSidebar,
-							} }
-						/>
-						<Header
-							isEditorSidebarOpened={ isEditorSidebarOpened }
-							toggleGeneralSidebar={ toggleGeneralSidebar }
-							toggleSidebarShortcut={ toggleSidebarShortcut }
-						/>
-						<BlockEditorProvider value={ [ onboardingBlock ] } settings={ { templateLock: 'all' } }>
-							<div className="edit-post-layout__content">
-								<div
-									className="edit-post-visual-editor editor-styles-wrapper"
-									role="region"
-									aria-label={ __( 'Onboarding screen content' ) }
-									tabIndex={ -1 }
-								>
-									<WritingFlow>
-										<ObserveTyping>
-											<BlockList className="gutenboarding-block-list" />
-										</ObserveTyping>
-									</WritingFlow>
-								</div>
-							</div>
-							<div>
-								<SettingsSidebar isActive={ isEditorSidebarOpened } />
-								<SidebarSlot />
-							</div>
-						</BlockEditorProvider>
-					</div>
-				</DropZoneProvider>
-			</SlotFillProvider>
+			<h3>previewing: { previewedTemplate }</h3>
+			<TemplateSelectorControl
+				label={ __( 'Layout', 'full-site-editing' ) }
+				templates={ templates ?? [] }
+				blocksByTemplates={ blocksByTemplateSlug }
+				onTemplateSelect={ setPreviewedTemplate }
+				useDynamicPreview={ false }
+				siteInformation={ undefined }
+				selectedTemplate={ previewedTemplate }
+				// handleTemplateConfirmation={ this.handleConfirmation }
+			/>
 			<Popover.Slot />
 		</div>
 	);
