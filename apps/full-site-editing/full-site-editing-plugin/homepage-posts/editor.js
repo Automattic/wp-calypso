b7a5a5d05b34c9b600523ec18ee82c8dc491c217
Marek Hrabe <marekhrabe@me.com>
2019-11-27 16:01:22 +0100

    export homepage posts block
    Co-authored-by: Damián Suárez <rdsuarez@gmail.com>

--- apps/full-site-editing/full-site-editing-plugin/homepage-posts/editor.js
+++ apps/full-site-editing/full-site-editing-plugin/homepage-posts/editor.js
@@ -1,1 +1,15 @@
-import './newspack-homepage-articles/blocks/homepage-articles/editor';
+/**
+ * Internal dependencies
+ */
+import { registerBlockType } from '@wordpress/blocks';
+
+/**
+ * NHA dependencies
+ */
+import { settings } from './newspack-homepage-articles/blocks/homepage-articles/index';
+
+registerBlockType( 'a8c/homepage-posts', {
+    ...settings,
+    title: __( 'Homepage Posts' ),
+    category: 'layout',
+} );
