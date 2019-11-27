f71cbf5d7d1ee0c294c7d8509804d343f035565a
Marek Hrabe <marekhrabe@me.com>
2019-11-27 16:19:47 +0100

    define newsblock constants
    Co-authored-by: Damián Suárez <rdsuarez@gmail.com>

--- apps/full-site-editing/full-site-editing-plugin/homepage-posts/index.php
+++ apps/full-site-editing/full-site-editing-plugin/homepage-posts/index.php
@@ -1,7 +1,7 @@
 <?php
-
-//define( 'NEWSPACK_BLOCKS__BLOCKS_DIRECTORY', __DIR__ );
-//define( 'NEWSPACK_BLOCKS__PLUGIN_DIR', __DIR__ );
+define( 'NEWSPACK_BLOCKS__BLOCKS_DIRECTORY', '../dist/' );
+define( 'NEWSPACK_BLOCKS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'NEWSPACK_BLOCKS__VERSION', '1.0.0-alpha.17' );
 
 require_once __DIR__ . '/newspack-homepage-articles/class-newspack-blocks.php';
 require_once __DIR__ . '/newspack-homepage-articles/class-newspack-blocks-api.php';
