9df8d354b3902cdcd5b91c9f821a6ecc6af24fe0
taggon <gonom9@gmail.com>
2019-11-28 10:56:32 +0900

    Limit the banner to the US

--- client/reader/sidebar/reader-sidebar-nudges/index.jsx
+++ client/reader/sidebar/reader-sidebar-nudges/index.jsx
@@ -11,6 +11,7 @@ import { localize, getLocaleSlug } from 'i18n-calypso';
 import QuerySitePlans from 'components/data/query-site-plans';
 import SidebarBanner from 'my-sites/current-site/sidebar-banner';
 import isDomainOnlySite from 'state/selectors/is-domain-only-site';
+import { getCurrentUserCountryCode } from 'state/current-user/selectors';
 import isEligibleForFreeToPaidUpsell from 'state/selectors/is-eligible-for-free-to-paid-upsell';
 import { isJetpackSite } from 'state/sites/selectors';
 import getSites from 'state/selectors/get-sites';
@@ -49,16 +50,20 @@ export function ReaderSidebarNudges( props ) {
 }
 
 function mapStateToProps( state ) {
+	const isDevelopment = 'development' === process.env.NODE_ENV;
 	const siteCount = getSites( state ).length;
 	const siteId = getPrimarySiteId( state );
 	const siteSlug = getPrimarySiteSlug( state );
+	const devCountryCode = isDevelopment && global.window && global.window.userCountryCode;
+	const countryCode = devCountryCode || getCurrentUserCountryCode( state );
 
 	return {
 		siteId,
 		siteSlug,
 		isEligibleForFreeToPaidUpsellNudge:
-			siteCount === 1 && // available for the user who owns one site only
+			siteCount === 1 && // available when a user owns one site only
 			'en' === getLocaleSlug( state ) && // only for English speakers
+			'US' === countryCode && // only for US residents
 			! isDomainOnlySite( state, siteId ) && // not for domain only sites
 			! isJetpackSite( state, siteId ) && // not for Jetpack sites
 			isEligibleForFreeToPaidUpsell( state, siteId ),
