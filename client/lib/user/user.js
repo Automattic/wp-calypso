0bd296e35a6ecec2faed672a34cba66aeeb08c01
Rob Landers <landers.robert@gmail.com>
2019-11-29 12:41:18 +0100

    Fix typo

--- client/lib/user/user.js
+++ client/lib/user/user.js
@@ -132,7 +132,7 @@ User.prototype.fetch = function() {
 		.get( {
 			meta: 'flags',
 			abtests: getActiveTestNames( { appendDatestamp: true, asCSV: true } ),
-			active_tets: config.isEnabled( 'ive/me' ),
+			active_tests: config.isEnabled( 'ive/me' ),
 		} )
 		.then( data => {
 			debug( 'User successfully retrieved from api:', data );
