758a44548596e51952a0bb92da604da2938801ca
Marek Hrabe <marekhrabe@me.com>
2019-11-29 12:55:00 +0100

    Revert "Revert "demo""
    This reverts commit a22a578a6fe3fddefb4567e36f3ba56b7f65b311.

--- apps/full-site-editing/full-site-editing-plugin/homepage-posts/editor.js
+++ apps/full-site-editing/full-site-editing-plugin/homepage-posts/editor.js
@@ -9,7 +9,7 @@ import { registerBlockType } from '@wordpress/blocks';
 import { settings } from './newspack-homepage-articles/blocks/homepage-articles/index';
 
 registerBlockType( 'a8c/homepage-posts', {
-    ...settings,
-    title: __( 'Homepage Posts' ),
-    category: 'layout',
+	...settings,
+	title: 'Homepage Posts',
+	category: 'layout',
 } );
