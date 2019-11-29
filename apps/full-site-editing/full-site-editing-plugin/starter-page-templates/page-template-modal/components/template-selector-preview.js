9572bd37323e48f246c3fa8af2ccbd6d14a95a1a
Copons <Copons@users.noreply.github.com>
2019-11-29 18:42:25 +0000

    Try to force the preview to behave when scaling

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-preview.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-preview.js
@@ -12,7 +12,14 @@ import classnames from 'classnames';
 /* eslint-disable import/no-extraneous-dependencies */
 import { __ } from '@wordpress/i18n';
 import { Disabled } from '@wordpress/components';
-import { useState, useEffect, useLayoutEffect, useRef, useReducer } from '@wordpress/element';
+import {
+	useState,
+	useEffect,
+	useLayoutEffect,
+	useRef,
+	useReducer,
+	useCallback,
+} from '@wordpress/element';
 /* eslint-enable import/no-extraneous-dependencies */
 
 /**
@@ -25,6 +32,7 @@ const TemplateSelectorPreview = ( { blocks, viewportWidth, title } ) => {
 	const THRESHOLD_RESIZE = 300;
 
 	const [ visibility, setVisibility ] = useState( 'hidden' );
+	const [ previewViewport, setPreviewViewport ] = useState( viewportWidth );
 	const ref = useRef( null );
 
 	const [ recompute, triggerRecompute ] = useReducer( state => state + 1, 0 );
@@ -38,52 +46,61 @@ const TemplateSelectorPreview = ( { blocks, viewportWidth, title } ) => {
 
 	const updateTemplateTitle = () => {
 		// Get DOM reference.
-		setTimeout( () => {
-			if ( ! ref || ! ref.current ) {
-				return;
+		if ( ! ref || ! ref.current ) {
+			return;
+		}
+
+		// Try to get the preview content element.
+		const previewContainerEl = ref.current.querySelector( '.block-editor-block-preview__content' );
+		if ( ! previewContainerEl ) {
+			return;
+		}
+
+		// Try to get the `transform` css rule from the preview container element.
+		const elStyles = window.getComputedStyle( previewContainerEl );
+		if ( elStyles && elStyles.transform ) {
+			const titleElement = ref.current.querySelector( '.editor-post-title' );
+			if ( titleElement ) {
+				// Apply the same transform css rule at template title element.
+				titleElement.style.transform = elStyles.transform;
 			}
 
-			// Try to get the preview content element.
-			const previewContainerEl = ref.current.querySelector(
-				'.block-editor-block-preview__content'
+			// Pick up scale factor from `transform` css.
+			let scale = elStyles.transform.replace( /matrix\((.+)\)$/i, '$1' ).split( ',' );
+			scale = scale && scale.length ? Number( scale[ 0 ] ) : null;
+			scale = isNaN( scale ) ? null : scale;
+
+			// Try to adjust vertical offset of the large preview.
+			const offsetCorrectionEl = previewContainerEl.closest(
+				'.template-selector-preview__offset-correction'
 			);
-			if ( ! previewContainerEl ) {
-				return;
+
+			if ( offsetCorrectionEl && scale ) {
+				const titleHeight = titleElement ? titleElement.offsetHeight : null;
+				offsetCorrectionEl.style.top = `${ titleHeight * scale }px`;
 			}
+		}
 
-			// Try to get the `transform` css rule from the preview container element.
-			const elStyles = window.getComputedStyle( previewContainerEl );
-			if ( elStyles && elStyles.transform ) {
-				const titleElement = ref.current.querySelector( '.editor-post-title' );
-				if ( titleElement ) {
-					// Apply the same transform css rule at template title element.
-					titleElement.style.transform = elStyles.transform;
-				}
-
-				// Pick up scale factor from `transform` css.
-				let scale = elStyles.transform.replace( /matrix\((.+)\)$/i, '$1' ).split( ',' );
-				scale = scale && scale.length ? Number( scale[ 0 ] ) : null;
-				scale = isNaN( scale ) ? null : scale;
-
-				// Try to adjust vertical offset of the large preview.
-				const offsetCorrectionEl = previewContainerEl.closest(
-					'.template-selector-preview__offset-correction'
-				);
-
-				if ( offsetCorrectionEl && scale ) {
-					const titleHeight = titleElement ? titleElement.offsetHeight : null;
-					offsetCorrectionEl.style.top = `${ titleHeight * scale }px`;
-				}
-			}
-
-			setVisibility( 'visible' );
-		}, 300 );
+		setVisibility( 'visible' );
 	};
+
+	const updatePreviewViewport = useCallback( () => {
+		if ( ! ref || ! ref.current ) {
+			return;
+		}
+		const wrapperWidth = ref.current.clientWidth;
+		if ( wrapperWidth >= viewportWidth ) {
+			setPreviewViewport( wrapperWidth );
+		} else {
+			setPreviewViewport( viewportWidth );
+		}
+	}, [ viewportWidth ] );
 
 	useLayoutEffect( () => {
 		setVisibility( 'hidden' );
+		updatePreviewViewport();
 		updateTemplateTitle();
-	}, [ blocks ] );
+	}, [ blocks, updatePreviewViewport ] );
 
 	useEffect( () => {
 		if ( ! blocks || ! blocks.length ) {
@@ -92,6 +109,7 @@ const TemplateSelectorPreview = ( { blocks, viewportWidth, title } ) => {
 
 		const rePreviewTemplate = () => {
 			updateTemplateTitle();
+			updatePreviewViewport();
 			triggerRecompute();
 		};
 
@@ -101,7 +119,7 @@ const TemplateSelectorPreview = ( { blocks, viewportWidth, title } ) => {
 		return () => {
 			window.removeEventListener( 'resize', refreshPreview );
 		};
-	}, [ blocks ] );
+	}, [ blocks, updatePreviewViewport ] );
 
 	if ( isEmpty( blocks ) || ! isArray( blocks ) ) {
 		return (
@@ -122,7 +140,11 @@ const TemplateSelectorPreview = ( { blocks, viewportWidth, title } ) => {
 						<div className="editor-writing-flow">
 							<PreviewTemplateTitle title={ title } />
 							<div className="template-selector-preview__offset-correction">
-								<BlockPreview key={ recompute } blocks={ blocks } viewportWidth={ viewportWidth } />
+								<BlockPreview
+									key={ recompute }
+									blocks={ blocks }
+									viewportWidth={ previewViewport }
+								/>
 							</div>
 						</div>
 					</div>
