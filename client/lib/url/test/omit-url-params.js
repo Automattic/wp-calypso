e300d6ec49fe892841b7ab55b7081f46d7acb0ef
Sérgio Gomes <mail@sgomes.com>
2019-12-02 13:26:03 +0000

    Use URL-encoded param in examples.

--- client/lib/url/test/omit-url-params.js
+++ client/lib/url/test/omit-url-params.js
@@ -25,26 +25,30 @@ describe( 'omitUrlParams()', () => {
 	describe( 'when an absolute URL is supplied', () => {
 		describe( 'when no omitting params are supplied', () => {
 			test( 'should return the URL without modification', () => {
-				const url = 'http://example.com/path?query=banana&query2=pineapple&query3=avocado';
+				const url =
+					'http://example.com/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const actual = omitUrlParams( url );
-				const expected = 'http://example.com/path?query=banana&query2=pineapple&query3=avocado';
+				const expected =
+					'http://example.com/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				expect( actual ).toBe( expected );
 			} );
 		} );
 
 		describe( 'when a single omitting param is supplied as a string', () => {
 			test( 'should return the URL with that param removed', () => {
-				const url = 'http://example.com/path?query=banana&query2=pineapple&query3=avocado';
+				const url =
+					'http://example.com/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const param = 'query2';
 				const actual = omitUrlParams( url, param );
-				const expected = 'http://example.com/path?query=banana&query3=avocado';
+				const expected = 'http://example.com/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query3=avocado';
 				expect( actual ).toBe( expected );
 			} );
 		} );
 
 		describe( 'when an array of omitting params is supplied', () => {
 			test( 'should return the URL with each of those params removed', () => {
-				const url = 'http://example.com/path?query=banana&query2=pineapple&query3=avocado';
+				const url =
+					'http://example.com/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const params = [ 'query', 'query2' ];
 				const actual = omitUrlParams( url, params );
 				const expected = 'http://example.com/path?query3=avocado';
@@ -56,26 +60,30 @@ describe( 'omitUrlParams()', () => {
 	describe( 'when a scheme-relative URL is supplied', () => {
 		describe( 'when no omitting params are supplied', () => {
 			test( 'should return the URL without modification', () => {
-				const url = '//example.com/path?query=banana&query2=pineapple&query3=avocado';
+				const url =
+					'//example.com/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const actual = omitUrlParams( url );
-				const expected = '//example.com/path?query=banana&query2=pineapple&query3=avocado';
+				const expected =
+					'//example.com/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				expect( actual ).toBe( expected );
 			} );
 		} );
 
 		describe( 'when a single omitting param is supplied as a string', () => {
 			test( 'should return the URL with that param removed', () => {
-				const url = '//example.com/path?query=banana&query2=pineapple&query3=avocado';
+				const url =
+					'//example.com/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const param = 'query2';
 				const actual = omitUrlParams( url, param );
-				const expected = '//example.com/path?query=banana&query3=avocado';
+				const expected = '//example.com/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query3=avocado';
 				expect( actual ).toBe( expected );
 			} );
 		} );
 
 		describe( 'when an array of omitting params is supplied', () => {
 			test( 'should return the URL with each of those params removed', () => {
-				const url = '//example.com/path?query=banana&query2=pineapple&query3=avocado';
+				const url =
+					'//example.com/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const params = [ 'query', 'query2' ];
 				const actual = omitUrlParams( url, params );
 				const expected = '//example.com/path?query3=avocado';
@@ -87,26 +95,26 @@ describe( 'omitUrlParams()', () => {
 	describe( 'when a path-absolute URL is supplied', () => {
 		describe( 'when no omitting params are supplied', () => {
 			test( 'should return the URL without modification', () => {
-				const url = '/path?query=banana&query2=pineapple&query3=avocado';
+				const url = '/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const actual = omitUrlParams( url );
-				const expected = '/path?query=banana&query2=pineapple&query3=avocado';
+				const expected = '/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				expect( actual ).toBe( expected );
 			} );
 		} );
 
 		describe( 'when a single omitting param is supplied as a string', () => {
 			test( 'should return the URL with that param removed', () => {
-				const url = '/path?query=banana&query2=pineapple&query3=avocado';
+				const url = '/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const param = 'query2';
 				const actual = omitUrlParams( url, param );
-				const expected = '/path?query=banana&query3=avocado';
+				const expected = '/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query3=avocado';
 				expect( actual ).toBe( expected );
 			} );
 		} );
 
 		describe( 'when an array of omitting params is supplied', () => {
 			test( 'should return the URL with each of those params removed', () => {
-				const url = '/path?query=banana&query2=pineapple&query3=avocado';
+				const url = '/path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const params = [ 'query', 'query2' ];
 				const actual = omitUrlParams( url, params );
 				const expected = '/path?query3=avocado';
@@ -118,26 +126,26 @@ describe( 'omitUrlParams()', () => {
 	describe( 'when a path-relative URL is supplied', () => {
 		describe( 'when no omitting params are supplied', () => {
 			test( 'should return the URL without modification', () => {
-				const url = 'path?query=banana&query2=pineapple&query3=avocado';
+				const url = 'path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const actual = omitUrlParams( url );
-				const expected = 'path?query=banana&query2=pineapple&query3=avocado';
+				const expected = 'path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				expect( actual ).toBe( expected );
 			} );
 		} );
 
 		describe( 'when a single omitting param is supplied as a string', () => {
 			test( 'should return the URL with that param removed', () => {
-				const url = 'path?query=banana&query2=pineapple&query3=avocado';
+				const url = 'path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const param = 'query2';
 				const actual = omitUrlParams( url, param );
-				const expected = 'path?query=banana&query3=avocado';
+				const expected = 'path?query=%E3%83%90%E3%83%8A%E3%83%8A&query3=avocado';
 				expect( actual ).toBe( expected );
 			} );
 		} );
 
 		describe( 'when an array of omitting params is supplied', () => {
 			test( 'should return the URL with each of those params removed', () => {
-				const url = 'path?query=banana&query2=pineapple&query3=avocado';
+				const url = 'path?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const params = [ 'query', 'query2' ];
 				const actual = omitUrlParams( url, params );
 				const expected = 'path?query3=avocado';
@@ -149,26 +157,26 @@ describe( 'omitUrlParams()', () => {
 	describe( 'when a path-relative URL with nothing but params is supplied', () => {
 		describe( 'when no omitting params are supplied', () => {
 			test( 'should return the URL without modification', () => {
-				const url = '?query=banana&query2=pineapple&query3=avocado';
+				const url = '?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const actual = omitUrlParams( url );
-				const expected = '?query=banana&query2=pineapple&query3=avocado';
+				const expected = '?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				expect( actual ).toBe( expected );
 			} );
 		} );
 
 		describe( 'when a single omitting param is supplied as a string', () => {
 			test( 'should return the URL with that param removed', () => {
-				const url = '?query=banana&query2=pineapple&query3=avocado';
+				const url = '?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const param = 'query2';
 				const actual = omitUrlParams( url, param );
-				const expected = '?query=banana&query3=avocado';
+				const expected = '?query=%E3%83%90%E3%83%8A%E3%83%8A&query3=avocado';
 				expect( actual ).toBe( expected );
 			} );
 		} );
 
 		describe( 'when an array of omitting params is supplied', () => {
 			test( 'should return the URL with each of those params removed', () => {
-				const url = '?query=banana&query2=pineapple&query3=avocado';
+				const url = '?query=%E3%83%90%E3%83%8A%E3%83%8A&query2=pineapple&query3=avocado';
 				const params = [ 'query', 'query2' ];
 				const actual = omitUrlParams( url, params );
 				const expected = '?query3=avocado';
