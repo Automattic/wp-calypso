b559cf8cc3435e72123d7d0960a1de068392001c
Marek Hrabe <marekhrabe@me.com>
2019-12-03 14:09:10 +0100

    remove title override

--- apps/full-site-editing/full-site-editing-plugin/homepage-posts/editor.js
+++ apps/full-site-editing/full-site-editing-plugin/homepage-posts/editor.js
@@ -1,7 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /**
  * Internal dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
+/* eslint-enable import/no-extraneous-dependencies */
 
 /**
  * NHA dependencies
@@ -9,7 +11,6 @@ import { registerBlockType } from '@wordpress/blocks';
 import { settings } from './newspack-homepage-articles/blocks/homepage-articles/index';
 
 registerBlockType( 'a8c/homepage-posts', {
-    ...settings,
-    title: __( 'Homepage Posts' ),
-    category: 'layout',
+	...settings,
+	category: 'layout',
 } );
