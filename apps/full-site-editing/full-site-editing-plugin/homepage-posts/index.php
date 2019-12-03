f1941bf9869909315558e52f6d75d5e0dc23c300
retrofox <rdsuarez@gmail.com>
2019-12-03 18:23:00 -0300

    add block name to config. Fix comparing name error

--- apps/full-site-editing/full-site-editing-plugin/homepage-posts/index.php
+++ apps/full-site-editing/full-site-editing-plugin/homepage-posts/index.php
@@ -18,7 +18,7 @@ $config = json_decode(
 	file_get_contents( __DIR__ . '/config.json' ), // phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
 	true
 );
-$plugin_name = $config['plugin_name'];
+
 
 /**
  * Filters block name.
@@ -26,15 +26,15 @@ $plugin_name = $config['plugin_name'];
  * @param string $name Block name.
  * @return string
  */
-//function homepage_posts_block_name( $name ) {
-$homepage_posts_block_name = function ( $name ) use ( $plugin_name ) {
-	if ( 'newspack-blocks/homepage-articles' === $name ) {
-		return "a8c/${plugin_name}";
+$homepage_posts_block_name = function ( $name ) use ( $config ) {
+	if ( $config['nha_block_name'] === $name ) {
+		return "a8c/${config['plugin_name']}";
 	}
 	return $name;
 };
 //add_filter( 'newspack_blocks_block_name', $homepage_posts_block_name );
 add_filter( 'newspack_blocks_block_name', $homepage_posts_block_name );
+
 
 /**
  * Filters block arguments for `register_block_type()`.
@@ -43,15 +43,15 @@ add_filter( 'newspack_blocks_block_name', $homepage_posts_block_name );
  * @param string $name Block name.
  * @return array
  */
-$homepage_posts_block_args = function ( $args, $name ) use( $plugin_name ){
-	if ( 'homepage-articles' !== $name ) {
+$homepage_posts_block_args = function ( $args, $name ) use( $config ){
+	if ( $config['nha_block_name'] !== $name ) {
 		return $args;
 	}
 
 	// Editor script.
 	$script_data = require NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.asset.php';
 	wp_register_script(
-		"${plugin_name}-editor",
+		"${config['plugin_name']}-editor",
 		plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.js', __FILE__ ),
 		$script_data['dependencies'],
 		$script_data['version'],
@@ -60,15 +60,15 @@ $homepage_posts_block_args = function ( $args, $name ) use( $plugin_name ){
 
 	// Editor style.
 	$editor_style = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.css', __FILE__ );
-	wp_register_style( "${plugin_name}-editor", $editor_style, [], NEWSPACK_BLOCKS__VERSION );
+	wp_register_style( "${config['plugin_name']}-editor", $editor_style, [], NEWSPACK_BLOCKS__VERSION );
 
 	// View style.
 	$editor_style = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'view.css', __FILE__ );
-	wp_register_style( "${plugin_name}-view", $editor_style, [], NEWSPACK_BLOCKS__VERSION );
+	wp_register_style( "${config['plugin_name']}-view", $editor_style, [], NEWSPACK_BLOCKS__VERSION );
 
-	$args['editor_script'] = "${plugin_name}-editor";
-	$args['editor_style']  = "${plugin_name}-editor";
-	$args['style']         = "${plugin_name}-view";
+	$args['editor_script'] = "${config['plugin_name']}-editor";
+	$args['editor_style']  = "${config['plugin_name']}-editor";
+	$args['style']         = "${config['plugin_name']}-view";
 
 	return $args;
 };
