2fe19a3d811f6d46c16c6f6d60911660ed2c22aa
Konstantin Obenland <obenland@gmx.de>
2019-12-03 14:20:11 -0800

    Namespace, comments update

--- apps/full-site-editing/full-site-editing-plugin/homepage-posts/index.php
+++ apps/full-site-editing/full-site-editing-plugin/homepage-posts/index.php
@@ -1,16 +1,37 @@
 <?php
+/**
+ * Hompage posts file.
+ *
+ * @package A8C\FSE
+ */
+
+namespace A8C\FSE;
+
 define( 'NEWSPACK_BLOCKS__BLOCKS_DIRECTORY', 'dist/' );
 define( 'NEWSPACK_BLOCKS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NEWSPACK_BLOCKS__VERSION', '1.0.0-alpha.17' );
 
+/**
+ * Filters block name.
+ *
+ * @param string $name Block name.
+ * @return string
+ */
 function homepage_posts_block_name( $name ) {
 	if ( 'newspack-blocks/homepage-articles' === $name ) {
 		return 'a8c/homepage-posts';
 	}
 	return $name;
 }
-add_filter( 'newspack_blocks_block_name', 'homepage_posts_block_name' );
+add_filter( 'newspack_blocks_block_name', __NAMESPACE__ . '\homepage_posts_block_name' );
 
+/**
+ * Filters block arguments for `register_block_type()`.
+ *
+ * @param array  $args Arguments to `register_block_type()`.
+ * @param string $name Block name.
+ * @return array
+ */
 function homepage_posts_block_args( $args, $name ) {
 	if ( 'newspack-blocks/homepage-articles' !== $name ) {
 		return $args;
@@ -28,11 +49,11 @@ function homepage_posts_block_args( $args, $name ) {
 
 	// Editor style.
 	$editor_style = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.css', __FILE__ );
-	wp_register_style( 'homepage-posts-editor', $editor_style, array(), NEWSPACK_BLOCKS__VERSION );
+	wp_register_style( 'homepage-posts-editor', $editor_style, [], NEWSPACK_BLOCKS__VERSION );
 
 	// View style.
 	$editor_style = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'view.css', __FILE__ );
-	wp_register_style( 'homepage-posts-view', $editor_style, array(), NEWSPACK_BLOCKS__VERSION );
+	wp_register_style( 'homepage-posts-view', $editor_style, [], NEWSPACK_BLOCKS__VERSION );
 
 	$args['editor_script'] = 'homepage-posts-editor';
 	$args['editor_style']  = 'homepage-posts-editor';
@@ -40,7 +61,7 @@ function homepage_posts_block_args( $args, $name ) {
 
 	return $args;
 }
-add_filter( 'newspack_blocks_block_args', 'homepage_posts_block_args', 10, 2 );
+add_filter( 'newspack_blocks_block_args', __NAMESPACE__ . '\homepage_posts_block_args', 10, 2 );
 
 require_once __DIR__ . '/newspack-homepage-articles/class-newspack-blocks.php';
 require_once __DIR__ . '/newspack-homepage-articles/class-newspack-blocks-api.php';
