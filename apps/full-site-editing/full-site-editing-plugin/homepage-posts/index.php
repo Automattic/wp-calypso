3c8637b6930a5818cd543f9cd2fb9fc57fe9546e
Marek Hrabe <marekhrabe@me.com>
2019-12-03 14:28:28 +0100

    declarative enqueues

--- apps/full-site-editing/full-site-editing-plugin/homepage-posts/index.php
+++ apps/full-site-editing/full-site-editing-plugin/homepage-posts/index.php
@@ -1,28 +1,46 @@
 <?php
-define( 'NEWSPACK_BLOCKS__BLOCKS_DIRECTORY', '../dist/' );
+define( 'NEWSPACK_BLOCKS__BLOCKS_DIRECTORY', 'dist/' );
 define( 'NEWSPACK_BLOCKS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NEWSPACK_BLOCKS__VERSION', '1.0.0-alpha.17' );
 
-function homepage_posts_view_assets( $style_path, $type, $is_rtl ) {
-	if ( 'homepage-articles' !== $type ) {
-		return $style_path;
-	}
-
-	if ( $is_rtl ) {
-		return plugins_url( 'dist/view.rtl.css', __FILE__ );
-	}
-
-	return plugins_url( 'dist/view.css', __FILE__ );
-}
-add_filter( 'newspack_blocks_enqueue_view_assets', 'homepage_posts_view_assets', 10, 3 );
-
 function homepage_posts_block_name( $name ) {
-	if ( 'homepage-articles' === $name ) {
+	if ( 'newspack-blocks/homepage-articles' === $name ) {
 		return 'a8c/homepage-posts';
 	}
 	return $name;
 }
 add_filter( 'newspack_blocks_block_name', 'homepage_posts_block_name' );
+
+function homepage_posts_block_args( $args, $name ) {
+	if ( 'newspack-blocks/homepage-articles' !== $name ) {
+		return $args;
+	}
+
+	// Editor script.
+	$script_data = require NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.asset.php';
+	wp_register_script(
+		'homepage-posts-editor',
+		plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.js', __FILE__ ),
+		$script_data['dependencies'],
+		$script_data['version'],
+		true
+	);
+
+	// Editor style.
+	$editor_style = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'editor.css', __FILE__ );
+	wp_register_style( 'homepage-posts-editor', $editor_style, array(), NEWSPACK_BLOCKS__VERSION );
+
+	// View style.
+	$editor_style = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . 'view.css', __FILE__ );
+	wp_enqueue_style( 'homepage-posts-view', $editor_style, array(), NEWSPACK_BLOCKS__VERSION );
+
+	$args['editor_script'] = 'homepage-posts-editor';
+	$args['editor_style']  = 'homepage-posts-editor';
+	$args['style']         = 'homepage-posts-view';
+
+	return $args;
+}
+add_filter( 'newspack_blocks_block_args', 'homepage_posts_block_args', 10, 2 );
 
 require_once __DIR__ . '/newspack-homepage-articles/class-newspack-blocks.php';
 require_once __DIR__ . '/newspack-homepage-articles/class-newspack-blocks-api.php';
