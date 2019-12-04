e6bfde54e0a00f7be32e3b2ead5bebf40b88f135
Miguel Torres <miguel.torres@automattic.com>
2019-12-04 10:41:59 +0100

    Hosting: Add testing coverage to SFTP/phpMyAdmin data layer (#38046)
    Adds testing coverage for all the SFTP/phpMyAdmin reducers and selectors available in the state.
    
    This also removes some tests added in #37966 that were just mere boilerplate to double check implementations details rather than behavior. Such tests are discouraged by our unit testing guidelines.

---
+++ client/state/selectors/test/get-atomic-hosting-sftp-users.js
@@ -0,0 +1,62 @@
+/**
+ * Internal dependencies
+ */
+import { getAtomicHostingSftpUsers } from 'state/selectors/get-atomic-hosting-sftp-users';
+
+describe( 'getAtomicHostingSftpUsers()', () => {
+	test( 'should return null if there is no hosting data', () => {
+		const state = {};
+		const users = getAtomicHostingSftpUsers( state, 123 );
+
+		expect( users ).toBeNull();
+	} );
+
+	test( 'should return null if the site is not tracked', () => {
+		const state = {
+			atomicHosting: {
+				123: {
+					sftpUsers: [
+						{ username: 'u1', password: 'p1' },
+						{ username: 'u2', password: 'p3' },
+						{ username: 'u3', password: 'p3' },
+					],
+				},
+			},
+		};
+		const users = getAtomicHostingSftpUsers( state, 124 );
+
+		expect( users ).toBeNull();
+	} );
+
+	test( 'should return null if there are no users data', () => {
+		const state = {
+			atomicHosting: {
+				123: {},
+			},
+		};
+		const users = getAtomicHostingSftpUsers( state, 123 );
+
+		expect( users ).toBeNull();
+	} );
+
+	test( 'should return list of users', () => {
+		const state = {
+			atomicHosting: {
+				123: {
+					sftpUsers: [
+						{ username: 'u1', password: 'p1' },
+						{ username: 'u2', password: 'p3' },
+						{ username: 'u3', password: 'p3' },
+					],
+				},
+			},
+		};
+		const users = getAtomicHostingSftpUsers( state, 123 );
+
+		expect( users ).toEqual( [
+			{ username: 'u1', password: 'p1' },
+			{ username: 'u2', password: 'p3' },
+			{ username: 'u3', password: 'p3' },
+		] );
+	} );
+} );
