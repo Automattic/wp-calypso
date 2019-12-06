a0c44688cf13ac6e7688308f43acea2d6f408f90
Kirk Wight <kwight@kwight.ca>
2019-12-06 10:33:54 -0800

    Hosting: Allow all sites to see the Hosting section in development. (#38155)

--- client/state/selectors/can-site-view-atomic-hosting.js
+++ client/state/selectors/can-site-view-atomic-hosting.js
@@ -1,8 +1,12 @@
 /**
  * Internal Dependencies
  */
-import { getSelectedSiteId } from 'state/ui/selectors';
 import { isEnabled } from 'config';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { isJetpackSite } from 'state/sites/selectors';
+import isVipSite from 'state/selectors/is-vip-site';
+import getRawSite from 'state/selectors/get-raw-site';
+import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
 import canCurrentUser from 'state/selectors/can-current-user';
 import isSiteOnAtomicPlan from 'state/selectors/is-site-on-atomic-plan';
 
@@ -20,9 +24,25 @@ export default function canSiteViewAtomicHosting( state ) {
 
 	const siteId = getSelectedSiteId( state );
 
-	if ( ! isSiteOnAtomicPlan( state, siteId ) ) {
+	if ( ! getRawSite( state, siteId ) ) {
 		return false;
 	}
 
-	return canCurrentUser( state, siteId, 'manage_options' );
+	if ( ! canCurrentUser( state, siteId, 'manage_options' ) ) {
+		return false;
+	}
+
+	if ( isVipSite( state, siteId ) ) {
+		return false;
+	}
+
+	if ( isJetpackSite( state, siteId ) && ! isSiteAutomatedTransfer( state, siteId ) ) {
+		return false;
+	}
+
+	if ( isEnabled( 'hosting/all-sites' ) ) {
+		return true;
+	}
+
+	return isSiteOnAtomicPlan( state, siteId );
 }
