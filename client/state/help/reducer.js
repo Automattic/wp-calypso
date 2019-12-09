e4107dfa61d62470a83f3c0fd1d6e492763ed46d
Jarda Snajdr <jsnajdr@gmail.com>
2019-12-09 13:29:44 +0100

    Remove withoutPersistence from state/help (#38281)
    I've been touching `state/help` today with some bugfixes, and this is a drive-by
    patch that uses the opportunity to remove `withoutPersistence` from its reducers.
    
    There is no persistence in `state/help` at all, and it's the default state even without
    the `withoutPersistence` helper (`combineReducers` adds that automatically).
    
    There are not even any failing tests after the helper is removed.

--- client/state/help/reducer.js
+++ client/state/help/reducer.js
@@ -3,32 +3,32 @@
  */
 import { HELP_CONTACT_FORM_SITE_SELECT, HELP_LINKS_RECEIVE } from 'state/action-types';
 import courses from './courses/reducer';
-import { combineReducers, withoutPersistence } from 'state/utils';
+import { combineReducers } from 'state/utils';
 import directly from './directly/reducer';
 import ticket from './ticket/reducer';
 
 /**
  * Tracks the site id for the selected site in the help/contact form
  *
- * @param  {Object} state  Current state
- * @param  {Object} action Action payload
- * @return {Object}        Updated state
+ * @param  {object} state  Current state
+ * @param  {object} action Action payload
+ * @returns {object}        Updated state
  */
-export const selectedSiteId = withoutPersistence( ( state = null, action ) => {
+export const selectedSiteId = ( state = null, action ) => {
 	switch ( action.type ) {
 		case HELP_CONTACT_FORM_SITE_SELECT:
 			return action.siteId;
 	}
 
 	return state;
-} );
+};
 
 /**
  * Responsible for the help search results links
  *
- * @param  {Object} state  Current state
- * @param  {Object} action Action payload
- * @return {Object}        Updated state
+ * @param  {object} state  Current state
+ * @param  {object} action Action payload
+ * @returns {object}        Updated state
  */
 export const links = ( state = {}, action ) => {
 	switch ( action.type ) {
