290bc3954cd29ac5c77c038667ad81b6666fa2c3
Dave Smith <getdavemail@gmail.com>
2019-12-09 11:48:12 +0000

    Updates to use existing Menu items as Nav Block items

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/navigation-menu/transforms.js
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/blocks/navigation-menu/transforms.js
@@ -10,7 +10,7 @@ import { createBlock } from '@wordpress/blocks';
 /* eslint-enable import/no-extraneous-dependencies */
 
 const CORE_NAVIGATION_BLOCK_TYPE = 'core/navigation';
-
+const CORE_NAVIGATION_ITEM_BLOCK_TYPE = 'core/navigation-link';
 export const isValidCoreNavigationBlockType = type => CORE_NAVIGATION_BLOCK_TYPE === type;
 
 export default {
@@ -18,11 +18,31 @@ export default {
 		{
 			type: 'block',
 			blocks: [ CORE_NAVIGATION_BLOCK_TYPE ],
-			transform: attributes => {
-				return createBlock( CORE_NAVIGATION_BLOCK_TYPE, {
-					align: attributes.align,
-					itemsJustification: attributes.textAlign,
+			__experimentalConvert( { attributes, clientId } ) {
+				// Existing Menu items are stored as JSON alongside the ServerSideRendered
+				// block HTML markup.
+				const menuItems = JSON.parse(
+					document.querySelector( `#block-${ clientId } script` ).innerHTML
+				);
+
+				// Each Menu Items should become a Nav Link Block
+				const navItems = menuItems.map( item => {
+					return createBlock( CORE_NAVIGATION_ITEM_BLOCK_TYPE, {
+						label: item.post_title,
+						title: item.post_title,
+						url: item.url,
+					} );
 				} );
+
+				// Create Nav Block mapping attrs and Nav Items
+				return createBlock(
+					CORE_NAVIGATION_BLOCK_TYPE,
+					{
+						align: attributes.align,
+						itemsJustification: attributes.textAlign,
+					},
+					navItems
+				);
 			},
 		},
 	],
