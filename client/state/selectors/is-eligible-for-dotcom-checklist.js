a7cd4f647f9a275512b20d3e94b48711211aa8bd
Jeff Bowen <jblz@users.noreply.github.com>
2019-12-09 20:44:17 -0500

    Enable dotcom checklist for Atomic sites in the client ..even in ecommerce. Let the server decide if tasks are provided.

--- client/state/selectors/is-eligible-for-dotcom-checklist.js
+++ client/state/selectors/is-eligible-for-dotcom-checklist.js
@@ -16,22 +16,16 @@ import isAtomicSite from 'state/selectors/is-site-automated-transfer';
  * @returns {boolean} True if current user is able to see the checklist
  */
 export default function isEligibleForDotcomChecklist( state, siteId ) {
-	const siteOptions = getSiteOptions( state, siteId );
-	const isWpComStore = get( siteOptions, 'is_wpcom_store' );
-	const createdAt = get( siteOptions, 'created_at', '' );
-
-	// Checklist should not show up if the site is created before the feature was launched.
-	if (
-		! createdAt ||
-		createdAt.substr( 0, 4 ) === '0000' ||
-		new Date( createdAt ) < new Date( '2018-02-01' )
-	) {
-		return false;
-	}
-
 	if ( isJetpackSite( state, siteId ) && ! isAtomicSite( state, siteId ) ) {
 		return false;
 	}
 
-	return ! isWpComStore;
+	const siteOptions = getSiteOptions( state, siteId );
+
+	// Checklist should not show up if the site is created before the feature was launched.
+	if ( get( siteOptions, 'created_at', '' ) < '2018-02-01' ) {
+		return false;
+	}
+
+	return true;
 }
