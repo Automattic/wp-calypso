67fbc8e16ff1ff50c503530b40bb05ec1897095a
Piotr Delawski <delawski@users.noreply.github.com>
2019-12-11 16:31:39 +0100

    Revert "Add `ProductIcon` component to `@automattic/components` package (#38253)" (#38340)
    This reverts commit ec1a2a0723b12ab191244dd4ff6ffd0ad74beb3c.

--- packages/calypso-build/bin/copy-styles.js
+++ packages/calypso-build/bin/copy-styles.js
@@ -14,15 +14,7 @@ const outputDirCommon = path.join( dir, 'dist', 'cjs' );
 
 const copyOptions = {
 	overwrite: true,
-	filter: [
-		'**/*.gif',
-		'**/*.jpg',
-		'**/*.jpeg',
-		'**/*.png',
-		'**/*.svg',
-		'**/*.scss',
-		'!**/test/**',
-	],
+	filter: [ '**/*.scss', '!**/test/**' ],
 	concurrency: 127,
 };
 
