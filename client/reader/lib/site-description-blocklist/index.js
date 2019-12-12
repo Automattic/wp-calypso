592da71e643847c8a54b0d3e232fbecb5c0f0044
Chris R <chris.rosser@automattic.com>
2019-12-12 09:22:57 -0700

    Reader: rename blacklist to blocklist (#38363)
    * Reader: rename blacklist to blocklist
    * Use 'blocked' rather than 'blocklisted'

--- client/reader/lib/site-description-blocklist/index.js
+++ client/reader/lib/site-description-blocklist/index.js
@@ -1,4 +1,4 @@
-const siteDescriptionBlacklist = new Set( [
+const siteDescriptionBlocklist = new Set( [
 	'Just another WordPress.com site',
 	'Just another WordPress site',
 	"This WordPress.com site is the bee's knees",
@@ -8,11 +8,11 @@ const siteDescriptionBlacklist = new Set( [
 ] );
 
 /**
- * Is the provided site description name blacklisted?
+ * Is the provided site description name blocked?
  *
  * @param {string} siteDescription Site description
- * @returns {boolean} True if blacklisted
+ * @returns {boolean} True if blocked
  */
-export const isSiteDescriptionBlacklisted = siteDescription => {
-	return siteDescriptionBlacklist.has( siteDescription );
+export const isSiteDescriptionBlocked = siteDescription => {
+	return siteDescriptionBlocklist.has( siteDescription );
 };
