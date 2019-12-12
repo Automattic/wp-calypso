15202490b2ae232b546689b8e7a946dd94991785
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2019-12-12 14:41:54 +0200

    Activity Log: Consider Backup products when displaying intro banner (#38316)
    * State: Introduce siteHasBackupProductPurchase selector
    
    * State: Add tests for siteHasBackupProductPurchase
    
    * Activity Log: Update Intro banner to take Backup product into consideration
    
    * Activity Log: Use siteHasBackupProductPurchase selector
    
    * Fix eslint errors
    
    * Add a docblock

--- client/state/purchases/test/selectors.js
+++ client/state/purchases/test/selectors.js
@@ -10,6 +10,7 @@ import {
 	isFetchingSitePurchases,
 	isFetchingUserPurchases,
 	isUserPaid,
+	siteHasBackupProductPurchase,
 } from '../selectors';
 
 // Gets rid of warnings such as 'UnhandledPromiseRejectionWarning: Error: No available storage method found.'
@@ -190,6 +191,43 @@ describe( 'selectors', () => {
 			expect( result ).toHaveLength( 2 );
 			expect( result[ 0 ].siteId ).toBe( 1234 );
 			expect( result[ 1 ].siteId ).toBe( 1234 );
+		} );
+	} );
+
+	describe( 'siteHasBackupProductPurchase', () => {
+		test( 'should return true if a site has a Jetpack Backup purchase, false otherwise', () => {
+			const state = {
+				purchases: {
+					data: [
+						{
+							ID: '81414',
+							blog_id: '1234',
+							active: true,
+							product_slug: 'jetpack_personal',
+						},
+						{
+							ID: '82867',
+							blog_id: '1234',
+							active: true,
+							product_slug: 'something',
+						},
+						{
+							ID: '105103',
+							blog_id: '123',
+							active: true,
+							product_slug: 'jetpack_backup_daily',
+						},
+					],
+					error: null,
+					isFetchingSitePurchases: true,
+					isFetchingUserPurchases: false,
+					hasLoadedSitePurchasesFromServer: false,
+					hasLoadedUserPurchasesFromServer: false,
+				},
+			};
+
+			expect( siteHasBackupProductPurchase( state, 1234 ) ).toBe( false );
+			expect( siteHasBackupProductPurchase( state, 123 ) ).toBe( true );
 		} );
 	} );
 
