fabaf9cbee3124befd7834e4b726cdd27dabdb2f
Payton Swick <payton@foolord.com>
2019-12-12 19:56:45 -0500

    Add new products when requested in WPCheckoutWrapper

--- packages/composite-checkout-wpcom/src/components/wp-checkout-wrapper.js
+++ packages/composite-checkout-wpcom/src/components/wp-checkout-wrapper.js
@@ -4,6 +4,8 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { CheckoutProvider } from '@automattic/composite-checkout';
+import debugFactory from 'debug';
+import { useSelector } from 'react-redux';
 
 /**
  * Internal dependencies
@@ -11,6 +13,16 @@ import { CheckoutProvider } from '@automattic/composite-checkout';
 import WPCheckout from './wp-checkout';
 import { useWpcomStore } from '../hooks/wpcom-store';
 import { useShoppingCart } from '../hooks/use-shopping-cart';
+import {
+	conciergeSessionItem,
+	domainMapping,
+	planItem,
+	themeItem,
+	jetpackProductItem,
+} from 'lib/cart-values/cart-items';
+import { getPlanBySlug } from 'state/plans/selectors';
+
+const debug = debugFactory( 'composite-checkout-wpcom:wp-checkout-wrapper' );
 
 // These are used only for redirect payment methods
 const successRedirectUrl = window.location.href;
@@ -24,6 +36,8 @@ const handleCheckoutEvent = () => () => {
 // This is the parent component which would be included on a host page
 export function WPCheckoutWrapper( {
 	siteSlug,
+	planSlug,
+	isJetpackNotAtomic,
 	setCart,
 	getCart,
 	availablePaymentMethods,
@@ -32,23 +46,35 @@ export function WPCheckoutWrapper( {
 	onSuccess,
 	onFailure,
 } ) {
-	const { items, tax, total, removeItem, changePlanLength } = useShoppingCart(
+	const { items, tax, total, removeItem, addItem, changePlanLength } = useShoppingCart(
 		siteSlug,
 		setCart,
 		getCart
 	);
+	const plan = useSelector( state => getPlanBySlug( state, planSlug ) );
 
 	const { select, subscribe, registerStore } = registry;
 	useWpcomStore( registerStore );
 
+	// Subscribe to all events
 	useEffect( () => {
+		debug( 'subscribing to composite-checkout events' );
 		return subscribe( handleCheckoutEvent( select ) );
 	}, [ select, subscribe ] );
+
+	// Add product if requested in the URL
+	useEffect( () => {
+		debug( 'adding item as requested in url', { planSlug, plan, isJetpackNotAtomic } );
+		planSlug && plan && addItem( createItemToAddToCart( { planSlug, plan, isJetpackNotAtomic } ) );
+	}, [ planSlug, plan, isJetpackNotAtomic, addItem ] );
+
+	const itemsForCheckout = items.length ? [ ...items, tax ] : [];
+	debug( 'items for checkout', itemsForCheckout );
 
 	return (
 		<CheckoutProvider
 			locale={ 'en-us' }
-			items={ [ ...items, tax ] }
+			items={ itemsForCheckout }
 			total={ total }
 			onSuccess={ onSuccess }
 			onFailure={ onFailure }
@@ -75,4 +101,38 @@ WPCheckoutWrapper.propTypes = {
 	onSuccess: PropTypes.func.isRequired,
 	onFailure: PropTypes.func.isRequired,
 	siteId: PropTypes.oneOfType( [ PropTypes.string, PropTypes.number ] ),
+	planSlug: PropTypes.string,
+	isJetpackNotAtomic: PropTypes.bool,
 };
+
+function createItemToAddToCart( { planSlug, plan, isJetpackNotAtomic } ) {
+	let cartItem, cartMeta;
+
+	if ( planSlug ) {
+		cartItem = planItem( planSlug );
+		cartItem.product_id = plan.product_id;
+	}
+
+	if ( planSlug.startsWith( 'theme' ) ) {
+		cartMeta = planSlug.split( ':' )[ 1 ];
+		cartItem = themeItem( cartMeta );
+	}
+
+	if ( planSlug.startsWith( 'domain-mapping' ) ) {
+		cartMeta = planSlug.split( ':' )[ 1 ];
+		cartItem = domainMapping( { domain: cartMeta } );
+	}
+
+	if ( planSlug.startsWith( 'concierge-session' ) ) {
+		// TODO: prevent adding a conciergeSessionItem if one already exists
+		cartItem = conciergeSessionItem();
+	}
+
+	if ( planSlug.startsWith( 'jetpack_backup' ) && isJetpackNotAtomic ) {
+		cartItem = jetpackProductItem( planSlug );
+	}
+
+	cartItem.extra = { ...cartItem.extra, context: 'calypstore' };
+
+	return cartItem;
+}
