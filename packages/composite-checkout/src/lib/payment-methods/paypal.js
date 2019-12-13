440a149018e0f398846807f0b8419acc4fb27128
Payton Swick <payton@automattic.com>
2019-12-13 11:23:54 -0500

    Restore Button imports (#38399)
    This reverts the changes made to composite-checkout by #38308 which
    accidentally replaced the local Button component imports with the Button
    component from `@automattic/components`.

--- packages/composite-checkout/src/lib/payment-methods/paypal.js
+++ packages/composite-checkout/src/lib/payment-methods/paypal.js
@@ -7,7 +7,7 @@ import styled from '@emotion/styled';
 /**
  * Internal dependencies
  */
-import { Button } from '@automattic/components';
+import Button from '../../components/button';
 import { useLocalize } from '../../lib/localize';
 import { useDispatch, useSelect, usePaymentData } from '../../lib/registry';
 import { useCheckoutHandlers, useCheckoutRedirects, useLineItems } from '../../public-api';
