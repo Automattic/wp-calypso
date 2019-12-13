2c8f4474880ce37435c74cbd262bde39cef79ce7
Kerry Liu <gwwar@users.noreply.github.com>
2019-12-13 10:26:39 -0800

    Gutenberg: autofix block validation issues on editor load (#38397)

--- apps/wpcom-block-editor/src/common/index.js
+++ apps/wpcom-block-editor/src/common/index.js
@@ -2,6 +2,7 @@
  * Internal dependencies
  */
 import './disable-nux-tour';
+import './fix-block-invalidation-errors';
 import './reorder-block-categories';
 import './rich-text';
 import './style.scss';
