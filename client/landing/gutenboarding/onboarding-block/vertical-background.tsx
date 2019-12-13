e07d41e612c15e9e2d76b12fb51d57f109c4ff6c
Jon Surrell <jon.surrell@automattic.com>
2019-12-13 14:33:44 +0100

    Dynamically check for images via webpack
    Preload without 404s

--- client/landing/gutenboarding/onboarding-block/vertical-background.tsx
+++ client/landing/gutenboarding/onboarding-block/vertical-background.tsx
@@ -1,8 +1,13 @@
 /**
  * External dependencies
  */
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import { SwitchTransition, CSSTransition } from 'react-transition-group';
+
+/**
+ * Calypso dependencies
+ */
+import defaultImageUrl from '../../../../static/images/verticals/default.jpg';
 
 export interface VerticalBackgroundProps {
 	id: string;
@@ -10,37 +15,56 @@ export interface VerticalBackgroundProps {
 }
 
 const VerticalBackground: FunctionComponent< VerticalBackgroundProps > = ( { id, onLoad } ) => {
-	const [ imageId, setimageId ] = useState( '' );
+	const [ imageUrl, setImageUrl ] = useState< string | null >( null );
+
+	// Prefetch the default image.
+	useEffect( () => void ( new window.Image().src = defaultImageUrl ), [] );
+
+	useEffect( () => {
+		const preloadImage = new window.Image();
+		const failureHandler = () => {
+			setImageUrl( defaultImageUrl );
+			onLoad();
+		};
+		// We get an esmodule wrapping the url here
+		const successHandler = ( { default: url }: { default: string } ) => {
+			preloadImage;
+			preloadImage.onload = () => {
+				setImageUrl( url );
+				onLoad();
+			};
+			preloadImage.onerror = failureHandler;
+			preloadImage.src = url;
+		};
+		import(
+			/**
+			 * Set eager mode because these are just image _paths_.
+			 */
+			/* webpackMode: "eager" */
+			/* webpackInclude: /\.jpg$/ */
+			`../../../../static/images/verticals/${ id }.jpg`
+		).then( successHandler, failureHandler );
+		return () => {
+			preloadImage.onload = null;
+			preloadImage.onerror = null;
+		};
+	}, [ id, onLoad ] );
 
 	return (
-		<>
-			<img
-				style={ { display: 'none' } }
-				src={ `/calypso/images/verticals/${ id }.jpg` }
-				onLoad={ () => {
-					setimageId( id );
-					onLoad();
-				} }
-				onError={ () => setimageId( 'default' ) }
-				alt=""
-			/>
-			<SwitchTransition mode="in-out">
-				<CSSTransition
-					key={ imageId }
-					timeout={ 1000 }
-					classNames="onboarding-block__background-fade"
-				>
-					<div
-						className="onboarding-block__background"
-						style={ {
-							backgroundImage: imageId
-								? `url('/calypso/images/verticals/${ imageId }.jpg')`
-								: 'none',
-						} }
-					/>
-				</CSSTransition>
-			</SwitchTransition>
-		</>
+		<SwitchTransition mode="in-out">
+			<CSSTransition
+				key={ imageUrl || 'empty' }
+				timeout={ 1000 }
+				classNames="onboarding-block__background-fade"
+			>
+				<div
+					className="onboarding-block__background"
+					style={ {
+						backgroundImage: imageUrl ? `url(${ imageUrl })` : 'none',
+					} }
+				/>
+			</CSSTransition>
+		</SwitchTransition>
 	);
 };
 export default VerticalBackground;
