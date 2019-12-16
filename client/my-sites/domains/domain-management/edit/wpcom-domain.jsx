a424f620954d859909fd2dad0dade763f5e9c486
Caroline Moore <calobee@gmail.com>
2019-12-16 10:14:02 -0500

    Domains: Move Change Site Address tool to its own page (#37566)

--- client/my-sites/domains/domain-management/edit/wpcom-domain.jsx
+++ client/my-sites/domains/domain-management/edit/wpcom-domain.jsx
@@ -14,9 +14,10 @@ import Header from './card/header';
 import Property from './card/property';
 import VerticalNav from 'components/vertical-nav';
 import VerticalNavItem from 'components/vertical-nav/item';
-import SiteAddressChanger from 'blocks/site-address-changer';
+import { domainManagementChangeSiteAddress } from 'my-sites/domains/paths';
 import { getDomainTypeText } from 'lib/domains';
 import { type as domainTypes } from 'lib/domains/constants';
+
 import { recordTracksEvent, recordGoogleEvent } from 'state/analytics/actions';
 
 class WpcomDomain extends React.Component {
@@ -37,6 +38,23 @@ class WpcomDomain extends React.Component {
 		} );
 	};
 
+	handleChangeSiteAddressClick = () => {
+		const { domain } = this.props;
+		const domainType = getDomainTypeText( domain );
+
+		this.props.recordGoogleEvent(
+			'Domain Management',
+			`Clicked "Change Site Address" navigation link on a ${ domainType } in Edit`,
+			'Domain Name',
+			domain.name
+		);
+
+		this.props.recordTracksEvent( 'calypso_domain_management_change_navigation_click', {
+			action: 'change_site_address',
+			section: domain.type,
+		} );
+	};
+
 	getEditSiteAddressBlock() {
 		const { domain } = this.props;
 
@@ -44,20 +62,25 @@ class WpcomDomain extends React.Component {
 			return;
 		}
 
-		if ( get( domain, 'type' ) === domainTypes.WPCOM ) {
-			const dotblogSubdomain = get( domain, 'name', '' ).match( /\.\w+\.blog$/ );
-			const domainSuffix = dotblogSubdomain ? dotblogSubdomain[ 0 ] : '.wordpress.com';
-			return <SiteAddressChanger currentDomain={ domain } currentDomainSuffix={ domainSuffix } />;
-		}
+		const isWpcomDomain = get( domain, 'type' ) === domainTypes.WPCOM;
+		const path = domainManagementChangeSiteAddress( this.props.selectedSite.slug, domain.name );
 
 		return (
 			<VerticalNav>
 				<VerticalNavItem
-					path={ `https://${ this.props.domain.name }/wp-admin/index.php?page=my-blogs#blog_row_${ this.props.selectedSite.ID }` }
-					external={ true }
-					onClick={ this.handleEditSiteAddressClick }
+					path={
+						isWpcomDomain
+							? path
+							: `https://${ this.props.domain.name }/wp-admin/index.php?page=my-blogs#blog_row_${ this.props.selectedSite.ID }`
+					}
+					external={ isWpcomDomain ? false : true }
+					onClick={
+						isWpcomDomain ? this.handleChangeSiteAddressClick : this.handleEditSiteAddressClick
+					}
 				>
-					{ this.props.translate( 'Edit Site Address' ) }
+					{ isWpcomDomain
+						? this.props.translate( 'Change Site Address' )
+						: this.props.translate( 'Edit Site Address' ) }
 				</VerticalNavItem>
 			</VerticalNav>
 		);
