ddb10ca2a5b530e69a76ce7c8a30c4452bb2a157
Andrés <andres.maneiro@automattic.com>
2019-12-17 14:18:22 +0100

    Add RangeControl for line-height

--- apps/full-site-editing/full-site-editing-plugin/global-styles/src/font-selection-panel.js
+++ apps/full-site-editing/full-site-editing-plugin/global-styles/src/font-selection-panel.js
@@ -6,7 +6,7 @@ import { __ } from '@wordpress/i18n';
 /**
  * WordPress dependencies.
  */
-import { SelectControl } from '@wordpress/components';
+import { RangeControl, SelectControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -16,12 +16,16 @@ import NoSupport from './no-support';
 export default ( {
 	fontBase,
 	fontBaseDefault,
+	fontBaseOptions,
 	fontHeadings,
 	fontHeadingsDefault,
-	fontBaseOptions,
 	fontHeadingsOptions,
 	updateBaseFont,
 	updateHeadingsFont,
+	lineHeightBase,
+	lineHeightHeadings,
+	updateBaseLineHeight,
+	updateHeadingsLineHeight,
 } ) => {
 	if ( ! fontBaseOptions || ! fontHeadingsOptions ) {
 		return <NoSupport unsupportedFeature={ __( 'custom font selection' ) } />;
@@ -36,12 +40,28 @@ export default ( {
 				onChange={ newValue => updateHeadingsFont( newValue ) }
 				style={ { fontFamily: fontHeadings !== 'unset' ? fontHeadings : fontHeadingsDefault } }
 			/>
+			<RangeControl
+				label={ __( 'Heading line height' ) }
+				value={ lineHeightBase }
+				onChange={ newValue => updateBaseLineHeight( newValue ) }
+				allowReset={ true }
+				min={ 1.2 }
+				max={ 2 }
+			/>
 			<SelectControl
 				label={ __( 'Base Font' ) }
 				value={ fontBase }
 				options={ fontBaseOptions }
 				onChange={ newValue => updateBaseFont( newValue ) }
 				style={ { fontFamily: fontBase !== 'unset' ? fontBase : fontBaseDefault } }
+			/>
+			<RangeControl
+				label={ __( 'Base line height' ) }
+				value={ lineHeightHeadings }
+				onChange={ newValue => updateHeadingsLineHeight( newValue ) }
+				allowReset={ true }
+				min={ 1.2 }
+				max={ 2 }
 			/>
 			<hr />
 		</>
