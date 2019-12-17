bf82cc46588288ae8a7ee79d2e5628f03b73d0a9
Bernie Reiter <ockham@raz.or.at>
2019-12-17 16:24:32 +0600

    Gutenboarding Design Selector: Add heading, srcset (#38465)

--- client/landing/gutenboarding/onboarding-block/design-selector/index.tsx
+++ client/landing/gutenboarding/onboarding-block/design-selector/index.tsx
@@ -1,6 +1,8 @@
 /**
  * External dependencies
  */
+import { __ as NO__ } from '@wordpress/i18n';
+import { addQueryArgs } from '@wordpress/url';
 import { useSelect } from '@wordpress/data';
 import React, { useState } from 'react';
 import classnames from 'classnames';
@@ -13,6 +15,11 @@ import { CardMedia } from '../../components/card/media';
 import { SiteVertical } from '../../stores/onboard/types';
 
 import './style.scss';
+
+const gridWidth = 960;
+
+const srcSet = ( src: string, widths: number[] ) =>
+	widths.map( width => addQueryArgs( src, { w: width } ) + ` ${ width }w` ).join( ', ' );
 
 export default () => {
 	const siteVertical = useSelect(
@@ -29,20 +36,32 @@ export default () => {
 	const [ selectedDesign, setSelectedDesign ] = useState< string | undefined >();
 	return (
 		<div className="design-selector">
-			{ homepageTemplates.map( template => (
-				<Card
-					className={ classnames( 'design-selector__design-option', {
-						'is-selected': template.slug === selectedDesign,
-					} ) }
-					key={ template.slug }
-					isElevated
-					onClick={ () => setSelectedDesign( template.slug ) }
-				>
-					<CardMedia>
-						<img alt={ template.title } src={ template.preview } />
-					</CardMedia>
-				</Card>
-			) ) }
+			<h1 className="design-selector__title">
+				{ NO__( 'Choose a starting design for your site' ) }
+			</h1>
+			<h2 className="design-selector__subtitle">
+				{ NO__( "You'll be able to customize your new site in hundreds of ways." ) }
+			</h2>
+			<div className="design-selector__grid">
+				{ homepageTemplates.map( template => (
+					<Card
+						className={ classnames( 'design-selector__design-option', {
+							'is-selected': template.slug === selectedDesign,
+						} ) }
+						key={ template.slug }
+						isElevated
+						onClick={ () => setSelectedDesign( template.slug ) }
+					>
+						<CardMedia>
+							<img
+								alt={ template.title }
+								src={ template.preview }
+								srcSet={ srcSet( template.preview, [ gridWidth / 2, gridWidth / 4 ] ) }
+							/>
+						</CardMedia>
+					</Card>
+				) ) }
+			</div>
 		</div>
 	);
 };
