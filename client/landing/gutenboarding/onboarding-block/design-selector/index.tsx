d0f3f1d977e4ec465bcaa72affb32a96656d5114
Bernie Reiter <ockham@raz.or.at>
2019-12-17 13:30:28 +0600

    Gutenboarding: Add headings to DesignSelector

--- client/landing/gutenboarding/onboarding-block/design-selector/index.tsx
+++ client/landing/gutenboarding/onboarding-block/design-selector/index.tsx
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { __ as NO__ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 import React, { useState } from 'react';
 import classnames from 'classnames';
@@ -29,20 +30,24 @@ export default () => {
 	const [ selectedDesign, setSelectedDesign ] = useState< string | undefined >();
 	return (
 		<div className="design-selector">
-			{ homepageTemplates.map( template => (
-				<Card
-					className={ classnames( 'design-selector__design-option', {
-						'is-selected': template.slug === selectedDesign,
-					} ) }
-					key={ template.slug }
-					isElevated
-					onClick={ () => setSelectedDesign( template.slug ) }
-				>
-					<CardMedia>
-						<img alt={ template.title } src={ template.preview } />
-					</CardMedia>
-				</Card>
-			) ) }
+			<h1>{ NO__( 'Choose a starting design for your site' ) }</h1>
+			<h2>{ NO__( "You'll be able to customize your new site in hundreds of ways." ) }</h2>
+			<div className="design-selector__grid">
+				{ homepageTemplates.map( template => (
+					<Card
+						className={ classnames( 'design-selector__design-option', {
+							'is-selected': template.slug === selectedDesign,
+						} ) }
+						key={ template.slug }
+						isElevated
+						onClick={ () => setSelectedDesign( template.slug ) }
+					>
+						<CardMedia>
+							<img alt={ template.title } src={ template.preview } />
+						</CardMedia>
+					</Card>
+				) ) }
+			</div>
 		</div>
 	);
 };
