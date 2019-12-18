fdc5e35a4c8cdf7eea58af7bedb935657ce11c31
Bernie Reiter <ockham@raz.or.at>
2019-12-18 19:04:07 +0600

    Add index.ts

---
+++ packages/calypso-data-stores/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Internal dependencies
+ */
+export { STORE_KEY as DOMAIN_SUGGESTIONS_STORE } from './domain-suggestions';
+export { STORE_KEY as VERTICALS_STORE } from './verticals';
+export { STORE_KEY as VERTICALS_TEMPLATES_STORE } from './verticals-templates';
