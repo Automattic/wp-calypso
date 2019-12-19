87f7db70b1c5a78312fd9f3191dcafa6ffd38d42
Adam Zieliński <adam@adamziel.com>
2019-12-19 15:49:28 +0100

    Remove a comma from warnings title

--- client/blocks/eligibility-warnings/hold-list.tsx
+++ client/blocks/eligibility-warnings/hold-list.tsx
@@ -197,11 +197,11 @@ export const HoldList = ( { context, holds, isPlaceholder, translate }: Props ) 
 function getCardHeading( context: string | null, translate: LocalizeProps[ 'translate' ] ) {
 	switch ( context ) {
 		case 'plugins':
-			return translate( "To install plugins, you'll need to:" );
+			return translate( "To install plugins you'll need to:" );
 		case 'themes':
-			return translate( "To install themes, you'll need to:" );
+			return translate( "To install themes you'll need to:" );
 		default:
-			return translate( "To continue, you'll need to:" );
+			return translate( "To continue you'll need to" + ':' );
 	}
 }
 
