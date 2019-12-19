a2bca282737d7e0cd6ecece089dec63f18f5416e
Payton Swick <payton@foolord.com>
2019-12-18 20:07:20 -0500

    Only jump initial step number if previous complete

--- packages/composite-checkout/src/components/checkout.js
+++ packages/composite-checkout/src/components/checkout.js
@@ -15,7 +15,6 @@ import CheckoutStep from './checkout-step';
 import CheckoutNextStepButton from './checkout-next-step-button';
 import CheckoutSubmitButton from './checkout-submit-button';
 import LoadingContent from './loading-content';
-import ProvisioningContent from './provisioning-content';
 import {
 	usePrimarySelect,
 	usePrimaryDispatch,
@@ -34,13 +33,14 @@ import {
 import { validateSteps } from '../lib/validation';
 import { useEvents, usePaymentComplete } from './checkout-provider';
 import { useFormStatus } from '../lib/form-status';
+import useConstructor from '../lib/use-constructor';
 
 const debug = debugFactory( 'composite-checkout:checkout' );
 
 function useRegisterCheckoutStore() {
 	const onEvent = useEvents();
 	useRegisterPrimaryStore( {
-		reducer( state = { stepNumber: getStepNumberFromUrl(), paymentData: {} }, action ) {
+		reducer( state = { stepNumber: 1, paymentData: {} }, action ) {
 			switch ( action.type ) {
 				case 'STEP_NUMBER_SET':
 					return { ...state, stepNumber: action.payload };
@@ -96,9 +96,6 @@ export default function Checkout( { steps, className } ) {
 	steps = steps || makeDefaultSteps( localize );
 	validateSteps( steps );
 
-	// Change the step if the url changes
-	useChangeStepNumberForUrl();
-
 	// Assign step numbers to all steps with numbers
 	let numberedStepNumber = 0;
 	let annotatedSteps = steps.map( ( step, index ) => {
@@ -127,6 +124,9 @@ export default function Checkout( { steps, className } ) {
 				step.isCompleteCallback( { paymentData, activeStep, activePaymentMethod } ),
 		};
 	} );
+
+	// Change the step if the url changes
+	useChangeStepNumberForUrl( annotatedSteps );
 
 	const nextStep = annotatedSteps.find( ( step, index ) => {
 		return index > activeStep.stepIndex && step.hasStepNumber;
@@ -331,7 +331,6 @@ function getStepNumberFromUrl() {
 	if ( hashValue?.startsWith?.( '#step' ) ) {
 		const parts = hashValue.split( '#step' );
 		const stepNumber = parts.length > 1 ? parts[ 1 ] : 1;
-		debug( 'found step number in url', stepNumber );
 		return parseInt( stepNumber, 10 );
 	}
 	return 1;
@@ -352,11 +351,32 @@ function saveStepNumberToUrl( stepNumber ) {
 	window.history.pushState( null, null, newUrl );
 }
 
-function useChangeStepNumberForUrl() {
+function areAllPreviousStepsComplete( steps, stepNumber ) {
+	return steps.reduce( ( allComplete, step ) => {
+		if ( step.stepNumber && step.stepNumber < stepNumber ) {
+			if ( allComplete === false ) {
+				return false;
+			}
+			return step.isComplete;
+		}
+		return allComplete;
+	}, false );
+}
+
+function useChangeStepNumberForUrl( steps ) {
 	const { changeStep } = usePrimaryDispatch();
+	useConstructor( () => {
+		const newStepNumber = getStepNumberFromUrl();
+		if ( areAllPreviousStepsComplete( steps, newStepNumber ) ) {
+			debug( 'changing initial step to', newStepNumber );
+			changeStep( newStepNumber );
+			return;
+		}
+		changeStep( 1 );
+	} );
 	useEffect( () => {
 		let isSubscribed = true;
-		window.addEventListener?.( 'hashchange', function() {
+		window.addEventListener?.( 'hashchange', () => {
 			const newStepNumber = getStepNumberFromUrl();
 			debug( 'step number in url changed to', newStepNumber );
 			isSubscribed && changeStep( newStepNumber );
