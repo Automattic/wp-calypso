f3379591a0b3ce8dab1e888934bb5b25061b9ae0
Piotr Delawski <piotr.delawski@automattic.com>
2019-12-19 16:16:36 +0100

    Support product and plan slugs directly in the component without facade

--- packages/components/src/product-icon/config.js
+++ packages/components/src/product-icon/config.js
@@ -13,7 +13,7 @@ import wpcomFree from './images/wpcom-free.svg';
 import wpcomPersonal from './images/wpcom-personal.svg';
 import wpcomPremium from './images/wpcom-premium.svg';
 
-export default {
+export const paths = {
 	'jetpack-backup-daily': jetpackBackup,
 	'jetpack-backup-realtime': jetpackBackup,
 	'jetpack-free': jetpackFree,
@@ -27,3 +27,20 @@ export default {
 	'wpcom-personal': wpcomPersonal,
 	'wpcom-premium': wpcomPremium,
 };
+
+export const iconToProductSlugMap = {
+	'wpcom-free': [ 'free_plan' ],
+	'wpcom-blogger': [ 'blogger-bundle', 'blogger-bundle-2y' ],
+	'wpcom-personal': [ 'personal-bundle', 'personal-bundle-2y', 'personal-bundle-monthly' ],
+	'wpcom-premium': [ 'value_bundle', 'value_bundle-2y', 'value_bundle-monthly' ],
+	'wpcom-ecommerce': [ 'ecommerce-bundle', 'ecommerce-bundle-2y', 'ecommerce-bundle-monthly' ],
+	'wpcom-business': [ 'business-bundle', 'business-bundle-2y', 'business-bundle-monthly' ],
+	'jetpack-free': [ 'jetpack_free' ],
+	'jetpack-personal': [ 'jetpack_personal', 'jetpack_personal_monthly' ],
+	'jetpack-premium': [ 'jetpack_premium', 'jetpack_premium_monthly' ],
+	'jetpack-professional': [ 'jetpack_business', 'jetpack_business_monthly' ],
+	'jetpack-backup-daily': [ 'jetpack_backup_daily', 'jetpack_backup_daily_monthly' ],
+	'jetpack-backup-realtime': [ 'jetpack_backup_realtime', 'jetpack_backup_realtime_monthly' ],
+};
+
+export const supportedSlugs = [ ...Object.values( iconToProductSlugMap ) ].flat();
