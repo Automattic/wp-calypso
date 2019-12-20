ddf29e091846472b4dc554b0da35ef28baf28951
Bernie Reiter <ockham@raz.or.at>
2019-12-20 14:05:09 +0600

    calypso-data-stores: Export types

--- packages/calypso-data-stores/src/index.ts
+++ packages/calypso-data-stores/src/index.ts
@@ -4,3 +4,10 @@
 export { STORE_KEY as DOMAIN_SUGGESTIONS_STORE } from './domain-suggestions';
 export { STORE_KEY as VERTICALS_STORE } from './verticals';
 export { STORE_KEY as VERTICALS_TEMPLATES_STORE } from './verticals-templates';
+
+/**
+ * Types
+ */
+export * from './domain-suggestions/types';
+export * from './verticals/types';
+export * from './verticals-templates/types';
