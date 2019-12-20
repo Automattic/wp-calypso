aa0149a277b7777a1cd1ea8c8d82a62bef67763d
Bernie Reiter <ockham@raz.or.at>
2019-12-20 14:08:16 +0600

    Directly import helper types

--- packages/calypso-data-stores/src/index.ts
+++ packages/calypso-data-stores/src/index.ts
@@ -11,3 +11,8 @@ export { STORE_KEY as VERTICALS_TEMPLATES_STORE } from './verticals-templates';
 export * from './domain-suggestions/types';
 export * from './verticals/types';
 export * from './verticals-templates/types';
+
+/**
+ * Helper types
+ */
+export { DispatchFromMap, SelectFromMap } from './mapped-types';
