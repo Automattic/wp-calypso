df97abd2f52cdbb64472beb83bbe5fe4037c55ba
Jon Surrell <jon.surrell@automattic.com>
2019-12-24 14:19:30 +0100

    Gutenboarding: persist vertical background (#38577)
    * Move siteVertical select into VerticalBackground component
    * Colocate background styles
    * Fix background update when deselecting vertical

--- client/landing/gutenboarding/onboarding-block/vertical-background.tsx
+++ client/landing/gutenboarding/onboarding-block/vertical-background.tsx
@@ -3,6 +3,13 @@
  */
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import { useSelect } from '@wordpress/data';
+import classnames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import { STORE_KEY } from '../stores/onboard';
 
 /**
  * Calypso dependencies
@@ -14,53 +21,56 @@ export interface VerticalBackgroundProps {
 	onLoad: () => void;
 }
 
-const VerticalBackground: FunctionComponent< VerticalBackgroundProps > = ( { id, onLoad } ) => {
-	const [ imageUrl, setImageUrl ] = useState< string | null >( null );
+const VerticalBackground: FunctionComponent< VerticalBackgroundProps > = ( { onLoad } ) => {
+	const { siteVertical } = useSelect( select => select( STORE_KEY ).getState() );
+	const [ imageUrl, setImageUrl ] = useState< string | undefined >();
 
 	// Prefetch the default image.
 	useEffect( () => void ( new window.Image().src = defaultImageUrl ), [] );
 
 	useEffect( () => {
-		const preloadImage = new window.Image();
-		const failureHandler = () => {
-			setImageUrl( defaultImageUrl );
-			onLoad();
-		};
-		// We get an esmodule wrapping the url here
-		const successHandler = ( { default: url }: { default: string } ) => {
-			preloadImage;
-			preloadImage.onload = () => {
-				setImageUrl( url );
+		if ( siteVertical ) {
+			const preloadImage = new window.Image();
+			const failureHandler = () => {
+				setImageUrl( defaultImageUrl );
 				onLoad();
 			};
-			preloadImage.onerror = failureHandler;
-			preloadImage.src = url;
-		};
-		import(
-			/**
-			 * Set eager mode because these are just image _paths_.
-			 */
-			/* webpackMode: "eager" */
-			/* webpackInclude: /\.jpg$/ */
-			`../../../../static/images/verticals/${ id }.jpg`
-		).then( successHandler, failureHandler );
-		return () => {
-			preloadImage.onload = null;
-			preloadImage.onerror = null;
-		};
-	}, [ id, onLoad ] );
+			// We get an esmodule wrapping the url here
+			const successHandler = ( { default: url }: { default: string } ) => {
+				preloadImage.onload = () => {
+					setImageUrl( url );
+					onLoad();
+				};
+				preloadImage.onerror = failureHandler;
+				preloadImage.src = url;
+			};
+			import(
+				/**
+				 * Set eager mode because these are just image _paths_.
+				 */
+				/* webpackMode: "eager" */
+				/* webpackInclude: /\.jpg$/ */
+				`../../../../static/images/verticals/${ siteVertical.id }.jpg`
+			).then( successHandler, failureHandler );
+			return () => {
+				preloadImage.onload = null;
+				preloadImage.onerror = null;
+			};
+		}
+		setImageUrl( undefined );
+	}, [ siteVertical, onLoad ] );
 
 	return (
 		<SwitchTransition mode="in-out">
 			<CSSTransition
-				key={ imageUrl || 'empty' }
+				key={ imageUrl || 'none' }
 				timeout={ 750 }
 				classNames="onboarding-block__background-fade"
 			>
 				<div
-					className="onboarding-block__background"
+					className={ classnames( 'onboarding-block__background', { 'has-background': imageUrl } ) }
 					style={ {
-						backgroundImage: imageUrl ? `url(${ imageUrl })` : 'none',
+						backgroundImage: imageUrl ? `url( ${ imageUrl } ) ` : 'none',
 					} }
 				/>
 			</CSSTransition>
