04b406ff2871faa89754da14aab641a9120867a0
Marin Atanasov <tyxla@abv.bg>
2019-12-30 01:10:55 +0200

    State: Import embeds data layer with embed action creators

--- client/state/embeds/actions.js
+++ client/state/embeds/actions.js
@@ -2,6 +2,7 @@
  * Internal dependencies
  */
 import { EMBED_RECEIVE, EMBED_REQUEST, EMBEDS_RECEIVE, EMBEDS_REQUEST } from 'state/action-types';
+import 'state/data-layer/wpcom/sites/embeds';
 
 /**
  * Returns an action object used in signalling that an embed item for the site
