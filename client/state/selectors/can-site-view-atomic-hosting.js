77daa306682bd11b4b28f251b775ddca4e21a861
mmtr <miguel.torres@automattic.com>
2020-01-02 13:19:24 +0100

    Hosting: Check new manage_hosting capability for access

--- client/state/selectors/can-site-view-atomic-hosting.js
+++ client/state/selectors/can-site-view-atomic-hosting.js
@@ -1,43 +1,22 @@
 /**
  * Internal Dependencies
  */
-import { isEnabled } from 'config';
 import { getSelectedSiteId } from 'state/ui/selectors';
-import { isJetpackSite } from 'state/sites/selectors';
-import isVipSite from 'state/selectors/is-vip-site';
 import getRawSite from 'state/selectors/get-raw-site';
-import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
 import canCurrentUser from 'state/selectors/can-current-user';
 
 /**
- * TODO: this selector should be backed by an API response instead
  * Returns true if hosting section should be viewable
  *
  * @param  {object}  state  Global state tree
  * @returns {?boolean}        Whether site can display the atomic hosting section
  */
 export default function canSiteViewAtomicHosting( state ) {
-	if ( ! isEnabled( 'hosting' ) ) {
-		return false;
-	}
-
 	const siteId = getSelectedSiteId( state );
 
 	if ( ! getRawSite( state, siteId ) ) {
 		return false;
 	}
 
-	if ( ! canCurrentUser( state, siteId, 'manage_options' ) ) {
-		return false;
-	}
-
-	if ( isVipSite( state, siteId ) ) {
-		return false;
-	}
-
-	if ( isJetpackSite( state, siteId ) && ! isSiteAutomatedTransfer( state, siteId ) ) {
-		return false;
-	}
-
-	return true;
+	return canCurrentUser( state, siteId, 'manage_hosting' );
 }
