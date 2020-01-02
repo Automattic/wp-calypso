356f159dd54ae79ae9ec322e334dc7cecbf51db6
James Newell <james.newell@automattic.com>
2020-01-02 12:12:12 +1100

    added typescript support for storybook

--- packages/components/.storybook/presets.js
+++ packages/components/.storybook/presets.js
@@ -1,1 +1,19 @@
-module.exports = [ '@storybook/preset-scss' ];
+const path = require( 'path' );
+
+module.exports = [
+	'@storybook/preset-scss',
+	{
+		name: '@storybook/preset-typescript',
+		options: {
+			tsLoaderOptions: {
+				transpileOnly: true,
+				configFile: path.resolve( __dirname, '../tsconfig.json' ),
+			},
+			tsDocgenLoaderOptions: {
+				tsconfigPath: path.resolve( __dirname, '../tsconfig.json' ),
+			},
+			forkTsCheckerWebpackPluginOptions: {},
+			include: [ path.resolve( __dirname, '../src' ) ],
+		},
+	},
+];
