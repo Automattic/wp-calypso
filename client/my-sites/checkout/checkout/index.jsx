091e06a64812f77b16f6525bf53e1b9774b893dd
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-01-07 11:49:14 +0530

    Add the plan bump test

--- client/my-sites/checkout/checkout/index.jsx
+++ client/my-sites/checkout/checkout/index.jsx
@@ -96,6 +96,7 @@ import {
 } from 'signup/utils';
 import { isExternal } from 'lib/url';
 import { withLocalizedMoment } from 'components/localized-moment';
+import { abtest } from 'lib/abtest';
 
 /**
  * Style dependencies
@@ -451,6 +452,18 @@ export class Checkout extends React.Component {
 		}
 	}
 
+	maybeRedirectToPlanBumpOffer( pendingOrReceiptId ) {
+		const { cart, selectedSiteSlug } = this.props;
+
+		if ( hasPersonalPlan( cart ) ) {
+			if ( 'variantShowPlanBumpOffer' === abtest( 'showPlanBumpVsGsuite' ) ) {
+				return `/checkout/${ selectedSiteSlug }/offer-plan-upgrade/premium/${ pendingOrReceiptId }`;
+			}
+		}
+
+		return null;
+	}
+
 	maybeRedirectToGSuiteNudge( pendingOrReceiptId, stepResult ) {
 		const { isNewlyCreatedSite, selectedSiteSlug, cart } = this.props;
 
@@ -465,7 +478,10 @@ export class Checkout extends React.Component {
 			) {
 				const domainsForGSuite = this.getEligibleDomainFromCart();
 				if ( domainsForGSuite.length ) {
-					return `/checkout/${ selectedSiteSlug }/with-gsuite/${ domainsForGSuite[ 0 ].meta }/${ pendingOrReceiptId }`;
+					return (
+						this.maybeRedirectToPlanBumpOffer( pendingOrReceiptId ) ||
+						`/checkout/${ selectedSiteSlug }/with-gsuite/${ domainsForGSuite[ 0 ].meta }/${ pendingOrReceiptId }`
+					);
 				}
 			}
 		}
