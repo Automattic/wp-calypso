aa872f1c10467ebf163acdea4ed5182ba48b0404
Jeff Bowen <jblz@users.noreply.github.com>
2020-01-07 13:11:44 -0500

    eCommerce Upgrade: Set site public on dialog confirmation

--- client/my-sites/plan-features/index.jsx
+++ client/my-sites/plan-features/index.jsx
@@ -28,6 +28,7 @@ import { getPlan, getPlanBySlug, getPlanRawPrice, getPlanSlug } from 'state/plan
 import { getSignupDependencyStore } from 'state/signup/dependency-store/selectors';
 import { planItem as getCartItemForPlan } from 'lib/cart-values/cart-items';
 import { recordTracksEvent } from 'state/analytics/actions';
+import { saveSiteSettings } from 'state/site-settings/actions';
 import { retargetViewPlans } from 'lib/analytics/ad-tracking';
 import canUpgradeToPlan from 'state/selectors/can-upgrade-to-plan';
 import { getDiscountByName } from 'lib/discounts';
@@ -87,6 +88,34 @@ export class PlanFeatures extends Component {
 
 	setDefaultState = () => {
 		this.setState( defaultState );
+	};
+
+	onLaunchDialogClose = async action => {
+		const { currentSitePlanSlug, siteId } = this.props;
+		const { checkoutUrl, choosingPlanSlug } = this.state;
+
+		if ( action !== 'continue' ) {
+			this.setDefaultState();
+			return;
+		}
+
+		this.props.recordTracksEvent( 'calypso_plan_upgrade_launch_dialog_confirmed', {
+			current_plan: currentSitePlanSlug,
+			upgrading_to: choosingPlanSlug,
+		} );
+
+		const setPublicResult = await this.props.saveSiteSettings( siteId, {
+			blog_public: 1,
+		} );
+
+		if ( ! get( setPublicResult, [ 'updated', 'blog_public' ] ) ) {
+			this.props.recordTracksEvent( 'calypso_plan_upgrade_launch_dialog_failed', {
+				current_plan: currentSitePlanSlug,
+				upgrading_to: choosingPlanSlug,
+			} );
+		}
+
+		page( checkoutUrl );
 	};
 
 	UNSAFE_componentWillReceiveProps( { siteId } ) {
@@ -192,7 +221,7 @@ export class PlanFeatures extends Component {
 
 	renderSiteLaunchDialog() {
 		const { currentSitePlanSlug, translate } = this.props;
-		const { checkoutUrl, choosingPlanSlug, showingSiteLaunchDialog } = this.state;
+		const { choosingPlanSlug, showingSiteLaunchDialog } = this.state;
 
 		if ( ! showingSiteLaunchDialog ) {
 			return null;
@@ -213,16 +242,9 @@ export class PlanFeatures extends Component {
 						action: 'continue',
 						label: translate( "Let's do it!" ),
 						isPrimary: true,
-						onClick: () => {
-							this.props.recordTracksEvent( 'calypso_plan_upgrade_launch_dialog_confirmed', {
-								current_plan: currentSitePlanSlug,
-								upgrading_to: choosingPlanSlug,
-							} );
-							page( checkoutUrl );
-						},
 					},
 				] }
-				onClose={ this.setDefaultState }
+				onClose={ this.onLaunchDialogClose }
 			>
 				<h1>{ translate( 'Site Privacy' ) }</h1>
 				<p>{ translate( 'Your site is only visible to you and users you approve.' ) }</p>
@@ -1005,6 +1027,7 @@ export default connect(
 	},
 	{
 		recordTracksEvent,
+		saveSiteSettings,
 	}
 )( localize( PlanFeatures ) );
 
