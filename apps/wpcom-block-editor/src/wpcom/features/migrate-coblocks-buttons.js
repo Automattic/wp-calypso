8f49ccddacaa496beb883b7e0d1fc653b52a21c0
mmtr <miguel.torres@automattic.com>
2020-01-08 16:32:38 +0100

    WP.com block editor: Migrate CoBlocks buttons block

---
+++ apps/wpcom-block-editor/src/wpcom/features/migrate-coblocks-buttons.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+/**
+ * External dependencies
+ */
+import { addFilter } from '@wordpress/hooks';
+
+function deprecateCoBlocksButtonsSettings( settings, name ) {
+	if ( name !== 'coblocks/buttons' ) {
+		return settings;
+	}
+
+	return {
+		...settings,
+		supports: {
+			...settings.supports,
+			inserter: false,
+		},
+	};
+}
+
+addFilter( 'blocks.registerBlockType', deprecateCoBlocksButtonsSettings );
