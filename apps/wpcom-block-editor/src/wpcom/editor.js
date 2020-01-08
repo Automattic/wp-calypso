3d4a7dc7eb52ddf835701fc5f86421848010dd94
mmtr <miguel.torres@automattic.com>
2020-01-08 16:45:19 +0100

    Import file

--- apps/wpcom-block-editor/src/wpcom/editor.js
+++ apps/wpcom-block-editor/src/wpcom/editor.js
@@ -2,5 +2,6 @@
  * Internal dependencies
  */
 import './features/fix-block-invalidation-errors';
+import './features/migrate-coblocks-buttons';
 import './features/reorder-block-categories';
 import './features/tracking';
