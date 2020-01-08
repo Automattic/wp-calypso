4bc964190c1305dd448df58cb75ae63623ba7af1
Nathan Bloomfield <nathan.bloomfield@a8c.com>
2020-01-08 13:06:22 -0600

    Add FormFieldAnnotation component (#38624)
    Add FormFieldAnnotation component
    
    Composite checkout needs a consistent way to annotate form fields with labels and error messages, including field components pulled in from calypso over which we have less control. This component extracts some functionality from the existing Field component to apply a border and show an error message to any component.

---
+++ packages/composite-checkout-wpcom/src/components/form-field-annotation.tsx
@@ -0,0 +1,130 @@
+/**
+ * External dependencies
+ */
+import React, { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+
+/**
+ * Annotate a form field element with a label, description, and an optional
+ * colored error border with error text.
+ *
+ * If you pass a labelText, you should also pass labelId and formFieldId
+ * for accessibility purposes. formFieldId must be the id attribute of the
+ * child component, of which there should be exactly one.
+ */
+type FormFieldAnnotationProps = {
+	// Semantic props
+	labelText: string;
+	normalDescription?: string;
+	errorDescription: string;
+
+	// Functional props
+	isError?: boolean; // default false
+	isDisabled?: boolean; // default false
+
+	// Technical props
+	className?: string;
+	labelId: string;
+	descriptionId: string;
+	formFieldId: string;
+};
+
+const FormFieldAnnotation: FunctionComponent< FormFieldAnnotationProps > = ( {
+	formFieldId,
+	labelText,
+	labelId,
+	normalDescription,
+	descriptionId,
+	errorDescription,
+	isError,
+	isDisabled,
+	className,
+	children,
+} ) => {
+	const isErrorWithDefault: boolean = isError === undefined ? false : isError;
+	const isDisabledWithDefault: boolean = isDisabled === undefined ? false : isDisabled;
+
+	return (
+		<div className={ className }>
+			<Label htmlFor={ formFieldId } id={ labelId } isDisabled={ isDisabledWithDefault }>
+				{ labelText }
+			</Label>
+			<FormFieldWrapper data-testid={ `${ className }_wrapper` } isError={ isErrorWithDefault }>
+				{ children }
+			</FormFieldWrapper>
+			<RenderedDescription
+				descriptionText={ normalDescription }
+				descriptionId={ descriptionId }
+				isError={ isError }
+				errorMessage={ errorDescription }
+			/>
+		</div>
+	);
+};
+
+type LabelProps = {
+	isDisabled: boolean;
+	theme?: any;
+};
+
+const Label = styled.label< LabelProps >`
+	display: block;
+	color: ${props => props.theme.colors.textColor};
+	font-weight: ${props => props.theme.weights.bold};
+	font-size: 14px;
+	margin-bottom: 8px;
+
+	:hover {
+		cursor: ${props => ( props.isDisabled ? 'default' : 'pointer' )};
+	}
+`;
+
+type FormFieldWrapperProps = {
+	isError: boolean;
+	theme?: any;
+};
+
+const FormFieldWrapper = styled.div< FormFieldWrapperProps >`
+	display: block;
+	width: 100%;
+	box-sizing: border-box;
+	font-size: 16px;
+	border: 1px solid
+		${props => ( props.isError ? props.theme.colors.error : props.theme.colors.borderColor )};
+
+	:focus {
+		outline: ${props => ( props.isError ? props.theme.colors.error : props.theme.colors.outline )}
+			solid 2px !important;
+	}
+
+	::-webkit-inner-spin-button,
+	::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+	}
+`;
+
+function RenderedDescription( { descriptionText, descriptionId, isError, errorMessage } ) {
+	if ( descriptionText || isError ) {
+		return (
+			<Description isError={ isError } id={ descriptionId }>
+				{ isError ? errorMessage : descriptionText }
+			</Description>
+		);
+	}
+	return null;
+}
+
+type DescriptionProps = {
+	isError: boolean;
+	theme?: any;
+};
+
+const Description = styled.p< DescriptionProps >`
+	margin: 8px 0 0 0;
+	color: ${props =>
+		props.isError ? props.theme.colors.error : props.theme.colors.textColorLight};
+	font-style: italic;
+	font-size: 14px;
+`;
+
+export default FormFieldAnnotation;
