1997bd8f14fed5829bbd13b2c034462f8b7b04ad
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-01-08 15:23:41 +0100

    Install react-spring and use to animate StepperWizard. * translate stepper wizard container up when advancing to next step

--- client/landing/gutenboarding/onboarding-block/stepper-wizard.tsx
+++ client/landing/gutenboarding/onboarding-block/stepper-wizard.tsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React, { FunctionComponent, useState, ComponentType } from 'react';
+import { useSpring, animated } from 'react-spring';
 import { Falsy } from 'utility-types';
 
 export interface StepProps {
@@ -18,23 +19,30 @@ interface Props {
 
 const StepperWizard: FunctionComponent< Props > = ( { stepComponents } ) => {
 	const [ activeStep, setActiveStep ] = useState( 0 );
-
 	const handleNext = () => setActiveStep( activeStep + 1 );
 
+	const visibleStepCount = stepComponents.filter( step => !! step ).length;
+	const translatePercent =
+		( 100 / visibleStepCount ) * Math.min( activeStep, visibleStepCount - 1 ); // sometimes activeStep is invisible because of current Next handling
+	const springProps = useSpring( {
+		transform: `translate(0, -${ translatePercent }%)`,
+	} );
+
 	return (
-		<>
-			{ stepComponents.map( ( Komponent, index ) => {
-				return Komponent ? (
-					<Komponent
-						inputClass="onboarding-block__question-input"
-						isActive={ index === activeStep }
-						key={ index }
-						onExpand={ () => setActiveStep( index ) }
-						onSelect={ handleNext }
-					/>
-				) : null;
-			} ) }
-		</>
+		<animated.div style={ springProps }>
+			{ stepComponents.map(
+				( Komponent, index ) =>
+					Komponent && (
+						<Komponent
+							inputClass="onboarding-block__question-input"
+							isActive={ index === activeStep }
+							key={ index }
+							onExpand={ () => setActiveStep( index ) }
+							onSelect={ handleNext }
+						/>
+					)
+			) }
+		</animated.div>
 	);
 };
 
