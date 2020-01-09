62db87e8fc51b2fbe08aca34d255f3d83229bdb5
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2020-01-09 19:03:25 +0200

    Add tests

--- client/components/language-picker/test/modal.js
+++ client/components/language-picker/test/modal.js
@@ -342,18 +342,89 @@ describe( 'LanguagePickerModal', () => {
 		} );
 	} );
 
-	describe( 'language search', () => {
+	describe( 'keyboard support', () => {
+		const simulateKeyDownEvent = key => {
+			window.dispatchEvent( new KeyboardEvent( 'keydown', { key } ) ); // eslint-disable-line no-undef
+		};
+
 		test( 'should update isSearchOpen state properly when search fires onOnSearchClose and onSearchOpen', () => {
 			const wrapper = shallow( <LanguagePickerModal { ...defaultProps } /> );
 			const searchWrapper = wrapper.find( 'Search' );
 
-			expect( wrapper.state().isSearchOpen ).toBe( true );
-
-			searchWrapper.prop( 'onSearchClose' )();
 			expect( wrapper.state().isSearchOpen ).toBe( false );
 
 			searchWrapper.prop( 'onSearchOpen' )();
 			expect( wrapper.state().isSearchOpen ).toBe( true );
+
+			searchWrapper.prop( 'onSearchClose' )();
+			expect( wrapper.state().isSearchOpen ).toBe( false );
+		} );
+
+		test( 'should expand search field when start typing', () => {
+			const wrapper = shallow( <LanguagePickerModal { ...defaultProps } /> );
+
+			simulateKeyDownEvent( 'a' );
+
+			expect( wrapper.state().isSearchOpen ).toBe( true );
+			expect( wrapper.state().search ).toBe( 'a' );
+		} );
+
+		test( "should not expand search field when start typing with space key as it's used to select focused language item", () => {
+			const wrapper = shallow( <LanguagePickerModal { ...defaultProps } /> );
+
+			simulateKeyDownEvent( ' ' );
+
+			expect( wrapper.state().isSearchOpen ).toBe( false );
+			expect( wrapper.state().search ).toBe( false );
+		} );
+
+		test( 'should auto select a language if search matches its slug', () => {
+			const wrapper = shallow( <LanguagePickerModal { ...defaultProps } /> );
+			const languages = defaultProps.languages.map( ( { langSlug } ) => langSlug );
+
+			expect( wrapper.state().selectedLanguageSlug ).toBe( 'en' );
+
+			languages.forEach( langSlug => {
+				wrapper.instance().handleSearch( langSlug );
+				expect( wrapper.state().selectedLanguageSlug ).toBe( langSlug );
+			} );
+		} );
+
+		test( 'should confirm language selection when pressing enter key', () => {
+			const mockOnSelected = jest.fn();
+
+			shallow( <LanguagePickerModal { ...defaultProps } onSelected={ mockOnSelected } /> );
+			simulateKeyDownEvent( 'Enter' );
+
+			expect( mockOnSelected ).toHaveBeenCalled();
+		} );
+
+		test( 'should navigate through languages with arrow keys', () => {
+			const wrapper = shallow( <LanguagePickerModal { ...defaultProps } /> );
+
+			// Set a search state that will match most of the test languages
+			wrapper.setState( { search: 'i' } );
+			const filteredLanguages = wrapper.instance().getFilteredLanguages();
+
+			wrapper.setState( { selectedLanguageSlug: filteredLanguages[ 0 ].langSlug } );
+
+			const horizontalStep = 1;
+
+			for ( let i = 0; i < filteredLanguages.length - 1; i += horizontalStep ) {
+				simulateKeyDownEvent( 'ArrowRight' );
+				expect( wrapper.state().selectedLanguageSlug ).toBe(
+					filteredLanguages[ i + horizontalStep ].langSlug
+				);
+			}
+
+			for ( let i = filteredLanguages.length - 1; i > 0; i -= horizontalStep ) {
+				simulateKeyDownEvent( 'ArrowLeft' );
+				expect( wrapper.state().selectedLanguageSlug ).toBe(
+					filteredLanguages[ i - horizontalStep ].langSlug
+				);
+			}
+
+			// @todo test up and down arrow keys
 		} );
 	} );
 } );
