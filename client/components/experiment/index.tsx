2a67a1dad5bc6e768f575f83247aa54bb47578c0
Rob Landers <landers.robert@gmail.com>
2020-01-09 18:34:15 +0100

    Add documentation

--- client/components/experiment/index.tsx
+++ client/components/experiment/index.tsx
@@ -5,6 +5,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { map, includes } from 'lodash';
 
+/**
+ * The expected props for the top-level experiment component
+ */
 interface ExperimentProps {
 	name: string;
 	children: Variation & DefaultVariation & LoadingVariations;
@@ -12,6 +15,9 @@ interface ExperimentProps {
 	isLoading?: boolean;
 }
 
+/**
+ * The expected props for variations
+ */
 interface VariationProps {
 	name: string;
 	children?: any;
@@ -19,19 +25,36 @@ interface VariationProps {
 	isLoading?: boolean;
 }
 
+/**
+ * The expected props for the loading component
+ */
 interface LoadingProps {
 	children?: any;
 	variation?: string;
 	isLoading?: boolean;
 }
 
+/**
+ * No state
+ */
 type State = {};
 
+/**
+ * Behavior for variations
+ * todo: is there a better way to do this?
+ */
 export class Variation extends Component< VariationProps, State > {
+	/**
+	 * Defines the variations this component expects
+	 */
 	acceptedVariation: ( string | null | undefined )[] = [ this.props.name ];
 
 	render() {
 		const { variation, isLoading, children } = this.props;
+
+		// if it's loading and there's a variation to display, maybe display the variation.
+		// if it's loading and there's not a variation, don't show the variation
+		// if there's a variation, maybe show the variation
 		if ( ( isLoading && variation ) || variation || ( ! isLoading && variation == null ) ) {
 			if ( includes( this.acceptedVariation, variation ) ) {
 				return children;
@@ -41,10 +64,16 @@ export class Variation extends Component< VariationProps, State > {
 	}
 }
 
+/**
+ * Overrides a regular variation, and adds `null` and `undefined` as possible variations to display
+ */
 export class DefaultVariation extends Variation {
 	acceptedVariation = [ null, undefined, this.props.name ];
 }
 
+/**
+ * This component displays when the variation is unknown and an API call needs to be made
+ */
 export class LoadingVariations extends Component< LoadingProps, State > {
 	render() {
 		const { variation, isLoading, children } = this.props;
@@ -53,6 +82,9 @@ export class LoadingVariations extends Component< LoadingProps, State > {
 	}
 }
 
+/**
+ * The experiment component to display the experiment variations
+ */
 export class RawExperiment extends Component< ExperimentProps, State > {
 	render() {
 		const { isLoading, variation } = this.props;
