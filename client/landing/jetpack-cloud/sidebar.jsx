6637429cc142684ec31e1ab51eeec093ee757e76
Marin Atanasov <tyxla@abv.bg>
2020-01-10 16:33:25 +0200

    Jetpack Cloud: Add a basic sidebar

---
+++ client/landing/jetpack-cloud/sidebar.jsx
@@ -0,0 +1,22 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+export default function() {
+	return (
+		<div>
+			<ul>
+				<li>
+					<a href="/jetpack-cloud">Dashboard</a>
+				</li>
+				<li>
+					<a href="/jetpack-cloud/backups">Backups</a>
+				</li>
+				<li>
+					<a href="/jetpack-cloud/scan">Scan</a>
+				</li>
+			</ul>
+		</div>
+	);
+}
