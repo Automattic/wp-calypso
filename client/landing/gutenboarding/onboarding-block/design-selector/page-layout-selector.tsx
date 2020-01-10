b08d9f6422751c7f4a7e78aba0e5a9a75aa2c9b7
Bernie Reiter <ockham@raz.or.at>
2020-01-10 12:54:57 +0100

    Gutenboarding: Add Template Title in Page Selector (#38749)
    Co-authored-by: Jon Surrell <jon.surrell@automattic.com>

--- client/landing/gutenboarding/onboarding-block/design-selector/page-layout-selector.tsx
+++ client/landing/gutenboarding/onboarding-block/design-selector/page-layout-selector.tsx
@@ -9,6 +9,7 @@ import classnames from 'classnames';
  * Internal dependencies
  */
 import Card from '../../components/card';
+import CardFooter from '../../components/card/footer';
 import CardMedia from '../../components/card/media';
 import { removeQueryArgs } from '@wordpress/url';
 import { Icon } from '@wordpress/components';
@@ -48,9 +49,12 @@ const PageLayoutSelector: FunctionComponent< Props > = ( {
 						<span className="page-layout-selector__selected-indicator">
 							<Icon icon="yes" size={ 24 } />
 						</span>
-						<CardMedia as="span">
+						<CardMedia className="page-layout-selector__card-media" as="span">
 							<img alt={ template.description } src={ removeQueryArgs( template.preview, 'w' ) } />
 						</CardMedia>
+						<CardFooter className="page-layout-selector__card-footer" as="span">
+							{ template.title }
+						</CardFooter>
 					</Card>
 				) ) }
 			</div>
