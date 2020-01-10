e2446f7b465cd6033541d32a9ffa8978ae360b56
Jarda Snajdr <jsnajdr@gmail.com>
2020-01-10 10:27:25 +0100

    Fixup paths for ICFY stats

--- bin/icfy-analyze.js
+++ bin/icfy-analyze.js
@@ -7,13 +7,13 @@ analyzeBundle();
 
 function analyzeBundle() {
 	console.log( 'Analyze: reading stats.json file' );
-	const stats = readStatsFromFile( 'stats.json' );
+	const stats = readStatsFromFile( 'client/stats.json' );
 	console.log( 'Analyze: analyzing' );
 	const chart = getViewerData( stats, './public/evergreen' );
 	console.log( 'Analyze: writing chart.json file' );
-	writeFileSync( 'chart.json', JSON.stringify( chart, null, 2 ) );
+	writeFileSync( 'client/chart.json', JSON.stringify( chart, null, 2 ) );
 	console.log( 'Analyze: analyzing the style.css file' );
-	analyzeStylesheet( './public/style.css', 'style.json' );
+	analyzeStylesheet( './public/style.css', 'client/style.json' );
 	console.log( 'Analyze: finished' );
 }
 
