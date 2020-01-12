705dc32e4202727276a3c29b15f53345571ab2f4
Eru Penkman <eru.penkman@a8c.com>
2020-01-13 12:27:21 +1300

    remove the settings menu from gutenboarding

--- client/landing/gutenboarding/gutenboard.tsx
+++ client/landing/gutenboarding/gutenboard.tsx
@@ -9,18 +9,11 @@ import {
 	WritingFlow,
 	ObserveTyping,
 } from '@wordpress/block-editor';
-import {
-	Popover,
-	SlotFillProvider,
-	DropZoneProvider,
-	KeyboardShortcuts,
-} from '@wordpress/components';
+import { Popover, DropZoneProvider } from '@wordpress/components';
 import { createBlock, registerBlockType } from '@wordpress/blocks';
-import { rawShortcut, displayShortcut, shortcutAriaLabel } from '@wordpress/keycodes';
 import { useSelect } from '@wordpress/data';
 import '@wordpress/format-library';
-import classnames from 'classnames';
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 // Uncomment and remove the redundant sass import from `./style.css` when a release after @wordpress/components@8.5.0 is published.
 // See https://github.com/WordPress/gutenberg/pull/19535
 // import '@wordpress/components/build-style/style.css';
@@ -31,26 +24,13 @@ import { useRouteMatch } from 'react-router-dom';
  */
 import Header from './components/header';
 import { name, settings } from './onboarding-block';
-import { Slot as SidebarSlot } from './components/sidebar';
-import SettingsSidebar from './components/settings-sidebar';
 import { STORE_KEY } from './stores/onboard';
 import { routes, Step } from './steps';
 import './style.scss';
 
-// Copied from https://github.com/WordPress/gutenberg/blob/c7d00c64a4c74236a4aab528b3987811ab928deb/packages/edit-post/src/keyboard-shortcuts.js#L11-L15
-// to be consistent with Gutenberg's shortcuts, and in order to avoid pulling in all of `@wordpress/edit-post`.
-const toggleSidebarShortcut = {
-	raw: rawShortcut.primaryShift( ',' ),
-	display: displayShortcut.primaryShift( ',' ),
-	ariaLabel: shortcutAriaLabel.primaryShift( ',' ),
-};
-
 registerBlockType( name, settings );
 
 export function Gutenboard() {
-	const [ isEditorSidebarOpened, updateIsEditorSidebarOpened ] = useState( false );
-	const toggleGeneralSidebar = () => updateIsEditorSidebarOpened( isOpen => ! isOpen );
-
 	const { siteVertical } = useSelect( select => select( STORE_KEY ).getState() );
 
 	// @TODO: This is currently needed in addition to the routing (inside the Onboarding Block)
@@ -79,53 +59,33 @@ export function Gutenboard() {
 	/* eslint-disable wpcalypso/jsx-classname-namespace */
 	return (
 		<div className="block-editor__container">
-			<SlotFillProvider>
-				<DropZoneProvider>
-					<div
-						className={ classnames( 'edit-post-layout', {
-							'is-sidebar-opened': isEditorSidebarOpened,
-						} ) }
+			<DropZoneProvider>
+				<div className="edit-post-layout">
+					<Header prev={ prev } next={ next } />
+					<BlockEditorProvider
+						useSubRegistry={ false }
+						value={ [ onboardingBlock.current ] }
+						settings={ {
+							templateLock: 'all',
+						} }
 					>
-						<KeyboardShortcuts
-							bindGlobal
-							shortcuts={ {
-								[ toggleSidebarShortcut.raw ]: toggleGeneralSidebar,
-							} }
-						/>
-						<Header
-							isEditorSidebarOpened={ isEditorSidebarOpened }
-							toggleGeneralSidebar={ toggleGeneralSidebar }
-							toggleSidebarShortcut={ toggleSidebarShortcut }
-							prev={ prev }
-							next={ next }
-						/>
-						<BlockEditorProvider
-							useSubRegistry={ false }
-							value={ [ onboardingBlock.current ] }
-							settings={ { templateLock: 'all' } }
-						>
-							<div className="gutenboard__edit-post-layout-content edit-post-layout__content ">
-								<div
-									className="edit-post-visual-editor editor-styles-wrapper"
-									role="region"
-									aria-label={ NO__( 'Onboarding screen content' ) }
-									tabIndex={ -1 }
-								>
-									<WritingFlow>
-										<ObserveTyping>
-											<BlockList className="gutenboarding-block-list" />
-										</ObserveTyping>
-									</WritingFlow>
-								</div>
+						<div className="gutenboard__edit-post-layout-content edit-post-layout__content ">
+							<div
+								className="edit-post-visual-editor editor-styles-wrapper"
+								role="region"
+								aria-label={ NO__( 'Onboarding screen content' ) }
+								tabIndex={ -1 }
+							>
+								<WritingFlow>
+									<ObserveTyping>
+										<BlockList className="gutenboarding-block-list" />
+									</ObserveTyping>
+								</WritingFlow>
 							</div>
-							<div>
-								<SettingsSidebar isActive={ isEditorSidebarOpened } />
-								<SidebarSlot />
-							</div>
-						</BlockEditorProvider>
-					</div>
-				</DropZoneProvider>
-			</SlotFillProvider>
+						</div>
+					</BlockEditorProvider>
+				</div>
+			</DropZoneProvider>
 			<Popover.Slot />
 		</div>
 	);
