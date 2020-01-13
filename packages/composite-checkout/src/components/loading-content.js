f5927d8fc14bfba8dfabe29fb7e2087bbf18c73f
fditrapani <filippo.di.trapani@automattic.com>
2020-01-13 14:15:28 -0500

    Composite checkout: add loading animation

--- packages/composite-checkout/src/components/loading-content.js
+++ packages/composite-checkout/src/components/loading-content.js
@@ -2,8 +2,112 @@
  * External dependencies
  */
 import React from 'react';
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/core';
+
+/**
+ * Internal dependencies
+ */
+import { useLocalize } from '../lib/localize';
 
 export default function LoadingContent() {
-	// TODO: fill this in and style it
-	return <div>Loading...</div>;
+	const localize = useLocalize();
+
+	return (
+		<div>
+			<LoadingCard>
+				<LoadingTitle>{ localize( 'Loading checkout' ) }</LoadingTitle>
+				<LoadingCopy />
+				<LoadingCopy />
+			</LoadingCard>
+			<LoadingCard>
+				<LoadingTitle />
+				<LoadingCopy />
+				<LoadingCopy />
+			</LoadingCard>
+			<LoadingCard>
+				<LoadingTitle />
+			</LoadingCard>
+			<LoadingCard>
+				<LoadingTitle />
+			</LoadingCard>
+			<LoadingFooter />
+		</div>
+	);
 }
+
+const LoadingCard = styled.div`
+	padding: 24px;
+	border-top: 1px solid ${props => props.theme.colors.borderColorLight};
+
+	:first-of-type {
+		border-top: 0;
+	}
+`;
+
+const pulse = keyframes`
+  0% {
+    opacity: 1;
+  }
+
+  70% {
+  	opacity: 0.5;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`;
+
+const LoadingTitle = styled.h1`
+	font-size: 14px;
+	content: '';
+	font-weight: ${props => props.theme.weights.normal};
+	background: ${props => props.theme.colors.borderColorLight};
+	color: ${props => props.theme.colors.borderColorLight};
+	width: 40%;
+	margin: 3px 0 0 35px;
+	padding: 0;
+	position: relative;
+	animation: ${pulse} 2s ease-in-out infinite;
+	height: 20px;
+
+	:before {
+		content: '';
+		display: block;
+		position: absolute;
+		left: -35px;
+		top: -3px;
+		width: 27px;
+		height: 27px;
+		background: ${props => props.theme.colors.borderColorLight};
+		border-radius: 100%;
+	}
+`;
+const LoadingCopy = styled.p`
+	font-size: 14px;
+	height: 16px;
+	content: '';
+	background: ${props => props.theme.colors.borderColorLight};
+	color: ${props => props.theme.colors.borderColorLight};
+	margin: 8px 0 0 35px;
+	padding: 0;
+	animation: ${pulse} 2s ease-in-out infinite;
+`;
+
+const LoadingFooter = styled.div`
+	background: ${props => props.theme.colors.background};
+	content: '';
+	border-top: 1px solid ${props => props.theme.colors.borderColorLight};
+	padding: 24px;
+
+	:before {
+		content: '';
+		display: block;
+		border: 1px solid ${props => props.theme.colors.borderColorLight};
+		border-radius: 3px;
+		font-size: 14px;
+		width: 100%;
+		height: 40px;
+	}
+`;
