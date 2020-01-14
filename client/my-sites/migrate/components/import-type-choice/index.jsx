29e0a866583737bb2860b10a5e024bd3bbe08dce
Biser Perchinkov <biser@bisko.be>
2020-01-14 18:08:25 +0200

    mid-sentence commit

--- client/my-sites/migrate/components/import-type-choice/index.jsx
+++ client/my-sites/migrate/components/import-type-choice/index.jsx
@@ -6,29 +6,71 @@ import React, { Component } from 'react';
 import './style.scss';
 
 export default class ImportTypeChoice extends Component {
-	state = {};
+	componentDidMount = () => {
+
+	};
 
 	onClickHandler = event => {
-		console.log;
+		event.preventDefault();
+
+		const chosenItem = event.currentTarget.dataset.key;
+
+		if ( !chosenItem ) {
+			return;
+		}
+
+		if ( this.state.activeItem !== chosenItem ) {
+			this.setState( { activeItem: chosenItem } );
+			// this.props.onChange( chosenItem ); // TODO implement
+		}
+	};
+
+	onKeyPressHandler = event => {
+		// TODO implement this to act on enter key
+		event.preventDefault();
+		console.log( event );
 	};
 
 	render() {
+
+		const items = choices; // TODO pass as props
+
 		return (
 			<div className="import-type-choice__wrapper">
-				<div className="import-type-choice__option-wrapper">
-					<input type="radio" />
-					<div className="import-type-choice__option-data">
-						<div className="import-type-choice__option-header">
-							<h3>Everything</h3>
-							<div className="import-type-choice__option-token-label">Upgrade Required</div>
-							<div className="import-type-choice__option-token-label">Other bubble</div>
-							<div className="import-type-choice__option-token-label">Third bubble</div>
-						</div>
-						<div className="import-type-choice__option-description">
-							All your site's content, themes, plugins, users and settings
+				{items.map( item => (
+					<div
+						className="import-type-choice__option-wrapper"
+						onClick={this.onClickHandler}
+						onKeyPress={this.onKeyPressHandler}
+						role="button"
+						tabIndex={0}
+						data-key={item.key}
+						key={item.key}
+					>
+						<input
+							type="radio"
+							checked={this.state.activeItem === item.key}
+							readOnly={true}
+						/>
+						<div className="import-type-choice__option-data">
+							<div className="import-type-choice__option-header">
+								<h3>{item.title}</h3>
+
+								{item.labels.map( ( label, idx ) => (
+									<div
+										className="import-type-choice__option-token-label"
+										key={idx}
+									>
+										{label}
+									</div>
+								) )}
+							</div>
+							<div className="import-type-choice__option-description">
+								{item.description}
+							</div>
 						</div>
 					</div>
-				</div>
+				) )}
 			</div>
 		);
 	}
