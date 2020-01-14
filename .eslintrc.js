8717e99c7912078812061838ac5f31bf3c3c9f7a
Jon Surrell <jon.surrell@automattic.com>
2020-01-14 22:42:17 +0100

    Add lint rule discouraging lodash.escapeRegExp

--- .eslintrc.js
+++ .eslintrc.js
@@ -124,6 +124,13 @@ module.exports = {
 						importNames: [ 'combineReducers' ],
 						message: "`combineReducers` should be imported from 'state/utils', not 'redux'.",
 					},
+					// Use alternate regexp escape module over lodash's
+					{
+						name: 'lodash',
+						importNames: [ 'escapeRegExp' ],
+						message:
+							"`lodash.escapeRegExp` is disallowed. Use `import escapeRegExp from 'escape-string-regexp';` instead.",
+					},
 					// Use fetch instead of superagent.
 					{
 						name: 'superagent',
