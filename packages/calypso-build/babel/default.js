b274846b951750c6f46ba17c3933fb2e6aa4ea05
Sérgio Gomes <mail@sgomes.com>
2020-01-14 11:30:58 +0000

    Switch back to core-js: 3.6  in config.
    This helps avoid the globalThis workaround.

--- packages/calypso-build/babel/default.js
+++ packages/calypso-build/babel/default.js
@@ -16,7 +16,7 @@ module.exports = ( api, opts ) => ( {
 		[
 			require.resolve( '@babel/preset-env' ),
 			{
-				corejs: 3,
+				corejs: 3.6,
 				modules: modulesOption( opts ),
 				useBuiltIns: 'entry',
 				// Exclude transforms that make all code slower, see https://github.com/facebook/create-react-app/pull/5278
