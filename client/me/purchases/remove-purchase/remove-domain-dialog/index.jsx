f45ddfa0fab80b7d1167b45d6fb58f39e6faaf9d
Elton Goci <eltongoci@gmail.com>
2020-01-15 11:24:12 +0100

    Restore contact support prompt
    This will be removed in a future PR

--- client/me/purchases/remove-purchase/remove-domain-dialog/index.jsx
+++ client/me/purchases/remove-purchase/remove-domain-dialog/index.jsx
@@ -15,7 +15,8 @@ import FormLabel from 'components/forms/form-label';
 import FormTextInput from 'components/forms/form-text-input';
 import FormInputValidation from 'components/forms/form-input-validation';
 import FormCheckbox from 'components/forms/form-checkbox';
-import { getName } from 'lib/purchases';
+import { CALYPSO_CONTACT } from 'lib/url/support';
+import { getName, isRefundable, maybeWithinRefundPeriod } from 'lib/purchases';
 
 class RemoveDomainDialog extends Component {
 	static propTypes = {
@@ -34,7 +35,7 @@ class RemoveDomainDialog extends Component {
 	};
 
 	renderFirstStep( productName ) {
-		const { translate } = this.props;
+		const { translate, purchase } = this.props;
 
 		return (
 			<Fragment>
@@ -55,6 +56,26 @@ class RemoveDomainDialog extends Component {
 						}
 					) }
 				</p>
+				{ ! isRefundable( purchase ) && maybeWithinRefundPeriod( purchase ) && (
+					<p>
+						<strong>
+							{ translate(
+								"We're not able to refund this purchase automatically. " +
+									"If you're canceling within %(refundPeriodInDays)s days of " +
+									'purchase, {{contactLink}}contact us{{/contactLink}} to ' +
+									'request a refund.',
+								{
+									args: {
+										refundPeriodInDays: purchase.refundPeriodInDays,
+									},
+									components: {
+										contactLink: <a href={ CALYPSO_CONTACT } />,
+									},
+								}
+							) }
+						</strong>
+					</p>
+				) }
 			</Fragment>
 		);
 	}
