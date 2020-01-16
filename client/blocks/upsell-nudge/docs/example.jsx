013d1a677cc47592e9751752273dc7297f84bcf9
sixhours <calobee@gmail.com>
2020-01-16 11:46:23 -0500

    Remove unnecessary localization.

--- client/blocks/upsell-nudge/docs/example.jsx
+++ client/blocks/upsell-nudge/docs/example.jsx
@@ -2,31 +2,29 @@
  * External dependencies
  */
 import React from 'react';
-import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import UpsellNudge from 'blocks/upsell-nudge';
 
-const UpsellNudgeExample = ( { translate } ) => (
+const UpsellNudgeExample = () => (
 	<>
 		<UpsellNudge
 			href="#"
-			callToAction={ translate( 'Upgrade' ) }
-			title={ translate( 'Free domain with a plan! This is a regular nudge with an icon.' ) }
+			callToAction="Upgrade"
+			title="Free domain with a plan! This is a regular nudge with an icon."
 			showIcon={ true }
 		/>
 		<UpsellNudge
 			href="#"
 			compact
-			callToAction={ translate( 'Upgrade' ) }
-			title={ translate( 'Free domain with a plan! This is a compact nudge with no icon.' ) }
+			callToAction="Upgrade"
+			title="Free domain with a plan! This is a compact nudge with no icon."
 		/>
 	</>
 );
 
-const LocalizedUpsellNudgeExample = localize( UpsellNudgeExample );
-LocalizedUpsellNudgeExample.displayName = 'UpsellNudge';
+UpsellNudgeExample.displayName = 'UpsellNudge';
 
-export default LocalizedUpsellNudgeExample;
+export default UpsellNudgeExample;
