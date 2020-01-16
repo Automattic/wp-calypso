16c0da2a0e57a449aa973b9d35c6a8ddbfc1b387
Bernie Reiter <ockham@raz.or.at>
2020-01-16 20:23:25 +0100

    preserve: false

--- apps/notifications/postcss.config.js
+++ apps/notifications/postcss.config.js
@@ -2,6 +2,9 @@ module.exports = () => ( {
 	plugins: {
 		'postcss-custom-properties': {
 			importFrom: [ require.resolve( '@automattic/calypso-color-schemes' ) ],
+			// @TODO: Drop `preserve: false` workaround if possible
+			// See https://github.com/Automattic/jetpack/pull/13854#issuecomment-550898168
+			preserve: false,
 		},
 		autoprefixer: {},
 	},
