ef431cc6527facca48769e62e592fabb6d322855
Andrés <andres.maneiro@automattic.com>
2020-01-17 12:24:45 +0100

    Update version

--- apps/full-site-editing/full-site-editing-plugin/global-styles/class-global-styles.php
+++ apps/full-site-editing/full-site-editing-plugin/global-styles/class-global-styles.php
@@ -78,7 +78,7 @@ class Global_Styles {
 	 */
 	private $plugin_name = 'jetpack-global-styles';
 
-	const VERSION = '1909241817';
+	const VERSION = '2001171221';
 
 	const SYSTEM_FONT     = '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif';
 	const AVAILABLE_FONTS = [
