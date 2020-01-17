2da0c1be3e7febfed45f210252832f5cafb695fe
Jon Surrell <jon.surrell@automattic.com>
2020-01-17 13:00:55 +0100

    Add skip pupeteer env var to bin scripts

--- bin/install-if-no-packages.js
+++ bin/install-if-no-packages.js
@@ -2,10 +2,11 @@ const spawnSync = require( 'child_process' ).spawnSync;
 const fs = require( 'fs' );
 
 if ( ! fs.existsSync( 'node_modules' ) ) {
-	console.log( 'No "node_modules" present, installing dependencies...' );
+	console.log( 'No "node_modules" present, installing dependencies…' );
 	const installResult = spawnSync( 'npm', [ 'ci' ], {
 		shell: true,
 		stdio: 'inherit',
+		env: Object.assign( { PUPPETEER_SKIP_CHROMIUM_DOWNLOAD: 'true' }, process.env ),
 	} ).status;
 	if ( installResult.status ) {
 		console.error( 'Failed install: %o', installResult );
