fa6fff98e8caec7055ddcb3130cfd313780a71b4
Bernie Reiter <ockham@raz.or.at>
2020-01-20 20:35:01 +0600

    Gutenberg e2e Tests: Use kebabCase instead of manually converting

--- test/e2e/lib/gutenberg/gutenberg-editor-component.js
+++ test/e2e/lib/gutenberg/gutenberg-editor-component.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { By, until } from 'selenium-webdriver';
+import { kebabCase } from 'lodash';
 
 /**
  * Internal dependencies
@@ -192,7 +193,7 @@ export default class GutenbergEditorComponent extends AsyncBaseContainer {
 	// return blockID - top level block id which is looks like `block-b91ce479-fb2d-45b7-ad92-22ae7a58cf04`. Should be used for further interaction with added block.
 	async addBlock( name ) {
 		name = name.charAt( 0 ).toUpperCase() + name.slice( 1 ); // Capitalize block name
-		let blockClass = name;
+		let blockClass = kebabCase( name );
 		let selectedBlockConfirmClass = 'is-selected';
 		let prefix = '';
 		switch ( name ) {
@@ -231,9 +232,7 @@ export default class GutenbergEditorComponent extends AsyncBaseContainer {
 		}
 
 		const inserterBlockItemSelector = By.css(
-			`li.block-editor-block-types-list__list-item button.editor-block-list-item-${ prefix }${ blockClass
-				.replace( /\s+/g, '-' )
-				.toLowerCase() }`
+			`li.block-editor-block-types-list__list-item button.editor-block-list-item-${ prefix }${ blockClass }`
 		);
 		const insertedBlockSelector = By.css(
 			`.block-editor-block-list__block.${ selectedBlockConfirmClass }[aria-label*='Block: ${ name }']`
