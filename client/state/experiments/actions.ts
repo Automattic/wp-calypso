37a91dc58e1ddeb1e01afd421de897d9493a0e00
Rob Landers <landers.robert@gmail.com>
2020-01-20 13:15:13 +0100

    Document the actions

--- client/state/experiments/actions.ts
+++ client/state/experiments/actions.ts
@@ -4,11 +4,21 @@
 import { EXPERIMENT_FETCH, EXPERIMENT_ASSIGN } from 'state/action-types';
 import { ExperimentResponse } from 'state/experiments/types';
 
+/**
+ * Assign the user to the specified experiments
+ *
+ * @param experiments The experiments to assign to the user
+ */
 export const assignToExperiments = ( experiments: ExperimentResponse ) => ( {
 	type: EXPERIMENT_ASSIGN,
 	...experiments,
 } );
 
+/**
+ * Inform the data-layer to request new experiments from the API
+ *
+ * @param anonId The anonymous identifier to send to the API
+ */
 export const fetchExperiments = ( anonId: string ) => ( {
 	type: EXPERIMENT_FETCH,
 	anonId,
