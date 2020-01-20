b7c2fd73ffea751f4c1e37497dffe24ac1b85272
Jon Surrell <jon.surrell@automattic.com>
2020-01-20 16:26:30 +0100

    Use @automattic/calypso-polyfills

--- client/landing/gutenboarding/index.tsx
+++ client/landing/gutenboarding/index.tsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import '../../boot/polyfills';
+import '@automattic/calypso-polyfills';
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
