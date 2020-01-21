3c48066fbaae55d6cdb6351ab9bca3f2bc25075e
mmtr <miguel.torres@automattic.com>
2020-01-21 12:32:54 +0100

    Tweak styles for one single badge

--- client/my-sites/customer-home/go-mobile-card/index.jsx
+++ client/my-sites/customer-home/go-mobile-card/index.jsx
@@ -30,38 +30,41 @@ export const GoMobileCard = ( { translate, email, sendMobileLoginEmail } ) => {
 	const isIos = isiPad || isiPod || isiPhone;
 	const showIosBadge = isDesktop() || isIos || ! isAndroid;
 	const showAndroidBadge = isDesktop() || isAndroid || ! isIos;
+	const showOnlyOneBadge = showIosBadge !== showAndroidBadge;
 
 	const emailLogin = () => {
 		sendMobileLoginEmail( email );
 	};
 
 	return (
-		<Card className={ classnames( 'go-mobile-card' ) }>
-			<div>
-				<CardHeading>{ translate( 'Go Mobile' ) }</CardHeading>
-				<h6 className="go-mobile-card__subheader">{ translate( 'Make updates on the go' ) }</h6>
-			</div>
-			<div className="go-mobile-card__app-badges">
-				{ showIosBadge && (
-					<AppsBadge
-						storeLink="https://apps.apple.com/app/apple-store/id335703880?pt=299112&ct=calypso-customer-home&mt=8"
-						storeName={ 'ios' }
-						titleText={ translate( 'Download the WordPress iOS mobile app.' ) }
-						altText={ translate( 'Apple App Store download badge' ) }
-					>
-						<img src={ appleStoreLogo } alt="" />
-					</AppsBadge>
-				) }
-				{ showAndroidBadge && (
-					<AppsBadge
-						storeLink="https://play.google.com/store/apps/details?id=org.wordpress.android&referrer=utm_source%3Dcalypso-customer-home%26utm_medium%3Dweb%26utm_campaign%3Dmobile-download-promo-pages"
-						storeName={ 'android' }
-						titleText={ translate( 'Download the WordPress Android mobile app.' ) }
-						altText={ translate( 'Google Play Store download badge' ) }
-					>
-						<img src={ googlePlayLogo } alt="" />
-					</AppsBadge>
-				) }
+		<Card className="go-mobile-card">
+			<div className={ classnames( 'go-mobile-card__row', { 'has-2-cols': showOnlyOneBadge } ) }>
+				<div className="go-mobile-card__title">
+					<CardHeading>{ translate( 'Go Mobile' ) }</CardHeading>
+					<h6 className="go-mobile-card__subheader">{ translate( 'Make updates on the go' ) }</h6>
+				</div>
+				<div className="go-mobile-card__app-badges">
+					{ showIosBadge && (
+						<AppsBadge
+							storeLink="https://apps.apple.com/app/apple-store/id335703880?pt=299112&ct=calypso-customer-home&mt=8"
+							storeName={ 'ios' }
+							titleText={ translate( 'Download the WordPress iOS mobile app.' ) }
+							altText={ translate( 'Apple App Store download badge' ) }
+						>
+							<img src={ appleStoreLogo } alt="" />
+						</AppsBadge>
+					) }
+					{ showAndroidBadge && (
+						<AppsBadge
+							storeLink="https://play.google.com/store/apps/details?id=org.wordpress.android&referrer=utm_source%3Dcalypso-customer-home%26utm_medium%3Dweb%26utm_campaign%3Dmobile-download-promo-pages"
+							storeName={ 'android' }
+							titleText={ translate( 'Download the WordPress Android mobile app.' ) }
+							altText={ translate( 'Google Play Store download badge' ) }
+						>
+							<img src={ googlePlayLogo } alt="" />
+						</AppsBadge>
+					) }
+				</div>
 			</div>
 			<Button className="go-mobile-card__email-link-button" onClick={ emailLogin }>
 				{ translate( 'Email me a log in link' ) }
