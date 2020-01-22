8c9dedd0e32e338d969b8f64e3ea8219e4d23393
Jarda Snajdr <jsnajdr@gmail.com>
2020-01-22 11:50:18 +0100

    Fix error handling in the install-if-deps-outdated script
    Fixes invalid assignment of `spawnSync().status` to `installResult`. We want to
    assign and check the whole result object, not just its `status` field.
    
    If `npm ci` failed, the error check code failed to detect it and proceeded to the
    `fs.utimesSync` call, even if `node_modules` didn't exist.
    
    Also, remove the `-s` argument from `npm run` when `install-if-deps-outdated`. The
    `-s` argument only hides errors, while stdout continues to be full of output from
    `npx` and `lerna`.

--- bin/install-if-deps-outdated.js
+++ bin/install-if-deps-outdated.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /**
- * Performs an `npm install`. Since that's a costly operation,
+ * Installs `node_modules` with `npm ci`. Since that's a costly operation,
  * it will only perform it if needed, that is, if the packages
  * installed at `node_modules` aren't in sync over what
  * `package-lock.json` has. For that, modification times of both
@@ -13,7 +13,7 @@
 
 const fs = require( 'fs' );
 const path = require( 'path' );
-const spawnSync = require( 'child_process' ).spawnSync;
+const { spawnSync } = require( 'child_process' );
 //const debug = require( 'debug' )( 'calypso:install' );
 
 const needsInstall = () => {
@@ -51,15 +51,16 @@ function install() {
 		console.error( 'failed to clean: %o', cleanResult );
 		process.exit( cleanResult.status );
 	}
+
 	const installResult = spawnSync( 'npm', [ 'ci' ], {
 		shell: true,
 		stdio: 'inherit',
-	} ).status;
+	} );
 	if ( installResult.status ) {
 		console.error( 'failed to install: %o', installResult );
 		process.exit( installResult.status );
 	}
-	const touchDate = new Date();
 
+	const touchDate = new Date();
 	fs.utimesSync( 'node_modules', touchDate, touchDate );
 }
