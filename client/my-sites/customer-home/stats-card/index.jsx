bd87aec28c9cc4bf906baaaa6bfed82be6c5f00b
mmtr <miguel.torres@automattic.com>
2020-01-22 13:12:45 +0100

    Fallback to most popular date and time when there are not enough visits

--- client/my-sites/customer-home/stats-card/index.jsx
+++ client/my-sites/customer-home/stats-card/index.jsx
@@ -22,11 +22,15 @@ import './style.scss';
 
 export const StatsCard = ( {
 	areStatsEnabled,
+	insightsData,
+	insightsStatsQuery,
+	insightsStatsType,
+	showInsights,
 	siteId,
 	siteSlug,
 	trafficData,
-	trafficStatsQuery,
 	trafficStatsType,
+	trafficStatsQuery,
 } ) => {
 	const translate = useTranslate();
 
@@ -37,23 +41,48 @@ export const StatsCard = ( {
 	return (
 		<Card className="stats-card">
 			{ siteId && (
-				<QuerySiteStats
-					siteId={ siteId }
-					statType={ trafficStatsType }
-					query={ trafficStatsQuery }
-				/>
+				<>
+					<QuerySiteStats
+						siteId={ siteId }
+						statType={ trafficStatsType }
+						query={ trafficStatsQuery }
+					/>
+					{ showInsights && (
+						<QuerySiteStats
+							siteId={ siteId }
+							statType={ insightsStatsType }
+							query={ insightsStatsQuery }
+						/>
+					) }
+				</>
 			) }
 			<CardHeading>{ translate( 'Stats at a glance' ) }</CardHeading>
 			<h6 className="stats-card__subheader">{ translate( 'Your site in the last week.' ) }</h6>
-			<div className="stats-card__visits">
-				<div className="stats-card__count">
-					<div className="stats-card__count-value">{ trafficData?.views ?? '-' }</div>
-					<div className="stats-card__count-label">{ translate( 'Views' ) }</div>
-				</div>
-				<div className="stats-card__count">
-					<div className="stats-card__count-value">{ trafficData?.visitors ?? '-' }</div>
-					<div className="stats-card__count-label">{ translate( 'Visitors' ) }</div>
-				</div>
+			<div className="stats-card__data">
+				{ ! showInsights && (
+					<div className="stats-card__data-item">
+						<div className="stats-card__data-value">{ trafficData?.views ?? '-' }</div>
+						<div className="stats-card__data-label">{ translate( 'Views' ) }</div>
+					</div>
+				) }
+				{ ! showInsights && (
+					<div className="stats-card__data-item">
+						<div className="stats-card__data-value">{ trafficData?.visitors ?? '-' }</div>
+						<div className="stats-card__data-label">{ translate( 'Visitors' ) }</div>
+					</div>
+				) }
+				{ showInsights && (
+					<div className="stats-card__data-item">
+						<div className="stats-card__data-label">{ translate( 'Most popular day' ) }</div>
+						<div className="stats-card__data-value">{ insightsData?.day ?? '-' }</div>
+					</div>
+				) }
+				{ showInsights && (
+					<div className="stats-card__data-item">
+						<div className="stats-card__data-label">{ translate( 'Most popular hour' ) }</div>
+						<div className="stats-card__data-value">{ insightsData?.hour ?? '-' }</div>
+					</div>
+				) }
 			</div>
 			<a href={ `/stats/day/${ siteSlug }` }>{ translate( 'See all stats' ) }</a>
 		</Card>
@@ -67,12 +96,12 @@ const mapStateToProps = state => {
 	const isStatsModuleActive = isJetpackModuleActive( state, siteId, 'stats' );
 	const areStatsEnabled = ! isJetpack || isStatsModuleActive;
 
+	const trafficStatsType = 'statsVisits';
 	const trafficStatsQuery = {
 		unit: 'week',
 		quantity: 1,
 		stat_fields: 'views,visitors',
 	};
-	const trafficStatsType = 'statsVisits';
 	const trafficStats = getSiteStatsNormalizedData(
 		state,
 		siteId,
@@ -81,8 +110,23 @@ const mapStateToProps = state => {
 	);
 	const trafficData = trafficStats && trafficStats.length ? trafficStats[ 0 ] : null;
 
+	const showInsights = trafficData && trafficData.views < 10;
+
+	const insightsStatsType = 'statsInsights';
+	const insightsStatsQuery = {};
+	const insightsData = getSiteStatsNormalizedData(
+		state,
+		siteId,
+		insightsStatsType,
+		insightsStatsQuery
+	);
+
 	return {
 		areStatsEnabled,
+		insightsData,
+		insightsStatsQuery,
+		insightsStatsType,
+		showInsights,
 		siteId,
 		siteSlug,
 		trafficData,
