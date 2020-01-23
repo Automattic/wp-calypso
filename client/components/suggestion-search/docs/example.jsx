1bb7be8966303192898b25461754e8129c743aa9
Jon Surrell <jon.surrell@automattic.com>
2020-01-23 09:58:43 +0100

    Devdocs: Fix SuggestionSearch (#38972)

--- client/components/suggestion-search/docs/example.jsx
+++ client/components/suggestion-search/docs/example.jsx
@@ -1,30 +1,35 @@
 /**
  * External dependencies
  */
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 
 /**
  * Internal dependencies
  */
 import SuggestionSearch from '../';
 
-class SuggestionSearchExample extends PureComponent {
-	onSiteTopicChange = value => this.state( { selected: value } );
+const SuggestionSearchExample = () => {
+	const [ state, setState ] = useState();
 
-	sortDisplayResults = ( suggestionsArray, queryString ) =>
+	const sortDisplayResults = ( suggestionsArray, queryString ) =>
 		suggestionsArray.sort().map( item => ( item === queryString ? `→ ${ item }` : item ) );
 
-	render() {
-		return (
-			<SuggestionSearch
-				id="siteTopic"
-				placeholder={ 'Try sushi' }
-				onChange={ this.onSiteTopicChange }
-				sortResults={ this.sortDisplayResults }
-				suggestions={ [ 'sushi', 'onigiri sushi', 'sea urchin sushi', 'saury sushi' ] }
-			/>
-		);
-	}
-}
+	return (
+		<SuggestionSearch
+			id="siteTopic"
+			placeholder="Try sushi"
+			onChange={ setState }
+			sortResults={ sortDisplayResults }
+			suggestions={ [
+				{ label: 'sushi' },
+				{ label: 'onigiri sushi' },
+				{ label: 'sea urchin sushi' },
+				{ label: 'saury sushi' },
+			] }
+			value={ state }
+		/>
+	);
+};
+SuggestionSearchExample.displayName = 'SuggestionSearchExample';
 
 export default SuggestionSearchExample;
