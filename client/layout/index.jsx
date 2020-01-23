1e7eaae86b0d0bbee4df6214c867c55b8ab63826
jeraldjuice <olivia.wolski@automattic.com>
2020-01-23 16:40:43 -0500

    Override signup colors for frankenflow

--- client/layout/index.jsx
+++ client/layout/index.jsx
@@ -125,9 +125,23 @@ class Layout extends Component {
 			}
 		);
 
+		const optionalBodyProps = () => {
+			const optionalProps = {};
+
+			if ( this.props.isFrankenflow ) {
+				optionalProps.bodyClass = 'is-frankenflow';
+			}
+
+			return optionalProps;
+		};
+
 		return (
 			<div className={ sectionClass }>
-				<BodySectionCssClass group={ this.props.sectionGroup } section={ this.props.sectionName } />
+				<BodySectionCssClass
+					group={ this.props.sectionGroup }
+					section={ this.props.sectionName }
+					{ ...optionalBodyProps() }
+				/>
 				<HtmlIsIframeClassname />
 				<DocumentHead />
 				<QuerySites primaryAndRecent />
@@ -206,6 +220,7 @@ export default connect( state => {
 	const oauth2Client = getCurrentOAuth2Client( state );
 	const wccomFrom = get( getCurrentQueryArguments( state ), 'wccom-from' );
 	const isEligibleForJITM = [ 'stats', 'plans', 'themes', 'plugins' ].indexOf( sectionName ) >= 0;
+	const isFrankenflow = startsWith( currentRoute, '/start/frankenflow' );
 
 	return {
 		masterbarIsHidden:
@@ -234,5 +249,6 @@ export default connect( state => {
 		authorization, it would remove the newly connected site that has been fetched separately.
 		See https://github.com/Automattic/wp-calypso/pull/31277 for more details. */
 		shouldQueryAllSites: currentRoute && currentRoute !== '/jetpack/connect/authorize',
+		isFrankenflow,
 	};
 } )( Layout );
