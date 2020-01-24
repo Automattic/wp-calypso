e784c34385e65387b043d991dc93b283c244aff8
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-01-24 14:37:16 -0600

    Refactor Calypso boot to avoid code duplication in Jetpack Cloud boot

--- client/boot/app.js
+++ client/boot/app.js
@@ -2,46 +2,15 @@
 import './polyfills';
 
 /**
- * External dependencies
- */
-import debugFactory from 'debug';
-import page from 'page';
-
-/**
  * Internal dependencies
  */
-import { configureReduxStore, setupMiddlewares, utils } from './common';
-import { setupLocale } from './locale';
-import { createReduxStore } from 'state';
-import initialReducer from 'state/reducer';
-import { getInitialState, persistOnChange, loadAllState } from 'state/initial-state';
-import detectHistoryNavigation from 'lib/detect-history-navigation';
-import userFactory from 'lib/user';
+import { bootApp } from './common';
 
 /**
  * Style dependencies
  */
 import 'assets/stylesheets/style.scss';
 
-const debug = debugFactory( 'calypso' );
-
-const boot = currentUser => {
-	debug( "Starting Calypso. Let's do this." );
-
-	utils();
-	loadAllState().then( () => {
-		const initialState = getInitialState( initialReducer );
-		const reduxStore = createReduxStore( initialState, initialReducer );
-		persistOnChange( reduxStore );
-		setupLocale( currentUser.get(), reduxStore );
-		configureReduxStore( currentUser, reduxStore );
-		setupMiddlewares( currentUser, reduxStore );
-		detectHistoryNavigation.start();
-		page.start( { decodeURLComponents: false } );
-	} );
-};
-
 window.AppBoot = () => {
-	const user = userFactory();
-	user.initialize().then( () => boot( user ) );
+	bootApp( 'Calypso' );
 };
--- client/landing/jetpack-cloud/index.js
+++ client/landing/jetpack-cloud/index.js
@@ -2,20 +2,12 @@
  * External dependencies
  */
 import config from '../../config';
-import page from 'page';
-import debugFactory from 'debug';
 
 /**
  * Internal dependencies
  */
-import { configureReduxStore, setupMiddlewares, utils } from 'boot/common';
 import initJetpackCloudRoutes from './routes';
-import userFactory from 'lib/user';
-import { createReduxStore } from 'state';
-import initialReducer from 'state/reducer';
-import { setupLocale } from 'boot/locale';
-import detectHistoryNavigation from 'lib/detect-history-navigation';
-import { getInitialState, persistOnChange } from 'state/initial-state';
+import { bootApp } from 'boot/common';
 
 /**
  * Style dependencies
@@ -24,31 +16,11 @@ import 'components/environment-badge/style.scss';
 import 'layout/style.scss';
 import 'assets/stylesheets/jetpack-cloud.scss';
 
-const debug = debugFactory( 'jetpack-cloud' );
-
-const boot = currentUser => {
-	debug( "Starting Jetpack Cloud. Let's do this." );
-	utils();
-	getInitialState( initialReducer ).then( initialState => {
-		const reduxStore = createReduxStore( initialState, initialReducer );
-		initJetpackCloudRoutes( '/jetpack-cloud' );
-		persistOnChange( reduxStore );
-		setupLocale( currentUser.get(), reduxStore );
-		configureReduxStore( currentUser, reduxStore );
-		setupMiddlewares( currentUser, reduxStore );
-		detectHistoryNavigation.start();
-
-		page.start( { decodeURLComponents: false } );
-	} );
-};
-
 window.AppBoot = () => {
 	if ( ! config.isEnabled( 'jetpack-cloud' ) ) {
 		window.location.href = '/';
 	} else {
-		debug( 'before userFactory' );
-		const user = userFactory();
-		debug( 'after userFactory' );
-		user.initialize().then( () => boot( user ) );
+		initJetpackCloudRoutes( '/jetpack-cloud' );
+		bootApp( 'Jetpack' );
 	}
 };
