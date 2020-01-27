5fef73d474e5c8ed7912774771b3d05fc786ec8c
Echo G <ChaosExAnima@users.noreply.github.com>
2020-01-27 17:37:31 -0500

    Creates LogItem component for Cloud logs

---
+++ client/landing/jetpack-cloud/components/log-item/index.tsx
@@ -0,0 +1,52 @@
+/**
+ * External dependencies
+ */
+import React, { ReactNode } from 'react';
+
+/**
+ * Internal dependencies
+ */
+import FoldableCard from 'components/foldable-card';
+import CardHeading from 'components/card-heading';
+
+import './style.scss';
+
+interface Props {
+	children?: ReactNode;
+	header: string;
+	subheader: string;
+	highlight?: string;
+	tag?: string;
+}
+
+class LogItem extends React.PureComponent< Props > {
+	static defaultProps = {
+		highlight: 'info',
+		tag: 'new',
+	};
+
+	renderHeader() {
+		const { header, subheader, tag } = this.props;
+
+		return (
+			<div className="log-item__header-wrapper">
+				<span className="log-item__tag">{ tag }</span>
+				<CardHeading tagName="h2" size={ 18 }>
+					{ header }
+				</CardHeading>
+				<p className="log-item__subheader">{ subheader }</p>
+			</div>
+		);
+	}
+
+	render() {
+		const { highlight, children } = this.props;
+		return (
+			<FoldableCard header={ this.renderHeader() } className="log-item" highlight={ highlight }>
+				{ children }
+			</FoldableCard>
+		);
+	}
+}
+
+export default LogItem;
