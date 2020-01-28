0eaa3d5eca3f5d0febb9eabcda415773fd13ebfc
Echo G <ChaosExAnima@users.noreply.github.com>
2020-01-28 10:25:15 -0500

    Removes routing, adds cloud emoji.

--- client/landing/jetpack-cloud/index.js
+++ client/landing/jetpack-cloud/index.js
@@ -20,7 +20,7 @@ window.AppBoot = () => {
 	if ( ! config.isEnabled( 'jetpack-cloud' ) ) {
 		window.location.href = '/';
 	} else {
-		initJetpackCloudRoutes( '/jetpack-cloud' );
-		bootApp( 'Jetpack' );
+		initJetpackCloudRoutes();
+		bootApp( 'Jetpack ☁️' );
 	}
 };
