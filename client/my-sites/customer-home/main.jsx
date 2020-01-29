Automattic/wp-calypso

commit 0b62225f87b6b3ebd7da3dcfb7d60d6e5f10aed7

Try Enabling Customer Home for all Simple and Atomic Sites (#39071)

client/my-sites/customer-home/main.jsx
@@ -57,6 +57,7 @@ import QueryActiveTheme from 'components/data/query-active-theme';
 import QueryCanonicalTheme from 'components/data/query-canonical-theme';
 import GoMobileCard from 'my-sites/customer-home/go-mobile-card';
 import StatsCard from './stats-card';
+import isEligibleForDotcomChecklist from 'state/selectors/is-eligible-for-dotcom-checklist';
 
 /**
  * Style dependencies
@@ -596,7 +597,7 @@ const connectHome = connect(
 		const createdAt = getSiteOption( state, siteId, 'created_at' );
 
 		return {
-			displayChecklist: !! ( ! isAtomic && hasChecklistData && ! isChecklistComplete ),
+			displayChecklist: isEligibleForDotcomChecklist( state, siteId ),
 			site: getSelectedSite( state ),
 			siteId,
 			siteSlug: getSelectedSiteSlug( state ),

client/state/sites/selectors/can-current-user-use-customer-home.js
@@ -13,6 +13,7 @@ import isVipSite from 'state/selectors/is-vip-site';
 import isAtomicSite from 'state/selectors/is-site-automated-transfer';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import getSite from './get-site';
+import config from 'config';
 
 /**
  * Returns true if the current user should be able to use the customer home screen
@@ -34,15 +35,17 @@ export default function canCurrentUserUseCustomerHome( state, siteId = null ) {
 		return false;
 	}
 
-	const siteOptions = getSiteOptions( state, siteId );
-	const createdAt = get( siteOptions, 'created_at', '' );
+	if ( ! config.isEnabled( 'home/all' ) ) {
+		const siteOptions = getSiteOptions( state, siteId );
+		const createdAt = get( siteOptions, 'created_at', '' );
 
-	if (
-		! createdAt ||
-		createdAt.substr( 0, 4 ) === '0000' ||
-		new Date( createdAt ) < new Date( '2019-08-06' )
-	) {
-		return false;
+		if (
+			! createdAt ||
+			createdAt.substr( 0, 4 ) === '0000' ||
+			new Date( createdAt ) < new Date( '2019-08-06' )
+		) {
+			return false;
+		}
 	}
 
 	const site = getSite( state, siteId );

client/state/sites/test/selectors.js
@@ -3857,30 +3857,6 @@ describe( 'selectors', () => {
 			},
 		} );
 
-		test( 'should return true for a simple site created after 2019-08-06', () => {
-			expect( canCurrentUserUseCustomerHome( createState( { created_at: '2020-01-01' } ) ) ).toBe(
-				true
-			);
-		} );
-
-		test( 'should return false for a simple site created before 2019-08-06', () => {
-			expect( canCurrentUserUseCustomerHome( createState( { created_at: '2019-08-01' } ) ) ).toBe(
-				false
-			);
-		} );
-
-		test( 'should return false for a simple site created on the 2019-08-06', () => {
-			expect( canCurrentUserUseCustomerHome( createState( { created_at: '2019-08-06' } ) ) ).toBe(
-				true
-			);
-		} );
-
-		test( "should return false for site with a zero'd out created_at option", () => {
-			expect(
-				canCurrentUserUseCustomerHome( createState( { created_at: '0000-00-00T00:00:00+00:00' } ) )
-			).toBe( false );
-		} );
-
 		test( "should return false if user can't manage site options", () => {
 			expect(
 				canCurrentUserUseCustomerHome(

commit a7f25766b372aa24d67b156f677aedd7e615b22e

Customer Home: Show Site Tools when checklist is complete (#39130)
Fixes a regression that was included in #39071 causing the checklist to be displayed even if it is completed.

client/my-sites/customer-home/main.jsx
@@ -624,7 +624,8 @@ const connectHome = connect(
 		const createdAt = getSiteOption( state, siteId, 'created_at' );
 
 		return {
-			displayChecklist: isEligibleForDotcomChecklist( state, siteId ),
+			displayChecklist:
+				isEligibleForDotcomChecklist( state, siteId ) && hasChecklistData && ! isChecklistComplete,
 			site: getSelectedSite( state ),
 			siteId,
 			siteSlug: getSelectedSiteSlug( state ),