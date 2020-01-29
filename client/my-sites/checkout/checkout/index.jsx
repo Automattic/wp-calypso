2c1d71d8a6c70bf5bc7ffbff66c4d6a18950e3d4
Payton Swick <payton@foolord.com>
2020-01-29 14:38:01 -0500

    Allow overriding redirect functions in Checkout

--- client/my-sites/checkout/checkout/index.jsx
+++ client/my-sites/checkout/checkout/index.jsx
@@ -113,6 +113,13 @@ export class Checkout extends React.Component {
 		clearPurchases: PropTypes.func.isRequired,
 		fetchReceiptCompleted: PropTypes.func.isRequired,
 		previousRoute: PropTypes.string.isRequired,
+		performRedirectTo: PropTypes.func,
+		performRedirectAndReplaceUrlTo: PropTypes.func,
+	};
+
+	static defaultProps = {
+		performRedirectTo: url => page( url ),
+		performRedirectAndReplaceUrlTo: url => page.redirect( url ),
 	};
 
 	state = {
@@ -354,7 +361,7 @@ export class Checkout extends React.Component {
 			);
 		}
 
-		page.redirect( redirectTo );
+		this.props.performRedirectAndReplaceUrlTo( redirectTo );
 
 		return true;
 	}
@@ -739,7 +746,7 @@ export class Checkout extends React.Component {
 				fetchSitesAndUser(
 					domainName,
 					() => {
-						page( redirectPath );
+						this.props.performRedirectTo( redirectPath );
 					},
 					reduxStore
 				);
@@ -748,7 +755,7 @@ export class Checkout extends React.Component {
 			}
 		}
 
-		page( redirectPath );
+		this.props.performRedirectTo( redirectPath );
 	};
 
 	content() {
