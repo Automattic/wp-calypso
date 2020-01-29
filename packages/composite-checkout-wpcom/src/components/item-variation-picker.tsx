bc870a1bf2c0c141e6cb29ccb8a6dbcaf76c1214
nbloomf <nathan.bloomfield@automattic.com>
2020-01-29 14:19:59 -0600

    Add ItemVariationPicker component
    This component renders a list of radio buttons used to display and select from a list of 'item variants'. Currently the only meaningful item variant in WPCOM is the plan length selector, but this component is designed to allow arbitrary kinds of variants. The `WPCOMProductVariant` type represents one of a list of variations on a given product; the label is the text representing the variant and the description component is for displaying the price, discount graphics, etc.
    
    Note the `getItemVariants` prop: this is a function from product slugs to product variants, and what allows this component to be generic to the product type. We'll have to construct this function and pass it in here. This requires touching a lot of global state, so we handle it at the CompositeCheckout component.

---
+++ packages/composite-checkout-wpcom/src/components/item-variation-picker.tsx
@@ -0,0 +1,108 @@
+/**
+ * External dependencies
+ */
+import React, { Component, FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+import { useTranslate } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import { WPCOMCartItem } from '../types';
+import RadioButton from './radio-button';
+
+export type WPCOMProductSlug = string;
+
+export type WPCOMProductVariant = {
+	variantLabel: string;
+	variantDetails: Component;
+	productSlug: WPCOMProductSlug;
+	productId: number;
+};
+
+export type ItemVariationPickerProps = {
+	selectedItem: WPCOMCartItem;
+	getItemVariants: ( WPCOMProductSlug ) => WPCOMProductVariant[];
+	onChangeItemVariant: ( WPCOMCartItem, WPCOMProductSlug, number ) => void;
+};
+
+export const ItemVariationPicker: FunctionComponent< ItemVariationPickerProps > = ( {
+	selectedItem,
+	getItemVariants,
+	onChangeItemVariant,
+} ) => {
+	const variants = getItemVariants( selectedItem.wpcom_meta.product_slug );
+
+	if ( variants.length < 2 ) {
+		return null;
+	}
+
+	return (
+		<TermOptions>
+			{ variants.map( renderProductVariant( selectedItem, onChangeItemVariant ) ) }
+		</TermOptions>
+	);
+};
+
+function renderProductVariant(
+	selectedItem: WPCOMCartItem,
+	onChangeItemVariant: ( WPCOMCartItem, WPCOMProductSlug, number ) => void
+): ( _0: WPCOMProductVariant, _1: number ) => JSX.Element {
+	return (
+		{ variantLabel, variantDetails, productSlug, productId }: WPCOMProductVariant,
+		index: number
+	) => {
+		const translate = useTranslate();
+		const key = index.toString() + productSlug;
+		const selectedProductSlug = selectedItem.wpcom_meta.product_slug;
+
+		return (
+			<TermOptionsItem key={ key }>
+				<RadioButton
+					name={ key }
+					id={ key }
+					value={ productSlug }
+					checked={ productSlug === selectedProductSlug }
+					onChange={ () => {
+						onChangeItemVariant( selectedItem, productSlug, productId );
+					} }
+					ariaLabel={ translate( 'Select a different term length' ) as string }
+					label={
+						<React.Fragment>
+							<VariantLabel>{ variantLabel }</VariantLabel>
+							{ variantDetails }
+						</React.Fragment>
+					}
+					children={ [] }
+				/>
+			</TermOptionsItem>
+		);
+	};
+}
+
+const TermOptions = styled.ul`
+	flex-basis: 100%;
+	margin: 12px 0 0;
+	padding: 0;
+`;
+
+const TermOptionsItem = styled.li`
+	margin: 8px 0 0;
+	padding: 0;
+	list-style: none;
+
+	:first-of-type {
+		margin-top: 0;
+	}
+`;
+
+const VariantLabel = styled.span`
+	flex: 1;
+`;
+
+/*
+const Discount = styled.span`
+	color: ${props => props.theme.colors.discount};
+	margin-right: 8px;
+`;
+*/
