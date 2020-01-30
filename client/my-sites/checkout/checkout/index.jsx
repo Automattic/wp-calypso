01bc21a894ab8dea17777bcf71b5c8415715d255
Payton Swick <payton@foolord.com>
2020-01-29 19:51:26 -0500

    Clarify logic around isEligibleForSignupDestinationRedirect

--- client/my-sites/checkout/checkout/index.jsx
+++ client/my-sites/checkout/checkout/index.jsx
@@ -1003,10 +1003,9 @@ function getCheckoutCompleteRedirectPath( {
 	// Display mode is used to show purchase specific messaging, for e.g. the Schedule Session button
 	// when purchasing a concierge session.
 	const displayModeParam = getDisplayModeParamFromCart( cart );
-	if ( isEligibleForSignupDestinationRedirect ) {
-		return getUrlWithQueryParam( signupDestination || fallbackUrl, displayModeParam );
+	if ( isEligibleForSignupDestinationRedirect && signupDestination ) {
+		return getUrlWithQueryParam( signupDestination, displayModeParam );
 	}
-
 	return getUrlWithQueryParam( fallbackUrl, displayModeParam );
 }
 
