69e8d84771fa7eeddd6a90aea667eb95cb4cf175
Rob Landers <landers.robert@gmail.com>
2020-02-04 10:13:06 +0100

    Default variation ignores the name

--- client/components/experiment/default-variation.ts
+++ client/components/experiment/default-variation.ts
@@ -7,5 +7,5 @@ import Variation from './variation';
  * Overrides a regular variation, and adds `null` and `undefined` as possible variations to display
  */
 export default class DefaultVariation extends Variation {
-	acceptedVariation = [ null, undefined, this.props.name ];
+	acceptedVariation = [ null, undefined ];
 }
