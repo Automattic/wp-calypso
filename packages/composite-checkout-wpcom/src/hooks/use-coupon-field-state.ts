cbe7e3a5c0faa14ed375471be18c260e21af2b66
nbloomf <nathan.bloomfield@automattic.com>
2020-02-04 12:51:54 -0600

    Add submit handler to useCouponFieldState

--- packages/composite-checkout-wpcom/src/hooks/use-coupon-field-state.ts
+++ packages/composite-checkout-wpcom/src/hooks/use-coupon-field-state.ts
@@ -1,18 +1,19 @@
 /**
  * External dependencies
  */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useEvents } from '@automattic/composite-checkout';
 
 export type CouponFieldStateProps = {
 	couponFieldValue: string;
 	setCouponFieldValue: ( string ) => void;
 	isApplyButtonActive: boolean;
-	setIsApplyButtonActive: ( boolean ) => void;
 	isFreshOrEdited: boolean;
 	setIsFreshOrEdited: ( boolean ) => void;
 };
 
-export default function useCouponFieldState(): CouponFieldStateProps {
+export default function useCouponFieldState( submitCoupon ): CouponFieldStateProps {
+	const onEvent = useEvents();
 	const [ couponFieldValue, setCouponFieldValue ] = useState< string >( '' );
 
 	// Used to hide the `Apply` button
@@ -21,12 +22,44 @@ export default function useCouponFieldState(): CouponFieldStateProps {
 	// Used to hide error messages if the user has edited the form field
 	const [ isFreshOrEdited, setIsFreshOrEdited ] = useState< boolean >( true );
 
+	useEffect( () => {
+		if ( couponFieldValue.length > 0 ) {
+			setIsApplyButtonActive( true );
+			return;
+		}
+		setIsApplyButtonActive( false );
+	}, [ couponFieldValue ] );
+
+	function handleCouponSubmit( event ) {
+		event.preventDefault();
+		if ( isCouponValid( couponFieldValue ) ) {
+			onEvent( {
+				type: 'a8c_checkout_add_coupon',
+				payload: { coupon: couponFieldValue },
+			} );
+
+			submitCoupon( couponFieldValue );
+
+			return;
+		}
+
+		onEvent( {
+			type: 'a8c_checkout_add_coupon_error',
+			payload: { type: 'Invalid code' },
+		} );
+	}
+
 	return {
 		couponFieldValue,
 		setCouponFieldValue,
 		isApplyButtonActive,
-		setIsApplyButtonActive,
 		isFreshOrEdited,
 		setIsFreshOrEdited,
+		handleCouponSubmit,
 	};
+}
+
+function isCouponValid( coupon ) {
+	// TODO: figure out some basic validation here
+	return coupon.match( /^[a-zA-Z0-9_-]+$/ );
 }
