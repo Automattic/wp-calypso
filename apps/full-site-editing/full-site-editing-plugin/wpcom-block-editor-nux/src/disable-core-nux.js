7151b39cd4a67c7c377b4744de677801541d449d
Addison-Stavlo <stavz01@gmail.com>
2020-02-03 19:01:54 -0500

    added disable for welcomeGuide

--- apps/full-site-editing/full-site-editing-plugin/wpcom-block-editor-nux/src/disable-core-nux.js
+++ apps/full-site-editing/full-site-editing-plugin/wpcom-block-editor-nux/src/disable-core-nux.js
@@ -5,8 +5,16 @@
 import { select, dispatch, subscribe } from '@wordpress/data';
 import '@wordpress/nux'; //ensure nux store loads
 
-dispatch( 'core/nux' ).disableTips();
+// Disable nux and welcome guide features from core.
+const unsubscribe = subscribe( () => {
+	dispatch( 'core/nux' ).disableTips();
+	if ( select( 'core/edit-post' ).isFeatureActive( 'welcomeGuide' ) ) {
+		dispatch( 'core/edit-post' ).toggleFeature( 'welcomeGuide' );
+	}
+	unsubscribe();
+} );
 
+// Listen for these features being triggered to call dotcom nux instead.
 subscribe( () => {
 	if ( select( 'core/nux' ).areTipsEnabled() ) {
 		dispatch( 'core/nux' ).disableTips();
