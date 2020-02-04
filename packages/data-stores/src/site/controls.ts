ccd6518373754b8c90b1029c3688c86a833b7b39
ramonjd <ramonjd@gmail.com>
2020-02-04 13:42:59 +1100

    Returning the promise. An async function is going to return a promise anyways, so unwrapping to return the result wrapped in a promise should be the same as return wpcomRequest( /* … */ ) See comment: https://github.com/Automattic/wp-calypso/pull/39050/files#r372356504

--- packages/data-stores/src/site/controls.ts
+++ packages/data-stores/src/site/controls.ts
@@ -8,39 +8,30 @@ export default function createControls( clientCreds: WpcomClientCredentials ) {
 	return {
 		[ ActionType.CREATE_SITE ]: async ( action: CreateSiteAction ) => {
 			const { authToken, ...providedParams } = action.params;
-			const defaultParams = {
-				blog_title: '',
-				public: -1,
-				blog_name: '',
-				client_id: clientCreds.client_id,
-				client_secret: clientCreds.client_secret,
-				lang_id: 1,
-				locale: 'en',
-				options: {
-					theme: 'pub/maywood',
-					site_segment: 1,
-					site_vertical: 'p1',
-					site_vertical_name: 'Restaurant',
-					site_information: {
-						title: 'Test Site for Gutenboarding',
-					},
-					site_creation_flow: 'gutenboarding',
-				},
-			};
-			const mergedParams = Object.assign(
-				{},
-				defaultParams,
-				{ ...providedParams },
-				{ validate: false } // Set to false because account validation should be a separate action
-			);
-			const newUser = await wpcomRequest( {
-				path: '/sites/new',
-				apiVersion: '1.1',
-				method: 'post',
-				body: mergedParams,
-				token: authToken || undefined,
-			} );
-			return newUser;
-		},
+			
+            const defaultParams = {
+                client_id: clientCreds.client_id,
+                client_secret: clientCreds.client_secret,
+                // will find an available `*.wordpress.com` url based on the `blog_name`
+                find_available_url: true,
+                // Private site is default, but overridable, setting
+                public: -1,
+            };
+
+            const mergedParams = {
+                ...defaultParams,
+                ...providedParams,
+                // Set to false because site validation should be a separate action
+                validate: false,
+            };
+
+            return wpcomRequest( {
+                path: '/sites/new',
+                apiVersion: '1.1',
+                method: 'post',
+                token: authToken || undefined,
+                body: mergedParams,
+            } );
+        },
 	};
 }
