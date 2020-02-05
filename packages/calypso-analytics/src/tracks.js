5d78bb61f22fe7ac21f0ae88152563d43534a13f
Eru Penkman <roo2@protonmail.com>
2020-02-05 16:12:41 +1300

    dont export generateRandomId from calypso-analytics

--- packages/calypso-analytics/src/tracks.js
+++ packages/calypso-analytics/src/tracks.js
@@ -40,6 +40,21 @@ if ( typeof document !== 'undefined' ) {
 	_loadTracksResult = loadScript( '//stats.wp.com/w.js?60' );
 }
 
+function createRandomId( randomBytesLength = 9 ) {
+	// 9 * 4/3 = 12
+	// this is to avoid getting padding of a random byte string when it is base64 encoded
+	let randomBytes;
+
+	if ( window.crypto && window.crypto.getRandomValues ) {
+		randomBytes = new Uint8Array( randomBytesLength );
+		window.crypto.getRandomValues( randomBytes );
+	} else {
+		randomBytes = times( randomBytesLength, () => Math.floor( Math.random() * 256 ) );
+	}
+
+	return window.btoa( String.fromCharCode.apply( String, randomBytes ) );
+}
+
 function getUrlParameter( name ) {
 	name = name.replace( /[[]/g, '\\[' ).replace( /[\]]/g, '\\]' );
 	const regex = new RegExp( '[\\?&]' + name + '=([^&#]*)' );
@@ -80,21 +95,6 @@ function checkForBlockedTracks() {
 }
 
 export const analyticsEvents = new EventEmitter();
-
-export function createRandomId( randomBytesLength = 9 ) {
-	// 9 * 4/3 = 12
-	// this is to avoid getting padding of a random byte string when it is base64 encoded
-	let randomBytes;
-
-	if ( window.crypto && window.crypto.getRandomValues ) {
-		randomBytes = new Uint8Array( randomBytesLength );
-		window.crypto.getRandomValues( randomBytes );
-	} else {
-		randomBytes = times( randomBytesLength, () => Math.floor( Math.random() * 256 ) );
-	}
-
-	return window.btoa( String.fromCharCode.apply( String, randomBytes ) );
-}
 
 /**
  * Returns the anoymous id stored in the `tk_ai` cookie
