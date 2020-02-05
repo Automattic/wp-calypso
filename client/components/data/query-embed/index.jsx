d26469269e46a7564e12588d612f6489377238dd
Marin Atanasov <tyxla@abv.bg>
2020-02-05 14:03:49 +0200

    Components: Simplify QueryEmbed by using hooks

--- client/components/data/query-embed/index.jsx
+++ client/components/data/query-embed/index.jsx
@@ -1,36 +1,20 @@
 /**
  * External dependencies
  */
-import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import { requestEmbed } from 'state/embeds/actions';
 
-class QueryEmbeds extends Component {
-	static propTypes = {
-		siteId: PropTypes.number.isRequired,
-		url: PropTypes.string.isRequired,
-	};
+export default function QueryEmbed( { siteId, url } ) {
+	const dispatch = useDispatch();
 
-	componentDidMount() {
-		this.props.requestEmbed( this.props.siteId, this.props.url );
-	}
+	React.useEffect( () => {
+		dispatch( requestEmbed( siteId, url ) );
+	}, [ dispatch, siteId, url ] );
 
-	componentDidUpdate( prevProps ) {
-		if ( prevProps.siteId !== this.props.siteId || prevProps.url !== this.props.url ) {
-			this.props.requestEmbed( this.props.siteId, this.props.url );
-		}
-	}
-
-	render() {
-		return null;
-	}
+	return null;
 }
-
-export default connect( null, {
-	requestEmbed,
-} )( QueryEmbeds );
