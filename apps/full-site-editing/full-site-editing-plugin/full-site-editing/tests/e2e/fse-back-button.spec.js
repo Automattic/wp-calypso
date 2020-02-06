0b928801e0f258391209a66818394059142fcdc4
Noah Allen <noahtallen@gmail.com>
2020-02-06 14:13:32 -0800

    Add an e2e test stub

---
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing/tests/e2e/fse-back-button.spec.js
@@ -0,0 +1,10 @@
+/**
+ * External dependencies
+ */
+
+// @TODO: Flesh out this file. This exists to make sure test scaffolding works.
+describe( 'Full Site Editing Back Button', () => {
+	it( 'Should have a chevron icon', () => {
+		expect( true ).toBe( true );
+	} );
+} );
