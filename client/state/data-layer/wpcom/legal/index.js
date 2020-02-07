7d0363865caceefc0ba12f692da7fb1c18af669a
Miguel Torres <miguel.torres@automattic.com>
2020-02-07 11:43:05 +0100

    Display ToS update notice (#39297)
    * Adds a selector that checks if user has accepted the current ToS.
    * Adds an action for accepting current ToS.
    * Displays a ToS update notice if user has not accepted the current ToS.
    * Dispatches an action for accepting current ToS when user clicks on "Accept".

--- client/state/data-layer/wpcom/legal/index.js
+++ client/state/data-layer/wpcom/legal/index.js
@@ -4,7 +4,7 @@
 import { http } from 'state/data-layer/wpcom-http/actions';
 import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
 import { registerHandlers } from 'state/data-layer/handler-registry';
-import { LEGAL_REQUEST } from 'state/action-types';
+import { LEGAL_REQUEST, TOS_ACCEPT } from 'state/action-types';
 import { setLegalData } from 'state/legal/actions';
 
 const requestLegalData = action => {
@@ -30,10 +30,31 @@ const formatLegalData = ( { tos: { accepted, active_date, display_prompt } } ) =
 	};
 };
 
+const acceptTos = action => {
+	return http(
+		{
+			method: 'POST',
+			path: `/legal`,
+			apiNamespace: 'wpcom/v2',
+			body: {
+				action: 'accept_tos',
+			},
+		},
+		action
+	);
+};
+
 registerHandlers( 'state/data-layer/legal/index.js', {
 	[ LEGAL_REQUEST ]: [
 		dispatchRequest( {
 			fetch: requestLegalData,
+			onSuccess: storeLegalData,
+			fromApi: formatLegalData,
+		} ),
+	],
+	[ TOS_ACCEPT ]: [
+		dispatchRequest( {
+			fetch: acceptTos,
 			onSuccess: storeLegalData,
 			fromApi: formatLegalData,
 		} ),
