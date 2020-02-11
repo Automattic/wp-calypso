dd0862f8f481d318a0dcd8fbee2207aa42405b13
Jon Surrell <jon.surrell@automattic.com>
2020-02-11 22:21:02 +0100

    Add easy debugging

--- client/landing/gutenboarding/index.tsx
+++ client/landing/gutenboarding/index.tsx
@@ -2,8 +2,9 @@
  * External dependencies
  */
 import '@automattic/calypso-polyfills';
+import { setLocaleData } from '@wordpress/i18n';
 import { I18nContext } from '@automattic/react-i18n';
-import React from 'react';
+import React, { useState, FunctionComponent } from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import config from '../../config';
@@ -30,12 +31,63 @@ window.AppBoot = () => {
 		accessibleFocus();
 
 		ReactDom.render(
-			<I18nContext.Provider value={ 'en' }>
+			<CalypsoI18n>
 				<BrowserRouter basename="gutenboarding">
 					<Gutenboard />
 				</BrowserRouter>
-			</I18nContext.Provider>,
+			</CalypsoI18n>,
 			document.getElementById( 'wpcom' )
 		);
 	}
 };
+
+const CalypsoI18n: FunctionComponent = ( { children } ) => {
+	const [ locale, setLocale ] = useState< string >( 'en' );
+
+	// TODO: Hack for demonstration
+	( window as any ).changeLocale = ( localeSlug: string ) => {
+		changeLocale( localeSlug ).then( setLocale );
+	};
+	return <I18nContext.Provider value={ locale }>{ children }</I18nContext.Provider>;
+};
+
+function getLanguageFilePathUrl() {
+	const protocol = typeof window === 'undefined' ? 'https://' : '//'; // use a protocol-relative path in the browser
+
+	return `${ protocol }widgets.wp.com/languages/calypso/`;
+}
+
+function getLanguageFileUrl( localeSlug: string, fileType = 'json', languageRevisions = {} ) {
+	if ( ! [ 'js', 'json' ].includes( fileType ) ) {
+		fileType = 'json';
+	}
+
+	const revision = languageRevisions[ localeSlug ];
+	const fileUrl = `${ getLanguageFilePathUrl() }${ localeSlug }-v1.1.${ fileType }`;
+
+	return typeof revision === 'number' ? fileUrl + `?v=${ revision }` : fileUrl;
+}
+
+async function getLanguageFile( targetLocaleSlug: string ) {
+	const url = getLanguageFileUrl( targetLocaleSlug, 'json', window.languageRevisions || {} );
+
+	const response = await globalThis.fetch( url );
+	if ( response.ok ) {
+		if ( response.bodyUsed ) {
+			// If the body was already used, we assume that we already parsed the
+			// response and set the locale in the DOM, so we don't need to do anything
+			// else here.
+			return;
+		}
+		return await response.json();
+	}
+
+	// Invalid response.
+	throw new Error();
+}
+
+async function changeLocale( localeSlug: string ) {
+	const localeData = await getLanguageFile( localeSlug );
+	setLocaleData( localeData );
+	return localeSlug;
+}
