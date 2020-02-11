be0c2ad7d2063a0bca05e0395bfef2c0da036df9
Payton Swick <payton@foolord.com>
2020-02-10 19:26:33 -0500

    Force paypal method submitTransaction to be added after creation

--- packages/composite-checkout/src/lib/payment-methods/paypal.js
+++ packages/composite-checkout/src/lib/payment-methods/paypal.js
@@ -28,19 +28,18 @@ export function createPayPalMethod( { registerStore } ) {
 
 	registerStore( 'paypal', {
 		controls: {
-			PAYPAL_TRANSACTION_SUBMIT( action ) {
-				debug( 'submitting paypal transaction' );
+			PAYPAL_TRANSACTION_SUBMIT() {
 				if ( ! paymentMethod.submitTransaction ) {
-					throw new Error( 'PayPal Payment Method object missing a submitTransaction method' );
+					throw new Error( 'PayPal payment method does not have a submitTransaction function' );
 				}
-				return paymentMethod.submitTransaction( action.payload );
+				return paymentMethod.submitTransaction();
 			},
 		},
 		actions: {
 			*submitPaypalPayment( payload ) {
 				try {
 					yield { type: 'PAYPAL_TRANSACTION_BEGIN', payload };
-					const paypalResponse = yield { type: 'PAYPAL_TRANSACTION_SUBMIT', payload };
+					const paypalResponse = yield { type: 'PAYPAL_TRANSACTION_SUBMIT' };
 					debug( 'received successful paypal endpoint response', paypalResponse );
 					return { type: 'PAYPAL_TRANSACTION_END', payload: paypalResponse };
 				} catch ( error ) {
@@ -72,13 +71,7 @@ export function createPayPalMethod( { registerStore } ) {
 		},
 	} );
 
-	return {
-		id: 'paypal',
-		label: <PaypalLabel />,
-		submitButton: <PaypalSubmitButton />,
-		inactiveContent: <PaypalSummary />,
-		getAriaLabel: localize => localize( 'PayPal' ),
-	};
+	return paymentMethod;
 }
 
 export function PaypalLabel() {
@@ -97,14 +90,10 @@ export function PaypalLabel() {
 export function PaypalSubmitButton( { disabled } ) {
 	const localize = useLocalize();
 	const { submitPaypalPayment } = useDispatch( 'paypal' );
-	const [ items ] = useLineItems();
 	useTransactionStatusHandler();
 	const { formStatus } = useFormStatus();
 
-	const onClick = () =>
-		submitPaypalPayment( {
-			items,
-		} );
+	const onClick = () => submitPaypalPayment();
 	return (
 		<Button
 			disabled={ disabled }
