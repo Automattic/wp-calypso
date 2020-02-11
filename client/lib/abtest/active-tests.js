c3480ac6ab84378f7c528e841c1647dfe4277264
niranjan-uma-shankar <niranjan.u@gmail.com>
2020-02-11 15:35:11 +0530

    Create new A/B test (#39276)

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -145,4 +145,13 @@ export default {
 		defaultVariation: 'control',
 		allowExistingUsers: true,
 	},
+	domainStepDesignUpdates: {
+		datestamp: '20200205',
+		variations: {
+			variantDesignUpdates: 50,
+			control: 50,
+		},
+		defaultVariation: 'control',
+		allowExistingUsers: true,
+	},
 };
