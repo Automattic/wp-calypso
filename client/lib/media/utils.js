79feae4f578b1b3f98538b7dd644273f077010e8
Marin Atanasov <tyxla@abv.bg>
2020-02-11 13:10:39 +0200

    Media: Move validateMediaItem as a media util

--- client/lib/media/utils.js
+++ client/lib/media/utils.js
@@ -17,6 +17,7 @@ import {
 	GalleryColumnedTypes,
 	GallerySizeableTypes,
 	GalleryDefaultAttrs,
+	ValidationErrors as MediaValidationErrors,
 } from './constants';
 import { stringify } from 'lib/shortcode';
 import impureLodash from 'lib/impure-lodash';
@@ -587,4 +588,33 @@ export function createTransientMedia( file ) {
 	}
 
 	return transientMedia;
+}
+
+/**
+ * Validates a media item for a site, and returns validation errors (if any).
+ *
+ * @param  {object}      site Site object
+ * @param  {object}      item Media item
+ * @returns {Array|null}      Validation errors, or null if no site.
+ */
+export function validateMediaItem( site, item ) {
+	const itemErrors = [];
+
+	if ( ! site ) {
+		return;
+	}
+
+	if ( ! isSupportedFileTypeForSite( item, site ) ) {
+		if ( isSupportedFileTypeInPremium( item, site ) ) {
+			itemErrors.push( MediaValidationErrors.FILE_TYPE_NOT_IN_PLAN );
+		} else {
+			itemErrors.push( MediaValidationErrors.FILE_TYPE_UNSUPPORTED );
+		}
+	}
+
+	if ( true === isExceedingSiteMaxUploadSize( item, site ) ) {
+		itemErrors.push( MediaValidationErrors.EXCEEDS_MAX_UPLOAD_SIZE );
+	}
+
+	return itemErrors;
 }
