490b3153d9fd672d2ca326b1356c8899365978fe
Sérgio Gomes <mail@sgomes.com>
2020-02-13 18:50:25 +0000

    Fix modularised state on the server.
    The server registers a new store for each request, so some of the
    assumptions we were making on the client side are no longer true.

--- client/state/redux-store.ts
+++ client/state/redux-store.ts
@@ -6,34 +6,35 @@ import { Reducer, Store } from 'redux';
 /**
  * Internal dependencies
  */
-import { addReducerToStore, WithAddReducer } from './add-reducer';
+import { addReducerToStore, clear as clearReducers, WithAddReducer } from './add-reducer';
 
 type QueueEntry = [ string[], Reducer ];
 
 let applicationStore: ( Store & WithAddReducer ) | undefined;
-let reducerRegistrationQueue: QueueEntry[] = [];
+const reducerRegistrationQueue: QueueEntry[] = [];
 
 export function setStore( store: Store & WithAddReducer ) {
-	// Only set store once.
+	// Clear any previously added reducers when replacing an existing store.
 	if ( applicationStore ) {
-		return;
+		clearReducers();
 	}
 
 	applicationStore = store;
+
 	// Synchronously add all pending reducers.
+	// These include reducers registered to previous stores, since their code has
+	// already been loaded.
 	for ( const [ key, reducer ] of reducerRegistrationQueue ) {
 		addReducerToStore( applicationStore )( key, reducer );
 	}
-	// Clear registration queue to avoid dangling references.
-	reducerRegistrationQueue = [];
 }
 
 export function registerReducer( key: string[], reducer: Reducer ) {
 	if ( applicationStore ) {
 		// Register immediately.
 		addReducerToStore( applicationStore )( key, reducer );
-		return;
 	}
 
+	// Add to queue, for future stores.
 	reducerRegistrationQueue.push( [ key, reducer ] );
 }
