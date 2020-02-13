fa264279bee85b1ad53010f82fd6a644521cd35e
Enej Bajgoric <enej.bajgoric@automattic.com>
2020-02-13 11:06:15 +0100

    Add the Example LogItem to Scan Page

--- client/landing/jetpack-cloud/controller.js
+++ client/landing/jetpack-cloud/controller.js
@@ -3,11 +3,6 @@
  */
 import React from 'react';
 
-/**
- * Internal Dependencies
- */
-import LogItem from './components/log-item';
-
 export function jetpackCloud( context, next ) {
 	context.primary = <div>Hi, this is the Jetpack.com Cloud!</div>;
 	next();
--- client/landing/jetpack-cloud/sections/scan/main.js
+++ client/landing/jetpack-cloud/sections/scan/main.js
@@ -3,9 +3,35 @@
  */
 import React, { Component } from 'react';
 
+/**
+ * Internal Dependencies
+ */
+import LogItem from './components/log-item';
+
 class ScanPage extends Component {
 	render() {
-		return <div>This is the Jetpack Scan landing page! { this.props.siteId } site</div>;
+		return (
+			<div>
+				<p>This is the Jetpack Scan landing page! { this.props.siteId } site</p>
+				<LogItem
+					header="Unexpected core file: sx--a4bp.php"
+					subheader="Threat found on 14 September, 2019"
+				>
+					<h3>Item Header</h3>
+					<p>Foo</p>
+					<h3>Item Header 2</h3>
+					<p>Bar</p>
+				</LogItem>
+				<LogItem
+					header="Unexpected core file: sx--a4bp.php"
+					subheader="Threat found on 14 September, 2019"
+					tag="critical"
+					highlight="error"
+				>
+					Hello
+				</LogItem>
+			</div>
+		);
 	}
 }
 
