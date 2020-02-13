c1c158cea72acc7e453559168553301b7e2b5063
Piotr Delawski <piotr.delawski@automattic.com>
2020-02-13 13:41:16 +0100

    Use a more accurate masterbar link title

--- client/landing/jetpack-cloud/components/masterbar/index.jsx
+++ client/landing/jetpack-cloud/components/masterbar/index.jsx
@@ -24,7 +24,7 @@ export default function() {
 			<Item
 				className="masterbar__item-home"
 				url="/"
-				tooltip={ translate( 'Jetpack Cloud Homepage', {
+				tooltip={ translate( 'Jetpack Cloud Dashboard', {
 					comment: 'Jetpack Cloud masterbar nav item',
 				} ) }
 			>
