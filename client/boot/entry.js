e23a37e69e91e529b1558b576b9a897ace03546d
Enej Bajgoric <enej.bajgoric@automattic.com>
2020-02-14 14:31:32 +0100

    Also add the file

---
+++ client/boot/entry.js
@@ -0,0 +1,27 @@
+/**
+ * External dependencies
+ */
+import page from 'page';
+
+/**
+ * Internal dependencies
+ */
+import { abtest } from 'lib/abtest'; // used by error logger
+import userFactory from 'lib/user';
+
+export default function() {
+	page( '/', context => {
+		const user = userFactory();
+		const { primarySiteSlug } = user.get();
+		let redirectPath =
+			primarySiteSlug && 'variant' === abtest( 'redirectToCustomerHome' )
+				? `/home/${ primarySiteSlug }`
+				: '/read';
+
+		if ( context.querystring ) {
+			redirectPath += `?${ context.querystring }`;
+		}
+
+		page.redirect( redirectPath );
+	} );
+}
