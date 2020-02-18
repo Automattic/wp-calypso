f3ca829a3730ef2832a1a240aca921683b63ae59
Yan Sern <yan.sern@automattic.com>
2020-02-18 20:11:02 +0100

    Revert type changes and newline that was accidentally removed during merge.

--- client/landing/gutenboarding/components/link/index.tsx
+++ client/landing/gutenboarding/components/link/index.tsx
@@ -5,6 +5,7 @@ import { Button } from '@wordpress/components';
 import { Link as RouterLink, LinkProps } from 'react-router-dom';
 import React, { forwardRef, FunctionComponent } from 'react';
 import { Assign } from 'utility-types';
+
 interface LinkButtonProps extends Button.AnchorProps {
 	navigate: () => void;
 }
@@ -45,7 +46,7 @@ type Assign3< T extends object, U extends object, V extends object > = Assign< T
 // LinkProps _without_ component
 // LinkProps `to` is optional
 type Props = Omit<
-	Assign3< Button.AnchorProps, Omit< LinkProps, 'component' >, { to?: string } >,
+	Assign3< Button.AnchorProps, Omit< LinkProps, 'component' >, Partial< Pick< LinkProps, 'to' > > >,
 	'disabled' | 'href'
 >;
 const Link: FunctionComponent< Props > = ( { children, ...props } ) => {
