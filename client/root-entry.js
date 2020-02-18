a45cfa8b0a0c8387f8bfe9cd9dd8d33d887fe9e5
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-02-18 17:34:52 -0600

    Add logged out and logged in logic to root-entry.js

--- client/root-entry.js
+++ client/root-entry.js
@@ -6,10 +6,48 @@ import page from 'page';
 /**
  * Internal dependencies
  */
+import config from 'config';
 import { abtest } from 'lib/abtest';
 import userFactory from 'lib/user';
 
 export default function() {
+	// TODO: Remove Jetpack Cloud specific logic when root route is no longer handled by the reader section
+	const jetpackCloudEnvs = [
+		'jetpack-cloud-development',
+		'jetpack-cloud-stage',
+		'jetpack-cloud-production',
+	];
+	const calypsoEnv = config( 'env_id' );
+	if ( jetpackCloudEnvs.includes( calypsoEnv ) ) {
+		return;
+	}
+
+	const user = userFactory();
+	const isLoggedOut = ! user.get();
+	if ( isLoggedOut ) {
+		setupLoggedOut();
+	} else {
+		setupLoggedIn();
+	}
+}
+
+function setupLoggedOut() {
+	if ( config.isEnabled( 'desktop' ) ) {
+		page( '/', () => {
+			if ( config.isEnabled( 'oauth' ) ) {
+				page.redirect( '/authorize' );
+			} else {
+				page.redirect( '/log-in' );
+			}
+		} );
+	} else if ( config.isEnabled( 'devdocs/redirect-loggedout-homepage' ) ) {
+		page( '/', () => {
+			page.redirect( '/devdocs/start' );
+		} );
+	}
+}
+
+function setupLoggedIn() {
 	page( '/', context => {
 		const user = userFactory();
 		const { primarySiteSlug } = user.get();
