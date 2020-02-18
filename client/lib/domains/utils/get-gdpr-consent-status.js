60df08ee190587610dd0500ba996e4abda40ed17
Sérgio Gomes <mail@sgomes.com>
2020-02-18 11:20:05 +0000

    Modularise lib/domains/utils (#39483)
    * Move utils.js to utils/index.js
    
    Also adds a `package.json` with `sideEffects: false` to prepare
    for modularization.
    
    * Modularize lib/domains/utils.

---
+++ client/lib/domains/utils/get-gdpr-consent-status.js
@@ -0,0 +1,25 @@
+/**
+ * Internal dependencies
+ */
+import { gdprConsentStatus } from 'lib/domains/constants';
+
+export function getGdprConsentStatus( domainFromApi ) {
+	switch ( domainFromApi.gdpr_consent_status ) {
+		case 'NONE':
+			return gdprConsentStatus.NONE;
+		case 'PENDING':
+			return gdprConsentStatus.PENDING;
+		case 'PENDING_ASYNC':
+			return gdprConsentStatus.PENDING_ASYNC;
+		case 'ACCEPTED_CONTRACTUAL_MINIMUM':
+			return gdprConsentStatus.ACCEPTED_CONTRACTUAL_MINIMUM;
+		case 'ACCEPTED_FULL':
+			return gdprConsentStatus.ACCEPTED_FULL;
+		case 'DENIED':
+			return gdprConsentStatus.DENIED;
+		case 'FORCED_ALL_CONTRACTUAL':
+			return gdprConsentStatus.FORCED_ALL_CONTRACTUAL;
+		default:
+			return null;
+	}
+}
---
+++ client/lib/domains/utils/index.js
@@ -0,0 +1,7 @@
+export { getDomainType } from 'lib/domains/utils/get-domain-type';
+export { getTransferStatus } from 'lib/domains/utils/get-transfer-status';
+export { getGdprConsentStatus } from 'lib/domains/utils/get-gdpr-consent-status';
+export { getDomainRegistrationAgreementUrl } from 'lib/domains/utils/get-domain-registration-agreement-url';
+export { isDomainConnectAuthorizePath } from 'lib/domains/utils/is-domain-connect-authorize-path';
+export { parseDomainAgainstTldList } from 'lib/domains/utils/parse-domain-against-tld-list';
+export { isRecentlyRegistered } from 'lib/domains/utils/is-recently-registered';
