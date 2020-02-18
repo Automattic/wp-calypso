d197f63a94574d8c13f2250c4546830aa3bdaade
Addison-Stavlo <stavz01@gmail.com>
2020-02-18 18:54:07 -0500

    added build-styles

--- client/landing/gutenboarding/onboarding-block/design-selector/dynamic-preview.js
+++ client/landing/gutenboarding/onboarding-block/design-selector/dynamic-preview.js
@@ -11,6 +11,9 @@ import React from 'react';
  */
 import { BlockPreview } from '@wordpress/block-editor';
 import { parse as parseBlocks } from '@wordpress/blocks';
+import '@wordpress/block-library/build-style/style.css';
+import '@wordpress/block-library/build-style/theme.css';
+import '@wordpress/block-library/build-style/editor.css';
 
 const BlockTemplatePreview = ( { design } ) => {
 	const blocks = parseBlocks( design?.content );
