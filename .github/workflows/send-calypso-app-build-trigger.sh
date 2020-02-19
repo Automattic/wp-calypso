9a4b8c60a9fe9223fc24f5555dce7ef0bf6b9878
Noah Allen <noahtallen@gmail.com>
2020-02-18 16:00:50 -0800

    Add script to send POST to mc

---
+++ .github/workflows/send-calypso-app-build-trigger.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+trigger_payload=`cat $GITHUB_EVENT_PATH`
+
+workflow_data="{
+	\"action\": \"$GITHUB_ACTION\",
+	\"actor\": \"$GITHUB_ACTOR\",
+	\"run_id\": \"$GITHUB_RUN_ID\",
+	\"run_num\": \"$GITHUB_RUN_NUMBER\",
+	\"repo\": \"$GITHUB_REPOSITORY\",
+	\"trigger_payload\": $trigger_payload
+}"
+
+curl -H "Content-Type: application/json" \
+	-d "$workflow_data" \
+	"$TRIGGER_CALYPSO_APP_BUILD_ENDPOINT?calypso_app=$CALYPSO_APP"
+
+exit 0
