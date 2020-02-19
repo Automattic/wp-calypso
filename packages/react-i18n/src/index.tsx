e625d6108042be8cd1cb6947f00833608ea219e1
Jon Surrell <jon.surrell@automattic.com>
2020-02-19 13:02:18 +0100

    React I18n: Simplify API (#39480)

--- packages/react-i18n/src/index.tsx
+++ packages/react-i18n/src/index.tsx
@@ -2,54 +2,37 @@
  * External dependencies
  */
 import * as React from 'react';
-import { __, _n, _nx, _x, setLocaleData } from '@wordpress/i18n';
+import { __, _n, _nx, _x } from '@wordpress/i18n';
+import { createHigherOrderComponent } from '@wordpress/compose';
+
+const DEFAULT_LOCALE = 'en';
 
 export interface I18nProps {
 	__: typeof __;
 	_n: typeof _n;
 	_nx: typeof _nx;
 	_x: typeof _x;
-	i18nLocale?: string;
+	i18nLocale: string;
 }
 
-const I18nContext = React.createContext< I18nProps >( { __, _n, _nx, _x } );
+const I18nContext = React.createContext< I18nProps >( makeI18n( DEFAULT_LOCALE ) );
 
 interface Props {
 	/**
 	 * The current locale
 	 */
 	locale: string;
-
-	/**
-	 * A callback that resolves with the translations data
-	 */
-	onLocaleChange?( newLocale: string | undefined ): Promise< object >;
 }
-export const I18nProvider: React.FunctionComponent< Props > = ( {
-	children,
-	locale,
-	onLocaleChange = () => Promise.resolve( {} ),
-} ) => {
+export const I18nProvider: React.FunctionComponent< Props > = ( { children, locale } ) => {
 	const [ contextValue, setContextValue ] = React.useState< I18nProps >( makeI18n( locale ) );
 	React.useEffect( () => {
-		let cancelled = false;
-		const cancel = () => {
-			cancelled = true;
-		};
-		onLocaleChange( locale ).then( nextLocaleData => {
-			if ( cancelled ) {
-				return;
-			}
-			setLocaleData( nextLocaleData );
-			setContextValue( makeI18n( locale ) );
-		} );
-		return cancel;
-	}, [ locale, onLocaleChange ] );
+		setContextValue( makeI18n( locale ) );
+	}, [ locale ] );
 	return <I18nContext.Provider value={ contextValue }>{ children }</I18nContext.Provider>;
 };
 
 function makeI18n( i18nLocale: string ) {
-	return { ...( i18nLocale && { i18nLocale } ), __, _n, _nx, _x };
+	return { __, _n, _nx, _x, i18nLocale };
 }
 
 /**
@@ -70,14 +53,10 @@ export const useI18n = (): I18nProps => {
 };
 
 /**
- * Remove from T the keys that are in common with K
- */
-type Optionalize< T extends K, K > = Omit< T, keyof K >;
-
-/**
  * React hook providing i18n translate functions
  *
- * @param WrappedComponent Component that will receive translate functions as props
+ * @param InnerComponent Component that will receive translate functions as props
+ * @returns Component enhanced with i18n context
  *
  * @example
  *
@@ -87,16 +66,9 @@ type Optionalize< T extends K, K > = Omit< T, keyof K >;
  * }
  * export default withI18n( MyComponent );
  */
-export const withI18n = < T extends I18nProps = I18nProps >(
-	WrappedComponent: React.ComponentType< T >
-): React.FunctionComponent< Optionalize< T, I18nProps > > => props => {
-	const i18n = useI18n();
-	return (
-		<WrappedComponent
-			{ ...i18n }
-			// Required cast `props as T`
-			// See https://github.com/Microsoft/TypeScript/issues/28938
-			{ ...( props as T ) }
-		/>
-	);
-};
+export const withI18n = createHigherOrderComponent< I18nProps >( InnerComponent => {
+	return props => {
+		const i18n = useI18n();
+		return <InnerComponent { ...i18n } { ...props } />;
+	};
+}, 'withI18n' );
