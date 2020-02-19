174d8b65c6a6d40a52fa7ee5cefa6cff371d272a
Kamen Stanev <hambai@gmail.com>
2020-02-19 16:50:20 +0200

    * fix the component to properly check if the state has loaded

--- client/my-sites/domains/domain-management/transfer/transfer-to-other-user/index.jsx
+++ client/my-sites/domains/domain-management/transfer/transfer-to-other-user/index.jsx
@@ -26,6 +26,7 @@ import DomainMainPlaceholder from 'my-sites/domains/domain-management/components
 import { successNotice, errorNotice } from 'state/notices/actions';
 import DesignatedAgentNotice from 'my-sites/domains/domain-management/components/designated-agent-notice';
 import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
+import { hasLoadedSiteDomains } from 'state/sites/domains/selectors';
 
 /**
  * Style dependencies
@@ -283,7 +284,11 @@ class TransferOtherUser extends React.Component {
 	}
 
 	isDataReady() {
-		return this.props.wapiDomainInfo.hasLoadedFromServer && ! this.props.isRequestingSiteDomains;
+		return (
+			this.props.hasSiteDomainsLoaded &&
+			this.props.wapiDomainInfo.hasLoadedFromServer &&
+			! this.props.isRequestingSiteDomains
+		);
 	}
 }
 
@@ -291,6 +296,7 @@ export default connect(
 	( state, ownProps ) => ( {
 		currentUser: getCurrentUser( state ),
 		isAtomic: isSiteAutomatedTransfer( state, ownProps.selectedSite.ID ),
+		hasSiteDomainsLoaded: hasLoadedSiteDomains( state, ownProps.selectedSite.ID ),
 	} ),
 	{
 		successNotice,
