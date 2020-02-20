49f7c4a82fd5a22664e059a7c54b1761fb465d74
Caleb Bauermeister <cbauerman@gmail.com>
2020-02-20 12:42:50 -0800

    reduce PR complexity, only show CurrentSite when site is selected

--- client/landing/jetpack-cloud/components/current-site/index.jsx
+++ client/landing/jetpack-cloud/components/current-site/index.jsx
@@ -9,7 +9,6 @@ import React from 'react';
  */
 import { Card } from '@automattic/components';
 import { getSelectedSite } from 'state/ui/selectors';
-import AllSites from 'blocks/all-sites';
 import Site from 'blocks/site';
 
 /**
@@ -20,17 +19,11 @@ import './style.scss';
 const CurrentSite = () => {
 	const selectedSite = useSelector( state => getSelectedSite( state ) );
 
-	return (
+	return selectedSite ? (
 		<Card className="current-site">
-			{ selectedSite ? (
-				<div>
-					<Site showHomeIcon={ false } site={ selectedSite } homeLink={ true } />
-				</div>
-			) : (
-				<AllSites />
-			) }
+			<Site showHomeIcon={ false } site={ selectedSite } />
 		</Card>
-	);
+	) : null;
 };
 
 export default CurrentSite;
