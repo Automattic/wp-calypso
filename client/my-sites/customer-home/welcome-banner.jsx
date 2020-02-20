a040c14c3c40b7280671380ed53ed2aedf91dfd7
Kirk Wight <kwight@kwight.ca>
2020-02-20 15:15:12 -0800

    Drop the button for a dismissible card instead.

--- client/my-sites/customer-home/welcome-banner.jsx
+++ client/my-sites/customer-home/welcome-banner.jsx
@@ -3,72 +3,46 @@
  */
 import React from 'react';
 import { connect } from 'react-redux';
-import { Card, Button } from '@automattic/components';
+import DismissibleCard from 'blocks/dismissible-card';
 import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import CardHeading from 'components/card-heading';
-import {
-	bumpStat,
-	composeAnalytics,
-	recordTracksEvent,
-	withAnalytics,
-} from 'state/analytics/actions';
-import { savePreference } from 'state/preferences/actions';
-import QueryPreferences from 'components/data/query-preferences';
-import { isFetchingPreferences, getPreference } from 'state/preferences/selectors';
+import { bumpStat, composeAnalytics, recordTracksEvent } from 'state/analytics/actions';
 
-export const WelcomeBanner = ( {
-	translate,
-	fetchingPreferences,
-	bannerDismissed,
-	dismissBanner,
-} ) => {
+export const WelcomeBanner = ( { translate, recordStats } ) => {
 	return (
-		<>
-			<QueryPreferences />
-			{ ! fetchingPreferences && ! bannerDismissed && (
-				<div className="customer-home__welcome-banner">
-					<Card>
-						<img
-							src="/calypso/images/extensions/woocommerce/woocommerce-setup.svg"
-							aria-hidden="true"
-							alt=""
-						/>
-						<div>
-							<CardHeading>{ translate( 'Learn and grow with My Home' ) }</CardHeading>
-							<p>
-								{ translate(
-									'This is your new home for quick links to common tasks, easy access to support, and guidance ' +
-										'tailored to you. We’ll keep improving what you see here to help you learn and grow with us.'
-								) }
-							</p>
-							<Button onClick={ dismissBanner }>{ translate( 'Got it!' ) }</Button>
-						</div>
-					</Card>
-				</div>
-			) }
-		</>
+		<DismissibleCard
+			className="customer-home__welcome-banner"
+			preferenceName="home-welcome-banner-dismissed"
+			onClick={ recordStats }
+		>
+			<img
+				src="/calypso/images/extensions/woocommerce/woocommerce-setup.svg"
+				aria-hidden="true"
+				alt=""
+			/>
+			<div>
+				<CardHeading>{ translate( 'Learn and grow with My Home' ) }</CardHeading>
+				<p>
+					{ translate(
+						'This is your new home for quick links to common tasks, easy access to support, and guidance ' +
+							'tailored to you. We’ll keep improving what you see here to help you learn and grow with us.'
+					) }
+				</p>
+			</div>
+		</DismissibleCard>
 	);
 };
 
-export default connect(
-	state => ( {
-		bannerDismissed: getPreference( state, 'home-welcome-banner-dismissed' ),
-		fetchingPreferences: isFetchingPreferences( state ),
-	} ),
-	dispatch => ( {
-		dismissBanner: () =>
-			dispatch(
-				withAnalytics(
-					composeAnalytics(
-						recordTracksEvent( 'calypso_home_welcome_banner_dismiss' ),
-						bumpStat( 'calypso-home-welcome-banner', 'dismiss' )
-					),
-					savePreference( 'home-welcome-banner-dismissed', true )
-				)
-			),
-	} )
-)( localize( WelcomeBanner ) );
+export default connect( null, dispatch => ( {
+	recordStats: () =>
+		dispatch(
+			composeAnalytics(
+				recordTracksEvent( 'calypso_home_welcome_banner_dismiss' ),
+				bumpStat( 'calypso-home-welcome-banner', 'dismiss' )
+			)
+		),
+} ) )( localize( WelcomeBanner ) );
