890326b77715b3bacc596c109a0b4cbb0adb1959
Elton Goci <eltongoci@gmail.com>
2020-02-20 20:10:11 +0100

    Minor refactoring

--- client/my-sites/checkout/cart/cart-free-user-plan-upsell.jsx
+++ client/my-sites/checkout/cart/cart-free-user-plan-upsell.jsx
@@ -9,45 +9,52 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
+import { Button } from '@automattic/components';
 import { getSelectedSite } from 'state/ui/selectors';
 import { siteHasPaidPlan } from 'signup/steps/site-picker/site-picker-submit';
 import { currentUserHasFlag, getCurrentUser } from 'state/current-user/selectors';
 import { PLAN_UPSELL_FOR_FREE_USERS } from 'state/current-user/constants';
 import { hasPlan } from 'lib/cart-values/cart-items';
+import SectionHeader from 'components/section-header';
 
 class CartFreeUserPlanUpsell extends React.Component {
 	static propTypes = {
 		cart: PropTypes.object,
 		selectedSite: PropTypes.oneOfType( [ PropTypes.object, PropTypes.bool ] ),
+		hasPaidPlan: PropTypes.bool,
+		hasPlanInCart: PropTypes.bool,
 		showPlanUpsell: PropTypes.bool,
 		translate: PropTypes.func.isRequired,
 	};
 
 	render() {
-		const { cart, selectedSite, showPlanUpsell } = this.props;
+		const { hasPaidPlan, hasPlanInCart, selectedSite, showPlanUpsell, translate } = this.props;
 
-		if (
-			! showPlanUpsell ||
-			! selectedSite ||
-			siteHasPaidPlan( selectedSite ) ||
-			hasPlan( cart )
-		) {
+		if ( ! showPlanUpsell || ! selectedSite || hasPaidPlan || hasPlanInCart ) {
 			return null;
 		}
 
+		const header = translate( 'Upgrade and save' );
+
 		return (
 			<div>
-				<span>TODO!</span>
+				<SectionHeader className="cart__header" label={ header } />
+				<div style={ { padding: '16px' } }>
+					<p>TODO!</p>
+					<Button>{ 'Add to cart' }</Button>
+				</div>
 			</div>
 		);
 	}
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = ( state, { cart } ) => {
 	const selectedSite = getSelectedSite( state );
 
 	return {
-		selectedSite,
+		hasPaidPlan: siteHasPaidPlan( selectedSite ),
+		hasPlanInCart: hasPlan( cart ),
+		selectedSite: selectedSite,
 		showPlanUpsell: getCurrentUser( state )
 			? currentUserHasFlag( state, PLAN_UPSELL_FOR_FREE_USERS )
 			: false,
