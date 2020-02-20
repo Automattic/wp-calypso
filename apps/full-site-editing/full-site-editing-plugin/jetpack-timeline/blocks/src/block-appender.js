8779231ffeb92c2f8bfd95d24ca419fd33711eea
Alex Kirk <alex.git@kirk.at>
2020-02-20 09:34:00 +0100

    Add missing textdomain

--- apps/full-site-editing/full-site-editing-plugin/jetpack-timeline/blocks/src/block-appender.js
+++ apps/full-site-editing/full-site-editing-plugin/jetpack-timeline/blocks/src/block-appender.js
@@ -32,7 +32,7 @@ export const BlockAppender = props => {
 			>
 				<path d="M10 1c-5 0-9 4-9 9s4 9 9 9 9-4 9-9-4-9-9-9zm0 16c-3.9 0-7-3.1-7-7s3.1-7 7-7 7 3.1 7 7-3.1 7-7 7zm1-11H9v3H6v2h3v3h2v-3h3V9h-3V6z"></path>
 			</svg>{ ' ' }
-			{ __( 'Add New Entry' ) }
+			{ __( 'Add New Entry', 'full-site-editing' ) }
 		</button>
 	);
 };
