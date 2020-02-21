a285c9021efbb479d4d4ce3b311502064c33445d
Sérgio Gomes <mail@sgomes.com>
2020-02-21 16:15:19 +0000

    Split out getPreActivateThemeId

---
+++ client/state/themes/selectors/get-pre-activate-theme-id.js
@@ -0,0 +1,18 @@
+/**
+ * Internal dependencies
+ */
+import 'state/themes/init';
+
+/**
+ * Return the theme ID of the theme BEFORE to activated it.
+ * It allows getting the possibility to take action when it's needed.
+ * Specifically, it helps to show a modal when the theme to activate
+ * will change the homepage of the site, usually,
+ * this feature is included in First-Template Themes.
+ *
+ * @param {object} state   Global state tree
+ * @returns {string} Theme ID,
+ */
+export function getPreActivateThemeId( state ) {
+	return state.themes.themeHasAutoLoadingHomepageWarning.themeId;
+}
