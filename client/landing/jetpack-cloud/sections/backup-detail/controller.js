6a036c4da54bcbe18c998d95e69becdc3d5b9397
Rich Collier <rcoll@users.noreply.github.com>
2020-02-21 09:07:35 -0500

    Update client/landing/jetpack-cloud/sections/backup-detail/controller.js
    Co-Authored-By: Piotr Delawski <delawski@users.noreply.github.com>

--- client/landing/jetpack-cloud/sections/backup-detail/controller.js
+++ client/landing/jetpack-cloud/sections/backup-detail/controller.js
@@ -11,7 +11,7 @@ import JetpackCloudBackupDetailPage from './main';
 export function jetpackCloudBackupDetail( context, next ) {
 	const backupId = parseInt( context.params.backupId );
 
-	context.primary = <JetpackCloudBackupDetailPage backupId={ backupId } />
+	context.primary = <JetpackCloudBackupDetailPage backupId={ backupId } />;
 	
 	next();
 }
