ec67c6cdb9487c3440e86973fad5a120e12b8aa2
sixhours <calobee@gmail.com>
2020-02-21 16:00:25 -0500

    Experiment with adding a CSS grid to the main layout

--- client/my-sites/g2/main.jsx
+++ client/my-sites/g2/main.jsx
@@ -23,10 +23,33 @@ const TestingG2 = () => {
 				<h2>I'm a heading.</h2>
 				<h3>I'm also a heading...</h3>
 				<h4>I'm a much smaller heading</h4>
-				<Button primary>Click me</Button>
-				<Button>Secondary click me</Button>
-				<Button compact>Compact click me</Button>
+				<div>
+					<Button primary>Click me</Button>
+					<Button>Secondary click me</Button>
+				</div>
+				<div>
+					<Button compact>Compact click me</Button>
+				</div>
 				<Button borderless>Borderless click me</Button>
+			</Card>
+			<Card>
+				<p>Some regular text within a card.</p>
+			</Card>
+			{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace */ }
+			<Card className="col-1">
+				<h3>I'm a heading.</h3>
+				<p>
+					Laum Ipsum slower than molasses going uphill in January bluebries from away got in a gaum
+					muckle riyht on'ta her can't get theyah from heeyah slower than molasses going uphill in
+					January, gummah Powrtland aht wreckah fish chowdah crunchah. Dinnahbucket The County clam
+					chowdah mistah man gettin' ugly what a cahd, Loyston-Ahban. Feed 'uh the hot suppah..
+					Katahdin gummah potatoes.
+				</p>
+			</Card>
+			{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace */ }
+			<Card className="col-2">
+				<p>Some regular text within a card.</p>
+				<Button primary>Click me</Button>
 			</Card>
 		</Main>
 	);
