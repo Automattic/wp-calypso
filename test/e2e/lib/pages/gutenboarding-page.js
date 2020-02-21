16bf3dc3522b4edc7ad2544beb769976f387a720
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-02-21 16:35:51 +0200

    Add basic gutenboarding e2e tests. - add gutenboarding-page to e2e lib/pages - add gutenboarding-spec checking loading of page for logged-in and non-loggedin users

---
+++ test/e2e/lib/pages/gutenboarding-page.js
@@ -0,0 +1,32 @@
+/**
+ * External dependencies
+ */
+import { By as by } from 'selenium-webdriver';
+
+/**
+ * Internal dependencies
+ */
+import AsyncBaseContainer from '../async-base-container';
+import * as dataHelper from '../data-helper';
+import * as driverHelper from '../driver-helper';
+
+export default class GutenboardingPage extends AsyncBaseContainer {
+	constructor( driver, url ) {
+		if ( ! url ) {
+			url = GutenboardingPage.getGutenboardingURL();
+		}
+		super( driver, by.css( '.is-section-gutenboarding' ), url );
+	}
+
+	static getGutenboardingURL() {
+		// using forceWpCalypso flag because Gutenboarding is enabled only on wpcalypso
+		return dataHelper.getCalypsoURL( 'gutenboarding', undefined, { forceWpCalypso: true } );
+	}
+
+	async waitForBlock() {
+		return await driverHelper.isElementPresent(
+			this.driver,
+			by.css( '.gutenboarding-block-list' )
+		);
+	}
+}
---
+++ test/e2e/specs/wp-gutenboarding-spec.js
@@ -0,0 +1,49 @@
+/**
+ * External dependencies
+ */
+import config from 'config';
+import assert from 'assert';
+
+/**
+ * Internal dependencies
+ */
+import LoginFlow from '../lib/flows/login-flow.js';
+
+import * as driverManager from '../lib/driver-manager.js';
+import GutenboardingPage from '../lib/pages/gutenboarding-page.js';
+
+const mochaTimeOut = config.get( 'mochaTimeoutMS' );
+const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
+const screenSize = driverManager.currentScreenSize();
+
+let driver;
+
+before( async function() {
+	this.timeout( startBrowserTimeoutMS );
+	driver = await driverManager.startBrowser();
+} );
+
+describe( 'Gutenboarding: (' + screenSize + ') @parallel', function() {
+	this.timeout( mochaTimeOut );
+	describe( 'Visit Gutenboarding page as a new user', function() {
+		before( async function() {
+			await driverManager.ensureNotLoggedIn( driver );
+		} );
+
+		step( 'Can visit Gutenboarding page and see Onboarding block', async function() {
+			const page = await GutenboardingPage.Visit( driver );
+			const blockExists = await page.waitForBlock();
+			return assert( blockExists, 'Onboarding block is not rendered' );
+		} );
+	} );
+
+	describe( 'Visit Gutenboarding page as a logged in user', function() {
+		step( 'Can log in as user', async function() {
+			this.loginFlow = new LoginFlow( driver );
+			return await this.loginFlow.login();
+		} );
+		step( 'Can visit Gutenboarding', async function() {
+			await GutenboardingPage.Visit( driver );
+		} );
+	} );
+} );
