c3b3b30f55148874b6660ba6a52c5a4c7822cffe
Marin Atanasov <tyxla@abv.bg>
2020-02-21 13:59:25 +0200

    Reader: Use entry point section for Search

--- client/reader/search/index.js
+++ client/reader/search/index.js
@@ -7,8 +7,10 @@ import page from 'page';
  * Internal dependencies
  */
 import { search } from './controller';
-import { preloadReaderBundle, sidebar, updateLastRoute } from 'reader/controller';
-import { makeLayout, render as clientRender } from 'controller';
+import { makeLayout, preloadReaderBundle, sidebar, updateLastRoute } from 'reader/controller';
+import { render as clientRender } from 'controller';
+import { setSection } from 'controller/shared';
+import { READER_SEARCH_DEFINITION } from 'reader';
 
 export default function() {
 	page(
@@ -16,6 +18,7 @@ export default function() {
 		preloadReaderBundle,
 		updateLastRoute,
 		sidebar,
+		setSection( READER_SEARCH_DEFINITION ),
 		search,
 		makeLayout,
 		clientRender
