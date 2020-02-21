cfdf46d808d2ee6157adcb1d4f576263b9821927
Sérgio Gomes <mail@sgomes.com>
2020-02-21 14:25:15 +0000

    Split out getThemesForQuery

--- client/state/themes/selectors/index.js
+++ client/state/themes/selectors/index.js
@@ -40,44 +40,7 @@ export { getTheme } from 'state/themes/selectors/get-theme';
 export { getCanonicalTheme } from 'state/themes/selectors/get-canonical-theme';
 export { isInstallingTheme } from 'state/themes/selectors/is-installing-theme';
 export { getThemeRequestErrors } from 'state/themes/selectors/get-theme-request-errors';
-
-/**
- * Returns an array of normalized themes for the themes query, or null if no
- * themes have been received.
- *
- * @param  {object}  state  Global state tree
- * @param  {number}  siteId Site ID
- * @param  {object}  query  Theme query object
- * @returns {?Array}         Themes for the theme query
- */
-export const getThemesForQuery = createSelector(
-	( state, siteId, query ) => {
-		const manager = state.themes.queries[ siteId ];
-		if ( ! manager ) {
-			return null;
-		}
-
-		const themes = manager.getItems( query );
-		if ( ! themes ) {
-			return null;
-		}
-
-		// ThemeQueryManager will return an array including undefined entries if
-		// it knows that a page of results exists for the query (via a previous
-		// request's `found` value) but the items haven't been received. While
-		// we could impose this on the developer to accommodate, instead we
-		// simply return null when any `undefined` entries exist in the set.
-		if ( includes( themes, undefined ) ) {
-			return null;
-		}
-
-		// FIXME: The themes endpoint weirdly sometimes returns duplicates (spread
-		// over different pages) which we need to remove manually here for now.
-		return uniq( themes );
-	},
-	state => state.themes.queries,
-	( state, siteId, query ) => getSerializedThemesQuery( query, siteId )
-);
+export { getThemesForQuery } from 'state/themes/selectors/get-themes-for-query';
 
 /**
  * Returns last query used.
