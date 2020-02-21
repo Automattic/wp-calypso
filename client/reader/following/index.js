086e032cf65b1c172b8134a3ddb1ead9cc44a4a0
Marin Atanasov <tyxla@abv.bg>
2020-02-21 13:58:03 +0200

    Reader: Use entry point section for Following

--- client/reader/following/index.js
+++ client/reader/following/index.js
@@ -7,11 +7,21 @@ import page from 'page';
  * Internal dependencies
  */
 import { followingManage } from './controller';
-import { initAbTests, updateLastRoute, sidebar } from 'reader/controller';
-import { makeLayout, render as clientRender } from 'controller';
+import { initAbTests, makeLayout, updateLastRoute, sidebar } from 'reader/controller';
+import { render as clientRender } from 'controller';
+import { setSection } from 'controller/shared';
+import { READER_FOLLOWING_DEFINITION } from 'reader';
 
 export default function() {
 	page( '/following/*', initAbTests );
-	page( '/following/manage', updateLastRoute, sidebar, followingManage, makeLayout, clientRender );
+	page(
+		'/following/manage',
+		updateLastRoute,
+		sidebar,
+		setSection( READER_FOLLOWING_DEFINITION ),
+		followingManage,
+		makeLayout,
+		clientRender
+	);
 	page( '/following/edit*', '/following/manage' );
 }
