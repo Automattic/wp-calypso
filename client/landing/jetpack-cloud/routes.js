76538d4c6e5fbf2b5aad304f262e2f7142ee5470
Piotr Delawski <piotr.delawski@automattic.com>
2020-02-21 17:57:18 +0100

    Remove redundant normalization - it's already done in the app boot

--- client/landing/jetpack-cloud/routes.js
+++ client/landing/jetpack-cloud/routes.js
@@ -6,7 +6,6 @@ import page from 'page';
 /**
  * Internal dependencies
  */
-import { normalize } from 'lib/route';
 import { siteSelection } from 'my-sites/controller';
 import { clientRender, makeLayout, setupSidebar } from './controller';
 import { jetpackCloudDashboard } from './sections/dashboard/controller';
@@ -19,23 +18,70 @@ import { jetpackCloudScanHistory } from './sections/scan-history/controller';
 import { jetpackCloudSettings } from './sections/settings/controller';
 
 const router = () => {
-	page( '*', normalize );
-
 	page( '/', setupSidebar, jetpackCloudDashboard, makeLayout, clientRender );
 
 	page( '/backups', siteSelection, setupSidebar, jetpackCloudBackups, makeLayout, clientRender );
-	page( '/backups/:site', siteSelection, setupSidebar, jetpackCloudBackups, makeLayout, clientRender );
-	page( '/backups/:site/detail/:backupId', siteSelection, setupSidebar, jetpackCloudBackupDetail, makeLayout, clientRender );
-	page( '/backups/:site/download/:downloadId', siteSelection, setupSidebar, jetpackCloudBackupDownload, makeLayout, clientRender );
-	page( '/backups/:site/restore', siteSelection, setupSidebar, jetpackCloudBackupRestore, makeLayout, clientRender );
-	page( '/backups/:site/restore/:restoreId', siteSelection, setupSidebar, jetpackCloudBackupRestore, makeLayout, clientRender );
+	page(
+		'/backups/:site',
+		siteSelection,
+		setupSidebar,
+		jetpackCloudBackups,
+		makeLayout,
+		clientRender
+	);
+	page(
+		'/backups/:site/detail/:backupId',
+		siteSelection,
+		setupSidebar,
+		jetpackCloudBackupDetail,
+		makeLayout,
+		clientRender
+	);
+	page(
+		'/backups/:site/download/:downloadId',
+		siteSelection,
+		setupSidebar,
+		jetpackCloudBackupDownload,
+		makeLayout,
+		clientRender
+	);
+	page(
+		'/backups/:site/restore',
+		siteSelection,
+		setupSidebar,
+		jetpackCloudBackupRestore,
+		makeLayout,
+		clientRender
+	);
+	page(
+		'/backups/:site/restore/:restoreId',
+		siteSelection,
+		setupSidebar,
+		jetpackCloudBackupRestore,
+		makeLayout,
+		clientRender
+	);
 
 	page( '/scan', siteSelection, setupSidebar, jetpackCloudScan, makeLayout, clientRender );
 	page( '/scan/:site', siteSelection, setupSidebar, jetpackCloudScan, makeLayout, clientRender );
-	page( '/scan/:site/history', siteSelection, setupSidebar, jetpackCloudScanHistory, makeLayout, clientRender );
+	page(
+		'/scan/:site/history',
+		siteSelection,
+		setupSidebar,
+		jetpackCloudScanHistory,
+		makeLayout,
+		clientRender
+	);
 
 	page( '/settings', siteSelection, setupSidebar, jetpackCloudSettings, makeLayout, clientRender );
-	page( '/settings/:site', siteSelection, setupSidebar, jetpackCloudSettings, makeLayout, clientRender );
+	page(
+		'/settings/:site',
+		siteSelection,
+		setupSidebar,
+		jetpackCloudSettings,
+		makeLayout,
+		clientRender
+	);
 };
 
 export default router;
