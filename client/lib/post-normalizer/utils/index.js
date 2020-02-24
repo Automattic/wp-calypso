73d0747de3d0ed0481b7dec9aaa5d4910e245691
Sérgio Gomes <mail@sgomes.com>
2020-02-24 17:46:41 +0000

    Split out maxWidthPhotonishURL

--- client/lib/post-normalizer/utils/index.js
+++ client/lib/post-normalizer/utils/index.js
@@ -1,7 +1,7 @@
 /**
  * External Dependencies
  */
-import { forEach, some, endsWith, findIndex } from 'lodash';
+import { some, endsWith, findIndex } from 'lodash';
 import url from 'url';
 
 /**
@@ -9,62 +9,11 @@ import url from 'url';
  */
 import safeImageURL from 'lib/safe-image-url';
 import { isPhotonHost } from 'lib/post-normalizer/utils/is-photon-host';
-
-const IMAGE_SCALE_FACTOR =
-	typeof window !== 'undefined' && window.devicePixelRatio && window.devicePixelRatio > 1 ? 2 : 1;
-
-const DEFAULT_PHOTON_QUALITY = 80; // 80 was chosen after some heuristic testing as the best blend of size and quality
+import { maxWidthPhotonishURL } from 'lib/post-normalizer/utils/max-width-photonish-url';
 
 export { isPhotonHost } from 'lib/post-normalizer/utils/is-photon-host';
 export { imageSizeFromAttachments } from 'lib/post-normalizer/utils/image-size-from-attachments';
-
-export function maxWidthPhotonishURL( imageURL, width ) {
-	if ( ! imageURL ) {
-		return imageURL;
-	}
-
-	let parsedURL = {};
-	try {
-		parsedURL = url.parse( imageURL, true, true ); // true, true means allow protocol-less hosts and parse the querystring
-	} catch ( e ) {
-		/**
-		 * `url.parse` throws in a few places where it calls decodeURIComponent
-		 *
-		 * @see e.g. https://github.com/Automattic/wp-calypso/issues/18645
-		 */
-	}
-
-	if ( ! parsedURL.host ) {
-		return imageURL;
-	}
-
-	const isGravatar = parsedURL.host.indexOf( 'gravatar.com' ) !== -1;
-
-	delete parsedURL.search;
-	// strip other sizing params
-	forEach( [ 'h', 'crop', 'resize', 'fit' ], function( param ) {
-		delete parsedURL.query[ param ];
-	} );
-
-	const sizeParam = isGravatar ? 's' : 'w';
-	parsedURL.query[ sizeParam ] = width * IMAGE_SCALE_FACTOR;
-
-	if ( ! isGravatar ) {
-		// gravatar doesn't support these, only photon / files.wordpress
-		parsedURL.query.quality = DEFAULT_PHOTON_QUALITY;
-		parsedURL.query.strip = 'info'; // strip all exif data, leave ICC intact
-	}
-
-	// make a new query object with keys in a known order
-	parsedURL.query = Object.keys( parsedURL.query )
-		.sort()
-		.reduce( ( memo, key ) => {
-			memo[ key ] = parsedURL.query[ key ];
-			return memo;
-		}, {} );
-
-	return url.format( parsedURL );
-}
+export { maxWidthPhotonishURL } from 'lib/post-normalizer/utils/max-width-photonish-url';
 
 export function makeImageURLSafe( object, propName, maxWidth, baseURL ) {
 	if ( object && object[ propName ] ) {
