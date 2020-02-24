0174720986500867b2d5060c1e6d06179f4cfeba
And Finally <andfinally@users.noreply.github.com>
2020-02-24 17:50:41 +0000

    Using the same layout as the Launched home.

--- client/my-sites/customer-home/main.jsx
+++ client/my-sites/customer-home/main.jsx
@@ -256,34 +256,6 @@ class Home extends Component {
 			}
 		}
 
-		if ( isRecentlyMigratedSite ) {
-			return (
-				<Card className="customer-home__migrate-card" highlight="info">
-					<img
-						src="/calypso/images/illustrations/fireworks.svg"
-						aria-hidden="true"
-						className="customer-home__migrate-fireworks"
-						alt=""
-					/>
-					<div className="customer-home__migrate-card-text">
-						<CardHeading>{ translate( 'Your site has been imported!' ) }</CardHeading>
-						<p className="customer-home__migrate-card-subtext">
-							{ this.getChecklistSubHeaderText() }
-						</p>
-					</div>
-					<div className="customer-home__view-site-button">
-						<Button
-							href={ site.URL }
-							onClick={ () => trackAction( 'my_site', 'view_site' ) }
-							primary={ true }
-						>
-							{ translate( 'View Site' ) }
-						</Button>
-					</div>
-				</Card>
-			);
-		}
-
 		// If launched, show a congratulatory message, else show the standard heading
 		return (
 			<>
@@ -303,6 +275,22 @@ class Home extends Component {
 						</div>
 					) }
 				</div>
+				{ isRecentlyMigratedSite && (
+					<Card className="customer-home__migrate-card" highlight="info">
+						<img
+							src="/calypso/images/illustrations/fireworks.svg"
+							aria-hidden="true"
+							className="customer-home__migrate-fireworks"
+							alt=""
+						/>
+						<div className="customer-home__migrate-card-text">
+							<CardHeading>{ translate( 'Your site has been imported!' ) }</CardHeading>
+							<p className="customer-home__migrate-card-subtext">
+								{ this.getChecklistSubHeaderText() }
+							</p>
+						</div>
+					</Card>
+				) }
 				{ ! siteIsUnlaunched && 'launched' === checklistMode ? (
 					<Card className="customer-home__launch-card" highlight="info">
 						<img
