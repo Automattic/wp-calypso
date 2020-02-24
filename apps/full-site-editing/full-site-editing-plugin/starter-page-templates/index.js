14df311308dd01cb390267ead22dd4b55e8c1757
retrofox <rdsuarez@gmail.com>
2020-02-24 11:50:12 -0300

    spt: revert experiment

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/index.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/index.js
@@ -2,9 +2,6 @@
 /**
  * External dependencies
  */
-import { parse } from 'querystring';
-import { get } from 'lodash';
-
 import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
 import { select, dispatch, subscribe } from '@wordpress/data';
@@ -34,67 +31,57 @@ if ( tracksUserData ) {
 	initializeWithIdentity( tracksUserData );
 }
 
-const previewTemplate = get(
-	parse( get( window, [ 'document', 'location', 'search' ] ) ),
-	'previewTemplate'
-);
-console.log( { previewTemplate } );
-
-if ( previewTemplate ) {
-	console.log( 'Preview!!!' );
-} else {
-	// Open plugin only if we are creating new page.
-	if ( screenAction === 'add' ) {
-		registerPlugin( 'page-templates', {
-			render: () => {
-				return (
-					<PageTemplatesPlugin
-						isFrontPage={ isFrontPage }
-						segment={ segment }
-						shouldPrefetchAssets={ false }
-						templates={ templates }
-						theme={ theme }
-						vertical={ vertical }
-					/>
-				);
-			},
-		} );
-	}
-
-	// Always register ability to open from document sidebar.
-	registerPlugin( 'page-templates-sidebar', {
+// Open plugin only if we are creating new page.
+if ( screenAction === 'add' ) {
+	registerPlugin( 'page-templates', {
 		render: () => {
 			return (
-				<PluginDocumentSettingPanel
-					name="Template Modal Opener"
-					title={ __( 'Page Layout' ) }
-					className="page-template-modal__sidebar" // eslint-disable-line wpcalypso/jsx-classname-namespace
-					icon="none"
-				>
-					<SidebarTemplatesPlugin
-						isFrontPage={ isFrontPage }
-						segment={ segment }
-						siteInformation={ siteInformation }
-						templates={ templates }
-						theme={ theme }
-						vertical={ vertical }
-					/>
-				</PluginDocumentSettingPanel>
+				<PageTemplatesPlugin
+					isFrontPage={ isFrontPage }
+					segment={ segment }
+					shouldPrefetchAssets={ false }
+					templates={ templates }
+					theme={ theme }
+					vertical={ vertical }
+				/>
 			);
 		},
 	} );
-
-	// Make sidebar plugin open by default.
-	const unsubscribe = subscribe( () => {
-		if (
-			! select( 'core/edit-post' ).isEditorPanelOpened(
-				'page-templates-sidebar/Template Modal Opener'
-			)
-		) {
-			dispatch( 'core/edit-post' ).toggleEditorPanelOpened(
-				'page-templates-sidebar/Template Modal Opener'
-			);
-		}
-		unsubscribe();
-	} );
 }
+
+// Always register ability to open from document sidebar.
+registerPlugin( 'page-templates-sidebar', {
+	render: () => {
+		return (
+			<PluginDocumentSettingPanel
+				name="Template Modal Opener"
+				title={ __( 'Page Layout' ) }
+				className="page-template-modal__sidebar" // eslint-disable-line wpcalypso/jsx-classname-namespace
+				icon="none"
+			>
+				<SidebarTemplatesPlugin
+					isFrontPage={ isFrontPage }
+					segment={ segment }
+					siteInformation={ siteInformation }
+					templates={ templates }
+					theme={ theme }
+					vertical={ vertical }
+				/>
+			</PluginDocumentSettingPanel>
+		);
+	},
+} );
+
+// Make sidebar plugin open by default.
+const unsubscribe = subscribe( () => {
+	if (
+		! select( 'core/edit-post' ).isEditorPanelOpened(
+			'page-templates-sidebar/Template Modal Opener'
+		)
+	) {
+		dispatch( 'core/edit-post' ).toggleEditorPanelOpened(
+			'page-templates-sidebar/Template Modal Opener'
+		);
+	}
+	unsubscribe();
+} );
