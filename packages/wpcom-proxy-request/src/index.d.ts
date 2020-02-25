93e07edacf4a46ba628856c307aa582903b620bb
Bernie Reiter <ockham@raz.or.at>
2020-02-26 00:26:45 +0600

    wpcom-proxy-request: Add a few type definitions

---
+++ packages/wpcom-proxy-request/src/index.d.ts
@@ -0,0 +1,22 @@
+export interface WpcomClientCredentials {
+	client_id: string;
+	client_secret: string;
+}
+
+export interface WpcomRequestParams {
+	path?: string;
+	method?: string;
+	apiVersion?: string;
+	body?: object;
+	token?: string;
+	metaAPI?: {
+		accessAllUsersBlogs?: boolean;
+	};
+}
+
+export function reloadProxy(): void;
+
+export default function request(
+	params: WpcomRequestParams,
+	callback?: Function
+): XMLHttpRequest | Promise;
