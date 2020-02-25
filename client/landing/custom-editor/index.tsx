5082ea397b5fe072d679f4d7b8bbd3705b9b1cc4
Bernie Reiter <ockham@raz.or.at>
2020-02-25 22:05:17 +0600

    Properly proxy requests

--- client/landing/custom-editor/index.tsx
+++ client/landing/custom-editor/index.tsx
@@ -4,6 +4,11 @@
 import config from '../../config';
 import { initialize } from '@wordpress/edit-site';
 
+/**
+ * Internal dependencies
+ */
+import './utils';
+
 window.AppBoot = () => {
 	if ( ! config.isEnabled( 'custom-editor' ) ) {
 		window.location.href = '/';
