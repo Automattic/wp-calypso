25af10f134d3d6d0e8163b7457b467b38bfcb1be
Sérgio Gomes <mail@sgomes.com>
2020-02-25 17:38:48 +0000

    Split out getFeaturedImageId

--- client/state/posts/utils/index.js
+++ client/state/posts/utils/index.js
@@ -33,31 +33,4 @@ export { getVisibility } from './get-visibility';
 export { removeSlug } from './remove-slug';
 export { getPermalinkBasePath } from './get-permalink-base-path';
 export { getPagePath } from './get-page-path';
-
-/**
- * Returns the ID of the featured image assigned to the specified post, or
- * `undefined` otherwise. A utility function is useful because the format
- * of a post varies between the retrieve and update endpoints. When
- * retrieving a post, the thumbnail ID is assigned in `post_thumbnail`, but
- * in creating a post, the thumbnail ID is assigned to `featured_image`.
- *
- * @param  {object} post Post object
- * @returns {*} featured image id or undefined
- */
-export const getFeaturedImageId = function( post ) {
-	if ( ! post ) {
-		return;
-	}
-
-	if ( 'featured_image' in post && ! /^https?:\/\//.test( post.featured_image ) ) {
-		// Return the `featured_image` property if it does not appear to be
-		// formatted as a URL
-		return post.featured_image;
-	}
-
-	if ( post.post_thumbnail ) {
-		// After the initial load from the REST API, pull the numeric ID
-		// from the thumbnail object if one exists
-		return post.post_thumbnail.ID;
-	}
-};
+export { getFeaturedImageId } from './get-featured-image-id';
