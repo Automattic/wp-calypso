b58a935c1f808ee8c8ba8fb7eb01ed45d6196e3e
Sérgio Gomes <mail@sgomes.com>
2020-02-26 12:45:18 +0000

    Split out getNormalizedPost

--- client/state/posts/selectors/index.js
+++ client/state/posts/selectors/index.js
@@ -28,25 +28,11 @@ import { DEFAULT_POST_QUERY, DEFAULT_NEW_POST_VALUES } from 'state/posts/constan
 import { addQueryArgs } from 'lib/route';
 
 import { getQueryManager } from 'state/posts/selectors/get-query-manager';
-import { getPost } from 'state/posts/selectors/get-post';
 
 import 'state/posts/init';
 
 export { getPost } from 'state/posts/selectors/get-post';
-
-/**
- * Returns a normalized post object by its global ID, or null if the post does
- * not exist. A normalized post includes common transformations to prepare the
- * post for display.
- *
- * @param   {object}  state    Global state tree
- * @param   {string}  globalId Post global ID
- * @returns {?object}          Post object
- */
-export const getNormalizedPost = createSelector(
-	( state, globalId ) => normalizePostForDisplay( getPost( state, globalId ) ),
-	state => [ state.posts.items, state.posts.queries ]
-);
+export { getNormalizedPost } from 'state/posts/selectors/get-normalized-post';
 
 /**
  * Returns an array of post objects by site ID.
