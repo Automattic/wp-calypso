ea0136d3f1f3e8e8e7180efaea5302afdceaba1d
Bart <bartlomiej.kalisz@gmail.com>
2020-02-26 14:41:28 +0100

    Add "hideFrontPageTitle" to the SPT config object

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/class-starter-page-templates.php
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/class-starter-page-templates.php
@@ -172,15 +172,16 @@ class Starter_Page_Templates {
 		$config = apply_filters(
 			'fse_starter_page_templates_config',
 			[
-				'siteInformation' => array_merge( $default_info, $site_info ),
-				'templates'       => array_merge( $default_templates, $vertical_templates ),
-				'vertical'        => $vertical,
-				'segment'         => $segment,
+				'siteInformation' 		=> array_merge( $default_info, $site_info ),
+				'templates'       		=> array_merge( $default_templates, $vertical_templates ),
+				'vertical'        		=> $vertical,
+				'segment'         		=> $segment,
 				// phpcs:ignore WordPress.Security.NonceVerification.NoNonceVerification
-				'screenAction'    => isset( $_GET['new-homepage'] ) ? 'add' : $screen->action,
-				'theme'           => normalize_theme_slug( get_stylesheet() ),
+				'screenAction'    		=> isset( $_GET['new-homepage'] ) ? 'add' : $screen->action,
+				'theme'           		=> normalize_theme_slug( get_stylesheet() ),
 				// phpcs:ignore WordPress.Security.NonceVerification.NoNonceVerification
-				'isFrontPage'     => isset( $_GET['post'] ) && get_option( 'page_on_front' ) === $_GET['post'],
+				'isFrontPage'     		=> isset( $_GET['post'] ) && get_option( 'page_on_front' ) === $_GET['post'],
+				'hideFrontPageTitle'	=> get_theme_mod( 'hide_front_page_title' ),
 			]
 		);
 		wp_localize_script( 'starter-page-templates', 'starterPageTemplatesConfig', $config );
