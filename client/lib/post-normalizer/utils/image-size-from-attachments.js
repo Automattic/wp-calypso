20dc98f50952259f14b7bd028942668022ec54e2
Sérgio Gomes <mail@sgomes.com>
2020-02-26 16:13:15 +0000

    Modularize post normalizer (#39661)
    * Move post-normalizer utils to its own dir
    
    * Split out isPhotonHost
    
    * Split out imageSizeFromAttachments
    
    * Split out maxWidthPhotonishURL
    
    * Split out makeImageURLSafe
    
    * Split out domForHtml
    
    * Split out isUrlLikelyAnImage
    
    * Split out thumbIsLikelyImage
    
    * Split out iframeIsWhitelisted
    
    * Split out isCandidateForCanonicalImage
    
    * Split out isFeaturedImageInContent
    
    * Split out deduceImageWidthAndHeight
    
    * Split out safeLink
    
    * Sort alphabetically
    
    * Indicate absence of side effects
    
    * Fix import in rule-pick-canonical-media

---
+++ client/lib/post-normalizer/utils/image-size-from-attachments.js
@@ -0,0 +1,19 @@
+/**
+ * External Dependencies
+ */
+import { find } from 'lodash';
+
+export function imageSizeFromAttachments( post, imageUrl ) {
+	if ( ! post.attachments ) {
+		return;
+	}
+
+	const found = find( post.attachments, attachment => attachment.URL === imageUrl );
+
+	if ( found ) {
+		return {
+			width: found.width,
+			height: found.height,
+		};
+	}
+}
---
+++ client/lib/post-normalizer/utils/is-url-likely-an-image.js
@@ -0,0 +1,19 @@
+/**
+ * External Dependencies
+ */
+import { some, endsWith } from 'lodash';
+import url from 'url';
+
+/** Determine if url is likely pointed to an image
+ *
+ * @param {string} uri - a url
+ * @returns {boolean} - true or false depending on if it is probably an image (has the right extension)
+ */
+export function isUrlLikelyAnImage( uri ) {
+	if ( ! uri ) {
+		return false;
+	}
+
+	const withoutQuery = url.parse( uri ).pathname;
+	return some( [ '.jpg', '.jpeg', '.png', '.gif' ], ext => endsWith( withoutQuery, ext ) );
+}
