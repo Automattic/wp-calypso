f9be940df33900e2e2c43f4c1f73901b6c9e413a
Sérgio Gomes <mail@sgomes.com>
2020-02-26 12:59:21 +0000

    Split out getPostsFoundForQuery

--- client/state/posts/selectors/index.js
+++ client/state/posts/selectors/index.js
@@ -39,24 +39,7 @@ export { getSitePosts } from 'state/posts/selectors/get-site-posts';
 export { getSitePost } from 'state/posts/selectors/get-site-post';
 export { getPostsForQuery } from 'state/posts/selectors/get-posts-for-query';
 export { isRequestingPostsForQuery } from 'state/posts/selectors/is-requesting-posts-for-query';
-
-/**
- * Returns the total number of items reported to be found for the given query,
- * or null if the total number of queryable posts is unknown.
- *
- * @param   {object}  state  Global state tree
- * @param   {?number} siteId Site ID, or `null` for all-sites queries
- * @param   {object}  query  Post query object
- * @returns {?number}        Total number of found items
- */
-export function getPostsFoundForQuery( state, siteId, query ) {
-	const manager = getQueryManager( state, siteId );
-	if ( ! manager ) {
-		return null;
-	}
-
-	return manager.getFound( query );
-}
+export { getPostsFoundForQuery } from 'state/posts/selectors/get-posts-found-for-query';
 
 /**
  * Returns the last queryable page of posts for the given query, or null if the
