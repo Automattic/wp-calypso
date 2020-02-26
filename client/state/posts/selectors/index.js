6aba2e9864796bf082c4639f795de3a28f7a9313
Sérgio Gomes <mail@sgomes.com>
2020-02-26 15:10:34 +0000

    Split out isRequestingSitePost

--- client/state/posts/selectors/index.js
+++ client/state/posts/selectors/index.js
@@ -38,27 +38,7 @@ export { getPostsLastPageForQuery } from 'state/posts/selectors/get-posts-last-p
 export { isPostsLastPageForQuery } from 'state/posts/selectors/is-posts-last-page-for-query';
 export { getPostsForQueryIgnoringPage } from 'state/posts/selectors/get-posts-for-query-ignoring-page';
 export { isRequestingPostsForQueryIgnoringPage } from 'state/posts/selectors/is-requesting-posts-for-query-ignoring-page';
-
-/**
- * Returns true if a request is in progress for the specified site post, or
- * false otherwise.
- *
- * @param   {object}  state  Global state tree
- * @param   {number}  siteId Site ID
- * @param   {number}  postId Post ID
- * @returns {boolean}        Whether request is in progress
- */
-export function isRequestingSitePost( state, siteId, postId ) {
-	if ( ! siteId ) {
-		return null;
-	}
-
-	if ( ! state.posts.siteRequests[ siteId ] ) {
-		return false;
-	}
-
-	return !! state.posts.siteRequests[ siteId ][ postId ];
-}
+export { isRequestingSitePost } from 'state/posts/selectors/is-requesting-site-post';
 
 /**
  * Returns an object of edited post attributes for the site ID post ID pairing.
