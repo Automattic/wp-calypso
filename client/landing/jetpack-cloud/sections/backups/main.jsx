a06b1c8ccc77b53cf623c6a39f4e7fd82712fe14
Echo <ChaosExAnima@users.noreply.github.com>
2020-02-27 10:52:26 -0500

    Loads backups from log and displays them. (#39684)
    This creates a new <ActivityList> component which displays log entries.

--- client/landing/jetpack-cloud/sections/backups/main.jsx
+++ client/landing/jetpack-cloud/sections/backups/main.jsx
@@ -8,17 +8,26 @@ import { connect } from 'react-redux';
  * Internal dependencies
  */
 import { getSelectedSiteId } from 'state/ui/selectors';
+import { requestActivityLogs } from 'state/data-getters';
+import ActivityList from '../../components/activity-list';
 
 class BackupsPage extends Component {
 	render() {
-		return <div>Welcome to the backup detail page for site { this.props.siteId }</div>;
+		return (
+			<div>
+				<p>Welcome to the backup detail page for site { this.props.siteId }</p>
+				<ActivityList logs={ this.props.logs } />
+			</div>
+		);
 	}
 }
 
 export default connect( state => {
 	const siteId = getSelectedSiteId( state );
+	const logs = requestActivityLogs( siteId, { group: 'rewind' } );
 
 	return {
 		siteId,
+		logs,
 	};
 } )( BackupsPage );
