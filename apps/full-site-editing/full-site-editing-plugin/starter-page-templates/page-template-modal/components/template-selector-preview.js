9b7e106fc5f13948545fe7d653125b1755a9d1e5
retrofox <rdsuarez@gmail.com>
2020-02-27 18:55:24 -0300

    spt: show not selected template message

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-preview.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/template-selector-preview.js
@@ -11,15 +11,18 @@ import { __ } from '@wordpress/i18n';
 import BlockFramePreview from './block-iframe-preview';
 
 const TemplateSelectorPreview = ( { blocks = [], viewportWidth } ) => {
+	const noBlocks = blocks.length <= 1;
 	return (
 		/* eslint-disable wpcalypso/jsx-classname-namespace */
-		<div className="template-selector-preview">
-			{ ! blocks.length && (
-				<div class="template-selector-preview__empty-state editor-styles-wrapper">
-					{ __( 'Select a layout to preview.', 'full-site-editing' ) }
+		<div className={ `template-selector-preview ${ noBlocks ? 'not-selected' : '' }` }>
+			{ noBlocks && (
+				<div className="editor-styles-wrapper">
+					<div class="template-selector-preview__empty-state">
+						{ __( 'Select a layout to preview.', 'full-site-editing' ) }
+					</div>
 				</div>
 			) }
-			<BlockFramePreview blocks={ blocks } viewportWidth={ viewportWidth } />
+			{ ! noBlocks && <BlockFramePreview blocks={ blocks } viewportWidth={ viewportWidth } /> }
 		</div>
 		/* eslint-enable wpcalypso/jsx-classname-namespace */
 	);
