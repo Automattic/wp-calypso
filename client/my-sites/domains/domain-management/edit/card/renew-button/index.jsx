dec63fdf10e26c57175d023b167b9159482c1757
Kamen Stanev <hambai@gmail.com>
2020-02-27 19:26:56 +0200

    Add renew button on the domain status screen (#39647)
    Add renew button on the new domain status screen that shows the correct renewal price

---
+++ client/my-sites/domains/domain-management/edit/card/renew-button/index.jsx
@@ -0,0 +1,83 @@
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+import React from 'react';
+import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import classNames from 'classnames';
+import formatCurrency from '@automattic/format-currency';
+import { Button } from '@automattic/components';
+import { handleRenewNowClick, getRenewalPrice } from 'lib/purchases';
+import { getByPurchaseId } from 'state/purchases/selectors';
+import QuerySitePurchases from 'components/data/query-site-purchases';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
+class RenewButton extends React.Component {
+	static propTypes = {
+		compact: PropTypes.bool,
+		primary: PropTypes.bool,
+		selectedSite: PropTypes.object,
+		subscriptionId: PropTypes.number,
+		redemptionProduct: PropTypes.object,
+		reactivate: PropTypes.bool,
+	};
+
+	handleRenew = () => {
+		handleRenewNowClick( this.props.purchase, this.props.selectedSite.slug );
+	};
+
+	render() {
+		const { purchase, selectedSite, redemptionProduct, reactivate } = this.props;
+
+		let formattedPrice = '...';
+		let loading = true;
+
+		if ( purchase && selectedSite.ID ) {
+			const renewalPrice =
+				getRenewalPrice( purchase ) + ( redemptionProduct ? redemptionProduct.cost : 0 );
+			const currencyCode = purchase.currencyCode;
+			formattedPrice = formatCurrency( renewalPrice, currencyCode, { stripZeros: true } );
+			loading = false;
+		}
+
+		const buttonClasses = classNames( 'renew-button', { 'is-loading': loading } );
+
+		return (
+			<React.Fragment>
+				{ selectedSite.ID && <QuerySitePurchases siteId={ selectedSite.ID } /> }
+
+				<Button
+					compact={ this.props.compact }
+					primary={ this.props.primary }
+					className={ buttonClasses }
+					onClick={ this.handleRenew }
+				>
+					{ reactivate
+						? this.props.translate( 'Reactivate for {{strong}}%(price)s{{/strong}}', {
+								components: { strong: <strong /> },
+								args: { price: formattedPrice },
+						  } )
+						: this.props.translate( 'Renew for {{strong}}%(price)s{{/strong}}', {
+								components: { strong: <strong /> },
+								args: { price: formattedPrice },
+						  } ) }
+				</Button>
+			</React.Fragment>
+		);
+	}
+}
+
+export default connect( ( state, ownProps ) => {
+	return {
+		purchase: getByPurchaseId( state, ownProps.subscriptionId ),
+	};
+} )( localize( RenewButton ) );
