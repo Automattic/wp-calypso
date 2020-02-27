26276bff68ef031ce38e82c793e14e2ad66e8ace
Caleb Bauermeister <cbauerman@gmail.com>
2020-02-27 13:53:46 -0800

    refine sidebar, add filled settings icon

--- client/landing/jetpack-cloud/components/sidebar/index.jsx
+++ client/landing/jetpack-cloud/components/sidebar/index.jsx
@@ -3,14 +3,15 @@
  */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
+import { getSelectedSiteSlug } from 'state/ui/selectors';
 import config from 'config';
 import CurrentSite from '../current-site';
-import ExpandableSidebarMenu from 'layout/sidebar/expandable';
 import Sidebar from 'layout/sidebar';
 import SidebarFooter from 'layout/sidebar/footer';
 import SidebarItem from 'layout/sidebar/item';
@@ -25,6 +26,7 @@ import './style.scss';
 class JetpackCloudSidebar extends Component {
 	static propTypes = {
 		path: PropTypes.string.isRequired,
+		selectedSiteSlug: PropTypes.string,
 	};
 
 	state = {
@@ -56,11 +58,12 @@ class JetpackCloudSidebar extends Component {
 	/**
 	 * Check if a menu item is selected.
 	 *
-	 * @param   {string}  path Menu item path
+	 * @param {string} path Menu item path
+	 * @param {boolean} allowStartsWith if a path is considered selected if the current path starts with the given one
 	 * @returns {boolean}      True if menu item is selected
 	 */
-	isSelected( path ) {
-		return this.props.path === path;
+	isSelected( path, allowStartsWith = true ) {
+		return this.props.path === path || ( allowStartsWith && this.props.path.startsWith( path ) );
 	}
 
 	handleExpandableMenuClick( path ) {
@@ -72,7 +75,7 @@ class JetpackCloudSidebar extends Component {
 	};
 
 	render() {
-		const { translate } = this.props;
+		const { selectedSiteSlug, translate } = this.props;
 
 		return (
 			<Sidebar>
@@ -87,88 +90,44 @@ class JetpackCloudSidebar extends Component {
 							} ) }
 							materialIcon="dashboard"
 							materialIconStyle="filled"
-							selected={ this.isSelected( '/' ) }
+							selected={ this.isSelected( '/', false ) }
 						/>
 					</SidebarMenu>
 					{ config.isEnabled( 'jetpack-cloud/backups' ) && (
-						<ExpandableSidebarMenu
-							expanded={ this.isExpanded( '/backups' ) }
-							onClick={ this.handleExpandableMenuClick( '/backups' ) }
+						<SidebarItem
+							label={ translate( 'Backups', {
+								comment: 'Jetpack Cloud sidebar navigation item',
+							} ) }
+							link={ selectedSiteSlug ? `/backups/${ selectedSiteSlug }` : '/backups' }
+							onNavigate={ this.onNavigate }
 							materialIcon="backup"
 							materialIconStyle="filled"
-							title={ translate( 'Backups', { comment: 'Jetpack Cloud sidebar navigation item' } ) }
-						>
-							<ul>
-								<SidebarItem
-									label={ translate( 'Backups', {
-										comment: 'Jetpack Cloud sidebar navigation item',
-									} ) }
-									link="/backups"
-									onNavigate={ this.onNavigate }
-									selected={ this.isSelected( '/backups' ) }
-								/>
-								{ config.isEnabled( 'jetpack-cloud/backups-restore' ) && (
-									<SidebarItem
-										label={ translate( 'Restore site', {
-											comment: 'Jetpack Cloud / Backups sidebar navigation item',
-										} ) }
-										link="#" // @todo: Add /backup/restore route
-										onNavigate={ this.onNavigate }
-										selected={ this.isSelected( '/backups/restore' ) }
-									/>
-								) }
-								{ config.isEnabled( 'jetpack-cloud/settings' ) && (
-									<SidebarItem
-										label={ translate( 'Settings', {
-											comment: 'Jetpack Cloud / Backups sidebar navigation item',
-										} ) }
-										link="#" // @todo: Add backup/settings route
-										onNavigate={ this.onNavigate }
-										selected={ this.isSelected( '/backups/settings' ) }
-									/>
-								) }
-							</ul>
-						</ExpandableSidebarMenu>
+							selected={ this.isSelected( '/backups' ) }
+						/>
 					) }
 					{ config.isEnabled( 'jetpack-cloud/scan' ) && (
-						<ExpandableSidebarMenu
-							expanded={ this.isExpanded( '/scan' ) }
-							onClick={ this.handleExpandableMenuClick( '/scan' ) }
+						<SidebarItem
+							label={ translate( 'Scan', {
+								comment: 'Jetpack Cloud / Scan sidebar navigation item',
+							} ) }
+							link={ selectedSiteSlug ? `/scan/${ selectedSiteSlug }` : '/scan' }
+							onNavigate={ this.onNavigate }
 							materialIcon="security" // @todo: The Scan logo differs from the Material Icon used here
 							materialIconStyle="filled"
-							title={ translate( 'Scan', { comment: 'Jetpack Cloud sidebar navigation item' } ) }
-						>
-							<ul>
-								<SidebarItem
-									label={ translate( 'Scanner', {
-										comment: 'Jetpack Cloud / Scan sidebar navigation item',
-									} ) }
-									link="/scan"
-									onNavigate={ this.onNavigate }
-									selected={ this.isSelected( '/scan' ) }
-								/>
-								{ config.isEnabled( 'jetpack-cloud/scan-history' ) && (
-									<SidebarItem
-										label={ translate( 'History', {
-											comment: 'Jetpack Cloud / Scan sidebar navigation item',
-										} ) }
-										link="#" // @todo: Add /scan/history route
-										onNavigate={ this.onNavigate }
-										selected={ this.isSelected( '/scan/history' ) }
-									/>
-								) }
-								{ config.isEnabled( 'jetpack-cloud/settings' ) && (
-									<SidebarItem
-										label={ translate( 'Settings', {
-											comment: 'Jetpack Cloud / Scan sidebar navigation item',
-										} ) }
-										link="#" // @todo: Add /scan/settings route
-										onNavigate={ this.onNavigate }
-										selected={ this.isSelected( '/scan/settings' ) }
-									/>
-								) }
-							</ul>
-						</ExpandableSidebarMenu>
+							selected={ this.isSelected( '/scan' ) }
+						/>
+					) }
+					{ config.isEnabled( 'jetpack-cloud/settings' ) && (
+						<SidebarItem
+							label={ translate( 'Settings', {
+								comment: 'Jetpack Cloud / Backups sidebar navigation item',
+							} ) }
+							link={ selectedSiteSlug ? `/settings/${ selectedSiteSlug }` : '/settings' }
+							onNavigate={ this.onNavigate }
+							materialIcon="settings"
+							materialIconStyle="filled"
+							selected={ this.isSelected( '/settings' ) }
+						/>
 					) }
 				</SidebarRegion>
 				<SidebarFooter>
@@ -197,4 +156,6 @@ class JetpackCloudSidebar extends Component {
 	}
 }
 
-export default localize( JetpackCloudSidebar );
+export default connect( state => ( { selectedSiteSlug: getSelectedSiteSlug( state ) } ) )(
+	localize( JetpackCloudSidebar )
+);
