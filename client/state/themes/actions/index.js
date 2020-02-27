8010305a96c3eafbaea92392d1b62dec36a14089
Sérgio Gomes <mail@sgomes.com>
2020-02-27 09:53:18 +0000

    Sort alphabetically

--- client/state/themes/actions/index.js
+++ client/state/themes/actions/index.js
@@ -1,35 +1,35 @@
-export { setBackPath } from 'state/themes/actions/set-back-path';
-export { receiveThemes } from 'state/themes/actions/receive-themes';
-export { receiveTheme } from 'state/themes/actions/receive-theme';
-export { requestThemes } from 'state/themes/actions/request-themes';
-export { themeRequestFailure } from 'state/themes/actions/theme-request-failure';
-export { requestTheme } from 'state/themes/actions/request-theme';
-export { requestActiveTheme } from 'state/themes/actions/request-active-theme';
-export { themeActivated } from 'state/themes/actions/theme-activated';
-export { activateTheme } from 'state/themes/actions/activate-theme';
-export { installTheme } from 'state/themes/actions/install-theme';
-export { clearActivated } from 'state/themes/actions/clear-activated';
-export { tryAndCustomizeTheme } from 'state/themes/actions/try-and-customize-theme';
-export { installAndTryAndCustomizeTheme } from 'state/themes/actions/install-and-try-and-customize-theme';
-export { tryAndCustomize } from 'state/themes/actions/try-and-customize';
-export { installAndActivateTheme } from 'state/themes/actions/install-and-activate-theme';
-export { uploadTheme } from 'state/themes/actions/upload-theme';
-export { clearThemeUpload } from 'state/themes/actions/clear-theme-upload';
-export { deleteTheme } from 'state/themes/actions/delete-theme';
-export { confirmDelete } from 'state/themes/actions/confirm-delete';
-export { showAutoLoadingHomepageWarning } from 'state/themes/actions/show-auto-loading-homepage-warning';
-export { activate } from 'state/themes/actions/activate';
-export {
-	initiateThemeTransfer,
-	pollThemeTransferStatus,
-} from 'state/themes/actions/theme-transfer';
-export { setThemePreviewOptions } from 'state/themes/actions/set-theme-preview-options';
-export { showThemePreview } from 'state/themes/actions/show-theme-preview';
-export { hideAutoLoadingHomepageWarning } from 'state/themes/actions/hide-auto-loading-homepage-warning';
 export { acceptAutoLoadingHomepageWarning } from 'state/themes/actions/accept-auto-loading-homepage-warning';
+export { activate } from 'state/themes/actions/activate';
+export { activateTheme } from 'state/themes/actions/activate-theme';
+export { clearActivated } from 'state/themes/actions/clear-activated';
+export { clearThemeUpload } from 'state/themes/actions/clear-theme-upload';
+export { confirmDelete } from 'state/themes/actions/confirm-delete';
+export { deleteTheme } from 'state/themes/actions/delete-theme';
+export { hideAutoLoadingHomepageWarning } from 'state/themes/actions/hide-auto-loading-homepage-warning';
 export { hideThemePreview } from 'state/themes/actions/hide-theme-preview';
-export { requestThemeFilters } from 'state/themes/actions/request-theme-filters';
+export { installAndActivateTheme } from 'state/themes/actions/install-and-activate-theme';
+export { installAndTryAndCustomizeTheme } from 'state/themes/actions/install-and-try-and-customize-theme';
+export { installTheme } from 'state/themes/actions/install-theme';
+export { receiveTheme } from 'state/themes/actions/receive-theme';
+export { receiveThemes } from 'state/themes/actions/receive-themes';
 export {
 	getRecommendedThemes,
 	receiveRecommendedThemes,
 } from 'state/themes/actions/recommended-themes';
+export { requestActiveTheme } from 'state/themes/actions/request-active-theme';
+export { requestTheme } from 'state/themes/actions/request-theme';
+export { requestThemeFilters } from 'state/themes/actions/request-theme-filters';
+export { requestThemes } from 'state/themes/actions/request-themes';
+export { setBackPath } from 'state/themes/actions/set-back-path';
+export { setThemePreviewOptions } from 'state/themes/actions/set-theme-preview-options';
+export { showAutoLoadingHomepageWarning } from 'state/themes/actions/show-auto-loading-homepage-warning';
+export { showThemePreview } from 'state/themes/actions/show-theme-preview';
+export { themeActivated } from 'state/themes/actions/theme-activated';
+export { themeRequestFailure } from 'state/themes/actions/theme-request-failure';
+export {
+	initiateThemeTransfer,
+	pollThemeTransferStatus,
+} from 'state/themes/actions/theme-transfer';
+export { tryAndCustomize } from 'state/themes/actions/try-and-customize';
+export { tryAndCustomizeTheme } from 'state/themes/actions/try-and-customize-theme';
+export { uploadTheme } from 'state/themes/actions/upload-theme';
