8381bea7bbbaa18efb82674f8edf0c88e2fd3fd6
Sérgio Gomes <mail@sgomes.com>
2020-02-27 11:50:49 +0000

    Split out trashPost

--- client/state/posts/actions/index.js
+++ client/state/posts/actions/index.js
@@ -16,8 +16,6 @@ import {
 	POST_RESTORE,
 	POST_RESTORE_FAILURE,
 	POST_RESTORE_SUCCESS,
-	POST_SAVE,
-	POST_SAVE_FAILURE,
 } from 'state/action-types';
 import { getSitePost, getEditedPost, getPostEdits, isEditedPostDirty } from 'state/posts/selectors';
 import { recordSaveEvent } from 'state/posts/stats';
@@ -67,58 +65,7 @@ export { updatePostMetadata } from 'state/posts/actions/update-post-metadata';
 export { deletePostMetadata } from 'state/posts/actions/delete-post-metadata';
 export { savePostSuccess } from 'state/posts/actions/save-post-success';
 export { savePost } from 'state/posts/actions/save-post';
-
-/**
- * Returns an action thunk which, when dispatched, triggers a network request
- * to trash the specified post.
- *
- * @param  {number}   siteId Site ID
- * @param  {number}   postId Post ID
- * @returns {Function}        Action thunk
- */
-export function trashPost( siteId, postId ) {
-	return dispatch => {
-		// Trashing post is almost equivalent to saving the post with status field set to `trash`
-		// and the action behaves like it was doing exactly that -- it dispatches the `POST_SAVE_*`
-		// actions with the right properties.
-		//
-		// But what we really do is to call the `wpcom.site().post().delete()` method, i.e., sending
-		// a `POST /sites/:site/posts/:post/delete` request. The difference is that an explicit delete
-		// will set a `_wp_trash_meta_status` meta property on the post and a later `restore` call
-		// can restore the original post status, i.e., `publish`. Without this, the post will be always
-		// recovered as `draft`.
-		const post = { status: 'trash' };
-
-		dispatch( {
-			type: POST_SAVE,
-			siteId,
-			postId,
-			post,
-		} );
-
-		const trashResult = wpcom
-			.site( siteId )
-			.post( postId )
-			.delete();
-
-		trashResult.then(
-			savedPost => {
-				dispatch( savePostSuccess( siteId, postId, savedPost, post ) );
-				dispatch( receivePost( savedPost ) );
-			},
-			error => {
-				dispatch( {
-					type: POST_SAVE_FAILURE,
-					siteId,
-					postId,
-					error,
-				} );
-			}
-		);
-
-		return trashResult;
-	};
-}
+export { trashPost } from 'state/posts/actions/trash-post';
 
 /**
  * Returns an action thunk which, when dispatched, triggers a network request
