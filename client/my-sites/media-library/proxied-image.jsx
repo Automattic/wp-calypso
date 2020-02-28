b854bfd3a8c0c1f42551cc9fc5aaa9350ff46bee
Jeff Bowen <jblz@users.noreply.github.com>
2020-02-28 13:54:50 -0500

    Use the cached data from previous fetches on subsequent mounts

--- client/my-sites/media-library/proxied-image.jsx
+++ client/my-sites/media-library/proxied-image.jsx
@@ -7,10 +7,21 @@ import { noop } from 'lodash';
 /**
  * Internal dependencies
  */
+import debugFactory from 'debug';
 import { http } from 'state/data-layer/wpcom-http/actions';
-import { requestHttpData, resetHttpData } from 'state/data-layer/http-data';
+import { getHttpData, requestHttpData } from 'state/data-layer/http-data';
 
+const debug = debugFactory( 'calypso:my-sites:media-library:proxied-image' );
 const { Blob } = globalThis; // The linter complains if I don't do this...?
+
+const getUrlFromBlob = blob => ( blob instanceof Blob ? URL.createObjectURL( blob ) : undefined );
+const fetchProxiedMediaFile = ( siteSlug, mediaUrl ) =>
+	http( {
+		method: 'GET',
+		apiNamespace: 'wpcom/v2',
+		path: `/sites/${ siteSlug }/atomic-auth-proxy/file${ mediaUrl }`,
+		responseType: 'blob',
+	} );
 
 export default function ProxiedImage( {
 	alt,
@@ -20,38 +31,41 @@ export default function ProxiedImage( {
 	siteSlug,
 	style,
 } ) {
-	const [ imageData, setImageData ] = useState( null );
-
 	const requestId = `media-library-proxied-image-${ siteSlug }${ mediaUrl }`;
 
+	const [ imageObjectUrl, setImageObjectUrl ] = useState( null );
+
 	useEffect( () => {
-		requestHttpData(
-			requestId,
-			http( {
-				method: 'GET',
-				apiNamespace: 'wpcom/v2',
-				path: `/sites/${ siteSlug }/atomic-auth-proxy/file${ mediaUrl }`,
-				responseType: 'blob',
-			} ),
-			{
-				fromApi: () => payload => {
-					const blobUrl = payload instanceof Blob ? URL.createObjectURL( payload ) : undefined;
-					setImageData( blobUrl );
-					return [ [ requestId, payload ] ];
-				},
-			}
-		);
+		if ( imageObjectUrl === null ) {
+			const cachedImageBlob = getHttpData( requestId )?.data;
+			const url = getUrlFromBlob( cachedImageBlob );
+			setImageObjectUrl( url );
+			debug( 'set image from cache', { url } );
+		}
+
+		requestHttpData( requestId, fetchProxiedMediaFile( siteSlug, mediaUrl ), {
+			freshness: 30000,
+			fromApi: () => payload => {
+				const imageBlob = payload;
+				const url = getUrlFromBlob( imageBlob );
+				setImageObjectUrl( url );
+				debug( 'got image from API', { payload, url } );
+				return [ [ requestId, payload ] ];
+			},
+		} );
 
 		return () => {
-			// Make sure stored Blobs get cleared from memory when the component dismounts
-			URL.revokeObjectURL( imageData );
-			resetHttpData( requestId );
+			if ( ! imageObjectUrl ) {
+				return;
+			}
+			debug( 'Cleared blob from memory on dismount: ' + imageObjectUrl );
+			URL.revokeObjectURL( imageObjectUrl );
 		};
-	}, [ imageData, mediaUrl, requestId, siteSlug ] );
+	}, [ imageObjectUrl, mediaUrl, requestId, siteSlug ] );
 
 	return (
 		<img
-			src={ imageData }
+			src={ imageObjectUrl }
 			onLoad={ onLoad }
 			alt={ alt }
 			style={ style }
