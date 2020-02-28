d63445ffa271cd83fee8e4985b3c956ce7006865
Dave Smith <getdavemail@gmail.com>
2020-02-28 15:40:09 +0000

    Fix comments

--- apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/block-preview.js
+++ apps/full-site-editing/full-site-editing-plugin/starter-page-templates/page-template-modal/components/block-preview.js
@@ -13,11 +13,11 @@
 import { BlockPreview } from '@wordpress/block-editor';
 /* eslint-enable import/no-extraneous-dependencies */
 
-// Exists as a pass through component to simplying testing
+// Exists as a pass through component to simplify testing
 // components which consume `BlockPreview` from
 // `@wordpress/block-editor`. This is because jest cannot mock
 // node modules that are not part of the root node modules.
-// Due to the way this projects dependencies are defined
+// Due to the way this project's dependencies are defined
 // `@wordpress/block-editor` does not exist within `node_modules`
 // and it is there impossible to mock it without providing a wrapping
 // component to act as a pass though.
