a934293cb61e458dd0eac3dd6ffe67959765feb1
Marin Atanasov <tyxla@abv.bg>
2020-03-01 16:53:04 +0200

    State: Add tests for isSavingWordadsSettings selector

---
+++ client/state/selectors/test/is-saving-wordads-settings.js
@@ -0,0 +1,27 @@
+/**
+ * Internal dependencies
+ */
+import isSavingWordadsSettings from 'state/selectors/is-saving-wordads-settings';
+
+describe( 'isSavingWordadsSettings()', () => {
+	const siteId = 12345678;
+	const state = {
+		wordads: {
+			settings: {
+				requests: {
+					[ siteId ]: true,
+				},
+			},
+		},
+	};
+
+	test( 'should return false for an unknown site', () => {
+		const output = isSavingWordadsSettings( state, 87654321 );
+		expect( output ).toBe( false );
+	} );
+
+	test( 'should return the request state for a known site', () => {
+		const output = isSavingWordadsSettings( state, siteId );
+		expect( output ).toBe( true );
+	} );
+} );
