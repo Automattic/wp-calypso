c43bb77c99234aa978dc0c913375b2f310091598
Marin Atanasov <tyxla@abv.bg>
2020-03-01 16:49:05 +0200

    State: Introduce isSavingWordadsSettings selector

---
+++ client/state/selectors/is-saving-wordads-settings.js
@@ -0,0 +1,10 @@
+/**
+ * Returns true if we are saving the Wordads settings for the specified site ID, false otherwise.
+ *
+ * @param  {object}  state  Global state tree
+ * @param  {number}  siteId Site ID
+ * @returns {boolean}       Whether site's Wordads settings are being saved
+ */
+export default function isSavingWordadsSettings( state, siteId ) {
+	return state.wordads.settings.requests[ siteId ] ?? false;
+}
