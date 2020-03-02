3f6e059b1b81fa050b039aeebb65f29543ca0f32
Caleb Bauermeister <cbauerman@gmail.com>
2020-03-02 08:57:11 -0800

    add header

--- client/landing/jetpack-cloud/sections/settings/main.jsx
+++ client/landing/jetpack-cloud/sections/settings/main.jsx
@@ -2,20 +2,22 @@
  * External dependencies
  */
 import { useSelector } from 'react-redux';
+import { useTranslate } from 'i18n-calypso';
 import React from 'react';
 
 /**
  * Internal dependencies
  */
 import { getSelectedSiteId } from 'state/ui/selectors';
+import FormattedHeader from 'components/formatted-header';
 import getRewindState from 'state/selectors/get-rewind-state';
 import getRewindStateRequestStatus from 'state/selectors/get-rewind-state-request-status';
-
 import QueryRewindState from 'components/data/query-rewind-state';
 import ServerConnectionIndicator from '../../components/server-connection-indicator';
 import ServerConnectionIndicatorPlaceholder from '../../components/server-connection-indicator/placeholder';
 
 const SettingsPage = () => {
+	const translate = useTranslate();
 	const selectedSiteId = useSelector( getSelectedSiteId );
 	const rewindStateRequestStatus = useSelector( state =>
 		getRewindStateRequestStatus( state, selectedSiteId )
@@ -27,7 +29,12 @@ const SettingsPage = () => {
 	return (
 		<div>
 			{ selectedSiteId && <QueryRewindState siteId={ selectedSiteId } /> }
-			{ /* @todo: actual placeholder component here */ }
+
+			<FormattedHeader
+				align="left"
+				headerText={ translate( 'Server connection details' ) }
+				isSecondary
+			/>
 			{ rewindStateRequestStatus === 'success' ? (
 				<ServerConnectionIndicator isConnected={ isConnected } />
 			) : (
