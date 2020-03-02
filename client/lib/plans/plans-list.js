a8daaa4227f2e1e4c7c2c65e7bbb15dc89c340b5
Echo G <ChaosExAnima@users.noreply.github.com>
2020-03-02 15:25:19 -0500

    Simplifies conditional check.

--- client/lib/plans/plans-list.js
+++ client/lib/plans/plans-list.js
@@ -673,9 +673,6 @@ export const PLANS_LIST = {
 			switch ( feature ) {
 				case constants.FEATURE_JETPACK_BACKUP_DAILY:
 				case constants.FEATURE_JETPACK_BACKUP_DAILY_MONTHLY:
-					return i18n.translate(
-						'Upgrade your site to access additional features, including spam protection and priority support.'
-					);
 				case constants.FEATURE_JETPACK_BACKUP_REALTIME:
 				case constants.FEATURE_JETPACK_BACKUP_REALTIME_MONTHLY:
 					return i18n.translate(
