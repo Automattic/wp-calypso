3df028f4403e4f2b7a59218f4ea2dd1b1c295219
Echo G <ChaosExAnima@users.noreply.github.com>
2020-03-02 15:24:01 -0500

    Adjusts so backups is mentioned only if no backup plan is in place.

--- client/lib/plans/plans-list.js
+++ client/lib/plans/plans-list.js
@@ -674,16 +674,16 @@ export const PLANS_LIST = {
 				case constants.FEATURE_JETPACK_BACKUP_DAILY:
 				case constants.FEATURE_JETPACK_BACKUP_DAILY_MONTHLY:
 					return i18n.translate(
-						'Upgrade your site to access additional features, including spam protection, and priority support.'
+						'Upgrade your site to access additional features, including spam protection and priority support.'
 					);
 				case constants.FEATURE_JETPACK_BACKUP_REALTIME:
 				case constants.FEATURE_JETPACK_BACKUP_REALTIME_MONTHLY:
 					return i18n.translate(
-						'Upgrade your site to access additional features, including spam protection, and priority support.'
+						'Upgrade your site to access additional features, including spam protection and priority support.'
 					);
 				default:
 					return i18n.translate(
-						'Upgrade your site to access additional features, including spam protection, and priority support.'
+						'Upgrade your site to access additional features, including spam protection, backups, and priority support.'
 					);
 			}
 		},
