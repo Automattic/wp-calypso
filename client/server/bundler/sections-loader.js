dd05bd93d7fdfd7daea196ebadf6c2c27b8b8ada
Enej Bajgoric <enej.bajgoric@automattic.com>
2020-03-02 15:23:21 +0100

    use length instead of count

--- client/server/bundler/sections-loader.js
+++ client/server/bundler/sections-loader.js
@@ -79,7 +79,7 @@ const loader = function() {
 			printSectionsAndPaths( allSections );
 		}
 	} else {
-		console.log( `[sections-loader] used ${ sections.count } of section.` );
+		console.log( `[sections-loader] used ${ sections.length } of section.` );
 		printSectionsAndPaths( sections );
 	}
 
