f5fb90e89d32c7cd0410597528ed7a3970b2bcce
Rob Landers <landers.robert@gmail.com>
2020-03-03 16:16:42 +0100

    Remove import lines

--- client/state/experiments/actions.ts
+++ client/state/experiments/actions.ts
@@ -1,9 +1,8 @@
 /**
  * Internal Dependencies
  */
-import { EXPERIMENT_FETCH, EXPERIMENT_ASSIGN } from 'state/action-types';
+import { EXPERIMENT_ASSIGN } from 'state/action-types';
 import { ExperimentResponse } from 'state/experiments/types';
-import { getAnonIdFromCookie } from './reducer';
 
 /**
  * Assign the user to the specified experiments
