c984c4795dd0b041668a13c0385b5ef1c137229d
nbloomf <nathan.bloomfield@automattic.com>
2020-03-03 12:01:20 -0600

    Add managed error messages to EuAddressFieldset

--- client/components/domains/contact-details-form-fields/custom-form-fieldsets/eu-address-fieldset.jsx
+++ client/components/domains/contact-details-form-fields/custom-form-fieldsets/eu-address-fieldset.jsx
@@ -12,11 +12,19 @@ import { localize } from 'i18n-calypso';
 import { Input } from 'my-sites/domains/components/form';
 
 const EuAddressFieldset = props => {
-	const { getFieldProps, translate } = props;
+	const { getFieldProps, translate, contactDetailsErrors } = props;
 	return (
 		<div className="custom-form-fieldsets__address-fields eu-address-fieldset">
-			<Input label={ translate( 'Postal Code' ) } { ...getFieldProps( 'postal-code' ) } />
-			<Input label={ translate( 'City' ) } { ...getFieldProps( 'city' ) } />
+			<Input
+				label={ translate( 'Postal Code' ) }
+				errorMessage={ contactDetailsErrors?.postalCode }
+				{ ...getFieldProps( 'postal-code' ) }
+			/>
+			<Input
+				label={ translate( 'City' ) }
+				errorMessage={ contactDetailsErrors?.city }
+				{ ...getFieldProps( 'city' ) }
+			/>
 		</div>
 	);
 };
@@ -24,11 +32,13 @@ const EuAddressFieldset = props => {
 EuAddressFieldset.propTypes = {
 	getFieldProps: PropTypes.func,
 	translate: PropTypes.func,
+	contactDetailsErrors: PropTypes.object,
 };
 
 EuAddressFieldset.defaultProps = {
 	getFieldProps: noop,
 	translate: identity,
+	contactDetailsErrors: {},
 };
 
 export default localize( EuAddressFieldset );
