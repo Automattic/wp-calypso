c68145126cd5e83548d0663ba9357cbc41dfb3c2
Michael Cain <cain@automattic.com>
2020-03-04 12:48:11 -0500

    Composite Checkout: add busy button (#39816)
    * Read-through.
    
    * Cleanup.
    
    * - add isBusy prop
    - add state, type, and busy class names
    - add busy animation
    - fix IDE whitespace issues
    
    * Add busy button to other payment methods.
    
    * Use busy button when steps are validating.

--- packages/composite-checkout/src/components/checkout-steps.js
+++ packages/composite-checkout/src/components/checkout-steps.js
@@ -326,6 +326,7 @@ export function CheckoutStepBody( {
 							}
 							buttonState={ formStatus !== 'ready' ? 'disabled' : 'primary' }
 							disabled={ formStatus !== 'ready' }
+							isBusy={ formStatus === 'validating' }
 						/>
 					) }
 				</StepContentUI>
