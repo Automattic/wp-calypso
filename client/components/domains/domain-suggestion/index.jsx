33888c242b8669b9ec13cd41092d4b6fbaf0302a
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-03-04 18:52:29 +0530

    Removing the design update changes from filter and product price sections

--- client/components/domains/domain-suggestion/index.jsx
+++ client/components/domains/domain-suggestion/index.jsx
@@ -45,8 +45,6 @@ class DomainSuggestion extends React.Component {
 			price,
 			priceRule,
 			salePrice,
-			showTestCopy,
-			showDesignUpdate,
 			isEligibleVariantForDomainTest,
 			isFeatured,
 		} = this.props;
@@ -62,7 +60,7 @@ class DomainSuggestion extends React.Component {
 		);
 
 		const contentClassName = classNames( 'domain-suggestion__content', {
-			'domain-suggestion__content-domain-copy-test': showTestCopy && ! isFeatured,
+			'domain-suggestion__content-domain-copy-test': isEligibleVariantForDomainTest && ! isFeatured,
 		} );
 
 		/* eslint-disable jsx-a11y/click-events-have-key-events */
@@ -83,8 +81,6 @@ class DomainSuggestion extends React.Component {
 							salePrice={ salePrice }
 							rule={ priceRule }
 							isFeatured={ isFeatured }
-							showTestCopy={ showTestCopy }
-							showDesignUpdate={ showDesignUpdate }
 							isEligibleVariantForDomainTest={ isEligibleVariantForDomainTest }
 							domain={ this.props.domain }
 						/>
