51762409be5d8ca3b07871121f92a1fcf2b8dfc4
Andrew Serong <andrewserong@users.noreply.github.com>
2020-03-04 18:08:54 +1100

    Silence no-unused-vars error for the domain parameter of getExistingSite selector

--- packages/data-stores/src/site/selectors.ts
+++ packages/data-stores/src/site/selectors.ts
@@ -10,6 +10,15 @@ export const getNewSiteError = ( state: State ) => state.newSite.error;
 export const isFetchingSite = ( state: State ) => state.newSite.isFetching;
 export const isNewSite = ( state: State ) => !! state.newSite.data;
 
-export const getExistingSite = ( state: State ) => {
+/**
+ * Get an existing site matched by domain string. This selector has a matching
+ * resolver that uses the `domain` parameter. The parameter is included here
+ * even though it is not used, to ensure consuming code doesn't error out.
+ *
+ * @param state {State}		state object
+ * @param _domain {string}	unused variable, exists here only to match the resolver
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getExistingSite = ( state: State, _domain: string ) => {
 	return state.existingSite;
 };
