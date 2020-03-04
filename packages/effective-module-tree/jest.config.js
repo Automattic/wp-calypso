65b077660388cc091544e360e1eb1cfa63dfbdb7
Sergio Cinos <sergio.cinos@gmail.com>
2020-03-04 11:52:46 +0100

    Do not transform node_modules for jest tests

--- packages/effective-module-tree/jest.config.js
+++ packages/effective-module-tree/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
 	preset: '@automattic/calypso-build',
 	rootDir: __dirname,
 	// Node project, no need to transform anything
-	transformIgnorePatterns: [ '<rootDir>/' ],
+	transformIgnorePatterns: [ '<rootDir>/', '/node_modules/' ],
 };
