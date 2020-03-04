b68921744dd51ab5d5a1e3d83cc4fbb6382cc276
roo2 <roo2@protonmail.com>
2020-03-04 14:35:04 +1100

    handle errors on login

--- client/landing/gutenboarding/onboarding-block/login-form/index.tsx
+++ client/landing/gutenboarding/onboarding-block/login-form/index.tsx
@@ -6,6 +6,7 @@ import { Button, ExternalLink, TextControl, Modal, Notice } from '@wordpress/com
 import { useDispatch, useSelect } from '@wordpress/data';
 import { __experimentalCreateInterpolateElement } from '@wordpress/element';
 import { useI18n } from '@automattic/react-i18n';
+
 /**
  * Internal dependencies
  */
@@ -21,16 +22,16 @@ declare module '@wordpress/element' {
 	): ReactNode;
 }
 
-const SignupForm = () => {
+const LoginForm = () => {
 	const { __: NO__, _x: NO_x } = useI18n();
 	const [ usernameOrEmailVal, setUsernameOrEmailVal ] = useState( '' );
 	const [ passwordVal, setPasswordVal ] = useState( '' );
 	const { submitUsernameOrEmail } = useDispatch( AUTH_STORE );
 	const { submitPassword } = useDispatch( AUTH_STORE );
 	const loginFlowState = useSelect( select => select( AUTH_STORE ).getLoginFlowState() );
+	const errors = useSelect( select => select( AUTH_STORE ).getErrors() );
 
-	// some kind of loading state like isFetchingNewUser
-	// todo: useSelect to get login errors from auth store
+	// todo: loading state like isFetchingNewUser
 
 	const handleLogin = ( event: React.FormEvent< HTMLFormElement > ) => {
 		event.preventDefault();
@@ -40,16 +41,12 @@ const SignupForm = () => {
 			submitUsernameOrEmail( usernameOrEmailVal );
 		} else if ( loginFlowState === 'ENTER_PASSWORD' ) {
 			submitPassword( passwordVal );
-			//todo: handle success?
+			//todo: handle success
 		}
 	};
 
 	const handleClose = () => {
-		// work out the equivilant of this code
-		// if ( shouldCreate ) {
-		// 	setShouldCreate( false );
-		// 	history.goBack();
-		// }
+		// todo
 	};
 
 	const tos = __experimentalCreateInterpolateElement(
@@ -58,25 +55,6 @@ const SignupForm = () => {
 			link_to_tos: <ExternalLink href="https://wordpress.com/tos/" />,
 		}
 	);
-
-	let errorMessage: string | undefined;
-
-	// todo: handle error messages like this:
-	// if ( newUserError ) {
-	// 	switch ( newUserError.error ) {
-	// 		case 'already_taken':
-	// 		case 'already_active':
-	// 		case 'email_exists':
-	// 			errorMessage = NO__( 'An account with this email address already exists.' );
-	// 			break;
-
-	// 		default:
-	// 			errorMessage = NO__(
-	// 				'Sorry, something went wrong when trying to create your account. Please try again.'
-	// 			);
-	// 			break;
-	// 	}
-	// }
 
 	// todo: may need to be updated as more states are handled
 	const shouldShowPasswordField = loginFlowState === 'ENTER_PASSWORD';
@@ -92,7 +70,7 @@ const SignupForm = () => {
 				<TextControl
 					label={ NO__( 'Email Address or Username' ) }
 					value={ usernameOrEmailVal }
-					// todo: dissable when saving
+					// disabled={ isLoading }
 					onChange={ setUsernameOrEmailVal }
 					placeholder={ NO_x(
 						'E.g., yourname@email.com',
@@ -107,28 +85,31 @@ const SignupForm = () => {
 				>
 					<TextControl
 						label={ NO__( 'Password' ) }
-						// todo: dissable when saving
+						// disabled={ isLoading }
 						type="password"
 						value={ passwordVal }
 						onChange={ setPasswordVal }
 					/>
-
-					{ /* { requestError && requestError.field === 'password' && (
-								<FormInputValidation isError text={ requestError.message } />
-							) } */ }
 				</div>
-				{ errorMessage && (
-					<Notice className="login-form__error-notice" status="error" isDismissible={ false }>
-						{ errorMessage }
-					</Notice>
-				) }
+				{ errors &&
+					errors.map( ( error, i ) => (
+						<Notice
+							className="login-form__error-notice"
+							status="error"
+							isDismissible={ false }
+							key={ error.code + i }
+						>
+							{ error.message }
+						</Notice>
+					) ) }
 				<div className="login-form__footer">
 					<p className="login-form__terms-of-service-link">{ tos }</p>
 
 					<Button
 						type="submit"
 						className="login-form__submit"
-						// todo: dissable and set `isBusy` when saving
+						// disabled={ isLoading }
+						// isBusy={ isLoading }
 						isPrimary
 					>
 						{ NO__( 'Login' ) }
@@ -139,4 +120,4 @@ const SignupForm = () => {
 	);
 };
 
-export default SignupForm;
+export default LoginForm;
