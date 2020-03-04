b25b296092c91801e30dcbe1d3c909824634855b
Echo G <ChaosExAnima@users.noreply.github.com>
2020-03-04 13:33:19 -0500

    Loads styles and logic into main scan page.

--- client/landing/jetpack-cloud/sections/scan/main.jsx
+++ client/landing/jetpack-cloud/sections/scan/main.jsx
@@ -3,16 +3,56 @@
  */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Button } from '@automattic/components';
+import { translate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
+import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
+import { withLocalizedMoment } from 'components/localized-moment';
+import SecurityIcon from 'landing/jetpack-cloud/components/security-icon';
 import StatsFooter from 'landing/jetpack-cloud/components/stats-footer';
-import { getSelectedSiteId } from 'state/ui/selectors';
+
+import './style.scss';
 
 class ScanPage extends Component {
 	renderScanOkay() {
-		return <p>Everything is okay!</p>;
+		const { siteSlug, moment, lastScanTimestamp, nextScanTimestamp } = this.props;
+
+		let nextScanString = translate( 'tomorrow' );
+		if ( moment( nextScanTimestamp ).isSame( Date.now(), 'd' ) ) {
+			nextScanString = translate( 'later today' );
+		}
+
+		return (
+			<>
+				<SecurityIcon className="scan__icon" />
+				<h1 className="scan__header scan__header--okay">
+					{ translate( 'Don’t worry about a thing' ) }
+				</h1>
+				<p>
+					{ translate(
+						'The last Jetpack scan ran {{strong}}%(time)s{{/strong}} and everything looked great.' +
+							' Run a manual scan now or wait for Jetpack to scan your site %(nextScan)s.',
+						{
+							args: {
+								time: moment( lastScanTimestamp ).fromNow(),
+								nextScan: nextScanString,
+							},
+							components: { strong: <strong /> },
+						}
+					) }
+				</p>
+				<Button
+					primary
+					href={ `/scan/${ siteSlug }/?scan-state=scanning` }
+					className="scan__button"
+				>
+					{ translate( 'Scan now' ) }
+				</Button>
+			</>
+		);
 	}
 
 	renderScanning() {
@@ -43,7 +83,6 @@ class ScanPage extends Component {
 	render() {
 		return (
 			<div>
-				Welcome to the scan page for site { this.props.siteId }
 				{ this.renderScanState() }
 				<StatsFooter
 					header="Scan Summary"
@@ -62,13 +101,20 @@ class ScanPage extends Component {
 
 export default connect( state => {
 	const siteId = getSelectedSiteId( state );
+	const siteSlug = getSelectedSiteSlug( state );
 
 	// TODO: Get state from actual API.
 	const params = new URL( document.location ).searchParams;
 	const scanState = params.get( 'scan-state' ) || 'okay';
 
+	const lastScanTimestamp = Date.now() - 5700000; // 1h 35m.
+	const nextScanTimestamp = Date.now() + 5700000;
+
 	return {
 		siteId,
+		siteSlug,
 		scanState,
+		lastScanTimestamp,
+		nextScanTimestamp,
 	};
-} )( ScanPage );
+} )( withLocalizedMoment( ScanPage ) );
