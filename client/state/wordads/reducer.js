b0bd9df3ed9f070528b7b9ff775885ad0af3f9e1
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2020-03-05 17:19:43 +0200

    State: Introduce Redux version of the ads settings store (#39800)
    * State: Add Wordads settings action types
    
    * State: Add Wordads settings action creators
    
    * State: Add Wordads settings items schema
    
    * State: Add Wordads settings reducer
    
    * State: Add settings reducer to the main Wordads reducer
    
    * State: Introduce getWordadsSettings selector
    
    * State: Introduce isSavingWordadsSettings selector
    
    * State: Add Wordads settings data layer
    
    * State: Load Wordads settings data layer with its action creators
    
    * Components: Introduce QueryWordadsSettings component
    
    * State: Add tests for Wordads settings reducer
    
    * State: Add tests for getWordadsSettings selector
    
    * State: Add tests for isSavingWordadsSettings selector

--- client/state/wordads/reducer.js
+++ client/state/wordads/reducer.js
@@ -5,10 +5,12 @@
 import { combineReducers } from 'state/utils';
 import approve from './approve/reducer';
 import earnings from './earnings/reducer';
+import settings from './settings/reducer';
 import status from './status/reducer';
 
 export default combineReducers( {
 	approve,
 	earnings,
+	settings,
 	status,
 } );
