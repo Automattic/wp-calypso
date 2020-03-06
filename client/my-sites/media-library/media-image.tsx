d5e86f9fcab9c8f38127b92ba973cd8f1431fd4e
Adam Zieliński <adam@adamziel.com>
2020-03-06 12:17:26 +0100

    Quick&Dirty stab at proxied media images

--- client/my-sites/media-library/media-image.tsx
+++ client/my-sites/media-library/media-image.tsx
@@ -1,87 +1,73 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { noop } from 'lodash';
 import { connect } from 'react-redux';
-import ImagePreloader from 'components/image-preloader';
 
 /**
  * Internal dependencies
  */
+import debugFactory from 'debug';
+import { http } from 'state/data-layer/wpcom-http/actions';
 import { getUrlParts } from 'lib/url/url-parts';
+import { getHttpData, requestHttpData } from 'state/data-layer/http-data';
 import isPrivateSite from 'state/selectors/is-private-site';
 import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
 import getSelectedSiteId from 'state/ui/selectors/get-selected-site-id';
 import getSelectedSiteSlug from 'state/ui/selectors/get-selected-site-slug';
-import ProxiedImage from './proxied-image';
+
+const debug = debugFactory( 'calypso:my-sites:media-library:proxied-image' );
+const { Blob } = globalThis; // The linter complains if I don't do this...?
+
+const getUrlFromBlob = ( blob: Blob ) =>
+	blob instanceof Blob ? URL.createObjectURL( blob ) : undefined;
+const fetchProxiedMediaFile = ( siteSlug: string, src: string ) =>
+	http( {
+		method: 'GET',
+		apiNamespace: 'wpcom/v2',
+		path: `/sites/${ siteSlug }/atomic-auth-proxy/file${ src }`,
+		responseType: 'blob',
+	} );
 
 const parseMediaURL = ( url: string, siteSlug: string ) => {
-	const { pathname, search: query, hostname } = getUrlParts( url );
-	let filePath = pathname;
-	let isRelativeToSiteRoot = true;
-	if (
-		hostname !== siteSlug &&
-		( hostname.endsWith( 'wp.com' ) || hostname.endsWith( 'wordpress.com' ) )
-	) {
-		const [ first, ...rest ] = filePath.substr( 1 ).split( '/' );
-		filePath = '/' + rest.join( '/' );
-
-		if ( first !== siteSlug ) {
-			isRelativeToSiteRoot = false;
+	const { pathname, hostname } = getUrlParts( url );
+	if ( hostname !== siteSlug ) {
+		if ( hostname.endsWith( 'wp.com' ) || hostname.endsWith( 'wordpress.com' ) ) {
+			const [ first, ...rest ] = pathname.substr( 1 ).split( '/' );
+			if ( first !== siteSlug ) {
+				return {
+					isRelativeToSiteRoot: false,
+				};
+			}
 		}
 	}
 
 	return {
-		query,
-		filePath,
-		isRelativeToSiteRoot,
+		relativePath: pathname,
+		isRelativeToSiteRoot: true,
 	};
 };
 
 interface Props {
 	src: string;
-
-	filePath: string;
-	query: string;
 	siteSlug: string;
 	onLoad: () => any;
-	placeholder: React.ReactNode | null;
 	useProxy: boolean;
-	dispatch: any;
 }
 
 const MediaImage: React.FC< Props > = function MediaImage( {
 	src,
-	query,
-	filePath,
+	relativePath,
 	siteSlug,
 	useProxy = false,
-	placeholder = null,
-	dispatch,
 	...rest
 } ) {
 	if ( useProxy ) {
-		return (
-			<ProxiedImage
-				siteSlug={ siteSlug }
-				filePath={ filePath }
-				query={ query }
-				placeholder={ placeholder }
-				{ ...rest }
-			/>
-		);
+		return <ProxiedImage siteSlug={ siteSlug } relativePath={ relativePath } { ...rest } />;
 	}
 
-	if ( placeholder ) {
-		return <ImagePreloader placeholder={ placeholder } src={ src } { ...rest } />;
-	}
-
-	/* eslint-disable-next-line jsx-a11y/alt-text */
 	return <img src={ src } { ...rest } />;
-};
-
-MediaImage.defaultProps = {
-	placeholder: null,
 };
 
 export default connect( ( state, { src }: Pick< Props, 'src' > ) => {
@@ -89,13 +75,54 @@ export default connect( ( state, { src }: Pick< Props, 'src' > ) => {
 	const siteSlug = getSelectedSiteSlug( state ) as string;
 	const isAtomic = !! isSiteAutomatedTransfer( state, siteId as number );
 	const isPrivate = !! isPrivateSite( state, siteId );
-	const { filePath, query, isRelativeToSiteRoot } = parseMediaURL( src, siteSlug );
-	const useProxy = ( isAtomic && isPrivate && filePath && isRelativeToSiteRoot ) as boolean;
+	const { relativePath, isRelativeToSiteRoot } = parseMediaURL( src, siteSlug );
 
+	const useProxy =
+		// Site privacy / coming soon are getting clobbered when the media lib loads
+		// Should be fixed when D39264-code lands
+		// Hard-coding for now so I can keep working...
+		( true || ( isAtomic && isPrivate ) ) && isRelativeToSiteRoot;
+	/*
+  : hostname === siteSlug && pathname.length > 0 && (true || isAtomic && isPrivate),
+   */
 	return {
-		query,
 		siteSlug,
 		useProxy,
-		filePath,
+		relativePath,
 	};
 } )( MediaImage );
+
+const ProxiedImage: React.FC< Props > = function MediaImage( { relativePath, siteSlug, ...rest } ) {
+	const [ imageObjectUrl, setImageObjectUrl ] = useState( null );
+	const requestId = `media-library-proxied-image-${ siteSlug }${ relativePath }`;
+
+	useEffect( () => {
+		if ( imageObjectUrl === null ) {
+			const cachedImageBlob = getHttpData( requestId )?.data;
+			const url = getUrlFromBlob( cachedImageBlob );
+			setImageObjectUrl( url );
+			debug( 'set image from cache', { url } );
+		}
+
+		requestHttpData( requestId, fetchProxiedMediaFile( siteSlug, relativePath ), {
+			freshness: 30000,
+			fromApi: () => payload => {
+				const imageBlob = payload;
+				const url = getUrlFromBlob( imageBlob );
+				setImageObjectUrl( url );
+				debug( 'got image from API', { payload, url } );
+				return [ [ requestId, payload ] ];
+			},
+		} );
+
+		return () => {
+			if ( ! imageObjectUrl ) {
+				return;
+			}
+			debug( 'Cleared blob from memory on dismount: ' + imageObjectUrl );
+			URL.revokeObjectURL( imageObjectUrl );
+		};
+	}, [ imageObjectUrl, relativePath, requestId, siteSlug ] );
+
+	return <img src={ imageObjectUrl } { ...rest } />;
+};
