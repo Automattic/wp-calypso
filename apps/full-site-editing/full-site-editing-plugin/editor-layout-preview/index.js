586ee82ba8d45b7c66d9dd92b3678804eea500fb
retrofox <rdsuarez@gmail.com>
2020-03-06 13:48:23 -0300

    elp: render a basic component. parse test template

--- apps/full-site-editing/full-site-editing-plugin/editor-layout-preview/index.js
+++ apps/full-site-editing/full-site-editing-plugin/editor-layout-preview/index.js
@@ -5,8 +5,36 @@
 import debugFactory from 'debug';
 /* eslint-enable import/no-extraneous-dependencies */
 
-// Load plugin styles
+/**
+ * WordPress dependencies
+ */
+/* eslint-disable import/no-extraneous-dependencies */
+import domReady from '@wordpress/dom-ready';
+import { render } from '@wordpress/element';
+import { registerCoreBlocks } from '@wordpress/block-library';
+import { parse as parseBlocks } from '@wordpress/blocks';
+/* eslint-enable import/no-extraneous-dependencies */
+
+// Load plugin styles.
 import './style.scss';
+
+// Load testing template.
+import testingTemplate from './util/testing_template';
 
 const debug = debugFactory( 'editor-layout-preview' );
 debug( 'init' );
+
+domReady( function() {
+	debug( 'on ready...' );
+
+	const settings = window.editor_layout_preview || {};
+	debug( 'settings: %o', settings );
+
+	registerCoreBlocks();
+
+	debug( 'testing template: %o', testingTemplate );
+	const blocks = parseBlocks( testingTemplate );
+	debug( 'blocks: %o', blocks );
+
+	render( <h1>Hi!</h1>, document.getElementById( 'editor-large-preview' ) );
+} );
---
+++ apps/full-site-editing/full-site-editing-plugin/editor-layout-preview/util/testing_template.js
@@ -0,0 +1,1 @@
+export default '<!-- wp:heading --><h2>Hello!</h2><!-- /wp:heading -->';
