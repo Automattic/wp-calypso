6c0d6d430d6ea9d902b713f9f173a2eb12c6d0f3
retrofox <rdsuarez@gmail.com>
2020-03-06 13:01:26 -0300

    elp: add super basic client script

---
+++ apps/full-site-editing/full-site-editing-plugin/editor-layout-preview/index.js
@@ -0,0 +1,9 @@
+/**
+ * External dependencies
+ */
+/* eslint-disable import/no-extraneous-dependencies */
+import debugFactory from 'debug';
+/* eslint-enable import/no-extraneous-dependencies */
+
+const debug = debugFactory( 'editor-layout-preview' );
+debug( 'init' );
