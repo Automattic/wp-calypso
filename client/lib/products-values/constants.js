015fb9be4b7108210092d722d5bb640d7cdf854d
Jason Moon <jason.moon@automattic.com>
2020-03-06 17:34:08 -0700

    Jetpack Search: Add scaffolding for plans page

--- client/lib/products-values/constants.js
+++ client/lib/products-values/constants.js
@@ -4,12 +4,20 @@
 import React, { Fragment } from 'react';
 import { translate } from 'i18n-calypso';
 
+/**
+ * Internal dependencies
+ */
+import { isEnabled } from 'config';
+
 // Jetpack products constants
 export const PRODUCT_JETPACK_BACKUP = 'jetpack_backup';
 export const PRODUCT_JETPACK_BACKUP_DAILY = 'jetpack_backup_daily';
 export const PRODUCT_JETPACK_BACKUP_REALTIME = 'jetpack_backup_realtime';
 export const PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY = 'jetpack_backup_daily_monthly';
 export const PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY = 'jetpack_backup_realtime_monthly';
+export const PRODUCT_JETPACK_SEARCH = 'jetpack_search';
+export const PRODUCT_JETPACK_SEARCH_MONTHLY = 'jetpack_search_monthly';
+export const PRODUCT_JETPACK_SEARCH_TRIAL = 'jetpack_search_trial';
 
 export const JETPACK_BACKUP_PRODUCTS_YEARLY = [
 	PRODUCT_JETPACK_BACKUP_DAILY,
@@ -24,7 +32,16 @@ export const JETPACK_BACKUP_PRODUCTS = [
 	...JETPACK_BACKUP_PRODUCTS_MONTHLY,
 ];
 
-export const JETPACK_PRODUCTS_LIST = [ ...JETPACK_BACKUP_PRODUCTS ];
+export const JETPACK_SEARCH_PRODUCTS = [
+	PRODUCT_JETPACK_SEARCH,
+	PRODUCT_JETPACK_SEARCH_MONTHLY,
+	PRODUCT_JETPACK_SEARCH_TRIAL,
+];
+
+export const JETPACK_PRODUCTS_LIST = [
+	...JETPACK_BACKUP_PRODUCTS,
+	...( isEnabled( 'jetpack/search-product' ) ? JETPACK_SEARCH_PRODUCTS : [] ),
+];
 
 export const JETPACK_BACKUP_PRODUCT_LANDING_PAGE_URL = 'https://jetpack.com/upgrade/backup/';
 
@@ -37,6 +54,10 @@ export const JETPACK_PRODUCT_PRICE_MATRIX = {
 		relatedProduct: PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY,
 		ratio: 12,
 	},
+	[ PRODUCT_JETPACK_SEARCH ]: {
+		relatedProduct: PRODUCT_JETPACK_SEARCH_MONTHLY,
+		ratio: 12,
+	},
 };
 
 // Translatable strings
@@ -46,6 +67,9 @@ export const getJetpackProductsShortNames = () => {
 		[ PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY ]: translate( 'Daily Backups' ),
 		[ PRODUCT_JETPACK_BACKUP_REALTIME ]: translate( 'Real-Time Backups' ),
 		[ PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY ]: translate( 'Real-Time Backups' ),
+		[ PRODUCT_JETPACK_SEARCH ]: translate( 'Search' ),
+		[ PRODUCT_JETPACK_SEARCH_MONTHLY ]: translate( 'Search' ),
+		[ PRODUCT_JETPACK_SEARCH_TRIAL ]: translate( 'Search (Trial)' ),
 	};
 };
 
@@ -87,10 +111,14 @@ export const getJetpackProductsDisplayNames = () => {
 				} ) }
 			</Fragment>
 		),
+		[ PRODUCT_JETPACK_SEARCH ]: translate( 'Jetpack Search' ),
+		[ PRODUCT_JETPACK_SEARCH_MONTHLY ]: translate( 'Jetpack Search' ),
+		[ PRODUCT_JETPACK_SEARCH_TRIAL ]: translate( 'Jetpack Search (Trial)' ),
 	};
 };
 
 export const getJetpackProductsTaglines = () => {
+	const searchTagline = translate( 'Search your site.' );
 	return {
 		[ PRODUCT_JETPACK_BACKUP_DAILY ]: translate(
 			'Your data is being securely backed up every day with a 30-day archive.'
@@ -104,10 +132,14 @@ export const getJetpackProductsTaglines = () => {
 		[ PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY ]: translate(
 			'Your data is being securely backed up as you edit.'
 		),
+		[ PRODUCT_JETPACK_SEARCH ]: searchTagline,
+		[ PRODUCT_JETPACK_SEARCH_MONTHLY ]: searchTagline,
+		[ PRODUCT_JETPACK_SEARCH_TRIAL ]: searchTagline,
 	};
 };
 
 export const getJetpackProductsDescriptions = () => {
+	const searchDescription = translate( 'Search your site.' );
 	return {
 		[ PRODUCT_JETPACK_BACKUP_DAILY ]: translate(
 			'Always-on backups ensure you never lose your site. Your changes are saved every day with a 30-day archive.'
@@ -121,23 +153,49 @@ export const getJetpackProductsDescriptions = () => {
 		[ PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY ]: translate(
 			'Always-on backups ensure you never lose your site. Your changes are saved as you edit and you have unlimited backup archives.'
 		),
+		[ PRODUCT_JETPACK_SEARCH ]: searchDescription,
+		[ PRODUCT_JETPACK_SEARCH_MONTHLY ]: searchDescription,
+		[ PRODUCT_JETPACK_SEARCH_TRIAL ]: searchDescription,
 	};
 };
 
-export const getJetpackProducts = () => [
-	{
-		title: translate( 'Jetpack Backup' ),
-		description: translate(
-			'Always-on backups ensure you never lose your site. Choose from real-time or daily backups.'
-		),
-		id: PRODUCT_JETPACK_BACKUP,
-		options: {
-			yearly: JETPACK_BACKUP_PRODUCTS_YEARLY,
-			monthly: JETPACK_BACKUP_PRODUCTS_MONTHLY,
+export const getJetpackProducts = () => {
+	const output = [
+		{
+			title: translate( 'Jetpack Backup' ),
+			description: translate(
+				'Always-on backups ensure you never lose your site. Choose from real-time or daily backups.'
+			),
+			id: PRODUCT_JETPACK_BACKUP,
+			options: {
+				yearly: JETPACK_BACKUP_PRODUCTS_YEARLY,
+				monthly: JETPACK_BACKUP_PRODUCTS_MONTHLY,
+			},
+			optionShortNames: getJetpackProductsShortNames(),
+			optionDisplayNames: getJetpackProductsDisplayNames(),
+			optionDescriptions: getJetpackProductsDescriptions(),
+			optionsLabel: translate( 'Select a backup option:' ),
+			slugs: JETPACK_BACKUP_PRODUCTS,
 		},
-		optionShortNames: getJetpackProductsShortNames(),
-		optionDisplayNames: getJetpackProductsDisplayNames(),
-		optionDescriptions: getJetpackProductsDescriptions(),
-		optionsLabel: translate( 'Select a backup option:' ),
-	},
-];
+	];
+	isEnabled( 'jetpack/search-product' ) &&
+		output.push( {
+			title: translate( 'Jetpack Search' ),
+			// TODO: Add new description copy for Search
+			description: translate(
+				'Always-on backups ensure you never lose your site. Choose from real-time or daily backups.'
+			),
+			id: PRODUCT_JETPACK_SEARCH,
+			options: {
+				yearly: [ PRODUCT_JETPACK_SEARCH ],
+				monthly: [ PRODUCT_JETPACK_SEARCH_MONTHLY ],
+				trial: [ PRODUCT_JETPACK_SEARCH_TRIAL ],
+			},
+			optionShortNames: getJetpackProductsShortNames(),
+			optionDisplayNames: getJetpackProductsDisplayNames(),
+			optionDescriptions: getJetpackProductsDescriptions(),
+			optionsLabel: translate( 'Select a product option:' ),
+			slugs: JETPACK_SEARCH_PRODUCTS,
+		} );
+	return output;
+};
