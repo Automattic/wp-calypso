022eb82241ae72f9dc76e1570c58b71e8143e463
Fabien MILLERAND <fab@millerf.com>
2020-03-09 13:14:56 +0100

    Allow gutenBerg opt-in "button" to be displayed when the user just opted out of the block editor

--- client/state/selectors/is-gutenberg-opt-in-enabled.js
+++ client/state/selectors/is-gutenberg-opt-in-enabled.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { get } from 'lodash';
-
-/**
  * Internal dependencies
  */
 import getSelectedEditor from 'state/selectors/get-selected-editor';
@@ -11,9 +6,7 @@ import isClassicEditorForced from 'state/selectors/is-classic-editor-forced';
 
 export const isGutenbergOptInEnabled = ( state, siteId ) => {
 	return (
-		get( state, [ 'gutenbergOptInOut', siteId, 'optIn' ], false ) &&
-		getSelectedEditor( state, siteId ) === 'classic' &&
-		! isClassicEditorForced( state, siteId )
+		getSelectedEditor( state, siteId ) === 'classic' && ! isClassicEditorForced( state, siteId )
 	);
 };
 
