01230dcf855b6d6ba12b927aaa047fd70305d6b9
Rich Collier <rcollier@iris.local>
2020-03-10 11:45:50 -0400

    Improve backup delta component for realtime backups

--- client/landing/jetpack-cloud/components/backup-delta/index.jsx
+++ client/landing/jetpack-cloud/components/backup-delta/index.jsx
@@ -1,13 +1,16 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 /**
  * Internal dependencies
  */
 import Gridicon from 'components/gridicon';
 import Button from 'components/forms/form-button';
+import { Card } from '@automattic/components';
+import ActivityActor from 'my-sites/activity/activity-log-item/activity-actor';
+import ActivityDescription from 'my-sites/activity/activity-log-item/activity-description';
 
 /**
  * Style dependencies
@@ -15,7 +18,49 @@ import Button from 'components/forms/form-button';
 import './style.scss';
 
 class BackupDelta extends Component {
-	render() {
+	renderRealtime() {
+		const { allowRestore } = this.props;
+
+		const realtimeEvents = this.props.realtimeEvents.filter( event => event.activityIsRewindable );
+
+		const events = realtimeEvents.map( event => (
+			<Fragment key={ event.activityId }>
+				<div className="backup-delta__time">
+					<Gridicon icon="cloud-upload" />
+					{ this.props.moment( event.activityDate ).format( 'LT' ) }
+				</div>
+				<Card>
+					<ActivityActor
+						{ ...{
+							actorAvatarUrl: event.actorAvatarUrl,
+							actorName: event.actorName,
+							actorRole: event.actorRole,
+							actorType: event.actorType,
+						} }
+					/>
+					<ActivityDescription activity={ event } rewindIsActive={ allowRestore } />
+					<div>{ event.activityTitle }</div>
+					<div>
+						<Button compact borderless>
+							See content <Gridicon icon="chevron-down" />
+						</Button>
+						<Button compact borderless>
+							Actions <Gridicon icon="add" />
+						</Button>
+					</div>
+				</Card>
+			</Fragment>
+		) );
+
+		return (
+			<div className="backup-delta__realtime">
+				<div>More backups from today</div>
+				{ events.length ? events : <div>you have no more backups for this day</div> }
+			</div>
+		);
+	}
+
+	renderDaily() {
 		const { backupAttempts, deltas } = this.props;
 		const mainBackup = backupAttempts.complete && backupAttempts.complete[ 0 ];
 		const meta = mainBackup && mainBackup.activityDescription[ 2 ].children[ 0 ];
@@ -47,14 +92,30 @@ class BackupDelta extends Component {
 		} );
 
 		return (
-			<div>
+			<div className="backup-delta__daily">
 				<div>Backup details</div>
-				<div>Media</div>
-				<div>{ deltas.mediaCreated && media }</div>
-				<div>Posts</div>
-				<div>{ deltas.posts && posts }</div>
+				{ !! deltas.mediaCreated.length && (
+					<Fragment>
+						<div>Media</div>
+						<div>{ media }</div>
+					</Fragment>
+				) }
+				{ !! deltas.posts.length && (
+					<Fragment>
+						<div>Posts</div>
+						<div>{ posts }</div>
+					</Fragment>
+				) }
 				<div>{ meta }</div>
 				<Button className="backup-delta__view-all-button">View all backup details</Button>
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<div className="backup-delta">
+				{ this.props.hasRealtimeBackups ? this.renderRealtime() : this.renderDaily() }
 			</div>
 		);
 	}
