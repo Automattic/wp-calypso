c2ff1add98cbd517136d06929cd7dbb1e382ff93
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-03-11 20:28:34 +0530

    Add comment

--- client/my-sites/checkout/checkout/test/composite-checkout-thank-you.js
+++ client/my-sites/checkout/checkout/test/composite-checkout-thank-you.js
@@ -396,6 +396,7 @@ describe( 'getThankYouPageUrl', () => {
 		expect( url ).toBe( '/checkout/thank-you/foo.bar/1234abcd' );
 	} );
 
+	// We are temporarily disabling the premium bump offer, so skipping this test for now. Check pNEWy-cIg-p2.
 	it.skip( 'redirects to premium upgrade nudge if concierge and jetpack are not in the cart, personal is in the cart, and the previous route is not the nudge', () => {
 		isEnabled.mockImplementation( flag => flag === 'upsell/concierge-session' );
 		const cart = {
