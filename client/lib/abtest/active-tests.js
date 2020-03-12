1371355a27ebba7c3cf3c5b5ba4e122af1b7ca74
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-03-12 12:55:08 +0530

    Update timestamp

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -116,7 +116,7 @@ export default {
 		allowExistingUsers: true,
 	},
 	planStepCopyUpdates: {
-		datestamp: '20200227',
+		datestamp: '20200312',
 		variations: {
 			variantCopyUpdates: 50,
 			control: 50,
