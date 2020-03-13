3ee0477cffc7dc656b130fbe52d06ebafb0a18a1
Jon Surrell <jon.surrell@automattic.com>
2020-03-13 17:37:01 +0100

    Do a bunch of things 🚀
    Mostly get that style preview ready for incremental progress

--- client/landing/gutenboarding/onboarding-block/style-preview/index.tsx
+++ client/landing/gutenboarding/onboarding-block/style-preview/index.tsx
@@ -3,6 +3,8 @@
  */
 import * as React from 'react';
 import { Button } from '@wordpress/components';
+import { useI18n } from '@automattic/react-i18n';
+import { ValuesType } from 'utility-types';
 
 /**
  * Internal dependencies
@@ -10,56 +12,47 @@ import { Button } from '@wordpress/components';
 import Preview from './preview';
 import Link from '../../components/link';
 import { usePath, Step } from '../../path';
-import { useI18n } from '@automattic/react-i18n';
-import ViewportSelect, { Viewport } from './viewport-select';
+import ViewportSelect from './viewport-select';
+import FontSelect, { fontPairings } from './font-select';
+import { Title, SubTitle } from '../../components/titles';
+import * as T from './types';
 
 import './style.scss';
-
-const fontPairings = [
-	[ 'Cabin', 'Raleway' ],
-	[ 'Chivo', 'OpenSans' ],
-	[ 'Playfair', 'FiraSans' ],
-	[ 'Arvo', 'Montserrat' ],
-	[ 'SpaceMono', 'Roboto' ],
-];
 
 const StylePreview: React.FunctionComponent = () => {
 	const { __: NO__ } = useI18n();
 	const makePath = usePath();
-	const [ selectedViewport, setSelectedViewport ] = React.useState< Viewport >( 'desktop' );
+	const [ selectedViewport, setSelectedViewport ] = React.useState< T.Viewport >( 'desktop' );
+	const [ selectedFonts, setSelectedFonts ] = React.useState< ValuesType< typeof fontPairings > >(
+		fontPairings[ 0 ]
+	);
 	return (
-		<>
-			<div className="gutenboarding-title">{ NO__( 'Select your fonts' ) }</div>
-			<div className="gutenboarding-subtitle">
-				{ NO__( 'Add some personality to your design.' ) }
+		<div className="style-preview">
+			<div className="style-preview__header">
+				<div className="style-preview__titles">
+					<Title>{ NO__( 'Select your fonts' ) }</Title>
+					<SubTitle>{ NO__( 'Add some personality to your design.' ) }</SubTitle>
+				</div>
+				<ViewportSelect selected={ selectedViewport } onSelect={ setSelectedViewport } />
+				<div className="style-preview__actions">
+					<Link isLink to={ makePath( Step.DesignSelection ) }>
+						{ NO__( 'Choose another design' ) }
+					</Link>
+					<Button
+						isPrimary
+						onClick={ () => {
+							window.alert( 'Not implemented!' );
+						} }
+					>
+						{ NO__( 'Continue' ) }
+					</Button>
+				</div>
 			</div>
-			<ViewportSelect selected={ selectedViewport } onSelect={ setSelectedViewport } />
-			<div className="gutenboarding-actions">
-				<Link isLink to={ makePath( Step.DesignSelection ) }>
-					{ NO__( 'Choose another design' ) }
-				</Link>
-				<Button
-					isPrimary
-					onClick={ () => {
-						window.alert( 'Not implemented!' );
-					} }
-				>
-					{ NO__( 'Continue' ) }
-				</Button>
+			<div className="style-preview__content">
+				<FontSelect selected={ selectedFonts } onSelect={ setSelectedFonts } />
+				<Preview fonts={ selectedFonts } viewport={ selectedViewport } />
 			</div>
-			<div className="style-preview__font-options">
-				<ul>
-					{ fontPairings.map( ( [ a, b ] ) => (
-						<li key={ a + b }>
-							<Button>
-								{ a } / { b }
-							</Button>
-						</li>
-					) ) }
-				</ul>
-			</div>
-			<Preview />
-		</>
+		</div>
 	);
 };
 
--- client/landing/gutenboarding/onboarding-block/style-preview/preview.tsx
+++ client/landing/gutenboarding/onboarding-block/style-preview/preview.tsx
@@ -8,10 +8,21 @@ import { useSelect } from '@wordpress/data';
  * Internal dependencies
  */
 import { STORE_KEY } from '../../stores/onboard';
+import * as T from './types';
+import { ValuesType } from 'utility-types';
 
-const Preview: React.FunctionComponent = () => {
+interface Props {
+	viewport: T.Viewport;
+	fonts: ValuesType< typeof import('./font-select').fontPairings >;
+}
+const Preview: React.FunctionComponent< Props > = ( { viewport } ) => {
 	const { selectedDesign } = useSelect( select => select( STORE_KEY ).getState() );
-	return <div>Preview to be implemented. You picked { selectedDesign ?? 'unknown' }.</div>;
+	return (
+		<div className="style-preview__preview">
+			Preview to be implemented. You picked { selectedDesign?.title ?? 'unknown' } design. Showing{ ' ' }
+			{ viewport } display.
+		</div>
+	);
 };
 
 export default Preview;
