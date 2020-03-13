4ee3cbe794d2d46541cb6e6f8ce200f722b75df3
Kerry Liu <kerryliu@gmail.com>
2020-03-13 16:18:49 -0700

    Home: set to autoplay

--- client/my-sites/customer-home/free-photo-library-card/index.jsx
+++ client/my-sites/customer-home/free-photo-library-card/index.jsx
@@ -36,7 +36,7 @@ const FreePhotoLibraryCard = () => {
 						title={ translate( 'Free Photo Library demonstration' ) }
 						width="560"
 						height="315"
-						src="https://www.youtube.com/embed/RHG_yfd1SVw?rel=0"
+						src="https://www.youtube.com/embed/RHG_yfd1SVw?rel=0&autoplay=1"
 						frameBorder="0"
 						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
 						allowFullScreen={ true }
