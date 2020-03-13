0d3edc06a409a4a05f7c3576cc8e6edcc8f98728
Jon Surrell <jon.surrell@automattic.com>
2020-03-13 19:56:44 +0100

    Fetch and use vertical template

--- client/landing/gutenboarding/onboarding-block/style-preview/preview.tsx
+++ client/landing/gutenboarding/onboarding-block/style-preview/preview.tsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import * as React from 'react';
+import { VerticalsTemplates } from '@automattic/data-stores';
 import { useSelect } from '@wordpress/data';
 
 /**
@@ -10,19 +11,31 @@ import { useSelect } from '@wordpress/data';
 import { STORE_KEY } from '../../stores/onboard';
 import * as T from './types';
 import { ValuesType } from 'utility-types';
+import { Design, SiteVertical } from '../../stores/onboard/types';
+
+const VT_STORE = VerticalsTemplates.register();
 
 interface Props {
 	viewport: T.Viewport;
 	fonts: ValuesType< typeof import('./font-select').fontPairings >;
 }
 const Preview: React.FunctionComponent< Props > = ( { fonts, viewport } ) => {
-	const { selectedDesign } = useSelect( select => select( STORE_KEY ).getState() );
+	const { selectedDesign, siteVertical } = useSelect( select =>
+		select( STORE_KEY ).getState()
+	) as { selectedDesign: Design; siteVertical: SiteVertical };
+	const template = useSelect( select =>
+		select( VT_STORE ).getVerticalTemplate( siteVertical.id ?? '', selectedDesign.slug )
+	);
 	return (
 		<div className="style-preview__preview">
 			<p>Preview to be implemented.</p>
 			<p>You picked { selectedDesign?.title ?? 'unknown' } design.</p>
 			<p>Showing { viewport } display.</p>
 			<p>With { fonts.join( ' / ' ) } display.</p>
+
+			<pre>
+				<code>{ template?.content }</code>
+			</pre>
 		</div>
 	);
 };
