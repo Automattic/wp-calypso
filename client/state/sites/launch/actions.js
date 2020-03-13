69d1d8a2dcf2f2c8c361181d233f9aa04d35f471
Kerry Liu <kerryliu@gmail.com>
2020-03-13 11:29:09 -0700

    Site Launch: skip upsell until regression is found

--- client/state/sites/launch/actions.js
+++ client/state/sites/launch/actions.js
@@ -4,8 +4,6 @@
 import { SITE_LAUNCH } from 'state/action-types';
 import 'state/data-layer/wpcom/sites/launch';
 import isUnlaunchedSite from 'state/selectors/is-unlaunched-site';
-import { getSiteSlug, isCurrentPlanPaid } from 'state/sites/selectors';
-import { getDomainsBySiteId } from 'state/sites/domains/selectors';
 
 export const launchSite = siteId => ( {
 	type: SITE_LAUNCH,
@@ -22,17 +20,5 @@ export const launchSiteOrRedirectToLaunchSignupFlow = siteId => ( dispatch, getS
 	if ( ! isUnlaunchedSite( getState(), siteId ) ) {
 		return;
 	}
-
-	if (
-		isCurrentPlanPaid( getState(), siteId ) &&
-		getDomainsBySiteId( getState(), siteId ).length > 1
-	) {
-		dispatch( launchSite( siteId ) );
-		return;
-	}
-
-	const siteSlug = getSiteSlug( getState(), siteId );
-
-	// TODO: consider using the `page` library instead of calling using `location.href` here
-	window.location.href = `/start/launch-site?siteSlug=${ siteSlug }`;
+	dispatch( launchSite( siteId ) );
 };
