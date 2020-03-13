1569b587d20f08c7dcc90d1c86e2c787c81ac643
Bernie Reiter <ockham@raz.or.at>
2020-03-13 14:55:27 +0100

    Docs: Move CONTRIBUTING.md to docs/ (#36167)

--- bin/pre-push-hook.js
+++ bin/pre-push-hook.js
@@ -6,7 +6,7 @@ const readline = require( 'readline-sync' );
 console.log(
 	'\nBy contributing to this project, you license the materials you contribute ' +
 		'under the GNU General Public License v2 (or later). All materials must have ' +
-		'GPLv2 compatible licenses — see .github/CONTRIBUTING.md for details.\n\n'
+		'GPLv2 compatible licenses — see docs/CONTRIBUTING.md for details.\n\n'
 );
 
 const currentBranch = execSync( 'git rev-parse --abbrev-ref HEAD' )
