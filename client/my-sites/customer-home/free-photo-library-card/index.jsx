a3b21ce4b87304c60602c28f39edd03791a742e4
Aurorum <torres126@clubpenguinmountains.com>
2020-03-15 18:48:54 +0000

    Free Photo Library Card: Display Support Article Dialog

--- client/my-sites/customer-home/free-photo-library-card/index.jsx
+++ client/my-sites/customer-home/free-photo-library-card/index.jsx
@@ -1,8 +1,9 @@
 /**
  * External dependencies
  */
-import React, { Fragment, useState } from 'react';
-import { useTranslate } from 'i18n-calypso';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
 import { Button, Card, Dialog } from '@automattic/components';
 import { isDesktop } from '@automattic/viewport';
 
@@ -10,7 +11,7 @@ import { isDesktop } from '@automattic/viewport';
  * Internal dependencies
  */
 import CardHeading from 'components/card-heading';
-import { localizeUrl } from 'lib/i18n-utils';
+import { openSupportArticleDialog } from 'state/inline-support-article/actions';
 
 /**
  * Style dependencies
@@ -23,55 +24,73 @@ import './style.scss';
 import freePhotoLibraryThumbnail from 'assets/images/customer-home/free-photo-library-thumbnail.png';
 import freePhotoLibraryVideoPrompt from 'assets/images/customer-home/free-photo-library-video-prompt.png';
 
-const FreePhotoLibraryCard = () => {
-	const [ showDialog, setShowDialog ] = useState( false );
-	const translate = useTranslate();
+class FreePhotoLibraryCard extends Component {
+	state = {
+		showDialog: false,
+	};
 
-	const toggleDialog = () => setShowDialog( ! showDialog );
+	toggleDialog = () => {
+		this.setState( state => ( {
+			showDialog: ! state.showDialog,
+		} ) );
+	};
 
-	return (
-		<Fragment>
-			<Dialog additionalClassNames="free-photo-library-card__dialog" isVisible={ showDialog }>
-				<video
-					className="free-photo-library-card__demonstration-video"
-					controls
-					muted
-					autoPlay
-					poster={ freePhotoLibraryThumbnail }
+	render() {
+		const { translate } = this.props;
+		return (
+			<Fragment>
+				<Dialog
+					additionalClassNames="free-photo-library-card__dialog"
+					isVisible={ this.state.showDialog }
 				>
-					<source
-						src="https://wpcom.files.wordpress.com/2020/02/free-photo-library-demonstration.mp4"
-						type="video/mp4"
-					/>
-				</video>
-				<div className="free-photo-library-card__dialog-close">
-					<Button onClick={ toggleDialog }>{ translate( 'Close' ) }</Button>
-				</div>
-			</Dialog>
-			<Card className="free-photo-library-card">
-				{ isDesktop() && (
-					<button onClick={ toggleDialog }>
-						<img
-							className="free-photo-library-card__demonstration-image"
-							src={ freePhotoLibraryVideoPrompt }
-							alt={ translate( 'Free Photo Library demonstration' ) }
+					<video
+						className="free-photo-library-card__demonstration-video"
+						controls
+						muted
+						autoPlay
+						poster={ freePhotoLibraryThumbnail }
+					>
+						<source
+							src="https://wpcom.files.wordpress.com/2020/02/free-photo-library-demonstration.mp4"
+							type="video/mp4"
 						/>
-					</button>
-				) }
-				<CardHeading>{ translate( 'Over 40,000 Free Photos' ) }</CardHeading>
-				<p className="free-photo-library-card__text customer-home__card-subheader">
-					{ translate(
-						'The WordPress.com Free Photo Library integrates ' +
-							'your site with beautiful copyright-free photos to ' +
-							'create stunning designs.'
+					</video>
+					<div className="free-photo-library-card__dialog-close">
+						<Button onClick={ this.toggleDialog }>{ translate( 'Close' ) }</Button>
+					</div>
+				</Dialog>
+				<Card className="free-photo-library-card">
+					{ isDesktop() && (
+						<button onClick={ this.toggleDialog }>
+							<img
+								className="free-photo-library-card__demonstration-image"
+								src={ freePhotoLibraryVideoPrompt }
+								alt={ translate( 'Free Photo Library demonstration' ) }
+							/>
+						</button>
 					) }
-				</p>
-				<Button href={ localizeUrl( 'https://support.wordpress.com/free-photo-library/' ) }>
-					{ translate( 'Learn more' ) }
-				</Button>
-			</Card>
-		</Fragment>
-	);
-};
+					<CardHeading>{ translate( 'Over 40,000 Free Photos' ) }</CardHeading>
+					<p className="free-photo-library-card__text customer-home__card-subheader">
+						{ translate(
+							'The WordPress.com Free Photo Library integrates ' +
+								'your site with beautiful copyright-free photos to ' +
+								'create stunning designs.'
+						) }
+					</p>
+					<Button
+						onClick={ () => {
+							this.props.openSupportArticleDialog( {
+								postId: 145498,
+								postUrl: 'https://support.wordpress.com/free-photo-library/',
+							} );
+						} }
+					>
+						{ translate( 'Learn more' ) }
+					</Button>
+				</Card>
+			</Fragment>
+		);
+	}
+}
 
-export default FreePhotoLibraryCard;
+export default connect( null, { openSupportArticleDialog } )( localize( FreePhotoLibraryCard ) );
