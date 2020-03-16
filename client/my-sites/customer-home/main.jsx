e7006d970181cbc9cd5a187e1f4c0ac3734ba674
And Finally <andfinally@users.noreply.github.com>
2020-03-16 17:34:09 +0000

    Don't show checklist complete banner if we're showing the recently migrated site banner.

--- client/my-sites/customer-home/main.jsx
+++ client/my-sites/customer-home/main.jsx
@@ -316,6 +316,7 @@ class Home extends Component {
 			isChecklistComplete,
 			siteIsUnlaunched,
 			isEstablishedSite,
+			isRecentlyMigratedSite,
 			displayWelcomeBanner,
 		} = this.props;
 
@@ -328,7 +329,9 @@ class Home extends Component {
 				/>
 			);
 		}
-		const renderChecklistCompleteBanner = 'render' === this.state.renderChecklistCompleteBanner;
+
+		const renderChecklistCompleteBanner =
+			'render' === this.state.renderChecklistCompleteBanner && ! isRecentlyMigratedSite;
 
 		return (
 			<Main className="customer-home__main is-wide-layout">
