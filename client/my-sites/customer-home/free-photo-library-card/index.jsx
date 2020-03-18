09b08b7f8a445a98bead22a4f30ec8b19242a767
Kirk Wight <kwight@kwight.ca>
2020-03-18 11:36:31 -0700

    My Home: Combine photo library buttons to one support dialog.

--- client/my-sites/customer-home/free-photo-library-card/index.jsx
+++ client/my-sites/customer-home/free-photo-library-card/index.jsx
@@ -1,9 +1,9 @@
 /**
  * External dependencies
  */
-import React, { Fragment, useState } from 'react';
+import React from 'react';
 import { useTranslate } from 'i18n-calypso';
-import { Button, Card, Dialog } from '@automattic/components';
+import { Button, Card } from '@automattic/components';
 import { isDesktop } from '@automattic/viewport';
 import { connect } from 'react-redux';
 
@@ -29,55 +29,37 @@ const FreePhotoLibraryCard = ( {
 	recordTracksEvent: tracks,
 	openSupportArticleDialogAndTrack,
 } ) => {
-	const [ showDialog, setShowDialog ] = useState( false );
 	const translate = useTranslate();
 
-	const toggleDialog = () => {
-		if ( ! showDialog ) {
-			tracks( 'calypso_customer_home_free_photo_library_video_dialog_view' );
-		}
-		return setShowDialog( ! showDialog );
+	const displayDialog = () => {
+		tracks( 'calypso_customer_home_free_photo_library_video_support_page_view' );
+		supportArticleDialog( {
+			postId: 145498,
+			postUrl: localizeUrl( 'https://support.wordpress.com/free-photo-library/' ),
+		} );
 	};
 
 	return (
-		<Fragment>
-			<Dialog additionalClassNames="free-photo-library-card__dialog" isVisible={ showDialog }>
-				<div className="free-photo-library-card__demonstration-video">
-					<iframe
-						title={ translate( 'Free Photo Library demonstration' ) }
-						width="560"
-						height="315"
-						src="https://www.youtube.com/embed/RHG_yfd1SVw?rel=0&autoplay=1"
-						frameBorder="0"
-						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-						allowFullScreen={ true }
+		<Card className="free-photo-library-card">
+			{ isDesktop() && (
+				<button onClick={ displayDialog }>
+					<img
+						className="free-photo-library-card__demonstration-image"
+						src={ freePhotoLibraryVideoPrompt }
+						alt={ translate( 'Free Photo Library demonstration' ) }
 					/>
-				</div>
-				<div className="free-photo-library-card__dialog-close">
-					<Button onClick={ toggleDialog }>{ translate( 'Close' ) }</Button>
-				</div>
-			</Dialog>
-			<Card className="free-photo-library-card">
-				{ isDesktop() && (
-					<button onClick={ toggleDialog }>
-						<img
-							className="free-photo-library-card__demonstration-image"
-							src={ freePhotoLibraryVideoPrompt }
-							alt={ translate( 'Free Photo Library demonstration' ) }
-						/>
-					</button>
+				</button>
+			) }
+			<CardHeading>{ translate( 'Over 40,000 Free Photos' ) }</CardHeading>
+			<p className="free-photo-library-card__text customer-home__card-subheader">
+				{ translate(
+					'The WordPress.com Free Photo Library integrates ' +
+						'your site with beautiful copyright-free photos to ' +
+						'create stunning designs.'
 				) }
-				<CardHeading>{ translate( 'Over 40,000 Free Photos' ) }</CardHeading>
-				<p className="free-photo-library-card__text customer-home__card-subheader">
-					{ translate(
-						'The WordPress.com Free Photo Library integrates ' +
-							'your site with beautiful copyright-free photos to ' +
-							'create stunning designs.'
-					) }
-				</p>
-				<Button onClick={ openSupportArticleDialogAndTrack }>{ translate( 'Learn more' ) }</Button>
-			</Card>
-		</Fragment>
+			</p>
+			<Button onClick={ openSupportArticleDialogAndTrack }>{ translate( 'Learn more' ) }</Button>
+		</Card>
 	);
 };
 
