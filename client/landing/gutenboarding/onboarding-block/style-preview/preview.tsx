4c776e26158d58bcc737005c708dbc15a5cbf98b
Jon Surrell <jon.surrell@automattic.com>
2020-03-19 12:33:42 +0100

    Rerender iframe contents

--- client/landing/gutenboarding/onboarding-block/style-preview/preview.tsx
+++ client/landing/gutenboarding/onboarding-block/style-preview/preview.tsx
@@ -20,34 +20,22 @@ interface Props {
 }
 const Preview: React.FunctionComponent< Props > = ( { fonts, viewport } ) => {
 	const [ previewHtml, setPreviewHtml ] = React.useState< string >();
-	const [ requestedFonts, setRequestedFonts ] = React.useState< Set< string > >( new Set() );
-
-	// Disabled while previews are implemented
-	// Cast: These are required to be on this step.
-	// const { selectedDesign, siteVertical } = useSelect( select =>
-	// 	select( STORE_KEY ).getState()
-	// ) as { selectedDesign: Design; siteVertical: SiteVertical };
-
 	const iframe = React.useRef< HTMLIFrameElement >( null );
 
-	React.useEffect(
-		() => {
-			const eff = async () => {
-				const [ { fontFamily: headings }, { fontFamily: base } ] = fonts;
-				const url = `https://public-api.wordpress.com/rest/v1/template-demo/${ encodeURIComponent(
-					'rockfield'
-				) }/${ encodeURIComponent( 'rockfield' ) }/en/${ encodeURIComponent(
-					'gym'
-				) }?font_headings=${ encodeURI( headings ) }&font_base=${ encodeURI( base ) }`;
-				const resp = await window.fetch( url );
-				const html = await resp.text();
-				setPreviewHtml( html );
-			};
-			eff();
-		},
-		// Disable reason: We'll handle font change elsewhere.
-		[] // eslint-disable-line react-hooks/exhaustive-deps
-	);
+	React.useEffect( () => {
+		const eff = async () => {
+			const [ { fontFamily: headings }, { fontFamily: base } ] = fonts;
+			const url = `https://public-api.wordpress.com/rest/v1/template-demo/${ encodeURIComponent(
+				'rockfield'
+			) }/${ encodeURIComponent( 'rockfield' ) }/en/${ encodeURIComponent(
+				'gym'
+			) }?font_headings=${ encodeURI( headings ) }&font_base=${ encodeURI( base ) }`;
+			const resp = await window.fetch( url );
+			const html = await resp.text();
+			setPreviewHtml( html );
+		};
+		eff();
+	}, [ fonts ] );
 
 	React.useEffect( () => {
 		if ( previewHtml ) {
@@ -59,45 +47,6 @@ const Preview: React.FunctionComponent< Props > = ( { fonts, viewport } ) => {
 			}
 		}
 	}, [ previewHtml ] );
-
-	React.useEffect( () => {
-		const iframeWindow = iframe.current?.contentWindow;
-		if ( iframeWindow?.document.body ) {
-			const iframeDocument = iframeWindow.document;
-			const [ { fontFamily: headings }, { fontFamily: base } ] = fonts;
-
-			const baseURL = 'https://fonts.googleapis.com/css2';
-
-			// matrix: regular,bold * regular,italic
-			const axis = 'ital,wght@0,400;0,700;1,400;1,700';
-			const query = [];
-
-			// To replace state
-			const nextFonts = new Set( requestedFonts );
-
-			if ( ! requestedFonts.has( headings ) ) {
-				query.push( `family=${ encodeURI( headings ) }:${ axis }` );
-				nextFonts.add( headings );
-			}
-			if ( ! requestedFonts.has( base ) ) {
-				query.push( `family=${ encodeURI( base ) }:${ axis }` );
-				nextFonts.add( base );
-			}
-
-			console.log( 'requested %o', requestedFonts );
-			if ( query.length ) {
-				console.log( 'adding %o', query );
-				const l = iframeDocument.createElement( 'link' );
-				l.rel = 'stylesheet';
-				l.type = 'text/css';
-				l.href = `${ baseURL }?${ query.join( '&' ) }&display=swap`;
-				iframeDocument.head.appendChild( l );
-				setRequestedFonts( nextFonts );
-			}
-			iframeDocument.body.style.setProperty( '--font-headings', `${ headings }` );
-			iframeDocument.body.style.setProperty( '--font-base', `${ headings }` );
-		}
-	}, [ fonts, previewHtml, requestedFonts ] );
 
 	return (
 		<div className={ `style-preview__preview is-viewport-${ viewport }` }>
