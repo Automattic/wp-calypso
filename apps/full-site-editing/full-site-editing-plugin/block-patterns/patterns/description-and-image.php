1e444ca4112e5a174cb914cdab31eacdad043175
Konstantin Obenland <obenland@gmx.de>
2020-03-19 10:23:43 -0700

    Add three images side by side pattern
    (And fix the others)

--- apps/full-site-editing/full-site-editing-plugin/block-patterns/patterns/description-and-image.php
+++ apps/full-site-editing/full-site-editing-plugin/block-patterns/patterns/description-and-image.php
@@ -7,7 +7,8 @@
 
 $markup = '
 <!-- wp:jetpack/layout-grid {"column1DesktopSpan":3,"column1TabletSpan":3,"column1MobileSpan":4,"column2DesktopSpan":9,"column2TabletSpan":5,"column2MobileSpan":4,"column3DesktopOffset":15,"column3TabletOffset":3,"column3MobileOffset":2,"className":"column1-desktop-grid__span-3 column1-desktop-grid__row-1 column2-desktop-grid__span-9 column2-desktop-grid__start-4 column2-desktop-grid__row-1 column1-tablet-grid__span-3 column1-tablet-grid__row-1 column2-tablet-grid__span-5 column2-tablet-grid__start-4 column2-tablet-grid__row-1 column1-mobile-grid__span-4 column1-mobile-grid__row-1 column2-mobile-grid__span-4 column2-mobile-grid__row-2"} -->
-<div class="wp-block-jetpack-layout-grid alignfull column1-desktop-grid__span-3 column1-desktop-grid__row-1 column2-desktop-grid__span-9 column2-desktop-grid__start-4 column2-desktop-grid__row-1 column1-tablet-grid__span-3 column1-tablet-grid__row-1 column2-tablet-grid__span-5 column2-tablet-grid__start-4 column2-tablet-grid__row-1 column1-mobile-grid__span-4 column1-mobile-grid__row-1 column2-mobile-grid__span-4 column2-mobile-grid__row-2"><!-- wp:jetpack/layout-grid-column -->
+<div class="wp-block-jetpack-layout-grid alignfull column1-desktop-grid__span-3 column1-desktop-grid__row-1 column2-desktop-grid__span-9 column2-desktop-grid__start-4 column2-desktop-grid__row-1 column1-tablet-grid__span-3 column1-tablet-grid__row-1 column2-tablet-grid__span-5 column2-tablet-grid__start-4 column2-tablet-grid__row-1 column1-mobile-grid__span-4 column1-mobile-grid__row-1 column2-mobile-grid__span-4 column2-mobile-grid__row-2">
+	<!-- wp:jetpack/layout-grid-column -->
 	<div class="wp-block-jetpack-layout-grid-column wp-block-jetpack-layout-grid__padding-none">
 		<!-- wp:paragraph {"fontSize":"small"} -->
 		<p class="has-small-font-size">%1$s</p>
--- apps/full-site-editing/full-site-editing-plugin/block-patterns/patterns/image-and-description.php
+++ apps/full-site-editing/full-site-editing-plugin/block-patterns/patterns/image-and-description.php
@@ -7,7 +7,8 @@
 
 $markup = '
 <!-- wp:jetpack/layout-grid {"column1DesktopSpan":9,"column1TabletSpan":5,"column1MobileSpan":4,"column2DesktopSpan":3,"column2TabletSpan":3,"column2MobileSpan":4,"column3DesktopOffset":21,"column3TabletOffset":6,"column3MobileOffset":2,"className":"column1-desktop-grid__span-9 column1-desktop-grid__row-1 column2-desktop-grid__span-3 column2-desktop-grid__start-10 column2-desktop-grid__row-1 column1-tablet-grid__span-5 column1-tablet-grid__row-1 column2-tablet-grid__span-3 column2-tablet-grid__start-6 column2-tablet-grid__row-1 column1-mobile-grid__span-4 column1-mobile-grid__row-1 column2-mobile-grid__span-4 column2-mobile-grid__row-2"} -->
-<div class="wp-block-jetpack-layout-grid alignfull column1-desktop-grid__span-9 column1-desktop-grid__row-1 column2-desktop-grid__span-3 column2-desktop-grid__start-10 column2-desktop-grid__row-1 column1-tablet-grid__span-5 column1-tablet-grid__row-1 column2-tablet-grid__span-3 column2-tablet-grid__start-6 column2-tablet-grid__row-1 column1-mobile-grid__span-4 column1-mobile-grid__row-1 column2-mobile-grid__span-4 column2-mobile-grid__row-2"><!-- wp:jetpack/layout-grid-column -->
+<div class="wp-block-jetpack-layout-grid alignfull column1-desktop-grid__span-9 column1-desktop-grid__row-1 column2-desktop-grid__span-3 column2-desktop-grid__start-10 column2-desktop-grid__row-1 column1-tablet-grid__span-5 column1-tablet-grid__row-1 column2-tablet-grid__span-3 column2-tablet-grid__start-6 column2-tablet-grid__row-1 column1-mobile-grid__span-4 column1-mobile-grid__row-1 column2-mobile-grid__span-4 column2-mobile-grid__row-2">
+	<!-- wp:jetpack/layout-grid-column -->
 	<div class="wp-block-jetpack-layout-grid-column wp-block-jetpack-layout-grid__padding-none">
 		<!-- wp:image {"sizeSlug":"full"} -->
 		<figure class="wp-block-image size-full">
