d7972bd1b42627af7efe58984c46c03a48cafc65
Mikael Korpela <mikael@ihminen.org>
2020-03-24 13:06:33 +0200

    Import new file via editor.scss

--- apps/wpcom-block-editor/src/wpcom/editor.js
+++ apps/wpcom-block-editor/src/wpcom/editor.js
@@ -5,8 +5,3 @@ import './features/deprecate-coblocks-buttons';
 import './features/fix-block-invalidation-errors';
 import './features/reorder-block-categories';
 import './features/tracking';
-
-/**
- * Style dependencies
- */
-import './features/legacy-preview-button.scss';
