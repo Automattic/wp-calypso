684123931a1fd77ae231ddd36daa945f14b2f6d2
Caleb Bauermeister <cbauerman@gmail.com>
2020-03-24 16:35:44 -0700

    add readme

--- client/landing/jetpack-cloud/sections/backups/main.jsx
+++ client/landing/jetpack-cloud/sections/backups/main.jsx
@@ -30,6 +30,7 @@ import ActivityCard from '../../components/activity-card';
 import siteSupportsRealtimeBackup from 'state/selectors/site-supports-realtime-backup';
 import Pagination from 'components/pagination';
 import MissingCredentialsWarning from '../../components/missing-credentials';
+import getDoesRewindNeedCredentials from 'state/selectors/get-does-rewind-need-credentials.js';
 
 /**
  * Style dependencies
@@ -73,10 +74,10 @@ class BackupsPage extends Component {
 	renderMain() {
 		const {
 			allowRestore,
+			doesRewindNeedCredentials,
 			hasRealtimeBackups,
 			logs,
 			moment,
-			rewindState,
 			siteId,
 			siteSlug,
 		} = this.props;
@@ -105,7 +106,7 @@ class BackupsPage extends Component {
 					backupAttempts={ backupAttempts }
 					siteSlug={ siteSlug }
 				/>
-				{ 'awaiting_credentials' === rewindState && (
+				{ doesRewindNeedCredentials && (
 					<MissingCredentialsWarning settingsLink={ `/settings/${ siteSlug }` } />
 				) }
 				<BackupDelta
@@ -214,14 +215,15 @@ const mapStateToProps = state => {
 	const logs = siteId && requestActivityLogs( siteId, filter );
 	const rewind = getRewindState( state, siteId );
 	const restoreStatus = rewind.rewind && rewind.rewind.status;
+	const doesRewindNeedCredentials = getDoesRewindNeedCredentials( state, siteId );
 	const allowRestore =
 		'active' === rewind.state && ! ( 'queued' === restoreStatus || 'running' === restoreStatus );
 
 	return {
 		allowRestore,
+		doesRewindNeedCredentials,
 		filter,
 		hasRealtimeBackups: siteSupportsRealtimeBackup( state, siteId ),
-		rewindState: rewind.state,
 		logs: logs?.data ?? [],
 		rewind,
 		siteId,
