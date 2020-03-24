e9c3a62c23933c4f92dbccae20fa3ade3f27fd08
Mikael Korpela <mikael@ihminen.org>
2020-03-24 11:58:07 +0200

    wpcom block editor: hide new Gutenberg preview button

--- apps/wpcom-block-editor/src/wpcom/editor.js
+++ apps/wpcom-block-editor/src/wpcom/editor.js
@@ -5,3 +5,8 @@ import './features/deprecate-coblocks-buttons';
 import './features/fix-block-invalidation-errors';
 import './features/reorder-block-categories';
 import './features/tracking';
+
+/**
+ * Style dependencies
+ */
+import './features/legacy-preview-button.scss';
