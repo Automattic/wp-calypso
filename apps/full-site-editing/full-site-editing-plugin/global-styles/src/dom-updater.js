581a43acd391cfe4a32772c2b26609b4649f4555
Michael P. Pfeiffer <michael@cssconf.eu>
2020-03-26 16:18:13 +0100

    Global Styles: fix previews not showing global styles

--- apps/full-site-editing/full-site-editing-plugin/global-styles/src/dom-updater.js
+++ apps/full-site-editing/full-site-editing-plugin/global-styles/src/dom-updater.js
@@ -3,6 +3,7 @@
  */
 import { subscribe } from '@wordpress/data';
 import domReady from '@wordpress/dom-ready';
+import { isEmpty } from 'lodash';
 
 /**
  * DOM updater
@@ -14,23 +15,32 @@ export default ( options, getOptionValue ) => {
 	domReady( () => {
 		const current = {};
 		const cssVariables = {};
+		const styleElement = document.createElement( 'style' );
+		document.body.appendChild( styleElement );
+		const styleSheet = styleElement.sheet;
+
 		options.forEach( option => {
 			current[ option ] = null;
 			cssVariables[ option ] = `--${ option.replace( '_', '-' ) }`;
 		} );
 
 		subscribe( () => {
+			let styleProps = '';
 			Object.keys( current ).forEach( key => {
 				const value = getOptionValue( key );
-				if ( current[ key ] !== value ) {
+				if ( ! isEmpty( value ) && current[ key ] !== value ) {
 					current[ key ] = value;
-					// We want to scope this to the root node of the editor.
-					const node = document.getElementsByClassName( 'editor-styles-wrapper' )[ 0 ];
-					if ( node ) {
-						node.style.setProperty( cssVariables[ key ], value );
-					}
+					styleProps += `${ cssVariables[ key ] }:${ value };`;
 				}
 			} );
+			// We want to scope this to the root node of the editor.
+			// We need this to be a stylesheet rather than inline styles so the styles apply to all editor instances incl. previews.
+			if ( ! isEmpty( styleProps ) ) {
+				styleSheet.insertRule(
+					`.editor-styles-wrapper{${ styleProps }}`,
+					styleSheet.cssRules.length
+				);
+			}
 		} );
 	} );
 };
