f83c8a8c0702b4c8fbd53a35d3dabff9dda06270
Sara <sara_rosso@hotmail.com>
2020-03-26 08:00:01 -0700

    Update Concierge language to Quick Start
    Updated the banner to reflect current product language.

--- client/me/purchases/concierge-banner/index.jsx
+++ client/me/purchases/concierge-banner/index.jsx
@@ -21,7 +21,7 @@ class ConciergeBanner extends Component {
 				<ActionCard
 					headerText={ this.props.translate( 'Looking for Expert Help?' ) }
 					mainText={ this.props.translate(
-						'Get 30 minutes dedicated to the success of your site. Schedule your free 1-1 concierge session with a Happiness Engineer!'
+						'Get 30 minutes dedicated to the success of your site. Schedule your free 1-1 Quick Start session with a Happiness Engineer!'
 					) }
 					buttonText="Schedule Now"
 					buttonIcon={ null }
