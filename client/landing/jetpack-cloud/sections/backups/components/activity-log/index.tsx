ed76fc724079fd2d81c3f197ff1a4b6be774b4d4
Caleb Bauermeister <cbauerman@gmail.com>
2020-03-26 15:19:55 -0700

    basic pagination

--- client/landing/jetpack-cloud/sections/backups/components/activity-log/index.tsx
+++ client/landing/jetpack-cloud/sections/backups/components/activity-log/index.tsx
@@ -1,36 +1,46 @@
 /**
  * External dependencies
  */
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 /**
  * Internal dependencies
  */
+// import Filterbar from 'my-sites/activity/filterbar';
 import { getHttpData } from 'state/data-layer/http-data';
 import { requestActivityLogs, requestActivityLogsId } from 'state/data-getters';
-import ActivityCard from 'landing/jetpack-cloud/components/activity-card';
 import { useLocalizedMoment } from 'components/localized-moment';
+import ActivityCard from 'landing/jetpack-cloud/components/activity-card';
+import Pagination from 'components/pagination';
 import Spinner from 'components/spinner';
 
 interface Props {
 	siteId: number;
 	allowRestore: boolean;
 	showDateRange: boolean;
+	pageSize?: number;
 }
 
 interface Activity {
 	activityId: string;
 }
 
-const BackupActivityLog: FunctionComponent< Props > = ( { siteId, allowRestore } ) => {
+const BackupActivityLog: FunctionComponent< Props > = ( {
+	siteId,
+	allowRestore,
+	pageSize = 10,
+} ) => {
 	const activities = useSelector( () => getHttpData( requestActivityLogsId( siteId, {} ) ) ).data;
 	const moment = useLocalizedMoment();
+
+	const [ page, setPage ] = useState( 1 );
 
 	useEffect( () => {
 		requestActivityLogs( siteId, {} );
 	}, [ siteId ] );
 
+	// todo: no spinners, but for now it works in a pinch
 	const renderLoading = () => {
 		return <Spinner />;
 	};
@@ -38,7 +48,13 @@ const BackupActivityLog: FunctionComponent< Props > = ( { siteId, allowRestore }
 	const renderData = ( loadedActivities: Array< Activity > ) => {
 		return (
 			<>
-				{ loadedActivities.map( activity => (
+				<Pagination
+					page={ page }
+					perPage={ pageSize }
+					total={ loadedActivities.length }
+					pageClick={ setPage }
+				/>
+				{ loadedActivities.slice( ( page - 1 ) * pageSize, page * pageSize ).map( activity => (
 					<ActivityCard
 						{ ...{
 							key: activity.activityId,
@@ -48,6 +64,12 @@ const BackupActivityLog: FunctionComponent< Props > = ( { siteId, allowRestore }
 						} }
 					/>
 				) ) }
+				<Pagination
+					page={ page }
+					perPage={ pageSize }
+					total={ loadedActivities.length }
+					pageClick={ setPage }
+				/>
 			</>
 		);
 	};
