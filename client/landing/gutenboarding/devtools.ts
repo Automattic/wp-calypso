5b8e2e830d0c52c5626c8ffc2b78efe0840e5100
Philip Jackson <p-jackson@live.com>
2020-03-27 17:58:14 +1300

    Gutenboarding: Remove experimental login link from signup form (#40507)
    * Remove experimental login link from signup form
    
    * Remove auth store from devtools

--- client/landing/gutenboarding/devtools.ts
+++ client/landing/gutenboarding/devtools.ts
@@ -22,36 +22,8 @@ export const setupWpDataDebug = () => {
 					client_secret: config( 'wpcom_signup_key' ),
 				};
 
-				const { Auth, Site } = require( '@automattic/data-stores' );
-				const AUTH_STORE = Auth.register( clientCreds );
+				const { Site } = require( '@automattic/data-stores' );
 				Site.register( clientCreds );
-
-				window.wp.auth = {};
-				let previousState = window.wp.data.select( AUTH_STORE ).getLoginFlowState();
-				let previousErrors = window.wp.data.select( AUTH_STORE ).getErrors();
-				window.wp.data.subscribe( () => {
-					const newState = window.wp?.data.select( AUTH_STORE ).getLoginFlowState();
-					const newErrors = window.wp?.data.select( AUTH_STORE ).getErrors();
-					if (
-						previousState !== newState ||
-						JSON.stringify( previousErrors ) !== JSON.stringify( newErrors )
-					) {
-						console.log( 'New loginFlowState =', newState );
-						if ( newErrors.length ) {
-							console.log( 'Errors =', JSON.stringify( newErrors, null, 2 ) );
-						} else {
-							console.log( 'No Errors!' );
-						}
-						previousState = newState;
-						previousErrors = newErrors;
-					}
-				} );
-				for ( const [ actionName, actionFn ] of Object.entries(
-					window.wp.data.dispatch( AUTH_STORE )
-				) ) {
-					// eslint-disable-next-line @typescript-eslint/no-explicit-any
-					window.wp.auth[ actionName ] = ( ...args: any[] ) => ( actionFn as any )( ...args );
-				}
 			}
 		}
 	}
