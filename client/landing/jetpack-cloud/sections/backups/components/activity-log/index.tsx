79e53ce3b80e70710e932b8cc3abc29160e5f40f
Caleb Bauermeister <cbauerman@gmail.com>
2020-03-27 14:50:08 -0700

    finish fixing up activiy-log component

--- client/landing/jetpack-cloud/sections/backups/components/activity-log/index.tsx
+++ client/landing/jetpack-cloud/sections/backups/components/activity-log/index.tsx
@@ -1,14 +1,16 @@
 /**
  * External dependencies
  */
-import { useSelector } from 'react-redux';
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { FunctionComponent, useCallback, useEffect } from 'react';
+import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import { getHttpData } from 'state/data-layer/http-data';
 import { requestActivityLogs, requestActivityLogsId } from 'state/data-getters';
+import { updateFilter } from 'state/activity-log/actions';
 import { useLocalizedMoment } from 'components/localized-moment';
 import ActivityCard from 'landing/jetpack-cloud/components/activity-card';
 import Filterbar from 'my-sites/activity/filterbar';
@@ -28,8 +30,13 @@ interface Props {
 	pageSize?: number;
 }
 
+//todo: move these typings somewhere else
 interface Activity {
 	activityId: string;
+}
+
+interface Filter {
+	page: number;
 }
 
 const BackupActivityLog: FunctionComponent< Props > = ( {
@@ -37,26 +44,43 @@ const BackupActivityLog: FunctionComponent< Props > = ( {
 	allowRestore,
 	pageSize = 10,
 } ) => {
-	const filter = useSelector( state => getActivityLogFilter( state, siteId ) );
-	const activities = useSelector( () => getHttpData( requestActivityLogsId( siteId, filter ) ) )
-		.data;
-
+	const dispatch = useDispatch();
 	const moment = useLocalizedMoment();
+	const translate = useTranslate();
 
-	const [ page, setPage ] = useState( 1 );
+	const filter = useSelector< object, Filter >( state => getActivityLogFilter( state, siteId ) );
+	const activities = useSelector< object, Array< Activity > | undefined >(
+		() => getHttpData( requestActivityLogsId( siteId, filter ) ).data
+	);
+
+	const setPage = useCallback(
+		( newPage: number ) => dispatch( updateFilter( siteId, { page: newPage } ) ),
+		[ dispatch, siteId ]
+	);
 
 	useEffect( () => {
 		requestActivityLogs( siteId, filter );
 	}, [ filter, siteId ] );
 
-	// todo: no spinners, but for now it works in a pinch
-	const renderLoading = () => {
-		return <Spinner />;
-	};
+	const renderPagination = ( key: string, total: number, page: number ) => (
+		<Pagination
+			key={ key }
+			nextLabel={ translate( 'Older' ) }
+			page={ page }
+			pageClick={ setPage }
+			perPage={ pageSize }
+			prevLabel={ translate( 'Newer' ) }
+			total={ total }
+		/>
+	);
 
 	const renderData = ( loadedActivities: Array< Activity > ) => {
+		const actualPage = Math.max(
+			1,
+			Math.min( filter.page, Math.ceil( loadedActivities.length / pageSize ) )
+		);
 		const renderedActivities = loadedActivities
-			.slice( ( page - 1 ) * pageSize, page * pageSize )
+			.slice( ( actualPage - 1 ) * pageSize, actualPage * pageSize )
 			.map( activity => (
 				<ActivityCard
 					{ ...{
@@ -70,32 +94,30 @@ const BackupActivityLog: FunctionComponent< Props > = ( {
 
 		return (
 			<>
-				<Filterbar
-					{ ...{
-						siteId,
-						filter,
-						isVisible: true,
-					} }
-				/>
-				<Pagination
-					page={ page }
-					perPage={ pageSize }
-					total={ loadedActivities.length }
-					pageClick={ setPage }
-				/>
+				{ renderPagination( 'activity-log__pagination-top', loadedActivities.length, actualPage ) }
 				{ renderedActivities }
-				<Pagination
-					page={ page }
-					perPage={ pageSize }
-					total={ loadedActivities.length }
-					pageClick={ setPage }
-				/>
+				{ renderPagination(
+					'activity-log__pagination-bottom',
+					loadedActivities.length,
+					actualPage
+				) }
 			</>
 		);
 	};
 
 	return (
-		<div className="activity-log">{ activities ? renderData( activities ) : renderLoading() }</div>
+		<div className="activity-log">
+			<Filterbar
+				{ ...{
+					siteId,
+					filter,
+					isVisible: true,
+					isLoading: ! activities,
+				} }
+			/>
+			{ /* todo: no spinners, but for now it works in a pinch */ }
+			{ activities ? renderData( activities ) : <Spinner /> }
+		</div>
 	);
 };
 
