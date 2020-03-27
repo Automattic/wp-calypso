d83f50392e1f3eaf17c97acb848f23d831bde45a
Omar Alshaker <omar@omaralshaker.com>
2020-03-27 18:32:04 +0100

    Handle both pre/post load states

--- client/landing/gutenboarding/onboarding-block/style-preview/disableNavigation.ts
+++ client/landing/gutenboarding/onboarding-block/style-preview/disableNavigation.ts
@@ -1,4 +1,27 @@
 /**
+ * Changes href attributes' values to "#!" and removes meta-redirect tags.
+ *
+ * @param contentDocument the document you want to process
+ */
+function disarmAnchorsAndMetaRefreshes( contentDocument: HTMLDocument ) {
+	// dis-arm anchors
+	contentDocument.body
+		?.querySelectorAll( 'a[href]:not([href="#!"])' )
+		.forEach( ( anchor: Element ) => {
+			// we don't want to remove href attribute because styling can change without it
+			// using only # will jump to top
+			anchor.setAttribute( 'href', '#!' );
+		} );
+
+	// remove meta redirects
+	contentDocument.head
+		?.querySelectorAll( 'meta[http-equiv="refresh"]' )
+		.forEach( ( element: Element ) => {
+			element.parentNode?.removeChild( element ); // element.remove() doesn't work in IE
+		} );
+}
+
+/**
  * Removes `href` attributes from all anchor elements, prevents submit events from all forms, and removes all meta-redirects
  *
  * @param iframe The iframe you want to disable navigation for.
@@ -7,28 +30,16 @@ export function disableNavigation( iframe: HTMLIFrameElement ) {
 	const { contentDocument, contentWindow } = iframe;
 
 	if ( contentDocument && contentWindow ) {
-		iframe.addEventListener( 'load', () => {
-			// dis-arm anchors
-			contentDocument.body.querySelectorAll( 'a[href]' ).forEach( ( anchor: Element ) => {
-				// we don't want to remove href attribute because styling can change without it
-				// using only # will jump to top
-				anchor.setAttribute( 'href', '#!' );
-			} );
-
-			// disarm forms
-			contentDocument.body.querySelectorAll( 'form' ).forEach( form => {
-				form.addEventListener( 'submit', ( event: Event ) => {
-					event.preventDefault();
-					return false;
-				} );
-			} );
-
-			// remove meta redirects
-			contentDocument.head
-				.querySelectorAll( 'meta[http-equiv="refresh"]' )
-				.forEach( ( element: Element ) => {
-					element.parentNode?.removeChild( element ); // element.remove() doesn't work in IE
-				} );
+		// submit event bubbles up, we only need to catch it on the window level
+		contentWindow.addEventListener( 'submit', ( event: Event ) => {
+			event.preventDefault();
+			return false;
 		} );
+
+		// run right after the HTML is injected
+		disarmAnchorsAndMetaRefreshes( contentDocument );
+
+		// run again after the iframe loads
+		iframe.addEventListener( 'load', () => disarmAnchorsAndMetaRefreshes( contentDocument ) );
 	}
 }
