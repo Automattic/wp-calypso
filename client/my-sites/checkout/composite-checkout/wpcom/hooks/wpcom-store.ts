bf1bf0fd850b8b099303029fc68fdf89a0551c6e
nbloomf <nathan.bloomfield@automattic.com>
2020-03-27 13:40:23 -0500

    Update domain contact updater in wpcom store

--- client/my-sites/checkout/composite-checkout/wpcom/hooks/wpcom-store.ts
+++ client/my-sites/checkout/composite-checkout/wpcom/hooks/wpcom-store.ts
@@ -21,7 +21,7 @@ type WpcomStoreAction =
 			type: 'APPLY_DOMAIN_CONTACT_VALIDATION_RESULTS';
 			payload: ManagedContactDetailsErrors;
 	  }
-	| { type: 'UPDATE_CONTACT_DETAILS'; payload: DomainContactDetails }
+	| { type: 'UPDATE_DOMAIN_CONTACT_FIELDS'; payload: DomainContactDetails }
 	| { type: 'SET_SITE_ID'; payload: string }
 	| { type: 'TRANSACTION_COMPLETE'; payload: object }
 	| { type: 'UPDATE_VAT_ID'; payload: string }
@@ -53,9 +53,9 @@ export function useWpcomStore(
 		action: WpcomStoreAction
 	): ManagedContactDetails {
 		switch ( action.type ) {
-			case 'UPDATE_CONTACT_DETAILS': {
+			case 'UPDATE_DOMAIN_CONTACT_FIELDS': {
 				updateContactDetailsCache( action.payload );
-				return updaters.updateDomainFields( state, action.payload );
+				return updaters.updateDomainContactFields( state, action.payload );
 			}
 			case 'UPDATE_VAT_ID':
 				return updaters.updateVatId( state, action.payload );
@@ -122,8 +122,8 @@ export function useWpcomStore(
 				return { type: 'TRANSACTION_COMPLETE', payload };
 			},
 
-			updateContactDetails( payload: DomainContactDetails ): WpcomStoreAction {
-				return { type: 'UPDATE_CONTACT_DETAILS', payload };
+			updateDomainContactFields( payload: DomainContactDetails ): WpcomStoreAction {
+				return { type: 'UPDATE_DOMAIN_CONTACT_FIELDS', payload };
 			},
 
 			updatePhone( payload: string ): WpcomStoreAction {
