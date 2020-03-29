18ae0604354dbb03c451519da6597bd9956575d3
AnnaMag <AnnaMag@users.noreply.github.com>
2020-03-29 13:47:47 +0200

    Jetpack Search: adapt post-checkout modals (#40481)
    * Jetpack Search: add draft of the thank you note
    
    * Search: adapt the Thank You modal copy
    
    Search: add CTA buttons to the thank you modal and set the primary button
    
    * Search: remove mention of wp-admin redirect from the Thank you modal.
    
    * Search: adapt the copy of the modal
    
    * Component: add correct redirect links in the Thank you modal
    
    * Correct button placement after adjusting the copy
    
    * styling and copy final touches
    
    * Extend the customizer link with the relevant section.

--- client/my-sites/plans/current-plan/current-plan-thank-you/search-thank-you.js
+++ client/my-sites/plans/current-plan/current-plan-thank-you/search-thank-you.js
@@ -11,21 +11,15 @@ import ThankYou from './thank-you';
 
 const SearchProductThankYou = ( { translate } ) => (
 	<ThankYou
-		illustration="/calypso/images/illustrations/security.svg"
-		showContinueButton
-		title={ translate( 'Hello Jetpack Search!' ) }
+		illustration="/calypso/images/illustrations/thankYou.svg"
+		showSearchRedirects
+		title={ translate( 'Welcome to Jetpack Search!' ) }
 	>
-		<p>{ translate( 'We just finished setting up search for you.' ) }</p>
+		<p>{ translate( 'We are currently indexing your site.' ) }</p>
 		<p>
 			{ translate(
-				'Next, we’ll take a look at your new WordPress.com dashboard. ' +
-					'You can manage Jetpack Search under “Tools > Activity” in the sidebar. ' +
-					'There’s also a checklist to help you get the most out of your Jetpack plan.'
-			) }
-		</p>
-		<p>
-			{ translate(
-				'You can return to your traditional WordPress dashboard anytime by using the link at the bottom of the sidebar.'
+				'In the meantime, we have configured Jetpack Search on your site — ' +
+					'you should try customizing it in your traditional WordPress dashboard.'
 			) }
 		</p>
 	</ThankYou>
