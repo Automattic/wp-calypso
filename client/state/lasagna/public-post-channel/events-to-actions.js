be2ee42f7baefea1e5abe60da7aac15508b9d4a3
Hew <github@mhs.fastmail.com>
2020-03-29 16:12:18 -0400

    One other file

--- client/state/lasagna/public-post-channel/events-to-actions.js
+++ client/state/lasagna/public-post-channel/events-to-actions.js
@@ -11,7 +11,7 @@ import { receiveComments } from 'state/comments/actions';
 const debug = debugFactory( 'lasagna:channel:public-post' );
 
 export default function( channel, store ) {
-	channel.on( 'new_comment', comment => {
+	channel.on( 'new_comment', ( { payload: comment } ) => {
 		debug( 'New comment', comment );
 
 		if ( ! comment ) {
