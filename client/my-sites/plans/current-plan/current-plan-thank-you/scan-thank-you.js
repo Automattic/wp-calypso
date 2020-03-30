42dd5960ec1b39b4fdfcf5d7bc0dc68f00fbad21
Renzo Canepa <rcanepag@gmail.com>
2020-03-30 15:05:21 -0300

    Update copy

--- client/my-sites/plans/current-plan/current-plan-thank-you/scan-thank-you.js
+++ client/my-sites/plans/current-plan/current-plan-thank-you/scan-thank-you.js
@@ -15,13 +15,13 @@ const ScanProductThankYou = ( { translate } ) => (
 		showContinueButton
 		title={ translate( 'Welcome to Jetpack Scan!' ) }
 	>
-		<p>{ translate( 'We are currently scanning your site.' ) }</p>
+		<p>{ translate( 'We just finished setting up automated malware scanning for you.' ) }</p>
 		<p>
 			{ translate(
-				'In the meantime, we have configured Jetpack Scan on your site — ' +
-					'you should try customizing it in your traditional WordPress dashboard.'
+				'Please add your server information to set up automated and one-click fixes. '
 			) }
 		</p>
+		<p>{ translate( "There's also a checklist to help you get the most out of Jetpack." ) }</p>
 	</ThankYou>
 );
 export default localize( ScanProductThankYou );
