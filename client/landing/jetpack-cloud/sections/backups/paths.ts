466b73be1cf6930a57867f55028ae389a5bb418c
Caleb Bauermeister <cbauerman@gmail.com>
2020-03-31 13:57:06 -0700

    add backups path functions

---
+++ client/landing/jetpack-cloud/sections/backups/paths.ts
@@ -0,0 +1,16 @@
+export const backupsMain = ( siteName?: string ) =>
+	undefined === siteName ? '/backups' : `/backups/${ siteName }`;
+
+const backupsSubSection = ( siteName: string, subSection: string, subSectionId?: string ) =>
+	subSectionId
+		? `/backups/${ siteName }/${ subSection }/${ subSectionId }`
+		: `/backups/${ siteName }/${ subSection }`;
+
+export const backupsRestore = ( siteName: string, rewindId: string ) =>
+	backupsSubSection( siteName, 'restore', rewindId );
+
+export const backupsDownload = ( siteName: string, rewindId: string ) =>
+	backupsSubSection( siteName, 'download', rewindId );
+
+export const backupsDetail = ( siteName: string, backupId: string ) =>
+	backupsSubSection( siteName, 'detail', backupId );
