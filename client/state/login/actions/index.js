da88cfc270cc773de0fc3237d038da0aeb41fb97
Sérgio Gomes <mail@sgomes.com>
2020-03-31 13:14:01 +0100

    Sort exports alphabetically

--- client/state/login/actions/index.js
+++ client/state/login/actions/index.js
@@ -1,17 +1,17 @@
+export { getAuthAccountType, resetAuthAccountType } from 'state/login/actions/auth-account-type';
+export { connectSocialUser } from 'state/login/actions/connect-social-user';
+export { createSocialUser } from 'state/login/actions/create-social-user';
+export { createSocialUserFailed } from 'state/login/actions/create-social-user-failed';
+export { disconnectSocialUser } from 'state/login/actions/disconnect-social-user';
+export { formUpdate } from 'state/login/actions/form-update';
+export { loginSocialUser } from 'state/login/actions/login-social-user';
 export { loginUser } from 'state/login/actions/login-user';
-export { updateNonce } from 'state/login/actions/update-nonce';
 export { loginUserWithSecurityKey } from 'state/login/actions/login-user-with-security-key';
 export { loginUserWithTwoFactorVerificationCode } from 'state/login/actions/login-user-with-two-factor-verification-code';
-export { loginSocialUser } from 'state/login/actions/login-social-user';
-export { createSocialUser } from 'state/login/actions/create-social-user';
-export { connectSocialUser } from 'state/login/actions/connect-social-user';
-export { disconnectSocialUser } from 'state/login/actions/disconnect-social-user';
-export { createSocialUserFailed } from 'state/login/actions/create-social-user-failed';
-export { sendSmsCode } from 'state/login/actions/send-sms-code';
 export {
 	receivedTwoFactorPushNotificationApproved,
 	startPollAppPushAuth,
 	stopPollAppPushAuth,
 } from 'state/login/actions/push';
-export { getAuthAccountType, resetAuthAccountType } from 'state/login/actions/auth-account-type';
-export { formUpdate } from 'state/login/actions/form-update';
+export { sendSmsCode } from 'state/login/actions/send-sms-code';
+export { updateNonce } from 'state/login/actions/update-nonce';
