Automattic/wp-calypso

commit 2a87c0051f2c07f1e9416c0c88ca498192f1d829

Update products copy/options

client/lib/products-values/constants.js
@@ -205,9 +205,7 @@ export const getJetpackProducts = () => {
 	isEnabled( 'jetpack/search-product' ) &&
 		output.push( {
 			title: translate( 'Jetpack Search' ),
-			description: translate(
-				'Incredibly powerful and customizable, Jetpack Search helps your visitors instantly find the right content – right when they need it.'
-			),
+			description: getJetpackProductsDescriptions()[ PRODUCT_JETPACK_SEARCH ],
 			id: PRODUCT_JETPACK_SEARCH,
 			// There is only one option per billing interval, but this
 			// component still needs the full display with radio buttons.
@@ -271,15 +269,17 @@ export const getJetpackProducts = () => {
 	isEnabled( 'jetpack/scan-product' ) &&
 		output.push( {
 			title: translate( 'Jetpack Scan' ),
-			description:
-				'Automatic scanning and one-click fixes keep your site one step ahead of security threats.',
+			description: getJetpackProductsDescriptions()[ PRODUCT_JETPACK_SCAN ],
 			id: PRODUCT_JETPACK_SCAN,
+			// There is only one option per billing interval, but this
+			// component still needs the full display with radio buttons.
+			forceRadios: true,
 			hasPromo: false,
 			options: {
 				yearly: [ PRODUCT_JETPACK_SCAN ],
 				monthly: [ PRODUCT_JETPACK_SCAN_MONTHLY ],
 			},
-			optionShortNames: getJetpackProductsShortNames(),
+			optionShortNames: getJetpackProductsShortNames()[ PRODUCT_JETPACK_SCAN ],
 			optionDisplayNames: getJetpackProductsDisplayNames(),
 			optionDescriptions: getJetpackProductsDescriptions(),
 			optionsLabel: translate( 'Select a product option:' ),

commit 4de474136c51b9660a20bd0cd67097b358ace1f9

Fix bug where short names are being returned as string instead of object
Introduced in https://github.com/Automattic/wp-calypso/pull/40621/commits/2a87c0051f2c07f1e9416c0c88ca498192f1d829#diff-67e84bb8c75685ed38b07fe2d8d7dd74R282

Reported in https://github.com/Automattic/wp-calypso/pull/40672#issuecomment-607471504

client/lib/products-values/constants.js
@@ -305,7 +305,7 @@ export const getJetpackProducts = () => {
 				yearly: [ PRODUCT_JETPACK_SCAN ],
 				monthly: [ PRODUCT_JETPACK_SCAN_MONTHLY ],
 			},
-			optionShortNames: getJetpackProductsShortNames()[ PRODUCT_JETPACK_SCAN ],
+			optionShortNames: getJetpackProductsShortNames(),
 			optionDisplayNames: getJetpackProductsDisplayNames(),
 			optionDescriptions: getJetpackProductsDescriptions(),
 			optionsLabel: translate( 'Select a product option:' ),