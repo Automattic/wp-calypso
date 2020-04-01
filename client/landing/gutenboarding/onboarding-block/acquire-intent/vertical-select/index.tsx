1fc69e9489a63ca978b579ec686d94ce2bb338d5
Omar Alshaker <omar@omaralshaker.com>
2020-04-01 10:53:19 +0200

    Minor visual fixes for #40321 (#40616)
    * Minor visual fixes:
    
    1. Remove the dot after vertical name
    2. Only show the vertical-field arrow for 3+ characters
    3. Keep the layout stable when the arrow appears or disappears
    
    * Use @wordpress/components for SVG and Path

--- client/landing/gutenboarding/onboarding-block/acquire-intent/vertical-select/index.tsx
+++ client/landing/gutenboarding/onboarding-block/acquire-intent/vertical-select/index.tsx
@@ -172,7 +172,7 @@ const VerticalSelect: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 		),
 	} );
 
-	const showArrow = isFocused && ! siteTitle && ! siteVertical;
+	const showArrow = isFocused && ! siteTitle && ! siteVertical && inputValue.length > 2;
 
 	return (
 		<form
@@ -181,7 +181,8 @@ const VerticalSelect: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 			} ) }
 		>
 			{ madlib }
-			{ showArrow ? <Arrow /> : '.' }
+			{ /* us visibility to keep the layout fixed with and without the arrow */ }
+			<Arrow style={ { visibility: showArrow ? 'visible' : 'hidden' } } />
 		</form>
 	);
 };
