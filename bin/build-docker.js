a6267a7a761874babe802c33949f02123dc1d9ca
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2020-04-03 19:06:13 +0300

    Remove i18n revision from docker image commit_sha (#40764)

--- bin/build-docker.js
+++ bin/build-docker.js
@@ -13,19 +13,8 @@
 const { spawnSync, execSync } = require( 'child_process' );
 
 const sha = String( execSync( 'git rev-parse HEAD' ) ).trim();
-const revisions = JSON.parse(
-	String( execSync( 'curl http://widgets.wp.com/languages/calypso/lang-revisions.json' ) ).trim()
-);
-const wpcomI18nSvnRevision = Math.max( ...Object.values( revisions ) );
 
-const args = [
-	'build',
-	'--build-arg',
-	'commit_sha=' + sha + '_' + wpcomI18nSvnRevision,
-	'-t',
-	'wp-calypso',
-	'.',
-];
+const args = [ 'build', '--build-arg', 'commit_sha=' + sha, '-t', 'wp-calypso', '.' ];
 
 console.log( 'docker ' + args.join( ' ' ) );
 spawnSync( 'docker', args, { stdio: 'inherit' } );
