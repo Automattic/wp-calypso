36821369171dcc1089138be0e74f4e5c0ff01441
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-04-03 12:55:03 +0300

    Update design selector items hover styles - remove scaling and box shadow effects - change border color on hover

--- client/landing/gutenboarding/onboarding-block/design-selector/index.tsx
+++ client/landing/gutenboarding/onboarding-block/design-selector/index.tsx
@@ -6,7 +6,6 @@ import { useDispatch } from '@wordpress/data';
 import { addQueryArgs } from '@wordpress/url';
 import { useI18n } from '@automattic/react-i18n';
 import { useHistory } from 'react-router-dom';
-import { Spring, animated } from 'react-spring/renderprops';
 
 /**
  * Internal dependencies
@@ -21,12 +20,6 @@ import { SubTitle, Title } from '../../components/titles';
 import './style.scss';
 
 type Design = import('../../stores/onboard/types').Design;
-
-// Values for springs:
-const ZOOM_OFF = { transform: 'scale(1)' };
-const ZOOM_ON = { transform: 'scale(1.015)' };
-const SHADOW_OFF = { boxShadow: '0 0px 0px rgba(0,0,0,.12)' };
-const SHADOW_ON = { boxShadow: '0 2px 12px rgba(0,0,0,.12)' };
 
 const DesignSelector: React.FunctionComponent = () => {
 	const { __: NO__ } = useI18n();
@@ -51,10 +44,6 @@ const DesignSelector: React.FunctionComponent = () => {
 		return mshotsUrl + encodeURIComponent( previewUrl );
 	};
 
-	// Track hover/focus
-	const [ hoverDesign, setHoverDesign ] = React.useState< string >();
-	const [ focusDesign, setFocusDesign ] = React.useState< string >();
-
 	return (
 		<div className="design-selector">
 			<div className="design-selector__header">
@@ -77,56 +66,29 @@ const DesignSelector: React.FunctionComponent = () => {
 			</div>
 			<div className="design-selector__design-grid">
 				<div className="design-selector__grid">
-					{ designs.featured.map( design => {
-						const isFocused = hoverDesign === design.slug || focusDesign === design.slug;
-						return (
-							<Spring
-								native
-								key={ design.slug }
-								from={ ZOOM_OFF }
-								to={ isFocused ? ZOOM_ON : ZOOM_OFF }
-							>
-								{ ( props: React.CSSProperties ) => (
-									<animated.button
-										style={ props }
-										onMouseEnter={ () => setHoverDesign( design.slug ) }
-										onMouseLeave={ () =>
-											setHoverDesign( s => ( s === design.slug ? undefined : s ) )
-										}
-										onFocus={ () => setFocusDesign( design.slug ) }
-										onBlur={ () => setFocusDesign( s => ( s === design.slug ? undefined : s ) ) }
-										className="design-selector__design-option"
-										onClick={ () => {
-											setSelectedDesign( design );
+					{ designs.featured.map( design => (
+						<button
+							key={ design.slug }
+							className="design-selector__design-option"
+							onClick={ () => {
+								setSelectedDesign( design );
 
-											// Update fonts to the design defaults
-											setFonts( design.fonts );
+								// Update fonts to the design defaults
+								setFonts( design.fonts );
 
-											if ( isEnabled( 'gutenboarding/style-preview' ) ) {
-												push( makePath( Step.Style ) );
-											}
-										} }
-									>
-										<Spring
-											native
-											key={ design.slug }
-											from={ SHADOW_OFF }
-											to={ isFocused ? SHADOW_ON : SHADOW_OFF }
-										>
-											{ ( props2: React.CSSProperties ) => (
-												<animated.span style={ props2 } className="design-selector__image-frame">
-													<img alt={ design.title } src={ getDesignUrl( design ) } />
-												</animated.span>
-											) }
-										</Spring>
-										<span className="design-selector__option-overlay">
-											<span className="design-selector__option-name">{ design.title }</span>
-										</span>
-									</animated.button>
-								) }
-							</Spring>
-						);
-					} ) }
+								if ( isEnabled( 'gutenboarding/style-preview' ) ) {
+									push( makePath( Step.Style ) );
+								}
+							} }
+						>
+							<span className="design-selector__image-frame">
+								<img alt={ design.title } src={ getDesignUrl( design ) } />
+							</span>
+							<span className="design-selector__option-overlay">
+								<span className="design-selector__option-name">{ design.title }</span>
+							</span>
+						</button>
+					) ) }
 				</div>
 			</div>
 		</div>
