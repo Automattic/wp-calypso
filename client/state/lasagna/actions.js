f6ef74e48ff1ed2db71ec48ff36678dfd4dd421e
Hew <github@mhs.fastmail.com>
2020-04-03 13:05:53 -0400

    Add Reader full post view state subtree
    We need a way to query what is onscreen in a Reader full post view if a user hits a reader link directly. The use case is joining related channel(s) after the websocket connects.

--- client/state/lasagna/actions.js
+++ client/state/lasagna/actions.js
@@ -1,5 +1,2 @@
-export const socketConnect = () => ( { type: 'LASAGNA_SOCKET_CONNECT' } );
-export const socketDisconnect = () => ( { type: 'LASAGNA_SOCKET_DISCONNECT' } );
-
 export const socketConnected = () => ( { type: 'LASAGNA_SOCKET_CONNECTED' } );
 export const socketDisconnected = () => ( { type: 'LASAGNA_SOCKET_DISCONNECTED' } );
---
+++ client/state/reader/full-view/reducer.js
@@ -0,0 +1,27 @@
+/**
+ * Internal dependencies
+ */
+import { READER_FULL_VIEW_POST_KEY_SET, SERIALIZE } from 'state/reader/action-types';
+import { combineReducers } from 'state/utils';
+
+/**
+ * Tracks the post key of the currently full viewed post
+ *
+ * @param  {object} state  Current state
+ * @param  {object} action Action payload
+ * @returns {object}        Updated state
+ */
+export function fullViewPostKey( state = null, action ) {
+	switch ( action.type ) {
+		case READER_FULL_VIEW_POST_KEY_SET:
+			return action.postKey;
+
+		case SERIALIZE:
+			return null;
+	}
+	return state;
+}
+
+export default combineReducers( {
+	fullViewPostKey,
+} );
