f91fe9da176af7a584641125fcd10fe5cf32bd4f
Alex Kirk <alex.git@kirk.at>
2020-04-03 17:31:16 +0200

    Don't show support document inline when in non-English

--- client/components/inline-support-link/index.jsx
+++ client/components/inline-support-link/index.jsx
@@ -4,7 +4,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { localize } from 'i18n-calypso';
+import { localize, getLocaleSlug } from 'i18n-calypso';
 import Gridicon from 'components/gridicon';
 
 /**
