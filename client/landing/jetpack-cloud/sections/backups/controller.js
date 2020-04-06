9a5d86d6a5f3bb658e241933dd03d4530dcf2c87
Caleb Bauermeister <cbauerman@gmail.com>
2020-04-06 08:32:12 -0700

    add basic activity log route

--- client/landing/jetpack-cloud/sections/backups/controller.js
+++ client/landing/jetpack-cloud/sections/backups/controller.js
@@ -10,6 +10,7 @@ import { SiteOffsetProvider } from 'landing/jetpack-cloud/components/site-offset
 import BackupDetailPage from './detail';
 import BackupRewindFlow, { RewindFlowPurpose } from './rewind-flow';
 import BackupsPage from './main';
+import BackupActivityLogPage from './backup-activity-log';
 
 export function wrapInSiteOffsetProvider( context, next ) {
 	context.primary = (
@@ -21,6 +22,12 @@ export function wrapInSiteOffsetProvider( context, next ) {
 /* handles /backups/:site, see `backupMainPath` */
 export function backups( context, next ) {
 	context.primary = <BackupsPage />;
+	next();
+}
+
+/* handles /backups/activity/:site, see `backupsActivityPath` */
+export function backupActivity( context, next ) {
+	context.primary = <BackupActivityLogPage />;
 	next();
 }
 
