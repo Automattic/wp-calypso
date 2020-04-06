3a6245ac63f627c3b04122a3a894274b94a6a1c6
AnnaMag <anna.mag.kedzierska@gmail.com>
2020-04-06 23:50:31 +0200

    Plans: adapt f-n calls to new redirect signature.

--- client/jetpack-connect/plans.jsx
+++ client/jetpack-connect/plans.jsx
@@ -97,7 +97,7 @@ class Plans extends Component {
 		const { canPurchasePlans, selectedSiteSlug } = this.props;
 
 		if ( selectedSiteSlug && canPurchasePlans ) {
-			return this.redirect( CALYPSO_MY_PLAN_PAGE, { 'thank-you': '' } );
+			return this.redirect( CALYPSO_MY_PLAN_PAGE, null, { 'thank-you': '' } );
 		}
 
 		return this.redirect( CALYPSO_REDIRECTION_PAGE );
@@ -176,7 +176,7 @@ class Plans extends Component {
 		this.props.completeFlow();
 		persistSignupDestination( this.getMyPlansDestination() );
 
-		this.redirect( '/checkout/', cartItem.product_slug );
+		this.redirect( '/checkout/', null, cartItem.product_slug );
 	};
 
 	shouldShowPlaceholder() {
