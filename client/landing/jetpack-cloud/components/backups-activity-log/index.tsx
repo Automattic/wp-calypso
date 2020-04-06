fbdb4e121711a15aa52afc4380ee2f7027971ff8
Caleb Bauermeister <cbauerman@gmail.com>
2020-04-06 11:05:08 -0700

    empty seperate component

---
+++ client/landing/jetpack-cloud/components/backups-activity-log/index.tsx
@@ -0,0 +1,12 @@
+/**
+ * External dependencies
+ */
+import React, { FunctionComponent } from 'react';
+// import { useTranslate } from 'i18n-calypso';
+// import { useSelector } from 'react-redux';
+
+const BackupsActivityLog: FunctionComponent = () => {
+	return <div>{ 'This is an Activity Log' }</div>;
+};
+
+export default BackupsActivityLog;
--- client/landing/jetpack-cloud/sections/backups/backup-activity-log/index.tsx
+++ client/landing/jetpack-cloud/sections/backups/backup-activity-log/index.tsx
@@ -2,9 +2,26 @@
  * External dependencies
  */
 import React, { FunctionComponent } from 'react';
+import { useTranslate } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import BackupsActivityLog from 'landing/jetpack-cloud/components/backups-activity-log';
 
 const BackupActivityLogPage: FunctionComponent = () => {
-	return <div>{ 'Welcome to activity log.' }</div>;
+	const translate = useTranslate();
+	return (
+		<div>
+			<h3>{ translate( 'Find a backup or restore point' ) }</h3>
+			<p>
+				{ translate(
+					'This is the complete event history for your site. Filter by date range and/or activity type.'
+				) }
+			</p>
+			<BackupsActivityLog />
+		</div>
+	);
 };
 
 export default BackupActivityLogPage;
