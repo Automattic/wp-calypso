223317192042f694de8a8ad087d2c0db3d9e367c
Renzo Canepa <rcanepag@gmail.com>
2020-04-06 20:25:39 -0300

    Import the data-layer module

--- client/state/jetpack-scan/threats/actions.js
+++ client/state/jetpack-scan/threats/actions.js
@@ -3,7 +3,7 @@
  */
 import { JETPACK_SCAN_THREAT_REQUEST } from 'state/action-types';
 
-// import 'state/data-layer/wpcom/sites/scan';
+import 'state/data-layer/wpcom/sites/scan';
 import 'state/jetpack-scan/init';
 
 export const requestJetpackScanThreatAction = ( siteId, threatId ) => ( {
