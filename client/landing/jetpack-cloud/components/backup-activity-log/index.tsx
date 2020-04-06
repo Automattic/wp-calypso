208126d28855a491894402a8316b5950e547b187
Caleb Bauermeister <cbauerman@gmail.com>
2020-04-06 12:24:34 -0700

    add pagination

--- client/landing/jetpack-cloud/components/backup-activity-log/index.tsx
+++ client/landing/jetpack-cloud/components/backup-activity-log/index.tsx
@@ -2,8 +2,9 @@
  * External dependencies
  */
 import { useSelector } from 'react-redux';
+import { useTranslate } from 'i18n-calypso';
 import moment from 'moment';
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 
 /**
  * Internal dependencies
@@ -11,6 +12,7 @@ import React, { FunctionComponent, useEffect } from 'react';
 import { getHttpData } from 'state/data-layer/http-data';
 import { requestActivityLogs, requestActivityLogsId } from 'state/data-getters';
 import ActivityCard from 'landing/jetpack-cloud/components/activity-card';
+import Pagination from 'components/pagination';
 
 // TODO: move these interfaces to dedicated types file
 // based on the how filters are used in client/state/data-getters/index.js
@@ -29,31 +31,73 @@ interface Activity {
 
 interface Props {
 	baseFilter: Filter;
+	pageSize?: number;
 	siteId: number;
 }
 
-const BackupActivityLog: FunctionComponent< Props > = ( { baseFilter, siteId } ) => {
+const BackupActivityLog: FunctionComponent< Props > = ( { baseFilter, pageSize = 10, siteId } ) => {
+	const translate = useTranslate();
+
 	const activities = useSelector< object, Array< Activity > | undefined >(
 		() => getHttpData( requestActivityLogsId( siteId, baseFilter ) ).data
 	);
 
+	const [ page, setPage ] = useState( 1 );
+
+	const filter = {
+		...baseFilter,
+		page,
+	};
+
 	useEffect( () => {
-		requestActivityLogs( siteId, baseFilter );
-	}, [ baseFilter, siteId ] );
+		requestActivityLogs( siteId, filter );
+	}, [ filter, siteId ] );
 
-	const renderActivities = ( loadedActivities: Array< Activity > ) =>
-		loadedActivities.map( activity => (
-			<ActivityCard
-				{ ...{
-					key: activity.activityId,
-					moment,
-					activity,
-					allowRestore: false,
-				} }
-			/>
-		) );
+	const renderPagination = ( key: string, total: number, actualPage: number ) => (
+		<Pagination
+			key={ key }
+			nextLabel={ translate( 'Older' ) }
+			page={ actualPage }
+			pageClick={ setPage }
+			perPage={ pageSize }
+			prevLabel={ translate( 'Newer' ) }
+			total={ total }
+		/>
+	);
 
-	return <div>{ activities ? renderActivities( activities ) : <p>{ 'loading...' }</p> }</div>;
+	const renderData = ( loadedActivities: Array< Activity > ) => {
+		const actualPage = Math.max(
+			1,
+			Math.min( page, Math.ceil( loadedActivities.length / pageSize ) )
+		);
+
+		const renderedActivities = loadedActivities
+			.slice( ( actualPage - 1 ) * pageSize, actualPage * pageSize )
+			.map( activity => (
+				<ActivityCard
+					{ ...{
+						key: activity.activityId,
+						moment,
+						activity,
+						allowRestore: false,
+					} }
+				/>
+			) );
+
+		return (
+			<>
+				{ renderPagination( 'activity-log__pagination-top', loadedActivities.length, actualPage ) }
+				{ renderedActivities }
+				{ renderPagination(
+					'activity-log__pagination-bottom',
+					loadedActivities.length,
+					actualPage
+				) }
+			</>
+		);
+	};
+
+	return <div>{ activities ? renderData( activities ) : <p>{ 'loading...' }</p> }</div>;
 };
 
 export default BackupActivityLog;
