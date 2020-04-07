ec93e30c3c7a1dcf4fd0f189d10dde8afa98da12
ramonjd <ramonjd@gmail.com>
2020-04-07 18:40:09 +1000

    changed constants to... well, CONSTANTS

--- client/landing/gutenboarding/onboarding-block/create-site/index.tsx
+++ client/landing/gutenboarding/onboarding-block/create-site/index.tsx
@@ -16,31 +16,33 @@ import './style.scss';
  */
 import { ProgressBar } from '@automattic/components';
 
+const CREATE_SITE_PROGRESS_TOTAL = 100;
+const CREATE_SITE_PROGRESS_STEP_COUNT = 3;
+const CREATE_SITE_PROGRESS_INTERVAL = 2000;
+
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 const CreateSite: FunctionComponent< {} > = () => {
 	const { __: NO__ } = useI18n();
 	const shouldTriggerCreate = useNewQueryParam();
-	const progressTotal = 100;
-	const progressStepCount = 3;
 	const [ progressComplete, setProgressComplete ] = React.useState( 0 );
 	const [ shouldCreateAndRedirect, setCreateAndRedirect ] = React.useState( false );
 
 	useEffect( () => {
-		if ( progressComplete < progressTotal ) {
+		if ( progressComplete < CREATE_SITE_PROGRESS_TOTAL ) {
 			setTimeout( () => {
-				const progressFactor = progressTotal / progressStepCount;
+				const progressFactor = CREATE_SITE_PROGRESS_TOTAL / CREATE_SITE_PROGRESS_STEP_COUNT;
 				setProgressComplete( progressComplete + progressFactor );
-			}, 2000 );
+			}, CREATE_SITE_PROGRESS_INTERVAL );
 		}
 
-		if ( progressComplete >= progressTotal ) {
+		if ( shouldTriggerCreate && progressComplete >= CREATE_SITE_PROGRESS_TOTAL ) {
 			setCreateAndRedirect( true );
 		}
-	}, [ progressComplete, shouldTriggerCreate ] );
+	}, [ progressComplete ] );
 
 	return (
 		<div className="create-site__background">
-			{ shouldCreateAndRedirect ? <CreateAndRedirect /> : null }
+			{ shouldCreateAndRedirect && <CreateAndRedirect /> }
 			<div className="create-site__layout">
 				<div className="create-site__header">
 					<div className="gutenboarding__header-wp-logo">
@@ -50,7 +52,7 @@ const CreateSite: FunctionComponent< {} > = () => {
 				<div className="create-site__content">
 					<h1 className="create-site__title">{ NO__( 'Building your site' ) }</h1>
 					<div className="create-site__progress">
-						<ProgressBar value={ progressComplete } total={ progressTotal } />
+						<ProgressBar value={ progressComplete } total={ CREATE_SITE_PROGRESS_TOTAL } />
 					</div>
 					{ /* TODO: interpolate this test with step count args */ }
 					<p className="create-site__progress-text">{ NO__( 'Step 1 of 3' ) }</p>
