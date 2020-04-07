10242ece0c734c4988297c48b6380ba3771a6051
Rich Collier <rcoll@users.noreply.github.com>
2020-04-07 10:14:55 -0400

    Adding redux state and data layer code to get site capabilities, and integrate into backups main (#40772)
    Co-authored-by: Richard Collier <rcollier@hypnos.local>

---
+++ client/state/data-layer/wpcom/sites/rewind/capabilities/index.js
@@ -0,0 +1,46 @@
+/**
+ * Internal dependencies
+ */
+import { registerHandlers } from 'state/data-layer/handler-registry';
+import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
+import { http } from 'state/data-layer/wpcom-http/actions';
+import { REWIND_CAPABILITIES_REQUEST, REWIND_CAPABILITIES_UPDATE } from 'state/action-types';
+
+const fetchCapabilities = action =>
+	http(
+		{
+			apiNamespace: 'wpcom/v2',
+			method: 'GET',
+			path: `/sites/${ action.siteId }/rewind/capabilities`,
+			query: {
+				force: 'wpcom',
+			},
+		},
+		action
+	);
+
+const updateCapabilities = ( { siteId }, data ) => {
+	return {
+		type: REWIND_CAPABILITIES_UPDATE,
+		siteId,
+		data: data.capabilities,
+	};
+};
+
+const onError = ( { siteId } ) => {
+	return {
+		type: REWIND_CAPABILITIES_UPDATE,
+		siteId,
+		data: [],
+	};
+};
+
+registerHandlers( 'state/data-layer/wpcom/sites/rewind/capabilities', {
+	[ REWIND_CAPABILITIES_REQUEST ]: [
+		dispatchRequest( {
+			fetch: fetchCapabilities,
+			onSuccess: updateCapabilities,
+			onError,
+		} ),
+	],
+} );
