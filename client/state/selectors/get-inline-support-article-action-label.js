2c604ef3fe2c253986610895000fcde3305d6831
mmtr <miguel.torres@automattic.com>
2020-04-08 11:10:05 +0200

    Use sentence case

--- client/state/selectors/get-inline-support-article-action-label.js
+++ client/state/selectors/get-inline-support-article-action-label.js
@@ -7,4 +7,4 @@ import { translate } from 'i18n-calypso';
  * @param {object} state Global app state
  * @returns {object} ...
  */
-export default state => state?.inlineSupportArticle?.actionLabel ?? translate( 'Visit Article' );
+export default state => state?.inlineSupportArticle?.actionLabel ?? translate( 'Visit article' );
