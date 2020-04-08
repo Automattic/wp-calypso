944aaf1e609219c89b858e27c6b8e189aadd4737
undemian <undemian@gmail.com>
2020-04-08 13:47:10 +0300

    Update post discussion count instead

--- client/state/lasagna/blog-channel/events-to-actions.js
+++ client/state/lasagna/blog-channel/events-to-actions.js
@@ -6,15 +6,10 @@ import debugFactory from 'debug';
 /**
  * Internal dependencies
  */
-import {
-	incrementCommentCount,
-	receiveCommentCount,
-	receiveComments,
-} from 'state/comments/actions';
+import { receiveComments } from 'state/comments/actions';
+import { incrementPostDiscussionCount } from 'state/reader/posts/actions';
 import { updateChannel } from 'state/lasagna/channel';
 import { namespace } from './manager';
-import { getPostTotalCommentsCount } from 'state/comments/selectors/get-post-total-comments-count';
-import { getPostById } from 'state/reader/posts/selectors';
 
 /**
  * Module variables
@@ -41,17 +36,8 @@ export default function( channel, topic, store ) {
 			} )
 		);
 
-		// update comment counts
-		const state = store.getState();
-		const totalComments = getPostTotalCommentsCount( state, siteId, postId );
-		if ( ! totalComments ) {
-			const post = getPostById( state, comment.global_ID );
-			store.dispatch(
-				receiveCommentCount( { siteId, postId, totalCommentsCount: post.discussion.comment_count } )
-			);
-		}
-
-		store.dispatch( incrementCommentCount( { siteId, postId } ) );
+		// update post comment counts
+		store.dispatch( incrementPostDiscussionCount( { globalId: comment.global_ID } ) );
 
 		// update channel
 		updateChannel( { namespace, topic } );
