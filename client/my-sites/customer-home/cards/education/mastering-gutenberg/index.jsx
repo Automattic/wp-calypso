8cf67838c0ed461ee90cddec2929f951025a0449
Aurorum <torres126@clubpenguinmountains.com>
2020-04-09 19:24:32 +0100

    Customer Home: Add Mastering Gutenberg Card (#39617)
    * Create Mastering Gutenberg Card
    
    * Some styling
    
    * Embed card
    
    * Only show for sites using Gutenberg
    
    * Localize support documents
    
    * Use switch cases
    
    * Fix accidental issues
    
    * Make design changes
    
    * Fix styling
    
    * Oops, keep Gridicon
    
    * Hide image on mobile
    
    * Capitalise title again
    
    * Review changes and update poster to test they work
    
    * Update card
    
    * Hide for sites unable to use Gutenberg
    
    * Switch from dismissible card
    
    * Create a conflict
    
    * Use support article dialog
    
    * Remove old styling
    
    * Address review
    
    * Switch order

---
+++ client/my-sites/customer-home/cards/education/mastering-gutenberg/index.jsx
@@ -0,0 +1,57 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { useTranslate } from 'i18n-calypso';
+import { Card } from '@automattic/components';
+import { isMobile } from '@automattic/viewport';
+
+/**
+ * Internal dependencies
+ */
+import CardHeading from 'components/card-heading';
+import InlineSupportLink from 'components/inline-support-link';
+import { localizeUrl } from 'lib/i18n-utils';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
+const MasteringGutenberg = () => {
+	const translate = useTranslate();
+
+	return (
+		<Card className="mastering-gutenberg">
+			{ ! isMobile() && (
+				<div className="mastering-gutenberg__illustration">
+					<img src="/calypso/images/illustrations/gutenberg-mini.svg" alt="" />
+				</div>
+			) }
+			<div>
+				<CardHeading>{ translate( 'Master the Block Editor' ) }</CardHeading>
+				<p className="mastering-gutenberg__text customer-home__card-subheader">
+					{ translate(
+						'Learn how to create stunning post and page layouts through our video guides.'
+					) }
+				</p>
+				<InlineSupportLink
+					supportPostId={ 147594 }
+					supportLink={ localizeUrl( 'https://wordpress.com/support/wordpress-editor/#blocks' ) }
+					showIcon={ false }
+					text={ translate( 'Customizing posts and pages with blocks' ) }
+				/>
+				<InlineSupportLink
+					supportPostId={ 147594 }
+					supportLink={ localizeUrl(
+						'https://wordpress.com/support/wordpress-editor/#configuring-a-block'
+					) }
+					showIcon={ false }
+					text={ translate( 'Adjusting settings of blocks' ) }
+				/>
+			</div>
+		</Card>
+	);
+};
+
+export default MasteringGutenberg;
--- client/my-sites/customer-home/locations/primary/index.jsx
+++ client/my-sites/customer-home/locations/primary/index.jsx
@@ -8,12 +8,14 @@ import React from 'react';
  */
 import GoMobile from 'my-sites/customer-home/cards/features/go-mobile';
 import ChecklistSiteSetup from 'my-sites/customer-home/cards/primary/checklist-site-setup';
+import MasteringGutenberg from 'my-sites/customer-home/cards/education/mastering-gutenberg';
 import QuickLinks from 'my-sites/customer-home/cards/primary/quick-links';
 
 const cardComponents = {
 	'home-feature-go-mobile-phones': GoMobile,
 	'home-primary-checklist-site-setup': ChecklistSiteSetup,
 	'home-primary-quick-links': QuickLinks,
+	'home-education-mastering-gutenberg': MasteringGutenberg,
 };
 
 const Primary = ( { checklistMode, cards } ) => {
