3031827187e27fe2b06d24652ec256e9d2ec9f82
Yan Sern <yan.sern@automattic.com>
2020-04-09 20:38:21 +0800

    Update placeholder with changes from actual suggestion item.

--- client/landing/gutenboarding/components/domain-picker/suggestion-item-placeholder.tsx
+++ client/landing/gutenboarding/components/domain-picker/suggestion-item-placeholder.tsx
@@ -6,9 +6,8 @@ import React, { FunctionComponent } from 'react';
 const DomainPickerSuggestionItemPlaceholder: FunctionComponent = () => {
 	return (
 		<div className="domain-picker__suggestion-item">
-			<div className="domain-picker__suggestion-item-name placeholder">
-				<input disabled className="domain-picker__suggestion-radio-button" type="radio" />
-			</div>
+			<input disabled className="domain-picker__suggestion-radio-button" type="radio" />
+			<div className="domain-picker__suggestion-item-name placeholder"></div>
 			<div className="domain-picker__price placeholder"></div>
 		</div>
 	);
