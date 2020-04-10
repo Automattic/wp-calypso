c8869d656aa62f24f3f6fef21923012db9834150
mmtr <miguel.torres@automattic.com>
2020-04-10 18:39:34 +0200

    Remove grow & earn card from "management" location

--- client/my-sites/customer-home/locations/management/index.jsx
+++ client/my-sites/customer-home/locations/management/index.jsx
@@ -8,13 +8,11 @@ import { useTranslate } from 'i18n-calypso';
  * Internal dependencies
  */
 import GoMobile from 'my-sites/customer-home/cards/features/go-mobile';
-import GrowEarn from 'my-sites/customer-home/cards/features/grow-earn';
 import Support from 'my-sites/customer-home/cards/features/support';
 import QuickLinks from 'my-sites/customer-home/cards/primary/quick-links';
 
 const cardComponents = {
 	'home-feature-go-mobile-desktop': GoMobile,
-	'home-feature-grow-and-earn': GrowEarn,
 	'home-feature-support': Support,
 	'home-primary-quick-links': QuickLinks,
 };
