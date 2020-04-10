c288625e19e0c906128d52749833768546c25c83
nbloomf <nathan.bloomfield@automattic.com>
2020-04-10 14:29:55 -0500

    Pass apiVersion parameter to validation endpoint wrapper

--- client/my-sites/checkout/composite-checkout/contact-validation.js
+++ client/my-sites/checkout/composite-checkout/contact-validation.js
@@ -28,41 +28,46 @@ export default function createContactValidationCallback( {
 				domainNames,
 				contactDetails
 			);
-			validateDomainContact( contact_information, domain_names, ( httpErrors, data ) => {
-				recordEvent( {
-					type: 'VALIDATE_DOMAIN_CONTACT_INFO',
-					payload: {
-						credits: null,
-						payment_method: translateCheckoutPaymentMethodToWpcomPaymentMethod( paymentMethodId ),
-					},
-				} );
-				debug(
-					'Domain contact info validation for domains',
-					domainNames,
-					'and contact info',
-					contactDetails,
-					'result:',
-					data
-				);
-				if ( ! data ) {
-					showErrorMessage(
-						translate(
-							'There was an error validating your contact information. Please contact support.'
-						)
+			validateDomainContact(
+				contact_information,
+				domain_names,
+				( httpErrors, data ) => {
+					recordEvent( {
+						type: 'VALIDATE_DOMAIN_CONTACT_INFO',
+						payload: {
+							credits: null,
+							payment_method: translateCheckoutPaymentMethodToWpcomPaymentMethod( paymentMethodId ),
+						},
+					} );
+					debug(
+						'Domain contact info validation for domains',
+						domainNames,
+						'and contact info',
+						contactDetails,
+						'result:',
+						data
 					);
-					resolve( false );
-					return;
-				}
-				if ( data.messages ) {
-					showErrorMessage(
-						translate(
-							'We could not validate your contact information. Please review and update all the highlighted fields.'
-						)
-					);
-				}
-				applyDomainContactValidationResults( { ...data.messages } );
-				resolve( ! ( data.success && areRequiredFieldsNotEmpty( decoratedContactDetails ) ) );
-			} );
+					if ( ! data ) {
+						showErrorMessage(
+							translate(
+								'There was an error validating your contact information. Please contact support.'
+							)
+						);
+						resolve( false );
+						return;
+					}
+					if ( data.messages ) {
+						showErrorMessage(
+							translate(
+								'We could not validate your contact information. Please review and update all the highlighted fields.'
+							)
+						);
+					}
+					applyDomainContactValidationResults( { ...data.messages } );
+					resolve( ! ( data.success && areRequiredFieldsNotEmpty( decoratedContactDetails ) ) );
+				},
+				{ apiVersion: '1.2' }
+			);
 		} );
 	};
 }
