a9fc09d65a2f4d62a2403cfeb19b82776b50d9ec
Yan Sern <yan.sern@automattic.com>
2020-04-14 18:54:13 +0800

    Self closing div.
    Co-Authored-By: Jon Surrell <jon.surrell@automattic.com>

--- client/landing/gutenboarding/components/domain-picker/suggestion-item-placeholder.tsx
+++ client/landing/gutenboarding/components/domain-picker/suggestion-item-placeholder.tsx
@@ -7,7 +7,7 @@ const DomainPickerSuggestionItemPlaceholder: FunctionComponent = () => {
 	return (
 		<div className="domain-picker__suggestion-item">
 			<input disabled className="domain-picker__suggestion-radio-button" type="radio" />
-			<div className="domain-picker__suggestion-item-name placeholder"></div>
+			<div className="domain-picker__suggestion-item-name placeholder" />
 			<div className="domain-picker__price placeholder"></div>
 		</div>
 	);
