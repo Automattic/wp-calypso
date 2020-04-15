07434a52801a9bc8a8aa85020c09bc85223ff6d6
retrofox <rdsuarez@gmail.com>
2020-04-15 08:36:04 -0300

    wpcom-block-editor: import tracking store

--- apps/wpcom-block-editor/src/wpcom/editor.js
+++ apps/wpcom-block-editor/src/wpcom/editor.js
@@ -14,6 +14,7 @@ import { registerPlugin } from '@wordpress/plugins';
 import './features/deprecate-coblocks-buttons';
 import './features/fix-block-invalidation-errors';
 import './features/reorder-block-categories';
+import './features/tracking/store';
 import './features/tracking';
 
 import InserterMenuTrackingEvent from './features/tracking/wpcom-inserter-menu-search-term';
