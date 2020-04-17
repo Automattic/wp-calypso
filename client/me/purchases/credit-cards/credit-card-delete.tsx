2e4303118586e6a55d552cf95f42d94704e04684
Daniel Guerra <daniel_guerra@outlook.com>
2020-04-17 10:51:12 -0500

    Use redux hooks and remove the connect HOC from CreditCardDelete. Add ReduxDispatch type to add proper redux-thunk support to useDispatch.

--- client/me/purchases/credit-cards/credit-card-delete.tsx
+++ client/me/purchases/credit-cards/credit-card-delete.tsx
@@ -3,12 +3,13 @@
  */
 
 import React, { FunctionComponent, useState, useCallback } from 'react';
-import { connect } from 'react-redux';
-import { useTranslate, TranslateResult } from 'i18n-calypso';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
+import { ReduxDispatch } from 'state/redux-store';
 import { deleteStoredCard } from 'state/stored-cards/actions';
 import { errorNotice, successNotice } from 'state/notices/actions';
 import { isDeletingStoredCard } from 'state/stored-cards/selectors';
@@ -28,41 +29,37 @@ import './credit-card-delete.scss';
 
 interface Props {
 	card: PaymentMethod;
-	errorNotice: ( msg: TranslateResult ) => void;
-	successNotice: ( msg: TranslateResult ) => void;
-	deleteStoredCard: ( card: PaymentMethod ) => Promise< void >;
-	isDeleting: boolean;
 }
 
-const CreditCardDelete: FunctionComponent< Props > = props => {
+const CreditCardDelete: FunctionComponent< Props > = ( { card } ) => {
 	const translate = useTranslate();
+	const isDeleting = useSelector( state => isDeletingStoredCard( state, card.stored_details_id ) );
+	const reduxDispatch = useDispatch< ReduxDispatch >();
 	const [ isDialogVisible, setIsDialogVisible ] = useState( false );
-
 	const closeDialog = useCallback( () => setIsDialogVisible( false ), [] );
 
 	const handleDelete = useCallback( () => {
 		closeDialog();
-		props
-			.deleteStoredCard( props.card )
+		reduxDispatch( deleteStoredCard( card ) )
 			.then( () => {
-				if ( isPaymentAgreement( props.card ) ) {
-					props.successNotice( translate( 'Payment method deleted successfully' ) );
+				if ( isPaymentAgreement( card ) ) {
+					reduxDispatch( successNotice( translate( 'Payment method deleted successfully' ) ) );
 				} else {
-					props.successNotice( translate( 'Card deleted successfully' ) );
+					reduxDispatch( successNotice( translate( 'Card deleted successfully' ) ) );
 				}
 			} )
 			.catch( ( error: Error ) => {
-				errorNotice( error.message );
+				reduxDispatch( errorNotice( error.message ) );
 			} );
-	}, [ closeDialog, props, translate ] );
+	}, [ closeDialog, card, translate, reduxDispatch ] );
 
 	const renderDeleteButton = () => {
-		const text = props.isDeleting ? translate( 'Deleting…' ) : translate( 'Delete' );
+		const text = isDeleting ? translate( 'Deleting…' ) : translate( 'Delete' );
 
 		return (
 			<Button
 				className="credit-cards__delete-button"
-				disabled={ props.isDeleting }
+				disabled={ isDeleting }
 				onClick={ () => setIsDialogVisible( true ) }
 			>
 				{ text }
@@ -75,38 +72,25 @@ const CreditCardDelete: FunctionComponent< Props > = props => {
 			<PaymentMethodDeleteDialog
 				paymentMethodSummary={ getPaymentMethodSummary( {
 					translate,
-					type: props.card.card_type || props.card.payment_partner,
-					digits: props.card.card,
-					email: props.card.email,
+					type: card.card_type || card.payment_partner,
+					digits: card.card,
+					email: card.email,
 				} ) }
 				isVisible={ isDialogVisible }
 				onClose={ closeDialog }
 				onConfirm={ handleDelete }
 			/>
 			<StoredCard
-				lastDigits={ props.card.card }
-				email={ props.card.email }
-				cardType={ props.card.card_type || '' }
-				paymentPartner={ props.card.payment_partner }
-				name={ props.card.name }
-				expiry={ props.card.expiry }
+				lastDigits={ card.card }
+				email={ card.email }
+				cardType={ card.card_type || '' }
+				paymentPartner={ card.payment_partner }
+				name={ card.name }
+				expiry={ card.expiry }
 			/>
 			{ renderDeleteButton() }
 		</div>
 	);
 };
 
-interface OwnProps {
-	card: PaymentMethod;
-}
-
-export default connect< {}, {}, OwnProps >(
-	( state, props ) => ( {
-		isDeleting: isDeletingStoredCard( state, props.card.stored_details_id ),
-	} ),
-	{
-		deleteStoredCard,
-		errorNotice,
-		successNotice,
-	}
-)( CreditCardDelete );
+export default CreditCardDelete;
