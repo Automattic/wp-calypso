5374cfe473035a8fab6065f7ea56f6f1bc8fec28
Jon Surrell <jon.surrell@automattic.com>
2020-04-17 10:46:41 +0200

    Use isSignup=true to get vendor

--- client/landing/gutenboarding/stores/domain-suggestions/index.ts
+++ client/landing/gutenboarding/stores/domain-suggestions/index.ts
@@ -5,5 +5,5 @@ import { DomainSuggestions } from '@automattic/data-stores';
 import { getSuggestionsVendor } from '../../../../lib/domains/suggestions';
 
 export const DOMAIN_SUGGESTIONS_STORE = DomainSuggestions.register( {
-	vendor: getSuggestionsVendor(),
+	vendor: getSuggestionsVendor( true ),
 } );
