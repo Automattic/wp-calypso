4ff5bee428f32ecc90d56c7998f9abfc8ed0fd60
James Tien <southp0105@gmail.com>
2020-04-20 13:59:44 +0800

    Update `site_id` as `blog_id` since the latter is what Tracks expects.

--- client/lib/analytics/signup.js
+++ client/lib/analytics/signup.js
@@ -41,9 +41,12 @@ export function recordSignupComplete(
 	}
 
 	// Tracks
+	// Note that Tracks expects blog_id to differntiate sites, hence using
+	// blog_id instead of site_id here. We keep using "siteId" otherwise since
+	// all the other fields still refer with "site". e.g. isNewSite
 	analytics.tracks.recordEvent( 'calypso_signup_complete', {
 		flow,
-		site_id: siteId,
+		blog_id: siteId,
 		is_new_user: isNewUser,
 		is_new_site: isNewSite,
 		has_cart_items: hasCartItems,
