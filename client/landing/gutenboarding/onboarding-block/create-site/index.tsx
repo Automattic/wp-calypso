6933ce1ee742dc7bbb0637a2331aa17b5c45cb20
Jon Surrell <jon.surrell@automattic.com>
2020-04-21 12:09:13 +0200

    Make domain step conditional

--- client/landing/gutenboarding/onboarding-block/create-site/index.tsx
+++ client/landing/gutenboarding/onboarding-block/create-site/index.tsx
@@ -5,6 +5,7 @@ import * as React from 'react';
 import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@automattic/react-i18n';
 import { Icon } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
 import { useInterval } from '../../../../lib/interval/use-interval';
 
 /**
@@ -13,6 +14,7 @@ import { useInterval } from '../../../../lib/interval/use-interval';
 import CreateAndRedirect from './create-and-redirect';
 import { useNewQueryParam } from '../../path';
 import { useTrackStep } from '../../hooks/use-track-step';
+import { STORE_KEY } from '../../stores/onboard';
 import './style.scss';
 
 // Total time to perform "loading"
@@ -23,11 +25,23 @@ const CreateSite: React.FunctionComponent = () => {
 	const { __ } = useI18n();
 	const shouldTriggerCreate = useNewQueryParam();
 	const [ shouldCreateAndRedirect, setCreateAndRedirect ] = React.useState( false );
+	const hasPaidDomain: boolean = useSelect( ( select ) => {
+		const domain = select( STORE_KEY ).getState().domain;
+
+		// No domain is not paid
+		if ( ! domain ) {
+			return false;
+		}
+
+		return ! domain.is_free;
+	} );
 
 	const steps = React.useRef< string[] >(
-		[ __( 'Building your site' ), __( 'Getting your domain' ), __( 'Applying design' ) ].filter(
-			Boolean
-		) as string[]
+		[
+			__( 'Building your site' ),
+			hasPaidDomain && __( 'Getting your domain' ),
+			__( 'Applying design' ),
+		].filter( Boolean ) as string[]
 	);
 	const totalSteps = steps.current.length;
 
