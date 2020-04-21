40b203e3d8141135e7fa56717e002e6d4c1b2393
Dan Hauk <hauk.dan@gmail.com>
2020-04-21 17:40:40 -0400

    Tweak styles of layout and educational card

--- client/my-sites/customer-home/cards/education/mastering-gutenberg/index.jsx
+++ client/my-sites/customer-home/cards/education/mastering-gutenberg/index.jsx
@@ -17,14 +17,14 @@ const MasteringGutenberg = () => {
 		<EducationalContent
 			title={ translate( 'Master the Block Editor' ) }
 			description={ translate(
-				'Learn how to create stunning post and page layouts through our video guides.'
+				'Learn how to create stunning post and page layouts with our short video guides.'
 			) }
 			links={ [
 				{
 					postId: 147594,
 					url: localizeUrl( 'https://wordpress.com/support/wordpress-editor/#blocks' ),
 					text: translate( 'Adding and moving blocks' ),
-					icon: 'add-outline',
+					icon: 'video',
 					tracksEvent: 'calypso_customer_home_customizing_with_blocks_support_page_view',
 					statsName: 'view_customizing_with_blocks_video',
 				},
@@ -32,7 +32,7 @@ const MasteringGutenberg = () => {
 					postId: 147594,
 					url: localizeUrl( 'https://wordpress.com/support/wordpress-editor/#configuring-a-block' ),
 					text: translate( 'Adjust settings of blocks' ),
-					icon: 'cog',
+					icon: 'video',
 					tracksEvent: 'calypso_customer_home_adjust_blocks_support_page_view',
 					statsName: 'view_adjust_blocks_video',
 				},
