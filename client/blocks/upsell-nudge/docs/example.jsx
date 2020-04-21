85ee2e91dda488757ab20f2fd9fee62996ed1706
Caroline Moore <calobee@gmail.com>
2020-04-21 12:09:11 -0400

    Revert "Devdocs: Update docs for Banners and fix UpsellNudge (#41328)"
    This reverts commit c7e810e0b6696f8b8c847e706de16384274eee11.

--- client/blocks/upsell-nudge/docs/example.jsx
+++ client/blocks/upsell-nudge/docs/example.jsx
@@ -11,14 +11,12 @@ import UpsellNudge from 'blocks/upsell-nudge';
 const UpsellNudgeExample = () => (
 	<>
 		<UpsellNudge
-			forceDisplay
 			href="#"
 			callToAction="Upgrade"
 			title="Free domain with a plan! This is a regular nudge with an icon."
 			showIcon={ true }
 		/>
 		<UpsellNudge
-			forceDisplay
 			href="#"
 			compact
 			callToAction="Upgrade"
