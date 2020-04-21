3267549b721d0410e02704cdf494121367bcda5f
Matt Gawarecki <matt.gawarecki@gmail.com>
2020-04-21 07:33:04 -0500

    Use addQueryArgs to build the URL query string

--- client/landing/jetpack-cloud/lib/contact-support-url.js
+++ client/landing/jetpack-cloud/lib/contact-support-url.js
@@ -2,6 +2,7 @@
  * Internal dependencies
  */
 import { JETPACK_CONTACT_SUPPORT } from 'lib/url/support';
+import { addQueryArgs } from 'lib/url';
 
 /**
  * Creates a URL that refers to the Jetpack 'Contact Support' page,
@@ -16,16 +17,11 @@ export default function contactSupportUrl( siteUrl, scanState ) {
 	// NOTE: This is an extremely simple, early implementation;
 	// if we add more options in the future, we'll want
 	// to come back and change how we build this query string
-	const params = [];
-	if ( siteUrl ) {
-		params.push( `url=${ siteUrl }` );
-	}
-
-	if ( scanState ) {
-		params.push( `scan-state=${ scanState }` );
-	}
-
-	// At present, JETPACK_CONTACT_SUPPORT already contains an initial query string
-	// parameter, so we can safely append all our parameters using '&'
-	return [ JETPACK_CONTACT_SUPPORT, ...params ].join( '&' );
+	return addQueryArgs(
+		{
+			url: siteUrl,
+			'scan-state': scanState
+		},
+		JETPACK_CONTACT_SUPPORT
+	);
 }
