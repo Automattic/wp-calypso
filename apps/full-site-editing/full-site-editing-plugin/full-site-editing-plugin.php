e3b5625abc11dfed0176ae245d68c67d38ce7583
Konstantin Obenland <obenland@gmx.de>
2020-04-22 10:52:34 -0700

    FSE: Bump plugin version to 1.1 (#41347)

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing-plugin.php
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Full Site Editing
  * Description: Enhances your page creation workflow within the Block Editor.
- * Version: 1.0
+ * Version: 1.1
  * Author: Automattic
  * Author URI: https://automattic.com/wordpress-plugins/
  * License: GPLv2 or later
@@ -35,7 +35,7 @@ namespace A8C\FSE;
  *
  * @var string
  */
-define( 'PLUGIN_VERSION', '1.0' );
+define( 'PLUGIN_VERSION', '1.1' );
 
 // Always include these helper files for dotcom FSE.
 require_once __DIR__ . '/dotcom-fse/helpers.php';
