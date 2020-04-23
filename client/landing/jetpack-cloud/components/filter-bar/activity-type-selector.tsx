d9f947ed806f331448f8687480165190f9aa7c20
Caleb Bauermeister <cbauerman@gmail.com>
2020-04-23 10:58:51 -0700

    feature complete filterbar

--- client/landing/jetpack-cloud/components/filter-bar/activity-type-selector.tsx
+++ client/landing/jetpack-cloud/components/filter-bar/activity-type-selector.tsx
@@ -2,7 +2,8 @@
  * External dependencies
  */
 import { useTranslate } from 'i18n-calypso';
-import React, { FunctionComponent, useRef, ChangeEvent } from 'react';
+import React, { FunctionComponent, useRef, ChangeEvent, useEffect, useState } from 'react';
+import { isEqual } from 'lodash';
 
 /**
  * Internal dependencies
@@ -38,16 +39,36 @@ const ActivityTypeSelector: FunctionComponent< Props > = ( {
 	const translate = useTranslate();
 	const buttonRef = useRef();
 
+	const [ localGroups, setLocalGroups ] = useState( groups );
+
 	const onChange = ( { target: { checked, name } }: ChangeEvent< HTMLInputElement > ) => {
-		onGroupsChange(
-			( checked ? [ ...groups, name ] : groups.filter( ( key ) => key !== name ) ).filter(
+		setLocalGroups(
+			( checked ? [ ...localGroups, name ] : localGroups.filter( ( key ) => key !== name ) ).filter(
 				( value, index, self ) => self.indexOf( value ) === index
 			)
 		);
 	};
 
+	const onApply = () => {
+		onGroupsChange( localGroups );
+		onClose();
+	};
+
+	const onSelectAll = () => {
+		setLocalGroups( activityCounts.map( ( { key } ) => key ) );
+	};
+
+	const hasChanges = ! isEqual( groups, localGroups );
+
+	const hasAllSelected = localGroups.length === activityCounts.length;
+
+	// whenever we get a new controlled value or we are hidden/visible that is the new standard
+	useEffect( () => {
+		setLocalGroups( groups );
+	}, [ groups, isVisible ] );
+
 	return (
-		<>
+		<div className="activity-type-selector">
 			<Button ref={ buttonRef } compact onClick={ onClick }>
 				{ translate( 'Activity type' ) }
 			</Button>
@@ -59,12 +80,23 @@ const ActivityTypeSelector: FunctionComponent< Props > = ( {
 			>
 				{ activityCounts.map( ( { name, key, count } ) => (
 					<FormLabel key={ key } optional={ false } required={ false }>
-						<FormCheckbox name={ key } checked={ groups.includes( key ) } onChange={ onChange } />
-						<span>{ `${ name } ( ${ count } )` }</span>
+						<FormCheckbox
+							className="activity-type-selector__check-box"
+							name={ key }
+							checked={ localGroups.includes( key ) }
+							onChange={ onChange }
+						/>
+						<span>{ `${ name } (${ count })` }</span>
 					</FormLabel>
 				) ) }
+				<Button borderless compact disabled={ hasAllSelected } onClick={ onSelectAll }>
+					{ translate( 'Select all' ) }
+				</Button>
+				<Button primary compact disabled={ ! hasChanges } onClick={ onApply }>
+					{ translate( 'Apply' ) }
+				</Button>
 			</Popover>
-		</>
+		</div>
 	);
 };
 
