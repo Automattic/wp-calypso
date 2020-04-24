b118d12592bdedf123534bd30a6b69a8895fb4a7
Echo <ChaosExAnima@users.noreply.github.com>
2020-04-24 14:19:14 -0400

    Refactors upsell to use state machine API (#41385)
    * Change capabilities API to return null when incomplete.
    
    * Adds component to show upsell if capability doesn't exist.
    
    * Adds component readme.
    
    * Displays the Jetpack logo during API call.
    
    * Removes extra controller in favor of using two routes.
    
    * Revert "Removes extra controller in favor of using two routes."
    
    This reverts commit a21e636f1803b6fecd961b87ea5e8966279e6f7d.
    
    * Refactors system to use state instead  of capabilities.

--- client/landing/jetpack-cloud/sections/scan/controller.js
+++ client/landing/jetpack-cloud/sections/scan/controller.js
@@ -2,49 +2,31 @@
  * External dependencies
  */
 import React from 'react';
-import { noop } from 'lodash';
 
 /**
  * Internal dependencies
  */
+import UpsellSwitch from 'landing/jetpack-cloud/components/upsell-switch';
 import ScanPage from './main';
 import ScanHistoryPage from './history';
 import ScanUpsellPage from './upsell';
-import { getSelectedSiteId } from 'state/ui/selectors';
-import { getSitePurchases } from 'state/purchases/selectors';
-import { JETPACK_SCAN_PRODUCTS, PRODUCT_JETPACK_SCAN } from 'lib/products-values/constants';
-import { getPlan, planHasFeature } from 'lib/plans';
-import { fetchSitePurchases } from 'state/purchases/actions';
-import { makeLayout, render as clientRender } from 'controller';
+import getSiteScanState from 'state/selectors/get-site-scan-state';
+import QueryJetpackScan from 'components/data/query-jetpack-scan';
 
 export function showUpsellIfNoScan( context, next ) {
-	const getState = context.store.getState;
-	const dispatch = context.store.dispatch;
-	const siteId = getSelectedSiteId( getState() );
-	let purchases = getSitePurchases( getState(), siteId );
-
-	const showUpsellOrNext = () => {
-		const hasScan = purchases.find(
-			( purchase ) =>
-				JETPACK_SCAN_PRODUCTS.includes( purchase.productSlug ) ||
-				( getPlan( purchase.productSlug ) &&
-					planHasFeature( purchase.productSlug, PRODUCT_JETPACK_SCAN ) )
-		);
-		if ( hasScan ) {
-			return next();
-		}
-		context.primary = <ScanUpsellPage />;
-		makeLayout( context, noop );
-		clientRender( context );
-	};
-
-	if ( purchases.length ) {
-		return showUpsellOrNext();
-	}
-	dispatch( fetchSitePurchases( siteId ) ).then( () => {
-		purchases = getSitePurchases( getState(), siteId );
-		showUpsellOrNext();
-	} );
+	context.primary = (
+		<UpsellSwitch
+			UpsellComponent={ ScanUpsellPage }
+			display={ context.primary }
+			getStateForSite={ getSiteScanState }
+			QueryComponent={ QueryJetpackScan }
+		>
+			<div className="scan__content">
+				<div className="scan__is-loading" />
+			</div>
+		</UpsellSwitch>
+	);
+	next();
 }
 
 export function scan( context, next ) {
