496aef4fa2d80466002547f02b759dbf73be807a
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2020-04-24 19:44:17 +0300

    Load lang revisions for logged out routes when using translation chunks

--- client/server/pages/index.js
+++ client/server/pages/index.js
@@ -391,7 +391,37 @@ function setUpLoggedOutRoute( req, res, next ) {
 		'X-Frame-Options': 'SAMEORIGIN',
 	} );
 
-	next();
+	const setupRequests = [];
+
+	if ( config.isEnabled( 'use-translation-chunks' ) ) {
+		const target = getBuildTargetFromRequest( req );
+		const rootPath = path.join( __dirname, '..', '..', '..' );
+		const langRevisionsPath = path.join(
+			rootPath,
+			'public',
+			target,
+			'languages',
+			'lang-revisions.json'
+		);
+		const langPromise = fs.promises
+			.readFile( langRevisionsPath, 'utf8' )
+			.then( ( languageRevisions ) => {
+				req.context.languageRevisions = filterLanguageRevisions( JSON.parse( languageRevisions ) );
+
+				return languageRevisions;
+			} )
+			.catch( ( error ) => {
+				console.error( 'Failed to read the language revision files.', error );
+
+				throw error;
+			} );
+
+		setupRequests.push( langPromise );
+	}
+
+	Promise.all( setupRequests )
+		.then( () => next() )
+		.catch( ( error ) => next( error ) );
 }
 
 function setUpLoggedInRoute( req, res, next ) {
@@ -416,7 +446,7 @@ function setUpLoggedInRoute( req, res, next ) {
 		const langPromise = fs.promises
 			.readFile( langRevisionsPath, 'utf8' )
 			.then( ( languageRevisions ) => {
-				req.context.languageRevisions = languageRevisions;
+				req.context.languageRevisions = filterLanguageRevisions( JSON.parse( languageRevisions ) );
 
 				return languageRevisions;
 			} )
