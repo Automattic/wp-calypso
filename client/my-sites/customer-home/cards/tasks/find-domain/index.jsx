3c9d74c8a3a4a4c2d0c180453968b02b6d76183a
Kirk Wight <kwight@kwight.ca>
2020-04-24 13:06:19 -0700

    Remove copy pasta query component.

--- client/my-sites/customer-home/cards/tasks/find-domain/index.jsx
+++ client/my-sites/customer-home/cards/tasks/find-domain/index.jsx
@@ -8,7 +8,6 @@ import { connect } from 'react-redux';
 /**
  * Internal dependencies
  */
-import QueryPublicizeConnections from 'components/data/query-publicize-connections';
 import { preventWidows } from 'lib/formatting';
 import { getSelectedSiteSlug } from 'state/ui/selectors';
 import Task from '../task';
@@ -17,22 +16,19 @@ const FindDomain = ( { siteSlug } ) => {
 	const translate = useTranslate();
 
 	return (
-		<>
-			<QueryPublicizeConnections selectedSite />
-			<Task
-				title={ translate( 'Look more professional' ) }
-				description={ preventWidows(
-					translate(
-						"Get a custom domain and show the world you're serious. Sites with custom domains look more professional and rank higher in search engine results."
-					)
-				) }
-				actionText={ translate( 'Find a domain' ) }
-				actionUrl={ `/domains/add/${ siteSlug }` }
-				illustration="/calypso/images/stats/tasks/social-links.svg"
-				timing={ 10 }
-				taskId="find-domain"
-			/>
-		</>
+		<Task
+			title={ translate( 'Look more professional' ) }
+			description={ preventWidows(
+				translate(
+					"Get a custom domain and show the world you're serious. Sites with custom domains look more professional and rank higher in search engine results."
+				)
+			) }
+			actionText={ translate( 'Find a domain' ) }
+			actionUrl={ `/domains/add/${ siteSlug }` }
+			illustration="/calypso/images/stats/tasks/social-links.svg"
+			timing={ 10 }
+			taskId="find-domain"
+		/>
 	);
 };
 
