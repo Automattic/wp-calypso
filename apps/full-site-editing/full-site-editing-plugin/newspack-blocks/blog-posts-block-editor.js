3783fc306e1fb8e05be0717b12b58974e97b0a99
retrofox <rdsuarez@gmail.com>
2020-04-28 14:46:40 -0300

    fse: add blog-posts-block sources files

---
+++ apps/full-site-editing/full-site-editing-plugin/newspack-blocks/blog-posts-block-editor.js
@@ -0,0 +1,40 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/**
+ * Internal dependencies
+ */
+import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+import { addFilter } from '@wordpress/hooks';
+/* eslint-enable import/no-extraneous-dependencies */
+
+/**
+ * NHA dependencies
+ */
+import { settings as blogPostsSettings } from './synced-newspack-blocks/blocks/homepage-articles/index';
+import { registerQueryStore } from './synced-newspack-blocks/blocks/homepage-articles/store';
+
+/**
+ * Block name in the A8C\FSE context.
+ */
+const blogPostsName = 'a8c/blog-posts';
+
+function setBlockTransformationName( name ) {
+	if ( name === 'newspack-blocks/homepage-articles' ) {
+		return blogPostsName;
+	}
+
+	return name;
+}
+
+addFilter(
+	'blocks.transforms_from_name',
+	'set-transformed-block-name',
+	setBlockTransformationName
+);
+
+registerBlockType( blogPostsName, {
+	...blogPostsSettings,
+	title: __( 'Blog Posts', 'full-site-editing' ),
+	category: 'layout'
+} );
+registerQueryStore( blogPostsName );
