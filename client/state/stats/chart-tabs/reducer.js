58b7d966763e336f5c89cf5a45fb636282d36d34
mmtr <miguel.torres@automattic.com>
2020-04-28 11:13:52 +0200

    Add comment explaining workaround

--- client/state/stats/chart-tabs/reducer.js
+++ client/state/stats/chart-tabs/reducer.js
@@ -25,6 +25,8 @@ export const counts = withSchemaValidation(
 		keyedReducer( 'period', ( state = [], action ) => {
 			switch ( action.type ) {
 				case STATS_CHART_COUNTS_RECEIVE: {
+					// Workaround to prevent new data from being appended to previous data when range period differs.
+					// See https://github.com/Automattic/wp-calypso/pull/41441#discussion_r415918092
 					if ( action.data.length !== state.length ) {
 						return action.data;
 					}
