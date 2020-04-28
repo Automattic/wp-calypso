8d8ebd4bcd723b87337bbbdf143a74949dcfcdd8
Renzo Canepa <rcanepag@gmail.com>
2020-04-28 16:16:51 -0300

    Add plain version of the ThreatItemHeader

--- client/landing/jetpack-cloud/components/threat-item-header/index.tsx
+++ client/landing/jetpack-cloud/components/threat-item-header/index.tsx
@@ -12,45 +12,75 @@ import { getThreatType } from 'landing/jetpack-cloud/components/threat-item/util
 
 interface Props {
 	threat: Threat;
+	// Set isStyled to false if you care about strings only
+	isStyled: boolean;
 }
 
 // This should be temporary since this data should be coming from the api
 // and not something that we should change to accommodate the results.
-const ThreatItemHeader = ( { threat }: Props ): i18nCalypso.TranslateResult => {
+const ThreatItemHeader = ( { threat, isStyled = true }: Props ) => {
 	const { filename, extension = { slug: 'unknown', version: 'n/a' } } = threat;
 
 	const basename = filename ? filename.replace( /.*\//, '' ) : '';
 
 	switch ( getThreatType( threat ) ) {
 		case 'core':
-			return translate( 'Infected core file: {{filename/}} ', {
-				components: {
-					filename: <code className="threat-item-header__alert-filename">{ basename }</code>,
-				},
-			} );
+			return isStyled
+				? translate( 'Infected core file: {{filename/}}', {
+						components: {
+							filename: <code className="threat-item-header__alert-filename">{ basename }</code>,
+						},
+				  } )
+				: translate( 'Infected core file: %s', {
+						args: [ basename ],
+				  } );
 
 		case 'file':
-			return translate( 'The file {{filename/}} contains a malicious code pattern.', {
-				components: {
-					filename: <code className="threat-item-header__alert-filename">{ basename }</code>,
-				},
-			} );
+			return isStyled
+				? translate( 'The file {{filename/}} contains a malicious code pattern', {
+						components: {
+							filename: <code className="threat-item-header__alert-filename">{ basename }</code>,
+						},
+				  } )
+				: translate( 'The file %s contains a malicious code pattern', {
+						args: [ basename ],
+				  } );
 
 		case 'plugin':
-			return translate( 'Vulnerable Plugin: {{pluginSlug/}} (version {{version/}})', {
-				components: {
-					pluginSlug: <span className="threat-item-header__alert-slug">{ extension.slug }</span>,
-					version: <span className="threat-item-header__alert-version">{ extension.version }</span>,
-				},
-			} );
+			return isStyled
+				? translate( 'Vulnerable Plugin: {{pluginSlug/}} (version {{version/}})', {
+						components: {
+							pluginSlug: (
+								<span className="threat-item-header__alert-slug">{ extension.slug }</span>
+							),
+							version: (
+								<span className="threat-item-header__alert-version">{ extension.version }</span>
+							),
+						},
+				  } )
+				: translate( 'Vulnerable Plugin: %(pluginSlug)s (version %(version)s)', {
+						args: {
+							pluginSlug: extension.slug,
+							version: extension.version,
+						},
+				  } );
 
 		case 'theme':
-			return translate( 'Vulnerable Theme {{themeSlug/}} (version {{version/}})', {
-				components: {
-					themeSlug: <span className="threat-item-header__alert-slug">{ extension.slug }</span>,
-					version: <span className="threat-item-header__alert-version">{ extension.version }</span>,
-				},
-			} );
+			return isStyled
+				? translate( 'Vulnerable Theme {{themeSlug/}} (version {{version/}})', {
+						components: {
+							themeSlug: <span className="threat-item-header__alert-slug">{ extension.slug }</span>,
+							version: (
+								<span className="threat-item-header__alert-version">{ extension.version }</span>
+							),
+						},
+				  } )
+				: translate( 'Vulnerable Theme %(themeSlug)s (version %(version)s)', {
+						args: {
+							themeSlug: extension.slug,
+							version: extension.version,
+						},
+				  } );
 
 		case 'database':
 			if ( ! threat.rows ) {
