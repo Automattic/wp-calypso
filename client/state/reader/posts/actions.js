Automattic/wp-calypso

commit 87a3ace24c73780900ae524f3426ebcb060c79ea

Split out the rest of lib/analytics (#41476)
This handles page-view, refer, and tracks.

client/state/reader/posts/actions.js
@@ -17,17 +17,17 @@ import { bumpStat } from 'lib/analytics/mc';
 
 import 'state/reader/init';
 
-// TODO: make underlying lib/analytics and reader/stats capable of existing in test code without mocks
+// TODO: make underlying lib/analytics/tracks and reader/stats capable of existing in test code without mocks
 // OR switch to analytics middleware
-let analytics = { tracks: { recordEvent: () => {} } };
+let tracks = { recordEvent: () => {} };
 let pageViewForPost = () => {};
 if ( process.env.NODE_ENV !== 'test' ) {
 	pageViewForPost = require( 'reader/stats' ).pageViewForPost;
-	analytics = require( 'lib/analytics' ).default;
+	tracks = require( 'lib/analytics/tracks' ).default;
 }
 
 function trackRailcarRender( post ) {
-	analytics.tracks.recordEvent( 'calypso_traintracks_render', post.railcar );
+	tracks.recordTracksEvent( 'calypso_traintracks_render', post.railcar );
 }
 
 function fetchForKey( postKey ) {

client/lib/invites/actions.js
@@ -12,7 +12,7 @@ import i18n from 'i18n-calypso';
 import Dispatcher from 'dispatcher';
 import wpcom from 'lib/wp';
 import { action as ActionTypes } from 'lib/invites/constants';
-import analytics from 'lib/analytics';
+import { recordTracksEvent } from 'lib/analytics/tracks';
 import { errorNotice, successNotice } from 'state/notices/actions';
 import { acceptedNotice } from 'my-sites/invites/utils';
 import { requestSites, receiveSites } from 'state/sites/actions';
@@ -41,7 +41,7 @@ export function fetchInvite( siteId, inviteKey ) {
 		} );
 
 		if ( error ) {
-			analytics.tracks.recordEvent( 'calypso_invite_validation_failure', {
+			recordTracksEvent( 'calypso_invite_validation_failure', {
 				error: error.error,
 			} );
 		}
@@ -62,11 +62,11 @@ export function createAccount( userData, invite, callback ) {
 						if ( error.message ) {
 							dispatch( errorNotice( error.message ) );
 						}
-						analytics.tracks.recordEvent( 'calypso_invite_account_creation_failed', {
+						recordTracksEvent( 'calypso_invite_account_creation_failed', {
 							error: error.error,
 						} );
 					} else {
-						analytics.tracks.recordEvent( 'calypso_invite_account_created' );
+						recordTracksEvent( 'calypso_invite_account_created' );
 					}
 					callback( error, bearerToken );
 				}
@@ -93,7 +93,7 @@ export function acceptInvite( invite, callback ) {
 				if ( error.message ) {
 					dispatch( errorNotice( error.message, { displayOnNextPage: true } ) );
 				}
-				analytics.tracks.recordEvent( 'calypso_invite_accept_failed', {
+				recordTracksEvent( 'calypso_invite_accept_failed', {
 					error: error.error,
 				} );
 			} else {
@@ -105,7 +105,7 @@ export function acceptInvite( invite, callback ) {
 					dispatch( successNotice( ...acceptedNotice( invite ) ) );
 				}
 
-				analytics.tracks.recordEvent( 'calypso_invite_accepted' );
+				recordTracksEvent( 'calypso_invite_accepted' );
 			}
 			dispatch( requestSites() );
 			if ( typeof callback === 'function' ) {
@@ -169,7 +169,7 @@ export function sendInvites( siteId, usernamesOrEmails, role, message, formId, i
 					}
 
 					dispatch( errorNotice( message ) );
-					analytics.tracks.recordEvent( 'calypso_invite_send_failed' );
+					recordTracksEvent( 'calypso_invite_send_failed' );
 				} else {
 					dispatch(
 						successNotice(
@@ -178,7 +178,7 @@ export function sendInvites( siteId, usernamesOrEmails, role, message, formId, i
 							} )
 						)
 					);
-					analytics.tracks.recordEvent( 'calypso_invite_send_success', { role } );
+					recordTracksEvent( 'calypso_invite_send_success', { role } );
 				}
 			} );
 	};
@@ -203,9 +203,9 @@ export function createInviteValidation( siteId, usernamesOrEmails, role ) {
 			data,
 		} );
 		if ( error ) {
-			analytics.tracks.recordEvent( 'calypso_invite_create_validation_failed' );
+			recordTracksEvent( 'calypso_invite_create_validation_failed' );
 		} else {
-			analytics.tracks.recordEvent( 'calypso_invite_create_validation_success' );
+			recordTracksEvent( 'calypso_invite_create_validation_success' );
 		}
 	} );
 }

client/lib/oauth-store/actions.js
@@ -3,7 +3,7 @@
  */
 import Dispatcher from 'dispatcher';
 import { actions, errors as errorTypes } from './constants';
-import analytics from 'lib/analytics';
+import { recordTracksEvent } from 'lib/analytics/tracks';
 import { bumpStat } from 'lib/analytics/mc';
 
 async function makeRequest( username, password, authCode = '' ) {
@@ -58,10 +58,10 @@ function bumpStats( error, data ) {
 	}
 
 	if ( errorType === errorTypes.ERROR_REQUIRES_2FA ) {
-		analytics.tracks.recordEvent( 'calypso_oauth_login_needs2fa' );
+		recordTracksEvent( 'calypso_oauth_login_needs2fa' );
 		bumpStat( 'calypso_oauth_login', 'success-needs-2fa' );
 	} else if ( errorType ) {
-		analytics.tracks.recordEvent( 'calypso_oauth_login_fail', {
+		recordTracksEvent( 'calypso_oauth_login_fail', {
 			error: error.error,
 		} );
 
@@ -70,7 +70,7 @@ function bumpStats( error, data ) {
 			calypso_oauth_login: 'error',
 		} );
 	} else {
-		analytics.tracks.recordEvent( 'calypso_oauth_login_success' );
+		recordTracksEvent( 'calypso_oauth_login_success' );
 		bumpStat( 'calypso_oauth_login', 'success' );
 	}
 }

client/lib/plugins/actions.js
@@ -7,7 +7,7 @@ import { defer } from 'lodash';
 /**
  * Internal dependencies
  */
-import analytics from 'lib/analytics';
+import { recordTracksEvent } from 'lib/analytics/tracks';
 import { bumpStat } from 'lib/analytics/mc';
 import Dispatcher from 'dispatcher';
 import { userCan } from 'lib/site/utils';
@@ -111,7 +111,7 @@ const getPluginBoundMethod = ( site, pluginId, method ) => {
 
 const recordEvent = ( eventType, plugin, site, error ) => {
 	if ( error ) {
-		analytics.tracks.recordEvent( eventType + '_error', {
+		recordTracksEvent( eventType + '_error', {
 			site: site.ID,
 			plugin: plugin.slug,
 			error: error.error,
@@ -119,7 +119,7 @@ const recordEvent = ( eventType, plugin, site, error ) => {
 		bumpStat( eventType, 'failed' );
 		return;
 	}
-	analytics.tracks.recordEvent( eventType + '_success', {
+	recordTracksEvent( eventType + '_success', {
 		site: site.ID,
 		plugin: plugin.slug,
 	} );
@@ -368,7 +368,7 @@ const PluginsActions = {
 				( ! ( data && data.active ) && ! error )
 			) {
 				bumpStat( 'calypso_plugin_activated', 'failed' );
-				analytics.tracks.recordEvent( 'calypso_plugin_activated_error', {
+				recordTracksEvent( 'calypso_plugin_activated_error', {
 					error: error && error.error ? error.error : 'Undefined activation error',
 					site: site.ID,
 					plugin: plugin.slug,
@@ -378,7 +378,7 @@ const PluginsActions = {
 			}
 
 			bumpStat( 'calypso_plugin_activated', 'succeeded' );
-			analytics.tracks.recordEvent( 'calypso_plugin_activated_success', {
+			recordTracksEvent( 'calypso_plugin_activated_success', {
 				site: site.ID,
 				plugin: plugin.slug,
 			} );
@@ -413,7 +413,7 @@ const PluginsActions = {
 			// Activation error is ok, because it means the plugin is already active
 			if ( error && error.error !== 'deactivation_error' ) {
 				bumpStat( 'calypso_plugin_deactivated', 'failed' );
-				analytics.tracks.recordEvent( 'calypso_plugin_deactivated_error', {
+				recordTracksEvent( 'calypso_plugin_deactivated_error', {
 					error: error.error ? error.error : 'Undefined deactivation error',
 					site: site.ID,
 					plugin: plugin.slug,
@@ -422,7 +422,7 @@ const PluginsActions = {
 				return;
 			}
 			bumpStat( 'calypso_plugin_deactivated', 'succeeded' );
-			analytics.tracks.recordEvent( 'calypso_plugin_deactivated_success', {
+			recordTracksEvent( 'calypso_plugin_deactivated_success', {
 				site: site.ID,
 				plugin: plugin.slug,
 			} );

client/lib/plugins/test/actions.js
@@ -15,10 +15,9 @@ import siteData from './fixtures/site';
 import mockedWpcom from './mocks/wpcom';
 import actions from 'lib/plugins/actions';
 
-jest.mock( 'lib/analytics', () => ( {} ) );
 jest.mock( 'lib/wp', () => require( './mocks/wpcom' ) );
-jest.mock( 'lib/analytics', () => ( {
-	tracks: { recordEvent: () => {} },
+jest.mock( 'lib/analytics/tracks', () => ( {
+	recordTracksEvent: () => {},
 } ) );
 
 describe( 'WPcom Data Actions', () => {

client/state/nps-survey/actions.js
@@ -7,7 +7,7 @@ import wpcom from 'lib/wp';
 /**
  * Internal dependencies
  */
-import analytics from 'lib/analytics';
+import { recordTracksEvent } from 'lib/analytics/tracks';
 import { bumpStat } from 'lib/analytics/mc';
 import {
 	NPS_SURVEY_SET_ELIGIBILITY,
@@ -72,7 +72,7 @@ export function submitNpsSurvey( surveyName, score ) {
 		dispatch( submitNpsSurveyRequesting( surveyName, score ) );
 
 		bumpStat( 'calypso_nps_survey', 'survey_submitted' );
-		analytics.tracks.recordEvent( 'calypso_nps_survey_submitted' );
+		recordTracksEvent( 'calypso_nps_survey_submitted' );
 
 		return wpcom
 			.undocumented()
@@ -94,7 +94,7 @@ export function submitNpsSurveyWithNoScore( surveyName ) {
 		dispatch( submitNpsSurveyWithNoScoreRequesting( surveyName ) );
 
 		bumpStat( 'calypso_nps_survey', 'survey_dismissed' );
-		analytics.tracks.recordEvent( 'calypso_nps_survey_dismissed' );
+		recordTracksEvent( 'calypso_nps_survey_dismissed' );
 
 		return wpcom
 			.undocumented()
@@ -116,7 +116,7 @@ export function sendNpsSurveyFeedback( surveyName, feedback ) {
 		dispatch( sendNpsSurveyFeedbackRequesting( surveyName, feedback ) );
 
 		bumpStat( 'calypso_nps_survey', 'feedback_submitted' );
-		analytics.tracks.recordEvent( 'calypso_nps_survey_feedback_submitted' );
+		recordTracksEvent( 'calypso_nps_survey_feedback_submitted' );
 
 		return wpcom
 			.undocumented()

client/state/nps-survey/test/actions.js
@@ -1,7 +1,7 @@
 /**
  * Internal dependencies
  */
-import analytics from 'lib/analytics';
+import { recordTracksEvent } from 'lib/analytics/tracks';
 import { bumpStat } from 'lib/analytics/mc';
 import {
 	submitNpsSurvey,
@@ -26,8 +26,8 @@ jest.mock( 'lib/wp', () => ( {
 		sendNPSSurveyFeedback: jest.fn().mockReturnValue( Promise.resolve() ),
 	} ),
 } ) );
-jest.mock( 'lib/analytics', () => ( {
-	tracks: { recordEvent: jest.fn() },
+jest.mock( 'lib/analytics/tracks', () => ( {
+	recordTracksEvent: jest.fn(),
 } ) );
 jest.mock( 'lib/analytics/mc', () => ( {
 	bumpStat: jest.fn(),
@@ -39,67 +39,63 @@ describe( 'actions', () => {
 	describe( '#submitNpsSurvey()', () => {
 		beforeEach( () => {
 			bumpStat.mockClear();
-			analytics.tracks.recordEvent.mockClear();
+			recordTracksEvent.mockClear();
 			dispatch.mockClear();
 		} );
 
 		test( 'should track the successful survey submission', async () => {
 			expect( bumpStat.mock.calls ).toHaveLength( 0 );
-			expect( analytics.tracks.recordEvent.mock.calls ).toHaveLength( 0 );
+			expect( recordTracksEvent.mock.calls ).toHaveLength( 0 );
 
 			await submitNpsSurvey( 'nps_test', 10 )( dispatch );
 
 			expect( bumpStat.mock.calls ).toHaveLength( 1 );
 			expect( bumpStat.mock.calls[ 0 ] ).toEqual( [ 'calypso_nps_survey', 'survey_submitted' ] );
 
-			expect( analytics.tracks.recordEvent.mock.calls ).toHaveLength( 1 );
-			expect( analytics.tracks.recordEvent.mock.calls[ 0 ] ).toEqual( [
-				'calypso_nps_survey_submitted',
-			] );
+			expect( recordTracksEvent.mock.calls ).toHaveLength( 1 );
+			expect( recordTracksEvent.mock.calls[ 0 ] ).toEqual( [ 'calypso_nps_survey_submitted' ] );
 		} );
 	} );
 
 	describe( '#submitNpsSurveyWithNoScore', () => {
 		beforeEach( () => {
 			bumpStat.mockClear();
-			analytics.tracks.recordEvent.mockClear();
+			recordTracksEvent.mockClear();
 			dispatch.mockClear();
 		} );
 
 		test( 'should track the successful survey dismissal', async () => {
 			expect( bumpStat.mock.calls ).toHaveLength( 0 );
-			expect( analytics.tracks.recordEvent.mock.calls ).toHaveLength( 0 );
+			expect( recordTracksEvent.mock.calls ).toHaveLength( 0 );
 
 			await submitNpsSurveyWithNoScore( 'nps_test' )( dispatch );
 
 			expect( bumpStat.mock.calls ).toHaveLength( 1 );
 			expect( bumpStat.mock.calls[ 0 ] ).toEqual( [ 'calypso_nps_survey', 'survey_dismissed' ] );
 
-			expect( analytics.tracks.recordEvent.mock.calls ).toHaveLength( 1 );
-			expect( analytics.tracks.recordEvent.mock.calls[ 0 ] ).toEqual( [
-				'calypso_nps_survey_dismissed',
-			] );
+			expect( recordTracksEvent.mock.calls ).toHaveLength( 1 );
+			expect( recordTracksEvent.mock.calls[ 0 ] ).toEqual( [ 'calypso_nps_survey_dismissed' ] );
 		} );
 	} );
 
 	describe( '#sendNpsSurveyFeedback', () => {
 		beforeEach( () => {
 			bumpStat.mockClear();
-			analytics.tracks.recordEvent.mockClear();
+			recordTracksEvent.mockClear();
 			dispatch.mockClear();
 		} );
 
 		test( 'should track the successful feedback submission', async () => {
 			expect( bumpStat.mock.calls ).toHaveLength( 0 );
-			expect( analytics.tracks.recordEvent.mock.calls ).toHaveLength( 0 );
+			expect( recordTracksEvent.mock.calls ).toHaveLength( 0 );
 
 			await sendNpsSurveyFeedback( 'nps_test', 'dummy feedback data' )( dispatch );
 
 			expect( bumpStat.mock.calls ).toHaveLength( 1 );
 			expect( bumpStat.mock.calls[ 0 ] ).toEqual( [ 'calypso_nps_survey', 'feedback_submitted' ] );
 
-			expect( analytics.tracks.recordEvent.mock.calls ).toHaveLength( 1 );
-			expect( analytics.tracks.recordEvent.mock.calls[ 0 ] ).toEqual( [
+			expect( recordTracksEvent.mock.calls ).toHaveLength( 1 );
+			expect( recordTracksEvent.mock.calls[ 0 ] ).toEqual( [
 				'calypso_nps_survey_feedback_submitted',
 			] );
 		} );

client/state/reader/posts/test/actions.js
@@ -2,7 +2,7 @@
  * Internal dependencies
  */
 import * as actions from '../actions';
-import { tracks } from 'lib/analytics';
+import * as tracks from 'lib/analytics/tracks';
 import { bumpStat } from 'lib/analytics/mc';
 
 import { READER_POSTS_RECEIVE, READER_POST_SEEN } from 'state/reader/action-types';
@@ -10,8 +10,8 @@ import wp from 'lib/wp';
 
 jest.mock( 'reader/stats', () => ( { pageViewForPost: jest.fn() } ) );
 
-jest.mock( 'lib/analytics', () => ( {
-	tracks: { recordEvent: jest.fn() },
+jest.mock( 'lib/analytics/tracks', () => ( {
+	recordTracksEvent: jest.fn(),
 } ) );
 
 jest.mock( 'lib/analytics/mc', () => ( {
@@ -35,7 +35,7 @@ const { pageViewForPost } = require( 'reader/stats' );
 
 describe( 'actions', () => {
 	const dispatchSpy = jest.fn();
-	const trackingSpy = tracks.recordEvent;
+	const trackingSpy = tracks.recordTracksEvent;
 	const readFeedStub = undocumented().readFeedPost;
 	const readSiteStub = undocumented().readSitePost;
 

commit 61417ef0e95de61d400d7d7d7c3928de4005ef87

Fix bug in reader tracking
Introduced by #41476

client/state/reader/posts/actions.js
@@ -23,7 +23,7 @@ let tracks = { recordEvent: () => {} };
 let pageViewForPost = () => {};
 if ( process.env.NODE_ENV !== 'test' ) {
 	pageViewForPost = require( 'reader/stats' ).pageViewForPost;
-	tracks = require( 'lib/analytics/tracks' ).default;
+	tracks = require( 'lib/analytics/tracks' );
 }
 
 function trackRailcarRender( post ) {