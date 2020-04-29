5ba8eea5f815c2636bdbd3535baa8dea554d50e9
Kirk Wight <kwight@kwight.ca>
2020-04-29 13:02:19 -0700

    Add ability to display specific content for completed tasks.

--- client/my-sites/customer-home/cards/tasks/site-setup-list/index.jsx
+++ client/my-sites/customer-home/cards/tasks/site-setup-list/index.jsx
@@ -108,52 +108,77 @@ const SiteSetupList = ( { menusUrl, siteId, siteSlug, tasks, taskUrls } ) => {
 		<Card className="site-setup-list">
 			<div className="site-setup-list__nav">
 				<CardHeading>{ translate( 'Site setup' ) }</CardHeading>
-				{ tasks.map( ( task ) => (
-					<NavItem
-						key={ task.id }
-						text={ getTask( task ).title }
-						isCompleted={ task.isCompleted }
-						isCurrent={ task.id === currentTask.id }
-						onClick={ () => setCurrentTask( getTask( task ) ) }
-					/>
-				) ) }
+				{ tasks.map( ( task ) => {
+					const taskData = getTask( task );
+					return (
+						<NavItem
+							key={ task.id }
+							text={ task.isCompleted ? taskData.completed.title : taskData.todo.title }
+							isCompleted={ task.isCompleted }
+							isCurrent={ task.id === currentTask.id }
+							onClick={ () => setCurrentTask( taskData ) }
+						/>
+					);
+				} ) }
 			</div>
-			<ActionPanel className="site-setup-list__task task">
-				<ActionPanelBody>
-					<div className="site-setup-list__task-text task__text">
-						<div className="site-setup-list__task-timing task__timing">
-							<Gridicon icon="time" size={ 18 } />
-							<p>
-								{ translate( '%d minute', '%d minutes', {
-									count: currentTask.timing,
-									args: [ currentTask.timing ],
-								} ) }
+			{ ! currentTask.isCompleted ? (
+				<ActionPanel className="site-setup-list__task task">
+					<ActionPanelBody>
+						<div className="site-setup-list__task-text task__text">
+							<div className="site-setup-list__task-timing task__timing">
+								<Gridicon icon="time" size={ 18 } />
+								<p>
+									{ translate( '%d minute', '%d minutes', {
+										count: currentTask.todo.timing,
+										args: [ currentTask.todo.timing ],
+									} ) }
+								</p>
+							</div>
+							<ActionPanelTitle>{ currentTask.todo.title }</ActionPanelTitle>
+							<p className="site-setup-list__task-description task__description">
+								{ currentTask.todo.description }
 							</p>
-						</div>
-						<ActionPanelTitle>{ currentTask.title }</ActionPanelTitle>
-						<p className="site-setup-list__task-description task__description">
-							{ currentTask.description }
-						</p>
-						<ActionPanelCta>
-							<Button
-								className="site-setup-list__task-action task__action"
-								primary
-								onClick={ () => startTask( dispatch, currentTask, siteId ) }
-							>
-								{ currentTask.actionText }
-							</Button>
-							{ currentTask.isSkippable && ! currentTask.isCompleted && (
+							<ActionPanelCta>
 								<Button
-									className="site-setup-list__task-skip task__skip is-link"
-									onClick={ () => skipTask( dispatch, currentTask, siteId ) }
+									className="site-setup-list__task-action task__action"
+									primary
+									onClick={ () => startTask( dispatch, currentTask, siteId ) }
 								>
-									{ translate( 'Skip for now' ) }
+									{ currentTask.todo.actionText }
 								</Button>
-							) }
-						</ActionPanelCta>
-					</div>
-				</ActionPanelBody>
-			</ActionPanel>
+								{ currentTask.todo.isSkippable && (
+									<Button
+										className="site-setup-list__task-skip task__skip is-link"
+										onClick={ () => skipTask( dispatch, currentTask.todo, siteId ) }
+									>
+										{ translate( 'Skip for now' ) }
+									</Button>
+								) }
+							</ActionPanelCta>
+						</div>
+					</ActionPanelBody>
+				</ActionPanel>
+			) : (
+				<ActionPanel className="site-setup-list__task task">
+					<ActionPanelBody>
+						<div className="site-setup-list__task-text task__text">
+							<ActionPanelTitle>{ currentTask.completed.title }</ActionPanelTitle>
+							<p className="site-setup-list__task-description task__description">
+								{ currentTask.completed.description }
+							</p>
+							<ActionPanelCta>
+								<Button
+									className="site-setup-list__task-action task__action"
+									primary
+									onClick={ () => startTask( dispatch, currentTask, siteId ) }
+								>
+									{ currentTask.completed.actionText }
+								</Button>
+							</ActionPanelCta>
+						</div>
+					</ActionPanelBody>
+				</ActionPanel>
+			) }
 		</Card>
 	);
 };
