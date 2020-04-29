3d84a77678fa5c8f6ec5a53a979f079a59014b53
sixhours <calobee@gmail.com>
2020-04-29 11:57:51 -0400

    Adds a horizontal prop and example in devdocs to match Banner.

--- client/blocks/upsell-nudge/docs/example.jsx
+++ client/blocks/upsell-nudge/docs/example.jsx
@@ -27,12 +27,31 @@ const UpsellNudgeExample = () => (
 		/>
 		<UpsellNudge
 			description="Domain registration is free for a year with purchase of a Premium or Business plan."
+			forceDisplay
+			href="#"
+			callToAction="Upgrade"
+			title="Free domain with a plan! This is a regular nudge with a description."
+			showIcon={ true }
+		/>
+		<UpsellNudge
+			description="Domain registration is free for a year with purchase of a Premium or Business plan."
 			dismissPreferenceName="calypso_upsell_nudge_devdocs_dismiss"
 			forceDisplay
 			href="#"
 			callToAction="Upgrade"
 			title="Free domain with a plan! This is a dismissible nudge with a description."
 			showIcon={ true }
+			horizontal
+		/>
+		<UpsellNudge
+			description="Domain registration is free for a year with purchase of a Premium or Business plan."
+			dismissPreferenceName="calypso_upsell_nudge_devdocs_dismiss"
+			forceDisplay
+			href="#"
+			callToAction="Upgrade"
+			title="Free domain with a plan! This is a dismissible nudge with a description."
+			showIcon={ true }
+			price={ [ 48, 50 ] }
 		/>
 		<UpsellNudge
 			forceDisplay
