bff2efe42e424ee9760cabb8f643124da3f72a26
Caleb Bauermeister <cbauerman@gmail.com>
2020-04-28 17:06:38 -0700

    working

--- client/landing/jetpack-cloud/components/filter-bar/date-range-selector/index.tsx
+++ client/landing/jetpack-cloud/components/filter-bar/date-range-selector/index.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { useTranslate } from 'i18n-calypso';
-import React, { FunctionComponent, RefObject } from 'react';
+import React, { FunctionComponent, RefObject, useState } from 'react';
 
 /**
  * Internal dependencies
@@ -22,24 +22,38 @@ interface Props {
 	onClose: () => void;
 }
 
-const ActivityTypeSelector: FunctionComponent< Props > = ( { context, isVisible, onClose } ) => {
+const DATE_FORMAT = 'MM/DD/YYYY';
+
+const DateRangeSelector: FunctionComponent< Props > = ( { context, isVisible, onClose } ) => {
 	const translate = useTranslate();
+
+	const [ firstDate, setFirstDate ] = useState( null );
+	const [ secondDate, setSecondDate ] = useState( null );
+
+	const renderInstructions = () => {
+		return firstDate === null
+			? translate( 'Please select the first date.' )
+			: translate( 'Please select the second date' );
+	};
 
 	return (
 		<Popover
+			className="date-range-selector__popover"
 			context={ context.current }
 			isVisible={ isVisible }
 			onClose={ onClose }
 			position="bottom"
 		>
-			<div>
-				<DatePicker />
+			<div className="date-range-selector__controls">
+				<h4>{ renderInstructions() }</h4>
+				<Button compact>{ translate( 'Cancel' ) }</Button>
 				<Button primary compact>
 					{ translate( 'Apply' ) }
 				</Button>
 			</div>
+			<DatePicker />
 		</Popover>
 	);
 };
 
-export default ActivityTypeSelector;
+export default DateRangeSelector;
