24a1e1f58807c5bc2871b49f4895fed13aff119f
James Tien <southp0105@gmail.com>
2020-04-30 21:09:56 +0800

    Add new constants for `currentUser` state tree: MARKETING_PRICE_GROUP_ series, which is in preparing for the new `marketingPriceGroup` substate.

--- client/state/current-user/constants.js
+++ client/state/current-user/constants.js
@@ -1,2 +1,6 @@
 export const DOMAINS_WITH_PLANS_ONLY = 'calypso_domains_with_plans_only';
 export const NON_PRIMARY_DOMAINS_TO_FREE_USERS = 'calypso_allow_nonprimary_domains_without_plan';
+
+export const MARKETING_PRICE_GROUP_2020_Q2_TEST_1 = 'marketing_price_group_2020_q2_test_1';
+export const MARKETING_PRICE_GROUP_2020_Q2_TEST_2 = 'marketing_price_group_2020_q2_test_2';
+export const MARKETING_PRICE_GROUP_2020_Q2_TEST_3 = 'marketing_price_group_2020_q2_test_3';
