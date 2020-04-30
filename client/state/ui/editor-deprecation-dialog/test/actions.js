498076c4d44f1a78f034868de664ec046792a53d
Gary <gary@pento.net>
2020-04-30 16:42:41 +1000

    Delete a test that should've been deleted in 179fd18387.

--- client/state/ui/editor-deprecation-dialog/test/actions.js
+++ client/state/ui/editor-deprecation-dialog/test/actions.js
@@ -1,21 +1,10 @@
 /**
  * Internal dependencies
  */
-import { showEditorDeprecationDialog, hideEditorDeprecationDialog } from '../actions';
+import { hideEditorDeprecationDialog } from '../actions';
 import { EDITOR_DEPRECATION_DIALOG_IS_SHOWING } from 'state/action-types';
 
 describe( 'actions', () => {
-	describe( 'showEditorDeprecationDialog()', () => {
-		test( 'should return an action object', () => {
-			const action = showEditorDeprecationDialog();
-
-			expect( action ).toEqual( {
-				type: EDITOR_DEPRECATION_DIALOG_IS_SHOWING,
-				isShowing: true,
-			} );
-		} );
-	} );
-
 	describe( 'hideEditorDeprecationDialog()', () => {
 		test( 'should return an action object', () => {
 			const action = hideEditorDeprecationDialog();
