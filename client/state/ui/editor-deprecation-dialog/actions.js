964544937c6bffaa599cfccdb01c69b4c0b48779
Gary <gary@pento.net>
2020-04-30 16:04:06 +1000

    Remove an unnecessary action to show the dialog.

--- client/state/ui/editor-deprecation-dialog/actions.js
+++ client/state/ui/editor-deprecation-dialog/actions.js
@@ -3,13 +3,6 @@
  */
 import { EDITOR_DEPRECATION_DIALOG_IS_SHOWING } from 'state/action-types';
 
-export function showEditorDeprecationDialog() {
-	return {
-		type: EDITOR_DEPRECATION_DIALOG_IS_SHOWING,
-		isShowing: true,
-	};
-}
-
 export function hideEditorDeprecationDialog() {
 	return {
 		type: EDITOR_DEPRECATION_DIALOG_IS_SHOWING,
