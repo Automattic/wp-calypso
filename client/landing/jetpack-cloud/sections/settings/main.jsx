a2c4fa95002367908788d05e4433e6e5574a697c
clemente <javacodia@gmail.com>
2020-05-01 16:57:50 +0100

    Fix the styles issues
    - Initializing block with pulse effect
    - Use flex to align status block and the icon
    - Use the correct icon
    - Updated the font size

--- client/landing/jetpack-cloud/sections/settings/main.jsx
+++ client/landing/jetpack-cloud/sections/settings/main.jsx
@@ -14,7 +14,6 @@ import RewindCredentialsForm from 'components/rewind-credentials-form';
 import { Card } from '@automattic/components';
 import getRewindState from 'state/selectors/get-rewind-state';
 import QueryRewindState from 'components/data/query-rewind-state';
-import Gridicon from 'components/gridicon';
 import Main from 'components/main';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
@@ -23,12 +22,41 @@ import PageViewTracker from 'lib/analytics/page-view-tracker';
  * Style dependencies
  */
 import './style.scss';
+import connectedIcon from './images/server-connected.svg';
+import disconnectedIcon from './images/server-disconnected.svg';
 
 class SettingsPage extends Component {
+	renderServerConnectionStatus() {
+		const { translate } = this.props;
+
+		const isConnected = false; // ( 'active' === rewind.state );
+
+		const iconPath = isConnected ? connectedIcon : disconnectedIcon;
+		const myClassName = isConnected ? 'settings__connected' : 'settings__disconnected';
+		const title = isConnected
+			? translate( 'Server status: Connected' )
+			: translate( 'Server status: Not connected' );
+		const content = isConnected
+			? translate( 'One-click restores are enabled.' )
+			: translate(
+					'Enter your server credentials to enable one-click restores for Backups. Find your server credentials.'
+			  );
+
+		return (
+			<Card compact={ true } className={ myClassName }>
+				<img className="settings__icon" src={ iconPath } alt="" />
+				<div className="settings__details">
+					<div className="settings__details-head"> { title } </div>
+					<div>{ content }</div>
+				</div>
+			</Card>
+		);
+	}
+
 	render() {
 		const { rewind, siteId, translate } = this.props;
 
-		const isConnected = 'active' === rewind.state;
+		const isInitialized = ! ( 'uninitialized' === rewind.state );
 
 		return (
 			<Main className="settings">
@@ -36,33 +64,14 @@ class SettingsPage extends Component {
 				<SidebarNavigation />
 				<QueryRewindState siteId={ siteId } />
 				<PageViewTracker path="/settings/:site" title="Settings" />
-				<div className="settings__page-title">{ translate( 'Server connection details' ) }</div>
-				{ isConnected && (
-					<Card compact={ true } className="settings__connected">
-						<Gridicon icon="checkmark-circle" />
-						<div className="settings__details">
-							<div className="settings__details-head">
-								{ translate( 'Server status: Connected' ) }
-							</div>
-							<div>{ translate( 'One-click restores are enabled.' ) }</div>
-						</div>
-					</Card>
-				) }
-				{ ! isConnected && (
-					<Card compact={ true } className="settings__disconnected">
-						<Gridicon icon="cross-circle" />
-						<div className="settings__details">
-							<div className="settings__details-head">
-								{ translate( 'Server status: Not connected' ) }
-							</div>
-							<div>
-								{ translate(
-									'Enter your server credentials to enable one-click restores for Backups. Find your server credentials.'
-								) }
-							</div>
-						</div>
-					</Card>
-				) }
+				<div className="settings__title">
+					<h2>{ translate( 'Server connection details' ) }</h2>
+				</div>
+
+				{ isInitialized && this.renderServerConnectionStatus() }
+
+				{ ! isInitialized && <div className="settings__is-uninitialized" /> }
+
 				<RewindCredentialsForm
 					{ ...{
 						allowCancel: false,
