aa1adbb7f9b3e1ecbf4c20faae0820f64bc449e2
Jon Surrell <jon.surrell@automattic.com>
2020-05-04 16:29:42 +0200

    Import calypso styles

--- apps/full-site-editing/full-site-editing-plugin/dotcom-fse/editor/domain-panel/index.tsx
+++ apps/full-site-editing/full-site-editing-plugin/dotcom-fse/editor/domain-panel/index.tsx
@@ -13,6 +13,13 @@ import DomainPicker from './domain-picker';
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import { Icon, globe } from '@wordpress/icons';
+
+import '../../../../../../client/landing/gutenboarding/components/domain-picker/style.scss';
+import '../../../../../../client/landing/gutenboarding/components/domain-picker-button/style.scss';
+import '../../../../../../client/landing/gutenboarding/components/domain-picker-modal/style.scss';
+import '../../../../../../client/landing/gutenboarding/components/domain-picker-popover/style.scss';
+import '../../../../../../client/landing/gutenboarding/components/domain-categories/style.scss';
+
 const ICON = <Icon icon={ globe } />;
 const PLUGIN_SIDEBAR_NAME = 'a8c-domain-sidebar';
 
