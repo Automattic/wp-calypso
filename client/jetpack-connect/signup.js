4d1079fc08e0e19e7abfd3ffad80eb1c5f6a249b
Daniel Rey López <daniel.rey.lopez@gmail.com>
2020-05-04 18:35:39 +0100

    Implement the signup/login Woo DNA flow

--- client/jetpack-connect/signup.js
+++ client/jetpack-connect/signup.js
@@ -13,7 +13,7 @@ import debugFactory from 'debug';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { flowRight, get, noop } from 'lodash';
+import { flowRight, get, includes, noop } from 'lodash';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -40,6 +40,17 @@ import {
 	createAccount as createAccountAction,
 	createSocialAccount as createSocialAccountAction,
 } from 'state/jetpack-connect/actions';
+import LoginBlock from 'blocks/login';
+import Gridicon from 'components/gridicon';
+import { decodeEntities } from 'lib/formatting';
+import {
+	getRequestError,
+	getLastCheckedUsernameOrEmail,
+	getAuthAccountType,
+} from 'state/login/selectors';
+import { resetAuthAccountType as resetAuthAccountTypeAction } from 'state/login/actions';
+import FormattedHeader from 'components/formatted-header';
+import wooDnaConfig from './woo-dna-config';
 
 const debug = debugFactory( 'calypso:jetpack-connect:authorize-form' );
 
@@ -59,6 +70,7 @@ export class JetpackSignup extends Component {
 		isCreatingAccount: false,
 		newUsername: null,
 		bearerToken: null,
+		showWooDnaLoginForm: true,
 	} );
 
 	state = this.constructor.initialState;
@@ -83,9 +95,45 @@ export class JetpackSignup extends Component {
 		} );
 	}
 
+	componentDidUpdate( prevProps ) {
+		const { requestError } = this.props;
+
+		if ( prevProps.requestError || ! requestError ) {
+			return;
+		}
+
+		if (
+			this.getWooDnaConfig() &&
+			'usernameOrEmail' === requestError.field &&
+			'unknown_user' === requestError.code
+		) {
+			this.showWooDnaSignupView();
+		}
+	}
+
+	showWooDnaSignupView = () => {
+		this.setState( {
+			showWooDnaLoginForm: false,
+		} );
+		this.props.resetAuthAccountType();
+	};
+
+	showWooDnaLoginView = ( usernameOrEmail ) => {
+		this.setState( {
+			showWooDnaLoginForm: true,
+			signUpUsernameOrEmail: usernameOrEmail || null,
+		} );
+		this.props.resetAuthAccountType();
+	};
+
 	isWoo() {
 		const { authQuery } = this.props;
 		return 'woocommerce-onboarding' === authQuery.from;
+	}
+
+	getWooDnaConfig() {
+		const { authQuery } = this.props;
+		return wooDnaConfig[ authQuery.from ];
 	}
 
 	getLoginRoute() {
@@ -212,7 +260,107 @@ export class JetpackSignup extends Component {
 			</LoggedOutFormLinks>
 		);
 	}
+
+	renderWooDna() {
+		const { authQuery, isFullLoginFormVisible, translate, usernameOrEmail } = this.props;
+		const { isCreatingAccount, signUpUsernameOrEmail } = this.state;
+		let header, subHeader, content;
+		const footerLinks = [];
+		const email = signUpUsernameOrEmail || usernameOrEmail || authQuery.userEmail;
+		const wooDna = this.getWooDnaConfig();
+
+		if ( this.state.showWooDnaLoginForm ) {
+			if ( isFullLoginFormVisible ) {
+				header = translate( 'Log in to your WordPress.com account' );
+				/* translators: pluginName is the name of the Woo extension that initiated the connection flow */
+				subHeader = translate(
+					'Your account will enable you to start using the features and benefits offered by %(pluginName)s',
+					{
+						args: {
+							pluginName: wooDna.name( translate ),
+						},
+					}
+				);
+				footerLinks.push(
+					<LoggedOutFormLinkItem key="signup" onClick={ this.showWooDnaSignupView }>
+						{ this.props.translate( 'Create a new account' ) }
+					</LoggedOutFormLinkItem>
+				);
+				footerLinks.push(
+					<LoggedOutFormLinkItem
+						key="lostpassword"
+						href={ addQueryArgs(
+							{ action: 'lostpassword' },
+							login( { locale: this.props.locale } )
+						) }
+					>
+						{ this.props.translate( 'Lost your password?' ) }
+					</LoggedOutFormLinkItem>
+				);
+			} else {
+				header = wooDna.name( translate );
+				subHeader = translate( 'Enter your email address to get started' );
+			}
+		} else {
+			header = wooDna.name( translate );
+			subHeader = translate( 'Create an account' );
+			footerLinks.push(
+				<LoggedOutFormLinkItem key="login" onClick={ () => this.showWooDnaLoginView() }>
+					{ this.props.translate( 'Log in with an existing WordPress.com account' ) }
+				</LoggedOutFormLinkItem>
+			);
+		}
+
+		footerLinks.push(
+			<LoggedOutFormLinkItem key="back" href={ authQuery.redirectAfterAuth }>
+				<Gridicon size={ 18 } icon="arrow-left" />{ ' ' }
+				{
+					// translators: eg: Return to The WordPress.com Blog
+					this.props.translate( 'Return to %(sitename)s', {
+						args: { sitename: decodeEntities( authQuery.blogname ) },
+					} )
+				}
+			</LoggedOutFormLinkItem>
+		);
+		const footer = <LoggedOutFormLinks>{ footerLinks }</LoggedOutFormLinks>;
+
+		if ( this.state.showWooDnaLoginForm ) {
+			content = <LoginBlock locale={ this.props.locale } footer={ footer } userEmail={ email } />;
+		} else {
+			content = (
+				<SignupForm
+					disabled={ isCreatingAccount }
+					email={ includes( email, '@' ) ? email : '' }
+					footerLink={ footer }
+					handleLogin={ this.showWooDnaLoginView }
+					handleSocialResponse={ this.handleSocialResponse }
+					isSocialSignupEnabled={ isEnabled( 'signup/social' ) }
+					locale={ this.props.locale }
+					redirectToAfterLoginUrl={ addQueryArgs( { auth_approved: true }, window.location.href ) }
+					submitButtonText={ this.props.translate( 'Create your account' ) }
+					submitForm={ this.handleSubmitSignup }
+					submitting={ isCreatingAccount }
+					suggestedUsername={ includes( email, '@' ) ? '' : email }
+				/>
+			);
+		}
+
+		return (
+			<MainWrapper wooDna={ wooDna }>
+				<div className="jetpack-connect__authorize-form">
+					{ this.renderLocaleSuggestions() }
+					<FormattedHeader headerText={ header } subHeaderText={ subHeader } />
+					{ content }
+					{ this.renderLoginUser() }
+				</div>
+			</MainWrapper>
+		);
+	}
+
 	render() {
+		if ( this.getWooDnaConfig() ) {
+			return this.renderWooDna();
+		}
 		const { isCreatingAccount } = this.state;
 		return (
 			<MainWrapper isWoo={ this.isWoo() }>
@@ -242,12 +390,20 @@ export class JetpackSignup extends Component {
 	}
 }
 
-const connectComponent = connect( null, {
-	createAccount: createAccountAction,
-	createSocialAccount: createSocialAccountAction,
-	errorNotice: errorNoticeAction,
-	recordTracksEvent: recordTracksEventAction,
-	warningNotice: warningNoticeAction,
-} );
+const connectComponent = connect(
+	( state ) => ( {
+		requestError: getRequestError( state ),
+		usernameOrEmail: getLastCheckedUsernameOrEmail( state ),
+		isFullLoginFormVisible: !! getAuthAccountType( state ),
+	} ),
+	{
+		createAccount: createAccountAction,
+		createSocialAccount: createSocialAccountAction,
+		errorNotice: errorNoticeAction,
+		recordTracksEvent: recordTracksEventAction,
+		warningNotice: warningNoticeAction,
+		resetAuthAccountType: resetAuthAccountTypeAction,
+	}
+);
 
 export default flowRight( connectComponent, localize )( JetpackSignup );
