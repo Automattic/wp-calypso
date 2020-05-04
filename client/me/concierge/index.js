2b2716f617cfd7e235052b4149924c5797ffe8a7
Kirk Wight <kwight@kwight.ca>
2020-05-04 10:09:24 -0700

    My Home: Add a Quick Start Session card. (#41112)

--- client/me/concierge/index.js
+++ client/me/concierge/index.js
@@ -10,15 +10,12 @@ import page from 'page';
 import controller from './controller';
 import { makeLayout, render as clientRender } from 'controller';
 import { siteSelection, sites } from 'my-sites/controller';
-import reducer from 'state/concierge/reducer';
 
 const redirectToBooking = ( context ) => {
 	page.redirect( `/me/concierge/${ context.params.siteSlug }/book` );
 };
 
-export default async ( _, addReducer ) => {
-	await addReducer( [ 'concierge' ], reducer );
-
+export default async () => {
 	page( '/me/concierge', controller.siteSelector, siteSelection, sites, makeLayout, clientRender );
 
 	// redirect to booking page after site selection
