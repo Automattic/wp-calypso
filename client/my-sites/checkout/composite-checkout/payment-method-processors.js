b70a09e66d8d1b313ab1682aaf153dbcd53675a4
Payton Swick <payton@foolord.com>
2020-05-04 17:00:42 -0400

    Create stripeCardProcessor

--- client/my-sites/checkout/composite-checkout/payment-method-processors.js
+++ client/my-sites/checkout/composite-checkout/payment-method-processors.js
@@ -10,7 +10,9 @@ import {
 	getDomainDetails,
 	wpcomTransaction,
 	submitApplePayPayment,
+	submitStripeCardTransaction,
 } from './payment-method-helpers';
+import { createStripePaymentMethod } from 'lib/stripe';
 
 const { select, dispatch } = defaultRegistry;
 
@@ -31,4 +33,40 @@ export function applePayProcessor( submitData ) {
 		dispatch( 'wpcom' ).setTransactionResponse( result );
 	} );
 	return pending;
+}
+
+export async function stripeCardProcessor( submitData ) {
+	const paymentMethodToken = await createStripePaymentMethodToken( {
+		...submitData,
+		country: select( 'wpcom' )?.getContactInfo?.()?.countryCode?.value,
+		postalCode: select( 'wpcom' )?.getContactInfo?.()?.postalCode?.value,
+	} );
+	const pending = submitStripeCardTransaction(
+		{
+			...submitData,
+			country: select( 'wpcom' )?.getContactInfo?.()?.countryCode?.value,
+			postalCode: select( 'wpcom' )?.getContactInfo?.()?.postalCode?.value,
+			subdivisionCode: select( 'wpcom' )?.getContactInfo?.()?.state?.value,
+			siteId: select( 'wpcom' )?.getSiteId?.(),
+			domainDetails: getDomainDetails( select ),
+			paymentMethodToken,
+		},
+		wpcomTransaction
+	);
+	// save result so we can get receipt_id and failed_purchases in getThankYouPageUrl
+	pending.then( ( result ) => {
+		// TODO: do this automatically when calling setTransactionComplete
+		dispatch( 'wpcom' ).setTransactionResponse( result );
+	} );
+	return pending;
+}
+
+function createStripePaymentMethodToken( { stripe, name, country, postalCode } ) {
+	return createStripePaymentMethod( stripe, {
+		name,
+		address: {
+			country,
+			postal_code: postalCode,
+		},
+	} );
 }
