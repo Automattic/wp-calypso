d178bc35370b8b58813905e72d4761140b7b5336
Payton Swick <payton@foolord.com>
2020-05-04 13:59:55 -0400

    Revert "My Home: Add a Quick Start Session card. (#41112)"
    This reverts commit 2b2716f617cfd7e235052b4149924c5797ffe8a7.

--- client/me/concierge/index.js
+++ client/me/concierge/index.js
@@ -10,12 +10,15 @@ import page from 'page';
 import controller from './controller';
 import { makeLayout, render as clientRender } from 'controller';
 import { siteSelection, sites } from 'my-sites/controller';
+import reducer from 'state/concierge/reducer';
 
 const redirectToBooking = ( context ) => {
 	page.redirect( `/me/concierge/${ context.params.siteSlug }/book` );
 };
 
-export default async () => {
+export default async ( _, addReducer ) => {
+	await addReducer( [ 'concierge' ], reducer );
+
 	page( '/me/concierge', controller.siteSelector, siteSelection, sites, makeLayout, clientRender );
 
 	// redirect to booking page after site selection
--- client/my-sites/customer-home/locations/tertiary/manage-site/index.jsx
+++ client/my-sites/customer-home/locations/tertiary/manage-site/index.jsx
@@ -10,7 +10,6 @@ import { useTranslate } from 'i18n-calypso';
  */
 import GoMobile from 'my-sites/customer-home/cards/features/go-mobile';
 import Support from 'my-sites/customer-home/cards/features/support';
-import QuickStart from 'my-sites/customer-home/cards/features/quick-start';
 import QuickLinks from 'my-sites/customer-home/cards/actions/quick-links-compact';
 import WpForTeamsQuickLinks from 'my-sites/customer-home/cards/actions/wp-for-teams-quick-links-compact';
 import { getSelectedSiteId } from 'state/ui/selectors';
@@ -20,7 +19,6 @@ const cardComponents = {
 	'home-feature-go-mobile': GoMobile,
 	'home-feature-support': Support,
 	'home-action-quick-links': QuickLinks,
-	'home-feature-quick-start': QuickStart,
 	'home-action-wp-for-teams-quick-links': WpForTeamsQuickLinks,
 };
 
