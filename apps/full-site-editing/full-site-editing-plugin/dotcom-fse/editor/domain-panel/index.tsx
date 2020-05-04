1dae20eda2e62b75d54cf1b5ac30c9fb857daa79
Jon Surrell <jon.surrell@automattic.com>
2020-05-04 16:30:47 +0200

    Render in a modal

--- apps/full-site-editing/full-site-editing-plugin/dotcom-fse/editor/domain-panel/index.tsx
+++ apps/full-site-editing/full-site-editing-plugin/dotcom-fse/editor/domain-panel/index.tsx
@@ -1,18 +1,14 @@
 /**
  * External dependencies
  */
-import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
+import { PluginPrePublishPanel } from '@wordpress/edit-post';
 import { registerPlugin } from '@wordpress/plugins';
-import { __ } from '@wordpress/i18n';
+import { Modal } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import DomainPicker from './domain-picker';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-import { Icon, globe } from '@wordpress/icons';
 
 import '../../../../../../client/landing/gutenboarding/components/domain-picker/style.scss';
 import '../../../../../../client/landing/gutenboarding/components/domain-picker-button/style.scss';
@@ -20,16 +16,16 @@ import '../../../../../../client/landing/gutenboarding/components/domain-picker-
 import '../../../../../../client/landing/gutenboarding/components/domain-picker-popover/style.scss';
 import '../../../../../../client/landing/gutenboarding/components/domain-categories/style.scss';
 
-const ICON = <Icon icon={ globe } />;
 const PLUGIN_SIDEBAR_NAME = 'a8c-domain-sidebar';
 
 registerPlugin( PLUGIN_SIDEBAR_NAME, {
 	render: () => (
 		<>
-			<PluginSidebarMoreMenuItem target={ PLUGIN_SIDEBAR_NAME } icon={ ICON } />
-			<PluginSidebar name={ PLUGIN_SIDEBAR_NAME } title={ __( 'Domains' ) } icon={ ICON }>
-				<DomainPicker />
-			</PluginSidebar>
+			<PluginPrePublishPanel id={ PLUGIN_SIDEBAR_NAME }>
+				<Modal shouldCloseOnClickOutside>
+					<DomainPicker />
+				</Modal>
+			</PluginPrePublishPanel>
 		</>
 	),
 } );
