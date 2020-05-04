a0647c8fae6d19a61c524a23e515e52697eabf84
Beau Collins <beau@collins.pub>
2020-05-04 09:29:09 -0700

    Porting changes from wp-earn

--- apps/full-site-editing/full-site-editing-plugin/premium-content/blocks/logged-out-view/index.js
+++ apps/full-site-editing/full-site-editing-plugin/premium-content/blocks/logged-out-view/index.js
@@ -7,7 +7,10 @@ import save from './save';
 /**
  * WordPress dependencies
  */
+import { compose, ifCondition } from '@wordpress/compose';
+import { withSelect, select, subscribe } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
+import { registerFormatType, unregisterFormatType } from '@wordpress/rich-text';
 
 const name = 'premium-content/logged-out-view';
 const category = 'common';
@@ -70,5 +73,48 @@ const settings = {
 	edit,
 	save,
 };
+
+/**
+ * Modify the rich text link button to not be enabled on the logged-in view (This is visible when the block is selected
+ * (including when the buttons are selected).
+ *
+ * This gets wrapped in `subscribe` to return an unsubscribe function which we can call to unregister the function after
+ * the rich text data is defined.
+ */
+// @ts-ignore
+const unsubscribe = subscribe( () => {
+	// Keep running until the 'core/link' format is defined
+	const linkFormat = select( 'core/rich-text' ).getFormatType( 'core/link' );
+	if ( ! linkFormat ) {
+		return;
+	}
+	// It's defined so we can stop after this iteration...
+	// @ts-ignore
+	unsubscribe();
+
+	unregisterFormatType( 'core/link' );
+
+	// Use the existing link button functionality but limit it so that it doesn't run inside this view.
+	const newLinkButton = compose(
+		withSelect( ( composeSelect ) => {
+			return {
+				selectedBlock: composeSelect( 'core/block-editor' ).getSelectedBlock(),
+			};
+		} ),
+		// @ts-ignore
+		ifCondition( ( props ) => {
+			// @ts-ignore
+			return props.selectedBlock && props.selectedBlock.name !== name;
+		} )
+		// @ts-ignore
+	)( linkFormat.edit );
+
+	// Overwrite the previous 'core/link' so others can extend
+	registerFormatType( 'core/link', {
+		...linkFormat,
+		// @ts-ignore
+		edit: newLinkButton,
+	} );
+} );
 
 export { name, category, settings };
