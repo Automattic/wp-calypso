f88aa03a498928d8178bae1a77464fe04c4e9beb
Payton Swick <payton@foolord.com>
2020-05-04 17:03:54 -0400

    Throw error if usePaymentProcessor fails

--- packages/composite-checkout/src/lib/payment-processors.js
+++ packages/composite-checkout/src/lib/payment-processors.js
@@ -10,5 +10,8 @@ import CheckoutContext from '../lib/checkout-context';
 
 export function usePaymentProcessor( key ) {
 	const { paymentProcessors } = useContext( CheckoutContext );
+	if ( ! paymentProcessors[ key ] ) {
+		throw new Error( 'No payment processor found with key', key );
+	}
 	return paymentProcessors[ key ];
 }
