ec9b2c1b05d9a425fc1b47869b53857cb9359706
Hew <hewsut@users.noreply.github.com>
2020-05-04 16:26:57 -0400

    Lasagna: add jwtType param to fetcher fn (#41763)
    * Lasagna: add jwtType param to fetcher fn
    
    * Snake case for backend

--- client/state/lasagna/middleware.js
+++ client/state/lasagna/middleware.js
@@ -18,7 +18,7 @@ const jwtFetcher = ( jwtType, { params } ) => {
 			apiNamespace: 'wpcom/v2',
 			method: 'POST',
 			path: '/lasagna/jwt/sign',
-			body: { payload: params },
+			body: { jwt_type: jwtType, payload: params },
 		} )
 		.then( ( { jwt } ) => jwt );
 };
