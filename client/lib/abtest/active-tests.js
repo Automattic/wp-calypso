02330f2d849ac55847fcdc54102f5258683f1165
Adam Zieliński <adam@adamziel.com>
2020-05-04 10:41:58 +0200

    Update the test split from 5/95 to 100/0 on ATPrivacy test

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -3,134 +3,134 @@ export default {
 		datestamp: '20180917',
 		variations: {
 			test: 50,
-			control: 50,
+			control: 50
 		},
 		defaultVariation: 'control',
-		allowExistingUsers: true,
+		allowExistingUsers: true
 	},
 	springSale30PercentOff: {
 		datestamp: '20180413',
 		variations: {
 			upsell: 50,
-			control: 50,
+			control: 50
 		},
 		defaultVariation: 'control',
-		allowExistingUsers: true,
+		allowExistingUsers: true
 	},
 	ATPromptOnCancel: {
 		datestamp: '20170515',
 		variations: {
 			hide: 20,
-			show: 80,
+			show: 80
 		},
 		defaultVariation: 'hide',
-		allowExistingUsers: true,
+		allowExistingUsers: true
 	},
 	showCompositeCheckout: {
 		datestamp: '20200326',
 		variations: {
 			composite: 50,
-			regular: 50,
+			regular: 50
 		},
 		defaultVariation: 'regular',
-		allowExistingUsers: true,
+		allowExistingUsers: true
 	},
 	skipThemesSelectionModal: {
 		datestamp: '20170904',
 		variations: {
 			skip: 50,
-			show: 50,
+			show: 50
 		},
 		defaultVariation: 'show',
 		allowExistingUsers: true,
-		localeTargets: 'any',
+		localeTargets: 'any'
 	},
 	builderReferralStatsNudge: {
 		datestamp: '20181218',
 		variations: {
 			builderReferralBanner: 25,
-			googleMyBusinessBanner: 75,
+			googleMyBusinessBanner: 75
 		},
-		defaultVariation: 'googleMyBusinessBanner',
+		defaultVariation: 'googleMyBusinessBanner'
 	},
 	builderReferralThemesBanner: {
 		datestamp: '20181218',
 		variations: {
 			builderReferralBanner: 25,
-			original: 75,
+			original: 75
 		},
-		defaultVariation: 'original',
+		defaultVariation: 'original'
 	},
 	pageBuilderMVP: {
 		datestamp: '20190419',
 		variations: {
 			control: 100,
-			test: 0,
+			test: 0
 		},
 		defaultVariation: 'control',
-		allowExistingUsers: true,
+		allowExistingUsers: true
 	},
 	conciergeUpsellDial: {
 		//this test is used to dial down the upsell offer
 		datestamp: '20200421',
 		variations: {
 			offer: 50,
-			noOffer: 50,
+			noOffer: 50
 		},
 		defaultVariation: 'noOffer',
-		allowExistingUsers: true,
+		allowExistingUsers: true
 	},
 	skippableDomainStep: {
 		datestamp: '20290717',
 		variations: {
 			skippable: 0,
-			notSkippable: 100,
+			notSkippable: 100
 		},
 		defaultVariation: 'notSkippable',
-		allowExistingUsers: true,
+		allowExistingUsers: true
 	},
 	passwordlessSignup: {
 		datestamp: '20291029',
 		variations: {
 			passwordless: 0,
-			default: 100,
+			default: 100
 		},
-		defaultVariation: 'default',
+		defaultVariation: 'default'
 	},
 	domainStepCopyUpdates: {
 		datestamp: '20191121',
 		variations: {
 			variantShowUpdates: 100,
-			control: 0,
+			control: 0
 		},
 		defaultVariation: 'variantShowUpdates',
-		allowExistingUsers: true,
+		allowExistingUsers: true
 	},
 	ATPrivacy: {
 		datestamp: '20200331',
 		variations: {
-			variant: 5,
-			control: 95,
+			variant: 100,
+			control: 0
 		},
-		defaultVariation: 'control',
-		allowExistingUsers: false,
+		defaultVariation: 'variant',
+		allowExistingUsers: false
 	},
 	domainStepPlanStepSwap: {
 		datestamp: '20200415',
 		variations: {
 			variantShowSwapped: 50,
-			control: 50,
+			control: 50
 		},
 		defaultVariation: 'control',
-		allowExistingUsers: true,
+		allowExistingUsers: true
 	},
 	showActiveTicketsNotice: {
 		datestamp: '20200430',
 		variations: {
 			showNotice: 50,
-			control: 50,
+			control: 50
 		},
 		defaultVariation: 'control',
-		allowExistingUsers: true,
-	},
+		allowExistingUsers: true
+	}
 };
