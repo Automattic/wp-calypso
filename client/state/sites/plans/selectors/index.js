fb0b0281c523a1fb5c13566dba2f5065c29f0b04
Sérgio Gomes <mail@sgomes.com>
2020-05-05 15:46:29 +0100

    Split out hasFeature

--- client/state/sites/plans/selectors/index.js
+++ client/state/sites/plans/selectors/index.js
@@ -1,10 +1,3 @@
-/**
- * Internal dependencies
- */
-import { planHasFeature } from 'lib/plans';
-
-import { getSitePlanSlug } from 'state/sites/plans/selectors/get-site-plan-slug';
-
 export { getPlansBySite, getPlansBySiteId } from 'state/sites/plans/selectors/get-plans-by-site';
 export { getCurrentPlan } from 'state/sites/plans/selectors/get-current-plan';
 export { getSitePlan } from 'state/sites/plans/selectors/get-site-plan';
@@ -17,19 +10,4 @@ export { isRequestingSitePlans } from 'state/sites/plans/selectors/is-requesting
 export { isCurrentPlanExpiring } from 'state/sites/plans/selectors/is-current-plan-expiring';
 export { isCurrentUserCurrentPlanOwner } from 'state/sites/plans/selectors/is-current-user-current-plan-owner';
 export { getSitePlanSlug } from 'state/sites/plans/selectors/get-site-plan-slug';
-
-/**
- * Whether a site's current plan includes a given feature
- *
- * DO NOT USE THIS FOR FEATURE GATES, this is only to be used for deciding
- * if nudge should be shown.
- * If you want a feature gate, you should make it backend-side.
- *
- * @param  {object}  state   Global State tree
- * @param  {number}  siteId  Site ID
- * @param  {string}  feature The feature we're looking for
- * @returns {boolean}         True if the site's current plan includes the feature
- */
-export function hasFeature( state, siteId, feature ) {
-	return planHasFeature( getSitePlanSlug( state, siteId ), feature );
-}
+export { hasFeature } from 'state/sites/plans/selectors/has-feature';
