801dbbad88d921f1801ee6b92405573cfb6baf2e
Sérgio Gomes <mail@sgomes.com>
2020-05-05 15:36:49 +0100

    Split out getSitePlanRawPrice

--- client/state/sites/plans/selectors/index.js
+++ client/state/sites/plans/selectors/index.js
@@ -20,27 +20,7 @@ export { getCurrentPlan } from 'state/sites/plans/selectors/get-current-plan';
 export { getSitePlan } from 'state/sites/plans/selectors/get-site-plan';
 export { isSitePlanDiscounted } from 'state/sites/plans/selectors/is-site-plan-discounted';
 export { getPlanDiscountedRawPrice } from 'state/sites/plans/selectors/get-plan-discounted-raw-price';
-
-/**
- * Returns a plan price before discount
- *
- * @param  {object}  state         global state
- * @param  {number}  siteId        the site id
- * @param  {string}  productSlug   the plan product slug
- * @param  {boolean} isMonthly     if true, returns monthly price
- * @returns {number}                plan raw price
- */
-export function getSitePlanRawPrice( state, siteId, productSlug, { isMonthly = false } = {} ) {
-	const plan = getSitePlan( state, siteId, productSlug );
-
-	if ( get( plan, 'rawPrice', -1 ) < 0 ) {
-		return null;
-	}
-
-	const price = plan.rawPrice + get( plan, 'rawDiscount', 0 );
-
-	return isMonthly ? calculateMonthlyPriceForPlan( productSlug, price ) : price;
-}
+export { getSitePlanRawPrice } from 'state/sites/plans/selectors/get-site-plan-raw-price';
 
 /**
  * Returns a plan raw discount. It's the value which was subtracted from the plan's original raw price.
