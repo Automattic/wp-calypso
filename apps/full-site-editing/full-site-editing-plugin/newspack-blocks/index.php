237b10b46dc3283ac10ef37b9fc2264295c72503
Michael P. Pfeiffer <frontdevde@users.noreply.github.com>
2020-05-05 18:31:58 +0200

    FSE: readd posts carousel view js (#41818)

--- apps/full-site-editing/full-site-editing-plugin/newspack-blocks/index.php
+++ apps/full-site-editing/full-site-editing-plugin/newspack-blocks/index.php
@@ -64,16 +64,14 @@ function newspack_blocks_block_args( $args, $name ) {
 	wp_register_style( $block_prefix . 'editor', $editor_style, array(), NEWSPACK_BLOCKS__VERSION );
 
 	// View script.
-	if ( 'carousel' !== $name ) {
-		$script_data = require NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . $block_prefix . 'view.asset.php';
-		wp_register_script(
-			$block_prefix . 'view',
-			plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . $block_prefix . 'view.js', __FILE__ ),
-			$script_data['dependencies'],
-			$script_data['version'],
-			true
-		);
-	}
+	$script_data = require NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . $block_prefix . 'view.asset.php';
+	wp_register_script(
+		$block_prefix . 'view',
+		plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . $block_prefix . 'view.js', __FILE__ ),
+		$script_data['dependencies'],
+		$script_data['version'],
+		true
+	);
 
 	// View style.
 	$editor_style = plugins_url( NEWSPACK_BLOCKS__BLOCKS_DIRECTORY . $block_prefix . 'view.css', __FILE__ );
