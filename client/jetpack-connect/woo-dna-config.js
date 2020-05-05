4fac5465162359218ea00c11622792931fed424e
Daniel Rey López <daniel.rey.lopez@gmail.com>
2020-05-05 16:28:48 +0100

    Fixed code style

--- client/jetpack-connect/woo-dna-config.js
+++ client/jetpack-connect/woo-dna-config.js
@@ -1,9 +1,12 @@
+/**
+ * Internal dependencies
+ */
 import { isEnabled } from 'config';
 
 export default isEnabled( 'jetpack/connect/woo-dna' )
 	? {
 			'woocommerce-payments': {
-				name: translate => translate( 'WooCommerce Payments' ),
+				name: ( translate ) => translate( 'WooCommerce Payments' ),
 				helpUrl: 'https://docs.woocommerce.com/document/payments/', // TODO: Write a WCPay-specific page for Jetpack connection troubles
 			},
 	  }
