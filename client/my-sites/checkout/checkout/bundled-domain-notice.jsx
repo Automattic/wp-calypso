99564e1ae69c70c089e85c2ee098148e23e69fb7
taggon <gonom9@gmail.com>
2020-05-07 15:25:34 +0900

    Change copy based on the length of subscription.

--- client/my-sites/checkout/checkout/bundled-domain-notice.jsx
+++ client/my-sites/checkout/checkout/bundled-domain-notice.jsx
@@ -9,6 +9,21 @@ import React from 'react';
 import { translate } from 'i18n-calypso';
 import { hasPlan, hasJetpackPlan, isNextDomainFree } from 'lib/cart-values/cart-items';
 import Gridicon from 'components/gridicon';
+import { getPlan, getBillingMonthsForTerm } from 'lib/plans';
+import { REGISTER_DOMAIN } from 'lib/url/support';
+
+function hasBiennialPlan( cart ) {
+	const plans = cart.products
+		.map( ( { product_slug } ) => getPlan( product_slug ) )
+		.filter( Boolean );
+	const plan = plans?.[ 0 ];
+
+	try {
+		return getBillingMonthsForTerm( plan?.term ) === 24;
+	} catch ( e ) {
+		return false;
+	}
+}
 
 export default function BundledDomainNotice( { cart } ) {
 	// A dotcom plan should exist.
@@ -21,14 +36,35 @@ export default function BundledDomainNotice( { cart } ) {
 		return null;
 	}
 
+	const domainRegistrationLink = (
+		<a href={ REGISTER_DOMAIN } target="_blank" rel="noopener noreferrer" />
+	);
+
+	let copy = translate(
+		'Purchasing a one-year subscription to a WordPress.com plan gives you one year of access to your plan’s features and one year of a custom domain name. To select your custom domain, follow {{domainRegistrationLink}}the registration instructions{{/domainRegistrationLink}}.',
+		{
+			components: {
+				domainRegistrationLink,
+			},
+		}
+	);
+
+	if ( hasBiennialPlan( cart ) ) {
+		copy = translate(
+			'Purchasing a two-year subscription to a WordPress.com plan gives you two years of access to your plan’s features and one-year of a custom domain name. To select your custom domain, follow {{domainRegistrationLink}}the registration instructions{{/domainRegistrationLink}}. ' +
+				'After the first year, you’ll continue to have access to your WordPress.com plan features but will need to renew the domain name.',
+			{
+				components: {
+					domainRegistrationLink,
+				},
+			}
+		);
+	}
+
 	return (
 		<div className="checkout__bundled-domain-notice">
 			<Gridicon icon="info-outline" size={ 18 } />
-			<p>
-				{ translate(
-					'By purchasing a year WordPress.com Plan, you will gain access to related plan features for 2 years and 1 year of custom domain name. Your domain name needs to be registered to become active.'
-				) }
-			</p>
+			<p>{ copy }</p>
 		</div>
 	);
 }
