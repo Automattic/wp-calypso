e14547d75ce9846f816088f1871b4162f2586136
Eoin Gallagher <eoin@automattic.com>
2020-05-07 22:01:35 +0100

    Add price and currency utility functions

--- apps/full-site-editing/full-site-editing-plugin/premium-content/blocks/container/index.js
+++ apps/full-site-editing/full-site-editing-plugin/premium-content/blocks/container/index.js
@@ -2,6 +2,8 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { getCurrencyDefaults } from '@automattic/format-currency';
+import { trimEnd } from 'lodash';
 
 /**
  * Internal dependencies
@@ -104,3 +106,70 @@ const settings = {
 };
 
 export { name, category, settings };
+
+/**
+ * Currencies we support and Stripe's minimum amount for a transaction in that currency.
+ *
+ * https://stripe.com/docs/currencies#minimum-and-maximum-charge-amounts
+ *
+ * @type { [currency: string]: number }
+ */
+export const SUPPORTED_CURRENCIES = {
+	USD: 0.5,
+	AUD: 0.5,
+	BRL: 0.5,
+	CAD: 0.5,
+	CHF: 0.5,
+	DKK: 2.5,
+	EUR: 0.5,
+	GBP: 0.3,
+	HKD: 4.0,
+	INR: 0.5,
+	JPY: 50,
+	MXN: 10,
+	NOK: 3.0,
+	NZD: 0.5,
+	PLN: 2.0,
+	SEK: 3.0,
+	SGD: 0.5,
+};
+
+/**
+ * Compute a list of currency value and display labels.
+ *
+ * - `value` is the currency's three character code
+ * - `label` is the user facing representation.
+ *
+ * @typedef {{value: string, label: string}} CurrencyDetails
+ *
+ * @type Array<CurrencyDetails>
+ */
+export const CURRENCY_OPTIONS = Object.keys( SUPPORTED_CURRENCIES ).map( value => {
+	const { symbol } = getCurrencyDefaults( value );
+	const label = symbol === value ? value : `${ value } ${ trimEnd( symbol, '.' ) }`;
+	return { value, label };
+} );
+
+/**
+ * Returns the minimum transaction amount for the given currency. If currency is not one of the
+ * known types it returns ...
+ *
+ * @param {string} currency_code three character currency code to get minimum charge for
+ * @return {number} Minimum charge amount for the given currency_code
+ */
+export function minimumTransactionAmountForCurrency( currency_code ) {
+	const minimum = SUPPORTED_CURRENCIES[ currency_code ];
+	return minimum;
+}
+
+/**
+ * True if the price is a number and at least the minimum allowed amount.
+ *
+ * @param {string} currency Currency for the given price.
+ * @param {number} price Price to check.
+ * @return {boolean} true if valid price
+ */
+export function isPriceValid( currency, price ) {
+	return ! isNaN( price ) && price >= minimumTransactionAmountForCurrency( currency );
+}
+
