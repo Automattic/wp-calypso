ea80581a8459944014cbe7b3e83d8e58beab6c2e
Jon Surrell <jon.surrell@automattic.com>
2020-05-08 22:22:15 +0200

    Replace G8 editor styles completely

--- apps/full-site-editing/full-site-editing-plugin/common/index.php
+++ apps/full-site-editing/full-site-editing-plugin/common/index.php
@@ -127,6 +127,8 @@ function append_gutenberg_editor_styles( $settings ) {
 	/**
 	 * Replace Gutenberg 8.0.0 editor styles.
 	 *
+	 * Find and replace the Gutenberg 8.0.0 editor styles with our patched version.
+	 *
 	 * @see
 	 *   - https://github.com/Automattic/wp-calypso/issues/41844
 	 *   - https://github.com/WordPress/gutenberg/issues/22139
@@ -134,9 +136,27 @@ function append_gutenberg_editor_styles( $settings ) {
 	 *   - https://github.com/WordPress/gutenberg/blob/c9ce32fb147b864d7f1d15df8ede97699ded4e91/lib/client-assets.php#L563-L622
 	 */
 	if ( '8.0.0' === $gutenberg_version ) {
-		$settings['styles'][] = array( 'css' => 'a{color:#0073aa}' );
+		if ( empty( $settings['styles'] ) ) {
+			return $settings;
+		}
+
+		$editor_styles_file      = gutenberg_dir_path() . 'build/editor/editor-styles.css';
+		$replacement_styles_file = plugin_dir_path( __FILE__ ) . 'gutenberg-8.0.0-editor-styles.css';
+		if ( ! file_exists( $editor_styles_file ) || ! file_exists( $replacement_styles_file ) ) {
+			return $settings;
+		}
+
+		$gutenberg_editor_styles = file_get_contents( $editor_styles_file );
+		$replacement_styles      = file_get_contents( $replacement_styles_file );
+
+		foreach ( $settings['styles'] as $k => $_ ) {
+			if ( isset( $settings['styles'][ $k ]['css'] ) && $settings['styles'][ $k ]['css'] === $gutenberg_editor_styles ) {
+				$settings['styles'][ $k ]['css'] = $replacement_styles;
+				break;
+			}
+		}
 	}
 
 	return $settings;
 }
-add_action( 'block_editor_settings', __NAMESPACE__ . '\append_gutenberg_editor_styles', 100 );
+add_action( 'block_editor_settings', __NAMESPACE__ . '\append_gutenberg_editor_styles', 11 );
