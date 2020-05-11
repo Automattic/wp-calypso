892990e0b1f9942700b0fe3caf3cc43796517800
retrofox <rdsuarez@gmail.com>
2020-05-11 06:13:29 -0300

    wpcom-block-editor: remove contextual tips

--- apps/wpcom-block-editor/src/wpcom/editor.js
+++ apps/wpcom-block-editor/src/wpcom/editor.js
@@ -14,7 +14,6 @@ import { registerPlugin } from '@wordpress/plugins';
 import './features/deprecate-coblocks-buttons';
 import './features/fix-block-invalidation-errors';
 import './features/reorder-block-categories';
-import './features/enrich-inserter-menu/index';
 import './features/tracking';
 
 import InserterMenuTrackingEvent from './features/tracking/wpcom-inserter-menu-search-term';
