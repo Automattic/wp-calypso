61b2093685152cf21a53a9d19f0706078ef6d733
sixhours <calobee@gmail.com>
2020-05-11 15:13:34 -0400

    Add UpsellNudge test for specific plans

---
+++ client/blocks/upsell-nudge/test/index.jsx
@@ -0,0 +1,80 @@
+jest.mock( 'i18n-calypso', () => ( {
+	localize: ( Comp ) => ( props ) => (
+		<Comp
+			{ ...props }
+			translate={ function ( x ) {
+				return x;
+			} }
+		/>
+	),
+	numberFormat: ( x ) => x,
+	translate: ( x ) => x,
+} ) );
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { shallow } from 'enzyme';
+
+/**
+ * Internal dependencies
+ */
+import { UpsellNudge } from '../index';
+import {
+	PLAN_BUSINESS,
+	PLAN_BUSINESS_2_YEARS,
+	PLAN_PREMIUM,
+	PLAN_PREMIUM_2_YEARS,
+	PLAN_PERSONAL,
+	PLAN_PERSONAL_2_YEARS,
+	PLAN_JETPACK_PERSONAL,
+	PLAN_JETPACK_PERSONAL_MONTHLY,
+	PLAN_JETPACK_PREMIUM,
+	PLAN_JETPACK_PREMIUM_MONTHLY,
+	PLAN_JETPACK_BUSINESS,
+	PLAN_JETPACK_BUSINESS_MONTHLY,
+} from 'lib/plans/constants';
+
+const props = {
+	callToAction: null,
+	title: 'banner title',
+};
+
+describe( 'UpsellNudge should have a class name corresponding to appropriate plan', () => {
+	[
+		PLAN_PERSONAL,
+		PLAN_PERSONAL_2_YEARS,
+		PLAN_JETPACK_PERSONAL,
+		PLAN_JETPACK_PERSONAL_MONTHLY,
+	].forEach( ( plan ) => {
+		test( 'Personal', () => {
+			const comp = shallow( <UpsellNudge { ...props } plan={ plan } /> );
+			expect( comp.find( '.is-upgrade-personal' ) ).toHaveLength( 1 );
+		} );
+	} );
+
+	[
+		PLAN_PREMIUM,
+		PLAN_PREMIUM_2_YEARS,
+		PLAN_JETPACK_PREMIUM,
+		PLAN_JETPACK_PREMIUM_MONTHLY,
+	].forEach( ( plan ) => {
+		test( 'Premium', () => {
+			const comp = shallow( <UpsellNudge { ...props } plan={ plan } /> );
+			expect( comp.find( '.is-upgrade-premium' ) ).toHaveLength( 1 );
+		} );
+	} );
+
+	[
+		PLAN_BUSINESS,
+		PLAN_BUSINESS_2_YEARS,
+		PLAN_JETPACK_BUSINESS,
+		PLAN_JETPACK_BUSINESS_MONTHLY,
+	].forEach( ( plan ) => {
+		test( 'Business', () => {
+			const comp = shallow( <UpsellNudge { ...props } plan={ plan } /> );
+			expect( comp.find( '.is-upgrade-business' ) ).toHaveLength( 1 );
+		} );
+	} );
+} );
