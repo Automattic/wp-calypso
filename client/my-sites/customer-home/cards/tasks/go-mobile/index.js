df7da337a355da32be4551df7fadf7fe527185b8
Fabien MILLERAND <fab@millerf.com>
2020-05-11 16:54:26 +0200

    Modify Task to accept custom action buttons

--- client/my-sites/customer-home/cards/tasks/go-mobile/index.js
+++ client/my-sites/customer-home/cards/tasks/go-mobile/index.js
@@ -9,6 +9,7 @@ import { useTranslate } from 'i18n-calypso';
  */
 import userAgent from 'lib/user-agent';
 import Task from '../task';
+import AppsBadge from 'blocks/get-apps/apps-badge';
 
 /**
  * Image dependencies
@@ -20,17 +21,35 @@ const GoMobile = () => {
 	const translate = useTranslate();
 	const { isAndroid } = userAgent;
 
+	const actionButton = isAndroid ? (
+		<AppsBadge
+			storeLink="https://play.google.com/store/apps/details?id=org.wordpress.android&referrer=utm_source%3Dcalypso-customer-home%26utm_medium%3Dweb%26utm_campaign%3Dmobile-download-promo-pages"
+			storeName={ 'android' }
+			titleText={ translate( 'Download the WordPress Android mobile app.' ) }
+			altText={ translate( 'Google Play Store download badge' ) }
+		>
+			<img src={ googlePlayLogo } alt="" />
+		</AppsBadge>
+	) : (
+		<AppsBadge
+			storeLink="https://apps.apple.com/app/apple-store/id335703880?pt=299112&ct=calypso-customer-home&mt=8"
+			storeName={ 'ios' }
+			titleText={ translate( 'Download the WordPress iOS mobile app.' ) }
+			altText={ translate( 'Apple App Store download badge' ) }
+		>
+			<img src={ appleStoreLogo } alt="" />
+		</AppsBadge>
+	);
+
 	return (
 		<Task
 			title={ translate( 'Update and manage on the go' ) }
 			description={ translate(
 				'Inspiration strikes any time, anywhere. Post, read, check stats, and more with the Wordpress app at your fingertips.'
 			) }
-			actionText={ translate( 'Remind me' ) }
-			// actionUrl={ `/domains/add/${ siteSlug }` }
-			illustration={ isAndroid ? googlePlayLogo : appleStoreLogo }
+			actionButton={ actionButton }
 			timing={ 2 }
-			taskId="find-domain"
+			taskId="go-mobile"
 		/>
 	);
 };
