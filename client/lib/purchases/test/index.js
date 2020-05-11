d8a4275ac9835df7132e408cd1e8909d7aebd03d
Daniel Guerra <daniel_guerra@outlook.com>
2020-05-11 14:07:18 -0500

    Replace chai expect with jest expect in lib/purchases tests

--- client/lib/purchases/test/index.js
+++ client/lib/purchases/test/index.js
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-import { expect } from 'chai';
 import moment from 'moment';
 
 /**
@@ -36,71 +35,71 @@ jest.mock( 'lib/user', () => () => {} );
 describe( 'index', () => {
 	describe( '#isRemovable', () => {
 		test( 'should not be removable when domain registration purchase is not expired', () => {
-			expect( isRemovable( DOMAIN_PURCHASE ) ).to.be.false;
+			expect( isRemovable( DOMAIN_PURCHASE ) ).toEqual( false );
 		} );
 
 		test( 'should not be removable when domain mapping purchase is not expired', () => {
-			expect( isRemovable( DOMAIN_MAPPING_PURCHASE ) ).to.be.false;
+			expect( isRemovable( DOMAIN_MAPPING_PURCHASE ) ).toEqual( false );
 		} );
 
 		test( 'should not be removable when site redirect purchase is not expired', () => {
-			expect( isRemovable( SITE_REDIRECT_PURCHASE ) ).to.be.false;
+			expect( isRemovable( SITE_REDIRECT_PURCHASE ) ).toEqual( false );
 		} );
 
 		test( 'should be removable when domain registration purchase is expired', () => {
-			expect( isRemovable( DOMAIN_PURCHASE_EXPIRED ) ).to.be.true;
+			expect( isRemovable( DOMAIN_PURCHASE_EXPIRED ) ).toEqual( true );
 		} );
 
 		test( 'should be removable when domain mapping purchase is expired', () => {
-			expect( isRemovable( DOMAIN_MAPPING_PURCHASE_EXPIRED ) ).to.be.true;
+			expect( isRemovable( DOMAIN_MAPPING_PURCHASE_EXPIRED ) ).toEqual( true );
 		} );
 
 		test( 'should be removable when site redirect purchase is expired', () => {
-			expect( isRemovable( SITE_REDIRECT_PURCHASE_EXPIRED ) ).to.be.true;
+			expect( isRemovable( SITE_REDIRECT_PURCHASE_EXPIRED ) ).toEqual( true );
 		} );
 	} );
 	describe( '#isCancelable', () => {
 		test( 'should not be cancelable when the purchase is included in a plan', () => {
-			expect( isCancelable( DOMAIN_PURCHASE_INCLUDED_IN_PLAN ) ).to.be.false;
+			expect( isCancelable( DOMAIN_PURCHASE_INCLUDED_IN_PLAN ) ).toEqual( false );
 		} );
 
 		test( 'should not be cancelable when the purchase is expired', () => {
-			expect( isCancelable( DOMAIN_PURCHASE_EXPIRED ) ).to.be.false;
+			expect( isCancelable( DOMAIN_PURCHASE_EXPIRED ) ).toEqual( false );
 		} );
 
 		test( 'should be cancelable when the purchase is refundable', () => {
-			expect( isCancelable( DOMAIN_PURCHASE ) ).to.be.true;
+			expect( isCancelable( DOMAIN_PURCHASE ) ).toEqual( true );
 		} );
 
 		test( 'should be cancelable when the purchase can have auto-renew disabled', () => {
-			expect( isCancelable( PLAN_PURCHASE ) ).to.be.true;
+			expect( isCancelable( PLAN_PURCHASE ) ).toEqual( true );
 		} );
 
 		test( 'should not be cancelable if domain is pending transfer', () => {
-			expect( isCancelable( DOMAIN_PURCHASE_PENDING_TRANSFER ) ).to.be.false;
+			expect( isCancelable( DOMAIN_PURCHASE_PENDING_TRANSFER ) ).toEqual( false );
 		} );
 	} );
 	describe( '#isPaidWithCredits', () => {
 		test( 'should be true when paid with credits', () => {
-			expect( isPaidWithCredits( PLAN_PURCHASE_WITH_CREDITS ) ).to.be.true;
+			expect( isPaidWithCredits( PLAN_PURCHASE_WITH_CREDITS ) ).toEqual( true );
 		} );
 		test( 'should false when not paid with credits', () => {
-			expect( isPaidWithCredits( PLAN_PURCHASE_WITH_PAYPAL ) ).to.be.false;
+			expect( isPaidWithCredits( PLAN_PURCHASE_WITH_PAYPAL ) ).toEqual( false );
 		} );
 		test( 'should be false when payment not set on purchase', () => {
-			expect( isPaidWithCredits( {} ) ).to.be.false;
+			expect( isPaidWithCredits( {} ) ).toEqual( false );
 		} );
 	} );
 	describe( '#hasPaymentMethod', () => {
 		test( 'should be false if no payment data at all', () => {
-			expect( hasPaymentMethod( {} ) ).to.be.false;
+			expect( hasPaymentMethod( {} ) ).toEqual( false );
 		} );
 		test( 'should be false if no payment type', () => {
 			expect(
 				hasPaymentMethod( {
 					payment: {},
 				} )
-			).to.be.false;
+			).toEqual( false );
 		} );
 		test( 'should be true if a payment type is available', () => {
 			expect(
@@ -109,7 +108,7 @@ describe( 'index', () => {
 						type: 'paypal',
 					},
 				} )
-			).to.be.true;
+			).toEqual( true );
 		} );
 	} );
 	describe( '#maybeWithinRefundPeriod', () => {
@@ -120,7 +119,7 @@ describe( 'index', () => {
 					refundPeriodInDays: 2,
 					subscribedDate: moment().subtract( 1, 'days' ).format(),
 				} )
-			).to.be.true;
+			).toEqual( true );
 		} );
 		test( 'should be true if the same amount of time as the refund period has elapsed since the subscription date', () => {
 			expect(
@@ -129,7 +128,7 @@ describe( 'index', () => {
 					refundPeriodInDays: 2,
 					subscribedDate: moment().subtract( 2, 'days' ).format(),
 				} )
-			).to.be.true;
+			).toEqual( true );
 		} );
 		test( 'should be true if less than a full day beyond the refund period has elapsed since the subscription date', () => {
 			// Give users the benefit of the doubt, due to timezones, etc.
@@ -139,7 +138,7 @@ describe( 'index', () => {
 					refundPeriodInDays: 2,
 					subscribedDate: moment().subtract( 71, 'hours' ).format(),
 				} )
-			).to.be.true;
+			).toEqual( true );
 		} );
 		test( 'should be false if one more day than the refund period has elapsed since the subscription date', () => {
 			expect(
@@ -148,7 +147,7 @@ describe( 'index', () => {
 					refundPeriodInDays: 2,
 					subscribedDate: moment().subtract( 3, 'days' ).format(),
 				} )
-			).to.be.false;
+			).toEqual( false );
 		} );
 		test( 'should be true if the purchase is marked as refundable, regardless of how much time has elapsed since the subscription date', () => {
 			expect(
@@ -157,7 +156,7 @@ describe( 'index', () => {
 					refundPeriodInDays: 2,
 					subscribedDate: moment().subtract( 3, 'days' ).format(),
 				} )
-			).to.be.true;
+			).toEqual( true );
 		} );
 		test( 'should be false if the refundPeriodInDays property is not set on the purchase', () => {
 			expect(
@@ -165,7 +164,7 @@ describe( 'index', () => {
 					isRefundable: false,
 					subscribedDate: moment().subtract( 1, 'days' ).format(),
 				} )
-			).to.be.false;
+			).toEqual( false );
 		} );
 		test( 'should be false if the subscribedDate property is not set on the purchase', () => {
 			expect(
@@ -173,26 +172,28 @@ describe( 'index', () => {
 					isRefundable: false,
 					refundPeriodInDays: 2,
 				} )
-			).to.be.false;
+			).toEqual( false );
 		} );
 	} );
 	describe( '#subscribedWithinPastWeek', () => {
 		test( 'should return false when no subscribed date', () => {
-			expect( subscribedWithinPastWeek( {} ) ).to.be.false;
+			expect( subscribedWithinPastWeek( {} ) ).toEqual( false );
 		} );
 		test( 'should return false when subscribed more than 1 week ago', () => {
 			expect(
 				subscribedWithinPastWeek( {
 					subscribedDate: moment().subtract( 8, 'days' ).format(),
 				} )
-			).to.be.false;
+			).toEqual( false );
 		} );
 		test( 'should return true when subscribed less than 1 week ago', () => {
 			expect(
 				subscribedWithinPastWeek( {
 					subscribedDate: moment().subtract( 3, 'days' ).format(),
 				} )
-			).to.be.true;
+			).toEqual( true );
+		} );
+	} );
 		} );
 	} );
 } );
