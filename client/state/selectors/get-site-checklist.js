d772e755deb05c1c8e78d3f08ed60f050448390d
Aurorum <torres126@clubpenguinmountains.com>
2020-05-11 19:27:25 +0100

    Fix bug where Customer Home is broken for Atomic (oops)

--- client/state/selectors/get-site-checklist.js
+++ client/state/selectors/get-site-checklist.js
@@ -1,23 +1,15 @@
 /**
  * External dependencies
  */
-
 import { get } from 'lodash';
 
 /**
- * Internal dependencies
- */
-import isEligibleForDotcomChecklist from 'state/selectors/is-eligible-for-dotcom-checklist';
-
-/**
- * Returns the checklist for the specified site ID if eligible
+ * Returns the checklist for the specified site ID
  *
  * @param  {object}  state  Global state tree
  * @param  {number}  siteId Site ID
  * @returns {object}        Site settings
  */
 export default function getSiteChecklist( state, siteId ) {
-	return ! isEligibleForDotcomChecklist( state, siteId )
-		? null
-		: get( state.checklist, [ siteId, 'items' ], null );
+	return get( state.checklist, [ siteId, 'items' ], null );
 }
