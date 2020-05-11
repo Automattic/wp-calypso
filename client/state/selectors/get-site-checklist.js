928cc4a377c1b73594f1a7ae27de3e88bbd546e8
Aurorum <torres126@clubpenguinmountains.com>
2020-05-11 17:05:31 +0100

    Check if site is eligible for checklist in selector

--- client/state/selectors/get-site-checklist.js
+++ client/state/selectors/get-site-checklist.js
@@ -5,12 +5,19 @@
 import { get } from 'lodash';
 
 /**
- * Returns the checklist for the specified site ID
+ * Internal dependencies
+ */
+import isEligibleForDotcomChecklist from 'state/selectors/is-eligible-for-dotcom-checklist';
+
+/**
+ * Returns the checklist for the specified site ID if eligible
  *
  * @param  {object}  state  Global state tree
  * @param  {number}  siteId Site ID
  * @returns {object}        Site settings
  */
 export default function getSiteChecklist( state, siteId ) {
-	return get( state.checklist, [ siteId, 'items' ], null );
+	return ! isEligibleForDotcomChecklist( state, siteId )
+		? null
+		: get( state.checklist, [ siteId, 'items' ], null );
 }
