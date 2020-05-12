2fc3e203e48192c74bcfd365a2b65d048e62e716
retrofox <rdsuarez@gmail.com>
2020-05-12 07:48:56 -0300

    fse: remove prefix in tips description

--- apps/full-site-editing/full-site-editing-plugin/block-inserter-modifications/tips/tips.js
+++ apps/full-site-editing/full-site-editing-plugin/block-inserter-modifications/tips/tips.js
@@ -22,13 +22,11 @@ const tips = [
 		context: 'theme',
 		keywords: [ 'theme', __( 'theme' ) ],
 		description: tipDescription(
-			__(
-				'theme - You can visit the <a>theme directory</a> to select a different design for your site.'
-			),
+			__( 'You can visit the <a>theme directory</a> to select a different design for your site.' ),
 			{
 				a: <a href="./themes.php" target="_blank" />,
 			},
-			__( 'theme - You can visit the theme directory to select a different design for your site.' )
+			__( 'You can visit the theme directory to select a different design for your site.' )
 		),
 		permission: () => select( 'core' ).canUser( 'create', 'settings' ),
 	},
@@ -36,11 +34,11 @@ const tips = [
 		context: 'css',
 		keywords: [ 'css', __( 'css' ), 'style', __( 'style' ) ],
 		description: tipDescription(
-			__( 'CSS - You can visit the the <a>Customizer</a> to edit the CSS on your site.' ),
+			__( 'You can visit the the <a>Customizer</a> to edit the CSS on your site.' ),
 			{
 				a: <a href="./customize.php?autofocus[section]=custom_css" target="_blank" />,
 			},
-			__( 'CSS - You can visit the the Customizer to edit the CSS on your site.' )
+			__( 'You can visit the the Customizer to edit the CSS on your site.' )
 		),
 		permission: () => select( 'core' ).canUser( 'create', 'settings' ),
 	},
@@ -48,11 +46,11 @@ const tips = [
 		context: 'plugin',
 		keywords: [ 'plugin', __( 'plugin' ) ],
 		description: tipDescription(
-			__( 'plugin - You can visit the <a>plugin directory</a> to install additional plugins.' ),
+			__( 'You can visit the <a>plugin directory</a> to install additional plugins.' ),
 			{
 				a: <a href="./plugin-install.php" target="_blank" />,
 			},
-			__( 'plugin - You can visit the plugin directory to install additional plugins.' )
+			__( 'You can visit the plugin directory to install additional plugins.' )
 		),
 		permission: () => select( 'core' ).canUser( 'create', 'settings' ),
 	},
@@ -60,11 +58,11 @@ const tips = [
 		context: 'header',
 		keywords: [ 'header', __( 'header' ) ],
 		description: tipDescription(
-			__( 'header - You can visit the the <a>Customizer</a> to edit your logo and site title.' ),
+			__( 'You can visit the the <a>Customizer</a> to edit your logo and site title.' ),
 			{
 				a: <a href="./customize.php?autofocus[section]=title_tagline" target="_blank" />,
 			},
-			__( 'header - You can visit the the Customizer to edit your logo and site title.' )
+			__( 'You can visit the the Customizer to edit your logo and site title.' )
 		),
 		permission: () => select( 'core' ).canUser( 'create', 'settings' ),
 	},
@@ -72,11 +70,11 @@ const tips = [
 		context: 'color',
 		keywords: [ 'color', __( 'color' ) ],
 		description: tipDescription(
-			__( 'colors - You can visit the the <a>Customizer</a> to edit the colors on your site.' ),
+			__( 'You can visit the the <a>Customizer</a> to edit the colors on your site.' ),
 			{
 				a: <a href="./customize.php?autofocus[section]=colors" target="_blank" />,
 			},
-			__( 'colors - You can visit the the Customizer to edit the colors on your site.' )
+			__( 'You can visit the the Customizer to edit the colors on your site.' )
 		),
 		permission: () => select( 'core' ).canUser( 'create', 'settings' ),
 	},
