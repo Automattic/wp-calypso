3d84e37c88c2f6f4e9b21035b1ad46e7a4e5ceeb
leogermani <leogermani@gmail.com>
2020-05-12 12:04:20 -0300

    update docs and linter

--- packages/jetpack-redirects/src/index.js
+++ packages/jetpack-redirects/src/index.js
@@ -1,7 +1,13 @@
 /**
  * Builds an URL using the jetpack.com/redirect service
  *
- * @param {string}  source The URL handler registered in the server
+ * If $source is a simple slug, it will be sent using the source query parameter. e.g. jetpack.com/redirect?source=slug
+ *
+ * If $source is a full URL, starting with https://, it will be sent using the url query parameter. e.g. jetpack.com/redirect?url=https://wordpress.com
+ *
+ * Note: if using full URL, query parameters and anchor must be passed in $args. Any querystring of url fragment in the URL will be discarded.
+ *
+ * @param {string}  source The URL handler registered in the server or the full destination URL (starting with https://).
  * @param {object}  args {
  *
  * 		Additional arguments to build the url
@@ -12,7 +18,7 @@
  * 		@type {string} anchor Anchor to be added to the URL
  * }
  *
- * @return {string} The redirect URL
+ * @returns {string} The redirect URL
  */
 export default function getRedirectUrl( source, args = {} ) {
 	const queryVars = {};
@@ -36,8 +42,8 @@ export default function getRedirectUrl( source, args = {} ) {
 	} );
 
 	const queryString = Object.keys( queryVars )
-		.map( ( key ) => key + '=' + queryVars[ key ] )
+		.map( ( key ) => `${ key }=${ queryVars[ key ] }` )
 		.join( '&' );
 
-	return `https://jetpack.com/redirect/?` + queryString;
+	return `https://jetpack.com/redirect/?${ queryString }`;
 }
