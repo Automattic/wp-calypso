60f8582cc3b37a13d3ca3dab81546c93fade15b1
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2020-05-12 17:16:34 +0300

    Fix language manifest fetching in switch locale

--- client/lib/i18n-utils/switch-locale.js
+++ client/lib/i18n-utils/switch-locale.js
@@ -277,7 +277,11 @@ export default function switchLocale( localeSlug ) {
 		// the manifest and all installed translation chunks for
 		// the requested locale
 		getLanguageManifestFile( localeSlug, window.BUILD_TARGET )
-			.then( ( { translatedChunks, locale } ) => {
+			.then( ( { translatedChunks, locale } = {} ) => {
+				if ( ! locale ) {
+					return;
+				}
+
 				const installedChunks = new Set(
 					( window.installedChunks || [] )
 						.concat( window.__requireChunkCallback__.getInstalledChunks() )
@@ -290,6 +294,10 @@ export default function switchLocale( localeSlug ) {
 				return Promise.all( [ Promise.resolve( locale ), ...chunksPromises ] );
 			} )
 			.then( ( localeArray ) => {
+				if ( ! localeArray ) {
+					return;
+				}
+
 				const body = localeArray.reduce( ( acc, item ) => ( { ...acc, ...item } ), {} );
 
 				i18n.setLocale( body );
