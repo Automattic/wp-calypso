30ef9bb2ea00a5d401c0699dc2959917f516c037
Glen Davies <glendaviesnz@users.noreply.github.com>
2020-05-13 11:39:03 +1200

    Customer Home: add editor deprecation notice (#41570)
    Co-authored-by: Glen Davies <glen.davies@a8c.com>
    Co-authored-by: mmtr <miguel.torres@automattic.com>
    Co-authored-by: Kirk Wight <kwight@kwight.ca>

--- client/my-sites/customer-home/locations/primary/index.jsx
+++ client/my-sites/customer-home/locations/primary/index.jsx
@@ -10,6 +10,7 @@ import ConnectAccounts from 'my-sites/customer-home/cards/tasks/connect-accounts
 import Webinars from 'my-sites/customer-home/cards/tasks/webinars';
 import FindDomain from 'my-sites/customer-home/cards/tasks/find-domain';
 import SiteSetupList from 'my-sites/customer-home/cards/tasks/site-setup-list';
+import DeprecateEditor from 'my-sites/customer-home/cards/tasks/deprecate-editor';
 import GoMobile from 'my-sites/customer-home/cards/tasks/go-mobile';
 
 const cardComponents = {
@@ -17,6 +18,7 @@ const cardComponents = {
 	'home-task-connect-accounts': ConnectAccounts,
 	'home-task-find-domain': FindDomain,
 	'home-task-webinars': Webinars,
+	'home-task-editor-deprecation': DeprecateEditor,
 	'home-task-go-mobile-android': GoMobile,
 	'home-task-go-mobile-ios': GoMobile,
 };
