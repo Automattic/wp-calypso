7ee9c27ed7fff9e072b511cc8ab5095d6e9674bc
Payton Swick <payton@foolord.com>
2020-05-12 22:32:42 -0400

    Add createGSuiteContactValidationCallback

--- client/my-sites/checkout/composite-checkout/contact-validation.js
+++ client/my-sites/checkout/composite-checkout/contact-validation.js
@@ -8,6 +8,7 @@ import {
 	translateCheckoutPaymentMethodToWpcomPaymentMethod,
 	areRequiredFieldsNotEmpty,
 } from 'my-sites/checkout/composite-checkout/wpcom';
+import { translate } from 'i18n-calypso';
 
 const debug = debugFactory( 'calypso:composite-checkout:contact-validation' );
 
@@ -69,5 +70,54 @@ export default function createContactValidationCallback( {
 				{ apiVersion: '1.2' }
 			);
 		} );
+	};
+}
+
+export function createGSuiteContactValidationCallback( {
+	validateGSuiteContact,
+	recordEvent,
+	showErrorMessage,
+} ) {
+	return async function contactValidationCallback(
+		paymentMethodId,
+		contactDetails,
+		applyDomainContactValidationResults
+	) {
+		const { contact_information } = prepareDomainContactValidationRequest( [], contactDetails );
+		try {
+			recordEvent( {
+				type: 'VALIDATE_DOMAIN_CONTACT_INFO',
+				payload: {
+					credits: null,
+					payment_method: translateCheckoutPaymentMethodToWpcomPaymentMethod( paymentMethodId ),
+				},
+			} );
+			const data = await validateGSuiteContact( contact_information );
+			debug( 'GSuite contact info validation for', contactDetails, 'result:', data );
+			if ( ! data ) {
+				showErrorMessage(
+					translate(
+						'There was an error validating your contact information. Please contact support.'
+					)
+				);
+			}
+			if ( data && data.messages ) {
+				showErrorMessage(
+					translate(
+						'We could not validate your contact information. Please review and update all the highlighted fields.'
+					)
+				);
+			}
+			applyDomainContactValidationResults( formatDomainContactValidationResponse( data ?? {} ) );
+			return ! ( data && data.success && areRequiredFieldsNotEmpty( contactDetails ) );
+		} catch ( error ) {
+			debug( 'GSuite contact info validation for', contactDetails, 'error:', error );
+			showErrorMessage(
+				translate(
+					'There was an error validating your contact information. Please contact support.'
+				)
+			);
+			return true;
+		}
 	};
 }
