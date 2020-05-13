Automattic/wp-calypso

commit 93da534ba64358879e585927520d20279003128d

Gutenboarding: Forward login page's own redirect_to param to social logins (#42135)

client/blocks/login/social.jsx
@@ -179,6 +179,12 @@ class SocialLoginForm extends Component {
 
 	getRedirectUrl = ( service ) => {
 		const host = typeof window !== 'undefined' && window.location.host;
+
+		if ( window?.localStorage?.getItem( 'a8c_testing_redirect_url' ) ) {
+			const { redirectTo } = this.props;
+			return `https://${ host + login( { isNative: true, socialService: service, redirectTo } ) }`;
+		}
+
 		return `https://${ host + login( { isNative: true, socialService: service } ) }`;
 	};
 

commit cd659c6fc1f41fbe4fb110890b0e4f7839d38ba7

Fix broken SSR due to client/blocks/login
Issue introduced in #42135

client/blocks/login/social.jsx
@@ -172,7 +172,7 @@ class SocialLoginForm extends Component {
 	trackLoginAndRememberRedirect = ( service ) => {
 		this.recordEvent( 'calypso_login_social_button_click', service );
 
-		if ( this.props.redirectTo ) {
+		if ( this.props.redirectTo && typeof window !== 'undefined' ) {
 			window.sessionStorage.setItem( 'login_redirect_to', this.props.redirectTo );
 		}
 	};
@@ -180,7 +180,7 @@ class SocialLoginForm extends Component {
 	getRedirectUrl = ( service ) => {
 		const host = typeof window !== 'undefined' && window.location.host;
 
-		if ( window?.localStorage?.getItem( 'a8c_testing_redirect_url' ) ) {
+		if ( host && window.localStorage?.getItem( 'a8c_testing_redirect_url' ) ) {
 			const { redirectTo } = this.props;
 			return `https://${ host + login( { isNative: true, socialService: service, redirectTo } ) }`;
 		}