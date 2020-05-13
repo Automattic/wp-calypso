f13b746828f07c6bd6492e3d1efce817bc625b9d
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2020-05-13 18:17:25 +0300

    Add missing theme details path with lang slug prefix

--- client/sections.js
+++ client/sections.js
@@ -221,7 +221,7 @@ const sections = [
 	},
 	{
 		name: 'theme',
-		paths: [ '/theme' ],
+		paths: [ '/theme', '/[a-zA-Z-]+/theme' ],
 		module: 'calypso/my-sites/theme',
 		enableLoggedOut: true,
 		group: 'sites',
