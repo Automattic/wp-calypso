a41e6cb2236ca0b96045713997ed632312a64543
Kirk Wight <kwight@kwight.ca>
2020-05-14 14:39:16 -0700

    Revert "Revert "My Home: Update celebratory notices (#41750)" (#42233)"
    This reverts commit ce9b02f7aadffcbf34effed2a618250f7f01d619.

--- client/my-sites/customer-home/locations/primary/index.jsx
+++ client/my-sites/customer-home/locations/primary/index.jsx
@@ -12,6 +12,10 @@ import FindDomain from 'my-sites/customer-home/cards/tasks/find-domain';
 import SiteSetupList from 'my-sites/customer-home/cards/tasks/site-setup-list';
 import DeprecateEditor from 'my-sites/customer-home/cards/tasks/deprecate-editor';
 import GoMobile from 'my-sites/customer-home/cards/tasks/go-mobile';
+import CelebrateSiteCreation from 'my-sites/customer-home/cards/notices/celebrate-site-creation-v2';
+import CelebrateSiteLaunch from 'my-sites/customer-home/cards/notices/celebrate-site-launch-v2';
+import CelebrateSiteMigration from 'my-sites/customer-home/cards/notices/celebrate-site-migration-v2';
+import CelebrateSiteSetupComplete from 'my-sites/customer-home/cards/notices/celebrate-site-setup-complete-v2';
 
 const cardComponents = {
 	'home-task-site-setup-checklist': SiteSetupList,
@@ -21,21 +25,26 @@ const cardComponents = {
 	'home-task-editor-deprecation': DeprecateEditor,
 	'home-task-go-mobile-android': GoMobile,
 	'home-task-go-mobile-ios': GoMobile,
+	'home-notice-celebrate-site-creation': CelebrateSiteCreation,
+	'home-notice-celebrate-site-launch': CelebrateSiteLaunch,
+	'home-notice-celebrate-site-migration': CelebrateSiteMigration,
+	'home-notice-celebrate-site-setup-complete': CelebrateSiteSetupComplete,
 };
 
-const Primary = ( { checklistMode, cards } ) => {
+const Primary = ( { cards } ) => {
 	if ( ! cards || ! cards.length ) {
 		return null;
 	}
 
 	return (
 		<>
-			{ cards.map( ( card, index ) =>
-				React.createElement( cardComponents[ card ], {
-					key: index,
-					checklistMode: card === 'home-task-site-setup-checklist' ? checklistMode : null,
-					isIos: card === 'home-task-go-mobile-ios' ? true : null,
-				} )
+			{ cards.map(
+				( card, index ) =>
+					cardComponents[ card ] &&
+					React.createElement( cardComponents[ card ], {
+						key: index,
+						isIos: card === 'home-task-go-mobile-ios' ? true : null,
+					} )
 			) }
 		</>
 	);
