b70bf987ddbf53e6efd255a26f730bed62275361
Caleb Bauermeister <cbauerman@gmail.com>
2020-05-15 09:08:44 -0700

    begin working on fix

--- client/landing/jetpack-cloud/components/activity-card/activity-description.tsx
+++ client/landing/jetpack-cloud/components/activity-card/activity-description.tsx
@@ -1,17 +1,20 @@
 /**
  * External dependencies
  */
+import { useTranslate } from 'i18n-calypso';
 import React, { FunctionComponent } from 'react';
-
 /**
  * Internal dependencies
  */
 import FormattedBlock from 'components/notes-formatted-block';
+import { useApplySiteOffset } from 'landing/jetpack-cloud/components/site-offset';
+import { useLocalizedMoment } from 'components/localized-moment';
 
 // FUTURE WORK: move this to a shared location
 interface Activity {
 	activityDescription: [
 		{
+			children?: [ { text: string } ];
 			intent?: string;
 			section?: string;
 			type?: string;
@@ -27,25 +30,69 @@ interface Props {
 
 const ActivityDescription: FunctionComponent< Props > = ( {
 	activity: { activityName, activityDescription },
-} ) => (
-	<>
-		{ activityDescription.map( ( description, index ) => {
-			const { intent, section, type, url } = description;
+} ) => {
+	const applySiteOffset = useApplySiteOffset();
+	const moment = useLocalizedMoment();
+	const translate = useTranslate();
 
-			const content =
-				type === 'link' && url?.startsWith( 'https://wordpress.com/' )
-					? { ...description, type: undefined, url: undefined }
-					: description;
+	return (
+		<>
+			{ activityDescription.map( ( description, index ) => {
+				const { children, intent, section, type, url } = description;
 
-			return (
-				<FormattedBlock
-					content={ content }
-					key={ index }
-					meta={ { activity: activityName, intent, section } }
-				/>
-			);
-		} ) }
-	</>
-);
+				let content = description;
+
+				if ( type === 'link' && url?.startsWith( 'https://wordpress.com/' ) ) {
+					content = { ...description, type: undefined, url: undefined };
+				} else if (
+					activityName === 'rewind__complete' &&
+					!! children &&
+					children.length >= 1 &&
+					!! children[ 0 ].text
+				) {
+					const matches = [
+						...children[ 0 ].text.matchAll(
+							/^Restored to ([1-3][0-9]?:[1-5]?[0-9] [p|a]m) on ([a-zA-Z]+ [1-3]?[0-9])$/g
+						),
+					];
+
+					const timeString = matches.map( ( match ) => match[ 1 ] )[ 0 ];
+					const dateString = matches.map( ( match ) => match[ 2 ] )[ 0 ];
+					const yearString = moment().format( 'YYYY' );
+
+					const siteRestoreTime =
+						applySiteOffset && timeString && dateString
+							? applySiteOffset(
+									moment( `${ timeString } ${ dateString }  ${ yearString }`, 'h:m a MMMM D YYYY' )
+							  ).format( 'LLL' )
+							: '';
+
+					content = {
+						...description,
+						children: [
+							{
+								text: siteRestoreTime
+									? translate( 'Restored to %(dateTime)s', {
+											args: {
+												dateTime: siteRestoreTime,
+											},
+									  } ).toString()
+									: '',
+							},
+						],
+					};
+				}
+
+				return (
+					<FormattedBlock
+						content={ content }
+						key={ index }
+						meta={ { activity: activityName, intent, section } }
+					/>
+				);
+			} ) }
+		</>
+	);
+};
 
 export default ActivityDescription;
