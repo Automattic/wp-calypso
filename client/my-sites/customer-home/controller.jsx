2b6a7a38b4602f8d2b8084522288c8856d13f943
Miguel Torres <miguel.torres@automattic.com>
2020-05-18 12:56:23 +0200

    My Home: Remove obsolete notices location (#42255)

--- client/my-sites/customer-home/controller.jsx
+++ client/my-sites/customer-home/controller.jsx
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import React from 'react';
-import { get } from 'lodash';
 import page from 'page';
 
 /**
@@ -11,7 +10,6 @@ import page from 'page';
 import CustomerHome from './main';
 import { getSelectedSiteSlug, getSelectedSiteId } from 'state/ui/selectors';
 import { canCurrentUserUseCustomerHome } from 'state/sites/selectors';
-import isRecentlyMigratedSite from 'state/selectors/is-site-recently-migrated';
 
 export default async function ( context, next ) {
 	const state = await context.store.getState();
@@ -22,12 +20,7 @@ export default async function ( context, next ) {
 		window.scrollTo( 0, 0 );
 	}
 
-	let checklistMode = get( context, 'query.d' );
-	if ( isRecentlyMigratedSite( state, siteId ) ) {
-		checklistMode = 'migrated';
-	}
-
-	context.primary = <CustomerHome checklistMode={ checklistMode } key={ siteId } />;
+	context.primary = <CustomerHome key={ siteId } />;
 
 	next();
 }
