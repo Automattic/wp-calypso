49bb936006b39bd8d7103a4bc88398426d8f55ed
Philip Jackson <p-jackson@live.com>
2020-05-19 10:29:37 +1200

    Refactor `rebootAfterLogin` to call selectors only when needed

--- client/state/login/actions/reboot-after-login.ts
+++ client/state/login/actions/reboot-after-login.ts
@@ -16,18 +16,15 @@ export const rebootAfterLogin = ( tracksEventArgs: object ) => async (
 	dispatch: Dispatch,
 	getState: () => object
 ) => {
-	const redirectToSanitized = getRedirectToSanitized( getState() );
-	const twoFactorEnabled = isTwoFactorEnabled( getState() );
-
 	dispatch(
 		recordTracksEvent( 'calypso_login_success', {
-			two_factor_enabled: twoFactorEnabled,
+			two_factor_enabled: isTwoFactorEnabled( getState() ),
 			...tracksEventArgs,
 		} )
 	);
 
 	// Redirects to / if no redirect url is available
-	const url = redirectToSanitized || '/';
+	const url = getRedirectToSanitized( getState() ) || '/';
 
 	// user data is persisted in localstorage at `lib/user/user` line 157
 	// therefore we need to reset it before we redirect, otherwise we'll get
