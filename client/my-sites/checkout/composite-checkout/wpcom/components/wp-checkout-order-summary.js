3ed2218775cf6a750a3cfdf0d8d40511debba8a2
Michael Cain <cain@automattic.com>
2020-05-18 15:18:02 -0400

    Checkout: add dynamic summary features (3) (#42023)
    * Add hook to check if cart has a plan
    
    * Update summary features based on cart items
    
    - Display all domains bolded at the top, with a conditional bundled message
    - Include live chat support if a plan is in the cart
    - 30 day money back for plans only, 4 day for domains only, and generic "money back guarantee" fallback
    
    * Add key to domain items; fix tests to reflect additional domain in UI
    
    * Remove styling from domain bundling message and render it inline with domain
    
    * Move domain list items into their own component
    
    * Add loading animation and placeholders to summary when cart is updating
    
    * Use form status hook instead of isCartPendingUpdate prop for loading state

--- client/my-sites/checkout/composite-checkout/wpcom/components/wp-checkout-order-summary.js
+++ client/my-sites/checkout/composite-checkout/wpcom/components/wp-checkout-order-summary.js
@@ -4,13 +4,15 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/core';
 import {
 	CheckoutCheckIcon,
 	CheckoutSummaryCard,
 	renderDisplayValueMarkdown,
+	useEvents,
+	useFormStatus,
 	useLineItemsOfType,
 	useTotal,
-	useEvents,
 } from '@automattic/composite-checkout';
 import { useTranslate } from 'i18n-calypso';
 
@@ -22,6 +24,8 @@ import getSupportVariation, {
 	SUPPORT_FORUM,
 	SUPPORT_DIRECTLY,
 } from 'state/selectors/get-inline-help-support-variation';
+import { useHasDomainsInCart, useDomainsInCart } from '../hooks/has-domains';
+import { useHasPlanInCart } from '../hooks/has-plan';
 
 export default function WPCheckoutOrderSummary() {
 	const reduxDispatch = useDispatch();
@@ -29,6 +33,9 @@ export default function WPCheckoutOrderSummary() {
 	const taxes = useLineItemsOfType( 'tax' );
 	const coupons = useLineItemsOfType( 'coupon' );
 	const total = useTotal();
+	const { formStatus } = useFormStatus();
+
+	const isCartUpdating = 'validating' === formStatus;
 
 	const isSupportChatUser = useSelector( ( state ) => {
 		return (
@@ -48,21 +55,16 @@ export default function WPCheckoutOrderSummary() {
 	};
 
 	return (
-		<CheckoutSummaryCardUI>
+		<CheckoutSummaryCardUI className={ isCartUpdating ? 'is-loading' : '' }>
 			<CheckoutSummaryFeatures>
 				<CheckoutSummaryFeaturesTitle>
 					{ translate( 'Included with your purchase' ) }
 				</CheckoutSummaryFeaturesTitle>
-				<CheckoutSummaryFeaturesList>
-					<CheckoutSummaryFeaturesListItem>
-						<WPCheckoutCheckIcon />
-						{ translate( 'Email and live chat support' ) }
-					</CheckoutSummaryFeaturesListItem>
-					<CheckoutSummaryFeaturesListItem>
-						<WPCheckoutCheckIcon />
-						{ translate( 'Money back guarantee' ) }
-					</CheckoutSummaryFeaturesListItem>
-				</CheckoutSummaryFeaturesList>
+				{ isCartUpdating ? (
+					<LoadingCheckoutSummaryFeaturesList />
+				) : (
+					<CheckoutSummaryFeaturesList />
+				) }
 				<CheckoutSummaryHelp onClick={ handleHelpButtonClicked }>
 					{ isSupportChatUser
 						? translate( 'Questions? {{underline}}Ask a Happiness Engineer.{{/underline}}', {
@@ -102,6 +104,88 @@ export default function WPCheckoutOrderSummary() {
 	);
 }
 
+function LoadingCheckoutSummaryFeaturesList() {
+	return (
+		<>
+			<LoadingCopy />
+			<LoadingCopy />
+			<LoadingCopy />
+		</>
+	);
+}
+
+function CheckoutSummaryFeaturesList() {
+	const hasDomainsInCart = useHasDomainsInCart();
+	const domains = useDomainsInCart();
+	const hasPlanInCart = useHasPlanInCart();
+	const translate = useTranslate();
+
+	const supportText = hasPlanInCart
+		? translate( 'Email and live chat support' )
+		: translate( 'Email support' );
+
+	let refundText = translate( 'Money back guarantee' );
+	if ( hasDomainsInCart && ! hasPlanInCart ) {
+		refundText = translate( '4 day money back guarantee' );
+	} else if ( hasPlanInCart && ! hasDomainsInCart ) {
+		refundText = translate( '30 day money back guarantee' );
+	}
+
+	return (
+		<CheckoutSummaryFeaturesListUI>
+			{ hasDomainsInCart &&
+				domains.map( ( domain ) => {
+					return <CheckoutSummaryFeaturesListDomainItem domain={ domain } key={ domain.id } />;
+				} ) }
+			<CheckoutSummaryFeaturesListItem>
+				<WPCheckoutCheckIcon />
+				{ supportText }
+			</CheckoutSummaryFeaturesListItem>
+			<CheckoutSummaryFeaturesListItem>
+				<WPCheckoutCheckIcon />
+				{ refundText }
+			</CheckoutSummaryFeaturesListItem>
+		</CheckoutSummaryFeaturesListUI>
+	);
+}
+
+function CheckoutSummaryFeaturesListDomainItem( { domain } ) {
+	const translate = useTranslate();
+	return (
+		<CheckoutSummaryFeaturesListItem>
+			<WPCheckoutCheckIcon />
+			{ domain.wpcom_meta.is_bundled ? (
+				translate( '{{strong}}%(domain)s{{/strong}} - %(bundled)s', {
+					components: {
+						strong: <strong />,
+					},
+					args: {
+						domain: domain.wpcom_meta.meta,
+						bundled: translate( 'free with plan' ),
+					},
+					comment: 'domain name and bundling message, separated by a dash',
+				} )
+			) : (
+				<strong>{ domain.wpcom_meta.meta }</strong>
+			) }
+		</CheckoutSummaryFeaturesListItem>
+	);
+}
+
+const pulse = keyframes`
+	0% {
+		opacity: 1;
+	}
+
+	70% {
+		opacity: 0.25;
+	}
+
+	100% {
+		opacity: 1;
+	}
+`;
+
 const CheckoutSummaryCardUI = styled( CheckoutSummaryCard )`
 	border-bottom: none 0;
 `;
@@ -117,10 +201,10 @@ const CheckoutSummaryFeatures = styled.div`
 const CheckoutSummaryFeaturesTitle = styled.h3`
 	font-size: 16px;
 	font-weight: ${( props ) => props.theme.weights.normal};
-	margin-bottom: 4px;
+	margin-bottom: 6px;
 `;
 
-const CheckoutSummaryFeaturesList = styled.ul`
+const CheckoutSummaryFeaturesListUI = styled.ul`
 	margin: 0;
 	list-style: none;
 	font-size: 14px;
@@ -139,11 +223,15 @@ const CheckoutSummaryHelp = styled.button`
 const WPCheckoutCheckIcon = styled( CheckoutCheckIcon )`
 	fill: ${( props ) => props.theme.colors.success};
 	margin-right: 4px;
-	vertical-align: bottom;
+	position: absolute;
+	top: 0;
+	left: 0;
 `;
 
 const CheckoutSummaryFeaturesListItem = styled.li`
 	margin-bottom: 4px;
+	padding-left: 24px;
+	position: relative;
 `;
 
 const CheckoutSummaryAmountWrapper = styled.div`
@@ -159,8 +247,37 @@ const CheckoutSummaryLineItem = styled.div`
 	flex-wrap: wrap;
 	justify-content: space-between;
 	margin-bottom: 4px;
+
+	.is-loading & {
+		animation: ${pulse} 1.5s ease-in-out infinite;
+	}
 `;
 
 const CheckoutSummaryTotal = styled( CheckoutSummaryLineItem )`
 	font-weight: ${( props ) => props.theme.weights.bold};
+`;
+
+const LoadingCopy = styled.p`
+	animation: ${pulse} 1.5s ease-in-out infinite;
+	background: ${( props ) => props.theme.colors.borderColorLight};
+	border-radius: 2px;
+	color: ${( props ) => props.theme.colors.borderColorLight};
+	content: '';
+	font-size: 14px;
+	height: 18px;
+	margin: 8px 0 0 26px;
+	padding: 0;
+	position: relative;
+
+	:before {
+		content: '';
+		display: block;
+		position: absolute;
+		left: -26px;
+		top: 0;
+		width: 18px;
+		height: 18px;
+		background: ${( props ) => props.theme.colors.borderColorLight};
+		border-radius: 100%;
+	}
 `;
