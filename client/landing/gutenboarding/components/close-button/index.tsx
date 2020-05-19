1bd9774f0d09469cfa93f2c104f1e4d2b0a8c463
Bernie Reiter <ockham@raz.or.at>
2020-05-19 10:59:12 +0200

    Gutenboarding: Use @wordpress/icons in Domain Picker (#40803)
    * Dependencies: Add @wordpress/icons 2.0.0
    * Gutenboarding: Use @wordpress/icons in Domain Picker
    * Set Close icon size to 16
    
    Co-authored-by: Jon Surrell <jon.surrell@automattic.com>

--- client/landing/gutenboarding/components/close-button/index.tsx
+++ client/landing/gutenboarding/components/close-button/index.tsx
@@ -2,22 +2,15 @@
  * External dependencies
  */
 import React from 'react';
-import { Button, Path, SVG } from '@wordpress/components';
+import { Button } from '@wordpress/components';
+import { Icon, close } from '@wordpress/icons';
 import { useI18n } from '@automattic/react-i18n';
 
 const CloseButton: React.FunctionComponent< Button.ButtonProps > = ( { ...buttonProps } ) => {
 	const { __ } = useI18n();
 	return (
 		<Button label={ __( 'Close dialog' ) } { ...buttonProps }>
-			<SVG
-				width="12"
-				height="12"
-				viewBox="0 0 16 16"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<Path d="M1.40456 1L15 15M1 15L14.5954 1" stroke="#1E1E1E" strokeWidth="1.5" />
-			</SVG>
+			<Icon icon={ close } size={ 16 } />
 		</Button>
 	);
 };
