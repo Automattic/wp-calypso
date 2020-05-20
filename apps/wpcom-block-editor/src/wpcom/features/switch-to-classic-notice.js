9077a51ae081f6300033dea066d1abe6ca50d86c
Glen Davies <glen.davies@a8c.com>
2020-05-20 16:06:30 +1200

    Switch to using a standard sidepanel slot injection

--- apps/wpcom-block-editor/src/wpcom/features/switch-to-classic-notice.js
+++ apps/wpcom-block-editor/src/wpcom/features/switch-to-classic-notice.js
@@ -1,38 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /**
  * External dependencies
  */
-/* eslint-disable import/no-extraneous-dependencies */
-import domReady from '@wordpress/dom-ready';
-import ReactDOM from 'react-dom';
 import { __ } from '@wordpress/i18n';
-/* eslint-disable import/no-extraneous-dependencies */
+import { registerPlugin } from '@wordpress/plugins';
+import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+/* eslint-enable import/no-extraneous-dependencies */
 
-function ClassicEditorNotice() {
-	return (
-		<div className="switch-to-classic-notice__content">
-			<h2>{ __( 'You are using the most modern WordPress editor yet.' ) }</h2>
-			<p>{ __( "If you'd prefer you canswitch back to the Classic Editor" ) } </p>
-		</div>
-	);
-}
-
-domReady( () => {
-	const editInception = setInterval( () => {
-		// Cycle through interval until sidebar is found.
-		const sidebar = document.querySelector( '.interface-interface-skeleton__sidebar' );
-
-		if ( ! sidebar ) {
-			return;
-		}
-
-		clearInterval( editInception );
-
-		sidebar.classList.add( 'show-classic-editor-notice' );
-
-		const switchToClassicNotice = document.createElement( 'div' );
-		switchToClassicNotice.className = 'edit-post-switch-to-classic-notice';
-		sidebar.append( switchToClassicNotice );
-
-		ReactDOM.render( <ClassicEditorNotice />, switchToClassicNotice );
-	} );
+const PluginDocumentSettingPanelDemo = () => (
+	<PluginDocumentSettingPanel
+		name="switch-to-classic"
+		title="Switch to Classic Editor"
+		className="features__switch-to-classic"
+	>
+		<h2>{ __( 'You are using the most modern WordPress editor yet.' ) }</h2>
+		<p>{ __( "If you'd prefer you canswitch back to the Classic Editor" ) } </p>
+	</PluginDocumentSettingPanel>
+);
+registerPlugin( 'plugin-document-setting-panel-demo', {
+	render: PluginDocumentSettingPanelDemo,
+	icon: null,
 } );
