e3e0617aad0f3bd82397f4e47f3ef1e2327ec23f
Glen Davies <glen.davies@a8c.com>
2020-05-20 16:27:55 +1200

    add query param limit

--- apps/wpcom-block-editor/src/wpcom/features/switch-to-classic-notice.js
+++ apps/wpcom-block-editor/src/wpcom/features/switch-to-classic-notice.js
@@ -5,6 +5,7 @@
 import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+import url from 'url';
 /* eslint-enable import/no-extraneous-dependencies */
 
 const PluginDocumentSettingPanelDemo = () => (
@@ -17,7 +18,13 @@ const PluginDocumentSettingPanelDemo = () => (
 		<p>{ __( "If you'd prefer you canswitch back to the Classic Editor" ) } </p>
 	</PluginDocumentSettingPanel>
 );
-registerPlugin( 'plugin-document-setting-panel-demo', {
-	render: PluginDocumentSettingPanelDemo,
-	icon: null,
-} );
+
+const parsedEditorUrl = url.parse( window.location.href, true );
+// just temporarily using the 'action' query param - we need to pass through a custom query param
+// to identify group to show this option to as outlined in https://github.com/Automattic/wp-calypso/issues/41087.
+if ( parsedEditorUrl.query.action ) {
+	registerPlugin( 'plugin-document-setting-panel-demo', {
+		render: PluginDocumentSettingPanelDemo,
+		icon: null,
+	} );
+}
