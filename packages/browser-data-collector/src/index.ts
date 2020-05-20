727e5f40b6812093917cb5a21514d881accb4453
Sergio Cinos <sergio.cinos@gmail.com>
2020-05-20 16:46:22 +0200

    Do not crash if there are no reports in progress

--- packages/browser-data-collector/src/index.ts
+++ packages/browser-data-collector/src/index.ts
@@ -14,16 +14,18 @@ const inFlightReporters: Map< string, ReportImpl > = new Map();
  * @param id id of the report, must be passed to `stop()` to stop it
  * @param obj Options
  * @param obj.fullPageLoad `true` if the report should start measuring from the load of the page, `false` to start measuring from now.
- * @returns The created report
  */
 export const start = ( id: string, { fullPageLoad = true }: { fullPageLoad?: boolean } = {} ) => {
 	const existingReport = inFlightReporters.get( id );
+
+	// There is a report in progress. Probably the user clicked twice.
+	if ( existingReport ) return;
+
 	if ( existingReport ) {
 		throw new Error( `A report with the same key '${ id }'is already in process.` );
 	}
 	const report = new ReportImpl( id, fullPageLoad );
 	inFlightReporters.set( id, report );
-	return report;
 };
 
 /**
@@ -34,9 +36,10 @@ export const start = ( id: string, { fullPageLoad = true }: { fullPageLoad?: boo
  */
 export const stop = async ( id: string ): Promise< boolean > => {
 	const existingReport = inFlightReporters.get( id );
-	if ( ! existingReport ) {
-		throw new Error( `There is no in progress report with the key '${ id }'` );
-	}
+
+	// There is no in progress report with the key, fail silently to avoid messing with the rendering
+	if ( ! existingReport ) return false;
+
 	existingReport.stop();
 	inFlightReporters.delete( id );
 	await applyGlobalCollectors( existingReport );
