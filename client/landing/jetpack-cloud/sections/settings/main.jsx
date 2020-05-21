b3939b31e1702b3984087447691345307274116a
Matt Gawarecki <matt.gawarecki@gmail.com>
2020-05-21 09:01:52 -0500

    Jetpack Cloud: Collapse connection details form when connected (#42376)
    * Convert SettingsPage to function-based component
    
    * Jetpack Cloud: Hide connected server creds behind a FoldableCard

--- client/landing/jetpack-cloud/sections/settings/main.jsx
+++ client/landing/jetpack-cloud/sections/settings/main.jsx
@@ -1,9 +1,9 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { localize } from 'i18n-calypso';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { localize, useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -12,6 +12,7 @@ import DocumentHead from 'components/data/document-head';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import ServerCredentialsForm from 'landing/jetpack-cloud/components/server-credentials-form';
 import { Button, Card } from '@automattic/components';
+import FoldableCard from 'components/foldable-card';
 import getRewindState from 'state/selectors/get-rewind-state';
 import QueryRewindState from 'components/data/query-rewind-state';
 import Main from 'components/main';
@@ -27,95 +28,108 @@ import './style.scss';
 import connectedIcon from './images/server-connected.svg';
 import disconnectedIcon from './images/server-disconnected.svg';
 
-class SettingsPage extends Component {
-	renderServerConnectionStatus() {
-		const { translate, rewind } = this.props;
+const connectedProps = ( translate ) => ( {
+	iconPath: connectedIcon,
+	className: 'settings_connected',
+	title: translate( 'Server status: Connected' ),
+	content: translate( 'One-click restores are enabled.' ),
+} );
 
-		const isConnected = 'active' === rewind.state;
+const disconnectedProps = ( translate ) => ( {
+	iconPath: disconnectedIcon,
+	className: 'settings_disconnected',
+	title: translate( 'Server status: Not connected' ),
+	content: translate(
+		'Enter your server credentials to enable one-click restores for Backups. {{a}}Need help? Find your server credentials{{/a}}',
+		{
+			components: {
+				a: (
+					<ExternalLink
+						className="settings__link-external"
+						icon
+						href="https://jetpack.com/support/adding-credentials-to-jetpack/"
+					/>
+				),
+			},
+		}
+	),
+} );
 
-		const iconPath = isConnected ? connectedIcon : disconnectedIcon;
-		const myClassName = isConnected ? 'settings__connected' : 'settings__disconnected';
-		const title = isConnected
-			? translate( 'Server status: Connected' )
-			: translate( 'Server status: Not connected' );
-		const content = isConnected
-			? translate( 'One-click restores are enabled.' )
-			: translate(
-					'Enter your server credentials to enable one-click restores for Backups. {{a}}Need help? Find your server credentials{{/a}}',
-					{
-						components: {
-							a: (
-								<ExternalLink
-									className="settings__link-external"
-									icon
-									href="https://jetpack.com/support/adding-credentials-to-jetpack/"
-								/>
-							),
-						},
+const ConnectionStatus = ( { isConnected } ) => {
+	const translate = useTranslate();
+	const cardProps = isConnected ? connectedProps( translate ) : disconnectedProps( translate );
+
+	return (
+		<Card compact={ true } className={ cardProps.className }>
+			<img className="settings__icon" src={ cardProps.iconPath } alt="" />
+			<div className="settings__details">
+				<div className="settings__details-head"> { cardProps.title } </div>
+				<div>{ cardProps.content }</div>
+			</div>
+		</Card>
+	);
+};
+
+const SettingsPage = () => {
+	const translate = useTranslate();
+	const siteId = useSelector( getSelectedSiteId );
+
+	const rewind = useSelector( ( state ) => getRewindState( state, siteId ) );
+	const isInitialized = rewind.state !== 'uninitialized';
+	const isConnected = rewind.state === 'active';
+
+	const [ formOpen, setFormOpen ] = useState( false );
+	useEffect( () => {
+		setFormOpen( ! isConnected );
+	}, [ isConnected ] );
+
+	// Clears everything user related on the client site by
+	// calling user.clear() which calls store.clearAll();
+	// @todo: track event (what type?)
+	const logOut = () => userUtilities.logout();
+
+	return (
+		<Main className="settings">
+			<DocumentHead title={ translate( 'Settings' ) } />
+			<SidebarNavigation />
+			<QueryRewindState siteId={ siteId } />
+			<PageViewTracker path="/settings/:site" title="Settings" />
+
+			<Button primary scary onClick={ logOut }>
+				Log out
+			</Button>
+
+			<div className="settings__title">
+				<h2>{ translate( 'Server connection details' ) }</h2>
+			</div>
+
+			{ ! isInitialized && <div className="settings__status-uninitialized" /> }
+			{ isInitialized && <ConnectionStatus isConnected={ isConnected } /> }
+
+			{ ! isInitialized && <div className="settings__form-uninitialized" /> }
+			{ isInitialized && (
+				<FoldableCard
+					header={
+						formOpen
+							? translate( 'Hide connection details' )
+							: translate( 'Show connection details' )
 					}
-			  );
+					expanded={ formOpen }
+					onClick={ () => setFormOpen( ! formOpen ) }
+					className="settings__form-card"
+				>
+					<ServerCredentialsForm
+						role="main"
+						siteId={ siteId }
+						labels={ {
+							save: translate( 'Save credentials' ),
+						} }
+						showCancelButton={ false }
+					/>
+				</FoldableCard>
+			) }
+		</Main>
+	);
+};
 
-		return (
-			<Card compact={ true } className={ myClassName }>
-				<img className="settings__icon" src={ iconPath } alt="" />
-				<div className="settings__details">
-					<div className="settings__details-head"> { title } </div>
-					<div>{ content }</div>
-				</div>
-			</Card>
-		);
-	}
-
-	logOut() {
-		// Clears everything user related on the client site by
-		// calling user.clear() which calls store.clearAll();
-		userUtilities.logout();
-		// @todo: track event (what type?)
-	}
-
-	render() {
-		const { rewind, siteId, translate } = this.props;
-
-		const isInitialized = ! ( 'uninitialized' === rewind.state );
-
-		return (
-			<Main className="settings">
-				<DocumentHead title={ translate( 'Settings' ) } />
-				<SidebarNavigation />
-				<QueryRewindState siteId={ siteId } />
-				<PageViewTracker path="/settings/:site" title="Settings" />
-
-				<Button primary scary onClick={ this.logOut }>
-					Log out
-				</Button>
-
-				<div className="settings__title">
-					<h2>{ translate( 'Server connection details' ) }</h2>
-				</div>
-
-				{ isInitialized && this.renderServerConnectionStatus() }
-
-				{ ! isInitialized && <div className="settings__is-uninitialized" /> }
-
-				<ServerCredentialsForm
-					role="main"
-					siteId={ siteId }
-					labels={ {
-						save: translate( 'Save credentials' ),
-					} }
-					showCancelButton={ false }
-				/>
-			</Main>
-		);
-	}
-}
-
-export default connect( ( state ) => {
-	const siteId = getSelectedSiteId( state );
-	const rewind = getRewindState( state, siteId );
-
-	return {
-		siteId,
-		rewind,
-	};
-} )( localize( SettingsPage ) );
+export default localize( SettingsPage );
