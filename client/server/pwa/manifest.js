cab9af76b16e4fc058fc94f5a88d6ad008b449e6
Caleb Bauermeister <cbauerman@gmail.com>
2020-05-20 17:44:53 -0700

    use new jetpack cloud values

--- client/server/pwa/manifest.js
+++ client/server/pwa/manifest.js
@@ -2,10 +2,62 @@
  * External dependencies
  */
 import querystring from 'querystring';
+import config from 'config';
 
-/**
- * Internal dependencies
- */
+const getWordPressOptions = ( environmentUrlSuffix ) => ( {
+	gcm_sender_id: '87234302238',
+	background_color: '#0078be',
+	theme_color: '#0078be',
+	icons: [
+		{
+			src: '/calypso/images/manifest/icon-144x144.png' + environmentUrlSuffix,
+			sizes: '144x144',
+			type: 'image/png',
+		},
+		{
+			src: '/calypso/images/manifest/icon-192x192.png' + environmentUrlSuffix,
+			sizes: '192x192',
+			type: 'image/png',
+		},
+		{
+			src: '/calypso/images/manifest/icon-512x512.png' + environmentUrlSuffix,
+			sizes: '512x512',
+			type: 'image/png',
+		},
+	],
+	related_applications: [
+		{
+			platform: 'play',
+			url: 'https://play.google.com/store/apps/details?id=org.wordpress.android',
+		},
+		{
+			platform: 'itunes',
+			url: 'https://itunes.apple.com/app/wordpress/id335703880',
+		},
+	],
+} );
+
+const getJetpackCloudOptions = ( environmentUrlSuffix ) => ( {
+	background_color: '#2fb41f',
+	theme_color: '#2fb41f',
+	icons: [
+		{
+			src: '/calypso/images/manifest/jetpack-icon-144x144.png' + environmentUrlSuffix,
+			sizes: '144x144',
+			type: 'image/png',
+		},
+		{
+			src: '/calypso/images/manifest/jetpack-icon-192x192.png' + environmentUrlSuffix,
+			sizes: '192x192',
+			type: 'image/png',
+		},
+		{
+			src: '/calypso/images/manifest/jetpack-icon-512x512.png' + environmentUrlSuffix,
+			sizes: '512x512',
+			type: 'image/png',
+		},
+	],
+} );
 
 /**
  * TODO:
@@ -27,40 +79,13 @@ const buildManifest = ( { branchName } ) => {
 	const environmentUrlSuffix = '?' + querystring.stringify( environmentUrlOptions );
 
 	return {
-		name: 'WordPress.com',
-		short_name: 'WordPress.com',
-		start_url: '/' + environmentUrlSuffix,
 		display: 'standalone',
-		gcm_sender_id: '87234302238',
-		background_color: '#0078be',
-		theme_color: '#0078be',
-		icons: [
-			{
-				src: '/calypso/images/manifest/icon-144x144.png' + environmentUrlSuffix,
-				sizes: '144x144',
-				type: 'image/png',
-			},
-			{
-				src: '/calypso/images/manifest/icon-192x192.png' + environmentUrlSuffix,
-				sizes: '192x192',
-				type: 'image/png',
-			},
-			{
-				src: '/calypso/images/manifest/icon-512x512.png' + environmentUrlSuffix,
-				sizes: '512x512',
-				type: 'image/png',
-			},
-		],
-		related_applications: [
-			{
-				platform: 'play',
-				url: 'https://play.google.com/store/apps/details?id=org.wordpress.android',
-			},
-			{
-				platform: 'itunes',
-				url: 'https://itunes.apple.com/app/wordpress/id335703880',
-			},
-		],
+		name: config( 'site_name' ),
+		short_name: config( 'site_name' ),
+		start_url: '/' + environmentUrlSuffix,
+		...( config.isEnabled( 'jetpack-cloud' )
+			? getJetpackCloudOptions( environmentUrlSuffix )
+			: getWordPressOptions( environmentUrlSuffix ) ),
 	};
 };
 
