d74e9f02e1e10ce24f4479c6b690a50c62561f6a
Jon Surrell <jon.surrell@automattic.com>
2020-05-25 18:10:51 +0200

    Revert "Enable package tests"
    This reverts commit d26e225c928152d05a5ce39b406d600a1de881ff.

--- packages/domain-picker/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	preset: '@automattic/calypso-build',
-	rootDir: __dirname,
-	testEnvironment: 'jsdom',
-};
--- packages/domain-picker/test/suggestion-item.tsx
+++ packages/domain-picker/test/suggestion-item.tsx
@@ -1,4 +1,8 @@
 /**
+ * @jest-environment jsdom
+ */
+
+/**
  * External dependencies
  */
 import * as React from 'react';
@@ -24,7 +28,7 @@ describe( 'traintracks events', () => {
 	describe( 'render event', () => {
 		it( 'sends render events when first rendered', async () => {
 			// Delay import so we have time to load configData in `beforeAll`
-			const { default: SuggestionItem } = await import( '../src/suggestion-item' );
+			const { default: SuggestionItem } = await import( '../suggestion-item' );
 
 			const recordAnalytics = jest.fn();
 			const railcarId = 'id';
