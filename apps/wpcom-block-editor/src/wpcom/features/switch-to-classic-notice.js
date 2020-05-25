56d0efbda4ff1014e34c0e0cf3595ad683085a01
Glen Davies <glen.davies@a8c.com>
2020-05-25 14:38:04 +1200

    Switch to showing a Notice component at top of document prefs panel

--- apps/wpcom-block-editor/src/wpcom/features/switch-to-classic-notice.js
+++ apps/wpcom-block-editor/src/wpcom/features/switch-to-classic-notice.js
@@ -3,37 +3,70 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { registerPlugin } from '@wordpress/plugins';
-import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+import domReady from '@wordpress/dom-ready';
+import ReactDOM from 'react-dom';
+import { useEffect, createInterpolateElement } from '@wordpress/element';
+import { withNotices } from '@wordpress/components';
 import url from 'url';
 /* eslint-enable import/no-extraneous-dependencies */
 
 const parsedEditorUrl = url.parse( window.location.href, true );
+const dismissNotice = () => {
+	console.log( 'Notice dismissed' );
+	localStorage.setItem( 'dismissedClassicEditorNotice', 'true' );
+};
 
-const classicEditorUrl = `${ parsedEditorUrl.protocol }//${ parsedEditorUrl.hostname }${ parsedEditorUrl.pathname }?post=${ parsedEditorUrl.query.post }&action=${ parsedEditorUrl.query.action }&set-editor=classic`;
+const ClassicEditorNotice = ( { noticeOperations, noticeUI } ) => {
+	const noticeText = createInterpolateElement(
+		'If you prefer the Classic editor, you can always switch. Open the options menu by click in the button at the top right of your screen, and select <strong>Switch to Classic editor</strong>.',
+		{ strong: <strong /> }
+	);
 
-const PluginDocumentSettingPanelDemo = () => (
-	<PluginDocumentSettingPanel
-		name="switch-to-classic"
-		title="Switch to Classic Editor"
-		className="features__switch-to-classic"
-	>
-		<h2>{ __( 'You are using the most modern WordPress editor yet.' ) }</h2>
-		<p>
-			If you'd prefer you can switch back to the
-			<a target="_top" href={ classicEditorUrl }>
-				Classic Editor
-			</a>
-		</p>
-	</PluginDocumentSettingPanel>
-);
+	const noticeContent = (
+		<div>
+			<h2>{ __( "You're usng the most modern WordPress Editor." ) } </h2>
+			<p>{ noticeText }</p>
+		</div>
+	);
 
-// Need to replace this check with one that checks for a classicEditorUrl.query param - we need to pass through a
-// a custom query param from calyps to identify group to show this option to as outlined in
+	useEffect( () => {
+		noticeOperations.createNotice( {
+			className: 'edit-post-switch-to-classic-notice',
+			status: 'info',
+			content: noticeContent,
+			isDismissible: true,
+			onRemove: dismissNotice,
+		} );
+	}, [ noticeOperations ] );
+
+	return <div>{ noticeUI }</div>;
+};
+
+const NoticeComponent = withNotices( ClassicEditorNotice );
+
+const noticeDismissed = localStorage.getItem( 'dismissedClassicEditorNotice' );
+
+// Need to replace this check with one that checks for a specific parsedEditorUrl.query param
 // https://github.com/Automattic/wp-calypso/issues/41087.
-if ( 1 === 1 ) {
-	registerPlugin( 'plugin-document-setting-panel-demo', {
-		render: PluginDocumentSettingPanelDemo,
-		icon: null,
+if ( parsedEditorUrl.query[ 'environment-id' ] === 'stage' && noticeDismissed !== 'true' ) {
+	domReady( () => {
+		const editInception = setInterval( () => {
+			// Cycle through interval until sidebar is found.
+			const sidebar = document.querySelector( '.components-panel__body.edit-post-post-status' );
+
+			if ( ! sidebar ) {
+				return;
+			}
+
+			clearInterval( editInception );
+
+			sidebar.classList.add( 'show-classic-editor-notice' );
+
+			const switchToClassicNotice = document.createElement( 'div' );
+			switchToClassicNotice.className = 'edit-post-switch-to-classic-notice';
+			sidebar.after( switchToClassicNotice );
+
+			ReactDOM.render( <NoticeComponent />, switchToClassicNotice );
+		} );
 	} );
 }
