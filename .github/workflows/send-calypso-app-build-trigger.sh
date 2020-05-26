6e59a9ad46bb60ddfe9178da593967143d2b6727
Noah Allen <noahtallen@gmail.com>
2020-05-26 10:51:42 -0700

    Check for environment variables on our side

--- .github/workflows/send-calypso-app-build-trigger.sh
+++ .github/workflows/send-calypso-app-build-trigger.sh
@@ -4,15 +4,30 @@ set -Eeuo pipefail
 # cd here so that the parent directories are not included in the zip file.
 cd apps/full-site-editing/full-site-editing-plugin
 
+echo -e "Creating archive file...\n"
+
 # Create a zip of the FSE plugin. Should include built files at this point.
 build_archive=plugin-archive.zip
 zip --quiet --recurse-paths $build_archive ./*
 
+echo -e "Creating JSON payload...\n"
 
 # Use node to process data into JSON file
 node -e '
 const fs = require("fs");
 const trigger_payload = JSON.parse( fs.readFileSync( process.env.GITHUB_EVENT_PATH, "utf8" ) );
+
+// Makes sure that the data we need exists.
+const checkEnv = ( varNames ) => {
+	varNames.forEach( ( varName ) => {
+		if ( ! process.env[ varName ] ) {
+			throw new Error( `${ varName } env variable missing!` );
+		}
+	} );
+}
+
+checkEnv( [ "GITHUB_ACTION", "GITHUB_ACTOR", "GITHUB_RUN_ID", "GITHUB_RUN_NUMBER", "GITHUB_REPOSITORY" ] );
+
 const output = JSON.stringify( {
 	action: process.env.GITHUB_ACTION,
 	actor: process.env.GITHUB_ACTOR,
