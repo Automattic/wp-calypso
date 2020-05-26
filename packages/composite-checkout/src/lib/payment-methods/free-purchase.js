b94a1efd7cbf561c110f8fbf2aa49dcc3601fa80
Payton Swick <payton@foolord.com>
2020-05-26 16:46:54 -0400

    Change free purchase payment method to use transaction status

--- packages/composite-checkout/src/lib/payment-methods/free-purchase.js
+++ packages/composite-checkout/src/lib/payment-methods/free-purchase.js
@@ -9,78 +9,19 @@ import debugFactory from 'debug';
  */
 import Button from '../../components/button';
 import {
-	useSelect,
-	useDispatch,
 	useMessages,
 	useLineItems,
 	useEvents,
 	renderDisplayValueMarkdown,
+	useTransactionStatus,
+	usePaymentProcessor,
 } from '../../public-api';
 import { sprintf, useLocalize } from '../localize';
 import { useFormStatus } from '../form-status';
 
 const debug = debugFactory( 'composite-checkout:free-purchase-payment-method' );
 
-export function createFreePaymentMethod( { registerStore, submitTransaction } ) {
-	const actions = {
-		*beginFreeTransaction( payload ) {
-			let response;
-			try {
-				response = yield {
-					type: 'FREE_TRANSACTION_BEGIN',
-					payload,
-				};
-				debug( 'free transaction complete', response );
-			} catch ( error ) {
-				debug( 'free transaction had an error', error.message );
-				return { type: 'FREE_PURCHASE_TRANSACTION_ERROR', payload: error.message };
-			}
-			debug( 'free transaction requires is successful' );
-			return { type: 'FREE_PURCHASE_TRANSACTION_END', payload: response };
-		},
-	};
-
-	const selectors = {
-		getTransactionError( state ) {
-			return state.transactionError;
-		},
-		getTransactionStatus( state ) {
-			return state.transactionStatus;
-		},
-	};
-
-	registerStore( 'free-purchase', {
-		reducer(
-			state = {
-				transactionStatus: null,
-				transactionError: null,
-			},
-			action
-		) {
-			switch ( action.type ) {
-				case 'FREE_PURCHASE_TRANSACTION_END':
-					return {
-						...state,
-						transactionStatus: 'complete',
-					};
-				case 'FREE_PURCHASE_TRANSACTION_ERROR':
-					return {
-						...state,
-						transactionStatus: 'error',
-						transactionError: action.payload,
-					};
-			}
-			return state;
-		},
-		actions,
-		selectors,
-		controls: {
-			FREE_TRANSACTION_BEGIN( action ) {
-				return submitTransaction( action.payload );
-			},
-		},
-	} );
-
+export function createFreePaymentMethod() {
 	return {
 		id: 'free-purchase',
 		label: <FreePurchaseLabel />,
@@ -102,17 +43,17 @@ function FreePurchaseLabel() {
 
 function FreePurchaseSubmitButton( { disabled } ) {
 	const localize = useLocalize();
-	const { beginFreeTransaction } = useDispatch( 'free-purchase' );
 	const [ items, total ] = useLineItems();
-	const transactionStatus = useSelect( ( select ) =>
-		select( 'free-purchase' ).getTransactionStatus()
-	);
-	const transactionError = useSelect( ( select ) =>
-		select( 'free-purchase' ).getTransactionError()
-	);
+	const {
+		transactionStatus,
+		transactionError,
+		setTransactionComplete,
+		setTransactionError,
+	} = useTransactionStatus();
 	const { showErrorMessage } = useMessages();
 	const { formStatus, setFormReady, setFormComplete, setFormSubmitting } = useFormStatus();
 	const onEvent = useEvents();
+	const submitTransaction = usePaymentProcessor( 'free-purchase' );
 
 	useEffect( () => {
 		if ( transactionStatus === 'error' ) {
@@ -139,9 +80,15 @@ function FreePurchaseSubmitButton( { disabled } ) {
 	const onClick = () => {
 		setFormSubmitting();
 		onEvent( { type: 'FREE_TRANSACTION_BEGIN' } );
-		beginFreeTransaction( {
+		submitTransaction( {
 			items,
-		} );
+		} )
+			.then( () => {
+				setTransactionComplete();
+			} )
+			.catch( ( error ) => {
+				setTransactionError( error.message );
+			} );
 	};
 
 	return (
