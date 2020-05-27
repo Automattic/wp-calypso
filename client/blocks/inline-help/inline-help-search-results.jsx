bc4c16e26f13a8e0a4515cbc757e562c20241f59
Dave Smith <getdavemail@gmail.com>
2020-05-27 10:37:47 +0100

    Update to simplify syntax.
    Addresses https://github.com/Automattic/wp-calypso/pull/42625#discussion_r430743178

--- client/blocks/inline-help/inline-help-search-results.jsx
+++ client/blocks/inline-help/inline-help-search-results.jsx
@@ -57,7 +57,7 @@ class InlineHelpSearchResults extends Component {
 	}
 
 	onHelpLinkClick = ( selectionIndex ) => ( event ) => {
-		const selectedResult = this.props.searchResults && this.props.searchResults[ selectionIndex ];
+		const selectedResult = this.props?.searchResults?.[ selectionIndex ] ?? null;
 		this.props.selectResult( selectionIndex );
 		this.props.openResult( event, selectedResult );
 	};
