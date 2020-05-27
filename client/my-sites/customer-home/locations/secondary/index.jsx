6397d21554fe8ffdb5ea7ee413a879a78a2d8a76
Don Lair <don.lair@gmail.com>
2020-05-27 17:43:18 -0400

    Removing references to the Discover Earn section.

--- client/my-sites/customer-home/locations/secondary/index.jsx
+++ client/my-sites/customer-home/locations/secondary/index.jsx
@@ -8,17 +8,11 @@ import React from 'react';
  */
 import Stats from 'my-sites/customer-home/cards/features/stats';
 import LearnGrow from './learn-grow';
-import DiscoverEarn from './discover-earn';
-import {
-	FEATURE_STATS,
-	SECTION_LEARN_GROW,
-	SECTION_DISCOVER_EARN,
-} from 'my-sites/customer-home/cards/constants';
+import { FEATURE_STATS, SECTION_LEARN_GROW } from 'my-sites/customer-home/cards/constants';
 
 const cardComponents = {
 	[ FEATURE_STATS ]: Stats,
 	[ SECTION_LEARN_GROW ]: LearnGrow,
-	[ SECTION_DISCOVER_EARN ]: DiscoverEarn,
 };
 
 const Secondary = ( { cards } ) => {
