b5b3705713be57674581c16923a7877432305184
mmtr <miguel.torres@automattic.com>
2020-05-28 16:08:17 +0200

    Premium content: Add paid flag

--- apps/full-site-editing/full-site-editing-plugin/premium-content/index.js
+++ apps/full-site-editing/full-site-editing-plugin/premium-content/index.js
@@ -1,9 +1,15 @@
+/* eslint-disable wpcalypso/import-docblock */
 /**
  * WordPress dependencies
  */
-import { registerBlockType } from '@wordpress/blocks';
+import apiFetch from '@wordpress/api-fetch';
+import { getBlockType, registerBlockType, unregisterBlockType } from '@wordpress/blocks';
+import { _x } from '@wordpress/i18n';
+/* eslint-enable wpcalypso/import-docblock */
 
-// Register Blocks
+/**
+ * Internal dependencies
+ */
 import * as container from './blocks/container';
 import * as subscriberView from './blocks/subscriber-view';
 import * as loggedOutView from './blocks/logged-out-view';
@@ -13,7 +19,7 @@ import * as loggedOutView from './blocks/logged-out-view';
  *
  * @typedef {import('@wordpress/blocks').BlockConfiguration} BlockConfiguration
  *
- * @typedef {Object} Block
+ * @typedef {object} Block
  * @property {string} name
  * @property {string} category
  * @property {BlockConfiguration} settings
@@ -34,10 +40,39 @@ const registerBlock = ( block ) => {
 };
 
 /**
+ * Appends a "paid" tag to the Premium Content block title if site requires an upgrade.
+ */
+const addPaidBlockFlags = async () => {
+	const membershipsStatus = await apiFetch( { path: '/wpcom/v2/memberships/status' } );
+	const shouldUpgrade = membershipsStatus.should_upgrade_to_access_memberships;
+	if ( shouldUpgrade ) {
+		const premiumContentBlock = getBlockType( container.name );
+		if ( ! premiumContentBlock ) {
+			return;
+		}
+
+		const paidFlag = _x(
+			'paid',
+			'Short label appearing near a block requiring a paid plan',
+			'premium-content'
+		);
+
+		unregisterBlockType( container.name );
+		registerBlockType( container.name, {
+			...premiumContentBlock,
+			title: `${ premiumContentBlock.title } (${ paidFlag })`,
+		} );
+	}
+};
+
+/**
  * Function to register blocks provided by CoBlocks.
  */
 export const registerPremiumContentBlocks = () => {
 	[ container, loggedOutView, subscriberView ].forEach( registerBlock );
+
+	// Done after blocks are registered so the status API request doesn't suspend the execution.
+	addPaidBlockFlags();
 };
 
 registerPremiumContentBlocks();
