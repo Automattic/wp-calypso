32616daf2811a20a30a7549e7f8f109ce18f172b
Don Lair <don.lair@gmail.com>
2020-05-29 12:43:26 -0400

    Adding Business illustration to the secondary card and finalizing copy edits.

--- client/my-sites/customer-home/cards/education/earn/index.jsx
+++ client/my-sites/customer-home/cards/education/earn/index.jsx
@@ -4,7 +4,6 @@
 import React from 'react';
 import { useTranslate } from 'i18n-calypso';
 import { connect } from 'react-redux';
-import { Button } from '@automattic/components';
 
 /**
  * Internal dependencies
