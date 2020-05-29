dc9f4e65226e838f01084ad5937d6dfd94e8d9b9
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-05-29 13:23:13 +0530

    Create white glove AB test

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -136,4 +136,13 @@ export default {
 		localeTargets: 'any',
 		countryCodeTargets: [ 'JP' ],
 	},
+	whiteGloveUpsell: {
+		datestamp: '20200526',
+		variations: {
+			variantShowOffer: 50,
+			control: 50,
+		},
+		defaultVariation: 'control',
+		allowExistingUsers: false,
+	},
 };
