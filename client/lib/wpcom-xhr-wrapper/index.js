d46489e58df13d5efe7a26dd0b1b87a41ce92c78
Sérgio Gomes <mail@sgomes.com>
2020-05-29 16:37:55 +0100

    Fix circular dependency in `wpcom-xhr-wrapper` (#42785)
    `lib/wpcom-xhr-wrapper` was introducing a circular dependency through
    `lib/user/utils`:
    
    ```
    client/lib/user/utils.js -> client/lib/user/index.js ->
    client/lib/user/user.js -> client/lib/user/support-user-interop.js ->
    client/lib/wp/browser.js -> client/lib/wpcom-xhr-wrapper/index.js ->
    client/lib/user/utils.js
    ```
    
    Since `lib/wpcom-xhr-wrapper` is all async anyway, we can load
    `lib/user/utils` dynamically, and avoid the circular dependency.

--- client/lib/wpcom-xhr-wrapper/index.js
+++ client/lib/wpcom-xhr-wrapper/index.js
@@ -8,15 +8,18 @@ import debugModule from 'debug';
  */
 const debug = debugModule( 'calypso:wpcom-xhr-wrapper' );
 
-export default function ( params, callback ) {
-	return import( /* webpackChunkName: "wpcom-xhr-request" */ 'wpcom-xhr-request' ).then( ( xhr ) =>
-		xhr.default( params, function ( error, response, headers ) {
-			if ( error && error.name === 'InvalidTokenError' ) {
-				debug( 'Invalid token error detected, authorisation probably revoked - logging out' );
-				require( 'lib/user/utils' ).logout();
-			}
+export default async function ( params, callback ) {
+	const xhr = ( await import( /* webpackChunkName: "wpcom-xhr-request" */ 'wpcom-xhr-request' ) )
+		.default;
+	const userUtils = ( await import( /* webpackChunkName: "lib-user-utils" */ 'lib/user/utils' ) )
+		.default;
 
-			callback( error, response, headers );
-		} )
-	);
+	return xhr( params, function ( error, response, headers ) {
+		if ( error && error.name === 'InvalidTokenError' ) {
+			debug( 'Invalid token error detected, authorisation probably revoked - logging out' );
+			userUtils.logout();
+		}
+
+		callback( error, response, headers );
+	} );
 }
