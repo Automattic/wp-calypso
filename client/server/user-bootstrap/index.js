Automattic/wp-calypso

commit 7d4846c49a28d60aa9506ba507e9d64136e8672b

Use ES modules in server sources (#42643)
* Sections Loader: output module with ESM exports

* Devdocs: turn selectors API router into ESM, use lodash.once

* Server: convert (most) modules to ESM

* Remove modules setting from Babel preset env, always use default

* Server: remove some conditional require calls in favor of static imports

client/server/user-bootstrap/index.js
@@ -32,10 +32,9 @@ const debug = debugFactory( 'calypso:bootstrap' ),
  * Requests the current user for user bootstrap.
  *
  * @param {object} request An Express request.
- *
  * @returns {Promise<object>} A promise for a user object.
  */
-module.exports = async function ( request ) {
+export default async function getBootstrappedUser( request ) {
 	const authCookieValue = request.cookies[ AUTH_COOKIE_NAME ];
 	const geoCountry = request.get( 'x-geoip-country-code' ) || '';
 	const supportSessionHeader = request.get( 'x-support-session' );
@@ -111,4 +110,4 @@ module.exports = async function ( request ) {
 
 		throw error;
 	}
-};
+}

client/server/api/index.js
@@ -1,17 +1,17 @@
 /**
  * External dependencies
  */
-const express = require( 'express' );
+import express from 'express';
 
 /**
  * Internal dependencies
  */
-const { version } = require( '../../package.json' );
-const config = require( 'config' );
-const oauth = require( './oauth' );
-const signInWithApple = require( './sign-in-with-apple' );
+import { version } from '../../package.json';
+import config from 'config';
+import oauth from './oauth';
+import signInWithApple from './sign-in-with-apple';
 
-module.exports = function () {
+export default function api() {
 	const app = express();
 
 	app.get( '/version', function ( request, response ) {
@@ -27,4 +27,4 @@ module.exports = function () {
 	}
 
 	return app;
-};
+}

client/server/boot/index.js
@@ -1,24 +1,28 @@
 /**
- * Module dependencies
+ * External dependencies
  */
+import path from 'path';
+import chalk from 'chalk';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import userAgent from 'express-useragent';
+import morgan from 'morgan';
 
-const path = require( 'path' );
-const chalk = require( 'chalk' );
-const express = require( 'express' );
-const cookieParser = require( 'cookie-parser' );
-const userAgent = require( 'express-useragent' );
-const morgan = require( 'morgan' );
-const config = require( 'server/config' );
-const pages = require( 'server/pages' );
-const pwa = require( 'server/pwa' ).default;
-const analytics = require( 'server/lib/analytics' ).default;
+/**
+ * Internal dependencies
+ */
+import analytics from 'server/lib/analytics';
+import config from 'server/config';
+import api from 'server/api';
+import pages from 'server/pages';
+import pwa from 'server/pwa';
 
 /**
  * Returns the server HTTP request handler "app".
  *
  * @returns {object} The express app
  */
-function setup() {
+export default function setup() {
 	const app = express();
 
 	// for nginx
@@ -91,7 +95,7 @@ function setup() {
 	} );
 
 	if ( config.isEnabled( 'devdocs' ) ) {
-		app.use( require( 'server/devdocs' )() );
+		app.use( require( 'server/devdocs' ).default() );
 	}
 
 	if ( config.isEnabled( 'desktop' ) ) {
@@ -101,15 +105,10 @@ function setup() {
 		);
 	}
 
-	app.use( require( 'server/api' )() );
+	app.use( api() );
 
 	// attach the pages module
 	app.use( pages() );
 
 	return app;
 }
-
-/**
- * Module exports
- */
-module.exports = setup;

client/server/devdocs/index.js
@@ -16,7 +16,7 @@ import 'prismjs/components/prism-scss';
  */
 import config from 'config';
 import searchIndex from 'server/devdocs/search-index';
-import selectors from './selectors';
+import { primeSelectorsCache, selectorsRouter } from './selectors';
 
 const docsIndex = lunr.Index.load( searchIndex.index );
 
@@ -135,7 +135,7 @@ function defaultSnippet( doc ) {
 	return escapeHTML( content ) + '…';
 }
 
-module.exports = function () {
+export default function devdocs() {
 	const app = express();
 
 	// this middleware enforces access control
@@ -210,11 +210,11 @@ module.exports = function () {
 	// In environments where enabled, prime the selectors search cache whenever
 	// a request is made for DevDocs
 	app.use( '/devdocs', function ( request, response, next ) {
-		selectors.prime();
+		primeSelectorsCache();
 		next();
 	} );
 
-	app.use( '/devdocs/service/selectors', selectors.router );
+	app.use( '/devdocs/service/selectors', selectorsRouter );
 
 	return app;
-};
+}

client/server/devdocs/selectors/index.js
@@ -1,15 +1,12 @@
 /**
  * External dependencies
  */
-
-const lodash = require( 'lodash' );
-const camelCase = lodash.camelCase;
-const forEach = lodash.forEach;
-const fs = require( 'fs' );
-const path = require( 'path' );
-const express = require( 'express' );
-const Fuse = require( 'fuse.js' );
-const doctrine = require( 'doctrine' );
+import { camelCase, forEach, once } from 'lodash';
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+import Fuse from 'fuse.js';
+import doctrine from 'doctrine';
 
 /**
  * Constants
@@ -17,13 +14,6 @@ const doctrine = require( 'doctrine' );
 
 const REGEXP_DOCBLOCKS = /\/\*\* *\n( *\*.*\n)* *\*\//g;
 const SELECTORS_DIR = path.resolve( __dirname, '../../../../client/state/selectors' );
-
-/**
- * Module variables
- */
-
-const router = express.Router();
-let prepareFuse;
 
 function parseSelectorFile( file ) {
 	return new Promise( ( resolve, reject ) => {
@@ -49,12 +39,8 @@ function parseSelectorFile( file ) {
 	} );
 }
 
-function prime() {
-	if ( prepareFuse ) {
-		return;
-	}
-
-	prepareFuse = new Promise( ( resolve ) => {
+export const primeSelectorsCache = once( () => {
+	return new Promise( ( resolve ) => {
 		fs.readdir( SELECTORS_DIR, ( error, files ) => {
 			if ( error ) {
 				files = [];
@@ -85,14 +71,13 @@ function prime() {
 				);
 			} );
 		} );
-	} ).then( ( fuse ) => {
-		prepareFuse = Promise.resolve( fuse );
-		return fuse;
 	} );
-}
+} );
 
-router.get( '/', ( request, response ) => {
-	prepareFuse
+export const selectorsRouter = express.Router();
+
+selectorsRouter.get( '/', ( request, response ) => {
+	primeSelectorsCache()
 		.then( ( fuse ) => {
 			let results;
 			if ( request.query.search ) {
@@ -107,6 +92,3 @@ router.get( '/', ( request, response ) => {
 			response.status( 500 ).json( error );
 		} );
 } );
-
-module.exports.prime = prime;
-module.exports.router = router;

client/server/index.js
@@ -1,19 +1,19 @@
 /* eslint-disable no-console */
 /* eslint-disable import/no-nodejs-modules */
 
-require( '@automattic/calypso-polyfills' );
+import '@automattic/calypso-polyfills';
 
 /**
  * External dependencies.
  */
-const chalk = require( 'chalk' );
-const fs = require( 'fs' );
+import chalk from 'chalk';
+import fs from 'fs';
 
 /**
  * Internal dependencies
  */
-const boot = require( './boot' );
-const config = require( './config' );
+import boot from './boot';
+import config from './config';
 
 const start = Date.now();
 const protocol = process.env.PROTOCOL || config( 'protocol' );

client/server/pages/index.js
@@ -45,6 +45,7 @@ import {
 	attachI18n,
 } from 'server/render';
 import stateCache from 'server/state-cache';
+import getBootstrappedUser from 'server/user-bootstrap';
 import { createReduxStore } from 'state';
 import { setStore } from 'state/redux-store';
 import initialReducer from 'state/reducer';
@@ -476,13 +477,11 @@ function setUpLoggedInRoute( req, res, next ) {
 			return;
 		}
 
-		const user = require( 'server/user-bootstrap' );
-
 		start = new Date().getTime();
 
 		debug( 'Issuing API call to fetch user object' );
 
-		const userPromise = user( req )
+		const userPromise = getBootstrappedUser( req )
 			.then( ( data ) => {
 				const end = new Date().getTime() - start;
 
@@ -710,7 +709,7 @@ function handleLocaleSubdomains( req, res, next ) {
 	next();
 }
 
-module.exports = function () {
+export default function pages() {
 	const app = express();
 
 	app.set( 'views', __dirname );
@@ -944,11 +943,8 @@ module.exports = function () {
 		}
 
 		// Maybe not logged in, note that you need docker to test this properly
-		const user = require( 'server/user-bootstrap' );
-
 		debug( 'Issuing API call to fetch user object' );
-
-		user( req )
+		getBootstrappedUser( req )
 			.then( ( data ) => {
 				const activeFlags = get( data, 'meta.data.flags.active_flags', [] );
 
@@ -958,7 +954,7 @@ module.exports = function () {
 				}
 
 				// Passed all checks, prepare support user session
-				return res.send(
+				res.send(
 					renderJsx( 'support-user', {
 						authorized: true,
 						supportUser: req.query.support_user,
@@ -974,7 +970,7 @@ module.exports = function () {
 					domain: '.wordpress.com',
 				} );
 
-				return res.send( renderJsx( 'support-user' ) );
+				res.send( renderJsx( 'support-user' ) );
 			} );
 	} );
 
@@ -985,4 +981,4 @@ module.exports = function () {
 	app.use( renderServerError() );
 
 	return app;
-};
+}

babel.config.js
@@ -1,12 +1,9 @@
 const isBrowser = process.env.BROWSERSLIST_ENV !== 'server';
 
-// Use commonjs for Node
-const modules = isBrowser ? false : 'commonjs';
-
 // We implicitly use browserslist configuration in package.json for build targets.
 
 const babelConfig = {
-	presets: [ [ '@automattic/calypso-build/babel/default', { modules } ] ],
+	presets: [ '@automattic/calypso-build/babel/default' ],
 	plugins: [ [ '@automattic/transform-wpcalypso-async', { async: isBrowser } ] ],
 	env: {
 		production: {

commit 97eee8a778c2d64477f79cee3e17ad8cfc7d7a35

Fix API key errors on local dev builds.
Local dev builds fail on missing config keys, and these keys are now
being fetched unconditionally on boot. This PR delays their load to
when strictly needed, which shouldn't happen on local dev builds.

Issue introduced in #42643.

client/server/user-bootstrap/index.js
@@ -14,19 +14,20 @@ import { getActiveTestNames } from 'lib/abtest/utility';
 import config from 'config';
 
 const debug = debugFactory( 'calypso:bootstrap' ),
-	API_KEY = config( 'wpcom_calypso_rest_api_key' ),
 	AUTH_COOKIE_NAME = 'wordpress_logged_in',
 	SUPPORT_SESSION_COOKIE_NAME = 'support_session_id',
 	/**
 	 * WordPress.com REST API /me endpoint.
 	 */
-	SUPPORT_SESSION_API_KEY = config( 'wpcom_calypso_support_session_rest_api_key' ),
 	API_PATH = 'https://public-api.wordpress.com/rest/v1/me',
 	apiQuery = {
 		meta: 'flags',
 		abtests: getActiveTestNames( { appendDatestamp: true, asCSV: true } ),
 	},
 	url = `${ API_PATH }?${ stringify( apiQuery ) }`;
+
+const getApiKey = () => config( 'wpcom_calypso_rest_api_key' );
+const getSupportSessionApiKey = () => config( 'wpcom_calypso_support_session_rest_api_key' );
 
 /**
  * Requests the current user for user bootstrap.
@@ -66,24 +67,27 @@ export default async function getBootstrappedUser( request ) {
 	req.set( 'Cookie', cookies.join( '; ' ) );
 
 	if ( supportSessionHeader ) {
-		if ( typeof SUPPORT_SESSION_API_KEY !== 'string' ) {
+		const supportSessionApiKey = getSupportSessionApiKey();
+		if ( typeof supportSessionApiKey !== 'string' ) {
 			throw new Error(
 				'Unable to boostrap user because of invalid SUPPORT SESSION API key in secrets.json'
 			);
 		}
 
-		const hmac = crypto.createHmac( 'md5', SUPPORT_SESSION_API_KEY );
+		const hmac = crypto.createHmac( 'md5', supportSessionApiKey );
 		hmac.update( supportSessionHeader );
 		const hash = hmac.digest( 'hex' );
 
 		req.set( 'Authorization', `X-WPCALYPSO-SUPPORT-SESSION ${ hash }` );
 		req.set( 'x-support-session', supportSessionHeader );
 	} else {
-		if ( typeof API_KEY !== 'string' ) {
+		const apiKey = getApiKey();
+
+		if ( typeof apiKey !== 'string' ) {
 			throw new Error( 'Unable to boostrap user because of invalid API key in secrets.json' );
 		}
 
-		const hmac = crypto.createHmac( 'md5', API_KEY );
+		const hmac = crypto.createHmac( 'md5', apiKey );
 		hmac.update( decodedAuthCookieValue );
 		const hash = hmac.digest( 'hex' );
 