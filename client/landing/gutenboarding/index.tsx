bf7390505906cfbb523178f816687d5d46884ff7
Omar Alshaker <omar@omaralshaker.com>
2020-05-30 17:18:19 +0200

    This commit shouldn't trigger building packages, either

--- client/landing/gutenboarding/index.tsx
+++ client/landing/gutenboarding/index.tsx
@@ -39,7 +39,7 @@ import { addHotJarScript } from 'lib/analytics/hotjar';
 import 'assets/stylesheets/gutenboarding.scss';
 import 'components/environment-badge/style.scss';
 
-// TODO: Remove when all needed core types are available
+// TODO: remove when all needed core types are available
 /*#__PURE__*/ import './gutenberg-types-patch';
 
 function generateGetSuperProps() {
