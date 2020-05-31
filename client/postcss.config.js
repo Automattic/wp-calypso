f0b1b4a12ad0adf0f679a5edc2c47de32ef47cd5
Omar Alshaker <omar@omaralshaker.com>
2020-05-31 16:36:24 +0200

    Disable PostCSS-custom-properties in development

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,8 +1,19 @@
-module.exports = () => ( {
-	plugins: {
-		'postcss-custom-properties': {
-			importFrom: [ require.resolve( '@automattic/calypso-color-schemes' ) ],
+const config = require( './server/config' );
+const bundleEnv = config( 'env' );
+
+if ( bundleEnv === 'development' ) {
+	module.exports = () => ( {
+		plugins: {
+			autoprefixer: {},
 		},
-		autoprefixer: {},
-	},
-} );
+	} );
+} else {
+	module.exports = () => ( {
+		plugins: {
+			'postcss-custom-properties': {
+				importFrom: [ require.resolve( '@automattic/calypso-color-schemes' ) ],
+			},
+			autoprefixer: {},
+		},
+	} );
+}
