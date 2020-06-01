4c67bd8d0ef652adfcb5be1310b3b2d2fabe7dbe
Payton Swick <payton@foolord.com>
2020-06-01 17:08:31 -0400

    Replace build-packages:watch to only rebuild changed package

---
+++ bin/build-package-containing-file.js
@@ -0,0 +1,39 @@
+const fs = require( 'fs' );
+const { spawnSync } = require( 'child_process' );
+
+const args = process.argv.slice( 2 );
+if ( args.length < 1 ) {
+	console.error( 'failed to rebuild package: at least one filename must be provided' );
+	process.exit( 1 );
+}
+const fileName = args[ 0 ];
+if ( ! fileName ) {
+	console.error( 'failed to rebuild package: missing filename' );
+	process.exit( 1 );
+}
+const packageDirectoryMatches = fileName.match( /^packages\/[^/]+/ );
+if ( packageDirectoryMatches.length < 1 ) {
+	console.error( 'failed to rebuild package: missing package directory' );
+	process.exit( 1 );
+}
+const packageDirectory = packageDirectoryMatches[ 0 ];
+if ( ! packageDirectory ) {
+	console.error( 'failed to rebuild package: missing package directory contents' );
+	process.exit( 1 );
+}
+const packageJSONFile = packageDirectory + '/package.json';
+const rawData = fs.readFileSync( packageJSONFile );
+const parsedData = JSON.parse( rawData );
+const packageName = parsedData.name;
+if ( ! packageName ) {
+	console.error( 'failed to rebuild package: missing package name' );
+	process.exit( 1 );
+}
+const buildResult = spawnSync( 'yarn', [ 'lerna run prepare', `--scope="${ packageName }"` ], {
+	shell: true,
+	stdio: 'inherit',
+} );
+if ( buildResult.status ) {
+	console.error( 'failed to rebuild package: exited with code %d', buildResult.status );
+	process.exit( buildResult.status );
+}
