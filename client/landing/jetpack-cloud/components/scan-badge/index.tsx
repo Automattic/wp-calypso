564726d9ce29b0da09ee42f534344cb59f06e938
Matt Gawarecki <matt.gawarecki@gmail.com>
2020-06-01 12:40:49 -0500

    Jetpack Cloud: Collapse the sidebar 'threats' badge on narrower layouts

--- client/landing/jetpack-cloud/components/scan-badge/index.tsx
+++ client/landing/jetpack-cloud/components/scan-badge/index.tsx
@@ -8,7 +8,9 @@ import { isNumber } from 'lodash';
 /**
  * Internal dependencies
  */
+import { ScreenReaderText } from '@automattic/components';
 import Badge from 'components/badge';
+import './style.scss';
 
 interface Props {
 	numberOfThreatsFound: number;
@@ -34,14 +36,33 @@ const ScanBadge: FunctionComponent< Props > = ( { numberOfThreatsFound, progress
 	}
 
 	if ( numberOfThreatsFound ) {
+		// We selectively hide the word "threats" based on screen width,
+		// but it should always remain visible to screen readers
 		return (
 			<Badge type="error">
-				{ translate( '%(number)d threat', '%(number)d threats', {
-					count: numberOfThreatsFound,
-					args: {
-						number: numberOfThreatsFound,
-					},
-				} ) }
+				<span aria-hidden="true">
+					{ translate(
+						'%(number)d {{span}}threat{{/span}}',
+						'%(number)d {{span}}threats{{/span}}',
+						{
+							count: numberOfThreatsFound,
+							args: {
+								number: numberOfThreatsFound,
+							},
+							components: {
+								span: <span className="scan-badge__words" />,
+							},
+						}
+					) }
+				</span>
+				<ScreenReaderText>
+					{ translate( '%(number)d threat', '%(number)d threats', {
+						count: numberOfThreatsFound,
+						args: {
+							number: numberOfThreatsFound,
+						},
+					} ) }
+				</ScreenReaderText>
 			</Badge>
 		);
 	}
