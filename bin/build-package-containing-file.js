7b21e4545fb1036aba991502b1021d8d704cda3e
Payton Swick <payton@foolord.com>
2020-06-02 15:35:48 -0400

    Use path for parsing/building paths

--- bin/build-package-containing-file.js
+++ bin/build-package-containing-file.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const yargs = require( 'yargs' );
 const fs = require( 'fs' );
+const path = require( 'path' );
 const { spawnSync } = require( 'child_process' );
 
 const argv = yargs
@@ -14,22 +15,28 @@ if ( args.length < 1 ) {
 	console.error( 'failed to rebuild package: at least one filename must be provided' );
 	process.exit( 1 );
 }
-const fileName = args[ 0 ];
-if ( ! fileName ) {
+const filePath = args[ 0 ];
+if ( ! filePath ) {
 	console.error( 'failed to rebuild package: missing filename' );
 	process.exit( 1 );
 }
-const packageDirectoryMatches = fileName.match( /^packages\/[^/]+/ );
-if ( packageDirectoryMatches.length < 1 ) {
-	console.error( 'failed to rebuild package: missing package directory' );
+
+const relativeFilePath = path.relative( '.', filePath );
+const relativeFilePathPieces = relativeFilePath.split( path.sep );
+if ( relativeFilePathPieces.length < 2 ) {
+	console.error( 'failed to rebuild package: file is outside packages directory' );
 	process.exit( 1 );
 }
-const packageDirectory = packageDirectoryMatches[ 0 ];
-if ( ! packageDirectory ) {
-	console.error( 'failed to rebuild package: missing package directory contents' );
+if ( relativeFilePathPieces[ 0 ] !== 'packages' ) {
+	console.error( 'failed to rebuild package: file is outside packages directory' );
 	process.exit( 1 );
 }
-const packageJSONFile = packageDirectory + '/package.json';
+
+const packageJSONFile = path.join(
+	relativeFilePathPieces[ 0 ],
+	relativeFilePathPieces[ 1 ],
+	'package.json'
+);
 const rawData = fs.readFileSync( packageJSONFile );
 const parsedData = JSON.parse( rawData );
 const packageName = parsedData.name;
@@ -37,6 +44,7 @@ if ( ! packageName ) {
 	console.error( 'failed to rebuild package: missing package name' );
 	process.exit( 1 );
 }
+console.log( 'rebuilding package:', packageName );
 const buildResult = spawnSync( 'yarn', [ 'workspace', `${ packageName }`, 'prepare' ], {
 	shell: true,
 	stdio: 'inherit',
