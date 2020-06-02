7fa1ba0bb61e8d15442257490480a9d472273877
Copons <Copons@users.noreply.github.com>
2020-06-02 11:28:45 +0100

    Convert Dialog to Core Modal component

--- client/components/convert-to-blocks/index.jsx
+++ client/components/convert-to-blocks/index.jsx
@@ -6,9 +6,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
 
 /**
+ * WordPress dependencies
+ */
+import { Button, Modal } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
-import { Dialog } from '@automattic/components';
 import isConvertToBlocksDialogDismissed from 'state/selectors/is-convert-to-blocks-dialog-dismissed';
 import { dismissConvertToBlocksDialog } from 'state/ui/convert-to-blocks-dialog/actions';
 import { getSelectedSiteId } from 'state/ui/selectors';
@@ -16,6 +20,7 @@ import { getSelectedSiteId } from 'state/ui/selectors';
 /**
  * Style dependencies
  */
+import '@wordpress/components/build-style/style.css';
 import './style.scss';
 
 export default function ConvertToBlocksDialog( { handleResponse, postId, showDialog } ) {
@@ -29,40 +34,34 @@ export default function ConvertToBlocksDialog( { handleResponse, postId, showDia
 	const dispatch = useDispatch();
 	const translate = useTranslate();
 
-	if ( isDismissed ) {
+	if ( ! showDialog || isDismissed ) {
 		return null;
 	}
 
-	const onClose = ( action ) => {
+	const onClose = ( action ) => () => {
 		handleResponse( action === 'convert' );
 		dispatch( dismissConvertToBlocksDialog( siteId, postId ) );
 	};
 
-	const buttons = [
-		{
-			action: 'convert',
-			isPrimary: true,
-			label: translate( 'Convert to Blocks' ),
-		},
-		{
-			action: 'cancel',
-			label: translate( 'Stick with the Classic Block' ),
-		},
-	];
-
 	return (
-		<Dialog
-			additionalClassNames="editor__gutenberg-convert-blocks-dialog"
-			buttons={ buttons }
-			isVisible={ showDialog }
-			onClose={ onClose }
+		<Modal
+			onRequestClose={ onClose( 'cancel' ) }
+			shouldCloseOnClickOutside={ false }
+			title={ translate( 'Convert to blocks?' ) }
 		>
-			<h1>{ translate( 'Convert to blocks?' ) }</h1>
 			<p>
 				{ translate(
-					'This post contains content you created with the classic editor. You can convert this content into blocks — each paragraph and item will be its own block, that you can edit and move around — or add a Classic block to simulate the classic editor.'
+					'This post contains content you created with the classic editor. You can convert this content into blocks — each paragraph and item will be its own block that you can edit and move around — or add a Classic block to simulate the classic editor.'
 				) }
 			</p>
-		</Dialog>
+			<div className="convert-to-blocks__dialog-actions">
+				<Button onClick={ onClose( 'convert' ) } isPrimary>
+					{ translate( 'Convert to Blocks' ) }
+				</Button>
+				<Button onClick={ onClose( 'cancel' ) }>
+					{ translate( 'Stick with the Classic Block' ) }
+				</Button>
+			</div>
+		</Modal>
 	);
 }
