33a2b220c4be37d247be42b04ef4d825bac71812
Ben Dwyer <ben@scruffian.com>
2020-06-03 16:38:31 +0100

    only load the classic editor if they have that one set

--- client/state/selectors/get-editor-url.js
+++ client/state/selectors/get-editor-url.js
@@ -8,14 +8,19 @@ import { getEditorPath } from 'state/ui/editor/selectors';
 import inEditorDeprecationGroup from 'state/editor-deprecation-group/selectors/in-editor-deprecation-group';
 import { isEnabled } from 'config';
 import getWpAdminClassicEditorRedirectionUrl from 'state/selectors/get-wp-admin-classic-editor-redirection-url';
+import { getSelectedEditor } from 'state/selectors/get-selected-editor';
 
 export const getEditorUrl = ( state, siteId, postId = null, postType = 'post' ) => {
-	if ( isEnabled( 'editor/after-deprecation' ) && inEditorDeprecationGroup( state ) ) {
-		return getWpAdminClassicEditorRedirectionUrl( state, siteId, postId );
-	}
-
 	if ( shouldLoadGutenberg( state, siteId ) ) {
 		return getGutenbergEditorUrl( state, siteId, postId, postType );
+	}
+
+	if (
+		getSelectedEditor( state, siteId ) === 'classic' &&
+		isEnabled( 'editor/after-deprecation' ) &&
+		inEditorDeprecationGroup( state )
+	) {
+		return getWpAdminClassicEditorRedirectionUrl( state, siteId, postId );
 	}
 
 	if ( postId ) {
