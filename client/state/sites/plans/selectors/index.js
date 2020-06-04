cb78a597cb3f69f009dcf5a90d2e00bec42ac877
Sérgio Gomes <mail@sgomes.com>
2020-06-04 14:37:47 +0100

    Modularise sites plans selectors (#41812)
    * Move selectors to their own directory
    
    * Declare selectors side-effect free
    
    * Split out getPlansBySite(Id)
    
    * Split out getCurrentPlan
    
    * Split out getSitePlan
    
    * Split out isSitePlanDiscounted
    
    * Split out getPlanDiscountedRawPrice
    
    * Split out getSitePlanRawPrice
    
    * Split out getPlanRawDiscount
    
    * Split out hasDomainCredit
    
    * Split out isRequestingSitePlans
    
    * Split out isCurrentPlanExpiring
    
    * Split out isCurrentUserCurrentPlanOwner
    
    * Split out getSitePlanSlug
    
    * Split out hasFeature
    
    * Sort exports alphabetically

---
+++ client/state/sites/plans/selectors/index.js
@@ -0,0 +1,13 @@
+export { getCurrentPlan } from 'state/sites/plans/selectors/get-current-plan';
+export { getPlanDiscountedRawPrice } from 'state/sites/plans/selectors/get-plan-discounted-raw-price';
+export { getPlanRawDiscount } from 'state/sites/plans/selectors/get-plan-raw-discount';
+export { getPlansBySite, getPlansBySiteId } from 'state/sites/plans/selectors/get-plans-by-site';
+export { getSitePlan } from 'state/sites/plans/selectors/get-site-plan';
+export { getSitePlanRawPrice } from 'state/sites/plans/selectors/get-site-plan-raw-price';
+export { getSitePlanSlug } from 'state/sites/plans/selectors/get-site-plan-slug';
+export { hasDomainCredit } from 'state/sites/plans/selectors/has-domain-credit';
+export { hasFeature } from 'state/sites/plans/selectors/has-feature';
+export { isCurrentPlanExpiring } from 'state/sites/plans/selectors/is-current-plan-expiring';
+export { isCurrentUserCurrentPlanOwner } from 'state/sites/plans/selectors/is-current-user-current-plan-owner';
+export { isRequestingSitePlans } from 'state/sites/plans/selectors/is-requesting-site-plans';
+export { isSitePlanDiscounted } from 'state/sites/plans/selectors/is-site-plan-discounted';
