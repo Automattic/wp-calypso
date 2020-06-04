b9c508acee5fc45c548aa116f19bf13373316a51
Omar Alshaker <omar@omaralshaker.com>
2020-06-04 12:27:55 +0200

    Move plans UI into a package (#42764)
    * Create `@automattic/plans-grid` package.
    * Use the new package in Gutenboarding to display as modal and as a step.
    * PlansGrid component get `header` as a prop.
    
    Co-authored-by: Razvan Papadopol <razvan.papadopol@automattic.com>

--- client/landing/gutenboarding/onboarding-block/plans/index.tsx
+++ client/landing/gutenboarding/onboarding-block/plans/index.tsx
@@ -5,11 +5,11 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useI18n } from '@automattic/react-i18n';
 import { Button } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
+import PlansGrid from '@automattic/plans-grid';
 
 /**
  * Internal dependencies
  */
-import PlansGrid from '../../components/plans/plans-grid';
 import SignupForm from '../../components/signup-form';
 import Link from '../../components/link';
 import {
@@ -21,6 +21,8 @@ import { useFreeDomainSuggestion } from '../../hooks/use-free-domain-suggestion'
 import { USER_STORE } from '../../stores/user';
 import { STORE_KEY as ONBOARD_STORE } from '../../stores/onboard';
 import { usePath, Step } from '../../path';
+import ActionButtons from '../../components/action-buttons';
+import { Title, SubTitle } from '../../components/titles';
 
 export default function PlansStep() {
 	const { __ } = useI18n();
@@ -54,10 +56,18 @@ export default function PlansStep() {
 		createSite( username, freeDomainSuggestion, bearerToken, shouldSiteBePublic );
 	};
 
-	return (
-		<div className="gutenboarding-page plans">
-			<PlansGrid
-				confirmButton={
+	const header = (
+		<>
+			<div>
+				<Title>{ __( 'Choose a plan' ) }</Title>
+				<SubTitle>
+					{ __(
+						'Pick a plan that’s right for you. Switch plans as your needs change. There’s no risk, you can cancel for a full refund within 30 days.'
+					) }
+				</SubTitle>
+			</div>
+			<ActionButtons
+				primaryButton={
 					<Button
 						isPrimary
 						onClick={ () => {
@@ -67,12 +77,18 @@ export default function PlansStep() {
 						{ __( 'Continue' ) }
 					</Button>
 				}
-				cancelButton={
+				secondaryButton={
 					<Link className="plans__back-link" isLink to={ makePath( Step.Style ) }>
 						{ __( 'Go back' ) }
 					</Link>
 				}
 			/>
+		</>
+	);
+
+	return (
+		<div className="gutenboarding-page plans">
+			<PlansGrid header={ header } currentPlan={ plan } />
 			{ showSignupDialog && <SignupForm onRequestClose={ () => setShowSignupDialog( false ) } /> }
 		</div>
 	);
--- packages/plans-grid/src/plans-grid/index.tsx
+++ packages/plans-grid/src/plans-grid/index.tsx
@@ -6,14 +6,13 @@ import { Button } from '@wordpress/components';
 import { Icon, chevronDown } from '@wordpress/icons';
 import { useDispatch } from '@wordpress/data';
 import { useI18n } from '@automattic/react-i18n';
+import { Plans } from '@automattic/data-stores';
 import classNames from 'classnames';
 
 /**
  * Internal dependencies
  */
-import { PLANS_STORE } from '../../../stores/plans';
-import { Title, SubTitle } from '../../titles';
-import ActionButtons from '../../action-buttons';
+import { Title } from '../titles';
 import PlansTable from '../plans-table';
 import PlansDetails from '../plans-details';
 
@@ -21,20 +20,19 @@ import PlansDetails from '../plans-details';
  * Style dependencies
  */
 import './style.scss';
-import { useSelectedPlan } from 'landing/gutenboarding/hooks/use-selected-plan';
+
+const PLANS_STORE = Plans.register();
 
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#Mobile_Tablet_or_Desktop
 const isMobile = window.navigator.userAgent.indexOf( 'Mobi' ) > -1;
 
 export interface Props {
-	confirmButton: React.ReactElement;
-	cancelButton?: React.ReactElement;
+	header: React.ReactElement;
+	currentPlan: Plans.Plan;
 }
 
-const PlansGrid: React.FunctionComponent< Props > = ( { confirmButton, cancelButton } ) => {
+const PlansGrid: React.FunctionComponent< Props > = ( { header, currentPlan } ) => {
 	const { __ } = useI18n();
-
-	const selectedPlan = useSelectedPlan();
 
 	const { setPlan } = useDispatch( PLANS_STORE );
 
@@ -51,22 +49,12 @@ const PlansGrid: React.FunctionComponent< Props > = ( { confirmButton, cancelBut
 				'show-details': showDetails,
 			} ) }
 		>
-			<div className="plans-grid__header">
-				<div>
-					<Title>{ __( 'Choose a plan' ) }</Title>
-					<SubTitle>
-						{ __(
-							'Pick a plan that’s right for you. Switch plans as your needs change. There’s no risk, you can cancel for a full refund within 30 days.'
-						) }
-					</SubTitle>
-				</div>
-				<ActionButtons primaryButton={ confirmButton } secondaryButton={ cancelButton } />
-			</div>
+			<div className="plans-grid__header">{ header }</div>
 
 			<div className="plans-grid__table">
 				<div className="plans-grid__table-container">
 					<PlansTable
-						selectedPlanSlug={ selectedPlan?.storeSlug ?? '' }
+						selectedPlanSlug={ currentPlan?.storeSlug ?? '' }
 						onPlanSelect={ setPlan }
 					></PlansTable>
 				</div>
