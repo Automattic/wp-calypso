eb30ccfa6c4607be6683672ad1cb280934baa7b9
Payton Swick <payton@foolord.com>
2020-06-03 20:07:28 -0400

    Remove assumption that parseInt recognizes integers

--- client/lib/route/path.ts
+++ client/lib/route/path.ts
@@ -58,9 +58,12 @@ export function getSiteFragment( path: URLString ): SiteSlug | SiteId | false {
 
 	// Check last and second-to-last piece for numeric site ID
 	for ( let i = 2; i > 0; i-- ) {
-		const piece = parseInt( pieces[ pieces.length - i ], 10 );
-		if ( Number.isSafeInteger( piece ) ) {
-			return piece;
+		const piece = pieces[ pieces.length - i ];
+		// We can't just do parseInt because some string look like numbers, eg: '404-hello'
+		const isNumber = String( piece ).match( /\D/ ) === null;
+		const intPiece = parseInt( piece, 10 );
+		if ( isNumber && Number.isSafeInteger( intPiece ) ) {
+			return intPiece;
 		}
 	}
 
