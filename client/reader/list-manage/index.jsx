561b2cacc53c46cc8db7440b3edce85536705663
Ben Lowery <blowery@users.noreply.github.com>
2020-06-08 09:24:41 -0400

    Add scaffolding for reader list editing (#43034)
    Adds in a new route handler for /read/list/:owner/:slug/edit, a config setting to control the feature, re-enables the settings cog and edit URL, and a small placeholder UI for the manage page

---
+++ client/reader/list-manage/index.jsx
@@ -0,0 +1,12 @@
+/**
+ * External dependencies
+ */
+import * as React from 'react';
+
+export default function ReaderListEdit( props ) {
+	return (
+		<div>
+			Editing the list { props.owner } / { props.slug }
+		</div>
+	);
+}
