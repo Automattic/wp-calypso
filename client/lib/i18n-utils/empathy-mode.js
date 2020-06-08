22a4adf3a24ea91d4b8d7cb2c250e316658153da
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2020-06-08 18:16:41 +0300

    Keep default placeholder in sync with i18n changes

--- client/lib/i18n-utils/empathy-mode.js
+++ client/lib/i18n-utils/empathy-mode.js
@@ -5,9 +5,14 @@
 import i18n, { translate } from 'i18n-calypso';
 import interpolateComponents from 'interpolate-components';
 
-const TRANSLATION_PLACEHOLDER = translate( "I don't understand" );
+let defaultUntranslatedPlacehoder = translate( "I don't understand" );
 
-function encodeUntranslatedString( originalString, placeholder = TRANSLATION_PLACEHOLDER ) {
+// keep `defaultUntranslatedPlacehoder` in sync with i18n changes
+i18n.on( 'change', () => {
+	defaultUntranslatedPlacehoder = translate( "I don't understand" );
+} );
+
+function encodeUntranslatedString( originalString, placeholder = defaultUntranslatedPlacehoder ) {
 	let output = placeholder;
 
 	while ( output.length < originalString.length ) {
@@ -21,7 +26,7 @@ export function enableLanguageEmpatyhMode() {
 	// wrap translations from i18n
 	i18n.registerTranslateHook( ( translation, options ) => {
 		const locale = i18n.getLocaleSlug();
-		if ( 'en' === locale ) {
+		if ( 'en' === locale || options.original === defaultUntranslatedPlacehoder ) {
 			return translation;
 		}
 
