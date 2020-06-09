65fee76150648da338168848e1be1778eb0d5344
Kerry Liu <kerryliu@gmail.com>
2020-06-08 17:11:51 -0700

    Home: add generic tracking events for educational cards

--- client/my-sites/customer-home/cards/education/educational-content/index.jsx
+++ client/my-sites/customer-home/cards/education/educational-content/index.jsx
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import { isDesktop } from '@automattic/viewport';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
@@ -10,13 +11,28 @@ import { isDesktop } from '@automattic/viewport';
 import ExternalLink from 'components/external-link';
 import InlineSupportLink from 'components/inline-support-link';
 import Gridicon from 'components/gridicon';
+import {
+	bumpStat,
+	composeAnalytics,
+	recordTracksEvent,
+	withAnalytics,
+} from 'state/analytics/actions';
+import { navigate } from 'state/ui/actions';
 
 /**
  * Style dependencies
  */
 import './style.scss';
 
-const EducationalContent = ( { title, description, links, illustration } ) => {
+const EducationalContent = ( {
+	title,
+	description,
+	links,
+	illustration,
+	cardName,
+	calypsoNavigation,
+	trackExternalClick,
+} ) => {
 	return (
 		<div className="educational-content">
 			<div className="educational-content__wrapper">
@@ -25,30 +41,48 @@ const EducationalContent = ( { title, description, links, illustration } ) => {
 					{ description }
 				</p>
 				<div className="educational-content__links">
-					{ links.map(
-						( { postId, url, calypsoLink, externalLink, text, icon, tracksEvent, statsName } ) => (
-							<div className="educational-content__link" key={ url }>
-								{ icon && <Gridicon icon={ icon } size={ 18 } /> }
-								{ postId && (
-									<InlineSupportLink
-										supportPostId={ postId }
-										supportLink={ url }
-										showIcon={ false }
-										text={ text }
-										tracksEvent={ tracksEvent }
-										statsGroup="calypso_customer_home"
-										statsName={ statsName }
-									/>
-								) }
-								{ externalLink && (
-									<ExternalLink href={ url } icon>
-										{ text }
-									</ExternalLink>
-								) }
-								{ calypsoLink && <a href={ url }>{ text }</a> }
-							</div>
-						)
-					) }
+					{ links.map( ( { postId, url, calypsoLink, externalLink, text, icon } ) => (
+						<div className="educational-content__link" key={ url }>
+							{ icon && <Gridicon icon={ icon } size={ 18 } /> }
+							{ postId && (
+								<InlineSupportLink
+									supportPostId={ postId }
+									supportLink={ url }
+									showIcon={ false }
+									text={ text }
+									tracksEvent="calypso_customer_home_education"
+									statsGroup="calypso_customer_home"
+									tracksOptions={ {
+										url,
+										card_name: cardName,
+									} }
+									statsName={ cardName }
+								/>
+							) }
+							{ externalLink && (
+								<ExternalLink
+									href={ url }
+									onClick={ () => {
+										trackExternalClick( url, cardName );
+									} }
+									icon
+								>
+									{ text }
+								</ExternalLink>
+							) }
+							{ calypsoLink && (
+								<a
+									href={ url }
+									onClick={ ( event ) => {
+										event.preventDefault();
+										calypsoNavigation( url, cardName );
+									} }
+								>
+									{ text }
+								</a>
+							) }
+						</div>
+					) ) }
 				</div>
 			</div>
 			{ isDesktop() && (
@@ -60,4 +94,24 @@ const EducationalContent = ( { title, description, links, illustration } ) => {
 	);
 };
 
-export default EducationalContent;
+const calypsoNavigation = ( url, cardName ) => {
+	return withAnalytics(
+		composeAnalytics(
+			recordTracksEvent( 'calypso_customer_home_education', { url, card_name: cardName } ),
+			bumpStat( 'calypso_customer_home', cardName )
+		),
+		navigate( url )
+	);
+};
+
+const trackExternalClick = ( url, cardName ) => {
+	return composeAnalytics(
+		recordTracksEvent( 'calypso_customer_home_education', { url, card_name: cardName } ),
+		bumpStat( 'calypso_customer_home', cardName )
+	);
+};
+
+export default connect( null, {
+	calypsoNavigation,
+	trackExternalClick,
+} )( EducationalContent );
