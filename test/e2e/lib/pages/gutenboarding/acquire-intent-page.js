1745fa2cc0b960111e0eb10285ce7150d75064c5
Răzvan Papadopol <razvan.papadopol@automattic.com>
2020-06-10 09:33:53 +0300

    [e2e] Update Gutenboarding test flow (#41753)
    * Add site creation Gutenboarding steps to existing e2e test suite.

---
+++ test/e2e/lib/pages/gutenboarding/acquire-intent-page.js
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+import { By } from 'selenium-webdriver';
+
+/**
+ * Internal dependencies
+ */
+import AsyncBaseContainer from '../../async-base-container';
+import * as driverHelper from '../../driver-helper';
+
+export default class AcquireIntentPage extends AsyncBaseContainer {
+	constructor( driver ) {
+		super( driver, By.css( '.acquire-intent' ) );
+	}
+
+	async enterSiteTitle( siteTitle ) {
+		const siteTitleSelector = By.css( '#site-title__input' );
+		return await driverHelper.setWhenSettable( this.driver, siteTitleSelector, siteTitle );
+	}
+
+	async goToNextStep() {
+		const nextButtonSelector = By.css( '.acquire-intent__next' );
+		return await driverHelper.clickWhenClickable( this.driver, nextButtonSelector );
+	}
+
+	async skipStep() {
+		const skipButtonSelector = By.css(
+			this.screenSize === 'MOBILE'
+				? '.acquire-intent__footer .acquire-intent__skip-site-title'
+				: '.site-title__input-wrapper .acquire-intent__skip-site-title'
+		);
+		return await driverHelper.clickWhenClickable( this.driver, skipButtonSelector );
+	}
+}
--- test/e2e/specs/wp-gutenboarding-spec.js
+++ test/e2e/specs/wp-gutenboarding-spec.js
@@ -8,9 +8,14 @@ import assert from 'assert';
  * Internal dependencies
  */
 import LoginFlow from '../lib/flows/login-flow.js';
+import NewPage from '../lib/pages/gutenboarding/new-page.js';
+import AcquireIntentPage from '../lib/pages/gutenboarding/acquire-intent-page.js';
+import DesignSelectorPage from '../lib/pages/gutenboarding/design-selector-page.js';
+import StylePreviewPage from '../lib/pages/gutenboarding/style-preview-page.js';
+import PlansPage from '../lib/pages/gutenboarding/plans-page.js';
 
 import * as driverManager from '../lib/driver-manager.js';
-import GutenboardingPage from '../lib/pages/gutenboarding-page.js';
+import * as dataHelper from '../lib/data-helper.js';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
@@ -26,24 +31,73 @@ before( async function () {
 describe( 'Gutenboarding: (' + screenSize + ')', function () {
 	this.timeout( mochaTimeOut );
 	describe( 'Visit Gutenboarding page as a new user @parallel @canary', function () {
+		const siteTitle = dataHelper.randomPhrase();
+
 		before( async function () {
 			await driverManager.ensureNotLoggedIn( driver );
 		} );
 
 		step( 'Can visit Gutenboarding page and see Onboarding block', async function () {
-			const page = await GutenboardingPage.Visit( driver );
+			const page = await NewPage.Visit( driver );
 			const blockExists = await page.waitForBlock();
-			return assert( blockExists, 'Onboarding block is not rendered' );
+			assert( blockExists, 'Onboarding block is not rendered' );
+		} );
+
+		step( 'Can see Acquire Intent and set site title', async function () {
+			const acquireIntentPage = await AcquireIntentPage.Expect( driver );
+			await acquireIntentPage.enterSiteTitle( siteTitle );
+			await acquireIntentPage.goToNextStep();
+		} );
+
+		step( 'Can see Design Selector and select a random free design', async function () {
+			const designSelectorPage = await DesignSelectorPage.Expect( driver );
+			await designSelectorPage.selectFreeDesign();
+		} );
+
+		step( 'Can see Style Preview and continue', async function () {
+			const stylePreviewPage = await StylePreviewPage.Expect( driver );
+			await stylePreviewPage.continue();
+		} );
+
+		step( 'Can see Plans Grid with no selected plan', async function () {
+			const plansPage = await PlansPage.Expect( driver );
+			const hasSelectedPlan = await plansPage.hasSelectedPlan();
+			assert.strictEqual( hasSelectedPlan, false, 'There is a preselected plan' );
 		} );
 	} );
 
 	describe( 'Visit Gutenboarding page as a logged in user', function () {
 		step( 'Can log in as user', async function () {
 			this.loginFlow = new LoginFlow( driver );
-			return this.loginFlow.login();
+			this.loginFlow.login();
 		} );
 		step( 'Can visit Gutenboarding', async function () {
-			await GutenboardingPage.Visit( driver );
+			await NewPage.Visit( driver );
+		} );
+	} );
+
+	describe( 'Skip first step in Gutenboarding and select paid design @parallel', function () {
+		before( async function () {
+			await driverManager.ensureNotLoggedIn( driver );
+			await NewPage.Visit( driver );
+		} );
+
+		step( 'Can skip Acquire Intent step', async function () {
+			const acquireIntentPage = await AcquireIntentPage.Expect( driver );
+			await acquireIntentPage.skipStep();
+		} );
+
+		step( 'Can see Design Selector and select a random paid design', async function () {
+			const designSelectorPage = await DesignSelectorPage.Expect( driver );
+			await designSelectorPage.selectPaidDesign();
+		} );
+
+		step( 'Can see Plans Grid with a pre-selected plan', async function () {
+			const stylePreviewPage = await StylePreviewPage.Expect( driver );
+			await stylePreviewPage.continue();
+			const plansPage = await PlansPage.Expect( driver );
+			const hasSelectedPlan = await plansPage.hasSelectedPlan();
+			assert.strictEqual( hasSelectedPlan, true, 'There is no pre-selected plan' );
 		} );
 	} );
 } );
