5e2ec8fa29cc695f198ff936c5f18b4c3469eba2
Gary <gary@pento.net>
2020-06-10 11:33:55 +1000

    Redirect the block editor iframe when we're in Safari.

--- client/state/selectors/should-redirect-gutenberg.js
+++ client/state/selectors/should-redirect-gutenberg.js
@@ -1,9 +1,24 @@
 /**
+ * External dependencies
+ */
+import UserAgent from 'express-useragent';
+
+/**
  * Internal dependencies
  */
 import { getSelectedEditor } from 'state/selectors/get-selected-editor';
+import versionCompare from 'lib/version-compare';
 
 export const shouldRedirectGutenberg = ( state, siteId ) => {
+	if ( window && window.navigator ) {
+		// Safari 13 implemented strict cross-site cookie restrictions,
+		// which cause the editor iframe to fail loading. Always redirect Safari 13+.
+		const { isSafari, version } = UserAgent.parse( window.navigator.userAgent );
+		if ( isSafari && versionCompare( version, 13, '>=' ) ) {
+			return true;
+		}
+	}
+
 	const validEditors = [ 'gutenberg-redirect', 'gutenberg-redirect-and-style' ];
 	const selectedEditor = getSelectedEditor( state, siteId );
 	return validEditors.indexOf( selectedEditor ) > -1;
