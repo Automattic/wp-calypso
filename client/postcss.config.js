43f57f367516466635e46fae4aa1a50528e37ad6
Omar Alshaker <omar@omaralshaker.com>
2020-06-10 12:38:59 +0200

    Export CSS props as JS from calypso-color-schemes
    This helps cut fallback browsers production build time (#43054)

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,7 +2,7 @@ module.exports = ( { options } ) => {
 	const plugins = { autoprefixer: {} };
 	if ( options.transformCssProperties ) {
 		plugins[ 'postcss-custom-properties' ] = {
-			importFrom: [ require.resolve( '@automattic/calypso-color-schemes' ) ],
+			importFrom: [ options.customProperties ],
 		};
 	}
 	return { plugins };
