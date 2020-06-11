3b7299cfcb2d0cc341c50966d8989f2b91b589cd
Aurorum <torres126@clubpenguinmountains.com>
2020-06-11 21:04:33 +0100

    Account Closure: Warn of Alternative Options

--- client/me/account-close/confirm-dialog.jsx
+++ client/me/account-close/confirm-dialog.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Fragment } from 'react';
 import { localize } from 'i18n-calypso';
 import { noop } from 'lodash';
 import { connect } from 'react-redux';
@@ -12,6 +12,7 @@ import page from 'page';
  */
 import { Dialog, Button } from '@automattic/components';
 import FormLabel from 'components/forms/form-label';
+import InlineSupportLink from 'components/inline-support-link';
 import { getCurrentUser } from 'state/current-user/selectors';
 import { closeAccount } from 'state/account/actions';
 
@@ -22,6 +23,7 @@ import './confirm-dialog.scss';
 
 class AccountCloseConfirmDialog extends React.Component {
 	state = {
+		displayAlternativeOptions: true,
 		inputValue: '',
 	};
 
@@ -48,6 +50,10 @@ class AccountCloseConfirmDialog extends React.Component {
 		}
 	};
 
+	handleProceedingToConfirmation = () => {
+		this.setState( { displayAlternativeOptions: false } );
+	};
+
 	handleConfirm = () => {
 		this.props.closeAccount();
 		page( '/me/account/closed' );
@@ -55,50 +61,136 @@ class AccountCloseConfirmDialog extends React.Component {
 
 	render() {
 		const { isVisible, currentUsername, translate } = this.props;
-		const isButtonDisabled = currentUsername && this.state.inputValue !== currentUsername;
+		const isDeleteButtonDisabled = currentUsername && this.state.inputValue !== currentUsername;
+
+		const alternativeOptionsButtons = [
+			<Button onClick={ this.handleCancel }>{ translate( 'Cancel' ) }</Button>,
+			<Button primary onClick={ this.handleProceedingToConfirmation }>
+				{ translate( 'Proceed' ) }
+			</Button>,
+		];
+
 		const deleteButtons = [
 			<Button onClick={ this.handleCancel }>{ translate( 'Cancel' ) }</Button>,
-			<Button primary scary disabled={ isButtonDisabled } onClick={ this.handleConfirm }>
+			<Button primary scary disabled={ isDeleteButtonDisabled } onClick={ this.handleConfirm }>
 				{ translate( 'Close your account' ) }
 			</Button>,
 		];
 
+		const guideLinkText = translate( 'View a guide' );
+		const actionLinkText = translate( 'Try it' );
+
 		return (
 			<Dialog
 				isVisible={ isVisible }
-				buttons={ deleteButtons }
+				buttons={ this.state.displayAlternativeOptions ? alternativeOptionsButtons : deleteButtons }
 				className="account-close__confirm-dialog"
 			>
 				<h1 className="account-close__confirm-dialog-header">
-					{ translate( 'Confirm account closure' ) }
+					{ this.state.displayAlternativeOptions
+						? translate(
+								'Before you close your account, were you looking to do the following instead?'
+						  )
+						: translate( 'Confirm account closure' ) }
 				</h1>
-				<FormLabel
-					htmlFor="confirmAccountCloseInput"
-					className="account-close__confirm-dialog-label"
-				>
-					{ translate(
-						'Please type {{warn}}%(currentUsername)s{{/warn}} in the field below to confirm. ' +
-							'Your account will then be gone forever.',
-						{
-							components: {
-								warn: <span className="account-close__confirm-dialog-target-username" />,
-							},
-							args: {
-								currentUsername,
-							},
-						}
-					) }
-				</FormLabel>
+				{ ! this.state.displayAlternativeOptions && (
+					<Fragment>
+						<FormLabel
+							htmlFor="confirmAccountCloseInput"
+							className="account-close__confirm-dialog-label"
+						>
+							{ translate(
+								'Please type {{warn}}%(currentUsername)s{{/warn}} in the field below to confirm. ' +
+									'Your account will then be gone forever.',
+								{
+									components: {
+										warn: <span className="account-close__confirm-dialog-target-username" />,
+									},
+									args: {
+										currentUsername,
+									},
+								}
+							) }
+						</FormLabel>
+						<input
+							autoCapitalize="off"
+							className="account-close__confirm-dialog-confirm-input"
+							type="text"
+							onChange={ this.handleInputChange }
+							value={ this.state.inputValue }
+							aria-required="true"
+							id="confirmAccountCloseInput"
+						/>
+					</Fragment>
+				) }
+				{ this.state.displayAlternativeOptions && (
+					<div>
+						<div className="account-close__confirm-dialog-alternative">
+							<p>{ translate( 'Start a new site' ) }</p>
+							<div className="account-close__confirm-dialog-alternative-actions">
+								<InlineSupportLink
+									supportPostId={ 3991 }
+									supportLink="https://wordpress.com/support/create-a-blog/#adding-a-new-site-or-blog-to-an-existing-account"
+									showIcon={ false }
+									text={ guideLinkText }
+								/>
+								<a href="/jetpack/new/">{ actionLinkText }</a>
+							</div>
+						</div>
 
-				<input
-					autoCapitalize="off"
-					className="account-close__confirm-dialog-confirm-input"
-					type="text"
-					onChange={ this.handleInputChange }
-					value={ this.state.inputValue }
-					aria-required="true"
-					id="confirmAccountCloseInput"
-				/>
+						<div className="account-close__confirm-dialog-alternative">
+							<p>{ translate( "Change your site's address" ) }</p>
+							<div className="account-close__confirm-dialog-alternative-actions">
+								<InlineSupportLink
+									supportPostId={ 11280 }
+									supportLink="https://wordpress.com/support/changing-site-address/"
+									showIcon={ false }
+									text={ guideLinkText }
+								/>
+								<a href="/settings/general">{ actionLinkText }</a>
+							</div>
+						</div>
+
+						<div className="account-close__confirm-dialog-alternative">
+							<p>{ translate( 'Change your username' ) }</p>
+							<div className="account-close__confirm-dialog-alternative-actions">
+								<InlineSupportLink
+									supportPostId={ 2116 }
+									supportLink="https://wordpress.com/support/change-your-username"
+									showIcon={ false }
+									text={ guideLinkText }
+								/>
+								<a href="/me/account">{ actionLinkText }</a>
+							</div>
+						</div>
+
+						<div className="account-close__confirm-dialog-alternative">
+							<p>{ translate( 'Change your password' ) }</p>
+							<div className="account-close__confirm-dialog-alternative-actions">
+								<InlineSupportLink
+									supportPostId={ 89 }
+									supportLink="https://wordpress.com/support/passwords/#change-your-password"
+									showIcon={ false }
+									text={ guideLinkText }
+								/>
+								<a href="/me/security">{ actionLinkText }</a>
+							</div>
+						</div>
+
+						<div className="account-close__confirm-dialog-alternative">
+							<p>{ translate( 'Delete a site' ) }</p>
+							<div className="account-close__confirm-dialog-alternative-actions">
+								<InlineSupportLink
+									supportPostId={ 14411 }
+									supportLink="https://wordpress.com/support/delete-site/"
+									showIcon={ false }
+									text={ guideLinkText }
+								/>
+								<a href="/settings/delete-site">{ actionLinkText }</a>
+							</div>
+						</div>
+					</div>
+				) }
 			</Dialog>
 		);
 	}
