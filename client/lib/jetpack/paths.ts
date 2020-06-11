73b6bdba3965130e74ca98f1b9a3037f83f20d2b
Matt Gawarecki <matt.gawarecki@gmail.com>
2020-06-11 13:17:49 -0500

    Jetpack Cloud: Unify sidebar items into one shared component (#43149)
    * Sidebar: Unify Jetpack sidebar menu items from Cloud and Calypso
    
    * Remove TODO comment about Tracks events
    
    * Refactor common Jetpack path logic into methods in one file
    
    * Fully write out Tracks events; open Jetpack sidebar when an item is selected

--- client/lib/jetpack/paths.ts
+++ client/lib/jetpack/paths.ts
@@ -2,8 +2,22 @@
  * Internal dependencies
  */
 import isJetpackCloud from 'lib/jetpack/is-jetpack-cloud';
+import { addQueryArgs } from 'lib/url';
+
+const activityLogBasePath = () => ( isJetpackCloud() ? '/backup/activity' : '/activity-log' );
+export const activityLogPath = ( siteSlug?: string ) =>
+	siteSlug ? `${ activityLogBasePath() }/${ siteSlug }` : activityLogBasePath();
+
+const backupBasePath = () => '/backup';
+export const backupPath = ( siteSlug?: string, query = {} ) => {
+	const path = siteSlug ? `${ backupBasePath() }/${ siteSlug }` : backupBasePath();
+	return addQueryArgs( query, path );
+};
+
+const scanBasePath = () => '/scan';
+export const scanPath = ( siteSlug?: string ) =>
+	siteSlug ? `${ scanBasePath() }/${ siteSlug }` : scanBasePath();
 
 const settingsBasePath = () => ( isJetpackCloud() ? '/settings' : '/settings/security' );
-
-export const settingsPath = ( siteName: string ) =>
-	siteName ? `${ settingsBasePath() }/${ siteName }` : settingsBasePath();
+export const settingsPath = ( siteSlug?: string ) =>
+	siteSlug ? `${ settingsBasePath() }/${ siteSlug }` : settingsBasePath();
