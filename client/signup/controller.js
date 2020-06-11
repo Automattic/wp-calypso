682b8ecb0718d32b63c22f62bb94dffae37513dd
lamosty <lamos.rasto@gmail.com>
2020-06-11 13:33:06 +0200

    Use includes instead of indexOf

--- client/signup/controller.js
+++ client/signup/controller.js
@@ -92,10 +92,10 @@ export default {
 		) {
 			removeWhiteBackground();
 			next();
-		} else if ( context.pathname.indexOf( 'p2' ) >= 0 ) {
+		} else if ( context.pathname.includes( 'p2' ) ) {
 			// We still want to keep the original styling for the new user creation step
 			// so people know they are creating an account at WP.com.
-			if ( context.pathname.indexOf( 'user' ) >= 0 ) {
+			if ( context.pathname.includes( 'user' ) ) {
 				removeP2SignupClassName();
 			} else {
 				addP2SignupClassName();
