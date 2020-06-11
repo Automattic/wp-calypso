666ae006ab60093fbb39e8edb2cddcd10a39c591
lamosty <lamos.rasto@gmail.com>
2020-06-11 14:56:25 +0200

    Rework components to stateless ones

--- client/signup/p2-processing-screen/index.jsx
+++ client/signup/p2-processing-screen/index.jsx
@@ -1,8 +1,8 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
-import { localize } from 'i18n-calypso';
+import React from 'react';
+import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -13,37 +13,37 @@ import { localize } from 'i18n-calypso';
  */
 import './style.scss';
 
-export class P2SignupProcessingScreen extends Component {
-	render() {
-		return (
-			<div className="p2-processing-screen">
-				<div className="p2-processing-screen__logo">
-					<img src="/calypso/images/p2/logo-white.png" width="67" height="32" alt="P2 logo" />
-				</div>
+function P2SignupProcessingScreen() {
+	const translate = useTranslate();
 
-				<div className="p2-processing-screen__text">
-					{ this.props.translate( '{{h2}}Hooray!{{br/}}Your new P2 is{{br/}}almost ready.{{/h2}}', {
-						components: {
-							// eslint-disable-next-line jsx-a11y/heading-has-content
-							h2: <h2 />,
-							br: <br />,
-						},
-					} ) }
-				</div>
-
-				<div className="p2-processing-screen__footer">
-					<img
-						src="/calypso/images/p2/w-logo-white.png"
-						className="p2-processing-screen__w-logo"
-						alt="WP.com logo"
-					/>
-					<span className="p2-processing-screen__footer-text">
-						{ this.props.translate( 'Powered by WordPress.com' ) }
-					</span>
-				</div>
+	return (
+		<div className="p2-processing-screen">
+			<div className="p2-processing-screen__logo">
+				<img src="/calypso/images/p2/logo-white.png" width="67" height="32" alt="P2 logo" />
 			</div>
-		);
-	}
+
+			<div className="p2-processing-screen__text">
+				{ translate( '{{h2}}Hooray!{{br/}}Your new P2 is{{br/}}almost ready.{{/h2}}', {
+					components: {
+						// eslint-disable-next-line jsx-a11y/heading-has-content
+						h2: <h2 />,
+						br: <br />,
+					},
+				} ) }
+			</div>
+
+			<div className="p2-processing-screen__footer">
+				<img
+					src="/calypso/images/p2/w-logo-white.png"
+					className="p2-processing-screen__w-logo"
+					alt="WP.com logo"
+				/>
+				<span className="p2-processing-screen__footer-text">
+					{ translate( 'Powered by WordPress.com' ) }
+				</span>
+			</div>
+		</div>
+	);
 }
 
-export default localize( P2SignupProcessingScreen );
+export default P2SignupProcessingScreen;
