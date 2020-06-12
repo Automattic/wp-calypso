01ce312a7ab0cf3cbd38fd3d6430293b7442f5d1
Aurorum <torres126@clubpenguinmountains.com>
2020-06-12 22:58:37 +0100

    Account Closure: Warn of Alternative Options (#43214)
    * Account Closure: Warn of Alternative Options
    
    * Separate links and add styling
    
    * Update design of alternative options
    
    * Track clicks on both Support and Action links to determine how effective they are
    
    Co-authored-by: Matt Wondra <matt.wondra@automattic.com>

--- client/me/account-close/confirm-dialog.jsx
+++ client/me/account-close/confirm-dialog.jsx
@@ -10,8 +10,11 @@ import page from 'page';
 /**
  * Internal dependencies
  */
+import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { Dialog, Button } from '@automattic/components';
+import Gridicon from 'components/gridicon';
 import FormLabel from 'components/forms/form-label';
+import InlineSupportLink from 'components/inline-support-link';
 import { getCurrentUser } from 'state/current-user/selectors';
 import { closeAccount } from 'state/account/actions';
 
@@ -22,6 +25,7 @@ import './confirm-dialog.scss';
 
 class AccountCloseConfirmDialog extends React.Component {
 	state = {
+		displayAlternativeOptions: true,
 		inputValue: '',
 	};
 
@@ -48,17 +52,75 @@ class AccountCloseConfirmDialog extends React.Component {
 		}
 	};
 
+	handleProceedingToConfirmation = () => {
+		this.setState( { displayAlternativeOptions: false } );
+	};
+
 	handleConfirm = () => {
 		this.props.closeAccount();
 		page( '/me/account/closed' );
 	};
 
+	handleAlternaticeActionClick = ( evt ) => {
+		recordTracksEvent( 'calypso_close_account_alternative_clicked', {
+			type: 'Action Link',
+			label: evt.target.dataset.tracksLabel,
+		} );
+	};
+
 	render() {
 		const { isVisible, currentUsername, translate } = this.props;
-		const isButtonDisabled = currentUsername && this.state.inputValue !== currentUsername;
+		const isDeleteButtonDisabled = currentUsername && this.state.inputValue !== currentUsername;
+
+		const alternativeOptions = [
+			{
+				englishText: 'Start a new site',
+				text: translate( 'Start a new site' ),
+				href: '/jetpack/new',
+				supportLink:
+					'https://wordpress.com/support/create-a-blog/#adding-a-new-site-or-blog-to-an-existing-account',
+				supportPostId: 3991,
+			},
+			{
+				englishText: "Change your site's address",
+				text: translate( "Change your site's address" ),
+				href: '/settings/general',
+				supportLink: 'https://wordpress.com/support/changing-site-address/',
+				supportPostId: 11280,
+			},
+			{
+				englishText: 'Change your username',
+				text: translate( 'Change your username' ),
+				href: '/me/account',
+				supportLink: 'https://wordpress.com/support/change-your-username',
+				supportPostId: 2116,
+			},
+			{
+				englishText: 'Change your password',
+				text: translate( 'Change your password' ),
+				href: '/me/security',
+				supportLink: 'https://wordpress.com/support/passwords/#change-your-password',
+				supportPostId: 89,
+			},
+			{
+				englishText: 'Delete a site',
+				text: translate( 'Delete a site' ),
+				href: '/settings/delete-site',
+				supportLink: 'https://wordpress.com/support/delete-site/',
+				supportPostId: 14411,
+			},
+		];
+
+		const alternativeOptionsButtons = [
+			<Button onClick={ this.handleCancel }>{ translate( 'Cancel' ) }</Button>,
+			<Button primary onClick={ this.handleProceedingToConfirmation }>
+				{ translate( 'Continue' ) }
+			</Button>,
+		];
+
 		const deleteButtons = [
 			<Button onClick={ this.handleCancel }>{ translate( 'Cancel' ) }</Button>,
-			<Button primary scary disabled={ isButtonDisabled } onClick={ this.handleConfirm }>
+			<Button primary scary disabled={ isDeleteButtonDisabled } onClick={ this.handleConfirm }>
 				{ translate( 'Close your account' ) }
 			</Button>,
 		];
@@ -66,39 +128,80 @@ class AccountCloseConfirmDialog extends React.Component {
 		return (
 			<Dialog
 				isVisible={ isVisible }
-				buttons={ deleteButtons }
+				buttons={ this.state.displayAlternativeOptions ? alternativeOptionsButtons : deleteButtons }
 				className="account-close__confirm-dialog"
 			>
 				<h1 className="account-close__confirm-dialog-header">
-					{ translate( 'Confirm account closure' ) }
+					{ this.state.displayAlternativeOptions
+						? translate( 'Are you sure?' )
+						: translate( 'Confirm account closure' ) }
 				</h1>
-				<FormLabel
-					htmlFor="confirmAccountCloseInput"
-					className="account-close__confirm-dialog-label"
-				>
-					{ translate(
-						'Please type {{warn}}%(currentUsername)s{{/warn}} in the field below to confirm. ' +
-							'Your account will then be gone forever.',
-						{
-							components: {
-								warn: <span className="account-close__confirm-dialog-target-username" />,
-							},
-							args: {
-								currentUsername,
-							},
-						}
-					) }
-				</FormLabel>
+				{ ! this.state.displayAlternativeOptions && (
+					<>
+						<FormLabel
+							htmlFor="confirmAccountCloseInput"
+							className="account-close__confirm-dialog-label"
+						>
+							{ translate(
+								'Please type {{warn}}%(currentUsername)s{{/warn}} in the field below to confirm. ' +
+									'Your account will then be gone forever.',
+								{
+									components: {
+										warn: <span className="account-close__confirm-dialog-target-username" />,
+									},
+									args: {
+										currentUsername,
+									},
+								}
+							) }
+						</FormLabel>
+						<input
+							autoCapitalize="off"
+							className="account-close__confirm-dialog-confirm-input"
+							type="text"
+							onChange={ this.handleInputChange }
+							value={ this.state.inputValue }
+							aria-required="true"
+							id="confirmAccountCloseInput"
+						/>
+					</>
+				) }
+				{ this.state.displayAlternativeOptions && (
+					<>
+						<p>
+							{ translate(
+								"Here's a few options to try before you permanently delete your account."
+							) }
+						</p>
 
-				<input
-					autoCapitalize="off"
-					className="account-close__confirm-dialog-confirm-input"
-					type="text"
-					onChange={ this.handleInputChange }
-					value={ this.state.inputValue }
-					aria-required="true"
-					id="confirmAccountCloseInput"
-				/>
+						{ alternativeOptions.map(
+							( { englishText, text, href, supportLink, supportPostId } ) => (
+								<div className="account-close__confirm-dialog-alternative" key={ href }>
+									<Button
+										href={ href }
+										className="account-close__confirm-dialog-alternative-action"
+										onClick={ this.handleAlternaticeActionClick }
+										data-tracks-label={ englishText }
+									>
+										{ text }
+										<Gridicon icon="chevron-right" />
+									</Button>
+									<InlineSupportLink
+										supportPostId={ supportPostId }
+										supportLink={ supportLink }
+										showText={ false }
+										iconSize={ 20 }
+										tracksEvent="calypso_close_account_alternative_clicked"
+										tracksOptions={ {
+											type: 'Support Doc',
+											label: englishText,
+										} }
+									/>
+								</div>
+							)
+						) }
+					</>
+				) }
 			</Dialog>
 		);
 	}
