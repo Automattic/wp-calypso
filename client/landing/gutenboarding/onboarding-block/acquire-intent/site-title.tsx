830725057e4d751748cba7371072a0a1b9879bcd
Omar Alshaker <omar@omaralshaker.com>
2020-06-12 13:50:38 +0200

    Add placeholder to site title input

--- client/landing/gutenboarding/onboarding-block/acquire-intent/site-title.tsx
+++ client/landing/gutenboarding/onboarding-block/acquire-intent/site-title.tsx
@@ -5,21 +5,33 @@ import * as React from 'react';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { TextControl } from '@wordpress/components';
 import { useI18n } from '@automattic/react-i18n';
+<<<<<<< HEAD
 import { Icon } from '@wordpress/icons';
 import classnames from 'classnames';
+=======
+import { _x } from '@wordpress/i18n';
+>>>>>>> Add placeholder to site title input
 
 /**
  * Internal dependencies
  */
 import { STORE_KEY } from '../../stores/onboard';
 import { recordSiteTitleSelection } from '../../lib/analytics';
+<<<<<<< HEAD
 import tip from './tip';
 
+=======
+import useTyper from '../../hooks/use-typer';
+>>>>>>> Add placeholder to site title input
 interface Props {
+	skipButton: React.ReactNode;
 	onSubmit: () => void;
 }
 
-const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
+const translationContext =
+	'This is an example of a site name, feel free to create your own but please keep it under 22 characters';
+
+const SiteTitle: React.FunctionComponent< Props > = ( { skipButton, onSubmit } ) => {
 	const { __ } = useI18n();
 	const { siteTitle } = useSelect( ( select ) => select( STORE_KEY ).getState() );
 	const { setSiteTitle } = useDispatch( STORE_KEY );
@@ -43,6 +55,20 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 	// translators: label for site title input in Gutenboarding
 	const inputLabel = __( 'My site is called' );
 
+	const placeHolder = useTyper(
+		[
+			_x( 'In Focus Photographers', translationContext ),
+			_x( 'Cortado Coffee SF', 'This is an example of a site name', translationContext ),
+			_x( 'Leah Rand', 'This is an example of a site name', translationContext ),
+			_x( 'Ava Young', 'This is an example of a site name', translationContext ),
+			_x( 'Mighty Leaf Tea Room', 'This is an example of a site name', translationContext ),
+		],
+		! siteTitle,
+		{
+			delayBetweenCharacters: 70,
+		}
+	);
+
 	return (
 		<form
 			className={ classnames( 'site-title', { 'is-focused': isFocused, 'is-empty': ! siteTitle } ) }
@@ -52,7 +78,12 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 				{ inputLabel }
 			</label>
 			<div className="site-title__input-wrapper">
+				{ /* Adding key makes it more performant
+					because without it the element is recreated
+					for every letter in the typing animation
+					*/ }
 				<TextControl
+					key="site-title__input"
 					id="site-title__input"
 					className="site-title__input"
 					onChange={ setSiteTitle }
@@ -62,6 +93,7 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 					autoFocus // eslint-disable-line jsx-a11y/no-autofocus
 					spellCheck={ false }
 					autoComplete="off"
+					placeholder={ placeHolder }
 					autoCorrect="off"
 					data-hj-whitelist
 				></TextControl>
