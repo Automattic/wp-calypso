c9667a5445a2199e93cce2cbcd83749edbd541e8
Kirk Wight <kwight@kwight.ca>
2020-06-12 14:35:38 -0700

    Add Tracks event.

--- apps/wpcom-block-editor/src/wpcom/features/tracking/delegate-event-tracking.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/delegate-event-tracking.js
@@ -9,6 +9,7 @@ import debugFactory from 'debug';
 import wpcomBlockEditorCloseClick from './wpcom-block-editor-close-click';
 import wpcomInserterInlineSearchTerm from './wpcom-inserter-inline-search-term';
 import wpcomBlockDonationsPlanUpgrade from './wpcom-block-donations-plan-upgrade';
+import wpcomBlockDonationsStripeConnect from './wpcom-block-donations-stripe-connect';
 import wpcomBlockPremiumContentPlanUpgrade from './wpcom-block-premium-content-plan-upgrade';
 import wpcomBlockPremiumContentStripeConnect from './wpcom-block-premium-content-stripe-connect';
 
@@ -25,6 +26,7 @@ const EVENTS_MAPPING = [
 	wpcomBlockEditorCloseClick(),
 	wpcomInserterInlineSearchTerm(),
 	wpcomBlockDonationsPlanUpgrade(),
+	wpcomBlockDonationsStripeConnect(),
 	wpcomBlockPremiumContentPlanUpgrade(),
 	wpcomBlockPremiumContentStripeConnect(),
 ];
