a6bec886e00a244aa4ed753b06a14c7a28ea5e81
Omar Alshaker <omar@omaralshaker.com>
2020-06-13 20:13:13 +0200

    Design tweeks to match designs

--- packages/plans-grid/src/plans-details/index.tsx
+++ packages/plans-grid/src/plans-details/index.tsx
@@ -3,10 +3,12 @@
  */
 import React from 'react';
 import { useI18n } from '@automattic/react-i18n';
-import { Path, SVG } from '@wordpress/primitives';
-import { Icon } from '@wordpress/icons';
 import { useSelect } from '@wordpress/data';
 import { Plans } from '@automattic/data-stores';
+
+import { Icon, check } from '@wordpress/icons';
+
+const TickIcon = <Icon icon={ check } size={ 20 } />;
 
 /**
  * Style dependencies
@@ -19,12 +21,6 @@ const PlansDetails: React.FunctionComponent = ( props ) => {
 	const plansDetails = useSelect( ( select ) => select( PLANS_STORE ).getPlansDetails() );
 
 	const { __ } = useI18n();
-
-	const checkIcon = (
-		<SVG width="20" height="20" viewBox="0 0 20 20">
-			<Path d="M10 2c-4.42 0-8 3.58-8 8s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8zm-.615 12.66h-1.34l-3.24-4.54 1.34-1.25 2.57 2.4 5.14-5.93 1.34.94-5.81 8.38z" />
-		</SVG>
-	);
 
 	return (
 		<div className="plans-details">
@@ -56,7 +52,7 @@ const PlansDetails: React.FunctionComponent = ( props ) => {
 												<>
 													{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace */ }
 													<span className="hidden">{ __( 'Available' ) }</span>
-													<Icon icon={ checkIcon } size={ 20 } />
+													{ TickIcon }
 												</>
 											) : (
 												<>
