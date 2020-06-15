8e3f1e802b1ad8a33a1f7d81341c999ba0e8dc05
Philip Jackson <p-jackson@live.com>
2020-06-16 10:02:40 +1200

    Disable wpcalypso/jsx-classname-namespace rule for all of FSE

--- apps/full-site-editing/full-site-editing-plugin/wpcom-block-editor-nav-sidebar/src/components/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	rules: {
-		// These components render in a Gutenberg environment and should
-		// conform to those naming conventions instead of Calypso's.
-		'wpcalypso/jsx-classname-namespace': 0,
-	},
-};
