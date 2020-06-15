f3d8e7ec44af34d64e10a145bbdffdbb8d5a1d7c
Omar Alshaker <omar@omaralshaker.com>
2020-06-15 11:53:15 +0200

    Drop summary/details combination for IE11 support

--- packages/plans-grid/src/plans-table/index.tsx
+++ packages/plans-grid/src/plans-table/index.tsx
@@ -32,7 +32,7 @@ const PlansTable: React.FunctionComponent< Props > = ( {
 } ) => {
 	const supportedPlans = useSelect( ( select ) => select( PLANS_STORE ).getSupportedPlans() );
 	const prices = useSelect( ( select ) => select( PLANS_STORE ).getPrices() );
-	const [ plansExpanded, setPlansExpanded ] = useState( false );
+	const [ allPlansExpanded, setAllPlansExpanded ] = useState( false );
 
 	return (
 		<div className="plans-table">
@@ -40,7 +40,7 @@ const PlansTable: React.FunctionComponent< Props > = ( {
 				( plan ) =>
 					plan && (
 						<PlanItem
-							isExpanded={ plansExpanded }
+							allPlansExpanded={ allPlansExpanded }
 							key={ plan.storeSlug }
 							slug={ plan.storeSlug }
 							domain={ currentDomain }
@@ -52,7 +52,7 @@ const PlansTable: React.FunctionComponent< Props > = ( {
 							isSelected={ plan.storeSlug === selectedPlanSlug }
 							onSelect={ onPlanSelect }
 							onPickDomainClick={ onPickDomainClick }
-							onToggleExpandAll={ () => setPlansExpanded( ( expand ) => ! expand ) }
+							onToggleExpandAll={ () => setAllPlansExpanded( ( expand ) => ! expand ) }
 						></PlanItem>
 					)
 			) }
