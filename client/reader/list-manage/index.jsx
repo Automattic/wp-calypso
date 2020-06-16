c60da3f3fb25defddbf155e82a6866f6d0cc4598
bluefuton <chris@bluefuton.com>
2020-06-17 11:44:49 +1200

    Reader Lists: include the list slug in the export filename

--- client/reader/list-manage/index.jsx
+++ client/reader/list-manage/index.jsx
@@ -126,7 +126,13 @@ function ReaderListEdit( props ) {
 										'You can export this list to use on other services. The file will be in OPML format.'
 									) }
 								</p>
-								<ReaderExportButton exportType={ READER_EXPORT_TYPE_LIST } listId={ list.ID } />
+								<ReaderExportButton
+									exportType={ READER_EXPORT_TYPE_LIST }
+									listId={ list.ID }
+									filename={ `reader-list-${ props.slug
+										.replace( /[^a-z0-9]/gi, '_' )
+										.toLowerCase() }.opml` }
+								/>
 							</Card>
 						) }
 					</>
