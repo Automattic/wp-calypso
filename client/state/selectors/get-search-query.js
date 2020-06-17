379fb59b40ca8f72080ac061e59e9099277c7bd7
retrofox <rdsuarez@gmail.com>
2020-06-17 12:37:00 -0300

    state: move inline-help selectors to global folder

---
+++ client/state/selectors/get-search-query.js
@@ -0,0 +1,15 @@
+
+/**
+ * External Dependencies
+ */
+import { get} from 'lodash';
+
+/**
+ * Returns the current search query.
+ *
+ * @param  {object}  state  Global state tree
+ * @returns {string}        The current search query
+ */
+export default function getSearchQuery( state ) {
+	return get( state, 'inlineHelp.searchResults.search.searchQuery', '' );
+}
