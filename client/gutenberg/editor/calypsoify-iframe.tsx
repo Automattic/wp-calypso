e81982617e413ef197388495b58ef2e529a30805
Glen Davies <glen.davies@a8c.com>
2020-06-17 15:16:53 +1200

    tidy up typings

--- client/gutenberg/editor/calypsoify-iframe.tsx
+++ client/gutenberg/editor/calypsoify-iframe.tsx
@@ -112,8 +112,6 @@ enum EditorActions {
 	TrackPerformance = 'trackPerformance',
 }
 
-let waitForIframeToInit, waitForIframeToLoad;
-
 class CalypsoifyIframe extends Component<
 	Props & ConnectedProps & ProtectedFormProps & LocalizeProps & PerformanceTrackProps,
 	State
@@ -134,14 +132,17 @@ class CalypsoifyIframe extends Component<
 	mediaCancelPort: MessagePort | null = null;
 	revisionsPort: MessagePort | null = null;
 	successfulIframeLoad = false;
+	waitForIframeToInit: ReturnType<typeof setInterval> | undefined = undefined;
+	waitForIframeToLoad: ReturnType<typeof setTimeout> | undefined = undefined;
 
 	componentDidMount() {
 		MediaStore.on( 'change', this.updateImageBlocks );
 		window.addEventListener( 'message', this.onMessage, false );
-		waitForIframeToInit = setInterval( () => {
+		this.waitForIframeToInit = setInterval( () => {
 			if ( this.props.shouldLoadIframe ) {
-				clearInterval( waitForIframeToInit );
-				waitForIframeToLoad = setTimeout( () => {
+				clearInterval( this.waitForIframeToInit );
+				this.waitForIframeToLoad = setTimeout( () => {
+					console.log('I am so sick of waiting dude!');
 					window.location.replace( this.props.iframeUrl );
 				}, 6000 ); // One second longer than we give the iframe to load
 			}
@@ -604,7 +605,7 @@ class CalypsoifyIframe extends Component<
 	};
 
 	onIframeLoaded = async ( iframeUrl: string ) => {
-		clearTimeout( waitForIframeToLoad );
+		clearTimeout( this.waitForIframeToLoad );
 		if ( ! this.successfulIframeLoad ) {
 			// Sometimes (like in IE) the WindowActions.Loaded message arrives after
 			// the onLoad event is fired. To deal with this case we'll poll
