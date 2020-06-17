af906a88f2070e37d2eb5a5b05e95ceb0063c64a
Yan Sern <yan.sern@automattic.com>
2020-06-17 14:00:11 +0200

    Gutenboarding: Add hesitation nudge on intent gathering. (#43317)
    * Add hesitation nudge on intent gathering.
    
    * Added 3s delay before showing hint, 1s delay before hiding hint.
    
    * Translation explantion.
    
    * Rework using transition to fix issue where input hint suddenly appears on first keystroke when fade-in animation hasn't completely kicked in yet.

--- client/landing/gutenboarding/onboarding-block/acquire-intent/site-title.tsx
+++ client/landing/gutenboarding/onboarding-block/acquire-intent/site-title.tsx
@@ -5,12 +5,15 @@ import * as React from 'react';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { TextControl } from '@wordpress/components';
 import { useI18n } from '@automattic/react-i18n';
+import { Icon } from '@wordpress/icons';
+import classnames from 'classnames';
 
 /**
  * Internal dependencies
  */
 import { STORE_KEY } from '../../stores/onboard';
 import { recordSiteTitleSelection } from '../../lib/analytics';
+import tip from './tip';
 
 interface Props {
 	onSubmit: () => void;
@@ -20,6 +23,7 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 	const { __ } = useI18n();
 	const { siteTitle } = useSelect( ( select ) => select( STORE_KEY ).getState() );
 	const { setSiteTitle } = useDispatch( STORE_KEY );
+	const [ isFocused, setIsFocused ] = React.useState( false );
 
 	const handleFormSubmit = ( e: React.FormEvent< HTMLFormElement > ) => {
 		// hitting 'Enter' when focused on the input field should direct to next step.
@@ -29,13 +33,21 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 
 	const handleBlur = () => {
 		recordSiteTitleSelection( !! siteTitle );
+		setIsFocused( false );
+	};
+
+	const handleFocus = () => {
+		setIsFocused( true );
 	};
 
 	// translators: label for site title input in Gutenboarding
 	const inputLabel = __( 'My site is called' );
 
 	return (
-		<form className="site-title" onSubmit={ handleFormSubmit }>
+		<form
+			className={ classnames( 'site-title', { 'is-focused': isFocused, 'is-empty': ! siteTitle } ) }
+			onSubmit={ handleFormSubmit }
+		>
 			<label htmlFor="site-title__input" className="site-title__input-label">
 				{ inputLabel }
 			</label>
@@ -44,6 +56,7 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 					id="site-title__input"
 					className="site-title__input"
 					onChange={ setSiteTitle }
+					onFocus={ handleFocus }
 					onBlur={ handleBlur }
 					value={ siteTitle }
 					autoFocus // eslint-disable-line jsx-a11y/no-autofocus
@@ -51,8 +64,12 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 					autoComplete="off"
 					autoCorrect="off"
 					data-hj-whitelist
-				/>
-				{ /* @TODO: add info tip here */ }
+				></TextControl>
+				<p className="site-title__input-hint">
+					<Icon icon={ tip } size={ 14 } />
+					{ /* translators: The "it" here refers to the site title. */ }
+					<span>{ __( "Don't worry, you can change it later." ) }</span>
+				</p>
 			</div>
 		</form>
 	);
