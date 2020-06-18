be8febacd330e1861dec8896f5c6a7227da8a731
Renzo Canepa <rcanepag@gmail.com>
2020-06-18 14:48:05 -0300

    Jetpack section: add switch to control when we display the Business AT (#43413)
    * Control when we show the Business AT component
    
    * Pass down the Automated Transfer Status
    
    * Fix infinite loop on blocked AT sites
    
    * Improve types
    
    * Don't display the BusinessAT if the AT has been completed

--- client/lib/jetpack/wpcom-upsell-controller.tsx
+++ client/lib/jetpack/wpcom-upsell-controller.tsx
@@ -9,15 +9,12 @@ import React from 'react';
 import { isEnabled } from 'config';
 import type { Context } from './types';
 import { isBusinessPlan } from 'lib/plans';
-import { isEligibleForAutomatedTransfer } from 'state/automated-transfer/selectors';
 import isSiteAutomatedTransfer from 'state/selectors/is-site-automated-transfer';
 import { getSitePlan, isJetpackSite } from 'state/sites/selectors';
 import { getSelectedSiteId } from 'state/ui/selectors';
+import BusinessATSwitch from 'components/jetpack/business-at-switch';
 
-export default function upsellSwitch(
-	UpsellComponent: typeof React.Component,
-	BusinessATComponent: typeof React.Component
-): Function {
+export default function upsellSwitch( UpsellComponent: typeof React.Component ): Function {
 	return ( context: Context, next: Function ) => {
 		const getState = context.store.getState;
 		const siteId = getSelectedSiteId( getState() );
@@ -37,12 +34,12 @@ export default function upsellSwitch(
 		}
 
 		const isAtomic = isSiteAutomatedTransfer( getState(), siteId );
-		const isEligibleForAT = isEligibleForAutomatedTransfer( getState(), siteId );
 		// Show the Automated Transfer for sites with a Business plan that are not
-		// Atomic sites. These sites must be eligible as well.
-		if ( isBusiness && ! isAtomic && isEligibleForAT ) {
-			const primaryProduct = context.path.includes( '/backup/' ) ? 'backup' : 'scan';
-			context.primary = <BusinessATComponent product={ primaryProduct } />;
+		// Atomic sites.
+		if ( isBusiness && ! isAtomic ) {
+			context.primary = (
+				<BusinessATSwitch fallbackDisplay={ context.primary } path={ context.path } />
+			);
 		}
 
 		next();
