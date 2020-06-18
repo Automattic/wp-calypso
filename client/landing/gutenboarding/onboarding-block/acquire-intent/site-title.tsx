fca3c72d444526fbe70fb5f310b204a159153fd7
Omar Alshaker <omar@omaralshaker.com>
2020-06-18 07:31:29 +0200

    Add placeholder to site title input (#43225)

--- client/landing/gutenboarding/onboarding-block/acquire-intent/site-title.tsx
+++ client/landing/gutenboarding/onboarding-block/acquire-intent/site-title.tsx
@@ -7,6 +7,7 @@ import { TextControl } from '@wordpress/components';
 import { useI18n } from '@automattic/react-i18n';
 import { Icon } from '@wordpress/icons';
 import classnames from 'classnames';
+import { _x } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -15,9 +16,62 @@ import { STORE_KEY } from '../../stores/onboard';
 import { recordSiteTitleSelection } from '../../lib/analytics';
 import tip from './tip';
 
+import useTyper from '../../hooks/use-typer';
 interface Props {
 	onSubmit: () => void;
 }
+
+/**
+ * Shuffles an array in place
+ *
+ * @param arr the array to shuffle
+ */
+function shuffle( arr: string[] ) {
+	return arr.sort( () => ( Math.random() > 0.5 ? -1 : 1 ) );
+}
+
+/* we have them outside ot the component to avoid re-shuffling on every render */
+const siteTitleExamples = shuffle( [
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'The Local Latest', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'North Peak Cycling', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'Sunshine Daycare', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'Quick Wins Consulting', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'Puns and Pedantry', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'Yoga For Everyone', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'Pugs Wearing Bowties', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'Behind the Lens', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'Marketing Magic', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'Cortado Coffee', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'Mumbai Bites', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'RPM Motors', 'sample site title' ),
+	/* translators: This is an example of a site name,
+	   feel free to create your own but please keep it under 22 characters */
+	_x( 'Max’s Burger Bar', 'sample site title' ),
+] );
 
 const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 	const { __ } = useI18n();
@@ -42,6 +96,10 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 	// translators: label for site title input in Gutenboarding
 	const inputLabel = __( 'My site is called' );
 
+	const placeHolder = useTyper( siteTitleExamples, ! siteTitle, {
+		delayBetweenCharacters: 70,
+	} );
+
 	return (
 		<form
 			className={ classnames( 'site-title', { 'is-touched': isTouched } ) }
@@ -51,7 +109,12 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 				{ inputLabel }
 			</label>
 			<div className="site-title__input-wrapper">
+				{ /* Adding key makes it more performant
+					because without it the element is recreated
+					for every letter in the typing animation
+					*/ }
 				<TextControl
+					key="site-title__input"
 					id="site-title__input"
 					className="site-title__input"
 					onChange={ setSiteTitle }
@@ -61,6 +124,7 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit } ) => {
 					autoFocus // eslint-disable-line jsx-a11y/no-autofocus
 					spellCheck={ false }
 					autoComplete="off"
+					placeholder={ placeHolder }
 					autoCorrect="off"
 					data-hj-whitelist
 				></TextControl>
