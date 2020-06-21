1af7cb4845d1f1d464c1ca9254283a77c1fa7976
AnnaMag <anna.mag.kedzierska@gmail.com>
2020-06-21 19:24:10 +0200

    Rewrite the Search component to use the Connection HoC.

--- client/jetpack-connect/search.jsx
+++ client/jetpack-connect/search.jsx
@@ -1,12 +1,10 @@
 /**
  * External dependencies
  */
-import debugModule from 'debug';
-import config from 'config';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { concat, flowRight, get, includes } from 'lodash';
+import { concat, flowRight } from 'lodash';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -14,7 +12,6 @@ import { localize } from 'i18n-calypso';
  */
 import { Card } from '@automattic/components';
 import HelpButton from './help-button';
-import JetpackConnectNotices from './jetpack-connect-notices';
 import LocaleSuggestions from 'components/locale-suggestions';
 import LoggedOutFormLinkItem from 'components/logged-out-form/link-item';
 import LoggedOutFormLinks from 'components/logged-out-form/links';
@@ -22,9 +19,7 @@ import MainHeader from './main-header';
 import MainWrapper from './main-wrapper';
 import page from 'page';
 import SiteUrlInput from './site-url-input';
-import versionCompare from 'lib/version-compare';
-import { addCalypsoEnvQueryArg, cleanUrl } from './utils';
-import { addQueryArgs, externalRedirect } from 'lib/route';
+import { cleanUrl } from './utils';
 import { checkUrl, dismissUrl } from 'state/jetpack-connect/actions';
 import { FLOW_TYPES } from 'state/jetpack-connect/constants';
 import { getConnectingSite, getJetpackSiteByUrl } from 'state/jetpack-connect/selectors';
@@ -35,28 +30,10 @@ import { persistSession, retrieveMobileRedirect } from './persistence-utils';
 import { recordTracksEvent } from 'state/analytics/actions';
 import { urlToSlug } from 'lib/url';
 import searchSites from 'components/search-sites';
+import jetpackConnection from './jetpack-connection';
 
-import {
-	JPC_PATH_PLANS,
-	JPC_PATH_REMOTE_INSTALL,
-	MINIMUM_JETPACK_VERSION,
-	REMOTE_PATH_AUTH,
-} from './constants';
-import {
-	ALREADY_CONNECTED,
-	ALREADY_OWNED,
-	IS_DOT_COM,
-	NOT_ACTIVE_JETPACK,
-	NOT_CONNECTED_JETPACK,
-	NOT_EXISTS,
-	NOT_JETPACK,
-	NOT_WORDPRESS,
-	OUTDATED_JETPACK,
-	SITE_BLACKLISTED,
-	WORDPRESS_DOT_COM,
-} from './connection-notice-types';
-
-const debug = debugModule( 'calypso:jetpack-connect:main' );
+import { JPC_PATH_REMOTE_INSTALL } from './constants';
+import { ALREADY_CONNECTED, IS_DOT_COM } from './connection-notice-types';
 
 export class SearchPurchase extends Component {
 	static propTypes = {
@@ -64,6 +41,7 @@ export class SearchPurchase extends Component {
 		path: PropTypes.string,
 		type: PropTypes.oneOf( concat( FLOW_TYPES, false ) ),
 		url: PropTypes.string,
+		processJpSite: PropTypes.func,
 	};
 
 	state = this.props.url
@@ -104,39 +82,17 @@ export class SearchPurchase extends Component {
 	}
 
 	componentDidMount() {
-		let from = 'direct';
-		if ( this.props.type === 'install' ) {
-			from = 'jpdotcom';
-		}
-		if ( this.props.type === 'pro' ) {
-			from = 'ad';
-		}
-		if ( this.props.type === 'premium' ) {
-			from = 'ad';
-		}
-		if ( this.props.type === 'personal' ) {
-			from = 'ad';
-		}
-
 		this.props.recordTracksEvent( 'calypso_jpc_url_view', {
-			jpc_from: from,
+			jpc_from: 'jp_lp',
 			cta_id: this.props.ctaId,
 			cta_from: this.props.ctaFrom,
 		} );
 	}
 
 	componentDidUpdate() {
-		const { isMobileAppFlow, skipRemoteInstall, forceRemoteInstall } = this.props;
-		const status = this.getStatus();
-
-		if (
-			status === NOT_CONNECTED_JETPACK &&
-			this.isCurrentUrlFetched() &&
-			! forceRemoteInstall &&
-			! this.state.redirecting
-		) {
-			return this.goToRemoteAuth( this.props.siteHomeUrl );
-		}
+		const { status, processJpSite } = this.props;
+		const { currentUrl } = this.state;
+		//	const { isMobileAppFlow, skipRemoteInstall, forceRemoteInstall } = this.props;
 
 		if ( status === IS_DOT_COM || status === ALREADY_CONNECTED ) {
 			const product = window.location.href.split( '/' )[ 5 ];
@@ -148,113 +104,17 @@ export class SearchPurchase extends Component {
 			page.redirect( redirectTo );
 		}
 
-		if ( status === ALREADY_OWNED && ! this.state.redirecting ) {
-			if ( isMobileAppFlow ) {
-				return this.redirectToMobileApp( 'already-connected' );
-			}
-			return this.goToPlans( this.state.currentUrl );
-		}
-
-		if ( this.state.waitingForSites && ! this.props.isRequestingSites ) {
-			// eslint-disable-next-line react/no-did-update-set-state
-			this.setState( { waitingForSites: false } );
-			this.checkUrl( this.state.currentUrl );
-		}
-
-		if (
-			includes( [ NOT_JETPACK, NOT_ACTIVE_JETPACK ], status ) ||
-			( status === NOT_CONNECTED_JETPACK && forceRemoteInstall )
-		) {
-			if (
-				config.isEnabled( 'jetpack/connect/remote-install' ) &&
-				! isMobileAppFlow &&
-				! skipRemoteInstall
-			) {
-				this.goToRemoteInstall( JPC_PATH_REMOTE_INSTALL );
-			} else {
-				this.goToInstallInstructions( '/jetpack/connect/instructions' );
-			}
-		}
+		processJpSite( currentUrl );
 	}
 
-	dismissUrl = () => this.props.dismissUrl( this.state.currentUrl );
-
-	goBack = () => page.back();
-
-	makeSafeRedirectionFunction( func ) {
-		return ( url ) => {
-			if ( ! this.state.redirecting ) {
-				this.setState( { redirecting: true } );
-				func( url );
-			}
-		};
-	}
-
-	goToCheckout = this.makeSafeRedirectionFunction( ( url ) => {
-		this.props.recordTracksEvent( 'calypso_jpc_success_redirect', {
-			url: url,
-			type: 'search_checkout',
-		} );
-
-		page.redirect( `checkout/${ urlToSlug( url ) }/jetpack_search` );
-	} );
-
-	goToPlans = this.makeSafeRedirectionFunction( ( url ) => {
-		this.props.recordTracksEvent( 'calypso_jpc_success_redirect', {
-			url: url,
-			type: 'plans_selection',
-		} );
-
-		page.redirect( `${ JPC_PATH_PLANS }/${ urlToSlug( url ) }` );
-	} );
-
-	goToRemoteAuth = this.makeSafeRedirectionFunction( ( url ) => {
-		this.props.recordTracksEvent( 'calypso_jpc_success_redirect', {
-			url: url,
-			type: 'remote_auth',
-		} );
-		externalRedirect( addCalypsoEnvQueryArg( url + REMOTE_PATH_AUTH ) );
-	} );
-
-	goToRemoteInstall = this.makeSafeRedirectionFunction( ( url ) => {
-		this.props.recordTracksEvent( 'calypso_jpc_success_redirect', {
-			url: url,
-			type: 'remote_install',
-		} );
-		page.redirect( url );
-	} );
-
-	goToInstallInstructions = this.makeSafeRedirectionFunction( ( url ) => {
-		const urlWithQuery = addQueryArgs( { url: this.state.currentUrl }, url );
-		this.props.recordTracksEvent( 'calypso_jpc_success_redirect', {
-			url: urlWithQuery,
-			type: 'install_instructions',
-		} );
-		page( urlWithQuery );
-	} );
-
-	redirectToMobileApp = this.makeSafeRedirectionFunction( ( reason ) => {
-		const url = addQueryArgs( { reason }, this.props.mobileAppRedirect );
-		debug( `Redirecting to mobile app ${ url }` );
-		externalRedirect( url );
-	} );
-
-	isCurrentUrlFetched() {
-		return (
-			this.props.jetpackConnectSite &&
-			this.state.currentUrl === this.props.jetpackConnectSite.url &&
-			this.props.jetpackConnectSite.isFetched
-		);
-	}
-
-	isCurrentUrlFetching() {
-		return (
-			this.state.currentUrl !== '' &&
-			this.props.jetpackConnectSite &&
-			this.state.currentUrl === this.props.jetpackConnectSite.url &&
-			this.props.jetpackConnectSite.isFetching
-		);
-	}
+	// goToCheckout = this.makeSafeRedirectionFunction( ( url ) => {
+	// 	this.props.recordTracksEvent( 'calypso_jpc_success_redirect', {
+	// 		url: url,
+	// 		type: 'search_checkout',
+	// 	} );
+	//
+	// 	page.redirect( `checkout/${ urlToSlug( url ) }/jetpack_search` );
+	// } );
 
 	handleUrlChange = ( url ) => {
 		this.setState( {
@@ -283,89 +143,7 @@ export class SearchPurchase extends Component {
 		}
 	};
 
-	checkProperty( propName ) {
-		return (
-			this.state.currentUrl &&
-			this.props.jetpackConnectSite &&
-			this.props.jetpackConnectSite.data &&
-			this.isCurrentUrlFetched() &&
-			this.props.jetpackConnectSite.data[ propName ]
-		);
-	}
-
-	isError( error ) {
-		return (
-			this.state.currentUrl &&
-			this.isCurrentUrlFetched() &&
-			get( this.props.jetpackConnectSite, [ 'error', 'error' ] ) === error
-		);
-	}
-
-	getStatus() {
-		if ( this.state.currentUrl === '' ) {
-			return false;
-		}
-
-		if ( this.isError( 'site_blacklisted' ) ) {
-			return SITE_BLACKLISTED;
-		}
-
-		// if ( this.checkProperty( 'userOwnsSite' ) ) {
-		// 	return ALREADY_OWNED;
-		// }
-
-		if ( this.props.jetpackConnectSite.installConfirmedByUser === false ) {
-			return NOT_JETPACK;
-		}
-
-		if ( this.props.jetpackConnectSite.installConfirmedByUser === true ) {
-			return NOT_ACTIVE_JETPACK;
-		}
-
-		if (
-			this.state.currentUrl.toLowerCase() === 'http://wordpress.com' ||
-			this.state.currentUrl.toLowerCase() === 'https://wordpress.com'
-		) {
-			return WORDPRESS_DOT_COM;
-		}
-
-		if ( this.checkProperty( 'isWordPressDotCom' ) ) {
-			return IS_DOT_COM;
-		}
-		if ( ! this.checkProperty( 'exists' ) ) {
-			return NOT_EXISTS;
-		}
-		if ( ! this.checkProperty( 'isWordPress' ) ) {
-			return NOT_WORDPRESS;
-		}
-		if ( ! this.checkProperty( 'hasJetpack' ) ) {
-			return NOT_JETPACK;
-		}
-		const jetpackVersion = this.checkProperty( 'jetpackVersion' );
-		if ( jetpackVersion && versionCompare( jetpackVersion, MINIMUM_JETPACK_VERSION, '<' ) ) {
-			return OUTDATED_JETPACK;
-		}
-		if ( ! this.checkProperty( 'isJetpackActive' ) ) {
-			return NOT_ACTIVE_JETPACK;
-		}
-		if (
-			! this.checkProperty( 'isJetpackConnected' ) ||
-			( this.checkProperty( 'isJetpackConnected' ) && ! this.checkProperty( 'userOwnsSite' ) )
-		) {
-			return NOT_CONNECTED_JETPACK;
-		}
-		if ( this.checkProperty( 'isJetpackConnected' ) && this.checkProperty( 'userOwnsSite' ) ) {
-			return ALREADY_CONNECTED;
-		}
-
-		return false;
-	}
-
 	handleOnClickTos = () => this.props.recordTracksEvent( 'calypso_jpc_tos_link_click' );
-
-	isInstall() {
-		return includes( FLOW_TYPES, this.props.type );
-	}
 
 	renderFooter() {
 		const { translate } = this.props;
@@ -380,33 +158,20 @@ export class SearchPurchase extends Component {
 	}
 
 	renderSiteInput( status ) {
-		const product = window.location.href.split( '/' )[ 5 ];
-
 		return (
 			<Card className="jetpack-connect__site-url-input-container">
-				{ ! this.isCurrentUrlFetching() &&
-				this.isCurrentUrlFetched() &&
-				! this.props.jetpackConnectSite.isDismissed &&
-				status &&
-				product !== 'jetpack_search' ? (
-					<JetpackConnectNotices
-						noticeType={ status }
-						onDismissClick={ IS_DOT_COM === status ? this.goBack : this.dismissUrl }
-						url={ this.state.currentUrl }
-						onTerminalError={ this.props.isMobileAppFlow ? this.redirectToMobileApp : null }
-					/>
-				) : null }
+				{ this.props.renderNotices() }
 
 				<SiteUrlInput
 					url={ this.state.shownUrl }
 					onTosClick={ this.handleOnClickTos }
 					onChange={ this.handleUrlChange }
 					onSubmit={ this.handleUrlSubmit }
-					isError={ this.getStatus() }
+					isError={ status }
 					isFetching={
-						this.isCurrentUrlFetching() || this.state.redirecting || this.state.waitingForSites
+						this.props.isCurrentUrlFetching || this.state.redirecting || this.state.waitingForSites
 					}
-					isInstall={ this.isInstall() }
+					isInstall={ true }
 					product={ 'jetpack_search' }
 					candidateSites={ this.state.candidateSites }
 				/>
@@ -423,17 +188,16 @@ export class SearchPurchase extends Component {
 	}
 
 	render() {
-		const status = this.getStatus();
-		const { type } = this.props;
+		const { renderFooter, status } = this.props;
 
 		return (
 			<MainWrapper>
 				{ this.renderLocaleSuggestions() }
 				<div className="jetpack-connect__site-url-entry-container">
-					<MainHeader type={ type } />
+					<MainHeader type={ 'jetpack_search' } />
 
 					{ this.renderSiteInput( status ) }
-					{ this.renderFooter() }
+					{ renderFooter() }
 				</div>
 			</MainWrapper>
 		);
@@ -472,4 +236,9 @@ const connectComponent = connect(
 	}
 );
 
-export default flowRight( connectComponent, searchSites, localize )( SearchPurchase );
+export default flowRight(
+	jetpackConnection,
+	connectComponent,
+	searchSites,
+	localize
+)( SearchPurchase );
