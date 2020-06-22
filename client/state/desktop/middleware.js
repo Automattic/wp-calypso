fe0125e2546bac3366e79ee97f254f1fce82329d
Nicholas Sakaimbo <nsakaimbo@users.noreply.github.com>
2020-06-22 15:01:01 -0400

    desktop: fix print codes button (#43523)
    Clicking "print recovery codes" after enabling two-factor authentication in the desktop app was silently broken. This change restores the functionality and makes the event handler consistent with other desktop events (i.e. routed through redux and middleware).

--- client/state/desktop/middleware.js
+++ client/state/desktop/middleware.js
@@ -13,12 +13,14 @@ import {
 	SITE_REQUEST_FAILURE,
 	JETPACK_MODULE_ACTIVATE_SUCCESS,
 	JETPACK_MODULE_ACTIVATE_FAILURE,
+	SEND_TO_PRINTER,
 } from '../../state/action-types';
 
 import {
 	NOTIFY_DESKTOP_CANNOT_USE_EDITOR,
 	NOTIFY_DESKTOP_DID_REQUEST_SITE,
 	NOTIFY_DESKTOP_DID_ACTIVATE_JETPACK_MODULE,
+	NOTIFY_DESKTOP_SEND_TO_PRINTER,
 	NOTIFY_DESKTOP_VIEW_POST_CLICKED,
 } from '../../state/desktop/window-events';
 
@@ -89,6 +91,20 @@ export const desktopMiddleware = () => {
 					} )
 				);
 				return next( action );
+
+			case SEND_TO_PRINTER: {
+				debug( 'Dispatching window event for action type: ', action.type );
+				const { title, contents } = action;
+				window.dispatchEvent(
+					new window.CustomEvent( NOTIFY_DESKTOP_SEND_TO_PRINTER, {
+						detail: {
+							title,
+							contents,
+						},
+					} )
+				);
+				return next( action );
+			}
 
 			case SITE_REQUEST_SUCCESS:
 				debug( 'Dispatching desktop window event for action type: ', action.type );
