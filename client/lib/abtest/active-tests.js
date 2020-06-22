2dde95e4c4fd5293ceae74c0f99563397a5a6085
Dale du Preez <dale@automattic.com>
2020-06-22 12:14:50 +0200

    Remove AB test for .jp domains in Japan

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -128,16 +128,6 @@ export default {
 		localeTargets: [ 'en' ],
 		countryCodeTargets: [ 'US', 'ID', 'NG', 'BD', 'NL', 'SE', 'SG', 'LK', 'NZ', 'IE', 'CA', 'AU' ],
 	},
-	domainShowJPResultsInJapan: {
-		datestamp: '20200506',
-		variations: {
-			variantShowJPResults: 50,
-			control: 50,
-		},
-		defaultVariation: 'control',
-		localeTargets: 'any',
-		countryCodeTargets: [ 'JP' ],
-	},
 	[ RUM_DATA_COLLECTION.AB_NAME ]: {
 		datestamp: '20200602',
 		variations: {
--- client/lib/domains/suggestions/index.js
+++ client/lib/domains/suggestions/index.js
@@ -1,26 +1,13 @@
 /**
- * Internal dependencies
- */
-import { abtest } from 'lib/abtest';
-
-/**
  * Get the suggestions vendor
  *
  * @param {boolean} [isSignup=false] Whether the query is part of a signup flow.
- * @param {string|null} [countryCodeForABTests=null] The country code to be used for A/B tests.
  *
  * @returns {string} Vendor string to pass as part of the domain suggestions query.
  */
-export const getSuggestionsVendor = ( isSignup = false, countryCodeForABTests = null ) => {
+export const getSuggestionsVendor = ( isSignup = false ) => {
 	if ( isSignup ) {
-		let vendor = 'variation4_front';
-		if (
-			countryCodeForABTests &&
-			abtest( 'domainShowJPResultsInJapan', countryCodeForABTests ) === 'variantShowJPResults'
-		) {
-			vendor = 'variation6_front';
-		}
-		return vendor;
+		return 'variation4_front';
 	}
 	return 'variation2_front';
 };
