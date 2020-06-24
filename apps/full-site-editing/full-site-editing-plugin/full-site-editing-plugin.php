b41d53d8e5344c39cf486dc8330dc953ed817d02
cpap <papazoglou.charalampos@gmail.com>
2020-06-24 11:18:33 +0300

    FSE: fix  undefined

--- apps/full-site-editing/full-site-editing-plugin/full-site-editing-plugin.php
+++ apps/full-site-editing/full-site-editing-plugin/full-site-editing-plugin.php
@@ -226,7 +226,7 @@ function load_blog_posts_block() {
 	$disable_block = (
 		( defined( 'WP_CLI' ) && WP_CLI ) ||
 		/* phpcs:ignore WordPress.Security.NonceVerification */
-		( isset( $_GET['action'], $_GET['plugin'] ) && 'activate' === $_GET['action'] && $slug === $_GET['plugin'] ) ||
+		( isset( $_GET['action'], $_GET['plugin'] ) && 'activate' === $_GET['action'] && preg_match( $slug_regex, $_GET['plugin'] ) ) ||
 		preg_grep( $slug_regex, (array) get_option( 'active_plugins' ) ) ||
 		preg_grep( $slug_regex, (array) get_site_option( 'active_sitewide_plugins' ) )
 	);
