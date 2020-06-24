b9168d4287f81b0f078d1e2130907cf4d67b409c
Sara Marcondes <saram@fastmail.com>
2020-06-24 08:13:59 -0700

    Correct test assertions

--- client/reader/lib/site-description-filter/test/index.js
+++ client/reader/lib/site-description-filter/test/index.js
@@ -6,11 +6,11 @@ import { isSiteDescriptionDisallowed } from '../';
 describe( 'isSiteDescriptionDisallowed', () => {
 	test( 'should return true if a site description is disallowed', () => {
 		const disallowedDescription = 'Just another WordPress site';
-		expect( isSiteDescriptionDisallowed( disallowedDescription ) ).toBeTrue();
+		expect( isSiteDescriptionDisallowed( disallowedDescription ) ).toBeTruthy();
 	} );
 
 	test( 'should return false if a site description is allowed', () => {
 		const allowedDescription = 'My site is marvellous';
-		expect( isSiteDescriptionDisallowed( allowedDescription ) ).toBeFalse();
+		expect( isSiteDescriptionDisallowed( allowedDescription ) ).toBeFalsy();
 	} );
 } );
