7be8c5cb74da41f7337e64350919900a004c43eb
Elton Goci <eltongoci@gmail.com>
2020-06-24 21:33:56 +0200

    Start shaping the new domain list item

--- client/my-sites/domains/domain-management/list/domain-item.jsx
+++ client/my-sites/domains/domain-management/list/domain-item.jsx
@@ -7,17 +7,32 @@ import React, { PureComponent } from 'react';
  * Internal dependencies
  */
 import { CompactCard } from '@automattic/components';
+import FormCheckbox from 'components/forms/form-checkbox';
 
 class DomainItem extends PureComponent {
 	handleClick = () => {
 		this.props.onClick( this.props.domain );
 	};
 
+	stopPropagation = ( event ) => {
+		event.stopPropagation();
+	};
+
+	onToggle = () => {
+		// TODO
+	};
+
 	render() {
 		return (
-			<CompactCard className={ 'domain-item' } onClick={ this.handleClick }>
+			<CompactCard className="domain-item" onClick={ this.handleClick }>
+				<FormCheckbox
+					className="domain-item__checkbox"
+					onChange={ this.onToggle }
+					onClick={ this.stopPropagation }
+				/>
 				<div className="domain-item__link">
 					<div className="domain-item__title">{ this.props.domain.domain }</div>
+					<div className="domain-item__meta">Site: site name</div>
 				</div>
 			</CompactCard>
 		);
