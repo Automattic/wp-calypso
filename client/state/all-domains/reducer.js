496e5b73b98cee05b9560e4080c7581b6f4ef9c4
Seni Oluwunmi <277661+olaseni@users.noreply.github.com>
2020-06-26 07:58:06 +0100

    Normalize domain types

--- client/state/all-domains/reducer.js
+++ client/state/all-domains/reducer.js
@@ -1,19 +1,22 @@
 /**
  * Internal dependencies
  */
-import { combineReducers, withSchemaValidation } from 'state/utils';
 import {
 	ALL_DOMAINS_REQUEST,
 	ALL_DOMAINS_REQUEST_FAILURE,
 	ALL_DOMAINS_REQUEST_SUCCESS,
 } from 'state/action-types';
-
 import { allDomainsSchema } from './schema';
+import { combineReducers, withSchemaValidation } from 'state/utils';
+import { getDomainType } from 'lib/domains/utils';
 
 export const allDomains = withSchemaValidation( allDomainsSchema, ( state = [], action ) => {
 	switch ( action.type ) {
 		case ALL_DOMAINS_REQUEST_SUCCESS:
-			return action.domains;
+			return ( action?.domains ?? [] ).map( ( domain ) => {
+				domain.type = getDomainType( domain.type );
+				return domain;
+			} );
 	}
 
 	return state;
