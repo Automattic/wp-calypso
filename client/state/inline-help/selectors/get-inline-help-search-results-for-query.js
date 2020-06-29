738b8a53a42ae1c9fb6dc5bf28f0a135e981feb7
Matthew Reishus <mreishus@users.noreply.github.com>
2020-06-29 14:27:19 -0500

    InlineHelp: getInlineHelpSearchResultsForQuery() should not receive query as a second argument
    getInlineHelpSearchResultsForQuery() pulls the current search from the
    state instead of taking an optional second argument of which search to
    look for.
    
    Also updated the docs to reflect that the function can never return
    null, in bad cases it returns an empty array instead (always).

--- client/state/inline-help/selectors/get-inline-help-search-results-for-query.js
+++ client/state/inline-help/selectors/get-inline-help-search-results-for-query.js
@@ -9,14 +9,15 @@ import { get } from 'lodash';
 import 'state/inline-help/init';
 
 /**
- * Returns an array of all search results for a given search query or `null`
- * if there are no results for that query.
+ * Returns an array of all search results for the current search query or an
+ * empty array if there are no results for that query.
  *
  * @param  {object}  state  Global state tree.
- * @param  {number}  searchQuery Search query.
- * @returns {?Array} List of results for a given search query.
+ * @returns {Array} List of results for a given search query.
  */
-export default function getInlineHelpSearchResultsForQuery( state, searchQuery ) {
+
+export default function getInlineHelpSearchResultsForQuery( state ) {
+	const searchQuery = get( state, 'inlineHelp.searchResults.search.searchQuery' );
 	const allResults = get( state, 'inlineHelp.searchResults.search.items' );
 	return get( allResults, [ searchQuery ], [] );
 }
