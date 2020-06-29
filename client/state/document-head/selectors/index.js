7d3fce652729f95a9a96da3ce4563ae769be97cd
Sérgio Gomes <mail@sgomes.com>
2020-06-29 10:42:31 +0100

    Split out `getDocumentHeadUnreadCount`

--- client/state/document-head/selectors/index.js
+++ client/state/document-head/selectors/index.js
@@ -13,20 +13,12 @@ import getSiteTitle from 'state/sites/selectors/get-site-title';
 import config from 'config';
 
 import { getDocumentHeadTitle } from 'state/document-head/selectors/get-document-head-title';
+import { getDocumentHeadUnreadCount } from 'state/document-head/selectors/get-document-head-unread-count';
 
 const UNREAD_COUNT_CAP = 40;
 
 export { getDocumentHeadTitle } from 'state/document-head/selectors/get-document-head-title';
-
-/**
- * Returns a count reflecting unread items.
- *
- * @param  {object}  state  Global state tree
- * @returns {?string}        Unread count (string because it can be e.g. '40+')
- */
-export function getDocumentHeadUnreadCount( state ) {
-	return state.documentHead.unreadCount;
-}
+export { getDocumentHeadUnreadCount } from 'state/document-head/selectors/get-document-head-unread-count';
 
 /**
  * Returns a count reflecting unread items, capped at a value determined by
