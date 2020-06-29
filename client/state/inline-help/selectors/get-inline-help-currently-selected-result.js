081e2fdfbce35e433e123c522f538da4e6e40a33
Matthew Reishus <mreishus@users.noreply.github.com>
2020-06-29 15:05:32 -0500

    getInlineHelpSearchResultsForQuery: Remove second argument at call sites
    getInlineHelpSearchResultsForQuery() no longer needs a second argument,
    it determines the current search from the current state.

--- client/state/inline-help/selectors/get-inline-help-currently-selected-result.js
+++ client/state/inline-help/selectors/get-inline-help-currently-selected-result.js
@@ -6,7 +6,6 @@ import { get } from 'lodash';
 /**
  * Internal dependencies
  */
-import getSearchQuery from 'state/inline-help/selectors/get-search-query';
 import getInlineHelpSearchResultsForQuery from 'state/inline-help/selectors/get-inline-help-search-results-for-query';
 import getSelectedResultIndex from 'state/inline-help/selectors/get-selected-result-index';
 import getContextualHelpResults from 'state/inline-help/selectors/get-contextual-help-results';
@@ -20,8 +19,7 @@ import 'state/inline-help/init';
  * @returns {object}        The selected search result
  */
 export default function getInlineHelpCurrentlySelectedResult( state ) {
-	const query = getSearchQuery( state );
-	const results = getInlineHelpSearchResultsForQuery( state, query );
+	const results = getInlineHelpSearchResultsForQuery( state );
 	const selectedIndex = getSelectedResultIndex( state );
 
 	return get( results, selectedIndex ) || getContextualHelpResults( state )[ selectedIndex ];
