d936398f69cfc1dc035d5eb39d91f16ac101f6d3
Andrew Serong <andrewserong@users.noreply.github.com>
2020-06-29 18:35:36 +1000

    Hide full-site-editing filter when selected site is not legacy-FSE

--- client/state/data-layer/wpcom/theme-filters/index.js
+++ client/state/data-layer/wpcom/theme-filters/index.js
@@ -3,6 +3,7 @@
  */
 
 import i18n from 'i18n-calypso';
+import { omit } from 'lodash';
 
 /**
  * Internal dependencies
@@ -11,6 +12,8 @@ import { THEME_FILTERS_REQUEST, THEME_FILTERS_ADD } from 'state/themes/action-ty
 import { dispatchRequest } from 'state/data-layer/wpcom-http/utils';
 import { http } from 'state/data-layer/wpcom-http/actions';
 import { errorNotice } from 'state/notices/actions';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import isSiteUsingFullSiteEditing from 'state/selectors/is-site-using-full-site-editing';
 
 import { registerHandlers } from 'state/data-layer/handler-registry';
 
@@ -24,16 +27,30 @@ const fetchFilters = ( action ) =>
 		action
 	);
 
-const storeFilters = ( action, data ) => ( { type: THEME_FILTERS_ADD, filters: data } );
+const storeFilters = ( action, data ) => {
+	const filters = action.isFse ? data : omit( data, 'feature.full-site-editing' );
+	return { type: THEME_FILTERS_ADD, filters };
+};
 
 const reportError = () => errorNotice( i18n.translate( 'Problem fetching theme filters.' ) );
 
+const themeFiltersHandlers = dispatchRequest( {
+	fetch: fetchFilters,
+	onSuccess: storeFilters,
+	onError: reportError,
+} );
+
 registerHandlers( 'state/data-layer/wpcom/theme-filters/index.js', {
 	[ THEME_FILTERS_REQUEST ]: [
-		dispatchRequest( {
-			fetch: fetchFilters,
-			onSuccess: storeFilters,
-			onError: reportError,
-		} ),
+		( store, action ) => {
+			const state = store.getState();
+			const selectedSiteId = getSelectedSiteId( state );
+			const isFse = isSiteUsingFullSiteEditing( state, selectedSiteId );
+
+			return themeFiltersHandlers( store, {
+				...action,
+				isFse,
+			} );
+		},
 	],
 } );
