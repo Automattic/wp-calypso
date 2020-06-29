b80cd5cec93424f9c7972a5949f608a0a8f80ef6
Sérgio Gomes <mail@sgomes.com>
2020-06-29 12:20:52 +0100

    Sort alphabetically

--- client/state/ui/actions/index.js
+++ client/state/ui/actions/index.js
@@ -1,10 +1,10 @@
 /**
  * Re-exports
  */
-export { setSection, hideSidebar } from '../section/actions';
-export { showMasterbar, hideMasterbar } from '../masterbar-visibility/actions';
-export { setSelectedSiteId, setAllSitesSelected } from './set-sites';
-export { toggleNotificationsPanel } from './notifications';
-export { setPreviewShowing } from './preview';
 export { navigate } from './navigate';
 export { replaceHistory } from './history';
+export { setPreviewShowing } from './preview';
+export { setSection, hideSidebar } from '../section/actions';
+export { setSelectedSiteId, setAllSitesSelected } from './set-sites';
+export { showMasterbar, hideMasterbar } from '../masterbar-visibility/actions';
+export { toggleNotificationsPanel } from './notifications';
