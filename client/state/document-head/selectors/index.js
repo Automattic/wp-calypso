12996ee1c880e0ea8df6dde4bdfc608dd30b435b
Sérgio Gomes <mail@sgomes.com>
2020-06-29 10:47:48 +0100

    Split out `getDocumentHeadLink`

--- client/state/document-head/selectors/index.js
+++ client/state/document-head/selectors/index.js
@@ -3,14 +3,4 @@ export { getDocumentHeadUnreadCount } from 'state/document-head/selectors/get-do
 export { getDocumentHeadCappedUnreadCount } from 'state/document-head/selectors/get-document-head-capped-unread-count';
 export { getDocumentHeadFormattedTitle } from 'state/document-head/selectors/get-document-head-formatted-title';
 export { getDocumentHeadMeta } from 'state/document-head/selectors/get-document-head-meta';
-
-/**
- * Returns an array of document link objects as set by the DocumentHead
- * component or setDocumentHeadLink action.
- *
- * @param  {object}  state  Global state tree
- * @returns {object[]}       Array of link objects
- */
-export function getDocumentHeadLink( state ) {
-	return state.documentHead.link;
-}
+export { getDocumentHeadLink } from 'state/document-head/selectors/get-document-head-link';
