26a5a4f472daf9bd5af0be8cb9c12b1e2e23674c
Sérgio Gomes <mail@sgomes.com>
2020-06-29 12:41:24 +0100

    Use named exports everywhere for consistency

--- client/state/analytics/actions/index.js
+++ client/state/analytics/actions/index.js
@@ -11,9 +11,9 @@ export {
 	recordGooglePageView,
 } from './record';
 export { recordTracksEventWithClientId, recordPageViewWithClientId } from './record-with-client-id';
-export { default as withAnalytics } from './with-analytics';
-export { default as enhanceWithSiteType } from './enhance-with-site-type';
-export { default as composeAnalytics } from './compose-analytics';
+export { withAnalytics } from './with-analytics';
+export { enhanceWithSiteType } from './enhance-with-site-type';
+export { composeAnalytics } from './compose-analytics';
 export { bumpStat } from './bump-stat';
 export { loadTrackingTool } from './load-tracking-tool';
 export { setTracksOptOut } from './set-tracks-opt-out';
