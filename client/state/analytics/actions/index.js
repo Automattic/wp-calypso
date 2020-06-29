210d7f08239664608bc737548624743131d9a49d
Sérgio Gomes <mail@sgomes.com>
2020-06-29 12:38:34 +0100

    Split out `setTracksOptOut`

--- client/state/analytics/actions/index.js
+++ client/state/analytics/actions/index.js
@@ -1,9 +1,4 @@
 /**
- * Internal dependencies
- */
-import { ANALYTICS_TRACKS_OPT_OUT } from 'state/action-types';
-
-/**
  * Re-exports
  */
 export {
@@ -21,10 +16,4 @@ export { default as enhanceWithSiteType } from './enhance-with-site-type';
 export { default as composeAnalytics } from './compose-analytics';
 export { bumpStat } from './bump-stat';
 export { loadTrackingTool } from './load-tracking-tool';
-
-export function setTracksOptOut( isOptingOut ) {
-	return {
-		type: ANALYTICS_TRACKS_OPT_OUT,
-		isOptingOut,
-	};
-}
+export { setTracksOptOut } from './set-tracks-opt-out';
