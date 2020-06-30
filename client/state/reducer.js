48671d2b8b724dc5489e2fec18c08705135acd18
Sérgio Gomes <mail@sgomes.com>
2020-06-30 14:27:47 +0100

    Modularise post types state
    Fixes #42469.

--- client/state/reducer.js
+++ client/state/reducer.js
@@ -53,7 +53,6 @@ import pageTemplates from './page-templates/reducer';
 import plans from './plans/reducer';
 import plugins from './plugins/reducer';
 import postFormats from './post-formats/reducer';
-import postTypes from './post-types/reducer';
 import productsList from './products-list/reducer';
 import pushNotifications from './push-notifications/reducer';
 import receipts from './receipts/reducer';
@@ -120,7 +119,6 @@ const reducers = {
 	plans,
 	plugins,
 	postFormats,
-	postTypes,
 	productsList,
 	pushNotifications,
 	receipts,
