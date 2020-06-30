4e11be462db68bea1c655ea0e5de0547653830b1
Kerry Liu <kerryliu@gmail.com>
2020-06-30 14:47:41 -0700

    Donations: remove unneeded fragment

--- apps/full-site-editing/full-site-editing-plugin/donations/donations.js
+++ apps/full-site-editing/full-site-editing-plugin/donations/donations.js
@@ -29,83 +29,81 @@ const Donations = ( { attributes, products, setAttributes, siteSlug, stripeConne
 	}, [ oneTimePlanId ] );
 
 	return (
-		<>
-			<Block.div>
-				{ stripeConnectUrl && <StripeNudge stripeConnectUrl={ stripeConnectUrl } /> }
-				<div className="donations__container">
-					{ ( monthlyPlanId || annuallyPlanId ) && (
-						<div className="donations__tabs">
+		<Block.div>
+			{ stripeConnectUrl && <StripeNudge stripeConnectUrl={ stripeConnectUrl } /> }
+			<div className="donations__container">
+				{ ( monthlyPlanId || annuallyPlanId ) && (
+					<div className="donations__tabs">
+						<Button
+							className={ classNames( { 'is-active': isTabActive( 'one-time' ) } ) }
+							onClick={ () => setActiveTab( 'one-time' ) }
+						>
+							{ __( 'One-Time', 'full-site-editing' ) }
+						</Button>
+						{ monthlyPlanId && (
 							<Button
-								className={ classNames( { 'is-active': isTabActive( 'one-time' ) } ) }
-								onClick={ () => setActiveTab( 'one-time' ) }
+								className={ classNames( { 'is-active': isTabActive( 'monthly' ) } ) }
+								onClick={ () => setActiveTab( 'monthly' ) }
 							>
-								{ __( 'One-Time', 'full-site-editing' ) }
+								{ __( 'Monthly', 'full-site-editing' ) }
 							</Button>
-							{ monthlyPlanId && (
-								<Button
-									className={ classNames( { 'is-active': isTabActive( 'monthly' ) } ) }
-									onClick={ () => setActiveTab( 'monthly' ) }
-								>
-									{ __( 'Monthly', 'full-site-editing' ) }
-								</Button>
-							) }
-							{ annuallyPlanId && (
-								<Button
-									className={ classNames( { 'is-active': isTabActive( 'annually' ) } ) }
-									onClick={ () => setActiveTab( 'annually' ) }
-								>
-									{ __( 'Annually', 'full-site-editing' ) }
-								</Button>
-							) }
-						</div>
-					) }
-					<div className="donations__content">
-						<div>
-							{ isTabActive( 'one-time' ) && __( 'Make a one-time donation', 'full-site-editing' ) }
-							{ isTabActive( 'monthly' ) && __( 'Make a monthly donation', 'full-site-editing' ) }
-							{ isTabActive( 'annually' ) && __( 'Make a yearly donation', 'full-site-editing' ) }
-						</div>
-						{ showCustomAmount && (
-							<div>{ __( 'Or enter a custom amount', 'full-site-editing' ) } </div>
+						) }
+						{ annuallyPlanId && (
+							<Button
+								className={ classNames( { 'is-active': isTabActive( 'annually' ) } ) }
+								onClick={ () => setActiveTab( 'annually' ) }
+							>
+								{ __( 'Annually', 'full-site-editing' ) }
+							</Button>
 						) }
 					</div>
+				) }
+				<div className="donations__content">
+					<div>
+						{ isTabActive( 'one-time' ) && __( 'Make a one-time donation', 'full-site-editing' ) }
+						{ isTabActive( 'monthly' ) && __( 'Make a monthly donation', 'full-site-editing' ) }
+						{ isTabActive( 'annually' ) && __( 'Make a yearly donation', 'full-site-editing' ) }
+					</div>
+					{ showCustomAmount && (
+						<div>{ __( 'Or enter a custom amount', 'full-site-editing' ) } </div>
+					) }
 				</div>
-				<InspectorControls>
-					<PanelBody title={ __( 'Settings', 'full-site-editing' ) }>
-						<ToggleControl
-							checked={ !! monthlyPlanId }
-							onChange={ ( showMonthlyDonations ) => {
-								setAttributes( {
-									monthlyPlanId: showMonthlyDonations ? products[ '1 month' ] : null,
-								} );
-							} }
-							label={ __( 'Show monthly donations', 'full-site-editing' ) }
-						/>
-						<ToggleControl
-							checked={ !! annuallyPlanId }
-							onChange={ ( showAnnuallyDonations ) => {
-								setAttributes( {
-									annuallyPlanId: showAnnuallyDonations ? products[ '1 year' ] : null,
-								} );
-							} }
-							label={ __( 'Show annual donations', 'full-site-editing' ) }
-						/>
-						<ToggleControl
-							checked={ showCustomAmount }
-							onChange={ ( newShowCustomAmountValue ) => {
-								setAttributes( {
-									showCustomAmount: newShowCustomAmountValue,
-								} );
-							} }
-							label={ __( 'Show custom amount option', 'full-site-editing' ) }
-						/>
-						<ExternalLink href={ `https://wordpress.com/earn/payments/${ siteSlug }` }>
-							{ __( 'View donation earnings', 'full-site-editing' ) }
-						</ExternalLink>
-					</PanelBody>
-				</InspectorControls>
-			</Block.div>
-		</>
+			</div>
+			<InspectorControls>
+				<PanelBody title={ __( 'Settings', 'full-site-editing' ) }>
+					<ToggleControl
+						checked={ !! monthlyPlanId }
+						onChange={ ( showMonthlyDonations ) => {
+							setAttributes( {
+								monthlyPlanId: showMonthlyDonations ? products[ '1 month' ] : null,
+							} );
+						} }
+						label={ __( 'Show monthly donations', 'full-site-editing' ) }
+					/>
+					<ToggleControl
+						checked={ !! annuallyPlanId }
+						onChange={ ( showAnnuallyDonations ) => {
+							setAttributes( {
+								annuallyPlanId: showAnnuallyDonations ? products[ '1 year' ] : null,
+							} );
+						} }
+						label={ __( 'Show annual donations', 'full-site-editing' ) }
+					/>
+					<ToggleControl
+						checked={ showCustomAmount }
+						onChange={ ( newShowCustomAmountValue ) => {
+							setAttributes( {
+								showCustomAmount: newShowCustomAmountValue,
+							} );
+						} }
+						label={ __( 'Show custom amount option', 'full-site-editing' ) }
+					/>
+					<ExternalLink href={ `https://wordpress.com/earn/payments/${ siteSlug }` }>
+						{ __( 'View donation earnings', 'full-site-editing' ) }
+					</ExternalLink>
+				</PanelBody>
+			</InspectorControls>
+		</Block.div>
 	);
 };
 
