17424707c70473828f02d642904acefd95fbf93e
Payton Swick <payton@foolord.com>
2020-07-01 18:18:19 -0400

    Reformat bancontact payment processor

--- client/my-sites/checkout/composite-checkout/composite-checkout.js
+++ client/my-sites/checkout/composite-checkout/composite-checkout.js
@@ -482,7 +482,12 @@ export default function CompositeCheckout( {
 			p24: ( transactionData ) =>
 				genericRedirectProcessor( 'p24', transactionData, getThankYouUrl, isWhiteGloveOffer ),
 			bancontact: ( transactionData ) =>
-				genericRedirectProcessor( 'bancontact', transactionData, getThankYouUrl, isWhiteGloveOffer ),
+				genericRedirectProcessor(
+					'bancontact',
+					transactionData,
+					getThankYouUrl,
+					isWhiteGloveOffer
+				),
 			giropay: ( transactionData ) =>
 				genericRedirectProcessor( 'giropay', transactionData, getThankYouUrl, isWhiteGloveOffer ),
 			wechat: ( transactionData ) =>
