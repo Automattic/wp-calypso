92e5978ac4cd6dd7166d83f743557a8d08261421
Nicholas Sakaimbo <nsakaimbo@users.noreply.github.com>
2020-07-01 15:08:04 -0400

    Revert "ci: add wp-desktop Slack + Github notifications (#43837)"
    This reverts commit 9ee1c141ff26e2a548fbd4ce7d178740929d1b06.

--- desktop/bin/github/add-review.js
+++
@@ -1,7 +0,0 @@
-const { addReview } = require( './review-actions' );
-
-try {
-	addReview();
-} catch ( error ) {
-	console.log( 'ERROR: failed to add review: ', error ); // eslint-disable-line no-console
-}
