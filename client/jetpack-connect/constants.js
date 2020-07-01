5fdfb68495e94547e99ba6e9bfe91ace02f7526f
Sara Marcondes <saram@fastmail.com>
2020-07-01 10:23:55 -0700

    Jetpack Connect: Remove problematic "blacklist"/"whitelist" language #43395 (#43647)

--- client/jetpack-connect/constants.js
+++ client/jetpack-connect/constants.js
@@ -7,4 +7,4 @@ export const REMOTE_PATH_AUTH =
 	'/wp-admin/admin.php?page=jetpack&connect_url_redirect=true&jetpack_connect_login_redirect=true';
 export const REMOTE_PATH_INSTALL =
 	'/wp-admin/plugin-install.php?tab=plugin-information&plugin=jetpack';
-export const MOBILE_APP_REDIRECT_URL_WHITELIST = [ /^wordpress:\/\// ];
+export const ALLOWED_MOBILE_APP_REDIRECT_URL_LIST = [ /^wordpress:\/\// ];
