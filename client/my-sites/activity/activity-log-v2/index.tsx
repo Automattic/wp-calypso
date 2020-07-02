f8885c372a0254d2ae79ff044158b91dce6b77d2
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-07-02 14:37:57 -0500

    Hide Activity Log v2 filter on free plans

--- client/my-sites/activity/activity-log-v2/index.tsx
+++ client/my-sites/activity/activity-log-v2/index.tsx
@@ -5,17 +5,22 @@ import { useSelector } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
 import classNames from 'classnames';
 import React, { FunctionComponent, useEffect } from 'react';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies
  */
+import QuerySitePlans from 'components/data/query-site-plans';
+import { isFreePlan } from 'lib/plans';
 import { getHttpData } from 'state/data-layer/http-data';
-import { getSelectedSiteId } from 'state/ui/selectors';
 import { requestActivityLogs, getRequestActivityLogsId } from 'state/data-getters';
+import { getCurrentPlan } from 'state/sites/plans/selectors';
+import { getSelectedSiteId } from 'state/ui/selectors';
 import ActivityCardList from 'components/activity-card-list';
 import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
 import getActivityLogFilter from 'state/selectors/get-activity-log-filter';
+import isVipSite from 'state/selectors/is-vip-site';
 import isJetpackCloud from 'lib/jetpack/is-jetpack-cloud';
 import Main from 'components/main';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
@@ -32,6 +37,13 @@ const ActivityLogV2: FunctionComponent = () => {
 	const siteId = useSelector( getSelectedSiteId );
 	const filter = useSelector( ( state ) => getActivityLogFilter( state, siteId ) );
 	const logs = useSelector( () => getHttpData( getRequestActivityLogsId( siteId, filter ) ).data );
+	const siteIsOnFreePlan = useSelector(
+		( state ) =>
+			isFreePlan( get( getCurrentPlan( state, siteId ), 'productSlug' ) ) &&
+			! isVipSite( state, siteId )
+	);
+
+	const showFilter = ! siteIsOnFreePlan;
 
 	// when the filter changes, re-request the logs
 	useEffect( () => {
@@ -45,6 +57,7 @@ const ActivityLogV2: FunctionComponent = () => {
 			} ) }
 			wideLayout={ ! isJetpackCloud() }
 		>
+			{ siteId && <QuerySitePlans siteId={ siteId } /> }
 			<DocumentHead title={ translate( 'Activity log' ) } />
 			<SidebarNavigation />
 			<PageViewTracker path="/activity-log/:site" title="Activity log" />
@@ -67,7 +80,7 @@ const ActivityLogV2: FunctionComponent = () => {
 				/>
 			) }
 			<div className="activity-log-v2__content">
-				<ActivityCardList logs={ logs } pageSize={ 10 } />
+				<ActivityCardList logs={ logs } pageSize={ 10 } showFilter={ showFilter } />
 			</div>
 		</Main>
 	);
