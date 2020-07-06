87e178ec3d8a82cf9556985f9afa82dabab7de66
Philip Jackson <p-jackson@live.com>
2020-07-06 16:28:16 +1200

    Nav sidebar: sidebar opens over the editor, behaves like a modal (#43510)

--- apps/full-site-editing/full-site-editing-plugin/wpcom-block-editor-nav-sidebar/src/attach-sidebar.tsx
+++ apps/full-site-editing/full-site-editing-plugin/wpcom-block-editor-nav-sidebar/src/attach-sidebar.tsx
@@ -1,15 +1,14 @@
 /**
  * External dependencies
  */
-import { useDispatch, useSelect } from '@wordpress/data';
-import { useEffect } from '@wordpress/element';
+import { useDispatch } from '@wordpress/data';
+import { useEffect, createPortal, useState } from '@wordpress/element';
 import { __experimentalMainDashboardButton as MainDashboardButton } from '@wordpress/interface';
 import { registerPlugin as originalRegisterPlugin, PluginSettings } from '@wordpress/plugins';
 
 /**
  * Internal dependencies
  */
-import { STORE_KEY } from './constants';
 import WpcomBlockEditorNavSidebar from './components/nav-sidebar';
 import ToggleSidebarButton from './components/toggle-sidebar-button';
 
@@ -19,7 +18,6 @@ const registerPlugin = ( name: string, settings: Omit< PluginSettings, 'icon' > 
 registerPlugin( 'a8c-full-site-editing-nav-sidebar', {
 	render: function NavSidebar() {
 		const { addEntities } = useDispatch( 'core' );
-		const isSidebarOpened = useSelect( ( select ) => select( STORE_KEY ).isSidebarOpened() );
 
 		useEffect( () => {
 			// Teach core data about the status entity so we can use selectors like `getEntityRecords()`
@@ -37,29 +35,18 @@ registerPlugin( 'a8c-full-site-editing-nav-sidebar', {
 			// eslint-disable-next-line react-hooks/exhaustive-deps
 		}, [] );
 
-		// TODO: remove this effect once sidebar opens over the editor and doesn't
-		// squish the editor content.
+		const [ clickGuardRoot ] = useState( () => document.createElement( 'div' ) );
 		useEffect( () => {
-			// Classes need to be attached to elements that aren't controlled by React,
-			// otherwise our alterations will be removed when React re-renders. So attach
-			// to <body> element.
-			document.body.classList.add( 'is-wpcom-block-editor-nav-sidebar-attached' );
-		}, [] );
-
-		// TODO: remove this effect once sidebar opens over the editor and doesn't
-		// squish the editor content.
-		useEffect( () => {
-			if ( isSidebarOpened ) {
-				document.body.classList.add( 'is-wpcom-block-editor-nav-sidebar-opened' );
-			} else {
-				document.body.classList.remove( 'is-wpcom-block-editor-nav-sidebar-opened' );
-			}
-		}, [ isSidebarOpened ] );
+			document.body.appendChild( clickGuardRoot );
+			return () => {
+				document.body.removeChild( clickGuardRoot );
+			};
+		} );
 
 		return (
 			<MainDashboardButton>
 				<ToggleSidebarButton />
-				<WpcomBlockEditorNavSidebar />
+				{ createPortal( <WpcomBlockEditorNavSidebar />, clickGuardRoot ) }
 			</MainDashboardButton>
 		);
 	},
