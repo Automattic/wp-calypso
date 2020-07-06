1f8eeb40a9a515fa0ead0deb363523fec7215eca
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-07-06 17:59:20 -0500

    Fix upgrade link

--- client/my-sites/activity/activity-log-v2/index.tsx
+++ client/my-sites/activity/activity-log-v2/index.tsx
@@ -61,7 +61,7 @@ const ActivityLogV2: FunctionComponent = () => {
 			bodyText={ translate(
 				'With your free plan, you can monitor the 20 most recent events. A paid plan unlocks more powerful features. You can access all site activity for the last 30 days and filter events by type and date range to quickly find the information you need. '
 			) }
-			buttonLink={ `https://jetpack.com/upgrade/backup/?site=${ selectedSiteSlug }` }
+			buttonLink={ `https://wordpress.com/plans/${ selectedSiteSlug }?feature=offsite-backup-vaultpress-daily&plan=jetpack_personal_monthly` }
 			buttonText={ translate( 'Upgrade Now' ) }
 			onClick={ () =>
 				dispatch( recordTracksEvent( 'calypso_jetpack_activity_log_upgrade_click' ) )
