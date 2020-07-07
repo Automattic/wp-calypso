2ae8e5cb01b76182c5acc05803abdba30d9d5a89
Damián Suárez <rdsuarez@gmail.com>
2020-07-07 14:25:55 -0300

    InlineHelp: Add admin help sections data (#43790)
    * inline-help: add admin sections file
    
    * inline-help: use simple quotes instead of `
    
    * inline-help: add new admin items. tidy code.
    
    * inline-help: add a selector for admin sections
    
    * inline-help: add unit test for real search term
    
    * inline-help: tidy code
    
    * inline-help: tidy code
    
    * inline-help: move admin helper function to cmp
    
    * inline-help: add unit tests for admin sections
    yarn test-client client/blocks/inline-help/test
    
    * inline-help: formcat code. tidy.
    
    * inline-help: remove duplicated test
    
    * inline-help: fixing link errors
    
    * inline-help: add site icon help
    
    * Update client/blocks/inline-help/admin-sections.js
    
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>
    
    * Update client/blocks/inline-help/admin-sections.js
    
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>
    
    * Update client/blocks/inline-help/admin-sections.js
    
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>
    
    * inline-help: format code
    
    * inline-help: early return if term is empty
    
    * Generalise limit description
    
    Co-authored-by: Dave Smith <getdavemail@gmail.com>
    
    * Simplify check for searchTermWords value
    
    Co-authored-by: Dave Smith <getdavemail@gmail.com>
    
    * Add default arg to siteSlug
    
    * Rename test file to match admin-sections.js
    
    * Change test descriptions to use natural language
    
    * inine-help: add synonyms to admin sections
    
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>
    Co-authored-by: Michael P. Pfeiffer <frontdevde@users.noreply.github.com>
    Co-authored-by: Dave Smith <getdavemail@gmail.com>
    Co-authored-by: Michael P. Pfeiffer <michael@cssconf.eu>

---
+++ client/state/inline-help/selectors/get-admin-help-results.js
@@ -0,0 +1,29 @@
+/**
+ * External dependencies
+ */
+
+/**
+ * Internal dependencies
+ */
+import { adminSections, filterListBySearchTerm } from 'blocks/inline-help/admin-sections';
+
+/**
+ * Returns a filtered site admin collection using the memoized adminSections.
+ *
+ * Note that the first argument `state` is not used,
+ * because the admin sections are store in the admin-sections.js,
+ * in the inline-block component.
+ *
+ * @param   {object} state  Global state tree
+ * @param   {string} searchTerm The search term
+ * @param   {string} siteSlug   The current site slug
+ * @param   {number} limit      The maximum number of results to show
+ * @returns {Array}             A filtered (or empty) array
+ */
+export default function getAdminHelpResults( state, searchTerm = '', siteSlug = '', limit ) {
+	if ( ! searchTerm ) {
+		return [];
+	}
+
+	return filterListBySearchTerm( searchTerm, adminSections( siteSlug ), limit );
+}
