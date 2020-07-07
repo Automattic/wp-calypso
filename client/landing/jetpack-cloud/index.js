27a4e24a72798649ff794b4843180b9c8713550b
Matt Gawarecki <matt.gawarecki@gmail.com>
2020-07-07 10:19:29 -0500

    Wire up landing controller and routes

--- client/landing/jetpack-cloud/index.js
+++ client/landing/jetpack-cloud/index.js
@@ -7,8 +7,24 @@ import React from 'react';
 /**
  * Internal dependencies
  */
+import { translate } from 'i18n-calypso';
 import { makeLayout, render as clientRender } from 'controller';
 import Home from './sections/home';
+import { siteSelection, sites, navigation } from 'my-sites/controller';
+import Landing from './sections/landing';
+
+const selectionPrompt = ( context, next ) => {
+	context.getSiteSelectionHeaderText = () =>
+		translate( 'Select a site to open {{strong}}Jetpack.com{{/strong}}', {
+			components: { strong: <strong /> },
+		} );
+	next();
+};
+
+const landingController = ( context, next ) => {
+	context.primary = <Landing />;
+	next();
+};
 
 export default function () {
 	const homeController = ( context, next ) => {
@@ -16,4 +32,6 @@ export default function () {
 		next();
 	};
 	page( '/', homeController, makeLayout, clientRender );
+	page( '/landing', siteSelection, selectionPrompt, sites, navigation, makeLayout, clientRender );
+	page( '/landing/:site', siteSelection, landingController, makeLayout, clientRender );
 }
