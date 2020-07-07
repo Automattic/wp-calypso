bf9004bebdf663339c5ca89d061e77bd7f2a1936
Seni Oluwunmi <277661+olaseni@users.noreply.github.com>
2020-07-07 04:05:12 +0100

    Install placeholders for action items

--- client/my-sites/domains/domain-management/list/domain-item.jsx
+++ client/my-sites/domains/domain-management/list/domain-item.jsx
@@ -112,6 +112,47 @@ class DomainItem extends PureComponent {
 		);
 	}
 
+	renderActionItems() {
+		const { isLoadingDomainDetails, domainDetails } = this.props;
+
+		if ( isLoadingDomainDetails ) {
+			return (
+				<>
+					<div className="list__domain-transfer-lock list__action_item_placeholder" />
+					<div className="list__domain-privacy list__action_item_placeholder" />
+					<div className="list__domain-auto-renew list__action_item_placeholder" />
+					<div className="list__domain-email list__action_item_placeholder" />
+					<div className="list__domain-options list__action_item_placeholder" />
+				</>
+			);
+		}
+
+		return (
+			<>
+				<div className="list__domain-transfer-lock">
+					{ domainDetails?.isLocked && (
+						<Gridicon className="domain-item__icon" size={ 18 } icon="checkmark" />
+					) }
+				</div>
+				<div className="list__domain-privacy">
+					{ domainDetails?.privateDomain && (
+						<Gridicon className="domain-item__icon" size={ 18 } icon="checkmark" />
+					) }
+				</div>
+				<div className="list__domain-auto-renew">
+					{ domainDetails?.bundledPlanSubscriptionId && (
+						<Gridicon className="domain-item__icon" size={ 18 } icon="minus" />
+					) }
+					{ ! domainDetails?.bundledPlanSubscriptionId && domainDetails?.isAutoRenewing && (
+						<Gridicon className="domain-item__icon" size={ 18 } icon="checkmark" />
+					) }
+				</div>
+				<div className="list__domain-email">{ this.renderEmail( domainDetails ) }</div>
+				{ this.renderOptionsButton() }
+			</>
+		);
+	}
+
 	getSiteName( site ) {
 		if ( site.name ) {
 			return `${ site.name } (${ withoutHttp( site.URL ) })`;
@@ -153,26 +194,7 @@ class DomainItem extends PureComponent {
 						</div>
 					) }
 				</div>
-				<div className="list__domain-transfer-lock">
-					{ domainDetails?.isLocked && (
-						<Gridicon className="domain-item__icon" size={ 18 } icon="checkmark" />
-					) }
-				</div>
-				<div className="list__domain-privacy">
-					{ domainDetails?.privateDomain && (
-						<Gridicon className="domain-item__icon" size={ 18 } icon="checkmark" />
-					) }
-				</div>
-				<div className="list__domain-auto-renew">
-					{ domainDetails?.bundledPlanSubscriptionId && (
-						<Gridicon className="domain-item__icon" size={ 18 } icon="minus" />
-					) }
-					{ ! domainDetails?.bundledPlanSubscriptionId && domainDetails?.isAutoRenewing && (
-						<Gridicon className="domain-item__icon" size={ 18 } icon="checkmark" />
-					) }
-				</div>
-				<div className="list__domain-email">{ this.renderEmail( domainDetails ) }</div>
-				{ this.renderOptionsButton() }
+				{ this.renderActionItems() }
 			</CompactCard>
 		);
 	}
