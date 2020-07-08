6b1cfc4a1e8eb5a32e8d3d8ab269c2458b12437d
cpap <papazoglou.charalampos@gmail.com>
2020-07-08 11:16:36 +0300

    Sites Checklist: transform api data so that: - calypso handles only known tasks, regardless what wpcom serves - remove legacy data conversion, task are an Array already

--- client/state/data-layer/wpcom/checklist/index.js
+++ client/state/data-layer/wpcom/checklist/index.js
@@ -13,10 +13,39 @@ import { receiveSiteChecklist } from 'state/checklist/actions';
 
 import { registerHandlers } from 'state/data-layer/handler-registry';
 
-// The checklist API requests use the http_envelope query param, however on
-// desktop the envelope is not being unpacked for some reason. This conversion
-// ensures the payload has been unpacked.
-const fromApi = ( payload ) => get( payload, 'body', payload );
+// A list of known tasks for the calypso client/my-sites/customer-home/cards/tasks/site-setup-list/get-task.js
+const knownTaskIds = [
+	'site_created',
+	'domain_verified',
+	'email_verified',
+	'blogname_set',
+	'mobile_app_installed',
+	'site_launched',
+	'front_page_updated',
+	'site_menu_updated',
+];
+
+// Transform the response to a data / schema calypso understands, eg filter out unknown tasks
+const fromApi = ( payload ) => {
+	// The checklist API requests use the http_envelope query param, however on
+	// desktop the envelope is not being unpacked for some reason. This conversion
+	// ensures the payload has been unpacked.
+	const data = get( payload, 'body', payload );
+
+	if ( ! data ) {
+		throw new TypeError( `Missing 'body' property on API response` );
+	}
+	if ( ! Array.isArray( data.tasks ) ) {
+		throw new TypeError( `API response needs array of tasks: found ${ typeof data.tasks }` );
+	}
+	return {
+		designType: data.designType,
+		phase2: data.phase2,
+		segment: data.segment,
+		verticals: data.verticals,
+		tasks: data.tasks.filter( ( task ) => knownTaskIds.includes( task.id ) ),
+	};
+};
 
 export const fetchChecklist = ( action ) =>
 	http(
@@ -33,24 +62,7 @@ export const fetchChecklist = ( action ) =>
 	);
 
 export const receiveChecklistSuccess = ( action, receivedChecklist ) => {
-	let checklist = receivedChecklist;
-
-	// Legacy object-based data format, let's convert it to the new array-based format and ultimately remove it.
-	if ( ! Array.isArray( receivedChecklist.tasks ) ) {
-		checklist = {
-			...receivedChecklist,
-			tasks: Object.keys( receivedChecklist.tasks ).map( ( taskId ) => {
-				const { completed, ...rest } = receivedChecklist.tasks[ taskId ];
-				return {
-					id: taskId,
-					isCompleted: completed,
-					...rest,
-				};
-			} ),
-		};
-	}
-
-	return receiveSiteChecklist( action.siteId, checklist );
+	return receiveSiteChecklist( action.siteId, receivedChecklist );
 };
 
 const dispatchChecklistRequest = dispatchRequest( {
