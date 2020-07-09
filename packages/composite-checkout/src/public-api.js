51e8c5e9712926578a24767324cefc80111ce2c8
Michael Cain <michael@michaeldcain.com>
2020-07-09 17:28:32 -0400

    Export theme from composite-checkout package

--- packages/composite-checkout/src/public-api.js
+++ packages/composite-checkout/src/public-api.js
@@ -72,6 +72,7 @@ import { useFormStatus } from './lib/form-status';
 import { CheckIcon as CheckoutCheckIcon } from './components/shared-icons';
 import { useTransactionStatus } from './lib/transaction-status';
 import { usePaymentProcessor } from './lib/payment-processors';
+import checkoutTheme from './theme';
 
 // Re-export the public API
 export {
@@ -140,4 +141,5 @@ export {
 	useSetStepComplete,
 	useTotal,
 	useTransactionStatus,
+	checkoutTheme,
 };
