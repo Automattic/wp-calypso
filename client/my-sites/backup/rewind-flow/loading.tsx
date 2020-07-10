af5deffb962fe883a4effff99d9e8ddaa46e9918
Caleb Bauermeister <caleb.bauermeister@automattic.com>
2020-07-10 09:38:24 -0700

    Jetpack Cloud: Disable downloadable backup creation while another one is in progress  (#43850)

--- client/my-sites/backup/rewind-flow/loading.tsx
+++ client/my-sites/backup/rewind-flow/loading.tsx
@@ -4,17 +4,12 @@
 import { useTranslate } from 'i18n-calypso';
 import React, { FunctionComponent } from 'react';
 
-/**
- * Internal dependencies
- */
-import Spinner from 'components/spinner';
-
 const RewindFlowLoading: FunctionComponent = () => {
 	const translate = useTranslate();
 	return (
 		<>
 			<div className="rewind-flow__header">
-				<Spinner size={ 48 } />
+				<div className="rewind-flow__icon-placeholder" />
 			</div>
 			<h3 className="rewind-flow__title-placeholder">{ translate( 'Loading restore status…' ) }</h3>
 		</>
