b9a0bab1361cf01affb606b47926999a9212246a
Glen Davies <glendaviesnz@users.noreply.github.com>
2020-07-14 02:38:13 +1200

    Editor: add a classic block guide for users in deprecation group (#43821)
    * Add classic block guide
    
    * Test that useDispatch works in this context
    
    * Add styles for the modal
    
    * Use registerPlugin instead of pushing into dom and disable standard nux guide by default
    
    * Set local storage flag when notice dismissed
    
    * Update to match the designs
    
    * copy change
    
    * Switch to the proper query param and remove hard coded value
    
    * Update wording and images
    
    Co-authored-by: Glen Davies <glen.davies@a8c.com>
    Co-authored-by: Ben Dwyer <ben@scruffian.com>

--- apps/wpcom-block-editor/src/wpcom/editor.js
+++ apps/wpcom-block-editor/src/wpcom/editor.js
@@ -16,6 +16,12 @@ import './features/fix-block-invalidation-errors';
 import './features/reorder-block-categories';
 import './features/tracking';
 import './features/convert-to-blocks-button';
+import './features/use-classic-block-guide';
+
+/**
+ * Style dependencies
+ */
+import './editor.scss';
 
 import InserterMenuTrackingEvent from './features/tracking/wpcom-inserter-menu-search-term';
 
