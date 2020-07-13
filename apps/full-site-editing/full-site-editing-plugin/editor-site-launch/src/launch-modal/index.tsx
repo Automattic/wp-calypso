ab57407ac08363edbebce26ccbe2794fdc7d6924
Yan Sern <yan.sern@automattic.com>
2020-07-13 05:17:18 +0200

    Use components from @automattic/onboarding package.

--- apps/full-site-editing/full-site-editing-plugin/editor-site-launch/src/launch-modal/index.tsx
+++ apps/full-site-editing/full-site-editing-plugin/editor-site-launch/src/launch-modal/index.tsx
@@ -2,13 +2,14 @@
  * External dependencies
  */
 import * as React from 'react';
-import { Modal } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { Modal, Button } from '@wordpress/components';
+import { Icon, wordpress, close } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import './styles.scss';
-import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -28,27 +29,44 @@ const LaunchModal: React.FunctionComponent< Props > = ( {
 	step,
 	isLaunching,
 } ) => {
+	const [ isOpen, setOpen ] = React.useState( true );
+
 	const handleClose = () => {
+		setOpen( false );
 		onClose?.();
 	};
 
 	return (
-		<Modal
-			className="nux-launch-modal"
-			overlayClassName="nux-launch-modal-overlay"
-			bodyOpenClassName="has-nux-launch-modal"
-			onRequestClose={ handleClose }
-			title={
-				isLaunching
-					? __( 'Hooray! Your site will be ready shortly.', 'full-site-editing' )
-					: __(
-							"You're almost there! Review a few things before launching your site!",
-							'full-site-editing'
-					  )
-			}
-		>
-			{ isLaunching ? 'launch animation' : <Launch step={ step } onSubmit={ onSubmit }></Launch> }
-		</Modal>
+		<>
+			{ isOpen && (
+				<Modal
+					className="nux-launch-modal"
+					overlayClassName="nux-launch-modal-overlay"
+					bodyOpenClassName="has-nux-launch-modal"
+					onRequestClose={ handleClose }
+					title=""
+				>
+					<div className="nux-launch-modal-header">
+						<div className="nux-launch-modal-header__wp-logo">
+							<Icon icon={ wordpress } size={ 36 } />
+						</div>
+						<Button
+							isLink
+							className="nux-launch-modal-header__close-button"
+							onClick={ onClose }
+							aria-label={ __( 'Close dialog', 'full-site-editing' ) }
+						>
+							<Icon icon={ close } size={ 24 } />
+						</Button>
+					</div>
+					{ isLaunching ? (
+						'launch animation'
+					) : (
+						<Launch step={ step } onSubmit={ onSubmit }></Launch>
+					) }
+				</Modal>
+			) }
+		</>
 	);
 };
 
