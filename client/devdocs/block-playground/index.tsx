e8b92ea4c8cce3664ab885b50964663095e3164a
Dennis Snell <dennis.snell@automattic.com>
2020-07-14 11:17:40 -0700

    Load block editor with core blocks

--- client/devdocs/block-playground/index.tsx
+++ client/devdocs/block-playground/index.tsx
@@ -12,17 +12,36 @@ import DocumentHead from 'components/data/document-head';
 /**
  * WordPress dependencies
  */
-import { BlockEditorProvider, BlockList } from '@wordpress/block-editor';
+import {
+	BlockEditorProvider,
+	BlockList,
+	ObserveTyping,
+	WritingFlow,
+} from '@wordpress/block-editor';
+import { registerCoreBlocks } from '@wordpress/block-library';
+import type { BlockInstance } from '@wordpress/blocks';
+
+import '@wordpress/components/build-style/style.css';
+import '@wordpress/block-editor/build-style/style.css';
+import '@wordpress/block-library/build-style/style.css';
+import '@wordpress/block-library/build-style/editor.css';
+import '@wordpress/block-library/build-style/theme.css';
+
+registerCoreBlocks();
 
 export const BlockPlayground: FunctionComponent = () => {
-	const [ blocks, setBlocks ] = useState< object[] >( [] );
+	const [ blocks, setBlocks ] = useState< BlockInstance[] >( [] );
 
 	return (
 		<Main className="devdocs block-playground">
 			<DocumentHead title="Block Playground" />
 			<div>
 				<BlockEditorProvider value={ blocks } onInput={ setBlocks } onChange={ setBlocks }>
-					<BlockList />
+					<WritingFlow>
+						<ObserveTyping>
+							<BlockList />
+						</ObserveTyping>
+					</WritingFlow>
 				</BlockEditorProvider>
 			</div>
 		</Main>
