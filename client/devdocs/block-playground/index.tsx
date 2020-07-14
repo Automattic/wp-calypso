a4f42b0c2378ebb1ce53039aa6d40de74f98bdf3
Dennis Snell <dennis.snell@automattic.com>
2020-07-14 16:10:52 -0700

    Add text area to prefill post with content

--- client/devdocs/block-playground/index.tsx
+++ client/devdocs/block-playground/index.tsx
@@ -19,7 +19,7 @@ import {
 	WritingFlow,
 } from '@wordpress/block-editor';
 import { registerCoreBlocks } from '@wordpress/block-library';
-import { BlockInstance, serialize } from '@wordpress/blocks';
+import { BlockInstance, parse, serialize } from '@wordpress/blocks';
 
 import '@wordpress/components/build-style/style.css';
 import '@wordpress/block-editor/build-style/style.css';
@@ -37,12 +37,17 @@ export const BlockPlayground: FunctionComponent = () => {
 	}, [] );
 
 	const [ blocks, setBlocks ] = useState< BlockInstance[] >( [] );
+	const [ post, setPost ] = useState< string >( '' );
+	const update = ( newBlocks: BlockInstance[] ) => {
+		setBlocks( newBlocks );
+		setPost( serialize( newBlocks ) );
+	};
 
 	return (
 		<Main className="devdocs block-playground">
 			<DocumentHead title="Block Playground" />
 			<div>
-				<BlockEditorProvider value={ blocks } onInput={ setBlocks } onChange={ setBlocks }>
+				<BlockEditorProvider value={ blocks } onInput={ update } onChange={ update }>
 					<WritingFlow>
 						<ObserveTyping>
 							<BlockList />
@@ -50,7 +55,12 @@ export const BlockPlayground: FunctionComponent = () => {
 					</WritingFlow>
 				</BlockEditorProvider>
 			</div>
-			<pre>{ serialize( blocks ) }</pre>
+			<textarea
+				style={ { minHeight: '480px' } }
+				value={ post }
+				onChange={ ( event ) => setPost( event.target.value ) }
+				onBlur={ () => setBlocks( parse( post ) ) }
+			/>
 		</Main>
 	);
 };
