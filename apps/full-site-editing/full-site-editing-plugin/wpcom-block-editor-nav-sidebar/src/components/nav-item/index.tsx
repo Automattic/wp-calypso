58c0c3dda692e10851ab450f5ffe628808381569
Philip Jackson <p-jackson@live.com>
2020-07-14 13:57:22 +1200

    Don't wrap long page titles and show tooltip

--- apps/full-site-editing/full-site-editing-plugin/wpcom-block-editor-nav-sidebar/src/components/nav-item/index.tsx
+++ apps/full-site-editing/full-site-editing-plugin/wpcom-block-editor-nav-sidebar/src/components/nav-item/index.tsx
@@ -44,7 +44,7 @@ export default function NavItem( { item, postType, selected, statusLabel }: NavI
 				href={ editUrl }
 				target={ applyFilters( 'a8c.WpcomBlockEditorNavSidebar.linkTarget', undefined ) }
 			>
-				<div className={ titleClasses }>
+				<div className={ titleClasses } title={ item.title?.raw }>
 					{ item.title?.raw ||
 						_x( 'Untitled', 'post title for posts with no title', 'full-site-editing' ) }
 				</div>
