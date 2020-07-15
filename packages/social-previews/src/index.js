2c19ed8778a7c0ee7f1c7e5e1494d17a4a776eec
Dave Smith <getdavemail@gmail.com>
2020-07-15 15:20:46 +0100

    Expose search preview and assert

--- packages/social-previews/src/index.js
+++ packages/social-previews/src/index.js
@@ -1,5 +1,6 @@
 export { default as Facebook } from './facebook-preview';
 export { default as Twitter } from './twitter-preview';
+export { default as Search } from './search-preview';
 
 // TODO: fix dependency issues and restore
 // export { default as Reader } from './reader-preview';
