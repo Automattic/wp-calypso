65bdaa707fec2a22db2f7a8ca588b43dc87b69fc
Dave Smith <getdavemail@gmail.com>
2020-07-15 15:39:34 +0100

    Test FB preview image rendering

--- packages/social-previews/test/index.js
+++ packages/social-previews/test/index.js
@@ -1,11 +1,28 @@
 /**
  * Internal dependencies
  */
+import React from 'react';
 import { Facebook, Twitter, Reader, Search } from '../src';
+import { shallow } from 'enzyme';
+
+const DUMMY_IMAGE_SRC = 'https://wordpress.com/someimagehere';
 
 describe( 'Facebook previews', () => {
 	it( 'should expose a Facebook preview component', () => {
 		expect( Facebook ).not.toBe( undefined );
+	} );
+
+	it( 'should render image only when provided', () => {
+		const wrapperNoImage = shallow( <Facebook /> );
+		const wrapperWithImage = shallow( <Facebook image={ DUMMY_IMAGE_SRC } /> );
+
+		// No image
+		expect( wrapperNoImage.find( 'img[alt="Facebook Preview Thumbnail"]' ).exists() ).toBeFalsy();
+
+		// Has image
+		const imageEl = wrapperWithImage.find( 'img[alt="Facebook Preview Thumbnail"]' );
+		expect( imageEl.exists() ).toBeTruthy();
+		expect( imageEl.html() ).toContain( `src="${ DUMMY_IMAGE_SRC }"` );
 	} );
 } );
 
