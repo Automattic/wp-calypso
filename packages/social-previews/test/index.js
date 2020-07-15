a372d510473d9e523a005d16fc6532903d83cdb1
Dave Smith <getdavemail@gmail.com>
2020-07-15 14:23:57 +0100

    Test assert Facebook component can be accessed.

--- packages/social-previews/test/index.js
+++ packages/social-previews/test/index.js
@@ -1,1 +1,10 @@
-// Silence is golden
+/**
+ * Internal dependencies
+ */
+import { Facebook } from '../src';
+
+describe( 'Facebook preview', () => {
+	it( 'should expose a Facebook preview component', () => {
+		expect( Facebook ).not.toBe( undefined );
+	} );
+} );
