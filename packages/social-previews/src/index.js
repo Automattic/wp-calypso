6aaa22d8e7fc2676a148757023448adb4327daa9
Dave Smith <getdavemail@gmail.com>
2020-07-15 14:25:10 +0100

    Expose Twitter previews and test

--- packages/social-previews/src/index.js
+++ packages/social-previews/src/index.js
@@ -1,1 +1,2 @@
 export { default as Facebook } from './facebook-preview';
+export { default as Twitter } from './twitter-preview';
--- packages/social-previews/test/index.js
+++ packages/social-previews/test/index.js
@@ -1,10 +1,16 @@
 /**
  * Internal dependencies
  */
-import { Facebook } from '../src';
+import { Facebook, Twitter } from '../src';
 
-describe( 'Facebook preview', () => {
+describe( 'Facebook previews', () => {
 	it( 'should expose a Facebook preview component', () => {
 		expect( Facebook ).not.toBe( undefined );
+	} );
+} );
+
+describe( 'Twitter previews', () => {
+	it( 'should expose a Twitter preview component', () => {
+		expect( Twitter ).not.toBe( undefined );
 	} );
 } );
