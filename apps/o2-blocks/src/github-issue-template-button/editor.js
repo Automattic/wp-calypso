c5fba684012fa1a312654d95737f0596142a238a
Marcelo Serpa <marcelo.serpa@automattic.com>
2020-07-15 19:47:02 -0500

    Implement fixes and improvements after code review
    - Take advantage of the built-in align controls;
    - Show a message and icon to communicate that the button should be configured if required data is still not set;
    - Add required help hints in the required fields in the settings panel.

--- apps/o2-blocks/src/github-issue-template-button/editor.js
+++ apps/o2-blocks/src/github-issue-template-button/editor.js
@@ -3,7 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
-import { TextControl, TextareaControl, Button, CustomSelectControl } from '@wordpress/components';
+import { TextControl, TextareaControl, Button as WordpressButton } from '@wordpress/components';
 import { Icon } from '@wordpress/icons';
 import { InspectorControls } from '@wordpress/block-editor';
 import * as newGithubIssueUrl from 'new-github-issue-url';
@@ -25,6 +25,23 @@ const icon = (
 			d="M335 499c14 0 12 17 12 17H165s-2-17 12-17c13 0 16-6 16-12l-1-50c-71 16-86-28-86-28-12-30-28-37-28-37-24-16 1-16 1-16 26 2 40 26 40 26 22 39 59 28 74 22 2-17 9-28 16-35-57-6-116-28-116-126 0-28 10-51 26-69-3-6-11-32 3-67 0 0 21-7 70 26 42-12 86-12 128 0 49-33 70-26 70-26 14 35 6 61 3 67 16 18 26 41 26 69 0 98-60 120-117 126 10 8 18 24 18 48l-1 70c0 6 3 12 16 12z"
 		/>
 	</svg>
+);
+
+const Button = ( props ) => (
+	<WordpressButton
+		isPrimary
+		style={ { display: 'flex', justifyContent: 'center', alignItems: 'center' } }
+		href={ props.url }
+	>
+		{ props.children }
+		{ props.label }
+	</WordpressButton>
+);
+
+const GithubButton = ( props ) => (
+	<Button { ...props }>
+		<Icon icon={ icon } style={ { marginRight: '10px' } } />
+	</Button>
 );
 
 registerBlockType( 'a8c/github-issue-template-button', {
@@ -49,20 +66,22 @@ registerBlockType( 'a8c/github-issue-template-button', {
 			type: 'string',
 			default: 'Create Issue',
 		},
-		alignment: {
+		align: {
 			type: 'string',
+			default: 'left',
 		},
+	},
+	supports: {
+		align: [ 'left', 'center', 'right' ],
 	},
 	edit: ( props ) => {
 		const {
-			attributes: { userOrOrg, repo, title, body, buttonLabel, alignment },
+			attributes: { userOrOrg, repo, title, body, buttonLabel, align },
 		} = props;
 
-		const alignmentOptions = [
-			{ key: 'flex-start', name: 'Left' },
-			{ key: 'center', name: 'Center' },
-			{ key: 'flex-end', name: 'End' },
-		];
+		const requiredFieldsFilled = userOrOrg && repo;
+
+		const UnconfiguredButton = () => <Button label="⚙ Configure me first" />;
 
 		const onChangeUserOrOrg = ( newUserOrOrg ) => {
 			props.setAttributes( { userOrOrg: newUserOrOrg } );
@@ -84,73 +103,52 @@ registerBlockType( 'a8c/github-issue-template-button', {
 			props.setAttributes( { buttonLabel: newButtonLabel } );
 		};
 
-		const onChangeAlignment = ( control ) => {
-			props.setAttributes( { alignment: control.selectedItem.key } );
-		};
-
 		return (
 			<>
-				<div className={ `wp-block-github-issue-template-button ${ alignment }` }>
-					<Button isPrimary>
-						<Icon icon={ icon } />
-						&nbsp;{ buttonLabel }
-					</Button>
-				</div>
+				{ requiredFieldsFilled ? <GithubButton label={ buttonLabel } /> : <UnconfiguredButton /> }
 				<InspectorControls>
 					<TextControl
-						label={ __( 'User or Organization' ) }
+						label={ __( 'User or Organization*' ) }
+						help="*This field is required"
 						onChange={ onChangeUserOrOrg }
 						value={ userOrOrg }
 					/>
 					<TextControl
-						label={ __( 'Repository Name' ) }
+						label={ __( 'Repository Name*' ) }
+						help="*This field is required"
 						onChange={ onChangeRepoName }
 						value={ repo }
 					/>
 					<TextControl label={ __( 'Issue Title' ) } onChange={ onChangeTitle } value={ title } />
-					<TextareaControl label={ __( 'Issue body' ) } onChange={ onChangeBody } value={ body } />
+					<TextareaControl label={ __( 'Issue Body' ) } onChange={ onChangeBody } value={ body } />
 					<TextControl
 						label={ __( 'Button Label' ) }
 						onChange={ onChangeButtonLabel }
 						value={ buttonLabel }
-					/>
-					<CustomSelectControl
-						label={ __( 'Alignment' ) }
-						options={ alignmentOptions }
-						value={
-							alignmentOptions.find( ( option ) => option.key === alignment ) ||
-							alignmentOptions[ 0 ]
-						}
-						onChange={ onChangeAlignment }
 					/>
 				</InspectorControls>
 			</>
 		);
 	},
 	save: ( props ) => {
-		let url;
-
 		const {
-			attributes: { userOrOrg, repo, title, body, buttonLabel, alignment },
+			attributes: { userOrOrg, repo, title, body, buttonLabel, align },
 		} = props;
 
-		if ( repo ) {
-			url = newGithubIssueUrl( {
+		const requiredFieldsFilled = userOrOrg && repo;
+		if ( requiredFieldsFilled ) {
+			const url = newGithubIssueUrl( {
 				title,
 				repo,
 				user: userOrOrg,
 				body,
 			} );
-		}
 
-		return (
-			<div className="block" style={ `display: flex; justify-content: ${ alignment }` }>
-				<Icon icon={ icon } />
-				&nbsp;
-				<Button isPrimary href={ url || '' }>
-					{ buttonLabel }
-				</Button>
-			</div>
-		);
+			return (
+				<div className={ `align${ align }` }>
+					<GithubButton label={ buttonLabel } url={ url } />
+				</div>
+			);
+		}
 	},
 } );
