cd9a3370e518dc7c732560aa9d9f9cbf031fcf5a
Don Lair <don.lair@gmail.com>
2020-07-16 12:56:13 -0400

    Adding Growth Summit task to My Home.

---
+++ client/my-sites/customer-home/cards/tasks/growth-summit/index.jsx
@@ -0,0 +1,28 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import Task from 'my-sites/customer-home/cards/tasks/task';
+import { TASK_GROWTH_SUMMIT } from 'my-sites/customer-home/cards/constants';
+import growthSummitIllustration from 'assets/images/customer-home/illustration--growth-summit.svg';
+
+const GrowthSummit = () => {
+	return (
+		<Task
+			title="The Official WordPress.com Growth Summit"
+			description="Learn how to build and grow your site, from start to scale. Come join us for this exclusive two-day virtual event August 11-13."
+			actionText="Register today for 20% off!"
+			actionUrl="http://www.wordpress.com/growth-summit/"
+			actionTarget="_blank"
+			completeOnStart={ false }
+			illustration={ growthSummitIllustration }
+			taskId={ TASK_GROWTH_SUMMIT }
+		/>
+	);
+};
+
+export default GrowthSummit;
