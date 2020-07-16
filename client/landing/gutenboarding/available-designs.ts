a49b6cbbbc6b61a963fcd4f6e6efdd9fcf0510e3
Ramon <ramonjd@users.noreply.github.com>
2020-07-16 10:23:37 +1000

    Update client/landing/gutenboarding/available-designs.ts
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>

--- client/landing/gutenboarding/available-designs.ts
+++ client/landing/gutenboarding/available-designs.ts
@@ -19,7 +19,7 @@ const availableDesigns: Readonly< AvailableDesigns > = availableDesignsConfig;
 function getCanUseWebP() {
 	if ( typeof window !== 'undefined' ) {
 		const elem = document.createElement( 'canvas' );
-		if ( !! elem.getContext && elem.getContext( '2d' ) ) {
+		if ( elem.getContext?.( '2d' ) ) {
 			return elem.toDataURL( 'image/webp' ).indexOf( 'data:image/webp' ) === 0;
 		}
 	}
