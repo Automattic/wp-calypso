a7777e8bec88b8dc7d5cedf81b5cac9e5e1411cd
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-07-16 14:33:12 +0530

    Implement destination after signup as - customer home if it's a free site OR checkout page with locale slug if there's a paid plan or domain in cart

--- client/signup/main.jsx
+++ client/signup/main.jsx
@@ -13,6 +13,7 @@ import {
 	get,
 	includes,
 	indexOf,
+	isEmpty,
 	isEqual,
 	kebabCase,
 	map,
@@ -77,6 +78,7 @@ import {
 import WpcomLoginForm from './wpcom-login-form';
 import SiteMockups from './site-mockup';
 import P2SignupProcessingScreen from 'signup/p2-processing-screen';
+import getCurrentLocaleSlug from 'state/selectors/get-current-locale-slug';
 
 /**
  * Style dependencies
@@ -247,7 +249,12 @@ class Signup extends React.Component {
 	}
 
 	handleSignupFlowControllerCompletion = ( dependencies, destination ) => {
-		const filteredDestination = getDestination( destination, dependencies, this.props.flowName );
+		const filteredDestination = getDestination(
+			destination,
+			dependencies,
+			this.props.flowName,
+			this.props.localeSlug
+		);
 
 		// If the filtered destination is different from the flow destination (e.g. changes to checkout), then save the flow destination so the user ultimately arrives there
 		if ( destination !== filteredDestination ) {
@@ -389,6 +396,15 @@ class Signup extends React.Component {
 			debug( `Handling regular login` );
 
 			const { bearer_token: bearerToken, username } = dependencies;
+
+			if (
+				isEmpty( bearerToken ) &&
+				isEmpty( username ) &&
+				'onboarding-new' === this.props.flowName
+			) {
+				window.location.href = destination;
+				return;
+			}
 
 			if ( this.state.bearerToken !== bearerToken && this.state.username !== username ) {
 				this.setState( {
@@ -654,6 +670,7 @@ export default connect(
 			siteType: getSiteType( state ),
 			shouldStepShowSitePreview,
 			isSitePreviewVisible: shouldStepShowSitePreview && isSitePreviewVisible( state ),
+			localeSlug: getCurrentLocaleSlug( state ),
 		};
 	},
 	{
