c3adca3218568c04528f8808b03a8233df0c0e23
Caleb Bauermeister <cbauerman@gmail.com>
2020-07-16 21:31:05 -0700

    finish up

--- client/landing/jetpack-cloud/sections/auth/paths.ts
+++ client/landing/jetpack-cloud/sections/auth/paths.ts
@@ -1,5 +1,3 @@
 export const authConnectPath = () => '/connect';
 
 export const authTokenRedirectPath = () => '/connect/oauth/token';
-
-export const supportSessionPath = () => '/connect/support-session';
