5d8e55cc7b82e133bf7f2c2df34aef050275591c
Dave Smith <getdavemail@gmail.com>
2020-07-20 14:29:37 +0100

    Remove ref to ReaderPreview component

--- packages/social-previews/src/index.js
+++ packages/social-previews/src/index.js
@@ -1,6 +1,3 @@
 export { default as FacebookPreview } from './facebook-preview';
 export { default as TwitterPreview } from './twitter-preview';
 export { default as SearchPreview } from './search-preview';
-
-// TODO: fix dependency issues and restore
-// export { default as Reader } from './reader-preview';
