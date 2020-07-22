f21acc105e2c8e88506e777bdd391bd70d56571b
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-07-22 11:40:07 +0530

    getCart should return a resolved promise

--- client/my-sites/checkout/checkout-system-decider.js
+++ client/my-sites/checkout/checkout-system-decider.js
@@ -149,7 +149,7 @@ export default function CheckoutSystemDecider( {
 						isWhiteGloveOffer={ isWhiteGloveOffer }
 						isComingFromUpsell={ isComingFromUpsell }
 						isLoggedOutCart={ isLoggedOutCart }
-						getCart={ isLoggedOutCart ? () => Promise.new( cart ) : null }
+						getCart={ isLoggedOutCart ? () => Promise.resolve( cart ) : null }
 					/>
 				</StripeHookProvider>
 			</CheckoutErrorBoundary>
