45fbbe2b2a6ef380391d59030136673a483611a1
Sergio Cinos <sergio.cinos@gmail.com>
2020-07-22 10:29:01 +0200

    Fix lint errors

--- apps/full-site-editing/full-site-editing-plugin/premium-content/blocks/container/tab.js
+++ apps/full-site-editing/full-site-editing-plugin/premium-content/blocks/container/tab.js
@@ -1,20 +1,14 @@
 /**
- *
  * @typedef { import('react').ReactNode } ReactNode
- *
  * @typedef {object} Tab
  * @property { string } id
  * @property { (ReactNode | string) } label
  * @property { string } className
- *
  * @typedef {object} Props
  * @property { Tab } tab
  * @property { Tab } selectedTab
- * @property { string } className
- * @property { ReactNode } label
  * @property { (tab: Tab) => void } onSelected
- *
- * @param { Props } props
+ * @param { Props } props the props
  */
 export default function Tab( props ) {
 	const { className, tab, label, selectedTab, onSelected } = props;
