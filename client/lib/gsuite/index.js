7463cab89b529220567ccaf87fec8ec151377f0f
Sérgio Gomes <mail@sgomes.com>
2020-07-22 12:21:50 +0100

    Sort exports

--- client/lib/gsuite/index.js
+++ client/lib/gsuite/index.js
@@ -1,16 +1,16 @@
+export { canDomainAddGSuite } from './can-domain-add-gsuite';
+export { canUserPurchaseGSuite } from './can-user-purchase-gsuite';
 export { getAnnualPrice } from './get-annual-price';
+export { getEligibleGSuiteDomain } from './get-eligible-gsuite-domain';
+export { getGSuiteMailboxCount } from './get-gsuite-mailbox-count';
+export { getLoginUrlWithTOSRedirect } from './get-login-url-with-tos-redirect';
 export { getMonthlyPrice } from './get-monthly-price';
 export {
 	isGSuiteExtraLicenseProductSlug,
 	isGSuiteOrExtraLicenseProductSlug,
 	isGSuiteProductSlug,
 } from './gsuite-product-slug';
-export { getLoginUrlWithTOSRedirect } from './get-login-url-with-tos-redirect';
-export { canUserPurchaseGSuite } from './can-user-purchase-gsuite';
-export { canDomainAddGSuite } from './can-domain-add-gsuite';
-export { getGSuiteMailboxCount } from './get-gsuite-mailbox-count';
-export { hasGSuiteWithUs } from './has-gsuite-with-us';
-export { hasGSuiteWithAnotherProvider } from './has-gsuite-with-another-provider';
-export { hasPendingGSuiteUsers } from './has-pending-gsuite-users';
 export { getGSuiteSupportedDomains, hasGSuiteSupportedDomain } from './gsuite-supported-domain';
-export { getEligibleGSuiteDomain } from './get-eligible-gsuite-domain';
+export { hasGSuiteWithAnotherProvider } from './has-gsuite-with-another-provider';
+export { hasGSuiteWithUs } from './has-gsuite-with-us';
+export { hasPendingGSuiteUsers } from './has-pending-gsuite-users';
