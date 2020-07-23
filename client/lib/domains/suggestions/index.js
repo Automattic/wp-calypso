9ffc4ff335a42c1495c69444d5e1abe4759e7ec5
Dale du Preez <dale@automattic.com>
2020-07-23 16:52:06 +0200

    Add helper function to manage permitted premium level

--- client/lib/domains/suggestions/index.js
+++ client/lib/domains/suggestions/index.js
@@ -1,4 +1,9 @@
 /**
+ * Internal dependencies
+ */
+import config from 'config';
+
+/**
  * Get the suggestions vendor
  *
  * @param {boolean} [isSignup=false] Whether the query is part of a signup flow.
@@ -9,5 +14,19 @@ export const getSuggestionsVendor = ( isSignup = false ) => {
 	if ( isSignup ) {
 		return 'variation4_front';
 	}
-	return 'variation2_front';
+	return 'variation7_front';
+};
+
+/**
+ * Get the premium level to be used for suggestions.
+ *
+ * @param {boolean} [isSignup=false] Whether we are in the signup flow.
+ *
+ * @returns {number} Premium level to supply to the domain suggestions query.
+ */
+export const getSuggestionsPremiumLevel = ( isSignup = false ) => {
+	if ( isSignup ) {
+		return 0;
+	}
+	return config.isEnabled( 'domains/premium-domain-purchases' ) ? 1 : 0;
 };
