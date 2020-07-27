0d72cbadd0f654424bac15494cdde51af22083c1
Kamen Stanev <hambai@gmail.com>
2020-07-27 15:00:04 +0300

    * fix the domain only domain management pages not working from the all domains view (#44362)
    Fixex some routing issues for domain management pages for standalone domains

--- client/my-sites/controller.js
+++ client/my-sites/controller.js
@@ -47,6 +47,7 @@ import {
 	domainManagementTransfer,
 	domainManagementTransferOut,
 	domainManagementTransferToOtherSite,
+	domainManagementRoot,
 } from 'my-sites/domains/paths';
 import {
 	emailManagement,
@@ -180,6 +181,12 @@ function isPathAllowedForDomainOnlySite( path, slug, primaryDomain, contextParam
 		}
 		return pathFactory( slug, slug );
 	} );
+
+	domainManagementPaths = domainManagementPaths.concat(
+		allPaths.map( ( pathFactory ) => {
+			return pathFactory( slug, slug, domainManagementRoot() );
+		} )
+	);
 
 	if ( primaryDomain && slug !== primaryDomain.name ) {
 		domainManagementPaths = domainManagementPaths.concat(
