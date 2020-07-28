d4f8625848ae870900867fab87c6255be0a51455
Sergio Cinos <sergio.cinos@gmail.com>
2020-07-28 10:18:52 +0200

    Round memory

--- packages/calypso-doctor/lib.js
+++ packages/calypso-doctor/lib.js
@@ -14,7 +14,7 @@ const getShellRc = () => {
 
 const getNpmRc = () => path.join( process.env.HOME, '.npmrc' );
 
-const getMemInMb = () => os.totalmem() / 1024 / 1024;
+const getMemInMb = () => Math.round( os.totalmem() / 1024 / 1024 );
 
 const getDockerSettings = () =>
 	path.join( process.env.HOME, 'Library/Group Containers/group.com.docker/settings.json' );
