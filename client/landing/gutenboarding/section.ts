cb0b9eb841856311315faea98f1ebbf9b338c570
Bernie Reiter <ockham@raz.or.at>
2020-07-28 16:58:14 +0200

    Gutenboarding: Remove /gutenboarding alias route

--- client/landing/gutenboarding/section.ts
+++ client/landing/gutenboarding/section.ts
@@ -5,10 +5,7 @@ import GUTENBOARDING_BASE_NAME from './basename.json';
 
 export const GUTENBOARDING_SECTION_DEFINITION = {
 	name: 'gutenboarding',
-	paths: [
-		`/${ GUTENBOARDING_BASE_NAME }`,
-		`/gutenboarding` /* Development path, maintained for redirection */,
-	],
+	paths: [ `/${ GUTENBOARDING_BASE_NAME }` ],
 	module: 'gutenboarding',
 	secondary: false,
 	group: 'gutenboarding',
--- client/server/pages/index.js
+++ client/server/pages/index.js
@@ -731,10 +731,6 @@ export default function pages() {
 		'entry-gutenboarding'
 	);
 
-	// Handle redirection from development basename
-	// TODO: Remove after a few months
-	handleSectionPath( GUTENBOARDING_SECTION_DEFINITION, `/gutenboarding`, 'entry-gutenboarding' );
-
 	// This is used to log to tracks Content Security Policy violation reports sent by browsers
 	app.post(
 		'/cspreport',
