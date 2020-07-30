8dd975e6df0fad8168d569b7b5723e83d0f7db9f
Sérgio Gomes <mail@sgomes.com>
2020-07-30 13:17:38 +0100

    Split out getUserPurchases

--- client/state/purchases/selectors/index.js
+++ client/state/purchases/selectors/index.js
@@ -21,21 +21,12 @@ import { TYPE_PERSONAL } from 'lib/plans/constants';
 import { getPlanRawPrice } from 'state/plans/selectors';
 
 import { getPurchases } from './get-purchases';
+import { getUserPurchases } from './get-user-purchases';
 
 import 'state/purchases/init';
 
 export { getPurchases } from './get-purchases';
-
-/**
- * Returns a list of Purchases associated with a User from the state using its userId
- *
- * @param   {object} state       global state
- * @param   {number} userId      the user id
- * @returns {object} the matching purchases if there are some
- */
-export const getUserPurchases = ( state, userId ) =>
-	state.purchases.hasLoadedUserPurchasesFromServer &&
-	getPurchases( state ).filter( ( purchase ) => purchase.userId === userId );
+export { getUserPurchases } from './get-user-purchases';
 
 /**
  * Returns the server error for site or user purchases (if there is one)
