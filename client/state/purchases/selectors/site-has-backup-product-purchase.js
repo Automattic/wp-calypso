4d74c694d5dcb6a918269310695de6fe70908a50
Sérgio Gomes <mail@sgomes.com>
2020-07-30 17:35:51 +0100

    Split purchases selectors (#44562)
    * Move purchases selectors into their own folder
    
    * Split out getPurchases
    
    * Split out getUserPurchases
    
    * Split out getPurchasesError
    
    * Split out getByPurchaseId
    
    * Split out getSitePurchases
    
    * Split out getRenewableSitePurchases
    
    * Split out siteHasJetpackProductPurchase
    
    * Split out siteHasBackupProductPurchase
    
    * Split out siteHasScanProductPurchase
    
    * Split out getIncludedDomainPurchase
    
    * Split out getDowngradePlanFromPurchase
    
    * Split out getDowngradePlanRawPrice
    
    * Split out isUserPaid
    
    * Split out fetching-related selectors
    
    * Sort exports alphabetically

---
+++ client/state/purchases/selectors/site-has-backup-product-purchase.js
@@ -0,0 +1,26 @@
+/**
+ * External dependencies
+ */
+import { some } from 'lodash';
+
+/**
+ * Internal Dependencies
+ */
+import { isJetpackBackup } from 'lib/products-values';
+import { getSitePurchases } from './get-site-purchases';
+
+import 'state/purchases/init';
+
+/**
+ * Whether a site has an active Jetpack backup purchase.
+ *
+ * @param   {object} state       global state
+ * @param   {number} siteId      the site id
+ * @returns {boolean} True if the site has an active Jetpack Backup purchase, false otherwise.
+ */
+export const siteHasBackupProductPurchase = ( state, siteId ) => {
+	return some(
+		getSitePurchases( state, siteId ),
+		( purchase ) => purchase.active && isJetpackBackup( purchase )
+	);
+};
