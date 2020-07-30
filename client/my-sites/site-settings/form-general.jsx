dfb52ea75b85bd7fba5c6363805c0853edd60d9d
Matthew Reishus <mreishus@users.noreply.github.com>
2020-07-30 15:39:47 -0500

    Settings: Change wording of blog_public checkbox (#44527)
    * Settings: Change wording of blog_public checkbox
    
    Old wording: "Do not allow search engines to index my site"
    
    New wording: "Discourage search engines from indexing my site"
    with explanation: "This option does not block access to your site — it is up to search engines to honor
                       your request."
    
    This brings the wording in line with wp-admin from wordpress.org:
    https://github.com/WordPress/WordPress/blob/53c909a8256bbd481656ae864a4b7eb5a22c3692/wp-admin/options-reading.php#L184
    
    The point of clarification being made is that even though we modify
    robots.txt entries to disallow everything, and add a meta robots tag
    with "noindex, nofollow", it can still be ignored by search engines.
    Putting a sign on your open lawn that says "Do not tresspass" does not
    actually stop people from walking forward.
    
    From my research, the wayback machine on archive.org either ignores
    robots.txt now or plans to in the future. [1] To be excluded,
    archive.org wants the author to send them an individual email, which
    they will "review". [2]
    
    1: https://blog.archive.org/2017/04/17/robots-txt-meant-for-search-engines-dont-work-well-for-web-archives/
    2: https://help.archive.org/hc/en-us/articles/360004651732-Using-The-Wayback-Machine
    
    This is stemming from a user who had ticked the box, but still found
    their site on archive.org, when they thought it would be blocked.  See
    issue #42180.
    
    For the unit tests, I needed to add {exact: false} to a few
    getByLabelText() calls because the label now spans html elements because
    of the <FormSettingExplanation> added.  It does not match text across
    elements. [3]
    
    3: https://testing-library.com/docs/dom-testing-library/api-queries
    
    * Small wording change to take advantage of pre-translated strings

--- client/my-sites/site-settings/form-general.jsx
+++ client/my-sites/site-settings/form-general.jsx
@@ -359,7 +359,12 @@ export class SiteSettingsFormGeneral extends Component {
 						disabled={ isRequestingSettings }
 						onClick={ eventTracker( 'Clicked Site Visibility Radio Button' ) }
 					/>
-					<span>{ translate( 'Do not allow search engines to index my site' ) }</span>
+					<span>{ translate( 'Discourage search engines from indexing this site' ) }</span>
+					<FormSettingExplanation isIndented>
+						{ translate(
+							'This option does not block access to your site — it is up to search engines to honor your request.'
+						) }
+					</FormSettingExplanation>
 				</FormLabel>
 				{ ! isNonAtomicJetpackSite && (
 					<>
--- client/my-sites/site-settings/test/form-general.jsx
+++ client/my-sites/site-settings/test/form-general.jsx
@@ -149,7 +149,7 @@ describe( 'SiteSettingsFormGeneral ', () => {
 			[ 'Public', 'Public', -1, { blog_public: 1, wpcom_coming_soon: 0 } ],
 			[
 				'Hidden',
-				'Do not allow search engines to index my site',
+				'Discourage search engines from indexing this site',
 				-1,
 				{ blog_public: 0, wpcom_coming_soon: 0 },
 			],
@@ -159,7 +159,7 @@ describe( 'SiteSettingsFormGeneral ', () => {
 				testProps.fields.blog_public = initialBlogPublic;
 				const { getByLabelText } = renderWithRedux( <SiteSettingsFormGeneral { ...testProps } /> );
 
-				const radioButton = getByLabelText( text );
+				const radioButton = getByLabelText( text, { exact: false } );
 				expect( radioButton ).not.toBeChecked();
 				fireEvent.click( radioButton );
 				expect( testProps.updateFields ).toBeCalledWith( updatedFields );
@@ -170,7 +170,9 @@ describe( 'SiteSettingsFormGeneral ', () => {
 			testProps.fields.blog_public = -1;
 			const { getByLabelText } = renderWithRedux( <SiteSettingsFormGeneral { ...testProps } /> );
 
-			const hiddenCheckbox = getByLabelText( 'Do not allow search engines to index my site' );
+			const hiddenCheckbox = getByLabelText( 'Discourage search engines from indexing this site', {
+				exact: false,
+			} );
 			expect( hiddenCheckbox ).not.toBeChecked();
 
 			const publicRadio = getByLabelText( 'Public' );
@@ -187,7 +189,9 @@ describe( 'SiteSettingsFormGeneral ', () => {
 			testProps.fields.blog_public = 0;
 			const { getByLabelText } = renderWithRedux( <SiteSettingsFormGeneral { ...testProps } /> );
 
-			const hiddenCheckbox = getByLabelText( 'Do not allow search engines to index my site' );
+			const hiddenCheckbox = getByLabelText( 'Discourage search engines from indexing this site', {
+				exact: false,
+			} );
 			expect( hiddenCheckbox ).toBeChecked();
 
 			const publicRadio = getByLabelText( 'Public' );
