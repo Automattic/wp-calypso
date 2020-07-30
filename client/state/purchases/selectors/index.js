4dc8f3df636b375e0ab95624ee6ff2e3cd71d17f
Sérgio Gomes <mail@sgomes.com>
2020-07-30 13:41:21 +0100

    Sort exports alphabetically

--- client/state/purchases/selectors/index.js
+++ client/state/purchases/selectors/index.js
@@ -3,22 +3,22 @@
  */
 import 'state/purchases/init';
 
-export { getPurchases } from './get-purchases';
-export { getUserPurchases } from './get-user-purchases';
-export { getPurchasesError } from './get-purchases-error';
-export { getByPurchaseId } from './get-by-purchase-id';
-export { getSitePurchases } from './get-site-purchases';
-export { getRenewableSitePurchases } from './get-renewable-site-purchases';
-export { siteHasJetpackProductPurchase } from './site-has-jetpack-product-purchase';
-export { siteHasBackupProductPurchase } from './site-has-backup-product-purchase';
-export { siteHasScanProductPurchase } from './site-has-scan-product-purchase';
-export { getIncludedDomainPurchase } from './get-included-domain-purchase';
-export { getDowngradePlanFromPurchase } from './get-downgrade-plan-from-purchase';
-export { getDowngradePlanRawPrice } from './get-downgrade-plan-raw-price';
-export { isUserPaid } from './is-user-paid';
 export {
 	isFetchingUserPurchases,
 	isFetchingSitePurchases,
 	hasLoadedUserPurchasesFromServer,
 	hasLoadedSitePurchasesFromServer,
 } from './fetching.js';
+export { getByPurchaseId } from './get-by-purchase-id';
+export { getDowngradePlanFromPurchase } from './get-downgrade-plan-from-purchase';
+export { getDowngradePlanRawPrice } from './get-downgrade-plan-raw-price';
+export { getIncludedDomainPurchase } from './get-included-domain-purchase';
+export { getPurchases } from './get-purchases';
+export { getPurchasesError } from './get-purchases-error';
+export { getRenewableSitePurchases } from './get-renewable-site-purchases';
+export { getSitePurchases } from './get-site-purchases';
+export { getUserPurchases } from './get-user-purchases';
+export { isUserPaid } from './is-user-paid';
+export { siteHasBackupProductPurchase } from './site-has-backup-product-purchase';
+export { siteHasJetpackProductPurchase } from './site-has-jetpack-product-purchase';
+export { siteHasScanProductPurchase } from './site-has-scan-product-purchase';
