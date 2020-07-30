810dea5fc91502e7acdfd24dbe29836f0297d74b
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-07-30 20:32:13 +0530

    Modify the cart remove messaging for the case where we are on checkout in an account with no sites yet

--- client/my-sites/checkout/composite-checkout/composite-checkout.js
+++ client/my-sites/checkout/composite-checkout/composite-checkout.js
@@ -607,6 +607,7 @@ export default function CompositeCheckout( {
 						CheckoutTerms={ CheckoutTerms }
 						showErrorMessageBriefly={ showErrorMessageBriefly }
 						isLoggedOutCart={ isLoggedOutCart }
+						createUserAndSiteBeforeTransaction={ createUserAndSiteBeforeTransaction }
 						infoMessage={ infoMessage }
 					/>
 				</CheckoutProvider>
