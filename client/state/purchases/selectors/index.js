9db2ea37d2006ce54a85a757a8dd764301cb6262
Sérgio Gomes <mail@sgomes.com>
2020-07-30 13:34:55 +0100

    Split out getIncludedDomainPurchase

--- client/state/purchases/selectors/index.js
+++ client/state/purchases/selectors/index.js
@@ -1,23 +1,11 @@
 /**
- * External dependencies
- */
-import { find } from 'lodash';
-
-/**
  * Internal Dependencies
  */
-import { isSubscription } from 'lib/purchases';
-import {
-	getIncludedDomainPurchaseAmount,
-	isDomainRegistration,
-	isDomainMapping,
-} from 'lib/products-values';
 import { getPlan, findPlansKeys } from 'lib/plans';
 import { TYPE_PERSONAL } from 'lib/plans/constants';
 import { getPlanRawPrice } from 'state/plans/selectors';
 
 import { getUserPurchases } from './get-user-purchases';
-import { getSitePurchases } from './get-site-purchases';
 
 import 'state/purchases/init';
 
@@ -30,38 +18,7 @@ export { getRenewableSitePurchases } from './get-renewable-site-purchases';
 export { siteHasJetpackProductPurchase } from './site-has-jetpack-product-purchase';
 export { siteHasBackupProductPurchase } from './site-has-backup-product-purchase';
 export { siteHasScanProductPurchase } from './site-has-scan-product-purchase';
-
-/**
- * Returns a purchase object that corresponds to that subscription's included domain
- *
- * Even if a domain registration was purchased with the subscription, it will
- * not be returned if the domain product was paid for separately (eg: if it was
- * renewed on its own).
- *
- * @param   {object} state  global state
- * @param   {object} subscriptionPurchase  subscription purchase object
- * @returns {object} domain purchase if there is one, null if none found or not a subscription object passed
- */
-export const getIncludedDomainPurchase = ( state, subscriptionPurchase ) => {
-	if (
-		! subscriptionPurchase ||
-		! isSubscription( subscriptionPurchase ) ||
-		getIncludedDomainPurchaseAmount( subscriptionPurchase )
-	) {
-		return null;
-	}
-
-	const { includedDomain } = subscriptionPurchase;
-	const sitePurchases = getSitePurchases( state, subscriptionPurchase.siteId );
-	const domainPurchase = find(
-		sitePurchases,
-		( purchase ) =>
-			( isDomainMapping( purchase ) || isDomainRegistration( purchase ) ) &&
-			includedDomain === purchase.meta
-	);
-
-	return domainPurchase;
-};
+export { getIncludedDomainPurchase } from './get-included-domain-purchase';
 
 export const getDowngradePlanFromPurchase = ( purchase ) => {
 	const plan = getPlan( purchase.productSlug );
