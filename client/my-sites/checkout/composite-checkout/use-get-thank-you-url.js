8f4798de409b51a4c96580cfffe26d8842f8762a
Victor Espigares <espigares@gmail.com>
2020-07-31 11:14:46 +0200

    Implementing Business plan bump offer

--- client/my-sites/checkout/composite-checkout/use-get-thank-you-url.js
+++ client/my-sites/checkout/composite-checkout/use-get-thank-you-url.js
@@ -230,7 +230,7 @@ function getFallbackDestination( {
 	return '/';
 }
 
-function maybeShowPlanBumpOfferConcierge( {
+function maybeShowPlanBumpOffer( {
 	pendingOrReceiptId,
 	orderId,
 	cart,
@@ -239,10 +239,7 @@ function maybeShowPlanBumpOfferConcierge( {
 	isTransactionResultEmpty,
 } ) {
 	if ( hasPremiumPlan( cart ) && ! isTransactionResultEmpty && ! didPurchaseFail ) {
-		if ( 'variantShowPlanBump' === abtest( 'showBusinessPlanBump' ) ) {
-			return `/checkout/${ siteSlug }/offer-plan-upgrade/business/${ pendingOrReceiptId }`;
-		}
-		return getQuickstartUrl( { pendingOrReceiptId, siteSlug, orderId } );
+		return `/checkout/${ siteSlug }/offer-plan-upgrade/business/${ pendingOrReceiptId }`;
 	}
 
 	return;
@@ -272,7 +269,7 @@ function getRedirectUrlForConciergeNudge( {
 		// A user just purchased one of the qualifying plans
 		// Show them the concierge session upsell page
 
-		const upgradePath = maybeShowPlanBumpOfferConcierge( {
+		const upgradePath = maybeShowPlanBumpOffer( {
 			pendingOrReceiptId,
 			orderId,
 			cart,
