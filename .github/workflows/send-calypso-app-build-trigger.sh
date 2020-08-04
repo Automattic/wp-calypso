3be4824cead4137dee7668279b4f01cf92c6fa6c
Philip Jackson <p-jackson@live.com>
2020-08-04 21:19:36 +1200

    test

--- .github/workflows/send-calypso-app-build-trigger.sh
+++ .github/workflows/send-calypso-app-build-trigger.sh
@@ -13,7 +13,6 @@ zip --quiet --recurse-paths $build_archive ./*
 echo -e "Creating JSON payload...\n"
 
 echo actionis${GITHUB_ACTION}x
-echo passthruidis${INPUT_PASS_THRU_ID}x
 
 printenv | sed 's;=.*;;'
 
