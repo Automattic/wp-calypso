17f281dd518dfcba16e52cbb3eb35813a32a7c9b
Kevin Zoschke <kevin.zoschke@automattic.com>
2020-08-04 14:20:51 -0400

    Move Jetpack cards components in a shared folder

---
+++ client/components/jetpack/card/jetpack-bundle-card/fixture/index.ts
@@ -0,0 +1,42 @@
+/**
+ * External dependencies
+ */
+import { noop } from 'lodash';
+
+export const featuresItems = [
+	{
+		text: 'Backup',
+	},
+	{
+		text: 'Scan',
+	},
+];
+
+export const features = {
+	items: featuresItems,
+};
+
+export const bundleCard = {
+	iconSlug: 'jetpack_anti_spam',
+	productName: 'Security Security',
+	subheadline: 'Comprehensive WordPress protection',
+	description:
+		'Enjoy the peace of mind of complete site security. Easy-to-use, powerful security tools guard your site, so you can focus on your business.',
+	currencyCode: 'USD',
+	originalPrice: 25,
+	billingTimeFrame: 'per month, billed monthly',
+	buttonLabel: 'Get Jetpack Security',
+	onButtonClick: noop,
+	features,
+};
+
+export const bundleCardWithBadge = {
+	...bundleCard,
+	badgeLabel: 'Best value',
+};
+
+export const bundleCardWithDiscount = {
+	...bundleCard,
+	discountedPrice: 20,
+	withStartingPrice: true,
+};
--- client/components/jetpack/card/jetpack-product-card/docs/example.tsx
+++ client/components/jetpack/card/jetpack-product-card/docs/example.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import JetpackProductCard from 'components/jetpack-product-card';
+import JetpackProductCard from 'components/jetpack/card/jetpack-product-card';
 import {
 	productCard,
 	highlightedProductCard,
