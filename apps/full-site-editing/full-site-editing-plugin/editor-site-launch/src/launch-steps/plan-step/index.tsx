f014fc278966eae029eefedffc212bc2f6df442a
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-08-05 20:20:27 +0300

    Plan step action buttons update

--- apps/full-site-editing/full-site-editing-plugin/editor-site-launch/src/launch-steps/plan-step/index.tsx
+++ apps/full-site-editing/full-site-editing-plugin/editor-site-launch/src/launch-steps/plan-step/index.tsx
@@ -3,7 +3,7 @@
  */
 import * as React from 'react';
 import { __ } from '@wordpress/i18n';
-import { Title, SubTitle, ActionButtons, BackButton } from '@automattic/onboarding';
+import { Title, SubTitle } from '@automattic/onboarding';
 
 /**
  * Internal dependencies
@@ -12,32 +12,23 @@ import LaunchStep, { Props as LaunchStepProps } from '../../launch-step';
 import PlansGridFSE from '../../../../editor-plans-grid/src/plans-grid-fse';
 import './styles.scss';
 
-const PlanStep: React.FunctionComponent< LaunchStepProps > = ( { onPrevStep, onNextStep } ) => {
-	const handleBack = () => {
-		onPrevStep?.();
-	};
-
-	return (
-		<LaunchStep className="nux-launch-plan-step">
-			<div className="nux-launch-step__header">
-				<div>
-					<Title>{ __( 'Choose a plan', 'full-site-editing' ) }</Title>
-					<SubTitle>
-						{ __(
-							'Pick a plan that’s right for you. Switch plans as your needs change. There’s no risk, you can cancel for a full refund within 30 days.',
-							'full-site-editing'
-						) }
-					</SubTitle>
-				</div>
-				<ActionButtons>
-					<BackButton onClick={ handleBack } />
-				</ActionButtons>
+const PlanStep: React.FunctionComponent< LaunchStepProps > = ( { onNextStep } ) => (
+	<LaunchStep className="nux-launch-plan-step">
+		<div className="nux-launch-step__header">
+			<div>
+				<Title>{ __( 'Choose a plan', 'full-site-editing' ) }</Title>
+				<SubTitle>
+					{ __(
+						'Pick a plan that’s right for you. There’s no risk, you can cancel for a full refund within 30 days.',
+						'full-site-editing'
+					) }
+				</SubTitle>
 			</div>
-			<div className="nux-launch-step__body">
-				<PlansGridFSE onSelect={ onNextStep } />
-			</div>
-		</LaunchStep>
-	);
-};
+		</div>
+		<div className="nux-launch-step__body">
+			<PlansGridFSE onSelect={ onNextStep } />
+		</div>
+	</LaunchStep>
+);
 
 export default PlanStep;
