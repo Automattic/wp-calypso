12bcd091805f4b654994e47a04403d3b1f2f2236
Philip Jackson <p-jackson@live.com>
2020-08-06 11:03:29 +1200

    Save the last-non-editor-route in redux instead of calculating it

--- client/state/route/reducer.js
+++ client/state/route/reducer.js
@@ -2,10 +2,12 @@
  * Internal dependencies
  */
 import { combineReducers, withStorageKey } from 'state/utils';
+import lastNonEditorRoute from './last-non-editor-route/reducer';
 import path from './path/reducer';
 import query from './query/reducer';
 
 const combinedReducer = combineReducers( {
+	lastNonEditorRoute,
 	path,
 	query,
 } );
