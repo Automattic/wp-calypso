57b23c6af2a48325a1bd90374980354566bdb6d1
nsakaimbo <nsakaimbo@a8c.com>
2020-08-04 23:14:17 -0400

    Revert "use nvm refresh script"
    This reverts commit 7f05a56be976f8343d25984873047482be356c9a.

--- desktop/bin/circle-refresh-nvm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Resolve pre-installed NVM_DIR on Linux machine executor:
-# https://discuss.circleci.com/t/circleci-forgetting-node-version-on-machine-executor/28813
-
-set +e
-set +x
-export NVM_DIR="/opt/circleci/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
