a1eb4ae6f6c2a2fdbc9296e23505c59003f7dd45
Renzo Canepa <rcanepag@gmail.com>
2020-08-06 16:05:58 -0300

    Offer Reset: create a filter bar for the new Plans page (#44712)
    * Add the PlansFilterBar component
    
    * Match styles to design
    
    * Sort items in the right order
    
    * Improve positioning in very small screens
    
    * Add Desktop styles
    
    * Connect the PlansFilterBar with the new plans page
    
    * Decrease horizontal padding
    
    * Move const to the constants file
    
    * Use border-radius: 4px for consistency
    
    * Fix wrong colors on hover event
    
    * Add marging between filters (required on mobile)
    
    * Set right color for selected and hover item

--- client/my-sites/plans-v2/selector.tsx
+++ client/my-sites/plans-v2/selector.tsx
@@ -2,25 +2,30 @@
  * External dependencies
  */
 import React, { useState } from 'react';
-import { Button } from '@automattic/components';
 
 /**
  * Internal dependencies
  */
-import { SelectorPageProps } from './types';
-import { TERM_ANNUALLY, TERM_MONTHLY } from 'lib/plans/constants';
+import PlansFilterBar from './plans-filter-bar';
+import type { Duration, ProductType, SelectorPageProps } from './types';
+import { TERM_ANNUALLY } from 'lib/plans/constants';
+import { SECURITY } from './constants';
 
 const SelectorPage = ( { defaultDuration = TERM_ANNUALLY }: SelectorPageProps ) => {
-	const [ currentDuration, setDuration ] = useState( defaultDuration );
+	const [ productType, setProductType ] = useState< ProductType >( SECURITY );
+	const [ currentDuration, setDuration ] = useState< Duration >( defaultDuration );
 	const currentDurationString = currentDuration === TERM_ANNUALLY ? 'annual' : 'monthly';
 
-	const toggleDuration = () =>
-		setDuration( currentDuration === TERM_ANNUALLY ? TERM_MONTHLY : TERM_ANNUALLY );
 	return (
 		<div>
+			<PlansFilterBar
+				onProductTypeChange={ setProductType }
+				productType={ productType }
+				onDurationChange={ setDuration }
+				duration={ currentDuration }
+			/>
 			<h1>Hello this is the Selector Page!</h1>
-			<p>{ `You are currently looking at ${ currentDurationString } products` }</p>
-			<Button onClick={ toggleDuration }>Toggle duration</Button>
+			<p>{ `You are currently looking at ${ currentDurationString } products of type ${ productType }` }</p>
 		</div>
 	);
 };
