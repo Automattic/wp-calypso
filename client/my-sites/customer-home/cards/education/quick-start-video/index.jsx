3857c9b6b1761557e5dbe7aa2c3bc7eafb525f22
Don Lair <don.lair@gmail.com>
2020-08-06 12:27:05 -0400

    Updating per feedback from GH thread.

--- client/my-sites/customer-home/cards/education/quick-start-video/index.jsx
+++ client/my-sites/customer-home/cards/education/quick-start-video/index.jsx
@@ -5,6 +5,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
 import { Card } from '@automattic/components';
+import { isDesktop } from '@automattic/viewport';
 
 /**
  * Internal dependencies
@@ -13,6 +14,7 @@ import InlineSupportLink from 'components/inline-support-link';
 import { localizeUrl } from 'lib/i18n-utils';
 import { bumpStat, composeAnalytics, recordTracksEvent } from 'state/analytics/actions';
 import { FEATURE_QUICK_START_VIDEO } from 'my-sites/customer-home/cards/constants';
+import MaterialIcon from 'components/material-icon';
 
 /**
  * Style dependencies
@@ -33,21 +35,56 @@ export const QuickStartVideo = ( { trackQuickStartImpression } ) => {
 				{ translate( 'Watch this video to get started' ) }
 			</h2>
 			<Card>
-				<div>
-					<InlineSupportLink
-						supportPostId={ 81083 }
-						supportLink={ localizeUrl( 'https://wordpress.com/support/start/' ) }
-						showIcon={ false }
-						showText={ true }
-						tracksEvent="calypso_customer_home_education"
-						statsGroup="calypso_customer_home"
-						tracksOptions={ {
-							card_name: "QuickStartVideo",
-						} }
-						statsName="QuickStartVideo" 
-					>
-						<img src={ quickStartVideoImage } />
-					</InlineSupportLink>
+				<div class="quick-start-video__content">
+					<div className="quick-start-video__content-wrapper">
+						<h3>{ translate( 'Video: Getting started on WordPress.com' ) }</h3>
+						<p className="quick-start-video__content-description customer-home__card-subheader">
+							{ translate(
+								'This video tour of WordPress.com will show you everything you need to know to start building your website.'
+							) }
+						</p>
+						<div className="quick-start-video__content-links">
+							<div className="quick-start-video__content-link">
+								<MaterialIcon icon="play_circle_outline" />
+								<InlineSupportLink
+									supportPostId={ 158974 }
+									supportLink={ localizeUrl(
+										'https://wordpress.com/support/getting-started-with-wordpress-com/#video-getting-started-with-word-press-com'
+									) }
+									showIcon={ false }
+									showText={ true }
+									tracksEvent="calypso_customer_home_education"
+									statsGroup="calypso_customer_home"
+									tracksOptions={ {
+										card_name: 'QuickStartVideo',
+									} }
+									statsName="QuickStartVideo"
+								>
+									{ translate( 'Watch the video' ) }
+								</InlineSupportLink>
+							</div>
+						</div>
+					</div>
+					{ isDesktop() && (
+						<div className="quick-start-video__content-illustration">
+							<InlineSupportLink
+								supportPostId={ 158974 }
+								supportLink={ localizeUrl(
+									'https://wordpress.com/support/getting-started-with-wordpress-com/#video-getting-started-with-word-press-com'
+								) }
+								showIcon={ false }
+								showText={ true }
+								tracksEvent="calypso_customer_home_education"
+								statsGroup="calypso_customer_home"
+								tracksOptions={ {
+									card_name: 'QuickStartVideo',
+								} }
+								statsName="QuickStartVideo"
+							>
+								<img src={ quickStartVideoImage } alt="" width="186px" />
+							</InlineSupportLink>
+						</div>
+					) }
 				</div>
 			</Card>
 		</div>
