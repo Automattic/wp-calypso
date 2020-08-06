89ad58c399cd191f8d9cffbe0e323128a5773cd7
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-08-06 15:54:25 -0400

    Passes currency and product types.

--- client/my-sites/plans-v2/products-column/index.tsx
+++ client/my-sites/plans-v2/products-column/index.tsx
@@ -3,12 +3,14 @@
  */
 import React, { useMemo } from 'react';
 import { translate } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
 
 /**
  * Internal dependencies
  */
+import { durationToText } from '../utils';
+import { PRODUCTS_TYPES } from '../constants';
 import { JETPACK_PRODUCTS_LIST } from 'lib/products-values/constants';
-import JetpackProductCard from 'components/jetpack/card/jetpack-product-card';
 import {
 	getJetpackProductDescription,
 	getJetpackProductDisplayName,
@@ -17,51 +19,81 @@ import {
 	isMonthly,
 	getProductFromSlug,
 } from 'lib/products-values';
-import { durationToText } from '../utils';
 import { TERM_ANNUALLY } from 'lib/plans/constants';
+import { getProductCost, isProductsListFetching } from 'state/products-list/selectors';
+import { getCurrentUserCurrencyCode } from 'state/current-user/selectors';
+import JetpackProductCard from 'components/jetpack/card/jetpack-product-card';
+import FormattedHeader from 'components/formatted-header';
 
 /**
  * Type dependencies
  */
-import type { Duration, PurchaseCallback } from '../types';
+import type { Duration, PurchaseCallback, ProductType } from '../types';
 import type { Product } from 'lib/products-values/products-list';
 
 interface ProductsColumnType {
 	duration: Duration;
 	onProductClick: PurchaseCallback;
+	productType: ProductType;
 }
 
-const ProductsColumn = ( { duration, onProductClick }: ProductsColumnType ) => {
+const ProductComponent = ( {
+	product,
+	onClick,
+	currencyCode,
+}: {
+	product: Product;
+	onClick: PurchaseCallback;
+	currencyCode: string;
+} ) => {
+	const price = useSelector( ( state ) => getProductCost( state, product.product_slug ) ) || 0;
+	return (
+		<JetpackProductCard
+			iconSlug={ product.product_slug }
+			productName={ getJetpackProductDisplayName( product ) }
+			subheadline={ getJetpackProductTagline( product ) }
+			description={ getJetpackProductDescription( product ) }
+			currencyCode={ currencyCode }
+			billingTimeFrame={ durationToText( product.term ) }
+			buttonLabel={ translate( 'Get %s', {
+				args: getJetpackProductShortName( product ),
+				context: '%s is the name of a product',
+			} ) }
+			onButtonClick={ () => onClick( product.product_slug ) }
+			features={ { items: [] } }
+			originalPrice={ price }
+		/>
+	);
+};
+
+const ProductsColumn = ( { duration, onProductClick, productType }: ProductsColumnType ) => {
+	const currencyCode = useSelector( ( state ) => getCurrentUserCurrencyCode( state ) );
+	const isFetchingProducts = useSelector( ( state ) => isProductsListFetching( state ) );
+
 	const productObjects: Product[] = useMemo(
 		() =>
-			JETPACK_PRODUCTS_LIST.map( ( productSlug ) => getProductFromSlug( productSlug ) )
-				.filter( ( product: Product ) =>
-					duration === TERM_ANNUALLY ? ! isMonthly( product ) : isMonthly( product )
-				)
-				.filter(
-					( product: Product ) =>
-						!! product?.product_slug && ! product.product_slug.startsWith( 'wpcom' )
-				),
-		[ duration ]
+			JETPACK_PRODUCTS_LIST.map( ( productSlug ) => getProductFromSlug( productSlug ) ).filter(
+				( product: Product ) =>
+					!! product?.product_slug &&
+					! product.product_slug.startsWith( 'wpcom' ) &&
+					PRODUCTS_TYPES[ productType ].includes( product.product_slug ) &&
+					( duration === TERM_ANNUALLY ? ! isMonthly( product ) : isMonthly( product ) )
+			),
+		[ duration, productType ]
 	);
+
+	if ( ! currencyCode || isFetchingProducts ) {
+		return null; // TODO: Loading component!
+	}
 
 	return (
 		<div>
+			<FormattedHeader headerText={ translate( 'Individual Products' ) } brandFont />
 			{ productObjects.map( ( product ) => (
-				<JetpackProductCard
-					iconSlug={ product.product_slug }
-					productName={ getJetpackProductDisplayName( product ) }
-					subheadline={ getJetpackProductTagline( product ) }
-					description={ getJetpackProductDescription( product ) }
-					currencyCode="USD"
-					billingTimeFrame={ durationToText( product.term ) }
-					buttonLabel={ translate( 'Get %s', {
-						args: getJetpackProductShortName( product ),
-						context: '%s is the name of a product',
-					} ) }
-					onButtonClick={ () => onProductClick( product.product_slug ) }
-					features={ { items: [] } }
-					originalPrice={ 100 }
+				<ProductComponent
+					onClick={ onProductClick }
+					product={ product }
+					currencyCode={ currencyCode }
 				/>
 			) ) }
 		</div>
--- client/my-sites/plans-v2/selector.tsx
+++ client/my-sites/plans-v2/selector.tsx
@@ -11,6 +11,7 @@ import PlansFilterBar from './plans-filter-bar';
 import type { Duration, ProductType, SelectorPageProps } from './types';
 import { SECURITY } from './constants';
 import { TERM_ANNUALLY } from 'lib/plans/constants';
+import QueryProductsList from 'components/data/query-products-list';
 
 const SelectorPage = ( { defaultDuration = TERM_ANNUALLY }: SelectorPageProps ) => {
 	const [ productType, setProductType ] = useState< ProductType >( SECURITY );
@@ -27,7 +28,12 @@ const SelectorPage = ( { defaultDuration = TERM_ANNUALLY }: SelectorPageProps ) 
 			/>
 			<h1>Hello this is the Selector Page!</h1>
 			<p>{ `You are currently looking at ${ currentDurationString } products of type ${ productType }` }</p>
-			<ProductsColumn duration={ currentDuration } onProductClick={ () => null } />
+			<ProductsColumn
+				duration={ currentDuration }
+				onProductClick={ () => null }
+				productType={ productType }
+			/>
+			<QueryProductsList />
 		</div>
 	);
 };
