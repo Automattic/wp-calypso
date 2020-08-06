b6be94b582bf6815e29336089dcea88f7780468e
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-08-06 14:09:02 -0400

    Provides more helpers for products.

--- client/lib/products-values/get-jetpack-product-description.js
+++ client/lib/products-values/get-jetpack-product-description.js
@@ -3,18 +3,18 @@
  */
 import { assertValidProduct } from 'lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'lib/products-values/format-product';
-import { getJetpackProductsDisplayNames } from 'lib/products-values/translations';
+import { getJetpackProductsDescriptions } from 'lib/products-values/translations';
 
 /**
- * Get Jetpack product display name based on the product purchase object.
+ * Get Jetpack product description based on the product purchase object.
  *
  * @param   product {object}             Product purchase object
  * @returns         {string|object} Product display name
  */
-export function getJetpackProductDisplayName( product ) {
+export function getJetpackProductDescription( product ) {
 	product = formatProduct( product );
 	assertValidProduct( product );
-	const jetpackProductsDisplayNames = getJetpackProductsDisplayNames();
+	const jetpackProductsDescriptions = getJetpackProductsDescriptions();
 
-	return jetpackProductsDisplayNames?.[ product.product_slug ];
+	return jetpackProductsDescriptions?.[ product.product_slug ];
 }
--- client/lib/products-values/products-list.ts
+++ client/lib/products-values/products-list.ts
@@ -9,10 +9,18 @@ import {
 	PLAN_MONTHLY_PERIOD,
 	PLAN_ANNUAL_PERIOD,
 } from 'lib/plans/constants';
+import type { TranslateResult } from 'i18n-calypso';
 
 const PRODUCT_SHORT_NAMES = getJetpackProductsShortNames();
 
-export const PRODUCTS_LIST = {
+export type Product = {
+	product_name: string | TranslateResult;
+	product_slug: string;
+	term: typeof TERM_ANNUALLY | typeof TERM_MONTHLY;
+	bill_period: typeof PLAN_ANNUAL_PERIOD | typeof PLAN_MONTHLY_PERIOD;
+};
+
+export const PRODUCTS_LIST: { [ key: string ]: Product } = {
 	[ constants.PRODUCT_JETPACK_BACKUP_DAILY ]: {
 		product_name: PRODUCT_SHORT_NAMES[ constants.PRODUCT_JETPACK_BACKUP_DAILY ],
 		product_slug: constants.PRODUCT_JETPACK_BACKUP_DAILY,
@@ -46,6 +54,18 @@ export const PRODUCTS_LIST = {
 	[ constants.PRODUCT_JETPACK_SEARCH_MONTHLY ]: {
 		product_name: PRODUCT_SHORT_NAMES[ constants.PRODUCT_JETPACK_SEARCH_MONTHLY ],
 		product_slug: constants.PRODUCT_JETPACK_SEARCH_MONTHLY,
+		term: TERM_MONTHLY,
+		bill_period: PLAN_MONTHLY_PERIOD,
+	},
+	[ constants.PRODUCT_JETPACK_ANTI_SPAM ]: {
+		product_name: PRODUCT_SHORT_NAMES[ constants.PRODUCT_JETPACK_ANTI_SPAM ],
+		product_slug: constants.PRODUCT_JETPACK_ANTI_SPAM,
+		term: TERM_ANNUALLY,
+		bill_period: PLAN_ANNUAL_PERIOD,
+	},
+	[ constants.PRODUCT_JETPACK_ANTI_SPAM_MONTHLY ]: {
+		product_name: PRODUCT_SHORT_NAMES[ constants.PRODUCT_JETPACK_ANTI_SPAM_MONTHLY ],
+		product_slug: constants.PRODUCT_JETPACK_ANTI_SPAM_MONTHLY,
 		term: TERM_MONTHLY,
 		bill_period: PLAN_MONTHLY_PERIOD,
 	},
