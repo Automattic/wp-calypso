Automattic/wp-calypso

commit ff8b5a6bc2b6eb7c7d4b5412e8416ca413f9d957

Registrationless checkout: Integration PR (#44206)
* Create A/B test for showing the userless checkout flow

* Create new signup flow called onboarding-new for the userless checkout

* Registrationless checkout: Show the checkout page for logged out users  (#44209)

* Registrationless checkout: Add email ID field with the new design (#44211)

* Registrationless checkout: Add email field validation (#44227)

* Registrationless checkout: Show checkout page if ccTLD domain item is present in the cart (#44229)

* Registrationless checkout: Initialize Google Recaptcha on the checkout page (#44231)

* Registrationless checkout: Create account, new site, and process transaction (#44234)

* Registrationless checkout: Handle removing items from cart (#44236)

* Registrationless checkout: Handle page reload scenarios (#44238)

* Registrationless checkout: Show Secure Checkout text (#44456)

* Registrationless checkout: Transfer cart after log in to account with no site (#44475)

* Registrationless checkout: Show the help icon in the checkout page (#44477)

* Create A/B test for showing the userless checkout flow

* Don't assign to the composite checkout i18n test when in registrationless context

* Update datestamp for the abtest

Co-authored-by: Payton Swick <payton@foolord.com>

client/layout/masterbar/logged-out.jsx
@@ -7,6 +7,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { getLocaleSlug, localize } from 'i18n-calypso';
 import { get, includes, startsWith } from 'lodash';
+import { defaultRegistry } from '@automattic/composite-checkout';
 
 /**
  * Internal dependencies
@@ -22,6 +23,7 @@ import getCurrentRoute from 'state/selectors/get-current-route';
 import { login } from 'lib/paths';
 import { isDomainConnectAuthorizePath } from 'lib/domains/utils';
 import { isDefaultLocale, addLocaleToPath } from 'lib/i18n-utils';
+import { recordTracksEvent } from 'state/analytics/actions';
 
 class MasterbarLoggedOut extends React.Component {
 	static propTypes = {
@@ -150,9 +152,42 @@ class MasterbarLoggedOut extends React.Component {
 			</Item>
 		);
 	}
+	clickClose = () => {
+		const { select } = defaultRegistry;
+		const siteSlug = select( 'wpcom' )?.getSiteSlug();
+		const closeUrl = siteSlug ? '/plans/' + siteSlug : '/start';
+		this.props.recordTracksEvent( 'calypso_masterbar_close_clicked' );
+		window.location = closeUrl;
+	};
 
 	render() {
-		const { title } = this.props;
+		const { title, isCheckout, translate } = this.props;
+
+		if ( isCheckout === true ) {
+			return (
+				<Masterbar>
+					<div className="masterbar__secure-checkout">
+						<Item
+							url={ '#' }
+							icon="cross"
+							className="masterbar__close-button"
+							onClick={ this.clickClose }
+							tooltip={ translate( 'Close Checkout' ) }
+							tipTarget="close"
+						/>
+						<Item className="masterbar__item-logo">
+							<WordPressLogo className="masterbar__wpcom-logo" />
+							<WordPressWordmark className="masterbar__wpcom-wordmark" />
+						</Item>
+						<span className="masterbar__secure-checkout-text">
+							{ translate( 'Secure checkout' ) }
+						</span>
+					</div>
+					<Item className="masterbar__item-title">{ title }</Item>
+				</Masterbar>
+			);
+		}
+
 		return (
 			<Masterbar>
 				<Item className="masterbar__item-logo">
@@ -169,7 +204,12 @@ class MasterbarLoggedOut extends React.Component {
 	}
 }
 
-export default connect( ( state ) => ( {
-	currentQuery: getCurrentQueryArguments( state ),
-	currentRoute: getCurrentRoute( state ),
-} ) )( localize( MasterbarLoggedOut ) );
+export default connect(
+	( state ) => (
+		{
+			currentQuery: getCurrentQueryArguments( state ),
+			currentRoute: getCurrentRoute( state ),
+		},
+		{ recordTracksEvent }
+	)
+)( localize( MasterbarLoggedOut ) );

client/layout/logged-out.jsx
@@ -57,6 +57,7 @@ const LayoutLoggedOut = ( {
 } ) => {
 	const sectionGroup = get( section, 'group', null );
 	const sectionName = get( section, 'name', null );
+	const isCheckout = sectionName === 'checkout';
 
 	const classes = {
 		[ 'is-group-' + sectionGroup ]: sectionGroup,
@@ -105,6 +106,7 @@ const LayoutLoggedOut = ( {
 			<MasterbarLoggedOut
 				title={ section.title }
 				sectionName={ section.name }
+				isCheckout={ isCheckout }
 				redirectUri={ redirectUri }
 			/>
 		);
@@ -121,6 +123,7 @@ const LayoutLoggedOut = ( {
 					id="notices"
 					notices={ notices.list }
 				/>
+				{ isCheckout && <AsyncLoad require="blocks/inline-help" placeholder={ null } /> }
 				<div id="primary" className="layout__primary">
 					{ primary }
 				</div>

client/boot/locale.js
@@ -21,6 +21,16 @@ import { initLanguageEmpathyMode } from 'lib/i18n-utils/empathy-mode';
 
 const debug = debugFactory( 'calypso:i18n' );
 
+function getLocaleFromPathname() {
+	const pathname = window.location.pathname.replace( /\/$/, '' );
+	const lastPathSegment = pathname.substr( pathname.lastIndexOf( '/' ) + 1 );
+	const pathLocaleSlug =
+		getLanguageSlugs().includes( lastPathSegment ) &&
+		! isDefaultLocale( lastPathSegment ) &&
+		lastPathSegment;
+	return pathLocaleSlug;
+}
+
 const setupTranslationChunks = async ( localeSlug, reduxStore ) => {
 	const { translatedChunks, locale } = await getLanguageManifestFile(
 		localeSlug,
@@ -95,13 +105,7 @@ export const setupLocale = ( currentUser, reduxStore ) => {
 		: currentUser.localeSlug;
 
 	if ( useTranslationChunks && '__requireChunkCallback__' in window ) {
-		const pathname = window.location.pathname.replace( /\/$/, '' );
-		const lastPathSegment = pathname.substr( pathname.lastIndexOf( '/' ) + 1 );
-		const pathLocaleSlug =
-			getLanguageSlugs().includes( lastPathSegment ) &&
-			! isDefaultLocale( lastPathSegment ) &&
-			lastPathSegment;
-		const localeSlug = userLocaleSlug || pathLocaleSlug;
+		const localeSlug = userLocaleSlug || getLocaleFromPathname();
 
 		if ( localeSlug && ! isDefaultLocale( localeSlug ) ) {
 			setupTranslationChunks( localeSlug, reduxStore );
@@ -111,6 +115,7 @@ export const setupLocale = ( currentUser, reduxStore ) => {
 	if ( window.i18nLocaleStrings ) {
 		// Use the locale translation data that were boostrapped by the server
 		const i18nLocaleStringsObject = JSON.parse( window.i18nLocaleStrings );
+
 		reduxStore.dispatch( setLocaleRawData( i18nLocaleStringsObject ) );
 		const languageSlug = get( i18nLocaleStringsObject, [ '', 'localeSlug' ] );
 		if ( languageSlug ) {
@@ -119,6 +124,10 @@ export const setupLocale = ( currentUser, reduxStore ) => {
 	} else if ( currentUser && currentUser.localeSlug ) {
 		// Use the current user's and load traslation data with a fetch request
 		reduxStore.dispatch( setLocale( userLocaleSlug, currentUser.localeVariant ) );
+	} else {
+		// For logged out Calypso pages, set the locale from slug
+		const pathLocaleSlug = getLocaleFromPathname();
+		pathLocaleSlug && reduxStore.dispatch( setLocale( pathLocaleSlug, '' ) );
 	}
 
 	// If user is logged out and translations are not boostrapped, we assume default locale

client/components/domains/contact-details-form-fields/managed-contact-details-form-fields.jsx
@@ -181,30 +181,61 @@ export class ManagedContactDetailsFormFields extends Component {
 		} );
 	};
 
-	renderContactDetailsFields() {
-		const { translate, hasCountryStates } = this.props;
-		const form = getFormFromContactDetails(
-			this.props.contactDetails,
-			this.props.contactDetailsErrors
-		);
-		const countryCode = form.countryCode?.value ?? '';
+	renderContactDetailsEmailPhone() {
+		const { translate, isLoggedOutCart } = this.props;
+
+		if ( isLoggedOutCart ) {
+			return (
+				<>
+					<div className="contact-details-form-fields__row">
+						{ this.createField(
+							'email',
+							Input,
+							{
+								label: translate( 'Email' ),
+								description: translate(
+									"You'll use this email address to access your account later"
+								),
+							},
+							{
+								customErrorMessage: this.props.contactDetailsErrors?.email,
+							}
+						) }
+					</div>
+
+					<div className="contact-details-form-fields__row">
+						{ this.createField(
+							'country-code',
+							CountrySelect,
+							{
+								label: translate( 'Country' ),
+								countriesList: this.props.countriesList,
+							},
+							{
+								customErrorMessage: this.props.contactDetailsErrors?.countryCode,
+							}
+						) }
+						{ this.createField(
+							'phone',
+							FormPhoneMediaInput,
+							{
+								label: translate( 'Phone' ),
+								onChange: this.handlePhoneChange,
+								countriesList: this.props.countriesList,
+								countryCode: this.state.phoneCountryCode,
+								enableStickyCountry: false,
+							},
+							{
+								customErrorMessage: this.props.contactDetailsErrors?.phone,
+							}
+						) }
+					</div>
+				</>
+			);
+		}
 
 		return (
-			<div className="contact-details-form-fields__contact-details">
-				<div className="contact-details-form-fields__row">
-					{ this.createField(
-						'organization',
-						HiddenInput,
-						{
-							label: translate( 'Organization' ),
-							text: translate( '+ Add organization name' ),
-						},
-						{
-							customErrorMessage: this.props.contactDetailsErrors?.organization,
-						}
-					) }
-				</div>
-
+			<>
 				<div className="contact-details-form-fields__row">
 					{ this.createField(
 						'email',
@@ -246,6 +277,35 @@ export class ManagedContactDetailsFormFields extends Component {
 						}
 					) }
 				</div>
+			</>
+		);
+	}
+
+	renderContactDetailsFields() {
+		const { translate, hasCountryStates } = this.props;
+		const form = getFormFromContactDetails(
+			this.props.contactDetails,
+			this.props.contactDetailsErrors
+		);
+		const countryCode = form.countryCode?.value ?? '';
+
+		return (
+			<div className="contact-details-form-fields__contact-details">
+				<div className="contact-details-form-fields__row">
+					{ this.createField(
+						'organization',
+						HiddenInput,
+						{
+							label: translate( 'Organization' ),
+							text: translate( '+ Add organization name' ),
+						},
+						{
+							customErrorMessage: this.props.contactDetailsErrors?.organization,
+						}
+					) }
+				</div>
+
+				{ this.renderContactDetailsEmailPhone() }
 
 				{ countryCode && (
 					<RegionAddressFieldsets

client/components/domains/registrant-extra-info/ca-form.jsx
@@ -37,10 +37,14 @@ export class RegistrantExtraInfoCaForm extends React.PureComponent {
 		ccTldDetails: PropTypes.object.isRequired,
 		onContactDetailsChange: PropTypes.func,
 		contactDetailsValidationErrors: PropTypes.object,
-		userWpcomLang: PropTypes.string.isRequired,
+		userWpcomLang: PropTypes.string,
 		translate: PropTypes.func.isRequired,
 		updateContactDetailsCache: PropTypes.func.isRequired,
 		isManaged: PropTypes.bool,
+	};
+
+	static defaultProps = {
+		userWpcomLang: 'EN',
 	};
 
 	constructor( props ) {
@@ -107,7 +111,7 @@ export class RegistrantExtraInfoCaForm extends React.PureComponent {
 		}
 
 		// Set the lang to FR if user languages is French, otherwise leave EN
-		if ( this.props.userWpcomLang.match( /^fr-?/i ) ) {
+		if ( this.props.userWpcomLang?.match( /^fr-?/i ) ) {
 			defaultValues.lang = 'FR';
 		}
 

client/components/site-selector/index.jsx
@@ -10,6 +10,7 @@ import page from 'page';
 import classNames from 'classnames';
 import { filter, find, flow, get, includes, isEmpty, noop } from 'lodash';
 import debugFactory from 'debug';
+import { format as formatUrl, parse as parseUrl } from 'url'; // eslint-disable-line no-restricted-imports
 
 /**
  * Internal dependencies
@@ -480,7 +481,19 @@ const navigateToSite = ( siteId, { allSitesPath, allSitesSingleUser, siteBasePat
 			// There is currently no "all sites" version of the insights page
 			return path.replace( /^\/stats\/insights\/?$/, '/stats/day' );
 		} else if ( siteBasePath ) {
-			return getSiteBasePath( site ) + '/' + site.slug;
+			const { protocol, hostname, port, pathname: urlPathname, query } = parseUrl(
+				getSiteBasePath( site ),
+				true
+			);
+			const newPathname = `${ urlPathname }/${ site.slug }`;
+
+			return formatUrl( {
+				protocol,
+				hostname,
+				port,
+				pathname: newPathname,
+				query,
+			} );
 		}
 	}
 

client/layout/masterbar/logged-in.jsx
@@ -174,7 +174,11 @@ class MasterbarLoggedIn extends React.Component {
 
 		if ( isCheckout === true ) {
 			let closeUrl = siteSlug ? '/plans/' + siteSlug : '/plans';
-			if ( '' !== previousPath && previousPath !== window.location.href ) {
+			if (
+				'' !== previousPath &&
+				previousPath !== window.location.href &&
+				! previousPath.includes( '/checkout/no-site' )
+			) {
 				closeUrl = previousPath;
 			}
 

client/lib/abtest/active-tests.js
@@ -224,4 +224,14 @@ export default {
 		defaultVariation: 'control',
 		allowExistingUsers: true,
 	},
+	userlessCheckout: {
+		datestamp: '20200806',
+		variations: {
+			variantUserless: 50,
+			control: 50,
+		},
+		defaultVariation: 'control',
+		allowExistingUsers: false,
+		countryCodeTargets: [ 'US', 'CA' ],
+	},
 };

commit 2fd2e13b9ff6d6ecce2748d7f98f711f3aacdc40

Avoid depending on checkout in masterbar
This fixes a performance problem introduced in #44206.
That PR added a number of dependencies to the critical path, by adding
them as dependencies to the masterbar.

This PR fixes things by isolating and loading the checkout parts
asynchronously, only when needed.

client/layout/masterbar/checkout.jsx
@@ -1,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { defaultRegistry } from '@automattic/composite-checkout';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import Masterbar from './masterbar';
+import Item from './item';
+import WordPressLogo from 'components/wordpress-logo';
+import WordPressWordmark from 'components/wordpress-wordmark';
+import { recordTracksEvent } from 'state/analytics/actions';
+
+class CheckoutMasterbar extends React.Component {
+	clickClose = () => {
+		const { select } = defaultRegistry;
+		const siteSlug = select( 'wpcom' )?.getSiteSlug();
+		const closeUrl = siteSlug ? '/plans/' + siteSlug : '/start';
+		this.props.recordTracksEvent( 'calypso_masterbar_close_clicked' );
+		window.location = closeUrl;
+	};
+
+	render() {
+		const { translate, title } = this.props;
+		return (
+			<Masterbar>
+				<div className="masterbar__secure-checkout">
+					<Item
+						url={ '#' }
+						icon="cross"
+						className="masterbar__close-button"
+						onClick={ this.clickClose }
+						tooltip={ translate( 'Close Checkout' ) }
+						tipTarget="close"
+					/>
+					<Item className="masterbar__item-logo">
+						<WordPressLogo className="masterbar__wpcom-logo" />
+						<WordPressWordmark className="masterbar__wpcom-wordmark" />
+					</Item>
+					<span className="masterbar__secure-checkout-text">
+						{ translate( 'Secure checkout' ) }
+					</span>
+				</div>
+				<Item className="masterbar__item-title">{ title }</Item>
+			</Masterbar>
+		);
+	}
+}
+
+export default connect( () => (null, { recordTracksEvent }) )( localize( CheckoutMasterbar ) );

client/layout/masterbar/logged-out.jsx
@@ -1,13 +1,11 @@
 /**
  * External dependencies
  */
-
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import { getLocaleSlug, localize } from 'i18n-calypso';
 import { get, includes, startsWith } from 'lodash';
-import { defaultRegistry } from '@automattic/composite-checkout';
 
 /**
  * Internal dependencies
@@ -23,7 +21,7 @@ import getCurrentRoute from 'state/selectors/get-current-route';
 import { login } from 'lib/paths';
 import { isDomainConnectAuthorizePath } from 'lib/domains/utils';
 import { isDefaultLocale, addLocaleToPath } from 'lib/i18n-utils';
-import { recordTracksEvent } from 'state/analytics/actions';
+import AsyncLoad from 'components/async-load';
 
 class MasterbarLoggedOut extends React.Component {
 	static propTypes = {
@@ -152,40 +150,12 @@ class MasterbarLoggedOut extends React.Component {
 			</Item>
 		);
 	}
-	clickClose = () => {
-		const { select } = defaultRegistry;
-		const siteSlug = select( 'wpcom' )?.getSiteSlug();
-		const closeUrl = siteSlug ? '/plans/' + siteSlug : '/start';
-		this.props.recordTracksEvent( 'calypso_masterbar_close_clicked' );
-		window.location = closeUrl;
-	};
 
 	render() {
-		const { title, isCheckout, translate } = this.props;
+		const { title, isCheckout } = this.props;
 
-		if ( isCheckout === true ) {
-			return (
-				<Masterbar>
-					<div className="masterbar__secure-checkout">
-						<Item
-							url={ '#' }
-							icon="cross"
-							className="masterbar__close-button"
-							onClick={ this.clickClose }
-							tooltip={ translate( 'Close Checkout' ) }
-							tipTarget="close"
-						/>
-						<Item className="masterbar__item-logo">
-							<WordPressLogo className="masterbar__wpcom-logo" />
-							<WordPressWordmark className="masterbar__wpcom-wordmark" />
-						</Item>
-						<span className="masterbar__secure-checkout-text">
-							{ translate( 'Secure checkout' ) }
-						</span>
-					</div>
-					<Item className="masterbar__item-title">{ title }</Item>
-				</Masterbar>
-			);
+		if ( isCheckout ) {
+			return <AsyncLoad require="layout/masterbar/checkout" placeholder={ null } title={ title } />;
 		}
 
 		return (
@@ -204,12 +174,7 @@ class MasterbarLoggedOut extends React.Component {
 	}
 }
 
-export default connect(
-	( state ) => (
-		{
-			currentQuery: getCurrentQueryArguments( state ),
-			currentRoute: getCurrentRoute( state ),
-		},
-		{ recordTracksEvent }
-	)
-)( localize( MasterbarLoggedOut ) );
+export default connect( ( state ) => ( {
+	currentQuery: getCurrentQueryArguments( state ),
+	currentRoute: getCurrentRoute( state ),
+} ) )( localize( MasterbarLoggedOut ) );