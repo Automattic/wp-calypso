21fc3df916d5887cf41665b37a56ffa93d5162d7
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-08-07 12:08:21 -0400

    Converts products to new SelectorProduct type.

--- client/my-sites/plans-v2/utils.ts
+++ client/my-sites/plans-v2/utils.ts
@@ -1,13 +1,22 @@
 /**
+ * External dependencies
+ */
+import { translate, TranslateResult } from 'i18n-calypso';
+import { difference } from 'lodash';
+
+/**
  * Internal dependencies
  */
 import { TERM_ANNUALLY, TERM_MONTHLY } from 'lib/plans/constants';
-import { translate, TranslateResult } from 'i18n-calypso';
+import { getJetpackProductDisplayName } from 'lib/products-values/get-jetpack-product-display-name';
+import { getJetpackProductTagline } from 'lib/products-values/get-jetpack-product-tagline';
+import { getJetpackProductDescription } from 'lib/products-values/get-jetpack-product-description';
 
 /**
  * Type dependencies
  */
-import type { Duration } from './types';
+import type { Duration, SelectorProduct } from './types';
+import type { Product } from 'lib/products-values/products-list';
 
 export function stringToDuration( duration?: string ): Duration | undefined {
 	if ( duration === undefined ) {
@@ -23,4 +32,38 @@ export function durationToText( duration: Duration ): TranslateResult {
 	return duration === TERM_MONTHLY
 		? translate( 'per month, billed monthly' )
 		: translate( 'per year' );
+}
+
+function objectIsSelectorProduct( item: object ): item is SelectorProduct {
+	const requiredKeys = [
+		'productSlug',
+		'iconSlug',
+		'displayName',
+		'tagline',
+		'description',
+		'term',
+	];
+	return difference( Object.keys( item ), requiredKeys ).length > 0;
+}
+
+/**
+ * Converts data from a product, plan, or selector product to selector product.
+ *
+ * @param item Product, Plan, or SelectorProduct.
+ * @returns SelectorProduct
+ */
+export function rawItemToSelectorProduct( item: SelectorProduct | Product ): SelectorProduct {
+	if ( objectIsSelectorProduct( item ) ) {
+		return item;
+	}
+
+	return {
+		productSlug: item.product_slug,
+		iconSlug: '',
+		displayName: getJetpackProductDisplayName( item ),
+		tagline: getJetpackProductTagline( item ),
+		description: getJetpackProductDescription( item ),
+		term: item.term,
+		features: [],
+	};
 }
