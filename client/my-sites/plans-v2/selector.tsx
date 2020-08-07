d832ce2aae106d9d3ab99211631fd9b4d84eabf5
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-08-07 15:15:15 -0400

    Removes lingering text.

--- client/my-sites/plans-v2/selector.tsx
+++ client/my-sites/plans-v2/selector.tsx
@@ -9,18 +9,21 @@ import { useSelector } from 'react-redux';
  */
 import ProductsColumn from './products-column';
 import PlansFilterBar from './plans-filter-bar';
-import type { Duration, ProductType, SelectorPageProps } from './types';
 import { SECURITY } from './constants';
 import { TERM_ANNUALLY } from 'lib/plans/constants';
 import { getSelectedSiteId } from 'state/ui/selectors';
 import QueryProductsList from 'components/data/query-products-list';
 import QuerySites from 'components/data/query-sites';
 
+/**
+ * Type dependencies
+ */
+import type { Duration, ProductType, SelectorPageProps } from './types';
+
 const SelectorPage = ( { defaultDuration = TERM_ANNUALLY }: SelectorPageProps ) => {
 	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
 	const [ productType, setProductType ] = useState< ProductType >( SECURITY );
 	const [ currentDuration, setDuration ] = useState< Duration >( defaultDuration );
-	const currentDurationString = currentDuration === TERM_ANNUALLY ? 'annual' : 'monthly';
 
 	return (
 		<div>
@@ -30,8 +33,6 @@ const SelectorPage = ( { defaultDuration = TERM_ANNUALLY }: SelectorPageProps ) 
 				onDurationChange={ setDuration }
 				duration={ currentDuration }
 			/>
-			<h1>Hello this is the Selector Page!</h1>
-			<p>{ `You are currently looking at ${ currentDurationString } products of type ${ productType }` }</p>
 			<ProductsColumn
 				duration={ currentDuration }
 				onProductClick={ () => null }
