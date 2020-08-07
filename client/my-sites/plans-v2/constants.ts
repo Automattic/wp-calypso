cce4f1b5b357b542abcf06952ed55963b686de2d
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-08-07 11:53:04 -0400

    Adds new selector product type and constants.

--- client/my-sites/plans-v2/constants.ts
+++ client/my-sites/plans-v2/constants.ts
@@ -2,17 +2,36 @@
  * External dependencies
  */
 import { translate } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
 import {
 	JETPACK_BACKUP_PRODUCTS,
 	JETPACK_SCAN_PRODUCTS,
 	JETPACK_ANTI_SPAM_PRODUCTS,
 	JETPACK_SEARCH_PRODUCTS,
+	PRODUCT_JETPACK_BACKUP_DAILY,
+	PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY,
+	PRODUCT_JETPACK_BACKUP_REALTIME,
+	PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY,
 } from 'lib/products-values/constants';
 import {
 	JETPACK_SECURITY_PLANS,
 	PLAN_JETPACK_COMPLETE,
 	PLAN_JETPACK_COMPLETE_MONTHLY,
+	TERM_ANNUALLY,
+	TERM_MONTHLY,
+	PLAN_JETPACK_SECURITY_DAILY,
+	PLAN_JETPACK_SECURITY_DAILY_MONTHLY,
+	PLAN_JETPACK_SECURITY_REALTIME,
+	PLAN_JETPACK_SECURITY_REALTIME_MONTHLY,
 } from 'lib/plans/constants';
+
+/**
+ * Type dependencies
+ */
+import { SelectorProduct } from './types';
 
 export const ALL = 'all';
 export const PERFORMANCE = 'performance';
@@ -36,6 +55,67 @@ export const PRODUCT_TYPE_OPTIONS = {
 	},
 };
 
+/**
+ * Plans and products that have options and can't be purchased themselves.
+ */
+export const OPTIONS_JETPACK_SECURITY = 'jetpack_security';
+export const OPTIONS_JETPACK_SECURITY_MONTHLY = 'jetpack_security_monthly';
+export const OPTIONS_JETPACK_BACKUP = 'jetpack_backup';
+export const OPTIONS_JETPACK_BACKUP_MONTHLY = 'jetpack_backup_monthly';
+
+export const PRODUCTS_WITH_OPTIONS = [
+	OPTIONS_JETPACK_SECURITY,
+	OPTIONS_JETPACK_SECURITY_MONTHLY,
+	OPTIONS_JETPACK_BACKUP,
+	OPTIONS_JETPACK_BACKUP_MONTHLY,
+];
+
+// Jetpack Security
+export const OPTION_PLAN_SECURITY: SelectorProduct = {
+	productSlug: OPTIONS_JETPACK_SECURITY,
+	term: TERM_ANNUALLY,
+	subtypes: [ PLAN_JETPACK_SECURITY_DAILY, PLAN_JETPACK_SECURITY_REALTIME ],
+	costProductSlug: PLAN_JETPACK_SECURITY_DAILY,
+	iconSlug: '',
+	displayName: translate( 'Jetpack Security' ),
+	tagline: translate( 'Comprehensive WordPress protection' ),
+	description: translate(
+		'Enjoy the peace of mind of complete site security. ' +
+			'Easy-to-use, powerful security tools guard your site, so you can focus on your business.'
+	),
+	features: [],
+};
+export const OPTION_PLAN_SECURITY_MONTHLY: SelectorProduct = {
+	...OPTION_PLAN_SECURITY,
+	productSlug: OPTIONS_JETPACK_SECURITY_MONTHLY,
+	term: TERM_MONTHLY,
+	subtypes: [ PLAN_JETPACK_SECURITY_DAILY_MONTHLY, PLAN_JETPACK_SECURITY_REALTIME_MONTHLY ],
+	costProductSlug: PLAN_JETPACK_SECURITY_DAILY_MONTHLY,
+};
+
+// Jetpack Backup
+export const OPTION_PRODUCT_BACKUP: SelectorProduct = {
+	productSlug: OPTIONS_JETPACK_BACKUP,
+	term: TERM_ANNUALLY,
+	subtypes: [ PRODUCT_JETPACK_BACKUP_DAILY, PRODUCT_JETPACK_BACKUP_REALTIME ],
+	costProductSlug: PRODUCT_JETPACK_BACKUP_DAILY,
+	iconSlug: '',
+	displayName: translate( 'Jetpack Backup' ),
+	tagline: '',
+	description: '',
+	features: [],
+};
+export const OPTION_PRODUCT_BACKUP_MONTHLY: SelectorProduct = {
+	...OPTION_PRODUCT_BACKUP,
+	productSlug: OPTIONS_JETPACK_BACKUP_MONTHLY,
+	term: TERM_MONTHLY,
+	subtypes: [ PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY, PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY ],
+	costProductSlug: PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY,
+};
+
+/**
+ * Provides categorization of products for filters.
+ */
 const PRODUCTS_TYPE_SECURITY = [
 	...JETPACK_SCAN_PRODUCTS,
 	...JETPACK_BACKUP_PRODUCTS,
--- client/my-sites/plans-v2/types.ts
+++ client/my-sites/plans-v2/types.ts
@@ -4,6 +4,12 @@
 import { TERM_ANNUALLY, TERM_MONTHLY } from 'lib/plans/constants';
 import { ALL, PERFORMANCE, SECURITY } from './constants';
 
+/**
+ * Type dependencies
+ */
+import type { TranslateResult } from 'i18n-calypso';
+import type { ReactNode } from 'react';
+
 export type Duration = typeof TERM_ANNUALLY | typeof TERM_MONTHLY;
 export type ProductType = typeof ALL | typeof PERFORMANCE | typeof SECURITY;
 export type PurchaseCallback = ( arg0: string ) => null;
@@ -20,4 +26,17 @@ export interface DetailsPageProps {
 export interface UpsellPageProps {
 	duration: Duration;
 	productSlug: string;
+}
+
+export interface SelectorProduct {
+	productSlug: string;
+	iconSlug: string;
+	costProductSlug?: string;
+	displayName: TranslateResult;
+	tagline: TranslateResult;
+	description: TranslateResult | ReactNode;
+	term: Duration;
+	buttonLabel?: TranslateResult;
+	features: string[];
+	subtypes?: string[];
 }
