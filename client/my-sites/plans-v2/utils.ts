2ec50abd62a34adbc0939d82148b3dc82fa80c3f
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-08-07 14:09:43 -0400

    Handles casting of plan to ProductSelector.

--- client/my-sites/plans-v2/utils.ts
+++ client/my-sites/plans-v2/utils.ts
@@ -2,14 +2,21 @@
  * External dependencies
  */
 import { translate, TranslateResult } from 'i18n-calypso';
-import { difference } from 'lodash';
+import { difference, get } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { PRODUCTS_WITH_OPTIONS, OPTIONS_SLUG_MAP } from './constants';
-import { TERM_ANNUALLY, TERM_MONTHLY } from 'lib/plans/constants';
-import { Product, JETPACK_PRODUCTS_LIST } from 'lib/products-values/products-list';
+import {
+	TERM_ANNUALLY,
+	TERM_MONTHLY,
+	JETPACK_PLANS,
+	JETPACK_RESET_PLANS,
+	TERM_BIENNIALLY,
+} from 'lib/plans/constants';
+import { getPlan } from 'lib/plans';
+import { Product, JETPACK_PRODUCTS_LIST, objectIsProduct } from 'lib/products-values/products-list';
 import { getJetpackProductDisplayName } from 'lib/products-values/get-jetpack-product-display-name';
 import { getJetpackProductTagline } from 'lib/products-values/get-jetpack-product-tagline';
 import { getJetpackProductDescription } from 'lib/products-values/get-jetpack-product-description';
@@ -18,6 +25,7 @@ import { getJetpackProductDescription } from 'lib/products-values/get-jetpack-pr
  * Type dependencies
  */
 import type { Duration, SelectorProduct, SelectorProductSlug } from './types';
+import type { JetpackPlanSlugs, Plan } from 'lib/plans/types';
 import type { JetpackProductSlug } from 'lib/products-values/types';
 
 export function stringToDuration( duration?: string ): Duration | undefined {
@@ -42,12 +50,17 @@ function slugIsSelectorProductSlug( slug: string ): slug is SelectorProductSlug 
 function slugIsJetpackProductSlug( slug: string ): slug is JetpackProductSlug {
 	return slug in Object.keys( JETPACK_PRODUCTS_LIST );
 }
+function slugIsJetpackPlanSlug( slug: string ): slug is JetpackPlanSlugs {
+	return slug in [ ...JETPACK_PLANS, JETPACK_RESET_PLANS ];
+}
 
-export function slugToItem( slug: string ): SelectorProduct | Product | null {
+export function slugToItem( slug: string ): Plan | Product | SelectorProduct | null {
 	if ( slugIsSelectorProductSlug( slug ) ) {
 		return OPTIONS_SLUG_MAP[ slug ];
 	} else if ( slugIsJetpackProductSlug( slug ) ) {
 		return JETPACK_PRODUCTS_LIST[ slug ];
+	} else if ( slugIsJetpackPlanSlug( slug ) ) {
+		return getPlan( slug ) as Plan;
 	}
 	return null;
 }
@@ -61,7 +74,20 @@ function objectIsSelectorProduct( item: object ): item is SelectorProduct {
 		'description',
 		'term',
 	];
-	return difference( Object.keys( item ), requiredKeys ).length > 0;
+	return difference( Object.keys( item ), requiredKeys ).length === 0;
+}
+function objectIsPlan( item: object ): item is Plan {
+	const requiredKeys = [
+		'group',
+		'type',
+		'term',
+		'getBillingTimeFrame',
+		'getTitle',
+		'getDescription',
+		'getProductId',
+		'getStoreSlug',
+	];
+	return difference( Object.keys( item ), requiredKeys ).length === 0;
 }
 
 /**
@@ -70,18 +96,31 @@ function objectIsSelectorProduct( item: object ): item is SelectorProduct {
  * @param item Product, Plan, or SelectorProduct.
  * @returns SelectorProduct
  */
-export function itemToSelectorProduct( item: SelectorProduct | Product ): SelectorProduct {
+export function itemToSelectorProduct(
+	item: Plan | Product | SelectorProduct | object
+): SelectorProduct | null {
 	if ( objectIsSelectorProduct( item ) ) {
 		return item;
+	} else if ( objectIsProduct( item ) ) {
+		return {
+			productSlug: item.product_slug,
+			iconSlug: '',
+			displayName: getJetpackProductDisplayName( item ),
+			tagline: getJetpackProductTagline( item ),
+			description: getJetpackProductDescription( item ),
+			term: item.term,
+			features: [],
+		};
+	} else if ( objectIsPlan( item ) ) {
+		return {
+			productSlug: item.getStoreSlug(),
+			iconSlug: '',
+			displayName: item.getTitle(),
+			tagline: get( item, 'getTagline', () => '' )(),
+			description: item.getDescription(),
+			term: item.term === TERM_BIENNIALLY ? TERM_ANNUALLY : item.term,
+			features: [],
+		};
 	}
-
-	return {
-		productSlug: item.product_slug,
-		iconSlug: '',
-		displayName: getJetpackProductDisplayName( item ),
-		tagline: getJetpackProductTagline( item ),
-		description: getJetpackProductDescription( item ),
-		term: item.term,
-		features: [],
-	};
+	return null;
 }
