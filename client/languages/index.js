7048f420ee6c19b37f783845aaf81bf788e97628
Sérgio Gomes <mail@sgomes.com>
2020-08-07 13:28:29 +0100

    Handle languages-meta fallback at build time (#44757)
    * Handle languages-meta fallback at build time
    
    The languages-meta fallback is currently handed at runtime, which means
    that when `languages-meta.json` is available, both it and the fallback
    get included in the bundle (and notably, in the critical path).
    
    This change will instead perform a replacement at build time when
    necessary, so that only one of the files ever gets included.
    
    * Replace the other way around, to fix tests

--- client/languages/index.js
+++ client/languages/index.js
@@ -3,13 +3,10 @@
  */
 import values from 'lodash/values';
 
-let languagesMeta = [];
-
-try {
-	languagesMeta = require( './languages-meta.json' );
-} catch ( error ) {
-	// Use fallback languages meta data if `languages-meta.json` has failed to download.
-	languagesMeta = require( './fallback-languages-meta.json' );
-}
+// If `languages-meta.json` is available, this import will be replaced with
+// `languages-meta.json` during build.
+// Be sure to keep the exact path (`languages/fallback-languages-meta.json`) as
+// is, so that webpack can find and replace it.
+import languagesMeta from 'languages/fallback-languages-meta.json';
 
 export const languages = values( languagesMeta );
