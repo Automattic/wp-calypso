858fa9978c1c2c0a5af6a920b287af58d8968fa3
Kamen Stanev <hambai@gmail.com>
2020-08-10 14:55:19 +0300

    Update how primary domain change affects the domain item row
    Hide the detailed domain information when the "Change primary domain" button is clicked and also change the "Abort primary domain change" button text to "Cancel primary domain change"

--- client/my-sites/domains/domain-management/list/index.jsx
+++ client/my-sites/domains/domain-management/list/index.jsx
@@ -502,7 +502,7 @@ export class List extends React.Component {
 						}
 					>
 						{ this.state.changePrimaryDomainModeEnabled
-							? translate( 'Abort primary domain change' )
+							? translate( 'Cancel primary domain change' )
 							: translate( 'Change primary domain' ) }
 					</Button>
 				</div>
@@ -577,7 +577,12 @@ export class List extends React.Component {
 
 		return [
 			<QuerySitePurchases key="query-purchases" siteId={ selectedSite.ID } />,
-			<ListHeader key="domains-header" />,
+			<ListHeader
+				key="domains-header"
+				headerClasses={ {
+					'domain-item__enable-selection': this.state.changePrimaryDomainModeEnabled,
+				} }
+			/>,
 			...domainListItems,
 			manageAllDomainsLink,
 		];
