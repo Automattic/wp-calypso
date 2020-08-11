d14882fdc87fa998c5df276628174a558f120c41
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-08-11 11:19:56 +0300

    Export UI components from composite-checkout and add dependency to FSE.

--- packages/composite-checkout/src/components/checkout-steps.js
+++ packages/composite-checkout/src/components/checkout-steps.js
@@ -444,7 +444,7 @@ const ContainerUI = styled.div`
 	}
 `;
 
-const MainContentUI = styled.div`
+export const MainContentUI = styled.div`
 	display: flex;
 	flex-direction: column;
 	width: 100%;
@@ -483,7 +483,7 @@ const CheckoutSummaryUI = styled.div`
 	}
 `;
 
-const CheckoutStepAreaUI = styled.div`
+export const CheckoutStepAreaUI = styled.div`
 	background: ${ ( props ) => props.theme.colors.surface };
 	box-sizing: border-box;
 	margin: 0 auto;
@@ -508,7 +508,7 @@ const CheckoutStepAreaUI = styled.div`
 	}
 `;
 
-const SubmitButtonWrapperUI = styled.div`
+export const SubmitButtonWrapperUI = styled.div`
 	background: ${ ( props ) => props.theme.colors.background };
 	padding: 24px;
 	bottom: 0;
