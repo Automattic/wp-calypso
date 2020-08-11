20b6787762f6717c79ebe420dccec7269706c801
Yan Sern <yan.sern@automattic.com>
2020-08-11 22:35:56 +0200

    Gutenboarding: release V2 Onboarding changes and new launch flow (#44820)
    This is a feature branch PR, including these PRs that were tested separately:
    
    - https://github.com/Automattic/wp-calypso/pull/44655 remove domain as a step
    - https://github.com/Automattic/wp-calypso/pull/44701 add vertical input
    - https://github.com/Automattic/wp-calypso/pull/44822 https://github.com/Automattic/wp-calypso/pull/44837 vertical gating with `?vertical` URL arg
    - https://github.com/Automattic/wp-calypso/pull/44833 enable launch flow in production and bump AB test slug
    - https://github.com/Automattic/wp-calypso/pull/44837 persistent show vertical input
    
    
    Co-authored-by: Razvan Papadopol <razvan.papadopol@automattic.com>
    Co-authored-by: Mikael Korpela <mikael@ihminen.org>
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>
    Co-authored-by: Yan Sern <yan.sern@automattic.com>

--- test/e2e/lib/pages/gutenboarding/acquire-intent-page.js
+++ test/e2e/lib/pages/gutenboarding/acquire-intent-page.js
@@ -15,17 +15,17 @@ export default class AcquireIntentPage extends AsyncBaseContainer {
 	}
 
 	async enterSiteTitle( siteTitle ) {
-		const siteTitleSelector = By.css( '#site-title__input' );
+		const siteTitleSelector = By.css( '.acquire-intent-text-input__input' );
 		return await driverHelper.setWhenSettable( this.driver, siteTitleSelector, siteTitle );
 	}
 
 	async goToNextStep() {
-		const nextButtonSelector = By.css( '.action-buttons__next' );
+		const nextButtonSelector = By.css( '.acquire-intent__question-skip.is-primary' );
 		return await driverHelper.clickWhenClickable( this.driver, nextButtonSelector );
 	}
 
 	async skipStep() {
-		const skipButtonSelector = By.css( '.action-buttons__skip' );
+		const skipButtonSelector = By.css( '.acquire-intent__question-skip' );
 		return await driverHelper.clickWhenClickable( this.driver, skipButtonSelector );
 	}
 }
