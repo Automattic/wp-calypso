7083b901915f95e73464dd121b844ce1e7ee9d22
Kamen Stanev <hambai@gmail.com>
2020-08-12 17:04:10 +0300

    * Add feature flag for premium domains * refactor the `getSuggestionsVendor` to support options and return the new variation for premium domains

--- client/lib/domains/suggestions/index.js
+++ client/lib/domains/suggestions/index.js
@@ -1,13 +1,21 @@
 /**
+ * Internal dependencies
+ */
+import config from 'config';
+
+/**
  * Get the suggestions vendor
  *
- * @param {boolean} [isSignup=false] Whether the query is part of a signup flow.
+ * @param {object} [options={}] Options to determine the suggestion vendor
  *
  * @returns {string} Vendor string to pass as part of the domain suggestions query.
  */
-export const getSuggestionsVendor = ( isSignup = false ) => {
-	if ( isSignup ) {
+export const getSuggestionsVendor = ( options = {} ) => {
+	if ( options?.isSignup && ! options?.isDomainOnly ) {
 		return 'variation4_front';
+	}
+	if ( config.isEnabled( 'domains/premium-domain-purchases' ) ) {
+		return 'variation7_front';
 	}
 	return 'variation2_front';
 };
