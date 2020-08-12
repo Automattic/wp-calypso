0def9082a304457ca356210b0c1a9e4343db5840
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-08-12 14:47:06 +0300

    Update header and domain-picker-button. - show correct states of domain button with correct data - load domain suggestion only after mounting the DomainPickerButton component - remove useDebounce dependency - re-add loading status of domain button - re-add slide-in animation to highlight domain button until Domain Picker is used. - hide Plans button on Plans step and PlansModal

--- client/landing/gutenboarding/components/header/index.tsx
+++ client/landing/gutenboarding/components/header/index.tsx
@@ -2,10 +2,7 @@
  * External dependencies
  */
 import * as React from 'react';
-import classnames from 'classnames';
 import { useI18n } from '@automattic/react-i18n';
-import { sprintf } from '@wordpress/i18n';
-import { useDebounce } from 'use-debounce';
 import { Icon, wordpress } from '@wordpress/icons';
 import { useSelect } from '@wordpress/data';
 
@@ -28,60 +25,20 @@ const Header: React.FunctionComponent = () => {
 	const { __, i18nLocale } = useI18n();
 	const currentStep = useCurrentStep();
 
-	const { domain, siteTitle } = useSelect( ( select ) => select( ONBOARD_STORE ).getState() );
+	const { siteTitle } = useSelect( ( select ) => select( ONBOARD_STORE ).getState() );
 
-	const [ domainSearch ] = useDebounce( siteTitle, 1000 /*ms*/ );
+	// steps (including modals) where we show Domains button
+	const showDomainsButton = [ 'DesignSelection', 'Style', 'Features', 'Plans' ].includes(
+		currentStep
+	);
 
-	const recommendedDomain = useSelect(
-		( select ) => {
-			if ( ! domainSearch || domainSearch.length < 2 ) {
-				return;
-			}
-			return select( 'automattic/domains/suggestions' ).getDomainSuggestions( domainSearch, {
-				// Avoid `only_wordpressdotcom` — it seems to fail to find results sometimes
-				include_wordpressdotcom: false,
-				include_dotblogsubdomain: false,
-				quantity: 1, // this will give the recommended domain only
-				locale: i18nLocale,
-			} );
-		},
-		[ domainSearch ]
-	)?.[ 0 ];
-
-	const getDomainElementContent = () => {
-		// If no site title entered (user skips intent gathering)
-		// and no domain was selected, show "Pick a domain"
-		if ( ! siteTitle && ! domain ) {
-			return null;
-		}
-
-		if ( recommendedDomain && ! domain ) {
-			/* translators: domain name is available, eg: "yourname.com is available" */
-			return sprintf( __( '%s is available' ), recommendedDomain.domain_name );
-		}
-
-		return __( 'Choose a domain' );
-	};
-
-	/* eslint-disable wpcalypso/jsx-classname-namespace */
-	const domainElement = domain
-		? domain.domain_name
-		: getDomainElementContent() && (
-				<span className="gutenboarding__header-domain-picker-button-domain">
-					{ getDomainElementContent() }
-				</span>
-		  );
-
-	const hideFullHeader = [
-		'IntentGathering',
-		'Domains',
-		'DomainsModal',
-		'LanguageModal',
-		'CreateSite',
-	].includes( currentStep );
+	// steps (including modals) where we hide Plans button
+	const showPlansButton = [ 'DesignSelection', 'Style', 'Features' ].includes( currentStep );
 
 	// CreateSite step clears state before redirecting, don't show the default text in this case
 	const siteTitleDefault = 'CreateSite' === currentStep ? '' : __( 'Start your website' );
+
+	/* eslint-disable wpcalypso/jsx-classname-namespace */
 
 	const changeLocaleButton = () => {
 		if ( isEnabled( 'gutenboarding/language-picker' ) ) {
@@ -104,11 +61,7 @@ const Header: React.FunctionComponent = () => {
 			aria-label={ __( 'Top bar' ) }
 			tabIndex={ -1 }
 		>
-			<section
-				className={ classnames( 'gutenboarding__header-section', {
-					'gutenboarding__header-section--compact': hideFullHeader,
-				} ) }
-			>
+			<section className="gutenboarding__header-section">
 				<div className="gutenboarding__header-section-item">
 					<div className="gutenboarding__header-wp-logo">
 						<Icon icon={ wordpress } size={ 28 } />
@@ -120,26 +73,11 @@ const Header: React.FunctionComponent = () => {
 					</div>
 				</div>
 				<div className="gutenboarding__header-section-item gutenboarding__header-domain-section">
-					{
-						// We display the DomainPickerButton as soon as we have a domain suggestion,
-						// unless we're still at the IntentGathering step. In that case, we only
-						// show it comes from a site title (but hide it if it comes from a vertical).
-						domainElement && (
-							<div
-								className={ classnames( 'gutenboarding__header-domain-picker-button-container', {
-									'no-domain': ! domain,
-								} ) }
-							>
-								<DomainPickerButton className="gutenboarding__header-domain-picker-button">
-									{ domainElement }
-								</DomainPickerButton>
-							</div>
-						)
-					}
+					{ showDomainsButton && <DomainPickerButton /> }
 				</div>
 				{ changeLocaleButton() }
 				<div className="gutenboarding__header-section-item gutenboarding__header-plan-section gutenboarding__header-section-item--right">
-					<PlansButton />
+					{ showPlansButton && <PlansButton /> }
 				</div>
 			</section>
 		</div>
