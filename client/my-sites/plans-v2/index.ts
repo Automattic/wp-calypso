b1750a1bada70b35ee050feda0471f80a1007dd0
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-08-12 18:28:46 -0400

    Ensure both old and new plan picker can appear.

--- client/my-sites/plans-v2/index.ts
+++ client/my-sites/plans-v2/index.ts
@@ -6,17 +6,15 @@ import page from 'page';
 /**
  * Internal dependencies
  */
-import { makeLayout, render as clientRender } from 'controller';
-import { siteSelection } from 'my-sites/controller';
+import { makeLayout, render as clientRender } from 'controller/index.web';
 import { productSelect, productDetails, productUpsell } from './controller';
 
 const trackedPage = ( url: string, ...rest: PageJS.Callback[] ) => {
-	page( url, siteSelection, ...rest, makeLayout, clientRender );
+	page( url, ...rest, makeLayout, clientRender );
 };
 
 export default function ( rootUrl: string, ...rest: PageJS.Callback[] ) {
-	trackedPage( `${ rootUrl }/:duration?`, productSelect( rootUrl ), ...rest );
-	trackedPage( `${ rootUrl }/:product/details`, productDetails( rootUrl ), ...rest );
-	trackedPage( `${ rootUrl }/:product/:duration/details`, productDetails( rootUrl ), ...rest );
-	trackedPage( `${ rootUrl }/:product/:duration/additions`, productUpsell( rootUrl ), ...rest );
+	trackedPage( `${ rootUrl }/:duration?`, ...rest, productSelect( rootUrl ) );
+	trackedPage( `${ rootUrl }/:product/:duration/details`, ...rest, productDetails( rootUrl ) );
+	trackedPage( `${ rootUrl }/:product/:duration/additions`, ...rest, productUpsell( rootUrl ) );
 }
