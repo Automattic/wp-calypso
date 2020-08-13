345dfc09380cb886492145f67d668db251b56aaa
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-08-13 14:37:41 -0400

    Wires up new ProductCard component.

--- client/my-sites/plans-v2/plans-column/index.tsx
+++ client/my-sites/plans-v2/plans-column/index.tsx
@@ -2,51 +2,24 @@
  * External dependencies
  */
 import { translate } from 'i18n-calypso';
-import React, { useMemo, ReactNode } from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 /**
  * Internal dependencies
  */
-import {
-	durationToText,
-	slugToItem,
-	slugToSelectorProduct,
-	itemToSelectorProduct,
-	productButtonLabel,
-	productBadgeLabel,
-	isUpgradeable,
-	getRealtimeFromDaily,
-} from '../utils';
-import {
-	PRODUCTS_TYPES,
-	SELECTOR_PLANS,
-	OPTIONS_JETPACK_SECURITY,
-	OPTIONS_JETPACK_SECURITY_MONTHLY,
-} from '../constants';
-import PlanRenewalMessage from '../plan-renewal-message';
-import { useLocalizedMoment } from 'components/localized-moment';
-import {
-	PLAN_JETPACK_FREE,
-	PLAN_JETPACK_SECURITY_DAILY,
-	PLAN_JETPACK_SECURITY_DAILY_MONTHLY,
-} from 'lib/plans/constants';
-import { isCloseToExpiration } from 'lib/purchases';
-import { getPurchaseByProductSlug } from 'lib/purchases/utils';
-import { getProductCost, isProductsListFetching } from 'state/products-list/selectors';
+import { PRODUCTS_TYPES, SELECTOR_PLANS } from '../constants';
+import { slugToSelectorProduct } from '../utils';
+import ProductCard from '../product-card';
+import { PLAN_JETPACK_FREE } from 'lib/plans/constants';
 import { getCurrentUserCurrencyCode } from 'state/current-user/selectors';
-import { getSitePurchases } from 'state/purchases/selectors';
 import getSitePlan from 'state/sites/selectors/get-site-plan';
-import JetpackBundleCard from 'components/jetpack/card/jetpack-bundle-card';
-import JetpackPlanCard from 'components/jetpack/card/jetpack-plan-card';
 import FormattedHeader from 'components/formatted-header';
-import JetpackProductCardUpgradeNudge from 'components/jetpack/card/jetpack-product-card/upgrade-nudge';
 
 /**
  * Type dependencies
  */
 import type { Duration, PurchaseCallback, ProductType, SelectorProduct } from '../types';
-import type { Purchase } from 'lib/purchases/types';
 
 interface PlanColumnType {
 	duration: Duration;
@@ -55,141 +28,34 @@ interface PlanColumnType {
 	siteId: number | null;
 }
 
-type PlanWithBought = SelectorProduct & { owned: boolean; legacy: boolean } & {
-	upgradeable: boolean;
-	UpgradeNudge?: ReactNode;
-};
-
-const PlanComponent = ( {
-	plan,
-	purchases,
-	onClick,
-	currencyCode,
-}: {
-	plan: PlanWithBought;
-	purchases: Purchase[];
-	onClick: PurchaseCallback;
-	currencyCode: string;
-} ) => {
-	const moment = useLocalizedMoment();
-	const price =
-		useSelector( ( state ) => getProductCost( state, plan.costProductSlug || plan.productSlug ) ) ||
-		0;
-	const CardComponent = [ OPTIONS_JETPACK_SECURITY, OPTIONS_JETPACK_SECURITY_MONTHLY ].includes(
-		plan.productSlug
-	)
-		? JetpackBundleCard
-		: JetpackPlanCard;
-	const purchase = getPurchaseByProductSlug( purchases, plan.productSlug );
-	const isExpiring = purchase && isCloseToExpiration( purchase );
-	const showExpiryNotice = plan.legacy && isExpiring;
-
-	if ( plan.upgradeable ) {
-		// TODO: remove this once we can purchase new plans.
-		// We need this now to make the nudge appear inside the Jetpack Security Bundle card
-		// and make it work as it was Jetpack Security Daily (monthly or annually).
-		const productSlug =
-			plan.productSlug === OPTIONS_JETPACK_SECURITY
-				? PLAN_JETPACK_SECURITY_DAILY
-				: PLAN_JETPACK_SECURITY_DAILY_MONTHLY;
-
-		const upgradeToProductSlug = productSlug && getRealtimeFromDaily( productSlug );
-		const selectorProductToUpgrade =
-			upgradeToProductSlug && slugToSelectorProduct( upgradeToProductSlug );
-
-		plan.UpgradeNudge = selectorProductToUpgrade ? (
-			<JetpackProductCardUpgradeNudge
-				billingTimeFrame={ durationToText( plan.term ) }
-				currencyCode={ currencyCode }
-				discountedPrice={ 67 }
-				originalPrice={ price || 100 }
-				onUpgradeClick={ () => null }
-				selectorProduct={ selectorProductToUpgrade }
-			/>
-		) : undefined;
-	}
-
-	return (
-		<CardComponent
-			iconSlug={ plan.iconSlug }
-			productName={ plan.displayName }
-			subheadline={ plan.tagline }
-			description={
-				showExpiryNotice ? (
-					<PlanRenewalMessage purchase={ purchase as Purchase } />
-				) : (
-					plan.description
-				)
-			}
-			currencyCode={ currencyCode }
-			billingTimeFrame={ durationToText( plan.term ) }
-			badgeLabel={ productBadgeLabel( plan ) }
-			buttonLabel={ productButtonLabel( plan ) }
-			onButtonClick={ () => onClick( plan ) }
-			features={ { items: [] } }
-			originalPrice={ price }
-			withStartingPrice={ plan.subtypes && plan.subtypes.length > 0 }
-			isOwned={ plan.owned }
-			isDeprecated={ plan.legacy }
-			UpgradeNudge={ plan.UpgradeNudge }
-			expiryDate={ showExpiryNotice ? moment( ( purchase as Purchase ).expiryDate ) : undefined }
-		/>
-	);
-};
-
 const PlansColumn = ( { duration, onPlanClick, productType, siteId }: PlanColumnType ) => {
 	const currencyCode = useSelector( ( state ) => getCurrentUserCurrencyCode( state ) );
-	const isFetchingProducts = useSelector( ( state ) => isProductsListFetching( state ) );
 	const currentPlan =
 		useSelector( ( state ) => getSitePlan( state, siteId ) )?.product_slug || null;
-	const purchases = useSelector( ( state ) => getSitePurchases( state, siteId ) );
 
 	// This gets all plan objects for us to parse.
-	const planObjects: PlanWithBought[] = useMemo( () => {
-		let owned = false;
+	const planObjects: SelectorProduct[] = useMemo( () => {
 		// Map over all plan slugs and convert them to SelectorProduct types.
-		const plans: PlanWithBought[] = SELECTOR_PLANS.map( ( productSlug ) => {
-			const item = slugToItem( productSlug );
-			if ( ! owned && currentPlan ) {
-				// Check if we own a plan in here. If we don't the user has a legacy plan, handled below.
-				owned = productSlug === currentPlan;
-			}
-			return item && itemToSelectorProduct( item );
-		} )
+		const plans: SelectorProduct[] = SELECTOR_PLANS.map( slugToSelectorProduct )
 			// Remove plans that don't fit the filters or have invalid data.
 			.filter(
 				( product: SelectorProduct | null ): product is SelectorProduct =>
 					!! product &&
 					duration === product.term &&
 					PRODUCTS_TYPES[ productType ].includes( product.productSlug )
-			)
-			// Iterate over plans and set whether they are legacy and if the user owns them.
-			// Verify if plan is upgradeable.
-			.map( ( product: SelectorProduct ) => {
-				const isOwned = product.productSlug === currentPlan;
-				return {
-					...product,
-					owned: isOwned,
-					legacy: false,
-					// TODO: since we can't own Jetpack Security Bundle, we have to disable
-					// this check while we can't purchase the real plan.
-					// upgradeable: isOwned && isUpgradeable( product.productSlug ),
-					upgradeable: isUpgradeable( product.productSlug ),
-				};
-			} );
+			);
 
 		// If the user does not own a current plan, get it and insert it on the top of the plan array.
-		if ( ! owned && currentPlan && currentPlan !== PLAN_JETPACK_FREE ) {
-			const item = slugToItem( currentPlan );
-			const currentPlanSelector = item && itemToSelectorProduct( item );
-			if ( currentPlanSelector ) {
-				plans.unshift( { ...currentPlanSelector, owned: true, legacy: true, upgradeable: false } );
+		if ( currentPlan && currentPlan !== PLAN_JETPACK_FREE ) {
+			const item = slugToSelectorProduct( currentPlan );
+			if ( item ) {
+				plans.unshift( { ...item, owned: true, legacy: true } );
 			}
 		}
 		return plans;
 	}, [ duration, productType, currentPlan ] );
 
-	if ( ! currencyCode || isFetchingProducts ) {
+	if ( ! currencyCode ) {
 		return null; // TODO: Loading component!
 	}
 
@@ -197,11 +63,10 @@ const PlansColumn = ( { duration, onPlanClick, productType, siteId }: PlanColumn
 		<div className="plans-column">
 			<FormattedHeader headerText={ translate( 'Plans' ) } brandFont />
 			{ planObjects.map( ( plan ) => (
-				<PlanComponent
+				<ProductCard
 					key={ plan.productSlug }
 					onClick={ onPlanClick }
-					plan={ plan }
-					purchases={ purchases }
+					item={ plan }
 					currencyCode={ currencyCode }
 				/>
 			) ) }
