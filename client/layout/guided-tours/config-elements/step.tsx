c1b62807fe71a0b258c0480a8b4e1e1d4b8c9020
Sergio Cinos <sergio.cinos@gmail.com>
2020-08-17 07:50:08 +0200

    Convert package relative dynamic imports to use wp-calypso-client (#44912)
    * Prefix programatic imports with wp-calypso-client
    
    * Do not import 'client' (the path), import from 'wp-calypso-client' (the package)

--- client/layout/guided-tours/config-elements/step.tsx
+++ client/layout/guided-tours/config-elements/step.tsx
@@ -22,7 +22,7 @@ import {
 } from '../positioning';
 import { contextTypes } from '../context-types';
 import { ArrowPosition, DialogPosition, Coordinate } from '../types';
-import { TimestampMS } from 'client/types';
+import { TimestampMS } from 'wp-calypso-client/types';
 
 const debug = debugFactory( 'calypso:guided-tours' );
 
