707e09202742acf767941e0aa68647886b55f213
Kevin Zoschke <kevin.zoschke@automattic.com>
2020-08-18 14:19:01 -0400

    Product card: add features subitems

--- client/components/jetpack/card/jetpack-product-card/features-item.tsx
+++ client/components/jetpack/card/jetpack-product-card/features-item.tsx
@@ -18,15 +18,24 @@ export type Props = {
 const DEFAULT_ICON = 'checkmark';
 
 const JetpackProductCardFeaturesItem: FunctionComponent< Props > = ( { item } ) => {
-	const { icon, text, description } = item;
+	const { icon, text, description, subitems } = item;
 
 	return (
 		<li className="jetpack-product-card__features-item">
-			<div className="jetpack-product-card__features-summary">
-				<Gridicon className="jetpack-product-card__features-icon" icon={ icon || DEFAULT_ICON } />
-				<p className="jetpack-product-card__features-text">{ preventWidows( text ) }</p>
+			<div className="jetpack-product-card__features-main">
+				<div className="jetpack-product-card__features-summary">
+					<Gridicon className="jetpack-product-card__features-icon" icon={ icon || DEFAULT_ICON } />
+					<p className="jetpack-product-card__features-text">{ preventWidows( text ) }</p>
+				</div>
+				{ description && <InfoPopover>{ preventWidows( description ) }</InfoPopover> }
 			</div>
-			{ description && <InfoPopover>{ preventWidows( description ) }</InfoPopover> }
+			{ subitems && subitems?.length > 0 && (
+				<ul className="jetpack-product-card__features-subitems">
+					{ subitems.map( ( subitem, index ) => (
+						<JetpackProductCardFeaturesItem key={ index } item={ subitem } />
+					) ) }
+				</ul>
+			) }
 		</li>
 	);
 };
