a30aa9a03d7eb699f7c095c9853987fa1c03d215
niranjan-uma-shankar <niranjan.u@gmail.com>
2020-08-19 12:59:43 +0530

    Revert "Checkout: Remove cart message for PWPO users (#44651)"
    This reverts commit 60027e6f7405e4789be95a0e9344c4112809229d.

--- client/my-sites/checkout/composite-checkout/components/wp-order-review-line-items.js
+++ client/my-sites/checkout/composite-checkout/components/wp-order-review-line-items.js
@@ -6,7 +6,6 @@ import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { CheckoutModal, useFormStatus, useEvents, Button } from '@automattic/composite-checkout';
 import { useTranslate } from 'i18n-calypso';
-import { useSelector } from 'react-redux';
 
 /**
  * Internal dependencies
@@ -15,8 +14,6 @@ import joinClasses from './join-classes';
 import { useHasDomainsInCart } from '../hooks/has-domains';
 import { ItemVariationPicker } from './item-variation-picker';
 import { isGSuiteProductSlug } from 'lib/gsuite';
-import { currentUserHasFlag } from 'state/current-user/selectors';
-import { NON_PRIMARY_DOMAINS_TO_FREE_USERS } from 'state/current-user/constants';
 
 export function WPOrderReviewSection( { children, className } ) {
 	return <div className={ joinClasses( [ className, 'order-review-section' ] ) }>{ children }</div>;
@@ -43,15 +40,11 @@ function WPLineItem( {
 	const itemSpanId = `checkout-line-item-${ item.id }`;
 	const deleteButtonId = `checkout-delete-button-${ item.id }`;
 	const [ isModalVisible, setIsModalVisible ] = useState( false );
-	const isPwpoUser = useSelector( ( state ) =>
-		currentUserHasFlag( state, NON_PRIMARY_DOMAINS_TO_FREE_USERS )
-	);
 	const modalCopy = returnModalCopy(
 		item.type,
 		translate,
 		hasDomainsInCart,
-		createUserAndSiteBeforeTransaction,
-		isPwpoUser
+		createUserAndSiteBeforeTransaction
 	);
 	const onEvent = useEvents();
 	const isDisabled = formStatus !== 'ready';
@@ -392,8 +385,7 @@ function returnModalCopy(
 	product,
 	translate,
 	hasDomainsInCart,
-	createUserAndSiteBeforeTransaction,
-	isPwpoUser
+	createUserAndSiteBeforeTransaction
 ) {
 	const modalCopy = {};
 	const productType = product === 'plan' && hasDomainsInCart ? 'plan with dependencies' : product;
@@ -401,19 +393,11 @@ function returnModalCopy(
 	switch ( productType ) {
 		case 'plan with dependencies':
 			modalCopy.title = translate( 'You are about to remove your plan from the cart' );
-
-			if ( createUserAndSiteBeforeTransaction ) {
-				modalCopy.description =
-					'When you press Continue, we will remove your plan from the cart. Your site will be created on the free plan when you complete payment for the other product(s) in your cart.';
-			} else {
-				modalCopy.description = isPwpoUser
-					? translate(
-							'When you press Continue, we will remove your plan from the cart and your site will continue to run with its current plan.'
-					  )
-					: translate(
-							'When you press Continue, we will remove your plan from the cart and your site will continue to run with its current plan. Since your other product(s) depend on your plan to be purchased, they will also be removed from the cart and we will take you back to your site.'
-					  );
-			}
+			modalCopy.description = createUserAndSiteBeforeTransaction
+				? 'When you press Continue, we will remove your plan from the cart. Your site will be created on the free plan when you complete payment for the other product(s) in your cart.'
+				: translate(
+						'When you press Continue, we will remove your plan from the cart and your site will continue to run with its current plan. Since your other product(s) depend on your plan to be purchased, they will also be removed from the cart and we will take you back to your site.'
+				  );
 			break;
 		case 'plan':
 			modalCopy.title = translate( 'You are about to remove your plan from the cart' );
