0ccb0fb496c4568d0d55b1e6046e01cbaeadeba2
Jarda Snajdr <jsnajdr@gmail.com>
2020-08-20 14:34:56 +0200

    Stats Controller: use individual ESM exports for functions

--- client/my-sites/stats/index.js
+++ client/my-sites/stats/index.js
@@ -8,7 +8,18 @@ import page from 'page';
  */
 import { navigation, siteSelection, sites } from 'my-sites/controller';
 import { getStatsDefaultSitePage } from 'lib/route';
-import statsController from './controller';
+import {
+	follows,
+	insights,
+	overview,
+	post,
+	site,
+	summary,
+	wordAds,
+	redirectToDefaultModulePage,
+	redirectToDefaultSitePage,
+	redirectToDefaultWordAdsPeriod,
+} from './controller';
 import { redirect as redirectToAcivity } from 'my-sites/activity/controller';
 import { makeLayout, render as clientRender } from 'controller';
 
@@ -29,21 +40,21 @@ export default function () {
 	page( '/stats', () => page.redirect( getStatsDefaultSitePage() ) );
 
 	// Stat Overview Page
-	trackedPage( '/stats/day', statsController.overview );
-	trackedPage( '/stats/week', statsController.overview );
-	trackedPage( '/stats/month', statsController.overview );
-	trackedPage( '/stats/year', statsController.overview );
+	trackedPage( '/stats/day', overview );
+	trackedPage( '/stats/week', overview );
+	trackedPage( '/stats/month', overview );
+	trackedPage( '/stats/year', overview );
 
 	trackedPage( '/stats/insights', sites );
 
 	// Stat Insights Page
-	trackedPage( '/stats/insights/:site', statsController.insights );
+	trackedPage( '/stats/insights/:site', insights );
 
 	// Stat Site Pages
-	trackedPage( '/stats/day/:site', statsController.site );
-	trackedPage( '/stats/week/:site', statsController.site );
-	trackedPage( '/stats/month/:site', statsController.site );
-	trackedPage( '/stats/year/:site', statsController.site );
+	trackedPage( '/stats/day/:site', site );
+	trackedPage( '/stats/week/:site', site );
+	trackedPage( '/stats/month/:site', site );
+	trackedPage( '/stats/year/:site', site );
 
 	const validModules = [
 		'posts',
@@ -60,45 +71,33 @@ export default function () {
 
 	// Redirect this to default /stats/day/:module/:site view to
 	// keep the paths and page view reporting consistent.
-	page(
-		`/stats/:module(${ validModules.join( '|' ) })/:site`,
-		statsController.redirectToDefaultModulePage
-	);
+	page( `/stats/:module(${ validModules.join( '|' ) })/:site`, redirectToDefaultModulePage );
 
 	// Stat Summary Pages
-	trackedPage( `/stats/day/:module(${ validModules.join( '|' ) })/:site`, statsController.summary );
-	trackedPage(
-		`/stats/week/:module(${ validModules.join( '|' ) })/:site`,
-		statsController.summary
-	);
-	trackedPage(
-		`/stats/month/:module(${ validModules.join( '|' ) })/:site`,
-		statsController.summary
-	);
-	trackedPage(
-		`/stats/year/:module(${ validModules.join( '|' ) })/:site`,
-		statsController.summary
-	);
+	trackedPage( `/stats/day/:module(${ validModules.join( '|' ) })/:site`, summary );
+	trackedPage( `/stats/week/:module(${ validModules.join( '|' ) })/:site`, summary );
+	trackedPage( `/stats/month/:module(${ validModules.join( '|' ) })/:site`, summary );
+	trackedPage( `/stats/year/:module(${ validModules.join( '|' ) })/:site`, summary );
 
 	// Stat Single Post Page
-	trackedPage( '/stats/post/:post_id/:site', statsController.post );
-	trackedPage( '/stats/page/:post_id/:site', statsController.post );
+	trackedPage( '/stats/post/:post_id/:site', post );
+	trackedPage( '/stats/page/:post_id/:site', post );
 
 	// Stat Follows Page
-	trackedPage( '/stats/follows/comment/:site', statsController.follows );
-	trackedPage( '/stats/follows/comment/:page_num/:site', statsController.follows );
+	trackedPage( '/stats/follows/comment/:site', follows );
+	trackedPage( '/stats/follows/comment/:page_num/:site', follows );
 
 	// Can't convert to trackedPage because it uses `sites` instead of `navigation`
 	page( '/stats/activity', siteSelection, sites, redirectToAcivity, makeLayout, clientRender );
 
 	trackedPage( '/stats/activity/:site', redirectToAcivity );
 
-	trackedPage( `/stats/ads/:period(${ validPeriods.join( '|' ) })/:site`, statsController.wordAds );
+	trackedPage( `/stats/ads/:period(${ validPeriods.join( '|' ) })/:site`, wordAds );
 
 	// Anything else should redirect to default WordAds stats page
-	page( '/stats/wordads/(.*)', statsController.redirectToDefaultWordAdsPeriod );
-	page( '/stats/ads/(.*)', statsController.redirectToDefaultWordAdsPeriod );
+	page( '/stats/wordads/(.*)', redirectToDefaultWordAdsPeriod );
+	page( '/stats/ads/(.*)', redirectToDefaultWordAdsPeriod );
 
 	// Anything else should redirect to default stats page
-	page( '/stats/(.*)', statsController.redirectToDefaultSitePage );
+	page( '/stats/(.*)', redirectToDefaultSitePage );
 }
