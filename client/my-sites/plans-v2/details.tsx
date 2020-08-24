63c723b736cd45ffaf5ddc7b43ce236e664732b1
Kev <kevin.zoschke@automattic.com>
2020-08-24 13:21:50 -0400

    Add loading states to cards in the details page (#45130)

--- client/my-sites/plans-v2/details.tsx
+++ client/my-sites/plans-v2/details.tsx
@@ -1,10 +1,10 @@
 /**
  * External dependencies
  */
+import { useTranslate } from 'i18n-calypso';
+import page from 'page';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import page from 'page';
-import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -16,11 +16,13 @@ import {
 } from './constants';
 import ProductCard from './product-card';
 import { slugToSelectorProduct, durationToString } from './utils';
+import ProductCardPlaceholder from 'components/jetpack/card/product-card-placeholder';
 import FormattedHeader from 'components/formatted-header';
 import HeaderCake from 'components/header-cake';
 import Main from 'components/main';
 import { getCurrentUserCurrencyCode } from 'state/current-user/selectors';
 import { isProductsListFetching } from 'state/products-list/selectors/is-products-list-fetching';
+import { getProductsList } from 'state/products-list/selectors';
 import { getSelectedSiteSlug, getSelectedSiteId } from 'state/ui/selectors';
 
 /**
@@ -35,9 +37,11 @@ const DetailsPage = ( { duration, productSlug, rootUrl, header, footer }: Detail
 	const siteSlug = useSelector( ( state ) => getSelectedSiteSlug( state ) ) || '';
 	const currencyCode = useSelector( ( state ) => getCurrentUserCurrencyCode( state ) );
 	const isFetchingProducts = useSelector( ( state ) => isProductsListFetching( state ) );
+	const products = useSelector( ( state ) => getProductsList( state ) );
 	const translate = useTranslate();
 	const root = rootUrl.replace( ':site', siteSlug );
 	const durationSuffix = duration ? '/' + durationToString( duration ) : '';
+	const isLoading = ! currencyCode || ( isFetchingProducts && ! products );
 
 	// If the product slug isn't one that has options, proceed to the upsell.
 	if ( ! ( PRODUCTS_WITH_OPTIONS as readonly string[] ).includes( productSlug ) ) {
@@ -50,10 +54,6 @@ const DetailsPage = ( { duration, productSlug, rootUrl, header, footer }: Detail
 	if ( ! product ) {
 		page.redirect( root + durationSuffix );
 		return null;
-	}
-
-	if ( ! currencyCode || isFetchingProducts ) {
-		return null; // TODO: Loading component!
 	}
 
 	// Go to a new page for upsells.
@@ -87,23 +87,30 @@ const DetailsPage = ( { duration, productSlug, rootUrl, header, footer }: Detail
 				brandFont
 			/>
 			<div className="plans-v2__columns">
-				{ product.subtypes.map( ( subtypeSlug ) => {
-					const subtypeItem = slugToSelectorProduct( subtypeSlug );
-					if ( ! subtypeItem ) {
-						// Exit if invalid product.
-						return null;
-					}
-					return (
-						<ProductCard
-							key={ subtypeSlug }
-							item={ subtypeItem }
-							siteId={ siteId }
-							currencyCode={ currencyCode }
-							onClick={ () => selectProduct( subtypeItem ) }
-							className="details__column"
-						/>
-					);
-				} ) }
+				{ isLoading ? (
+					<>
+						<ProductCardPlaceholder className="details__column" />
+						<ProductCardPlaceholder className="details__column" />
+					</>
+				) : (
+					product.subtypes.map( ( subtypeSlug ) => {
+						const subtypeItem = slugToSelectorProduct( subtypeSlug );
+						if ( ! subtypeItem ) {
+							// Exit if invalid product.
+							return null;
+						}
+						return (
+							<ProductCard
+								key={ subtypeSlug }
+								item={ subtypeItem }
+								siteId={ siteId }
+								currencyCode={ currencyCode as string }
+								onClick={ () => selectProduct( subtypeItem ) }
+								className="details__column"
+							/>
+						);
+					} )
+				) }
 			</div>
 			{ footer }
 		</Main>
