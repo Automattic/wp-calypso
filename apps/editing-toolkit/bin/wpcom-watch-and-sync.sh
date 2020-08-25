0e184462e20d562c94fd13fb55664b453e39dc2e
Noah Allen <noahtallen@gmail.com>
2020-08-24 17:05:21 -0700

    Fix remote sync path

--- apps/editing-toolkit/bin/wpcom-watch-and-sync.sh
+++ apps/editing-toolkit/bin/wpcom-watch-and-sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 LOCAL_PATH="./editing-toolkit-plugin/"
-REMOTE_PATH="/home/wpcom/public_html/wp-content/plugins/editing-toolkit-plugin/dev"
+REMOTE_PATH="/home/wpcom/public_html/wp-content/plugins/full-site-editing-plugin/dev"
 REMOTE_SSH="wpcom-sandbox"
 COMMAND="rsync -ahz $LOCAL_PATH $REMOTE_SSH:$REMOTE_PATH"
 
