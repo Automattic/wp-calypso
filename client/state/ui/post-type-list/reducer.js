94f3456ae058d02d7af090a7284a2dbe07f08845
Marin Atanasov <tyxla@abv.bg>
2020-08-26 11:39:25 +0300

    State: Remove multi selection reducer

--- client/state/ui/post-type-list/reducer.js
+++ client/state/ui/post-type-list/reducer.js
@@ -1,17 +1,9 @@
 /**
- * External dependencies
- */
-
-import { without } from 'lodash';
-
-/**
  * Internal dependencies
  */
 import {
 	POST_TYPE_LIST_LIKES_POPOVER_HIDE,
 	POST_TYPE_LIST_LIKES_POPOVER_TOGGLE,
-	POST_TYPE_LIST_MULTI_SELECTION_MODE_TOGGLE,
-	POST_TYPE_LIST_SELECTION_TOGGLE,
 	POST_TYPE_LIST_SHARE_PANEL_HIDE,
 	POST_TYPE_LIST_SHARE_PANEL_TOGGLE,
 	ROUTE_SET,
@@ -20,37 +12,14 @@ import {
 const initialState = {
 	postIdWithActiveSharePanel: null,
 	postIdWithActiveLikesPopover: null,
-	isMultiSelectEnabled: false,
-	selectedPosts: [],
 };
 
 export const postTypeList = ( state = initialState, action ) => {
 	switch ( action.type ) {
-		case POST_TYPE_LIST_MULTI_SELECTION_MODE_TOGGLE:
-			return {
-				...state,
-				isMultiSelectEnabled: ! state.isMultiSelectEnabled,
-				selectedPosts: state.isMultiSelectEnabled ? state.selectedPosts : [],
-			};
-
-		case POST_TYPE_LIST_SELECTION_TOGGLE:
-			if ( state.selectedPosts.indexOf( action.postGlobalId ) > -1 ) {
-				return {
-					...state,
-					selectedPosts: without( state.selectedPosts, action.postGlobalId ),
-				};
-			}
-			return {
-				...state,
-				selectedPosts: [ ...state.selectedPosts, action.postGlobalId ],
-			};
-
 		case ROUTE_SET:
 			return {
 				...state,
-				isMultiSelectEnabled: false,
 				postIdWithActiveLikesPopover: null,
-				selectedPosts: [],
 			};
 
 		case POST_TYPE_LIST_LIKES_POPOVER_HIDE:
