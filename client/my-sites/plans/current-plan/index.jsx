923adceb2c175933ad28d15e790a26fd17793be5
Claudiu Filip <claudiucelfilip@gmail.com>
2020-08-26 12:28:53 +0300

    Remove Quick Start from Bussiness feature on My Plan page (#44461)
    * Remove Quick Start from Bussiness feature on My Plan page for new accounts
    
    * Simplify business onboarding check by checking the plans expiry date
    
    * Address situation where currentPlan might not be set yet.
    
    * Show business QS for old accounts only if sessions are available
    
    * Remove unnecessary logging
    
    * Remove unnecessary changes
    
    * Integrate the extra year until QS expiration into the constant
    
    * Lint fixes
    
    * Correlate expiration date to the QS mention removal deployment date
    
    * Use scheduleId to check for included and purchased sessions
    
    * Fix failing tests
    
    * Refresh concierge data on site change
    
    * Add support for unexpired included QS sessions for 2Y Business plan
    
    * Use constant for 2 year plan check

--- client/my-sites/plans/current-plan/index.jsx
+++ client/my-sites/plans/current-plan/index.jsx
@@ -49,6 +49,8 @@ import AntiSpamProductThankYou from './current-plan-thank-you/anti-spam-thank-yo
 import SearchProductThankYou from './current-plan-thank-you/search-thank-you';
 import { isFreeJetpackPlan, isFreePlan } from 'lib/products-values';
 import { getSitePurchases } from 'state/purchases/selectors';
+import QueryConciergeInitial from 'components/data/query-concierge-initial';
+import getConciergeScheduleId from 'state/selectors/get-concierge-schedule-id';
 
 /**
  * Style dependencies
@@ -83,9 +85,9 @@ class CurrentPlan extends Component {
 	}
 
 	isLoading() {
-		const { selectedSite, isRequestingSitePlans: isRequestingPlans } = this.props;
+		const { selectedSite, isRequestingSitePlans: isRequestingPlans, scheduleId } = this.props;
 
-		return ! selectedSite || isRequestingPlans;
+		return ! selectedSite || isRequestingPlans || null === scheduleId;
 	}
 
 	renderThankYou() {
@@ -159,6 +161,10 @@ class CurrentPlan extends Component {
 					headerText={ translate( 'Plans' ) }
 					align="left"
 				/>
+				{ selectedSiteId && (
+					// key={ selectedSiteId } ensures data is refetched for changing selectedSiteId
+					<QueryConciergeInitial key={ selectedSiteId } siteId={ selectedSiteId } />
+				) }
 				<QuerySites siteId={ selectedSiteId } />
 				<QuerySitePlans siteId={ selectedSiteId } />
 				<QuerySitePurchases siteId={ selectedSiteId } />
@@ -258,5 +264,6 @@ export default connect( ( state, { requestThankYou } ) => {
 		shouldShowDomainWarnings: ! isJetpack || isAutomatedTransfer,
 		showJetpackChecklist: isJetpackNotAtomic,
 		showThankYou: requestThankYou && isJetpackNotAtomic,
+		scheduleId: getConciergeScheduleId( state ),
 	};
 } )( localize( CurrentPlan ) );
