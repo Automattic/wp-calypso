802fc68b62e633346c6e060f376400b41c1ff07b
Paul Bunkham <paul@dobit.co.uk>
2020-08-26 20:10:27 +0100

    Set a default of using Gutenframe

--- client/state/data-layer/wpcom/sites/gutenberg/index.js
+++ client/state/data-layer/wpcom/sites/gutenberg/index.js
@@ -40,7 +40,7 @@ const setGutenbergOptInData = (
 		opt_in: optIn,
 		opt_out: optOut,
 		in_editor_deprecation_group: inEditorDeprecationGroup,
-		eligible_gutenframe: isEligibleForGutenframe,
+		eligible_gutenframe: isEligibleForGutenframe = true,
 	}
 ) => ( dispatch ) => {
 	dispatch( { type: EDITOR_TYPE_SET, siteId, editor } );
