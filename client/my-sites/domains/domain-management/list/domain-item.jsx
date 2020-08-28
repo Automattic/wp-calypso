054e950bcba4916796fb7f94c698e12e7a8ba29c
fditrapani <filippo.di.trapani@a8c.com>
2020-08-27 22:50:39 -0400

    Add domain type label to site-level domains

--- client/my-sites/domains/domain-management/list/domain-item.jsx
+++ client/my-sites/domains/domain-management/list/domain-item.jsx
@@ -22,7 +22,12 @@ import { hasGSuiteWithUs, getGSuiteMailboxCount } from 'lib/gsuite';
 import { withoutHttp } from 'lib/url';
 import { type as domainTypes } from 'lib/domains/constants';
 import { handleRenewNowClick } from 'lib/purchases';
-import { resolveDomainStatus, isDomainInGracePeriod, isDomainUpdateable } from 'lib/domains';
+import {
+	resolveDomainStatus,
+	isDomainInGracePeriod,
+	isDomainUpdateable,
+	getDomainTypeText,
+} from 'lib/domains';
 import InfoPopover from 'components/info-popover';
 import { emailManagement } from 'my-sites/email/paths';
 import {
@@ -370,7 +375,7 @@ class DomainItem extends PureComponent {
 			);
 		}
 
-		return <span>Domain type</span>;
+		return <React.Fragment>{ getDomainTypeText( domainDetails ) }</React.Fragment>;
 	}
 
 	busyMessage() {
