a8bab01fd18ab2020b4db0d4d70620d3823ad000
Marcelo Serpa <marcelo.serpa@a8c.com>
2020-08-28 10:29:51 -0500

    Automate the testing of popular blocks in current and edge versions of GB (#45048)
    * Automate the testing of popular blocks in current and edge versions of GB
    
    * Take screenshots
    
    * Improve code a bit and group sshots in a directory named after the site
    
    * Simplified the assertions by DRYing it in a single re-usable function
    
    * Update config with the new test account credentials
    
    Add a new account in the "testAccounts" object called "gutenbergUpgradeUser". This account has all the test sites used by the GB upgrade tests.
    
    * Cleanup
    
    * Take screenshot of editor blocks and preview for edge sites too
    
    * Step description improvement
    
    * Use the published page instead of the preview to check content rendered on the frontendn
    
    * Fix code editor text area not being found in headless mode
    
    * Reduce repetition and some other minor improvements
    
    - Get rid of repeat block factory methods in the GutenbergEditorComponent class;
    - Let the block class know about its name and title;
    - Change the usage of name to more closely match the semantics of the block API (name and title);
    - Added a step to check for errors in the editor in the edge site;
    - Added a step to check if all expected blocks are in the editor in the edge site;
    
    * Use static getters to define block name and title in the test block component classes
    
    * Close the tools and options menu after using to avoid it getting on the way of sshots
    
    * Added check for invalid blocks and grouped other common steps in a re-usable helper
    
    * Comment-out the invalid block assertion temporarily
    
    Because of  because of https://github.com/Automattic/jetpack/issues/16514
    
    * Move the file selector for the TiledGalleryBlockComponent to a field with a name that better describes it
    
    * Simplify a bit more and be explicit about the editor checks
    
    * Use static properties for block{Name,Title}
    
    Co-authored-by: Jon Surrell <jon.surrell@automattic.com>

---
+++ test/e2e/lib/gutenberg/blocks/index.js
@@ -0,0 +1,18 @@
+// TODO some blocks are exported as default and some not... maybe change all of them to export as default?
+export { BlogPostsBlockComponent } from './blog-posts-block-component';
+export { ContactFormBlockComponent } from './contact-form-block-component';
+export { ContactInfoBlockComponent } from './contact-info-block-component';
+export { DynamicSeparatorBlockComponent } from './dynamic-separator-block-component';
+export { GalleryMasonryBlockComponent } from './gallery-masonry-block-component';
+export { ImageBlockComponent } from './image-block-component';
+export { LayoutGridBlockComponent } from './layout-grid-block-component';
+export { RatingStarBlockComponent } from './rating-star-block-component';
+export { ShortcodeBlockComponent } from './shortcode-block-component';
+export { SlideshowBlockComponent } from './slideshow-block-component';
+export { SubscriptionsBlockComponent } from './subscriptions-block-component';
+export { TiledGalleryBlockComponent } from './tiled-gallery-block-component';
+export { YoutubeBlockComponent } from './youtube-block-component';
+export { default as EmbedsBlockComponent } from './embeds-block-component';
+export { default as GutenbergBlockComponent } from './gutenberg-block-component';
+export { default as MarkdownBlockComponent } from './markdown-block-component';
+export { default as SimplePaymentBlockComponent } from './payment-block-component';
---
+++ test/e2e/lib/gutenberg/blocks/youtube-block-component.js
@@ -0,0 +1,11 @@
+/**
+ * Internal dependencies
+ */
+import GutenbergBlockComponent from './gutenberg-block-component';
+
+class YoutubeBlockComponent extends GutenbergBlockComponent {
+	static blockTitle = 'YouTube';
+	static blockName = 'core-embed/youtube';
+}
+
+export { YoutubeBlockComponent };
