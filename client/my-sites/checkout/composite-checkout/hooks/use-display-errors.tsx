454f6dab7757e7db2b653535b681e9ba3d5ebb71
Payton Swick <payton@foolord.com>
2020-08-28 12:10:32 -0400

    Convert useDisplayErrors to TS and fix a bug

--- client/my-sites/checkout/composite-checkout/hooks/use-display-errors.tsx
+++ client/my-sites/checkout/composite-checkout/hooks/use-display-errors.tsx
@@ -8,16 +8,14 @@ import React, { useEffect, useRef } from 'react';
  */
 import notices from 'notices';
 
-export default function useDisplayErrors( errorMessages ) {
-	const previousMessages = useRef( [] );
+export default function useDisplayErrors( errorMessages: string[] ) {
+	const previousMessages = useRef< string[] >( [] );
 	useEffect( () => {
 		const messagesToDisplay = errorMessages.filter(
 			( message ) => ! previousMessages.current.includes( message )
 		);
 		if ( messagesToDisplay.length > 0 ) {
-			notices.error(
-				messagesToDisplay.errors.map( ( message ) => <p key={ message }>{ message }</p> )
-			);
+			notices.error( messagesToDisplay.map( ( message ) => <p key={ message }>{ message }</p> ) );
 		}
 		previousMessages.current = messagesToDisplay;
 	}, [ errorMessages ] );
