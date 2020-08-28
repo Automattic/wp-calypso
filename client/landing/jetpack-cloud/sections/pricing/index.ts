e7ec9dc3e21e12562eec01dd323dcdc1a90bfd6b
Renzo Canepa <rcanepag@gmail.com>
2020-08-28 17:48:20 -0300

    Remove check since this isn't part of CalypsoBlue

--- client/landing/jetpack-cloud/sections/pricing/index.ts
+++ client/landing/jetpack-cloud/sections/pricing/index.ts
@@ -8,10 +8,7 @@ import page from 'page';
  */
 import { makeLayout, render as clientRender } from 'controller';
 import { pricing } from 'landing/jetpack-cloud/sections/pricing/controller';
-import isJetpackCloud from 'lib/jetpack/is-jetpack-cloud';
 
 export default function () {
-	if ( isJetpackCloud() ) {
-		page( '/pricing', pricing, makeLayout, clientRender );
-	}
+	page( '/pricing', pricing, makeLayout, clientRender );
 }
