dfefce8ee1273a6c30896e033d03d4cb1b5dfcb1
Renzo Canepa <rcanepag@gmail.com>
2020-08-31 15:15:42 -0300

    Fix positioning since there is no masterbar

--- client/landing/jetpack-cloud/sections/pricing/index.ts
+++ client/landing/jetpack-cloud/sections/pricing/index.ts
@@ -4,6 +4,11 @@
 import plansV2 from 'my-sites/plans-v2';
 import * as controller from './controller';
 
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
 export default function () {
 	plansV2( `/pricing`, controller.jetpackPricingContext );
 }
