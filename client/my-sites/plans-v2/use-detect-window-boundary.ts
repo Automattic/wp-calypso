81185c5228b3ff12d0168054bb5a73df1793227c
Renzo Canepa <rcanepag@gmail.com>
2020-08-31 15:36:59 -0300

    Fix glitch/jump when going from sticky to normal

--- client/my-sites/plans-v2/use-detect-window-boundary.ts
+++ client/my-sites/plans-v2/use-detect-window-boundary.ts
@@ -13,13 +13,24 @@ import { throttle } from 'lodash';
 const useDetectWindowBoundary = ( elementRef: MutableRefObject< HTMLDivElement | null > ) => {
 	// Indicates whether the elementRef has crossed the upper window boundary
 	const [ borderCrossed, setBorderCrossed ] = useState( false );
+	// Stores the initial position of the element in the Y axis. We need this to put the
+	// element back in its place when the scroll bar reaches this point a not when the scroll
+	// bar reaches the top of the page (0 in the Y axis).
+	const initialTopPos = useRef< number | null >( null );
 
 	const addStickyClass = useRef< ( this: Window, e: Event ) => void >(
 		throttle( () => {
 			if ( ! elementRef || ! elementRef.current ) {
 				return;
 			}
-			setBorderCrossed( window.pageYOffset > elementRef.current.offsetTop );
+
+			const { top: distanceRelativeToViewport } = elementRef.current.getBoundingClientRect();
+
+			if ( ! initialTopPos.current && distanceRelativeToViewport ) {
+				initialTopPos.current = distanceRelativeToViewport;
+			}
+
+			setBorderCrossed( window.pageYOffset > initialTopPos.current );
 		}, 50 )
 	);
 
