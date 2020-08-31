23eba83cb8e975f49564bfc2d93cdeed5b3e66e8
Renzo Canepa <rcanepag@gmail.com>
2020-08-31 15:04:36 -0300

    Hide the Jetpack Masterbar

--- client/landing/jetpack-cloud/sections/pricing/controller.tsx
+++ client/landing/jetpack-cloud/sections/pricing/controller.tsx
@@ -7,8 +7,10 @@ import React from 'react';
  * Internal dependencies
  */
 import StoreFooter from 'jetpack-connect/store-footer';
+import { hideMasterbar } from 'state/ui/actions';
 
 export function jetpackPricingContext( context, next ) {
+	context.store.dispatch( hideMasterbar() );
 	context.footer = <StoreFooter />;
 	next();
 }
