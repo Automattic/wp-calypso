5be23692baf30ae520b2a7c6ae45912297543079
Sergio Cinos <sergio.cinos@gmail.com>
2020-08-31 15:25:58 +0200

    Save Jest cache into `./cache/jest` (#44904)

--- packages/calypso-codemods/jest.config.js
+++ packages/calypso-codemods/jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
 	rootDir: __dirname,
 	testMatch: [ '<rootDir>/tests/*/codemod.spec.js' ],
 	setupFiles: [ '<rootDir>/setup-tests.js' ],
+	cacheDirectory: '<rootDir>/../../.cache/jest',
 };
