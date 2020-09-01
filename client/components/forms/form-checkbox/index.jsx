92b71075afa0b9fa3121ca77f1c3957d2e749580
Marin Atanasov <tyxla@abv.bg>
2020-09-01 11:17:54 +0300

    Components: Move checkbox styles from global to component

--- client/components/forms/form-checkbox/index.jsx
+++ client/components/forms/form-checkbox/index.jsx
@@ -4,6 +4,11 @@
 import React from 'react';
 import classnames from 'classnames';
 
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
 const FormInputCheckbox = ( { className, ...otherProps } ) => (
 	<input { ...otherProps } type="checkbox" className={ classnames( className, 'form-checkbox' ) } />
 );
