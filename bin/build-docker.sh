971dbf1f3435680a0d20b52461c012919a9fc96c
Sergio Cinos <sergio.cinos@gmail.com>
2020-09-01 12:45:21 +0200

    Wip

--- bin/build-docker.sh
+++ bin/build-docker.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+set -o errexit
+set -o nounset
+set -o pipefail
 
-VERSION="1.0.5"
+if [[ -z "${1:-}" ]] ; then
+    echo "Missing version. Usage:"
+	echo "$0 <version>"
+    exit 1
+fi
+
+VERSION="$1"
 
 BUILDER_IMAGE_NAME="automattic/wp-calypso-base"
 CI_IMAGE_NAME="automattic/wp-calypso-ci"
@@ -9,9 +18,12 @@ CI_IMAGE="${CI_IMAGE_NAME}:${VERSION}"
 
 docker build -f docker/Dockerfile --target builder -t "$BUILDER_IMAGE" .
 docker build -f docker/Dockerfile --target ci -t "$CI_IMAGE" .
+
 docker tag "$BUILDER_IMAGE" "${BUILDER_IMAGE_NAME}:latest"
 docker tag "$CI_IMAGE" "${CI_IMAGE_NAME}:latest"
+
 docker push "$CI_IMAGE"
 docker push "${CI_IMAGE_NAME}:latest"
+
 docker push "$BUILDER_IMAGE"
 docker push "${BUILDER_IMAGE_NAME}:latest"
