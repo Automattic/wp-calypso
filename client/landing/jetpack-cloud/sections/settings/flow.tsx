f52c821ec67d7283d6c1fdad9d06cc0789545d02
Caleb Bauermeister <cbauerman@gmail.com>
2020-09-02 12:58:01 -0700

    add main frame to new settings flow

--- client/landing/jetpack-cloud/sections/settings/flow.tsx
+++ client/landing/jetpack-cloud/sections/settings/flow.tsx
@@ -2,11 +2,23 @@
  * External dependencies
  */
 import React, { FunctionComponent } from 'react';
+import { useTranslate } from 'i18n-calypso';
 
-const SettingsFlow: FunctionComponent = () => (
-	<div>
-		<p>{ 'Jetpack Cloud Setting Flow is a WIP.' }</p>
-	</div>
-);
+/**
+ * Internal dependencies
+ */
+import DocumentHead from 'components/data/document-head';
+import Main from 'components/main';
+import SidebarNavigation from 'my-sites/sidebar-navigation';
+
+const SettingsFlow: FunctionComponent = () => {
+	const translate = useTranslate();
+	return (
+		<Main className="settings">
+			<DocumentHead title={ translate( 'Settings' ) } />
+			<SidebarNavigation />
+		</Main>
+	);
+};
 
 export default SettingsFlow;
