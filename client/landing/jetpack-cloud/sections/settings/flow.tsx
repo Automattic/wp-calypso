efa2f31c76f87eac8cf075324cdf38c548587d72
Caleb Bauermeister <cbauerman@gmail.com>
2020-09-02 13:05:50 -0700

    add step progress

--- client/landing/jetpack-cloud/sections/settings/flow.tsx
+++ client/landing/jetpack-cloud/sections/settings/flow.tsx
@@ -1,22 +1,35 @@
 /**
  * External dependencies
  */
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
+import { Card } from '@automattic/components';
 import DocumentHead from 'components/data/document-head';
 import Main from 'components/main';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
+import StepProgress from 'components/step-progress';
 
 const SettingsFlow: FunctionComponent = () => {
 	const translate = useTranslate();
+	const [ step ] = useState( 0 );
+
+	const steps = [
+		translate( 'Host locator' ),
+		translate( 'Credentials' ),
+		translate( 'Verification' ),
+	];
+
 	return (
 		<Main className="settings">
 			<DocumentHead title={ translate( 'Settings' ) } />
 			<SidebarNavigation />
+			<Card>
+				<StepProgress currentStep={ step } steps={ steps } />
+			</Card>
 		</Main>
 	);
 };
