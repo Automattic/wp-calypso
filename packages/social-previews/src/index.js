374acc846ffb3d69cd2deb6bd98f36e09db34b76
Gary <gary@pento.net>
2020-09-02 17:14:13 +1000

    Add support for Twitter Cards, and merge back into the original TwitterPreview component.

--- packages/social-previews/src/index.js
+++ packages/social-previews/src/index.js
@@ -1,4 +1,3 @@
 export { default as FacebookPreview } from './facebook-preview';
 export { default as TwitterPreview } from './twitter-preview';
-export { default as TwitterThreadPreview } from './twitter-thread-preview';
 export { default as SearchPreview } from './search-preview';
