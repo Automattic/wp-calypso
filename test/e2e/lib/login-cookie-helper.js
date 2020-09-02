305e55fb298db5440ecd567ec7454f83ed8e38b5
Brent Sessions <brent.sessions@automattic.com>
2020-09-02 08:06:05 -0600

    Disable cookie reuse

--- test/e2e/lib/login-cookie-helper.js
+++ test/e2e/lib/login-cookie-helper.js
@@ -23,7 +23,6 @@ export async function loadLoginCookies( driver, filePath ) {
 	const rawData = await fs.readFileSync( filePath );
 	const cookies = await JSON.parse( rawData );
 	for ( const cookie of cookies ) {
-		cookie.sameSite='Strict';
 		await driver.manage().addCookie( cookie );
 	}
 }
