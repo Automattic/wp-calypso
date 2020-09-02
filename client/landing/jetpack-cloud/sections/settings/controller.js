684c8ce839b20faf99c211336f4e8f4a23b87dcc
Caleb Bauermeister <cbauerman@gmail.com>
2020-09-02 13:29:33 -0700

    add message

--- client/landing/jetpack-cloud/sections/settings/controller.js
+++ client/landing/jetpack-cloud/sections/settings/controller.js
@@ -8,7 +8,7 @@ import React from 'react';
  */
 import { isEnabled } from 'config';
 import SettingsPage from './main';
-import SettingsFlow from './flow';
+import SettingsFlow from './settings-flow';
 
 export function settings( context, next ) {
 	context.primary = isEnabled( 'jetpack/server-credentials-advanced-flow' ) ? (
--- client/landing/jetpack-cloud/sections/settings/settings-flow/index.tsx
+++ client/landing/jetpack-cloud/sections/settings/settings-flow/index.tsx
@@ -13,6 +13,11 @@ import Main from 'components/main';
 import SidebarNavigation from 'my-sites/sidebar-navigation';
 import StepProgress from 'components/step-progress';
 
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
 const SettingsFlow: FunctionComponent = () => {
 	const translate = useTranslate();
 	const [ step ] = useState( 0 );
@@ -24,11 +29,21 @@ const SettingsFlow: FunctionComponent = () => {
 	];
 
 	return (
-		<Main className="settings">
+		<Main className="settings-flow">
 			<DocumentHead title={ translate( 'Settings' ) } />
 			<SidebarNavigation />
 			<Card>
 				<StepProgress currentStep={ step } steps={ steps } />
+				<div className="settings-flow__notice">
+					{ translate(
+						'In order to restore your site, should something go wrong, you’ll need to provide your websites {{strong}}SSH{{/strong}}, {{strong}}SFTP{{/strong}} or {{strong}}FTP{{/strong}} server credentials. We’ll guide you through it:',
+						{
+							components: { strong: <strong /> },
+						}
+					) }
+				</div>
+				<h3>{ translate( 'Select your website host for example.com' ) }</h3>
+				<p>{ translate( 'It looks like your host is SiteGround' ) }</p>
 			</Card>
 		</Main>
 	);
