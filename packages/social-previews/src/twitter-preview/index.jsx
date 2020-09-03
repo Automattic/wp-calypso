d03ef3edf91ad2d329186189f1c4fee9e9ca6e2b
Gary <gary@pento.net>
2020-09-03 10:12:25 +1000

    Add back compat with the old style of props.

--- packages/social-previews/src/twitter-preview/index.jsx
+++ packages/social-previews/src/twitter-preview/index.jsx
@@ -16,7 +16,24 @@ import './style.scss';
 
 export class TwitterPreview extends PureComponent {
 	render() {
-		const { tweets } = this.props;
+		// Previous versions expected passed props to be the Twitter Card details for a single tweet,
+		// rather than an array of tweets. If the tweet array isn't passed, we can construct it from
+		// the old props.
+		const tweets = this.props.tweets || [
+			{
+				text: '',
+				media: [],
+				card: {
+					...this.props,
+					type: 'large_image_summary' === this.props.type ? 'summary_large_image' : this.props.type,
+				},
+				date: Date.now(),
+				name: 'Account Name',
+				profileImage:
+					'https://abs.twimg.com/sticky/default_profile_images/default_profile_bigger.png',
+				screenName: '@account',
+			},
+		];
 
 		return (
 			<div className="twitter-preview">
