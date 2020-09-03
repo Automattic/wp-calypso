6e5ede2a9d3d99660dbaa91ef44c446af6063ad5
Renzo Canepa <rcanepag@gmail.com>
2020-09-03 19:42:16 -0300

    Offer Reset: add JPCOM master bar to the Pricing page (#45369)
    * Create desktop version of Jetpack.com's masterbar
    
    * Rename to JetpackComMasterbar
    
    * Add JetpackComMasterbar to the Header
    
    * Set the background-color to white
    
    * Add Jetpack.com masterbar hamburguer menu
    
    * Replace JetpackComMasterBar with JPCOMMasterbar
    
    * Remove unused CSS (we are hiding the label from JS)
    
    * Use the same size it has in jetpack.com
    
    * Fix cross to hamburguer animation
    
    * Use a more appropriate component for the link

---
+++ client/landing/jetpack-cloud/sections/pricing/jpcom-masterbar/index.tsx
@@ -0,0 +1,90 @@
+/**
+ * External dependencies
+ */
+import React, { useRef, useState } from 'react';
+import classNames from 'classnames';
+import { translate } from 'i18n-calypso';
+
+/**
+ * Internal dependencies
+ */
+import { Button } from '@automattic/components';
+import ExternalLink from 'components/external-link';
+import JetpackLogo from 'components/jetpack-logo';
+import useDetectWindowBoundary from 'my-sites/plans-v2/use-detect-window-boundary';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
+const JETPACK_COM_BASE_URL = 'https://jetpack.com';
+const MENU_ITEMS = [
+	{
+		title: translate( 'Product Tour' ),
+		path: 'features',
+	},
+	{
+		title: translate( 'Pricing' ),
+		path: 'pricing',
+	},
+	{
+		title: translate( 'Support' ),
+		path: 'support',
+	},
+	{
+		title: translate( 'Blog' ),
+		path: 'blog',
+	},
+];
+
+const JetpackComMasterbar = () => {
+	const [ isMenuOpen, setIsMenuOpen ] = useState( false );
+	const barRef = useRef< HTMLDivElement | null >( null );
+	const hasCrossed = useDetectWindowBoundary( barRef );
+
+	const toggleMenu = () => {
+		setIsMenuOpen( ( currentState ) => ! currentState );
+	};
+
+	return (
+		<div ref={ barRef } className={ classNames( 'jpcom-masterbar', { sticky: hasCrossed } ) }>
+			<div className="jpcom-masterbar__inner">
+				<ExternalLink className="jpcom-masterbar__logo" href={ JETPACK_COM_BASE_URL }>
+					<JetpackLogo className="jpcom-masterbar__jetpack-logo" full size={ 49 } />
+				</ExternalLink>
+
+				<Button
+					className={ classNames( [ 'jpcom-masterbar__navbutton', 'mobilenav' ], {
+						'is-active': isMenuOpen,
+					} ) }
+					aria-label={ translate( 'Menu' ) }
+					aria-controls="navigation"
+					onClick={ toggleMenu }
+				>
+					<span className="jpcom-masterbar__navbox">
+						<span className="jpcom-masterbar__navinner"></span>
+					</span>
+					<span className="jpcom-masterbar__navlabel">
+						{ isMenuOpen ? null : translate( 'Menu' ) }
+					</span>
+				</Button>
+
+				<ul className={ classNames( 'jpcom-masterbar__nav', { 'is-open': isMenuOpen } ) }>
+					{ MENU_ITEMS.map( ( { title, path }, index ) => (
+						<li className="jpcom-masterbar__nav-item" key={ index }>
+							<a
+								className={ path === 'pricing' ? 'current' : '' }
+								href={ `${ JETPACK_COM_BASE_URL }/${ path }` }
+							>
+								{ title }
+							</a>
+						</li>
+					) ) }
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default JetpackComMasterbar;
