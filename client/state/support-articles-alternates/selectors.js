62e3c6ae192f9fe28e495f05ad4e838a8065c144
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2020-09-04 14:13:46 +0300

    Add tests

--- client/state/support-articles-alternates/selectors.js
+++ client/state/support-articles-alternates/selectors.js
@@ -8,6 +8,13 @@ import { get, has, isEmpty } from 'lodash';
  */
 import { keyToString } from 'reader/post-key';
 
+/**
+ * Whether it's currently requesting alternates for a post
+ *
+ * @param   {object} state           Store state
+ * @param   {object} postKeySegments Post key object that should have `blogId` and `postId` keys
+ * @returns {boolean}                Is it currently requesting alternates
+ */
 export const isRequestingSupportArticleAlternates = ( state, postKeySegments ) => {
 	const postKey = keyToString( postKeySegments );
 
@@ -17,18 +24,39 @@ export const isRequestingSupportArticleAlternates = ( state, postKeySegments ) =
 	);
 };
 
+/**
+ * Whether post alternates request has completed successfully
+ *
+ * @param   {object} state           Store state
+ * @param   {object} postKeySegments Post key object that should have `blogId` and `postId` keys
+ * @returns {boolean}                Has post alternates request completed successfully
+ */
 export const isRequestingSupportArticleAlternatesCompleted = ( state, postKeySegments ) => {
 	const postKey = keyToString( postKeySegments );
 
 	return get( state.supportArticlesAlternates.requests, [ postKey, 'completed' ], false );
 };
 
+/**
+ * Whether post alternates request has failed
+ *
+ * @param   {object} state           Store state
+ * @param   {object} postKeySegments Post key object that should have `blogId` and `postId` keys
+ * @returns {boolean}                Has post alternates request failed
+ */
 export const isRequestingSupportArticleAlternatesFailed = ( state, postKeySegments ) => {
 	const postKey = keyToString( postKeySegments );
 
 	return get( state.supportArticlesAlternates.requests, [ postKey, 'failed' ], false );
 };
 
+/**
+ * Determine if alternates should be request for a post based on the current requests state
+ *
+ * @param   {object} state           Store state
+ * @param   {object} postKeySegments Post key object that should have `blogId` and `postId` keys
+ * @returns {boolean}                Should post alternates be requested
+ */
 export const shouldRequestSupportArticleAlternates = ( state, postKeySegments ) => {
 	return (
 		! isRequestingSupportArticleAlternates( state, postKeySegments ) &&
@@ -36,12 +64,27 @@ export const shouldRequestSupportArticleAlternates = ( state, postKeySegments ) 
 	);
 };
 
+/**
+ * Get all available post alternates state
+ *
+ * @param   {object} state           Store state
+ * @param   {object} postKeySegments Post key object that should have `blogId` and `postId` keys
+ * @returns {object?}                Post alternates
+ */
 export const getSupportArticleAlternates = ( state, postKeySegments ) => {
 	const postKey = keyToString( postKeySegments );
 
 	return get( state.supportArticlesAlternates.items, [ postKey ] );
 };
 
+/**
+ * Get post alternates for specific locale
+ *
+ * @param   {object} state           Store state
+ * @param   {object} postKeySegments Post key object that should have `blogId` and `postId` keys
+ * @param   {string} locale          Locale slug
+ * @returns {object?}                Post alternate for specific locale
+ */
 export const getSupportArticleAlternatesForLocale = ( state, postKeySegments, locale ) => {
 	return get( getSupportArticleAlternates( state, postKeySegments ), [ locale ] );
 };
