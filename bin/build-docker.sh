9ac0fa84906fce65a4686d48b6f04cb7def4a933
Sergio Cinos <sergio.cinos@gmail.com>
2020-09-04 12:34:54 +0200

    Disable cache for base image

--- bin/build-docker.sh
+++ bin/build-docker.sh
@@ -15,5 +15,5 @@ CI_IMAGE_NAME="registry.a8c.com/calypso/ci"
 BUILDER_IMAGE="${BUILDER_IMAGE_NAME}:${VERSION}"
 CI_IMAGE="${CI_IMAGE_NAME}:${VERSION}"
 
-docker build -f Dockerfile.base --target builder -t "$BUILDER_IMAGE" .
+docker build -f Dockerfile.base --no-cache --target builder -t "$BUILDER_IMAGE" .
 docker build -f Dockerfile.base --target ci -t "$CI_IMAGE" .
