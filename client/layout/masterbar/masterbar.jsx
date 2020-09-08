46cc8cea3cacb4b8da924f3e702806e552249dc9
cpap <papazoglou.charalampos@gmail.com>
2020-09-08 11:17:29 +0300

    Nav Unification: rename flag, remove test code

--- client/layout/masterbar/masterbar.jsx
+++ client/layout/masterbar/masterbar.jsx
@@ -4,19 +4,11 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
-import config from 'config';
 
 /**
  * Style dependencies
  */
 import './style.scss';
-
-/**
- * Remove after checking in calypso live
- */
-config.isEnabled( 'nav-unification' )
-	? console.log( 'nav-unification feature: Enabled' )
-	: console.log( 'nav-unification feature: Disabled' );
 
 const Masterbar = ( { children, className } ) => (
 	<header id="header" className={ classNames( 'masterbar', className ) }>
