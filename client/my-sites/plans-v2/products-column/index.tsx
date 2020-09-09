699f96433fa3bae347b9732603c26a17bcc0d9e4
Renzo Canepa <rcanepag@gmail.com>
2020-09-09 18:45:51 -0300

    Create hook to control the list of products

--- client/my-sites/plans-v2/products-column/index.tsx
+++ client/my-sites/plans-v2/products-column/index.tsx
@@ -8,19 +8,12 @@ import { useSelector } from 'react-redux';
 /**
  * Internal dependencies
  */
-import {
-	getAllOptionsFromSlug,
-	slugToSelectorProduct,
-	getJetpackDescriptionWithOptions,
-} from '../utils';
-import { PRODUCTS_TYPES, SELECTOR_PRODUCTS } from '../constants';
+import { getJetpackDescriptionWithOptions } from '../utils';
+import { PRODUCTS_TYPES } from '../constants';
 import ProductCard from '../product-card';
+import useGetPlansGridProducts from '../use-get-plans-grid-products';
 import FormattedHeader from 'components/formatted-header';
-import { getPlan } from 'lib/plans';
-import { JETPACK_PRODUCTS_LIST } from 'lib/products-values/constants';
 import { getCurrentUserCurrencyCode } from 'state/current-user/selectors';
-import getSitePlan from 'state/sites/selectors/get-site-plan';
-import getSiteProducts from 'state/sites/selectors/get-site-products';
 
 /**
  * Type dependencies
@@ -42,60 +35,29 @@ const ProductsColumn = ( {
 }: ProductsColumnType ) => {
 	const currencyCode = useSelector( ( state ) => getCurrentUserCurrencyCode( state ) );
 
-	// Plan
-	const currentPlan =
-		useSelector( ( state ) => getSitePlan( state, siteId ) )?.product_slug || null;
+	const { availableProducts, purchasedProducts, includedInPlanProducts } = useGetPlansGridProducts(
+		siteId
+	);
 
-	const siteProducts = useSelector( ( state ) => getSiteProducts( state, siteId ) ) || [];
-
-	// Features included in plan
-	const includedInPlanProducts: string[] =
-		( currentPlan && getPlan( currentPlan )?.getHiddenFeatures() ) || [];
-
-	// The list of displayed products comes from a concatenation of:
-	// - Owned products from a direct purchase.
-	// - Included as a feature of an owned plan.
-	// - Generic/option product bundles with subtypes.
 	const productObjects: SelectorProduct[] = useMemo( () => {
-		// Owned products (plans are filtered out).
-		const ownedProducts = siteProducts
-			.map( ( { productSlug } ) => productSlug )
-			.filter( ( productSlug ) => JETPACK_PRODUCTS_LIST.includes( productSlug ) )
-			.map( slugToSelectorProduct )
-			.filter( ( product ): product is SelectorProduct => product !== null );
-
-		// Slugs of options associated with owned products. We don't want to show an option
-		// if the user owns a subtype of it.
-		const allOptionsFromOwnedSlugs = ownedProducts.flatMap( ( product ) =>
-			getAllOptionsFromSlug( product.productSlug )
-		);
-
-		// Convert product slugs to ProductSelector types.
-		const otherProducts = [ ...new Set( [ ...includedInPlanProducts, ...SELECTOR_PRODUCTS ] ) ]
-			.map( slugToSelectorProduct )
+		// Products that have not been directly purchased must honor the current filter
+		// selection since they exist in both monthly and yearly version.
+		const filteredProducts = [ ...includedInPlanProducts, ...availableProducts ]
 			// Remove products that don't fit the filters or have invalid data.
-			.filter(
-				( product: SelectorProduct | null ): product is SelectorProduct =>
-					!! product &&
-					duration === product.term &&
-					// Only show card that correspond to the selected filter
-					PRODUCTS_TYPES[ productType ].includes( product.productSlug ) &&
-					// Don't include a generic/option card if the user already owns a subtype.
-					! allOptionsFromOwnedSlugs.includes( product.productSlug ) &&
-					// Don't include a generic/option card if the product is included in the owned plan.
-					! includedInPlanProducts.some( ( includedProduct ) =>
-						product.subtypes.includes( includedProduct )
-					) &&
-					//  Don't show if the user owns the product regardless of the duration.
-					! ownedProducts.find(
-						( ownedProduct ) => ownedProduct.productSlug === product.productSlug
-					)
-			);
-		return ownedProducts.concat( otherProducts ).map( ( product: SelectorProduct ) => ( {
-			...product,
-			description: getJetpackDescriptionWithOptions( product ),
-		} ) );
-	}, [ duration, includedInPlanProducts, siteProducts, productType ] );
+			.filter( ( product ): product is SelectorProduct => !! product && duration === product.term );
+		return (
+			[ ...purchasedProducts, ...filteredProducts ]
+				// Only show cards that correspond to the selected productType filter.
+				.filter(
+					( product ): product is SelectorProduct =>
+						!! product && PRODUCTS_TYPES[ productType ].includes( product.productSlug )
+				)
+				.map( ( product ) => ( {
+					...product,
+					description: getJetpackDescriptionWithOptions( product as SelectorProduct ),
+				} ) )
+		);
+	}, [ duration, availableProducts, includedInPlanProducts, purchasedProducts, productType ] );
 
 	return (
 		<div className="plans-column products-column">
---
+++ client/my-sites/plans-v2/use-get-plans-grid-products.ts
@@ -0,0 +1,83 @@
+/**
+ * External dependencies
+ */
+import { useSelector } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { slugToSelectorProduct } from './utils';
+import { getPlan } from 'lib/plans';
+import {
+	JETPACK_ANTI_SPAM_PRODUCTS,
+	JETPACK_BACKUP_PRODUCTS,
+	JETPACK_SCAN_PRODUCTS,
+	JETPACK_SEARCH_PRODUCTS,
+	JETPACK_PRODUCTS_LIST,
+} from 'lib/products-values/constants';
+import {
+	OPTIONS_JETPACK_BACKUP,
+	OPTIONS_JETPACK_BACKUP_MONTHLY,
+} from 'my-sites/plans-v2/constants';
+import getSitePlan from 'state/sites/selectors/get-site-plan';
+import getSiteProducts from 'state/sites/selectors/get-site-products';
+
+const useSelectorPageProducts = ( siteId: number | null ) => {
+	let availableProducts: string[] = [];
+
+	// Products/features included in the current plan
+	const currentPlan =
+		useSelector( ( state ) => getSitePlan( state, siteId ) )?.product_slug || null;
+	const includedInPlanProducts: string[] =
+		( currentPlan && getPlan( currentPlan )?.getHiddenFeatures() ) || [];
+
+	// Owned products from direct purchases
+	const purchasedProducts =
+		useSelector( ( state ) => getSiteProducts( state, siteId ) )
+			?.map( ( { productSlug } ) => productSlug )
+			.filter( ( productSlug ) => JETPACK_PRODUCTS_LIST.includes( productSlug ) ) ?? [];
+
+	// Directly and indirectly owned products
+	const ownedProducts = [ ...purchasedProducts, ...includedInPlanProducts ];
+
+	// If Jetpack Backup is directly or indirectly owned, continue, otherwise make it available by displaying
+	// the option cards.
+	if (
+		! ownedProducts.some( ( ownedProduct ) => JETPACK_BACKUP_PRODUCTS.includes( ownedProduct ) )
+	) {
+		availableProducts = [
+			...availableProducts,
+			OPTIONS_JETPACK_BACKUP,
+			OPTIONS_JETPACK_BACKUP_MONTHLY,
+		];
+	}
+
+	// If Jetpack Scan is directly or indirectly owned, continue, otherwise make it available.
+	if (
+		! ownedProducts.some( ( ownedProduct ) => JETPACK_SCAN_PRODUCTS.includes( ownedProduct ) )
+	) {
+		availableProducts = [ ...availableProducts, ...JETPACK_SCAN_PRODUCTS ];
+	}
+
+	// If Jetpack Anti-spam is directly or indirectly owned, continue, otherwise make it available.
+	if (
+		! ownedProducts.some( ( ownedProduct ) => JETPACK_ANTI_SPAM_PRODUCTS.includes( ownedProduct ) )
+	) {
+		availableProducts = [ ...availableProducts, ...JETPACK_ANTI_SPAM_PRODUCTS ];
+	}
+
+	// If Jetpack Search is directly or indirectly owned, continue, otherwise make it available.
+	if (
+		! ownedProducts.some( ( ownedProduct ) => JETPACK_SEARCH_PRODUCTS.includes( ownedProduct ) )
+	) {
+		availableProducts = [ ...availableProducts, ...JETPACK_SEARCH_PRODUCTS ];
+	}
+
+	return {
+		availableProducts: availableProducts.map( slugToSelectorProduct ),
+		purchasedProducts: purchasedProducts.map( slugToSelectorProduct ),
+		includedInPlanProducts: includedInPlanProducts.map( slugToSelectorProduct ),
+	};
+};
+
+export default useSelectorPageProducts;
