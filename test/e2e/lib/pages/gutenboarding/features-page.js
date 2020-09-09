b396809294df240fcba0ec1be25466a53213a498
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-09-09 22:31:15 +0300

    Update Gutenboarding e2e test

---
+++ test/e2e/lib/pages/gutenboarding/features-page.js
@@ -0,0 +1,21 @@
+/**
+ * External dependencies
+ */
+import { By } from 'selenium-webdriver';
+
+/**
+ * Internal dependencies
+ */
+import * as driverHelper from '../../driver-helper.js';
+import AsyncBaseContainer from '../../async-base-container';
+
+export default class PlansPage extends AsyncBaseContainer {
+	constructor( driver ) {
+		super( driver, By.css( '.features' ) );
+	}
+
+	async skipStep() {
+		const skipButtonSelector = By.css( '.action-buttons__skip' );
+		return await driverHelper.clickWhenClickable( this.driver, skipButtonSelector );
+	}
+}
