fa3271dd4a6b3b724e5db87a20701989817d8c0b
Dave Smith <getdavemail@gmail.com>
2020-09-09 17:08:12 +0100

    Remove keyedReducer helper as not currently required.

--- client/state/admin-menu/reducer.js
+++ client/state/admin-menu/reducer.js
@@ -2,7 +2,7 @@
  * Internal dependencies
  */
 
-import { keyedReducer, withStorageKey } from 'state/utils';
+import { withStorageKey } from 'state/utils';
 import 'state/data-layer/wpcom/admin-menu';
 import { ADMIN_MENU_RECEIVE } from 'state/action-types';
 
