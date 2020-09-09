68c21440646dd3f7e598025d0e58d80852d5455f
Caleb Bauermeister <cbauerman@gmail.com>
2020-09-09 13:18:45 -0700

    add form

--- client/landing/jetpack-cloud/sections/settings/credentials/form.tsx
+++ client/landing/jetpack-cloud/sections/settings/credentials/form.tsx
@@ -4,12 +4,18 @@
 import { useTranslate } from 'i18n-calypso';
 import React, { FunctionComponent } from 'react';
 
+/**
+ * Internal dependencies
+ */
+import ServerCredentialsForm from 'components/jetpack/server-credentials-form';
+
 const Form: FunctionComponent = () => {
 	const translate = useTranslate();
 
 	return (
 		<div className="credentials__form">
 			<h1>{ translate( 'Provide your SSH, SFTP or FTP server credentials' ) }</h1>
+			<ServerCredentialsForm />
 		</div>
 	);
 };
