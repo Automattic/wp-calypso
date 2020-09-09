10e7c67f46b51bdc06eeb2381d0fa95703f7fa13
Dave Smith <getdavemail@gmail.com>
2020-09-09 13:47:50 +0100

    Add skeleton of data layer and state

---
+++ client/my-sites/sidebar-unified/index.jsx
@@ -0,0 +1,74 @@
+/**
+ * MySitesSidebarUnified
+ *   Renders the Sidebar for "My Sites", except all of the menus and items are
+ *   driven off a WPCom endpoint: /sites/${sideId}/admin-menu, which is loaded
+ *   into state.adminMenu in a data layer.
+ *
+ *    Currently experimental/WIP.
+ **/
+
+/**
+ * External dependencies
+ */
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { getCurrentRoute } from 'state/selectors/get-current-route';
+import { getSelectedSite } from 'state/ui/selectors';
+import { isUnderDomainManagementAll } from 'my-sites/domains/paths';
+import { isUnderEmailManagementAll } from 'my-sites/email/paths';
+import { requestAdminMenu } from '../../state/admin-menu/actions';
+import CurrentSite from 'my-sites/current-site';
+import MySitesSidebarUnifiedItem from './item';
+import MySitesSidebarUnifiedMenu from './menu';
+import Sidebar from 'layout/sidebar';
+
+export const MySitesSidebarUnified = ( { path } ) => {
+	const reduxDispatch = useDispatch();
+	const selectedSite = useSelector( ( state ) => getSelectedSite( state ) );
+	useEffect( () => {
+		if ( selectedSite !== null && selectedSite.ID ) {
+			reduxDispatch( requestAdminMenu( selectedSite.ID ) );
+		}
+	}, [ reduxDispatch, selectedSite ] );
+
+	// Extract this?
+	const menuItems = useSelector( ( state ) => {
+		const thisSelectedSite = getSelectedSite( state );
+		if (
+			thisSelectedSite !== null &&
+			thisSelectedSite.ID &&
+			state.adminMenu != null &&
+			thisSelectedSite.ID in state.adminMenu
+		) {
+			return Object.values( state.adminMenu[ thisSelectedSite.ID ] );
+		}
+		return [];
+	} );
+
+	// Extract this?
+	const isAllDomainsView = useSelector( ( state ) => {
+		const currentRoute = getCurrentRoute( state );
+		return isUnderDomainManagementAll( currentRoute ) || isUnderEmailManagementAll( currentRoute );
+	} );
+
+	//console.log( { menuItems } );
+	return (
+		<Sidebar>
+			<CurrentSite forceAllSitesView={ isAllDomainsView } />
+			{ menuItems.map( ( item, i ) => {
+				if ( 'type' in item && item.type === 'separator' ) {
+					return <hr key={ i } />;
+				}
+				if ( ! ( 'children' in item ) || item.children.length === 0 ) {
+					return <MySitesSidebarUnifiedItem key={ item.slug } path={ path } { ...item } />;
+				}
+				return <MySitesSidebarUnifiedMenu key={ item.slug } path={ path } { ...item } />;
+			} ) }
+		</Sidebar>
+	);
+};
+export default MySitesSidebarUnified;
