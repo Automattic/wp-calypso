8a25f77c3740842cb5dea02a34ab6e221ac9d983
Matthew Reishus <mreishus@users.noreply.github.com>
2020-09-11 13:10:33 -0500

    Ternary operator checking against null -> or operator

--- client/layout/sidebar/custom-icon.jsx
+++ client/layout/sidebar/custom-icon.jsx
@@ -15,11 +15,7 @@ import React from 'react';
 const SidebarCustomIcon = ( props ) => {
 	const { alt, className, ...rest } = props;
 	return (
-		<img
-			alt={ alt != null ? alt : '' }
-			className={ 'sidebar__menu-icon ' + ( className != null ? className : '' ) }
-			{ ...rest }
-		/>
+		<img alt={ alt || '' } className={ 'sidebar__menu-icon ' + ( className || '' ) } { ...rest } />
 	);
 };
 export default SidebarCustomIcon;
