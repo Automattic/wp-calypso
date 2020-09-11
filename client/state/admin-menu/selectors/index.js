040142a777f7210dd1dca850c082e313c9c32c12
Dave Smith <getdavemail@gmail.com>
2020-09-11 16:08:09 +0100

    Add selectors for adminMenu state (#45576)
    * Add selectors (with tests)
    
    * Reduce size of fixture data. Move to fixture dir.
    
    * Make sure it’s possible to have the test fail!

--- client/state/admin-menu/selectors/index.js
+++ client/state/admin-menu/selectors/index.js
@@ -2,3 +2,13 @@
  * Internal dependencies
  */
 import 'state/inline-help/init';
+
+export function getAdminMenu( state, siteId ) {
+	const stateSlice = state?.adminMenu;
+
+	if ( ! stateSlice || ! siteId ) {
+		return null;
+	}
+
+	return state.adminMenu[ siteId ] || null;
+}
