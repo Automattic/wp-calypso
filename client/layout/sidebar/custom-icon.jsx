2bb289e5450b2759d6c7ad343bbe91b46df9b88b
Matthew Reishus <mreishus@users.noreply.github.com>
2020-09-11 12:41:30 -0500

    Add description of SidebarCustomIcon

--- client/layout/sidebar/custom-icon.jsx
+++ client/layout/sidebar/custom-icon.jsx
@@ -1,4 +1,13 @@
 /**
+ * SidebarCustomIcon -
+ *   Renders an <img> tag with props supplied, but adds
+ *   className="sidebar__menu-icon" to the supplied className.
+ *
+ *   Purpose: To display a custom icon in the sidebar when using a
+ *   source other than grid icons or material icons.
+ **/
+
+/**
  * External dependencies
  */
 
