d42452f7918a8b85d2aa30e6072e91cb738bbc73
Payton Swick <payton@foolord.com>
2020-09-11 20:07:45 -0400

    Explicitly import Purchases from tsx file

--- client/my-sites/purchases/controller.js
+++ client/my-sites/purchases/controller.js
@@ -6,7 +6,7 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import Purchases from 'my-sites/purchases/main';
+import Purchases from 'my-sites/purchases/main.tsx';
 
 export const purchases = ( context, next ) => {
 	context.primary = <Purchases />;
