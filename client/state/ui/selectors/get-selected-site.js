cd2734a8f27b4f1b2e90193e15a21fb4997a9fb0
Payton Swick <payton@foolord.com>
2020-09-11 20:10:19 -0400

    Add very basic types to getSelectedSite

--- client/state/ui/selectors/get-selected-site.js
+++ client/state/ui/selectors/get-selected-site.js
@@ -5,10 +5,21 @@ import getSite from 'state/sites/selectors/get-site';
 import getSelectedSiteId from './get-selected-site-id';
 
 /**
+ * @typedef {object} SiteData
+ * @property {number} ID
+ * @property {string} name
+ * @property {string} URL
+ * @property {string} slug
+ * @property {string} domain
+ * @property {string} locale
+ * TODO: fill this out and/or move it to a TS file
+ */
+
+/**
  * Returns the site object for the currently selected site.
  *
  * @param  {object}  state  Global state tree
- * @returns {?object}        Selected site
+ * @returns {SiteData|null}        Selected site
  */
 export default function getSelectedSite( state ) {
 	const siteId = getSelectedSiteId( state );
