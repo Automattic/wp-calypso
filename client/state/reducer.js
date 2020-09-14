a11165a75159312f135ca81d0ca0196777d50c42
Matthew Reishus <mreishus@users.noreply.github.com>
2020-09-14 08:52:52 -0500

    My Sites sidebar: Show items from admin-menu state when 'nav-unification' flag is on (#45531) (#45631)
    * "My sites" sidebar is driven off the state in admin-menu (see: #45495)
    This is an experiment of having both calypso and wp-admin menus driven off a single source of truth
    
    * WIP, But behind a feature flag (nav-unification)
    * "My sites" sidebar acts the same if the feature flag is off
    
    Co-authored-by: Dave Smith <getdavemail@gmail.com>

--- client/state/reducer.js
+++ client/state/reducer.js
@@ -10,7 +10,6 @@
  */
 import { combineReducers } from 'state/utils';
 import { reducer as httpData } from 'state/data-layer/http-data';
-import config from 'config';
 
 /**
  * Reducers
@@ -94,11 +93,5 @@ const reducers = {
 	userSettings,
 	users,
 };
-
-// TODO: remove this once admin-menu state is consumed by UI components
-// to allow it to be loaded on demand as modularized state.
-if ( config.isEnabled( 'nav-unification' ) ) {
-	import( 'state/admin-menu/init' );
-}
 
 export default combineReducers( reducers );
