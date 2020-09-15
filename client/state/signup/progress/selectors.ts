9ca15c182193bd0a1056b8a0d927d80fbe1d7046
Dehan <dehandecroos@gmail.com>
2020-09-15 07:33:14 +0530

    Fixed flows bug by specifying the correct depdendencies Moved selector to correct file

--- client/state/signup/progress/selectors.ts
+++ client/state/signup/progress/selectors.ts
@@ -17,3 +17,18 @@ const initialState: ProgressState = {};
 export function getSignupProgress( state: any ): ProgressState {
 	return get( state, 'signup.progress', initialState );
 }
+
+/**
+ * Returns true if a plans step exists and is skipped in the current signup progress
+ *
+ * @param   {object}  state The current client state
+ * @returns  {boolean} denoting whether the plans step existed AND it was skipped
+ */
+export const isPlanStepExistsAndSkipped = ( state: any ) => {
+	const { signup: { progress = {} } = {} } = state;
+	const planName =
+		Object.keys( progress ).find( ( stepName ) => stepName.includes( 'plans' ) ) ?? '';
+	const plan = progress[ planName ] ?? {};
+	const { wasSkipped = false } = plan;
+	return wasSkipped;
+};
