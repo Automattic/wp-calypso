2104452ed965935656deb0b6be44bd02280f2e5e
Markus Dobmann <m.do@posteo.net>
2020-09-15 12:39:37 +0200

    createTransientMediaId: rename moreSpecifiedPrefix to moreSpecificPrefix

--- client/lib/media/utils/create-transient-media-id.js
+++ client/lib/media/utils/create-transient-media-id.js
@@ -9,10 +9,10 @@ const { uniqueId } = impureLodash;
  * Returns an ID for transient media items. To be consistent in creating
  * transient media IDs they are all prefixed with 'media-'
  *
- * @param {string} moreSpecifiedPrefix can be used to further specify the prefix
+ * @param {string} moreSpecificPrefix can be used to further specify the prefix
  */
-function createTransientMediaId( moreSpecifiedPrefix = '' ) {
-	return uniqueId( `media-${ moreSpecifiedPrefix }` );
+function createTransientMediaId( moreSpecificPrefix = '' ) {
+	return uniqueId( `media-${ moreSpecificPrefix }` );
 }
 
 export default createTransientMediaId;
