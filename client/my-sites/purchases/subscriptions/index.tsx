9f9e95327dc936301e34d5140153239a141ab340
Payton Swick <payton@foolord.com>
2020-09-15 19:17:25 -0400

    Swap out PurchasesHeader for a simple SectionHeader

--- client/my-sites/purchases/subscriptions/index.tsx
+++ client/my-sites/purchases/subscriptions/index.tsx
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal Dependencies
@@ -10,21 +11,23 @@ import { useSelector } from 'react-redux';
 import Main from 'components/main';
 import MeSidebarNavigation from 'me/sidebar-navigation';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
-import PurchasesHeader from '../header';
 import QueryUserPurchases from 'components/data/query-user-purchases';
 import QueryConciergeInitial from 'components/data/query-concierge-initial';
 import { getCurrentUserId } from 'state/current-user/selectors';
 import SubscriptionsContent from './subscriptions-content';
 import AccountLevelPurchaseLinks from './account-level-purchase-links';
+import SectionHeader from 'components/section-header';
 
 export default function Subscriptions() {
 	const userId = useSelector( ( state ) => getCurrentUserId( state ) );
+	const translate = useTranslate();
+
 	return (
 		<Main className="subscriptions">
 			<QueryUserPurchases userId={ userId } />
 			<PageViewTracker path="/purchases/subscriptions" title="Subscriptions" />
 			<MeSidebarNavigation />
-			<PurchasesHeader section="purchases" />
+			<SectionHeader label={ translate( 'Subscriptions' ) } />
 			<SubscriptionsContent />
 			<QueryConciergeInitial />
 			<AccountLevelPurchaseLinks />
