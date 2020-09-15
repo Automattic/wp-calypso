4b00c18dcbc8d13b7417eec5027ee75f89ce90b5
Renzo Canepa <rcanepag@gmail.com>
2020-09-15 11:17:59 -0300

    Open up Offer Reset for everyone (#45642)

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -184,10 +184,10 @@ export default {
 	offerResetFlow: {
 		datestamp: '20200804',
 		variations: {
-			showOfferResetFlow: 0,
-			control: 100,
+			showOfferResetFlow: 100,
+			control: 0,
 		},
-		defaultVariation: 'control',
+		defaultVariation: 'showOfferResetFlow',
 		allowExistingUsers: true,
 	},
 	userlessCheckout: {
