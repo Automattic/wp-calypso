a1e2fb3d32416970374c31591aab5c151e82340c
Payton Swick <payton@foolord.com>
2020-09-16 16:01:00 -0400

    Remove unused paths constants

--- client/my-sites/purchases/paths.ts
+++
@@ -1,7 +0,0 @@
-export const purchasesRoot = '/purchases';
-
-export const subscriptions = purchasesRoot + '/subscriptions';
-
-export const billingHistory = purchasesRoot + '/billing-history';
-
-export const paymentMethods = purchasesRoot + '/payment-methods';
