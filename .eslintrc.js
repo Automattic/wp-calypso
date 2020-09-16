5a942beffdc0826674eb64fcf813cb579e5e243c
Sergio Cinos <sergio.cinos@gmail.com>
2020-09-16 11:02:05 +0200

    Fix bad merge

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,8 +30,6 @@ module.exports = {
 							[ 'lint-no-duplicate-headings', false ],
 
 							// Rules we would like to enable eventually. Violations needs to be fixed manually before enabling the rule.
-							[ 'lint-no-emphasis-as-heading', false ],
-							[ 'lint-code-block-style', false ],
 							[ 'lint-no-multiple-toplevel-headings', false ],
 							[ 'lint-fenced-code-flag', false ],
 
