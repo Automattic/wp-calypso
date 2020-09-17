7b7caf85f4de69a9e2edd4fc9cbfbcec6c6ff4db
fditrapani <filippo.di.trapani@a8c.com>
2020-09-16 20:01:12 -0400

    Add title

--- client/my-sites/purchases/main.tsx
+++ client/my-sites/purchases/main.tsx
@@ -2,16 +2,29 @@
  * External dependencies
  */
 import React from 'react';
+import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import Main from 'components/main';
 import Subscriptions from './subscriptions';
+import DocumentHead from 'components/data/document-head';
+import FormattedHeader from 'components/formatted-header';
 
 export default function Purchases() {
+	const translate = useTranslate();
+
 	return (
 		<Main className="purchases is-wide-layout">
+			<DocumentHead title={ translate( 'Billing' ) } />
+			<FormattedHeader
+				brandFont
+				className="purchases__page-heading"
+				headerText={ translate( 'Billing' ) }
+				align="left"
+			/>
+
 			<Subscriptions />
 		</Main>
 	);
