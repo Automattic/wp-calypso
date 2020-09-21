a7c54eb31e234a497f7fbde26e6aab8ad74e4a47
Dehan <dehandecroos@gmail.com>
2020-09-21 18:22:19 +0530

    Removed deps when not needed and made step optional

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -307,8 +307,7 @@ export function generateSteps( {
 
 		'domain-only': {
 			stepName: 'domain-only',
-			providesDependencies: [ 'siteId', 'siteSlug', 'domainItem', 'shouldHideFreePlan' ],
-			optionalDependencies: [ 'shouldHideFreePlan' ],
+			providesDependencies: [ 'siteId', 'siteSlug', 'domainItem' ],
 			props: {
 				isDomainOnly: true,
 				shouldShowDomainTestCopy: false,
@@ -317,8 +316,7 @@ export function generateSteps( {
 
 		'select-domain': {
 			stepName: 'select-domain',
-			providesDependencies: [ 'siteId', 'siteSlug', 'domainItem', 'shouldHideFreePlan' ],
-			optionalDependencies: [ 'shouldHideFreePlan' ],
+			providesDependencies: [ 'siteId', 'siteSlug', 'domainItem' ],
 			props: {
 				isAllDomains: true,
 				isDomainOnly: true,
