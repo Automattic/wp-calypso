2727401a19616212f72f807065c7baa613b1cf29
Payton Swick <payton@foolord.com>
2020-09-21 16:30:50 -0400

    Add JSDoc for useActOnceOnStrings

--- client/my-sites/checkout/composite-checkout/hooks/use-act-once-on-strings.ts
+++ client/my-sites/checkout/composite-checkout/hooks/use-act-once-on-strings.ts
@@ -3,6 +3,19 @@
  */
 import { useEffect, useRef } from 'react';
 
+/**
+ * Run a callback for strings, but only new ones
+ *
+ * This hook accepts an array of strings and a callback. The callback is called
+ * if any of the strings have not been passed to this hook previously. The
+ * argument to the callback is an array of the strings that have not been
+ * passed before.
+ *
+ * This can be used, for example, to record or display error messages.
+ *
+ * @param {string[]} messages - strings
+ * @param {(messages: string[]) => void} handleMessages - the callback
+ */
 export default function useActOnceOnStrings(
 	messages: string[],
 	handleMessages: ( messages: string[] ) => void
