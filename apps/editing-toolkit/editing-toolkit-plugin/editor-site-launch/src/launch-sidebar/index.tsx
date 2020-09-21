bdfb138c3649ab90b5d09ae96d56aab56ca1b22c
Yan Sern <yan.sern@automattic.com>
2020-09-21 23:02:48 +0200

    Fix "Get started" / "Go back" button abruptly closing modal. Make sidebar consistent with onboarding heading and margins on mobile view.

--- apps/editing-toolkit/editing-toolkit-plugin/editor-site-launch/src/launch-sidebar/index.tsx
+++ apps/editing-toolkit/editing-toolkit-plugin/editor-site-launch/src/launch-sidebar/index.tsx
@@ -4,7 +4,7 @@
 import * as React from 'react';
 import { __ } from '@wordpress/i18n';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { ActionButtons, NextButton } from '@automattic/onboarding';
+import { Title, SubTitle, ActionButtons, NextButton } from '@automattic/onboarding';
 
 /**
  * Internal dependencies
@@ -20,7 +20,6 @@ const LaunchSidebar = () => {
 	const LaunchSequence = useSelect( ( select ) => select( LAUNCH_STORE ).getLaunchSequence() );
 
 	const handleStart = () => {
-		// TODO: Fix modal closing itself.
 		setStep( LaunchSequence[ 0 ] );
 		unsetSidebarFullscreen();
 	};
@@ -31,19 +30,25 @@ const LaunchSidebar = () => {
 
 	return (
 		<div className="nux-launch-sidebar">
-			<h2>{ __( "You're almost there!", 'full-site-editing' ) }</h2>
-			<h3>
-				{ __(
-					'Complete the following steps to launch your site. Your site will remain private until you Launch.',
-					'full-site-editing'
-				) }
-			</h3>
-			<LaunchMenu onMenuItemClick={ handleMenuItemClick } />
-			<ActionButtons stickyBreakpoint="medium">
-				<NextButton onClick={ handleStart }>
-					{ __( 'Get Started', 'full-site-editing' ) }
-				</NextButton>
-			</ActionButtons>
+			<div className="nux-launch-sidebar__header">
+				<Title>{ __( "You're almost there!", 'full-site-editing' ) }</Title>
+				<SubTitle>
+					{ __(
+						'Complete the following steps to launch your site. Your site will remain private until you Launch.',
+						'full-site-editing'
+					) }
+				</SubTitle>
+			</div>
+			<div className="nux-launch-sidebar__body">
+				<LaunchMenu onMenuItemClick={ handleMenuItemClick } />
+			</div>
+			<div className="nux-launch-sidebar__footer">
+				<ActionButtons sticky={ true }>
+					<NextButton onClick={ handleStart }>
+						{ __( 'Get Started', 'full-site-editing' ) }
+					</NextButton>
+				</ActionButtons>
+			</div>
 		</div>
 	);
 };
