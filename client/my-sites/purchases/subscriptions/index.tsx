9424c4918cb05c7db77b2f73145def29c9333ae0
Payton Swick <payton@foolord.com>
2020-09-21 16:36:45 -0400

    Remove QueryConciergeInitial

--- client/my-sites/purchases/subscriptions/index.tsx
+++ client/my-sites/purchases/subscriptions/index.tsx
@@ -12,7 +12,6 @@ import Main from 'components/main';
 import MySitesSidebarNavigation from 'my-sites/sidebar-navigation';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
 import QueryUserPurchases from 'components/data/query-user-purchases';
-import QueryConciergeInitial from 'components/data/query-concierge-initial';
 import { getCurrentUserId } from 'state/current-user/selectors';
 import SubscriptionsContent from './subscriptions-content';
 import AccountLevelPurchaseLinks from './account-level-purchase-links';
@@ -29,7 +28,6 @@ export default function Subscriptions() {
 			<MySitesSidebarNavigation />
 			<SectionHeader label={ translate( 'Subscriptions' ) } />
 			<SubscriptionsContent />
-			<QueryConciergeInitial />
 			<AccountLevelPurchaseLinks />
 		</Main>
 	);
