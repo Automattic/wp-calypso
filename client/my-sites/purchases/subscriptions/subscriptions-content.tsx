e8635f48dbf03b6766c95ed9c0e8ac9b1da3f1a6
Payton Swick <payton@foolord.com>
2020-09-21 17:18:11 -0400

    Clarify logic of SubscriptionsContent

--- client/my-sites/purchases/subscriptions/subscriptions-content.tsx
+++ client/my-sites/purchases/subscriptions/subscriptions-content.tsx
@@ -26,41 +26,24 @@ export default function SubscriptionsContent() {
 	const selectedSiteId = useSelector( ( state ) => getSelectedSiteId( state ) );
 	const selectedSite = useSelector( ( state ) => getSelectedSite( state ) );
 	const purchases = useSelector( ( state ) => getSitePurchases( state, selectedSiteId ) );
-	const translate = useTranslate();
 
+	// If we are loading purchases, show the placeholder
 	if ( isFetchingPurchases && ! hasLoadedPurchases ) {
 		return <PurchasesSite isPlaceholder />;
 	}
 
+	// If there is no selected site, show the "no sites" page
 	if ( ! selectedSiteId ) {
 		return <NoSitesMessage />;
 	}
 
+	// If there is a selected site but no site data, show the placeholder
 	if ( ! selectedSite?.ID ) {
 		return <PurchasesSite isPlaceholder />;
 	}
 
-	if ( hasLoadedPurchases && ! purchases.length ) {
-		return <NoSitesMessage />;
-	}
-
-	if ( hasLoadedPurchases && ! purchases.length ) {
-		return (
-			<CompactCard className="subscriptions__list--empty">
-				<EmptyContent
-					title={ translate( 'Looking to upgrade?' ) }
-					line={ translate(
-						'Our plans give your site the power to thrive. ' + 'Find the plan that works for you.'
-					) }
-					action={ translate( 'Upgrade now' ) }
-					actionURL={ '/plans' }
-					illustration={ '/calypso/images/illustrations/illustration-nosites.svg' }
-				/>
-			</CompactCard>
-		);
-	}
-
-	if ( hasLoadedPurchases && purchases.length && selectedSite?.ID ) {
+	// If there are purchases, show them
+	if ( hasLoadedPurchases && purchases.length ) {
 		return (
 			<PurchasesSite
 				showHeader={ false }
@@ -74,8 +57,14 @@ export default function SubscriptionsContent() {
 		);
 	}
 
+	// If there is selected site data but no purchases, show the "no purchases" page
+	return <NoPurchasesMessage />;
+}
+
+function NoPurchasesMessage() {
+	const translate = useTranslate();
 	return (
-		<CompactCard className="subscriptions__list--no-content">
+		<CompactCard className="subscriptions__list--empty">
 			<EmptyContent
 				title={ translate( 'Looking to upgrade?' ) }
 				line={ translate(
