1231e5e37dca1b49cc695bb78370387660a58c21
Payton Swick <payton@foolord.com>
2020-09-22 14:38:13 -0400

    Fix useActOnceOnStrings: previous messages only included new strings

--- client/my-sites/checkout/composite-checkout/hooks/use-act-once-on-strings.ts
+++ client/my-sites/checkout/composite-checkout/hooks/use-act-once-on-strings.ts
@@ -2,6 +2,9 @@
  * External dependencies
  */
 import { useEffect, useRef } from 'react';
+import debugFactory from 'debug';
+
+const debug = debugFactory( 'calypso:composite-checkout:use-act-once-on-strings' );
 
 /**
  * Run a callback for strings, but only new ones
@@ -26,8 +29,14 @@ export default function useActOnceOnStrings(
 			( message ) => ! previousMessages.current.includes( message )
 		);
 		if ( newMessages.length > 0 ) {
+			debug(
+				'discovered new messages',
+				newMessages,
+				'not in the previous list',
+				previousMessages.current
+			);
 			handleMessages( newMessages );
 		}
-		previousMessages.current = newMessages;
-	}, [ messages, handleMessages ] );
+		previousMessages.current = messages;
+	}, [ messages ] );
 }
