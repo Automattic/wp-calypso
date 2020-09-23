79bfeff3c86f82a40975b2889599a3afa9a4cc66
Payton Swick <payton@automattic.com>
2020-09-23 15:33:16 -0400

    Add warnings when useInitializeCartFromServer is used incorrectly (#45479)
    useInitializeCartFromServer can be provided with two arguments that are
    used for initialization: `productsToAddOnInitialize` and
    `couponToAddOnInitialize`. However, it's expected that neither argument
    will change once the cart has initialized because after that point the
    hook will not use those arguments.
    
    This change adds a console warning if either of those arguments change
    (note that for `productsToAddOnInitialize` this is a referential
    equality check, so it may be incorrect if the array is regenerated).

--- client/my-sites/checkout/composite-checkout/hooks/use-shopping-cart-manager/use-initialize-cart-from-server.ts
+++ client/my-sites/checkout/composite-checkout/hooks/use-shopping-cart-manager/use-initialize-cart-from-server.ts
@@ -31,6 +31,9 @@ export default function useInitializeCartFromServer(
 	onEvent?: ( arg0: ReactStandardAction ) => void
 ): void {
 	const isInitialized = useRef( false );
+	const initializedProducts = useRef< RequestCartProduct[] | null >( null );
+	const initializedCoupon = useRef< string | null >( null );
+
 	useEffect( () => {
 		if ( cacheStatus !== 'fresh' ) {
 			debug( 'not initializing cart; cacheStatus is not fresh' );
@@ -43,10 +46,32 @@ export default function useInitializeCartFromServer(
 
 		if ( isInitialized.current ) {
 			debug( 'not initializing cart; already initialized' );
+			if ( productsToAddOnInitialize !== initializedProducts.current ) {
+				// eslint-disable-next-line no-console
+				console.warn(
+					'The products list provided to useInitializeCartFromServer',
+					productsToAddOnInitialize,
+					'did not match the products list used for initialization',
+					initializedProducts.current,
+					'; they will not be added because the cart has already initialized. productsToAddOnInitialize should not change once canInitializeCart is true!'
+				);
+			}
+			if ( couponToAddOnInitialize !== initializedCoupon.current ) {
+				// eslint-disable-next-line no-console
+				console.warn(
+					'The coupon provided to useInitializeCartFromServer',
+					couponToAddOnInitialize,
+					'did not match the coupon used for initialization',
+					initializedCoupon.current,
+					'; it will not be added because the cart has already initialized. couponToAddOnInitialize should not change once canInitializeCart is true!'
+				);
+			}
 			return;
 		}
 		isInitialized.current = true;
 		debug( `initializing the cart; cacheStatus is ${ cacheStatus }` );
+		initializedProducts.current = productsToAddOnInitialize;
+		initializedCoupon.current = couponToAddOnInitialize;
 
 		getCart()
 			.then( ( response ) => {
