a281972206760bed2907e37ded58d9c4c2be8ce3
Dave Smith <getdavemail@gmail.com>
2020-09-23 14:22:11 +0100

    Update menus reducer export to be keyed
    This enables higher fidelity unit testing as we like to assert on the keyed nature of the reducer.

--- client/state/admin-menu/reducer.js
+++ client/state/admin-menu/reducer.js
@@ -5,14 +5,14 @@ import { withStorageKey, keyedReducer, combineReducers } from 'state/utils';
 import 'state/data-layer/wpcom/sites/admin-menu';
 import { ADMIN_MENU_RECEIVE, ADMIN_MENU_REQUEST } from 'state/action-types';
 
-export const menus = ( state = [], action ) => {
+export const menus = keyedReducer( 'siteId', ( state = [], action ) => {
 	switch ( action.type ) {
 		case ADMIN_MENU_RECEIVE:
 			return [ ...state, ...action.menu ];
 		default:
 			return state;
 	}
-};
+} );
 
 export function requesting( state = false, action ) {
 	switch ( action.type ) {
@@ -25,8 +25,8 @@ export function requesting( state = false, action ) {
 }
 
 const reducer = combineReducers( {
+	menus,
 	requesting,
-	menus: keyedReducer( 'siteId', menus ),
 } );
 
 export default withStorageKey( 'adminMenu', reducer );
