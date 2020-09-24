5376df484fa6fe6c5e26a0ae038472d5ec8f96c3
Sergio Cinos <sergio.cinos@gmail.com>
2020-09-24 10:14:43 +0200

    Write server logs to a file

--- client/server/lib/logger/index.js
+++ client/server/lib/logger/index.js
@@ -6,7 +6,24 @@ import bunyan from 'bunyan';
 let logger;
 
 const createLogger = () => {
-	logger = bunyan.createLogger( { name: 'calypso' } );
+	logger = bunyan.createLogger( {
+		name: 'calypso',
+		streams: [
+			{
+				stream: process.stdout,
+				level: 'info',
+			},
+			{
+				stream: process.stderr,
+				level: 'error',
+			},
+		],
+	} );
+	if ( process.env.CALYPSO_LOGFILE ) {
+		logger.addStream( {
+			path: process.env.CALYPSO_LOGFILE,
+		} );
+	}
 };
 
 export const getLogger = () => {
