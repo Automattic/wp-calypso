0123044f7327bb60c2ee9fd287e4d9522ecb06a2
Rosalind W <blackjacksoftwaredev@gmail.com>
2020-09-24 16:32:54 -0500

    Adding parsing of heading elements to web notifications

--- apps/notifications/src/panel/indices-to-html/index.js
+++ apps/notifications/src/panel/indices-to-html/index.js
@@ -81,6 +81,12 @@ function render_range( new_sub_text, new_sub_range, range_info, range_data, opti
 		case 'strong':
 		case 'em':
 		case 'list':
+		case 'h1':
+		case 'h2':
+		case 'h3':
+		case 'h4':
+		case 'h5':
+		case 'h6':
 			switch ( range_info_type ) {
 				case 'list':
 					range_info_type = 'span';
