5efab4f597319666fd2a2602184ff85c515b0919
Payton Swick <payton@foolord.com>
2020-09-24 18:31:16 -0400

    Move useRecordCheckoutLoaded to own file
    And convert it to TypeScript.

---
+++ client/my-sites/checkout/composite-checkout/types/stored-cards.ts
@@ -0,0 +1,14 @@
+export interface StoredCard {
+	user_id: string;
+	stored_details_id: string;
+	expiry: string;
+	card: string;
+	card_type: string;
+	mp_ref: string;
+	payment_partner: string;
+	name: string;
+	email: string;
+	remember: string;
+	added: string;
+	last_used: string;
+}
