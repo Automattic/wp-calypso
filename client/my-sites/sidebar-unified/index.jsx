21e97231155f4c772a4d4f962ca309738b3188da
Dave Smith <getdavemail@gmail.com>
2020-09-24 14:49:45 +0100

    Render immediately if we have menu data available even if requesting

--- client/my-sites/sidebar-unified/index.jsx
+++ client/my-sites/sidebar-unified/index.jsx
@@ -35,7 +35,13 @@ export const MySitesSidebarUnified = ( { path } ) => {
 	const isAllDomainsView = useDomainsViewStatus();
 	const isRequestingMenu = useSelector( getIsRequestingAdminMenu );
 
-	if ( isRequestingMenu ) {
+	/**
+	 * If there are no menu items and we are currently requesting some,
+	 * then show a spinner. The check for menuItems is necessary because
+	 * we may choose to render the menu from statically stored JSON data
+	 * and therefore we need to be ready to render.
+	 */
+	if ( ! menuItems && isRequestingMenu ) {
 		return <Spinner className="sidebar-unified__menu-loading" />;
 	}
 
