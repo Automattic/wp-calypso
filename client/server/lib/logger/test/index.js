54d495229993f1aaccb2fe59b38fd5c6f9ffcc0d
Sergio Cinos <sergio.cinos@gmail.com>
2020-09-24 14:47:42 +0200

    Write server logs to a file (#45897)
    * Write server logs to a file
    
    * Do not send logs to stderr

--- client/server/lib/logger/test/index.js
+++ client/server/lib/logger/test/index.js
@@ -1,38 +1,78 @@
 /**
- * Internal dependencies
+ * External dependencies
  */
-import { getLogger } from '../index';
-import bunyan from 'bunyan';
+import { mockProcessStdout } from 'jest-mock-process';
+import mockFs from 'mock-fs';
+import fs from 'fs';
 
-/**
- * Mocks `bunyan` so every instance returned by `createLogger` logs to a buffer instead
- * of process.stdout. We can retrieve that buffer with `bunyan.getOutputBuffer()` and make
- * assertions on it.
- */
-jest.mock( 'bunyan', () => {
-	const realBunyan = jest.requireActual( 'bunyan' );
-	const buffer = new realBunyan.RingBuffer( { limit: 100 } );
+let mockStdout;
+let getLogger;
 
-	return {
-		createLogger: jest.fn( ( options ) => {
-			return realBunyan.createLogger( {
-				...options,
-				streams: [ { type: 'raw', stream: buffer } ],
-			} );
-		} ),
-		getOutputBuffer: () => buffer,
-	};
+beforeEach( () => {
+	( { getLogger } = require( '../index' ) );
+	mockStdout = mockProcessStdout();
+	mockFs( {
+		'/tmp': {},
+	} );
+} );
+
+afterEach( () => {
+	jest.resetModules();
+	mockStdout.mockRestore();
+	delete process.env.CALYPSO_LOGFILE;
+	mockFs.restore();
 } );
 
 it( "Returns a logger with the name 'calypso'", () => {
-	const buffer = bunyan.getOutputBuffer();
+	const logger = getLogger();
 
-	getLogger().info( 'message' );
+	logger.info( 'message' );
 
-	expect( buffer.records ).toHaveLength( 1 );
-	expect( buffer.records ).toEqual(
-		expect.arrayContaining( [ expect.objectContaining( { name: 'calypso' } ) ] )
-	);
+	expect( mockStdout ).toHaveBeenCalledTimes( 1 );
+	const loggedMessage = mockStdout.mock.calls[ 0 ][ 0 ];
+	expect( JSON.parse( loggedMessage ) ).toEqual( expect.objectContaining( { name: 'calypso' } ) );
+} );
+
+it( 'Logs info and above levels to stdout', () => {
+	const logger = getLogger();
+
+	logger.trace( 'trace' ); // not logged
+	logger.debug( 'debug' ); // not logged
+	logger.info( 'info' );
+	logger.warn( 'warn' );
+	logger.error( 'error' );
+	logger.fatal( 'fatal' );
+
+	const loggedMessages = mockStdout.mock.calls.map( ( args ) => JSON.parse( args[ 0 ] ) );
+	expect( loggedMessages ).toEqual( [
+		expect.objectContaining( { msg: 'info', level: 30 } ),
+		expect.objectContaining( { msg: 'warn', level: 40 } ),
+		expect.objectContaining( { msg: 'error', level: 50 } ),
+		expect.objectContaining( { msg: 'fatal', level: 60 } ),
+	] );
+} );
+
+it( 'Logs info and above levels to the filesystem when the env variable is present', async () => {
+	process.env.CALYPSO_LOGFILE = '/tmp/calypso.log';
+	const logger = getLogger();
+
+	logger.trace( 'trace' ); // not logged
+	logger.debug( 'debug' ); // not logged
+	logger.info( 'info' );
+	logger.warn( 'warn' );
+	logger.error( 'error' );
+	logger.fatal( 'fatal' );
+
+	const logLines = ( await fs.promises.readFile( '/tmp/calypso.log', 'utf8' ) )
+		.split( '\n' )
+		.filter( ( line ) => line.length > 0 )
+		.map( JSON.parse );
+	expect( logLines ).toEqual( [
+		expect.objectContaining( { msg: 'info', level: 30 } ),
+		expect.objectContaining( { msg: 'warn', level: 40 } ),
+		expect.objectContaining( { msg: 'error', level: 50 } ),
+		expect.objectContaining( { msg: 'fatal', level: 60 } ),
+	] );
 } );
 
 it( 'Reuses the same logger', () => {
