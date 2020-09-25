41ce5b911fd7f985034f151e00a1b2e45382af4b
nbloomf <nathan.bloomfield@automattic.com>
2020-09-25 15:15:45 -0500

    Stub SitePurchases component

--- client/my-sites/purchases/main.tsx
+++ client/my-sites/purchases/main.tsx
@@ -2,15 +2,18 @@
  * External dependencies
  */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import Main from 'components/main';
-import Subscriptions from './subscriptions';
+import { Subscriptions, SiteSubscriptions } from './subscriptions';
 import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
+import getSelectedSiteId from 'state/ui/selectors/get-selected-site-id';
+import getSiteUrl from 'state/sites/selectors/get-site-url';
 
 export function Purchases() {
 	const translate = useTranslate();
@@ -30,6 +33,26 @@ export function Purchases() {
 	);
 }
 
-export function PurchaseSettings() {
-	return <Main></Main>;
+export function SitePurchases() {
+	const siteId = useSelector( getSelectedSiteId );
+	const translate = useTranslate();
+
+	const siteUrl = useSelector( ( state ) => getSiteUrl( state, siteId ) );
+	const siteHostname = new URL( siteUrl ).hostname; // TODO: use the primary domain here if we can
+
+	return (
+		<Main className="purchases is-wide-layout">
+			<DocumentHead
+				title={ translate( 'Billing for %(siteHostname)s', { args: { siteHostname } } ) }
+			/>
+			<FormattedHeader
+				brandFont
+				className="purchases__page-heading"
+				headerText={ translate( 'Billing for %(siteHostname)s', { args: { siteHostname } } ) }
+				align="left"
+			/>
+
+			<SiteSubscriptions siteId={ siteId } />
+		</Main>
+	);
 }
