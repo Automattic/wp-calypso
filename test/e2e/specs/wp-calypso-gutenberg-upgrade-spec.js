64e816dd3f6f25126a46fa149fb4aa63b663c7a2
Bart <bartlomiej.kalisz@gmail.com>
2020-09-25 17:27:17 +0200

    Clean up: fix linter errors, rewrap comments, etc.

--- test/e2e/specs/wp-calypso-gutenberg-upgrade-spec.js
+++ test/e2e/specs/wp-calypso-gutenberg-upgrade-spec.js
@@ -4,9 +4,9 @@
 import assert from 'assert';
 import config from 'config';
 import { times } from 'lodash';
-import { By } from 'selenium-webdriver';
-import { join } from 'path';
-import { promises as fs } from 'fs';
+// import { By } from 'selenium-webdriver';
+// import { join } from 'path';
+// import { promises as fs } from 'fs';
 
 /**
  * Internal dependencies
@@ -59,12 +59,12 @@ const blockInits = new Map()
 	} );
 
 /**
- * Wrapper that provides an uniform API for creating blocks on the page. It uses
- * the `inits` dictionary defined in this module. If an entry is not found for the
- * passed `blockClass`, it fall-backs to just inserting the block on the page.
+ * Wrapper that provides an uniform API for creating blocks on the page. It uses the `inits`
+ * dictionary defined in this module. If an entry is not found for the passed `blockClass`, it
+ * fall-backs to just inserting the block on the page.
  *
- * IMPORTANT: It relies on the `gEditorComponent` having a proper instance of `GutenbergEditorComponent`,
- * so make sure to call it only when this instance is available.
+ * IMPORTANT: It relies on the `gEditorComponent` having a proper instance of
+ * `GutenbergEditorComponent`, so make sure to call it only when this instance is available.
  *
  * @param { typeof GutenbergBlockComponent } blockClass A block class.
  * @returns { Function } the init function to be called.
@@ -76,79 +76,90 @@ async function insertBlock( blockClass ) {
 	return blockInit && blockInit( block );
 }
 
-/**
- * Take screenshot(s) of a given site's page.
- *
- * There's no way I know of to take a full-height screenshot of the page using wedriver. Because of that,
- * we basically scroll down the page in pieces and take screenshot of those areas. In the end we have multiple
- * screenshots that do represent the full page, vertically.
- *
- * @param { string } siteName the name of the WP test site (it will prepended to .wordpress.com)
- * @param { number } totalHeight The total height of the page
- * @param { number } viewportHeight The visible height
- * @param { Function } scrollCb A callback function that will be called to scroll down the page, it should accept an integer that
- * will be used in formula to calculate how much it should scroll.
- */
-async function takeScreenshot( siteName, totalHeight, viewportHeight, scrollCb ) {
-	const now = Date.now() / 1000;
+// Commented out for now because of ENOMEM erros on CI
+// /**
+//  * Take screenshot(s) of a given site's page.
+//  *
+//  * There's no way I know of to take a full-height screenshot of the page using wedriver. Because of
+//  * that, we basically scroll down the page in pieces and take screenshot of those areas. In the end
+//  * we have multiple screenshots that do represent the full page, vertically.
+//  *
+//  * @param { string } siteName the name of the WP test site (it will prepended to .wordpress.com)
+//  * @param { number } totalHeight The total height of the page
+//  * @param { number } viewportHeight The visible height
+//  * @param { Function } scrollCb A callback function that will be called to scroll down the page, it
+//  * should accept an integer that will be used in formula to calculate how much it should scroll.
+//  */
+// async function takeScreenshot( siteName, totalHeight, viewportHeight, scrollCb ) {
+// 	const now = Date.now() / 1000;
 
-	const siteScreenshotsDir = join( mediaHelper.screenshotsDir, siteName );
-	await fs
-		.access( siteScreenshotsDir )
-		.catch( () => fs.mkdir( siteScreenshotsDir, { recursive: true } ) );
+// 	const siteScreenshotsDir = join( mediaHelper.screenshotsDir, siteName );
+// 	await fs
+// 		.access( siteScreenshotsDir )
+// 		.catch( () => fs.mkdir( siteScreenshotsDir, { recursive: true } ) );
 
-	for ( let i = 0; i <= totalHeight / viewportHeight; i++ ) {
-		await scrollCb( i );
+// 	for ( let i = 0; i <= totalHeight / viewportHeight; i++ ) {
+// 		await scrollCb( i );
 
-		const screenshotData = await driver.takeScreenshot();
-		const url = await driver.getCurrentUrl();
-		await mediaHelper.writeScreenshot(
-			screenshotData,
-			() => join( siteName, `${ siteName }-${ i }-${ now }` ),
-			{ url }
-		);
-	}
+// 		const screenshotData = await driver.takeScreenshot();
+// 		const url = await driver.getCurrentUrl();
+// 		await mediaHelper.writeScreenshot(
+// 			screenshotData,
+// 			() => join( siteName, `${ siteName }-${ i }-${ now }` ),
+// 			{ url }
+// 		);
+// 	}
+// }
+
+// Commented out for now because of ENOMEM erros on CI
+// /**
+//  * Take screenshot(s) of the editor. Useful to diagnose glitches that might manifest visually and
+//  * that we still don't verify automatically.
+//  *
+//  * @param { string } siteName the name of the WP test site (it will prepended to .wordpress.com)
+//  */
+// async function takeEditorScreenshots( siteName ) {
+// 	const editorViewport = await driver.findElement(
+// 		By.css( 'div.interface-interface-skeleton__content' )
+// 	);
+
+// 	const editorViewportScrollHeight = await driver.executeScript(
+// 		'return arguments[0].scrollHeight',
+// 		editorViewport
+// 	);
+// 	const editorViewportClientHeight = await driver.executeScript(
+// 		'return arguments[0].clientHeight',
+// 		editorViewport
+// 	);
+
+// 	await takeScreenshot(
+// 		`${ siteName }-editor`,
+// 		editorViewportScrollHeight,
+// 		editorViewportClientHeight,
+// 		( i ) =>
+// 			driver.executeScript(
+// 				`arguments[0].scroll({top: arguments[0].clientHeight*${ i }})`,
+// 				editorViewport
+// 			)
+// 	);
+// }
+
+async function assertNoErrorInEditor() {
+	const errorDisplayed = await gEditorComponent.errorDisplayed();
+	assert.strictEqual( errorDisplayed, false, 'The block errored in the editor!' );
 }
 
-/**
- * Take screenshot(s) of the editor. Useful to diagnose glitches that might manifest visually and that
- * we still don't verify automatically.
- *
- * @param { string } siteName the name of the WP test site (it will prepended to .wordpress.com)
- */
-async function takeEditorScreenshots( siteName ) {
-	const editorViewport = await driver.findElement(
-		By.css( 'div.interface-interface-skeleton__content' )
-	);
-
-	const editorViewportScrollHeight = await driver.executeScript(
-		'return arguments[0].scrollHeight',
-		editorViewport
-	);
-	const editorViewportClientHeight = await driver.executeScript(
-		'return arguments[0].clientHeight',
-		editorViewport
-	);
-
-	await takeScreenshot(
-		`${ siteName }-editor`,
-		editorViewportScrollHeight,
-		editorViewportClientHeight,
-		( i ) =>
-			driver.executeScript(
-				`arguments[0].scroll({top: arguments[0].clientHeight*${ i }})`,
-				editorViewport
-			)
-	);
-}
+// Commented-out for now because of https://github.com/Automattic/jetpack/issues/16514
+// async function assertNoInvalidBlocksInEditor() {
+// 	assert.strictEqual( await gEditorComponent.hasInvalidBlocks(), false, 'The block is invalid!' );
+// }
 
 /**
  * Re-usable collection of steps for verifying blocks in the editor.
  *
  * @param { typeof GutenbergBlockComponent } blockClass A block class.
- * @param { string } siteName the name of the WP test site (it will prepended to .wordpress.com)
  */
-function verifyBlockInEditor( blockClass, siteName ) {
+function verifyBlockInEditor( blockClass ) {
 	step( 'Block is displayed in the editor', async function () {
 		const displayed = await gEditorComponent.blockDisplayedInEditor( blockClass.blockName );
 		assert.strictEqual(
@@ -162,44 +173,31 @@ function verifyBlockInEditor( blockClass, siteName ) {
 		await assertNoErrorInEditor();
 	} );
 
-	/* Commented-out for now because of https://github.com/Automattic/jetpack/issues/16514
-	step( 'Blocks do not invalidate', async function () {
-			await assertNoInvalidBlocksInEditor();
-	} );
-	*/
+	// Commented-out for now because of https://github.com/Automattic/jetpack/issues/16514
+	// step( 'Blocks do not invalidate', async function () {
+	// 		await assertNoInvalidBlocksInEditor();
+	// } );
 
-	/* Commented out for now because of ENOMEM erros on CI
-	step( 'Take screenshots of the block in the editor', async function () {
-		await takeEditorScreenshots( siteName );
-	} );
-	*/
-}
-
-async function assertNoErrorInEditor() {
-	const errorDisplayed = await gEditorComponent.errorDisplayed();
-	assert.strictEqual( errorDisplayed, false, 'The block errored in the editor!' );
-}
-
-async function assertNoInvalidBlocksInEditor() {
-	assert.strictEqual( await gEditorComponent.hasInvalidBlocks(), false, 'The block is invalid!' );
+	// Commented out for now because of ENOMEM erros on CI
+	// step( 'Take screenshots of the block in the editor', async function () {
+	// 	await takeEditorScreenshots( siteName );
+	// } );
 }
 
 /**
  * Re-usable collection of steps for verifying blocks in the frontend/published page.
  *
  * @param { typeof GutenbergBlockComponent } blockClass A block class.
- * @param { string } siteName the name of the WP test site (it will prepended to .wordpress.com)
  */
-function verifyBlockInPublishedPage( blockClass, siteName ) {
+function verifyBlockInPublishedPage( blockClass ) {
 	step( 'Publish page', async function () {
 		await gEditorComponent.publish( { visit: true } );
 	} );
 
-	/* Commented out for now because of ENOMEM errors on CI
-	step( 'Take screenshots of the published page', async function () {
-		await takePublishedScreenshots( siteName );
-	} );
-	*/
+	// Commented out for now because of ENOMEM errors on CI
+	// step( 'Take screenshots of the published page', async function () {
+	// 	await takePublishedScreenshots( siteName );
+	// } );
 
 	/**
 	 * This is a temporary hack for this changeset to skip checking some blocks in the frontend until
@@ -214,23 +212,24 @@ function verifyBlockInPublishedPage( blockClass, siteName ) {
 	}
 }
 
-/**
- * Take screenshot(s) of the published page for given site. Useful to diagnose glitches that might manifest visually and that
- * we still don't verify automatically.
- *
- * @param { string } siteName the name of the WP test site (it will prepended to .wordpress.com)
- */
-async function takePublishedScreenshots( siteName ) {
-	// Give blocks a chance to render and load assets before taking the screenshots
-	await driver.sleep( 2000 );
+// Commented out for now because of ENOMEM erros on CI
+// /**
+//  * Take screenshot(s) of the published page for given site. Useful to diagnose glitches that might
+//  * manifest visually and that we still don't verify automatically.
+//  *
+//  * @param { string } siteName the name of the WP test site (it will prepended to .wordpress.com)
+//  */
+// async function takePublishedScreenshots( siteName ) {
+// 	// Give blocks a chance to render and load assets before taking the screenshots.
+// 	await driver.sleep( 2000 );
 
-	const totalHeight = await driver.executeScript( 'return document.body.offsetHeight' );
-	const windowHeight = await driver.executeScript( 'return window.outerHeight' );
+// 	const totalHeight = await driver.executeScript( 'return document.body.offsetHeight' );
+// 	const windowHeight = await driver.executeScript( 'return window.outerHeight' );
 
-	await takeScreenshot( `${ siteName }-published`, totalHeight, windowHeight, ( i ) =>
-		driver.executeScript( `window.scrollTo(0, window.outerHeight*${ i })` )
-	);
-}
+// 	await takeScreenshot( `${ siteName }-published`, totalHeight, windowHeight, ( i ) =>
+// 		driver.executeScript( `window.scrollTo(0, window.outerHeight*${ i })` )
+// 	);
+// }
 
 before( async function () {
 	this.timeout( startBrowserTimeoutMS );
