f7ca0fbc0adac865807137ad4288ae683421f7b2
Ramon <ramonjd@users.noreply.github.com>
2020-09-25 11:03:41 +1000

    This commit removes the use patterns feature flag and sets the default value of the API query parameter to true. In short, we're turning on the patterns toolkit for Gutenboarding in all environments. This enables translations of starter designs. (#45895)

--- client/landing/gutenboarding/onboarding-block/style-preview/preview.tsx
+++ client/landing/gutenboarding/onboarding-block/style-preview/preview.tsx
@@ -80,7 +80,7 @@ const Preview: React.FunctionComponent< Props > = ( { viewport } ) => {
 						font_headings: selectedFonts.headings,
 						font_base: selectedFonts.base,
 					} ),
-					use_patterns: isEnabled( 'gutenboarding/use-patterns' ),
+					use_patterns: true,
 				} );
 				if ( isEnabled( 'gutenboarding/style-preview-verticals' ) ) {
 					url = addQueryArgs( url, {
