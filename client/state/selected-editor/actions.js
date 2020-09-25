98a43e7dd4726de2a9ae5bb980d88a35a96a50b5
Allison Levine <allison.levine@automattic.com>
2020-09-25 16:33:26 -0400

    Remove requestSelectedEditor action

--- client/state/selected-editor/actions.js
+++ client/state/selected-editor/actions.js
@@ -5,13 +5,8 @@
 /**
  * Internal dependencies
  */
-import { EDITOR_TYPE_REQUEST, EDITOR_TYPE_UPDATE } from 'state/action-types';
+import { EDITOR_TYPE_UPDATE } from 'state/action-types';
 import 'state/data-layer/wpcom/sites/gutenberg';
-
-export const requestSelectedEditor = ( siteId ) => ( {
-	type: EDITOR_TYPE_REQUEST,
-	siteId,
-} );
 
 export const setSelectedEditor = ( siteId, editor, redirectUrl ) => ( {
 	type: EDITOR_TYPE_UPDATE,
