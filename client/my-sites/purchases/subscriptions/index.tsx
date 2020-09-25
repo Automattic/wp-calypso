93b209b3407b0e206c81af674f9d22ed8b324a2d
nbloomf <nathan.bloomfield@automattic.com>
2020-09-25 15:16:30 -0500

    Add SiteSubscriptions component

--- client/my-sites/purchases/subscriptions/index.tsx
+++ client/my-sites/purchases/subscriptions/index.tsx
@@ -12,12 +12,14 @@ import Main from 'components/main';
 import MySitesSidebarNavigation from 'my-sites/sidebar-navigation';
 import PageViewTracker from 'lib/analytics/page-view-tracker';
 import QueryUserPurchases from 'components/data/query-user-purchases';
+import QuerySitePurchases from 'components/data/query-site-purchases';
 import { getCurrentUserId } from 'state/current-user/selectors';
 import SubscriptionsContent from './subscriptions-content';
 import AccountLevelPurchaseLinks from './account-level-purchase-links';
+import SiteLevelPurchaseLinks from './site-level-purchase-links';
 import SectionHeader from 'components/section-header';
 
-export default function Subscriptions() {
+export function Subscriptions() {
 	const userId = useSelector( ( state ) => getCurrentUserId( state ) );
 	const translate = useTranslate();
 
@@ -29,6 +31,21 @@ export default function Subscriptions() {
 			<SectionHeader label={ translate( 'Subscriptions' ) } />
 			<SubscriptionsContent />
 			<AccountLevelPurchaseLinks />
+		</Main>
+	);
+}
+
+export function SiteSubscriptions( { siteId } ) {
+	const translate = useTranslate();
+
+	return (
+		<Main className="subscriptions is-wide-layout">
+			<QuerySitePurchases siteId={ siteId } />
+			<PageViewTracker path="/purchases/subscriptions" title="Subscriptions" />
+			<MySitesSidebarNavigation />
+			<SectionHeader label={ translate( 'Subscriptions' ) } />
+			<SubscriptionsContent />
+			<SiteLevelPurchaseLinks />
 		</Main>
 	);
 }
