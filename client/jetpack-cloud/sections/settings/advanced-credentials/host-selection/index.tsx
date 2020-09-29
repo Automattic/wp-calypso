bb2bcc27031a9de2bb0533bcb8af2ed8f6df3e8a
Caleb Bauermeister <cbauerman@gmail.com>
2020-09-29 15:38:29 -0700

    further refinements

--- client/jetpack-cloud/sections/settings/advanced-credentials/host-selection/index.tsx
+++ client/jetpack-cloud/sections/settings/advanced-credentials/host-selection/index.tsx
@@ -3,7 +3,7 @@
  */
 import { useSelector } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
-import React, { FunctionComponent, useEffect, useMemo, SetStateAction } from 'react';
+import React, { FunctionComponent, useEffect, useMemo } from 'react';
 
 /**
  * Internal dependencies
@@ -13,6 +13,7 @@ import { getProviderNameFromId, hosts } from '../../utils';
 import { getRequestHosingProviderGuessId, requestHosingProviderGuess } from 'state/data-getters';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
 import Badge from 'components/badge';
+import Gridicon from 'components/gridicon';
 
 /**
  * Style dependencies
@@ -60,36 +61,37 @@ const HostSelection: FunctionComponent< Props > = ( { onHostChange } ) => {
 
 	return (
 		<div className="host-selection">
-			<div className="host-selection__notice">
-				{ translate(
-					'In order to restore your site, should something go wrong, you’ll need to provide your website’s {{strong}}SSH{{/strong}}, {{strong}}SFTP{{/strong}} or {{strong}}FTP{{/strong}} server credentials. We’ll guide you through it:',
-					{
-						components: { strong: <strong /> },
-					}
+			<div className="host-selection__header">
+				<div className="host-selection__notice">
+					{ translate(
+						'In order to restore your site, should something go wrong, you’ll need to provide your website’s {{strong}}SSH{{/strong}}, {{strong}}SFTP{{/strong}} or {{strong}}FTP{{/strong}} server credentials. We’ll guide you through it:',
+						{
+							components: { strong: <strong /> },
+						}
+					) }
+				</div>
+				<h3>
+					{ translate( 'Select your website host for %(siteSlug)s', {
+						args: {
+							siteSlug,
+						},
+					} ) }
+				</h3>
+				{ providerGuessName && (
+					<p>
+						{ translate( 'It looks like your host is %(providerGuessName)s', {
+							args: { providerGuessName },
+						} ) }
+					</p>
 				) }
 			</div>
-			<h3>
-				{ translate( 'Select your website host for %(siteSlug)s', {
-					args: {
-						siteSlug,
-					},
-				} ) }
-			</h3>
-			{ providerGuessName && (
-				<p>
-					{ translate( 'It looks like your host is %(providerGuessName)s', {
-						args: { providerGuessName },
-					} ) }
-				</p>
-			) }
-
 			<div className="host-selection__list">
 				{ hostsToShow.map( ( { id, name } ) => (
-					<div
+					<button
 						className={
 							loadingProviderGuess
-								? 'host-selection__list-item'
-								: 'host-selection__list-item-placeholder'
+								? 'host-selection__list-item-placeholder'
+								: 'host-selection__list-item'
 						}
 						key={ id }
 						onClick={ () => onHostChange( id ) }
@@ -100,13 +102,14 @@ const HostSelection: FunctionComponent< Props > = ( { onHostChange } ) => {
 							<Badge>{ translate( 'If we had to guess your host, this would be it' ) }</Badge>
 						) }
 						{ /* </div> */ }
-					</div>
+						<Gridicon icon="chevron-right" />
+					</button>
 				) ) }
-				<div
+				<button
 					className={
 						loadingProviderGuess
-							? 'host-selection__list-item'
-							: 'host-selection__list-item-placeholder'
+							? 'host-selection__list-item-placeholder'
+							: 'host-selection__list-item'
 					}
 					key={ 'unknown' }
 					onClick={ () => onHostChange( 'unknown' ) }
@@ -114,7 +117,8 @@ const HostSelection: FunctionComponent< Props > = ( { onHostChange } ) => {
 					{ translate(
 						'I don’t know / my host is not listed here / I have my server credentials'
 					) }
-				</div>
+					<Gridicon icon="chevron-right" />
+				</button>
 			</div>
 		</div>
 	);
