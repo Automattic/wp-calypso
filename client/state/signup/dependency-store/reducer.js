01d8fda19bf1ee8227d8446114e0e36d073f5f19
Dehan <dehandecroos@gmail.com>
2020-09-29 18:55:49 +0530

    Revert "Reducer change to clear current step's dependencies when they are empty, but leave other dependencies in the flow intact"
    This reverts commit ca245264dd1076f567b6ac73105996f485cca7d9.

--- client/state/signup/dependency-store/reducer.js
+++ client/state/signup/dependency-store/reducer.js
@@ -9,23 +9,8 @@ import {
 } from 'state/action-types';
 import { dependencyStoreSchema } from './schema';
 import { withSchemaValidation } from 'state/utils';
-import steps from 'signup/config/steps-pure';
+
 const EMPTY = {};
-
-function getStateDependenciesNotInStep( state, stepName ) {
-	const { providesDependencies: currentStepDependencySchemaArray = [] } = steps[ stepName ] ?? {};
-
-	const otherDependenciesInFlow = Object.keys( state ).reduce( ( otherDeps, dep ) => {
-		if ( ! currentStepDependencySchemaArray.includes( dep ) ) {
-			return {
-				...otherDeps,
-				[ dep ]: state[ dep ],
-			};
-		}
-		return otherDeps;
-	}, {} );
-	return otherDependenciesInFlow;
-}
 
 function reducer( state = EMPTY, action ) {
 	switch ( action.type ) {
@@ -34,15 +19,11 @@ function reducer( state = EMPTY, action ) {
 
 		case SIGNUP_PROGRESS_SUBMIT_STEP:
 		case SIGNUP_PROGRESS_COMPLETE_STEP: {
-			const { stepName, providedDependencies: dependenciesProvidedByCurrentStep } = action.step;
-			/**
-			 * This is to allow the removal of a given dependency which is supposed to be provided by the current step
-			 * Therefore, optional dependencies in a given step can now be removed when moving back and forth across a given flow
-			 * The assumption is if a step provides a dependency it should be specified in the action, otherwise it is removed.
-			 */
-			const otherDependenciesInFlow = getStateDependenciesNotInStep( state, stepName );
-			const finalState = { ...otherDependenciesInFlow, ...dependenciesProvidedByCurrentStep };
-			return finalState;
+			const { providedDependencies } = action.step;
+			if ( ! providedDependencies ) {
+				return state;
+			}
+			return { ...state, ...providedDependencies };
 		}
 
 		case SIGNUP_COMPLETE_RESET:
