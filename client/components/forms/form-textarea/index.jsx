7dda41b73a7b2c7643d00d64666dc48cc8bd5e09
Marin Atanasov <tyxla@abv.bg>
2020-10-01 13:51:12 +0300

    Components: Use FormTextarea in TextareaAutosize

--- client/components/forms/form-textarea/index.jsx
+++ client/components/forms/form-textarea/index.jsx
@@ -4,13 +4,14 @@
 import React from 'react';
 import classnames from 'classnames';
 
-const FormTextarea = ( { className, isError, isValid, children, ...otherProps } ) => (
+const FormTextarea = ( { className, isError, isValid, children, forwardedRef, ...otherProps } ) => (
 	<textarea
 		{ ...otherProps }
 		className={ classnames( className, 'form-textarea', {
 			'is-error': isError,
 			'is-valid': isValid,
 		} ) }
+		ref={ forwardedRef }
 	>
 		{ children }
 	</textarea>
