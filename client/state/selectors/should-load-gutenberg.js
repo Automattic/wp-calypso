4fd407ed908786c9a642c4873a0dd789910d626c
Allison Levine <allison.levine@automattic.com>
2020-10-02 15:21:32 -0400

    Remove shouldLoadGutenberg and always show block editor publish confirmation.

--- client/state/selectors/should-load-gutenberg.js
+++
@@ -1,8 +0,0 @@
-export const shouldLoadGutenberg = () => {
-	/* Now the Calypso editor is deprecated we should always load Gutenberg.
-	 * If we need to redirect to wp-admin, that is handled as Gutenberg is loaded.
-	 */
-	return true;
-};
-
-export default shouldLoadGutenberg;
