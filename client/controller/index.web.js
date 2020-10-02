d1ce04dd81013f987e33c31627d6e23893fa72b9
Jarda Snajdr <jsnajdr@gmail.com>
2020-10-02 09:53:55 +0200

    Eliminate moment locales from server build (#46082)
    * Move MomentProvider to a separate module
    
    * Server build: ignore moment locale modules

--- client/controller/index.web.js
+++ client/controller/index.web.js
@@ -14,7 +14,7 @@ import Layout from 'layout';
 import LayoutLoggedOut from 'layout/logged-out';
 import EmptyContent from 'components/empty-content';
 import CalypsoI18nProvider from 'components/calypso-i18n-provider';
-import { MomentProvider } from 'components/localized-moment/context';
+import MomentProvider from 'components/localized-moment/provider';
 import { login } from 'lib/paths';
 import { makeLayoutMiddleware } from './shared.js';
 import { isUserLoggedIn } from 'state/current-user/selectors';
