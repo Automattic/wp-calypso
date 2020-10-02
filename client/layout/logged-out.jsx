27a64f4fb55512772c56cc26d66b8dd82ecff5ec
Marin Atanasov <tyxla@abv.bg>
2020-10-02 23:33:13 +0300

    Layout: AsyncLoad GdprBanner in LayoutLoggedOut

--- client/layout/logged-out.jsx
+++ client/layout/logged-out.jsx
@@ -22,7 +22,6 @@ import getCurrentQueryArguments from 'state/selectors/get-current-query-argument
 import getInitialQueryArguments from 'state/selectors/get-initial-query-arguments';
 import { getSection, masterbarIsVisible } from 'state/ui/selectors';
 import BodySectionCssClass from './body-section-css-class';
-import GdprBanner from 'blocks/gdpr-banner';
 import wooDnaConfig from 'jetpack-connect/woo-dna-config';
 
 /**
@@ -120,7 +119,9 @@ const LayoutLoggedOut = ( {
 					{ secondary }
 				</div>
 			</div>
-			{ config.isEnabled( 'gdpr-banner' ) && <GdprBanner /> }
+			{ config.isEnabled( 'gdpr-banner' ) && (
+				<AsyncLoad require="blocks/gdpr-banner" placeholder={ null } />
+			) }
 		</div>
 	);
 };
