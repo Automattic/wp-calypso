4333751f9e103ca626cf0c1b04cb94ea7da6b0eb
Marin Atanasov <tyxla@abv.bg>
2020-10-05 14:47:24 +0300

    Post Editor: Use FormLabel in EditorDrawerLabel

--- client/post-editor/editor-drawer/label.jsx
+++ client/post-editor/editor-drawer/label.jsx
@@ -8,6 +8,7 @@ import React from 'react';
 /**
  * Internal dependencies
  */
+import FormLabel from 'components/forms/form-label';
 import InfoPopover from 'components/info-popover';
 
 /**
@@ -17,13 +18,13 @@ import './label.scss';
 
 export default function EditorDrawerLabel( { children, labelText, helpText } ) {
 	return (
-		<label className="editor-drawer__label">
+		<FormLabel className="editor-drawer__label">
 			<span className="editor-drawer__label-text">
 				{ labelText }
 				{ helpText && <InfoPopover position="top left">{ helpText }</InfoPopover> }
 			</span>
 			{ children }
-		</label>
+		</FormLabel>
 	);
 }
 
