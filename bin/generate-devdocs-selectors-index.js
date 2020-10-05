10409fa227d1a15475044e25c70673d3656b64d4
Jarda Snajdr <jsnajdr@gmail.com>
2020-10-05 08:33:22 +0200

    Devdocs: extract JSDoc info from selector sources at build time
    Instead of reading all source files at runtime and building an index from them,
    loop through the selectors source files at build time, extract JSDoc info,
    and write a JSON file. At runtime, we only load the JSON file to a fuse.js
    instance.

---
+++ bin/generate-devdocs-selectors-index.js
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+const fs = require( 'fs' );
+const path = require( 'path' );
+const doctrine = require( 'doctrine' );
+const { camelCase, forEach } = require( 'lodash' );
+
+const REGEXP_DOCBLOCKS = /\/\*\* *\n( *\*.*\n)* *\*\//g;
+const SELECTORS_DIR = 'client/state/selectors';
+const DEST_FILE = 'build/devdocs-selectors-index.json';
+
+// Omit index, system files, and subdirectories
+const files = fs
+	.readdirSync( SELECTORS_DIR )
+	.filter( ( file ) => file !== 'index.js' && file.endsWith( '.js' ) );
+
+const selectors = files.map( ( file ) => {
+	const selector = { name: camelCase( path.basename( file, '.js' ) ) };
+
+	const contents = fs.readFileSync( path.join( SELECTORS_DIR, file ), 'utf8' );
+	forEach( contents.match( REGEXP_DOCBLOCKS ), ( docblock ) => {
+		const doc = doctrine.parse( docblock, { unwrap: true } );
+		if ( doc.tags.length > 0 ) {
+			Object.assign( selector, doc );
+			return false;
+		}
+	} );
+
+	return selector;
+} );
+
+selectors.sort( ( a, b ) => a.name > b.name );
+
+fs.writeFileSync( DEST_FILE, JSON.stringify( selectors, null, 2 ) );
