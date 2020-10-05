996fb1babf686ef1294c9fa56df705641a1b9f51
Sergio Cinos <sergio.cinos@gmail.com>
2020-10-05 12:08:46 +0200

    Clean up logs

--- client/server/middleware/test/logger.js
+++ client/server/middleware/test/logger.js
@@ -7,6 +7,7 @@ import EventEmitter from 'events';
  * Internal dependencies
  */
 import loggerMiddleware from '../logger';
+import config from 'config';
 
 const mockLogger = {
 	info: jest.fn(),
@@ -14,20 +15,19 @@ const mockLogger = {
 jest.mock( 'server/lib/logger', () => ( {
 	getLogger: () => mockLogger,
 } ) );
+jest.mock( 'config', () => jest.fn() );
 
-const withRequest = ( { method, url, ip, httpVersionMajor, httpVersionMinor, headers } ) => {
+const fakeRequest = ( { method, url, ip, httpVersion, headers = {} } = {} ) => {
 	const req = new EventEmitter();
 	req.method = method;
 	req.originalUrl = url;
 	req.ip = ip;
 	req.get = ( header ) => headers[ header.toLowerCase() ];
-
-	req.httpVersionMajor = httpVersionMajor;
-	req.httpVersionMinor = httpVersionMinor;
+	req.httpVersion = httpVersion;
 	return req;
 };
 
-const withResponse = ( { statusCode, headers } ) => {
+const fakeResponse = ( { statusCode, headers = {} } = {} ) => {
 	const res = new EventEmitter();
 	res.headersSent = true;
 	res.get = ( header ) => headers[ header.toLowerCase() ];
@@ -35,13 +35,29 @@ const withResponse = ( { statusCode, headers } ) => {
 	return res;
 };
 
-const withNodeEnv = ( env ) => {
-	const current = process.env.NODE_ENV;
-	process.env.NODE_ENV = env;
+const withCommitSha = ( sha ) => {
+	const current = process.env.COMMIT_SHA;
+	process.env.COMMIT_SHA = sha;
 
 	afterAll( () => {
-		process.env.NODE_ENV = current;
+		process.env.COMMIT_SHA = current;
 	} );
+};
+
+const withEnv = ( env ) => {
+	config.mockImplementation( ( key ) => {
+		if ( key === 'env_id' ) return env;
+	} );
+
+	afterAll( () => {
+		config.reset();
+	} );
+};
+
+const simulateRequest = ( { req, res, delay } ) => {
+	loggerMiddleware()( req, res, () => {} );
+	jest.advanceTimersByTime( delay );
+	res.emit( 'finish' );
 };
 
 beforeEach( () => {
@@ -54,75 +70,78 @@ afterEach( () => {
 } );
 
 it( 'Adds a `logger` property to the request', () => {
-	const req = withRequest( {} );
-	const res = withResponse( {} );
-	const next = () => {};
+	const req = fakeRequest();
 
-	loggerMiddleware()( req, res, next );
+	simulateRequest( {
+		req,
+		res: fakeResponse(),
+	} );
 
 	expect( req.logger ).toBe( mockLogger );
 } );
 
-it( 'When the response ends, it logs info about the request in dev mode', () => {
-	withNodeEnv( 'development' );
-	const req = withRequest( {
-		method: 'GET',
-		url: '/example.html',
-	} );
-	const res = withResponse( {
-		statusCode: '200',
-		headers: {
-			'content-length': 123,
-		},
-	} );
+it( 'It logs info about the request', () => {
+	withEnv( 'production' );
 
-	loggerMiddleware()( req, res, () => {} );
-	jest.advanceTimersByTime( 100 );
-	res.emit( 'finish' );
+	simulateRequest( {
+		req: fakeRequest( {
+			method: 'GET',
+			httpVersion: '2.0',
+			headers: {
+				'user-agent':
+					'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+			},
+			url: '/example.html',
+		} ),
+		res: fakeResponse( {
+			statusCode: '200',
+			headers: {
+				'content-length': 123,
+			},
+		} ),
+		delay: 100,
+	} );
 
 	expect( mockLogger.info ).toHaveBeenCalledWith( {
 		length: 123,
 		duration: 100,
 		status: '200',
 		method: 'GET',
+		env: 'production',
 		url: '/example.html',
+		httpVersion: '2.0',
+		userAgent: 'Chrome 85',
 	} );
 } );
 
-it( 'When the response ends, it logs info about the request in production mode', () => {
-	withNodeEnv( 'production' );
-	const req = withRequest( {
-		method: 'GET',
-		url: '/example.html',
-		ip: '127.0.0.1',
-		user: 'foo',
-		httpVersionMajor: 2,
-		httpVersionMinor: 0,
-		headers: {
-			referer: 'http://wordpress.com',
-			'user-agent': 'Chrome',
-		},
-	} );
-	const res = withResponse( {
-		statusCode: '200',
-		headers: {
-			'content-length': 123,
-		},
+it( "Logs raw UserAgent if it can't be parsed", () => {
+	simulateRequest( {
+		req: fakeRequest( {
+			headers: {
+				'user-agent': 'A random browser',
+			},
+		} ),
+		res: fakeResponse(),
 	} );
 
-	loggerMiddleware()( req, res, () => {} );
-	jest.advanceTimersByTime( 100 );
-	res.emit( 'finish' );
+	expect( mockLogger.info ).toHaveBeenCalledWith(
+		expect.objectContaining( {
+			userAgent: 'A random browser',
+		} )
+	);
+} );
 
-	expect( mockLogger.info ).toHaveBeenCalledWith( {
-		length: 123,
-		duration: 100,
-		status: '200',
-		method: 'GET',
-		url: '/example.html',
-		remoteAddr: '127.0.0.1',
-		httpVersion: '2.0',
-		referrer: 'http://wordpress.com',
-		userAgent: 'Chrome',
+it( 'Adds the COMMIT_SHA as version', () => {
+	withCommitSha( 'abcd1234' );
+
+	simulateRequest( {
+		req: fakeRequest(),
+		res: fakeResponse(),
 	} );
+
+	expect( mockLogger.info ).toHaveBeenCalledWith(
+		expect.objectContaining( {
+			version: 'abcd1234',
+		} )
+	);
 } );
