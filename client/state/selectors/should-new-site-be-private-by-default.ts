95e350ea9b53c62eaa0652ec8c2bdc0d30a6d6a0
Louis Laugesen <louis.laugesen@gmail.com>
2020-10-06 17:01:50 +1100

    Delete no longer used constant functions

--- client/state/selectors/should-new-site-be-private-by-default.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Should the site be private by default
- *
- * @returns `true` for private by default & `false` for not
- */
-export default function shouldNewSiteBePrivateByDefault(): boolean {
-	return true;
-}
