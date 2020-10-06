ef8adf346b3fffe5e3800599472b0b13f98c07f2
Sergio Cinos <sergio.cinos@gmail.com>
2020-10-06 06:19:23 +0200

    Drop bunuan field hostname

--- client/server/lib/logger/index.js
+++ client/server/lib/logger/index.js
@@ -1,7 +1,50 @@
+/* eslint jsdoc/no-undefined-types:["error", {definedTypes: ["stream"]}] */
+
 /**
  * External dependencies
  */
 import bunyan from 'bunyan';
+import fs from 'fs';
+import { Transform } from 'stream';
+import { EOL } from 'os';
+
+/**
+ * Creates a stream that filters out some fields.
+ *
+ * This is required because `bunyan` does not implement an option to disable core fields.
+ * We want to disable `hostname` because our logging system already adds `host` with the same
+ * info.
+ *
+ * @param {string[]} fields List of fields to filter out
+ * @param {stream.Writable} destination Stream to write the filtered fields to
+ */
+const filteredStream = ( fields, destination ) => {
+	const transformStream = new Transform( {
+		// Set object mode to receive unserialized JSON objects
+		objectMode: true,
+		transform( data, encoding, callback ) {
+			// Make a copye of `data`, excluding any field in `fields`
+			const filteredData = Object.entries( data )
+				.filter( ( [ key ] ) => ! fields.includes( key ) )
+				.reduce( ( record, [ key, value ] ) => {
+					record[ key ] = value;
+					return record;
+				}, {} );
+
+			if ( destination.writableObjectMode ) {
+				callback( null, transformStream );
+			} else {
+				try {
+					callback( null, JSON.stringify( filteredData ) + EOL );
+				} catch ( err ) {
+					callback( err );
+				}
+			}
+		},
+	} );
+	transformStream.pipe( destination );
+	return transformStream;
+};
 
 let logger;
 
@@ -10,15 +53,20 @@ const createLogger = () => {
 		name: 'calypso',
 		streams: [
 			{
-				stream: process.stdout,
+				type: 'raw',
 				level: 'info',
+				stream: filteredStream( [ 'hostname' ], process.stdout ),
 			},
 		],
 	} );
 	if ( process.env.CALYPSO_LOGFILE ) {
 		logger.addStream( {
-			path: process.env.CALYPSO_LOGFILE,
+			type: 'raw',
 			level: 'info',
+			stream: filteredStream(
+				[ 'hostname' ],
+				fs.createWriteStream( process.env.CALYPSO_LOGFILE, { flags: 'a', encoding: 'utf8' } )
+			),
 		} );
 	}
 };
