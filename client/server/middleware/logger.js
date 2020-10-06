2dba733d33daa0f3a0046c902b89ae05845c72c4
Sergio Cinos <sergio.cinos@gmail.com>
2020-10-06 14:20:27 +0200

    Fix/cleanup logs (#46148)
    * Adds ua-parser-js dependency
    
    * Clean up logs
    
    * Add fields `rawUserAgent`, `remoteAddr` and `referrer`

--- client/server/middleware/logger.js
+++ client/server/middleware/logger.js
@@ -1,14 +1,29 @@
 /**
+ * External dependencies
+ */
+import uaParser from 'ua-parser-js';
+
+/**
  * Internal dependencies
  */
 import { getLogger } from 'server/lib/logger';
+import config from 'config';
 
 const NS_TO_MS = 1e-6;
 
-const logRequest = ( req, res, options ) => {
-	const { requestStart } = options;
+const parseUA = ( rawUA ) => {
+	const parsedUA = uaParser( rawUA );
+	if ( parsedUA.browser.name && parsedUA.browser.version ) {
+		const version = parsedUA.browser.version.split( '.' )[ 0 ];
+		return `${ parsedUA.browser.name } ${ version }`;
+	}
+	return rawUA;
+};
 
-	let fields = {
+const logRequest = ( req, res, options ) => {
+	const { requestStart, env, version } = options;
+
+	const fields = {
 		method: req.method,
 		status: res.statusCode,
 		length: res.get( 'content-length' ),
@@ -16,28 +31,27 @@ const logRequest = ( req, res, options ) => {
 		duration: Number(
 			( Number( process.hrtime.bigint() - requestStart ) * NS_TO_MS ).toFixed( 3 )
 		),
+		httpVersion: req.httpVersion,
+		version,
+		env,
+		userAgent: parseUA( req.get( 'user-agent' ) ),
+		rawUserAgent: req.get( 'user-agent' ),
+		remoteAddr: req.ip,
+		referrer: req.get( 'referer' ),
 	};
-
-	if ( process.env.NODE_ENV === 'production' ) {
-		// Standard Apache combined log output minus the remote-user
-		fields = {
-			...fields,
-			remoteAddr: req.ip,
-			httpVersion: req.httpVersionMajor + '.' + req.httpVersionMinor,
-			referrer: req.get( 'referer' ),
-			userAgent: req.get( 'user-agent' ),
-		};
-	}
 
 	req.logger.info( fields );
 };
 
 export default () => {
 	const logger = getLogger();
+	const env = config( 'env_id' );
+	const version = process.env.COMMIT_SHA;
+
 	return ( req, res, next ) => {
 		req.logger = logger;
 		const requestStart = process.hrtime.bigint();
-		res.on( 'finish', () => logRequest( req, res, { requestStart } ) );
+		res.on( 'finish', () => logRequest( req, res, { requestStart, env, version } ) );
 		next();
 	};
 };
