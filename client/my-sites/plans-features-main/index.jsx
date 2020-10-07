f04a91933f4e84dc271a8fa55df0a2f5bd82fd22
lamosty <lamos.rasto@gmail.com>
2020-10-07 11:25:44 +0200

    Remove hardcoded P2+ plan from PlansFeaturesMain

--- client/my-sites/plans-features-main/index.jsx
+++ client/my-sites/plans-features-main/index.jsx
@@ -29,7 +29,6 @@ import {
 	GROUP_WPCOM,
 	GROUP_JETPACK,
 	PLAN_PERSONAL,
-	PLAN_P2_PLUS,
 } from 'lib/plans/constants';
 import { JETPACK_PRODUCTS_LIST, JETPACK_PRODUCT_PRICE_MATRIX } from 'lib/products-values/constants';
 import { getJetpackProducts } from 'lib/products-values/translations';
@@ -191,9 +190,9 @@ export class PlansFeaturesMain extends Component {
 					isLandingPage={ isLandingPage }
 					isLaunchPage={ isLaunchPage }
 					onUpgradeClick={ onUpgradeClick }
-					plans={ [ ...plans, PLAN_P2_PLUS ] }
+					plans={ plans }
 					redirectTo={ redirectTo }
-					visiblePlans={ [ ...availablePlans, PLAN_P2_PLUS ] }
+					visiblePlans={ availablePlans }
 					selectedFeature={ selectedFeature }
 					selectedPlan={ selectedPlan }
 					withDiscount={ withDiscount }
