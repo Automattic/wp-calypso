db3978a6fc60399c6be71a3a26d42101f972e8a9
Allison Levine <allison.levine@automattic.com>
2020-10-07 11:57:35 -0400

    Add back query-site-selected-editor/index.jsx

---
+++ client/components/data/query-site-selected-editor/index.jsx
@@ -0,0 +1,25 @@
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { requestSelectedEditor } from 'state/selected-editor/actions';
+
+export default function QuerySiteSelectedEditor( { siteId } ) {
+	const dispatch = useDispatch();
+
+	useEffect( () => {
+		if ( siteId ) {
+			dispatch( requestSelectedEditor( siteId ) );
+		}
+	}, [ dispatch, siteId ] );
+
+	return null;
+}
+
+QuerySiteSelectedEditor.propTypes = { siteId: PropTypes.number };
