33c11c11ecac205f75d77942dc210d0b0bb91045
Allison Levine <allison.levine@automattic.com>
2020-10-07 11:41:07 -0400

    Revert removal of opt-in API editor actions Add back setSelectedEditor and requestSelectedEditor to allow for multiple editor flows defined in Gutenbert opt-in API.

---
+++ client/state/selected-editor/test/actions.js
@@ -0,0 +1,22 @@
+/**
+ * External dependencies
+ */
+
+/**
+ * Internal dependencies
+ */
+import { setSelectedEditor } from '../actions';
+import { EDITOR_TYPE_UPDATE } from 'state/action-types';
+
+describe( 'actions', () => {
+	describe( 'setSelectedEditor()', () => {
+		test( 'should return an action object', () => {
+			const action = setSelectedEditor( 2916284, 'gutenberg' );
+			expect( action ).toEqual( {
+				type: EDITOR_TYPE_UPDATE,
+				siteId: 2916284,
+				editor: 'gutenberg',
+			} );
+		} );
+	} );
+} );
---
+++ client/state/selected-editor/test/reducer.js
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import deepFreeze from 'deep-freeze';
+
+/**
+ * Internal dependencies
+ */
+import { EDITOR_TYPE_SET } from 'state/action-types';
+import selectedEditor from '../reducer';
+
+describe( 'reducer', () => {
+	test( 'should add a new given site ID key and its value', () => {
+		const state = selectedEditor( deepFreeze( {} ), {
+			type: EDITOR_TYPE_SET,
+			siteId: 2916284,
+			editor: 'gutenberg',
+		} );
+
+		expect( state ).toEqual( {
+			2916284: 'gutenberg',
+		} );
+	} );
+
+	test( 'should update an existing given site ID key', () => {
+		const original = deepFreeze( {
+			2916284: 'classic',
+		} );
+		const state = selectedEditor( original, {
+			type: EDITOR_TYPE_SET,
+			siteId: 2916284,
+			editor: 'gutenberg',
+		} );
+
+		expect( state ).toEqual( {
+			2916284: 'gutenberg',
+		} );
+	} );
+} );
