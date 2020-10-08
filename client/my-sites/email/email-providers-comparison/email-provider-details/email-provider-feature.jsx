1aa6c0149a3c7b9c6653f454025bebb97bae687a
Elton <eltongo@users.noreply.github.com>
2020-10-08 18:08:55 +0200

    Remove orphans in email provider features (#46280)

--- client/my-sites/email/email-providers-comparison/email-provider-details/email-provider-feature.jsx
+++ client/my-sites/email/email-providers-comparison/email-provider-details/email-provider-feature.jsx
@@ -2,13 +2,15 @@
  * External dependencies
  */
 import React from 'react';
-import Gridicon from 'components/gridicon';
+import Gridicon from 'calypso/components/gridicon';
 
 export default ( { title } ) => {
+	const deorphanedTitle =
+		'string' === typeof title ? title.replace( /([^ ]) ([^ ]+)$/, '$1\u00a0$2' ) : title;
 	return (
 		<span className="email-provider-details__feature">
 			<Gridicon icon="checkmark" size="18" />
-			{ title }
+			{ deorphanedTitle }
 		</span>
 	);
 };
