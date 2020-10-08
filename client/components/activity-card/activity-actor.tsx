12e5da5c52691b9e22810b8c261a4381a8725a3e
Kevin Zoschke <kevin.zoschke@automattic.com>
2020-10-08 15:21:57 -0400

    Pass size as prop in AcitivityActor component

--- client/components/activity-card/activity-actor.tsx
+++ client/components/activity-card/activity-actor.tsx
@@ -21,66 +21,70 @@ import { Activity } from 'calypso/state/activity-log/types';
 /**
  * Module constants
  */
-const JETPACK_ACTOR = (
-	<div className="activity-card__actor">
-		{ ! isEnabled( 'jetpack/backup-simplified-screens' ) && <JetpackLogo size={ 40 } /> }
-		<div className="activity-card__actor-info">
-			<div className="activity-card__actor-name">Jetpack</div>
-		</div>
-	</div>
-);
+export const SIZE_XS = 18;
+export const SIZE_S = 32;
+export const SIZE_M = 40;
 
-const HAPPINESS_ACTOR = (
-	<div className="activity-card__actor">
-		{ ! isEnabled( 'jetpack/backup-simplified-screens' ) && <JetpackLogo size={ 40 } /> }
-		<div className="activity-card__actor-info">
-			<div className="activity-card__actor-name">Happiness Engineer</div>
-		</div>
-	</div>
-);
-
-const WORDPRESS_ACTOR = (
-	<div className="activity-card__actor">
-		<SocialLogo icon="wordpress" size={ 40 } />
-		<div className="activity-card__actor-info">
-			<div className="activity-card__actor-name">WordPress</div>
-		</div>
-	</div>
-);
-
-const MULTIPLE_ACTORS = (
-	<div className="activity-card__actor">
-		<Gridicon icon="multiple-users" size={ 18 } />
-		<div className="activity-card__actor-info">
-			<div className="activity-card__actor-name">{ translate( 'Multiple users' ) }</div>
-		</div>
-	</div>
-);
-
-type Props = Partial< Activity >;
+type Props = Partial< Activity > & {
+	size?: typeof SIZE_XS | typeof SIZE_S | typeof SIZE_M;
+};
 
 const ActivityActor: FunctionComponent< Props > = ( {
 	actorAvatarUrl,
 	actorName,
 	actorRole,
 	actorType,
+	size = SIZE_M,
 } ) => {
 	if ( actorName === 'WordPress' && actorType === 'Application' ) {
-		return WORDPRESS_ACTOR;
+		return (
+			<div className="activity-card__actor">
+				<SocialLogo icon="wordpress" size={ size } />
+				<div className="activity-card__actor-info">
+					<div className="activity-card__actor-name">WordPress</div>
+				</div>
+			</div>
+		);
 	}
+
 	if ( actorName === 'Jetpack' && actorType === 'Application' ) {
-		return JETPACK_ACTOR;
+		return (
+			<div className="activity-card__actor">
+				{ ( ! isEnabled( 'jetpack/backup-simplified-screens' ) ||
+					isEnabled( 'jetpack/backup-simplified-screens-i4' ) ) && <JetpackLogo size={ size } /> }
+				<div className="activity-card__actor-info">
+					<div className="activity-card__actor-name">Jetpack</div>
+				</div>
+			</div>
+		);
 	}
+
 	if ( actorName === 'Happiness Engineer' && actorType === 'Happiness Engineer' ) {
-		return HAPPINESS_ACTOR;
+		return (
+			<div className="activity-card__actor">
+				{ ( ! isEnabled( 'jetpack/backup-simplified-screens' ) ||
+					isEnabled( 'jetpack/backup-simplified-screens-i4' ) ) && <JetpackLogo size={ size } /> }
+				<div className="activity-card__actor-info">
+					<div className="activity-card__actor-name">Happiness Engineer</div>
+				</div>
+			</div>
+		);
 	}
+
 	if ( actorType === 'Multiple' ) {
-		return MULTIPLE_ACTORS;
+		return (
+			<div className="activity-card__actor">
+				<Gridicon icon="multiple-users" size={ SIZE_XS } />
+				<div className="activity-card__actor-info">
+					<div className="activity-card__actor-name">{ translate( 'Multiple users' ) }</div>
+				</div>
+			</div>
+		);
 	}
 
 	return (
 		<div className="activity-card__actor">
-			<Gravatar user={ { avatar_URL: actorAvatarUrl } } size={ 40 } />
+			<Gravatar user={ { avatar_URL: actorAvatarUrl } } size={ size } />
 			<div className="activity-card__actor-info">
 				<div className="activity-card__actor-name">{ actorName }</div>
 				{ actorRole && <div className="activity-card__actor-role">{ actorRole }</div> }
