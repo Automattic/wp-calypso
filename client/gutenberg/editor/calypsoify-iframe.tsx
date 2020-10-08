b7d2a69b1fd07c403e3035fcc90110778398b509
retrofox <rdsuarez@gmail.com>
2020-10-08 16:57:58 -0300

    editor: update calypsoify iframe getStatsPath()

--- client/gutenberg/editor/calypsoify-iframe.tsx
+++ client/gutenberg/editor/calypsoify-iframe.tsx
@@ -632,7 +632,7 @@ class CalypsoifyIframe extends Component<
 
 	getStatsPath = () => {
 		const { postId } = this.props;
-		return postId ? '/block-editor/:post_type/:site/:post_id' : '/block-editor/:post_type/:site';
+		return postId ? '/:post_type/:site/:post_id' : '/:post_type/:site';
 	};
 
 	getStatsProps = () => {
