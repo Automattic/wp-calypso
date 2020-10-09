4c6e2aefd407615177b9251458901b80dbef770e
Veselin Nikolov <veselin@automattic.com>
2020-10-09 16:51:46 +0300

    Update the schema to reflect the most recent endpoint changes

--- client/state/secure-your-brand/schema.js
+++ client/state/secure-your-brand/schema.js
@@ -1,13 +1,23 @@
 export const secureYourBrandSchema = {
-	type: 'array',
-	domains: {
-		type: 'object',
-		required: [ 'domain' ],
-		properties: {
-			domain: { type: 'string' },
-			product_id: { type: 'string' },
-			product_slug: { type: 'string' },
-			cost: { type: 'string' },
+	type: 'object',
+	properties: {
+		product_data: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					domain: { type: 'string' },
+					product_id: { type: 'number' },
+					product_slug: { type: 'string' },
+					cost: { type: 'number' },
+					currency: { type: 'string' },
+				},
+				required: [ 'domain', 'product_id', 'product_slug', 'cost', 'currency' ],
+				additionalProperties: false,
+			},
 		},
+		currency: { type: 'string' },
+		discounted_cost: { type: 'number' },
+		total_cost: { type: 'number' },
 	},
 };
