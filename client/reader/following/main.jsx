8f943a4b549738261229987cc89c412c3eecf367
bluefuton <chris@bluefuton.com>
2020-10-13 12:15:57 +1300

    Add NZ vote banner

--- client/reader/following/main.jsx
+++ client/reader/following/main.jsx
@@ -6,27 +6,27 @@ import { localize } from 'i18n-calypso';
 import page from 'page';
 import { initial, flatMap, trim } from 'lodash';
 import { connect, useDispatch } from 'react-redux';
-import config from 'config';
+import config from 'calypso/config';
 
 /**
  * Internal dependencies
  */
-import BlankSuggestions from 'reader/components/reader-blank-suggestions';
-import Stream from 'reader/stream';
+import BlankSuggestions from 'calypso/reader/components/reader-blank-suggestions';
+import Stream from 'calypso/reader/stream';
 import { CompactCard, Button } from '@automattic/components';
-import SearchInput from 'components/search';
-import { recordTrack } from 'reader/stats';
-import Suggestion from 'reader/search-stream/suggestion';
-import SuggestionProvider from 'reader/search-stream/suggestion-provider';
+import SearchInput from 'calypso/components/search';
+import { recordTrack } from 'calypso/reader/stats';
+import Suggestion from 'calypso/reader/search-stream/suggestion';
+import SuggestionProvider from 'calypso/reader/search-stream/suggestion-provider';
 import FollowingIntro from './intro';
-import { getSearchPlaceholderText } from 'reader/search/utils';
-import Banner from 'components/banner';
-import { getCurrentUserCountryCode } from 'state/current-user/selectors';
-import SectionHeader from 'components/section-header';
-import { requestMarkAllAsSeen } from 'state/reader/seen-posts/actions';
-import { SECTION_FOLLOWING } from 'state/reader/seen-posts/constants';
-import { getReaderOrganizationFeedsInfo } from 'state/reader/organizations/selectors';
-import { NO_ORG_ID } from 'state/reader/organizations/constants';
+import { getSearchPlaceholderText } from 'calypso/reader/search/utils';
+import Banner from 'calypso/components/banner';
+import { getCurrentUserCountryCode } from 'calypso/state/current-user/selectors';
+import SectionHeader from 'calypso/components/section-header';
+import { requestMarkAllAsSeen } from 'calypso/state/reader/seen-posts/actions';
+import { SECTION_FOLLOWING } from 'calypso/state/reader/seen-posts/constants';
+import { getReaderOrganizationFeedsInfo } from 'calypso/state/reader/organizations/selectors';
+import { NO_ORG_ID } from 'calypso/state/reader/organizations/constants';
 
 /**
  * Style dependencies
@@ -43,7 +43,7 @@ function handleSearch( query ) {
 	}
 }
 
-const lastDayForVoteBanner = new Date( '2018-11-07T00:00:00' );
+const lastDayForVoteBanner = new Date( '2020-10-18T00:00:00' );
 
 const FollowingStream = ( props ) => {
 	const suggestionList =
@@ -56,7 +56,7 @@ const FollowingStream = ( props ) => {
 		);
 	const placeholderText = getSearchPlaceholderText();
 	const now = new Date();
-	const showRegistrationMsg = props.userInUSA && now < lastDayForVoteBanner;
+	const showRegistrationMsg = props.userInNZ && now < lastDayForVoteBanner;
 	const { translate } = props;
 	const dispatch = useDispatch();
 
@@ -72,14 +72,14 @@ const FollowingStream = ( props ) => {
 			{ showRegistrationMsg && (
 				<Banner
 					className="following__reader-vote"
-					title="Election Day: Tuesday November 6th"
+					title="NZ Election Day: Saturday 17 October"
 					callToAction="How to vote"
-					description="Remember to vote."
-					dismissPreferenceName="reader-vote-prompt"
+					description="You can vote from 3 October to election day, 17 October."
 					event="reader-vote-prompt"
-					href="https://www.usa.gov/election-office"
+					href="https://vote.nz/"
 					icon="star"
 					horizontal
+					target="_blank"
 				/>
 			) }
 			<CompactCard className="following__search">
@@ -111,6 +111,6 @@ const FollowingStream = ( props ) => {
 };
 
 export default connect( ( state ) => ( {
-	userInUSA: getCurrentUserCountryCode( state ) === 'US',
+	userInNZ: getCurrentUserCountryCode( state ) === 'NZ',
 	feedsInfo: getReaderOrganizationFeedsInfo( state, NO_ORG_ID ),
 } ) )( SuggestionProvider( localize( FollowingStream ) ) );
