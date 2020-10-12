1629c0e2134a5e32fdb1013794b775318a12e2b7
Payton Swick <payton@foolord.com>
2020-10-12 16:49:23 -0400

    Replace lib/cart-values types with composite-checkout types
    The types in composite-checkout are more up-to-date and this avoids type
    coercion when really it's all the same data.

--- client/lib/cart-values/types.ts
+++ client/lib/cart-values/types.ts
@@ -1,55 +1,14 @@
 /**
  * Internal dependencies
  */
-import type { GSuiteProductUser } from 'lib/gsuite/new-users';
-import type { DomainContactDetails } from 'my-sites/checkout/composite-checkout/types/backend/domain-contact-details-components';
+import {
+	ResponseCart,
+	ResponseCartProduct,
+	ResponseCartProductExtra,
+} from 'calypso/my-sites/checkout/composite-checkout/hooks/use-shopping-cart-manager/types';
 
-export type CartItemValue = {
-	product_id?: number;
-	product_name?: string;
-	product_slug?: string;
-	meta?: string;
-	cost?: number;
-	orig_cost?: number | null;
-	currency?: string;
-	volume?: number;
-	free_trial?: boolean;
-	is_domain_registration?: boolean;
-	extra?: CartItemExtra;
-};
+export type CartItemValue = ResponseCartProduct;
 
-export type CartItemExtra = {
-	context?: string;
-	source?: string;
-	domain_to_bundle?: string;
-	google_apps_users?: GSuiteProductUser[];
-	google_apps_registration_data?: DomainContactDetails;
-	purchaseId?: string;
-	purchaseDomain?: string;
-	purchaseType?: string;
-	includedDomain?: string;
-	privacy?: boolean;
-};
+export type CartItemExtra = ResponseCartProductExtra;
 
-export type CartValue = {
-	blog_id: string | number;
-	products: CartItemValue[];
-	total_cost?: number;
-	temporary?: boolean;
-	currency?: string;
-	coupon?: string;
-	bundled_domain?: string;
-	is_coupon_applied?: boolean;
-	has_bundle_credit?: boolean;
-	next_domain_is_free?: boolean;
-	next_domain_condition?: string;
-	messages?: {
-		errors?: string[];
-		success?: string[];
-	};
-	client_metadata?: ClientMetadata;
-};
-
-export type ClientMetadata = {
-	lastServerResponseDate: string;
-};
+export type CartValue = ResponseCart;
