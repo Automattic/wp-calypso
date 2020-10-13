6db59d182d73b5f4994a45f29673fa91a8f5a161
Marin Atanasov <tyxla@abv.bg>
2020-10-13 15:12:34 +0300

    Security: Refactor Security2faProgressItem to a functional component

--- client/me/security-2fa-progress/progress-item.jsx
+++ client/me/security-2fa-progress/progress-item.jsx
@@ -9,20 +9,16 @@ import classNames from 'classnames';
  */
 import Gridicon from 'calypso/components/gridicon';
 
-export default class extends React.Component {
-	static displayName = 'Security2faProgressItem';
+const Security2faProgressItem = ( { icon, label, step } ) => (
+	<div
+		className={ classNames( 'security-2fa-progress__item', {
+			'is-highlighted': step.isHighlighted,
+			'is-completed': step.isCompleted,
+		} ) }
+	>
+		<Gridicon icon={ icon } />
+		<label>{ label } </label>
+	</div>
+);
 
-	render() {
-		return (
-			<div
-				className={ classNames( 'security-2fa-progress__item', {
-					'is-highlighted': this.props.step.isHighlighted,
-					'is-completed': this.props.step.isCompleted,
-				} ) }
-			>
-				<Gridicon icon={ this.props.icon } />
-				<label>{ this.props.label } </label>
-			</div>
-		);
-	}
-}
+export default Security2faProgressItem;
