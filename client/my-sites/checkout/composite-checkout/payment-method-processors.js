956a76c07fa7de276c8d835d7b878c9869c6528d
Payton Swick <payton@foolord.com>
2020-10-13 17:03:29 -0400

    Convert freePurchaseProcessor to use standard response

--- client/my-sites/checkout/composite-checkout/payment-method-processors.js
+++ client/my-sites/checkout/composite-checkout/payment-method-processors.js
@@ -243,7 +243,11 @@ export async function freePurchaseProcessor(
 			postalCode: null,
 		},
 		wpcomTransaction
-	).then( saveTransactionResponseToWpcomStore );
+	)
+		.then( saveTransactionResponseToWpcomStore )
+		.then( ( response ) => {
+			return { type: 'SUCCESS', payload: response };
+		} );
 }
 
 export async function fullCreditsProcessor(
