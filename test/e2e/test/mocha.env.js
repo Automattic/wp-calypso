713cd7e056298cdc4cfbe921df80a7b3a7457b1b
Marco Ciampini <marco.ciampini@automattic.com>
2020-10-13 14:45:33 +0200

    fix: install puppeteer/chromedriver before tests
    Depending on the env variables, puppeteer/chromedriver can be skipped
    when installing project dependencies.
    These code changes make sure that those deps are installed before
    running any mocha commands.

--- test/e2e/test/mocha.env.js
+++ test/e2e/test/mocha.env.js
@@ -1,1 +1,27 @@
 process.env.SELENIUM_PROMISE_MANAGER = '0';
+
+/**
+ * External dependencies (can also be listed in the root package.json)
+ */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const execa = require( 'execa' );
+
+// Make sure that puppeteer and chromedriver are installed before running tests
+try {
+	execa.sync( 'node', [ require.resolve( 'puppeteer/install' ) ], {
+		env: {
+			PUPPETEER_SKIP_DOWNLOAD: '',
+			PUPPETEER_SKIP_CHROMIUM_DOWNLOAD: '',
+		},
+		stdio: 'inherit',
+	} );
+
+	execa.sync( 'node', [ require.resolve( 'chromedriver/install' ) ], {
+		env: {
+			CHROMEDRIVER_SKIP_DOWNLOAD: '',
+		},
+		stdio: 'inherit',
+	} );
+} catch ( error ) {
+	console.error( error );
+}
