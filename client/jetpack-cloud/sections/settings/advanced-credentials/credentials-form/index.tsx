fd46f96285d70f70d1d59804e3fd37d1dfef6651
Scott Evans <git@scott.ee>
2020-10-14 00:24:59 +0200

    Jetpack cloud credentials typography (#46390)

--- client/jetpack-cloud/sections/settings/advanced-credentials/credentials-form/index.tsx
+++ client/jetpack-cloud/sections/settings/advanced-credentials/credentials-form/index.tsx
@@ -319,7 +319,7 @@ const ServerCredentialsForm: FunctionComponent< Props > = ( {
 	return (
 		<div className="credentials-form">
 			<h3>{ translate( 'Provide your SSH, SFTP or FTP server credentials' ) }</h3>
-			<p>{ getSubHeaderText() }</p>
+			<p class="credentials-form__intro-text">{ getSubHeaderText() }</p>
 			{ renderCredentialLinks() }
 			<FormFieldset className="credentials-form__protocol-type">
 				<div className="credentials-form__support-info">
