2cd1add092acc78c653d259d6b0c5a0a3a32223e
Nathan Bloomfield <nathan.bloomfield@a8c.com>
2020-10-13 12:46:28 -0500

    Link to site level page in purchases listing action button (#46333)
    Update the links in /plans and /plans/my-plan to use site-level routes /purchases/subscriptions/:siteUrl/:purchaseId when appropriate.
    
    Also refactors a deeply nested if block in PlanFeaturesActions using the following:
    
    If p,q,r,s and a,b,c,d,e are values with no side effects, then the following JS fragments are equivalent:
    
    (1) let x = a;
        if (p) { x = b; }
        if (q) { x = c; }
        if (r) { x = d; }
        if (s) { x = e; }
        return x;
    
    (2) if (s) { return e; }
        if (r) { return d; }
        if (q) { return c; }
        if (p) { return b; }
        return a;
    
    And the following two fragments are equivalent:
    
    (1) if (p) {
          if (q) {return a;}
          if (r) {return b;}
          if (s) (return c;}
          if (t) (return d;}
          return e;
        }
    
    (2) if (p&&q) {return a;}
        if (p&&r) {return b;}
        if (p&&s) {return c;}
        if (p&&t) {return d;}
        if (p) return e;
    
    Co-authored-by: nbloomf <nathan.bloomfield@automattic.com>

--- client/my-sites/plan-features/actions.jsx
+++ client/my-sites/plan-features/actions.jsx
@@ -13,13 +13,23 @@ import classNames from 'classnames';
  * Internal dependencies
  */
 import { Button } from '@automattic/components';
-import { getCurrentPlan } from 'state/sites/plans/selectors';
-import { getSelectedSiteId } from 'state/ui/selectors';
-import { isMonthly } from 'lib/plans/constants';
-import { getPlanClass, planLevelsMatch } from 'lib/plans';
-import { recordTracksEvent } from 'state/analytics/actions';
+import { getCurrentPlan } from 'calypso/state/sites/plans/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { isMonthly } from 'calypso/lib/plans/constants';
+import { getPlanClass, planLevelsMatch } from 'calypso/lib/plans';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 
-const PlanFeaturesActions = ( {
+const PlanFeaturesActions = ( props ) => {
+	return (
+		<div className="plan-features__actions">
+			<div className="plan-features__actions-buttons">
+				<PlanFeaturesActionsButton { ...props } />
+			</div>
+		</div>
+	);
+};
+
+const PlanFeaturesActionsButton = ( {
 	availableForPurchase = true,
 	canPurchase,
 	className,
@@ -42,8 +52,6 @@ const PlanFeaturesActions = ( {
 	translate,
 	...props
 } ) => {
-	let upgradeButton;
-
 	const classes = classNames(
 		'plan-features__actions-button',
 		{
@@ -55,81 +63,107 @@ const PlanFeaturesActions = ( {
 		className
 	);
 
+	const handleUpgradeButtonClick = () => {
+		if ( isPlaceholder ) {
+			return;
+		}
+
+		trackTracksEvent( 'calypso_plan_features_upgrade_click', {
+			current_plan: currentSitePlanSlug,
+			upgrading_to: planType,
+		} );
+
+		onUpgradeClick();
+	};
+
 	if ( current && ! isInSignup ) {
-		upgradeButton = (
+		return (
 			<Button className={ classes } href={ manageHref } disabled={ ! manageHref }>
 				{ canPurchase ? translate( 'Manage plan' ) : translate( 'View plan' ) }
 			</Button>
 		);
-	} else if ( availableForPurchase || isPlaceholder ) {
-		let buttonText = freePlan
-			? translate( 'Select Free', { context: 'button' } )
-			: translate( 'Upgrade', { context: 'verb' } );
-		if ( isLandingPage ) {
-			buttonText = translate( 'Select', { context: 'button' } );
-		}
-		if ( isLaunchPage ) {
-			if ( freePlan ) {
-				buttonText = translate( 'Keep this plan', {
-					comment:
-						'A selection to keep the current plan. Check screenshot - https://cloudup.com/cb_9FMG_R01',
-				} );
-			} else {
-				buttonText = translate( 'Select %(plan)s', {
-					args: {
-						plan: planName,
-					},
-					context: 'Button to select a paid plan by plan name, e.g., "Select Personal"',
-					comment:
-						'A button to select a new paid plan. Check screenshot - https://cloudup.com/cb_9FMG_R01',
-				} );
-			}
-		} else if ( isInSignup ) {
-			buttonText = translate( 'Start with %(plan)s', {
-				args: {
-					plan: planName,
-				},
-			} );
-		}
+	}
 
-		if (
-			isMonthly( currentSitePlanSlug ) &&
-			getPlanClass( planType ) === getPlanClass( currentSitePlanSlug )
-		) {
-			buttonText = translate( 'Upgrade to Yearly' );
-		}
-
-		const handleUpgradeButtonClick = () => {
-			if ( isPlaceholder ) {
-				return;
-			}
-
-			trackTracksEvent( 'calypso_plan_features_upgrade_click', {
-				current_plan: currentSitePlanSlug,
-				upgrading_to: planType,
-			} );
-
-			onUpgradeClick();
-		};
-
-		upgradeButton = (
+	if (
+		( availableForPurchase || isPlaceholder ) &&
+		isMonthly( currentSitePlanSlug ) &&
+		getPlanClass( planType ) === getPlanClass( currentSitePlanSlug )
+	) {
+		return (
 			<Button className={ classes } onClick={ handleUpgradeButtonClick } disabled={ isPlaceholder }>
-				{ props.buttonText || buttonText }
+				{ props.buttonText || translate( 'Upgrade to Yearly' ) }
 			</Button>
 		);
-	} else if ( ! availableForPurchase && forceDisplayButton ) {
-		upgradeButton = (
+	}
+
+	if ( ( availableForPurchase || isPlaceholder ) && ! isLaunchPage && isInSignup ) {
+		return (
+			<Button className={ classes } onClick={ handleUpgradeButtonClick } disabled={ isPlaceholder }>
+				{ props.buttonText ||
+					translate( 'Start with %(plan)s', {
+						args: {
+							plan: planName,
+						},
+					} ) }
+			</Button>
+		);
+	}
+
+	if ( ( availableForPurchase || isPlaceholder ) && isLaunchPage && ! freePlan ) {
+		return (
+			<Button className={ classes } onClick={ handleUpgradeButtonClick } disabled={ isPlaceholder }>
+				{ props.buttonText ||
+					translate( 'Select %(plan)s', {
+						args: {
+							plan: planName,
+						},
+						context: 'Button to select a paid plan by plan name, e.g., "Select Personal"',
+						comment:
+							'A button to select a new paid plan. Check screenshot - https://cloudup.com/cb_9FMG_R01',
+					} ) }
+			</Button>
+		);
+	}
+
+	if ( ( availableForPurchase || isPlaceholder ) && isLaunchPage && freePlan ) {
+		return (
+			<Button className={ classes } onClick={ handleUpgradeButtonClick } disabled={ isPlaceholder }>
+				{ props.buttonText ||
+					translate( 'Keep this plan', {
+						comment:
+							'A selection to keep the current plan. Check screenshot - https://cloudup.com/cb_9FMG_R01',
+					} ) }
+			</Button>
+		);
+	}
+
+	if ( ( availableForPurchase || isPlaceholder ) && isLandingPage ) {
+		return (
+			<Button className={ classes } onClick={ handleUpgradeButtonClick } disabled={ isPlaceholder }>
+				{ props.buttonText || translate( 'Select', { context: 'button' } ) }
+			</Button>
+		);
+	}
+
+	if ( availableForPurchase || isPlaceholder ) {
+		return (
+			<Button className={ classes } onClick={ handleUpgradeButtonClick } disabled={ isPlaceholder }>
+				{ props.buttonText || freePlan
+					? translate( 'Select Free', { context: 'button' } )
+					: translate( 'Upgrade', { context: 'verb' } ) }
+			</Button>
+		);
+	}
+
+	if ( ! availableForPurchase && forceDisplayButton ) {
+		return (
 			<Button className={ classes } disabled={ true }>
 				{ props.buttonText }
 			</Button>
 		);
 	}
 
-	return (
-		<div className="plan-features__actions">
-			<div className="plan-features__actions-buttons">{ upgradeButton }</div>
-		</div>
-	);
+	return null;
 };
 
 PlanFeaturesActions.propTypes = {
