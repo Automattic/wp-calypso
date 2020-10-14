c2b2c0277d9920aaf29b6dc53091fc9c843000ac
Marco Ciampini <marco.ciampini@automattic.com>
2020-10-14 14:31:44 +0200

    [e2e] Installchromedriver before running e2e tests (#46400)
    Depending on the env variables, chromedriver can be skipped
    when installing project dependencies.
    These code changes make sure that it gets installed before
    running any mocha commands.

--- test/e2e/test/mocha.env.js
+++ test/e2e/test/mocha.env.js
@@ -1,1 +1,19 @@
 process.env.SELENIUM_PROMISE_MANAGER = '0';
+
+/**
+ * External dependencies (can also be listed in the root package.json)
+ */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const execa = require( 'execa' );
+
+// Make sure that chromedriver is installed before running tests
+try {
+	execa.sync( 'node', [ require.resolve( 'chromedriver/install' ) ], {
+		env: {
+			CHROMEDRIVER_SKIP_DOWNLOAD: '',
+		},
+		stdio: 'inherit',
+	} );
+} catch ( error ) {
+	console.error( error );
+}
