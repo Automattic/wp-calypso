c928ab7da747ebe26f4a7eb6d822fd15b42b2eca
roo2 <roo2@users.noreply.github.com>
2020-10-14 15:46:50 +1000

    remove editable coming soon page for now (#46357)
    * remove editable coming soon page for now
    * remove code to render the custom coming soon page

--- apps/editing-toolkit/editing-toolkit-plugin/coming-soon/coming-soon.php
+++ apps/editing-toolkit/editing-toolkit-plugin/coming-soon/coming-soon.php
@@ -7,8 +7,6 @@
 
 namespace A8C\FSE\Coming_soon;
 
-require_once __DIR__ . '/coming-soon-page-template.php';
-
 /**
  * Determines whether the coming soon page should be shown.
  *
@@ -78,38 +76,6 @@ function add_public_coming_soon_to_settings_endpoint_post( $input, $unfiltered_i
 add_filter( 'rest_api_update_site_settings', __NAMESPACE__ . '\add_public_coming_soon_to_settings_endpoint_post', 10, 2 );
 
 /**
- * This filter adds the coming soon page to the list of page templates using the `vertical_templates` hook
- *
- * @param array $templates The array of all available page templates.
- *
- * @return array The updated $templates array
- */
-function add_public_coming_soon_page_template( $templates ) : array {
-	$templates[] = (object) COMING_SOON_PAGE_TEMPLATE;
-	return $templates;
-}
-add_filter( 'vertical_templates', __NAMESPACE__ . '\add_public_coming_soon_page_template' );
-
-/**
- * Adds an editable coming soon page to a site
- */
-function add_coming_soon_page_to_new_site() {
-	$coming_soon_template = COMING_SOON_PAGE_TEMPLATE;
-	$new_page_id          = wp_insert_post(
-		array(
-			'post_title'     => $coming_soon_template['post_title'],
-			'post_content'   => $coming_soon_template['post_content'],
-			'post_status'    => 'publish',
-			'post_type'      => 'page',
-			'comment_status' => 'closed',
-			'ping_status'    => 'closed',
-		)
-	);
-	add_option( 'wpcom_public_coming_soon_page_id', $new_page_id );
-}
-add_action( 'signup_finished', __NAMESPACE__ . '\add_coming_soon_page_to_new_site', 10, 1 );
-
-/**
  * Launch the site when the privacy mode changes from public-not-indexed
  * This can happen due to clicking the launch button from the banner
  * Or due to manually updating the setting in wp-admin or calypso settings page.
@@ -159,47 +125,7 @@ function coming_soon_page() {
 	add_filter( 'wpcom_disable_logged_out_follow', '__return_true', 10, 1 ); // Disable follow actionbar.
 	add_filter( 'wpl_is_enabled_sitewide', '__return_false', 10, 1 ); // Disable likes.
 
-	$should_show_fallback = false;
-
-	$id = (int) get_option( 'wpcom_public_coming_soon_page_id', 0 );
-	if ( empty( $id ) ) {
-		$should_show_fallback = true;
-	}
-
-	$custom_coming_soon_page = get_post( $id );
-	if ( ! $custom_coming_soon_page ) {
-		$should_show_fallback = true;
-	}
-
-	if ( $should_show_fallback ) {
-		render_fallback_coming_soon_page();
-		die();
-	}
-
-	?>
-	<!doctype html>
-	<html <?php language_attributes(); ?>>
-		<head>
-			<meta charset="<?php bloginfo( 'charset' ); ?>" />
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<?php wp_head(); ?>
-		</head>
-		<body>
-			<?php
-				// Replicates the core `the_content()` function except using the custom
-				// coming soon page instead of the current page.
-				$content = get_the_content( null, false, $custom_coming_soon_page );
-				$content = apply_filters( 'the_content', $content );
-
-				// Perform the same escaping done by the `the_content()` function.
-				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-				echo str_replace( ']]>', ']]&gt;', $content );
-			?>
-			<?php wp_footer(); ?>
-		</body>
-	</html>
-	<?php
-
+	render_fallback_coming_soon_page();
 	die();
 }
 add_action( 'template_redirect', __NAMESPACE__ . '\coming_soon_page' );
