035f705c69d90fcf2f179e2908030ef4359c3b24
Dave Smith <getdavemail@gmail.com>
2020-10-14 13:11:50 +0100

    Cache JITM state selector and optimise via default state of null
    Returning null as the default state enables us to distinguish between “empty” and “not yet set”. This in turn allows the selector to utilise a cache to return the previous JITM messages, until such time as the JITM state updates to notify there are no (empty) JITMs for the given message path.
    
    This allows us to optimise re-renders of the JITM component

--- client/state/jitm/selectors.js
+++ client/state/jitm/selectors.js
@@ -4,7 +4,7 @@
 import { get } from 'lodash';
 
 /** Internal dependencies */
-import { getSelectedSiteId } from 'state/ui/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 /**
  * Get the list of available jitms for the current site/section
@@ -14,7 +14,7 @@ import { getSelectedSiteId } from 'state/ui/selectors';
  * @returns {Array} An array of jitms
  */
 export const getJITM = ( state, messagePath ) =>
-	get( state, [ 'jitm', 'sitePathJITM', messagePath + getSelectedSiteId( state ) ], [] );
+	get( state, [ 'jitm', 'sitePathJITM', messagePath + getSelectedSiteId( state ) ], null );
 
 /**
  * Get the top jitm available for the current site/section
@@ -23,12 +23,29 @@ export const getJITM = ( state, messagePath ) =>
  * @param {string} messagePath The jitm message path (ex: calypso:comments:admin_notices)
  * @returns {object} A jitm
  */
+
+let JITMCache = [];
+
 export const getTopJITM = ( state, messagePath ) => {
 	const jitms = getJITM( state, messagePath );
 
-	if ( jitms.length === 0 ) {
+	// If we have yet to populate the JITM state for the
+	// given message path then return from the previous JITM
+	// cache until JITMS can be fulfilled from network.
+	if ( jitms === null ) {
+		return JITMCache[ 0 ] || null;
+	}
+
+	// If having fullfilled we still have no JITMs then
+	// clear the cache and return null to signify empty.
+	if ( ! jitms.length ) {
+		JITMCache = [];
 		return null;
 	}
 
+	// Cache the results.
+	JITMCache = Array.from( jitms );
+
+	// Return results.
 	return jitms[ 0 ];
 };
