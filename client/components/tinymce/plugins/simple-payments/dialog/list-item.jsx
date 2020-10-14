d0ef4f2f89a828efdef8acfe00f2d214ef8609ae
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2020-10-14 10:50:40 +0300

    Simple Payments: Use FormLabel instead of label in the dialog (#46395)
    * Components: Sort imports and make relative to Calypso root in ProductListItem
    
    * Components: Use FormLabel in ProductListItem
    
    * Components: Fix simple payments modal label styles

--- client/components/tinymce/plugins/simple-payments/dialog/list-item.jsx
+++ client/components/tinymce/plugins/simple-payments/dialog/list-item.jsx
@@ -11,12 +11,13 @@ import { getCurrencyObject } from '@automattic/format-currency';
 /**
  * Internal dependencies
  */
-import { CompactCard } from '@automattic/components';
 import EllipsisMenu from 'calypso/components/ellipsis-menu';
+import FormLabel from 'calypso/components/forms/form-label';
 import FormRadio from 'calypso/components/forms/form-radio';
 import log from 'calypso/lib/catch-js-errors/log';
 import PopoverMenuItem from 'calypso/components/popover/menu-item';
 import ProductImage from './product-image';
+import { CompactCard } from '@automattic/components';
 import { DEFAULT_CURRENCY } from 'calypso/lib/simple-payments/constants';
 
 /* eslint-disable wpcalypso/jsx-classname-namespace */
@@ -77,10 +78,10 @@ class ProductListItem extends Component {
 					checked={ isSelected }
 					onChange={ this.handleRadioChange }
 				/>
-				<label className={ labelClasses } htmlFor={ radioId }>
+				<FormLabel className={ labelClasses } htmlFor={ radioId }>
 					<div className="editor-simple-payments-modal__list-name">{ title }</div>
 					<div>{ this.formatPrice( price, currency ) }</div>
-				</label>
+				</FormLabel>
 				<ProductImage siteId={ siteId } imageId={ featuredImageId } />
 				<EllipsisMenu
 					className="editor-simple-payments-modal__list-menu"
