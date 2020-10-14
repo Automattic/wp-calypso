4dd232c80e19884a9f394bdb0a2c45fbd27cf7d4
apeatling <apeatling@gmail.com>
2020-10-14 10:27:17 -0700

    Restore loading of old patterns, and load new patterns via API if filter is set. Fixes issues mentioned in #46422.

--- apps/editing-toolkit/editing-toolkit-plugin/full-site-editing-plugin.php
+++ apps/editing-toolkit/editing-toolkit-plugin/full-site-editing-plugin.php
@@ -262,7 +262,11 @@ add_action( 'plugins_loaded', __NAMESPACE__ . '\load_wpcom_block_editor_nux' );
 /**
  * Load editing toolkit block patterns
  */
-function load_block_patterns() {
+function load_local_block_patterns() {
+	if ( apply_filters( 'a8c_enable_block_patterns_api', false ) ) {
+		return;
+	}
+
 	if ( ! function_exists( '\gutenberg_load_block_pattern' ) ) {
 		return;
 	}
@@ -271,7 +275,30 @@ function load_block_patterns() {
 
 	Block_Patterns::get_instance();
 }
-add_action( 'init', __NAMESPACE__ . '\load_block_patterns', 20 );
+add_action( 'init', __NAMESPACE__ . '\load_local_block_patterns', 20 );
+
+/**
+ * Load editing toolkit block patterns from the API
+ *
+ * @param obj $current_screen The current screen object.
+ */
+function load_block_patterns_from_api( $current_screen ) {
+	if ( ! apply_filters( 'a8c_enable_block_patterns_api', false ) ) {
+		return;
+	}
+
+	if ( ! function_exists( '\gutenberg_load_block_pattern' ) ) {
+		return;
+	}
+
+	if ( ! $current_screen->is_block_editor ) {
+		return;
+	}
+
+	require_once __DIR__ . '/block-patterns/class-block-patterns-api.php';
+	Block_Patterns_From_API::get_instance();
+}
+add_action( 'current_screen', __NAMESPACE__ . '\load_block_patterns_from_api' );
 
 /**
  * Load Premium Content Block
