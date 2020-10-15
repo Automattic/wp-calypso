d2dfb85065f5c46e8743f5a2ebe09bb510ae3ee8
taggon <gonom9@gmail.com>
2020-10-16 01:49:28 +0900

    Fix lint errors / broken tests

---
+++ client/my-sites/plans-features-main/test/free-plan-banner.jsx
@@ -0,0 +1,25 @@
+jest.mock( 'i18n-calypso', () => ( {
+	localize: ( Component ) => ( props ) => <Component { ...props } translate={ ( x ) => x } />,
+	numberFormat: ( x ) => x,
+	translate: ( x ) => x,
+	useTranslate: jest.fn( () => ( x ) => x ),
+} ) );
+
+/**
+ * External dependencies
+ */
+import { shallow } from 'enzyme';
+import React from 'react';
+import FreePlanBanner from '../free-plan-banner';
+
+describe( '<FreePlanBanner />', () => {
+	it( 'should be hidden when hidden prop is true', () => {
+		const node = shallow( <FreePlanBanner hidden={ true } /> );
+		expect( node.isEmptyRender() ).toBe( true );
+	} );
+
+	it( 'should be hidden when customHeader is passed', () => {
+		const node = shallow( <FreePlanBanner customHeader={ {} } /> );
+		expect( node.isEmptyRender() ).toBe( true );
+	} );
+} );
