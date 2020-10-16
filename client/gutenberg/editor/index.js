314a934ca5a46645c1cb88aebc0e7c33b3fb286f
retrofox <rdsuarez@gmail.com>
2020-10-16 08:43:33 -0300

    editor: simplify /new redirecting

--- client/gutenberg/editor/index.js
+++ client/gutenberg/editor/index.js
@@ -23,7 +23,7 @@ export default function () {
 	);
 
 	page( '/post', siteSelection, sites, makeLayout, clientRender );
-	page( '/post/new', () => page.redirect( '/post' ) ); // redirect from beep-beep-boop
+	page( '/post/new', '/post' ); // redirect from beep-beep-boop
 	page(
 		'/post/:site/:post?',
 		siteSelection,
@@ -37,7 +37,7 @@ export default function () {
 	page( '/post/:site?', siteSelection, redirect, makeLayout, clientRender );
 
 	page( '/page', siteSelection, sites, makeLayout, clientRender );
-	page( '/page/new', () => page.redirect( '/post' ) ); // redirect from beep-beep-boop
+	page( '/page/new', '/page' ); // redirect from beep-beep-boop
 	page(
 		'/page/:site/:post?',
 		siteSelection,
