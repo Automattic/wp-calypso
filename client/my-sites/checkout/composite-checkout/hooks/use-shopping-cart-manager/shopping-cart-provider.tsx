1a985062fe3eee09e6eec3a54cf4f550393151d4
Payton Swick <payton@foolord.com>
2020-10-16 00:11:03 -0400

    Add ShoppingCartProvider and useShoppingCart

---
+++ client/my-sites/checkout/composite-checkout/hooks/use-shopping-cart-manager/shopping-cart-provider.tsx
@@ -0,0 +1,62 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { RequestCart, ResponseCart } from './types';
+import useShoppingCartManager from './index';
+import ShoppingCartContext from './shopping-cart-context';
+
+export default function ShoppingCartProvider( {
+	cartKey,
+	setCart,
+	getCart,
+	children,
+}: {
+	cartKey: string | number | null | undefined;
+	setCart: ( cartKey: string, arg1: RequestCart ) => Promise< ResponseCart >;
+	getCart: ( cartKey: string ) => Promise< ResponseCart >;
+	children: JSX.Element;
+} ): JSX.Element {
+	const {
+		removeProductFromCart,
+		couponStatus,
+		applyCoupon,
+		removeCoupon,
+		updateLocation,
+		replaceProductInCart,
+		isLoading,
+		isPendingUpdate,
+		responseCart,
+		loadingError,
+		loadingErrorType,
+		addProductsToCart,
+		replaceProductsInCart,
+	} = useShoppingCartManager( {
+		cartKey,
+		canInitializeCart: true,
+		setCart,
+		getCart,
+	} );
+
+	const value = {
+		removeProductFromCart,
+		couponStatus,
+		applyCoupon,
+		removeCoupon,
+		updateLocation,
+		replaceProductInCart,
+		isLoading,
+		isPendingUpdate,
+		responseCart,
+		loadingError,
+		loadingErrorType,
+		addProductsToCart,
+		replaceProductsInCart,
+	};
+
+	return <ShoppingCartContext.Provider value={ value }>{ children }</ShoppingCartContext.Provider>;
+}
