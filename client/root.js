5ff296467586b83870f0222e998fa3086fcdff9f
Jarda Snajdr <jsnajdr@gmail.com>
2020-10-19 14:24:37 +0200

    Root redirect: check the logged in/out state when handling the / route, not when installing it
    Should fix `wp-desktop` test failures that are related to login and logout.
    
    If the user was logged in at the time when the `/` route handler was installed, then the `/`
    route won't be handled well right after the user logs out. And vice versa during the logged-out
    to logged-in transition.
    
    The fix moves the logged in/out check inside the route handler, to be executed every time.
    
    The Desktop app doesn't reboot/reload during these transitions, unlike the Web app, that's why
    the transitions fail.

--- client/root.js
+++ client/root.js
@@ -7,40 +7,35 @@ import page from 'page';
  * Internal dependencies
  */
 import config from 'calypso/config';
-import userFactory from 'calypso/lib/user';
+import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import { requestSite } from 'calypso/state/sites/actions';
 import getPrimarySiteId from 'calypso/state/selectors/get-primary-site-id';
 import { canCurrentUserUseCustomerHome, getSite, getSiteSlug } from 'calypso/state/sites/selectors';
 
 export default function () {
-	const user = userFactory();
-	const isLoggedOut = ! user.get();
-	if ( isLoggedOut ) {
-		setupLoggedOut();
-	} else {
-		setupLoggedIn();
-	}
+	page( '/', ( context ) => {
+		const isLoggedIn = isUserLoggedIn( context.store.getState() );
+		if ( isLoggedIn ) {
+			handleLoggedIn( context );
+		} else {
+			handleLoggedOut( context );
+		}
+	} );
 }
 
-function setupLoggedOut() {
+function handleLoggedOut() {
 	if ( config.isEnabled( 'desktop' ) ) {
-		page( '/', () => {
-			if ( config.isEnabled( 'oauth' ) ) {
-				page.redirect( '/oauth-login' );
-			} else {
-				page.redirect( '/log-in' );
-			}
-		} );
+		if ( config.isEnabled( 'oauth' ) ) {
+			page.redirect( '/oauth-login' );
+		} else {
+			page.redirect( '/log-in' );
+		}
 	} else if ( config.isEnabled( 'devdocs/redirect-loggedout-homepage' ) ) {
-		page( '/', () => {
-			page.redirect( '/devdocs/start' );
-		} );
+		page.redirect( '/devdocs/start' );
 	} else if ( config.isEnabled( 'jetpack-cloud' ) ) {
-		page( '/', () => {
-			if ( config.isEnabled( 'oauth' ) ) {
-				page.redirect( '/connect' );
-			}
-		} );
+		if ( config.isEnabled( 'oauth' ) ) {
+			page.redirect( '/connect' );
+		}
 	}
 }
 
@@ -59,32 +54,30 @@ const waitForSite = ( siteId ) => async ( dispatch, getState ) => {
 	}
 };
 
-function setupLoggedIn() {
-	page( '/', async ( context ) => {
-		// determine the primary site ID (it's a property of "current user" object) and then
-		// ensure that the primary site info is loaded into Redux before proceeding.
-		const primarySiteId = getPrimarySiteId( context.store.getState() );
-		await context.store.dispatch( waitForSite( primarySiteId ) );
+async function handleLoggedIn( context ) {
+	// determine the primary site ID (it's a property of "current user" object) and then
+	// ensure that the primary site info is loaded into Redux before proceeding.
+	const primarySiteId = getPrimarySiteId( context.store.getState() );
+	await context.store.dispatch( waitForSite( primarySiteId ) );
 
-		const state = context.store.getState();
-		const siteSlug = getSiteSlug( state, primarySiteId );
-		const isCustomerHomeEnabled = canCurrentUserUseCustomerHome( state, primarySiteId );
+	const state = context.store.getState();
+	const siteSlug = getSiteSlug( state, primarySiteId );
+	const isCustomerHomeEnabled = canCurrentUserUseCustomerHome( state, primarySiteId );
 
-		let redirectPath;
+	let redirectPath;
 
-		if ( ! siteSlug ) {
-			// there is no primary site or the site info couldn't be fetched. Redirect to Reader.
-			redirectPath = '/read';
-		} else if ( isCustomerHomeEnabled ) {
-			redirectPath = `/home/${ siteSlug }`;
-		} else {
-			redirectPath = `/stats/${ siteSlug }`;
-		}
+	if ( ! siteSlug ) {
+		// there is no primary site or the site info couldn't be fetched. Redirect to Reader.
+		redirectPath = '/read';
+	} else if ( isCustomerHomeEnabled ) {
+		redirectPath = `/home/${ siteSlug }`;
+	} else {
+		redirectPath = `/stats/${ siteSlug }`;
+	}
 
-		if ( context.querystring ) {
-			redirectPath += `?${ context.querystring }`;
-		}
+	if ( context.querystring ) {
+		redirectPath += `?${ context.querystring }`;
+	}
 
-		page.redirect( redirectPath );
-	} );
+	page.redirect( redirectPath );
 }
