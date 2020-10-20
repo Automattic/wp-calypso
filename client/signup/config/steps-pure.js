77d224954039e7e32aec1bcef75b0bf85c332b6b
Veselin Nikolov <veselin@automattic.com>
2020-10-20 16:46:53 +0300

    Rename variable as requested

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -328,7 +328,7 @@ export function generateSteps( {
 		'secure-your-brand': {
 			stepName: 'secure-your-brand',
 			dependencies: [ 'domainItem', 'siteSlug' ],
-			providesDependencies: [ 'cartItems' ],
+			providesDependencies: [ 'domainUpsellItems' ],
 			fulfilledStepCallback: isSecureYourBrandFulfilled,
 		},
 
