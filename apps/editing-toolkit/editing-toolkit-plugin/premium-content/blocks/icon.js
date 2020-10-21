023540b173bae214d5980412f3f6fac502ede57e
Andy Peatling <apeatling@users.noreply.github.com>
2020-10-21 11:14:51 -0700

    Premium Content Block: Remove the intermediate block UI. (#46619)
    * Remove the intermediate block UI.
    
    * Add dependencies to useEffect hook.

---
+++ apps/editing-toolkit/editing-toolkit-plugin/premium-content/blocks/icon.js
@@ -0,0 +1,15 @@
+export default (
+	<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M12.7439 14.4271L8.64053 13.165L8.51431 13.8718L8.09208 20.7415C8.06165 21.2365 8.61087 21.5526 9.02363 21.2776L12.7439 18.799L16.7475 21.304C17.1687 21.5676 17.7094 21.2343 17.6631 20.7396L17.0212 13.8718L17.0212 13.165L12.7439 14.4271Z"
+			fill="black"
+		/>
+		<circle cx="12.7439" cy="8.69796" r="5.94466" stroke="black" strokeWidth="1.5" fill="none" />
+		<path
+			d="M9.71023 8.12461L11.9543 10.3687L15.7776 6.54533"
+			stroke="black"
+			strokeWidth="1.5"
+			fill="none"
+		/>
+	</svg>
+);
