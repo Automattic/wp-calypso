fd55e78486b1fe12bcb139c791798750f1b61077
Jonathan (JB) Belcher <belcherj@gmail.com>
2020-10-21 11:05:37 -0400

    Update comment and variable names

--- client/layout/utils.ts
+++ client/layout/utils.ts
@@ -1,16 +1,16 @@
 const HOUR_IN_MS = 60 * 60 * 1000;
 
 /**
- * Returns false if the site is unlaunched or is younger than 1 week
+ * Returns false if the site is unlaunched or is younger than 1 hour
  *
  * @param site the site object
  */
 export function getShouldShowAppBanner( site: any ): boolean {
 	if ( site && site.options ) {
-		const olderThanAWeek = Date.now() - new Date( site.options.created_at ).getTime() > HOUR_IN_MS;
+		const olderThanAnHour = Date.now() - new Date( site.options.created_at ).getTime() > HOUR_IN_MS;
 		const isLaunched = site.launch_status !== 'unlaunched';
 
-		return olderThanAWeek && isLaunched;
+		return olderThanAnHour && isLaunched;
 	}
 	return false;
 }
