395263397600ff9d335ee98096dbd1edd9577779
nbloomf <nathan.bloomfield@automattic.com>
2020-10-21 16:53:20 -0500

    Delete the unused upcomingCharges controller

--- client/me/billing-history/controller.js
+++ client/me/billing-history/controller.js
@@ -4,16 +4,10 @@
 
 import React from 'react';
 import BillingHistoryComponent from './main';
-import UpcomingChargesComponent from './upcoming-charges';
 import Receipt from './receipt';
 
 export function billingHistory( context, next ) {
 	context.primary = React.createElement( BillingHistoryComponent );
-	next();
-}
-
-export function upcomingCharges( context, next ) {
-	context.primary = React.createElement( UpcomingChargesComponent );
 	next();
 }
 
