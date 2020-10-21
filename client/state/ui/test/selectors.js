16f8476c2789d8fac8f394972a0b8bfa17412743
cpap <papazoglou.charalampos@gmail.com>
2020-10-21 11:01:44 +0300

    Remove stale test

--- client/state/ui/test/selectors.js
+++ client/state/ui/test/selectors.js
@@ -243,35 +243,6 @@ describe( 'selectors', () => {
 		} );
 	} );
 
-	describe( '#isSectionIsomorphic()', () => {
-		test( 'should return false if there is no section currently selected', () => {
-			const selected = isSectionIsomorphic( {
-				ui: {
-					section: false,
-				},
-			} );
-
-			expect( selected ).to.be.false;
-		} );
-
-		test( 'should return true if current section is isomorphic', () => {
-			const section = {
-				enableLoggedOut: true,
-				group: 'sites',
-				isomorphic: true,
-				module: 'my-sites/themes',
-				name: 'themes',
-				paths: [ '/themes' ],
-			};
-
-			const selected = isSectionIsomorphic( {
-				ui: { section },
-			} );
-
-			expect( selected ).to.be.true;
-		} );
-	} );
-
 	describe( '#getSidebarIsCollapsed()', () => {
 		test( 'should return false', () => {
 			const selected = getSidebarIsCollapsed( {
