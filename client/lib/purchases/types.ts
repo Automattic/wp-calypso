e6682806eeb1327d405be5bcc2c3c1a536546c52
Michael Cain <michael@michaeldcain.com>
2020-10-22 14:52:49 -0400

    Add types to MembershipItem

--- client/lib/purchases/types.ts
+++ client/lib/purchases/types.ts
@@ -14,3 +14,18 @@ export interface Purchase {
 	siteId: number;
 	subscribedDate: string;
 }
+
+export interface MembershipSubscription {
+	ID: string;
+	currency: string;
+	end_date?: string;
+	product_id: string;
+	renew_interval?: null;
+	renewal_price: string;
+	site_id: string;
+	site_title: string;
+	site_url: string;
+	start_date: string;
+	status: string;
+	title: string;
+}
--- client/me/purchases/membership-item/index.tsx
+++ client/me/purchases/membership-item/index.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import { useTranslate } from 'i18n-calypso';
+import { useTranslate, LocalizeProps } from 'i18n-calypso';
 import formatCurrency from '@automattic/format-currency';
 
 /**
@@ -11,6 +11,7 @@ import formatCurrency from '@automattic/format-currency';
 import { CompactCard } from '@automattic/components';
 import Gridicon from 'calypso/components/gridicon';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import { MembershipSubscription } from 'calypso/lib/purchases/types';
 
 /**
  * Style dependencies
@@ -22,26 +23,26 @@ const getMembershipTerms = ( {
 	translate,
 	moment,
 }: {
-	subscription: any;
-	translate: any;
+	subscription: MembershipSubscription;
+	translate: LocalizeProps[ 'translate' ];
 	moment: any;
 } ) => {
 	/* $5 - never expires. */
 	if ( subscription.end_date === null ) {
 		return translate( '%(amount)s - never expires.', {
 			args: {
-				amount: formatCurrency( subscription.renewal_price, subscription.currency ),
+				amount: formatCurrency( Number( subscription.renewal_price ), subscription.currency ),
 			},
 		} );
 	}
 
 	/* Renews every month for $5. Next renewal on November 22, 2020. */
-	if ( subscription.renewal_interval ) {
+	if ( subscription.renew_interval ) {
 		return translate( 'Renews every %(interval)s for %(amount)s. Next renewal on %(date)s.', {
 			args: {
-				interval: subscription.renewal_interval,
-				amount: formatCurrency( subscription.renewal_price, subscription.currency ),
-				date: moment( subscription.endDate ).format( 'LL' ),
+				interval: subscription.renew_interval,
+				amount: formatCurrency( Number( subscription.renewal_price ), subscription.currency ),
+				date: moment( subscription.end_date ).format( 'LL' ),
 			},
 		} );
 	}
@@ -50,13 +51,17 @@ const getMembershipTerms = ( {
 	/* I'm not sure we can have a renewal without an interval, so this might not get called. */
 	return translate( 'Renews at %(amount)s on %(date)s.', {
 		args: {
-			amount: formatCurrency( subscription.renewal_price, subscription.currency ),
-			date: moment( subscription.endDate ).format( 'LL' ),
+			amount: formatCurrency( Number( subscription.renewal_price ), subscription.currency ),
+			date: moment( subscription.end_date ).format( 'LL' ),
 		},
 	} );
 };
 
-export default function MembershipItem( { subscription }: { subscription: any } ): JSX.Element {
+export default function MembershipItem( {
+	subscription,
+}: {
+	subscription: MembershipSubscription;
+} ): JSX.Element {
 	const translate = useTranslate();
 	const moment = useLocalizedMoment();
 
