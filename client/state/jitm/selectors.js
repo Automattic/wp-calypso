e6bbc77931683e0ed292f26bdee9cad044f996fc
Dave Smith <getdavemail@gmail.com>
2020-10-22 14:11:41 +0100

    Update JITM selectors to provide better defaults and utilise JITM cache

--- client/state/jitm/selectors.js
+++ client/state/jitm/selectors.js
@@ -4,7 +4,7 @@
 import { get } from 'lodash';
 
 /** Internal dependencies */
-import { getSelectedSiteId } from 'state/ui/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 /**
  * Get the list of available jitms for the current site/section
@@ -14,7 +14,7 @@ import { getSelectedSiteId } from 'state/ui/selectors';
  * @returns {Array} An array of jitms
  */
 export const getJITM = ( state, messagePath ) =>
-	get( state, [ 'jitm', 'sitePathJITM', messagePath + getSelectedSiteId( state ) ], [] );
+	get( state, [ 'jitm', 'sitePathJITM', messagePath + getSelectedSiteId( state ) ], null );
 
 /**
  * Get the top jitm available for the current site/section
@@ -26,9 +26,18 @@ export const getJITM = ( state, messagePath ) =>
 export const getTopJITM = ( state, messagePath ) => {
 	const jitms = getJITM( state, messagePath );
 
-	if ( jitms.length === 0 ) {
+	// If JITMs haven't been populated from network for given messagePath + siteId
+	// then fallback to the most recent JITM for the given siteId.
+	if ( null === jitms ) {
+		return state?.jitm?.sitePathJITMCache[ getSelectedSiteId( state ) ] || null;
+	}
+
+	// If JITMs have been populated for the given messagePath + siteId and they are
+	// genuinely empty (ie: no messages) then legitimately return null to signify this.
+	if ( ! jitms.length ) {
 		return null;
 	}
 
+	// Otherwise default to returning the top JITM
 	return jitms[ 0 ];
 };
