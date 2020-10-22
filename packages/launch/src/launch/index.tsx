1051d7e15c0109d6f0d2a019366cbb583d454556
Marco Ciampini <marco.ciampini@automattic.com>
2020-10-22 16:19:07 +0200

    Initial version of Focused Launch Flow modal

--- packages/launch/src/launch/index.tsx
+++ packages/launch/src/launch/index.tsx
@@ -1,32 +1,60 @@
 /**
  * External dependencies
  */
-import React from 'react';
-import { useI18n } from '@automattic/react-i18n';
-import { Button } from '@wordpress/components';
-import { Icon, close } from '@wordpress/icons';
-import classnames from 'classnames';
+import * as React from 'react';
+import { __ } from '@wordpress/i18n';
+import { Modal, Button } from '@wordpress/components';
+import { Icon, wordpress, close } from '@wordpress/icons';
+import FocusedLaunch from '../focused-launch';
 
-/**
- * Style dependencies
- */
-import './style.scss';
+import './styles.scss';
 
 interface Props {
-	onClose?: () => void;
+	onClose: () => void;
 }
 
-const Launch: React.FunctionComponent< Props > = ( { onClose } ) => {
-	const { __ } = useI18n();
-
+const FocusedLaunchModal: React.FunctionComponent< Props > = ( { onClose } ) => {
 	return (
-		<div className={ classnames( 'nux-launch' ) }>
-			<p>{ __( 'Launch' ) }</p>
-			<Button onClick={ onClose }>
-				<Icon icon={ close } size={ 24 } />
-			</Button>
-		</div>
+		<Modal
+			open={ true }
+			className="focused-launch-modal"
+			overlayClassName="focused-launch-modal-overlay"
+			bodyOpenClassName="has-focused-launch-modal"
+			onRequestClose={ onClose }
+			title=""
+		>
+			<>
+				<div className="focused-launch-modal-body">
+					<div className="focused-launch-modal-header">
+						<div className="focused-launch-modal-header__wp-logo">
+							<Icon icon={ wordpress } size={ 36 } />
+						</div>
+						<Button
+							isLink
+							className="focused-launch-modal__close-button"
+							onClick={ onClose }
+							aria-label={ __( 'Close dialog', 'full-site-editing' ) }
+							disabled={ ! onClose }
+						>
+							<span>
+								<Icon icon={ close } size={ 24 } />
+							</span>
+						</Button>
+					</div>
+					<div className="focused-launch-body">
+						<div className="focused-launch-inputs">
+							<FocusedLaunch />
+						</div>
+						<div className="focused-launch-modal-aside">
+							<div>
+								<strong>46.9%</strong> of globally registered domains are <strong>.com</strong>
+							</div>
+						</div>
+					</div>
+				</div>
+			</>
+		</Modal>
 	);
 };
 
-export default Launch;
+export default FocusedLaunchModal;
