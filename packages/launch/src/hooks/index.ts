9f123f44eb78d356473fecea7cc7120ef003f5f8
Marco Ciampini <marco.ciampini@automattic.com>
2020-10-22 16:17:15 +0200

    Copy hooks folder from editing toolkit to launch package
    Original path: apps/editing-toolkit/editing-toolkit-plugin/editor-site-launch/src/hooks
    Copy path: packages/launch/src/hooks

---
+++ packages/launch/src/hooks/index.ts
@@ -0,0 +1,8 @@
+// this whole folder is a copy of apps/editing-toolkit/editing-toolkit-plugin/editor-site-launch/src/hooks
+// TODO: avoid code duplication
+
+export * from './use-site';
+export * from './use-on-launch';
+export * from './use-domain-suggestion';
+export * from './use-domain-search';
+export * from './use-title';
