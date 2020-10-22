4042bed4e5439db72916a61674f5b4a5d2224bcb
Marco Ciampini <marco.ciampini@automattic.com>
2020-10-22 16:14:35 +0200

    Remove EditorLaunchModal block and related code / feature flag

--- client/gutenberg/editor/calypsoify-iframe.tsx
+++ client/gutenberg/editor/calypsoify-iframe.tsx
@@ -694,7 +694,6 @@ class CalypsoifyIframe extends Component<
 
 		const isUsingClassicBlock = !! classicBlockEditorId;
 		const isCheckoutOverlayEnabled = config.isEnabled( 'post-editor/checkout-overlay' );
-		const isLaunchOverlayEnabled = config.isEnabled( 'post-editor/launch-overlay' );
 
 		return (
 			<Fragment>
@@ -741,7 +740,6 @@ class CalypsoifyIframe extends Component<
 						isOpen
 					/>
 				) }
-				{ isLaunchOverlayEnabled && <AsyncLoad require="calypso/blocks/editor-launch-modal" /> }
 				<EditorRevisionsDialog loadRevision={ this.loadRevision } />
 				<WebPreview
 					externalUrl={ postUrl }
