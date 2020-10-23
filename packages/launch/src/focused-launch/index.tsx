2b0facccfbc212de337b38992594f2eccdc2a801
Omar Alshaker <omar@omaralshaker.com>
2020-10-23 18:10:14 +0200

    Clean up HTML and implement steps logic

--- packages/launch/src/focused-launch/index.tsx
+++ packages/launch/src/focused-launch/index.tsx
@@ -11,6 +11,10 @@ import { useSite, useDomainSearch } from '../hooks';
 
 import './styles.scss';
 
+function noop( ...args: unknown[] ) {
+	return args;
+}
+
 const FocusedLaunch: React.FunctionComponent = () => {
 	const { getSite } = useSelect( ( select ) => select( 'core' ) ) as any;
 	const title = getSite()?.title;
@@ -22,7 +26,7 @@ const FocusedLaunch: React.FunctionComponent = () => {
 	const domainSearch = useDomainSearch();
 
 	return (
-		<>
+		<div className="focused-launch__container">
 			<div className="focused-launch__section">
 				<Title>{ __( "You're almost there", 'launch' ) }</Title>
 				<p className="focused-launch__caption">
@@ -32,36 +36,56 @@ const FocusedLaunch: React.FunctionComponent = () => {
 					) }
 				</p>
 			</div>
-			<div className="focused-launch__section">
-				<TextControl
-					className="focused-launch__input"
-					label={ __( '1. Name your site', 'launch' ) }
-					value={ siteTitle }
-					onChange={ ( value ) => setSiteTitle( value ) }
-				/>
+			<div className="focused-launch__step">
+				<div className="focused-launch__data-input">
+					<div className="focused-launch__section">
+						<TextControl
+							className="focused-launch__input"
+							label={ __( '1. Name your site', 'launch' ) }
+							value={ siteTitle }
+							onChange={ ( value ) => setSiteTitle( value ) }
+							// eslint-disable-next-line jsx-a11y/no-autofocus
+							autoFocus={ true }
+						/>
+					</div>
+					<div className="focused-launch__section">
+						<DomainPicker
+							header={
+								<label className="focused-launch__label">
+									{ __( '2. Confirm your domain', 'launch' ) }
+								</label>
+							}
+							existingSubdomain={ siteDomainName }
+							currentDomain={ siteDomainName }
+							onDomainSelect={ setSiteTitle }
+							initialDomainSearch={ domainSearch }
+							showSearchField={ false }
+							analyticsFlowId="focused-launch"
+							analyticsUiAlgo="focused_launch_domain_picker"
+							onDomainSearchBlur={ () => noop( 'TODO: on domain search blur' ) }
+							onSetDomainSearch={ () => noop( 'TODO: on set domain search' ) }
+						/>
+					</div>
+				</div>
+				<div className="focused-launch__commentary">
+					<div>
+						<strong>46.9%</strong> of globally registered domains are <strong>.com</strong>
+					</div>
+				</div>
 			</div>
-			<div className="focused-launch__section">
-				<DomainPicker
-					header={
-						<label className="focused-launch__label">
-							{ __( '2. Confirm your domain', 'launch' ) }
-						</label>
-					}
-					existingSubdomain={ siteDomainName }
-					currentDomain={ siteDomainName }
-					onDomainSelect={ setSiteTitle }
-					initialDomainSearch={ domainSearch }
-					showSearchField={ false }
-					analyticsFlowId="focused-launch"
-					analyticsUiAlgo="focused_launch_domain_picker"
-					onDomainSearchBlur={ () => console.log( 'TODO: on domain search blur' ) }
-					onSetDomainSearch={ () => console.log( 'TODO: on set domain search' ) }
-				/>
+			<div className="focused-launch__step">
+				<div className="focused-launch__data-input">
+					<label className="focused-launch__label">
+						{ __( '3. Confirm your plan', 'launch' ) }
+					</label>
+				</div>
+				<div className="focused-launch__commentary">
+					<div>
+						<strong>Some</strong> other interesting info
+					</div>
+				</div>
 			</div>
-			<div className="focused-launch__section">
-				<label className="focused-launch__label">{ __( '3. Confirm your plan', 'launch' ) }</label>
-			</div>
-		</>
+		</div>
 	);
 };
 
