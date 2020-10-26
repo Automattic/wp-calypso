50607660639f01f713b30282d608f76f6743a876
Payton Swick <payton@foolord.com>
2020-10-26 12:26:32 -0400

    Throw error if missing required props from createRequestCartProduct

--- packages/shopping-cart/src/create-request-cart-product.ts
+++ packages/shopping-cart/src/create-request-cart-product.ts
@@ -7,6 +7,9 @@ export default function createRequestCartProduct(
 	properties: Partial< RequestCartProduct > &
 		Pick< RequestCartProduct, 'product_slug' | 'product_id' >
 ): RequestCartProduct {
+	if ( ! properties.product_slug || ! properties.product_id ) {
+		throw new Error( 'Both product_slug and product_id are required for createRequestCartProduct' );
+	}
 	return {
 		meta: '',
 		volume: 1,
