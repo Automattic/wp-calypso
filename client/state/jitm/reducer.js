cc0eb7fe7ce15e3f7addff888a2998ab5254166a
Matthew Reishus <mreishus@users.noreply.github.com>
2020-10-27 09:50:24 -0500

    Revert "Cache latest JITMs in state to reduce SiteNotice and JITM component rerenders (#46677)"
    This reverts commit 4a96dff11c5dd8120a5a05448c84ed8d923d1e21.

--- client/state/jitm/reducer.js
+++ client/state/jitm/reducer.js
@@ -6,22 +6,8 @@ import { combineReducers, keyedReducer } from 'calypso/state/utils';
 
 export const storeJITM = ( state = {}, { type, jitms } ) => ( type === JITM_SET ? jitms : state );
 
-const jitmCache = ( state = null, { type, jitms } ) => {
-	if ( type === JITM_SET ) {
-		if ( ! jitms || ! jitms.length ) {
-			return null;
-		}
-		// Cache the first item only
-		return jitms[ 0 ];
-	}
-	return state;
-};
-
-const sitePathJITMCache = keyedReducer( 'siteId', jitmCache );
-
 const sitePathJITM = keyedReducer( 'keyedPath', storeJITM );
 
 export default combineReducers( {
 	sitePathJITM,
-	sitePathJITMCache,
 } );
