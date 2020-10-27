dad9bb022c4e2c07041665e4b84fda0e6293eb26
Payton Swick <payton@foolord.com>
2020-10-27 16:01:02 -0400

    Fix return type of getCountries selector

--- client/state/selectors/get-countries.js
+++ client/state/selectors/get-countries.js
@@ -13,7 +13,7 @@ import 'calypso/state/countries/init';
  *
  * @param {object} state - global state tree
  * @param {string} type - type of list ('domains, 'payments', or 'sms)
- * @returns {Array?} the list of countries
+ * @returns {null|({code: string, name: string})[]} the list of countries
  */
 export default function getCountries( state, type ) {
 	return get( state, [ 'countries', type ], null );
