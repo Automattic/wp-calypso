c452eebbef1f7cd590a93eb1338b19f253459518
Markus Dobmann <m.do@posteo.net>
2020-10-28 13:58:54 +0100

    adjust copy on edit media failure notice
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>

--- client/state/data-layer/wpcom/sites/media/index.js
+++ client/state/data-layer/wpcom/sites/media/index.js
@@ -72,7 +72,7 @@ export const updateMediaSuccess = ( { siteId }, mediaItem ) => ( dispatch ) => {
 
 export const updateMediaError = ( { siteId, originalMediaItem } ) => [
 	receiveMedia( siteId, originalMediaItem ),
-	errorNotice( translate( "We weren't able to process this media item." ), {
+	errorNotice( translate( "We were unable to process this media item." ), {
 		id: `update-media-notice-${ originalMediaItem.ID }`,
 	} ),
 ];
