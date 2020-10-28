889d552ceb18c4524bf7c500815140bea562336c
elliottprogrammer <bryan@elliottprogrammer.com>
2020-10-28 09:01:30 -0700

    Rebase & implement JetpackProductSlideOutCard.

--- client/components/jetpack/card/jetpack-product-card-alt-2/features-product-slide-out.tsx
+++ client/components/jetpack/card/jetpack-product-card-alt-2/features-product-slide-out.tsx
@@ -10,7 +10,7 @@ import { useTranslate } from 'i18n-calypso';
  * Internal dependencies
  */
 import getSelectedSiteId from 'calypso/state/ui/selectors/get-selected-site-id';
-//import JetpackProductSlideOutCard from 'calypso/components/jetpack/card/jetpack-product-slide-out-card';
+import JetpackProductSlideOutCard from 'calypso/components/jetpack/card/jetpack-product-slide-out-card';
 import useItemPrice from 'calypso/my-sites/plans-v2/use-item-price';
 import { durationToText } from 'calypso/my-sites/plans-v2/utils';
 import { getCurrentUserCurrencyCode } from 'calypso/state/current-user/selectors';
@@ -56,45 +56,16 @@ const FeaturesProductSlideOut: FunctionComponent< Props > = ( {
 	} );
 
 	return product ? (
-		<>
-			{ /* This markup is just for testing.  See note below. */ }
-			<div style={ { fontSize: '0.825rem' } }>
-				<div>
-					<strong>Icon Slug:</strong> { product.iconSlug }
-				</div>
-				<div>
-					<strong>Product Name:</strong> { product.displayName }
-				</div>
-				<div>
-					<strong>CurrencyCode:</strong> { currencyCode }
-				</div>
-				<div>
-					<strong>Price:</strong> { productPrice }
-				</div>
-				<div>
-					<strong>BillingTimeFrame:</strong> { billingTimeFrame }
-				</div>
-				<div>
-					<strong>Button Label:</strong> { slideOutButtonLabel }
-				</div>
-				<div>
-					<strong>onButtonClick:</strong> { onProductClick.toString() }
-				</div>
-			</div>
-			{ /*
-			 * The JetpackSlideOutCard Component is not available in this branch.  Once this branch is merged,
-			 * we can remove the markup above and uncomment the JetpackProductSlideOutCard component below.
-			 */ }
-			{ /* <JetpackProductSlideOutCard
-                iconSlug={ product.iconSlug }
-                productName={ product.displayName }
-                currencyCode={ currencyCode }
-                price={ discountedPrice }
-                billingTimeFrame={ billingTimeFrame }
-                buttonLabel={ slideOutButtonLabel }
-                onButtonClick={ () => onProductClick(product, false, true) }
-            /> */ }
-		</>
+		<JetpackProductSlideOutCard
+			iconSlug={ product.iconSlug }
+			productName={ product.displayName }
+			currencyCode={ currencyCode }
+			price={ productPrice }
+			billingTimeFrame={ billingTimeFrame }
+			description={ product?.description }
+			buttonLabel={ slideOutButtonLabel }
+			onButtonClick={ () => onProductClick( product, false, true ) }
+		/>
 	) : null;
 };
 
