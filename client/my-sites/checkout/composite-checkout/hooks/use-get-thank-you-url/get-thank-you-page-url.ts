29a2f76f636fd791140f527bae1edc682cc7176b
Veselin Nikolov <veselin@automattic.com>
2020-10-28 13:51:56 +0200

    The upgrade slug for monthly plan should be business-monthly
    Providing an annual slug for a monthly plan broke the code that
    identifies the upgrade path

--- client/my-sites/checkout/composite-checkout/hooks/use-get-thank-you-url/get-thank-you-page-url.ts
+++ client/my-sites/checkout/composite-checkout/hooks/use-get-thank-you-url/get-thank-you-page-url.ts
@@ -23,6 +23,7 @@ import {
 	hasPremiumPlan,
 	hasBusinessPlan,
 	hasEcommercePlan,
+	hasMonthlyCartItem,
 } from 'calypso/lib/cart-values/cart-items';
 import { managePurchase } from 'calypso/me/purchases/paths';
 import { isValidFeatureKey } from 'calypso/lib/plans/features-list';
@@ -291,7 +292,8 @@ function maybeShowPlanBumpOffer( {
 		return;
 	}
 	if ( hasPremiumPlan( cart ) && ! isTransactionResultEmpty && ! didPurchaseFail ) {
-		return `/checkout/${ siteSlug }/offer-plan-upgrade/business/${ pendingOrReceiptId }`;
+		const upgradeItem = hasMonthlyCartItem( cart ) ? 'business-monthly' : 'business';
+		return `/checkout/${ siteSlug }/offer-plan-upgrade/${ upgradeItem }/${ pendingOrReceiptId }`;
 	}
 	return;
 }
