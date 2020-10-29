401ce28e959af19c8c1f3f3c2178e64adedbe1dc
Renzo Canepa <rcanepag@gmail.com>
2020-10-29 17:02:25 -0300

    Include Jetpack Scan status in the check

--- client/components/jetpack/has-jetpack-connection-failed-switch/index.tsx
+++ client/components/jetpack/has-jetpack-connection-failed-switch/index.tsx
@@ -8,9 +8,22 @@ import { useSelector } from 'react-redux';
  * Internal dependencies
  */
 import getSelectedSiteId from 'calypso/state/ui/selectors/get-selected-site-id';
-// import getRewindState from 'calypso/state/selectors/get-rewind-state';
+import getRewindState from 'calypso/state/selectors/get-rewind-state';
+import QueryJetpackScan from 'calypso/components/data/query-jetpack-scan';
 import QueryRewindState from 'calypso/components/data/query-rewind-state';
+import getSiteScanState from 'calypso/state/selectors/get-site-scan-state';
 import RenderSwitch from 'calypso/components/jetpack/render-switch';
+
+const stateImpliesJetpackIsDisconnected = ( productState?: {
+	state?: string;
+	reason?: string;
+} ) => {
+	if ( ! productState ) {
+		return undefined;
+	}
+
+	return productState.code === 'no_connected_jetpack' && productState.status === 412;
+};
 
 const isInitialized = ( productState: { state?: string } | null ) =>
 	productState && productState.state !== 'uninitialized';
@@ -21,38 +34,30 @@ const HasJetpackConnectionFailedSwitch: React.FC< Props > = ( {
 	falseComponent,
 } ) => {
 	const siteId = useSelector( getSelectedSiteId );
-	// const rewindState = useSelector( ( state ) => getRewindState( state, siteId ) );
-	const rewindState = {
-		code: 'no_connected_jetpack',
-		status: 412,
-	};
+	const rewindState = useSelector( ( state ) => getRewindState( state, siteId ) );
+	const scanState = useSelector( ( state ) => getSiteScanState( state, siteId ) );
 
-	const hasJetpackConnectionFailed = useCallback( () => {
-		if ( ! rewindState ) {
-			return undefined;
-		}
-
-		const rewindShowsJetpackIsDisconnected =
-			rewindState.code === 'no_connected_jetpack' && rewindState.status === 412;
-
-		return rewindShowsJetpackIsDisconnected;
-	}, [ rewindState ] );
+	const isJetpackDisconnected = useCallback(
+		() => [ rewindState, scanState ].some( stateImpliesJetpackIsDisconnected ),
+		[ rewindState, scanState ]
+	);
 
 	const isLoading = useCallback( () => {
-		if ( hasJetpackConnectionFailed() ) {
+		if ( isJetpackDisconnected() ) {
 			return false;
 		}
 
-		return [ rewindState ].some( ( state ) => ! isInitialized( state ) );
-	}, [ hasJetpackConnectionFailed, rewindState ] );
+		return [ rewindState, scanState ].some( ( state ) => ! isInitialized( state ) );
+	}, [ isJetpackDisconnected, rewindState, scanState ] );
 
 	return (
 		<RenderSwitch
 			loadingCondition={ isLoading }
-			renderCondition={ hasJetpackConnectionFailed }
+			renderCondition={ isJetpackDisconnected }
 			queryComponent={
 				<>
 					<QueryRewindState siteId={ siteId } />
+					<QueryJetpackScan siteId={ siteId } />
 				</>
 			}
 			loadingComponent={ loadingComponent }
