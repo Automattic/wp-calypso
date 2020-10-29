833c73e5e79cd3f3f11938ea2fdc8cb88a608cc3
Philip Jackson <p-jackson@live.com>
2020-10-29 15:31:19 +1300

    Update camelCase algorithm to one used by dependency-extraction-webpack-plugin

--- test/e2e/specs/wp-gutenberg-experimental-features-spec.js
+++ test/e2e/specs/wp-gutenberg-experimental-features-spec.js
@@ -3,7 +3,6 @@
  */
 import assert from 'assert';
 import config from 'config';
-import _ from 'lodash';
 
 /**
  * Internal dependencies
@@ -31,6 +30,19 @@ const EXPERIMENTAL_FEATURES = {
 	],
 };
 
+/**
+ * Given a string, returns a new string with dash separators converted to
+ * camelCase equivalent. This is not as aggressive as `_.camelCase` in
+ * converting to uppercase, where Lodash will also capitalize letters
+ * following numbers.
+ *
+ * @param {string} string Input dash-delimited string.
+ * @returns {string} Camel-cased string.
+ */
+function camelCaseDash( string ) {
+	return string.replace( /-([a-z])/g, ( _, letter ) => letter.toUpperCase() );
+}
+
 let driver;
 
 before( async function () {
@@ -47,8 +59,9 @@ describe( `[${ host }] Experimental features we depend on are available (${ scre
 	} );
 
 	for ( const [ packageName, features ] of Object.entries( EXPERIMENTAL_FEATURES ) ) {
-		// Remove the `@wordpress/` prefix and hyphens from package name
-		const wpGlobalName = _.camelCase( packageName.substr( '@wordpress/'.length ) );
+		// Removes the `@wordpress/` prefix and hyphens from package name
+		// The algorithm WP uses to convert package names to variable names is here: https://github.com/WordPress/gutenberg/blob/a03ea51e11a36d0abeecb4ce4e4cea5ffebdffc5/packages/dependency-extraction-webpack-plugin/lib/util.js#L40-L45
+		const wpGlobalName = camelCaseDash( packageName.substr( '@wordpress/'.length ) );
 
 		describe( packageName, () => {
 			step(
