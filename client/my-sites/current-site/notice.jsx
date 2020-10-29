db3e7c8e8151074b203a2282a6e3668dd15def7a
Jarda Snajdr <jsnajdr@gmail.com>
2020-10-29 14:49:03 +0100

    Don't show placeholder when loading JITMs (#46837)

--- client/my-sites/current-site/notice.jsx
+++ client/my-sites/current-site/notice.jsx
@@ -275,6 +275,7 @@ export class SiteNotice extends React.Component {
 				{ showJitms && (
 					<AsyncLoad
 						require="calypso/blocks/jitm"
+						placeholder={ null }
 						messagePath="calypso:sites:sidebar_notice"
 						template="sidebar-banner"
 					/>
