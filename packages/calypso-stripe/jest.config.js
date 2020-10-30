9114e2d813666484e4da70c275ef5f0c7d5c5cb0
Payton Swick <payton@foolord.com>
2020-10-30 12:23:05 -0400

    Add Calypso Stripe package

---
+++ packages/calypso-stripe/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	preset: '@automattic/calypso-build',
+	rootDir: __dirname,
+	testEnvironment: 'jsdom',
+	globals: { window: { navigator: { userAgent: 'jest' } } },
+	cacheDirectory: '<rootDir>/../../.cache/jest',
+};
