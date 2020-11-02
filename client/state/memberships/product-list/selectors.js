8cbd1e92b508d738486df9ff9dbc28a53ac9649f
Marin Atanasov <tyxla@abv.bg>
2020-11-02 14:30:40 +0200

    State: Fix default membership products state

--- client/state/memberships/product-list/selectors.js
+++ client/state/memberships/product-list/selectors.js
@@ -1,13 +1,8 @@
 /**
- * External dependencies
- */
-import { get } from 'lodash';
-
-/**
  * Internal dependencies
  */
 import 'calypso/state/memberships/init';
 
 export function getProductsForSiteId( state, siteId ) {
-	return get( state, [ 'memberships', 'productList', 'items', siteId ] );
+	return state.memberships?.productList.items[ siteId ] ?? [];
 }
