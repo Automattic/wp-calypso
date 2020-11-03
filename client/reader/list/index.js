06a32e0ef4fccbc90a6ca0661d729fde100d162f
Chris R <chris.rosser@automattic.com>
2020-11-04 11:45:13 +1300

    Hide list editing routes behind feature flag (#47074)

--- client/reader/list/index.js
+++ client/reader/list/index.js
@@ -16,44 +16,47 @@ import {
 } from './controller';
 import { sidebar, updateLastRoute } from 'calypso/reader/controller';
 import { makeLayout, render as clientRender } from 'calypso/controller';
+import config from 'calypso/config';
 
 export default function () {
-	page(
-		'/read/list/:user/:list/edit/items',
-		updateLastRoute,
-		sidebar,
-		editListItems,
-		makeLayout,
-		clientRender
-	);
-	page(
-		'/read/list/:user/:list/edit',
-		updateLastRoute,
-		sidebar,
-		editList,
-		makeLayout,
-		clientRender
-	);
+	if ( config.isEnabled( 'reader/list-management' ) ) {
+		page(
+			'/read/list/:user/:list/edit/items',
+			updateLastRoute,
+			sidebar,
+			editListItems,
+			makeLayout,
+			clientRender
+		);
+		page(
+			'/read/list/:user/:list/edit',
+			updateLastRoute,
+			sidebar,
+			editList,
+			makeLayout,
+			clientRender
+		);
 
-	page( '/read/list/new', updateLastRoute, sidebar, createList, makeLayout, clientRender );
+		page( '/read/list/new', updateLastRoute, sidebar, createList, makeLayout, clientRender );
 
-	page(
-		'/read/list/:user/:list/export',
-		updateLastRoute,
-		sidebar,
-		exportList,
-		makeLayout,
-		clientRender
-	);
+		page(
+			'/read/list/:user/:list/export',
+			updateLastRoute,
+			sidebar,
+			exportList,
+			makeLayout,
+			clientRender
+		);
 
-	page(
-		'/read/list/:user/:list/delete',
-		updateLastRoute,
-		sidebar,
-		deleteList,
-		makeLayout,
-		clientRender
-	);
+		page(
+			'/read/list/:user/:list/delete',
+			updateLastRoute,
+			sidebar,
+			deleteList,
+			makeLayout,
+			clientRender
+		);
+	}
 
 	page( '/read/list/:user/:list', updateLastRoute, sidebar, listListing, makeLayout, clientRender );
 }
