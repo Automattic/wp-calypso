dd76283c0982a997f95a1fb201e97a164775570a
Payton Swick <payton@foolord.com>
2020-11-04 17:23:58 -0500

    Change WPContactForm to use contactDetailsType

--- client/my-sites/checkout/composite-checkout/components/wp-contact-form.tsx
+++ client/my-sites/checkout/composite-checkout/components/wp-contact-form.tsx
@@ -9,17 +9,16 @@ import {
 	useFormStatus,
 	useIsStepActive,
 } from '@automattic/composite-checkout';
-import { useShoppingCart } from '@automattic/shopping-cart';
 
 /**
  * Internal dependencies
  */
-import { isGSuiteProductSlug } from 'calypso/lib/gsuite';
 import useSkipToLastStepIfFormComplete from '../hooks/use-skip-to-last-step-if-form-complete';
 import useIsCachedContactFormValid from '../hooks/use-is-cached-contact-form-valid';
 import ContactDetailsContainer from './contact-details-container';
 import type { CountryListItem } from '../types/country-list-item';
 import type { ManagedContactDetails } from '../types/wpcom-store-state';
+import type { ContactDetailsType } from '../types/contact-details';
 
 const BillingFormFields = styled.div`
 	margin-bottom: 16px;
@@ -40,20 +39,16 @@ const BillingFormFields = styled.div`
 export default function WPContactForm( {
 	countriesList,
 	shouldShowContactDetailsValidationErrors,
-	shouldShowDomainContactFields,
+	contactDetailsType,
 	contactValidationCallback,
 	isLoggedOutCart,
 }: {
 	countriesList: CountryListItem[];
 	shouldShowContactDetailsValidationErrors: boolean;
-	shouldShowDomainContactFields: boolean;
+	contactDetailsType: ContactDetailsType;
 	contactValidationCallback: () => Promise< boolean >;
 	isLoggedOutCart: boolean;
 } ): JSX.Element {
-	const { responseCart } = useShoppingCart();
-	const isGSuiteInCart = responseCart.products.some( ( item ) =>
-		isGSuiteProductSlug( item.product_slug )
-	);
 	const contactInfo: ManagedContactDetails = useSelect( ( select ) =>
 		select( 'wpcom' ).getContactInfo()
 	);
@@ -67,8 +62,7 @@ export default function WPContactForm( {
 	return (
 		<BillingFormFields>
 			<ContactDetailsContainer
-				shouldShowDomainContactFields={ shouldShowDomainContactFields }
-				isGSuiteInCart={ isGSuiteInCart }
+				contactDetailsType={ contactDetailsType }
 				contactInfo={ contactInfo }
 				countriesList={ countriesList }
 				shouldShowContactDetailsValidationErrors={ shouldShowContactDetailsValidationErrors }
