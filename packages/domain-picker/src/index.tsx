cd6080bd6e91fa46efbe5dbf5a49e20a46f636f4
Yan Sern <yan.sern@automattic.com>
2020-11-04 14:56:25 +0200

    DomainPicker: Add select button. (#46946)
    * Added domain picker select button.
    
    * Remove duplicate types patch.
    
    * Revert "Remove duplicate types patch."
    
    This reverts commit 1236fb40df12a23ac6ed48313b4eb86307ccda6f.
    
    * Remove types patch only in gutenboarding.
    
    * Remove types patch only in gutenboarding.

--- packages/domain-picker/src/index.tsx
+++ packages/domain-picker/src/index.tsx
@@ -2,3 +2,5 @@
  * Internal dependencies
  */
 export { default, Props } from './domain-picker';
+export { ITEM_TYPE_RADIO, ITEM_TYPE_BUTTON } from './domain-picker/suggestion-item';
+export type { SUGGESTION_ITEM_TYPE } from './domain-picker/suggestion-item';
