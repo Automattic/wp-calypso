054d9d16588b137376f81df1562b05541dd7666f
Renzo Canepa <rcanepag@gmail.com>
2020-11-06 15:07:40 -0300

    Jetpack: Handle disconnected site case (#47121)
    * Create Jetpack disconnected interstitial page
    
    * Create Jetpack disconnected switch component
    
    * Create new Upsell component status
    
    * Add an optional secondary button to Upsell
    
    * Include Jetpack Scan status in the check
    
    * Add Jetpack Disconnected gray SVG
    
    * Rename the switch component
    
    * Use Jetpack Support URL
    
    * Configure CTAs links
    
    * Abstract Disconnected components in their own files
    
    * Support disconnected state in Scan controller
    
    * Add `type=down` query parameter

--- client/my-sites/backup/wpcom-backup-upsell.tsx
+++ client/my-sites/backup/wpcom-backup-upsell.tsx
@@ -10,11 +10,12 @@ import { Button } from '@automattic/components';
 /**
  * Internal dependencies
  */
-import { getSelectedSiteSlug, getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import canCurrentUser from 'calypso/state/selectors/can-current-user';
 import { preventWidows } from 'calypso/lib/formatting';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
+import JetpackDisconnectedWPCOM from 'calypso/components/jetpack/jetpack-disconnected-wpcom';
 import Main from 'calypso/components/main';
 import Notice from 'calypso/components/notice';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
@@ -134,6 +135,9 @@ export default function WPCOMUpsellPage( { reason }: { reason: string } ): React
 			break;
 		case 'vp_active_on_site':
 			body = <BackupVPActiveBody />;
+			break;
+		case 'no_connected_jetpack':
+			body = <JetpackDisconnectedWPCOM />;
 			break;
 		default:
 			body = <BackupUpsellBody />;
