7996ec0140af5104ca8a98ca0acfa9e5180e6abf
Matthew Reishus <mreishus@users.noreply.github.com>
2020-11-09 16:03:28 -0600

    Add sunrise color scheme (#47171)
    * Add sunrise theme from wp-admin

--- client/blocks/color-scheme-picker/constants.js
+++ client/blocks/color-scheme-picker/constants.js
@@ -12,6 +12,7 @@ import powderSnowImg from 'calypso/assets/images/color-schemes/color-scheme-thum
 import nightfallImg from 'calypso/assets/images/color-schemes/color-scheme-thumbnail-nightfall.svg';
 import sakuraImg from 'calypso/assets/images/color-schemes/color-scheme-thumbnail-sakura.svg';
 import oceanImg from 'calypso/assets/images/color-schemes/color-scheme-thumbnail-ocean.svg';
+import sunriseImg from 'calypso/assets/images/color-schemes/color-scheme-thumbnail-sunrise.svg';
 import sunsetImg from 'calypso/assets/images/color-schemes/color-scheme-thumbnail-sunset.svg';
 import midnightImg from 'calypso/assets/images/color-schemes/color-scheme-thumbnail-midnight.svg';
 import contrastImg from 'calypso/assets/images/color-schemes/color-scheme-thumbnail-contrast.svg';
@@ -72,6 +73,14 @@ export default function ( translate ) {
 			thumbnail: {
 				cssClass: 'is-ocean',
 				imageUrl: oceanImg,
+			},
+		},
+		{
+			label: translate( 'Sunrise' ),
+			value: 'sunrise',
+			thumbnail: {
+				cssClass: 'is-sunrise',
+				imageUrl: sunriseImg,
 			},
 		},
 		{
--- client/state/preferences/schema.js
+++ client/state/preferences/schema.js
@@ -59,6 +59,7 @@ export const remoteValuesSchema = {
 				'ocean',
 				'powder-snow',
 				'sakura',
+				'sunrise',
 				'sunset',
 			],
 		},
