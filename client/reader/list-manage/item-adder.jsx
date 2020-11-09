1b181afd3bc643cae19a491d2588b173275cdfdc
Jason Moon <jason.moon@automattic.com>
2020-11-09 14:26:36 -0700

    Modularize filterFollowsByQuery

--- client/reader/list-manage/item-adder.jsx
+++ client/reader/list-manage/item-adder.jsx
@@ -4,7 +4,6 @@
 import React from 'react';
 import { useTranslate } from 'i18n-calypso';
 import { useSelector } from 'react-redux';
-import { escapeRegExp } from 'lodash';
 
 /**
  * Internal Dependencies
@@ -14,36 +13,12 @@ import QueryReaderFeedsSearch from 'calypso/components/data/query-reader-feeds-s
 import SyncReaderFollows from 'calypso/components/data/sync-reader-follows';
 import SearchInput from 'calypso/components/search';
 import { resemblesUrl } from 'calypso/lib/url';
-import {
-	getSiteName,
-	getSiteUrl,
-	getSiteDescription,
-	getSiteAuthorName,
-} from 'calypso/reader/get-helpers';
+import { filterFollowsByQuery } from 'calypso/reader/follow-helpers';
 import { getReaderFeedsForQuery } from 'calypso/state/reader/feed-searches/selectors';
 import { getReaderFollows } from 'calypso/state/reader/follows/selectors';
 import { SORT_BY_RELEVANCE } from 'calypso/state/reader/feed-searches/actions';
 import FeedUrlAdder from './feed-url-adder';
 import ListItem from './list-item';
-
-function filterFollows( query, follows ) {
-	const phraseRe = new RegExp( escapeRegExp( query ), 'i' );
-
-	return follows.filter( ( follow ) => {
-		const feed = follow.feed;
-		const site = follow.site;
-		const siteName = getSiteName( { feed, site } );
-		const siteUrl = getSiteUrl( { feed, site } );
-		const siteDescription = getSiteDescription( { feed, site } );
-		const siteAuthor = getSiteAuthorName( site );
-
-		return (
-			`${ follow.URL }${ siteName }${ siteUrl }${ siteDescription }${ siteAuthor }`.search(
-				phraseRe
-			) !== -1
-		);
-	} );
-}
 
 export default function ItemAdder( props ) {
 	const translate = useTranslate();
@@ -51,7 +26,7 @@ export default function ItemAdder( props ) {
 	const [ query, updateQuery ] = React.useState( '' );
 	const queryIsUrl = resemblesUrl( query );
 	const followResults = useSelector( ( state ) =>
-		filterFollows( query, getReaderFollows( state ) ).slice( 0, 7 )
+		filterFollowsByQuery( query, getReaderFollows( state ) ).slice( 0, 7 )
 	);
 	const feedResults = useSelector( ( state ) =>
 		getReaderFeedsForQuery( state, { query, excludeFollowed: false, sort: SORT_BY_RELEVANCE } )
@@ -85,7 +60,7 @@ export default function ItemAdder( props ) {
 				followResults?.map( ( item ) => (
 					<ListItem
 						hideIfInList
-						key={ item.feed_ID || item.site_ID || item.tag_ID }
+						key={ item.feed_ID || item.site_ID || item.tag_ID || item.feed_URL }
 						item={ item }
 						list={ props.list }
 						owner={ props.owner }
@@ -95,7 +70,7 @@ export default function ItemAdder( props ) {
 				feedResults?.map( ( item ) => (
 					<ListItem
 						hideIfInList
-						key={ item.feed_ID || item.site_ID || item.tag_ID }
+						key={ item.feed_ID || item.feed_URL }
 						item={ item }
 						list={ props.list }
 						owner={ props.owner }
