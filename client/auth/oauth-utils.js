6a1db65db2ccc1ec23c53ccaa23f9a3b2d51eef8
Markus Dobmann <m.do@posteo.net>
2020-11-09 16:30:05 +0100

    cleanup: remove unused auth actions

--- client/auth/oauth-utils.js
+++ client/auth/oauth-utils.js
@@ -4,12 +4,6 @@
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { bumpStat } from 'calypso/lib/analytics/mc';
 
-export const actions = {
-	AUTH_LOGIN: 'AUTH_LOGIN',
-	RECEIVE_AUTH_LOGIN: 'RECEIVE_AUTH_LOGIN',
-	AUTH_RESET: 'AUTH_RESET',
-};
-
 export const errors = {
 	ERROR_REQUIRES_2FA: 'needs_2fa', // From WP.com API
 	ERROR_INVALID_OTP: 'invalid_otp', // From WP.com API
