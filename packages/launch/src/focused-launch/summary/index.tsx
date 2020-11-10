17a56a27096736be2eda0e4a9b899860ea0c43f5
Marco Ciampini <marco.ciampini@automattic.com>
2020-11-10 13:44:08 +0100

    Focused Launch: render site name step only if necessary

--- packages/launch/src/focused-launch/summary/index.tsx
+++ packages/launch/src/focused-launch/summary/index.tsx
@@ -23,6 +23,8 @@ import LaunchContext from '../../context';
 
 import './style.scss';
 
+const DEFAULT_SITE_NAME = 'Site Title';
+
 const bulb = (
 	<SVG viewBox="0 0 24 24">
 		<Path d="M12 15.8c-3.7 0-6.8-3-6.8-6.8s3-6.8 6.8-6.8c3.7 0 6.8 3 6.8 6.8s-3.1 6.8-6.8 6.8zm0-12C9.1 3.8 6.8 6.1 6.8 9s2.4 5.2 5.2 5.2c2.9 0 5.2-2.4 5.2-5.2S14.9 3.8 12 3.8zM8 17.5h8V19H8zM10 20.5h4V22h-4z" />
@@ -383,7 +385,13 @@ const Summary: React.FunctionComponent = () => {
 	const disabledSteps: ( ( index: number ) => ReactNode )[] = [];
 	const activeSteps: ( ( index: number ) => ReactNode )[] = [];
 
-	activeSteps.push( renderSiteNameStep );
+	// Only show site name step if the current site title is still the default one.
+	// The RegExp check is more relaxed on purpose — chances are that if the title
+	// matches (even it not exactly) the default title, the user would like want
+	// to change it before launch.
+	if ( new RegExp( DEFAULT_SITE_NAME, 'i' ).test( title ) ) {
+		activeSteps.push( renderSiteNameStep );
+	}
 	( hasPaidDomain ? disabledSteps : activeSteps ).push( renderDomainStep );
 	( hasPaidPlan ? disabledSteps : activeSteps ).push( renderPlanStep );
 
