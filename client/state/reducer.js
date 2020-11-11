e1792fbcf0f3d9ba3e472141088cd1a8e17078f1
Marin Atanasov <tyxla@abv.bg>
2020-11-11 18:13:58 +0200

    State: Remove unused embeds state

--- client/state/reducer.js
+++ client/state/reducer.js
@@ -18,7 +18,6 @@ import atomicTransfer from './atomic-transfer/reducer';
 import currentUser from './current-user/reducer';
 import { reducer as dataRequests } from './data-layer/wpcom-http/utils';
 import documentHead from './document-head/reducer';
-import embeds from './embeds/reducer';
 import experiments from './experiments/reducer';
 import gsuiteUsers from './gsuite-users/reducer';
 import happychat from './happychat/reducer';
@@ -56,7 +55,6 @@ const reducers = {
 	currentUser,
 	dataRequests,
 	documentHead,
-	embeds,
 	experiments,
 	gsuiteUsers,
 	happychat,
