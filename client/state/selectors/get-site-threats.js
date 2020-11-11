d2ed7ed5c8d3b6f70551f7ced6200a60c799d07b
Sérgio Gomes <mail@sgomes.com>
2020-11-11 12:24:10 +0000

    Modularise rewind state
    Fixes #42473.

--- client/state/selectors/get-site-threats.js
+++ client/state/selectors/get-site-threats.js
@@ -1,3 +1,8 @@
+/**
+ * Internal dependencies
+ */
+import 'calypso/state/rewind/init';
+
 export default function getSiteThreats( state, siteId ) {
 	return state.rewind?.[ siteId ]?.state?.alerts?.threats ?? [];
 }
