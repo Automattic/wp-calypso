c8f4b1361766d51cd1e9c4d07a71dad6e6626833
Paul Bunkham <paul@dobit.co.uk>
2020-11-12 21:44:57 +0000

    Update logic and stop using isClassicEditorForced

--- client/state/selectors/get-editor-url.js
+++ client/state/selectors/get-editor-url.js
@@ -2,16 +2,13 @@
  * Internal dependencies
  */
 import { isEligibleForGutenframe } from 'calypso/state/gutenberg-iframe-eligible/is-eligible-for-gutenframe';
-import { getSelectedEditor } from 'calypso/state/selectors/get-selected-editor';
 import { getSiteAdminUrl, getSiteSlug } from 'calypso/state/sites/selectors';
 import { getEditorPath } from 'calypso/state/editor/selectors';
 import { addQueryArgs } from 'calypso/lib/route';
-import isClassicEditorForced from 'calypso/state/selectors/is-classic-editor-forced';
+import isVipSite from 'calypso/state/selectors/is-vip-site';
 
 export const getEditorUrl = ( state, siteId, postId = null, postType = 'post' ) => {
-	// isEligibleForGutenframe deals with the server side checks for classic editor plugin etc.
-	// isClassicEditerForced has the client side checks. We should combine these!
-	if ( ! isEligibleForGutenframe( state, siteId ) || isClassicEditorForced( state, siteId ) ) {
+	if ( ! isEligibleForGutenframe( state, siteId ) ) {
 		const siteAdminUrl = getSiteAdminUrl( state, siteId );
 		let url = `${ siteAdminUrl }post-new.php?post_type=${ postType }`;
 
@@ -19,7 +16,7 @@ export const getEditorUrl = ( state, siteId, postId = null, postType = 'post' ) 
 			url = `${ siteAdminUrl }post.php?post=${ postId }&action=edit`;
 		}
 
-		if ( 'gutenberg-redirect-and-style' === getSelectedEditor( state, siteId ) ) {
+		if ( ! isVipSite( state, siteId ) ) {
 			url = addQueryArgs( { calypsoify: '1' }, url );
 		}
 
