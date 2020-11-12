1b8734c498db2101da05285db15d6c00b05780c6
Kev <kevin.zoschke@automattic.com>
2020-11-12 08:51:18 -0500

    Implement product card of pricing page i5 (#47346)
    * Implement product card of pricing page i5
    
    * Remove comment

---
+++ client/components/jetpack/card/jetpack-product-card-i5/types.ts
@@ -0,0 +1,14 @@
+/**
+ * Internal dependencies
+ */
+
+/**
+ * Type dependencies
+ */
+import type {
+	SelectorProductFeaturesItem,
+	SelectorProductFeatures,
+} from 'calypso/my-sites/plans-v2/types';
+
+export type ProductCardFeaturesItem = SelectorProductFeaturesItem;
+export type ProductCardFeatures = SelectorProductFeatures;
