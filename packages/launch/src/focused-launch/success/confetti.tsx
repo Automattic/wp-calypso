a35e7f7b79fc3e6f8e628691bff5fd1faa5dd8c7
Marco Ciampini <marco.ciampini@automattic.com>
2020-11-13 20:35:09 +0100

    Add confetti SVG illustration to be used in Success view

---
+++ packages/launch/src/focused-launch/success/confetti.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+const Confetti: React.FunctionComponent = () => (
+	<svg
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 356 136"
+		className="focused-launch-success__confetti"
+	>
+		<rect
+			x="314.913"
+			y="59.963"
+			width="7"
+			height="22"
+			rx="3.5"
+			transform="rotate(-50 314.913 59.963)"
+			fill="#984A9C"
+		/>
+		<rect
+			y="121.876"
+			width="5.091"
+			height="16"
+			rx="2.545"
+			transform="rotate(-50 0 121.876)"
+			fill="#64CA43"
+		/>
+		<rect
+			x="21.447"
+			y="85.104"
+			width="7"
+			height="22"
+			rx="3.5"
+			transform="rotate(-120 21.447 85.104)"
+			fill="#37E788"
+		/>
+		<rect
+			x="75"
+			y="61.64"
+			width="4"
+			height="12.571"
+			rx="2"
+			transform="rotate(130 75 61.64)"
+			fill="#FF2D55"
+		/>
+		<rect
+			x="247.461"
+			y="63.86"
+			width="4.773"
+			height="15"
+			rx="2.386"
+			transform="rotate(118 247.461 63.86)"
+			fill="#E7C037"
+		/>
+		<rect
+			x="97"
+			y="2.529"
+			width="4.773"
+			height="15"
+			rx="2.386"
+			transform="rotate(-32 97 2.53)"
+			fill="#117AC9"
+		/>
+		<rect
+			x="323.638"
+			y="110.513"
+			width="5.091"
+			height="16"
+			rx="2.545"
+			transform="rotate(40 323.638 110.513)"
+			fill="#3361CC"
+		/>
+		<rect
+			x="160.138"
+			y="27"
+			width="7"
+			height="22"
+			rx="3.5"
+			transform="rotate(40 160.138 27)"
+			fill="#FF8085"
+		/>
+		<circle cx="40.5" cy="101.095" r="4.5" fill="#F0B849" />
+		<circle cx="20.928" cy="47.751" r="3" fill="#BF5AF2" />
+		<circle cx="341" cy="134" r="2" fill="#F0C930" />
+		<circle cx="131" cy="83" r="2" fill="#3361CC" />
+		<circle cx="214.5" cy="13.5" r="2.5" fill="#37E688" />
+		<circle cx="352.763" cy="98.263" r="3" fill="#09A884" />
+		<circle cx="285.181" cy="60.541" r="3" fill="#FF3B30" />
+	</svg>
+);
+
+export default Confetti;
