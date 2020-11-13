e12275ef93193e7c1d7fb2910c97aec59c04552a
Marin Atanasov <tyxla@abv.bg>
2020-11-13 10:09:12 +0200

    Further simplify reducer

--- client/state/domains/nameservers/reducer.js
+++ client/state/domains/nameservers/reducer.js
@@ -9,37 +9,22 @@ import {
 import { keyedReducer } from 'calypso/state/utils';
 import initialDomainState from './initial';
 
-/**
- * Updates name servers entry for given domain.
- *
- * @param {object} [state] Current state.
- * @param {object} [data] Domain name servers data.
- * @returns {object} New state
- */
-function updateState( state, data ) {
-	return Object.assign( {}, state, {
-		...initialDomainState,
-		...state,
-		...data,
-	} );
-}
-
-const reducer = keyedReducer( 'domainName', ( state = {}, action ) => {
+const reducer = keyedReducer( 'domainName', ( state = initialDomainState, action ) => {
 	switch ( action.type ) {
 		case DOMAIN_NAMESERVERS_FETCH: {
-			return updateState( state, {
+			return Object.assign( {}, state, {
 				isFetching: true,
 				error: false,
 			} );
 		}
 		case DOMAIN_NAMESERVERS_FETCH_FAILURE: {
-			return updateState( state, {
+			return Object.assign( {}, state, {
 				isFetching: false,
 				error: true,
 			} );
 		}
 		case DOMAIN_NAMESERVERS_RECEIVE: {
-			return updateState( state, {
+			return Object.assign( {}, state, {
 				isFetching: false,
 				hasLoadedFromServer: true,
 				list: action.nameservers,
