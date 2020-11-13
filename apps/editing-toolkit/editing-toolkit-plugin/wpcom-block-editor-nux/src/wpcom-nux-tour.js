c943dd34ab74e2ed3e759c0c2b7e80dd2ed0faab
Autumn Fjeld <autumn@automattic.com>
2020-11-13 12:58:08 +1100

    Trying: styling Card components, adding icons and buttons mechanics

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/wpcom-nux-tour.js
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/wpcom-nux-tour.js
@@ -5,20 +5,18 @@ import './public-path';
  * Internal dependencies
  */
 import './style-tour.scss';
-import editorImage from './images/editor.svg';
 
 /**
  * External dependencies
  */
-import { Button, Card, CardBody, CardFooter, Text } from '@wordpress/components';
-import { Icon, expand } from '@wordpress/icons';
+import { Button, Card, CardBody, CardFooter, CardMedia, Flex } from '@wordpress/components';
+// import styled from '@emotion/styled';
+import { Icon, expand, chevronUp } from '@wordpress/icons';
 
 import { createPortal, useEffect, useState } from '@wordpress/element';
 import { registerPlugin } from '@wordpress/plugins';
 
 function LaunchWpcomNuxTour() {
-	console.log( 'LaunchWpcomNuxTour' );
-
 	// Create parent for welcome tour portal
 	const portalParent = document.createElement( 'div' );
 	useEffect( () => {
@@ -33,28 +31,69 @@ function LaunchWpcomNuxTour() {
 
 function WelcomeTour() {
 	const [ isMinimized, setIsMinimized ] = useState( false );
+	// TODO: replace with wp.data
+	const [ isNuxEnabled, setIsNuxEnabled ] = useState( true );
+
+	const dismissWpcomNuxTour = () => {
+		// TODO recordTracksEvent
+		// setWpcomNuxStatus( { isNuxEnabled: false } );
+		setIsNuxEnabled( false );
+	};
+
+	if ( ! isNuxEnabled ) {
+		return null;
+	}
+
 	// const containerClass = isMinimized ? 'minimized';
 	return (
 		<div className="wpcom-editor-welcome-tour-container">
 			{ ! isMinimized ? (
-				<Card>
+				<Card className="welcome-tour-card">
+					<div className="welcome-tour-card__overlay-controls">
+						<Flex>
+							<Button
+								isPrimary
+								icon="pets"
+								iconSize={ 14 }
+								onClick={ () => setIsMinimized( true ) }
+							></Button>
+							<Button
+								isPrimary
+								icon="no-alt"
+								iconSize={ 14 }
+								onClick={ dismissWpcomNuxTour }
+							></Button>
+						</Flex>
+					</div>
+					<CardMedia>
+						<img
+							alt="Editor Welcome Tour"
+							src="https://nuxtourtest.files.wordpress.com/2020/11/mock-slide-1.jpg?resize=400px"
+						/>
+					</CardMedia>
 					<CardBody>
-						<h2 className="wpcom-editor-welcome-tour__card-heading">
-							Let's learn the basics of Block Editing
-						</h2>
+						<h2 className="welcome-tour-card__card-heading">Welcome to WordPress</h2>
+						<p>Learn the basic editor tools so you can edit and build your dream website.</p>
 					</CardBody>
 					<CardFooter>
-						<Button isPrimary={ true } onClick={ () => setIsMinimized( true ) }>
-							Minimize Me
-						</Button>
+						<div>• • • • • •</div>
+						<div>
+							<Button isTertiary={ true }>No thanks</Button>
+							<Button isPrimary={ true } className="welcome-tour-card__next-btn">
+								Let's start
+							</Button>
+						</div>
 					</CardFooter>
 				</Card>
 			) : (
 				<div className="wpcom-editor-welcome-tour__minimized-container">
-					<Button icon="expand" onClick={ () => setIsMinimized( false ) }>
-						Expand to resume
-						{ /* <Icon icon={ expand } size={ 16 } /> */ }
-						{ /* // Hm, Icon not working */ }
+					<Button
+						onClick={ () => setIsMinimized( false ) }
+						className="wpcom-editor-welcome-tour__resume-btn"
+					>
+						<p className="wpcom-editor-welcome-tour__resume-btn-text"> Click to resume tutorial</p>
+						<Icon icon={ chevronUp } size={ 16 } />
+						{ /* expand icon is throwing an error <Icon icon={ expand } size={ 16 } /> */ }
 					</Button>
 				</div>
 			) }
@@ -62,9 +101,9 @@ function WelcomeTour() {
 	);
 }
 
-// export default LaunchWpcomNuxTour;
+export default LaunchWpcomNuxTour;
 
-// For deving
-// registerPlugin( 'wpcom-block-editor-nux', {
-// 	render: () => <LaunchWpcomNuxTour />,
-// } );
+// TODO rename to nuxtour?
+registerPlugin( 'wpcom-block-editor-nux', {
+	render: () => <LaunchWpcomNuxTour />,
+} );
