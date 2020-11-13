a1a71e7269a4d7c909a3f98a2a61c0fb24d3567a
Payton Swick <payton@automattic.com>
2020-11-12 19:24:10 -0500

    Plans: Remove PaymentMethods block (#47262)
    * Remove web-payment translation in PaymentMethods
    
    The new function will return `apple-pay` already.
    
    * Use 'card' payment method name in PaymentMethods
    
    'credit-card' refers to the deprecated
    'WPCOM_Billing_MoneyPress_Paygate' class.
    
    * Remove PaymentMethods from plans pages
    
    * Remove PaymentMethods block

--- client/jetpack-connect/store-footer.tsx
+++ client/jetpack-connect/store-footer.tsx
@@ -6,16 +6,11 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import CartData from 'calypso/components/data/cart';
-import PaymentMethods from 'calypso/blocks/payment-methods';
 import JetpackFAQ from 'calypso/my-sites/plans-features-main/jetpack-faq';
 
 export default function StoreFooter() {
 	return (
 		<>
-			<CartData>
-				<PaymentMethods />
-			</CartData>
 			<JetpackFAQ />
 		</>
 	);
