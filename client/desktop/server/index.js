51a358bfc4fbbecee1869fe8d2c518ddf02c421f
nsakaimbo <nsakaimbo@me.com>
2020-11-13 13:44:39 -0500

    Add IPC handlers for config + settings

--- client/desktop/server/index.js
+++ client/desktop/server/index.js
@@ -55,7 +55,7 @@ function showAppWindow() {
 		mainWindow.webContents.send( 'app-config', Config, Settings.isDebug(), System.getDetails() );
 
 		ipc.on( 'mce-contextmenu', function ( ev ) {
-			mainWindow.send( 'mce-contextmenu', ev );
+			mainWindow.webContents.send( 'mce-contextmenu', ev );
 		} );
 	} );
 
@@ -92,8 +92,15 @@ function showAppWindow() {
 		callback( { cancel: false } );
 	} );
 
+	ipc.handle( 'get-config', () => {
+		return Config.toRenderer();
+	} );
+
+	ipc.handle( 'get-settings', () => {
+		return Settings.toRenderer();
+	} );
+
 	mainWindow.loadURL( appUrl );
-	// mainWindow.openDevTools();
 
 	mainWindow.on( 'close', function () {
 		const currentURL = mainWindow.webContents.getURL();
