bcfb8543ed56394c3485340a15e5838b27d226dc
Payton Swick <payton@foolord.com>
2020-11-16 18:19:54 -0500

    Only count full credits if there is a subtotal

--- client/my-sites/checkout/composite-checkout/lib/does-purchase-have-full-credits.ts
+++ client/my-sites/checkout/composite-checkout/lib/does-purchase-have-full-credits.ts
@@ -5,5 +5,6 @@ import type { ResponseCart } from '@automattic/shopping-cart';
 
 export default function doesPurchaseHaveFullCredits( cart: ResponseCart ): boolean {
 	const credits = cart.credits_integer;
-	return credits > 0 && credits >= cart.sub_total_integer;
+	const subtotal = cart.sub_total_integer;
+	return credits > 0 && subtotal > 0 && credits >= subtotal;
 }
