165a98ba15604b2ca5052ba0901d8cc90db6a113
Marco Ciampini <marco.ciampini@automattic.com>
2020-11-16 17:45:13 +0100

    Use useFocusedLaunchModal() hook when attaching focused launch modal

--- apps/editing-toolkit/editing-toolkit-plugin/editor-site-launch/src/attach-focused-launch.tsx
+++ apps/editing-toolkit/editing-toolkit-plugin/editor-site-launch/src/attach-focused-launch.tsx
@@ -2,14 +2,8 @@
  * External dependencies
  */
 import * as React from 'react';
-import { useDispatch, useSelect } from '@wordpress/data';
 import { registerPlugin as originalRegisterPlugin, PluginSettings } from '@wordpress/plugins';
-import FocusedLaunchModal from '@automattic/launch';
-
-/**
- * Internal dependencies
- */
-import { LAUNCH_STORE } from './stores';
+import FocusedLaunchModal, { useFocusedLaunchModal } from '@automattic/launch';
 
 const registerPlugin = ( name: string, settings: Omit< PluginSettings, 'icon' > ) =>
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -17,8 +11,7 @@ const registerPlugin = ( name: string, settings: Omit< PluginSettings, 'icon' > 
 
 registerPlugin( 'a8c-editor-editor-focused-launch', {
 	render: function LaunchSidebar() {
-		const { isFocusedLaunchOpen } = useSelect( ( select ) => select( LAUNCH_STORE ).getState() );
-		const { closeFocusedLaunch } = useDispatch( LAUNCH_STORE );
+		const { isFocusedLaunchOpen, closeFocusedLaunch } = useFocusedLaunchModal();
 
 		if ( ! isFocusedLaunchOpen ) {
 			return null;
