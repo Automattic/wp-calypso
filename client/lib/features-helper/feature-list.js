3e4b19545ac47879b7ef5ae079683222181b0a4f
Matthew Reishus <mreishus@users.noreply.github.com>
2020-11-17 17:21:41 -0600

    Feature helper displays both Current XL Projects and Enabled Features

--- client/lib/features-helper/feature-list.js
+++ client/lib/features-helper/feature-list.js
@@ -2,30 +2,38 @@
  * External dependencies
  */
 import React from 'react';
+
 /**
  * Internal dependencies
  */
 import config from 'calypso/config';
 import { Card } from '@automattic/components';
-//import Item from './item';
+
+const headerClass = ' features-helper__feature-header';
+const itemClass = ' features-helper__feature-item';
+const enabledClass = ' features-helper__feature-item-enabled';
+const disabledClass = ' features-helper__feature-item-disabled';
 
 export const FeatureList = React.memo( () => {
-	const flagsToCheck = [ 'anchor-fm' ];
+	const currentXLProjects = [ 'anchor-fm', 'nav-unification' ];
+	const enabledFeatures = config.enabledFeatures();
 	return (
 		<>
 			<div>Features</div>
 			<Card className="features-helper__current-features">
-				{ flagsToCheck
-					.filter( ( flag ) => config.isEnabled( flag ) )
-					.map( ( flag ) => (
-						<div
-							className="features-helper__feature-header"
-							key={ flag }
-							tooltip={ `Development flag ${ flag } enabled` }
-						>
-							{ flag }
-						</div>
-					) ) }
+				<div className={ headerClass }>Current XL Projects</div>
+				{ currentXLProjects.map( ( flag ) => {
+					if ( config.isEnabled( flag ) ) {
+						return <div className={ itemClass + enabledClass }>{ flag } ON</div>;
+					}
+					return <div className={ itemClass + disabledClass }>{ flag } OFF</div>;
+				} ) }
+				<div className={ headerClass }>All Enabled Features</div>
+				{ enabledFeatures.map( ( flag ) => (
+					<div className={ itemClass } key={ flag }>
+						{ flag }
+					</div>
+				) ) }
 			</Card>
 		</>
 	);
