72bdd02af5fc9ffad75fc66f6f3c90bcf43c3081
sixhours <calobee@gmail.com>
2020-11-17 14:58:30 -0500

    Use non-bordered version of help icon

--- client/blocks/inline-help/index.jsx
+++ client/blocks/inline-help/index.jsx
@@ -154,7 +154,7 @@ class InlineHelp extends Component {
 					title={ translate( 'Help' ) }
 					ref={ this.inlineHelpToggleRef }
 				>
-					<Gridicon icon="help-outline" size={ 36 } />
+					<Gridicon icon="help" size={ 36 } />
 				</Button>
 				{ isPopoverVisible && (
 					<InlineHelpPopover
