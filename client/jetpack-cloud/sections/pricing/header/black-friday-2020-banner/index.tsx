0f0a56a1090336f3674d0534f964bba0956cb3ef
elliottprogrammer <bryan@elliottprogrammer.com>
2020-11-17 13:48:08 -0800

    Add Black Friday 2020 banner.

---
+++ client/jetpack-cloud/sections/pricing/header/black-friday-2020-banner/index.tsx
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import { useTranslate } from 'i18n-calypso';
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import banner from './black-friday-2020-banner.png';
+import banner2x from './black-friday-2020-banner-2x.png';
+import bannerMobile from './black-friday-2020-banner_mobile.png';
+import bannerMobile2x from './black-friday-2020-banner_mobile-2x.png';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
+const BlackFriday2020Banner: React.FC = () => {
+	const translate = useTranslate();
+	return (
+		<div className="black-friday-2020-banner">
+			<picture className="black-friday-2020-banner__image">
+				<source
+					media="(max-width: 782px)"
+					srcSet={ `${ bannerMobile } 375w, ${ bannerMobile2x } 750w` }
+				/>
+
+				<source media="(min-width: 783px)" srcSet={ `${ banner } 1080w, ${ banner2x } 2160w` } />
+
+				<img
+					src={ `${ banner }` }
+					alt={ translate( 'Use code JETBLACK at checkout through November 30.' ) }
+				/>
+			</picture>
+		</div>
+	);
+};
+
+export default BlackFriday2020Banner;
--- client/jetpack-cloud/sections/pricing/header/index.tsx
+++ client/jetpack-cloud/sections/pricing/header/index.tsx
@@ -4,6 +4,7 @@
 import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
 import React, { useMemo } from 'react';
+import moment from 'moment';
 
 /**
  * Internal dependencies
@@ -15,13 +16,19 @@ import config from 'calypso/config';
 import { preventWidows } from 'calypso/lib/formatting';
 import { getJetpackCROActiveVersion } from 'calypso/my-sites/plans-v2/abtest';
 import { Iterations } from 'calypso/my-sites/plans-v2/iterations';
+// Black Friday 2020 promotion; runs Nov 20-30 automatically. Safe to remove after Dec 1
+import BlackFriday2020Banner from './black-friday-2020-banner';
 
 /**
  * Style dependencies
  */
 import './style.scss';
 
-const Header: React.FC = () => {
+type HeaderProps = {
+	urlQueryArgs: { [ key: string ]: string };
+};
+
+const Header: React.FC< HeaderProps > = ( { urlQueryArgs } ) => {
 	const identity = config( 'olark_chat_identity' );
 	const translate = useTranslate();
 	const iteration = useMemo( getJetpackCROActiveVersion, [] ) as Iterations;
@@ -44,10 +51,23 @@ const Header: React.FC = () => {
 			'Get everything your site needs, in one package — so you can focus on your business.'
 		);
 
+	// Black Friday 2020 promotion; runs Nov 20-30 automatically. Safe to remove after Dec 1
+	const promoStartDate = moment( '2020-11-20', 'YYYY-MM-DD h:mm:ss a' );
+	const promoEndDate = moment( '2020-11-30 11:59:59 pm', 'YYYY-MM-DD h:mm:ss a' );
+	const today = moment();
+
+	// Use query param `?bf=true` to preview the banner outside of Black Friday: https://cloud.jetpack.com/pricing?bf=true
+	const hasPromoQueryParam = urlQueryArgs?.bf && urlQueryArgs.bf === 'true' ? true : false;
+	const isWithinPromoDate =
+		moment( today ).isBetween( promoStartDate, promoEndDate ) || hasPromoQueryParam;
+
 	return (
 		<>
 			{ identity && <OlarkChat { ...{ identity } } /> }
 			<JetpackComMasterbar />
+
+			{ isWithinPromoDate && <BlackFriday2020Banner /> }
+
 			<div className={ classNames( 'header', iteration ) }>
 				<FormattedHeader
 					className="header__main-title"
