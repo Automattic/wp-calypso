40699867f63dbb4873c74d692386bfd548f5661d
Louis Laugesen <louis.laugesen@gmail.com>
2020-11-17 18:10:29 +1100

    wrap the new localizeUrl function

--- packages/i18n-utils/src/index.ts
+++ packages/i18n-utils/src/index.ts
@@ -1,2 +1,25 @@
-export { localizeUrl } from './localize-url';
-export { LocaleProvider, useLocale } from './locale-context';
+/**
+ * Internal dependencies
+ */
+import { localizeUrl } from './localize-url';
+import { Locale } from './locales';
+import { LocaleProvider, useLocale } from './locale-context';
+
+export { LocaleProvider, useLocale };
+
+export interface I18nUtils {
+	localizeUrl: typeof localizeUrl;
+}
+
+export function useI18nUtils(): I18nUtils {
+	const locale = useLocale();
+
+	return {
+		localizeUrl: ( fullUrl: string, toLocale?: Locale ) => {
+			if ( toLocale ) {
+				return localizeUrl( fullUrl, toLocale );
+			}
+			return localizeUrl( fullUrl, locale );
+		},
+	};
+}
