1cd34df6ab93e856c5042cfe17aa49a5cc334298
Atanas Angelov <hi@atanas.dev>
2020-11-17 20:59:40 +0200

    Jetpack Licensing: WIP improvements to UI and data layer of license inspection so they work

--- client/jetpack-cloud/sections/licensing/inspect-license-form/index.tsx
+++ client/jetpack-cloud/sections/licensing/inspect-license-form/index.tsx
@@ -2,20 +2,19 @@
  * External dependencies
  */
 import React, { ChangeEvent, FormEvent, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import { Button, Card } from '@automattic/components';
-import Spinner from 'calypso/components/spinner';
 import CardHeading from 'calypso/components/card-heading';
 import FormTextInput from 'calypso/components/forms/form-text-input';
 import FormLabel from 'calypso/components/forms/form-label';
-import FormInputValidation from 'calypso/components/forms/form-input-validation';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
 import { setInspectedLicenseKey } from 'calypso/state/jetpack-licensing/actions';
+import { isInspecting as isInspectingSelector } from 'calypso/state/jetpack-licensing/selectors';
 
 /**
  * Style dependencies
@@ -25,26 +24,17 @@ import './style.scss';
 const InspectLicenseForm: React.FC = () => {
 	const dispatch = useDispatch();
 	const translate = useTranslate();
+	const isInspecting = useSelector( isInspectingSelector );
 	const [ licenseKey, setLicenseKey ] = useState( '' );
-	const [ error, setError ] = useState( '' );
-	const [ inProgress, setInProgress ] = useState( false );
 
 	const submit = ( e: FormEvent< HTMLFormElement > ) => {
 		e.preventDefault();
 
-		if ( inProgress ) {
+		if ( isInspecting ) {
 			return;
 		}
 
-		setInProgress( true );
-
 		dispatch( setInspectedLicenseKey( licenseKey ) );
-		/*dispatch( inspectLicense( domain ) );
-
-		setTimeout( () => {
-			setError( translate( 'TBD read error code' ) );
-			setInProgress( false );
-		}, 1000 );*/
 	};
 
 	return (
@@ -59,38 +49,19 @@ const InspectLicenseForm: React.FC = () => {
 						id="inspect_license_key"
 						name="license_key"
 						placeholder="jetpack-backup-daily__WdeXxuAYbwwEau8maXA1cTj8I"
-						isError={ !! error }
 						value={ licenseKey }
-						disabled={ inProgress }
+						disabled={ isInspecting }
 						onChange={ ( e: ChangeEvent< HTMLInputElement > ) => setLicenseKey( e.target.value ) }
 					/>
-					{ error && <FormInputValidation isError text={ error } /> }
 				</FormFieldset>
 				<FormFieldset className="inspect-license-form__actions">
-					<Button type="submit" disabled={ inProgress } primary>
+					<Button type="submit" disabled={ isInspecting } primary>
 						{ translate( 'Inspect' ) }
 					</Button>
-					{ inProgress && <Spinner /> }
 				</FormFieldset>
 			</form>
 		</Card>
 	);
-
-	/*
-
-		<>
-			{ identity && <OlarkChat { ...{ identity } } /> }
-			<JetpackComMasterbar />
-			<div className={ classNames( 'header', iteration ) }>
-				<FormattedHeader
-					className="header__main-title"
-					headerText={ preventWidows( title ) }
-					align="center"
-				/>
-				{ tagline && <p>{ tagline }</p> }
-			</div>
-		</>
-	 */
 };
 
 export default InspectLicenseForm;
