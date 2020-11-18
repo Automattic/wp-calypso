ce885aa384a8e88a47e4da4c992c77fe2577978d
Omar Alshaker <omar@omaralshaker.com>
2020-11-18 19:09:13 +0100

    Fix types

--- packages/launch/src/focused-launch/go-back-button/index.tsx
+++ packages/launch/src/focused-launch/go-back-button/index.tsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import { __ } from '@wordpress/i18n';
 import { Icon, chevronLeft } from '@wordpress/icons';
+import type { Button } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -13,7 +14,7 @@ import './style.scss';
 
 declare const __i18n_text_domain__: string;
 
-export default function GoBackButton( props ) {
+export default function GoBackButton( props: Button.ButtonProps ) {
 	return (
 		<BackButton { ...props }>
 			<Icon icon={ chevronLeft } />
