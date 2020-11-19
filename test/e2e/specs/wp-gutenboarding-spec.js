baadeaea6519329a7d7f7342c2b6a7dcf6e02d94
Stefan Nieuwenhuis <StefanNieuwenhuis@users.noreply.github.com>
2020-11-19 16:19:46 +0100

    Revert "Simulate E2E failure (part of onboarding process) (#47588)" (#47593)
    This reverts commit 41735ebfb887e992e8a6aeca99f7d8ba238b5b3b.

--- test/e2e/specs/wp-gutenboarding-spec.js
+++ test/e2e/specs/wp-gutenboarding-spec.js
@@ -39,10 +39,6 @@ describe( 'Gutenboarding: (' + screenSize + ')', function () {
 			await driverManager.ensureNotLoggedIn( driver );
 		} );
 
-		step( 'FAILING Test', function () {
-			assert.strictEqual( true, false, 'This should fail on purpose' );
-		} );
-
 		step( 'Can visit Gutenboarding page and see Onboarding block', async function () {
 			const page = await NewPage.Visit( driver );
 			const blockExists = await page.waitForBlock();
