d132081ecb6e2ec234c2aea13e84007d8c5544b1
Yan Sern <yan.sern@automattic.com>
2020-11-19 16:52:08 +0200

    Spacing things out (#47585)

--- client/landing/gutenboarding/stores/domain-suggestions/index.ts
+++ client/landing/gutenboarding/stores/domain-suggestions/index.ts
@@ -5,6 +5,6 @@ import { DomainSuggestions } from '@automattic/data-stores';
 import { getSuggestionsVendor } from 'calypso/lib/domains/suggestions';
 
 export const DOMAIN_SUGGESTIONS_STORE = DomainSuggestions.register( {
-	/* Returns an ID for the domain suggestions vendor. Passing `{ isSignup: true }` to getSuggestionsVendor returns the signup variant.*/
+	/* Returns an ID for the domain suggestions vendor. Passing `{ isSignup: true }` to getSuggestionsVendor returns the signup variant. */
 	vendor: getSuggestionsVendor( { isSignup: true } ),
 } );
