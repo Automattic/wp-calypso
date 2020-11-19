3fdd3cc30b981df51694e143927fb9ee01f2fc57
Stefan Nieuwenhuis <StefanNieuwenhuis@users.noreply.github.com>
2020-11-19 09:26:29 +0100

    Onboarding: Allow Title & SubTitle to render <h*/> & <p/> tags (#47572)
    We want to improve accessibility of Gutenboarding, hence we need to allow <Title/> & <SubTitle/> to render <h*/> & <p/> tags.
    Why? The <Title/> renders a <h1/> element by default and <SubTitle/> a <h2/>. When used in another component that already renders a <h1/> this will be semantically incorrect and impacts  accessibility negatively.

--- packages/onboarding/src/titles/index.tsx
+++ packages/onboarding/src/titles/index.tsx
@@ -6,15 +6,31 @@ import classnames from 'classnames';
 
 import './styles.scss';
 
+type AllowedTagNames = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
 interface TitlesProps {
 	className?: string;
+	tagName?: AllowedTagNames;
 }
 
 /* eslint-disable wpcalypso/jsx-classname-namespace */
-export const Title: React.FunctionComponent< TitlesProps > = ( { className, children } ) => (
-	<h1 className={ classnames( 'onboarding-title', className ) }>{ children }</h1>
-);
+export const Title: React.FunctionComponent< TitlesProps > = ( {
+	className,
+	children,
+	tagName = 'h1',
+} ) =>
+	React.createElement(
+		tagName,
+		{ className: classnames( 'onboarding-title', className ) },
+		children
+	);
 
-export const SubTitle: React.FunctionComponent< TitlesProps > = ( { className, children } ) => (
-	<h2 className={ classnames( 'onboarding-subtitle', className ) }>{ children }</h2>
-);
+export const SubTitle: React.FunctionComponent< TitlesProps > = ( {
+	className,
+	children,
+	tagName = 'h2',
+} ) =>
+	React.createElement(
+		tagName,
+		{ className: classnames( 'onboarding-subtitle', className ) },
+		children
+	);
