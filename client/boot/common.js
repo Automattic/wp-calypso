e41d89d55a9f7c4b4d733a37c8c97f3c8ea0314e
Kevin Zoschke <kevin.zoschke@automattic.com>
2020-11-20 10:50:58 -0500

    Make /jetpack/connect accessible when unauthenticated in cloud

--- client/boot/common.js
+++ client/boot/common.js
@@ -52,7 +52,7 @@ import userFactory from 'calypso/lib/user';
 import { getUrlParts, isOutsideCalypso } from 'calypso/lib/url';
 import { setStore } from 'calypso/state/redux-store';
 import { requestUnseenStatus } from 'calypso/state/reader-ui/seen-posts/actions';
-import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
+import isJetpackCloud, { isJetpackCloudProd } from 'calypso/lib/jetpack/is-jetpack-cloud';
 import { inJetpackCloudOAuthOverride } from 'calypso/lib/jetpack/oauth-override';
 import { getLanguageSlugs } from 'calypso/lib/i18n-utils/utils';
 
@@ -126,6 +126,10 @@ const oauthTokenMiddleware = () => {
 			'/api/oauth/token',
 			'/connect',
 		];
+
+		if ( isJetpackCloud() && ! isJetpackCloudProd() ) {
+			loggedOutRoutes.push( '/jetpack/connect' );
+		}
 
 		if ( isJetpackCloud() && config.isEnabled( 'jetpack/pricing-page' ) ) {
 			loggedOutRoutes.push( '/pricing' );
