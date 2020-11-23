64b4b5838007f0d2a5251b9cffb9271177490856
Ramon <ramonjd@users.noreply.github.com>
2020-11-23 13:19:48 +1100

    Editing toolkit: use `is_` WordPress checks and clean up hooks (#47563)
    * Removing dupe hooks
    Also not setting wpcom_public_coming_soon if it's not in the site creation meta.
    
    * renaming method to be a little more specific
    
    * ecommerce sites don't page global $post available for some reason so let's target the things we want to hide using is_ methods
    
    * tagging the coming soon page with HTML comment
    
    * Update coming-soon.php
    
    Grouped action and filter hooks. Added space between hooks and wp_enqueue_style
    
    * Remove Whitespace
    
    Fixing 49 | ERROR | [x] Whitespace found at end of line Squiz.WhiteSpace.SuperfluousWhitespace.EndLine

--- apps/editing-toolkit/editing-toolkit-plugin/coming-soon/coming-soon.php
+++ apps/editing-toolkit/editing-toolkit-plugin/coming-soon/coming-soon.php
@@ -12,11 +12,8 @@ namespace A8C\FSE\Coming_soon;
  *
  * @return boolean
  */
-function show_coming_soon_page() {
-	global $post;
-
-	// Handle the case where we are not rendering a post.
-	if ( ! isset( $post ) ) {
+function should_show_coming_soon_page() {
+	if ( ! is_singular() && ! is_archive() && ! is_search() ) {
 		return false;
 	}
 
@@ -33,6 +30,11 @@ function show_coming_soon_page() {
  * Renders a fallback coming soon page
  */
 function render_fallback_coming_soon_page() {
+	if ( ! defined( 'GRAVATAR_HOVERCARDS__DISABLE' ) ) {
+		define( 'GRAVATAR_HOVERCARDS__DISABLE', true );
+	}
+
+	// Disable WP scripts, likes, social og meta, cookie banner.
 	remove_action( 'wp_enqueue_scripts', 'wpcom_actionbar_enqueue_scripts', 101 );
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 	remove_action( 'wp_print_styles', 'print_emoji_styles' );
@@ -40,6 +42,11 @@ function render_fallback_coming_soon_page() {
 	remove_action( 'wp_head', 'global_css', 5 );
 	remove_action( 'wp_footer', 'wpcom_subs_js' );
 	remove_action( 'wp_footer', 'stats_footer', 101 );
+	add_filter( 'jetpack_disable_eu_cookie_law_widget', '__return_true', 1 );
+	add_filter( 'jetpack_enable_opengraph', '__return_false', 1 );
+	add_filter( 'wpcom_disable_logged_out_follow', '__return_true', 10, 1 ); // Disable follow actionbar.
+	add_filter( 'wpl_is_enabled_sitewide', '__return_false', 10, 1 ); // Disable likes.
+
 	wp_enqueue_style( 'recoleta-font', '//s1.wp.com/i/fonts/recoleta/css/400.min.css', array(), PLUGIN_VERSION );
 
 	include __DIR__ . '/fallback-coming-soon-page.php';
@@ -106,8 +113,6 @@ add_action( 'update_option_blog_public', __NAMESPACE__ . '\disable_coming_soon_o
 function add_option_to_new_site( $blog_id, $user_id, $domain, $path, $network_id, $meta ) {
 	if ( 0 === $meta['public'] && 1 === (int) $meta['options']['wpcom_public_coming_soon'] ) {
 		add_blog_option( $blog_id, 'wpcom_public_coming_soon', 1 );
-	} else {
-		add_blog_option( $blog_id, 'wpcom_public_coming_soon', 0 );
 	}
 }
 // phpcs:enable Generic.CodeAnalysis.UnusedFunctionParameter.FoundBeforeLastUsed
@@ -118,24 +123,9 @@ add_action( 'wpmu_new_blog', __NAMESPACE__ . '\add_option_to_new_site', 10, 6 );
  * the redirect.
  */
 function coming_soon_page() {
-	if ( ! show_coming_soon_page() ) {
+	if ( ! should_show_coming_soon_page() ) {
 		return;
 	}
-	if ( ! defined( 'GRAVATAR_HOVERCARDS__DISABLE' ) ) {
-		define( 'GRAVATAR_HOVERCARDS__DISABLE', true );
-	}
-	add_filter( 'wpcom_disable_logged_out_follow', '__return_true', 10, 1 ); // Disable follow actionbar.
-	add_filter( 'wpl_is_enabled_sitewide', '__return_false', 10, 1 ); // Disable likes.
-	// Disable WP scripts, social og meta, cookie banner.
-	remove_action( 'wp_enqueue_scripts', 'wpcom_actionbar_enqueue_scripts', 101 );
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	remove_action( 'wp_head', 'header_js', 5 );
-	remove_action( 'wp_head', 'global_css', 5 );
-	remove_action( 'wp_footer', 'wpcom_subs_js' );
-	remove_action( 'wp_footer', 'stats_footer', 101 );
-	add_filter( 'jetpack_disable_eu_cookie_law_widget', '__return_true', 1 );
-	add_filter( 'jetpack_enable_opengraph', '__return_false', 1 );
 
 	render_fallback_coming_soon_page();
 	die();
