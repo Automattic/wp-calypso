37f1e3a0c1b17e6e12b922af375c19f946c33674
Marin Atanasov <tyxla@abv.bg>
2020-11-23 14:12:21 +0200

    Extensions: Migrate Zoninator components away from redux forms

--- client/extensions/zoninator/components/settings/zone-creator/index.jsx
+++ client/extensions/zoninator/components/settings/zone-creator/index.jsx
@@ -13,6 +13,7 @@ import { flowRight } from 'lodash';
  */
 import HeaderCake from 'calypso/components/header-cake';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import { isSavingZone } from '../../../state/zones/selectors';
 import ZoneDetailsForm from '../../forms/zone-details-form';
 import { addZone } from '../../../state/zones/actions';
 import { settingsPath } from '../../../app/util';
@@ -26,7 +27,7 @@ class ZoneCreator extends PureComponent {
 		translate: PropTypes.func.isRequired,
 	};
 
-	save = ( form, data ) => this.props.addZone( this.props.siteId, this.props.siteSlug, form, data );
+	save = ( data ) => this.props.addZone( this.props.siteId, this.props.siteSlug, data );
 
 	render() {
 		const { siteSlug, translate } = this.props;
@@ -42,17 +43,26 @@ class ZoneCreator extends PureComponent {
 					{ translate( 'Add a zone' ) }
 				</HeaderCake>
 
-				<ZoneDetailsForm label={ translate( 'New zone' ) } onSubmit={ this.save } />
+				<ZoneDetailsForm
+					label={ translate( 'New zone' ) }
+					onSubmit={ this.save }
+					submitting={ this.props.isSavingZone }
+				/>
 			</div>
 		);
 	}
 }
 
 const connectComponent = connect(
-	( state ) => ( {
-		siteId: getSelectedSiteId( state ),
-		siteSlug: getSelectedSiteSlug( state ),
-	} ),
+	( state ) => {
+		const siteId = getSelectedSiteId( state );
+
+		return {
+			siteId,
+			siteSlug: getSelectedSiteSlug( state ),
+			isSavingZone: isSavingZone( state, siteId ),
+		};
+	},
 	{ addZone }
 );
 
