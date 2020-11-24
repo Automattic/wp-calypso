82d14decc0cfdc6c12650d0f63c9554c71982b1c
Don Lair <don.lair@gmail.com>
2020-11-24 17:52:24 -0500

    Adding Google Analytics tracking params to link.

--- client/my-sites/customer-home/cards/tasks/wp-courses/index.jsx
+++ client/my-sites/customer-home/cards/tasks/wp-courses/index.jsx
@@ -16,7 +16,7 @@ const WpCourses = () => {
 			title="Introducing WordPress Courses"
 			description="Enroll today in our new educational courses for all skill levels. From Blogging for Beginners to Podcasting for Beginners (coming soon!) – these courses offer step-by step guidance on getting started, office hours with WordPress experts, an exclusive community, and virtual meetups."
 			actionText="Enroll today and start learning!"
-			actionUrl="https://wpcourses.com"
+			actionUrl="https://wpcourses.com/?utm_source=wordpressdotcom&utm_medium=referral&utm_campaign=courses_launch_myhome"
 			actionTarget="_blank"
 			illustration={ coursesLogo }
 			completeOnStart={ false }
