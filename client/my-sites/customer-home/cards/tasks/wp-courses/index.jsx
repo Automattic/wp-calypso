b56f9fa3699dc98a8ddb9d52d4e5c7037196ea0b
Don Lair <don.lair@gmail.com>
2020-11-24 17:38:23 -0500

    Adding WP Courses task to My Home.

---
+++ client/my-sites/customer-home/cards/tasks/wp-courses/index.jsx
@@ -0,0 +1,28 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import Task from 'calypso/my-sites/customer-home/cards/tasks/task';
+import { TASK_WP_COURSES } from 'calypso/my-sites/customer-home/cards/constants';
+import coursesLogo from 'calypso/assets/images/customer-home/courses-logo.png';
+
+const WpCourses = () => {
+	return (
+		<Task
+			title="Introducing WordPress Courses"
+			description="Enroll today in our new educational courses for all skill levels. From Blogging for Beginners to Podcasting for Beginners (coming soon!) – these courses offer step-by step guidance on getting started, office hours with WordPress experts, an exclusive community, and virtual meetups."
+			actionText="Enroll today and start learning!"
+			actionUrl="https://wpcourses.com"
+			actionTarget="_blank"
+			illustration={ coursesLogo }
+			completeOnStart={ false }
+			taskId={ TASK_WP_COURSES }
+		/>
+	);
+};
+
+export default WpCourses;
