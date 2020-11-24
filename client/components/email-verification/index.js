2482666e08f1d5caf011a95fff06237351567268
Andrew Serong <andrewserong@users.noreply.github.com>
2020-11-24 15:58:27 +1100

    Increase delay to 500ms to allow time for translations to be loaded

--- client/components/email-verification/index.js
+++ client/components/email-verification/index.js
@@ -24,7 +24,7 @@ export default function emailVerification( context, next ) {
 			const message = i18n.translate( 'Email confirmed!' );
 			const notice = successNotice( message, { duration: 10000 } );
 			context.store.dispatch( notice );
-		}, 100 ); // A delay is needed here, because the notice state seems to be cleared upon page load
+		}, 500 ); // A delay is needed here, because the notice state seems to be cleared upon page load
 	}
 
 	next();
