c62c26d2e5bed19238aef1208a6f7668fe79fa68
Marco Ciampini <marco.ciampini@automattic.com>
2020-11-24 10:23:51 +0100

    Calypso Feature Helper: add missing 'key' property (#47677)

--- client/lib/features-helper/feature-list.js
+++ client/lib/features-helper/feature-list.js
@@ -9,10 +9,10 @@ import React from 'react';
 import config from 'calypso/config';
 import { Card } from '@automattic/components';
 
-const headerClass = ' features-helper__feature-header';
-const itemClass = ' features-helper__feature-item';
-const enabledClass = ' features-helper__feature-item-enabled';
-const disabledClass = ' features-helper__feature-item-disabled';
+const headerClass = 'features-helper__feature-header';
+const itemClass = 'features-helper__feature-item';
+const enabledClass = 'features-helper__feature-item-enabled';
+const disabledClass = 'features-helper__feature-item-disabled';
 
 export const FeatureList = React.memo( () => {
 	const currentXLProjects = [ 'anchor-fm', 'nav-unification' ];
@@ -23,10 +23,15 @@ export const FeatureList = React.memo( () => {
 			<Card className="features-helper__current-features">
 				<div className={ headerClass }>Current XL Projects</div>
 				{ currentXLProjects.map( ( flag ) => {
-					if ( config.isEnabled( flag ) ) {
-						return <div className={ itemClass + enabledClass }>{ flag } ON</div>;
-					}
-					return <div className={ itemClass + disabledClass }>{ flag } OFF</div>;
+					const isFlagEnabled = config.isEnabled( flag );
+					return (
+						<div
+							key={ flag }
+							className={ [ itemClass, isFlagEnabled ? enabledClass : disabledClass ].join( ' ' ) }
+						>
+							{ flag } { isFlagEnabled ? 'ON' : 'OFF' }
+						</div>
+					);
 				} ) }
 				<div className={ headerClass }>All Enabled Features</div>
 				{ enabledFeatures.map( ( flag ) => (
