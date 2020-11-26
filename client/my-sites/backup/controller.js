ca10a3847fe72b4ccbecea4d7b7dd28232f547cf
Renzo Canepa <rcanepag@gmail.com>
2020-11-26 10:41:21 -0300

    Jetpack: reset AL's filter selection when entering the Backup section (#47764)
    * Reset AL filter selection when visiting /backup
    
    * Add debug to /backup controllers

--- client/my-sites/backup/controller.js
+++ client/my-sites/backup/controller.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React from 'react';
+import Debug from 'debug';
 
 /**
  * Internal dependencies
@@ -14,6 +15,7 @@ import WPCOMBackupUpsell from './wpcom-backup-upsell';
 import BackupPlaceholder from 'calypso/components/jetpack/backup-placeholder';
 import FormattedHeader from 'calypso/components/formatted-header';
 import SidebarNavigation from 'calypso/my-sites/sidebar-navigation';
+import { setFilter } from 'calypso/state/activity-log/actions';
 import getRewindState from 'calypso/state/selectors/get-rewind-state';
 import isJetpackSiteMultiSite from 'calypso/state/sites/selectors/is-jetpack-site-multi-site';
 import getSelectedSiteId from 'calypso/state/ui/selectors/get-selected-site-id';
@@ -23,7 +25,11 @@ import { isJetpackBackupSlug } from 'calypso/lib/products-values';
 import HasVaultPressSwitch from 'calypso/components/jetpack/has-vaultpress-switch';
 import IsJetpackDisconnectedSwitch from 'calypso/components/jetpack/is-jetpack-disconnected-switch';
 
+const debug = new Debug( 'calypso:my-sites:backup:controller' );
+
 export function showUpsellIfNoBackup( context, next ) {
+	debug( 'controller: showUpsellIfNoBackup', context.params );
+
 	const UpsellComponent = isJetpackCloud() ? BackupUpsell : WPCOMBackupUpsell;
 	context.primary = (
 		<>
@@ -49,6 +55,8 @@ export function showUpsellIfNoBackup( context, next ) {
 }
 
 export function showJetpackIsDisconnected( context, next ) {
+	debug( 'controller: showJetpackIsDisconnected', context.params );
+
 	const JetpackConnectionFailed = isJetpackCloud() ? (
 		<BackupUpsell reason="no_connected_jetpack" />
 	) : (
@@ -64,6 +72,8 @@ export function showJetpackIsDisconnected( context, next ) {
 }
 
 export function showUnavailableForVaultPressSites( context, next ) {
+	debug( 'controller: showUnavailableForVaultPressSites', context.params );
+
 	const message = isJetpackCloud() ? (
 		<BackupUpsell reason="vp_active_on_site" />
 	) : (
@@ -78,6 +88,8 @@ export function showUnavailableForVaultPressSites( context, next ) {
 }
 
 export function showUnavailableForMultisites( context, next ) {
+	debug( 'controller: showUnavailableForMultisites', context.params );
+
 	const state = context.store.getState();
 	const siteId = getSelectedSiteId( state );
 	if ( isJetpackSiteMultiSite( state, siteId ) ) {
@@ -93,6 +105,19 @@ export function showUnavailableForMultisites( context, next ) {
 
 /* handles /backup/:site, see `backupMainPath` */
 export function backups( context, next ) {
+	debug( 'controller: backups', context.params );
+
+	// When a user visits `/backup`, we don't want to carry over any filter
+	// selection that could've happened in the Activity Log, otherwise,
+	// the app will render the `SearchResults` component instead of the
+	// `BackupStatus`.
+	const state = context.store.getState();
+	const siteId = getSelectedSiteId( state );
+	context.store.dispatch( {
+		...setFilter( siteId, {} ),
+		meta: { skipUrlUpdate: true },
+	} );
+
 	const { date } = context.query;
 
 	context.primary = <BackupsPage queryDate={ date } />;
@@ -101,6 +126,8 @@ export function backups( context, next ) {
 
 /* handles /backup/:site/download/:rewindId, see `backupDownloadPath` */
 export function backupDownload( context, next ) {
+	debug( 'controller: backupDownload', context.params );
+
 	context.primary = (
 		<BackupRewindFlow rewindId={ context.params.rewindId } purpose={ RewindFlowPurpose.DOWNLOAD } />
 	);
@@ -109,6 +136,8 @@ export function backupDownload( context, next ) {
 
 /* handles /backup/:site/restore/:rewindId, see `backupRestorePath` */
 export function backupRestore( context, next ) {
+	debug( 'controller: backupRestore', context.params );
+
 	context.primary = (
 		<BackupRewindFlow rewindId={ context.params.rewindId } purpose={ RewindFlowPurpose.RESTORE } />
 	);
