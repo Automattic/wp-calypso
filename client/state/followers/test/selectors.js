f983f725d2240ae7cfffc6a9f60ae96a0bc6601a
Markus Dobmann <m.do@posteo.net>
2020-11-26 18:23:39 +0100

    fix followers selectors tests

--- client/state/followers/test/selectors.js
+++ client/state/followers/test/selectors.js
@@ -8,102 +8,100 @@ import {
 } from '../selectors';
 import { getSerializedQuery } from '../utils';
 
-describe( 'selctors', () => {
-	describe( '#getFollowersForQuery()', () => {
-		test( 'should should only return followers that match query', () => {
-			const state = {
-				followers: {
-					items: {
-						101: {
-							ID: 101,
-							avatar: 'https://1.gravatar.com/avatar',
-							avatar_URL: 'https://1.gravatar.com/avatar',
-							login: 'test101',
-							label: 'Test Person101',
-							display_name: 'test101',
-						},
-						201: {
-							ID: 201,
-							avatar: 'https://1.gravatar.com/avatar',
-							avatar_URL: 'https://1.gravatar.com/avatar',
-							login: 'test201',
-							label: 'Test Person201',
-							display_name: 'test201',
-						},
+describe( '#getFollowersForQuery()', () => {
+	test( 'should should only return followers that match query', () => {
+		const state = {
+			followers: {
+				items: {
+					101: {
+						ID: 101,
+						avatar: 'https://1.gravatar.com/avatar',
+						avatar_URL: 'https://1.gravatar.com/avatar',
+						login: 'test101',
+						label: 'Test Person101',
+						display_name: 'test101',
 					},
-					queries: {
-						[ getSerializedQuery( { siteId: 555 } ) ]: {
-							ids: [ 101 ],
-						},
+					201: {
+						ID: 201,
+						avatar: 'https://1.gravatar.com/avatar',
+						avatar_URL: 'https://1.gravatar.com/avatar',
+						login: 'test201',
+						label: 'Test Person201',
+						display_name: 'test201',
 					},
 				},
-			};
-			const query = { siteId: 555 };
-			expect( getFollowersForQuery( state, query ) ).toEqual( [ state.followers.items[ 101 ] ] );
-		} );
-
-		test( 'should return null for untracked queries', () => {
-			const state = {
-				followers: {
-					queries: {},
+				queries: {
+					[ getSerializedQuery( { siteId: 555 } ) ]: {
+						ids: [ 101 ],
+					},
 				},
-			};
-			const query = { siteId: 555 };
-			expect( getFollowersForQuery( state, query ) ).toBeNull;
-		} );
+			},
+		};
+		const query = { siteId: 555 };
+		expect( getFollowersForQuery( state, query ) ).toEqual( [ state.followers.items[ 101 ] ] );
 	} );
 
-	describe( '#isFetchingFollowersForQuery', () => {
-		test( 'should return true if currently fetching', () => {
-			const serializedQuery = getSerializedQuery( { siteId: 555 } );
-			const state = {
-				followers: {
-					queryRequests: {
-						[ serializedQuery ]: true,
-					},
+	test( 'should return an empty array for untracked queries', () => {
+		const state = {
+			followers: {
+				queries: {},
+			},
+		};
+		const query = { siteId: 555 };
+		expect( getFollowersForQuery( state, query ) ).toEqual( [] );
+	} );
+} );
+
+describe( '#isFetchingFollowersForQuery', () => {
+	test( 'should return true if currently fetching', () => {
+		const serializedQuery = getSerializedQuery( { siteId: 555 } );
+		const state = {
+			followers: {
+				queryRequests: {
+					[ serializedQuery ]: true,
 				},
-			};
-			expect( isFetchingFollowersForQuery( state, { siteId: 555 } ) ).toBe( true );
-		} );
-
-		test( 'should return false if not currently fetching', () => {
-			const serializedQuery = getSerializedQuery( { siteId: 555 } );
-			const state = {
-				followers: {
-					queryRequests: {
-						[ serializedQuery ]: false,
-					},
-				},
-			};
-			expect( isFetchingFollowersForQuery( state, { siteId: 555 } ) ).toBe( false );
-		} );
-
-		test( 'should return false if no data is available', () => {
-			const state = { followers: { queryRequests: {} } };
-
-			expect( isFetchingFollowersForQuery( state, { siteId: 555 } ) ).toBe( false );
-		} );
+			},
+		};
+		expect( isFetchingFollowersForQuery( state, { siteId: 555 } ) ).toBe( true );
 	} );
 
-	describe( '#getTotalFollowersForQuery', () => {
-		test( 'should return the total number of followers for a query', () => {
-			const serializedQuery = getSerializedQuery( { siteId: 555 } );
-			const state = {
-				followers: {
-					queries: {
-						[ serializedQuery ]: {
-							total: 5,
-						},
+	test( 'should return false if not currently fetching', () => {
+		const serializedQuery = getSerializedQuery( { siteId: 555 } );
+		const state = {
+			followers: {
+				queryRequests: {
+					[ serializedQuery ]: false,
+				},
+			},
+		};
+		expect( isFetchingFollowersForQuery( state, { siteId: 555 } ) ).toBe( false );
+	} );
+
+	test( 'should return false if no data is available', () => {
+		const state = { followers: { queryRequests: {} } };
+
+		expect( isFetchingFollowersForQuery( state, { siteId: 555 } ) ).toBe( false );
+	} );
+} );
+
+describe( '#getTotalFollowersForQuery', () => {
+	test( 'should return the total number of followers for a query', () => {
+		const serializedQuery = getSerializedQuery( { siteId: 555 } );
+		const state = {
+			followers: {
+				queries: {
+					[ serializedQuery ]: {
+						total: 5,
 					},
 				},
-			};
-			expect( getTotalFollowersForQuery( state, { siteId: 555 } ) ).toBe( 5 );
-		} );
+			},
+		};
+		expect( getTotalFollowersForQuery( state, { siteId: 555 } ) ).toBe( 5 );
+	} );
 
-		test( "should return `undefined` if there's no data", () => {
-			const state = { followers: { queries: {} } };
+	test( "should return `undefined` if there's no data", () => {
+		const state = { followers: { queries: {} } };
 
-			expect( getTotalFollowersForQuery( state, { siteId: 555 } ) ).toBeUndefined();
-		} );
+		expect( getTotalFollowersForQuery( state, { siteId: 555 } ) ).toBeUndefined();
 	} );
 } );
