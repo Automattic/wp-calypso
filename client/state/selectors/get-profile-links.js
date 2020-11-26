fc0a94050dc4ba5e03787d4ea4e02bd7a5675cc8
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2020-11-26 13:51:52 +0200

    State: Modularize profile links (#47765)

--- client/state/selectors/get-profile-links.js
+++ client/state/selectors/get-profile-links.js
@@ -1,9 +1,14 @@
 /**
+ * Internal dependencies
+ */
+import 'calypso/state/profile-links/init';
+
+/**
  * Returns all profile links of the current user.
  *
  * @param {object}  state  Global state tree
  * @returns {?Array}        Profile links
  */
 export default function getProfileLinks( state ) {
-	return state.userProfileLinks.items;
+	return state.userProfileLinks?.items;
 }
