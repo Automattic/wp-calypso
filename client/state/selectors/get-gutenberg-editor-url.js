df71e18b99ac1abf9a9ea32646620830a13fa701
Jeremy Herve <jeremy@jeremy.hu>
2020-12-01 18:31:50 +0100

    Calypso editor: do not set Calypsoify on Jetpack 9.2
    Calypsoify is currently broken in Jetpack 9.2:
    https://github.com/Automattic/jetpack/pull/17939
    
    Until a point release is available, this may avoid creating Fatal errors on sites that do not have Calypsoify enabled yet.

--- client/state/selectors/get-gutenberg-editor-url.js
+++ client/state/selectors/get-gutenberg-editor-url.js
@@ -2,10 +2,13 @@
  * Internal dependencies
  */
 import { isEligibleForGutenframe } from 'calypso/state/gutenberg-iframe-eligible/is-eligible-for-gutenframe';
+import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 import { getSelectedEditor } from 'calypso/state/selectors/get-selected-editor';
 import { getSiteAdminUrl, getSiteSlug } from 'calypso/state/sites/selectors';
+import getSiteOption from 'calypso/state/sites/selectors/get-site-option';
 import { getEditorPath } from 'calypso/state/editor/selectors';
 import { addQueryArgs } from 'calypso/lib/route';
+import versionCompare from 'calypso/lib/version-compare';
 
 export const getGutenbergEditorUrl = ( state, siteId, postId = null, postType = 'post' ) => {
 	if ( ! isEligibleForGutenframe( state, siteId ) ) {
@@ -16,8 +19,22 @@ export const getGutenbergEditorUrl = ( state, siteId, postId = null, postType = 
 			url = `${ siteAdminUrl }post.php?post=${ postId }&action=edit`;
 		}
 
-		if ( 'gutenberg-redirect-and-style' === getSelectedEditor( state, siteId ) ) {
+		// On some Jetpack sites (9.2+, not Atomic),
+		// Calypsoify is currently broken.
+		// Let's not enable it for them.
+		// Reference: https://github.com/Automattic/jetpack/pull/17939
+		const jetpackVersion = getSiteOption( state, siteId, 'jetpack_version' );
+		const isBrokenJetpack =
+			jetpackVersion &&
+			versionCompare( jetpackVersion, '9.2-alpha', '>=' ) &&
+			! isAtomicSite( state, siteId );
+		if (
+			'gutenberg-redirect-and-style' === getSelectedEditor( state, siteId ) &&
+			! isBrokenJetpack
+		) {
 			url = addQueryArgs( { calypsoify: '1' }, url );
+		} else {
+			url = addQueryArgs( { calypsoify: '0' }, url );
 		}
 
 		return url;
