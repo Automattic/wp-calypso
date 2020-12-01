a1e7eaf2b536f11cdaec6a53dae09c2279f6883d
Philip Jackson <p-jackson@live.com>
2020-12-01 19:52:20 +1300

    Flip the domain picker illustration in RTL languages

--- packages/domain-picker/src/domain-name-explanation/index.tsx
+++ packages/domain-picker/src/domain-name-explanation/index.tsx
@@ -2,9 +2,11 @@
  * External dependencies
  */
 import React, { FunctionComponent } from 'react';
-import { __ } from '@wordpress/i18n';
+import { useI18n } from '@automattic/react-i18n';
 
 export const DomainNameExplanationImage: FunctionComponent = () => {
+	const { __, isRTL } = useI18n();
+
 	return (
 		<svg
 			version="1.1"
@@ -15,20 +17,23 @@ export const DomainNameExplanationImage: FunctionComponent = () => {
 			viewBox="0 0 300 40"
 			xmlSpace="preserve"
 			width="300"
+			transform={ isRTL() ? 'scale(-1,1)' : undefined }
 		>
 			<rect x="0" width="310" height="50" rx="10" fill="#D8D8D8" />
 			<rect x="8" y="8" width="25" height="25" rx="5" fill="#fff" />
 			<rect x="40" y="8" width="25" height="25" rx="5" fill="#fff" />
 			<rect x="72" y="8" width="300" height="25" rx="5" fill="#fff" />
-			<text x="80" y="26" fill="#999">
-				https://
-			</text>
-			<text x="133" y="26" fill="#515151">
-				{
-					/* translators: An example domain name. Used to describe what a domain name is. */
-					__( 'example.com', __i18n_text_domain__ )
-				}
-			</text>
+			<g direction="ltr" transform={ isRTL() ? 'scale(-1,1) translate(-305,0)' : undefined }>
+				<text x="80" y="26" fill="#999">
+					https://
+				</text>
+				<text x="133" y="26" fill="#515151">
+					{
+						/* translators: An example domain name. Used to describe what a domain name is. */
+						__( 'example.com', __i18n_text_domain__ )
+					}
+				</text>
+			</g>
 		</svg>
 	);
 };
