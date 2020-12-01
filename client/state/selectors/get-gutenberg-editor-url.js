a39c6cf9ea3f16ce3ac8819881b453e5691debac
Jeremy Herve <jeremy@jeremy.hu>
2020-12-01 18:57:37 +0100

    Check against broken Jetpack version earlier

--- client/state/selectors/get-gutenberg-editor-url.js
+++ client/state/selectors/get-gutenberg-editor-url.js
@@ -11,7 +11,17 @@ import { addQueryArgs } from 'calypso/lib/route';
 import versionCompare from 'calypso/lib/version-compare';
 
 export const getGutenbergEditorUrl = ( state, siteId, postId = null, postType = 'post' ) => {
-	if ( ! isEligibleForGutenframe( state, siteId ) ) {
+	// On some Jetpack sites (9.2+, not Atomic),
+	// Calypsoify is currently broken.
+	// Let's not enable it for them.
+	// Reference: https://github.com/Automattic/jetpack/pull/17939
+	const jetpackVersion = getSiteOption( state, siteId, 'jetpack_version' );
+	const isBrokenJetpack =
+		jetpackVersion &&
+		versionCompare( jetpackVersion, '9.2-alpha', '>=' ) &&
+		! isAtomicSite( state, siteId );
+
+	if ( ! isEligibleForGutenframe( state, siteId ) && ! isBrokenJetpack ) {
 		const siteAdminUrl = getSiteAdminUrl( state, siteId );
 		let url = `${ siteAdminUrl }post-new.php?post_type=${ postType }`;
 
@@ -19,22 +29,8 @@ export const getGutenbergEditorUrl = ( state, siteId, postId = null, postType = 
 			url = `${ siteAdminUrl }post.php?post=${ postId }&action=edit`;
 		}
 
-		// On some Jetpack sites (9.2+, not Atomic),
-		// Calypsoify is currently broken.
-		// Let's not enable it for them.
-		// Reference: https://github.com/Automattic/jetpack/pull/17939
-		const jetpackVersion = getSiteOption( state, siteId, 'jetpack_version' );
-		const isBrokenJetpack =
-			jetpackVersion &&
-			versionCompare( jetpackVersion, '9.2-alpha', '>=' ) &&
-			! isAtomicSite( state, siteId );
-		if (
-			'gutenberg-redirect-and-style' === getSelectedEditor( state, siteId ) &&
-			! isBrokenJetpack
-		) {
+		if ( 'gutenberg-redirect-and-style' === getSelectedEditor( state, siteId ) ) {
 			url = addQueryArgs( { calypsoify: '1' }, url );
-		} else {
-			url = addQueryArgs( { calypsoify: '0' }, url );
 		}
 
 		return url;
