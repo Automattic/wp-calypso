7d795dcb96684ccfab1164e9507cd5642a1db2a8
Kevin Zoschke <kevin.zoschke@automattic.com>
2020-12-02 14:05:10 -0500

    Pricing page: implement jetpack_pricing_switch_plan_sides experiment

---
+++ client/my-sites/plans/jetpack-plans/experiments.ts
@@ -0,0 +1,6 @@
+/**
+ * Experiment: jetpack_pricing_switch_plan_sides
+ */
+export const SWITCH_PLAN_SIDES_EXPERIMENT = 'jetpack_pricing_switch_plan_sides';
+export const SWITCH_PLAN_SIDES_CONTROL = 'control';
+export const SWITCH_PLAN_SIDES_TREATMENT = 'treatment';
