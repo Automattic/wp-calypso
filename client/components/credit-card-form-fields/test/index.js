0b59f0868e44f3d6db71c4f4c55d58f737687358
nbloomf <nathan.bloomfield@automattic.com>
2020-12-03 13:32:04 -0600

    Remove tests for ebanx in CreditCardFormFields, since we got rid of them

--- client/components/credit-card-form-fields/test/index.js
+++ client/components/credit-card-form-fields/test/index.js
@@ -13,12 +13,7 @@ import { identity, noop } from 'lodash';
  * Internal dependencies
  */
 import { CreditCardFormFields } from '../';
-import { shouldRenderAdditionalCountryFields } from 'calypso/lib/checkout/processor-specific';
 import CountrySpecificPaymentFields from 'calypso/my-sites/checkout/checkout/country-specific-payment-fields';
-
-jest.mock( 'lib/checkout/processor-specific', () => ( {
-	shouldRenderAdditionalCountryFields: jest.fn( false ),
-} ) );
 
 const defaultProps = {
 	card: {},
@@ -39,26 +34,5 @@ describe( 'CreditCardFormFields', () => {
 	test( 'should not render ebanx fields', () => {
 		const wrapper = shallow( <CreditCardFormFields { ...defaultProps } /> );
 		expect( wrapper.find( CountrySpecificPaymentFields ) ).toHaveLength( 0 );
-	} );
-
-	describe( 'with ebanx activated', () => {
-		beforeAll( () => {
-			shouldRenderAdditionalCountryFields.mockReturnValue( true );
-		} );
-		afterAll( () => {
-			shouldRenderAdditionalCountryFields.mockReturnValue( false );
-		} );
-
-		test( 'should display Ebanx fields when an Ebanx payment country is selected and there is a transaction in process', () => {
-			const wrapper = shallow( <CreditCardFormFields { ...defaultProps } /> );
-			wrapper.setProps( { card: { country: 'BR' } } );
-			expect( wrapper.find( CountrySpecificPaymentFields ) ).toHaveLength( 1 );
-		} );
-
-		test( 'should not display Ebanx fields when there is a transaction in process', () => {
-			const wrapper = shallow( <CreditCardFormFields { ...defaultProps } /> );
-			wrapper.setProps( { card: { country: 'BR' }, isNewTransaction: false } );
-			expect( wrapper.find( CountrySpecificPaymentFields ) ).toHaveLength( 0 );
-		} );
 	} );
 } );
