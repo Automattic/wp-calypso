cadc2fb0b9295a5de62988168e3216bfa82eee64
Delan Azabani <delan@azabani.com>
2020-12-03 23:48:14 +1100

    Add performance note + README for the Rewind vendor

--- client/state/selectors/get-jetpack-credentials-update-progress.js
+++ client/state/selectors/get-jetpack-credentials-update-progress.js
@@ -10,6 +10,8 @@ import 'calypso/state/jetpack/init';
 import { CredentialsTestProgress } from '../data-layer/wpcom/activity-log/update-credentials/vendor';
 
 export default function getJetpackCredentialsUpdateProgress( state, siteId ) {
+	// This is O(n²) over n updates, but even the worst update attempts have < 50 updates, and I
+	// don’t know how to avoid this without rewriting the state machine as a Redux reducer.
 	return new CredentialsTestProgress.Parser(
 		...get( state, [ 'jetpack', 'credentials', 'progressUpdates', siteId ], [] )
 	);
