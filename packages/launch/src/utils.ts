52a2c46df45cb70a8513e3946b87d33abcda8be1
Tom Cafferkey <tjcafferkey@gmail.com>
2020-12-03 11:26:26 +0000

    Remove comment which refers to a function argument that no longer exists

--- packages/launch/src/utils.ts
+++ packages/launch/src/utils.ts
@@ -7,9 +7,6 @@ import type { Plans, DomainSuggestions } from '@automattic/data-stores';
 
 const DEFAULT_SITE_NAME = __( 'Site Title', __i18n_text_domain__ );
 
-// When `exact === false', the check is more relaxed — chances are that if the title
-// matches (even it not exactly) the default title, the user would like want
-// to change it before launch.
 export const isDefaultSiteTitle = ( {
 	currentSiteTitle = '',
 }: {
