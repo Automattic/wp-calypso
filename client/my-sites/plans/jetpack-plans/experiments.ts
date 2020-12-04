ab926336e64a6dc509564ed55d6c796799404c3b
Kevin Zoschke <kevin.zoschke@automattic.com>
2020-12-04 10:57:57 -0500

    Update `jetpack_pricing_switch_plan_sides` experiment name

--- client/my-sites/plans/jetpack-plans/experiments.ts
+++ client/my-sites/plans/jetpack-plans/experiments.ts
@@ -1,6 +1,6 @@
 /**
  * Experiment: jetpack_pricing_switch_plan_sides
  */
-export const SWITCH_PLAN_SIDES_EXPERIMENT = 'jetpack_pricing_switch_plan_sides';
+export const SWITCH_PLAN_SIDES_EXPERIMENT = 'jetpack_pricing_switch_plan_sides2';
 export const SWITCH_PLAN_SIDES_CONTROL = 'control';
 export const SWITCH_PLAN_SIDES_TREATMENT = 'treatment';
