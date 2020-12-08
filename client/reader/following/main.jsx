90db341919e77cc72b30b164ead6788e742b435d
Andrei Demian <unDemian@users.noreply.github.com>
2020-12-08 13:03:16 +0200

    Add mark as seen tracks (#48015)
    * Add mark as seen tracks
    
    * Use reader tracks
    
    * remove not needed

--- client/reader/following/main.jsx
+++ client/reader/following/main.jsx
@@ -22,6 +22,7 @@ import FollowingIntro from './intro';
 import { getSearchPlaceholderText } from 'calypso/reader/search/utils';
 import SectionHeader from 'calypso/components/section-header';
 import { requestMarkAllAsSeen } from 'calypso/state/reader/seen-posts/actions';
+import { recordReaderTracksEvent } from 'calypso/state/reader/analytics/actions';
 import { SECTION_FOLLOWING } from 'calypso/state/reader/seen-posts/constants';
 import { getReaderOrganizationFeedsInfo } from 'calypso/state/reader/organizations/selectors';
 import { NO_ORG_ID } from 'calypso/state/reader/organizations/constants';
@@ -57,6 +58,7 @@ const FollowingStream = ( props ) => {
 	const voteBanner = <FollowingVoteBanner />;
 	const markAllAsSeen = ( feedsInfo ) => {
 		const { feedIds, feedUrls } = feedsInfo;
+		dispatch( recordReaderTracksEvent( 'calypso_reader_mark_all_as_seen_clicked' ) );
 		dispatch( requestMarkAllAsSeen( { identifier: SECTION_FOLLOWING, feedIds, feedUrls } ) );
 	};
 
