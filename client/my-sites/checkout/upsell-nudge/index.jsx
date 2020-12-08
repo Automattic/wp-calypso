6acab468cf353ad7c26f5c27749eeaf5b7db18b1
Payton Swick <payton@foolord.com>
2020-12-08 16:12:34 -0500

    Wrap UpsellNudge with withShoppingCart

--- client/my-sites/checkout/upsell-nudge/index.jsx
+++ client/my-sites/checkout/upsell-nudge/index.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import page from 'page';
 import { omit } from 'lodash';
+import { withShoppingCart } from '@automattic/shopping-cart';
 
 /**
  * Internal dependencies
@@ -362,4 +363,4 @@ export default connect(
 	{
 		trackUpsellButtonClick,
 	}
-)( localize( UpsellNudge ) );
+)( withShoppingCart( localize( UpsellNudge ) ) );
