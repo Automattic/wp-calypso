4e50a32783be0dfd01d9eb602b8bd6837af81046
Stefan Nieuwenhuis <stefan.nieuwenhuis@automattic.com>
2020-12-08 15:14:26 +0100

    Focused-Launch: Fix failing CircleCI Build

--- packages/onboarding/src/scroll-to-top/index.tsx
+++ packages/onboarding/src/scroll-to-top/index.tsx
@@ -4,7 +4,7 @@
 import * as React from 'react';
 import { useLocation } from 'react-router-dom';
 
-const ScrollToTop: React.FunctionComponent | null = () => {
+const ScrollToTop: React.FunctionComponent = () => {
 	const { pathname } = useLocation();
 
 	React.useEffect( () => document.querySelector( '.components-modal__content' )?.scrollTo( 0, 0 ), [
