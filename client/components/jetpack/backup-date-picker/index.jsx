8ec9ed7a9fd8470a3944e6a35608aa37464056c7
Matt Gawarecki <matt.gawarecki@gmail.com>
2020-12-09 11:14:55 -0600

    Make BackupDatePicker a functional component

--- client/components/jetpack/backup-date-picker/index.jsx
+++ client/components/jetpack/backup-date-picker/index.jsx
@@ -3,15 +3,16 @@
  */
 import { localize } from 'i18n-calypso';
 import classNames from 'classnames';
-import page from 'page';
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 
 /**
  * Internal dependencies
  */
 import { isEnabled } from 'calypso/config';
+import { recordTracksEvent } from 'calypso/state/analytics/actions/record';
 import { withLocalizedMoment } from 'calypso/components/localized-moment';
+import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import Button from 'calypso/components/forms/form-button';
 import DateRangeSelector from 'calypso/my-sites/activity/filterbar/date-range-selector';
 import Gridicon from 'calypso/components/gridicon';
@@ -21,17 +22,18 @@ import Gridicon from 'calypso/components/gridicon';
  */
 import './style.scss';
 
-class BackupDatePicker extends Component {
-	static propTypes = {
-		siteId: PropTypes.number.isRequired,
-		selectedDate: PropTypes.object.isRequired,
-		onDateChange: PropTypes.func.isRequired,
-		oldestDateAvailable: PropTypes.object,
-	};
-
-	getDisplayDate = ( date, showTodayYesterday = true ) => {
-		const { today, moment, translate } = this.props;
-
+const BackupDatePicker = ( {
+	siteId,
+	siteSlug,
+	today,
+	selectedDate,
+	onDateChange,
+	oldestDateAvailable,
+	moment,
+	translate,
+	dispatchRecordTracksEvent,
+} ) => {
+	const getDisplayDate = ( date, showTodayYesterday = true ) => {
 		if ( showTodayYesterday ) {
 			const isToday = today.isSame( date, 'day' );
 			const isYesterday = moment( today ).add( -1, 'day' ).isSame( date, 'day' );
@@ -51,49 +53,41 @@ class BackupDatePicker extends Component {
 		return moment( date ).format( dateFormat );
 	};
 
-	goToPreviousDay = () => {
-		if ( ! this.canGoToPreviousDay() ) {
+	const previousDate = moment( selectedDate ).subtract( 1, 'days' );
+	const nextDate = moment( selectedDate ).add( 1, 'days' );
+
+	const previousDisplayDate = getDisplayDate( previousDate );
+	const nextDisplayDate = getDisplayDate( nextDate, false );
+
+	const canGoToPreviousDay = () =>
+		! oldestDateAvailable || ! moment( selectedDate ).isSame( oldestDateAvailable, 'day' );
+	const canGoToNextDay = () => ! moment( today ).isSame( moment( selectedDate ), 'day' );
+
+	const goToPreviousDay = () => {
+		if ( ! canGoToPreviousDay() ) {
 			return false;
 		}
 
-		const { moment, onDateChange, selectedDate } = this.props;
 		const newSelectedDate = moment( selectedDate ).subtract( 1, 'days' );
 
-		this.props.dispatchRecordTracksEvent( 'calypso_jetpack_backup_date_previous' );
+		dispatchRecordTracksEvent( 'calypso_jetpack_backup_date_previous' );
 
 		onDateChange( newSelectedDate );
 	};
 
-	goToNextDay = () => {
-		if ( ! this.canGoToNextDay() ) {
+	const goToNextDay = () => {
+		if ( ! canGoToNextDay() ) {
 			return false;
 		}
 
-		const { moment, onDateChange, selectedDate } = this.props;
 		const newSelectedDate = moment( selectedDate ).add( 1, 'days' );
 
-		this.props.dispatchRecordTracksEvent( 'calypso_jetpack_backup_date_next' );
+		dispatchRecordTracksEvent( 'calypso_jetpack_backup_date_next' );
 
 		onDateChange( newSelectedDate );
 	};
 
-	canGoToPreviousDay = () => {
-		const { moment, selectedDate, oldestDateAvailable } = this.props;
-
-		return ! oldestDateAvailable || ! moment( selectedDate ).isSame( oldestDateAvailable, 'day' );
-	};
-
-	canGoToNextDay = () => {
-		const { today, moment, selectedDate } = this.props;
-
-		return ! moment( today ).isSame( moment( selectedDate ), 'day' );
-	};
-
-	goToActivityLog = () => {
-		page.redirect( `/activity-log/${ this.props.siteSlug }` );
-	};
-
-	onSpace = ( evt, fn ) => {
+	const onSpace = ( evt, fn ) => {
 		if ( evt.key === ' ' ) {
 			return fn;
 		}
@@ -101,88 +95,82 @@ class BackupDatePicker extends Component {
 		return () => {};
 	};
 
-	render() {
-		const { dispatchRecordTracksEvent, selectedDate, siteId, siteSlug, moment } = this.props;
+	return (
+		<div className="backup-date-picker">
+			<div className="backup-date-picker__select-date">
+				<div
+					className="backup-date-picker__select-date--previous"
+					role="button"
+					tabIndex={ 0 }
+					onClick={ goToPreviousDay }
+					onKeyDown={ onSpace( goToPreviousDay ) }
+				>
+					<Button compact borderless className="backup-date-picker__button--previous">
+						<Gridicon icon="chevron-left" className={ ! canGoToPreviousDay() && 'disabled' } />
+					</Button>
 
-		const previousDate = moment( selectedDate ).subtract( 1, 'days' );
-		const nextDate = moment( selectedDate ).add( 1, 'days' );
-
-		const previousDisplayDate = this.getDisplayDate( previousDate );
-		const nextDisplayDate = this.getDisplayDate( nextDate, false );
-
-		return (
-			<div className="backup-date-picker">
-				<div className="backup-date-picker__select-date">
-					<div
-						className="backup-date-picker__select-date--previous"
-						role="button"
-						tabIndex={ 0 }
-						onClick={ this.goToPreviousDay }
-						onKeyDown={ this.onSpace( this.goToPreviousDay ) }
+					<span
+						className={ classNames( 'backup-date-picker__display-date', {
+							disabled: ! canGoToPreviousDay(),
+						} ) }
 					>
-						<Button compact borderless className="backup-date-picker__button--previous">
-							<Gridicon
-								icon="chevron-left"
-								className={ ! this.canGoToPreviousDay() && 'disabled' }
-							/>
-						</Button>
+						{ previousDisplayDate }
+					</span>
+				</div>
 
+				{ isEnabled( 'jetpack/backups-date-picker' ) && (
+					<DateRangeSelector
+						siteId={ siteId }
+						enabled={ true }
+						customLabel={ <Gridicon icon="calendar" /> }
+					/>
+				) }
+
+				<div
+					className="backup-date-picker__select-date--next"
+					role="button"
+					tabIndex={ 0 }
+					onClick={ goToNextDay }
+					onKeyDown={ onSpace( goToNextDay ) }
+				>
+					<div className="backup-date-picker__next-date-link">
 						<span
 							className={ classNames( 'backup-date-picker__display-date', {
-								disabled: ! this.canGoToPreviousDay(),
+								disabled: ! canGoToNextDay(),
 							} ) }
 						>
-							{ previousDisplayDate }
+							{ nextDisplayDate }
 						</span>
-					</div>
 
+						<Button compact borderless className="backup-date-picker__button--next">
+							<Gridicon icon="chevron-right" className={ ! canGoToNextDay() && 'disabled' } />
+						</Button>
+					</div>
 					{ isEnabled( 'jetpack/backups-date-picker' ) && (
-						<DateRangeSelector
-							siteId={ siteId }
-							enabled={ true }
-							customLabel={ <Gridicon icon="calendar" /> }
-						/>
+						<a
+							className="backup-date-picker__search-link"
+							href={ `/activity-log/${ siteSlug }` }
+							onClick={ () => {
+								dispatchRecordTracksEvent( 'calypso_jetpack_backup_search' );
+							} }
+						>
+							<Gridicon icon="search" className="backup-date-picker__search-icon" />
+						</a>
 					) }
-
-					<div
-						className="backup-date-picker__select-date--next"
-						role="button"
-						tabIndex={ 0 }
-						onClick={ this.goToNextDay }
-						onKeyDown={ this.onSpace( this.goToNextDay ) }
-					>
-						<div className="backup-date-picker__next-date-link">
-							<span
-								className={ classNames( 'backup-date-picker__display-date', {
-									disabled: ! this.canGoToNextDay(),
-								} ) }
-							>
-								{ nextDisplayDate }
-							</span>
-
-							<Button compact borderless className="backup-date-picker__button--next">
-								<Gridicon
-									icon="chevron-right"
-									className={ ! this.canGoToNextDay() && 'disabled' }
-								/>
-							</Button>
-						</div>
-						{ isEnabled( 'jetpack/backups-date-picker' ) && (
-							<a
-								className="backup-date-picker__search-link"
-								href={ `/activity-log/${ siteSlug }` }
-								onClick={ () => {
-									dispatchRecordTracksEvent( 'calypso_jetpack_backup_search' );
-								} }
-							>
-								<Gridicon icon="search" className="backup-date-picker__search-icon" />
-							</a>
-						) }
-					</div>
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
-export default localize( withLocalizedMoment( BackupDatePicker ) );
+export default connect(
+	( state ) => {
+		return {
+			siteId: getSelectedSiteId( state ),
+			siteSlug: getSelectedSiteSlug( state ),
+		};
+	},
+	{
+		dispatchRecordTracksEvent: recordTracksEvent,
+	}
+)( localize( withLocalizedMoment( BackupDatePicker ) ) );
