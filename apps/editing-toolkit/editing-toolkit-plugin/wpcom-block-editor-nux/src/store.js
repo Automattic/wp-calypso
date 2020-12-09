78af0b80c54807ef00c7dc67eb06de75d26642ce
Autumn Fjeld <autumn@automattic.com>
2020-12-09 17:14:41 +1100

    Reinstate the NUX modal. This unbreaks things from last commit. Next WelcomeTour needs to be wired up

---
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/store.js
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
+import { registerStore } from '@wordpress/data';
+
+const reducer = ( state = {}, { type, isNuxEnabled } ) =>
+	'WPCOM_BLOCK_EDITOR_NUX_SET_STATUS' === type ? { ...state, isNuxEnabled } : state;
+
+const actions = {
+	setWpcomNuxStatus: ( { isNuxEnabled, bypassApi } ) => {
+		if ( ! bypassApi ) {
+			apiFetch( {
+				path: '/wpcom/v2/block-editor/nux',
+				method: 'POST',
+				data: { isNuxEnabled },
+			} );
+		}
+		return {
+			type: 'WPCOM_BLOCK_EDITOR_NUX_SET_STATUS',
+			isNuxEnabled,
+		};
+	},
+};
+
+const selectors = {
+	isWpcomNuxEnabled: ( state ) => state.isNuxEnabled,
+};
+
+registerStore( 'automattic/nux', {
+	reducer,
+	actions,
+	selectors,
+	persist: true,
+} );
