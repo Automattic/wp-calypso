d5dc33b5453f40d3c8760eca2afa34b5630bbf3a
Payton Swick <payton@foolord.com>
2020-12-08 20:11:21 -0500

    Set actual types on getDomainNameFromReceiptOrCart

--- client/lib/domains/cart-utils.js
+++ client/lib/domains/cart-utils.js
@@ -13,10 +13,10 @@ import { isDomainRegistration } from 'calypso/lib/products-values/is-domain-regi
  * Depending on the current step in checkout, the user's domain can be found in
  * either the cart or the receipt.
  *
- * @param {?object} [receipt] - The receipt for the transaction
- * @param {?object} cart - The cart for the transaction
+ * @param {import('calypso/my-sites/checkout/composite-checkout/types/wpcom-store-state').TransactionResponse} [receipt] - The receipt for the transaction
+ * @param {import('@automattic/shopping-cart').ResponseCart} cart - The cart for the transaction
  *
- * @returns {?string} the name of the first domain for the transaction.
+ * @returns {string|null} the name of the first domain for the transaction.
  */
 export function getDomainNameFromReceiptOrCart( receipt, cart ) {
 	let domainRegistration;
