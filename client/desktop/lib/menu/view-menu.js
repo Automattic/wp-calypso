92aedf4f548bea7f7c0141bf0f8b31ca1801fefa
Nicholas Sakaimbo <nsakaimbo@users.noreply.github.com>
2020-12-09 17:46:48 -0700

    Add zoom controls to View menu (#48188)

--- client/desktop/lib/menu/view-menu.js
+++ client/desktop/lib/menu/view-menu.js
@@ -16,19 +16,61 @@ if ( Config.debug ) {
 	menuItems = debugMenu;
 }
 
-menuItems.push( {
-	label: 'Toggle Full Screen',
-	accelerator: platform.isOSX() ? 'Command+Ctrl+F' : undefined,
-	fullscreen: true,
-	click: function () {
-		const focusedWindow = BrowserWindow.getFocusedWindow();
+menuItems.push(
+	{
+		label: 'Toggle Full Screen',
+		accelerator: platform.isOSX() ? 'Command+Ctrl+F' : undefined,
+		fullscreen: true,
+		click: function () {
+			const focusedWindow = BrowserWindow.getFocusedWindow();
 
-		if ( focusedWindow ) {
-			const toggle = ! focusedWindow.isFullScreen();
+			if ( focusedWindow ) {
+				const toggle = ! focusedWindow.isFullScreen();
 
-			focusedWindow.setFullScreen( toggle );
-		}
+				focusedWindow.setFullScreen( toggle );
+			}
+		},
 	},
-} );
+	{
+		type: 'separator',
+	},
+	{
+		role: 'zoomIn',
+	},
+	{
+		// enable ZoomIn shortcut to work both with and without Shift
+		// the default accelerator added by Electron is CommandOrControl+Shift+=
+		role: 'zoomIn',
+		visible: false,
+		acceleratorWorksWhenHidden: true,
+		accelerator: 'CommandOrControl+=',
+	},
+	{
+		role: 'zoomOut',
+	},
+	{
+		role: 'resetZoom',
+	},
+	// backup shortcuts for numeric keypad,
+	// see https://github.com/electron/electron/issues/5256#issuecomment-692068367
+	{
+		role: 'zoomIn',
+		visible: false,
+		acceleratorWorksWhenHidden: true,
+		accelerator: 'CommandOrControl+numadd',
+	},
+	{
+		role: 'zoomOut',
+		visible: false,
+		acceleratorWorksWhenHidden: true,
+		accelerator: 'CommandOrControl+numsub',
+	},
+	{
+		role: 'resetZoom',
+		visible: false,
+		acceleratorWorksWhenHidden: true,
+		accelerator: 'CommandOrControl+num0',
+	}
+);
 
 module.exports = menuItems;
