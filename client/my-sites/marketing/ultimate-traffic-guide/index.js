d724f0f4669aec5461e1b221b904c11e94f04d75
Aneesh Devasthale <aneeshdevasthale@gmail.com>
2020-12-11 18:50:11 +0530

    Adds alt copy for AB test

--- client/my-sites/marketing/ultimate-traffic-guide/index.js
+++ client/my-sites/marketing/ultimate-traffic-guide/index.js
@@ -19,6 +19,7 @@ import { getUserPurchases } from 'calypso/state/purchases/selectors';
 import { getCurrentUserId } from 'calypso/state/current-user/selectors';
 import { getSiteSlug } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
 
 /**
  * Style dependencies
@@ -26,6 +27,39 @@ import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import './style.scss';
 
 const WPCOM_TRAFFIC_GUIDE = 'traffic-guide';
+
+export default function UltimateTrafficGuide() {
+	const translate = useTranslate();
+	const userId = useSelector( ( state ) => getCurrentUserId( state ) );
+	const purchases = useSelector( ( state ) => getUserPurchases( state, userId ) );
+	const [ hasPurchase, setHasPurchase ] = useState( false );
+	useEffect( () => {
+		if (
+			purchases &&
+			purchases.find( ( purchase ) => WPCOM_TRAFFIC_GUIDE === purchase.productSlug )
+		) {
+			setHasPurchase( true );
+		}
+	}, [ purchases, setHasPurchase ] );
+
+	const renderPlaceholders = () =>
+		[ ...Array( 5 ) ].map( ( e, i ) => <Placeholder key={ `${ i }` } /> );
+
+	const renderContent = () => {
+		return hasPurchase ? (
+			<DownloadPage translate={ translate } />
+		) : (
+			<SalesPage translate={ translate } />
+		);
+	};
+
+	return (
+		<CompactCard className="ultimate-traffic-guide">
+			{ ! purchases && <QueryUserPurchases userId={ userId } /> }
+			{ ! purchases ? renderPlaceholders() : renderContent() }
+		</CompactCard>
+	);
+}
 
 const Placeholder = () => (
 	<div className="ultimate-traffic-guide__placeholder">
@@ -39,7 +73,7 @@ const SalesPage = ( { translate } ) => {
 	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
 	const siteSlug = useSelector( ( state ) => getSiteSlug( state, siteId ) );
 
-	return (
+	const defaultVariation = () => (
 		<>
 			<h1 className="ultimate-traffic-guide__header">
 				{ translate( 'Introducing the WordPress.com Ultimate Traffic Guide' ) }
@@ -120,7 +154,82 @@ const SalesPage = ( { translate } ) => {
 					</strong>
 				</p>
 			) }
+		</>
+	);
 
+	// const treatmentVariation = () => (
+	// 	<>
+	// 		<h1 className="ultimate-traffic-guide__header">
+	// 			{ translate(
+	// 				'Discover the most important traffic secrets used by our most successful customers'
+	// 			) }
+	// 		</h1>
+	// 		<h2 className="ultimate-traffic-guide__sub-header">
+	// 			{ translate( 'Claim your copy of the Ultimate Traffic Guide today' ) }
+	// 		</h2>
+	// 		<p>
+	// 			{ translate(
+	// 				'Do you want more traffic to your website?' +
+	// 					' Of course you do, who builds a website and doesn’t want traffic?' +
+	// 					' You’ve time building your website and you’ve just realized that “if you build it, they will come” does not always apply.'
+	// 			) }
+	// 		</p>
+	// 		<p>
+	// 			{ translate(
+	// 				'Our marketing team developed The Ultimate Traffic Guide to help WordPress.com users learn some of the same tactics and strategies our most successful customers use to build their audiences. ' +
+	// 					'This easy-to-follow and beginner-friendly guide reveals today’s best strategies and tactics for attracting more visitors.'
+	// 			) }
+	// 		</p>
+	// 		<p>
+	// 			{ translate( 'Here are just some of the topics we cover in The Ultimate Traffic Guide:' ) }
+	// 		</p>
+	// 		<ul>
+	// 			<li>
+	// 				{ translate(
+	// 					'{{b}}Use Your Traffic to Build Even More Traffic{{/b}} The Ultimate Traffic Guide covers how to use your own site stats to further increase your traffic, keep visitors on your site longer, and convert traffic into qualified leads.',
+	// 					{
+	// 						components: { b: <b /> },
+	// 					}
+	// 				) }
+	// 			</li>
+	// 			<li>
+	// 				{ translate(
+	// 					'{{b}}Leverage Social Media to Expand Your Reach{{/b}} Know where your ideal audience spends time and learn how to leverage those social channels to increase traffic.',
+	// 					{
+	// 						components: { b: <b /> },
+	// 					}
+	// 				) }
+	// 			</li>
+	// 			<li>
+	// 				{ translate(
+	// 					'{{b}}Keeping the Engagement Going{{/b}} we’ll share how to choose the right newsletter service, write compelling content, build your list, and monitor key performance indicators to manage the critical ongoing relationship with your site visitors.',
+	// 					{
+	// 						components: { b: <b /> },
+	// 					}
+	// 				) }
+	// 			</li>
+	// 		</ul>
+	// 		<p>
+	// 			{ translate(
+	// 				'You’re going to want to print out all 96 pages of the Ultimate Traffic Guide and keep it by your desk, because you’re going to consult this guide often.'
+	// 			) }
+	// 		</p>
+	// 		<p>
+	// 			{ translate(
+	// 				'TODO: Similar resources are valued up to $100, but this amazing resource is available to our users at $X. But for a limited time we’re offering the Ultimate Traffic Guide to you for just $4.95.'
+	// 			) }
+	// 		</p>
+	// 	</>
+	// );
+
+	return (
+		<>
+			<PageViewTracker
+				path={ '/marketing/ultimate-traffic-guide' }
+				title={ 'Ultimate Traffic Guide' }
+			/>
+			{ defaultVariation() }
+			{ /* { treatmentVariation() } */ }
 			<p>
 				<Button href={ `/checkout/${ siteSlug }/${ WPCOM_TRAFFIC_GUIDE }` } primary>
 					{ translate( 'Click here to buy the guide now!' ) }
@@ -145,7 +254,12 @@ const DownloadPage = ( { translate } ) => {
 			</p>
 			<p>
 				{ translate(
-					'The Ultimate Traffic GuideThis is a goldmine of traffic tips and how-tos that reveals the exact “Breakthrough Traffic” process we’ve developed over the past decade.'
+					'{{i}}The Ultimate Traffic Guide{{/i}} is a goldmine of traffic tips and how-tos that reveals the exact “Breakthrough Traffic” process we’ve developed over the past decade.',
+					{
+						components: {
+							i: <i />,
+						},
+					}
 				) }
 			</p>
 			<p>
@@ -161,36 +275,3 @@ const DownloadPage = ( { translate } ) => {
 		</>
 	);
 };
-
-export default function UltimateTrafficGuide() {
-	const translate = useTranslate();
-	const userId = useSelector( ( state ) => getCurrentUserId( state ) );
-	const purchases = useSelector( ( state ) => getUserPurchases( state, userId ) );
-	const [ hasPurchase, setHasPurchase ] = useState( false );
-	useEffect( () => {
-		if (
-			purchases &&
-			purchases.find( ( purchase ) => WPCOM_TRAFFIC_GUIDE === purchase.productSlug )
-		) {
-			setHasPurchase( true );
-		}
-	}, [ purchases, setHasPurchase ] );
-
-	const renderPlaceholders = () =>
-		[ ...Array( 5 ) ].map( ( e, i ) => <Placeholder key={ `${ i }` } /> );
-
-	const renderContent = () => {
-		return hasPurchase ? (
-			<DownloadPage translate={ translate } />
-		) : (
-			<SalesPage translate={ translate } />
-		);
-	};
-
-	return (
-		<CompactCard className="ultimate-traffic-guide">
-			{ ! purchases && <QueryUserPurchases userId={ userId } /> }
-			{ ! purchases ? renderPlaceholders() : renderContent() }
-		</CompactCard>
-	);
-}
