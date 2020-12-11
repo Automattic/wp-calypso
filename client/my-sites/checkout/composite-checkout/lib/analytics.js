b1a631fbc595cacccdd3b447b67e584b0eaf2dc2
Payton Swick <payton@foolord.com>
2020-12-10 19:37:40 -0500

    Merge recordTransactionBeginAnalytics functions

--- client/my-sites/checkout/composite-checkout/lib/analytics.js
+++ client/my-sites/checkout/composite-checkout/lib/analytics.js
@@ -4,6 +4,7 @@
 import { logToLogstash } from 'calypso/state/logstash/actions';
 import config from 'calypso/config';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import { translateCheckoutPaymentMethodToWpcomPaymentMethod } from '../lib/translate-payment-method-names';
 
 export function logStashLoadErrorEventAction( errorType, errorMessage, additionalData = {} ) {
 	return logStashEventAction( 'composite checkout load error', {
@@ -45,4 +46,35 @@ export function recordCompositeCheckoutErrorDuringAnalytics( {
 			action_type: failureDescription,
 		} )
 	);
+}
+
+export function recordTransactionBeginAnalytics( { reduxDispatch, paymentMethodId } ) {
+	try {
+		reduxDispatch( recordTracksEvent( 'calypso_checkout_form_redirect', {} ) );
+		reduxDispatch(
+			recordTracksEvent( 'calypso_checkout_form_submit', {
+				credits: null,
+				payment_method: translateCheckoutPaymentMethodToWpcomPaymentMethod( paymentMethodId ) || '',
+			} )
+		);
+		reduxDispatch(
+			recordTracksEvent( 'calypso_checkout_composite_form_submit', {
+				credits: null,
+				payment_method: translateCheckoutPaymentMethodToWpcomPaymentMethod( paymentMethodId ) || '',
+			} )
+		);
+		const paymentMethodIdForTracks = paymentMethodId.replace( /-/, '_' ).toLowerCase();
+		reduxDispatch(
+			recordTracksEvent(
+				`calypso_checkout_composite_${ paymentMethodIdForTracks }_submit_clicked`,
+				{}
+			)
+		);
+	} catch ( errorObject ) {
+		recordCompositeCheckoutErrorDuringAnalytics( {
+			reduxDispatch,
+			errorObject,
+			failureDescription: `transaction-begin: ${ paymentMethodId }`,
+		} );
+	}
 }
