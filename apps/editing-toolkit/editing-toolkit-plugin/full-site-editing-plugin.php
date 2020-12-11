0754557fd4c448992224e9aec4148e220976a637
Andrew Serong <andrewserong@users.noreply.github.com>
2020-12-11 16:03:06 +1100

    Bump Editing Toolkit plugin version to 2.8.14

--- apps/editing-toolkit/editing-toolkit-plugin/full-site-editing-plugin.php
+++ apps/editing-toolkit/editing-toolkit-plugin/full-site-editing-plugin.php
@@ -35,7 +35,7 @@ namespace A8C\FSE;
  *
  * @var string
  */
-define( 'PLUGIN_VERSION', '2.8.13' );
+define( 'PLUGIN_VERSION', '2.8.14' );
 
 // Always include these helper files for dotcom FSE.
 require_once __DIR__ . '/dotcom-fse/helpers.php';
