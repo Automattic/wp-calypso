81fd1db00fdee578d27735919f0376fce8ee1bb5
Payton Swick <payton@foolord.com>
2020-12-11 14:12:18 -0500

    Pass correct args to existingCardProcessor

--- client/my-sites/checkout/upsell-nudge/purchase-modal/index.jsx
+++ client/my-sites/checkout/upsell-nudge/purchase-modal/index.jsx
@@ -1,10 +1,12 @@
 /**
  * External dependencies
  */
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import { useDispatch } from 'react-redux';
 import { Dialog } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { CheckoutProvider } from '@automattic/composite-checkout';
+import { useStripe } from '@automattic/calypso-stripe';
 
 /**
  * Internal dependencies
@@ -15,6 +17,7 @@ import Content from './content';
 import Placeholder from './placeholder';
 import useCreatePaymentCompleteCallback from 'calypso/my-sites/checkout/composite-checkout/hooks/use-create-payment-complete-callback';
 import existingCardProcessor from 'calypso/my-sites/checkout/composite-checkout/lib/existing-card-processor';
+import getContactDetailsType from 'calypso/my-sites/checkout/composite-checkout/lib/get-contact-details-type';
 
 /**
  * Style dependencies
@@ -23,11 +26,12 @@ import './style.scss';
 
 const noop = () => null;
 
-export function PurchaseModal( { cart, cards, isCartUpdating, onClose, siteSlug } ) {
+export function PurchaseModal( { cart, cards, isCartUpdating, onClose, siteSlug, siteId } ) {
 	const translate = useTranslate();
 	const [ step, setStep ] = useState( BEFORE_SUBMIT );
 	const submitTransaction = useSubmitTransaction( {
 		cart,
+		siteId,
 		setStep,
 		storedCard: cards?.[ 0 ],
 		onClose,
@@ -53,6 +57,24 @@ export default function PurchaseModalWrapper( props ) {
 	const onComplete = useCreatePaymentCompleteCallback( {
 		isComingFromUpsell: true,
 	} );
+	const { stripeConfiguration } = useStripe();
+	const reduxDispatch = useDispatch();
+
+	const contactDetailsType = getContactDetailsType( props.cart );
+	const includeDomainDetails = contactDetailsType === 'domain';
+	const includeGSuiteDetails = contactDetailsType === 'gsuite';
+	const dataForProcessor = useMemo(
+		() => ( {
+			includeDomainDetails,
+			includeGSuiteDetails,
+			recordEvent: noop,
+			stripeConfiguration,
+			createUserAndSiteBeforeTransaction: false,
+			reduxDispatch,
+		} ),
+		[ includeDomainDetails, includeGSuiteDetails, stripeConfiguration, reduxDispatch ]
+	);
+
 	return (
 		<CheckoutProvider
 			paymentMethods={ [] }
@@ -60,7 +82,10 @@ export default function PurchaseModalWrapper( props ) {
 			showErrorMessage={ noop }
 			showInfoMessage={ noop }
 			showSuccessMessage={ noop }
-			paymentProcessors={ { 'existing-card': existingCardProcessor } }
+			paymentProcessors={ {
+				'existing-card': ( transactionData ) =>
+					existingCardProcessor( transactionData, dataForProcessor ),
+			} }
 		>
 			<PurchaseModal { ...props } />;
 		</CheckoutProvider>
