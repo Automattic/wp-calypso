ee0034c7c29704db11b220d96618cae7514fcf45
Payton Swick <payton@foolord.com>
2020-12-10 19:05:48 -0500

    Add recordCompositeCheckoutErrorDuringAnalytics

--- client/my-sites/checkout/composite-checkout/lib/analytics.js
+++ client/my-sites/checkout/composite-checkout/lib/analytics.js
@@ -3,6 +3,7 @@
  */
 import { logToLogstash } from 'calypso/state/logstash/actions';
 import config from 'calypso/config';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 
 export function logStashLoadErrorEventAction( errorType, errorMessage, additionalData = {} ) {
 	return logStashEventAction( 'composite checkout load error', {
@@ -22,4 +23,26 @@ export function logStashEventAction( message, dataForLog = {} ) {
 			...dataForLog,
 		},
 	} );
+}
+
+export function recordCompositeCheckoutErrorDuringAnalytics( {
+	errorObject,
+	failureDescription,
+	reduxDispatch,
+} ) {
+	// This is a fallback to catch any errors caused by the analytics code
+	// Anything in this block should remain very simple and extremely
+	// tolerant of any kind of data. It should make no assumptions about
+	// the data it uses. There's no fallback for the fallback!
+	reduxDispatch(
+		recordTracksEvent( 'calypso_checkout_composite_error', {
+			error_message: errorObject.message,
+			action_type: failureDescription,
+		} )
+	);
+	reduxDispatch(
+		logStashLoadErrorEventAction( 'calypso_checkout_composite_error', errorObject.message, {
+			action_type: failureDescription,
+		} )
+	);
 }
