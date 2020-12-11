a2ba69ffcb4b32f857090c4ba5334e4272ef37eb
Payton Swick <payton@foolord.com>
2020-12-10 21:23:56 -0500

    Convert useSubmitTransaction to use useProcessPayment

--- client/my-sites/checkout/upsell-nudge/purchase-modal/util.js
+++ client/my-sites/checkout/upsell-nudge/purchase-modal/util.js
@@ -2,43 +2,14 @@
  * External dependencies
  */
 import { useCallback } from 'react';
-import { find, pick } from 'lodash';
+import { useProcessPayment } from '@automattic/composite-checkout';
 
 /**
  * Internal dependencies
  */
-import { RECEIVED_WPCOM_RESPONSE } from 'calypso/lib/store-transactions/step-types';
-import { preprocessCartForServer } from 'calypso/lib/cart-values';
-import { submit } from 'calypso/lib/store-transactions';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import notices from 'calypso/notices';
-
-function extractStoredCardMetaValue( card, key ) {
-	return find( card.meta, [ 'meta_key', key ] )?.meta_value;
-}
-
-function generateTransactionData( cart, storedCard ) {
-	const countryCode = extractStoredCardMetaValue( storedCard, 'country_code' );
-	const postalCode = extractStoredCardMetaValue( storedCard, 'card_zip' );
-
-	return {
-		cart: {
-			...pick( cart, [ 'blog_id', 'cart_key' ] ),
-			...preprocessCartForServer( cart ),
-			create_new_blog: false,
-		},
-		domainDetails: null,
-		payment: {
-			paymentMethod: 'WPCOM_Billing_MoneyPress_Stored',
-			storedCard,
-			name: storedCard.name,
-			country: countryCode,
-			country_code: countryCode,
-			postal_code: postalCode,
-			zip: postalCode,
-		},
-	};
-}
+import { translateResponseCartToWPCOMCart } from 'calypso/my-sites/checkout/composite-checkout/lib/translate-cart';
 
 export function useSubmitTransaction( {
 	cart,
@@ -48,30 +19,34 @@ export function useSubmitTransaction( {
 	onComplete,
 	successMessage,
 } ) {
+	const callPaymentProcessor = useProcessPayment();
+
 	return useCallback( () => {
-		const transactionData = generateTransactionData( cart, storedCard );
-		submit( transactionData, ( { name, data, error } ) => {
-			if ( error ) {
+		const wpcomCart = translateResponseCartToWPCOMCart( cart );
+		callPaymentProcessor( 'existing-card', {
+			items: wpcomCart.items,
+			name: storedCard.name,
+			storedDetailsId: storedCard.stored_details_id,
+			paymentMethodToken: storedCard.mp_ref,
+			paymentPartnerProcessorId: storedCard.payment_partner,
+		} )
+			.then( () => {
+				setStep( name );
+				notices.success( successMessage, {
+					persistent: true,
+				} );
+				recordTracksEvent( 'calypso_oneclick_upsell_payment_success', {} );
+				onComplete?.();
+			} )
+			.catch( ( error ) => {
 				recordTracksEvent( 'calypso_oneclick_upsell_payment_error', {
 					error_code: error.code || error.error,
 					reason: error.message,
 				} );
 				notices.error( error.message );
 				onClose();
-				return;
-			}
-
-			setStep( name );
-
-			if ( RECEIVED_WPCOM_RESPONSE === name && data && ! error ) {
-				notices.success( successMessage, {
-					persistent: true,
-				} );
-				recordTracksEvent( 'calypso_oneclick_upsell_payment_success', {} );
-				onComplete?.();
-			}
-		} );
-	}, [ cart, storedCard, setStep, onClose, onComplete, successMessage ] );
+			} );
+	}, [ callPaymentProcessor, cart, storedCard, setStep, onClose, onComplete, successMessage ] );
 }
 
 export function formatDate( cardExpiry ) {
