db67c7a1279f8d982053955aa2f373a03356cde0
Markus Dobmann <m.do@posteo.net>
2020-12-14 11:13:54 +0100

    dataLayer(viewers): cleanup returns

--- client/state/data-layer/wpcom/viewers/index.js
+++ client/state/data-layer/wpcom/viewers/index.js
@@ -12,52 +12,38 @@ import {
 	requestViewersSuccess,
 } from 'calypso/state/viewers/actions';
 
-const fetchViewers = ( action ) => {
-	const { siteId, query } = action;
+const fetchViewers = ( action ) =>
+	http(
+		{
+			method: 'GET',
+			path: `/sites/${ action.siteId }/viewers`,
+			apiVersion: '1.1',
+			query: action.query,
+		},
+		action
+	);
 
-	return [
-		http(
-			{
-				method: 'GET',
-				path: `/sites/${ siteId }/viewers`,
-				apiVersion: '1.1',
-				query,
-			},
-			action
-		),
-	];
-};
+const fetchViewersSuccess = ( { siteId, query }, data ) =>
+	requestViewersSuccess( siteId, query, data );
 
-const fetchViewersSuccess = ( { siteId, query }, data ) => {
-	return [ requestViewersSuccess( siteId, query, data ) ];
-};
+const fetchViewersFailure = ( { siteId, query }, error ) =>
+	requestViewersFailure( siteId, query, error );
 
-const fetchViewersFailure = ( { siteId, query }, error ) => [
-	requestViewersFailure( siteId, query, error ),
-];
+const requestRemoveViewer = ( action ) =>
+	http(
+		{
+			method: 'POST',
+			path: `/sites/${ action.siteId }/viewers/${ action.viewerId }/delete`,
+			apiVersion: '1.1',
+		},
+		action
+	);
 
-const requestRemoveViewer = ( action ) => {
-	const { siteId, viewerId } = action;
+const requestRemoveViewerSuccess = ( { siteId, viewerId }, data ) =>
+	removeViewerSuccess( siteId, viewerId, data );
 
-	return [
-		http(
-			{
-				method: 'POST',
-				path: `/sites/${ siteId }/viewers/${ viewerId }/delete`,
-				apiVersion: '1.1',
-			},
-			action
-		),
-	];
-};
-
-const requestRemoveViewerSuccess = ( { siteId, viewerId }, data ) => [
-	removeViewerSuccess( siteId, viewerId, data ),
-];
-
-const requestRemoveViewerFailure = ( { siteId, viewerId }, error ) => [
-	removeViewerFailure( siteId, viewerId, error ),
-];
+const requestRemoveViewerFailure = ( { siteId, viewerId }, error ) =>
+	removeViewerFailure( siteId, viewerId, error );
 
 registerHandlers( 'state/data-layer/wpcom/viewers/index.js', {
 	[ VIEWERS_REQUEST ]: [
