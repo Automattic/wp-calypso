af8906f95ca34031777af001075651fe20ed9309
Louis Laugesen <louis.laugesen@gmail.com>
2020-12-16 16:12:09 +1100

    Prevent instant error on domain step during launch

--- packages/launch/src/hooks/use-domain-search.ts
+++ packages/launch/src/hooks/use-domain-search.ts
@@ -23,7 +23,7 @@ export function useDomainSearch(): {
 
 	const { setDomainSearch } = useDispatch( LAUNCH_STORE );
 
-	let search = domainSearch.trim() || title;
+	let search = domainSearch.trim() || filterUnsuitableTitles( title );
 
 	if ( ! search || isDefaultSiteTitle( { currentSiteTitle: search } ) ) {
 		search = siteSubdomain?.domain?.split( '.' )[ 0 ] ?? '';
@@ -34,4 +34,21 @@ export function useDomainSearch(): {
 		isLoading: isLoadingSite,
 		setDomainSearch,
 	};
+}
+
+function filterUnsuitableTitles( title: string ): string {
+	return isGoodDefaultDomainQuery( title ) ? title : '';
+}
+
+export function isGoodDefaultDomainQuery( domainQuery: string ): boolean {
+	if ( typeof domainQuery.normalize === 'undefined' ) {
+		// If the browser doesn't support String.prototype.normalize then
+		// play it safe and assume this isn't a safe domain query.
+		return false;
+	}
+
+	return !! domainQuery
+		.normalize( 'NFD' ) // Encode diacritics in a consistent way so we can remove them
+		.replace( /[\u0300-\u036f]/g, '' )
+		.match( /[a-z0-9-.\s]/i );
 }
