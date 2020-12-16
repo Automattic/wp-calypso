cc87289a587f900f2f44aae72b477430580e15b6
Autumn Fjeld <autumn@automattic.com>
2020-12-16 16:52:31 +1100

    Fix thumbs naming

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-welcome-tour/src/icons/thumbs_up.js
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-welcome-tour/src/icons/thumbs_up.js
@@ -3,15 +3,15 @@
  */
 import { SVG, Path } from '@wordpress/primitives';
 
-const thumbsDown = (
-	<SVG width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+const thumbsUp = (
+	<SVG width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/SVG">
 		<Path
 			fillRule="evenodd"
 			clipRule="evenodd"
-			d="M13.131 19.12a.667.667 0 001.227-.417l-.436-4.989h3.88c.954 0 1.64-.916 1.37-1.831L17.42 5.919a.286.286 0 00-.274-.205H9.429v7.588l3.702 5.818zm-5.417-5.977V5.714h-2v7.429h2zm5.98 8a2.381 2.381 0 01-2.01-1.103l-3.297-5.183H4V4h13.145a2 2 0 011.919 1.436l1.753 5.963a3.143 3.143 0 01-3.015 4.03h-2.01l.274 3.125a2.381 2.381 0 01-2.372 2.589z"
+			d="M13.131 4.023a.667.667 0 011.227.416l-.436 4.99h3.88c.954 0 1.64.916 1.37 1.831l-1.753 5.963a.286.286 0 01-.274.206H9.429V9.84l3.702-5.818zM7.714 10v7.428h-2V10h2zm5.98-8c-.814 0-1.572.416-2.01 1.103L8.388 8.286H4v10.857h13.145a2 2 0 001.919-1.436l1.753-5.963a3.143 3.143 0 00-3.015-4.03h-2.01l.274-3.125A2.381 2.381 0 0013.694 2z"
 			fill="#000"
 		/>
 	</SVG>
 );
 
-export default thumbsDown;
+export default thumbsUp;
