fa87558103fb89a1cba8404e7cba960570574054
Philip Jackson <p-jackson@live.com>
2020-12-17 10:42:08 +1300

    Share the same __i18n_text_domain__ mock for all package tests (#48396)

--- test/packages/jest-preset.js
+++ test/packages/jest-preset.js
@@ -5,4 +5,7 @@ module.exports = {
 	...base,
 	cacheDirectory: path.join( __dirname, '../../.cache/jest' ),
 	resolver: path.join( __dirname, '../../test/module-resolver.js' ),
+	globals: {
+		__i18n_text_domain__: 'default',
+	},
 };
