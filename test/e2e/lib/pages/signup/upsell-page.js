929d995ec1ef5f0d97c0a7d7c4d202f608bbe584
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-12-17 08:59:27 +0200

    Update selectors used in UpsellPage

--- test/e2e/lib/pages/signup/upsell-page.js
+++ test/e2e/lib/pages/signup/upsell-page.js
@@ -11,14 +11,19 @@ import AsyncBaseContainer from '../../async-base-container';
 
 export default class UpsellPage extends AsyncBaseContainer {
 	constructor( driver ) {
-		super( driver, By.css( '.concierge-quickstart-session, .plan-upgrade-upsell' ) );
+		super(
+			driver,
+			By.css(
+				'.concierge-quickstart-session, .premium-plan-upgrade-upsell, .business-plan-upgrade-upsell'
+			)
+		);
 	}
 
 	async declineOffer() {
 		return await driverHelper.clickWhenClickable(
 			this.driver,
 			By.css(
-				'.concierge-quickstart-session__decline-offer-button, .plan-upgrade-upsell__decline-offer-button'
+				'.concierge-quickstart-session__decline-offer-button, .business-plan-upgrade-upsell__decline-offer-button, .premium-plan-upgrade-upsell__decline-offer-button'
 			)
 		);
 	}
