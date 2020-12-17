4f1bba0ab0b81ce42efd71df0f9d2da0d9e064bb
Jessie Ross <jessie.ross@automattic.com>
2020-12-18 05:42:50 +0800

    Mock implement explat client

---
+++ client/lib/explat/index.ts
@@ -0,0 +1,37 @@
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks'
+import { getAnonIdFromCookie } from 'calypso/state/experiments/reducer'
+import { createExPlatClient, createExPlatReactClient } from 'explat-client'
+
+const makeRequest = () => {
+    // TODO
+    return
+}
+
+let hasTracksEventFiredToEnsureAnonIdInCookie = false
+const getAnonId = () => {
+    if (!hasTracksEventFiredToEnsureAnonIdInCookie) {
+		recordTracksEvent( 'calypso_explat_first_experiment_fetch' );
+    }
+    return getAnonIdFromCookie()
+}
+
+const logError = (errorMessage) => {
+    // TODO Logstashing
+    return
+}
+
+const isDevelopmentMode = process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+
+const ExPlatClient = createExPlatClient(
+    makeRequest,
+    getAnonId,
+    logError,
+    isDevelopmentMode,
+)
+
+const ExPlatReactClient = createExPlatReactClient(ExPlatClient)
+
+export { 
+    ExPlatClient.getExperimentAssignment as getExperimentAssignment
+    ExPlatReactClient.useExperiment as useExperiment
+}
