59c2bcc2c7690ec425b316b7bf3bdd96c2f7af0e
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-12-18 00:05:17 +0200

    update jsdoc and return type of usePrevious

--- client/landing/gutenboarding/hooks/use-previous.ts
+++ client/landing/gutenboarding/hooks/use-previous.ts
@@ -8,15 +8,15 @@ import { useEffect, useRef } from '@wordpress/element';
  *
  * see https://reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
  *
- * @param  {any}  value state or prop value for which previous value is required
- * @returns {any}  previous value of requested state or prop value
+ * @param value state or prop value for which previous value is required
+ * @returns previous value of requested state or prop value
  */
-export function usePrevious< T >( value: T ): T {
+export function usePrevious< T >( value: T ): T | undefined {
 	const ref = useRef< T >();
 
 	useEffect( () => {
 		ref.current = value;
 	}, [ value ] );
 
-	return ref.current as T;
+	return ref.current;
 }
