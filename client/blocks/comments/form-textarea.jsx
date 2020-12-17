6b86940fe6681459a795bb9d299df9f630ce2df8
Sara Marcondes <saram@fastmail.com>
2020-12-17 09:38:42 -0800

    Clean up unused auto-completers

--- client/blocks/comments/form-textarea.jsx
+++ client/blocks/comments/form-textarea.jsx
@@ -5,18 +5,9 @@ import React from 'react';
 
 import IsolatedBlockEditor from 'isolated-editor';
 
-/**
- * Internal dependencies
- */
-import FormTextarea from 'calypso/components/forms/form-textarea';
-import withUserMentions from 'calypso/blocks/user-mentions/index';
-import withPasteToLink from 'calypso/lib/paste-to-link';
-
-/* eslint-disable jsx-a11y/no-autofocus */
 const PostCommentFormTextarea = ( props ) => (
 	<IsolatedBlockEditor
 		onSaveContent={ ( html ) => props.onChange( html ) }
-		onLoad={ ( parse ) => {} }
 		onError={ console.error }
 		settings={ {
 			iso: {
@@ -31,12 +22,12 @@ const PostCommentFormTextarea = ( props ) => (
 						'core/video',
 						'core/table',
 						'core/quote',
+						'core/separator',
 					],
 				},
 			},
 		} }
 	/>
 );
-/* eslint-enable jsx-a11y/no-autofocus */
 
-export default withUserMentions( withPasteToLink( PostCommentFormTextarea ) );
+export default PostCommentFormTextarea;
