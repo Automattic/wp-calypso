33308c002d06aaaddc062194a1da9afb3d1b7617
Addison-Stavlo <stavz01@gmail.com>
2020-12-17 13:40:00 -0500

    make formatting happy

--- apps/editing-toolkit/editing-toolkit-plugin/starter-page-templates/page-template-modal/components/sidebar-modal-opener.js
+++ apps/editing-toolkit/editing-toolkit-plugin/starter-page-templates/page-template-modal/components/sidebar-modal-opener.js
@@ -15,7 +15,7 @@ class SidebarModalOpener extends Component {
 	};
 
 	toggleTemplateModal = () => {
-		this.setState( state => ( { isOpenFromSidebar: ! state.isOpenFromSidebar } ) );
+		this.setState( ( state ) => ( { isOpenFromSidebar: ! state.isOpenFromSidebar } ) );
 	};
 
 	toggleWarningModal = () => {
