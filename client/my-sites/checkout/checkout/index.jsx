915774b517a5eb1e8d71179478e68d2abec85e63
Payton Swick <payton@foolord.com>
2020-12-18 16:08:31 -0500

    Remove unnecessary eslint comment

--- client/my-sites/checkout/checkout/index.jsx
+++ client/my-sites/checkout/checkout/index.jsx
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import React from 'react';
-// eslint-disable-next-line no-restricted-imports
 
 /**
  * Internal dependencies
