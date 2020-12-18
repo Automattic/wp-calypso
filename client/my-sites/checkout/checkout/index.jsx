cb8c8d4b51dc68d6c5f1e694ed1ad184e0a08ba8
Payton Swick <payton@foolord.com>
2020-12-18 13:38:10 -0500

    Remove unused props and state from Checkout

--- client/my-sites/checkout/checkout/index.jsx
+++ client/my-sites/checkout/checkout/index.jsx
@@ -16,20 +16,10 @@ import { clearSitePlans } from 'calypso/state/sites/plans/actions';
 import { clearPurchases } from 'calypso/state/purchases/actions';
 import { fetchReceiptCompleted } from 'calypso/state/receipts/actions';
 import QueryStoredCards from 'calypso/components/data/query-stored-cards';
-import getUpgradePlanSlugFromPath from 'calypso/state/selectors/get-upgrade-plan-slug-from-path';
-import isDomainOnlySite from 'calypso/state/selectors/is-domain-only-site';
-import isEligibleForSignupDestination from 'calypso/state/selectors/is-eligible-for-signup-destination';
 import { getStoredCards, isFetchingStoredCards } from 'calypso/state/stored-cards/selectors';
 import { recordViewCheckout } from 'calypso/lib/analytics/ad-tracking';
 import { requestSite } from 'calypso/state/sites/actions';
-import { isJetpackSite } from 'calypso/state/sites/selectors';
-import {
-	getSelectedSite,
-	getSelectedSiteId,
-	getSelectedSiteSlug,
-} from 'calypso/state/ui/selectors';
 import { isApplePayAvailable } from 'calypso/lib/web-payment';
-import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 import { loadTrackingTool } from 'calypso/state/analytics/actions';
 import { withLocalizedMoment } from 'calypso/components/localized-moment';
 
@@ -41,16 +31,7 @@ import './style.scss';
 export class Checkout extends React.Component {
 	static propTypes = {
 		cards: PropTypes.array.isRequired,
-		couponCode: PropTypes.string,
-		isJetpackNotAtomic: PropTypes.bool,
-		returnToBlockEditor: PropTypes.bool,
-		returnToHome: PropTypes.bool,
-		selectedFeature: PropTypes.string,
 		loadTrackingTool: PropTypes.func.isRequired,
-	};
-
-	state = {
-		cartSettled: false,
 	};
 
 	componentDidMount() {
@@ -95,20 +76,10 @@ export class Checkout extends React.Component {
 }
 
 export default connect(
-	( state, props ) => {
-		const selectedSiteId = getSelectedSiteId( state );
-
+	( state ) => {
 		return {
 			cards: getStoredCards( state ),
-			isDomainOnly: isDomainOnlySite( state, selectedSiteId ),
-			selectedSite: getSelectedSite( state ),
-			selectedSiteId,
-			selectedSiteSlug: getSelectedSiteSlug( state ),
-			isEligibleForSignupDestination: isEligibleForSignupDestination( props.cart ),
 			isFetchingStoredCards: isFetchingStoredCards( state ),
-			planSlug: getUpgradePlanSlugFromPath( state, selectedSiteId, props.product ),
-			isJetpackNotAtomic:
-				isJetpackSite( state, selectedSiteId ) && ! isAtomicSite( state, selectedSiteId ),
 		};
 	},
 	{
