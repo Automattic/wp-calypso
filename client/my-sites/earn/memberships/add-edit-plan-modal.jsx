a042160c27f9a4ea57736be9e59d2346fff87877
Aurorum <torres126@clubpenguinmountains.com>
2020-12-19 10:04:54 +0000

    Edit Payment Plan: Update Support Link

--- client/my-sites/earn/memberships/add-edit-plan-modal.jsx
+++ client/my-sites/earn/memberships/add-edit-plan-modal.jsx
@@ -306,8 +306,8 @@ const RecurringPaymentsPlanAddEditModal = ( {
 							'Allow members of this payment plan to opt into receiving new posts via email.'
 						) }{ ' ' }
 						<InlineSupportLink
-							supportPostId={ 154624 }
-							supportLink="https://wordpress.com/support/wordpress-editor/blocks/recurring-payments-button/" // TODO: Link to specific section once article is update
+							supportPostId={ 168381 }
+							supportLink="https://wordpress.com/support/paid-newsletters/"
 							showIcon={ false }
 						>
 							{ translate( 'Learn more.' ) }
