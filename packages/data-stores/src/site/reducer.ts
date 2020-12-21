5c495b5d99bf97d2a55f0281eb8239825d1c3f19
Stefan Nieuwenhuis <StefanNieuwenhuis@users.noreply.github.com>
2020-12-21 12:11:47 +0100

    Focused-Launch: Add error handling for LaunchSite (#48348)

--- packages/data-stores/src/site/reducer.ts
+++ packages/data-stores/src/site/reducer.ts
@@ -7,7 +7,14 @@ import { combineReducers } from '@wordpress/data';
 /**
  * Internal dependencies
  */
-import type { NewSiteBlogDetails, NewSiteErrorResponse, SiteDetails, Domain } from './types';
+import {
+	NewSiteBlogDetails,
+	NewSiteErrorResponse,
+	SiteDetails,
+	Domain,
+	SiteLaunchState,
+	SiteLaunchStatus,
+} from './types';
 import type { Action } from './actions';
 
 export const newSiteData: Reducer< NewSiteBlogDetails | undefined, Action > = ( state, action ) => {
@@ -101,15 +108,30 @@ export const sitesDomains: Reducer< { [ key: number ]: Domain[] }, Action > = (
 	return state;
 };
 
-export const launchStatus: Reducer<
-	{ [ key: number ]: { isSiteLaunched: boolean; isSiteLaunching: boolean } },
-	Action
-> = ( state = {}, action ) => {
+export const launchStatus: Reducer< { [ key: number ]: SiteLaunchState }, Action > = (
+	state = {},
+	action
+) => {
 	if ( action.type === 'LAUNCH_SITE_START' ) {
-		return { ...state, [ action.siteId ]: { isSiteLaunched: false, isSiteLaunching: true } };
+		return {
+			...state,
+			[ action.siteId ]: { status: SiteLaunchStatus.IN_PROGRESS, errorCode: undefined },
+		};
 	}
-	if ( action.type === 'LAUNCH_SITE_COMPLETE' ) {
-		return { ...state, [ action.siteId ]: { isSiteLaunched: true, isSiteLaunching: false } };
+	if ( action.type === 'LAUNCH_SITE_SUCCESS' ) {
+		return {
+			...state,
+			[ action.siteId ]: { status: SiteLaunchStatus.SUCCESS, errorCode: undefined },
+		};
+	}
+	if ( action.type === 'LAUNCH_SITE_FAILURE' ) {
+		return {
+			...state,
+			[ action.siteId ]: {
+				status: SiteLaunchStatus.FAILURE,
+				errorCode: action.error,
+			},
+		};
 	}
 	return state;
 };
