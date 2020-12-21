dba0e789a59f5106dc5d62032694ff7fbffe64c6
Moon <moon.kyong@automattic.com>
2020-12-21 13:48:41 -0800

    Remove unncessary code (#48514)
    * Remove CSS override
    
    * Remove div wrap
    
    * Removed icon

--- client/extensions/woocommerce/components/store-deprecated-notice/index.js
+++ client/extensions/woocommerce/components/store-deprecated-notice/index.js
@@ -11,23 +11,19 @@ import React, { Component } from 'react';
 
 import Notice from 'calypso/components/notice';
 import NoticeAction from 'calypso/components/notice/notice-action';
-import './style.scss';
 
 class StoreDeprecatedNotice extends Component {
 	render() {
 		return (
-			<div className="store-deprecated-notice">
-				<Notice
-					status="is-success"
-					icon="notice"
-					text={ translate( 'Store is moving to WooCommerce' ) }
-					showDismiss={ false }
-				>
-					<NoticeAction href="https://wordpress.com/support/store/">
-						{ translate( 'More' ) }
-					</NoticeAction>
-				</Notice>
-			</div>
+			<Notice
+				status="is-warning"
+				text={ translate( 'Store is moving to WooCommerce' ) }
+				showDismiss={ false }
+			>
+				<NoticeAction href="https://wordpress.com/support/store/">
+					{ translate( 'More' ) }
+				</NoticeAction>
+			</Notice>
 		);
 	}
 }
