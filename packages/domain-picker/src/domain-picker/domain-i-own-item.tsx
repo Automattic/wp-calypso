9fdc7af20616f43da63b2925ccbbef1c6844a60b
Tom Cafferkey <tjcafferkey@gmail.com>
2020-12-22 16:34:00 +0000

    Remove createInterpolateElement where its not needed for the domain I own item

--- packages/domain-picker/src/domain-picker/domain-i-own-item.tsx
+++ packages/domain-picker/src/domain-picker/domain-i-own-item.tsx
@@ -20,43 +20,39 @@ const DomainIOwn: React.FunctionComponent< Props > = ( { siteSlug, source } ) =>
 	const localizeUrl = useLocalizeUrl();
 
 	return (
-		<label className="domain-picker__suggestion-item contains-link">
-			<div className="domain-picker__suggestion-item-name">
-				<span className="domain-picker__domain-wrapper with-margin">
-					{ createInterpolateElement(
-						__( '<strong>Already own a domain?</strong>', __i18n_text_domain__ ),
-						{
-							strong: <strong />,
-						}
-					) }
-				</span>
-				<div>
-					<span className="domain-picker__item-tip">
-						{ __( "You can use it as your site's address", __i18n_text_domain__ ) }
+		<>
+			{ /* eslint-disable-next-line */ }
+			<label className="domain-picker__suggestion-item contains-link">
+				<div className="domain-picker__suggestion-item-name">
+					<span className="domain-picker__domain-wrapper with-margin">
+						{ createInterpolateElement(
+							__( '<strong>Already own a domain?</strong>', __i18n_text_domain__ ),
+							{
+								strong: <strong />,
+							}
+						) }
 					</span>
+					<div>
+						<span className="domain-picker__item-tip">
+							{ __( "You can use it as your site's address", __i18n_text_domain__ ) }
+						</span>
+					</div>
 				</div>
-			</div>
-			<div className="domain-picker__action-link domain-picker__action-link domain-picker__action-link--with-underline">
-				{ createInterpolateElement(
-					__(
-						'<strong><use_domain_link>Use a domain I own</use_domain_link></strong>',
-						__i18n_text_domain__
-					),
-					{
-						strong: <strong />,
-						use_domain_link: (
-							<a
-								rel="noreferrer"
-								href={ localizeUrl(
-									`https://wordpress.com/start/new-launch/domains-launch/use-your-domain?siteSlug=${ siteSlug }&source=${ source }`
-								) }
-							/>
-						),
-					}
-				) }
-				<Icon icon={ chevronRight } />
-			</div>
-		</label>
+				<div className="domain-picker__action-link domain-picker__action-link domain-picker__action-link--with-underline">
+					<strong>
+						<a
+							rel="noreferrer"
+							href={ localizeUrl(
+								`https://wordpress.com/start/new-launch/domains-launch/use-your-domain?siteSlug=${ siteSlug }&source=${ source }`
+							) }
+						>
+							{ __( 'Use a domain I own', __i18n_text_domain__ ) }
+						</a>
+					</strong>
+					<Icon icon={ chevronRight } />
+				</div>
+			</label>
+		</>
 	);
 };
 
