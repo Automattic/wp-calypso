379742321e07575e5ac3bae186a456c4bda382e6
Payton Swick <payton@foolord.com>
2020-12-22 19:22:50 -0500

    Add PayPal payment method object to useAssignablePaymentMethods

--- client/me/purchases/manage-purchase/change-payment-method/index.jsx
+++ client/me/purchases/manage-purchase/change-payment-method/index.jsx
@@ -56,6 +56,7 @@ import { concatTitle } from 'calypso/lib/react-helpers';
 import {
 	useCreateCreditCard,
 	useCreateExistingCards,
+	useCreatePayPal,
 } from 'calypso/my-sites/checkout/composite-checkout/use-create-payment-methods';
 import Gridicon from 'calypso/components/gridicon';
 import { localizeUrl } from 'calypso/lib/i18n-utils';
@@ -446,6 +447,8 @@ function useAssignablePaymentMethods() {
 		activePayButtonText: translate( 'Save card' ),
 	} );
 
+	const payPalMethod = useCreatePayPal();
+
 	// getStoredCards always returns a new array, but we need a memoized version
 	// to pass to useCreateExistingCards, which has storedCards as a data dependency.
 	const rawStoredCards = useSelector( getStoredCards );
@@ -457,8 +460,8 @@ function useAssignablePaymentMethods() {
 	} );
 
 	const paymentMethods = useMemo(
-		() => [ ...existingCardMethods, stripeMethod ].filter( Boolean ),
-		[ stripeMethod, existingCardMethods ]
+		() => [ ...existingCardMethods, stripeMethod, payPalMethod ].filter( Boolean ),
+		[ stripeMethod, existingCardMethods, payPalMethod ]
 	);
 
 	return paymentMethods;
