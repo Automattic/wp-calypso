9f517b9753cb26202d89c0e0aac5ea12c4be9636
Elton Goci <eltongoci@gmail.com>
2020-12-23 14:54:45 +0100

    Use function to create Titan Mail cart item

--- client/my-sites/email/titan-mail-quantity-selection/index.jsx
+++ client/my-sites/email/titan-mail-quantity-selection/index.jsx
@@ -20,7 +20,7 @@ import { getSelectedSite } from 'calypso/state/ui/selectors';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import { recordTracksEvent as recordTracksEventAction } from 'calypso/state/analytics/actions';
 import { addItems } from 'calypso/lib/cart/actions';
-import { TITAN_MAIL_MONTHLY_SLUG } from 'calypso/lib/titan/constants';
+import { titanMailMonthly } from 'calypso/lib/cart-values/cart-items';
 
 /**
  * Style dependencies
@@ -65,11 +65,7 @@ class TitanMailQuantitySelection extends React.Component {
 		);
 
 		addItems( [
-			{
-				product_slug: TITAN_MAIL_MONTHLY_SLUG,
-				quantity: this.state.quantity,
-				meta: selectedDomainName,
-			},
+			titanMailMonthly( { domain: selectedDomainName, quantity: this.state.quantity } ),
 		] );
 		page( '/checkout/' + selectedSite.slug );
 	};
