8909f2e58d57e89f30cf3e4d08eb51802b04a2a8
Sara Marcondes <saram@fastmail.com>
2020-12-24 08:01:28 -0800

    Search: i18nify the component (#46825)
    * Search: i18nify the component
    
    * Fix storybook import
    
    * Don't use TS for storybook examples
    
    * Fix various type issues causing improper export
    
    * Fix storybook

--- packages/search/src/search.stories.jsx
+++ packages/search/src/search.stories.jsx
@@ -3,13 +3,14 @@
  */
 import React from 'react';
 
-import Search from '.';
+import { Search } from './search';
 
 export default { title: 'Search', component: Search };
 
-const BoxedSearch = ( props: any ) => (
+const BoxedSearch = ( props ) => (
 	<div style={ { position: 'relative', width: '270px', height: '50px' } }>
 		<Search
+			__={ ( str ) => str }
 			placeholder="Search..."
 			fitsContainer
 			onSearch={ ( search ) => console.log( 'Searched: ', search ) } // eslint-disable-line no-console
@@ -43,7 +44,7 @@ export const Compact = () => <BoxedSearch compact />;
 export const CompactPinned = () => <BoxedSearch pinned compact fitsContainer />;
 
 export const WithOverlayStyling = () => {
-	const overlayStyling = ( input: string ) => {
+	const overlayStyling = ( input ) => {
 		const tokens = input.split( /(\s+)/ );
 
 		return tokens
