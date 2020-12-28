1a8775faa51c50f3345e7aef6735f2c40f18e4e0
lamosty <lamos.rasto@gmail.com>
2020-12-28 15:09:02 +0100

    P2+: add to isPlan check

---
+++ client/lib/products-values/is-p2-plus.js
@@ -0,0 +1,13 @@
+/**
+ * Internal dependencies
+ */
+import { isP2PlusPlan } from 'calypso/lib/plans';
+import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
+import { formatProduct } from 'calypso/lib/products-values/format-product';
+
+export function isP2Plus( product ) {
+	product = formatProduct( product );
+	assertValidProduct( product );
+
+	return isP2PlusPlan( product.product_slug );
+}
