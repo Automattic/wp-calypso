a08af687c3f2ed2a02b2ff40201402024f24ce0a
Elton <eltongo@users.noreply.github.com>
2020-12-29 15:48:42 +0100

    Email: Create Titan Mail checkout flow (#48600)

--- client/my-sites/email/controller.js
+++ client/my-sites/email/controller.js
@@ -11,6 +11,8 @@ import EmailForwarding from 'calypso/my-sites/email/email-forwarding';
 import EmailManagement from 'calypso/my-sites/email/email-management';
 import { emailManagementAddGSuiteUsers } from 'calypso/my-sites/email/paths';
 import GSuiteAddUsers from 'calypso/my-sites/email/gsuite-add-users';
+import TitanMailQuantitySelection from 'calypso/my-sites/email/titan-mail-quantity-selection';
+import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 
 export default {
 	emailManagementAddGSuiteUsers( pageContext, next ) {
@@ -30,6 +32,15 @@ export default {
 				planType={ pageContext.params.planType }
 				selectedDomainName={ pageContext.params.domain }
 			/>
+		);
+		next();
+	},
+
+	emailManagementNewTitanAccount( pageContext, next ) {
+		pageContext.primary = (
+			<CalypsoShoppingCartProvider>
+				<TitanMailQuantitySelection selectedDomainName={ pageContext.params.domain } />
+			</CalypsoShoppingCartProvider>
 		);
 		next();
 	},
