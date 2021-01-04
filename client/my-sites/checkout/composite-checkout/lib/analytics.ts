a6237f77ebc3d07d263c36731b6c27f3311e786f
Payton Swick <payton@automattic.com>
2021-01-04 15:14:18 -0500

    Checkout: Move analytics for redirect payment methods into payment processors (#48435)
    * Remove onEvent in StripeCreditCardFields
    
    This payment method is unused currently.
    
    * Include reduxDispatch in CardProcessorOptions in CompositeCheckout
    
    * Add recordCompositeCheckoutErrorDuringAnalytics
    
    * Replace fallback in useCreatePaymentCompleteCallback with import
    
    * Replace fallback in createAnalyticsEventHandler with import
    
    * Move redirect start analytics to genericRedirectProcessor
    
    * Call recordRedirectTransactionBeginAnalytics in paypal/wechat also
    
    * Remove onEvent from redirect payment methods
    
    * Merge recordTransactionBeginAnalytics functions
    
    * Only record redirect begin event for redirect methods
    
    * Convert analytics file to TypeScript
    
    * Fixup class names and event names
    
    * Fixup class names in ideal logo SVG
    
    * Fixup class name in Alipay logo svg
    
    Co-authored-by: nbloomf <nathan.bloomfield@automattic.com>

---
+++ client/my-sites/checkout/composite-checkout/lib/analytics.ts
@@ -0,0 +1,108 @@
+/**
+ * External dependencies
+ */
+import { useDispatch } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import { logToLogstash } from 'calypso/state/logstash/actions';
+import config from 'calypso/config';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import {
+	translateCheckoutPaymentMethodToWpcomPaymentMethod,
+	isRedirectPaymentMethod,
+} from '../lib/translate-payment-method-names';
+import type { CheckoutPaymentMethodSlug } from '../types/checkout-payment-method-slug';
+
+export function logStashLoadErrorEventAction(
+	errorType: string,
+	errorMessage: string,
+	additionalData: Record< string, string > = {}
+): ReturnType< typeof logToLogstash > {
+	return logStashEventAction( 'composite checkout load error', {
+		...additionalData,
+		type: errorType,
+		message: errorMessage,
+	} );
+}
+
+export function logStashEventAction(
+	message: string,
+	dataForLog: Record< string, string > = {}
+): ReturnType< typeof logToLogstash > {
+	return logToLogstash( {
+		feature: 'calypso_client',
+		message,
+		severity: config( 'env_id' ) === 'production' ? 'error' : 'debug',
+		extra: {
+			env: config( 'env_id' ),
+			...dataForLog,
+		},
+	} );
+}
+
+export function recordCompositeCheckoutErrorDuringAnalytics( {
+	errorObject,
+	failureDescription,
+	reduxDispatch,
+}: {
+	errorObject: Error;
+	failureDescription: string;
+	reduxDispatch: ReturnType< typeof useDispatch >;
+} ): void {
+	// This is a fallback to catch any errors caused by the analytics code
+	// Anything in this block should remain very simple and extremely
+	// tolerant of any kind of data. It should make no assumptions about
+	// the data it uses. There's no fallback for the fallback!
+	reduxDispatch(
+		recordTracksEvent( 'calypso_checkout_composite_error', {
+			error_message: errorObject.message,
+			action_type: failureDescription,
+		} )
+	);
+	reduxDispatch(
+		logStashLoadErrorEventAction( 'calypso_checkout_composite_error', errorObject.message, {
+			action_type: failureDescription,
+		} )
+	);
+}
+
+export function recordTransactionBeginAnalytics( {
+	reduxDispatch,
+	paymentMethodId,
+}: {
+	reduxDispatch: ReturnType< typeof useDispatch >;
+	paymentMethodId: CheckoutPaymentMethodSlug;
+} ): void {
+	try {
+		if ( isRedirectPaymentMethod( paymentMethodId ) ) {
+			reduxDispatch( recordTracksEvent( 'calypso_checkout_form_redirect', {} ) );
+		}
+		reduxDispatch(
+			recordTracksEvent( 'calypso_checkout_form_submit', {
+				credits: null,
+				payment_method: translateCheckoutPaymentMethodToWpcomPaymentMethod( paymentMethodId ) || '',
+			} )
+		);
+		reduxDispatch(
+			recordTracksEvent( 'calypso_checkout_composite_form_submit', {
+				credits: null,
+				payment_method: translateCheckoutPaymentMethodToWpcomPaymentMethod( paymentMethodId ) || '',
+			} )
+		);
+		const paymentMethodIdForTracks = paymentMethodId.replace( /-/, '_' ).toLowerCase();
+		reduxDispatch(
+			recordTracksEvent(
+				`calypso_checkout_composite_${ paymentMethodIdForTracks }_submit_clicked`,
+				{}
+			)
+		);
+	} catch ( errorObject ) {
+		recordCompositeCheckoutErrorDuringAnalytics( {
+			reduxDispatch,
+			errorObject,
+			failureDescription: `transaction-begin: ${ paymentMethodId }`,
+		} );
+	}
+}
