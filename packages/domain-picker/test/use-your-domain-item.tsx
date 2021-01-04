77924e6c519722fe9cf42016e467605ccbc005da
Tom Cafferkey <tjcafferkey@gmail.com>
2021-01-04 09:50:03 +0000

    Fix component tests for <UseYourDomainItem />

--- packages/domain-picker/test/use-your-domain-item.tsx
+++ packages/domain-picker/test/use-your-domain-item.tsx
@@ -6,7 +6,7 @@
  * External dependencies
  */
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 /**
@@ -15,29 +15,21 @@ import '@testing-library/jest-dom/extend-expect';
 import UseYourDomainItem from '../src/domain-picker/use-your-domain-item';
 
 describe( '<UseYourDomainItem />', () => {
-	test( 'Component renders correct text', () => {
-		render(
-			<UseYourDomainItem
-				siteSlug="test.wordpress.com"
-				source="https://wordpress.com/page/test.worpdress.com/home"
-			/>
-		);
+	test( 'Component renders with correct messaging', () => {
+		const onClick = jest.fn();
+
+		render( <UseYourDomainItem onClick={ onClick } /> );
 
 		expect( screen.getByText( 'Already own a domain?' ) ).toBeTruthy();
 		expect( screen.getByText( "You can use it as your site's address" ) ).toBeTruthy();
 	} );
 
-	test( 'Component renders correctly', () => {
-		render(
-			<UseYourDomainItem
-				siteSlug="test.wordpress.com"
-				source="https://wordpress.com/page/test.worpdress.com/home"
-			/>
-		);
+	test( 'Component fires onClick callback onClick', () => {
+		const onClick = jest.fn();
 
-		expect( screen.getByText( 'Use a domain I own' ).closest( 'a' ) ).toHaveAttribute(
-			'href',
-			'https://wordpress.com/start/new-launch/domains-launch/use-your-domain/?siteSlug=test.wordpress.com&source=https://wordpress.com/page/test.worpdress.com/home'
-		);
+		render( <UseYourDomainItem onClick={ onClick } /> );
+
+		fireEvent.click( screen.getByText( 'Use a domain I own' ) );
+		expect( onClick ).toHaveBeenCalled();
 	} );
 } );
