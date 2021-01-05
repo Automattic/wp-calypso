34809ae2660e52a646e31ddcd745c3982d7c26f8
Delan Azabani <delan.azabani@automattic.com>
2021-01-05 17:30:03 +1100

    Step-by-step credentials API (with wpcom-proxy-request fix) (#48492)
    * Use step-by-step credentials API internally (no functional change)
    
    * Rename expectStreamMode to processResponseInStreamMode
    
    * Make onStreamRecord imply pRISM in action builder
    
    * Eliminate processResponseInStreamMode entirely (h/t @jsnajdr)
    
    * Bump to wpcom-xhr-request@1.2.0
    
    * Pull in client/state/data-layer/wpcom-http changes from #47614
    
    * Update data-layer snapshot for installJetpackPlugin
    
    * Strip onSR out of proxy message + fail if stream processing required
    
    * Add a changelog entry to wpcom-proxy-request package
    
    Co-authored-by: Jarda Snajdr <jsnajdr@gmail.com>

--- client/state/data-layer/wpcom-http/index.js
+++ client/state/data-layer/wpcom-http/index.js
@@ -3,6 +3,7 @@
  */
 
 import { compact, get } from 'lodash';
+import debugModule from 'debug';
 
 /**
  * Internal dependencies
@@ -14,6 +15,8 @@ import {
 	processInbound as inboundProcessor,
 	processOutbound as outboundProcessor,
 } from './pipeline';
+
+const debug = debugModule( 'calypso:data-layer:wpcom-http' );
 
 /**
  * Returns the appropriate fetcher in wpcom given the request method
@@ -38,6 +41,7 @@ export const failureMeta = ( error, headers ) => ( { meta: { dataLayer: { error,
 export const progressMeta = ( { total, loaded } ) => ( {
 	meta: { dataLayer: { progress: { total, loaded } } },
 } );
+export const streamRecordMeta = ( streamRecord ) => ( { meta: { dataLayer: { streamRecord } } } );
 
 export const queueRequest = ( processOutbound, processInbound ) => ( { dispatch }, rawAction ) => {
 	const action = processOutbound( rawAction, dispatch );
@@ -46,17 +50,34 @@ export const queueRequest = ( processOutbound, processInbound ) => ( { dispatch 
 		return;
 	}
 
-	const { body, formData, method: rawMethod, onProgress, options, path, query = {} } = action;
+	const {
+		body,
+		formData,
+		onStreamRecord: rawOnStreamRecord,
+		method: rawMethod,
+		onProgress,
+		options,
+		path,
+		query = {},
+	} = action;
 	const { responseType } = options || {};
+
+	const onStreamRecord =
+		rawOnStreamRecord &&
+		( ( record ) => {
+			return dispatch( extendAction( rawOnStreamRecord, streamRecordMeta( record ) ) );
+		} );
 
 	const method = rawMethod.toUpperCase();
 
 	const request = fetcherMap( method )(
 		...compact( [
-			{ path, formData, responseType },
+			{ path, formData, onStreamRecord, responseType },
 			{ ...query }, // wpcom mutates the query so hand it a copy
 			method === 'POST' && body,
 			( error, data, headers ) => {
+				debug( 'callback fn by Req method: error=%o data=%o headers=%o', error, data, headers );
+
 				const {
 					failures,
 					nextData,
