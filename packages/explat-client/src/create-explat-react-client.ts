e8526f36ea424dd240561a8903f0af5ce8c7f1ce
Jessie Ross <jessie.ross@automattic.com>
2021-01-05 11:49:22 +0800

    Clean up hook client

--- packages/explat-client/src/create-explat-react-client.ts
+++ packages/explat-client/src/create-explat-react-client.ts
@@ -13,27 +13,29 @@ interface ExPlatReactClient {
     useExperiment: (experimentName: string) => [boolean, ExperimentAssignment | null]
 }
 
-export default function createExPlatReactClient(ExPlatClient: ExPlatClient) {
-    useExperiment: (experimentName: string) => {
-        const [isLoading, setIsLoading] = useState<boolean>(true);
-        const [
-            experimentAssignment,
-            setExperimentAssignment,
-        ] = useState<ExperimentAssignment | null>(null);
+export default function createExPlatReactClient(ExPlatClient: ExPlatClient): ExPlatReactClient {
+    return {
+        useExperiment: (experimentName: string) => {
+            const [isLoading, setIsLoading] = useState<boolean>(true);
+            const [
+                experimentAssignment,
+                setExperimentAssignment,
+            ] = useState<ExperimentAssignment | null>(null);
 
-        useEffect(() => {
-            let isSubscribed = true;
-            ExPlatClient.loadExperimentAssignmentDangerousInSSR(experimentName).then((experimentAssignment) => {
-                if (isSubscribed) {
-                    setExperimentAssignment(experimentAssignment);
-                    setIsLoading(false);
-                }
+            useEffect(() => {
+                let isSubscribed = true;
+                ExPlatClient.loadExperimentAssignmentDangerousInSSR(experimentName).then((experimentAssignment) => {
+                    if (isSubscribed) {
+                        setExperimentAssignment(experimentAssignment);
+                        setIsLoading(false);
+                    }
+                });
+                return () => {
+                    isSubscribed = false;
+                };
             });
-            return () => {
-                isSubscribed = false;
-            };
-        });
 
-        return [isLoading, experimentAssignment];
+            return [isLoading, experimentAssignment];
+        }
     }
 }
