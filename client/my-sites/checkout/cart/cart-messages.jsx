bad476dcdfed0508fa87c2e2369b1d3da3bb0146
Payton Swick <payton@foolord.com>
2021-01-05 19:19:45 -0500

    Convert CartMessages to a functional component

--- client/my-sites/checkout/cart/cart-messages.jsx
+++ client/my-sites/checkout/cart/cart-messages.jsx
@@ -1,10 +1,9 @@
 /**
  * External dependencies
  */
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { localize } from 'i18n-calypso';
+import React, { useRef, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -13,120 +12,108 @@ import notices from 'calypso/notices';
 import { getNewMessages } from 'calypso/lib/cart-values';
 import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
-class CartMessages extends PureComponent {
-	static propTypes = {
-		cart: PropTypes.object.isRequired,
-		isLoadingCart: PropTypes.bool,
+export default function CartMessages( { cart, isLoadingCart } ) {
+	const previousCart = useRef( null );
+	const selectedSiteSlug = useSelector( getSelectedSiteSlug );
+	const translate = useTranslate();
 
-		// from HOCs
-		translate: PropTypes.func.isRequired,
-		selectedSiteSlug: PropTypes.string,
-	};
-
-	previousCart = null;
-
-	componentDidMount() {
-		this.displayCartMessages();
-	}
-
-	componentDidUpdate() {
-		this.displayCartMessages();
-	}
-
-	getChargebackErrorMessage() {
-		return this.props.translate(
-			'{{strong}}Warning:{{/strong}} One or more transactions linked to this site were refunded due to a contested charge. ' +
-				'This may have happened because of a chargeback by the credit card holder or a PayPal investigation. Each contested ' +
-				'charge carries a fine. To resolve the issue and re-enable posting, please {{a}}pay for the chargeback fine{{/a}}.',
-			{
-				components: {
-					strong: <strong />,
-					a: <a href={ '/checkout/' + this.props.selectedSiteSlug ?? '' } />,
-				},
-			}
-		);
-	}
-
-	getBlockedPurchaseErrorMessage() {
-		return this.props.translate(
-			'Purchases are currently disabled. Please {{a}}contact us{{/a}} to re-enable purchases.',
-			{
-				components: {
-					a: (
-						<a
-							href={
-								'https://wordpress.com/error-report/' + this.props.selectedSiteSlug
-									? '?url=payment@' + this.props.selectedSiteSlug
-									: ''
-							}
-							target="_blank"
-							rel="noopener noreferrer"
-						/>
-					),
-				},
-			}
-		);
-	}
-
-	getPrettyErrorMessages( messages ) {
-		if ( ! messages ) {
-			return [];
-		}
-
-		return messages.map( ( error ) => {
-			switch ( error.code ) {
-				case 'chargeback':
-					return Object.assign( error, { message: this.getChargebackErrorMessage() } );
-
-				case 'blocked':
-					return Object.assign( error, { message: this.getBlockedPurchaseErrorMessage() } );
-
-				default:
-					return error;
-			}
+	useEffect( () => {
+		displayCartMessages( {
+			cart,
+			isLoadingCart,
+			translate,
+			selectedSiteSlug,
+			previousCart: previousCart.current,
 		} );
-	}
+		previousCart.current = cart;
+	}, [ cart, isLoadingCart, translate, selectedSiteSlug ] );
 
-	displayCartMessages() {
-		const newCart = this.props.cart;
-		if ( this.props.isLoadingCart ) {
-			return;
-		}
-		const messages = getNewMessages( this.previousCart, newCart );
-
-		messages.errors = this.getPrettyErrorMessages( messages.errors );
-
-		this.previousCart = newCart;
-
-		if ( messages.errors?.length > 0 ) {
-			notices.error(
-				messages.errors.map( ( error ) => (
-					<p key={ `${ error.code }-${ error.message }` }>{ error.message }</p>
-				) ),
-				{ persistent: true }
-			);
-			return;
-		}
-
-		if ( messages.success?.length > 0 ) {
-			notices.success(
-				messages.success.map( ( success ) => (
-					<p key={ `${ success.code }-${ success.message }` }>{ success.message }</p>
-				) )
-			);
-			return;
-		}
-	}
-
-	render() {
-		return null;
-	}
+	return null;
 }
 
-function mapStateToProps( state ) {
-	return {
-		selectedSiteSlug: getSelectedSiteSlug( state ),
-	};
+function getChargebackErrorMessage( { translate, selectedSiteSlug } ) {
+	return translate(
+		'{{strong}}Warning:{{/strong}} One or more transactions linked to this site were refunded due to a contested charge. ' +
+			'This may have happened because of a chargeback by the credit card holder or a PayPal investigation. Each contested ' +
+			'charge carries a fine. To resolve the issue and re-enable posting, please {{a}}pay for the chargeback fine{{/a}}.',
+		{
+			components: {
+				strong: <strong />,
+				a: <a href={ '/checkout/' + selectedSiteSlug ?? '' } />,
+			},
+		}
+	);
 }
 
-export default connect( mapStateToProps )( localize( CartMessages ) );
+function getBlockedPurchaseErrorMessage( { translate, selectedSiteSlug } ) {
+	return translate(
+		'Purchases are currently disabled. Please {{a}}contact us{{/a}} to re-enable purchases.',
+		{
+			components: {
+				a: (
+					<a
+						href={
+							'https://wordpress.com/error-report/' + selectedSiteSlug
+								? '?url=payment@' + selectedSiteSlug
+								: ''
+						}
+						target="_blank"
+						rel="noopener noreferrer"
+					/>
+				),
+			},
+		}
+	);
+}
+
+function getPrettyErrorMessages( messages, { translate, selectedSiteSlug } ) {
+	if ( ! messages ) {
+		return [];
+	}
+
+	return messages.map( ( error ) => {
+		switch ( error.code ) {
+			case 'chargeback':
+				return Object.assign( error, {
+					message: getChargebackErrorMessage( { translate, selectedSiteSlug } ),
+				} );
+
+			case 'blocked':
+				return Object.assign( error, {
+					message: getBlockedPurchaseErrorMessage( { translate, selectedSiteSlug } ),
+				} );
+
+			default:
+				return error;
+		}
+	} );
+}
+
+function displayCartMessages( { cart, isLoadingCart, translate, selectedSiteSlug, previousCart } ) {
+	const newCart = cart;
+	if ( isLoadingCart ) {
+		return;
+	}
+	const messages = getNewMessages( previousCart, newCart );
+
+	messages.errors = getPrettyErrorMessages( messages.errors, { translate, selectedSiteSlug } );
+
+	if ( messages.errors?.length > 0 ) {
+		notices.error(
+			messages.errors.map( ( error ) => (
+				<p key={ `${ error.code }-${ error.message }` }>{ error.message }</p>
+			) ),
+			{ persistent: true }
+		);
+		return;
+	}
+
+	if ( messages.success?.length > 0 ) {
+		notices.success(
+			messages.success.map( ( success ) => (
+				<p key={ `${ success.code }-${ success.message }` }>{ success.message }</p>
+			) )
+		);
+		return;
+	}
+}
