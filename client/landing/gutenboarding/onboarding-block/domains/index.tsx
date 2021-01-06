dff6c5dbfa59b1814a4c1d9e1bd0a28a77321bc3
Razvan Papadopol <razvan.papadopol@automattic.com>
2021-01-06 16:17:55 +0200

    Enable the feature only for existing users

--- client/landing/gutenboarding/onboarding-block/domains/index.tsx
+++ client/landing/gutenboarding/onboarding-block/domains/index.tsx
@@ -25,6 +25,7 @@ import useStepNavigation from '../../hooks/use-step-navigation';
 import { trackEventWithFlow } from '../../lib/analytics';
 import { STORE_KEY as ONBOARD_STORE } from '../../stores/onboard';
 import { DOMAIN_SUGGESTIONS_STORE } from '../../stores/domain-suggestions';
+import { USER_STORE } from '../../stores/user';
 import { FLOW_ID, domainIsAvailableStatus } from '../../constants';
 import waitForDomainAvailability from './wait-for-domain-availability';
 
@@ -50,6 +51,8 @@ const DomainsStep: React.FunctionComponent< Props > = ( { isModal } ) => {
 
 	// using the selector will get the explicit domain search query with site title and vertical as fallbacks
 	const domainSearch = useSelect( ( select ) => select( ONBOARD_STORE ).getDomainSearch() );
+
+	const currentUser = useSelect( ( select ) => select( USER_STORE ).getCurrentUser() );
 
 	const isCheckingDomainAvailability = useSelect( ( select ): boolean =>
 		select( 'core/data' ).isResolving( DOMAIN_SUGGESTIONS_STORE, 'isAvailable', [
@@ -157,7 +160,7 @@ const DomainsStep: React.FunctionComponent< Props > = ( { isModal } ) => {
 				onDomainSelect={ onDomainSelect }
 				analyticsUiAlgo={ isModal ? 'domain_modal' : 'domain_page' }
 				locale={ locale }
-				onUseYourDomainClick={ handleUseYourDomain }
+				onUseYourDomainClick={ currentUser ? handleUseYourDomain : undefined }
 			/>
 		</div>
 	);
