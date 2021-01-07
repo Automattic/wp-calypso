Automattic/wp-calypso

commit b1f5f2c93f96a432ed819d30cce5366df1eb96c3

Update GSuiteAddUsers to use withShoppingCart (#48687)

client/my-sites/email/controller.js
@@ -28,10 +28,12 @@ export default {
 
 	emailManagementNewGSuiteAccount( pageContext, next ) {
 		pageContext.primary = (
-			<GSuiteAddUsers
-				planType={ pageContext.params.planType }
-				selectedDomainName={ pageContext.params.domain }
-			/>
+			<CalypsoShoppingCartProvider>
+				<GSuiteAddUsers
+					planType={ pageContext.params.planType }
+					selectedDomainName={ pageContext.params.domain }
+				/>
+			</CalypsoShoppingCartProvider>
 		);
 		next();
 	},

client/my-sites/email/gsuite-add-users/index.jsx
@@ -7,11 +7,11 @@ import { localize } from 'i18n-calypso';
 import page from 'page';
 import PropTypes from 'prop-types';
 import React, { Fragment } from 'react';
+import { withShoppingCart } from '@automattic/shopping-cart';
 
 /**
  * Internal dependencies
  */
-import { addItems } from 'calypso/lib/cart/actions';
 import AddEmailAddressesCardPlaceholder from './add-users-placeholder';
 import { Button, Card } from '@automattic/components';
 import DomainManagementHeader from 'calypso/my-sites/domains/domain-management/components/header';
@@ -46,6 +46,8 @@ import QueryGSuiteUsers from 'calypso/components/data/query-gsuite-users';
 import getGSuiteUsers from 'calypso/state/selectors/get-gsuite-users';
 import { recordTracksEvent as recordTracksEventAction } from 'calypso/state/analytics/actions';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
+import { fillInSingleCartItemAttributes } from 'calypso/lib/cart-values';
+import { getProductsList } from 'calypso/state/products-list/selectors/get-products-list';
 
 /**
  * Style dependencies
@@ -56,6 +58,8 @@ class GSuiteAddUsers extends React.Component {
 	state = {
 		users: [],
 	};
+
+	isMounted = false;
 
 	static getDerivedStateFromProps(
 		{ domains, isRequestingDomains, selectedDomainName },
@@ -80,14 +84,17 @@ class GSuiteAddUsers extends React.Component {
 		this.recordClickEvent( 'calypso_email_management_gsuite_add_users_continue_button_click' );
 
 		if ( canContinue ) {
-			addItems(
-				getItemsForCart(
-					domains,
-					'basic' === planType ? GSUITE_BASIC_SLUG : GOOGLE_WORKSPACE_BUSINESS_STARTER_YEARLY,
-					users
+			this.props.shoppingCartManager
+				.addProductsToCart(
+					getItemsForCart(
+						domains,
+						'basic' === planType ? GSUITE_BASIC_SLUG : GOOGLE_WORKSPACE_BUSINESS_STARTER_YEARLY,
+						users
+					).map( ( item ) => fillInSingleCartItemAttributes( item, this.props.productsList ) )
 				)
-			);
-			page( '/checkout/' + selectedSite.slug );
+				.then( () => {
+					this.isMounted && page( '/checkout/' + selectedSite.slug );
+				} );
 		}
 	};
 
@@ -137,6 +144,11 @@ class GSuiteAddUsers extends React.Component {
 	componentDidMount() {
 		const { domains, isRequestingDomains, selectedDomainName } = this.props;
 		this.redirectIfCannotAddEmail( domains, isRequestingDomains, selectedDomainName );
+		this.isMounted = true;
+	}
+
+	componentWillUnmount() {
+		this.isMounted = false;
 	}
 
 	shouldComponentUpdate( nextProps ) {
@@ -287,8 +299,9 @@ export default connect(
 			domainsWithForwards: getDomainsWithForwards( state, domains ),
 			gsuiteUsers: getGSuiteUsers( state, siteId ),
 			isRequestingDomains: isRequestingSiteDomains( state, siteId ),
+			productsList: getProductsList( state ),
 			selectedSite,
 		};
 	},
 	{ recordTracksEvent: recordTracksEventAction }
-)( localize( GSuiteAddUsers ) );
+)( withShoppingCart( localize( GSuiteAddUsers ) ) );

commit fd252ce544d130781b8785720a99ac43ab7ac97d

G Suite: Fix purchase of extra licenses which no longer work
This fixes a bug introduced in https://github.com/Automattic/wp-calypso/pull/48687 which would trigger a 'useShoppingCart must be used inside a ShoppingCartProvider' error when clicking on the `Add New Users` button on the `Email` page, and would result in displaying an error page to the user.

client/my-sites/email/controller.js
@@ -16,7 +16,12 @@ import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopp
 
 export default {
 	emailManagementAddGSuiteUsers( pageContext, next ) {
-		pageContext.primary = <GSuiteAddUsers selectedDomainName={ pageContext.params.domain } />;
+		pageContext.primary = (
+			<CalypsoShoppingCartProvider>
+				<GSuiteAddUsers selectedDomainName={ pageContext.params.domain } />
+			</CalypsoShoppingCartProvider>
+		);
+
 		next();
 	},
 
@@ -35,6 +40,7 @@ export default {
 				/>
 			</CalypsoShoppingCartProvider>
 		);
+
 		next();
 	},
 
@@ -44,16 +50,19 @@ export default {
 				<TitanMailQuantitySelection selectedDomainName={ pageContext.params.domain } />
 			</CalypsoShoppingCartProvider>
 		);
+
 		next();
 	},
 
 	emailManagementForwarding( pageContext, next ) {
 		pageContext.primary = <EmailForwarding selectedDomainName={ pageContext.params.domain } />;
+
 		next();
 	},
 
 	emailManagement( pageContext, next ) {
 		pageContext.primary = <EmailManagement selectedDomainName={ pageContext.params.domain } />;
+
 		next();
 	},
 };