0ff4ccbc3e133d319b5e2f6f2872b64958c86429
Allison Levine <allilevine@users.noreply.github.com>
2021-01-08 08:45:01 -0500

    Fetch Podcast Title From Gutenboarding (#48430)
    * Fetch podcast title to pre-fill site title
    
    * Refactor to fetch podcast title from site title step only
    
    * Update to use useAnchorFmParams()
    
    * Update feed request with podcast-details endpoint
    
    * Move podcast title request to window.AppBoot
    
    * Clean up from moving request
    
    * Remove try...catch since an unresolved Promise should not be a blocker
    
    * Fetch podcast data and set title with hooks
    
    * Fix response type error
    
    * Change type to interface for PodcastDetailsObject
    
    * Clean up hooks based on feedback
    Rename PodcastDetails, specify a generic instead of an overload, remove
    check for podcast title length.

---
+++ client/landing/gutenboarding/hooks/use-podcast-title.ts
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import { useEffect, useState } from 'react';
+import apiFetch from '@wordpress/api-fetch';
+
+/**
+ * Internal dependencies
+ */
+import { useAnchorFmParams } from '../path';
+
+export default function usePodcastTitle(): string | null {
+	const [ podcastTitle, setPodcastTitle ] = useState< string | null >( '' );
+	const { anchorFmPodcastId } = useAnchorFmParams();
+	interface PodcastDetails {
+		title?: string;
+	}
+
+	useEffect( () => {
+		if ( ! anchorFmPodcastId ) {
+			return;
+		}
+		// Fetch podcast title from /podcast-details endpoint
+		( apiFetch< PodcastDetails >( {
+			path: `https://public-api.wordpress.com/wpcom/v2/podcast-details?url=https://anchor.fm/s/${ encodeURIComponent(
+				anchorFmPodcastId
+			) }/podcast/rss&_fields=title`,
+		} ) )
+			.then( ( response ) => {
+				if ( response?.title ) {
+					setPodcastTitle( response.title );
+				}
+			} )
+			.catch( () => {
+				setPodcastTitle( null );
+			} );
+	}, [ anchorFmPodcastId ] );
+	return podcastTitle;
+}
