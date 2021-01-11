7d8c2239619e8735a1c5480ae10410bc391094c2
Markus Dobmann <m.do@posteo.net>
2021-01-11 13:50:52 +0100

    me/acount/password: remove `createReactClass`

--- client/me/account-password/index.jsx
+++ client/me/account-password/index.jsx
@@ -5,7 +5,6 @@
 import { localize } from 'i18n-calypso';
 import { debounce, flowRight as compose, head, isEmpty } from 'lodash';
 import React from 'react';
-import createReactClass from 'create-react-class';
 import debugFactory from 'debug';
 import classNames from 'classnames';
 import { connect } from 'react-redux';
@@ -37,39 +36,35 @@ const wpcom = wp.undocumented();
 const debug = debugFactory( 'calypso:me:account-password' );
 
 /* eslint-disable react/prefer-es6-class */
-const AccountPassword = createReactClass( {
-	displayName: 'AccountPassword',
+class AccountPassword extends React.Component {
+	state = {
+		password: '',
+		validation: null,
+		pendingValidation: true,
+		isUnsaved: false,
+	};
 
 	componentDidMount() {
 		this.debouncedPasswordValidate = debounce( this.validatePassword, 300 );
-	},
+	}
 
 	componentDidUpdate( prevProps ) {
 		if ( prevProps.isPendingPasswordChange && ! this.props.isPendingPasswordChange ) {
 			// eslint-disable-next-line react/no-did-update-set-state
 			this.setState( { isUnsaved: false } );
 		}
-	},
+	}
 
-	getInitialState() {
-		return {
-			password: '',
-			validation: null,
-			pendingValidation: true,
-			isUnsaved: false,
-		};
-	},
-
-	generateStrongPassword() {
+	generateStrongPassword = () => {
 		this.setState( {
 			password: generatePassword(),
 			pendingValidation: true,
 			isUnsaved: true,
 		} );
 		this.debouncedPasswordValidate();
-	},
+	};
 
-	validatePassword() {
+	validatePassword = () => {
 		const password = this.state.password;
 		debug( 'Validating password' );
 
@@ -89,9 +84,9 @@ const AccountPassword = createReactClass( {
 
 			this.setState( { pendingValidation: false, validation: { password, ...data } } );
 		} );
-	},
+	};
 
-	handlePasswordChange( event ) {
+	handlePasswordChange = ( event ) => {
 		const newPassword = event.currentTarget.value;
 		debug( 'Handle password change has been called.' );
 		this.debouncedPasswordValidate();
@@ -100,28 +95,28 @@ const AccountPassword = createReactClass( {
 			pendingValidation: true,
 			isUnsaved: '' !== newPassword,
 		} );
-	},
+	};
 
-	handleSaveButtonClick() {
+	handleSaveButtonClick = () => {
 		this.props.recordGoogleEvent( 'Me', 'Clicked on Save Password Button' );
-	},
+	};
 
-	handleGenerateButtonClick() {
+	handleGenerateButtonClick = () => {
 		this.props.recordGoogleEvent( 'Me', 'Clicked on Generate Strong Password Button' );
 		this.generateStrongPassword();
-	},
+	};
 
-	handleNewPasswordFieldFocus() {
+	handleNewPasswordFieldFocus = () => {
 		this.props.recordGoogleEvent( 'Me', 'Focused on New Password Field' );
-	},
+	};
 
-	submitForm( event ) {
+	submitForm = ( event ) => {
 		event.preventDefault();
 
 		this.props.saveUserSettings( { password: this.state.password } );
-	},
+	};
 
-	renderValidationNotices() {
+	renderValidationNotices = () => {
 		const { translate } = this.props;
 		const failure = head( this.state.validation?.test_results.failed );
 
@@ -132,7 +127,7 @@ const AccountPassword = createReactClass( {
 		} else if ( ! isEmpty( failure ) ) {
 			return <FormInputValidation isError text={ failure.explanation } />;
 		}
-	},
+	};
 
 	render() {
 		const { translate } = this.props;
@@ -188,8 +183,8 @@ const AccountPassword = createReactClass( {
 				</FormButtonsBar>
 			</form>
 		);
-	},
-} );
+	}
+}
 
 export default compose(
 	connect(
