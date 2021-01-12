3d462fa836c61603170b6868acd8481a7f4e8759
Veselin Nikolov <veselin@automattic.com>
2021-01-12 17:17:27 +0200

    End the Free Plans Domain Upsell experiment
    The test variation is winning

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -65,15 +65,6 @@ export default {
 		defaultVariation: 'control',
 		allowExistingUsers: false,
 	},
-	freePlansDomainUpsell: {
-		datestamp: '20201210',
-		variations: {
-			test: 50,
-			control: 50,
-		},
-		defaultVariation: 'control',
-		allowExistingUsers: true,
-	},
 	newUsersWithFreePlan: {
 		datestamp: '20210107',
 		variations: {
