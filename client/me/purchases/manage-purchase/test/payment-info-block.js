49d4b587e9a14aac517f23da6f1158cae01cb534
Payton Swick <payton@foolord.com>
2021-01-13 13:15:24 -0500

    Add tests for when auto-renew is disabled

--- client/me/purchases/manage-purchase/test/payment-info-block.js
+++ client/me/purchases/manage-purchase/test/payment-info-block.js
@@ -14,95 +14,102 @@ import { render, screen } from '@testing-library/react';
 import PaymentInfoBlock from '../payment-info-block';
 
 describe( 'PaymentInfoBlock', () => {
-	it( 'renders "Credits" when the purchase was paid with credits', () => {
-		const purchase = { payment: { type: 'credits' } };
-		render( <PaymentInfoBlock purchase={ purchase } /> );
-		expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'Credits' );
-	} );
-
-	it( 'renders "Included with plan" when the purchase is bundled', () => {
-		const purchase = { expiryStatus: 'included' };
-		render( <PaymentInfoBlock purchase={ purchase } /> );
-		expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'Included with plan' );
-	} );
-
-	it( 'renders "None" when the purchase has no payment method', () => {
-		const purchase = {};
-		render( <PaymentInfoBlock purchase={ purchase } /> );
-		expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'None' );
-	} );
-
-	describe( 'when the purchase has a credit card as the payment method', () => {
-		const expiryDate = new Date();
-		expiryDate.setDate( expiryDate.getDate() + 365 );
-		const purchase = {
-			payment: {
-				type: 'credit_card',
-				creditCard: { number: '1234', expiryDate, type: 'mastercard' },
-			},
-		};
-
-		it( 'renders the credit card last4', () => {
+	describe.each( [
+		[ 'enabled', undefined ],
+		[ 'disabled', 'manualRenew' ],
+	] )( 'when auto-renew is %s', ( autoRenewStatus, expiryStatus ) => {
+		it( 'renders "Credits" when the purchase was paid with credits', () => {
+			const purchase = { expiryStatus, payment: { type: 'credits' } };
 			render( <PaymentInfoBlock purchase={ purchase } /> );
-			expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( '1234' );
+			expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'Credits' );
 		} );
 
-		it( 'renders the credit card logo', () => {
+		it( 'renders "Included with plan" when the purchase is bundled', () => {
+			const purchase = { expiryStatus: 'included' };
 			render( <PaymentInfoBlock purchase={ purchase } /> );
-			expect( screen.getByLabelText( 'Mastercard' ) ).toBeInTheDocument();
-		} );
-	} );
-
-	it( 'renders "None" when the purchase has an expired credit card as the payment method', () => {
-		const expiryDate = new Date();
-		expiryDate.setDate( expiryDate.getDate() - 365 );
-		const purchase = {
-			expiryStatus: 'expired',
-			payment: {
-				type: 'credit_card',
-				creditCard: { number: '1234', expiryDate, type: 'mastercard' },
-			},
-		};
-		render( <PaymentInfoBlock purchase={ purchase } /> );
-		expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'None' );
-	} );
-
-	it( 'renders "None" when the purchase has an nearly-expiring credit card as the payment method', () => {
-		const expiryDate = new Date();
-		expiryDate.setDate( expiryDate.getDate() - 365 );
-		const purchase = {
-			expiryStatus: 'expiring',
-			payment: {
-				type: 'credit_card',
-				creditCard: { number: '1234', expiryDate, type: 'mastercard' },
-			},
-		};
-		render( <PaymentInfoBlock purchase={ purchase } /> );
-		expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'None' );
-	} );
-
-	describe( 'when the purchase has PayPal Direct as the payment method', () => {
-		const expiryDate = new Date();
-		expiryDate.setDate( expiryDate.getDate() + 365 );
-		const purchase = {
-			payment: {
-				type: 'paypal_direct',
-				expiryDate,
-			},
-		};
-
-		it( 'renders the expiration date', () => {
-			const month = expiryDate.toLocaleString( 'default', { month: 'long' } );
-			const expiryAsMonthYear = `${ month } ${ expiryDate.getFullYear() }`;
-			render( <PaymentInfoBlock purchase={ purchase } /> );
-			expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent(
-				`expiring ${ expiryAsMonthYear }`
-			);
+			expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'Included with plan' );
 		} );
 
-		it( 'renders the placeholder logo', () => {
+		it( 'renders "None" when the purchase has no payment method', () => {
+			const purchase = {};
 			render( <PaymentInfoBlock purchase={ purchase } /> );
-			expect( screen.getByLabelText( 'Payment logo' ) ).toBeInTheDocument();
+			expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'None' );
+		} );
+
+		describe( 'when the purchase has a credit card as the payment method', () => {
+			const expiryDate = new Date();
+			expiryDate.setDate( expiryDate.getDate() + 365 );
+			const purchase = {
+				expiryStatus,
+				payment: {
+					type: 'credit_card',
+					creditCard: { number: '1234', expiryDate, type: 'mastercard' },
+				},
+			};
+
+			it( 'renders the credit card last4', () => {
+				render( <PaymentInfoBlock purchase={ purchase } /> );
+				expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( '1234' );
+			} );
+
+			it( 'renders the credit card logo', () => {
+				render( <PaymentInfoBlock purchase={ purchase } /> );
+				expect( screen.getByLabelText( 'Mastercard' ) ).toBeInTheDocument();
+			} );
+		} );
+
+		it( 'renders "None" when the purchase has an expired credit card as the payment method', () => {
+			const expiryDate = new Date();
+			expiryDate.setDate( expiryDate.getDate() - 365 );
+			const purchase = {
+				expiryStatus: 'expired',
+				payment: {
+					type: 'credit_card',
+					creditCard: { number: '1234', expiryDate, type: 'mastercard' },
+				},
+			};
+			render( <PaymentInfoBlock purchase={ purchase } /> );
+			expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'None' );
+		} );
+
+		it( 'renders "None" when the purchase has an nearly-expiring credit card as the payment method', () => {
+			const expiryDate = new Date();
+			expiryDate.setDate( expiryDate.getDate() - 365 );
+			const purchase = {
+				expiryStatus: 'expiring',
+				payment: {
+					type: 'credit_card',
+					creditCard: { number: '1234', expiryDate, type: 'mastercard' },
+				},
+			};
+			render( <PaymentInfoBlock purchase={ purchase } /> );
+			expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'None' );
+		} );
+
+		describe( 'when the purchase has PayPal Direct as the payment method', () => {
+			const expiryDate = new Date();
+			expiryDate.setDate( expiryDate.getDate() + 365 );
+			const purchase = {
+				expiryStatus,
+				payment: {
+					type: 'paypal_direct',
+					expiryDate,
+				},
+			};
+
+			it( 'renders the expiration date', () => {
+				const month = expiryDate.toLocaleString( 'default', { month: 'long' } );
+				const expiryAsMonthYear = `${ month } ${ expiryDate.getFullYear() }`;
+				render( <PaymentInfoBlock purchase={ purchase } /> );
+				expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent(
+					`expiring ${ expiryAsMonthYear }`
+				);
+			} );
+
+			it( 'renders the placeholder logo', () => {
+				render( <PaymentInfoBlock purchase={ purchase } /> );
+				expect( screen.getByLabelText( 'Payment logo' ) ).toBeInTheDocument();
+			} );
 		} );
 	} );
 } );
