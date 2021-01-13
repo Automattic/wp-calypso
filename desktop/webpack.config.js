72fb0a97ba0a7b3d49c8336812c8d95d9fa52a02
nsakaimbo <nsakaimbo@users.noreply.github.com>
2021-01-13 16:33:32 -0700

    desktop: add webpack config

---
+++ desktop/webpack.config.js
@@ -0,0 +1,41 @@
+/**
+ * External Dependencies
+ */
+const path = require( 'path' );
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+	target: 'node',
+	entry: './app/index.js',
+	devtool: 'source-map',
+	node: {
+		__dirname: false,
+	},
+	externals: {
+		electron: 'commonjs electron',
+	},
+	resolve: {
+		alias: {
+			app: path.resolve( __dirname, 'app' ),
+		},
+	},
+	output: {
+		path: path.join( __dirname, 'build' ),
+		filename: 'desktop.js',
+	},
+	module: {
+		rules: [
+			// Needed to resolve dependency keytar
+			{
+				test: /\.node$/,
+				loader: 'node-loader',
+			},
+		],
+	},
+	watch: isDevelopment,
+	watchOptions: {
+		ignored: /node_modules/,
+	},
+	mode: isDevelopment ? 'development' : 'production',
+};
