5d099806ff055d7844755408edf3ec8298c2d568
Payton Swick <payton@foolord.com>
2021-01-13 12:54:45 -0500

    Add more tests for PaymentInfoBlock

--- client/me/purchases/manage-purchase/test/payment-info-block.js
+++ client/me/purchases/manage-purchase/test/payment-info-block.js
@@ -19,4 +19,62 @@ describe( 'PaymentInfoBlock', () => {
 		render( <PaymentInfoBlock purchase={ purchase } /> );
 		expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'Credits' );
 	} );
+
+	it( 'renders "Included with plan" when the purchase is bundled', () => {
+		const purchase = { expiryStatus: 'included' };
+		render( <PaymentInfoBlock purchase={ purchase } /> );
+		expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'Included with plan' );
+	} );
+
+	it( 'renders "None" when the purchase has no payment method', () => {
+		const purchase = {};
+		render( <PaymentInfoBlock purchase={ purchase } /> );
+		expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( 'None' );
+	} );
+
+	describe( 'when the purchase has a credit card as the payment method', () => {
+		const expiryDate = new Date();
+		expiryDate.setDate( expiryDate.getDate() + 365 );
+		const purchase = {
+			payment: {
+				type: 'credit_card',
+				creditCard: { number: '1234', expiryDate, type: 'mastercard' },
+			},
+		};
+
+		it( 'renders the credit card last4', () => {
+			render( <PaymentInfoBlock purchase={ purchase } /> );
+			expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent( '1234' );
+		} );
+
+		it( 'renders the credit card logo', () => {
+			render( <PaymentInfoBlock purchase={ purchase } /> );
+			expect( screen.getByLabelText( 'Mastercard' ) ).toBeInTheDocument();
+		} );
+	} );
+
+	describe( 'when the purchase has PayPal Direct as the payment method', () => {
+		const expiryDate = new Date();
+		expiryDate.setDate( expiryDate.getDate() + 365 );
+		const purchase = {
+			payment: {
+				type: 'paypal_direct',
+				expiryDate,
+			},
+		};
+
+		it( 'renders the expiration date', () => {
+			const month = expiryDate.toLocaleString( 'default', { month: 'long' } );
+			const expiryAsMonthYear = `${ month } ${ expiryDate.getFullYear() }`;
+			render( <PaymentInfoBlock purchase={ purchase } /> );
+			expect( screen.getByLabelText( 'Payment method' ) ).toHaveTextContent(
+				`expiring ${ expiryAsMonthYear }`
+			);
+		} );
+
+		it( 'renders the placeholder logo', () => {
+			render( <PaymentInfoBlock purchase={ purchase } /> );
+			expect( screen.getByLabelText( 'Payment logo' ) ).toBeInTheDocument();
+		} );
+	} );
 } );
