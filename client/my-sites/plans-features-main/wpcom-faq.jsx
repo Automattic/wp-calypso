46fdd304b50f474e8aa5dffbf11f8835461cc156
Michael Cain <michael@michaeldcain.com>
2021-01-13 13:05:15 -0500

    Update plan FAQ to reflect monthly refund period

--- client/my-sites/plans-features-main/wpcom-faq.jsx
+++ client/my-sites/plans-features-main/wpcom-faq.jsx
@@ -142,8 +142,7 @@ const WpcomFAQ = ( { isChatAvailable, siteSlug, translate } ) => {
 			<FAQItem
 				question={ translate( 'Can I cancel my subscription?' ) }
 				answer={ translate(
-					'Yes. We want you to love everything you do at WordPress.com, so we provide a 14-day' +
-						' refund on all of our plans. {{a}}Manage purchases{{/a}}.',
+					'Yes. We want you to love everything you do at WordPress.com, so we provide a 14-day refund on all of our annual plans and a 7-day refund on all of our monthly plans. {{a}}Manage purchases{{/a}}.',
 					{
 						components: { a: <a href={ purchasesRoot } /> },
 					}
