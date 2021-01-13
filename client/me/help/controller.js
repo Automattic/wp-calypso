4f76fb94313b4e38bbdbcd50503567b497fb9a07
Caroline Moore <calobee@gmail.com>
2021-01-13 10:47:37 -0500

    Remove unused feature flag (#48834)

--- client/me/help/controller.js
+++ client/me/help/controller.js
@@ -42,7 +42,7 @@ export function help( context, next ) {
 	// FIXME: Auto-converted from the Flux setTitle action. Please use <DocumentHead> instead.
 	context.store.dispatch( setTitle( i18n.translate( 'Help', { textOnly: true } ) ) );
 
-	context.primary = <HelpComponent isCoursesEnabled={ config.isEnabled( 'help/courses' ) } />;
+	context.primary = <HelpComponent />;
 	next();
 }
 
