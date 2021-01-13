027a1f028d977fdf5f8ab552a74d084e88ba2103
Kamen Stanev <hambai@gmail.com>
2021-01-13 14:55:42 +0200

    * add more error handling in the redirector component

--- client/my-sites/email/titan-redirector/titan-redirector.jsx
+++ client/my-sites/email/titan-redirector/titan-redirector.jsx
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
 import page from 'page';
+import { map, fromPairs } from 'lodash-es';
 
 /**
  * Internal dependencies
@@ -13,6 +14,9 @@ import wp from 'calypso/lib/wp';
 import EmptyContent from 'calypso/components/empty-content';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import { getManagePurchaseUrlFor } from 'calypso/my-sites/purchases/paths';
+import { emailManagementNewTitanAccount } from 'calypso/my-sites/email/paths';
+import getSitesItems from 'calypso/state/selectors/get-sites-items';
+import { getSiteSlug } from 'calypso/state/sites/selectors';
 
 class TitanRedirector extends Component {
 	state = {
@@ -21,11 +25,17 @@ class TitanRedirector extends Component {
 		action: null,
 		orderId: null,
 		subscriptionId: null,
+		blogId: null,
+		domain: null,
 		error: null,
 	};
 
 	componentDidMount() {
-		const { mode, jwt } = this.props;
+		const { mode, jwt, isLoggedIn } = this.props;
+
+		if ( ! isLoggedIn ) {
+			return;
+		}
 
 		wp.undocumented()
 			.getTitanIncomingURL( mode, jwt )
@@ -37,6 +47,8 @@ class TitanRedirector extends Component {
 						action: data?.action,
 						orderId: data?.order_id,
 						subscriptionId: data?.subscription_id,
+						blogId: data?.blog_id,
+						domain: data?.domain,
 					} );
 				},
 				( error ) => {
@@ -50,8 +62,8 @@ class TitanRedirector extends Component {
 	}
 
 	render() {
-		const { translate, isLoggedIn } = this.props;
-		const { loaded, hasError, error, action, subscriptionId } = this.state;
+		const { translate, isLoggedIn, siteSlugs } = this.props;
+		const { loaded, hasError, error, action, domain, blogId, subscriptionId } = this.state;
 
 		if ( ! isLoggedIn ) {
 			return (
@@ -67,25 +79,63 @@ class TitanRedirector extends Component {
 		}
 
 		if ( loaded && hasError ) {
-			return <EmptyContent title={ translate( 'Oops - an error occurred' ) } line={ error } />;
+			return (
+				<EmptyContent
+					title={ translate( 'An error occurred when decoding your request' ) }
+					line={ error }
+				/>
+			);
 		}
 
-		if ( ! subscriptionId ) {
+		if ( ! subscriptionId || ! blogId ) {
 			return <EmptyContent title={ translate( 'No subscription found' ) } />;
 		}
 
-		if ( 'billing' === action ) {
-			return <div>You'll be redirected to: { getManagePurchaseUrlFor( '', subscriptionId ) }</div>;
-			// eslint-disable-next-line no-constant-condition,no-unreachable
-			if ( false ) {
-				page( getManagePurchaseUrlFor( '', subscriptionId ) );
-			}
+		const siteSlug = siteSlugs[ blogId ];
+
+		let redirectURL;
+
+		switch ( action ) {
+			case 'billing':
+				redirectURL = getManagePurchaseUrlFor( siteSlug, subscriptionId );
+				break;
+			case 'buyMoreAccounts':
+				redirectURL = emailManagementNewTitanAccount( siteSlug, domain );
+				break;
+		}
+
+		if ( ! redirectURL ) {
+			return (
+				<EmptyContent
+					title={ translate( 'There is a problem with this page' ) }
+					line={ translate( 'Please contact our support team' ) }
+				/>
+			);
+		}
+
+		return (
+			<div>
+				You'll be redirected to: <a href={ redirectURL }>{ redirectURL }</a>
+			</div>
+		);
+
+		// eslint-disable-next-line no-constant-condition,no-unreachable
+		if ( false ) {
+			page.redirect( redirectURL );
 		}
 	}
 }
 
 export default connect( ( state ) => {
+	const sitesItems = getSitesItems( state );
+
+	// eslint-disable-next-line wpcalypso/redux-no-bound-selectors
+	const siteSlugPairs = map( sitesItems, ( site ) => {
+		return [ site.ID, getSiteSlug( state, site.ID ) ];
+	} );
+
 	return {
 		isLoggedIn: isUserLoggedIn( state ),
+		siteSlugs: fromPairs( siteSlugPairs ),
 	};
 } )( localize( TitanRedirector ) );
