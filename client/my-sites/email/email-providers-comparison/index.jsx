b59c9c357631bbcca960b2e1221ce43ae7047799
Dale du Preez <dale@automattic.com>
2021-01-13 13:39:59 +0200

    Use helper function in email comparison UI

--- client/my-sites/email/email-providers-comparison/index.jsx
+++ client/my-sites/email/email-providers-comparison/index.jsx
@@ -44,6 +44,7 @@ import titanLogo from 'calypso/assets/images/email-providers/titan.svg';
 import googleWorkspaceLogo from 'calypso/assets/images/email-providers/google-workspace.svg';
 import gSuiteLogo from 'calypso/assets/images/email-providers/gsuite.svg';
 import forwardingIcon from 'calypso/assets/images/email-providers/forwarding.svg';
+import { getTitanProductName } from 'calypso/lib/titan/get-titan-product-name';
 
 /**
  * Style dependencies
@@ -195,12 +196,13 @@ class EmailProvidersComparison extends React.Component {
 					},
 					comment: '{{price/}} is the formatted price, e.g. $20',
 			  } );
-		const providerTitle = config.isEnabled( 'titan/phase-2' )
-			? translate( 'Email' )
-			: translate( 'Titan Mail' );
-		const providerCtaText = config.isEnabled( 'titan/phase-2' )
-			? translate( 'Add Email' )
-			: translate( 'Add Titan Mail' );
+		const providerName = getTitanProductName();
+		const providerCtaText = translate( 'Add %(emailProductName)s', {
+			args: {
+				emailProductName: providerName,
+			},
+			comment: '%(emailProductName)s is the product name, either "Email" or "Titan Mail"',
+		} );
 		const providerEmailLogo = config.isEnabled( 'titan/phase-2' ) ? (
 			<Gridicon
 				className="email-providers-comparison__providers-wordpress-com-email"
@@ -212,7 +214,7 @@ class EmailProvidersComparison extends React.Component {
 
 		return (
 			<EmailProviderDetails
-				title={ providerTitle }
+				title={ providerName }
 				description={ translate(
 					'Easy-to-use email with incredibly powerful features. Manage your email and more on any device.'
 				) }
