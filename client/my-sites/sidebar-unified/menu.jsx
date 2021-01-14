78b5ab7066df2cd8a23030bace08dddfaa688f0e
cpap <papazoglou.charalampos@gmail.com>
2021-01-14 18:20:16 +0200

    Fixes items pointing to wp-admin

--- client/my-sites/sidebar-unified/menu.jsx
+++ client/my-sites/sidebar-unified/menu.jsx
@@ -65,13 +65,13 @@ export const MySitesSidebarUnifiedMenu = ( {
 		<li>
 			<ExpandableSidebarMenu
 				onClick={ () => {
-					if ( isExternal( link ) ) {
-						// If the URL is external, page() will fail to replace state between different domains.
-						externalRedirect( link );
-						return;
-					}
-
 					if ( isWithinBreakpoint( '>660px' ) ) {
+						if ( isExternal( link ) ) {
+							// If the URL is external, page() will fail to replace state between different domains.
+							externalRedirect( link );
+							return;
+						}
+
 						// Only open the page if menu is NOT full-width, otherwise just open / close the section instead of directly redirecting to the section.
 						page( link );
 
