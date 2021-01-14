e01f5290b2ba6e16f6589f4352db7e751f6c4f5c
Dale du Preez <dale@automattic.com>
2021-01-14 14:41:32 +0200

    Show number of Email mailboxes in domain management nav

--- client/my-sites/domains/domain-management/edit/navigation/enhanced.jsx
+++ client/my-sites/domains/domain-management/edit/navigation/enhanced.jsx
@@ -43,7 +43,7 @@ import { withoutHttp } from 'calypso/lib/url';
 import RemovePurchase from 'calypso/me/purchases/remove-purchase';
 import { hasGSuiteWithUs, getGSuiteMailboxCount } from 'calypso/lib/gsuite';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
-import { getTitanProductName } from 'calypso/lib/titan/get-titan-product-name';
+import { getMaxTitanMailboxCount } from 'calypso/lib/titan';
 import { isRecentlyRegistered } from 'calypso/lib/domains/utils';
 import { hasTitanMailWithUs } from 'calypso/lib/titan/has-titan-mail-with-us';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
@@ -113,7 +113,18 @@ class DomainManagementNavigationEnhanced extends React.Component {
 				}
 			);
 		} else if ( hasTitanMailWithUs( domain ) ) {
-			navigationDescription = getTitanProductName();
+			const titanMailboxCount = getMaxTitanMailboxCount( domain );
+			navigationDescription = translate(
+				'%(titanMailboxCount)d mailbox',
+				'%(titanMailboxCount)d mailboxes',
+				{
+					args: {
+						titanMailboxCount,
+					},
+					count: titanMailboxCount,
+					comment: '%(titanMailboxCount)d is the number of mailboxes for the current domain',
+				}
+			);
 		} else if ( emailForwardsCount > 0 ) {
 			navigationDescription = translate(
 				'%(emailForwardsCount)d forward',
