95a9c38ca62c39170b93f2635d0b6b1af427e75a
Dale du Preez <dale@automattic.com>
2021-01-14 22:24:12 +0200

    Add function to get current number of mailboxes

--- client/lib/titan/index.js
+++ client/lib/titan/index.js
@@ -1,3 +1,4 @@
 export { getTitanMailOrderId } from './get-titan-mail-order-id';
+export { getConfiguredTitanMailboxCount } from './get-configured-titan-mailbox-count';
 export { getMaxTitanMailboxCount } from './get-max-titan-mailbox-count';
 export { hasTitanMailWithUs } from './has-titan-mail-with-us';
