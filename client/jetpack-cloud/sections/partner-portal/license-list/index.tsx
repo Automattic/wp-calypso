5ba0393099ceee19a7dc72d08eabf68fe631c0de
Atanas Angelov <hi@atanas.dev>
2021-01-14 23:34:52 +0200

    Partner Portal: Progress on the list view UI implementation

--- client/jetpack-cloud/sections/partner-portal/license-list/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/license-list/index.tsx
@@ -1,8 +1,9 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { useTranslate } from 'i18n-calypso';
+import classnames from 'classnames';
 
 /**
  * Internal dependencies
@@ -27,54 +28,86 @@ function LicenseListHeader() {
 			<CardHeading size={ 36 }>{ translate( 'Licenses' ) }</CardHeading>
 
 			<CompactCard>
-				<div className="license-list__item">
-					<div>{ translate( 'License state' ) }</div>
-					<div>{ translate( 'Issued on' ) }</div>
-					<div>{ translate( 'Attached on' ) }</div>
-					<div>{ /* Intentionally empty header. */ }</div>
-					<div>{ /* Intentionally empty header. */ }</div>
+				<div className="license-list__item license-list__item--header">
+					<h2>{ translate( 'License state' ) }</h2>
+					<h2>{ translate( 'Issued on' ) }</h2>
+					<h2>{ translate( 'Attached on' ) }</h2>
+					<h2>{ /* Intentionally empty header. */ }</h2>
+					<h2>{ /* Intentionally empty header. */ }</h2>
 				</div>
 			</CompactCard>
 		</>
 	);
 }
 
-function LicensePreview() {
+function LicensePreview( { licenseKey, domain, product, issuedOn, attachedOn } ) {
 	const translate = useTranslate();
+	const [ isOpen, setOpen ] = useState();
+
+	const open = useCallback( () => {
+		setOpen( ! isOpen );
+	}, [ isOpen ] );
 
 	return (
-		<CompactCard>
-			<div className="license-preview license-list__item">
-				<div>
-					<h3 className="license-list__item-title">example.org</h3>
-					{ translate( 'Product: %s', { args: [ 'Jetpack Security Daily' ] } ) }
+		<>
+			<CompactCard
+				className={ classnames( {
+					'license-preview': true,
+					'license-preview--is-open': isOpen,
+				} ) }
+			>
+				<div className="license-list__item">
+					<div>
+						{ domain && <h3 className="license-preview__domain">{ domain }</h3> }
+
+						{ ! domain && <h3 className="license-preview__domain">{ 'WIP: Detached' }</h3> }
+
+						<span>{ translate( 'Product: %s', { args: [ product ] } ) }</span>
+					</div>
+
+					<div>
+						<FormattedDate date={ issuedOn } format="YYYY-MM-DD" />
+					</div>
+
+					<div>
+						{ attachedOn && <FormattedDate date={ attachedOn } format="YYYY-MM-DD" /> }
+						{ ! attachedOn && <>&mdash;</> }
+					</div>
+
+					<div>
+						{ ! domain && (
+							<ClipboardButton
+								text={ licenseKey }
+								className="license-preview__copy-license-key"
+								compact
+							>
+								{ translate( 'Copy License' ) }
+							</ClipboardButton>
+						) }
+					</div>
+
+					<div>
+						<Button onClick={ open } borderless>
+							<Gridicon icon={ isOpen ? 'chevron-up' : 'chevron-down' } />
+						</Button>
+					</div>
 				</div>
-				<div>
-					<FormattedDate date="2020-11-24 18:24:52" format="YYYY-MM-DD" />
-				</div>
-				<div>
-					<FormattedDate date="2020-11-25 18:24:52" format="YYYY-MM-DD" />
-				</div>
-				<div>
-					<ClipboardButton
-						text="Text to copy"
-						className="license-preview__copy-license-key"
-						compact
-					>
-						{ translate( 'Copy License' ) }
-					</ClipboardButton>
-				</div>
-				<div>
-					<Button borderless compact>
-						<Gridicon icon="chevron-down" />
-					</Button>
-				</div>
-			</div>
-		</CompactCard>
+			</CompactCard>
+
+			{ isOpen && (
+				<LicenseDetails
+					licenseKey={ 'jetpack-security-realtime_AcNAyEhPaSXeFVgRj0gZkgn0Z' }
+					issuedOn={ '2020-11-24 18:24:52' }
+					attachedOn={ '2020-11-25 18:24:52' }
+					username={ 'ianramosc' }
+					blogId={ '883882032' }
+				/>
+			) }
+		</>
 	);
 }
 
-function LicenseDetails() {
+function LicenseDetails( { licenseKey, issuedOn, attachedOn, username, blogId } ) {
 	const translate = useTranslate();
 
 	return (
@@ -84,10 +117,10 @@ function LicenseDetails() {
 					<li className="license-details__list-item license-details__list-item--wide">
 						<h4 className="license-details__label">{ translate( 'License code' ) }</h4>
 
-						<code className="license-details__license-key">premium_AcNAyEhPaSXeFVgRj0gZkgn0Z</code>
+						<code className="license-details__license-key">{ licenseKey }</code>
 
 						<ClipboardButton
-							text="Text to copy"
+							text={ licenseKey }
 							className="license-details__clipboard-button"
 							borderless
 							compact
@@ -97,19 +130,19 @@ function LicenseDetails() {
 					</li>
 					<li className="license-details__list-item">
 						<h4 className="license-details__label">{ translate( 'Issued on' ) }</h4>
-						<FormattedDate date="2020-11-24 18:24:52" format="LLL" />
+						<FormattedDate date={ issuedOn } format="LLL" />
 					</li>
 					<li className="license-details__list-item">
 						<h4 className="license-details__label">{ translate( 'Attached on' ) }</h4>
-						<FormattedDate date="2020-11-25 18:24:52" format="LLL" />
+						<FormattedDate date={ attachedOn } format="LLL" />
 					</li>
 					<li className="license-details__list-item">
 						<h4 className="license-details__label">{ translate( "Owner's User ID" ) }</h4>
-						<span>1234567890</span>
+						<span>{ username }</span>
 					</li>
 					<li className="license-details__list-item">
 						<h4 className="license-details__label">{ translate( 'Blog ID' ) }</h4>
-						<span>1234567890</span>
+						<span>{ blogId }</span>
 					</li>
 				</ul>
 				<div className="license-details__actions">
@@ -126,9 +159,29 @@ export default function LicenseList() {
 			<Main wideLayout={ true } className="license-list">
 				<LicenseListHeader />
 
-				<LicensePreview />
+				<LicensePreview
+					licenseKey={ 'jetpack-security-realtime_AcNAyEhPaSXeFVgRj0gZkgn0Z' }
+					domain={ 'yetanothersite.net' }
+					product={ 'Jetpack Security Daily' }
+					issuedOn={ '2020-11-26 18:24:52' }
+					attachedOn={ '2020-11-27 18:24:52' }
+				/>
 
-				<LicenseDetails />
+				<LicensePreview
+					licenseKey={ 'jetpack-security-realtime_AcNAyEhPaSXeFVgRj0gZkgn0Z' }
+					domain={ '' }
+					product={ 'Jetpack Backup Daily' }
+					issuedOn={ '2020-11-26 17:12:32' }
+					attachedOn={ '' }
+				/>
+
+				<LicensePreview
+					licenseKey={ 'jetpack-security-realtime_AcNAyEhPaSXeFVgRj0gZkgn0Z' }
+					domain={ 'mygroovysite.co.uk' }
+					product={ 'Jetpack Security Real-time' }
+					issuedOn={ '2020-11-24 18:24:52' }
+					attachedOn={ '2020-11-25 18:24:52' }
+				/>
 			</Main>
 		</>
 	);
