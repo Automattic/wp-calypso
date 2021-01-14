f2db009f5b6dc170fe888851b6b9a04c6646540a
flootr <m.do@posteo.net>
2021-01-14 14:36:10 +0100

    reduxify: user-settings in `me/privacy` (#48779)
    * privacy: remove `formBase` and reduxify user settings
    
    * privacy: remove `createReactClass`
    
    * fix: `requestDpa` shouldn't be bound to `dispatch`
    
    * privacy: use `hasUnsavedUserSettings` to indicate pristine form
    
    * privacy: move `requestDpa` to data-getters file

--- client/me/privacy/controller.js
+++ client/me/privacy/controller.js
@@ -6,12 +6,9 @@ import React from 'react';
 /**
  * Internal dependencies
  */
-import userSettings from 'calypso/lib/user-settings';
 import PrivacyComponent from 'calypso/me/privacy/main';
 
 export function privacy( context, next ) {
-	context.primary = React.createElement( PrivacyComponent, {
-		userSettings: userSettings,
-	} );
+	context.primary = React.createElement( PrivacyComponent );
 	next();
 }
