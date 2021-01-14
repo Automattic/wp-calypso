140964f33063d73c89c6be78479efcacbef5469c
Noah Allen <noahtallen@gmail.com>
2021-01-14 12:41:58 -0800

    Convert config to output a single build

--- apps/editing-toolkit/webpack.config.js
+++ apps/editing-toolkit/webpack.config.js
@@ -11,6 +11,7 @@ const getBaseWebpackConfig = require( '@automattic/calypso-build/webpack.config.
 const path = require( 'path' );
 const webpack = require( 'webpack' );
 const { readdirSync } = require( 'fs' );
+const packageFile = require( './package.json' );
 
 const FSE_MODULE_PREFIX = 'a8c-fse';
 
@@ -20,27 +21,61 @@ const FSE_MODULE_PREFIX = 'a8c-fse';
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 /**
+ * Returns the "entry" section of the webpack config.
+ *
+ * 1. Includes all top-level directories from 'full-site-editing-plugin' which
+ *    contain an index.js or index.ts file.
+ * 2. Excludes any sources which are in the package.json exclude list.
+ * 3. Includes additional source paths from package.json
+ *
+ * @returns {Object<string,string>} A dictionary of source names to source paths.
+ */
+function getWebpackEntry() {
+	// Modules which are imported by other modules and do not need compiled themselves.
+	const detectedSources = readdirSync( 'editing-toolkit-plugin', { withFileTypes: true } )
+		.filter(
+			( dir ) =>
+				dir.isDirectory() &&
+				! packageFile.excludedSources.includes( dir.name ) &&
+				readdirSync( path.join( 'editing-toolkit-plugin', dir.name ) ).some(
+					( file ) => file === 'index.ts' || file === 'index.js'
+				)
+		)
+		.map( ( dir ) => dir.name );
+
+	const sources = [ ...detectedSources, ...packageFile.additionalSources ];
+
+	return sources.reduce( ( acc, source ) => {
+		const sourceName = path.basename( source );
+		acc[ sourceName ] = path.join( __dirname, 'editing-toolkit-plugin', source );
+		return acc;
+	}, {} );
+}
+
+/**
  * Return a webpack config object
  *
- * Arguments to this function replicate webpack's so this config can be used on the command line,
- * with individual options overridden by command line args.
+ * Arguments to this function replicate webpack's so this config can be used on
+ * the command line, with individual options overridden by command line args.
  *
  * @see {@link https://webpack.js.org/configuration/configuration-types/#exporting-a-function}
  * @see {@link https://webpack.js.org/api/cli/}
  *
  * @param   {object}  env                           environment options
  * @param   {object}  argv                          options map
- * @param   {string}  argv.source                   plugin slug
  * @param   {string}  argv.entry                    entry path
  * @returns {object}                                webpack config
  */
 function getWebpackConfig( env = {}, argv = {} ) {
 	env.WP = true;
 	const webpackConfig = getBaseWebpackConfig( env, argv );
-	const baseConfig = {
+
+	return {
 		...webpackConfig,
+		entry: getWebpackEntry(),
 		output: {
 			...webpackConfig.output,
+			path: path.join( __dirname, 'editing-toolkit-plugin', 'dist' ),
 			filename: '[name].js', // dynamic filename
 		},
 		plugins: [
@@ -72,62 +107,6 @@ function getWebpackConfig( env = {}, argv = {} ) {
 		devtool: isDevelopment ? 'inline-cheap-source-map' : false,
 		stats: 'minimal',
 	};
-
-	// Modules which are imported by other modules and do not need compiled themselves.
-	const EXCLUDED_MODULES = [ 'e2e-test-helpers', 'block-helpers' ];
-	const sources = readdirSync( 'editing-toolkit-plugin', { withFileTypes: true } )
-		.filter(
-			( dir ) =>
-				dir.isDirectory() &&
-				! EXCLUDED_MODULES.includes( dir.name ) &&
-				readdirSync( path.join( 'editing-toolkit-plugin', dir.name ) ).some(
-					( file ) => file === 'index.ts' || file === 'index.js'
-				)
-		)
-		.map( ( dir ) => dir.name );
-
-	// TODO: Since we parse the sources inline above, this really doesn't do anything.
-	// We still need to add support for common/data-stores, and other "same-package" scripts.
-
-	// Group sources by their root path.
-	const packageSources = sources.reduce( ( accumulator, source ) => {
-		const sourceSegments = source.split( path.sep );
-		// Basically, given 'package/path/to/script', returns 'package'.
-		const packageName = sourceSegments[ 0 ];
-		// Basically, given 'package/path/to/script', returns 'path/to/script'. It removes the "package" part of the path.
-		const scriptName =
-			sourceSegments.length > 1 ? sourceSegments.slice( 1 ).join( path.sep ) : source;
-
-		// Group scripts in the same package together.
-		if ( ! accumulator[ packageName ] ) {
-			accumulator[ packageName ] = {};
-		}
-
-		accumulator[ packageName ][ scriptName ] = path.join(
-			__dirname,
-			'editing-toolkit-plugin',
-			source
-		);
-		return accumulator;
-	}, {} );
-
-	// Each package requires a different webpack config because each package requires
-	// its own output path. So we return an array of webpack configs, one for each
-	// package, based on a similar root. The only real difference is "entry" and "output".
-	return Object.keys( packageSources ).map( ( packageName ) => {
-		const entry = packageSources[ packageName ];
-		const outputPath = path.join( __dirname, 'editing-toolkit-plugin', packageName, 'dist' );
-
-		return {
-			...baseConfig,
-			name: packageName,
-			output: {
-				...baseConfig.output,
-				path: outputPath,
-			},
-			entry,
-		};
-	} );
 }
 
 module.exports = getWebpackConfig;
