3fd70bb3fc335cde925e6abd0d4a4d606edbe891
Matt Sherman <matt@jam123.com>
2021-01-13 22:18:17 -0500

    Update sidebar to use updated selectors

--- client/my-sites/sidebar/index.jsx
+++ client/my-sites/sidebar/index.jsx
@@ -55,6 +55,7 @@ import {
 	canCurrentUserUseEarn,
 	getSiteOption,
 	canCurrentUserUseCalypsoStore,
+	canCurrentUserUseWooCommerceCoreStore,
 } from 'calypso/state/sites/selectors';
 import getSiteChecklist from 'calypso/state/selectors/get-site-checklist';
 import getSiteTaskList from 'calypso/state/selectors/get-site-task-list';
@@ -662,17 +663,27 @@ export class MySitesSidebar extends Component {
 	};
 
 	store() {
-		const { translate, site, siteSuffix, canUserUseCalypsoStore } = this.props;
-		const isCalypsoStoreDeprecatedOrRemoved =
-			isEnabled( 'woocommerce/store-deprecated' ) || isEnabled( 'woocommerce/store-removed' );
+		const {
+			translate,
+			site,
+			siteSuffix,
+			canUserUseCalypsoStore,
+			canUserUseWooCommerceCoreStore,
+		} = this.props;
 
-		if ( ! site || ! canUserUseCalypsoStore ) {
+		if ( ! site ) {
 			return null;
 		}
 
 		let storeLink = '/store' + siteSuffix;
-		if ( isEcommerce( site.plan ) ) {
+		if ( isEcommerce( site.plan ) && canUserUseWooCommerceCoreStore ) {
+			// Eventually, the plan is to have the WooCommerce Core menu item labelled the same
+			// for both Business and eCommerce users. But, for now, we want to continue to
+			// use the "Store" label for eCommerce users because that is what they are used to.
+			// So, we'll just continue to change the link here as we have been doing.
 			storeLink = site.options.admin_url + 'admin.php?page=wc-admin';
+		} else if ( ! canUserUseCalypsoStore ) {
+			return null;
 		}
 
 		const infoCopy = translate(
@@ -683,6 +694,7 @@ export class MySitesSidebar extends Component {
 				},
 			}
 		);
+		const isCalypsoStoreDeprecated = isEnabled( 'woocommerce/store-deprecated' );
 
 		return (
 			<SidebarItem
@@ -693,7 +705,7 @@ export class MySitesSidebar extends Component {
 				forceInternalLink
 				className="sidebar__store"
 			>
-				{ isCalypsoStoreDeprecatedOrRemoved && isBusiness( site.plan ) && (
+				{ isCalypsoStoreDeprecated && isBusiness( site.plan ) && (
 					<InfoPopover className="sidebar__store-tooltip" position="bottom right">
 						{ infoCopy }
 					</InfoPopover>
@@ -711,15 +723,21 @@ export class MySitesSidebar extends Component {
 	woocommerce() {
 		const { site, canUserUseWooCommerceCoreStore, siteSuffix, isSiteWpcomStore } = this.props;
 
+		if ( ! site ) {
+			return null;
+		}
+
 		const isCalypsoStoreDeprecatedOrRemoved =
 			isEnabled( 'woocommerce/store-deprecated' ) || isEnabled( 'woocommerce/store-removed' );
 
 		if (
 			! isCalypsoStoreDeprecatedOrRemoved ||
-			! site ||
 			! isBusiness( site.plan ) ||
 			! canUserUseWooCommerceCoreStore
 		) {
+			// Right now, we only use the "WooCommerce" label for Business plan sites.
+			// eCommerce sites continue to use the "Store" label for now
+			// (see handling in `store()` above.
 			return null;
 		}
 
@@ -1104,6 +1122,7 @@ function mapStateToProps( state ) {
 		canUserViewStats: canCurrentUser( state, siteId, 'view_stats' ),
 		canUserManagePlugins: canCurrentUserManagePlugins( state ),
 		canUserUseCalypsoStore: canCurrentUserUseCalypsoStore( state, siteId ),
+		canUserUseWooCommerceCoreStore: canCurrentUserUseWooCommerceCoreStore( state, siteId ),
 		canUserUseEarn: canCurrentUserUseEarn( state, siteId ),
 		canUserUseCustomerHome: canCurrentUserUseCustomerHome( state, siteId ),
 		currentUser,
