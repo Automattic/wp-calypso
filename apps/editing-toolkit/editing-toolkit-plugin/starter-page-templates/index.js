58da84206da6b84eb1273d59e823a764dd950f02
ramonjd <ramonjd@gmail.com>
2021-01-15 17:24:53 +1100

    Remove sidebar component code

--- apps/editing-toolkit/editing-toolkit-plugin/starter-page-templates/index.js
+++ apps/editing-toolkit/editing-toolkit-plugin/starter-page-templates/index.js
@@ -1,16 +1,13 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
-import { select, dispatch, subscribe } from '@wordpress/data';
-import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+import { dispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import { PageTemplatesPlugin } from './page-template-modal';
-import SidebarTemplatesPlugin from './page-template-modal/components/sidebar-modal-opener';
 import { initializeWithIdentity } from './page-template-modal/utils/tracking';
 import './store';
 
@@ -52,25 +49,7 @@ registerPlugin( 'page-templates', {
 		return (
 			<>
 				<PageTemplatesPlugin { ...templatesPluginSharedProps } shouldPrefetchAssets={ false } />
-				<PluginDocumentSettingPanel
-					name="Template Modal Opener"
-					title={ __( 'Page Layout', 'full-site-editing' ) }
-					className="page-template-modal__sidebar"
-					icon="none"
-				>
-					<SidebarTemplatesPlugin />
-				</PluginDocumentSettingPanel>
 			</>
 		);
 	},
-} );
-
-// Make sidebar plugin open by default.
-const unsubscribe = subscribe( () => {
-	if (
-		! select( 'core/edit-post' ).isEditorPanelOpened( 'page-templates/Template Modal Opener' )
-	) {
-		dispatch( 'core/edit-post' ).toggleEditorPanelOpened( 'page-templates/Template Modal Opener' );
-	}
-	unsubscribe();
 } );
