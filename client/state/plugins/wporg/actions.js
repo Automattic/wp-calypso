e308ffe9d5c521d25e4a9fd4169802b1e4d23a01
Marin Atanasov <tyxla@abv.bg>
2021-01-15 16:32:18 +0200

    State: Implement basic wporg plugins list fetching

--- client/state/plugins/wporg/actions.js
+++ client/state/plugins/wporg/actions.js
@@ -8,14 +8,23 @@ const debug = debugFactory( 'calypso:wporg-data:actions' );
 /**
  * Internal dependencies
  */
-import { fetchPluginInformation } from 'calypso/lib/wporg';
-import { normalizePluginData } from 'calypso/lib/plugins/utils';
+import Dispatcher from 'calypso/dispatcher';
+import wpcom from 'calypso/lib/wp';
 import {
+	fetchPluginInformation,
+	fetchPluginsList as fetchWporgPluginsList,
+} from 'calypso/lib/wporg';
+import { normalizePluginData, normalizePluginsList } from 'calypso/lib/plugins/utils';
+import {
+	PLUGINS_WPORG_LIST_RECEIVE,
+	PLUGINS_WPORG_LIST_REQUEST,
 	PLUGINS_WPORG_PLUGIN_RECEIVE,
 	PLUGINS_WPORG_PLUGIN_REQUEST,
 } from 'calypso/state/action-types';
 
 import 'calypso/state/plugins/init';
+
+const PLUGINS_LIST_DEFAULT_SIZE = 24;
 
 // TODO: fix the selector in `selectors.js` to not return `true` by default
 function isFetching( state, pluginSlug ) {
@@ -50,5 +59,109 @@ export function fetchPluginData( pluginSlug ) {
 				error,
 			} );
 		}
+	};
+}
+
+function receivePluginsList( category, page, searchTerm, data, error, pagination = null ) {
+	return ( dispatch ) =>
+		dispatch( {
+			type: PLUGINS_WPORG_LIST_RECEIVE,
+			category,
+			page,
+			searchTerm,
+			data: normalizePluginsList( data ),
+			pagination,
+			error,
+		} );
+}
+
+/**
+ * WP.org plugins can be filtered either by category or search term.
+ * Pagination is supported only for category queries.
+ * Category can be one of "featured", "popular", "new", "beta" or "recommended".
+ * Search term is an open text field.
+ *
+ * @param {string} category   Plugin category
+ * @param {number} page       Page (1-based)
+ * @param {string} searchTerm Search term
+ * @returns {Function} Action thunk
+ */
+export function fetchPluginsList( category, page, searchTerm ) {
+	return ( dispatch ) => {
+		// @TODO: Implement a fetching check here
+
+		dispatch( {
+			type: PLUGINS_WPORG_LIST_REQUEST,
+			category,
+			page,
+			searchTerm,
+		} );
+
+		// @TODO: Remove when this flux action is completely reduxified
+		Dispatcher.handleViewAction( {
+			type: 'FETCH_WPORG_PLUGINS_LIST',
+			action: 'FETCH_WPORG_PLUGINS_LIST',
+			page,
+			category,
+			searchTerm,
+		} );
+
+		// The "Featured" category is managed by WP.com instead of WP.org
+		if ( 'featured' === category ) {
+			wpcom
+				.undocumented()
+				.getFeaturedPlugins()
+				.then( ( data ) => {
+					dispatch( receivePluginsList( category, page, searchTerm, data, null ) );
+
+					// @TODO: Remove when this flux action is completely reduxified
+					Dispatcher.handleServerAction( {
+						type: 'RECEIVE_WPORG_PLUGINS_LIST',
+						action: 'FETCH_WPORG_PLUGINS_LIST',
+						page: 1,
+						category: 'featured',
+						data: normalizePluginsList( data ),
+						error: null,
+					} );
+				} )
+				.catch( ( error ) => {
+					dispatch( receivePluginsList( category, page, searchTerm, [], error ) );
+
+					// @TODO: Remove when this flux action is completely reduxified
+					Dispatcher.handleServerAction( {
+						type: 'RECEIVE_WPORG_PLUGINS_LIST',
+						action: 'FETCH_WPORG_PLUGINS_LIST',
+						page: 1,
+						category: 'featured',
+						data: normalizePluginsList( [] ),
+						error: null,
+					} );
+				} );
+			return;
+		}
+
+		fetchWporgPluginsList(
+			{
+				pageSize: PLUGINS_LIST_DEFAULT_SIZE,
+				page,
+				category,
+				search: searchTerm,
+			},
+			function ( error, data ) {
+				dispatch(
+					receivePluginsList( category, page, searchTerm, data?.plugins ?? [], error, data?.info )
+				);
+
+				// @TODO: Remove when this flux action is completely reduxified
+				Dispatcher.handleServerAction( {
+					type: 'RECEIVE_WPORG_PLUGINS_LIST',
+					action: 'FETCH_WPORG_PLUGINS_LIST',
+					page,
+					category,
+					data: data ? normalizePluginsList( data?.plugins ) : null,
+					error,
+				} );
+			}
+		);
 	};
 }
