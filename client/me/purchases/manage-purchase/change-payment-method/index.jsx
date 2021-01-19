994f437428b7af32b62424b2fe88fc1a46ed6638
Payton Swick <payton@foolord.com>
2021-01-19 18:56:10 -0500

    Remove unnecessary PaymentMethodSelector props in ChangePaymentMethod

--- client/me/purchases/manage-purchase/change-payment-method/index.jsx
+++ client/me/purchases/manage-purchase/change-payment-method/index.jsx
@@ -108,7 +108,6 @@ function ChangePaymentMethod( props ) {
 							purchase={ props.purchase }
 							paymentMethods={ paymentMethods }
 							successCallback={ successCallback }
-							siteSlug={ props.siteSlug }
 						/>
 					) : (
 						<PaymentMethodForm
