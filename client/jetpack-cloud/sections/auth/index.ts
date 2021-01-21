7cfbdbdff7a7cd2e5aa0efe42f17d166775d3bef
Renzo Canepa <rcanepag@gmail.com>
2021-01-21 13:16:05 -0300

    Add new controller to initialize user

--- client/jetpack-cloud/sections/auth/index.ts
+++ client/jetpack-cloud/sections/auth/index.ts
@@ -6,14 +6,15 @@ import page from 'page';
 /**
  * Internal dependencies
  */
-import { authConnectPath, authTokenRedirectPath } from './paths';
-import { connect, tokenRedirect } from './controller';
+import { authConnectPath, authTokenRedirectPath, authUserInitializeRedirectPath } from './paths';
+import { connect, tokenRedirect, userInitialize } from './controller';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 import config from 'calypso/config';
 
-export default () => {
+export default (): void => {
 	if ( config.isEnabled( 'jetpack-cloud' ) ) {
 		page( authConnectPath(), connect, makeLayout, clientRender );
-		page( authTokenRedirectPath(), tokenRedirect, makeLayout, clientRender );
+		page( authTokenRedirectPath(), tokenRedirect, userInitialize, makeLayout, clientRender );
+		page( authUserInitializeRedirectPath(), userInitialize, makeLayout, clientRender );
 	}
 };
