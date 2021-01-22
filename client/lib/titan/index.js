c20c657d78a436b06e04cb7ae1a9c420758f4bc8
Dale du Preez <dale@automattic.com>
2021-01-23 01:03:37 +0200

    Add fetcher functions

--- client/lib/titan/index.js
+++ client/lib/titan/index.js
@@ -1,4 +1,7 @@
-export { getTitanMailOrderId } from './get-titan-mail-order-id';
 export { getConfiguredTitanMailboxCount } from './get-configured-titan-mailbox-count';
 export { getMaxTitanMailboxCount } from './get-max-titan-mailbox-count';
+export { getTitanExpiryDate } from './get-titan-expiry-date';
+export { getTitanMailboxPurchaseCost } from './get-titan-mailbox-purchase-cost';
+export { getTitanMailboxRenewalCost } from './get-titan-mailbox-renewal-cost';
+export { getTitanMailOrderId } from './get-titan-mail-order-id';
 export { hasTitanMailWithUs } from './has-titan-mail-with-us';
