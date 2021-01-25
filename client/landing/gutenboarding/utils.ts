d9f2ceba31a278958e50c5410fb2f7c7d7e43902
Matthew Reishus <mreishus@users.noreply.github.com>
2021-01-25 16:11:21 -0600

    getIsAnchorFmSignup(): Provide a base url to the URL() constructor
    To prevent crashes from relative urls.

--- client/landing/gutenboarding/utils.ts
+++ client/landing/gutenboarding/utils.ts
@@ -2,8 +2,10 @@
  getIsAnchorFmSignup is a utility function for parsing the anchor_podcast parameter from a given URL
 */
 export function getIsAnchorFmSignup( urlString: string ): boolean {
-	if ( ! urlString ) return false;
-	const url = new URL( urlString );
+	if ( ! urlString ) {
+		return false;
+	}
+	const url = new URL( urlString, window.location.origin );
 	const decodedUrl = decodeURIComponent( url.search );
 	const searchParams = new URLSearchParams( decodedUrl );
 	const anchorFmPodcastId = searchParams.get( 'anchor_podcast' );
