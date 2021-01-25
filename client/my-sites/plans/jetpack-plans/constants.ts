b8bfee245ff2c834ea617c74cc11bbab39400df4
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-01-25 14:32:40 -0600

    Re-add dynamic getters for translations

--- client/my-sites/plans/jetpack-plans/constants.ts
+++ client/my-sites/plans/jetpack-plans/constants.ts
@@ -110,48 +110,72 @@ export const PRODUCTS_WITH_OPTIONS = [
 ] as const;
 
 // Jetpack Security
-export const OPTION_PLAN_SECURITY: ( variation: string ) => SelectorProduct = ( variation ) => ( {
-	productSlug: OPTIONS_JETPACK_SECURITY,
-	annualOptionSlug: OPTIONS_JETPACK_SECURITY,
-	monthlyOptionSlug: OPTIONS_JETPACK_SECURITY_MONTHLY,
-	term: TERM_ANNUALLY,
-	type: ITEM_TYPE_BUNDLE,
-	subtypes: [ PLAN_JETPACK_SECURITY_DAILY, PLAN_JETPACK_SECURITY_REALTIME ],
-	costProductSlug: PLAN_JETPACK_SECURITY_DAILY,
-	monthlyProductSlug: PLAN_JETPACK_SECURITY_DAILY_MONTHLY,
-	iconSlug: 'jetpack_security_v2',
-	displayName: translate( 'Jetpack Security' ),
-	shortName: translate( 'Security', {
-		comment: 'Short name of the Jetpack Security generic plan',
-	} ),
-	tagline: translate( 'Comprehensive WordPress protection' ),
-	description: translate(
-		'Enjoy the peace of mind of complete site security. ' +
-			'Easy-to-use, powerful security tools guard your site, so you can focus on your business.'
-	),
-	features: {
-		items: buildCardFeaturesFromItem(
-			{
-				[ FEATURE_CATEGORY_SECURITY ]: [
-					FEATURE_PRODUCT_BACKUP_V2,
-					FEATURE_PRODUCT_SCAN_V2,
-					FEATURE_PRODUCT_ANTISPAM_V2,
-					FEATURE_ACTIVITY_LOG_V2,
-				],
-				[ FEATURE_CATEGORY_OTHER ]: [
-					FEATURE_VIDEO_HOSTING_V2,
-					FEATURE_SOCIAL_MEDIA_POSTING_V2,
-					FEATURE_COLLECT_PAYMENTS_V2,
-					FEATURE_SITE_MONETIZATION_V2,
-					FEATURE_PRIORITY_SUPPORT_V2,
-				],
-			},
-			undefined,
-			variation
+export const OPTION_PLAN_SECURITY: ( variation: string ) => SelectorProduct = ( variation ) => {
+	const plan = {
+		productSlug: OPTIONS_JETPACK_SECURITY,
+		annualOptionSlug: OPTIONS_JETPACK_SECURITY,
+		monthlyOptionSlug: OPTIONS_JETPACK_SECURITY_MONTHLY,
+		term: TERM_ANNUALLY,
+		type: ITEM_TYPE_BUNDLE,
+		subtypes: [ PLAN_JETPACK_SECURITY_DAILY, PLAN_JETPACK_SECURITY_REALTIME ],
+		costProductSlug: PLAN_JETPACK_SECURITY_DAILY,
+		monthlyProductSlug: PLAN_JETPACK_SECURITY_DAILY_MONTHLY,
+		iconSlug: 'jetpack_security_v2',
+		displayName: translate( 'Jetpack Security' ),
+		shortName: translate( 'Security', {
+			comment: 'Short name of the Jetpack Security generic plan',
+		} ),
+		tagline: translate( 'Comprehensive WordPress protection' ),
+		description: translate(
+			'Enjoy the peace of mind of complete site security. ' +
+				'Easy-to-use, powerful security tools guard your site, so you can focus on your business.'
 		),
-		more: MORE_FEATURES_LINK,
-	},
-} );
+		features: {
+			items: buildCardFeaturesFromItem(
+				{
+					[ FEATURE_CATEGORY_SECURITY ]: [
+						FEATURE_PRODUCT_BACKUP_V2,
+						FEATURE_PRODUCT_SCAN_V2,
+						FEATURE_PRODUCT_ANTISPAM_V2,
+						FEATURE_ACTIVITY_LOG_V2,
+					],
+					[ FEATURE_CATEGORY_OTHER ]: [
+						FEATURE_VIDEO_HOSTING_V2,
+						FEATURE_SOCIAL_MEDIA_POSTING_V2,
+						FEATURE_COLLECT_PAYMENTS_V2,
+						FEATURE_SITE_MONETIZATION_V2,
+						FEATURE_PRIORITY_SUPPORT_V2,
+					],
+				},
+				undefined,
+				variation
+			),
+			more: MORE_FEATURES_LINK,
+		},
+	};
+
+	Object.defineProperties( plan, {
+		displayName: {
+			get: () => translate( 'Jetpack Security' ),
+		},
+		shortName: {
+			get: () =>
+				translate( 'Security', {
+					comment: 'Short name of the Jetpack Security generic plan',
+				} ),
+		},
+		tagline: { get: () => translate( 'Comprehensive WordPress protection' ) },
+		description: {
+			get: () =>
+				translate(
+					'Enjoy the peace of mind of complete site security. ' +
+						'Easy-to-use, powerful security tools guard your site, so you can focus on your business.'
+				),
+		},
+	} );
+
+	return plan;
+};
 export const OPTION_PLAN_SECURITY_MONTHLY: ( variation: string ) => SelectorProduct = (
 	variation
 ) => ( {
@@ -163,37 +187,58 @@ export const OPTION_PLAN_SECURITY_MONTHLY: ( variation: string ) => SelectorProd
 } );
 
 // Jetpack Backup
-export const OPTION_PRODUCT_BACKUP: ( variation: string ) => SelectorProduct = ( variation ) => ( {
-	productSlug: OPTIONS_JETPACK_BACKUP,
-	annualOptionSlug: OPTIONS_JETPACK_BACKUP,
-	monthlyOptionSlug: OPTIONS_JETPACK_BACKUP_MONTHLY,
-	term: TERM_ANNUALLY,
-	type: ITEM_TYPE_PRODUCT,
-	subtypes: [ PRODUCT_JETPACK_BACKUP_DAILY, PRODUCT_JETPACK_BACKUP_REALTIME ],
-	costProductSlug: PRODUCT_JETPACK_BACKUP_DAILY,
-	monthlyProductSlug: PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY,
-	iconSlug: 'jetpack_backup_v2',
-	displayName: translate( 'Jetpack Backup' ),
-	shortName: translate( 'Backup', {
-		comment: 'Short name of the Jetpack Backup generic product',
-	} ),
-	tagline: translate( 'Recommended for all sites' ),
-	description: translate( 'Never lose a word, image, page, or time worrying about your site.' ),
-	buttonLabel: translate( 'Get Backup' ),
-	features: {
-		items: buildCardFeaturesFromItem(
-			[
-				FEATURE_BACKUP_V2,
-				FEATURE_ONE_CLICK_RESTORE_V2,
-				FEATURE_SECURE_STORAGE_V2,
-				FEATURE_ACTIVITY_LOG_V2,
-				FEATURE_PRIORITY_SUPPORT_V2,
-			],
-			{ withoutDescription: true, withoutIcon: true },
-			variation
-		),
-	},
-} );
+export const OPTION_PRODUCT_BACKUP: ( variation: string ) => SelectorProduct = ( variation ) => {
+	const plan = {
+		productSlug: OPTIONS_JETPACK_BACKUP,
+		annualOptionSlug: OPTIONS_JETPACK_BACKUP,
+		monthlyOptionSlug: OPTIONS_JETPACK_BACKUP_MONTHLY,
+		term: TERM_ANNUALLY,
+		type: ITEM_TYPE_PRODUCT,
+		subtypes: [ PRODUCT_JETPACK_BACKUP_DAILY, PRODUCT_JETPACK_BACKUP_REALTIME ],
+		costProductSlug: PRODUCT_JETPACK_BACKUP_DAILY,
+		monthlyProductSlug: PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY,
+		iconSlug: 'jetpack_backup_v2',
+		displayName: translate( 'Jetpack Backup' ),
+		shortName: translate( 'Backup', {
+			comment: 'Short name of the Jetpack Backup generic product',
+		} ),
+		tagline: translate( 'Recommended for all sites' ),
+		description: translate( 'Never lose a word, image, page, or time worrying about your site.' ),
+		buttonLabel: translate( 'Get Backup' ),
+		features: {
+			items: buildCardFeaturesFromItem(
+				[
+					FEATURE_BACKUP_V2,
+					FEATURE_ONE_CLICK_RESTORE_V2,
+					FEATURE_SECURE_STORAGE_V2,
+					FEATURE_ACTIVITY_LOG_V2,
+					FEATURE_PRIORITY_SUPPORT_V2,
+				],
+				{ withoutDescription: true, withoutIcon: true },
+				variation
+			),
+		},
+	};
+
+	Object.defineProperties( plan, {
+		displayName: {
+			get: () => translate( 'Jetpack Backup' ),
+		},
+		shortName: {
+			get: () =>
+				translate( 'Backup', {
+					comment: 'Short name of the Jetpack Backup generic product',
+				} ),
+		},
+		tagline: { get: () => translate( 'Recommended for all sites' ) },
+		description: {
+			get: () => translate( 'Never lose a word, image, page, or time worrying about your site.' ),
+		},
+		buttonLabel: { get: () => translate( 'Get Backup' ) },
+	} );
+
+	return plan;
+};
 
 export const OPTION_PRODUCT_BACKUP_MONTHLY: ( variation: string ) => SelectorProduct = (
 	variation
