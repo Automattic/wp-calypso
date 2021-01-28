d29e9230aac2c63ed30562f24e0ef12d100b468c
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-01-28 11:31:44 +0200

    Remove unnecessary jest-environment in library code

--- client/lib/mobile-app/index.js
+++ client/lib/mobile-app/index.js
@@ -1,7 +1,4 @@
 /**
- * @jest-environment jsdom
- */
-/**
  * Returns whether user is using a WordPress mobile app.
  *
  * @returns {boolean} Whether the user agent matches the ones used on the WordPress mobile apps.
