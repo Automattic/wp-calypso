70648af036f049e03ce738d8bec45416f6794bd5
Razvan Papadopol <razvan.papadopol@automattic.com>
2021-01-28 10:44:12 +0200

    Use 'free' pathSlug for Free plan since the API doesn't return one.

--- packages/data-stores/src/plans/resolvers.ts
+++ packages/data-stores/src/plans/resolvers.ts
@@ -110,7 +110,7 @@ function normalizePlanProducts(
 			periodAgnosticSlug: periodAgnosticPlan.periodAgnosticSlug,
 			storeSlug: planProduct.product_slug,
 			rawPrice: planProduct.raw_price,
-			pathSlug: planProduct.path_slug,
+			pathSlug: planProduct.product_slug === 'free_plan' ? 'free' : planProduct.path_slug,
 			price:
 				planProduct?.bill_period === MONTHLY_PLAN_BILLING_PERIOD || planProduct.raw_price === 0
 					? getFormattedPrice( planProduct )
