76886aa71a268a58330ff68d04fddc719ad7a4b3
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-01-29 09:33:14 -0600

    Use new iteration getter to pick FAQ component

--- client/jetpack-connect/store-footer.tsx
+++ client/jetpack-connect/store-footer.tsx
@@ -1,15 +1,29 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useMemo } from 'react';
 
 /**
  * Internal dependencies
  */
-import { getFaqComponent } from 'calypso/my-sites/plans/jetpack-plans/iterations';
+import {
+	getForCurrentCROIteration,
+	Iterations,
+} from 'calypso/my-sites/plans/jetpack-plans/iterations';
+import JetpackFAQ from 'calypso/my-sites/plans-features-main/jetpack-faq';
+import JetpackFAQi5 from 'calypso/my-sites/plans-features-main/jetpack-faq-i5';
 
 const StoreFooter: React.FC = () => {
-	const JetpackFAQComponent = getFaqComponent();
+	const JetpackFAQComponent = useMemo(
+		() =>
+			getForCurrentCROIteration( {
+				[ Iterations.V1 ]: JetpackFAQ as React.FC,
+				[ Iterations.V2 ]: JetpackFAQ as React.FC,
+				[ Iterations.I5 ]: JetpackFAQi5,
+				[ Iterations.SPP ]: JetpackFAQi5,
+			} ),
+		[]
+	);
 
 	if ( JetpackFAQComponent ) {
 		return <JetpackFAQComponent />;
