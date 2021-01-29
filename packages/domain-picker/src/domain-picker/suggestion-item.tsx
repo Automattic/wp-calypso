670e4d7a76de523024d90ab51a7c1d2b76b8de3b
Marco Ciampini <marco.ciampini@automattic.com>
2021-01-29 16:47:10 +0100

    Add missing translator comment

--- packages/domain-picker/src/domain-picker/suggestion-item.tsx
+++ packages/domain-picker/src/domain-picker/suggestion-item.tsx
@@ -226,7 +226,11 @@ const DomainPickerSuggestionItem: React.FC< Props > = ( {
 							{ paidIncludedDomainLabel }{ ' ' }
 						</span>
 						<span className="domain-picker__price-renewal">
-							{ sprintf( __( 'Renews at: %s /year', __i18n_text_domain__ ), cost ) }
+							{ sprintf(
+								// translators: %s is the domain renewal cost (i.e. "Renews at: 20$ / year" )
+								__( 'Renews at: %s /year', __i18n_text_domain__ ),
+								cost
+							) }
 						</span>
 					</>
 				) }
