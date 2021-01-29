6ac409942dc1fa3ccd0738bbcb5112635a1f736c
Dominik Porada <dominik@porada.co>
2021-01-29 18:04:53 +0100

    Update color audit script

--- bin/audit-svg-colors.js
+++ bin/audit-svg-colors.js
@@ -33,16 +33,8 @@ const PALETTE_ILLUSTRATION_COLORS = _.pickBy( PALETTE.colors, ( colorValue, colo
 	if ( colorValue === '#000' ) {
 		return;
 	}
-	// Since illustrations are a part of WordPress.com’s and Jetpack’s visual
-	// identity, we prefer them over generic Blue, Green, and Celadon. We don’t
-	// use Simplenote Blue or WooCommerce Purple either
-	return ! (
-		_.startsWith( colorName, 'Blue' ) ||
-		_.startsWith( colorName, 'Green' ) ||
-		_.startsWith( colorName, 'Celadon' ) ||
-		_.startsWith( colorName, 'Simplenote Blue' ) ||
-		_.startsWith( colorName, 'WooCommerce Purple' )
-	);
+	// Avoid specific colors for illustration use
+	return ! _.startsWith( colorName, 'Simplenote Blue' );
 } );
 
 // The subset of palette colors used in app-related images is slightly wider
@@ -71,27 +63,35 @@ const PALETTE_APP_COLOR_VALUES = _.uniq( Object.values( PALETTE_APP_COLORS ) );
 
 // The image paths that match the following patterns will not be processed
 const SVG_IGNORE_PATHS = [
-	// Common logos found in the repository
-	/(?:google-photos|paypal|stripe)(-logo)?\.svg$/,
-
-	// Selected logos from the G-Suite asset directory
+	// Logos found in the repository
+	/(?:billcom|canva|cloudflare|evernote|facebook-messenger|fiverr|google-photos|monday|paypal|quickbooks|sendinblue|stripe|todoist|vaultpress)(-logo)?\.svg$/,
 	/images\/g-suite\/logo_/,
+	/images\/email-providers\/google-workspace/,
+
+	// Illustrations that contain logos
+	/images\/customer-home\/illustration--task-podcasting.svg/,
+	/images\/customer-home\/illustration--task-connect-social-accounts.svg/,
 
 	// Credit card and payment gateway logos (the disabled versions are allowed)
 	/upgrades\/cc-(?:amex|diners|discover|jcb|mastercard|unionpay|visa)\.svg$/,
-	/upgrades\/(?:alipay|bancontact|brazil-tef|emergent-paywall|eps|giropay|ideal|netbanking|ovo|p24|paypal|paytm|sofort|tef|wechat)\.svg$/,
+	/upgrades\/(?:alipay|bancontact|brazil-tef|emergent-paywall|eps|giropay|ideal|netbanking|ovo|p24|paypal|paytm|sofort|tef|wechat)/,
+
+	// Color scheme thumbnails that rely on .org colors
+	/color-scheme-thumbnail-(?:blue|classic-dark|coffee|ectoplasm|light|modern|ocean|sunrise)\.svg$/,
 
 	// Old WooCommerce mascotte
 	/ninja-joy\.svg$/,
 
-	// Specific directories
+	// Specific images directories
 	/^apps\/editing-toolkit\/editing-toolkit-plugin/,
-	/^docs/,
 	/^static\/images\/marketing/,
 	/^static\/images\/me/,
+	/^static\/images\/illustrations\/illustration-woo-magic-link.svg/,
+	/^static\/images\/jetpack\/favicons/,
 
-	// Images that seem to be based on a completely different set of colors
-	/customer-home\/illustration-+(?:task|webinars)/,
+	// Documentation
+	/^docs/,
+	/^client\/my-sites\/checkout\/docs/,
 ];
 
 // The image paths that match the following patterns will use `PALETTE_APP_COLORS`,
@@ -100,20 +100,20 @@ const SVG_APP_PATHS = [
 	// Color scheme thumbnails
 	/color-scheme-thumbnail-[a-z-]+\.svg$/,
 
-	// Screenshots and Gutenberg images
-	/^client\/assets\/images\/customer-home\/illustration/,
-	/^static\/images\/illustrations\/gutenberg/,
+	// Component icons
+	/^client\/components\/jetpack\/daily-backup-status\/status-card/,
 
 	// Plan icons
 	/^static\/images\/plans\//,
-	/^packages\/components\/src\/product-icon\/images\//,
 ];
 
-// The regular expression used to identify color values
+// The regular expressions used to identify color values
 const SVG_VALUE_EXPRESSION = /(?:fill|flood-color|lighting-color|stop-color|stroke)="([a-z0-9#]*?)"/gi;
+const SVG_STYLE_EXPRESSION = /(?:fill|flood-color|lighting-color|stop-color|stroke):\s*([a-z0-9#]*?)\s*[;}]/gi;
 
-// The specific color values to ignore
-const SVG_IGNORE_VALUES = [ 'currentcolor', 'none', 'transparent' ];
+// The specific color values to ignore, including other variations of white
+// primarily to decrease the amount of noise in the output
+const SVG_IGNORE_VALUES = [ 'currentcolor', 'none', 'transparent', 'white', '#ffffff' ];
 
 /**
  * Other constants
@@ -135,9 +135,9 @@ const REPLACEMENT_RULES = [];
  */
 
 SVG_FILES_TO_PROCESS.forEach( ( imagePath ) => {
-	const targetValues = isAppImagePath( imagePath )
-		? PALETTE_APP_COLOR_VALUES
-		: PALETTE_ILLUSTRATION_COLOR_VALUES;
+	const targetPreset = isAppImagePath( imagePath ) ? 'app' : 'illustration';
+	const targetValues =
+		targetPreset === 'app' ? PALETTE_APP_COLOR_VALUES : PALETTE_ILLUSTRATION_COLOR_VALUES;
 
 	const imageContent = getFileContents( imagePath );
 	const matchedColorValues = matchColorValues( imageContent );
@@ -165,6 +165,7 @@ SVG_FILES_TO_PROCESS.forEach( ( imagePath ) => {
 
 	REPLACEMENT_RULES.push( {
 		file: imagePath,
+		preset: targetPreset,
 		rules: colorValuesToReplace.map( ( value ) => {
 			const replacementValue = findClosestColor( value, targetValues );
 			const replacementName = findPaletteColorName( replacementValue );
@@ -213,10 +214,12 @@ function matchColorValues( content ) {
 	const values = [];
 	let match;
 
-	// `String.matchAll` is unsupported at the moment
-	while ( ( match = SVG_VALUE_EXPRESSION.exec( content ) ) !== null ) {
-		values.push( match[ 1 ].toLowerCase() );
-	}
+	[ SVG_VALUE_EXPRESSION, SVG_STYLE_EXPRESSION ].forEach( ( expression ) => {
+		// `String.matchAll` is unsupported at the moment
+		while ( ( match = expression.exec( content ) ) !== null ) {
+			values.push( match[ 1 ].toLowerCase() );
+		}
+	} );
 
 	return values;
 }
@@ -230,6 +233,7 @@ function findClosestColor( value, targetValues ) {
 		const distance = chroma.distance( value, targetValue );
 
 		// This bit shortens existing variations of white to `#fff`
+		// for consitent notation that this script relies on
 		if ( distance === 0 ) {
 			closestValue = targetValue;
 			closestDistance = distance;
@@ -278,7 +282,10 @@ function printReplacementRules( replacementObjects ) {
 
 		replacementObjects.forEach( ( replacementObject ) => {
 			const replacementRules = formatReplacementRules( replacementObject.rules );
-			console.log( `\n${ replacementObject.file }\n${ replacementRules.join( '\n' ) }` );
+			const replacementSuffix = getReplacementPrefixSuffix( replacementObject );
+			console.log(
+				`\n${ replacementObject.file }${ replacementSuffix }\n${ replacementRules.join( '\n' ) }`
+			);
 		} );
 	}
 }
@@ -290,4 +297,9 @@ function formatReplacementRules( rules ) {
 
 		return `${ valueFrom } → ${ valueTo } (${ rule.to.name })`;
 	} );
+}
+
+function getReplacementPrefixSuffix( replacementObject ) {
+	const { preset } = replacementObject;
+	return preset === 'app' ? ' 🖥' : '';
 }
