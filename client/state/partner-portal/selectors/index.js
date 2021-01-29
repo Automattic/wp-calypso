73e594636e581f4a5b14a9af3731b7a0737d1859
Atanas Angelov <hi@atanas.dev>
2021-01-29 15:23:24 +0200

    Partner Portal: Replace unnecessary regex with startsWith

--- client/state/partner-portal/selectors/index.js
+++ client/state/partner-portal/selectors/index.js
@@ -1,8 +1,14 @@
 /**
+ * External dependencies
+ */
+import startsWith from 'lodash/startsWith';
+
+/**
  * Internal dependencies
  */
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 
 export * from 'calypso/state/partner-portal/selectors/partner';
 
-export const isPartnerPortal = ( state ) => /^\/partner-portal(?:\/[^/]*)?/.test( getCurrentRoute( state ) );
+export const isPartnerPortal = ( state ) =>
+	startsWith( getCurrentRoute( state ), '/partner-portal' );
