6780bbb62003f70855899c8eb42191076070d32f
Jessie Ross <jessie.ross@automattic.com>
2021-02-01 19:45:11 +0800

    Implement and test internals

---
+++ packages/explat-client/src/internal/state.ts
@@ -0,0 +1,42 @@
+/**
+ * Internal dependencies
+ */
+import { ExperimentAssignment } from '../types';
+import * as Validations from './validations';
+import * as ExperimentAssignments from './experiment-assignments';
+
+const experimentNameToExperimentAssignment: Record< string, ExperimentAssignment | undefined > = {};
+
+export function storeExperimentAssignment( experimentAssignment: ExperimentAssignment ): void {
+	Validations.validateExperimentAssignment( experimentAssignment );
+
+	const previousExperimentAssignment =
+		experimentNameToExperimentAssignment[ experimentAssignment.experimentName ];
+	if (
+		previousExperimentAssignment &&
+		experimentAssignment.retrievedTimestamp < previousExperimentAssignment.retrievedTimestamp
+	) {
+		throw new Error(
+			'Trying to store an older experiment assignment than is present in the store, likely a race condition.'
+		);
+	}
+
+	if (
+		previousExperimentAssignment &&
+		! previousExperimentAssignment.isFallbackExperimentAssignment &&
+		ExperimentAssignments.isRecent( previousExperimentAssignment ) &&
+		experimentAssignment.isFallbackExperimentAssignment
+	) {
+		throw new Error( 'Replacing recent ExperimentAssignment with fallback ExperimentAssignment.' );
+	}
+
+	experimentNameToExperimentAssignment[
+		experimentAssignment.experimentName
+	] = experimentAssignment;
+}
+
+export function retrieveExperimentAssignment(
+	experimentName: string
+): ExperimentAssignment | undefined {
+	return experimentNameToExperimentAssignment[ experimentName ];
+}
