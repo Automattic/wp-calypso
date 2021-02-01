c0b66ed4f406b1e8a185dd93640cc8ed279df05e
Payton Swick <payton@foolord.com>
2021-02-01 16:50:13 -0500

    Remove unused notice actions

--- client/me/purchases/manage-purchase/change-payment-method/index.jsx
+++ client/me/purchases/manage-purchase/change-payment-method/index.jsx
@@ -18,7 +18,6 @@ import QueryUserPurchases from 'calypso/components/data/query-user-purchases';
 import titles from 'calypso/me/purchases/titles';
 import TrackPurchasePageView from 'calypso/me/purchases/track-purchase-page-view';
 import { clearPurchases } from 'calypso/state/purchases/actions';
-import { errorNotice, infoNotice, successNotice } from 'calypso/state/notices/actions';
 import { getStripeConfiguration } from 'calypso/lib/store-transactions';
 import {
 	getByPurchaseId,
