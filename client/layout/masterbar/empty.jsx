090982773b72a0b3411ba5acdd382843b845f071
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-02-02 14:59:43 -0800

    Update the emptyMasterbar to be more YAGNI

--- client/layout/masterbar/empty.jsx
+++ client/layout/masterbar/empty.jsx
@@ -1,20 +1,8 @@
 /**
  * External dependencies
  */
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
 import React from 'react';
 
-/**
- * Style dependencies
- */
-
-const EmptyMasterbar = ( { className } ) => (
-	<header id="header" className={ classNames( 'masterbar', className ) } />
+export default EmptyMasterbar = () => (
+	<header id="header" className="masterbar" />
 );
-
-EmptyMasterbar.propTypes = {
-	className: PropTypes.string,
-};
-
-export default EmptyMasterbar;
