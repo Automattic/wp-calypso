0330236e688c131c9763a0874272c7244cf31f4d
Nicholas Sakaimbo <nsakaimbo@users.noreply.github.com>
2021-02-02 10:06:40 -0700

    desktop rewrite: remove embedded Calypso server (#49283)

---
+++ desktop/app/app-handlers/updater/manual-updater/index.js
@@ -0,0 +1,177 @@
+/**
+ * External Dependencies
+ */
+const { app, shell } = require( 'electron' );
+const fetch = require( 'electron-fetch' ).default;
+const yaml = require( 'js-yaml' );
+const semver = require( 'semver' );
+
+/**
+ * Internal dependencies
+ */
+const Updater = require( '../../../lib/updater' );
+const log = require( '../../../lib/logger' )( 'desktop:updater:manual' );
+const { bumpStat, sanitizeVersion, getPlatform } = require( '../../../lib/desktop-analytics' );
+
+const statsPlatform = getPlatform( process.platform );
+const sanitizedVersion = sanitizeVersion( app.getVersion() );
+
+const getStatsString = ( isBeta ) =>
+	`${ statsPlatform }${ isBeta ? '-b' : '' }-${ sanitizedVersion }`;
+
+const requestOptions = {
+	headers: {
+		'User-Agent': `WP-Desktop/${ app.getVersion() }`,
+	},
+};
+
+// Comparison fn to sort Github releases by published date (descending)
+function compareByPublishedDate( lhs, rhs ) {
+	return new Date( lhs.published_at ) > new Date( rhs.published_at ) ? -1 : 1;
+}
+
+class ManualUpdater extends Updater {
+	constructor( { apiUrl, downloadUrl, options = {} } ) {
+		super( options );
+
+		this.apiUrl = apiUrl;
+		this.downloadUrl = downloadUrl;
+
+		this.isEffectiveBeta = false;
+	}
+
+	async ping() {
+		try {
+			const url = this.apiUrl;
+			log.info(
+				`Checking for update. Channel: ${
+					this.beta
+				}, Version: ${ app.getVersion() }, Update URL: ${ url }`
+			);
+
+			const releaseResp = await fetch( url, requestOptions );
+
+			if ( releaseResp.status !== 200 ) {
+				return;
+			}
+
+			const releases = await releaseResp.json();
+
+			// Sort by published date (descending)
+			releases.sort( compareByPublishedDate );
+
+			// Get latest release and pre-release from sorted releases
+			const latestStableRelease = releases.find(
+				( d ) => ! d.prerelease && this.getConfigUrl( d.assets )
+			);
+			const latestBetaRelease = releases.find(
+				( d ) => d.prerelease && this.getConfigUrl( d.assets )
+			);
+
+			if ( ! latestStableRelease && ! this.beta ) {
+				log.info( 'No stable release found, skipping update.' );
+				return;
+			}
+
+			let latestStableReleaseVersion;
+			if ( latestStableRelease ) {
+				log.info(
+					`Latest stable release: ${ latestStableRelease.name } (${ latestStableRelease.tag_name })`
+				);
+				const assetUrl = this.getConfigUrl( latestStableRelease.assets );
+				latestStableReleaseVersion = await this.getReleaseVersion( assetUrl );
+			}
+
+			let latestReleaseVersion;
+
+			if ( this.beta && latestBetaRelease ) {
+				log.info(
+					`Latest beta release: ${ latestBetaRelease.name } (${ latestBetaRelease.tag_name })`
+				);
+				const assetUrl = this.getConfigUrl( latestBetaRelease.assets );
+				const latestBetaReleaseVersion = await this.getReleaseVersion( assetUrl );
+
+				if (
+					semver.valid( latestStableReleaseVersion ) &&
+					semver.valid( latestBetaReleaseVersion ) &&
+					semver.lt( latestBetaReleaseVersion, latestStableReleaseVersion )
+				) {
+					latestReleaseVersion = latestStableReleaseVersion;
+
+					log.info(
+						'Latest stable version is newer than latest latest beta. Switching to stable channel:',
+						latestReleaseVersion
+					);
+				} else if ( semver.valid( latestBetaReleaseVersion ) ) {
+					latestReleaseVersion = latestBetaReleaseVersion;
+
+					this.isEffectiveBeta = true;
+				}
+			} else if ( latestStableReleaseVersion ) {
+				latestReleaseVersion = latestStableReleaseVersion;
+			}
+
+			if ( ! latestReleaseVersion ) {
+				log.info( 'No release found' );
+
+				return;
+			}
+
+			if ( semver.lt( app.getVersion(), latestReleaseVersion ) ) {
+				log.info( 'New update is available, prompting user to update to', latestReleaseVersion );
+				bumpStat( 'wpcom-desktop-update-check', `${ getStatsString( this.beta ) }-needs-update` );
+
+				this.setVersion( latestReleaseVersion );
+				this.notify();
+			} else {
+				log.info( 'Update is not available' );
+				bumpStat( 'wpcom-desktop-update-check', `${ getStatsString( this.beta ) }-no-update` );
+
+				return;
+			}
+		} catch ( err ) {
+			log.error( `Update check failed with error: ${ err.message }` );
+			bumpStat( 'wpcom-desktop-update-check', `${ getStatsString( this.beta ) }-check-failed` );
+		}
+	}
+
+	onConfirm() {
+		shell.openExternal( `${ this.downloadUrl }${ this.isEffectiveBeta ? '?beta=1' : '' }` );
+
+		bumpStat( 'wpcom-desktop-update', `${ getStatsString( this.beta ) }-dl-update` );
+	}
+
+	onCancel() {
+		bumpStat( 'wpcom-desktop-update', `${ getStatsString( this.beta ) }-update-cancel` );
+	}
+
+	getConfigUrl( assets ) {
+		if ( ! assets ) {
+			return null;
+		}
+		if ( ! Array.isArray( assets ) ) {
+			return null;
+		}
+		const asset = assets.find( ( file ) => file.name === 'latest.yml' );
+		return asset ? asset.browser_download_url : null;
+	}
+
+	async getReleaseVersion( url ) {
+		try {
+			const resp = await fetch( url, requestOptions );
+
+			if ( resp.status !== 200 ) {
+				return null;
+			}
+
+			const body = await resp.text();
+			const config = yaml.safeLoad( body );
+
+			return config.version || null;
+		} catch ( err ) {
+			log.error( `Failed to get release version: ${ err.message }` );
+		}
+	}
+}
+
+module.exports = ManualUpdater;
---
+++ desktop/app/server/index.js
@@ -0,0 +1,127 @@
+/**
+ * External Dependencies
+ */
+const { app, BrowserWindow, ipcMain: ipc } = require( 'electron' );
+
+/**
+ * Internal dependencies
+ */
+const Config = require( '../lib/config' );
+const Settings = require( '../lib/settings' );
+const cookieAuth = require( '../lib/cookie-auth' );
+const appInstance = require( '../lib/app-instance' );
+const platform = require( '../lib/platform' );
+const System = require( '../lib/system' );
+const log = require( '../lib/logger' )( 'desktop:runapp' );
+const { getPath } = require( '../lib/assets' );
+
+/**
+ * Module variables
+ */
+let mainWindow = null;
+
+function showAppWindow() {
+	const preloadFile = getPath( 'preload.js' );
+	const appUrl = Config.loginURL;
+	// TODO:
+	// - Use BrowserView
+	// - Restore last window location
+	// - Handle migration from prior relative path implementation
+	// - Developer mode with localhost webapp configuration
+	log.info( 'Loading app (' + appUrl + ') in mainWindow' );
+
+	const config = Settings.getSettingGroup( Config.mainWindow, 'window', [
+		'x',
+		'y',
+		'width',
+		'height',
+	] );
+	config.webPreferences.spellcheck = Settings.getSetting( 'spellcheck-enabled' );
+	config.webPreferences.preload = preloadFile;
+
+	mainWindow = new BrowserWindow( config );
+
+	cookieAuth( mainWindow, function () {
+		mainWindow.webContents.send( 'cookie-auth-complete' );
+	} );
+
+	mainWindow.webContents.on( 'did-finish-load', function () {
+		mainWindow.webContents.send( 'app-config', System.getDetails() );
+
+		ipc.on( 'mce-contextmenu', function ( ev ) {
+			mainWindow.webContents.send( 'mce-contextmenu', ev );
+		} );
+	} );
+
+	mainWindow.webContents.session.webRequest.onBeforeRequest( function ( details, callback ) {
+		if (
+			details.resourceType === 'script' &&
+			details.url.startsWith( 'http://' ) &&
+			! details.url.startsWith( Config.server_url + ':' + Config.server_port + '/' )
+		) {
+			log.info(
+				'Redirecting http request ' + details.url + ' to ' + details.url.replace( 'http', 'https' )
+			);
+			callback( { redirectURL: details.url.replace( 'http', 'https' ) } );
+		} else {
+			callback( {} );
+		}
+	} );
+
+	mainWindow.webContents.session.webRequest.onHeadersReceived( function ( details, callback ) {
+		// always allow previews to be loaded in iframes
+		if ( details.resourceType === 'subFrame' ) {
+			const headers = Object.assign( {}, details.responseHeaders );
+			Object.keys( headers ).forEach( function ( name ) {
+				if ( name.toLowerCase() === 'x-frame-options' ) {
+					delete headers[ name ];
+				}
+			} );
+			callback( {
+				cancel: false,
+				responseHeaders: headers,
+			} );
+			return;
+		}
+		callback( { cancel: false } );
+	} );
+
+	ipc.handle( 'get-config', () => {
+		return Config.toRenderer();
+	} );
+
+	ipc.handle( 'get-settings', () => {
+		return Settings.toRenderer();
+	} );
+
+	mainWindow.loadURL( appUrl );
+
+	mainWindow.on( 'close', function () {
+		const currentURL = mainWindow.webContents.getURL();
+		log.info( `Closing main window, last location: '${ currentURL }'` );
+		// TODO: Save last window location
+	} );
+
+	mainWindow.on( 'closed', function () {
+		log.info( 'Window closed' );
+		mainWindow = null;
+	} );
+
+	require( '../window-handlers/failed-to-load' )( mainWindow );
+	require( '../window-handlers/login-status' )( mainWindow );
+	require( '../window-handlers/notifications' )( mainWindow );
+	require( '../window-handlers/external-links' )( mainWindow );
+	require( '../window-handlers/window-saver' )( mainWindow );
+	require( '../window-handlers/debug-tools' )( mainWindow );
+	require( '../window-handlers/spellcheck' )( mainWindow );
+
+	platform.setMainWindow( mainWindow );
+
+	return mainWindow;
+}
+
+module.exports = function () {
+	if ( appInstance.isSingleInstance() ) {
+		app.on( 'ready', showAppWindow );
+	}
+};
