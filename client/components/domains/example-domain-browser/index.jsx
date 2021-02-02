3f986879d1d395aa4d4814884cf87944f2c71d91
Igor Klimer <klimeryk@users.noreply.github.com>
2021-02-02 10:57:37 +0000

    Domains: Fix positioning of the example domain (#49460)
    The way the example domain is built is causing alignment issues - let's
    simplify the markup and let the browser do the positioning of the parts
    of the URL.

--- client/components/domains/example-domain-browser/index.jsx
+++ client/components/domains/example-domain-browser/index.jsx
@@ -17,15 +17,9 @@ export default function ExampleDomainBrowser( { className } ) {
 				<g fill="none" fillRule="evenodd">
 					<path fill="#D8D8D8" d="M10 0h285v50H0V10C0 4.477 4.477 0 10 0z" />
 					<path fill="#FFF" d="M0 50h295v50H0zM94 9h201v30H94a4 4 0 0 1-4-4V13a4 4 0 0 1 4-4z" />
-					<text className="example-domain-browser__protocol">
-						<tspan x="99" y="29">
-							https://
-						</tspan>
-					</text>
-					<text className="example-domain-browser__domain">
-						<tspan x="148" y="29">
-							example.com
-						</tspan>
+					<text x="99" y="29">
+						<tspan className="example-domain-browser__protocol">https://</tspan>
+						<tspan className="example-domain-browser__domain">example.com</tspan>
 					</text>
 					<rect width="30" height="30" x="10" y="10" fill="#FFF" rx="4" />
 					<rect width="30" height="30" x="50" y="10" fill="#FFF" rx="4" />
