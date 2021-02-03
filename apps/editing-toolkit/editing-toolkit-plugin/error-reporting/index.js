7e41d531d51f72f29c8d5191d7587042ecb8bc2c
Marcelo Serpa <81248+fullofcaffeine@users.noreply.github.com>
2021-02-02 19:51:53 -0600

    Simplify by removing TraceKit, test manually a bit more and attempt at adding some unit tests

--- apps/editing-toolkit/editing-toolkit-plugin/error-reporting/index.js
+++ apps/editing-toolkit/editing-toolkit-plugin/error-reporting/index.js
@@ -1,78 +1,18 @@
 /**
- * External dependencies
- */
-import TraceKit from 'tracekit';
-
-/**
- * Internal dependencies
+ * External dependences
  */
 import apiFetch from '@wordpress/api-fetch';
 
 /**
- * Module variables
+ * Internal dependencies
  */
+import handler from './src/handler';
 
-/**
- * Interval for error reports so we don't flood te endpoint. More frequent
- * reports get throttled.
- *
- * @type {number}
- */
-const REPORT_INTERVAL = 60000;
+window.onerror = handler( apiFetch );
 
-const diagnosticData = {
-	user_id: 'todo-get-the-user-id',
-	blog_id: 'todo-get-the-blog-id',
-	extra: {
-		throttled: 0,
-	},
-};
-
-let lastReport = 0;
-
-if ( ! window.onerror ) {
-	TraceKit.report.subscribe( ( errorReport ) => {
-		const error = {
-			message: errorReport.message,
-			url: document.location.href,
-		};
-
-		if ( Array.isArray( errorReport.stack ) ) {
-			const trace = errorReport.stack.slice( 0, 10 );
-			trace.forEach( ( report ) =>
-				Object.keys( report ).forEach( ( key ) => {
-					if ( key === 'context' && report[ key ] ) {
-						report[ key ] = JSON.stringify( report[ key ] ).substring( 0, 256 );
-					} else if ( typeof report[ key ] === 'string' && report[ key ].length > 512 ) {
-						report[ key ] = report[ key ].substring( 0, 512 );
-					} else if ( Array.isArray( report[ key ] ) ) {
-						report[ key ] = report[ key ].slice( 0, 3 );
-					}
-				} )
-			);
-			if ( JSON.stringify( trace ).length < 8192 ) {
-				error.trace = trace;
-			}
-		}
-
-		const now = Date.now();
-		if ( lastReport + REPORT_INTERVAL < now ) {
-			lastReport = now;
-
-			apiFetch( {
-				global: true,
-				path: '/rest/v1.1/js-error',
-				method: 'POST',
-				data: { error: JSON.stringify( error ) },
-			} )
-				.then( () => {
-					diagnosticData.extra.throttled = 0;
-				} )
-				.catch( () => console.error( 'Error: Unable to record the error in Logstash.', error ) );
-		} else {
-			diagnosticData.extra.throttled++;
-		}
-	} );
-}
-
-throw new Error( 'BOOM!' );
+// Remove this code later, here to test the request and the throttling logic manually
+/*setInterval( function () {
+	for ( let i = 0; i < 100; i++ ) {
+		throw new Error( 'BOOOOOM!' );
+	}
+}, 2000 );*/
