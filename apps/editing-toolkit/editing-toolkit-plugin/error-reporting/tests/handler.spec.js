e3162f83b1fbdacfc65e81dd9e349612c11781f1
Marcelo Serpa <81248+fullofcaffeine@users.noreply.github.com>
2021-02-02 19:51:53 -0600

    Simplify by removing TraceKit, test manually a bit more and attempt at adding some unit tests

---
+++ apps/editing-toolkit/editing-toolkit-plugin/error-reporting/tests/handler.spec.js
@@ -0,0 +1,11 @@
+import { before } from 'lodash';
+/**
+ * Internal dependencies
+ */
+import handler from '../src/handler';
+
+describe( 'JS error reporting', () => {
+	it( 'logs the error', async () => {} );
+
+	it( 'throttles if subsequent request sent within the throttling interval', () => {} );
+} );
