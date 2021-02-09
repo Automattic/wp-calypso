7cc0c1a847c309da71382fed3c852af2f5bfd274
roo2 <roo2@protonmail.com>
2021-02-09 15:00:11 +1000

    pr feedback, specify dependency versions

--- packages/page-template-modal/.eslintrc.js
+++ packages/page-template-modal/.eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
 	rules: {
 		'react/react-in-jsx-scope': 0,
-		// page-template-modal renders in a Gutenberg environment and should
-		// conform to those naming conventions instead of Calypso's.
-		'wpcalypso/jsx-classname-namespace': 0,
 	},
 };
