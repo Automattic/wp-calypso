41d17f4cdce4b2e671349238848a3df1871e68fe
Marcelo Serpa <81248+fullofcaffeine@users.noreply.github.com>
2021-02-09 19:05:30 -0600

    Capture head errors

--- apps/editing-toolkit/editing-toolkit-plugin/error-reporting/index.js
+++ apps/editing-toolkit/editing-toolkit-plugin/error-reporting/index.js
@@ -13,25 +13,43 @@ import { throttle } from 'lodash';
  *
  * @type {number} throttling interval (since the last request) in milliseconds.
  */
-const REPORT_INTERVAL = 60000; // 1 minute
+const REPORT_INTERVAL = 1000; // 1 minute
 
-window.addEventListener(
-	'error',
-	throttle( ( { error } ) => {
-		const data = {
-			message: error.message,
-			trace: error.stack,
-			url: document.location.href,
-			feature: 'wp-admin',
-		};
+/**
+ * Errors that happened before this script had a chance to load
+ * are captured in a global array. See `./index.php`.
+ */
+const headErrors = window._jsErr || [];
+const headErrorHandler = window._headJsErrorHandler;
 
-		apiFetch( {
-			global: true,
-			path: '/rest/v1.1/js-error',
-			method: 'POST',
-			data: { error: JSON.stringify( data ) },
-		} )
-			// eslint-disable-next-line no-console
-			.catch( () => console.error( 'Error: Unable to record the error in Logstash.' ) );
-	}, REPORT_INTERVAL )
-);
+const reportError = throttle( ( { error } ) => {
+	debugger;
+	console.log( 'reporting error!', error.message );
+	const data = {
+		message: error.message,
+		trace: error.stack,
+		url: document.location.href,
+		feature: 'wp-admin',
+	};
+
+	apiFetch( {
+		global: true,
+		path: '/rest/v1.1/js-error',
+		method: 'POST',
+		data: { error: JSON.stringify( data ) },
+	} )
+		// eslint-disable-next-line no-console
+		.catch( () => console.error( 'Error: Unable to record the error in Logstash.' ) );
+}, REPORT_INTERVAL );
+
+window.addEventListener( 'error', reportError );
+// Remove the head handler as it's not needed anymore after we set the main one above
+window.removeEventListener( 'error', headErrorHandler );
+// We still need to report the head errors, if any. If more than one, they will be throttled.
+Promise.all( headErrors.map( reportError ) ).then( () => {
+	// Cleanup the global head error variables
+	delete window._jsErr;
+	delete window._headJsErrorHandler;
+} );
+
+throw new Error( 'CORS error?' );
