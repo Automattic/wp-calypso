ce785a7ea98acf480986293ba560a553f22c248d
Payton Swick <payton@foolord.com>
2021-02-12 16:41:09 -0500

    Add useForAllSubscriptions option to saveCreditCard

--- client/me/purchases/manage-purchase/payment-method-selector/stored-payment-method-api.ts
+++ client/me/purchases/manage-purchase/payment-method-selector/stored-payment-method-api.ts
@@ -17,13 +17,16 @@ type StoredCardEndpointResponse = unknown;
 export async function saveCreditCard( {
 	token,
 	stripeConfiguration,
+	useForAllSubscriptions,
 }: {
 	token: string;
 	stripeConfiguration: StripeConfiguration;
+	useForAllSubscriptions: boolean;
 } ): Promise< StoredCardEndpointResponse > {
 	const additionalData = getParamsForApi( {
 		cardToken: token,
 		stripeConfiguration,
+		useForAllSubscriptions,
 	} );
 	const response = await wpcom.me().storedCardAdd( token, additionalData );
 	if ( response.error ) {
@@ -61,14 +64,18 @@ function getParamsForApi( {
 	cardToken,
 	stripeConfiguration,
 	purchase,
+	useForAllSubscriptions,
 }: {
 	cardToken: string;
 	stripeConfiguration: StripeConfiguration;
 	purchase?: Purchase | undefined;
+	useForAllSubscriptions?: boolean;
 } ) {
 	return {
 		payment_partner: stripeConfiguration ? stripeConfiguration.processor_id : '',
 		paygate_token: cardToken,
+		...( useForAllSubscriptions === true ? { use_for_existing: true } : {} ),
+		...( useForAllSubscriptions === false ? { use_for_existing: false } : {} ), // if undefined, we do not add this property
 		...( purchase ? { purchaseId: purchase.id } : {} ),
 	};
 }
