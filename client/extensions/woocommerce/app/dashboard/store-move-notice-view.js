e3a38cfcc483dd08830960fd96e38deec1eba666
Rebecca Scott <me@becdetat.com>
2021-02-15 17:27:53 +1000

    Toggle flag to remove store in dev, desktop and wpcalypso

--- client/extensions/woocommerce/app/dashboard/store-move-notice-view.js
+++ client/extensions/woocommerce/app/dashboard/store-move-notice-view.js
@@ -13,25 +13,12 @@ import { localize, translate } from 'i18n-calypso';
 
 import { Card, Button } from '@automattic/components';
 import { getSelectedSiteWithFallback } from 'woocommerce/state/sites/selectors';
-import config from '@automattic/calypso-config';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 /**
  * Image dependencies
  */
 
 import megaphoneImage from 'calypso/assets/images/woocommerce/megaphone.svg';
-
-function getStoreStatus( isStoreDeprecated, isStoreRemoved ) {
-	if ( isStoreDeprecated ) {
-		return 'store-deprecated';
-	}
-
-	if ( isStoreRemoved ) {
-		return 'store-removed';
-	}
-
-	return '';
-}
 
 class StoreMoveNoticeView extends Component {
 	trackTryWooCommerceClick = () => {
@@ -43,38 +30,26 @@ class StoreMoveNoticeView extends Component {
 	};
 
 	render = () => {
-		const { site, isStoreRemoved } = this.props;
-		const status = getStoreStatus( true, isStoreRemoved );
+		const { site } = this.props;
 
 		return (
-			<Card className={ classNames( 'dashboard__store-move-notice', status ) }>
+			<Card className={ classNames( 'dashboard__store-move-notice', 'store-removed' ) }>
 				<img src={ megaphoneImage } alt="" />
 				<h1>{ translate( 'Find all of your business features in WooCommerce' ) }</h1>
 				<p>
 					{ translate(
-						'We’re retiring Store on February 22. With WooCommerce, discover a more flexible store management experience — including top-level access to your Analytics, Marketing, and Customers. {{link}}Learn more{{/link}} about what to expect.',
+						'We’ve rolled your favorite Store features into WooCommerce. In addition to Products and Orders, you have top-level access for managing your Analytics, Marketing, and Customers. {{link}}Learn more{{/link}} about what has changed.',
 						{
 							components: {
 								link: (
-									<a href="https://wordpress.com/support/new-woocommerce-experience-on-wordpress-dot-com/" />
+									<a
+										onClick={ this.trackLearnMoreAboutWooCommerceClick }
+										href="https://wordpress.com/support/new-woocommerce-experience-on-wordpress-dot-com/"
+									/>
 								),
 							},
 						}
 					) }
-					{ isStoreRemoved &&
-						translate(
-							'We’ve rolled your favorite Store features into WooCommerce. In addition to Products and Orders, you have top-level access for managing your Analytics, Marketing, and Customers. {{link}}Learn more{{/link}} about what has changed.',
-							{
-								components: {
-									link: (
-										<a
-											onClick={ this.trackLearnMoreAboutWooCommerceClick }
-											href="https://wordpress.com/support/new-woocommerce-experience-on-wordpress-dot-com/"
-										/>
-									),
-								},
-							}
-						) }
 				</p>
 				<Button
 					primary
@@ -91,7 +66,6 @@ class StoreMoveNoticeView extends Component {
 function mapStateToProps( state ) {
 	return {
 		site: getSelectedSiteWithFallback( state ),
-		isStoreRemoved: config.isEnabled( 'woocommerce/store-removed' ),
 	};
 }
 
