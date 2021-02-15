e99bbe54ca37bda495b0ff5586b707a7fb0e2273
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-02-15 13:10:18 +0530

    Move Experiment assignment to the plans step

--- client/signup/steps/plans/index.jsx
+++ client/signup/steps/plans/index.jsx
@@ -264,6 +264,7 @@ export class PlansStep extends Component {
 		return (
 			<>
 				<Experiment name="vertical_plan_listing_v2" />
+				<Experiment name="signup_plans_step_redesign_v1" />
 				<StepWrapper
 					flowName={ flowName }
 					stepName={ stepName }
