80119dd0c59b64f1571d0473bdd975267d07cfb7
roo2 <roo2@protonmail.com>
2021-02-15 16:56:16 +1000

    remove outdated code

--- packages/page-template-modal/src/index.js
+++ packages/page-template-modal/src/index.js
@@ -1,7 +1,2 @@
-/**
- * Internal dependencies
- */
-import './styles/page-template-modal.scss';
-
 export { default as PageTemplateModal } from './components/page-template-modal';
 export { initializeWithIdentity as initializeTracksWithIdentity } from './utils/tracking';
