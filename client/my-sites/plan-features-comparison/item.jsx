0065fb25e8df7339424db7cea9e0e3a369c64d27
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-02-15 19:56:52 +0530

    Force prettier

--- client/my-sites/plan-features-comparison/item.jsx
+++ client/my-sites/plan-features-comparison/item.jsx
@@ -1,13 +1,10 @@
 /**
  * External dependencies
  */
-import { useMobileBreakpoint } from '@automattic/viewport-react';
 import React from 'react';
 import Gridicon from 'calypso/components/gridicon';
 
 export function PlanFeaturesAvailableItem( { children } ) {
-	const isMobile = useMobileBreakpoint();
-
 	return (
 		<div className="plan-features-comparison__item">
 			<Gridicon className="plan-features-comparison__item-checkmark" size={ 18 } icon="checkmark" />
@@ -17,8 +14,6 @@ export function PlanFeaturesAvailableItem( { children } ) {
 }
 
 export function PlanFeaturesMissingItem( { children } ) {
-	const isMobile = useMobileBreakpoint();
-
 	return (
 		<div className="plan-features-comparison__item">
 			<Gridicon className="plan-features-comparison__item-cross" size={ 18 } icon="cross" />
