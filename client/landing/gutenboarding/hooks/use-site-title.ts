952d1ba95628fc6f1ad6969e216a61be243c9433
Allison Levine <allison.levine@automattic.com>
2021-02-16 14:24:41 -0500

    Check for existing site title before replacing with podcast title

--- client/landing/gutenboarding/hooks/use-site-title.ts
+++ client/landing/gutenboarding/hooks/use-site-title.ts
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { useEffect } from 'react';
 
 /**
@@ -13,11 +13,14 @@ import usePodcastTitle from './use-podcast-title';
 export default function useSiteTitle(): void {
 	const { setSiteTitle } = useDispatch( ONBOARD_STORE );
 	const podcastTitle = usePodcastTitle();
+	const { siteTitle } = useSelect( ( select ) => ( {
+		siteTitle: select( ONBOARD_STORE ).getSelectedSiteTitle(),
+	} ) );
 
 	useEffect( () => {
-		if ( podcastTitle ) {
+		if ( podcastTitle && ! siteTitle ) {
 			// Set initial site title to podcast title
 			setSiteTitle( podcastTitle );
 		}
-	}, [ podcastTitle, setSiteTitle ] );
+	}, [ podcastTitle, setSiteTitle, siteTitle ] );
 }
