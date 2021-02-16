eec6f9297ed71bcc55ad6e6ffd002562bf9236c9
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-02-16 17:20:29 +0530

    Make the unavailable features in a light shade of gray; use a different version of unlimited premium themes feature

--- client/lib/plans/constants.js
+++ client/lib/plans/constants.js
@@ -194,7 +194,6 @@ export const FEATURE_ALL_PREMIUM_FEATURES = 'all-premium-features';
 export const FEATURE_ALL_PREMIUM_FEATURES_JETPACK = 'all-premium-features-jetpack';
 export const FEATURE_ADVANCED_CUSTOMIZATION = 'advanced-customization';
 export const FEATURE_PREMIUM_THEMES = 'unlimited-premium-themes';
-export const FEATURE_WPCOM_PREMIUM_THEMES = 'premium-themes';
 export const FEATURE_UPLOAD_THEMES_PLUGINS = 'upload-themes-and-plugins';
 export const FEATURE_GOOGLE_ANALYTICS_SIGNUP = 'google-analytics-signup';
 export const FEATURE_FREE_DOMAIN = 'free-custom-domain';
