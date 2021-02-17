c232a4c5d4f87dfcdb9df8fc891efd7bcb51c848
Matthew Reishus <mreishus@users.noreply.github.com>
2021-02-17 17:48:09 -0600

    Anchor Gutenboarding Error: Styling

--- client/landing/gutenboarding/onboarding-block/anchor-error/index.tsx
+++ client/landing/gutenboarding/onboarding-block/anchor-error/index.tsx
@@ -1,13 +1,42 @@
-/* eslint-disable wpcalypso/jsx-classname-namespace */
 /**
  * External dependencies
  */
 import * as React from 'react';
+import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+
+/**
+ * Internal dependencies
+ */
+import { Title } from '@automattic/onboarding';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
+
 const AnchorError: React.FunctionComponent = () => {
 	return (
-		<div className="gutenboarding-page">
-			Sorry, we were not able to locate that Anchor Podcast ID.{ ' ' }
-			<a href="https://anchor.fm">Return to anchor.</a>
+		<div className="anchor-error__container">
+			<div className="anchor-error__center">
+				<Title>{ __( 'We’re sorry!' ) }</Title>
+				<div className="anchor-error__text-container">
+					We’re unable to locate your podcast.
+					<br />
+					Return to Anchor or continue with site creation.
+				</div>
+
+				<div className="anchor-error__button-container">
+					<Button isPrimary href="/new" className="anchor-error__button">
+						{ __( 'Continue' ) }
+					</Button>
+				</div>
+				<div className="anchor-error__link-container">
+					<Button isLink href="https://anchor.fm">
+						{ __( 'Back to Anchor.fm' ) }
+					</Button>
+				</div>
+			</div>
 		</div>
 	);
 };
