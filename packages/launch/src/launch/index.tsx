c20a4ac935c06dfad98f8163f04926f8e8728b34
Marco Ciampini <marco.ciampini@automattic.com>
2021-02-17 14:53:22 +0100

    Add more details to Focused Launch when isLaunchImmediately is true

--- packages/launch/src/launch/index.tsx
+++ packages/launch/src/launch/index.tsx
@@ -43,8 +43,12 @@ const FocusedLaunchModal: React.FunctionComponent< FocusedLaunchModalProps > = (
 
 	React.useEffect( () => {
 		if ( isLaunchImmediately ) {
-			// if there was a plan in cart before redirect to payment processing,
+			// This happens when the user enters the block editor again after
+			// checkout redirection (i.e. when in Calypso, paying with paypal).
+			// If there was a plan in cart before redirect to payment processing,
 			// remove it now since we don't need to reload the page when dismissing Success view
+			// For more details, see
+			// https://github.com/Automattic/wp-calypso/issues/50122
 			unsetPlanProductId();
 			launchSite( siteId );
 		}
