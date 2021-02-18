c64553323e7a61770a9044d568912f0763f1da0c
Atanas Angelov <hi@atanas.dev>
2021-02-18 21:04:46 +0200

    Partner Portal: Rework license sort field and direction to use enums

--- client/jetpack-cloud/sections/partner-portal/controller.tsx
+++ client/jetpack-cloud/sections/partner-portal/controller.tsx
@@ -10,12 +10,17 @@ import type PageJS from 'page';
  */
 import { addQueryArgs } from 'calypso/lib/route';
 import { getActivePartnerKey } from 'calypso/state/partner-portal/partner/selectors';
-import { stringToLicenseFilter } from 'calypso/jetpack-cloud/sections/partner-portal/utils';
+import { valueToEnum } from 'calypso/jetpack-cloud/sections/partner-portal/utils';
 import Header from './header';
 import JetpackComFooter from 'calypso/jetpack-cloud/sections/pricing/jpcom-footer';
 import PartnerPortalSidebar from 'calypso/jetpack-cloud/sections/partner-portal/sidebar';
 import SelectPartnerKey from 'calypso/jetpack-cloud/sections/partner-portal/select-partner-key';
 import Licenses from 'calypso/jetpack-cloud/sections/partner-portal/primary/licenses';
+import {
+	LicenseFilter,
+	LicenseSortDirection,
+	LicenseSortField,
+} from 'calypso/jetpack-cloud/sections/partner-portal/types';
 
 export function partnerKeyContext( context: PageJS.Context, next: () => void ): void {
 	context.header = <Header />;
@@ -26,14 +31,28 @@ export function partnerKeyContext( context: PageJS.Context, next: () => void ): 
 }
 
 export function partnerPortalContext( context: PageJS.Context, next: () => void ): void {
-	const { s: search, sort_field: sortField, sort_direction: sortDirection } = context.query;
-	const licenseFilter = stringToLicenseFilter( context.params.state );
+	const { s: search, sort_field, sort_direction } = context.query;
+	const filter = valueToEnum< LicenseFilter >(
+		LicenseFilter,
+		context.params.state,
+		LicenseFilter.NotRevoked
+	);
+	const sortField = valueToEnum< LicenseSortField >(
+		LicenseSortField,
+		sort_field,
+		LicenseSortField.IssuedAt
+	);
+	const sortDirection = valueToEnum< LicenseSortDirection >(
+		LicenseSortDirection,
+		sort_direction,
+		LicenseSortDirection.Descending
+	);
 
 	context.header = <Header />;
 	context.secondary = <PartnerPortalSidebar path={ context.path } />;
 	context.primary = (
 		<Licenses
-			filter={ licenseFilter }
+			filter={ filter }
 			search={ search || '' }
 			sortDirection={ sortDirection }
 			sortField={ sortField }
