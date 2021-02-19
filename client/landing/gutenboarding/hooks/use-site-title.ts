025bc52eb26c457c581cdc382dea09eb8da6a9ee
Matthew Reishus <mreishus@users.noreply.github.com>
2021-02-19 10:26:12 -0600

    Make comment more clear

--- client/landing/gutenboarding/hooks/use-site-title.ts
+++ client/landing/gutenboarding/hooks/use-site-title.ts
@@ -19,9 +19,12 @@ export default function useSiteTitle(): void {
 		hasSiteTitle: select( ONBOARD_STORE ).getSelectedSiteTitle().length > 0,
 	} ) );
 
-	// When first loading a url including an ?anchor_podcast query param, clear the title
-	// This stops any pre-saved title in state from interfering, while retaining the ability
-	// to reload the browser in steps after aquire-intent and keep custom titles.
+	// When first loading a url including an ?anchor_podcast query param, clear
+	// the title.
+	// This allows the title retrieved from from backend to be applied, since
+	// we only load it over blank titles to prevent wiping out custom titles.
+	// This also retains the ability to keep custom titles when reloading the
+	// browser during steps after "acquire-intent".
 	const { search } = useLocation();
 	useEffect( () => {
 		const queryAnchorPodcast = new URLSearchParams( search ).get( 'anchor_podcast' );
