e42c633a9a54677e848f43530142136bed4b84db
Jessie Ross <jessie.ross@automattic.com>
2021-02-19 23:31:27 +0800

    Improve anonId

--- client/lib/explat/internals/anon-id.ts
+++ client/lib/explat/internals/anon-id.ts
@@ -9,30 +9,52 @@ import { logError, isDevelopmentMode } from './log-error';
 // Using typescript to ensure we are being safe in SSR
 declare const window: undefined | ( Window & typeof globalThis );
 
+const logErrorOrThrowInDevelopmentMode = ( message: string ) => {
+	if ( isDevelopmentMode ) {
+		throw new Error( message );
+	} else {
+		logError( { message } );
+	}
+};
+
+/**
+ * setInterval, but it runs first callback immediately instead of after interval.
+ *
+ * @param f The callback function
+ * @param intervalMs The interval in milliseconds
+ */
+const immediateStartSetInterval = ( f: () => void, intervalMs: number ) => {
+	f();
+	return setInterval( f, intervalMs );
+};
+
 let initializeAnonIdPromise: null | Promise< string | null > = null;
-const anonIdPollingIntervalMilliseconds = 50;
-const anonIdPollingIntervalMaxAttempts = 100; // 50 * 100 = 5000 = 5 seconds
+const anonIdPollingIntervalMilliseconds = 100;
+const anonIdPollingIntervalMaxAttempts = 50; // 100 * 50 = 5000 = 5 seconds
 /**
  * Initialized the anonId
  * - Polls for AnonId receival
  * - Should only be called once at startup
- * - Happens to be safe to call multiple times if it is necessary to reset it
+ * - Happens to be safe to call multiple times if it is necessary to reset the anonId - something like this was necessary for testing.
  *
  * The state of initializeAnonIdPromise should be used rather than the return of this function.
  * The return is avaliable to make this easier to test.
  *
  * Throws on error.
+ *
+ * TODO: More error checking?
+ * TODO: Better timing?
  */
 export const initializeAnonId = async (): Promise< string | null > => {
 	if ( typeof window === 'undefined' ) {
-		throw new Error( 'Trying to initialize anonId outside of a browser context.' );
+		logErrorOrThrowInDevelopmentMode( 'Trying to initialize anonId outside of a browser context.' );
 	}
 
 	recordTracksEvent( 'calypso_explat_initialization' );
 
 	let attempt = 0;
 	initializeAnonIdPromise = new Promise( ( res ) => {
-		const anonIdPollingInterval = setInterval( () => {
+		const anonIdPollingInterval = immediateStartSetInterval( () => {
 			if ( attempt > anonIdPollingIntervalMaxAttempts - 1 || userUtils.isLoggedIn() ) {
 				clearInterval( anonIdPollingInterval );
 				res( null );
@@ -58,13 +80,14 @@ export const initializeAnonId = async (): Promise< string | null > => {
  */
 export const getAnonId = async (): Promise< string | null > => {
 	if ( typeof window === 'undefined' ) {
-		logError( { message: 'Trying to getAnonId in non browser context.' } );
+		logErrorOrThrowInDevelopmentMode( 'Trying to getAnonId in non browser context.' );
 		return null;
 	}
 
-	// Check setup
-	if ( isDevelopmentMode && initializeAnonIdPromise === null ) {
-		throw new Error( 'AnonId initialization should have already started.' );
+	if ( initializeAnonIdPromise === null ) {
+		logErrorOrThrowInDevelopmentMode(
+			'AnonId initialization should have started before this function call.'
+		);
 	}
 
 	try {
