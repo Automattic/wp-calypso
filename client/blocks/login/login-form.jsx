1532e393f1f329803ed693788116d73f88e3bfd6
elliottprogrammer <bryan@elliottprogrammer.com>
2021-02-22 10:30:38 -0500

    Import shared function 'getSignupUrl()' and remove local 'this.getSignupUrl()' definition.

--- client/blocks/login/login-form.jsx
+++ client/blocks/login/login-form.jsx
@@ -6,7 +6,7 @@ import classNames from 'classnames';
 import page from 'page';
 import PropTypes from 'prop-types';
 import ReactDom from 'react-dom';
-import { capitalize, defer, includes, get } from 'lodash';
+import { capitalize, defer, includes, get, startsWith } from 'lodash';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import { addQueryArgs } from '@wordpress/url';
@@ -99,7 +99,16 @@ export class LoginForm extends Component {
 	}
 
 	componentDidUpdate( prevProps ) {
-		const { disableAutoFocus, requestError } = this.props;
+		const {
+			disableAutoFocus,
+			requestError,
+			currentQuery,
+			currentRoute,
+			oauth2Client,
+			locale,
+			pathname,
+			isGutenboarding,
+		} = this.props;
 
 		if ( prevProps.requestError || ! requestError ) {
 			return;
@@ -112,7 +121,15 @@ export class LoginForm extends Component {
 		if ( this.showUsernameError() ) {
 			! disableAutoFocus && defer( () => this.usernameOrEmail && this.usernameOrEmail.focus() );
 		} else {
-			page( addQueryArgs( this.getSignupUrl(), { email: this.state.usernameOrEmail } ) );
+			const signupUrl = getSignupUrl(
+				currentQuery,
+				currentRoute,
+				oauth2Client,
+				locale,
+				pathname,
+				isGutenboarding
+			);
+			page( addQueryArgs( signupUrl, { email: this.state.usernameOrEmail } ) );
 		}
 	}
 
@@ -176,63 +193,6 @@ export class LoginForm extends Component {
 		const { hasAccountTypeLoaded, socialAccountIsLinking } = this.props;
 
 		return ! socialAccountIsLinking && ! hasAccountTypeLoaded;
-	}
-
-	getSignupUrl() {
-		const {
-			oauth2Client,
-			redirectTo,
-			isGutenboarding,
-			currentRoute,
-			currentQuery,
-			pathname,
-			locale,
-		} = this.props;
-		const isOauthLogin = !! oauth2Client;
-		const langFragment = locale && locale !== 'en' ? `/${ locale }` : '';
-		const signupFlow = get( currentQuery, 'signup_flow' );
-		let signupUrl = config( 'signup_url' );
-
-		// copied from login-links.jsx
-		if (
-			// Match locales like `/log-in/jetpack/es`
-			startsWith( currentRoute, '/log-in/jetpack' )
-		) {
-			// Basic validation that we're in a valid Jetpack Authorization flow
-			if (
-				includes( get( currentQuery, 'redirect_to' ), '/jetpack/connect/authorize' ) &&
-				includes( get( currentQuery, 'redirect_to' ), '_wp_nonce' )
-			) {
-				/**
-				 * `log-in/jetpack/:locale` is reached as part of the Jetpack connection flow. In
-				 * this case, the redirect_to will handle signups as part of the flow. Use the
-				 * `redirect_to` parameter directly for signup.
-				 */
-				signupUrl = currentQuery.redirect_to;
-			} else {
-				signupUrl = '/jetpack/connect';
-			}
-		} else if ( '/jetpack-connect' === pathname ) {
-			signupUrl = '/jetpack/connect';
-		} else if ( signupFlow ) {
-			signupUrl += '/' + signupFlow;
-		}
-
-		if ( isOauthLogin && config.isEnabled( 'signup/wpcc' ) ) {
-			const oauth2Flow = isCrowdsignalOAuth2Client( oauth2Client ) ? 'crowdsignal' : 'wpcc';
-			const oauth2Params = new globalThis.URLSearchParams( {
-				oauth2_client_id: oauth2Client.id,
-				oauth2_redirect: redirectTo || '',
-			} );
-
-			signupUrl = `/start/${ oauth2Flow }?${ oauth2Params.toString() }`;
-		}
-
-		if ( isGutenboarding ) {
-			signupUrl = '/new' + langFragment;
-		}
-
-		return signupUrl;
 	}
 
 	showUsernameError() {
@@ -501,8 +461,13 @@ export class LoginForm extends Component {
 			requestError,
 			socialAccountIsLinking: linkingSocialUser,
 			isJetpackWooCommerceFlow,
+			isGutenboarding,
 			isJetpackWooDnaFlow,
 			wccomFrom,
+			currentRoute,
+			currentQuery,
+			pathname,
+			locale,
 		} = this.props;
 		const isOauthLogin = !! oauth2Client;
 		const isPasswordHidden = this.isUsernameOrEmailView();
