7b250953d99e935f84b128614ff6dc151ee213f3
Markus Dobmann <m.do@posteo.net>
2021-02-23 16:24:17 +0100

    people(users): use `useUsers` query to fetch data

--- client/my-sites/people/team-list/index.jsx
+++ client/my-sites/people/team-list/index.jsx
@@ -3,33 +3,66 @@
  */
 import { localize } from 'i18n-calypso';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { errorNotice, removeNotice } from 'calypso/state/notices/actions';
 
 /**
  * Internal dependencies
  */
-import SiteUsersFetcher from 'calypso/components/site-users-fetcher';
 import Team from './team';
+import useUsers from 'calypso/data/users/use-users';
 
-class TeamList extends React.Component {
-	static displayName = 'TeamList';
+function TeamList( props ) {
+	const dispatch = useDispatch();
+	const { site, search, translate } = props;
+	const fetchOptions = {
+		siteId: site?.ID,
+		order: 'ASC',
+		order_by: 'display_name',
+	};
 
-	render() {
-		const fetchOptions = {
-			siteId: this.props.site && this.props.site.ID,
-			order: 'ASC',
-			order_by: 'display_name',
-			search: this.props.search ? '*' + this.props.search + '*' : null,
-			search_columns: [ 'display_name', 'user_login' ],
-		};
-
-		Object.freeze( fetchOptions );
-
-		return (
-			<SiteUsersFetcher fetchOptions={ fetchOptions }>
-				<Team { ...this.props } />
-			</SiteUsersFetcher>
-		);
+	if ( search ) {
+		fetchOptions.search = `*${ search }*`;
+		fetchOptions.search_columns = [ 'display_name', 'user_login' ];
 	}
+
+	const {
+		data,
+		isLoading,
+		isFetchingNextPage,
+		hasNextPage,
+		fetchNextPage,
+		error,
+		refetch,
+	} = useUsers( fetchOptions );
+
+	React.useEffect( () => {
+		error &&
+			dispatch(
+				errorNotice( translate( 'There was an error retrieving users' ), {
+					id: 'site-users-notice',
+					button: 'Try again.',
+					onClick: () => {
+						dispatch( removeNotice( 'site-users-notice' ) );
+						refetch();
+					},
+				} )
+			);
+	}, [ dispatch, translate, refetch, error ] );
+
+	return (
+		<Team
+			fetchingUsers={ isLoading }
+			fetchingNextPage={ isFetchingNextPage }
+			totalUsers={ data?.total }
+			users={ data?.users ?? [] }
+			excludedUsers={ [] }
+			fetchOptions={ fetchOptions }
+			fetchNextPage={ fetchNextPage }
+			hasNextPage={ hasNextPage }
+			{ ...props }
+		/>
+	);
 }
 
 export default localize( TeamList );
