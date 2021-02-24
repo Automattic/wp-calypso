8e0226fe6694cc47038ac879ccfea92778049f07
Edwin Takahashi <egao@outlook.com>
2021-02-23 18:07:27 -0800

    Handle domain upsell when launching site. - look out for the domain upsell screen during the site launch process. - if upsell is detected, skip the upsell.

--- test/e2e/lib/flows/launch-site-flow.js
+++ test/e2e/lib/flows/launch-site-flow.js
@@ -1,9 +1,17 @@
 /**
+ * External dependencies
+ */
+
+import { By } from 'selenium-webdriver';
+
+/**
  * Internal dependencies
  */
 import FindADomainComponent from '../components/find-a-domain-component';
 import PickAPlanPage from '../pages/signup/pick-a-plan-page';
 import MyHomePage from '../pages/my-home-page';
+
+import * as driverHelper from '../driver-helper.js';
 
 export default class LaunchSiteFlow {
 	constructor( driver ) {
@@ -18,6 +26,15 @@ export default class LaunchSiteFlow {
 
 		const pickAPlanPage = await PickAPlanPage.Expect( this.driver );
 		await pickAPlanPage.selectFreePlan();
+
+		// Wait for navigation to take place from Plan Selection to domain upsell.
+		await this.driver.sleep( 1000 );
+
+		// Dismiss the domain upsell if present.
+		const dismissDomainUpsellSelector = By.css( ' .domain-upsell__continue-link button ' );
+		if ( driverHelper.isElementPresent( dismissDomainUpsellSelector ) ) {
+			return await driverHelper.clickWhenClickable( this.driver, dismissDomainUpsellSelector );
+		}
 
 		return await myHomePage.isSiteLaunched();
 	}
