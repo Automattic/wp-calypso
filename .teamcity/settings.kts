07baf33378110f7d811924988b96d5583b6ec454
Sergio Cinos <sergio.cinos@gmail.com>
2021-02-25 15:02:14 +0100

    ci(lint): Fail eslint build if the number of errors goes up (#50457)
    * Fail the build if the number of errors goes up
    
    * Fix imports
    
    * Fix imports

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -13,6 +13,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.githubConnec
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.failOnMetricChange
+import jetbrains.buildServer.configs.kotlin.v2019_2.failureConditions.BuildFailureOnMetric
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -701,6 +703,15 @@ object CheckCodeStyle : BuildType({
 
 	failureConditions {
 		executionTimeoutMin = 20
+		nonZeroExitCode = false
+		failOnMetricChange {
+			metric = BuildFailureOnMetric.MetricType.INSPECTION_ERROR_COUNT
+			units = BuildFailureOnMetric.MetricUnit.DEFAULT_UNIT
+			comparison = BuildFailureOnMetric.MetricComparison.MORE
+			compareTo = build {
+				buildRule = lastSuccessful()
+			}
+		}
 	}
 
 	features {
