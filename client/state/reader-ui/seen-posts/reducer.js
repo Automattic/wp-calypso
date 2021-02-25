f3be1bdd950c913228b290ae1ce5c02a724c9291
Jarda Snajdr <jsnajdr@gmail.com>
2021-02-25 10:05:19 +0100

    Reader UI state: remove watermarks reducer, cleanup serialization (#50374)

--- client/state/reader-ui/seen-posts/reducer.js
+++ client/state/reader-ui/seen-posts/reducer.js
@@ -1,7 +1,7 @@
 /**
  * Internal dependencies
  */
-import { SERIALIZE, READER_UNSEEN_STATUS_RECEIVE } from 'calypso/state/action-types';
+import { READER_UNSEEN_STATUS_RECEIVE } from 'calypso/state/action-types';
 
 /**
  * Reader Unseen status for any section (used global reader unseen bubble)
@@ -14,9 +14,6 @@ export default ( state = false, action ) => {
 	switch ( action.type ) {
 		case READER_UNSEEN_STATUS_RECEIVE:
 			return action.status;
-
-		case SERIALIZE:
-			return false;
 	}
 	return state;
 };
