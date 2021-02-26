73ca1258d0938ac7e13794640951c5eb18fa0b46
Dale du Preez <dale@automattic.com>
2021-02-26 13:34:33 +0200

    Add mailbox setup prompt to Titan nav menu

--- client/my-sites/email/email-management/titan-management-nav/index.jsx
+++ client/my-sites/email/email-management/titan-management-nav/index.jsx
@@ -9,7 +9,7 @@ import { connect } from 'react-redux';
 /**
  * Internal dependencies
  */
-import { Button } from '@automattic/components';
+import { Button, CompactCard } from '@automattic/components';
 import {
 	emailManagementManageTitanAccount,
 	emailManagementNewTitanAccount,
@@ -28,6 +28,8 @@ import titanMailIcon from 'calypso/assets/images/email-providers/titan/services/
 import VerticalNav from 'calypso/components/vertical-nav';
 import VerticalNavItem from 'calypso/components/vertical-nav/item';
 import { getTitanProductName } from 'calypso/lib/titan/get-titan-product-name';
+import { getConfiguredTitanMailboxCount, getMaxTitanMailboxCount } from 'calypso/lib/titan';
+import { TITAN_CONTROL_PANEL_CONTEXT_CREATE_EMAIL } from 'calypso/lib/titan/constants';
 
 /**
  * Style
@@ -37,6 +39,13 @@ import './style.scss';
 class TitanManagementNav extends React.Component {
 	static propTypes = {
 		domain: PropTypes.object.isRequired,
+
+		// Props injected via connect()
+		currentRoute: PropTypes.string.isRequired,
+		selectedSiteSlug: PropTypes.string.isRequired,
+
+		// Props injected via localize()
+		translate: PropTypes.func.isRequired,
 	};
 
 	renderTitanManagementLink = () => {
@@ -84,6 +93,59 @@ class TitanManagementNav extends React.Component {
 			>
 				{ translate( 'Update your billing and payment settings' ) }
 			</VerticalNavItem>
+		);
+	};
+
+	renderMailboxSetupPrompt = () => {
+		const { currentRoute, domain, selectedSiteSlug, translate } = this.props;
+
+		const numberOfUnusedMailboxes =
+			getMaxTitanMailboxCount( domain ) - getConfiguredTitanMailboxCount( domain );
+
+		if ( numberOfUnusedMailboxes <= 0 ) {
+			return;
+		}
+
+		const showExternalControlPanelLink = ! isEnabled( 'titan/iframe-control-panel' );
+		const controlPanelUrl = showExternalControlPanelLink
+			? emailManagementTitanControlPanelRedirect( selectedSiteSlug, domain.name, currentRoute, {
+					context: TITAN_CONTROL_PANEL_CONTEXT_CREATE_EMAIL,
+			  } )
+			: emailManagementManageTitanAccount( selectedSiteSlug, domain.name, currentRoute, {
+					context: TITAN_CONTROL_PANEL_CONTEXT_CREATE_EMAIL,
+			  } );
+
+		return (
+			<CompactCard className="titan-management-nav__mailbox-setup-prompt">
+				<span>
+					<Gridicon icon="info-outline" size={ 18 } />
+					<em>
+						{ translate(
+							'You have %(numberOfMailboxes)d unused mailbox',
+							'You have %(numberOfMailboxes)d unused mailboxes',
+							{
+								count: numberOfUnusedMailboxes,
+								args: {
+									numberOfMailboxes: numberOfUnusedMailboxes,
+								},
+								comment:
+									'This refers to the number of mailboxes purchased that have not been set up yet',
+							}
+						) }
+					</em>
+				</span>
+
+				{ /* eslint-disable wpcalypso/jsx-gridicon-size */ }
+				<Button
+					compact
+					href={ controlPanelUrl }
+					target={ showExternalControlPanelLink ? '_blank' : null }
+				>
+					{ translate( 'Finish Setup' ) }
+					{ showExternalControlPanelLink && <Gridicon icon="external" size={ 16 } /> }
+				</Button>
+				{ /* eslint-enable wpcalypso/jsx-gridicon-size */ }
+			</CompactCard>
 		);
 	};
 
@@ -158,6 +220,7 @@ class TitanManagementNav extends React.Component {
 				</FoldableCard>
 
 				<VerticalNav>
+					{ this.renderMailboxSetupPrompt() }
 					{ this.renderTitanManagementLink() }
 					{ this.renderPurchaseManagementLink() }
 				</VerticalNav>
