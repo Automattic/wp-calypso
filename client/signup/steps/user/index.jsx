1a9fff8b67120657185afaec7eca9f403ab1049c
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-02-26 11:55:48 +0530

    Remove flicker of old user step UI when the treatment loads the user step

--- client/signup/steps/user/index.jsx
+++ client/signup/steps/user/index.jsx
@@ -38,7 +38,7 @@ import config from '@automattic/calypso-config';
 import AsyncLoad from 'calypso/components/async-load';
 import WooCommerceConnectCartHeader from 'calypso/extensions/woocommerce/components/woocommerce-connect-cart-header';
 import { getSocialServiceFromClientId } from 'calypso/lib/login';
-import { getABTestVariation } from 'calypso/lib/abtest';
+import { isLoading } from 'calypso/state/experiments/selectors';
 import JetpackLogo from 'calypso/components/jetpack-logo';
 
 /**
@@ -416,7 +416,7 @@ export class UserStep extends Component {
 	}
 
 	renderSignupForm() {
-		const { oauth2Client, wccomFrom, flowName } = this.props;
+		const { oauth2Client, wccomFrom, isReskinned } = this.props;
 		let socialService;
 		let socialServiceResponse;
 		let isSocialSignupEnabled = this.props.isSocialSignupEnabled;
@@ -437,9 +437,6 @@ export class UserStep extends Component {
 		) {
 			isSocialSignupEnabled = true;
 		}
-
-		const isReskinned =
-			'onboarding' === flowName && 'reskinned' === getABTestVariation( 'reskinSignupFlow' );
 
 		return (
 			<>
@@ -466,6 +463,10 @@ export class UserStep extends Component {
 	}
 
 	render() {
+		if ( this.props.isLoadingExperiment ) {
+			return null;
+		}
+
 		return (
 			<StepWrapper
 				flowName={ this.props.flowName }
@@ -485,6 +486,7 @@ export default connect(
 		oauth2Client: getCurrentOAuth2Client( state ),
 		suggestedUsername: getSuggestedUsername( state ),
 		wccomFrom: get( getCurrentQueryArguments( state ), 'wccom-from' ),
+		isLoadingExperiment: isLoading( state ),
 	} ),
 	{
 		errorNotice,
