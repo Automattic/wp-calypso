97fbf9b8f5cd935bff958bc7450d1598fc58f094
mmtr <miguel.torres@automattic.com>
2021-03-01 12:42:44 +0100

    Remove "is Nav Unification enabled" check

--- client/me/sidebar/index.jsx
+++ client/me/sidebar/index.jsx
@@ -180,13 +180,11 @@ class MeSidebar extends React.Component {
 							preloadSectionName="privacy"
 						/>
 
-						{ config.isEnabled( 'nav-unification' ) && (
-							<SidebarItem
-								link={ 'https://dashboard.wordpress.com/wp-admin/index.php?page=my-blogs' }
-								label={ translate( 'Manage Blogs' ) }
-								materialIcon="apps"
-							/>
-						) }
+						<SidebarItem
+							link={ 'https://dashboard.wordpress.com/wp-admin/index.php?page=my-blogs' }
+							label={ translate( 'Manage Blogs' ) }
+							materialIcon="apps"
+						/>
 
 						<SidebarItem
 							selected={ selected === 'notifications' }
