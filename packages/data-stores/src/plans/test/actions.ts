01950cab160017d2134e4b8fabae0b6dfa0c2749
Marco Ciampini <marco.ciampini@automattic.com>
2021-03-02 19:16:27 +0100

    Action creators:
    - wrap each action creator into its own describe
    - rename "test" to "it"
    - write test name following BDD concepts (i.e. "should ...")

--- packages/data-stores/src/plans/test/actions.ts
+++ packages/data-stores/src/plans/test/actions.ts
@@ -8,57 +8,67 @@ import { buildPlanFeaturesDict } from '../test-utils';
 const MOCK_LOCALE = 'test-locale';
 
 describe( 'Plans action creators', () => {
-	test( 'setFeatures', () => {
-		const mockFeatures = buildPlanFeaturesDict( [
-			MockData.STORE_PLAN_FEATURE_CUSTOM_DOMAIN,
-			MockData.STORE_PLAN_FEATURE_LIVE_SUPPORT,
-			MockData.STORE_PLAN_FEATURE_WORDADS,
-		] );
+	describe( 'setFeatures', () => {
+		it( 'should create the correct action object', () => {
+			const mockFeatures = buildPlanFeaturesDict( [
+				MockData.STORE_PLAN_FEATURE_CUSTOM_DOMAIN,
+				MockData.STORE_PLAN_FEATURE_LIVE_SUPPORT,
+				MockData.STORE_PLAN_FEATURE_WORDADS,
+			] );
 
-		expect( Actions.setFeatures( mockFeatures, MOCK_LOCALE ) ).toEqual( {
-			type: 'SET_FEATURES',
-			features: mockFeatures,
-			locale: MOCK_LOCALE,
+			expect( Actions.setFeatures( mockFeatures, MOCK_LOCALE ) ).toEqual( {
+				type: 'SET_FEATURES',
+				features: mockFeatures,
+				locale: MOCK_LOCALE,
+			} );
 		} );
 	} );
 
-	test( 'setFeaturesByType', () => {
-		const features = [
-			MockData.API_FEATURES_BY_TYPE_GENERAL,
-			MockData.API_FEATURES_BY_TYPE_COMMERCE,
-			MockData.API_FEATURES_BY_TYPE_MARKETING,
-		];
-		expect( Actions.setFeaturesByType( features, MOCK_LOCALE ) ).toEqual( {
-			type: 'SET_FEATURES_BY_TYPE',
-			featuresByType: features,
-			locale: MOCK_LOCALE,
+	describe( 'setFeaturesByType', () => {
+		it( 'should create the correct action object', () => {
+			const features = [
+				MockData.API_FEATURES_BY_TYPE_GENERAL,
+				MockData.API_FEATURES_BY_TYPE_COMMERCE,
+				MockData.API_FEATURES_BY_TYPE_MARKETING,
+			];
+			expect( Actions.setFeaturesByType( features, MOCK_LOCALE ) ).toEqual( {
+				type: 'SET_FEATURES_BY_TYPE',
+				featuresByType: features,
+				locale: MOCK_LOCALE,
+			} );
 		} );
 	} );
 
-	test( 'setPlans', () => {
-		const plans = [ MockData.STORE_PLAN_FREE, MockData.STORE_PLAN_PREMIUM ];
-		expect( Actions.setPlans( plans, MOCK_LOCALE ) ).toEqual( {
-			type: 'SET_PLANS',
-			plans,
-			locale: MOCK_LOCALE,
+	describe( 'setPlans', () => {
+		it( 'should create the correct action object', () => {
+			const plans = [ MockData.STORE_PLAN_FREE, MockData.STORE_PLAN_PREMIUM ];
+			expect( Actions.setPlans( plans, MOCK_LOCALE ) ).toEqual( {
+				type: 'SET_PLANS',
+				plans,
+				locale: MOCK_LOCALE,
+			} );
 		} );
 	} );
 
-	test( 'setPlanProducts', () => {
-		const planProducts = [
-			MockData.STORE_PRODUCT_FREE,
-			MockData.STORE_PRODUCT_PREMIUM_ANNUALLY,
-			MockData.STORE_PRODUCT_PREMIUM_MONTHLY,
-		];
-		expect( Actions.setPlanProducts( planProducts ) ).toEqual( {
-			type: 'SET_PLAN_PRODUCTS',
-			products: planProducts,
+	describe( 'setPlanProducts', () => {
+		it( 'should create the correct action object', () => {
+			const planProducts = [
+				MockData.STORE_PRODUCT_FREE,
+				MockData.STORE_PRODUCT_PREMIUM_ANNUALLY,
+				MockData.STORE_PRODUCT_PREMIUM_MONTHLY,
+			];
+			expect( Actions.setPlanProducts( planProducts ) ).toEqual( {
+				type: 'SET_PLAN_PRODUCTS',
+				products: planProducts,
+			} );
 		} );
 	} );
 
-	test( 'resetPlan', () => {
-		expect( Actions.resetPlan() ).toEqual( {
-			type: 'RESET_PLAN',
+	describe( 'resetPlan', () => {
+		it( 'should create the correct action object', () => {
+			expect( Actions.resetPlan() ).toEqual( {
+				type: 'RESET_PLAN',
+			} );
 		} );
 	} );
 } );
