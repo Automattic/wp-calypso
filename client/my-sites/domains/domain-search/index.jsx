38d7714e2f2b4142ef3b0ad227b9c02146b81cc6
niranjan-uma-shankar <niranjan.u@gmail.com>
2021-03-02 10:25:43 +0530

    Signup domain step fixes: Blank screen and UI fix (#50329)
    * Fix UI bug with clearing icon in JA locale
    
    * Fix bug that causes add new site to blank out

--- client/my-sites/domains/domain-search/index.jsx
+++ client/my-sites/domains/domain-search/index.jsx
@@ -207,6 +207,11 @@ class DomainSearch extends Component {
 
 	render() {
 		const { selectedSite, selectedSiteSlug, translate, isManagingAllDomains } = this.props;
+
+		if ( ! selectedSite ) {
+			return null;
+		}
+
 		const classes = classnames( 'main-column', {
 			'domain-search-page-wrapper': this.state.domainRegistrationAvailable,
 		} );
