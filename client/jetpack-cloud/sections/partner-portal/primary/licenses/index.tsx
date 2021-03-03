3e1eecfb787fe2ec8e55f78652d6c28e2e1498ed
Atanas Angelov <hi@atanas.dev>
2021-03-03 21:07:34 +0200

    Partner Portal: Add a quick and dirty way for users to switch between keys

--- client/jetpack-cloud/sections/partner-portal/primary/licenses/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/primary/licenses/index.tsx
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
 
 /**
@@ -17,6 +18,17 @@ import {
 	LicenseSortField,
 } from 'calypso/jetpack-cloud/sections/partner-portal/types';
 import LicenseStateFilter from 'calypso/jetpack-cloud/sections/partner-portal/license-state-filter';
+import SelectDropdown from 'calypso/components/select-dropdown';
+import {
+	getActivePartnerKeyId,
+	getCurrentPartner,
+} from 'calypso/state/partner-portal/partner/selectors';
+import { setActivePartnerKey } from 'calypso/state/partner-portal/partner/actions';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
 
 interface Props {
 	filter: LicenseFilter;
@@ -34,12 +46,37 @@ export default function Licenses( {
 	sortField,
 }: Props ): ReactElement {
 	const translate = useTranslate();
+	const dispatch = useDispatch();
+	const partner = useSelector( getCurrentPartner );
+	const activeKeyId = useSelector( getActivePartnerKeyId );
+	const onKeySelect = useCallback( ( option ) => {
+		dispatch( setActivePartnerKey( parseInt( option.value ) ) );
+	}, [] );
+
+	const options =
+		partner &&
+		partner.keys.map( ( key ) => ( {
+			value: key.id.toString(),
+			label: key.name,
+		} ) );
+
+	options?.unshift( { label: translate( 'Partner Key' ) as string, value: '', isLabel: true } );
 
 	return (
 		<Main wideLayout={ true } className="licenses">
 			<DocumentHead title={ translate( 'Licenses' ) } />
 
-			<CardHeading size={ 36 }>{ translate( 'Licenses' ) }</CardHeading>
+			<div className="licenses__header">
+				<CardHeading size={ 36 }>{ translate( 'Licenses' ) }</CardHeading>
+				{ options && options.length > 2 && (
+					<SelectDropdown
+						initialSelected={ activeKeyId.toString() }
+						options={ options }
+						onSelect={ onKeySelect }
+						compact
+					/>
+				) }
+			</div>
 
 			<LicenseStateFilter filter={ filter } search={ search } />
 
