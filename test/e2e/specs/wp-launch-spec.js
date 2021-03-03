f0f7fa77340dddbe310f0c70608bbc2c65590597
Edwin Takahashi <egao@outlook.com>
2021-03-02 18:07:31 -0800

    Refactor the multiple site launch test.

--- test/e2e/specs/wp-launch-spec.js
+++ test/e2e/specs/wp-launch-spec.js
@@ -9,35 +9,18 @@ import config from 'config';
 import * as driverManager from '../lib/driver-manager.js';
 import * as dataHelper from '../lib/data-helper.js';
 
-import SignUpFlow from '../lib/flows/sign-up-flow.js';
 import LoginFlow from '../lib/flows/login-flow.js';
 import CreateSiteFlow from '../lib/flows/create-site-flow.js';
 import LaunchSiteFlow from '../lib/flows/launch-site-flow.js';
-import DeleteAccountFlow from '../lib/flows/delete-account-flow.js';
 import DeleteSiteFlow from '../lib/flows/delete-site-flow.js';
 import SidebarComponent from '../lib/components/sidebar-component';
-import FindADomainComponent from '../lib/components/find-a-domain-component.js';
-import MyHomePage from '../lib/pages/my-home-page.js';
 
 const host = dataHelper.getJetpackHost();
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
 const screenSize = driverManager.currentScreenSize();
-const signupInboxId = config.get( 'signupInboxId' );
 
 let driver;
-const createAndActivateAccount = async function ( accountName ) {
-	const emailAddress = dataHelper.getEmailAddress( accountName, signupInboxId );
-	const password = config.get( 'passwordForNewTestSignUps' );
-
-	const signupFlow = new SignUpFlow( driver, {
-		accountName,
-		emailAddress: emailAddress,
-		password: password,
-	} );
-	await signupFlow.signupFreeAccount();
-	await signupFlow.activateAccount();
-};
 
 before( async function () {
 	this.timeout( startBrowserTimeoutMS );
@@ -73,26 +56,29 @@ describe( `[${ host }] Launch (${ screenSize }) @signup @parallel`, function () 
 	// https://github.com/Automattic/wp-calypso/issues/50547
 	// Potential issue that trigger this failure:
 	// https://github.com/Automattic/wp-calypso/issues/50273
-	describe.skip( 'Launch when having multiple sites', function () {
-		const accountName = dataHelper.getNewBlogName();
+	describe.only( 'Launch when having multiple sites', function () {
 		const firstSiteName = dataHelper.getNewBlogName();
 		const secondSiteName = dataHelper.getNewBlogName();
 
-		before( 'Create 2 free sites as a new user', async function () {
-			await createAndActivateAccount( accountName );
-			await new CreateSiteFlow( driver, firstSiteName ).createFreeSite();
-			await new CreateSiteFlow( driver, secondSiteName ).createFreeSite();
+		before( 'Can log in', async function () {
+			const loginFlow = new LoginFlow( driver );
+			await loginFlow.login();
 		} );
 
-		step( 'Can start launch flow and abandon', async function () {
-			const myHomePage = await MyHomePage.Expect( driver );
-			await myHomePage.launchSiteFromSiteSetup();
-			await FindADomainComponent.Expect( driver );
-			// force reloading the page from the server to simulate an expired cache
-			await driver.executeScript( 'window.location.reload(true);' );
-			await FindADomainComponent.Expect( driver );
-			return await driver.navigate().back();
+		step( 'Can create free sites', async function () {
+			await new CreateSiteFlow( driver, firstSiteName ).createFreeSite();
+			return await new CreateSiteFlow( driver, secondSiteName ).createFreeSite();
 		} );
+
+		// step( 'Can start launch flow and abandon', async function () {
+		// 	const myHomePage = await MyHomePage.Expect( driver );
+		// 	await myHomePage.launchSiteFromSiteSetup();
+		// 	await FindADomainComponent.Expect( driver );
+		// 	// force reloading the page from the server to simulate an expired cache
+		// 	await driver.executeScript( 'window.location.reload(true);' );
+		// 	await FindADomainComponent.Expect( driver );
+		// 	return await driver.navigate().back();
+		// } );
 
 		step( 'Can switch sites and launch the first site', async function () {
 			const sideBarComponent = await SidebarComponent.Expect( driver );
@@ -105,8 +91,21 @@ describe( `[${ host }] Launch (${ screenSize }) @signup @parallel`, function () 
 			return await new LaunchSiteFlow( driver ).launchFreeSite();
 		} );
 
-		after( 'Delete the newly created account', async function () {
-			return await new DeleteAccountFlow( driver ).deleteAccount( accountName );
+		step( 'Can switch sites and launch the second site', async function () {
+			const sideBarComponent = await SidebarComponent.Expect( driver );
+			await sideBarComponent.selectSiteSwitcher();
+			await sideBarComponent.searchForSite( secondSiteName );
+			if ( driverManager.currentScreenSize() === 'mobile' ) {
+				await sideBarComponent.ensureSidebarMenuVisible();
+				await sideBarComponent.selectMyHome();
+			}
+			return await new LaunchSiteFlow( driver ).launchFreeSite();
+		} );
+
+		after( 'Delete the newly created site', async function () {
+			const deleteSite = new DeleteSiteFlow( driver );
+			await deleteSite.deleteSite( firstSiteName + '.wordpress.com' );
+			return await deleteSite.deleteSite( secondSiteName + '.wordpress.com' );
 		} );
 	} );
 } );
