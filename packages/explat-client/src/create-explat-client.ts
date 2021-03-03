322e36076ba1459c9fa81cf1e165443b80bc9492
Jessie Ross <jessie.ross@automattic.com>
2021-03-03 22:27:18 +0800

    Fix MissingExperimentAssignmentError

--- packages/explat-client/src/create-explat-client.ts
+++ packages/explat-client/src/create-explat-client.ts
@@ -43,9 +43,8 @@ export interface ExPlatClient {
 }
 
 export class MissingExperimentAssignmentError extends Error {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	constructor( ...params: any[] ) {
-		super( ...params );
+	constructor( message?: string ) {
+		super( message );
 
 		// Maintains proper stack trace for where our error was thrown (only available on V8)
 		if ( Error.captureStackTrace ) {
