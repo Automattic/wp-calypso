3371311074e7ddbdeefefb493229899d0605a753
Kevin Zoschke <kevin.zoschke@automattic.com>
2021-03-03 13:53:33 -0500

    Compute Jetpack Free button props in hook

--- client/components/jetpack/card/jetpack-free-card/button.tsx
+++ client/components/jetpack/card/jetpack-free-card/button.tsx
@@ -1,29 +1,21 @@
 /**
  * External dependencies
  */
-import React, { FC } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
 import { TranslateResult, useTranslate } from 'i18n-calypso';
 import { Button } from '@automattic/components';
 
 /**
  * Internal dependencies
  */
-import { storePlan } from 'calypso/jetpack-connect/persistence-utils';
-import { PLAN_JETPACK_FREE } from 'calypso/lib/plans/constants';
-import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
-import useTrackCallback from 'calypso/lib/jetpack/use-track-callback';
-import { addQueryArgs } from 'calypso/lib/route';
-import { getUrlParts, getUrlFromParts } from 'calypso/lib/url';
-import { JPC_PATH_BASE } from 'calypso/jetpack-connect/constants';
-import getJetpackWpAdminUrl from 'calypso/state/selectors/get-jetpack-wp-admin-url';
+import useJetpackFreeButtonProps from './use-jetpack-free-button-props';
 
 /**
  * Type dependencies
  */
 import type { QueryArgs } from 'calypso/my-sites/plans/jetpack-plans/types';
 
-interface JetpackFreeCardButtonProps {
+interface Props {
 	className?: string;
 	label?: TranslateResult;
 	primary?: boolean;
@@ -31,7 +23,7 @@ interface JetpackFreeCardButtonProps {
 	urlQueryArgs: QueryArgs;
 }
 
-const JetpackFreeCardButton: FC< JetpackFreeCardButtonProps > = ( {
+const JetpackFreeCardButton: React.FC< Props > = ( {
 	className,
 	label,
 	primary = false,
@@ -39,57 +31,10 @@ const JetpackFreeCardButton: FC< JetpackFreeCardButtonProps > = ( {
 	urlQueryArgs,
 } ) => {
 	const translate = useTranslate();
-	const wpAdminUrl = useSelector( getJetpackWpAdminUrl );
-	const trackCallback = useTrackCallback( undefined, 'calypso_product_jpfree_click', {
-		site_id: siteId || undefined,
-	} );
-	const { site } = urlQueryArgs;
-
-	// If the user is not logged in and there is a site in the URL, we need to construct
-	// the URL to wp-admin from the `site` query parameter
-	let jetpackAdminUrlFromQuery;
-
-	if ( site ) {
-		let wpAdminUrlFromQuery;
-
-		// Ensure that URL is valid
-		try {
-			// Slugs of secondary sites of a multisites network follow this syntax: example.net::second-site
-			wpAdminUrlFromQuery = new URL(
-				`https://${ site.replaceAll( '::', '/' ) }/wp-admin/admin.php`
-			);
-		} catch ( e ) {}
-
-		if ( wpAdminUrlFromQuery ) {
-			jetpackAdminUrlFromQuery = getUrlFromParts( {
-				...getUrlParts( wpAdminUrlFromQuery.href ),
-				search: '?page=jetpack',
-				hash: '/my-plan',
-			} ).href;
-		}
-	}
-
-	const onClickTrack = () => {
-		storePlan( PLAN_JETPACK_FREE );
-		trackCallback();
-	};
-
-	// `siteId` is going to be null if the user is not logged in, so we need to check
-	// if there is a site in the URL also
-	const isSiteinContext = siteId || site;
-
-	// Jetpack Connect flow uses `url` instead of `site` as the query parameter for a site URL
-	const { site: url, ...restQueryArgs } = urlQueryArgs;
-	const startHref =
-		isJetpackCloud() && ! isSiteinContext
-			? addQueryArgs(
-					{ url, ...restQueryArgs, plan: PLAN_JETPACK_FREE },
-					`https://wordpress.com${ JPC_PATH_BASE }`
-			  )
-			: wpAdminUrl || jetpackAdminUrlFromQuery || JPC_PATH_BASE;
+	const props = useJetpackFreeButtonProps( siteId, urlQueryArgs );
 
 	return (
-		<Button primary={ primary } className={ className } href={ startHref } onClick={ onClickTrack }>
+		<Button primary={ primary } className={ className } { ...props }>
 			{ label || translate( 'Start for free' ) }
 		</Button>
 	);
