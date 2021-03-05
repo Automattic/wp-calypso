7fb7f7a47fe962aab40f4a4234cab3a5de8faf00
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-03-05 12:13:50 -0600

    Plans: Implement money-back guarantee banner in HTML+SVG (#50797)
    * Plans: Implement money-back guarantee banner in HTML+SVG
    
    * Adjust banner bottom margin on large screens
    
    * Adjust font sizes for mobile vs desktop screens
    
    * Adjust badge size for mobile vs desktop screens
    
    * Remove side borders and radii for mobile screens

--- client/components/jetpack/intro-pricing-banner/images/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Internal dependencies
- */
-export * as Desktop from './desktop';
-export * as Desktop2x from './desktop-2x';
-export * as Mobile from './mobile';
-export * as Mobile2x from './mobile-2x';
