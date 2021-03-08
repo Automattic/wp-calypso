76a42700ae9c71acca409448efe199e1a56e6ed5
cpap <papazoglou.charalampos@gmail.com>
2021-03-08 19:24:36 +0200

    Excludes test users.

--- client/state/selectors/is-nav-unification-enabled.js
+++ client/state/selectors/is-nav-unification-enabled.js
@@ -16,6 +16,33 @@ import isNavUnificationNewUser from 'calypso/state/selectors/is-nav-unification-
 
 // Gradual rollout (segment of existing users + all new users registered after March 5, 2021).
 const CURRENT_ROLLOUT_SEGMENT_PERCENTAGE = 5;
+// Test users found in test/e2e/config/local-decrypted.json.
+const TEST_USER_IDS = [
+	114387625,
+	196072882,
+	147903583,
+	176665678,
+	191685058,
+	99044787,
+	115575915,
+	125513142,
+	140566456,
+	139474794,
+	139474794,
+	102365391,
+	106276422,
+	106276977,
+	107524077,
+	107524337,
+	107524381,
+	107524493,
+	107524451,
+	107523085,
+	127372741,
+	123892165,
+	126716843,
+	132724203,
+];
 
 export default ( state ) => {
 	// Disable if explicitly requested by the `?disable-nav-unification` query param.
@@ -29,8 +56,13 @@ export default ( state ) => {
 		return false;
 	}
 
-	// Users belonging to the current segment OR New Users.
 	const userId = getCurrentUserId( state );
+	// Disable for Test Users.
+	if ( TEST_USER_IDS.includes( userId ) ) {
+		return false;
+	}
+
+	// Users belonging to the current segment OR New Users.
 	if ( userId % 100 < CURRENT_ROLLOUT_SEGMENT_PERCENTAGE || isNavUnificationNewUser( state ) ) {
 		return true;
 	}
