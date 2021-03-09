e79a2b50dd4283a91f2687ad2ca684be2baf1ed3
Edwin Takahashi <egao@outlook.com>
2021-03-08 17:27:10 -0800

    Modify demo test to: - run in headful mode - load the login page - set the default timeout

--- test/e2e/specs/wp-log-in-out-spec-playwright.js
+++ test/e2e/specs/wp-log-in-out-spec-playwright.js
@@ -1,11 +1,8 @@
 /**
  * External dependencies
  */
-import assert from 'assert';
-import { get } from 'lodash';
-import speakeasy from 'speakeasy';
-import config from 'config';
 import playwright from 'playwright';
+import config from 'config';
 
 /**
  * External dependencies
@@ -13,31 +10,22 @@ import playwright from 'playwright';
 import * as driverManager from '../lib/driver-manager.js';
 import * as dataHelper from '../lib/data-helper';
 
-import EmailClient from '../lib/email-client.js';
-import { listenForSMS } from '../lib/xmpp-client';
-import { subscribeToPush, approvePushToken } from '../lib/push-client';
-import ReaderPage from '../lib/pages/reader-page';
-import ProfilePage from '../lib/pages/profile-page';
-import WPHomePage from '../lib/pages/wp-home-page';
-import MagicLoginPage from '../lib/pages/magic-login-page';
-
-import NavBarComponent from '../lib/components/nav-bar-component.js';
-import LoggedOutMasterbarComponent from '../lib/components/logged-out-masterbar-component';
-
-import LoginFlow from '../lib/flows/login-flow';
 import LoginPage from '../lib/pages/login-page';
-import WPAdminLogonPage from '../lib/pages/wp-admin/wp-admin-logon-page.js';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
-const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
 const screenSize = driverManager.currentScreenSize();
 const host = dataHelper.getJetpackHost();
 
 describe( `[${ host }] Playwright Canary: (${ screenSize }) @playwright @parallel`, function () {
+	this.timeout( mochaTimeOut );
+
 	describe( 'Loading the log-in screen using Playwright', function () {
 		step( 'Can see the log in screen', async function () {
-			process.env.PLAYWRIGHT_BROWSERS_PATH = '~/workspace';
-			const browser = await playwright.chromium.launch();
+			const browser = await playwright.chromium.launch( { headless: false } );
+			const url = await LoginPage.getLoginURL();
+			const browserContext = await browser.newContext();
+			const page = await browserContext.newPage();
+			await page.goto( url, { waitUntill: 'networkidle' } );
 			await browser.close();
 		} );
 	} );
