ce156af0b5f3bdcd232c4294563d72fc730a97f0
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-03-09 13:55:58 -0600

    Show social proof header (if appropriate) on Jetpack plan/pricing pages

--- client/jetpack-connect/store-header.tsx
+++ client/jetpack-connect/store-header.tsx
@@ -15,6 +15,7 @@ import DocumentHead from 'calypso/components/data/document-head';
 import getPartnerSlugFromQuery from 'calypso/state/selectors/get-partner-slug-from-query';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import IntroPricingBanner from 'calypso/components/jetpack/intro-pricing-banner';
+import useMaybeSocialProofHeader from 'calypso/my-sites/plans/jetpack-plans/use-maybe-social-proof-header';
 
 import './style.scss';
 
@@ -30,6 +31,8 @@ export default function StoreHeader(): React.ReactElement {
 		'add-bottom-margin': ! isStoreLanding,
 	} );
 
+	const headerText = useMaybeSocialProofHeader() ?? translate( 'Explore our Jetpack plans' );
+
 	return (
 		<>
 			<DocumentHead title={ translate( 'Jetpack Connect' ) } />
@@ -43,7 +46,7 @@ export default function StoreHeader(): React.ReactElement {
 			</div>
 			{ isStoreLanding && (
 				<FormattedHeader
-					headerText={ translate( 'Explore our Jetpack plans' ) }
+					headerText={ headerText }
 					subHeaderText={ translate( 'Pick a plan that fits your needs.' ) }
 					brandFont
 				/>
