1d0e803cad92fab80fa829c88e73924095717057
cpap <papazoglou.charalampos@gmail.com>
2021-03-10 09:58:12 +0200

    Updates new user id.

--- client/state/selectors/is-nav-unification-new-user.jsx
+++ client/state/selectors/is-nav-unification-new-user.jsx
@@ -3,7 +3,7 @@
  */
 import { getCurrentUserId } from 'calypso/state/current-user/selectors';
 
-const NEW_USER_ID_THRESHOLD = 202731080; // ID of user who first registered on March 5, 2021.
+const NEW_USER_ID_THRESHOLD = 203012733; // ID of user who first registered on March 5, 2021.
 
 export default ( state ) => {
 	const userId = getCurrentUserId( state );
