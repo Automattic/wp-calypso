8676c643fced1c06dc9ee634ca43582666af231b
Marco Ciampini <marco.ciampini@automattic.com>
2021-03-11 15:36:32 +0100

    Mock useDomainSuggestions at the start of the file, and use TypeScript types for stricter checking

--- packages/domain-picker/test/search-and-select-domain-suggestion.tsx
+++ packages/domain-picker/test/search-and-select-domain-suggestion.tsx
@@ -4,35 +4,37 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import { DataStatus } from '@automattic/data-stores/src/domain-suggestions/constants';
+import type { DomainSuggestion } from '@automattic/data-stores/src/domain-suggestions/types';
 
 /**
  * Internal dependencies
  */
 import '../matchMedia.mock';
 import DomainPicker from '../src/domain-picker';
+import { useDomainSuggestions } from '../src/hooks/use-domain-suggestions';
 
+// TODO: should we extract to separate mock data file/folder?
+const MOCK_SUGGESTION: DomainSuggestion = {
+	domain_name: 'example.com',
+	cost: '€15.00',
+	raw_price: 15,
+	currency_code: 'EUR',
+	relevance: 0.5,
+};
 
-import { useDomainSuggestions } from '../src/hooks';
-jest.mock( '../src/hooks/use-domain-suggestions' );
+const mockUseDomainSuggestionsResult: ReturnType< typeof useDomainSuggestions > = {
+	allDomainSuggestions: [ MOCK_SUGGESTION ],
+	errorMessage: null,
+	state: DataStatus.Success,
+	retryRequest: jest.fn(),
+};
+
+jest.mock( '../src/hooks/use-domain-suggestions', () => ( {
+	useDomainSuggestions: () => mockUseDomainSuggestionsResult,
+} ) );
+
 
 describe( 'Search for a domain and select a suggestion', () => {
-	beforeAll( () => {
-		useDomainSuggestions.mockReturnValue( {
-			allDomainSuggestions: [
-				{
-					domain_name: 'example.com',
-					cost: '€15.00',
-					raw_price: 15,
-					currency_code: 'USD',
-					relevance: 0.5,
-				},
-			],
-			errorMessage: null,
-			state: DataStatus.Success,
-			retryRequest: jest.fn(),
-		} );
-	} );
-
 	it( 'should search for a domain', () => {
 		const { debug } = render(
 			<DomainPicker
