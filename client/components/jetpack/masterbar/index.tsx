a995e5dda5bb9292828f642c0f8b684f5357256f
Kevin Zoschke <kevin.zoschke@automattic.com>
2021-03-11 08:43:27 -0500

    Implement accessible profile dropdown

--- client/components/jetpack/masterbar/index.tsx
+++ client/components/jetpack/masterbar/index.tsx
@@ -3,7 +3,6 @@
  */
 import React from 'react';
 import { useTranslate } from 'i18n-calypso';
-import classnames from 'classnames';
 
 /**
  * Internal dependencies
@@ -16,7 +15,6 @@ import JetpackLogo from 'calypso/components/jetpack-logo';
 import Masterbar from 'calypso/layout/masterbar/masterbar';
 import ProfileDropdown from 'calypso/components/jetpack/profile-dropdown';
 import { useBreakpoint } from '@automattic/viewport-react';
-import useTrackCallback from 'calypso/lib/jetpack/use-track-callback';
 import AsyncLoad from 'calypso/components/async-load';
 
 /**
@@ -24,26 +22,12 @@ import AsyncLoad from 'calypso/components/async-load';
  */
 import './style.scss';
 
-const useOpenClose = () => {
-	const [ isOpen, setIsOpen ] = React.useState( false );
-	const close = React.useCallback( () => {
-		setIsOpen( false );
-	}, [ setIsOpen ] );
-	const toggle = React.useCallback( () => {
-		setIsOpen( ! isOpen );
-	}, [ isOpen, setIsOpen ] );
-	return { isOpen, close, toggle };
-};
-
-const JetpackCloudMasterBar = () => {
+const JetpackCloudMasterBar: React.FC = () => {
 	const translate = useTranslate();
 	const headerTitle = useSelector( getDocumentHeadTitle );
 	const currentRoute = useSelector( getCurrentRoute );
 	const isNarrow = useBreakpoint( '<660px' );
 	const isExteriorPage = /^\/(?:backup|scan)\/[^/]*$/.test( currentRoute );
-	const { isOpen, close, toggle } = useOpenClose();
-	const trackedToggle = useTrackCallback( toggle, 'calypso_jetpack_masterbar_profile_toggle' );
-	const trackedClose = useTrackCallback( close, 'calypso_jetpack_masterbar_profile_close' );
 
 	return (
 		<Masterbar
@@ -60,18 +44,7 @@ const JetpackCloudMasterBar = () => {
 			</Item>
 			<AsyncLoad require="calypso/components/jetpack/portal-nav" placeholder={ null } />
 			{ headerTitle && <h1 className="masterbar__item-title">{ headerTitle }</h1> }
-			<Item
-				tipTarget="me"
-				url="#" // @todo: add a correct URL
-				onClick={ trackedToggle }
-				icon="user-circle"
-				className={ classnames( 'masterbar__item-me', {
-					'masterbar__item-me--open': isOpen,
-				} ) }
-				tooltip={ translate( 'Log out of Jetpack Cloud' ) }
-			>
-				<ProfileDropdown isOpen={ isOpen } close={ trackedClose } />
-			</Item>
+			<ProfileDropdown />
 		</Masterbar>
 	);
 };
