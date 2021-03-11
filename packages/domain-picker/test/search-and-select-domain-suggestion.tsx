db00b3b40e2432cd15a2d9c5ecb4a9778d1d701d
Stefan Nieuwenhuis <stefan.nieuwenhuis@automattic.com>
2021-03-11 08:03:03 +0100

    Add tests for searching and selecting domain suggestions

---
+++ packages/domain-picker/test/search-and-select-domain-suggestion.tsx
@@ -0,0 +1,60 @@
+/**
+ * External dependencies
+ */
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import DomainPicker from '../src/domain-picker';
+import { DataStatus } from '@automattic/data-stores/src/domain-suggestions/constants';
+
+Object.defineProperty( window, 'matchMedia', {
+	writable: true,
+	value: jest.fn().mockImplementation( ( query ) => ( {
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(), // deprecated
+		removeListener: jest.fn(), // deprecated
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn(),
+		dispatchEvent: jest.fn(),
+	} ) ),
+} );
+
+import { useDomainSuggestions } from '../src/hooks';
+jest.mock( '../src/hooks/use-domain-suggestions' );
+
+describe( 'Search for a domain and select a suggestion', () => {
+	beforeAll( () => {
+		useDomainSuggestions.mockReturnValue( {
+			allDomainSuggestions: [
+				{
+					domain_name: 'example.com',
+					cost: '€15.00',
+					raw_price: 15,
+					currency_code: 'USD',
+					relevance: 0.5,
+				},
+			],
+			errorMessage: null,
+			state: DataStatus.Success,
+			retryRequest: jest.fn(),
+		} );
+	} );
+
+	it( 'should search for a domain', () => {
+		const { debug } = render(
+			<DomainPicker
+				onDomainSelect={ jest.fn() }
+				analyticsUiAlgo="testalgo"
+				analyticsFlowId="12345"
+				initialDomainSearch="water"
+			/>
+		);
+		debug();
+		expect( screen.getByText( '€15.00/year' ) ).toBeTruthy();
+	} );
+} );
