dfa111da9553161b02804777f61c2f4d67f9780f
sixhours <calobee@gmail.com>
2021-03-12 15:54:04 -0500

    Change the subHeaderText for different filtered sections of People

--- client/my-sites/people/main.jsx
+++ client/my-sites/people/main.jsx
@@ -55,6 +55,21 @@ class People extends React.Component {
 		}
 	}
 
+	renderSubheaderText() {
+		const { translate, filter } = this.props;
+
+		switch ( filter ) {
+			case 'followers':
+				return translate(
+					'People who have subscribed to your site using their WordPress.com account.'
+				);
+			case 'email-followers':
+				return translate( 'People who have subscribed to your site using their email address.' );
+			default:
+				return translate( 'Invite contributors to your site and manage their access settings.' );
+		}
+	}
+
 	render() {
 		const {
 			isComingSoon,
@@ -94,9 +109,7 @@ class People extends React.Component {
 					brandFont
 					className="people__page-heading"
 					headerText={ translate( 'People' ) }
-					subHeaderText={ translate(
-						'Invite contributors to your site and manage their access settings.'
-					) }
+					subHeaderText={ this.renderSubheaderText() }
 					align="left"
 				/>
 				<div>
