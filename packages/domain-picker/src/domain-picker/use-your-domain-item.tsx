bb89794e5fc4377e1a7568627dbb1a32179e1e7c
Marco Ciampini <marco.ciampini@automattic.com>
2021-03-15 19:01:30 +0100

    Domain picker: stop click event propagation on the "Use a domain I own" button

--- packages/domain-picker/src/domain-picker/use-your-domain-item.tsx
+++ packages/domain-picker/src/domain-picker/use-your-domain-item.tsx
@@ -17,6 +17,16 @@ interface Props {
 const UseYourDomainItem: React.FunctionComponent< Props > = ( { onClick } ) => {
 	const { __, _x } = useI18n();
 
+	// Stopping propagation to prevent the `click` event from firing twice when
+	// clicking the button (once on the button, once on the wrapping component)
+	const onClickWithNoPropagation = React.useCallback(
+		( e: React.MouseEvent< HTMLButtonElement, MouseEvent > ) => {
+			onClick();
+			e.stopPropagation();
+		},
+		[ onClick ]
+	);
+
 	return (
 		<WrappingComponent
 			type="button"
@@ -37,7 +47,7 @@ const UseYourDomainItem: React.FunctionComponent< Props > = ( { onClick } ) => {
 					</span>
 				</div>
 			</div>
-			<ArrowButton arrow="right" onClick={ onClick }>
+			<ArrowButton arrow="right" onClick={ onClickWithNoPropagation }>
 				{ _x(
 					'Use a domain I own',
 					'Domain transfer or mapping suggestion button',
