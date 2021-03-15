05df00dad50c135dbf9e95bffc66a19a343b04e3
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2021-03-15 17:05:52 +0200

    i18n: Fix quick language switcher for unified nav

--- client/layout/masterbar/quick-language-switcher.jsx
+++ client/layout/masterbar/quick-language-switcher.jsx
@@ -1,12 +1,15 @@
 /**
  * External dependencies
  */
-import React, { Fragment, useReducer } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
 import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
+import { requestAdminMenu } from 'calypso/state/admin-menu/actions';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import config from '@automattic/calypso-config';
 import MasterbarItem from './item';
 import LanguagePickerModal from 'calypso/components/language-picker/modal';
@@ -26,6 +29,11 @@ function QuickLanguageSwitcher( props ) {
 		);
 		toggleLanguageEmpathyMode( empathyMode );
 	};
+
+	// After changing the language, the menu should be requested again so that items are fetched with the current locale.
+	useEffect( () => {
+		props.requestAdminMenu( props.selectedSiteId );
+	}, [ props.locale ] );
 
 	return (
 		<Fragment>
@@ -53,6 +61,7 @@ function QuickLanguageSwitcher( props ) {
 export default connect(
 	( state ) => ( {
 		selectedLanguageSlug: getCurrentLocaleSlug( state ),
+		selectedSiteId: getSelectedSiteId( state ),
 	} ),
-	{ setLocale }
-)( QuickLanguageSwitcher );
+	{ setLocale, requestAdminMenu }
+)( localize( QuickLanguageSwitcher ) );
