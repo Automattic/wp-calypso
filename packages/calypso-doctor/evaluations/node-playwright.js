558927cd2fa4fc08cb3a9119dfb67845c9746d93
Edwin Takahashi <egao@outlook.com>
2021-03-15 10:32:31 -0700

    Add in PLAYWRIGHT_SKIP_DOWNLOAD check to calypso-doctor, and set the aforementioned environment variable where appropriate in Dockerfiles.

---
+++ packages/calypso-doctor/evaluations/node-playwright.js
@@ -0,0 +1,25 @@
+const { getShellRc } = require( '../lib' );
+
+module.exports = {
+	title: 'Skip Playwright download',
+	group: 'Node.js',
+	description: 'Do not download Playwright',
+	test: ( { pass, fail, ignore } ) => {
+		if ( process.platform !== 'darwin' && process.platform !== 'linux' ) {
+			ignore( 'This evaluation only works in OSX or Linux' );
+			return;
+		}
+
+		if ( ! process.env.PLAYWRIGHT_SKIP_DOWNLOAD ) {
+			fail( 'PLAYWRIGHT_SKIP_DOWNLOAD is not set' );
+			return;
+		}
+
+		pass();
+	},
+	fix: () => {
+		const shell = getShellRc();
+		if ( shell ) return `Add \`export PLAYWRIGHT_SKIP_DOWNLOAD=true\` to ${ shell }`;
+		return 'Set env variable `PLAYWRIGHT_SKIP_DOWNLOAD` with value `true`';
+	},
+};
