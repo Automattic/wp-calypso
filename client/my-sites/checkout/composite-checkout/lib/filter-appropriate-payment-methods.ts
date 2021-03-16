76b0b2017a67c8e3f375c26c218a4ff4c26e84fb
Payton Swick <payton@foolord.com>
2021-03-16 14:23:59 -0400

    Update filterAppropriatePaymentMethods to get required data from cart

--- client/my-sites/checkout/composite-checkout/lib/filter-appropriate-payment-methods.ts
+++ client/my-sites/checkout/composite-checkout/lib/filter-appropriate-payment-methods.ts
@@ -1,8 +1,7 @@
 /**
  * External dependencies
  */
-import debugFactory from 'debug';
-import type { PaymentMethod, LineItem } from '@automattic/composite-checkout';
+import type { PaymentMethod } from '@automattic/composite-checkout';
 import type { ResponseCart } from '@automattic/shopping-cart';
 
 /**
@@ -13,36 +12,19 @@ import doesPurchaseHaveFullCredits from './does-purchase-have-full-credits';
 import { readCheckoutPaymentMethodSlug } from './translate-payment-method-names';
 import isPaymentMethodEnabled from './is-payment-method-enabled';
 
-const debug = debugFactory( 'calypso:composite-checkout:filter-appropriate-payment-methods' );
-
 export default function filterAppropriatePaymentMethods( {
 	paymentMethodObjects,
 	countryCode,
-	total,
-	credits,
-	subtotal,
 	responseCart,
 	allowedPaymentMethods,
 }: {
 	paymentMethodObjects: PaymentMethod[];
 	countryCode: string;
-	total: LineItem;
-	credits: LineItem | null;
-	subtotal: LineItem;
 	allowedPaymentMethods: CheckoutPaymentMethodSlug[];
 	responseCart: ResponseCart;
 } ): PaymentMethod[] {
-	const isPurchaseFree = total.amount.value === 0;
+	const isPurchaseFree = responseCart.total_cost_integer === 0;
 	const isFullCredits = doesPurchaseHaveFullCredits( responseCart );
-	debug( 'filtering payment methods with this input', {
-		total,
-		credits,
-		subtotal,
-		allowedPaymentMethods,
-		isPurchaseFree,
-		isFullCredits,
-		countryCode,
-	} );
 
 	return paymentMethodObjects
 		.filter( ( methodObject ) => {
