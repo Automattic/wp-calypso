4c08c8afd53c30256621a47db1305e4c0c10d9c7
cpap <papazoglou.charalampos@gmail.com>
2021-03-16 20:09:49 +0200

    Adds tracking.

--- client/my-sites/sidebar-unified/add-new-site.jsx
+++ client/my-sites/sidebar-unified/add-new-site.jsx
@@ -7,7 +7,7 @@
  * External dependencies
  */
 import React, { Fragment } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
 /**
@@ -19,14 +19,22 @@ import SidebarItem from 'calypso/layout/sidebar/item';
 import SidebarSeparator from 'calypso/layout/sidebar/separator';
 import SidebarCustomIcon from 'calypso/layout/sidebar/custom-icon';
 import TranslatableString from 'calypso/components/translatable/proptype';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import { setLayoutFocus } from 'calypso/state/ui/layout-focus/actions';
 
 export const AddNewSite = ( { title, icon } ) => {
+	const reduxDispatch = useDispatch();
 	const visibleSiteCount = useSelector( getCurrentUser ).visible_site_count;
 	const onboardingUrl = useSelector( getOnboardingUrl );
 
 	if ( visibleSiteCount > 1 ) {
 		return null;
 	}
+
+	const onNavigate = () => {
+		reduxDispatch( recordTracksEvent( 'calypso_add_new_wordpress_click' ) );
+		reduxDispatch( setLayoutFocus( 'content' ) );
+	};
 
 	return (
 		<Fragment>
@@ -35,6 +43,7 @@ export const AddNewSite = ( { title, icon } ) => {
 				label={ title }
 				link={ `${ onboardingUrl }?ref=calypso-sidebar` }
 				customIcon={ <SidebarCustomIcon icon={ icon } /> }
+				onNavigate={ onNavigate }
 			/>
 		</Fragment>
 	);
