e630afce12516e9a03d0af6a35ba7a96d4f4babd
flootr <m.do@posteo.net>
2021-03-16 12:15:49 +0100

    people(followers): use `react-query` to fetch data & remove followers (#50378)
    * people(followers): use `react-query` to fetch data & remove followers
    
    * useFollowers: expect `siteId` as first param
    
    * followers: move `listKeyUp` & don't pass on `query`
    
    * rename `useFollowers` to `useFollowersQuery`
    
    * rename `useRemoveFollower` to `useRemoveFollowerMutation`
    
    * useFollowersQuery: use `wpcom.req.get` directly
    
    * FollowersList: extract `useErrorNotice`
    
    * useFollowersQuery: make `type` mandatory & introduce `defaults`
    
    * make `removeFollower` a wrapper around `mutate`
    
    * useFollowersQuery: set `type` to `wpcom` by default
    
    * useFollowersQuery: change query params order
    
    * removeFollower: use `mutate` as dep explicitly
    
    * FollowersList: make sure strings are translated
    
    * FollowersList: simplify `noFollowerSearchResult`
    
    * useFollowersQuery: don't mutate internal data when normalizing
    
    * useRemoveFollowerMutation: change how `mutate` is extracted from `useMutation`

---
+++ client/data/followers/use-followers-query.js
@@ -0,0 +1,67 @@
+/**
+ * External dependencies
+ */
+import { useInfiniteQuery } from 'react-query';
+import { uniqueBy } from '@automattic/js-utils';
+
+/**
+ * Internal dependencies
+ */
+import wpcom from 'calypso/lib/wp';
+
+const MAX_FOLLOWERS = 10; // means pages (= 1000 followers);
+const defaults = {
+	max: 100,
+};
+
+/**
+ * Normalizes a follower object. Changes 'avatar' to 'avatar_URL' allowing a follower
+ * object to be used with the Gravatar component.
+ *
+ * @param  {object} follower A follower ojbect
+ * @returns {object}          A normalized follower object
+ */
+export function normalizeFollower( follower ) {
+	return {
+		avatar_URL: follower.avatar,
+		...follower,
+	};
+}
+
+const extractPages = ( pages = [] ) =>
+	pages.flatMap( ( page ) => page.subscribers ).map( normalizeFollower );
+const compareUnique = ( a, b ) => a.ID === b.ID;
+
+const useFollowersQuery = ( siteId, type = 'wpcom', fetchOptions = {}, queryOptions = {} ) => {
+	const { search } = fetchOptions;
+
+	return useInfiniteQuery(
+		[ 'followers', siteId, type, search ],
+		async ( { pageParam = 1 } ) =>
+			wpcom.req.get( `/sites/${ siteId }/followers`, {
+				...defaults,
+				...fetchOptions,
+				type,
+				page: pageParam,
+			} ),
+		{
+			...queryOptions,
+			getNextPageParam: ( lastPage, allPages ) => {
+				if ( lastPage.pages <= allPages.length || allPages.length >= MAX_FOLLOWERS ) {
+					return;
+				}
+				return allPages.length + 1;
+			},
+			select: ( data ) => {
+				return {
+					/* @TODO: `uniqueBy` is necessary, because the API can return duplicates */
+					followers: uniqueBy( extractPages( data.pages ), compareUnique ),
+					total: data.pages[ 0 ].total,
+					...data,
+				};
+			},
+		}
+	);
+};
+
+export default useFollowersQuery;
