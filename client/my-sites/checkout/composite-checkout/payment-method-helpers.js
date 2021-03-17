ccdec479c84004d13fdbda508e4e6dcdbb2d97f5
Payton Swick <payton@foolord.com>
2021-03-17 17:28:13 -0400

    Remove old versionos of PayPal processor submission functions

--- client/my-sites/checkout/composite-checkout/payment-method-helpers.js
+++ client/my-sites/checkout/composite-checkout/payment-method-helpers.js
@@ -10,7 +10,6 @@ import { createStripePaymentMethod } from '@automattic/calypso-stripe';
  * Internal dependencies
  */
 import wp from 'calypso/lib/wp';
-import { createPayPalExpressEndpointRequestPayloadFromLineItems } from './types/paypal-express';
 import { translateCheckoutPaymentMethodToWpcomPaymentMethod } from './lib/translate-payment-method-names';
 import { getSavedVariations } from 'calypso/lib/abtest';
 import { stringifyBody } from 'calypso/state/login/utils';
@@ -28,14 +27,6 @@ export async function submitApplePayPayment( transactionData, submit, transactio
 		paymentPartnerProcessorId: transactionData.stripeConfiguration.processor_id,
 	} );
 	debug( 'submitting apple-pay transaction', formattedTransactionData );
-	return submit( formattedTransactionData, transactionOptions );
-}
-
-export async function submitPayPalExpressRequest( transactionData, submit, transactionOptions ) {
-	const formattedTransactionData = createPayPalExpressEndpointRequestPayloadFromLineItems( {
-		...transactionData,
-	} );
-	debug( 'sending paypal transaction', formattedTransactionData );
 	return submit( formattedTransactionData, transactionOptions );
 }
 
@@ -218,32 +209,6 @@ export async function wpcomTransaction( payload, transactionOptions ) {
 	}
 
 	return wp.undocumented().transactions( payload );
-}
-
-export async function wpcomPayPalExpress( payload, transactionOptions ) {
-	if ( transactionOptions && transactionOptions.createUserAndSiteBeforeTransaction ) {
-		return createAccount().then( ( response ) => {
-			const siteIdFromResponse = response?.blog_details?.blogid;
-
-			// If the account is already created(as happens when we are reprocessing after a transaction error), then
-			// the create account response will not have a site ID, so we fetch from state.
-			const siteId = siteIdFromResponse || select( 'wpcom' )?.getSiteId();
-			const newPayload = {
-				...payload,
-				siteId,
-				cart: {
-					...payload.cart,
-					blog_id: siteId || '0',
-					cart_key: siteId || 'no-site',
-					create_new_blog: false,
-				},
-			};
-
-			return wp.undocumented().paypalExpressUrl( newPayload );
-		} );
-	}
-
-	return wp.undocumented().paypalExpressUrl( payload );
 }
 
 export function createStripePaymentMethodToken( { stripe, name, country, postalCode } ) {
