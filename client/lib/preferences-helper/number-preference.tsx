b8ef23c7aad32188029acce435e3bdeafb5e7452
Caleb Bauermeister <cbauerman@gmail.com>
2021-03-18 12:47:42 -0700

    Update NumberPreference helper to allow editing

---
+++ client/lib/preferences-helper/number-preference.tsx
@@ -0,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import { useDispatch } from 'react-redux';
+import React, {
+	FunctionComponent,
+	ChangeEventHandler,
+	useCallback,
+	useEffect,
+	useState,
+} from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { savePreference } from 'calypso/state/preferences/actions';
+
+interface Props {
+	name: string;
+	value: number;
+}
+
+const NumberPreference: FunctionComponent< Props > = ( { name, value } ) => {
+	const dispatch = useDispatch();
+
+	const [ localValue, setLocalValue ] = useState( value );
+
+	const savePreferenceChange = useCallback( () => {
+		dispatch( savePreference( name, localValue ) );
+	}, [ dispatch, localValue, name ] );
+
+	const handleLocalChange: ChangeEventHandler< HTMLInputElement > = ( event ) => {
+		setLocalValue( parseInt( event.target.value ) );
+	};
+
+	useEffect( () => setLocalValue( value ), [ value ] );
+
+	return (
+		<ul className="preferences-helper__list">
+			<li key={ name }>
+				<input type="number" onChange={ handleLocalChange } value={ localValue } />{ ' ' }
+				<button
+					className="preferences-helper__save-pref-button"
+					onClick={ savePreferenceChange }
+					disabled={ value === localValue }
+				>
+					{ 'save' }
+				</button>
+			</li>
+		</ul>
+	);
+};
+
+export default NumberPreference;
