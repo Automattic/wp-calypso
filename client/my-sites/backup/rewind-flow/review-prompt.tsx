73c950b87b09a1cc70f30a5031693b09452e6067
Caleb Bauermeister <cbauerman@gmail.com>
2021-03-18 10:52:04 -0700

    Restyle review prompt

--- client/my-sites/backup/rewind-flow/review-prompt.tsx
+++ client/my-sites/backup/rewind-flow/review-prompt.tsx
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { useSelector } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
 import React, { FunctionComponent } from 'react';
 
@@ -8,21 +9,44 @@ import React, { FunctionComponent } from 'react';
  * Internal dependencies
  */
 import { Button } from '@wordpress/components';
-import DismissibleCard from 'calypso/blocks/dismissible-card';
+import { hasReceivedRemotePreferences as getHasReceivedRemotePreferences } from 'calypso/state/preferences/selectors';
+import Gridicon from 'calypso/components/gridicon';
+import QueryPreferences from 'calypso/components/data/query-preferences';
+import RewindFlowNotice, { RewindFlowNoticeLevel } from './rewind-flow-notice';
 
 const JetpackReviewPrompt: FunctionComponent = () => {
 	const translate = useTranslate();
+	const hasReceivedRemotePreferences = useSelector( ( state ) =>
+		getHasReceivedRemotePreferences( state )
+	);
 	return (
-		<DismissibleCard preferenceName="jetpack-review-prompt" className="review-prompt">
-			<p>{ translate( 'Had an easy restore experience?' ) }</p>
-			<Button
-				href="https://wordpress.org/support/plugin/jetpack/reviews/#new-post"
-				isSecondary={ true }
-				target="_blank"
-			>
-				{ translate( 'Leave Jetpack a review' ) }
-			</Button>
-		</DismissibleCard>
+		<>
+			<QueryPreferences />
+			{ hasReceivedRemotePreferences && (
+				<div className="review-prompt">
+					<div className="review-prompt__header">
+						<RewindFlowNotice
+							gridicon="star"
+							type={ RewindFlowNoticeLevel.REMINDER }
+							title={ translate( 'Had an easy restore experience?' ) }
+						/>
+					</div>
+					<div className="review-prompt__button-row">
+						<Button
+							href="https://wordpress.org/support/plugin/jetpack/reviews/#new-post"
+							target="_blank"
+						>
+							<Gridicon icon="external" />
+							{ translate( 'Leave Jetpack a review' ) }
+						</Button>
+						<Button>
+							<Gridicon icon="cross" />
+							{ translate( 'No thanks' ) }
+						</Button>
+					</div>
+				</div>
+			) }
+		</>
 	);
 };
 
