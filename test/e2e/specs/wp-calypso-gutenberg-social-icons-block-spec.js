a918cd6166149339cda9c1af10fd7243d46c7593
Jagjeet Khalsa <Jagjeet.Khalsa@gmail.com>
2021-03-18 10:30:36 -0700

    Add e2e test for inserting social icons block
    This adds a basic test of testing an out-of-box social icons block and
    verifying it can be inserted and viewed in the previewed post.

--- test/e2e/specs/wp-calypso-gutenberg-social-icons-block-spec.js
+++ test/e2e/specs/wp-calypso-gutenberg-social-icons-block-spec.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import assert from 'assert';
 import config from 'config';
 import { By } from 'selenium-webdriver';
 
@@ -10,10 +11,10 @@ import { By } from 'selenium-webdriver';
 import LoginFlow from '../lib/flows/login-flow.js';
 
 import GutenbergEditorComponent from '../lib/gutenberg/gutenberg-editor-component';
+import PostPreviewComponent from '../lib/components/post-preview-component';
 
 import * as driverManager from '../lib/driver-manager';
 import * as driverHelper from '../lib/driver-helper';
-import * as mediaHelper from '../lib/media-helper';
 import * as dataHelper from '../lib/data-helper';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
@@ -33,20 +34,19 @@ describe( `[${ host }] Calypso Gutenberg Editor: Social Icons Block (${ screenSi
 	} );
 
 	describe( 'Preview and Publish a Post with a Social Icons block', function () {
-		let fileDetails;
-
-		// Create image file for upload
-		before( async function () {
-			fileDetails = await mediaHelper.createFile();
-			return fileDetails;
-		} );
+		const blogPostTitle = dataHelper.randomPhrase();
 
 		step( 'Can log in', async function () {
 			this.loginFlow = new LoginFlow( driver, gutenbergUser );
 			return await this.loginFlow.loginAndStartNewPost( null, true );
 		} );
 
-		step( 'Can insert the Social icons block', async function () {
+		step( 'Can enter post title', async function () {
+			const gEditorComponent = await GutenbergEditorComponent.Expect( driver );
+			return await gEditorComponent.enterTitle( blogPostTitle );
+		} );
+
+		step( 'Can insert the Social Icons block', async function () {
 			const gEditorComponent = await GutenbergEditorComponent.Expect( driver );
 			await gEditorComponent.addBlock( 'Social Links' );
 
@@ -56,11 +56,28 @@ describe( `[${ host }] Calypso Gutenberg Editor: Social Icons Block (${ screenSi
 			);
 		} );
 
-		after( async function () {
-			if ( fileDetails ) {
-				await mediaHelper.deleteFile( fileDetails );
-			}
-			await driverHelper.acceptAlertIfPresent( driver );
+		step( 'Can launch post preview', async function () {
+			const gEditorComponent = await GutenbergEditorComponent.Expect( driver );
+			await gEditorComponent.ensureSaved();
+			await gEditorComponent.launchPreview();
+		} );
+
+		step( 'Can see correct post title in preview', async function () {
+			this.postPreviewComponent = await PostPreviewComponent.Expect( driver );
+
+			const postTitle = await this.postPreviewComponent.postTitle();
+			assert.strictEqual(
+				postTitle.toLowerCase(),
+				blogPostTitle.toLowerCase(),
+				'The blog post preview title is not correct'
+			);
+		} );
+
+		step( 'Can see the cover block in our published post', async function () {
+			return await driverHelper.waitTillPresentAndDisplayed(
+				driver,
+				By.css( '.wp-block-social-links' )
+			);
 		} );
 	} );
 } );
