3939b361a98f7ee8d77ffb2251128f95b2b80447
Kevin Zoschke <kevin.zoschke@automattic.com>
2021-03-18 15:03:59 -0400

    Add tests to AdvancedCredentialsLoadingPlaceholder

---
+++ client/jetpack-cloud/sections/settings/loading/test/index.jsx
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+
+/**
+ * Internal dependencies
+ */
+import { render } from 'config/testing-library';
+import AdvancedCredentialsLoadingPlaceholder from '../index';
+
+describe( 'AdvancedCredentialsLoadingPlaceholder', () => {
+	it( 'should render correctly', () => {
+		const { container } = render(
+			<AdvancedCredentialsLoadingPlaceholder></AdvancedCredentialsLoadingPlaceholder>
+		);
+
+		expect( container ).toMatchSnapshot();
+	} );
+} );
