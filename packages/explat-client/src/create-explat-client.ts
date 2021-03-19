d03a1c916eb0908d6d2dce4c035752a04a66405c
Jessie Ross <jessie.ross@automattic.com>
2021-03-19 12:03:57 +0800

    ExPlat: Increase the loadExperiment timeout (#51181)
    * Increase the loadExperiment timeout
    
    * Increase the experiment fetch timeout

--- packages/explat-client/src/create-explat-client.ts
+++ packages/explat-client/src/create-explat-client.ts
@@ -12,7 +12,7 @@ import { createFallbackExperimentAssignment as createFallbackExperimentAssignmen
 /**
  * The number of milliseconds before we abandon fetching an experiment
  */
-const EXPERIMENT_FETCH_TIMEOUT = 5000;
+const EXPERIMENT_FETCH_TIMEOUT = 10000;
 
 export interface ExPlatClient {
 	/**
