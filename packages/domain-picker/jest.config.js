71c19980fa8a31dd4f33f944a588dd910f9c2e12
Marco Ciampini <marco.ciampini@automattic.com>
2021-03-19 15:49:34 +0100

    Fix jest match patter from __tests__ to __test__

--- packages/domain-picker/jest.config.js
+++ packages/domain-picker/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	preset: '../../test/packages/jest-preset.js',
 	testEnvironment: 'jsdom',
-	testMatch: [ '<rootDir>/**/__tests__/**/*.[jt]s?(x)', '!**/.eslintrc.*' ],
+	testMatch: [ '<rootDir>/**/__test__/**/*.[jt]s?(x)', '!**/.eslintrc.*' ],
 	setupFilesAfterEnv: [ '@testing-library/jest-dom/extend-expect' ],
 };
