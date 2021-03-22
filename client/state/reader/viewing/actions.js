0632599ee41ad6c45c75b7a3ff1a9a79af2d4a2a
Jarda Snajdr <jsnajdr@gmail.com>
2021-03-22 08:56:21 +0100

    Reader State: cleanup the Follows and Viewing reducers (#51222)
    * Reader State: cleanup the Follows and Viewing reducers
    
    * Remove the reader.viewing state

--- client/state/reader/viewing/actions.js
+++ client/state/reader/viewing/actions.js
@@ -5,7 +5,6 @@ import {
 	READER_VIEWING_FULL_POST_SET,
 	READER_VIEWING_FULL_POST_UNSET,
 } from 'calypso/state/reader/action-types';
-import 'calypso/state/reader/init';
 
 export const setViewingFullPostKey = ( postKey ) => ( {
 	type: READER_VIEWING_FULL_POST_SET,
