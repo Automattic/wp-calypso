3c9f0508e50da1a1b8bb05e31dbc0851552e4aae
Julian de Bhal <julian.debhal@automattic.com>
2021-03-23 12:51:10 +1000

    restore updates

--- apps/editing-toolkit/editing-toolkit-plugin/starter-page-templates/page-template-plugin.tsx
+++ apps/editing-toolkit/editing-toolkit-plugin/starter-page-templates/page-template-plugin.tsx
@@ -2,72 +2,100 @@
  * External dependencies
  */
 import '@wordpress/nux';
-import { compose } from '@wordpress/compose';
-import { withDispatch, withSelect } from '@wordpress/data';
+import { useSelect, useDispatch } from '@wordpress/data';
 import { addFilter, removeFilter } from '@wordpress/hooks';
-import { PageTemplateModal } from '@automattic/page-template-modal';
+import { LayoutDefinition, PageTemplateModal } from '@automattic/page-template-modal';
+import React, { useCallback } from '@wordpress/element';
 
 const INSERTING_HOOK_NAME = 'isInsertingPageTemplate';
 const INSERTING_HOOK_NAMESPACE = 'automattic/full-site-editing/inserting-template';
 
-export const PageTemplatesPlugin = compose(
-	withSelect( ( select ) => {
-		const getMeta = () => select( 'core/editor' ).getEditedPostAttribute( 'meta' );
+interface PageTemplatesPluginProps {
+	templates: LayoutDefinition[];
+	locale?: string;
+	theme?: string;
+}
+
+export function PageTemplatesPlugin( props: PageTemplatesPluginProps ): JSX.Element {
+	const { setOpenState } = useDispatch( 'automattic/starter-page-layouts' );
+	const { replaceInnerBlocks } = useDispatch( 'core/block-editor' );
+	const { editPost } = useDispatch( 'core/editor' );
+	const { toggleFeature } = useDispatch( 'core/edit-post' );
+	const { disableTips } = useDispatch( 'core/nux' );
+
+	const selectProps = useSelect( ( select ) => {
 		const { isOpen } = select( 'automattic/starter-page-layouts' );
-		const currentBlocks = select( 'core/editor' ).getBlocks();
 		return {
 			isOpen: isOpen(),
-			getMeta,
-			currentPostTitle: select( 'core/editor' ).getCurrentPost().title,
-			postContentBlock: currentBlocks.find( ( block ) => block.name === 'a8c/post-content' ),
-			isWelcomeGuideActive: select( 'core/edit-post' ).isFeatureActive( 'welcomeGuide' ), // Gutenberg 7.2.0 or higher
-			areTipsEnabled: select( 'core/nux' ) ? select( 'core/nux' ).areTipsEnabled() : false, // Gutenberg 7.1.0 or lower
+			isWelcomeGuideActive: select( 'core/edit-post' ).isFeatureActive( 'welcomeGuide' ) as boolean, // Gutenberg 7.2.0 or higher
+			areTipsEnabled: select( 'core/nux' )
+				? ( select( 'core/nux' ).areTipsEnabled() as boolean )
+				: false, // Gutenberg 7.1.0 or lower
 		};
-	} ),
-	withDispatch( ( dispatch, ownProps ) => {
-		const editorDispatcher = dispatch( 'core/editor' );
-		const { setOpenState } = dispatch( 'automattic/starter-page-layouts' );
+	} );
+
+	const { getMeta, postContentBlock } = useSelect( ( select ) => {
+		const getMeta = () => select( 'core/editor' ).getEditedPostAttribute( 'meta' );
+		const currentBlocks = select( 'core/editor' ).getBlocks();
 		return {
-			setOpenState,
-			saveTemplateChoice: ( name ) => {
-				// Save selected template slug in meta.
-				const currentMeta = ownProps.getMeta();
-				editorDispatcher.editPost( {
-					meta: {
-						...currentMeta,
-						_starter_page_template: name,
-					},
-				} );
-			},
-			insertTemplate: ( title, blocks ) => {
-				// Add filter to let the tracking library know we are inserting a template.
-				addFilter( INSERTING_HOOK_NAME, INSERTING_HOOK_NAMESPACE, () => true );
-
-				// Set post title.
-				if ( title ) {
-					editorDispatcher.editPost( { title } );
-				}
-
-				// Replace blocks.
-				const postContentBlock = ownProps.postContentBlock;
-				dispatch( 'core/block-editor' ).replaceInnerBlocks(
-					postContentBlock ? postContentBlock.clientId : '',
-					blocks,
-					false
-				);
-
-				// Remove filter.
-				removeFilter( INSERTING_HOOK_NAME, INSERTING_HOOK_NAMESPACE );
-			},
-			hideWelcomeGuide: () => {
-				if ( ownProps.isWelcomeGuideActive ) {
-					// Gutenberg 7.2.0 or higher.
-					dispatch( 'core/edit-post' ).toggleFeature( 'welcomeGuide' );
-				} else if ( ownProps.areTipsEnabled ) {
-					// Gutenberg 7.1.0 or lower.
-					dispatch( 'core/nux' ).disableTips();
-				}
-			},
+			getMeta,
+			postContentBlock: currentBlocks.find( ( block ) => block.name === 'a8c/post-content' ),
 		};
-	} )
-)( PageTemplateModal );
+	} );
+
+	const saveTemplateChoice = useCallback(
+		( name: string ) => {
+			// Save selected template slug in meta.
+			const currentMeta = getMeta();
+			editPost( {
+				meta: {
+					...currentMeta,
+					_starter_page_template: name,
+				},
+			} );
+		},
+		[ editPost, getMeta ]
+	);
+
+	const insertTemplate = useCallback(
+		( title, blocks ) => {
+			// Add filter to let the tracking library know we are inserting a template.
+			addFilter( INSERTING_HOOK_NAME, INSERTING_HOOK_NAMESPACE, () => true );
+
+			// Set post title.
+			if ( title ) {
+				editPost( { title } );
+			}
+
+			// Replace blocks.
+			replaceInnerBlocks( postContentBlock ? postContentBlock.clientId : '', blocks, false );
+
+			// Remove filter.
+			removeFilter( INSERTING_HOOK_NAME, INSERTING_HOOK_NAMESPACE );
+		},
+		[ editPost, postContentBlock, replaceInnerBlocks ]
+	);
+
+	const { isWelcomeGuideActive, areTipsEnabled } = selectProps;
+
+	const hideWelcomeGuide = useCallback( () => {
+		if ( isWelcomeGuideActive ) {
+			// Gutenberg 7.2.0 or higher.
+			toggleFeature( 'welcomeGuide' );
+		} else if ( areTipsEnabled ) {
+			// Gutenberg 7.1.0 or lower.
+			disableTips();
+		}
+	}, [ areTipsEnabled, disableTips, isWelcomeGuideActive, toggleFeature ] );
+
+	return (
+		<PageTemplateModal
+			{ ...selectProps }
+			setOpenState={ setOpenState }
+			saveTemplateChoice={ saveTemplateChoice }
+			insertTemplate={ insertTemplate }
+			hideWelcomeGuide={ hideWelcomeGuide }
+			{ ...props }
+		/>
+	);
+}
