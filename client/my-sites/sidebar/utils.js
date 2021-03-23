6fb9889b71d4ab4da535ec208836dad21add8f00
cpap <papazoglou.charalampos@gmail.com>
2021-03-23 17:30:02 +0200

    Resets legacy functions.

--- client/my-sites/sidebar/utils.js
+++ client/my-sites/sidebar/utils.js
@@ -5,11 +5,7 @@ import { includes, some } from 'lodash';
 
 // Check if `path` starts with `pathPrefix` and the very next character
 // (if there is any) is `/` or `?`
-// If shallow is true, check only the very first fragment.
-function pathStartsWith( fullPath, pathPrefix, shallow ) {
-	if ( shallow && ! includes( [ 'types', 'marketing' ], fullPath.split( '/' )?.[ 1 ] ) ) {
-		return fullPath.split( '/' )?.[ 1 ] === pathPrefix.split( '/' )?.[ 1 ];
-	}
+function pathStartsWith( fullPath, pathPrefix ) {
 	return (
 		fullPath === pathPrefix ||
 		( fullPath.slice( 0, pathPrefix.length ) === pathPrefix &&
@@ -17,10 +13,10 @@ function pathStartsWith( fullPath, pathPrefix, shallow ) {
 	);
 }
 
-export function itemLinkMatches( paths, currentPath, shallow = false ) {
+export function itemLinkMatches( paths, currentPath ) {
 	if ( ! Array.isArray( paths ) ) {
 		paths = [ paths ];
 	}
 
-	return some( paths, ( path ) => pathStartsWith( currentPath, path, shallow ) );
+	return some( paths, ( path ) => pathStartsWith( currentPath, path ) );
 }
