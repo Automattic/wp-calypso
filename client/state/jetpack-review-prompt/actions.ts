dc1f028881cf77040d1cbfdeb65f22ea727f1942
Caleb Bauermeister <cbauerman@gmail.com>
2021-03-24 15:05:14 -0700

    Factor out action logic, add tests

--- client/state/jetpack-review-prompt/actions.ts
+++ client/state/jetpack-review-prompt/actions.ts
@@ -3,46 +3,64 @@
  */
 import { getPreference } from 'calypso/state/preferences/selectors';
 import { getExistingPreference } from './selectors';
-import { PREFERENCE_NAME } from './constants';
+import { PREFERENCE_NAME, PreferenceType } from './constants';
 import { savePreference } from 'calypso/state/preferences/actions';
+import { AppState } from 'calypso/types';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const dismiss = ( type: 'restore' | 'scan', reviewed = false ) => ( dispatch, getState ) => {
-	const state = getState();
+const combineDismissPreference = (
+	state: AppState,
+	type: 'restore' | 'scan',
+	dismissedAt: number,
+	reviewed: boolean
+): PreferenceType => {
 	const fullPref = getPreference( state, PREFERENCE_NAME ) ?? {};
 	const previousPref = getExistingPreference( state, type );
 
-	return dispatch(
-		savePreference( PREFERENCE_NAME, {
-			...fullPref,
-			[ type ]: {
-				...previousPref,
-				dismissCount: previousPref.dismissCount + ( reviewed ? 0 : 1 ),
-				dismissedAt: Date.now(),
-				reviewed,
-			},
-		} )
-	);
+	return {
+		...fullPref,
+		[ type ]: {
+			...previousPref,
+			dismissCount: previousPref.dismissCount + ( reviewed ? 0 : 1 ),
+			dismissedAt,
+			reviewed,
+		},
+	};
 };
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const dismiss = (
+	type: 'restore' | 'scan',
+	dismissedAt: number = Date.now(),
+	reviewed = false
+) => ( dispatch, getState ) =>
+	dispatch(
+		savePreference(
+			PREFERENCE_NAME,
+			combineDismissPreference( getState(), type, dismissedAt, reviewed )
+		)
+	);
+
+const combineValidPreference = (
+	state: AppState,
+	type: 'restore' | 'scan',
+	validFrom: number | null
+): PreferenceType => {
+	const fullPref = getPreference( state, PREFERENCE_NAME ) ?? {};
+	const previousPref = getExistingPreference( state, type );
+
+	return {
+		...fullPref,
+		[ type ]: {
+			...previousPref,
+			validFrom: validFrom ?? Date.now(),
+		},
+	};
+};
+
 const setValidFrom = ( type: 'restore' | 'scan', validFrom: number | null = null ) => (
 	dispatch,
 	getState
-) => {
-	const state = getState();
-	const fullPref = getPreference( state, PREFERENCE_NAME ) ?? {};
-	const previousPref = getExistingPreference( state, type );
-
-	return dispatch(
-		savePreference( PREFERENCE_NAME, {
-			...fullPref,
-			[ type ]: {
-				...previousPref,
-				validFrom: validFrom ?? Date.now(),
-			},
-		} )
+) =>
+	dispatch(
+		savePreference( PREFERENCE_NAME, combineValidPreference( getState(), type, validFrom ) )
 	);
-};
-
-export { dismiss, setValidFrom };
+export { dismiss, setValidFrom, combineDismissPreference, combineValidPreference };
