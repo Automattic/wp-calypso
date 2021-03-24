f1e2f41f0498107272dfed17df155f8cf8087932
Payton Swick <payton@foolord.com>
2021-03-24 13:08:44 -0400

    Update freePurchaseProcessor to use cart for creating transaction cart

--- client/my-sites/checkout/composite-checkout/lib/free-purchase-processor.ts
+++ client/my-sites/checkout/composite-checkout/lib/free-purchase-processor.ts
@@ -10,38 +10,30 @@ import type { PaymentProcessorResponse } from '@automattic/composite-checkout';
  */
 import getDomainDetails from './get-domain-details';
 import submitWpcomTransaction from './submit-wpcom-transaction';
-import { createTransactionEndpointRequestPayloadFromLineItems } from './translate-cart';
+import {
+	createTransactionEndpointRequestPayload,
+	createTransactionEndpointCartFromResponseCart,
+} from './translate-cart';
 import type { PaymentProcessorOptions } from '../types/payment-processors';
-import type { WPCOMCartItem } from '../types/checkout-cart';
 import type {
-	TransactionRequestWithLineItems,
+	TransactionRequest,
 	WPCOMTransactionEndpointResponse,
 } from '../types/transaction-endpoint';
 
 const debug = debugFactory( 'calypso:composite-checkout:free-purchase-processor' );
 
-type FreePurchaseTransactionRequest = {
-	items: WPCOMCartItem[];
-};
-
 type SubmitFreePurchaseTransactionData = Omit<
-	TransactionRequestWithLineItems,
-	'paymentMethodType' | 'paymentPartnerProcessorId'
+	TransactionRequest,
+	'paymentMethodType' | 'paymentPartnerProcessorId' | 'cart'
 >;
 
 export default async function freePurchaseProcessor(
-	submitData: unknown,
 	transactionOptions: PaymentProcessorOptions
 ): Promise< PaymentProcessorResponse > {
-	if ( ! isValidTransactionData( submitData ) ) {
-		throw new Error( 'Required purchase data is missing' );
-	}
-
 	const { siteId, responseCart, includeDomainDetails, includeGSuiteDetails } = transactionOptions;
 
 	return submitFreePurchaseTransaction(
 		{
-			...submitData,
 			name: '',
 			couponId: responseCart.coupon,
 			siteId: siteId ? String( siteId ) : '',
@@ -59,20 +51,15 @@ async function submitFreePurchaseTransaction(
 	transactionOptions: PaymentProcessorOptions
 ): Promise< WPCOMTransactionEndpointResponse > {
 	debug( 'formatting free transaction', transactionData );
-	const formattedTransactionData = createTransactionEndpointRequestPayloadFromLineItems( {
+	const formattedTransactionData = createTransactionEndpointRequestPayload( {
 		...transactionData,
+		cart: createTransactionEndpointCartFromResponseCart( {
+			siteId: transactionOptions.siteId ? String( transactionOptions.siteId ) : undefined,
+			contactDetails: transactionData.domainDetails ?? null,
+			responseCart: transactionOptions.responseCart,
+		} ),
 		paymentMethodType: 'WPCOM_Billing_WPCOM',
 	} );
 	debug( 'submitting free transaction', formattedTransactionData );
 	return submitWpcomTransaction( formattedTransactionData, transactionOptions );
-}
-
-function isValidTransactionData(
-	submitData: unknown
-): submitData is FreePurchaseTransactionRequest {
-	const data = submitData as FreePurchaseTransactionRequest;
-	if ( ! ( data?.items?.length > 0 ) ) {
-		throw new Error( 'Transaction requires items and none were provided' );
-	}
-	return true;
 }
