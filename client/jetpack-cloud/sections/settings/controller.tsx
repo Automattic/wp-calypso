4557bf9835b027183deb1d797d0740433887e10d
Kevin Zoschke <kevin.zoschke@automattic.com>
2021-03-25 11:45:29 -0400

    Don't show empty settings state for sites with credentials

--- client/jetpack-cloud/sections/settings/controller.tsx
+++ client/jetpack-cloud/sections/settings/controller.tsx
@@ -12,6 +12,7 @@ import NotAuthorizedPage from 'calypso/components/jetpack/not-authorized-page';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import AdvancedCredentials from './advanced-credentials';
 import NoSitesPurchasesMessage from './empty-content';
+import HasSiteCredentialsSwitch from './has-site-credentials-switch';
 import AdvancedCredentialsLoadingPlaceholder from './loading';
 import SettingsPage from './main';
 
@@ -23,12 +24,20 @@ export const settings: PageJS.Callback = ( context, next ) => {
 export const advancedCredentials: PageJS.Callback = ( context, next ) => {
 	const { host, action } = context.query;
 	const siteId = getSelectedSiteId( context.store.getState() ) as number;
+	const sectionElt = <AdvancedCredentials action={ action } host={ host } role="main" />;
 
 	context.primary = (
-		<HasSitePurchasesSwitch
+		<HasSiteCredentialsSwitch
 			siteId={ siteId }
-			trueComponent={ <AdvancedCredentials action={ action } host={ host } role="main" /> }
-			falseComponent={ <NoSitesPurchasesMessage /> }
+			trueComponent={ sectionElt }
+			falseComponent={
+				<HasSitePurchasesSwitch
+					siteId={ siteId }
+					trueComponent={ sectionElt }
+					falseComponent={ <NoSitesPurchasesMessage /> }
+					loadingComponent={ <AdvancedCredentialsLoadingPlaceholder /> }
+				/>
+			}
 			loadingComponent={ <AdvancedCredentialsLoadingPlaceholder /> }
 		/>
 	);
