726051b99acfe99534afffbecdd3b70a7a52f2e7
Sergio Cinos <sergio.cinos@gmail.com>
2021-03-25 06:19:05 +0100

    build: Add profile info to webpack builds in CI (#51269)
    * Add support for WEBPACK_OPTIONS
    
    * Add WEBPACK_OPTIONS to ICFY
    
    * Add WEBPACK_OPTIONS to desktop build
    
    * Do not set WEBPACK_OPTIONS in package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,7 +22,6 @@ const calypsoColorSchemes = require( '@automattic/calypso-color-schemes/js' );
 const TranspileConfig = require( '@automattic/calypso-build/webpack/transpile' );
 const {
 	cssNameFromFilename,
-	IncrementalProgressPlugin,
 	shouldTranspileDependency,
 } = require( '@automattic/calypso-build/webpack/util' );
 const ExtensiveLodashReplacementPlugin = require( '@automattic/webpack-extensive-lodash-replacement-plugin' );
@@ -51,7 +50,6 @@ const shouldMinify =
 	process.env.MINIFY_JS === 'true' ||
 	( process.env.MINIFY_JS !== 'false' && bundleEnv === 'production' );
 const shouldEmitStats = process.env.EMIT_STATS && process.env.EMIT_STATS !== 'false';
-const shouldShowProgress = process.env.PROGRESS && process.env.PROGRESS !== 'false';
 const shouldEmitStatsWithReasons = process.env.EMIT_STATS === 'withreasons';
 const shouldCheckForDuplicatePackages = process.env.CHECK_DUPLICATE_PACKAGES === 'true';
 const shouldCheckForCycles = process.env.CHECK_CYCLES === 'true';
@@ -335,7 +333,6 @@ const webpackConfig = {
 					chunkGroups: true,
 				},
 			} ),
-		shouldShowProgress && new IncrementalProgressPlugin(),
 		new MomentTimezoneDataPlugin( {
 			startYear: 2000,
 			cacheDir: path.resolve( cachePath, 'moment-timezone' ),
