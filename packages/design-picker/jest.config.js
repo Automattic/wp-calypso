80aa17b0705acd9f78234a5089b48739b324a9f5
Marco Ciampini <marco.ciampini@automattic.com>
2021-03-26 12:09:39 +0100

    Add jest-canvas-mock

--- packages/design-picker/jest.config.js
+++ packages/design-picker/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
 	preset: '../../test/packages/jest-preset.js',
 	testEnvironment: 'jsdom',
 	testMatch: [ '<rootDir>/**/__tests__/**/*.[jt]s?(x)', '!**/.eslintrc.*' ],
-	setupFilesAfterEnv: [ '@testing-library/jest-dom/extend-expect' ],
+	setupFilesAfterEnv: [ '@testing-library/jest-dom/extend-expect', 'jest-canvas-mock' ],
 };
