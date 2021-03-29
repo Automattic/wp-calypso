6c30ab2a44764734566beeeb0878d73ab0c38132
Marin Atanasov <tyxla@abv.bg>
2021-03-29 13:47:37 +0300

    Preferences: Remove editorAdvancedVisible preference

--- client/state/preferences/constants.js
+++ client/state/preferences/constants.js
@@ -6,7 +6,6 @@ export const DEFAULT_PREFERENCE_VALUES = {
 	'guided-tours-history': [],
 	recentSites: [],
 	mediaScale: 0.157,
-	editorAdvancedVisible: false,
 	editorConfirmationDisabledSites: [],
 	colorScheme: 'default',
 	'store-dashboardStatsWidgetUnit': 'week',
--- client/state/preferences/schema.js
+++ client/state/preferences/schema.js
@@ -36,9 +36,6 @@ export const remoteValuesSchema = {
 			minimum: 0,
 			maximum: 1,
 		},
-		editorAdvancedVisible: {
-			type: 'boolean',
-		},
 		editorConfirmationDisabledSites: {
 			type: 'array',
 			items: { type: 'number' },
