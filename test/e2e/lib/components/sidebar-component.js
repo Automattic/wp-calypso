a17bbe010086e0428415251882b0b5f4fdfd396d
Bart <bartlomiej.kalisz@gmail.com>
2021-03-29 19:51:56 +0200

    Remove obsolete waiter

--- test/e2e/lib/components/sidebar-component.js
+++ test/e2e/lib/components/sidebar-component.js
@@ -166,10 +166,6 @@ export default class SidebarComponent extends AsyncBaseContainer {
 
 	async _scrollToAndClickMenuItem( target, { clickButton = false } = {} ) {
 		const selector = SidebarComponent._getSidebarSelector( target, { getButton: clickButton } );
-		await driverHelper.waitUntilLocatedAndVisible(
-			this.driver,
-			By.css( '.current-site__notices' )
-		);
 
 		if ( ! ( await driverHelper.isEventuallyPresentAndDisplayed( this.driver, selector, 500 ) ) ) {
 			const settingsSelector = SidebarComponent._getSidebarSelector( 'settings' );
