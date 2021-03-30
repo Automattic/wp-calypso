9d30a2dd567e019ad8e62dd658261cf15d0dbb51
Bart <bartlomiej.kalisz@gmail.com>
2021-03-30 18:05:12 +0200

    Fix username getter that was always silently failing

--- test/e2e/lib/pages/account/account-settings-page.js
+++ test/e2e/lib/pages/account/account-settings-page.js
@@ -7,8 +7,9 @@ import { By as by } from 'selenium-webdriver';
  * Internal dependencies
  */
 import * as driverHelper from '../../driver-helper.js';
-import AsyncBaseContainer from '../../async-base-container';
 import * as dataHelper from '../../data-helper';
+import AsyncBaseContainer from '../../async-base-container';
+import SidebarComponent from '../../components/sidebar-component';
 
 export default class AccountSettingsPage extends AsyncBaseContainer {
 	constructor( driver, url = dataHelper.getCalypsoURL( 'me/account' ) ) {
@@ -22,12 +23,12 @@ export default class AccountSettingsPage extends AsyncBaseContainer {
 	}
 
 	async getUsername() {
-		await driverHelper.waitUntilLocatedAndVisible(
+		await SidebarComponent.Expect( this.driver );
+		const displayName = await driverHelper.waitUntilLocatedAndVisible(
 			this.driver,
 			by.css( '.profile-gravatar__user-display-name' )
 		);
-		return await this.driver
-			.findElement( by.css( '.profile-gravatar__user-display-name' ) )
-			.getText();
+
+		return displayName.getText();
 	}
 }
