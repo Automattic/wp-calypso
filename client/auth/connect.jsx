84b5c1d1ad173027164bb147a9a29b7a94d431e3
Jarda Snajdr <jsnajdr@gmail.com>
2021-03-30 09:51:54 +0200

    Authorize screen: remove links to developer resources, convert to FC

--- client/auth/connect.jsx
+++ client/auth/connect.jsx
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import { localize } from 'i18n-calypso';
+import { useTranslate } from 'i18n-calypso';
 import Gridicon from 'calypso/components/gridicon';
 
 /**
@@ -15,55 +15,46 @@ import Main from 'calypso/components/main';
 import Notice from 'calypso/components/notice';
 import NoticeAction from 'calypso/components/notice/notice-action';
 import WordPressLogo from 'calypso/components/wordpress-logo';
+import { localizeUrl } from 'calypso/lib/i18n-utils';
 
-class Connect extends React.Component {
-	getCreateAccountUrl = () => {
-		return config.isEnabled( 'devdocs' )
-			? 'https://wordpress.com/start/developer'
-			: 'https://wordpress.com/start';
-	};
+export default function Connect( { authUrl } ) {
+	const translate = useTranslate();
 
-	render() {
-		return (
-			<Main className="auth auth__connect">
-				<WordPressLogo size={ 72 } />
-				{ ! config( 'oauth_client_id' ) ? (
-					<Notice
-						status="is-warning"
-						text={ this.props.translate(
-							'You need to set `oauth_client_id` in your config file.'
-						) }
-						showDismiss={ false }
-					>
-						<NoticeAction href="https://developer.wordpress.com/apps/">
-							{ this.props.translate( 'Go to Developer Resources' ) }
-						</NoticeAction>
-					</Notice>
-				) : (
-					<div className="auth__connect-intro">
-						<p className="auth__welcome">
-							{ this.props.translate(
-								'Welcome to Calypso. Authorize the application with your WordPress.com credentials to get started.'
-							) }
-						</p>
-						<Button href={ this.props.authUrl }>{ this.props.translate( 'Authorize App' ) }</Button>
-					</div>
-				) }
-				<a
-					className="auth__help"
-					target="_blank"
-					rel="noopener noreferrer"
-					title={ this.props.translate( 'Visit the Calypso GitHub repository for help' ) }
-					href="https://github.com/Automattic/wp-calypso"
+	return (
+		<Main className="auth auth__connect">
+			<WordPressLogo size={ 72 } />
+			{ ! config( 'oauth_client_id' ) ? (
+				<Notice
+					status="is-warning"
+					text={ translate( 'You need to set `oauth_client_id` in your config file.' ) }
+					showDismiss={ false }
 				>
-					<Gridicon icon="help" />
-				</a>
-				<div className="auth__links">
-					<a href={ this.getCreateAccountUrl() }>{ this.props.translate( 'Create account' ) }</a>
+					<NoticeAction href="https://developer.wordpress.com/apps/">
+						{ translate( 'Go to Developer Resources' ) }
+					</NoticeAction>
+				</Notice>
+			) : (
+				<div className="auth__connect-intro">
+					<p className="auth__welcome">
+						{ translate(
+							'Welcome to Calypso. Authorize the application with your WordPress.com credentials to get started.'
+						) }
+					</p>
+					<Button href={ authUrl }>{ translate( 'Authorize App' ) }</Button>
 				</div>
-			</Main>
-		);
-	}
+			) }
+			<a
+				className="auth__help"
+				target="_blank"
+				rel="noopener noreferrer"
+				title={ translate( 'Visit the WordPress.com support site for help' ) }
+				href={ localizeUrl( 'https://wordpress.com/support/' ) }
+			>
+				<Gridicon icon="help" />
+			</a>
+			<div className="auth__links">
+				<a href="/start">{ translate( 'Create account' ) }</a>
+			</div>
+		</Main>
+	);
 }
-
-export default localize( Connect );
