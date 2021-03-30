ecf9f5ca857f3623dd120defb01e96f425a3e886
Yan Sern <yan.sern@automattic.com>
2021-03-31 02:49:04 +0300

    Bump.

--- client/landing/gutenboarding/onboarding-block/edit.tsx
+++ client/landing/gutenboarding/onboarding-block/edit.tsx
@@ -112,7 +112,7 @@ const OnboardingEdit: React.FunctionComponent< BlockEditProps< Attributes > > = 
 		if ( newSiteError ) {
 			// Temporarily capture error related to new site creation to E2E
 			if ( isE2ETest() ) {
-				document.body.innerHTML = JSON.stringify( newSiteError );
+				document.body.innerHTML = '2:' + JSON.stringify( newSiteError );
 				return;
 				// throw new Error( `onboarding-debug ${ JSON.stringify( newSiteError ) }` );
 			}
