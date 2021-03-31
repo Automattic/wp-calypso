461e260e87dabf63e5702adc1311709a4d9056e3
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-03-31 11:26:39 +0400

    Disable eslint for click event error and fix a couple of eslint errors

--- client/components/domains/reskin-side-explainer/index.jsx
+++ client/components/domains/reskin-side-explainer/index.jsx
@@ -13,15 +13,22 @@ class ReskinSideExplainer extends React.Component {
 		const { title, subtitle, ctaText } = this.props;
 
 		return (
+			/* eslint-disable jsx-a11y/click-events-have-key-events */
 			<div className="reskin-side-explainer">
 				<div className="reskin-side-explainer__title">{ title }</div>
 				<div className="reskin-side-explainer__subtitle">{ subtitle }</div>
 				<div className="reskin-side-explainer__cta">
-					<span className="cta-text" onClick={ this.props.onClick }>
+					<span
+						className="reskin-side-explainer__cta-text"
+						role="button"
+						onClick={ this.props.onClick }
+						tabindex="0"
+					>
 						{ ctaText }
 					</span>
 				</div>
 			</div>
+			/* eslint-enable jsx-a11y/click-events-have-key-events */
 		);
 	}
 }
