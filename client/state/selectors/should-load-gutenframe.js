d71122d16887e0984f4298513cb3c76d65ea434a
Paul Bunkham <paul@dobit.co.uk>
2021-04-01 09:19:31 +0100

    Remove unnecessary call to shouldLoadGutenframe

--- client/state/selectors/should-load-gutenframe.js
+++ client/state/selectors/should-load-gutenframe.js
@@ -2,14 +2,12 @@
  * Internal dependencies
  */
 import { isEligibleForGutenframe } from 'calypso/state/gutenberg-iframe-eligible/is-eligible-for-gutenframe';
-import { shouldCalypsoifyJetpack } from 'calypso/state/selectors/should-calypsoify-jetpack';
 import isNavUnificationEnabled from 'calypso/state/selectors/is-nav-unification-enabled';
 import getUserSettings from 'calypso/state/selectors/get-user-settings';
 
 export const shouldLoadGutenframe = ( state, siteId ) => {
 	return (
 		isEligibleForGutenframe( state, siteId ) &&
-		shouldCalypsoifyJetpack( state, siteId ) &&
 		( ! isNavUnificationEnabled( state ) ||
 			! getUserSettings( state )?.calypso_preferences?.linkDestination )
 	);
