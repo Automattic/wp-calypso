989d71004b6aad9265a4c98e8945f84924209ff4
Marin Atanasov <tyxla@abv.bg>
2021-04-01 11:40:46 +0300

    Improve not()

--- client/layout/guided-tours/utils.js
+++ client/layout/guided-tours/utils.js
@@ -2,8 +2,6 @@ const noop = () => {};
 
 const and = ( ...conditions ) => () => conditions.every( ( cond ) => cond() );
 
-const not = function ( func ) {
-	return ! func.apply( null, arguments );
-};
+const not = ( fn ) => ( ...args ) => ! fn( ...args );
 
 export { and, not, noop };
