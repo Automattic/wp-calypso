1004e91e5076464ac241f1d9a61d9cf2990c3bb6
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-04-01 04:46:35 +0400

    Refactor: the strings for the sidebar are now stored in the component to make it more reusable

--- client/components/domains/reskin-side-explainer/index.jsx
+++ client/components/domains/reskin-side-explainer/index.jsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React from 'react';
+import { localize } from 'i18n-calypso';
 
 /**
  * Style dependencies
@@ -9,8 +10,41 @@ import React from 'react';
 import './style.scss';
 
 class ReskinSideExplainer extends React.Component {
+	getStrings() {
+		const { type, translate } = this.props;
+
+		let title;
+		let subtitle;
+		let ctaText;
+
+		switch ( type ) {
+			case 'free-domain-explainer':
+				title = translate(
+					'Get a {{b}}free{{/b}} one-year domain registration with any paid annual plan.',
+					{
+						components: { b: <strong /> },
+					}
+				);
+				subtitle = translate(
+					"You can claim your free custom domain later if you aren't ready yet."
+				);
+				ctaText = translate( 'View plans' );
+				break;
+
+			case 'use-your-domain':
+				title = translate( 'Already own a domain?' );
+				subtitle = translate(
+					'Connect your domain purchased elsewhere to your WordPress.com site through mapping or transfer.'
+				);
+				ctaText = translate( 'Use a domain I own' );
+				break;
+		}
+
+		return { title, subtitle, ctaText };
+	}
+
 	render() {
-		const { title, subtitle, ctaText } = this.props;
+		const { title, subtitle, ctaText } = this.getStrings();
 
 		return (
 			/* eslint-disable jsx-a11y/click-events-have-key-events */
@@ -22,7 +56,7 @@ class ReskinSideExplainer extends React.Component {
 						className="reskin-side-explainer__cta-text"
 						role="button"
 						onClick={ this.props.onClick }
-						tabindex="0"
+						tabIndex="0"
 					>
 						{ ctaText }
 					</span>
@@ -33,4 +67,4 @@ class ReskinSideExplainer extends React.Component {
 	}
 }
 
-export default ReskinSideExplainer;
+export default localize( ReskinSideExplainer );
