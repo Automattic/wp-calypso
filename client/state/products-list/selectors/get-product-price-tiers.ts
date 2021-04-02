b1260a4e76cbfcdafff1b65d276e6ff80080e0ea
Payton Swick <payton@foolord.com>
2021-04-02 17:42:07 -0400

    Add getProductPriceTierList selector

--- client/state/products-list/selectors/get-product-price-tiers.ts
+++ client/state/products-list/selectors/get-product-price-tiers.ts
@@ -7,11 +7,31 @@ import 'calypso/state/products-list/init';
 import type { AppState } from 'calypso/types';
 
 type Product = {
+	/**
+	 * @deprecated use price_tier_list
+	 */
 	price_tiers: PriceTiers;
+	price_tier_list: PriceTierEntry[];
 };
 
+export interface PriceTierEntry {
+	minimum_units: number;
+	maximum_units?: undefined | null | number;
+	per_unit_fee: number;
+	flat_fee: number;
+	undiscounted_per_unit_fee?: undefined | null | number;
+	transform_quantity_divide_by?: undefined | null | number;
+	transform_quantity_round?: undefined | null | 'up' | 'down';
+}
+
+/**
+ * @deprecated use PriceTierEntry
+ */
 export type PriceTiers = Record< string, PriceTier >;
 
+/**
+ * @deprecated use PriceTierEntry
+ */
 export type PriceTier =
 	| {
 			flat_price: number;
@@ -23,6 +43,8 @@ export type PriceTier =
 
 /**
  * Returns the price tiers of the specified product.
+ *
+ * @deprecated Use getProductPriceTierList
  *
  * @param {object} state - global state tree
  * @param {string} productSlug - internal product slug, eg 'jetpack_premium'
@@ -42,3 +64,20 @@ export function getProductPriceTiers( state: AppState, productSlug?: string ): P
 }
 
 export default getProductPriceTiers;
+
+/**
+ * Returns the price tiers of the specified product.
+ *
+ * @param {object} state - global state tree
+ * @param {string} productSlug - internal product slug, eg 'jetpack_premium'
+ * @returns {PriceTierEntry[]} The price tiers.
+ */
+export function getProductPriceTierList( state: AppState, productSlug: string ): PriceTierEntry[] {
+	const product = getProductBySlug( state, productSlug ) as Product;
+
+	if ( ! product || ! product.price_tier_list ) {
+		return [];
+	}
+
+	return product.price_tier_list;
+}
