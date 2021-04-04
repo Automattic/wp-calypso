f927f62e329f66b060b4e364323f9a55eb55a672
Payton Swick <payton@foolord.com>
2021-04-04 18:01:41 -0400

    Move styled wrapper to wpcom-checkout

--- packages/wpcom-checkout/src/index.ts
+++ packages/wpcom-checkout/src/index.ts
@@ -9,3 +9,4 @@ export { useDisplayCartMessages };
 export { createApplePayMethod } from './payment-methods/apple-pay';
 export * from './postal-code';
 export { default as Field } from './field';
+export { default as styled } from './styled';
