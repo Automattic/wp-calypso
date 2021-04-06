2288bedff9702bce17fa487ade3fe4a9af04e878
Grant Kinney <grant.kinney@automattic.com>
2021-04-05 21:24:34 -0500

    Use a default value for library

--- apps/editing-toolkit/webpack.config.js
+++ apps/editing-toolkit/webpack.config.js
@@ -72,7 +72,7 @@ function getWebpackConfig( env = { source: '' }, argv = {} ) {
 			// Don't output a global variable to access plugin js, as this isn't a library
 			// Also the calypso-build default value of `window` can cause errors in JS loaded by Core
 			// See https://github.com/Automattic/wp-calypso/pull/51700
-			library: undefined,
+			library: { name: 'EditingToolkit', type: 'var' },
 		},
 		optimization: {
 			...webpackConfig.optimization,
