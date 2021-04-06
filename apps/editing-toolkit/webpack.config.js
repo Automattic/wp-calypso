2be841f676207ae5e91b1fb5148ad9d8053cf9c9
Grant Kinney <grant.kinney@automattic.com>
2021-04-06 10:33:03 -0500

    Specify only library name

--- apps/editing-toolkit/webpack.config.js
+++ apps/editing-toolkit/webpack.config.js
@@ -69,10 +69,7 @@ function getWebpackConfig( env = { source: '' }, argv = {} ) {
 			...webpackConfig.output,
 			path: outputPath,
 			filename: '[name].js', // dynamic filename
-			// Use a global variable, rather than window for outputing the compiled JS.
-			// The calypso-build default value of `window` can cause errors in JS loaded by Core
-			// See https://github.com/Automattic/wp-calypso/pull/51700
-			library: { name: 'EditingToolkit', type: 'var' },
+			library: 'EditingToolkit',
 		},
 		optimization: {
 			...webpackConfig.optimization,
