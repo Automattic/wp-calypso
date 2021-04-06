3d6bab777d7bcb824e48a53425df526e3900af6d
Payton Swick <payton@foolord.com>
2021-04-06 19:29:48 -0400

    Move SignupValidationResponse to wpcom-checkout

--- packages/wpcom-checkout/src/types.ts
+++ packages/wpcom-checkout/src/types.ts
@@ -368,3 +368,19 @@ export type ManagedContactDetailsUpdaters = {
 		arg1: PossiblyCompleteDomainContactDetails
 	) => ManagedContactDetails;
 };
+
+/**
+ * Request parameter expected by the domain contact validation endpoint.
+ *
+ * @see WPCOM_JSON_API_Signups_Validation_User_Endpoint
+ */
+export type SignupValidationResponse = {
+	success: boolean;
+	messages: {
+		first_name?: string[];
+		last_name?: string[];
+		email?: Record< string, string >;
+		username?: string[];
+		password?: string[];
+	};
+};
