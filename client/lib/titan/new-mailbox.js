96dddab82f78f448b32e8ffe2ccb87300d22b762
Seni Oluwunmi <277661+olaseni@users.noreply.github.com>
2021-04-06 15:14:36 +0100

    Remove unnecessary export

--- client/lib/titan/new-mailbox.js
+++ client/lib/titan/new-mailbox.js
@@ -303,7 +303,6 @@ export {
 	areAllMailboxesAvailable,
 	areAllMailboxesValid,
 	buildNewTitanMailbox,
-	checkEmailAvailability,
 	getMailboxPropTypeShape,
 	sanitizeEmailSuggestion,
 	transformMailboxForCart,
