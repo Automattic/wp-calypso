921e125f92cc3cc081ac923f6b53f82923d7b68b
Kev <kevin.zoschke@automattic.com>
2021-04-07 09:11:08 -0400

    Remove Security Realtime experiment (#51721)

--- client/my-sites/plans/jetpack-plans/product-grid/products-order.ts
+++ client/my-sites/plans/jetpack-plans/product-grid/products-order.ts
@@ -1,7 +1,6 @@
 /**
  * Internal dependencies
  */
-import { getForCurrentCROIteration, Iterations } from '../iterations';
 import {
 	PLAN_JETPACK_SECURITY_DAILY,
 	PLAN_JETPACK_SECURITY_DAILY_MONTHLY,
@@ -45,27 +44,5 @@ const PRODUCT_POSITION_IN_GRID: Record< string, number > = {
 	...setProductsInPosition( JETPACK_CRM_FREE_PRODUCTS, 80 ),
 };
 
-const PRODUCT_POSITION_IN_GRID_SECURITY: Record< string, number > = {
-	[ PRODUCT_JETPACK_BACKUP_DAILY ]: 1,
-	[ PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY ]: 1,
-	[ PLAN_JETPACK_SECURITY_DAILY ]: 10,
-	[ PLAN_JETPACK_SECURITY_DAILY_MONTHLY ]: 10,
-	[ PLAN_JETPACK_SECURITY_REALTIME ]: 20,
-	[ PLAN_JETPACK_SECURITY_REALTIME_MONTHLY ]: 20,
-	...setProductsInPosition( JETPACK_COMPLETE_PLANS, 30 ),
-	[ PRODUCT_JETPACK_BACKUP_REALTIME ]: 40,
-	[ PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY ]: 40,
-	...setProductsInPosition( JETPACK_SCAN_PRODUCTS, 50 ),
-	...setProductsInPosition( JETPACK_ANTI_SPAM_PRODUCTS, 60 ),
-	...setProductsInPosition( JETPACK_SEARCH_PRODUCTS, 70 ),
-	...setProductsInPosition( JETPACK_CRM_FREE_PRODUCTS, 80 ),
-};
-
-export const getProductPosition = ( slug: JetpackPlanSlugs | JetpackProductSlug ): number => {
-	const positions =
-		getForCurrentCROIteration( {
-			[ Iterations.SECURITY ]: PRODUCT_POSITION_IN_GRID_SECURITY,
-		} ) || PRODUCT_POSITION_IN_GRID;
-
-	return positions[ slug ] ?? 100;
-};
+export const getProductPosition = ( slug: JetpackPlanSlugs | JetpackProductSlug ): number =>
+	PRODUCT_POSITION_IN_GRID[ slug ] ?? 100;
