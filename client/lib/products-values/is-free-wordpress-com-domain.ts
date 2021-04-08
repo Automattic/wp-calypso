19b0b55ca6030b8f7ecbbefa457666624f798584
Payton Swick <payton@foolord.com>
2021-04-07 20:38:17 -0400

    Convert isFreeWordPressComDomain to TS

--- client/lib/products-values/is-free-wordpress-com-domain.ts
+++ client/lib/products-values/is-free-wordpress-com-domain.ts
@@ -1,11 +1,10 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isFreeWordPressComDomain( product ) {
+export function isFreeWordPressComDomain( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
 	return product.is_free === true;
 }
