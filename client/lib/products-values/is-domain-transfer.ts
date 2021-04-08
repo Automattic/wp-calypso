30183cd7802ee5adc1390b4b82f0f55551692832
Payton Swick <payton@foolord.com>
2021-04-07 20:33:54 -0400

    Convert isDomainTransfer to TS

--- client/lib/products-values/is-domain-transfer.ts
+++ client/lib/products-values/is-domain-transfer.ts
@@ -1,13 +1,11 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
 import { domainProductSlugs } from 'calypso/lib/domains/constants';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isDomainTransfer( product ) {
+export function isDomainTransfer( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return product.product_slug === domainProductSlugs.TRANSFER_IN;
 }
