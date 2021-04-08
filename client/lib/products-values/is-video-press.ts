490b0974f63c9b6cc818bdb3716975ffa6e49913
Payton Swick <payton@foolord.com>
2021-04-07 21:10:25 -0400

    Convert isVideoPress to TS

--- client/lib/products-values/is-video-press.ts
+++ client/lib/products-values/is-video-press.ts
@@ -1,12 +1,10 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isVideoPress( product ) {
+export function isVideoPress( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return 'videopress' === product.product_slug;
 }
