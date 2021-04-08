c594f6e617c1e11bf2ed2dc3ae5176e115b0aa00
Payton Swick <payton@foolord.com>
2021-04-07 20:31:30 -0400

    Convert isDomainProduct to TS

--- client/lib/products-values/is-domain-product.ts
+++ client/lib/products-values/is-domain-product.ts
@@ -1,14 +1,12 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
 import { isDomainMapping } from 'calypso/lib/products-values/is-domain-mapping';
 import { isDomainRegistration } from 'calypso/lib/products-values/is-domain-registration';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isDomainProduct( product ) {
+export function isDomainProduct( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return isDomainMapping( product ) || isDomainRegistration( product );
 }
