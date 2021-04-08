6153328dd84e2024f966e4bc75172008038f04b5
Payton Swick <payton@foolord.com>
2021-04-07 20:32:02 -0400

    Convert isDomainRedemption to TS

--- client/lib/products-values/is-domain-redemption.ts
+++ client/lib/products-values/is-domain-redemption.ts
@@ -1,12 +1,10 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isDomainRedemption( product ) {
+export function isDomainRedemption( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return product.product_slug === 'domain_redemption';
 }
