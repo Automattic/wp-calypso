29486101898348791125d6c8bac916247795266c
Payton Swick <payton@foolord.com>
2021-04-07 20:20:49 -0400

    Convert isCustomDesign to TS

--- client/lib/products-values/is-custom-design.ts
+++ client/lib/products-values/is-custom-design.ts
@@ -1,12 +1,10 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isCustomDesign( product ) {
+export function isCustomDesign( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return 'custom-design' === product.product_slug;
 }
