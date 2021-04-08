b95d0220d0226892fe02a807bdc6725e96cc50c2
Payton Swick <payton@foolord.com>
2021-04-07 21:04:17 -0400

    Convert isPersonal to TS

--- client/lib/products-values/is-personal.ts
+++ client/lib/products-values/is-personal.ts
@@ -2,12 +2,10 @@
  * Internal dependencies
  */
 import { isPersonalPlan } from 'calypso/lib/plans';
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isPersonal( product ) {
+export function isPersonal( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return isPersonalPlan( product.product_slug );
 }
