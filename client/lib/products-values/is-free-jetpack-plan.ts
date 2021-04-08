ff2d1266487abf56f1008acd1f0dcc05e920664e
Payton Swick <payton@foolord.com>
2021-04-07 20:36:16 -0400

    Convert isFreeJetpackPlan to TS

--- client/lib/products-values/is-free-jetpack-plan.ts
+++ client/lib/products-values/is-free-jetpack-plan.ts
@@ -2,12 +2,10 @@
  * Internal dependencies
  */
 import { PLAN_JETPACK_FREE } from 'calypso/lib/plans/constants';
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isFreeJetpackPlan( product ) {
+export function isFreeJetpackPlan( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return product.product_slug === PLAN_JETPACK_FREE;
 }
