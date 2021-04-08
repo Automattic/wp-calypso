4695a9fed586f4f3dd87aeebe5261daf7458e09d
Payton Swick <payton@foolord.com>
2021-04-07 21:15:37 -0400

    Convert getDomainProductRanking to TS

--- client/lib/products-values/get-domain-product-ranking.ts
+++ client/lib/products-values/get-domain-product-ranking.ts
@@ -1,15 +1,15 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
 import { isDomainRegistration } from 'calypso/lib/products-values/is-domain-registration';
 import { isDomainMapping } from 'calypso/lib/products-values/is-domain-mapping';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function getDomainProductRanking( product ) {
+export function getDomainProductRanking(
+	product: FormattedProduct | CamelCaseProduct
+): number | undefined {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	if ( isDomainRegistration( product ) ) {
 		return 0;
 	} else if ( isDomainMapping( product ) ) {
