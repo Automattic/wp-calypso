caedb29afff6f9233f264b541258d3a1af43efa6
Payton Swick <payton@foolord.com>
2021-04-07 20:54:50 -0400

    Convert isJetpackScan to TS

--- client/lib/products-values/is-jetpack-scan.ts
+++ client/lib/products-values/is-jetpack-scan.ts
@@ -1,13 +1,11 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
 import { isJetpackScanSlug } from 'calypso/lib/products-values/is-jetpack-scan-slug';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isJetpackScan( product ) {
+export function isJetpackScan( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return isJetpackScanSlug( product.product_slug );
 }
