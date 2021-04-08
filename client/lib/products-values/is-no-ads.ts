68bf57e865c9f815546287e48746de0f1cf3c044
Payton Swick <payton@foolord.com>
2021-04-07 21:02:44 -0400

    Convert isNoAds to TS

--- client/lib/products-values/is-no-ads.ts
+++ client/lib/products-values/is-no-ads.ts
@@ -1,12 +1,10 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isNoAds( product ) {
+export function isNoAds( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return 'no-adverts/no-adverts.php' === product.product_slug;
 }
