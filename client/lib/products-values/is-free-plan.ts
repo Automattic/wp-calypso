69b22356aa466d5f3b344b3eeaeb4574507e9f07
Payton Swick <payton@foolord.com>
2021-04-07 20:36:43 -0400

    Convert isFreePlan to TS

--- client/lib/products-values/is-free-plan.ts
+++ client/lib/products-values/is-free-plan.ts
@@ -2,12 +2,10 @@
  * Internal dependencies
  */
 import { PLAN_FREE } from 'calypso/lib/plans/constants';
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isFreePlan( product ) {
+export function isFreePlan( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return product.product_slug === PLAN_FREE;
 }
