02c7337a39fc6c802e59fdb70f487bd2ef098670
Payton Swick <payton@foolord.com>
2021-04-07 20:47:16 -0400

    Convert isJetpackBackup to TS

--- client/lib/products-values/is-jetpack-backup.ts
+++ client/lib/products-values/is-jetpack-backup.ts
@@ -1,13 +1,11 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
 import { isJetpackBackupSlug } from 'calypso/lib/products-values/is-jetpack-backup-slug';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isJetpackBackup( product ) {
+export function isJetpackBackup( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return isJetpackBackupSlug( product.product_slug );
 }
