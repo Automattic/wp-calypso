efce494b7881069c63e7001c4076205a21686ba1
Payton Swick <payton@foolord.com>
2021-04-07 20:49:36 -0400

    Convert isJetpackPlanSlug to TS

--- client/lib/products-values/is-jetpack-plan-slug.ts
+++ client/lib/products-values/is-jetpack-plan-slug.ts
@@ -4,6 +4,6 @@
 import { GROUP_JETPACK } from 'calypso/lib/plans/constants';
 import { planMatches } from 'calypso/lib/plans';
 
-export function isJetpackPlanSlug( productSlug ) {
+export function isJetpackPlanSlug( productSlug: string ): boolean {
 	return planMatches( productSlug, { group: GROUP_JETPACK } );
 }
