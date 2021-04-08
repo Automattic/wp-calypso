f407f700f776a77bfdf7c4645b1c9eb8f5f67df9
Payton Swick <payton@foolord.com>
2021-04-07 20:35:43 -0400

    Convert isEnterprise to TS

--- client/lib/products-values/is-enterprise.ts
+++ client/lib/products-values/is-enterprise.ts
@@ -2,12 +2,10 @@
  * Internal dependencies
  */
 import { PLAN_WPCOM_ENTERPRISE } from 'calypso/lib/plans/constants';
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isEnterprise( product ) {
+export function isEnterprise( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return product.product_slug === PLAN_WPCOM_ENTERPRISE;
 }
