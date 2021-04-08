372ba2f13ed1707781dbc9eebb0009d28fde4b72
Payton Swick <payton@foolord.com>
2021-04-07 20:19:58 -0400

    Convert isCredits to TS

--- client/lib/products-values/is-credits.ts
+++ client/lib/products-values/is-credits.ts
@@ -1,12 +1,10 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isCredits( product ) {
+export function isCredits( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return 'wordpress-com-credits' === product.product_slug;
 }
