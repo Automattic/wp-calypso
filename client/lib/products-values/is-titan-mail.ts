315c5bcdcfa077df976c66a02cfbe8bf8ff7a6d5
Payton Swick <payton@foolord.com>
2021-04-07 21:08:39 -0400

    Convert isTitanMail to TS

--- client/lib/products-values/is-titan-mail.ts
+++ client/lib/products-values/is-titan-mail.ts
@@ -1,13 +1,11 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
 import { TITAN_MAIL_MONTHLY_SLUG } from 'calypso/lib/titan/constants';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isTitanMail( product ) {
+export function isTitanMail( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return product.product_slug === TITAN_MAIL_MONTHLY_SLUG;
 }
