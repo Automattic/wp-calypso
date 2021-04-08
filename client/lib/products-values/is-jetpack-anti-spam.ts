26d7bf05fd24767ecf967232daa3fe9071453afe
Payton Swick <payton@foolord.com>
2021-04-07 20:45:18 -0400

    Convert isJetpackAntiSpam to TS

--- client/lib/products-values/is-jetpack-anti-spam.ts
+++ client/lib/products-values/is-jetpack-anti-spam.ts
@@ -1,13 +1,11 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
 import { isJetpackAntiSpamSlug } from 'calypso/lib/products-values/is-jetpack-anti-spam-slug';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isJetpackAntiSpam( product ) {
+export function isJetpackAntiSpam( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return isJetpackAntiSpamSlug( product.product_slug );
 }
