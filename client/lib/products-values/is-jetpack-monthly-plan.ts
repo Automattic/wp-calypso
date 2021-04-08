9bd69b12b1799b15afedd694479d9dbe92c6bcba
Payton Swick <payton@foolord.com>
2021-04-07 20:49:00 -0400

    Convert isJetpackMonthlyPlan to TS

--- client/lib/products-values/is-jetpack-monthly-plan.ts
+++ client/lib/products-values/is-jetpack-monthly-plan.ts
@@ -3,7 +3,8 @@
  */
 import { isJetpackPlan } from 'calypso/lib/products-values/is-jetpack-plan';
 import { isMonthly } from 'calypso/lib/products-values/is-monthly';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isJetpackMonthlyPlan( product ) {
+export function isJetpackMonthlyPlan( product: FormattedProduct | CamelCaseProduct ): boolean {
 	return isMonthly( product ) && isJetpackPlan( product );
 }
