ac07137bc6a0fbc31934a51c95890e16d18d8b7a
Payton Swick <payton@foolord.com>
2021-04-07 21:08:11 -0400

    Convert isTheme to TS

--- client/lib/products-values/is-theme.ts
+++ client/lib/products-values/is-theme.ts
@@ -1,12 +1,10 @@
 /**
  * Internal dependencies
  */
-import { assertValidProduct } from 'calypso/lib/products-values/utils/assert-valid-product';
 import { formatProduct } from 'calypso/lib/products-values/format-product';
+import type { FormattedProduct, CamelCaseProduct } from './types';
 
-export function isTheme( product ) {
+export function isTheme( product: FormattedProduct | CamelCaseProduct ): boolean {
 	product = formatProduct( product );
-	assertValidProduct( product );
-
 	return 'premium_theme' === product.product_slug;
 }
