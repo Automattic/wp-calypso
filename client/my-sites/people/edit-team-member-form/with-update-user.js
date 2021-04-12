45e9b9734bce1eabfd576fd7a436ad9ee185c58f
Markus Dobmann <m.do@posteo.net>
2021-04-12 08:39:09 +0200

    withUpdateUser: use non-destructive effect pattern for notices
    huge thanks to @jsnajdr

--- client/my-sites/people/edit-team-member-form/with-update-user.js
+++ client/my-sites/people/edit-team-member-form/with-update-user.js
@@ -12,44 +12,50 @@ import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 import { useTranslate } from 'i18n-calypso';
 
 const useSuccessNotice = ( isSuccess, user ) => {
+	const showNotice = React.useRef();
 	const dispatch = useDispatch();
 	const translate = useTranslate();
 
 	React.useEffect( () => {
-		isSuccess &&
+		showNotice.current = () => {
 			dispatch(
 				successNotice(
 					translate( 'Successfully updated @%(user)s', {
-						args: { user: user?.login },
+						args: { user: user.login },
 						context: 'Success message after a user has been modified.',
 					} ),
 					{ id: 'update-user-notice' }
 				)
 			);
-		// Note: We only want to run this effect in case `isSuccess`
-		// changes and ignore changes to other deps like `user`.
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+		};
+	}, [ dispatch, translate, user.login ] );
+
+	React.useEffect( () => {
+		isSuccess && showNotice.current();
 	}, [ isSuccess ] );
 };
 
 const useErrorNotice = ( isError, user ) => {
+	const showNotice = React.useRef();
 	const dispatch = useDispatch();
 	const translate = useTranslate();
 
 	React.useEffect( () => {
-		isError &&
+		showNotice.current = () => {
 			dispatch(
 				errorNotice(
 					translate( 'There was an error updating @%(user)s', {
-						args: { user: user?.login },
+						args: { user: user.login },
 						context: 'Error message after A site has failed to perform actions on a user.',
 					} ),
 					{ id: 'update-user-notice' }
 				)
 			);
-		// Note: We only want to run this effect in case `isError`
-		// changes and ignore changes to other deps like `user`.
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+		};
+	}, [ dispatch, translate, user.login ] );
+
+	React.useEffect( () => {
+		isError && showNotice.current();
 	}, [ isError ] );
 };
 
