0902d637e4cd6105f73d5fca626e449d5d859291
Payton Swick <payton@automattic.com>
2021-04-12 11:25:43 -0400

    Checkout: Move Apple Pay payment method to wpcom-checkout (#51667)
    * Move Apple Pay payment method to calypso
    
    * Update createApplePayMethod import in checkout
    
    * Remove applePayMethod from checkout demo
    
    * Remove createApplePayMethod from composite-checkout API
    
    * Remove createApplePayMethod from composite-checkout README
    
    * Move StripePaymentRequest type to calypso-stripe
    
    * Fix type imports for StripePaymentRequest
    
    * Convert createApplePayMethod to TypeScript
    
    * Move createApplePayMethod to wpcom-checkout package
    
    * Add new deps to wpcom-checkout for createApplePayMethod
    
    * Update import of createApplePayMethod to wpcom-checkout
    
    * Clarify existing types when calling createApplePayMethod
    
    stripe and stripeConfiguration already exist if
    shouldCreateApplePayMethod is truthy, but this convinces TypeScript.
    
    * Add PaymentMethodLogos to wpcom-checkout

--- packages/wpcom-checkout/src/index.ts
+++ packages/wpcom-checkout/src/index.ts
@@ -5,3 +5,4 @@ import useDisplayCartMessages from './use-display-cart-messages';
 
 export * from './transformations';
 export { useDisplayCartMessages };
+export { createApplePayMethod } from './payment-methods/apple-pay';
