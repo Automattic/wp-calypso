db3ff57235430e7613ca53a1d254d6775fcdecd3
Markus Dobmann <m.do@posteo.net>
2021-04-12 09:38:07 +0200

    merge error notice callbacks

--- client/my-sites/people/delete-user/with-delete-user.js
+++ client/my-sites/people/delete-user/with-delete-user.js
@@ -43,61 +43,47 @@ const useSuccessNotice = ( isSuccess, user, isMultisite, siteSlug ) => {
 };
 
 const useErrorNotice = ( isError, user, isMultisite, error ) => {
-	const showDomainErrorNotice = React.useRef();
-	const showGeneralErrorNotice = React.useRef();
+	const showNotice = React.useRef();
 	const dispatch = useDispatch();
 	const translate = useTranslate();
 
 	React.useEffect( () => {
-		showDomainErrorNotice.current = ( domainError ) => {
-			const numDomains = domainError.message.split( ',' ).length;
-			dispatch(
-				errorNotice(
-					translate(
-						'@%(user)s owns following domain used on this site: {{strong}}%(domains)s{{/strong}}. This domain will have to be transferred to a different site, transferred to a different registrar, or canceled before removing or deleting @%(user)s.',
-						'@%(user)s owns following domains used on this site: {{strong}}%(domains)s{{/strong}}. These domains will have to be transferred to a different site, transferred to a different registrar, or canceled before removing or deleting @%(user)s.',
-						{
-							count: numDomains,
-							args: {
-								domains: domainError.message,
-								user: user.login,
-							},
-							components: {
-								strong: <strong />,
-							},
-						}
-					),
-					{ id: 'delete-user-notice' }
-				)
-			);
-		};
+		showNotice.current = ( errorObj ) => {
+			let message;
+			if ( 'user_owns_domain_subscription' === errorObj.error ) {
+				const numDomains = errorObj.message.split( ',' ).length;
+				message = translate(
+					'@%(user)s owns following domain used on this site: {{strong}}%(domains)s{{/strong}}. This domain will have to be transferred to a different site, transferred to a different registrar, or canceled before removing or deleting @%(user)s.',
+					'@%(user)s owns following domains used on this site: {{strong}}%(domains)s{{/strong}}. These domains will have to be transferred to a different site, transferred to a different registrar, or canceled before removing or deleting @%(user)s.',
+					{
+						count: numDomains,
+						args: {
+							domains: errorObj.message,
+							user: user.login,
+						},
+						components: {
+							strong: <strong />,
+						},
+					}
+				);
+			} else {
+				message = isMultisite
+					? translate( 'There was an error removing @%(user)s', {
+							args: { user: user.login },
+							context: 'Error message after A site has failed to perform actions on a user.',
+					  } )
+					: translate( 'There was an error deleting @%(user)s', {
+							args: { user: user.login },
+							context: 'Error message after A site has failed to perform actions on a user.',
+					  } );
+			}
 
-		showGeneralErrorNotice.current = () => {
-			dispatch(
-				errorNotice(
-					isMultisite
-						? translate( 'There was an error removing @%(user)s', {
-								args: { user: user.login },
-								context: 'Error message after A site has failed to perform actions on a user.',
-						  } )
-						: translate( 'There was an error deleting @%(user)s', {
-								args: { user: user.login },
-								context: 'Error message after A site has failed to perform actions on a user.',
-						  } ),
-					{ id: 'delete-user-notice' }
-				)
-			);
+			dispatch( errorNotice( message, { id: 'delete-user-notice' } ) );
 		};
 	}, [ dispatch, isMultisite, translate, user.login ] );
 
 	React.useEffect( () => {
-		if ( isError ) {
-			if ( 'user_owns_domain_subscription' === error.error ) {
-				showDomainErrorNotice.current( error );
-				return;
-			}
-			showGeneralErrorNotice.current();
-		}
+		isError && showNotice.current( error );
 	}, [ isError, error ] );
 };
 
