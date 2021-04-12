0cabbf1e6b6e794e49340205f5dbc9962f18d7f2
Markus Dobmann <m.do@posteo.net>
2021-04-12 08:45:15 +0200

    withDeleteUser: use non-destructive effect pattern for notices

--- client/my-sites/people/delete-user/with-delete-user.js
+++ client/my-sites/people/delete-user/with-delete-user.js
@@ -13,11 +13,12 @@ import useDeleteUserMutation from 'calypso/data/users/use-delete-user-mutation';
 import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 
 const useSuccessNotice = ( isSuccess, user, isMultisite, siteSlug ) => {
+	const showNotice = React.useRef();
 	const dispatch = useDispatch();
 	const translate = useTranslate();
 
 	React.useEffect( () => {
-		if ( isSuccess ) {
+		showNotice.current = () => {
 			dispatch(
 				successNotice(
 					isMultisite
@@ -33,43 +34,45 @@ const useSuccessNotice = ( isSuccess, user, isMultisite, siteSlug ) => {
 				)
 			);
 			page.redirect( `/people/team${ siteSlug ? `/${ siteSlug }` : '' }` );
-		}
-		// Note: We only want to run this effect in case `isSuccess`
-		// changes and ignore changes to other deps like `user`.
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+		};
+	} );
+
+	React.useEffect( () => {
+		isSuccess && showNotice.current();
 	}, [ isSuccess ] );
 };
 
 const useErrorNotice = ( isError, user, isMultisite, error ) => {
+	const showDomainErrorNotice = React.useRef();
+	const showGeneralErrorNotice = React.useRef();
 	const dispatch = useDispatch();
 	const translate = useTranslate();
 
 	React.useEffect( () => {
-		if ( isError ) {
-			if ( 'user_owns_domain_subscription' === error.error ) {
-				const numDomains = error.message.split( ',' ).length;
-				dispatch(
-					errorNotice(
-						translate(
-							'@%(user)s owns following domain used on this site: {{strong}}%(domains)s{{/strong}}. This domain will have to be transferred to a different site, transferred to a different registrar, or canceled before removing or deleting @%(user)s.',
-							'@%(user)s owns following domains used on this site: {{strong}}%(domains)s{{/strong}}. These domains will have to be transferred to a different site, transferred to a different registrar, or canceled before removing or deleting @%(user)s.',
-							{
-								count: numDomains,
-								args: {
-									domains: error.message,
-									user: user.login,
-								},
-								components: {
-									strong: <strong />,
-								},
-							}
-						),
-						{ id: 'delete-user-notice' }
-					)
-				);
-				return;
-			}
+		showDomainErrorNotice.current = ( domainError ) => {
+			const numDomains = domainError.message.split( ',' ).length;
+			dispatch(
+				errorNotice(
+					translate(
+						'@%(user)s owns following domain used on this site: {{strong}}%(domains)s{{/strong}}. This domain will have to be transferred to a different site, transferred to a different registrar, or canceled before removing or deleting @%(user)s.',
+						'@%(user)s owns following domains used on this site: {{strong}}%(domains)s{{/strong}}. These domains will have to be transferred to a different site, transferred to a different registrar, or canceled before removing or deleting @%(user)s.',
+						{
+							count: numDomains,
+							args: {
+								domains: domainError.message,
+								user: user.login,
+							},
+							components: {
+								strong: <strong />,
+							},
+						}
+					),
+					{ id: 'delete-user-notice' }
+				)
+			);
+		};
 
+		showGeneralErrorNotice.current = () => {
 			dispatch(
 				errorNotice(
 					isMultisite
@@ -84,11 +87,18 @@ const useErrorNotice = ( isError, user, isMultisite, error ) => {
 					{ id: 'delete-user-notice' }
 				)
 			);
+		};
+	}, [ dispatch, isMultisite, translate, user.login ] );
+
+	React.useEffect( () => {
+		if ( isError ) {
+			if ( 'user_owns_domain_subscription' === error.error ) {
+				showDomainErrorNotice.current( error );
+				return;
+			}
+			showGeneralErrorNotice.current();
 		}
-		// Note: We only want to run this effect in case `isError`
-		// changes and ignore changes to other deps like `user`.
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [ isError ] );
+	}, [ isError, error ] );
 };
 
 const withDeleteUser = ( Component ) => ( props ) => {
