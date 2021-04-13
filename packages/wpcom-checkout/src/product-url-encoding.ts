f8081c8d772ae9c562f8a22764ca6ac2b91ed494
Payton Swick <payton@foolord.com>
2021-04-13 19:12:37 -0400

    Add comment to mention that backend uses the same encoding

--- packages/wpcom-checkout/src/product-url-encoding.ts
+++ packages/wpcom-checkout/src/product-url-encoding.ts
@@ -4,6 +4,10 @@
 // slash (eg: /checkout/example.com/foo%2Fbar breaks routing). We cannot use
 // the same characters for encoding as for decoding if they are UTF-8 encoded
 // because they will already be decoded by the time the decoding takes place.
+//
+// If this is ever changed, please make sure to also change the code that
+// generates renewal emails on the backend, because it uses the same encoding!
+
 const slashForEncoding = '%25';
 const slashForDecoding = '%'; // WARNING: This will be used in a RegExp so it must not contain RegExp special characters unless they are escaped!
 
