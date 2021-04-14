159298d0a583e6d57a15ec7228be2ae268daa3b2
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-04-14 20:43:08 +0400

    New search icon for the input box

---
+++ client/components/search/search-icon.jsx
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { SVG, Path } from '@wordpress/components';
+
+const searchIcon = (
+	<SVG viewBox="0 0 32 32">
+		<Path
+			d="M13.8269 19.8984L8.49362 24.5651L7.50586 23.4362L12.8392 18.7695L13.8269 19.8984Z"
+			fill="#A7AAAD"
+		/>{ ' ' }
+		<path
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M17.9994 21.1667C21.221 21.1667 23.8327 18.555 23.8327 15.3333C23.8327 12.1117 21.221 9.5 17.9994 9.5C14.7777 9.5 12.166 12.1117 12.166 15.3333C12.166 18.555 14.7777 21.1667 17.9994 21.1667ZM17.9994 22.6667C22.0494 22.6667 25.3327 19.3834 25.3327 15.3333C25.3327 11.2832 22.0494 8 17.9994 8C13.9493 8 10.666 11.2832 10.666 15.3333C10.666 19.3834 13.9493 22.6667 17.9994 22.6667Z"
+		/>
+	</SVG>
+);
+
+export default searchIcon;
+{
+	/* <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" clip-rule="evenodd"  fill="#A7AAAD"/> </svg> */
+}
