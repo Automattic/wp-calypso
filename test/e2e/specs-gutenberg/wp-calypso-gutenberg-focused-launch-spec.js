0aad53db384cf1dcb3a72ef6dbfc8691ff148134
Yan Sern <yan.sern@automattic.com>
2021-04-14 15:34:38 +0300

    Use Gutenboarding create site flow.

--- test/e2e/specs-gutenberg/wp-calypso-gutenberg-focused-launch-spec.js
+++ test/e2e/specs-gutenberg/wp-calypso-gutenberg-focused-launch-spec.js
@@ -13,11 +13,8 @@ import * as driverManager from '../lib/driver-manager.js';
 import * as dataHelper from '../lib/data-helper.js';
 import * as driverHelper from '../lib/driver-helper';
 import LoginFlow from '../lib/flows/login-flow.js';
-import CreateSiteFlow from '../lib/flows/create-site-flow.js';
 import DeleteSiteFlow from '../lib/flows/delete-site-flow.js';
-import MyHomePage from '../lib/pages/my-home-page';
-import NavBarComponent from '../lib/components/nav-bar-component.js';
-import GutenbergEditorComponent from '../lib/gutenberg/gutenberg-editor-component';
+import GutenboardingFlow from '../lib/flows/gutenboarding-flow.js';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
@@ -43,18 +40,9 @@ describe( `[${ host }] Calypso Gutenberg Editor: Focused launch on (${ screenSiz
 		} );
 
 		step( 'Can create a free site', async function () {
-			await new CreateSiteFlow( driver, siteName ).createFreeSite();
-		} );
-
-		step( 'Can open block editor', async function () {
-			await MyHomePage.Expect( driver );
-			await NavBarComponent.Expect( driver );
-
-			const navbarComponent = await NavBarComponent.Expect( driver );
-			await navbarComponent.clickCreateNewPost();
-
-			const gEditorComponent = await GutenbergEditorComponent.Expect( driver );
-			await gEditorComponent.initEditor();
+			const gutenboardingUrl = dataHelper.getCalypsoURL( '/new' );
+			await driver.get( gutenboardingUrl );
+			await new GutenboardingFlow( driver ).createFreeSite( siteName );
 		} );
 
 		step( 'Can open focused launch modal', async function () {
