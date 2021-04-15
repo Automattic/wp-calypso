be0671b71f8f9ad653c33fd8884617cadfe0a892
stefannhs <StefanNieuwenhuis@users.noreply.github.com>
2021-04-15 13:29:30 +0200

    [Design Picker]: Add integration tests (#51775)
    * Initial commit
    
    * Add integration test for selecting a design
    
    * Update import to satisfy eslint
    
    * Update retrieving available designs

--- packages/design-picker/src/__tests__/integration.test.tsx
+++ packages/design-picker/src/__tests__/integration.test.tsx
@@ -1,6 +1,48 @@
+/**
+ * External dependencies
+ */
+import * as React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@automattic/calypso-config';
+
+/**
+ * Internal dependencies
+ */
+import DesignPicker from '../components';
+import { getAvailableDesigns } from '../utils';
+import type { Design } from '../types';
+
+jest.mock( `@automattic/calypso-config`, () => ( {
+	isEnabled: jest.fn().mockImplementation( ( feature: string ) => {
+		switch ( feature ) {
+			case `gutenboarding/landscape-preview`:
+				return false;
+			case `gutenboarding/mshot-preview`:
+				return false;
+		}
+	} ),
+} ) );
+
+const MOCK_LOCALE = `en`;
+const MOCK_DESIGN_TITLE = 'Cassel';
+
 // Design picker integration tests
 describe( '<DesignPicker /> integration', () => {
-	it( 'should be replaced with real integration tests', () => {
-		expect( 1 ).toBeTruthy();
+	beforeEach( () => {
+		jest.clearAllMocks();
+	} );
+
+	it( 'should select a design', async () => {
+		const mockedOnSelectCallback = jest.fn();
+
+		render( <DesignPicker locale={ MOCK_LOCALE } onSelect={ mockedOnSelectCallback } /> );
+
+		fireEvent.click( screen.getByLabelText( new RegExp( MOCK_DESIGN_TITLE, 'i' ) ) );
+
+		expect( mockedOnSelectCallback ).toHaveBeenCalledWith(
+			getAvailableDesigns().featured.find(
+				( design: Design ) => design.title === MOCK_DESIGN_TITLE
+			)
+		);
 	} );
 } );
