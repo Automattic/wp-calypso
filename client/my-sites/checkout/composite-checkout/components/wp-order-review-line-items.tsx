d0c93c567a4011a7a3a5ef75211fdfd36a41c696
Payton Swick <payton@foolord.com>
2021-04-16 16:04:12 -0400

    Rename isMonthly from lib/products-values to isMonthlyProduct

--- client/my-sites/checkout/composite-checkout/components/wp-order-review-line-items.tsx
+++ client/my-sites/checkout/composite-checkout/components/wp-order-review-line-items.tsx
@@ -41,7 +41,7 @@ import { currentUserHasFlag, getCurrentUser } from 'calypso/state/current-user/s
 import { NON_PRIMARY_DOMAINS_TO_FREE_USERS } from 'calypso/state/current-user/constants';
 import { TITAN_MAIL_MONTHLY_SLUG } from 'calypso/lib/titan/constants';
 import { getSublabel, getLabel } from '../lib/translate-cart';
-import { isPlan, isMonthly, isYearly, isBiennially } from 'calypso/lib/products-values';
+import { isPlan, isMonthlyProduct, isYearly, isBiennially } from 'calypso/lib/products-values';
 import type {
 	WPCOMProductSlug,
 	WPCOMProductVariant,
@@ -569,7 +569,7 @@ function LineItemSublabelAndPrice( {
 			},
 		};
 
-		if ( isMonthly( product ) ) {
+		if ( isMonthlyProduct( product ) ) {
 			return <>{ translate( '%(sublabel)s: %(price)s per month', options ) }</>;
 		}
 
@@ -623,7 +623,7 @@ function FirstTermDiscountCallout( {
 		return null;
 	}
 
-	if ( isMonthly( product ) ) {
+	if ( isMonthlyProduct( product ) ) {
 		return <DiscountCallout>{ translate( 'Discount for first month' ) }</DiscountCallout>;
 	}
 
