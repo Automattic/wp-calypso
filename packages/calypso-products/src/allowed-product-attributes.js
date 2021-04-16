fb947d8915e0df17951f2297287f922cf5d0583f
Payton Swick <payton@foolord.com>
2021-04-16 17:31:23 -0400

    Fix a schema import

--- packages/calypso-products/src/allowed-product-attributes.js
+++ packages/calypso-products/src/allowed-product-attributes.js
@@ -6,7 +6,7 @@ import { pick } from 'lodash';
 /**
  * Internal dependencies
  */
-import schema from './schema.json';
+import schema from './utils/schema.json';
 
 export function allowedProductAttributes( product ) {
 	return pick( product, Object.keys( schema.properties ) );
