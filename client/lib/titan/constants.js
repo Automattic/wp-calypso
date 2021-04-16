e0e81c1427ba40e289e478e8f73e763c98152cca
Payton Swick <payton@foolord.com>
2021-04-16 16:46:47 -0400

    Move TITAN_MAIL_MONTHLY_SLUG into calypso-products

--- client/lib/titan/constants.js
+++ client/lib/titan/constants.js
@@ -1,3 +1,3 @@
 export const TITAN_CONTROL_PANEL_CONTEXT_CREATE_EMAIL = 'create_email_account';
 export const TITAN_CONTROL_PANEL_CONTEXT_IMPORT_EMAIL = 'import_email';
-export const TITAN_MAIL_MONTHLY_SLUG = 'wp_titan_mail_monthly';
+export { TITAN_MAIL_MONTHLY_SLUG } from '@automattic/calypso-products';
--- packages/calypso-products/src/is-titan-mail.js
+++ packages/calypso-products/src/is-titan-mail.js
@@ -3,7 +3,7 @@
  */
 import { assertValidProduct } from './utils/assert-valid-product';
 import { formatProduct } from './format-product';
-import { TITAN_MAIL_MONTHLY_SLUG } from 'calypso/lib/titan/constants';
+import { TITAN_MAIL_MONTHLY_SLUG } from './constants';
 
 export function isTitanMail( product ) {
 	product = formatProduct( product );
