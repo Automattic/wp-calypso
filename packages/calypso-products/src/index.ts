6039be005dac8c02314af55828746b49655a9464
Payton Swick <payton@foolord.com>
2021-04-16 15:56:31 -0400

    Export lib/products-values code from calypso-products

--- packages/calypso-products/src/index.ts
+++ packages/calypso-products/src/index.ts
@@ -2,4 +2,5 @@ export * from './main';
 export * from './types';
 export * from './constants';
 export * from './utils';
+export * from './product-values';
 export { default as getIntervalTypeForTerm } from './get-interval-type-for-term';
