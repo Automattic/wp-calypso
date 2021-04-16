732bb311a940614476b7f4d0090a88733fe7147c
Payton Swick <payton@foolord.com>
2021-04-16 19:39:44 -0400

    Remove lodash from utils

--- packages/calypso-products/src/utils.js
+++ packages/calypso-products/src/utils.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 
-import { map, head, property } from 'lodash';
-
-export const findCurrencyFromPlans = ( plans ) =>
-	head( map( plans, property( 'currency_code' ) ) ) || 'USD';
+export const findCurrencyFromPlans = ( plans ) => {
+	const firstPlanWithCurrency = plans.find( ( plan ) => plan.currency_code );
+	return firstPlanWithCurrency?.currency_code ?? 'USD';
+};
