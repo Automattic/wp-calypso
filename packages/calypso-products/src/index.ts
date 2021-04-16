27c5b1a7ab48497a5fdcdf990342122a6e43eee0
Payton Swick <payton@foolord.com>
2021-04-16 16:56:18 -0400

    Move gsuite product slug functions to calypso-products

--- packages/calypso-products/src/index.ts
+++ packages/calypso-products/src/index.ts
@@ -6,3 +6,4 @@ export * from './product-values';
 export { default as getIntervalTypeForTerm } from './get-interval-type-for-term';
 export { default as isJetpackLegacyItem } from './is-jetpack-legacy-item';
 export { default as isJetpackPurchasableItem } from './is-jetpack-purchasable-item';
+export * from './gsuite-product-slug';
