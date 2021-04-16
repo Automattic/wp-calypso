da8694b29348521fe43afec72dcf9b729517c8d9
Payton Swick <payton@foolord.com>
2021-04-16 16:10:37 -0400

    Update reference to schema and move schema to calypso-products

--- packages/calypso-products/src/utils/assert-valid-product.js
+++ packages/calypso-products/src/utils/assert-valid-product.js
@@ -6,7 +6,7 @@ import { difference, isEmpty } from 'lodash';
 /**
  * Internal dependencies
  */
-import schema from 'calypso/lib/products-values/schema.json';
+import schema from './schema.json';
 
 export function assertValidProduct( product ) {
 	const missingAttributes = difference( schema.required, Object.keys( product ) );
