56911a478bfe29d63885db4f454d3fb47b48c2e5
Payton Swick <payton@foolord.com>
2021-04-16 16:22:57 -0400

    Update tests that mock lib/products-values to use calypso-products

--- client/my-sites/checkout/checkout-thank-you/test/index.js
+++ client/my-sites/checkout/checkout-thank-you/test/index.js
@@ -21,6 +21,7 @@ import {
 	PLAN_JETPACK_PREMIUM_MONTHLY,
 	PLAN_JETPACK_BUSINESS,
 	PLAN_JETPACK_BUSINESS_MONTHLY,
+	isDotComPlan,
 } from '@automattic/calypso-products';
 
 /**
@@ -36,12 +37,8 @@ jest.unmock( '@automattic/calypso-products' );
 jest.mock( '@automattic/calypso-products', () => ( {
 	...jest.requireActual( '@automattic/calypso-products' ),
 	shouldFetchSitePlans: () => false,
+	isDotComPlan: jest.fn( () => false ),
 } ) );
-
-jest.unmock( 'calypso/lib/products-values' );
-const productValues = require( 'calypso/lib/products-values' );
-const isDotComPlan = require( 'calypso/lib/products-values/is-dot-com-plan' );
-isDotComPlan.isDotComPlan = jest.fn( () => false );
 
 jest.mock( 'calypso/lib/analytics/tracks', () => ( {
 	recordTracksEvent: () => null,
@@ -231,7 +228,7 @@ describe( 'CheckoutThankYou', () => {
 		};
 
 		afterAll( () => {
-			productValues.isDotComPlan.mockImplementation( () => false );
+			isDotComPlan.mockImplementation( () => false );
 		} );
 
 		test( 'Should be there for AT', () => {
@@ -244,7 +241,7 @@ describe( 'CheckoutThankYou', () => {
 			comp = shallow( <CheckoutThankYou { ...props } transferComplete={ false } /> );
 			expect( comp.find( 'component--AtomicStoreThankYouCard' ) ).toHaveLength( 0 );
 
-			productValues.isDotComPlan.mockImplementation( () => true );
+			isDotComPlan.mockImplementation( () => true );
 
 			comp = shallow( <CheckoutThankYou { ...props } /> );
 			expect( comp.find( 'component--AtomicStoreThankYouCard' ) ).toHaveLength( 0 );
