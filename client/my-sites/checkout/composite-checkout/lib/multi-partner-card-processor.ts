a16a977eea36eea4344ef60de0581e3410fc32c0
Payton Swick <payton@foolord.com>
2021-04-16 11:09:39 -0400

    Explicitly handle createEbanxToken errors

--- client/my-sites/checkout/composite-checkout/lib/multi-partner-card-processor.ts
+++ client/my-sites/checkout/composite-checkout/lib/multi-partner-card-processor.ts
@@ -152,13 +152,22 @@ async function ebanxCardProcessor(
 		contactDetails,
 	} = transactionOptions;
 
-	const paymentMethodToken: EbanxToken = await createEbanxToken( 'new_purchase', {
-		country: submitData.countryCode,
-		name: submitData.name,
-		number: submitData.number,
-		cvv: submitData.cvv,
-		'expiration-date': submitData[ 'expiration-date' ],
-	} );
+	let paymentMethodToken;
+	try {
+		const ebanxTokenResponse: EbanxToken = await createEbanxToken( 'new_purchase', {
+			country: submitData.countryCode,
+			name: submitData.name,
+			number: submitData.number,
+			cvv: submitData.cvv,
+			'expiration-date': submitData[ 'expiration-date' ],
+		} );
+		paymentMethodToken = ebanxTokenResponse;
+	} catch ( error ) {
+		debug( 'transaction failed' );
+		// Errors here are "expected" errors, meaning that they (hopefully) come
+		// from Ebanx and not from some bug in the frontend code.
+		return makeErrorResponse( error.message );
+	}
 
 	const formattedTransactionData = createTransactionEndpointRequestPayload( {
 		...submitData,
