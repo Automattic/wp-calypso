68d92065f319869ffbabcd75e26b348f6ae05038
Payton Swick <payton@foolord.com>
2021-04-16 22:21:06 -0400

    Remove lodash from the rest of calypso-products

--- packages/calypso-products/src/is-jetpack-product-slug.js
+++ packages/calypso-products/src/is-jetpack-product-slug.js
@@ -1,13 +1,8 @@
 /**
  * External dependencies
  */
-import { includes } from 'lodash';
-
-/**
- * External dependencies
- */
 import { JETPACK_PRODUCTS_LIST } from './index';
 
 export function isJetpackProductSlug( productSlug ) {
-	return includes( JETPACK_PRODUCTS_LIST, productSlug );
+	return JETPACK_PRODUCTS_LIST.includes( productSlug );
 }
