d0485aebfc9942075c44e81741029ff0ab0c470b
Payton Swick <payton@foolord.com>
2021-04-17 17:40:47 -0400

    Replace "import *" of calypso-products with explicit imports

--- client/components/marketing-survey/cancel-purchase-form/test/steps-for-product-and-survey.js
+++ client/components/marketing-survey/cancel-purchase-form/test/steps-for-product-and-survey.js
@@ -8,7 +8,14 @@ import { expect } from 'chai';
  */
 import * as steps from '../steps';
 import stepsForProductAndSurvey from '../steps-for-product-and-survey';
-import * as plans from '@automattic/calypso-products';
+import {
+	PLAN_BUSINESS,
+	PLAN_BUSINESS_2_YEARS,
+	PLAN_PERSONAL,
+	PLAN_PERSONAL_2_YEARS,
+	PLAN_PREMIUM,
+	PLAN_PREMIUM_2_YEARS,
+} from '@automattic/calypso-products';
 
 const PLAN_SURVEY_STEPS = [ steps.INITIAL_STEP, steps.FINAL_STEP ];
 const DEFAULT_STEPS_WITH_UPGRADE_AT_STEP = [
@@ -40,84 +47,84 @@ describe( 'stepsForProductAndSurvey', () => {
 		const precancellationChatToggle = true;
 
 		test( 'should include happychat step if product is personal plan and happychat is available', () => {
-			const product = { product_slug: plans.PLAN_PERSONAL };
+			const product = { product_slug: PLAN_PERSONAL };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( steps.DEFAULT_STEPS_WITH_HAPPYCHAT );
 		} );
 
 		test( 'should include happychat step if product is personal plan and happychat is available (2y)', () => {
-			const product = { product_slug: plans.PLAN_PERSONAL_2_YEARS };
+			const product = { product_slug: PLAN_PERSONAL_2_YEARS };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( steps.DEFAULT_STEPS_WITH_HAPPYCHAT );
 		} );
 
 		test( 'should not include happychat step if product is personal plan but happychat is not available', () => {
-			const product = { product_slug: plans.PLAN_PERSONAL };
+			const product = { product_slug: PLAN_PERSONAL };
 			expect(
 				stepsForProductAndSurvey( survey, product, false, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
 		} );
 
 		test( 'should not include happychat step if product is personal plan but happychat is not available (2y)', () => {
-			const product = { product_slug: plans.PLAN_PERSONAL_2_YEARS };
+			const product = { product_slug: PLAN_PERSONAL_2_YEARS };
 			expect(
 				stepsForProductAndSurvey( survey, product, false, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
 		} );
 
 		test( 'should include happychat step if product is premium plan and happychat is available', () => {
-			const product = { product_slug: plans.PLAN_PREMIUM };
+			const product = { product_slug: PLAN_PREMIUM };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( steps.DEFAULT_STEPS_WITH_HAPPYCHAT );
 		} );
 
 		test( 'should include happychat step if product is premium plan and happychat is available (2y)', () => {
-			const product = { product_slug: plans.PLAN_PREMIUM_2_YEARS };
+			const product = { product_slug: PLAN_PREMIUM_2_YEARS };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( steps.DEFAULT_STEPS_WITH_HAPPYCHAT );
 		} );
 
 		test( 'should not include happychat step if product is premium plan but happychat is not available', () => {
-			const product = { product_slug: plans.PLAN_PREMIUM };
+			const product = { product_slug: PLAN_PREMIUM };
 			expect(
 				stepsForProductAndSurvey( survey, product, false, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
 		} );
 
 		test( 'should not include happychat step if product is premium plan but happychat is not available (2y)', () => {
-			const product = { product_slug: plans.PLAN_PREMIUM_2_YEARS };
+			const product = { product_slug: PLAN_PREMIUM_2_YEARS };
 			expect(
 				stepsForProductAndSurvey( survey, product, false, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
 		} );
 
 		test( 'should include happychat step if product is business plan and happychat is available', () => {
-			const product = { product_slug: plans.PLAN_BUSINESS };
+			const product = { product_slug: PLAN_BUSINESS };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( steps.DEFAULT_STEPS_WITH_HAPPYCHAT );
 		} );
 
 		test( 'should include happychat step if product is business plan and happychat is available (2y)', () => {
-			const product = { product_slug: plans.PLAN_BUSINESS_2_YEARS };
+			const product = { product_slug: PLAN_BUSINESS_2_YEARS };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( steps.DEFAULT_STEPS_WITH_HAPPYCHAT );
 		} );
 
 		test( 'should include happychat step if product is business plan but happychat is not available', () => {
-			const product = { product_slug: plans.PLAN_BUSINESS };
+			const product = { product_slug: PLAN_BUSINESS };
 			expect(
 				stepsForProductAndSurvey( survey, product, false, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
 		} );
 
 		test( 'should include happychat step if product is business plan but happychat is not available (2y)', () => {
-			const product = { product_slug: plans.PLAN_BUSINESS_2_YEARS };
+			const product = { product_slug: PLAN_BUSINESS_2_YEARS };
 			expect(
 				stepsForProductAndSurvey( survey, product, false, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
@@ -129,42 +136,42 @@ describe( 'stepsForProductAndSurvey', () => {
 		const precancellationChatToggle = false;
 
 		test( 'should not include happychat step if product is personal plan and happychat is available', () => {
-			const product = { product_slug: plans.PLAN_PERSONAL };
+			const product = { product_slug: PLAN_PERSONAL };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
 		} );
 
 		test( 'should not include happychat step if product is personal plan and happychat is available (2y)', () => {
-			const product = { product_slug: plans.PLAN_PERSONAL_2_YEARS };
+			const product = { product_slug: PLAN_PERSONAL_2_YEARS };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
 		} );
 
 		test( 'should not include happychat step if product is premium plan and happychat is available', () => {
-			const product = { product_slug: plans.PLAN_PREMIUM };
+			const product = { product_slug: PLAN_PREMIUM };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
 		} );
 
 		test( 'should not include happychat step if product is premium plan and happychat is available (2y)', () => {
-			const product = { product_slug: plans.PLAN_PREMIUM_2_YEARS };
+			const product = { product_slug: PLAN_PREMIUM_2_YEARS };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
 		} );
 
 		test( 'should not include happychat step if product is business plan and happychat is available', () => {
-			const product = { product_slug: plans.PLAN_BUSINESS };
+			const product = { product_slug: PLAN_BUSINESS };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
 		} );
 
 		test( 'should not include happychat step if product is business plan and happychat is available (2y)', () => {
-			const product = { product_slug: plans.PLAN_BUSINESS_2_YEARS };
+			const product = { product_slug: PLAN_BUSINESS_2_YEARS };
 			expect(
 				stepsForProductAndSurvey( survey, product, true, precancellationChatToggle )
 			).to.deep.equal( PLAN_SURVEY_STEPS );
@@ -175,42 +182,42 @@ describe( 'stepsForProductAndSurvey', () => {
 		const survey = { questionOneRadio: 'couldNotInstall' };
 
 		test( 'should include AT upgrade step if product is personal plan', () => {
-			const product = { product_slug: plans.PLAN_PERSONAL };
+			const product = { product_slug: PLAN_PERSONAL };
 			expect( stepsForProductAndSurvey( survey, product, true ) ).to.deep.equal(
 				DEFAULT_STEPS_WITH_UPGRADE_AT_STEP
 			);
 		} );
 
 		test( 'should include AT upgrade step if product is personal plan (2y)', () => {
-			const product = { product_slug: plans.PLAN_PERSONAL_2_YEARS };
+			const product = { product_slug: PLAN_PERSONAL_2_YEARS };
 			expect( stepsForProductAndSurvey( survey, product, true ) ).to.deep.equal(
 				DEFAULT_STEPS_WITH_UPGRADE_AT_STEP
 			);
 		} );
 
 		test( 'should include AT upgrade step if product is premium plan', () => {
-			const product = { product_slug: plans.PLAN_PREMIUM };
+			const product = { product_slug: PLAN_PREMIUM };
 			expect( stepsForProductAndSurvey( survey, product, true ) ).to.deep.equal(
 				DEFAULT_STEPS_WITH_UPGRADE_AT_STEP
 			);
 		} );
 
 		test( 'should include AT upgrade step if product is premium plan (2y)', () => {
-			const product = { product_slug: plans.PLAN_PREMIUM_2_YEARS };
+			const product = { product_slug: PLAN_PREMIUM_2_YEARS };
 			expect( stepsForProductAndSurvey( survey, product, true ) ).to.deep.equal(
 				DEFAULT_STEPS_WITH_UPGRADE_AT_STEP
 			);
 		} );
 
 		test( 'should include business AT step if product is personal plan', () => {
-			const product = { product_slug: plans.PLAN_BUSINESS };
+			const product = { product_slug: PLAN_BUSINESS };
 			expect( stepsForProductAndSurvey( survey, product, true ) ).to.deep.equal(
 				DEFAULT_STEPS_WITH_BUSINESS_AT_STEP
 			);
 		} );
 
 		test( 'should include business AT step if product is personal plan (2y)', () => {
-			const product = { product_slug: plans.PLAN_BUSINESS_2_YEARS };
+			const product = { product_slug: PLAN_BUSINESS_2_YEARS };
 			expect( stepsForProductAndSurvey( survey, product, true ) ).to.deep.equal(
 				DEFAULT_STEPS_WITH_BUSINESS_AT_STEP
 			);
