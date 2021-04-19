330a9ddb2fd1c95cf63322783e78eb53d89e655c
Jarda Snajdr <jsnajdr@gmail.com>
2021-04-19 09:23:14 +0200

    BodySectionCssClass: remove leftover prefixed CSS classes on mount

--- client/layout/body-section-css-class.js
+++ client/layout/body-section-css-class.js
@@ -13,29 +13,35 @@ import React from 'react';
  * `div.layout` has the advantage of being maintained by React, where class names can be
  * specified declaratively and the DOM diffing and patching is done by React itself.
  */
-const addBodyClass = ( toClass ) => ( value ) => () => {
-	// if value is empty-ish, don't add any CSS classes
-	if ( ! value ) {
-		return;
-	}
+function useBodyClass( prefix, value ) {
+	React.useEffect( () => {
+		// remove any existing classes with the same prefix, coming from example from a
+		// server HTML markup.
+		for ( const className of document.body.classList ) {
+			if ( className.startsWith( prefix ) ) {
+				document.body.classList.remove( className );
+			}
+		}
 
-	// convert the value (section or group name) to a CSS class name
-	const className = toClass( value );
+		// if value is empty-ish, don't add any CSS classes
+		if ( ! value ) {
+			return;
+		}
 
-	// add the CSS class to body when performing the effect
-	document.body.classList.add( className );
+		// convert the value (section or group name) to a CSS class name
+		const className = prefix + value;
 
-	// remove the CSS class from body in the effect cleanup function
-	return () => document.body.classList.remove( className );
-};
+		// add the CSS class to body when performing the effect
+		document.body.classList.add( className );
 
-// two effect creators for groups and sections
-const addGroupClass = addBodyClass( ( g ) => `is-group-${ g }` );
-const addSectionClass = addBodyClass( ( s ) => `is-section-${ s }` );
+		// remove the CSS class from body in the effect cleanup function
+		return () => document.body.classList.remove( className );
+	}, [ prefix, value ] );
+}
 
 export default function BodySectionCssClass( { group, section, bodyClass } ) {
-	React.useEffect( addGroupClass( group ), [ group ] );
-	React.useEffect( addSectionClass( section ), [ section ] );
+	useBodyClass( 'is-group-', group );
+	useBodyClass( 'is-section-', section );
 	React.useEffect( () => {
 		if ( ! Array.isArray( bodyClass ) || bodyClass.length === 0 ) {
 			return;
