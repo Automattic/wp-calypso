9cebbdfa894cc8c2c4c74af79047a7d793f0c600
Dale du Preez <dale@automattic.com>
2021-04-19 22:58:22 +0200

    * Extract header and item into local functions * Add loading/placeholder content * Show "No mailboxes" message when no mailboxes were loaded

--- client/my-sites/email/email-management/home/email-plan-mailboxes-list.jsx
+++ client/my-sites/email/email-management/home/email-plan-mailboxes-list.jsx
@@ -2,7 +2,9 @@
  * External dependencies
  */
 import React from 'react';
+import classNames from 'classnames';
 import { CompactCard } from '@automattic/components';
+import PropTypes from 'prop-types';
 import { useTranslate } from 'i18n-calypso';
 
 /**
@@ -13,18 +15,49 @@ import SectionHeader from 'calypso/components/section-header';
 import Badge from 'calypso/components/badge';
 import { isEmailUserAdmin } from 'calypso/lib/emails';
 
-function EmailPlanMailboxesList( { emails } ) {
+function EmailPlanMailboxesList( { emails, isLoadingEmails } ) {
 	const translate = useTranslate();
+
+	const MailboxListHeader = ( { children } ) => {
+		return (
+			<div className="email-plan-mailboxes-list__mailbox-list">
+				<SectionHeader label={ translate( 'Mailboxes' ) } />
+				{ children }
+			</div>
+		);
+	};
+
+	const MailboxListItem = ( { children, isPlaceholder, noEmails } ) => {
+		const className = classNames( 'email-plan-mailboxes-list__mailbox-list-item', {
+			'is-placeholder': isPlaceholder,
+			'no-emails': noEmails,
+		} );
+		return <CompactCard className={ className }>{ children }</CompactCard>;
+	};
+
+	if ( isLoadingEmails ) {
+		return (
+			<MailboxListHeader>
+				<MailboxListItem isPlaceholder>
+					<span> </span>
+				</MailboxListItem>
+			</MailboxListHeader>
+		);
+	}
+
 	if ( ! emails || emails.length < 1 ) {
-		return null;
+		return (
+			<MailboxListHeader>
+				<MailboxListItem noEmails>
+					<span>{ translate( 'No mailboxes' ) }</span>
+				</MailboxListItem>
+			</MailboxListHeader>
+		);
 	}
 
 	const emailsItems = emails.map( ( email ) => {
 		return (
-			<CompactCard
-				key={ `email-row-${ email.mailbox }` }
-				className="email-plan-mailboxes-list__mailbox-list-item"
-			>
+			<MailboxListItem key={ `email-row-${ email.mailbox }` }>
 				<MaterialIcon icon="email" />
 				<span>
 					{ email.mailbox }@{ email.domain }
@@ -36,16 +69,16 @@ function EmailPlanMailboxesList( { emails } ) {
 						} ) }
 					</Badge>
 				) }
-			</CompactCard>
+			</MailboxListItem>
 		);
 	} );
 
-	return (
-		<div className="email-plan-mailboxes-list__mailbox-list">
-			<SectionHeader label={ translate( 'Mailboxes' ) } />
-			{ emailsItems }
-		</div>
-	);
+	return <MailboxListHeader>{ emailsItems }</MailboxListHeader>;
 }
+
+EmailPlanMailboxesList.propTypes = {
+	emails: PropTypes.array,
+	isLoadingEmails: PropTypes.bool,
+};
 
 export default EmailPlanMailboxesList;
