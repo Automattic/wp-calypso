c6a8da0b4710aedddfd03033933b5f85bfaa6301
Atanas Angelov <hi@atanas.dev>
2021-04-19 20:08:23 +0300

    Partner Portal: Enable the Billing Dashboard in the production environment (#52069)

--- client/jetpack-cloud/sections/partner-portal/sidebar/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/sidebar/index.tsx
@@ -19,7 +19,6 @@ import { itemLinkMatches } from 'calypso/my-sites/sidebar/utils';
  * Style dependencies
  */
 import 'calypso/components/jetpack/sidebar/style.scss';
-import config from '@automattic/calypso-config';
 
 interface Props {
 	path: string;
@@ -42,18 +41,16 @@ class PartnerPortalSidebar extends Component< Props > {
 			<Sidebar className="sidebar__jetpack-cloud">
 				<SidebarRegion>
 					<SidebarMenu>
-						{ config.isEnabled( 'jetpack-cloud/partner-portal/billing-dashboard' ) && (
-							<SidebarItem
-								materialIcon="credit_card"
-								materialIconStyle="outline"
-								label={ translate( 'Billing', {
-									comment: 'Jetpack sidebar navigation item',
-								} ) }
-								link="/partner-portal"
-								onNavigate={ this.onNavigate( 'Jetpack Cloud / Partner Portal' ) }
-								selected={ path === '/partner-portal' }
-							/>
-						) }
+						<SidebarItem
+							materialIcon="credit_card"
+							materialIconStyle="outline"
+							label={ translate( 'Billing', {
+								comment: 'Jetpack sidebar navigation item',
+							} ) }
+							link="/partner-portal"
+							onNavigate={ this.onNavigate( 'Jetpack Cloud / Partner Portal' ) }
+							selected={ path === '/partner-portal' }
+						/>
 
 						<SidebarItem
 							materialIcon="vpn_key"
