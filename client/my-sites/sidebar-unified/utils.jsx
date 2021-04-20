6714a5c16d443ea8dd4928bcd918ff9ec2c5f3ea
sixhours <calobee@gmail.com>
2021-04-20 15:53:26 -0400

    Add special logic for handling taxonomies, which have a different parent menu from their actual location in Calypso

--- client/my-sites/sidebar-unified/utils.jsx
+++ client/my-sites/sidebar-unified/utils.jsx
@@ -20,6 +20,18 @@ export const itemLinkMatches = ( path, currentPath ) => {
 	if ( pathIncludes( currentPath, 'taxonomies', 2 ) ) {
 		return fragmentIsEqual( path, currentPath, 3 );
 	}
+	if ( pathIncludes( currentPath, 'settings', 1 ) ) {
+		/*
+		 * If the menu item URL contains 'taxonomies', ignore it when on Settings screens.
+		 * Taxonomies are located under the Posts parent menu; this prevents the Posts parent
+		 * from being highlighted when Settings screens are active.
+		 */
+		if ( pathIncludes( path, 'taxonomies', 2 ) ) {
+			return false;
+		}
+		//Match the first path for other Settings screens as usual
+		return fragmentIsEqual( path, currentPath, 1 );
+	}
 	// Temp fix till we remove duplicate menu entry of sharing buttons from 'Settings' menu. See https://github.com/Automattic/wp-calypso/issues/49756.
 	if ( pathIncludes( currentPath, 'marketing', 1 ) ) {
 		return fragmentIsEqual( path, currentPath, 1 ) && fragmentIsEqual( path, '//tools', 2 );
