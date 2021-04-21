09e109d74bfb2af42fa20d2b1a7febf2da3e1bb6
Kamen Stanev <hambai@gmail.com>
2021-04-21 20:56:53 +0300

    New emails home screen (#51452)
    * initial structure of the new email management home
    * add plan view
    * extract the email type icon
    * fixed the active email list styling
    * fixed the active email mailbox count string
    * fix the inactive list button placement
    * query for email forwards
    * request the titan mailboxes
    * fix the plan page back button
    * fix the email list styling
    * move the mailbox list into separate component
    * normalize the titan mailboxes list
    * add subscription component to show the expiry date, renew button and auto-renew toggle
    * fix the number of mailboxes/users/forwards text in the active emails list
    * fix the titan mail icon color
    * fix tracking property for the auto-renew toggle
    * initial email plan status icon, class and text
    * no need for separate constant to hold the list header
    * fix hasEmailForwards to use getEmailForwardsCount
    * update the list title to "Domains with emails"
    * reorder some constants to be closer to where they're used
    * renamed the selectedDomain to firstDomainWithNoEmail
    * order component props alphabetically
    * renamed the css classes to email-list-active and email-list-inactive
    * renamed the EmailPlanView component to EmailPlan
    * added a few comments for translators
    * no need to prefix the key property
    * fix the email-plan import
    * fix the css for the email-plan-view to email-plan rename
    * move to <> instead of React.Fragment
    * Refactor permission checks to use existing `canCurrentUserAddEmail()` helper
    * Refactor to use new REST API
    * Remove @ prefix from main page listings; increase Add Email button size
    * Add getTitanSubscriptionId() helper function
    * Match Other Domains spacing with most recent design
    * Implement new mailbox and billing links for domain-specific page
    * Create dedicated functions to get the product slug and subscription ID out of the Google subscription data
    * Update header text for domain-specific page and use new Google helper functions
    * Increase vertical height of items in domain-specific page and lighten mailbox icon
    * Add Manage all mailboxes link
    * Remove code to fetch provider-specific email data
    * Enable email/centralized-home feature flag in development
    * Use correct case for isPlaceholder prop
    * Increase padding for rows on subscription/domain screen
    * Add documentation RE domain arguments
    * Rename getGoogleProductSlug() to getGSuiteProductSlug()
    * Remove unused class
    * Rename loadedEmailAccounts to hasLoadedEmailAccounts
    * Minor variable rename
    * Simplify internal function name
    
    Authored-by: Dale du Preez <dale@automattic.com>

---
+++ client/my-sites/email/email-management/home/email-plan-mailboxes-list.jsx
@@ -0,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { localize } from 'i18n-calypso';
+import { CompactCard } from '@automattic/components';
+
+/**
+ * Internal dependencies
+ */
+import MaterialIcon from 'calypso/components/material-icon';
+import SectionHeader from 'calypso/components/section-header';
+import Badge from 'calypso/components/badge';
+import { isEmailUserAdmin } from 'calypso/lib/emails';
+
+class EmailPlanMailboxesList extends React.Component {
+	render() {
+		const { emails, translate } = this.props;
+
+		if ( ! emails || emails.length < 1 ) {
+			return null;
+		}
+
+		const emailsItems = emails.map( ( email ) => {
+			return (
+				<CompactCard
+					key={ `email-row-${ email.mailbox }` }
+					className="email-plan-mailboxes-list__mailbox-list-item"
+				>
+					<MaterialIcon icon="email" />
+					<span>
+						{ email.mailbox }@{ email.domain }
+					</span>
+					{ isEmailUserAdmin( email ) && (
+						<Badge type="info">
+							{ translate( 'Admin', {
+								comment: 'Email user role displayed as a badge',
+							} ) }
+						</Badge>
+					) }
+				</CompactCard>
+			);
+		} );
+
+		return (
+			<div className="email-plan-mailboxes-list__mailbox-list">
+				<SectionHeader label={ translate( 'Mailboxes' ) } />
+				{ emailsItems }
+			</div>
+		);
+	}
+}
+
+export default localize( EmailPlanMailboxesList );
---
+++ client/my-sites/email/email-management/home/email-plan.jsx
@@ -0,0 +1,319 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import classnames from 'classnames';
+import { CompactCard } from '@automattic/components';
+import { connect } from 'react-redux';
+import { isEnabled } from '@automattic/calypso-config';
+import { localize } from 'i18n-calypso';
+import page from 'page';
+import PropTypes from 'prop-types';
+
+/**
+ * Internal dependencies
+ */
+import wp from 'calypso/lib/wp';
+import {
+	getGoogleAdminUrl,
+	getGoogleMailServiceFamily,
+	getGSuiteProductSlug,
+	getGSuiteSubscriptionId,
+	getProductType,
+	hasGSuiteWithUs,
+} from 'calypso/lib/gsuite';
+import { getTitanSubscriptionId, hasTitanMailWithUs } from 'calypso/lib/titan';
+import HeaderCake from 'calypso/components/header-cake';
+import VerticalNav from 'calypso/components/vertical-nav';
+import VerticalNavItem from 'calypso/components/vertical-nav/item';
+import EmailTypeIcon from 'calypso/my-sites/email/email-management/home/email-type-icon';
+import {
+	emailManagement,
+	emailManagementAddGSuiteUsers,
+	emailManagementForwarding,
+	emailManagementManageTitanAccount,
+	emailManagementNewTitanAccount,
+	emailManagementTitanControlPanelRedirect,
+} from 'calypso/my-sites/email/paths';
+import EmailPlanMailboxesList from 'calypso/my-sites/email/email-management/home/email-plan-mailboxes-list';
+import {
+	getByPurchaseId,
+	hasLoadedSitePurchasesFromServer,
+	isFetchingSitePurchases,
+} from 'calypso/state/purchases/selectors';
+import QuerySitePurchases from 'calypso/components/data/query-site-purchases';
+import EmailPlanSubscription from 'calypso/my-sites/email/email-management/home/email-plan-subscription';
+import MaterialIcon from 'calypso/components/material-icon';
+import { resolveEmailPlanStatus } from 'calypso/my-sites/email/email-management/home/utils';
+import { getManagePurchaseUrlFor } from 'calypso/my-sites/purchases/paths';
+import getCurrentRoute from 'calypso/state/selectors/get-current-route';
+
+class EmailPlan extends React.Component {
+	static propTypes = {
+		domain: PropTypes.object.isRequired,
+		selectedSite: PropTypes.object.isRequired,
+
+		// Connected props
+		currentRoute: PropTypes.string,
+		hasEmailPlanSubscription: PropTypes.bool,
+		isLoadingPurchase: PropTypes.bool,
+		purchase: PropTypes.object,
+	};
+
+	state = {
+		isLoadingEmailAccounts: false,
+		errorLoadingEmailAccounts: false,
+		hasLoadedEmailAccounts: false,
+		emailAccounts: [],
+	};
+
+	componentDidMount() {
+		this.loadEmailAccounts();
+	}
+
+	componentDidUpdate() {
+		this.loadEmailAccounts();
+	}
+
+	loadEmailAccounts() {
+		const { domain, selectedSite } = this.props;
+
+		if ( this.state.isLoadingEmailAccounts || this.state.hasLoadedEmailAccounts ) {
+			return;
+		}
+
+		this.setState( {
+			isLoadingEmailAccounts: true,
+		} );
+
+		wp.undocumented()
+			.getEmailAccountsForSiteAndDomain( selectedSite.ID, domain.name )
+			.then(
+				( data ) => {
+					this.setState( {
+						isLoadingEmailAccounts: false,
+						errorLoadingEmailAccounts: false,
+						hasLoadedEmailAccounts: true,
+						emailAccounts: data?.accounts || [],
+					} );
+				},
+				() => {
+					this.setState( {
+						isLoadingEmailAccounts: false,
+						errorLoadingEmailAccounts: true,
+						hasLoadedEmailAccounts: true,
+						emailAccounts: [],
+					} );
+				}
+			);
+	}
+
+	getEmails() {
+		if ( this.state.emailAccounts[ 0 ] ) {
+			return this.state.emailAccounts[ 0 ].emails;
+		}
+		return [];
+	}
+
+	handleBack = () => {
+		const { selectedSite } = this.props;
+		page( emailManagement( selectedSite.slug ) );
+	};
+
+	getAddMailboxProps() {
+		const { currentRoute, domain, selectedSite } = this.props;
+
+		if ( hasGSuiteWithUs( domain ) ) {
+			return {
+				path: emailManagementAddGSuiteUsers(
+					selectedSite.slug,
+					domain.name,
+					getProductType( getGSuiteProductSlug( domain ) ),
+					currentRoute
+				),
+			};
+		}
+
+		if ( hasTitanMailWithUs( domain ) ) {
+			const hasSubscriptionId = !! getTitanSubscriptionId( domain );
+			return {
+				external: ! hasSubscriptionId,
+				path: emailManagementNewTitanAccount( selectedSite.slug, domain.name, currentRoute ),
+			};
+		}
+
+		return {
+			path: emailManagementForwarding( selectedSite.slug, domain.name, currentRoute ),
+		};
+	}
+
+	getHeaderText() {
+		const { domain, translate } = this.props;
+
+		if ( hasGSuiteWithUs( domain ) ) {
+			const googleMailService = getGoogleMailServiceFamily( getGSuiteProductSlug( domain ) );
+			return translate( '%(googleMailService)s settings', {
+				args: {
+					googleMailService,
+				},
+				comment: '%(googleMailService)s can be either "GSuite" or "Google Workspace"',
+			} );
+		}
+
+		if ( hasTitanMailWithUs( domain ) ) {
+			return translate( 'Email settings' );
+		}
+
+		return translate( 'Email forwarding settings' );
+	}
+
+	renderBillingNavItem() {
+		const { hasEmailPlanSubscription, purchase, selectedSite, translate } = this.props;
+
+		if ( ! hasEmailPlanSubscription ) {
+			return null;
+		}
+
+		if ( ! selectedSite || ! purchase ) {
+			return <VerticalNavItem isPlaceholder />;
+		}
+
+		const managePurchaseUrl = getManagePurchaseUrlFor( selectedSite.slug, purchase.id );
+		return (
+			<VerticalNavItem path={ managePurchaseUrl }>
+				{ translate( 'Billing and payment settings' ) }
+			</VerticalNavItem>
+		);
+	}
+
+	getManageAllNavItemProps() {
+		const { currentRoute, domain, selectedSite } = this.props;
+
+		if ( hasGSuiteWithUs( domain ) ) {
+			return {
+				external: true,
+				path: getGoogleAdminUrl( domain.name ),
+			};
+		}
+
+		if ( hasTitanMailWithUs( domain ) ) {
+			if ( isEnabled( 'titan/iframe-control-panel' ) ) {
+				return {
+					path: emailManagementManageTitanAccount( selectedSite.slug, domain.name, currentRoute ),
+				};
+			}
+
+			return {
+				external: true,
+				path: emailManagementTitanControlPanelRedirect(
+					selectedSite.slug,
+					domain.name,
+					currentRoute
+				),
+			};
+		}
+
+		return {
+			path: emailManagementForwarding( selectedSite.slug, domain.name, currentRoute ),
+		};
+	}
+
+	renderManageAllNavItem() {
+		const { domain, translate } = this.props;
+
+		if ( ! domain ) {
+			return null;
+		}
+
+		if ( ! hasGSuiteWithUs( domain ) && ! hasTitanMailWithUs( domain ) ) {
+			return null;
+		}
+
+		const manageAllNavItemProps = this.getManageAllNavItemProps();
+
+		return (
+			<VerticalNavItem { ...manageAllNavItemProps }>
+				{ translate( 'Manage all mailboxes', {
+					comment:
+						'This is the text for a link to manage all email accounts/mailboxes for a subscription',
+				} ) }
+			</VerticalNavItem>
+		);
+	}
+
+	render() {
+		const {
+			domain,
+			selectedSite,
+			hasEmailPlanSubscription,
+			purchase,
+			isLoadingPurchase,
+			translate,
+		} = this.props;
+
+		const { statusClass, text, icon } = resolveEmailPlanStatus( domain );
+
+		const cardClasses = classnames( 'email-plan__general', statusClass );
+		const addMailboxProps = this.getAddMailboxProps();
+
+		return (
+			<>
+				{ selectedSite && hasEmailPlanSubscription && (
+					<QuerySitePurchases siteId={ selectedSite.ID } />
+				) }
+				<HeaderCake onClick={ this.handleBack }>{ this.getHeaderText() }</HeaderCake>
+				<CompactCard className={ cardClasses }>
+					<span className="email-plan__general-icon">
+						<EmailTypeIcon domain={ domain } />
+					</span>
+					<div>
+						<h2>{ domain.name }</h2>
+						<span className="email-plan__status">
+							<MaterialIcon icon={ icon } /> { text }
+						</span>
+					</div>
+				</CompactCard>
+
+				{ hasEmailPlanSubscription && (
+					<EmailPlanSubscription
+						purchase={ purchase }
+						domain={ domain }
+						selectedSite={ selectedSite }
+						isLoadingPurchase={ isLoadingPurchase }
+					/>
+				) }
+
+				<EmailPlanMailboxesList emails={ this.getEmails() } />
+
+				<div className="email-plan__actions">
+					<VerticalNav>
+						<VerticalNavItem { ...addMailboxProps }>
+							{ translate( 'Add new mailbox' ) }
+						</VerticalNavItem>
+						{ this.renderManageAllNavItem() }
+						{ this.renderBillingNavItem() }
+					</VerticalNav>
+				</div>
+			</>
+		);
+	}
+}
+
+export default connect( ( state, ownProps ) => {
+	let subscriptionId = null;
+	if ( hasGSuiteWithUs( ownProps.domain ) ) {
+		subscriptionId = getGSuiteSubscriptionId( ownProps.domain );
+	} else if ( hasTitanMailWithUs( ownProps.domain ) ) {
+		subscriptionId = getTitanSubscriptionId( ownProps.domain );
+	}
+
+	const purchase = subscriptionId ? getByPurchaseId( state, parseInt( subscriptionId, 10 ) ) : null;
+
+	return {
+		currentRoute: getCurrentRoute( state ),
+		isLoadingPurchase:
+			isFetchingSitePurchases( state ) || ! hasLoadedSitePurchasesFromServer( state ),
+		purchase,
+		hasEmailPlanSubscription: !! subscriptionId,
+	};
+} )( localize( EmailPlan ) );
