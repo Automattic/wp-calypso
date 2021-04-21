71d60b91a49df492b912e881e24da63b7f4834da
Payton Swick <payton@foolord.com>
2021-04-21 19:47:27 -0400

    Update calypso-products to remove schema

--- packages/calypso-products/src/allowed-product-attributes.js
+++ packages/calypso-products/src/allowed-product-attributes.js
@@ -1,13 +1,18 @@
-/**
- * External dependencies
- */
-import { pick } from 'lodash';
-
-/**
- * Internal dependencies
- */
-import schema from './utils/schema.json';
-
 export function allowedProductAttributes( product ) {
-	return pick( product, Object.keys( schema.properties ) );
+	const {
+		product_id,
+		product_name,
+		product_slug,
+		is_domain_registration,
+		term,
+		bill_period,
+	} = product;
+	return {
+		product_id,
+		product_name,
+		product_slug,
+		is_domain_registration,
+		term,
+		bill_period,
+	};
 }
