fc8a44c2d847f66912d093e8bcaacad23ff14abf
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-04-22 08:50:41 -0500

    Make copies of products to avoid mutating the original list

--- client/state/products-list/actions.js
+++ client/state/products-list/actions.js
@@ -2,25 +2,31 @@
  * Internal dependencies
  */
 import wpcom from 'calypso/lib/wp';
-import { createProductObject } from './assembler';
 import {
 	PRODUCTS_LIST_RECEIVE,
 	PRODUCTS_LIST_REQUEST,
 	PRODUCTS_LIST_REQUEST_FAILURE,
 } from 'calypso/state/action-types';
+import { ensureNumericCost } from './assembler';
 
 import 'calypso/state/products-list/init';
 
 export function receiveProductsList( productsList ) {
 	// Since the request succeeded, productsList should be guaranteed non-null;
 	// thus, we don't have any safety checks before this line.
-	Object.keys( productsList ).forEach(
-		( slug ) => ( productsList[ slug ] = createProductObject( productsList[ slug ] ) )
+
+	// Create a completely new products list to avoid mutating the original
+	const sanitizedProductsList = Object.entries( productsList ).reduce(
+		( acc, [ slug, product ] ) => {
+			acc[ slug ] = ensureNumericCost( product );
+			return acc;
+		},
+		{}
 	);
 
 	return {
 		type: PRODUCTS_LIST_RECEIVE,
-		productsList,
+		productsList: sanitizedProductsList,
 	};
 }
 
