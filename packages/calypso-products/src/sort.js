5db0a39e52764e5821860ca026c056075065d053
Payton Swick <payton@foolord.com>
2021-04-22 15:39:06 -0400

    Refactor sortProducts to remove lodash
    Also to make it clear this is used only for the purchases list via the
    `getPurchases` selector and not the cart.

--- packages/calypso-products/src/sort.js
+++ packages/calypso-products/src/sort.js
@@ -1,64 +1,56 @@
 /**
- * External dependencies
- */
-
-import { difference, flatten, groupBy, sortBy } from 'lodash';
-
-/**
  * Internal dependencies
  */
-import { getDomainProductRanking, isCredits, isDomainProduct, isPlan } from '.';
+import { getDomainProductRanking, isDomainProduct, isPlan } from './index';
+
+// https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore
+const groupBy = ( items, key ) =>
+	items.reduce(
+		( r, v, i, a, k = v[ key ] ) => ( ( r[ k ] || ( r[ k ] = [] ) ).push( v ), r ),
+		{}
+	);
 
 /**
- * Sorts all products in the following order:
+ * Sort all purchases in the following order:
  *
- * 1. Plan
- * 2. Domain Cart Items. Included in plans come first.
+ * 1. Plans.
+ * 2. Domains. Included in plans come first.
  *   a. Domain Registration
  *   b. Domain Mapping
  *   c. Privacy Protection
- * 3. Other Cart Items
- * 4. Credits Cart Item
+ * 3. Others.
  *
- * @param {object[]} products
- *
- * @returns {object[]} the sorted list of items in the shopping cart
+ * @param {object[]} products The purchase objects to sort
+ * @returns {object[]} the sorted list of purchases
  */
-
-function sortProducts( products ) {
+export default function sortProducts( products ) {
 	let domainItems;
 
 	const planItems = products.filter( isPlan );
 
 	const includedItems = products.filter( isIncludedWithPlan );
 
-	domainItems = difference( products, includedItems );
+	domainItems = products.filter( ( product ) => ! includedItems.includes( product ) );
 	domainItems = domainItems.filter( isDomainProduct );
 	domainItems = Object.entries( groupBy( domainItems, 'meta' ) );
-	domainItems = sortBy( domainItems, function ( pair ) {
+	domainItems = domainItems.sort( function ( pair ) {
 		if ( pair[ 1 ][ 0 ] && pair[ 1 ][ 0 ].cost === 0 ) {
 			return -1;
 		}
 		return pair[ 0 ];
 	} );
 	domainItems = domainItems.map( function ( pair ) {
-		return sortBy( pair[ 1 ], getDomainProductRanking );
+		return pair[ 1 ].sort( getDomainProductRanking );
 	} );
-	domainItems = flatten( domainItems );
+	domainItems = domainItems.flat();
 
-	const creditItems = products.filter( isCredits );
+	const otherItems = products.filter(
+		( product ) => ! [ ...planItems, ...domainItems, ...includedItems ].includes( product )
+	);
 
-	const otherItems = difference( products, planItems, domainItems, includedItems, creditItems );
-
-	return planItems
-		.concat( includedItems )
-		.concat( domainItems )
-		.concat( otherItems )
-		.concat( creditItems );
+	return planItems.concat( includedItems ).concat( domainItems ).concat( otherItems );
 }
 
 function isIncludedWithPlan( purchase ) {
 	return 'included' === purchase.expiryStatus;
 }
-
-export default sortProducts;
