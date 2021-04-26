52b76c3c6d884622690bd6567389c475a7f1e98d
Kev <kevin.zoschke@automattic.com>
2021-04-26 11:09:59 -0400

    Add helper to detect Jetpack legacy plan (#52292)

--- packages/calypso-products/src/index.ts
+++ packages/calypso-products/src/index.ts
@@ -3,3 +3,5 @@ export * from './types';
 export * from './constants';
 export * from './utils';
 export { default as getIntervalTypeForTerm } from './get-interval-type-for-term';
+export { default as isJetpackLegacyItem } from './is-jetpack-legacy-item';
+export { default as isJetpackPurchasableItem } from './is-jetpack-purchasable-item';
--- packages/calypso-products/src/is-jetpack-purchasable-item.ts
+++ packages/calypso-products/src/is-jetpack-purchasable-item.ts
@@ -3,10 +3,15 @@
  */
 import { JETPACK_LEGACY_PLANS, JETPACK_PRODUCTS_LIST, JETPACK_RESET_PLANS } from './constants';
 
+/**
+ * Type dependencies
+ */
+import type { JetpackPurchasableItem } from './types';
+
 export default function isJetpackPurchasableItem(
 	itemSlug: string,
 	options: { includeLegacy?: boolean } = {}
-): boolean {
+): itemSlug is JetpackPurchasableItem {
 	return [
 		...JETPACK_PRODUCTS_LIST,
 		...JETPACK_RESET_PLANS,
