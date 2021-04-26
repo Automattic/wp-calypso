a2fbbc5ee10b0114594a4edb38109a664fd48be3
nsakaimbo <nsakaimbo@users.noreply.github.com>
2021-04-26 15:36:23 -0600

    Remove redundant keys

--- packages/calypso-config/src/desktop.ts
+++ packages/calypso-config/src/desktop.ts
@@ -8,16 +8,8 @@ const config = {
 	env: 'production',
 	env_id: 'desktop',
 	client_slug: 'desktop',
-	i18n_default_locale_slug: 'en',
-	facebook_api_key: '249643311490',
-	rebrand_cities_prefix: 'rebrandcitiessite',
-	livechat_support_locales: [ 'en', 'es', 'pt-br' ],
 	readerFollowingSource: 'desktop',
-	siftscience_key: 'a4f69f6759',
-	wpcom_signup_id: '39911',
-	wpcom_signup_key: 'cOaYKdrkgXz8xY7aysv4fU6wL6sK5J8a6ojReEIAPwggsznj4Cb6mW0nffTxtYT8',
 	boom_analytics_key: 'desktop',
-	google_analytics_key: 'UA-10673494-10',
 	google_recaptcha_site_key: '6LdoXcAUAAAAAM61KvdgP8xwnC19YuzAiOWn5Wtn',
 };
 
