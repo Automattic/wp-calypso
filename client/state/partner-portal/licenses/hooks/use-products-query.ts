ab39ec72fdf12b76630737ecb68d47aac1b8974b
Atanas Angelov <hi@atanas.dev>
2021-04-27 19:45:54 +0300

    Partner Portal: Exclude legacy plans from the Issue License dropdown (#52336)

--- client/state/partner-portal/licenses/hooks/use-products-query.ts
+++ client/state/partner-portal/licenses/hooks/use-products-query.ts
@@ -10,10 +10,27 @@ import { APIProductFamily } from 'calypso/state/partner-portal/types';
 import wpcom from 'calypso/lib/wp';
 
 function queryProducts(): Promise< APIProductFamily[] > {
-	return wpcom.req.get( {
-		apiNamespace: 'wpcom/v2',
-		path: '/jetpack-licensing/product-families',
-	} );
+	return wpcom.req
+		.get( {
+			apiNamespace: 'wpcom/v2',
+			path: '/jetpack-licensing/product-families',
+		} )
+		.then( ( data: APIProductFamily[] ) => {
+			const exclude = [ 'free', 'personal', 'premium', 'professional' ];
+
+			return data
+				.map( ( family ) => {
+					return {
+						...family,
+						products: family.products.filter( ( product ) => {
+							return exclude.indexOf( product.slug ) === -1;
+						} ),
+					};
+				} )
+				.filter( ( family ) => {
+					return family.products.length > 0;
+				} );
+		} );
 }
 
 export default function useProductsQuery< TError = unknown, TData = unknown >(
