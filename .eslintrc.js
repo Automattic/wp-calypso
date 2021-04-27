b2aeaaf5b791fc9bb7f30cfa85473107a7488f2c
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-04-27 14:39:45 +0300

    Framework: Refactor away from `_.replace()` (#52234)
    * Framework: Refactor away from _.replace()
    
    * ESLint: Deprecate _.replace()
    
    * Remove optional chaining and fix failing test

--- .eslintrc.js
+++ .eslintrc.js
@@ -425,6 +425,7 @@ module.exports = {
 		'you-dont-need-lodash-underscore/pad-start': 'error',
 		'you-dont-need-lodash-underscore/reduce-right': 'error',
 		'you-dont-need-lodash-underscore/repeat': 'error',
+		'you-dont-need-lodash-underscore/replace': 'error',
 		'you-dont-need-lodash-underscore/reverse': 'error',
 		'you-dont-need-lodash-underscore/select': 'error',
 		'you-dont-need-lodash-underscore/slice': 'error',
--- client/my-sites/domains/domain-management/name-servers/dns-templates.jsx
+++ client/my-sites/domains/domain-management/name-servers/dns-templates.jsx
@@ -1,9 +1,8 @@
 /**
  * External dependencies
  */
-
 import React from 'react';
-import { find, replace } from 'lodash';
+import { find } from 'lodash';
 import { localize } from 'i18n-calypso';
 
 /**
@@ -58,7 +57,7 @@ class DnsTemplates extends React.Component {
 					dnsTemplateService: dnsTemplates.MICROSOFT_OFFICE365.SERVICE,
 					modifyVariables: ( variables ) =>
 						Object.assign( {}, variables, {
-							mxdata: replace( variables.domain, '.', '-' ) + '.mail.protection.outlook.com',
+							mxdata: variables.domain.replace( '.', '-' ) + '.mail.protection.outlook.com',
 						} ),
 				},
 				{
