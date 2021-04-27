8e96b37d76829952df4cca3e3876ead0d9322fa3
Razvan Papadopol <razvan.papadopol@automattic.com>
2021-04-27 11:47:49 +0300

    load experiment earlier

--- client/signup/controller.js
+++ client/signup/controller.js
@@ -102,7 +102,9 @@ export const removeP2SignupClassName = function () {
 export default {
 	redirectTests( context, next ) {
 		const currentFlowName = getFlowName( context.params );
-		currentFlowName === 'onboarding' && loadExperimentAssignment( 'refined_reskin_v1' );
+		currentFlowName === 'onboarding' &&
+			loadExperimentAssignment( 'refined_reskin_v1' ) &&
+			loadExperimentAssignment( 'design_picker_after_onboarding' );
 		currentFlowName === 'launch-site' && loadExperimentAssignment( 'hide_ecommerce_launch_site' );
 		if ( context.pathname.indexOf( 'new-launch' ) >= 0 ) {
 			next();
