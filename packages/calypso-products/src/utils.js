6157406c2c0b47f9d3c71c4ed96e4e8e20e04345
Payton Swick <payton@foolord.com>
2021-04-27 13:54:11 -0400

    Restore single-plan search of findCurrencyFromPlans
    This is how it worked with lodash.

--- packages/calypso-products/src/utils.js
+++ packages/calypso-products/src/utils.js
@@ -3,6 +3,5 @@
  */
 
 export const findCurrencyFromPlans = ( plans ) => {
-	const firstPlanWithCurrency = plans.find( ( plan ) => plan.currency_code );
-	return firstPlanWithCurrency?.currency_code ?? 'USD';
+	return plans[ 0 ]?.currency_code ?? 'USD';
 };
