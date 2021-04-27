e32d37aa483dd8857579e21c40bc839bfd1ac14d
Payton Swick <payton@foolord.com>
2021-04-27 13:56:58 -0400

    Simplify getDomain logic a bit

--- packages/calypso-products/src/get-domain.js
+++ packages/calypso-products/src/get-domain.js
@@ -5,11 +5,5 @@ import { formatProduct } from './format-product';
 
 export function getDomain( product ) {
 	product = formatProduct( product );
-
-	const domainToBundle = product.extra?.domain_to_bundle;
-	if ( domainToBundle ) {
-		return domainToBundle;
-	}
-
-	return product.meta;
+	return product.extra?.domain_to_bundle ?? product.meta;
 }
