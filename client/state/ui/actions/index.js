adc2558c356b16a6820be665697c4959957e74f0
Jarda Snajdr <jsnajdr@gmail.com>
2021-04-29 08:16:23 +0200

    Remove state.ui.isPreviewShowing (#52364)

--- client/state/ui/actions/index.js
+++ client/state/ui/actions/index.js
@@ -3,7 +3,6 @@
  */
 export { navigate } from './navigate';
 export { replaceHistory } from './history';
-export { setPreviewShowing } from './preview';
 export { setSection, setSectionLoading } from '../section/actions';
 export { setSelectedSiteId, setAllSitesSelected } from './set-sites';
 export { showMasterbar, hideMasterbar } from '../masterbar-visibility/actions';
