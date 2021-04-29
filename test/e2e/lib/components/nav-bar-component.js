880ce1bdceefeb9387b6f045946e282a8b49fcde
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2021-04-29 15:58:29 +0200

    E2E: Remove unnecessary presence checks from clickMySites helper (#52386)

--- test/e2e/lib/components/nav-bar-component.js
+++ test/e2e/lib/components/nav-bar-component.js
@@ -41,14 +41,6 @@ export default class NavBarComponent extends AsyncBaseContainer {
 	async clickMySites() {
 		const mySitesSelector = by.css( 'header.masterbar a.masterbar__item' );
 		await driverHelper.clickWhenClickable( this.driver, mySitesSelector );
-		await driverHelper.isEventuallyPresentAndDisplayed(
-			this.driver,
-			by.css( '.sidebar__menu-wrapper' )
-		);
-		return await driverHelper.isEventuallyPresentAndDisplayed(
-			this.driver,
-			by.css( '.is-group-sites' )
-		);
 	}
 	hasUnreadNotifications() {
 		return this.driver
