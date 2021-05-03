7f0dd9576f5e3e009e74684fdc26dcab5123cd41
cpap <papazoglou.charalampos@gmail.com>
2021-05-03 18:26:29 +0300

    Reader Sidebar: removes language & country restriction from upsell nudge.

--- client/reader/sidebar/reader-sidebar-nudges/index.jsx
+++ client/reader/sidebar/reader-sidebar-nudges/index.jsx
@@ -3,7 +3,7 @@
  */
 import React, { Fragment } from 'react';
 import { connect, useDispatch } from 'react-redux';
-import { localize, getLocaleSlug } from 'i18n-calypso';
+import { localize } from 'i18n-calypso';
 import debugFactory from 'debug';
 
 /**
@@ -11,7 +11,6 @@ import debugFactory from 'debug';
  */
 import QuerySitePlans from 'calypso/components/data/query-site-plans';
 import isDomainOnlySite from 'calypso/state/selectors/is-domain-only-site';
-import { getCurrentUserCountryCode } from 'calypso/state/current-user/selectors';
 import isEligibleForFreeToPaidUpsell from 'calypso/state/selectors/is-eligible-for-free-to-paid-upsell';
 import { isJetpackSite } from 'calypso/state/sites/selectors';
 import getSites from 'calypso/state/selectors/get-sites';
@@ -54,16 +53,10 @@ function mapStateToProps( state ) {
 	const siteCount = getSites( state ).length;
 	const siteId = getPrimarySiteId( state );
 	const siteSlug = getPrimarySiteSlug( state );
-	const devCountryCode = isDevelopment && global.window && global.window.userCountryCode;
-	const countryCode = devCountryCode || getCurrentUserCountryCode( state );
-	const userLocale = getLocaleSlug( state );
-	const isEnglish = [ 'en', 'en-gb' ].indexOf( userLocale ) !== -1;
 
 	isDevelopment &&
 		debug(
-			'country: %s, locale: %s, siteCount: %d, eligible: %s',
-			countryCode,
-			userLocale,
+			'siteCount: %d, eligible: %s',
 			siteCount,
 			isEligibleForFreeToPaidUpsell( state, siteId )
 		);
@@ -75,10 +68,7 @@ function mapStateToProps( state ) {
 			siteCount === 1 && // available when a user owns one site only
 			! isJetpackSite( state, siteId ) && // not for Jetpack sites
 			! isDomainOnlySite( state, siteId ) && // not for domain only sites
-			isEligibleForFreeToPaidUpsell( state, siteId ) &&
-			// This nudge only shows up to US EN users.
-			isEnglish &&
-			'US' === countryCode,
+			isEligibleForFreeToPaidUpsell( state, siteId ),
 	};
 }
 
