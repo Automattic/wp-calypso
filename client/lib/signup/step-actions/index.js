00c9446c3609aefe899c7f1f77a686390debccac
Camilla Krag Jensen <naxoc@users.noreply.github.com>
2021-05-03 20:20:39 +0200

    Add an option to site creation call for P2

--- client/lib/signup/step-actions/index.js
+++ client/lib/signup/step-actions/index.js
@@ -680,6 +680,7 @@ export function createWpForTeamsSite( callback, dependencies, stepData, reduxSto
 			theme: themeSlugWithRepo,
 			timezone_string: guessTimezone(),
 			is_wpforteams_site: true,
+			p2_initialize_as_hub: true,
 		},
 		validate: false,
 	};
