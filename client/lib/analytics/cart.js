7d83f9cb30870d825649f0a3193de8aa282fe5e8
Marin Atanasov <tyxla@abv.bg>
2021-05-04 23:12:32 +0300

    Framework: Refactor away from _.differenceWith()

--- client/lib/analytics/cart.js
+++ client/lib/analytics/cart.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { differenceWith, isEqual, each, omit } from 'lodash';
+import { isEqual, each, omit } from 'lodash';
 
 /**
  * Internal dependencies
@@ -10,12 +10,16 @@ import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { recordAddToCart } from 'calypso/lib/analytics/record-add-to-cart';
 import { getAllCartItems } from 'calypso/lib/cart-values/cart-items';
 
+function difference( items1, items2 ) {
+	return items1.filter( ( a ) => ! items2.some( ( b ) => isEqual( a, b ) ) );
+}
+
 export function recordEvents( previousCart, nextCart ) {
 	const previousItems = getAllCartItems( previousCart );
 	const nextItems = getAllCartItems( nextCart );
 
-	each( differenceWith( nextItems, previousItems, isEqual ), recordAddEvent );
-	each( differenceWith( previousItems, nextItems, isEqual ), recordRemoveEvent );
+	each( difference( nextItems, previousItems, isEqual ), recordAddEvent );
+	each( difference( previousItems, nextItems, isEqual ), recordRemoveEvent );
 }
 
 function removeNestedProperties( cartItem ) {
