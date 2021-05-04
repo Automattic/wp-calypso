ecebc29b0f735a8025ae2cbfea880aa3a31997b7
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-05-04 08:29:50 -0500

    Remove the jetpackReverseFeaturedProducts test, resetting to the control variation

--- client/my-sites/plans/jetpack-plans/product-grid/products-order.ts
+++ client/my-sites/plans/jetpack-plans/product-grid/products-order.ts
@@ -18,11 +18,6 @@ import {
 } from '@automattic/calypso-products';
 
 /**
- * Internal dependencies
- */
-import { getForCurrentCROIteration, Iterations } from '../iterations';
-
-/**
  * Type dependencies
  */
 import type { JetpackPlanSlugs, JetpackProductSlug } from '@automattic/calypso-products';
@@ -46,27 +41,5 @@ const PRODUCT_POSITION_IN_GRID: Record< string, number > = {
 	...setProductsInPosition( JETPACK_CRM_FREE_PRODUCTS, 80 ),
 };
 
-const REVERSE_FEATURED_PRODUCTS_POSITION_IN_GRID: Record< string, number > = {
-	...setProductsInPosition( JETPACK_COMPLETE_PLANS, 1 ),
-	[ PLAN_JETPACK_SECURITY_DAILY ]: 10,
-	[ PLAN_JETPACK_SECURITY_DAILY_MONTHLY ]: 10,
-	[ PRODUCT_JETPACK_BACKUP_DAILY ]: 20,
-	[ PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY ]: 20,
-	[ PLAN_JETPACK_SECURITY_REALTIME ]: 30,
-	[ PLAN_JETPACK_SECURITY_REALTIME_MONTHLY ]: 30,
-	[ PRODUCT_JETPACK_BACKUP_REALTIME ]: 40,
-	[ PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY ]: 40,
-	...setProductsInPosition( JETPACK_SCAN_PRODUCTS, 50 ),
-	...setProductsInPosition( JETPACK_ANTI_SPAM_PRODUCTS, 60 ),
-	...setProductsInPosition( JETPACK_SEARCH_PRODUCTS, 70 ),
-	...setProductsInPosition( JETPACK_CRM_FREE_PRODUCTS, 80 ),
-};
-
-export const getProductPosition = ( slug: JetpackPlanSlugs | JetpackProductSlug ): number => {
-	const positions =
-		getForCurrentCROIteration( {
-			[ Iterations.REVERSE_FEATURED ]: REVERSE_FEATURED_PRODUCTS_POSITION_IN_GRID,
-		} ) ?? PRODUCT_POSITION_IN_GRID;
-
-	return positions[ slug ] ?? 100;
-};
+export const getProductPosition = ( slug: JetpackPlanSlugs | JetpackProductSlug ): number =>
+	PRODUCT_POSITION_IN_GRID[ slug ] ?? 100;
