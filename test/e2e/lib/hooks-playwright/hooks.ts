9fa139b11ca7cdb4b4b68dd23da2e1980d508cf1
Edwin Takahashi <egao@outlook.com>
2021-05-04 14:36:31 -0700

    Update test spec file with multiple subsuites as well as an expected failing suite.
    Change hook to launch new browser, then browsercontext then a new page at beginning of test run.

--- test/e2e/lib/hooks-playwright/hooks.ts
+++ test/e2e/lib/hooks-playwright/hooks.ts
@@ -3,15 +3,21 @@
 /**
  * External dependencies
  */
+import config from 'config';
 import { BrowserManager } from '@automattic/calypso-e2e';
 
+const playwrightTimeoutMS = config.get( 'playwrightTimeoutMS' );
+
 /**
- * Hook to launch a Browser and obtain the BrowserContext.
+ * Hook to launch a new instance of BrowserContext then a new
+ * Page for testing.
  *
  * @returns {void} No return value.
  */
 before( 'Launch browser instance', async function () {
 	this.browserContext = await BrowserManager.newBrowserContext();
+	await this.browserContext.setDefaultTimeout( playwrightTimeoutMS );
+	this.page = await this.browserContext.newPage();
 } );
 
 /**
--- test/e2e/specs-playwright/wp-log-in-out-spec.js
+++ test/e2e/specs-playwright/wp-log-in-out-spec.js
@@ -13,22 +13,47 @@ import LoginPage from '../lib/pages/login-page';
  */
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 
-describe( `Auth Screen @canary @parallel`, function () {
+describe( `Main Suite 1`, function () {
 	this.timeout( mochaTimeOut );
-
-	// Page represents a tab in a browser.
-	// Test steps interact with the page to execute its instructions.
 	let page;
 
-	// Open a new page (tab) within the BrowserContext.
-	// This is akin to the following call in the Selenium test:
-	// driver = await driverManager.startBrowser();
-	before( 'Open new test tab', async function () {
-		page = await this.browserContext.newPage();
+	describe( 'Subsuite 1', function () {
+		step( 'Can see the log in page', async function () {
+			page = this.page;
+			const url = LoginPage.getLoginURL();
+			/*
+			Waits for network activity to cease.
+			Only as a proof of concept. In a production test, should check
+			for the presence of desired elements using a selector.
+			*/
+			return await page.goto( url, { waitUntill: 'networkidle' } );
+		} );
 	} );
 
-	describe( 'Loading the log-in page', function () {
-		step( 'Can see the log in page', async function () {
+	describe( 'Subsuite 2', function () {
+		step( 'Can alsosee the log in page', async function () {
+			const url = LoginPage.getLoginURL();
+			/*
+			Waits for network activity to cease.
+			Only as a proof of concept. In a production test, should check
+			for the presence of desired elements using a selector.
+			*/
+			return await page.goto( url, { waitUntill: 'networkidle' } );
+		} );
+	} );
+} );
+
+describe( `Main Suite 2`, function () {
+	this.timeout( mochaTimeOut );
+	let page;
+
+	describe( 'Subsuite 1', function () {
+		step( 'Should fail', async function () {
+			page = this.page;
+			await page.click( 'non-existing-selector' );
+		} );
+
+		step( 'Should be aborted', async function () {
 			const url = LoginPage.getLoginURL();
 			/*
 			Waits for network activity to cease.
