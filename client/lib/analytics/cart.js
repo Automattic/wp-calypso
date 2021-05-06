f52b39f5d25c61eccf4f2f8ae1012ec78cee4368
Marin Atanasov <tyxla@abv.bg>
2021-05-06 14:00:09 +0300

    Analytics: Remove unused cart analytics functions

--- client/lib/analytics/cart.js
+++ client/lib/analytics/cart.js
@@ -1,22 +1,13 @@
 /**
  * External dependencies
  */
-import { differenceWith, isEqual, each, omit } from 'lodash';
+import { omit } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { recordAddToCart } from 'calypso/lib/analytics/record-add-to-cart';
-import { getAllCartItems } from 'calypso/lib/cart-values/cart-items';
-
-export function recordEvents( previousCart, nextCart ) {
-	const previousItems = getAllCartItems( previousCart );
-	const nextItems = getAllCartItems( nextCart );
-
-	each( differenceWith( nextItems, previousItems, isEqual ), recordAddEvent );
-	each( differenceWith( previousItems, nextItems, isEqual ), recordRemoveEvent );
-}
 
 function removeNestedProperties( cartItem ) {
 	return omit( cartItem, [ 'extra' ] );
@@ -25,8 +16,4 @@ function removeNestedProperties( cartItem ) {
 export function recordAddEvent( cartItem ) {
 	recordTracksEvent( 'calypso_cart_product_add', removeNestedProperties( cartItem ) );
 	recordAddToCart( { cartItem } );
-}
-
-function recordRemoveEvent( cartItem ) {
-	recordTracksEvent( 'calypso_cart_product_remove', removeNestedProperties( cartItem ) );
 }
