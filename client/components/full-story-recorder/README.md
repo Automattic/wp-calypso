7e157ac22d62bc9587e741207be93e625cd8daca
Caleb Bauermeister <cbauerman@gmail.com>
2021-05-06 10:47:06 -0700

    Move FullStoryInit to recorder component
    # Conflicts:
    #	client/boot/common.js

---
+++ client/components/full-story-recorder/README.md
@@ -0,0 +1,1 @@
+# FullStory Recorder Component
--- client/components/full-story-recorder/index.tsx
+++ client/components/full-story-recorder/index.tsx
@@ -1,12 +1,32 @@
 /**
  * External dependencies
  */
+import { useSelector } from 'react-redux';
 import { useEffect, FunctionComponent } from 'react';
-import { shutdown, restart } from '@fullstory/browser';
+import { init, shutdown, restart } from '@fullstory/browser';
+
+/**
+ * Internal dependencies
+ */
+import { getCurrentUserCountryCode } from 'calypso/state/current-user/selectors';
+import { TRACKING_IDS } from 'calypso/lib/analytics/ad-tracking/constants';
+
+interface WindowWithFullStory extends Window {
+	_fs_initialized?: boolean;
+}
 
 const FullStoryRecorder: FunctionComponent = () => {
+	const currentUserCountryCode = useSelector< string | null >( ( state ) =>
+		getCurrentUserCountryCode( state )
+	);
+
 	useEffect( () => {
-		restart();
+		if ( ! ( window as WindowWithFullStory )._fs_initialized && 'US' === currentUserCountryCode ) {
+			init( { orgId: TRACKING_IDS.fullStoryOrgId } );
+		} else {
+			restart();
+		}
+
 		return () => {
 			shutdown();
 		};
