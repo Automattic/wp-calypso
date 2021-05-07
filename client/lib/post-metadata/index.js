87483580e93e48ca698ece2f688f7d2df2c19dfa
Marin Atanasov <tyxla@abv.bg>
2021-05-07 17:19:10 +0300

    Move homepageTemplate to component

--- client/lib/post-metadata/index.js
+++ client/lib/post-metadata/index.js
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import { find } from 'lodash';
-import { getThemeIdFromStylesheet } from 'calypso/state/themes/utils';
 
 function getValueByKey( metadata, key ) {
 	const meta = find( metadata, { key: key } );
@@ -13,21 +12,6 @@ function getValueByKey( metadata, key ) {
 }
 
 const PostMetadata = {
-	/**
-	 * Given a post object, returns the theme id of a template-first theme, or `undefined` if the value
-	 * cannot be determined.
-	 *
-	 * @param  {object} post Post object
-	 * @returns {string|undefined} ThemeId on success.
-	 */
-	homepageTemplate: function ( post ) {
-		if ( ! post ) {
-			return;
-		}
-
-		return getThemeIdFromStylesheet( getValueByKey( post.metadata, '_tft_homepage_template' ) );
-	},
-
 	/**
 	 * Given a post object, returns the custom post meta description for
 	 * the post, or undefined if it is has not been set.
