9f5b8f4c001acf602685d9850067af668d2573bf
Dehan <dehandecroos@gmail.com>
2021-05-07 18:54:30 +0530

    Initial work on marketplace thank you page

--- client/my-sites/checkout/checkout-thank-you/marketplace/marketplace-thank-you.tsx
+++ client/my-sites/checkout/checkout-thank-you/marketplace/marketplace-thank-you.tsx
@@ -5,6 +5,8 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { useSelector } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
+import { isDesktop } from '@automattic/viewport';
+
 /**
  * Internal dependencies
  */
@@ -12,23 +14,49 @@ import Masterbar from 'calypso/layout/masterbar/masterbar';
 import Item from 'calypso/layout/masterbar/item';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 import getPreviousPath from 'calypso/state/selectors/get-previous-path';
+import yoastInstalledImage from 'calypso/assets/images/marketplace/yoast-installed.svg';
+
+/**
+ * style dependencies
+ */
+import './style.scss';
 
 const MarketplaceThankYouContainer = styled.div`
-	margin-left: -20px;
-	margin-top: -20px;
 	background-color: #fff;
 `;
 
-const MarketplacePurchaseHeader = styled.div`
-	width: 100%;
-	height: 240px;
-	background-color: var( --studio-grey-0 );
+const MarketplaceThankYouHeader = styled.div`
+    width: 100%;
+    height: 240px;
+    background-color: var( --studio-gray-0 );
+    margin-top: var(--masterbar-height);
+    display: flex;
+    justify-content: center;
+}`;
+
+const MarketplaceThankyouTitle = styled.h1`
+	font-size: ${ ( { subtitle = false } ) => ( subtitle ? '1.5em' : '2em' ) };
 `;
 
-const MarketplaceThankYou = ( { purchases } ) => {
+const MarketplaceThankyouSection = styled.div`
+    margin-bottom: 50px;
+}`;
+
+const ThankYouBody = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	margin-top: 50px;
+	> div {
+		width: 460px;
+	}
+`;
+
+const MarketplaceThankYou = () => {
 	const selectedSite = useSelector( getSelectedSite );
 	const previousPath = useSelector( getPreviousPath );
 	const translate = useTranslate();
+	const imageWidth = isDesktop() ? 200 : 150;
 
 	return (
 		<>
@@ -47,9 +75,50 @@ const MarketplaceThankYou = ( { purchases } ) => {
 				/>
 			</Masterbar>
 			<MarketplaceThankYouContainer className="marketplace-thank-you__container">
-				<MarketplacePurchaseHeader />
-				<h1>MarketplacePurchaseDetails</h1>
-				<div>{ JSON.stringify( purchases ) }</div>
+				<MarketplaceThankYouHeader>
+					<img alt="yoast logo" width={ imageWidth } src={ yoastInstalledImage }></img>
+				</MarketplaceThankYouHeader>
+				<ThankYouBody>
+					<div>
+						<MarketplaceThankyouSection>
+							<MarketplaceThankyouTitle className="marketplace-thank-you__body-header wp-brand-font">
+								{ translate( 'Yoast SEO Premium is installed' ) }
+							</MarketplaceThankyouTitle>
+							<p>{ translate( "You're all set. Yoast SEO is installed and ready to go." ) }</p>
+						</MarketplaceThankyouSection>
+						<MarketplaceThankyouSection>
+							<MarketplaceThankyouTitle
+								subtitle
+								className="marketplace-thank-you__body-header wp-brand-font"
+							>
+								{ translate( 'What’s next?' ) }
+							</MarketplaceThankyouTitle>
+							<h3>{ translate( 'Plugin setup' ) }</h3>
+							<p>
+								{ translate(
+									'Get to know Yoast SEO and customize it, so you can hit the ground running.'
+								) }
+							</p>
+							<h3>{ translate( 'Start putting SEO to work' ) }</h3>
+							<p>
+								{ translate( "Improve your site's performance and rank higher with a few tips." ) }
+							</p>
+						</MarketplaceThankyouSection>
+						<MarketplaceThankyouSection>
+							<MarketplaceThankyouTitle
+								subtitle
+								className="marketplace-thank-you__body-header wp-brand-font"
+							>
+								{ translate( 'How can we help?' ) }
+							</MarketplaceThankyouTitle>
+							<p>
+								{ translate(
+									'Our Happiness Engineers are here if you need help, or if you have any questions.'
+								) }
+							</p>
+						</MarketplaceThankyouSection>
+					</div>
+				</ThankYouBody>
 			</MarketplaceThankYouContainer>
 		</>
 	);
