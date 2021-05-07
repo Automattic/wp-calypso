4a9ccce6514283afda854a06ae003915e6799784
Marin Atanasov <tyxla@abv.bg>
2021-05-07 16:49:41 +0300

    Move publicize metadata functionality to component

--- client/lib/post-metadata/index.js
+++ client/lib/post-metadata/index.js
@@ -1,14 +1,8 @@
 /**
  * External dependencies
  */
-
 import { find } from 'lodash';
 import { getThemeIdFromStylesheet } from 'calypso/state/themes/utils';
-
-/**
- * Module variables
- */
-const REGEXP_PUBLICIZE_SERVICE_SKIPPED = /^_wpas_skip_(\d+)$/;
 
 function getValueByKey( metadata, key ) {
 	const meta = find( metadata, { key: key } );
@@ -18,52 +12,7 @@ function getValueByKey( metadata, key ) {
 	}
 }
 
-function getConnectionIdsByPattern( metadata, pattern ) {
-	if ( ! metadata ) {
-		return [];
-	}
-
-	return metadata
-		.filter( function ( meta ) {
-			return pattern.test( meta.key ) && 1 === parseInt( meta.value, 10 );
-		} )
-		.map( function ( meta ) {
-			return parseInt( meta.key.match( pattern )[ 1 ], 10 );
-		} );
-}
-
 const PostMetadata = {
-	/**
-	 * Given a post object, returns the Publicize custom message assigned to
-	 * that post, or `undefined` if the value cannot be determined.
-	 *
-	 * @param  {object} post Post object
-	 * @returns {string}      Publicize custom message
-	 */
-	publicizeMessage: function ( post ) {
-		if ( ! post ) {
-			return;
-		}
-
-		return getValueByKey( post.metadata, '_wpas_mess' );
-	},
-
-	/**
-	 * Given a post object, returns an array of Publicize service IDs for which
-	 * the user has chosen not to have the post Publicized, or `undefined` if
-	 * the value cannot be determined.
-	 *
-	 * @param  {object} post Post object
-	 * @returns {Array}       Array of Publicize service IDs
-	 */
-	publicizeSkipped: function ( post ) {
-		if ( ! post ) {
-			return;
-		}
-
-		return getConnectionIdsByPattern( post.metadata, REGEXP_PUBLICIZE_SERVICE_SKIPPED );
-	},
-
 	/**
 	 * Given a post object, returns the theme id of a template-first theme, or `undefined` if the value
 	 * cannot be determined.
