ba277120fda6750d839a6f3d8609419e15b71cad
Marin Atanasov <tyxla@abv.bg>
2021-05-07 16:32:22 +0300

    Posts: Remove unused PostMetadata methods

--- client/lib/post-metadata/index.js
+++ client/lib/post-metadata/index.js
@@ -9,7 +9,6 @@ import { getThemeIdFromStylesheet } from 'calypso/state/themes/utils';
  * Module variables
  */
 const REGEXP_PUBLICIZE_SERVICE_SKIPPED = /^_wpas_skip_(\d+)$/;
-const REGEXP_PUBLICIZE_SERVICE_DONE = /^_wpas_done_(\d+)$/;
 
 function getValueByKey( metadata, key ) {
 	const meta = find( metadata, { key: key } );
@@ -50,22 +49,6 @@ const PostMetadata = {
 	},
 
 	/**
-	 * Given a post object, returns an array of Publicize service IDs to which
-	 * the post has been successfully Publicized, or `undefined` if the value
-	 * cannot be determined.
-	 *
-	 * @param  {object} post Post object
-	 * @returns {Array}       Array of Publicize service IDs
-	 */
-	publicizeDone: function ( post ) {
-		if ( ! post ) {
-			return;
-		}
-
-		return getConnectionIdsByPattern( post.metadata, REGEXP_PUBLICIZE_SERVICE_DONE );
-	},
-
-	/**
 	 * Given a post object, returns an array of Publicize service IDs for which
 	 * the user has chosen not to have the post Publicized, or `undefined` if
 	 * the value cannot be determined.
@@ -97,22 +80,6 @@ const PostMetadata = {
 	},
 
 	/**
-	 * Given a post object, returns a human-readable address label representing
-	 * the geographic location saved for that post, or `undefined` if the value
-	 * cannot be determined.
-	 *
-	 * @param  {object} post Post object
-	 * @returns {string}      Human-readable geographic address label
-	 */
-	geoLabel: function ( post ) {
-		if ( ! post ) {
-			return;
-		}
-
-		return getValueByKey( post.metadata, 'geo_address' );
-	},
-
-	/**
 	 * Given a post object, returns the custom post meta description for
 	 * the post, or undefined if it is has not been set.
 	 *
@@ -125,74 +92,6 @@ const PostMetadata = {
 		}
 
 		return getValueByKey( post.metadata, 'advanced_seo_description' );
-	},
-
-	/**
-	 * Given a post object, returns an array of float coordinates representing
-	 * the geographic location saved for that post, or `undefined` if the value
-	 * cannot be determined.
-	 *
-	 * @param  {object} post Post object
-	 * @returns {string}      Array of geographic float coordinates
-	 */
-	geoCoordinates: function ( post ) {
-		if ( ! post ) {
-			return;
-		}
-
-		const latitude = parseFloat( getValueByKey( post.metadata, 'geo_latitude' ) );
-		const longitude = parseFloat( getValueByKey( post.metadata, 'geo_longitude' ) );
-
-		if ( latitude && longitude ) {
-			return [ latitude, longitude ];
-		}
-	},
-
-	geoStaticMapUrl: function ( post ) {
-		if ( ! post ) {
-			return;
-		}
-
-		const coordinates = this.geoCoordinates( post );
-		if ( ! coordinates ) {
-			return;
-		}
-
-		return post?.geo?.map_url;
-	},
-
-	/**
-	 * Given a post object, return a boolean, indicating whether the geo-location data
-	 * associated with the post is allowed to be displayed publicly.
-	 *
-	 * @param {object} post Post object
-	 * @returns {boolean|null} Whether the geo-location data is shared publicly.
-	 */
-	geoIsSharedPublicly: function ( post ) {
-		if ( ! post ) {
-			return null;
-		}
-
-		const isSharedPublicly = getValueByKey( post.metadata, 'geo_public' );
-
-		if ( parseInt( isSharedPublicly, 10 ) ) {
-			return true;
-		}
-
-		if ( undefined === isSharedPublicly ) {
-			// If they have no geo_public value but they do have a lat/long, then we assume they saved with Calypso
-			// before it supported geo_public, in which case we should treat it as private.
-			if (
-				getValueByKey( post.metadata, 'geo_latitude' ) ||
-				getValueByKey( post.metadata, 'geo_longitude' )
-			) {
-				return false;
-			}
-
-			return true;
-		}
-
-		return false;
 	},
 };
 
