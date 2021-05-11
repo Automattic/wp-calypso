d16be1a72f30f54ac593964ef8ebcd4f09fd9b24
Dale du Preez <dale@automattic.com>
2021-05-11 10:43:35 +0200

    Update central helper function

--- client/lib/titan/get-titan-product-name.js
+++ client/lib/titan/get-titan-product-name.js
@@ -1,18 +1,8 @@
 /**
  * External dependencies
  */
-import i18n from 'i18n-calypso';
-
-/**
- * Internal dependencies
- */
-import { isEnabled } from '@automattic/calypso-config';
+import { translate } from 'i18n-calypso';
 
 export function getTitanProductName() {
-	if ( isEnabled( 'titan/phase-2' ) ) {
-		return i18n.translate( 'Email', {
-			comment: 'Email is the name of a WordPress.com product, not just a capitalization of "email"',
-		} );
-	}
-	return i18n.translate( 'Titan Mail' );
+	return translate( 'Professional Email' );
 }
