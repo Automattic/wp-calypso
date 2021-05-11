1f3178d61208796de6721b589e89bbad0b1d95ff
Sergio Cinos <sergio.cinos@gmail.com>
2021-05-11 05:42:05 +0200

    Do not install a cusotm chrome version

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -54,15 +54,6 @@ object RunCalypsoE2eDesktopTests : BuildType({
 				shopt -s globstar
 				set -x
 
-				# Chrome upgrade start
-				wget --no-verbose https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_90.0.4430.93-1_amd64.deb
-				sudo apt-get install -y ./google-chrome-stable_90.0.4430.93-1_amd64.deb
-				rm ./google-chrome-stable_90.0.4430.93-1_amd64.deb
-				cd test/e2e
-				npm install chromedriver --chromedriver-force-download --detect_chromedriver_version
-				cd ../..
-				# Chrome upgrade end
-
 				cd test/e2e
 				mkdir temp
 
@@ -228,15 +219,6 @@ object RunCalypsoE2eMobileTests : BuildType({
 			scriptContent = """
 				shopt -s globstar
 				set -x
-
-				# Chrome upgrade start
-				wget --no-verbose https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_90.0.4430.93-1_amd64.deb
-				sudo apt-get install -y ./google-chrome-stable_90.0.4430.93-1_amd64.deb
-				rm ./google-chrome-stable_90.0.4430.93-1_amd64.deb
-				cd test/e2e
-				npm install chromedriver --chromedriver-force-download --detect_chromedriver_version
-				cd ../..
-				# Chrome upgrade end
 
 				cd test/e2e
 				mkdir temp
