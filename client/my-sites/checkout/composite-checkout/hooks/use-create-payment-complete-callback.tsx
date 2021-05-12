eb195bf348b0111a6e882e1b3bb71d88486f8e00
Caleb Bauermeister <cbauerman@gmail.com>
2021-05-12 09:52:40 -0700

    Remove extra paren

--- client/my-sites/checkout/composite-checkout/hooks/use-create-payment-complete-callback.tsx
+++ client/my-sites/checkout/composite-checkout/hooks/use-create-payment-complete-callback.tsx
@@ -83,7 +83,7 @@ export default function useCreatePaymentCompleteCallback( {
 	const reduxStore = useStore();
 	const selectedSiteData = useSelector( getSelectedSite );
 	const adminUrl = selectedSiteData?.options?.admin_url;
-	const isEligibleForSignupDestinationResult = isEligibleForSignupDestination( responseCart ); );
+	const isEligibleForSignupDestinationResult = isEligibleForSignupDestination( responseCart );
 	const isJetpackNotAtomic =
 		useSelector(
 			( state ) => siteId && isJetpackSite( state, siteId ) && ! isAtomicSite( state, siteId )
