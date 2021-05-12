b5b851d440de603bb6c92cfa68b06377b017c7be
Matthew Reishus <mreishus@users.noreply.github.com>
2021-05-12 17:24:32 -0500

    Update e2e tests after multiSite themes was removed

--- test/e2e/specs/specs-calypso/wp-theme__multisite-activate-spec.js
+++ test/e2e/specs/specs-calypso/wp-theme__multisite-activate-spec.js
@@ -3,6 +3,8 @@
  */
 import assert from 'assert';
 import config from 'config';
+import { By } from 'selenium-webdriver';
+import * as driverHelper from '../lib/driver-helper';
 
 /**
  * Internal dependencies
@@ -11,7 +13,6 @@ import * as driverManager from '../../lib/driver-manager.js';
 import * as dataHelper from '../../lib/data-helper';
 import ThemesPage from '../../lib/pages/themes-page.js';
 import SidebarComponent from '../../lib/components/sidebar-component';
-import SiteSelectorComponent from '../../lib/components/site-selector-component';
 import LoginFlow from '../../lib/flows/login-flow.js';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
@@ -26,10 +27,10 @@ describe( `[${ host }] Themes: Activate a theme, all sites (${ screenSize }) @pa
 		this.expectedTheme = 'Twenty F';
 
 		this.loginFlow = new LoginFlow( this.driver, 'multiSiteUser' );
-		await this.loginFlow.loginAndSelectAllSites();
+		await this.loginFlow.loginAndSelectMySite();
 
 		this.sidebarComponent = await SidebarComponent.Expect( this.driver );
-		await this.sidebarComponent.selectAllSitesThemes();
+		await this.sidebarComponent.selectThemes();
 	} );
 
 	it( 'can search for free themes', async function () {
@@ -53,16 +54,18 @@ describe( `[${ host }] Themes: Activate a theme, all sites (${ screenSize }) @pa
 
 	it( 'can click activate', async function () {
 		await this.themesPage.clickPopoverItem( 'Activate' );
-		return ( this.siteSelector = await SiteSelectorComponent.Expect( this.driver ) );
+		const thanksModalShown = await driverHelper.isElementEventuallyLocatedAndVisible(
+			this.driver,
+			By.css( '.themes__thanks-modal' )
+		);
+		return assert(
+			thanksModalShown,
+			"The 'Thanks for Choosing Twenty Sixteen' modal was not displayed after activating"
+		);
 	} );
 
-	it( 'shows the site selector', async function () {
-		const siteSelectorShown = await this.siteSelector.displayed();
-		return assert( siteSelectorShown, 'The site selector was not shown' );
-	} );
-
-	it( 'can select the first site sites', async function () {
-		await this.siteSelector.selectFirstSite();
-		return await this.siteSelector.ok();
-	} );
+	// Some tests about activating a theme in the context of "All Sites" were
+	// removed - check git history. Visiting "/themes" while logged out should now
+	// force you to select a site before proceeding. You'll be redirected to
+	// "/themes/<site>" and be operating only in the context of one site.
 } );
