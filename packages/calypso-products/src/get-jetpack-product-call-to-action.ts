039fa8c71063b9d68238a64a3c52fe8dc911b750
Kevin Zoschke <kevin.zoschke@automattic.com>
2021-05-13 16:16:10 -0400

    Fix TS issue

--- packages/calypso-products/src/get-jetpack-product-call-to-action.ts
+++ packages/calypso-products/src/get-jetpack-product-call-to-action.ts
@@ -17,17 +17,15 @@ import type { Product } from './types';
 /**
  * Get Jetpack product call-to-action based on the product purchase object.
  *
- * @param {object} product Product purchase object
+ * @param {Product} product Product purchase object
  * @returns {TranslateResult} Product display name
  */
-export function getJetpackProductCallToAction(
-	product: Record< string, unknown >
-): TranslateResult | undefined {
+export function getJetpackProductCallToAction( product: Product ): TranslateResult | undefined {
 	product = formatProduct( product );
 	const jetpackProductsCallToActions = getJetpackProductsCallToAction() as Record<
 		string,
 		TranslateResult
 	>;
 
-	return jetpackProductsCallToActions?.[ ( product as Product ).product_slug ];
+	return jetpackProductsCallToActions?.[ product.product_slug ];
 }
