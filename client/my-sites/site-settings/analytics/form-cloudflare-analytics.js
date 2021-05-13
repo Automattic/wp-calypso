5c059106d7bcf7d9d67bb8f7ef59fca6f9194165
Renzo Canepa <rcanepag@gmail.com>
2021-05-13 09:45:16 -0400

    Undo changes to the Cloudflare component

--- client/my-sites/site-settings/analytics/form-cloudflare-analytics.js
+++ client/my-sites/site-settings/analytics/form-cloudflare-analytics.js
@@ -25,7 +25,7 @@ import SettingsSectionHeader from 'calypso/my-sites/site-settings/settings-secti
 import cloudflareIllustration from 'calypso/assets/images/illustrations/cloudflare-logo-small.svg';
 import {
 	findFirstSimilarPlanKey,
-	TYPE_SECURITY_DAILY,
+	TYPE_PREMIUM,
 	FEATURE_CLOUDFLARE_ANALYTICS,
 } from '@automattic/calypso-products';
 import UpsellNudge from 'calypso/blocks/upsell-nudge';
@@ -128,10 +128,10 @@ export function CloudflareAnalyticsSettings( {
 	const renderForm = () => {
 		const placeholderText = isRequestingSettings ? translate( 'Loading' ) : '';
 
-		const nudgeTitle = translate( 'Available with Security plans or higher' );
+		const nudgeTitle = translate( 'Available with Premium plans or higher' );
 
 		const plan = findFirstSimilarPlanKey( site.plan.product_slug, {
-			type: TYPE_SECURITY_DAILY,
+			type: TYPE_PREMIUM,
 		} );
 
 		const nudge = (
