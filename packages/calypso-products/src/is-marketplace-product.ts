fa15444f97382fae605402be924923f72bad6097
Dehan <dehandecroos@gmail.com>
2021-05-14 11:36:32 +0530

    Code Review fixes

--- packages/calypso-products/src/is-marketplace-product.ts
+++ packages/calypso-products/src/is-marketplace-product.ts
@@ -2,9 +2,7 @@
  * Internal dependencies
  */
 import { YOAST_SEO } from './constants';
-import { formatProduct } from './format-product';
 
-export function isMarketplaceProduct( product : any ): boolean {
-	product = formatProduct( product );
+export function isMarketplaceProduct( product: { productSlug: string } ): boolean {
 	return product.productSlug === YOAST_SEO;
 }
