a6bf094c159778002d84aa1194a227dd1cdd8fc2
daledupreez <dale@automattic.com>
2021-05-14 08:21:59 +0200

    Fix mobile display for new email forward page (#52750)
    * First pass for strictly mobile layouts
    
    * Fix warning font size and improve small tablet handling
    * Left-align warning text in all views, not just desktop
    * Fix alignment and colour for error card highlight
    * Restructure button HTML to fix layout; shift breakpoints
    * Make sure warning and button are centered
    This also simplifies some rules while making sure the warning is always stuck to the first column.
    * Revert "Make sure warning and button are centered"
    This reverts commit 10d3765e01e6976d96251e4b9260ad8b9b2fa003.

--- client/my-sites/email/email-management/home/email-plan-mailboxes-list.jsx
+++ client/my-sites/email/email-management/home/email-plan-mailboxes-list.jsx
@@ -80,6 +80,16 @@ const MailboxListItemWarning = ( { warningText } ) => {
 	);
 };
 
+const MailboxListItemAction = ( { buttonText, onClick } ) => {
+	return (
+		<div className="email-plan-mailboxes-list__mailbox-list-item-action">
+			<Button compact onClick={ onClick }>
+				{ buttonText }
+			</Button>
+		</div>
+	);
+};
+
 const resendEmailForwardVerification = ( mailbox, dispatch ) => {
 	const destination = getEmailForwardAddress( mailbox );
 	dispatch(
@@ -111,9 +121,10 @@ const getActionsForMailbox = ( mailbox, translate, dispatch ) => {
 	if ( isEmailForward( mailbox ) && ! isEmailForwardVerified( mailbox ) ) {
 		return {
 			action: (
-				<Button compact onClick={ () => resendEmailForwardVerification( mailbox, dispatch ) }>
-					{ translate( 'Resend verification email' ) }
-				</Button>
+				<MailboxListItemAction
+					buttonText={ translate( 'Resend verification email' ) }
+					onClick={ () => resendEmailForwardVerification( mailbox, dispatch ) }
+				/>
 			),
 			warning: <MailboxListItemWarning warningText={ translate( 'Verification required' ) } />,
 		};
