f84316fa6382402796d63decf131bd3875de72ae
Tom Cafferkey <tjcafferkey@gmail.com>
2021-05-14 10:34:44 +0100

    Add isSiteWpcomAtomic check to siteCanUploadThemesOrPlugins selector

--- client/state/sites/selectors/can-upload-themes-or-plugins.js
+++ client/state/sites/selectors/can-upload-themes-or-plugins.js
@@ -3,9 +3,13 @@
  */
 import getSitePlanSlug from 'calypso/state/sites/selectors/get-site-plan-slug';
 import { isBusinessPlan, isEcommercePlan } from '@automattic/calypso-products';
+import isSiteWpcomAtomic from 'calypso/state/selectors/is-site-wpcom-atomic';
 
 // Only sites with plans Business and Ecommerce can install third-party themes and/or plugins.
 export default function siteCanUploadThemesOrPlugins( state, siteId ) {
 	const planSlug = getSitePlanSlug( state, siteId );
-	return isBusinessPlan( planSlug ) || isEcommercePlan( planSlug );
+	return (
+		( isBusinessPlan( planSlug ) || isEcommercePlan( planSlug ) ) &&
+		isSiteWpcomAtomic( state, siteId )
+	);
 }
