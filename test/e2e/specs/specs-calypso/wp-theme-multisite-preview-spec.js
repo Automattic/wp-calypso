472fdb1f9ab4582263023a99879ff8f376fc5df7
Matthew Reishus <mreishus@users.noreply.github.com>
2021-05-19 10:37:55 -0500

    Don't merge, test PR to reproduce e2e failure

--- test/e2e/specs/specs-calypso/wp-theme-multisite-preview-spec.js
+++ test/e2e/specs/specs-calypso/wp-theme-multisite-preview-spec.js
@@ -11,10 +11,8 @@ import * as driverManager from '../../lib/driver-manager.js';
 import * as dataHelper from '../../lib/data-helper';
 
 import ThemesPage from '../../lib/pages/themes-page.js';
-import CustomizerPage from '../../lib/pages/customizer-page.js';
 
 import SidebarComponent from '../../lib/components/sidebar-component';
-import SiteSelectorComponent from '../../lib/components/site-selector-component';
 
 import LoginFlow from '../../lib/flows/login-flow.js';
 
@@ -31,6 +29,23 @@ describe( `[${ host }] Themes: All sites (${ screenSize })`, function () {
 		driver = await driverManager.startBrowser();
 	} );
 
+	// TODO: Add test that verifies when visiting /themes,
+	// you're asked to select a site before proceeding. (in new file? All @parallel tests are split)
+	/*
+	describe( 'Visiting /themes in the context of all sites asks to select a site @parallel', function () {
+		this.timeout( mochaTimeOut );
+		it( 'asks to select a site when visiting /themes logged in', async function () {
+			this.loginFlow = new LoginFlow( driver, 'multiSiteUser' );
+			await this.loginFlow.loginAndSelectAllSites();
+
+			this.sidebarComponent = await SidebarComponent.Expect( driver );
+			await this.sidebarComponent.selectAllSitesThemes();
+
+            // Check to see if "choose a site" screen is shown
+		} );
+	} );
+    */
+
 	describe( 'Preview a theme @parallel', function () {
 		this.timeout( mochaTimeOut );
 
@@ -39,10 +54,10 @@ describe( `[${ host }] Themes: All sites (${ screenSize })`, function () {
 			this.expectedTheme = 'Twenty F';
 
 			this.loginFlow = new LoginFlow( driver, 'multiSiteUser' );
-			await this.loginFlow.loginAndSelectAllSites();
+			await this.loginFlow.loginAndSelectMySite();
 
 			this.sidebarComponent = await SidebarComponent.Expect( driver );
-			await this.sidebarComponent.selectAllSitesThemes();
+			await this.sidebarComponent.selectThemes();
 		} );
 
 		it( 'can search for free themes', async function () {
@@ -64,35 +79,8 @@ describe( `[${ host }] Themes: All sites (${ screenSize })`, function () {
 				assert( displayed, 'Popover menu not displayed' );
 			} );
 
-			describe.skip( 'when "Try & Customize" is clicked', function () {
-				it( 'click try and customize popover', async function () {
-					await this.themesPage.clickPopoverItem( 'Try & Customize' );
-					this.siteSelector = await SiteSelectorComponent.Expect( driver );
-				} );
-
-				it( 'should show the site selector', async function () {
-					const siteSelectorShown = await this.siteSelector.displayed();
-					return assert( siteSelectorShown, 'The site selector was not shown' );
-				} );
-
-				describe( 'when a site is selected, and Customize is clicked', function () {
-					it( 'select first site', async function () {
-						await this.siteSelector.selectFirstSite();
-						await this.siteSelector.ok();
-					} );
-
-					it( 'should open the customizer with the selected site and theme', async function () {
-						this.customizerPage = await CustomizerPage.Expect( driver );
-						const url = await driver.getCurrentUrl();
-						assert( url.indexOf( this.siteSelector.selectedSiteDomain ) > -1, 'Wrong site domain' );
-						assert( url.indexOf( this.themeSearchName ) > -1, 'Wrong theme' );
-					} );
-
-					after( async function () {
-						await this.customizerPage.close();
-					} );
-				} );
-			} );
+			// Try & Customize had a skipped test that relied on the
+			// now removed SiteSelectorModal - make a new Try & Customize test?
 		} );
 	} );
 } );
