0b8a1795528700eb4f8dba97b83028061ac7905a
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-05-19 15:30:59 +0300

    State: Remove Secure Your Brand state (#52970)

--- client/state/secure-your-brand/init.js
+++
@@ -1,9 +0,0 @@
-/**
- * Internal dependencies
- */
-import { registerReducer } from 'calypso/state/redux-store';
-import secureYourBrandReducer from './reducer';
-
-export const secureYourBrand = 'secureYourBrand';
-
-registerReducer( [ secureYourBrand ], secureYourBrandReducer );
