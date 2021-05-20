51cbfa3d510712e0f96945d66837a68a61df5b60
Payton Swick <payton@foolord.com>
2021-05-19 20:54:00 -0400

    Add title, total, and styles to popover

--- client/layout/masterbar/masterbar-cart.tsx
+++ client/layout/masterbar/masterbar-cart.tsx
@@ -6,6 +6,7 @@ import page from 'page';
 import { useSelector } from 'react-redux';
 import { useShoppingCart } from '@automattic/shopping-cart';
 import { CheckoutProvider, CheckoutErrorBoundary, Button } from '@automattic/composite-checkout';
+import { styled } from '@automattic/wpcom-checkout';
 import { useTranslate } from 'i18n-calypso';
 
 /**
@@ -15,17 +16,15 @@ import MasterbarItem from './item';
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 import Popover from 'calypso/components/popover';
-import {
-	WPOrderReviewLineItems,
-	WPOrderReviewSection,
-} from 'calypso/my-sites/checkout/composite-checkout/components/wp-order-review-line-items';
+import { WPOrderReviewLineItems } from 'calypso/my-sites/checkout/composite-checkout/components/wp-order-review-line-items';
+import { CheckoutSummaryTotal } from 'calypso/my-sites/checkout/composite-checkout/components/wp-checkout-order-summary';
 
 type MasterbarCartProps = { tooltip: string; children: React.ReactNode };
 
 function MasterbarCart( { children, tooltip }: MasterbarCartProps ): JSX.Element | null {
 	const { responseCart } = useShoppingCart();
 	const selectedSite = useSelector( getSelectedSite );
-	const ref = useRef( null );
+	const masterbarButtonRef = useRef( null );
 	const [ isActive, setIsActive ] = useState( false );
 
 	if ( ! selectedSite?.slug || responseCart.products.length < 1 ) {
@@ -35,13 +34,19 @@ function MasterbarCart( { children, tooltip }: MasterbarCartProps ): JSX.Element
 	const onClick = () => setIsActive( ( active ) => ! active );
 	const onClose = () => setIsActive( false );
 
+	// TODO: Add dot overlay to show number of items in cart
 	return (
-		<div className="masterbar__cart" ref={ ref }>
+		<div className="masterbar__cart" ref={ masterbarButtonRef }>
 			<CheckoutErrorBoundary errorMessage="Error">
 				<MasterbarItem icon="cart" tooltip={ tooltip } onClick={ onClick }>
 					{ children }
 				</MasterbarItem>
-				<Popover isVisible={ isActive } onClose={ onClose } context={ ref.current }>
+				<Popover
+					isVisible={ isActive }
+					onClose={ onClose }
+					context={ masterbarButtonRef.current }
+					position="bottom left"
+				>
 					<MasterbarCartContents selectedSiteSlug={ selectedSite.slug } />
 				</Popover>
 			</CheckoutErrorBoundary>
@@ -50,8 +55,24 @@ function MasterbarCart( { children, tooltip }: MasterbarCartProps ): JSX.Element
 }
 
 function noop() {
-	// TODO
+	// TODO: get rid of this and provide actual handlers for CheckoutProvider
 }
+
+const MasterbarCartContentsWrapper = styled.div`
+	margin: 10px;
+	text-align: left;
+	font-size: 16px;
+`;
+
+const ButtonSection = styled.div`
+	margin-top: 10px;
+`;
+
+const MasterbarCartTitle = styled.h2`
+	text-align: center;
+	border-bottom: 1px solid #ccc;
+	padding-bottom: 10px;
+`;
 
 function MasterbarCartContents( { selectedSiteSlug }: { selectedSiteSlug: string } ) {
 	const { removeCoupon } = useShoppingCart();
@@ -60,6 +81,7 @@ function MasterbarCartContents( { selectedSiteSlug }: { selectedSiteSlug: string
 		const checkoutUrl = `/checkout/${ selectedSiteSlug }`;
 		page( checkoutUrl );
 	};
+	// TODO: Add trash icon to every item (probably requires https://github.com/Automattic/wp-calypso/pull/52850)
 	return (
 		<CheckoutProvider
 			paymentMethods={ [] }
@@ -69,15 +91,21 @@ function MasterbarCartContents( { selectedSiteSlug }: { selectedSiteSlug: string
 			showInfoMessage={ noop }
 			showSuccessMessage={ noop }
 		>
-			<WPOrderReviewSection>
-				<WPOrderReviewLineItems removeCoupon={ removeCoupon } />
-				<Button onClick={ goToCheckout }>{ translate( 'Checkout' ) }</Button>
-			</WPOrderReviewSection>
+			<MasterbarCartContentsWrapper>
+				<MasterbarCartTitle>{ translate( 'Cart' ) }</MasterbarCartTitle>
+				<WPOrderReviewLineItems removeCoupon={ removeCoupon } isCompact />
+				<CheckoutSummaryTotal />
+				<ButtonSection>
+					<Button buttonType={ 'primary' } fullWidth onClick={ goToCheckout }>
+						{ translate( 'Checkout' ) }
+					</Button>
+				</ButtonSection>
+			</MasterbarCartContentsWrapper>
 		</CheckoutProvider>
 	);
 }
 
-export default function MasterBarCartWrapper( props: MasterbarCartProps ): JSX.Element {
+export default function MasterbarCartWrapper( props: MasterbarCartProps ): JSX.Element {
 	return (
 		<CalypsoShoppingCartProvider>
 			<MasterbarCart { ...props } />
