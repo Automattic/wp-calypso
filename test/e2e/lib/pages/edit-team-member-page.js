cda449c9b45a612fa0e4da6000ec97d691210c76
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2021-05-20 13:42:54 +0200

    Make driverHelper naming consistent

--- test/e2e/lib/pages/edit-team-member-page.js
+++ test/e2e/lib/pages/edit-team-member-page.js
@@ -8,7 +8,7 @@ import { By } from 'selenium-webdriver';
  */
 import AsyncBaseContainer from '../async-base-container';
 
-import * as DriverHelper from '../driver-helper.js';
+import * as driverHelper from '../driver-helper.js';
 
 export default class EditTeamMemberPage extends AsyncBaseContainer {
 	constructor( driver ) {
@@ -16,23 +16,23 @@ export default class EditTeamMemberPage extends AsyncBaseContainer {
 	}
 
 	async removeUserAndDeleteContent() {
-		await DriverHelper.clickWhenClickable( this.driver, By.css( 'input[value="delete"]' ) );
-		return await DriverHelper.clickWhenClickable(
+		await driverHelper.clickWhenClickable( this.driver, By.css( 'input[value="delete"]' ) );
+		return await driverHelper.clickWhenClickable(
 			this.driver,
 			By.css( '.delete-user__single-site button' )
 		);
 	}
 
 	async changeToNewRole( roleName ) {
-		await DriverHelper.clickWhenClickable(
+		await driverHelper.clickWhenClickable(
 			this.driver,
 			By.css( `select#roles option[value=${ roleName }]` )
 		);
-		await DriverHelper.clickWhenClickable(
+		await driverHelper.clickWhenClickable(
 			this.driver,
 			By.css( 'button.form-button.is-primary:not([disabled])' )
 		);
-		return await DriverHelper.waitUntilElementLocatedAndVisible(
+		return await driverHelper.waitUntilElementLocatedAndVisible(
 			this.driver,
 			By.css( '.is-success' )
 		);
