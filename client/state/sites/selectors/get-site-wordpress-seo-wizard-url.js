09efc1fb54c9c339c695a67508ef4765464ecf81
retrofox <rdsuarez@gmail.com>
2021-05-20 07:43:57 -0300

    fox yoast selector jsdoc

--- client/state/sites/selectors/get-site-wordpress-seo-wizard-url.js
+++ client/state/sites/selectors/get-site-wordpress-seo-wizard-url.js
@@ -4,12 +4,12 @@
 import getSiteAdminUrl from 'calypso/state/sites/selectors/get-site-admin-url';
 
 /**
- * Returns a site's wp-admin WooCommerce Wizard plugin URL,
+ * Returns a site's wp-admin Yoast onboarding page URL,
  * or null if the admin URL for the site cannot be determined.
  *
  * @param  {object}  state  Global state tree
  * @param  {number}  siteId Site ID
- * @returns {?string}       Full URL to WooCommerce Wizard plugin in wp-admin
+ * @returns {?string}       Full URL to Yoast onboarding page in wp-admin
  */
 export default function getSiteWordPressSeoWizardUrl( state, siteId ) {
 	return getSiteAdminUrl( state, siteId, '/wp-admin/admin.php?page=wpseo_configurator' );
