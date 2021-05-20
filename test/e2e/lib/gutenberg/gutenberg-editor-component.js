ff70d2574d94b0cca7e24f746a44ed0b4bb6660a
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2021-05-20 16:23:03 +0200

    Fix element presence checks isElement* helpers only return a boolean value, they won't throw in any case. To verify an element is or isn't present a `waitUntilElement*` helpers should be used.

--- test/e2e/lib/gutenberg/gutenberg-editor-component.js
+++ test/e2e/lib/gutenberg/gutenberg-editor-component.js
@@ -465,7 +465,7 @@ export default class GutenbergEditorComponent extends AsyncBaseContainer {
 
 	async removeBlock( blockID ) {
 		const blockLocator = By.css( `.wp-block[id="${ blockID }"]` );
-		await driverHelper.isElementEventuallyLocatedAndVisible(
+		await driverHelper.waitUntilElementLocatedAndVisible(
 			this.driver,
 			blockLocator,
 			this.explicitWaitMS / 5
@@ -475,7 +475,7 @@ export default class GutenbergEditorComponent extends AsyncBaseContainer {
 			this.driver,
 			By.css( '.block-editor-block-settings-menu' )
 		);
-		await driverHelper.isElementEventuallyLocatedAndVisible(
+		await driverHelper.waitUntilElementLocatedAndVisible(
 			this.driver,
 			By.css( '.components-menu-group' ),
 			this.explicitWaitMS / 5
