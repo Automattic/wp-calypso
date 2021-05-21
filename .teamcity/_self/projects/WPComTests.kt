624486858f0edb7687689ac4e5bf40984773b75c
Sergio Cinos <sergio.cinos@gmail.com>
2021-05-21 11:43:17 +0200

    ci(teamcity): Create Jetpack build in TeamCity (#52856)
    * Move jetpack tests
    
    * Use new paths in magellan config
    
    * Create TeamCity build
    
    * Move wp-log-in-out tp specs-jetpack
    
    * Split file

--- .teamcity/_self/projects/WPComTests.kt
+++ .teamcity/_self/projects/WPComTests.kt
@@ -29,7 +29,9 @@ object WPComTests : Project({
 	// Keep the previous ID in order to preserve the historical data
 	buildType(gutenbergBuildType("desktop", "aee94c18-ee11-4c80-b6aa-245b967a97db"));
 	buildType(gutenbergBuildType("mobile","2af2eaed-87d5-41f4-ab1d-4ed589d5ae82"));
-	buildType(VisualRegressionTests)
+	buildType(jetpackBuildType("desktop"));
+	buildType(jetpackBuildType("mobile"));
+	buildType(VisualRegressionTests);
 })
 
 fun gutenbergBuildType(screenSize: String, buildUuid: String): BuildType {
@@ -190,6 +192,133 @@ fun gutenbergBuildType(screenSize: String, buildUuid: String): BuildType {
 	}
 }
 
+fun jetpackBuildType(screenSize: String): BuildType {
+	return BuildType {
+		id("WPComTests_jetpack_$screenSize")
+		name = "Jetpack tests ($screenSize)"
+		description = "Runs Calypso Jetpack E2E tests using $screenSize screen resolution"
+
+		artifactRules = """
+			reports => reports
+			logs.tgz => logs.tgz
+			screenshots => screenshots
+		""".trimIndent()
+
+		vcs {
+			root(Settings.WpCalypso)
+			cleanCheckout = true
+		}
+
+		steps {
+			bashNodeScript {
+				name = "Prepare environment"
+				scriptContent = """
+					export NODE_ENV="test"
+
+					# Install modules
+					${_self.yarn_install_cmd}
+				"""
+			}
+			bashNodeScript {
+				name = "Run e2e tests ($screenSize)"
+				scriptContent = """
+					shopt -s globstar
+					set -x
+
+					cd test/e2e
+					mkdir temp
+
+					export LIVEBRANCHES=false
+					export NODE_CONFIG_ENV=test
+					export TEST_VIDEO=true
+					export HIGHLIGHT_ELEMENT=true
+					export BROWSERSIZE=$screenSize
+					export BROWSERLOCALE=en
+
+					# Instructs Magellan to not hide the output from individual `mocha` processes. This is required for
+					# mocha-teamcity-reporter to work.
+					export MAGELLANDEBUG=true
+
+					# Decrypt config
+					openssl aes-256-cbc -md sha1 -d -in ./config/encrypted.enc -out ./config/local-test.json -k "%CONFIG_E2E_ENCRYPTION_KEY%"
+
+					# Run the test
+					yarn magellan --config=magellan-jetpack.json --max_workers=%E2E_WORKERS% --local_browser=chrome --mocha_args="--reporter mocha-multi-reporters --reporter-options configFile=mocha-reporter.json"
+				""".trimIndent()
+				dockerRunParameters = "-u %env.UID% --security-opt seccomp=.teamcity/docker-seccomp.json --shm-size=8gb"
+			}
+			bashNodeScript {
+				name = "Collect results"
+				executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
+				scriptContent = """
+					set -x
+
+					mkdir -p screenshots
+					find test/e2e -type f -path '*/screenshots/*' -print0 | xargs -r -0 mv -t screenshots
+
+					mkdir -p logs
+					find test/e2e -name '*.log' -print0 | xargs -r -0 tar cvfz logs.tgz
+				""".trimIndent()
+			}
+		}
+
+		features {
+			perfmon {
+			}
+			notifications {
+				notifierSettings = slackNotifier {
+					connection = "PROJECT_EXT_11"
+					sendTo = "#e2e-jetpack-notiff"
+					messageFormat = verboseMessageFormat {
+						addBranch = true
+						addStatusText = true
+						maximumNumberOfChanges = 10
+					}
+				}
+				branchFilter = "+:<default>"
+				buildFailed = true
+				buildFinishedSuccessfully = true
+			}
+		}
+
+		failureConditions {
+			executionTimeoutMin = 20
+			// TeamCity will mute a test if it fails and then succeeds within the same build. Otherwise TeamCity UI will not
+			// display a difference between real errors and retries, making it hard to understand what is actually failing.
+			supportTestRetry = true
+
+			// Don't fail if the runner exists with a non zero code. This allows a build to pass if the failed tests have
+			// been muted previously.
+			nonZeroExitCode = false
+
+			// Fail if the number of passing tests is 50% or less than the last build. This will catch the case where the test runner
+			// crashes and no tests are run.
+			failOnMetricChange {
+				metric = BuildFailureOnMetric.MetricType.PASSED_TEST_COUNT
+				threshold = 50
+				units = BuildFailureOnMetric.MetricUnit.PERCENTS
+				comparison = BuildFailureOnMetric.MetricComparison.LESS
+				compareTo = build {
+					buildRule = lastSuccessful()
+				}
+			}
+		}
+
+		triggers {
+			schedule {
+				schedulingPolicy = daily {
+					hour = 2
+				}
+				branchFilter = """
+					+:trunk
+				""".trimIndent()
+				triggerBuild = always()
+				withPendingChangesOnly = false
+			}
+		}
+	}
+}
+
 private object VisualRegressionTests : BuildType({
 	name = "Visual Regression Tests"
 	description = "Runs visual regression tests"
@@ -244,6 +373,7 @@ private object VisualRegressionTests : BuildType({
 	}
 
 	failureConditions {
-    	executionTimeoutMin = 30
-    }
+		executionTimeoutMin = 30
+	}
 })
+
