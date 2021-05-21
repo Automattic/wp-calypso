63b766ab75e58729b255839a0664d6b9ed7f1194
Julian de Bhal <julian.debhal@automattic.com>
2021-05-21 19:38:40 +1000

    Add action hooks

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-tasks/index.php
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-tasks/index.php
@@ -8,6 +8,28 @@
 namespace A8C\FSE\WPCOMTasks;
 
 /**
+ * This logic is duplicated from Onboarding_Checklist\Blog
+ */
+function page_post_saved( $post_ID, $post, $update ) {
+	if ( get_option( 'show_on_front' ) === 'page' && (int) get_option( 'page_on_front' ) === $post_ID ) {
+		set_option( 'onboarding_checklist_task_front_page_updated', true );
+	}
+}
+add_action( 'save_post_page', __NAMESPACE__ . '\\page_post_saved', 10, 3 );
+
+/**
+ * This logic is duplicated from Onboarding_Checklist\Blog
+ */
+function update_option_page_on_front( $old_value, $new_value ) {
+	if ( $old_value === $new_value ) {
+		return;
+	}
+
+	set_option( 'onboarding_checklist_task_front_page_updated', true );
+}
+add_action( 'update_option_page_on_front', __NAMESPACE__ . '\\update_option_page_on_front', 10, 3 );
+
+/**
  * Add onboarding checklist task options
  *
  * @param [ string ] $options incoming options to filter.
@@ -15,6 +37,7 @@ namespace A8C\FSE\WPCOMTasks;
  * @return [ string ]
  */
 function wpcom_tasks_jetpack_options_whitelist( $options ) {
+	$options[] = 'onboarding_checklist_initialized';
 	$options[] = 'onboarding_checklist_task_front_page_updated';
 	return $options;
 }
