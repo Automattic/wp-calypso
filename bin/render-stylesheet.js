6acfb7c69ff363007ef32b05741995d5983adde7
Noah Allen <noahtallen@gmail.com>
2021-05-21 18:14:29 -0700

    Use same package importer

--- bin/render-stylesheet.js
+++ bin/render-stylesheet.js
@@ -2,7 +2,7 @@
 
 const { renderSync } = require( 'sass' );
 const fs = require( 'fs' );
-const path = require( 'path' );
+const packageImporter = require( 'node-sass-package-importer' );
 
 if ( ! process.argv[ 2 ] ) {
 	throw new Error( 'No input file provided.' );
@@ -16,24 +16,7 @@ const { css } = renderSync( {
 	file: process.argv[ 2 ],
 	outputStyle: 'compressed',
 	includePaths: [ 'client' ],
-	importer: npmImporter,
+	importer: packageImporter(),
 } );
 
 fs.writeFileSync( process.argv[ 3 ], css );
-
-function npmImporter( specifiedPath ) {
-	const parsedPath = specifiedPath.startsWith( '~' ) ? specifiedPath.slice( 1 ) : specifiedPath;
-	const variations = [
-		parsedPath + '.scss',
-		parsedPath + '.sass',
-		'_' + parsedPath + '.scss',
-		'_' + parsedPath + '.sass',
-	];
-	for ( const variation of variations ) {
-		const file = path.resolve( './node_modules', variation );
-		if ( fs.existsSync( file ) ) {
-			return { file };
-		}
-	}
-	return null;
-}
