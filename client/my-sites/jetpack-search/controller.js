f8b505e712d94b381f6ca4c050928e4645a1c7b8
Chris R <chris.rosser@automattic.com>
2021-05-24 16:03:42 +1200

    Handle site disconnection

--- client/my-sites/jetpack-search/controller.js
+++ client/my-sites/jetpack-search/controller.js
@@ -6,7 +6,19 @@ import React from 'react';
 /**
  * Internal dependencies
  */
+import IsJetpackDisconnectedSwitch from 'calypso/components/jetpack/is-jetpack-disconnected-switch';
 import JetpackSearchMain from './main';
+import JetpackSearchDisconnected from './disconnected';
+
+export function showJetpackIsDisconnected( context, next ) {
+	context.primary = (
+		<IsJetpackDisconnectedSwitch
+			trueComponent={ <JetpackSearchDisconnected /> }
+			falseComponent={ context.primary }
+		/>
+	);
+	next();
+}
 
 /* handles /jetpack-search/:site, see `jetpackSearchMainPath` */
 export function jetpackSearchMain( context, next ) {
