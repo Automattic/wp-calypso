5266d0acc13b1adc6ab890344ec35b5bb6a25d7c
Marcelo Serpa <81248+fullofcaffeine@users.noreply.github.com>
2021-05-24 17:33:00 -0500

    Use `__unstable` `InserterMenuExtension` if available, fallback to `_experimental`
    It has been renamed here: https://github.com/WordPress/gutenberg/pull/31417 (>= Gutenberg v10.7.0-rc.1)
    
    The fallback is needed until we're sure all relevant systems are consuming packages from GB >= v10.7.0-rc.1.
    
    + Add exclusion to the `wpcalypso/no-unsafe-wp-apis` esline rule, keep the old one for now;
    + Update the experimental features E2E test to fail if the new symbol can't be found.

--- apps/editing-toolkit/editing-toolkit-plugin/block-inserter-modifications/contextual-tips.js
+++ apps/editing-toolkit/editing-toolkit-plugin/block-inserter-modifications/contextual-tips.js
@@ -7,7 +7,10 @@ import { debounce } from 'lodash';
  * WordPress dependencies
  */
 import { useState } from '@wordpress/element';
-import { __experimentalInserterMenuExtension as InserterMenuExtension } from '@wordpress/block-editor';
+import {
+	__unstableInserterMenuExtension,
+	__experimentalInserterMenuExtension,
+} from '@wordpress/block-editor';
 import { registerPlugin } from '@wordpress/plugins';
 
 /**
@@ -15,6 +18,13 @@ import { registerPlugin } from '@wordpress/plugins';
  */
 import ContextualTip from './contextual-tips/contextual-tip';
 import './contextual-tips/style.scss';
+
+// InserterMenuExtension has been made unstable in this PR: https://github.com/WordPress/gutenberg/pull/31417 / Gutenberg v10.7.0-rc.1.
+// We need to support both symbols until we're sure Gutenberg < v10.7.x is not used anymore in WPCOM.
+const InserterMenuExtension =
+	typeof __unstableInserterMenuExtension !== 'undefined'
+		? __unstableInserterMenuExtension
+		: __experimentalInserterMenuExtension;
 
 const ContextualTips = function () {
 	const [ debouncedFilterValue, setFilterValue ] = useState( '' );
