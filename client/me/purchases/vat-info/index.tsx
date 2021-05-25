50331701467e7f5c7430055933e86b8048d5a6ee
Payton Swick <payton@foolord.com>
2021-05-25 19:46:48 -0400

    Update VatInfoPage to use useVatDetails

--- client/me/purchases/vat-info/index.tsx
+++ client/me/purchases/vat-info/index.tsx
@@ -3,43 +3,64 @@
  */
 import React from 'react';
 import { useTranslate } from 'i18n-calypso';
-import { CompactCard } from '@automattic/components';
+import { CompactCard, Button } from '@automattic/components';
 
 /**
  * Internal dependencies
  */
 import SectionHeader from 'calypso/components/section-header';
+import useVatDetails from './use-vat-details';
 
 export default function VatInfoPage(): JSX.Element {
 	const translate = useTranslate();
-	const { countryCode, vatId } = useVatInfo();
+	const { vatDetails, isLoading, error } = useVatDetails();
+
+	if ( error ) {
+		return (
+			<div className="vat-info">
+				<SectionHeader label={ translate( 'VAT Details' ) } />
+
+				<CompactCard>{ translate( 'An error occurred while fetching VAT details.' ) }</CompactCard>
+			</div>
+		);
+	}
+
+	if ( isLoading ) {
+		return (
+			<div className="vat-info">
+				<SectionHeader label={ translate( 'VAT Details' ) } />
+
+				<CompactCard>{ translate( 'Loading…' ) }</CompactCard>
+			</div>
+		);
+	}
+
+	const saveDetails = () => {};
 
 	return (
 		<div className="vat-info">
-			<SectionHeader label={ translate( 'VAT Information' ) } />
+			<SectionHeader label={ translate( 'VAT Details' ) } />
 
 			<CompactCard>
 				<div>
 					<label>{ translate( 'Country' ) }</label>
-					<input value={ countryCode } />
+					<input value={ vatDetails.country ?? '' } />
 				</div>
 				<div>
 					<label>{ translate( 'VAT' ) }</label>
-					<input value={ vatId } />
+					<input value={ vatDetails.id ?? '' } />
 				</div>
+				<div>
+					<label>{ translate( 'Name' ) }</label>
+					<input value={ vatDetails.name ?? '' } />
+				</div>
+				<div>
+					<label>{ translate( 'Address' ) }</label>
+					<input value={ vatDetails.address ?? '' } />
+				</div>
+
+				<Button onClick={ saveDetails }>{ translate( 'Validate and save' ) }</Button>
 			</CompactCard>
 		</div>
 	);
-}
-
-interface VatDetails {
-	countryCode: string;
-	vatId: string;
-}
-
-function useVatInfo(): VatDetails {
-	return {
-		countryCode: 'UK',
-		vatId: '12345',
-	};
 }
