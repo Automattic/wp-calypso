dba918bb44708c93a9be433af0b4785c43ea1b18
Aurorum <torres126@clubpenguinmountains.com>
2021-05-26 17:03:29 +0100

    Remove get import

--- client/my-sites/themes/theme-activation-confirmation-modal.jsx
+++ client/my-sites/themes/theme-activation-confirmation-modal.jsx
@@ -4,7 +4,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { get } from 'lodash';
 import { translate } from 'i18n-calypso';
 import classNames from 'classnames';
 
