7b4c7f135c838eed94912a557c7d11fac407fbfe
retrofox <rdsuarez@gmail.com>
2021-05-27 07:20:03 -0300

    upsell-nudge: rename prop to isJetpack

--- client/blocks/upsell-nudge/index.jsx
+++ client/blocks/upsell-nudge/index.jsx
@@ -53,7 +53,7 @@ export const UpsellNudge = ( {
 	horizontal,
 	href,
 	isJetpackDevDocs,
-	jetpack,
+	isJetpack,
 	isAtomic,
 	isVip,
 	siteIsWPForTeams,
@@ -85,8 +85,8 @@ export const UpsellNudge = ( {
 		( feature && planHasFeature ) ||
 		( ! feature && ! isFreePlanProduct( site.plan ) ) ||
 		( feature === FEATURE_NO_ADS && site.options.wordads ) ||
-		( ! jetpack && site.jetpack ) ||
-		( jetpack && ! site.jetpack );
+		( ! isJetpack && site.jetpack ) ||
+		( isJetpack && ! site.jetpack );
 
 	if ( shouldNotDisplay && ! forceDisplay ) {
 		return null;
@@ -130,7 +130,8 @@ export const UpsellNudge = ( {
 			horizontal={ horizontal }
 			href={ href }
 			icon="star"
-			jetpack={ ( jetpack && ! isAtomic ) || isJetpackDevDocs } //Force show Jetpack example in Devdocs
+			jetpack={ isJetpack || isJetpackDevDocs } //Force show Jetpack example in Devdocs
+			isAtomic={ isAtomic }
 			list={ list }
 			onClick={ onClick }
 			onDismissClick={ onDismissClick }
@@ -163,6 +164,7 @@ export default connect( ( state, ownProps ) => {
 		canManageSite: canCurrentUser( state, siteId, 'manage_options' ),
 		jetpack: isJetpackSite( state, siteId ),
 		isAtomic: isSiteAutomatedTransfer( state, siteId ),
+		isJetpack: isJetpackSite( state, siteId ),
 		isVip: isVipSite( state, siteId ),
 		siteSlug: ownProps.disableHref ? null : getSelectedSiteSlug( state ),
 		canUserUpgrade: canCurrentUser( state, getSelectedSiteId( state ), 'manage_options' ),
