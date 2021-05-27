fa174d5bc515b11810f2d9d1a09f91731dfb8ddf
Marin Atanasov <tyxla@abv.bg>
2021-05-27 12:58:07 +0300

    Use wpcom.req.get directly

--- client/data/post-formats/use-post-formats-query.js
+++ client/data/post-formats/use-post-formats-query.js
@@ -9,8 +9,6 @@ import { useQuery } from 'react-query';
 import wpcom from 'calypso/lib/wp';
 
 const usePostFormatsQuery = ( siteId ) =>
-	useQuery( [ 'postFormats', siteId ], () =>
-		wpcom.undocumented().site( siteId ).postFormatsList()
-	);
+	useQuery( [ 'postFormats', siteId ], () => wpcom.req.get( `/sites/${ siteId }/post-formats` ) );
 
 export default usePostFormatsQuery;
