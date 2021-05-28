dd5be20e8259c1f86d6182317dc3684cfe9a4fea
Dehan <dehandecroos@gmail.com>
2021-05-28 11:21:22 +0530

    Fix broken unit tests for marketplace

--- client/my-sites/plugins/marketplace/marketplace-plugin-details/test/index.js
+++ client/my-sites/plugins/marketplace/marketplace-plugin-details/test/index.js
@@ -39,6 +39,9 @@ describe( '<PurchaseArea/> Plugin details next step tests', () => {
 		);
 		const yoastPremiumButton = await marketplacePluginDetails.findByText( 'Buy Yoast Premium' );
 		await fireEvent.click( yoastPremiumButton );
+		//onAddPlugin call in the PurchaseArea component is async so we have to queue and await a promise  for the following mocks to be called
+		await Promise.resolve();
+
 		expect( onAddYoastPremiumToCart ).toHaveBeenCalled();
 		expect( onNavigateToCheckoutMockFunction ).toHaveBeenCalled();
 		expect( onNavigateToDomainsSelectionMockFunction ).not.toHaveBeenCalled();
@@ -61,9 +64,10 @@ describe( '<PurchaseArea/> Plugin details next step tests', () => {
 			/>
 		);
 		const yoastPremiumButton = await marketplacePluginDetails.findByText( 'Buy Yoast Premium' );
-		const yoastFreeButton = await marketplacePluginDetails.findByText( 'Add Yoast Free' );
-
 		await fireEvent.click( yoastPremiumButton );
+		//onAddPlugin call in the PurchaseArea component is async so we have to queue and await a promise  for the following mocks to be called
+		await Promise.resolve();
+
 		expect( onAddYoastPremiumToCart ).toHaveBeenCalled();
 		expect( onNavigateToCheckoutMockFunction ).not.toHaveBeenCalled();
 		expect( onNavigateToDomainsSelectionMockFunction ).toHaveBeenCalled();
@@ -71,7 +75,11 @@ describe( '<PurchaseArea/> Plugin details next step tests', () => {
 		await onNavigateToCheckoutMockFunction.mockReset();
 		await onNavigateToDomainsSelectionMockFunction.mockReset();
 
+		const yoastFreeButton = await marketplacePluginDetails.findByText( 'Add Yoast Free' );
 		await fireEvent.click( yoastFreeButton );
+		//onAddPlugin call in the PurchaseArea component is async so we have to queue and await a promise  for the following mocks to be called
+		await Promise.resolve();
+
 		expect( onAddYoastPremiumToCart ).not.toHaveBeenCalled();
 		expect( onNavigateToCheckoutMockFunction ).not.toHaveBeenCalled();
 		expect( onNavigateToDomainsSelectionMockFunction ).toHaveBeenCalled();
