a427da9634a2ca2cee16aff1534bb9604cbc3fc5
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-05-31 14:08:57 +0300

    Reader: Remove unused abtest framework (#53274)
    * Reader: Remove unused abtest framework
    
    * Export as separate functions

--- client/reader/following/index.js
+++ client/reader/following/index.js
@@ -7,11 +7,10 @@ import page from 'page';
  * Internal dependencies
  */
 import { followingManage } from './controller';
-import { initAbTests, updateLastRoute, sidebar } from 'calypso/reader/controller';
+import { updateLastRoute, sidebar } from 'calypso/reader/controller';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 
 export default function () {
-	page( '/following/*', initAbTests );
 	page( '/following/manage', updateLastRoute, sidebar, followingManage, makeLayout, clientRender );
 	page( '/following/edit*', '/following/manage' );
 
