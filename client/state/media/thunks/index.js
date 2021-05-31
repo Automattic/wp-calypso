90e51f1a715a4ccff4a4fb574c0a3c983d1e574c
Marin Atanasov <tyxla@abv.bg>
2021-05-31 13:59:56 +0300

    State: Remove unused addWoocommerceProductImage

--- client/state/media/thunks/index.js
+++ client/state/media/thunks/index.js
@@ -5,7 +5,6 @@ export { editMedia } from './edit-media';
 export { updateMedia } from './update-media';
 export { deleteMedia } from './delete-media';
 export { fetchMediaItem } from './fetch-media-item';
-export { addWoocommerceProductImage } from './add-woocommerce-product-image';
 export { fetchNextMediaPage } from './fetch-next-media-page';
 export { default as getMediaItem } from './get-media-item';
 export { gutenframeUpdateImageBlocks } from './gutenframe-update-image-blocks';
