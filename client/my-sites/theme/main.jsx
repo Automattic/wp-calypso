53565703f38c415025a6925804535abb8e7f9ab6
Aurorum <torres126@clubpenguinmountains.com>
2021-06-02 22:34:12 +0100

    Add type as prop

--- client/my-sites/theme/main.jsx
+++ client/my-sites/theme/main.jsx
@@ -218,7 +218,7 @@ class ThemeSheet extends React.Component {
 		}
 		event.preventDefault();
 
-		this.props.recordTracksEvent( `calypso_theme_live_demo_${ type }_preview` );
+		this.props.recordTracksEvent( 'calypso_theme_live_demo_preview', { type } );
 
 		const { preview } = this.props.options;
 		this.props.setThemePreviewOptions( this.props.defaultOption, this.props.secondaryOption );
