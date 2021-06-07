644d569bf479655164300c9579d99708e04290f0
David Szabo <david.szabo97@gmail.com>
2021-06-07 12:34:37 +0200

    Track global styles button click in header

--- apps/wpcom-block-editor/src/wpcom/features/tracking/delegate-event-tracking.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/delegate-event-tracking.js
@@ -7,6 +7,7 @@ import debugFactory from 'debug';
  * Internal dependencies
  */
 import wpcomBlockEditorCloseClick from './wpcom-block-editor-close-click';
+import wpcomBlockEditorGlobalStylesClick from './wpcom-block-editor-global-styles-click';
 import wpcomInserterInlineSearchTerm from './wpcom-inserter-inline-search-term';
 import wpcomInserterTabPanelSelected from './wpcom-inserter-tab-panel-selected';
 import wpcomBlockDonationsPlanUpgrade from './wpcom-block-donations-plan-upgrade';
@@ -42,6 +43,7 @@ export const registerSubscriber = ( id, type, handler ) => {
  */
 const EVENTS_MAPPING = [
 	wpcomBlockEditorCloseClick(),
+	wpcomBlockEditorGlobalStylesClick(),
 	wpcomInserterInlineSearchTerm(),
 	wpcomInserterTabPanelSelected(),
 	wpcomBlockDonationsPlanUpgrade(),
