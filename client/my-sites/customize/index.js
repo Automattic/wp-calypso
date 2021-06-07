2eb749067daf964603ac706fa49f97bce094a603
Jarda Snajdr <jsnajdr@gmail.com>
2021-06-07 10:01:09 +0200

    Remove manage/customize flag

--- client/my-sites/customize/index.js
+++ client/my-sites/customize/index.js
@@ -9,19 +9,16 @@ import page from 'page';
  */
 import { siteSelection, sites, navigation } from 'calypso/my-sites/controller';
 import { customize } from './controller';
-import config from '@automattic/calypso-config';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 
 export default function () {
-	if ( config.isEnabled( 'manage/customize' ) ) {
-		page( '/customize/:panel([^.]+)?', siteSelection, sites, makeLayout, clientRender );
-		page(
-			'/customize/:panel?/:domain',
-			siteSelection,
-			navigation,
-			customize,
-			makeLayout,
-			clientRender
-		);
-	}
+	page( '/customize/:panel([^.]+)?', siteSelection, sites, makeLayout, clientRender );
+	page(
+		'/customize/:panel?/:domain',
+		siteSelection,
+		navigation,
+		customize,
+		makeLayout,
+		clientRender
+	);
 }
