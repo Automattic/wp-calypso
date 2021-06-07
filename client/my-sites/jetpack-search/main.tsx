a3425568f22cd5967992e4af6f438985794aa975
Chris R <chris.rosser@automattic.com>
2021-06-08 11:14:08 +1200

    Search: match Jetpack Cloud look and feel (#53296)
    * Search: match Jetpack Cloud look and feel
    
    * Fix logo size in Cloud
    
    * Handle different output via the content component
    
    * Make placeholder clearer and fix type errors
    
    * Fix heading in README
    
    * Fix typing and console errors
    
    * Whitespace

--- client/my-sites/jetpack-search/main.tsx
+++ client/my-sites/jetpack-search/main.tsx
@@ -12,9 +12,6 @@ import DocumentHead from 'calypso/components/data/document-head';
 import Main from 'calypso/components/main';
 import SidebarNavigation from 'calypso/my-sites/sidebar-navigation';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
-import FormattedHeader from 'calypso/components/formatted-header';
-import PromoCard from 'calypso/components/promo-section/promo-card';
-import PromoCardCTA from 'calypso/components/promo-section/promo-card/cta';
 import useTrackCallback from 'calypso/lib/jetpack/use-track-callback';
 import {
 	getSelectedSite,
@@ -23,19 +20,17 @@ import {
 } from 'calypso/state/ui/selectors';
 import getSiteSetting from 'calypso/state/selectors/get-site-setting';
 import QuerySiteSettings from 'calypso/components/data/query-site-settings';
-import JetpackSearchUpsell from './upsell';
-import JetpackSearchPlaceholder from './placeholder';
 import { isJetpackSearch, planHasJetpackSearch } from '@automattic/calypso-products';
 import {
 	getSitePurchases,
 	hasLoadedSitePurchasesFromServer,
 } from 'calypso/state/purchases/selectors';
+import JetpackSearchContent from './content';
 import JetpackSearchFooter from './footer';
-
-/**
- * Asset dependencies
- */
-import JetpackSearchSVG from 'calypso/assets/images/illustrations/jetpack-search.svg';
+import JetpackSearchLogo from './logo';
+import JetpackSearchPlaceholder from './placeholder';
+import JetpackSearchUpsell from './upsell';
+import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
 
 export default function JetpackSearchMain(): ReactElement {
 	const site = useSelector( getSelectedSite );
@@ -50,6 +45,13 @@ export default function JetpackSearchMain(): ReactElement {
 		sitePurchases.find( checkForSearchProduct ) || planHasJetpackSearch( site?.plan?.product_slug );
 	const hasLoadedSitePurchases = useSelector( hasLoadedSitePurchasesFromServer );
 	const onSettingsClick = useTrackCallback( undefined, 'calypso_jetpack_search_settings' );
+	const isCloud = isJetpackCloud();
+
+	// Send Jetpack Cloud users to wp-admin settings and everyone else to Calypso blue
+	const settingsUrl =
+		isCloud && site?.options?.admin_url
+			? `${ site.options.admin_url }admin.php?page=jetpack#/performance`
+			: `/settings/performance/${ siteSlug }`;
 
 	if ( ! hasLoadedSitePurchases ) {
 		return <JetpackSearchPlaceholder siteId={ siteId } />;
@@ -66,39 +68,22 @@ export default function JetpackSearchMain(): ReactElement {
 			<PageViewTracker path="/jetpack-search/:site" title="Jetpack Search" />
 			<QuerySiteSettings siteId={ siteId } />
 
-			<FormattedHeader
-				headerText={ translate( 'Jetpack Search' ) }
-				id="jetpack-search-header"
-				align="left"
-				brandFont
-			/>
-
-			<PromoCard
-				title={
+			<JetpackSearchContent
+				headerText={
 					isSearchEnabled
 						? translate( 'Jetpack Search is enabled on your site.' )
 						: translate( 'Jetpack Search is disabled on your site.' )
 				}
-				image={ { path: JetpackSearchSVG } }
-				isPrimary
-			>
-				<p>
-					{ isSearchEnabled
+				bodyText={
+					isSearchEnabled
 						? translate( 'Your visitors are getting our fastest search experience.' )
-						: translate( 'Enable it to ensure your visitors get our fastest search experience.' ) }
-				</p>
-
-				<PromoCardCTA
-					cta={ {
-						text: translate( 'Settings' ),
-						action: {
-							url: `/settings/performance/${ siteSlug }`,
-							onClick: onSettingsClick,
-							selfTarget: true,
-						},
-					} }
-				/>
-			</PromoCard>
+						: translate( 'Enable it to ensure your visitors get our fastest search experience.' )
+				}
+				buttonLink={ settingsUrl }
+				buttonText={ translate( 'Settings' ) }
+				onClick={ onSettingsClick }
+				iconComponent={ <JetpackSearchLogo /> }
+			/>
 
 			<JetpackSearchFooter />
 		</Main>
