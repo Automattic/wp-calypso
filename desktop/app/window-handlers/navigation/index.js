3a25697c603d5e8d0c8e5050ca5e3ea1e96eec51
Sandy McFadden <sandy.mcfadden@automattic.com>
2021-06-07 14:17:37 -0300

    Add ability to doubleclick titlebar on mac to maximize or unmaximize the window

--- desktop/app/window-handlers/navigation/index.js
+++ desktop/app/window-handlers/navigation/index.js
@@ -1,7 +1,7 @@
 /**
  * External Dependencies
  */
-const { ipcMain } = require( 'electron' );
+const { ipcMain, systemPreferences } = require( 'electron' );
 const Config = require( '../../lib/config' );
 const isCalypso = require( '../../lib/is-calypso' );
 const ipc = require( '../../lib/calypso-commands' );
@@ -13,7 +13,7 @@ const webBase = Config.baseURL();
  */
 const log = require( '../../lib/logger' )( 'desktop:navigation' );
 
-module.exports = function ( { view } ) {
+module.exports = function ( { view, window } ) {
 	ipcMain.on( 'back-button-clicked', () => {
 		log.info( `User clicked 'go back'...` );
 		view.webContents.goBack();
@@ -31,5 +31,15 @@ module.exports = function ( { view } ) {
 		} else {
 			view.webContents.loadURL( webBase + 'stats/day' );
 		}
+	} );
+
+	ipcMain.on( 'title-bar-double-click', () => {
+		if ( process.platform === 'darwin' ) {
+			const action = systemPreferences.getUserDefault( 'AppleActionOnDoubleClick', 'string' );
+			if ( action === 'None' ) return;
+			if ( action === 'Minimize' ) return window.minimize();
+		}
+		if ( window.isMaximized() ) return window.unmaximize();
+		return window.maximize();
 	} );
 };
