14e453753e2b38ab164fb457ff5df83ddebcc9b8
flootr <m.do@posteo.net>
2021-06-07 16:00:37 +0200

    remove unused `requestFromUrl` data getter (#53465)

--- client/state/data-getters/index.js
+++ client/state/data-getters/index.js
@@ -1,4 +1,3 @@
-export { default as requestFromUrl } from './request-from-url';
 export { default as requestActivityActionTypeCounts } from './request-activity-action-type-counts';
 export { default as getRequestActivityLogsId } from './get-request-activity-logs-id';
 export { default as requestActivityLogs } from './request-activity-logs';
