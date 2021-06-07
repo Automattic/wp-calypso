a93809e0fc4b18492402e67f440180e427995a05
David Szabo <david.szabo97@gmail.com>
2021-06-07 17:09:16 +0200

    Rename event

--- apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-details-click.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-details-click.js
@@ -4,12 +4,12 @@
 import tracksRecordEvent from './track-record-event';
 
 /**
- * Return the event definition object to track `wpcom-block-editor-details-click`.
+ * Return the event definition object to track `wpcom_block_editor_details_click`.
  *
  * @returns {{handler: Function, selector: string, type: string}} event object definition.
  */
 export default () => ( {
 	selector: '.edit-post-header .table-of-contents button',
 	type: 'click',
-	handler: () => tracksRecordEvent( 'wpcom-block-editor-details-click' ),
+	handler: () => tracksRecordEvent( 'wpcom_block_editor_details_click' ),
 } );
