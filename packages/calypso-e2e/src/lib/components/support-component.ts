b5f6d0d9737acc6b68115502da97d4b2bc000d34
Edwin Takahashi <egao@outlook.com>
2021-06-08 15:59:33 -0700

    Implement all other tests

--- packages/calypso-e2e/src/lib/components/support-component.ts
+++ packages/calypso-e2e/src/lib/components/support-component.ts
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import assert from 'assert';
+
+/**
  * Internal dependencies
  */
 import { BaseContainer } from '../base-container';
@@ -12,11 +17,14 @@ const selectors = {
 	supportButton: '.inline-help__button',
 	supportPopover: '.inline-help__popover',
 	searchInput: '.form-text-input.search__input',
+	spinner: '.spinner',
+	clearSearch: '[aria-label="Close Search"]',
+
 	resultsList: '.inline-help__results',
 	results: '.inline-help__results-item',
-
 	supportItems: '[aria-labelledby="inline-search--api_help"] li',
 	adminItems: '[aria-labelledby="inline-search--admin_section"] li',
+	emptyResults: '.inline-help__empty-results',
 };
 
 export class SupportComponent extends BaseContainer {
@@ -51,12 +59,12 @@ export class SupportComponent extends BaseContainer {
 
 	/* Returns the overall number of results, not distinguishing the support and admin results.
 	 */
-	async getOverallResultItems(): Promise< ElementHandle[] > {
+	async getOverallResults(): Promise< ElementHandle[] > {
 		return await this.getResults( selectors.results );
 	}
 
 	async getOverallResultsCount(): Promise< number > {
-		const items = await this.getOverallResultItems();
+		const items = await this.getOverallResults();
 		return items.length;
 	}
 
@@ -82,6 +90,14 @@ export class SupportComponent extends BaseContainer {
 		return items.length;
 	}
 
+	async noResults(): Promise< void > {
+		const adminResults = await this.getAdminResults();
+		assert.deepStrictEqual( [], adminResults );
+		const supportResults = await this.getSupportResults();
+		assert.deepStrictEqual( [], supportResults );
+		await this.page.waitForSelector( selectors.emptyResults );
+	}
+
 	async clickResult( target: number ): Promise< void > {
 		const popOver = await this.page.waitForSelector( selectors.supportPopover );
 		await popOver.waitForElementState( 'stable' );
@@ -101,9 +117,14 @@ export class SupportComponent extends BaseContainer {
 		await this.page.fill( selectors.searchInput, text );
 
 		if ( ! text ) {
-			await this.page.waitForLoadState( 'domcontentloaded' );
+			await this.page.waitForSelector( selectors.spinner, { state: 'hidden' } );
 		} else {
-			await this.page.waitForLoadState( 'networkidle' );
+			await this.page.waitForSelector( selectors.spinner );
+			await this.page.waitForSelector( selectors.spinner, { state: 'hidden' } );
 		}
+	}
+
+	async clearSearch(): Promise< void > {
+		await this.page.click( selectors.clearSearch );
 	}
 }
--- test/e2e/specs/specs-playwright/wp-support__search-spec.js
+++ test/e2e/specs/specs-playwright/wp-support__search-spec.js
@@ -49,5 +49,86 @@ describe( DataHelper.createSuiteTitle( 'Support' ), function () {
 			const adminCount = await supportComponent.getAdminResultsCount();
 			assert.strictEqual( 3, adminCount );
 		} );
+
+		it( 'Clear keyword', async function () {
+			await supportComponent.clearSearch();
+			const defaultResults = await supportComponent.getOverallResultsCount();
+			assert.strictEqual( 6, defaultResults );
+			const supportResults = await supportComponent.getSupportResultsCount();
+			assert.strictEqual( 0, supportResults );
+			const adminResults = await supportComponent.getAdminResultsCount();
+			assert.strictEqual( 0, adminResults );
+		} );
+	} );
+
+	describe( 'Empty search string', async function () {
+		let supportComponent;
+
+		it( 'Log in', async function () {
+			const loginFlow = new LoginFlow( this.page );
+			await loginFlow.login();
+		} );
+
+		it( 'Open Settings page', async function () {
+			await MyHomePage.Expect( this.page );
+			const sidebarComponent = await SidebarComponent.Expect( this.page );
+			await sidebarComponent.clickMenuItem( 'Settings' );
+		} );
+
+		it( 'Open support popover', async function () {
+			await SettingsPage.Expect( this.page );
+			supportComponent = await SupportComponent.Expect( this.page );
+			await supportComponent.clickSupportButton();
+		} );
+
+		it( 'Displays six entries by default', async function () {
+			const defaultResults = await supportComponent.getOverallResultsCount();
+			assert.strictEqual( 6, defaultResults );
+		} );
+
+		it( 'Enter invalid search keyword', async function () {
+			const keyword = '';
+			await supportComponent.search( keyword );
+		} );
+
+		it( 'Continues to display default results', async function () {
+			const defaultResults = await supportComponent.getOverallResultsCount();
+			assert.strictEqual( 6, defaultResults );
+		} );
+	} );
+
+	describe( 'Invalid search string', async function () {
+		let supportComponent;
+
+		it( 'Log in', async function () {
+			const loginFlow = new LoginFlow( this.page );
+			await loginFlow.login();
+		} );
+
+		it( 'Open Settings page', async function () {
+			await MyHomePage.Expect( this.page );
+			const sidebarComponent = await SidebarComponent.Expect( this.page );
+			await sidebarComponent.clickMenuItem( 'Settings' );
+		} );
+
+		it( 'Open support popover', async function () {
+			await SettingsPage.Expect( this.page );
+			supportComponent = await SupportComponent.Expect( this.page );
+			await supportComponent.clickSupportButton();
+		} );
+
+		it( 'Displays six entries by default', async function () {
+			const defaultResults = await supportComponent.getOverallResultsCount();
+			assert.strictEqual( 6, defaultResults );
+		} );
+
+		it( 'Enter invalid search keyword', async function () {
+			const keyword = ';;;ppp;;;';
+			await supportComponent.search( keyword );
+		} );
+
+		it( 'No search results are shown', async function () {
+			await supportComponent.noResults();
+		} );
 	} );
 } );
