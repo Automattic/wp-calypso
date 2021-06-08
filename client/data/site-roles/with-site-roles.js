28d96b9cfad9b13878fabc154bc0c20fa4ffa235
Markus Dobmann <m.do@posteo.net>
2021-06-08 18:15:28 +0200

    `withSiteRoles`: explicitly derive current `siteId`

--- client/data/site-roles/with-site-roles.js
+++ client/data/site-roles/with-site-roles.js
@@ -2,16 +2,19 @@
  * External dependencies
  */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { createHigherOrderComponent } from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
 import useSiteRolesQuery from './use-site-roles-query';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 const withSiteRoles = createHigherOrderComponent(
 	( Wrapped ) => ( props ) => {
-		const { data } = useSiteRolesQuery( props.siteId );
+		const siteId = useSelector( getSelectedSiteId );
+		const { data } = useSiteRolesQuery( siteId );
 
 		return <Wrapped { ...props } siteRoles={ data ?? [] } />;
 	},
