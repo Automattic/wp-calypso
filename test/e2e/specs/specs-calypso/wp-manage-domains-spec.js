57c0b04526ac5c77594871005813c28e434b388b
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2021-06-08 15:58:27 +0200

    Clean up and simplify domain specs The `SKIP_DOMAIN_TESTS` env var is no longer used so I removed the check. Also, skipping the "Can see the domain search component" test didn't make much sense, because the suite would fail anyway on the very next step. The whole step can be removed.

--- test/e2e/specs/specs-calypso/wp-manage-domains-spec.js
+++ test/e2e/specs/specs-calypso/wp-manage-domains-spec.js
@@ -13,17 +13,13 @@ import DomainsPage from '../../lib/pages/domains-page.js';
 import CheckOutPage from '../../lib/pages/signup/checkout-page.js';
 
 import FindADomainComponent from '../../lib/components/find-a-domain-component.js';
-import RegistrationUnavailableComponent from '../../lib/components/domain-registration-unavailable-component';
 import SecurePaymentComponent from '../../lib/components/secure-payment-component.js';
 import ShoppingCartWidgetComponent from '../../lib/components/shopping-cart-widget-component.js';
 import SidebarComponent from '../../lib/components/sidebar-component.js';
 import MyOwnDomainPage from '../../lib/pages/domain-my-own-page';
-import MapADomainPage from '../../lib/pages/domain-map-page';
 import EnterADomainComponent from '../../lib/components/enter-a-domain-component';
 
 import LoginFlow from '../../lib/flows/login-flow.js';
-
-import * as SlackNotifier from '../../lib/slack-notifier';
 
 const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 const screenSize = driverManager.currentScreenSize();
@@ -33,77 +29,52 @@ const host = dataHelper.getJetpackHost();
 describe( `[${ host }] Managing Domains: (${ screenSize }) @parallel`, function () {
 	this.timeout( mochaTimeOut );
 
-	describe( 'Adding a domain to an existing site', function () {
+	describe( 'Add a domain to an existing site', function () {
 		const blogName = dataHelper.getNewBlogName();
 		const domainEmailAddress = dataHelper.getEmailAddress( blogName, domainsInboxId );
 		const expectedDomainName = blogName + '.com';
 		const testDomainRegistarDetails = dataHelper.getTestDomainRegistarDetails( domainEmailAddress );
 
-		before( async function () {
-			if ( process.env.SKIP_DOMAIN_TESTS === 'true' ) {
-				await SlackNotifier.warn(
-					'Domains tests are currently disabled as SKIP_DOMAIN_TESTS is set to true',
-					{ suppressDuplicateMessages: true }
-				);
-				return this.skip();
-			}
+		it( 'Log in and go to Domains page', async function () {
+			const loginFlow = new LoginFlow( this.driver, 'gutenbergSimpleSiteUser' );
+			await loginFlow.loginAndSelectDomains();
 		} );
 
-		it( 'Log In and Select Domains', async function () {
-			return await new LoginFlow( this.driver, 'gutenbergSimpleSiteUser' ).loginAndSelectDomains();
-		} );
-
-		it( 'Can see the Domains page and choose add a domain', async function () {
+		it( 'Add a domain', async function () {
 			const domainsPage = await DomainsPage.Expect( this.driver );
-			await domainsPage.waitForPage();
 			await domainsPage.clickAddDomain();
-			await domainsPage.popOverMenuDisplayed();
-			return await domainsPage.clickPopoverItem( 'to this site' );
+			await domainsPage.clickPopoverItem( 'to this site' );
 		} );
 
-		it( 'Can see the domain search component', async function () {
-			let findADomainComponent;
-			try {
-				findADomainComponent = await FindADomainComponent.Expect( this.driver );
-			} catch ( err ) {
-				if ( await RegistrationUnavailableComponent.Expect( this.driver ) ) {
-					await SlackNotifier.warn( 'SKIPPING: Domain registration is currently unavailable. ', {
-						suppressDuplicateMessages: true,
-					} );
-					return this.skip();
-				}
-			}
-			return await findADomainComponent.waitForResults();
-		} );
-
-		it( 'Can search for a blog name', async function () {
+		it( 'Search for a blog name', async function () {
 			const findADomainComponent = await FindADomainComponent.Expect( this.driver );
-			// Search for the full blog name including the .com, as the default TLD suggestion is not always .com.
-			return await findADomainComponent.searchForBlogNameAndWaitForResults( expectedDomainName );
+			// Search for the full blog name including the .com, as the default TLD suggestion is not
+			// always .com
+			await findADomainComponent.searchForBlogNameAndWaitForResults( expectedDomainName );
 		} );
 
-		it( 'Can select the .com search result and decline Google Apps for email', async function () {
+		it( 'Select .com search result and decline Google Apps offer', async function () {
 			const findADomainComponent = await FindADomainComponent.Expect( this.driver );
 			await findADomainComponent.selectDomainAddress( expectedDomainName );
-			return await findADomainComponent.declineGoogleApps();
+			await findADomainComponent.declineGoogleApps();
 		} );
 
-		it( 'Can see checkout page and enter registrar details', async function () {
+		it( 'Go to checkout page and enter registrar details', async function () {
 			const checkOutPage = await CheckOutPage.Expect( this.driver );
 			await checkOutPage.enterRegistrarDetails( testDomainRegistarDetails );
-			return await checkOutPage.submitForm();
+			await checkOutPage.submitForm();
 		} );
 
-		it( 'Can then see secure payment component', async function () {
+		it( 'See the Secure Payment', async function () {
 			await SecurePaymentComponent.Expect( this.driver );
 		} );
 
-		it( 'Can close the checkout page', async function () {
+		it( 'Close the checkout page', async function () {
 			const checkOutPage = await CheckOutPage.Expect( this.driver );
 			await checkOutPage.close();
 		} );
 
-		it( 'Can remove added item from cart', async function () {
+		it( 'Remove added domain from cart', async function () {
 			const sidebarComponent = await SidebarComponent.Expect( this.driver );
 			await sidebarComponent.selectDomains();
 			const shoppingCartWidgetComponent = await ShoppingCartWidgetComponent.Expect( this.driver );
@@ -114,62 +85,28 @@ describe( `[${ host }] Managing Domains: (${ screenSize }) @parallel`, function 
 	describe( 'Map a domain to an existing site', function () {
 		const blogName = 'nature.com';
 
-		before( async function () {
-			if ( process.env.SKIP_DOMAIN_TESTS === 'true' ) {
-				await SlackNotifier.warn(
-					'Domains tests are currently disabled as SKIP_DOMAIN_TESTS is set to true',
-					{ suppressDuplicateMessages: true }
-				);
-				return this.skip();
-			}
+		it( 'Log in and go to Domains page', async function () {
+			const loginFlow = new LoginFlow( this.driver, 'gutenbergSimpleSiteUser' );
+			await loginFlow.loginAndSelectDomains();
 		} );
 
-		it( 'Log In and Select Domains', async function () {
-			return await new LoginFlow( this.driver, 'gutenbergSimpleSiteUser' ).loginAndSelectDomains();
-		} );
-
-		it( 'Can see the Domains page and choose add a domain', async function () {
+		it( 'Add a domain', async function () {
 			const domainsPage = await DomainsPage.Expect( this.driver );
-			await domainsPage.waitForPage();
 			await domainsPage.clickAddDomain();
-			await domainsPage.popOverMenuDisplayed();
-			return await domainsPage.clickPopoverItem( 'to this site' );
+			await domainsPage.clickPopoverItem( 'to this site' );
 		} );
 
-		it( 'Can see the domain search component', async function () {
-			let findADomainComponent;
-			try {
-				findADomainComponent = await FindADomainComponent.Expect( this.driver );
-			} catch ( err ) {
-				if ( await RegistrationUnavailableComponent.Expect( this.driver ) ) {
-					await SlackNotifier.warn( 'SKIPPING: Domain registration is currently unavailable. ', {
-						suppressDuplicateMessages: true,
-					} );
-					return this.skip();
-				}
-			}
-			return await findADomainComponent.waitForResults();
-		} );
-
-		it( 'Can select to use an existing domain', async function () {
+		it( 'Use own domain', async function () {
 			const findADomainComponent = await FindADomainComponent.Expect( this.driver );
-			return await findADomainComponent.selectUseOwnDomain();
+			await findADomainComponent.selectUseOwnDomain();
 		} );
 
-		it( 'Can see use my own domain page', async function () {
-			return await MyOwnDomainPage.Expect( this.driver );
-		} );
-
-		it( 'Can select to buy domain mapping', async function () {
+		it( 'Buy domain mapping', async function () {
 			const myOwnDomainPage = await MyOwnDomainPage.Expect( this.driver );
 			return await myOwnDomainPage.selectBuyDomainMapping();
 		} );
 
-		it( 'Can see enter a domain component', async function () {
-			return await MapADomainPage.Expect( this.driver );
-		} );
-
-		it( 'Can enter the domain name', async function () {
+		it( 'Enter domain name', async function () {
 			const enterADomainComponent = await EnterADomainComponent.Expect( this.driver );
 			return await enterADomainComponent.enterADomain( blogName );
 		} );
