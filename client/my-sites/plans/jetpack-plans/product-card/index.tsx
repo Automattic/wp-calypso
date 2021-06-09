07240e8238ea4acec1f30565201efaf584c1e20f
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-06-09 13:24:00 -0500

    Move productTooltip to its own file under product-card

--- client/my-sites/plans/jetpack-plans/product-card/index.tsx
+++ client/my-sites/plans/jetpack-plans/product-card/index.tsx
@@ -11,7 +11,7 @@ import { TranslateResult, useTranslate } from 'i18n-calypso';
 import JetpackProductCard from 'calypso/components/jetpack/card/jetpack-product-card';
 import PlanRenewalMessage from '../plan-renewal-message';
 import useItemPrice from '../use-item-price';
-import { productAboveButtonText, productTooltip } from './utils';
+import { productAboveButtonText } from './utils';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import {
 	planHasFeature,
@@ -30,6 +30,7 @@ import { getSitePurchases } from 'calypso/state/purchases/selectors';
 import { getSiteAvailableProduct } from 'calypso/state/sites/products/selectors';
 import { isJetpackSiteMultiSite } from 'calypso/state/sites/selectors';
 import productButtonLabel from './product-button-label';
+import productTooltip from './product-tooltip';
 
 /**
  * Type dependencies
