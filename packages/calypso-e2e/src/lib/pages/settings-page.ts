2b89114b530b5bb2577fb74269bb036cfde5bb14
Edwin Takahashi <egao@outlook.com>
2021-06-09 13:30:59 -0700

    Placate the linter.

--- packages/calypso-e2e/src/lib/pages/settings-page.ts
+++ packages/calypso-e2e/src/lib/pages/settings-page.ts
@@ -16,10 +16,20 @@ const selectors = {
  * Represents the Settings page.
  */
 export class SettingsPage extends BaseContainer {
+	/**
+	 * Construct an instance of the SettingsPage.
+	 *
+	 * @param {Page} page Underlying page where interactions take place.
+	 */
 	constructor( page: Page ) {
 		super( page, selectors.main );
 	}
 
+	/**
+	 * Post-initialization checks.
+	 *
+	 * @returns {Promise<void>} No return value.
+	 */
 	async _postInit(): Promise< void > {
 		await this.page.waitForLoadState( 'domcontentloaded' );
 	}
