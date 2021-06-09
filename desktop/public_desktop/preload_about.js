452fa33b60b8ff8a70348f427053b285a66696ca
Sandy McFadden <sandy.mcfadden@automattic.com>
2021-06-09 08:29:03 -0300

    Fix about screen secrets

--- desktop/public_desktop/preload_about.js
+++ desktop/public_desktop/preload_about.js
@@ -3,6 +3,11 @@ const { ipcRenderer, contextBridge } = require( 'electron' );
 ( async () => {
 	const config = await ipcRenderer.invoke( 'get-config' );
 	contextBridge.exposeInMainWorld( 'electron', {
+		send: ( channel, ...args ) => {
+			if ( channel === 'secrets' ) {
+				ipcRenderer.send( channel, ...args );
+			}
+		},
 		config,
 	} );
 } )();
