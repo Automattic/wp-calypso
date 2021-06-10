fdc733fc03c1ad00deea3f23a9ec3c6e6e53deb7
Edwin Takahashi <egao@outlook.com>
2021-06-10 11:06:12 -0700

    Add JSDoc where it was missing.

--- packages/calypso-e2e/src/lib/pages/my-home-page.ts
+++ packages/calypso-e2e/src/lib/pages/my-home-page.ts
@@ -13,7 +13,17 @@ const selectors = {
 	statsCard: '.stats',
 };
 
+/**
+ * Represents the My Home page that users land on after logging in.
+ *
+ * @augments {BaseContainer}
+ */
 export class MyHomePage extends BaseContainer {
+	/**
+	 * Constructs an instance of the MyHomePage object.
+	 *
+	 * @param {Page} page Underlying page on which interactions take place.
+	 */
 	constructor( page: Page ) {
 		super( page, selectors.dashboard );
 	}
