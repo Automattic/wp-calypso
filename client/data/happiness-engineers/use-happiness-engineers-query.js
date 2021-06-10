b277d6719639e1c57c38347576c9641cd1a40af8
flootr <m.do@posteo.net>
2021-06-10 10:17:19 +0200

    `useHappinessEngineersQuery`: use `wp.req.get` directly (#53573)

--- client/data/happiness-engineers/use-happiness-engineers-query.js
+++ client/data/happiness-engineers/use-happiness-engineers-query.js
@@ -9,7 +9,7 @@ import { useQuery } from 'react-query';
 import wpcom from 'calypso/lib/wp';
 
 const useHappinessEngineersQuery = () =>
-	useQuery( 'happinessEngineers', async () => await wpcom.undocumented().getHappinessEngineers(), {
+	useQuery( 'happinessEngineers', async () => await wpcom.req.get( '/meta/happiness-engineers/' ), {
 		refetchOnWindowFocus: false,
 	} );
 
