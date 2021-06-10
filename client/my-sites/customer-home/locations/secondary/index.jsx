e9cd7ae90f0e70ecf3b0c36bcc7083a71e473356
Philip Jackson <p-jackson@live.com>
2021-06-10 17:10:39 +1200

    Secondary area of My Home supports the support search card

--- client/my-sites/customer-home/locations/secondary/index.jsx
+++ client/my-sites/customer-home/locations/secondary/index.jsx
@@ -8,17 +8,20 @@ import React from 'react';
  */
 import Stats from 'calypso/my-sites/customer-home/cards/features/stats';
 import QuickStartVideo from 'calypso/my-sites/customer-home/cards/education/quick-start-video';
+import HelpSearch from 'calypso/my-sites/customer-home/cards/features/help-search';
 import LearnGrow from './learn-grow';
 import {
 	FEATURE_STATS,
 	SECTION_LEARN_GROW,
 	FEATURE_QUICK_START_VIDEO,
+	FEATURE_SUPPORT,
 } from 'calypso/my-sites/customer-home/cards/constants';
 
 const cardComponents = {
 	[ FEATURE_STATS ]: Stats,
 	[ FEATURE_QUICK_START_VIDEO ]: QuickStartVideo,
 	[ SECTION_LEARN_GROW ]: LearnGrow,
+	[ FEATURE_SUPPORT ]: HelpSearch,
 };
 
 const Secondary = ( { cards } ) => {
