9c807b1a6c4c1af8ce442fa921aef9b162b588d6
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2021-06-14 12:34:20 +0300

    i18n: Add hreflang links for themes section (#53449)
    * i18n: Add hreflang links for themes section
    
    * Re-organize lang codes
    
    * Skip setting hreflang links for logged in users

--- client/controller/index.node.js
+++ client/controller/index.node.js
@@ -3,15 +3,20 @@
  */
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
+import config from '@automattic/calypso-config';
+import { localizeUrl } from '@automattic/i18n-utils';
 
 /**
  * Internal dependencies
  */
 import { makeLayoutMiddleware } from './shared.js';
 import { ssrSetupLocaleMiddleware } from './ssr-setup-locale.js';
+import { getLanguageSlugs } from 'calypso/lib/i18n-utils';
 import LayoutLoggedOut from 'calypso/layout/logged-out';
 import CalypsoI18nProvider from 'calypso/components/calypso-i18n-provider';
 import { RouteProvider } from 'calypso/components/route';
+import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
+import { setDocumentHeadLink } from 'calypso/state/document-head/actions';
 
 /**
  * Re-export
@@ -50,6 +55,42 @@ const ProviderWrappedLoggedOutLayout = ( {
 export const makeLayout = makeLayoutMiddleware( ProviderWrappedLoggedOutLayout );
 
 export const ssrSetupLocale = ssrSetupLocaleMiddleware();
+
+export const setHrefLangLinks = ( context, next ) => {
+	const isLoggedIn = isUserLoggedIn( context.store.getState() );
+
+	if ( isLoggedIn ) {
+		next();
+		return;
+	}
+
+	const langCodes = [ 'x-default', 'en', ...config( 'magnificent_non_en_locales' ) ];
+	const hrefLangBlock = langCodes.map( ( hrefLang ) => {
+		let localeSlug = hrefLang;
+
+		if ( localeSlug === 'x-default' ) {
+			localeSlug = config( 'i18n_default_locale_slug' );
+		}
+
+		const baseUrl = `${ context.res.req.protocol }://${ context.res.req.get( 'host' ) }${
+			context.res.req.originalUrl
+		}`;
+		const baseUrlWithoutLang = baseUrl.replace(
+			new RegExp( `\\/(${ getLanguageSlugs().join( '|' ) })(\\/|\\?|$)` ),
+			'$2'
+		);
+		const href = localizeUrl( baseUrlWithoutLang, localeSlug, isLoggedIn );
+
+		return {
+			rel: 'alternate',
+			hrefLang,
+			href,
+		};
+	} );
+
+	context.store.dispatch( setDocumentHeadLink( hrefLangBlock ) );
+	next();
+};
 
 /**
  * These functions are not used by Node. It is here to provide an APi compatible with `./index.web.js`
