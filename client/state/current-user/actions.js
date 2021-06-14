c19d1a5742b5076f8ad3fc9c6add24903688199b
Marin Atanasov <tyxla@abv.bg>
2021-06-14 11:15:34 +0300

    State: Remove clearCurrentUser()

--- client/state/current-user/actions.js
+++ client/state/current-user/actions.js
@@ -1,9 +1,7 @@
 /**
  * Internal dependencies
  */
-import userLibrary from 'calypso/lib/user';
-import { CURRENT_USER_CLEAR, CURRENT_USER_RECEIVE } from 'calypso/state/action-types';
-import { clearStore } from 'calypso/lib/user/store';
+import { CURRENT_USER_RECEIVE } from 'calypso/state/action-types';
 
 /**
  * Returns an action object that sets the current user object on the store
@@ -15,27 +13,5 @@ export function setCurrentUser( user ) {
 	return {
 		type: CURRENT_USER_RECEIVE,
 		user,
-	};
-}
-
-/**
- * Returns an action object that clears the current user object from the store
- *
- * @returns {object}        Action object
- */
-export function clearCurrentUser() {
-	return async ( dispatch ) => {
-		// @TODO: Remove once `lib/user` has fully been reduxified
-		userLibrary().data = false;
-
-		/**
-		 * Clear internal user data and empty localStorage cache
-		 * to discard any user reference that the application may hold
-		 */
-		await clearStore();
-
-		dispatch( {
-			type: CURRENT_USER_CLEAR,
-		} );
 	};
 }
