3251059f15dbffc50957df088be864ef7dd03b08
mmtr <miguel.torres@automattic.com>
2021-06-14 12:18:48 +0200

    Add toggle for Markdown on comments

--- client/my-sites/site-settings/composing/markdown.jsx
+++ client/my-sites/site-settings/composing/markdown.jsx
@@ -3,17 +3,24 @@
  */
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useTranslate } from 'i18n-calypso';
+import { ToggleControl } from '@wordpress/components';
+import { Card } from '@automattic/components';
 
 /**
  * Internal dependencies
  */
 import SupportInfo from 'calypso/components/support-info';
-import { ToggleControl } from '@wordpress/components';
-import { Card } from '@automattic/components';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import isJetpackSite from 'calypso/state/sites/selectors/is-jetpack-site';
+import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 
 const Markdown = ( { fields, handleToggle, isRequestingSettings, isSavingSettings } ) => {
 	const translate = useTranslate();
+	const siteId = useSelector( getSelectedSiteId );
+	const isJetpack = useSelector( ( state ) => isJetpackSite( state, siteId ) );
+	const isAtomic = useSelector( ( state ) => isAtomicSite( state, siteId ) );
 
 	return (
 		<Card className="composing__card site-settings__card">
@@ -21,7 +28,11 @@ const Markdown = ( { fields, handleToggle, isRequestingSettings, isSavingSetting
 				text={ translate(
 					'Use Markdown syntax to compose content with links, lists, and other styles. This setting enables Markdown in the Classic Editor as well as within a Classic Editor block.'
 				) }
-				link="https://wordpress.com/support/markdown-quick-reference/"
+				link={
+					isJetpack && ! isAtomic
+						? 'https://jetpack.com/support/markdown/'
+						: 'https://wordpress.com/support/markdown-quick-reference/'
+				}
 			/>
 			<ToggleControl
 				checked={ !! fields.wpcom_publish_posts_with_markdown }
