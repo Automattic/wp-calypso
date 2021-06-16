ab2e83ea4766475a54e0fa280d1a12d5e7fe5aa5
David Szabo <david.szabo97@gmail.com>
2021-06-16 13:25:29 +0200

    Add clicked_element property

--- apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-custom-post-template-actions-click.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-custom-post-template-actions-click.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import tracksRecordEvent from './track-record-event';
@@ -8,14 +13,30 @@ const TRACKABLE_TAG_NAMES = [ 'BUTTON', 'SELECT' ];
 /**
  * Return the event definition object to track `wpcom_block_editor_custom_post_template_save`.
  *
- * @returns {{handler: Function, selector: string, type: string}} event object definition.
+ * @returns {import('./types').DelegateEventHandler} event object definition.
  */
 export default () => ( {
 	selector: '.edit-post-sidebar .components-panel__body',
 	type: 'click',
+	capture: true,
 	handler: ( event ) => {
-		if ( TRACKABLE_TAG_NAMES.includes( event.target.tagName ) ) {
-			tracksRecordEvent( 'wpcom_block_editor_custom_post_template_actions_click' );
+		if ( ! TRACKABLE_TAG_NAMES.includes( event.target.tagName ) ) {
+			return;
 		}
+
+		let clickedElement;
+		if ( event.target.classList.contains( 'components-panel__body-toggle' ) ) {
+			clickedElement = 'panel-toggle';
+		} else if ( event.target.classList.contains( 'components-select-control__input' ) ) {
+			clickedElement = 'dropdown';
+		} else if ( event.target.innerText === __( 'Edit' ) ) {
+			clickedElement = 'button-edit';
+		} else if ( event.target.innerText === __( 'New' ) ) {
+			clickedElement = 'button-new';
+		}
+
+		tracksRecordEvent( 'wpcom_block_editor_custom_post_template_actions_click', {
+			clicked_element: clickedElement,
+		} );
 	},
 } );
