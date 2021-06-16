bfd96f6b41c439a30eb8621e68b8533f413e3e5a
Sergio Cinos <sergio.cinos@gmail.com>
2021-06-16 11:29:12 +0200

    Restore missing tests

--- test/e2e/jest.config.js
+++ test/e2e/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	cacheDirectory: '<rootDir>/../../.cache/jest',
 	testMatch: [ '<rootDir>/specs/**/*.js' ],
 	globalSetup: '<rootDir>/jest.global-setup.js',
-	setupFilesAfterEnv: [ '<rootDir>/jest.setup.js' ],
+	setupFilesAfterEnv: [ '<rootDir>/jest.setup.js', '<rootDir>/lib/hooks/jest.js' ],
 	verbose: true,
 	transform: {
 		'\\.[jt]sx?$': [ 'babel-jest', { configFile: '../../babel.config.js' } ],
