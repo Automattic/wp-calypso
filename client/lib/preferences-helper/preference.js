ad9a3b2aea463f15922e1592f1fcc1d2614eb5bb
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-06-16 13:57:14 -0500

    Preferences: Memoize pref handlers to prevent unnecessary re-renders

--- client/lib/preferences-helper/preference.js
+++ client/lib/preferences-helper/preference.js
@@ -1,10 +1,10 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { localize } from 'i18n-calypso';
-import { partial, isPlainObject } from 'lodash';
+import React, { useCallback, useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { useTranslate } from 'i18n-calypso';
+import { isPlainObject } from 'lodash';
 
 /**
  * Internal dependencies
@@ -16,46 +16,58 @@ import BooleanPreference from './boolean-preference';
 import StringPreference from './string-preference';
 import NumberPreference from './number-preference';
 
-class Preference extends Component {
-	render() {
-		const { name, value, translate, unsetPreference } = this.props;
+const Preference = ( { name, value } ) => {
+	const dispatch = useDispatch();
+	const translate = useTranslate();
 
-		let preferenceHandler = (
+	const preferenceHandler = useMemo( () => {
+		if ( Array.isArray( value ) ) {
+			return <ArrayPreference name={ name } value={ value } />;
+		}
+
+		if ( isPlainObject( value ) ) {
+			return <ObjectPreference name={ name } value={ value } />;
+		}
+
+		if ( 'string' === typeof value ) {
+			return <StringPreference name={ name } value={ value } />;
+		}
+
+		if ( 'boolean' === typeof value ) {
+			return <BooleanPreference name={ name } value={ value } />;
+		}
+
+		if ( 'number' === typeof value ) {
+			return <NumberPreference name={ name } value={ value } />;
+		}
+
+		return (
 			<ul>
 				<li key={ name }>{ value.toString() } </li>
 			</ul>
 		);
+	}, [ name, value ] );
 
-		if ( Array.isArray( value ) ) {
-			preferenceHandler = <ArrayPreference name={ name } value={ value } />;
-		} else if ( isPlainObject( value ) ) {
-			preferenceHandler = <ObjectPreference name={ name } value={ value } />;
-		} else if ( 'string' === typeof value ) {
-			preferenceHandler = <StringPreference name={ name } value={ value } />;
-		} else if ( 'boolean' === typeof value ) {
-			preferenceHandler = <BooleanPreference name={ name } value={ value } />;
-		} else if ( 'number' === typeof value ) {
-			preferenceHandler = <NumberPreference name={ name } value={ value } />;
-		}
+	const unsetPreference = useCallback( () => dispatch( savePreference( name, null ) ), [
+		dispatch,
+		name,
+	] );
 
-		return (
-			<div>
-				<div className="preferences-helper__preference-header">
-					<button
-						className="preferences-helper__unset"
-						onClick={ partial( unsetPreference, name, null ) }
-						title={ translate( 'Unset Preference' ) }
-					>
-						{ 'X' }
-					</button>
-					<span>{ name }</span>
-				</div>
-				{ preferenceHandler }
+	return (
+		<div>
+			<div className="preferences-helper__preference-header">
+				<button
+					className="preferences-helper__unset"
+					onClick={ unsetPreference }
+					title={ translate( 'Unset Preference' ) }
+				>
+					{ 'X' }
+				</button>
+				<span>{ name }</span>
 			</div>
-		);
-	}
-}
+			{ preferenceHandler }
+		</div>
+	);
+};
 
-export default connect( null, {
-	unsetPreference: savePreference,
-} )( localize( Preference ) );
+export default Preference;
