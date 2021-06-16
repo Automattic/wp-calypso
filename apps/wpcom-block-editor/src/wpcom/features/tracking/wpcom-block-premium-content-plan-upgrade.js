d99c17dcebce1b78e5b54375d1ba6f80e9ecee1b
Dávid Szabó <david.szabo97@gmail.com>
2021-06-16 15:57:39 +0200

    Block Editor Tracking: Add event subscribers system and add capture event listener (#53598)

--- apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-premium-content-plan-upgrade.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-premium-content-plan-upgrade.js
@@ -6,9 +6,10 @@ import tracksRecordEvent from './track-record-event';
 /**
  * Return the event definition object to track `wpcom_block_premium_content_upgrade_click`.
  *
- * @returns {{handler: Function, selector: string, type: string}} event object definition.
+ * @returns {import('./types').DelegateEventHandler} event object definition.
  */
 export default () => ( {
+	id: 'wpcom-block-premium-content-plan-upgrade',
 	selector: '.wp-block[data-type="premium-content/container"] .plan-nudge__button',
 	type: 'click',
 	handler: () => tracksRecordEvent( 'wpcom_block_premium_content_plan_upgrade_click' ),
