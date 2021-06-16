7a0237bd9ac1240328e6f7a12e6a856db13ae6b2
Sergio Cinos <sergio.cinos@gmail.com>
2021-06-16 05:21:50 +0200

    Debug test capture

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -104,6 +104,8 @@ object RunCalypsoE2eDesktopTests : BuildType({
 				export BROWSERLOCALE="en"
 				export NODE_CONFIG="{\"calypsoBaseURL\":\"${'$'}{URL%/}\"}"
 
+				MOCHA_CAPTURE_PATH=test.json yarn mocha --reporter-options tags=parallel --reporter /Users/sergio/src/automattic/wp-calypso/packages/magellan-mocha-plugin/lib/suite_capture.js specs/specs-calypso
+
 				yarn magellan --config=magellan-calypso.json --max_workers=%E2E_WORKERS% --local_browser=chrome --mocha_args="--reporter mocha-multi-reporters --reporter-options configFile=mocha-reporter.json"
 			""".trimIndent()
 			dockerImage = "%docker_image_e2e%"
