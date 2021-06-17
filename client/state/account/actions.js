90a76101564fbeb140838284da57c1ccb2f3a1d2
Marin Atanasov <tyxla@abv.bg>
2021-06-17 13:11:01 +0300

    Remove unnecessary local storage clearing

--- client/state/account/actions.js
+++ client/state/account/actions.js
@@ -2,7 +2,6 @@
  * Internal dependencies
  */
 import { ACCOUNT_CLOSE, ACCOUNT_CLOSE_SUCCESS } from 'calypso/state/action-types';
-import { clearStore } from 'calypso/lib/user/store';
 
 import 'calypso/state/data-layer/wpcom/me/account/close';
 import 'calypso/state/account/init';
@@ -14,10 +13,7 @@ export function closeAccount() {
 }
 
 export function closeAccountSuccess() {
-	return async ( dispatch ) => {
-		await clearStore();
-		dispatch( {
-			type: ACCOUNT_CLOSE_SUCCESS,
-		} );
+	return {
+		type: ACCOUNT_CLOSE_SUCCESS,
 	};
 }
