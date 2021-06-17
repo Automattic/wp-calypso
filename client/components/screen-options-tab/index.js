5747cd1d26e1a2c3f8cd9bd56b666594efc2c565
Tom Cafferkey <tjcafferkey@gmail.com>
2021-06-17 16:18:34 +0100

    Accessibility styles

--- client/components/screen-options-tab/index.js
+++ client/components/screen-options-tab/index.js
@@ -1,7 +1,7 @@
 /**
  * External Dependencies
  */
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import classNames from 'classnames';
 import { useI18n } from '@wordpress/react-i18n';
 
@@ -10,13 +10,50 @@ import { useI18n } from '@wordpress/react-i18n';
  */
 import './style.scss';
 
+const isBoolean = ( val ) => 'boolean' === typeof val;
+
 const ScreenOptionsTab = ( { children } ) => {
+	const ref = useRef( null );
 	const [ isOpen, setIsOpen ] = useState( false );
 	const { __ } = useI18n();
 
+	const handleToggle = ( bool ) => {
+		if ( isBoolean( bool ) ) {
+			setIsOpen( bool );
+		} else {
+			setIsOpen( ! isOpen );
+		}
+	};
+
+	const handleClosing = ( e ) => {
+		if ( e instanceof KeyboardEvent ) {
+			if ( e.key === 'Escape' ) {
+				handleToggle( false );
+			}
+			return;
+		}
+
+		if ( ref.current && ! ref.current.contains( e.target ) ) {
+			handleToggle( false );
+			return;
+		}
+	};
+
+	useEffect( () => {
+		// Close the component when a click outside happens or users clicks Esc key.
+		document.addEventListener( 'click', handleClosing, true );
+		document.addEventListener( 'keydown', handleClosing, true );
+
+		return () => {
+			// Lets cleanup after ourselves when the component unmounts.
+			document.removeEventListener( 'click', handleClosing, true );
+			document.removeEventListener( 'keydown', handleClosing, true );
+		};
+	}, [] );
+
 	return (
-		<div className="screen-options-tab">
-			<button className="screen-options-tab__button" onClick={ () => setIsOpen( ! isOpen ) }>
+		<div className="screen-options-tab" ref={ ref }>
+			<button className="screen-options-tab__button" onClick={ handleToggle }>
 				<span className="screen-options-tab__label">{ __( 'Screen Options' ) }</span>
 				<span
 					className={ classNames( 'screen-options-tab__icon', {
