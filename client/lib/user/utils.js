f92624ef72b550d163f2c22b42eea9a763badf31
Caleb Bauermeister <cbauerman@gmail.com>
2021-06-18 16:50:25 -0700

    revert changes to wpcom xhr wrapper

---
+++ client/lib/user/utils.js
@@ -0,0 +1,20 @@
+/**
+ * Internal dependencies
+ */
+import user from 'calypso/lib/user';
+import { getLogoutUrl } from 'calypso/lib/user/shared-utils';
+
+const userUtils = {
+	logout( redirect ) {
+		const logoutUrl = getLogoutUrl( user().get(), redirect );
+
+		// Clear any data stored locally within the user data module or localStorage
+		user()
+			.clear()
+			.then( () => {
+				window.location.href = logoutUrl;
+			} );
+	},
+};
+
+export default userUtils;
--- client/lib/wpcom-xhr-wrapper/index.js
+++ client/lib/wpcom-xhr-wrapper/index.js
@@ -4,6 +4,11 @@
 import debugModule from 'debug';
 
 /**
+ * Internal dependencies
+ */
+import userUtils from 'calypso/lib/user/utils';
+
+/**
  * Module variables
  */
 const debug = debugModule( 'calypso:wpcom-xhr-wrapper' );
@@ -15,7 +20,7 @@ export default async function ( params, callback ) {
 	return xhr( params, function ( error, response, headers ) {
 		if ( error && error.name === 'InvalidTokenError' ) {
 			debug( 'Invalid token error detected, authorisation probably revoked - logging out' );
-			window.location.reload();
+			userUtils.logout();
 		}
 
 		callback( error, response, headers );
