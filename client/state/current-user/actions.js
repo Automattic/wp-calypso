9ab79e4c0eeeaf6ae0662cdc27c59d1c6cc6da65
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-06-18 11:32:42 +0300

    State: Introduce a fetchCurrentUser() thunk (#53780)

--- client/state/current-user/actions.js
+++ client/state/current-user/actions.js
@@ -1,10 +1,11 @@
 /**
  * Internal dependencies
  */
-import { CURRENT_USER_RECEIVE } from 'calypso/state/action-types';
-import { clearStore } from 'calypso/lib/user/store';
+import wpcom from 'calypso/lib/wp';
+import { clearStore, getStoredUserId, setStoredUserId } from 'calypso/lib/user/store';
+import { CURRENT_USER_FETCH, CURRENT_USER_RECEIVE } from 'calypso/state/action-types';
+import { filterUserObject, getLogoutUrl } from 'calypso/lib/user/shared-utils';
 import { getCurrentUser } from 'calypso/state/current-user/selectors';
-import { getLogoutUrl } from 'calypso/lib/user/shared-utils';
 
 /**
  * Returns an action object that sets the current user object on the store
@@ -16,6 +17,29 @@ export function setCurrentUser( user ) {
 	return {
 		type: CURRENT_USER_RECEIVE,
 		user,
+	};
+}
+
+export function fetchCurrentUser() {
+	return async ( dispatch ) => {
+		dispatch( {
+			type: CURRENT_USER_FETCH,
+		} );
+
+		try {
+			const user = await wpcom.me().get( {
+				meta: 'flags',
+			} );
+			const userData = filterUserObject( user );
+
+			const storedUserId = getStoredUserId();
+			if ( storedUserId != null && storedUserId !== userData.ID ) {
+				await clearStore();
+			}
+
+			setStoredUserId( userData.ID );
+			dispatch( setCurrentUser( userData ) );
+		} catch {}
 	};
 }
 
