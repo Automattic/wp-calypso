770ae67ef2882c5bf4c2709a8f594606e345b908
David Szabo <david.szabo97@gmail.com>
2021-06-18 09:31:02 +0200

    Add ID and fix typing

--- apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-details-click.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-details-click.js
@@ -6,9 +6,10 @@ import tracksRecordEvent from './track-record-event';
 /**
  * Return the event definition object to track `wpcom_block_editor_details_click`.
  *
- * @returns {{handler: Function, selector: string, type: string}} event object definition.
+ * @returns {import('./types').DelegateEventHandler} event object definition.
  */
 export default () => ( {
+	id: 'wpcom-block-editor-details-click',
 	selector: '.edit-post-header .table-of-contents button[aria-disabled="false"]',
 	type: 'click',
 	handler: () => tracksRecordEvent( 'wpcom_block_editor_details_click' ),
