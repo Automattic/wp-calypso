629ba36c82771222c95ee3d33b532a534b26f09b
Ian Ramos <ianramosc@gmail.com>
2021-06-20 18:04:45 -0300

    Partner Portal: Change logic

--- client/jetpack-cloud/sections/partner-portal/primary/landing-page/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/primary/landing-page/index.tsx
@@ -1,57 +1,25 @@
 /**
  * External dependencies
  */
-import React, { useEffect, ReactElement } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useTranslate } from 'i18n-calypso';
 import page from 'page';
 
 /**
  * Internal dependencies
  */
-import { LicenseCounts } from 'calypso/state/partner-portal/types';
-import {
-	hasFetchedLicenseCounts,
-	getLicenseCounts,
-} from 'calypso/state/partner-portal/licenses/selectors';
-import QueryJetpackPartnerPortalLicenseCounts from 'calypso/components/data/query-jetpack-partner-portal-license-counts';
-import Main from 'calypso/components/main';
-import CardHeading from 'calypso/components/card-heading';
-import Spinner from 'calypso/components/spinner';
+import { getActivePartnerKey } from 'calypso/state/partner-portal/partner/selectors';
 
-const getTotalCount = ( counts: LicenseCounts ): number => {
-	return Object.values( counts ).reduce( ( acc, count ) => {
-		if ( typeof count !== 'number' ) return acc;
-
-		return acc + count;
-	} );
-};
-
-export default function LandingPage(): ReactElement | null {
-	const translate = useTranslate();
-	const counts = useSelector( getLicenseCounts );
-	const hasFetched = useSelector( hasFetchedLicenseCounts );
-	const totalCount = getTotalCount( counts );
+export default function LandingPage(): React.ReactElement | null {
+	const activePartnerKey = useSelector( getActivePartnerKey );
 
 	useEffect( () => {
-		if ( ! hasFetched ) {
-			return;
-		}
-
-		if ( totalCount > 0 ) {
+		if ( activePartnerKey?.hasLicenses ) {
 			return page.redirect( '/partner-portal/billing' );
 		}
 
 		return page.redirect( '/partner-portal/licenses' );
-	}, [ totalCount, hasFetched ] );
+	}, [ activePartnerKey ] );
 
-	return (
-		<Main className="landing-page__partner-portal">
-			<QueryJetpackPartnerPortalLicenseCounts />
-
-			<CardHeading size={ 36 }>{ translate( 'Partner Portal' ) }</CardHeading>
-
-			{ ! hasFetched && <Spinner /> }
-		</Main>
-	);
+	return null;
 }
