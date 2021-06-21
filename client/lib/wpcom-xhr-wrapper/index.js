bdda5d493da9ae2e5fe5a9cb7b35310ff7f54c60
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-06-21 22:55:36 +0300

    wpcom-xhr-wrapper: Remove lib/user usage (#53873)
    * wpcom-xhr-wrapper: Remove lib/user usage
    
    * Add clearStore()

--- client/lib/wpcom-xhr-wrapper/index.js
+++ client/lib/wpcom-xhr-wrapper/index.js
@@ -1,34 +1,17 @@
 /**
- * External dependencies
- */
-import debugModule from 'debug';
-
-/**
  * Internal dependencies
  */
-import user from 'calypso/lib/user';
+import { clearStore } from 'calypso/lib/user/store';
 import { getLogoutUrl } from 'calypso/lib/user/shared-utils';
-
-/**
- * Module variables
- */
-const debug = debugModule( 'calypso:wpcom-xhr-wrapper' );
 
 export default async function ( params, callback ) {
 	const xhr = ( await import( /* webpackChunkName: "wpcom-xhr-request" */ 'wpcom-xhr-request' ) )
 		.default;
 
-	return xhr( params, function ( error, response, headers ) {
+	return xhr( params, async function ( error, response, headers ) {
 		if ( error && error.name === 'InvalidTokenError' ) {
-			debug( 'Invalid token error detected, authorization probably revoked - logging out' );
-
-			const logoutUrl = getLogoutUrl( user().get() );
-			// Clear any data stored locally within the user data module or localStorage
-			user()
-				.clear()
-				.then( () => {
-					window.location.href = logoutUrl;
-				} );
+			await clearStore();
+			window.location.href = getLogoutUrl();
 		}
 
 		callback( error, response, headers );
