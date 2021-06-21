83fdb341a6f428eafa8a6147374c03e513a68253
David Szabo <david.szabo97@gmail.com>
2021-06-21 14:01:53 +0200

    Track sidebar open and editor close propertly

--- apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-close-click.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-close-click.js
@@ -10,7 +10,8 @@ import tracksRecordEvent from './track-record-event';
  */
 export default () => ( {
 	id: 'wpcom-block-editor-close-click',
-	selector: '.edit-post-header .edit-post-fullscreen-mode-close',
+	selector:
+		'.edit-post-header .edit-post-fullscreen-mode-close:not(.wpcom-block-editor-nav-sidebar-toggle-sidebar-button__button), .wpcom-block-editor-nav-sidebar-nav-sidebar__home-button',
 	type: 'click',
 	handler: () => tracksRecordEvent( 'wpcom_block_editor_close_click' ),
 } );
---
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-nav-sidebar-open.js
@@ -0,0 +1,16 @@
+/**
+ * Internal dependencies
+ */
+import tracksRecordEvent from './track-record-event';
+
+/**
+ * Return the event definition object to track `wpcom_block_editor_nav_sidebar_open`.
+ *
+ * @returns {import('./types').DelegateEventHandler} event object definition.
+ */
+export default () => ( {
+	id: 'wpcom-block-editor-nav-sidebar-open',
+	selector: '.edit-post-header .wpcom-block-editor-nav-sidebar-toggle-sidebar-button__button',
+	type: 'click',
+	handler: () => tracksRecordEvent( 'wpcom_block_editor_nav_sidebar_open' ),
+} );
