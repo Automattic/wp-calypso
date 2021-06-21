54f6c647c7eab465a297a617dab000092d9418f6
Marin Atanasov <tyxla@abv.bg>
2021-06-21 21:32:53 +0300

    wpcom-xhr-wrapper: Remove lib/user usage

--- client/lib/wpcom-xhr-wrapper/index.js
+++ client/lib/wpcom-xhr-wrapper/index.js
@@ -1,18 +1,7 @@
 /**
- * External dependencies
- */
-import debugModule from 'debug';
-
-/**
  * Internal dependencies
  */
-import user from 'calypso/lib/user';
 import { getLogoutUrl } from 'calypso/lib/user/shared-utils';
-
-/**
- * Module variables
- */
-const debug = debugModule( 'calypso:wpcom-xhr-wrapper' );
 
 export default async function ( params, callback ) {
 	const xhr = ( await import( /* webpackChunkName: "wpcom-xhr-request" */ 'wpcom-xhr-request' ) )
@@ -20,15 +9,7 @@ export default async function ( params, callback ) {
 
 	return xhr( params, function ( error, response, headers ) {
 		if ( error && error.name === 'InvalidTokenError' ) {
-			debug( 'Invalid token error detected, authorization probably revoked - logging out' );
-
-			const logoutUrl = getLogoutUrl( user().get() );
-			// Clear any data stored locally within the user data module or localStorage
-			user()
-				.clear()
-				.then( () => {
-					window.location.href = logoutUrl;
-				} );
+			window.location.href = getLogoutUrl();
 		}
 
 		callback( error, response, headers );
