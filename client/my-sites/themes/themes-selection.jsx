cf2dec9b8535092fcb600d6b350fa78c36942881
DanielSmithMichigan <dsmith11ql@gmail.com>
2021-06-22 12:54:25 -0400

    Theme Showcase: Add headers back in between lists of themes. (#53832)
    * Theme Showcase: Add headers back in between lists of themes. These were removed in 528ccccc36c38daa4015c92765385b12436b0568 due to not working well, but they seem to be working alright now.
    
    * Theme Showcase: Removed counts from themes headers.
    
    * Theme Showcase: Removed some redundant margin
    
    * Theme Showcase: Adjusted margin
    
    * Themes Showcase: Dont show theme selection header when logged out
    
    Co-authored-by: danielsmithmichigan <danielsmithmichigan@users.noreply.github.com>

--- client/my-sites/themes/themes-selection.jsx
+++ client/my-sites/themes/themes-selection.jsx
@@ -28,6 +28,7 @@ import {
 } from 'calypso/state/themes/selectors';
 import { setThemePreviewOptions } from 'calypso/state/themes/actions';
 import config from '@automattic/calypso-config';
+import ThemesSelectionHeader from './themes-selection-header';
 
 /**
  * Style dependencies
@@ -157,11 +158,17 @@ class ThemesSelection extends Component {
 	};
 
 	render() {
-		const { source, query, upsellUrl } = this.props;
+		const { source, query, upsellUrl, listLabel, noMarginBeforeHeader } = this.props;
 
 		return (
 			<div className="themes__selection">
 				<QueryThemes query={ query } siteId={ source } />
+				{ ! this.props.recommendedThemes && this.props.isLoggedIn && (
+					<ThemesSelectionHeader
+						label={ listLabel }
+						noMarginBeforeHeader={ noMarginBeforeHeader }
+					/>
+				) }
 				<ThemesList
 					upsellUrl={ upsellUrl }
 					themes={ this.props.recommendedThemes || this.props.themes }
