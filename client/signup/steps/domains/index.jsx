a7b8fa6eaafb25440928288a0cf91a202261105a
Stefan Nieuwenhuis <stefan.nieuwenhuis@automattic.com>
2021-06-23 13:45:22 +0200

    Revert "update title & subtitle alignment"
    This reverts commit 4dddbe77ed800bed09c34a614439950b6879f712.

--- client/signup/steps/domains/index.jsx
+++ client/signup/steps/domains/index.jsx
@@ -812,6 +812,7 @@ class DomainsStep extends React.Component {
 				goToNextStep={ this.handleSkip }
 				skipHeadingText={ translate( 'Not sure yet?' ) }
 				skipLabelText={ translate( 'Choose a domain later' ) }
+				align={ isReskinned ? 'left' : 'center' }
 				isWideLayout={ isReskinned }
 			/>
 		);
