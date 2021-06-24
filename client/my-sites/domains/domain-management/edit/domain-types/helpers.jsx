073202c504243fec3cec958d7a63beab14d6f571
Don Lair <don.lair@gmail.com>
2021-06-24 10:18:02 -0400

    Revert helpers file.

--- client/my-sites/domains/domain-management/edit/domain-types/helpers.jsx
+++ client/my-sites/domains/domain-management/edit/domain-types/helpers.jsx
@@ -3,22 +3,12 @@
  */
 import React from 'react';
 import classNames from 'classnames';
-import styled from '@emotion/styled';
 
 /**
  * Internal dependencies
  */
 import { isExpiring, shouldRenderExpiringCreditCard } from 'calypso/lib/purchases';
 import { type as domainTypes } from 'calypso/lib/domains/constants';
-
-const TextDiv = styled.div`
-	display: block;
-`;
-
-const ParentDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-`;
 
 function WrapDomainStatusButtons( props ) {
 	const wrapperClassNames = classNames( 'domain-types__wrap-me', props.className );
@@ -39,13 +29,12 @@ function DomainExpiryOrRenewal( { domain, isLoadingPurchase, moment, purchase, t
 		);
 	}
 
-	let renewalText;
-	let expirationText;
+	let text;
 
 	if ( domain.type === domainTypes.MAPPED && ! domain.expiry ) {
-		expirationText = translate( 'Expires: Never' );
+		text = translate( 'Expires: Never' );
 	} else if ( domain.expired ) {
-		expirationText = translate( 'Expired: %(expiry_date)s', {
+		text = translate( 'Expired: %(expiry_date)s', {
 			args: {
 				expiry_date: moment( domain.expiry ).format( 'LL' ),
 			},
@@ -58,44 +47,33 @@ function DomainExpiryOrRenewal( { domain, isLoadingPurchase, moment, purchase, t
 		! shouldRenderExpiringCreditCard( purchase )
 	) {
 		if ( domain.type === domainTypes.MAPPED && domain.bundledPlanSubscriptionId ) {
-			renewalText = translate( 'Renews with your plan on %(renewal_date)s', {
+			text = translate( 'Renews with your plan on %(renewal_date)s', {
 				args: {
 					renewal_date: moment( domain.autoRenewalDate ).format( 'LL' ),
 				},
 			} );
 		} else {
-			renewalText = translate( 'Renews: %(renewal_date)s', {
+			text = translate( 'Renews: %(renewal_date)s', {
 				args: {
 					renewal_date: moment( domain.autoRenewalDate ).format( 'LL' ),
 				},
 			} );
-
-			expirationText = translate( 'Expires: %(expiry_date)s', {
-				args: {
-					expiry_date: moment.utc( domain.expiry ).format( 'LL' ),
-				},
-			} );
 		}
 	} else if ( domain.type === domainTypes.MAPPED && domain.bundledPlanSubscriptionId ) {
-		expirationText = translate( 'Expires with your plan on %(expiry_date)s', {
+		text = translate( 'Expires with your plan on %(expiry_date)s', {
 			args: {
 				expiry_date: moment( domain.expiry ).format( 'LL' ),
 			},
 		} );
 	} else {
-		expirationText = translate( 'Expires: %(expiry_date)s', {
+		text = translate( 'Expires: %(expiry_date)s', {
 			args: {
 				expiry_date: moment( domain.expiry ).format( 'LL' ),
 			},
 		} );
 	}
 
-	return (
-		<ParentDiv>
-			{ renewalText && <TextDiv>{ renewalText }</TextDiv> }
-			{ expirationText && <TextDiv>{ expirationText }</TextDiv> }
-		</ParentDiv>
-	);
+	return <div>{ text }</div>;
 }
 
 export { WrapDomainStatusButtons, DomainExpiryOrRenewal };
