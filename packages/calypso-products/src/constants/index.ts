5c68a5833728f6880d4699ffb9e09ebe48a4abc2
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-06-28 13:35:45 +0300

    Woo: Remove unused feature flags, selector and tests (#53991)

--- packages/calypso-products/src/constants/index.ts
+++ packages/calypso-products/src/constants/index.ts
@@ -2,7 +2,6 @@ export * from './domain';
 export * from './features';
 export * from './google';
 export * from './jetpack';
-export * from './store';
 export * from './terms';
 export * from './titan';
 export * from './types';
