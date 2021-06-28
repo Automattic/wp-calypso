e7c97461d00b511b22519f9b8d03c0907fc3bd78
Payton Swick <payton@foolord.com>
2021-06-28 16:12:55 -0400

    Explicitly support passing no stored card to useSubmitTransaction

--- client/my-sites/checkout/upsell-nudge/purchase-modal/util.ts
+++ client/my-sites/checkout/upsell-nudge/purchase-modal/util.ts
@@ -31,7 +31,7 @@ export function useSubmitTransaction( {
 }: {
 	cart: ResponseCart;
 	siteId: string | number;
-	storedCard: StoredCard;
+	storedCard: StoredCard | undefined;
 	setStep: SetStep;
 	onClose: OnClose;
 } ): SubmitTransactionFunction {
@@ -39,6 +39,9 @@ export function useSubmitTransaction( {
 	const reduxDispatch = useDispatch();
 
 	return useCallback( () => {
+		if ( ! storedCard ) {
+			throw new Error( 'No saved card found' );
+		}
 		const wpcomCart = translateResponseCartToWPCOMCart( cart );
 		const countryCode = extractStoredCardMetaValue( storedCard, 'country_code' );
 		const postalCode = extractStoredCardMetaValue( storedCard, 'card_zip' );
