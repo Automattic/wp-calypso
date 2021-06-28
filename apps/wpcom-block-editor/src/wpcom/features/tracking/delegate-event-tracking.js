d8a9ceb72b6f95b5eda283094525758f39847c22
Addison-Stavlo <stavz01@gmail.com>
2021-06-28 14:39:32 -0400

    track replace and choose existing

--- apps/wpcom-block-editor/src/wpcom/features/tracking/delegate-event-tracking.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/delegate-event-tracking.js
@@ -15,6 +15,11 @@ import wpcomBlockDonationsPlanUpgrade from './wpcom-block-donations-plan-upgrade
 import wpcomBlockDonationsStripeConnect from './wpcom-block-donations-stripe-connect';
 import wpcomBlockPremiumContentPlanUpgrade from './wpcom-block-premium-content-plan-upgrade';
 import wpcomBlockPremiumContentStripeConnect from './wpcom-block-premium-content-stripe-connect';
+import {
+	wpcomTemplatePartReplaceCapture,
+	wpcomTemplatePartReplaceBubble,
+} from './wpcom-template-part-replace';
+import wpcomTemplatePartChooseExisting from './wpcom-template-part-choose-existing';
 
 // Debugger.
 const debug = debugFactory( 'wpcom-block-editor:tracking' );
@@ -52,6 +57,9 @@ const EVENTS_MAPPING = [
 	wpcomBlockDonationsStripeConnect(),
 	wpcomBlockPremiumContentPlanUpgrade(),
 	wpcomBlockPremiumContentStripeConnect(),
+	wpcomTemplatePartReplaceCapture(),
+	wpcomTemplatePartReplaceBubble(),
+	wpcomTemplatePartChooseExisting(),
 ];
 const EVENTS_MAPPING_CAPTURE = EVENTS_MAPPING.filter( ( { capture } ) => capture );
 const EVENTS_MAPPING_NON_CAPTURE = EVENTS_MAPPING.filter( ( { capture } ) => ! capture );
