9df3f5f500724acd603907a617a9641e664eecb1
nsakaimbo <nsakaimbo@users.noreply.github.com>
2021-06-28 11:02:32 -0600

    external-links: minor cleanup and comments

--- desktop/app/window-handlers/external-links/index.js
+++ desktop/app/window-handlers/external-links/index.js
@@ -3,12 +3,14 @@
  */
 const log = require( '../../lib/logger' )( 'desktop:external-links' );
 
-let savedURL = '';
+let targetURL = '';
 
 module.exports = function ( { view } ) {
+	// TODO: Replace the "new-window" event with webcontents.setWindowOpenHandler
+	// when Electron is updated to >= 13.x
 	view.webContents.on( 'new-window', function ( event, url ) {
-		// Check if the incoming URL is blank and if it is send to the savedURL instead
-		const urlToLoad = url.includes( 'about:blank' ) || url === '' ? savedURL : url;
+		// Check if the incoming URL is blank and if it is send to the targetURL instead
+		const urlToLoad = url.includes( 'about:blank' ) || url === '' ? targetURL : url;
 		log.info( `Navigating to URL: '${ urlToLoad }'` );
 
 		event.preventDefault();
@@ -21,6 +23,6 @@ module.exports = function ( { view } ) {
 	// This allows us to capture the url of a link when hovered and then navigate
 	// to it in the new-window event listener above if the url sent to it is blank
 	view.webContents.on( 'update-target-url', function ( event, url ) {
-		savedURL = url;
+		targetURL = url;
 	} );
 };
