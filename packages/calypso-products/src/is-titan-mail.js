2ebc0712debd5e05cb53dff24803968ff74ec64f
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-06-28 15:10:05 +0400

    Check product is defined

--- packages/calypso-products/src/is-titan-mail.js
+++ packages/calypso-products/src/is-titan-mail.js
@@ -2,6 +2,10 @@ import { TITAN_MAIL_MONTHLY_SLUG } from './constants';
 import { formatProduct } from './format-product';
 
 export function isTitanMail( product ) {
+	if ( ! product ) {
+		return false;
+	}
+
 	product = formatProduct( product );
 
 	return product.product_slug === TITAN_MAIL_MONTHLY_SLUG;
