78da3dcf37d9bd50fbb2267f657394474f41611b
sixhours <calobee@gmail.com>
2021-06-30 09:10:17 -0400

    Move all the Tracks events and props into UpsellNudge; cleans up the code and fixes the dismiss Tracks event

--- client/blocks/upwork-banner/index.jsx
+++ client/blocks/upwork-banner/index.jsx
@@ -9,12 +9,9 @@ import React, { PureComponent } from 'react';
 /**
  * Internal dependencies
  */
-import { dismissBanner } from './actions';
 import { getCurrentPlan } from 'calypso/state/sites/plans/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
-import isUpworkBannerDismissed from 'calypso/state/selectors/is-upwork-banner-dismissed';
 import QueryPreferences from 'calypso/components/data/query-preferences';
-import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import UpsellNudge from 'calypso/blocks/upsell-nudge';
 
 /**
@@ -25,56 +22,28 @@ import './style.scss';
 class UpworkBanner extends PureComponent {
 	static propTypes = {
 		currentPlan: PropTypes.object,
-		dismissBanner: PropTypes.func.isRequired,
-		isBannerVisible: PropTypes.bool.isRequired,
 		location: PropTypes.string.isRequired,
-		recordTracksEvent: PropTypes.func.isRequired,
 		siteId: PropTypes.number,
 		translate: PropTypes.func.isRequired,
 	};
 
-	componentDidMount() {
-		this.recordView();
-	}
-
-	componentDidUpdate( prevProps ) {
-		if ( prevProps.siteId && this.props.siteId && this.props.siteId !== prevProps.siteId ) {
-			this.recordView();
-		}
-	}
-
-	onStartNowClick = () => {
-		this.recordEvent( 'calypso_upwork_banner_start_now_button_click' );
-	};
-
-	recordEvent = ( eventName ) => {
-		const { currentPlan, location } = this.props;
-		const plan = currentPlan ? currentPlan.productSlug : '';
-		this.props.recordTracksEvent( eventName, { location, ...( plan && { plan } ) } );
-	};
-
-	recordView() {
-		if ( this.props.isBannerVisible ) {
-			this.recordEvent( 'calypso_upwork_banner_view' );
-		}
-	}
-
 	render() {
-		const { isBannerVisible, translate, location, currentPlan } = this.props;
-		if ( ! isBannerVisible ) {
-			return null;
-		}
+		const { translate, location, currentPlan } = this.props;
+		const plan = currentPlan?.productSlug;
 		return (
 			<>
 				<QueryPreferences />
 				<UpsellNudge
+					event={ 'calypso_upwork_banner_start_now_button_click' }
 					forceDisplay //Upwork banner has its own logic for showing/hiding
 					className="upwork-banner"
 					callToAction={ translate( 'Find your expert' ) }
-					onClick={ this.onStartNowClick }
 					dismissPreferenceName={ 'upwork-dismissible-banner' }
+					tracksClickName={ 'calypso_upwork_banner_start_now_button_click' }
+					tracksClickProperties={ { location, plan } }
+					tracksImpressionName={ 'calypso_upwork_banner_view' }
 					tracksDismissName={ 'calypso_upwork_banner_dismiss_icon_click' }
-					tracksDismissProperties={ { location: location, plan: currentPlan } }
+					tracksDismissProperties={ { location: location, plan } }
 					href="https://wordpress.com/built-by-wordpress-com/"
 					title={ translate( 'Let our WordPress.com experts build your site!' ) }
 					description={ translate(
@@ -86,19 +55,14 @@ class UpworkBanner extends PureComponent {
 	}
 }
 
-const mapStateToProps = ( state, ownProps ) => {
+const mapStateToProps = ( state ) => {
 	const siteId = getSelectedSiteId( state );
 	const currentPlan = getCurrentPlan( state, siteId );
-	const isBannerVisible = ! isUpworkBannerDismissed( state, ownProps.location );
 
 	return {
 		currentPlan,
-		isBannerVisible,
 		siteId,
 	};
 };
 
-export default connect( mapStateToProps, {
-	dismissBanner,
-	recordTracksEvent,
-} )( localize( UpworkBanner ) );
+export default connect( mapStateToProps, null )( localize( UpworkBanner ) );
