329aea40718036b7820e961e17bd97e8510c2e56
Paul Von Schrottky <paul.von.schrottky@automattic.com>
2021-06-30 13:55:21 -0400

    Hide masterbar when adding credit card via WC apps
    The WooCommerce apps show Calypso's "add credit card" pages, but have no need for the masterbar. This PR uses the browser's (or webview's) user agent to hide the masterbar when the WCiOS or WCAndroid user agent is present.

--- client/me/purchases/add-new-payment-method/index.jsx
+++ client/me/purchases/add-new-payment-method/index.jsx
@@ -26,6 +26,8 @@ import PaymentMethodSidebar from 'calypso/me/purchases/components/payment-method
 import PaymentMethodSelector from 'calypso/me/purchases/manage-purchase/payment-method-selector';
 import { useCreateCreditCard } from 'calypso/my-sites/checkout/composite-checkout/hooks/use-create-payment-methods';
 import PaymentMethodLoader from 'calypso/me/purchases/components/payment-method-loader';
+import { hideMasterbar } from 'calypso/state/ui/actions';
+import { isWcMobileApp } from 'calypso/lib/mobile-app';
 
 function AddNewPaymentMethod() {
 	const goToPaymentMethods = () => page( paymentMethods );
@@ -55,6 +57,10 @@ function AddNewPaymentMethod() {
 	}
 
 	const title = `${ titles.activeUpgrades } › ${ addPaymentMethodTitle }`;
+
+	if ( isWcMobileApp() ) {
+		reduxDispatch( hideMasterbar() );
+	}
 
 	return (
 		<Main wideLayout className="add-new-payment-method">
