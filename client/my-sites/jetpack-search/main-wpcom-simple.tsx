4193a82460cecca0554887f7d4057a26211742f4
Chris R <chris.rosser@automattic.com>
2021-07-01 16:15:48 +1200

    Try isFetchingPurchases

--- client/my-sites/jetpack-search/main-wpcom-simple.tsx
+++ client/my-sites/jetpack-search/main-wpcom-simple.tsx
@@ -7,32 +7,36 @@ import { useSelector } from 'react-redux';
 /**
  * Internal dependencies
  */
-import { getSelectedSite } from 'calypso/state/ui/selectors';
 import getSiteSetting from 'calypso/state/selectors/get-site-setting';
 import { isJetpackSearch, planHasJetpackSearch } from '@automattic/calypso-products';
 import {
 	getSitePurchases,
 	hasLoadedSitePurchasesFromServer,
+	isFetchingSitePurchases,
 } from 'calypso/state/purchases/selectors';
 import JetpackSearchPlaceholder from './placeholder';
 import JetpackSearchUpsell from './upsell';
 import { isRequestingSiteSettings, getSiteSettings } from 'calypso/state/site-settings/selectors';
 import JetpackSearchDetails from './details';
+import { getSite } from 'calypso/state/sites/selectors';
 
 interface Props {
 	siteId: number;
 }
 
 export default function JetpackSearchMainWpcomSimple( { siteId }: Props ): ReactElement {
-	const site = useSelector( getSelectedSite );
+	const site = useSelector( ( state ) => getSite( state, siteId ) );
+
 	const checkForSearchProduct = ( purchase ) => purchase.active && isJetpackSearch( purchase );
 	const sitePurchases = useSelector( ( state ) => getSitePurchases( state, siteId ) );
 	const hasSearchProduct =
-		sitePurchases.find( checkForSearchProduct ) || planHasJetpackSearch( site?.plan?.product_slug );
-	const hasLoadedSitePurchases =
-		useSelector( hasLoadedSitePurchasesFromServer ) && Array.isArray( sitePurchases );
+		!! sitePurchases.find( checkForSearchProduct ) ||
+		planHasJetpackSearch( site?.plan?.product_slug );
+	const isFetchingPurchases = useSelector( isFetchingSitePurchases );
+	const hasLoadedPurchases = useSelector( hasLoadedSitePurchasesFromServer );
+	const isRequestingPurchases =
+		isFetchingPurchases || ! hasLoadedPurchases || ! Array.isArray( sitePurchases );
 
-	// Have we loaded the necessary purchases and site settings? If not, show the placeholder.
 	const settings = useSelector( ( state ) => getSiteSettings( state, siteId ) );
 
 	const isRequestingSettings =
@@ -43,10 +47,12 @@ export default function JetpackSearchMainWpcomSimple( { siteId }: Props ): React
 		getSiteSetting( state, siteId, 'jetpack_search_enabled' )
 	);
 
-	if ( ! hasLoadedSitePurchases || isRequestingSettings ) {
+	// Have we loaded the necessary purchases and site settings? If not, show the placeholder.
+	if ( isRequestingPurchases || isRequestingSettings ) {
 		return <JetpackSearchPlaceholder siteId={ siteId } isJetpack={ false } />;
 	}
 
+	// If the user doesn't have any search products, try to sell them one.
 	if ( ! hasSearchProduct ) {
 		return <JetpackSearchUpsell />;
 	}
