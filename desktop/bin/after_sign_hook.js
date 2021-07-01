17ab10938e35a9ab3eff71470e1136cf70d2350b
Nicholas Sakaimbo <nsakaimbo@users.noreply.github.com>
2021-07-01 13:20:15 -0600

    desktop(ci): fix macOS notarization (#54193)

--- desktop/bin/after_sign_hook.js
+++ desktop/bin/after_sign_hook.js
@@ -9,7 +9,8 @@ const NOTARIZATION_PWD = process.env.WPDESKTOP_NOTARIZATION_PWD;
 const NOTARIZATION_ASC_PROVIDER = process.env.APPLE_DEVELOPER_SHORT_NAME;
 
 const circleTag = process.env.CIRCLE_TAG;
-const shouldNotarize = process.platform === 'darwin' && !! circleTag && circleTag.startsWith( 'v' );
+const shouldNotarize =
+	process.platform === 'darwin' && !! circleTag && circleTag.startsWith( 'desktop-v' );
 
 function elapsed( start ) {
 	const now = new Date();
