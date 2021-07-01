a009690405b376c639889df257b65b82ba2fc033
Aurorum <torres126@clubpenguinmountains.com>
2021-07-01 09:09:24 +0100

    My Home: Directly Link to Support Documentation

--- client/my-sites/customer-home/cards/features/help-search/index.jsx
+++ client/my-sites/customer-home/cards/features/help-search/index.jsx
@@ -14,17 +14,14 @@ import CardHeading from 'calypso/components/card-heading';
 import Gridicon from 'calypso/components/gridicon';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import getSearchQuery from 'calypso/state/inline-help/selectors/get-search-query';
-import { openSupportArticleDialog } from 'calypso/state/inline-support-article/actions';
 import HelpSearchCard from 'calypso/blocks/inline-help/inline-help-search-card';
 import HelpSearchResults from 'calypso/blocks/inline-help/inline-help-search-results';
 import getInlineHelpCurrentlySelectedResult from 'calypso/state/inline-help/selectors/get-inline-help-currently-selected-result';
 import {
-	RESULT_POST_ID,
 	RESULT_ARTICLE,
 	RESULT_LINK,
 	RESULT_TOUR,
 	RESULT_TYPE,
-	RESULT_BLOG_ID,
 } from 'calypso/blocks/inline-help/constants';
 
 /**
@@ -39,7 +36,7 @@ const amendYouTubeLink = ( link = '' ) =>
 
 const getResultLink = ( result ) => amendYouTubeLink( get( result, RESULT_LINK ) );
 
-const HelpSearch = ( { searchQuery, openDialog, track } ) => {
+const HelpSearch = ( { searchQuery, track } ) => {
 	const translate = useTranslate();
 
 	// trackResultView: Given a result, send an "_open" tracking event indicating that result is opened.
@@ -69,10 +66,7 @@ const HelpSearch = ( { searchQuery, openDialog, track } ) => {
 
 		trackResultView( result );
 
-		const resultPostId = get( result, RESULT_POST_ID );
-		const resultBlogId = get( result, RESULT_BLOG_ID );
-		const resultLink = getResultLink( result );
-		openDialog( { postId: resultPostId, actionUrl: resultLink, blogId: resultBlogId } );
+		window.open( getResultLink( result ) );
 	};
 
 	return (
@@ -115,7 +109,6 @@ const mapStateToProps = ( state ) => ( {
 } );
 
 const mapDispatchToProps = {
-	openDialog: openSupportArticleDialog,
 	track: recordTracksEvent,
 };
 
