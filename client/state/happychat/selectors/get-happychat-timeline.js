d761b686fb0f3e7f79ed82dfe2362ab7ee27361d
Matt Wondra <matt.wondra@automattic.com>
2021-07-02 09:03:07 -0500

    Happy Chat: Allow customers to receive message updates

--- client/state/happychat/selectors/get-happychat-timeline.js
+++ client/state/happychat/selectors/get-happychat-timeline.js
@@ -1,22 +1,7 @@
 /**
- * External dependencies
- */
-import { map } from 'lodash';
-
-/**
- * Internal dependencies
- */
-import { createSelector } from '@automattic/state-utils';
-
-import 'calypso/state/happychat/init';
-
-/**
  * Gets timeline chat events from the happychat state
  *
  * @param {object} state - Global redux state
  * @returns [{object}] events - an array of timeline chat events
  */
-export default createSelector(
-	( state ) => state.happychat.chat.timeline,
-	( state ) => map( state.happychat.chat.timeline, 'id' )
-);
+export default ( state ) => state.happychat.chat.timeline;
