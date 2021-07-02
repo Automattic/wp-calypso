8d34b094f361d0f7ef419fadcf98ee270768f981
Marcelo Serpa <81248+fullofcaffeine@users.noreply.github.com>
2021-07-02 12:24:38 -0500

    Activate sentry for a segment of users, and a12s if the blog has the `error-reporting-use-sentry` sticker

--- apps/editing-toolkit/editing-toolkit-plugin/error-reporting/index.js
+++ apps/editing-toolkit/editing-toolkit-plugin/error-reporting/index.js
@@ -1,45 +1,71 @@
 import apiFetch from '@wordpress/api-fetch';
 
-/**
- * Errors that happened before this script had a chance to load
- * are captured in a global array. See `./index.php`.
- */
-const headErrors = window._jsErr || [];
-const headErrorHandler = window._headJsErrorHandler;
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
 
-const reportError = ( { error } ) => {
-	// Sanitized error event objects do not include a nested error attribute. In
-	// that case, we return early to prevent a needless TypeError when defining
-	// `data`, below. Also, sanitized errors don't include any useful information,
-	// so the sensible thing to do is to completely ignore them.
-	if ( ! error ) {
-		return;
-	}
+const dataFromPHP = window.dataFromPHP;
 
-	const data = {
-		message: error.message,
-		trace: error.stack,
-		url: document.location.href,
-		feature: 'wp-admin',
+function activateSentry() {
+	Sentry.init( {
+		dsn: 'https://732ae01df1fe4974820b55d2c14028fa@o892859.ingest.sentry.io/5840079',
+		integrations: [ new Integrations.BrowserTracing() ],
+
+		// Set tracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production
+		tracesSampleRate: 1.0,
+	} );
+}
+
+// Activate the home-brew error-reporting
+function activateHomebrewErrorReporting() {
+	/**
+	 * Errors that happened before this script had a chance to load
+	 * are captured in a global array. See `./index.php`.
+	 */
+	const headErrors = window._jsErr || [];
+	const headErrorHandler = window._headJsErrorHandler;
+
+	const reportError = ( { error } ) => {
+		// Sanitized error event objects do not include a nested error attribute. In
+		// that case, we return early to prevent a needless TypeError when defining
+		// `data`, below. Also, sanitized errors don't include any useful information,
+		// so the sensible thing to do is to completely ignore them.
+		if ( ! error ) {
+			return;
+		}
+
+		const data = {
+			message: error.message,
+			trace: error.stack,
+			url: document.location.href,
+			feature: 'wp-admin',
+		};
+
+		return (
+			apiFetch( {
+				global: true,
+				path: '/rest/v1.1/js-error',
+				method: 'POST',
+				data: { error: JSON.stringify( data ) },
+			} )
+				// eslint-disable-next-line no-console
+				.catch( () => console.error( 'Error: Unable to record the error in Logstash.' ) )
+		);
 	};
 
-	return (
-		apiFetch( {
-			global: true,
-			path: '/rest/v1.1/js-error',
-			method: 'POST',
-			data: { error: JSON.stringify( data ) },
-		} )
-			// eslint-disable-next-line no-console
-			.catch( () => console.error( 'Error: Unable to record the error in Logstash.' ) )
-	);
-};
+	window.addEventListener( 'error', reportError );
 
-window.addEventListener( 'error', reportError );
+	// Remove the head handler as it's not needed anymore after we set the main one above
+	window.removeEventListener( 'error', headErrorHandler );
+	delete window._headJsErrorHandler;
 
-// Remove the head handler as it's not needed anymore after we set the main one above
-window.removeEventListener( 'error', headErrorHandler );
-delete window._headJsErrorHandler;
+	// We still need to report the head errors, if any.
+	Promise.allSettled( headErrors.map( reportError ) ).then( () => delete window._jsErr );
+}
 
-// We still need to report the head errors, if any.
-Promise.allSettled( headErrors.map( reportError ) ).then( () => delete window._jsErr );
+if ( dataFromPHP.activateSentry ) {
+	activateSentry();
+} else {
+	activateHomebrewErrorReporting();
+}
--- apps/editing-toolkit/editing-toolkit-plugin/error-reporting/index.php
+++ apps/editing-toolkit/editing-toolkit-plugin/error-reporting/index.php
@@ -41,31 +41,38 @@ function add_crossorigin_to_script_els( $tag ) {
 }
 
 /**
- * Enqueue assets
+ * Temporary function to feature flag Sentry by segment. We'll be testing
+ * it on production (simple sites) for a while to see if it's feasible to
+ * activate it for all sites and perhaps get rid of our custom solution.
+ * If it works well, we'll activate for all simple sites and look into.
+ * activating it for WoA, too.
+ *
+ * @param int $user_id the user id.
+ * @return bool
  */
-function enqueue_script() {
-	$asset_file          = include plugin_dir_path( __FILE__ ) . 'dist/error-reporting.asset.php';
-	$script_dependencies = isset( $asset_file['dependencies'] ) ? $asset_file['dependencies'] : array();
-	$script_version      = isset( $asset_file['version'] ) ? $asset_file['version'] : filemtime( plugin_dir_path( __FILE__ ) . 'dist/error-reporting.js' );
+function user_in_sentry_test_segment( $user_id ) {
+	$current_segment = 10; // segment of existing users that will get this feature in %.
+	$user_segment    = $user_id % 100;
 
-	wp_enqueue_script(
-		'a8c-fse-error-reporting-script',
-		plugins_url( 'dist/error-reporting.js', __FILE__ ),
-		$script_dependencies,
-		$script_version,
-		true
-	);
+	// We get the last two digits of the user id and that will be used to decide in what
+	// segment the user is. i.e if current_segment is 10, then only ids that end in < 10
+	// will be considered part of the segment.
+	return $user_segment < $current_segment;
 }
 
 /**
- * Effectivelly activates the error reporting module by setting the necessary hooks.
+ * Returns whether or not the user is an automattician, but first verifies
+ * if the function `is_automattician` exists in the current context. If not,
+ * then we consider the user to not be an automattician. This guard is needed
+ * because this function is not present in some envs, namely the testing env.
+ *
+ * @todo Remove once roll-out is complete
+ *
+ * @param int $user_id the user id.
+ * @return bool
  */
-function activate_error_reporting() {
-	add_action( 'admin_print_scripts', __NAMESPACE__ . '\head_error_handler' );
-	add_filter( 'script_loader_tag', __NAMESPACE__ . '\add_crossorigin_to_script_els', 99, 2 );
-	// We load as last as possible for perf reasons. The head handler will
-	// capture errors until the main handler is loaded.
-	add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\enqueue_script', 99 );
+function user_is_automattician( $user_id ) {
+	return function_exists( 'is_automattician' ) && is_automattician( $user_id );
 }
 
 /**
@@ -77,8 +84,49 @@ function is_atomic() {
 	return defined( 'IS_ATOMIC' ) && IS_ATOMIC;
 }
 
+/**
+ * Enqueue assets
+ */
+function enqueue_script() {
+	$asset_file          = include plugin_dir_path( __FILE__ ) . 'dist/error-reporting.asset.php';
+	$script_dependencies = isset( $asset_file['dependencies'] ) ? $asset_file['dependencies'] : array();
+	$script_version      = isset( $asset_file['version'] ) ? $asset_file['version'] : filemtime( plugin_dir_path( __FILE__ ) . 'dist/error-reporting.js' );
+	$script_id           = 'a8c-fse-error-reporting-script';
+	$user_id             = get_current_user_id();
+
+	wp_enqueue_script(
+		$script_id,
+		plugins_url( 'dist/error-reporting.js', __FILE__ ),
+		$script_dependencies,
+		$script_version,
+		true
+	);
+
+	wp_localize_script(
+		$script_id,
+		'dataFromPHP',
+		array(
+			'activateSentry' => ( user_is_automattician( $user_id ) && has_blog_sticker( 'error-reporting-use-sentry' ) ) || user_in_sentry_test_segment( $user_id ),
+		)
+	);
+}
+
+/**
+ * Setup the error reporting module by setting the necessary hooks. Whether or not we get the
+ * homebrew version or Sentry is decided inside the `enqueue_script` function, stored in a bool
+ * var, and passed over to the clientside script (index.js) which will then use this value to
+ * decide what tool to activate.
+ */
+function setup_error_reporting() {
+	add_action( 'admin_print_scripts', __NAMESPACE__ . '\head_error_handler' );
+	add_filter( 'script_loader_tag', __NAMESPACE__ . '\add_crossorigin_to_script_els', 99, 2 );
+	// We load as last as possible for perf reasons. The head handler will
+	// capture errors until the main handler is loaded.
+	add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\enqueue_script', 99 );
+}
+
 // We don't want to activate this module in AT just yet. See https://wp.me/p4TIVU-9DI#comment-10922.
 // @todo Remove once we have a version that works for WPCOM simple sites and WoA.
 if ( ! is_atomic() ) {
-	activate_error_reporting();
+	setup_error_reporting();
 }
