43aed0006be16d70722b411a5e30f6e0ff3a600b
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-07-02 19:14:37 +0400

    Undo change to isTitanMail() since we're already checking for null value one level up

--- packages/calypso-products/src/is-titan-mail.js
+++ packages/calypso-products/src/is-titan-mail.js
@@ -5,10 +5,6 @@ import { formatProduct } from './format-product';
 import { TITAN_MAIL_MONTHLY_SLUG } from './constants';
 
 export function isTitanMail( product ) {
-	if ( ! product ) {
-		return false;
-	}
-
 	product = formatProduct( product );
 
 	return product.product_slug === TITAN_MAIL_MONTHLY_SLUG;
