a96d609cdba9876db383a41de6a465e88f68f6ab
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-07-03 11:39:04 +0400

    Redirect /me/concierge to /me/quickstart

--- client/me/concierge/index.js
+++ client/me/concierge/index.js
@@ -12,26 +12,29 @@ import { makeLayout, render as clientRender } from 'calypso/controller';
 import { siteSelection, sites } from 'calypso/my-sites/controller';
 
 const redirectToBooking = ( context ) => {
-	page.redirect( `/me/concierge/${ context.params.siteSlug }/book` );
+	page.redirect( `/me/quickstart/${ context.params.siteSlug }/book` );
 };
 
 export default () => {
-	page( '/me/concierge', controller.siteSelector, siteSelection, sites, makeLayout, clientRender );
+	page( '/me/concierge', controller.redirectToQuickStart, makeLayout, clientRender );
+	page( '/me/concierge/:siteSlug', redirectToBooking );
+	page( '/me/concierge/:siteSlug?/*', controller.redirectToQuickStart, makeLayout, clientRender );
+	page( '/me/quickstart', controller.siteSelector, siteSelection, sites, makeLayout, clientRender );
 
 	// redirect to booking page after site selection
-	page( '/me/concierge/:siteSlug', redirectToBooking );
+	page( '/me/quickstart/:siteSlug', redirectToBooking );
 
-	page( '/me/concierge/:siteSlug/book', controller.book, makeLayout, clientRender );
+	page( '/me/quickstart/:siteSlug/book', controller.book, makeLayout, clientRender );
 
 	page(
-		'/me/concierge/:siteSlug/:appointmentId/cancel',
+		'/me/quickstart/:siteSlug/:appointmentId/cancel',
 		controller.cancel,
 		makeLayout,
 		clientRender
 	);
 
 	page(
-		'/me/concierge/:siteSlug/:appointmentId/reschedule',
+		'/me/quickstart/:siteSlug/:appointmentId/reschedule',
 		controller.reschedule,
 		makeLayout,
 		clientRender
