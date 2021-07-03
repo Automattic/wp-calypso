95a69ff28cfcd61d0bd6e7d7e7891626a5181ba0
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-07-03 11:41:55 +0400

    Undo stray commit

--- packages/calypso-products/src/is-titan-mail.js
+++ packages/calypso-products/src/is-titan-mail.js
@@ -2,10 +2,6 @@ import { TITAN_MAIL_MONTHLY_SLUG } from './constants';
 import { formatProduct } from './format-product';
 
 export function isTitanMail( product ) {
-	if ( ! product ) {
-		return false;
-	}
-
 	product = formatProduct( product );
 
 	return product.product_slug === TITAN_MAIL_MONTHLY_SLUG;
