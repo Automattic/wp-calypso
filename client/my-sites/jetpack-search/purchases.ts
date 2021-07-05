480eaf8ff16a191887a82c8da26006e19cc6b0e9
Chris R <chris.rosser@automattic.com>
2021-07-06 11:46:38 +1200

    sitePurchases is an array

--- client/my-sites/jetpack-search/purchases.ts
+++ client/my-sites/jetpack-search/purchases.ts
@@ -4,7 +4,7 @@
 import { isJetpackSearch, planHasJetpackSearch } from '@automattic/calypso-products';
 
 export const hasJetpackSearchPurchaseOrPlan = (
-	sitePurchases: Record< string, unknown > | null,
+	sitePurchases: Record< string, unknown >[] | null,
 	planSlug: string | null
 ): boolean => {
 	if ( ! sitePurchases || ! planSlug ) {
