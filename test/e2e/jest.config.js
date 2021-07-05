81d103d3c67fe0c591b44fde477b9c2db0b2efd2
Sergio Cinos <sergio.cinos@gmail.com>
2021-07-05 07:11:53 +0200

    USe hyphen for file names

--- test/e2e/jest.config.js
+++ test/e2e/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	cacheDirectory: '<rootDir>/../../.cache/jest',
 	testMatch: [ '<rootDir>/specs/**/*.js' ],
-	globalSetup: '<rootDir>/lib/jest/jest.global-setup.js',
-	setupFilesAfterEnv: [ '<rootDir>/lib/jest/jest.setup.js', '<rootDir>/lib/hooks/jest.js' ],
+	globalSetup: '<rootDir>/lib/jest/jest-global-setup.js',
+	setupFilesAfterEnv: [ '<rootDir>/lib/jest/jest-setup.js', '<rootDir>/lib/hooks/jest.js' ],
 	verbose: true,
 	transform: {
 		'\\.[jt]sx?$': [ 'babel-jest', { configFile: '../../babel.config.js' } ],
