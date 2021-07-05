3d61c18e1cbf09e995a6eb1e91fe14ce80060f87
Sergio Cinos <sergio.cinos@gmail.com>
2021-07-05 12:11:42 +0200

    Get test name from expect state

--- test/e2e/lib/hooks/jest.js
+++ test/e2e/lib/hooks/jest.js
@@ -1,10 +1,10 @@
-/* eslint-disable mocha/no-top-level-hooks */
 /**
  * External dependencies
  */
 import config from 'config';
 import path from 'path';
 import { mkdtemp } from 'fs/promises';
+import { getState } from 'expect';
 
 /**
  * Internal dependencies
@@ -12,53 +12,42 @@ import { mkdtemp } from 'fs/promises';
 import { buildHooks as buildVideoHooks, isVideoEnabled } from './video';
 import { buildHooks as buildBrowserHooks } from './browser';
 
-const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
-
 let tempDir;
-beforeAll( async () => {
-	tempDir =
-		process.env.TEMP_ASSET_PATH || ( await mkdtemp( path.resolve( __dirname, '../../test-' ) ) );
-} );
-
-if ( isVideoEnabled() ) {
-	const {
-		startFramebuffer,
-		stopFramebuffer,
-		takeScreenshot,
-		startVideoRecording,
-		saveVideoRecording,
-		stopVideoRecording,
-	} = buildVideoHooks();
-
-	beforeAll( async () => {
-		await startFramebuffer();
-		await startVideoRecording( { tempDir } );
-	} );
-
-	afterEach( async () => {
-		if ( global.__CURRENT_TEST_FAILED__ ) {
-			await takeScreenshot( {
-				tempDir,
-				testName: global.__CURRENT_TEST_NAME__,
-				driver: global.__BROWSER__,
-			} );
-			await saveVideoRecording( { tempDir, testName: global.__CURRENT_TEST_NAME__ } );
-		}
-	} );
-
-	afterAll( async () => {
-		await stopFramebuffer();
-		await stopVideoRecording();
-	} );
-}
-
+let driver;
+const startBrowserTimeoutMS = config.get( 'startBrowserTimeoutMS' );
 const browserHooks = buildBrowserHooks();
+const videoHooks = isVideoEnabled() ? buildVideoHooks() : null;
+
 beforeAll( async () => {
-	const driver = await browserHooks.createBrowser( { tempDir } );
+	const { testPath } = getState();
+	tempDir = await mkdtemp(
+		path.join( __dirname, 'results', path.basename( testPath, path.extname( testPath ) ) )
+	);
+
+	if ( isVideoEnabled() ) {
+		await videoHooks.startFramebuffer();
+		await videoHooks.startVideoRecording( { tempDir } );
+	}
+
+	driver = await browserHooks.createBrowser( { tempDir } );
 	global.__BROWSER__ = driver;
 }, startBrowserTimeoutMS );
 
 afterAll( async () => {
+	if ( isVideoEnabled() ) {
+		if ( global.__CURRENT_TEST_FAILED__ ) {
+			await videoHooks.takeScreenshot( {
+				tempDir,
+				testName: global.__CURRENT_TEST_NAME__,
+				driver: driver,
+			} );
+			await videoHooks.saveVideoRecording( { tempDir, testName: global.__CURRENT_TEST_NAME__ } );
+		}
+
+		await videoHooks.stopFramebuffer();
+		await videoHooks.stopVideoRecording();
+	}
+
 	await browserHooks.saveBrowserLogs( { tempDir } );
 	await browserHooks.closeBrowser();
 } );
