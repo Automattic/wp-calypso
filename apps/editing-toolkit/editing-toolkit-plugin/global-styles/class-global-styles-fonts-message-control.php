588df3b2a36077e60ba45e326de872b8996fd148
danielsmithmichigan <danielsmithmichigan@users.noreply.github.com>
2021-07-06 12:28:09 -0400

    Global-Styles/Customizer: Handle situations where there is no homepage returned by page_on_front option

--- apps/editing-toolkit/editing-toolkit-plugin/global-styles/class-global-styles-fonts-message-control.php
+++ apps/editing-toolkit/editing-toolkit-plugin/global-styles/class-global-styles-fonts-message-control.php
@@ -23,7 +23,7 @@ class Global_Styles_Fonts_Message_Control extends \WP_Customize_Control {
 
 		// Is there a better way to direct calypso users back to calypso.localhost:3000 and production users to wordpress.com?
 		// I'd prefer not to ignore the warning. But the nonce isn't needed here since the link only opens a page.
-        // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		$base_url = \esc_url_raw( $_GET['calypsoOrigin'] ) . '/';
 
 		$site_slug = \WPCOM_Masterbar::get_calypso_site_slug( \get_current_blog_id() ) . '/';
@@ -43,19 +43,32 @@ class Global_Styles_Fonts_Message_Control extends \WP_Customize_Control {
 	 * Render the customizer help document content
 	 */
 	public function render_content() {
-		$homepage_link  = $this->get_link_to_editor_with_global_styles_sidebar();
-		$content_string = sprintf(
-			// Translators: %1$s is a link which opens the block editor, and then opens the global styles sidebar.
-			__(
-				' <p>You can change your fonts using Global Styles, which can be found in the Block Editor.</p>
-                <p><a href="%1$s" target="_blank">Click here to open the Block Editor and change your fonts.</a></p>
-                <p><a href="https://wordpress.com/support/custom-fonts/#changing-fonts-with-global-styles" target="_blank">Learn more about changing fonts using Global Styles.</a></p> ',
-				'full-site-editing'
-			),
-			\esc_url( $homepage_link )
-		);
+		$high_level_explanation = __( 'You can change your fonts using Global Styles, which can be found in the Block Editor.', 'full-site-editing' );
 		echo wp_kses(
-			$content_string,
+			'<p>' . $high_level_explanation . '</p>',
+			array( 'p' => array() )
+		);
+		$editable_page_id = $this->get_link_to_editor_with_global_styles_sidebar();
+		if ( null !== $editable_page_id ) {
+			$open_block_editor_link = sprintf(
+				// Translators: %1$s is a link which opens the block editor, and then opens the global styles sidebar.
+				__( '<a href="%1$s" target="_blank">Click here to open the Block Editor and change your fonts.</a>', 'full-site-editing' ),
+				\esc_url( $editable_page_id )
+			);
+			echo wp_kses(
+				'<p>' . $open_block_editor_link . '</p>',
+				array(
+					'p' => array(),
+					'a' => array(
+						'href'   => array(),
+						'target' => array(),
+					),
+				)
+			);
+		}
+		$learn_more_link = __( '<a href="https://wordpress.com/support/custom-fonts/#changing-fonts-with-global-styles" target="_blank">Learn more about changing fonts using Global Styles.</a>', 'full-site-editing' );
+		echo wp_kses(
+			'<p>' . $learn_more_link . '</p>',
 			array(
 				'p' => array(),
 				'a' => array(
