0213ec51368b8553a60efe48f89bf061d5cec043
Matthew Reishus <mreishus@users.noreply.github.com>
2021-07-06 14:43:05 -0500

    Chai -> Jest

--- client/lib/trigram/test/index.js
+++ client/lib/trigram/test/index.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { expect } from 'chai';
-
-/**
  * Internal dependencies
  */
 import { cosine_similarity } from '../';
@@ -13,18 +8,21 @@ describe( 'trigram', () => {
 		const str1 = 'This is a string.';
 		const str2 = 'This is a string.';
 		const sim = cosine_similarity( str1, str2 );
-		expect( sim ).to.be.within( 0.99, 1.01 );
+		expect( sim ).toBeGreaterThan( 0.99 );
+		expect( sim ).toBeLessThan( 1.01 );
 	} );
 	test( 'Completely different strings should have cosine_similarity of ~0', () => {
 		const str1 = 'abcabcabc';
 		const str2 = 'xyzxyzxyz';
 		const sim = cosine_similarity( str1, str2 );
-		expect( sim ).to.be.within( -0.01, 0.01 );
+		expect( sim ).toBeGreaterThan( -0.01 );
+		expect( sim ).toBeLessThan( 0.01 );
 	} );
 	test( 'Somewhat overlapping strings should have a cosine_similarity between 0 and 1', () => {
 		const str1 = 'There';
 		const str2 = 'Their';
 		const sim = cosine_similarity( str1, str2 );
-		expect( sim ).to.be.within( 0.39, 0.41 );
+		expect( sim ).toBeGreaterThan( 0.39 );
+		expect( sim ).toBeLessThan( 0.41 );
 	} );
 } );
