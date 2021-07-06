db5c2c19a3471b09c6c278b19d22bef925ebbd2e
Sergio Cinos <sergio.cinos@gmail.com>
2021-07-06 06:47:52 +0200

    USe jest 26 for ETK

--- apps/editing-toolkit/bin/babel-transform.js
+++ apps/editing-toolkit/bin/babel-transform.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-const babelJest = require( 'babel-jest' ).default;
+const babelJest = require( 'babel-jest' );
 
 module.exports = babelJest.createTransformer( {
 	presets: [ '@wordpress/babel-preset-default', '@babel/preset-typescript' ],
