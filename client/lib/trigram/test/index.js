0ab436f656d23a5a1d3ee9d0918abf1743ed8356
Matthew Reishus <mreishus@users.noreply.github.com>
2021-07-06 14:35:29 -0500

    lib/trigram: Add unit tests

---
+++ client/lib/trigram/test/index.js
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ */
+import { expect } from 'chai';
+
+/**
+ * Internal dependencies
+ */
+import { cosine_similarity } from '../';
+
+describe( 'trigram', () => {
+	test( 'Equal Strings should have cosine_similarity of ~1', () => {
+		const str1 = 'This is a string.';
+		const str2 = 'This is a string.';
+		const sim = cosine_similarity( str1, str2 );
+		expect( sim ).to.be.within( 0.99, 1.01 );
+	} );
+	test( 'Completely different strings should have cosine_similarity of ~0', () => {
+		const str1 = 'abcabcabc';
+		const str2 = 'xyzxyzxyz';
+		const sim = cosine_similarity( str1, str2 );
+		expect( sim ).to.be.within( -0.01, 0.01 );
+	} );
+	test( 'Somewhat overlapping strings should have a cosine_similarity between 0 and 1', () => {
+		const str1 = 'There';
+		const str2 = 'Their';
+		const sim = cosine_similarity( str1, str2 );
+		expect( sim ).to.be.within( 0.39, 0.41 );
+	} );
+} );
