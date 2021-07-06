687b77dc87e6b52c70870ff742d26db7e853f243
Sergio Cinos <sergio.cinos@gmail.com>
2021-07-06 06:23:18 +0200

    Fix babel transformer

--- apps/editing-toolkit/bin/babel-transform.js
+++ apps/editing-toolkit/bin/babel-transform.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-const babelJest = require( 'babel-jest' );
+const babelJest = require( 'babel-jest' ).default;
 
 module.exports = babelJest.createTransformer( {
 	presets: [ '@wordpress/babel-preset-default', '@babel/preset-typescript' ],
