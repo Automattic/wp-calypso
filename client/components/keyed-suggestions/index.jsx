d599123d4e42b9c911944d95c39cdc3546aa4e94
Matthew Reishus <mreishus@users.noreply.github.com>
2021-07-06 15:52:42 -0500

    Extract SEARCH_THRESHOLD

--- client/components/keyed-suggestions/index.jsx
+++ client/components/keyed-suggestions/index.jsx
@@ -25,6 +25,7 @@ import { cosineSimilarity } from 'calypso/lib/trigram';
  */
 import './style.scss';
 
+const SEARCH_THRESHOLD = 0.45;
 const noop = () => {};
 
 function SuggestionsButtonAll( props ) {
@@ -256,7 +257,7 @@ class KeyedSuggestions extends React.Component {
 				// Order is important!
 				// Arg 1 can be multiple words. "flexible header" or "accepts header images of any size"
 				// Arg 2 will only be one word; even if the user types multiple words we search on each one individually.
-				const matcher = ( term1, term2_single, threshold = 0.45 ) => {
+				const matcher = ( term1, term2_single, threshold = SEARCH_THRESHOLD ) => {
 					let max_seen = 0;
 					for ( const term1_single of term1.split( /\s+/ ) ) {
 						const sim = cosineSimilarity( term1_single, term2_single );
