61ab5dccb70f476f705d6a521b742bba5529c66a
Matthew Reishus <mreishus@users.noreply.github.com>
2021-07-06 13:22:43 -0500

    Choosing tabfilter=recommended removes tier filters

--- client/my-sites/themes/themes-magic-search-card/index.jsx
+++ client/my-sites/themes/themes-magic-search-card/index.jsx
@@ -353,6 +353,7 @@ class ThemesMagicSearchCard extends React.Component {
 						) }
 						{ isPremiumThemesEnabled && showTierThemesControl && (
 							<SimplifiedSegmentedControl
+								key={ this.props.tier }
 								initialSelected={ this.props.tier }
 								options={ tiers }
 								onSelect={ this.props.select }
