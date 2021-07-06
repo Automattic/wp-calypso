0b7bab439b6c9f01abc31e9c8024a8c6461bbcdb
Rosalind W <blackjacksoftwaredev@gmail.com>
2021-07-06 14:20:57 -0500

    Wrapping new bar in feature flag

--- client/my-sites/themes/current-theme/index.jsx
+++ client/my-sites/themes/current-theme/index.jsx
@@ -21,6 +21,7 @@ import QueryActiveTheme from 'calypso/components/data/query-active-theme';
 import QueryCanonicalTheme from 'calypso/components/data/query-canonical-theme';
 import ExternalLink from 'calypso/components/external-link';
 import { localizeUrl } from 'calypso/lib/i18n-utils';
+import config from '@automattic/calypso-config';
 
 /**
  * Style dependencies
@@ -66,56 +67,59 @@ class CurrentTheme extends Component {
 			<Card className="current-theme">
 				<QueryActiveTheme siteId={ siteId } />
 				{ currentThemeId && <QueryCanonicalTheme themeId={ currentThemeId } siteId={ siteId } /> }
-				<div className="current-theme__current">
-					<div className="current-theme__details">
-						{ showScreenshotPlaceholder && <div className="current-theme__img-placeholder" /> }
-						{ showScreenshot && (
-							<img
-								src={ currentTheme.screenshot + '?w=150' }
-								className="current-theme__img"
-								alt=""
-							/>
-						) }
-						<div className="current-theme__description">
-							<div className="current-theme__title-wrapper">
-								<span className="current-theme__label">
-									{ currentTheme && currentTheme.name && translate( 'Current Theme' ) }
-								</span>
-								<span className="current-theme__name">{ text }</span>
+
+				{ config.isEnabled( 'theme/showcase-revamp' ) && (
+					<div className="current-theme__current">
+						<div className="current-theme__details">
+							{ showScreenshotPlaceholder && <div className="current-theme__img-placeholder" /> }
+							{ showScreenshot && (
+								<img
+									src={ currentTheme.screenshot + '?w=150' }
+									className="current-theme__img"
+									alt=""
+								/>
+							) }
+							<div className="current-theme__description">
+								<div className="current-theme__title-wrapper">
+									<span className="current-theme__label">
+										{ currentTheme && currentTheme.name && translate( 'Current Theme' ) }
+									</span>
+									<span className="current-theme__name">{ text }</span>
+								</div>
+								<p>
+									{ translate( 'This is the active theme on your site.' ) }{ ' ' }
+									<ExternalLink
+										href={ localizeUrl( 'https://wordpress.com/support/changing-themes/' ) }
+										icon
+										target="__blank"
+									>
+										{ translate( 'Learn more.' ) }
+									</ExternalLink>
+								</p>
 							</div>
-							<p>
-								{ translate( 'This is the active theme on your site.' ) }{ ' ' }
-								<ExternalLink
-									href={ localizeUrl( 'https://wordpress.com/support/changing-themes/' ) }
-									icon
-									target="__blank"
+						</div>
+						<div className={ classNames( 'current-theme__actions' ) }>
+							{ map( options, ( option, name ) => (
+								<Button
+									className={ classNames(
+										'current-theme__button',
+										'components-button',
+										'current-theme__' + this.props.name
+									) }
+									primary={ option.label.toLowerCase() === 'customize' }
+									name={ name }
+									key={ name }
+									label={ option.label }
+									href={ currentThemeId && option.getUrl( currentThemeId ) }
+									onClick={ this.trackClick }
 								>
-									{ translate( 'Learn more.' ) }
-								</ExternalLink>
-							</p>
+									{ option.icon && <Gridicon icon={ option.icon } size={ 18 } /> }
+									{ option.label }
+								</Button>
+							) ) }
 						</div>
 					</div>
-					<div className={ classNames( 'current-theme__actions' ) }>
-						{ map( options, ( option, name ) => (
-							<Button
-								className={ classNames(
-									'current-theme__button',
-									'components-button',
-									'current-theme__' + this.props.name
-								) }
-								primary={ option.label.toLowerCase() === 'customize' }
-								name={ name }
-								key={ name }
-								label={ option.label }
-								href={ currentThemeId && option.getUrl( currentThemeId ) }
-								onClick={ this.trackClick }
-							>
-								{ option.icon && <Gridicon icon={ option.icon } size={ 18 } /> }
-								{ option.label }
-							</Button>
-						) ) }
-					</div>
-				</div>
+				) }
 			</Card>
 		);
 	}
