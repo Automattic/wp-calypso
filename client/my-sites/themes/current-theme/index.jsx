0cc263fb1e30ac91f2c320337a029d0dcde7684c
Rosalind W <blackjacksoftwaredev@gmail.com>
2021-07-07 11:31:40 -0500

    Converting current theme bar display to new, more clear design (#54142)
    * Converting current theme bar display to new, more clear design
    
    * Updating button styles and spacing
    
    * Styling for mobile views
    
    * Tweaks to label placement on mobile
    
    * Breakpoints look better wider
    
    * Middle sized screens looked really bad; this only moves the buttons to give more horizontal space at an earlier breakpoint
    
    * Set max height back
    
    * Style fixes
    
    * Wrapping new bar in feature flag
    
    * Re-adding pre-revamp code
    
    * Pre- and post-revamp styles
    
    * Fixing button styles
    
    Co-authored-by: sixhours <calobee@gmail.com>

--- client/my-sites/themes/current-theme/index.jsx
+++ client/my-sites/themes/current-theme/index.jsx
@@ -12,13 +12,17 @@ import { localize } from 'i18n-calypso';
 /**
  * Internal dependencies
  */
-import { Card } from '@automattic/components';
+import { Card, Button } from '@automattic/components';
+import Gridicon from 'calypso/components/gridicon';
 import CurrentThemeButton from './button';
 import { connectOptions } from '../theme-options';
 import { trackClick } from '../helpers';
 import { getActiveTheme, getCanonicalTheme } from 'calypso/state/themes/selectors';
 import QueryActiveTheme from 'calypso/components/data/query-active-theme';
 import QueryCanonicalTheme from 'calypso/components/data/query-canonical-theme';
+import ExternalLink from 'calypso/components/external-link';
+import { localizeUrl } from 'calypso/lib/i18n-utils';
+import config from '@automattic/calypso-config';
 
 /**
  * Style dependencies
@@ -64,30 +68,95 @@ class CurrentTheme extends Component {
 			<Card className="current-theme">
 				<QueryActiveTheme siteId={ siteId } />
 				{ currentThemeId && <QueryCanonicalTheme themeId={ currentThemeId } siteId={ siteId } /> }
-				<div className="current-theme__current">
-					{ showScreenshotPlaceholder && <div className="current-theme__img-placeholder" /> }
-					{ showScreenshot && (
-						<img src={ currentTheme.screenshot + '?w=150' } className="current-theme__img" alt="" />
-					) }
-					<span className="current-theme__label">{ translate( 'Current Theme' ) }</span>
-					<span className="current-theme__name">{ text }</span>
-				</div>
-				<div
-					className={ classNames( 'current-theme__actions', {
-						'two-buttons': Object.keys( options ).length === 2,
-					} ) }
-				>
-					{ map( options, ( option, name ) => (
-						<CurrentThemeButton
-							name={ name }
-							key={ name }
-							label={ option.label }
-							icon={ option.icon }
-							href={ currentThemeId && option.getUrl( currentThemeId ) }
-							onClick={ this.trackClick }
-						/>
-					) ) }
-				</div>
+				{
+					// @TODO Remove this code as well as associated styles after feature flag deploy
+					! config.isEnabled( 'theme/showcase-revamp' ) && (
+						<div className="current-theme__pre-revamp">
+							<div className="current-theme__current">
+								{ showScreenshotPlaceholder && <div className="current-theme__img-placeholder" /> }
+								{ showScreenshot && (
+									<img
+										src={ currentTheme.screenshot + '?w=150' }
+										className="current-theme__img"
+										alt=""
+									/>
+								) }
+								<span className="current-theme__label">{ translate( 'Current Theme' ) }</span>
+								<span className="current-theme__name">{ text }</span>
+							</div>
+							<div
+								className={ classNames( 'current-theme__actions', {
+									'two-buttons': Object.keys( options ).length === 2,
+								} ) }
+							>
+								{ map( options, ( option, name ) => (
+									<CurrentThemeButton
+										name={ name }
+										key={ name }
+										label={ option.label }
+										icon={ option.icon }
+										href={ currentThemeId && option.getUrl( currentThemeId ) }
+										onClick={ this.trackClick }
+									/>
+								) ) }
+							</div>
+						</div>
+					)
+				}
+				{ config.isEnabled( 'theme/showcase-revamp' ) && (
+					<div className="current-theme__post-revamp">
+						<div className="current-theme__current">
+							<div className="current-theme__details">
+								{ showScreenshotPlaceholder && <div className="current-theme__img-placeholder" /> }
+								{ showScreenshot && (
+									<img
+										src={ currentTheme.screenshot + '?w=150' }
+										className="current-theme__img"
+										alt=""
+									/>
+								) }
+								<div className="current-theme__description">
+									<div className="current-theme__title-wrapper">
+										<span className="current-theme__label">
+											{ currentTheme && currentTheme.name && translate( 'Current Theme' ) }
+										</span>
+										<span className="current-theme__name">{ text }</span>
+									</div>
+									<p>
+										{ translate( 'This is the active theme on your site.' ) }{ ' ' }
+										<ExternalLink
+											href={ localizeUrl( 'https://wordpress.com/support/changing-themes/' ) }
+											icon
+											target="__blank"
+										>
+											{ translate( 'Learn more.' ) }
+										</ExternalLink>
+									</p>
+								</div>
+							</div>
+							<div className={ classNames( 'current-theme__actions' ) }>
+								{ map( options, ( option, name ) => (
+									<Button
+										className={ classNames(
+											'current-theme__button',
+											'components-button',
+											'current-theme__' + this.props.name
+										) }
+										primary={ option.label.toLowerCase() === 'customize' }
+										name={ name }
+										key={ name }
+										label={ option.label }
+										href={ currentThemeId && option.getUrl( currentThemeId ) }
+										onClick={ this.trackClick }
+									>
+										{ option.icon && <Gridicon icon={ option.icon } size={ 18 } /> }
+										{ option.label }
+									</Button>
+								) ) }
+							</div>
+						</div>
+					</div>
+				) }
 			</Card>
 		);
 	}
