5fbf7de33ef7265202da0211da624bb66a43a006
Dusty Reagan <dusty@dustyreagan.com>
2021-07-08 10:06:59 -0500

    Hide the masterbar during the checkout process from simple to Atomic site. (#54225)
    * Hide the masterBar during checkout process from simple to Atomic site.
    
    * Hide the masterBar during checkout process from simple to Atomic site.
    
    * Masterbar is hidden during simple to Atomic loading screen and reappears on final 'thank you' modal.
    
    Co-authored-by: Dusty Reagan <dustyreagan@MacBook-Pro.localdomain>

--- client/my-sites/checkout/checkout-thank-you/atomic-store-thank-you-card.jsx
+++ client/my-sites/checkout/checkout-thank-you/atomic-store-thank-you-card.jsx
@@ -22,6 +22,7 @@ import {
 import { errorNotice, removeNotice } from 'calypso/state/notices/actions';
 import { fetchCurrentUser } from 'calypso/state/current-user/actions';
 import wpcom from 'calypso/lib/wp';
+import { showMasterbar } from 'calypso/state/ui/masterbar-visibility/actions';
 
 const VERIFY_EMAIL_ERROR_NOTICE = 'ecommerce-verify-email-error';
 const RESEND_ERROR = 'RESEND_ERROR';
@@ -31,6 +32,10 @@ const RESEND_SUCCESS = 'RESEND_SUCCESS';
 
 class AtomicStoreThankYouCard extends Component {
 	state = { resendStatus: RESEND_NOT_SENT };
+
+	componentDidMount() {
+		this.props.showMasterbar();
+	}
 
 	componentDidUpdate( prevProps ) {
 		/**
@@ -183,5 +188,5 @@ export default connect(
 			planClass,
 		};
 	},
-	{ errorNotice, fetchCurrentUser, removeNotice }
+	{ errorNotice, fetchCurrentUser, removeNotice, showMasterbar }
 )( localize( AtomicStoreThankYouCard ) );
