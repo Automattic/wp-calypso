8a2312067c5bbee2d638758135b17b9616f284d9
Markus Dobmann <m.do@posteo.net>
2021-07-12 12:10:58 +0200

    withDeleteUser: simplify success & error notices

--- client/my-sites/people/delete-user/with-delete-user.js
+++ client/my-sites/people/delete-user/with-delete-user.js
@@ -12,13 +12,13 @@ import { useTranslate } from 'i18n-calypso';
 import useDeleteUserMutation from 'calypso/data/users/use-delete-user-mutation';
 import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 
-const useSuccessNotice = ( isSuccess, user, isMultisite, siteSlug ) => {
-	const showNotice = React.useRef();
+const withDeleteUser = ( Component ) => ( props ) => {
+	const { siteId, user, isMultisite, siteSlug } = props;
 	const dispatch = useDispatch();
 	const translate = useTranslate();
 
-	React.useEffect( () => {
-		showNotice.current = () => {
+	const { deleteUser } = useDeleteUserMutation( siteId, {
+		onSuccess() {
 			dispatch(
 				successNotice(
 					isMultisite
@@ -34,24 +34,11 @@ const useSuccessNotice = ( isSuccess, user, isMultisite, siteSlug ) => {
 				)
 			);
 			page.redirect( `/people/team${ siteSlug ? `/${ siteSlug }` : '' }` );
-		};
-	}, [ dispatch, isMultisite, siteSlug, translate, user.login ] );
-
-	React.useEffect( () => {
-		isSuccess && showNotice.current();
-	}, [ isSuccess ] );
-};
-
-const useErrorNotice = ( isError, user, isMultisite, error ) => {
-	const showNotice = React.useRef();
-	const dispatch = useDispatch();
-	const translate = useTranslate();
-
-	React.useEffect( () => {
-		showNotice.current = ( errorObj ) => {
+		},
+		onError( error ) {
 			let message;
-			if ( 'user_owns_domain_subscription' === errorObj.error ) {
-				const domains = errorObj.data ?? errorObj.message.split( ',' );
+			if ( 'user_owns_domain_subscription' === error.error ) {
+				const domains = error.data ?? error.message.split( ',' );
 				message = translate(
 					'@%(user)s owns following domain used on this site: {{strong}}%(domains)s{{/strong}}. This domain will have to be transferred to a different site, transferred to a different registrar, or canceled before removing or deleting @%(user)s.',
 					'@%(user)s owns following domains used on this site: {{strong}}%(domains)s{{/strong}}. These domains will have to be transferred to a different site, transferred to a different registrar, or canceled before removing or deleting @%(user)s.',
@@ -66,8 +53,8 @@ const useErrorNotice = ( isError, user, isMultisite, error ) => {
 						},
 					}
 				);
-			} else if ( 'user_has_active_subscriptions' === errorObj.error ) {
-				const productNames = errorObj.data ?? [];
+			} else if ( 'user_has_active_subscriptions' === error.error ) {
+				const productNames = error.data ?? [];
 				message = translate(
 					'@%(user)s owns following product used on this site: {{strong}}%(productNames)s{{/strong}}. This product will have to be transferred to a different site, user, or canceled before removing or deleting @%(user)s.',
 					'@%(user)s owns following products used on this site: {{strong}}%(productNames)s{{/strong}}. These products will have to be transferred to a different site, user, or canceled before removing or deleting @%(user)s.',
@@ -95,20 +82,8 @@ const useErrorNotice = ( isError, user, isMultisite, error ) => {
 			}
 
 			dispatch( errorNotice( message, { id: 'delete-user-notice' } ) );
-		};
-	}, [ dispatch, isMultisite, translate, user.login ] );
-
-	React.useEffect( () => {
-		isError && showNotice.current( error );
-	}, [ isError, error ] );
-};
-
-const withDeleteUser = ( Component ) => ( props ) => {
-	const { siteId, user, isMultisite, siteSlug } = props;
-	const { deleteUser, isSuccess, isError, error } = useDeleteUserMutation( siteId );
-
-	useSuccessNotice( isSuccess, user, isMultisite, siteSlug );
-	useErrorNotice( isError, user, isMultisite, error );
+		},
+	} );
 
 	return <Component { ...props } deleteUser={ deleteUser } />;
 };
