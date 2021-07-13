3af12c801e5a1e2580b89cf6929d840c4d70d322
Dehan <dehandecroos@gmail.com>
2021-07-13 11:10:12 +0530

    Add new data structure to maintain marketplace product details

--- packages/calypso-products/src/constants/marketplace.ts
+++ packages/calypso-products/src/constants/marketplace.ts
@@ -1,3 +1,3 @@
-export const YOAST_SEO = 'yoast_premium';
-
-export const MARKETPLACE_PRODUCTS = [ YOAST_SEO ];
+export const YOAST_PREMIUM = 'yoast_premium';
+export const YOAST_FREE = 'yoast_premium';
+export const MARKETPLACE_PRODUCTS = [ YOAST_PREMIUM, YOAST_FREE ];
