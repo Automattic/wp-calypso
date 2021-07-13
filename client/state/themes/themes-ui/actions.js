7ffc03a3f33d5b8e4b2a8677c67dec593de40cdd
sixhours <calobee@gmail.com>
2021-07-13 13:03:23 -0400

    Remove unused state for showcase open/closed and related tests.

--- client/state/themes/themes-ui/actions.js
+++ client/state/themes/themes-ui/actions.js
@@ -1,16 +1,9 @@
 /**
  * Internal dependencies
  */
-import { THEMES_SHOWCASE_OPEN, THEMES_BOOKMARK_SET } from 'calypso/state/themes/action-types';
+import { THEMES_BOOKMARK_SET } from 'calypso/state/themes/action-types';
 
 import 'calypso/state/themes/init';
-
-// Open "More Themes" search area.
-export function openThemesShowcase() {
-	return {
-		type: THEMES_SHOWCASE_OPEN,
-	};
-}
 
 export function setThemesBookmark( state ) {
 	return {
