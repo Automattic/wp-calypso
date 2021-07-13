a789d1e0ae2e8c19245189d241452422ca9399fb
Edwin Takahashi <egao@outlook.com>
2021-07-13 14:20:55 -0700

    Switch the Authentication test to a Social Login test, designed to ensure the social login buttons redirect as expected.

---
+++ test/e2e/specs/specs-playwright/wp-auth__social-login-spec.js
@@ -0,0 +1,31 @@
+import { DataHelper, LoginFlow } from '@automattic/calypso-e2e';
+
+describe( DataHelper.createSuiteTitle( 'Social Login' ), function () {
+	describe( 'Google', function () {
+		let popupPage;
+
+		it( 'Click to continue with Google', async function () {
+			const loginFlow = new LoginFlow( this.page );
+			popupPage = await loginFlow.initiateSocialLogin( 'Google' );
+		} );
+
+		it( 'Google login page references WordPress', async function () {
+			await popupPage.waitForSelector( 'text=/[C|c]ontinue to WordPress/i' );
+		} );
+
+		it( 'Close popup', async function () {
+			await popupPage.close();
+		} );
+	} );
+
+	describe( 'Apple', function () {
+		it( 'Click to continue with Apple', async function () {
+			const loginFlow = new LoginFlow( this.page );
+			await loginFlow.initiateSocialLogin( 'Apple' );
+		} );
+
+		it( 'Apple login page references WordPress', async function () {
+			await this.page.waitForSelector( 'text=Use your Apple ID to sign in to WordPress.' );
+		} );
+	} );
+} );
