1d360a7c6f96158c9aacc84299302d2ba3dcd93a
Sergio Cinos <sergio.cinos@gmail.com>
2021-07-13 16:30:59 +0200

    Initialize global vars in the environment

--- test/e2e/lib/jest/environment.js
+++ test/e2e/lib/jest/environment.js
@@ -4,29 +4,25 @@ class JestEnvironmentE2E extends JestEnvironmentNode {
 	testFailed = false;
 
 	async handleTestEvent( event ) {
-		// Top describe (event.describeBlock.name)
-		// 		event.type=="run_describe_start" && event.describeBlock.parent.name === "ROOT_DESCRIBE_BLOCK"
+		switch ( event.name ) {
+			case 'setup':
+				this.global.__CURRENT_TEST_NAME__ = null;
+				this.global.__CURRENT_TEST_FAILED__ = false;
+				break;
 
-		// Test (event.test.name)
-		//
+			case 'test_start':
+				this.global.__CURRENT_TEST_NAME__ = event.test.name;
 
-		// Save reference to current test
-		if ( event.name === 'test_start' ) {
-			this.global.__CURRENT_TEST_NAME__ = event.test.name;
-			return;
-		}
+				if ( this.testFailed ) {
+					event.test.mode = 'skip';
+				}
+				break;
 
-		// If a hook or a test fails, enter in "failed mode"
-		if ( event.name === 'hook_failure' || event.name === 'test_fn_failure' ) {
-			this.global.__CURRENT_TEST_FAILED__ = true;
-			this.testFailed = true;
-			return;
-		}
-
-		// If a test starts after a test has already failed, skip it
-		if ( this.testFailed && event.name === 'test_start' ) {
-			event.test.mode = 'skip';
-			return;
+			case 'hook_failure':
+			case 'test_fn_failure':
+				this.global.__CURRENT_TEST_FAILED__ = true;
+				this.testFailed = true;
+				break;
 		}
 	}
 }
