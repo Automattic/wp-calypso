d9408befc4e762a01822d159e907e2ec19a1c5f2
Sergio Cinos <sergio.cinos@gmail.com>
2021-07-14 08:10:44 +0200

    ci(e2e): Migrate playwright tests to jest (#54496)
    * Migrate playwright tests to jest
    
    * Add exception for eslint rule `jest/valid-title`
    
    * Use `xvfb-run` to run jest
    
    * Lint files
    
    * Delete debug assertion
    
    * Export results
    
    * Initialize global vars in the environment
    
    * Add logger capabilities
    
    * Undo extra changes
    
    * Add test results to gitignore
    
    * Correctly capture the page from the hooks callback
    
    * Fix test
    
    * Use global describe to set up the hook

---
+++ test/e2e/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	env: {
+		node: true,
+		mocha: false,
+	},
+	overrides: [
+		{
+			plugins: [ 'mocha' ],
+			files: [
+				'test/e2e/specs/specs-jetpack/*',
+				'test/e2e/specs/specs-calypso/*',
+				'test/e2e/specs/specs-wpcom/*',
+			],
+			rules: {
+				'mocha/no-exclusive-tests': 'error',
+				'mocha/handle-done-callback': [ 'error', { ignoreSkipped: true } ],
+				'mocha/no-global-tests': 'error',
+				'mocha/no-async-describe': 'error',
+				'mocha/no-top-level-hooks': 'error',
+				'mocha/max-top-level-suites': [ 'error', { limit: 1 } ],
+				// Disable all rules from "plugin:jest/recommended", as e2e tests use mocha
+				...Object.keys( require( 'eslint-plugin-jest' ).configs.recommended.rules ).reduce(
+					( disabledRules, key ) => ( { ...disabledRules, [ key ]: 'off' } ),
+					{}
+				),
+			},
+			globals: {
+				step: false,
+			},
+		},
+	],
+	rules: {
+		'import/no-nodejs-modules': 'off',
+		'no-console': 'off',
+
+		// We have many tests that don't make an explicit `expect`, but instead puts the browser
+		// in certain state that will be used by the next test, or asserted by WebDriver
+		'jest/expect-expect': 'off',
+
+		// We compose the test titles dynamically
+		'jest/valid-title': 'off',
+	},
+};
