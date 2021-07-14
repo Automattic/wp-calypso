3a7cf443299bc0707518924a5005af19ce3eca0f
Caroline Moore <caroline@a8c.com>
2021-07-14 09:32:41 -0400

    Remove unused state for showcase open/closed and related tests. (#54519)

--- client/state/themes/themes-ui/test/actions.js
+++ client/state/themes/themes-ui/test/actions.js
@@ -1,19 +1,10 @@
 /**
  * Internal dependencies
  */
-import { openThemesShowcase, setThemesBookmark } from '../actions';
-import { THEMES_SHOWCASE_OPEN, THEMES_BOOKMARK_SET } from 'calypso/state/themes/action-types';
+import { setThemesBookmark } from '../actions';
+import { THEMES_BOOKMARK_SET } from 'calypso/state/themes/action-types';
 
 describe( 'actions', () => {
-	describe( 'openThemesShowcase()', () => {
-		test( 'Should return the expected action object', () => {
-			const action = openThemesShowcase();
-			expect( action ).toEqual( {
-				type: THEMES_SHOWCASE_OPEN,
-			} );
-		} );
-	} );
-
 	describe( 'setThemesBookmark()', () => {
 		test( 'Should return the expected action object', () => {
 			const action = setThemesBookmark( 'abc' );
