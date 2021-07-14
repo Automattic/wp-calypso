05d2ed8b300985ffb56ba53ac1e6fa15063f8f67
Edwin Takahashi <egao@outlook.com>
2021-07-14 09:35:27 -0700

    Switch theme activate spec to Jest.

--- test/e2e/specs/specs-playwright/wp-theme__switch-activate-spec.js
+++ test/e2e/specs/specs-playwright/wp-theme__switch-activate-spec.js
@@ -1,7 +1,5 @@
-/**
- * External dependencies
- */
 import {
+	setupHooks,
 	DataHelper,
 	LoginFlow,
 	SidebarComponent,
@@ -10,47 +8,50 @@ import {
 	ThemesCustomizerPage,
 } from '@automattic/calypso-e2e';
 
-describe( DataHelper.createSuiteTitle( 'Theme: Activate' ), function () {
-	describe( 'Free theme from theme details page', function () {
-		let sidebarComponent;
-		let themesPage;
-		let themesDetailPage;
-		let themesCustomizerPage;
-		const themeName = 'Twenty Twen';
-		const user = 'defaultUser';
+describe( DataHelper.createSuiteTitle( 'Theme: Activate' ), () => {
+	let sidebarComponent;
+	let themesPage;
+	let themesDetailPage;
+	let themesCustomizerPage;
+	let page;
+	const themeName = 'Twenty Twen';
+	const user = 'defaultUser';
 
-		it( 'Log In', async function () {
-			const loginFlow = new LoginFlow( this.page, user );
-			await loginFlow.logIn();
-		} );
+	setupHooks( ( args ) => {
+		page = args.page;
+	} );
 
-		it( 'Navigate to Themes', async function () {
-			sidebarComponent = await SidebarComponent.Expect( this.page );
-			await sidebarComponent.gotoMenu( { item: 'Appearance', subitem: 'Themes' } );
-		} );
+	it( 'Log In', async function () {
+		const loginFlow = new LoginFlow( page, user );
+		await loginFlow.logIn();
+	} );
 
-		it( 'Search for free theme', async function () {
-			themesPage = await ThemesPage.Expect( this.page );
-			await themesPage.filterThemes( 'Free' );
-			await themesPage.search( themeName );
-		} );
+	it( 'Navigate to Appearance > Themes', async function () {
+		sidebarComponent = await SidebarComponent.Expect( page );
+		await sidebarComponent.gotoMenu( { item: 'Appearance', subitem: 'Themes' } );
+	} );
 
-		it( `Select a theme starting with ${ themeName }`, async function () {
-			await themesPage.select( themeName );
-		} );
+	it( 'Search for free theme', async function () {
+		themesPage = await ThemesPage.Expect( page );
+		await themesPage.filterThemes( 'Free' );
+		await themesPage.search( themeName );
+	} );
 
-		it( 'Activate theme', async function () {
-			themesDetailPage = await ThemesDetailPage.Expect( this.page );
-			await themesDetailPage.activate();
-		} );
+	it( `Select a theme starting with ${ themeName }`, async function () {
+		await themesPage.select( themeName );
+	} );
 
-		it( 'Theme customizer loads in a new tab', async function () {
-			const popupTab = await themesDetailPage.customizeSite();
-			themesCustomizerPage = await ThemesCustomizerPage.Expect( popupTab );
-		} );
+	it( 'Activate theme', async function () {
+		themesDetailPage = await ThemesDetailPage.Expect( page );
+		await themesDetailPage.activate();
+	} );
 
-		it( 'Close theme customizer', async function () {
-			await themesCustomizerPage.closeTab();
-		} );
+	it( 'Theme customizer loads in a new tab', async function () {
+		const popupTab = await themesDetailPage.customizeSite();
+		themesCustomizerPage = await ThemesCustomizerPage.Expect( popupTab );
+	} );
+
+	it( 'Close theme customizer', async function () {
+		await themesCustomizerPage.closeTab();
 	} );
 } );
