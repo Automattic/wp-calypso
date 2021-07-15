6f345378a08b6301cc82993596e7403de72ccef6
sixhours <calobee@gmail.com>
2021-07-15 16:26:00 -0400

    Remove old banner

--- client/blocks/upwork-banner/index.jsx
+++ client/blocks/upwork-banner/index.jsx
@@ -9,14 +9,8 @@ import React, { PureComponent } from 'react';
 /**
  * Internal dependencies
  */
-import config from '@automattic/calypso-config';
-import { Button } from '@automattic/components';
-import { dismissBanner } from './actions';
 import { getCurrentPlan } from 'calypso/state/sites/plans/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
-import QueryPreferences from 'calypso/components/data/query-preferences';
-import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import ExternalLink from 'calypso/components/external-link';
 import UpsellNudge from 'calypso/blocks/upsell-nudge';
 
 /**
@@ -39,52 +33,26 @@ class UpworkBanner extends PureComponent {
 		if ( ! isBannerVisible ) {
 			return null;
 		}
-		if ( config.isEnabled( 'upsell/troubleshooting' ) ) {
-			return (
-				<UpsellNudge
-					event={ 'calypso_upwork_banner_start_now_button_click' }
-					forceDisplay //Upwork banner has its own logic for showing/hiding
-					className="upwork-banner__troubleshooting"
-					showIcon
-					onClick={ () => window.open( 'https://wordpress.com/built-by-wordpress-com/', '_blank' ) }
-					callToAction={ translate( 'Find your expert' ) }
-					dismissPreferenceName={ 'upwork-dismissible-banner' }
-					tracksClickName={ 'calypso_upwork_banner_start_now_button_click' }
-					tracksClickProperties={ { location, plan } }
-					tracksImpressionName={ 'calypso_upwork_banner_view' }
-					tracksDismissName={ 'calypso_upwork_banner_dismiss_icon_click' }
-					tracksDismissProperties={ { location: location, plan } }
-					href="#"
-					title={ translate( 'Let our WordPress.com experts build your site!' ) }
-					description={ translate(
-						'You want the website of your dreams. Our experts can create it for you.'
-					) }
-				/>
-			);
-		}
 		return (
-			<>
-				<QueryPreferences />
-				<UpsellNudge
-					event={ 'calypso_upwork_banner_start_now_button_click' }
-					forceDisplay //Upwork banner has its own logic for showing/hiding
-					className="upwork-banner"
-					showIcon
-					onClick={ () => window.open( 'https://wordpress.com/built-by-wordpress-com/', '_blank' ) }
-					callToAction={ translate( 'Find your expert' ) }
-					dismissPreferenceName={ 'upwork-dismissible-banner' }
-					tracksClickName={ 'calypso_upwork_banner_start_now_button_click' }
-					tracksClickProperties={ { location, plan } }
-					tracksImpressionName={ 'calypso_upwork_banner_view' }
-					tracksDismissName={ 'calypso_upwork_banner_dismiss_icon_click' }
-					tracksDismissProperties={ { location: location, plan } }
-					href="#"
-					title={ translate( 'Let our WordPress.com experts build your site!' ) }
-					description={ translate(
-						'You want the website of your dreams. Our experts can create it for you.'
-					) }
-				/>
-			</>
+			<UpsellNudge
+				event={ 'calypso_upwork_banner_start_now_button_click' }
+				forceDisplay //Upwork banner has its own logic for showing/hiding
+				className="upwork-banner__troubleshooting"
+				showIcon
+				onClick={ () => window.open( 'https://wordpress.com/built-by-wordpress-com/', '_blank' ) }
+				callToAction={ translate( 'Find your expert' ) }
+				dismissPreferenceName={ 'upwork-dismissible-banner' }
+				tracksClickName={ 'calypso_upwork_banner_start_now_button_click' }
+				tracksClickProperties={ { location, plan } }
+				tracksImpressionName={ 'calypso_upwork_banner_view' }
+				tracksDismissName={ 'calypso_upwork_banner_dismiss_icon_click' }
+				tracksDismissProperties={ { location: location, plan } }
+				href="#"
+				title={ translate( 'Let our WordPress.com experts build your site!' ) }
+				description={ translate(
+					'You want the website of your dreams. Our experts can create it for you.'
+				) }
+			/>
 		);
 	}
 }
