60341c2c8c07176537fc78f12ffb2c20f2fc7fa7
Del Putnam <del@putnams.net>
2021-07-19 06:36:55 -0400

    Update the appearance of the domain search show more results button (#54566)
    * Update the appearance of the domain search show more results button
    
    * Replace button with link-like text
    
    * Re-style button for domains section only
    
    * button typography size updates
    
    * remove unused code
    
    * Simplify style selectors
    
    * Revert "Simplify style selectors"
    
    This reverts commit 61564d40a4aa3d264b4f4a7af36e9af473d3cc17.

--- client/components/domains/register-domain-step/index.jsx
+++ client/components/domains/register-domain-step/index.jsx
@@ -34,7 +34,7 @@ import Search from '@automattic/search';
  */
 import config from '@automattic/calypso-config';
 import wpcom from 'calypso/lib/wp';
-import { CompactCard, Button } from '@automattic/components';
+import { Button, CompactCard } from '@automattic/components';
 import Notice from 'calypso/components/notice';
 import StickyPanel from 'calypso/components/sticky-panel';
 import {
