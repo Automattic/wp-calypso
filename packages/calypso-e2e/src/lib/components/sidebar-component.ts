55abd4e3a8f7c425fa5a53f2df1752fcef98fde5
Edwin Takahashi <egao@outlook.com>
2021-07-19 10:30:56 -0700

    Playwright: additional tweaks to sidebar.click (#54666)
    * Prior to clicking on sidebar, additional wait for stable.
    
    * Re-enable all tests.
    
    * Wait for `load` instead of `domcontentloaded`
    
    * Remove potentially unnecesasry `waitForElementState` call after the target element has been clicked.

--- packages/calypso-e2e/src/lib/components/sidebar-component.ts
+++ packages/calypso-e2e/src/lib/components/sidebar-component.ts
@@ -124,14 +124,17 @@ export class SidebarComponent extends BaseContainer {
 	 */
 	async _click( selector: string ): Promise< void > {
 		// Wait for these promises in no particular order. We simply want to ensure the sidebar
-		// and the page is in a state to accept inputs.
+		// and the page is not animating and is ready to accept inputs.
 		await Promise.all( [
-			this.page.waitForLoadState( 'domcontentloaded' ),
+			this.page.waitForLoadState( 'load' ),
 			this.sidebar.waitForElementState( 'stable' ),
 		] );
 
 		const elementHandle = await this.page.waitForSelector( selector );
 
+		// Scroll to reveal the element fully using a page function if required.
+		// This workaround is necessary as the sidebar is 'sticky' in calypso, so a traditional
+		// scroll behavior does not adequately expose the sidebar element.
 		await this.page.evaluate(
 			( [ element ] ) => {
 				const elementBottom = element.getBoundingClientRect().bottom;
