991cacb2d8987cd5dc4ce407ab365eb36c01a66c
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-07-21 10:42:06 -0500

    Activity Log: Convert ActivityCard from class-based to functional

--- client/components/activity-card/index.jsx
+++ client/components/activity-card/index.jsx
@@ -1,20 +1,19 @@
 /**
  * External dependencies
  */
-import { localize } from 'i18n-calypso';
 import classnames from 'classnames';
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { isEnabled } from '@automattic/calypso-config';
+import { Card } from '@automattic/components';
 
 /**
  * Internal dependencies
  */
-import { Card } from '@automattic/components';
-import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
-import { withApplySiteOffset } from 'calypso/components/site-offset';
-import { withLocalizedMoment } from 'calypso/components/localized-moment';
+import { applySiteOffset } from 'calypso/lib/site/timezone';
+import getSiteGmtOffset from 'calypso/state/selectors/get-site-gmt-offset';
+import getSiteTimezoneValue from 'calypso/state/selectors/get-site-timezone-value';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import ActivityActor from 'calypso/components/activity-card/activity-actor';
 import ActivityDescription from 'calypso/components/activity-card/activity-description';
 import getAllowRestore from 'calypso/state/selectors/get-allow-restore';
@@ -30,122 +29,92 @@ import Toolbar from './toolbar';
  */
 import './style.scss';
 
-class ActivityCard extends Component {
-	static propTypes = {
-		activity: PropTypes.object.isRequired,
-		allowRestore: PropTypes.bool.isRequired,
-		applySiteOffset: PropTypes.func,
-		className: PropTypes.string,
-		shareable: PropTypes.bool,
-		moment: PropTypes.func.isRequired,
-		siteId: PropTypes.number,
-		siteSlug: PropTypes.string.isRequired,
-		summarize: PropTypes.bool,
-		translate: PropTypes.func.isRequired,
+const useToggleContent = () => {
+	const [ isVisible, setVisible ] = useState( false );
+
+	const toggle = () => {
+		setVisible( ! isVisible );
 	};
 
-	static defaultProps = {
-		summarize: false,
-		shareable: false,
-	};
+	return [ isVisible, toggle ];
+};
 
-	constructor( props ) {
-		super( props );
+const useBackupTimeDisplay = ( activityTs ) => {
+	const siteId = useSelector( getSelectedSiteId );
+	const gmtOffset = useSelector( ( state ) => getSiteGmtOffset( state, siteId ) );
+	const timezone = useSelector( ( state ) => getSiteTimezoneValue( state, siteId ) );
 
-		this.state = {
-			showContent: false,
-		};
-	}
+	return useMemo( () => applySiteOffset( activityTs, { gmtOffset, timezone } ).format( 'LT' ), [
+		activityTs,
+		gmtOffset,
+		timezone,
+	] );
+};
 
-	render() {
-		const {
-			activity,
-			shareable,
-			allowRestore,
-			applySiteOffset,
-			className,
-			siteId,
-			summarize,
-		} = this.props;
+const ActivityCard = ( { className, summarize, activity } ) => {
+	const [ showContent, toggleShowContent ] = useToggleContent();
 
-		const backupTimeDisplay = applySiteOffset
-			? applySiteOffset( activity.activityTs ).format( 'LT' )
-			: '';
-		const showStreamsContent = this.state.showContent && activity.streams;
-		const hasActivityFailed = activity.activityStatus === 'error';
+	const siteId = useSelector( getSelectedSiteId );
+	const allowRestore = useSelector( ( state ) => getAllowRestore( state, siteId ) );
+	const backupTimeDisplay = useBackupTimeDisplay( activity.activityTs );
 
-		const onToggleContent = () => {
-			this.setState( { showContent: ! this.state.showContent } );
-		};
+	const showStreamsContent = showContent && activity.streams;
+	const hasActivityFailed = activity.activityStatus === 'error';
 
-		return (
-			<div
-				className={ classnames( className, 'activity-card', {
-					'with-error': hasActivityFailed,
-				} ) }
-			>
-				<QueryRewindState siteId={ siteId } />
+	return (
+		<div
+			className={ classnames( className, 'activity-card', {
+				'with-error': hasActivityFailed,
+			} ) }
+		>
+			<QueryRewindState siteId={ siteId } />
+			{ ! summarize && (
+				<div className="activity-card__header">
+					<div className="activity-card__time">
+						<Gridicon icon={ activity.activityIcon } className="activity-card__time-icon" />
+						<div className="activity-card__time-text">{ backupTimeDisplay }</div>
+					</div>
+					{ isEnabled( 'jetpack/activity-log-sharing' ) && (
+						<ShareActivity siteId={ siteId } activity={ activity } />
+					) }
+				</div>
+			) }
+			<Card>
+				<ActivityActor
+					actorAvatarUrl={ activity.actorAvatarUrl }
+					actorName={ activity.actorName }
+					actorRole={ activity.actorRole }
+					actorType={ activity.actorType }
+				/>
+				<div className="activity-card__activity-description">
+					<MediaPreview activity={ activity } />
+					<ActivityDescription activity={ activity } rewindIsActive={ allowRestore } />
+				</div>
+				<div className="activity-card__activity-title">{ activity.activityTitle }</div>
+
 				{ ! summarize && (
-					<div className="activity-card__header">
-						<div className="activity-card__time">
-							<Gridicon icon={ activity.activityIcon } className="activity-card__time-icon" />
-							<div className="activity-card__time-text">{ backupTimeDisplay }</div>
-						</div>
-						{ isEnabled( 'jetpack/activity-log-sharing' ) && shareable && (
-							<ShareActivity siteId={ siteId } activity={ activity } />
-						) }
-					</div>
-				) }
-				<Card>
-					<ActivityActor
-						actorAvatarUrl={ activity.actorAvatarUrl }
-						actorName={ activity.actorName }
-						actorRole={ activity.actorRole }
-						actorType={ activity.actorType }
+					<Toolbar
+						siteId={ siteId }
+						activity={ activity }
+						isContentExpanded={ showContent }
+						onToggleContent={ toggleShowContent }
 					/>
-					<div className="activity-card__activity-description">
-						<MediaPreview activity={ activity } />
-						<ActivityDescription activity={ activity } rewindIsActive={ allowRestore } />
-					</div>
-					<div className="activity-card__activity-title">{ activity.activityTitle }</div>
+				) }
 
-					{ ! summarize && (
+				{ showStreamsContent && (
+					<div className="activity-card__content">
+						<StreamsContent streams={ activity.streams } />
 						<Toolbar
 							siteId={ siteId }
 							activity={ activity }
-							isContentExpanded={ this.state.showContent }
-							onToggleContent={ onToggleContent }
+							isContentExpanded={ showContent }
+							onToggleContent={ toggleShowContent }
 						/>
-					) }
-
-					{ showStreamsContent && (
-						<div className="activity-card__content">
-							<StreamsContent streams={ activity.streams } />
-							<Toolbar
-								siteId={ siteId }
-								activity={ activity }
-								isContentExpanded={ this.state.showContent }
-								onToggleContent={ onToggleContent }
-							/>
-						</div>
-					) }
-				</Card>
-			</div>
-		);
-	}
-}
-
-const mapStateToProps = ( state ) => {
-	const siteId = getSelectedSiteId( state );
-	const siteSlug = getSelectedSiteSlug( state );
-
-	return {
-		allowRestore: getAllowRestore( state, siteId ),
-		siteId,
-		siteSlug,
-	};
+					</div>
+				) }
+			</Card>
+		</div>
+	);
 };
 
-export default connect( mapStateToProps )(
-	withLocalizedMoment( withApplySiteOffset( localize( ActivityCard ) ) )
-);
+export default ActivityCard;
