a7971f4e35cd6bf83c4892d77dc582bdc6cfdbcb
Markus Dobmann <m.do@posteo.net>
2021-07-22 15:56:09 +0200

    chore: migrate `client/auth` to `import/order`

--- .eslintrc.js
+++ .eslintrc.js
@@ -178,6 +178,7 @@ module.exports = {
 		{
 			files: [
 				'apps/editing-toolkit/**/*',
+				'client/auth/**/*',
 				'client/my-sites/customer-home/**/*',
 				'client/sections-filter.js',
 				'client/sections-helper.js',
