89acf736d11e9214070845b62c9429bcdc48770f
dpasque <dan.speckhard.pasque@automattic.com>
2021-07-23 09:08:50 -0500

    Revert "use ts-jest for running e2e .ts scripts"
    This reverts commit 87d89484793dbe005d9fb6e437ad3fc188b73516.

--- test/e2e/jest.config.js
+++ test/e2e/jest.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-	globals: {
-		'ts-jest': {
-			babelConfig: '../../babel.config.js',
-			tsConfig: './tsconfig.json',
-		},
-	},
 	cacheDirectory: '<rootDir>/../../.cache/jest',
 	testMatch: [ '<rootDir>/specs/**/*.[jt]s' ],
 	setupFilesAfterEnv: [ '<rootDir>/lib/jest/setup.js' ],
 	verbose: true,
 	transform: {
-		'\\.jsx?$': [ 'babel-jest', { configFile: '../../babel.config.js' } ],
-		'\\.tsx?$': 'ts-jest',
+		'\\.[jt]sx?$': [ 'babel-jest', { configFile: '../../babel.config.js' } ],
 	},
 	testRunner: 'jest-circus/runner',
 	testEnvironment: '<rootDir>/lib/jest/environment.js',
