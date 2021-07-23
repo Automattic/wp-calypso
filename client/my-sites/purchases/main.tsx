e1d62754d5077b6a544bd2800bafe16b17fa37b2
Christopher Smith <chris@chriswsmith.info>
2021-07-23 12:26:21 -0700

    Added 'for this site' for /purchases/subscriptions

--- client/my-sites/purchases/main.tsx
+++ client/my-sites/purchases/main.tsx
@@ -68,7 +68,9 @@ export function Purchases(): JSX.Element {
 				brandFont
 				className="purchases__page-heading"
 				headerText={ titles.sectionTitle }
-				subHeaderText={ translate( 'View, manage, or cancel your plan and other purchases.' ) }
+				subHeaderText={ translate(
+					'View, manage, or cancel your plan and other purchases for this site.'
+				) }
 				align="left"
 			/>
 			<PurchasesNavigation sectionTitle={ 'Active Upgrades' } siteSlug={ siteSlug } />
