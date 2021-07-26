6844b2e83aa26cd897a46b87031b6c0e2298e7e2
Markus Dobmann <m.do@posteo.net>
2021-07-26 09:11:48 +0200

    chore: migrate `packages/explat-client-react-helpers` to `import/order`

--- .eslintrc.js
+++ .eslintrc.js
@@ -205,6 +205,7 @@ module.exports = {
 				'packages/data-stores/**/*',
 				'packages/domain-picker/**/*',
 				'packages/eslint-plugin-wpcalypso/**/*',
+				'packages/explat-client-react-helpers/**/*',
 				'packages/explat-client/**/*',
 				'packages/format-currency/**/*',
 				'packages/js-utils/**/*',
