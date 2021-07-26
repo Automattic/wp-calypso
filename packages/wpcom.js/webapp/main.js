35af52116c27fa3f83224fd694e3dc7e1b3f747d
Jarda Snajdr <jsnajdr@gmail.com>
2021-07-26 10:51:00 +0200

    Update webpack config for wpcom.js website build

--- packages/wpcom.js/webapp/main.js
+++ packages/wpcom.js/webapp/main.js
@@ -1,8 +1,8 @@
 /**
  * Internal dependencies
  */
-import wpcomFactory from '../';
-import pkg from '../package';
+import wpcomFactory from '..';
+import pkg from '../package.json';
 
 /**
  * Expose globals
