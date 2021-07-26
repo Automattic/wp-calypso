162a3aca628a40b0fef4e23767a3189bc908835f
Markus Dobmann <m.do@posteo.net>
2021-07-26 15:55:38 +0200

    chore: migrate `boot` to `import/order`

--- .eslintrc.js
+++ .eslintrc.js
@@ -180,6 +180,7 @@ module.exports = {
 				'apps/editing-toolkit/**/*',
 				'apps/notifications/**/*',
 				'client/auth/**/*',
+				'client/boot/**/*',
 				'client/controller/**/*',
 				'client/incoming-redirect/**/*',
 				'client/mailing-lists/**/*',
