c45bea237297eed6d0467b8f2f5d7382a8124e79
Paul Von Schrottky <paulvonschrottky@gmail.com>
2021-07-26 19:10:07 -0400

    Hide masterbar when Calypso is viewed from WC apps (#54181)
    * Hide masterbar when adding credit card via WC apps
    
    The WooCommerce apps show Calypso's "add credit card" pages, but have no need for the masterbar. This PR uses the browser's (or webview's) user agent to hide the masterbar when the WCiOS or WCAndroid user agent is present. This affects any page not just the payment pages, because we will want to hide the masterbar on other pages in the future.

--- client/lib/mobile-app/test/index.js
+++ client/lib/mobile-app/test/index.js
@@ -1,10 +1,10 @@
 /**
  * Internal dependencies
  */
-import { isWpMobileApp } from 'calypso/lib/mobile-app';
+import { isWpMobileApp, isWcMobileApp } from 'calypso/lib/mobile-app';
 
-describe( 'mobile-app', () => {
-	test( 'should identify the iOS mobile app', () => {
+describe( 'isWpMobileApp', () => {
+	test( 'should identify the WordPress for iOS mobile app', () => {
 		global.navigator = {
 			userAgent:
 				'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16B91 wp-iphone/12.1',
@@ -13,7 +13,7 @@ describe( 'mobile-app', () => {
 		expect( isWpMobileApp() ).toBeTruthy();
 	} );
 
-	test( 'should identify the Android mobile app', () => {
+	test( 'should identify the WordPress for Android mobile app', () => {
 		global.navigator = {
 			userAgent:
 				'Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36 wp-android/4.7',
@@ -29,5 +29,34 @@ describe( 'mobile-app', () => {
 		};
 
 		expect( isWpMobileApp() ).toBeFalsy();
+	} );
+} );
+
+describe( 'isWcMobileApp', () => {
+	test( 'should identify the WooCommerce for iOS mobile app', () => {
+		global.navigator = {
+			userAgent:
+				'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16B91 wc-ios/12.1',
+		};
+
+		expect( isWcMobileApp() ).toBeTruthy();
+	} );
+
+	test( 'should identify the WooCommerce for Android mobile app', () => {
+		global.navigator = {
+			userAgent:
+				'Mozilla/5.0 (Linux; Android 6.0; Android SDK built for x86_64 Build/MASTER; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36 wc-android/4.7',
+		};
+
+		expect( isWcMobileApp() ).toBeTruthy();
+	} );
+
+	test( 'should not identify an unknown user agent', () => {
+		global.navigator = {
+			userAgent:
+				'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+		};
+
+		expect( isWcMobileApp() ).toBeFalsy();
 	} );
 } );
