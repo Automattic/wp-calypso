7fcd13f97165fa2a103cfd26777103aecbb60c1c
Chris Diehl <jjchrisdiehl@gmail.com>
2021-07-27 17:32:11 -0400

    Add Learn More link to Email section

--- client/my-sites/email/email-header/index.js
+++ client/my-sites/email/email-header/index.js
@@ -2,6 +2,7 @@ import { useTranslate } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import React from 'react';
 import FormattedHeader from 'calypso/components/formatted-header';
+import InlineSupportLink from 'calypso/components/inline-support-link';
 import HeaderCart from 'calypso/my-sites/checkout/cart/header-cart';
 
 import './style.scss';
@@ -15,7 +16,18 @@ function EmailHeader( { currentRoute, selectedSite } ) {
 				brandFont
 				headerText={ translate( 'Emails' ) }
 				subHeaderText={ translate(
-					'Your home base for accessing, setting up, and managing your emails.'
+					'Your home base for accessing, setting up, and managing your emails. {{learnMoreLink}}Learn more{{/learnMoreLink}}.',
+					{
+						components: {
+							learnMoreLink: (
+								<InlineSupportLink
+									supportLink="https://wordpress.com/support/add-email/"
+									supportPostId={ 34087 }
+									showIcon={ false }
+								/>
+							),
+						},
+					}
 				) }
 				align="left"
 			/>
