651f44a4bc29c686a082d11b6bc409f571b6b6cd
Seni Oluwunmi <277661+olaseni@users.noreply.github.com>
2021-07-27 18:25:41 +0100

    Documentation for queries and mutations

--- client/data/emails/use-get-titan-mailbox-availability.js
+++ client/data/emails/use-get-titan-mailbox-availability.js
@@ -8,6 +8,14 @@ import { useQuery } from 'react-query';
  */
 import wpcom from 'calypso/lib/wp';
 
+/**
+ * Checks whether a mailbox name/domain i.e. email address is available for creation.
+ *
+ * @param {string} domainName The domain name of the mailbox
+ * @param {string} mailboxName The mailbox name
+ * @param {object} queryOptions Optional options to pass to the underlying query engine
+ * @returns {{ data, error, isLoading: boolean ...}} Returns various parameters piped from `useQuery`
+ */
 export const useGetTitanMailboxAvailability = ( domainName, mailboxName, queryOptions = {} ) => {
 	return useQuery(
 		[ domainName, mailboxName ],
