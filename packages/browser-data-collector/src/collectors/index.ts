d33709b537fe038b01c5c491d008d3f803ec4f55
Sergio Cinos <sergio.cinos@gmail.com>
2021-07-27 08:35:01 +0200

    Refactor packages/browser-data-collector to use import/order

--- packages/browser-data-collector/src/collectors/index.ts
+++ packages/browser-data-collector/src/collectors/index.ts
@@ -1,6 +1,3 @@
-/**
- * Internal dependencies
- */
 export { collector as deviceMemory } from './device-memory';
 export { collector as performanceTiming } from './performance-timing';
 export { collector as environment } from './environment';
