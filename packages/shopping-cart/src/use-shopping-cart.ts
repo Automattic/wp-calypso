2f3bdb4bf2f6d914c51ecaf5e5ed0c3078670d8b
Payton Swick <payton@foolord.com>
2021-07-28 19:03:04 -0400

    Refactor useShoppingCart to work with different cart keys

--- packages/shopping-cart/src/use-shopping-cart.ts
+++ packages/shopping-cart/src/use-shopping-cart.ts
@@ -1,11 +1,41 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect, useRef, useMemo } from 'react';
 import ShoppingCartContext from './shopping-cart-context';
-import type { ShoppingCartManager } from './types';
+import ShoppingCartOptionsContext from './shopping-cart-options-context';
+import useRefetchOnFocus from './use-refetch-on-focus';
+import type { UseShoppingCart } from './types';
 
-export default function useShoppingCart(): ShoppingCartManager {
-	const manager = useContext( ShoppingCartContext );
-	if ( ! manager ) {
+export default function useShoppingCart( cartKey?: string ): UseShoppingCart {
+	const managerClient = useContext( ShoppingCartContext );
+	if ( ! managerClient ) {
 		throw new Error( 'useShoppingCart must be used inside a ShoppingCartProvider' );
 	}
-	return manager;
+
+	const { defaultCartKey } = useContext( ShoppingCartOptionsContext ) ?? {};
+	const finalCartKey = cartKey ?? defaultCartKey;
+	const manager = managerClient.forCartKey( finalCartKey );
+
+	// Re-render when the cart changes
+	const isMounted = useRef( true );
+	useEffect( () => {
+		isMounted.current = true;
+		return () => {
+			isMounted.current = false;
+		};
+	}, [] );
+	const [ cartState, setCartState ] = useState( manager.getState() );
+	useEffect( () => {
+		return manager.subscribe( () => {
+			isMounted.current && setCartState( manager.getState() );
+		} );
+	}, [ manager ] );
+
+	useRefetchOnFocus( finalCartKey );
+
+	return useMemo(
+		() => ( {
+			...cartState,
+			...manager.actions,
+		} ),
+		[ cartState, manager ]
+	);
 }
