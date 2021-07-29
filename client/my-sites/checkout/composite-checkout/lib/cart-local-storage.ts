a36d73430542601738fb752c3fb87bb225072dcb
Payton Swick <payton@foolord.com>
2021-07-29 13:41:12 -0400

    Change localStorage cart key to be more specific

--- client/my-sites/checkout/composite-checkout/lib/cart-local-storage.ts
+++ client/my-sites/checkout/composite-checkout/lib/cart-local-storage.ts
@@ -8,11 +8,13 @@ const debug = debugFactory( 'calypso:cart-local-storage' );
 
 let cartProductsFallbackStorage: Partial< RequestCartProduct >[] = [];
 
+const localStorageKey = 'automatticShoppingCartProducts';
+
 // Used by signup; see https://github.com/Automattic/wp-calypso/pull/44206
 // These products are likely missing product_id.
 export function getCartFromLocalStorage(): Partial< RequestCartProduct >[] {
 	try {
-		const products = JSON.parse( window.localStorage.getItem( 'shoppingCart' ) || '[]' );
+		const products = JSON.parse( window.localStorage.getItem( localStorageKey ) || '[]' );
 		if ( Array.isArray( products ) ) {
 			return products;
 		}
@@ -31,7 +33,7 @@ export function saveCartItemsToLocalStorage( newItems: Partial< RequestCartProdu
 	debug( 'saving cart items to localStorage', newItems );
 	cartProductsFallbackStorage = newItems;
 	try {
-		window.localStorage.setItem( 'shoppingCart', JSON.stringify( newItems ) );
+		window.localStorage.setItem( localStorageKey, JSON.stringify( newItems ) );
 	} catch ( err ) {
 		debug( 'An error ocurred writing saved cart products to localStorage: ' + err );
 	}
@@ -41,7 +43,7 @@ export function clearCartFromLocalStorage(): void {
 	debug( 'clearing cart items from localStorage' );
 	cartProductsFallbackStorage = [];
 	try {
-		window.localStorage.removeItem( 'shoppingCart' );
+		window.localStorage.removeItem( localStorageKey );
 	} catch ( err ) {
 		debug( 'An error ocurred deleting saved cart products from localStorage: ' + err );
 	}
