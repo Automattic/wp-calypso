12eab855e501ac47e44cc7cf957cb391e724c0cf
Payton Swick <payton@foolord.com>
2021-07-29 15:47:39 -0400

    Change useOnSiteCreation to add products to localStorage instead
    They will later be added to the cart when checkout loads.

--- client/landing/gutenboarding/hooks/use-on-site-creation.ts
+++ client/landing/gutenboarding/hooks/use-on-site-creation.ts
@@ -4,8 +4,7 @@
 import * as React from 'react';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { createRequestCartProduct } from '@automattic/shopping-cart';
-import type { RequestCartProduct, ResponseCart } from '@automattic/shopping-cart';
-import wp from '../../../lib/wp';
+import type { RequestCartProduct } from '@automattic/shopping-cart';
 
 /**
  * Internal dependencies
@@ -18,15 +17,15 @@ import { recordOnboardingComplete } from '../lib/analytics';
 import { useSelectedPlan, useShouldRedirectToEditorAfterCheckout } from './use-selected-plan';
 import { clearLastNonEditorRoute } from '../lib/clear-last-non-editor-route';
 import { useOnboardingFlow } from '../path';
-
-const wpcom = wp.undocumented();
+import doesValueExist from 'calypso/my-sites/checkout/composite-checkout/lib/does-value-exist';
+import { addCartItemsToLocalStorage } from 'calypso/my-sites/checkout/composite-checkout/lib/cart-local-storage';
 
 /**
  * After a new site has been created there are 3 scenarios to cover:
  * 1. The user explicitly selected a paid plan using PlansGrid => redirect to checkout with that plan + any selected paid domain in cart
  * 2. The user selected a paid domain using DomainPicker (Premium Plan appears in top-right corner) => show PlansGrid as the last step of Gutenboarding => scenario 1
  * 3. The user is still seeing 'Free Plan' label on PlansButton => redirect to editor
- **/
+ */
 
 export default function useOnSiteCreation(): void {
 	const { domain } = useSelect( ( select ) => select( ONBOARD_STORE ).getState() );
@@ -85,11 +84,7 @@ export default function useOnSiteCreation(): void {
 
 				const go = async () => {
 					if ( planProduct || domainProduct ) {
-						const cart: ResponseCart = await wpcom.getCart( newSite.blogid );
-						await wpcom.setCart( newSite.blogid, {
-							...cart,
-							products: [ ...cart.products, planProduct, domainProduct ].filter( Boolean ),
-						} );
+						addCartItemsToLocalStorage( [ planProduct, domainProduct ].filter( doesValueExist ) );
 					}
 					resetOnboardStore();
 					clearLastNonEditorRoute();
