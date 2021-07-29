4ef60f61009fa19aafa352fa95479802f03ad997
sixhours <calobee@gmail.com>
2021-07-29 15:58:38 -0400

    Set the y screen position and reset it after tab click

--- client/my-sites/themes/theme-showcase.jsx
+++ client/my-sites/themes/theme-showcase.jsx
@@ -233,6 +233,7 @@ class ThemeShowcase extends React.Component {
 	};
 
 	onFilterClick = ( tabFilter ) => {
+		const scrollPos = window.pageYOffset;
 		trackClick( 'section nav filter', tabFilter );
 		this.setState( { tabFilter } );
 
@@ -240,7 +241,10 @@ class ThemeShowcase extends React.Component {
 		// In this state: tabFilter = [ Recommended | ##All(1)## ]  tier = [ All(2) | Free | ##Premium## ]
 		// Clicking "Recommended" forces tier to be "all", since Recommend themes cannot filter on tier.
 		if ( tabFilter.key !== this.tabFilters.ALL.key && 'all' !== this.props.tier ) {
-			callback = () => this.onTierSelect( { value: 'all' } );
+			callback = () => {
+				this.onTierSelect( { value: 'all' } );
+				window.scrollTo( 0, scrollPos );
+			};
 		}
 		this.setState( { tabFilter }, callback );
 	};
