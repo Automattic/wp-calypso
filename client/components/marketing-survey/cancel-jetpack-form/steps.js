b191d21180eab4446976f07e38eb9f3a35057839
jboland88 <18016357+jboland88@users.noreply.github.com>
2021-07-29 08:55:22 -0400

    Add a new cancellation form for jetpack products

---
+++ client/components/marketing-survey/cancel-jetpack-form/steps.js
@@ -0,0 +1,8 @@
+export const FEATURES_LOST_STEP = 'features_lost';
+export const CANCELLATION_REASON_STEP = 'cancellation_reason';
+export const CANCELLATION_OFFER_STEP = 'cancellation_offer';
+export const OFFER_APPLIED_STEP = 'offer_applied';
+
+// set aliases for the first and last steps
+export const INITIAL_STEP = FEATURES_LOST_STEP;
+export const LAST_STEP = CANCELLATION_REASON_STEP;
