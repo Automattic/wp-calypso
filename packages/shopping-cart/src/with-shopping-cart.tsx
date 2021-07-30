c3a3bce84fd480b7dfb664dc387496d9eb06a354
Payton Swick <payton@foolord.com>
2021-07-30 18:13:36 -0400

    Modify withShoppingCart to add a mapPropsToCartKey function

--- packages/shopping-cart/src/with-shopping-cart.tsx
+++ packages/shopping-cart/src/with-shopping-cart.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ShoppingCartContext from './shopping-cart-context';
 import useShoppingCart from './use-shopping-cart';
 import type { WithShoppingCartProps } from './types';
 
 export default function withShoppingCart< ComponentProps >(
-	Component: React.ComponentType< ComponentProps & WithShoppingCartProps >
+	Component: React.ComponentType< ComponentProps & WithShoppingCartProps >,
+	mapPropsToCartKey?: ( props: ComponentProps ) => string | undefined
 ): React.FC< ComponentProps > {
 	return function ShoppingCartWrapper( props ): JSX.Element {
-		const shoppingCartManager = useShoppingCart();
+		const cartKey = mapPropsToCartKey?.( props );
+
+		// Even though managerClient isn't used here this guard will provide a
+		// better error message than waiting for the one in useShoppingCart.
+		const managerClient = useContext( ShoppingCartContext );
+		if ( ! managerClient ) {
+			throw new Error( 'withShoppingCart must be used inside a ShoppingCartProvider' );
+		}
+
+		const shoppingCartManager = useShoppingCart( cartKey );
 		return (
 			<Component
 				{ ...props }
