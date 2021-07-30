c786cad868ff7200ac7b5ec5a99b0f78218cee02
sixhours <calobee@gmail.com>
2021-07-30 12:48:57 -0400

    Use a wider variety of font weights across the board

--- apps/editing-toolkit/editing-toolkit-plugin/global-styles/class-global-styles.php
+++ apps/editing-toolkit/editing-toolkit-plugin/global-styles/class-global-styles.php
@@ -483,18 +483,11 @@ class Global_Styles {
 		}
 
 		if ( count( $font_list ) > 0 ) {
-			$expanded_font_list = array( 'Montserrat', 'Work Sans', 'Roboto' );
-			$font_list_str      = '';
+			$font_list_str = '';
 			foreach ( $font_list as $font ) {
-				// Some fonts have more available styles than others,
-				// so return those if available.
-				if ( in_array( $font, $expanded_font_list, true ) ) {
-					$font_list_str = $font_list_str . $font . ':thin,extralight,light,regular,medium,semibold,bold,italic,bolditalic,extrabold,black|';
-				} else {
-					// Some fonts lack italic variants,
-					// the API will return only the regular and bold CSS for those.
-					$font_list_str = $font_list_str . $font . ':regular,bold,italic,bolditalic|';
-				}
+				// Some fonts lack italic variants,
+				// the API will return only the regular and bold CSS for those.
+				$font_list_str = $font_list_str . $font . ':thin,extralight,light,regular,medium,semibold,bold,italic,bolditalic,extrabold,black|';
 			}
 			$result = $result . "@import url('https://fonts.googleapis.com/css?family=" . $font_list_str . "');";
 		}
