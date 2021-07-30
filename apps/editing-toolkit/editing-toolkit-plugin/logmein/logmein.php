da0e8e0b99517dc1e47a0f5d7a7f6a870ca58d24
Louis Laugesen <louis.laugesen@gmail.com>
2021-07-30 15:28:30 +1000

    Move filter check to enqueue

--- apps/editing-toolkit/editing-toolkit-plugin/logmein/logmein.php
+++ apps/editing-toolkit/editing-toolkit-plugin/logmein/logmein.php
@@ -18,12 +18,7 @@ namespace A8C\FSE\Logmein;
  * Init
  */
 function logmein_init() {
-	/**
-	 * This filter prevents loading logmein on sites where logmein isn't supported.
-	 */
-	if ( apply_filters( 'wpcom_logmein_enable_etk', false ) ) {
-		add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\logmein_enqueue', 100 );
-	}
+	add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\logmein_enqueue', 100 );
 }
 add_action( 'init', __NAMESPACE__ . '\logmein_init' );
 
@@ -31,6 +26,11 @@ add_action( 'init', __NAMESPACE__ . '\logmein_init' );
  * Enqueue
  */
 function logmein_enqueue() {
+	// This filter is used by wpcom to selectively choose when to enable the logmein feature.
+	if ( ! apply_filters( 'wpcom_logmein_enable_etk', false ) ) {
+		return;
+	}
+
 	$assets       = include __DIR__ . '/dist/logmein.asset.php';
 	$dependencies = isset( $assets['dependencies'] ) ? $assets['dependencies'] : array();
 	$version      = isset( $assets['version'] ) ? $assets['version'] : filemtime( __DIR__ . '/index.ts' );
