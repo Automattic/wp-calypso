0702d8a47564bcbe0e0d1932269f37a889124654
Payton Swick <payton@foolord.com>
2021-07-30 18:49:19 -0400

    Use singleton cartManagerClient in CalypsoShoppingCartProvider

--- client/my-sites/checkout/calypso-shopping-cart-provider.tsx
+++ client/my-sites/checkout/calypso-shopping-cart-provider.tsx
@@ -2,51 +2,23 @@
  * External dependencies
  */
 import React, { useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import {
-	ShoppingCartProvider,
-	useShoppingCart,
-	getEmptyResponseCart,
-} from '@automattic/shopping-cart';
-import type { RequestCart } from '@automattic/shopping-cart';
+import { ShoppingCartProvider, useShoppingCart } from '@automattic/shopping-cart';
 
 /**
  * Internal Dependencies
  */
-import wp from 'calypso/lib/wp';
 import useCartKey from './use-cart-key';
 import CartMessages from 'calypso/my-sites/checkout/cart/cart-messages';
-import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
+import { cartManagerClient } from './cart-manager-client';
 
-const wpcomGetCart = ( cartKey: string ) => wp.req.get( `/me/shopping-cart/${ cartKey }` );
-const wpcomSetCart = ( cartKey: string, cartData: RequestCart ) =>
-	wp.req.post( `/me/shopping-cart/${ cartKey }`, cartData );
-
-const emptyCart = getEmptyResponseCart();
-
-// A convenience wrapper around ShoppingCartProvider to set the necessary props for calypso
+// A convenience wrapper around ShoppingCartProvider to set the necessary props
+// for calypso and to display error and success messages returned from calls to
+// the cart endpoint.
 export default function CalypsoShoppingCartProvider( {
 	children,
 }: {
 	children: React.ReactNode;
 } ): JSX.Element {
-	const isLoggedOutCart = ! useSelector( isUserLoggedIn );
-	const currentUrlPath = window.location.pathname;
-	const searchParams = new URLSearchParams( window.location.search );
-	const jetpackPurchaseToken = searchParams.has( 'purchasetoken' );
-	const jetpackPurchaseNonce = searchParams.has( 'purchaseNonce' );
-	const isJetpackCheckout =
-		currentUrlPath.includes( '/checkout/jetpack' ) &&
-		isLoggedOutCart &&
-		( !! jetpackPurchaseToken || !! jetpackPurchaseNonce );
-	const isNoSiteCart =
-		isJetpackCheckout ||
-		( ! isLoggedOutCart &&
-			currentUrlPath.includes( '/checkout/no-site' ) &&
-			'no-user' === searchParams.get( 'cart' ) );
-
-	const getCart = isLoggedOutCart || isNoSiteCart ? () => Promise.resolve( emptyCart ) : undefined;
-
 	const defaultCartKey = useCartKey();
 
 	const options = useMemo(
@@ -58,11 +30,7 @@ export default function CalypsoShoppingCartProvider( {
 	);
 
 	return (
-		<ShoppingCartProvider
-			getCart={ getCart || wpcomGetCart }
-			setCart={ wpcomSetCart }
-			options={ options }
-		>
+		<ShoppingCartProvider managerClient={ cartManagerClient } options={ options }>
 			<CalypsoShoppingCartMessages />
 			{ children }
 		</ShoppingCartProvider>
