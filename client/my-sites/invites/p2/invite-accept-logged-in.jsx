78227c237276718255d6f42bb1631b9ae43e87b4
annemirasol <annemirasol@gmail.com>
2021-08-03 23:26:06 +0800

    Add more details to invite-accept-logged-in page

--- client/my-sites/invites/p2/invite-accept-logged-in.jsx
+++ client/my-sites/invites/p2/invite-accept-logged-in.jsx
@@ -19,19 +19,20 @@ import './style.scss';
 export function renderInviteAcceptForP2( props ) {
 	return (
 		<div>
-			<div className="invite-accept-logged-in__p2-logo">
-				<img src="/calypso/images/p2/logo.png" width="67" height="32" alt="P2 logo" />
+			<div className="invite-accept-logged-in__p2-logo-bar">
+				<div className="invite-accept-logged-in__p2-logo">
+					<img src="/calypso/images/p2/logo.png" width="67" height="32" alt="P2 logo" />
+				</div>
+				<div className="invite-accept-logged-in__p2-learn-more">
+					<a href="https://wordpress.com/p2" target="_blank" rel="noreferrer">
+						{ props.translate( 'Learn more about P2' ) }
+					</a>
+				</div>
 			</div>
-			<div className="invite-accept-logged-in__p2-learn-more">
-				<a href="https://p2help.wordpress.com">
-					{ props.translate( 'Learn more about P2', { context: 'link' } ) }
-				</a>
-			</div>
-
 			<div className="invite-accept-logged-in__invite-container">
 				<div className="invite-accept-logged-in__header">
 					<div className="invite-accept-logged-in__site-icon">
-						<img src={ props.invite.site.icon.img } width="64" height="64" alt="P2 logo" />
+						{ props.invite.site.title.charAt( 0 ) }
 					</div>
 					<div className="invite-accept-logged-in__join-p2-title">
 						{ props.translate( 'Join %(siteName)s on P2', {
@@ -54,7 +55,17 @@ export function renderInviteAcceptForP2( props ) {
 
 				<div className="invite-accept-logged-in__join-as">
 					<Gravatar user={ props.user } size={ 72 } />
-					<div className="invite-accept-logged-in__join-as-text">{ props.joinAsText }</div>
+					<div className="invite-accept-logged-in__join-as-text">
+						{ /* { props.translate( "You'll join as {{usernameWrap}}%(username)s{{/usernameWrap}}", {
+							components: {
+								usernameWrap: <span className="invite-accept-logged-in__join-as-username" />,
+							},
+							args: {
+								username: props.user && props.user.display_name,
+							},
+						} ) } */ }
+						{ props.joinAsText }
+					</div>
 					<LoggedOutFormLinks>
 						<LoggedOutFormLinkItem onClick={ props.signIn } href={ props.signInLink }>
 							{ props.translate( 'Sign in as a different user' ) }
