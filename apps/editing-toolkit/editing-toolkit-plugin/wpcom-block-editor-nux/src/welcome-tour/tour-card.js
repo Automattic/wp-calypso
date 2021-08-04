b9ba76740b708101f30fc4c9faed703b0c00f72f
Caroline Moore <caroline@a8c.com>
2021-08-04 10:04:20 -0400

    Welcome Tour and Modal: Allow modal to be dismissed on mobile devices (#55131)

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/welcome-tour/tour-card.js
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/welcome-tour/tour-card.js
@@ -1,7 +1,8 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
+import { subscribeIsMobile } from '@automattic/viewport';
 import { Button, Card, CardBody, CardFooter, CardMedia, Flex } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
-import { useEffect } from '@wordpress/element';
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { close } from '@wordpress/icons';
 import classNames from 'classnames';
@@ -120,6 +121,15 @@ function CardNavigation( { cardIndex, lastCardIndex, onDismiss, setCurrentCardIn
 }
 
 function CardOverlayControls( { onMinimize, onDismiss, slideNumber } ) {
+	const [ isMobile, setIsMobile ] = useState( false );
+
+	useEffectOnlyOnce( () => {
+		const unsubscribe = subscribeIsMobile( ( isNarrow ) => setIsMobile( isNarrow ) );
+		return function cleanup() {
+			unsubscribe();
+		};
+	} );
+
 	const handleOnMinimize = () => {
 		onMinimize( true );
 		recordTracksEvent( 'calypso_editor_wpcom_tour_minimize', {
@@ -127,9 +137,12 @@ function CardOverlayControls( { onMinimize, onDismiss, slideNumber } ) {
 			slide_number: slideNumber,
 		} );
 	};
+	const buttonClasses = classNames( 'welcome-tour-card__overlay-controls', {
+		'welcome-tour-card__overlay-controls-visible': isMobile,
+	} );
 
 	return (
-		<div className="welcome-tour-card__overlay-controls">
+		<div className={ buttonClasses }>
 			<Flex>
 				<Button
 					label={ __( 'Minimize Tour', 'full-site-editing' ) }
