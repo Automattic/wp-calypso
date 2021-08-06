97b132ae09c1b3c7a17ce6921cc8065fd323eb79
Noah Allen <noahtallen@gmail.com>
2021-08-06 13:50:13 -0700

    Define entrypoints in webpack config

--- apps/editing-toolkit/webpack.config.js
+++ apps/editing-toolkit/webpack.config.js
@@ -4,7 +4,7 @@
 /* eslint-disable import/no-nodejs-modules */
 
 const path = require( 'path' );
-const getBaseWebpackConfig = require( '@automattic/calypso-build/webpack.config.js' );
+const getBaseWebpackConfig = require( '@automattic/calypso-build/webpack.config' );
 const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
 const webpack = require( 'webpack' );
 
@@ -15,14 +15,67 @@ const FSE_MODULE_PREFIX = 'a8c-fse';
  */
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
+const ETK_ENTRYPOINTS = [
+	'dotcom-fse',
+	'posts-list-block',
+	'common/hide-plugin-buttons-mobile',
+	'common/data-stores',
+	'common',
+	'editor-site-launch/launch-button',
+	'editor-site-launch/gutenboarding-launch',
+	'editor-site-launch/focused-launch',
+	'error-reporting',
+	'newspack-blocks/blog-posts-block-editor',
+	'newspack-blocks/blog-posts-block-view',
+	'newspack-blocks/carousel-block-editor',
+	'newspack-blocks/carousel-block-view',
+	'starter-page-templates',
+	'event-countdown-block',
+	'global-styles',
+	'global-styles/customizer-fonts',
+	'jetpack-timeline',
+	'wpcom-block-editor-nux',
+	'block-inserter-modifications/contextual-tips',
+	'wpcom-block-editor-nav-sidebar',
+	'whats-new',
+];
+
+/**
+ * Generates the webpack entry object.
+ *
+ * @param {Array<string>} entries A list of paths to entrypoints.
+ */
+function generateEntry( entries ) {
+	return entries.reduce( ( acc, source ) => {
+		// If the source is 'test-package/foo/my-feat', the containing package is "test-package" and the scriptname is "my-feat".
+		// This will resolve "scriptname.(j|t)s(x)" or "scriptname/index(j|t)s(x)"
+		const sourceSegments = source.split( path.sep );
+		const scriptName =
+			sourceSegments.length > 1 ? sourceSegments.slice( sourceSegments.length - 1 ) : source;
+		// The containing package.
+		const packageName = sourceSegments[ 0 ];
+		const bundleLocation = `editing-toolkit-plugin/${ packageName }/dist/${ scriptName }`;
+
+		if ( acc[ bundleLocation ] ) {
+			throw new Error( 'The bundle ' + bundleLocation + ' has already been defined.' );
+		}
+		acc[ bundleLocation ] = {
+			import: `./editing-toolkit-plugin/${ source }`,
+			filename: '[name].js',
+		};
+
+		return acc;
+	}, {} );
+}
+
 /**
  * Return a webpack config object
  *
  * Arguments to this function replicate webpack's so this config can be used on the command line,
  * with individual options overridden by command line args.
  *
- * @see {@link https://webpack.js.org/configuration/configuration-types/#exporting-a-function}
- * @see {@link https://webpack.js.org/api/cli/}
+ * @see {@link https://webpack.org/configuration/configuration-types/#exporting-a-function}
+ * @see {@link https://webpack.org/api/cli/}
  * @param   {object}  env                           environment options
  * @param   {string}  env.source                    plugin slugs, comma separated list
  * @param   {object}  argv                          options map
@@ -32,41 +85,15 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 function getWebpackConfig( env = { source: '' }, argv = {} ) {
 	env.WP = true;
 
-	// object provides ability to name the entry point
-	// which enables dynamic file names
-	const sources = env.source.split( ',' );
-
-	// Output path
-	let packageName;
-	const entry = sources.reduce( ( accumulator, source ) => {
-		const sourceSegments = source.split( path.sep );
-		const scriptName =
-			sourceSegments.length > 1 ? sourceSegments.slice( 1 ).join( path.sep ) : source;
-
-		// All outputs from a particular package must share an output path.
-		const sourcePackage = sourceSegments[ 0 ];
-		if ( packageName && packageName !== sourceSegments[ 0 ] ) {
-			throw new Error( 'FSE can build multiple sources for a single package.' );
-		}
-		packageName = sourcePackage;
-
-		accumulator[ scriptName ] = path.join( __dirname, 'editing-toolkit-plugin', source );
-		return accumulator;
-	}, {} );
-
-	const outputPath = path.join( __dirname, 'editing-toolkit-plugin', packageName, 'dist' );
-
 	const webpackConfig = getBaseWebpackConfig( env, argv );
 
-	return {
+	const config = {
 		...webpackConfig,
-		entry,
 		output: {
-			...webpackConfig.output,
-			path: outputPath,
-			filename: '[name].js', // dynamic filename
 			library: 'EditingToolkit',
+			path: __dirname,
 		},
+		entry: generateEntry( ETK_ENTRYPOINTS ),
 		optimization: {
 			...webpackConfig.optimization,
 			// disable module concatenation so that instances of `__()` are not renamed
@@ -101,6 +128,9 @@ function getWebpackConfig( env = { source: '' }, argv = {} ) {
 		devtool: isDevelopment ? 'inline-cheap-source-map' : false,
 		stats: 'minimal',
 	};
+
+	console.log( config );
+	return config;
 }
 
 module.exports = getWebpackConfig;
