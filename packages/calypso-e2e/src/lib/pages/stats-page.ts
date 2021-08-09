3bc37d147a8ac9e2d7df4a1b1bd39f52b31c8fad
Edwin Takahashi <egao@outlook.com>
2021-08-09 09:00:01 -0700

    Simplify check if tab is selected.

--- packages/calypso-e2e/src/lib/pages/stats-page.ts
+++ packages/calypso-e2e/src/lib/pages/stats-page.ts
@@ -30,12 +30,7 @@ export class StatsPage {
 	async clickTabItem( name: StatsTabs ): Promise< void > {
 		await this.page.click( selectors.tabItems( name ) );
 
-		// Confirm the tab is now selected, by matching the `li` that holds the
-		// selector, then checking whether the `li` element has a class
-		// `is-selected`.
-		await this.page.waitForFunction(
-			( element: any ) => element.classList.contains( 'is-selected' ),
-			await this.page.waitForSelector( `*css=li >> ${ selectors.tabItems( name ) }` )
-		);
+		// Confirm the tab is now selected.
+		await this.page.waitForSelector( `*css=li.is-selected >> ${ selectors.tabItems( name ) }` );
 	}
 }
