1dc7bfe9e9f8e4482df0f2a9f887b8e10b272100
Edwin Takahashi <egao@outlook.com>
2021-08-09 11:33:41 -0700

    Split off main `SiteSelector.selectSite` method into two.

--- packages/calypso-e2e/src/lib/components/site-select-component.ts
+++ packages/calypso-e2e/src/lib/components/site-select-component.ts
@@ -26,32 +26,36 @@ export class SiteSelectComponent {
 	 *
 	 * @returns {Promise<boolean>} Whether the site selector is shown.
 	 */
-	async siteSelectorShown(): Promise< boolean > {
+	async isSiteSelectorVisible(): Promise< boolean > {
 		await this.page.waitForLoadState( 'load' );
 		return await this.page.isVisible( ':has-text("Select a site")' );
 	}
 
 	/**
+	 * Navigate to the site selector endpoint.
+	 *
+	 * @returns {Promise<void>} No return value.
+	 */
+	async showSiteSelector(): Promise< void > {
+		// Wait for any preceding page load to finish first.
+		await this.page.waitForLoadState( 'load' );
+		// Obtain and navigate to the raw /home endpoint, which de-selects the
+		// currently selected site.
+		const homeURL = getCalypsoURL( 'home' );
+		await this.page.goto( homeURL );
+		await this.page.waitForLoadState( 'load' );
+	}
+
+	/**
 	 * Given a site URL, search for and select the matching site.
 	 *
-	 * This method will forcibly navigate the page to the /home endpoint with no site forming
-	 * part of the URL. The raw /home endpoint will present to the user a list of sites belonging
-	 * to the said user.
-	 *
-	 * This method will then enter the desired site URL to the input field, wait for the matching
+	 * This method will enter the desired site URL to the input field, wait for the matching
 	 * result to load, then click on the entry.
 	 *
 	 * @param {string} url URL of the desired site. Must not include the protocol (https).
 	 * @returns {Promise<void>} No return value.
 	 */
 	async selectSite( url: string ): Promise< void > {
-		// Wait for any preceding page load to finish first.
-		await this.page.waitForLoadState( 'load' );
-		const homeURL = getCalypsoURL( 'home' );
-		await this.page.goto( homeURL );
-		// Navigate to the raw /home endpoint to trigger site selection.
-		await this.page.waitForLoadState( 'load' );
-
 		await this.page.fill( selectors.searchInput, url );
 		// For some accounts with many sites, the search process takes a looooong time.
 		await this.page.waitForSelector( '.is-loading', { state: 'hidden', timeout: 60 * 1000 } );
