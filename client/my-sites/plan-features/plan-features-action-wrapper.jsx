874c37123df42e758f690c9433d997a3227e5025
Chris Diehl <jjchrisdiehl@gmail.com>
2021-08-10 16:01:47 -0700

    Refactor how plans add CTA buttons Deduplicate the PlanFeaturesActions component into a wrapper component

---
+++ client/my-sites/plan-features/plan-features-action-wrapper.jsx
@@ -0,0 +1,82 @@
+/**
+ * Internal dependencies
+ */
+import React from 'react';
+import PlanFeaturesActions from './actions';
+import classNames from 'classnames';
+import { TYPE_BLOGGER, planMatches, isFreePlan, getPlanClass } from '@automattic/calypso-products';
+import { getManagePurchaseUrlFor } from 'calypso/my-sites/purchases/paths';
+import { useTranslate } from 'i18n-calypso';
+
+export default function PlanFeaturesActionsWrapper( {
+	canPurchase,
+	className,
+	disableBloggerPlanWithNonBlogDomain,
+	handleUpgradeClick,
+	isInSignup,
+	isLandingPage,
+	isLaunchPage,
+	nonDotBlogDomains,
+	planPropertiesPlan,
+	redirectToAddDomainFlow,
+	selectedPlan,
+	selectedSiteSlug,
+	purchaseId,
+} ) {
+	const translate = useTranslate();
+	let { availableForPurchase } = planPropertiesPlan;
+	const {
+		current,
+		planName,
+		primaryUpgrade,
+		isPlaceholder,
+		planConstantObj,
+		popular,
+	} = planPropertiesPlan;
+
+	const classes = classNames( 'plan-features__table-item', 'has-border-bottom', className );
+	let buttonText;
+	let forceDisplayButton = false;
+
+	if ( disableBloggerPlanWithNonBlogDomain || nonDotBlogDomains.length > 0 ) {
+		if ( planMatches( planName, { type: TYPE_BLOGGER } ) ) {
+			availableForPurchase = false;
+			forceDisplayButton = true;
+			buttonText = translate( 'Only with .blog domains' );
+		}
+	}
+
+	if ( redirectToAddDomainFlow ) {
+		buttonText = translate( 'Add to Cart' );
+		forceDisplayButton = true;
+	}
+
+	return (
+		<td key={ planName } className={ classes }>
+			<PlanFeaturesActions
+				availableForPurchase={ availableForPurchase }
+				forceDisplayButton={ forceDisplayButton }
+				buttonText={ buttonText }
+				canPurchase={ canPurchase }
+				className={ getPlanClass( planName ) }
+				current={ current }
+				freePlan={ isFreePlan( planName ) }
+				isInSignup={ isInSignup }
+				isLandingPage={ isLandingPage }
+				isLaunchPage={ isLaunchPage }
+				isPlaceholder={ isPlaceholder }
+				isPopular={ popular }
+				manageHref={
+					purchaseId
+						? getManagePurchaseUrlFor( selectedSiteSlug, purchaseId )
+						: `/plans/my-plan/${ selectedSiteSlug }`
+				}
+				planName={ planConstantObj.getTitle() }
+				planType={ planName }
+				primaryUpgrade={ primaryUpgrade }
+				onUpgradeClick={ () => handleUpgradeClick( planPropertiesPlan ) }
+				selectedPlan={ selectedPlan }
+			/>
+		</td>
+	);
+}
