c871314853851368652ab6b9b7ab14ea813de0f7
cpap <papazoglou.charalampos@gmail.com>
2021-08-10 17:20:10 +0300

    Marketplace: fix type and data structure.

--- client/data/marketplace/use-wpcom-plugins-query.ts
+++ client/data/marketplace/use-wpcom-plugins-query.ts
@@ -7,15 +7,12 @@ interface Options {
 	refetchOnMount?: boolean;
 }
 
-enum Type {
-	all = 'all',
-	featured = 'featured',
-}
+type Type = 'all' | 'featured';
 
-export const getCacheKey = ( type: string ): QueryKey => [ 'wpcom-plugins', type ];
+export const getCacheKey = ( type: Type ): QueryKey => [ 'wpcom-plugins', type ];
 
 const useWPCOMPlugins = (
-	type: string,
+	type: Type,
 	{ enabled = true, staleTime = 1000 * 60 * 60 * 24, refetchOnMount = false }: Options = {}
 ): UseQueryResult => {
 	return useQuery( getCacheKey( type ), () => fetchWPCOMPlugins( type ), {
@@ -25,51 +22,39 @@ const useWPCOMPlugins = (
 	} );
 };
 
-export function fetchWPCOMPlugins( type: string ) {
+export function fetchWPCOMPlugins( type: Type ) {
 	const query = {
 		type: type,
 	};
 
 	// return wpcom.req.get(
 	// 	{
-	// 		path: `wpcom-plugins`,
+	// 		path: `marketplace/plugins`,
 	// 		apiNamespace: 'wpcom/v2',
 	// 	},
 	// 	query
 	// );
 	return [
 		{
-			added: '2010-10-11',
-			author: '<a href="https://yoa.st/1uk">Team Yoast</a>',
-			author_profile: 'https://profiles.wordpress.org/joostdevalk',
-			banners: { low: 'https://ps.w.org/wordpress-seo/assets/banner-772x250.png?rev=1843435' },
-			compatibility: [],
-			donate_link: 'https://yoa.st/1up',
-			download_link: 'https://downloads.wordpress.org/plugin/wordpress-seo.16.8.zip',
-			downloaded: 345240282,
-			homepage: 'https://yoa.st/1uj',
-			icons: { '1x': 'https://ps.w.org/wordpress-seo/assets/icon.svg?rev=2363699' },
-			last_updated: '2021-07-27 6:55am GMT',
-			name: 'Yoast SEO Premium',
-			num_ratings: 27372,
-			rating: 96,
-			ratings: { 1: 717, 2: 123, 3: 170, 4: 616, 5: 25746 },
-			requires_php: '5.6.20',
-			screenshots: {
-				1: { src: 'https://ps.w.org/wordpress-seo/assets/screenshot-1.png?rev=2363699' },
-			},
-			short_description:
-				'Improve your WordPress SEO: Write better content and have a fully optimized WordPress site using the Yoast SEO plugin.',
-			slug: 'wordpress-seo-premium',
-			support_threads: 575,
-			support_threads_resolved: 529,
-			tags: {
-				'content-analysis': 'Content analysis',
-				readability: 'Readability',
-				schema: 'schema',
-				seo: 'seo',
-			},
-			version: '16.8',
+			author: '<a href="https://yoast.com/">Team Yoast</a>',
+			author_name: 'Team Yoast',
+			author_url: 'https://yoast.com/',
+			icon: 'https://ps.w.org/wordpress-seo/assets/icon-256x256.png?rev=1550389',
+			name: 'Yoast SEO',
+			rating: 98,
+			slug: 'wordpress-seo',
+
+			raw_price: 70,
+			billed: 'annually',
+		},
+		{
+			author: '<a href="https://yoast.com/">Team Yoast</a>',
+			author_name: 'Team Yoast',
+			author_url: 'https://yoast.com/',
+			icon: 'https://ps.w.org/wordpress-seo/assets/icon-256x256.png?rev=1550389',
+			name: 'Yoast SEO',
+			rating: 98,
+			slug: 'wordpress-seo',
 
 			raw_price: 70,
 			billed: 'annually',
