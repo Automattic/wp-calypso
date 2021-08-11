f8c9e46a0266859e584255db16a79e711def9cff
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2021-08-11 16:56:08 +0200

    Replace `this.state.alt` with `this.props.alt`
    The proposed change makes sure that the alt text is passed directly via component prop (instead of its state).

--- client/blocks/reader-full-post/featured-image.jsx
+++ client/blocks/reader-full-post/featured-image.jsx
@@ -7,18 +7,15 @@ import React from 'react';
 export default class FeaturedImage extends React.Component {
 	constructor( props ) {
 		super();
-		this.state = { src: props.src, alt: props.alt };
+		this.state = { src: props.src };
 		this.handleImageError = () => {
-			this.setState( { src: '', alt: '' } );
+			this.setState( { src: '' } );
 		};
 	}
 
 	UNSAFE_componentWillReceiveProps( nextProps ) {
 		if ( nextProps.src !== this.props.src ) {
 			this.setState( { src: nextProps.src } );
-		}
-		if ( nextProps.alt !== this.props.alt ) {
-			this.setState( { alt: nextProps.alt } );
 		}
 	}
 
@@ -29,7 +26,7 @@ export default class FeaturedImage extends React.Component {
 
 		return (
 			<div className="reader-full-post__featured-image">
-				<img src={ this.state.src } onError={ this.handleImageError } alt={ this.state.alt } />
+				<img src={ this.state.src } onError={ this.handleImageError } alt={ this.props.alt } />
 			</div>
 		);
 	}
