Automattic/wp-calypso

commit 7bb37ea78b18b7877348edcaec0f43b928f5edc7

Domains: Update domain item list layout (#54649)
Changes proposed in this Pull Request:

This PR corresponds to item 3 of pcYYhz-e4-p2, which is a redesign of the domain items list in the site's domains page.

The main updates are:

- Removal of the primary domain card, which is replaced by a "Primary site address" green badge
- The domain item list header was simplified and an "Add a domain to this site" dropdown button was added
- Each domain item component was simplified, with auto-renewal and email information shown as text and the domain connection status shown at the bottom of each card

Testing instructions:

The ideal way to test this would be to check the most domain statuses as you can and ensure the layout and info shown are accurate:

- Mapped domains
- Mapped less than 72 hours ago and still not resolving to WPCOM (should show "Verifying connection" status)
- Mapped more than 72 hours ago and still not resolving to WPCOM (should show "Connection error" status)
- Registered domains
- Transferred domains

With:

- Configured emails
- Expiring soon (in 30 days or less)
- Any other variations you can think of

Also please check the "Manage all domains" page to ensure information is shown correctly there as well, as they use the same DomainItem component.

Commits:

* Comment primary domain card

* Rename "Mapped Domain" to "Connected Domain"

* Increase domain name size and remove "domain-item__primary" CSS class

* Add auto-renew status besides site meta information

* Add email status besides site meta and auto-renew information

* Decrease space between items in site meta information

* Add primary site address badge placeholder

* Remove DomainNotice component from besides the domain name

* Show placeholder in place of site meta when details are not loaded yet

* Remove action column items

* Move renderAutoRenew method back to its original position

client/lib/domains/resolve-domain-status.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import React from 'react';
 import { translate } from 'i18n-calypso';
 import moment from 'moment';
 
@@ -12,11 +13,18 @@ import { isExpiringSoon } from 'calypso/lib/domains/utils/is-expiring-soon';
 import { isRecentlyRegistered } from 'calypso/lib/domains/utils/is-recently-registered';
 import { hasPendingGSuiteUsers } from 'calypso/lib/gsuite';
 import { shouldRenderExpiringCreditCard } from 'calypso/lib/purchases';
+import { domainManagementEdit } from 'calypso/my-sites/domains/paths';
 
 export function resolveDomainStatus(
 	domain,
 	purchase = null,
-	{ isJetpackSite = null, isSiteAutomatedTransfer = null, isDomainOnlySite = false } = {}
+	{
+		isJetpackSite = null,
+		isSiteAutomatedTransfer = null,
+		isDomainOnlySite = false,
+		hasMappingError = false,
+		siteSlug = null,
+	} = {}
 ) {
 	switch ( domain.type ) {
 		case domainTypes.MAPPED:
@@ -44,13 +52,45 @@ export function resolveDomainStatus(
 				};
 			}
 
-			if ( ( ! isJetpackSite || isSiteAutomatedTransfer ) && ! domain.pointsToWpcom ) {
+			if ( hasMappingError ) {
+				const status = translate(
+					"{{strong}}Connection error:{{/strong}} We couldn't verify your connection. Please {{a}}follow this setup again{{/a}}.",
+					{
+						components: {
+							strong: <strong />,
+							a: (
+								<a
+									href={ domainManagementEdit( siteSlug, domain.domain ) }
+									onClick={ ( e ) => e.stopPropagation() }
+								/>
+							),
+						},
+					}
+				);
 				return {
-					statusText: translate( 'Complete setup' ),
-					statusClass: 'status-warning',
+					statusText: translate( 'Connection error' ),
+					statusClass: 'status-alert',
 					icon: 'info',
-					listStatusText: translate( 'Complete setup' ),
-					listStatusClass: 'warning',
+					listStatusText: status,
+					listStatusClass: 'alert',
+				};
+			}
+
+			if ( ( ! isJetpackSite || isSiteAutomatedTransfer ) && ! domain.pointsToWpcom ) {
+				const status = translate(
+					'{{strong}}Verifying connection:{{/strong}} You can continue to work on your site, but you domain won’t be reachable just yet.',
+					{
+						components: {
+							strong: <strong />,
+						},
+					}
+				);
+				return {
+					statusText: translate( 'Verifying connection' ),
+					statusClass: 'status-verifying',
+					icon: 'verifying',
+					listStatusText: status,
+					listStatusClass: 'verifying',
 				};
 			}
 

client/components/popover/menu.jsx
@@ -17,6 +17,7 @@ class PopoverMenu extends Component {
 		popoverComponent: PropTypes.elementType,
 		popoverTitle: PropTypes.string, // used by ReaderPopover
 		customPosition: PropTypes.object,
+		relativePosition: PropTypes.object,
 	};
 
 	static defaultProps = {
@@ -46,6 +47,7 @@ class PopoverMenu extends Component {
 			className,
 			context,
 			customPosition,
+			relativePosition,
 			isVisible,
 			popoverTitle,
 			position,
@@ -60,6 +62,7 @@ class PopoverMenu extends Component {
 				className={ className }
 				context={ context }
 				customPosition={ customPosition }
+				relativePosition={ relativePosition }
 				isVisible={ isVisible }
 				// Make sure we focus on PopoverMenu so that we can control PopoverMenuItem by keyboard
 				focusOnShow={ false }

client/lib/domains/get-domain-type-text.js
@@ -23,7 +23,7 @@ function translatePlaceholder( string ) {
 export function getDomainTypeText( domain = {}, __ = translatePlaceholder ) {
 	switch ( domain.type ) {
 		case domainTypes.MAPPED:
-			return __( 'Mapped Domain' );
+			return __( 'Managed by external provider' );
 
 		case domainTypes.REGISTERED:
 			if ( domain?.isPremium ) {

client/my-sites/domains/domain-management/components/domain-notice/index.jsx
@@ -10,11 +10,12 @@ import classnames from 'classnames';
  * Style dependencies
  */
 import './style.scss';
+import MaterialIcon from 'calypso/components/material-icon';
 
 export default class DomainNotice extends React.Component {
 	static propTypes = {
-		text: PropTypes.string,
-		status: PropTypes.oneOf( [ 'success', 'info', 'warning', 'alert' ] ),
+		text: PropTypes.oneOfType( [ PropTypes.string, PropTypes.array ] ),
+		status: PropTypes.oneOf( [ 'success', 'info', 'warning', 'alert', 'verifying' ] ),
 		className: PropTypes.string,
 	};
 
@@ -22,21 +23,32 @@ export default class DomainNotice extends React.Component {
 		status: 'info',
 	};
 
+	renderGridIcon( icon ) {
+		return <Gridicon icon={ icon } size={ 18 } />;
+	}
+
+	renderIcon() {
+		const { status } = this.props;
+		switch ( status ) {
+			case 'info':
+				return this.renderGridIcon( 'time' );
+			case 'success':
+				return this.renderGridIcon( 'checkmark' );
+			case 'verifying':
+				return <MaterialIcon icon="compare_arrows" />;
+			default:
+				return this.renderGridIcon( 'notice-outline' );
+		}
+	}
+
 	render() {
 		const { status, text, className } = this.props;
 
 		const classes = classnames( 'domain-notice', `domain-notice__${ status }`, className );
-		let icon = 'notice-outline';
-		if ( 'info' === status ) {
-			icon = 'time';
-		}
-		if ( 'success' === status ) {
-			icon = 'checkmark';
-		}
 
 		return (
 			<span className={ classes }>
-				<Gridicon icon={ icon } size={ 18 } />
+				{ this.renderIcon() }
 				{ text }
 			</span>
 		);

client/my-sites/domains/domain-management/components/domain-notice/style.scss
@@ -6,17 +6,55 @@
 		transform: translateY( 3.5px );
 		margin-right: 4px;
 	}
+
+	svg.material-icon {
+		width: 18px;
+		height: 18px;
+		margin-right: 4px;
+	}
 }
 
 .domain-notice__info,
 .domain-notice__success {
-	color: var( --color-success-50 );
+	svg.material-icon {
+		fill: var( --color-success-50 );
+	}
+
+	svg,
+	strong {
+		color: var( --color-success-50 );
+	}
+}
+
+.domain-notice__verifying {
+	svg.material-icon {
+		fill: var( --color-success-60 );
+	}
+
+	svg,
+	strong {
+		color: var( --color-success-60 );
+	}
 }
 
 .domain-notice__warning {
-	color: var( --color-warning-40 );
+	svg.material-icon {
+		fill: var( --color-warning-40 );
+	}
+
+	svg,
+	strong {
+		color: var( --color-warning-40 );
+	}
 }
 
 .domain-notice__alert {
-	color: var( --color-error );
+	svg.material-icon {
+		fill: var( --color-error );
+	}
+
+	svg,
+	strong {
+		color: var( --color-error );
+	}
 }

client/my-sites/domains/domain-management/list/add-domain-button.jsx
@@ -17,11 +17,16 @@ import PopoverMenuItem from 'calypso/components/popover/menu-item';
 import Gridicon from 'calypso/components/gridicon';
 import { composeAnalytics, recordGoogleEvent } from 'calypso/state/analytics/actions';
 import { recordTracksEvent } from '@automattic/calypso-analytics';
-import { domainAddNew } from 'calypso/my-sites/domains/paths';
+import { domainAddNew, domainUseYourDomain } from 'calypso/my-sites/domains/paths';
 
 class AddDomainButton extends React.Component {
 	static propTypes = {
 		selectedSiteSlug: PropTypes.string,
+		specificSiteActions: PropTypes.bool,
+	};
+
+	static defaultProps = {
+		specificSiteActions: false,
 	};
 
 	state = {
@@ -49,13 +54,54 @@ class AddDomainButton extends React.Component {
 		this.props.trackAddDomainMenuClick( reactEvent.target.pathname );
 	};
 
-	render() {
-		const { translate } = this.props;
+	renderOptions = () => {
+		const { specificSiteActions, translate } = this.props;
+
+		if ( specificSiteActions ) {
+			const useYourDomainUrl = domainUseYourDomain( this.props.selectedSiteSlug );
+			return (
+				<React.Fragment>
+					<PopoverMenuItem onClick={ this.clickAddDomain }>
+						{ translate( 'Search for a domain' ) }
+					</PopoverMenuItem>
+					<PopoverMenuItem href={ useYourDomainUrl } onClick={ this.trackMenuClick }>
+						{ translate( 'Use a domain I own' ) }
+					</PopoverMenuItem>
+				</React.Fragment>
+			);
+		}
 
 		return (
 			<React.Fragment>
-				<Button primary compact className="add-domain-button" onClick={ this.toggleAddMenu }>
-					{ translate( 'Add a domain' ) }
+				{ this.props.selectedSiteSlug && (
+					<PopoverMenuItem onClick={ this.clickAddDomain }>
+						{ translate( 'to this site' ) }
+					</PopoverMenuItem>
+				) }
+				<PopoverMenuItem href="/new" onClick={ this.trackMenuClick }>
+					{ translate( 'to a new site' ) }
+				</PopoverMenuItem>
+				<PopoverMenuItem href="/domains/add" onClick={ this.trackMenuClick }>
+					{ translate( 'to a different site' ) }
+				</PopoverMenuItem>
+				<PopoverMenuItem href="/start/domain" onClick={ this.trackMenuClick }>
+					{ translate( 'without a site' ) }
+				</PopoverMenuItem>
+			</React.Fragment>
+		);
+	};
+
+	render() {
+		const { translate } = this.props;
+
+		const label = this.props.specificSiteActions
+			? translate( 'Add a domain to this site' )
+			: translate( 'Add a domain' );
+
+		return (
+			<React.Fragment>
+				<Button primary compact className={ 'add-domain-button' } onClick={ this.toggleAddMenu }>
+					{ label }
 					<Gridicon icon="chevron-down" ref={ this.addDomainButtonRef } />
 				</Button>
 				<PopoverMenu
@@ -63,21 +109,9 @@ class AddDomainButton extends React.Component {
 					onClose={ this.closeAddMenu }
 					context={ this.addDomainButtonRef.current }
 					position="bottom"
+					relativePosition={ { left: -162 } }
 				>
-					{ this.props.selectedSiteSlug && (
-						<PopoverMenuItem onClick={ this.clickAddDomain }>
-							{ translate( 'to this site' ) }
-						</PopoverMenuItem>
-					) }
-					<PopoverMenuItem href="/new" onClick={ this.trackMenuClick }>
-						{ translate( 'to a new site' ) }
-					</PopoverMenuItem>
-					<PopoverMenuItem href="/domains/add" onClick={ this.trackMenuClick }>
-						{ translate( 'to a different site' ) }
-					</PopoverMenuItem>
-					<PopoverMenuItem href="/start/domain" onClick={ this.trackMenuClick }>
-						{ translate( 'without a site' ) }
-					</PopoverMenuItem>
+					{ this.renderOptions() }
 				</PopoverMenu>
 			</React.Fragment>
 		);

client/my-sites/domains/domain-management/list/domain-item.jsx
@@ -5,14 +5,15 @@ import { connect } from 'react-redux';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import page from 'page';
-import Gridicon from 'calypso/components/gridicon';
 import { localize } from 'i18n-calypso';
 import classNames from 'classnames';
+import moment from 'moment';
 
 /**
  * Internal dependencies
  */
 import { Button, CompactCard } from '@automattic/components';
+import Badge from 'calypso/components/badge';
 import FormCheckbox from 'calypso/components/forms/form-checkbox';
 import FormRadio from 'calypso/components/forms/form-radio';
 import DomainNotice from 'calypso/my-sites/domains/domain-management/components/domain-notice';
@@ -184,44 +185,32 @@ class DomainItem extends PureComponent {
 		);
 	}
 
-	renderCheckmark( present ) {
-		return present ? <Gridicon className="domain-item__icon" size={ 18 } icon="checkmark" /> : null;
-	}
-
-	renderMinus() {
-		return <Gridicon className="domain-item__icon" size={ 18 } icon="minus" />;
-	}
-
-	renderTransferLock() {
-		const { domainDetails } = this.props;
-		if ( domainDetails?.type === domainTypes.REGISTERED ) {
-			return this.renderCheckmark( domainDetails?.isLocked );
-		}
-
-		return this.renderMinus();
-	}
-
 	renderAutoRenew() {
-		const { domainDetails } = this.props;
-		switch ( domainDetails?.type ) {
-			case domainTypes.WPCOM:
-			case domainTypes.TRANSFER:
-				return this.renderMinus();
-			default:
-				return domainDetails?.bundledPlanSubscriptionId
-					? this.renderMinus()
-					: this.renderCheckmark( domainDetails?.isAutoRenewing );
-		}
-	}
+		const { translate, domainDetails } = this.props;
 
-	renderPrivacy() {
-		const { domainDetails } = this.props;
-		if ( domainDetails?.type === domainTypes.REGISTERED ) {
-			return this.renderCheckmark( domainDetails?.privateDomain );
+		if ( ! this.shouldShowAutoRenewStatus() ) {
+			return;
 		}
 
-		return this.renderMinus();
+		const autoRenewLabel = translate( 'Auto-renew: %(autoRenewValue)s', {
+			args: {
+				autoRenewValue: domainDetails?.isAutoRenewing ? 'on' : 'off',
+			},
+			comment: 'Auto-renew a domain registration. %(autoRenewValue)s can be "on" or "off"',
+		} );
+		return <span className="domain-item__meta-item">{ autoRenewLabel }</span>;
 	}
+
+	shouldShowAutoRenewStatus = () => {
+		const { domainDetails } = this.props;
+		if (
+			domainDetails?.type === domainTypes.WPCOM ||
+			domainDetails?.type === domainTypes.TRANSFER
+		) {
+			return false;
+		}
+		return ! domainDetails?.bundledPlanSubscriptionId;
+	};
 
 	renderOptionsButton() {
 		const { disabled, isBusy, site, domain, domainDetails, currentRoute, translate } = this.props;
@@ -306,56 +295,62 @@ class DomainItem extends PureComponent {
 		const { translate } = this.props;
 
 		if ( [ domainTypes.MAPPED, domainTypes.REGISTERED ].indexOf( domainDetails.type ) === -1 ) {
-			return this.renderMinus();
+			return;
 		}
 
 		if ( hasGSuiteWithUs( domainDetails ) ) {
 			const gSuiteMailboxCount = getGSuiteMailboxCount( domainDetails );
 
-			return translate( '%(gSuiteMailboxCount)d mailbox', '%(gSuiteMailboxCount)d mailboxes', {
-				count: gSuiteMailboxCount,
-				args: {
-					gSuiteMailboxCount,
-				},
-				comment: 'The number of GSuite mailboxes active for the current domain',
-			} );
+			const text = translate(
+				'%(gSuiteMailboxCount)d mailbox',
+				'%(gSuiteMailboxCount)d mailboxes',
+				{
+					count: gSuiteMailboxCount,
+					args: {
+						gSuiteMailboxCount,
+					},
+					comment: 'The number of GSuite mailboxes active for the current domain',
+				}
+			);
+			return <span className="domain-item__meta-item">{ text }</span>;
 		}
 
 		if ( hasTitanMailWithUs( domainDetails ) ) {
 			const titanMailboxCount = getMaxTitanMailboxCount( domainDetails );
 
-			return translate( '%(titanMailboxCount)d mailbox', '%(titanMailboxCount)d mailboxes', {
+			const text = translate( '%(titanMailboxCount)d mailbox', '%(titanMailboxCount)d mailboxes', {
 				args: {
 					titanMailboxCount,
 				},
 				count: titanMailboxCount,
 				comment: '%(titanMailboxCount)d is the number of mailboxes for the current domain',
 			} );
+			return <span className="domain-item__meta-item">{ text }</span>;
 		}
 
 		if ( hasEmailForwards( domainDetails ) ) {
 			const emailForwardsCount = getEmailForwardsCount( domainDetails );
 
-			return translate( '%(emailForwardsCount)d forward', '%(emailForwardsCount)d forwards', {
+			const text = translate( '%(emailForwardsCount)d forward', '%(emailForwardsCount)d forwards', {
 				count: emailForwardsCount,
 				args: {
 					emailForwardsCount,
 				},
 				comment: 'The number of email forwards active for the current domain',
 			} );
+			return <span className="domain-item__meta-item">{ text }</span>;
 		}
 
 		if ( ! canCurrentUserAddEmail( domainDetails ) ) {
-			return this.renderMinus();
+			return;
 		}
 
 		return (
-			<Button compact onClick={ this.addEmailClick }>
-				{ translate( 'Add', {
-					context: 'Button label',
-					comment: '"Add" as in "Add an email"',
-				} ) }
-			</Button>
+			<span className="domain-item__meta-item">
+				<Button compact onClick={ this.addEmailClick } className="domain-item__email-button">
+					{ translate( 'Add email', { context: 'Button label' } ) }
+				</Button>
+			</span>
 		);
 	}
 
@@ -369,24 +364,12 @@ class DomainItem extends PureComponent {
 		if ( isLoadingDomainDetails || ! domainDetails ) {
 			return (
 				<>
-					<div className="list__domain-transfer-lock list__action_item_placeholder" />
-					<div className="list__domain-privacy list__action_item_placeholder" />
-					<div className="list__domain-auto-renew list__action_item_placeholder" />
-					<div className="list__domain-email list__action_item_placeholder" />
 					<div className="list__domain-options list__action_item_placeholder" />
 				</>
 			);
 		}
 
-		return (
-			<>
-				<div className="list__domain-transfer-lock">{ this.renderTransferLock() }</div>
-				<div className="list__domain-privacy">{ this.renderPrivacy() }</div>
-				<div className="list__domain-auto-renew">{ this.renderAutoRenew() }</div>
-				<div className="list__domain-email">{ this.renderEmail( domainDetails ) }</div>
-				{ this.renderOptionsButton() }
-			</>
-		);
+		return <>{ this.renderOptionsButton() }</>;
 	}
 
 	getSiteName( site ) {
@@ -397,11 +380,23 @@ class DomainItem extends PureComponent {
 	}
 
 	renderSiteMeta() {
-		if ( ! this.props.showDomainDetails ) {
+		const { showDomainDetails, isLoadingDomainDetails, domainDetails } = this.props;
+
+		if ( ! showDomainDetails ) {
 			return;
 		}
 
-		return <div className="domain-item__meta">{ this.getSiteMeta() }</div>;
+		if ( isLoadingDomainDetails || ! domainDetails ) {
+			return <div className="domain-item__meta domain-item__meta-placeholder" />;
+		}
+
+		return (
+			<div className="domain-item__meta">
+				{ this.getSiteMeta() }
+				{ this.renderAutoRenew() }
+				{ this.renderEmail( domainDetails ) }
+			</div>
+		);
 	}
 
 	getSiteMeta() {
@@ -477,6 +472,23 @@ class DomainItem extends PureComponent {
 		return <DomainNotice status={ statusClass } text={ message } />;
 	}
 
+	renderPrimaryBadge() {
+		return (
+			<Badge className="domain-item__primary-badge" type="info-green">
+				{ this.props.translate( 'Primary site address' ) }
+			</Badge>
+		);
+	}
+
+	hasMappingError( domain ) {
+		const registrationDatePlus3Days = moment.utc( domain.registrationDate ).add( 3, 'days' );
+		return (
+			domain.type === domainTypes.MAPPED &&
+			! domain.pointsToWpcom &&
+			moment.utc().isAfter( registrationDatePlus3Days )
+		);
+	}
+
 	render() {
 		const {
 			domain,
@@ -488,14 +500,16 @@ class DomainItem extends PureComponent {
 			isManagingAllSites,
 			showCheckbox,
 			enableSelection,
+			site,
 		} = this.props;
-		const { listStatusText, listStatusClass } = resolveDomainStatus( domainDetails || domain );
+		const { listStatusText, listStatusClass } = resolveDomainStatus(
+			domainDetails || domain,
+			null,
+			{ siteSlug: site?.slug, hasMappingError: this.hasMappingError( domain ) }
+		);
 
 		const rowClasses = classNames( 'domain-item', `domain-item__status-${ listStatusClass }`, {
 			'domain-item__enable-selection': enableSelection,
-		} );
-		const domainTitleClass = classNames( 'domain-item__title', {
-			'domain-item__primary': ! isManagingAllSites && domainDetails?.isPrimary,
 		} );
 
 		return (
@@ -518,13 +532,14 @@ class DomainItem extends PureComponent {
 				) }
 				<div className="list__domain-link">
 					<div className="domain-item__status">
-						<div className={ domainTitleClass }>{ domain.domain }</div>
-						{ listStatusText && (
-							<DomainNotice status={ listStatusClass || 'info' } text={ listStatusText } />
-						) }
+						<div className="domain-item__title">{ domain.domain }</div>
+						{ domainDetails?.isPrimary && ! isManagingAllSites && this.renderPrimaryBadge() }
 						{ this.renderActionResult() }
 					</div>
 					{ this.renderSiteMeta() }
+					{ listStatusText && (
+						<DomainNotice status={ listStatusClass || 'info' } text={ listStatusText } />
+					) }
 				</div>
 				{ this.renderActionItems() }
 				{ this.renderOverlay() }

client/my-sites/domains/domain-management/list/empty-domains-list-card.jsx
@@ -24,6 +24,7 @@ function EmptyDomainsListCard( {
 	hasDomainCredit,
 	isCompact,
 	dispatchRecordTracksEvent,
+	hasNonWpcomDomains,
 } ) {
 	const translate = useTranslate();
 
@@ -47,6 +48,9 @@ function EmptyDomainsListCard( {
 	let contentType = 'no_plan';
 
 	if ( siteHasPaidPlan && ! hasDomainCredit ) {
+		if ( hasNonWpcomDomains ) {
+			return null;
+		}
 		title = translate( 'Add your domain' );
 		line = translate( 'You have no domains added to this site.' );
 		action = translate( 'Search for a domain' );
@@ -73,9 +77,9 @@ function EmptyDomainsListCard( {
 	);
 
 	return (
-		<Card>
+		<Card className="empty-domains-list-card">
 			<div
-				className={ classNames( 'empty-domains-list-card', {
+				className={ classNames( 'empty-domains-list-card__wrapper', {
 					'is-compact': isCompact,
 					'has-title-only': title && ! line,
 				} ) }
@@ -117,6 +121,7 @@ EmptyDomainsListCard.propTypes = {
 	isCompact: PropTypes.bool,
 	domains: PropTypes.array,
 	dispatchRecordTracksEvent: PropTypes.func,
+	hasNonWpcomDomains: PropTypes.bool,
 };
 
 export default connect( null, { dispatchRecordTracksEvent: recordTracksEvent } )(

commit 0aaeaaa01b87b344507194cd6ef92d5aea3846a2

Link domain connection error status with the new "connect a domain" flow (#55400)
Changes proposed in this Pull Request

This PR modifies the link in the "connection error" domain status message to take the user to the suggested_update step of the new "connect a domain" flow, added in #54685. Previously, the link took the user to the old domain mapping instructions page. The "connection error" domain status was introduced by #54649 and is shown for connected domains that don't resolve to WPCOM after 72 hours.

This is part of the domains pages redesign work described in pcYYhz-e4-p2.

Testing instructions

Open the live Calypso link, select a site where you have a connected domain that isn't resolving to WPCOM and ensure the "follow this setup again" link takes you to the suggested_update step of the new "connect a domain" flow (the URL should look like /domains/mapping/<site_slug>/setup/<connected_domain>?step=suggested_update).

client/lib/domains/resolve-domain-status.js
@@ -13,7 +13,7 @@ import { isExpiringSoon } from 'calypso/lib/domains/utils/is-expiring-soon';
 import { isRecentlyRegistered } from 'calypso/lib/domains/utils/is-recently-registered';
 import { hasPendingGSuiteUsers } from 'calypso/lib/gsuite';
 import { shouldRenderExpiringCreditCard } from 'calypso/lib/purchases';
-import { domainManagementEdit } from 'calypso/my-sites/domains/paths';
+import { domainMappingSetup } from 'calypso/my-sites/domains/paths';
 
 export function resolveDomainStatus(
 	domain,
@@ -60,7 +60,7 @@ export function resolveDomainStatus(
 							strong: <strong />,
 							a: (
 								<a
-									href={ domainManagementEdit( siteSlug, domain.domain ) }
+									href={ domainMappingSetup( siteSlug, domain.domain, 'suggested_update' ) }
 									onClick={ ( e ) => e.stopPropagation() }
 								/>
 							),