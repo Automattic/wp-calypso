4143a8e0e2bb0aeac821679292a7404a04bbd2cf
Nicholas Sakaimbo <nsakaimbo@users.noreply.github.com>
2021-08-12 16:21:49 -0600

    desktop: use calypso.live for desktop TeamCity e2e (#55323)

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -31,6 +31,7 @@ object BuildDockerImage : BuildType({
 
 	params {
 		text("base_image", "registry.a8c.com/calypso/base:latest", label = "Base docker image", description = "Base docker image", allowEmpty = false)
+		text("calypso_live_url", "")
 	}
 
 	vcs {
@@ -103,6 +104,40 @@ object BuildDockerImage : BuildType({
 		}
 
 		script {
+			name = "Generate calypso.live URL"
+			scriptContent = """
+				#!/usr/bin/env bash
+				IMAGE_URL="https://calypso.live?image=registry.a8c.com/calypso/app:build-%build.number%";
+				MAX_LOOP=10
+				COUNTER=0
+
+				# Transform an URL like https://calypso.live?image=... into https://<container>.calypso.live
+				while [[ ${'$'}COUNTER -le ${'$'}MAX_LOOP ]]; do
+					COUNTER=${'$'}((COUNTER+1))
+					REDIRECT=${'$'}(curl --output /dev/null --silent --show-error  --write-out "%{http_code} %{redirect_url}" "${'$'}{IMAGE_URL}")
+					read HTTP_STATUS URL <<< "${'$'}{REDIRECT}"
+
+					# 202 means the image is being downloaded, retry in a few seconds
+					if [[ "${'$'}{HTTP_STATUS}" -eq "202" ]]; then
+						sleep 5
+						continue
+					fi
+
+					break
+				done
+
+				if [[ -z "${'$'}URL" ]]; then
+					echo "Can't redirect to ${'$'}{IMAGE_URL}" >&2
+					echo "Curl response: ${'$'}{REDIRECT}" >&2
+					exit 1
+				fi
+
+				echo "##teamcity[setParameter name='calypso_live_url' value='${'$'}URL']"
+				echo "Calypso.live URL is: ${'$'}URL$"
+			"""
+		}
+
+		script {
 			name = "Post PR comment with link"
 			scriptContent = """
 				#!/usr/bin/env bash
@@ -113,7 +148,7 @@ object BuildDockerImage : BuildType({
 				export GH_TOKEN="%matticbot_oauth_token%"
 				chmod +x ./bin/add-pr-comment.sh
 				./bin/add-pr-comment.sh "%teamcity.build.branch%" "calypso-live" <<- EOF || true
-				Link to Calypso live: https://calypso.live?image=registry.a8c.com/calypso/app:build-%build.number%
+				Link to Calypso live: %calypso_live_url%
 				Link to Jetpack Cloud live: https://calypso.live?image=registry.a8c.com/calypso/app:build-%build.number%&env=jetpack
 				EOF
 			"""
@@ -477,30 +512,7 @@ fun seleniumBuildType( viewportName: String, buildUuid: String): BuildType  {
 					# mocha-teamcity-reporter to work.
 					export MAGELLANDEBUG=true
 
-					IMAGE_URL="https://calypso.live?image=registry.a8c.com/calypso/app:build-${BuildDockerImage.depParamRefs.buildNumber}";
-					MAX_LOOP=10
-					COUNTER=0
-
-					# Transform an URL like https://calypso.live?image=... into https://<container>.calypso.live
-					while [[ ${'$'}COUNTER -le ${'$'}MAX_LOOP ]]; do
-						COUNTER=${'$'}((COUNTER+1))
-						REDIRECT=${'$'}(curl --output /dev/null --silent --show-error  --write-out "%{http_code} %{redirect_url}" "${'$'}{IMAGE_URL}")
-						read HTTP_STATUS URL <<< "${'$'}{REDIRECT}"
-
-						# 202 means the image is being downloaded, retry in a few seconds
-						if [[ "${'$'}{HTTP_STATUS}" -eq "202" ]]; then
-							sleep 5
-							continue
-						fi
-
-						break
-					done
-
-					if [[ -z "${'$'}URL" ]]; then
-						echo "Can't redirect to ${'$'}{IMAGE_URL}" >&2
-						echo "Curl response: ${'$'}{REDIRECT}" >&2
-						exit 1
-					fi
+					URL="%dep.calypso_BuildDockerImage.calypso_live_url%"
 
 					# Decrypt config
 					openssl aes-256-cbc -md sha1 -d -in ./config/encrypted.enc -out ./config/local-test.json -k "%CONFIG_E2E_ENCRYPTION_KEY%"
@@ -643,34 +655,7 @@ fun playwrightBuildType( viewportName: String, buildUuid: String ): BuildType {
 					export PLAYWRIGHT_BROWSERS_PATH=0
 					export TEAMCITY_VERSION=2021
 
-					IMAGE_URL="https://calypso.live?image=registry.a8c.com/calypso/app:build-${BuildDockerImage.depParamRefs.buildNumber}";
-					MAX_LOOP=10
-					COUNTER=0
-
-					# Transform an URL like https://calypso.live?image=... into https://<container>.calypso.live
-					while [[ ${'$'}COUNTER -le ${'$'}MAX_LOOP ]]; do
-						COUNTER=${'$'}((COUNTER+1))
-						REDIRECT=${'$'}(curl --output /dev/null --silent --show-error  --write-out "%{http_code} %{redirect_url}" "${'$'}{IMAGE_URL}")
-						read HTTP_STATUS URL <<< "${'$'}{REDIRECT}"
-
-						# 202 means the image is being downloaded, retry in a few seconds
-						if [[ "${'$'}{HTTP_STATUS}" -eq "202" ]]; then
-							sleep 5
-							continue
-						fi
-
-						# Wait some seconds to alleviate simulateneous traffic to the serving container
-						# to avoid incurring HTTP 304.
-						sleep 10
-
-						break
-					done
-
-					if [[ -z "${'$'}URL" ]]; then
-						echo "Can't redirect to ${'$'}{IMAGE_URL}" >&2
-						echo "Curl response: ${'$'}{REDIRECT}" >&2
-						exit 1
-					fi
+					URL="%dep.calypso_BuildDockerImage.calypso_live_url%"
 
 					# Decrypt config
 					openssl aes-256-cbc -md sha1 -d -in ./config/encrypted.enc -out ./config/local-test.json -k "%CONFIG_E2E_ENCRYPTION_KEY%"
