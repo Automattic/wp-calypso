6663f8c2b0a0d233eb57888842b06f173feade48
Andrei Demian <unDemian@users.noreply.github.com>
2021-08-13 12:02:07 +0300

    Add Slack P2 connection (#55195)

--- client/my-sites/marketing/connections/connections.jsx
+++ client/my-sites/marketing/connections/connections.jsx
@@ -8,19 +8,29 @@ import { localize } from 'i18n-calypso';
  * Internal dependencies
  */
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import QueryP2Connections from 'calypso/components/data/query-p2-connections';
 import QueryKeyringConnections from 'calypso/components/data/query-keyring-connections';
 import QueryKeyringServices from 'calypso/components/data/query-keyring-services';
 import QueryPublicizeConnections from 'calypso/components/data/query-publicize-connections';
 import SharingServicesGroup from './services-group';
 
-const SharingConnections = ( { translate } ) => (
+const SharingConnections = ( { translate, isP2Hub, siteId } ) => (
 	<div className="connections__sharing-settings connections__sharing-connections">
 		<PageViewTracker path="/marketing/connections/:site" title="Marketing > Connections" />
-		<QueryKeyringConnections />
+
+		{ isP2Hub && <QueryP2Connections siteId={ siteId } /> }
+		{ ! isP2Hub && <QueryKeyringConnections /> }
+		{ ! isP2Hub && <QueryPublicizeConnections selectedSite /> }
+		{ ! isP2Hub && (
+			<SharingServicesGroup type="publicize" title={ translate( 'Publicize posts' ) } />
+		) }
+
 		<QueryKeyringServices />
-		<QueryPublicizeConnections selectedSite />
-		<SharingServicesGroup type="publicize" title={ translate( 'Publicize posts' ) } />
-		<SharingServicesGroup type="other" title={ translate( 'Manage connections' ) } />
+		<SharingServicesGroup
+			type="other"
+			title={ translate( 'Manage connections' ) }
+			numberOfPlaceholders={ isP2Hub ? 1 : undefined }
+		/>
 	</div>
 );
 
