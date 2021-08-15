3d5859b30f8e19bd1bb1590c8ab7e04fa96e6d32
Payton Swick <payton@foolord.com>
2021-08-15 15:02:56 -0400

    Add tests for ShoppingCartManager

---
+++ packages/shopping-cart/test/cart-manager.ts
@@ -0,0 +1,32 @@
+import { createShoppingCartManagerClient } from '../src/index';
+import { getCart, setCart, mainCartKey, planOne } from './utils/mock-cart-api';
+
+describe( 'ShoppingCartManager', () => {
+	describe( 'getCart', () => {
+		it( 'returns the responseCart', async () => {
+			const cartManagerClient = createShoppingCartManagerClient( {
+				getCart,
+				setCart,
+			} );
+			const manager = cartManagerClient.forCartKey( mainCartKey );
+			await manager.waitForReady();
+			const { responseCart } = manager.getState();
+			expect( responseCart.products.length ).toBe( 0 );
+			expect( responseCart.cart_key ).toBe( mainCartKey );
+		} );
+	} );
+
+	describe( 'actions', () => {
+		it( 'addProductsToCart adds the products to the cart', async () => {
+			const cartManagerClient = createShoppingCartManagerClient( {
+				getCart,
+				setCart,
+			} );
+			const manager = cartManagerClient.forCartKey( mainCartKey );
+			await manager.actions.addProductsToCart( [ planOne ] );
+			const { responseCart } = manager.getState();
+			expect( responseCart.products.length ).toBe( 1 );
+			expect( responseCart.products[ 0 ].product_slug ).toBe( planOne.product_slug );
+		} );
+	} );
+} );
