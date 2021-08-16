6342b3a1e52841a6474656ed562e0a7a18514866
mmtr <miguel.torres@automattic.com>
2021-08-16 16:36:47 +0200

    Use `wpcom/v2` namespace for cancellations

--- client/lib/purchases/actions.js
+++ client/lib/purchases/actions.js
@@ -20,6 +20,7 @@ export function cancelAndRefundPurchase( purchaseId, data, onComplete ) {
 		{
 			path: `/upgrades/${ purchaseId }/cancel`,
 			body: data,
+			apiNamespace: 'wpcom/v2',
 		},
 		onComplete
 	);
