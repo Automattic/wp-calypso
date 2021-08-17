4080908402486fc6d3585e5434540580e5b8ef14
Payton Swick <payton@foolord.com>
2021-08-17 14:15:04 -0400

    Make sure useShoppingCart returns the noop cart manager for undefined

--- packages/shopping-cart/src/use-shopping-cart.ts
+++ packages/shopping-cart/src/use-shopping-cart.ts
@@ -1,13 +1,10 @@
-import debugFactory from 'debug';
 import { useContext, useState, useEffect, useRef, useMemo } from 'react';
 import ShoppingCartContext from './shopping-cart-context';
 import ShoppingCartOptionsContext from './shopping-cart-options-context';
 import useRefetchOnFocus from './use-refetch-on-focus';
 import type { UseShoppingCart } from './types';
 
-const debug = debugFactory( 'shopping-cart:use-shopping-cart' );
-
-export default function useShoppingCart( cartKey?: string | undefined ): UseShoppingCart {
+export default function useShoppingCart( cartKey?: string ): UseShoppingCart {
 	const managerClient = useContext( ShoppingCartContext );
 	if ( ! managerClient ) {
 		throw new Error( 'useShoppingCart must be used inside a ShoppingCartProvider' );
@@ -15,11 +12,10 @@ export default function useShoppingCart( cartKey?: string | undefined ): UseShop
 
 	const { defaultCartKey } = useContext( ShoppingCartOptionsContext ) ?? {};
 	const finalCartKey = cartKey ?? defaultCartKey;
-	debug( `getting cart manager for cartKey ${ finalCartKey }` );
 	const manager = managerClient.forCartKey( finalCartKey );
 
 	// Re-render when the cart changes
-	const isMounted = useRef< boolean >( true );
+	const isMounted = useRef( true );
 	useEffect( () => {
 		isMounted.current = true;
 		return () => {
@@ -28,14 +24,10 @@ export default function useShoppingCart( cartKey?: string | undefined ): UseShop
 	}, [] );
 	const [ cartState, setCartState ] = useState( manager.getState() );
 	useEffect( () => {
-		if ( finalCartKey ) {
-			debug( 'subscribing to cartKey', finalCartKey );
-			return managerClient.forCartKey( finalCartKey ).subscribe( () => {
-				debug( 'cart manager changed; re-rendering' );
-				isMounted.current && setCartState( manager.getState() );
-			} );
-		}
-	}, [ managerClient, finalCartKey, manager ] );
+		return manager.subscribe( () => {
+			isMounted.current && setCartState( manager.getState() );
+		} );
+	}, [ manager ] );
 
 	useRefetchOnFocus( finalCartKey );
 
