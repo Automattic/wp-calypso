55e63d82fa8aa80c2d30359b026c8d61bc86b577
Sandy McFadden <sandy.mcfadden@automattic.com>
2021-08-17 14:24:33 -0300

    Attempt to get social logins to work

--- desktop/app/window-handlers/external-links/index.js
+++ desktop/app/window-handlers/external-links/index.js
@@ -1,3 +1,4 @@
+const { BrowserWindow } = require( 'electron' );
 const assets = require( '../../lib/assets' );
 const Config = require( '../../lib/config' );
 const log = require( '../../lib/logger' )( 'desktop:external-links' );
@@ -7,24 +8,69 @@ let targetURL = '';
 module.exports = function ( { view } ) {
 	// TODO: Replace the "new-window" event with webcontents.setWindowOpenHandler
 	// when Electron is updated to >= 13.x
-	view.webContents.on( 'new-window', function ( event, url ) {
-		// Check if the incoming URL is blank and if it is send to the targetURL instead
-		const urlToLoad = url.includes( 'about:blank' ) || url === '' ? targetURL : url;
-		log.info( `Navigating to URL: '${ urlToLoad }'` );
+	view.webContents.on(
+		'new-window',
+		function (
+			event,
+			url,
+			frameName,
+			disposition,
+			options,
+			additionalFeatures,
+			referrer,
+			postBody
+		) {
+			if ( url.includes( 'https://accounts.google.com' ) ) {
+				event.preventDefault();
+				const win = new BrowserWindow( {
+					webContents: options.webContents, // use existing webContents if provided
+					show: false,
+				} );
 
-		event.preventDefault();
-		view.webContents.loadURL( urlToLoad );
-		return;
-	} );
+				win.webContents.on( 'will-navigate', function ( e, u ) {
+					log.info( 'Google Window will navigate: ' + u );
+				} );
+
+				win.webContents.on( 'will-redirect', function ( e, u ) {
+					log.info( 'Google Window will redirect: ' + u );
+				} );
+
+				win.once( 'ready-to-show', () => win.show() );
+				if ( ! options.webContents ) {
+					const loadOptions = {
+						httpReferrer: referrer,
+					};
+					if ( postBody != null ) {
+						const { data, contentType, boundary } = postBody;
+						loadOptions.postData = postBody.data;
+						loadOptions.extraHeaders = `content-type: ${ contentType }; boundary=${ boundary }`;
+					}
+
+					win.loadURL( url, loadOptions ); // existing webContents will be navigated automatically
+				}
+				event.newGuest = win;
+			} else {
+				// Check if the incoming URL is blank and if it is send to the targetURL instead
+				const urlToLoad = url.includes( 'about:blank' ) || url === '' ? targetURL : url;
+				log.info( `Navigating to URL: '${ urlToLoad }'` );
+
+				event.preventDefault();
+				view.webContents.loadURL( urlToLoad );
+				return;
+			}
+		}
+	);
 
 	// Magic links aren't supported in the app currently. Instead we'll show a message about how
 	// to set a password on the account to log in that way.
 	view.webContents.on( 'will-navigate', function ( event, url ) {
-		const urlToLoad =
-			url === Config.baseURL() + 'log-in/link'
-				? 'file://' + assets.getPath( 'magic-links-unsupported.html' )
-				: url;
-		view.webContents.loadURL( urlToLoad );
+		log.info( 'Will navigate: ' + url );
+		if ( url === Config.baseURL() + 'log-in/link' ) {
+			const urlToLoad = 'file://' + assets.getPath( 'magic-links-unsupported.html' );
+			log.info( `Navigating to URL: '${ urlToLoad }'` );
+			view.webContents.loadURL( urlToLoad );
+		}
+
 		return;
 	} );
 
@@ -37,8 +83,9 @@ module.exports = function ( { view } ) {
 	} );
 
 	view.webContents.on( 'will-redirect', function ( _, url ) {
+		log.info( 'Will redirect: ' + url );
 		if ( url.includes( 'https://wordpress.com/log-in/apple/callback' ) ) {
-			log.info( 'Redirecting to URL: ', url );
+			//log.info( 'Redirecting to URL: ', url );
 			view.webContents.loadURL( url );
 		}
 	} );
