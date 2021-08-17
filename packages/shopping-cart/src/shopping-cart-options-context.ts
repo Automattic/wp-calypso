d0b3018d42daff78c82fed04c99124fd9e22fde7
Payton Swick <payton@foolord.com>
2021-08-17 14:50:39 -0400

    Default ShoppingCartOptionsContext to an empty object

--- packages/shopping-cart/src/shopping-cart-options-context.ts
+++ packages/shopping-cart/src/shopping-cart-options-context.ts
@@ -1,7 +1,5 @@
 import { createContext } from 'react';
 import type { ShoppingCartManagerOptions } from './types';
 
-const ShoppingCartOptionsContext = createContext< ShoppingCartManagerOptions | undefined >(
-	undefined
-);
+const ShoppingCartOptionsContext = createContext< ShoppingCartManagerOptions >( {} );
 export default ShoppingCartOptionsContext;
