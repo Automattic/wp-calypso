4ff987e88beda4bd9ee02d71b12ed34b3a7c8068
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-08-18 16:39:58 -0300

    Add connection mode information to the domains state

--- client/state/sites/domains/assembler.js
+++ client/state/sites/domains/assembler.js
@@ -51,6 +51,7 @@ export const createSiteDomainObject = ( domain ) => {
 		blogId: Number( domain.blog_id ),
 		bundledPlanSubscriptionId: domain.bundled_plan_subscription_id,
 		canSetAsPrimary: Boolean( domain.can_set_as_primary ),
+		connectionMode: String( domain.connection_mode ),
 		contactInfoDisclosureAvailable: Boolean( domain.contact_info_disclosure_available ),
 		contactInfoDisclosed: Boolean( domain.contact_info_disclosed ),
 		currentUserCanAddEmail: Boolean( domain.current_user_can_add_email ),
