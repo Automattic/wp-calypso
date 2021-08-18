54a82d164240b350a9f600adee3be59753ba634a
Payton Swick <payton@foolord.com>
2021-08-18 12:54:09 -0400

    Refactor createActions to inline function definitions

--- packages/shopping-cart/src/shopping-cart-actions.ts
+++ packages/shopping-cart/src/shopping-cart-actions.ts
@@ -1,54 +1,36 @@
 import { createRequestCartProducts } from './create-request-cart-product';
 import type {
-	AddProductsToCart,
-	ReplaceProductsInCart,
-	RemoveProductFromCart,
-	ReplaceProductInCart,
-	UpdateTaxLocationInCart,
-	ApplyCouponToCart,
-	RemoveCouponFromCart,
-	ReloadCartFromServer,
 	RequestCartProduct,
 	DispatchAndWaitForValid,
 	ShoppingCartManagerActions,
 } from './types';
 
 export function createActions( dispatch: DispatchAndWaitForValid ): ShoppingCartManagerActions {
-	const removeCoupon: RemoveCouponFromCart = () => dispatch( { type: 'REMOVE_COUPON' } );
-	const addProductsToCart: AddProductsToCart = async ( products ) =>
-		dispatch( {
-			type: 'CART_PRODUCTS_ADD',
-			products: createRequestCartProducts( products ),
-		} );
-	const removeProductFromCart: RemoveProductFromCart = ( uuidToRemove ) =>
-		dispatch( { type: 'REMOVE_CART_ITEM', uuidToRemove } );
-	const replaceProductsInCart: ReplaceProductsInCart = async ( products ) =>
-		dispatch( {
-			type: 'CART_PRODUCTS_REPLACE_ALL',
-			products: createRequestCartProducts( products ),
-		} );
-	const replaceProductInCart: ReplaceProductInCart = (
-		uuidToReplace: string,
-		productPropertiesToChange: Partial< RequestCartProduct >
-	) =>
-		dispatch( {
-			type: 'CART_PRODUCT_REPLACE',
-			uuidToReplace,
-			productPropertiesToChange,
-		} );
-	const updateLocation: UpdateTaxLocationInCart = ( location ) =>
-		dispatch( { type: 'SET_LOCATION', location } );
-	const applyCoupon: ApplyCouponToCart = ( newCoupon ) =>
-		dispatch( { type: 'ADD_COUPON', couponToAdd: newCoupon } );
-	const reloadFromServer: ReloadCartFromServer = () => dispatch( { type: 'CART_RELOAD' } );
 	return {
-		reloadFromServer,
-		applyCoupon,
-		updateLocation,
-		replaceProductInCart,
-		replaceProductsInCart,
-		removeProductFromCart,
-		addProductsToCart,
-		removeCoupon,
+		reloadFromServer: () => dispatch( { type: 'CART_RELOAD' } ),
+		removeCoupon: () => dispatch( { type: 'REMOVE_COUPON' } ),
+		addProductsToCart: async ( products ) =>
+			dispatch( {
+				type: 'CART_PRODUCTS_ADD',
+				products: createRequestCartProducts( products ),
+			} ),
+		removeProductFromCart: ( uuidToRemove ) =>
+			dispatch( { type: 'REMOVE_CART_ITEM', uuidToRemove } ),
+		replaceProductsInCart: async ( products ) =>
+			dispatch( {
+				type: 'CART_PRODUCTS_REPLACE_ALL',
+				products: createRequestCartProducts( products ),
+			} ),
+		replaceProductInCart: (
+			uuidToReplace: string,
+			productPropertiesToChange: Partial< RequestCartProduct >
+		) =>
+			dispatch( {
+				type: 'CART_PRODUCT_REPLACE',
+				uuidToReplace,
+				productPropertiesToChange,
+			} ),
+		updateLocation: ( location ) => dispatch( { type: 'SET_LOCATION', location } ),
+		applyCoupon: ( newCoupon ) => dispatch( { type: 'ADD_COUPON', couponToAdd: newCoupon } ),
 	};
 }
