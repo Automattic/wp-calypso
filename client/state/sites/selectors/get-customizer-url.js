9ed7af3d89b3e78190d0967c49a66d701f95eb60
Eru Penkman <roo2@users.noreply.github.com>
2021-08-19 11:39:26 +1000

    change add a menu link to point to the top level of customizer with the tour shown (#54583)

--- client/state/sites/selectors/get-customizer-url.js
+++ client/state/sites/selectors/get-customizer-url.js
@@ -10,19 +10,22 @@ import isJetpackSite from './is-jetpack-site';
 /**
  * Returns the customizer URL for a site, or null if it cannot be determined.
  *
- * @param   {object}  state     Global state tree
- * @param   {?number} siteId    Site ID
- * @param   {string}  panel     Optional panel to autofocus
- * @param   {string}  returnUrl Optional return url for when the user closes the customizer
- * @returns {string}            Customizer URL
+ * @param   {object}  state        Global state tree
+ * @param   {?number} siteId       Site ID
+ * @param   {?string} panel        Optional panel to autofocus
+ * @param   {?string} returnUrl    Optional return url for when the user closes the customizer
+ * @param   {?string} guide        Optional parameter to show a help guide in the customizer. possible values:
+ *                                 'add-menu' and 'social-media' show custom guides, any other value shows the default guide
+ * @returns {string}               Customizer URL
  */
-export default function getCustomizerUrl( state, siteId, panel, returnUrl ) {
+export default function getCustomizerUrl( state, siteId, panel, returnUrl, guide ) {
 	if ( ! isJetpackSite( state, siteId ) ) {
 		const siteSlug = getSiteSlug( state, siteId );
 		const url = [ '' ].concat( [ 'customize', panel, siteSlug ].filter( Boolean ) ).join( '/' );
 		return addQueryArgs(
 			{
 				return: returnUrl,
+				guide,
 			},
 			url
 		);
@@ -42,6 +45,7 @@ export default function getCustomizerUrl( state, siteId, panel, returnUrl ) {
 		{
 			return: returnUrl,
 			...getCustomizerFocus( panel ),
+			guide,
 		},
 		adminUrl
 	);
