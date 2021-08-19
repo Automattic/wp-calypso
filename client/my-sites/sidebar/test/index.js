dd8bb998226007978af27001b021313345c6d201
Markus Dobmann <m.do@posteo.net>
2021-08-19 12:14:57 +0200

    chore: migrate `client/my-sites/sidebar` to `import/order`

--- client/my-sites/sidebar/test/index.js
+++ client/my-sites/sidebar/test/index.js
@@ -1,11 +1,4 @@
-/**
- * External dependencies
- */
 import { expect } from 'chai';
-
-/**
- * Internal dependencies
- */
 import { itemLinkMatches } from '../utils';
 
 describe( 'MySitesSidebar', () => {
