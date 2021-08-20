cb1ac7cc766397829692281790334b7fd6451e2e
niranjan-uma-shankar <niranjan.u@gmail.com>
2021-08-20 17:12:01 +0400

    Block rescheduling of sessions if the appointment is less than 60 minutes away (#54375)
    * Don't render the calendar page for rescheduling if the user is less than 60 mins away from an appointment
    
    * Move renderDisallowed() function to a shared folder so that the function can be reused
    
    * Move renderDisallowed() function to a shared folder so that the function can be reused
    
    * Change /me/concierge to /me/quickstart

--- client/me/concierge/cancel/index.js
+++ client/me/concierge/cancel/index.js
@@ -13,7 +13,7 @@ import HeaderCake from 'calypso/components/header-cake';
 import QuerySites from 'calypso/components/data/query-sites';
 import QueryConciergeInitial from 'calypso/components/data/query-concierge-initial';
 import QueryConciergeAppointmentDetails from 'calypso/components/data/query-concierge-appointment-details';
-import { Button, CompactCard } from '@automattic/components';
+import { Button } from '@automattic/components';
 import Main from 'calypso/components/main';
 import { localize } from 'i18n-calypso';
 import Confirmation from '../shared/confirmation';
@@ -25,6 +25,7 @@ import getConciergeScheduleId from 'calypso/state/selectors/get-concierge-schedu
 import getConciergeSignupForm from 'calypso/state/selectors/get-concierge-signup-form';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import { renderDisallowed } from '../shared/utils';
 
 class ConciergeCancel extends Component {
 	static propTypes = {
@@ -51,24 +52,6 @@ class ConciergeCancel extends Component {
 					<div className="cancel__placeholder-button is-placeholder" />
 				</div>
 			</div>
-		);
-	}
-
-	renderDisallowed() {
-		const { translate, siteSlug } = this.props;
-		return (
-			<>
-				<HeaderCake backHref={ `/me/quickstart/${ siteSlug }/book` }>
-					{ translate( 'Reschedule or cancel' ) }
-				</HeaderCake>
-				<CompactCard>
-					<div>
-						{ translate(
-							'Sorry, you cannot reschedule or cancel less than 60 minutes before the session.'
-						) }
-					</div>
-				</CompactCard>
-			</>
 		);
 	}
 
@@ -116,7 +99,7 @@ class ConciergeCancel extends Component {
 				const canChangeAppointment = appointmentDetails?.meta.canChangeAppointment;
 
 				if ( appointmentDetails && ! canChangeAppointment ) {
-					return this.renderDisallowed();
+					return renderDisallowed( translate, siteSlug );
 				}
 
 				return (
