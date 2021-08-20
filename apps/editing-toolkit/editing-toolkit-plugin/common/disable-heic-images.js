ff2e711aa831eb49ea053f0f281e25c0128ceea7
sixhours <calobee@gmail.com>
2021-08-20 15:26:36 -0400

    Try throwing an error to see if I can get it to work on Atomic

--- apps/editing-toolkit/editing-toolkit-plugin/common/disable-heic-images.js
+++ apps/editing-toolkit/editing-toolkit-plugin/common/disable-heic-images.js
@@ -1,16 +1,23 @@
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { createElement } from '@wordpress/element';
 import { addFilter } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
 
 const DisableHEICImages = createHigherOrderComponent( ( MediaPlaceholder ) => {
 	return ( props ) => {
 		props.onFilesPreUpload = ( files ) => {
 			if ( files ) {
-				return Object.values( files ).filter( ( file ) => {
-					if ( 'image/heic' !== file.type ) {
-						return file;
+				const error = __(
+					'HEIC images are not viewable in the editor. Please convert to a JPG, PNG, or GIF and try again.',
+					'full-site-editing'
+				);
+				Object.values( files ).filter( ( file ) => {
+					if ( 'image/heic' === file.type ) {
+						props.onError( error );
+						throw Error( error );
 					}
 				} );
+				return files;
 			}
 		};
 
