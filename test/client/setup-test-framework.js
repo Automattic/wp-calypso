8d3d96aea4ef6c5ecf9ef8bd13a6bbdf6718811c
Noah Allen <noahtallen@gmail.com>
2021-08-24 13:56:39 -0700

    Fixes for wp monorepo update
    Squashed commit of the following:
    
    commit 95ca82b
    Author: Sergio Cinos <sergio.cinos@gmail.com>
    Date:   Thu Jul 22 11:27:51 2021 +0200
    
        Add global CSS
    
    commit 1e0c99d
    Author: Sergio Cinos <sergio.cinos@gmail.com>
    Date:   Thu Jul 22 09:36:36 2021 +0200
    
        Do not import types-patch
    
    commit dee6bd6
    Author: Sergio Cinos <sergio.cinos@gmail.com>
    Date:   Thu Jul 22 09:29:26 2021 +0200
    
        Delete unused types patch
    
    commit 2465d15
    Author: Sergio Cinos <sergio.cinos@gmail.com>
    Date:   Thu Jul 22 09:18:20 2021 +0200
    
        Fix types

--- test/client/setup-test-framework.js
+++ test/client/setup-test-framework.js
@@ -58,3 +58,9 @@ jest.mock( 'sinon', () => {
 	}
 	return actualSinon;
 } );
+
+// This is used by @wordpress/components in https://github.com/WordPress/gutenberg/blob/trunk/packages/components/src/ui/utils/space.ts#L33
+// JSDOM or CSSDOM don't provide an implementation for it, so for now we have to mock it.
+global.CSS = {
+	supports: jest.fn(),
+};
