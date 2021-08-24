4cc63b9e9f71a258e7146511dc51c1dc3cfdb71b
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2021-08-24 12:59:00 +0200

    Do not attempt to retry if we've navigated outside wp.com

--- packages/calypso-e2e/src/lib/components/sidebar-component.ts
+++ packages/calypso-e2e/src/lib/components/sidebar-component.ts
@@ -48,25 +48,43 @@ export class SidebarComponent {
 			await this.openMobileSidebar();
 		}
 
+		const itemSelector = `${ selectors.sidebar } :text-is("${ item }"):visible`;
+		await this.scrollItemIntoViewIfNeeded( itemSelector );
+
+		if ( subitem ) {
+			// Click top-level item without waiting for navigation if targeting subitem.
+			await this.page.click( itemSelector );
+
+			const subitemSelector = `:text-is("${ subitem }"):visible:below(${ itemSelector })`;
+			await this.scrollItemIntoViewIfNeeded( subitemSelector );
+
+			await Promise.all( [ this.page.waitForNavigation(), this.page.click( subitemSelector ) ] );
+		} else {
+			await Promise.all( [ this.page.waitForNavigation(), this.page.click( itemSelector ) ] );
+		}
+
+		/**
+		 * Do not attempt to retry if we've navigated outside wp.com as the sidebar
+		 * is no longer present.
+		 */
+		const currentURL = await this.page.url();
+		if ( ! currentURL.startsWith( 'https://wordpress.com' ) ) {
+			return;
+		}
+
+		/**
+		 * Retry if the click missed the expected item. This can happen because of
+		 * the lazy-loading nature of the sidenav items.
+		 */
 		try {
-			const itemSelector = `${ selectors.sidebar } :text-is("${ item }"):visible`;
-			await this.scrollItemIntoViewIfNeeded( itemSelector );
-
-			if ( subitem ) {
-				// Click top-level item without waiting for navigation if targeting subitem.
-				await this.page.click( itemSelector );
-
-				const subitemSelector = `:text-is("${ subitem }"):visible:below(${ itemSelector })`;
-				await this.scrollItemIntoViewIfNeeded( subitemSelector );
-
-				await Promise.all( [ this.page.waitForNavigation(), this.page.click( subitemSelector ) ] );
-			} else {
-				await Promise.all( [ this.page.waitForNavigation(), this.page.click( itemSelector ) ] );
-			}
-			/**
-			 * Retry if the click missed the expected item. This can happen because of
-			 * the lazy-loading nature of the sidenav items.
-			 */
+			const selectedItemSelector = `${ selectors.sidebar } .selected :text-is("${
+				subitem || item
+			}")`;
+			await this.page.waitForSelector( selectedItemSelector, {
+				timeout: 3000,
+				state: 'attached',
+			} );
+			return;
 		} catch {
 			if ( retries === 0 ) {
 				const itemPath = subitem ? `${ item } > ${ subitem }` : item;
