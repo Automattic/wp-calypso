0af074f56908faf6164096e1654c6ed036ad3842
Vishnu Gopal <533+vishnugopal@users.noreply.github.com>
2021-08-24 11:08:22 +0530

    Substack Importer: Fix custom domain (#55275)
    * Support custom domains by removing .substack.com validation

--- client/lib/importer/importer-config.js
+++ client/lib/importer/importer-config.js
@@ -185,9 +185,6 @@ function getConfig( { siteTitle = '' } = {} ) {
 			invalidDescription: translate( 'Enter a valid Substack Newsletter URL (%(exampleUrl)s).', {
 				args: { exampleUrl: 'https://example-newsletter.substack.com/' },
 			} ),
-			validate: ( urlInput ) => {
-				return /^https:\/\/[\w-]+\.substack\.com\/?$/.test( urlInput.trim() );
-			},
 		},
 		weight: 0,
 	};
