c6c34cb910fd0c3c733746890a26fb9e8f34630f
Caleb Bauermeister <cbauerman@gmail.com>
2021-08-24 09:08:14 -0700

    update unit test for async

--- client/lib/analytics/test/google-analytics.js
+++ client/lib/analytics/test/google-analytics.js
@@ -28,6 +28,7 @@ jest.mock( 'calypso/lib/analytics/utils', () => ( {
 	isGoogleAnalyticsAllowed: () => true,
 	isPiiUrl: () => false,
 	mayWeTrackCurrentUserGdpr: () => true,
+	refreshCountryCodeCookieGdpr: () => null,
 } ) );
 
 jest.mock( '@automattic/calypso-analytics', () => ( {
@@ -41,9 +42,9 @@ describe( 'analytics/ga', () => {
 	describe( 'makeGoogleAnalyticsTrackingFunction', () => {
 		test( 'calls the wrapped function with passed arguments when enabled', () => {
 			const wrapped = jest.fn();
-			makeGoogleAnalyticsTrackingFunction( wrapped )( 'a', 'b', 'c' );
-
-			expect( wrapped ).toHaveBeenCalledWith( 'a', 'b', 'c' );
+			return makeGoogleAnalyticsTrackingFunction( wrapped )( 'a', 'b', 'c' ).then( () => {
+				expect( wrapped ).toHaveBeenCalledWith( 'a', 'b', 'c' );
+			} );
 		} );
 	} );
 } );
