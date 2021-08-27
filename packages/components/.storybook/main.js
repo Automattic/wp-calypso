de494689fedc0413699cbaded73d93e02d6fc360
Noah Allen <noahtallen@gmail.com>
2021-08-27 15:14:41 -0700

    Fix storybook

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -7,4 +7,16 @@ module.exports = {
 		check: false,
 		reactDocgen: false,
 	},
+	webpackFinal: async ( config, { configType } ) => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			// Storybook does not support Emotion 11, so resolve it manually.
+			// TODO: Remove once Storybook supports Emotion 11.
+			'@emotion/styled': path.join( process.cwd(), 'node_modules/@emotion/styled' ),
+			'@emotion/core': path.join( process.cwd(), 'node_modules/@emotion/react' ),
+			'@emotion-theming': path.join( process.cwd(), 'node_modules/@emotion/react' ),
+			'@emotion/react': path.join( process.cwd(), 'node_modules/@emotion/react' ),
+		};
+		return config;
+	},
 };
