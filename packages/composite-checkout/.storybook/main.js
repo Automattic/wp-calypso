2a6481061046efe7b56dc75cbadf44f214afc9fb
Noah Allen <noahtallen@gmail.com>
2021-08-27 15:14:41 -0700

    Fix storybook

--- packages/composite-checkout/.storybook/main.js
+++ packages/composite-checkout/.storybook/main.js
@@ -12,6 +12,12 @@ module.exports = {
 		config.resolve.alias = {
 			...config.resolve.alias,
 			'@automattic/composite-checkout': path.join( __dirname, '../src/public-api.ts' ),
+			// Storybook does not support Emotion 11, so resolve it manually.
+			// TODO: Remove once Storybook supports Emotion 11.
+			'@emotion/styled': path.join( process.cwd(), 'node_modules/@emotion/styled' ),
+			'@emotion/core': path.join( process.cwd(), 'node_modules/@emotion/react' ),
+			'@emotion-theming': path.join( process.cwd(), 'node_modules/@emotion/react' ),
+			'@emotion/react': path.join( process.cwd(), 'node_modules/@emotion/react' ),
 		};
 		config.resolve.mainFields = [ 'browser', 'calypso:src', 'module', 'main' ];
 		return config;
