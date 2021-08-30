65a9d76c350405aec46f859868d4bf0514364c1a
harshit <harshitgarg545@gmail.com>
2021-08-30 22:29:57 +0530

    extracting InfoTooltip

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -9,3 +9,4 @@ export { default as Ribbon } from './ribbon';
 export { default as RootChild } from './root-child';
 export { default as ScreenReaderText } from './screen-reader-text';
 export { default as Suggestions } from './suggestions';
+export { default as InfoTooltip } from '@automattic/domain-picker/src/components/info-tooltip';
