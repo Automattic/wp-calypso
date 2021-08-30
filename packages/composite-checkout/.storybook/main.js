6e1fecb141b8230fc1da2cbafcc126a6d1af2f45
Noah Allen <noahtallen@gmail.com>
2021-08-30 14:49:15 -0700

    Use require.resolve for storybook

--- packages/composite-checkout/.storybook/main.js
+++ packages/composite-checkout/.storybook/main.js
@@ -14,10 +14,10 @@ module.exports = {
 			'@automattic/composite-checkout': path.join( __dirname, '../src/public-api.ts' ),
 			// Storybook does not support Emotion 11, so resolve it manually.
 			// TODO: Remove once Storybook supports Emotion 11.
-			'@emotion/styled': path.join( process.cwd(), 'node_modules/@emotion/styled' ),
-			'@emotion/core': path.join( process.cwd(), 'node_modules/@emotion/react' ),
-			'@emotion-theming': path.join( process.cwd(), 'node_modules/@emotion/react' ),
-			'@emotion/react': path.join( process.cwd(), 'node_modules/@emotion/react' ),
+			'@emotion/styled': require.resolve( '@emotion/styled' ),
+			'@emotion/core': require.resolve( '@emotion/react' ),
+			'@emotion-theming': require.resolve( '@emotion/react' ),
+			'@emotion/react': require.resolve( '@emotion/react' ),
 		};
 		config.resolve.mainFields = [ 'browser', 'calypso:src', 'module', 'main' ];
 		return config;
