bb3645ff29dae65cf6e37e149a2f51c9cd164adc
Edwin Takahashi <egao@outlook.com>
2021-08-30 15:28:24 -0700

    Add new component CookieBannerComponent.

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -642,7 +642,7 @@ fun playwrightBuildType( viewportName: String, buildUuid: String ): BuildType {
 					export NODE_CONFIG="{\"calypsoBaseURL\":\"${'$'}{URL%/}\"}"
 					export DEBUG=pw:api
 
-					xvfb-run yarn jest --reporters=jest-teamcity --reporters=default --testNamePattern @parallel --maxWorkers=%E2E_WORKERS% specs/specs-playwright
+					for i in {1..100}; do xvfb-run yarn jest --reporters=jest-teamcity --reporters=default --testNamePattern @parallel --maxWorkers=%E2E_WORKERS% specs/specs-playwright/wp-likes__post-spec.js specs/specs-playwright/wp-notifications__trash-spec.js; done
 				""".trimIndent()
 				dockerImage = "%docker_image_e2e%"
 			}
