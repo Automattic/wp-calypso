07708114871f6af32089ec98863b72766356047c
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-09-01 13:24:35 -0500

    Add Rewind policy selectors to state/rewind/selectors

--- client/state/rewind/selectors/index.ts
+++ client/state/rewind/selectors/index.ts
@@ -1,5 +1,7 @@
 export { default as getInProgressBackupForSite } from './get-in-progress-backup-for-site';
 export { default as getSiteBackupStorageAvailable } from './get-site-backup-storage-available';
 export { default as getSiteBackupStorageUsed } from './get-site-backup-storage-used';
+export { default as getRewindPoliciesRequestStatus } from './get-rewind-policies-request-status';
+export { default as isRequestingRewindPolicies } from './is-requesting-rewind-policies';
 export { default as siteHasBackupInProgress } from './site-has-backup-in-progress';
 export { default as siteHasRealtimeBackups } from './site-has-realtime-backups';
