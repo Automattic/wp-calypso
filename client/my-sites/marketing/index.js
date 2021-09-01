c01b04cf4d767324431992755a8dffe3c5f68f93
Aneesh Devasthale <aneeshdevasthale@gmail.com>
2021-09-01 19:31:06 +0530

    Fixes routes

--- client/my-sites/marketing/index.js
+++ client/my-sites/marketing/index.js
@@ -29,13 +29,11 @@ export default function () {
 		'/sharing',
 		'/sharing/buttons',
 		'/marketing/business-tools',
-		'/marketing/do-it-for-me/landing',
-		'/marketing/do-it-for-me/site-info',
 	];
 
 	if ( config.isEnabled( 'difm-lite-pilot' ) ) {
 		paths.push( '/marketing/do-it-for-me/landing' );
-		paths.push( '/marketing/do-it-for-me/site-info-lite' );
+		paths.push( '/marketing/do-it-for-me/site-info' );
 	}
 
 	paths.forEach( ( path ) => page( path, ...[ siteSelection, sites, makeLayout, clientRender ] ) );
