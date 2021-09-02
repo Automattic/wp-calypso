2b5295bc550cedaa6767d254816dcd424893b832
Aneesh Devasthale <aneeshd16@users.noreply.github.com>
2021-09-02 18:24:07 +0530

    Adds form to DIFM Lite (#55892)
    * Do it for me initial files
    
    * Completed implementation flow
    
    * Adds test typeform embed
    
    * Fixes routes
    
    * Use translate for strings
    
    * Removes translate and memo usage
    
    Co-authored-by: Dehan <dehandecroos@gmail.com>

--- client/my-sites/marketing/do-it-for-me/site-info-collection.tsx
+++ client/my-sites/marketing/do-it-for-me/site-info-collection.tsx
@@ -1,51 +1,39 @@
-import { Button } from '@automattic/components';
-import { useShoppingCart } from '@automattic/shopping-cart';
+import config from '@automattic/calypso-config';
 import styled from '@emotion/styled';
+import { Widget } from '@typeform/embed-react';
 import page from 'page';
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import ActionPanel from 'calypso/components/action-panel';
 import ActionPanelBody from 'calypso/components/action-panel/body';
-import ActionPanelFooter from 'calypso/components/action-panel/footer';
+import CardHeading from 'calypso/components/card-heading';
 import FormattedHeader from 'calypso/components/formatted-header';
-import { fillInSingleCartItemAttributes } from 'calypso/lib/cart-values';
+import Spinner from 'calypso/components/spinner';
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
-import { getProductsList } from 'calypso/state/products-list/selectors';
+import { getCurrentUserName } from 'calypso/state/current-user/selectors';
 import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
-const TemporaryTypeformContainer = styled.div`
-	width: 95%;
-	height: 18em;
-	padding: 1em 0 0 1em;
-	background-color: #eee;
-	font-size: 2em;
-	color: #b8b8b8;
+function getTypeformId(): string {
+	return config< string >( 'difm_typeform_id' );
+}
+
+const LoadingContainer = styled.div`
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	height: 500px;
+	justify-content: center;
 `;
 
 function SiteInformationCollection() {
 	const selectedSiteSlug = useSelector( getSelectedSiteSlug );
-	const [ isAddingToBasket, setIsAddingToBasket ] = useState( false );
-	const products = useSelector( getProductsList );
+	const username = useSelector( getCurrentUserName );
+	const [ isFormSubmitted, setIsFormSubmitted ] = useState( false );
 
-	const { replaceProductsInCart } = useShoppingCart();
-
-	const onAddDIFMProductsToCart = async () => {
-		setIsAddingToBasket( true );
-		const difmProduct = fillInSingleCartItemAttributes(
-			{ product_slug: 'wp_difm_lite' },
-			products
-		);
-		const premiumPlan = fillInSingleCartItemAttributes(
-			{ product_slug: 'value_bundle' },
-			products
-		);
-		await replaceProductsInCart( [ difmProduct, premiumPlan ] );
-		page( `/marketing/do-it-for-me/site-info/${ selectedSiteSlug }` );
-	};
 	const onSubmit = async () => {
 		// After Submitting typeform checkout
-		await onAddDIFMProductsToCart();
-		page( `/checkout/${ selectedSiteSlug }` );
+		setIsFormSubmitted( true );
+		page( `/checkout/${ selectedSiteSlug }/wp_difm_lite` );
 	};
 
 	return (
@@ -53,17 +41,31 @@ function SiteInformationCollection() {
 			<ActionPanelBody>
 				<FormattedHeader
 					brandFont
-					headerText="Tell us more about your site"
-					subHeaderText="We need some basic details to build your site, you will also be able to get a glimpse of what your site will look like"
+					headerText={ 'Tell us more about your site' }
+					subHeaderText={
+						'We need some basic details to build your site, you will also be able to get a glimpse of what your site will look like'
+					}
 					align="left"
 				/>
-				<TemporaryTypeformContainer>Typeform Content goes here</TemporaryTypeformContainer>
+				{ isFormSubmitted ? (
+					<LoadingContainer>
+						<CardHeading tagName="h5" size={ 24 }>
+							{ 'Redirecting to Checkout…' }
+						</CardHeading>
+						<Spinner />
+					</LoadingContainer>
+				) : (
+					<Widget
+						hidden={ {
+							username,
+						} }
+						id={ getTypeformId() }
+						style={ { width: '100%', height: '500px', padding: '1em 0 0 1em' } }
+						onSubmit={ onSubmit }
+						disableAutoFocus={ true }
+					/>
+				) }
 			</ActionPanelBody>
-			<ActionPanelFooter>
-				<Button busy={ isAddingToBasket } primary onClick={ onSubmit }>
-					Checkout
-				</Button>
-			</ActionPanelFooter>
 		</ActionPanel>
 	);
 }
