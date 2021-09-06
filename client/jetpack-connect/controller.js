a0072cdc67ab51b04b1c0f27e71815afb89571f9
Renzo Canepa <rcanepag@gmail.com>
2021-09-06 16:20:32 -0300

    Include WPCOM Search to the list

--- client/jetpack-connect/controller.js
+++ client/jetpack-connect/controller.js
@@ -418,7 +418,7 @@ export function redirectToSiteLessCheckout( context, next ) {
 
 	if (
 		config.isEnabled( 'jetpack/siteless-checkout' ) &&
-		! [ PRODUCT_JETPACK_SEARCH, PRODUCT_JETPACK_SEARCH_MONTHLY ].includes( planSlug )
+		! JETPACK_SEARCH_PRODUCTS.includes( planSlug )
 	) {
 		if ( ! urlQueryArgs?.checkoutBackUrl ) {
 			urlQueryArgs.checkoutBackUrl = 'https://jetpack.com';
