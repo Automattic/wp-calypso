75a86fd9c58904ffa8d9e14fb34db9d6fd87f328
Sangeeth Sudheer <sangeeth96@users.noreply.github.com>
2021-09-06 19:50:56 +0530

    Domains: Add GSuite email upsell skip button (#55831)
    * Add skip button to gsuite card in email upsell page
    * Add busy prop to titan card skip button
    * Change form button label to "Skip"
    * Use the skip label prop for header button
    * Shorten primary button labels
    * Add back auto left margin for titan CTA
    * Reduce left margin of titan form skip button
    * Replace owl selector with .button + .button
    * Fix css properties order, add comment

--- client/my-sites/email/email-providers-comparison/index.jsx
+++ client/my-sites/email/email-providers-comparison/index.jsx
@@ -314,7 +314,10 @@ class EmailProvidersComparison extends React.Component {
 			gSuiteProduct,
 			hasCartDomain,
 			isGSuiteSupported,
+			onSkipClick,
 			selectedDomainName,
+			showSkipButton,
+			skipButtonLabel,
 			translate,
 		} = this.props;
 
@@ -352,7 +355,9 @@ class EmailProvidersComparison extends React.Component {
 				? newUsers( selectedDomainName )
 				: this.state.googleUsers;
 
-		const buttonLabel = this.isUpgrading()
+		const buttonLabel = this.isUpgrading() ? translate( 'Upgrade' ) : translate( 'Add' );
+
+		const expandButtonLabel = this.isUpgrading()
 			? translate( 'Upgrade to %(googleMailService)s', {
 					args: {
 						googleMailService: getGoogleMailServiceFamily(),
@@ -379,14 +384,21 @@ class EmailProvidersComparison extends React.Component {
 						users={ googleUsers }
 						onReturnKeyPress={ this.onGoogleFormReturnKeyPress }
 					>
-						<Button
-							className="email-providers-comparison__gsuite-user-list-action-continue"
-							primary
-							busy={ this.state.addingToCart }
-							onClick={ this.onGoogleConfirmNewUsers }
-						>
-							{ buttonLabel }
-						</Button>
+						<div className="email-providers-comparison__gsuite-user-list-actions-container">
+							<Button
+								primary
+								busy={ this.state.addingToCart }
+								onClick={ this.onGoogleConfirmNewUsers }
+							>
+								{ buttonLabel }
+							</Button>
+
+							{ showSkipButton && (
+								<Button busy={ this.state.addingToCart } onClick={ onSkipClick }>
+									{ skipButtonLabel }
+								</Button>
+							) }
+						</div>
 					</GSuiteNewUserList>
 				</FormFieldset>
 			) : null;
@@ -407,7 +419,7 @@ class EmailProvidersComparison extends React.Component {
 				onExpandedChange={ this.onExpandedStateChange }
 				onButtonClick={ this.onGoogleConfirmNewUsers }
 				showExpandButton={ this.isDomainEligibleForEmail( domain ) }
-				expandButtonLabel={ buttonLabel }
+				expandButtonLabel={ expandButtonLabel }
 				features={ getGoogleFeatures() }
 			/>
 		);
@@ -448,7 +460,9 @@ class EmailProvidersComparison extends React.Component {
 			<img src={ poweredByTitanLogo } alt={ translate( 'Powered by Titan' ) } />
 		);
 
-		const buttonLabel = this.isUpgrading()
+		const buttonLabel = this.isUpgrading() ? translate( 'Upgrade' ) : translate( 'Add' );
+
+		const expandButtonLabel = this.isUpgrading()
 			? translate( 'Upgrade to %(titanProductName)s', {
 					args: {
 						titanProductName: getTitanProductName(),
@@ -482,7 +496,15 @@ class EmailProvidersComparison extends React.Component {
 					{ buttonLabel }
 				</Button>
 
-				{ showSkipButton && <Button onClick={ onSkipClick }>{ skipButtonLabel }</Button> }
+				{ showSkipButton && (
+					<Button
+						className="email-providers-comparison__titan-mailbox-action-skip"
+						busy={ this.state.addingToCart }
+						onClick={ onSkipClick }
+					>
+						{ skipButtonLabel }
+					</Button>
+				) }
 			</TitanNewMailboxList>
 		);
 
@@ -501,7 +523,7 @@ class EmailProvidersComparison extends React.Component {
 				discount={ discount }
 				formFields={ formFields }
 				showExpandButton={ this.isDomainEligibleForEmail( domain ) }
-				expandButtonLabel={ buttonLabel }
+				expandButtonLabel={ expandButtonLabel }
 				features={ getTitanFeatures() }
 			/>
 		);
@@ -587,11 +609,11 @@ class EmailProvidersComparison extends React.Component {
 	}
 
 	renderHeaderSkipButton() {
-		const { showSkipButton, onSkipClick, translate } = this.props;
+		const { showSkipButton, onSkipClick, skipButtonLabel } = this.props;
 
 		return showSkipButton ? (
 			<Button compact borderless onClick={ onSkipClick }>
-				{ translate( 'Skip' ) }
+				{ skipButtonLabel }
 				<Gridicon icon={ 'arrow-right' } size={ 18 } />
 			</Button>
 		) : null;
