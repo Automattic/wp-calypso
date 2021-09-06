4fadd8fe95551ef87e5f560b48d923f216d8c72f
Michael Cain <michael@michaeldcain.com>
2021-09-06 10:30:29 -0400

    Nudge business plan, update upsell event name, and remove inaccessible upsell

--- client/my-sites/themes/single-site-wpcom.jsx
+++ client/my-sites/themes/single-site-wpcom.jsx
@@ -1,4 +1,4 @@
-import { PLAN_PREMIUM } from '@automattic/calypso-products';
+import { PLAN_BUSINESS } from '@automattic/calypso-products';
 import React from 'react';
 import { connect } from 'react-redux';
 import UpsellNudge from 'calypso/blocks/upsell-nudge';
@@ -21,38 +21,24 @@ const ConnectedSingleSiteWpcom = connectOptions( ( props ) => {
 	const upsellUrl = `/plans/${ siteSlug }`;
 	let upsellBanner = null;
 	if ( displayUpsellBanner ) {
-		if ( bannerLocationBelowSearch ) {
-			upsellBanner = (
-				<UpsellNudge
-					plan={ PLAN_PREMIUM }
-					customerType="business"
-					className="themes__showcase-banner"
-					title={ translate( 'Upload your own themes with our Business and eCommerce plans!' ) }
-					event="themes_plans_free_personal"
-					forceHref={ true }
-					showIcon={ true }
-				/>
-			);
-		} else {
-			upsellBanner = (
-				<UpsellNudge
-					plan={ PLAN_PREMIUM }
-					title={ translate( 'Upload your own themes with our Business and eCommerce plans!' ) }
-					description={ translate(
-						'Get advanced customization, more storage space, and video support along with the ability to upload any theme.'
-					) }
-					event="themes_plans_free_personal"
-					showIcon={ true }
-				/>
-			);
-		}
+		upsellBanner = (
+			<UpsellNudge
+				plan={ PLAN_BUSINESS }
+				customerType="business"
+				className="themes__showcase-banner"
+				title={ translate( 'Upload your own themes with our Business and eCommerce plans!' ) }
+				event="calypso_themes_list_install_themes"
+				forceHref={ true }
+				showIcon={ true }
+			/>
+		);
 	}
+
 	return (
 		<Main fullWidthLayout className="themes">
 			<SidebarNavigation />
 			<ThemesHeader />
 			<CurrentTheme siteId={ siteId } />
-			{ bannerLocationBelowSearch ? null : upsellBanner }
 
 			<ThemeShowcase
 				{ ...props }
