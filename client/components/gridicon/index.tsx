204fb1bc81f8ba5a4e670faa1f001e7f67043a16
Jarda Snajdr <jsnajdr@gmail.com>
2021-09-07 10:44:29 +0200

    Gridicon: fix TS errors, simplify className calculation

--- client/components/gridicon/index.tsx
+++ client/components/gridicon/index.tsx
@@ -6,33 +6,31 @@ import {
 } from 'gridicons/dist/util/icons-offset';
 import spritePath from 'gridicons/svg-sprite/gridicons.svg';
 import React from 'react';
-import { Assign } from 'utility-types';
-
-function needsOffset( name: string, icons: string[] ) {
-	return icons.indexOf( name ) >= 0;
-}
+import type { ReactNode } from 'react';
+import type { Assign } from 'utility-types';
 
 interface Props {
 	icon: string;
 	size?: number;
+	title?: ReactNode;
 }
+
 type AllProps = Assign< React.SVGProps< SVGSVGElement >, Props >;
 
 const Gridicon = React.forwardRef< SVGSVGElement, AllProps >( ( props: AllProps, ref ) => {
-	const { size = 24, icon, onClick, className, title, ...otherProps } = props;
+	const { size = 24, icon, className, title, ...otherProps } = props;
 	const isModulo18 = size % 18 === 0;
 
 	// Using a missing icon doesn't produce any errors, just a blank icon, which is the exact intended behaviour.
 	// This means we don't need to perform any checks on the icon name.
 	const iconName = `gridicons-${ icon }`;
-	const offsetClasses = isModulo18
-		? [
-				needsOffset( iconName, iconsThatNeedOffset ) ? 'needs-offset' : false,
-				needsOffset( iconName, iconsThatNeedOffsetX ) ? 'needs-offset-x' : false,
-				needsOffset( iconName, iconsThatNeedOffsetY ) ? 'needs-offset-y' : false,
-		  ]
-		: [];
-	const iconClass = classnames( 'gridicon', iconName, className, ...offsetClasses );
+
+	const iconClass = classnames( 'gridicon', iconName, className, {
+		'needs-offset': isModulo18 && iconsThatNeedOffset.includes( iconName ),
+		'needs-offset-x': isModulo18 && iconsThatNeedOffsetX.includes( iconName ),
+		'needs-offset-y': isModulo18 && iconsThatNeedOffsetY.includes( iconName ),
+	} );
+
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -40,7 +38,6 @@ const Gridicon = React.forwardRef< SVGSVGElement, AllProps >( ( props: AllProps,
 			className={ iconClass }
 			height={ size }
 			width={ size }
-			onClick={ onClick }
 			ref={ ref }
 			{ ...otherProps }
 		>
