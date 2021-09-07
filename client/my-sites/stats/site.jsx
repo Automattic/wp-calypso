2f9c22ad0ed5d3ef3e0671280cabd4144c4002b1
Caroline Moore <caroline@a8c.com>
2021-09-07 13:16:53 -0400

    Stats: Add banner for Parse.ly (#55993)

--- client/my-sites/stats/site.jsx
+++ client/my-sites/stats/site.jsx
@@ -1,4 +1,4 @@
-import { FEATURE_WORDADS_INSTANT } from '@automattic/calypso-products';
+import { Card, Button } from '@automattic/components';
 import { localize, translate } from 'i18n-calypso';
 import { find } from 'lodash';
 import page from 'page';
@@ -6,10 +6,10 @@ import { parse as parseQs, stringify as stringifyQs } from 'qs';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import titlecase from 'to-title-case';
+import parselyIcon from 'calypso/assets/images/icons/parsely-logo.svg';
 import JetpackBackupCredsBanner from 'calypso/blocks/jetpack-backup-creds-banner';
 import PrivacyPolicyBanner from 'calypso/blocks/privacy-policy-banner';
 import StatsNavigation from 'calypso/blocks/stats-navigation';
-import Banner from 'calypso/components/banner';
 import DocumentHead from 'calypso/components/data/document-head';
 import QueryJetpackModules from 'calypso/components/data/query-jetpack-modules';
 import QueryKeyringConnections from 'calypso/components/data/query-keyring-connections';
@@ -21,6 +21,8 @@ import JetpackColophon from 'calypso/components/jetpack-colophon';
 import Main from 'calypso/components/main';
 import StickyPanel from 'calypso/components/sticky-panel';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import TrackComponentView from 'calypso/lib/analytics/track-component-view';
+import { preventWidows } from 'calypso/lib/formatting';
 import memoizeLast from 'calypso/lib/memoize-last';
 import SidebarNavigation from 'calypso/my-sites/sidebar-navigation';
 import {
@@ -29,15 +31,10 @@ import {
 	withAnalytics,
 } from 'calypso/state/analytics/actions';
 import { activateModule } from 'calypso/state/jetpack/modules/actions';
-import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
 import getCurrentRouteParameterized from 'calypso/state/selectors/get-current-route-parameterized';
 import isJetpackModuleActive from 'calypso/state/selectors/is-jetpack-module-active';
-import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
-import isVipSite from 'calypso/state/selectors/is-vip-site';
-import { hasFeature } from 'calypso/state/sites/plans/selectors';
-import { isJetpackSite, getSitePlanSlug, getSiteOption } from 'calypso/state/sites/selectors';
+import { isJetpackSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
-import Cloudflare from './cloudflare';
 import ChartTabs from './stats-chart-tabs';
 import Countries from './stats-countries';
 import DatePicker from './stats-date-picker';
@@ -144,24 +141,17 @@ class StatsSite extends Component {
 		}
 	};
 
+	parselyClick = () => {
+		this.props.recordTracksEvent( 'calypso_stats_parsely_banner_click' );
+	};
+
 	renderStats() {
-		const {
-			date,
-			hasWordAds,
-			planSupportsWordAdsInstantFeature,
-			siteId,
-			slug,
-			isAdmin,
-			isJetpack,
-			isAtomic,
-			isVip,
-		} = this.props;
+		const { date, siteId, slug, isJetpack } = this.props;
 
 		const queryDate = date.format( 'YYYY-MM-DD' );
 		const { period, endOf } = this.props.period;
 		const moduleStrings = statsStrings();
 		let fileDownloadList;
-		const isJetpackNonAtomic = isJetpack && ! isAtomic;
 
 		const query = memoizedQuery( period, endOf );
 
@@ -205,8 +195,6 @@ class StatsSite extends Component {
 					siteId={ siteId }
 					slug={ slug }
 				/>
-
-				{ ! isVip && isAdmin && ! hasWordAds && ! isJetpackNonAtomic && <Cloudflare /> }
 
 				<div id="my-stats-content">
 					<ChartTabs
@@ -300,28 +288,36 @@ class StatsSite extends Component {
 							/>
 						</div>
 					</div>
-					{ ! isVip && isAdmin && ! hasWordAds && (
-						<Banner
-							className="stats__upsell-nudge"
-							icon="star"
-							title={ translate( 'Start earning money now' ) }
-							description={ translate(
-								'Accept payments for just about anything and turn your website into a reliable source of income with payments and ads.'
-							) }
-							callToAction={ planSupportsWordAdsInstantFeature ? translate( 'Learn more' ) : null }
-							href={ `/earn/${ slug }` }
-							dismissPreferenceName={
-								planSupportsWordAdsInstantFeature ? `stats-earn-nudge-wordads-${ siteId }` : null
-							}
-							event="stats_earn_nudge"
-							tracksImpressionName="calypso_upgrade_nudge_impression"
-							tracksClickName="calypso_upgrade_nudge_cta_click"
-							showIcon={ true }
-							jetpack={ false }
-							horizontal
-						/>
-					) }
 				</div>
+				<Card className="stats__parsely-banner">
+					<TrackComponentView eventName="calypso_stats_parsely_banner_view" />
+					<img src={ parselyIcon } alt="" aria-hidden="true" />
+					<div>
+						<FormattedHeader
+							brandFont
+							className="stats__parsely-banner-header"
+							headerText={ preventWidows(
+								translate( 'Discover more stats with Parse.ly Analytics' )
+							) }
+							align="left"
+						/>
+						<p>
+							{ preventWidows(
+								translate(
+									"Need deeper insights? Parse.ly Analytics makes it easy to understand the full impact of your content. {{br/}}Measure what's driving awareness, engagement, and conversions.",
+									{
+										components: {
+											br: <br />,
+										},
+									}
+								)
+							) }
+						</p>
+					</div>
+					<Button primary href="https://parse.ly" onClick={ this.parselyClick } target="_blank">
+						{ translate( 'Learn more' ) }
+					</Button>
+				</Card>
 				<JetpackColophon />
 			</>
 		);
@@ -379,23 +375,15 @@ export default connect(
 	( state ) => {
 		const siteId = getSelectedSiteId( state );
 		const isJetpack = isJetpackSite( state, siteId );
-		const isVip = isVipSite( state, siteId );
-		const isAtomic = isSiteAutomatedTransfer( state, siteId );
 		const showEnableStatsModule =
 			siteId && isJetpack && isJetpackModuleActive( state, siteId, 'stats' ) === false;
 		return {
-			isAdmin: canCurrentUser( state, siteId, 'manage_options' ),
 			isJetpack,
-			isAtomic,
-			hasWordAds: getSiteOption( state, siteId, 'wordads' ),
 			siteId,
-			isVip,
 			slug: getSelectedSiteSlug( state ),
-			planSlug: getSitePlanSlug( state, siteId ),
-			planSupportsWordAdsInstantFeature: hasFeature( state, siteId, FEATURE_WORDADS_INSTANT ),
 			showEnableStatsModule,
 			path: getCurrentRouteParameterized( state, siteId ),
 		};
 	},
-	{ recordGoogleEvent, enableJetpackStatsModule }
+	{ recordGoogleEvent, enableJetpackStatsModule, recordTracksEvent }
 )( localize( StatsSite ) );
