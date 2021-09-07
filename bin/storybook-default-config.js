45b08df85ad7d35cb0a9a63a1182cb8f8926cd96
Noah Allen <noahtallen@gmail.com>
2021-09-07 13:17:27 -0700

    Improve import logic and comments

--- bin/storybook-default-config.js
+++ bin/storybook-default-config.js
@@ -1,8 +1,9 @@
 const { dirname } = require( 'path' );
 
-// require.resolve returns a path to a file in /dist. To get to the root of the
-// package, we need to go up the directory tree twice.
-const toPath = ( module ) => dirname( dirname( require.resolve( module ) ) );
+// Using `require.resolve` by itself gives a path to a file in /dist. Instead,
+// resolve `/package.json` and then get the containing directory to find the root
+// of the given module.
+const findModule = ( module ) => dirname( require.resolve( module + '/package.json' ) );
 
 module.exports = function storybookDefaultConfig( { stories, webpackAliases = {} } = {} ) {
 	return {
@@ -22,12 +23,19 @@ module.exports = function storybookDefaultConfig( { stories, webpackAliases = {}
 		webpackFinal: async ( config ) => {
 			config.resolve.alias = {
 				...config.resolve.alias,
-				// Storybook does not support Emotion 11, so resolve it manually.
-				// TODO: Remove once Storybook supports Emotion 11.
-				'@emotion/styled': toPath( '@emotion/styled' ),
-				'@emotion/core': toPath( '@emotion/react' ),
-				'@emotion-theming': toPath( '@emotion/react' ),
-				'@emotion/react': toPath( '@emotion/react' ),
+				/**
+				 * Storybook manually resolves emotion 10 internally. To render
+				 * a package using Emotion 11, we must manually alias Emotion
+				 * imports to use the v11 packages instead of v10.
+				 *
+				 * @todo Remove once Storybook supports Emotion 11.
+				 * @see https://github.com/storybookjs/storybook/issues/12262
+				 * @see https://github.com/storybookjs/storybook/pull/13300#issuecomment-783268111
+				 */
+				'@emotion/styled': findModule( '@emotion/styled' ),
+				'@emotion/core': findModule( '@emotion/react' ),
+				'@emotion-theming': findModule( '@emotion/react' ),
+				'@emotion/react': findModule( '@emotion/react' ),
 				...webpackAliases,
 			};
 			config.resolve.mainFields = [ 'browser', 'calypso:src', 'module', 'main' ];
