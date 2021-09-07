09027f133459d28e123b7da8f984ec4d98c01d55
dpasque <dan.speckhard.pasque@automattic.com>
2021-09-07 10:42:03 -0500

    Try jest-runner-groups as a way for e2e suites

--- test/e2e/specs/specs-playwright/wp-blocks__coblocks-gutter-spec.ts
+++ test/e2e/specs/specs-playwright/wp-blocks__coblocks-gutter-spec.ts
@@ -1,3 +1,8 @@
+/**
+ * @group gutenberg
+ * @group coblocks
+ */
+
 import {
 	setupHooks,
 	DataHelper,
--- test/e2e/specs/specs-playwright/wp-blocks__coblocks.ts
+++ test/e2e/specs/specs-playwright/wp-blocks__coblocks.ts
@@ -1,3 +1,8 @@
+/**
+ * @group gutenberg
+ * @group coblocks
+ */
+
 import path from 'path';
 import {
 	setupHooks,
