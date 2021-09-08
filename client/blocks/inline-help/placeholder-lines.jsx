17528f6186303b16f3f3f30052f663be3d779b7e
Markus Dobmann <m.do@posteo.net>
2021-09-08 14:52:20 +0200

    placeholder-lines: just use `n` as `key`

--- client/blocks/inline-help/placeholder-lines.jsx
+++ client/blocks/inline-help/placeholder-lines.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default ( { lines = 4 } ) => (
 	<div className="inline-help__results-placeholder">
 		{ Array.from( { length: lines }, ( _, n ) => (
-			<div key={ `line-${ n }` } className="inline-help__results-placeholder-item" />
+			<div key={ n } className="inline-help__results-placeholder-item" />
 		) ) }
 	</div>
 );
