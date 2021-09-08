22a5a7bd57a2920b3fbae93c9758685429489cfc
Payton Swick <payton@foolord.com>
2021-09-08 16:56:37 -0400

    Add stripe to PaymentProcessorOptions

--- client/my-sites/checkout/composite-checkout/types/payment-processors.ts
+++ client/my-sites/checkout/composite-checkout/types/payment-processors.ts
@@ -4,12 +4,13 @@ import type { ReactStandardAction } from '../types/analytics';
 import type { StripeConfiguration } from '@automattic/calypso-stripe';
 import type { ResponseCart } from '@automattic/shopping-cart';
 import type { ManagedContactDetails } from '@automattic/wpcom-checkout';
-import type { StripeCardNumberElement } from '@stripe/stripe-js';
+import type { Stripe, StripeCardNumberElement } from '@stripe/stripe-js';
 
 export interface PaymentProcessorOptions {
 	includeDomainDetails: boolean;
 	includeGSuiteDetails: boolean;
 	createUserAndSiteBeforeTransaction: boolean;
+	stripe: Stripe | null;
 	stripeConfiguration: StripeConfiguration | null;
 	recordEvent: ( action: ReactStandardAction ) => void;
 	reduxDispatch: ReturnType< typeof useDispatch >;
