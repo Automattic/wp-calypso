091f787270908173b3eee109ed37a20cbe9612ea
Edwin Takahashi <egao@outlook.com>
2021-09-09 09:58:23 -0700

    Playwright: use an interface for passing TestFile paths. (#55693)
    * Define and implement interface for TestFile.
    - update Media (Upload) spec.
    - update CoBlocks spec.
    - update Blocks Media (Upload) spec.
    
    * Correct comments referencing old way of obtaining file names.
    Move Media Upload spec to TypeScript.
    
    Update Media (Edit) spec
    
    * Refactor how source test file paths are handled.
    - new file `constants.js` to hold the source paths for the test files.
    - call `createTestFile` by default and provide path.
    - refactor the createTestFile method to use better variable names.
    
    Revise behavior when hook fails.
    
    Revise environment behavior again.
    - set the `hookFailed` attribute to true if hook failure is experienced.
    - add clause at `test_start` event to check for hook failure, and if detected, mark rest of the test cases as failed.
    
    Rebase.

--- test/e2e/specs/specs-playwright/wp-blocks__coblocks.ts
+++ test/e2e/specs/specs-playwright/wp-blocks__coblocks.ts
@@ -2,7 +2,6 @@
  * @group gutenberg
  */
 
-import path from 'path';
 import {
 	setupHooks,
 	DataHelper,
@@ -15,14 +14,16 @@ import {
 	HeroBlock,
 	ClicktoTweetBlock,
 	LogosBlock,
+	TestFile,
 } from '@automattic/calypso-e2e';
 import { Page } from 'playwright';
+import { TEST_IMAGE_PATH } from '../constants';
 
-describe( DataHelper.createSuiteTitle( 'Blocks: CoBlocks' ), () => {
+describe( DataHelper.createSuiteTitle( 'Blocks: CoBlocks' ), function () {
 	let gutenbergEditorPage: GutenbergEditorPage;
 	let pricingTableBlock: PricingTableBlock;
 	let page: Page;
-	let logoImage: string;
+	let logoImage: TestFile;
 
 	// Test data
 	const pricingTableBlockPrice = 888;
@@ -30,12 +31,12 @@ describe( DataHelper.createSuiteTitle( 'Blocks: CoBlocks' ), () => {
 	const clicktoTweetBlockTweet =
 		'The foolish man seeks happiness in the distance. The wise grows it under his feet. — James Oppenheim';
 
-	setupHooks( ( args ) => {
+	setupHooks( ( args: { page: Page } ) => {
 		page = args.page;
 	} );
 
 	beforeAll( async () => {
-		logoImage = await MediaHelper.createTestImage();
+		logoImage = await MediaHelper.createTestFile( TEST_IMAGE_PATH );
 	} );
 
 	it( 'Log in', async function () {
@@ -78,7 +79,7 @@ describe( DataHelper.createSuiteTitle( 'Blocks: CoBlocks' ), () => {
 	it( `Insert ${ LogosBlock.blockName } block and set image`, async function () {
 		const blockHandle = await gutenbergEditorPage.addBlock( LogosBlock.blockName );
 		const logosBlock = new LogosBlock( blockHandle );
-		await logosBlock.upload( logoImage );
+		await logosBlock.upload( logoImage.fullpath );
 	} );
 
 	it( 'Publish and visit post', async function () {
@@ -104,6 +105,6 @@ describe( DataHelper.createSuiteTitle( 'Blocks: CoBlocks' ), () => {
 	);
 
 	it( `Confirm Logos block is visible in published post`, async () => {
-		await LogosBlock.validatePublishedContent( page, path.parse( logoImage ).name );
+		await LogosBlock.validatePublishedContent( page, [ logoImage.filename ] );
 	} );
 } );
--- test/e2e/specs/specs-playwright/wp-blocks__media-spec.ts
+++ test/e2e/specs/specs-playwright/wp-blocks__media-spec.ts
@@ -3,7 +3,6 @@
  * @group gutenberg
  */
 
-import path from 'path';
 import {
 	setupHooks,
 	DataHelper,
@@ -14,13 +13,15 @@ import {
 	ImageBlock,
 	AudioBlock,
 	FileBlock,
+	TestFile,
 } from '@automattic/calypso-e2e';
 import { Page } from 'playwright';
+import { TEST_IMAGE_PATH, TEST_AUDIO_PATH } from '../constants';
 
-describe( DataHelper.createSuiteTitle( 'Blocks: Media (Upload)' ), () => {
+describe( DataHelper.createSuiteTitle( 'Blocks: Media (Upload)' ), function () {
 	let gutenbergEditorPage: GutenbergEditorPage;
 	let page: Page;
-	let testFiles: { image: string; image_reserved_name: string; audio: string };
+	let testFiles: { image: TestFile; image_reserved_name: TestFile; audio: TestFile };
 
 	setupHooks( ( args ) => {
 		page = args.page;
@@ -28,12 +29,11 @@ describe( DataHelper.createSuiteTitle( 'Blocks: Media (Upload)' ), () => {
 
 	beforeAll( async () => {
 		testFiles = {
-			image: await MediaHelper.createTestImage(),
-			image_reserved_name: await MediaHelper.createTestFile( {
-				sourceFileName: 'image0.jpg',
-				testFileName: 'filewith#?#?reservedurlchars',
+			image: await MediaHelper.createTestFile( TEST_IMAGE_PATH ),
+			image_reserved_name: await MediaHelper.createTestFile( TEST_IMAGE_PATH, {
+				postfix: 'filewith#?#?reservedurlchars',
 			} ),
-			audio: await MediaHelper.createTestAudio(),
+			audio: await MediaHelper.createTestFile( TEST_AUDIO_PATH ),
 		};
 	} );
 
@@ -55,47 +55,49 @@ describe( DataHelper.createSuiteTitle( 'Blocks: Media (Upload)' ), () => {
 	it( `${ ImageBlock.blockName } block: upload image file`, async function () {
 		const blockHandle = await gutenbergEditorPage.addBlock( ImageBlock.blockName );
 		const imageBlock = new ImageBlock( blockHandle );
-		await imageBlock.upload( testFiles.image );
+		await imageBlock.upload( testFiles.image.fullpath );
 	} );
 
 	it( `${ ImageBlock.blockName } block: upload image file with reserved URL characters`, async function () {
 		const blockHandle = await gutenbergEditorPage.addBlock( ImageBlock.blockName );
 		const imageBlock = new ImageBlock( blockHandle );
-		await imageBlock.upload( testFiles.image_reserved_name );
+		await imageBlock.upload( testFiles.image_reserved_name.fullpath );
 	} );
 
 	it( `${ AudioBlock.blockName } block: upload audio file`, async function () {
 		const blockHandle = await gutenbergEditorPage.addBlock( AudioBlock.blockName );
 		const audioBlock = new AudioBlock( blockHandle );
-		await audioBlock.upload( testFiles.audio );
+		await audioBlock.upload( testFiles.audio.fullpath );
 	} );
 
 	it( `${ FileBlock.blockName } block: upload audio file`, async function () {
 		const blockHandle = await gutenbergEditorPage.addBlock( FileBlock.blockName );
 		const fileBlock = new FileBlock( blockHandle );
-		await fileBlock.upload( testFiles.audio );
+		await fileBlock.upload( testFiles.audio.fullpath );
 	} );
 
 	it( 'Publish and visit post', async function () {
-		await gutenbergEditorPage.publish( { visit: true } );
+		// Must save as draft first to bypass issue with post-publish panel being auto-dismissed
+		// after publishing. May be related to the following issue?
+		// See https://github.com/Automattic/wp-calypso/issues/54421.
+		await gutenbergEditorPage.publish( { visit: true, saveDraft: true } );
 	} );
 
-	it( `Confirm Image block is visible in published post`, async () => {
-		await ImageBlock.validatePublishedContent( page, path.parse( testFiles.image ).name );
+	it( `Confirm Image block is visible in published post`, async function () {
+		await ImageBlock.validatePublishedContent( page, [ testFiles.image.filename ] );
 	} );
 
-	it( `Confirm Image block is visible in published post (reserved name)`, async () => {
-		await ImageBlock.validatePublishedContent(
-			page,
-			path.parse( testFiles.image_reserved_name ).name.replace( /[^a-zA-Z ]/g, '' )
-		);
+	it( `Confirm Image block is visible in published post (reserved name)`, async function () {
+		await ImageBlock.validatePublishedContent( page, [
+			testFiles.image_reserved_name.filename.replace( /[^a-zA-Z ]/g, '' ),
+		] );
 	} );
 
-	it( `Confirm Audio block is visible in published post`, async () => {
+	it( `Confirm Audio block is visible in published post`, async function () {
 		await AudioBlock.validatePublishedContent( page );
 	} );
 
-	it( `Confirm File block is visible in published post`, async () => {
-		await FileBlock.validatePublishedContent( page, path.parse( testFiles.audio ).name );
+	it( `Confirm File block is visible in published post`, async function () {
+		await FileBlock.validatePublishedContent( page, [ testFiles.audio.filename ] );
 	} );
 } );
