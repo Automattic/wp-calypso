9aca375f386894f35e2774435dd0e40d3a9e9443
Payton Swick <payton@foolord.com>
2021-09-09 19:55:21 -0400

    Remove unused and not required props from CheckoutProvider

--- client/layout/masterbar/masterbar-cart.tsx
+++ client/layout/masterbar/masterbar-cart.tsx
@@ -5,8 +5,8 @@ import { CheckoutProvider, CheckoutErrorBoundary, Button } from '@automattic/com
 import { useShoppingCart } from '@automattic/shopping-cart';
 import { useTranslate } from 'i18n-calypso';
 import page from 'page';
-import React, { useRef, useState, useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 /**
  * Internal dependencies
  */
@@ -15,7 +15,6 @@ import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopp
 import CartMessages from 'calypso/my-sites/checkout/cart/cart-messages';
 import { CheckoutSummaryTotal } from 'calypso/my-sites/checkout/composite-checkout/components/wp-checkout-order-summary';
 import { WPOrderReviewLineItems } from 'calypso/my-sites/checkout/composite-checkout/components/wp-order-review-line-items';
-import { errorNotice, infoNotice, successNotice } from 'calypso/state/notices/actions';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 import MasterbarItem from './item';
 
@@ -63,8 +62,6 @@ function MasterbarCart( { children, tooltip }: MasterbarCartProps ): JSX.Element
 	);
 }
 
-function noop() {} // eslint-disable-line @typescript-eslint/no-empty-function
-
 function MasterbarCartCount( { productsInCart }: { productsInCart: number } ): JSX.Element {
 	return (
 		<div className="masterbar-cart__count-wrapper">
@@ -86,37 +83,10 @@ function MasterbarCartContents( { selectedSiteSlug }: { selectedSiteSlug: string
 		const checkoutUrl = `/checkout/${ selectedSiteSlug }`;
 		page( checkoutUrl );
 	};
-	const reduxDispatch = useDispatch();
-	const showErrorMessage = useCallback(
-		( error ) => {
-			const message = error && error.toString ? error.toString() : error;
-			reduxDispatch( errorNotice( message ) );
-		},
-		[ reduxDispatch ]
-	);
-	const showInfoMessage = useCallback(
-		( message ) => {
-			reduxDispatch( infoNotice( message ) );
-		},
-		[ reduxDispatch ]
-	);
-	const showSuccessMessage = useCallback(
-		( message ) => {
-			reduxDispatch( successNotice( message ) );
-		},
-		[ reduxDispatch ]
-	);
 	const isDisabled = isLoading || isPendingUpdate;
 
 	return (
-		<CheckoutProvider
-			paymentMethods={ [] }
-			paymentProcessors={ {} }
-			onPaymentComplete={ noop }
-			showErrorMessage={ showErrorMessage }
-			showInfoMessage={ showInfoMessage }
-			showSuccessMessage={ showSuccessMessage }
-		>
+		<CheckoutProvider paymentMethods={ [] } paymentProcessors={ {} }>
 			<CartMessages isLoadingCart={ isLoading } cart={ responseCart } />
 			<div className="masterbar-cart__content-wrapper">
 				<div className="masterbar-cart__content-header">
