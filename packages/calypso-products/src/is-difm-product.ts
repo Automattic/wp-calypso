0a09cf9eb6d2e134b00bc3e9217db23aa9a0a60f
Dehan <dehandecroos@gmail.com>
2021-09-09 11:44:48 +0530

    Add test for upsell nudge prevention and other fixes

--- packages/calypso-products/src/is-difm-product.ts
+++ packages/calypso-products/src/is-difm-product.ts
@@ -1,7 +1,7 @@
 import { WPCOM_DIFM_LITE } from './constants';
 import { formatProduct } from './format-product';
 
-export function isDIFMProduct( product: any ): boolean {
+export function isDIFMProduct( product: { product_slug?: string; productSlug?: string } ): boolean {
 	product = formatProduct( product );
 
 	return product.product_slug === WPCOM_DIFM_LITE;
