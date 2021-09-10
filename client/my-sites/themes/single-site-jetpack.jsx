f9cb5991d2ac56f529e4f1edc141992069daa084
Michael Cain <michael@michaeldcain.com>
2021-09-10 14:39:54 -0400

    Remove Jetpack upsell nudge

--- client/my-sites/themes/single-site-jetpack.jsx
+++ client/my-sites/themes/single-site-jetpack.jsx
@@ -1,22 +1,15 @@
-import {
-	FEATURE_UPLOAD_THEMES,
-	PLAN_BUSINESS,
-	PLAN_JETPACK_SECURITY_REALTIME,
-} from '@automattic/calypso-products';
+import { FEATURE_UPLOAD_THEMES, PLAN_BUSINESS } from '@automattic/calypso-products';
 import { pickBy } from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import UpsellNudge from 'calypso/blocks/upsell-nudge';
 import Main from 'calypso/components/main';
-import { isPartnerPurchase } from 'calypso/lib/purchases';
 import SidebarNavigation from 'calypso/my-sites/sidebar-navigation';
 import CurrentTheme from 'calypso/my-sites/themes/current-theme';
-import { getByPurchaseId } from 'calypso/state/purchases/selectors';
 import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 import { getCurrentPlan, isRequestingSitePlans } from 'calypso/state/sites/plans/selectors';
 import { isJetpackSiteMultiSite } from 'calypso/state/sites/selectors';
 import { getLastThemeQuery, getThemesFoundForQuery } from 'calypso/state/themes/selectors';
-import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import { addTracking } from './helpers';
 import { connectOptions } from './theme-options';
 import ThemeShowcase from './theme-showcase';
@@ -44,7 +37,6 @@ const ConnectedSingleSiteJetpack = connectOptions( ( props ) => {
 		filter,
 		getScreenshotOption,
 		isAtomic,
-		purchase,
 		showWpcomThemesList,
 		search,
 		siteId,
@@ -52,54 +44,33 @@ const ConnectedSingleSiteJetpack = connectOptions( ( props ) => {
 		tier,
 		translate,
 		requestingSitePlans,
-		siteSlug,
 	} = props;
 
-	const isPartnerPlan = purchase && isPartnerPurchase( purchase );
+	const displayUpsellBanner = isAtomic && ! requestingSitePlans && currentPlan;
 
-	const displayUpsellBanner = ! requestingSitePlans && currentPlan;
-
-	let upsellBanner = null;
-	if ( isAtomic ) {
-		upsellBanner = (
-			<UpsellNudge
-				className="themes__showcase-banner"
-				event="calypso_themes_list_install_themes"
-				feature={ FEATURE_UPLOAD_THEMES }
-				plan={ PLAN_BUSINESS }
-				title={ translate( 'Upload your own themes with our Business and eCommerce plans!' ) }
-				forceHref={ true }
-				showIcon={ true }
-			/>
-		);
-	} else {
-		upsellBanner = (
-			<UpsellNudge
-				forceDisplay
-				title={ translate( 'Upload your own themes' ) }
-				description={ translate(
-					'In addition to uploading your own themes, get comprehensive WordPress' +
-						' security, real-time backups, and unlimited video hosting.'
-				) }
-				event="themes_plans_free_personal_premium"
-				showIcon={ true }
-				href={ `/checkout/${ siteSlug }/${ PLAN_JETPACK_SECURITY_REALTIME }` }
-			/>
-		);
-	}
+	const upsellBanner = (
+		<UpsellNudge
+			className="themes__showcase-banner"
+			event="calypso_themes_list_install_themes"
+			feature={ FEATURE_UPLOAD_THEMES }
+			plan={ PLAN_BUSINESS }
+			title={ translate( 'Upload your own themes with our Business and eCommerce plans!' ) }
+			forceHref={ true }
+			showIcon={ true }
+		/>
+	);
 
 	return (
 		<Main fullWidthLayout className="themes">
 			<SidebarNavigation />
 			<ThemesHeader />
 			<CurrentTheme siteId={ siteId } />
-			{ displayUpsellBanner && ! isAtomic && ! isPartnerPlan && upsellBanner }
 			<ThemeShowcase
 				{ ...props }
 				siteId={ siteId }
 				emptyContent={ showWpcomThemesList ? <div /> : null }
 				isJetpackSite={ true }
-				upsellBanner={ displayUpsellBanner && isAtomic ? upsellBanner : null }
+				upsellBanner={ displayUpsellBanner ? upsellBanner : null }
 			>
 				{ showWpcomThemesList && (
 					<div>
@@ -139,7 +110,6 @@ const ConnectedSingleSiteJetpack = connectOptions( ( props ) => {
 } );
 
 export default connect( ( state, { siteId, tier } ) => {
-	const siteSlug = getSelectedSiteSlug( state );
 	const currentPlan = getCurrentPlan( state, siteId );
 	const isMultisite = isJetpackSiteMultiSite( state, siteId );
 	const showWpcomThemesList = ! isMultisite;
@@ -153,13 +123,11 @@ export default connect( ( state, { siteId, tier } ) => {
 	}
 	return {
 		currentPlan,
-		purchase: currentPlan ? getByPurchaseId( state, currentPlan.id ) : null,
 		tier,
 		showWpcomThemesList,
 		emptyContent,
 		isAtomic: isAtomicSite( state, siteId ),
 		isMultisite,
 		requestingSitePlans: isRequestingSitePlans( state, siteId ),
-		siteSlug,
 	};
 } )( ConnectedSingleSiteJetpack );
