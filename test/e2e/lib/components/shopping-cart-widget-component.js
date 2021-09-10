c01bd0e2ec15087cd21277775fdf1855b3673bc1
Payton Swick <payton@foolord.com>
2021-09-09 20:59:11 -0400

    Update e2e tests to use masterbar cart for clearing cart

--- test/e2e/lib/components/shopping-cart-widget-component.js
+++ test/e2e/lib/components/shopping-cart-widget-component.js
@@ -4,13 +4,11 @@ import * as driverHelper from '../driver-helper.js';
 
 export default class ShoppingCartWidgetComponent extends AsyncBaseContainer {
 	constructor( driver ) {
-		super( driver, By.css( '.popover-cart .header-button' ) );
+		super( driver, By.css( '.masterbar-cart .masterbar__item' ) );
 
 		this.cartButtonLocator = this.expectedElementLocator;
-		this.cartPopoverLocator = By.css( '.popover-cart__popover' );
-		this.cartExpandButtonLocator = By.css( '.popover-cart__popover .cart-items__expander' );
-		this.cartCheckoutButtonLocator = By.css( '.popover-cart__popover .cart-checkout-button' );
-		this.cartLoadingPlaceholderLocator = By.css( '.cart-body__loading-placeholder' );
+		this.cartPopoverLocator = By.css( '.masterbar-cart__content-wrapper' );
+		this.cartCheckoutButtonLocator = By.css( '.masterbar-cart__checkout' );
 	}
 
 	async open() {
@@ -30,34 +28,24 @@ export default class ShoppingCartWidgetComponent extends AsyncBaseContainer {
 			this.driver,
 			this.cartCheckoutButtonLocator
 		);
-
-		await this.expand();
-	}
-
-	async expand() {
-		await driverHelper.clickIfPresent( this.driver, this.cartExpandButtonLocator );
-	}
-
-	async close() {
-		const isClosed = await driverHelper.isElementNotLocated( this.driver, this.cartPopoverLocator );
-		if ( isClosed ) {
-			return;
-		}
-
-		await driverHelper.clickWhenClickable( this.driver, this.expectedElementLocator );
-		await driverHelper.waitUntilElementNotLocated( this.driver, this.cartPopoverLocator );
 	}
 
 	async removeItem( self ) {
-		const cartEmpty = await driverHelper.isElementLocated( this.driver, By.css( '.cart-empty' ) );
-		if ( ! cartEmpty ) {
-			return await driverHelper.clickWhenClickable( self.driver, By.css( '.cart__remove-item' ) );
+		const doCartItemsRemain = await driverHelper.isElementLocated(
+			this.driver,
+			this.cartButtonLocator
+		);
+		if ( doCartItemsRemain ) {
+			return await driverHelper.clickWhenClickable(
+				self.driver,
+				By.css( '.checkout-line-item__remove-product' )
+			);
 		}
 	}
 
 	async empty() {
 		const self = this;
-		const cartBadgeLocator = By.css( '.cart__count-badge' );
+		const cartBadgeLocator = By.css( '.masterbar-cart__count-container' );
 
 		const present = await driverHelper.isElementLocated( self.driver, cartBadgeLocator );
 		if ( present ) {
@@ -67,26 +55,5 @@ export default class ShoppingCartWidgetComponent extends AsyncBaseContainer {
 				await self.removeItem( self );
 			}
 		}
-	}
-
-	async removeDomainRegistration( domain ) {
-		return this.remove( 'domain_reg', domain );
-	}
-
-	async remove( type, name ) {
-		const itemLocator = By.xpath(
-			// Find an element X with class=.cart-item that contains an element with
-			// data-e2e-product-slug=`type` and a sibling with class="product-domain" and text=`name` and
-			// then select an element inside X that matches class=cart__remove-item
-			`//*[@class="cart-item"][.//*[@data-e2e-product-slug="${ type }"]/following-sibling::*[@class="product-domain"][text()="${ name }"]]//*[@class="cart__remove-item"]`
-		);
-
-		await this.open();
-		await driverHelper.clickWhenClickable( this.driver, itemLocator );
-		await driverHelper.waitUntilElementNotLocated(
-			this.driver,
-			this.cartLoadingPlaceholderLocator
-		);
-		await this.close();
 	}
 }
