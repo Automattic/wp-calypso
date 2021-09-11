4b53de62b3fd2d58f7e8e877c48d4a81489de009
Del Putnam <del@putnams.net>
2021-09-11 00:33:43 -0400

    Add transfer components

---
+++ client/components/domains/connect-domain-step/transfer-domain-step-auth-code.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ConnectDomainStepOwnershipAuthCode from 'calypso/components/domains/connect-domain-step/connect-domain-step-ownership-auth-code';
+
+const TransferDomainStepAuthCode = ( props ) => {
+	return <ConnectDomainStepOwnershipAuthCode { ...props } />;
+};
+
+export default TransferDomainStepAuthCode;
