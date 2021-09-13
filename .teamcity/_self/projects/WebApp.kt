587ea5217cf079ba5fee9eaa25598f916f83f1f7
Edwin Takahashi <egao@outlook.com>
2021-09-13 12:51:05 -0700

    Remove stress test

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -642,7 +642,7 @@ fun playwrightBuildType( targetDevice: String, buildUuid: String ): BuildType {
 					export NODE_CONFIG="{\"calypsoBaseURL\":\"${'$'}{URL%/}\"}"
 					export DEBUG=pw:api
 
-					for i in {1..100}; do xvfb-run yarn jest --reporters=jest-teamcity --reporters=default --maxWorkers=%E2E_WORKERS% specs/specs-playwright/wp-reader; done
+					xvfb-run yarn jest --reporters=jest-teamcity --reporters=default --maxWorkers=%E2E_WORKERS% --group=calypso-pr
 				""".trimIndent()
 				dockerImage = "%docker_image_e2e%"
 			}
