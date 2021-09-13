4962aac0a16dbe289330c1a67e07665b9832630f
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-09-13 17:12:54 +0400

    If access is denied, then show the appropriate error message

--- client/state/data-layer/wpcom/concierge/initial/index.js
+++ client/state/data-layer/wpcom/concierge/initial/index.js
@@ -23,10 +23,21 @@ export const fetchConciergeInitial = ( action ) =>
 export const storeFetchedConciergeInitial = ( action, initial ) =>
 	updateConciergeInitial( initial );
 
-export const conciergeInitialFetchError = () =>
-	errorNotice( translate( "We couldn't load our Concierge schedule. Please try again later." ) );
+export const conciergeInitialFetchError = ( errorMessage ) => errorNotice( errorMessage );
 
-export const showConciergeInitialFetchError = () => conciergeInitialFetchError();
+const unauthorizedError = ( errorMessage ) => errorNotice( errorMessage );
+
+export const showConciergeInitialFetchError = ( action ) => {
+	const errorCode = action?.meta?.dataLayer?.error?.code;
+	const errorMessage =
+		action?.meta?.dataLayer?.error?.message ||
+		translate( 'Something went wrong with Quick Start. Please try again later.' );
+	if ( 'rest_concierge_access_denied' === errorCode ) {
+		return unauthorizedError( errorMessage );
+	}
+
+	return conciergeInitialFetchError( errorMessage );
+};
 
 registerHandlers( 'state/data-layer/wpcom/concierge/schedules/initial/index.js', {
 	[ CONCIERGE_INITIAL_REQUEST ]: [
