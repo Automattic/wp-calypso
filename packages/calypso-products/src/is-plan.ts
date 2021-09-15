8c79ec4f424581819a057c91b90eed4c5f0fde1d
Payton Swick <payton@foolord.com>
2021-09-14 20:54:48 -0400

    Allow isPlan to operate on camelCase slugs also

--- packages/calypso-products/src/is-plan.ts
+++ packages/calypso-products/src/is-plan.ts
@@ -1,5 +1,10 @@
 import { getPlansSlugs } from './main';
 
-export function isPlan( { product_slug }: { product_slug: string } ): boolean {
-	return getPlansSlugs().includes( product_slug );
+export function isPlan( {
+	product_slug,
+	productSlug,
+}:
+	| { productSlug: never; product_slug: string }
+	| { product_slug: never; productSlug: string } ): boolean {
+	return getPlansSlugs().includes( product_slug ?? productSlug );
 }
