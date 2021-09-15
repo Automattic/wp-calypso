512b51d1ad986f44ef11b9229d4954e786702d0c
Payton Swick <payton@foolord.com>
2021-09-14 20:43:07 -0400

    Simplify isPlan function to directly query plan slugs

--- packages/calypso-products/src/is-plan.js
+++ packages/calypso-products/src/is-plan.js
@@ -1,26 +1,5 @@
-import { formatProduct } from './format-product';
-import { isBlogger } from './is-blogger';
-import { isBusiness } from './is-business';
-import { isEcommerce } from './is-ecommerce';
-import { isEnterprise } from './is-enterprise';
-import { isJetpackPlan } from './is-jetpack-plan';
-import { isJpphpBundle } from './is-jpphp-bundle';
-import { isP2Plus } from './is-p2-plus';
-import { isPersonal } from './is-personal';
-import { isPremium } from './is-premium';
+import { getPlansSlugs } from './main';
 
-export function isPlan( product ) {
-	product = formatProduct( product );
-
-	return (
-		isBlogger( product ) ||
-		isPersonal( product ) ||
-		isPremium( product ) ||
-		isBusiness( product ) ||
-		isEcommerce( product ) ||
-		isEnterprise( product ) ||
-		isJetpackPlan( product ) ||
-		isJpphpBundle( product ) ||
-		isP2Plus( product )
-	);
+export function isPlan( { product_slug } ) {
+	return getPlansSlugs().includes( product_slug );
 }
