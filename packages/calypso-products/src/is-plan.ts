236325a54f9f9de74504af2b1b6e11adb7a0e376
Payton Swick <payton@foolord.com>
2021-09-14 21:28:40 -0400

    Improve type def of product argument

--- packages/calypso-products/src/is-plan.ts
+++ packages/calypso-products/src/is-plan.ts
@@ -1,13 +1,10 @@
 import { PLAN_HOST_BUNDLE, PLAN_FREE } from './constants';
 import { getPlansSlugs } from './main';
 
-export function isPlan( {
-	product_slug,
-	productSlug,
-}:
-	| { productSlug: never; product_slug: string }
-	| { product_slug: never; productSlug: string } ): boolean {
-	const slug = product_slug ?? productSlug;
+type SnakeCaseProduct = { product_slug: string };
+type CamelCaseProduct = { productSlug: string };
+export function isPlan( product: SnakeCaseProduct | CamelCaseProduct ): boolean {
+	const slug = 'product_slug' in product ? product.product_slug : product.productSlug;
 	if ( slug === PLAN_FREE ) {
 		return false;
 	}
