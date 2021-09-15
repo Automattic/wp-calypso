0bd9f6bd23b2021e1b23eff6ecfb4fb1e2dabc08
Payton Swick <payton@foolord.com>
2021-09-14 21:13:53 -0400

    Return false for free plans

--- packages/calypso-products/src/is-plan.ts
+++ packages/calypso-products/src/is-plan.ts
@@ -1,4 +1,4 @@
-import { PLAN_HOST_BUNDLE } from './constants';
+import { PLAN_HOST_BUNDLE, PLAN_FREE } from './constants';
 import { getPlansSlugs } from './main';
 
 export function isPlan( {
@@ -8,5 +8,8 @@ export function isPlan( {
 	| { productSlug: never; product_slug: string }
 	| { product_slug: never; productSlug: string } ): boolean {
 	const slug = product_slug ?? productSlug;
+	if ( slug === PLAN_FREE ) {
+		return false;
+	}
 	return getPlansSlugs().includes( slug ) || slug === PLAN_HOST_BUNDLE;
 }
