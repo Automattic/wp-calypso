89ab6e25c5949c1e42963bcf80cccd00712833c6
Payton Swick <payton@foolord.com>
2021-09-14 20:44:10 -0400

    Convert isPlan to TS

--- packages/calypso-products/src/is-plan.ts
+++ packages/calypso-products/src/is-plan.ts
@@ -1,5 +1,5 @@
 import { getPlansSlugs } from './main';
 
-export function isPlan( { product_slug } ) {
+export function isPlan( { product_slug }: { product_slug: string } ): boolean {
 	return getPlansSlugs().includes( product_slug );
 }
