df3b4ab97c4810ef8283ae7aa389d93cd35ce651
Edwin Takahashi <egao@outlook.com>
2021-09-16 16:37:49 -0700

    Re-stress test

--- test/e2e/specs/specs-playwright/wp-stats__insights-spec.ts
+++ test/e2e/specs/specs-playwright/wp-stats__insights-spec.ts
@@ -20,22 +20,63 @@ describe( DataHelper.createSuiteTitle( 'Stats' ), function () {
 
 	describe.each`
 		siteType      | user
-		${ 'Simple' } | ${ 'defaultUser' }
 		${ 'Atomic' } | ${ 'wooCommerceUser' }
+		${ 'Simple' } | ${ 'defaultUser' }
 	`( 'View Insights ($siteType)', function ( { user } ) {
 		it( 'Log In', async function () {
 			const loginFlow = new LoginFlow( page, user );
 			await loginFlow.logIn();
 		} );
 
-		it( 'Navigate to Stats', async function () {
+		it( 'Navigate to Settings', async function () {
 			const sidebarComponent = new SidebarComponent( page );
-			await sidebarComponent.navigate( 'Stats' );
+			await sidebarComponent.navigate( 'Settings', 'General' );
+			await page.waitForSelector( ':has-text("Manage your site settings")' );
+		} );
+		it( 'Navigate to Tools', async function () {
+			const sidebarComponent = new SidebarComponent( page );
+			await sidebarComponent.navigate( 'Tools', 'Marketing' );
+			await page.waitForSelector( ':has-text("Explore tools")' );
 		} );
 
-		it( 'Click on Insights tab', async function () {
-			const statsPage = new StatsPage( page );
-			await statsPage.clickTab( 'Insights' );
+		it( 'Navigate to Upgrades', async function () {
+			const sidebarComponent = new SidebarComponent( page );
+			await sidebarComponent.navigate( 'Upgrades', 'Plans' );
+			await page.waitForSelector( ':has-text("Learn about the features")' );
+		} );
+
+		it( 'Navigate to Media', async function () {
+			const sidebarComponent = new SidebarComponent( page );
+			await page.reload();
+			await sidebarComponent.navigate( 'Media' );
+			await page.waitForSelector( ':has-text("Manage all the media")' );
+		} );
+
+		it( 'Navigate to Apperance > Themes', async function () {
+			await page.reload();
+			const sidebarComponent = new SidebarComponent( page );
+			await sidebarComponent.navigate( 'Appearance', 'Themes' );
+			await page.waitForSelector( ':has-text("Select or update the visual")' );
+		} );
+
+		it( 'Navigate to Users > All Users', async function () {
+			await page.reload();
+			const sidebarComponent = new SidebarComponent( page );
+			await sidebarComponent.navigate( 'Users', 'All Users' );
+			await page.waitForSelector( ':has-text("Invite contributors to your site")' );
+		} );
+
+		it( 'Navigate to Stats', async function () {
+			await page.reload();
+			const sidebarComponent = new SidebarComponent( page );
+			await sidebarComponent.navigate( 'Stats' );
+			await page.waitForSelector( ':has-text("Learn more about the activity")' );
+		} );
+
+		it( 'Navigate to Apperance > Widgets', async function () {
+			await page.reload();
+			const sidebarComponent = new SidebarComponent( page );
+			await sidebarComponent.navigate( 'Appearance', 'Widgets' );
 		} );
 	} );
 } );
