63bc61741e3c7d221491ce37076c542c19f9489a
Olaseni Oluwunmi <277661+olaseni@users.noreply.github.com>
2021-09-16 17:46:57 +0100

    Add some comments to the query

--- client/data/emails/use-get-mailboxes.js
+++ client/data/emails/use-get-mailboxes.js
@@ -1,6 +1,13 @@
 import { useQuery } from 'react-query';
 import wpcom from 'calypso/lib/wp';
 
+/**
+ * Get the associated mailboxes for a given site
+ *
+ * @param {number} siteId Site identifier
+ * @param {object} queryOptions Query options
+ * @returns {data, error, isLoading} Returns an object with the requested mailboxes
+ */
 export const useGetMailboxes = ( siteId, queryOptions = {} ) => {
 	return useQuery(
 		[ 'emails/mailboxes', siteId ],
