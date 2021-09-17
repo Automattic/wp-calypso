449eed660b170c7f29711d93601618c86891923a
Daniel López <daniel.lopez@automattic.com>
2021-09-17 14:26:24 +0200

    Emails: Add mailbox without site parameter

--- client/my-sites/email/index.js
+++ client/my-sites/email/index.js
@@ -167,8 +167,9 @@ export default function () {
 		handlers: [ ...commonHandlers, controller.emailManagementForwarding, makeLayout, clientRender ],
 	} );
 
+	page( paths.emailManagementInbox(), siteSelection, sites, makeLayout, clientRender );
 	registerMultiPage( {
-		paths: [ paths.emailManagementInbox( ':site' ), paths.emailManagementInbox() ],
+		paths: [ paths.emailManagementInbox( ':site' ) ],
 		handlers: [ ...commonHandlers, controller.emailManagementInbox, makeLayout, clientRender ],
 	} );
 }
