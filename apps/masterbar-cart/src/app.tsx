ff7fc2a02cd394ca6daebcd67a1468fa5e09b920
Payton Swick <payton@foolord.com>
2021-09-17 15:12:19 -0400

    Pass selectedSiteSlug to MasterbarCart

--- apps/masterbar-cart/src/app.tsx
+++ apps/masterbar-cart/src/app.tsx
@@ -1,9 +1,16 @@
+import { ShoppingCartProvider } from '@automattic/shopping-cart';
 import React from 'react';
 import ReactDom from 'react-dom';
-import MasterbarCart from 'calypso/layout/masterbar/masterbar-cart';
+import { MasterbarCart } from 'calypso/layout/masterbar/masterbar-cart';
+import { cartManagerClient } from 'calypso/my-sites/checkout/cart-manager-client';
 
 async function AppBoot() {
-	ReactDom.render( <MasterbarCart />, document.getElementById( 'masterbar-cart-area' ) );
+	ReactDom.render(
+		<ShoppingCartProvider managerClient={ cartManagerClient }>
+			<MasterbarCart selectedSiteSlug={ window.location.search } />
+		</ShoppingCartProvider>,
+		document.getElementById( 'masterbar-cart-area' )
+	);
 }
 
 AppBoot();
