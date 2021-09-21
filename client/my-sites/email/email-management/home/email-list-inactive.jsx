fef4be169379533089f96d2e7644ae7d600ca584
Daniel López <daniel.lopez@automattic.com>
2021-09-22 01:41:54 +0200

    Emails: Section header label in email-list-inactive fixed

--- client/my-sites/email/email-management/home/email-list-inactive.jsx
+++ client/my-sites/email/email-management/home/email-list-inactive.jsx
@@ -7,11 +7,17 @@ import { emailManagementPurchaseNewEmailAccount } from 'calypso/my-sites/email/p
 
 class EmailListInactive extends React.Component {
 	render() {
-		const { currentRoute, domains, header, sectionLabel, selectedSiteSlug, translate } = this.props;
+		const {
+			currentRoute,
+			domains,
+			header,
+			sectionHeaderLabel,
+			selectedSiteSlug,
+			translate,
+		} = this.props;
 		if ( domains.length < 1 ) {
 			return null;
 		}
-		const sectionHeaderLabel = sectionLabel ? sectionLabel : translate( 'Other domains' );
 
 		const emailListItems = domains.map( ( domain ) => {
 			return (
