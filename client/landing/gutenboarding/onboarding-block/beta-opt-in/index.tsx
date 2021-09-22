dd10ac6a882fd613b863af1aae13aaf73a3d4e37
Matt Wiebe <wiebe@automattic.com>
2021-09-22 17:03:42 -0500

    simplified step

--- client/landing/gutenboarding/onboarding-block/beta-opt-in/index.tsx
+++ client/landing/gutenboarding/onboarding-block/beta-opt-in/index.tsx
@@ -1,5 +1,13 @@
-import { Title, SubTitle, ActionButtons, NextButton, ArrowButton } from '@automattic/onboarding';
+import {
+	Title,
+	SubTitle,
+	ActionButtons,
+	NextButton,
+	BackButton,
+	SkipButton,
+} from '@automattic/onboarding';
 import { useDispatch } from '@wordpress/data';
+import { Icon, siteLogo, header, navigation, arrowRight } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 import React from 'react';
 import useStepNavigation from '../../hooks/use-step-navigation';
@@ -9,7 +17,7 @@ import './style.scss';
 
 const BetaOptIn: React.FunctionComponent = () => {
 	const { __ } = useI18n();
-	const { goNext } = useStepNavigation();
+	const { goNext, goBack } = useStepNavigation();
 	const { enrollInFseBeta } = useDispatch( ONBOARD_STORE );
 	const pickBeta = ( shouldEnroll: boolean ) => {
 		enrollInFseBeta( shouldEnroll );
@@ -18,34 +26,43 @@ const BetaOptIn: React.FunctionComponent = () => {
 	return (
 		<div className="gutenboarding-page beta-opt-in">
 			<div className="beta-opt-in__header">
-				<Title>{ __( 'Join Our Full Site Editing Beta' ) }</Title>
-				<SubTitle>{ __( 'Experience the new WordPress.com site editing features' ) }</SubTitle>
+				<div>
+					<Title>{ __( 'Join Our Full Site Editing Beta' ) }</Title>
+					<SubTitle>{ __( 'Experience the new WordPress.com site editing features' ) }</SubTitle>
+				</div>
+				<ActionButtons className="beta-opt-in__actions">
+					<BackButton onClick={ goBack } />
+					<SkipButton onClick={ () => pickBeta( false ) }>{ __( 'No Thanks' ) }</SkipButton>
+				</ActionButtons>
 			</div>
-			<div className="beta-opt-in__content">
-				<div className="beta-opt-in__column">
-					<h3>{ __( 'Do it all, right from the editor' ) }</h3>
+			<ol className="beta-opt-in__content">
+				<li className="beta-opt-in__item">
+					<Icon icon={ siteLogo } />
+					<h3>{ __( 'Edit Your Logo' ) }</h3>
 					<p>
 						{ __(
-							'The new Full Site Editing-enabled editor allows you to change and update your site with ease.'
+							'Change and update your logo and other header elements without leaving the editor'
 						) }
 					</p>
-				</div>
-				<div className="beta-opt-in__column">
-					<h3>{ __( 'Style everything' ) }</h3>
+				</li>
+				<li className="beta-opt-in__item">
+					<Icon icon={ header } />
+					<h3>{ __( 'Update Your Header and Footer' ) }</h3>
 					<p>
-						{ __(
-							'Express yourself with custom typography and color styles at the site, block type, and block level.'
-						) }
+						{ __( 'Full Site Editing allows you to make changes on your site header and footer.' ) }
 					</p>
-				</div>
-			</div>
+				</li>
+				<li className="beta-opt-in__item">
+					<Icon icon={ navigation } />
+					<h3>{ __( 'Build Your Navigation' ) }</h3>
+					<p>{ __( 'Take your site menus to the next level by adding blocks to it.' ) }</p>
+				</li>
+			</ol>
 
-			<ActionButtons className="beta-opt-in__actions">
-				<ArrowButton arrow="right" onClick={ () => pickBeta( true ) }>
-					{ __( 'Enroll in Beta' ) }
-				</ArrowButton>
-				<NextButton onClick={ () => pickBeta( false ) }>{ __( 'No Thanks' ) }</NextButton>
-			</ActionButtons>
+			<NextButton className="beta-opt-in__submit" onClick={ () => pickBeta( true ) }>
+				{ __( 'Enroll in Beta' ) }
+				<Icon icon={ arrowRight } />
+			</NextButton>
 		</div>
 	);
 };
