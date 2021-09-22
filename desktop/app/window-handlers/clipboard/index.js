255186d2fd8b897daabd5bfcdc2d1213bcc9ecd7
nsakaimbo <nsakaimbo@users.noreply.github.com>
2021-09-22 13:08:46 -0600

    Add event to ipc handler

--- desktop/app/window-handlers/clipboard/index.js
+++ desktop/app/window-handlers/clipboard/index.js
@@ -2,8 +2,12 @@ const { ipcMain: ipc, clipboard } = require( 'electron' );
 const log = require( '../../lib/logger' )( 'desktop:clipboard' );
 
 module.exports = function () {
-	ipc.on( 'copy-text-to-clipboard', ( text ) => {
+	ipc.on( 'copy-text-to-clipboard', ( _, text ) => {
 		log.info( 'Copying text to clipboard: ', text );
-		clipboard.writeText( text );
+		try {
+			clipboard.writeText( text );
+		} catch ( e ) {
+			log.error( 'Failed to write to clipboard: ', e );
+		}
 	} );
 };
