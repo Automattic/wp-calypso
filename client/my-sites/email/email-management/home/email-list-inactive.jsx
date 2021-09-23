f60739bb2ac62eb002f96c110bde84930490f952
Daniel <daniel.lopez@automattic.com>
2021-09-23 19:09:02 +0200

    Emails: Logic to show either inbox selector or domains picker or domains CTA buy (#56403)
    * Emails: Logic to show either inbox selector or domains picker or domains CTA buy.

--- client/my-sites/email/email-management/home/email-list-inactive.jsx
+++ client/my-sites/email/email-management/home/email-list-inactive.jsx
@@ -1,5 +1,6 @@
 import { Button, Card } from '@automattic/components';
 import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
 import React from 'react';
 import SectionHeader from 'calypso/components/section-header';
 import { canCurrentUserAddEmail } from 'calypso/lib/domains';
@@ -7,8 +8,14 @@ import { emailManagementPurchaseNewEmailAccount } from 'calypso/my-sites/email/p
 
 class EmailListInactive extends React.Component {
 	render() {
-		const { selectedSiteSlug, currentRoute, domains, translate } = this.props;
-
+		const {
+			currentRoute,
+			domains,
+			headerComponent,
+			sectionHeaderLabel,
+			selectedSiteSlug,
+			translate,
+		} = this.props;
 		if ( domains.length < 1 ) {
 			return null;
 		}
@@ -34,11 +41,21 @@ class EmailListInactive extends React.Component {
 
 		return (
 			<div className="email-list-inactive">
-				<SectionHeader label={ translate( 'Other domains' ) } />
+				{ headerComponent }
+				<SectionHeader label={ sectionHeaderLabel ?? translate( 'Other domains' ) } />
 				{ emailListItems }
 			</div>
 		);
 	}
 }
+
+EmailListInactive.propTypes = {
+	currentRoute: PropTypes.string,
+	domains: PropTypes.array,
+	headerComponent: PropTypes.element,
+	sectionHeaderLabel: PropTypes.string,
+	selectedSiteSlug: PropTypes.string,
+	translate: PropTypes.func.isRequired,
+};
 
 export default localize( EmailListInactive );
