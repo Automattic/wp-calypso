008461dafcd55b64807b8b09502d184cdbdb0de9
Sergio Cinos <sergio.cinos@gmail.com>
2021-09-23 06:01:08 +0200

    Add comment explaning why we still need postcss-custom-properties

--- apps/notifications/postcss.config.js
+++ apps/notifications/postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = () => ( {
 	plugins: {
+		// In theory we don't need to resolve custom properties, as all supported browsers understand CSS variables.
+		// However that would require for Notifications to import @automattic/calypso-color-schemes styles, and they
+		// come with useless bloat for Notifications (namely color schemes).
 		'postcss-custom-properties': {
 			importFrom: [ require.resolve( '@automattic/calypso-color-schemes/json' ) ],
 			// @TODO: Drop `preserve: false` workaround if possible
