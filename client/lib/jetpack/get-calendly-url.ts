e80ffd3e9a2aae1ee62abd6394d70e79f439f1cf
Renzo Canepa <rcanepag@gmail.com>
2021-09-23 16:32:53 -0300

    Remove unused code from site-less checkout flow

--- client/lib/jetpack/get-calendly-url.ts
+++
@@ -1,9 +0,0 @@
-import config from '@automattic/calypso-config';
-
-const getCalendlyURL = (): string | null => {
-	const url = config< string >( 'calendly_jetpack_appointment_url' );
-
-	return url ? url : null;
-};
-
-export default getCalendlyURL;
