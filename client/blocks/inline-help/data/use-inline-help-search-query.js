eae23a5e34824ce7c200dffeaae1d149344e3c18
Markus Dobmann <m.do@posteo.net>
2021-09-23 13:44:08 +0200

    inline-help: use `useHelpSearchQuery` as base

--- client/blocks/inline-help/data/use-inline-help-search-query.js
+++ client/blocks/inline-help/data/use-inline-help-search-query.js
@@ -1,21 +1,15 @@
 import { createHigherOrderComponent } from '@wordpress/compose';
-import { useQuery } from 'react-query';
 import { useSelector } from 'react-redux';
-import wpcom from 'calypso/lib/wp';
+import { useHelpSearchQuery } from 'calypso/data/help/use-help-search-query';
 import getSearchQuery from 'calypso/state/inline-help/selectors/get-search-query';
 
 export const useInlineHelpSearchQuery = ( search, queryOptions = {} ) =>
-	useQuery(
-		[ 'inline-help', search ],
-		() => wpcom.req.get( '/help/search', { query: search, include_post_id: 1 } ),
-		{
-			enabled: !! search,
-			select( data ) {
-				return data.wordpress_support_links ?? [];
-			},
-			...queryOptions,
-		}
-	);
+	useHelpSearchQuery( search, {
+		...queryOptions,
+		select( data ) {
+			return data.wordpress_support_links ?? [];
+		},
+	} );
 
 export const withInlineHelpSearchResults = createHigherOrderComponent(
 	( Wrapped ) => ( props ) => {
