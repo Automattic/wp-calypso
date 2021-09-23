c6b7e482e080d88e42198334dc363047bb7bb6d9
Sergio Cinos <sergio.cinos@gmail.com>
2021-09-23 13:03:43 +0200

    Convert @automattic/calypso-products to ESM

---
+++ packages/calypso-products/CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## 2.0.0
+
+- Breaking: Module converted to ESM only. CJS build is not provided anymore.
--- packages/calypso-products/jest.config.js
+++ packages/calypso-products/jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
 	preset: '../../test/packages/jest-preset.js',
 	testEnvironment: 'jsdom',
 	globals: {
