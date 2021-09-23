8264156095c32cc4341a58d249af8ca9b0a00318
rafaelgalani <galani.rafael@gmail.com>
2021-09-23 14:59:21 -0300

    Add lock subtitle to locked domains' content

--- client/components/domains/connect-domain-step/transfer-domain-step-unlock.jsx
+++ client/components/domains/connect-domain-step/transfer-domain-step-unlock.jsx
@@ -2,12 +2,23 @@ import { Button } from '@automattic/components';
 import { useI18n } from '@wordpress/react-i18n';
 import PropTypes from 'prop-types';
 import React, { useCallback, useState } from 'react';
+import CardHeading from 'calypso/components/card-heading';
 import ConnectDomainStepWrapper from 'calypso/components/domains/connect-domain-step/connect-domain-step-wrapper';
-import { stepsHeadingTransfer } from 'calypso/components/domains/connect-domain-step/constants';
+import {
+	stepsHeadingTransfer,
+	domainLockStatusType,
+} from 'calypso/components/domains/connect-domain-step/constants';
+import MaterialIcon from 'calypso/components/material-icon';
 import Notice from 'calypso/components/notice';
 import wpcom from 'calypso/lib/wp';
 
-const TransferDomainStepUnlock = ( { className, onNextStep, domain, ...props } ) => {
+const TransferDomainStepUnlock = ( {
+	className,
+	onNextStep,
+	domain,
+	domainLockStatus: initialDomainLockStatus,
+	...props
+} ) => {
 	const { __ } = useI18n();
 
 	const [ checkInProgress, setCheckInProgress ] = useState( false );
@@ -32,19 +43,40 @@ const TransferDomainStepUnlock = ( { className, onNextStep, domain, ...props } )
 		}
 	};
 
+	const lockedDomainContent =
+		initialDomainLockStatus === domainLockStatusType.UNLOCKED ? null : (
+			<CardHeading tagName="h2" className={ className + '__sub-heading' }>
+				<MaterialIcon className={ className + '__sub-heading-icon' } size={ 24 } icon="lock" />
+				{ initialDomainLockStatus === domainLockStatusType.LOCKED
+					? __( 'Your domain is locked' )
+					: __( "Can't get the domain's lock status" ) }
+			</CardHeading>
+		);
+
+	const lockedDomainDescription = __(
+		'Domain providers lock domains to prevent unauthorized transfers. You’ll need to unlock it on your domain provider’s settings page. Some domain providers require you to contact them via their customer support to unlock it.'
+	);
+
+	const unkownLockStatusAdditionalDescription = (
+		<>{ __( 'Please check that your domain is unlocked.' ) + ' ' }</>
+	);
+
 	const stepContent = (
 		<div className={ className + '__domain-unlock' }>
 			{ domainStatusError && ! checkInProgress && (
 				<Notice
 					status="is-error"
 					showDismiss={ false }
-					text="Your domain is still locked. If you’ve already unlocked it, wait a few minutes and try again."
+					text={ __(
+						'Your domain is still locked. If you’ve already unlocked it, wait a few minutes and try again.'
+					) }
 				></Notice>
 			) }
+			{ lockedDomainContent }
 			<p className={ className + '__text' }>
-				{ __(
-					'Domain providers lock domains to prevent unauthorized transfers. You’ll need to unlock it on your domain provider’s settings page. Some domain providers require you to contact them via their customer support to unlock it.'
-				) }
+				{ initialDomainLockStatus === domainLockStatusType.UNKNOWN &&
+					unkownLockStatusAdditionalDescription }
+				{ lockedDomainDescription }
 				<br />
 				<br />
 				{ __( 'It might take a few minutes for any changes to take effect.' ) }
