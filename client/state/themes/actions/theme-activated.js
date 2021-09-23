e7589ca96b8d4104f0a49323ac0c8104e1182ca6
Jeremy Yip <jeremy.yip@automattic.com>
2021-09-23 16:13:15 -0700

    Update admin menu when switching between any themes

--- client/state/themes/actions/theme-activated.js
+++ client/state/themes/actions/theme-activated.js
@@ -5,22 +5,11 @@ import { THEME_ACTIVATE_SUCCESS } from 'calypso/state/themes/action-types';
 import {
 	getActiveTheme,
 	getLastThemeQuery,
-	getCanonicalTheme,
 	prependThemeFilterKeys,
 } from 'calypso/state/themes/selectors';
 import { getThemeIdFromStylesheet } from 'calypso/state/themes/utils';
 
 import 'calypso/state/themes/init';
-
-function isClassicTheme( theme ) {
-	const themeFeatures = theme?.taxonomies?.theme_feature;
-	return themeFeatures && themeFeatures.every( ( feature ) => feature.slug !== 'block-templates' );
-}
-
-function isBlockTheme( theme ) {
-	const themeFeatures = theme?.taxonomies?.theme_feature;
-	return themeFeatures && themeFeatures.some( ( feature ) => feature.slug === 'block-templates' );
-}
 
 /**
  * Returns an action thunk to be used in signalling that a theme has been activated
@@ -55,18 +44,9 @@ export function themeActivated( themeStylesheet, siteId, source = 'unknown', pur
 		} );
 		dispatch( withAnalytics( trackThemeActivation, action ) );
 
-		// When switching from a block-based theme to a classic theme and vice versa, the admin
-		// sidebar toggles site editor and customizer menu item visiblity. The admin bar needs to be
-		// refreshed to see the updates.
-		const previousTheme = getCanonicalTheme( getState(), siteId, previousThemeId );
-		const newTheme = getCanonicalTheme( getState(), siteId, themeId );
-
-		if (
-			( isClassicTheme( previousTheme ) && isBlockTheme( newTheme ) ) ||
-			( isBlockTheme( previousTheme ) && isClassicTheme( newTheme ) )
-		) {
-			dispatch( requestAdminMenu( siteId ) );
-		}
+		// There are instances where switching themes toggles menu items. This action refreshes
+		// the admin bar to ensure that those updates are displayed in the UI.
+		dispatch( requestAdminMenu( siteId ) );
 
 		// Update pages in case the front page was updated on theme switch.
 		dispatch( requestSitePosts( siteId, { type: 'page' } ) );
