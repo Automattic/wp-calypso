31693d12a47fcd77f73d755b59118b5072298da5
rafaelgalani <galani.rafael@gmail.com>
2021-09-23 16:45:18 -0300

    Add missing header to login step

--- client/components/domains/connect-domain-step/transfer-domain-step-login.jsx
+++ client/components/domains/connect-domain-step/transfer-domain-step-login.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ConnectDomainStepLogin from 'calypso/components/domains/connect-domain-step/connect-domain-step-login';
-import { stepsHeadingTransfer } from 'calypso/components/domains/connect-domain-step/constants';
 
 const TransferDomainStepLogin = ( props ) => {
-	return <ConnectDomainStepLogin { ...props } heading={ stepsHeadingTransfer } />;
+	return <ConnectDomainStepLogin { ...props } />;
 };
 
 export default TransferDomainStepLogin;
