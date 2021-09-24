30dfb1f1e75cb91ab84b7abb49dd330fbd00303a
Del Putnam <del@putnams.net>
2021-09-24 16:56:56 -0400

    Remove unused default transfer action

--- client/components/domains/use-my-domain/utilities/default-transfer-domain-action.js
+++
@@ -1,9 +0,0 @@
-import page from 'page';
-import { domainTransferIn } from 'calypso/my-sites/domains/paths';
-
-export const transferDomainAction = ( { domain, selectedSite, transferDomainUrl } ) => {
-	// TODO: This will be replaced with a new transfer in flow in a near-term future update.
-	const defaultTransferUrl = domainTransferIn( selectedSite.slug, domain, true );
-	const transferUrl = transferDomainUrl ?? defaultTransferUrl;
-	page( transferUrl );
-};
