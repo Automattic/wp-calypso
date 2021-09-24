a4c307e7060e876fef17f0a331042f56c8fad2ba
Markus Dobmann <m.do@posteo.net>
2021-09-24 10:55:03 +0200

    `QueryWhois`: migrate to functional component

--- client/components/data/query-whois/index.jsx
+++ client/components/data/query-whois/index.jsx
@@ -1,29 +1,27 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { requestWhois } from 'calypso/state/domains/management/actions';
 import isRequestingWhois from 'calypso/state/selectors/is-requesting-whois';
 
-class QueryWhois extends Component {
-	UNSAFE_componentWillMount() {
-		if ( this.props.isRequesting ) {
-			return;
-		}
-		this.props.requestWhois( this.props.domain );
+const request = ( domain ) => ( dispatch, getState ) => {
+	if ( domain && ! isRequestingWhois( getState(), domain ) ) {
+		dispatch( requestWhois( domain ) );
 	}
+};
 
-	render() {
-		return null;
-	}
+function QueryWhois( { domain } ) {
+	const dispatch = useDispatch();
+
+	useEffect( () => {
+		dispatch( request( domain ) );
+	}, [ dispatch, domain ] );
+
+	return null;
 }
 
 QueryWhois.propTypes = {
 	domain: PropTypes.string.isRequired,
-	isRequesting: PropTypes.bool.isRequired,
-	requestWhois: PropTypes.func.isRequired,
 };
 
-export default connect(
-	( state, { domain } ) => ( { isRequesting: isRequestingWhois( state, domain ) } ),
-	{ requestWhois }
-)( QueryWhois );
+export default QueryWhois;
