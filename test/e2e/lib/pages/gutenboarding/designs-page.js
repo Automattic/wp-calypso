a1d5d391999c2de8416db68ad355641393d28b98
dpasque <dan.speckhard.pasque@automattic.com>
2021-10-01 10:09:31 -0500

    Make design pick in gutenboarding deterministic

--- test/e2e/lib/pages/gutenboarding/designs-page.js
+++ test/e2e/lib/pages/gutenboarding/designs-page.js
@@ -1,6 +1,5 @@
 import { By } from 'selenium-webdriver';
 import AsyncBaseContainer from '../../async-base-container';
-import * as dataHelper from '../../data-helper';
 
 export default class DesignLocatorPage extends AsyncBaseContainer {
 	constructor( driver ) {
@@ -11,11 +10,14 @@ export default class DesignLocatorPage extends AsyncBaseContainer {
 
 	async selectFreeDesign() {
 		const freeOptions = await this.driver.findElements( this.freeOptionLocator );
-		await freeOptions[ dataHelper.getRandomInt( 0, freeOptions.length - 1 ) ].click();
-	}
+		if ( freeOptions.length === 0 ) {
+			throw new Error( 'No free designs were found on design selection page' );
+		}
 
-	async selectPaidDesign() {
-		const paidOptions = await this.driver.findElements( this.paidOptionLocator );
-		await paidOptions[ dataHelper.getRandomInt( 0, paidOptions.length - 1 ) ].click();
+		const blankCanvasIndex = 0;
+		const firstNonBlankIndex = 1;
+		// If possible, we should skip first design, which is the blank canvas, and pick the first other one.
+		const selectedDesignIndex = freeOptions.length > 1 ? firstNonBlankIndex : blankCanvasIndex;
+		await freeOptions[ selectedDesignIndex ].click();
 	}
 }
