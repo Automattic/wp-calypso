c2cc8d649e40909a497ace81d29ad33b028188b7
Marko Andrijasevic <marko.andrijasevic@automattic.com>
2021-10-01 13:46:45 +0200

    Replace ReactElement with PropsWithChildren type

--- client/landing/gutenboarding/components/badge/index.tsx
+++ client/landing/gutenboarding/components/badge/index.tsx
@@ -1,12 +1,9 @@
 import classNames from 'classnames';
-import type { ReactElement, FunctionComponent } from 'react';
+import type { PropsWithChildren, FunctionComponent } from 'react';
 
 import './style.scss';
 
-interface Props {
-	children: ReactElement[];
-	className?: string;
-}
+type Props = PropsWithChildren< { className?: string } >;
 
 const Badge: FunctionComponent< Props > = ( { children, className, ...props } ) => (
 	<span { ...props } className={ classNames( 'badge', className ) }>
