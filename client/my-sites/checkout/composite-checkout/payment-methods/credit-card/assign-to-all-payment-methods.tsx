fca55dbd9de616b6a6681f05dc43478920848156
Payton Swick <payton@foolord.com>
2021-10-01 16:24:23 -0400

    Add margin around checkbox

--- client/my-sites/checkout/composite-checkout/payment-methods/credit-card/assign-to-all-payment-methods.tsx
+++ client/my-sites/checkout/composite-checkout/payment-methods/credit-card/assign-to-all-payment-methods.tsx
@@ -1,6 +1,11 @@
+import styled from '@emotion/styled';
 import { CheckboxControl } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import ExternalLink from 'calypso/components/external-link';
+
+const CheckboxWrapper = styled.div`
+	margin-top: 16px;
+`;
 
 export default function AssignToAllPaymentMethods( {
 	isChecked,
@@ -12,17 +17,19 @@ export default function AssignToAllPaymentMethods( {
 	const translate = useTranslate();
 	// TODO: Add real "Learn more" link target
 	return (
-		<CheckboxControl
-			checked={ isChecked }
-			onChange={ onChange }
-			label={ translate(
-				'Use this payment method for all subscriptions on my account. {{link}}Learn more.{{/link}}',
-				{
-					components: {
-						link: <ExternalLink icon href="https://wordpress.com/support/manage-purchases/" />,
-					},
-				}
-			) }
-		/>
+		<CheckboxWrapper>
+			<CheckboxControl
+				checked={ isChecked }
+				onChange={ onChange }
+				label={ translate(
+					'Use this payment method for all subscriptions on my account. {{link}}Learn more.{{/link}}',
+					{
+						components: {
+							link: <ExternalLink icon href="https://wordpress.com/support/manage-purchases/" />,
+						},
+					}
+				) }
+			/>
+		</CheckboxWrapper>
 	);
 }
