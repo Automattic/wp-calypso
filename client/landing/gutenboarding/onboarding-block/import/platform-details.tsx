62354afd33eb4f3f926822267c144de005a53798
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-10-04 18:36:10 +0200

    Divide import details per platform

--- client/landing/gutenboarding/onboarding-block/import/platform-details.tsx
+++ client/landing/gutenboarding/onboarding-block/import/platform-details.tsx
@@ -10,70 +10,54 @@ interface DetailsProps {
 	onClose: () => void;
 }
 
-const WixDetails: React.FunctionComponent< DetailsProps > = () => {
+const WixPanelInfo: React.FunctionComponent< DetailsProps > = () => {
 	const { __ } = useI18n();
 
 	return (
-		<div className="import__details-modal-content">
-			<p>
-				{ __(
-					"Our Wix content importer is the quickest way to move your content. Simply click 'Import your content' and provide your site's web address (called a URL). Once the import is complete, you'll have a site that's pre-filled with your content."
-				) }
-			</p>
+		<Panel>
+			<PanelBody title={ __( 'Import to a new site' ) } initialOpen={ false }>
+				<p>Body txt: lorem ipsum dolor sit amet</p>
+			</PanelBody>
+			<PanelBody title={ __( 'Import to an existing site' ) } initialOpen={ false }>
+				<p>Body txt: lorem ipsum dolor sit amet</p>
+			</PanelBody>
+		</Panel>
+	);
+};
 
-			<BackButton>{ __( 'Learn more' ) }</BackButton>
+const SquarespacePanelInfo: React.FunctionComponent< DetailsProps > = () => {
+	const { __ } = useI18n();
 
-			<div className={ 'import__details-features' }>
-				<p>
-					{ createInterpolateElement( __( 'Things we <strong>can</strong> import:' ), {
-						strong: createElement( 'strong' ),
-					} ) }
-				</p>
-				<ul className={ 'import__details-list' }>
-					<li>
-						<Icon size={ 20 } icon={ check } /> { __( 'Blog posts' ) }
-					</li>
-					<li>
-						<Icon size={ 20 } icon={ check } /> { __( 'Static pages' ) }
-					</li>
-					<li>
-						<Icon size={ 20 } icon={ check } /> { __( 'Various blocks and features' ) }
-					</li>
-					<li>
-						<Icon size={ 20 } icon={ check } /> { __( 'Images' ) }
-					</li>
-				</ul>
+	return (
+		<Panel>
+			<PanelBody title={ __( 'Downloading your Squarespace export file' ) } initialOpen={ false }>
+				<p>Body txt: lorem ipsum dolor sit amet</p>
+			</PanelBody>
+			<PanelBody
+				title={ __( 'Importing your Squarespace export file to WordPress.com' ) }
+				initialOpen={ false }
+			>
+				<p>Body txt: lorem ipsum dolor sit amet</p>
+			</PanelBody>
+			<PanelBody title={ __( 'Business and eCommerce plan sites' ) } initialOpen={ false }>
+				<p>Body txt: lorem ipsum dolor sit amet</p>
+			</PanelBody>
+		</Panel>
+	);
+};
 
-				<p>
-					{ createInterpolateElement( __( "Things we <strong>can't</strong> import:" ), {
-						strong: createElement( 'strong' ),
-					} ) }
-				</p>
-				<ul className={ 'import__details-list' }>
-					<li>
-						<Icon size={ 20 } icon={ close } /> { __( 'Site styles' ) }
-					</li>
-					<li>
-						<Icon size={ 20 } icon={ close } /> { __( 'Themes' ) }
-					</li>
-					<li>
-						<Icon size={ 20 } icon={ close } /> { __( 'Colors' ) }
-					</li>
-					<li>
-						<Icon size={ 20 } icon={ close } /> { __( 'Fonts' ) }
-					</li>
-				</ul>
-			</div>
+const BloggerPanelInfo: React.FunctionComponent< DetailsProps > = () => {
+	const { __ } = useI18n();
 
-			<Panel>
-				<PanelBody title={ __( 'Import to a new site' ) } initialOpen={ false }>
-					<p>Body txt: lorem ipsum dolor sit amet</p>
-				</PanelBody>
-				<PanelBody title={ __( 'Import to an existing site' ) } initialOpen={ false }>
-					<p>Body txt: lorem ipsum dolor sit amet</p>
-				</PanelBody>
-			</Panel>
-		</div>
+	return (
+		<Panel>
+			<PanelBody title={ __( 'Export from Blogger' ) } initialOpen={ false }>
+				<p>Body txt: lorem ipsum dolor sit amet</p>
+			</PanelBody>
+			<PanelBody title={ __( 'Import to WordPress.com' ) } initialOpen={ false }>
+				<p>Body txt: lorem ipsum dolor sit amet</p>
+			</PanelBody>
+		</Panel>
 	);
 };
 
@@ -85,6 +69,29 @@ const ImportPlatformDetails: React.FunctionComponent< DetailsProps > = ( data ) 
 		switch ( _platform ) {
 			case 'Wix':
 				return __( 'Importing content from Wix' );
+			case 'Squarespace':
+				return __( 'Importing content from Squarespace' );
+			case 'Blogger':
+				return __( 'Importing content from Blogger' );
+			default:
+				return '';
+		}
+	};
+
+	const getInfo = ( _platform: string ): string => {
+		switch ( _platform ) {
+			case 'Wix':
+				return __(
+					"Our Wix content importer is the quickest way to move your content. Simply click 'Import your content' and provide your site's web address (called a URL). Once the import is complete, you'll have a site that's pre-filled with your content."
+				);
+			case 'Squarespace':
+				return __(
+					"Our Squarespace content importer is the quickest way to move your content. Simply export the contents from Squarespace as a WordPress format XML file, then click 'Import your content' and upload it to our importer."
+				);
+			case 'Blogger':
+				return __(
+					"Our Blogger content importer is the quickest way to move your content. Simply export the contents from Blogger as a XML file, then click 'Import your content' and upload it to our importer."
+				);
 			default:
 				return '';
 		}
@@ -96,14 +103,66 @@ const ImportPlatformDetails: React.FunctionComponent< DetailsProps > = ( data ) 
 			title={ getTitle( platform ) }
 			onRequestClose={ onClose }
 		>
-			{ ( () => {
-				switch ( platform ) {
-					case 'Wix':
-						return <WixDetails { ...data } />;
-					default:
-						return null;
-				}
-			} )() }
+			<div className="import__details-modal-content">
+				<p>{ getInfo( platform ) }</p>
+
+				<BackButton>{ __( 'Learn more' ) }</BackButton>
+
+				<div className={ 'import__details-features' }>
+					<p>
+						{ createInterpolateElement( __( 'Things we <strong>can</strong> import:' ), {
+							strong: createElement( 'strong' ),
+						} ) }
+					</p>
+					<ul className={ 'import__details-list' }>
+						<li>
+							<Icon size={ 20 } icon={ check } /> { __( 'Blog posts' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ check } /> { __( 'Static pages' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ check } /> { __( 'Various blocks and features' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ check } /> { __( 'Images' ) }
+						</li>
+					</ul>
+
+					<p>
+						{ createInterpolateElement( __( "Things we <strong>can't</strong> import:" ), {
+							strong: createElement( 'strong' ),
+						} ) }
+					</p>
+					<ul className={ 'import__details-list' }>
+						<li>
+							<Icon size={ 20 } icon={ close } /> { __( 'Site styles' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ close } /> { __( 'Themes' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ close } /> { __( 'Colors' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ close } /> { __( 'Fonts' ) }
+						</li>
+					</ul>
+				</div>
+
+				{ ( () => {
+					switch ( platform ) {
+						case 'Wix':
+							return <WixPanelInfo { ...data } />;
+						case 'Squarespace':
+							return <SquarespacePanelInfo { ...data } />;
+						case 'Blogger':
+							return <BloggerPanelInfo { ...data } />;
+						default:
+							return null;
+					}
+				} )() }
+			</div>
 		</Modal>
 	);
 };
