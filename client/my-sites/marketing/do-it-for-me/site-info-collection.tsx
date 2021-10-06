f6c02b54bc26bc56860345c659692323925e23db
Dehan <dehandecroos@gmail.com>
2021-10-06 12:24:38 +0530

    Send data as hidden data

--- client/my-sites/marketing/do-it-for-me/site-info-collection.tsx
+++ client/my-sites/marketing/do-it-for-me/site-info-collection.tsx
@@ -1,48 +1,51 @@
-import config from '@automattic/calypso-config';
+import { Button } from '@automattic/components';
+import { useShoppingCart } from '@automattic/shopping-cart';
 import styled from '@emotion/styled';
-import { Widget } from '@typeform/embed-react';
 import page from 'page';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import ActionPanel from 'calypso/components/action-panel';
 import ActionPanelBody from 'calypso/components/action-panel/body';
-import CardHeading from 'calypso/components/card-heading';
+import ActionPanelFooter from 'calypso/components/action-panel/footer';
 import FormattedHeader from 'calypso/components/formatted-header';
-import Spinner from 'calypso/components/spinner';
+import { fillInSingleCartItemAttributes } from 'calypso/lib/cart-values';
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
-import { getCurrentUserName } from 'calypso/state/current-user/selectors';
+import { getProductsList } from 'calypso/state/products-list/selectors';
 import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
-interface SiteInfoCollectionProps {
-	typeFormStyles?: any;
-	onSubmit?: () => void;
-}
-
-function getTypeformId(): string {
-	return config< string >( 'difm_typeform_id' );
-}
-
-const LoadingContainer = styled.div`
-	align-items: center;
-	display: flex;
-	flex-direction: column;
-	height: 500px;
-	justify-content: center;
+const TemporaryTypeformContainer = styled.div`
+	width: 95%;
+	height: 18em;
+	padding: 1em 0 0 1em;
+	background-color: #eee;
+	font-size: 2em;
+	color: #b8b8b8;
 `;
 
-function SiteInformationCollection( {
-	typeFormStyles = { width: '100%', height: '500px', padding: '1em 0 0 1em' },
-	onSubmit,
-}: SiteInfoCollectionProps ) {
+function SiteInformationCollection() {
 	const selectedSiteSlug = useSelector( getSelectedSiteSlug );
-	const username = useSelector( getCurrentUserName );
-	const [ isFormSubmitted, setIsFormSubmitted ] = useState( false );
+	const [ isAddingToBasket, setIsAddingToBasket ] = useState( false );
+	const products = useSelector( getProductsList );
 
-	const checkout = async () => {
-		setIsFormSubmitted( true );
-		selectedSiteSlug
-			? page( `/checkout/${ selectedSiteSlug }/wp_difm_lite` )
-			: page( '/checkout/wp_difm_lite' );
+	const { replaceProductsInCart } = useShoppingCart();
+
+	const onAddDIFMProductsToCart = async () => {
+		setIsAddingToBasket( true );
+		const difmProduct = fillInSingleCartItemAttributes(
+			{ product_slug: 'wp_difm_lite' },
+			products
+		);
+		const premiumPlan = fillInSingleCartItemAttributes(
+			{ product_slug: 'value_bundle' },
+			products
+		);
+		await replaceProductsInCart( [ difmProduct, premiumPlan ] );
+		page( `/marketing/do-it-for-me/site-info/${ selectedSiteSlug }` );
+	};
+	const onSubmit = async () => {
+		// After Submitting typeform checkout
+		await onAddDIFMProductsToCart();
+		page( `/checkout/${ selectedSiteSlug }` );
 	};
 
 	return (
@@ -50,41 +53,25 @@ function SiteInformationCollection( {
 			<ActionPanelBody>
 				<FormattedHeader
 					brandFont
-					headerText={ 'Tell us more about your site' }
-					subHeaderText={
-						'We need some basic details to build your site, you will also be able to get a glimpse of what your site will look like'
-					}
+					headerText="Tell us more about your site"
+					subHeaderText="We need some basic details to build your site, you will also be able to get a glimpse of what your site will look like"
 					align="left"
 				/>
-				{ isFormSubmitted ? (
-					<LoadingContainer>
-						<CardHeading tagName="h5" size={ 24 }>
-							{ 'Redirecting to Checkout…' }
-						</CardHeading>
-						<Spinner />
-					</LoadingContainer>
-				) : (
-					<Widget
-						hidden={ {
-							username,
-						} }
-						id={ getTypeformId() }
-						style={ typeFormStyles }
-						onSubmit={ () => ( onSubmit ? onSubmit() : checkout() ) }
-						disableAutoFocus={ true }
-					/>
-				) }
+				<TemporaryTypeformContainer>Typeform Content goes here</TemporaryTypeformContainer>
 			</ActionPanelBody>
+			<ActionPanelFooter>
+				<Button busy={ isAddingToBasket } primary onClick={ onSubmit }>
+					Checkout
+				</Button>
+			</ActionPanelFooter>
 		</ActionPanel>
 	);
 }
 
-export default function WrappedSiteInformationCollection(
-	props: SiteInfoCollectionProps
-): JSX.Element {
+export default function WrappedSiteInformationCollection(): JSX.Element {
 	return (
 		<CalypsoShoppingCartProvider>
-			<SiteInformationCollection { ...props } />
+			<SiteInformationCollection />
 		</CalypsoShoppingCartProvider>
 	);
 }
--- client/signup/steps/site-info-collection/index.jsx
+++ client/signup/steps/site-info-collection/index.jsx
@@ -1,14 +1,46 @@
+import config from '@automattic/calypso-config';
 import { WPCOM_DIFM_LITE } from '@automattic/calypso-products';
-import SiteInformationCollection from 'calypso/my-sites/marketing/do-it-for-me/site-info-collection';
+import styled from '@emotion/styled';
+import { Widget } from '@typeform/embed-react';
+import { useTranslate } from 'i18n-calypso';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import ActionPanel from 'calypso/components/action-panel';
+import ActionPanelBody from 'calypso/components/action-panel/body';
+import CardHeading from 'calypso/components/card-heading';
+import FormattedHeader from 'calypso/components/formatted-header';
+import Spinner from 'calypso/components/spinner';
+import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
+import { getCurrentUserName } from 'calypso/state/current-user/selectors';
+import { getSignupDependencyStore } from 'calypso/state/signup/dependency-store/selectors';
+
 import './style.scss';
 
-export default function SiteInfoCollectionStep( {
+function getTypeformId() {
+	return config( 'difm_typeform_id' );
+}
+
+const LoadingContainer = styled.div`
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	height: 500px;
+	justify-content: center;
+`;
+
+function SiteInformationCollection( {
 	additionalStepData,
 	stepName,
 	submitSignupStep,
 	goToNextStep,
 } ) {
+	const translate = useTranslate();
+	const { selectedDesign } = useSelector( getSignupDependencyStore );
+	const username = useSelector( getCurrentUserName );
+	const [ isFormSubmitted, setIsFormSubmitted ] = useState( false );
+
 	const nextStep = () => {
+		setIsFormSubmitted( true );
 		const cartItem = { product_slug: WPCOM_DIFM_LITE };
 		const step = {
 			stepName,
@@ -22,17 +54,52 @@ export default function SiteInfoCollectionStep( {
 	};
 
 	return (
-		<SiteInformationCollection
-			onSubmit={ nextStep }
-			typeFormStyles={ {
-				width: 'calc(100% - 2px)',
-				height: '50vh',
-				minHeight: '745px',
-				padding: '0',
-				marginTop: '50px',
-				border: '1px solid rgba( 220, 220, 222, 0.64 )',
-				borderRadius: '4px',
-			} }
-		/>
+		<ActionPanel>
+			<ActionPanelBody>
+				<FormattedHeader
+					brandFont
+					headerText={ translate( 'Tell us more about your site' ) }
+					subHeaderText={ translate(
+						'We need some basic details to build your site, you will also be able to get a glimpse of what your site will look like'
+					) }
+					align="left"
+				/>
+				{ isFormSubmitted ? (
+					<LoadingContainer>
+						<CardHeading tagName="h5" size={ 24 }>
+							{ translate( 'Redirecting to Checkout…' ) }
+						</CardHeading>
+						<Spinner />
+					</LoadingContainer>
+				) : (
+					<Widget
+						hidden={ {
+							username,
+							design: selectedDesign,
+						} }
+						id={ getTypeformId() }
+						style={ {
+							width: 'calc(100% - 2px)',
+							height: '50vh',
+							minHeight: '745px',
+							padding: '0',
+							marginTop: '50px',
+							border: '1px solid rgba( 220, 220, 222, 0.64 )',
+							borderRadius: '4px',
+						} }
+						onSubmit={ nextStep }
+						disableAutoFocus={ true }
+					/>
+				) }
+			</ActionPanelBody>
+		</ActionPanel>
+	);
+}
+
+export default function WrappedSiteInformationCollection( props ) {
+	return (
+		<CalypsoShoppingCartProvider>
+			<SiteInformationCollection { ...props } />
+		</CalypsoShoppingCartProvider>
 	);
 }
