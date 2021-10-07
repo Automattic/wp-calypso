b9516daf67b741932a3f444b604e2adf968d2f0e
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2021-10-07 14:40:20 -0300

    Annotate useGutenbergFSESettingsQuery's return type

--- client/data/gutenberg/fse-settings-query.ts
+++ client/data/gutenberg/fse-settings-query.ts
@@ -1,11 +1,13 @@
-import { useQuery } from 'react-query';
+import { useQuery, UseQueryResult } from 'react-query';
 import wpcom from 'calypso/lib/wp';
 
 export type GutenbergFSESettings = {
 	is_core_fse_eligible: boolean;
 };
 
-export const useGutenbergFSESettingsQuery = ( siteId: string ) => {
+export const useGutenbergFSESettingsQuery = (
+	siteId: string
+): UseQueryResult< GutenbergFSESettings > => {
 	const queryKey = `${ siteId }:gutenbergFSESettings`;
 
 	return useQuery< GutenbergFSESettings >( queryKey, () => {
