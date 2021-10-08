16192917f1229678d4771c1cb87fa77092e4df93
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-10-08 13:32:30 -0500

    Apply highlighting to features when they're marked as such

--- client/components/jetpack/card/jetpack-product-card/features-item.tsx
+++ client/components/jetpack/card/jetpack-product-card/features-item.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import classnames from 'classnames';
 import { preventWidows } from 'calypso/lib/formatting';
 import type { ProductCardFeaturesItem } from './types';
 
@@ -6,8 +6,14 @@ interface Props {
 	item: ProductCardFeaturesItem;
 }
 
-const JetpackProductCardFeaturesItem: React.FC< Props > = ( { item: { text } } ) => (
-	<li className="jetpack-product-card__features-item">{ preventWidows( text ) }</li>
+const JetpackProductCardFeaturesItem: React.FC< Props > = ( { item: { text, isHighlighted } } ) => (
+	<li
+		className={ classnames( 'jetpack-product-card__features-item', {
+			'is-highlighted': isHighlighted,
+		} ) }
+	>
+		{ preventWidows( text ) }
+	</li>
 );
 
 export default JetpackProductCardFeaturesItem;
