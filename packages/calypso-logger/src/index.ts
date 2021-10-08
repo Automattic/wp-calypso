bf6591f99f0bba1e5a06f98d07ed6486898b0063
Sergio Cinos <sergio.cinos@gmail.com>
2021-10-08 12:20:54 +0200

    Improve api

--- packages/calypso-logger/src/index.ts
+++ packages/calypso-logger/src/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 
 import log from 'loglevel';
-import type { LogLevelDesc } from 'loglevel';
+import type { LogLevelDesc, Logger } from 'loglevel';
 
 type ConsoleImpl = {
 	trace: ( ...args: unknown[] ) => void;
@@ -21,25 +21,46 @@ const nativeConsoleMethods: ConsoleImpl = {
 	error: window.console.error,
 };
 
-export const setup = ( logLevel: LogLevelDesc ): void => {
-	log.setDefaultLevel( logLevel );
-
-	log.methodFactory = function ( methodName ) {
-		return nativeConsoleMethods[ methodName as keyof ConsoleImpl ] ?? nativeConsoleMethods.log;
-	};
-	// This call is used to apply methodFactory changes
-	log.setLevel( log.getLevel() );
-
+const interceptNativeConsole = ( log: Logger ): void => {
 	window.console.trace = log.trace.bind( log );
 	window.console.debug = log.debug.bind( log );
 	window.console.log = log.log.bind( log );
 	window.console.info = log.info.bind( log );
 	window.console.warn = log.warn.bind( log );
 	window.console.error = log.error.bind( log );
-
-	window.__setLoggerLevel = log.setLevel.bind( log );
 };
 
+export const setup = ( logLevel: LogLevelDesc ): void => {
+	// Set the default level. This is not persisted on page reload
+	log.setDefaultLevel( logLevel );
+
+	// Delegate log methods to `nativeConsoleMethods`. We need to use native methods
+	// so they maintain the stack trace.
+	log.methodFactory = ( methodName ) =>
+		nativeConsoleMethods[ methodName as keyof ConsoleImpl ] ?? nativeConsoleMethods.log;
+
+	// This call is used to apply methodFactory changes
+	log.setLevel( log.getLevel() );
+
+	// Overwrite console.* methods so they defer to log.*
+	interceptNativeConsole( log );
+
+	window.__setLoggerLevel = ( level: LogLevelDesc, persist = false ) => {
+		// By default, don't persist the level.
+		log.setLevel( level, persist );
+		// loglevel works by setting some log methods to noop when we set a level, so we need to
+		// re-attach them to window.console
+		interceptNativeConsole( log );
+	};
+};
+
+/**
+ * Experimental debug-like API.
+ *
+ * Usage:
+ *   const log = debugLoggerFactory("foo");
+ *   log("bar") // will output 'foo bar' to the console
+ */
 export const debugLoggerFactory = ( name: string ): ( ( ...msg: unknown[] ) => void ) => {
 	const namedLogger = log.getLogger( name );
 	namedLogger.methodFactory = function ( methodName, logLevel, loggerName ) {
