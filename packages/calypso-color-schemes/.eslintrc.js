311a7cdf293a58ee11ff4b90853fb9338f99440d
Sergio Cinos <sergio.cinos@gmail.com>
2021-10-08 06:14:48 +0200

    Fix typo in path

--- packages/calypso-color-schemes/.eslintrc.js
+++ packages/calypso-color-schemes/.eslintrc.js
@@ -1,5 +1,5 @@
 const nodeConfig = require( '@automattic/calypso-eslint-overrides/node' );
 
 module.exports = {
-	overrides: [ { files: './bin', ...nodeConfig } ],
+	overrides: [ { files: './bin/**/*', ...nodeConfig } ],
 };
