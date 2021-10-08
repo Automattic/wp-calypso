19e90c8999ee3a716f12bc7d92eb8994246ffcae
Edwin Takahashi <egao@outlook.com>
2021-10-08 14:31:10 -0700

    Remove stress testing code

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -595,7 +595,7 @@ fun playwrightPrBuildType( targetDevice: String, buildUuid: String ): BuildType 
 					export NODE_CONFIG="{\"calypsoBaseURL\":\"${'$'}{URL%/}\"}"
 					export DEBUG=pw:api
 
-					for i in {1..160}; do yarn jest --reporters=jest-teamcity --reporters=default --maxWorkers=%E2E_WORKERS% specs/specs-playwright/wp-stats__insights; done
+					yarn jest --reporters=jest-teamcity --reporters=default --maxWorkers=%E2E_WORKERS% --group=calypso-pr
 				""".trimIndent()
 				dockerImage = "%docker_image_e2e%"
 			}
--- test/e2e/specs/specs-playwright/wp-stats__insights-spec.ts
+++ test/e2e/specs/specs-playwright/wp-stats__insights-spec.ts
@@ -2,7 +2,13 @@
  * @group calypso-pr
  */
 
-import { setupHooks, DataHelper, LoginPage } from '@automattic/calypso-e2e';
+import {
+	setupHooks,
+	DataHelper,
+	LoginFlow,
+	StatsPage,
+	SidebarComponent,
+} from '@automattic/calypso-e2e';
 import { Page } from 'playwright';
 
 describe( DataHelper.createSuiteTitle( 'Stats' ), function () {
@@ -17,21 +23,19 @@ describe( DataHelper.createSuiteTitle( 'Stats' ), function () {
 		${ 'Simple' } | ${ 'defaultUser' }
 		${ 'Atomic' } | ${ 'wooCommerceUser' }
 	`( 'View Insights ($siteType)', function ( { user } ) {
-		it( 'Log in', async function () {
-			const loginPage = new LoginPage( page );
-			await loginPage.login( { account: user } );
-			await page.click( 'a[data-tip-target="me"]' );
-			await page.waitForSelector( 'h1:text("My Profile")' );
+		it( 'Log In', async function () {
+			const loginFlow = new LoginFlow( page, user );
+			await loginFlow.logIn();
 		} );
-	} );
 
-	it( 'Log in with rejection', async function () {
-		const loginPage = new LoginPage( page );
-		await expect(
-			loginPage.login( {
-				username: 'a11ne2eflowtestingdomainonly1633711161507.6ob24pgn@mailosaur.io',
-				password: 'lkjwlerjwe',
-			} )
-		).rejects.toThrow();
+		it( 'Navigate to Stats', async function () {
+			const sidebarComponent = new SidebarComponent( page );
+			await sidebarComponent.navigate( 'Stats' );
+		} );
+
+		it( 'Click on Insights tab', async function () {
+			const statsPage = new StatsPage( page );
+			await statsPage.clickTab( 'Insights' );
+		} );
 	} );
 } );
