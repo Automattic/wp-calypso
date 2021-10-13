0fc669036a7092af0b1a4fd2affc68d84fa23126
Kev <kevin.zoschke@automattic.com>
2021-10-13 11:02:55 -0400

    Add superseded state to product cards in pricing page (#56829)
    * Add superseded state to product cards in pricing page
    
    * Replace superseded product card state by included state
    
    * Add Backup and Security tiers to upgrade maps
    
    * Fix TS error
    
    * Update URL of superseded product cards

---
+++ packages/calypso-products/src/get-jetpack-item-term-variants.ts
@@ -0,0 +1,26 @@
+import { JETPACK_PLANS_BY_TERM, JETPACK_PRODUCTS_BY_TERM } from './constants';
+import { isJetpackPlanSlug, isJetpackProductSlug, JetpackPurchasableItemSlug } from '.';
+
+type termMap = { yearly: JetpackPurchasableItemSlug; monthly: JetpackPurchasableItemSlug };
+
+/**
+ * Return the slugs of a Jetpack plan or product for all existing terms.
+ *
+ * @param itemSlug Slug of the Jetpack product or plan
+ * @returns Object with term and product slug as key/value
+ */
+export function getJetpackItemTermVariants(
+	itemSlug: JetpackPurchasableItemSlug
+): termMap | undefined {
+	if ( isJetpackPlanSlug( itemSlug ) ) {
+		return JETPACK_PLANS_BY_TERM.find( ( terms ) =>
+			( Object.values( terms ) as JetpackPurchasableItemSlug[] ).includes( itemSlug )
+		) as termMap;
+	}
+
+	if ( isJetpackProductSlug( itemSlug ) ) {
+		return JETPACK_PRODUCTS_BY_TERM.find( ( terms ) =>
+			( Object.values( terms ) as JetpackPurchasableItemSlug[] ).includes( itemSlug )
+		) as termMap;
+	}
+}
---
+++ packages/calypso-products/src/is-superseding-jetpack-item.ts
@@ -0,0 +1,39 @@
+import { getJetpackItemTermVariants } from './get-jetpack-item-term-variants';
+import {
+	isJetpackPlanSlug,
+	isJetpackProductSlug,
+	JetpackPurchasableItemSlug,
+	JETPACK_PLAN_UPGRADE_MAP,
+	JETPACK_PRODUCT_UPGRADE_MAP,
+	planHasSuperiorFeature,
+} from '.';
+
+/**
+ * Check if a Jetpack item (product or plan) is superseding another one. Based on yearly variants.
+ *
+ * @param supersedingItem Item potentially superseding the other one
+ * @param supersededItem Item potentially superseded by the other one
+ * @returns True if the first item is superseding the second one
+ */
+export default function isSupersedingJetpackItem(
+	supersedingItem: JetpackPurchasableItemSlug,
+	supersededItem: JetpackPurchasableItemSlug
+): boolean | undefined {
+	const yearlySuperseding = getJetpackItemTermVariants( supersedingItem )?.yearly;
+	const yearlySuperseded = getJetpackItemTermVariants( supersededItem )?.yearly;
+
+	if ( ! yearlySuperseding || ! yearlySuperseded ) {
+		return undefined;
+	}
+
+	if ( isJetpackPlanSlug( yearlySuperseding ) ) {
+		return (
+			JETPACK_PLAN_UPGRADE_MAP[ yearlySuperseding ]?.includes( yearlySuperseded ) ||
+			planHasSuperiorFeature( supersedingItem, supersededItem )
+		);
+	}
+
+	if ( isJetpackProductSlug( yearlySuperseding ) ) {
+		return JETPACK_PRODUCT_UPGRADE_MAP[ yearlySuperseding ]?.includes( yearlySuperseded );
+	}
+}
