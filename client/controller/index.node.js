812d317e5eb157cdb8b677fd0b93571d8ab3ba0b
Jeremy Yip <jeremy.yip@automattic.com>
2021-10-14 15:06:58 -0700

    Add inline comment to server side rendered ProviderWrappedLoggedOutLayout

--- client/controller/index.node.js
+++ client/controller/index.node.js
@@ -17,6 +17,23 @@ import { ssrSetupLocaleMiddleware } from './ssr-setup-locale.js';
  */
 export { setSectionMiddleware, setLocaleMiddleware } from './shared.js';
 
+/**
+ * Server side rendering (SSR) is used exclusively for logged out users. Normally, for
+ * logged out users, we wouldn't need the react-query QueryClientProvider. This is
+ * because only logged-in users have access to REST endpoints, hence no fetches made by
+ * react-query, and no need for the QueryClientProvider.
+ *
+ * We, however add it here in order to prevent errors when server side rendering (SSR).
+ * For context, there are components shared between logged-in and logged-out views.
+ * If ProviderWrappedLoggedOutLayout initializes content that contains a useQuery call
+ * in one of these shared components (even if it's not enabled), SSR will throw an error
+ * regarding an inaccessible QueryClient.
+ *
+ * Ideally, useQuery would only be called for logged-in routes on a site, but ensuring
+ * that will require more planning and deliberate refactoring.
+ *
+ * https://github.com/Automattic/wp-calypso/pull/56916#issuecomment-942730743
+ */
 const ProviderWrappedLoggedOutLayout = ( {
 	store,
 	queryClient = new QueryClient(),
