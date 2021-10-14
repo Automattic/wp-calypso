44c11f656a2811bc5282192c32cf0ec5a70f9a84
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-10-14 18:24:36 -0300

    Add help bubble component and functionality

--- client/my-sites/domains/domain-management/components/breadcrumbs/index.jsx
+++ client/my-sites/domains/domain-management/components/breadcrumbs/index.jsx
@@ -2,6 +2,7 @@ import { Gridicon } from '@automattic/components';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import InfoPopover from 'calypso/components/info-popover';
 
 import './style.scss';
 
@@ -13,12 +14,12 @@ import './style.scss';
  * {
  *   label (required): string,
  *   href (optional): string,
- *   helpText (optional): string,
+ *   helpBubble (optional): string|JSX element,
  * }
  *
  * `label` is the text that will be rendered for that item. If `href` is present, it will render the
- * label as a link. If `helpText` is present, a help bubble with the informed text will be rendered
- * besides that item's label.
+ * label as a link. If `helpBubble` is present, a help bubble with a popover containing the informed
+ * text or element will be rendered besides that item's label.
  *
  * The `buttons` property is optional and can contain an array of Buttons that will be rendered on the
  * right end of the breadcrumbs section.
@@ -29,6 +30,22 @@ const Breadcrumbs = ( { items, buttons, className } ) => {
 			return <a href={ item.href }>{ item.label }</a>;
 		}
 		return item.label || item;
+	};
+
+	const renderHelpBubble = ( item ) => {
+		if ( ! item.helpBubble ) {
+			return null;
+		}
+		return (
+			<InfoPopover
+				className="breadcrumbs__help-bubble"
+				icon="help-outline"
+				position={ 'right' }
+				screenReaderText={ 'Learn more' }
+			>
+				{ item.helpBubble }
+			</InfoPopover>
+		);
 	};
 
 	const renderSeparator = ( index ) => {
@@ -43,6 +60,7 @@ const Breadcrumbs = ( { items, buttons, className } ) => {
 	const renderItem = ( item, index ) => (
 		<React.Fragment key={ `breadcrumb${ index }` }>
 			<span>{ renderItemLabel( item ) }</span>
+			{ renderHelpBubble( item ) }
 			{ renderSeparator( index ) }
 		</React.Fragment>
 	);
