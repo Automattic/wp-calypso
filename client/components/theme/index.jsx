56bf82b3f7e93987f9a88527f3497094284af4ce
Jeremy Yip <jeremy.yip7@gmail.com>
2021-10-15 10:33:46 -0700

    Themes Showcase: Only display beta badges for FSE beta eligible sites (#56916)
    * In preparation for the full site editing beta, we're using an `fse-eligible` blog sticker to dictate whether or not certain visual elements are displayed to end users. This PR ensures that FSE theme beta badges are only displayed when the blog sticker is present.

--- client/components/theme/index.jsx
+++ client/components/theme/index.jsx
@@ -9,13 +9,12 @@ import { connect } from 'react-redux';
 import Badge from 'calypso/components/badge';
 import InfoPopover from 'calypso/components/info-popover';
 import PulsingDot from 'calypso/components/pulsing-dot';
+import withBlockEditorSettings from 'calypso/data/block-editor/with-block-editor-settings';
 import TrackComponentView from 'calypso/lib/analytics/track-component-view';
 import { decodeEntities } from 'calypso/lib/formatting';
 import { isFullSiteEditingTheme } from 'calypso/my-sites/themes/is-full-site-editing-theme';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import isSiteUsingCoreSiteEditorSelector from 'calypso/state/selectors/is-site-using-core-site-editor';
 import { setThemesBookmark } from 'calypso/state/themes/themes-ui/actions';
-import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import ThemeMoreButton from './more-button';
 
 import './style.scss';
@@ -72,7 +71,9 @@ export class Theme extends Component {
 			PropTypes.func,
 			PropTypes.shape( { current: PropTypes.any } ),
 		] ),
-		isSiteUsingCoreSiteEditor: PropTypes.bool,
+		blockEditorSettings: PropTypes.shape( {
+			is_fse_eligible: PropTypes.bool,
+		} ),
 	};
 
 	static defaultProps = {
@@ -144,7 +145,7 @@ export class Theme extends Component {
 	};
 
 	render() {
-		const { active, isSiteUsingCoreSiteEditor, price, theme, translate, upsellUrl } = this.props;
+		const { active, blockEditorSettings, price, theme, translate, upsellUrl } = this.props;
 		const { name, description, screenshot } = theme;
 		const isActionable = this.props.screenshotClickUrl || this.props.onScreenshotClick;
 		const themeClass = classNames( 'theme', {
@@ -205,7 +206,8 @@ export class Theme extends Component {
 		const e2eThemeName = name.toLowerCase().replace( /\s+/g, '-' );
 
 		const bookmarkRef = this.props.bookmarkRef ? { ref: this.props.bookmarkRef } : {};
-		const showBetaBadge = isFullSiteEditingTheme( this.props.theme ) && isSiteUsingCoreSiteEditor;
+		const isFSEEligible = blockEditorSettings?.is_fse_eligible ?? false;
+		const showBetaBadge = isFullSiteEditingTheme( this.props.theme ) && isFSEEligible;
 
 		return (
 			<Card className={ themeClass } data-e2e-theme={ e2eThemeName } onClick={ this.setBookmark }>
@@ -275,12 +277,8 @@ export class Theme extends Component {
 	}
 }
 
-export default connect(
-	( state ) => {
-		const siteId = getSelectedSiteId( state );
-		return {
-			isSiteUsingCoreSiteEditor: isSiteUsingCoreSiteEditorSelector( state, siteId ),
-		};
-	},
-	{ recordTracksEvent, setThemesBookmark }
-)( localize( Theme ) );
+const ThemeWithEditorSettings = withBlockEditorSettings( Theme );
+
+export default connect( null, { recordTracksEvent, setThemesBookmark } )(
+	localize( ThemeWithEditorSettings )
+);
