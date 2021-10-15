d109f7a1640a0eca5a71dd6b1c59fec10eae96a4
cpap <papazoglou.charalampos@gmail.com>
2021-10-15 19:05:46 +0300

    Themes generic marketplace page.

--- client/my-sites/marketplace/pages/marketplace-stand-alone-thank-you/index.tsx
+++
@@ -1,5 +0,0 @@
-import MarketplaceThankYou from 'calypso/my-sites/checkout/checkout-thank-you/marketplace/marketplace-thank-you';
-
-export default function MarketplaceStandaloneThankYou(): JSX.Element {
-	return <MarketplaceThankYou />;
-}
