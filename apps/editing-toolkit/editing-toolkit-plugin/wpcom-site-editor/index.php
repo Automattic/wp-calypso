9b5d05b4960fc5bb8949d351e0d8fa6f9922ed1e
Addison Stavlo <Stavz01@gmail.com>
2021-10-15 12:37:55 -0400

    ETK: Site Editor overrides - remove unnecessary css rule and file. (#57034)
    * force importance
    
    * remove style.scss
    
    * dont forget the import, duh
    
    * remove enqueue style

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-site-editor/index.php
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-site-editor/index.php
@@ -10,7 +10,7 @@ namespace A8C\FSE;
 /**
  * Enqueue block editor assets.
  */
-function wpcom_site_editor_script_and_style() {
+function wpcom_site_editor_script() {
 	$asset_file          = include plugin_dir_path( __FILE__ ) . 'dist/wpcom-site-editor.asset.php';
 	$script_dependencies = $asset_file['dependencies'];
 	$version             = $asset_file['version'];
@@ -22,13 +22,5 @@ function wpcom_site_editor_script_and_style() {
 		$version,
 		true
 	);
-
-	$style_path = 'dist/wpcom-site-editor' . ( is_rtl() ? '.rtl' : '' ) . '.css';
-	wp_enqueue_style(
-		'wpcom-site-editor-style',
-		plugins_url( $style_path, __FILE__ ),
-		array(),
-		filemtime( plugin_dir_path( __FILE__ ) . $style_path )
-	);
 }
-add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\wpcom_site_editor_script_and_style' );
+add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\wpcom_site_editor_script' );
