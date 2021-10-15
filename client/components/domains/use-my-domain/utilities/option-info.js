03120d42fa3bb552160a446d55d91553ea70f74a
Sorin Nunca <s.d.nunca@gmail.com>
2021-10-15 16:16:59 +0300

    i18n: Fix translations on the Use my domain screen

--- client/components/domains/use-my-domain/utilities/option-info.js
+++ client/components/domains/use-my-domain/utilities/option-info.js
@@ -8,35 +8,96 @@ const optionTitleText = {
 	connect: __( 'Connect your domain' ),
 };
 
+const transferSupported = {
+	illustration: transferIllustration,
+	titleText: optionTitleText.transfer,
+	topText: __( 'Manage your domain directly on WordPress.com' ),
+	learnMoreLink: INCOMING_DOMAIN_TRANSFER,
+	benefits: [
+		__( "We'll renew your domain for another year" ),
+		__( 'Manage everything you need in one place' ),
+		__( 'Private domain registration and SSL certificate included for free' ),
+	],
+};
+
+const transferNotSupported = {
+	illustration: transferIllustration,
+	titleText: optionTitleText.transfer,
+	topText: __( 'This domain cannot be transfered.' ),
+	learnMoreLink: INCOMING_DOMAIN_TRANSFER,
+};
+
+const connectSupported = {
+	illustration: connectIllustration,
+	titleText: optionTitleText.connect,
+	topText: __( 'Keep your domain with your current provider and point it to WordPress.com' ),
+	learnMoreLink: MAP_EXISTING_DOMAIN,
+	benefits: [ __( 'Keep your current provider' ), __( 'SSL certificate included for free' ) ],
+};
+
+const connectNotSupported = {
+	illustration: connectIllustration,
+	titleText: optionTitleText.connect,
+	topText: __( 'This domain cannot be connected.' ),
+	learnMoreLink: MAP_EXISTING_DOMAIN,
+};
 export const optionInfo = {
-	transferSupported: {
-		illustration: transferIllustration,
-		titleText: optionTitleText.transfer,
-		topText: __( 'Manage your domain directly on WordPress.com' ),
-		learnMoreLink: INCOMING_DOMAIN_TRANSFER,
-		benefits: [
+	transferSupported,
+	transferNotSupported,
+	connectSupported,
+	connectNotSupported,
+};
+
+/**
+ * Define properties with translatable strings getters.
+ */
+Object.defineProperties( optionTitleText, {
+	transfer: {
+		get: () => __( 'Transfer your domain' ),
+	},
+	connect: {
+		get: () => __( 'Connect your domain' ),
+	},
+} );
+Object.defineProperties( transferSupported, {
+	topText: {
+		get: () => __( 'Manage your domain directly on WordPress.com' ),
+	},
+	benefits: {
+		get: () => [
 			__( "We'll renew your domain for another year" ),
 			__( 'Manage everything you need in one place' ),
 			__( 'Private domain registration and SSL certificate included for free' ),
 		],
 	},
-	transferNotSupported: {
-		illustration: transferIllustration,
-		titleText: optionTitleText.transfer,
-		topText: __( 'This domain cannot be transfered.' ),
-		learnMoreLink: INCOMING_DOMAIN_TRANSFER,
+	titleText: {
+		get: () => optionTitleText.transfer,
 	},
-	connectSupported: {
-		illustration: connectIllustration,
-		titleText: optionTitleText.connect,
-		topText: __( 'Keep your domain with your current provider and point it to WordPress.com' ),
-		learnMoreLink: MAP_EXISTING_DOMAIN,
-		benefits: [ __( 'Keep your current provider' ), __( 'SSL certificate included for free' ) ],
+} );
+Object.defineProperties( transferNotSupported, {
+	topText: {
+		get: () => __( 'This domain cannot be transfered.' ),
 	},
-	connectNotSupported: {
-		illustration: connectIllustration,
-		titleText: optionTitleText.connect,
-		topText: __( 'This domain cannot be connected.' ),
-		learnMoreLink: MAP_EXISTING_DOMAIN,
+	titleText: {
+		get: () => optionTitleText.transfer,
 	},
-};
+} );
+Object.defineProperties( connectSupported, {
+	topText: {
+		get: () => __( 'Keep your domain with your current provider and point it to WordPress.com' ),
+	},
+	benefits: {
+		get: () => [ __( 'Keep your current provider' ), __( 'SSL certificate included for free' ) ],
+	},
+	titleText: {
+		get: () => optionTitleText.connect,
+	},
+} );
+Object.defineProperties( connectNotSupported, {
+	topText: {
+		get: () => __( 'This domain cannot be connected.' ),
+	},
+	titleText: {
+		get: () => optionTitleText.connect,
+	},
+} );
