f3ae2a1c68e45dd198c0c2eaed3e74e4031e794b
Razvan Papadopol <razvan.papadopol@automattic.com>
2021-10-17 19:01:53 +0300

    Add Help Center modal as a block

---
+++ client/blocks/help-center-modal/index.jsx
@@ -0,0 +1,32 @@
+import { Modal } from '@wordpress/components';
+import { Icon, wordpress } from '@wordpress/icons';
+import { removeQueryArgs } from '@wordpress/url';
+import { useTranslate } from 'i18n-calypso';
+import page from 'page';
+import HelpComponent from 'calypso/me/help/main';
+import './style.scss';
+
+const HelpCentertModal = ( { isOpen } ) => {
+	const translate = useTranslate();
+
+	const onClose = () =>
+		page.redirect(
+			removeQueryArgs( window.location.pathname + window.location.search, 'showHelpCenter' )
+		);
+
+	return (
+		isOpen && (
+			<Modal
+				overlayClassName="help-center-modal"
+				onRequestClose={ onClose }
+				title={ String( translate( 'Help Center' ) ) }
+				shouldCloseOnClickOutside={ false }
+				icon={ <Icon icon={ wordpress } size={ 36 } /> }
+			>
+				<HelpComponent />
+			</Modal>
+		)
+	);
+};
+
+export default HelpCentertModal;
