050dbedc372028aa409fa063b9a8a4f33bd2d0ba
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2021-10-18 16:27:26 +0300

    Fix leaving page with unsaved changes. Fix opening child block settings panel. More blocks definitions.

--- test/e2e/specs/specs-i18n/wp-i18n__editor.ts
+++ test/e2e/specs/specs-i18n/wp-i18n__editor.ts
@@ -61,44 +61,102 @@ const translations = {
 			},
 		],
 	},
-	// fr: {
-	// 	blocks: [
-	// 		{
-	// 			blockName: 'Applaudissement',
-	// 			blockEditorSelector: '[data-type="crowdsignal-forms/applause"]',
-	// 			blockEditorContent: [ ':text("Claps")' ],
-	// 			blockPanelTitle: 'Applaudissement',
-	// 		},
-	// 		{
-	// 			blockName: 'Sondage',
-	// 			blockEditorSelector: '[data-type="crowdsignal-forms/poll"]',
-	// 			blockEditorContent: [
-	// 				'[aria-label="Entrez votre question"]',
-	// 				'[aria-label="Ajouter une note (facultatif)"]',
-	// 			],
-	// 			blockPanelTitle: 'Sondage',
-	// 		},
-	// 	],
-	// },
-	// he: {
-	// 	blocks: [
-	// 		{
-	// 			blockName: 'מחיאות כפיים',
-	// 			blockEditorSelector: '[data-type="crowdsignal-forms/applause"]',
-	// 			blockEditorContent: [ ':text("Claps")' ],
-	// 			blockPanelTitle: 'מחיאות כפיים',
-	// 		},
-	// 		{
-	// 			blockName: 'סקר',
-	// 			blockEditorSelector: '[data-type="crowdsignal-forms/poll"]',
-	// 			blockEditorContent: [
-	// 				'[aria-label="יש להזין את השאלה שלך"]',
-	// 				'[aria-label="להוסיף פתק (אופציונלי)"]',
-	// 			],
-	// 			blockPanelTitle: 'סקר',
-	// 		},
-	// 	],
-	// },
+	fr: {
+		blocks: [
+			// Core
+			{
+				blockName: 'Image',
+				blockEditorSelector: '[data-type="core/image"]',
+				blockEditorContent: [
+					'.components-placeholder__label:has-text("Image")',
+					'.jetpack-external-media-button-menu:text("Select Image")', // Jetpack extension
+				],
+				blockPanelTitle: 'Image',
+			},
+
+			// Jetpack
+			{
+				blockName: 'Contact Form',
+				blockEditorSelector: '[data-type="jetpack/contact-form"]',
+				blockEditorContent: [ ':text("Name")', ':text("Email")', ':text("Message")' ],
+				blockPanelTitle: 'Form',
+			},
+			{
+				blockName: 'Business Hours',
+				blockEditorSelector: '[data-type="jetpack/business-hours"]',
+				blockEditorContent: [
+					'.business-hours__day-name:text("lundi")',
+					'.business-hours__hours:has-text("Add Hours")',
+				],
+				blockPanelTitle: 'Business Hours',
+			},
+
+			// Crowdsignal Forms
+			{
+				blockName: 'Applaudissement',
+				blockEditorSelector: '[data-type="crowdsignal-forms/applause"]',
+				blockEditorContent: [ ':text("Claps")' ],
+				blockPanelTitle: 'Applaudissement',
+			},
+			{
+				blockName: 'Sondage',
+				blockEditorSelector: '[data-type="crowdsignal-forms/poll"]',
+				blockEditorContent: [
+					'[aria-label="Entrez votre question"]',
+					'[aria-label="Ajouter une note (facultatif)"]',
+				],
+				blockPanelTitle: 'Sondage',
+			},
+		],
+	},
+	he: {
+		blocks: [
+			// Core
+			{
+				blockName: 'תמונה',
+				blockEditorSelector: '[data-type="core/image"]',
+				blockEditorContent: [
+					'.components-placeholder__label:has-text("תמונה")',
+					'.jetpack-external-media-button-menu:text("Select Image")', // Jetpack extension
+				],
+				blockPanelTitle: 'תמונה',
+			},
+
+			// Jetpack
+			{
+				blockName: 'Contact Form',
+				blockEditorSelector: '[data-type="jetpack/contact-form"]',
+				blockEditorContent: [ ':text("Name")', ':text("Email")', ':text("Message")' ],
+				blockPanelTitle: 'Form',
+			},
+			{
+				blockName: 'Business Hours',
+				blockEditorSelector: '[data-type="jetpack/business-hours"]',
+				blockEditorContent: [
+					'.business-hours__day-name:text("יום שני")',
+					'.business-hours__hours:has-text("Add Hours")',
+				],
+				blockPanelTitle: 'Business Hours',
+			},
+
+			// Crowdsignal Forms
+			{
+				blockName: 'מחיאות כפיים',
+				blockEditorSelector: '[data-type="crowdsignal-forms/applause"]',
+				blockEditorContent: [ ':text("Claps")' ],
+				blockPanelTitle: 'מחיאות כפיים',
+			},
+			{
+				blockName: 'סקר',
+				blockEditorSelector: '[data-type="crowdsignal-forms/poll"]',
+				blockEditorContent: [
+					'[aria-label="יש להזין את השאלה שלך"]',
+					'[aria-label="להוסיף פתק (אופציונלי)"]',
+				],
+				blockPanelTitle: 'סקר',
+			},
+		],
+	},
 };
 const locales = Object.keys( translations );
 
@@ -108,11 +166,17 @@ describe( DataHelper.createSuiteTitle( 'Editor Translations' ), function () {
 
 	setupHooks( ( args ) => {
 		page = args.page;
+
+		// Confirm page leave with unsaved changes prompt.
+		page.on( 'dialog', async ( dialog ) => {
+			if ( dialog.type() === 'beforeunload' ) {
+				await dialog.accept();
+			}
+		} );
 	} );
 
 	it( 'Log in', async function () {
 		const loginFlow = new LoginFlow( page, 'i18nUser' ); // @todo: Login with i18n testing account.
-		// const loginFlow = new LoginFlow( page, 'gutenbergSimpleSiteUser' ); // @todo: Login with i18n testing account.
 		await loginFlow.logIn();
 	} );
 
@@ -145,9 +209,13 @@ describe( DataHelper.createSuiteTitle( 'Editor Translations' ), function () {
 					await frame.waitForSelector( `${ block.blockEditorSelector } ${ content }` );
 				} );
 
+				// Open block settings.
 				const settingsToggleLabel = await frame.evaluate( 'wp?.i18n?.__( "Settings" )' );
 				await gutenbergEditorPage.openSettings( `[aria-label="${ settingsToggleLabel }"]` );
 				await frame.click( block.blockEditorSelector );
+				if ( await frame.isVisible( '.block-editor-block-parent-selector__button' ) ) {
+					await frame.click( '.block-editor-block-parent-selector__button' );
+				}
 
 				await frame.waitForSelector(
 					`.block-editor-block-card__title:has-text("${ block.blockPanelTitle }")`
