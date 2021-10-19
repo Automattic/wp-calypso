dbe82216d32b1781becd3fcdc5b399fdc8324d50
Marin Atanasov <tyxla@abv.bg>
2021-10-19 14:14:17 +0300

    Remove default arg of maybeFetch()

--- client/components/data/query-reader-post/index.jsx
+++ client/components/data/query-reader-post/index.jsx
@@ -18,9 +18,11 @@ class QueryReaderPost extends Component {
 		this.maybeFetch();
 	}
 
-	maybeFetch = ( props = this.props ) => {
-		if ( isPostKeyLike( props.postKey ) && ( ! props.post || props.post._state === 'minimal' ) ) {
-			this.props.fetchPost( props.postKey );
+	maybeFetch = () => {
+		const { post, postKey } = this.props;
+
+		if ( isPostKeyLike( postKey ) && ( ! post || post._state === 'minimal' ) ) {
+			this.props.fetchPost( postKey );
 		}
 	};
 
