7436aadf1d32ff544fe507655b08094c83c8bdc0
Copons <Copons@users.noreply.github.com>
2021-10-20 12:03:15 +0100

    Update FSE Beta opt-in step design

--- client/landing/gutenboarding/onboarding-block/fse-beta-opt-in/index.tsx
+++ client/landing/gutenboarding/onboarding-block/fse-beta-opt-in/index.tsx
@@ -1,11 +1,4 @@
-import {
-	Title,
-	SubTitle,
-	ActionButtons,
-	NextButton,
-	BackButton,
-	SkipButton,
-} from '@automattic/onboarding';
+import { Title, SubTitle, ActionButtons, NextButton, SkipButton } from '@automattic/onboarding';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { Icon, siteLogo, header, navigation, arrowRight } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
@@ -19,7 +12,7 @@ import './style.scss';
 
 const FseBetaOptIn: FunctionComponent = () => {
 	const { __ } = useI18n();
-	const { goNext, goBack } = useStepNavigation();
+	const { goNext } = useStepNavigation();
 	const { enrollInFseBeta } = useDispatch( ONBOARD_STORE );
 	const { shouldEnrollInFseBeta } = useSelect( ( select ) => select( ONBOARD_STORE ) );
 	const pickBeta = ( shouldEnroll: boolean ) => {
@@ -37,47 +30,39 @@ const FseBetaOptIn: FunctionComponent = () => {
 	return (
 		<div className="gutenboarding-page fse-beta-opt-in">
 			<div className="fse-beta-opt-in__header">
-				<div>
-					<Title>{ __( 'Join Our Full Site Editing Beta' ) }</Title>
-					<SubTitle>{ __( 'Experience the new WordPress.com site editing features' ) }</SubTitle>
-				</div>
-				<ActionButtons className="fse-beta-opt-in__actions">
-					<BackButton onClick={ goBack } />
-					<SkipButton onClick={ () => pickBeta( false ) } />
-				</ActionButtons>
+				<Title>{ __( 'Join Our Full Site Editing Beta' ) }</Title>
+				<SubTitle>{ __( 'Experience the new WordPress.com site editing features' ) }</SubTitle>
 			</div>
-			<div className="fse-beta-opt-in__content">
-				<ol>
-					<li>
-						<Icon icon={ siteLogo } />
-						<h3>{ __( 'Edit Your Logo' ) }</h3>
-						<p>
-							{ __(
-								'Change and update your logo and other header elements without leaving the editor'
-							) }
-						</p>
-					</li>
-					<li>
-						<Icon icon={ header } />
-						<h3>{ __( 'Update Your Header and Footer' ) }</h3>
-						<p>
-							{ __(
-								'Full Site Editing allows you to make changes on your site header and footer.'
-							) }
-						</p>
-					</li>
-					<li>
-						<Icon icon={ navigation } />
-						<h3>{ __( 'Build Your Navigation' ) }</h3>
-						<p>{ __( 'Take your site menus to the next level by adding blocks to it.' ) }</p>
-					</li>
-				</ol>
-
-				<NextButton className="fse-beta-opt-in__submit" onClick={ () => pickBeta( true ) }>
+			<ol className="fse-beta-opt-in__content">
+				<li>
+					<Icon icon={ siteLogo } />
+					<h3>{ __( 'Edit Your Logo' ) }</h3>
+					<p>
+						{ __(
+							'Change and update your logo and other header elements without leaving the editor'
+						) }
+					</p>
+				</li>
+				<li>
+					<Icon icon={ header } />
+					<h3>{ __( 'Update Your Header and Footer' ) }</h3>
+					<p>
+						{ __( 'Full Site Editing allows you to make changes on your site header and footer.' ) }
+					</p>
+				</li>
+				<li>
+					<Icon icon={ navigation } />
+					<h3>{ __( 'Build Your Navigation' ) }</h3>
+					<p>{ __( 'Take your site menus to the next level by adding blocks to it.' ) }</p>
+				</li>
+			</ol>
+			<ActionButtons className="fse-beta-opt-in__actions">
+				<SkipButton onClick={ () => pickBeta( false ) } />
+				<NextButton onClick={ () => pickBeta( true ) }>
 					{ __( 'Enroll in Beta' ) }
 					<Icon icon={ arrowRight } />
 				</NextButton>
-			</div>
+			</ActionButtons>
 		</div>
 	);
 };
