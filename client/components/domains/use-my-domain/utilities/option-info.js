bf1c5f852b73c4840964daf255a0902aeafca65d
sdnunca <sorin@php.net>
2021-10-20 03:18:47 +0300

    Use gettter syntax instead of Object.defineProperties

--- client/components/domains/use-my-domain/utilities/option-info.js
+++ client/components/domains/use-my-domain/utilities/option-info.js
@@ -4,41 +4,65 @@ import connectIllustration from 'calypso/assets/images/illustrations/domain-conn
 import { INCOMING_DOMAIN_TRANSFER, MAP_EXISTING_DOMAIN } from 'calypso/lib/url/support';
 
 const optionTitleText = {
-	transfer: __( 'Transfer your domain' ),
-	connect: __( 'Connect your domain' ),
+	get transfer() {
+		return __( 'Transfer your domain' );
+	},
+	get connect() {
+		return __( 'Connect your domain' );
+	},
 };
 
 const transferSupported = {
 	illustration: transferIllustration,
-	titleText: optionTitleText.transfer,
-	topText: __( 'Manage your domain directly on WordPress.com' ),
+	get titleText() {
+		return optionTitleText.transfer;
+	},
+	get topText() {
+		return __( 'Manage your domain directly on WordPress.com' );
+	},
 	learnMoreLink: INCOMING_DOMAIN_TRANSFER,
-	benefits: [
-		__( "We'll renew your domain for another year" ),
-		__( 'Manage everything you need in one place' ),
-		__( 'Private domain registration and SSL certificate included for free' ),
-	],
+	get benefits() {
+		return [
+			__( "We'll renew your domain for another year" ),
+			__( 'Manage everything you need in one place' ),
+			__( 'Private domain registration and SSL certificate included for free' ),
+		];
+	},
 };
 
 const transferNotSupported = {
 	illustration: transferIllustration,
-	titleText: optionTitleText.transfer,
-	topText: __( 'This domain cannot be transfered.' ),
+	get titleText() {
+		return optionTitleText.transfer;
+	},
+	get topText() {
+		return __( 'This domain cannot be transfered.' );
+	},
 	learnMoreLink: INCOMING_DOMAIN_TRANSFER,
 };
 
 const connectSupported = {
 	illustration: connectIllustration,
-	titleText: optionTitleText.connect,
-	topText: __( 'Keep your domain with your current provider and point it to WordPress.com' ),
+	get titleText() {
+		return optionTitleText.connect;
+	},
+	get topText() {
+		return __( 'Keep your domain with your current provider and point it to WordPress.com' );
+	},
 	learnMoreLink: MAP_EXISTING_DOMAIN,
-	benefits: [ __( 'Keep your current provider' ), __( 'SSL certificate included for free' ) ],
+	get benefits() {
+		return [ __( 'Keep your current provider' ), __( 'SSL certificate included for free' ) ];
+	},
 };
 
 const connectNotSupported = {
 	illustration: connectIllustration,
-	titleText: optionTitleText.connect,
-	topText: __( 'This domain cannot be connected.' ),
+	get titleText() {
+		return optionTitleText.connect;
+	},
+	get topText() {
+		return __( 'This domain cannot be connected.' );
+	},
 	learnMoreLink: MAP_EXISTING_DOMAIN,
 };
 export const optionInfo = {
@@ -47,57 +71,3 @@ export const optionInfo = {
 	connectSupported,
 	connectNotSupported,
 };
-
-/**
- * Define properties with translatable strings getters.
- */
-Object.defineProperties( optionTitleText, {
-	transfer: {
-		get: () => __( 'Transfer your domain' ),
-	},
-	connect: {
-		get: () => __( 'Connect your domain' ),
-	},
-} );
-Object.defineProperties( transferSupported, {
-	topText: {
-		get: () => __( 'Manage your domain directly on WordPress.com' ),
-	},
-	benefits: {
-		get: () => [
-			__( "We'll renew your domain for another year" ),
-			__( 'Manage everything you need in one place' ),
-			__( 'Private domain registration and SSL certificate included for free' ),
-		],
-	},
-	titleText: {
-		get: () => optionTitleText.transfer,
-	},
-} );
-Object.defineProperties( transferNotSupported, {
-	topText: {
-		get: () => __( 'This domain cannot be transfered.' ),
-	},
-	titleText: {
-		get: () => optionTitleText.transfer,
-	},
-} );
-Object.defineProperties( connectSupported, {
-	topText: {
-		get: () => __( 'Keep your domain with your current provider and point it to WordPress.com' ),
-	},
-	benefits: {
-		get: () => [ __( 'Keep your current provider' ), __( 'SSL certificate included for free' ) ],
-	},
-	titleText: {
-		get: () => optionTitleText.connect,
-	},
-} );
-Object.defineProperties( connectNotSupported, {
-	topText: {
-		get: () => __( 'This domain cannot be connected.' ),
-	},
-	titleText: {
-		get: () => optionTitleText.connect,
-	},
-} );
