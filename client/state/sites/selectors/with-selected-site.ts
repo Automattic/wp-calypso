2ca10f533b190aa6469a8a3ce46b0b1933d05ea6
Louis Laugesen <louis.laugesen@gmail.com>
2021-10-21 16:16:50 +1100

    Typing improvements
    props: @p-jackson

--- client/state/sites/selectors/with-selected-site.ts
+++ client/state/sites/selectors/with-selected-site.ts
@@ -1,7 +1,10 @@
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import type { AppState } from 'calypso/types';
 
-export type SelectedSiteSelector = ( state: AppState, siteId: number | null ) => unknown;
+export type SelectedSiteSelector< Result > = (
+	state: AppState,
+	siteId: ReturnType< typeof getSelectedSiteId >
+) => Result;
 
 /**
  * Closes over the getSelectedSiteId step when using selected site selectors.
@@ -13,9 +16,8 @@ export type SelectedSiteSelector = ( state: AppState, siteId: number | null ) =>
  * @param selector A selector that takes both state and a site id.
  * @returns A selector that only takes application state for use in useSelector.
  */
-export default function withSelectedSite( selector: SelectedSiteSelector ) {
-	return ( state: AppState ): unknown => {
-		const siteId = getSelectedSiteId( state );
-		return selector( state, siteId );
-	};
+export default function withSelectedSite< Result >(
+	selector: SelectedSiteSelector< Result >
+): ( state: AppState ) => Result {
+	return ( state ) => selector( state, getSelectedSiteId( state ) );
 }
