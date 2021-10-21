56e327827483bc64b1097b36f3e3bdf95a173be0
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2021-10-21 19:21:59 +0300

    Removed is_locked from Subscription.

--- client/state/purchases/selectors/is-purchase-management-locked.js
+++ client/state/purchases/selectors/is-purchase-management-locked.js
@@ -1,14 +1,4 @@
-import { getByPurchaseId } from 'calypso/state/purchases/selectors/get-by-purchase-id';
-
-export const isPurchaseManagementLocked = ( state, purchaseId ) => {
-	if ( ! purchaseId ) {
-		return false;
-	}
-
-	const purchase = getByPurchaseId( state, purchaseId );
-	if ( ! purchase ) {
-		return false;
-	}
-
-	return purchase.isLocked;
+export const isPurchaseManagementLocked = () => {
+	// @todo: This function should be removed.
+	return false;
 };
