973188e8893fe5f2a11d673100d7665155218ac3
Copons <Copons@users.noreply.github.com>
2021-10-21 14:21:16 +0100

    Skip the FSE Beta flow in E2E tests

--- client/landing/gutenboarding/hooks/use-fse-beta-opt-in-step.ts
+++ client/landing/gutenboarding/hooks/use-fse-beta-opt-in-step.ts
@@ -1,3 +1,4 @@
+import { isE2ETest } from 'calypso/lib/e2e';
 import { useIsAnchorFm } from '../path';
 import useFseBetaEligibility from './use-fse-beta-eligibility';
 
@@ -5,7 +6,7 @@ export default function useFseBetaOptInStep(): boolean {
 	const isAnchorFm = useIsAnchorFm();
 	const isFseBetaEligible = useFseBetaEligibility();
 
-	const canUseFseBetaOptInStep = isFseBetaEligible && ! isAnchorFm;
+	const canUseFseBetaOptInStep = isFseBetaEligible && ! isAnchorFm && ! isE2ETest();
 
 	return canUseFseBetaOptInStep;
 }
