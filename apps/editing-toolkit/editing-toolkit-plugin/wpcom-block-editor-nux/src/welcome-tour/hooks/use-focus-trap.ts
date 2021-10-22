f19036a4d3e2eb57c1afe7bf6f4f661f065ec5bd
Christos <chriskmnds@gmail.com>
2021-10-22 13:51:18 +0300

    cleanup. eslint

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/welcome-tour/hooks/use-focus-trap.ts
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/welcome-tour/hooks/use-focus-trap.ts
@@ -3,33 +3,31 @@
  */
 import { useEffect, useCallback, useState } from '@wordpress/element';
 
- /**
-  * A hook that constraints tabbing/focus on focuable elements in the given element ref.
-  *
-  * @param ref React.MutableRefObject< null | HTMLElement >
-  *
-  */
-const useFocusTrap = (
-	ref: React.MutableRefObject< null | HTMLElement >,
-) => {
-	const [ firstFocusableElement, setFirstFocusableElement ] = useState<HTMLElement | undefined>();
-	const [ lastFocusableElement, setLastFocusableElement ] = useState<HTMLElement | undefined>();
+/**
+ * A hook that constraints tabbing/focus on focuable elements in the given element ref.
+ *
+ * @param ref React.MutableRefObject< null | HTMLElement >
+ *
+ */
+const useFocusTrap = ( ref: React.MutableRefObject< null | HTMLElement > ): void => {
+	const [ firstFocusableElement, setFirstFocusableElement ] = useState< HTMLElement | undefined >();
+	const [ lastFocusableElement, setLastFocusableElement ] = useState< HTMLElement | undefined >();
 
 	const handleTrapFocus = useCallback(
 		( event ) => {
 			let handled = false;
 
 			if ( event.key === 'Tab' ) {
-				if ( event.shiftKey ) { // Shift + Tab
+				if ( event.shiftKey ) {
+					// Shift + Tab
 					if ( document.activeElement === firstFocusableElement ) {
 						lastFocusableElement?.focus();
 						handled = true;
 					}
-				} else { // Tab
-					if ( document.activeElement === lastFocusableElement ) {
-						firstFocusableElement?.focus();
-						handled = true;
-					}
+				} else if ( document.activeElement === lastFocusableElement ) {
+					// Tab
+					firstFocusableElement?.focus();
+					handled = true;
 				}
 			}
 
@@ -49,10 +47,12 @@ const useFocusTrap = (
 			'input[type="text"]:not([disabled])',
 			'input[type="radio"]:not([disabled])',
 			'input[type="checkbox"]:not([disabled])',
-			'select:not([disabled])'
+			'select:not([disabled])',
 		];
 
-		const focusableElements = ref.current?.querySelectorAll<HTMLElement>( focuableElementSelectors.toString() );
+		const focusableElements = ref.current?.querySelectorAll< HTMLElement >(
+			focuableElementSelectors.toString()
+		);
 
 		if ( focusableElements && focusableElements.length ) {
 			setFirstFocusableElement( focusableElements[ 0 ] );
@@ -64,7 +64,7 @@ const useFocusTrap = (
 		return () => {
 			document.removeEventListener( 'keydown', handleTrapFocus );
 		};
-	}, [ ref.current, handleTrapFocus ] );
+	}, [ ref, handleTrapFocus ] );
 };
 
 export default useFocusTrap;
