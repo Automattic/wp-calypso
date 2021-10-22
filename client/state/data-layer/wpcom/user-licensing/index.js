9a5b7b8a009d10e6cae55bf850568d03b57e7c8f
Bryan Elliott <bryan@elliottprogrammer.com>
2021-10-22 12:06:38 -0400

    Jetpack User Licensing: Add redux state management (#57136)

---
+++ client/state/data-layer/wpcom/user-licensing/index.js
@@ -0,0 +1,122 @@
+import { USER_LICENSES_REQUEST, USER_LICENSES_COUNTS_REQUEST } from 'calypso/state/action-types';
+import { registerHandlers } from 'calypso/state/data-layer/handler-registry';
+import { convertToCamelCase } from 'calypso/state/data-layer/utils';
+import { http } from 'calypso/state/data-layer/wpcom-http/actions';
+import { dispatchRequest } from 'calypso/state/data-layer/wpcom-http/utils';
+import {
+	licensesReceiveAction,
+	licensesRequestFailureAction,
+	licensesRequestSuccessAction,
+	licensesCountsReceiveAction,
+	licensesCountsRequestFailureAction,
+	licensesCountsRequestSuccessAction,
+} from 'calypso/state/user-licensing/actions';
+
+/**
+ * @module state/data-layer/wpcom/user-licensing
+ */
+
+// -- LICENSES handlers --
+/**
+ * Dispatches a request to fetch user licenses
+ *
+ * @param {object} action Redux action
+ * @returns {object} original action
+ */
+export const requestLicenses = ( action ) =>
+	http(
+		{
+			method: 'GET',
+			apiNamespace: 'wpcom/v2',
+			path: '/jetpack-licensing/licenses/user',
+			query: {
+				// Do not apply filters during search as search takes over (matches Calypso Blue Post search behavior).
+				...( action.search
+					? { search: action.search }
+					: { filter: action.filter, page: action.page } ),
+				sort_field: action.sortField,
+				sort_direction: action.sortDirection,
+				per_page: action.perPage,
+			},
+		},
+		action
+	);
+
+/**
+ * Dispatches returned user licenses data
+ *
+ * @param {object} action Redux action
+ * @param {Object} licenses raw data from user licensing API
+ * @returns {Array<object>} Redux actions
+ */
+export const receiveLicenses = ( action, licenses ) => [
+	licensesRequestSuccessAction(),
+	licensesReceiveAction( licenses ),
+];
+
+/**
+ * Dispatches returned error from user licenses request
+ *
+ * @param {object} action Redux action
+ * @param {object} rawError raw error from HTTP request
+ * @returns {object} Redux action
+ */
+export const receiveLicensesError = ( action, rawError ) =>
+	licensesRequestFailureAction( rawError instanceof Error ? rawError.message : rawError );
+
+// -- License COUNTS handlers --
+/**
+ * Dispatches a request to fetch user licenses counts
+ *
+ * @param {object} action Redux action
+ * @returns {object} original action
+ */
+export const requestCounts = ( action ) =>
+	http(
+		{
+			method: 'GET',
+			apiNamespace: 'wpcom/v2',
+			path: '/jetpack-licensing/licenses/user/counts',
+		},
+		action
+	);
+
+/**
+ * Dispatches returned user licenses counts data
+ *
+ * @param {object} action Redux action
+ * @param {Object} counts raw count data from user licensing API
+ * @returns {Array<object>} Redux actions
+ */
+export const receiveCounts = ( action, counts ) => [
+	licensesCountsRequestSuccessAction(),
+	licensesCountsReceiveAction( counts ),
+];
+
+/**
+ * Dispatches returned error user license counts request
+ *
+ * @param {object} action Redux action
+ * @param {object} rawError raw error from HTTP request
+ * @returns {object} Redux action
+ */
+export const receiveCountsError = ( action, rawError ) =>
+	licensesCountsRequestFailureAction( rawError instanceof Error ? rawError.message : rawError );
+
+export const dispatchCountsRequest = dispatchRequest( {
+	fetch: requestCounts,
+	onSuccess: receiveCounts,
+	onError: receiveCountsError,
+} );
+
+export const dispatchLicensesRequest = dispatchRequest( {
+	fetch: requestLicenses,
+	onSuccess: receiveLicenses,
+	onError: receiveLicensesError,
+	fromApi: ( paginatedItems ) => convertToCamelCase( paginatedItems ),
+} );
+
+registerHandlers( 'state/data-layer/wpcom/user-licensing', {
+	[ USER_LICENSES_REQUEST ]: [ dispatchLicensesRequest ],
+	[ USER_LICENSES_COUNTS_REQUEST ]: [ dispatchCountsRequest ],
+} );
---
+++ client/state/user-licensing/types.ts
@@ -0,0 +1,52 @@
+import { Action, AnyAction } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+
+export interface LicenseCounts {
+	attached: number;
+	detached: number;
+	revoked: number;
+	not_revoked: number;
+}
+
+export interface PaginatedItems< T > {
+	currentItems: number;
+	currentPage: number;
+	items: T[];
+	itemsPerPage: number;
+	totalItems: number;
+	totalPages: number;
+}
+
+export interface License {
+	licenseId: number;
+	licenseKey: string;
+	productId: number;
+	product: string;
+	userId: number | null;
+	username: string | null;
+	blogId: number | null;
+	siteurl: string | null;
+	issuedAt: string;
+	attachedAt: string | null;
+	revokedAt: string | null;
+}
+
+export interface LicensesStore {
+	licensesFetching: boolean;
+	hasFetchedLicenses: boolean;
+	licenses: PaginatedItems< License > | null;
+	countsFetching: boolean;
+	hasFetchedLicenseCounts: boolean;
+	counts: LicenseCounts | null;
+}
+
+export interface UserLicensingStore {
+	userLicensing: LicensesStore;
+}
+
+export type UserLicensingThunkAction< A extends Action = AnyAction, R = unknown > = ThunkAction<
+	void,
+	UserLicensingStore,
+	R,
+	A
+>;
