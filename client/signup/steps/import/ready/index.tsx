9858bce3c3bfca650e5cc9edbef6a2f626990d0d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-10-22 17:34:45 +0200

    Import: add ready screens with proper styles

---
+++ client/signup/steps/import/ready/index.tsx
@@ -0,0 +1,131 @@
+import { BackButton, NextButton, SubTitle, Title } from '@automattic/onboarding';
+import { createElement, createInterpolateElement } from '@wordpress/element';
+import { sprintf } from '@wordpress/i18n';
+import { useI18n } from '@wordpress/react-i18n';
+import * as React from 'react';
+import ImportPlatformDetails from './platform-details';
+import ImportPreview from './preview';
+import './style.scss';
+
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+
+interface Props {
+	website: string;
+	platform: string;
+}
+
+const ReadyStep: React.FunctionComponent< Props > = ( { website, platform } ) => {
+	const { __ } = useI18n();
+	const [ isModalDetailsOpen, setIsModalDetailsOpen ] = React.useState( false );
+
+	const convertToFrendlyWebsiteName = ( website: string ): string => {
+		return website.replace( 'https://', '' ).replace( 'http://', '' ).replace( 'www.', '' );
+	};
+
+	return (
+		<>
+			<div className="import__header">
+				<div className="import__heading import__heading-center">
+					<Title>{ __( 'Your content is ready for its brand new home' ) }</Title>
+					<SubTitle>
+						{ createInterpolateElement(
+							sprintf(
+								/* translators: the website could be any domain (eg: "yourname.com") that is hosted by a platform (eg: Wix, Squarespace, Blogger, etc.) */
+								__(
+									'It looks like <strong>%(website)s</strong> is hosted by %(platform)s. To move your existing content to your newly created WordPress.com site, try our %(platform)s importer.'
+								),
+								{
+									website: convertToFrendlyWebsiteName( website ),
+									platform,
+								}
+							),
+							{ strong: createElement( 'strong' ) }
+						) }
+					</SubTitle>
+
+					<div className="import__buttons-group">
+						<NextButton>{ __( 'Import your content' ) }</NextButton>
+						<div>
+							<BackButton onClick={ setIsModalDetailsOpen.bind( this, true ) }>
+								{ __( 'What can be imported?' ) }
+							</BackButton>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="import__content">
+				<ImportPreview website={ website } />
+			</div>
+
+			{ isModalDetailsOpen && (
+				<ImportPlatformDetails
+					platform={ platform }
+					onClose={ setIsModalDetailsOpen.bind( this, false ) }
+				/>
+			) }
+		</>
+	);
+};
+
+const ReadyNotStep: React.FunctionComponent = () => {
+	const { __ } = useI18n();
+
+	return (
+		<div className="import-layout__center">
+			<div className="import__header">
+				<div className="import__heading  import__heading-center">
+					<Title>{ __( "Your existing content can't be imported" ) }</Title>
+					<SubTitle>
+						{ __(
+							"Unfortunately, your content is on a platform that we don't yet support. Try Building a new WordPress site instead."
+						) }
+					</SubTitle>
+
+					<div className="import__buttons-group">
+						<NextButton>{ __( 'Start building' ) }</NextButton>
+						<div>
+							<BackButton>{ __( 'Back to the start' ) }</BackButton>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+interface PropsWithoutUrl {
+	platform: string;
+}
+const ReadyNoUrlStep: React.FunctionComponent< PropsWithoutUrl > = ( { platform } ) => {
+	const { __ } = useI18n();
+
+	return (
+		<div className="import-layout__center">
+			<div className="import__header">
+				<div className="import__heading  import__heading-center">
+					<Title>{ __( 'Your content is ready for its new home' ) }</Title>
+					<SubTitle>
+						{ sprintf(
+							/* translators: platform name (eg: Wix, Squarespace, Blogger, etc.) */
+							__(
+								'To move your existing %(platform)s hosted content to your newly created WordPress.com site, try our %(platform)s importer.'
+							),
+							{
+								platform,
+							}
+						) }
+					</SubTitle>
+
+					<div className="import__buttons-group">
+						<NextButton>{ __( 'Import your content' ) }</NextButton>
+						<div>
+							<BackButton>{ __( 'View the import guide' ) }</BackButton>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export { ReadyStep, ReadyNotStep, ReadyNoUrlStep };
---
+++ client/signup/steps/import/ready/platform-details.tsx
@@ -0,0 +1,120 @@
+import { BackButton } from '@automattic/onboarding';
+import { Modal } from '@wordpress/components';
+import { createElement, createInterpolateElement } from '@wordpress/element';
+import { Icon, close, check } from '@wordpress/icons';
+import { useI18n } from '@wordpress/react-i18n';
+import type * as React from 'react';
+
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+
+interface DetailsProps {
+	platform: string;
+	onClose: () => void;
+}
+
+const ImportPlatformDetails: React.FunctionComponent< DetailsProps > = ( data ) => {
+	const { __ } = useI18n();
+	const { platform, onClose } = data;
+
+	const getTitle = ( _platform: string ): string => {
+		switch ( _platform ) {
+			case 'Wix':
+				return __( 'Importing content from Wix' );
+			case 'Squarespace':
+				return __( 'Importing content from Squarespace' );
+			case 'Blogger':
+				return __( 'Importing content from Blogger' );
+			case 'Wordpress':
+				return __( 'Importing content from self-hosted WordPress to Wordpress.com' );
+			case 'Medium':
+				return __( 'Importing content from Medium' );
+			default:
+				return '';
+		}
+	};
+
+	const getInfo = ( _platform: string ): string => {
+		switch ( _platform ) {
+			case 'Wix':
+				return __(
+					"Our Wix content importer is the quickest way to move your content. Simply click 'Import your content' and provide your site's web address (called a URL). Once the import is complete, you'll have a site that's pre-filled with your content."
+				);
+			case 'Squarespace':
+				return __(
+					"Our Squarespace content importer is the quickest way to move your content. Simply export the contents from Squarespace as a WordPress format XML file, then click 'Import your content' and upload it to our importer."
+				);
+			case 'Blogger':
+				return __(
+					"Our Blogger content importer is the quickest way to move your content. Simply export the contents from Blogger as a XML file, then click 'Import your content' and upload it to our importer."
+				);
+			case 'Wordpress':
+				return __(
+					"Our Self-Hosted WordPress content importer is the quickest way to move your content. After clicking 'Import your content', either enter your site's URL to move all your content, plugins, and custom themes to WordPress.com, or use the 'Import' feature to import just your site's content, including posts, pages, and media."
+				);
+			case 'Medium':
+				return __(
+					"Our Mediun content importer is the quickest way to move your content. Simply export the contents from Medium as a .ZIP file, then click 'Import your content' and upload it to our importer."
+				);
+			default:
+				return '';
+		}
+	};
+
+	return (
+		<Modal
+			className="components-modal-new__frame import__details-modal"
+			title={ getTitle( platform ) }
+			onRequestClose={ onClose }
+		>
+			<div className="import__details-modal-content">
+				<p>{ getInfo( platform ) }</p>
+
+				<BackButton>{ __( 'Learn more' ) }</BackButton>
+
+				<div className={ 'import__details-features' }>
+					<p>
+						{ createInterpolateElement( __( 'Things we <strong>can</strong> import:' ), {
+							strong: createElement( 'strong' ),
+						} ) }
+					</p>
+					<ul className={ 'import__details-list' }>
+						<li>
+							<Icon size={ 20 } icon={ check } /> { __( 'Blog posts' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ check } /> { __( 'Static pages' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ check } /> { __( 'Various blocks and features' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ check } /> { __( 'Images' ) }
+						</li>
+					</ul>
+
+					<p>
+						{ createInterpolateElement( __( "Things we <strong>can't</strong> import:" ), {
+							strong: createElement( 'strong' ),
+						} ) }
+					</p>
+					<ul className={ 'import__details-list' }>
+						<li>
+							<Icon size={ 20 } icon={ close } /> { __( 'Site styles' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ close } /> { __( 'Themes' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ close } /> { __( 'Colors' ) }
+						</li>
+						<li>
+							<Icon size={ 20 } icon={ close } /> { __( 'Fonts' ) }
+						</li>
+					</ul>
+				</div>
+			</div>
+		</Modal>
+	);
+};
+
+export default ImportPlatformDetails;
