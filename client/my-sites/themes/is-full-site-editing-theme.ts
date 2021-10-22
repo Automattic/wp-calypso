becd3624975b04f432bbc638263a3358cdf0b813
Jeremy Yip <jeremy.yip@automattic.com>
2021-10-22 12:29:01 -0700

    Convert is-full-site-editing-theme to typescript

---
+++ client/my-sites/themes/is-full-site-editing-theme.ts
@@ -0,0 +1,6 @@
+import { Theme } from './types';
+
+export function isFullSiteEditingTheme( theme: Theme | null ): boolean | undefined {
+	const features = theme?.taxonomies?.theme_feature;
+	return features && features.some( ( feature ) => feature.slug === 'block-templates' );
+}
