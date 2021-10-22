058a98b15b8ed1f06363c15744691f1e86b6e484
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-10-22 18:35:36 +0200

    Import: show proper ready screen depending on stepSegmentName

--- client/signup/steps/import/index.tsx
+++ client/signup/steps/import/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import CaptureStep from './capture';
 import ListStep from './list';
+import { ReadyStep, ReadyNotStep, ReadyNoUrlStep } from './ready';
 import ScanningStep from './scanning';
+import { GoToStep } from './types';
 import './style.scss';
 
 interface Props {
+	goToStep: GoToStep;
 	stepName: string;
-	goToStep: ( name: string ) => void;
+	stepSectionName: string;
 }
+
+const MOCK_DATA = {
+	website: 'https://openweb.com',
+	platform: 'Wix',
+};
 
 export default function ImportOnboarding( props: Props ): React.ReactNode {
 	return (
@@ -15,6 +23,14 @@ export default function ImportOnboarding( props: Props ): React.ReactNode {
 			{ props.stepName === 'capture' && <CaptureStep goToStep={ props.goToStep } /> }
 			{ props.stepName === 'scanning' && <ScanningStep goToStep={ props.goToStep } /> }
 			{ props.stepName === 'list' && <ListStep /> }
+
+			{ props.stepName === 'ready' && ! props.stepSectionName && (
+				<ReadyNoUrlStep platform={ MOCK_DATA.platform } />
+			) }
+			{ props.stepName === 'ready' && props.stepSectionName === 'not' && <ReadyNotStep /> }
+			{ props.stepName === 'ready' && props.stepSectionName === 'preview' && (
+				<ReadyStep website={ MOCK_DATA.website } platform={ MOCK_DATA.platform } />
+			) }
 		</div>
 	);
 }
