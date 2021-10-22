9b1d63ee8a6537453b39a8ec76d604c311eb7c75
Payton Swick <payton@foolord.com>
2021-10-22 12:50:31 -0400

    Add docs for mapRecordKeysRecursively

--- packages/wpcom-checkout/src/map-record-keys-recursively.ts
+++ packages/wpcom-checkout/src/map-record-keys-recursively.ts
@@ -1,3 +1,14 @@
+/**
+ * Transform the keys of an record object recursively
+ *
+ * This transforms an object, modifying all of its keys using a tranform
+ * function. If any of the values of the object are also record objects, their
+ * keys will also be transformed, and so on.
+ *
+ * Note that even though Arrays are objects, this will not modify arrays that
+ * it finds, so any objects contained within arrays that are properties of the
+ * original object will be returned unchanged.
+ */
 export function mapRecordKeysRecursively(
 	record: Record< string, unknown >,
 	transform: ( original: string ) => string
