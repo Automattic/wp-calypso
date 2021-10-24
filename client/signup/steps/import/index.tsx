cfce26b2cd088d2d8f2f4798bf628bbc4c093377
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-10-25 00:24:20 +0200

    Import: integrate navigation by adding StepWrapper component

--- client/signup/steps/import/index.tsx
+++ client/signup/steps/import/index.tsx
@@ -1,4 +1,6 @@
+import { useI18n } from '@wordpress/react-i18n';
 import React from 'react';
+import StepWrapper from 'calypso/signup/step-wrapper';
 import CaptureStep from './capture';
 import ListStep from './list';
 import { ReadyPreviewStep, ReadyNotStep, ReadyStep } from './ready';
@@ -17,20 +19,46 @@ const MOCK_DATA = {
 	platform: 'Wix',
 };
 
-export default function ImportOnboarding( props: Props ): React.ReactNode {
-	return (
-		<div className="import__onboarding-page">
-			{ props.stepName === 'capture' && <CaptureStep goToStep={ props.goToStep } /> }
-			{ props.stepName === 'scanning' && <ScanningStep goToStep={ props.goToStep } /> }
-			{ props.stepName === 'list' && <ListStep /> }
+const shouldHideBackBtn = ( stepName: string ): boolean => {
+	const STEPS_WITHOUT_BACK = [ 'scanning' ];
 
-			{ props.stepName === 'ready' && ! props.stepSectionName && (
-				<ReadyStep platform={ MOCK_DATA.platform } />
-			) }
-			{ props.stepName === 'ready' && props.stepSectionName === 'not' && <ReadyNotStep /> }
-			{ props.stepName === 'ready' && props.stepSectionName === 'preview' && (
-				<ReadyPreviewStep website={ MOCK_DATA.website } platform={ MOCK_DATA.platform } />
-			) }
-		</div>
+	return STEPS_WITHOUT_BACK.includes( stepName );
+};
+
+const shouldHideNextBtn = ( stepName: string ): boolean => {
+	const STEPS_WITH_NEXT = [ 'capture' ];
+
+	return ! STEPS_WITH_NEXT.includes( stepName );
+};
+
+export default function ImportOnboarding( props: Props ): React.ReactNode {
+	const { __ } = useI18n();
+
+	return (
+		<StepWrapper
+			flowName={ 'importer' }
+			hideSkip={ true }
+			hideBack={ shouldHideBackBtn( props.stepName ) }
+			hideNext={ shouldHideNextBtn( props.stepName ) }
+			nextLabelText={ __( "I don't have a site address" ) }
+			allowBackFirstStep={ true }
+			hideFormattedHeader={ true }
+			stepContent={
+				<div className="import__onboarding-page">
+					{ props.stepName === 'capture' && <CaptureStep goToStep={ props.goToStep } /> }
+					{ props.stepName === 'scanning' && <ScanningStep goToStep={ props.goToStep } /> }
+					{ props.stepName === 'list' && <ListStep /> }
+
+					{ props.stepName === 'ready' && ! props.stepSectionName && (
+						<ReadyStep platform={ MOCK_DATA.platform } />
+					) }
+					{ props.stepName === 'ready' && props.stepSectionName === 'not' && <ReadyNotStep /> }
+					{ props.stepName === 'ready' && props.stepSectionName === 'preview' && (
+						<ReadyPreviewStep website={ MOCK_DATA.website } platform={ MOCK_DATA.platform } />
+					) }
+				</div>
+			}
+			{ ...props }
+		/>
 	);
 }
