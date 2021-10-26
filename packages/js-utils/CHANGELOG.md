b9bb60ee7d930d0fe48d0692d8d83ea3250d4869
roo2 <roo2@protonmail.com>
2021-10-26 19:29:35 +1000

    address feedback

--- packages/js-utils/CHANGELOG.md
+++ packages/js-utils/CHANGELOG.md
@@ -1,7 +1,4 @@
 ## 0.1.0
 
 - Add `uniqueBy`
-
-## 0.2.0
-
 - Add `shuffle`
--- packages/js-utils/src/index.ts
+++ packages/js-utils/src/index.ts
@@ -1,2 +1,2 @@
-export { default as uniqueBy } from './unique-by';
 export { default as shuffle } from './shuffle';
+export { default as uniqueBy } from './unique-by';
