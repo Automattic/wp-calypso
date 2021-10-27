120721689bb0b029620d8a1e70258861e50dd51a
Jarda Snajdr <jsnajdr@gmail.com>
2021-10-27 15:38:30 +0200

    PopupSearch: use the hook without the HOC

--- client/layout/popup-search/index.js
+++ client/layout/popup-search/index.js
@@ -2,17 +2,19 @@
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 
-import { localize, useTranslate } from 'i18n-calypso';
-import { connect } from 'react-redux';
-import { withInlineHelpSearchResults } from 'calypso/blocks/inline-help/data/use-inline-help-search-query';
+import { useTranslate } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
 import HelpSearchCard from 'calypso/blocks/inline-help/inline-help-search-card';
+import { useHelpSearchQuery } from 'calypso/data/help/use-help-search-query';
 import { localizeUrl } from 'calypso/lib/i18n-utils';
-import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import getSearchQuery from 'calypso/state/inline-help/selectors/get-search-query';
 import './style.scss';
 
-export function PopUpSearch( props ) {
+export default function PopUpSearch( { onClose } ) {
 	const translate = useTranslate();
+	const searchQuery = useSelector( getSearchQuery );
+	const { data } = useHelpSearchQuery( searchQuery );
+	const searchResults = data?.wordpress_support_links ?? [];
 
 	const onChildClick = ( e ) => e.stopPropagation();
 
@@ -26,7 +28,7 @@ export function PopUpSearch( props ) {
 	};
 
 	const onResultClick = ( link ) => {
-		props.onClose();
+		onClose();
 		const linkUrlObject = new URL( window.location.origin + link );
 		const combinedQueryParams = mergeQueryParams( linkUrlObject.search, window.location.search );
 		linkUrlObject.search = combinedQueryParams;
@@ -34,15 +36,15 @@ export function PopUpSearch( props ) {
 		window.location.href = localizeUrl( linkUrlObject.toString() );
 	};
 	return (
-		<div role="button" className="popup-search__mask" onClick={ props.onClose }>
+		<div role="button" className="popup-search__mask" onClick={ onClose }>
 			<div className="popup-search__container" onClick={ onChildClick }>
 				<HelpSearchCard
-					query={ props.searchQuery }
+					query={ searchQuery }
 					placeholder={ translate( 'Search wordpress actions' ) }
 				/>
-				{ props.searchResults.length > 0 && (
+				{ searchResults.length > 0 && (
 					<div className="popup-search__results" aria-label="Pop Up Search">
-						{ props.searchResults.slice( 0, 10 ).map( ( { link, key, title, description } ) => (
+						{ searchResults.slice( 0, 10 ).map( ( { link, key, title, description } ) => (
 							<div key={ title }>
 								<div
 									role="button"
@@ -63,12 +65,3 @@ export function PopUpSearch( props ) {
 		</div>
 	);
 }
-
-export default connect(
-	( state ) => ( {
-		searchQuery: getSearchQuery( state ),
-	} ),
-	{
-		track: recordTracksEvent,
-	}
-)( localize( withInlineHelpSearchResults( PopUpSearch ) ) );
