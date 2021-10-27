5afb2d8a7be4e4308b338e44b234b58c9ded8cf2
dpasque <dan.speckhard.pasque@automattic.com>
2021-10-27 12:05:53 -0500

    Use TS and new context to trash notification

--- test/e2e/specs/specs-playwright/wp-notifications__trash-spec.ts
+++ test/e2e/specs/specs-playwright/wp-notifications__trash-spec.ts
@@ -12,11 +12,12 @@ import {
 	NavbarComponent,
 	NotificationsComponent,
 } from '@automattic/calypso-e2e';
+import { Page } from 'playwright';
 
 describe( DataHelper.createSuiteTitle( 'Notifications' ), function () {
-	let page;
-	let publishedPostsListPage;
-	let notificationsComponent;
+	let page: Page;
+	let publishedPostsListPage: PublishedPostsListPage;
+	let notificationsComponent: NotificationsComponent;
 
 	const commentingUser = 'commentingUser';
 	const notificationsUser = 'notificationsUser';
@@ -47,25 +48,35 @@ describe( DataHelper.createSuiteTitle( 'Notifications' ), function () {
 			const commentsComponent = new CommentsComponent( page );
 			await commentsComponent.postComment( comment );
 		} );
-
-		it( 'Clear authentication state', async function () {
-			await BrowserManager.clearAuthenticationState( page );
-		} );
 	} );
 
 	describe( `Trash comment as ${ notificationsUser }`, function () {
+		let notificationsPage: Page;
+
+		afterAll( async function () {
+			if ( notificationsPage ) {
+				await BrowserManager.closePage( notificationsPage, { closeContext: true } );
+			}
+		} );
+
+		it( 'Launch new context', async function () {
+			notificationsPage = await BrowserManager.newPage( {
+				context: await BrowserManager.newBrowserContext(),
+			} );
+		} );
+
 		it( `Log in as ${ notificationsUser }`, async function () {
-			const loginPage = new LoginPage( page );
+			const loginPage = new LoginPage( notificationsPage );
 			await loginPage.login( { account: notificationsUser } );
 		} );
 
 		it( 'Open notification using keyboard shortcut', async function () {
-			const navbarComponent = new NavbarComponent( page );
+			const navbarComponent = new NavbarComponent( notificationsPage );
 			await navbarComponent.openNotificationsPanel( { useKeyboard: true } );
 		} );
 
 		it( `See notification for the comment left by ${ commentingUser }`, async function () {
-			notificationsComponent = new NotificationsComponent( page );
+			notificationsComponent = new NotificationsComponent( notificationsPage );
 			await notificationsComponent.clickNotification( comment );
 		} );
 
