9aca9bb8c11e5623c24114269d4fc5ea5925545a
Markus Dobmann <m.do@posteo.net>
2021-10-27 10:28:29 +0200

    `QueryUserPurchases`: refactor away from `UNSAFE_*`

--- client/components/data/query-user-purchases/index.jsx
+++ client/components/data/query-user-purchases/index.jsx
@@ -1,51 +1,33 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { fetchUserPurchases } from 'calypso/state/purchases/actions';
 import {
 	hasLoadedUserPurchasesFromServer,
 	isFetchingUserPurchases,
 } from 'calypso/state/purchases/selectors';
 
-class QueryUserPurchases extends Component {
-	requestUserPurchases( nextProps ) {
-		const userChanged = nextProps && this.props.userId !== nextProps.userId;
-		const props = nextProps || this.props;
+const request = ( userId ) => ( dispatch, getState ) => {
+	const isFetching = isFetchingUserPurchases( getState() );
+	const hasLoaded = hasLoadedUserPurchasesFromServer( getState() );
 
-		if (
-			( ! props.isFetchingUserPurchases && ! props.hasLoadedUserPurchasesFromServer ) ||
-			userChanged
-		) {
-			this.props.fetchUserPurchases( props.userId );
-		}
+	if ( userId && ! isFetching && ! hasLoaded ) {
+		dispatch( fetchUserPurchases( userId ) );
 	}
+};
 
-	UNSAFE_componentWillMount() {
-		this.requestUserPurchases();
-	}
+function QueryUserPurchases( { userId } ) {
+	const dispatch = useDispatch();
 
-	UNSAFE_componentWillReceiveProps( nextProps ) {
-		this.requestUserPurchases( nextProps );
-	}
+	useEffect( () => {
+		dispatch( request( userId ) );
+	}, [ dispatch, userId ] );
 
-	render() {
-		return null;
-	}
+	return null;
 }
 
 QueryUserPurchases.propTypes = {
 	userId: PropTypes.number.isRequired,
-	hasLoadedUserPurchasesFromServer: PropTypes.bool.isRequired,
-	isFetchingUserPurchases: PropTypes.bool.isRequired,
-	fetchUserPurchases: PropTypes.func.isRequired,
 };
 
-export default connect(
-	( state ) => {
-		return {
-			hasLoadedUserPurchasesFromServer: hasLoadedUserPurchasesFromServer( state ),
-			isFetchingUserPurchases: isFetchingUserPurchases( state ),
-		};
-	},
-	{ fetchUserPurchases }
-)( QueryUserPurchases );
+export default QueryUserPurchases;
