fdf5f29ba694c4e9c91a20c063881d32bb09fc1a
Matthew Reishus <mreishus@users.noreply.github.com>
2021-10-29 15:31:15 -0500

    Reformat and remove any type

--- client/components/forms/form-label/index.tsx
+++ client/components/forms/form-label/index.tsx
@@ -1,6 +1,6 @@
 import classnames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
-import { Children, FunctionComponent, LabelHTMLAttributes, forwardRef, LegacyRef } from 'react';
+import { Children, FunctionComponent, LabelHTMLAttributes, forwardRef } from 'react';
 
 import './style.scss';
 
@@ -11,28 +11,36 @@ interface Props {
 
 type LabelProps = LabelHTMLAttributes< HTMLLabelElement >;
 
-const FormLabel: FunctionComponent< Props & LabelProps > = forwardRef(( {
-	children,
-	required,
-	optional,
-	className, // Via LabelProps
-	...labelProps
-}, ref: any ) => {
-	const translate = useTranslate();
+const FormLabel: FunctionComponent< Props & LabelProps > = forwardRef<
+	HTMLLabelElement,
+	Props & LabelProps
+>(
+	(
+		{
+			children,
+			required,
+			optional,
+			className, // Via LabelProps
+			...labelProps
+		},
+		ref
+	) => {
+		const translate = useTranslate();
 
-	const hasChildren: boolean = Children.count( children ) > 0;
+		const hasChildren: boolean = Children.count( children ) > 0;
 
-	return (
-		<label ref={ref} { ...labelProps } className={ classnames( className, 'form-label' ) }>
-			{ children }
-			{ hasChildren && required && (
-				<small className="form-label__required">{ translate( 'Required' ) }</small>
-			) }
-			{ hasChildren && optional && (
-				<small className="form-label__optional">{ translate( 'Optional' ) }</small>
-			) }
-		</label>
-	);
-});
+		return (
+			<label ref={ ref } { ...labelProps } className={ classnames( className, 'form-label' ) }>
+				{ children }
+				{ hasChildren && required && (
+					<small className="form-label__required">{ translate( 'Required' ) }</small>
+				) }
+				{ hasChildren && optional && (
+					<small className="form-label__optional">{ translate( 'Optional' ) }</small>
+				) }
+			</label>
+		);
+	}
+);
 
 export default FormLabel;
