140e157f363cb879cd6b1a1ca0b79f336b7a6450
Payton Swick <payton@foolord.com>
2021-11-01 13:32:29 -0400

    Add tests for camelToSnakeCase

---
+++ packages/wpcom-checkout/test/camel-to-snake-case.ts
@@ -0,0 +1,55 @@
+import { camelToSnakeCase } from '../src/camel-to-snake-case';
+
+describe( 'camelToSnakeCase', () => {
+	it( 'transforms camelCase to snake_case for strings with two words', () => {
+		expect( camelToSnakeCase( 'camelCase' ) ).toBe( 'camel_case' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings with four words', () => {
+		expect( camelToSnakeCase( 'thisIsCamelCase' ) ).toBe( 'this_is_camel_case' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings with a number', () => {
+		expect( camelToSnakeCase( 'camelCase1' ) ).toBe( 'camel_case_1' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings with multiple adjacent numbers', () => {
+		expect( camelToSnakeCase( 'hello1234Thing' ) ).toBe( 'hello_1234_thing' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings with an existing underscore', () => {
+		expect( camelToSnakeCase( 'camelCase_word' ) ).toBe( 'camel_case_word' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings with a capitalized first letter', () => {
+		expect( camelToSnakeCase( 'CamelCase' ) ).toBe( 'camel_case' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings with a space', () => {
+		expect( camelToSnakeCase( 'camel case' ) ).toBe( 'camel_case' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings that start with a space', () => {
+		expect( camelToSnakeCase( ' camel case' ) ).toBe( 'camel_case' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings that start with an underscore', () => {
+		expect( camelToSnakeCase( '_camel_case' ) ).toBe( 'camel_case' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings that end with a space', () => {
+		expect( camelToSnakeCase( 'camel case ' ) ).toBe( 'camel_case' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings that end with an underscore', () => {
+		expect( camelToSnakeCase( 'camel_case_' ) ).toBe( 'camel_case' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings with multiple adjacent spaces', () => {
+		expect( camelToSnakeCase( 'camel  case' ) ).toBe( 'camel_case' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings with multiple adjacent capitals', () => {
+		expect( camelToSnakeCase( 'helloWORLDThing' ) ).toBe( 'hello_world_thing' );
+	} );
+} );
