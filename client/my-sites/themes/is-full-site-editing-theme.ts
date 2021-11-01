4833d99a8a358ae04d917fc9fa0fafbd2f01decd
Jeremy Yip <jeremy.yip@automattic.com>
2021-11-01 12:54:59 -0700

    Remove redundant features variable validation

--- client/my-sites/themes/is-full-site-editing-theme.ts
+++ client/my-sites/themes/is-full-site-editing-theme.ts
@@ -2,7 +2,5 @@ import { Theme } from 'calypso/types';
 
 export function isFullSiteEditingTheme( theme: Theme | null ): boolean {
 	const features = theme?.taxonomies?.theme_feature;
-	return (
-		( features && features?.some( ( feature ) => feature.slug === 'block-templates' ) ) ?? false
-	);
+	return features?.some( ( feature ) => feature.slug === 'block-templates' ) ?? false;
 }
