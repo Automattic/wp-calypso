e3849ba112c27f6e3483c9d72691c0d7d970c4a9
cpap <papazoglou.charalampos@gmail.com>
2021-11-01 16:58:56 +0200

    Fixed Navigation Header: changes example to function component.

--- client/components/fixed-navigation-header/docs/example.jsx
+++ client/components/fixed-navigation-header/docs/example.jsx
@@ -1,15 +1,11 @@
-import { Fragment, PureComponent } from 'react';
+import { Fragment } from 'react';
 import FixedNavigationHeader from 'calypso/components/fixed-navigation-header';
 
-export default class FixedNavigationHeaderExample extends PureComponent {
-	static displayName = 'FormattedHeaderExample';
-
-	render() {
-		return (
-			<Fragment>
-				<FixedNavigationHeader headerText="This is the header." />
-				<FixedNavigationHeader headerText="This is the header with branded font" brandFont />
-			</Fragment>
-		);
-	}
+export default function FixedNavigationHeaderExample() {
+	return (
+		<Fragment>
+			<FixedNavigationHeader headerText="This is the header." />
+			<FixedNavigationHeader headerText="This is the header with branded font" brandFont />
+		</Fragment>
+	);
 }
