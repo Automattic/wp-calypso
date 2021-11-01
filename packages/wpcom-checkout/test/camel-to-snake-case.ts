49ed5bfe7c54e9ac74c53366324622afc4efe820
Payton Swick <payton@foolord.com>
2021-11-01 16:18:06 -0400

    Add two more camelToSnakeCase tests

--- packages/wpcom-checkout/test/camel-to-snake-case.ts
+++ packages/wpcom-checkout/test/camel-to-snake-case.ts
@@ -13,6 +13,14 @@ describe( 'camelToSnakeCase', () => {
 		expect( camelToSnakeCase( 'camelCase1' ) ).toBe( 'camel_case_1' );
 	} );
 
+	it( 'transforms camelCase to snake_case for strings with a number_followed by a capital', () => {
+		expect( camelToSnakeCase( 'camelCase1Hi' ) ).toBe( 'camel_case_1_hi' );
+	} );
+
+	it( 'transforms camelCase to snake_case for strings with a number_followed by a lowercase', () => {
+		expect( camelToSnakeCase( 'camelCase1hi' ) ).toBe( 'camel_case_1_hi' );
+	} );
+
 	it( 'transforms camelCase to snake_case for strings with multiple adjacent numbers', () => {
 		expect( camelToSnakeCase( 'hello1234Thing' ) ).toBe( 'hello_1234_thing' );
 	} );
