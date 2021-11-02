c977c74f35aa8813bee7ab29582907da1975bf00
arthur <arthur.chu@automattic.com>
2021-11-02 15:58:20 +0800

    Onboarding: Add new props to signup complete event

--- client/signup/main.jsx
+++ client/signup/main.jsx
@@ -417,6 +417,9 @@ class Signup extends Component {
 			( isNewishUser && dependencies && dependencies.siteSlug && existingSiteCount <= 1 )
 		);
 		const hasCartItems = dependenciesContainCartItem( dependencies );
+		const selectedDesign = get( dependencies, 'selectedDesign' );
+		const intent = get( dependencies, 'intent' );
+		const startingPoint = get( dependencies, 'startingPoint' );
 
 		const debugProps = {
 			isNewishUser,
@@ -426,6 +429,9 @@ class Signup extends Component {
 			isNew7DUserSite,
 			flow: this.props.flowName,
 			siteId,
+			theme: selectedDesign?.theme,
+			intent,
+			startingPoint,
 		};
 		debug( 'Tracking signup completion.', debugProps );
 
@@ -435,6 +441,10 @@ class Signup extends Component {
 			isNewUser,
 			hasCartItems,
 			isNew7DUserSite,
+			// Record the following values so that we can know the user completed which branch under the hero flow
+			theme: selectedDesign?.theme,
+			intent,
+			startingPoint,
 		} );
 
 		this.handleLogin( dependencies, destination );
