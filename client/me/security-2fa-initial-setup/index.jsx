4c0cc0332671d5ea510a24839446b337bc7a8d93
Alex Concha <alex@buayacorp.com>
2021-11-02 11:48:48 +0100

    Use the appropriate Form components.

--- client/me/security-2fa-initial-setup/index.jsx
+++ client/me/security-2fa-initial-setup/index.jsx
@@ -4,8 +4,10 @@ import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import FormButton from 'calypso/components/forms/form-button';
+import FormFieldset from 'calypso/components/forms/form-fieldset';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormRadio from 'calypso/components/forms/form-radio';
+import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
 import { gaRecordEvent } from 'calypso/lib/analytics/ga';
 
 import './style.scss';
@@ -46,39 +48,45 @@ class Security2faInitialSetup extends Component {
 					) }
 				</p>
 
-				<p>
-					<FormLabel>
-						<FormRadio
-							name="auth_method"
-							value="app-based"
-							defaultChecked
-							onChange={ this.setAuth }
-						/>
+				<FormFieldset>
+					<FormRadio
+						id="app-auth"
+						name="auth_method"
+						value="app-based"
+						defaultChecked
+						onChange={ this.setAuth }
+					/>
+					<FormLabel htmlFor="app-auth">
 						<Gridicon icon="phone" />
 						<span className="security-2fa-initial-setup__item-title">
 							{ this.props.translate( 'Set up using an app' ) }
 						</span>
-						<span className="security-2fa-initial-setup__description">
-							{ this.props.translate(
-								'Use an application on your phone to get two-step authentication codes when you login.'
-							) }
-						</span>
 					</FormLabel>
-				</p>
-				<p>
-					<FormLabel className="security-2fa-initial-setup__label">
-						<FormRadio name="auth_method" value="sms-settings" onChange={ this.setAuth } />
+					<FormSettingExplanation>
+						{ this.props.translate(
+							'Use an application on your phone to get two-step authentication codes when you login.'
+						) }
+					</FormSettingExplanation>
+				</FormFieldset>
+				<FormFieldset>
+					<FormRadio
+						id="sms-auth"
+						name="auth_method"
+						value="sms-settings"
+						onChange={ this.setAuth }
+					/>
+					<FormLabel htmlFor="sms-auth">
 						<Gridicon icon="chat" />
 						<span className="security-2fa-initial-setup__item-title">
 							{ this.props.translate( 'Set up using SMS' ) }
 						</span>
-						<span className="security-2fa-initial-setup__description">
-							{ this.props.translate(
-								'WordPress.com will send you a SMS with a two-step authentication code when you log in.'
-							) }
-						</span>
 					</FormLabel>
-				</p>
+					<FormSettingExplanation>
+						{ this.props.translate(
+							'WordPress.com will send you a SMS with a two-step authentication code when you log in.'
+						) }
+					</FormSettingExplanation>
+				</FormFieldset>
 
 				<FormButton
 					onClick={ ( event ) => {
