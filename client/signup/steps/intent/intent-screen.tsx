c23d48f4c2b7878a4cd3712e9f3e7ea5286adf5f
Bogdan Nikolic <bogdan.nikolic@automattic.com>
2021-11-03 13:50:31 +0100

    Gutenboarding import flow: additonal adjustments (#57393)
    * Import ready: update href attr of the learn more link
    
    * Import list: provide importer href URLs
    
    * Import: set back button URL to point back to the intent screen
    
    * Import: fix typo
    
    * Intent: add alternative list of intents
    
    * Intent: switch onboarding flow to `importer'
    
    * Import: generate flow url by `getStepUrl` function
    
    * Import: fix back url
    
    * Import list: redirect user to the proper ready page
    
    * Import list: make the whole bar clickable

--- client/signup/steps/intent/intent-screen.tsx
+++ client/signup/steps/intent/intent-screen.tsx
@@ -1,10 +1,13 @@
+import { isEnabled } from '@automattic/calypso-config';
 import { localize, LocalizeProps } from 'i18n-calypso';
 import React from 'react';
 import { build, write } from '../../icons';
 import SelectItems, { SelectItem } from '../../select-items';
+import SelectItemsAlt, { SelectAltItem } from '../../select-items-alt';
 import type { IntentFlag } from './types';
 
 type Intent = SelectItem< IntentFlag >;
+type IntentAlt = SelectAltItem< IntentFlag >;
 
 interface Props {
 	onSelect: ( value: IntentFlag ) => void;
@@ -32,10 +35,28 @@ const useIntents = ( { translate }: Pick< Props, 'translate' > ): Intent[] => {
 	];
 };
 
+const useIntentsAlt = ( { translate }: Pick< Props, 'translate' > ): IntentAlt[] => {
+	return [
+		{
+			show: isEnabled( 'gutenboarding/import' ),
+			key: 'import',
+			description: translate( 'Already have an existing website?' ),
+			value: 'import',
+			actionText: translate( 'Import your site content' ),
+		},
+	];
+};
+
 const IntentScreen: React.FC< Props > = ( { onSelect, translate } ) => {
 	const intents = useIntents( { translate } );
+	const intentsAlt = useIntentsAlt( { translate } );
 
-	return <SelectItems items={ intents } onSelect={ onSelect } />;
+	return (
+		<>
+			<SelectItems items={ intents } onSelect={ onSelect } />
+			<SelectItemsAlt items={ intentsAlt } onSelect={ onSelect } />
+		</>
+	);
 };
 
 export default localize( IntentScreen );
