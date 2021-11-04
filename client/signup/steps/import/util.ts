1a7136a7e53a6530982b33b9f99951fc6a483488
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-11-04 18:12:50 +0100

    Import: add util methods

--- client/signup/steps/import/util.ts
+++ client/signup/steps/import/util.ts
@@ -20,18 +20,41 @@ export const platformImporterNameMap: { [ key: string ]: string } = {
 	movabletype: 'mt',
 };
 
+export const orgImporters: string[] = [
+	'xanga',
+	'tumblr',
+	'movabletype',
+	'livejournal',
+	'ghost',
+	'blogroll',
+];
+
+export function getPlatformImporterName( platform: string ): string {
+	return platformImporterNameMap[ platform ] ? platformImporterNameMap[ platform ] : platform;
+}
+
 export function convertPlatformName( platform: string ): string {
 	return platformMap[ platform ] !== undefined ? platformMap[ platform ] : 'Unknown';
 }
 
-export function getWpComImporterUrl( siteSlug: string, importer: string ): string {
-	const wpComBase = 'https://wordpress.com/import/{siteSlug}?engine={importer}';
+export function getWpComImporterUrl( siteSlug: string, platform: string ): string {
+	const wpComBase = '/import/{siteSlug}?engine={importer}';
 
-	return wpComBase.replace( '{siteSlug}', siteSlug ).replace( '{importer}', importer );
+	return wpComBase
+		.replace( '{siteSlug}', siteSlug )
+		.replace( '{importer}', getPlatformImporterName( platform ) );
 }
 
-export function getWpAdminImporterUrl( siteSlug: string, importer: string ): string {
+export function getWpOrgImporterUrl( siteSlug: string, platform: string ): string {
 	const wpAdminBase = 'https://{siteSlug}/wp-admin/admin.php?import={importer}';
 
-	return wpAdminBase.replace( '{siteSlug}', siteSlug ).replace( '{importer}', importer );
+	return wpAdminBase
+		.replace( '{siteSlug}', siteSlug )
+		.replace( '{importer}', getPlatformImporterName( platform ) );
+}
+
+export function getImporterUrl( siteSlug: string, platform: string ): string {
+	return orgImporters.includes( platform )
+		? getWpOrgImporterUrl( siteSlug, platform )
+		: getWpComImporterUrl( siteSlug, platform );
 }
