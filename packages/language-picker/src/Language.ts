f4d5baf2958cfb4ccbc8f779f63ded248d9c57bb
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2021-11-04 14:32:34 +0200

    languages: Use languages data keys for LanguageSlug type def (#57554)

--- packages/language-picker/src/Language.ts
+++ packages/language-picker/src/Language.ts
@@ -1,3 +1,10 @@
+/**
+ * External dependencies
+ */
+import type { Language } from '@automattic/languages';
+
+export type { Language };
+
 export type LanguageGroup = {
 	id: string;
 	name: () => string;
@@ -5,20 +12,3 @@ export type LanguageGroup = {
 	countries?: string[];
 	default?: boolean;
 };
-
-type LanguageSlug = string;
-type WPLocale = string;
-
-type BaseLanguage = {
-	langSlug: LanguageSlug;
-	name: string;
-	popular?: number;
-	rtl?: boolean;
-	territories: string[];
-	value: number;
-	wpLocale: WPLocale | '';
-};
-
-type SubLanguage = BaseLanguage & { parentLangSlug: string };
-
-export type Language = BaseLanguage | SubLanguage;
