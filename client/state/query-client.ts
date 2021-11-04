11d60415ad7dc8eea33ff15394477b59df280fdd
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2021-11-04 17:01:25 -0300

    Introduce query persistence opt-out mechanism

--- client/state/query-client.ts
+++ client/state/query-client.ts
@@ -4,6 +4,30 @@ import { persistQueryClient } from 'react-query/persistQueryClient-experimental'
 import { shouldPersist, MAX_AGE, SERIALIZE_THROTTLE } from 'calypso/state/initial-state';
 import { getPersistedStateItem, storePersistedStateItem } from 'calypso/state/persisted-state';
 
+const SHOULD_PERSIST_QUERY_KEY = 'shouldPersistQuery';
+
+type PersistencePredicate< T > = ( data: T ) => boolean;
+
+type PersistenceMetaPartial< T > = {
+	[ SHOULD_PERSIST_QUERY_KEY ]: PersistencePredicate< T > | boolean;
+};
+
+const hasPersistenceSetting = ( value: unknown ): value is boolean => {
+	return typeof value === 'boolean';
+};
+
+const hasPersistenceCustomSetting = (
+	value: unknown
+): value is PersistencePredicate< unknown > => {
+	return typeof value === 'function';
+};
+
+export const shouldPersistQuery = < T >(
+	predicate: PersistencePredicate< T > | boolean
+): PersistenceMetaPartial< T > => ( {
+	[ SHOULD_PERSIST_QUERY_KEY ]: predicate,
+} );
+
 export async function createQueryClient( userId: number | undefined ): Promise< QueryClient > {
 	const queryClient = new QueryClient( {
 		defaultOptions: { queries: { cacheTime: MAX_AGE } },
@@ -26,6 +50,25 @@ export async function createQueryClient( userId: number | undefined ): Promise< 
 			queryClient,
 			persistor,
 			maxAge: MAX_AGE,
+			dehydrateOptions: {
+				shouldDehydrateQuery: ( query ) => {
+					if ( query.state.status !== 'success' ) {
+						return false;
+					}
+
+					const shouldPersist = query.meta?.[ SHOULD_PERSIST_QUERY_KEY ];
+
+					if ( hasPersistenceSetting( shouldPersist ) ) {
+						return shouldPersist;
+					}
+
+					if ( hasPersistenceCustomSetting( shouldPersist ) ) {
+						return shouldPersist( query.state.data );
+					}
+
+					return true;
+				},
+			},
 		} );
 	}
 	return queryClient;
