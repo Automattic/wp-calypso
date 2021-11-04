185eb296e65bee0e99bc6641a53768c3fc21681d
Edwin Takahashi <egao@outlook.com>
2021-11-04 12:39:50 -0700

    E2E: add test to check admin bar presence in mobile. (#57579)
    * Change how `openNavSidebar` and `returnToHomeDashboard` works.
    - support both mobile and desktop sizes
    
    * Update tests
    
    * Use an array of promises intead.
    Add new sped `Editor: Navbar`
    
    * Fix lint error.
    
    * Another lint error.
    
    * Minor updates from code review.
    - open the sidebar as part of the returning to home dashboard.
    - change type of the action array.

--- test/e2e/specs/specs-playwright/wp-signup__free.ts
+++ test/e2e/specs/specs-playwright/wp-signup__free.ts
@@ -11,7 +11,6 @@ import {
 	setupHooks,
 	UserSignupPage,
 	SignupPickPlanPage,
-	BrowserHelper,
 	CloseAccountFlow,
 	GutenboardingFlow,
 } from '@automattic/calypso-e2e';
@@ -73,13 +72,9 @@ describe.skip( DataHelper.createSuiteTitle( 'Signup: WordPress.com Free' ), func
 		it( 'Return to Home dashboard', async function () {
 			// Temporary workaround due to https://github.com/Automattic/wp-calypso/issues/51162.
 			// Conditional can be removed once fixed.
-			if ( BrowserHelper.getTargetDeviceName() === 'mobile' ) {
-				await page.goBack();
-			} else {
-				gutenbergEditorPage = new GutenbergEditorPage( page );
-				await gutenbergEditorPage.openNavSidebar();
-				await gutenbergEditorPage.returnToDashboard();
-			}
+			gutenbergEditorPage = new GutenbergEditorPage( page );
+			await gutenbergEditorPage.openNavSidebar();
+			await gutenbergEditorPage.returnToHomeDashboard();
 		} );
 	} );
 
