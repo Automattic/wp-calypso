8e895f154e1a17ba8109510b985dbbeb978df968
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2021-11-06 14:24:31 +0200

    Update test names

--- test/e2e/specs/specs-i18n/wp-i18n__gutenboarding.ts
+++ test/e2e/specs/specs-i18n/wp-i18n__gutenboarding.ts
@@ -20,45 +20,45 @@ describe( `Gutenboarding translations @i18n (${ locale })`, function () {
 		page = args.page;
 	} );
 
-	it( 'should navigate to /new', async function () {
+	it( 'Navigate to /new', async function () {
 		await page.goto( DataHelper.getCalypsoURL( 'new' ) );
 		gutenboardingFlow = new GutenboardingFlow( page );
 	} );
 
-	it( `should change the locale (${ locale })`, async function () {
+	it( `Change the locale (${ locale })`, async function () {
 		await gutenboardingFlow.switchLanguage( locale );
 	} );
 
-	it( `should display translations on aquire intent page (${ locale })`, async function () {
+	it( `Display translations on aquire intent page (${ locale })`, async function () {
 		await validatePageTranslations( page, locale );
 		await gutenboardingFlow.clickSkipButton();
 	} );
 
-	it( `should display translations on design selector page (${ locale })`, async function () {
+	it( `Display translations on design selector page (${ locale })`, async function () {
 		await validatePageTranslations( page, locale );
 		await gutenboardingFlow.selectDesign( 'Stratford' );
 	} );
 
-	it( `should display translations on style preview page (${ locale })`, async function () {
+	it( `Display translations on style preview page (${ locale })`, async function () {
 		await gutenboardingFlow.selectFont( 'Raleway' );
 		await validatePageTranslations( page, locale );
 		await gutenboardingFlow.clickNextButton();
 	} );
 
-	it( `should display translations on domains page (${ locale })`, async function () {
+	it( `Display translations on domains page (${ locale })`, async function () {
 		await gutenboardingFlow.searchDomain( DataHelper.getRandomPhrase() );
 		await gutenboardingFlow.selectDomain( '.wordpress.com' );
 		await validatePageTranslations( page, locale );
 		await gutenboardingFlow.clickNextButton();
 	} );
 
-	it( `should display translations on features page (${ locale })`, async function () {
+	it( `Display translations on features page (${ locale })`, async function () {
 		await gutenboardingFlow.selectFeatures( [] );
 		await validatePageTranslations( page, locale );
 		await gutenboardingFlow.clickSkipButton();
 	} );
 
-	it( `should display translations on plans page (${ locale })`, async function () {
+	it( `Display translations on plans page (${ locale })`, async function () {
 		await gutenboardingFlow.expandAllPlans();
 		await validatePageTranslations( page, locale );
 	} );
