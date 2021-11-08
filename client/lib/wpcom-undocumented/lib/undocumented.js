c9d9e70a87978f25d6fa5c826da55d93cefafbe5
Marin Atanasov <tyxla@abv.bg>
2021-11-08 17:54:31 +0200

    WPCOM: Migrate wpcom.undocumented() site products to wpcom.req

--- client/lib/wpcom-undocumented/lib/undocumented.js
+++ client/lib/wpcom-undocumented/lib/undocumented.js
@@ -350,29 +350,6 @@ Undocumented.prototype.validateDomainContactInformation = function (
 };
 
 /**
- * Get site specific details for WordPress.com products
- *
- * @param {Function} siteDomain The site slug
- * @param {Function} fn The callback function
- */
-Undocumented.prototype.getSiteProducts = function ( siteDomain, fn ) {
-	debug( '/sites/:site_domain:/products query' );
-
-	// the site domain could be for a jetpack site installed in
-	// a subdirectory.  encode any forward slash present before making
-	// the request
-	siteDomain = encodeURIComponent( siteDomain );
-
-	return this._sendRequest(
-		{
-			path: '/sites/' + siteDomain + '/products',
-			method: 'get',
-		},
-		fn
-	);
-};
-
-/**
  * Get a site specific details for WordPress.com plans
  *
  * @param {Function} siteDomain The site slug
