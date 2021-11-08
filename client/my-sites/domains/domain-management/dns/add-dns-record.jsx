db22caf1ade2c83c21c411dbd3aebb37a72c72b3
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-11-08 19:48:47 -0300

    Remove unused code

--- client/my-sites/domains/domain-management/dns/add-dns-record.jsx
+++ client/my-sites/domains/domain-management/dns/add-dns-record.jsx
@@ -4,13 +4,10 @@ import PropTypes from 'prop-types';
 import { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import QueryDomainDns from 'calypso/components/data/query-domain-dns';
-import QuerySiteDomains from 'calypso/components/data/query-site-domains';
 import ExternalLink from 'calypso/components/external-link';
 import FormattedHeader from 'calypso/components/formatted-header';
 import Main from 'calypso/components/main';
-import VerticalNav from 'calypso/components/vertical-nav';
 import BodySectionCssClass from 'calypso/layout/body-section-css-class';
-import { getSelectedDomain, isMappedDomain } from 'calypso/lib/domains';
 import { localizeUrl } from 'calypso/lib/i18n-utils';
 import Breadcrumbs from 'calypso/my-sites/domains/domain-management/components/breadcrumbs';
 import DomainMainPlaceholder from 'calypso/my-sites/domains/domain-management/components/domain/main-placeholder';
@@ -23,53 +20,18 @@ import { fetchDns } from 'calypso/state/domains/dns/actions';
 import { getDomainDns } from 'calypso/state/domains/dns/selectors';
 import { successNotice, errorNotice } from 'calypso/state/notices/actions';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
-import { getDomainsBySiteId, isRequestingSiteDomains } from 'calypso/state/sites/domains/selectors';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
-import DnsTemplates from '../name-servers/dns-templates';
 import DnsAddNew from './dns-add-new';
 
 import './add-dns-record.scss';
 
 class AddDnsRecprd extends Component {
 	static propTypes = {
-		domains: PropTypes.array.isRequired,
 		dns: PropTypes.object.isRequired,
 		showPlaceholder: PropTypes.bool.isRequired,
 		selectedDomainName: PropTypes.string.isRequired,
 		selectedSite: PropTypes.oneOfType( [ PropTypes.object, PropTypes.bool ] ).isRequired,
 	};
-
-	constructor( props ) {
-		super( props );
-		this.onRestoreSuccess = this.onRestoreSuccess.bind( this );
-		this.onRestoreError = this.onRestoreError.bind( this );
-	}
-
-	onRestoreSuccess() {
-		const { translate, selectedDomainName } = this.props;
-		this.props.fetchDns( selectedDomainName, true );
-		this.props.successNotice(
-			translate( 'Yay, the name servers have been successfully updated!' )
-		);
-	}
-
-	onRestoreError( errorMessage ) {
-		this.props.errorNotice( errorMessage );
-	}
-
-	renderDnsTemplates() {
-		const selectedDomain = getSelectedDomain( this.props );
-
-		if ( ! selectedDomain || ! isMappedDomain( selectedDomain ) ) {
-			return null;
-		}
-
-		return (
-			<VerticalNav>
-				<DnsTemplates selectedDomainName={ this.props.selectedDomainName } />
-			</VerticalNav>
-		);
-	}
 
 	renderBreadcrumbs() {
 		const { translate, selectedSite, currentRoute, selectedDomainName } = this.props;
@@ -170,11 +132,10 @@ class AddDnsRecprd extends Component {
 	}
 
 	render() {
-		const { showPlaceholder, selectedDomainName, selectedSite } = this.props;
+		const { showPlaceholder, selectedDomainName } = this.props;
 
 		return (
 			<Fragment>
-				<QuerySiteDomains siteId={ selectedSite.ID } />
 				<QueryDomainDns domain={ selectedDomainName } />
 				{ showPlaceholder ? <DomainMainPlaceholder goBack={ this.goBack } /> : this.renderMain() }
 			</Fragment>
@@ -185,14 +146,11 @@ class AddDnsRecprd extends Component {
 export default connect(
 	( state, { selectedDomainName } ) => {
 		const selectedSite = getSelectedSite( state );
-		const domains = getDomainsBySiteId( state, selectedSite.ID );
-		const isRequestingDomains = isRequestingSiteDomains( state, selectedSite.ID );
 		const dns = getDomainDns( state, selectedDomainName );
-		const showPlaceholder = ! dns.hasLoadedFromServer || isRequestingDomains;
+		const showPlaceholder = false; // ! dns.hasLoadedFromServer;
 
 		return {
 			selectedSite,
-			domains,
 			dns,
 			showPlaceholder,
 			currentRoute: getCurrentRoute( state ),
