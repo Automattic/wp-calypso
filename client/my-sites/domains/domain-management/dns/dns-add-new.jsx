c5ddbfdd7819dc39574ef31feac1010594fe3a43
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-11-08 18:29:43 -0300

    Make DNS record type explanations translatable

--- client/my-sites/domains/domain-management/dns/dns-add-new.jsx
+++ client/my-sites/domains/domain-management/dns/dns-add-new.jsx
@@ -25,69 +25,79 @@ class DnsAddNew extends React.Component {
 		goBack: PropTypes.func,
 	};
 
-	state = {
-		fields: null,
-		type: 'A',
-	};
+	constructor( props ) {
+		super( props );
+		const { translate } = props;
 
-	dnsRecords = [
-		{
-			component: ARecord,
-			types: [ 'A', 'AAAA' ],
-			description:
-				'An A record is used to point a domain (e.g. example.com) or a subdomain (e.g. subdomain.example.com) to an IP address (192.168.1.1).',
-			initialFields: {
-				name: '',
-				data: '',
+		this.state = {
+			fields: null,
+			type: 'A',
+		};
+
+		this.dnsRecords = [
+			{
+				component: ARecord,
+				types: [ 'A', 'AAAA' ],
+				description: translate(
+					'An A record is used to point a domain (e.g. example.com) or a subdomain (e.g. subdomain.example.com) to an IP address (192.168.1.1).'
+				),
+				initialFields: {
+					name: '',
+					data: '',
+				},
 			},
-		},
-		{
-			component: CnameRecord,
-			types: [ 'CNAME' ],
-			description:
-				'CNAME (canonical name) records are typically used to link a subdomain (e.g. subdomain.example.com) to a domain (e.g. example.com).',
-			initialFields: {
-				name: '',
-				data: '',
+			{
+				component: CnameRecord,
+				types: [ 'CNAME' ],
+				description: translate(
+					'CNAME (canonical name) records are typically used to link a subdomain (e.g. subdomain.example.com) to a domain (e.g. example.com).'
+				),
+				initialFields: {
+					name: '',
+					data: '',
+				},
 			},
-		},
-		{
-			component: MxRecord,
-			types: [ 'MX' ],
-			description:
-				'MX (mail exchange) records are used to route emails to the correct mail servers.',
-			initialFields: {
-				name: '',
-				data: '',
-				aux: 10,
+			{
+				component: MxRecord,
+				types: [ 'MX' ],
+				description: translate(
+					'MX (mail exchange) records are used to route emails to the correct mail servers.'
+				),
+				initialFields: {
+					name: '',
+					data: '',
+					aux: 10,
+				},
 			},
-		},
-		{
-			component: TxtRecord,
-			types: [ 'TXT' ],
-			description:
-				"TXT (text) records are used to record any textual information on a domain. They're typically used by other service providers (e.g. email services) to ensure you are the owner of the domain.",
-			initialFields: {
-				name: '',
-				data: '',
+			{
+				component: TxtRecord,
+				types: [ 'TXT' ],
+				description: translate(
+					"TXT (text) records are used to record any textual information on a domain. They're typically used by other service providers (e.g. email services) to ensure you are the owner of the domain."
+				),
+				initialFields: {
+					name: '',
+					data: '',
+				},
 			},
-		},
-		{
-			component: SrvRecord,
-			types: [ 'SRV' ],
-			description:
-				'SRV (service) records define the information to access certain internet services.',
-			initialFields: {
-				name: '',
-				service: '',
-				aux: 10,
-				weight: 10,
-				target: '',
-				port: '',
-				protocol: 'tcp',
+			{
+				component: SrvRecord,
+				types: [ 'SRV' ],
+				description: translate(
+					'SRV (service) records define the information to access certain internet services.'
+				),
+				initialFields: {
+					name: '',
+					service: '',
+					aux: 10,
+					weight: 10,
+					target: '',
+					port: '',
+					protocol: 'tcp',
+				},
 			},
-		},
-	];
+		];
+	}
 
 	getFieldsForType( type ) {
 		const dnsRecord = find( this.dnsRecords, ( record ) => {
