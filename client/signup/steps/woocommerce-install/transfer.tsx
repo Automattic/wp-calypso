d47d0d230a24c6c7b83ecac7f94ed9a27e975f1a
Louis Laugesen <louis.laugesen@gmail.com>
2021-11-09 17:17:09 +1100

    Implement basic transfer step in new woo installer

--- client/signup/steps/woocommerce-install/transfer.tsx
+++ client/signup/steps/woocommerce-install/transfer.tsx
@@ -1,7 +1,19 @@
+import { ProgressBar } from '@automattic/components';
 import { BackButton, NextButton, SubTitle, Title } from '@automattic/onboarding';
 import { useI18n } from '@wordpress/react-i18n';
+import { ReactElement, useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAutomatedTransferStatus } from 'calypso/state/automated-transfer/actions';
+import { transferStates } from 'calypso/state/automated-transfer/constants';
+import {
+	isFetchingAutomatedTransferStatus,
+	getAutomatedTransferStatus,
+} from 'calypso/state/automated-transfer/selectors';
+import { initiateThemeTransfer } from 'calypso/state/themes/actions';
+import { hasUploadFailed } from 'calypso/state/themes/upload-theme/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import type { GoToStep } from '../../types';
-import type { ReactElement } from 'react';
+import type { AppState } from 'calypso/types';
 
 interface Props {
 	goToStep: GoToStep;
@@ -9,6 +21,76 @@ interface Props {
 
 export default function Transfer( { goToStep }: Props ): ReactElement | null {
 	const { __ } = useI18n();
+
+	const siteId = useSelector( getSelectedSiteId ) as number;
+
+	const [ progress, setProgress ] = useState( 0 );
+	const [ error, setError ] = useState( { transferFailed: false, transferStatus: null } );
+
+	const dispatch = useDispatch();
+
+	// Select state
+	const fetchingTransferStatus = !! useSelector( ( state: AppState ) =>
+		isFetchingAutomatedTransferStatus( state, siteId )
+	);
+	const transferStatus = useSelector( ( state: AppState ) =>
+		getAutomatedTransferStatus( state, siteId )
+	);
+
+	const transferFailed = useSelector( ( state: AppState ) => hasUploadFailed( state, siteId ) );
+
+	// Initiate Atomic transfer
+	useEffect( () => {
+		if ( ! siteId ) {
+			return;
+		}
+
+		dispatch( fetchAutomatedTransferStatus( siteId ) ); // todo: can possibly drop this, polling should be kicked off by initiate call
+		dispatch( initiateThemeTransfer( siteId, null, 'woocommerce' ) );
+	}, [ siteId, dispatch ] );
+
+	// Watch transfer status
+	useEffect( () => {
+		if ( fetchingTransferStatus ) {
+			return;
+		}
+
+		switch ( transferStatus ) {
+			case transferStates.NONE:
+			case transferStates.PENDING:
+			case transferStates.INQUIRING:
+			case transferStates.PROVISIONED:
+			case transferStates.FAILURE:
+			case transferStates.START:
+			case transferStates.REVERTED:
+				setProgress( 20 );
+				break;
+			case transferStates.SETUP:
+			case transferStates.CONFLICTS:
+			case transferStates.ACTIVE:
+				setProgress( 50 );
+				break;
+			case transferStates.UPLOADING:
+			case transferStates.BACKFILLING:
+				setProgress( 60 );
+				break;
+			case transferStates.COMPLETE:
+				setProgress( 100 );
+				goToStep( 'complete' );
+				break;
+		}
+
+		if (
+			transferFailed ||
+			transferStatus === transferStates.ERROR ||
+			transferStatus === transferStates.FAILURE ||
+			transferStatus === transferStates.REQUEST_FAILURE ||
+			transferStatus === transferStates.CONFLICTS
+		) {
+			setProgress( 100 );
+			setError( { transferFailed, transferStatus } );
+		}
+	}, [ setProgress, goToStep, fetchingTransferStatus, transferStatus, transferFailed ] );
 
 	return (
 		<>
@@ -26,6 +108,9 @@ export default function Transfer( { goToStep }: Props ): ReactElement | null {
 			</div>
 			<div className="woocommerce-install__content">
 				<div>
+					{ error.transferFailed && 'error...' }
+					{ error.transferFailed && error.transferStatus }
+					<ProgressBar value={ progress || 1 } total={ 100 } isPulsing />
 					<NextButton onClick={ () => goToStep( 'install' ) }>{ __( 'Next' ) }</NextButton>
 				</div>
 			</div>
