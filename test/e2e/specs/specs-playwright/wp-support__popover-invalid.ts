9407753c30910b826e8775035d3a12f796220cd2
Edwin Takahashi <egao@outlook.com>
2021-11-09 11:29:06 -0800

    Split the poover tests into two - valid and invalid. For the valid test (one with frequent intermittent failure), use a different keyword.

--- test/e2e/specs/specs-playwright/wp-support__popover-invalid.ts
+++ test/e2e/specs/specs-playwright/wp-support__popover-invalid.ts
@@ -11,60 +11,11 @@ import {
 } from '@automattic/calypso-e2e';
 import { Page } from 'playwright';
 
-describe( DataHelper.createSuiteTitle( 'Support: Popover' ), function () {
+describe( DataHelper.createSuiteTitle( 'Support: Popover/Invalid Keywords' ), function () {
 	let page: Page;
 
 	setupHooks( ( args: { page: Page } ) => {
 		page = args.page;
-	} );
-
-	describe.each( [
-		{ siteType: 'Simple', user: 'defaultUser' },
-		{ siteType: 'Atomic', user: 'eCommerceUser' },
-	] )( 'Search and view a support article ($siteType)', function ( { user } ) {
-		let supportComponent: SupportComponent;
-		let supportArticlePage: Page;
-
-		it( 'Log in', async function () {
-			const loginPage = new LoginPage( page );
-			await loginPage.login( { account: user } );
-		} );
-
-		it( 'Navigate to Tools > Marketing', async function () {
-			const sidebarComponent = new SidebarComponent( page );
-			await sidebarComponent.navigate( 'Tools', 'Marketing' );
-		} );
-
-		it( 'Open support popover', async function () {
-			supportComponent = new SupportComponent( page );
-			await supportComponent.openPopover();
-		} );
-
-		it( 'Displays default entries', async function () {
-			await supportComponent.defaultStateShown();
-		} );
-
-		it( 'Enter search keyword', async function () {
-			const keyword = 'domain';
-			await supportComponent.search( keyword );
-		} );
-
-		it( 'Search results are shown', async function () {
-			const results = await supportComponent.getResults( 'article' );
-			expect( results.length ).toBeGreaterThan( 0 );
-		} );
-
-		it( 'Click and visit first support article', async function () {
-			await supportComponent.clickResult( 'article', 1 );
-			await supportComponent.clickReadMore();
-			// Obtain handle to the popup page.
-			supportArticlePage = await supportComponent.visitArticle();
-		} );
-
-		it( 'Close article page and preview', async function () {
-			await supportArticlePage.close();
-			await supportComponent.closeArticle();
-		} );
 	} );
 
 	describe.each( [
