3d67b392877af219d32e87b0122fb08b8d91c796
Bryan Elliott <bryan@elliottprogrammer.com>
2021-11-09 16:28:56 -0500

    User licensing: Add post-checkout UI for activation "completed" (#57708)

---
+++ client/state/selectors/get-jetpack-checkout-support-ticket-incompatible-products.ts
@@ -0,0 +1,18 @@
+import 'calypso/state/data-layer/wpcom/jetpack-checkout/support-ticket';
+
+import type { AppState } from 'calypso/types';
+
+/**
+ * Returns the product ID's of the site's subscriptions that are incompatible with
+ * the subscription that could not be transferred.
+ *
+ * @param  {object}    state     Global state tree
+ * @param  {number}    siteId    The ID of the temporary site
+ * @returns {Array} The response body containing  incompatible_product_ids: []
+ */
+export default function getJetpackCheckoutIncompatibleProductIds(
+	state: AppState,
+	siteId: number
+): number[] {
+	return state.jetpackCheckout?.submittedSiteUrl?.[ siteId ]?.incompatible_product_ids || [];
+}
