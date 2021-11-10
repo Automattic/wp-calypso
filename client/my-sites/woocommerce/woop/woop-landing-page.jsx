34535997d03761de74739312a328810730e5b97b
Allison Levine <allison.levine@automattic.com>
2021-11-10 16:30:39 -0500

    Add cta header component

--- client/my-sites/woocommerce/woop/woop-landing-page.jsx
+++ client/my-sites/woocommerce/woop/woop-landing-page.jsx
@@ -1,14 +1,26 @@
 import { Button } from '@automattic/components';
 import { translate } from 'i18n-calypso';
+import CtaHeader from 'calypso/components/cta-header';
+import FixedNavigationHeader from 'calypso/components/fixed-navigation-header';
 
 const WoopLandingPage = ( props ) => {
 	const { startSetup } = props;
+	const navigationItems = [ { label: 'WooCommerce', href: `/woocommerce-installation` } ];
 	return (
 		<div className="woop-landing-page">
-			<h1>Woop Landing Page</h1>
-			<Button onClick={ startSetup } primary>
-				{ translate( 'Set up my store!' ) }
-			</Button>
+			<FixedNavigationHeader navigationItems={ navigationItems }>
+				<Button onClick={ startSetup } primary>
+					{ translate( 'Set up my store!' ) }
+				</Button>
+			</FixedNavigationHeader>
+			<CtaHeader
+				title={ translate( 'Have something to sell?' ) }
+				subtitle={ translate( 'Upgrade to the Premium plan and set up your WooCommerce store.' ) }
+				buttonText={ translate( 'Set up my store!' ) }
+				buttonAction={ startSetup }
+			>
+				Right content
+			</CtaHeader>
 		</div>
 	);
 };
