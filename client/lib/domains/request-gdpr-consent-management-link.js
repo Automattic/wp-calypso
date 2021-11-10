407cfb4e1cc5e717be735150c804590ed0f30323
Marin Atanasov <tyxla@abv.bg>
2021-11-10 18:00:02 +0200

    Inline the request

--- client/lib/domains/request-gdpr-consent-management-link.js
+++
@@ -1,5 +0,0 @@
-import wpcom from 'calypso/lib/wp';
-
-export function requestGdprConsentManagementLink( domainName ) {
-	return wpcom.req.get( `/domains/${ domainName }/request-gdpr-consent-management-link` );
-}
