0713c73f407aa59f13cfe76dd9ffd03f897e6c4f
dpasque <dan.speckhard.pasque@automattic.com>
2021-11-11 13:29:12 -0600

    Take metric change out of quarantined tests

--- .teamcity/_self/projects/WebApp.kt
+++ .teamcity/_self/projects/WebApp.kt
@@ -789,14 +789,5 @@ object QuarantinedE2ETests: BuildType( {
 	failureConditions {
 		executionTimeoutMin = 20
 		nonZeroExitCode = false
-		failOnMetricChange {
-			metric = BuildFailureOnMetric.MetricType.PASSED_TEST_COUNT
-			threshold = 50
-			units = BuildFailureOnMetric.MetricUnit.PERCENTS
-			comparison = BuildFailureOnMetric.MetricComparison.LESS
-			compareTo = build {
-				buildRule = lastSuccessful()
-			}
-		}
 	}
 })
