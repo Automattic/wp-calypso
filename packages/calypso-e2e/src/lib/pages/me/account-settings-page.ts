bf8f2c77b2a1d30fb8d096981864aaa63ed08e1d
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2021-11-11 19:06:51 +0200

    e2e: Add i18n editor tests (#56940)
    * e2e: Add i18n editor tests

--- packages/calypso-e2e/src/lib/pages/me/account-settings-page.ts
+++ packages/calypso-e2e/src/lib/pages/me/account-settings-page.ts
@@ -1,4 +1,5 @@
 import { Page } from 'playwright';
+import type { LanguageSlug } from '@automattic/languages';
 
 const selectors = {
 	// Close account
@@ -11,6 +12,13 @@ const selectors = {
 	usernameSpan: `span.account-close__confirm-dialog-target-username`,
 	usernameConfirmationInput: `input[id="confirmAccountCloseInput"]`,
 	modalCloseAccountButton: `button:text("Close your account")`,
+
+	// UI Language
+	uiLanguageButton: 'button.language-picker',
+	uiLanguageSearch: '.language-picker-component__search-desktop .search-component__input',
+	uiLanguageItem: ( localeSlug: LanguageSlug ) =>
+		`.language-picker-component__language-button:has([lang="${ localeSlug }"])`,
+	uiLanguageApplyButton: '.language-picker__modal-buttons button.is-secondary',
 };
 
 /**
@@ -51,5 +59,15 @@ export class AccountSettingsPage {
 			.then( ( element ) => element.innerText() );
 		await this.page.fill( selectors.usernameConfirmationInput, username );
 		await this.page.click( selectors.modalCloseAccountButton );
+	}
+
+	/**
+	 * Changes the UI language for the currently logged in user.
+	 */
+	async changeUILanguage( localeSlug: LanguageSlug ): Promise< void > {
+		await this.page.click( selectors.uiLanguageButton );
+		await this.page.fill( selectors.uiLanguageSearch, localeSlug );
+		await this.page.click( selectors.uiLanguageItem( localeSlug ) );
+		await this.page.click( selectors.uiLanguageApplyButton );
 	}
 }
