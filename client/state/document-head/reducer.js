01b6109484a4c1db09199a1e47be2203443850df
Jarda Snajdr <jsnajdr@gmail.com>
2021-11-11 11:35:48 +0100

    Lodash: remove usages of uniqWith

--- client/state/document-head/reducer.js
+++ client/state/document-head/reducer.js
@@ -1,6 +1,6 @@
 import config from '@automattic/calypso-config';
 import { withStorageKey } from '@automattic/state-utils';
-import { uniqWith, isEqual } from 'lodash';
+import { isEqual } from 'lodash';
 import {
 	DOCUMENT_HEAD_LINK_SET,
 	DOCUMENT_HEAD_META_SET,
@@ -47,17 +47,19 @@ export const meta = withSchemaValidation( metaSchema, ( state = DEFAULT_META_STA
 
 export const link = withSchemaValidation( linkSchema, ( state = [], action ) => {
 	switch ( action.type ) {
-		case DOCUMENT_HEAD_LINK_SET:
+		case DOCUMENT_HEAD_LINK_SET: {
 			if ( ! action.link ) {
 				return state;
 			}
 
 			// Append action.link to the state array and prevent duplicate objects.
 			// Works with action.link being a single link object or an array of link objects.
-			return uniqWith(
-				[ ...state, ...( Array.isArray( action.link ) ? action.link : [ action.link ] ) ],
-				isEqual
-			);
+			const links = Array.isArray( action.link ) ? action.link : [ action.link ];
+			state = links.reduce( ( accuState, newLink ) => {
+				const isNew = ! accuState.some( ( accuLink ) => isEqual( accuLink, newLink ) );
+				return isNew ? [ ...accuState, newLink ] : accuState;
+			}, state );
+		}
 	}
 
 	return state;
