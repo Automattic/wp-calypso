d9b7a2ee923f6fbcdac81aa8b76415f3a15a0c1e
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2021-11-12 21:06:59 +0400

    Take checkout page screenshot with billing details filled and ToS link visible

--- test/e2e/specs/specs-playwright/wp-signup__screenshot.ts
+++ test/e2e/specs/specs-playwright/wp-signup__screenshot.ts
@@ -37,14 +37,12 @@ describe( DataHelper.createSuiteTitle( 'Signup: WordPress.com Free' ), function 
 	} );
 
 	describe( 'Signup', function () {
-		const targetDomain = `${ blogName }.live`;
-		
 		let cartCheckoutPage: CartCheckoutPage;
 
 		it( 'Screenshot Login page and navigate to Signup page', async function () {
 			const loginPage = new LoginPage( page );
 			await loginPage.visit();
-			await page.screenshot( { path: 'login.png' } );
+			await page.screenshot( { path: 'login.png', fullPage: true } );
 			let form = new FormData();
 			form.append( 'my_file', fs.createReadStream( 'login.png' ) );
 			await fetch( 'https://public-api.wordpress.com/wpcom/v2/screenshots', {
@@ -62,7 +60,7 @@ describe( DataHelper.createSuiteTitle( 'Signup: WordPress.com Free' ), function 
 
 		it( 'Screenshot signup page and sign up as new user', async function () {
 			const userSignupPage = new UserSignupPage( page );
-			await page.screenshot( { path: 'signup.png' } );
+			await page.screenshot( { path: 'signup.png', fullPage: true } );
 			let form = new FormData();
             form.append( 'my_file', fs.createReadStream( 'signup.png' ) );
             await fetch( 'https://public-api.wordpress.com/wpcom/v2/screenshots', {
@@ -74,10 +72,10 @@ describe( DataHelper.createSuiteTitle( 'Signup: WordPress.com Free' ), function 
 			await userSignupPage.signup( email, username, signupPassword );
 		} );
 
-		it( 'Select a .live domain', async function () {
+		it( 'Select a free .wordpress.com domain', async function () {
 			const domainSearchComponent = new DomainSearchComponent( page );
 			await domainSearchComponent.search( blogName );
-			await domainSearchComponent.selectDomain( targetDomain );
+			await domainSearchComponent.selectDomain( '.wordpress.com' );
 		} );
 
 		it( 'Select WordPress.com Personal plan', async function () {
@@ -88,11 +86,16 @@ describe( DataHelper.createSuiteTitle( 'Signup: WordPress.com Free' ), function 
 		it( 'See secure payment', async function () {
 			cartCheckoutPage = new CartCheckoutPage( page );
 			await cartCheckoutPage.validateCartItem( 'WordPress.com Personal' );
-			await cartCheckoutPage.validateCartItem( targetDomain );
+		} );
+
+		it( 'Enter billing and payment details', async function () {
+			const paymentDetails = DataHelper.getTestPaymentDetails();
+			await cartCheckoutPage.enterBillingDetails( paymentDetails );
+			await cartCheckoutPage.enterPaymentDetails( paymentDetails );
 		} );
 
 		it( 'Take screenshot of checkout page', async function () {
-			await page.screenshot( { path: 'checkout.png' } );
+			await page.screenshot( { path: 'checkout.png', fullPage: true } );
 			let form = new FormData();
             form.append( 'my_file', fs.createReadStream( 'checkout.png' ) );
             await fetch( 'https://public-api.wordpress.com/wpcom/v2/screenshots', {
