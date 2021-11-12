8f7b08c6eb17a011e764f8f64bb220b7de575909
Del Putnam <del@putnams.net>
2021-11-12 07:30:13 -0500

    Better types for promises

--- client/my-sites/domains/domain-management/dns/types.tsx
+++ client/my-sites/domains/domain-management/dns/types.tsx
@@ -1,5 +1,13 @@
 import { setDnsDefaultARecords } from 'calypso/state/domains/dns/actions';
 import { errorNotice, successNotice } from 'calypso/state/notices/actions';
+import type { AnyAction, Dispatch } from 'redux';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UnpackPromisedValue< T > = T extends ( ...args: any[] ) => infer R
+	? R extends ( dispatch: Dispatch< AnyAction > ) => infer ActionType
+		? ( ...args: Parameters< T > ) => Promise< ActionType >
+		: ( ...args: Parameters< T > ) => Promise< R >
+	: T;
 
 export type DnsRecord = {
 	id: string;
@@ -16,9 +24,9 @@ export type DnsRecord = {
 export type DnsMenuOptionsButtonProps = {
 	domain: string;
 	pointsToWpcom: boolean;
-	dispatchSetDnsDefaultARecords: typeof setDnsDefaultARecords;
-	dispatchSuccessNotice: typeof successNotice;
-	dispatchErrorNotice: typeof errorNotice;
+	dispatchSetDnsDefaultARecords: UnpackPromisedValue< typeof setDnsDefaultARecords >;
+	dispatchSuccessNotice: UnpackPromisedValue< typeof successNotice >;
+	dispatchErrorNotice: UnpackPromisedValue< typeof errorNotice >;
 };
 
 export type RestoreDialogResult = {
