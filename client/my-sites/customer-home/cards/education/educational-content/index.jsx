ccd58d03eafc33a9fb77735db3489ae4ac357c74
Andres Blanco <andresblanco@gmail.com>
2021-11-12 14:27:31 -0300

    Revert "Adding educational card with link to videos UI modal (#57515)"
    This reverts commit 526b287e035ea62d852d9d6b456de581360e455b.

--- client/my-sites/customer-home/cards/education/educational-content/index.jsx
+++ client/my-sites/customer-home/cards/education/educational-content/index.jsx
@@ -19,7 +19,6 @@ export default function EducationalContent( {
 	title,
 	description,
 	links,
-	modalLinks,
 	illustration,
 	cardName,
 } ) {
@@ -33,50 +32,42 @@ export default function EducationalContent( {
 					{ description }
 				</p>
 				<div className="educational-content__links">
-					{ links &&
-						links.map( ( { postId, url, calypsoLink, externalLink, text, icon, materialIcon } ) => (
-							<div className="educational-content__link" key={ url }>
-								{ icon && <Gridicon icon={ icon } size={ 18 } /> }
-								{ materialIcon && <MaterialIcon icon={ materialIcon } /> }
-								{ postId && (
-									<InlineSupportLink
-										supportPostId={ postId }
-										supportLink={ url }
-										showIcon={ false }
-										tracksEvent="calypso_customer_home_education"
-										statsGroup="calypso_customer_home"
-										tracksOptions={ {
-											url,
-											card_name: cardName,
-										} }
-										statsName={ cardName }
-									>
-										{ text }
-									</InlineSupportLink>
-								) }
-								{ externalLink && (
-									<ExternalLink
-										href={ url }
-										onClick={ () => dispatch( trackNavigation( url, cardName ) ) }
-										icon
-									>
-										{ text }
-									</ExternalLink>
-								) }
-								{ calypsoLink && (
-									<a href={ url } onClick={ () => dispatch( trackNavigation( url, cardName ) ) }>
-										{ text }
-									</a>
-								) }
-							</div>
-						) ) }
-					{ modalLinks &&
-						modalLinks.map( ( { ModalComponent, modalComponentProps, onClick, text } ) => (
-							<div className="educational-content__link" key={ text }>
-								<ModalComponent { ...modalComponentProps } />
-								<button onClick={ () => onClick() }>{ text }</button>
-							</div>
-						) ) }
+					{ links.map( ( { postId, url, calypsoLink, externalLink, text, icon, materialIcon } ) => (
+						<div className="educational-content__link" key={ url }>
+							{ icon && <Gridicon icon={ icon } size={ 18 } /> }
+							{ materialIcon && <MaterialIcon icon={ materialIcon } /> }
+							{ postId && (
+								<InlineSupportLink
+									supportPostId={ postId }
+									supportLink={ url }
+									showIcon={ false }
+									tracksEvent="calypso_customer_home_education"
+									statsGroup="calypso_customer_home"
+									tracksOptions={ {
+										url,
+										card_name: cardName,
+									} }
+									statsName={ cardName }
+								>
+									{ text }
+								</InlineSupportLink>
+							) }
+							{ externalLink && (
+								<ExternalLink
+									href={ url }
+									onClick={ () => dispatch( trackNavigation( url, cardName ) ) }
+									icon
+								>
+									{ text }
+								</ExternalLink>
+							) }
+							{ calypsoLink && (
+								<a href={ url } onClick={ () => dispatch( trackNavigation( url, cardName ) ) }>
+									{ text }
+								</a>
+							) }
+						</div>
+					) ) }
 				</div>
 			</div>
 			{ isDesktop() && (
