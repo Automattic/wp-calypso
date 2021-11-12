748d28cd581c22212b80e982b479ff434df57fcf
retrofox <rdsuarez@gmail.com>
2021-11-12 11:27:15 -0300

    woop: tweak step styles and dims

--- client/signup/steps/woocommerce-install-confirm-wordpress-subdomain/index.tsx
+++ client/signup/steps/woocommerce-install-confirm-wordpress-subdomain/index.tsx
@@ -16,6 +16,7 @@ import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import type { GoToStep } from '../../types';
 import type { AppState } from 'calypso/types';
 
+import './style.scss';
 interface Props {
 	goToStep: GoToStep;
 	isReskinned: boolean;
