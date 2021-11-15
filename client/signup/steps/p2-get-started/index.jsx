e21685cea151e2fb76a1960cb3880b8fbd39ec7a
annemirasol <annemirasol@gmail.com>
2021-11-15 23:23:27 +0800

    Step p2-get-started: add some details

--- client/signup/steps/p2-get-started/index.jsx
+++ client/signup/steps/p2-get-started/index.jsx
@@ -1,13 +1,25 @@
+import { useTranslate } from 'i18n-calypso';
+import P2StepWrapper from 'calypso/signup/p2-step-wrapper';
+
 import './style.scss';
 
-function P2GetStarted() {
+function P2GetStarted( { flowName, stepName, positionInFlow } ) {
+	const translate = useTranslate();
+
 	return (
-		<div>
-			<span role="img" aria-label="emoji">
-				🚧
-			</span>{ ' ' }
-			I am under construction!
-		</div>
+		<P2StepWrapper
+			flowName={ flowName }
+			stepName={ stepName }
+			positionInFlow={ positionInFlow }
+			headerText={ translate( 'Get started with P2' ) }
+		>
+			<div className="p2-get-started">
+				<span role="img" aria-label="emoji">
+					🚧
+				</span>{ ' ' }
+				I am under construction!
+			</div>
+		</P2StepWrapper>
 	);
 }
 
