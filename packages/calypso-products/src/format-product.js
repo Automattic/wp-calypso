7686f605c84ff93c3b300e5818a90609a23fc06b
Payton Swick <payton@foolord.com>
2021-11-15 16:22:20 -0500

    Add deprecated comment to formatProduct

--- packages/calypso-products/src/format-product.js
+++ packages/calypso-products/src/format-product.js
@@ -1,3 +1,17 @@
+/**
+ * This function is deprecated. Do not use it for new code.
+ *
+ * Please use the properties you expect rather than running objects through
+ * this function superstitiously. It will make type checking much easier.
+ *
+ * This function basically just camelCases specific properties, but it does so
+ * for various definitions of the word "product". Some products are shopping
+ * cart products, some are product endpoint products, some are products list
+ * products, and some are even stranger things. As long as this function is
+ * used, we cannot know what sorts of data we are using.
+ *
+ * @deprecated
+ */
 export function formatProduct( product ) {
 	return {
 		...product,
