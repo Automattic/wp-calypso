668e64e7549115e18a8eb37f15f8d01b0c8082db
Del Putnam <del@putnams.net>
2021-11-15 10:21:26 -0500

    Use data returned from endpoint on successful DNS record delete.

--- client/state/domains/dns/reducer.js
+++ client/state/domains/dns/reducer.js
@@ -205,7 +205,7 @@ export default function reducer( state = {}, action ) {
 			} );
 			break;
 		case DOMAINS_DNS_DELETE_COMPLETED:
-			state = deleteDns( state, action.domainName, action.record );
+			state = updateDomainState( state, action.domainName, { records: action.records } );
 			break;
 		case DOMAINS_DNS_DELETE_FAILED:
 			state = updateDnsState( state, action.domainName, action.record, {
