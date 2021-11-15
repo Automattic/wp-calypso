8f5aece6893d9e7120f923fa5eff8d6d09cf9bba
Payton Swick <payton@foolord.com>
2021-11-15 18:48:00 -0500

    Allow extra to be undefined in getDomain

--- packages/calypso-products/src/get-domain.ts
+++ packages/calypso-products/src/get-domain.ts
@@ -1,6 +1,6 @@
 export function getDomain( product: {
 	meta: string;
-	extra: { domain_to_bundle?: string };
+	extra?: { domain_to_bundle?: string };
 } ): string {
-	return product.extra?.domain_to_bundle ?? product.meta ?? '';
+	return product.extra?.domain_to_bundle ?? product.meta;
 }
