32d51d76608b1fd9e422211795ce2920fb3e5519
Payton Swick <payton@foolord.com>
2021-11-15 16:58:29 -0500

    Clarify comment on formatProduct

--- packages/calypso-products/src/format-product.js
+++ packages/calypso-products/src/format-product.js
@@ -4,11 +4,12 @@
  * Please use the properties you expect rather than running objects through
  * this function superstitiously. It will make type checking much easier.
  *
- * This function basically just camelCases specific properties, but it does so
+ * This function basically just snake_cases specific properties, but it does so
  * for various definitions of the word "product". Some products are shopping
  * cart products, some are product endpoint products, some are products list
- * products, and some are even stranger things. As long as this function is
- * used, we cannot know what sorts of data we are using.
+ * products, some are purchases list products, and some are even stranger
+ * things. As long as this function is used, we cannot know what sorts of data
+ * we are using.
  *
  * @deprecated
  */
