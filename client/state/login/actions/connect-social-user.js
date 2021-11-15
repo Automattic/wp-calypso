b5f2c074fcc9f291c739f4e6c77f87a8e4961be4
Marin Atanasov <tyxla@abv.bg>
2021-11-15 16:19:36 +0200

    WPCOM: Migrate wpcom.undocumented() social connect to wpcom.req

--- client/state/login/actions/connect-social-user.js
+++ client/state/login/actions/connect-social-user.js
@@ -1,3 +1,4 @@
+import config from '@automattic/calypso-config';
 import { translate } from 'i18n-calypso';
 import wpcom from 'calypso/lib/wp';
 import {
@@ -12,14 +13,19 @@ import 'calypso/state/login/init';
 /**
  * Connects the current WordPress.com account with a third-party social account (Google ...).
  *
- * @param  {object}   socialInfo     Object containing { service, access_token, id_token, redirectTo }
- *           {string}   service      The external social service name
- *           {string}   access_token OAuth2 access token provided by the social service
- *           {string}   id_token     JWT ID token such as the one provided by Google OpenID Connect
- * @param  {string}   redirectTo     Url to redirect the user to upon successful login
- * @returns {Function}                A thunk that can be dispatched
+ * @param {object} socialInfo              Object containing the social service config data
+ * @param {string} socialInfo.service      Social service associated with token, e.g. google.
+ * @param {string} socialInfo.access_token OAuth2 Token returned from service.
+ * @param {string} socialInfo.id_token     (Optional) OpenID Connect Token returned from service.
+ * @param {string} socialInfo.user_name    (Optional) The user name associated with this connection, in case it's not part of id_token.
+ * @param {string} socialInfo.user_email   (Optional) The user email associated with this connection, in case it's not part of id_token.
+ * @param {string} redirectTo              Url to redirect the user to upon successful login
+ * @returns {Function} A thunk that can be dispatched
  */
-export const connectSocialUser = ( socialInfo, redirectTo ) => ( dispatch ) => {
+export const connectSocialUser = (
+	{ service, access_token, id_token, user_name, user_email },
+	redirectTo
+) => ( dispatch ) => {
 	dispatch( {
 		type: SOCIAL_CONNECT_ACCOUNT_REQUEST,
 		notice: {
@@ -27,10 +33,29 @@ export const connectSocialUser = ( socialInfo, redirectTo ) => ( dispatch ) => {
 		},
 	} );
 
-	return wpcom
-		.undocumented()
-		.me()
-		.socialConnect( { ...socialInfo, redirect_to: redirectTo } )
+	/*
+	 * Before attempting the social connect, we reload the proxy.
+	 * This ensures that the proxy iframe has set the correct API cookie,
+	 * particularly after the user has logged in, but Calypso hasn't
+	 * been reloaded yet.
+	 */
+	require( 'wpcom-proxy-request' ).reloadProxy();
+
+	wpcom.req.post( { metaAPI: { accessAllUsersBlogs: true } } );
+
+	return wpcom.req
+		.post( '/me/social-login/connect', {
+			service,
+			access_token,
+			id_token,
+			user_name,
+			user_email,
+			redirect_to: redirectTo,
+
+			// This API call is restricted to these OAuth keys
+			client_id: config( 'wpcom_signup_id' ),
+			client_secret: config( 'wpcom_signup_key' ),
+		} )
 		.then(
 			( wpcomResponse ) => {
 				dispatch( {
