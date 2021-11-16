ada2ecd94d2df7601a63ea07b1434d51e70631a4
Rolando Perez <me@rolilink.com>
2021-11-16 18:29:59 -0500

    renaming cta images to a more descriptive name | removing unused cta images

--- client/my-sites/woocommerce/woop/landing-page.tsx
+++ client/my-sites/woocommerce/woop/landing-page.tsx
@@ -2,10 +2,10 @@ import { Button } from '@automattic/components';
 import { useRef } from '@wordpress/element';
 import { translate } from 'i18n-calypso';
 import './style.scss';
-import Image01 from 'calypso/assets/images/woocommerce/image01.jpeg';
-import Image02 from 'calypso/assets/images/woocommerce/image02.jpeg';
-import Image04 from 'calypso/assets/images/woocommerce/image04.jpeg';
-import Image05 from 'calypso/assets/images/woocommerce/image05.jpeg';
+import Image01 from 'calypso/assets/images/woocommerce/woop-cta-image01.jpeg';
+import Image02 from 'calypso/assets/images/woocommerce/woop-cta-image02.jpeg';
+import Image03 from 'calypso/assets/images/woocommerce/woop-cta-image03.jpeg';
+import Image04 from 'calypso/assets/images/woocommerce/woop-cta-image04.jpeg';
 import CtaSection from 'calypso/components/cta-section';
 import FixedNavigationHeader from 'calypso/components/fixed-navigation-header';
 import MasonryWave from 'calypso/components/masonry-wave';
@@ -14,7 +14,7 @@ interface Props {
 	startSetup: () => void;
 }
 
-const images = [ { src: Image01 }, { src: Image05 }, { src: Image02 }, { src: Image04 } ];
+const images = [ { src: Image01 }, { src: Image02 }, { src: Image03 }, { src: Image04 } ];
 
 const WoopLandingPage: React.FunctionComponent< Props > = ( { startSetup } ) => {
 	const navigationItems = [ { label: 'WooCommerce', href: `/woocommerce-installation` } ];
