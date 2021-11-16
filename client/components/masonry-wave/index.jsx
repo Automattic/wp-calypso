b88d53a695756e25e37557b02959f27bab239635
Rolando Perez <me@rolilink.com>
2021-11-16 18:01:07 -0500

    adds @automattic/viewport-react and fixes key related warnings
    - Adds @automattic/viewport-react to the MasonryWave component to conditionally render different layouts on mobile and web.
    - Fixes missing key warnings on the MasonryWave component.

--- client/components/masonry-wave/index.jsx
+++ client/components/masonry-wave/index.jsx
@@ -1,29 +1,5 @@
 import './style.scss';
-
-import { useState, useEffect } from 'react';
-
-function getWindowDimensions() {
-	const { innerWidth: width, innerHeight: height } = window;
-	return {
-		width,
-		height,
-	};
-}
-
-function useWindowDimensions() {
-	const [ windowDimensions, setWindowDimensions ] = useState( getWindowDimensions() );
-
-	useEffect( () => {
-		function handleResize() {
-			setWindowDimensions( getWindowDimensions() );
-		}
-
-		window.addEventListener( 'resize', handleResize );
-		return () => window.removeEventListener( 'resize', handleResize );
-	}, [] );
-
-	return windowDimensions;
-}
+import { useDesktopBreakpoint } from '@automattic/viewport-react';
 
 // fills the columns with images LTR to imitate the CSS grid behavior
 const fillColumnsWithImages = ( amountOfColumns, images ) => {
@@ -60,17 +36,16 @@ const MasonryWaveColumn = ( props ) => {
 
 	return (
 		<div className="masonry-wave__column">
-			{ images.map( ( image ) => (
-				<MasonryWaveElement image={ image } />
+			{ images.map( ( image, index ) => (
+				<MasonryWaveElement key={ `masonry-wave__row-${ index }` } image={ image } />
 			) ) }
 		</div>
 	);
 };
 
 const MasonryWave = ( props ) => {
-	const { width } = useWindowDimensions();
+	const isDesktop = useDesktopBreakpoint();
 
-	const isDesktop = width >= 480; // should probably do this better?
 	const { numberOfColumns, images } = props;
 	const numberOfDesktopColumns = numberOfColumns?.desktop ?? 2;
 	const numberOfMobileColumns = numberOfColumns?.mobile ?? numberOfDesktopColumns;
@@ -81,8 +56,8 @@ const MasonryWave = ( props ) => {
 
 	return (
 		<div className=" masonry-wave__container" style={ styles }>
-			{ columns.map( ( col ) => (
-				<MasonryWaveColumn images={ col } />
+			{ columns.map( ( col, index ) => (
+				<MasonryWaveColumn key={ `masonry-wave__col-${ index }` } images={ col } />
 			) ) }
 		</div>
 	);
