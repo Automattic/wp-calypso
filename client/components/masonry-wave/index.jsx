c9a673a1f457f2f176e377f35a3812b18aa392d8
Louis Laugesen <louis.laugesen@gmail.com>
2021-11-16 17:19:22 +1100

    Update client/components/masonry-wave/index.jsx

--- client/components/masonry-wave/index.jsx
+++ client/components/masonry-wave/index.jsx
@@ -51,7 +51,7 @@ const MasonryWaveElement = ( props ) => {
 	const { image } = props;
 
 	return (
-			<img src={ image?.src } alt={ image?.alt ?? 'alt text' } className="masonry-wave__element" />
+		<img src={ image?.src } alt={ image?.alt ?? 'alt text' } className="masonry-wave__element" />
 	);
 };
 
