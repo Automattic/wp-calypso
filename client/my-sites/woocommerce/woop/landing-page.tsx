846b1798ed6d03787d70292c672ab3a0f32c02cd
Allison Levine <allison.levine@automattic.com>
2021-11-16 10:37:07 -0500

    Clean up merge conflicts

--- client/my-sites/woocommerce/woop/landing-page.tsx
+++ client/my-sites/woocommerce/woop/landing-page.tsx
@@ -1,19 +1,20 @@
-/**
- * External dependencies
- */
 import { Button } from '@automattic/components';
 import { useRef } from '@wordpress/element';
 import { translate } from 'i18n-calypso';
-/**
- * Internal dependencies
- */
+import './style.scss';
+import Image01 from 'calypso/assets/images/woocommerce/image01.jpeg';
+import Image02 from 'calypso/assets/images/woocommerce/image02.jpeg';
+import Image04 from 'calypso/assets/images/woocommerce/image04.jpeg';
+import Image05 from 'calypso/assets/images/woocommerce/image05.jpeg';
 import CtaSection from 'calypso/components/cta-section';
 import FixedNavigationHeader from 'calypso/components/fixed-navigation-header';
-import './style.scss';
+import MasonryWave from 'calypso/components/masonry-wave';
 
 interface Props {
 	startSetup: () => void;
 }
+
+const images = [ { src: Image01 }, { src: Image05 }, { src: Image02 }, { src: Image04 } ];
 
 const WoopLandingPage: React.FunctionComponent< Props > = ( props ) => {
 	const { startSetup } = props;
@@ -34,7 +35,7 @@ const WoopLandingPage: React.FunctionComponent< Props > = ( props ) => {
 				buttonAction={ startSetup }
 				ctaRef={ ctaRef }
 			>
-				Right content
+				<MasonryWave images={ images } />
 			</CtaSection>
 		</div>
 	);
