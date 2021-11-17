4b62539c70322be7c72d00fb193e8be25704aa23
Philip Jackson <p-jackson@live.com>
2021-11-18 12:10:48 +1300

    Move all design category state into a single useCategorization hook (#58151)

--- packages/design-picker/src/hooks/use-categorization.ts
+++ packages/design-picker/src/hooks/use-categorization.ts
@@ -1,18 +1,80 @@
 import { useI18n } from '@wordpress/react-i18n';
+import { useEffect, useMemo, useState } from 'react';
 import { SHOW_ALL_SLUG } from '../constants';
 import { Category, Design } from '../types';
 import { gatherCategories } from '../utils';
 
-export function useCategorization( designs: Design[], showAllFilter: boolean ): Category[] {
+export interface Categorization {
+	selection: string | null;
+	onSelect: ( selectedSlug: string | null ) => void;
+	categories: Category[];
+}
+
+interface UseCategorizationOptions {
+	defaultSelection: string | null;
+	showAllFilter: boolean;
+	sort?: ( a: Category, b: Category ) => number;
+}
+
+export function useCategorization(
+	designs: Design[],
+	{ defaultSelection, showAllFilter, sort }: UseCategorizationOptions
+): Categorization {
 	const { __ } = useI18n();
 
-	const categories = gatherCategories( designs );
-	if ( showAllFilter && designs.length ) {
-		categories.unshift( {
-			name: __( 'Show All', __i18n_text_domain__ ),
-			slug: SHOW_ALL_SLUG,
-		} );
+	const categories = useMemo( () => {
+		const result = gatherCategories( designs );
+		if ( sort ) {
+			result.sort( sort );
+		}
+
+		if ( showAllFilter && designs.length ) {
+			result.unshift( {
+				name: __( 'Show All', __i18n_text_domain__ ),
+				slug: SHOW_ALL_SLUG,
+			} );
+		}
+
+		return result;
+	}, [ designs, showAllFilter, sort, __ ] );
+
+	const [ selection, onSelect ] = useState< string | null >(
+		chooseDefaultSelection( categories, defaultSelection )
+	);
+
+	useEffect( () => {
+		// When the category list changes check that the current selection
+		// still matches one of the given slugs, and if it doesn't reset
+		// the current selection.
+		const findResult = categories.find( ( { slug } ) => slug === selection );
+		if ( ! findResult ) {
+			onSelect( chooseDefaultSelection( categories, defaultSelection ) );
+		}
+	}, [ categories, defaultSelection, selection ] );
+
+	return {
+		categories,
+		selection,
+		onSelect,
+	};
+}
+
+/**
+ * Chooses which category is the one that should be used by default.
+ * If `defaultSelection` is a valid category slug then it'll be used, otherwise it'll be whichever
+ * category appears first in the list.
+ *
+ * @param categories the categories from which the default will be selected
+ * @param defaultSelection use this category as the default selection if possible
+ * @returns the default category or null if none is available
+ */
+function chooseDefaultSelection(
+	categories: Category[],
+	defaultSelection: string | null
+): string | null {
+	if ( defaultSelection && categories.find( ( { slug } ) => slug === defaultSelection ) ) {
+		return defaultSelection;
 	}
 
-	return categories;
+	return categories[ 0 ]?.slug ?? null;
 }
