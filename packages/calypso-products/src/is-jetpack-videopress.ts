31696860b5bbda73be654ad651330b467a6216cb
Payton Swick <payton@foolord.com>
2021-11-16 19:28:11 -0500

    Remove formatProduct from isJetpackVideoPress

---
+++ packages/calypso-products/src/is-jetpack-videopress.ts
@@ -0,0 +1,12 @@
+import { JETPACK_VIDEOPRESS_PRODUCTS } from './constants';
+
+type WithSnakeCaseSlug = { product_slug: string };
+type WithCamelCaseSlug = { productSlug: string };
+
+export function isJetpackVideoPress( product: WithSnakeCaseSlug | WithCamelCaseSlug ): boolean {
+	const products = JETPACK_VIDEOPRESS_PRODUCTS as ReadonlyArray< string >;
+	if ( 'product_slug' in product ) {
+		return products.includes( product.product_slug );
+	}
+	return products.includes( product.productSlug );
+}
