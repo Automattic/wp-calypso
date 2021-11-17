245ec26880bac8f578844fd626bc215b47e0ee20
Payton Swick <payton@foolord.com>
2021-11-17 15:08:32 -0500

    Reuse types in isPlan

--- packages/calypso-products/src/is-plan.ts
+++ packages/calypso-products/src/is-plan.ts
@@ -1,9 +1,8 @@
 import { PLAN_HOST_BUNDLE, PLAN_WPCOM_ENTERPRISE } from './constants';
 import { getPlansSlugs, isFreePlan } from './main';
+import type { WithSnakeCaseSlug, WithCamelCaseSlug } from './types';
 
-type HasSnakeCaseProductSlug = { product_slug: string };
-type HasCamelCaseProductSlug = { productSlug: string };
-export function isPlan( product: HasSnakeCaseProductSlug | HasCamelCaseProductSlug ): boolean {
+export function isPlan( product: WithSnakeCaseSlug | WithCamelCaseSlug ): boolean {
 	const slug = 'product_slug' in product ? product.product_slug : product.productSlug;
 	if ( isFreePlan( slug ) ) {
 		return false;
