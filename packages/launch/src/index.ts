0320a38dd2ac2cd7c34b0cc3766d3518f5553bd5
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-11-17 16:33:56 +0100

    Expose Confetti component from the @automattic/launch package

--- packages/launch/src/index.ts
+++ packages/launch/src/index.ts
@@ -1,3 +1,4 @@
 export { default } from './launch';
 export * from './hooks';
 export { default as LaunchContext } from './context';
+export { default as Confetti } from './focused-launch/success/confetti';
