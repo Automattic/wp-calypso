12d047847f763289fcac0626bb7ca10cd42b5d41
Niranjan Uma Shankar <niranjanumashankar@Niranjans-MacBook-Pro.local>
2021-11-18 20:14:43 +0400

    Take screenshots of the mag16 across all the specified pages

--- test/e2e/specs/specs-playwright/wp-signup__screenshot.ts
+++ test/e2e/specs/specs-playwright/wp-signup__screenshot.ts
@@ -3,6 +3,7 @@
  */
 import fs from 'fs';
 import {
+	ChangeUILanguageFlow,
 	DataHelper,
 	DomainSearchComponent,
 	LoginPage,
@@ -11,13 +12,13 @@ import {
 	UserSignupPage,
 	SignupPickPlanPage,
 	CloseAccountFlow,
-	NavbarComponent,
 	BrowserManager,
 } from '@automattic/calypso-e2e';
 import archiver from 'archiver';
 import FormData from 'form-data';
 import fetch from 'node-fetch';
 import { Page } from 'playwright';
+import type { LanguageSlug } from '@automattic/languages';
 
 describe( DataHelper.createSuiteTitle( 'Signup: WordPress.com Free' ), function () {
 	const inboxId = DataHelper.config.get( 'inviteInboxId' ) as string;
@@ -30,18 +31,60 @@ describe( DataHelper.createSuiteTitle( 'Signup: WordPress.com Free' ), function 
 	const blogName = DataHelper.getBlogName();
 
 	let page: Page;
+	let selectedDomain: string;
 
 	setupHooks( ( args ) => {
 		page = args.page;
 	} );
 
 	describe( 'Signup', function () {
+		jest.setTimeout( 1000000 );
 		let cartCheckoutPage: CartCheckoutPage;
+		const magnificientNonEnLocales = [
+			'pt-br',
+			'fr',
+			'es',
+			'de',
+			'he',
+			'ja',
+			'it',
+			'nl',
+			'ru',
+			'tr',
+			'id',
+			'zh-cn',
+			'zh-tw',
+			'ko',
+			'ar',
+			'sv',
+		];
 
-		it( 'Screenshot Login page and navigate to Signup page', async function () {
+		it( 'Screenshot login page in desktop viewport, en and Mag-16 locales', async function () {
 			const loginPage = new LoginPage( page );
-			await loginPage.visit();
-			await page.screenshot( { path: 'login.png', fullPage: true } );
+			for ( const locale of [ 'en', ...magnificientNonEnLocales ] ) {
+				await loginPage.visitLocale( locale );
+				await page.screenshot( {
+					path: `tos_login_desktop_${ locale }.png`,
+					fullPage: true,
+					type: 'jpeg',
+					quality: 20,
+				} );
+				page.setViewportSize( { width: 410, height: 820 } );
+				await page.screenshot( {
+					path: `tos_login_mobile_${ locale }.png`,
+					fullPage: true,
+					type: 'jpeg',
+					quality: 20,
+				} );
+				page.setViewportSize( { width: 1024, height: 1366 } );
+				await page.screenshot( {
+					path: `tos_login_tablet_${ locale }.png`,
+					fullPage: true,
+					type: 'jpeg',
+					quality: 20,
+				} );
+				page.setViewportSize( { width: 1280, height: 720 } );
+			}
 			await loginPage.signup();
 		} );
 
@@ -49,16 +92,40 @@ describe( DataHelper.createSuiteTitle( 'Signup: WordPress.com Free' ), function 
 			await BrowserManager.setStoreCookie( page, { currency: 'GBP' } );
 		} );
 
-		it( 'Screenshot signup page and sign up as new user', async function () {
+		it( 'Screenshot signup page in desktop viewport, en and Mag-16 locales', async function () {
 			const userSignupPage = new UserSignupPage( page );
-			await page.screenshot( { path: 'signup.png', fullPage: true } );
+			await page.screenshot( { path: 'tos_signup_desktop_en.png', fullPage: true } );
+			for ( const locale of [ ...magnificientNonEnLocales, 'en' ] ) {
+				await userSignupPage.visitLocale( locale );
+				await page.screenshot( {
+					path: `tos_signup_desktop_${ locale }.png`,
+					fullPage: true,
+					type: 'jpeg',
+					quality: 20,
+				} );
+				page.setViewportSize( { width: 410, height: 820 } );
+				await page.screenshot( {
+					path: `tos_signup_mobile_${ locale }.png`,
+					fullPage: true,
+					type: 'jpeg',
+					quality: 20,
+				} );
+				page.setViewportSize( { width: 1024, height: 1366 } );
+				await page.screenshot( {
+					path: `tos_signup_tablet_${ locale }.png`,
+					fullPage: true,
+					type: 'jpeg',
+					quality: 20,
+				} );
+				page.setViewportSize( { width: 1280, height: 720 } );
+			}
 			await userSignupPage.signup( email, username, signupPassword );
 		} );
 
 		it( 'Select a free .wordpress.com domain', async function () {
 			const domainSearchComponent = new DomainSearchComponent( page );
 			await domainSearchComponent.search( blogName );
-			await domainSearchComponent.selectDomain( '.wordpress.com' );
+			selectedDomain = await domainSearchComponent.selectDomain( '.wordpress.com' );
 		} );
 
 		it( 'Select WordPress.com Personal plan', async function () {
@@ -74,18 +141,74 @@ describe( DataHelper.createSuiteTitle( 'Signup: WordPress.com Free' ), function 
 		it( 'Enter billing and payment details', async function () {
 			const paymentDetails = DataHelper.getTestPaymentDetails();
 			await cartCheckoutPage.enterBillingDetails( paymentDetails );
-			await cartCheckoutPage.enterPaymentDetails( paymentDetails );
+			await cartCheckoutPage.enterCardholderName( paymentDetails );
 		} );
 
-		it( 'Take screenshot of checkout page', async function () {
+		it( 'Screenshot checkout page in desktop, en locale', async function () {
+			await page.screenshot( {
+				path: 'tos_checkout_desktop_en.png',
+				fullPage: true,
+				type: 'jpeg',
+				quality: 20,
+			} );
+			page.setViewportSize( { width: 410, height: 820 } );
+			await page.screenshot( {
+				path: 'tos_checkout_mobile_en.png',
+				fullPage: true,
+				type: 'jpeg',
+				quality: 20,
+			} );
+			page.setViewportSize( { width: 1024, height: 1366 } );
+			await page.screenshot( {
+				path: 'tos_checkout_tablet_en.png',
+				fullPage: true,
+				type: 'jpeg',
+				quality: 20,
+			} );
+			page.setViewportSize( { width: 1280, height: 720 } );
+		} );
+
+		it( 'Close checkout and change UI language', async function () {
+			const changeUILanguageFlow = new ChangeUILanguageFlow( page );
+			for ( const locale of magnificientNonEnLocales ) {
+				await cartCheckoutPage.clickCloseCheckout();
+				console.log( 'change UI lang to ' + locale );
+				await changeUILanguageFlow.changeUILanguage( locale as LanguageSlug );
+				cartCheckoutPage.visit( selectedDomain );
+				const paymentDetails = DataHelper.getTestPaymentDetails();
+				await cartCheckoutPage.enterBillingDetails( paymentDetails );
+				await cartCheckoutPage.enterCardholderName( paymentDetails );
+				await page.screenshot( {
+					path: `tos_checkout_desktop_${ locale }.png`,
+					fullPage: true,
+					type: 'jpeg',
+					quality: 20,
+				} );
+				page.setViewportSize( { width: 410, height: 820 } );
+				await page.screenshot( {
+					path: `tos_checkout_mobile_${ locale }.png`,
+					fullPage: true,
+					type: 'jpeg',
+					quality: 20,
+				} );
+				page.setViewportSize( { width: 1024, height: 1366 } );
+				await page.screenshot( {
+					path: `tos_checkout_tablet_${ locale }.png`,
+					fullPage: true,
+					type: 'jpeg',
+					quality: 20,
+				} );
+				page.setViewportSize( { width: 1280, height: 720 } );
+			}
+		} );
+
+		it( 'Zip screenshots and upload', async function () {
 			const archive = archiver( 'zip', {
 				zlib: { level: 9 }, // Sets the compression level.
 			} );
 			const output = fs.createWriteStream( 'test-screenshots-1.zip' );
 			archive.pipe( output );
-			archive.append( fs.createReadStream( 'login.png' ), { name: 'login.png' } );
-			archive.append( fs.createReadStream( 'signup.png' ), { name: 'signup.png' } );
-			archive.append( fs.createReadStream( 'checkout.png' ), { name: 'checkout.png' } );
+			archive.glob( 'tos_*' );
 			archive.finalize();
 
 			output.on( 'close', function () {
@@ -98,9 +221,6 @@ describe( DataHelper.createSuiteTitle( 'Signup: WordPress.com Free' ), function 
 					.then( ( response ) => response.json() )
 					.then( ( response ) => expect( response?.upload_status ).toStrictEqual( 'success' ) );
 			} );
-
-			const navbarComponent = new NavbarComponent( page );
-			await navbarComponent.clickCloseCheckout();
 		} );
 	} );
 
