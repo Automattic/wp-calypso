f27fd7ab645871ce1aa56789d5fc33f838aa2204
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-11-18 19:39:09 -0300

    Create new domain transfer page

---
+++ client/my-sites/domains/domain-management/transfer/transfer-page/index.jsx
@@ -0,0 +1,36 @@
+import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Main from 'calypso/components/main';
+import { getSelectedDomain, isMappedDomain } from 'calypso/lib/domains';
+import getCurrentRoute from 'calypso/state/selectors/get-current-route';
+import getPrimaryDomainBySiteId from 'calypso/state/selectors/get-primary-domain-by-site-id';
+import isDomainOnlySite from 'calypso/state/selectors/is-domain-only-site';
+import isPrimaryDomainBySiteId from 'calypso/state/selectors/is-primary-domain-by-site-id';
+import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
+import { hasLoadedSiteDomains } from 'calypso/state/sites/domains/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+
+const TransferPage = () => {
+	return <Main>The page goes here</Main>;
+};
+
+const transferPageComponent = connect( ( state, ownProps ) => {
+	const domain = getSelectedDomain( ownProps );
+	const siteId = getSelectedSiteId( state );
+	return {
+		currentRoute: getCurrentRoute( state ),
+		hasSiteDomainsLoaded: hasLoadedSiteDomains( state, siteId ),
+		isAtomic: isSiteAutomatedTransfer( state, siteId ),
+		isDomainOnly: isDomainOnlySite( state, siteId ),
+		isMapping: Boolean( domain ) && isMappedDomain( domain ),
+		isPrimaryDomain: isPrimaryDomainBySiteId( state, siteId, ownProps.selectedDomainName ),
+		primaryDomain: getPrimaryDomainBySiteId( state, siteId ),
+	};
+} )( localize( TransferPage ) );
+
+transferPageComponent.propTypes = {
+	selectedDomainName: PropTypes.string.isRequired,
+};
+
+export default transferPageComponent;
