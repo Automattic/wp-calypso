260b609addffc1b1ecad657155be50a311c315d7
retrofox <rdsuarez@gmail.com>
2021-11-18 11:14:49 -0300

    woop: propagate step section properly

--- client/signup/steps/woocommerce-install/index.tsx
+++ client/signup/steps/woocommerce-install/index.tsx
@@ -1,16 +1,15 @@
 import { useI18n } from '@wordpress/react-i18n';
+import { ReactElement } from 'react';
 import StepWrapper from 'calypso/signup/step-wrapper';
 import Confirm from './confirm';
 import Install from './install';
 import Transfer from './transfer';
 import type { GoToStep } from '../../types';
-import type { ReactElement } from 'react';
 
 import './style.scss';
 
 interface WooCommerceInstallProps {
 	goToStep: GoToStep;
-	submitSignupStep: ( step: string, data: unknown ) => void;
 	stepName: string;
 	stepSectionName: string;
 	isReskinned: boolean;
@@ -22,14 +21,14 @@ interface WooCommerceInstallProps {
 export default function WooCommerceInstall( props: WooCommerceInstallProps ): ReactElement | null {
 	const { __ } = useI18n();
 
-	const { stepName, isReskinned, goToStep, submitSignupStep } = props;
+	const { stepName, isReskinned, goToStep, stepSectionName } = props;
 
 	if ( stepName === 'confirm' ) {
 		return (
 			<Confirm
 				goToStep={ goToStep }
 				isReskinned={ isReskinned }
-				submitSignupStep={ submitSignupStep }
+				stepSectionName={ stepSectionName }
 			/>
 		);
 	}
