1dc3d2db738a654a35cefef96e72a7af42d03f46
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-11-19 17:55:00 -0300

    Make background white and layout wide

--- client/my-sites/domains/domain-management/settings/index.jsx
+++ client/my-sites/domains/domain-management/settings/index.jsx
@@ -1,11 +1,17 @@
 import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
 import Main from 'calypso/components/main';
+import BodySectionCssClass from 'calypso/layout/body-section-css-class';
 import { getCurrentRoute } from 'calypso/state/selectors/get-current-route';
 import isDomainOnlySite from 'calypso/state/selectors/is-domain-only-site';
 
 const Settings = () => {
-	return <Main>Page goes here.</Main>;
+	return (
+		<Main wideLayout>
+			<BodySectionCssClass bodyClass={ [ 'edit__body-white' ] } />
+			Page goes here.
+		</Main>
+	);
 };
 
 export default connect( ( state, ownProps ) => {
