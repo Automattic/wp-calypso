474cf53d4bd0d84f59e03881207adac1d089a5fd
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-11-19 17:50:09 -0300

    Rename folder `edit-contact-info-updated` to `edit-contact-info-page` and update layout

--- client/my-sites/domains/domain-management/edit-contact-info-page/index.jsx
+++ client/my-sites/domains/domain-management/edit-contact-info-page/index.jsx
@@ -2,11 +2,17 @@ import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Main from 'calypso/components/main';
+import BodySectionCssClass from 'calypso/layout/body-section-css-class';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import isRequestingWhois from 'calypso/state/selectors/is-requesting-whois';
 
 const EditContactInfoPage = () => {
-	return <Main className="edit-contact-info-updated">Page goes here.</Main>;
+	return (
+		<Main wideLayout>
+			<BodySectionCssClass bodyClass={ [ 'edit__body-white' ] } />
+			Page goes here.
+		</Main>
+	);
 };
 
 EditContactInfoPage.propTypes = {
--- client/my-sites/domains/domain-management/index.jsx
+++ client/my-sites/domains/domain-management/index.jsx
@@ -9,7 +9,7 @@ import DnsRecordsList from './dns/dns-records-list';
 import DomainConnectMapping from './domain-connect-mapping';
 import Edit from './edit';
 import EditContactInfo from './edit-contact-info';
-import EditContactInfoPage from './edit-contact-info-updated';
+import EditContactInfoPage from './edit-contact-info-page';
 import SiteRedirect from './edit/site-redirect';
 import TransferIn from './edit/transfer-in';
 import List from './list';
