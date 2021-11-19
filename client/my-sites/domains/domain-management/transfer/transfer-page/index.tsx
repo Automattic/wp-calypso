e4c344f42bde759f646c64619d0ea05c90f89745
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-11-19 17:10:49 -0300

    Add breadcrumbs to new transfer page

--- client/my-sites/domains/domain-management/transfer/transfer-page/index.tsx
+++ client/my-sites/domains/domain-management/transfer/transfer-page/index.tsx
@@ -2,6 +2,12 @@ import { connect } from 'react-redux';
 import Main from 'calypso/components/main';
 import BodySectionCssClass from 'calypso/layout/body-section-css-class';
 import { getSelectedDomain, isMappedDomain } from 'calypso/lib/domains';
+import Breadcrumbs from 'calypso/my-sites/domains/domain-management/components/breadcrumbs';
+import {
+	domainManagementEdit,
+	domainManagementList,
+	domainManagementDns,
+} from 'calypso/my-sites/domains/paths';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import getPrimaryDomainBySiteId from 'calypso/state/selectors/get-primary-domain-by-site-id';
 import isDomainOnlySite from 'calypso/state/selectors/is-domain-only-site';
@@ -11,9 +17,37 @@ import { hasLoadedSiteDomains } from 'calypso/state/sites/domains/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 const TransferPage = (): JSX.Element => {
+	const renderBreadcrumbs = () => {
+		const items = [
+			{
+				label: translate( 'Domains', { comment: 'Internet domains, e.g. mygroovydomain.com' } ),
+				href: domainManagementList( selectedSite.slug, selectedDomainName ),
+			},
+			{
+				label: selectedDomainName,
+				href: domainManagementEdit( selectedSite.slug, selectedDomainName, currentRoute ),
+			},
+			{
+				label: translate( 'Transfer', { comment: 'Verb - Transfer a domain somewhere else' } ),
+			},
+		];
+
+		const mobileItem = {
+			label: translate( 'Back to %(domain)s', {
+				args: { domain: selectedDomainName },
+				comment: 'Link to return to the settings management page of a domain ',
+			} ),
+			href: domainManagementDns( selectedSite.slug, selectedDomainName ),
+			showBackArrow: true,
+		};
+
+		return <Breadcrumbs items={ items } mobileItem={ mobileItem } />;
+	};
+
 	return (
 		<Main className="transfer-page" wideLayout>
 			<BodySectionCssClass bodyClass={ [ 'edit__body-white' ] } />
+			{ renderBreadcrumbs() }
 			The page goes here
 		</Main>
 	);
