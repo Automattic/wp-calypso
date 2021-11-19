5e455322b136fa8018f5abbc05b4d344feafac31
Daniel <daniel.lopez@automattic.com>
2021-11-19 15:26:27 +0100

    Emails: Improve forward form for email comparison (#57492)

---
+++ client/my-sites/email/email-forwarding/email-forwarding-add-new-compact-list.jsx
@@ -0,0 +1,171 @@
+import { Button } from '@automattic/components';
+import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
+import { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { validateAllFields } from 'calypso/lib/domains/email-forwarding';
+import EmailForwardingAddNewCompact from 'calypso/my-sites/email/email-forwarding/email-forwarding-add-new-compact';
+import { composeAnalytics, withAnalytics } from 'calypso/state/analytics/actions';
+import { addEmailForward } from 'calypso/state/email-forwarding/actions';
+import getEmailForwardingLimit from 'calypso/state/selectors/get-email-forwarding-limit';
+import {
+	addEmailForwardSuccess,
+	getEmailForwards,
+	isAddingEmailForward,
+} from 'calypso/state/selectors/get-email-forwards';
+import { isRequestingSiteDomains } from 'calypso/state/sites/domains/selectors';
+import { getSiteSlug } from 'calypso/state/sites/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+
+class EmailForwardingAddNewCompactList extends Component {
+	static propTypes = {
+		emailForwards: PropTypes.array,
+		emailForwardingLimit: PropTypes.number,
+		onConfirmEmailForwarding: PropTypes.func.isRequired,
+		onAddEmailForwardSuccess: PropTypes.func,
+		selectedDomainName: PropTypes.string.isRequired,
+	};
+
+	constructor( props ) {
+		super( props );
+		this.state = {
+			emailForwards: [ { destination: '', mailbox: '', isValid: false } ],
+			isRedirecting: false,
+			newEmailForwardAdded: false,
+		};
+	}
+
+	hasValidEmailForwards() {
+		return ! this.state.emailForwards?.some( ( forward ) => ! forward.isValid );
+	}
+
+	addNewEmailForwardWithAnalytics = ( domainName, mailbox, destination ) => {
+		withAnalytics(
+			composeAnalytics(
+				this.props.onConfirmEmailForwarding(),
+				this.props.addEmailForward( domainName, mailbox, destination )
+			)
+		);
+	};
+
+	addNewEmailForwardsClick = ( event ) => {
+		const { isSubmittingEmailForward, selectedSiteSlug, selectedDomainName } = this.props;
+
+		event.preventDefault();
+
+		if ( isSubmittingEmailForward ) {
+			return;
+		}
+
+		this.state.emailForwards?.map( ( forward ) => {
+			const { mailbox, destination } = forward;
+
+			this.addNewEmailForwardWithAnalytics(
+				selectedDomainName,
+				mailbox,
+				destination,
+				selectedSiteSlug
+			);
+		} );
+		this.setState( { newEmailForwardAdded: true } );
+	};
+
+	onAddNewEmailForward = () => {
+		this.setState( {
+			emailForwards: [ ...this.state.emailForwards, { destination: '', mailbox: '' } ],
+		} );
+	};
+
+	renderActionsButtons() {
+		const { translate } = this.props;
+		return (
+			<div className="email-forwarding-add-new-compact-list__actions">
+				<Button
+					primary
+					onClick={ this.addNewEmailForwardsClick }
+					disabled={ ! this.hasValidEmailForwards() }
+				>
+					{ translate( 'Add' ) }
+				</Button>
+			</div>
+		);
+	}
+
+	onRemoveEmailForward = ( index ) => {
+		const [ emailForwards ] = this.state;
+		emailForwards.splice( index, 1 );
+		this.setState( { emailForwards } );
+	};
+
+	onUpdateEmailForward = ( index, name, value ) => {
+		// eslint-disable-next-line prefer-const
+		let emailForwards = this.state.emailForwards;
+		emailForwards[ index ][ name ] = value;
+
+		const validEmailForward = validateAllFields( emailForwards[ index ] );
+		emailForwards[ index ].isValid =
+			validEmailForward.mailbox.length === 0 && validEmailForward.destination.length === 0;
+
+		this.setState( { emailForwards } );
+	};
+
+	componentDidUpdate( prevProps ) {
+		const { emailForwardSuccess, onAddEmailForwardSuccess } = this.props;
+
+		const { newEmailForwardAdded } = this.state;
+
+		if (
+			emailForwardSuccess &&
+			onAddEmailForwardSuccess &&
+			newEmailForwardAdded &&
+			prevProps.emailForwardSuccess !== emailForwardSuccess
+		) {
+			onAddEmailForwardSuccess();
+		}
+	}
+
+	render() {
+		const { selectedDomainName } = this.props;
+
+		return (
+			<>
+				{ this.state.emailForwards?.map( ( fields, index ) => (
+					<Fragment key={ `email-forwarding__add-new_fragment__card-${ index }` }>
+						<form className="email-forwarding__add-new">
+							<EmailForwardingAddNewCompact
+								emailForwards={ this.state.emailForwards }
+								fields={ fields }
+								index={ index }
+								onAddEmailForward={ this.onAddNewEmailForward }
+								onRemoveEmailForward={ this.onRemoveEmailForward }
+								onUpdateEmailForward={ this.onUpdateEmailForward }
+								selectedDomainName={ selectedDomainName }
+							/>
+						</form>
+						<hr
+							className="email-forwarding__add-new-separator"
+							key={ `email-forwarding__add-new_hr-${ index }` }
+						/>
+					</Fragment>
+				) ) }
+				<div>{ this.renderActionsButtons() }</div>
+			</>
+		);
+	}
+}
+
+export default connect(
+	( state, ownProps ) => {
+		const siteId = getSelectedSiteId( state );
+		return {
+			emailForwardSuccess: addEmailForwardSuccess( state, ownProps.selectedDomainName ),
+			selectedSiteSlug: getSiteSlug( state, getSelectedSiteId( state ) ),
+			isSubmittingEmailForward: isAddingEmailForward( state, ownProps.selectedDomainName ),
+			isRequestingDomains: isRequestingSiteDomains( state, siteId ),
+			emailForwards: getEmailForwards( state, ownProps.selectedDomainName ),
+			emailForwardingLimit: getEmailForwardingLimit( state, siteId ),
+			siteId,
+		};
+	},
+	{ addEmailForward }
+)( localize( EmailForwardingAddNewCompactList ) );
---
+++ client/my-sites/email/email-forwarding/email-forwarding-add-new-compact.jsx
@@ -0,0 +1,189 @@
+import { Button, Gridicon } from '@automattic/components';
+import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import FormButton from 'calypso/components/forms/form-button';
+import FormFieldset from 'calypso/components/forms/form-fieldset';
+import FormInputValidation from 'calypso/components/forms/form-input-validation';
+import FormLabel from 'calypso/components/forms/form-label';
+import FormTextInput from 'calypso/components/forms/form-text-input';
+import FormTextInputWithAffixes from 'calypso/components/forms/form-text-input-with-affixes';
+import { validateAllFields } from 'calypso/lib/domains/email-forwarding';
+import formState from 'calypso/lib/form-state';
+
+class EmailForwardingAddNewCompact extends Component {
+	static propTypes = {
+		fields: PropTypes.object,
+		index: PropTypes.number,
+		onAddEmailForward: PropTypes.func.isRequired,
+		onRemoveEmailForward: PropTypes.func.isRequired,
+		selectedDomainName: PropTypes.string.isRequired,
+		onUpdateEmailForward: PropTypes.func.isRequired,
+		emailForwards: PropTypes.array,
+	};
+
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			formSubmitting: false,
+			fields: this.props.fields,
+		};
+
+		this.formStateController = formState.Controller( {
+			initialFields: this.getInitialFields(),
+			onNewState: this.setFormState,
+			validatorFunction: ( fieldValues, onComplete ) => {
+				onComplete( null, validateAllFields( fieldValues, this.props.emailForwards ?? [] ) );
+			},
+		} );
+	}
+
+	getInitialFields() {
+		return this.props.fields;
+	}
+
+	setFormState = ( fields ) => {
+		this.setState( { fields } );
+	};
+
+	renderAddButton() {
+		const { onAddEmailForward, onButtonClick, translate } = this.props;
+		return (
+			<div className="email-forwarding-add-new-compact__actions">
+				<Button
+					className="email-forwarding-add-new-compact__add-another-forward-button"
+					onClick={ onAddEmailForward }
+				>
+					<Gridicon icon="plus" />
+					<span>{ translate( 'Add another forward' ) }</span>
+				</Button>
+
+				<Button primary onClick={ onButtonClick }>
+					{ translate( 'Add' ) }
+				</Button>
+			</div>
+		);
+	}
+
+	renderRemoveButton() {
+		const updateForm = () => {
+			this.props.onRemoveEmailForward( this.props.index );
+		};
+
+		return (
+			<FormButton type="button" isPrimary={ false } onClick={ () => updateForm() }>
+				<Gridicon icon="trash" />
+				{ this.props.translate( 'Remove this forward' ) }
+			</FormButton>
+		);
+	}
+
+	renderFormFields() {
+		const { translate, selectedDomainName, index, fields } = this.props;
+		const contactText = translate( 'contact', {
+			context: 'part of e-mail address',
+			comment: 'As it would be part of an e-mail address contact@example.com',
+		} );
+		const exampleEmailText = translate( 'e.g. %(example)s', {
+			args: { example: contactText },
+		} );
+		const isValidMailbox = this.isValid( 'mailbox' );
+		const isValidDestination = this.isValid( 'destination' );
+		const { mailbox, destination } = fields;
+		const mailboxError = this.getError( 'mailbox' );
+		const destinationError = this.getError( 'destination' );
+
+		return (
+			<div className="email-forwarding__form-content">
+				<FormFieldset>
+					<FormLabel>{ translate( 'Emails Sent To' ) }</FormLabel>
+					<FormTextInputWithAffixes
+						disabled={ this.state.formSubmitting }
+						name="mailbox"
+						onChange={ ( event ) => this.onChange( event, index ) }
+						isError={ ! isValidMailbox }
+						placeholder={ exampleEmailText }
+						suffix={ '@' + selectedDomainName }
+						value={ mailbox }
+					/>
+					{ ! isValidMailbox && <FormInputValidation text={ mailboxError } isError={ true } /> }
+				</FormFieldset>
+
+				<FormFieldset>
+					<FormLabel>{ translate( 'Will Be Forwarded To' ) }</FormLabel>
+					<FormTextInput
+						disabled={ this.state.formSubmitting }
+						name="destination"
+						onChange={ ( event ) => this.onChange( event, index ) }
+						isError={ ! isValidDestination }
+						placeholder={ translate( 'Your Existing Email Address' ) }
+						value={ destination }
+					/>
+					{ ! isValidDestination && (
+						<FormInputValidation text={ destinationError } isError={ true } />
+					) }
+				</FormFieldset>
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<>
+				{ this.renderFormFields() }
+				{ this.props.index > 0 ? this.renderRemoveButton() : null }
+			</>
+		);
+	}
+
+	onChange = ( event, index ) => {
+		const { name } = event.target;
+		let { value } = event.target;
+
+		value = value.replace( /\s/g, '' );
+		if ( name === 'mailbox' ) {
+			// Removes the domain part
+			value = value.replace( /@.*$/, '' );
+		}
+
+		this.formStateController.handleFieldChange( {
+			name,
+			value,
+		} );
+
+		this.props.onUpdateEmailForward( index, name, value );
+	};
+
+	isValid( fieldName ) {
+		return ! formState.isFieldInvalid( this.state.fields, fieldName );
+	}
+
+	getError( fieldName ) {
+		const { translate } = this.props;
+		const errorMessage = formState.getFieldErrorMessages( this.state.fields, fieldName );
+		if ( ! errorMessage ) {
+			return null;
+		}
+
+		if ( fieldName === 'mailbox' ) {
+			if ( errorMessage.filter( ( t ) => t === 'Invalid' ).length === 1 ) {
+				return translate( 'Invalid mailbox - only characters [a-z0-9._+-] are allowed' );
+			}
+
+			if ( errorMessage.filter( ( t ) => t === 'Duplicated' ).length === 1 ) {
+				return translate( 'Invalid mailbox - Duplicated' );
+			}
+		}
+
+		if ( fieldName === 'destination' ) {
+			if ( errorMessage.filter( ( t ) => t === 'Invalid' ).length === 1 ) {
+				return translate( 'Invalid destination address' );
+			}
+		}
+
+		return null;
+	}
+}
+
+export default localize( EmailForwardingAddNewCompact );
