34d0867aab3c32713f5d0aa13dfd3a9ce55c4450
Payton Swick <payton@foolord.com>
2021-11-19 11:43:21 -0500

    Fix Domain type import in useSiteDomains

--- client/my-sites/checkout/composite-checkout/hooks/use-site-domains.ts
+++ client/my-sites/checkout/composite-checkout/hooks/use-site-domains.ts
@@ -3,27 +3,32 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchSiteDomains } from 'calypso/state/sites/domains/actions';
 import { getDomainsBySiteId, hasLoadedSiteDomains } from 'calypso/state/sites/domains/selectors';
-import type { Domain } from '@automattic/data-stores';
+import type { SiteDomain } from 'calypso/state/sites/domains/types';
 
 const debug = debugFactory( 'calypso:composite-checkout:use-site-domains' );
 
-export default function useSiteDomains( siteId: number | undefined ): Domain[] {
+export default function useSiteDomains( siteId: number | undefined ): SiteDomain[] {
 	const dispatch = useDispatch();
 
-	const [ siteDomains, setSiteDomains ] = useState( [] );
+	const [ siteDomains, setSiteDomains ] = useState< SiteDomain[] >( [] );
 
-	const areDomainsLoaded = useSelector( ( state ) => hasLoadedSiteDomains( state, siteId ) );
-
-	const domains = useSelector(
-		( state ) => areDomainsLoaded && getDomainsBySiteId( state, siteId )
+	const areDomainsLoaded = useSelector(
+		( state ) => siteId && hasLoadedSiteDomains( state, siteId )
 	);
+
+	const domains: SiteDomain[] = useSelector( ( state ) => {
+		if ( areDomainsLoaded && siteId ) {
+			return getDomainsBySiteId( state, siteId );
+		}
+		return [];
+	} );
 
 	useEffect( () => {
 		if ( areDomainsLoaded ) {
 			debug( 'Setting list of domains', domains );
 
 			setSiteDomains( domains );
-		} else {
+		} else if ( siteId ) {
 			debug( 'Fetching list of domains' );
 
 			dispatch( fetchSiteDomains( siteId ) );
