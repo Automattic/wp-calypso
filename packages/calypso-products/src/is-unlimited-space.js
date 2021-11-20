8ae427af20bb870efebbe6f86ebb26fd91ca08e3
Payton Swick <payton@foolord.com>
2021-11-19 23:11:18 -0500

    Remove formatProduct from isUnlimitedSpace

--- packages/calypso-products/src/is-unlimited-space.js
+++
@@ -1,7 +0,0 @@
-import { formatProduct } from './format-product';
-
-export function isUnlimitedSpace( product ) {
-	product = formatProduct( product );
-
-	return 'unlimited_space' === product.product_slug;
-}
