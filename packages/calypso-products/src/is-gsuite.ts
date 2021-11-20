2163a968f967ee41c50d1b5ddc1327ae3c47265c
Payton Swick <payton@foolord.com>
2021-11-19 23:35:37 -0500

    Allow isGoogleWorkspaceExtraLicence to handle purchases also

--- packages/calypso-products/src/is-gsuite.ts
+++ packages/calypso-products/src/is-gsuite.ts
@@ -13,18 +13,19 @@ export function isGoogleWorkspace( product: WithCamelCaseSlug | WithSnakeCaseSlu
 
 /**
  * Determines whether the provided Google Workspace product is for a user purchasing extra licenses (versus a new account).
- *
- * @see isGoogleWorkspaceExtraLicence() in client/lib/purchases for a function that works on a purchase object
  */
 export function isGoogleWorkspaceExtraLicence(
-	product: ( WithCamelCaseSlug | WithSnakeCaseSlug ) & { extra?: { new_quantity?: number } }
+	product: ( WithCamelCaseSlug | WithSnakeCaseSlug ) & {
+		newQuantity?: number;
+		extra?: { new_quantity?: number };
+	}
 ): boolean {
 	if ( ! isGoogleWorkspaceProductSlug( camelOrSnakeSlug( product ) ) ) {
 		return false;
 	}
 
 	// Checks if the 'new_quantity' property exists as it should only be specified for extra licenses
-	return product?.extra?.new_quantity !== undefined;
+	return product?.extra?.new_quantity !== undefined || product?.newQuantity !== undefined;
 }
 
 export function isGSuite( product: WithCamelCaseSlug | WithSnakeCaseSlug ): boolean {
