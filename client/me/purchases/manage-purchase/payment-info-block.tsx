de6692b06f6d3c80092cad869fe062f46935166a
Payton Swick <payton@foolord.com>
2021-11-20 00:26:41 -0500

    Change backup payment method links to site-level payment methods

--- client/me/purchases/manage-purchase/payment-info-block.tsx
+++ client/me/purchases/manage-purchase/payment-info-block.tsx
@@ -114,7 +114,7 @@ function BackupPaymentMethodNotice() {
 	/* translators: Notice that this purchase may use a backup payment method if it fails to renew */
 	const noticeText = translate( 'or a {{link}}backup payment method{{/link}}', {
 		components: {
-			link: <a href="https://wordpress.com/support/payment/#manage-payment-methods" />,
+			link: <a href="/me/purchases/payment-methods" />,
 		},
 	} );
 	return <div>{ noticeText }</div>;
