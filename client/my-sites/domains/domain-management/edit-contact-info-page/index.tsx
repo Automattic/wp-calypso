e557553785f17faf9f68a53ddf23cfda32e7639d
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-11-22 17:19:38 -0300

    Remove unused props for now

--- client/my-sites/domains/domain-management/edit-contact-info-page/index.tsx
+++ client/my-sites/domains/domain-management/edit-contact-info-page/index.tsx
@@ -3,9 +3,8 @@ import Main from 'calypso/components/main';
 import BodySectionCssClass from 'calypso/layout/body-section-css-class';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import isRequestingWhois from 'calypso/state/selectors/is-requesting-whois';
-import type { EditContactInfoPageProps } from './types';
 
-const EditContactInfoPage = ( props: EditContactInfoPageProps ): JSX.Element => {
+const EditContactInfoPage = (): JSX.Element => {
 	return (
 		<Main className="edit-contact-info-page" wideLayout>
 			<BodySectionCssClass bodyClass={ [ 'edit__body-white' ] } />
