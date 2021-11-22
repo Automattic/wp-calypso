f13eb8a9e159132d89979aa6d0c3c3bc1169466d
Noah Allen <noahtallen@gmail.com>
2021-11-22 15:40:35 -0800

    Explicitly list unsupported browsers.

--- client/server/middleware/unsupported-browser.js
+++ client/server/middleware/unsupported-browser.js
@@ -3,7 +3,37 @@ import { matchesUA } from 'browserslist-useragent';
 import { addQueryArgs } from 'calypso/lib/url';
 import analytics from 'calypso/server/lib/analytics';
 
-function isSupportedBrowser( req ) {
+/**
+ * This is a list of browsers which DEFINITELY do not work on WordPress.com.
+ *
+ * I created this list by finding the oldest version of Chrome which supports the
+ * log-in page, and then finding which language feature was added in that release.
+ *
+ * In this case, the feature was optional chaining, which we use in our bundle,
+ * but is not supported by these browsers. As a result, these browsers do not
+ * work with the WordPress.com login page. See https://caniuse.com/?search=optional%20chaining.
+ *
+ * In other words, if you use Chrome v79, you won't be able to log in. But if you
+ * use v80, the form works.
+ *
+ * For browsers not listed, we have not tested whether they work.
+ *
+ * Importantly, browsers are only completely supported if they fall within the range
+ * listed in package.json. This list only serves as a way to assist users who are
+ * using a browser which is definitely broken. It is not a guarantee that things
+ * will work flawlessly on newer versions.
+ */
+const UNSUPPORTED_BROWSERS = [
+	'ie <= 11',
+	'edge <= 80',
+	'firefox <= 74',
+	'chrome <= 80',
+	'safari <= 13', // Note: 13.1 IS supported. Browserslist considers Safari point releases as new versions.
+	'opera <= 67',
+	'ios <= 13.3',
+];
+
+function isUnsupportedBrowser( req ) {
 	// The desktop app sends a UserAgent including WordPress, Electron and Chrome.
 	// We need to check if the chrome portion is supported, but the UA library
 	// will select WordPress and Electron before Chrome, giving a result not
@@ -15,6 +45,7 @@ function isSupportedBrowser( req ) {
 		ignorePatch: true,
 		ignoreMinor: true,
 		allowHigherVersions: true,
+		browsers: UNSUPPORTED_BROWSERS,
 	} );
 }
 
@@ -69,7 +100,7 @@ export default () => ( req, res, next ) => {
 	}
 
 	const forceRedirect = config.isEnabled( 'redirect-fallback-browsers/test' );
-	if ( ! forceRedirect && isSupportedBrowser( req ) ) {
+	if ( ! forceRedirect && ! isUnsupportedBrowser( req ) ) {
 		next();
 		return;
 	}
