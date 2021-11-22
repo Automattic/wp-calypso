937cbc0a4d8ef6aff1a42981fe865d7f669d917d
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2021-11-22 14:24:44 -0300

    Intro Banner: Hide it if running a sale

--- client/jetpack-cloud/sections/pricing/header/index.tsx
+++ client/jetpack-cloud/sections/pricing/header/index.tsx
@@ -1,19 +1,17 @@
 import { useTranslate } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
 import FormattedHeader from 'calypso/components/formatted-header';
 import IntroPricingBanner from 'calypso/components/jetpack/intro-pricing-banner';
-import { useExperiment } from 'calypso/lib/explat';
 import { preventWidows } from 'calypso/lib/formatting';
+import { getJetpackSaleCoupon } from 'calypso/state/marketing/selectors';
 
 import './style.scss';
 
 const Header: React.FC< Props > = () => {
 	const translate = useTranslate();
-	const [ isLoadingExperimentAssignment, experimentAssignment ] = useExperiment(
-		'calypso_jetpack_pricing_page_without_money_back_banner'
-	);
 
-	const suppressIntroBanner =
-		! isLoadingExperimentAssignment && experimentAssignment?.variationName === 'treatment';
+	/** Suprees the intro banner if we have a sale running */
+	const suppressIntroBanner = !! useSelector( getJetpackSaleCoupon );
 
 	return (
 		<>
