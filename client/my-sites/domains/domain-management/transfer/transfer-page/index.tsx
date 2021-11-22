2f7a653f24108ae9beb857dbb9d3712ed4b987bf
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-11-22 12:56:57 -0300

    Create new transfer item components

--- client/my-sites/domains/domain-management/transfer/transfer-page/index.tsx
+++ client/my-sites/domains/domain-management/transfer/transfer-page/index.tsx
@@ -1,11 +1,19 @@
 import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
+import { Card } from '@automattic/components';
+import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
+import ActionCard from 'calypso/components/action-card';
 import Main from 'calypso/components/main';
 import BodySectionCssClass from 'calypso/layout/body-section-css-class';
 import { getSelectedDomain, isMappedDomain } from 'calypso/lib/domains';
 import Breadcrumbs from 'calypso/my-sites/domains/domain-management/components/breadcrumbs';
-import { domainManagementEdit, domainManagementList } from 'calypso/my-sites/domains/paths';
+import {
+	domainManagementEdit,
+	domainManagementList,
+	domainManagementTransferToAnotherUser,
+	domainManagementTransferToOtherSite,
+} from 'calypso/my-sites/domains/paths';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import getPrimaryDomainBySiteId from 'calypso/state/selectors/get-primary-domain-by-site-id';
 import isDomainOnlySite from 'calypso/state/selectors/is-domain-only-site';
@@ -13,6 +21,8 @@ import isPrimaryDomainBySiteId from 'calypso/state/selectors/is-primary-domain-b
 import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
 import { hasLoadedSiteDomains } from 'calypso/state/sites/domains/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+
+import './style.scss';
 
 const TransferPage = ( props: TransferPageProps ): JSX.Element => {
 	const { __ } = useI18n();
@@ -49,10 +59,36 @@ const TransferPage = ( props: TransferPageProps ): JSX.Element => {
 	};
 
 	return (
-		<Main className="transfer-page" wideLayout>
+		<Main wideLayout className="transfer-page">
 			<BodySectionCssClass bodyClass={ [ 'edit__body-white' ] } />
 			{ renderBreadcrumbs() }
-			The page goes here
+			<Card>
+				<ActionCard
+					buttonHref={ domainManagementTransferToAnotherUser(
+						selectedSite.slug,
+						selectedDomainName,
+						currentRoute
+					) }
+					buttonText={ translate( 'Continue', { context: 'Verb' } ) }
+					headerText={ translate( 'To another user', {
+						comment: 'Transfer a domain to another user',
+					} ) }
+					mainText={ translate( 'Transfer this domain to any administrator on this site' ) }
+				/>
+				<div className="transfer-page__item-separator"></div>
+				<ActionCard
+					buttonHref={ domainManagementTransferToOtherSite(
+						selectedSite.slug,
+						selectedDomainName,
+						currentRoute
+					) }
+					buttonText={ translate( 'Continue', { context: 'Verb' } ) }
+					headerText={ translate( 'To another WordPress.com site', {
+						comment: 'Transfer a domain to another WordPress.com site',
+					} ) }
+					mainText={ translate( 'Transfer this domain to any site you are an administrator on' ) }
+				/>
+			</Card>
 		</Main>
 	);
 };
