4837e28a439e895937b6c13e68a65200a1e5be78
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-11-23 14:17:06 +0200

    Settings: Cleanup unused publish confirmation code (#58351)
    * Settings: Cleanup unused publish confirmation code
    
    * Fix pre-existing typo

--- client/my-sites/site-settings/composing/publish-confirmation.jsx
+++ client/my-sites/site-settings/composing/publish-confirmation.jsx
@@ -1,82 +1,21 @@
-import { localize } from 'i18n-calypso';
-import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useTranslate } from 'i18n-calypso';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
-import { saveConfirmationSidebarPreference } from 'calypso/state/editor/actions';
-import { isConfirmationSidebarEnabled } from 'calypso/state/editor/selectors';
-import { isFetchingPreferences } from 'calypso/state/preferences/selectors';
-import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
-class PublishConfirmation extends Component {
-	constructor( props ) {
-		super( props );
-		this.state = { isToggleOn: props.publishConfirmationEnabled };
-		this.handleToggle = this.handleToggle.bind( this );
-	}
+export default function PublishConfirmation() {
+	const translate = useTranslate();
 
-	UNSAFE_componentWillReceiveProps( nextProps ) {
-		if ( nextProps.publishConfirmationEnabled !== this.state.isToggleOn ) {
-			this.setState( { isToggleOn: nextProps.publishConfirmationEnabled } );
-		}
-	}
-
-	handleToggle() {
-		const { siteId, savePublishConfirmationPreference } = this.props;
-		const isToggleOn = ! this.state.isToggleOn;
-		this.setState( { isToggleOn: isToggleOn } );
-		savePublishConfirmationPreference( siteId, isToggleOn );
-	}
-
-	render() {
-		const { translate } = this.props;
-
-		return (
-			<FormFieldset>
-				<FormLabel>{ translate( 'Show publish confirmation' ) }</FormLabel>
-				<FormSettingExplanation>
-					{ translate(
-						'The Block Editor handles the Publish confirmation setting. ' +
-							'To enable it, go to Options under the Ellipses menu in the Editor ' +
-							'and check "Enable Pre-publish checks."'
-					) }
-				</FormSettingExplanation>
-			</FormFieldset>
-		);
-	}
+	return (
+		<FormFieldset>
+			<FormLabel>{ translate( 'Show publish confirmation' ) }</FormLabel>
+			<FormSettingExplanation>
+				{ translate(
+					'The Block Editor handles the Publish confirmation setting. ' +
+						'To enable it, go to Options under the Ellipsis menu in the Editor ' +
+						'and check "Enable Pre-publish checks."'
+				) }
+			</FormSettingExplanation>
+		</FormFieldset>
+	);
 }
-
-PublishConfirmation.defaultProps = {
-	publishConfirmationEnabled: true,
-};
-
-PublishConfirmation.propTypes = {
-	siteId: PropTypes.number,
-	fetchingPreferences: PropTypes.bool,
-	publishConfirmationEnabled: PropTypes.bool,
-	savePublishConfirmationPreference: PropTypes.func,
-	translate: PropTypes.func,
-};
-
-export default connect(
-	( state ) => {
-		const siteId = getSelectedSiteId( state );
-
-		return {
-			siteId,
-			fetchingPreferences: isFetchingPreferences( state ),
-			publishConfirmationEnabled: isConfirmationSidebarEnabled( state, siteId ),
-		};
-	},
-	( dispatch ) => {
-		return bindActionCreators(
-			{
-				savePublishConfirmationPreference: saveConfirmationSidebarPreference,
-			},
-			dispatch
-		);
-	}
-)( localize( PublishConfirmation ) );
--- client/state/preferences/constants.js
+++ client/state/preferences/constants.js
@@ -4,7 +4,6 @@ export const DEFAULT_PREFERENCE_VALUES = {
 	'guided-tours-history': [],
 	recentSites: [],
 	mediaScale: 0.157,
-	editorConfirmationDisabledSites: [],
 	colorScheme: 'default',
 	homeQuickLinksToggleStatus: 'expanded',
 };
