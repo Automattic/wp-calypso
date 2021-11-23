ea16ab2d8557b32792dfd5927e942621b9c1f955
leonardost <leonardost@users.noreply.github.com>
2021-11-23 14:22:57 -0300

    Domains: Add breadcrumbs to new transfer page (#58313)
    Changes proposed in this Pull Request
    
    This PR adds breadcrumbs to the new redesigned transfer page.
    
    This is part of the domain management redesign project described in pcYYhz-m2-p2.
    
    Testing instructions
    
    - Build this branch locally or open the live Calypso link
    - Go to "Upgrades > Domains"
    - Select one of your registered domains
    - If you're in the live Calypso link, please append ?flags=domains/transfers-redesign to your URL to enable the appropriate feature flag
    - Select the "Transfer your domain" options
    
    These should be checked:
    
    - Ensure the transfer page contains breadcrumbs like shown in the screenshot above
    - Please ensure the breadcrumb links take you to the correct pages - `Domains` should take you to the domains list page and `yourdomain.com` should take you to the specific domain's settings page
    - Lastly, please also check the mobile view version and ensure the back link takes you to the domain settings page
    
    Commits:
    
    * Create new domain transfer page
    
    * Make page's layout wide and the background white
    
    * Convert component to typescript
    
    * Remove unused props for now
    
    * Add breadcrumbs to new transfer page
    
    * Fix breadcrumb link for mobile view
    
    * Use `useI18n` hook instead of `localize` and add properties to `TransferPageProps`

--- client/my-sites/domains/domain-management/transfer/transfer-page/index.tsx
+++ client/my-sites/domains/domain-management/transfer/transfer-page/index.tsx
@@ -1,7 +1,11 @@
+import { sprintf } from '@wordpress/i18n';
+import { useI18n } from '@wordpress/react-i18n';
 import { connect } from 'react-redux';
 import Main from 'calypso/components/main';
 import BodySectionCssClass from 'calypso/layout/body-section-css-class';
 import { getSelectedDomain, isMappedDomain } from 'calypso/lib/domains';
+import Breadcrumbs from 'calypso/my-sites/domains/domain-management/components/breadcrumbs';
+import { domainManagementEdit, domainManagementList } from 'calypso/my-sites/domains/paths';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import getPrimaryDomainBySiteId from 'calypso/state/selectors/get-primary-domain-by-site-id';
 import isDomainOnlySite from 'calypso/state/selectors/is-domain-only-site';
@@ -9,11 +13,46 @@ import isPrimaryDomainBySiteId from 'calypso/state/selectors/is-primary-domain-b
 import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
 import { hasLoadedSiteDomains } from 'calypso/state/sites/domains/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import type { TransferPageProps } from './types';
 
-const TransferPage = (): JSX.Element => {
+const TransferPage = ( props: TransferPageProps ): JSX.Element => {
+	const { __ } = useI18n();
+	const { selectedSite, currentRoute, selectedDomainName } = props;
+
+	const renderBreadcrumbs = () => {
+		const items = [
+			{
+				// translators: Internet domains, e.g. mygroovydomain.com
+				label: __( 'Domains' ),
+				href: domainManagementList( selectedSite.slug, selectedDomainName ),
+			},
+			{
+				label: selectedDomainName,
+				href: domainManagementEdit( selectedSite.slug, selectedDomainName, currentRoute ),
+			},
+			{
+				// translators: Verb - Transfer a domain somewhere else
+				label: __( 'Transfer' ),
+			},
+		];
+
+		const mobileItem = {
+			label: sprintf(
+				/* translators: Link to return to the settings management page of a specific domain (%s = domain, e.g. example.com) */
+				__( 'Back to %s' ),
+				selectedDomainName
+			),
+			href: domainManagementEdit( selectedSite.slug, selectedDomainName, currentRoute ),
+			showBackArrow: true,
+		};
+
+		return <Breadcrumbs items={ items } mobileItem={ mobileItem } />;
+	};
+
 	return (
 		<Main className="transfer-page" wideLayout>
 			<BodySectionCssClass bodyClass={ [ 'edit__body-white' ] } />
+			{ renderBreadcrumbs() }
 			The page goes here
 		</Main>
 	);
--- client/my-sites/domains/domain-management/transfer/transfer-page/types.tsx
+++ client/my-sites/domains/domain-management/transfer/transfer-page/types.tsx
@@ -1,3 +1,7 @@
+import { SiteData } from 'calypso/state/ui/selectors/site-data';
+
 export type TransferPageProps = {
 	selectedDomainName: string;
+	selectedSite: SiteData;
+	currentRoute: string;
 };
