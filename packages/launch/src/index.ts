ed59de394209d3b524a51a2a70e8fe9558c36f8a
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-11-23 17:18:02 +0100

    Onboarding: remove circular dependency

--- packages/launch/src/index.ts
+++ packages/launch/src/index.ts
@@ -1,4 +1,3 @@
 export { default } from './launch';
 export * from './hooks';
 export { default as LaunchContext } from './context';
-export { default as Confetti } from './focused-launch/success/confetti';
