00fb213ccff35b2ab861cc1778e9e6ddc69d2530
Dean Sas <dean.sas@automattic.com>
2021-11-23 16:04:17 +0000

    Correct tracks events for JITM SidebarBanner
    When dismissing a SidebarBanner the events was tracking twice - once for
    the JITM specific tracking and once for the tracking that is part of the
    Banner component. The clicks and dismiss events were using the Banner
    component to trigger them, rather than the JITM code, though it was
    implemented compatibly.
    
    This change simplifies SidebarBanner and brings it further in-line with
    other JITM templates by relying on the jitm specific logic more.
    
    h/t @mmtr

--- client/blocks/jitm/templates/sidebar-banner.jsx
+++ client/blocks/jitm/templates/sidebar-banner.jsx
@@ -7,21 +7,11 @@ export default function SidebarBannerTemplate( {
 	CTA,
 	message,
 	id,
+	onClick,
 	onDismiss,
-	tracks,
 	isDismissible = false,
+	trackImpression,
 } ) {
-	const clickName = tracks?.click?.name ?? `jitm_nudge_click_${ id }`;
-	const clickProps = tracks?.click?.props;
-
-	const displayName = tracks?.display?.name ?? `jitm_nudge_impression_${ id }`;
-	const displayProps = tracks?.display?.props;
-
-	const dismissName = tracks?.dismiss?.name ?? `jitm_nudge_dismiss_${ id }`;
-	const dismissProps = tracks?.dismiss?.props;
-
-	const jitmProps = { id: id, jitm: true };
-
 	let dismissPreferenceName = '';
 	let forceHref = true;
 	if ( isDismissible ) {
@@ -31,23 +21,20 @@ export default function SidebarBannerTemplate( {
 	}
 
 	return (
-		<UpsellNudge
-			callToAction={ CTA.message }
-			compact
-			event={ displayName }
-			forceHref={ forceHref }
-			forceDisplay={ true }
-			dismissPreferenceName={ dismissPreferenceName }
-			dismissTemporary={ true }
-			href={ CTA.link }
-			onDismissClick={ onDismiss }
-			title={ preventWidows( message ) }
-			tracksClickName={ clickName }
-			tracksClickProperties={ { ...jitmProps, ...clickProps } }
-			tracksImpressionName={ displayName }
-			tracksImpressionProperties={ { ...jitmProps, ...displayProps } }
-			tracksDismissName={ dismissName }
-			tracksDismissProperties={ { ...jitmProps, ...dismissProps } }
-		/>
+		<>
+			<UpsellNudge
+				callToAction={ CTA.message }
+				compact
+				forceHref={ forceHref }
+				forceDisplay={ true }
+				dismissPreferenceName={ dismissPreferenceName }
+				dismissTemporary={ true }
+				href={ CTA.link }
+				onClick={ onClick }
+				onDismissClick={ onDismiss }
+				title={ preventWidows( message ) }
+			/>
+			{ trackImpression && trackImpression() }
+		</>
 	);
 }
