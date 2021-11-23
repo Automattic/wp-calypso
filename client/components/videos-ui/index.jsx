3dfee6350253dbc670bbe50978888eef4bf723e3
Andres Blanco <andresblanco@gmail.com>
2021-11-23 18:03:28 -0300

    add checkmark loaded from server

--- client/components/videos-ui/index.jsx
+++ client/components/videos-ui/index.jsx
@@ -1,6 +1,8 @@
 import { Button, Gridicon } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { useEffect, useState, useRef } from 'react';
+import getOriginalUserSetting from 'calypso/state/selectors/get-original-user-setting';
+import { useSelector } from 'react-redux';
 import useCourseQuery from 'calypso/data/courses/use-course-query';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import VideoFooterBar from './video-footer-bar';
@@ -10,12 +12,20 @@ import './style.scss';
 
 const VideosUi = ( { shouldDisplayTopLinks = false, onBackClick = () => {} } ) => {
 	const translate = useTranslate();
-	const { data: course } = useCourseQuery( 'blogging-quick-start', { retry: false } );
-	const [ selectedVideoIndex, setSelectedVideoIndex ] = useState( null );
 
+	const courseSlug = 'blogging-quick-start';
+	const { data: course } = useCourseQuery( courseSlug, { retry: false } );
+
+	const userCourseProgression = useSelector( ( state ) => {
+		const courses = getOriginalUserSetting( state, 'courses' );
+		return courses !== null && courseSlug in courses ? courses[ courseSlug ] : [];
+	} );
+
+	const [ selectedVideoIndex, setSelectedVideoIndex ] = useState( null );
 	const [ currentVideoKey, setCurrentVideoKey ] = useState( null );
 	const [ currentVideo, setCurrentVideo ] = useState( null );
 	const [ isPlaying, setIsPlaying ] = useState( false );
+
 	const videoRef = useRef( null );
 
 	const onVideoPlayClick = ( videoSlug, videoInfo ) => {
@@ -135,7 +145,10 @@ const VideosUi = ( { shouldDisplayTopLinks = false, onBackClick = () => {} } ) =
 					<div className="videos-ui__chapters">
 						{ course &&
 							Object.entries( course.videos ).map( ( data, i ) => {
+								const isVideoCompleted =
+									data[ 0 ] in userCourseProgression && userCourseProgression[ data[ 0 ] ];
 								const videoInfo = data[ 1 ];
+
 								return (
 									<div
 										key={ i }
@@ -146,22 +159,25 @@ const VideosUi = ( { shouldDisplayTopLinks = false, onBackClick = () => {} } ) =
 											className="videos-ui__chapter-accordion-toggle"
 											onClick={ () => onVideoSelected( i ) }
 										>
-											{ /* to be restored when completion functionality is implmented */ }
-											{ /* <span className="videos-ui__completed">
-											<Gridicon icon="checkmark" size={ 12 } />
-										</span> */ }
-											{ isVideoSelected( i ) && (
+											<span className="videos-ui__video-title">
+												{ i + 1 }. { videoInfo.title }{ ' ' }
+											</span>
+											<span className="videos-ui__duration"> { videoInfo.duration } </span>{ ' ' }
+											{ isVideoCompleted && (
+												<span className="videos-ui__completed-checkmark">
+													<Gridicon icon="checkmark" size={ 12 } />
+												</span>
+											) }
+											{ isVideoSelected( i ) && ! isVideoCompleted && (
 												<span className="videos-ui__status-icon">
 													<Gridicon icon="chevron-up" size={ 18 } />
 												</span>
 											) }
-											{ ! isVideoSelected( i ) && (
+											{ ! isVideoSelected( i ) && ! isVideoCompleted && (
 												<span className="videos-ui__status-icon">
 													<Gridicon icon="chevron-down" size={ 18 } />
 												</span>
 											) }
-											{ i + 1 }. { videoInfo.title }{ ' ' }
-											<span className="videos-ui__duration"> { videoInfo.duration } </span>{ ' ' }
 										</button>
 										<div className="videos-ui__active-video-content">
 											<div>
