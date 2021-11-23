e295df02944eb375830f8b424a36b40a799461a2
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-11-23 11:27:42 +0200

    Editor: Remove obsolete unused EditorMediaModalGalleryHelp (#58349)

--- client/state/preferences/constants.js
+++ client/state/preferences/constants.js
@@ -1,8 +1,6 @@
 export const USER_SETTING_KEY = 'calypso_preferences';
 
 export const DEFAULT_PREFERENCE_VALUES = {
-	mediaModalGalleryInstructionsDismissed: false,
-	mediaModalGalleryInstructionsDismissedForSession: false,
 	'guided-tours-history': [],
 	recentSites: [],
 	mediaScale: 0.157,
