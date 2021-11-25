ea11779134280b4e06ade3229bbca5ae5509b012
Kirk Wight <kwight@kwight.ca>
2021-11-25 13:17:40 -0800

    Add markup and props.

--- client/components/videos-ui/modal-footer-bar.jsx
+++ client/components/videos-ui/modal-footer-bar.jsx
@@ -1,10 +1,10 @@
-import { Gridicon } from '@automattic/components';
+import { Button, Gridicon } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 
 import './modal-footer-bar.scss';
 
-const ModalFooterBar = ( { onBackClick = () => {}, course = {} } ) => {
+const ModalFooterBar = ( { onBackClick = () => {}, course = {}, isCourseComplete = false } ) => {
 	const translate = useTranslate();
 	const onBackLinkClick = ( event ) => {
 		recordTracksEvent( 'calypso_courses_mobile_back_click', {
@@ -14,11 +14,14 @@ const ModalFooterBar = ( { onBackClick = () => {}, course = {} } ) => {
 	};
 
 	return (
-		<div className={ 'videos-ui__bar videos-ui__modal-footer-bar' }>
+		<div className={ 'videos-ui__bar videos-ui__footer-bar videos-ui__modal-footer-bar' }>
 			<a href="/" onClick={ onBackLinkClick }>
 				<Gridicon icon="chevron-left" size={ 24 } />
 				<span>{ translate( 'Back' ) }</span>
 			</a>
+			{ isCourseComplete && (
+				<Button className="videos-ui__button">{ translate( 'Start writing' ) }</Button>
+			) }
 		</div>
 	);
 };
