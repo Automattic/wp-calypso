7baaa70bc8e6d9a9023e93980d0e94853ca45713
Emanuele Buccelli <e.buccelli@gmail.com>
2021-11-25 12:38:40 +0100

    Cleanup. Remove getImageSrcForView

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/welcome-tour/utils.ts
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/welcome-tour/utils.ts
@@ -2,20 +2,11 @@
  * External Dependencies
  */
 import { useMobileBreakpoint } from '@automattic/viewport-react';
+import { useEffect } from 'react';
 /**
  * Internal dependencies
  */
 import type { TourAsset } from './types';
-
-/**
- * This function returns the src string for the current Welcome Tour View based on the current viewport
- *
- * @param asset {TourAsset} list of assets for current Welcome Tour View
- * @returns {string} asset src string
- */
-export const getImageSrcForView = ( asset: TourAsset ): string => {
-	return ! isDesktop() && asset.mobile ? asset.mobile.src : asset.desktop.src;
-};
 
 export function useImageSrcForView( asset: TourAsset ): string {
 	const isMobile = useMobileBreakpoint();
