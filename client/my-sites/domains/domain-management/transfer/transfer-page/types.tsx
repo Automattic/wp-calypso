a660a687e1952e658564b1f094cac24890c288a8
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-11-25 21:22:45 -0300

    Add missing properties to props types declaration

--- client/my-sites/domains/domain-management/transfer/transfer-page/types.tsx
+++ client/my-sites/domains/domain-management/transfer/transfer-page/types.tsx
@@ -3,16 +3,19 @@ import { SiteData } from 'calypso/state/ui/selectors/site-data';
 
 export type TransferPageProps = {
 	currentRoute: string;
-	domain: any; // TODO: Fix this
 	domains: ResponseDomain[];
 	isAtomic: boolean;
+	isDomainInfoLoading: boolean;
+	isDomainLocked: boolean;
 	isDomainOnly: boolean;
+	isLockingOrUnlockingDomain: boolean;
 	isMapping: boolean;
 	isPrimaryDomain: boolean;
 	isRequestingTransferCode: boolean;
 	isCancelingTransfer: boolean;
-	unlockDomain: any; // TODO: Fix this
+	lockDomain: any; // TODO: Fix this
 	requestDomainTransferCodeOnly: any; // TODO: Fix this
 	selectedDomainName: string;
 	selectedSite: SiteData;
+	unlockDomain: any; // TODO: Fix this
 };
