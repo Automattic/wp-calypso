ca01e5e9191e5fd404ce0b198f357daf4ed6eebd
retrofox <rdsuarez@gmail.com>
2021-11-26 16:55:28 -0300

    woop: add custom useEligibility hook

---
+++ client/signup/steps/woocommerce-install/hooks/use-eligibility/Readme.md
@@ -0,0 +1,22 @@
+# useEligibility
+
+Custom React hook to process eligibility data to provide a clear object to deal with the woocommerce-install flow.
+
+## Usage
+
+```es6
+import useEligibility from '../hooks/use-eligibility';
+
+function WordPressSubdomainWarningCard() {
+	const { wpcomSubdomainWarning, stagingDomain } = useEligibility( siteId );
+
+	if ( ! wpcomSubdomainWarning ) {
+		return null;
+	}
+
+	return (
+		<div className="card is-warning">
+			Domain is going to change to { stagingDomain }.
+		</div>
+	);
+}
---
+++ client/signup/steps/woocommerce-install/hooks/use-eligibility/index.ts
@@ -0,0 +1,73 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	fetchAutomatedTransferStatusOnce,
+	requestEligibility,
+} from 'calypso/state/automated-transfer/actions';
+import {
+	isFetchingAutomatedTransferStatus,
+	getEligibility,
+	EligibilityData,
+	EligibilityWarning,
+} from 'calypso/state/automated-transfer/selectors';
+import { getSiteDomain } from 'calypso/state/sites/selectors';
+// import { eligibilityHolds } from 'calypso/state/automated-transfer/constants';
+
+type EligibilityHook = {
+	eligibilityHolds?: string[];
+	eligibilityWarnings?: EligibilityWarning[];
+	isFetchingTransferStatus: boolean;
+	wpcomSubdomainWarning: EligibilityWarning | undefined;
+	wpcomDomain: string | null;
+	stagingDomain: string | null;
+};
+
+export default function useEligibility( siteId: number ): EligibilityHook {
+	const dispatch = useDispatch();
+
+	// Request eligibility data.
+	useEffect( () => {
+		if ( ! siteId ) {
+			return;
+		}
+
+		dispatch( fetchAutomatedTransferStatusOnce( siteId ) );
+		dispatch( requestEligibility( siteId ) );
+	}, [ siteId, dispatch ] );
+
+	// Get eligibility data.
+	const {
+		eligibilityHolds,
+		eligibilityWarnings: allEligibilityWarnings,
+	}: EligibilityData = useSelector( ( state ) => getEligibility( state, siteId ) );
+
+	// Pick the wpcom subdomain.
+	const wpcomDomain = useSelector( ( state ) => getSiteDomain( state, siteId ) );
+
+	// Get staging sudomain based on the wpcom subdomain.
+	const stagingDomain = wpcomDomain?.replace( /\b\.wordpress\.com/, '.wpcomstaging.com' ) || null;
+
+	// Check whether it's requesting eligibility data.
+	const isFetchingTransferStatus = !! useSelector( ( state ) =>
+		isFetchingAutomatedTransferStatus( state, siteId )
+	);
+
+	// Check whether the wpcom.com subdomain warning is present.
+	const wpcomSubdomainWarning: EligibilityWarning | undefined = allEligibilityWarnings?.find(
+		( { id } ) => id === 'wordpress_subdomain'
+	);
+
+	// Remove warnings.
+	const eligibilityWarnings = allEligibilityWarnings?.filter(
+		( { id } ) => id !== 'wordpress_subdomain'
+	);
+
+	return {
+		eligibilityHolds,
+		eligibilityWarnings,
+		isFetchingTransferStatus,
+		wpcomSubdomainWarning,
+		wpcomDomain,
+		stagingDomain,
+	};
+}
