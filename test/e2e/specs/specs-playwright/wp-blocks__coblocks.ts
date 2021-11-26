895fb31215a31133dbd9a474d290385debe30043
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2021-11-26 17:48:49 +0100

    Merge CoBlocks tests into one file

---
+++ test/e2e/specs/specs-playwright/wp-blocks__coblocks.ts
@@ -0,0 +1,203 @@
+/**
+ * @group gutenberg
+ */
+import path from 'path';
+import {
+	setupHooks,
+	BrowserHelper,
+	DataHelper,
+	MediaHelper,
+	LoginPage,
+	GutenbergEditorPage,
+	TestFile,
+	ClicktoTweetBlock,
+	DynamicHRBlock,
+	HeroBlock,
+	ImageBlock,
+	LogosBlock,
+	PricingTableBlock,
+} from '@automattic/calypso-e2e';
+import { Page } from 'playwright';
+import { TEST_IMAGE_PATH } from '../constants';
+
+const user = BrowserHelper.targetCoBlocksEdge()
+	? 'coBlocksSimpleSiteEdgeUser'
+	: 'gutenbergSimpleSiteUser';
+
+const siteHost = DataHelper.getAccountSiteURL( user, { protocol: false } );
+const calypsoURL = DataHelper.getCalypsoURL();
+
+async function startNewPost( page: Page ): Promise< GutenbergEditorPage > {
+	await page.goto( path.join( calypsoURL, 'post', siteHost ) );
+	return new GutenbergEditorPage( page );
+}
+
+describe( DataHelper.createSuiteTitle( 'CoBlocks' ), () => {
+	let gutenbergEditorPage: GutenbergEditorPage;
+	let page: Page;
+
+	setupHooks( ( args ) => {
+		page = args.page;
+		page.on( 'dialog', async ( dialog ) => {
+			await dialog.accept();
+		} );
+	} );
+
+	beforeAll( async () => {
+		await new LoginPage( page ).login( { account: user } );
+	} );
+
+	afterEach( async () => {
+		// Prevents the autosave so we don't see the "restore from autosave" message.
+		await page.evaluate( () => {
+			window.sessionStorage.clear();
+		} );
+	} );
+
+	describe( 'Blocks', function () {
+		let pricingTableBlock: PricingTableBlock;
+		let logoImage: TestFile;
+
+		// Test data
+		const pricingTableBlockPrice = 888;
+		const heroBlockHeading = 'Hero heading';
+		const clicktoTweetBlockTweet =
+			'The foolish man seeks happiness in the distance. The wise grows it under his feet. — James Oppenheim';
+
+		beforeAll( async () => {
+			logoImage = await MediaHelper.createTestFile( TEST_IMAGE_PATH );
+			gutenbergEditorPage = await startNewPost( page );
+		} );
+
+		it( `Insert ${ PricingTableBlock.blockName } block and enter price to left table`, async function () {
+			const blockHandle = await gutenbergEditorPage.addBlock(
+				PricingTableBlock.blockName,
+				PricingTableBlock.blockEditorSelector
+			);
+			pricingTableBlock = new PricingTableBlock( blockHandle );
+			await pricingTableBlock.enterPrice( 1, pricingTableBlockPrice );
+		} );
+
+		it( `Insert ${ DynamicHRBlock.blockName } block`, async function () {
+			await gutenbergEditorPage.addBlock(
+				DynamicHRBlock.blockName,
+				DynamicHRBlock.blockEditorSelector
+			);
+		} );
+
+		it( `Insert ${ HeroBlock.blockName } block and enter heading`, async function () {
+			const blockHandle = await gutenbergEditorPage.addBlock(
+				HeroBlock.blockName,
+				HeroBlock.blockEditorSelector
+			);
+			const heroBlock = new HeroBlock( blockHandle );
+			await heroBlock.enterHeading( heroBlockHeading );
+		} );
+
+		it( `Insert ${ ClicktoTweetBlock.blockName } block and enter tweet content`, async function () {
+			const blockHandle = await gutenbergEditorPage.addBlock(
+				ClicktoTweetBlock.blockName,
+				ClicktoTweetBlock.blockEditorSelector
+			);
+			const clickToTweetBlock = new ClicktoTweetBlock( blockHandle );
+			await clickToTweetBlock.enterTweetContent( clicktoTweetBlockTweet );
+		} );
+
+		it( `Insert ${ LogosBlock.blockName } block and set image`, async function () {
+			const blockHandle = await gutenbergEditorPage.addBlock(
+				LogosBlock.blockName,
+				LogosBlock.blockEditorSelector
+			);
+			const logosBlock = new LogosBlock( blockHandle );
+			await logosBlock.upload( logoImage.fullpath );
+		} );
+
+		it( 'Publish and visit post', async function () {
+			// Must save as draft first to bypass issue with post-publish panel being auto-dismissed when
+			// ClickToTweet and Logos blocks are present.
+			// See https://github.com/Automattic/wp-calypso/issues/54421.
+			await gutenbergEditorPage.publish( { visit: true, saveDraft: true } );
+		} );
+
+		// Pass in a 1D array of values or text strings to validate each block.
+		it.each`
+			block                  | content
+			${ PricingTableBlock } | ${ [ pricingTableBlockPrice ] }
+			${ DynamicHRBlock }    | ${ null }
+			${ HeroBlock }         | ${ [ heroBlockHeading ] }
+			${ ClicktoTweetBlock } | ${ [ clicktoTweetBlockTweet ] }
+		`(
+			`Confirm $block.blockName block is visible in published post`,
+			async ( { block, content } ) => {
+				// Pass the Block object class here then call the static method to validate.
+				await block.validatePublishedContent( page, content );
+			}
+		);
+
+		it( `Confirm Logos block is visible in published post`, async () => {
+			await LogosBlock.validatePublishedContent( page, [ logoImage.filename ] );
+		} );
+	} );
+
+	describe( 'Extensions', () => {
+		describe( 'Gutter Control', () => {
+			let pricingTableBlock: PricingTableBlock;
+
+			beforeAll( async () => {
+				gutenbergEditorPage = await startNewPost( page );
+			} );
+
+			it( 'Insert Pricing Table block', async function () {
+				const blockHandle = await gutenbergEditorPage.addBlock(
+					PricingTableBlock.blockName,
+					PricingTableBlock.blockEditorSelector
+				);
+				pricingTableBlock = new PricingTableBlock( blockHandle );
+			} );
+
+			it( 'Open settings sidebar', async function () {
+				await gutenbergEditorPage.openSettings();
+			} );
+
+			it.each( PricingTableBlock.gutterValues )( 'Set gutter value to %s', async ( value ) => {
+				await pricingTableBlock.setGutter( value );
+			} );
+		} );
+
+		describe( 'Replace Image', () => {
+			let imageBlock: ImageBlock;
+			let imageFile: TestFile;
+			let uploadedImagePath: string;
+
+			beforeAll( async () => {
+				imageFile = await MediaHelper.createTestFile( TEST_IMAGE_PATH );
+				gutenbergEditorPage = await startNewPost( page );
+			} );
+
+			it( `Insert ${ ImageBlock.blockName } block and upload image`, async () => {
+				const blockHandle = await gutenbergEditorPage.addBlock(
+					ImageBlock.blockName,
+					ImageBlock.blockEditorSelector
+				);
+				imageBlock = new ImageBlock( blockHandle );
+				const uploadedImage = await imageBlock.upload( imageFile.fullpath );
+				uploadedImagePath = await uploadedImage.getAttribute( 'src' );
+			} );
+
+			it( `Replace uploaded image`, async () => {
+				const editorFrame = await gutenbergEditorPage.getEditorFrame();
+
+				await editorFrame.click( 'button:text("Replace")' );
+				await editorFrame.setInputFiles(
+					'.components-form-file-upload input[type="file"]',
+					imageFile.fullpath
+				);
+				await imageBlock.waitUntilUploaded();
+				const newImage = await imageBlock.getImage();
+				const newImagePath = await newImage.getAttribute( 'src' );
+
+				expect( uploadedImagePath ).not.toBe( newImagePath );
+			} );
+		} );
+	} );
+} );
