7224fd144de4f98835ebec03d6cfed6969f9885f
Daniel López <daniel.lopez@automattic.com>
2021-11-26 15:03:24 +0100

    Emails: Initial commit for adding the first form

--- client/my-sites/email/email-providers-stacked-comparison/index.tsx
+++ client/my-sites/email/email-providers-stacked-comparison/index.tsx
@@ -4,14 +4,17 @@ import {
 } from '@automattic/calypso-products';
 import { withShoppingCart } from '@automattic/shopping-cart';
 import { useTranslate } from 'i18n-calypso';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { connect } from 'react-redux';
 import QueryProductsList from 'calypso/components/data/query-products-list';
 import QuerySiteDomains from 'calypso/components/data/query-site-domains';
 import Main from 'calypso/components/main';
 import { getSelectedDomain } from 'calypso/lib/domains';
 import { hasGSuiteSupportedDomain } from 'calypso/lib/gsuite';
+import { buildNewTitanMailbox } from 'calypso/lib/titan/new-mailbox';
 import withCartKey from 'calypso/my-sites/checkout/with-cart-key';
+import TitanNewMailboxList from 'calypso/my-sites/email/titan-new-mailbox-list';
+import { FullWidthButton } from 'calypso/my-sites/marketplace/components';
 import { Site } from 'calypso/reader/list-manage/types';
 import { getCurrentUserCurrencyCode } from 'calypso/state/currency-code/selectors';
 import { errorNotice } from 'calypso/state/notices/actions';
@@ -23,6 +26,7 @@ import { getDomainsWithForwards } from 'calypso/state/selectors/get-email-forwar
 import { fetchSiteDomains } from 'calypso/state/sites/domains/actions';
 import { getDomainsBySiteId, isRequestingSiteDomains } from 'calypso/state/sites/domains/selectors';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
+import { PASSWORD_RESET_TITAN_FIELD, FULL_NAME_TITAN_FIELD } from '../titan-new-mailbox';
 import EmailProvidersStackedCard from './email-provider-stacked-card';
 import { professionalEmailCard } from './provider-cards/professional-email-card';
 import type { TranslateResult } from 'i18n-calypso';
@@ -30,10 +34,15 @@ import type { ReactElement } from 'react';
 
 import './style.scss';
 
-type Site = {
-	ID: number;
-	slug: string;
+type TitanMailbox = {
+	uuid: any;
+	domain: any;
+	mailbox: any;
+	password: any;
 };
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const noop = () => {};
 
 type EmailProvidersStackedComparisonProps = {
 	comparisonContext: string;
@@ -71,9 +80,37 @@ const EmailProvidersStackedComparison: FunctionComponent< EmailProvidersStackedC
 ) => {
 	const translate = useTranslate();
 	const professionalEmailCardProps: ProviderCard = professionalEmailCard;
-	const { selectedSite } = props;
+	const { selectedSite, selectedDomainName } = props;
+	const onTitanFormReturnKeyPress = noop;
+	const validatedTitanMailboxUuids: TitanMailbox[] = [];
+	const onTitanConfirmNewMailboxes = noop;
 
-	professionalEmailCardProps.formFields = <p>Placeholder</p>;
+	const [ titanMailbox, setTitanMailbox ] = useState( [
+		buildNewTitanMailbox( selectedDomainName, false ),
+	] );
+	const [ addingToCart ] = useState( false );
+
+	professionalEmailCardProps.formFields = (
+		<TitanNewMailboxList
+			onMailboxesChange={ setTitanMailbox }
+			mailboxes={ titanMailbox }
+			selectedDomainName={ selectedDomainName }
+			onReturnKeyPress={ onTitanFormReturnKeyPress }
+			showLabels={ true }
+			validatedMailboxUuids={ validatedTitanMailboxUuids }
+			showAddAnotherMailboxButton={ false }
+			hiddenFields={ [ FULL_NAME_TITAN_FIELD, PASSWORD_RESET_TITAN_FIELD ] }
+		>
+			<FullWidthButton
+				className="email-providers-stacked-comparison__continue"
+				primary
+				busy={ addingToCart }
+				onClick={ onTitanConfirmNewMailboxes }
+			>
+				{ translate( 'Create your mailbox' ) }
+			</FullWidthButton>
+		</TitanNewMailboxList>
+	);
 
 	return (
 		<Main className={ 'email-providers-stacked-comparison__main' } wideLayout>
