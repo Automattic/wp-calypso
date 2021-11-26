78666d375e4e2ec9731153b70b494bca427daa0a
arthur <arthur.chu@automattic.com>
2021-11-26 16:31:35 +0800

    Setup Header and Footer

---
+++ client/signup/steps/courses/footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+	return null;
+};
+
+export default Footer;
--- client/signup/steps/courses/index.tsx
+++ client/signup/steps/courses/index.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import VideosUi from 'calypso/components/videos-ui';
 import StepWrapper from 'calypso/signup/step-wrapper';
 import { saveSignupStep } from 'calypso/state/signup/progress/actions';
+import Footer from './footer';
 import './style.scss';
 
 interface Props {
@@ -23,7 +24,7 @@ export default function CoursesStep( props: Props ): React.ReactNode {
 		<StepWrapper
 			className="courses"
 			hideFormattedHeader
-			stepContent={ <VideosUi /> }
+			stepContent={ <VideosUi headerBar={ null } footerBar={ <Footer /> } /> }
 			skipLabelText={ translate( 'Draft your first post' ) }
 			skipButtonAlign="top"
 			{ ...props }
