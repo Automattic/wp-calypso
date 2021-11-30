14f3387162051debd3e29e337000c412e62b7394
sixhours <calobee@gmail.com>
2021-11-30 14:13:54 -0500

    Add todo to remove test line

--- client/my-sites/theme/main.jsx
+++ client/my-sites/theme/main.jsx
@@ -824,6 +824,7 @@ export default connect(
 		const backPath = getBackPath( state );
 		const isCurrentUserPaid = isUserPaid( state );
 		const theme = getCanonicalTheme( state, siteId, id );
+		//@TODO: This is only for test purposes, remove next line before deploying.
 		theme.retired = false;
 		const siteIdOrWpcom = siteId || 'wpcom';
 		const error = theme ? false : getThemeRequestErrors( state, id, siteIdOrWpcom );
