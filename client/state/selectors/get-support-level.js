f9978696e78c95637fe1530eccb0497933fcd794
Marin Atanasov <tyxla@abv.bg>
2021-11-30 11:53:00 +0200

    Help: Cleanup some unused bits from HelpContact

--- client/state/selectors/get-support-level.js
+++ client/state/selectors/get-support-level.js
@@ -1,14 +1,4 @@
-import { get } from 'lodash';
-
 import 'calypso/state/help/init';
-
-export const SUPPORT_LEVEL_FREE = 'free';
-export const SUPPORT_LEVEL_PERSONAL = 'personal';
-export const SUPPORT_LEVEL_PERSONAL_WITH_LEGACY_CHAT = 'personal-with-legacy-chat';
-export const SUPPORT_LEVEL_PREMIUM = 'premium';
-export const SUPPORT_LEVEL_BUSINESS = 'business';
-export const SUPPORT_LEVEL_ECOMMERCE = 'ecommerce';
-export const SUPPORT_LEVEL_JETPACK_PAID = 'jetpack-paid';
 
 /**
  * Returns the current user's support level, representing their highest paid plan.
@@ -17,5 +7,5 @@ export const SUPPORT_LEVEL_JETPACK_PAID = 'jetpack-paid';
  * @returns {string} Level of support
  */
 export default function getSupportLevel( state ) {
-	return get( state, 'help.supportLevel', null );
+	return state.help.supportLevel ?? null;
 }
