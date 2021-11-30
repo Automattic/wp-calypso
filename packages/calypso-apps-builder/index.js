07d7384dccbc066f012276f128836da4773a4dbf
Sergio Cinos <sergio.cinos@gmail.com>
2021-11-30 22:40:03 +0100

    chore: Create @automattic/calypso-apps-builder package (#58481)
    * Create calypso-apps-builder package
    
    * Remove old file
    
    * Do not pass .cjs files to ts

--- packages/calypso-apps-builder/index.js
+++ packages/calypso-apps-builder/index.js
@@ -1,8 +1,4 @@
-/**
- * This script is used to build calypso apps (anything in /apps). Since most of
- * those apps use a similar structure and ultimately deploy to WordPress.com, we
- * include sandbox sync logic to assist with local DevX.
- */
+#!/usr/bin/env node
 
 import { exec } from 'child_process';
 import { EventEmitter } from 'events';
