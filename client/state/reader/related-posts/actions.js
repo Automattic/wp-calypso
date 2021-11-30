27265ecd71bc485024a77441f3ba568279a024c3
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-11-30 13:48:45 +0200

    WPCOM: Migrate wpcom.undocumented() reader related posts to wpcom.req (#58568)

--- client/state/reader/related-posts/actions.js
+++ client/state/reader/related-posts/actions.js
@@ -1,5 +1,6 @@
 import { filter, map } from 'lodash';
 import wpcom from 'calypso/lib/wp';
+import readerContentWidth from 'calypso/reader/lib/content-width';
 import {
 	READER_RELATED_POSTS_REQUEST,
 	READER_RELATED_POSTS_REQUEST_SUCCESS,
@@ -24,10 +25,14 @@ export function requestRelatedPosts( siteId, postId, scope = SCOPE_ALL ) {
 		} );
 
 		const query = {
-			site_id: siteId,
-			post_id: postId,
+			apiVersion: '1.2',
 			meta: 'site',
 		};
+
+		const contentWidth = readerContentWidth();
+		if ( contentWidth ) {
+			query.content_width = contentWidth;
+		}
 
 		if ( scope === SCOPE_SAME ) {
 			query.size_local = 2;
@@ -37,52 +42,49 @@ export function requestRelatedPosts( siteId, postId, scope = SCOPE_ALL ) {
 			query.size_global = 2;
 		}
 
-		return wpcom
-			.undocumented()
-			.readSitePostRelated( query )
-			.then(
-				( response ) => {
+		return wpcom.req.get( `/read/site/${ siteId }/post/${ postId }/related`, query ).then(
+			( response ) => {
+				dispatch( {
+					type: READER_RELATED_POSTS_REQUEST_SUCCESS,
+					payload: { siteId, postId, scope },
+				} );
+				const sites = filter( map( response && response.posts, 'meta.data.site' ), Boolean );
+				if ( sites && sites.length !== 0 ) {
 					dispatch( {
-						type: READER_RELATED_POSTS_REQUEST_SUCCESS,
-						payload: { siteId, postId, scope },
+						type: READER_SITE_UPDATE,
+						payload: sites,
 					} );
-					const sites = filter( map( response && response.posts, 'meta.data.site' ), Boolean );
-					if ( sites && sites.length !== 0 ) {
-						dispatch( {
-							type: READER_SITE_UPDATE,
-							payload: sites,
-						} );
-					}
-					// collect posts and dispatch
-					dispatch( receivePosts( response && response.posts ) ).then( () => {
-						dispatch( {
-							type: READER_RELATED_POSTS_RECEIVE,
-							payload: {
-								siteId,
-								postId,
-								scope,
-								posts: ( response && response.posts ) || [],
-							},
-						} );
-					} );
-				},
-				( err ) => {
-					dispatch( {
-						type: READER_RELATED_POSTS_REQUEST_FAILURE,
-						payload: { siteId, postId, scope, error: err },
-						error: true,
-					} );
-
+				}
+				// collect posts and dispatch
+				dispatch( receivePosts( response && response.posts ) ).then( () => {
 					dispatch( {
 						type: READER_RELATED_POSTS_RECEIVE,
 						payload: {
 							siteId,
 							postId,
 							scope,
-							posts: [],
+							posts: ( response && response.posts ) || [],
 						},
 					} );
-				}
-			);
+				} );
+			},
+			( err ) => {
+				dispatch( {
+					type: READER_RELATED_POSTS_REQUEST_FAILURE,
+					payload: { siteId, postId, scope, error: err },
+					error: true,
+				} );
+
+				dispatch( {
+					type: READER_RELATED_POSTS_RECEIVE,
+					payload: {
+						siteId,
+						postId,
+						scope,
+						posts: [],
+					},
+				} );
+			}
+		);
 	};
 }
