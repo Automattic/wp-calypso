16dbd252640ddaf5602883a3b2f221059837b0b3
retrofox <rdsuarez@gmail.com>
2021-12-01 09:29:24 -0300

    woocommerce: remove site plan hanlding

--- client/my-sites/woocommerce/woop/landing-page.tsx
+++ client/my-sites/woocommerce/woop/landing-page.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@automattic/components';
 import { useRef } from '@wordpress/element';
-import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import './style.scss';
 import Image01 from 'calypso/assets/images/woocommerce/woop-cta-image01.jpeg';
@@ -10,72 +9,30 @@ import Image04 from 'calypso/assets/images/woocommerce/woop-cta-image04.jpeg';
 import CtaSection from 'calypso/components/cta-section';
 import FixedNavigationHeader from 'calypso/components/fixed-navigation-header';
 import MasonryWave from 'calypso/components/masonry-wave';
-import { addQueryArgs } from 'calypso/lib/url';
 
 interface Props {
 	startSetup: () => void;
-	isFeatureActive: boolean;
-	upgradingPlan: Record< string, unknown >;
-	siteSlug: string;
 }
 
 const images = [ { src: Image01 }, { src: Image02 }, { src: Image03 }, { src: Image04 } ];
 
-const WoopLandingPage: React.FunctionComponent< Props > = ( {
-	startSetup,
-	siteSlug,
-	isFeatureActive,
-	upgradingPlan,
-} ) => {
+const WoopLandingPage: React.FunctionComponent< Props > = ( { startSetup } ) => {
 	const { __ } = useI18n();
 	const navigationItems = [ { label: 'WooCommerce', href: `/woocommerce-installation` } ];
 	const ctaRef = useRef( null );
 
-	function onCTAHandler() {
-		if ( ! isFeatureActive && upgradingPlan?.product_slug ) {
-			return ( window.location.href = addQueryArgs(
-				{ redirect_to: window.location.href },
-				`/checkout/${ siteSlug }/${ upgradingPlan.product_slug }`
-			) );
-		}
-
-		startSetup();
-	}
-
-	const upgradeButtonText = upgradingPlan.product_name
-		? sprintf(
-				/* translators: %s: The upgrading plan name (ex.: WordPress.com Business) */
-				__( 'Upgrade to %s' ),
-				upgradingPlan.product_name
-		  )
-		: __( 'Upgrade' );
-
-	let headlineText;
-
-	if ( isFeatureActive ) {
-		headlineText = __( 'Build exactly the eCommerce website you want.' );
-	} else {
-		headlineText = upgradingPlan.product_name
-			? sprintf(
-					/* translators: %s: The upgrading plan name (ex.: WordPress.com Business) */
-					__( 'Upgrade to the %s plan and set up your WooCommerce store.' ),
-					upgradingPlan.product_name
-			  )
-			: __( 'Upgrade to set up your WooCommerce store.' );
-	}
-
 	return (
 		<div className="woop__landing-page">
 			<FixedNavigationHeader navigationItems={ navigationItems } contentRef={ ctaRef }>
-				<Button onClick={ onCTAHandler } primary>
-					{ isFeatureActive ? __( 'Set up my store!' ) : upgradeButtonText }
+				<Button onClick={ startSetup } primary>
+					{ __( 'Set up my store!' ) }
 				</Button>
 			</FixedNavigationHeader>
 			<CtaSection
 				title={ __( 'Have something to sell?' ) }
-				headline={ headlineText }
-				buttonText={ isFeatureActive ? __( 'Set up my store!' ) : upgradeButtonText }
-				buttonAction={ onCTAHandler }
+				headline={ __( 'Build exactly the eCommerce website you want.' ) }
+				buttonText={ __( 'Set up my store!' ) }
+				buttonAction={ startSetup }
 				ctaRef={ ctaRef }
 			>
 				<MasonryWave images={ images } />
