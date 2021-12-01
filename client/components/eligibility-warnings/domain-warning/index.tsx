258d37664da01f5a2bd886b7a1a4afee7a07562d
retrofox <rdsuarez@gmail.com>
2021-12-01 18:16:40 -0300

    components: translate DomainEligibilityWarning description

--- client/components/eligibility-warnings/domain-warning/index.tsx
+++ client/components/eligibility-warnings/domain-warning/index.tsx
@@ -1,4 +1,4 @@
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { ReactElement } from 'react';
 import Card from '../card';
 import InfoLabel from '../info-label';
@@ -15,7 +15,13 @@ const DomainEligibilityWarning = ( {
 	<Card title={ __( 'New Store Domain' ) }>
 		<InfoLabel label={ __( 'New' ) }>{ stagingDomain }</InfoLabel>
 		<p>
-			{ `By installing this product your subdomain will change. Your old subdomain (${ wpcomDomain }) will no longer work. You can change it to a custom domain on us at anytime in future.` }
+			{ sprintf(
+				/* translators: %s: The wordpress domain (ex.: myawesomeblog.wordpress.com) */
+				__(
+					'By installing this product your subdomain will change. Your old subdomain (%s) will no longer work. You can change it to a custom domain on us at anytime in future.'
+				),
+				wpcomDomain
+			) }
 		</p>
 	</Card>
 );
