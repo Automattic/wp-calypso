46af886f9022e3caa906c4efdff657328618ed80
Kirk Wight <kwight@kwight.ca>
2021-12-01 11:00:24 -0800

    Videos UI: Record a Tracks event for every video marked as completed. (#58691)
    * Videos UI: Record a Tracks event for every video marked as completed.
    
    * Reset addTimeUpdateHandler when course_slug or video_slug changed
    
    * Cleaning up useEffects relating to course progressiong
    
    Co-authored-by: Andres Blanco <andresblanco@gmail.com>
    Co-authored-by: Rosalind W <blackjacksoftwaredev@gmail.com>

--- client/components/videos-ui/video-player.jsx
+++ client/components/videos-ui/video-player.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 
-const VideoPlayer = ( { completedSeconds, videoRef, videoUrl, isPlaying, poster = undefined } ) => {
+const VideoPlayer = ( { videoData, videoRef, isPlaying, course } ) => {
 	const [ addTimeUpdateHandler, setAddTimeUpdateHandler ] = useState( true );
 
 	const markVideoAsComplete = () => {
-		if ( videoRef.current.currentTime < completedSeconds ) {
+		if ( videoRef.current.currentTime < videoData.completed_seconds ) {
 			return;
 		}
+		recordTracksEvent( 'calypso_courses_video_completed', {
+			course: course.slug,
+			video: videoData.slug,
+		} );
 		setAddTimeUpdateHandler( false );
 	};
 
@@ -16,16 +21,20 @@ const VideoPlayer = ( { completedSeconds, videoRef, videoUrl, isPlaying, poster 
 		}
 	} );
 
+	useEffect( () => {
+		setAddTimeUpdateHandler( true );
+	}, [ course?.slug, videoData?.slug ] );
+
 	return (
-		<div key={ videoUrl } className="videos-ui__video">
+		<div key={ videoData.url } className="videos-ui__video">
 			<video
 				controls
 				ref={ videoRef }
-				poster={ poster }
+				poster={ videoData.poster }
 				autoPlay={ isPlaying }
 				onTimeUpdate={ addTimeUpdateHandler ? markVideoAsComplete : undefined }
 			>
-				<source src={ videoUrl } />{ ' ' }
+				<source src={ videoData.url } />{ ' ' }
 				{ /* @TODO: check if tracks are available, the linter demands one */ }
 				<track src="caption.vtt" kind="captions" srcLang="en" label="english_captions" />
 			</video>
