ce15f8e656213406d52a33ae8fcfe2b54b387936
Damián Suárez <rdsuarez@gmail.com>
2021-12-01 07:21:07 -0300

    Woop Installer: Add checkout step (#58602)
    * woop: add custom useEligibility hook
    
    * woop: rename hook prop to isFetching
    
    * woop: add plugins and widgets warning to hook
    
    * woop: do not filter eligibility warnings
    
    * woop: handle transferring blocker in the hook
    
    * woop: update confirm using useEligibility hook
    
    * woop: add siteNeedUpgrade prop to hook
    
    * woop: woo installer redirects to landing page when no proper plan
    
    * woop: minor doc improvement
    
    * woop: rename to useWoopHandling()
    
    * woop: update siteUpgrading prop in the hook
    
    * woop: update confirm step with new hook API
    
    * woop: fill useWoopHandling hook with plan datat commit
    
    * woop: add checkout plan subset into confirm
    
    * woop: add siteSlug to checkout redirect_to
    
    * woop: clean plugins and widgets warning
    
    * woop: fix typo - rename hook
    
    * woop: rename initial confirm sub step to `info`

--- client/signup/steps/woocommerce-install/confirm/index.tsx
+++ client/signup/steps/woocommerce-install/confirm/index.tsx
@@ -1,23 +1,14 @@
 import { NextButton } from '@automattic/onboarding';
 import { createInterpolateElement } from '@wordpress/element';
 import { useI18n } from '@wordpress/react-i18n';
-import { ReactElement, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { ReactElement } from 'react';
+import { useSelector } from 'react-redux';
 import { default as HoldList } from 'calypso/blocks/eligibility-warnings/hold-list';
 import WarningList from 'calypso/blocks/eligibility-warnings/warning-list';
 import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
 import StepWrapper from 'calypso/signup/step-wrapper';
-import {
-	fetchAutomatedTransferStatusOnce,
-	requestEligibility,
-} from 'calypso/state/automated-transfer/actions';
-import {
-	isFetchingAutomatedTransferStatus,
-	getEligibility,
-	EligibilityData,
-} from 'calypso/state/automated-transfer/selectors';
-import { getSiteSlug } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import useWooCommerceOnPlansEligibility from '../hooks/use-woop-handling';
 import type { WooCommerceInstallProps } from '../';
 
 import './style.scss';
@@ -25,49 +16,28 @@ import './style.scss';
 export default function Confirm( props: WooCommerceInstallProps ): ReactElement | null {
 	const { goToStep, isReskinned, stepSectionName, headerTitle, headerDescription } = props;
 	const { __ } = useI18n();
-	const dispatch = useDispatch();
 
 	// selectedSiteId is set by the controller whenever site is provided as a query param.
 	const siteId = useSelector( getSelectedSiteId ) as number;
 
-	// Request eligibility data.
-	useEffect( () => {
-		if ( ! siteId ) {
-			return;
-		}
-		dispatch( fetchAutomatedTransferStatusOnce( siteId ) );
-		dispatch( requestEligibility( siteId ) );
-	}, [ siteId, dispatch ] );
-
-	// Check whether it's requesting eligibility data.
-	const isFetchingTransferStatus = !! useSelector( ( state ) =>
-		isFetchingAutomatedTransferStatus( state, siteId )
-	);
-
-	// Get eligibility data.
 	const {
+		isFetching,
+		wpcomDomain,
+		stagingDomain,
 		eligibilityHolds,
-		eligibilityWarnings: allEligibilityWarnings,
-	}: EligibilityData = useSelector( ( state ) => getEligibility( state, siteId ) );
+		eligibilityWarnings,
+		wpcomSubdomainWarning,
+		siteUpgrading,
+	} = useWooCommerceOnPlansEligibility( siteId );
 
-	// Check whether the wpcom.com subdomain warning is present.
-	const wordPressSubdomainWarning = allEligibilityWarnings?.find(
-		( { id } ) => id === 'wordpress_subdomain'
-	);
+	const isLoading = ! siteId || isFetching;
 
-	const eligibilityWarnings = allEligibilityWarnings?.filter(
-		( { id } ) => id !== 'wordpress_subdomain'
-	);
-
-	// Pick the wpcom subdomain.
-	const wpcomDomain = useSelector( ( state ) => getSiteSlug( state, siteId ) );
-
-	const isLoading = ! siteId || isFetchingTransferStatus;
+	const backUrl =
+		stepSectionName === 'info' || typeof stepSectionName === 'undefined'
+			? `/woocommerce-installation/${ wpcomDomain }`
+			: 'info';
 
 	function getWPComSubdomainWarningContent() {
-		// Get staging sudomain based on the wpcom subdomain.
-		const stagingDomain = wpcomDomain?.replace( /\b\.wordpress\.com/, '.wpcomstaging.com' );
-
 		return (
 			<>
 				<div className="confirm__info-section">{ isLoading && <LoadingEllipsis /> }</div>
@@ -94,11 +64,7 @@ export default function Confirm( props: WooCommerceInstallProps ): ReactElement 
 					<NextButton
 						disabled={ isLoading }
 						onClick={ () => {
-							if ( eligibilityHolds?.length || eligibilityWarnings?.length ) {
-								return goToStep( 'confirm', 'eligibility_substep' );
-							}
-
-							return goToStep( 'transfer' );
+							return goToStep( 'confirm', 'plan' );
 						} }
 					>
 						{ __( 'Sounds good' ) }
@@ -108,13 +74,41 @@ export default function Confirm( props: WooCommerceInstallProps ): ReactElement 
 		);
 	}
 
+	function getCheckoutContent() {
+		return (
+			<>
+				<div className="confirm__info-section">{ isLoading && <LoadingEllipsis /> }</div>
+
+				<div className="confirm__instructions-container">
+					<div className="confirm__instructions-title">{ __( 'Upgrading is required' ) }</div>
+
+					<p>{ siteUpgrading.description }</p>
+
+					<NextButton
+						disabled={ isLoading }
+						onClick={ () => {
+							window.location.href = siteUpgrading.checkoutUrl;
+						} }
+					>
+						{ siteUpgrading.checkoutText }
+					</NextButton>
+				</div>
+			</>
+		);
+	}
+
 	function getContent() {
 		// wpcom subdomain warning.
 		if (
-			wordPressSubdomainWarning &&
-			( stepSectionName === 'wpcom_subdomain_substep' || typeof stepSectionName === 'undefined' )
+			wpcomSubdomainWarning &&
+			( stepSectionName === 'info' || typeof stepSectionName === 'undefined' )
 		) {
 			return getWPComSubdomainWarningContent();
+		}
+
+		// Check/confirm checkout site plan.
+		if ( siteUpgrading.required && stepSectionName === 'plan' ) {
+			return getCheckoutContent();
 		}
 
 		return (
@@ -145,7 +139,7 @@ export default function Confirm( props: WooCommerceInstallProps ): ReactElement 
 			hideSkip={ true }
 			nextLabelText={ __( 'Confirm' ) }
 			allowBackFirstStep={ true }
-			backUrl={ `/woocommerce-installation/${ wpcomDomain }` }
+			backUrl={ backUrl }
 			headerText={ headerTitle }
 			fallbackHeaderText={ headerTitle }
 			subHeaderText={ headerDescription }
---
+++ client/signup/steps/woocommerce-install/hooks/use-woop-handling/index.ts
@@ -0,0 +1,150 @@
+import { FEATURE_WOOP } from '@automattic/calypso-products';
+import { sprintf, __ } from '@wordpress/i18n';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addQueryArgs } from 'calypso/lib/url';
+import {
+	fetchAutomatedTransferStatusOnce,
+	requestEligibility,
+} from 'calypso/state/automated-transfer/actions';
+import { eligibilityHolds as eligibilityHoldsConstants } from 'calypso/state/automated-transfer/constants';
+import {
+	isFetchingAutomatedTransferStatus,
+	getEligibility,
+	EligibilityData,
+	EligibilityWarning,
+} from 'calypso/state/automated-transfer/selectors';
+import { requestProductsList } from 'calypso/state/products-list/actions';
+import { getProductBySlug } from 'calypso/state/products-list/selectors';
+import hasActiveSiteFeature from 'calypso/state/selectors/has-active-site-feature';
+import hasAvailableSiteFeature from 'calypso/state/selectors/has-available-site-feature';
+import { getSiteDomain } from 'calypso/state/sites/selectors';
+
+const TRANSFERRING_NOT_BLOCKERS = [
+	eligibilityHoldsConstants.NO_BUSINESS_PLAN, // let's redirect to checkout page
+	eligibilityHoldsConstants.TRANSFER_ALREADY_EXISTS, // let's handle it in transferring endpoint.
+];
+
+type EligibilityHook = {
+	eligibilityHolds?: string[];
+	eligibilityWarnings?: EligibilityWarning[];
+	isFetching: boolean;
+	wpcomDomain: string | null;
+	stagingDomain: string | null;
+	wpcomSubdomainWarning: EligibilityWarning | undefined;
+	transferringBlockers: string[];
+	hasBlockers: boolean;
+	siteUpgrading: {
+		required: boolean;
+		checkoutUrl: string;
+		checkoutText: string;
+		productName: string;
+		description: string;
+	};
+};
+
+export default function useEligibility( siteId: number ): EligibilityHook {
+	const dispatch = useDispatch();
+
+	// Request eligibility data.
+	useEffect( () => {
+		if ( ! siteId ) {
+			return;
+		}
+
+		dispatch( fetchAutomatedTransferStatusOnce( siteId ) );
+		dispatch( requestEligibility( siteId ) );
+		dispatch( requestProductsList() );
+	}, [ siteId, dispatch ] );
+
+	// Get eligibility data.
+	const { eligibilityHolds, eligibilityWarnings }: EligibilityData = useSelector( ( state ) =>
+		getEligibility( state, siteId )
+	);
+
+	// Pick the wpcom subdomain.
+	const wpcomDomain = useSelector( ( state ) => getSiteDomain( state, siteId ) );
+
+	// Get staging sudomain based on the wpcom subdomain.
+	const stagingDomain = wpcomDomain?.replace( /\b\.wordpress\.com/, '.wpcomstaging.com' ) || null;
+
+	// Check whether it's requesting eligibility data.
+	const isFetching = !! useSelector( ( state ) =>
+		isFetchingAutomatedTransferStatus( state, siteId )
+	);
+
+	// Check whether the wpcom.com subdomain warning is present.
+	const wpcomSubdomainWarning = eligibilityWarnings?.find(
+		( { id } ) => id === 'wordpress_subdomain'
+	);
+
+	// Transferring blockers
+	const transferringBlockers =
+		eligibilityHolds?.filter( ( hold ) => ! TRANSFERRING_NOT_BLOCKERS.includes( hold ) ) || [];
+
+	/*
+	 * Plan site and `woop` site feature.
+	 * If the eligibility holds contains `NO_BUSINESS_PLAN`,
+	 * if the site doesn't have the `woop` feature,
+	 * and if the site has the `woop` feature upgradaable,
+	 * then the site needs to be upgraded.
+	 */
+	const eligibilityNoProperPlan = eligibilityHolds?.includes(
+		eligibilityHoldsConstants.NO_BUSINESS_PLAN
+	);
+	const isWoopFeatureActive = useSelector( ( state ) =>
+		hasActiveSiteFeature( state, siteId, FEATURE_WOOP )
+	);
+	const hasWoopFeatureAvailable = useSelector( ( state ) =>
+		hasAvailableSiteFeature( state, siteId, FEATURE_WOOP )
+	);
+
+	const requiresUpgrade = Boolean(
+		eligibilityNoProperPlan && ! isWoopFeatureActive && hasWoopFeatureAvailable
+	);
+
+	/*
+	 * We pick the first plan from the available plans list.
+	 * The priority is defined by the store products list.
+	 */
+	const upgradingPlan =
+		useSelector( ( state ) => getProductBySlug( state, hasWoopFeatureAvailable?.[ 0 ] ) ) || {};
+
+	const productName = upgradingPlan.product_name;
+
+	const siteUpgrading = {
+		required: requiresUpgrade,
+		checkoutUrl: addQueryArgs(
+			{ redirect_to: addQueryArgs( { siteSlug: wpcomDomain }, window.location.href ) },
+			`/checkout/${ wpcomDomain }/${ upgradingPlan.product_slug }`
+		),
+		productName,
+		description: productName
+			? sprintf(
+					/* translators: %s: The upgrading plan name (ex.: WordPress.com Business) */
+					__( 'Upgrade to the %s plan and set up your WooCommerce store.' ),
+					productName
+			  )
+			: __( 'Upgrade to set up your WooCommerce store.' ),
+		checkoutText: productName
+			? sprintf(
+					/* translators: %s: The upgrading plan name (ex.: WordPress.com Business) */
+					__( 'Upgrade to %s' ),
+					productName
+			  )
+			: __( 'Upgrade' ),
+	};
+
+	return {
+		isFetching,
+		eligibilityHolds,
+		eligibilityWarnings,
+		wpcomDomain,
+		stagingDomain,
+		wpcomSubdomainWarning,
+
+		transferringBlockers,
+		hasBlockers: !! transferringBlockers.length,
+		siteUpgrading,
+	};
+}
