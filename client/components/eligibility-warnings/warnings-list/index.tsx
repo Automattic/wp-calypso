933da7a9814aaf0a955293ab47cb8c444baca874
retrofox <rdsuarez@gmail.com>
2021-12-01 16:55:29 -0300

    components: add WarningList eligibility cmp

---
+++ client/components/eligibility-warnings/warnings-list/index.tsx
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+import { ReactElement } from 'react';
+
+const WarningsList = styled.ul`
+	margin: 10px 0 10px 20px;
+`;
+
+const WarningListItem = styled.li`
+	font-size: 0.875rem;
+	letter-spacing: -0.16px;
+	color: var( --studio-gray-60 );
+	line-height: 1.25rem;
+	margin-bottom: 0.75rem;
+`;
+
+const WarningDescription = styled.span``;
+
+const WarningTitle = styled.span`
+	font-weight: bold;
+`;
+
+type WarningListProps = {
+	warnings: Array< {
+		name: string;
+		description: string;
+	} >;
+};
+
+const WarningList = ( { warnings }: WarningListProps ): ReactElement => (
+	<WarningsList>
+		{ warnings?.map( ( { name, description }, index ) => (
+			<WarningListItem key={ index }>
+				<WarningTitle>{ name }</WarningTitle>
+				:&nbsp;
+				<WarningDescription>{ description }</WarningDescription>
+			</WarningListItem>
+		) ) }
+	</WarningsList>
+);
+
+export default WarningList;
