2439248f6c2ca31488af20445d4856c9b7a0eebe
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-12-02 10:49:12 -0800

    When OS is unknow show a learn more link.

--- client/my-sites/customer-home/cards/tasks/site-setup-list/mobile-app-download.jsx
+++ client/my-sites/customer-home/cards/tasks/site-setup-list/mobile-app-download.jsx
@@ -1,3 +1,4 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { translate } from 'i18n-calypso';
 import AppsBadge from 'calypso/blocks/get-apps/apps-badge';
 import userAgent from 'calypso/lib/user-agent';
@@ -5,10 +6,31 @@ import userAgent from 'calypso/lib/user-agent';
 const MobileAppDownload = () => {
 	const { isiPad, isiPod, isiPhone, isAndroid } = userAgent;
 	const isIos = isiPad || isiPod || isiPhone;
-	const showIosBadge = ! isAndroid;
-	const showAndroidBadge = ! isIos;
 
-	const utm_source = 'calypso-customer-home-site-setup';
+	const showBadge = ( isIosDevice, isAndroidDevice ) => {
+		const utm_source = 'calypso-customer-home-site-setup';
+
+		if ( isIosDevice ) {
+			return <AppsBadge storeName={ 'ios' } utm_source={ utm_source }></AppsBadge>;
+		}
+		if ( isAndroidDevice ) {
+			return <AppsBadge storeName={ 'android' } utm_source={ utm_source }></AppsBadge>;
+		}
+
+		return (
+			<a
+				href="https://apps.wordpress.com/mobile"
+				onClick={ () => {
+					recordTracksEvent( 'calypso_mobile_app_download_link_click_learn_more' );
+				} }
+				rel="noopener noreferrer"
+				className="mobile-app-download__link"
+				target="_blank"
+			>
+				{ translate( 'Learn more' ) }
+			</a>
+		);
+	};
 
 	return (
 		<div className="mobile-app-download">
@@ -17,10 +39,7 @@ const MobileAppDownload = () => {
 				<p className="mobile-app-download__description">
 					{ translate( 'Make updates on the go.' ) }
 				</p>
-				{ showIosBadge && <AppsBadge storeName={ 'ios' } utm_source={ utm_source }></AppsBadge> }
-				{ showAndroidBadge && (
-					<AppsBadge storeName={ 'android' } utm_source={ utm_source }></AppsBadge>
-				) }
+				{ showBadge( isIos, isAndroid ) }
 			</div>
 		</div>
 	);
