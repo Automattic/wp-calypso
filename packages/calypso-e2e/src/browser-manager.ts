7125ab4d0b17b26165fd779fb7bef54fbf3efd6d
Edwin Takahashi <egao@outlook.com>
2021-12-01 17:03:31 -0800

    Playwright: QoL improvements to the authentication cookie mechanism (#58267)
    * QoL improvements to the authentication cookie mechanism:
    - check whether the cookie file is recent enough (3 days) in `globalSetup`.
    - if the cookie file is recent, skip the authentication & save process.
    - additional check in `BrowserManager.setLoginCookie` to check whether the cookie file found is recent.
    - if the cookie file is not recent, go straight to the traditional username/password login branch.
    
    * Adjust comment location
    
    * Remove errant console.log

--- packages/calypso-e2e/src/browser-manager.ts
+++ packages/calypso-e2e/src/browser-manager.ts
@@ -1,4 +1,4 @@
-import { readFile, access } from 'fs/promises';
+import { readFile, access, stat } from 'fs/promises';
 import path from 'path';
 import config from 'config';
 import { BrowserType } from 'playwright';
@@ -198,6 +198,21 @@ export async function setLoginCookie( page: Page, accountType: string ): Promise
 		await access( cookiePath );
 	} catch {
 		throw new Error( `Cookie file ${ cookiePath } not found on disk.` );
+	}
+
+	/*
+	Check the creation time of the cookie.
+	WordPress.com cookies are valid for a limited time, typically less than 3 days.
+	So if a cookie file is found, but the creation time is older than 3 days, pretend that
+	a cookie file did not exist.
+	This is only an issue when running the tests locally.
+	*/
+	const stats = await stat( cookiePath );
+	const createdTime = Math.trunc( stats.birthtimeMs );
+	const currentTime = new Date();
+
+	if ( createdTime < currentTime.setDate( currentTime.getDate() - 3 ) ) {
+		throw new Error( `Cookie file is stale: created time is ${ stats.birthtime }` );
 	}
 
 	const cookie = JSON.parse( await readFile( cookiePath, 'utf8' ) );
