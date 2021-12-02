6f5247a6bdc292ef243f04b4e5cc74fd2f79d10b
Marin Atanasov <tyxla@abv.bg>
2021-12-02 18:28:19 +0200

    Remove isRequestingPlans check

--- client/components/data/query-plans/index.jsx
+++ client/components/data/query-plans/index.jsx
@@ -1,19 +1,12 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { requestPlans } from 'calypso/state/plans/actions';
-import { isRequestingPlans } from 'calypso/state/plans/selectors';
-
-const request = () => ( dispatch, getState ) => {
-	if ( ! isRequestingPlans( getState() ) ) {
-		dispatch( requestPlans() );
-	}
-};
 
 export default function QueryPlans() {
 	const dispatch = useDispatch();
 
 	useEffect( () => {
-		dispatch( request() );
+		dispatch( requestPlans() );
 	}, [ dispatch ] );
 
 	return null;
