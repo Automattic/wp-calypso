393195d2c380865bf584084a75adc7f28627892b
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-12-02 16:33:54 -0300

    Remove unused type declaration

--- client/my-sites/domains/domain-management/transfer/transfer-page/types.tsx
+++ client/my-sites/domains/domain-management/transfer/transfer-page/types.tsx
@@ -1,10 +1,8 @@
-import { ResponseDomain } from 'calypso/lib/domains/types';
 import { SiteData } from 'calypso/state/ui/selectors/site-data';
 import type { NoticeActionCreator } from 'calypso/state/notices/types';
 
 export type TransferPageProps = {
 	currentRoute: string;
-	domains: ResponseDomain[];
 	errorNotice: ( notice: string, options: Record< string, unknown > ) => NoticeActionCreator;
 	isAtomic: boolean;
 	isDomainInfoLoading: boolean;
