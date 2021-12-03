5879b89f68c20c071faea66bec9edd9d29592915
arthur <arthur.chu@automattic.com>
2021-12-03 17:28:00 +0800

    Implement courses footer to handle the completion of courses

--- client/signup/steps/courses/footer.tsx
+++ client/signup/steps/courses/footer.tsx
@@ -1,5 +1,31 @@
-const Footer = () => {
-	return null;
+import { Button } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import React from 'react';
+
+interface Props {
+	isCourseComplete?: boolean;
+	onStartWriting: () => void;
+}
+
+const CoursesFooter: React.FC< Props > = ( { isCourseComplete, onStartWriting } ) => {
+	const translate = useTranslate();
+
+	if ( ! isCourseComplete ) {
+		return null;
+	}
+
+	return (
+		<div className="courses__footer">
+			<div className="courses__footer-content">
+				{ translate(
+					"You did it! Now it's time to put your skills to work and draft your first post."
+				) }
+				<Button className="courses__footer-button" onClick={ onStartWriting }>
+					{ translate( 'Start writing' ) }
+				</Button>
+			</div>
+		</div>
+	);
 };
 
-export default Footer;
+export default CoursesFooter;
