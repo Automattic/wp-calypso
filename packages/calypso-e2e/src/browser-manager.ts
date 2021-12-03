931aca238dff52c27097336e285e718df469be18
Edwin Takahashi <egao@outlook.com>
2021-12-02 17:02:50 -0800

    Force production Chrome channel

--- packages/calypso-e2e/src/browser-manager.ts
+++ packages/calypso-e2e/src/browser-manager.ts
@@ -84,6 +84,7 @@ export async function startBrowser( browserType: BrowserType ): Promise< Browser
 	}
 
 	browser = await browserType.launch( {
+		channel: 'chrome',
 		headless: getHeadless(),
 		args: [ '--window-position=0,0' ],
 	} );
