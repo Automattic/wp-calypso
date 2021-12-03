7e1242d3349866059b824c6cea8b06fd32ef8bc3
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-12-02 20:30:27 -0800

    Example component convert to functional component

--- client/components/swipeable/docs/example.jsx
+++ client/components/swipeable/docs/example.jsx
@@ -1,28 +1,24 @@
-import { PureComponent } from 'react';
+import { useState } from 'react';
 import Swipeable from 'calypso/components/swipeable';
 
-export default class extends PureComponent {
-	static displayName = 'Swipeable';
+const SwipeableExample = () => {
+	const [ currentPage, setCurrentPage ] = useState( 0 );
 
-	constructor( props ) {
-		super( props );
-		this.state = { currentPage: 0 };
-	}
+	return (
+		<Swipeable
+			onPageSelect={ ( index ) => {
+				setCurrentPage( index );
+			} }
+			currentPage={ currentPage }
+			pageClassName="example-page-component-class"
+			hasDynamicHeight
+		>
+			<div>Page 1</div>
+			<div>Page 2</div>
+			<div>Page 3</div>
+		</Swipeable>
+	);
+};
 
-	render() {
-		return (
-			<Swipeable
-				onPageSelect={ ( index ) => {
-					this.setState( { currentPage: index } );
-				} }
-				currentPage={ this.state.currentPage }
-				pageClassName="example-page-component-class"
-				hasDynamicHeight
-			>
-				<div>Page 1</div>
-				<div>Page 2</div>
-				<div>Page 3</div>
-			</Swipeable>
-		);
-	}
-}
+SwipeableExample.displayName = 'Swipeable';
+export default SwipeableExample;
