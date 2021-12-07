5a261a8b75788f1ead53d3979fd6d2aa3f5508a4
Andrés Blanco <andres.blanco@automattic.com>
2021-12-07 09:46:07 -0300

    Videos UI: use `duration_seconds` (#58863)
    * Use
    
    * import order
    
    * syntax
    
    * linting
    
    * more linting

--- client/components/videos-ui/index.jsx
+++ client/components/videos-ui/index.jsx
@@ -1,6 +1,7 @@
 import { Button, Gridicon } from '@automattic/components';
 import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
+import moment from 'moment';
 import { cloneElement, useEffect, useState } from 'react';
 import useCourseQuery from 'calypso/data/courses/use-course-query';
 import useUpdateUserCourseProgressionMutation from 'calypso/data/courses/use-update-user-course-progression-mutation';
@@ -150,7 +151,9 @@ const VideosUi = ( { headerBar, footerBar } ) => {
 											<span className="videos-ui__video-title">
 												{ i + 1 }. { videoInfo.title }{ ' ' }
 											</span>
-											<span className="videos-ui__duration"> { videoInfo.duration } </span>{ ' ' }
+											<span className="videos-ui__duration">
+												{ moment.unix( videoInfo.duration_seconds ).format( 'm:ss' ) }
+											</span>
 											{ isVideoCompleted && (
 												<span className="videos-ui__completed-checkmark">
 													<Gridicon icon="checkmark" size={ 12 } />
