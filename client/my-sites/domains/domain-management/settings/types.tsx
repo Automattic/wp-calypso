6bc6750ba3b90ee5323431855531f060ebd82715
Igor Giussani <gius80@gmail.com>
2021-12-08 09:10:11 +0100

    Fix small typescript issues

--- client/my-sites/domains/domain-management/settings/types.tsx
+++ client/my-sites/domains/domain-management/settings/types.tsx
@@ -3,6 +3,6 @@ import { SiteData } from 'calypso/state/ui/selectors/site-data';
 export type SettingsPageProps = {
 	currentRoute: string;
 	selectedDomainName: string;
-	selectedSite: SiteData | null;
-	hasDomainOnlySite: boolean | null;
+	selectedSite: SiteData;
+	hasDomainOnlySite: boolean;
 };
