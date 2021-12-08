aac352cff49954092b681e93495e57dfd058fff4
Del Putnam <del@putnams.net>
2021-12-08 15:17:56 -0500

    Add availability message for reserved domain names.

--- client/lib/domains/registration/availability-messages.js
+++ client/lib/domains/registration/availability-messages.js
@@ -366,6 +366,19 @@ function getAvailabilityNotice( domain, error, errorData ) {
 			);
 			break;
 
+		case domainAvailability.AVAILABLE_RESERVED:
+			message = translate(
+				'Sorry, {{strong}}%(domain)s{{/strong}} is reserved by the .%(tld)s registry and cannot be registered without permission.',
+				{
+					args: { domain, tld },
+					components: {
+						strong: <strong />,
+						a: <a rel="noopener noreferrer" href={ domainMapping( site, domain ) } />,
+					},
+				}
+			);
+			break;
+
 		case domainAvailability.TRANSFERRABLE_PREMIUM:
 			message = translate(
 				"Sorry, {{strong}}%(domain)s{{/strong}} is a premium domain. We don't support transfers of premium domains on WordPress.com, but if you are the owner of this domain, you can {{a}}map it to your site{{/a}}.",
