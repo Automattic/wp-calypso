831c996cf95ebf8deb24e3ff39a120e55a4b105e
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-12-08 14:25:28 -0600

    Remove FRESHPACK auto-apply logic

--- client/my-sites/checkout/composite-checkout/hooks/use-maybe-jetpack-intro-coupon-code.ts
+++ client/my-sites/checkout/composite-checkout/hooks/use-maybe-jetpack-intro-coupon-code.ts
@@ -1,18 +1,13 @@
-import {
-	isJetpackProductSlug,
-	isJetpackPlanSlug,
-	isJetpackSearch,
-} from '@automattic/calypso-products';
+import { isJetpackSearch } from '@automattic/calypso-products';
 import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { getJetpackSaleCoupon } from 'calypso/state/marketing/selectors';
 import type { MinimalRequestCartProduct } from '@automattic/shopping-cart';
 
 const JETPACK_SEARCH_INTRO_COUPON_CODE = 'FIRSTYEAR50';
-const JETPACK_INTRO_COUPON_CODE = 'FRESHPACK';
 
-const isYearlySearch = ( product: RequestCartProduct ) =>
-	isJetpackSearch( product ) && ! product.product_slug.endsWith( '_monthly' );
+const isYearly = ( product: MinimalRequestCartProduct ) =>
+	! product.product_slug.endsWith( '_monthly' );
+const isNewPurchase = ( product: MinimalRequestCartProduct ) =>
+	product.extra?.purchaseType === 'renewal';
 
 // **NOTE**: This hook can be safely deleted when we no longer need to
 // rely on auto-applied coupons for introductory new purchase pricing.
@@ -20,44 +15,22 @@ const useMaybeJetpackIntroCouponCode = (
 	products: MinimalRequestCartProduct[],
 	isCouponApplied: boolean
 ): string | undefined => {
-	const jetpackSaleCoupon = useSelector( getJetpackSaleCoupon );
 	return useMemo( () => {
+		// If a coupon is already applied to this cart, don't try to apply another one.
 		if ( isCouponApplied ) {
 			return undefined;
 		}
-		const jetpackProducts = products.filter(
-			( product ) =>
-				isJetpackProductSlug( product.product_slug ) || isJetpackPlanSlug( product.product_slug )
+
+		const isEligibleForFirstYear50 = products.filter(
+			( product ) => isNewPurchase( product ) && isYearly( product ) && isJetpackSearch( product )
 		);
 
-		// Only apply FRESHPACK if there's a Jetpack
-		// product or plan present in the cart
-		if ( jetpackProducts.length < 1 ) {
-			return undefined;
-		}
-
-		// Only apply FRESHPACK for new purchases, not renewals.
-		if ( jetpackProducts.some( ( product ) => product.extra?.purchaseType === 'renewal' ) ) {
-			return undefined;
-		}
-
-		// Only apply FRESHPACK to monthly products if a sale is running
-		if (
-			jetpackSaleCoupon &&
-			! jetpackProducts.some( ( product ) => product.product_slug.endsWith( '_monthly' ) )
-		) {
-			return undefined;
-		}
-
-		// 2021-12-08: Tiered products like Search don't currently support introductory offers,
-		// so we simulate a 50%-off first year discount by auto-applying a coupon.
-		// This only applies to new subscriptions with yearly billing terms.
-		if ( jetpackProducts.some( isYearlySearch ) ) {
+		if ( isEligibleForFirstYear50 ) {
 			return JETPACK_SEARCH_INTRO_COUPON_CODE;
 		}
 
-		return JETPACK_INTRO_COUPON_CODE;
-	}, [ products, isCouponApplied, jetpackSaleCoupon ] );
+		return undefined;
+	}, [ products, isCouponApplied ] );
 };
 
 export default useMaybeJetpackIntroCouponCode;
