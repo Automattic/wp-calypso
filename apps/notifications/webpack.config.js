18552e50b3eb44a94e643d0c455587f11c39aac7
Sérgio Gomes <mail@sgomes.com>
2021-12-08 12:28:46 +0000

    Use Preact in standalone notifications
    Standalone notifications are meant to be a widget loaded on every page
    view for logged-in users, and as such critical to optimise.
    
    Preact is a much better fit for this scenario than React, as it's much
    smaller, while essentially being a drop-in replacement.
    
    This change replaces React with Preact for the notifications build,
    through build-time aliasing in webpack and Babel.

--- apps/notifications/webpack.config.js
+++ apps/notifications/webpack.config.js
@@ -65,6 +65,15 @@ function getWebpackConfig(
 			},
 			watchFiles: [ 'dist/**/*' ],
 		},
+		resolve: {
+			...webpackConfig.resolve,
+			alias: {
+				react: 'preact/compat',
+				'react-dom/test-utils': 'preact/test-utils',
+				'react-dom': 'preact/compat',
+				'react/jsx-runtime': 'preact/jsx-runtime',
+			},
+		},
 		plugins: [
 			...webpackConfig.plugins,
 			new HtmlWebpackPlugin( {
