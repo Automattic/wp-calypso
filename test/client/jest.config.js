99120ea5cf41e65e76ee10c46f2821bab3925195
Sergio Cinos <sergio.cinos@gmail.com>
2021-12-08 09:41:37 +0100

    chore: Extract jest config from calypso-build to calypso-jest (#58905)
    * Extract jest config from calypso-build to calypso-jest
    
    * Updated docs

--- test/client/jest.config.js
+++ test/client/jest.config.js
@@ -6,13 +6,13 @@ module.exports = {
 	transform: {
 		'\\.[jt]sx?$': 'babel-jest',
 		'\\.(gif|jpg|jpeg|png|svg|scss|sass|css)$': require.resolve(
-			'@automattic/calypso-build/jest/transform/asset.js'
+			'@automattic/calypso-jest/src/transform/asset.js'
 		),
 	},
 	modulePaths: [ '<rootDir>/extensions' ],
 	moduleDirectories: [ 'node_modules', '<rootDir>/test-helpers/' ],
 	rootDir: '../../client',
-	resolver: '<rootDir>../test/module-resolver.js',
+	resolver: require.resolve( '@automattic/calypso-jest/src/module-resolver.js' ),
 	testEnvironment: 'node',
 	transformIgnorePatterns: [
 		'node_modules[\\/\\\\](?!flag-icon-css|simple-html-tokenizer|draft-js|social-logos|gridicons|calypso)',
