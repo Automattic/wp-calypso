f9ca20ecd59e4fdc1edc8dc2e0308b449ac5116f
Jarda Snajdr <jsnajdr@gmail.com>
2021-12-08 14:46:31 +0100

    Migrate wpcom.undocumented().launchSite() to wpcom.req

--- client/lib/signup/step-actions/index.js
+++ client/lib/signup/step-actions/index.js
@@ -530,15 +530,10 @@ function addPrivacyProtectionIfSupported( item, state ) {
 export function launchSiteApi( callback, dependencies ) {
 	const { siteSlug } = dependencies;
 
-	wpcom.undocumented().launchSite( siteSlug, function ( error ) {
-		if ( error ) {
-			callback( error );
-
-			return;
-		}
-
-		callback();
-	} );
+	wpcom.req
+		.post( `/sites/${ siteSlug }/launch` )
+		.then( () => callback() )
+		.catch( ( error ) => callback( error ) );
 }
 
 export function createAccount(
