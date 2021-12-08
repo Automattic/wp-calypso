eff0a4185ec2205d8ac3eb64e6af620c69c8e06b
Payton Swick <payton@foolord.com>
2021-12-08 16:38:31 -0500

    Clarify types in usePrepareProductsForCart

--- client/my-sites/checkout/composite-checkout/lib/get-cart-from-local-storage.ts
+++ client/my-sites/checkout/composite-checkout/lib/get-cart-from-local-storage.ts
@@ -1,8 +1,8 @@
-import type { RequestCartProduct } from '@automattic/shopping-cart';
+import { IncompleteRequestCartProduct } from 'calypso/lib/cart-values/cart-items';
 
 // Used by signup; see https://github.com/Automattic/wp-calypso/pull/44206
 // These products are likely missing product_id.
-export default function getCartFromLocalStorage(): Partial< RequestCartProduct >[] {
+export default function getCartFromLocalStorage(): IncompleteRequestCartProduct[] {
 	try {
 		return JSON.parse( window.localStorage.getItem( 'shoppingCart' ) || '[]' );
 	} catch ( err ) {
