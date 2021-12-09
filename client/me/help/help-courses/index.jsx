5a09b616ef151dfae8c864d07fed4f8ba6fa4e35
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-12-09 12:48:54 +0200

    Help: Cleanup courses of unnecessary state (#58857)
    * Help: Remove unnecessary 'fetching' from courses
    
    * Help: Move QueryUserPurchases up top in Courses
    
    * Help: Convert Courses to a functional component
    
    * Help: Remove unnecessary Courses tests
    
    * State: Remove unused help courses state

--- client/me/help/help-courses/index.jsx
+++ client/me/help/help-courses/index.jsx
@@ -1,13 +1,10 @@
 import { isWpComBusinessPlan, isWpComEcommercePlan } from '@automattic/calypso-products';
-import { localize } from 'i18n-calypso';
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useTranslate } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
 import QueryUserPurchases from 'calypso/components/data/query-user-purchases';
 import HeaderCake from 'calypso/components/header-cake';
 import Main from 'calypso/components/main';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
-import { receiveHelpCourses } from 'calypso/state/help/courses/actions';
-import { getHelpCourses } from 'calypso/state/help/courses/selectors';
 import {
 	getUserPurchases,
 	isFetchingUserPurchases,
@@ -18,65 +15,31 @@ import CourseList, { CourseListPlaceholder } from './course-list';
 
 import './style.scss';
 
-class Courses extends Component {
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillMount() {
-		this.fetchCoursesIfNeeded();
-	}
-
-	fetchCoursesIfNeeded() {
-		//TODO: When courses make it into the API we will no longer need this code.
-		//      We can move towards the use of something like <QueryHelpCourses />
-		const { courses, fetchCourses } = this.props;
-
-		if ( courses ) {
-			return;
-		}
-
-		fetchCourses();
-	}
-
-	render() {
-		const { courses, isEligible, isLoading, translate } = this.props;
-
-		return (
-			<Main className="help-courses">
-				<PageViewTracker path="/help/courses" title="Help > Courses" />
-				<HeaderCake backHref="/help" isCompact={ false } className="help-courses__header-cake">
-					{ translate( 'Courses' ) }
-				</HeaderCake>
-				{ isLoading ? (
-					<CourseListPlaceholder />
-				) : (
-					<CourseList courses={ courses } isBusinessPlanUser={ isEligible } />
-				) }
-
-				<QueryUserPurchases />
-			</Main>
-		);
-	}
-}
-
-export function mapStateToProps( state ) {
-	const purchases = getUserPurchases( state );
+function Courses() {
+	const translate = useTranslate();
+	const purchases = useSelector( getUserPurchases );
 	const purchaseSlugs = purchases && purchases.map( ( purchase ) => purchase.productSlug );
 	const isEligible =
 		purchaseSlugs &&
 		( purchaseSlugs.some( isWpComBusinessPlan ) || purchaseSlugs.some( isWpComEcommercePlan ) );
-	const courses = getHelpCourses( state );
-	const isLoading =
-		isFetchingUserPurchases( state ) || ! courses || ! hasLoadedUserPurchasesFromServer( state );
+	const fetchingUserPurchases = useSelector( isFetchingUserPurchases );
+	const loadedUserPurchases = useSelector( hasLoadedUserPurchasesFromServer );
+	const isLoading = fetchingUserPurchases || ! loadedUserPurchases;
 
-	return {
-		isLoading,
-		isEligible,
-		courses,
-	};
+	return (
+		<Main className="help-courses">
+			<PageViewTracker path="/help/courses" title="Help > Courses" />
+			<QueryUserPurchases />
+			<HeaderCake backHref="/help" isCompact={ false } className="help-courses__header-cake">
+				{ translate( 'Courses' ) }
+			</HeaderCake>
+			{ isLoading ? (
+				<CourseListPlaceholder />
+			) : (
+				<CourseList courses={ helpCourses } isBusinessPlanUser={ isEligible } />
+			) }
+		</Main>
+	);
 }
 
-// This function only adds a way of dispatching courses because we don't have another mechanism yet.
-// Once the courses make it into the API this function should go away in preference for
-// something like <QueryHelpCourses />
-const fetchCourses = () => receiveHelpCourses( helpCourses );
-
-export default connect( mapStateToProps, { fetchCourses } )( localize( Courses ) );
+export default Courses;
