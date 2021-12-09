08367fa08437af5ea856b7e4ae8652921dcff4ed
Jeremy Yip <jeremy.yip@automattic.com>
2021-12-09 12:32:44 -0800

    Modify theme template slug if site is atomic

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-universal-themes/index.php
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-universal-themes/index.php
@@ -26,8 +26,12 @@ function is_core_fse_active() {
 		return false;
 	}
 
+	$universal_theme_template = defined( 'IS_ATOMIC' ) && IS_ATOMIC
+		? 'blockase'
+		: 'pub/blockase';
+
 	// For universal themes, we check for our own option.
-	if ( 'pub/blockase' === get_option( 'template' ) ) {
+	if ( get_option( 'template' ) === $universal_theme_template ) {
 		return (bool) get_option( ACTIVATE_FSE_OPTION_NAME );
 	}
 
