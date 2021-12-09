d0fff6da04fdb33dae36aba474054dcab1a37d4f
gcsecsey <gcsecsey@gmail.com>
2021-12-09 15:17:24 +0100

    find products by subscription slugs too

--- client/state/products-list/selectors/is-marketplace-product.js
+++ client/state/products-list/selectors/is-marketplace-product.js
@@ -1,6 +1,10 @@
 import 'calypso/state/products-list/init';
 
+const productSlugToSubscriptionSlug = ( productSlug ) => productSlug.replace( '-', '_' );
+
 export function isMarketplaceProduct( state, productSlug ) {
-	const product = state.productsList?.items?.[ productSlug ];
+	const product =
+		state.productsList?.items?.[ productSlug ] ||
+		state.productsList?.items?.[ productSlugToSubscriptionSlug( productSlug ) ];
 	return Boolean( product ) && product.product_type.startsWith( 'marketplace' );
 }
