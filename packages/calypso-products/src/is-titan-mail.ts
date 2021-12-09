1fe4602d8165228a3be8c6b9e06d448e6a94401d
Dale du Preez <dale@automattic.com>
2021-12-09 19:58:46 +0200

    Fix stale reference

--- packages/calypso-products/src/is-titan-mail.ts
+++ packages/calypso-products/src/is-titan-mail.ts
@@ -1,10 +1,10 @@
 import { camelOrSnakeSlug } from './camel-or-snake-slug';
-import { TITAN_MAIL_MONTHLY_SLUG, TITAN_MAIL_ANNUALLY_SLUG } from './constants';
+import { TITAN_MAIL_MONTHLY_SLUG, TITAN_MAIL_YEARLY_SLUG } from './constants';
 import type { WithCamelCaseSlug, WithSnakeCaseSlug } from './types';
 
 export function isTitanMail( product: WithCamelCaseSlug | WithSnakeCaseSlug ): boolean {
 	return (
 		camelOrSnakeSlug( product ) === TITAN_MAIL_MONTHLY_SLUG ||
-		camelOrSnakeSlug( product ) === TITAN_MAIL_ANNUALLY_SLUG
+		camelOrSnakeSlug( product ) === TITAN_MAIL_YEARLY_SLUG
 	);
 }
