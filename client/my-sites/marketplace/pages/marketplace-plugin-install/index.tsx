119b70dd8e34fdff7f756236940beba34d7de483
cpap <papazoglou.charalampos@gmail.com>
2021-12-10 14:07:01 +0200

    Marketplace: fixes class names.

--- client/my-sites/marketplace/pages/marketplace-plugin-install/index.tsx
+++ client/my-sites/marketplace/pages/marketplace-plugin-install/index.tsx
@@ -313,10 +313,10 @@ const MarketplacePluginInstall = ( {
 			/>
 			{ siteId && <QueryJetpackPlugins siteIds={ [ siteId ] } /> }
 			<Masterbar>
-				<WordPressWordmark className="marketplace-plugin-upload-install__wpcom-wordmark" />
+				<WordPressWordmark className="marketplace-plugin-install__wpcom-wordmark" />
 				<Item>{ translate( 'Plugin Installation' ) }</Item>
 			</Masterbar>
-			<div className="marketplace-plugin-upload-install__root">
+			<div className="marketplace-plugin-install__root">
 				{ renderError() || <MarketplaceProgressBar steps={ steps } currentStep={ currentStep } /> }
 			</div>
 		</ThemeProvider>
