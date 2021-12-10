c2448ed5878d0f2e37d69a41a57f15c5a9fff552
rafaelgalani <galani.rafael@gmail.com>
2021-12-10 14:37:34 -0300

    Add `href` path to email card

--- client/my-sites/domains/domain-management/components/domain/domain-info-card/email/index.tsx
+++ client/my-sites/domains/domain-management/components/domain/domain-info-card/email/index.tsx
@@ -1,6 +1,7 @@
 import { useTranslate } from 'i18n-calypso';
 import { useEmailAccountsQuery } from 'calypso/data/emails/use-emails-query';
 import { getEmailAddress } from 'calypso/lib/emails';
+import { emailManagement } from 'calypso/my-sites/email/paths';
 import DomainInfoCard from '..';
 import { DomainInfoCardProps } from '../types';
 import { EmailAccount } from './types';
@@ -25,6 +26,8 @@ const DomainEmailInfoCard = ( { domain, selectedSite }: DomainInfoCardProps ): J
 
 	return ! emailAddresses.length ? (
 		<DomainInfoCard
+			type="href"
+			href={ emailManagement( selectedSite.slug, domain.name ) }
 			title={ translate( 'Email' ) }
 			description={ translate( 'Send and receive emails from youremail@%(domainName)s', {
 				args: { domainName: domain.name },
@@ -34,6 +37,8 @@ const DomainEmailInfoCard = ( { domain, selectedSite }: DomainInfoCardProps ): J
 		/>
 	) : (
 		<DomainInfoCard
+			type="href"
+			href={ emailManagement( selectedSite.slug, domain.name ) }
 			title={ translate( 'Email' ) }
 			description={ emailAddresses.join( '\n' ) }
 			ctaText={ translate( 'View emails' ) }
