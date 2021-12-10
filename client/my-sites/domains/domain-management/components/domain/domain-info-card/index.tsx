8fe4e1ea93fa3e04ab4b9efb224c8a29d8470fb3
rafaelgalani <galani.rafael@gmail.com>
2021-12-10 00:41:52 -0300

    Add component styles and specific button

--- client/my-sites/domains/domain-management/components/domain/domain-info-card/index.tsx
+++ client/my-sites/domains/domain-management/components/domain/domain-info-card/index.tsx
@@ -1,5 +1,8 @@
+import { Button } from '@automattic/components';
 import ActionCard from 'calypso/components/action-card';
 import type { CardProps, DomainInfoCardProps } from './types';
+
+import './style.scss';
 
 const DomainInfoCard = ( {
 	title,
@@ -18,7 +21,15 @@ const DomainInfoCard = ( {
 		cardProps.buttonPrimary = isPrimary;
 	}
 
-	return <ActionCard { ...cardProps }></ActionCard>;
+	return (
+		<ActionCard { ...cardProps }>
+			{ ctaText && (
+				<Button compact primary={ isPrimary }>
+					{ ctaText }
+				</Button>
+			) }
+		</ActionCard>
+	);
 };
 
 export default DomainInfoCard;
