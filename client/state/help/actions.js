a6aece9dfacd5f7cec8f551d1f8afcfc9e4fc0e1
Marin Atanasov <tyxla@abv.bg>
2021-12-10 13:58:20 +0200

    State: Remove unused help support history action creators

--- client/state/help/actions.js
+++ client/state/help/actions.js
@@ -1,25 +1,8 @@
-import {
-	HELP_CONTACT_FORM_SITE_SELECT,
-	SUPPORT_HISTORY_REQUEST,
-	SUPPORT_HISTORY_SET,
-} from 'calypso/state/action-types';
+import { HELP_CONTACT_FORM_SITE_SELECT } from 'calypso/state/action-types';
 
-// Support History data needs to be disabled temporarily to avoid rate-limiting with our Zendesk API.
-// It is only used for non-essential read-only operations right now.
-// import 'calypso/state/data-layer/wpcom/help/support-history';
 import 'calypso/state/help/init';
 
 export const selectSiteId = ( siteId ) => ( {
 	type: HELP_CONTACT_FORM_SITE_SELECT,
 	siteId,
-} );
-
-export const requestSupportHistory = ( email ) => ( {
-	type: SUPPORT_HISTORY_REQUEST,
-	email,
-} );
-
-export const setSupportHistory = ( items ) => ( {
-	type: SUPPORT_HISTORY_SET,
-	items,
 } );
