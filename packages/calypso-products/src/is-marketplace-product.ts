5b40df7ab58f6a6195fadf21236b019efd4844be
cpap <papazoglou.charalampos@gmail.com>
2021-12-13 17:40:43 +0200

    Rebase fixes.

--- packages/calypso-products/src/is-marketplace-product.ts
+++
@@ -1,4 +0,0 @@
-export function isMarketplaceProduct( productSlug: string ): boolean {
-	// Temporary return specific product until https://github.com/Automattic/wp-calypso/issues/56349 is merged.
-	return productSlug === 'woocommerce_subscriptions_monthly';
-}
