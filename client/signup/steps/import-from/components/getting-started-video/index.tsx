250be9fe181197afbed32407d51344f767e56208
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-12-13 19:17:44 +0100

    Localize paragraph

--- client/signup/steps/import-from/components/getting-started-video/index.tsx
+++ client/signup/steps/import-from/components/getting-started-video/index.tsx
@@ -1,9 +1,12 @@
+import { createElement, createInterpolateElement } from '@wordpress/element';
+import { useI18n } from '@wordpress/react-i18n';
 import React from 'react';
 import ReaderFeaturedVideoBlock from 'calypso/blocks/reader-featured-video';
 
 import './style.scss';
 
 const GettingStartedVideo: React.FunctionComponent = () => {
+	const { __ } = useI18n();
 	const video = {
 		autoplayIframe:
 			'<iframe src="https://www.youtube.com/embed/twGLN4lug-I" width="768px" height="426px" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
@@ -18,7 +21,12 @@ const GettingStartedVideo: React.FunctionComponent = () => {
 	return (
 		<div className="getting-started-video">
 			<p>
-				Watch <strong>Getting started on WordPress.com</strong> while you wait
+				{ createInterpolateElement(
+					__( 'Watch <strong>Getting started on WordPress.com</strong> while you wait' ),
+					{
+						strong: createElement( 'strong' ),
+					}
+				) }
 			</p>
 			<ReaderFeaturedVideoBlock { ...video } videoEmbed={ video } isExpanded={ true } />
 		</div>
