c45fa647610265112c5173efbcbfb92b4e43d012
rafaelgalani <galani.rafael@gmail.com>
2021-12-13 23:41:48 -0300

    Add whois types

--- client/my-sites/domains/domain-management/settings/types.tsx
+++ client/my-sites/domains/domain-management/settings/types.tsx
@@ -2,6 +2,24 @@ import type { ResponseDomain } from 'calypso/lib/domains/types';
 import type { Purchase } from 'calypso/lib/purchases/types';
 import type { SiteData } from 'calypso/state/ui/selectors/site-data';
 
+type WhoisData = {
+	fname: string;
+	lname: string;
+	org: string;
+	email: string;
+	city: string;
+	sp: string;
+	pc: string;
+	cc: string;
+	phone: string;
+	fax: string;
+	sa1: string;
+	sa2: string;
+	country_code: string;
+	state: string;
+	type: string;
+};
+
 export type SettingsPagePassedProps = {
 	currentRoute: string;
 	domains: ResponseDomain[] | null;
@@ -14,6 +32,10 @@ export type SettingsPageConnectedProps = {
 	domain: ResponseDomain;
 	isLoadingPurchase: boolean;
 	purchase: Purchase | null;
+
+	whoisData: WhoisData[];
+
+	requestWhois: ( domain: string ) => void;
 };
 
 export type SettingsHeaderProps = {
