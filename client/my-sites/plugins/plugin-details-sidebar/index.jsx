15a321ffc3f424eb3fcd45b1b1f373776bbccfb3
gcsecsey <gcsecsey@gmail.com>
2021-12-14 19:08:47 +0100

    hide `active_installs` and `tested` fields if there's no data for them

--- client/my-sites/plugins/plugin-details-sidebar/index.jsx
+++ client/my-sites/plugins/plugin-details-sidebar/index.jsx
@@ -3,7 +3,9 @@ import './style.scss';
 import { formatNumberMetric } from 'calypso/lib/format-number-compact';
 
 const PluginDetailsSidebar = ( props ) => {
-	const { plugin } = props;
+	const {
+		plugin: { active_installs, tested },
+	} = props;
 
 	const translate = useTranslate();
 
@@ -13,20 +15,24 @@ const PluginDetailsSidebar = ( props ) => {
 				{ translate( 'Plugin details' ) }
 			</div>
 			<div className="plugin-details-sidebar__plugin-details-content">
-				<div className="plugin-details-sidebar__active-installs">
-					<div className="plugin-details-sidebar__active-installs-text title">
-						{ translate( 'Active installations' ) }
+				{ Boolean( active_installs ) && (
+					<div className="plugin-details-sidebar__active-installs">
+						<div className="plugin-details-sidebar__active-installs-text title">
+							{ translate( 'Active installations' ) }
+						</div>
+						<div className="plugin-details-sidebar__active-installs-value value">
+							{ formatNumberMetric( active_installs, 'en' ) }
+						</div>
 					</div>
-					<div className="plugin-details-sidebar__active-installs-value value">
-						{ formatNumberMetric( plugin.active_installs, 'en' ) }
+				) }
+				{ Boolean( tested ) && (
+					<div className="plugin-details-sidebar__tested">
+						<div className="plugin-details-sidebar__tested-text title">
+							{ translate( 'Tested up to' ) }
+						</div>
+						<div className="plugin-details-sidebar__tested-value value">{ tested }</div>
 					</div>
-				</div>
-				<div className="plugin-details-sidebar__tested">
-					<div className="plugin-details-sidebar__tested-text title">
-						{ translate( 'Tested up to' ) }
-					</div>
-					<div className="plugin-details-sidebar__tested-value value">{ plugin.tested }</div>
-				</div>
+				) }
 			</div>
 		</>
 	);
