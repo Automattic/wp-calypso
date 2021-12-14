f2b4193f6ddb56aae421317a31ee77aec3e6ed5c
Don Lair <don.lair@gmail.com>
2021-12-14 10:54:24 -0500

    Resolving rebase conflict

--- client/signup/steps/user/index.jsx
+++ client/signup/steps/user/index.jsx
@@ -103,53 +103,6 @@ export class UserStep extends Component {
 		isDesktop: isDesktop(),
 	};
 
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillReceiveProps( nextProps ) {
-		if (
-			this.props.flowName !== nextProps.flowName ||
-			this.props.locale !== nextProps.locale ||
-			this.props.subHeaderText !== nextProps.subHeaderText
-		) {
-			this.setSubHeaderText( nextProps );
-		}
-	}
-
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillMount() {
-		const { oauth2Signup, initialContext, flowName } = this.props;
-
-		const clientId = get( initialContext, 'query.oauth2_client_id', null );
-
-		this.setSubHeaderText( this.props );
-
-		if ( oauth2Signup && clientId ) {
-			this.props.fetchOAuth2ClientData( clientId );
-		}
-		const signupFlows = [
-			'onboarding',
-			'free',
-			'personal',
-			'premium',
-			'business',
-			'ecommerce',
-			'with-theme',
-			'personal-monthly',
-			'premium-monthly',
-			'business-monthly',
-			'ecommerce-monthly',
-			'with-design-picker',
-		];
-		if ( signupFlows.includes( flowName ) ) {
-			const experimentCheck = this.state.isDesktop
-				? 'registration_email_only_desktop_random_usernames'
-				: 'registration_email_only_mobile_random_usernames';
-
-			loadExperimentAssignment( experimentCheck ).then( ( experimentName ) => {
-				this.setState( { experiment: experimentName } );
-			} );
-		}
-	}
-
 	componentDidUpdate() {
 		if ( this.userCreationCompletedAndHasHistory( this.props ) ) {
 			// It looks like the user just completed the User Registartion Step
@@ -175,10 +128,24 @@ export class UserStep extends Component {
 			this.props.fetchOAuth2ClientData( clientId );
 		}
 
-		if ( this.props.flowName === 'onboarding' ) {
+		const signupFlows = [
+			'onboarding',
+			'free',
+			'personal',
+			'premium',
+			'business',
+			'ecommerce',
+			'with-theme',
+			'personal-monthly',
+			'premium-monthly',
+			'business-monthly',
+			'ecommerce-monthly',
+			'with-design-picker',
+		];
+		if ( signupFlows.includes( this.props.flowName ) ) {
 			const experimentCheck = this.state.isDesktop
-				? 'registration_email_only_desktop_relaunch'
-				: 'registration_email_only_mobile_relaunch';
+				? 'registration_email_only_desktop_random_usernames'
+				: 'registration_email_only_mobile_random_usernames';
 
 			loadExperimentAssignment( experimentCheck ).then( ( experimentName ) => {
 				this.setState( { experiment: experimentName } );
