3101bf5764be7321193c9d465da3e6e0539738f1
Dean Sas <dean.sas@automattic.com>
2021-12-14 14:47:24 +0000

    Remove unused code from site notices (#59147)
    * Remove unused code from site notices
    
    After moving some sidebar upsell nudges from calypso in #58652 some
    unused code was left over. This change removes that code.

--- client/my-sites/current-site/notice.jsx
+++ client/my-sites/current-site/notice.jsx
@@ -8,33 +8,15 @@ import { connect } from 'react-redux';
 import UpsellNudge from 'calypso/blocks/upsell-nudge';
 import AsyncLoad from 'calypso/components/async-load';
 import QueryActivePromotions from 'calypso/components/data/query-active-promotions';
-import QueryProductsList from 'calypso/components/data/query-products-list';
 import QuerySitePlans from 'calypso/components/data/query-site-plans';
 import Notice from 'calypso/components/notice';
 import NoticeAction from 'calypso/components/notice/notice-action';
 import { domainManagementList } from 'calypso/my-sites/domains/paths';
-import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import { getCurrentUserCurrencyCode } from 'calypso/state/currency-code/selectors';
-import { getTopJITM } from 'calypso/state/jitm/selectors';
-import { savePreference } from 'calypso/state/preferences/actions';
-import { getPreference } from 'calypso/state/preferences/selectors';
-import { getProductsList } from 'calypso/state/products-list/selectors';
-import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
 import getActiveDiscount from 'calypso/state/selectors/get-active-discount';
 import isDomainOnlySite from 'calypso/state/selectors/is-domain-only-site';
-import isEligibleForFreeToPaidUpsell from 'calypso/state/selectors/is-eligible-for-free-to-paid-upsell';
 import isSiteMigrationActiveRoute from 'calypso/state/selectors/is-site-migration-active-route';
 import isSiteMigrationInProgress from 'calypso/state/selectors/is-site-migration-in-progress';
 import isSiteWPForTeams from 'calypso/state/selectors/is-site-wpforteams';
-import { getDomainsBySiteId } from 'calypso/state/sites/domains/selectors';
-import {
-	hasDomainCredit,
-	isCurrentUserCurrentPlanOwner,
-} from 'calypso/state/sites/plans/selectors';
-import { isJetpackSite } from 'calypso/state/sites/selectors';
-import { getSectionName } from 'calypso/state/ui/selectors';
-
-const DOMAIN_UPSELL_NUDGE_DISMISS_KEY = 'domain_upsell_nudge_dismiss';
 
 export class SiteNotice extends Component {
 	static propTypes = {
@@ -125,7 +107,6 @@ export class SiteNotice extends Component {
 
 		return (
 			<div className="current-site__notices">
-				<QueryProductsList />
 				<QueryActivePromotions />
 				{ siteRedirectNotice }
 				{ showJitms && (
@@ -142,51 +123,15 @@ export class SiteNotice extends Component {
 	}
 }
 
-export default connect(
-	( state, ownProps ) => {
-		const siteId = ownProps.site && ownProps.site.ID ? ownProps.site.ID : null;
-		const sectionName = getSectionName( state );
-		const isMigrationInProgress =
-			isSiteMigrationInProgress( state, siteId ) || isSiteMigrationActiveRoute( state );
+export default connect( ( state, ownProps ) => {
+	const siteId = ownProps.site && ownProps.site.ID ? ownProps.site.ID : null;
+	const isMigrationInProgress =
+		isSiteMigrationInProgress( state, siteId ) || isSiteMigrationActiveRoute( state );
 
-		// Avoid passing `messagePath` as a prop to `SiteNotice` as it changes frequently and
-		// thus will cause a lot of re-renders.
-		const messagePath = `calypso:${ sectionName }:sidebar_notice`;
-
-		return {
-			isDomainOnly: isDomainOnlySite( state, siteId ),
-			isEligibleForFreeToPaidUpsell: isEligibleForFreeToPaidUpsell( state, siteId ),
-			isJetpack: isJetpackSite( state, siteId ),
-			activeDiscount: getActiveDiscount( state ),
-			hasDomainCredit: hasDomainCredit( state, siteId ),
-			canManageOptions: canCurrentUser( state, siteId, 'manage_options' ),
-			productsList: getProductsList( state ),
-			domains: getDomainsBySiteId( state, siteId ),
-			isPlanOwner: isCurrentUserCurrentPlanOwner( state, siteId ),
-			currencyCode: getCurrentUserCurrencyCode( state ),
-			domainUpsellNudgeDismissedDate: getPreference( state, DOMAIN_UPSELL_NUDGE_DISMISS_KEY ),
-			isSiteWPForTeams: isSiteWPForTeams( state, siteId ),
-			isMigrationInProgress,
-			hasJITM: getTopJITM( state, messagePath ),
-		};
-	},
-	( dispatch ) => {
-		return {
-			clickClaimDomainNotice: () =>
-				dispatch(
-					recordTracksEvent( 'calypso_domain_credit_reminder_click', {
-						cta_name: 'current_site_domain_notice',
-					} )
-				),
-			clickDomainUpsellGo: () =>
-				dispatch(
-					recordTracksEvent( 'calypso_upgrade_nudge_cta_click', {
-						cta_name: 'domain-upsell-nudge',
-					} )
-				),
-			clickDomainUpsellDismiss: () => {
-				dispatch( savePreference( DOMAIN_UPSELL_NUDGE_DISMISS_KEY, new Date().toISOString() ) );
-			},
-		};
-	}
-)( localize( SiteNotice ) );
+	return {
+		isDomainOnly: isDomainOnlySite( state, siteId ),
+		activeDiscount: getActiveDiscount( state ),
+		isSiteWPForTeams: isSiteWPForTeams( state, siteId ),
+		isMigrationInProgress,
+	};
+} )( localize( SiteNotice ) );
