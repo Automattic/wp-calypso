855684f550ef7d6bcee7ad445ca221b4d6767075
retrofox <rdsuarez@gmail.com>
2021-12-15 17:31:34 -0300

    woop: remove error details from Error cmp

--- client/signup/steps/woocommerce-install/transfer/error.tsx
+++ client/signup/steps/woocommerce-install/transfer/error.tsx
@@ -1,32 +1,25 @@
 import styled from '@emotion/styled';
 import { useI18n } from '@wordpress/react-i18n';
 import { ReactElement } from 'react';
-import WarningCard from 'calypso/components/warning-card';
+import { SupportLink } from '../confirm';
 import StepContent from './step-content';
 
 const WarningsOrHoldsSection = styled.div`
-	margin-bottom: 40px;
+	margin-top: 40px;
 `;
 
-export default function Error( { message }: { message: string } ): ReactElement {
+export default function Error(): ReactElement {
 	const { __ } = useI18n();
 	// todo: both messages sort of say the same thing, refer to figma and fix
 	return (
 		<StepContent
 			title={ __( "We've hit a snag" ) }
 			subtitle={ __(
-				'It looks like something went wrong while setting up your store. If this is unexpected, please contact support so that we can help you out.'
+				'It looks like something went wrong while setting up your store. Please contact support so that we can help you out.'
 			) }
 		>
 			<WarningsOrHoldsSection>
-				<WarningCard
-					message={
-						message ||
-						__(
-							'There is an error that is stopping us from being able to install this product, please contact support.'
-						)
-					}
-				/>
+				<SupportLink />
 			</WarningsOrHoldsSection>
 		</StepContent>
 	);
