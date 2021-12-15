f193af1dc5e9e6eb7c8c2ce2a757f5b972d37a13
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2021-12-15 15:23:17 +0100

    Temporarily remove the main effect
    Removed the only effect in this first version.

--- client/signup/steps/import-from/medium/index.tsx
+++ client/signup/steps/import-from/medium/index.tsx
@@ -1,14 +1,13 @@
 import { Hooray, SubTitle, Title } from '@automattic/onboarding';
 import { useI18n } from '@wordpress/react-i18n';
 import classnames from 'classnames';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { resetImport, startImport } from 'calypso/state/imports/actions';
-import { appStates } from 'calypso/state/imports/constants';
 import { importSite } from 'calypso/state/imports/site-importer/actions';
 import DoneButton from '../components/done-button';
 import { Importer, ImportJob, ImportJobParams } from '../types';
-import { getImporterTypeForEngine } from '../util';
+
 import './style.scss';
 
 interface Props {
@@ -24,40 +23,7 @@ interface Props {
 export const MediumImporter: React.FunctionComponent< Props > = ( props ) => {
 	const importer: Importer = 'medium';
 	const { __ } = useI18n();
-	const { job, run, siteId, siteSlug, fromSite, importSite, startImport, resetImport } = props;
-
-	/**
-	 ↓ Effects
-	 */
-	useEffect( runImport, [ job ] );
-
-	/**
-	 ↓ Methods
-	 */
-	function runImport() {
-		if ( ! run ) return;
-
-		// If there is no existing import job, start a new
-		if ( job === undefined ) {
-			startImport( siteId, getImporterTypeForEngine( importer ) );
-		} else if ( job.importerState === appStates.READY_FOR_UPLOAD ) {
-			importSite( prepareImportParams() );
-		}
-	}
-
-	function prepareImportParams(): ImportJobParams {
-		const targetSiteUrl = fromSite.startsWith( 'http' ) ? fromSite : 'https://' + fromSite;
-
-		return {
-			engine: importer,
-			importerStatus: job as ImportJob,
-			params: { engine: importer },
-			site: { ID: siteId },
-			targetSiteUrl,
-			supportedContent: [],
-			unsupportedContent: [],
-		};
-	}
+	const { job, siteId, siteSlug } = props;
 
 	function checkIsSuccess() {
 		return true;
