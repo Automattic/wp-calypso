376f8a0554a95652c10d39a1ffb88dfa6824dcd1
gcsecsey <gcsecsey@gmail.com>
2021-12-15 12:30:17 +0100

    query product list on plugin details page

--- client/my-sites/plugins/plugin-details.jsx
+++ client/my-sites/plugins/plugin-details.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import DocumentHead from 'calypso/components/data/document-head';
 import QueryEligibility from 'calypso/components/data/query-atat-eligibility';
 import QueryJetpackPlugins from 'calypso/components/data/query-jetpack-plugins';
+import QueryProductsList from 'calypso/components/data/query-products-list';
 import EmptyContent from 'calypso/components/empty-content';
 import FixedNavigationHeader from 'calypso/components/fixed-navigation-header';
 import MainComponent from 'calypso/components/main';
@@ -203,6 +204,7 @@ function PluginDetails( props ) {
 			<QueryJetpackPlugins siteIds={ siteIds } />
 			<SidebarNavigation />
 			<QueryEligibility siteId={ selectedSite?.ID } />
+			<QueryProductsList />
 			<FixedNavigationHeader navigationItems={ getNavigationItems() } />
 			<PluginNotices
 				pluginId={ fullPlugin.id }
