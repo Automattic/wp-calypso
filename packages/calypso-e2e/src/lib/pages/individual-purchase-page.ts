3116e99295ed5268636136353f2fbeec5ae90285
Payton Swick <payton@foolord.com>
2021-12-15 18:27:38 -0500

    Fix upgradeButton selector in e2e

--- packages/calypso-e2e/src/lib/pages/individual-purchase-page.ts
+++ packages/calypso-e2e/src/lib/pages/individual-purchase-page.ts
@@ -14,7 +14,7 @@ const selectors = {
 	renewNowCardButton: 'button.card:has-text("Renew Now")',
 	cancelAndRefundButton: 'a:text("Cancel Subscription and Refund")',
 	cancelSubscriptionButton: 'button:text("Cancel Subscription")',
-	upgradeButton: 'button:text("Upgrade")',
+	upgradeButton: 'a:text("Upgrade")',
 
 	// Purchased item actions: domains
 	deleteDomainCard: 'a:has-text("Delete your domain permanently")',
