52e4f6c0aaeeb999685915543a3b84f63cc88efe
Payton Swick <payton@automattic.com>
2021-12-15 12:23:46 -0500

    Checkout: Add modal to close button with option to clear cart (#58795)
    * Add simple unstyled modal to perform the required behavior
    
    * Add secondaryAction to CheckoutModal
    
    * Use CheckoutModal in close button
    
    * Allow close button to use relative SPA redirects
    
    * Add missing color on modal copy
    
    * Adjust modal wording
    
    * Use navigate function to perform redirect
    
    * Allow any ReactNodes as title/copy/buttons in CheckoutModal
    
    * Remove unnecessary String casts in CheckoutMasterbar
    
    * Add hideCancelButton option to CheckoutModal
    
    * Use hideCancelButton in CheckoutMasterbar close button
    
    * Remove default cancel button on CheckoutModal
    
    * Rename buttonCTA to primaryButtonCTA
    
    * Add explicit cancel buttons to checkout line items removal dialogs
    
    * Rename buttonCTA to primaryButtonCTA in CheckoutMasterbar
    
    * Rename primary action to "Do not clear"
    
    * Change the modal button verbs to be Leave/Empty

--- packages/wpcom-checkout/src/checkout-line-items.tsx
+++ packages/wpcom-checkout/src/checkout-line-items.tsx
@@ -215,9 +215,13 @@ function WPNonProductLineItem( {
 						closeModal={ () => {
 							setIsModalVisible( false );
 						} }
+						secondaryAction={ () => {
+							setIsModalVisible( false );
+						} }
 						primaryAction={ () => {
 							removeProductFromCart();
 						} }
+						secondaryButtonCTA={ String( 'Cancel' ) }
 						title={ modalCopy.title }
 						copy={ modalCopy.description }
 					/>
@@ -850,6 +854,10 @@ function WPLineItem( {
 						cancelAction={ () => {
 							onRemoveProductCancel?.( label );
 						} }
+						secondaryAction={ () => {
+							onRemoveProductCancel?.( label );
+						} }
+						secondaryButtonCTA={ String( translate( 'Cancel' ) ) }
 						title={ modalCopy.title }
 						copy={ modalCopy.description }
 					/>
