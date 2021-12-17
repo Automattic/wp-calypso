01d550534c83f84c42a1c57b086620cc6f90c370
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2021-12-17 11:23:35 +0200

    Lib: Cleanup unused formatting functions (#59289)

--- client/lib/formatting/index.js
+++ client/lib/formatting/index.js
@@ -1,9 +1,6 @@
 export { capitalPDangit } from './capital-p-dangit';
 export { decodeEntities } from './decode-entities';
-export { interpose } from './interpose';
 export { parseHtml } from './parse-html';
 export { preventWidows } from './prevent-widows';
-export { removep } from './removep';
 export { stripHTML } from './strip-html';
 export { unescapeAndFormatSpaces } from './unescape-and-format-spaces';
-export { wpautop } from './wpautop';
