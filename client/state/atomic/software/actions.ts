5251595e5710e8b96924eba47616369e4118f7d0
retrofox <rdsuarez@gmail.com>
2021-12-20 09:34:36 -0300

    state: add action to clean atomic software status

--- client/state/atomic/software/actions.ts
+++ client/state/atomic/software/actions.ts
@@ -86,3 +86,19 @@ export const setAtomicSoftwareError = (
 		softwareSet,
 		error,
 	} as const );
+
+/**
+ * Clean the install status.
+ *
+ * @param {number} siteId The site id to which the status belongs.
+ * @param {string} softwareSet The software set slug.*
+ * @returns {object} An action object
+ */
+export const cleanAtomicSoftwareStatus = ( siteId: number, softwareSet: string ) =>
+	( {
+		type: ATOMIC_SOFTWARE_SET_STATUS,
+		siteId,
+		softwareSet,
+		status: null,
+		error: null,
+	} as const );
