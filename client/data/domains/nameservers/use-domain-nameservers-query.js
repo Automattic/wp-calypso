eed802506f00e5067cdb8083a3c1380788968f8c
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-12-21 11:46:02 -0300

    Prevent name servers from being fetched on every windows focus

--- client/data/domains/nameservers/use-domain-nameservers-query.js
+++ client/data/domains/nameservers/use-domain-nameservers-query.js
@@ -2,8 +2,10 @@ import { useQuery } from 'react-query';
 import wp from 'calypso/lib/wp';
 
 const useDomainNameserversQuery = ( domainName ) =>
-	useQuery( [ 'domain-nameservers', domainName ], () =>
-		wp.req.get( `/domains/${ domainName }/nameservers/` )
+	useQuery(
+		[ 'domain-nameservers', domainName ],
+		() => wp.req.get( `/domains/${ domainName }/nameservers/` ),
+		{ refetchOnWindowFocus: false }
 	);
 
 export default useDomainNameserversQuery;
