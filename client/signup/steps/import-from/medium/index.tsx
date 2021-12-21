4259d4175407883ff9d117032a4aa6168d0b46e6
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2021-12-21 08:10:02 +0100

    Add MediumImportert component

--- client/signup/steps/import-from/medium/index.tsx
+++ client/signup/steps/import-from/medium/index.tsx
@@ -1,14 +1,16 @@
 import { Hooray, SubTitle, Title } from '@automattic/onboarding';
 import { useI18n } from '@wordpress/react-i18n';
 import classnames from 'classnames';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import ImporterMedium from 'calypso/my-sites/importer/importer-medium';
 import { resetImport, startImport } from 'calypso/state/imports/actions';
+import { appStates } from 'calypso/state/imports/constants';
 import { importSite } from 'calypso/state/imports/site-importer/actions';
 import DoneButton from '../components/done-button';
 import { Importer, ImportJob, ImportJobParams } from '../types';
-
 import './style.scss';
+import { getImporterTypeForEngine } from '../util';
 
 interface Props {
 	job?: ImportJob;
@@ -23,10 +25,48 @@ interface Props {
 export const MediumImporter: React.FunctionComponent< Props > = ( props ) => {
 	const importer: Importer = 'medium';
 	const { __ } = useI18n();
-	const { job, siteId, siteSlug } = props;
+	const { job, run, siteId, siteSlug, fromSite } = props;
+	// const importerConfig = getImporterByKey( 'importer-type-medium', { siteTitle: 'Medium' } );
+
+	/**
+	 ↓ Effects
+	 */
+	useEffect( runImport, [ job ] );
+
+	/**
+	 ↓ Methods
+	 */
+	function runImport() {
+		if ( ! run ) return;
+
+		// If there is no existing import job, start a new
+		if ( job === undefined ) {
+			startImport( siteId, getImporterTypeForEngine( importer ) );
+		} else if ( job.importerState === appStates.READY_FOR_UPLOAD ) {
+			importSite( prepareImportParams() );
+		}
+	}
+
+	function prepareImportParams(): ImportJobParams {
+		const targetSiteUrl = fromSite.startsWith( 'http' ) ? fromSite : 'https://' + fromSite;
+
+		return {
+			engine: importer,
+			importerStatus: job as ImportJob,
+			params: { engine: importer },
+			site: { ID: siteId },
+			targetSiteUrl,
+			supportedContent: [],
+			unsupportedContent: [],
+		};
+	}
 
 	function checkIsSuccess() {
-		return true;
+		return job && job.importerState === appStates.IMPORT_SUCCESS;
+	}
+
+	function afterStartImport() {
+		return;
 	}
 
 	return (
@@ -52,6 +92,18 @@ export const MediumImporter: React.FunctionComponent< Props > = ( props ) => {
 							</Hooray>
 						);
 					}
+
+					/**
+					 * Upload section
+					 */
+					return (
+						<ImporterMedium
+							site={ props }
+							engine={ importer }
+							fromSite={ fromSite }
+							afterStartImport={ afterStartImport }
+						/>
+					);
 				} )() }
 			</div>
 		</>
