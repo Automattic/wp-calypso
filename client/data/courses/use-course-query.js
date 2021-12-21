3521befb55926d2f57f4623c9c1058957e3f788e
arthur <arthur.chu@automattic.com>
2021-12-21 17:09:11 +0800

    Add useCourseData hook to share the logic of the computed data

--- client/data/courses/use-course-query.js
+++ client/data/courses/use-course-query.js
@@ -5,7 +5,16 @@ const useCourseQuery = ( courseSlug, queryOptions = {} ) => {
 	return useQuery(
 		[ 'courses', courseSlug ],
 		() => wpcom.req.get( '/courses', { course_slug: courseSlug, apiNamespace: 'wpcom/v2' } ),
-		queryOptions
+		{
+			// Our course offering doesn't change that often, we don't need to
+			// re-fetch until the next page refresh.
+			staleTime: Infinity,
+			...queryOptions,
+			meta: {
+				persist: false,
+				...queryOptions.meta,
+			},
+		}
 	);
 };
 
