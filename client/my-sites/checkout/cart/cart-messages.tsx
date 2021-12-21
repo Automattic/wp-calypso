97242805e1bfc13f06f0095ff1a18cb268feb381
Payton Swick <payton@foolord.com>
2021-12-21 14:24:12 -0500

    Pass reloadFromServer to useDisplayCartMessages

--- client/my-sites/checkout/cart/cart-messages.tsx
+++ client/my-sites/checkout/cart/cart-messages.tsx
@@ -24,7 +24,9 @@ function CartMessage( { message }: { message: ResponseCartMessage } ): JSX.Eleme
 
 export default function CartMessages(): null {
 	const cartKey = useCartKey();
-	const { responseCart: cart, isLoading: isLoadingCart } = useShoppingCart( cartKey );
+	const { responseCart: cart, isLoading: isLoadingCart, reloadFromServer } = useShoppingCart(
+		cartKey
+	);
 	const reduxDispatch = useDispatch();
 
 	const showErrorMessages = useCallback(
@@ -43,6 +45,7 @@ export default function CartMessages(): null {
 
 	useDisplayCartMessages( {
 		cart,
+		reloadFromServer,
 		isLoadingCart,
 		showErrorMessages,
 		showSuccessMessages,
