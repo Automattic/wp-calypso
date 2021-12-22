1d71e72541187eeca0ce749beb26c96e4f9a89da
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-12-21 17:19:40 -0800

    Use ResizeObserver instead of resize and orientationchange listeners

--- client/components/swipeable/index.js
+++ client/components/swipeable/index.js
@@ -1,42 +1,31 @@
 import classnames from 'classnames';
 import { useRtl } from 'i18n-calypso';
-import { Children, useState, useEffect, useRef, useCallback } from 'react';
+import { Children, useState, useLayoutEffect, useRef, useCallback } from 'react';
 
 import './style.scss';
 
 const OFFSET_THRESHOLD_PERCENTAGE = 0.45; // Percentage of width to travel before we trigger the slider to move to the desired slide.
 const VELOCITY_THRESHOLD = 0.5; // Speed of drag above, before we trigger the slider to move to the desired slide.
+const TRANSITION_DURATION = `300ms`;
 
-function useUpdateLayout( enabled, currentPageIndex, updateLayout ) {
-	// save callback to a ref so that it doesn't need to be a dependency of other hooks
-	const savedUpdateLayout = useRef();
-	useEffect( () => {
-		savedUpdateLayout.current = updateLayout;
-	}, [ updateLayout ] );
+function useResizeObserver() {
+	const [ observerEntry, setObserverEntry ] = useState( {} );
+	const [ node, setNode ] = useState( null );
+	const observer = useRef( null );
 
-	// fire when the `currentPageIndex` parameter changes
-	useEffect( () => {
-		if ( ! enabled ) {
-			return;
-		}
+	const disconnect = useCallback( () => observer.current?.disconnect(), [] );
 
-		savedUpdateLayout.current();
-	}, [ enabled, currentPageIndex ] );
+	const observe = useCallback( () => {
+		observer.current = new ResizeObserver( ( [ entry ] ) => setObserverEntry( entry ) );
+		if ( node ) observer.current.observe( node );
+	}, [ node ] );
 
-	// fire when the window resizes
-	useEffect( () => {
-		if ( ! enabled ) {
-			return;
-		}
+	useLayoutEffect( () => {
+		observe();
+		return () => disconnect();
+	}, [ disconnect, observe ] );
 
-		const onResize = () => savedUpdateLayout.current( false );
-		window.addEventListener( 'resize', onResize );
-		window.addEventListener( 'orientationchange', onResize );
-		return () => {
-			window.removeEventListener( 'resize', onResize );
-			window.removeEventListener( 'orientationchange', onResize );
-		};
-	}, [ enabled ] );
+	return [ setNode, observerEntry ];
 }
 
 function getDragPositionAndTime( event ) {
@@ -73,52 +62,46 @@ export const Swipeable = ( {
 	containerClassName,
 	...otherProps
 } ) => {
-	const [ pageWidth, setPageWidth ] = useState( null );
 	const [ swipeableArea, setSwipeableArea ] = useState();
 	const isRtl = useRtl();
 
+	const [ resizeObserverRef, entry ] = useResizeObserver();
+
 	const [ pagesStyle, setPagesStyle ] = useState( {
-		transform: `translate3d(0px, 0px, 0px)`,
-		transitionDuration: `300ms`,
+		transitionDuration: TRANSITION_DURATION,
 	} );
 	const [ dragStartData, setDragStartData ] = useState( null );
 
 	const pagesRef = useRef();
-	const resizeObserverRef = useRef();
 	const numPages = Children.count( children );
+	const containerWidth = entry?.contentRect?.width;
 
 	const getOffset = useCallback(
 		( index ) => {
-			const offset = pageWidth * index;
+			const offset = containerWidth * index;
 			return isRtl ? offset : -offset;
 		},
-		[ isRtl, pageWidth ]
+		[ isRtl, containerWidth ]
 	);
 
 	const updateEnabled = hasDynamicHeight && numPages > 1;
 
-	useUpdateLayout( updateEnabled, currentPage, ( hasTransitionDuration = true ) => {
-		const offset = getOffset( currentPage );
-		const transform = {
-			transform: `translate3d(${ offset }px, 0px, 0px)`,
-			transitionDuration: hasTransitionDuration ? `300ms` : `0ms`,
-		};
-		let height = {};
+	useLayoutEffect( () => {
+		if ( ! updateEnabled ) {
+			return;
+		}
 		const targetHeight = pagesRef.current?.querySelector( '.is-current' )?.offsetHeight;
 		if ( targetHeight && pagesStyle?.height !== targetHeight ) {
-			height = { height: targetHeight };
+			setPagesStyle( { ...pagesStyle, height: targetHeight } );
 		}
-		setPageWidth( resizeObserverRef.current?.getBoundingClientRect().width );
-		setSwipeableArea( pagesRef.current?.getBoundingClientRect() );
-		setPagesStyle( { ...pagesStyle, ...height, ...transform } );
-	} );
+	}, [ pagesRef, currentPage, pagesStyle, updateEnabled, containerWidth ] );
 
 	const handleDragStart = useCallback(
 		( event ) => {
 			const position = getDragPositionAndTime( event );
 			setSwipeableArea( pagesRef.current?.getBoundingClientRect() );
 			setDragStartData( position );
-			setPageWidth( resizeObserverRef.current?.getBoundingClientRect().width );
+			// setPageWidth( resizeObserverRef.current?.getBoundingClientRect().width );
 			setPagesStyle( { ...pagesStyle, transitionDuration: `0ms` } ); // Set transition Duration to 0 for smooth dragging.
 		},
 		[ pagesStyle ]
@@ -143,7 +126,8 @@ export const Swipeable = ( {
 			const velocity = absoluteDelta / ( dragPosition.timeStamp - dragStartData.timeStamp );
 
 			const hasMetThreshold =
-				absoluteDelta > OFFSET_THRESHOLD_PERCENTAGE * pageWidth || velocity > VELOCITY_THRESHOLD;
+				absoluteDelta > OFFSET_THRESHOLD_PERCENTAGE * containerWidth ||
+				velocity > VELOCITY_THRESHOLD;
 
 			let newIndex = currentPage;
 			if ( hasSwipedToNextPage( delta ) && hasMetThreshold && numPages !== currentPage + 1 ) {
@@ -153,11 +137,12 @@ export const Swipeable = ( {
 			if ( hasSwipedToPreviousPage( delta ) && hasMetThreshold && currentPage !== 0 ) {
 				newIndex = currentPage - 1;
 			}
-			const offset = getOffset( newIndex );
+
+			delete pagesStyle.transform;
+
 			setPagesStyle( {
 				...pagesStyle,
-				transform: `translate3d(${ offset }px, 0px, 0px)`,
-				transitionDuration: `300ms`,
+				transitionDuration: TRANSITION_DURATION,
 			} );
 			onPageSelect( newIndex );
 			setDragStartData( null );
@@ -171,7 +156,7 @@ export const Swipeable = ( {
 			numPages,
 			onPageSelect,
 			pagesStyle,
-			pageWidth,
+			containerWidth,
 		]
 	);
 
@@ -255,6 +240,8 @@ export const Swipeable = ( {
 		return null;
 	}, [ handleDragStart, handleDrag, handleDragEnd ] );
 
+	const offset = getOffset( currentPage );
+
 	return (
 		<>
 			<div
@@ -265,11 +252,15 @@ export const Swipeable = ( {
 			>
 				<div
 					className={ classnames( 'swipeable__pages', containerClassName ) }
-					style={ { ...pagesStyle, width: getPagesWidth( pageWidth, numPages ) } }
+					style={ {
+						transform: `translate3d(${ offset }px, 0px, 0px)`,
+						...pagesStyle,
+						width: getPagesWidth( containerWidth, numPages ),
+					} }
 				>
 					{ Children.map( children, ( child, index ) => (
 						<div
-							style={ { width: `${ pageWidth }px` } } // Setting the page width is important for iOS browser.
+							style={ { width: `${ containerWidth }px` } } // Setting the page width is important for iOS browser.
 							className={ classnames( 'swipeable__page', pageClassName, {
 								'is-current': index === currentPage,
 								'is-prev': index < currentPage,
