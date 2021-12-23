6611173879bc665f0db0f9360e15fe1aae41599d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2021-12-23 17:19:14 +0100

    Create content chooser component

--- client/signup/steps/import-from/wordpress/index.tsx
+++ client/signup/steps/import-from/wordpress/index.tsx
@@ -1,14 +1,6 @@
-import { NextButton } from '@automattic/onboarding';
-import { useI18n } from '@wordpress/react-i18n';
-import classnames from 'classnames';
 import React from 'react';
 import { ImportJob } from '../types';
 import { ContentChooser } from './content-chooser';
-import ActionCard from 'calypso/components/action-card';
-import FormattedHeader from 'calypso/components/formatted-header';
-import { jetpack } from '../../../icons';
-import SelectItems from '../../../select-items';
-import { ImportJob } from '../types';
 
 import './style.scss';
 
