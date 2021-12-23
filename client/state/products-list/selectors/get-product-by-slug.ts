941562cdb7c8ee0d02d681865b9ac6ff6c6b5e33
Payton Swick <payton@foolord.com>
2021-12-23 16:24:03 -0500

    Convert getProductBySlug to TS

--- client/state/products-list/selectors/get-product-by-slug.ts
+++ client/state/products-list/selectors/get-product-by-slug.ts
@@ -1,6 +1,6 @@
-import { get } from 'lodash';
-
 import 'calypso/state/products-list/init';
+import type { ProductListItem } from './get-products-list';
+import type { AppState } from 'calypso/types';
 
 /**
  * Retrieves the product with the specified slug.
@@ -9,6 +9,6 @@ import 'calypso/state/products-list/init';
  * @param {string} productSlug - internal product slug, eg 'jetpack_premium'
  * @returns {import('./get-products-list').ProductListItem|null} the corresponding product, or null if not found
  */
-export function getProductBySlug( state, productSlug ) {
-	return get( state, [ 'productsList', 'items', productSlug ], null );
+export function getProductBySlug( state: AppState, productSlug: string ): ProductListItem | null {
+	return state.productsList?.items?.[ productSlug ] ?? null;
 }
