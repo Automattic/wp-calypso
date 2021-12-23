b677231c149e76932c1da4960005f5b5ffa86e01
Payton Swick <payton@foolord.com>
2021-12-23 14:39:52 -0500

    Convert getSitesItems to TS

---
+++ client/state/selectors/get-sites-items.ts
@@ -0,0 +1,27 @@
+import type { AppState } from 'calypso/types';
+
+const EMPTY_SITES: SitesItem = Object.freeze( {} );
+
+export interface SitesItem {
+	ID?: number;
+	name?: string;
+	description?: string;
+	URL?: string;
+	capabilities?: Record< string, boolean >;
+	jetpack?: boolean;
+	is_multisite?: boolean;
+	site_owner?: number;
+	lang?: string;
+	visible?: boolean;
+	is_private?: boolean;
+	is_coming_soon?: boolean;
+	single_user_site?: boolean;
+	is_vip?: boolean;
+}
+
+/**
+ * Returns site items object or empty object.
+ */
+export default function getSitesItems( state: AppState ): Record< number, SitesItem > {
+	return state.sites.items || EMPTY_SITES;
+}
