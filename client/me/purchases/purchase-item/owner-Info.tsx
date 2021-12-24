c28e8fd9decbda77f7ae850c4f93e158928c41f3
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-12-24 12:54:17 +0530

    Create OwnerInfo component

---
+++ client/me/purchases/purchase-item/owner-Info.tsx
@@ -0,0 +1,47 @@
+import { localize, useTranslate } from 'i18n-calypso';
+import { connect } from 'react-redux';
+import InfoPopover from 'calypso/components/info-popover';
+import { getCurrentUserId } from 'calypso/state/current-user/selectors';
+import type { Purchase } from 'calypso/lib/purchases/types';
+import type { AppState } from 'calypso/types';
+
+import './style.scss';
+
+type InjectedProps = {
+	isProductOwner: boolean;
+};
+
+type NeedsProps = {
+	purchase: Purchase | undefined;
+};
+
+export type OwnerInfoProps = InjectedProps & NeedsProps;
+
+export const OwnerInfo: React.FC< OwnerInfoProps > = ( { isProductOwner } ) => {
+	const translate = useTranslate();
+
+	if ( isProductOwner ) {
+		return null;
+	}
+
+	return (
+		<InfoPopover className="owner-Info__pop-over">
+			<span>
+				{ translate(
+					'To manage this subscription, log in to the WordPress.com account that purchased it or contact the owner.'
+				) }
+			</span>
+		</InfoPopover>
+	);
+};
+
+export default connect< InjectedProps, unknown, NeedsProps, AppState >(
+	( state: AppState, { purchase }: NeedsProps ): InjectedProps => {
+		const userId = getCurrentUserId( state );
+		const isProductOwner = Boolean( purchase && purchase.userId === userId );
+
+		return {
+			isProductOwner,
+		};
+	}
+)( localize( OwnerInfo ) );
