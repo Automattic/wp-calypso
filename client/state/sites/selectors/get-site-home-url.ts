0856b06a21fa6c636dff7509cc72b5fa3569fee4
Payton Swick <payton@foolord.com>
2021-12-23 19:48:00 -0500

    Fix TS errors in getSiteHomeUrl

--- client/state/sites/selectors/get-site-home-url.ts
+++ client/state/sites/selectors/get-site-home-url.ts
@@ -1,6 +1,7 @@
 import { getStatsDefaultSitePage } from 'calypso/lib/route/path';
 import { canCurrentUserUseCustomerHome, getSiteSlug } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import type { AppState } from 'calypso/types';
 
 /**
  * Determine the default section to show for the specified site.
@@ -9,14 +10,11 @@ import { getSelectedSiteId } from 'calypso/state/ui/selectors';
  * @param  {?number} siteId Site ID.
  * @returns {string}         Url of the site home.
  */
-export default function getSiteHomeUrl(
-	state: Record< string, unknown >,
-	siteId?: number
-): string {
+export default function getSiteHomeUrl( state: AppState, siteId?: number ): string {
 	const selectedSiteId = siteId || getSelectedSiteId( state );
 	const siteSlug = getSiteSlug( state, selectedSiteId );
 
 	return canCurrentUserUseCustomerHome( state, selectedSiteId )
 		? `/home/${ siteSlug }`
-		: getStatsDefaultSitePage( siteSlug );
+		: getStatsDefaultSitePage( siteSlug ?? undefined );
 }
