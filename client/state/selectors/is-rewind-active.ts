8a1e423bb3b25e7e59a677cd808902ca90cb36d0
Payton Swick <payton@foolord.com>
2021-12-28 16:24:00 -0500

    Fix TS errors in isRewindActive

--- client/state/selectors/is-rewind-active.ts
+++ client/state/selectors/is-rewind-active.ts
@@ -1,4 +1,5 @@
 import getRewindState from 'calypso/state/selectors/get-rewind-state';
+import type { AppState } from 'calypso/types';
 
 /**
  * Indicates whether the Rewind feature is active
@@ -7,7 +8,7 @@ import getRewindState from 'calypso/state/selectors/get-rewind-state';
  * @param {number|string} siteId the site ID
  * @returns {boolean} true if rewind is active
  */
-export default function isRewindActive( state, siteId ): boolean {
+export default function isRewindActive( state: AppState, siteId: number ): boolean {
 	return [ 'awaitingCredentials', 'provisioning', 'active' ].includes(
 		getRewindState( state, siteId ).state
 	);
