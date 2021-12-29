c94678ab20925017e7943662c25176f9724f3f6a
Renzo Canepa <rcanepag@gmail.com>
2021-12-29 18:16:05 -0300

    Add the modal content

--- client/components/jetpack/licensing-prompt-dialog/index.tsx
+++ client/components/jetpack/licensing-prompt-dialog/index.tsx
@@ -1,20 +1,32 @@
-import { Dialog } from '@automattic/components';
-import { useEffect, useState } from 'react';
+import { Button, Dialog } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import { useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import QueryJetpackUserLicenses from 'calypso/components/data/query-jetpack-user-licenses';
 import QueryJetpackUserLicensesCounts from 'calypso/components/data/query-jetpack-user-licenses-counts';
+import { preventWidows } from 'calypso/lib/formatting';
 import {
 	getUserLicenses,
 	getUserLicensesCounts,
 	userHasDetachedLicenses,
 } from 'calypso/state/user-licensing/selectors';
 
-function LicensingPromptDialog() {
+import './style.scss';
+
+interface Props {
+	urlQueryArgs: {
+		[ key: string ]: string;
+	};
+}
+
+function LicensingPromptDialog( { urlQueryArgs }: Props ) {
+	const translate = useTranslate();
 	const hasDetachedLicenses = useSelector( userHasDetachedLicenses );
 	const userLicenses = useSelector( getUserLicenses );
 	const userLicensesCounts = useSelector( getUserLicensesCounts );
 	const [ showLicensesDialog, setShowLicensesDialog ] = useState< boolean >( false );
 
+	const { redirect } = urlQueryArgs;
 	const hasOneDetachedLicense = userLicensesCounts && userLicensesCounts[ 'detached' ] === 1;
 
 	const detachedUserLicense =
@@ -28,13 +40,54 @@ function LicensingPromptDialog() {
 		}
 	}, [ hasDetachedLicenses ] );
 
+	const title = useMemo( () => {
+		if ( hasOneDetachedLicense ) {
+			return preventWidows(
+				translate( 'Your %(productName)s is pending activation', {
+					args: {
+						productName: detachedUserLicense && detachedUserLicense.product,
+					},
+				} )
+			);
+		}
+		return preventWidows( translate( 'You have an available product license key' ) );
+	}, [ detachedUserLicense, hasOneDetachedLicense, translate ] );
+
+	const closeDialog = () => {
+		setShowLicensesDialog( false );
+	};
+
 	return (
 		<>
 			<QueryJetpackUserLicenses />
 			<QueryJetpackUserLicensesCounts />
-			<Dialog isVisible={ showLicensesDialog } onClose={ () => setShowLicensesDialog( false ) }>
-				<h1>Hey!</h1>
-				<pre>{ JSON.stringify( detachedUserLicense, null, 2 ) }</pre>
+			<Dialog
+				additionalClassNames="licensing-prompt-dialog"
+				isVisible={ showLicensesDialog }
+				onClose={ () => setShowLicensesDialog( false ) }
+				shouldCloseOnEsc
+			>
+				<h1 className="licensing-prompt-dialog__title">{ title }</h1>
+				<p>
+					{ preventWidows(
+						translate(
+							'{{strong}}Check your email{{/strong}} for your license key. You should have received it after making your purchase.',
+							{
+								components: {
+									strong: <strong />,
+								},
+							}
+						)
+					) }
+				</p>
+				<div className="licensing-prompt-dialog__actions">
+					<Button className="licensing-prompt-dialog__btn" primary href={ redirect }>
+						{ translate( 'Activate it now' ) }
+					</Button>
+					<Button className="licensing-prompt-dialog__btn" onClick={ closeDialog }>
+						{ translate( 'Select another product' ) }
+					</Button>
+				</div>
 			</Dialog>
 		</>
 	);
