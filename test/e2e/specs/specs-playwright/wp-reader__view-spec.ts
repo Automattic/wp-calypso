3b34b0a6c5841075d8847771577295600789f174
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2021-12-29 11:51:10 +0100

    Use TestAccount in wp-reader__view-spec.ts

--- test/e2e/specs/specs-playwright/wp-reader__view-spec.ts
+++ test/e2e/specs/specs-playwright/wp-reader__view-spec.ts
@@ -4,7 +4,7 @@
 
 import {
 	DataHelper,
-	LoginPage,
+	TestAccount,
 	setupHooks,
 	ReaderPage,
 	NotificationsComponent,
@@ -14,56 +14,62 @@ import { Page } from 'playwright';
 
 describe( DataHelper.createSuiteTitle( 'Reader: View and Comment' ), function () {
 	let page: Page;
-	let loginPage: LoginPage;
 	let readerPage: ReaderPage;
 	let notificationsComponent: NotificationsComponent;
 	const comment = DataHelper.getRandomPhrase() + ' wp-reader__view-spec';
 
 	setupHooks( ( args: { page: Page } ) => {
 		page = args.page;
-	} );
-
-	it( 'Log in as commenting user', async function () {
-		loginPage = new LoginPage( page );
 		readerPage = new ReaderPage( page );
-
-		await readerPage.visit();
-		await loginPage.logInWithTestAccount( 'commentingUser' );
 	} );
 
-	it( 'View the Reader stream', async function () {
-		const testSiteForNotifications = DataHelper.config.get( 'testSiteForNotifications' );
-		const siteOfLatestPost = await readerPage.siteOfLatestPost();
-		expect( siteOfLatestPost ).toEqual( testSiteForNotifications );
+	describe( 'As the commenting user', () => {
+		beforeAll( async () => {
+			const tst = new TestAccount( 'coBlocksSimpleSiteEdgeUser' );
+			await tst.getCookies();
+			const account = new TestAccount( 'commentingUser' );
+			await account.authenticate( page );
+		} );
+
+		it( 'Go to Reader page', async function () {
+			await readerPage.visit();
+		} );
+
+		it( 'View the Reader stream', async function () {
+			const testSiteForNotifications = DataHelper.config.get( 'testSiteForNotifications' );
+			const siteOfLatestPost = await readerPage.siteOfLatestPost();
+			expect( siteOfLatestPost ).toEqual( testSiteForNotifications );
+		} );
+
+		it( 'Visit latest post', async function () {
+			await readerPage.visitPost( { index: 1 } );
+		} );
+
+		it( 'Comment and confirm it is shown', async function () {
+			await readerPage.comment( comment );
+		} );
 	} );
 
-	it( 'Visit latest post', async function () {
-		await readerPage.visitPost( { index: 1 } );
-	} );
+	describe( 'As the site owner', () => {
+		beforeAll( async () => {
+			const account = new TestAccount( 'notificationsUser' );
+			await account.authenticate( page );
+		} );
 
-	it( 'Comment and confirm it is shown', async function () {
-		await readerPage.comment( comment );
-	} );
+		it( 'Open Notifications panel', async function () {
+			const navBarComponent = new NavbarComponent( page );
+			await navBarComponent.openNotificationsPanel();
+		} );
 
-	it( 'Log in as a site owner', async function () {
-		await loginPage.visit();
-		await loginPage.clickChangeAccount();
-		await loginPage.logInWithTestAccount( 'notificationsUser' );
-	} );
+		it( 'Delete the new comment', async function () {
+			notificationsComponent = new NotificationsComponent( page );
+			await notificationsComponent.clickNotification( comment );
+			await notificationsComponent.clickNotificationAction( 'Trash' );
+		} );
 
-	it( 'Open Notifications panel', async function () {
-		const navBarComponent = new NavbarComponent( page );
-		await navBarComponent.openNotificationsPanel();
-	} );
-
-	it( 'Delete the new comment', async function () {
-		notificationsComponent = new NotificationsComponent( page );
-		await notificationsComponent.clickNotification( comment );
-		await notificationsComponent.clickNotificationAction( 'Trash' );
-	} );
-
-	it( 'Wait for Undo Message to display and then disappear', async function () {
-		await notificationsComponent.waitForUndoMessage();
-		await notificationsComponent.waitForUndoMessageToDisappear();
+		it( 'Wait for Undo Message to display and then disappear', async function () {
+			await notificationsComponent.waitForUndoMessage();
+			await notificationsComponent.waitForUndoMessageToDisappear();
+		} );
 	} );
 } );
