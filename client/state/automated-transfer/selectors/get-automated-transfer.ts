cb9a0d56659c33601323a87ef88aa690dd7e3d6a
Payton Swick <payton@automattic.com>
2021-12-28 22:23:56 -0500

    Allow getAutomatedTransfer to work with undefined state (#59609)

--- client/state/automated-transfer/selectors/get-automated-transfer.ts
+++ client/state/automated-transfer/selectors/get-automated-transfer.ts
@@ -5,4 +5,4 @@ import 'calypso/state/automated-transfer/init';
 const emptyData = {};
 
 export const getAutomatedTransfer = ( state: AppState, siteId: number | null ) =>
-	siteId ? state.automatedTransfer?.[ siteId ] ?? emptyData : emptyData;
+	siteId ? state?.automatedTransfer?.[ siteId ] ?? emptyData : emptyData;
