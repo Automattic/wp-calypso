317729f5e94196b4310d38186652ce5c2c91dd04
Stéphane Thomas <email@stephanethomas.com>
2022-01-04 16:33:14 +0100

    Email: Add header on In-Depth Comparison page

--- client/my-sites/email/email-providers-in-depth-comparison/index.tsx
+++ client/my-sites/email/email-providers-in-depth-comparison/index.tsx
@@ -1,9 +1,12 @@
+import { useTranslate } from 'i18n-calypso';
 import { FunctionComponent } from 'react';
 import ComparisonTable from 'calypso/my-sites/email/email-providers-in-depth-comparison/comparison-table';
 import {
 	professionalEmailFeatures,
 	googleWorkspaceFeatures,
 } from 'calypso/my-sites/email/email-providers-in-depth-comparison/data';
+
+import './style.scss';
 
 type EmailProvidersInDepthComparisonProps = {
 	comparisonContext: string;
@@ -13,8 +16,16 @@ type EmailProvidersInDepthComparisonProps = {
 };
 
 const EmailProvidersInDepthComparison: FunctionComponent< EmailProvidersInDepthComparisonProps > = () => {
+	const translate = useTranslate();
+
 	return (
-		<ComparisonTable emailProviders={ [ professionalEmailFeatures, googleWorkspaceFeatures ] } />
+		<>
+			<h1 className="email-providers-in-depth-comparison__header wp-brand-font">
+				{ translate( 'See how they compare' ) }
+			</h1>
+
+			<ComparisonTable emailProviders={ [ professionalEmailFeatures, googleWorkspaceFeatures ] } />
+		</>
 	);
 };
 
