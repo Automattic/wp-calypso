bdae0f4be6842dcc35417046145bcfef9e9e3338
Yoav Farhi <yoav@automattic.com>
2022-01-04 09:56:34 +0200

    Sibyl: correctly route atomic sites requests  (#59669)
    * Sibyl: correctly route atomic sites requests: Atomic sites should return support results from WP.com support, not Jetpack support.
    * fix no-shadow lint error

--- client/me/help/help-contact-form/index.jsx
+++ client/me/help/help-contact-form/index.jsx
@@ -177,7 +177,7 @@ export class HelpContactForm extends PureComponent {
 			// The API would reject something like "https://wp.com/slug".
 			// It'd need to either be "http(s)://wp.com" or "wp.com".
 			const url = this.state.userDeclaredUrl;
-			const query = url.includes( '://' )
+			const queryUrl = url.includes( '://' )
 				? new URL( this.state.userDeclaredUrl ).hostname
 				: new URL( 'http://' + this.state.userDeclaredUrl ).hostname;
 
@@ -195,7 +195,7 @@ export class HelpContactForm extends PureComponent {
 						this.setState( { errorData: error.error, siteData: null, hasRetriedRequest: false } );
 					} );
 
-			return request( query );
+			return request( queryUrl );
 		}
 	};
 
@@ -216,9 +216,11 @@ export class HelpContactForm extends PureComponent {
 			newIDs.sort();
 			return existingIDs.toString() === newIDs.toString();
 		};
-		const site = this.props.helpSite.jetpack
-			? config( 'jetpack_support_blog' )
-			: config( 'wpcom_support_blog' );
+
+		const site =
+			! this.props.helpSite.jetpack || this.props.helpSite.is_wpcom_atomic
+				? config( 'wpcom_support_blog' )
+				: config( 'jetpack_support_blog' );
 
 		wpcom.req
 			.get( '/help/qanda', { query, site } )
