fc873808df62221139eb7f42175f27ed5b8e6473
retrofox <rdsuarez@gmail.com>
2022-01-05 12:24:46 -0300

    woop: first store-address super basic implementation

--- client/signup/steps/woocommerce-install/step-store-address/index.tsx
+++ client/signup/steps/woocommerce-install/step-store-address/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@automattic/components';
 import { TextControl } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
 import { ReactElement } from 'react';
@@ -8,12 +7,12 @@ import StepWrapper from 'calypso/signup/step-wrapper';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import SupportCard from '../components/support-card';
 import { ActionSection, StyledNextButton } from '../confirm';
-import { useStoreAddressOptions } from '../hooks/use-site-options';
+import { useSiteOption } from '../hooks/use-site-options';
 import useWooCommerceOnPlansEligibility from '../hooks/use-woop-handling';
 import type { WooCommerceInstallProps } from '..';
 import './style.scss';
 
-export default function Confirm( props: WooCommerceInstallProps ): ReactElement | null {
+export default function StepStoreAddress( props: WooCommerceInstallProps ): ReactElement | null {
 	const { goToStep, isReskinned, headerTitle, headerDescription } = props;
 	const { __ } = useI18n();
 
@@ -21,32 +20,41 @@ export default function Confirm( props: WooCommerceInstallProps ): ReactElement 
 
 	const { wpcomDomain } = useWooCommerceOnPlansEligibility( siteId );
 
-	const { get, save, clean, update } = useStoreAddressOptions( siteId );
-
+	const { get, save, update } = useSiteOption( siteId );
 	function getContent() {
 		return (
 			<>
-				<div className="store-address__info-section" />
-				<div className="store-address__instructions-container">
+				<div className="step-store-address__info-section" />
+				<div className="step-store-address__instructions-container">
 					<TextControl
 						label={ __( 'Address line 1', 'woocommerce-admin' ) }
 						value={ get( 'address_1' ) }
-						onChange={ ( value ) => update( { address_1: value } ) }
+						onChange={ ( value ) => update( 'address_1', value ) }
 					/>
 
 					<TextControl
 						label={ __( 'Address line 2', 'woocommerce-admin' ) }
 						value={ get( 'address_2' ) }
-						onChange={ ( value ) => update( { address_2: value } ) }
+						onChange={ ( value ) => update( 'address_2', value ) }
+					/>
+
+					<TextControl
+						label={ __( 'Country/Region', 'woocommerce-admin' ) }
+						value={ get( 'country' ) }
+						onChange={ ( value ) => update( 'country', value ) }
+					/>
+
+					<TextControl
+						label={ __( 'City', 'woocommerce-admin' ) }
+						value={ get( 'city' ) }
+						onChange={ ( value ) => update( 'city', value ) }
 					/>
 
 					<TextControl
 						label={ __( 'Postcode', 'woocommerce-admin' ) }
 						value={ get( 'postcode' ) }
-						onChange={ ( value ) => update( { postcode: value } ) }
+						onChange={ ( value ) => update( 'postcode', value ) }
 					/>
-
-					<Button onClick={ clean }>Clean</Button>
 
 					<ActionSection>
 						<SupportCard />
@@ -66,7 +74,7 @@ export default function Confirm( props: WooCommerceInstallProps ): ReactElement 
 
 	if ( ! siteId ) {
 		return (
-			<div className="store-address__info-section">
+			<div className="step-store-address__info-section">
 				<LoadingEllipsis />
 			</div>
 		);
