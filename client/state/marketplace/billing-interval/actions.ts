144cf3261311ea08bf1a2e9d9b1d6c4ee31fe0df
Rahul <rahul.gavande@automattic.com>
2022-01-05 22:14:40 +0530

    replace any with billing interval options

--- client/state/marketplace/billing-interval/actions.ts
+++ client/state/marketplace/billing-interval/actions.ts
@@ -1,8 +1,11 @@
+import { IntervalLength } from 'calypso/my-sites/marketplace/components/billing-interval-switcher/constants';
 import { MARKETPLACE_BILLING_INTERVAL_SELECT } from 'calypso/state/action-types';
-
+import type { AnyAction } from 'redux';
 import 'calypso/state/marketplace/init';
 
-export function setBillingInterval( interval: any ) {
+export function setBillingInterval(
+	interval: IntervalLength.MONTHLY | IntervalLength.ANNUALLY
+): AnyAction {
 	return {
 		type: MARKETPLACE_BILLING_INTERVAL_SELECT,
 		interval,
