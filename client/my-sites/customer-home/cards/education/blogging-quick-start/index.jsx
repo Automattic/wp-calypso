77ba7d95f1a7b14081a92261c077ed932a7ee4f2
arthur791004 <arthur.chu@automattic.com>
2022-01-05 12:15:22 +0800

    My Home: Update start learning image (#59698)

--- client/my-sites/customer-home/cards/education/blogging-quick-start/index.jsx
+++ client/my-sites/customer-home/cards/education/blogging-quick-start/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from '@wordpress/element';
 import { useTranslate } from 'i18n-calypso';
-import playImage from 'calypso/assets/images/customer-home/plain-play.png';
+import startLearningPrompt from 'calypso/assets/images/customer-home/illustration--secondary-start-learning.svg';
 import { EDUCATION_BLOGGING_QUICK_START } from 'calypso/my-sites/customer-home/cards/constants';
 import EducationalContent from '../educational-content';
 import BloggingQuickStartModal from './blogging-quick-start-modal';
@@ -29,7 +29,7 @@ const BloggingQuickStart = () => {
 					text: translate( 'Start learning' ),
 				},
 			] }
-			illustration={ playImage }
+			illustration={ startLearningPrompt }
 			cardName={ EDUCATION_BLOGGING_QUICK_START }
 		/>
 	);
