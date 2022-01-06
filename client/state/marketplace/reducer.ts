39be4be432483bd2e9b3172bb6f6de9e7fc048e8
gavande1 <86406124+gavande1@users.noreply.github.com>
2022-01-06 14:50:30 +0530

    Marketplace: Persist pricing selector options between navigation and page reloads (#59764)
    * add action type for billing interval
    
    * add action/reducer/selectors for billing interval selector
    
    * use redux store instead of local state
    
    * add billing interval types
    
    * fix tests
    
    * replace any with billing interval options
    
    * fix auto import: replace relative path with absolute calpyso path
    
    * replace string type to interval types

--- client/state/marketplace/reducer.ts
+++ client/state/marketplace/reducer.ts
@@ -1,9 +1,11 @@
 import { withStorageKey } from '@automattic/state-utils';
 import { combineReducers } from 'calypso/state/utils';
+import billingInterval from './billing-interval/reducer';
 import purchaseFlow from './purchase-flow/reducer';
 
 const combinedReducers = combineReducers( {
 	purchaseFlow,
+	billingInterval,
 } );
 
 export default withStorageKey( 'marketplace', combinedReducers );
