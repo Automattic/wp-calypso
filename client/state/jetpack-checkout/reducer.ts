4a4cd514a43dbfcdce62fa0e03954ac2ebcbba11
Payton Swick <payton@foolord.com>
2022-01-06 18:09:57 -0500

    Fix TS errors in submittedSiteUrl reducer

--- client/state/jetpack-checkout/reducer.ts
+++ client/state/jetpack-checkout/reducer.ts
@@ -5,6 +5,7 @@ import {
 	JETPACK_CHECKOUT_UPDATE_SUPPORT_TICKET_REQUEST_SUCCESS,
 } from 'calypso/state/action-types';
 import { combineReducers, keyedReducer } from 'calypso/state/utils';
+import type { Action } from 'redux';
 
 export const requestStatus = keyedReducer( 'receiptId', ( state, { type } ) => {
 	switch ( type ) {
@@ -21,9 +22,13 @@ export const requestStatus = keyedReducer( 'receiptId', ( state, { type } ) => {
 	return state;
 } );
 
-export const submittedSiteUrl = keyedReducer(
+interface SubmittedSiteUrlAction extends Action {
+	payload?: string;
+}
+
+export const submittedSiteUrl = keyedReducer< string | undefined, SubmittedSiteUrlAction >(
 	'jetpackTemporarySiteId',
-	( state, { type, payload } ) => {
+	( state, { type, payload }: SubmittedSiteUrlAction ) => {
 		switch ( type ) {
 			case JETPACK_CHECKOUT_UPDATE_SUPPORT_TICKET_RECEIVE:
 				return payload;
