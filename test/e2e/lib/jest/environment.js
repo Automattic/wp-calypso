51ea69d0e1a0c3330bf15c393f83a545175770df
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2022-01-06 17:08:09 +0100

    Add handling for multiple contexts and pages

--- test/e2e/lib/jest/environment.js
+++ test/e2e/lib/jest/environment.js
@@ -1,15 +1,18 @@
 import fs from 'fs/promises';
+import os from 'os';
 import path from 'path';
-const JestEnvironmentNode = require( 'jest-environment-node' );
+import JestEnvironmentNode from 'jest-environment-node';
+import { chromium } from 'playwright';
+
+const sanitizeString = ( string ) => {
+	return string.replace( /[^a-z0-9]/gi, '-' ).toLowerCase();
+};
 
 class JestEnvironmentE2E extends JestEnvironmentNode {
-	testFailed = false;
-	describeFailed = false;
-	hookFailed = false;
-
 	constructor( config, context ) {
 		super( config, context );
-		this.fileName = path.parse( context.testPath ).name;
+
+		this.testFilename = path.parse( context.testPath ).name;
 	}
 
 	/**
@@ -18,56 +21,125 @@ class JestEnvironmentE2E extends JestEnvironmentNode {
 	async setup() {
 		await super.setup();
 
-		// Intermediate path storing all artifacts.
-		const resultsPath = path.join( process.cwd(), 'results' );
-		// Create the directory if necessary.
-		await fs.mkdir( resultsPath, { recursive: true } );
-		// Generate a unique name by appending a random string after the file name.
-		const artifactPath = await fs.mkdtemp( path.join( resultsPath, this.fileName + '-' ) );
-		this.global.artifactPath = artifactPath;
+		// Start the browser
+		const browser = await chromium.launch( {
+			args: [ '--window-position=0,0' ],
+			headless: process.env.HEADLESS === 'true',
+			slowMo: process.env.SLOWMO && Number( process.env.SLOWMO ),
+		} );
+
+		// Create a helper for creating new contexts with our custom options
+		const newContext = async ( options = {} ) => {
+			const context = await browser.newContext( {
+				...options,
+				userAgent: `Mozilla/5.0 (wp-e2e-tests) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${ browser.version() } Safari/537.36`,
+				recordVideo: { dir: os.tmpdir() },
+			} );
+
+			await context.tracing.start( {
+				screenshots: true,
+				snapshots: true,
+			} );
+
+			return context;
+		};
+
+		// Initiate new context and page
+		const context = await newContext();
+		const page = await context.newPage();
+
+		// Expose globally
+		this.global.browser = browser;
+		this.global.context = context;
+		this.global.page = page;
+		this.global.newContext = newContext;
 	}
 
 	/**
-	 * Custom handler for the test events emitted by jest-circus.
-	 *
-	 * @param {*} event Test event emitted.
+	 * Handle events emitted by jest-circus.
 	 */
 	async handleTestEvent( event ) {
 		switch ( event.name ) {
-			case 'setup':
-				this.global.__FAILED_STEP_NAME__ = null;
-				this.global.__STEP_FAILED__ = false;
-				this.global.__FILE_NAME__ = this.fileName;
-
-			case 'test_start':
+			case 'test_start': {
 				// If a test has failed, skip rest of the steps.
-				if ( this.testFailed ) {
-					event.test.mode = 'skip';
-				}
-
-				/* If a hook has failed, mark all subsequent test
-				steps as failed.
-				Handling is different compared to test steps because
-				Jest treats failed hooks differently from tests.
-				*/
-				if ( this.hookFailed ) {
+				if ( this.failure?.type === 'hook' ) {
 					event.test.mode = 'fail';
 				}
+				// If a hook has failed, mark all subsequent test steps as failed.
+				// Handling is different compared to test steps because Jest treats
+				// failed hooks differently from tests.
+				if ( this.failure?.type === 'test' ) {
+					event.test.mode = 'skip';
+				}
 				break;
+			}
+			case 'hook_failure': {
+				this.failure = { type: 'hook', name: event.hook.type };
+				break;
+			}
+			case 'test_fn_failure': {
+				this.failure = { type: 'test', name: event.test.name };
+				break;
+			}
+			case 'teardown': {
+				const contexts = this.global.browser.contexts();
 
-			case 'hook_failure':
-				this.global.__FAILED_STEP_NAME__ = event.hook.type;
-				this.global.__STEP_FAILED__ = true;
-				this.hookFailed = true;
-				break;
+				if ( this.failure ) {
+					// Create artefacts folder
+					const resultsPath = path.join( process.cwd(), 'results' );
+					await fs.mkdir( resultsPath, { recursive: true } );
+					const artefactsPath = await fs.mkdtemp(
+						path.join( resultsPath, `${ this.testFilename }__${ Date.now() }-` )
+					);
+					const mediaPath = path.join( artefactsPath, 'screenshots' );
+					await fs.mkdir( mediaPath );
 
-			case 'test_fn_failure':
-				this.global.__FAILED_STEP_NAME__ = event.test.name;
-				this.global.__STEP_FAILED__ = true;
-				this.testFailed = true;
+					const artefactFilename = `${ this.testFilename }__${ sanitizeString(
+						this.failure.name
+					) }`;
+
+					let contextIndex = 0;
+					let pageIndex = 0;
+
+					// Save trace, screenshot and video artefacts for every open page
+					for await ( const context of contexts ) {
+						const traceFilepath = path.join(
+							artefactsPath,
+							`${ artefactFilename }__${ contextIndex }.zip`
+						);
+
+						await context.tracing.stop( { path: traceFilepath } );
+
+						for await ( const page of context.pages() ) {
+							const mediaFilepath = path.join(
+								mediaPath,
+								`${ artefactFilename }__${ contextIndex }-${ pageIndex }`
+							);
+
+							await page.screenshot( { path: `${ mediaFilepath }.png` } );
+							await page.close();
+							await page.video()?.saveAs( `${ mediaFilepath }.webm` );
+
+							pageIndex++;
+						}
+						contextIndex++;
+					}
+				}
+
+				// Delete videos from tmp folder
+				for await ( const context of contexts ) {
+					for await ( const page of context.pages() ) {
+						await page.close();
+						await page.video().delete();
+					}
+					await context.close();
+				}
+
+				await this.global.browser.close();
 				break;
+			}
 		}
 	}
 }
 
-module.exports = JestEnvironmentE2E;
+export default JestEnvironmentE2E;
