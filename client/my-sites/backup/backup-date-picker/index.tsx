d1da5e88d3c247565b142bc4b21f6689ade99323
jboland88 <18016357+jboland88@users.noreply.github.com>
2022-01-06 09:16:11 -0500

    Update backups date navigation to add a date picker component (#56367)
    * Update the backup date picker component to add a date picker component
    
    * Added event tracking, removed feature flag for previous date selection component, add some logic to check for number of visible days for backups
    
    * Allow datepicker element for backups to determine which dates do not have backups
    
    * Revert some files that were reformatted by prettier
    
    * Update unit tests
    
    * Add additional unit test to help cover date exclusion from the datepicker
    
    * Revert some auto-formatted files back to where they are on trunk
    
    * Fix issues with incorrect date ranges being available in the backups date picker
    
    * Fix typo in comment
    
    * Repair unit tests, always show today's date as selectable in the caldendar, even if it has no backup
    
    * Allow date picker to be enabled with a feature flag
    
    * Fix typo, re-order feature flags

--- client/my-sites/backup/backup-date-picker/index.tsx
+++ client/my-sites/backup/backup-date-picker/index.tsx
@@ -11,7 +11,10 @@ import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import useDateWithOffset from 'calypso/lib/jetpack/hooks/use-date-with-offset';
 import DateRangeSelector from 'calypso/my-sites/activity/filterbar/date-range-selector';
 import { recordTracksEvent } from 'calypso/state/analytics/actions/record';
+import getActivityLogVisibleDays from 'calypso/state/rewind/selectors/get-activity-log-visible-days';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import { useDatesWithNoSuccessfulBackups } from '../status/hooks';
+import DateButton from './date-button';
 import { useCanGoToDate, useFirstKnownBackupAttempt } from './hooks';
 
 import './style.scss';
@@ -19,6 +22,7 @@ import './style.scss';
 const SEARCH_LINK_CLICK = recordTracksEvent( 'calypso_jetpack_backup_search' );
 const PREV_DATE_CLICK = recordTracksEvent( 'calypso_jetpack_backup_date_previous' );
 const NEXT_DATE_CLICK = recordTracksEvent( 'calypso_jetpack_backup_date_next' );
+const CALENDAR_DATE_CLICK = recordTracksEvent( 'calypso_jetpack_backup_date_calendar_select_day' );
 
 const onSpace = ( fn: () => void ) => ( { key }: { key?: string } ) => {
 	if ( key === ' ' ) {
@@ -32,6 +36,9 @@ const BackupDatePicker: React.FC< Props > = ( { selectedDate, onDateChange } ) =
 
 	const moment = useLocalizedMoment();
 	const translate = useTranslate();
+	// Note: only one of these features should be enabled for a given environment.
+	const isUsingDateRangePicker = isEnabled( 'jetpack/backups-date-picker' );
+	const isUsingDateCalendarPicker = isEnabled( 'jetpack/backups-date-calendar' );
 
 	const siteId = useSelector( getSelectedSiteId ) as number;
 	const siteSlug = useSelector( getSelectedSiteSlug );
@@ -45,28 +52,67 @@ const BackupDatePicker: React.FC< Props > = ( { selectedDate, onDateChange } ) =
 		firstKnownBackupAttempt.backupAttempt?.activityTs,
 		{ shouldExecute: !! firstKnownBackupAttempt.backupAttempt }
 	);
+	// Get the oldest visible backup date.
+	// This is added into the state via QueryRewindPolicies
+	const visibleDays = useSelector( ( state ) => getActivityLogVisibleDays( state, siteId ) );
+	// If the number of visible days is falsy, then use the oldest date as the first visible backup date.
+	const firstVisibleBackupDate = visibleDays
+		? today.clone().subtract( visibleDays, 'days' )
+		: oldestDateAvailable;
 
 	const canGoToDate = useCanGoToDate( siteId, selectedDate, oldestDateAvailable );
+	const datesWithNoBackups = useDatesWithNoSuccessfulBackups(
+		siteId,
+		firstVisibleBackupDate,
+		today.clone()
+	);
 
-	const { previousDisplayDate, nextDisplayDate } = useMemo( () => {
+	// Don't filter out today's date from the calendar options.
+	let todayIndex;
+	if ( ( todayIndex = datesWithNoBackups.dates.indexOf( today.format( 'MM-DD-YYYY' ) ) ) > -1 ) {
+		datesWithNoBackups.dates.splice( todayIndex, 1 );
+	}
+
+	const { previousDisplayDate, nextDisplayDate, selectedDisplayDate } = useMemo( () => {
 		const yesterday = moment( today ).subtract( 1, 'day' );
 
 		const dateFormat = today.year() === selectedDate.year() ? 'MMM D' : 'MMM D, YYYY';
 
-		let _previousDisplayDate;
-		if ( previousDate.isSame( today, 'day' ) ) {
-			// Should never happen unless we manually change
-			// the URL to travel into the future
-			_previousDisplayDate = translate( 'Today' );
-		} else if ( previousDate.isSame( yesterday, 'day' ) ) {
-			_previousDisplayDate = translate( 'Yesterday' );
-		} else {
-			_previousDisplayDate = previousDate.format( dateFormat );
-		}
+		const dateOutputs = [
+			{
+				label: 'previous',
+				date: previousDate,
+				string: previousDate.format( dateFormat ),
+			},
+			{
+				label: 'next',
+				date: nextDate,
+				string: nextDate.format( dateFormat ),
+			},
+			{
+				label: 'current',
+				date: selectedDate,
+				string: selectedDate.format( dateFormat ),
+			},
+		];
+
+		dateOutputs.forEach( ( formatObject ) => {
+			// Don't modify the "next" date control
+			if ( 'next' === formatObject.label ) {
+				return;
+			}
+			// For current and previous dates, set to say "Today" or "Yesterday" when appropriate
+			if ( formatObject.date.isSame( today, 'day' ) ) {
+				formatObject.string = translate( 'Today' ).toString();
+			} else if ( formatObject.date.isSame( yesterday, 'day' ) ) {
+				formatObject.string = translate( 'Yesterday' ).toString();
+			}
+		} );
 
 		return {
-			previousDisplayDate: _previousDisplayDate,
-			nextDisplayDate: nextDate.format( dateFormat ),
+			previousDisplayDate: dateOutputs[ 0 ].string,
+			nextDisplayDate: dateOutputs[ 1 ].string,
+			selectedDisplayDate: dateOutputs[ 2 ].string,
 		};
 	}, [ moment, today, selectedDate, previousDate, nextDate, translate ] );
 
@@ -95,64 +141,93 @@ const BackupDatePicker: React.FC< Props > = ( { selectedDate, onDateChange } ) =
 		onDateChange( nextDate );
 	}, [ canGoToNextDate, dispatch, onDateChange, nextDate ] );
 
+	const goToCalendarDate = useCallback(
+		( selectedDate ) => {
+			if ( ! canGoToDate( selectedDate ) ) {
+				return false;
+			}
+
+			dispatch( CALENDAR_DATE_CLICK );
+			onDateChange( selectedDate );
+		},
+		[ dispatch, onDateChange ]
+	);
+
 	return (
 		<div className="backup-date-picker">
-			<div className="backup-date-picker__select-date">
+			<div className="backup-date-picker__select-date-container">
 				<div
-					className="backup-date-picker__select-date--previous"
-					role="button"
-					tabIndex={ 0 }
-					onClick={ goToPreviousDate }
-					onKeyDown={ onSpace( goToPreviousDate ) }
+					className={
+						'backup-date-picker__select-date' +
+						( isUsingDateRangePicker
+							? ' backup-date-picker__select-date--with-date-range-picker'
+							: '' ) +
+						( isUsingDateCalendarPicker
+							? ' backup-date-picker__select-date--with-date-calendar-picker'
+							: '' )
+					}
 				>
-					<Button compact borderless className="backup-date-picker__button--previous">
-						<Gridicon
-							icon="chevron-left"
-							className={ ! canGoToPreviousDate ? 'disabled' : undefined }
-						/>
-					</Button>
-
-					<span
-						className={ classNames( 'backup-date-picker__display-date', {
-							disabled: ! canGoToPreviousDate,
-						} ) }
+					<div
+						className="backup-date-picker__select-date--previous"
+						role="button"
+						tabIndex={ 0 }
+						onClick={ goToPreviousDate }
+						onKeyDown={ onSpace( goToPreviousDate ) }
 					>
-						{ previousDisplayDate }
-					</span>
-				</div>
-
-				{ isEnabled( 'jetpack/backups-date-picker' ) && (
-					<DateRangeSelector
-						siteId={ siteId }
-						enabled={ true }
-						customLabel={ <Gridicon icon="calendar" /> }
-					/>
-				) }
-
-				<div
-					className="backup-date-picker__select-date--next"
-					role="button"
-					tabIndex={ 0 }
-					onClick={ goToNextDate }
-					onKeyDown={ onSpace( goToNextDate ) }
-				>
-					<div className="backup-date-picker__next-date-link">
-						<span
-							className={ classNames( 'backup-date-picker__display-date', {
-								disabled: ! canGoToNextDate,
-							} ) }
-						>
-							{ nextDisplayDate }
-						</span>
-
-						<Button compact borderless className="backup-date-picker__button--next">
+						<Button compact borderless className="backup-date-picker__button--previous">
 							<Gridicon
-								icon="chevron-right"
-								className={ ! canGoToNextDate ? 'disabled' : undefined }
+								icon="chevron-left"
+								className={ ! canGoToPreviousDate ? 'disabled' : undefined }
 							/>
 						</Button>
+
+						<span
+							className={ classNames( 'backup-date-picker__display-date', {
+								disabled: ! canGoToPreviousDate,
+							} ) }
+						>
+							{ previousDisplayDate }
+						</span>
 					</div>
-					{ isEnabled( 'jetpack/backups-date-picker' ) && (
+
+					{ isUsingDateRangePicker && ! isUsingDateCalendarPicker && (
+						<DateRangeSelector
+							siteId={ siteId }
+							enabled={ true }
+							customLabel={ <Gridicon icon="calendar" /> }
+						/>
+					) }
+					{ ! isUsingDateRangePicker && isUsingDateCalendarPicker && (
+						<div className="backup-date-picker__current-date">
+							<b>{ selectedDisplayDate }</b>
+						</div>
+					) }
+
+					<div
+						className="backup-date-picker__select-date--next"
+						role="button"
+						tabIndex={ 0 }
+						onClick={ goToNextDate }
+						onKeyDown={ onSpace( goToNextDate ) }
+					>
+						<div className="backup-date-picker__next-date-link">
+							<span
+								className={ classNames( 'backup-date-picker__display-date', {
+									disabled: ! canGoToNextDate,
+								} ) }
+							>
+								{ nextDisplayDate }
+							</span>
+
+							<Button compact borderless className="backup-date-picker__button--next">
+								<Gridicon
+									icon="chevron-right"
+									className={ ! canGoToNextDate ? 'disabled' : undefined }
+								/>
+							</Button>
+						</div>
+					</div>
+					{ isUsingDateRangePicker && ! isUsingDateCalendarPicker && (
 						<a
 							className="backup-date-picker__search-link"
 							href={ `/activity-log/${ siteSlug }` }
@@ -163,6 +238,15 @@ const BackupDatePicker: React.FC< Props > = ( { selectedDate, onDateChange } ) =
 					) }
 				</div>
 			</div>
+
+			{ ! isUsingDateRangePicker && isUsingDateCalendarPicker && (
+				<DateButton
+					onDateSelected={ goToCalendarDate }
+					selectedDate={ selectedDate }
+					firstBackupDate={ firstVisibleBackupDate }
+					disabledDates={ datesWithNoBackups.dates }
+				/>
+			) }
 		</div>
 	);
 };
---
+++ client/my-sites/backup/status/test/hooks.jsx
@@ -0,0 +1,52 @@
+const mockUseSelector = ( func ) => func();
+const mockUseMemo = ( func ) => func();
+
+jest.mock( 'react', () => ( {
+	...jest.requireActual( 'react' ),
+	useMemo: jest.fn( ( fn ) => fn ),
+} ) );
+jest.mock( 'react-redux', () => ( {
+	...jest.requireActual( 'react-redux' ),
+	useSelector: jest.fn(),
+} ) );
+jest.mock( 'calypso/my-sites/backup/hooks', () => ( {
+	...jest.requireActual( 'calypso/my-sites/backup/hooks' ),
+	useMatchingBackupAttemptsInRange: jest.fn(),
+} ) );
+jest.mock( 'calypso/components/localized-moment' );
+jest.mock( 'calypso/state/ui/selectors/get-selected-site-id' );
+jest.mock( 'calypso/state/selectors/get-site-gmt-offset' );
+jest.mock( 'calypso/state/selectors/get-site-timezone-value' );
+
+import moment from 'moment';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import { useMatchingBackupAttemptsInRange } from 'calypso/my-sites/backup/hooks';
+import { useDatesWithNoSuccessfulBackups } from 'calypso/my-sites/backup/status/hooks';
+
+describe( 'useDatesWithNoSuccessfulBackups', () => {
+	beforeEach( () => {
+		jest.clearAllMocks();
+
+		useMemo.mockImplementation( mockUseMemo );
+		useSelector.mockImplementation( mockUseSelector );
+		useLocalizedMoment.mockImplementation( () => moment );
+		useMatchingBackupAttemptsInRange.mockImplementation( () => {
+			return {
+				isLoading: false,
+				backups: [ { activityDate: '2021-12-01' }, { activityDate: '2021-12-04' } ],
+			};
+		} );
+	} );
+
+	test( 'Dates with backups are correctly excluded from range of dates', () => {
+		const start = moment( '2021-12-01' );
+		const end = moment( '2021-12-05' );
+
+		const { isLoading, dates } = useDatesWithNoSuccessfulBackups( null, start, end );
+
+		expect( dates ).toEqual( [ '12-02-2021', '12-03-2021', '12-05-2021' ] );
+		expect( isLoading ).toBeFalsy();
+	} );
+} );
