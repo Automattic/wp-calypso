9c7467a75b8bc46e2ad7a85e5c3c3a1232974e72
Oli I <oikeme@users.noreply.github.com>
2022-01-07 15:53:26 -0700

    Add banner for license activation
    Add a banner in the checkout process to indicate when there are licenses that are available and have not yet been activated

---
+++ client/components/jetpack/licensing-activation-banner/index.tsx
@@ -0,0 +1,32 @@
+import { useTranslate } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
+import { getSiteAdminUrl } from 'calypso/state/sites/selectors';
+import lockIcon from './lock-icon.svg';
+
+import './style.scss';
+
+interface Props {
+	siteId: number;
+}
+
+function LicensingActivationBanner( { siteId }: Props ) {
+	const translate = useTranslate();
+	const siteAdminUrl = useSelector( ( state ) => getSiteAdminUrl( state, siteId ) );
+	const jetpackDashboardUrl = siteAdminUrl + 'admin.php?page=jetpack#/my-plan';
+
+	return (
+		<>
+			<div className="licensing-activation-banner">
+				<div className="licensing-activation-banner__copy">
+					<img src={ lockIcon } alt="" />
+					{ translate( 'You have an available product license key ' ) }
+					<span className="licensing-activation-banner__activate">
+						<a href={ jetpackDashboardUrl }>{ translate( 'Activate it now.' ) }</a>
+					</span>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default LicensingActivationBanner;
