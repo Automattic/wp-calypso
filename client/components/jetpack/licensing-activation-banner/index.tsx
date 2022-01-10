047c6b2bf7238442bb1f936bafc6afdfe6fb8878
Oli I <oikeme@users.noreply.github.com>
2022-01-10 11:58:46 -0700

    Update banner css and display conditions
    Update the activate a license banner css for mobile and Jetpack connect and fix conditions under which the banner displays.

--- client/components/jetpack/licensing-activation-banner/index.tsx
+++ client/components/jetpack/licensing-activation-banner/index.tsx
@@ -1,6 +1,7 @@
 import { useTranslate } from 'i18n-calypso';
 import { useSelector } from 'react-redux';
 import { getSiteAdminUrl } from 'calypso/state/sites/selectors';
+import { getUserLicensesCounts } from 'calypso/state/user-licensing/selectors';
 import keyIcon from './key-icon.svg';
 
 import './style.scss';
@@ -13,18 +14,24 @@ function LicensingActivationBanner( { siteId }: Props ) {
 	const translate = useTranslate();
 	const siteAdminUrl = useSelector( ( state ) => getSiteAdminUrl( state, siteId ) );
 	const jetpackDashboardUrl = siteAdminUrl + 'admin.php?page=jetpack#/my-plan';
+	const userLicensesCounts = useSelector( getUserLicensesCounts );
+	const hasDetachedLicenses = userLicensesCounts && userLicensesCounts[ 'detached' ] !== 0;
 
-	return (
-		<>
-			<div className="licensing-activation-banner">
-				<img className="licensing-activation-banner__key-icon" src={ keyIcon } alt="" />
-				{ translate( 'You have an available product license key ' ) }
-				<span className="licensing-activation-banner__activate">
-					<a href={ jetpackDashboardUrl }>{ translate( 'Activate it now.' ) }</a>
-				</span>
-			</div>
-		</>
-	);
+	if ( hasDetachedLicenses ) {
+		return (
+			<>
+				<div className="licensing-activation-banner">
+					<img className="licensing-activation-banner__key-icon" src={ keyIcon } alt="" />
+					{ translate( 'You have an available product license key. ' ) }
+					<span className="licensing-activation-banner__activate">
+						<a href={ jetpackDashboardUrl }>{ translate( 'Activate it now' ) }</a>
+					</span>
+				</div>
+			</>
+		);
+	}
+
+	return null;
 }
 
 export default LicensingActivationBanner;
