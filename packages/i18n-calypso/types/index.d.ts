1a95b660428783f700cff5d3013efeff1abeb0e1
Jarda Snajdr <jsnajdr@gmail.com>
2022-01-10 09:56:16 +0100

    i18n-calypso: add support for React context

--- packages/i18n-calypso/types/index.d.ts
+++ packages/i18n-calypso/types/index.d.ts
@@ -145,3 +145,5 @@ export type LocalizedComponent< C > = React.ComponentClass<
 export function localize< C >( component: C ): LocalizedComponent< C >;
 export function useTranslate(): typeof translate & { localeSlug: string | undefined };
 export function useRtl(): boolean;
+
+export declare const I18NContext: React.Context< I18N >;
