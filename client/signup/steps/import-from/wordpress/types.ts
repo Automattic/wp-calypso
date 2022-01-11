bc45d9c363020b8646c39e5b457b40b225e22c75
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-01-11 19:57:16 +0100

    Export MigrationStatus enum

--- client/signup/steps/import-from/wordpress/types.ts
+++ client/signup/steps/import-from/wordpress/types.ts
@@ -1,1 +1,11 @@
 export type WPImportType = 'everything' | 'content_only';
+
+export enum MigrationStatus {
+	UNKNOWN = 'unknown',
+	INACTIVE = 'inactive',
+	NEW = 'new',
+	BACKING_UP = 'backing-up',
+	RESTORING = 'restoring',
+	DONE = 'done',
+	ERROR = 'error',
+}
