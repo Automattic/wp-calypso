dc53f4b4901ad225cbdd0f54cf0c790d6cc6a5a7
Louis Laugesen <louis.laugesen@gmail.com>
2022-01-11 13:28:13 +1100

    Remove i18n contexts blocking translation reuse

--- client/signup/steps/woocommerce-install/step-store-address/index.tsx
+++ client/signup/steps/woocommerce-install/step-store-address/index.tsx
@@ -85,19 +85,19 @@ export default function StepStoreAddress( props: WooCommerceInstallProps ): Reac
 				<div className="step-store-address__info-section" />
 				<div className="step-store-address__instructions-container">
 					<TextControl
-						label={ __( 'Address line 1', 'woocommerce-admin' ) }
+						label={ __( 'Address line 1' ) }
 						value={ get( WOOCOMMERCE_STORE_ADDRESS_1 ) }
 						onChange={ ( value ) => update( WOOCOMMERCE_STORE_ADDRESS_1, value ) }
 					/>
 
 					<TextControl
-						label={ __( 'Address line 2', 'woocommerce-admin' ) }
+						label={ __( 'Address line 2' ) }
 						value={ get( WOOCOMMERCE_STORE_ADDRESS_2 ) }
 						onChange={ ( value ) => update( WOOCOMMERCE_STORE_ADDRESS_2, value ) }
 					/>
 
 					<ComboboxControl
-						label={ __( 'Country / Region', 'woocommerce-admin' ) }
+						label={ __( 'Country / Region' ) }
 						value={ get( WOOCOMMERCE_DEFAULT_COUNTRY ) }
 						onChange={ handleCountryChange }
 						options={ countriesAsOptions }
@@ -105,13 +105,13 @@ export default function StepStoreAddress( props: WooCommerceInstallProps ): Reac
 
 					<CityZipRow>
 						<TextControl
-							label={ __( 'City', 'woocommerce-admin' ) }
+							label={ __( 'City' ) }
 							value={ get( WOOCOMMERCE_STORE_CITY ) }
 							onChange={ ( value ) => update( WOOCOMMERCE_STORE_CITY, value ) }
 						/>
 
 						<TextControl
-							label={ __( 'Postcode', 'woocommerce-admin' ) }
+							label={ __( 'Postcode' ) }
 							value={ get( WOOCOMMERCE_STORE_POSTCODE ) }
 							onChange={ ( value ) => update( WOOCOMMERCE_STORE_POSTCODE, value ) }
 						/>
