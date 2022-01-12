d0cfdf71b3b77e11a743dfa463e64e18d993f21f
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2022-01-12 19:29:14 -0300

    Fix `List` unit tests by replacing it with `SiteDomains`

--- client/my-sites/domains/domain-management/list/test/index.js
+++ client/my-sites/domains/domain-management/list/test/index.js
@@ -11,7 +11,7 @@ import deepFreeze from 'deep-freeze';
 import { shallow, mount } from 'enzyme';
 import { Provider as ReduxProvider } from 'react-redux';
 import { createReduxStore } from 'calypso/state';
-import { List as DomainList } from '..';
+import { SiteDomains } from '../site-domains';
 
 const noop = () => {};
 
@@ -102,7 +102,7 @@ describe( 'index', () => {
 			}
 		);
 		const cartManagerClient = createShoppingCartManagerClient( { getCart, setCart } );
-		return mount( <DomainList { ...props } />, {
+		return mount( <SiteDomains { ...props } />, {
 			wrappingComponent: TestProvider,
 			wrappingComponentProps: { store, cartManagerClient },
 		} );
@@ -114,7 +114,7 @@ describe( 'index', () => {
 		} );
 
 		test( 'should list two domains', () => {
-			expect( component.find( 'DomainItem' ) ).toHaveLength( 2 );
+			expect( component.find( 'DomainRow' ) ).toHaveLength( 2 );
 		} );
 	} );
 
@@ -125,7 +125,7 @@ describe( 'index', () => {
 				userCanManageOptions: false,
 			} );
 
-			const wrapper = shallow( <DomainList { ...props } /> );
+			const wrapper = shallow( <SiteDomains { ...props } /> );
 
 			expect( wrapper.find( 'EmptyContent' ) ).toHaveLength( 1 );
 		} );
