3d54e4fb3fc000959c0b0ff1843b8f66ee9a3484
Louis Laugesen <louis.laugesen@gmail.com>
2022-01-13 16:02:20 +1100

    Add form validation to the woo installer address step (#59974)
    Co-authored-by: Allison Levine <allison.levine@automattic.com>

--- client/signup/steps/woocommerce-install/step-store-address/index.tsx
+++ client/signup/steps/woocommerce-install/step-store-address/index.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 import { TextControl, ComboboxControl } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
-import { ReactElement, useEffect } from 'react';
+import emailValidator from 'email-validator';
+import { ReactElement, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import FormInputValidation from 'calypso/components/forms/form-input-validation';
 import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
 import StepWrapper from 'calypso/signup/step-wrapper';
 import { fetchWooCommerceCountries } from 'calypso/state/countries/actions';
@@ -18,6 +20,7 @@ import {
 	WOOCOMMERCE_DEFAULT_COUNTRY,
 	WOOCOMMERCE_STORE_POSTCODE,
 	WOOCOMMERCE_ONBOARDING_PROFILE,
+	optionNameType,
 } from '../hooks/use-site-settings';
 import useWooCommerceOnPlansEligibility from '../hooks/use-woop-handling';
 import type { WooCommerceInstallProps } from '..';
@@ -43,19 +46,17 @@ export default function StepStoreAddress( props: WooCommerceInstallProps ): Reac
 	}, [ dispatch ] );
 
 	const siteId = useSelector( getSelectedSiteId ) as number;
+
 	const countriesList = useSelector( ( state ) => getCountries( state, 'woocommerce' ) ) || [];
-
-	const { wpcomDomain } = useWooCommerceOnPlansEligibility( siteId );
-
-	const { get, save, update } = useSiteSettings( siteId );
-
 	const countriesAsOptions = Object.entries( countriesList ).map( ( [ key, value ] ) => {
 		return { value: key, label: value };
 	} );
 
-	const handleCountryChange = ( value: string | null ) => {
-		update( WOOCOMMERCE_DEFAULT_COUNTRY, value || '' );
-	};
+	const { get, save, update } = useSiteSettings( siteId );
+
+	const { wpcomDomain } = useWooCommerceOnPlansEligibility( siteId );
+
+	const { validate, clearError, getError } = useAddressFormValidation( siteId );
 
 	// @todo: Add a general hook to get and update multi-option data like the profile.
 	function updateProfileEmail( email: string ) {
@@ -75,6 +76,19 @@ export default function StepStoreAddress( props: WooCommerceInstallProps ): Reac
 		return onboardingProfile[ 'store_email' ] || '';
 	}
 
+	// Form validation.
+	const address1Error = getError( WOOCOMMERCE_STORE_ADDRESS_1 );
+	const address2Error = getError( WOOCOMMERCE_STORE_ADDRESS_2 );
+	const countryError = getError( WOOCOMMERCE_DEFAULT_COUNTRY );
+	const cityError = getError( WOOCOMMERCE_STORE_CITY );
+	const postcodeError = getError( WOOCOMMERCE_STORE_POSTCODE );
+	const emailError = getError( WOOCOMMERCE_ONBOARDING_PROFILE );
+
+	const [ submitted, setSubmitted ] = useState( 0 );
+	useEffect( () => {
+		return;
+	}, [ submitted ] );
+
 	function getContent() {
 		return (
 			<>
@@ -83,49 +97,85 @@ export default function StepStoreAddress( props: WooCommerceInstallProps ): Reac
 					<TextControl
 						label={ __( 'Address line 1' ) }
 						value={ get( WOOCOMMERCE_STORE_ADDRESS_1 ) }
-						onChange={ ( value ) => update( WOOCOMMERCE_STORE_ADDRESS_1, value ) }
+						onChange={ ( value ) => {
+							update( WOOCOMMERCE_STORE_ADDRESS_1, value );
+							clearError( WOOCOMMERCE_STORE_ADDRESS_1 );
+						} }
+						className={ address1Error ? 'is-error' : '' }
 					/>
+					<ControlError error={ address1Error } />
 
 					<TextControl
 						label={ __( 'Address line 2' ) }
 						value={ get( WOOCOMMERCE_STORE_ADDRESS_2 ) }
-						onChange={ ( value ) => update( WOOCOMMERCE_STORE_ADDRESS_2, value ) }
+						onChange={ ( value ) => {
+							update( WOOCOMMERCE_STORE_ADDRESS_2, value );
+							clearError( WOOCOMMERCE_STORE_ADDRESS_2 );
+						} }
+						className={ address2Error ? 'is-error' : '' }
 					/>
-
-					<ComboboxControl
-						label={ __( 'Country / Region' ) }
-						value={ get( WOOCOMMERCE_DEFAULT_COUNTRY ) as string }
-						onChange={ handleCountryChange }
-						options={ countriesAsOptions }
-						allowReset={ false }
-					/>
+					<ControlError error={ address2Error } />
 
 					<CityZipRow>
-						<TextControl
-							label={ __( 'City' ) }
-							value={ get( WOOCOMMERCE_STORE_CITY ) }
-							onChange={ ( value ) => update( WOOCOMMERCE_STORE_CITY, value ) }
-						/>
+						<div>
+							<TextControl
+								label={ __( 'City' ) }
+								value={ get( WOOCOMMERCE_STORE_CITY ) }
+								onChange={ ( value ) => {
+									update( WOOCOMMERCE_STORE_CITY, value );
+									clearError( WOOCOMMERCE_STORE_CITY );
+								} }
+								className={ cityError ? 'is-error' : '' }
+							/>
+							<ControlError error={ cityError } />
+						</div>
 
-						<TextControl
-							label={ __( 'Postcode' ) }
-							value={ get( WOOCOMMERCE_STORE_POSTCODE ) }
-							onChange={ ( value ) => update( WOOCOMMERCE_STORE_POSTCODE, value ) }
-						/>
+						<div>
+							<TextControl
+								label={ __( 'Postcode' ) }
+								value={ get( WOOCOMMERCE_STORE_POSTCODE ) }
+								onChange={ ( value ) => {
+									update( WOOCOMMERCE_STORE_POSTCODE, value );
+									clearError( WOOCOMMERCE_STORE_POSTCODE );
+								} }
+								className={ postcodeError ? 'is-error' : '' }
+							/>
+							<ControlError error={ postcodeError } />
+						</div>
 					</CityZipRow>
+
+					<ComboboxControl
+						label={ __( 'Country / State' ) }
+						value={ get( WOOCOMMERCE_DEFAULT_COUNTRY ) }
+						onChange={ ( value: string | null ) => {
+							update( WOOCOMMERCE_DEFAULT_COUNTRY, value || '' );
+							clearError( WOOCOMMERCE_DEFAULT_COUNTRY );
+						} }
+						options={ countriesAsOptions }
+						className={ countryError ? 'is-error' : '' }
+					/>
+					<ControlError error={ countryError } />
 
 					<TextControl
 						label={ __( 'Email address' ) }
 						value={ getProfileEmail() }
-						onChange={ updateProfileEmail }
+						onChange={ ( value ) => {
+							updateProfileEmail( value );
+							clearError( WOOCOMMERCE_ONBOARDING_PROFILE );
+						} }
+						className={ emailError ? 'is-error' : '' }
 					/>
+					<ControlError error={ emailError } />
 
 					<ActionSection>
 						<SupportCard />
 						<StyledNextButton
 							onClick={ () => {
-								save();
-								goToStep( 'confirm' );
+								setSubmitted( submitted + 1 );
+								if ( validate() ) {
+									save();
+									goToStep( 'confirm' );
+								}
 							} }
 						>
 							{ __( 'Continue' ) }
@@ -161,4 +211,57 @@ export default function StepStoreAddress( props: WooCommerceInstallProps ): Reac
 			{ ...props }
 		/>
 	);
+}
+
+function ControlError( props: { error: string } ): ReactElement | null {
+	const { error } = props;
+	if ( error ) {
+		return <FormInputValidation isError={ true } isValid={ false } text={ error } />;
+	}
+	return null;
+}
+
+function useAddressFormValidation( siteId: number ) {
+	const { get } = useSiteSettings( siteId );
+	const { __ } = useI18n();
+
+	const [ errors, setErrors ] = useState( {} as Record< optionNameType, string > );
+
+	const validate = () => {
+		errors[ WOOCOMMERCE_STORE_ADDRESS_1 ] = ! get( WOOCOMMERCE_STORE_ADDRESS_1 )
+			? __( 'Please add an address' )
+			: '';
+		errors[ WOOCOMMERCE_STORE_ADDRESS_2 ] = ''; // Optional field
+		errors[ WOOCOMMERCE_DEFAULT_COUNTRY ] = ! get( WOOCOMMERCE_DEFAULT_COUNTRY )
+			? __( 'Please select a country / region' )
+			: '';
+		errors[ WOOCOMMERCE_STORE_CITY ] = ! get( WOOCOMMERCE_STORE_CITY )
+			? __( 'Please add a city' )
+			: '';
+		errors[ WOOCOMMERCE_STORE_POSTCODE ] = ! get( WOOCOMMERCE_STORE_POSTCODE )
+			? __( 'Please add a postcode' )
+			: '';
+		errors[ WOOCOMMERCE_ONBOARDING_PROFILE ] = ! emailValidator.validate(
+			get( WOOCOMMERCE_ONBOARDING_PROFILE )?.[ 'store_email' ]
+		)
+			? __( 'Please add a store email' )
+			: '';
+
+		setErrors( errors );
+
+		return Object.values( errors ).filter( Boolean ).length === 0;
+	};
+
+	const clearError = ( field: optionNameType ) => {
+		errors[ field ] = '';
+		setErrors( errors );
+	};
+
+	const getError = ( field: optionNameType ) => errors[ field ] || '';
+
+	return {
+		validate,
+		clearError,
+		getError,
+	};
 }
