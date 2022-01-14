33312ff15abdc4f37f055273bb70d3de7f0142b5
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2022-01-14 11:32:20 +0200

    Gutenboarding: Remove disabled vertical input (#59307)
    * Gutenboarding: Remove disabled vertical input
    
    * Inline the label
    
    * Remove getSelectedVertical selector
    
    * Remove site vertical skipping
    
    * Remove vertical resetting
    
    * Remove site vertical setting
    
    * Remove siteVertical reducer
    
    * Remove SiteVertical types
    
    * Cleanup unused TracksAcquireIntentEventProperties properties
    
    * Cleanup inline docs
    
    * Remove unused verticals css
    
    * Fall back to undefined instead of false
    
    * Remove css of back and next arrows
    
    * Remove unused togglePageLayout action and pageLayouts reducer

--- client/landing/gutenboarding/onboarding-block/acquire-intent/site-title.tsx
+++ client/landing/gutenboarding/onboarding-block/acquire-intent/site-title.tsx
@@ -1,5 +1,3 @@
-import config from '@automattic/calypso-config';
-import { useViewportMatch } from '@wordpress/compose';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { Icon } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
@@ -23,7 +21,6 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit, inputRef } ) =
 	const isAnchorFmSignup = useIsAnchorFm();
 	const { setSiteTitle } = useDispatch( STORE_KEY );
 	const [ isTouched, setIsTouched ] = React.useState( false );
-	const showVerticalInput = config.isEnabled( 'gutenboarding/show-vertical-input' );
 	const siteTitleExamples = [
 		/* translators: This is an example of a site name,
 		   feel free to create your own but please keep it under 22 characters */
@@ -80,18 +77,6 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit, inputRef } ) =
 		setIsTouched( true );
 	};
 
-	const isMobile = useViewportMatch( 'small', '<' );
-
-	// translators: label for site title input in Gutenboarding
-	let inputLabel;
-	if ( showVerticalInput && ! isMobile ) {
-		inputLabel = __( "It's called" );
-	} else if ( isAnchorFmSignup ) {
-		inputLabel = __( 'My podcast is called' );
-	} else {
-		inputLabel = __( 'My site is called' );
-	}
-
 	const placeHolder = useTyper( siteTitleExamples, ! siteTitle, {
 		delayBetweenCharacters: 70,
 	} );
@@ -106,7 +91,8 @@ const SiteTitle: React.FunctionComponent< Props > = ( { onSubmit, inputRef } ) =
 				className="site-title__input-label"
 				data-e2e-string="My site is called"
 			>
-				{ inputLabel }
+				{ /* translators: label for site title input in Gutenboarding */ }
+				{ isAnchorFmSignup ? __( 'My podcast is called' ) : __( 'My site is called' ) }
 			</label>
 			<div className="site-title__input-wrapper">
 				{ /* Adding key makes it more performant
