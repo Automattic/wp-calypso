2f8bd314086cc200a4893035505a6b2189f0ccbb
Marin Atanasov <tyxla@abv.bg>
2022-01-14 11:35:51 +0200

    Relative imports

--- client/state/happychat/selectors/can-user-send-messages.js
+++ client/state/happychat/selectors/can-user-send-messages.js
@@ -5,8 +5,8 @@ import {
 	HAPPYCHAT_CHAT_STATUS_MISSED,
 	HAPPYCHAT_CHAT_STATUS_PENDING,
 } from 'calypso/state/happychat/constants';
-import getHappychatChatStatus from 'calypso/state/happychat/selectors/get-happychat-chat-status';
-import isHappychatClientConnected from 'calypso/state/happychat/selectors/is-happychat-client-connected';
+import getHappychatChatStatus from './get-happychat-chat-status';
+import isHappychatClientConnected from './is-happychat-client-connected';
 
 const CHAT_STATUSES = [
 	HAPPYCHAT_CHAT_STATUS_BLOCKED,
