61dae5b12a767ee052c72f8b039e6b0cc248d7bd
Payton Swick <payton@foolord.com>
2022-01-17 19:43:29 -0500

    Fix typo in comment

--- client/state/guided-tours/selectors/index.js
+++ client/state/guided-tours/selectors/index.js
@@ -16,7 +16,7 @@ import 'calypso/state/guided-tours/init';
 const SECTIONS_WITHOUT_TOURS = [
 	'signup',
 	'upgrades', // checkout
-	'checkout-pending', // checkout pending pager
+	'checkout-pending', // checkout pending page
 	'checkout-thank-you', // thank you page
 ];
 
