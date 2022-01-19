f1f27eed6c4a2440bb773b6bae49b48925989d0c
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2022-01-19 18:52:33 -0300

    Remove design picker sidebar since FSE is always enabled

--- client/landing/gutenboarding/onboarding-block/designs/index.tsx
+++ client/landing/gutenboarding/onboarding-block/designs/index.tsx
@@ -1,61 +1,30 @@
-import { isEnabled } from '@automattic/calypso-config';
-import DesignPicker, {
-	FeaturedPicksButtons,
-	PremiumBadge,
-	getAvailableDesigns,
-	useCategorization,
-} from '@automattic/design-picker';
-import { useLocale, englishLocales } from '@automattic/i18n-utils';
+import DesignPicker, { PremiumBadge, getAvailableDesigns } from '@automattic/design-picker';
+import { useLocale } from '@automattic/i18n-utils';
 import { Title, SubTitle, ActionButtons, BackButton } from '@automattic/onboarding';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useI18n } from '@wordpress/react-i18n';
-import classnames from 'classnames';
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import * as React from 'react';
 import useStepNavigation from '../../hooks/use-step-navigation';
 import { useTrackStep } from '../../hooks/use-track-step';
 import { useIsAnchorFm } from '../../path';
 import { STORE_KEY as ONBOARD_STORE } from '../../stores/onboard';
-import type { Design, Category } from '@automattic/design-picker';
+import type { Design } from '@automattic/design-picker';
 
 import './style.scss';
 
-// Ensures Blog category appears at the top of the design category list
-// (directly below the All Themes category).
-const sortBlogToTop = ( a: Category, b: Category ) => {
-	if ( a.slug === b.slug ) {
-		return 0;
-	} else if ( a.slug === 'blog' ) {
-		return -1;
-	} else if ( b.slug === 'blog' ) {
-		return 1;
-	}
-	return 0;
-};
+interface HeaderProps {
+	isAnchorFmSignup: boolean;
+}
 
-const Header: React.FunctionComponent = () => {
+const Header: React.FunctionComponent< HeaderProps > = ( { isAnchorFmSignup } ) => {
 	const { __ } = useI18n();
-	const locale = useLocale();
-	const isAnchorFmSignup = useIsAnchorFm();
-	const isDesignPickerCategoriesEnabled =
-		! isAnchorFmSignup && isEnabled( 'signup/design-picker-categories' );
 
 	const { goBack } = useStepNavigation();
-	const title = isDesignPickerCategoriesEnabled ? __( 'Themes' ) : __( 'Choose a design' );
-	let subTitle = isAnchorFmSignup
+	const title = __( 'Choose a design' );
+	const subTitle = isAnchorFmSignup
 		? __( 'Pick a homepage layout for your podcast site. You can customize or change it later.' )
 		: __( 'Pick your favorite homepage layout. You can customize or change it later.' );
-
-	if ( isDesignPickerCategoriesEnabled ) {
-		subTitle = __( 'Choose a starting theme. You can change it later.' );
-
-		if ( englishLocales.includes( locale ) ) {
-			// An English only trick so the line wraps between sentences.
-			subTitle = subTitle
-				.replace( /\s/g, '\xa0' ) // Replace all spaces with non-breaking spaces
-				.replace( /\.\s/g, '. ' ); // Replace all spaces at the end of sentences with a regular breaking space
-		}
-	}
 
 	return (
 		<div className="designs__header">
@@ -86,36 +55,6 @@ const Designs: React.FunctionComponent = () => {
 		};
 	} );
 	const isAnchorFmSignup = useIsAnchorFm();
-
-	// As the amount of the anchorfm related designs is little, we don't need to enable categories filter
-	const isDesignPickerCategoriesEnabled =
-		! isAnchorFmSignup && isEnabled( 'signup/design-picker-categories' );
-
-	const useFeaturedPicksButtons =
-		isDesignPickerCategoriesEnabled &&
-		isEnabled( 'signup/design-picker-use-featured-picks-buttons' );
-
-	const allDesigns = randomizedDesigns.featured.filter(
-		( design ) =>
-			// TODO Add finalized design templates to available designs config
-			// along with `is_anchorfm` prop (config is stored in the
-			// `@automattic/design-picker` package)
-			isAnchorFmSignup === design.features.includes( 'anchorfm' )
-	);
-
-	const { designs, featuredPicksDesigns } = useMemo(
-		() => ( {
-			designs: allDesigns.filter( ( theme ) => ! theme.is_featured_picks ),
-			featuredPicksDesigns: allDesigns.filter( ( theme ) => theme.is_featured_picks ),
-		} ),
-		[ allDesigns ]
-	);
-
-	const categorization = useCategorization( designs, {
-		showAllFilter: isDesignPickerCategoriesEnabled,
-		defaultSelection: null,
-		sort: sortBlogToTop,
-	} );
 
 	useTrackStep( 'DesignSelection', () => ( {
 		selected_design: selectedDesign?.slug,
@@ -164,27 +103,13 @@ const Designs: React.FunctionComponent = () => {
 
 	return (
 		<div className="gutenboarding-page designs">
-			{ ! isDesignPickerCategoriesEnabled && <Header /> }
+			<Header isAnchorFmSignup={ isAnchorFmSignup } />
 			<DesignPicker
-				className={ classnames( {
-					'designs__has-categories': isDesignPickerCategoriesEnabled,
-				} ) }
-				designs={ useFeaturedPicksButtons ? designs : allDesigns }
+				designs={ randomizedDesigns.featured }
 				isGridMinimal={ isAnchorFmSignup }
 				locale={ locale }
 				onSelect={ onSelect }
 				premiumBadge={ <PremiumBadge /> }
-				categorization={ isDesignPickerCategoriesEnabled ? categorization : undefined }
-				categoriesHeading={ isDesignPickerCategoriesEnabled && <Header /> }
-				categoriesFooter={
-					useFeaturedPicksButtons && (
-						<FeaturedPicksButtons
-							className="designs__featured-picks-buttons"
-							designs={ featuredPicksDesigns }
-							onSelect={ onSelect }
-						/>
-					)
-				}
 				highResThumbnails
 			/>
 		</div>
