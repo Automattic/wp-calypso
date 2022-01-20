9e590c516ed2d85e967809c33a5218b9c5979639
Marin Atanasov <tyxla@abv.bg>
2022-01-20 15:51:51 +0200

    Export: Refactor away ExportCard from UNSAFE_ methods

--- client/my-sites/exporter/export-card/index.jsx
+++ client/my-sites/exporter/export-card/index.jsx
@@ -20,16 +20,8 @@ import {
 import AdvancedSettings from './advanced-settings';
 
 class ExportCard extends Component {
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillMount() {
+	componentDidMount() {
 		this.props.advancedSettingsFetch( this.props.siteId );
-	}
-
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillReceiveProps( newProps ) {
-		if ( newProps.siteId !== this.props.siteId ) {
-			this.props.advancedSettingsFetch( newProps.siteId );
-		}
 	}
 
 	render() {
