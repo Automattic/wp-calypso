29291fd2a64b741e5eb9110eebea0efca9d6313a
Konstantin Obenland <obenland@automattic.com>
2022-01-21 08:57:44 -0800

    Woo Installer: Make zip field optional. (#60321)

--- client/signup/steps/woocommerce-install/step-store-address/index.tsx
+++ client/signup/steps/woocommerce-install/step-store-address/index.tsx
@@ -236,16 +236,14 @@ function useAddressFormValidation( siteId: number ) {
 		errors[ WOOCOMMERCE_STORE_ADDRESS_1 ] = ! get( WOOCOMMERCE_STORE_ADDRESS_1 )
 			? __( 'Please add an address' )
 			: '';
-		errors[ WOOCOMMERCE_STORE_ADDRESS_2 ] = ''; // Optional field
+		errors[ WOOCOMMERCE_STORE_ADDRESS_2 ] = ''; // Optional field.
 		errors[ WOOCOMMERCE_DEFAULT_COUNTRY ] = ! get( WOOCOMMERCE_DEFAULT_COUNTRY )
 			? __( 'Please select a country / region' )
 			: '';
 		errors[ WOOCOMMERCE_STORE_CITY ] = ! get( WOOCOMMERCE_STORE_CITY )
 			? __( 'Please add a city' )
 			: '';
-		errors[ WOOCOMMERCE_STORE_POSTCODE ] = ! get( WOOCOMMERCE_STORE_POSTCODE )
-			? __( 'Please add a postcode' )
-			: '';
+		errors[ WOOCOMMERCE_STORE_POSTCODE ] = ''; // Optional field.
 		errors[ WOOCOMMERCE_ONBOARDING_PROFILE ] = ! emailValidator.validate(
 			get( WOOCOMMERCE_ONBOARDING_PROFILE )?.[ 'store_email' ]
 		)
