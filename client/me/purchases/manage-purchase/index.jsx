6ad4d40558818cc32bd992e372b4d7d2270ab7a4
Chris Diehl <jjchrisdiehl@gmail.com>
2022-01-21 17:03:30 -0500

    Updated styling to be mobile first

--- client/me/purchases/manage-purchase/index.jsx
+++ client/me/purchases/manage-purchase/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable wpcalypso/jsx-classname-namespace */
 import {
 	isPersonal,
 	isPremium,
@@ -524,7 +525,7 @@ class ManagePurchase extends Component {
 		};
 
 		return (
-			<CompactCard href={ link } className={ `remove-purchase__card` } onClick={ onClick }>
+			<CompactCard href={ link } className="remove-purchase__card" onClick={ onClick }>
 				{ text }
 			</CompactCard>
 		);
