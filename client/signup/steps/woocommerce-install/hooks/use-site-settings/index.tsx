9f463e12b300ccf75386c0dd8c378a302d153249
Louis Laugesen <louis.laugesen@gmail.com>
2022-01-21 13:37:59 +1100

    Send woocommerce_onboarding_profile.completed when submitting the business info step (#60231)
    Co-authored-by: Allison Levine <allison.levine@automattic.com>

--- client/signup/steps/woocommerce-install/hooks/use-site-settings/index.tsx
+++ client/signup/steps/woocommerce-install/hooks/use-site-settings/index.tsx
@@ -1,4 +1,3 @@
-import { uniqueBy } from '@automattic/js-utils';
 import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -6,7 +5,10 @@ import {
 	saveSiteSettings,
 	updateSiteSettings,
 } from 'calypso/state/site-settings/actions';
-import { getSiteSettings } from 'calypso/state/site-settings/selectors';
+import {
+	getSiteSettings,
+	isSiteSettingsSaveSuccessful,
+} from 'calypso/state/site-settings/selectors';
 
 // WooCommerce enable options.
 export const WOOCOMMERCE_STORE_ADDRESS_1 = 'woocommerce_store_address';
@@ -37,12 +39,9 @@ export function useSiteSettings( siteId: number ) {
 	const dispatch = useDispatch();
 
 	const settings = useSelector( ( state ) => getSiteSettings( state, siteId ) );
+	const settingsSaved = useSelector( ( state ) => isSiteSettingsSaveSuccessful( state, siteId ) );
 
-	/*
-	 * Private settings store.
-	 * It collects the options that will be updated/saved
-	 */
-	const [ editedSettings, setEditedSettings ] = useState( [] as optionNameType[] );
+	const [ updates, setUpdates ] = useState( {} as Record< optionNameType, OptionValueType > );
 
 	// Dispatch action to request the site settings.
 	useEffect( () => {
@@ -51,10 +50,14 @@ export function useSiteSettings( siteId: number ) {
 		}
 
 		dispatch( requestSiteSettings( siteId ) );
-	}, [ dispatch, siteId ] );
+	}, [ dispatch, siteId, settingsSaved ] );
 
 	// Simple getter helper.
 	function get( option: optionNameType ) {
+		if ( updates[ option ] ) {
+			return updates[ option ];
+		}
+
 		if ( ! settings || Object.keys( settings ).length === 0 ) {
 			return '';
 		}
@@ -68,12 +71,13 @@ export function useSiteSettings( siteId: number ) {
 	 */
 	const update = useCallback(
 		( option: optionNameType, value: OptionValueType ) => {
-			setEditedSettings( ( state ) => uniqueBy( [ ...state, option ] ) );
+			updates[ option ] = value;
+			setUpdates( updates );
 
 			// Store the edited option in the private store.
 			dispatch( updateSiteSettings( siteId, { [ option ]: value } ) );
 		},
-		[ siteId, dispatch ]
+		[ siteId, dispatch, updates ]
 	);
 
 	/*
@@ -81,22 +85,8 @@ export function useSiteSettings( siteId: number ) {
 	 * The data will be saved to the remote server.
 	 */
 	const save = useCallback( () => {
-		if ( ! editedSettings || ! editedSettings.length ) {
-			return;
-		}
-
-		/*
-		 * Save the edited options to the server.
-		 * After the save is complete, clean the private store.
-		 */
-		const newSettings = editedSettings.reduce(
-			( acc, settingKey ) => ( { ...acc, [ settingKey ]: settings[ settingKey ] } ),
-			{}
-		);
-
-		dispatch( saveSiteSettings( siteId, newSettings ) );
-		setEditedSettings( [] );
-	}, [ dispatch, editedSettings, settings, siteId ] );
+		dispatch( saveSiteSettings( siteId, updates ) );
+	}, [ dispatch, updates, siteId ] );
 
 	return { save, update, get };
 }
