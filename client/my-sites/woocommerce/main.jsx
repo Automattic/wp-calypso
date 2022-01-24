c141853ef5b38433c88c983eaed4f646b38c024f
Allison Levine <allilevine@users.noreply.github.com>
2022-01-24 10:14:03 -0500

    Revert "Remove plugins loaded check (#60348)"
    This reverts commit 9e22bb095c39dcd94041c0f74d8ce0b48ed5d9d1.

--- client/my-sites/woocommerce/main.jsx
+++ client/my-sites/woocommerce/main.jsx
@@ -5,11 +5,15 @@ import QueryJetpackPlugins from 'calypso/components/data/query-jetpack-plugins';
 import QuerySiteFeatures from 'calypso/components/data/query-site-features';
 import Main from 'calypso/components/main';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import { isLoaded as arePluginsLoaded } from 'calypso/state/plugins/installed/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import RequiredPluginsInstallView from './dashboard/required-plugins-install-view';
 
 function WooCommerce() {
 	const siteId = useSelector( getSelectedSiteId );
+	const areInstalledPluginsLoadedIntoState = useSelector( ( state ) =>
+		arePluginsLoaded( state, siteId )
+	);
 
 	if ( ! siteId ) {
 		return null;
@@ -22,7 +26,7 @@ function WooCommerce() {
 				<PageViewTracker path="/woocommerce-installation/:site" title="WooCommerce Installation" />
 				<QuerySiteFeatures siteId={ siteId } />
 				<QueryJetpackPlugins siteIds={ [ siteId ] } />
-				<RequiredPluginsInstallView siteId={ siteId } />
+				{ areInstalledPluginsLoadedIntoState && <RequiredPluginsInstallView siteId={ siteId } /> }
 			</Main>
 		</div>
 	);
