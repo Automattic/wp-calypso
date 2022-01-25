7b10bcfdd2d2c35378b44141b0efff10cc2bfd00
Jeremy Yip <jeremy.yip@automattic.com>
2022-01-25 10:21:40 -0800

    Record the tracks event in all editor types

--- apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-site-editor-save-click.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-site-editor-save-click.js
@@ -1,18 +1,16 @@
-import { getEditorType } from '../utils';
 import tracksRecordEvent from './track-record-event';
 
 /**
- * Return the event definition object to track `wpcom_block_editor_site_editor_save_click`.
+ * Return the event definition object to track `wpcom_block_editor_save_click`.
  *
  * @returns {import('./types').DelegateEventHandler} event object definition.
  */
 export default () => ( {
-	id: 'wpcom-block-editor-site-editor-save-click',
-	selector: '.editor-entities-saved-states__save-button',
+	id: 'wpcom-block-editor-save-click',
+	// The first selector is for site editing. The second is for post editing.
+	selector: '.editor-entities-saved-states__save-button, .editor-post-publish-button',
 	type: 'click',
 	handler: () => {
-		if ( getEditorType() === 'site' ) {
-			tracksRecordEvent( 'wpcom_block_editor_site_editor_save_click' );
-		}
+		tracksRecordEvent( 'wpcom_block_editor_save_click' );
 	},
 } );
