70a6fe284284f20062b22ee3fd1879fc56cb4558
Aneesh Devasthale <aneeshd16@users.noreply.github.com>
2022-01-25 19:03:21 +0530

    Refactors AccordionForm into an independent component (#60334)
    * Refactors AccordionForm into an independent component
    
    * Rename useSections to sectionGenerator

---
+++ client/signup/accordion-form/types.ts
@@ -0,0 +1,25 @@
+import { TranslateResult, useTranslate } from 'i18n-calypso';
+import { ChangeEvent, ReactNode } from 'react';
+
+export type ValidationErrors = Record< string, TranslateResult | null >;
+
+export interface ValidationResult {
+	result: boolean;
+	errors: ValidationErrors;
+}
+
+export type ValidatorFunction< T > = ( formValues: T ) => ValidationResult;
+
+export interface AccordionSectionProps< T > {
+	title: TranslateResult;
+	component: ReactNode;
+	showSkip: boolean;
+	summary: string | undefined;
+	validate?: ValidatorFunction< T >;
+}
+export interface sectionGeneratorReturnType< T > {
+	translate: ReturnType< typeof useTranslate >;
+	formValues: T;
+	formErrors: ValidationErrors;
+	onChangeField: ( { target: { name, value } }: ChangeEvent< HTMLInputElement > ) => void;
+}
