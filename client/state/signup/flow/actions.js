c12a73cb9be089a7f43cc438d90b7a51edc8afac
arthur <arthur.chu@automattic.com>
2022-01-26 16:17:22 +0800

    Signup: Persist excluded steps for the flow with branching steps

--- client/state/signup/flow/actions.js
+++ client/state/signup/flow/actions.js
@@ -1,6 +1,9 @@
 import {
 	SIGNUP_CURRENT_FLOW_NAME_SET,
 	SIGNUP_PREVIOUS_FLOW_NAME_SET,
+	SIGNUP_FLOW_ADD_EXCLUDE_STEPS,
+	SIGNUP_FLOW_REMOVE_EXCLUDE_STEPS,
+	SIGNUP_FLOW_RESET_EXCLUDE_STEPS,
 } from 'calypso/state/action-types';
 
 import 'calypso/state/signup/init';
@@ -16,5 +19,25 @@ export function setPreviousFlowName( flowName ) {
 	return {
 		type: SIGNUP_PREVIOUS_FLOW_NAME_SET,
 		flowName,
+	};
+}
+
+export function addExcludeSteps( steps ) {
+	return {
+		type: SIGNUP_FLOW_ADD_EXCLUDE_STEPS,
+		steps,
+	};
+}
+
+export function removeExcludeSteps( steps ) {
+	return {
+		type: SIGNUP_FLOW_REMOVE_EXCLUDE_STEPS,
+		steps,
+	};
+}
+
+export function resetExcludeSteps() {
+	return {
+		type: SIGNUP_FLOW_RESET_EXCLUDE_STEPS,
 	};
 }
