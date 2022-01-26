bea882e8a60d81ef101c29be68576449293646d1
Jarda Snajdr <jsnajdr@gmail.com>
2022-01-26 15:02:21 +0100

    Migrate the requestActivityActionTypeCounts data getter to React Query (#60399)
    * Migrate the requestActivityActionTypeCounts data getter to React Query
    
    * Make the query results stale after 10 seconds

--- client/state/data-getters/index.js
+++ client/state/data-getters/index.js
@@ -1,3 +1,2 @@
-export { default as requestActivityActionTypeCounts } from './request-activity-action-type-counts';
 export { default as getRequestActivityLogsId } from './get-request-activity-logs-id';
 export { default as requestActivityLogs } from './request-activity-logs';
