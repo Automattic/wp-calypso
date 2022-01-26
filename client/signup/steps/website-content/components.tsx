04b4c27475b095621d7441410da3ed204ce78ca7
Dehan <dehandecroos@gmail.com>
2022-01-26 15:21:09 +0530

    Component to upload image to media library and related UI (#60436)
    * Adds site info store to redux
    
    * Adds site information step (#59986)
    
    * Adds site information step
    
    * Adds siteSlug to dependencies for setDesignOnSite
    
    * Changes div to React fragment
    
    * Email text fix
    
    * Fixes unit test
    
    * Rebased
    
    * Removes hardcoded serial number
    
    * Adds errors for form validation
    
    * Fixes RTL bug
    
    * Review feedback - 1
    
    * Review Feedback - 2
    
    * Created new component AccordionForm and related refactoring.
    * Typo fix.
    
    * Fix typo
    
    * Fixes form styles
    
    * Refactors function name
    
    * Removes typeform dependency and config ids (#60029)
    
    * The state and step scaffolding for the website-content step in DIFM onboarding (#60293)
    
    * Initial state infrastructure for website content
    
    * Move website content to different flow
    
    * Fix signup destination call
    
    * Refactor data structure
    
    * Apply suggestions from code review
    
    Co-authored-by: Aneesh Devasthale <aneeshd16@users.noreply.github.com>
    
    Co-authored-by: Aneesh Devasthale <aneeshd16@users.noreply.github.com>
    
    * Refactors AccordionForm into an independent component (#60334)
    
    * Refactors AccordionForm into an independent component
    
    * Rename useSections to sectionGenerator
    
    * A seperate component for picking files and uploading them to the wordpress user's relevant account.
    
    * Apply suggestions from code review
    
    Co-authored-by: Aneesh Devasthale <aneeshd16@users.noreply.github.com>
    
    * Review fixes
    
    * Update client/signup/steps/website-content/components.tsx
    
    Co-authored-by: Aneesh Devasthale <aneeshd16@users.noreply.github.com>
    
    * Cursor fix
    
    Co-authored-by: Aneesh Devasthale <aneeshdevasthale@gmail.com>
    Co-authored-by: Aneesh Devasthale <aneeshd16@users.noreply.github.com>

---
+++ client/signup/steps/website-content/components.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+import FormLabel from 'calypso/components/forms/form-label';
+
+export const HorizontalGrid = styled.div`
+	display: flex;
+	justify-content: space-between;
+	width: 627px;
+`;
+
+export const Label = styled( FormLabel )`
+	font-style: normal;
+	font-weight: 600;
+	font-size: 13px;
+	line-height: 20px;
+	text-align: center;
+	color: var( --studio-gray-80 );
+	text-decoration-line: underline;
+	letter-spacing: -0.16px;
+	text-overflow: ellipsis;
+	max-width: 195px;
+	white-space: nowrap;
+	overflow: hidden;
+`;
+export const SubLabel = styled( Label )`
+	font-weight: 400;
+	text-decoration-line: none;
+	color: ${ ( props ) => ( props.color ? props.color : 'inherited' ) };
+`;
+
+export const Paragraph = styled.p`
+	font-size: 13px;
+`;
---
+++ client/signup/steps/website-content/wordpress-media-upload.tsx
@@ -0,0 +1,143 @@
+import styled from '@emotion/styled';
+import debugFactory from 'debug';
+import { useTranslate } from 'i18n-calypso';
+import { useState } from 'react';
+import placeholder from 'calypso/assets/images/difm/placeholder.svg';
+import FilePicker from 'calypso/components/file-picker';
+import Spinner from 'calypso/components/spinner';
+import { useAddMedia } from 'calypso/data/media/use-add-media';
+import { SiteData } from 'calypso/state/ui/selectors/site-data';
+import { Label, SubLabel } from './components';
+
+const debug = debugFactory( 'difm:website-content' );
+
+const UPLOAD_STATES = {
+	NOT_SELECTED: 'NOT_SELECTED',
+	IN_PROGRESS: 'IN_PROGRESS',
+	COMPLETED: 'COMPLETED',
+	FAILED: 'FAILED',
+};
+
+const FileSelectThumbnailContainer = styled.div`
+	cursor: pointer;
+	min-width: 190px;
+	width: 190px;
+	max-height: 145px;
+	min-height: 93px;
+	background: rgba( 187, 224, 250, 0.12 );
+	border: 1px dashed var( --studio-gray-5 );
+	border-radius: 5px;
+	text-align: center;
+	padding: 26px 5px;
+`;
+
+const CroppedImage = styled.div`
+	overflow: hidden;
+	height: 80%;
+	text-align: center;
+	img {
+		margin: 0 auto;
+	}
+`;
+
+const Preview = styled.div`
+	width: 140px;
+	height: 43px;
+	margin: 0 auto;
+`;
+
+interface WordpressMediaUploadProps {
+	onMediaUploaded: ( { title, URL, ID }: { title: string; URL: string; ID: string } ) => void;
+	mediaId: number;
+	site: SiteData;
+}
+
+export function WordpressMediaUpload( {
+	mediaId,
+	site,
+	onMediaUploaded,
+}: WordpressMediaUploadProps ) {
+	const [ uploadState, setUploadState ] = useState( UPLOAD_STATES.NOT_SELECTED );
+	const [ uploadedImageUrl, setUploadedImageUrl ] = useState( '' );
+	const [ imageCaption, setImageCaption ] = useState( '' );
+	const translate = useTranslate();
+	const addMedia = useAddMedia();
+	const onPick = async function ( file: FileList ) {
+		setImageCaption( '' );
+		setUploadState( UPLOAD_STATES.IN_PROGRESS );
+		try {
+			const [ { title, URL, ID } ] = await addMedia( file, site );
+			setUploadedImageUrl( URL );
+			setImageCaption( title );
+			onMediaUploaded( { title, URL, ID } );
+			setUploadState( UPLOAD_STATES.COMPLETED );
+		} catch ( e: any ) {
+			setUploadState( UPLOAD_STATES.FAILED );
+			debug( 'Image upload failed' );
+			debug( e.message );
+		}
+	};
+
+	switch ( uploadState ) {
+		case UPLOAD_STATES.COMPLETED:
+			return (
+				<FilePicker key={ mediaId } accept="image/*" onPick={ onPick }>
+					<FileSelectThumbnailContainer>
+						<div>
+							<Preview>
+								<CroppedImage>
+									<img src={ uploadedImageUrl } alt={ imageCaption } />
+								</CroppedImage>
+							</Preview>
+							<Label>{ translate( 'Change File' ) }</Label>
+							<SubLabel>{ imageCaption }</SubLabel>
+						</div>
+					</FileSelectThumbnailContainer>
+				</FilePicker>
+			);
+		case UPLOAD_STATES.IN_PROGRESS:
+			return (
+				<FileSelectThumbnailContainer key={ mediaId }>
+					<div>
+						<Preview>
+							<Spinner />
+						</Preview>
+					</div>
+				</FileSelectThumbnailContainer>
+			);
+		case UPLOAD_STATES.FAILED:
+			return (
+				<FilePicker accept="image/*" onPick={ onPick } key={ mediaId }>
+					<FileSelectThumbnailContainer>
+						<div>
+							<Preview>
+								<img src={ placeholder } alt="placeholder" />
+							</Preview>
+							<Label>{ translate( 'Choose file' ) }</Label>
+							<SubLabel color="red">{ translate( 'Image upload failed' ) }</SubLabel>
+						</div>
+					</FileSelectThumbnailContainer>
+				</FilePicker>
+			);
+
+		case UPLOAD_STATES.NOT_SELECTED:
+		default:
+			return (
+				<FilePicker accept="image/*" onPick={ onPick } key={ mediaId }>
+					<FileSelectThumbnailContainer>
+						<div>
+							<Preview>
+								<img src={ placeholder } alt="placeholder" />
+							</Preview>
+							<div
+								className="wordpress-media-upload__temporary-spacer"
+								style={ { width: '100px', height: '22px' } }
+							/>
+							<Label>{ translate( 'Choose file' ) }</Label>
+							{ /* <SubLabel>{ translate( 'or drag here')}</SubLabel> */ }
+						</div>
+					</FileSelectThumbnailContainer>
+				</FilePicker>
+			);
+	}
+}
