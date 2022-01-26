850fc05ebb55d5295c166d8775b0db9355140823
Markus Dobmann <m.do@posteo.net>
2022-01-26 13:57:07 +0100

    `QueryReaderSite`: refactor away from `UNSAFE_*`

--- client/components/data/query-reader-site/index.jsx
+++ client/components/data/query-reader-site/index.jsx
@@ -1,49 +1,24 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { requestSite } from 'calypso/state/reader/sites/actions';
 import { shouldSiteBeFetched } from 'calypso/state/reader/sites/selectors';
 
-class QueryReaderSite extends Component {
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillMount() {
-		if ( this.props.shouldSiteBeFetched ) {
-			this.props.requestSite( this.props.siteId );
+function QueryReaderSite( { siteId } ) {
+	const dispatch = useDispatch();
+	const shouldFetch = useSelector( ( state ) => shouldSiteBeFetched( state, siteId ) );
+
+	useEffect( () => {
+		if ( siteId && shouldFetch ) {
+			dispatch( requestSite( siteId ) );
 		}
-	}
+	}, [ dispatch, siteId, shouldFetch ] );
 
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillReceiveProps( nextProps ) {
-		if ( ! nextProps.shouldSiteBeFetched || this.props.siteId === nextProps.siteId ) {
-			return;
-		}
-
-		nextProps.requestSite( nextProps.siteId );
-	}
-
-	render() {
-		return null;
-	}
+	return null;
 }
 
 QueryReaderSite.propTypes = {
 	siteId: PropTypes.number,
-	shouldSiteBeFetched: PropTypes.bool,
-	requestSite: PropTypes.func,
 };
 
-QueryReaderSite.defaultProps = {
-	requestSite: () => {},
-};
-
-export default connect(
-	( state, ownProps ) => {
-		const { siteId } = ownProps;
-		return {
-			shouldSiteBeFetched: shouldSiteBeFetched( state, siteId ),
-		};
-	},
-	{
-		requestSite,
-	}
-)( QueryReaderSite );
+export default QueryReaderSite;
