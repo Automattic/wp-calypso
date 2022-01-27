4309cd9b020a3c96c9cd896c6b7dad414f2ec2f3
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2022-01-27 10:52:36 +0200

    Editor: Refactor PostRevisionsDialog away from UNSAFE_ methods (#60484)

--- client/post-editor/editor-revisions/dialog.jsx
+++ client/post-editor/editor-revisions/dialog.jsx
@@ -36,14 +36,12 @@ class PostRevisionsDialog extends PureComponent {
 		translate: PropTypes.func.isRequired,
 	};
 
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillMount() {
+	componentDidMount() {
 		this.toggleBodyClass( { isVisible: this.props.isVisible } );
 		this.props.selectPostRevision( null );
 	}
 
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillUpdate( { isVisible } ) {
+	componentDidUpdate( { isVisible } ) {
 		this.toggleBodyClass( { isVisible } );
 	}
 
