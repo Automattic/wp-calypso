8d0c685fbd2fe2b5f7f856bbad7e2ce05fe42f27
Emanuele Buccelli <escapemanuele@gmail.com>
2022-01-28 18:27:16 +0100

    Update packages/tour-kit/src/types.ts
    Update documentation
    
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>

--- packages/tour-kit/src/types.ts
+++ packages/tour-kit/src/types.ts
@@ -15,7 +15,13 @@ export type Step = {
 	};
 	options?: {
 		classNames?: {
-			desktop?: string | string[];
+		    /**
+		    * `desktop` classes are applied when min-width is larger or equal to 480px.
+		    */
+		    desktop?: string;
+		    /**
+		    * `mobile` classes are applied when max-width is smaller than 480px.
+		    */
 			mobile?: string | string[];
 		};
 	};
