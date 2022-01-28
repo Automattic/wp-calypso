0198508e3219d159580f0976076b7bdae36e5233
Caleb Bauermeister <cbauerman@gmail.com>
2022-01-28 09:19:44 -0800

    remove logic for coupon discount calculations

--- client/components/jetpack/intro-pricing-banner/index.tsx
+++ client/components/jetpack/intro-pricing-banner/index.tsx
@@ -1,13 +1,11 @@
 import { useViewportMatch } from '@wordpress/compose';
 import { useTranslate } from 'i18n-calypso';
 import { FunctionComponent, useMemo } from 'react';
-import { useSelector } from 'react-redux';
 import useDetectWindowBoundary from 'calypso/lib/detect-window-boundary';
 import { preventWidows } from 'calypso/lib/formatting';
 import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
 import { INTRO_PRICING_DISCOUNT_PERCENTAGE } from 'calypso/my-sites/plans/jetpack-plans/constants';
 import { isConnectStore } from 'calypso/my-sites/plans/jetpack-plans/product-grid/utils';
-import { getJetpackSaleCouponDiscountRatio } from 'calypso/state/marketing/selectors';
 import './style.scss';
 import guaranteeBadge from './14-day-badge.svg';
 import rocket from './rocket.svg';
@@ -17,7 +15,6 @@ const GUARANTEE_DAYS = 14;
 
 const IntroPricingBanner: FunctionComponent = () => {
 	const translate = useTranslate();
-	const jetpackSaleDiscount = useSelector( getJetpackSaleCouponDiscountRatio );
 	const isNotNarrow = useViewportMatch( 'medium', '>=' );
 
 	const CALYPSO_MASTERBAR_HEIGHT = 47;
@@ -34,12 +31,7 @@ const IntroPricingBanner: FunctionComponent = () => {
 
 	const outerDivProps = barRef ? { ref: barRef as React.RefObject< HTMLDivElement > } : {};
 
-	// jetpackSaleDiscount is a percent saved ( i.e. .2 ), INTRO_PRICING_DISCOUNT_PERCENTAGE
-	// INTRO_PRICING_DISCOUNT_PERCENTAGE is a percent * 100 saved i.e. 50
-	// We need to convert both to the total cost ( 1 - discount, 20% saved is 80% total cost, etc ), multiply them
-	// then switch back to the percent saved
-	const discountPercentage =
-		100 - ( ( 100 - INTRO_PRICING_DISCOUNT_PERCENTAGE ) / 100 ) * ( 1 - jetpackSaleDiscount ) * 100;
+	const discountPercentage = INTRO_PRICING_DISCOUNT_PERCENTAGE;
 
 	return (
 		<>
