14a54ea32e1b85f016de4d3692894333a66b5237
Wesley Berrêdo <wberredo@gmail.com>
2022-02-02 15:10:29 -0300

    Use the current props instead of the previous to retrieve plugin information.

--- client/components/data/query-jetpack-plugins/index.jsx
+++ client/components/data/query-jetpack-plugins/index.jsx
@@ -24,7 +24,7 @@ class QueryJetpackPlugins extends Component {
 		if ( isEqual( prevProps.siteIds, this.props.siteIds ) ) {
 			return;
 		}
-		this.refresh( prevProps.isRequestingForSites, prevProps.siteIds );
+		this.refresh( this.props.isRequestingForSites, this.props.prevProps.siteIds );
 	}
 
 	refresh( isRequesting, siteIds ) {
