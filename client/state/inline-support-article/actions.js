a3a154c718efc94eb4fc96583c9e995de3402ac6
Enej Bajgoric <enej.bajgoric@automattic.com>
2022-02-02 20:13:39 -0800

    undo changed

--- client/state/inline-support-article/actions.js
+++ client/state/inline-support-article/actions.js
@@ -14,6 +14,7 @@ import 'calypso/state/inline-support-article/init';
  * @param {string} options.actionLabel Label of the action
  * @param {string} options.actionUrl   URL of the action
  * @param {number} options.blogId      The blog id of the support article
+ *
  * @returns {object}		Action
  */
 export function openSupportArticleDialog( {
