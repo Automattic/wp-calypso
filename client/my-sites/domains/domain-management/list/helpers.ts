61530a318d0a1b318fd1c89fcea2a4c9fc43c775
Igor Giussani <gius80@gmail.com>
2022-02-03 12:33:17 +0100

    Add `domain-only` filter to All domains list (#60759)
    * add `domain-only` filter to All domains list
    
    * Update SiteData type
    
    * code refactoring

--- client/my-sites/domains/domain-management/list/helpers.ts
+++ client/my-sites/domains/domain-management/list/helpers.ts
@@ -1,8 +1,14 @@
 import { ResponseDomain } from 'calypso/lib/domains/types';
+import { SiteData } from 'calypso/state/ui/selectors/site-data';
 
 type FilterDomainsByOwnerType = (
 	domains: Array< ResponseDomain >,
 	filter: 'owned-by-me' | 'owned-by-others' | undefined
+) => Array< ResponseDomain >;
+
+type FilterDomainsDomainOnlyType = (
+	domains: Array< ResponseDomain >,
+	sites: Array< SiteData >
 ) => Array< ResponseDomain >;
 
 export const filterDomainsByOwner: FilterDomainsByOwnerType = ( domains, filter ) => {
@@ -13,5 +19,11 @@ export const filterDomainsByOwner: FilterDomainsByOwnerType = ( domains, filter 
 			return ! domain.currentUserIsOwner;
 		}
 		return true;
+	} );
+};
+
+export const filterDomainOnlyDomains: FilterDomainsDomainOnlyType = ( domains, sites ) => {
+	return domains.filter( ( domain: ResponseDomain ) => {
+		return sites[ domain.blogId ].options?.is_domain_only;
 	} );
 };
