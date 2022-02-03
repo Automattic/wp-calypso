33393e17d94ed38b52cb7249818275b4967b7455
Marin Atanasov <tyxla@abv.bg>
2022-02-03 14:37:08 +0200

    Add enabled to ensure siteId is passed

--- client/data/site-monitor/use-site-monitor-settings-query.js
+++ client/data/site-monitor/use-site-monitor-settings-query.js
@@ -4,6 +4,7 @@ import wp from 'calypso/lib/wp';
 const useSiteMonitorSettingsQuery = ( siteId ) =>
 	useQuery( [ 'site-monitor-settings', siteId ], () => wp.req.get( `/jetpack-blogs/${ siteId }` ), {
 		refetchOnWindowFocus: false,
+		enabled: !! siteId,
 		meta: {
 			persist: false,
 		},
