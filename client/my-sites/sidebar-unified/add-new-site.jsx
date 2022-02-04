9639df5827476ad30b2023cd4f5a713e5598cf36
Eru Penkman <roo2@protonmail.com>
2022-02-04 16:18:19 +1000

    add experiment to unified sidebar

--- client/my-sites/sidebar-unified/add-new-site.jsx
+++ client/my-sites/sidebar-unified/add-new-site.jsx
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
 import { Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import useOnboardingUrl from 'calypso/components/site-selector/use-onboarding-url';
 import TranslatableString from 'calypso/components/translatable/proptype';
 import SidebarCustomIcon from 'calypso/layout/sidebar/custom-icon';
 import SidebarItem from 'calypso/layout/sidebar/item';
 import SidebarSeparator from 'calypso/layout/sidebar/separator';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { getCurrentUser } from 'calypso/state/current-user/selectors';
-import getOnboardingUrl from 'calypso/state/selectors/get-onboarding-url';
 import { setLayoutFocus } from 'calypso/state/ui/layout-focus/actions';
 
 export const AddNewSite = ( { title, icon } ) => {
 	const reduxDispatch = useDispatch();
+	const onboardingUrl = useOnboardingUrl();
 	const visibleSiteCount = useSelector( getCurrentUser ).visible_site_count;
-	const onboardingUrl = useSelector( getOnboardingUrl );
-
+	if ( ! onboardingUrl ) {
+		return null;
+	}
 	if ( visibleSiteCount > 1 ) {
 		return null;
 	}
