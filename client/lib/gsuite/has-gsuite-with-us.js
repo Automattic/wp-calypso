17fdd965f5cd51c91fa5aa6f6118762aeac4adcd
Daniel <daniel.lopez@automattic.com>
2022-02-07 10:03:13 +0100

    Change endpoint to get domains and use emails api (#60708)

--- client/lib/gsuite/has-gsuite-with-us.js
+++ client/lib/gsuite/has-gsuite-with-us.js
@@ -3,7 +3,7 @@ import { getGSuiteSubscriptionStatus } from 'calypso/lib/gsuite/get-gsuite-subsc
 /**
  * Given a domain object, does that domain have G Suite with us.
  *
- * @param {undefined|{googleAppsSubscription?:{status?: string}}} domain - Domain object
+ * @param {undefined|{googleAppsSubscription?:{status?: string}}|import('calypso/lib/domains/types').ResponseDomain} domain - Domain object
  * @returns {boolean} - true if the domain is under our management, false otherwise
  */
 export function hasGSuiteWithUs( domain ) {
