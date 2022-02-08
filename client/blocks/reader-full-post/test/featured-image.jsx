3e303785d5df05aec75b23c7a3fa39f1ff393fa2
Marin Atanasov <tyxla@abv.bg>
2022-02-08 12:15:59 +0200

    Fix test

--- client/blocks/reader-full-post/test/featured-image.jsx
+++ client/blocks/reader-full-post/test/featured-image.jsx
@@ -1,12 +1,17 @@
-import { assert } from 'chai';
-import { shallow } from 'enzyme';
+/**
+ * @jest-environment jsdom
+ */
+import { mount } from 'enzyme';
 import FeaturedImage from '../featured-image';
 
 describe( 'FeaturedImage', () => {
 	test( 'sets the source to an empty string if the image fails to load', () => {
 		const nonExistentImage = 'http://sketchy-feed.com/missing-image-2.jpg';
-		const wrapper = shallow( <FeaturedImage src={ nonExistentImage } /> );
-		wrapper.instance().handleImageError();
-		assert.equal( '', wrapper.state( 'src' ) );
+		const wrapper = mount( <FeaturedImage src={ nonExistentImage } /> );
+		const div = wrapper.find( '.reader-full-post__featured-image' ).at( 0 ).getDOMNode();
+
+		wrapper.find( 'img' ).simulate( 'error' );
+
+		expect( getComputedStyle( div ).getPropertyValue( 'display' ) ).toBe( 'none' );
 	} );
 } );
