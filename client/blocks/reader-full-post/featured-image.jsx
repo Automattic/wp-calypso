03c54ecd84dea2913d80de43c2b2050422e17eed
Marin Atanasov <tyxla@abv.bg>
2022-02-08 11:43:37 +0200

    Reader: Refactor FeaturedImage away from UNSAFE_ lifecycle methods

--- client/blocks/reader-full-post/featured-image.jsx
+++ client/blocks/reader-full-post/featured-image.jsx
@@ -1,33 +1,15 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 
-export default class FeaturedImage extends Component {
-	constructor( props ) {
-		super();
-		this.state = { src: props.src };
-		this.handleImageError = () => {
-			this.setState( { src: '' } );
-		};
-	}
+const hideImageOnError = ( event ) => {
+	event.target.style.display = 'none';
+};
 
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillReceiveProps( nextProps ) {
-		if ( nextProps.src !== this.props.src ) {
-			this.setState( { src: nextProps.src } );
-		}
-	}
-
-	render() {
-		if ( ! this.state.src ) {
-			return null;
-		}
-
-		return (
-			<div className="reader-full-post__featured-image">
-				<img src={ this.state.src } onError={ this.handleImageError } alt={ this.props.alt } />
-			</div>
-		);
-	}
+export default function FeaturedImage( { src, alt } ) {
+	return (
+		<div className="reader-full-post__featured-image">
+			<img src={ src } alt={ alt } onError={ hideImageOnError } />
+		</div>
+	);
 }
 
 FeaturedImage.propTypes = {
