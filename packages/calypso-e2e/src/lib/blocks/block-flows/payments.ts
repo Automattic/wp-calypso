14ed944463a1c12697a723cf16d343748331c264
Dan Speckhard Pasque <dan.speckhard.pasque@automattic.com>
2022-02-09 11:41:28 -0600

    Playwright: Implement remaining Jetpack block smoke tests (#60935)
    * Add contact, payments, star, subscribe blocks
    
    * Add premium content block
    
    * Add migrated flows to their specs
    
    * Add slideshow and tiled gallery blocks

---
+++ packages/calypso-e2e/src/lib/blocks/block-flows/payments.ts
@@ -0,0 +1,54 @@
+import { BlockFlow, EditorContext, PublishedPostContext } from '..';
+
+interface ConfigurationData {
+	buttonText: string;
+}
+
+const blockParentSelector = '[aria-label="Block: Payments"]';
+const selectors = {
+	buttonText: `${ blockParentSelector } [role=textbox]`,
+};
+
+/**
+ * Class representing the flow of using a Contact Info block in the editor.
+ */
+export class PaymentsBlockFlow implements BlockFlow {
+	private configurationData: ConfigurationData;
+
+	/**
+	 * Constructs an instance of this block flow with data to be used when configuring and validating the block.
+	 *
+	 * @param {ConfigurationData} configurationData data with which to configure and validate the block
+	 */
+	constructor( configurationData: ConfigurationData ) {
+		this.configurationData = configurationData;
+	}
+
+	blockSidebarName = 'Payments';
+	blockEditorSelector = blockParentSelector;
+
+	/**
+	 * Configure the block in the editor with the configuration data from the constructor
+	 *
+	 * @param {EditorContext} context The current context for the editor at the point of test execution
+	 */
+	async configure( context: EditorContext ): Promise< void > {
+		await context.editorIframe.fill( selectors.buttonText, this.configurationData.buttonText );
+	}
+
+	/**
+	 * Validate the block in the published post
+	 *
+	 * @param {PublishedPostContext} context The current context for the published post at the point of test execution
+	 */
+	async validateAfterPublish( context: PublishedPostContext ): Promise< void > {
+		// Because Stripe isn't connected to this WordPress.com account, we shouldn't see this block published.
+		if (
+			await context.page.isVisible( `button:has-text("${ this.configurationData.buttonText }")` )
+		) {
+			throw new Error(
+				'Payments button should not be visible on published post without Stripe connection'
+			);
+		}
+	}
+}
--- test/e2e/specs/constants.js
+++ test/e2e/specs/constants.js
@@ -2,6 +2,7 @@ import path from 'path';
 
 export const TEST_FILES_FOLDER_PATH = path.join( __dirname, '..', 'image-uploads' );
 export const TEST_IMAGE_PATH = path.join( TEST_FILES_FOLDER_PATH, 'test-image-01.png' );
+export const ALT_TEST_IMAGE_PATH = path.join( TEST_FILES_FOLDER_PATH, 'test-image-02.png' );
 export const TEST_AUDIO_PATH = path.join( TEST_FILES_FOLDER_PATH, 'test-audio-01.mp3' );
 export const TEST_UNSUPPORTED_FILE_PATH = path.join(
 	TEST_FILES_FOLDER_PATH,
