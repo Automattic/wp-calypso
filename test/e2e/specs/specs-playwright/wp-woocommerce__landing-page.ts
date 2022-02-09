801f996171ffcd349b9ef12db17e356598aac413
Louis Laugesen <louis.laugesen@gmail.com>
2022-02-09 15:49:55 +1100

    Simplify

--- test/e2e/specs/specs-playwright/wp-woocommerce__landing-page.ts
+++ test/e2e/specs/specs-playwright/wp-woocommerce__landing-page.ts
@@ -11,59 +11,53 @@ declare const browser: Browser;
 describe( DataHelper.createSuiteTitle( 'WooCommerce Landing Page' ), function () {
 	let page: Page;
 
-	describe.each( [ { siteType: 'Simple', accountName: 'defaultUser' } ] )(
-		'View the learn more support article ($siteType sites)',
-		function ( { accountName } ) {
-			let landingPage: WoocommerceLandingPage;
+	describe( 'Learn more button opens a support article', function () {
+		let landingPage: WoocommerceLandingPage;
 
-			beforeAll( async () => {
-				page = await browser.newPage();
+		beforeAll( async () => {
+			page = await browser.newPage();
 
-				const testAccount = new TestAccount( accountName );
-				await testAccount.authenticate( page );
-			} );
+			const testAccount = new TestAccount( 'defaultUser' );
+			await testAccount.authenticate( page );
+		} );
 
-			afterAll( async () => {
-				await page.close();
-			} );
+		afterAll( async () => {
+			await page.close();
+		} );
 
-			it( 'Navigate to WooCommerce (landing page)', async function () {
-				const sidebarComponent = new SidebarComponent( page );
-				await sidebarComponent.navigate( 'WooCommerce' );
-			} );
+		it( 'Navigate to WooCommerce (landing page)', async function () {
+			const sidebarComponent = new SidebarComponent( page );
+			await sidebarComponent.navigate( 'WooCommerce' );
+		} );
 
-			it( 'Open Learn more', async function () {
-				landingPage = new WoocommerceLandingPage( page );
-				await landingPage.openLearnMore();
-			} );
-		}
-	);
+		it( 'Open Learn more', async function () {
+			landingPage = new WoocommerceLandingPage( page );
+			await landingPage.openLearnMore();
+		} );
+	} );
 
-	describe.each( [ { siteType: 'Simple', accountName: 'defaultUser' } ] )(
-		'Enter the woo installer at /start ($siteType sites)',
-		function ( { accountName } ) {
-			let landingPage: WoocommerceLandingPage;
+	describe( 'Start a new store takes you to the woo installer', function () {
+		let landingPage: WoocommerceLandingPage;
 
-			beforeAll( async () => {
-				page = await browser.newPage();
+		beforeAll( async () => {
+			page = await browser.newPage();
 
-				const testAccount = new TestAccount( accountName );
-				await testAccount.authenticate( page );
-			} );
+			const testAccount = new TestAccount( 'defaultUser' );
+			await testAccount.authenticate( page );
+		} );
 
-			afterAll( async () => {
-				await page.close();
-			} );
+		afterAll( async () => {
+			await page.close();
+		} );
 
-			it( 'Navigate to WooCommerce (landing page)', async function () {
-				const sidebarComponent = new SidebarComponent( page );
-				await sidebarComponent.navigate( 'WooCommerce' );
-			} );
+		it( 'Navigate to WooCommerce (landing page)', async function () {
+			const sidebarComponent = new SidebarComponent( page );
+			await sidebarComponent.navigate( 'WooCommerce' );
+		} );
 
-			it( 'Open WooCommerce installer (/start)', async function () {
-				landingPage = new WoocommerceLandingPage( page );
-				await landingPage.openStoreSetup();
-			} );
-		}
-	);
+		it( 'Open WooCommerce installer (/start)', async function () {
+			landingPage = new WoocommerceLandingPage( page );
+			await landingPage.openStoreSetup();
+		} );
+	} );
 } );
