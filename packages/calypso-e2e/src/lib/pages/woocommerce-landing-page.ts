500437e2bb12e6d4eaac2d46465cf7040dbe488f
Louis Laugesen <louis.laugesen@gmail.com>
2022-02-11 01:34:26 +1100

    Woo installer: e2e tests (#59548)
    * init
    
    * Basic test setup
    
    * Bug fixes, get first two tests working
    
    * Simplify
    
    * Address feedback
    
    * This component will be more re-usable by splitting this close op up

---
+++ packages/calypso-e2e/src/lib/pages/woocommerce-landing-page.ts
@@ -0,0 +1,46 @@
+import { Page } from 'playwright';
+
+export const selectors = {
+	start: '.woocommerce .empty-content button:text("Start a new store")',
+	installer: '.is-woocommerce-install',
+	learnMore: '.woocommerce span:text("Learn more")',
+	supportDialog: '.support-article-dialog',
+	supportDialogClose: '.support-article-dialog button:text("Close")',
+};
+
+/**
+ * Class representing the landing page at https://wordpress.com/woocommerce-installation/.
+ */
+export class WoocommerceLandingPage {
+	private page: Page;
+
+	/**
+	 * Constructs an instance.
+	 */
+	constructor( page: Page ) {
+		this.page = page;
+	}
+
+	/**
+	 * Click the Learn more button
+	 */
+	async openLearnMore(): Promise< void > {
+		await this.page.click( selectors.learnMore );
+		await this.page.waitForSelector( selectors.supportDialog );
+	}
+
+	/**
+	 * Click the Close button
+	 */
+	async closeLearnMore(): Promise< void > {
+		await this.page.click( selectors.supportDialogClose );
+	}
+
+	/**
+	 * Click the Start a new store button
+	 */
+	async openStoreSetup(): Promise< void > {
+		await this.page.click( selectors.start );
+		await this.page.waitForSelector( selectors.installer );
+	}
+}
