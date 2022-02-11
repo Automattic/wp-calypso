df337b9dfc588f10f1ddddf1d08fad2a7d6f4906
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2022-02-11 18:32:28 -0300

    Update copy in the first step of the advanced setup

--- client/components/domains/connect-domain-step/connect-domain-step-advanced-start.jsx
+++ client/components/domains/connect-domain-step/connect-domain-step-advanced-start.jsx
@@ -4,8 +4,6 @@ import { useI18n } from '@wordpress/react-i18n';
 import PropTypes from 'prop-types';
 import CardHeading from 'calypso/components/card-heading';
 import MaterialIcon from 'calypso/components/material-icon';
-import Notice from 'calypso/components/notice';
-import NoticeAction from 'calypso/components/notice/notice-action';
 import ConnectDomainStepWrapper from './connect-domain-step-wrapper';
 import { modeType, stepSlug, stepsHeading } from './constants';
 
@@ -24,23 +22,11 @@ export default function ConnectDomainStepAdvancedStart( {
 
 	const stepContent = (
 		<>
-			<Notice
-				className={ className + '__advanced-start-notice' }
-				status="is-warning"
-				showDismiss={ false }
-			>
-				{ __(
-					'We advise using our recommended setup instead, with WordPress.com name servers. Our advanced setup requires manually managing DNS records for any added services such as Professional Email yourself.'
-				) }
-				<NoticeAction onClick={ switchToSuggestedSetup }>
-					{ __( 'Back to recommended setup' ) }
-				</NoticeAction>
-			</Notice>
 			<div className={ className + '__suggested-start' }>
 				<p className={ className + '__text' }>
 					{ createInterpolateElement(
 						__(
-							'This is the advanced way to connect your domain, using root A records & CNAME records. We advise using our <a>suggested setup</a> instead, with WordPress.com name servers.'
+							'This is the advanced way to connect your domain, using root A records & CNAME records. This setup requires manually managing DNS records for any added services such as Professional Email yourself. If you want to connect your domain in the easiest way, switch to our <a>suggested setup</a>.'
 						),
 						{
 							a: createElement( 'a', {
