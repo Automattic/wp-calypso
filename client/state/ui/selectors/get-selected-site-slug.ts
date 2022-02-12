2ac3f465a2a077efff4b9a5002ef24fe766976da
Jeremy Yip <jeremy.yip@automattic.com>
2022-02-12 14:09:44 -0800

    Convert get selected site slug to typescript

--- client/state/ui/selectors/get-selected-site-slug.ts
+++ client/state/ui/selectors/get-selected-site-slug.ts
@@ -1,14 +1,8 @@
 import getSiteSlug from 'calypso/state/sites/selectors/get-site-slug';
+import { IAppState } from 'calypso/state/types';
 import getSelectedSiteId from './get-selected-site-id';
 
-/**
- * Returns the slug of the currently selected site,
- * or null if no site is selected.
- *
- * @param  {object}  state Global state tree
- * @returns {?string}       Selected site slug
- */
-export default function getSelectedSiteSlug( state ) {
+export default function getSelectedSiteSlug( state: IAppState ) {
 	const siteId = getSelectedSiteId( state );
 	if ( ! siteId ) {
 		return null;
