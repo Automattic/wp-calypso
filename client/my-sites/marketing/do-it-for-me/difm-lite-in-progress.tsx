2ad2939ecbd7ecc15a656dcd70495d1d4c0a6bec
Aneesh Devasthale <aneeshdevasthale@gmail.com>
2022-02-14 16:04:00 +0530

    Adds an action required state to the DIFM Lite in progress screen

--- client/my-sites/marketing/do-it-for-me/difm-lite-in-progress.tsx
+++ client/my-sites/marketing/do-it-for-me/difm-lite-in-progress.tsx
@@ -2,6 +2,7 @@ import { useTranslate } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import SiteBuildInProgressIllustration from 'calypso/assets/images/difm/site-build-in-progress.svg';
+import WebsiteContentRequiredIllustration from 'calypso/assets/images/difm/website-content-required.svg';
 import QuerySiteDomains from 'calypso/components/data/query-site-domains';
 import EmptyContent from 'calypso/components/empty-content';
 import { hasGSuiteWithUs } from 'calypso/lib/gsuite';
@@ -10,8 +11,9 @@ import { domainManagementList } from 'calypso/my-sites/domains/paths';
 import { emailManagement } from 'calypso/my-sites/email/paths';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import getPrimaryDomainBySiteId from 'calypso/state/selectors/get-primary-domain-by-site-id';
+import isDIFMLiteWebsiteContentSubmitted from 'calypso/state/selectors/is-difm-lite-website-content-submitted';
 import { getSiteSlug } from 'calypso/state/sites/selectors';
-import { AppState } from 'calypso/types';
+import { AppState, SiteSlug } from 'calypso/types';
 
 import './difm-lite-in-progress.scss';
 
@@ -21,6 +23,9 @@ type DIFMLiteInProgressProps = {
 
 function DIFMLiteInProgress( { siteId }: DIFMLiteInProgressProps ): React.ReactElement {
 	const slug = useSelector( ( state: AppState ) => getSiteSlug( state, siteId ) );
+	const isWebsiteContentSubmitted = useSelector( ( state ) =>
+		isDIFMLiteWebsiteContentSubmitted( state, siteId )
+	);
 	const primaryDomain = useSelector( ( state: AppState ) =>
 		getPrimaryDomainBySiteId( state, siteId )
 	);
@@ -54,24 +59,43 @@ function DIFMLiteInProgress( { siteId }: DIFMLiteInProgressProps ): React.ReactE
 		);
 	};
 
-	return (
-		<div>
-			<EmptyContent
-				title={ translate( 'Our experts are building your site' ) }
-				line={ translate(
-					"Our team is building your site. We'll be in touch when your site is ready."
-				) }
-				action={ translate( 'Manage domain' ) }
-				actionURL={ domainManagementList( slug ) }
-				secondaryAction={ hasEmailWithUs ? translate( 'Manage email' ) : translate( 'Add email' ) }
-				secondaryActionURL={ emailManagement( slug, null ) }
-				secondaryActionCallback={ recordEmailClick }
-				illustration={ SiteBuildInProgressIllustration }
-				illustrationWidth={ 144 }
-				className="difm-lite-in-progress__content"
-			/>
-		</div>
+	// No need to check for null here since the button is not rendered till we have a slug
+	const getWebsiteContentLink = ( slug: SiteSlug | null ) =>
+		`/start/site-content-collection/website-content?siteSlug=${ slug }`;
+
+	const renderUserActionNotRequired = () => (
+		<EmptyContent
+			title={ translate( 'Our experts are building your site' ) }
+			line={ translate(
+				"Our team is building your site. We'll be in touch when your site is ready."
+			) }
+			action={ translate( 'Manage domain' ) }
+			actionURL={ domainManagementList( slug ) }
+			secondaryAction={ hasEmailWithUs ? translate( 'Manage email' ) : translate( 'Add email' ) }
+			secondaryActionURL={ emailManagement( slug, null ) }
+			secondaryActionCallback={ recordEmailClick }
+			illustration={ SiteBuildInProgressIllustration }
+			illustrationWidth={ 144 }
+			className="difm-lite-in-progress__content"
+		/>
 	);
+
+	const renderUserActionRequired = () => (
+		<EmptyContent
+			title={ translate( 'Website content not submitted' ) }
+			line={ translate(
+				'Please provide the necessary information for the creation of your website. To access click on the button below.'
+			) }
+			action={ translate( 'Add content to your website' ) }
+			actionURL={ getWebsiteContentLink( slug ) }
+			secondaryActionCallback={ recordEmailClick }
+			illustration={ WebsiteContentRequiredIllustration }
+			illustrationWidth={ 144 }
+			className="difm-lite-in-progress__content"
+		/>
+	);
+
+	return isWebsiteContentSubmitted ? renderUserActionNotRequired() : renderUserActionRequired();
 }
 
 DIFMLiteInProgress.propTypes = {
