0920378e78e32cc12dd7c4846e2a6fd6c921b896
dpasque <dan.speckhard.pasque@automattic.com>
2022-02-15 13:51:57 -0600

    Add temporary smoke test for FSE on WPCOM

---
+++ test/e2e/specs/specs-wpcom/wp-fse__temp-smoke-test.ts
@@ -0,0 +1,47 @@
+/**
+ * @group calypso-pr
+ * @group gutenberg
+ */
+
+import { DataHelper, envVariables, SidebarComponent, TestAccount } from '@automattic/calypso-e2e';
+import { Browser, Page } from 'playwright';
+
+declare const browser: Browser;
+
+/**
+ * This is a temporary smoke test for FSE on WordPress.com until a more comprehensive E2E strategy
+ * can be designed and implemented.
+ *
+ * The goal here is to catch major breaks with the integration -- i.e. Calypso navigation no long working,
+ * or getting a WSOD when trying to load the editor.
+ */
+describe( DataHelper.createSuiteTitle( 'Editor: Basic Post Flow' ), function () {
+	let page: Page;
+	const accountName = envVariables.GUTENBERG_EDGE
+		? 'siteEditorSimpleSiteEdgeUser'
+		: 'siteEditorSimpleSiteUser';
+
+	beforeAll( async () => {
+		page = await browser.newPage();
+
+		const testAccount = new TestAccount( accountName );
+		await testAccount.authenticate( page );
+	} );
+
+	it( 'Navigate to Full Site Editor', async function () {
+		// Explicitly doing sidebar navigation to ensure Calypso navigation is intact.
+		const sidebarComponent = new SidebarComponent( page );
+		await sidebarComponent.navigate( 'Appearance', 'Editor' );
+	} );
+
+	it( 'Editor content loads', async function () {
+		// Because this is a temporary smoke test, adding the needed FSE selectors here instead of
+		// spinning up a POM class that we will later needed to redo.
+		// This should ensure the editor hasn't done a WSOD.
+		const locator = page
+			.frameLocator( '.calypsoify.is-iframe iframe.is-loaded' )
+			.frameLocator( 'iframe[name="editor-canvas"]' )
+			.locator( 'text=Home' );
+		await locator.waitFor( { timeout: 90 * 1000 } );
+	} );
+} );
