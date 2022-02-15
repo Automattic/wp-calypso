96e47ca93c336279d09cee919d9cfa93d549e976
Caleb Bauermeister <cbauerman@gmail.com>
2022-02-15 13:47:14 -0800

    add basic dropdown functionality

--- client/my-sites/checkout/composite-checkout/components/item-variation-picker/item-variation-dropdown.tsx
+++ client/my-sites/checkout/composite-checkout/components/item-variation-picker/item-variation-dropdown.tsx
@@ -1,6 +1,47 @@
+import styled from '@emotion/styled';
 import { FunctionComponent } from 'react';
-import type { ItemVariationPickerProps } from './types';
+import { useGetProductVariants } from '../../hooks/product-variants';
+import type { ItemVariationPickerProps, WPCOMProductVariant } from './types';
 
-export const ItemVariationDropDown: FunctionComponent< ItemVariationPickerProps > = () => {
-	return <div></div>;
+const TermOptions = styled.select`
+	padding: 0;
+`;
+
+const TermOptionsItem = styled.option`
+	padding: 0;
+`;
+
+export const ItemVariationDropDown: FunctionComponent< ItemVariationPickerProps > = ( {
+	selectedItem,
+	onChangeItemVariant,
+	isDisabled,
+	siteId,
+	productSlug,
+} ) => {
+	const variants = useGetProductVariants( siteId, productSlug );
+
+	if ( variants.length < 2 ) {
+		return null;
+	}
+
+	return (
+		<TermOptions
+			disabled={ isDisabled }
+			value={ `${ selectedItem.product_id.toString() }:${ selectedItem.product_slug }` }
+			className="item-variation-dropdown"
+			onChange={ ( event ) => {
+				const [ productId, productSlug ] = event.target.value.split( ':' );
+				onChangeItemVariant( selectedItem.uuid, productSlug, parseInt( productId ) );
+			} }
+		>
+			{ variants.map( ( productVariant: WPCOMProductVariant ) => (
+				<TermOptionsItem
+					key={ productVariant.productId }
+					value={ `${ productVariant.productId }:${ productVariant.productSlug }` }
+				>
+					{ productVariant.variantLabel }
+				</TermOptionsItem>
+			) ) }
+		</TermOptions>
+	);
 };
