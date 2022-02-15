6b92041db56dc4867fa615b11eeee5fdcda09825
Enej Bajgoric <enej.bajgoric@automattic.com>
2022-02-15 12:51:36 -0800

    Fix remove mobile app banner if previous flow was onboarding

--- client/state/ui/selectors/app-banner-is-visible.js
+++ client/state/ui/selectors/app-banner-is-visible.js
@@ -1,7 +1,12 @@
 import 'calypso/state/ui/init';
+import { getPreviousFlowName } from 'calypso/state/signup/flow/selectors';
 import { getCurrentLayoutFocus } from 'calypso/state/ui/layout-focus/selectors';
 
 export default function isAppBannerEnabled( state ) {
+	// Never show the AppBanner if the previous flow is set to onboarding.
+	if ( getPreviousFlowName( state ) === 'onboarding' ) {
+		return false;
+	}
 	// since on mobile the sidebar layout focus take up the whole "Page" we never want to show the App Banner
 	// when the side bar in in focus.
 	return state.ui.appBannerVisibility && getCurrentLayoutFocus( state ) !== 'sidebar';
