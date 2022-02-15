19f77958c6a8d4999d810aa3e3ed7a84f94da3f0
Caleb Bauermeister <cbauerman@gmail.com>
2022-02-15 13:20:09 -0800

    add dropdown as sub-component to item picker

--- client/my-sites/checkout/composite-checkout/components/item-variation-picker/index.tsx
+++ client/my-sites/checkout/composite-checkout/components/item-variation-picker/index.tsx
@@ -1,9 +1,14 @@
+import config from '@automattic/calypso-config';
 import { FunctionComponent } from 'react';
+import { ItemVariationDropDown } from './item-variation-dropdown';
 import { ItemVariationRadioButtons } from './item-variation-radio-buttons';
 import type { ItemVariationPickerProps } from './types';
 
-export const ItemVariationPicker: FunctionComponent< ItemVariationPickerProps > = ( props ) => (
-	<ItemVariationRadioButtons { ...props } />
-);
+export const ItemVariationPicker: FunctionComponent< ItemVariationPickerProps > = ( props ) =>
+	config.isEnabled( 'checkout/variant-dropdown' ) ? (
+		<ItemVariationDropDown { ...props } />
+	) : (
+		<ItemVariationRadioButtons { ...props } />
+	);
 
 export * from './types';
---
+++ client/my-sites/checkout/composite-checkout/components/item-variation-picker/item-variation-dropdown.tsx
@@ -0,0 +1,6 @@
+import { FunctionComponent } from 'react';
+import type { ItemVariationPickerProps } from './types';
+
+export const ItemVariationDropDown: FunctionComponent< ItemVariationPickerProps > = () => {
+	return <div></div>;
+};
