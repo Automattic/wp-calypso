a4c5326b6c2ab362f48455825506c293e0b15684
Dehan <dehandecroos@gmail.com>
2022-02-16 17:16:50 +0530

    Broken type fix

--- client/signup/steps/website-content/page-details.tsx
+++ client/signup/steps/website-content/page-details.tsx
@@ -1,5 +1,5 @@
 import { useTranslate } from 'i18n-calypso';
-import { ChangeEvent, ReactChild } from 'react';
+import { ChangeEvent } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
 	SubLabel,
@@ -7,6 +7,7 @@ import {
 	TextAreaField,
 	HorizontalGrid,
 } from 'calypso/signup/accordion-form/form-components';
+import { ValidationErrors } from 'calypso/signup/accordion-form/types';
 import { imageUploaded, textChanged } from 'calypso/state/signup/steps/website-content/actions';
 import { PageData } from 'calypso/state/signup/steps/website-content/schema';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
@@ -22,7 +23,7 @@ export function PageDetails( {
 	onChangeField,
 }: {
 	page: PageData;
-	formErrors: Record< string, ReactChild >;
+	formErrors: ValidationErrors;
 	onChangeField?: ( { target: { name, value } }: ChangeEvent< HTMLInputElement > ) => void;
 } ) {
 	const translate = useTranslate();
