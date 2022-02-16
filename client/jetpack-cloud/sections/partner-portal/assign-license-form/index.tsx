12b03ac1fa30e6fce19cf321198d5fa318323ba7
Heyde Moura <heydemoura@Heydes-MacBook-Pro.local>
2022-02-15 22:24:45 -0300

    JetpackCloud: add pagination to license assignment

--- client/jetpack-cloud/sections/partner-portal/assign-license-form/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/assign-license-form/index.tsx
@@ -5,14 +5,23 @@ import page from 'page';
 import { ReactElement, useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import FormRadio from 'calypso/components/forms/form-radio';
+import Pagination from 'calypso/components/pagination';
 import SearchCard from 'calypso/components/search-card';
+import { SITE_CARDS_PER_PAGE } from 'calypso/jetpack-cloud/sections/partner-portal/assign-license-form/constants';
 import { addQueryArgs } from 'calypso/lib/url';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { errorNotice } from 'calypso/state/notices/actions';
 import useAssignLicenseMutation from 'calypso/state/partner-portal/licenses/hooks/use-assign-license-mutation';
 import './style.scss';
 
-export default function AssignLicenseForm( { sites }: any ): ReactElement {
+function setPage( pageNumber: number ): void {
+	const queryParams = { page: pageNumber };
+	const currentPath = window.location.pathname + window.location.search;
+
+	page( addQueryArgs( queryParams, currentPath ) );
+}
+
+export default function AssignLicenseForm( { sites, currentPage }: any ): ReactElement {
 	const translate = useTranslate();
 	const dispatch = useDispatch();
 	const [ filter, setFilter ] = useState( false );
@@ -21,21 +30,23 @@ export default function AssignLicenseForm( { sites }: any ): ReactElement {
 	const licenseKey = getQueryArg( window.location.href, 'key' ) as string;
 	const onSelectSite = ( site: any ) => setSelectedSite( site );
 
-	const siteCards = sites.map( ( site: any ) => {
-		if ( -1 !== site.domain.search( filter ) || false === filter ) {
-			return (
-				<Card key={ site.ID } className="assign-license-form__site-card">
-					<FormRadio
-						className="assign-license-form__site-card-radio"
-						label={ site.domain }
-						name="site_select"
-						disabled={ isSubmitting }
-						onClick={ () => onSelectSite( site.ID ) }
-					/>
-				</Card>
-			);
-		}
-	} );
+	const siteCards = sites
+		.slice( SITE_CARDS_PER_PAGE * ( currentPage - 1 ), SITE_CARDS_PER_PAGE * currentPage )
+		.map( ( site: any ) => {
+			if ( -1 !== site.domain.search( filter ) || false === filter ) {
+				return (
+					<Card key={ site.ID } className="assign-license-form__site-card">
+						<FormRadio
+							className="assign-license-form__site-card-radio"
+							label={ site.domain }
+							name="site_select"
+							disabled={ isSubmitting }
+							onClick={ () => onSelectSite( site.ID ) }
+						/>
+					</Card>
+				);
+			}
+		} );
 
 	const assignLicense = useAssignLicenseMutation( {
 		onSuccess: ( license: any ) => {
@@ -63,6 +74,13 @@ export default function AssignLicenseForm( { sites }: any ): ReactElement {
 
 	const onAssignLater = () =>
 		page.redirect( addQueryArgs( { highlight: licenseKey }, '/partner-portal/licenses' ) );
+
+	const onPageClick = useCallback(
+		( pageNumber: number ) => {
+			setPage( pageNumber );
+		},
+		[ setPage ]
+	);
 
 	return (
 		<div className="assign-license-form">
@@ -98,6 +116,14 @@ export default function AssignLicenseForm( { sites }: any ): ReactElement {
 			/>
 
 			{ siteCards }
+
+			<Pagination
+				className="assign-license-form__sites-pagination"
+				page={ currentPage }
+				perPage={ SITE_CARDS_PER_PAGE }
+				total={ sites.length }
+				pageClick={ onPageClick }
+			/>
 		</div>
 	);
 }
