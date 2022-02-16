a09e5f9061933a8105a6e2b522107cd439b18f91
Jarda Snajdr <jsnajdr@gmail.com>
2022-02-16 10:31:25 +0100

    Slash one more undefined

--- client/lib/use-memo-compare/index.ts
+++ client/lib/use-memo-compare/index.ts
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 // See https://usehooks.com/useMemoCompare/
 export function useMemoCompare< T >( next: T, compare: ( previous: T, next: T ) => boolean ): T {
 	// Ref for storing previous value
-	const previousRef = useRef< undefined | T >();
+	const previousRef = useRef< T >();
 
 	// If not equal update previousRef to next value.
 	// We only update if not equal so that this hook continues to return
