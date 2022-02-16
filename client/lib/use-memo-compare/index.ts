bc27b98a552349e117d0483ed18984c6d21e2ea1
Jarda Snajdr <jsnajdr@gmail.com>
2022-02-16 10:17:03 +0100

    Eliminate the undefined type

--- client/lib/use-memo-compare/index.ts
+++ client/lib/use-memo-compare/index.ts
@@ -1,22 +1,14 @@
 import { useRef } from 'react';
 
 // See https://usehooks.com/useMemoCompare/
-export function useMemoCompare< T >(
-	next: T,
-	compare: ( previous: T | undefined, next: T ) => boolean
-): T | undefined {
+export function useMemoCompare< T >( next: T, compare: ( previous: T, next: T ) => boolean ): T {
 	// Ref for storing previous value
 	const previousRef = useRef< undefined | T >();
-	const previous = previousRef.current;
-
-	// Pass previous and next value to compare function
-	// to determine whether to consider them equal.
-	const isEqual = compare( previous, next );
 
 	// If not equal update previousRef to next value.
 	// We only update if not equal so that this hook continues to return
 	// the same old value if compare keeps returning true.
-	if ( ! isEqual ) {
+	if ( previousRef.current === undefined || ! compare( previousRef.current, next ) ) {
 		previousRef.current = next;
 	}
 
