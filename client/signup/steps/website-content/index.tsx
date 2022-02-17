9a37b795b92a47880839def3892080c7f22c6d0b
Dehan <dehandecroos@gmail.com>
2022-02-17 09:51:09 +0530

    Prevent loading UI if relevent information has not yet loaded

--- client/signup/steps/website-content/index.tsx
+++ client/signup/steps/website-content/index.tsx
@@ -48,40 +48,10 @@ function WebsiteContentStep( {
 	const [ formErrors, setFormErrors ] = useState< ValidationErrors >( {} );
 	const dispatch = useDispatch();
 	const translate = useTranslate();
-
+	const siteId = useSelector( ( state ) => getSiteId( state, queryObject.siteSlug as string ) );
 	const websiteContent = useSelector( getWebsiteContent );
 	const currentIndex = useSelector( getWebsiteContentDataCollectionIndex );
-	const siteId = useSelector( ( state ) => getSiteId( state, queryObject.siteSlug as string ) );
 	const siteCategory = useSelector( ( state ) => getDIFMLiteSiteCategory( state, siteId ) );
-	const isWebsiteContentSubmitted = useSelector( ( state ) =>
-		isDIFMLiteWebsiteContentSubmitted( state, siteId )
-	);
-	const isLoadingSiteInformation = useSelector( ( state ) =>
-		isRequestingSite( state, siteId as number )
-	);
-
-	// We assume that difm lite is purchased when the is_difm_lite_in_progress sticker is active in a given blog
-	const isDifmLitePurchased = useSelector( ( state ) => isDIFMLiteInProgress( state, siteId ) );
-
-	//Make sure the most up to date site information is loaded so that we can validate access to this page
-	useEffect( () => {
-		siteId && dispatch( requestSite( siteId ) );
-	}, [ siteId ] );
-
-	useEffect( () => {
-		if ( ! isLoadingSiteInformation && ! isDifmLitePurchased ) {
-			debug( 'DIFM not purchased yet, redirecting to DIFM purchase flow' );
-			page( `/start/do-it-for-me` );
-		} else if ( isWebsiteContentSubmitted ) {
-			debug( 'Website content content already submitted, redirecting to home' );
-			page( `/home/${ queryObject.siteSlug }` );
-		}
-	}, [
-		isLoadingSiteInformation,
-		isDifmLitePurchased,
-		isWebsiteContentSubmitted,
-		queryObject.siteSlug,
-	] );
 
 	useEffect( () => {
 		function getPageFromCategory( category: string | null ) {
@@ -140,18 +110,16 @@ function WebsiteContentStep( {
 	);
 	const generatedSections = generatedSectionsCallback();
 	return (
-		<>
-			<AccordionForm
-				generatedSections={ generatedSections }
-				onErrorUpdates={ ( errors ) => setFormErrors( errors ) }
-				formValuesInitialState={ websiteContent }
-				currentIndex={ currentIndex }
-				updateCurrentIndex={ ( currentIndex ) => {
-					dispatch( updateWebsiteContentCurrentIndex( currentIndex ) );
-				} }
-				onSubmit={ onSubmit }
-			/>
-		</>
+		<AccordionForm
+			generatedSections={ generatedSections }
+			onErrorUpdates={ ( errors ) => setFormErrors( errors ) }
+			formValuesInitialState={ websiteContent }
+			currentIndex={ currentIndex }
+			updateCurrentIndex={ ( currentIndex ) => {
+				dispatch( updateWebsiteContentCurrentIndex( currentIndex ) );
+			} }
+			onSubmit={ onSubmit }
+		/>
 	);
 }
 
@@ -160,17 +128,54 @@ export default function WrapperWebsiteContent(
 		flowName: string;
 		stepName: string;
 		positionInFlow: string;
+		queryObject: {
+			siteSlug?: string;
+			siteId?: string;
+		};
 	} & WebsiteContentStepProps
 ) {
-	const { flowName, stepName, positionInFlow } = props;
+	const { flowName, stepName, positionInFlow, queryObject } = props;
 	const translate = useTranslate();
-
+	const dispatch = useDispatch();
 	const headerText = translate( 'Website Content' );
 	const subHeaderText = translate(
 		'In this step, you will add your brand visuals, pages and media to be used on your website.'
 	);
+	const siteId = useSelector( ( state ) => getSiteId( state, queryObject.siteSlug as string ) );
 
-	return (
+	const isWebsiteContentSubmitted = useSelector( ( state ) =>
+		isDIFMLiteWebsiteContentSubmitted( state, siteId )
+	);
+	const isLoadingSiteInformation = useSelector( ( state ) =>
+		isRequestingSite( state, siteId as number )
+	);
+
+	// We assume that difm lite is purchased when the is_difm_lite_in_progress sticker is active in a given blog
+	const isDifmLitePurchased = useSelector( ( state ) => isDIFMLiteInProgress( state, siteId ) );
+
+	//Make sure the most up to date site information is loaded so that we can validate access to this page
+	useEffect( () => {
+		siteId && dispatch( requestSite( siteId ) );
+	}, [ siteId ] );
+
+	useEffect( () => {
+		if ( ! isLoadingSiteInformation ) {
+			if ( ! isDifmLitePurchased ) {
+				debug( 'DIFM not purchased yet, redirecting to DIFM purchase flow' );
+				page( `/start/do-it-for-me` );
+			} else if ( isWebsiteContentSubmitted ) {
+				debug( 'Website content content already submitted, redirecting to home' );
+				page( `/home/${ queryObject.siteSlug }` );
+			}
+		}
+	}, [
+		isLoadingSiteInformation,
+		isDifmLitePurchased,
+		isWebsiteContentSubmitted,
+		queryObject.siteSlug,
+	] );
+
+	return isLoadingSiteInformation ? null : (
 		<StepWrapper
 			headerText={ headerText }
 			subHeaderText={ subHeaderText }
