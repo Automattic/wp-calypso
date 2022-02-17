6391a3a9c3f1cc7d154631409bdde821b6d1834f
Enej Bajgoric <enej.bajgoric@automattic.com>
2022-02-17 12:17:21 -0800

    Use the CurrentFlow instead of previous flow.

--- client/state/ui/selectors/app-banner-is-visible.js
+++ client/state/ui/selectors/app-banner-is-visible.js
@@ -1,10 +1,10 @@
 import 'calypso/state/ui/init';
-import { getPreviousFlowName } from 'calypso/state/signup/flow/selectors';
+import { getCurrentFlowName } from 'calypso/state/signup/flow/selectors';
 import { getCurrentLayoutFocus } from 'calypso/state/ui/layout-focus/selectors';
 
 export default function isAppBannerEnabled( state ) {
-	// Never show the AppBanner if the previous flow is set to onboarding.
-	if ( getPreviousFlowName( state ) === 'onboarding' ) {
+	// Never show the AppBanner in the site setup flow.
+	if ( getCurrentFlowName( state ) === 'setup-site' ) {
 		return false;
 	}
 	// since on mobile the sidebar layout focus take up the whole "Page" we never want to show the App Banner
