c58583eeb34705f4d4a29eb140cf40390d6b11e6
Matt Wiebe <wiebe@automattic.com>
2022-02-18 11:11:25 -0600

    Full Site Editing: no hiding the FSE tag

--- client/state/data-layer/wpcom/theme-filters/index.js
+++ client/state/data-layer/wpcom/theme-filters/index.js
@@ -1,5 +1,4 @@
 import i18n from 'i18n-calypso';
-import { omit } from 'lodash';
 import { registerHandlers } from 'calypso/state/data-layer/handler-registry';
 import { http } from 'calypso/state/data-layer/wpcom-http/actions';
 import { dispatchRequest } from 'calypso/state/data-layer/wpcom-http/utils';
@@ -21,9 +20,7 @@ const fetchFilters = ( action ) =>
 	);
 
 const storeFilters = ( action, data ) => {
-	// Don't show FSE themes to non-FSE sites until switching to a FSE theme activates FSE.
-	const filters = action.isCoreFse ? data : omit( data, 'feature.full-site-editing' );
-	return { type: THEME_FILTERS_ADD, filters };
+	return { type: THEME_FILTERS_ADD, filters: data };
 };
 
 const reportError = () => errorNotice( i18n.translate( 'Problem fetching theme filters.' ) );
