710f0676d75e074d161fc7f7b666129ee95dcd56
arthur791004 <arthur.chu@automattic.com>
2022-02-18 10:46:19 +0800

    Videos UI: Fix back button is broken on mobile (#61110)
    * Introduce useRouteModal to control opening and closing modal
    
    * Control BloggingQuickStartModal via query string
    
    * Refine useRouteModal usage
    
    * Reviews

---
+++ client/lib/route-modal/index.ts
@@ -0,0 +1,1 @@
+export { default as useRouteModal } from './use-route-modal';
--- client/my-sites/customer-home/cards/education/blogging-quick-start/index.jsx
+++ client/my-sites/customer-home/cards/education/blogging-quick-start/index.jsx
@@ -1,13 +1,17 @@
-import { useState } from '@wordpress/element';
 import { useTranslate } from 'i18n-calypso';
 import startLearningPrompt from 'calypso/assets/images/customer-home/illustration--secondary-start-learning.svg';
+import { COURSE_SLUGS } from 'calypso/data/courses';
+import { useRouteModal } from 'calypso/lib/route-modal';
 import { EDUCATION_BLOGGING_QUICK_START } from 'calypso/my-sites/customer-home/cards/constants';
 import EducationalContent from '../educational-content';
 import BloggingQuickStartModal from './blogging-quick-start-modal';
 
 const BloggingQuickStart = () => {
 	const translate = useTranslate();
-	const [ isModalVisible, setIsModalVisible ] = useState( false );
+	const { isModalOpen, openModal, closeModal } = useRouteModal(
+		'courseSlug',
+		COURSE_SLUGS.BLOGGING_QUICK_START
+	);
 
 	return (
 		<EducationalContent
@@ -19,13 +23,13 @@ const BloggingQuickStart = () => {
 				{
 					ModalComponent: BloggingQuickStartModal,
 					modalComponentProps: {
-						isVisible: isModalVisible,
+						isVisible: isModalOpen,
 						onClose: ( event ) => {
 							event.preventDefault();
-							setIsModalVisible( false );
+							closeModal();
 						},
 					},
-					onClick: () => setIsModalVisible( true ),
+					onClick: () => openModal(),
 					text: translate( 'Start learning' ),
 				},
 			] }
