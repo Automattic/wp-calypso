5b32877e86140a9311ae2f746e285790c54f1f4d
Olaseni Oluwunmi <277661+olaseni@users.noreply.github.com>
2022-02-18 12:09:21 +0100

    Fix Titan URLs for staging platforms (#61053)
    * - Introduce a domain argument that uses the subscription's `baseUrl` to fetch the appropriate URL
    - Convert thank you component into a true functional component. Remove duplication.
    
    * - Integrate some review feedback
    
    * - Change variable name
    
    * Change webmailUrl to appsUrl
    
    * Wrap functionality into hooks
    
    * Re-export hook from central lib

--- client/lib/titan/get-titan-urls.js
+++ client/lib/titan/get-titan-urls.js
@@ -4,20 +4,35 @@
 const TITAN_APPS = {
 	CALENDAR: 'calendar',
 	CONTACTS: 'contacts',
-	EMAIL: 'email',
+	EMAIL: 'mail',
 };
+
+/**
+ * Returns the base URL for Titan Apps
+ *
+ * @param {undefined|{titanMailSubscription?:{appsUrl?: string}}} domain - Domain object
+ * @returns {string} - The Apps URL prefix
+ */
+export function getTitanAppsUrlPrefix( domain ) {
+	return domain?.titanMailSubscription?.appsUrl ?? 'https://wp.titan.email';
+}
 
 /**
  * Generates a URL pointing to the given Titan App
  *
+ * @param {string} titanAppsUrlPrefix - The base url for Titan Apps
  * @param {string?} email - The email address of the Titan account. Used for autofill on Titan's login page.
  * @param {string?} app - Can be one of the `TITAN_APPS` - `email`, `calendar` or `contacts`
- * @param {boolean?} clearPreviousSessions - Whether to clear previously logged in sessions.
+ * @param {boolean?} clearPreviousSessions - Whether to clear previously logged-in sessions.
  * @returns The URL with app and prefilled `email_account` as query parameter
  */
-function getTitanUrl( email, app = TITAN_APPS.EMAIL, clearPreviousSessions = false ) {
-	const titanBaseUrl = 'https://wp.titan.email';
-	const titanAppUrl = new URL( `${ titanBaseUrl }/${ app }` );
+function getTitanUrl(
+	titanAppsUrlPrefix,
+	email,
+	app = TITAN_APPS.EMAIL,
+	clearPreviousSessions = false
+) {
+	const titanAppUrl = new URL( `${ titanAppsUrlPrefix }/${ app }/` );
 
 	if ( email?.includes( '@' ) ) {
 		titanAppUrl.searchParams.append( 'email_account', email );
@@ -30,14 +45,14 @@ function getTitanUrl( email, app = TITAN_APPS.EMAIL, clearPreviousSessions = fal
 	return titanAppUrl.href;
 }
 
-export function getTitanCalendarlUrl( email ) {
-	return getTitanUrl( email, TITAN_APPS.CALENDAR );
+export function getTitanCalendarUrl( titanAppsUrlPrefix, email ) {
+	return getTitanUrl( titanAppsUrlPrefix, email, TITAN_APPS.CALENDAR );
 }
 
-export function getTitanContactsUrl( email ) {
-	return getTitanUrl( email, TITAN_APPS.CONTACTS );
+export function getTitanContactsUrl( titanAppsUrlPrefix, email ) {
+	return getTitanUrl( titanAppsUrlPrefix, email, TITAN_APPS.CONTACTS );
 }
 
-export function getTitanEmailUrl( email, clearPreviousSessions = false ) {
-	return getTitanUrl( email, TITAN_APPS.EMAIL, clearPreviousSessions );
+export function getTitanEmailUrl( titanAppsUrlPrefix, email, clearPreviousSessions = false ) {
+	return getTitanUrl( titanAppsUrlPrefix, email, TITAN_APPS.EMAIL, clearPreviousSessions );
 }
