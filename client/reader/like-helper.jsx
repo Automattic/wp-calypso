da301e888b7c8ebe00e49ea0ba5e18548070c898
Eru Penkman <roo2@protonmail.com>
2022-02-21 12:24:34 +1000

    check for likes button being disabled in reader

--- client/reader/like-helper.jsx
+++ client/reader/like-helper.jsx
@@ -4,13 +4,28 @@ import {
 	isDiscoverSitePick,
 } from 'calypso/reader/discover/helper';
 
+/**
+ * True if likes are explicitly disabled.
+ * False if likes are enabled or the property does not exist.
+ *
+ * @param {object} post
+ * @returns {boolean} True if likes are explicitly disabled
+ */
+function isLikesDisabled( post ) {
+	return post?.likes_enabled === false;
+}
+
 export function shouldShowLikes( post ) {
 	let showLikes = false;
 	if ( isDiscoverPost( post ) ) {
-		if ( isInternalDiscoverPost( post ) && ! isDiscoverSitePick( post ) ) {
+		if (
+			isInternalDiscoverPost( post ) &&
+			! isDiscoverSitePick( post ) &&
+			! isLikesDisabled( post )
+		) {
 			showLikes = true;
 		}
-	} else if ( post && post.site_ID && ! post.is_external ) {
+	} else if ( post && post.site_ID && ! post.is_external && ! isLikesDisabled( post ) ) {
 		showLikes = true;
 	}
 
