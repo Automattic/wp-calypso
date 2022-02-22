e14ad784eb6cff8c720b441ff058c06c9be452e8
Daniel López <daniel.lopez@automattic.com>
2022-02-22 17:40:01 +0100

    Emails: Properties order & docblock removal

--- client/lib/gsuite/is-domain-eligible-for-google-free-trial.ts
+++ client/lib/gsuite/is-domain-eligible-for-google-free-trial.ts
@@ -1,11 +1,5 @@
 import type { ResponseDomain } from 'calypso/lib/domains/types';
 
-/**
- * Determines if the specified domain is eligible for the Google 1-month free trial.
- *
- * @param {ResponseDomain|undefined} domain - domain object
- * @returns {boolean} whether the domain is eligible or not
- */
 export function isDomainEligibleForGoogleFreeTrial( domain: ResponseDomain ) {
 	return domain?.googleAppsSubscription?.isEligibleForIntroductoryOffer ?? false;
 }
