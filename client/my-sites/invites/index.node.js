77b0f20306614a67de0bb51a755012cbfd627850
annemirasol <annemirasol@gmail.com>
2022-02-22 17:41:03 +0800

    Support routes with locale

--- client/my-sites/invites/index.node.js
+++ client/my-sites/invites/index.node.js
@@ -1,6 +1,18 @@
 import { makeLayout } from 'calypso/controller';
-import { setOembedProviderLink } from './controller';
+import { getLanguageRouteParam } from 'calypso/lib/i18n-utils';
+import { setOembedProviderLink, redirectWithoutLocaleifLoggedIn } from './controller';
 
 export default ( router ) => {
-	router( [ `/accept-invite/:site_id/:invitation_key` ], setOembedProviderLink, makeLayout );
+	const locale = getLanguageRouteParam( 'locale' );
+
+	router(
+		[
+			`/accept-invite/:site_id/:invitation_key/${ locale }`,
+			`/accept-invite/:site_id/:invitation_key/:activation_key/${ locale }`,
+			`/accept-invite/:site_id/:invitation_key/:activation_key/:auth_key/${ locale }`,
+		],
+		setOembedProviderLink,
+		redirectWithoutLocaleifLoggedIn,
+		makeLayout
+	);
 };
