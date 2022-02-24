0eb52f37964538bbaa77c4f9d84977150b03bd96
Tony Arcangelini <tony@arcangelini.com>
2022-02-24 16:45:55 +0100

    Create detailed README for intent-screen

--- packages/onboarding-components/src/components/onboarding-components-select-items/index.tsx
+++ packages/onboarding-components/src/components/onboarding-components-select-items/index.tsx
@@ -20,31 +20,34 @@ function SelectItems< T >( {
 }: Props< T > ): React.ReactElement {
 	return (
 		<div className={ classnames( 'onboarding-components-select-items', className ) }>
-			{ items.map( ( { key, title, description, icon, actionText, value } ) => (
-				<div key={ key } className="onboarding-components-select-items__item">
-					<Icon
-						className="onboarding-components-select-items__item-icon"
-						icon={ icon }
-						size={ 24 }
-					/>
-					<div className="onboarding-components-select-items__item-info-wrapper">
-						<div className="onboarding-components-select-items__item-info">
-							<h2 className="onboarding-components-select-items__item-title">
-								{ preventWidows( title ) }
-							</h2>
-							<div className="onboarding-components-select-items__item-description">
-								{ preventWidows( description ) }
+			{ items.map(
+				( { key, title, description, icon, actionText, value, hidden } ) =>
+					hidden && (
+						<div key={ key } className="onboarding-components-select-items__item">
+							<Icon
+								className="onboarding-components-select-items__item-icon"
+								icon={ icon }
+								size={ 24 }
+							/>
+							<div className="onboarding-components-select-items__item-info-wrapper">
+								<div className="onboarding-components-select-items__item-info">
+									<h2 className="onboarding-components-select-items__item-title">
+										{ preventWidows( title ) }
+									</h2>
+									<div className="onboarding-components-select-items__item-description">
+										{ preventWidows( description ) }
+									</div>
+								</div>
+								<Button
+									className="onboarding-components-select-items__item-button"
+									onClick={ () => onSelect( value ) }
+								>
+									{ actionText }
+								</Button>
 							</div>
 						</div>
-						<Button
-							className="onboarding-components-select-items__item-button"
-							onClick={ () => onSelect( value ) }
-						>
-							{ actionText }
-						</Button>
-					</div>
-				</div>
-			) ) }
+					)
+			) }
 		</div>
 	);
 }
--- packages/onboarding-components/src/index.ts
+++ packages/onboarding-components/src/index.ts
@@ -1,4 +1,4 @@
-export { default } from './components/onboarding-components';
+export { default } from './components/intent-screen';
 export { default as SelectItems } from './components/onboarding-components-select-items';
 export { default as SelectItemsAlt } from './components/onboarding-components-select-items-alt';
 export * from './types';
