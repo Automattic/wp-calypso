19aba355a958eb55fd966a57226f068661f616b5
mmtr <miguel.torres@automattic.com>
2022-02-25 17:17:23 +0100

    Remove unnecessary import

--- client/my-sites/email/email-management/email-home.jsx
+++ client/my-sites/email/email-management/email-home.jsx
@@ -10,7 +10,6 @@ import QuerySiteDomains from 'calypso/components/data/query-site-domains';
 import EmptyContent from 'calypso/components/empty-content';
 import Main from 'calypso/components/main';
 import SectionHeader from 'calypso/components/section-header';
-import SidebarNavigation from 'calypso/components/sidebar-navigation';
 import { hasEmailForwards } from 'calypso/lib/domains/email-forwarding';
 import { hasGSuiteWithUs } from 'calypso/lib/gsuite';
 import { hasTitanMailWithUs } from 'calypso/lib/titan';
@@ -178,8 +177,6 @@ class EmailManagementHome extends Component {
 				{ selectedSiteId && <QuerySiteDomains siteId={ selectedSiteId } /> }
 
 				<DocumentHead title={ titleCase( translate( 'Emails' ) ) } />
-
-				<SidebarNavigation />
 
 				<EmailHeader />
 
