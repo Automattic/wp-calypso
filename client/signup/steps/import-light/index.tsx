59f13830e70bc5a5a09bbd146fb62223aff731c2
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-02-25 15:52:41 +0100

    Integrate import-light capture screen

--- client/signup/steps/import-light/index.tsx
+++ client/signup/steps/import-light/index.tsx
@@ -1,8 +1,33 @@
+import classnames from 'classnames';
 import React from 'react';
+import StepWrapper from 'calypso/signup/step-wrapper';
+import Capture from './capture';
 import type { FunctionComponent } from 'react';
 
-const ImportLight: FunctionComponent = () => {
-	return <div>Import Light module</div>;
+import './style.scss';
+
+interface Props {
+	stepName: string;
+	stepSectionName: string;
+}
+const ImportLight: FunctionComponent< Props > = ( props ) => {
+	return (
+		<StepWrapper
+			flowName={ 'import-light' }
+			stepName={ props.stepName }
+			stepSectionName={ props.stepSectionName }
+			hideSkip={ false }
+			hideNext={ false }
+			hideBack={ false }
+			nextLabelText={ 'Skip this step' }
+			shouldHideNavButtons={ false }
+			stepContent={
+				<div className={ classnames( 'import__onboarding-page' ) }>
+					<Capture />
+				</div>
+			}
+		/>
+	);
 };
 
 export default ImportLight;
