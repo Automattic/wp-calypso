b4fe6a596da17d8346a9dd2dd7343ecd6bf7cbfa
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-02-27 17:48:08 +0100

    Add capture input validation

--- client/signup/steps/import-light/capture/capture-input.tsx
+++ client/signup/steps/import-light/capture/capture-input.tsx
@@ -3,12 +3,13 @@ import { createElement, createInterpolateElement } from '@wordpress/element';
 import { Icon } from '@wordpress/icons';
 import classnames from 'classnames';
 import { localize, translate } from 'i18n-calypso';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
 import FormTextInput from 'calypso/components/forms/form-text-input';
 import { bulb } from 'calypso/signup/icons';
+import { CAPTURE_URL_RGX } from 'calypso/signup/steps/import/util';
 import type { FunctionComponent } from 'react';
 
 interface Props {
@@ -17,7 +18,20 @@ interface Props {
 const CaptureInput: FunctionComponent< Props > = ( props ) => {
 	const { translate } = props;
 
-	const [ isValid ] = useState( true );
+	const [ urlValue, setUrlValue ] = useState( '' );
+	const [ isValid, setIsValid ] = useState( true );
+
+	function validateUrl( url: string ) {
+		return CAPTURE_URL_RGX.test( url );
+	}
+
+	function onInputBlur() {
+		setIsValid( validateUrl( urlValue ) );
+	}
+
+	function onInputChange( e: ChangeEvent< HTMLInputElement > ) {
+		setUrlValue( e.target.value );
+	}
 
 	return (
 		<div className={ classnames( 'import-light__capture' ) }>
@@ -30,7 +44,18 @@ const CaptureInput: FunctionComponent< Props > = ( props ) => {
 						}
 					) }
 				</FormLabel>
-				<FormTextInput type="text" />
+				<FormTextInput
+					type="text"
+					// eslint-disable-next-line jsx-a11y/no-autofocus
+					autoFocus
+					autoComplete="off"
+					autoCorrect="off"
+					spellCheck="false"
+					dir="ltr"
+					value={ urlValue }
+					onBlur={ onInputBlur }
+					onChange={ onInputChange }
+				/>
 				<FormSettingExplanation>
 					<span className={ classnames( { isError: ! isValid } ) }>
 						{ isValid && (
