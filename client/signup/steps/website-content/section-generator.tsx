be9992e42cc43ab97a45987903b71c7336eb2fb8
Aneesh Devasthale <aneeshdevasthale@gmail.com>
2022-03-01 18:00:53 +0530

    Removes redundant condition

--- client/signup/steps/website-content/section-generator.tsx
+++ client/signup/steps/website-content/section-generator.tsx
@@ -37,7 +37,7 @@ const generateWebsiteContentSections = (
 				<PageDetails
 					page={ page }
 					formErrors={ formErrors }
-					label={ PAGE_LABELS[ page.id ] || undefined }
+					label={ PAGE_LABELS[ page.id ] }
 					onChangeField={ onChangeField }
 				/>
 			),
