b8ad76a21030870697f1118f77538b4d94d37292
Daniel López <daniel.lopez@automattic.com>
2022-03-01 13:54:06 +0100

    Emails: Refactor GoogleMailboxPricingNotice into MailboxPricingNotice

---
+++ client/my-sites/email/email-pricing-notice/index.tsx
@@ -0,0 +1,176 @@
+import { translate as originalTranslate, useTranslate } from 'i18n-calypso';
+import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import Notice from 'calypso/components/notice';
+import type { EmailCost, ResponseDomain } from 'calypso/lib/domains/types';
+import type { ProductListItem } from 'calypso/state/products-list/selectors/get-products-list';
+import type { TranslateResult } from 'i18n-calypso';
+import type { ReactElement } from 'react';
+
+const doesAdditionalPriceMatchStandardPrice = (
+	domain: ResponseDomain,
+	mailProduct: ProductListItem,
+	purchaseCost: EmailCost | null
+): boolean => {
+	if ( ! domain ) {
+		return true;
+	}
+
+	if ( ! purchaseCost ) {
+		return true;
+	}
+
+	return (
+		purchaseCost.amount === mailProduct.cost && purchaseCost.currency === mailProduct.currency_code
+	);
+};
+
+function getPriceMessage( {
+	mailboxPurchaseCost,
+	translate,
+}: {
+	mailboxPurchaseCost: EmailCost | null;
+	translate: typeof originalTranslate;
+} ): TranslateResult {
+	if ( mailboxPurchaseCost === null ) {
+		return '';
+	}
+	return mailboxPurchaseCost.amount === 0
+		? translate( 'You can add new mailboxes for free until the end of your trial period.' )
+		: translate(
+				'You can purchase new mailboxes at the prorated price of {{strong}}%(proratedPrice)s{{/strong}} per mailbox.',
+				{
+					args: {
+						proratedPrice: mailboxPurchaseCost.text,
+					},
+					components: {
+						strong: <strong />,
+					},
+					comment:
+						'%(proratedPrice)s is a formatted price for an email subscription (e.g. $3.50, €3.75, or PLN 4.50)',
+				}
+		  );
+}
+
+function getPriceMessageExplanation( {
+	mailboxPurchaseCost,
+	mailboxRenewalCost,
+	translate,
+}: {
+	mailboxPurchaseCost: EmailCost | null;
+	mailboxRenewalCost: EmailCost | null;
+	mailProduct: ProductListItem;
+	translate: typeof originalTranslate;
+} ): TranslateResult {
+	if ( mailboxPurchaseCost === null || mailboxRenewalCost === null ) {
+		return '';
+	}
+
+	// We don't need any explanation of the price at this point, because we have already handled it previously.
+	if ( mailboxPurchaseCost.amount === 0 ) {
+		return '';
+	}
+
+	if ( mailboxPurchaseCost.amount < mailboxRenewalCost.amount ) {
+		return translate(
+			'This is less than the regular price because you are only charged for the remainder of the current year.'
+		);
+	}
+
+	return translate(
+		'This is more than the regular price because you are charged for the remainder of the current year plus any additional year until renewal.'
+	);
+}
+
+function getPriceMessageRenewal( {
+	expiryDate,
+	mailboxRenewalCost,
+	translate,
+}: {
+	expiryDate: string;
+	mailboxRenewalCost: EmailCost | null;
+	translate: typeof originalTranslate;
+} ): TranslateResult {
+	if ( mailboxRenewalCost === null ) {
+		return '';
+	}
+
+	return translate(
+		'All of your mailboxes are due to renew at the regular price of {{strong}}%(fullPrice)s{{/strong}} per mailbox when your subscription renews on {{strong}}%(expiryDate)s{{/strong}}.',
+		{
+			args: {
+				fullPrice: mailboxRenewalCost.text,
+				expiryDate: expiryDate,
+			},
+			components: {
+				strong: <strong />,
+			},
+			comment:
+				'%(fullPrice)s is a formatted price for an email subscription (e.g. $3.50, €3.75, or PLN 4.50), ' +
+				'%(expiryDate)s is a localized date (e.g. February 17, 2021)',
+		}
+	);
+}
+
+interface MailboxPricingNoticeProps {
+	domain: ResponseDomain;
+	expiryDate: string | null;
+	mailProduct: ProductListItem;
+	mailboxPurchaseCost: EmailCost | null;
+	mailboxRenewalCost: EmailCost | null;
+}
+
+const MailboxPricingNotice = ( {
+	domain,
+	expiryDate,
+	mailProduct,
+	mailboxPurchaseCost,
+	mailboxRenewalCost,
+}: MailboxPricingNoticeProps ): ReactElement | null => {
+	const moment = useLocalizedMoment();
+	const translate = useTranslate();
+
+	if ( doesAdditionalPriceMatchStandardPrice( domain, mailProduct, mailboxPurchaseCost ) ) {
+		const translateArgs = {
+			args: {
+				price: mailboxPurchaseCost?.text,
+			},
+			components: {
+				strong: <strong />,
+			},
+			comment:
+				'%(price)s is a formatted price for an email subscription (e.g. $3.50, €3.75, or PLN 4.50)',
+		};
+
+		return (
+			<Notice icon="info-outline" showDismiss={ false } status="is-success">
+				{ translate(
+					'You can purchase new mailboxes at the regular price of {{strong}}%(price)s{{/strong}} per mailbox per year.',
+					translateArgs
+				) }
+			</Notice>
+		);
+	}
+
+	const priceMessage = getPriceMessage( { mailboxPurchaseCost, translate } );
+	const priceMessageExplanation = getPriceMessageExplanation( {
+		mailboxPurchaseCost,
+		mailboxRenewalCost,
+		mailProduct,
+		translate,
+	} );
+	const priceMessageRenewal = getPriceMessageRenewal( {
+		expiryDate: moment( expiryDate ).format( 'LL' ),
+		mailboxRenewalCost,
+		translate,
+	} );
+
+	return (
+		<Notice icon="info-outline" showDismiss={ false } status="is-success">
+			<>
+				{ priceMessage } { priceMessageExplanation } { priceMessageRenewal }
+			</>
+		</Notice>
+	);
+};
+
+export default MailboxPricingNotice;
