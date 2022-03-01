9242da5c143be5ee0a0ef27752e29882435b9904
Enej Bajgoric <enej.bajgoric@automattic.com>
2022-02-28 16:34:45 -0800

    Revert changes  to isAppBannerEnabled

--- client/state/ui/selectors/app-banner-is-visible.js
+++ client/state/ui/selectors/app-banner-is-visible.js
@@ -1,12 +1,7 @@
 import 'calypso/state/ui/init';
-import { getCurrentFlowName } from 'calypso/state/signup/flow/selectors';
 import { getCurrentLayoutFocus } from 'calypso/state/ui/layout-focus/selectors';
 
 export default function isAppBannerEnabled( state ) {
-	// Never show the AppBanner in the site setup flow.
-	if ( getCurrentFlowName( state ) === 'setup-site' ) {
-		return false;
-	}
 	// since on mobile the sidebar layout focus take up the whole "Page" we never want to show the App Banner
 	// when the side bar in in focus.
 	return state.ui.appBannerVisibility && getCurrentLayoutFocus( state ) !== 'sidebar';
