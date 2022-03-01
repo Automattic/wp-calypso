8f26163809b8523e7cedce1aeaf7a964c887f402
vitozev <kristian.vitozev@automattic.com>
2022-03-01 22:04:47 +0200

    Rename the existing payment methods store

--- client/jetpack-cloud/sections/partner-portal/payment-methods/hooks/use-create-stored-credit-card.ts
+++ client/jetpack-cloud/sections/partner-portal/payment-methods/hooks/use-create-stored-credit-card.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import { createStoredCreditCardMethod } from 'calypso/jetpack-cloud/sections/partner-portal/payment-methods/stored-credit-card-method';
-import { createStoredCreditCardPaymentMethodStore } from 'calypso/state/partner-portal/payment-methods';
+import { createStoredCreditCardPaymentMethodStore } from 'calypso/state/partner-portal/credit-card-form';
 import type { StripeConfiguration, StripeLoadingError } from '@automattic/calypso-stripe';
 import type { PaymentMethod } from '@automattic/composite-checkout';
 import type { Stripe } from '@stripe/stripe-js';
