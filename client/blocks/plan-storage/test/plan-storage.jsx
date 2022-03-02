6f7d16fb2e73cafb9e4e2fca42abadab5017810a
Marin Atanasov <tyxla@abv.bg>
2022-03-02 22:16:12 +0200

    Blocks: Update PlanStorage tests

--- client/blocks/plan-storage/test/plan-storage.jsx
+++ client/blocks/plan-storage/test/plan-storage.jsx
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import {
 	PLAN_ECOMMERCE,
 	PLAN_ECOMMERCE_2_YEARS,
@@ -10,90 +13,141 @@ import {
 	PLAN_FREE,
 } from '@automattic/calypso-products';
 import { assert } from 'chai';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
 import { PlanStorage } from '../index';
 
-describe( 'PlanStorage basic tests', () => {
-	const props = {
-		canViewBar: true,
-		mediaStorage: {
-			max_storage_bytes: 1000,
-		},
-		siteId: 123,
-		siteSlug: 'example.com',
-		sitePlanSlug: PLAN_FREE,
-	};
+const siteId = 123;
 
+function render( component, additionalState = {}, planSlug = 'free_plan' ) {
+	const queryClient = new QueryClient();
+	const store = {
+		getState: () => ( {
+			currentUser: {
+				capabilities: {
+					[ siteId ]: {
+						publish_posts: true,
+					},
+				},
+			},
+			sites: {
+				items: {
+					[ siteId ]: {
+						name: 'yourjetpack.blog',
+						plan: {
+							product_slug: planSlug,
+						},
+					},
+				},
+			},
+			...additionalState,
+		} ),
+		subscribe: () => {},
+		dispatch: () => {},
+	};
+	return mount(
+		<QueryClientProvider client={ queryClient }>
+			<Provider store={ store }>{ component }</Provider>
+		</QueryClientProvider>
+	);
+}
+
+describe( 'PlanStorage basic tests', () => {
 	test( 'should not blow up and have class .plan-storage', () => {
-		const storage = shallow( <PlanStorage { ...props } /> );
+		const storage = render( <PlanStorage siteId={ siteId } /> );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 	} );
 
 	test( 'should render a PlanStorageBar', () => {
-		const storage = shallow( <PlanStorage { ...props } /> );
+		const storage = render( <PlanStorage siteId={ siteId } /> );
 		const bar = storage.find( 'Localized(PlanStorageBar)' );
 		assert.lengthOf( bar, 1 );
-		assert.equal( bar.props().sitePlanSlug, props.sitePlanSlug );
-		assert.equal( bar.props().mediaStorage, props.mediaStorage );
 	} );
 
 	test( 'should render when storage is limited', () => {
 		let storage;
 
-		storage = shallow( <PlanStorage { ...props } sitePlanSlug={ PLAN_PREMIUM } /> );
+		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_PREMIUM );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 
-		storage = shallow( <PlanStorage { ...props } sitePlanSlug={ PLAN_PREMIUM_2_YEARS } /> );
+		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_PREMIUM_2_YEARS );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 
-		storage = shallow( <PlanStorage { ...props } sitePlanSlug={ PLAN_PERSONAL } /> );
+		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_PERSONAL );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 
-		storage = shallow( <PlanStorage { ...props } sitePlanSlug={ PLAN_PERSONAL_2_YEARS } /> );
+		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_PERSONAL_2_YEARS );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 
-		storage = shallow( <PlanStorage { ...props } sitePlanSlug={ PLAN_FREE } /> );
+		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_FREE );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 
-		storage = shallow( <PlanStorage { ...props } sitePlanSlug={ PLAN_BUSINESS } /> );
+		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_BUSINESS );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 
-		storage = shallow( <PlanStorage { ...props } sitePlanSlug={ PLAN_BUSINESS_2_YEARS } /> );
+		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_BUSINESS_2_YEARS );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 
-		storage = shallow( <PlanStorage { ...props } sitePlanSlug={ PLAN_ECOMMERCE } /> );
+		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_ECOMMERCE );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 
-		storage = shallow( <PlanStorage { ...props } sitePlanSlug={ PLAN_ECOMMERCE_2_YEARS } /> );
+		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_ECOMMERCE_2_YEARS );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 	} );
 
 	test( 'should render for atomic sites', () => {
-		const storage = shallow(
-			<PlanStorage
-				{ ...props }
-				sitePlanSlug={ PLAN_BUSINESS }
-				jetpackSite={ true }
-				atomicSite={ true }
-			/>
-		);
+		const storage = render( <PlanStorage siteId={ siteId } />, {
+			sites: {
+				items: {
+					[ siteId ]: {
+						name: 'yourjetpack.blog',
+						jetpack: true,
+						plan: {
+							product_slug: PLAN_BUSINESS,
+						},
+						options: {
+							is_automated_transfer: true,
+						},
+					},
+				},
+			},
+		} );
 		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
 	} );
 
 	test( 'should not render for jetpack sites', () => {
-		const storage = shallow(
-			<PlanStorage { ...props } jetpackSite={ true } atomicSite={ false } />
-		);
+		const storage = render( <PlanStorage siteId={ siteId } />, {
+			sites: {
+				items: {
+					[ siteId ]: {
+						name: 'yourjetpack.blog',
+						jetpack: true,
+						plan: {
+							product_slug: 'free_plan',
+						},
+					},
+				},
+			},
+		} );
 		assert.lengthOf( storage.find( '.plan-storage' ), 0 );
 	} );
 
 	test( 'should not render for contributors', () => {
-		const storage = shallow( <PlanStorage { ...props } canViewBar={ false } /> );
+		const storage = render( <PlanStorage siteId={ siteId } />, {
+			currentUser: {
+				capabilities: {
+					[ siteId ]: {
+						publish_posts: false,
+					},
+				},
+			},
+		} );
 		assert.lengthOf( storage.find( '.plan-storage' ), 0 );
 	} );
 
 	test( 'should not render when site plan slug is empty', () => {
-		const storage = shallow( <PlanStorage { ...props } sitePlanSlug={ null } /> );
+		const storage = render( <PlanStorage siteId={ siteId } />, {}, null );
 		assert.lengthOf( storage.find( '.plan-storage' ), 0 );
 	} );
 } );
