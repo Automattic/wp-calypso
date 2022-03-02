2e23f00cccdaff33640a6f10503e118eefb9d333
Roberto Aranda <ese.rober@gmail.com>
2022-03-02 11:54:02 +0100

    Plugins: Display live chat support only for annual plans (#61371)
    * Display live chat support only for annual plans
    
    The live chat support message when purchasing a plugin will only be
    displayed for annual plans regardless of the billing period of the
    plugin.
    
    Fixes #61365
    
    * Apply suggestions from code review
    
    Co-authored-by: Harris Papazoglou <hrrsppzgl@gmail.com>
    
    * Fix style after applying code review suggestions
    
    * Add an additional condition to cope with upgrades
    
    When the upgrade information is displayed, showing the live chat support
    will depend on the billing period of the plugin as that will subsequently
    derive the period of the plan to be upgraded.
    
    * proposal: use the same logic in sidebar and under CTA button to display support USP
    
    * Extract common logic to a selector
    
    That will make the logic reusable and robust as the same logic will be
    shared by the consumers
    
    Co-authored-by: Harris Papazoglou <hrrsppzgl@gmail.com>
    Co-authored-by: gcsecsey <gcsecsey@gmail.com>

--- client/state/marketplace/selectors.ts
+++ client/state/marketplace/selectors.ts
@@ -1,7 +1,11 @@
-import { isBusiness, isEcommerce, isEnterprise } from '@automattic/calypso-products';
+import { isBusiness, isEcommerce, isEnterprise, isMonthly } from '@automattic/calypso-products';
+import { IntervalLength } from 'calypso/my-sites/marketplace/components/billing-interval-switcher/constants';
+import { getBillingInterval } from 'calypso/state/marketplace/billing-interval/selectors';
 import { default as isVipSite } from 'calypso/state/selectors/is-vip-site';
+import { getCurrentPlan } from 'calypso/state/sites/plans/selectors';
 import { isJetpackSite } from 'calypso/state/sites/selectors';
 import { IAppState } from 'calypso/state/types';
+import { getSelectedSiteId, getSelectedSite } from 'calypso/state/ui/selectors';
 import type { WithCamelCaseSlug, WithSnakeCaseSlug } from '@automattic/calypso-products';
 
 const shouldUpgradeCheck = (
@@ -17,6 +21,20 @@ const shouldUpgradeCheck = (
 				isVipSite( state, selectedSite?.ID )
 		  )
 		: null;
+};
+
+export const isAnnualPlanOrUpgradeableAnnualPeriod = ( state: IAppState ) => {
+	const selectedSiteId = getSelectedSiteId( state );
+	const currentPlan = selectedSiteId && getCurrentPlan( state, selectedSiteId );
+	const isAnnualPlan = currentPlan && ! isMonthly( currentPlan.productSlug );
+
+	const selectedSite = getSelectedSite( state );
+	const shouldUpgrade = selectedSite && shouldUpgradeCheck( state, selectedSite );
+
+	const billingPeriod = getBillingInterval( state );
+	const isAnnualPeriod = billingPeriod === IntervalLength.ANNUALLY;
+
+	return ( ! shouldUpgrade && isAnnualPlan ) || ( shouldUpgrade && isAnnualPeriod );
 };
 
 export default shouldUpgradeCheck;
