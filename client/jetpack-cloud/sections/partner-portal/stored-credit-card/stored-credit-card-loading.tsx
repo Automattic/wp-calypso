a346b12558d2b3b02b45162e3c5791e8ba2a4bbf
vitozev <kristian.vitozev@automattic.com>
2022-03-02 19:32:09 +0200

    Simplify styles for payment method placeholder

--- client/jetpack-cloud/sections/partner-portal/stored-credit-card/stored-credit-card-loading.tsx
+++ client/jetpack-cloud/sections/partner-portal/stored-credit-card/stored-credit-card-loading.tsx
@@ -3,10 +3,10 @@ import './style.scss';
 export default function StoredCreditCardLoading(): JSX.Element {
 	return (
 		<div className="stored-credit-card stored-credit-card-loading">
-			<div className="stored-credit-card-loading__top stored-credit-card-loading--placeholder" />
+			<div className="stored-credit-card-loading__content" />
 			<div className="stored-credit-card-loading__bottom">
-				<div className="stored-credit-card-loading__cardholder stored-credit-card-loading--placeholder" />
-				<div className="stored-credit-card-loading__card stored-credit-card-loading--placeholder" />
+				<div className="stored-credit-card-loading__content" />
+				<div className="stored-credit-card-loading__content" />
 			</div>
 		</div>
 	);
