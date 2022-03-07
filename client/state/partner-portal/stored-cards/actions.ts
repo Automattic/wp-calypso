033eaed42827e918f20fc89b285c0784b877e86f
Kristian Vitozev <kristian.vitozev@automattic.com>
2022-03-07 08:49:30 +0200

    Licensing Portal: Remove non-primary payment methods modal (#61247)
    * Credit card removal modal skeleton
    
    * Add new files
    
    * Rebase
    
    * Basic flow for removing a non-primary payment mehod in Stripe
    
    * Fix dialog action buttons width on small screen sizes

--- client/state/partner-portal/stored-cards/actions.ts
+++ client/state/partner-portal/stored-cards/actions.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18n-calypso';
-import { PaymentMethod } from 'calypso/jetpack-cloud/sections/partner-portal/payment-methods';
 import { wpcomJetpackLicensing as wpcomJpl } from 'calypso/lib/wp';
+import type { PaymentMethod } from 'calypso/jetpack-cloud/sections/partner-portal/payment-methods';
 import type { AnyAction, Dispatch } from 'redux';
 
 import 'calypso/state/partner-portal/stored-cards/init';
@@ -21,10 +21,40 @@ export const fetchStoredCards = () => ( dispatch: Dispatch< AnyAction > ) => {
 				list: data,
 			} );
 		} )
-		.catch( ( error: { message: string } ) => {
+		.catch( ( error: Error ) => {
 			dispatch( {
 				type: 'STORED_CARDS_FETCH_FAILED',
 				error: error.message || i18n.translate( 'There was a problem retrieving stored cards.' ),
 			} );
+		} );
+};
+
+export const deleteStoredCard = ( card: PaymentMethod ) => ( dispatch: Dispatch< AnyAction > ) => {
+	dispatch( {
+		type: 'STORED_CARDS_DELETE',
+		card,
+	} );
+
+	return wpcomJpl.req
+		.post( {
+			method: 'DELETE',
+			apiNamespace: 'wpcom/v2',
+			path: '/jetpack-licensing/stripe/payment-method',
+			body: { payment_method_id: card.id },
+		} )
+		.then( () => {
+			dispatch( {
+				type: 'STORED_CARDS_DELETE_COMPLETED',
+				card,
+			} );
+		} )
+		.catch( ( error: Error ) => {
+			dispatch( {
+				type: 'STORED_CARDS_DELETE_FAILED',
+				card,
+				error: error.message || i18n.translate( 'There was a problem deleting the stored card.' ),
+			} );
+
+			return Promise.reject( error );
 		} );
 };
