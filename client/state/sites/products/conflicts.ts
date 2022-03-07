64853f7f6c3b9972f94210c63678cc5a6b2469dc
Nate Weller <nate.weller@automattic.com>
2022-03-07 12:41:23 -0700

    Check against all backup features in isBackupProductIncludedInSitePlan() (#61582)

--- client/state/sites/products/conflicts.ts
+++ client/state/sites/products/conflicts.ts
@@ -1,20 +1,23 @@
 import {
-	planHasFeature,
 	planHasAtLeastOneFeature,
 	planHasSuperiorFeature,
+	JETPACK_PLANS,
 	FEATURE_JETPACK_BACKUP_REALTIME,
 	FEATURE_JETPACK_BACKUP_DAILY,
-	JETPACK_PLANS,
-	PRODUCT_JETPACK_BACKUP_DAILY,
-	PRODUCT_JETPACK_BACKUP_REALTIME,
-	PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY,
-	PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY,
 	FEATURE_JETPACK_BACKUP_DAILY_MONTHLY,
 	FEATURE_JETPACK_BACKUP_REALTIME_MONTHLY,
 	FEATURE_JETPACK_BACKUP_T1_MONTHLY,
 	FEATURE_JETPACK_BACKUP_T1_YEARLY,
 	FEATURE_JETPACK_BACKUP_T2_MONTHLY,
 	FEATURE_JETPACK_BACKUP_T2_YEARLY,
+	PRODUCT_JETPACK_BACKUP_DAILY,
+	PRODUCT_JETPACK_BACKUP_REALTIME,
+	PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY,
+	PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY,
+	PRODUCT_JETPACK_BACKUP_T1_MONTHLY,
+	PRODUCT_JETPACK_BACKUP_T1_YEARLY,
+	PRODUCT_JETPACK_BACKUP_T2_MONTHLY,
+	PRODUCT_JETPACK_BACKUP_T2_YEARLY,
 } from '@automattic/calypso-products';
 import { createSelector } from '@automattic/state-utils';
 import getRewindCapabilities from 'calypso/state/selectors/get-rewind-capabilities';
@@ -26,6 +29,11 @@ const DAILY_BACKUP_FEATURES = [
 	FEATURE_JETPACK_BACKUP_DAILY,
 ];
 
+const DAILY_BACKUP_PRODUCTS = [
+	PRODUCT_JETPACK_BACKUP_DAILY,
+	PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY,
+];
+
 const REALTIME_BACKUP_FEATURES = [
 	FEATURE_JETPACK_BACKUP_REALTIME_MONTHLY,
 	FEATURE_JETPACK_BACKUP_REALTIME,
@@ -35,23 +43,46 @@ const REALTIME_BACKUP_FEATURES = [
 	FEATURE_JETPACK_BACKUP_T2_YEARLY,
 ];
 
+const REALTIME_BACKUP_PRODUCTS = [
+	PRODUCT_JETPACK_BACKUP_REALTIME,
+	PRODUCT_JETPACK_BACKUP_T1_YEARLY,
+	PRODUCT_JETPACK_BACKUP_T2_YEARLY,
+	PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY,
+	PRODUCT_JETPACK_BACKUP_T1_MONTHLY,
+	PRODUCT_JETPACK_BACKUP_T2_MONTHLY,
+];
+
 /**
  * Check is a Jetpack plan is including a daily backup feature.
  *
  * @param {string} planSlug The plan slug.
+ * @param {boolean} orSuperior When true, also checks plan for superior features.
  * @returns {boolean} True if the plan includes a daily backup feature.
  */
-export const planHasDailyBackup = ( planSlug: string ): boolean =>
-	planHasAtLeastOneFeature( planSlug, DAILY_BACKUP_FEATURES );
+export const planHasDailyBackup = ( planSlug: string, orSuperior = false ): boolean => {
+	const hasFeature = planHasAtLeastOneFeature( planSlug, DAILY_BACKUP_FEATURES );
+	const hasSuperiorFeature = DAILY_BACKUP_FEATURES.some( ( feature ) =>
+		planHasSuperiorFeature( planSlug, feature )
+	);
+
+	return orSuperior ? hasFeature || hasSuperiorFeature : hasFeature;
+};
 
 /**
  * Check is a Jetpack plan is including a real-time backup feature.
  *
  * @param {string} planSlug The plan slug.
+ * @param {boolean} orSuperior When true, also checks plan for superior features.
  * @returns {boolean} True if the plan includes a real-time backup feature.
  */
-export const planHasRealTimeBackup = ( planSlug: string ): boolean =>
-	planHasAtLeastOneFeature( planSlug, REALTIME_BACKUP_FEATURES );
+export const planHasRealTimeBackup = ( planSlug: string, orSuperior = false ): boolean => {
+	const hasFeature = planHasAtLeastOneFeature( planSlug, REALTIME_BACKUP_FEATURES );
+	const hasSuperiorFeature = REALTIME_BACKUP_FEATURES.some( ( feature ) =>
+		planHasSuperiorFeature( planSlug, feature )
+	);
+
+	return orSuperior ? hasFeature || hasSuperiorFeature : hasFeature;
+};
 
 /**
  * Check if a Jetpack plan is including a Backup product a site might already have.
@@ -118,25 +149,15 @@ export const isBackupProductIncludedInSitePlan = createSelector(
 			return null;
 		}
 
-		let feature;
-
-		if (
-			[ PRODUCT_JETPACK_BACKUP_DAILY, PRODUCT_JETPACK_BACKUP_DAILY_MONTHLY ].includes( productSlug )
-		) {
-			feature = FEATURE_JETPACK_BACKUP_DAILY;
-		} else if (
-			[ PRODUCT_JETPACK_BACKUP_REALTIME, PRODUCT_JETPACK_BACKUP_REALTIME_MONTHLY ].includes(
-				productSlug
-			)
-		) {
-			feature = FEATURE_JETPACK_BACKUP_REALTIME;
-		} else {
-			return null;
+		if ( DAILY_BACKUP_PRODUCTS.includes( productSlug ) ) {
+			return planHasDailyBackup( sitePlanSlug, true );
 		}
 
-		return (
-			planHasFeature( sitePlanSlug, feature ) || planHasSuperiorFeature( sitePlanSlug, feature )
-		);
+		if ( REALTIME_BACKUP_PRODUCTS.includes( productSlug ) ) {
+			return planHasRealTimeBackup( sitePlanSlug, true );
+		}
+
+		return null;
 	},
 	[
 		( state: AppState, siteId: number | null, productSlug: string ) => [
