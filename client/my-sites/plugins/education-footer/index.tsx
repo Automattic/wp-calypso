d943283049f5b739b787515462e7ba30a25d1bbc
Rolando Perez <me@rolilink.com>
2022-03-08 20:37:06 -0500

    adjusting font sizes

--- client/my-sites/plugins/education-footer/index.tsx
+++ client/my-sites/plugins/education-footer/index.tsx
@@ -10,14 +10,14 @@ const FeatureItemContainer = styled.div`
 
 const FeatureItemHeader = styled.div`
 	margin-bottom: 16px;
-	font-size: var( --font-body );
+	font-size: var( --scss-font-body );
 	font-weight: 500;
 	line-height: 24px;
 	color: var( --color-text-inverted );
 `;
 
 const FeatureItemContent = styled.p`
-	font-size: var( --font-body-small );
+	font-size: var( --scss-font-body-small );
 	font-weight: 400;
 	line-height: 22px;
 	color: var( --color-neutral-20 );
