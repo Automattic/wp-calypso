f4394f996b9094917ab60a15a5905b2f70918989
Aneesh Devasthale <aneeshdevasthale@gmail.com>
2022-03-11 16:44:21 +0530

    Add socialProfiles reducer

--- client/state/signup/steps/reducer.js
+++ client/state/signup/steps/reducer.js
@@ -4,6 +4,7 @@ import siteInformationCollection from './site-info-collection/reducer';
 import siteTitle from './site-title/reducer';
 import siteType from './site-type/reducer';
 import siteVertical from './site-vertical/reducer';
+import socialProfiles from './social-profiles/reducer';
 import survey from './survey/reducer';
 import websiteContentCollection from './website-content/reducer';
 
@@ -15,4 +16,5 @@ export default combineReducers( {
 	survey,
 	siteInformationCollection,
 	websiteContentCollection,
+	socialProfiles,
 } );
