8a9e2d7ff4d454183015a817306c13a0e3900e08
Aneesh Devasthale <aneeshd16@users.noreply.github.com>
2022-03-11 10:43:04 +0530

    Adds feature flag for the redesigned DIFM flow (#61795)
    * Adds feature flag for the redesigned DIFM flow
    
    * Revert branch steps change
    
    * Update config/development.json
    
    Sets feature flag value to true.
    
    * Remove branch steps flag

--- client/signup/config/flows-pure.js
+++ client/signup/config/flows-pure.js
@@ -375,17 +375,26 @@ export function generateFlows( {
 		},
 		{
 			name: 'do-it-for-me',
-			steps: [
-				'user',
-				'new-or-existing-site',
-				'difm-site-picker',
-				'site-info-collection',
-				'difm-design-setup-site',
-			],
+			steps: isEnabled( 'signup/redesigned-difm-flow' )
+				? [
+						// Add or replace new steps here
+						'user',
+						'new-or-existing-site',
+						'difm-site-picker',
+						'site-info-collection',
+						'difm-design-setup-site',
+				  ]
+				: [
+						'user',
+						'new-or-existing-site',
+						'difm-site-picker',
+						'site-info-collection',
+						'difm-design-setup-site',
+				  ],
 			destination: getDIFMSignupDestination,
 			description: 'A flow for DIFM Lite leads',
 			excludeFromManageSiteFlows: true,
-			lastModified: '2021-09-30',
+			lastModified: '2022-03-10',
 		},
 
 		{
