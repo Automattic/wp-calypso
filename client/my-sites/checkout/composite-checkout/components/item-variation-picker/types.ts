02c76e42ad2318be1b022fe73f2b3f5092432d39
Caleb Bauermeister <cbauerman@gmail.com>
2022-03-14 09:11:20 -0700

    remove new, unused property from type

--- client/my-sites/checkout/composite-checkout/components/item-variation-picker/types.ts
+++ client/my-sites/checkout/composite-checkout/components/item-variation-picker/types.ts
@@ -5,7 +5,6 @@ export type WPCOMProductSlug = string;
 export type WPCOMProductVariant = {
 	variantLabel: string;
 	variantDetails: React.ReactNode;
-	variantPrice: string | null;
 	productSlug: WPCOMProductSlug;
 	productId: number;
 };
