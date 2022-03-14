b6db6ed8e5283fd62c8f76ee741d3e8d75ac163e
Aneesh Devasthale <aneeshdevasthale@gmail.com>
2022-03-14 19:12:34 +0530

    Move state to DIFM slice outside the signup state

--- client/state/signup/steps/social-profiles/selectors.ts
+++
@@ -1,7 +0,0 @@
-import 'calypso/state/signup/init';
-import { initialState } from './schema';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function getSocialProfiles( state: any ) {
-	return state.signup?.steps?.socialProfiles || initialState;
-}
