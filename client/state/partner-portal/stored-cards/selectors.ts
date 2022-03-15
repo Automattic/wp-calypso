df363b6f70ae04a30164cbab6e211869d7a22629
Kristian Vitozev <kristian.vitozev@automattic.com>
2022-03-15 15:49:44 +0200

    Licensing Portal: Use "per_page" count from the payment methods API response (#61777)
    * Use per_page count from the payment methods API response
    
    * Fix initial state
    
    * Replace Number with parseInt (only integers are supported)
    
    Co-authored-by: Renzo Canepa <rcanepag@gmail.com>

--- client/state/partner-portal/stored-cards/selectors.ts
+++ client/state/partner-portal/stored-cards/selectors.ts
@@ -2,8 +2,13 @@ import 'calypso/state/partner-portal/stored-cards/init';
 import type { PaymentMethod } from 'calypso/jetpack-cloud/sections/partner-portal/payment-methods';
 import type { AppState } from 'calypso/types';
 
+const DEFAULT_CARDS_PER_PAGE = 30;
+
 export const getAllStoredCards = ( state: AppState ): PaymentMethod[] =>
 	state?.partnerPortal?.storedCards?.items ?? [];
+
+export const getStoredCardsPerPage = ( state: AppState ): number =>
+	parseInt( state?.partnerPortal?.storedCards?.itemsPerPage ) || DEFAULT_CARDS_PER_PAGE;
 
 export const isFetchingStoredCards = ( state: AppState ) =>
 	Boolean( state?.partnerPortal?.storedCards?.isFetching );
