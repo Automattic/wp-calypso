294de6bd1213f1d420b9495ad9ad762d5da2f5a5
elliottprogrammer <bryan@elliottprogrammer.com>
2022-03-15 10:18:28 -0400

    Adapt pricing cards to Jetpack Cloud upsells.

--- client/my-sites/scan/scan-upsell/index.jsx
+++ client/my-sites/scan/scan-upsell/index.jsx
@@ -1,10 +1,13 @@
-import { PRODUCT_JETPACK_SCAN } from '@automattic/calypso-products';
+import { PRODUCT_JETPACK_SCAN, TERM_ANNUALLY } from '@automattic/calypso-products';
 import { useTranslate } from 'i18n-calypso';
 import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import VaultPressLogo from 'calypso/assets/images/jetpack/vaultpress-logo.svg';
 import DocumentHead from 'calypso/components/data/document-head';
-import JetpackProductCard from 'calypso/components/jetpack/card/jetpack-product-card';
+import QueryIntroOffers from 'calypso/components/data/query-intro-offers';
+import QueryJetpackSaleCoupon from 'calypso/components/data/query-jetpack-sale-coupon';
+import QueryProductsList from 'calypso/components/data/query-products-list';
+import QuerySiteProducts from 'calypso/components/data/query-site-products';
 import JetpackDisconnected from 'calypso/components/jetpack/jetpack-disconnected';
 import SecurityIcon from 'calypso/components/jetpack/security-icon';
 import Upsell from 'calypso/components/jetpack/upsell';
@@ -12,9 +15,13 @@ import Main from 'calypso/components/main';
 import SidebarNavigation from 'calypso/components/sidebar-navigation';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
 import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
+import { getPurchaseURLCallback } from 'calypso/my-sites/plans/jetpack-plans/get-purchase-url-callback';
+import ProductCard from 'calypso/my-sites/plans/jetpack-plans/product-card';
 import slugToSelectorProduct from 'calypso/my-sites/plans/jetpack-plans/slug-to-selector-product';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import { getCurrentUserCurrencyCode } from 'calypso/state/currency-code/selectors';
 import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import getSelectedSiteId from 'calypso/state/ui/selectors/get-selected-site-id';
 
 import './style.scss';
 
@@ -58,26 +65,46 @@ function ScanVPActiveBody() {
 }
 
 function ScanUpsellBody() {
-	const selectedSiteSlug = useSelector( getSelectedSiteSlug );
-	const dispatch = useDispatch();
 	const translate = useTranslate();
+	const siteId = useSelector( getSelectedSiteId ) || -1;
+	const selectedSiteSlug = useSelector( getSelectedSiteSlug ) || '';
+	const currencyCode = useSelector( getCurrentUserCurrencyCode );
+	const createProductURL = getPurchaseURLCallback( selectedSiteSlug, {} );
+	const dispatch = useDispatch();
 	const item = slugToSelectorProduct( PRODUCT_JETPACK_SCAN );
+
 	const onClick = useCallback(
 		() => dispatch( recordTracksEvent( 'calypso_jetpack_scan_upsell_click' ) ),
 		[ dispatch ]
 	);
 
 	return (
-		<JetpackProductCard
-			buttonLabel={ translate( 'Upgrade now' ) }
-			buttonPrimary
-			buttonURL={ `https://jetpack.com/upgrade/scan/?site=${ selectedSiteSlug }` }
-			description={ item.description }
-			headerLevel={ 3 }
-			hidePrice
-			item={ item }
-			onButtonClick={ onClick }
-		/>
+		<>
+			<QueryJetpackSaleCoupon />
+			<QueryProductsList type="jetpack" />
+			{ siteId && <QueryIntroOffers siteId={ siteId } /> }
+			{ siteId && <QuerySiteProducts siteId={ siteId } /> }
+			<Upsell
+				headerText={ translate( 'Your site does not have Scan' ) }
+				bodyText={ translate(
+					'Scan gives you automated scanning and one-click fixes to keep your site ahead of security threats.'
+				) }
+				iconComponent={
+					<div className="scan-upsell__icon">
+						<SecurityIcon icon="info" />
+					</div>
+				}
+			/>
+			<ProductCard
+				item={ item }
+				onClick={ onClick }
+				createButtonURL={ createProductURL }
+				siteId={ siteId }
+				currencyCode={ currencyCode }
+				selectedTerm={ TERM_ANNUALLY }
+				hideSavingLabel={ false }
+			/>
+		</>
 	);
 }
 
