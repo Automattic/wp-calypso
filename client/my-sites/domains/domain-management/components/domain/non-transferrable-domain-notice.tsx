588d32b6ae91ef6fae1f779ca145f31961fd253b
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2022-03-15 18:23:40 -0300

    Correct non-transferrable domain notice message

--- client/my-sites/domains/domain-management/components/domain/non-transferrable-domain-notice.tsx
+++ client/my-sites/domains/domain-management/components/domain/non-transferrable-domain-notice.tsx
@@ -4,7 +4,7 @@ import Notice from 'calypso/components/notice';
 const NonTransferrableDomainNotice = ( { domainName }: { domainName: string } ): JSX.Element => {
 	const translate = useTranslate();
 	const text = translate(
-		"{{strong}}%(domain)s{{/strong}} is already past its redemption period so it's not possible to transfer it.",
+		"{{strong}}%(domain)s{{/strong}} is in redemption so it's not possible to transfer it.",
 		{
 			components: { strong: <strong /> },
 			args: { domain: domainName },
