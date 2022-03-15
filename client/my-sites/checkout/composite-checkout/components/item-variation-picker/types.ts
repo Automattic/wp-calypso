3ee88fde98fb76459d3b3e47fbe288709b27da25
Caleb Bauermeister <cbauerman@gmail.com>
2022-03-15 14:48:11 -0700

    Refactor Variant Item Rendering (#61929)

--- client/my-sites/checkout/composite-checkout/components/item-variation-picker/types.ts
+++ client/my-sites/checkout/composite-checkout/components/item-variation-picker/types.ts
@@ -3,10 +3,12 @@ import type { ResponseCartProduct } from '@automattic/shopping-cart';
 export type WPCOMProductSlug = string;
 
 export type WPCOMProductVariant = {
-	variantLabel: string;
-	variantDetails: React.ReactNode;
-	productSlug: WPCOMProductSlug;
+	discountPercentage: number;
+	formattedCurrentPrice: string | null;
+	formattedPriceBeforeDiscount: string | null;
 	productId: number;
+	productSlug: WPCOMProductSlug;
+	variantLabel: string;
 };
 
 export type ItemVariationPickerProps = {
