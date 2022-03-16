fe98b01a2b617a3ac56b7d47cb912a6c1bed8297
Roberto Aranda <roberto.aranda@automattic.com>
2022-03-16 20:46:32 +0100

    Breadcrumb: Update styles based on the Domains one
    *Note:* The behaviour of the existing Breadcrumb component has *not* been
    modified. Only difference is that the back icon will not be displayed
    when the page is in the root component so there is only one item
    
    Additionally, the component has been refactored to cope with three
    different conditions:
    1. When it is in the compact view and there are multiple items:
      - Only show Back navigation
    2. When it is not in the compact view and there are multiple items:
      - Standard breadcrumb behaviour with navigation for the parents
    3. When there is only one item:
      - No back button is displayed

--- client/components/breadcrumb/index.tsx
+++ client/components/breadcrumb/index.tsx
@@ -3,34 +3,46 @@ import styled from '@emotion/styled';
 import { useTranslate } from 'i18n-calypso';
 import { Key } from 'react';
 
+const flexAligned = {
+	display: 'flex',
+	alignItems: 'center',
+};
+
 const StyledUl = styled.ul`
-	display: flex;
-	align-items: center;
+	${ flexAligned };
 	list-style-type: none;
 	margin: 0;
 `;
 
 const StyledLi = styled.li`
-	display: flex;
-	align-items: center;
+	${ flexAligned };
 	font-size: 13px;
-	font-weight: 500;
-	--color-link: var( --studio-gray-100 );
-
-	:first-of-type {
-		font-size: 16px;
-		font-weight: 600;
-		--color-link: var( --studio-gray-80 );
-	}
+	font-weight: 400;
+	--color-link: var( --studio-gray-50 );
 
 	:last-of-type:not( :first-of-type ) {
-		--color-link: var( --studio-gray-50 );
+		--color-link: var( --studio-gray-80 );
+		font-weight: 500;
 	}
 `;
 
+const StyledBackLink = styled.a`
+	${ flexAligned };
+	color: var( --studio-gray-80 ) !important; // It uses --studio-gray-50 if not using important
+	> svg {
+		margin-right: 5px;
+	}
+`;
+
+const StyledRootLabel = styled.span`
+	${ flexAligned };
+	font-size: 1rem;
+	font-weight: 600;
+	color: var( --cstudio-gray-80 );
+`;
+
 const StyledGridicon = styled( Gridicon )`
-	margin: 0 7px;
-	fill: var( --studio-gray-50 );
+	margin: 0 12px;
 `;
 
 interface Props {
@@ -41,33 +53,38 @@ interface Props {
 const Breadcrumb: React.FunctionComponent< Props > = ( { items, compact = false } ) => {
 	const translate = useTranslate();
 
-	return (
-		<StyledUl>
-			{ compact && items.length > 1 ? (
-				<StyledLi>
-					<StyledGridicon icon="chevron-left" size={ 18 } />
-					{ /*  Show the exactly previous page with items[ items.length - 2 ] */ }
-					<a href={ items[ items.length - 2 ].href }>{ translate( 'Back' ) }</a>
-				</StyledLi>
-			) : (
-				<>
-					{ items.map( ( item: { href?: string; label: string }, index: Key ) => {
-						return (
-							<StyledLi key={ index }>
-								{ compact && <StyledGridicon icon="chevron-left" size={ 18 } /> }
-								{ index !== 0 && <StyledGridicon icon="chevron-right" size={ 18 } /> }
-								{ item.href ? (
-									<a href={ item.href }>{ item.label }</a>
-								) : (
-									<span>{ item.label }</span>
-								) }
-							</StyledLi>
-						);
-					} ) }
-				</>
-			) }
-		</StyledUl>
-	);
+	if ( compact && items.length > 1 ) {
+		return (
+			<StyledBackLink href={ items[ items.length - 2 ].href }>
+				<Gridicon icon="chevron-left" size={ 18 } />
+				{ /*  Show the exactly previous page with items[ items.length - 2 ] */ }
+				{ translate( 'Back' ) }
+			</StyledBackLink>
+		);
+	}
+
+	if ( items.length > 1 ) {
+		return (
+			<StyledUl>
+				{ items.map( ( item: { href?: string; label: string }, index: Key ) => (
+					<StyledLi key={ index }>
+						{ index !== 0 && <StyledGridicon icon="chevron-right" size={ 14 } /> }
+						{ item.href && index !== items.length - 1 ? (
+							<a href={ item.href }>{ item.label }</a>
+						) : (
+							<span>{ item.label }</span>
+						) }
+					</StyledLi>
+				) ) }
+			</StyledUl>
+		);
+	}
+
+	if ( items.length === 1 ) {
+		return <StyledRootLabel>{ items[ 0 ].label }</StyledRootLabel>;
+	}
+
+	return null;
 };
 
 Breadcrumb.defaultProps = {
