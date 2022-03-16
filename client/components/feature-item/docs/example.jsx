cb0c82851f5457b507a23c43134e239c5b546ed6
Rolando Perez <me@rolilink.com>
2022-03-15 22:38:24 -0500

    adding FeatureCard to devdocs

--- client/components/feature-item/docs/example.jsx
+++ client/components/feature-item/docs/example.jsx
@@ -4,10 +4,12 @@ const header = 'The title of the item goes here';
 
 const FeatureItemExample = () => {
 	return (
-		<FeatureItem header={ header }>
-			{ ' ' }
-			Some content to be rendered in the item content section.{ ' ' }
-		</FeatureItem>
+		<div style={ { backgroundColor: 'var( --studio-gray-90 )', padding: '10px 20px' } }>
+			<FeatureItem header={ header }>
+				{ ' ' }
+				Some content to be rendered in the item content section.{ ' ' }
+			</FeatureItem>
+		</div>
 	);
 };
 
