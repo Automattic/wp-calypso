1c2ecfca3d94b6f7ea43db78bd3a10a4d9828e9b
Nate Weller <nate.weller@automattic.com>
2022-03-17 13:50:01 -0600

    add additional threat descriptions based on threat family/type

--- client/components/jetpack/threat-item/index.tsx
+++ client/components/jetpack/threat-item/index.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ExternalLinkWithTracking from 'calypso/components/external-link/with-tracking';
 import ThreatItemHeader from 'calypso/components/jetpack/threat-item-header';
-import { getThreatFix } from 'calypso/components/jetpack/threat-item/utils';
+import { getThreatFamily, getThreatFix } from 'calypso/components/jetpack/threat-item/utils';
 import { recordTracksEvent } from 'calypso/state/analytics/actions/record';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import LogItem from '../log-item';
@@ -39,6 +39,78 @@ const ThreatItem: React.FC< Props > = ( {
 	isFixing,
 } ) => {
 	const dispatch = useDispatch();
+
+	const getProblem = React.useCallback( () => {
+		if ( threat.description ) {
+			return threat.description;
+		}
+
+		const family = getThreatFamily( threat );
+		switch ( family ) {
+			case 'backdoor':
+				return translate(
+					'Backdoors are pieces of code that allows unauthorized users to run arbitrary code on a website. It has fewer functions than a webshell, usually just one at a time.'
+				);
+			case 'ccskimmers':
+				return translate(
+					"Credit card skimmers, or CCSkimmers for short, as the name already defines, are malicious code, usually written in JavaScript or PHP that will monitor website's visitors' requests in order to capture payment information."
+				);
+			case 'cryptominer':
+				return translate(
+					"Cryptominers are either JavaScript code that will use the website visitors's computing power, or server runnable code that will mine for cryptocurrencies. Although there are valid cases for this use, most of them are the result of a website compromise."
+				);
+			case 'dropper':
+				return translate(
+					'Droppers, as the name implies, are malicious code known to drop an arbitrary file on the affected server. This file may contain other types of malware. Usually the source path of the dropped file is hardcoded or randomly generated.'
+				);
+			case 'generic':
+				return translate(
+					'Sometimes Jetpack has enough information to pinpoint a file as malicious. It may happen due to the presence of common Indicators of Compromise for malware. However, since the final payload is unknown, the classification is generic.'
+				);
+			case 'hacktool':
+				return translate(
+					'Hacktools are uploaded to compromised sites in order to leverage other types of attacks to the server, to attack other servers, sites or the visitors. Common types of hacktools are mailers, ddos tools, rogue IRC servers and exploit kits.'
+				);
+			case 'hardening':
+				return translate(
+					'Jetpack found that your site is hosting a tool that, if misconfigured, may lead to your site to be compromised.'
+				);
+			case 'malware':
+				return translate(
+					"Sometimes the code is so heavily obfuscated that it's hard to tell what are the final intentions of it, however, Jetpack Scan team's experience allows them to pinpoint the most common indicators and alert when something is wrong."
+				);
+			case 'malvertising':
+				return translate(
+					'Malvertising is the combination of the words Malicious and Advertising, this type of malware will inject rogue ad campaigns to the website, loading unwanted banners, pop-ups, or pop-unders, and even exploit kits targeting visitors.'
+				);
+			case 'phishing':
+				return translate(
+					'Phishing scams are well crafted pages or websites made to perfectly emulate a valid service in order to collect valuable information from the victims. It may target bank accounts, popular streaming services, and even postal offices.'
+				);
+			case 'redirect':
+				return translate(
+					'Malicious code may be installed on a compromised website to redirect visitors to unwanted destinations. Those can be websites running exploit kits, phishing or fake sites, or just sites the attacker is affiliated with and will give them money based on visits.'
+				);
+			case 'seospam':
+				return translate(
+					"Compromised websites may be used by attackers to act as part of a link farm, this is done to increase a website's Search Engine rank in detriment of causing harm on the affected sites."
+				);
+			case 'suspicious':
+				return translate(
+					"Sometimes Jetpack has enough information to pinpoint a file as potentially malicious. Or at least suspicious. This may happen due to the presence of common Indicators of Compromise for malware. However, since we don't have much information to classify it on a proper type, we recommend you review the code."
+				);
+			case 'uploader':
+				return translate(
+					'Attackers may upload a specific type of hacktool to a compromised website that will allow them to maintain access. Those tools we call Uploaders will serve only one purpose: upload files to the site.'
+				);
+			case 'webshell':
+				return translate(
+					'Webshells are complex tools that will give full control over the compromised website, and sometimes the server, to the attacker. They are known to have a nice interface and many functions.'
+				);
+			default:
+				return;
+		}
+	}, [ threat ] );
 
 	/**
 	 * Render a CTA button. Currently, this button is rendered three
@@ -167,7 +239,7 @@ const ThreatItem: React.FC< Props > = ( {
 			<ThreatDescription
 				status={ threat.status }
 				fix={ getFix() }
-				problem={ threat.description }
+				problem={ getProblem() }
 				context={ threat.context }
 				diff={ threat.diff }
 				rows={ threat.rows }
