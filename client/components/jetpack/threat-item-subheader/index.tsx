57106edfb09a2dbbd108deebd6185faf8e4e5724
Nate Weller <nate.weller@automattic.com>
2022-03-17 13:49:42 -0600

    add additional threat subtitles based on threat family/type

--- client/components/jetpack/threat-item-subheader/index.tsx
+++ client/components/jetpack/threat-item-subheader/index.tsx
@@ -1,10 +1,11 @@
 import classnames from 'classnames';
-import { useTranslate } from 'i18n-calypso';
+import { translate, useTranslate } from 'i18n-calypso';
 import moment from 'moment';
 import * as React from 'react';
 import Badge from 'calypso/components/badge';
 import { Threat } from 'calypso/components/jetpack/threat-item/types';
 import {
+	getThreatFamily,
 	getThreatType,
 	getThreatVulnerability,
 } from 'calypso/components/jetpack/threat-item/utils';
@@ -24,6 +25,62 @@ const entryActionClassNames = ( threat: Threat ) => {
 
 const formatDate = ( date: Date ) => {
 	return moment( date ).format( 'LL' );
+};
+
+const getThreatSubTitleFromFamily = ( threat: Threat ) => {
+	const family = getThreatFamily( threat );
+	switch ( family ) {
+		case 'backdoor':
+			return translate( 'Backdoor found on your site. Please take immediate action.' );
+		case 'ccskimmers':
+			return translate(
+				'Jetpack found a credit card skimmer script on your website, your attention is required.'
+			);
+		case 'cryptominer':
+			return translate(
+				'Jetpack found a cryptominer script on your website, your attention is required.'
+			);
+		case 'dropper':
+			return translate(
+				'A malicious code known to upload malware to servers was found on your site. Please take immediate action.'
+			);
+		case 'generic':
+			return translate(
+				'A potentially malicious file was found on your site. Please review its code.'
+			);
+		case 'hacktool':
+			return translate( 'A hacktool was found on your site. Please take immediate action.' );
+		case 'hardening':
+			return translate(
+				'A misconfigured or a potentially vulnerable tool was found on your site. Please take immediate action.'
+			);
+		case 'malware':
+			return translate( 'A malware was found on your site. Please take immediate action.' );
+		case 'malvertising':
+			return translate(
+				'A file loading malicious ad was found on your site. Please take immediate action.'
+			);
+		case 'phishing':
+			return translate( 'A phishing page was found on your site. Please take immediate action.' );
+		case 'redirect':
+			return translate(
+				'Your website is redirecting users to unwanted destinations. Please take immediate action.'
+			);
+		case 'seospam':
+			return translate( 'SEO spam was found on your site. Please take immediate action.' );
+		case 'suspicious':
+			return translate(
+				'A suspicious code was found on your site. We recommend you review it and take proper action.'
+			);
+		case 'uploader':
+			return translate(
+				'A malicious file with upload capabilities was found on your site. Please take immediate action.'
+			);
+		case 'webshell':
+			return translate( 'A webshell was found on your site. Please take immediate action.' );
+		default:
+			return null;
+	}
 };
 
 const getThreatStatusMessage = ( translate: ReturnType< typeof useTranslate >, threat: Threat ) => {
@@ -61,6 +118,11 @@ const getThreatStatusMessage = ( translate: ReturnType< typeof useTranslate >, t
 const ThreatItemSubheader: React.FC< Props > = ( { threat } ) => {
 	const translate = useTranslate();
 	if ( threat.status === 'current' ) {
+		const threatSubTitleFromFamily = getThreatSubTitleFromFamily( threat );
+		if ( threatSubTitleFromFamily ) {
+			return <>{ threatSubTitleFromFamily }</>;
+		}
+
 		switch ( getThreatType( threat ) ) {
 			case 'file':
 				return (
