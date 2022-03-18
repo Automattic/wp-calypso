9e348ef3c580db3ad17a3e89e6b7a7bf7174ca74
Nate Weller <nate.weller@automattic.com>
2022-03-17 20:20:30 -0600

    add tests for getSignatureComponents

---
+++ client/components/jetpack/threat-item/test/utils.ts
@@ -0,0 +1,58 @@
+import { Threat } from '../types';
+import { getThreatSignatureComponents } from '../utils';
+
+describe( 'getThreatSignatureComponents', () => {
+	test( 'parses signature names containing a signature ID', () => {
+		expect(
+			getThreatSignatureComponents( {
+				signature: '(123)php_generic_backdoor_001',
+			} as Threat )
+		).toMatchObject( {
+			signature_id: '123',
+			language: 'php',
+			payload: 'generic',
+			family: 'backdoor',
+			variant: '001',
+		} );
+	} );
+
+	test( 'parses signature names without a signature ID', () => {
+		expect(
+			getThreatSignatureComponents( {
+				signature: 'php_generic_backdoor_001',
+			} as Threat )
+		).toMatchObject( {
+			signature_id: undefined,
+			language: 'php',
+			payload: 'generic',
+			family: 'backdoor',
+			variant: '001',
+		} );
+	} );
+
+	test( 'parses signature names containing a family value that includes underscores', () => {
+		expect(
+			getThreatSignatureComponents( {
+				signature: '(123)php_generic_obfuscated_fopo_001',
+			} as Threat )
+		).toMatchObject( {
+			signature_id: '123',
+			language: 'php',
+			payload: 'generic',
+			family: 'obfuscated_fopo',
+			variant: '001',
+		} );
+
+		expect(
+			getThreatSignatureComponents( {
+				signature: '(123)php_generic_foo_bar_baz_001',
+			} as Threat )
+		).toMatchObject( {
+			signature_id: '123',
+			language: 'php',
+			payload: 'generic',
+			family: 'foo_bar_baz',
+			variant: '001',
+		} );
+	} );
+} );
