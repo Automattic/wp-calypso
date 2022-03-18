25e3c964934de4bad16f3f2c31f3074cc0b86845
Tony Arcangelini <33258733+arcangelini@users.noreply.github.com>
2022-03-18 20:14:45 +0100

    Onboaring: add blogger flow (#62070)
    Co-authored-by: Emanuele Buccelli <escapemanuele@gmail.com>
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>

---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/courses/index.tsx
@@ -0,0 +1,49 @@
+import { StepContainer } from '@automattic/onboarding';
+import { useViewportMatch } from '@wordpress/compose';
+import { useTranslate } from 'i18n-calypso';
+import VideosUi from 'calypso/components/videos-ui';
+import { COURSE_SLUGS, useCourseData } from 'calypso/data/courses';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import CoursesFooter from './footer';
+import CoursesHeader from './header';
+import type { Step } from '../../types';
+import './style.scss';
+
+/**
+ * The courses step
+ */
+const CoursesStep: Step = function CoursesStep( { navigation } ) {
+	const { goBack, goNext } = navigation;
+	const translate = useTranslate();
+	const isMobile = useViewportMatch( 'small', '<' );
+	const courseSlug = COURSE_SLUGS.BLOGGING_QUICK_START;
+	const { isCourseComplete } = useCourseData( courseSlug );
+	const hideSkip = isMobile && isCourseComplete;
+
+	return (
+		<StepContainer
+			className="courses"
+			goBack={ goBack }
+			isFullLayout
+			hideFormattedHeader
+			skipLabelText={ translate( 'Draft your first post' ) }
+			nextLabelText={ translate( 'Start writing' ) }
+			hideSkip={ hideSkip }
+			hideNext={ ! hideSkip }
+			skipButtonAlign="top"
+			stepContent={
+				<VideosUi
+					courseSlug={ courseSlug }
+					areVideosTranslated={ false }
+					HeaderBar={ CoursesHeader }
+					FooterBar={ () => (
+						<CoursesFooter isCourseComplete={ isCourseComplete } onStartWriting={ goNext } />
+					) }
+				/>
+			}
+			recordTracksEvent={ recordTracksEvent }
+		/>
+	);
+};
+
+export default CoursesStep;
--- client/landing/stepper/declarative-flow/internals/steps-repository/index.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/index.ts
@@ -1,4 +1,5 @@
 export { default as domain } from './domain';
+export { default as courses } from './courses';
 export { default as design } from './design';
 export { default as intent } from './intent-step';
 export { default as options } from './site-options';
@@ -7,16 +8,20 @@ export { default as sell } from './sell';
 export { default as write } from './write';
 export { default as import } from './import';
 export { default as wpadmin } from './wpadmin';
+export { default as bloggerStartingPoint } from './blogger-starting-point';
 export { default as designSetupSite } from './design-setup-site-step';
 
 export type StepPath =
+	| 'courses'
 	| 'domain'
 	| 'design'
 	| 'intent'
 	| 'build'
 	| 'options'
 	| 'sell'
+	| 'skip-to-my-home'
 	| 'write'
 	| 'import'
 	| 'wpadmin'
+	| 'bloggerStartingPoint';
 	| 'designSetupSite';
