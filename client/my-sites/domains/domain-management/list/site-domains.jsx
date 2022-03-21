f1576012c345768f245e791fad82ea068494c79e
Igor Giussani <gius80@gmail.com>
2022-03-21 17:23:36 +0100

    Fix domain status bubble counter (#62018)

--- client/my-sites/domains/domain-management/list/site-domains.jsx
+++ client/my-sites/domains/domain-management/list/site-domains.jsx
@@ -133,6 +133,7 @@ export class SiteDomains extends Component {
 					nonWpcomDomains.map( ( domain ) =>
 						resolveDomainStatus( domain, null, {
 							getMappingErrors: true,
+							siteSlug: selectedSite.slug,
 						} )
 					)
 				),
