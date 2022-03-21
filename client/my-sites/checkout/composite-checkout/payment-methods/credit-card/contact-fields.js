a5070eb49cdfe7e2c908f5c7fc7666abbe0a9032
Payton Swick <payton@foolord.com>
2022-03-21 12:55:56 -0400

    Payment Methods: Change TaxFields to have a single onChange prop (#62090)
    * Change TaxFields to have one onChange handler
    
    * Change useCountryList to make override optional
    
    * Update all uses of TaxFields to use onChange prop

--- client/my-sites/checkout/composite-checkout/payment-methods/credit-card/contact-fields.js
+++ client/my-sites/checkout/composite-checkout/payment-methods/credit-card/contact-fields.js
@@ -13,8 +13,12 @@ export default function ContactFields( {
 } ) {
 	const { formStatus } = useFormStatus();
 	const isDisabled = formStatus !== FormStatus.READY;
-	const countriesList = useCountryList( [] );
+	const countriesList = useCountryList();
 	const fields = useSelect( ( select ) => select( 'credit-card' ).getFields() );
+	const onChangeContactInfo = ( newInfo ) => {
+		setFieldValue( 'countryCode', newInfo.countryCode?.value ?? '' );
+		setFieldValue( 'postalCode', newInfo.postalCode?.value ?? '' );
+	};
 
 	return (
 		<div className="contact-fields">
@@ -32,10 +36,9 @@ export default function ContactFields( {
 				<TaxFields
 					section="update-to-new-card"
 					taxInfo={ fields }
+					onChange={ onChangeContactInfo }
 					countriesList={ countriesList }
 					isDisabled={ isDisabled }
-					updateCountryCode={ ( value ) => setFieldValue( 'countryCode', value ) }
-					updatePostalCode={ ( value ) => setFieldValue( 'postalCode', value ) }
 				/>
 			) }
 		</div>
