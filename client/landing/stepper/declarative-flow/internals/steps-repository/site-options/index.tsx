7890520a40afc56344a52601d9e2f2f85ad961c7
Emanuele Buccelli <escapemanuele@gmail.com>
2022-03-21 14:36:24 +0100

    Duplicated property removed

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-options/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-options/index.tsx
@@ -110,7 +110,6 @@ const SiteOptions: Step = function SiteOptions( { navigation } ) {
 			goBack={ goBack }
 			goNext={ goNext }
 			isHorizontalLayout={ true }
-			skipButtonAlign="top"
 			formattedHeader={
 				<FormattedHeader id={ 'site-options-header' } headerText={ headerText } align={ 'left' } />
 			}
