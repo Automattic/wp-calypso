360d38d7c073685797df460bc7e3008722f8ef08
Emanuele Buccelli <escapemanuele@gmail.com>
2022-03-22 11:12:43 +0100

    Removed example flow

--- client/landing/stepper/index.tsx
+++ client/landing/stepper/index.tsx
@@ -9,7 +9,6 @@ import { requestAllBlogsAccess } from 'wpcom-proxy-request';
 import { LocaleContext } from '../gutenboarding/components/locale-context';
 import { WindowLocaleEffectManager } from '../gutenboarding/components/window-locale-effect-manager';
 import { setupWpDataDebug } from '../gutenboarding/devtools';
-// import { exampleFlow } from './declarative-flow/example-flow';
 import { FlowRenderer } from './declarative-flow/internals';
 import { siteSetupFlow } from './declarative-flow/site-setup-flow';
 import 'calypso/components/environment-badge/style.scss';
