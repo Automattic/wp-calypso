ff7360a9551dc4384b5d33a43720d76b69879719
Aneesh Devasthale <aneeshdevasthale@gmail.com>
2022-03-22 14:10:30 +0530

    Adds test

--- client/state/difm/assemblers.ts
+++ client/state/difm/assemblers.ts
@@ -50,8 +50,8 @@ export function createDIFMCartExtrasObject(
 		facebook_url: facebookUrl || difmState.socialProfiles.FACEBOOK,
 		linkedin_url: linkedinUrl || difmState.socialProfiles.LINKEDIN,
 		instagram_url: instagramUrl || difmState.socialProfiles.INSTAGRAM,
-		display_email: displayEmail,
-		display_phone: displayPhone,
-		display_address: displayAddress,
+		...( displayEmail && { display_email: displayEmail } ),
+		...( displayPhone && { display_phone: displayPhone } ),
+		...( displayAddress && { display_address: displayAddress } ),
 	};
 }
