53f3685c315b8eb84d31e66864a6eda4799345d0
Marin Atanasov <tyxla@abv.bg>
2022-03-23 11:13:59 +0200

    Data: Introduce support article alternates query

---
+++ client/data/support-article-alternates/use-support-article-alternates-query.js
@@ -0,0 +1,33 @@
+import { isDefaultLocale } from '@automattic/i18n-utils';
+import { useQuery } from 'react-query';
+import { useSelector } from 'react-redux';
+import wp from 'calypso/lib/wp';
+import { isPostKeyLike } from 'calypso/reader/post-key';
+import getCurrentLocaleSlug from 'calypso/state/selectors/get-current-locale-slug';
+
+export function fetchSupportArticleAlternates( blogId, postId ) {
+	return wp.req.get( `/support/alternates/${ blogId }/posts/${ postId }` );
+}
+
+function useSupportArticleAlternatesQuery( blogId, postId, queryOptions = {} ) {
+	const postKey = {
+		blogId,
+		postId,
+	};
+	const locale = useSelector( getCurrentLocaleSlug );
+
+	return useQuery(
+		[ 'support-article-alternates', blogId, postId ],
+		() => fetchSupportArticleAlternates( blogId, postId ),
+		{
+			...queryOptions,
+			enabled: ! isDefaultLocale( locale ) && !! isPostKeyLike( postKey ),
+			refetchOnWindowFocus: false,
+			select: ( data ) => {
+				return data[ locale ];
+			},
+		}
+	);
+}
+
+export default useSupportArticleAlternatesQuery;
