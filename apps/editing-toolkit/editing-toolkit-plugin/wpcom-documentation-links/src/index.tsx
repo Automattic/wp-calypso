a07a6f03ff1b606e38711fa64847c2ee47330ee5
Emanuele Buccelli <escapemanuele@gmail.com>
2022-03-23 18:51:01 +0100

    Replace Editor Help Link with WPcom

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-documentation-links/src/index.tsx
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-documentation-links/src/index.tsx
@@ -6,6 +6,8 @@ function overrideCoreDocumentationLinksToWpcom( translation: string, text: strin
 			return 'https://wordpress.com/support/excerpts/';
 		case 'https://wordpress.org/support/article/writing-posts/#post-field-descriptions':
 			return 'https://wordpress.com/support/permalinks-and-slugs/';
+		case 'https://wordpress.org/support/article/wordpress-editor/':
+			return 'https://wordpress.com/support/wordpress-editor/';
 	}
 
 	return translation;
