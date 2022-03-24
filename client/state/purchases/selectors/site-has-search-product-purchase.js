056d8b4da55aac74f57b7ba13794e45446128e26
Jasper Kang <jasper.kang@automattic.com>
2022-03-24 19:08:28 +1300

    comments

--- client/state/purchases/selectors/site-has-search-product-purchase.js
+++ client/state/purchases/selectors/site-has-search-product-purchase.js
@@ -5,11 +5,11 @@ import { getSitePurchases } from './get-site-purchases';
 import 'calypso/state/purchases/init';
 
 /**
- * Whether a site has an active Jetpack backup purchase.
+ * Whether a site has an active Jetpack Search purchase.
  *
  * @param   {object} state       global state
  * @param   {number} siteId      the site id
- * @returns {boolean} True if the site has an active Jetpack Backup purchase, false otherwise.
+ * @returns {boolean} True if the site has an active Jetpack Search purchase, false otherwise.
  */
 export const siteHasSearchProductPurchase = ( state, siteId ) => {
 	return some(
