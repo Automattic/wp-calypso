4079faee2ef70486ca8fffa6f99ed757b0606a34
mmtr <miguel.torres@automattic.com>
2022-03-25 14:05:53 +0100

    Fix tests

--- client/my-sites/sidebar/utils.js
+++ client/my-sites/sidebar/utils.js
@@ -1,19 +1,37 @@
-import { includes, some } from 'lodash';
+const pathIncludes = ( currentPath, term, position ) =>
+	currentPath.split( /[/,?]/ )?.[ position ]?.includes( term );
 
-// Check if `path` starts with `pathPrefix` and the very next character
-// (if there is any) is `/` or `?`
-function pathStartsWith( fullPath, pathPrefix ) {
-	return (
-		fullPath === pathPrefix ||
-		( fullPath.slice( 0, pathPrefix.length ) === pathPrefix &&
-			includes( '/?', fullPath[ pathPrefix.length ] ) )
-	);
-}
+const fragmentIsEqual = ( path, currentPath, position ) =>
+	currentPath.split( /[/,?]/ )?.[ position ] === path.split( /[/,?]/ )?.[ position ];
 
-export function itemLinkMatches( paths, currentPath ) {
-	if ( ! Array.isArray( paths ) ) {
-		paths = [ paths ];
+/**
+ * Checks if `currentPath` starts with the first fragment of `path`
+ *
+ * @param {string} path The path to check against.
+ * @param {string} currentPath The current path.
+ * @returns {boolean} True if paths match, false otherwise.
+ */
+export const itemLinkMatches = ( path, currentPath ) => {
+	// Accounts for jetpack custom post types, eg portfolio, testimonials.
+	if ( pathIncludes( currentPath, 'types', 1 ) ) {
+		return fragmentIsEqual( path, currentPath, 2 );
+	}
+	// Account for taxonomies, eg. tags, categories
+	if ( pathIncludes( currentPath, 'taxonomies', 2 ) ) {
+		return fragmentIsEqual( path, currentPath, 3 );
+	}
+	/*
+	 * If the menu item URL contains 'taxonomies', ignore it when on Settings screens.
+	 * Taxonomies are located under the Posts parent menu; this prevents the Posts parent
+	 * from being highlighted when Settings screens are active.
+	 */
+	if ( pathIncludes( currentPath, 'settings', 1 ) && pathIncludes( path, 'taxonomies', 2 ) ) {
+		return false;
+	}
+	// Account for rest of settings pages.
+	if ( pathIncludes( currentPath, 'settings', 1 ) ) {
+		return fragmentIsEqual( path, currentPath, 2 );
 	}
 
-	return some( paths, ( path ) => pathStartsWith( currentPath, path ) );
-}
+	return fragmentIsEqual( path, currentPath, 1 );
+};
