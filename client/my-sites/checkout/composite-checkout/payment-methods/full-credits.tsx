e30e9f1680f1c077820eb96595d55b670ccdda49
Payton Swick <payton@foolord.com>
2022-03-25 18:13:59 -0400

    Convert createFullCreditsMethod to TS

--- client/my-sites/checkout/composite-checkout/payment-methods/full-credits.tsx
+++ client/my-sites/checkout/composite-checkout/payment-methods/full-credits.tsx
@@ -5,8 +5,9 @@ import { useI18n } from '@wordpress/react-i18n';
 import { Fragment } from 'react';
 import useCartKey from 'calypso/my-sites/checkout/use-cart-key';
 import WordPressLogo from '../components/wordpress-logo';
+import type { PaymentMethod, ProcessPayment } from '@automattic/composite-checkout';
 
-export function createFullCreditsMethod() {
+export function createFullCreditsMethod(): PaymentMethod {
 	return {
 		id: 'full-credits',
 		paymentProcessorId: 'full-credits',
@@ -17,11 +18,26 @@ export function createFullCreditsMethod() {
 	};
 }
 
-function FullCreditsSubmitButton( { disabled, onClick } ) {
+function FullCreditsSubmitButton( {
+	disabled,
+	onClick,
+}: {
+	disabled?: boolean;
+	onClick?: ProcessPayment;
+} ) {
 	const [ items ] = useLineItems();
 	const cartKey = useCartKey();
 	const { responseCart } = useShoppingCart( cartKey );
 	const { formStatus } = useFormStatus();
+
+	// This must be typed as optional because it's injected by cloning the
+	// element in CheckoutSubmitButton, but the uncloned element does not have
+	// this prop yet.
+	if ( ! onClick ) {
+		throw new Error(
+			'Missing onClick prop; FullCreditsSubmitButton must be used as a payment button in CheckoutSubmitButton'
+		);
+	}
 
 	const handleButtonPress = () => {
 		onClick( {
@@ -45,16 +61,16 @@ function FullCreditsSubmitButton( { disabled, onClick } ) {
 	);
 }
 
-function ButtonContents( { formStatus, total } ) {
+function ButtonContents( { formStatus, total }: { formStatus: FormStatus; total: string } ) {
 	const { __ } = useI18n();
 	if ( formStatus === FormStatus.SUBMITTING ) {
-		return __( 'Processing…' );
+		return <>{ __( 'Processing…' ) }</>;
 	}
 	if ( formStatus === FormStatus.READY ) {
 		/* translators: %s is the total to be paid in localized currency */
-		return sprintf( __( 'Pay %s with credits' ), total );
+		return <>{ sprintf( __( 'Pay %s with credits' ), total ) }</>;
 	}
-	return __( 'Please wait…' );
+	return <>{ __( 'Please wait…' ) }</>;
 }
 
 function WordPressCreditsLabel() {
