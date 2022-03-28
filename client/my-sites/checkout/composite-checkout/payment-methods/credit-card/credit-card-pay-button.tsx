ed9957499de90b3e77e449e1dc077d6462af73d2
Payton Swick <payton@foolord.com>
2022-03-28 19:14:53 -0400

    Start converting CreditCardPayButton to TS

--- client/my-sites/checkout/composite-checkout/payment-methods/credit-card/credit-card-pay-button.tsx
+++ client/my-sites/checkout/composite-checkout/payment-methods/credit-card/credit-card-pay-button.tsx
@@ -5,6 +5,11 @@ import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import debugFactory from 'debug';
 import { validatePaymentDetails } from 'calypso/lib/checkout/validation';
+import { actions, selectors } from './store';
+import type { CardStoreType } from './types';
+import type { StripeConfiguration } from '@automattic/calypso-stripe';
+import type { ProcessPayment, LineItem } from '@automattic/composite-checkout';
+import type { Stripe } from '@stripe/stripe-js';
 
 const debug = debugFactory( 'calypso:composite-checkout:credit-card' );
 
@@ -16,6 +21,14 @@ export default function CreditCardPayButton( {
 	stripeConfiguration,
 	shouldUseEbanx,
 	activeButtonText = undefined,
+}: {
+	disabled?: boolean;
+	onClick?: ProcessPayment;
+	store: CardStoreType;
+	stripe: Stripe;
+	stripeConfiguration: StripeConfiguration;
+	shouldUseEbanx?: boolean;
+	activeButtonText?: string;
 } ) {
 	const { __ } = useI18n();
 	const [ items, total ] = useLineItems();
@@ -28,6 +41,15 @@ export default function CreditCardPayButton( {
 	const paymentPartner = shouldUseEbanx ? 'ebanx' : 'stripe';
 	const elements = useElements();
 	const cardNumberElement = elements?.getElement( CardNumberElement ) ?? undefined;
+
+	// This must be typed as optional because it's injected by cloning the
+	// element in CheckoutSubmitButton, but the uncloned element does not have
+	// this prop yet.
+	if ( ! onClick ) {
+		throw new Error(
+			'Missing onClick prop; CreditCardPayButton must be used as a payment button in CheckoutSubmitButton'
+		);
+	}
 
 	return (
 		<Button
@@ -90,40 +112,50 @@ export default function CreditCardPayButton( {
 	);
 }
 
-function ButtonContents( { formStatus, total, activeButtonText = undefined } ) {
+function ButtonContents( {
+	formStatus,
+	total,
+	activeButtonText,
+}: {
+	formStatus: FormStatus;
+	total: LineItem;
+	activeButtonText?: string;
+} ) {
 	const { __ } = useI18n();
 	if ( formStatus === FormStatus.SUBMITTING ) {
-		return __( 'Processing…' );
+		return <>{ __( 'Processing…' ) }</>;
 	}
 	if ( formStatus === FormStatus.READY ) {
 		/* translators: %s is the total to be paid in localized currency */
-		return activeButtonText || sprintf( __( 'Pay %s' ), total.amount.displayValue );
+		return <>{ activeButtonText || sprintf( __( 'Pay %s' ), total.amount.displayValue ) }</>;
 	}
-	return __( 'Please wait…' );
+	return <>{ __( 'Please wait…' ) }</>;
 }
 
-function isCreditCardFormValid( store, paymentPartner, __ ) {
+function isCreditCardFormValid(
+	store: CardStoreType,
+	paymentPartner: string,
+	__: ( value: string ) => string
+) {
 	debug( 'validating credit card fields for partner', paymentPartner );
 
 	switch ( paymentPartner ) {
 		case 'stripe': {
-			const fields = store.selectors.getFields( store.getState() );
+			const fields = selectors.getFields( store.getState() );
 			const cardholderName = fields.cardholderName;
 			if ( ! cardholderName?.value.length ) {
 				// Touch the field so it displays a validation error
-				store.dispatch( store.actions.setFieldValue( 'cardholderName', '' ) );
-				store.dispatch(
-					store.actions.setFieldError( 'cardholderName', __( 'This field is required' ) )
-				);
+				store.dispatch( actions.setFieldValue( 'cardholderName', '' ) );
+				store.dispatch( actions.setFieldError( 'cardholderName', __( 'This field is required' ) ) );
 			}
-			const errors = store.selectors.getCardDataErrors( store.getState() );
-			const incompleteFieldKeys = store.selectors.getIncompleteFieldKeys( store.getState() );
+			const errors = selectors.getCardDataErrors( store.getState() );
+			const incompleteFieldKeys = selectors.getIncompleteFieldKeys( store.getState() );
 			const areThereErrors = Object.keys( errors ).some( ( errorKey ) => errors[ errorKey ] );
 
 			if ( incompleteFieldKeys.length > 0 ) {
 				// Show "this field is required" for each incomplete field
 				incompleteFieldKeys.map( ( key ) =>
-					store.dispatch( store.actions.setCardDataError( key, __( 'This field is required' ) ) )
+					store.dispatch( actions.setCardDataError( key, __( 'This field is required' ) ) )
 				);
 			}
 			if ( areThereErrors || ! cardholderName?.value.length || incompleteFieldKeys.length > 0 ) {
@@ -134,22 +166,23 @@ function isCreditCardFormValid( store, paymentPartner, __ ) {
 
 		case 'ebanx': {
 			// Touch fields so that we show errors
-			store.dispatch( store.actions.touchAllFields() );
+			store.dispatch( actions.touchAllFields() );
 			let isValid = true;
 
-			const rawState = store.selectors.getFields( store.getState() );
+			const rawState = selectors.getFields( store.getState() );
 			const cardholderName = rawState.cardholderName;
 			const numberWithoutSpaces = {
 				value: rawState?.number?.value?.replace( /\s+/g, '' ),
 			}; // the validator package we're using requires this
+			const paymentDetailsData = {
+				...rawState,
+				country: rawState.countryCode,
+				name: cardholderName,
+				number: numberWithoutSpaces,
+			};
 			const validationResults = validatePaymentDetails(
-				Object.entries( {
-					...rawState,
-					country: rawState.countryCode,
-					name: cardholderName,
-					number: numberWithoutSpaces,
-				} ).reduce( ( accum, [ key, managedValue ] ) => {
-					accum[ key ] = managedValue?.value;
+				Object.keys( paymentDetailsData ).reduce( ( accum: Record< string, string >, key ) => {
+					accum[ key ] = paymentDetailsData[ key ]?.value;
 					return accum;
 				}, {} ),
 				'ebanx'
@@ -157,7 +190,7 @@ function isCreditCardFormValid( store, paymentPartner, __ ) {
 			Object.entries( validationResults.errors ).map( ( [ key, errors ] ) => {
 				errors.map( ( error ) => {
 					isValid = false;
-					store.dispatch( store.actions.setFieldError( key, error ) );
+					store.dispatch( actions.setFieldError( key, error ) );
 				} );
 			} );
 			debug( 'ebanx card details validation results: ', validationResults );
