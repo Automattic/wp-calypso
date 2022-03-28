c8dc0032e425b1994cc241d8e0bc05d674f4245b
elliottprogrammer <bryan@elliottprogrammer.com>
2022-03-28 18:09:15 -0400

    Fix Search loading time - refactor using UpsellSwitch.

--- client/my-sites/jetpack-search/details.tsx
+++ client/my-sites/jetpack-search/details.tsx
@@ -2,23 +2,31 @@ import { translate } from 'i18n-calypso';
 import { ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 import DocumentHead from 'calypso/components/data/document-head';
+import QuerySiteSettings from 'calypso/components/data/query-site-settings';
 import Main from 'calypso/components/main';
 import SidebarNavigation from 'calypso/components/sidebar-navigation';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
 import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
 import useTrackCallback from 'calypso/lib/jetpack/use-track-callback';
-import { getSelectedSite, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import getSiteSetting from 'calypso/state/selectors/get-site-setting';
+import {
+	getSelectedSite,
+	getSelectedSiteId,
+	getSelectedSiteSlug,
+} from 'calypso/state/ui/selectors';
 import JetpackSearchContent from './content';
 import JetpackSearchFooter from './footer';
 import JetpackSearchLogo from './logo';
 
-interface Props {
-	isSearchEnabled: boolean;
-}
+import './style.scss';
 
-export default function JetpackSearchDetails( { isSearchEnabled }: Props ): ReactElement {
+export default function JetpackSearchDetails(): ReactElement {
+	const siteId = useSelector( getSelectedSiteId ) || -1;
 	const site = useSelector( getSelectedSite );
 	const siteSlug = useSelector( getSelectedSiteSlug );
+	const isSearchEnabled = useSelector( ( state ) =>
+		getSiteSetting( state, siteId, 'jetpack_search_enabled' )
+	);
 	const isCloud = isJetpackCloud();
 	const onSettingsClick = useTrackCallback( undefined, 'calypso_jetpack_search_settings' );
 
@@ -30,6 +38,7 @@ export default function JetpackSearchDetails( { isSearchEnabled }: Props ): Reac
 
 	return (
 		<Main className="jetpack-search">
+			<QuerySiteSettings siteId={ siteId } />
 			<DocumentHead title="Jetpack Search" />
 			{ isCloud && <SidebarNavigation /> }
 			<PageViewTracker path="/jetpack-search/:site" title="Jetpack Search" />
