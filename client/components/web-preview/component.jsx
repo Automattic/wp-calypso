Automattic/wp-calypso

commit 051a33a5d84c5ba09b1297a634de9a326c220314

Stepper Flow: Blogger/Builder Flow and the first two steps of Sell (#61732)
Co-authored-by: Emanuele Buccelli <escapemanuele@gmail.com>
Co-authored-by: Omar Alshaker <omar@omaralshaker.com>
Co-authored-by: Tony Arcangelini <33258733+arcangelini@users.noreply.github.com>
Co-authored-by: Tony Arcangelini <tony@arcangelini.com>

client/components/web-preview/component.jsx
@@ -3,8 +3,8 @@ import classNames from 'classnames';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
+import AsyncLoad from 'calypso/components/async-load';
 import { hasTouch } from 'calypso/lib/touch-detect';
-import WebPreviewContent from './content';
 
 import './style.scss';
 
@@ -57,6 +57,8 @@ export class WebPreviewModal extends Component {
 		frontPageMetaDescription: PropTypes.string,
 		// A post object used to override the selected post in the SEO preview
 		overridePost: PropTypes.object,
+		// Whether WebPreviewContent should be connected to the Calypso Redux store.
+		shouldConnectContent: PropTypes.bool,
 	};
 
 	static defaultProps = {
@@ -73,6 +75,7 @@ export class WebPreviewModal extends Component {
 		onEdit: noop,
 		hasSidebar: false,
 		overridePost: null,
+		shouldConnectContent: true,
 	};
 
 	constructor( props ) {
@@ -142,12 +145,25 @@ export class WebPreviewModal extends Component {
 					<div className="web-preview__backdrop" onClick={ this.props.onClose } />
 					{ /* eslint-enable jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */ }
 					<div className="web-preview__content">
-						<WebPreviewContent
-							{ ...this.props }
-							onDeviceUpdate={ this.setDeviceViewport }
-							isModalWindow={ true }
-							frontPageMetaDescription={ this.props.frontPageMetaDescription || null }
-						/>
+						{ this.props.shouldConnectContent ? (
+							<AsyncLoad
+								{ ...this.props }
+								onDeviceUpdate={ this.setDeviceViewport }
+								isModalWindow={ true }
+								frontPageMetaDescription={ this.props.frontPageMetaDescription || null }
+								require="calypso/components/web-preview/connectedContent"
+								placeholder={ null }
+							></AsyncLoad>
+						) : (
+							<AsyncLoad
+								{ ...this.props }
+								onDeviceUpdate={ this.setDeviceViewport }
+								isModalWindow={ true }
+								frontPageMetaDescription={ this.props.frontPageMetaDescription || null }
+								require="calypso/components/web-preview/content"
+								placeholder={ null }
+							></AsyncLoad>
+						) }
 					</div>
 				</div>
 			</RootChild>
@@ -158,6 +174,21 @@ export class WebPreviewModal extends Component {
 const LocalizedWebPreviewModal = localize( WebPreviewModal );
 
 const WebPreviewInner = ( { isContentOnly, ...restProps } ) => {
+	const WebPreviewContent = ( props ) =>
+		restProps.shouldConnectContent ? (
+			<AsyncLoad
+				{ ...props }
+				placeholder={ null }
+				require="calypso/components/web-preview/connectedContent"
+			></AsyncLoad>
+		) : (
+			<AsyncLoad
+				{ ...props }
+				placeholder={ null }
+				require="calypso/components/web-preview/content"
+			></AsyncLoad>
+		);
+
 	const WebPreviewComponent = isContentOnly ? WebPreviewContent : LocalizedWebPreviewModal;
 
 	return <WebPreviewComponent { ...restProps } />;

client/components/web-preview/connectedContent.jsx
@@ -1,0 +1,18 @@
+import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import isPrivateSite from 'calypso/state/selectors/is-private-site';
+import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
+import { getSelectedSite } from 'calypso/state/ui/selectors';
+import getSelectedSiteId from 'calypso/state/ui/selectors/get-selected-site-id';
+import WebPreviewContent from './content';
+
+const mapState = ( state ) => {
+	const siteId = getSelectedSiteId( state );
+	return {
+		isPrivateAtomic: isSiteAutomatedTransfer( state, siteId ) && isPrivateSite( state, siteId ),
+		url: getSelectedSite( state )?.URL,
+	};
+};
+
+export default connect( mapState, { recordTracksEvent } )( localize( WebPreviewContent ) );

client/components/web-preview/content.jsx
@@ -1,27 +1,20 @@
 import { isWithinBreakpoint } from '@automattic/viewport';
 import classNames from 'classnames';
 import debugModule from 'debug';
-import { localize } from 'i18n-calypso';
 import page from 'page';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
-import { connect } from 'react-redux';
 import { v4 as uuid } from 'uuid';
 import SeoPreviewPane from 'calypso/components/seo-preview-pane';
 import SpinnerLine from 'calypso/components/spinner-line';
 import { addQueryArgs } from 'calypso/lib/route';
 import { hasTouch } from 'calypso/lib/touch-detect';
-import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import isPrivateSite from 'calypso/state/selectors/is-private-site';
-import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
-import { getSelectedSite } from 'calypso/state/ui/selectors';
-import getSelectedSiteId from 'calypso/state/ui/selectors/get-selected-site-id';
 import Toolbar from './toolbar';
 
 const debug = debugModule( 'calypso:web-preview' );
 const noop = () => {};
 
-export class WebPreviewContent extends Component {
+export default class WebPreviewContent extends Component {
 	previewId = uuid();
 
 	state = {
@@ -408,13 +401,3 @@ WebPreviewContent.defaultProps = {
 	overridePost: null,
 	toolbarComponent: Toolbar,
 };
-
-const mapState = ( state ) => {
-	const siteId = getSelectedSiteId( state );
-	return {
-		isPrivateAtomic: isSiteAutomatedTransfer( state, siteId ) && isPrivateSite( state, siteId ),
-		url: getSelectedSite( state )?.URL,
-	};
-};
-
-export default connect( mapState, { recordTracksEvent } )( localize( WebPreviewContent ) );

client/landing/stepper/declarative-flow/example-flow.ts
@@ -1,22 +1,0 @@
-import type { StepPath } from './internals/steps-repository';
-import type { Flow } from './internals/types';
-
-export const exampleFlow: Flow = {
-	useSteps(): Array< StepPath > {
-		return [ 'domain', 'design' ];
-	},
-	useStepNavigation( currentStep, navigate ) {
-		const goBack = () => {
-			if ( currentStep === 'domain' ) {
-				navigate( 'design' );
-			} else {
-				navigate( 'domain' );
-			}
-		};
-		const goNext = goBack;
-		const goToStep = ( slug: StepPath ) => {
-			navigate( slug );
-		};
-		return { goNext, goBack, goToStep };
-	},
-};

client/landing/stepper/declarative-flow/internals/analytics/record-submit-step.ts
@@ -1,0 +1,41 @@
+import { resolveDeviceTypeByViewPort } from '@automattic/viewport';
+import { reduce, snakeCase } from 'lodash';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { ProvidedDependencies } from '../types';
+
+export function recordSubmitStep(
+	providedDependencies: ProvidedDependencies = {},
+	intent: string,
+	step: string
+) {
+	const device = resolveDeviceTypeByViewPort();
+	const inputs = reduce(
+		providedDependencies,
+		( props, propValue, propName: string ) => {
+			propName = snakeCase( propName );
+
+			// Ensure we don't capture identifiable user data we don't need.
+			if ( propName === 'email' ) {
+				propName = `user_entered_${ propName }`;
+				propValue = !! propValue;
+			}
+
+			if ( propName === 'selected_design' ) {
+				propValue = ( propValue as { slug: string } ).slug;
+			}
+
+			return {
+				...props,
+				[ propName ]: propValue,
+			};
+		},
+		{}
+	);
+
+	recordTracksEvent( 'calypso_signup_actions_submit_step', {
+		device,
+		step,
+		intent,
+		...inputs,
+	} );
+}

client/landing/stepper/declarative-flow/internals/global.scss
@@ -1,7 +1,115 @@
 /*
     Global styles for Stepper framework
 */
-body {
+@import '@wordpress/components/build-style/style';
+@import '@automattic/calypso-color-schemes';
+@import '@automattic/typography/styles/fonts';
+@import '@automattic/onboarding/styles/mixins';
+
+/**
+ * General onboarding styling
+ */
+body,
+button {
 	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen-Sans', 'Ubuntu',
 		'Cantarell', 'Helvetica Neue', sans-serif;
+	text-rendering: optimizeLegibility;
+	-moz-osx-font-smoothing: grayscale;
+	-webkit-font-smoothing: antialiased;
+
+	// Basic stylings
+	margin: 0;
+	line-height: 1.5;
+
+	h1,
+	h2,
+	h3,
+	p {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen-Sans', 'Ubuntu',
+			'Cantarell', 'Helvetica Neue', sans-serif;
+		font-weight: 400;
+		margin: 0;
+	}
+
+	a,
+	a:visited {
+		text-decoration: none;
+		color: var( --color-link );
+	}
+
+	button {
+		background-color: transparent;
+		border: none;
+	}
+
+	ul {
+		padding: 0;
+	}
+}
+
+/**
+ * Site Setup
+ */
+.site-setup {
+	position: relative;
+	padding: 48px 0 0;
+	box-sizing: border-box;
+
+	&.courses {
+		padding: 0;
+
+		.signup-header {
+			z-index: 1;
+			color: #fff;
+
+			svg path {
+				fill: #fff;
+				stroke: #fff;
+			}
+		}
+	}
+
+	/**
+ 	 * Signup Header
+ 	 */
+	.signup-header {
+		.wordpress-logo {
+			position: absolute;
+			left: 20px;
+			top: 20px;
+			fill: var( --color-text );
+			stroke: var( --color-text );
+			transform-origin: 0 0;
+		}
+
+		.signup-header__right {
+			top: 22px;
+			right: 20px;
+
+			.flow-progress-indicator {
+				font-weight: 500;
+				/* stylelint-disable-line */
+				font-size: 0.875rem;
+				color: var( --studio-gray-30 );
+			}
+		}
+
+		@include break-small {
+			.wordpress-logo {
+				left: 24px;
+			}
+
+			.signup-header__right {
+				right: 24px;
+			}
+		}
+	}
+
+	/**
+ 	 * Step Container
+ 	 */
+	.step-container {
+		--color-accent: #117ac9;
+		--color-accent-60: #0e64a5;
+	}
 }

client/landing/stepper/declarative-flow/internals/index.tsx
@@ -1,4 +1,6 @@
+import classnames from 'classnames';
 import { Switch, Route, Redirect, generatePath, useHistory, useLocation } from 'react-router-dom';
+import SignupHeader from 'calypso/signup/signup-header';
 import * as Steps from './steps-repository';
 import type { StepPath } from './steps-repository';
 import type { Flow } from './types';
@@ -17,11 +19,15 @@ import './global.scss';
  */
 export const FlowRenderer: React.FC< { flow: Flow } > = ( { flow } ) => {
 	const stepPaths = flow.useSteps();
-	const currentRoute = useLocation().pathname.substring( 1 ) as StepPath;
+	const location = useLocation();
+	const currentRoute = location.pathname.substring( 1 ) as StepPath;
 	const history = useHistory();
+	const { search } = useLocation();
 	const stepNavigation = flow.useStepNavigation( currentRoute, ( path: StepPath ) =>
-		history.push( generatePath( path ), stepPaths )
+		history.push( generatePath( path + search ), stepPaths )
 	);
+	const pathToClass = ( path: string ) =>
+		path.replace( /([a-z0-9])([A-Z])/g, '$1-$2' ).toLowerCase();
 
 	return (
 		<Switch>
@@ -29,12 +35,15 @@ export const FlowRenderer: React.FC< { flow: Flow } > = ( { flow } ) => {
 				const StepComponent = Steps[ path ];
 				return (
 					<Route key={ path } path={ `/${ path }` }>
-						<StepComponent navigation={ stepNavigation } />
+						<div className={ classnames( flow.name, flow.classnames, pathToClass( path ) ) }>
+							<SignupHeader />
+							<StepComponent navigation={ stepNavigation } />
+						</div>
 					</Route>
 				);
 			} ) }
 			<Route>
-				<Redirect to={ stepPaths[ 0 ] } />
+				<Redirect to={ stepPaths[ 0 ] + search } />
 			</Route>
 		</Switch>
 	);

client/landing/stepper/declarative-flow/internals/steps-repository/blogger-starting-point/index.tsx
@@ -1,0 +1,60 @@
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+import { IntentScreen, StepContainer } from '@automattic/onboarding';
+import { useTranslate } from 'i18n-calypso';
+import startingPointImageUrl from 'calypso/assets/images/onboarding/starting-point.svg';
+import FormattedHeader from 'calypso/components/formatted-header';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { preventWidows } from 'calypso/lib/formatting';
+import useStartingPoints from './starting-points';
+import type { Step } from '../../types';
+import './style.scss';
+
+/**
+ * The starting point step
+ */
+const StartingPointStep: Step = function StartingPointStep( { navigation } ) {
+	const { goBack, submit } = navigation;
+	const translate = useTranslate();
+	const headerText = translate( 'Nice job! Now it’s{{br}}{{/br}} time to get creative.', {
+		components: { br: <br /> },
+	} );
+	const subHeaderText = translate( "Don't worry. You can come back to these steps!" );
+	const intents = useStartingPoints();
+
+	const submitIntent = ( startingPoint: string ) => {
+		const providedDependencies = { startingPoint };
+		recordTracksEvent( 'calypso_signup_starting_point_select', { starting_point: startingPoint } );
+		submit?.( providedDependencies, startingPoint );
+	};
+
+	return (
+		<StepContainer
+			stepName={ 'blogger-starting-point' }
+			headerImageUrl={ startingPointImageUrl }
+			goBack={ goBack }
+			skipLabelText={ translate( 'Skip to My Home' ) }
+			goNext={ () => submitIntent( 'skip-to-my-home' ) }
+			skipButtonAlign={ 'top' }
+			isHorizontalLayout={ true }
+			formattedHeader={
+				<FormattedHeader
+					id={ 'intent-header' }
+					headerText={ headerText }
+					subHeaderText={ subHeaderText }
+					align={ 'left' }
+				/>
+			}
+			stepContent={
+				<IntentScreen
+					intents={ intents }
+					intentsAlt={ [] }
+					onSelect={ submitIntent }
+					preventWidows={ preventWidows }
+				/>
+			}
+			recordTracksEvent={ recordTracksEvent }
+		/>
+	);
+};
+
+export default StartingPointStep;

commit 102ee6000dc9b59d93a03dc3a9bab8d4bd4d714f

Fix webpreview component broken in #61732 (#62345)

client/components/web-preview/component.jsx
@@ -173,9 +173,9 @@ export class WebPreviewModal extends Component {
 
 const LocalizedWebPreviewModal = localize( WebPreviewModal );
 
-const WebPreviewInner = ( { isContentOnly, ...restProps } ) => {
+const WebPreviewInner = ( { isContentOnly, shouldConnectContent = true, ...restProps } ) => {
 	const WebPreviewContent = ( props ) =>
-		restProps.shouldConnectContent ? (
+		shouldConnectContent ? (
 			<AsyncLoad
 				{ ...props }
 				placeholder={ null }