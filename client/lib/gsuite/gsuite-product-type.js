f858b85ae30c6abeb6c2f838a47caf7c93feefbc
Stéphane Thomas <email@stephanethomas.com>
2022-03-29 16:26:43 +0200

    G Suite: Simplify code from getProductType() function

--- client/lib/gsuite/gsuite-product-type.js
+++ client/lib/gsuite/gsuite-product-type.js
@@ -1,7 +1,4 @@
-import {
-	GOOGLE_WORKSPACE_BUSINESS_STARTER_MONTHLY,
-	GOOGLE_WORKSPACE_BUSINESS_STARTER_YEARLY,
-} from '@automattic/calypso-products';
+import { isGoogleWorkspaceProductSlug } from '@automattic/calypso-products';
 import { GOOGLE_WORKSPACE_PRODUCT_TYPE, GSUITE_PRODUCT_TYPE } from 'calypso/lib/gsuite/constants';
 
 /**
@@ -12,12 +9,7 @@ import { GOOGLE_WORKSPACE_PRODUCT_TYPE, GSUITE_PRODUCT_TYPE } from 'calypso/lib/
  * @see emailManagementAddGSuiteUsers() in client/my-sites/email/paths.js
  */
 export function getProductType( productSlug ) {
-	if (
-		[
-			GOOGLE_WORKSPACE_BUSINESS_STARTER_MONTHLY,
-			GOOGLE_WORKSPACE_BUSINESS_STARTER_YEARLY,
-		].includes( productSlug )
-	) {
+	if ( isGoogleWorkspaceProductSlug( productSlug ) ) {
 		return GOOGLE_WORKSPACE_PRODUCT_TYPE;
 	}
 
