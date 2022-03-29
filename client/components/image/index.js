2b47ee156b207984ad476d6bbfc509a59b6c4bbe
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2022-03-29 12:37:01 +0300

    Components: Refactor Image away from UNSAFE_ methods (#62103)

--- client/components/image/index.js
+++ client/components/image/index.js
@@ -1,37 +1,24 @@
 import classnames from 'classnames';
-import { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import './style.scss';
+const hideImageOnError = ( event ) => {
+	event.target.style.display = 'none';
+};
 
-export default class Image extends Component {
-	static defaultProps = {
-		className: '',
-	};
-	// by default, we give the image a shot at loading
-	state = {
-		isError: false,
-	};
-
-	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
-	UNSAFE_componentWillReceiveProps( nextProps ) {
-		// reset the error state if we switch images
-		// TODO: support srcsets?
-		if ( nextProps.src !== this.props.src ) {
-			this.setState( { isError: false } );
-		}
-	}
-
-	handleError = () => {
-		this.setState( { isError: true } );
-	};
-
-	render() {
-		const { className, ...others } = this.props;
-		const allClasses = classnames( className, {
-			image: true,
-			'is-error': this.state.isError,
-		} );
-		// eslint-disable-next-line jsx-a11y/alt-text
-		return <img onError={ this.handleError } className={ allClasses } { ...others } />;
-	}
+export default function Image( { alt, className, src, ...restProps } ) {
+	return (
+		<img
+			src={ src }
+			onError={ hideImageOnError }
+			className={ classnames( className, 'image' ) }
+			alt={ alt }
+			{ ...restProps }
+		/>
+	);
 }
+
+Image.propTypes = {
+	alt: PropTypes.string,
+	className: PropTypes.string,
+	src: PropTypes.string,
+};
