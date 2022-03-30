9e370b14198022982206788b68a3f4b8c4d4446a
Edwin Takahashi <egao@outlook.com>
2022-03-30 10:51:27 +0900

    Check for '1'

--- packages/calypso-e2e/src/jest-playwright-config/setup.ts
+++ packages/calypso-e2e/src/jest-playwright-config/setup.ts
@@ -1,7 +1,7 @@
 import { jest } from '@jest/globals';
 import config from 'config';
 
-if ( process.env.PWDEBUG ) {
+if ( process.env.PWDEBUG === '1' ) {
 	// If the user intends to debug the suite, extend the default timeout.
 	jest.setTimeout( ( config.get( 'jestTimeoutMS' ) as number ) * 1000 );
 } else {
