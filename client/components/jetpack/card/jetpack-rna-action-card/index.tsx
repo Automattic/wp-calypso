413d2085b3db640181557b3f0bbf5f444d9de27c
elliottprogrammer <bryan@elliottprogrammer.com>
2022-03-30 10:20:14 -0400

    Refactor UpsellProductCard to use the reusable JetpackRnaActionCard component.

--- client/components/jetpack/card/jetpack-rna-action-card/index.tsx
+++ client/components/jetpack/card/jetpack-rna-action-card/index.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@automattic/components';
+import { Button, Card } from '@automattic/components';
 import classNames from 'classnames';
-import { useTranslate, TranslateResult } from 'i18n-calypso';
+import { TranslateResult } from 'i18n-calypso';
 import { ReactNode } from 'react';
 import UpsellBackgroundImage from 'calypso/assets/images/jetpack/rna-card-bg.png';
-import DefaultImage from 'calypso/assets/images/jetpack/rna-image-default.png';
+import DefaultImage from 'calypso/assets/images/jetpack/rna-image-Default.png';
 
 import './style.scss';
 
@@ -15,7 +15,7 @@ interface RnaActionCardProps {
 	ctaButtonURL?: string;
 	ctaButtonLabel: TranslateResult;
 	cardImage?: string;
-	cardImageAlt?: string;
+	cardImageAlt?: string | TranslateResult;
 	isPlaceholder?: boolean;
 }
 
@@ -30,29 +30,28 @@ const JetpackRnaActionCard: React.FC< RnaActionCardProps > = ( {
 	cardImageAlt,
 	isPlaceholder,
 } ) => {
-	const translate = useTranslate();
 	return (
-		<div
-			className={ classNames( 'jetpack-rna-action-card', {
+		<Card
+			className={ classNames( 'jetpack-RNA-action-card', {
 				'is-placeholder': isPlaceholder,
 			} ) }
 			{ ...( ! isPlaceholder && {
 				style: { backgroundImage: `url(${ UpsellBackgroundImage })` },
 			} ) }
 		>
-			<div className="jetpack-rna-action-card__body">
-				<div className="jetpack-rna-action-card__content">
-					<h2 className="jetpack-rna-action-card__header">{ headerText }</h2>
+			<div className="jetpack-RNA-action-card__body">
+				<div className="jetpack-RNA-action-card__content">
+					<h2 className="jetpack-RNA-action-card__header">{ headerText }</h2>
 					{ subHeaderText && (
-						<p className="jetpack-rna-action-card__description">{ subHeaderText }</p>
+						<p className="jetpack-RNA-action-card__description">{ subHeaderText }</p>
 					) }
 
 					{ children && children }
 				</div>
-				<div className="jetpack-rna-action-card__action">
+				<div className="jetpack-RNA-action-card__action">
 					<Button
 						primary
-						className="jetpack-rna-action-card__button"
+						className="jetpack-RNA-action-card__button"
 						onClick={ onCtaButtonClick && onCtaButtonClick }
 						href={ ctaButtonURL ? ctaButtonURL : '#' }
 						disabled={ ! ctaButtonURL }
@@ -61,19 +60,16 @@ const JetpackRnaActionCard: React.FC< RnaActionCardProps > = ( {
 					</Button>
 				</div>
 			</div>
-			<div className="jetpack-rna-action-card__footer">
-				<div className="jetpack-rna-action-card__footer-image">
+			<div className="jetpack-RNA-action-card__footer">
+				<div className="jetpack-RNA-action-card__footer-image">
 					{ isPlaceholder ? (
-						<div className="jetpack-rna-action-card__placeholder-image"></div>
+						<div className="jetpack-RNA-action-card__placeholder-image"></div>
 					) : (
-						<img
-							src={ cardImage }
-							alt={ cardImageAlt ? cardImageAlt : translate( 'Jetpack', { textOnly: true } ) }
-						/>
+						<img src={ cardImage } alt={ ( cardImageAlt ? cardImageAlt : 'Jetpack ' ) as string } />
 					) }
 				</div>
 			</div>
-		</div>
+		</Card>
 	);
 };
 
