9404eda8234b6d6660e0baa40729871b700536d6
Andrii Lysenko <lysenkoa.work@gmail.com>
2022-04-01 18:36:37 +0300

    update: fix validation of backURL

--- client/my-sites/checkout/composite-checkout/hooks/use-valid-checkout-back-url.ts
+++ client/my-sites/checkout/composite-checkout/hooks/use-valid-checkout-back-url.ts
@@ -1,7 +1,18 @@
+import { getLanguageSlugs } from '@automattic/i18n-utils';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { resemblesUrl } from 'calypso/lib/url';
 import getInitialQueryArguments from 'calypso/state/selectors/get-initial-query-arguments';
+
+const getAllowedHosts = ( siteSlug: string ) => {
+	const basicHosts = [ 'jetpack.com', 'jetpack.cloud.localhost', 'cloud.jetpack.com', siteSlug ];
+
+	const languageSpecificJetpackHosts = getLanguageSlugs().map(
+		( lang: string ) => `${ lang }.jetpack.com`
+	);
+
+	return basicHosts.concat( languageSpecificJetpackHosts );
+};
 
 const useValidCheckoutBackUrl = ( siteSlug: string | undefined ): string | undefined => {
 	const { checkoutBackUrl } = useSelector( getInitialQueryArguments ) ?? {};
@@ -11,12 +22,7 @@ const useValidCheckoutBackUrl = ( siteSlug: string | undefined ): string | undef
 			return undefined;
 		}
 
-		const allowedHosts = [
-			'jetpack.com',
-			'jetpack.cloud.localhost',
-			'cloud.jetpack.com',
-			siteSlug,
-		];
+		const allowedHosts = getAllowedHosts( siteSlug );
 
 		let parsedUrl;
 		try {
