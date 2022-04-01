e99e330c28a87258aaf7a23fc70d88fccf498837
David Rothstein <david.rothstein@automattic.com>
2022-04-01 17:05:51 -0400

    Fix TypeScript error (@props to sirbrillig)

--- client/state/selectors/is-legacy-site-with-higher-limits.ts
+++ client/state/selectors/is-legacy-site-with-higher-limits.ts
@@ -23,7 +23,7 @@ export default function isLegacySiteWithHigherLimits( state: AppState, siteId: n
 		return false;
 	}
 
-	const siteOptions = getSiteOptions( state, siteId );
+	const siteOptions = getSiteOptions( state, siteId ) as undefined | { created_at: string };
 	const createdAt = siteOptions?.created_at ?? '';
 
 	if ( ! createdAt ) {
