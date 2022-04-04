7ec702be499870c5ddfc930ea70adefe6c8fe655
Niranjan Uma Shankar <niranjan.u@gmail.com>
2022-04-04 13:40:07 +0400

    Remove stray throw

--- client/components/olark-chat/index.jsx
+++ client/components/olark-chat/index.jsx
@@ -10,7 +10,7 @@ class OlarkChat extends Component {
 		script.setAttribute( 'async', true );
 		script.innerHTML = `
 			;(function(o,l,a,r,k,y){if(o.olark)return; r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0]; y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r); y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)}; y.extend=function(i,j){y("extend",i,j)}; y.identify=function(i){y("identify",k.i=i)}; y.configure=function(i,j){y("configure",i,j);k.c[i]=j}; k=y._={s:[],t:[+new Date],c:{},l:a}; })(window,document,"static.olark.com/jsclient/loader.js");
-			olark.identify('${ identity }'); throw 'NIRANJAN';
+			olark.identify('${ identity }');
 		`;
 
 		if ( shouldDisablePreChatSurvey ) {
