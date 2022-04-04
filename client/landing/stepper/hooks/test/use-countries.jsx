addd2c1695c928eed765b28524473057c0b051ca
Mark Biek <mark.biek@automattic.com>
2022-04-04 11:15:44 -0400

    Added hook to load Woo countries from the API

---
+++ client/landing/stepper/hooks/test/use-countries.jsx
@@ -0,0 +1,36 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { renderHook } from '@testing-library/react-hooks';
+import nock from 'nock';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useCountries } from '../use-countries';
+
+describe( 'use-countries hook', () => {
+	test( 'returns list of countries from the api', async () => {
+		const queryClient = new QueryClient();
+		const wrapper = ( { children } ) => (
+			<QueryClientProvider client={ queryClient }>{ children }</QueryClientProvider>
+		);
+
+		const expected = {
+			'AL:AL-01': 'Albania — Berat',
+			'AL:AL-09': 'Albania — Dibër',
+			'AL:AL-02': 'Albania — Durrës',
+			'AL:AL-03': 'Albania — Elbasan',
+		};
+
+		nock( 'https://public-api.wordpress.com' )
+			.get( '/wpcom/v2/woocommerce/countries/regions/' )
+			.reply( 200, expected );
+
+		const { result, waitFor } = renderHook( () => useCountries(), {
+			wrapper,
+		} );
+
+		await waitFor( () => result.current.isSuccess );
+
+		expect( result.current.data ).toEqual( expected );
+	} );
+} );
