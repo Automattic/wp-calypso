19197c66cdb64ef860964d84f4be07ad5f526869
Andres Blanco <andresblanco@gmail.com>
2022-04-05 15:57:59 -0300

    added feature check on stepper

--- client/landing/stepper/declarative-flow/internals/steps-repository/store-features/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/store-features/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
+import { FEATURE_SIMPLE_PAYMENTS, FEATURE_WOOP } from '@automattic/calypso-products';
 import { IntentScreen, StepContainer } from '@automattic/onboarding';
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { useTranslate } from 'i18n-calypso';
 import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
@@ -8,7 +9,7 @@ import { preventWidows } from 'calypso/lib/formatting';
 import { useIntents } from 'calypso/signup/steps/store-features/intents';
 import { useSite } from '../../../../hooks/use-site';
 import { useSiteSlugParam } from '../../../../hooks/use-site-slug-param';
-import { ONBOARD_STORE } from '../../../../stores';
+import { ONBOARD_STORE, SITE_STORE } from '../../../../stores';
 import type { Step } from '../../types';
 import './style.scss';
 
@@ -30,7 +31,14 @@ const StoreFeatures: Step = function StartingPointStep( { navigation } ) {
 	const subHeaderText = translate( 'Let’s create a website that suits your needs.' );
 	const siteSlug = useSiteSlugParam();
 	const site = useSite();
-	const intents = useIntents( siteSlug, site?.plan?.product_slug, trackSupportLinkClick );
+	const hasPaymentsFeature = useSelect(
+		( select ) =>
+			site && select( SITE_STORE ).hasActiveSiteFeature( site?.ID, FEATURE_SIMPLE_PAYMENTS )
+	);
+	const hasWooFeature = useSelect(
+		( select ) => site && select( SITE_STORE ).hasActiveSiteFeature( site?.ID, FEATURE_WOOP )
+	);
+	const intents = useIntents( siteSlug, hasPaymentsFeature, hasWooFeature, trackSupportLinkClick );
 	const { setStoreType } = useDispatch( ONBOARD_STORE );
 
 	const submitIntent = ( storeType: string ) => {
