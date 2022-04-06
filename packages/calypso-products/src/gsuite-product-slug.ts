1086d3cf20b405e945887f134f29adca8eaccfd4
Stéphane Thomas <email@stephanethomas.com>
2022-04-06 18:08:24 +0200

    G Suite: Move the isGSuiteOrExtraLicenseProductSlug() selector to its own file

--- packages/calypso-products/src/gsuite-product-slug.ts
+++ packages/calypso-products/src/gsuite-product-slug.ts
@@ -1,16 +1,5 @@
 import { isGoogleWorkspaceProductSlug } from './is-google-workspace-product-slug';
-import { isGSuiteExtraLicenseProductSlug } from './is-gsuite-extra-license-product-slug';
 import { isGSuiteProductSlug } from './is-gsuite-product-slug';
-
-/**
- * Determines whether the specified product slug refers to any type of G Suite product.
- *
- * @param {string} productSlug - slug of the product
- * @returns {boolean} true if the slug refers to any G Suite product, false otherwise
- */
-export function isGSuiteOrExtraLicenseProductSlug( productSlug: string ): boolean {
-	return isGSuiteProductSlug( productSlug ) || isGSuiteExtraLicenseProductSlug( productSlug );
-}
 
 /**
  * Determines whether the specified product slug refers to either G Suite or Google Workspace.
