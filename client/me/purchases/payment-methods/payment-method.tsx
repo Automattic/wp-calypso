dacbe2ff4a53a2a78bb5e32a1470cbbc1b0a4afc
Payton Swick <payton@foolord.com>
2022-04-06 18:08:01 -0400

    Remove unused card prop from PaymentMethod

--- client/me/purchases/payment-methods/payment-method.tsx
+++ client/me/purchases/payment-methods/payment-method.tsx
@@ -2,25 +2,17 @@ import { CompactCard } from '@automattic/components';
 import classNames from 'classnames';
 import { FunctionComponent } from 'react';
 import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
-import PaymentMethodDetails from './payment-method-details';
-import type { PaymentMethod as PaymentMethodType } from 'calypso/lib/checkout/payment-methods';
 
 import 'calypso/me/purchases/payment-methods/style.scss';
 
-interface Props {
-	card?: PaymentMethodType;
-}
-
-const PaymentMethod: FunctionComponent< Props > = ( { card, children } ) => {
+const PaymentMethod: FunctionComponent = ( { children } ) => {
 	return (
 		<CompactCard
 			className={ classNames( 'payment-method__wrapper', {
 				'is-jetpack-cloud': isJetpackCloud(),
 			} ) }
 		>
-			<div className="payment-method">
-				{ card ? <PaymentMethodDetails { ...card } /> : children }
-			</div>
+			<div className="payment-method">{ children }</div>
 		</CompactCard>
 	);
 };
