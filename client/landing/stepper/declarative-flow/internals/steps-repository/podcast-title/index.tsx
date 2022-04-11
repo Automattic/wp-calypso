05bbd40bc234818c56e6e20fb8aa41d562700041
sixhours <calobee@gmail.com>
2022-04-11 17:49:32 -0400

    Begin setting up UI for Anchor.fm podcast title step

--- client/landing/stepper/declarative-flow/internals/steps-repository/podcast-title/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/podcast-title/index.tsx
@@ -1,16 +1,48 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import { Button } from '@automattic/components';
 import { StepContainer } from '@automattic/onboarding';
+import { Icon } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
-import FormattedHeader from 'calypso/components/formatted-header';
+import FormLabel from 'calypso/components/forms/form-label';
+import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
+import FormInput from 'calypso/components/forms/form-text-input';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { tip } from 'calypso/signup/icons';
 import type { Step } from '../../types';
+
+import './style.scss';
+
+interface Props {
+	podcastTitle?: string;
+}
 
 const PodcastTitleStep: Step = function PodcastTitleStep( { navigation } ) {
 	const { goBack, submit } = navigation;
 	const translate = useTranslate();
-	const headerText = translate( 'My podcast is called' );
-	const subHeaderText = translate( "Don't worry, you can change it later." );
+
+	const PodcastTitleForm: React.FC< Props > = ( { podcastTitle } ) => {
+		return (
+			<form className="podcast-title__form" onSubmit={ goNext }>
+				<div className="podcast-title__input-wrapper">
+					<FormLabel htmlFor="podcast-title">{ translate( 'My podcast is called' ) }</FormLabel>
+					<div className="podcast-title__explanation">
+						<FormInput id="podcast-title" value={ podcastTitle } />
+						<div
+							className="podcast-title__underline"
+							style={ { width: podcastTitle?.length || '100%' } }
+						/>
+						<FormSettingExplanation>
+							<Icon className="podcast-title__form-icon" icon={ tip } size={ 20 } />
+							{ translate( "Don't worry, you can change it later." ) }
+						</FormSettingExplanation>
+					</div>
+				</div>
+				<Button className="podcast-title__submit-button" type="submit" primary>
+					{ translate( 'Continue' ) }
+				</Button>
+			</form>
+		);
+	};
 
 	const handleClick = () => {
 		submit?.();
@@ -20,18 +52,9 @@ const PodcastTitleStep: Step = function PodcastTitleStep( { navigation } ) {
 		<StepContainer
 			stepName={ 'podcast-title-step' }
 			goBack={ goBack }
-			hideSkip
 			hideBack
-			isHorizontalLayout={ true }
-			formattedHeader={
-				<FormattedHeader
-					id={ 'podcast-title-header' }
-					headerText={ headerText }
-					subHeaderText={ subHeaderText }
-					align={ 'left' }
-				/>
-			}
-			stepContent={ <Button onClick={ handleClick }>Go to next step</Button> }
+			isFullLayout
+			stepContent={ <PodcastTitleForm /> }
 			recordTracksEvent={ recordTracksEvent }
 		/>
 	);
