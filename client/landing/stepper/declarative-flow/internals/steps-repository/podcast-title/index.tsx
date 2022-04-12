918c7954fc1b5c068a47c54045455fbdcbbc4a47
sixhours <calobee@gmail.com>
2022-04-12 10:50:02 -0400

    Swap i18n libraries Calypso -> core

--- client/landing/stepper/declarative-flow/internals/steps-repository/podcast-title/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/podcast-title/index.tsx
@@ -2,8 +2,8 @@
 import { Button } from '@automattic/components';
 import { StepContainer } from '@automattic/onboarding';
 import { Icon } from '@wordpress/icons';
+import { useI18n } from '@wordpress/react-i18n';
 import classNames from 'classnames';
-import { useTranslate } from 'i18n-calypso';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
 import FormInput from 'calypso/components/forms/form-text-input';
@@ -19,13 +19,17 @@ interface Props {
 
 const PodcastTitleStep: Step = function PodcastTitleStep( { navigation } ) {
 	const { goBack, submit } = navigation;
-	const translate = useTranslate();
+	const { __ } = useI18n();
+
+	const handleSubmit = () => {
+		submit?.();
+	};
 
 	const PodcastTitleForm: React.FC< Props > = ( { podcastTitle } ) => {
 		return (
 			<form className="podcast-title__form" onSubmit={ goNext }>
 				<div className="podcast-title__input-wrapper">
-					<FormLabel htmlFor="podcast-title">{ translate( 'My podcast is called' ) }</FormLabel>
+					<FormLabel htmlFor="podcast-title">{ __( 'My podcast is called' ) }</FormLabel>
 					<div className="podcast-title__explanation">
 						<FormInput id="podcast-title" value={ podcastTitle } />
 						<div
@@ -36,12 +40,12 @@ const PodcastTitleStep: Step = function PodcastTitleStep( { navigation } ) {
 						/>
 						<FormSettingExplanation>
 							<Icon className="podcast-title__form-icon" icon={ tip } size={ 20 } />
-							{ translate( "Don't worry, you can change it later." ) }
+							{ __( "Don't worry, you can change it later." ) }
 						</FormSettingExplanation>
 					</div>
 				</div>
 				<Button className="podcast-title__submit-button" type="submit" primary>
-					{ translate( 'Continue' ) }
+					{ __( 'Continue' ) }
 				</Button>
 			</form>
 		);
