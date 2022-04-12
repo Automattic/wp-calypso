6a69350ba9221d5f6d2f19c3b96b6c044cafc043
sixhours <calobee@gmail.com>
2022-04-12 13:11:49 -0400

    Add underline and tip fade-in, start cleaning up mobile styles

--- client/landing/stepper/declarative-flow/internals/steps-repository/podcast-title/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/podcast-title/index.tsx
@@ -4,39 +4,55 @@ import { StepContainer } from '@automattic/onboarding';
 import { Icon } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 import classNames from 'classnames';
+import { useState, useRef } from 'react';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
 import FormInput from 'calypso/components/forms/form-text-input';
+import getTextWidth from 'calypso/landing/gutenboarding/onboarding-block/acquire-intent/get-text-width';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { tip } from 'calypso/signup/icons';
 import type { Step } from '../../types';
 
 import './style.scss';
 
-interface Props {
-	podcastTitle?: string;
-}
-
 const PodcastTitleStep: Step = function PodcastTitleStep( { navigation } ) {
 	const { goBack, submit } = navigation;
 	const { __ } = useI18n();
 
-	const handleSubmit = () => {
-		submit?.();
-	};
+	const PodcastTitleForm: React.FC = () => {
+		const [ podcastTitle, setPodcastTitle ] = useState( '' );
+		const [ formTouched, setFormTouched ] = useState( false );
+		const [ underlineWidth, setUnderlineWidth ] = useState( 0 );
+		const inputRef = useRef() as React.MutableRefObject< HTMLInputElement >;
 
-	const PodcastTitleForm: React.FC< Props > = ( { podcastTitle } ) => {
+		const handleSubmit = () => {
+			submit?.();
+		};
+
+		const handleChange = ( event: React.FormEvent< HTMLInputElement > ) => {
+			setFormTouched( true );
+			const underlineWidth = getTextWidth( podcastTitle as string, inputRef.current );
+			setUnderlineWidth( underlineWidth );
+			return setPodcastTitle( event.currentTarget.value );
+		};
 		return (
-			<form className="podcast-title__form" onSubmit={ goNext }>
-				<div className="podcast-title__input-wrapper">
+			<form className="podcast-title__form" onSubmit={ handleSubmit }>
+				<div
+					className={ classNames( 'podcast-title__input-wrapper', { 'is-touched': formTouched } ) }
+				>
 					<FormLabel htmlFor="podcast-title">{ __( 'My podcast is called' ) }</FormLabel>
 					<div className="podcast-title__explanation">
-						<FormInput id="podcast-title" value={ podcastTitle } />
+						<FormInput
+							id="podcast-title"
+							inputRef={ inputRef }
+							value={ podcastTitle }
+							onChange={ handleChange }
+						/>
 						<div
 							className={ classNames( 'podcast-title__underline', {
-								'is-empty': ! podcastTitle?.length,
+								'is-empty': ! ( podcastTitle as string )?.length,
 							} ) }
-							style={ { width: ( podcastTitle as string )?.length || '100%' } }
+							style={ { width: underlineWidth } }
 						/>
 						<FormSettingExplanation>
 							<Icon className="podcast-title__form-icon" icon={ tip } size={ 20 } />
