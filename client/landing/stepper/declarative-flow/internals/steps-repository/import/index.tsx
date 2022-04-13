9f9685f8ccc098d1a14e95516bc871552a53a32e
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-04-13 11:15:58 +0200

    Setup main redux store in order to consume onboarding import module

--- client/landing/stepper/declarative-flow/internals/steps-repository/import/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/import/index.tsx
@@ -1,10 +1,41 @@
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+import { StepContainer } from '@automattic/onboarding';
+import { useSite } from 'calypso/landing/stepper/hooks/use-site';
+import { useSiteSlugParam } from 'calypso/landing/stepper/hooks/use-site-slug-param';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import ImportOnboarding from 'calypso/signup/steps/import';
+import { GoToStep } from 'calypso/signup/steps/import/types';
 import type { Step } from '../../types';
 
 /**
  * The import step
  */
-const ImportStep: Step = function ImportStep() {
-	return <div>Import step</div>;
+const ImportStep: Step = function ImportStep( props ) {
+	const { navigation } = props;
+
+	const site = useSite();
+	const siteSlug = useSiteSlugParam();
+
+	return (
+		<StepContainer
+			stepName={ 'import-step' }
+			hideSkip={ false }
+			hideBack={ true }
+			hideNext={ true }
+			isHorizontalLayout={ false }
+			stepContent={
+				<ImportOnboarding
+					stepName={ 'capture' }
+					siteId={ site?.ID as number }
+					stepSectionName={ '' }
+					signupDependencies={ { siteSlug: siteSlug as string } }
+					goToStep={ navigation.goToStep as GoToStep }
+					goToNextStep={ navigation.goNext }
+				/>
+			}
+			recordTracksEvent={ recordTracksEvent }
+		/>
+	);
 };
 
 export default ImportStep;
--- client/landing/stepper/index.tsx
+++ client/landing/stepper/index.tsx
@@ -1,11 +1,18 @@
 import '@automattic/calypso-polyfills';
 import accessibleFocus from '@automattic/accessible-focus';
 import { initializeAnalytics } from '@automattic/calypso-analytics';
+import { CurrentUser } from '@automattic/calypso-analytics/dist/types/utils/current-user';
 import config from '@automattic/calypso-config';
 import ReactDom from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { requestAllBlogsAccess } from 'wpcom-proxy-request';
+import { initializeCurrentUser } from 'calypso/lib/user/shared-utils';
+import { createReduxStore } from 'calypso/state';
+import { getInitialState, getStateFromCache } from 'calypso/state/initial-state';
+import initialReducer from 'calypso/state/reducer';
+import { setStore } from 'calypso/state/redux-store';
 import { LocaleContext } from '../gutenboarding/components/locale-context';
 import { WindowLocaleEffectManager } from '../gutenboarding/components/window-locale-effect-manager';
 import { setupWpDataDebug } from '../gutenboarding/devtools';
@@ -45,6 +52,13 @@ window.AppBoot = async () => {
 
 	const queryClient = new QueryClient();
 
+	const user = ( await initializeCurrentUser() ) as unknown;
+	const userId = ( user as CurrentUser ).ID;
+
+	const initialState = getInitialState( initialReducer, userId );
+	const reduxStore = createReduxStore( initialState, initialReducer );
+	setStore( reduxStore, getStateFromCache( userId ) );
+
 	let flow = siteSetupFlow;
 
 	const anchorPodcastId = getAnchorPodcastId();
@@ -55,12 +69,14 @@ window.AppBoot = async () => {
 
 	ReactDom.render(
 		<LocaleContext>
-			<QueryClientProvider client={ queryClient }>
-				<WindowLocaleEffectManager />
-				<BrowserRouter basename="setup">
-					<FlowRenderer flow={ flow } />
-				</BrowserRouter>
-			</QueryClientProvider>
+			<Provider store={ reduxStore }>
+				<QueryClientProvider client={ queryClient }>
+					<WindowLocaleEffectManager />
+					<BrowserRouter basename="setup">
+						<FlowRenderer flow={ flow } />
+					</BrowserRouter>
+				</QueryClientProvider>
+			</Provider>
 		</LocaleContext>,
 		document.getElementById( 'wpcom' )
 	);
