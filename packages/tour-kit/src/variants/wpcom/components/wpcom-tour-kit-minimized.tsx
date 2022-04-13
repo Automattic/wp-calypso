afd826dad26ef65c602caf5e6a553f714fed4bf8
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2022-04-13 17:17:11 +0300

    @automattic/tour-kit: Fix missing text domain from translate calls (#62659)

--- packages/tour-kit/src/variants/wpcom/components/wpcom-tour-kit-minimized.tsx
+++ packages/tour-kit/src/variants/wpcom/components/wpcom-tour-kit-minimized.tsx
@@ -1,7 +1,8 @@
 import { Button, Flex } from '@wordpress/components';
 import { createInterpolateElement } from '@wordpress/element';
-import { __, sprintf } from '@wordpress/i18n';
+import { sprintf } from '@wordpress/i18n';
 import { Icon, close } from '@wordpress/icons';
+import { useI18n } from '@wordpress/react-i18n';
 import maximize from '../icons/maximize';
 import type { MinimizedTourRendererProps } from '../../../types';
 
@@ -11,19 +12,20 @@ const WpcomTourKitMinimized: React.FunctionComponent< MinimizedTourRendererProps
 	onDismiss,
 	currentStepIndex,
 } ) => {
+	const { __ } = useI18n();
 	const lastStepIndex = steps.length - 1;
 	const page = currentStepIndex + 1;
 	const numberOfPages = lastStepIndex + 1;
 
 	return (
 		<Flex gap={ 0 } className="wpcom-tour-kit-minimized">
-			<Button onClick={ onMaximize } aria-label={ __( 'Resume Tour' ) }>
+			<Button onClick={ onMaximize } aria-label={ __( 'Resume Tour', __i18n_text_domain__ ) }>
 				<Flex gap={ 13 }>
 					<p>
 						{ createInterpolateElement(
 							sprintf(
 								/* translators: 1: current page number, 2: total number of pages */
-								__( 'Resume tour <span>(%1$d/%2$d)</span>' ),
+								__( 'Resume tour <span>(%1$d/%2$d)</span>', __i18n_text_domain__ ),
 								page,
 								numberOfPages
 							),
@@ -35,7 +37,10 @@ const WpcomTourKitMinimized: React.FunctionComponent< MinimizedTourRendererProps
 					<Icon icon={ maximize } size={ 24 } />
 				</Flex>
 			</Button>
-			<Button onClick={ onDismiss( 'close-btn-minimized' ) } aria-label={ __( 'Close Tour' ) }>
+			<Button
+				onClick={ onDismiss( 'close-btn-minimized' ) }
+				aria-label={ __( 'Close Tour', __i18n_text_domain__ ) }
+			>
 				<Icon icon={ close } size={ 24 } />
 			</Button>
 		</Flex>
