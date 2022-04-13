eba638d62fd7ea4cede12d1f6ede6a169471a557
Payton Swick <payton@foolord.com>
2022-04-13 15:56:56 -0400

    Checkout: Do not update cart contact details on auto-refresh (#62572)

--- client/my-sites/checkout/composite-checkout/hooks/use-cached-domain-contact-details.ts
+++ client/my-sites/checkout/composite-checkout/hooks/use-cached-domain-contact-details.ts
@@ -7,13 +7,17 @@ import { useSelector, useDispatch as useReduxDispatch } from 'react-redux';
 import useCartKey from 'calypso/my-sites/checkout/use-cart-key';
 import { requestContactDetailsCache } from 'calypso/state/domains/management/actions';
 import getContactDetailsCache from 'calypso/state/selectors/get-contact-details-cache';
-import type { CountryListItem } from '@automattic/wpcom-checkout';
+import type {
+	PossiblyCompleteDomainContactDetails,
+	CountryListItem,
+} from '@automattic/wpcom-checkout';
 
 const debug = debugFactory( 'calypso:composite-checkout:use-cached-domain-contact-details' );
 
 export default function useCachedDomainContactDetails( countriesList: CountryListItem[] ): void {
 	const reduxDispatch = useReduxDispatch();
 	const haveRequestedCachedDetails = useRef( false );
+	const previousCachedContactDetails = useRef< PossiblyCompleteDomainContactDetails >();
 	const cartKey = useCartKey();
 	const {
 		updateLocation: updateCartLocation,
@@ -52,20 +56,29 @@ export default function useCachedDomainContactDetails( countriesList: CountryLis
 	// When we have fetched or loaded contact details, send them to the
 	// to the shopping cart for calculating taxes.
 	useEffect( () => {
-		if ( isLoadingCart || cartLoadingError ) {
+		if ( isLoadingCart || cartLoadingError || ! cachedContactDetails ) {
 			return;
 		}
 		if (
-			cachedContactDetails?.countryCode ||
-			cachedContactDetails?.postalCode ||
-			cachedContactDetails?.state
+			! cachedContactDetails.countryCode &&
+			! cachedContactDetails.postalCode &&
+			! cachedContactDetails.state
 		) {
-			updateCartLocation( {
-				countryCode: cachedContactDetails.countryCode ?? '',
-				postalCode: arePostalCodesSupported ? cachedContactDetails.postalCode ?? '' : '',
-				subdivisionCode: cachedContactDetails.state ?? '',
-			} );
+			return;
 		}
+		if (
+			cachedContactDetails.countryCode === previousCachedContactDetails.current?.countryCode &&
+			cachedContactDetails.postalCode === previousCachedContactDetails.current?.postalCode &&
+			cachedContactDetails.state === previousCachedContactDetails.current?.state
+		) {
+			return;
+		}
+		updateCartLocation( {
+			countryCode: cachedContactDetails.countryCode ?? '',
+			postalCode: arePostalCodesSupported ? cachedContactDetails.postalCode ?? '' : '',
+			subdivisionCode: cachedContactDetails.state ?? '',
+		} );
+		previousCachedContactDetails.current = cachedContactDetails;
 	}, [
 		cartLoadingError,
 		isLoadingCart,
