1272e139f7f039e9e7d09a41443d95a1a401ba04
Nelson <nelsonec87@gmail.com>
2022-04-13 15:03:32 -0300

    persist podcast title

--- client/landing/stepper/declarative-flow/internals/steps-repository/podcast-title/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/podcast-title/index.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import { Button } from '@automattic/components';
 import { StepContainer } from '@automattic/onboarding';
+import { TextControl } from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { useTranslate } from 'i18n-calypso';
 import FormattedHeader from 'calypso/components/formatted-header';
+import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import type { Step } from '../../types';
 
@@ -11,6 +14,8 @@ const PodcastTitleStep: Step = function PodcastTitleStep( { navigation } ) {
 	const translate = useTranslate();
 	const headerText = translate( 'My podcast is called' );
 	const subHeaderText = translate( "Don't worry, you can change it later." );
+	const { siteTitle } = useSelect( ( select ) => select( ONBOARD_STORE ).getState() );
+	const { setSiteTitle } = useDispatch( ONBOARD_STORE );
 
 	const handleClick = () => {
 		submit?.();
@@ -31,7 +36,12 @@ const PodcastTitleStep: Step = function PodcastTitleStep( { navigation } ) {
 					align={ 'left' }
 				/>
 			}
-			stepContent={ <Button onClick={ handleClick }>Go to next step</Button> }
+			stepContent={
+				<>
+					<TextControl onChange={ setSiteTitle } value={ siteTitle } />
+					<Button onClick={ handleClick }>Continue</Button>
+				</>
+			}
 			recordTracksEvent={ recordTracksEvent }
 		/>
 	);
