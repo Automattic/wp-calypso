c37f236d848c85d8763e91d703db20b3350f8b20
Claudiu Filip <claudiucelfilip@gmail.com>
2022-04-15 11:13:33 +0300

    Nudge premium and personal plan users to the Pro checkout page

--- client/blocks/upsell-nudge/index.jsx
+++ client/blocks/upsell-nudge/index.jsx
@@ -19,7 +19,7 @@ import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
 import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
 import isSiteWPForTeams from 'calypso/state/selectors/is-site-wpforteams';
 import isVipSite from 'calypso/state/selectors/is-vip-site';
-import { hasFeature } from 'calypso/state/sites/plans/selectors';
+import { getCurrentPlan, hasFeature } from 'calypso/state/sites/plans/selectors';
 import { getSite, isJetpackSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
@@ -33,6 +33,7 @@ export const UpsellNudge = ( {
 	canUserUpgrade,
 	className,
 	compact,
+	currentPlan,
 	customerType,
 	description,
 	disableHref,
@@ -90,9 +91,16 @@ export const UpsellNudge = ( {
 	}
 
 	if ( ! href && siteSlug && canUserUpgrade ) {
-		href = addQueryArgs( { feature, plan }, `/plans/${ siteSlug }` );
-		if ( customerType ) {
-			href = `/plans/${ siteSlug }?customerType=${ customerType }`;
+		const currentPlanSlug = currentPlan?.productSlug;
+
+		// Redirect to the checkout page if the user is on a Premium or Personal plan since the can't view the overhauled Plans page.
+		if ( isPremiumPlan( currentPlanSlug ) || isPersonalPlan( currentPlanSlug ) ) {
+			href = `/checkout/${ siteSlug }/pro`;
+		} else {
+			href = addQueryArgs( { feature, plan }, `/plans/${ siteSlug }` );
+			if ( customerType ) {
+				href = `/plans/${ siteSlug }?customerType=${ customerType }`;
+			}
 		}
 	}
 
@@ -164,6 +172,7 @@ export default connect( ( state, ownProps ) => {
 		isJetpack: isJetpackSite( state, siteId ),
 		isAtomic: isSiteAutomatedTransfer( state, siteId ),
 		isVip: isVipSite( state, siteId ),
+		currentPlan: getCurrentPlan( state, siteId ),
 		siteSlug: ownProps.disableHref ? null : getSelectedSiteSlug( state ),
 		canUserUpgrade: canCurrentUser( state, getSelectedSiteId( state ), 'manage_options' ),
 		siteIsWPForTeams: isSiteWPForTeams( state, getSelectedSiteId( state ) ),
