5284e5912b2499b906b2212d80b6a7ade327a28d
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2022-04-15 14:46:21 -0500

    Add redirct if feature flag isn't enabled

--- client/jetpack-cloud/sections/agency-dashboard/controller.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/controller.tsx
@@ -1,4 +1,12 @@
+import config from '@automattic/calypso-config';
+import page from 'page';
+
 export function agencyDashboardContext( context: PageJS.Context, next: () => void ): void {
+	if ( ! config.isEnabled( 'jetpack/agency-dashboard' ) ) {
+		page.redirect( '/' );
+		next();
+	}
+
 	context.primary = <div>Agency dashboard placeholder</div>;
 	next();
 }
