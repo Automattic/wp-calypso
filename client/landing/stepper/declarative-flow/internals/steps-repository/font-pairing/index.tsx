31d56d09e69a60faee882bc660466d1d03c2b209
Valter Lorran <valterlorran@hotmail.com>
2022-04-15 11:26:51 -0300

    Add font loading on the "font pairing step" page

--- client/landing/stepper/declarative-flow/internals/steps-repository/font-pairing/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/font-pairing/index.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
+import { FontPair, getFontTitle } from '@automattic/design-picker';
 import { StepContainer } from '@automattic/onboarding';
+import React from 'react';
+import { useFontPairings } from 'calypso/landing/gutenboarding/fonts';
 import StylePreview from 'calypso/landing/gutenboarding/onboarding-block/style-preview';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import type { Step } from '../../types';
@@ -7,6 +10,32 @@ import './style.scss';
 
 const FontPairingStep: Step = function FontPairingStep( { navigation } ) {
 	const { goBack, submit } = navigation;
+
+	const effectiveFontPairings = useFontPairings();
+	// TODO: Explore alternatives for loading fonts and optimizations
+	// TODO: Don't load like this
+	React.useEffect( () => {
+		effectiveFontPairings.forEach( ( { base, headings }: FontPair ) => {
+			const linkBase = document.createElement( 'link' );
+			const linkHeadings = document.createElement( 'link' );
+
+			linkBase.href = `https://fonts.googleapis.com/css2?family=${ encodeURI(
+				base
+			) }&text=${ encodeURI( getFontTitle( base ) ) }&display=swap`;
+			linkHeadings.href = `https://fonts.googleapis.com/css2?family=${ encodeURI(
+				headings
+			) }:wght@700&text=${ encodeURI( getFontTitle( headings ) ) }&display=swap`;
+
+			linkBase.rel = 'stylesheet';
+			linkHeadings.rel = 'stylesheet';
+
+			linkBase.type = 'text/css';
+			linkHeadings.type = 'text/css';
+
+			document.head.appendChild( linkBase );
+			document.head.appendChild( linkHeadings );
+		} );
+	}, [] ); // eslint-disable-line react-hooks/exhaustive-deps
 
 	return (
 		<StepContainer
