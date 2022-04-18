818a23107c89b1a7882b8abe4c51b2d8aef62998
Olaseni Oluwunmi <277661+olaseni@users.noreply.github.com>
2022-04-18 21:13:31 +0100

    Freeze

---
+++ client/my-sites/email/email-providers-stacked-comparison/email-provider-card.tsx
@@ -0,0 +1,17 @@
+/*
+import type { ReactElement } from 'react';
+import { EmailProvidersStackedCardProps } from 'calypso/my-sites/email/email-providers-stacked-comparison/provider-cards/provider-card-props';
+
+
+const EmailProviderCard = ( {
+	comparisonContext,
+	detailsExpanded,
+	intervalLength,
+	isDomainInCart = false,
+	onExpandedChange,
+	selectedDomainName,
+	source,
+}: EmailProvidersStackedCardProps ): ReactElement => {
+	return <></>;
+};
+*/
