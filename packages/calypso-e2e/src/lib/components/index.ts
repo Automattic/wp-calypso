e6c2dd1a2c68794d06ebaa41c6c1be175e81a3f2
Dan Speckhard Pasque <dan.speckhard.pasque@automattic.com>
2022-04-19 10:24:10 -0500

    Playwright: Editor Tracking -- Add global styles event tracking tests (#62834)

--- packages/calypso-e2e/src/lib/components/index.ts
+++ packages/calypso-e2e/src/lib/components/index.ts
@@ -24,5 +24,9 @@ export * from './editor-block-list-view-component';
 export * from './editor-sidebar-block-inserter-component';
 export * from './editor-welcome-tour-component';
 export * from './editor-popover-menu-component';
+export * from './editor-site-styles-component';
+export * from './editor-color-picker-component';
+export * from './editor-typography-component';
+export * from './full-site-editor-save-panel-component';
 
 export * from './me';
