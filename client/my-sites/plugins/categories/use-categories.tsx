e4f1f18c732c27a9ec5c86b8c832b9ad51b4be2d
Louis Laugesen <louis.laugesen@gmail.com>
2022-04-19 19:36:13 +1000

    Enable more categories

--- client/my-sites/plugins/categories/use-categories.tsx
+++ client/my-sites/plugins/categories/use-categories.tsx
@@ -5,7 +5,27 @@ import { isJetpackSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import type { Category } from '.';
 
-export const allowedCategories = [ 'discover', 'paid', 'popular', 'featured' ];
+export const allowedCategories = [
+	'discover',
+	'paid',
+	'popular',
+	'featured',
+	'analytics',
+	'business',
+	'customer',
+	'design',
+	'ecommerce',
+	'education',
+	'finance',
+	'marketing',
+	'seo',
+	'photo',
+	'social',
+	'widgets',
+	'email',
+	'security',
+	'posts',
+];
 
 export function useCategories(): Record< string, Category > {
 	const { __ } = useI18n();
