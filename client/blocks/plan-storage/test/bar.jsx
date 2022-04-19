0c28dca2112060e2d1d8c8252a013f674e4427c6
Marin Atanasov <tyxla@abv.bg>
2022-04-19 15:50:30 +0300

    enzyme -> @testing-library/react

--- client/blocks/plan-storage/test/bar.jsx
+++ client/blocks/plan-storage/test/bar.jsx
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const translate = ( x ) => x;
 
 import {
@@ -11,8 +15,9 @@ import {
 	PLAN_PERSONAL_2_YEARS,
 	PLAN_FREE,
 } from '@automattic/calypso-products';
+import { render, screen } from '@testing-library/react';
 import { assert } from 'chai';
-import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
 import { PlanStorageBar } from '../bar';
 
 describe( 'PlanStorageBar basic tests', () => {
@@ -27,84 +32,107 @@ describe( 'PlanStorageBar basic tests', () => {
 	};
 
 	test( 'should not blow up and have class .plan-storage-bar', () => {
-		const bar = shallow( <PlanStorageBar { ...props } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container } = render( <PlanStorageBar { ...props } /> );
+		assert.lengthOf( container.getElementsByClassName( 'plan-storage__bar' ), 1 );
 	} );
 
 	test( 'should render ProgressBar', () => {
-		const bar = shallow( <PlanStorageBar { ...props } /> );
-		const progressBar = bar.find( 'ProgressBar' );
-		assert.lengthOf( progressBar, 1 );
-		assert.equal( progressBar.props().value, 10 );
+		render( <PlanStorageBar { ...props } /> );
+		const progressBar = screen.queryByRole( 'progressbar' );
+		assert.isDefined( progressBar );
+		expect( progressBar ).toHaveAttribute( 'aria-valuenow', '10' );
 	} );
 
 	test( 'should render when storage is limited', () => {
-		let bar;
+		const { container: premContainer } = render( <PlanStorageBar { ...props } /> );
+		render( <PlanStorageBar { ...props } sitePlanSlug={ PLAN_PREMIUM } /> );
+		assert.lengthOf( premContainer.getElementsByClassName( 'plan-storage__bar' ), 1 );
 
-		bar = shallow( <PlanStorageBar { ...props } sitePlanSlug={ PLAN_PREMIUM } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container: prem2Container } = render(
+			<PlanStorageBar { ...props } sitePlanSlug={ PLAN_PREMIUM_2_YEARS } />
+		);
+		assert.lengthOf( prem2Container.getElementsByClassName( 'plan-storage__bar' ), 1 );
 
-		bar = shallow( <PlanStorageBar { ...props } sitePlanSlug={ PLAN_PREMIUM_2_YEARS } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container: persContailer } = render(
+			<PlanStorageBar { ...props } sitePlanSlug={ PLAN_PERSONAL } />
+		);
+		assert.lengthOf( persContailer.getElementsByClassName( 'plan-storage__bar' ), 1 );
 
-		bar = shallow( <PlanStorageBar { ...props } sitePlanSlug={ PLAN_PERSONAL } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container: pers2Container } = render(
+			<PlanStorageBar { ...props } sitePlanSlug={ PLAN_PERSONAL_2_YEARS } />
+		);
+		assert.lengthOf( pers2Container.getElementsByClassName( 'plan-storage__bar' ), 1 );
 
-		bar = shallow( <PlanStorageBar { ...props } sitePlanSlug={ PLAN_PERSONAL_2_YEARS } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container: freeContainer } = render(
+			<PlanStorageBar { ...props } sitePlanSlug={ PLAN_FREE } />
+		);
+		assert.lengthOf( freeContainer.getElementsByClassName( 'plan-storage__bar' ), 1 );
 
-		bar = shallow( <PlanStorageBar { ...props } sitePlanSlug={ PLAN_FREE } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container: busContainer } = render(
+			<PlanStorageBar { ...props } sitePlanSlug={ PLAN_BUSINESS } />
+		);
+		assert.lengthOf( busContainer.getElementsByClassName( 'plan-storage__bar' ), 1 );
 
-		bar = shallow( <PlanStorageBar { ...props } sitePlanSlug={ PLAN_BUSINESS } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container: bus2Container } = render(
+			<PlanStorageBar { ...props } sitePlanSlug={ PLAN_BUSINESS_2_YEARS } />
+		);
+		assert.lengthOf( bus2Container.getElementsByClassName( 'plan-storage__bar' ), 1 );
 
-		bar = shallow( <PlanStorageBar { ...props } sitePlanSlug={ PLAN_BUSINESS_2_YEARS } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container: ecomContainer } = render(
+			<PlanStorageBar { ...props } sitePlanSlug={ PLAN_ECOMMERCE } />
+		);
+		assert.lengthOf( ecomContainer.getElementsByClassName( 'plan-storage__bar' ), 1 );
 
-		bar = shallow( <PlanStorageBar { ...props } sitePlanSlug={ PLAN_ECOMMERCE } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
-
-		bar = shallow( <PlanStorageBar { ...props } sitePlanSlug={ PLAN_ECOMMERCE_2_YEARS } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container: ecom2Container } = render(
+			<PlanStorageBar { ...props } sitePlanSlug={ PLAN_ECOMMERCE_2_YEARS } />
+		);
+		assert.lengthOf( ecom2Container.getElementsByClassName( 'plan-storage__bar' ), 1 );
 	} );
 
 	test( 'should not render when storage has valid max_storage_bytes', () => {
-		let bar;
+		const { container: storage1 } = render(
+			<PlanStorageBar { ...props } mediaStorage={ { max_storage_bytes: 1 } } />
+		);
+		assert.lengthOf( storage1.getElementsByClassName( 'plan-storage__bar' ), 1 );
 
-		bar = shallow( <PlanStorageBar { ...props } mediaStorage={ { max_storage_bytes: 1 } } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container: storage0 } = render(
+			<PlanStorageBar { ...props } mediaStorage={ { max_storage_bytes: 0 } } />
+		);
+		assert.lengthOf( storage0.getElementsByClassName( 'plan-storage__bar' ), 1 );
 
-		bar = shallow( <PlanStorageBar { ...props } mediaStorage={ { max_storage_bytes: 0 } } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
-
-		bar = shallow( <PlanStorageBar { ...props } mediaStorage={ { max_storage_bytes: 50 } } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 1 );
+		const { container: storage50 } = render(
+			<PlanStorageBar { ...props } mediaStorage={ { max_storage_bytes: 50 } } />
+		);
+		assert.lengthOf( storage50.getElementsByClassName( 'plan-storage__bar' ), 1 );
 	} );
 
 	test( 'should not render when storage is falsey or -1', () => {
-		let bar;
+		const { container: storage0 } = render( <PlanStorageBar { ...props } mediaStorage={ 0 } /> );
+		assert.lengthOf( storage0.getElementsByClassName( 'plan-storage__bar' ), 0 );
 
-		bar = shallow( <PlanStorageBar { ...props } mediaStorage={ 0 } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 0 );
+		const { container: storageFalse } = render(
+			<PlanStorageBar { ...props } mediaStorage={ false } />
+		);
+		assert.lengthOf( storageFalse.getElementsByClassName( 'plan-storage__bar' ), 0 );
 
-		bar = shallow( <PlanStorageBar { ...props } mediaStorage={ false } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 0 );
+		const { container: storageNull } = render(
+			<PlanStorageBar { ...props } mediaStorage={ null } />
+		);
+		assert.lengthOf( storageNull.getElementsByClassName( 'plan-storage__bar' ), 0 );
 
-		bar = shallow( <PlanStorageBar { ...props } mediaStorage={ null } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 0 );
-
-		bar = shallow( <PlanStorageBar { ...props } mediaStorage={ { max_storage_bytes: -1 } } /> );
-		assert.lengthOf( bar.find( '.plan-storage__bar' ), 0 );
+		const { container: storageUnlimited } = render(
+			<PlanStorageBar { ...props } mediaStorage={ { max_storage_bytes: -1 } } />
+		);
+		assert.lengthOf( storageUnlimited.getElementsByClassName( 'plan-storage__bar' ), 0 );
 	} );
 
 	test( 'should include upgrade link when displayUpgradeLink is true', () => {
-		const bar = shallow( <PlanStorageBar { ...props } displayUpgradeLink={ true } /> );
-		assert.lengthOf( bar.find( '.plan-storage__storage-link' ), 1 );
+		const { container } = render( <PlanStorageBar { ...props } displayUpgradeLink={ true } /> );
+		assert.lengthOf( container.getElementsByClassName( 'plan-storage__storage-link' ), 1 );
 	} );
 
 	test( 'should not include upgrade link when displayUpgradeLink is false', () => {
-		const bar = shallow( <PlanStorageBar { ...props } displayUpgradeLink={ false } /> );
-		assert.lengthOf( bar.find( '.plan-storage__storage-link' ), 0 );
+		const { container } = render( <PlanStorageBar { ...props } displayUpgradeLink={ false } /> );
+		assert.lengthOf( container.getElementsByClassName( 'plan-storage__storage-link' ), 0 );
 	} );
 } );
--- client/blocks/plan-storage/test/plan-storage.jsx
+++ client/blocks/plan-storage/test/plan-storage.jsx
@@ -12,15 +12,16 @@ import {
 	PLAN_PERSONAL_2_YEARS,
 	PLAN_FREE,
 } from '@automattic/calypso-products';
+import { render, screen } from '@testing-library/react';
 import { assert } from 'chai';
-import { mount } from 'enzyme';
+import nock from 'nock';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import { PlanStorage } from '../index';
 
 const siteId = 123;
 
-function render( component, additionalState = {}, planSlug = 'free_plan' ) {
+function renderComponent( component, additionalState = {}, planSlug = 'free_plan' ) {
 	const queryClient = new QueryClient();
 	const store = {
 		getState: () => ( {
@@ -46,7 +47,7 @@ function render( component, additionalState = {}, planSlug = 'free_plan' ) {
 		subscribe: () => {},
 		dispatch: () => {},
 	};
-	return mount(
+	return render(
 		<QueryClientProvider client={ queryClient }>
 			<Provider store={ store }>{ component }</Provider>
 		</QueryClientProvider>
@@ -54,50 +55,91 @@ function render( component, additionalState = {}, planSlug = 'free_plan' ) {
 }
 
 describe( 'PlanStorage basic tests', () => {
+	beforeAll( () => {
+		nock( 'https://public-api.wordpress.com:443' )
+			.persist()
+			.get( '/rest/v1.1/sites/123/media-storage' )
+			.reply( 200 );
+	} );
+
 	test( 'should not blow up and have class .plan-storage', () => {
-		const storage = render( <PlanStorage siteId={ siteId } /> );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
+		const { container } = renderComponent( <PlanStorage siteId={ siteId } /> );
+		assert.lengthOf( container.getElementsByClassName( 'plan-storage' ), 1 );
 	} );
 
 	test( 'should render a PlanStorageBar', () => {
-		const storage = render( <PlanStorage siteId={ siteId } /> );
-		const bar = storage.find( 'Localized(PlanStorageBar)' );
-		assert.lengthOf( bar, 1 );
+		renderComponent( <PlanStorage siteId={ siteId } /> );
+		const progressBar = screen.queryByRole( 'progressbar' );
+		assert.isDefined( progressBar );
 	} );
 
 	test( 'should render when storage is limited', () => {
-		let storage;
+		const { container: premContainer } = renderComponent(
+			<PlanStorage siteId={ siteId } />,
+			{},
+			PLAN_PREMIUM
+		);
+		assert.lengthOf( premContainer.getElementsByClassName( 'plan-storage' ), 1 );
 
-		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_PREMIUM );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
+		const { container: prem2Container } = renderComponent(
+			<PlanStorage siteId={ siteId } />,
+			{},
+			PLAN_PREMIUM_2_YEARS
+		);
+		assert.lengthOf( prem2Container.getElementsByClassName( 'plan-storage' ), 1 );
 
-		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_PREMIUM_2_YEARS );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
+		const { container: persContainer } = renderComponent(
+			<PlanStorage siteId={ siteId } />,
+			{},
+			PLAN_PERSONAL
+		);
+		assert.lengthOf( persContainer.getElementsByClassName( 'plan-storage' ), 1 );
 
-		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_PERSONAL );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
+		const { container: pers2Container } = renderComponent(
+			<PlanStorage siteId={ siteId } />,
+			{},
+			PLAN_PERSONAL_2_YEARS
+		);
+		assert.lengthOf( pers2Container.getElementsByClassName( 'plan-storage' ), 1 );
 
-		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_PERSONAL_2_YEARS );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
+		const { container: freeContainer } = renderComponent(
+			<PlanStorage siteId={ siteId } />,
+			{},
+			PLAN_FREE
+		);
+		assert.lengthOf( freeContainer.getElementsByClassName( 'plan-storage' ), 1 );
 
-		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_FREE );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
+		const { container: busContainer } = renderComponent(
+			<PlanStorage siteId={ siteId } />,
+			{},
+			PLAN_BUSINESS
+		);
+		assert.lengthOf( busContainer.getElementsByClassName( 'plan-storage' ), 1 );
 
-		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_BUSINESS );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
+		const { container: bus2Container } = renderComponent(
+			<PlanStorage siteId={ siteId } />,
+			{},
+			PLAN_BUSINESS_2_YEARS
+		);
+		assert.lengthOf( bus2Container.getElementsByClassName( 'plan-storage' ), 1 );
 
-		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_BUSINESS_2_YEARS );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
+		const { container: ecomContainer } = renderComponent(
+			<PlanStorage siteId={ siteId } />,
+			{},
+			PLAN_ECOMMERCE
+		);
+		assert.lengthOf( ecomContainer.getElementsByClassName( 'plan-storage' ), 1 );
 
-		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_ECOMMERCE );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
-
-		storage = render( <PlanStorage siteId={ siteId } />, {}, PLAN_ECOMMERCE_2_YEARS );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
+		const { container: ecom2Container } = renderComponent(
+			<PlanStorage siteId={ siteId } />,
+			{},
+			PLAN_ECOMMERCE_2_YEARS
+		);
+		assert.lengthOf( ecom2Container.getElementsByClassName( 'plan-storage' ), 1 );
 	} );
 
 	test( 'should render for atomic sites', () => {
-		const storage = render( <PlanStorage siteId={ siteId } />, {
+		const { container } = renderComponent( <PlanStorage siteId={ siteId } />, {
 			sites: {
 				items: {
 					[ siteId ]: {
@@ -113,11 +155,11 @@ describe( 'PlanStorage basic tests', () => {
 				},
 			},
 		} );
-		assert.lengthOf( storage.find( '.plan-storage' ), 1 );
+		assert.lengthOf( container.getElementsByClassName( 'plan-storage' ), 1 );
 	} );
 
 	test( 'should not render for jetpack sites', () => {
-		const storage = render( <PlanStorage siteId={ siteId } />, {
+		const { container } = renderComponent( <PlanStorage siteId={ siteId } />, {
 			sites: {
 				items: {
 					[ siteId ]: {
@@ -130,11 +172,11 @@ describe( 'PlanStorage basic tests', () => {
 				},
 			},
 		} );
-		assert.lengthOf( storage.find( '.plan-storage' ), 0 );
+		assert.lengthOf( container.getElementsByClassName( 'plan-storage' ), 0 );
 	} );
 
 	test( 'should not render for contributors', () => {
-		const storage = render( <PlanStorage siteId={ siteId } />, {
+		const { container } = renderComponent( <PlanStorage siteId={ siteId } />, {
 			currentUser: {
 				capabilities: {
 					[ siteId ]: {
@@ -143,11 +185,11 @@ describe( 'PlanStorage basic tests', () => {
 				},
 			},
 		} );
-		assert.lengthOf( storage.find( '.plan-storage' ), 0 );
+		assert.lengthOf( container.getElementsByClassName( 'plan-storage' ), 0 );
 	} );
 
 	test( 'should not render when site plan slug is empty', () => {
-		const storage = render( <PlanStorage siteId={ siteId } />, {}, null );
-		assert.lengthOf( storage.find( '.plan-storage' ), 0 );
+		const { container } = renderComponent( <PlanStorage siteId={ siteId } />, {}, null );
+		assert.lengthOf( container.getElementsByClassName( '.plan-storage' ), 0 );
 	} );
 } );
