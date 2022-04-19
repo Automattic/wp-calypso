e3fa62d7c05f37b47a37bff6547ddfc04f7aa33a
Griffith Chen <griffith.chen@automattic.com>
2022-04-19 10:40:21 +0800

    Add featured suggestions to the vertical question screen

--- client/components/select-vertical/index.tsx
+++ client/components/select-vertical/index.tsx
@@ -2,7 +2,11 @@ import { useTranslate } from 'i18n-calypso';
 import { useEffect, useState } from 'react';
 import { useDebounce } from 'use-debounce';
 import FormLabel from 'calypso/components/forms/form-label';
-import { useSiteVerticalQueryById, useSiteVerticalsQuery } from 'calypso/data/site-verticals';
+import {
+	useSiteVerticalQueryById,
+	useSiteVerticalsQuery,
+	useSiteVerticalsFeatured,
+} from 'calypso/data/site-verticals';
 import SuggestionSearch from './suggestion-search';
 import type { Vertical } from './types';
 import type { SiteVerticalsResponse } from 'calypso/data/site-verticals';
@@ -22,10 +26,13 @@ const SelectVertical: React.FC< Props > = ( { defaultVertical, isSkipSynonyms, o
 	const { data: defaultValue, isLoading: isLoadingDefaultVertical } = useSiteVerticalQueryById(
 		defaultVertical || ''
 	);
+
 	const { data: suggestions, isLoading: isLoadingSuggestions } = useSiteVerticalsQuery( {
 		term: debouncedSearchTerm,
 		skip_synonyms: isSkipSynonyms,
 	} );
+
+	const { data: featured } = useSiteVerticalsFeatured();
 
 	const mapOneSiteVerticalsResponseToVertical = ( vertical: SiteVerticalsResponse ): Vertical => ( {
 		value: vertical.id,
@@ -55,7 +62,11 @@ const SelectVertical: React.FC< Props > = ( { defaultVertical, isSkipSynonyms, o
 				placeholder={ String( translate( 'Ex. Cafe, Education, Photography' ) ) }
 				searchTerm={ searchTerm }
 				suggestions={
-					! isDebouncing ? mapManySiteVerticalsResponseToVertical( suggestions || [] ) : []
+					! isDebouncing
+						? mapManySiteVerticalsResponseToVertical(
+								( searchTerm === '' ? featured : suggestions ) || []
+						  )
+						: []
 				}
 				isLoading={ isDebouncing || isLoadingDefaultVertical || isLoadingSuggestions }
 				isDisableInput={ isLoadingDefaultVertical }
