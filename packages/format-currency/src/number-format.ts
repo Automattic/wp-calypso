34a7d5a01f56f7462865f8b5cea113ca0aa44851
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-04-20 12:08:01 +0530

    Improve number_format

--- packages/format-currency/src/number-format.ts
+++ packages/format-currency/src/number-format.ts
@@ -11,24 +11,22 @@ function toFixedFix( n: number, prec: number ) {
 }
 
 export default function number_format(
-	_number: number,
-	decimals: number,
-	dec_point: string,
-	thousands_sep: string
+	_number: number | string,
+	decimals = 0,
+	dec_point = '.',
+	thousands_sep = ','
 ) {
 	const number = ( _number + '' ).replace( /[^0-9+\-Ee.]/g, '' );
 	const n = ! isFinite( +number ) ? 0 : +number;
 	const prec = ! isFinite( +decimals ) ? 0 : Math.abs( decimals );
-	const sep = typeof thousands_sep === 'undefined' ? ',' : thousands_sep;
-	const dec = typeof dec_point === 'undefined' ? '.' : dec_point;
 	// Fix for IE parseFloat(0.55).toFixed(0) = 0;
 	const s = ( prec ? toFixedFix( n, prec ) : '' + Math.round( n ) ).split( '.' );
 	if ( s[ 0 ].length > 3 ) {
-		s[ 0 ] = s[ 0 ].replace( /\B(?=(?:\d{3})+(?!\d))/g, sep );
+		s[ 0 ] = s[ 0 ].replace( /\B(?=(?:\d{3})+(?!\d))/g, thousands_sep );
 	}
 	if ( ( s[ 1 ] || '' ).length < prec ) {
 		s[ 1 ] = s[ 1 ] || '';
 		s[ 1 ] += new Array( prec - s[ 1 ].length + 1 ).join( '0' );
 	}
-	return s.join( dec );
+	return s.join( dec_point );
 }
