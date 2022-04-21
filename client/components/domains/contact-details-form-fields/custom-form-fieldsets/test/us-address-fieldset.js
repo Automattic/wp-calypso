37904025c923b9a0347b4c7b584a81be255a79c3
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2022-04-21 14:24:38 +0300

    Domains: Refactor contact form fieldset tests to `@testing-library/react` and `jest` (#62920)
    * enzyme -> @testing-library/react
    
    * Improve mechanisms for element finding
    
    * chai -> jest

--- client/components/domains/contact-details-form-fields/custom-form-fieldsets/test/us-address-fieldset.js
+++ client/components/domains/contact-details-form-fields/custom-form-fieldsets/test/us-address-fieldset.js
@@ -1,14 +1,13 @@
 /**
  * @jest-environment jsdom
  */
-
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { render, screen } from 'calypso/test-helpers/config/testing-library';
 import UsAddressFieldset from '../us-address-fieldset';
 
 jest.mock( 'i18n-calypso', () => ( {
 	localize: ( x ) => x,
 	translate: ( x ) => x,
+	useTranslate: () => ( x ) => x,
 } ) );
 
 describe( 'US Address Fieldset', () => {
@@ -24,21 +23,21 @@ describe( 'US Address Fieldset', () => {
 	};
 
 	test( 'should render correctly with default props', () => {
-		const wrapper = shallow( <UsAddressFieldset { ...defaultProps } /> );
-		expect( wrapper.find( '.us-address-fieldset' ) ).to.have.length( 1 );
+		const { container } = render( <UsAddressFieldset { ...defaultProps } /> );
+		expect( container.getElementsByClassName( 'us-address-fieldset' ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should render expected input components', () => {
-		const wrapper = shallow( <UsAddressFieldset { ...defaultProps } /> );
-		expect( wrapper.find( '[name="city"]' ) ).to.have.length( 1 );
-		expect( wrapper.find( '[name="state"]' ) ).to.have.length( 1 );
-		expect( wrapper.find( '[name="postal-code"]' ) ).to.have.length( 1 );
+		render( <UsAddressFieldset { ...defaultProps } /> );
+		expect( screen.queryByLabelText( 'City' ) ).toBeDefined();
+		expect( screen.queryByLabelText( 'State' ) ).toBeDefined();
+		expect( screen.queryByLabelText( 'ZIP code' ) ).toBeDefined();
 	} );
 
 	test( 'should render all expected input components but postal code', () => {
-		const wrapper = shallow( <UsAddressFieldset { ...propsWithoutPostalCode } /> );
-		expect( wrapper.find( '[name="city"]' ) ).to.have.length( 1 );
-		expect( wrapper.find( '[name="state"]' ) ).to.have.length( 1 );
-		expect( wrapper.find( '[name="postal-code"]' ) ).to.have.length( 0 );
+		render( <UsAddressFieldset { ...propsWithoutPostalCode } /> );
+		expect( screen.queryByLabelText( 'City' ) ).toBeDefined();
+		expect( screen.queryByLabelText( 'State' ) ).toBeDefined();
+		expect( screen.queryByLabelText( 'Postal Code' ) ).toBeNull();
 	} );
 } );
