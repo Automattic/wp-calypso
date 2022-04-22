d752565941d1ec48d382bd44b427a6233980663e
Paul Bunkham <paul@dobit.co.uk>
2022-04-22 17:37:09 +0100

    Redirect if not Jetpack Cloud and show permissions error

--- client/jetpack-cloud/sections/jetpack-social/index.js
+++ client/jetpack-cloud/sections/jetpack-social/index.js
@@ -1,13 +1,18 @@
 import page from 'page';
 import { makeLayout, render as clientRender } from 'calypso/controller';
-import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
 import { socialPath } from 'calypso/lib/jetpack/paths';
 import { navigation, siteSelection, sites } from 'calypso/my-sites/controller';
-import { connections } from './controller';
+import { connections, redirectIfNotJetpackCloud } from './controller';
 
 export default function () {
-	if ( isJetpackCloud() ) {
-		page( socialPath(), siteSelection, sites, makeLayout, clientRender );
-		page( socialPath( ':site' ), siteSelection, navigation, connections, makeLayout, clientRender );
-	}
+	page( socialPath(), siteSelection, redirectIfNotJetpackCloud, sites, makeLayout, clientRender );
+	page(
+		socialPath( ':site' ),
+		siteSelection,
+		redirectIfNotJetpackCloud,
+		navigation,
+		connections,
+		makeLayout,
+		clientRender
+	);
 }
