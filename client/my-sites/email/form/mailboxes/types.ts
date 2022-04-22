3ec042066a59b7d85200487380ba2e72efb6a4fb
Olaseni Oluwunmi <277661+olaseni@users.noreply.github.com>
2022-04-22 11:38:24 +0100

    Initial data models for creating consolidated email provider forms (#62968)
    * initial consolidated data models
    
    * Rename mailbox field
    
    * Rename file path

---
+++ client/my-sites/email/form/mailboxes/types.ts
@@ -0,0 +1,29 @@
+import { PROVIDER_GOOGLE, PROVIDER_TITAN } from 'calypso/my-sites/email/form/mailboxes/constants';
+import type { TranslateResult } from 'i18n-calypso';
+
+type FieldError = TranslateResult | null;
+type FieldValue = string | boolean | null;
+
+enum Provider {
+	Google = PROVIDER_GOOGLE,
+	Titan = PROVIDER_TITAN,
+}
+
+interface MailboxFormField {
+	error: FieldError;
+	value: FieldValue;
+}
+
+interface Mailbox {
+	alternativeEmail: MailboxFormField;
+	domain: MailboxFormField;
+	firstName: MailboxFormField;
+	isAdmin: MailboxFormField;
+	lastName: MailboxFormField;
+	mailbox: MailboxFormField;
+	password: MailboxFormField;
+	provider: Provider;
+	uuid: string;
+}
+
+export type { FieldError, FieldValue, Mailbox, MailboxFormField, Provider, TranslateResult };
