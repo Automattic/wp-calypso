b61adca9a5857e004495282850c96ecd62edf4f8
Payton Swick <payton@foolord.com>
2022-04-22 15:16:32 -0400

    Add test for checkout contact form using cached details

--- client/my-sites/checkout/composite-checkout/test/use-cached-domain-contact-details.tsx
+++ client/my-sites/checkout/composite-checkout/test/use-cached-domain-contact-details.tsx
@@ -9,6 +9,7 @@ import {
 } from '@automattic/shopping-cart';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen, waitFor } from '@testing-library/react';
+import { useSelect } from '@wordpress/data';
 import nock from 'nock';
 import { Provider as ReduxProvider } from 'react-redux';
 import useCachedDomainContactDetails from 'calypso/my-sites/checkout/composite-checkout/hooks/use-cached-domain-contact-details';
@@ -19,6 +20,7 @@ import {
 	mockGetCartEndpointWith,
 	mockSetCartEndpointWith,
 } from './util';
+import type { ManagedContactDetails } from '@automattic/wpcom-checkout';
 
 const countryCode = 'US';
 const postalCode = '10001';
@@ -55,10 +57,15 @@ function MyTestContent() {
 	useWpcomStore();
 	const { responseCart } = useShoppingCart( initialCart.cart_key );
 	useCachedDomainContactDetails( countryList );
+	const contactInfo: ManagedContactDetails = useSelect( ( select ) =>
+		select( 'wpcom-checkout' ).getContactInfo()
+	);
 	return (
 		<div>
-			<div>Country: { responseCart.tax.location.country_code }</div>
-			<div>Postal: { responseCart.tax.location.postal_code }</div>
+			<div>Tax Country: { responseCart.tax.location.country_code }</div>
+			<div>Tax Postal: { responseCart.tax.location.postal_code }</div>
+			<div>Form Country: { contactInfo.countryCode?.value }</div>
+			<div>Form Postal: { contactInfo.postalCode?.value }</div>
 		</div>
 	);
 }
@@ -68,8 +75,17 @@ describe( 'useCachedDomainContactDetails', () => {
 		mockCachedContactDetailsEndpoint();
 		render( <MyTestWrapper /> );
 		await waitFor( () => {
-			expect( screen.queryByText( `Country: ${ countryCode }` ) ).toBeInTheDocument();
-			expect( screen.queryByText( `Postal: ${ postalCode }` ) ).toBeInTheDocument();
+			expect( screen.queryByText( `Tax Country: ${ countryCode }` ) ).toBeInTheDocument();
+			expect( screen.queryByText( `Tax Postal: ${ postalCode }` ) ).toBeInTheDocument();
+		} );
+	} );
+
+	it( 'sends the postal code and country from the contact details endpoint to the checkout data store', async () => {
+		mockCachedContactDetailsEndpoint();
+		render( <MyTestWrapper /> );
+		await waitFor( () => {
+			expect( screen.queryByText( `Form Country: ${ countryCode }` ) ).toBeInTheDocument();
+			expect( screen.queryByText( `Form Postal: ${ postalCode }` ) ).toBeInTheDocument();
 		} );
 	} );
 } );
