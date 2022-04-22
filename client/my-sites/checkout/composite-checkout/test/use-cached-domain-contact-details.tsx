3fdbb82d00a1b44a6fd68a7e2ab8ba4ba7e73225
Payton Swick <payton@foolord.com>
2022-04-22 15:27:31 -0400

    Add tests for countries without postal code support

--- client/my-sites/checkout/composite-checkout/test/use-cached-domain-contact-details.tsx
+++ client/my-sites/checkout/composite-checkout/test/use-cached-domain-contact-details.tsx
@@ -22,8 +22,6 @@ import {
 } from './util';
 import type { ManagedContactDetails } from '@automattic/wpcom-checkout';
 
-const countryCode = 'US';
-const postalCode = '10001';
 const initialCart = getEmptyResponseCart();
 const getCart = mockGetCartEndpointWith( initialCart );
 const setCart = mockSetCartEndpointWith( initialCart );
@@ -40,17 +38,13 @@ function MyTestWrapper() {
 	);
 }
 
-function mockCachedContactDetailsEndpoint() {
+function mockCachedContactDetailsEndpoint( data ) {
 	const endpoint = jest.fn();
 	endpoint.mockReturnValue( true );
-	const mockDomainContactResponse = () => [
-		200,
-		{ country_code: countryCode, postal_code: postalCode },
-	];
+	const mockDomainContactResponse = () => [ 200, data ];
 	nock( 'https://public-api.wordpress.com' )
 		.get( '/rest/v1.1/me/domain-contact-information' )
-		.reply( mockDomainContactResponse )
-		.persist();
+		.reply( mockDomainContactResponse );
 }
 
 function MyTestContent() {
@@ -62,17 +56,28 @@ function MyTestContent() {
 	);
 	return (
 		<div>
-			<div>Tax Country: { responseCart.tax.location.country_code }</div>
-			<div>Tax Postal: { responseCart.tax.location.postal_code }</div>
-			<div>Form Country: { contactInfo.countryCode?.value }</div>
-			<div>Form Postal: { contactInfo.postalCode?.value }</div>
+			{ responseCart.tax.location.country_code && (
+				<div>Tax Country: { responseCart.tax.location.country_code }</div>
+			) }
+			{ responseCart.tax.location.postal_code && (
+				<div>Tax Postal: { responseCart.tax.location.postal_code }</div>
+			) }
+			{ contactInfo.countryCode?.value && (
+				<div>Form Country: { contactInfo.countryCode?.value }</div>
+			) }
+			{ contactInfo.postalCode?.value && <div>Form Postal: { contactInfo.postalCode?.value }</div> }
 		</div>
 	);
 }
 
 describe( 'useCachedDomainContactDetails', () => {
-	it( 'sends the postal code and country from the contact details endpoint to the cart', async () => {
-		mockCachedContactDetailsEndpoint();
+	it( 'sends the postal code and country from the contact details endpoint to the cart for country with postal code', async () => {
+		const countryCode = 'US';
+		const postalCode = '10001';
+		mockCachedContactDetailsEndpoint( {
+			country_code: countryCode,
+			postal_code: postalCode,
+		} );
 		render( <MyTestWrapper /> );
 		await waitFor( () => {
 			expect( screen.queryByText( `Tax Country: ${ countryCode }` ) ).toBeInTheDocument();
@@ -80,12 +85,43 @@ describe( 'useCachedDomainContactDetails', () => {
 		} );
 	} );
 
-	it( 'sends the postal code and country from the contact details endpoint to the checkout data store', async () => {
-		mockCachedContactDetailsEndpoint();
+	it( 'sends the country from the contact details endpoint to the cart for country without postal code', async () => {
+		mockCachedContactDetailsEndpoint( {
+			country_code: 'CW',
+			postal_code: '10001',
+		} );
+		render( <MyTestWrapper /> );
+		await waitFor( () => {
+			expect( screen.queryByText( `Tax Country: CW` ) ).toBeInTheDocument();
+			expect( screen.queryByText( `Tax Postal:` ) ).not.toBeInTheDocument();
+		} );
+	} );
+
+	it( 'sends the postal code and country from the contact details endpoint to the checkout data store for country with postal code', async () => {
+		const countryCode = 'US';
+		const postalCode = '10001';
+		mockCachedContactDetailsEndpoint( {
+			country_code: countryCode,
+			postal_code: postalCode,
+		} );
 		render( <MyTestWrapper /> );
 		await waitFor( () => {
 			expect( screen.queryByText( `Form Country: ${ countryCode }` ) ).toBeInTheDocument();
 			expect( screen.queryByText( `Form Postal: ${ postalCode }` ) ).toBeInTheDocument();
+		} );
+	} );
+
+	it( 'sends the country from the contact details endpoint to the checkout data store for country without postal code', async () => {
+		const countryCode = 'CW';
+		const postalCode = '10001';
+		mockCachedContactDetailsEndpoint( {
+			country_code: countryCode,
+			postal_code: postalCode,
+		} );
+		render( <MyTestWrapper /> );
+		await waitFor( () => {
+			expect( screen.queryByText( `Form Country: ${ countryCode }` ) ).toBeInTheDocument();
+			expect( screen.queryByText( `Form Postal: ${ postalCode }` ) ).not.toBeInTheDocument();
 		} );
 	} );
 } );
