f8c06265ff23361e1241861ad533a8bb90f1a88c
Olaseni Oluwunmi <277661+olaseni@users.noreply.github.com>
2022-04-25 16:28:38 +0100

    Freeze

--- client/my-sites/email/form/mailboxes/types.ts
+++ client/my-sites/email/form/mailboxes/types.ts
@@ -1,29 +1,88 @@
-import { PROVIDER_GOOGLE, PROVIDER_TITAN } from 'calypso/my-sites/email/form/mailboxes/constants';
+import { v4 as uuid_v4 } from 'uuid';
 import type { TranslateResult } from 'i18n-calypso';
 
 type FieldError = TranslateResult | null;
-type FieldValue = string | boolean | null;
 
-enum Provider {
-	Google = PROVIDER_GOOGLE,
-	Titan = PROVIDER_TITAN,
+enum EmailProvider {
+	Google = 'Google',
+	Titan = 'Titan',
 }
 
-interface MailboxFormField {
+interface MailboxFormField< Type > {
 	error: FieldError;
-	value: FieldValue;
+	value: Type;
 }
 
-interface Mailbox {
-	alternativeEmail: MailboxFormField;
-	domain: MailboxFormField;
-	firstName: MailboxFormField;
-	isAdmin: MailboxFormField;
-	lastName: MailboxFormField;
-	mailbox: MailboxFormField;
-	password: MailboxFormField;
-	provider: Provider;
-	uuid: string;
+abstract class MailboxFormFieldBase< T > implements MailboxFormField< T > {
+	error: FieldError = null;
+	value!: T;
 }
 
-export type { FieldError, FieldValue, Mailbox, MailboxFormField, Provider, TranslateResult };
+class DataMailboxFormField extends MailboxFormFieldBase< string > {
+	value = uuid_v4();
+}
+
+class TextMailboxFormField extends MailboxFormFieldBase< string | null > {
+	value = null;
+}
+
+class BooleanMailboxFormField extends MailboxFormFieldBase< boolean > {
+	value = false;
+}
+
+interface IBaseMailboxFormFields {
+	domain: TextMailboxFormField;
+	mailbox: TextMailboxFormField;
+	password: TextMailboxFormField;
+	readonly uuid: DataMailboxFormField;
+}
+
+interface IGoogleMailboxFormFields extends IBaseMailboxFormFields {
+	firstName: TextMailboxFormField;
+	lastName: TextMailboxFormField;
+}
+
+interface ITitanMailboxFormFields extends IBaseMailboxFormFields {
+	alternativeEmail: TextMailboxFormField;
+	name: TextMailboxFormField;
+	isAdmin: BooleanMailboxFormField;
+}
+
+abstract class MailboxFormFields implements IBaseMailboxFormFields {
+	domain = new TextMailboxFormField();
+	mailbox = new TextMailboxFormField();
+	password = new TextMailboxFormField();
+	readonly uuid = new DataMailboxFormField();
+}
+
+class GoogleMailboxFormFields extends MailboxFormFields implements IGoogleMailboxFormFields {
+	firstName = new TextMailboxFormField();
+	lastName = new TextMailboxFormField();
+}
+
+class TitanMailboxFormFields extends MailboxFormFields implements ITitanMailboxFormFields {
+	alternativeEmail = new TextMailboxFormField();
+	isAdmin = new BooleanMailboxFormField();
+	name = new TextMailboxFormField();
+}
+
+const MailboxFormFieldsMap = {
+	[ EmailProvider.Google ]: GoogleMailboxFormFields,
+	[ EmailProvider.Titan ]: TitanMailboxFormFields,
+};
+
+type ValidatorFieldNames = keyof GoogleMailboxFormFields | keyof TitanMailboxFormFields | null;
+
+type ProviderKeys = keyof typeof MailboxFormFieldsMap;
+type ProviderTypes = typeof MailboxFormFieldsMap[ ProviderKeys ];
+type ExtractInstanceType< T > = T extends new () => infer R ? R : never;
+
+class MailboxFormFieldsFactory {
+	static create( providerKey: ProviderKeys ): ExtractInstanceType< ProviderTypes > {
+		return new MailboxFormFieldsMap[ providerKey ]();
+	}
+}
+
+export type { ValidatorFieldNames, MailboxFormFieldBase, MailboxFormFields };
+
+export { EmailProvider, MailboxFormFieldsFactory };
