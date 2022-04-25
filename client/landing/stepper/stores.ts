1e93977869117ca8c957b6039807bf956538407c
Mark Biek <mark.biek@automattic.com>
2022-04-25 11:14:49 -0400

    Added a products-list data-store package (#62862)
    This is based on
    [client/state/products-list](https://github.com/Automattic/wp-calypso/tree/trunk/client/state/products-list) but only has the selector to return the complete list of products. The other selectors can be ported over as they're needed.

--- client/landing/stepper/stores.ts
+++ client/landing/stepper/stores.ts
@@ -1,8 +1,9 @@
 import config from '@automattic/calypso-config';
-import { Onboard, Site } from '@automattic/data-stores';
+import { Onboard, Site, ProductsList } from '@automattic/data-stores';
 
 export const ONBOARD_STORE = Onboard.register();
 export const SITE_STORE = Site.register( {
 	client_id: config( 'wpcom_signup_id' ),
 	client_secret: config( 'wpcom_signup_key' ),
 } );
+export const PRODUCTS_LIST_STORE = ProductsList.register();
