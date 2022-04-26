ed30833cb87ff1fc19f51f376675f4e376d8204a
Edwin Takahashi <egao@outlook.com>
2022-04-26 13:04:46 +0900

    Add UUID for the build

--- .teamcity/_self/projects/WPComTests.kt
+++ .teamcity/_self/projects/WPComTests.kt
@@ -35,8 +35,8 @@ object WPComTests : Project({
 	buildType(gutenbergPlaywrightBuildType("desktop", "fab2e82e-d27b-4ba2-bbd7-232df944e75c", false, false));
 	buildType(gutenbergPlaywrightBuildType("mobile", "77a5a0f1-9644-4c04-9d27-0066cd2d4ada", false, false));
 	// Gutenberg Simple Edge
-	buildType(gutenbergPlaywrightBuildType("desktop", "", false, true));
-	buildType(gutenbergPlaywrightBuildType("mobile", "", false, true));
+	buildType(gutenbergPlaywrightBuildType("desktop", "e8817ab4-ec4e-4d58-a215-d1f87b2227b6", false, true));
+	buildType(gutenbergPlaywrightBuildType("mobile", "a655d304-4dcf-4864-8d82-8b22dba29feb", false, true));
 	// Gutenberg Atomic
 	buildType(gutenbergPlaywrightBuildType("desktop", "c341e9b9-1118-48e9-a569-325100f5fd9" , true, false));
 	buildType(gutenbergPlaywrightBuildType("mobile", "e0f7e412-ae6c-41d3-9eec-c57c94dd8385", true, false));
@@ -53,10 +53,10 @@ object WPComTests : Project({
 
 fun gutenbergPlaywrightBuildType( targetDevice: String, buildUuid: String, atomic: Boolean = false ): E2EBuildType {
 	var siteType = if (atomic) "atomic" else "simple";
-	var edgeType = if (edge) "Edge" else "";
+	var edgeType = if (edge) "edge" else "production";
 
     return E2EBuildType (
-		buildId = "WPComTests_gutenberg_Playwright_${siteType}_$targetDevice",
+		buildId = "WPComTests_gutenberg_Playwright_${siteType}_${edgeType}_$targetDevice",
 		buildUuid = buildUuid,
 		buildName = "Gutenberg $siteType E2E tests ($targetDevice)",
 		buildDescription = "Runs Gutenberg $siteType E2E tests on $targetDevice size",
@@ -69,14 +69,6 @@ fun gutenbergPlaywrightBuildType( targetDevice: String, buildUuid: String, atomi
 				description = "URL to test against",
 				allowEmpty = false
 			)
-			// checkbox(
-			// 	name = "env.GUTENBERG_EDGE",
-			// 	value = "false",
-			// 	label = "Use gutenberg-edge",
-			// 	description = "Use a blog with gutenberg-edge sticker",
-			// 	checked = "true",
-			// 	unchecked = "false"
-			// )
 			checkbox(
 				name = "env.COBLOCKS_EDGE",
 				value = "false",
