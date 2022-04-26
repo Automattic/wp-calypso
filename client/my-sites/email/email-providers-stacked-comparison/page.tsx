45a522f4a449748ffeceac1cd09f1ff92cd28170
Olaseni Oluwunmi <277661+olaseni@users.noreply.github.com>
2022-04-26 06:49:00 +0100

    Cleanup test file

--- client/my-sites/email/email-providers-stacked-comparison/page.tsx
+++ client/my-sites/email/email-providers-stacked-comparison/page.tsx
@@ -1,21 +1,25 @@
-import { MailboxForm } from 'calypso/my-sites/email/form/mailboxes';
-import { EmailProvider, TitanMailboxFormFields } from 'calypso/my-sites/email/form/mailboxes/types';
+import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import EmailProvidersStackedComparison from 'calypso/my-sites/email/email-providers-stacked-comparison';
+import { emailManagementPurchaseNewEmailAccount } from 'calypso/my-sites/email/paths';
+import type { EmailProvidersStackedComparisonProps } from 'calypso/my-sites/email/email-providers-stacked-comparison';
 
-const EmailProvidersStackedComparisonPage = (): JSX.Element => {
-	const mb = new MailboxForm( EmailProvider.Titan, 'rodent.com', [] );
-	const formFields: TitanMailboxFormFields = mb.formFields;
-
-	formFields.mailbox.value = 'formica-ca';
-	if ( formFields.alternativeEmail ) {
-		formFields.alternativeEmail.value = 'com@rodent.com';
-	}
-
-	mb.validate();
-	window.console.log( 'ZKK', mb.provider, formFields.password.error );
-	// mb.clearErrors();
-	window.console.log( 'ZKK', mb.provider, formFields );
-
-	return <></>;
+const EmailProvidersStackedComparisonPage = (
+	props: EmailProvidersStackedComparisonProps
+): JSX.Element => {
+	return (
+		<>
+			<PageViewTracker
+				path={ emailManagementPurchaseNewEmailAccount( ':site', ':domain' ) }
+				title="Email Comparison"
+				properties={ {
+					source: props.source,
+					context: props.comparisonContext,
+					provider: props.selectedEmailProviderSlug,
+				} }
+			/>
+			<EmailProvidersStackedComparison { ...props } />
+		</>
+	);
 };
 
 export default EmailProvidersStackedComparisonPage;
