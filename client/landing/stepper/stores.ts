aa2687dbf9010e4be052e8a70cc0417360a9ae07
Valter Lorran <valterlorran@hotmail.com>
2022-04-27 15:16:10 -0300

    We need the user store to get the username to create a new site

--- client/landing/stepper/stores.ts
+++ client/landing/stepper/stores.ts
@@ -1,5 +1,5 @@
 import config from '@automattic/calypso-config';
-import { Onboard, Site, ProductsList } from '@automattic/data-stores';
+import { Onboard, Site, ProductsList, User } from '@automattic/data-stores';
 
 export const ONBOARD_STORE = Onboard.register();
 export const SITE_STORE = Site.register( {
@@ -7,3 +7,7 @@ export const SITE_STORE = Site.register( {
 	client_secret: config( 'wpcom_signup_key' ),
 } );
 export const PRODUCTS_LIST_STORE = ProductsList.register();
+export const USER_STORE = User.register( {
+	client_id: config( 'wpcom_signup_id' ),
+	client_secret: config( 'wpcom_signup_key' ),
+} );
