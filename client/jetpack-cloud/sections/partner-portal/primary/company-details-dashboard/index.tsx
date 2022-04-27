1b5c5a1de7feec049cc6394acce0410d0e59f7b7
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2022-04-27 17:08:29 +0200

    Licensing Portal: Add company details form

---
+++ client/jetpack-cloud/sections/partner-portal/primary/company-details-dashboard/index.tsx
@@ -0,0 +1,31 @@
+import { useTranslate } from 'i18n-calypso';
+import { ReactElement } from 'react';
+import CardHeading from 'calypso/components/card-heading';
+import DocumentHead from 'calypso/components/data/document-head';
+import Main from 'calypso/components/main';
+import CompanyDetailsForm from 'calypso/jetpack-cloud/sections/partner-portal/company-details-form';
+import SidebarNavigation from 'calypso/jetpack-cloud/sections/partner-portal/sidebar-navigation';
+import './style.scss';
+
+export default function CompanyDetailsDashboard(): ReactElement {
+	const translate = useTranslate();
+
+	return (
+		<Main wideLayout className="company-details-dashboard">
+			<DocumentHead title={ translate( 'Company Details' ) } />
+			<SidebarNavigation />
+
+			<div className="company-details-dashboard__header">
+				<CardHeading size={ 36 }>{ translate( 'Company Details' ) }</CardHeading>
+
+				<p className="company-details-dashboard__description">
+					{ translate(
+						'Update your company details. Updates will appear on all future receipts.'
+					) }
+				</p>
+			</div>
+
+			<CompanyDetailsForm />
+		</Main>
+	);
+}
--- client/state/partner-portal/types.ts
+++ client/state/partner-portal/types.ts
@@ -59,10 +59,20 @@ export interface APIPartnerKey {
 	has_licenses: boolean;
 }
 
+export interface APIPartnerAddress {
+	country: string | null;
+	city: string | null;
+	line1: string | null;
+	line2: string | null;
+	postal_code: string | number | null;
+	state: string | null;
+}
+
 export interface APIPartner {
 	id: number;
 	slug: string;
 	name: string;
+	address: APIPartnerAddress;
 	keys: APIPartnerKey[];
 	tos: string;
 	partner_type: string;
@@ -140,10 +150,20 @@ export interface PartnerKey {
 	hasLicenses: boolean;
 }
 
+export interface PartnerAddress {
+	country: string | null;
+	city: string | null;
+	line1: string | null;
+	line2: string | null;
+	postal_code: string | number | null;
+	state: string | null;
+}
+
 export interface Partner {
 	id: number;
 	slug: string;
 	name: string;
+	address: PartnerAddress;
 	keys: PartnerKey[];
 	tos: string;
 	partner_type: string;
