8f92903e0b7b1dffc4b9cafb7a7d6962e0cd6fbf
sixhours <calobee@gmail.com>
2022-04-27 10:27:03 -0400

    Simplify site title logic by bringing over custom hook from Gutenboarding to handle whether the site title has already been filled out

--- client/landing/stepper/declarative-flow/internals/steps-repository/podcast-title/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/podcast-title/index.tsx
@@ -10,7 +10,7 @@ import FormLabel from 'calypso/components/forms/form-label';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
 import FormInput from 'calypso/components/forms/form-text-input';
 import getTextWidth from 'calypso/landing/gutenboarding/onboarding-block/acquire-intent/get-text-width';
-import usePodcastTitle from 'calypso/landing/stepper/hooks/use-podcast-title';
+import useSiteTitle from 'calypso/landing/stepper/hooks/use-site-title';
 import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { tip } from 'calypso/signup/icons';
@@ -22,20 +22,21 @@ const PodcastTitleStep: Step = function PodcastTitleStep( { navigation } ) {
 	const { __ } = useI18n();
 
 	const PodcastTitleForm: React.FC = () => {
-		const [ formTouched, setFormTouched ] = useState( false );
+		//Sets the site title from the API on first load if a custom title has not been set
+		useSiteTitle();
 		const { siteTitle } = useSelect( ( select ) => select( ONBOARD_STORE ).getState() );
 		const { setSiteTitle } = useDispatch( ONBOARD_STORE );
-		const titleFromApi = usePodcastTitle();
-		const podcastTitle = siteTitle ? siteTitle : titleFromApi;
+
 		const inputRef = useRef< HTMLInputElement >();
+		const underlineWidth = getTextWidth( ( siteTitle as string ) || '', inputRef.current );
+
+		const [ formTouched, setFormTouched ] = useState( false );
 
 		const handleSubmit = async ( siteTitle: string ) => {
 			const providedDependencies = { siteTitle };
 			setSiteTitle( siteTitle );
 			submit?.( providedDependencies, siteTitle );
 		};
-
-		const underlineWidth = getTextWidth( ( podcastTitle as string ) || '', inputRef.current );
 
 		const handleChange = ( event: React.FormEvent< HTMLInputElement > ) => {
 			setFormTouched( true );
@@ -56,13 +57,13 @@ const PodcastTitleStep: Step = function PodcastTitleStep( { navigation } ) {
 						<FormInput
 							id="podcast-title"
 							inputRef={ inputRef }
-							value={ podcastTitle }
+							value={ siteTitle }
 							onChange={ handleChange }
 							placeholder="Good Fun"
 						/>
 						<div
 							className={ classNames( 'podcast-title__underline', {
-								'is-empty': ! ( podcastTitle as string )?.length,
+								'is-empty': ! ( siteTitle as string )?.length,
 							} ) }
 							style={ { width: underlineWidth || '100%' } }
 						/>
