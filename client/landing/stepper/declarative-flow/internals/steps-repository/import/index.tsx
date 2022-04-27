3dca25224f4f2eceb281d8679ca3d0a881136405
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-04-27 20:58:46 +0200

    Simplify import component showing only capture step

--- client/landing/stepper/declarative-flow/internals/steps-repository/import/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/import/index.tsx
@@ -2,25 +2,12 @@
 import { StepContainer } from '@automattic/onboarding';
 import { useI18n } from '@wordpress/react-i18n';
 import React, { ReactElement } from 'react';
-import { useSelector } from 'react-redux';
 import DocumentHead from 'calypso/components/data/document-head';
 import { useCurrentRoute } from 'calypso/landing/stepper/hooks/use-current-route';
-import { useSite } from 'calypso/landing/stepper/hooks/use-site';
-import { useSiteSlugParam } from 'calypso/landing/stepper/hooks/use-site-slug-param';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import CaptureStep from 'calypso/signup/steps/import/capture';
-import ListStep from 'calypso/signup/steps/import/list';
-import {
-	ReadyStep,
-	ReadyNotStep,
-	ReadyPreviewStep,
-	ReadyAlreadyOnWPCOMStep,
-} from 'calypso/signup/steps/import/ready';
-import { GoToStep } from 'calypso/signup/types';
-import { getUrlData } from 'calypso/state/imports/url-analyzer/selectors';
 import { BASE_ROUTE } from './config';
-import { generateStepPath, getFinalImporterUrl } from './helper';
-import { redirect } from './util';
+import { generateStepPath } from './helper';
 import type { Step } from '../../types';
 import './style.scss';
 
@@ -53,87 +40,13 @@ export const ImportWrapper: Step = function ( props ) {
 const ImportStep: Step = function ImportStep( props ) {
 	const { navigation } = props;
 
-	/**
-	 ↓ Fields
-	 */
-	const siteSlug = useSiteSlugParam();
-	const site = useSite();
-	const currentRoute = useCurrentRoute();
-	const isAtomicSite = !! site?.options?.is_automated_transfer;
-	const urlData = useSelector( getUrlData );
-
-	/**
-	 ↓ Effects
-	 */
-	if ( ! urlData && currentRoute !== BASE_ROUTE && currentRoute !== 'import/list' ) {
-		goToHomeStep();
-		return null;
-	}
-
-	/**
-	 ↓ Methods
-	 */
-	const goToStep: GoToStep = function ( stepName, stepSectionName ) {
-		navigation.goToStep?.( generateStepPath( stepName, stepSectionName ) );
-	};
-
-	function goToHomeStep() {
-		navigation.goToStep?.( BASE_ROUTE );
-	}
-
-	function goToImporterPage() {
-		const url = getFinalImporterUrl(
-			siteSlug as string,
-			urlData.url,
-			urlData.platform,
-			isAtomicSite
-		);
-
-		redirect( url );
-	}
-
-	/**
-	 ↓ Renders
-	 */
-	function renderStepContent() {
-		return (
-			<>
-				{ currentRoute === 'import' && <CaptureStep goToStep={ goToStep } /> }
-				{ currentRoute === 'import/list' && <ListStep goToStep={ goToStep } /> }
-
-				{ currentRoute === 'import/ready' && (
-					<ReadyStep
-						platform={ urlData?.platform }
-						goToImporterPage={ goToImporterPage }
-						recordTracksEvent={ recordTracksEvent }
-					/>
-				) }
-
-				{ currentRoute === 'import/ready/preview' && (
-					<ReadyPreviewStep
-						urlData={ urlData }
-						goToImporterPage={ goToImporterPage }
-						siteSlug={ siteSlug as string }
-						recordTracksEvent={ recordTracksEvent }
-					/>
-				) }
-
-				{ currentRoute === 'import/ready/not' && (
-					<ReadyNotStep goToStep={ goToStep } recordTracksEvent={ recordTracksEvent } />
-				) }
-
-				{ currentRoute === 'import/ready/wpcom' && (
-					<ReadyAlreadyOnWPCOMStep
-						urlData={ urlData }
-						goToStep={ goToStep }
-						recordTracksEvent={ recordTracksEvent }
-					/>
-				) }
-			</>
-		);
-	}
-
-	return <ImportWrapper { ...props }>{ renderStepContent() }</ImportWrapper>;
+	return (
+		<ImportWrapper { ...props }>
+			<CaptureStep
+				goToStep={ ( step, section ) => navigation.goToStep?.( generateStepPath( step, section ) ) }
+			/>
+		</ImportWrapper>
+	);
 };
 
 export default ImportStep;
