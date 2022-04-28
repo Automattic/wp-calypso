8226c24024cd38edd3fa21e74355ace7e157ef68
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-04-28 18:04:16 +0200

    Create importerWordpress step component

---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/importer-wordpress/index.tsx
@@ -0,0 +1,25 @@
+import { useRef } from 'react';
+import { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
+import { WordpressImporter } from 'calypso/signup/steps/import-from/wordpress';
+import { ImporterWrapper } from '../importer-wrapper';
+import { ImporterWrapperRefAttr } from '../importer-wrapper/types';
+import './style.scss';
+
+const ImporterWordpress: Step = function ( props ) {
+	const wrapperRef = useRef< ImporterWrapperRefAttr >();
+	const refObj = wrapperRef.current;
+
+	return (
+		<ImporterWrapper importer={ 'wordpress' } { ...props } ref={ wrapperRef }>
+			{ refObj && (
+				<WordpressImporter
+					siteId={ refObj.siteId }
+					siteSlug={ refObj.siteSlug }
+					fromSite={ refObj.fromSite }
+				/>
+			) }
+		</ImporterWrapper>
+	);
+};
+
+export default ImporterWordpress;
