762507f20580aa8a45bc7ce3cda7351fe7d9437b
dpasque <dan.speckhard.pasque@automattic.com>
2022-04-29 12:40:12 -0500

    Add validation to the parsed secrets

--- packages/calypso-e2e/src/secrets/secrets.ts
+++ packages/calypso-e2e/src/secrets/secrets.ts
@@ -1,33 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */ // We have to do some dynamic parsing and iteration as we validate JSON.
 import fs from 'fs';
 import path from 'path';
-
-const SECRETS_FILE_NAME = 'decrypted-secrets.json';
-
-/**
- * Initializes the secrets needed for E2E testing.
- */
-function initializeSecrets(): Secrets {
-	try {
-		// Normally, any fs sync operations are a big no-no.
-		// However, this only runs once on module load, and should be safe to do.
-		// This is a more runtime deferred version of just having a '.json' module.
-		// Once we're on ES2022, this would be a perfect use case for a top-level-await.
-		const secretsJson = fs.readFileSync( path.join( __dirname, SECRETS_FILE_NAME ), {
-			encoding: 'utf-8',
-		} );
-		return JSON.parse( secretsJson );
-	} catch ( err ) {
-		const error: Error = err as Error;
-		throw new Error(
-			'Failed to initialize the E2E secrets.\n' +
-				'Have you decrypted the secrets file yet?\n' +
-				'Export the decryption key to E2E_SECRETS_KEY and run "yarn decrypt-secrets".\n' +
-				`Original error message: ${ error.message }`
-		);
-	}
-}
-
-export const secrets = initializeSecrets();
 
 export const TEST_ACCOUNT_NAMES = [
 	'defaultUser',
@@ -90,5 +63,140 @@ export interface Secrets {
 	};
 	otherTestSites: {
 		[ key in OtherTestSiteName ]: string;
+	};
+}
+
+export const secrets = initializeSecrets();
+
+/**
+ * Initializes the secrets needed for E2E testing.
+ */
+function initializeSecrets(): Secrets {
+	const SECRETS_FILE_NAME = 'decrypted-secrets.json';
+	const parsedSecrets = parseSecretsFile( path.join( __dirname, SECRETS_FILE_NAME ) );
+	const fakeReferenceSecrets = createFakeSecretsForValidation();
+	validateSecrets( parsedSecrets, fakeReferenceSecrets );
+	return parsedSecrets as Secrets;
+}
+
+/**
+ * Read and JSON parse the decrypted secrets file.
+ *
+ * @param {string} filePath Full path to the expected decrypted secrets file.
+ * @returns A parsed JSON object.
+ */
+function parseSecretsFile( filePath: string ): any {
+	try {
+		// Normally, any fs sync operations are a big no-no.
+		// However, this only runs once on module load, and should be safe to do.
+		// This is a more runtime deferred version of just having a '.json' module.
+		// Once we're on ES2022, this would be a perfect use case for a top-level-await.
+		const secretsJson = fs.readFileSync( filePath, {
+			encoding: 'utf-8',
+		} );
+		return JSON.parse( secretsJson );
+	} catch ( err ) {
+		const error: Error = err as Error;
+		throw new Error(
+			'Failed to initialize the E2E secrets: Could not find and parse the secrets file.\n' +
+				'Have you decrypted the secrets file yet?\n' +
+				'Export the decryption key to E2E_SECRETS_KEY and run "yarn decrypt-secrets".\n' +
+				`Original error message: ${ error.message }`
+		);
+	}
+}
+
+/**
+ * Validates a parsed secrets object against a fake set of reference secrets.
+ * This ensures we have all the keys we expect and they are of the right type.
+ *
+ * @param {any} parsedSecrets The parsed JSON object from the local secrets file.
+ * @param {Secrets} fakeReferenceSecrets A fake secrets object that can be used for reference validation.
+ * @throws If any expected key is missing or the wrong type.
+ */
+function validateSecrets( parsedSecrets: any, fakeReferenceSecrets: Secrets ): void {
+	const compareRecursive = ( target: any, reference: any, keyPath: string[] ) => {
+		for ( const key in reference ) {
+			if ( ! target?.[ key ] || typeof reference[ key ] !== typeof target[ key ] ) {
+				const fullKeyPath = [ ...keyPath, key ].join( '.' );
+				throw new Error(
+					'Failed to initialize the E2E secrets: Invalid or missing key found in the decrypted secrets.\n' +
+						'Make sure you have decrypted the most recent version of the encrypted secrets.\n' +
+						'This also may mean the typings for the secrets are stale and need updating.\n\n' +
+						'Details:\n' +
+						`\tInvalid or missing key: ${ fullKeyPath }\n` +
+						`\tExpected type: ${ typeof reference[ key ] }`
+				);
+			}
+
+			if ( typeof reference[ key ] === 'object' ) {
+				compareRecursive( target[ key ], reference[ key ], [ ...keyPath, key ] );
+			}
+		}
+	};
+
+	compareRecursive( parsedSecrets, fakeReferenceSecrets, [] );
+}
+
+/**
+ * Creates a fake set of secrets that can be used for comparative validation.
+ *
+ * @returns A fake secret object with all expcted required properties.
+ */
+function createFakeSecretsForValidation(): Secrets {
+	const fakeAccount = {
+		username: 'FAKE_VALUE',
+		password: 'FAKE_VALUE',
+	};
+
+	return {
+		passwordForNewTestSignUps: 'FAKE_VALUE',
+		storeSandboxCookieValue: 'FAKE_VALUE',
+		testCouponCode: 'FAKE_VALUE',
+		wpccAuthPath: 'FAKE_VALUE',
+		restApiApplication: {
+			user: 'FAKE_VALUE',
+			client_id: 'FAKE_VALUE',
+			redirect_uri: 'FAKE_VALUE',
+			client_secret: 'FAKE_VALUE',
+			access_code: 'FAKE_VALUE',
+		},
+		martechTosUploadCredentials: {
+			bearer_token: 'FAKE_VALUE',
+		},
+		mailosaur: {
+			apiKey: 'FAKE_VALUE',
+			inviteInboxId: 'FAKE_VALUE',
+			signupInboxId: 'FAKE_VALUE',
+			domainsInboxId: 'FAKE_VALUE',
+			defaultUserInboxId: 'FAKE_VALUE',
+		},
+		testAccounts: {
+			defaultUser: { ...fakeAccount },
+			eCommerceUser: { ...fakeAccount },
+			simpleSiteFreePlanUser: { ...fakeAccount },
+			simpleSitePersonalPlanUser: { ...fakeAccount },
+			gutenbergSimpleSiteUser: { ...fakeAccount },
+			gutenbergSimpleSiteEdgeUser: { ...fakeAccount },
+			gutenbergAtomicSiteUser: { ...fakeAccount },
+			gutenbergAtomicSiteEdgeUser: { ...fakeAccount },
+			coBlocksSimpleSiteEdgeUser: { ...fakeAccount },
+			siteEditorSimpleSiteUser: { ...fakeAccount },
+			siteEditorSimpleSiteEdgeUser: { ...fakeAccount },
+			martechTosUser: { ...fakeAccount },
+			calypsoPreReleaseUser: { ...fakeAccount },
+			i18nUser: { ...fakeAccount },
+			// This one needs a totpKey
+			p2User: { ...fakeAccount, totpKey: 'FAKE_VALUE' },
+			jetpackUser: { ...fakeAccount },
+			jetpackUserPREMIUM: { ...fakeAccount },
+			jetpackUserJN: { ...fakeAccount },
+			commentingUser: { ...fakeAccount },
+			notificationsUser: { ...fakeAccount },
+			googleLoginUser: { ...fakeAccount },
+		},
+		otherTestSites: {
+			notifications: 'FAKE_VALUE',
+		},
 	};
 }
