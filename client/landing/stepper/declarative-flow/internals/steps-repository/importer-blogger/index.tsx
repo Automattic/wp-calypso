b533f8f38bd2bdc0ef421badbdfaf834d19800d3
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-04-30 20:22:04 +0200

    Create importerBlogger step component

---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/importer-blogger/index.tsx
@@ -0,0 +1,29 @@
+import { useRef } from 'react';
+import { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
+import BloggerImporter from 'calypso/signup/steps/import-from/blogger';
+import { ImporterWrapper } from '../importer-wrapper';
+import { ImporterWrapperRefAttr } from '../importer-wrapper/types';
+import './style.scss';
+
+const ImporterBlogger: Step = function ( props ) {
+	const wrapperRef = useRef< ImporterWrapperRefAttr >();
+	const refObj = wrapperRef.current;
+
+	return (
+		<ImporterWrapper importer={ 'blogger' } { ...props } ref={ wrapperRef }>
+			{ refObj && (
+				<BloggerImporter
+					run={ refObj.run }
+					job={ refObj.job }
+					siteId={ refObj.siteId }
+					site={ refObj.site }
+					siteSlug={ refObj.siteSlug }
+					fromSite={ refObj.fromSite }
+					urlData={ refObj.urlData }
+				/>
+			) }
+		</ImporterWrapper>
+	);
+};
+
+export default ImporterBlogger;
