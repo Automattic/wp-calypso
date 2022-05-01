86fb284aa5494160388be6beaf9a01e9d370e72b
Omar Alshaker <omar@omaralshaker.com>
2022-05-01 13:40:49 +0200

    Revert contact-page options

--- client/blocks/inline-help/inline-help-contact-page.tsx
+++ client/blocks/inline-help/inline-help-contact-page.tsx
@@ -1,6 +1,5 @@
 import { Button, Gridicon } from '@automattic/components';
 import { useI18n } from '@wordpress/react-i18n';
-import classnames from 'classnames';
 import InlineHelpSearchResults from './inline-help-search-results';
 
 import './inline-help-contact-page.scss';
@@ -22,24 +21,20 @@ const InlineHelpContactPage: React.FC< Props > = ( { closeContactPage, onSelectR
 			<div className="inline-help__contact-content">
 				<h3>{ __( 'Contact our WordPress.com experts' ) }</h3>
 				<div className="inline-help__contact-boxes">
-					<button className={ classnames( 'inline-help__contact-box', 'chat' ) }>
-						<div className="inline-help__contact-box-icon">
-							<Gridicon icon={ 'comment' } />
+					<div className="inline-help__contact-box">
+						<div style={ { backgroundColor: 'red' } }>
+							<Gridicon icon={ 'comment' } size={ 18 } />
 						</div>
-						<div>
-							<h2>{ __( 'Live chat' ) }</h2>
-							<p>{ __( 'Get an immediate reply' ) }</p>
+						<h2>{ __( 'Live chat' ) }</h2>
+						<p>{ __( 'Get an immediate reply' ) }</p>
+					</div>
+					<div className="inline-help__contact-box">
+						<div style={ { backgroundColor: 'blue' } }>
+							<Gridicon icon={ 'mail' } size={ 18 } />
 						</div>
-					</button>
-					<button className={ classnames( 'inline-help__contact-box', 'email' ) }>
-						<div className="inline-help__contact-box-icon">
-							<Gridicon icon={ 'mail' } />
-						</div>
-						<div>
-							<h2>{ __( 'Email' ) }</h2>
-							<p>{ __( 'An expert will get back to you soon' ) }</p>
-						</div>
-					</button>
+						<h2>{ __( 'Email' ) }</h2>
+						<p>{ __( 'An expert will get back to you soon' ) }</p>
+					</div>
 				</div>
 			</div>
 			<InlineHelpSearchResults onSelect={ onSelectResource } placeholderLines={ 4 } />
