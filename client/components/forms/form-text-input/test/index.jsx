83eb60e1de01e4d0579668923b8cadc603ecc689
Markus Dobmann <m.do@posteo.net>
2022-05-03 16:19:47 +0200

    chai → jest

--- client/components/forms/form-text-input/test/index.jsx
+++ client/components/forms/form-text-input/test/index.jsx
@@ -1,35 +1,33 @@
-import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import { spy } from 'sinon';
 import FormTextInput from '../';
 
 describe( '<FormTextInput />', () => {
 	test( 'should add the provided class names', () => {
 		const wrapper = shallow( <FormTextInput className="test" isError isValid /> );
 
-		expect( wrapper.hasClass( 'test' ) ).to.equal( true );
-		expect( wrapper.hasClass( 'is-error' ) ).to.equal( true );
-		expect( wrapper.hasClass( 'is-valid' ) ).to.equal( true );
+		expect( wrapper.hasClass( 'test' ) ).toBe( true );
+		expect( wrapper.hasClass( 'is-error' ) ).toBe( true );
+		expect( wrapper.hasClass( 'is-valid' ) ).toBe( true );
 	} );
 
 	test( 'should have form-text-input class name', () => {
 		const wrapper = shallow( <FormTextInput /> );
 
-		expect( wrapper.hasClass( 'form-text-input' ) ).to.equal( true );
+		expect( wrapper.hasClass( 'form-text-input' ) ).toBe( true );
 	} );
 
 	test( "should not pass component's own props down to the input", () => {
 		const wrapper = shallow( <FormTextInput isValid isError selectOnFocus /> );
 
-		expect( wrapper.prop( 'isValid' ) ).to.equal( undefined );
-		expect( wrapper.prop( 'isError' ) ).to.equal( undefined );
-		expect( wrapper.prop( 'selectOnFocus' ) ).to.equal( undefined );
+		expect( wrapper.prop( 'isValid' ) ).toBeUndefined();
+		expect( wrapper.prop( 'isError' ) ).toBeUndefined();
+		expect( wrapper.prop( 'selectOnFocus' ) ).toBeUndefined();
 	} );
 
 	test( "should pass props aside from component's own to the input", () => {
 		const wrapper = shallow( <FormTextInput placeholder="test placeholder" /> );
 
-		expect( wrapper.prop( 'placeholder' ) ).to.equal( 'test placeholder' );
+		expect( wrapper.prop( 'placeholder' ) ).toBe( 'test placeholder' );
 	} );
 
 	test( 'should call select if selectOnFocus is true', () => {
@@ -40,10 +38,12 @@ describe( '<FormTextInput />', () => {
 			},
 		};
 
-		spy( event.target, 'select' );
+		const spy = jest.spyOn( event.target, 'select' );
 		wrapper.simulate( 'click', event );
 
-		expect( event.target.select ).to.have.been.calledOnce;
+		expect( spy ).toHaveBeenCalledTimes( 1 );
+
+		spy.mockRestore();
 	} );
 
 	test( 'should not call select if selectOnFocus is false', () => {
@@ -54,9 +54,10 @@ describe( '<FormTextInput />', () => {
 			},
 		};
 
-		spy( event.target, 'select' );
+		const spy = jest.spyOn( event.target, 'select' );
 		wrapper.simulate( 'click', event );
 
-		expect( event.target.select ).to.not.have.been.called;
+		expect( spy ).not.toHaveBeenCalled();
+		spy.mockRestore();
 	} );
 } );
