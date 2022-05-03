d5e9ca94c4fd09ac844b6fa78621ae577afb3b09
Praveen <spraveenitpro@gmail.com>
2022-05-02 20:58:13 -0700

    Explicitly assert to ensure that the element is also visible

--- client/blocks/follow-button/test/button.js
+++ client/blocks/follow-button/test/button.js
@@ -2,12 +2,13 @@
  * @jest-environment jsdom
  */
 
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import FollowButton from '../button';
 
 describe( 'FollowButton', () => {
 	test( 'should apply a custom follow label', () => {
 		render( <FollowButton followLabel="Follow Tag" /> );
-		expect( screen.getByText( 'Follow Tag' ) );
+		expect( screen.getByText( 'Follow Tag' ) ).toBeVisible();
 	} );
 } );
