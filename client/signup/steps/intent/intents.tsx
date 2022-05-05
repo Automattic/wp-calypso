ab57895d6c8dbf97d2ee90477c57d3b527967560
Aneesh Devasthale <aneeshdevasthale@gmail.com>
2022-05-05 10:55:32 +0530

    Hide DIFM intent behind feature flag

--- client/signup/steps/intent/intents.tsx
+++ client/signup/steps/intent/intents.tsx
@@ -73,7 +73,7 @@ export const useIntentsAlt = ( canImport: boolean ): IntentAlt[] => {
 			),
 		},
 		{
-			show: true,
+			show: isEnabled( 'onboarding/difm' ),
 			key: 'difm',
 			description: translate( 'Let our experts create your dream site' ),
 			value: 'difm',
