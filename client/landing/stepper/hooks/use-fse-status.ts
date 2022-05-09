e0248d4643cedab7b9dd9abe8506c480350da840
Omar Alshaker <omar@omaralshaker.com>
2022-05-09 13:06:25 +0200

    Reimplement USEFseStatus hook

--- client/landing/stepper/hooks/use-fse-status.ts
+++ client/landing/stepper/hooks/use-fse-status.ts
@@ -1,10 +1,30 @@
+import { useQuery } from 'react-query';
+import wpcomRequest from 'wpcom-proxy-request';
 import { useSite } from './use-site';
+
+type Response = {
+	is_fse_eligible: boolean;
+	is_fse_active: boolean;
+};
 
 export function useFSEStatus() {
 	const site = useSite();
-	const FSEEligible = site?.is_fse_eligible;
-	const FSEActive = site?.is_fse_active;
-	const isLoading = !! site;
 
-	return { FSEEligible, FSEActive, isLoading };
+	const { data, isLoading } = useQuery< Response >(
+		site?.ID.toString() ?? 'no-site',
+		() =>
+			wpcomRequest( {
+				path: `/sites/${ site?.ID }/block-editor`,
+				apiNamespace: 'wpcom/v2',
+			} ),
+		{
+			enabled: !! site,
+		}
+	);
+
+	return {
+		FSEEligible: Boolean( data?.is_fse_eligible ),
+		FSEActive: Boolean( data?.is_fse_active ),
+		isLoading,
+	};
 }
