2601f71deadc91a29a242e16c7f185e67c30c791
sixhours <calobee@gmail.com>
2022-05-09 11:41:26 -0400

    Add test for usePodcastTitle

---
+++ client/landing/stepper/hooks/test/use-podcast-title.ts
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom/extend-expect';
+import { renderHook } from '@testing-library/react-hooks';
+import { usePodcastTitle } from '../use-podcast-title';
+import * as hook from '../use-podcast-title';
+
+jest.mock( '../use-podcast-title' );
+
+describe( 'use-podcast-title test', () => {
+	test( 'Returns the correct title from the response', () => {
+		const spy = jest.spyOn( hook, 'usePodcastTitle' ).mockReturnValue( 'My Podcast Title' );
+		const title = renderHook( () => usePodcastTitle() );
+
+		expect( spy ).toHaveBeenCalled();
+		expect( title.result.current ).toBe( 'My Podcast Title' );
+
+		spy.mockRestore();
+	} );
+} );
