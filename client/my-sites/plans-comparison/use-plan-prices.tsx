3eab11ffd5b491f680ae2ddd721442749eac0fc6
Christos <chriskmnds@gmail.com>
2022-05-11 15:21:17 +0300

    clean up potentially redundant check

--- client/my-sites/plans-comparison/use-plan-prices.tsx
+++ client/my-sites/plans-comparison/use-plan-prices.tsx
@@ -1,4 +1,3 @@
-// import { TYPE_FREE, TYPE_FLEXIBLE } from '@automattic/calypso-products';
 import { useSelector } from 'react-redux';
 import { getPlanRawPrice, getDiscountedRawPrice } from 'calypso/state/plans/selectors';
 import type { WPComPlan } from '@automattic/calypso-products';
@@ -15,12 +14,6 @@ export default function usePlanPrices( plans: WPComPlan[] ): PlanPrices[] {
 	return useSelector( ( state ) => {
 		return plans.map( ( plan ) => {
 			const productId = plan.getProductId();
-
-			// @todo clk
-			// if ( plan.type === TYPE_FREE || plan.type === TYPE_FLEXIBLE ) {
-			// 	return [ 0 ];
-			// }
-
 			const [ price, discountPrice ] = [
 				getPlanRawPrice( state, productId ),
 				getDiscountedRawPrice( state, productId ),
