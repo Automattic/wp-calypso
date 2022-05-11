32c34b7ab1c5f3a7f55cc1be98dac67e338e4d86
Daniel López <daniel.lopez@automattic.com>
2022-05-11 13:43:39 +0200

    Emails: When the email forward verification happens, we are redirecting to a wrong URL

--- client/my-sites/email/controller.js
+++ client/my-sites/email/controller.js
@@ -10,7 +10,7 @@ import { castIntervalLength } from 'calypso/my-sites/email/email-providers-compa
 import EmailProvidersStackedComparisonPage from 'calypso/my-sites/email/email-providers-stacked-comparison/page';
 import GSuiteAddUsers from 'calypso/my-sites/email/gsuite-add-users';
 import InboxManagement from 'calypso/my-sites/email/inbox';
-import { emailManagement } from 'calypso/my-sites/email/paths';
+import { emailManagementEdit } from 'calypso/my-sites/email/paths';
 import TitanAddMailboxes from 'calypso/my-sites/email/titan-add-mailboxes';
 import TitanSetUpMailbox from 'calypso/my-sites/email/titan-set-up-mailbox';
 import TitanSetUpThankYou from 'calypso/my-sites/email/titan-set-up-thank-you';
@@ -146,7 +146,9 @@ export default {
 	},
 
 	emailManagementForwardingRedirect( pageContext ) {
-		page.redirect( emailManagement( pageContext.params.site, pageContext.params.domain ) );
+		page.redirect(
+			emailManagementEdit( pageContext.params.site, pageContext.params.domain, 'edit' )
+		);
 	},
 
 	emailManagement( pageContext, next ) {
