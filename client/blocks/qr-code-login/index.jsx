a0d650828f9d30e639d5c16c584444dbaf6aa8e7
Enej Bajgoric <enej.bajgoric@automattic.com>
2022-05-10 21:45:47 -0700

    Move QR-code component to a QR code block.

---
+++ client/blocks/qr-code-login/index.jsx
@@ -0,0 +1,198 @@
+import { getTracksAnonymousUserId } from '@automattic/calypso-analytics';
+import { Card } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import QRCode from 'qrcode.react';
+import { useEffect, useState, useRef } from 'react';
+import qrCenter from 'calypso/assets/images/qr-login/wp.png';
+
+import './style.scss';
+
+const AUTH_PULL_INTERVAL = 5000; // 5 seconds
+const LOCALE_STORAGE_KEY = 'qr-login-token';
+
+const isStillValidToken = ( tokenData ) => {
+	if ( ! tokenData ) {
+		return false;
+	}
+	const { expires } = tokenData;
+	if ( ! expires ) {
+		return false;
+	}
+	return expires > Date.now() / 1000;
+};
+
+const setLocalTokenData = ( data ) => {
+	try {
+		window.localStorage.setItem( LOCALE_STORAGE_KEY, JSON.stringify( data ) );
+	} catch ( e ) {}
+};
+
+const getLocalTokenData = () => {
+	try {
+		const valueString = window.localStorage.getItem( LOCALE_STORAGE_KEY );
+		if ( valueString === undefined || valueString === null ) {
+			return false;
+		}
+
+		const tokenData = JSON.parse( valueString );
+		if ( isStillValidToken( tokenData ) ) {
+			return tokenData;
+		}
+	} catch ( e ) {}
+
+	return false;
+};
+
+const getLoginActionResponse = async ( action, args ) => {
+	const url = new URL( 'https://wordpress.com/wp-login.php' );
+	url.searchParams.append( 'action', action );
+
+	Object.keys( args ).forEach( ( key ) => {
+		url.searchParams.append( key, args[ key ] );
+	} );
+
+	const response = await fetch( url.href );
+	return await response.json();
+};
+
+const fetchQRCodeData = async ( setTokenData, tokenData, anonymousUserId ) => {
+	if ( isStillValidToken( tokenData ) ) {
+		return tokenData;
+	}
+
+	const responseData = await getLoginActionResponse( 'qr-code-token-request-endpoint', {
+		anon_id: anonymousUserId,
+	} );
+
+	setTokenData( responseData.data );
+	setLocalTokenData( responseData.data );
+};
+
+const fetchAuthState = async ( setAuthState, setTokenData, tokenData, anonymousUserId ) => {
+	if ( ! tokenData ) {
+		return;
+	}
+
+	if ( ! isStillValidToken( tokenData ) ) {
+		fetchQRCodeData( setTokenData, tokenData, anonymousUserId );
+		return;
+	}
+
+	const { token, encrypted } = tokenData;
+
+	const responseData = await getLoginActionResponse( 'qr-code-authentication-endpoint', {
+		anon_id: anonymousUserId,
+		token: token,
+		data: encrypted,
+	} );
+
+	setAuthState( responseData.data );
+};
+
+function TokenQRCode( { tokenData } ) {
+	if ( ! tokenData ) {
+		return <QRCodePlaceholder />;
+	}
+	const { token, encrypted } = tokenData;
+	const imageSettings = {
+		src: qrCenter,
+		height: 64,
+		width: 64,
+		excavate: false,
+	};
+	return (
+		<QRCode
+			value={ `https://apps.wordpress.com/get/?campaign=login-qr-code#qr-code-login?token=${ token }&data=${ encrypted }` }
+			size={ 352 }
+			imageSettings={ imageSettings }
+		/>
+	);
+}
+
+function QRCodePlaceholder() {
+	return (
+		<div className="qr-code-login__placeholder">
+			<span className="qr-code-login__corner-box"></span>
+			<span className="qr-code-login__corner-box"></span>
+			<span className="qr-code-login__corner-box"></span>
+		</div>
+	);
+}
+
+function QRCodeLogin() {
+	const translate = useTranslate();
+	const [ tokenData, setTokenData ] = useState( getLocalTokenData() );
+	const [ authState, setAuthState ] = useState( false );
+	const currentTimer = useRef( null );
+
+	const anonymousUserId = getTracksAnonymousUserId();
+
+	// Fetch QR code data.
+	useEffect( () => {
+		fetchQRCodeData( setTokenData, tokenData, anonymousUserId );
+	}, [ setTokenData, tokenData, anonymousUserId ] );
+
+	// Fetch the Auth Data.
+	useEffect( () => {
+		clearInterval( currentTimer.current );
+		currentTimer.current = setInterval( () => {
+			fetchAuthState( setAuthState, setTokenData, tokenData, anonymousUserId );
+		}, AUTH_PULL_INTERVAL );
+
+		return () => clearInterval( currentTimer.current );
+	}, [ setAuthState, setTokenData, tokenData, anonymousUserId, currentTimer ] );
+
+	useEffect( () => {
+		if ( ! authState ) {
+			return;
+		}
+		if ( authState.auth_url ) {
+			window.location.replace( authState.auth_url );
+			return;
+		}
+
+		return null;
+	}, [ authState ] );
+
+	const steps = [
+		translate( 'Open the {{link/}}on your phone.', {
+			components: {
+				link: (
+					<a href="https://apps.wordpress.com/get/?campaign=calypso-qrcode-apps">WordPress App</a>
+				),
+			},
+		} ),
+		translate( 'Tap the My Site Tab.' ),
+		translate( 'Tap your Profile image in the top right corner of the screen.' ),
+		translate( 'Tap the {{strong}}Scan QR Code{{/strong}} option.', {
+			components: {
+				strong: <strong />,
+			},
+		} ),
+		translate( 'Point your phone to this screen to scan the code.' ),
+	];
+
+	return (
+		<Card className="qr-code-login">
+			<div className="qr-code-login__container">
+				<TokenQRCode tokenData={ tokenData } />
+			</div>
+
+			<div className="qr-code-login__instructions">
+				<h2 className="qr-code-login__heading">{ translate( 'Use QR Code to login' ) }</h2>
+				<ol className="qr-code-login__steps">
+					{ steps.map( ( step, index ) => (
+						<li key={ 'step-' + index } className="qr-code-login__step">
+							{ step }
+						</li>
+					) ) }
+				</ol>
+				<p>
+					<a href="https://apps.wordpress.com/mobile/login-via-qr-code">Need help?</a>
+				</p>
+			</div>
+		</Card>
+	);
+}
+
+export default QRCodeLogin;
--- client/login/qr-code-login-page/index.jsx
+++ client/login/qr-code-login-page/index.jsx
@@ -7,24 +7,22 @@ import { login } from 'calypso/lib/paths';
 import './style.scss';
 
 function QrCodeLoginPlaceholder() {
-	return <div className="qr-code-login__placeholder"></div>;
+	return <Card className="qr-code-login-page__placeholder"></Card>;
 }
 
 function QrCodeLoginPage( { locale } ) {
 	const translate = useTranslate();
 
 	return (
-		<Main className="qr-code-login">
-			<div className="qr-code-login__form">
-				<h1 className="qr-code-login__heading">{ 'Login via the mobile app' }</h1>
-				<Card className="qr-code-login__card">
-					<AsyncLoad
-						require="calypso/components/qr-code-login"
-						placeholder={ <QrCodeLoginPlaceholder /> }
-						size={ 352 }
-					/>
-				</Card>
-				<div className="qr-code-login__footer">
+		<Main className="qr-code-login-page">
+			<div className="qr-code-login-page__form">
+				<h1 className="qr-code-login-page__heading">{ 'Login via the mobile app' }</h1>
+				<AsyncLoad
+					require="calypso/blocks/qr-code-login"
+					placeholder={ <QrCodeLoginPlaceholder /> }
+					size={ 352 }
+				/>
+				<div className="qr-code-login-page__footer">
 					<a href={ login( { locale } ) }>
 						<Gridicon icon="arrow-left" size={ 18 } />
 						{ translate( 'Enter a password instead' ) }
