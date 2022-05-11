19e312ff0d30dd6b1ec00349563a50c79516286b
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-05-11 23:39:13 +0200

    Create Wix importer step

---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/importer-wix/index.tsx
@@ -0,0 +1,12 @@
+import { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
+import WixImporter from 'calypso/signup/steps/import-from/wix';
+import { withImporterWrapper } from '../importer';
+import './style.scss';
+
+const ImporterWix: Step = function ( props ) {
+	const Importer = withImporterWrapper( WixImporter );
+
+	return <Importer importer={ 'wix' } { ...props } />;
+};
+
+export default ImporterWix;
