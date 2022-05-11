edd494a80e19a6c9ec00fef56e9a3797e204999b
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2022-05-11 14:34:25 +0300

    Domains: Polish mocking in form component tests (#63496)

--- client/my-sites/domains/components/form/test/hidden-input.js
+++ client/my-sites/domains/components/form/test/hidden-input.js
@@ -6,11 +6,21 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { HiddenInput } from '../hidden-input';
 
 describe( 'HiddenInput', () => {
+	let originalScroll;
+	const noop = () => {};
 	const defaultProps = {
 		text: 'Love cannot be hidden.',
 		label: 'Your name',
 	};
-	window.scroll = jest.fn();
+
+	beforeAll( () => {
+		originalScroll = window.scroll;
+		window.scroll = noop;
+	} );
+
+	afterAll( () => {
+		window.scroll = originalScroll;
+	} );
 
 	test( 'it should return expected elements with defaultProps and no props value', () => {
 		render( <HiddenInput { ...defaultProps } /> );
