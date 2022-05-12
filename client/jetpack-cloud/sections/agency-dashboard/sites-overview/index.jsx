bbf40acb896a3eb61d24468b842871f04be096c5
Yashwin <yashwinpoojary@gmail.com>
2022-05-12 17:35:23 +0530

    Add page title and subtitle for agency dashboard

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/index.jsx
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/index.jsx
@@ -1,10 +1,16 @@
+import { useTranslate } from 'i18n-calypso';
 import SiteContent from './site-content';
 
 import './style.scss';
 
 export default function SitesOverview() {
+	const translate = useTranslate();
 	return (
 		<div className="sites-overview">
+			<h2 className="sites-overview__page-title">{ translate( 'Dashboard' ) }</h2>
+			<div className="sites-overview__page-subtitle">
+				{ translate( 'Manage all your Jetpack sites from one location' ) }
+			</div>
 			<SiteContent />
 		</div>
 	);
