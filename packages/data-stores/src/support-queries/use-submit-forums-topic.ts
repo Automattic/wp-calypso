bd81be700d9ff5d4809109a094f94fb724a7a719
Emanuele Buccelli <escapemanuele@gmail.com>
2022-05-16 18:05:08 +0200

    Help Center: Allow forum ticket with no site (#63614)
    Submit forum ticket without site

--- packages/data-stores/src/support-queries/use-submit-forums-topic.ts
+++ packages/data-stores/src/support-queries/use-submit-forums-topic.ts
@@ -3,11 +3,12 @@ import wpcomRequest from 'wpcom-proxy-request';
 import { SiteDetails } from '../site';
 
 type ForumTopic = {
-	site: SiteDetails;
+	site?: SiteDetails;
 	message: string;
 	subject: string;
 	locale: string;
 	hideInfo: boolean;
+	userDeclaredSiteUrl?: string;
 };
 
 type Response = {
@@ -15,35 +16,41 @@ type Response = {
 };
 
 export function useSubmitForumsMutation() {
-	return useMutation( ( { site, message, subject, locale, hideInfo }: ForumTopic ) => {
-		const blogHelpMessages = [ message ];
+	return useMutation(
+		( { site, message, subject, locale, hideInfo, userDeclaredSiteUrl }: ForumTopic ) => {
+			const blogHelpMessages = [];
 
-		if ( site.jetpack ) {
-			blogHelpMessages.push( 'WP.com: Unknown' );
-			blogHelpMessages.push( 'Jetpack: Yes' );
-		} else {
-			blogHelpMessages.push( 'WP.com: Yes' );
+			if ( site ) {
+				if ( site.jetpack ) {
+					blogHelpMessages.push( 'WP.com: Unknown' );
+					blogHelpMessages.push( 'Jetpack: Yes' );
+				} else {
+					blogHelpMessages.push( 'WP.com: Yes' );
+				}
+
+				blogHelpMessages.push( 'Correct account: yes' );
+			} else if ( userDeclaredSiteUrl ) {
+				blogHelpMessages.push( `Self-declared URL: ${ userDeclaredSiteUrl }` );
+			}
+
+			const forumMessage = message + '\n\n' + blogHelpMessages.join( '\n' );
+
+			const requestData = {
+				subject,
+				message: forumMessage,
+				locale,
+				client: 'help-center',
+				hide_blog_info: hideInfo,
+				blog_id: site?.ID,
+				blog_url: site?.URL,
+			};
+
+			return wpcomRequest< Response >( {
+				path: '/help/forums/support/topics/new',
+				apiVersion: '1.1',
+				method: 'POST',
+				body: requestData,
+			} );
 		}
-
-		blogHelpMessages.push( 'Correct account: yes' );
-
-		const forumMessage = message + '\n\n' + 'Correct account: yes';
-
-		const requestData = {
-			subject,
-			message: forumMessage,
-			locale,
-			client: 'help-center',
-			hide_blog_info: hideInfo,
-			blog_id: site.ID,
-			blog_url: site.URL,
-		};
-
-		return wpcomRequest< Response >( {
-			path: '/help/forums/support/topics/new',
-			apiVersion: '1.1',
-			method: 'POST',
-			body: requestData,
-		} );
-	} );
+	);
 }
