732361e27c2b48160a3f859ce22f9efda1c6978c
Rolando Perez <me@rolilink.com>
2022-05-16 16:38:19 -0500

    adding initialActiveIndex prop adds and cleanup stories

--- packages/components/src/responsive-toolbar-group/index.stories.tsx
+++ packages/components/src/responsive-toolbar-group/index.stories.tsx
@@ -1,3 +1,4 @@
+import { action } from '@storybook/addon-actions';
 import ResponsiveToolbarGroup from '.';
 
 import '@wordpress/components/build-style/style.css';
@@ -5,8 +6,51 @@ import '@wordpress/components/build-style/style.css';
 export default { title: 'ResponsiveToolbarGroup' };
 
 export const Normal = () => (
-	<div style={ { width: '50%' } }>
-		<ResponsiveToolbarGroup>
+	<div style={ { width: '100%' } }>
+		<ResponsiveToolbarGroup onClick={ action( 'onClick' ) }>
+			<span> Item #1</span>
+			<span> Item #2</span>
+			<span> Item #3</span>
+			<span> Item #4</span>
+			<span> Item #5</span>
+			<span> Item #6</span>
+		</ResponsiveToolbarGroup>
+	</div>
+);
+
+export const NormalWithSelectedIndex = () => (
+	<div style={ { width: '100%' } }>
+		<ResponsiveToolbarGroup onClick={ action( 'onClick' ) } initialActiveIndex={ 4 }>
+			<span> Item #1</span>
+			<span> Item #2</span>
+			<span> Item #3</span>
+			<span> Item #4</span>
+			<span> Item #5</span>
+			<span> Item #6</span>
+		</ResponsiveToolbarGroup>
+	</div>
+);
+
+export const WithGrouped = () => (
+	<div style={ { width: '30%' } }>
+		<ResponsiveToolbarGroup onClick={ action( 'onClick' ) }>
+			<span> Item #1</span>
+			<span> Item #2</span>
+			<span> Item #3</span>
+			<span> Item #4</span>
+			<span> Item #5</span>
+			<span> Item #6</span>
+			<span> Item #7</span>
+			<span> Item #8</span>
+			<span> Item #9</span>
+			<span> Item #10</span>
+		</ResponsiveToolbarGroup>
+	</div>
+);
+
+export const WithGroupedAndSelectedIndex = () => (
+	<div style={ { width: '30%' } }>
+		<ResponsiveToolbarGroup onClick={ action( 'onClick' ) } initialActiveIndex={ 8 }>
 			<span> Item #1</span>
 			<span> Item #2</span>
 			<span> Item #3</span>
