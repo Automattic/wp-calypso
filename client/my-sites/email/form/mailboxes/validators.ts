36dba32e880fc6b0e22e7283cd717884c63ff794
Olaseni Oluwunmi <277661+olaseni@users.noreply.github.com>
2022-05-16 16:19:19 +0100

    Unify mailbox forms:  Add missing validator methods and improve test coverage (#63079)
    * Add more methods to the mailbox for validation
    
    * Change iteration
    
    * Suggested fixes
    
    * Import password validation logic
    
    * Fix ts error
    
    * Remove validator dependence on form
    
    * Incorporate Google validations for the password field
    
    * Rename existing mailboxes validator
    
    * Freeze
    
    * Improve test coverage for validators
    
    * Add abstract base to remove boilerplate code
    
    * Fix wrong property access and add one more convenience method
    
    * Convert the domain field to a data field. Expose mutable field names
    
    * Refactor variables
    
    * Clarify unit test validation messages

--- client/my-sites/email/form/mailboxes/validators.ts
+++ client/my-sites/email/form/mailboxes/validators.ts
@@ -1,25 +1,40 @@
 import emailValidator from 'email-validator';
 import i18n from 'i18n-calypso';
 import React from 'react';
-import { validatePasswordField } from 'calypso/lib/gsuite/new-users';
-import { MailboxForm } from 'calypso/my-sites/email/form/mailboxes/index';
-import { EmailProvider } from 'calypso/my-sites/email/form/mailboxes/types';
-import type { MailboxFormFieldBase } from 'calypso/my-sites/email/form/mailboxes/types';
+import type { FieldError, MailboxFormFieldBase } from 'calypso/my-sites/email/form/mailboxes/types';
 
 interface Validator< T > {
 	validate( field?: MailboxFormFieldBase< T > ): void;
 }
 
-class RequiredValidator< T > implements Validator< T > {
+abstract class BaseValidator< T > implements Validator< T > {
 	validate( field?: MailboxFormFieldBase< T > ): void {
-		if ( ! field || field.error ) {
+		if ( ! field || field.hasError() ) {
 			return;
 		}
 
-		const requiredFieldError = i18n.translate( 'This field is required.' );
+		this.validateField( field );
+	}
+
+	abstract validateField( field: MailboxFormFieldBase< T > ): void;
+}
+
+class RequiredValidator< T > extends BaseValidator< T > {
+	static getRequiredFieldError(): FieldError {
+		return i18n.translate( 'This field is required.' );
+	}
+
+	validateField( field: MailboxFormFieldBase< T > ): void {
+		if ( ! field.isRequired ) {
+			return;
+		}
+
+		const requiredFieldError = RequiredValidator.getRequiredFieldError();
 
 		if ( ! field.value ) {
 			field.error = requiredFieldError;
+
+			return;
 		}
 
 		if ( typeof field.value === 'string' && field.value.trim() === '' ) {
@@ -28,108 +43,238 @@ class RequiredValidator< T > implements Validator< T > {
 	}
 }
 
-class MailboxNameValidator< T extends EmailProvider > implements Validator< string > {
-	form: MailboxForm< T >;
-
-	constructor( form: MailboxForm< T > ) {
-		this.form = form;
-	}
-
-	validate( field?: MailboxFormFieldBase< string > ): void {
-		if ( ! field || field.error ) {
+class RequiredIfVisibleValidator extends RequiredValidator< string > {
+	validateField( field: MailboxFormFieldBase< string > ): void {
+		if ( ! field.isVisible ) {
 			return;
 		}
 
-		if ( ! /^[\da-z_-](\.?[\da-z_-])*$/i.test( field.value ) ) {
-			field.error = i18n.translate(
-				'Only numbers, letters, dashes, underscores, and periods are allowed.'
+		super.validateField( field );
+	}
+}
+
+class MaximumStringLengthValidator extends BaseValidator< string > {
+	private readonly maximumStringLength: number;
+
+	constructor( maximumStringLength: number ) {
+		super();
+		this.maximumStringLength = maximumStringLength;
+	}
+
+	static getFieldTooLongError( maximumStringLength: number ): FieldError {
+		return i18n.translate( "This field can't be longer than %s characters.", {
+			args: maximumStringLength,
+		} );
+	}
+
+	validateField( field: MailboxFormFieldBase< string > ): void {
+		if ( this.maximumStringLength < ( field.value?.length ?? 0 ) ) {
+			field.error = MaximumStringLengthValidator.getFieldTooLongError( this.maximumStringLength );
+		}
+	}
+}
+
+class MailboxNameValidator extends BaseValidator< string > {
+	areApostrophesSupported: boolean;
+	domainName: string;
+	mailboxHasDomainError: boolean;
+
+	constructor(
+		domainName: string,
+		mailboxHasDomainError: boolean,
+		areApostrophesSupported: boolean
+	) {
+		super();
+		this.domainName = domainName;
+		this.mailboxHasDomainError = mailboxHasDomainError;
+		this.areApostrophesSupported = areApostrophesSupported;
+	}
+
+	static getInvalidEmailError(): FieldError {
+		return i18n.translate( 'Please supply a valid email address.' );
+	}
+
+	static getUnsupportedCharacterError( supportsApostrophes: boolean ): FieldError {
+		return supportsApostrophes
+			? i18n.translate(
+					'Only numbers, letters, dashes, underscores, apostrophes and periods are allowed.'
+			  )
+			: i18n.translate( 'Only numbers, letters, dashes, underscores, and periods are allowed.' );
+	}
+
+	validateField( field: MailboxFormFieldBase< string > ): void {
+		const regex = this.areApostrophesSupported
+			? /^[\da-z_'-](\.?[\da-z_'-])*$/i
+			: /^[\da-z_-](\.?[\da-z_-])*$/i;
+
+		if ( ! regex.test( field.value ) ) {
+			field.error = MailboxNameValidator.getUnsupportedCharacterError(
+				this.areApostrophesSupported
 			);
 			return;
 		}
 
-		const domain = this.form?.formFields?.domain ?? null;
-
 		if (
-			domain &&
-			! domain.error &&
-			domain.value &&
-			! emailValidator.validate( `${ field.value }@${ domain.value }` )
+			this.domainName &&
+			! this.mailboxHasDomainError &&
+			! emailValidator.validate( `${ field.value }@${ this.domainName }` )
 		) {
-			field.error = i18n.translate( 'Please supply a valid email address.' );
+			field.error = MailboxNameValidator.getInvalidEmailError();
 			return;
 		}
 	}
 }
 
-class AlternateEmailValidator< T extends EmailProvider > implements Validator< string > {
-	form: MailboxForm< T >;
+class AlternateEmailValidator extends BaseValidator< string > {
+	domainName: string;
 
-	constructor( form: MailboxForm< T > ) {
-		this.form = form;
+	constructor( domainName: string ) {
+		super();
+		this.domainName = domainName;
 	}
 
-	validate( field?: MailboxFormFieldBase< string > ): void {
-		if ( ! field || field.error || ! field.value || field.value.trim() === '' ) {
+	static getInvalidEmailError(): FieldError {
+		return i18n.translate( 'Please supply a valid email address.' );
+	}
+
+	static getSameDomainError( domainName: string ): FieldError {
+		return i18n.translate(
+			'This email address must have a different domain than {{strong}}%(domain)s{{/strong}}. Please use a different email address.',
+			{
+				args: {
+					domain: domainName,
+				},
+				components: {
+					strong: React.createElement( 'strong' ),
+				},
+			}
+		);
+	}
+
+	validateField( field: MailboxFormFieldBase< string > ): void {
+		if ( ! field.value || field.value.trim() === '' ) {
 			return;
 		}
 
 		const value = `${ field.value }`;
 
 		if ( ! emailValidator.validate( value ) ) {
-			field.error = i18n.translate( 'Please supply a valid email address.' );
+			field.error = AlternateEmailValidator.getInvalidEmailError();
 
 			return;
 		}
 
 		const parts = `${ value }`.split( '@' );
-		const domain = this.form?.formFields?.domain?.value ?? null;
 
-		if ( domain && parts.length > 1 && parts[ 1 ].toLowerCase() === domain?.toLowerCase() ) {
-			field.error = i18n.translate(
-				'This email address must have a different domain than {{strong}}%(domain)s{{/strong}}. Please use a different email address.',
-				{
-					args: {
-						domain: domain,
-					},
-					components: {
-						strong: React.createElement( 'strong' ),
-					},
-				}
-			);
+		if (
+			this.domainName &&
+			parts.length > 1 &&
+			parts[ 1 ].toLowerCase() === this.domainName?.toLowerCase()
+		) {
+			field.error = AlternateEmailValidator.getSameDomainError( this.domainName );
 		}
 	}
 }
 
-class PasswordValidator implements Validator< string > {
-	validate( field?: MailboxFormFieldBase< string > ): void {
-		if ( ! field || field.error ) {
+class PasswordValidator extends BaseValidator< string > {
+	private readonly minimumPasswordLength: number;
+	static readonly maximumPasswordLength = 100;
+
+	constructor( minimumPasswordLength: number ) {
+		super();
+		this.minimumPasswordLength = minimumPasswordLength;
+	}
+
+	static getPasswordTooShortError( minimumPasswordLength: number ): FieldError {
+		return i18n.translate( "This field can't be shorter than %s characters.", {
+			args: String( minimumPasswordLength ),
+		} );
+	}
+
+	static getPasswordTooLongError(): FieldError {
+		return i18n.translate( "This field can't be longer than %s characters.", {
+			args: PasswordValidator.maximumPasswordLength,
+		} );
+	}
+
+	static getPasswordStartsWithSpaceError(): FieldError {
+		return i18n.translate( "This field can't start with a white space." );
+	}
+
+	static getPasswordEndsWithSpaceError(): FieldError {
+		return i18n.translate( "This field can't end with a white space." );
+	}
+
+	static getPasswordContainsForbiddenCharacterError(
+		firstForbiddenCharacter: string | undefined
+	): FieldError {
+		return i18n.translate( "This field can't accept '%s' as character.", {
+			args: firstForbiddenCharacter,
+			comment: '%s denotes a single character that is not allowed in this field',
+		} );
+	}
+
+	validateField( field: MailboxFormFieldBase< string > ): void {
+		const value = field.value;
+
+		if ( this.minimumPasswordLength > value.length ) {
+			field.error = PasswordValidator.getPasswordTooShortError( this.minimumPasswordLength );
 			return;
 		}
 
-		const { error } = validatePasswordField( { value: field.value, error: field.error }, 10 );
-		field.error = error;
+		if ( PasswordValidator.maximumPasswordLength < value.length ) {
+			field.error = PasswordValidator.getPasswordTooLongError();
+			return;
+		}
+
+		if ( value.startsWith( ' ' ) ) {
+			field.error = PasswordValidator.getPasswordStartsWithSpaceError();
+			return;
+		}
+
+		// Checks that passwords only have ASCII characters (see https://en.wikipedia.org/wiki/ASCII#Character_set)
+		const regexp = /[^\x20-\x7E]/;
+
+		if ( regexp.test( value ) ) {
+			const firstForbiddenCharacter = [ ...value ].find( ( character ) =>
+				regexp.test( character )
+			);
+
+			field.error =
+				PasswordValidator.getPasswordContainsForbiddenCharacterError( firstForbiddenCharacter );
+			return;
+		}
+
+		if ( value.endsWith( ' ' ) ) {
+			field.error = PasswordValidator.getPasswordEndsWithSpaceError();
+		}
 	}
 }
 
-class ExistingMailboxesValidator< T extends EmailProvider > implements Validator< string > {
-	form: MailboxForm< T >;
+class ExistingMailboxNamesValidator extends BaseValidator< string > {
+	existingMailboxNames: string[];
 
-	constructor( form: MailboxForm< T > ) {
-		this.form = form;
+	constructor( existingMailboxNames: string[] ) {
+		super();
+		this.existingMailboxNames = existingMailboxNames;
 	}
 
-	validate( field?: MailboxFormFieldBase< string > ): void {
-		const existingMailboxes = this.form.existingMailboxes ?? [];
-		if ( ! field || field.error || ! existingMailboxes ) {
+	static getExistingMailboxError(): FieldError {
+		return i18n.translate( 'Please use unique mailboxes.' );
+	}
+
+	validateField( field: MailboxFormFieldBase< string > ): void {
+		const existingMailboxNames = this.existingMailboxNames ?? [];
+		if ( ! existingMailboxNames ) {
 			return;
 		}
 
 		if (
-			existingMailboxes
+			existingMailboxNames
 				.map( ( item ) => item.toLowerCase() )
 				.includes( field.value?.toLowerCase() ?? '' )
 		) {
-			field.error = i18n.translate( 'Please use unique mailboxes' );
+			field.error = ExistingMailboxNamesValidator.getExistingMailboxError();
 		}
 	}
 }
@@ -138,8 +283,10 @@ export type { Validator };
 
 export {
 	AlternateEmailValidator,
-	ExistingMailboxesValidator,
+	ExistingMailboxNamesValidator,
 	MailboxNameValidator,
 	PasswordValidator,
 	RequiredValidator,
+	RequiredIfVisibleValidator,
+	MaximumStringLengthValidator,
 };
