a599ca3f6e44cd1623e65979e19b1912c03f7da8
Matthew Reishus <mreishus@users.noreply.github.com>
2022-05-17 13:29:05 -0500

    Remove stepper disables

--- client/signup/config/flows.js
+++ client/signup/config/flows.js
@@ -73,11 +73,9 @@ function getSignupDestination( { domainItem, siteId, siteSlug }, localeSlug ) {
 		queryParam = { siteId };
 	}
 
-	/*
 	if ( isEnabled( 'signup/stepper-flow' ) ) {
 		return addQueryArgs( queryParam, '/setup/intent' );
 	}
-	*/
 
 	// Initially ship to English users only, then ship to all users when translations complete
 	if ( englishLocales.includes( localeSlug ) || isEnabled( 'signup/hero-flow-non-en' ) ) {
