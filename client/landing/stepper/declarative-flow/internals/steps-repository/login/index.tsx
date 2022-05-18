f22e958a841d9f7f4b791111be954989a1dc8ff2
sixhours <calobee@gmail.com>
2022-05-18 13:47:00 -0400

    Begin adding a login step to the Stepper framework

---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/login/index.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+import { Button } from '@automattic/components';
+import { StepContainer } from '@automattic/onboarding';
+import { useSelect } from '@wordpress/data';
+import { useI18n } from '@wordpress/react-i18n';
+import { useEffect } from 'react';
+import { USER_STORE } from 'calypso/landing/stepper/stores';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import type { Step } from '../../types';
+import '../style.scss';
+
+const LoginStep: Step = function LoginStep( { navigation } ) {
+	const { submit, goNext } = navigation;
+	const { __ } = useI18n();
+	const currentUser = useSelect( ( select ) => select( USER_STORE ).getCurrentUser() );
+
+	const LoginForm: React.FC = () => {
+		const handleSubmit = () => {
+			submit?.();
+		};
+
+		return (
+			<form
+				className="login__form"
+				onSubmit={ () => {
+					handleSubmit?.();
+				} }
+			>
+				<Button className="login__submit-button" type="submit" primary>
+					{ __( 'Continue' ) }
+				</Button>
+			</form>
+		);
+	};
+
+	//If we have a current user, proceed to the next step; no need to log in.
+	useEffect( () => {
+		if ( currentUser ) {
+			goNext?.();
+		}
+	}, [ currentUser ] );
+
+	return (
+		<StepContainer
+			stepName={ 'login-step' }
+			hideBack
+			hideSkip
+			hideNext
+			stepContent={ <LoginForm /> }
+			recordTracksEvent={ recordTracksEvent }
+		/>
+	);
+};
+
+export default LoginStep;
