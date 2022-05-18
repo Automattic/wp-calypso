1edb8671458669c2f740c3bf182ccdaa2fe03877
Chris R <chris.rosser@automattic.com>
2022-05-19 10:10:46 +1200

    Remove redundant true on primary prop
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>

--- client/my-sites/people/viewers-list/invite-button.jsx
+++ client/my-sites/people/viewers-list/invite-button.jsx
@@ -3,7 +3,7 @@ import { localize } from 'i18n-calypso';
 
 const InviteButton = ( { siteSlug, translate } ) => {
 	return (
-		<Button primary={ true } href={ `/people/new/${ siteSlug }` }>
+		<Button primary href={ `/people/new/${ siteSlug }` }>
 			<Gridicon icon="user-add" />
 			<span>{ translate( 'Invite', { context: 'Verb. Button to invite more users.' } ) }</span>
 		</Button>
