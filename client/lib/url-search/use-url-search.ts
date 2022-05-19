1b0d8e46e49d06fa841ba9ebaa84504eaf7e8ffa
Paulo Trentin <paulo@paulotrentin.com.br>
2022-05-19 14:20:23 -0300

    Improved use-url-search: - Fixed tests - Added empty query behavior - Improved docs #63013

--- client/lib/url-search/use-url-search.ts
+++ client/lib/url-search/use-url-search.ts
@@ -1,46 +1,33 @@
 import { getQueryArgs } from '@wordpress/url';
 import debugFactory from 'debug';
 import page from 'page';
+import { useState } from 'react';
 const debug = debugFactory( 'calypso:url-search' );
 
-/**
- * Function for constructing the url to page to. Here are some examples:
- * 1. { uri: 'https://wordpress.com/plugins', search:'hello' } --> '/plugins?s=hello'
- * 2. {
- *     uri: 'https://wordpress.com/read/search?q=reader+is+awesome',
- *     search: 'reader is super awesome'
- *     queryKey: 'q',
- *    } --> '/read/search?q=reader+is+super+awesome'
- *
- * @param {string} defaultSearchKey Default search key to add search string to
- * @param {string | object} querySearch search string or search object
- * If a string is provided on querySearch, the default search key 's' will be used
- * If an object is provided, every object key found in the URL will be replaced.
- * @returns {string} The built search url
- */
 function buildSearchUrl( defaultSearchKey: string, querySearch: string | object ): string {
 	const urlSearch = new URLSearchParams( window.location.search );
+	let returnPath = window.location.pathname;
 
-	if ( typeof querySearch === 'string' ) {
-		urlSearch.set( defaultSearchKey, querySearch );
-	} else {
-		for ( const [ key, value ] of Object.entries( querySearch ) ) {
-			urlSearch.set( key, value );
+	if ( querySearch ) {
+		if ( typeof querySearch === 'string' ) {
+			urlSearch.set( defaultSearchKey, querySearch );
+		} else {
+			for ( const [ key, value ] of Object.entries( querySearch ) ) {
+				urlSearch.set( key, value );
+			}
 		}
+
+		returnPath += '?' + urlSearch.toString();
 	}
 
-	return window.location.pathname + '?' + urlSearch.toString();
+	return returnPath;
 }
 
 function useUrlSearch() {
-	/**
-	 * Performs URL search by adding or replacing terms in the URL
-	 *
-	 * @param querySearch search string or search object
-	 * If a string is provided on querySearch, the default search key 's' will be used
-	 * If an object is provided, every object key found in the URL will be replaced
-	 */
+	const [ isSearchOpen, setSearchOpen ] = useState( false );
+
 	function doSearch( querySearch: string | object ) {
+		setSearchOpen( '' !== querySearch );
 		debug( 'search for: %s', querySearch );
 
 		const defaultSearchKey = 's';
@@ -50,8 +37,41 @@ function useUrlSearch() {
 		page.replace( searchWithoutBaseURL );
 	}
 
+	function getSearchOpen() {
+		return isSearchOpen;
+	}
+
 	return {
+		/**
+		 * Return true if the last querySearch has anything different from empty string
+		 */
+		getSearchOpen,
+
+		/**
+		 * Performs URL search by adding or replacing terms in the URL
+		 * 1. { uri: 'https://wordpress.com/plugins', querySearch:'hello' } --> '/plugins?s=hello'
+		 * 2. {
+		 *     uri: 'https://wordpress.com/read/search?q=reader+is+awesome',
+		 *     querySearch: '{ q: "reader is super awesome" }'
+		 *    } --> '/read/search?q=reader+is+super+awesome'
+		 * 3. {
+		 *     uri: 'https://wordpress.com/read/search',
+		 *     querySearch: '{ s: seo, c: "category" }'
+		 *    } --> '/read/search?s=seo&c=category'
+		 *
+		 * @param querySearch search string or search object
+		 * If a string is provided on querySearch, the default search key 's' will be used
+		 * If an object is provided, every object key found in the URL will be replaced
+		 */
 		doSearch,
+
+		/**
+		 * Get query args from a URL
+		 *
+		 * @returns object Object containing query args or empty object if doesn't have any
+		 * 1. { uri: 'https://wordpress.com/plugins?s=hello' } --> { s: 'hello' }
+		 * 2. { uri: 'https://wordpress.com/plugins?s=seo&c=category' } --> { s: 'seo', c: 'category' }
+		 */
 		getQueryArgs: () => getQueryArgs( window.location.href ),
 	};
 }
