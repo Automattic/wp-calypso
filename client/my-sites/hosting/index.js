68144154d2787168cc0d05ad416faee78e129226
Konstantin Obenland <obenland@gmx.de>
2022-05-23 13:19:16 -0700

    Redirect Jetpack sites

--- client/my-sites/hosting/index.js
+++ client/my-sites/hosting/index.js
@@ -1,16 +1,25 @@
 import page from 'page';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 import { navigation, siteSelection, sites } from 'calypso/my-sites/controller';
-import { layout, activationLayout } from './controller';
+import { handleHostingPanelRedirect, layout, activationLayout } from './controller';
 
 export default function () {
 	page( '/hosting-config', siteSelection, sites, makeLayout, clientRender );
-	page( '/hosting-config/:site_id', siteSelection, navigation, layout, makeLayout, clientRender );
+	page(
+		'/hosting-config/:site_id',
+		siteSelection,
+		navigation,
+		handleHostingPanelRedirect,
+		layout,
+		makeLayout,
+		clientRender
+	);
 
 	page(
 		'/hosting-config/activate/:site_id',
 		siteSelection,
 		navigation,
+		handleHostingPanelRedirect,
 		activationLayout,
 		makeLayout,
 		clientRender
