4bd1e55047955736f0407e05db23f781fb7fb68d
Konstantin Obenland <obenland@gmx.de>
2022-05-23 12:20:05 -0700

    Hosting: Show upgrade banner to WoA sites on low-tier plans

--- client/my-sites/hosting/controller.js
+++ client/my-sites/hosting/controller.js
@@ -1,46 +1,6 @@
-import page from 'page';
 import { createElement } from 'react';
-import canSiteViewAtomicHosting from 'calypso/state/selectors/can-site-view-atomic-hosting';
-import { fetchSitePlans } from 'calypso/state/sites/plans/actions';
-import { getCurrentPlan } from 'calypso/state/sites/plans/selectors';
-import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import HostingActivate from './hosting-activate';
 import Hosting from './main';
-
-function waitForState( context ) {
-	return new Promise( ( resolve ) => {
-		const unsubscribe = context.store.subscribe( () => {
-			const state = context.store.getState();
-
-			const siteId = getSelectedSiteId( state );
-			if ( ! siteId ) {
-				return;
-			}
-
-			const currentPlan = getCurrentPlan( state, siteId );
-			if ( ! currentPlan ) {
-				return;
-			}
-			unsubscribe();
-			resolve();
-		} );
-		// Trigger a `store.subscribe()` callback
-		context.store.dispatch( fetchSitePlans( getSelectedSiteId( context.store.getState() ) ) );
-	} );
-}
-
-export async function handleHostingPanelRedirect( context, next ) {
-	const { store } = context;
-	await waitForState( context );
-	const state = store.getState();
-
-	if ( canSiteViewAtomicHosting( state ) ) {
-		next();
-		return;
-	}
-
-	page.redirect( '/hosting-config' );
-}
 
 export function layout( context, next ) {
 	context.primary = createElement( Hosting );
