d0d0cb14d72f1c1baaadef293dcfce6163150247
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-05-23 13:00:20 +0200

    Integrate static capture screen into light import flow

--- client/landing/stepper/declarative-flow/internals/steps-repository/import-light/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/import-light/index.tsx
@@ -1,13 +1,33 @@
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+import { StepContainer } from '@automattic/onboarding';
+import { useI18n } from '@wordpress/react-i18n';
 import React from 'react';
-import { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
-import { ImportWrapper } from '../import';
+import Capture from 'calypso/blocks/import-light/capture';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import type { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
 import './style.scss';
 
 const ImportLight: Step = function ImportStep( props ) {
+	const { __ } = useI18n();
+	const { navigation } = props;
+
+	function renderStepContent() {
+		return <Capture />;
+	}
+
 	return (
-		<ImportWrapper { ...props }>
-			<div>Import Light</div>
-		</ImportWrapper>
+		<StepContainer
+			stepName={ 'import-step' }
+			className={ 'import__onboarding-page' }
+			hideSkip={ false }
+			hideFormattedHeader={ true }
+			goBack={ navigation.goBack }
+			goNext={ navigation.goNext }
+			skipLabelText={ __( 'Skip this step' ) }
+			isFullLayout={ true }
+			stepContent={ renderStepContent() }
+			recordTracksEvent={ recordTracksEvent }
+		/>
 	);
 };
 
