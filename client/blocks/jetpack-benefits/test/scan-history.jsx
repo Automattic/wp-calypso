e6068a91aae847c00a4b432709ec0f4810c3e8ca
Markus Dobmann <m.do@posteo.net>
2022-05-23 16:17:21 +0200

    jetbackBenefits/scan-history: migrate tests to `@testing-library`

--- client/blocks/jetpack-benefits/test/scan-history.jsx
+++ client/blocks/jetpack-benefits/test/scan-history.jsx
@@ -1,6 +1,7 @@
-import { shallow } from 'enzyme';
-import { JetpackBenefitsCard } from 'calypso/blocks/jetpack-benefits/benefit-card';
-import { JetpackBenefitsStandaloneCard } from 'calypso/blocks/jetpack-benefits/standalone-benefit-card';
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react';
 import getSiteScanState from 'calypso/state/selectors/get-site-scan-state';
 import isRequestingJetpackScan from 'calypso/state/selectors/is-requesting-jetpack-scan';
 import JetpackBenefitsScanHistory from '../scan-history';
@@ -17,11 +18,6 @@ jest.mock( 'calypso/state/selectors/is-requesting-jetpack-scan-threat-counts' );
 jest.mock( 'calypso/state/selectors/get-site-scan-progress' );
 
 describe( 'Scan History Jetpack Benefit Block', () => {
-	const getBenefitsCard = () => {
-		const element = shallow( <JetpackBenefitsScanHistory /> );
-		return element.find( 'JetpackBenefitsScanHistory' ).dive().find( JetpackBenefitsCard );
-	};
-
 	beforeEach( () => {
 		getSiteScanState.mockReset();
 		isRequestingJetpackScan.mockReset();
@@ -30,43 +26,59 @@ describe( 'Scan History Jetpack Benefit Block', () => {
 	test( 'When the scan state is actively scanning, show a scanning message', () => {
 		const scanState = { state: 'scanning' };
 		getSiteScanState.mockReturnValue( scanState );
-		expect( getBenefitsCard().props() ).toHaveProperty( 'jestMarker', 'scanning' );
+
+		render( <JetpackBenefitsScanHistory /> );
+		const card = screen.getByTestId( 'scanning' );
+
+		expect( card ).toBeInTheDocument();
 	} );
 
 	test( 'When the scan state is provisioning, show a preparing message', () => {
 		const scanState = { state: 'provisioning' };
 		getSiteScanState.mockReturnValue( scanState );
-		expect( getBenefitsCard().props() ).toHaveProperty( 'jestMarker', 'provisioning' );
+
+		render( <JetpackBenefitsScanHistory /> );
+		const card = screen.getByTestId( 'provisioning' );
+
+		expect( card ).toBeInTheDocument();
 	} );
 
 	test( 'When the scan state is still loading, show a loading placeholder', () => {
 		getSiteScanState.mockReturnValue( null );
 		isRequestingJetpackScan.mockReturnValue( true );
-		expect( getBenefitsCard().props() ).toHaveProperty( 'jestMarker', 'loading' );
+
+		render( <JetpackBenefitsScanHistory /> );
+		const card = screen.getByTestId( 'loading' );
+
+		expect( card ).toBeInTheDocument();
 	} );
 
 	test( 'When the scan state is not present and not loading, show an error', () => {
 		getSiteScanState.mockReturnValue( null );
 		isRequestingJetpackScan.mockReturnValue( false );
-		expect( getBenefitsCard().props() ).toHaveProperty( 'jestMarker', 'error' );
+
+		render( <JetpackBenefitsScanHistory /> );
+		const card = screen.getByTestId( 'error' );
+
+		expect( card ).toBeInTheDocument();
 	} );
 
 	test( 'When the scan state is idle, but there is not a recent scan, indicate that a scan is scheduled', () => {
 		const scanState = { state: 'idle' };
 		getSiteScanState.mockReturnValue( scanState );
 		isRequestingJetpackScan.mockReturnValue( false );
-		expect( getBenefitsCard().props() ).toHaveProperty( 'jestMarker', 'scheduled' );
+
+		render( <JetpackBenefitsScanHistory /> );
+		const card = screen.getByTestId( 'scheduled' );
+
+		expect( card ).toBeInTheDocument();
 	} );
 
 	test( 'If product is standalone scan, the expanded standalone card is rendered', () => {
 		getSiteScanState.mockReturnValue( { state: 'idle', mostRecent: { timestamp: '' } } );
 		isRequestingJetpackScan.mockReturnValue( false );
 
-		const standaloneCard = shallow( <JetpackBenefitsScanHistory isStandalone={ true } /> )
-			.find( 'JetpackBenefitsScanHistory' )
-			.dive()
-			.find( JetpackBenefitsStandaloneCard );
-
-		expect( standaloneCard ).toHaveLength( 1 );
+		const { container } = render( <JetpackBenefitsScanHistory isStandalone={ true } /> );
+		expect( container ).toMatchSnapshot();
 	} );
 } );
