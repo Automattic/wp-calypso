a60406171eb4a3fb1eb3f28694075893dfc2a234
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-05-25 18:09:06 +0530

    Hide the routes behind feature flag

--- client/my-sites/plans/jetpack-plans/index.ts
+++ client/my-sites/plans/jetpack-plans/index.ts
@@ -1,3 +1,4 @@
+import config from '@automattic/calypso-config';
 import page from 'page';
 import { makeLayout, render as clientRender } from 'calypso/controller/index.web';
 import {
@@ -8,9 +9,14 @@ import {
 } from './controller';
 
 export default function ( rootUrl: string, ...rest: PageJS.Callback[] ): void {
+	const addBoostAndSocialRouts = config.isEnabled( 'jetpack/pricing-add-boost-social' );
+
 	page( `${ rootUrl }/jetpack-free/welcome`, jetpackFreeWelcome, makeLayout, clientRender );
-	page( `${ rootUrl }/jetpack-boost/welcome`, jetpackBoostWelcome, makeLayout, clientRender );
-	page( `${ rootUrl }/jetpack-social/welcome`, jetpackSocialWelcome, makeLayout, clientRender );
+
+	if ( addBoostAndSocialRouts ) {
+		page( `${ rootUrl }/jetpack-boost/welcome`, jetpackBoostWelcome, makeLayout, clientRender );
+		page( `${ rootUrl }/jetpack-social/welcome`, jetpackSocialWelcome, makeLayout, clientRender );
+	}
 
 	page(
 		`${ rootUrl }/:duration?/:site?`,
