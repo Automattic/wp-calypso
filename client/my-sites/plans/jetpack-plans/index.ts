83abcb72f4210674015a746e1ba2888c6d8607e1
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-05-25 12:13:02 +0530

    Wire the routes up

--- client/my-sites/plans/jetpack-plans/index.ts
+++ client/my-sites/plans/jetpack-plans/index.ts
@@ -1,9 +1,16 @@
 import page from 'page';
 import { makeLayout, render as clientRender } from 'calypso/controller/index.web';
-import { jetpackFreeWelcome, productSelect } from './controller';
+import {
+	jetpackBoostWelcome,
+	jetpackFreeWelcome,
+	jetpackSocialWelcome,
+	productSelect,
+} from './controller';
 
 export default function ( rootUrl: string, ...rest: PageJS.Callback[] ): void {
 	page( `${ rootUrl }/jetpack-free/welcome`, jetpackFreeWelcome, makeLayout, clientRender );
+	page( `${ rootUrl }/jetpack-boost/welcome`, jetpackBoostWelcome, makeLayout, clientRender );
+	page( `${ rootUrl }/jetpack-social/welcome`, jetpackSocialWelcome, makeLayout, clientRender );
 
 	page(
 		`${ rootUrl }/:duration?/:site?`,
