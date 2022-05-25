bb3feb837d473037dae1357462c9738e008d0832
Marcelo Vega <marcelo.vega@automattic.com>
2022-05-24 20:34:12 -0400

    Removing chai and unnecessary test

--- client/lib/notifications/test/note-block-parser.js
+++ client/lib/notifications/test/note-block-parser.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { expect } from 'chai';
-
-/**
  * Internal dependencies
  */
 import { parseBlock } from '../note-block-parser';
@@ -36,34 +31,6 @@ describe( 'Test parsing blocks with url and type', () => {
 
 		const result = parseBlock( context );
 
-		expect( result ).to.deep.equal( expected );
-	} );
-
-	test( 'old parsing when block contained type and url is not longer valid', () => {
-		const context = {
-			text: 'New Order: Order #339',
-			ranges: [
-				{
-					url: 'https://examplesite/wp-admin/post.php?post=339&action=edit',
-					indices: [ 11, 21 ],
-					type: 'a',
-					section: 'shop_order',
-					intent: 'edit',
-				},
-			],
-		};
-
-		const oldExpected = [
-			'New Order: ',
-			{
-				type: 'a',
-				children: [ 'Order #339' ],
-			},
-		];
-
-		const result = parseBlock( context );
-
-		expect( result ).to.deep.not.equal( oldExpected );
-		expect( result[ 1 ] ).to.have.ownPropertyDescriptor( 'url' );
+		expect( result ).toEqual( expected );
 	} );
 } );
