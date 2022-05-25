43a947035ced83f70ef98d73ad6b9f320fe007a1
valterlorran <valterlorran@hotmail.com>
2022-05-25 12:10:09 +0100

    Themes: Adding alert on themes that need to be updated (#63761)
    * Adding alert on themes that need to be updated
    
    * Improving design and changing temporary class name
    
    * Adding actions and reducers for updating the themes
    
    * Adding reducers and actions for theme update
    
    * Fixing tests for the theme update action
    
    * Updating the UI after the theme update and adding more tests
    
    * Fixing test
    
    * Animating the update icon

---
+++ client/state/themes/actions/theme-update.js
@@ -0,0 +1,93 @@
+import { translate } from 'i18n-calypso';
+import { CALYPSO_CONTACT } from 'calypso/lib/url/support';
+import wpcom from 'calypso/lib/wp';
+import { requestAdminMenu } from 'calypso/state/admin-menu/actions';
+import { errorNotice } from 'calypso/state/notices/actions';
+import {
+	THEMES_UPDATE,
+	THEMES_UPDATE_FAILURE,
+	THEMES_UPDATE_SUCCESS,
+} from 'calypso/state/themes/action-types';
+import { requestThemes } from 'calypso/state/themes/actions';
+
+import 'calypso/state/themes/init';
+
+/**
+ * Updates the themes to the a newer version
+ *
+ * @param {Array} themeSlugs    Array of themes to be updated.
+ * @param {number} siteId       Site ID
+ * @param {boolean} autoupdate  Whether should enable or disable the auto update.
+ * @returns {Function}          Action thunk
+ */
+export function updateThemes( themeSlugs, siteId, autoupdate = false ) {
+	return ( dispatch ) => {
+		dispatch( {
+			type: THEMES_UPDATE,
+			themeSlugs,
+			siteId,
+		} );
+
+		return wpcom.req
+			.post( `/sites/${ siteId }/themes`, {
+				action: 'update',
+				themes: themeSlugs,
+				autoupdate,
+			} )
+			.then( ( response ) => {
+				const successfullyUpdatedThemes = response.themes.map( ( theme ) => theme.id );
+				dispatch( themesUpdated( siteId, successfullyUpdatedThemes ) );
+			} )
+			.catch( ( error ) => {
+				dispatch( {
+					type: THEMES_UPDATE_FAILURE,
+					themeSlugs,
+					siteId,
+				} );
+
+				if ( error.error === 'theme_not_found' ) {
+					dispatch( errorNotice( translate( 'Theme not yet available for this site' ) ) );
+				} else {
+					dispatch(
+						errorNotice(
+							translate(
+								'Unable to activate theme. {{contactSupportLink}}Contact support{{/contactSupportLink}}.',
+								{
+									components: {
+										contactSupportLink: (
+											<a target="_blank" href={ CALYPSO_CONTACT } rel="noreferrer" />
+										),
+									},
+								}
+							)
+						)
+					);
+				}
+			} );
+	};
+}
+
+/**
+ * Updates the state and UI after successfuly update the themes.
+ *
+ * @param {number} siteId
+ * @param {Array} themeSlugs
+ * @returns
+ */
+export function themesUpdated( siteId, themeSlugs ) {
+	const themeUpdatedThunk = ( dispatch ) => {
+		dispatch( {
+			type: THEMES_UPDATE_SUCCESS,
+			themeSlugs,
+			siteId,
+		} );
+
+		// There are instances where switching themes toggles menu items. This action refreshes
+		// the admin bar to ensure that those updates are displayed in the UI.
+		dispatch( requestAdminMenu( siteId ) );
+
+		dispatch( requestThemes( siteId, {} ) );
+	};
+
+	return themeUpdatedThunk;
+}
