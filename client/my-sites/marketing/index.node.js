58c3e6add48124a62a59104146a83ffe57d18478
Noah Allen <noahtallen@gmail.com>
2022-05-26 13:56:17 -0700

    Add redirect for ultimate traffic guide

--- client/my-sites/marketing/index.node.js
+++ client/my-sites/marketing/index.node.js
@@ -2,4 +2,11 @@ export default function ( router ) {
 	router( [ '/marketing/do-it-for-me', '/marketing/do-it-for-me/*' ], ( { res } ) => {
 		res.redirect( 301, 'https://wordpress.com/do-it-for-me/' );
 	} );
+
+	router(
+		[ '/marketing/ultimate-traffic-guide', '/marketing/ultimate-traffic-guide/*' ],
+		( { res } ) => {
+			res.redirect( 301, 'https://wpcourses.com/course/intro-to-search-engine-optimization-seo/' );
+		}
+	);
 }
