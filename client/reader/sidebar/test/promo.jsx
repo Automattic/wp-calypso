c5c98b5cccd161f83f682f9385b8de760f2cbf0f
Praveen <spraveenitpro@gmail.com>
2022-05-26 00:03:03 -0700

    Adding the settings reducer for migration from enzyme to React testing library

--- client/reader/sidebar/test/promo.jsx
+++ client/reader/sidebar/test/promo.jsx
@@ -2,8 +2,47 @@
  * @jest-environment jsdom
  */
 
-import { render } from '@testing-library/react';
+import editorReducer from 'calypso/state/editor/reducer';
+import mediaReducer from 'calypso/state/media/reducer';
+import siteSettingsReducer from 'calypso/state/site-settings/reducer';
+import timezonesReducer from 'calypso/state/timezones/reducer';
+import uiReducer from 'calypso/state/ui/reducer';
+import { renderWithProvider } from 'calypso/test-helpers/testing-library';
 import { ReaderSidebarPromo, shouldRenderAppPromo } from '../promo';
+
+const initialState = {
+	siteSettings: {},
+	sites: {
+		items: [],
+	},
+	media: {
+		queries: {},
+	},
+	currentUser: {
+		capabilities: {},
+	},
+	editor: {
+		imageEditor: {},
+	},
+	timezones: {
+		labels: {},
+		byContinents: {},
+	},
+	ui: {},
+};
+
+function renderWithRedux( ui ) {
+	return renderWithProvider( ui, {
+		initialState,
+		reducers: {
+			editor: editorReducer,
+			media: mediaReducer,
+			siteSettings: siteSettingsReducer,
+			timezones: timezonesReducer,
+			ui: uiReducer,
+		},
+	} );
+}
 
 describe( 'ReaderSidebarPromo', () => {
 	const shouldRenderAppPromoDefaultProps = {
@@ -17,7 +56,7 @@ describe( 'ReaderSidebarPromo', () => {
 
 	test( 'should render the AppPromo when the shouldRenderAppPromo property is true', () => {
 		const adjustedProperties = { shouldRenderAppPromo: true };
-		const { wrapper } = render( <ReaderSidebarPromo { ...adjustedProperties } /> );
+		const { wrapper } = renderWithRedux( <ReaderSidebarPromo { ...adjustedProperties } /> );
 		expect( wrapper.getElementsByClassName( 'sidebar__app-promo' ) ).toHaveLength( 1 );
 	} );
 
@@ -25,7 +64,8 @@ describe( 'ReaderSidebarPromo', () => {
 		const adjustedProperties = {
 			shouldRenderAppPromo: false,
 		};
-		const { wrapper } = render( <ReaderSidebarPromo { ...adjustedProperties } /> );
+		const { wrapper } = renderWithRedux( <ReaderSidebarPromo { ...adjustedProperties } /> );
+
 		expect( wrapper.getElementsByClassName( '.sidebar__app-promo' ) ).toHaveLength( 0 );
 	} );
 
