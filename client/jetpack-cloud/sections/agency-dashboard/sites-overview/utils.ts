ba129660741389252df53e8466a4d841689e7b3a
Yashwin <yashwinpoojary@gmail.com>
2022-05-26 17:04:34 +0530

    Integrate new sites API and row status formatting for agency dashboard (#64059)
    * Integrate new API and row status formatting
    
    * Update utils.ts

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/utils.ts
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/utils.ts
@@ -1,5 +1,5 @@
 import { translate } from 'i18n-calypso';
-import type { AllowedTypes, SiteData } from './types';
+import type { AllowedTypes, SiteData, FormattedRowObj } from './types';
 import type { ReactChild } from 'react';
 
 /**
@@ -92,62 +92,104 @@ export const getRowMetaData = (
 	};
 };
 
+const formatBackupData = ( site: SiteData ) => {
+	const backup: FormattedRowObj = {
+		value: '',
+		status: '',
+		type: 'backup',
+	};
+	if ( ! site.has_backup ) {
+		backup.status = 'inactive';
+		return backup;
+	}
+	switch ( site.latest_backup_status ) {
+		case 'rewind_backup_complete':
+		case 'backup_only_complete':
+			backup.status = 'success';
+			break;
+		case 'rewind_backup_error':
+		case 'backup_only_error':
+			backup.status = 'failed';
+			backup.value = translate( 'Failed' );
+			break;
+		case 'rewind_backup_complete_warning':
+		case 'backup_only_complete_warning':
+			backup.status = 'warning';
+			backup.value = translate( 'Warning' );
+			break;
+		default:
+			backup.status = 'progress';
+			break;
+	}
+	return backup;
+};
+
+const formatScanData = ( site: SiteData ) => {
+	const scan: FormattedRowObj = {
+		value: '',
+		status: '',
+		type: 'scan',
+		threats: 0,
+	};
+	if ( ! site.has_scan ) {
+		scan.status = 'inactive';
+	} else if ( site.latest_scan_threats_found.length > 0 ) {
+		const scanThreats = site.latest_scan_threats_found.length;
+		scan.status = 'failed';
+		scan.value = translate(
+			'%(threats)d Threat',
+			'%(threats)d Threats', // plural version of the string
+			{
+				count: scanThreats,
+				args: {
+					threats: scanThreats,
+				},
+			}
+		);
+		scan.threats = scanThreats;
+	} else {
+		scan.status = 'success';
+	}
+	return scan;
+};
+
+const formatMonitorData = ( site: SiteData ) => {
+	const monitor: FormattedRowObj = {
+		value: '',
+		status: '',
+		type: 'monitor',
+		error: false,
+	};
+	if ( ! site.monitor_active ) {
+		monitor.status = 'disabled';
+	} else if ( ! site.monitor_site_status ) {
+		monitor.status = 'failed';
+		monitor.value = translate( 'Site Down' );
+	} else {
+		monitor.status = 'success';
+	}
+	return monitor;
+};
+
 /**
  * Returns formatted sites
  */
-export const formatSites = ( data: { items: Array< any > } ): Array< any > => {
-	const sites = data?.items || [];
+export const formatSites = ( sites: Array< any > = [] ): Array< any > => {
 	return sites.map( ( site ) => {
 		const pluginUpdates = site.awaiting_plugin_updates;
-		let scanValue;
-		if ( site.latest_scan_status === 'failed' ) {
-			scanValue = translate( 'Failed' );
-		}
-		const scanThreats = site.latest_scan_threats_found.length;
-		if ( scanThreats > 0 ) {
-			scanValue = translate(
-				'%(threats)d Threat',
-				'%(threats)d Threats', // plural version of the string
-				{
-					count: scanThreats,
-					args: {
-						threats: scanThreats,
-					},
-				}
-			);
-		}
-		const error =
-			! site.is_connection_healthy ||
-			! site.access_xmlrpc ||
-			! site.valid_xmlrpc ||
-			! site.authenticated_xmlrpc;
 		return {
 			site: {
 				value: site,
-				error,
+				error: ! site.is_connection_healthy,
 				status: '',
 				type: 'site',
 			},
-			backup: {
-				value: 'failed' === site.latest_backup_status ? translate( 'Failed' ) : '',
-				status: site.backup_enabled ? site.latest_backup_status : 'inactive',
-				type: 'backup',
-			},
-			scan: {
-				value: scanValue,
-				status: site.scan_enabled ? site.latest_scan_status : 'inactive',
-				type: 'scan',
-				threats: site.latest_scan_threats_found.length,
-			},
-			monitor: {
-				value: 'failed' === site.monitor_status ? translate( 'Site Down' ) : '',
-				status: site.monitor_status === 'accessible' ? 'success' : site.monitor_status,
-				type: 'monitor',
-				error: 'failed' === site.monitor_status,
-			},
+			backup: formatBackupData( site ),
+			scan: formatScanData( site ),
+			monitor: formatMonitorData( site ),
 			plugin: {
 				value: `${ pluginUpdates.length } ${ translate( 'Available' ) }`,
-				status: pluginUpdates.length > 0 ? 'warning' : 'active',
+				status: pluginUpdates.length > 0 ? 'warning' : 'success',
 				type: 'plugin',
 				updates: pluginUpdates.length,
 			},
