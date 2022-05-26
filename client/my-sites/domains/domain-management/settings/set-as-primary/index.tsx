cfeaea9a4345e2f00e4c5c75e1effec60f505a5a
Jarda Snajdr <jsnajdr@gmail.com>
2022-05-26 09:36:36 +0200

    SetAsPrimary: rewrite connect to hooks

--- client/my-sites/domains/domain-management/settings/set-as-primary/index.tsx
+++ client/my-sites/domains/domain-management/settings/set-as-primary/index.tsx
@@ -2,15 +2,11 @@ import { FEATURE_SET_PRIMARY_CUSTOM_DOMAIN } from '@automattic/calypso-products'
 import { Button } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { useState } from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Accordion from 'calypso/components/domains/accordion';
 import { type } from 'calypso/lib/domains/constants';
 import { isUnderDomainManagementAll } from 'calypso/my-sites/domains/paths';
-import {
-	composeAnalytics,
-	recordGoogleEvent,
-	recordTracksEvent,
-} from 'calypso/state/analytics/actions';
+import { recordGoogleEvent, recordTracksEvent } from 'calypso/state/analytics/actions';
 import { NON_PRIMARY_DOMAINS_TO_FREE_USERS } from 'calypso/state/current-user/constants';
 import { currentUserHasFlag, getCurrentUser } from 'calypso/state/current-user/selectors';
 import {
@@ -21,76 +17,90 @@ import { getCurrentRoute } from 'calypso/state/selectors/get-current-route';
 import isDomainOnlySite from 'calypso/state/selectors/is-domain-only-site';
 import siteHasFeature from 'calypso/state/selectors/site-has-feature';
 import { setPrimaryDomain } from 'calypso/state/sites/domains/actions';
-import type {
-	SetAsPrimaryProps,
-	SetAsPrimaryPassedProps,
-	SetAsPrimaryStateProps,
-	ChangePrimaryFunction,
-} from './types';
+import type { ResponseDomain } from 'calypso/lib/domains/types';
+import type { CalypsoDispatch } from 'calypso/state/types';
+import type { SiteData } from 'calypso/state/ui/selectors/site-data';
 
-const SetAsPrimary = ( props: SetAsPrimaryProps ) => {
+type SetAsPrimaryProps = {
+	domain: ResponseDomain;
+	selectedSite: SiteData;
+};
+
+const SetAsPrimary = ( { domain, selectedSite }: SetAsPrimaryProps ) => {
 	const translate = useTranslate();
+	const dispatch = useDispatch< CalypsoDispatch >();
+
+	const isOnFreePlan = selectedSite?.plan?.is_free ?? false;
+	const hasDomainOnlySite = useSelector( ( state ) => isDomainOnlySite( state, selectedSite.ID ) );
+	const hasNonPrimaryDomainsFlag = useSelector( ( state ) =>
+		getCurrentUser( state ) ? currentUserHasFlag( state, NON_PRIMARY_DOMAINS_TO_FREE_USERS ) : false
+	);
+	const isManagingAllSites = useSelector( ( state ) =>
+		isUnderDomainManagementAll( getCurrentRoute( state ) )
+	);
+	const canSetPrimaryDomain = useSelector( ( state ) =>
+		siteHasFeature( state, selectedSite.ID, FEATURE_SET_PRIMARY_CUSTOM_DOMAIN )
+	);
+
 	const [ isSettingPrimaryDomain, setIsSettingPrimaryDomain ] = useState( false );
-	const {
-		domain,
-		hasDomainOnlySite,
-		isOnFreePlan,
-		hasNonPrimaryDomainsFlag,
-		canSetPrimaryDomain,
-		isManagingAllSites,
-		selectedSite,
-	} = props;
 
-	const shouldUpgradeToMakeDomainPrimary = () => {
-		return (
-			hasNonPrimaryDomainsFlag &&
-			isOnFreePlan &&
-			( domain.type === type.REGISTERED || domain.type === type.MAPPED ) &&
-			! hasDomainOnlySite &&
-			! domain.isPrimary &&
-			! domain.isWPCOMDomain &&
-			! domain.isWpcomStagingDomain &&
-			! canSetPrimaryDomain
-		);
-	};
+	const shouldUpgradeToMakeDomainPrimary =
+		hasNonPrimaryDomainsFlag &&
+		isOnFreePlan &&
+		( domain.type === type.REGISTERED || domain.type === type.MAPPED ) &&
+		! hasDomainOnlySite &&
+		! domain.isPrimary &&
+		! domain.isWPCOMDomain &&
+		! domain.isWpcomStagingDomain &&
+		! canSetPrimaryDomain;
 
-	const canSetAsPrimary = () => {
-		return (
-			! isManagingAllSites &&
-			domain &&
-			domain.canSetAsPrimary &&
-			! domain.isPrimary &&
-			! shouldUpgradeToMakeDomainPrimary() &&
-			! domain.aftermarketAuction
-		);
-	};
+	const canSetAsPrimary =
+		! isManagingAllSites &&
+		domain &&
+		domain.canSetAsPrimary &&
+		! domain.isPrimary &&
+		! shouldUpgradeToMakeDomainPrimary &&
+		! domain.aftermarketAuction;
 
-	const setPrimaryDomain = ( domainName: string ) => {
-		return new Promise( ( resolve, reject ) => {
-			props.setPrimaryDomain( selectedSite.ID, domainName, ( error, data ) => {
-				if ( ! error && data && data.success ) {
-					resolve( null );
-				} else {
-					reject( error );
-				}
-			} );
-		} );
-	};
+	if ( ! canSetAsPrimary ) {
+		return null;
+	}
 
 	const handleSetPrimaryDomainClick = async () => {
 		setIsSettingPrimaryDomain( true );
-		props.changePrimary( domain, 'button_click' );
+		dispatch(
+			recordGoogleEvent(
+				'Domain Management',
+				'Changed Primary Domain in Settings',
+				'Domain Name',
+				domain.name
+			)
+		);
+		dispatch(
+			recordTracksEvent( 'calypso_domain_management_settings_change_primary_domain_click', {
+				section: domain.type,
+				mode: 'button_click',
+			} )
+		);
 		try {
-			await setPrimaryDomain( domain.name );
-			props.showUpdatePrimaryDomainSuccessNotice( domain.name );
+			await new Promise( ( resolve, reject ) => {
+				dispatch(
+					setPrimaryDomain( selectedSite.ID, domain.name, ( error, data ) => {
+						if ( ! error && data && data.success ) {
+							resolve( null );
+						} else {
+							reject( error );
+						}
+					} )
+				);
+			} );
+			dispatch( showUpdatePrimaryDomainSuccessNotice( domain.name ) );
 		} catch ( error ) {
-			props.showUpdatePrimaryDomainErrorNotice( ( error as Error ).message );
+			dispatch( showUpdatePrimaryDomainErrorNotice( ( error as Error ).message ) );
 		} finally {
 			setIsSettingPrimaryDomain( false );
 		}
 	};
-
-	if ( ! canSetAsPrimary() ) return null;
 
 	return (
 		<div className="set-as-primary">
@@ -116,42 +126,4 @@ const SetAsPrimary = ( props: SetAsPrimaryProps ) => {
 	);
 };
 
-const changePrimary: ChangePrimaryFunction = ( domain, mode ) =>
-	composeAnalytics(
-		recordGoogleEvent(
-			'Domain Management',
-			'Changed Primary Domain in Settings',
-			'Domain Name',
-			domain.name
-		),
-		recordTracksEvent( 'calypso_domain_management_settings_change_primary_domain_click', {
-			section: domain.type,
-			mode,
-		} )
-	);
-
-export default connect(
-	( state, ownProps: SetAsPrimaryPassedProps ): SetAsPrimaryStateProps => {
-		const { selectedSite } = ownProps;
-		const isOnFreePlan = selectedSite?.plan?.is_free || false;
-		return {
-			hasDomainOnlySite: !! isDomainOnlySite( state, selectedSite.ID ),
-			hasNonPrimaryDomainsFlag: getCurrentUser( state )
-				? currentUserHasFlag( state, NON_PRIMARY_DOMAINS_TO_FREE_USERS )
-				: false,
-			isOnFreePlan,
-			isManagingAllSites: isUnderDomainManagementAll( getCurrentRoute( state ) ),
-			canSetPrimaryDomain: siteHasFeature(
-				state,
-				selectedSite.ID,
-				FEATURE_SET_PRIMARY_CUSTOM_DOMAIN
-			),
-		};
-	},
-	{
-		changePrimary,
-		setPrimaryDomain,
-		showUpdatePrimaryDomainErrorNotice,
-		showUpdatePrimaryDomainSuccessNotice,
-	}
-)( SetAsPrimary );
+export default SetAsPrimary;
