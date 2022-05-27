308cab5e7dc646bb9946d56a36827a9583abd7ff
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-05-27 14:03:04 +0200

    Add/stepper light importer (#63906)
    * Register import light route
    
    * Move import-light module to the blocks folder
    
    * Update stepper config with feature flag checking
    
    * Integrate static capture screen into light import flow
    
    * Add document title

---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/import-light/index.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+import { StepContainer } from '@automattic/onboarding';
+import { useI18n } from '@wordpress/react-i18n';
+import React from 'react';
+import Capture from 'calypso/blocks/import-light/capture';
+import DocumentHead from 'calypso/components/data/document-head';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import type { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
+import './style.scss';
+
+const ImportLight: Step = function ImportStep( props ) {
+	const { __ } = useI18n();
+	const { navigation } = props;
+
+	function renderStepContent() {
+		return <Capture />;
+	}
+
+	return (
+		<>
+			<DocumentHead title={ __( 'Import light' ) } />
+
+			<StepContainer
+				stepName={ 'import-step' }
+				className={ 'import__onboarding-page' }
+				hideSkip={ false }
+				hideFormattedHeader={ true }
+				goBack={ navigation.goBack }
+				goNext={ navigation.goNext }
+				skipLabelText={ __( 'Skip this step' ) }
+				isFullLayout={ true }
+				stepContent={ renderStepContent() }
+				recordTracksEvent={ recordTracksEvent }
+			/>
+		</>
+	);
+};
+
+export default ImportLight;
