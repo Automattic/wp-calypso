75a322999f54e6fdcee455525d07afc5a502db87
elliottprogrammer <bryan@elliottprogrammer.com>
2022-05-30 13:01:38 -0400

    Show product/plugin "active" state in Boost Free card.

---
+++ client/my-sites/plans/jetpack-plans/jetpack-boost-free-card/use-boost-free-button-props.ts
@@ -0,0 +1,54 @@
+import { TranslateResult, useTranslate } from 'i18n-calypso';
+import { useCallback, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
+import useTrackCallback from 'calypso/lib/jetpack/use-track-callback';
+import getSiteAdminUrl from 'calypso/state/sites/selectors/get-site-admin-url';
+
+type SiteId = number | null;
+
+interface Props {
+	primary: boolean;
+	href: string;
+	label: TranslateResult;
+	onClick: React.MouseEventHandler;
+}
+
+const JETPACK_BOOST_PAGE = 'https://jetpack.com/boost/';
+const CALYPSO_BOOST_LANDING_PAGE = '/pricing/jetpack-boost/welcome';
+
+export default function useBoostFreeButtonProps( siteId: SiteId, isOwned: boolean ): Props {
+	const translate = useTranslate();
+	const siteWpAdminUrl = useSelector( ( state ) =>
+		getSiteAdminUrl( state, siteId, 'admin.php?page=jetpack-boost' )
+	);
+
+	const primary = ! isOwned;
+
+	const href = useMemo( () => {
+		if ( isOwned && siteWpAdminUrl ) {
+			return siteWpAdminUrl;
+		}
+		if ( isJetpackCloud() ) {
+			return CALYPSO_BOOST_LANDING_PAGE;
+		}
+		return JETPACK_BOOST_PAGE;
+	}, [ isOwned, siteWpAdminUrl ] );
+
+	const label = isOwned ? translate( 'Manage plugin' ) : translate( 'Get Boost' );
+
+	const trackCallback = useTrackCallback( undefined, 'calypso_product_jpboost_free_click', {
+		site_id: siteId || undefined,
+	} );
+
+	const onClick = useCallback( () => {
+		trackCallback();
+	}, [ trackCallback ] );
+
+	return {
+		primary,
+		href,
+		label,
+		onClick,
+	};
+}
