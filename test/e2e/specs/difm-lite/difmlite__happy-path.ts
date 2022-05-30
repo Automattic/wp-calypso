79c1cf9d46af25126ac4a308ca5c6cf9a9368739
steveycee <stevey.cee@gmail.com>
2022-05-30 16:07:29 +0100

    Update difmlite__happy-path to make use of GutenboardingFlow click abstraction

--- test/e2e/specs/difm-lite/difmlite__happy-path.ts
+++ test/e2e/specs/difm-lite/difmlite__happy-path.ts
@@ -2,15 +2,7 @@
  * @group calypso-release
  */
 
-import {
-	DataHelper,
-	// CloseAccountFlow,
-	// GutenboardingFlow,
-	// DifmLitePage,
-	// FullSiteEditorPage,
-	// SecretsManager,
-	TestAccount,
-} from '@automattic/calypso-e2e';
+import { DataHelper, GutenboardingFlow, TestAccount } from '@automattic/calypso-e2e';
 import { Page, Browser } from 'playwright';
 
 declare const browser: Browser;
@@ -20,7 +12,8 @@ describe(
 	function () {
 		let testAccount: TestAccount;
 		let page: Page;
-		// let difmLite: DifmLitePage;
+		// let difmLite: DifmLiteFlow;
+		let gutenboardingFlow: GutenboardingFlow;
 
 		beforeAll( async function () {
 			page = await browser.newPage();
@@ -35,8 +28,8 @@ describe(
 			it( 'Step 1: Select an Existing WordPress.com site', async function () {
 				// CHECK A PRICE IS SHOWING
 				// CHECK THE PRICE IS IN AUD?
-				const clickSelectSite = page.locator( 'text=Select a site' );
-				await clickSelectSite.click();
+				gutenboardingFlow = new GutenboardingFlow( page );
+				await gutenboardingFlow.clickButton( 'Select a site' );
 			} );
 			it( 'Step 2: Choose site to redo (delete)', async function () {
 				const selectSiteByIcon = page.locator( '.site-icon >> nth=0 ' );
@@ -50,31 +43,25 @@ describe(
 			} );
 			it( 'Step 3: Enter site name (skip tagline)', async function () {
 				const siteTitleInput = page.locator( '#siteTitle ' );
-				const continueButton = page.locator( 'text=Continue ' );
 				await siteTitleInput.fill( 'Test Site Name' );
-				await continueButton.click();
+				await gutenboardingFlow.clickButton( 'Continue' );
 			} );
 			it( 'Step 4: Continue on socials (enter no socials)', async function () {
-				const continueButton = page.locator( 'text=Continue ' );
-				await continueButton.click();
+				await gutenboardingFlow.clickButton( 'Continue' );
 			} );
 			it( 'Step 5: Select a theme.', async function () {
 				const firstThemeChoice = page.locator( '.design-picker__design-option >> nth=0 ' );
 				await firstThemeChoice.click();
 			} );
 			it( 'Step 6: Add 2 pages ontop of default.', async function () {
-				const goToCheckout = page.locator( 'text=Go to Checkout ' );
-				await goToCheckout.click();
+				await gutenboardingFlow.clickButton( 'Go to Checkout' );
 			} );
 			it( 'Confirm journey has resolved to the basket.', async function () {
-				// await page.locator( '.masterbar__secure-checkout >> visible=false' );
 				await page.locator( '.masterbar__secure-checkout' );
 			} );
 			it.skip( 'Pause at the end', async function () {
 				await page.locator( '.siteSearch__input' ).press( 'Enter' );
 			} );
-
-			// Step through new site journey
 		} );
 	}
 );
