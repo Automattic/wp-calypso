66aed5f9bb4dc4287df6f43f97bf285613f70428
elliottprogrammer <bryan@elliottprogrammer.com>
2022-05-31 09:02:59 -0400

    Refactor Social CTA tracking function.

--- client/my-sites/plans/jetpack-plans/jetpack-social-free-card/use-social-free-button-props.ts
+++ client/my-sites/plans/jetpack-plans/jetpack-social-free-card/use-social-free-button-props.ts
@@ -1,8 +1,8 @@
 import { TranslateResult, useTranslate } from 'i18n-calypso';
 import { useCallback, useMemo } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
-import useTrackCallback from 'calypso/lib/jetpack/use-track-callback';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import getSiteAdminUrl from 'calypso/state/sites/selectors/get-site-admin-url';
 
 type SiteId = number | null;
@@ -19,6 +19,7 @@ const CALYPSO_SOCIAL_LANDING_PAGE = '/pricing/jetpack-social/welcome';
 
 export default function useSocialFreeButtonProps( siteId: SiteId, isOwned: boolean ): Props {
 	const translate = useTranslate();
+	const dispatch = useDispatch();
 	const siteWpAdminUrl = useSelector( ( state ) =>
 		getSiteAdminUrl( state, siteId, 'admin.php?page=jetpack-social' )
 	);
@@ -37,13 +38,14 @@ export default function useSocialFreeButtonProps( siteId: SiteId, isOwned: boole
 
 	const label = isOwned ? translate( 'Manage plugin' ) : translate( 'Get Social' );
 
-	const trackCallback = useTrackCallback( undefined, 'calypso_product_jpsocial_free_click', {
-		site_id: siteId || undefined,
-	} );
-
 	const onClick = useCallback( () => {
-		trackCallback();
-	}, [ trackCallback ] );
+		dispatch(
+			recordTracksEvent( 'calypso_product_jpsocial_free_click', {
+				site_id: siteId ?? undefined,
+				is_owned: !! isOwned,
+			} )
+		);
+	}, [ dispatch, siteId, isOwned ] );
 
 	return {
 		primary,
