d07ad8d679875ff62940ab98504d2650eac72785
Edwin Takahashi <egao@outlook.com>
2022-06-02 13:34:31 -0700

    Try waiting for networkidle instead.

--- packages/calypso-e2e/src/lib/flows/start-site-flow.ts
+++ packages/calypso-e2e/src/lib/flows/start-site-flow.ts
@@ -49,11 +49,9 @@ export class StartSiteFlow {
 	 * @param {string} name Name for the blog.
 	 */
 	async enterBlogName( name: string ): Promise< void > {
+		await this.page.waitForLoadState( 'networkidle' );
 		const locator = this.page.locator( selectors.blogNameInput );
-		// Use the type method to simulate user interaction to avoid
-		// https://github.com/Automattic/wp-calypso/issues/64271.
-		await locator.fill( '' );
-		await locator.type( name );
+		await locator.fill( name );
 	}
 
 	/**
@@ -62,11 +60,9 @@ export class StartSiteFlow {
 	 * @param {string} tagline Tagline for the blog.
 	 */
 	async enterTagline( tagline: string ): Promise< void > {
+		await this.page.waitForLoadState( 'networkidle' );
 		const locator = this.page.locator( selectors.taglineInput );
-		// Use the type method to simulate user interaction to avoid
-		// https://github.com/Automattic/wp-calypso/issues/64271.
-		await locator.fill( '' );
-		await locator.type( tagline );
+		await locator.fill( tagline );
 	}
 
 	/**
