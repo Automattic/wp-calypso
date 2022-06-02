cb6910d5d9c0ee1b775aeeb3fbf792232be1c4f1
Jarda Snajdr <jsnajdr@gmail.com>
2022-06-02 10:59:37 +0200

    popup-monitor: inline Emitter definition (#64243)

--- packages/popup-monitor/src/emitter.js
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from 'events';
-
-export default function ( prototype ) {
-	Object.assign( prototype, EventEmitter.prototype );
-	prototype.emitChange = function () {
-		this.emit( 'change' );
-	};
-	prototype.off = prototype.removeListener;
-}
