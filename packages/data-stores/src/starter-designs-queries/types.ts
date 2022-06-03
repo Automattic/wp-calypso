cc72ab522171a20e4ceb6c9685abb61d79df880a
Eru Penkman <roo2@users.noreply.github.com>
2022-06-03 10:20:44 +1000

    add _locale to generated designs query (#64282)

--- packages/data-stores/src/starter-designs-queries/types.ts
+++ packages/data-stores/src/starter-designs-queries/types.ts
@@ -3,6 +3,7 @@ import type { DesignRecipe } from '@automattic/design-picker/src/types';
 export interface StarterDesignsGeneratedQueryParams {
 	vertical_id: string;
 	seed?: string;
+	_locale: string;
 }
 
 export interface StarterDesignsGenerated {
