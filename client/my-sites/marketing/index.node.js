4929cfd0b68689721cc405b7936e51eb9f4cd4ae
Noah Allen <noahtallen@gmail.com>
2022-06-06 14:08:25 -0700

    Add client-side routing handler to server

--- client/my-sites/marketing/index.node.js
+++ client/my-sites/marketing/index.node.js
@@ -1,12 +1,17 @@
+import { makeLayout } from 'calypso/controller';
+
 export default function ( router ) {
+	// Handle redirects on the server.
 	router( [ '/marketing/do-it-for-me', '/marketing/do-it-for-me/*' ], ( { res } ) => {
 		res.redirect( 301, 'https://wordpress.com/do-it-for-me/' );
 	} );
-
 	router(
 		[ '/marketing/ultimate-traffic-guide', '/marketing/ultimate-traffic-guide/*' ],
 		( { res } ) => {
 			res.redirect( 301, 'https://wpcourses.com/course/intro-to-search-engine-optimization-seo/' );
 		}
 	);
+
+	// Otherwise, use client-side routing.
+	router( [ '/marketing', '/marketing/*', '/sharing', '/sharing/*' ], makeLayout );
 }
