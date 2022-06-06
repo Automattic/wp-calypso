3b67dce68bf97146a71a84bfcd4a4d9e9bd2328c
Olaseni Oluwunmi <277661+olaseni@users.noreply.github.com>
2022-06-06 14:15:42 +0100

    Add validator function and tests. Also add new `isTouched` field to mailbox fields

--- client/my-sites/email/form/mailboxes/validators.ts
+++ client/my-sites/email/form/mailboxes/validators.ts
@@ -1,6 +1,8 @@
 import emailValidator from 'email-validator';
 import i18n from 'i18n-calypso';
-import React from 'react';
+import { createElement } from 'react';
+import wp from 'calypso/lib/wp';
+import { EmailProvider } from 'calypso/my-sites/email/form/mailboxes/types';
 import type { FieldError, MailboxFormFieldBase } from 'calypso/my-sites/email/form/mailboxes/types';
 
 interface Validator< T > {
@@ -75,9 +77,9 @@ class MaximumStringLengthValidator extends BaseValidator< string > {
 }
 
 class MailboxNameValidator extends BaseValidator< string > {
-	areApostrophesSupported: boolean;
-	domainName: string;
-	mailboxHasDomainError: boolean;
+	private readonly areApostrophesSupported: boolean;
+	private readonly domainName: string;
+	private readonly mailboxHasDomainError: boolean;
 
 	constructor(
 		domainName: string,
@@ -126,7 +128,7 @@ class MailboxNameValidator extends BaseValidator< string > {
 }
 
 class AlternateEmailValidator extends BaseValidator< string > {
-	domainName: string;
+	private readonly domainName: string;
 
 	constructor( domainName: string ) {
 		super();
@@ -145,7 +147,7 @@ class AlternateEmailValidator extends BaseValidator< string > {
 					domain: domainName,
 				},
 				components: {
-					strong: React.createElement( 'strong' ),
+					strong: createElement( 'strong' ),
 				},
 			}
 		);
@@ -252,7 +254,7 @@ class PasswordValidator extends BaseValidator< string > {
 }
 
 class ExistingMailboxNamesValidator extends BaseValidator< string > {
-	existingMailboxNames: string[];
+	private readonly existingMailboxNames: string[];
 
 	constructor( existingMailboxNames: string[] ) {
 		super();
@@ -279,12 +281,80 @@ class ExistingMailboxNamesValidator extends BaseValidator< string > {
 	}
 }
 
+class MailboxNameValidityValidator extends BaseValidator< string > {
+	private readonly domainName: string;
+	private readonly provider: EmailProvider;
+
+	constructor( domainName: string, provider: EmailProvider ) {
+		super();
+		this.domainName = domainName;
+		this.provider = provider;
+	}
+
+	static getUnavailableMailboxError( mailboxName: string, message: string ): FieldError {
+		return i18n.translate( '{{strong}}%(mailbox)s{{/strong}} is not available: %(message)s', {
+			comment:
+				'%(mailbox)s is the local part of an email address. %(message)s is a translated message that gives context to why the mailbox is not available',
+			args: {
+				mailbox: mailboxName,
+				message,
+			},
+			components: {
+				strong: createElement( 'strong' ),
+			},
+		} );
+	}
+
+	async checkMailboxAvailability( domain: string, mailbox: string ) {
+		try {
+			const encDomain = encodeURIComponent( domain );
+			const encMailbox = encodeURIComponent( mailbox );
+			const response = await wp.req.get( {
+				path: `/emails/titan/${ encDomain }/check-mailbox-availability/${ encMailbox }`,
+				apiNamespace: 'wpcom/v2',
+			} );
+			return { message: response.message, status: 200 };
+		} catch ( error: any ) {
+			return { message: error?.message, status: error?.statusCode };
+		}
+	}
+
+	async validate( field?: MailboxFormFieldBase< string > ) {
+		if ( ! field || field.hasError() ) {
+			return;
+		}
+
+		await this.validateField( field );
+	}
+
+	async validateField( field: MailboxFormFieldBase< string > ) {
+		// Google has no mailbox name validator at this time
+		if ( this.provider === EmailProvider.Google ) {
+			return;
+		}
+
+		// There's nothing to validate if the field has no value
+		if ( ! field.value ) {
+			return;
+		}
+
+		// Check that this mailbox name is available against the domain
+		const { message, status } = await this.checkMailboxAvailability( this.domainName, field.value );
+
+		// If mailbox name is not available ...
+		if ( status !== 200 ) {
+			field.error = MailboxNameValidityValidator.getUnavailableMailboxError( field.value, message );
+		}
+	}
+}
+
 export type { Validator };
 
 export {
 	AlternateEmailValidator,
 	ExistingMailboxNamesValidator,
 	MailboxNameValidator,
+	MailboxNameValidityValidator,
 	PasswordValidator,
 	RequiredValidator,
 	RequiredIfVisibleValidator,
