dd0d5c2874d3e45828b77da23c0ff280e880a344
Niranjan Uma Shankar <niranjan.u@gmail.com>
2022-06-07 17:06:39 +0400

    Add a confirmation card

--- client/my-sites/site-settings/start-over.jsx
+++ client/my-sites/site-settings/start-over.jsx
@@ -1,70 +1,157 @@
-import { Button, Gridicon } from '@automattic/components';
-import { useLocalizeUrl } from '@automattic/i18n-utils';
+import { Card } from '@automattic/components';
+import { CheckboxControl } from '@wordpress/components';
 import { localize } from 'i18n-calypso';
+import { Component } from 'react';
 import { connect } from 'react-redux';
-import ActionPanel from 'calypso/components/action-panel';
-import ActionPanelBody from 'calypso/components/action-panel/body';
-import ActionPanelFigure from 'calypso/components/action-panel/figure';
-import ActionPanelFooter from 'calypso/components/action-panel/footer';
-import ActionPanelTitle from 'calypso/components/action-panel/title';
+import ExternalLink from 'calypso/components/external-link';
+import FormattedHeader from 'calypso/components/formatted-header';
 import HeaderCake from 'calypso/components/header-cake';
-import { EMPTY_SITE } from 'calypso/lib/url/support';
-import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import getAtomicTransfer from 'calypso/state/selectors/get-atomic-transfer';
+import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
-const StartOver = ( { translate, selectedSiteSlug } ) => {
-	const localizeUrl = useLocalizeUrl();
-	return (
-		<div
-			className="main main-column" // eslint-disable-line wpcalypso/jsx-classname-namespace
-			role="main"
-		>
-			<HeaderCake backHref={ '/settings/general/' + selectedSiteSlug }>
-				<h1>{ translate( 'Start Over' ) }</h1>
-			</HeaderCake>
-			<ActionPanel>
-				<ActionPanelBody>
-					<ActionPanelFigure inlineBodyText={ true }>
-						<img src="/calypso/images/wordpress/logo-stars.svg" alt="" width="170" height="143" />
-					</ActionPanelFigure>
-					<ActionPanelTitle>{ translate( 'Start Over' ) }</ActionPanelTitle>
-					<p>
-						{ translate(
-							"If you want a site but don't want any of the posts and pages you have now, our support " +
-								'team can delete your posts, pages, media, and comments for you.'
-						) }
-					</p>
-					<p>
-						{ translate(
-							'This will keep your site and URL active, but give you a fresh start on your content ' +
-								'creation. Just contact us to have your current content cleared out.'
-						) }
-					</p>
-					<p>
-						{ translate(
-							'Alternatively, you can delete all content from your site by following the steps here.'
-						) }
-					</p>
-				</ActionPanelBody>
-				<ActionPanelFooter>
-					<Button
-						className="action-panel__support-button is-external" // eslint-disable-line wpcalypso/jsx-classname-namespace
-						href={ localizeUrl( EMPTY_SITE ) }
-					>
-						{ translate( 'Follow the steps' ) }
-						<Gridicon icon="external" size={ 48 } />
-					</Button>
-					<Button
-						className="action-panel__support-button" // eslint-disable-line wpcalypso/jsx-classname-namespace
-						href="/help/contact"
-					>
-						{ translate( 'Contact support' ) }
-					</Button>
-				</ActionPanelFooter>
-			</ActionPanel>
-		</div>
-	);
-};
+class StartOver extends Component {
+	state = {
+		atomicRevertCheckOne: false,
+		atomicRevertCheckTwo: false,
+		simpleRevertCheckOne: false,
+		simpleRevertCheckTwo: false,
+	};
 
-export default connect( ( state ) => ( {
-	selectedSiteSlug: getSelectedSiteSlug( state ),
-} ) )( localize( StartOver ) );
+	static defaultProps = {
+		atomicTransfer: false,
+	};
+
+	getCheckboxConsent() {
+		const { translate } = this.props;
+		const {
+			atomicRevertCheckOne,
+			atomicRevertCheckTwo,
+			atomicTransfer,
+			simpleRevertCheckOne,
+			simpleRevertCheckTwo,
+		} = this.state;
+
+		if ( atomicTransfer ) {
+			return (
+				<>
+					<CheckboxControl
+						label={ translate(
+							'Any themes/plugins you have installed on the site will be removed, along with their data.'
+						) }
+						checked={ atomicRevertCheckOne }
+						onChange={ ( isChecked ) => this.setState( { atomicRevertCheckOne: isChecked } ) }
+					/>
+					<CheckboxControl
+						label={ translate(
+							'Your site will return to its original settings and theme right before the first plugin or custom theme was installed.'
+						) }
+						checked={ atomicRevertCheckTwo }
+						onChange={ ( isChecked ) => this.setState( { atomicRevertCheckTwo: isChecked } ) }
+					/>
+				</>
+			);
+		}
+
+		return (
+			<>
+				<CheckboxControl
+					label={ translate(
+						'All posts, pages, media, comments, tags, and themes will be deleted.'
+					) }
+					checked={ simpleRevertCheckOne }
+					onChange={ ( isChecked ) => this.setState( { simpleRevertCheckOne: isChecked } ) }
+				/>
+				<CheckboxControl
+					label={ translate(
+						'I understand that there is no way to retrieve my data unless I have downloaded a backup.'
+					) }
+					checked={ simpleRevertCheckTwo }
+					onChange={ ( isChecked ) => this.setState( { simpleRevertCheckTwo: isChecked } ) }
+				/>
+			</>
+		);
+	}
+
+	render() {
+		const atomicTransferDate = null;
+		const { selectedSiteSlug, translate, atomicTransfer } = this.props;
+
+		let subHeaderText;
+
+		if ( atomicTransfer ) {
+			subHeaderText = translate(
+				'After emptying your site, we will return your site back to the point when you installed your first plugin or custom theme or activated hosting features on {{strong}}%(atomicTransferDate)s{{/strong}}. All your posts, pages and media will be deleted.',
+				{
+					args: { atomicTransferDate },
+					components: {
+						// eslint-disable-next-line wpcalypso/jsx-classname-namespace
+						strong: <strong className="is-highlighted" />,
+					},
+				}
+			);
+		}
+
+		return (
+			<>
+				<div
+					className="main main-column start-over" // eslint-disable-line wpcalypso/jsx-classname-namespace
+					role="main"
+				>
+					<HeaderCake backHref={ '/settings/general/' + selectedSiteSlug }>Start Over</HeaderCake>
+					<Card>
+						<h1>{ translate( 'Start Over' ) }</h1>
+						<p>
+							{ translate(
+								"If you want a site but don't want any of the posts and pages you have now, " +
+									'then proceed to delete your posts, pages, media, and comments.'
+							) }
+						</p>
+						<p>
+							{ translate(
+								'This will keep your site and URL active, but give you a fresh start on your content ' +
+									'creation.'
+							) }
+						</p>
+					</Card>
+				</div>
+				<div className=" main main-column start-over start-over__confirm">
+					<Card>
+						<FormattedHeader
+							brandFont
+							headerText={ translate( 'Proceed with caution' ) }
+							subHeaderText={ subHeaderText }
+						/>
+						<p>
+							{ translate(
+								'Please {{strong}}confirm and check{{/strong}} the following items before you continue with emptying your site:',
+								{ components: { strong: <strong /> } }
+							) }
+						</p>
+						{ this.getCheckboxConsent() }
+
+						<div className="start-over__backups">
+							<h4>{ translate( 'Would you like to download the backup of your site?' ) }</h4>
+							<p>
+								{ translate(
+									'If you change your mind later or want to secure your data, you can download a backup.'
+								) }
+							</p>
+							<ExternalLink icon href={ `/backup/${ selectedSiteSlug }` }>
+								{ translate( 'Go to your backups' ) }
+							</ExternalLink>
+						</div>
+					</Card>
+				</div>
+			</>
+		);
+	}
+}
+
+export default connect( ( state ) => {
+	const siteId = getSelectedSiteId( state );
+
+	return {
+		selectedSiteSlug: getSelectedSiteSlug( state ),
+		atomicTransfer: getAtomicTransfer( state, siteId ),
+	};
+} )( localize( StartOver ) );
