dc749dcc1c0420a59d9dded749b6886c8aaa635e
Niranjan Uma Shankar <niranjan.u@gmail.com>
2022-06-07 23:38:30 +0400

    Refactor

--- client/my-sites/site-settings/start-over.jsx
+++ client/my-sites/site-settings/start-over.jsx
@@ -6,16 +6,17 @@ import { connect } from 'react-redux';
 import ExternalLink from 'calypso/components/external-link';
 import FormattedHeader from 'calypso/components/formatted-header';
 import HeaderCake from 'calypso/components/header-cake';
+import { withLocalizedMoment } from 'calypso/components/localized-moment';
 import wpcom from 'calypso/lib/wp';
+import { fetchAtomicTransfer } from 'calypso/state/atomic-transfer/actions';
 import getAtomicTransfer from 'calypso/state/selectors/get-atomic-transfer';
+import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
 class StartOver extends Component {
 	state = {
-		atomicRevertCheckOne: false,
-		atomicRevertCheckTwo: false,
-		simpleRevertCheckOne: false,
-		simpleRevertCheckTwo: false,
+		checkOne: false,
+		checkTwo: false,
 		isEmptyButtonBusy: false,
 	};
 
@@ -23,17 +24,24 @@ class StartOver extends Component {
 		atomicTransfer: false,
 	};
 
-	getCheckboxConsent() {
+	componentDidMount() {
+		const { siteId } = this.props;
+
+		if ( this.props.isAtomicSite && siteId ) {
+			this.props.fetchAtomicTransfer( siteId );
+		}
+	}
+
+	getCheckboxContent( hasAtomicTransferCompleted = false ) {
 		const { selectedSiteSlug, translate } = this.props;
 		const {
 			atomicRevertCheckOne,
 			atomicRevertCheckTwo,
-			atomicTransfer,
 			simpleRevertCheckOne,
 			simpleRevertCheckTwo,
 		} = this.state;
 
-		if ( atomicTransfer ) {
+		if ( hasAtomicTransferCompleted ) {
 			return (
 				<>
 					<CheckboxControl
@@ -42,7 +50,7 @@ class StartOver extends Component {
 							'Any themes/plugins you have installed on the site will be removed, along with their data.'
 						) }
 						checked={ atomicRevertCheckOne }
-						onChange={ ( isChecked ) => this.setState( { atomicRevertCheckOne: isChecked } ) }
+						onChange={ ( isChecked ) => this.setState( { checkOne: isChecked } ) }
 					/>
 					<CheckboxControl
 						className="start-over__checkbox-container"
@@ -50,7 +58,7 @@ class StartOver extends Component {
 							'Your site will return to its original settings and theme right before the first plugin or custom theme was installed.'
 						) }
 						checked={ atomicRevertCheckTwo }
-						onChange={ ( isChecked ) => this.setState( { atomicRevertCheckTwo: isChecked } ) }
+						onChange={ ( isChecked ) => this.setState( { checkTwo: isChecked } ) }
 					/>
 				</>
 			);
@@ -71,7 +79,7 @@ class StartOver extends Component {
 						}
 					) }
 					checked={ simpleRevertCheckOne }
-					onChange={ ( isChecked ) => this.setState( { simpleRevertCheckOne: isChecked } ) }
+					onChange={ ( isChecked ) => this.setState( { checkOne: isChecked } ) }
 				/>
 				<CheckboxControl
 					className="start-over__checkbox-container"
@@ -79,7 +87,7 @@ class StartOver extends Component {
 						'I understand that there is no way to retrieve my data unless I have downloaded a backup.'
 					) }
 					checked={ simpleRevertCheckTwo }
-					onChange={ ( isChecked ) => this.setState( { simpleRevertCheckTwo: isChecked } ) }
+					onChange={ ( isChecked ) => this.setState( { checkTwo: isChecked } ) }
 				/>
 			</>
 		);
@@ -107,34 +115,76 @@ class StartOver extends Component {
 		window.location.href = currentPageUrl.toString();
 	};
 
-	render() {
-		const atomicTransferDate = null;
-		const { selectedSiteSlug, translate, atomicTransfer } = this.props;
-		const {
-			atomicRevertCheckOne,
-			atomicRevertCheckTwo,
-			simpleRevertCheckOne,
-			simpleRevertCheckTwo,
-			isEmptyButtonBusy,
-		} = this.state;
-		let canClickEmptyButton;
-		let subHeaderText;
+	getSubheaderText( hasAtomicTransferCompleted = false ) {
+		const { atomicTransfer, moment, translate } = this.props;
+		const atomicTransferDate = moment( atomicTransfer.created_at ).format( 'LL' );
 
-		if ( Object.keys( atomicTransfer ).length ) {
-			canClickEmptyButton = atomicRevertCheckOne && atomicRevertCheckTwo;
-			subHeaderText = translate(
-				'After emptying your site, we will return your site back to the point when you installed your first plugin or custom theme or activated hosting features on {{strong}}%(atomicTransferDate)s{{/strong}}. All your posts, pages and media will be deleted.',
-				{
-					args: { atomicTransferDate },
-					components: {
-						// eslint-disable-next-line wpcalypso/jsx-classname-namespace
-						strong: <strong className="is-highlighted" />,
-					},
-				}
-			);
-		} else {
-			canClickEmptyButton = simpleRevertCheckOne && simpleRevertCheckTwo;
+		if ( ! hasAtomicTransferCompleted ) {
+			return;
 		}
+
+		return translate(
+			'After emptying your site, we will return your site back to the point when you installed your first plugin or custom theme or activated hosting features on {{strong}}%(atomicTransferDate)s{{/strong}}. All your posts, pages and media will be deleted.',
+			{
+				args: { atomicTransferDate },
+				components: {
+					// eslint-disable-next-line wpcalypso/jsx-classname-namespace
+					strong: <strong className="is-highlighted" />,
+				},
+			}
+		);
+	}
+
+	renderEmptySiteConfirmationContent() {
+		const { translate, selectedSiteSlug, atomicTransfer } = this.props;
+		const { checkOne, checkTwo, isEmptyButtonBusy } = this.state;
+		const shouldEnableButton = checkOne && checkTwo;
+		const hasAtomicTransferCompleted = Object.keys( atomicTransfer ).length > 0;
+
+		return (
+			<div className=" main main-column start-over start-over__confirm">
+				<Card>
+					<FormattedHeader
+						brandFont
+						headerText={ translate( 'Proceed with caution - this will delete all your content' ) }
+						subHeaderText={ this.getSubheaderText( hasAtomicTransferCompleted ) }
+					/>
+					<p>
+						{ translate(
+							'Please {{strong}}confirm and check{{/strong}} the following items before you continue with emptying your site:',
+							{ components: { strong: <strong /> } }
+						) }
+					</p>
+					{ this.getCheckboxContent( hasAtomicTransferCompleted ) }
+
+					<div className="start-over__backups">
+						<h4>{ translate( 'Would you like to download the backup of your site?' ) }</h4>
+						<p>
+							{ translate(
+								'If you change your mind later or want to secure your data, you can download a backup.'
+							) }
+						</p>
+						<ExternalLink icon href={ `/backup/${ selectedSiteSlug }` }>
+							{ translate( 'Go to your backups' ) }
+						</ExternalLink>
+					</div>
+					<Button
+						primary
+						scary
+						disabled={ ! shouldEnableButton }
+						onClick={ this.clickEmptySiteButton }
+						className="start-over__empty-site-button"
+						busy={ isEmptyButtonBusy }
+					>
+						{ translate( 'Empty Site' ) }
+					</Button>
+				</Card>
+			</div>
+		);
+	}
+
+	render() {
+		const { selectedSiteSlug, translate } = this.props;
 
 		return (
 			<>
@@ -144,7 +194,7 @@ class StartOver extends Component {
 				>
 					<HeaderCake backHref={ '/settings/general/' + selectedSiteSlug }>Start Over</HeaderCake>
 					<Card>
-						<FormattedHeader brandFont headerText={ translate( 'Start Over' ) } />
+						{ /* <FormattedHeader brandFont headerText={ translate( 'Start Over' ) } /> */ }
 						<p>
 							{ translate(
 								"If you want a site but don't want any of the posts and pages you have now, " +
@@ -159,55 +209,22 @@ class StartOver extends Component {
 						</p>
 					</Card>
 				</div>
-				<div className=" main main-column start-over start-over__confirm">
-					<Card>
-						<FormattedHeader
-							brandFont
-							headerText={ translate( 'Proceed with caution' ) }
-							subHeaderText={ subHeaderText }
-						/>
-						<p>
-							{ translate(
-								'Please {{strong}}confirm and check{{/strong}} the following items before you continue with emptying your site:',
-								{ components: { strong: <strong /> } }
-							) }
-						</p>
-						{ this.getCheckboxConsent() }
-
-						<div className="start-over__backups">
-							<h4>{ translate( 'Would you like to download the backup of your site?' ) }</h4>
-							<p>
-								{ translate(
-									'If you change your mind later or want to secure your data, you can download a backup.'
-								) }
-							</p>
-							<ExternalLink icon href={ `/backup/${ selectedSiteSlug }` }>
-								{ translate( 'Go to your backups' ) }
-							</ExternalLink>
-						</div>
-						<Button
-							primary
-							scary
-							disabled={ ! canClickEmptyButton }
-							onClick={ this.clickEmptySiteButton }
-							className="start-over__empty-site-button"
-							busy={ isEmptyButtonBusy }
-						>
-							{ translate( 'Empty Site' ) }
-						</Button>
-					</Card>
-				</div>
+				{ this.renderEmptySiteConfirmationContent() }
 			</>
 		);
 	}
 }
 
-export default connect( ( state ) => {
-	const siteId = getSelectedSiteId( state );
+export default connect(
+	( state ) => {
+		const siteId = getSelectedSiteId( state );
 
-	return {
-		selectedSiteSlug: getSelectedSiteSlug( state ),
-		atomicTransfer: getAtomicTransfer( state, siteId ),
-		siteId,
-	};
-} )( localize( StartOver ) );
+		return {
+			selectedSiteSlug: getSelectedSiteSlug( state ),
+			atomicTransfer: getAtomicTransfer( state, siteId ),
+			isAtomicSite: isSiteAutomatedTransfer( state, siteId ),
+			siteId,
+		};
+	},
+	{ fetchAtomicTransfer }
+)( localize( withLocalizedMoment( StartOver ) ) );
