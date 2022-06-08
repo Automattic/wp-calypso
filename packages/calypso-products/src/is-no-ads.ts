93782f273c79e8e2f1d09eba0eff288f20109452
David Rothstein <david.rothstein@automattic.com>
2022-06-08 14:48:05 -0400

    Use isNoAds() in isAddOn() (#64434)

--- packages/calypso-products/src/is-no-ads.ts
+++ packages/calypso-products/src/is-no-ads.ts
@@ -1,6 +1,7 @@
 import { camelOrSnakeSlug } from './camel-or-snake-slug';
+import { PRODUCT_NO_ADS } from './constants';
 import type { WithCamelCaseSlug, WithSnakeCaseSlug } from './types';
 
 export function isNoAds( product: WithCamelCaseSlug | WithSnakeCaseSlug ): boolean {
-	return 'no-adverts/no-adverts.php' === camelOrSnakeSlug( product );
+	return PRODUCT_NO_ADS === camelOrSnakeSlug( product );
 }
