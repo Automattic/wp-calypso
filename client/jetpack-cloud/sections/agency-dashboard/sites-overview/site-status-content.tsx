8dffb3465ef7f37c9e274c4c8659c6b2e61bc479
Yashwin <yashwinpoojary@gmail.com>
2022-06-08 11:11:24 +0530

    Agency dashboard style fixes (#64415)
    * Add box shadow to the search similar to filter
    
    * Add overlay for site name at the end
    
    * Overlay for large screen
    
    * Filter margin fix for mobile devices
    
    * Fix overlay on table

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content.tsx
@@ -77,6 +77,7 @@ export default function SiteStatusContent( {
 		return (
 			<>
 				<span className="sites-overview__row-text">{ value.url }</span>
+				<span className="sites-overview__overlay"></span>
 				{ errorContent }
 			</>
 		);
