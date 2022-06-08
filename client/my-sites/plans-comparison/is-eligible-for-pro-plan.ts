887ead4a9ff284be09142d5556f306bf07030012
Claudiu Filip <claudiucelfilip@gmail.com>
2022-06-08 18:14:32 +0300

    Add plan check to the Pro plan eligibility checker

--- client/my-sites/plans-comparison/is-eligible-for-pro-plan.ts
+++ client/my-sites/plans-comparison/is-eligible-for-pro-plan.ts
@@ -1,6 +1,8 @@
 import { isEnabled } from '@automattic/calypso-config';
+import { isPro } from '@automattic/calypso-products';
 import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 import isSiteWPForTeams from 'calypso/state/selectors/is-site-wpforteams';
+import { getCurrentPlan } from 'calypso/state/sites/plans/selectors';
 import { isJetpackSite } from 'calypso/state/sites/selectors';
 import type { AppState } from 'calypso/types';
 
@@ -11,6 +13,11 @@ export function isEligibleForProPlan( state: AppState, siteId?: number ): boolea
 			isSiteWPForTeams( state, siteId ) )
 	) {
 		return false;
+	}
+
+	const currentPlan = getCurrentPlan( state, siteId );
+	if ( currentPlan && isPro( currentPlan ) ) {
+		return true;
 	}
 
 	return isEnabled( 'plans/pro-plan' );
