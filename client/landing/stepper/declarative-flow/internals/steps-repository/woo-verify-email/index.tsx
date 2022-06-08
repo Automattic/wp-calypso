bb853ed1f00056b87f48ec0a104cce81c2100eb9
Valter Lorran <valterlorran@hotmail.com>
2022-06-08 16:49:50 +0100

    Adding skip label text

--- client/landing/stepper/declarative-flow/internals/steps-repository/woo-verify-email/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/woo-verify-email/index.tsx
@@ -5,14 +5,19 @@ import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import { useSelector } from 'react-redux';
 import FormattedHeader from 'calypso/components/formatted-header';
+import { useSiteSlugParam } from 'calypso/landing/stepper/hooks/use-site-slug-param';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { UserData } from 'calypso/lib/user/user';
 import { getCurrentUser } from 'calypso/state/current-user/selectors';
 import type { Step } from '../../types';
 import './style.scss';
 
+function redirect( to: string ) {
+	window.location.href = to;
+}
+
 const WooVerifyEmail: Step = function WooVerifyEmail( { navigation } ) {
-	const { goBack, goNext } = navigation;
+	const { goBack } = navigation;
 	const { __ } = useI18n();
 	const user = useSelector( getCurrentUser ) as UserData;
 
@@ -23,7 +28,7 @@ const WooVerifyEmail: Step = function WooVerifyEmail( { navigation } ) {
 					{ __( 'Resend verification email' ) }
 				</Button>
 				<br />
-				<Button className="woo-verify-email__link" borderless href="https://anchor.fm">
+				<Button className="woo-verify-email__link" borderless href="#">
 					{ __( 'Edit email address' ) }
 				</Button>
 			</div>
@@ -52,12 +57,16 @@ const WooVerifyEmail: Step = function WooVerifyEmail( { navigation } ) {
 		{ br: createElement( 'br' ) }
 	);
 
+	const siteSlug = useSiteSlugParam();
+
 	return (
 		<StepContainer
 			stepName={ 'woo-verify-email-step' }
 			goBack={ goBack }
-			goNext={ goNext }
+			goNext={ () => redirect( `/home/${ siteSlug }` ) }
 			isHorizontalLayout={ false }
+			skipButtonAlign={ 'top' }
+			skipLabelText={ __( 'Confirm my email later' ) }
 			formattedHeader={
 				<FormattedHeader
 					id={ 'woo-verify-email-title-header' }
@@ -67,9 +76,6 @@ const WooVerifyEmail: Step = function WooVerifyEmail( { navigation } ) {
 			}
 			stepContent={ getContent() }
 			recordTracksEvent={ recordTracksEvent }
-			hideBack
-			hideSkip
-			hideNext
 		/>
 	);
 };
