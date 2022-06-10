2d31a9636cd58d74518c34511a2aceca43169db9
Matthew Reishus <mreishus@users.noreply.github.com>
2022-06-10 13:10:42 -0500

    atomic eligibility-warnings: Add more detailed message for excessive disk space (#64388)

---
+++ client/blocks/eligibility-warnings/excessive-disk-space.tsx
@@ -0,0 +1,41 @@
+import { isBusinessPlan, isEcommercePlan, isProPlan } from '@automattic/calypso-products';
+import { localize, LocalizeProps } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
+import PlanStorage from 'calypso/blocks/plan-storage';
+import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
+import { getSiteSlug, getSitePlanSlug } from 'calypso/state/sites/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+
+const ExcessiveDiskSpace = ( { translate }: { translate: LocalizeProps[ 'translate' ] } ) => {
+	const selectedSiteId = useSelector( getSelectedSiteId );
+
+	const siteSlug = useSelector( ( state ) => getSiteSlug( state, selectedSiteId ) );
+	const canUserUpgrade = useSelector( ( state ) =>
+		canCurrentUser( state, selectedSiteId ?? 0, 'manage_options' )
+	);
+	const sitePlanSlug = useSelector( ( state ) => getSitePlanSlug( state, selectedSiteId ) ?? '' );
+
+	const planHasTopStorageSpace =
+		isBusinessPlan( sitePlanSlug ) || isEcommercePlan( sitePlanSlug ) || isProPlan( sitePlanSlug );
+	const displayUpgradeLink = canUserUpgrade && ! planHasTopStorageSpace;
+
+	return (
+		<div>
+			{ translate( 'Your site does not have enough available storage space.' ) }
+			<div className="eligibility-warnings__plan-storage-wrapper">
+				<PlanStorage siteId={ selectedSiteId }>{ null }</PlanStorage>
+			</div>
+			{ displayUpgradeLink &&
+				translate(
+					'Please {{a}}purchase a plan with additional storage{{/a}} or contact our support team for help.',
+					{
+						components: {
+							a: <a href={ `/plans/${ siteSlug }` } />,
+						},
+					}
+				) }
+			{ ! displayUpgradeLink && translate( 'Please contact our support team for help.' ) }
+		</div>
+	);
+};
+export default localize( ExcessiveDiskSpace );
