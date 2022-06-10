62b49153375f7a384147ca56d34c7410f5a98c72
Claudiu Filip <claudiucelfilip@gmail.com>
2022-06-10 11:06:16 +0300

    Update isEligibleForProPlan selector to check if siteId is Pro as well (#64457)
    * Add plan check to the Pro plan eligibility checker
    
    * Add server-friendly plan check
    
    * Remove unnecessary and faulty current plan fetch

--- client/my-sites/plans-comparison/is-eligible-for-pro-plan.ts
+++ client/my-sites/plans-comparison/is-eligible-for-pro-plan.ts
@@ -1,16 +1,24 @@
 import { isEnabled } from '@automattic/calypso-config';
+import { isPro } from '@automattic/calypso-products';
 import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 import isSiteWPForTeams from 'calypso/state/selectors/is-site-wpforteams';
-import { isJetpackSite } from 'calypso/state/sites/selectors';
+import { isJetpackSite, getSite } from 'calypso/state/sites/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import type { AppState } from 'calypso/types';
 
-export function isEligibleForProPlan( state: AppState, siteId?: number ): boolean {
+export function isEligibleForProPlan( state: AppState, siteId?: number | null ): boolean {
 	if (
 		siteId &&
 		( ( isJetpackSite( state, siteId ) && ! isAtomicSite( state, siteId ) ) ||
 			isSiteWPForTeams( state, siteId ) )
 	) {
 		return false;
+	}
+	siteId = siteId || getSelectedSiteId( state );
+	const selectedSite = getSite( state, siteId || '' );
+	const currentPlan = selectedSite?.plan;
+	if ( currentPlan && isPro( currentPlan ) ) {
+		return true;
 	}
 
 	return isEnabled( 'plans/pro-plan' );
