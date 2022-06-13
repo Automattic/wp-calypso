4cff049513fa24e302ca47fb2644f36a6d028c39
Wesley Berrêdo <Wberredo@gmail.com>
2022-06-13 15:11:57 -0400

    Plugin Install: Add more additional messages (#64534)
    * Add additional progress messages.
    
    * Show all additional messages first and then start to repeat.

--- client/my-sites/marketplace/pages/marketplace-plugin-install/index.tsx
+++ client/my-sites/marketplace/pages/marketplace-plugin-install/index.tsx
@@ -261,6 +261,14 @@ const MarketplacePluginInstall = ( { productSlug }: MarketplacePluginInstallProp
 			translate( 'Still working' ),
 			translate( 'Wheels are in motion' ),
 			translate( 'Working magic' ),
+			translate( 'Putting the pieces together' ),
+			translate( 'Assembling the parts' ),
+			translate( 'Stacking the building blocks' ),
+			translate( 'Getting our ducks in a row' ),
+			translate( 'Initiating countdown' ),
+			translate( 'Flipping the switches' ),
+			translate( 'Unlocking potential' ),
+			translate( 'Gears are turning' ),
 		],
 		[ translate ]
 	);
