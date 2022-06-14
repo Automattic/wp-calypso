f459fe759a9079f7c8dbb30f33c8fed86535e836
Fredrik Rombach Ekelund <fredrik@fredrik.computer>
2022-06-14 14:05:48 +0200

    Convert useGetTitanMailboxAvailability to Typescript

--- client/data/emails/use-get-titan-mailbox-availability.ts
+++ client/data/emails/use-get-titan-mailbox-availability.ts
@@ -1,7 +1,8 @@
 import { useQuery } from 'react-query';
 import wpcom from 'calypso/lib/wp';
+import type { UseQueryOptions } from 'react-query';
 
-export const getCacheKey = ( domain, mailboxName ) => [
+export const getCacheKey = ( domain: string, mailboxName: string ) => [
 	'emails',
 	'titan',
 	domain,
@@ -9,23 +10,23 @@ export const getCacheKey = ( domain, mailboxName ) => [
 	mailboxName,
 ];
 
-/**
- * A list of error statuses that are seen as final and shouldn't be retried for this query
- *
- * @type {number[]}
- */
+// A list of error statuses that are seen as final and shouldn't be retried for this query
 const finalErrorStatuses = [ 400, 401, 403, 409 ];
 
 /**
  * Checks whether a mailbox name/domain i.e. email address is available for creation.
  *
- * @param {string} domainName The domain name of the mailbox
- * @param {string} mailboxName The mailbox name
- * @param {object} queryOptions Optional options to pass to the underlying query engine
- * @returns {{ data, error, isLoading: boolean ...}} Returns various parameters piped from `useQuery`
+ * @param domainName The domain name of the mailbox
+ * @param mailboxName The mailbox name
+ * @param queryOptions Optional options to pass to the underlying query engine
+ * @returns Returns various parameters piped from `useQuery`
  */
-export const useGetTitanMailboxAvailability = ( domainName, mailboxName, queryOptions = {} ) => {
-	return useQuery(
+export const useGetTitanMailboxAvailability = (
+	domainName: string,
+	mailboxName: string,
+	queryOptions: UseQueryOptions< any, any > = {}
+) => {
+	return useQuery< any, any >(
 		getCacheKey( domainName, mailboxName ),
 		() =>
 			wpcom.req.get( {
