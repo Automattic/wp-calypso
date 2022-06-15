7994b1dc4cb0e45234ae6fa766bb44ecd5490cf5
Claudiu Filip <claudiucelfilip@gmail.com>
2022-06-15 11:38:23 +0300

    Change Pro plan mention on domain search (#64615)
    * Use starter plan copy overrides in all sitations
    
    * Mention paid annual plans instead of just paid

--- client/components/domains/reskin-side-explainer/index.jsx
+++ client/components/domains/reskin-side-explainer/index.jsx
@@ -1,127 +1,79 @@
 import i18n, { getLocaleSlug, localize } from 'i18n-calypso';
 import { Component } from 'react';
 import { connect } from 'react-redux';
-import { isStarterPlanEnabled } from 'calypso/my-sites/plans-comparison';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 import './style.scss';
 
 class ReskinSideExplainer extends Component {
-	getStarterPlanOverrides( isEnLocale ) {
-		const { translate } = this.props;
-
-		const fallbackTitle = translate(
-			'Get a free one-year domain registration with any paid plan.'
-		);
-
-		const isPaidPlan = [ 'starter', 'pro' ].includes( this.props.flowName );
-		const hasFreeTitle =
-			i18n.hasTranslation(
-				'Get a {{b}}free{{/b}} one-year domain registration with any paid plan.'
-			) || isEnLocale;
-
-		const freeTitle = hasFreeTitle
-			? translate( 'Get a {{b}}free{{/b}} one-year domain registration with any paid plan.', {
-					components: { b: <strong /> },
-			  } )
-			: fallbackTitle;
-
-		const hasPaidTitle =
-			i18n.hasTranslation( 'Get a {{b}}free{{/b}} one-year domain registration with your plan.' ) ||
-			isEnLocale;
-
-		const paidTitle = hasPaidTitle
-			? translate( 'Get a {{b}}free{{/b}} one-year domain registration with your plan.', {
-					components: { b: <strong /> },
-			  } )
-			: fallbackTitle;
-
-		const title = isPaidPlan ? paidTitle : freeTitle;
-
-		const hasFreeSubtitle =
-			i18n.hasTranslation(
-				'Use the search tool on this page to find a domain you love, then select a paid plan.'
-			) || isEnLocale;
-
-		const freeSubtitle =
-			hasFreeSubtitle &&
-			translate(
-				'Use the search tool on this page to find a domain you love, then select a paid plan.'
-			);
-
-		const paidSubtitle = translate( 'Use the search tool on this page to find a domain you love.' );
-
-		let subtitle = isPaidPlan ? paidSubtitle : freeSubtitle;
-
-		let subtitle2 = translate(
-			'We’ll pay the first year’s domain registration fees for you, simple as that!'
-		);
-
-		if ( ! subtitle ) {
-			subtitle = subtitle2;
-			subtitle2 = null;
-		}
-		return { title, subtitle, subtitle2, ctaText: false };
-	}
 	getStrings() {
 		const { type, translate } = this.props;
 
 		let title;
+		let freeTitle;
+		let paidTitle;
 		let subtitle;
+		let freeSubtitle;
+		let hasFreeSubtitle;
+		let paidSubtitle;
 		let subtitle2;
 		let ctaText;
 
+		const isPaidPlan = [
+			'starter',
+			'pro',
+			'personal',
+			'premium',
+			'business',
+			'ecommerce',
+			'domain',
+		].includes( this.props.flowName );
+
 		const isEnLocale = [ 'en', 'en-gb' ].includes( getLocaleSlug() );
-		const showNewTitle =
-			i18n.hasTranslation( 'Get your domain {{b}}free{{/b}} with WordPress Pro' ) || isEnLocale;
-
-		const showNewSubtitle =
-			i18n.hasTranslation(
-				'Use the search tool on this page to find a domain you love, then select the {{b}}WordPress Pro{{/b}} plan.'
-			) || isEnLocale;
-
-		const newSubtitleCopy =
-			'pro' === this.props.flowName
-				? translate( 'Use the search tool on this page to find a domain you love.' )
-				: translate(
-						'Use the search tool on this page to find a domain you love, then select the {{b}}WordPress Pro{{/b}} plan.',
-						{
-							components: { b: <strong /> },
-						}
-				  );
 
 		switch ( type ) {
 			case 'free-domain-explainer':
-				title = showNewTitle
-					? translate( 'Get your domain {{b}}free{{/b}} with WordPress Pro', {
-							components: { b: <strong /> },
-					  } )
-					: translate(
-							'Get a {{b}}free{{/b}} one-year domain registration with your WordPress Pro annual plan.',
-							{
-								components: { b: <strong /> },
-							}
-					  );
+				freeTitle = translate(
+					'Get a {{b}}free{{/b}} one-year domain registration with any paid annual plan.',
+					{
+						components: { b: <strong /> },
+					}
+				);
 
-				subtitle = showNewSubtitle
-					? newSubtitleCopy
-					: translate( "You can claim your free custom domain later if you aren't ready yet." );
+				paidTitle = translate(
+					'Get a {{b}}free{{/b}} one-year domain registration with your plan.',
+					{
+						components: { b: <strong /> },
+					}
+				);
 
-				subtitle2 =
-					showNewSubtitle &&
-					translate(
-						'We’ll pay the first year’s domain registration fees for you, simple as that!'
-					);
-				ctaText = ! showNewSubtitle && translate( 'Choose my domain later' );
+				title = isPaidPlan ? paidTitle : freeTitle;
 
-				//todo: use only getStarterPlanOverrides() after Starter plan deploy
-				if ( isStarterPlanEnabled() ) {
-					const overrides = this.getStarterPlanOverrides( isEnLocale );
-					title = overrides.title;
-					subtitle = overrides.subtitle;
-					subtitle2 = overrides.subtitle2;
-					ctaText = overrides.ctaText;
+				hasFreeSubtitle =
+					i18n.hasTranslation(
+						'Use the search tool on this page to find a domain you love, then select any paid annual plan.'
+					) || isEnLocale;
+
+				freeSubtitle = hasFreeSubtitle
+					? translate(
+							'Use the search tool on this page to find a domain you love, then select any paid annual plan.'
+					  )
+					: null;
+
+				paidSubtitle = translate( 'Use the search tool on this page to find a domain you love.' );
+
+				subtitle = isPaidPlan ? paidSubtitle : freeSubtitle;
+
+				subtitle2 = translate(
+					'We’ll pay the first year’s domain registration fees for you, simple as that!'
+				);
+
+				if ( ! subtitle ) {
+					subtitle = subtitle2;
+					subtitle2 = null;
 				}
+
+				ctaText = false;
 
 				break;
 
