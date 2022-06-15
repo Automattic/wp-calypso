def280ff935d7bbfeebdf6a1054fa5aba3b22c59
Griffith Chen <griffith.chen@automattic.com>
2022-06-15 11:07:28 +0800

    Prevent selected design preview for Anchor sites

--- client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/index.tsx
@@ -345,7 +345,8 @@ const designSetup: Step = function DesignSetup( { navigation, flow } ) {
 		return null;
 	}
 
-	if ( selectedDesign && ! showGeneratedDesigns ) {
+	// In Anchor sites, full-screen design previews are disabled.
+	if ( selectedDesign && ! showGeneratedDesigns && ! isAnchorSite ) {
 		const isBlankCanvas = isBlankCanvasDesign( selectedDesign );
 		const designTitle = isBlankCanvas ? translate( 'Blank Canvas' ) : selectedDesign.title;
 		const shouldUpgrade = selectedDesign.is_premium && ! isPremiumThemeAvailable;
