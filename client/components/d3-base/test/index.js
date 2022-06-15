2e14c183ebdcbbad3691627738e0a111b761bf1d
Praveen <spraveenitpro@gmail.com>
2022-06-14 23:41:10 -0700

    Migrate from enzyme to Testing library for d3-base component

--- client/components/d3-base/test/index.js
+++ client/components/d3-base/test/index.js
@@ -2,26 +2,21 @@
  * @jest-environment jsdom
  */
 
-import { shallow, mount } from 'enzyme';
+import { render } from '@testing-library/react';
 import D3Base from '..';
 
 const noop = () => {};
 
 describe( 'D3base', () => {
-	const shallowWithoutLifecycle = ( arg ) => shallow( arg, { disableLifecycleMethods: true } );
-
 	test( 'should have d3-base CSS class', () => {
-		const base = shallowWithoutLifecycle( <D3Base drawChart={ noop } getParams={ noop } /> );
-
-		expect( base.find( '.d3-base' ).length ).toBe( 1 );
+		const { container } = render( <D3Base drawChart={ noop } getParams={ noop } /> );
+		expect( container.firstChild ).toHaveClass( 'd3-base' );
 	} );
 
 	test( 'should render an svg', () => {
 		const getParams = () => ( { width: 100, height: 100 } );
-
-		const base = mount( <D3Base drawChart={ noop } getParams={ getParams } /> );
-
-		expect( base.render().find( 'svg' ).length ).toBe( 1 );
+		const { container } = render( <D3Base drawChart={ noop } getParams={ getParams } /> );
+		expect( container.getElementsByTagName( 'svg' ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should render a result of the drawChart prop', () => {
@@ -34,9 +29,8 @@ describe( 'D3base', () => {
 			height: 100,
 		} );
 
-		const base = mount( <D3Base drawChart={ drawChart } getParams={ getParams } /> );
-
-		expect( base.render().find( 'circle' ).length ).toBe( 1 );
+		const { container } = render( <D3Base drawChart={ drawChart } getParams={ getParams } /> );
+		expect( container.getElementsByTagName( 'circle' ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should pass a property of getParams output to drawChart function', () => {
@@ -48,8 +42,7 @@ describe( 'D3base', () => {
 			tagName: 'circle',
 		} );
 
-		const base = mount( <D3Base drawChart={ drawChart } getParams={ getParams } /> );
-
-		expect( base.render().find( 'circle' ).length ).toBe( 1 );
+		const { container } = render( <D3Base drawChart={ drawChart } getParams={ getParams } /> );
+		expect( container.getElementsByTagName( 'circle' ) ).toHaveLength( 1 );
 	} );
 } );
