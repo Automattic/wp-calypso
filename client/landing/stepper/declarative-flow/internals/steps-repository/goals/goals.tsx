1132f7c41be7c781b75e099db9111566d0735cc6
Maciej Grabowski <mashikag@users.noreply.github.com>
2022-06-16 17:55:38 +0200

    Implement 'Continue' button for goals capture screen (#64656)

--- client/landing/stepper/declarative-flow/internals/steps-repository/goals/goals.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/goals/goals.tsx
@@ -2,35 +2,35 @@ import { Onboard } from '@automattic/data-stores';
 import { useTranslate } from 'i18n-calypso';
 import type { Goal } from './types';
 
-const GoalKey = Onboard.GoalKey;
+const SiteGoal = Onboard.SiteGoal;
 
 export const useGoals = (): Goal[] => {
 	const translate = useTranslate();
 
 	const goals: Goal[] = [
 		{
-			key: GoalKey.Write,
+			key: SiteGoal.Write,
 			title: translate( 'Write and publish' ),
 		},
 		{
-			key: GoalKey.Sell,
+			key: SiteGoal.Sell,
 			title: translate( 'Sell goods or products' ),
 		},
 		{
-			key: GoalKey.Promote,
+			key: SiteGoal.Promote,
 			title: translate( 'Promote myself or my business' ),
 		},
 		{
-			key: GoalKey.DIFM,
+			key: SiteGoal.DIFM,
 			title: translate( 'Hire a professional to design my website' ),
 			isPremium: true,
 		},
 		{
-			key: GoalKey.Import,
+			key: SiteGoal.Import,
 			title: translate( 'Import my existing website content' ),
 		},
 		{
-			key: GoalKey.Other,
+			key: SiteGoal.Other,
 			title: translate( 'Other' ),
 		},
 	];
