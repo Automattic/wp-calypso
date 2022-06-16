110806a819dca23a6118518d7808145b37bec2a6
Rolando Perez <me@rolilink.com>
2022-06-16 13:22:00 -0500

    Marketplace: Separates categories from plugins (#64686)
    * updating routes to separate categories and search from plugin
    
    * categories menu correctly redirects to new routes
    
    * breadcrumbs link to new routes
    
    * update home lists path and analytics
    
    * cleanup
    
    * fixes e2e test
    
    * trying to fix e2e test

--- packages/calypso-e2e/src/lib/pages/plugins-page.ts
+++ packages/calypso-e2e/src/lib/pages/plugins-page.ts
@@ -15,7 +15,7 @@ const selectors = {
 	pluginTitleOnSection: ( section: string, plugin: string ) =>
 		`.plugins-browser-list:has(.plugins-browser-list__title.${ section }) :text-is("${ plugin }")`,
 	sectionTitles: '.plugins-browser-list__title',
-	browseAllPopular: 'a[href^="/plugins/popular"]',
+	browseAllPopular: 'a[href^="/plugins/browse/popular"]',
 	breadcrumb: ( section: string ) => `.plugins-browser__header a:text("${ section }") `,
 	pricingToggle: ':text("Monthly Price"), :text("Annual Price")',
 	monthlyPricingSelect: 'a[data-bold-text^="Monthly price"]',
