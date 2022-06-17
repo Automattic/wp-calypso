78030990c73f79b3b819979403a6e3fb18779ccd
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-06-17 19:36:08 +0200

    Integrate Analyzer data store with the light importer screens

--- client/landing/stepper/declarative-flow/internals/steps-repository/import-light/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/import-light/index.tsx
@@ -1,10 +1,17 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import { StepContainer } from '@automattic/onboarding';
+import { useSelect, useDispatch } from '@wordpress/data';
 import { useI18n } from '@wordpress/react-i18n';
-import React from 'react';
+import { delay } from 'lodash';
+import React, { useEffect, useState } from 'react';
 import Capture from 'calypso/blocks/import-light/capture';
+import Colors from 'calypso/blocks/import-light/colors';
+import Scanning from 'calypso/blocks/import-light/scanning';
+import Summary from 'calypso/blocks/import-light/summary';
 import DocumentHead from 'calypso/components/data/document-head';
+import { ANALYZER_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import type { ProgressState } from './types';
 import type { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
 import './style.scss';
 
@@ -12,8 +19,48 @@ const ImportLight: Step = function ImportStep( props ) {
 	const { __ } = useI18n();
 	const { navigation } = props;
 
+	const [ url, setUrl ] = useState( '' );
+	const [ progressState, setProgressState ] = useState< ProgressState >( 'capture' );
+	const { analyzeColors } = useDispatch( ANALYZER_STORE );
+	const colorsData = useSelect( ( select ) => select( ANALYZER_STORE ).getSiteColors( url ) );
+	const fetchingColorsInProgress = useSelect( ( select ) =>
+		select( ANALYZER_STORE ).isSiteColorsInAnalysis()
+	);
+
+	useEffect( () => {
+		if ( progressState === 'scanning' && ! fetchingColorsInProgress ) setProgressState( 'colors' );
+	} );
+
+	function onUrlEnter( url: string ) {
+		setUrl( url );
+		analyzeColors( url );
+		setProgressState( 'scanning' );
+	}
+
+	function onColorAnimationFinish() {
+		delay( () => setProgressState( 'summary' ), 2000 );
+	}
+
 	function renderStepContent() {
-		return <Capture />;
+		switch ( progressState ) {
+			case 'scanning':
+				return <Scanning website={ url } />;
+
+			case 'colors':
+				return (
+					<Colors
+						colors={ colorsData?.logo || [] }
+						onColorAnimationFinish={ onColorAnimationFinish }
+					/>
+				);
+
+			case 'summary':
+				return <Summary />;
+
+			case 'capture':
+			default:
+				return <Capture onInputEnter={ onUrlEnter } />;
+		}
 	}
 
 	return (
