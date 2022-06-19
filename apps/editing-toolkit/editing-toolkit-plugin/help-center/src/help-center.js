bc3bc27d6ff38410ce7bcd8c1cd0913a7e1e8276
Tony Arcangelini <33258733+arcangelini@users.noreply.github.com>
2022-06-20 00:23:57 +0200

    HelpCenter: fix some accessibility bugs (#64761)
    * HelpCenter: fix some accessibility bugs
    
    * Fix VoiceOver

--- apps/editing-toolkit/editing-toolkit-plugin/help-center/src/help-center.js
+++ apps/editing-toolkit/editing-toolkit-plugin/help-center/src/help-center.js
@@ -35,13 +35,14 @@ function HelpCenterContent() {
 	};
 
 	const content = (
-		<span className="etk-help-center">
-			<Button
-				className={ cx( 'entry-point-button', { 'is-active': show } ) }
-				onClick={ handleToggleHelpCenter }
-				icon={ <HelpIcon newItems={ showHelpIconDot } /> }
-			></Button>
-		</span>
+		<Button
+			className={ cx( 'entry-point-button', 'help-center', { 'is-active': show } ) }
+			onClick={ handleToggleHelpCenter }
+			icon={ <HelpIcon newItems={ showHelpIconDot } /> }
+			label="Help"
+			aria-pressed={ show ? true : false }
+			aria-expanded={ show ? true : false }
+		></Button>
 	);
 
 	return (
