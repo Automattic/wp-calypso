69326ec396bda86cef838e118970158c58060b8a
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-06-20 17:53:39 +0200

    Rename and update scanning comp for better children el handling

---
+++ client/blocks/import-light/analysis-progress/index.tsx
@@ -0,0 +1,31 @@
+import { ProgressBar } from '@automattic/components';
+import { Title, Progress } from '@automattic/onboarding';
+import type * as React from 'react';
+import './style.scss';
+
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+
+interface Props {
+	percentage: number;
+}
+const AnalysisProgress: React.FunctionComponent< Props > = ( props ) => {
+	const { percentage, children } = props;
+
+	const elements = Array.isArray( children ) ? children : [ children ];
+	const titleEl = elements[ 0 ].type === Title ? elements[ 0 ] : undefined;
+	const otherEl = titleEl ? elements.slice( 1 ) : elements;
+
+	return (
+		<div className="import-layout__center import-light__progress">
+			<div className="import__heading-center">
+				<Progress>
+					{ titleEl }
+					{ percentage && <ProgressBar value={ percentage } compact={ true } /> }
+					{ otherEl }
+				</Progress>
+			</div>
+		</div>
+	);
+};
+
+export default AnalysisProgress;
