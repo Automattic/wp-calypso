23506196c6da6ec1e6fb8b8b2afa991bfd2b833f
mmtr <miguel.torres@automattic.com>
2022-06-21 16:42:16 +0200

    Backup: Display upsell for Atomic sites missing the needed plan

--- client/lib/jetpack/wpcom-upsell-controller.tsx
+++ client/lib/jetpack/wpcom-upsell-controller.tsx
@@ -1,5 +1,6 @@
 import { isEnabled } from '@automattic/calypso-config';
 import BusinessATSwitch from 'calypso/components/jetpack/business-at-switch';
+import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
 import { isJetpackSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import type { Context } from './types';
@@ -12,9 +13,14 @@ export default function upsellSwitch(
 		const getState = context.store.getState;
 		const siteId = getSelectedSiteId( getState() );
 		const isJetpack = isJetpackSite( getState(), siteId );
+		const isAtomic = isSiteAutomatedTransfer( getState(), siteId );
 
-		if ( ! isJetpack && ! isEnabled( 'jetpack-cloud' ) && context.primary ) {
-			context.primary = <BusinessATSwitch UpsellComponent={ UpsellComponent } />;
+		if ( ! isEnabled( 'jetpack-cloud' ) && context.primary ) {
+			if ( isAtomic ) {
+				context.primary = <UpsellComponent />;
+			} else if ( ! isJetpack ) {
+				context.primary = <BusinessATSwitch UpsellComponent={ UpsellComponent } />;
+			}
 		}
 
 		next();
