fc8fc9ec4b6596e0baf0520c6e7c45f5a3a9772d
Fredrik Rombach Ekelund <fredrik@fredrik.computer>
2022-06-21 17:04:25 +0200

    Make all registered domains eligible for Google Workspace within 15 mins after purchase

--- client/lib/gsuite/gsuite-supported-domain.js
+++ client/lib/gsuite/gsuite-supported-domain.js
@@ -6,13 +6,27 @@ import { hasGSuiteWithUs } from './has-gsuite-with-us';
 /**
  * Filters a list of domains by the domains that eligible for G Suite.
  *
- * @param {Array} domains - list of domain objects
- * @returns {Array} - the list of domains that are eligible for G Suite
+ * @param {import('calypso/lib/domains/types').ResponseDomain[]} domains - list of domain objects
+ * @returns {import('calypso/lib/domains/types').ResponseDomain[]} - the list of domains that are eligible for G Suite
  */
 export function getGSuiteSupportedDomains( domains ) {
 	return domains.filter( function ( domain ) {
 		if ( hasGSuiteWithAnotherProvider( domain ) ) {
 			return false;
+		}
+
+		// If the domain is registered through us, there is a provisioning period when
+		// `hasWpcomNameservers` will be false. We still want to let users buy Google Workspace
+		// during that period, even if we normally wouldn't let them under these conditions.
+		// Therefore, we check those conditions and only return `gsuiteIsUnavailable` if the
+		// registration happened less than 15 minutes ago. 15 minutes is an arbitrary number.
+		if ( isRegisteredDomain( domain ) && ! domain.hasWpcomNameservers ) {
+			const registeredTimestamp = Date.parse( domain.registrationDate );
+			const timeSinceRegistration = Date.now() - registeredTimestamp;
+
+			if ( timeSinceRegistration < 15 * 60 * 1000 ) {
+				return true;
+			}
 		}
 
 		const isHostedOnWpcom =
@@ -29,7 +43,7 @@ export function getGSuiteSupportedDomains( domains ) {
 /**
  * Given a list of domains does one of them support G Suite
  *
- * @param {Array} domains - list of domain objects
+ * @param {import('calypso/lib/domains/types').ResponseDomain[]} domains - list of domain objects
  * @returns {boolean} - Does list of domains contain a G Suited supported domain
  */
 export function hasGSuiteSupportedDomain( domains ) {
