c05881695f059a3028383c92e227b67e95a0d37a
Edwin Takahashi <egao@outlook.com>
2022-06-21 09:46:38 -0700

    Revise test step description and inputs

--- client/lib/phone-validation/test/index.jsx
+++ client/lib/phone-validation/test/index.jsx
@@ -7,28 +7,31 @@ describe( 'Phone Validation Library', () => {
 		{ input: '+123456789a', expected: 'phone_number_contains_letters' },
 		{ input: '+(12345)6789', expected: 'phone_number_contains_special_characters' },
 		{ input: '+111111111', expected: 'phone_number_invalid' },
-	] )( 'Input $input shows error message $expected', function ( { input, expected } ) {
-		expect( phoneValidation( input ).error ).toStrictEqual( expected );
-	} );
+	] )(
+		'Invalid phone number ($input) results in error banner ($expected)',
+		function ( { input, expected } ) {
+			expect( phoneValidation( input ).error ).toStrictEqual( expected );
+		}
+	);
 
 	test.each( [
-		{ input: '+447941952721', expected: 'phone_number_valid' }, // Valid United Kingdom number
-		{ input: '+14631234567', expected: 'phone_number_valid' }, // Valid United States number
-		{ input: '+542231234567', expected: 'phone_number_valid' }, // Valid Argentina without leading 9
-		{ input: '+5492231234567', expected: 'phone_number_valid' }, // Valid Argentina with leading 9
-		{ input: '+38598123456', expected: 'phone_number_valid' }, // Valid Croatian number
-		{ input: '+4528123456', expected: 'phone_number_valid' }, // Valid Danish number
-		{ input: '+18761234567', expected: 'phone_number_valid' }, // Valid Jamaican number
-		{ input: '+84761234567', expected: 'phone_number_valid' }, // Valid Vietnamese (new) with leading 7
-		{ input: '+84361234567', expected: 'phone_number_valid' }, // Valid Vietnamese (new) with leading 3
-		{ input: '+840361234567', expected: 'phone_number_valid' }, // Valid Vietnamese (new) with leading 0
-		{ input: '+299239349', expected: 'phone_number_valid' }, // Valid Greenland with leading 2
-		{ input: '+97466641333', expected: 'phone_number_valid' }, // Valid Qatar with leading 2
-		{ input: '+9595512345', expected: 'phone_number_valid' }, // Valid Myanmar with 7 digits
-		{ input: '+95942612345', expected: 'phone_number_valid' }, // Valid Myanmar with 8 digits
-		{ input: '+959426123456', expected: 'phone_number_valid' }, // Valid Myanmar with 9 digits
-		{ input: '+5511961231234', expected: 'phone_number_valid' }, // Valid Brazil with 9 digits
-	] )( 'Input $input validates as valid number', function ( { input, expected } ) {
-		expect( phoneValidation( input ).info ).toStrictEqual( expected );
+		{ country: 'UK', input: '+447941952721' }, // Valid United Kingdom number
+		{ country: 'US', input: '+14631234567' }, // Valid United States number
+		{ country: 'AR', input: '+542231234567' }, // Valid Argentina without leading 9
+		{ country: 'AR', input: '+5492231234567' }, // Valid Argentina with leading 9
+		{ country: 'HR', input: '+38598123456' }, // Valid Croatian number
+		{ country: 'DK', input: '+4528123456' }, // Valid Danish number
+		{ country: 'JM', input: '+18761234567' }, // Valid Jamaican number
+		{ country: 'VN', input: '+84761234567' }, // Valid Vietnamese (new) with leading 7
+		{ country: 'VN', input: '+84361234567' }, // Valid Vietnamese (new) with leading 3
+		{ country: 'VN', input: '+840361234567' }, // Valid Vietnamese (new) with leading 0
+		{ country: 'GL', input: '+299239349' }, // Valid Greenland with leading 2
+		{ country: 'QA', input: '+97466641333' }, // Valid Qatar with leading 2
+		{ country: 'MM', input: '+9595512345' }, // Valid Myanmar with 7 digits
+		{ country: 'MM', input: '+95942612345' }, // Valid Myanmar with 8 digits
+		{ country: 'MM', input: '+959426123456' }, // Valid Myanmar with 9 digits
+		{ country: 'BR', input: '+5511961231234' }, // Valid Brazil with 9 digits
+	] )( 'International phone number ($country: $input) is validated', function ( { input } ) {
+		expect( phoneValidation( input ).info ).toStrictEqual( 'phone_number_valid' );
 	} );
 } );
