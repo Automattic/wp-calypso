42f90c10e8b0fa505b3dc0f771966bbdef077663
Valter Lorran <valterlorran@hotmail.com>
2022-06-21 10:40:13 -0300

    Instead of creating a new component from scratch, I will use the old one as basis and use the flag on the root designSetup component to decide which version to render

--- client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/index.tsx
@@ -1,4 +1,6 @@
+import { isEnabled } from '@automattic/calypso-config';
 import AnchorFmDesignPicker from './anchor-fm-design-picker';
+import NewSiteSetupDesignPicker from './new-site-setup-design-picker';
 import SiteSetupDesignPicker from './site-setup-design-picker';
 import type { Step } from '../../types';
 
@@ -6,7 +8,9 @@ import type { Step } from '../../types';
  * The design picker step
  */
 const DesignSetup: Step = ( props ) => {
-	if ( 'anchor-fm' === props.flow ) {
+	if ( isEnabled( 'signup/theme-preview-screen' ) ) {
+		return <NewSiteSetupDesignPicker { ...props } />;
+	} else if ( 'anchor-fm' === props.flow ) {
 		return <AnchorFmDesignPicker { ...props } />;
 	}
 
--- client/landing/stepper/declarative-flow/site-setup-flow.ts
+++ client/landing/stepper/declarative-flow/site-setup-flow.ts
@@ -62,7 +62,6 @@ export const siteSetupFlow: Flow = {
 			'wooConfirm',
 			'editEmail',
 			...( isEnabled( 'signup/woo-verify-email' ) ? [ 'editEmail' ] : [] ),
-			...( isEnabled( 'signup/theme-preview-screen' ) ? [ 'themeDesignPicker' ] : [] ),
 		] as StepPath[];
 	},
 	useSideEffect() {
