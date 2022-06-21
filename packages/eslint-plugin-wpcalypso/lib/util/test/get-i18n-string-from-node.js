25597cd5a88621bb80a79aed6adccd67357b263e
Edwin Takahashi <egao@outlook.com>
2022-06-21 09:32:13 -0700

    Fix additional errors: (#64755)
    - package names
    - use Jest expect instead of assert

--- packages/eslint-plugin-wpcalypso/lib/util/test/get-i18n-string-from-node.js
+++ packages/eslint-plugin-wpcalypso/lib/util/test/get-i18n-string-from-node.js
@@ -5,7 +5,6 @@
  * See LICENSE.md file in root directory for full license.
  */
 
-const assert = require( 'assert' );
 const parser = require( '@babel/eslint-parser' );
 const getTextContentFromNode = require( '../../../lib/util/get-text-content-from-node.js' );
 
@@ -25,58 +24,56 @@ function parseExpressionStatement( code ) {
 
 describe( '#getStringFromNode', function () {
 	it( 'should return simple strings', function () {
-		assert.equal(
-			'a simple string',
-			getTextContentFromNode( parseExpressionStatement( "'a simple string'" ) )
-		);
+		const result = getTextContentFromNode( parseExpressionStatement( "'a simple string'" ) );
+		expect( result ).toEqual( 'a simple string' );
 	} );
 
 	it( 'should return concatentated strings', function () {
-		assert.equal(
-			'A string in two parts',
-			getTextContentFromNode( parseExpressionStatement( '"A string" + " in two parts"' ) )
+		const result = getTextContentFromNode(
+			parseExpressionStatement( '"A string" + " in two parts"' )
 		);
+		expect( result ).toEqual( 'A string in two parts' );
 	} );
 
 	it( 'should return more concatentated strings', function () {
-		assert.equal(
-			'A string in three parts',
-			getTextContentFromNode( parseExpressionStatement( '"A string" + " in " + "three parts"' ) )
+		const result = getTextContentFromNode(
+			parseExpressionStatement( '"A string" + " in " + "three parts"' )
 		);
+		expect( result ).toEqual( 'A string in three parts' );
 	} );
 
 	it( 'should return strings from template literals', function () {
-		assert.equal(
-			'A template literal string',
-			getTextContentFromNode( parseExpressionStatement( '`A template literal string`' ) )
+		const result = getTextContentFromNode(
+			parseExpressionStatement( '`A template literal string`' )
 		);
+		expect( result ).toEqual( 'A template literal string' );
 	} );
 
 	it( 'should handle different literal types', function () {
-		assert.equal(
-			'A template and a string',
-			getTextContentFromNode( parseExpressionStatement( '`A template` + " and a string"' ) )
+		const result = getTextContentFromNode(
+			parseExpressionStatement( '`A template` + " and a string"' )
 		);
+		expect( result ).toEqual( 'A template and a string' );
 	} );
 
 	it( 'should return false for functions', function () {
 		const functionNode = parseExpressionStatement( 'foo()' );
 
-		assert.strictEqual( false, getTextContentFromNode( functionNode ) );
+		expect( getTextContentFromNode( functionNode ) ).toStrictEqual( false );
 	} );
 
 	it( 'should return false for variable assignments', function () {
 		const variableDeclarationNode = parseCode( "var aVariable = 'a string to assign';" );
 		const variableDeclarator = variableDeclarationNode.declarations[ 0 ];
 
-		assert.strictEqual( false, getTextContentFromNode( variableDeclarationNode ) );
-		assert.strictEqual( false, getTextContentFromNode( variableDeclarator ) );
+		expect( getTextContentFromNode( variableDeclarationNode ) ).toStrictEqual( false );
+		expect( getTextContentFromNode( variableDeclarator ) ).toStrictEqual( false );
 	} );
 
 	it( 'should return false for a binary structure including invalid node types', function () {
-		assert.strictEqual(
-			false,
-			getTextContentFromNode( parseExpressionStatement( "'a string plus a function' + foo()" ) )
+		const result = getTextContentFromNode(
+			parseExpressionStatement( "'a string plus a function' + foo()" )
 		);
+		expect( result ).toStrictEqual( false );
 	} );
 } );
