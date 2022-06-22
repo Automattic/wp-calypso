47d06ee22cf95c92f1df9f2d119befeb0e3427bc
Aneesh Devasthale <aneeshdevasthale@gmail.com>
2022-06-22 16:43:49 +0530

    Styling changes

--- client/my-sites/marketing/do-it-for-me/difm-landing.tsx
+++ client/my-sites/marketing/do-it-for-me/difm-landing.tsx
@@ -9,10 +9,9 @@ import { Gridicon } from '@automattic/components';
 import styled from '@emotion/styled';
 import { Card, Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import QueryProductsList from 'calypso/components/data/query-products-list';
-import FoldableFAQ from 'calypso/components/foldable-faq';
+import FoldableFAQComponent from 'calypso/components/foldable-faq';
 import FormattedHeader from 'calypso/components/formatted-header';
 import {
 	getProductDisplayCost,
@@ -49,6 +48,12 @@ const FAQSection = styled.div`
 	margin: 3rem 0;
 	.foldable-faq__question-text {
 		font-size: 1.125rem;
+	}
+`;
+
+const FoldableFAQ = styled( FoldableFAQComponent )`
+	&.is-expanded .foldable-faq__answer {
+		padding-bottom: 0;
 	}
 `;
 
@@ -90,9 +95,17 @@ const HeaderCard = styled( Card )`
 	}
 `;
 
-const HireButtonWrapper = styled.div`
+const CTASectionWrapper = styled.div`
 	text-align: center;
 	margin: 2rem 0;
+`;
+
+const CTAButton = styled( Button )`
+	border: 1px solid var( --color-accent );
+	font-size: 14px;
+	height: unset;
+	line-height: 22px;
+	padding: 8px 14px;
 `;
 
 const LinkButton = styled( Button )`
@@ -104,28 +117,20 @@ export default function DIFMLanding( { onSubmit }: { onSubmit: () => void } ) {
 	const displayCost = useSelector( ( state ) => getProductDisplayCost( state, WPCOM_DIFM_LITE ) );
 	const isLoading = useSelector( isProductsListFetching );
 
-	const [ expandedFAQId, setExpandedFAQId ] = useState( 'faq-1' );
-
-	const onFAQToggle = ( faqArgs: { id: string; isExpanded: boolean; height: number } ) => {
-		if ( faqArgs.isExpanded ) {
-			setExpandedFAQId( faqArgs.id );
-		}
-	};
-
 	const planTitle = isEnabled( 'plans/pro-plan' )
 		? getPlan( PLAN_WPCOM_PRO )?.getTitle()
 		: getPlan( PLAN_PREMIUM )?.getTitle();
 
-	const HireButton = () => (
-		<HireButtonWrapper>
-			<Button onClick={ onSubmit } isPrimary={ true }>
+	const CTASection = () => (
+		<CTASectionWrapper>
+			<CTAButton onClick={ onSubmit } isPrimary={ true }>
 				{ translate( 'Hire a Professional' ) }
-			</Button>
-		</HireButtonWrapper>
+			</CTAButton>
+		</CTASectionWrapper>
 	);
 	return (
 		<div>
-			{ ! displayCost && <QueryProductsList /> }
+			{ ! displayCost && <QueryProductsList persist /> }
 			<FormattedHeader
 				align={ 'center' }
 				headerText={ isLoading ? <Placeholder /> : displayCost }
@@ -150,7 +155,7 @@ export default function DIFMLanding( { onSubmit }: { onSubmit: () => void } ) {
 						}
 					) }
 				</p>
-				<HireButton />
+				<CTASection />
 				{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace */ }
 				<StyledHeading className="wp-brand-font" fontSize={ '2.25rem' }>
 					{ translate( 'It only takes 4 simple steps.' ) }
@@ -195,120 +200,100 @@ export default function DIFMLanding( { onSubmit }: { onSubmit: () => void } ) {
 			<FAQSection>
 				<FoldableFAQ
 					id="faq-1"
-					expanded={ expandedFAQId === 'faq-1' }
-					key={ `faq-1_${ expandedFAQId }` }
-					onToggle={ onFAQToggle }
+					expanded={ true }
 					question={ translate( 'What is Do It For Me: Website Design Service?' ) }
 				>
-					{ translate(
-						'This service was created for customers who would like to hire a professional to set up their website. Our professional builders have expert knowledge of the WordPress editor, themes, and available blocks; they take advantage of all the best options for your new site build. Once you’ve provided us with your content, we’ll create custom layouts for each page of your website and add your content to each of them. You will receive an email with a link to your finished website within 4 business days. You can then edit all of the content of the site using the WordPress editor. Add as many new pages or posts as you need, and contact WordPress.com support with any questions regarding how to edit or further customize your new site!'
-					) }
+					<p>
+						{ translate(
+							'This service was created for customers who would like to hire a professional to set up their website. Our professional builders have expert knowledge of the WordPress editor, themes, and available blocks; they take advantage of all the best options for your new site build. Once you’ve provided us with your content, we’ll create custom layouts for each page of your website and add your content to each of them. You will receive an email with a link to your finished website within 4 business days. You can then edit all of the content of the site using the WordPress editor. Add as many new pages or posts as you need, and contact WordPress.com support with any questions regarding how to edit or further customize your new site!'
+						) }
+					</p>
 				</FoldableFAQ>
-				<FoldableFAQ
-					id="faq-2"
-					expanded={ expandedFAQId === 'faq-2' }
-					key={ `faq-2_${ expandedFAQId }` }
-					onToggle={ onFAQToggle }
-					question={ translate( 'How do I get started?' ) }
-				>
-					<li>
-						{ translate( 'Click {{a}}Hire a Professional{{/a}} to begin.', {
-							components: {
-								a: <LinkButton isLink={ true } onClick={ onSubmit } />,
-							},
-						} ) }
-					</li>
-					<li>
-						{ translate(
-							'Choose {{b}}New site{{/b}} to begin a new site or {{b}}Existing WordPress.com{{/b}} site if you’d like to use an existing site on your account. (Note that all existing website content will be deleted from the site so we can start fresh).',
-							{
+				<FoldableFAQ id="faq-2" question={ translate( 'How do I get started?' ) }>
+					<ul>
+						<li>
+							{ translate( 'Click {{a}}Hire a Professional{{/a}} to begin.', {
 								components: {
-									b: <b />,
+									a: <LinkButton isLink={ true } onClick={ onSubmit } />,
 								},
-							}
-						) }
-					</li>
-					<li>
-						{ translate(
-							'Submit your business information and optionally add your social media profiles.'
-						) }
-					</li>
-					<li>
-						{ translate(
-							'Select your design from our catalog of professionally designed themes, or select {{b}}Let us choose{{/b}} to let our professionals select the best design for your site (recommended).',
-							{
-								components: {
-									b: <b />,
-								},
-							}
-						) }
-					</li>
-					<li>
-						{ translate(
-							'Select up to five page types to use for your new site (About, Services, Contact, etc).'
-						) }
-					</li>
-					<li>{ translate( 'Complete the purchase at Checkout.' ) }</li>
-					<li>
-						{ translate(
-							'Submit the content for your new website, adding text and images (optional) for each page. You can always edit this content on the site later using the WordPress editor.'
-						) }
-					</li>
-					<li>{ translate( 'Receive your finished website in 4 business days or less!' ) }</li>
-					<HireButton />
+							} ) }
+						</li>
+						<li>
+							{ translate(
+								'Choose {{b}}New site{{/b}} to begin a new site or {{b}}Existing WordPress.com{{/b}} site if you’d like to use an existing site on your account. (Note that all existing website content will be deleted from the site so we can start fresh).',
+								{
+									components: {
+										b: <b />,
+									},
+								}
+							) }
+						</li>
+						<li>
+							{ translate(
+								'Submit your business information and optionally add your social media profiles.'
+							) }
+						</li>
+						<li>
+							{ translate(
+								'Select your design from our catalog of professionally designed themes, or select {{b}}Let us choose{{/b}} to let our professionals select the best design for your site (recommended).',
+								{
+									components: {
+										b: <b />,
+									},
+								}
+							) }
+						</li>
+						<li>
+							{ translate(
+								'Select up to five page types to use for your new site (About, Services, Contact, etc).'
+							) }
+						</li>
+						<li>{ translate( 'Complete the purchase at Checkout.' ) }</li>
+						<li>
+							{ translate(
+								'Submit the content for your new website, adding text and images (optional) for each page. You can always edit this content on the site later using the WordPress editor.'
+							) }
+						</li>
+						<li>{ translate( 'Receive your finished website in 4 business days or less!' ) }</li>
+					</ul>
+					<CTASection />
 				</FoldableFAQ>
-				<FoldableFAQ
-					id="faq-3"
-					expanded={ expandedFAQId === 'faq-3' }
-					key={ `faq-3_${ expandedFAQId }` }
-					onToggle={ onFAQToggle }
-					question={ translate( 'Who is this service for?' ) }
-				>
+				<FoldableFAQ id="faq-3" question={ translate( 'Who is this service for?' ) }>
 					<ul>
 						<li>{ translate( 'Small business owners looking to get online quickly.' ) }</li>
 						<li>{ translate( 'Bloggers wanting some help with their initial site setup.' ) }</li>
 						<li>{ translate( 'Anyone who could benefit from professional page layouts.' ) }</li>
 					</ul>
 				</FoldableFAQ>
-				<FoldableFAQ
-					id="faq-4"
-					expanded={ expandedFAQId === 'faq-4' }
-					key={ `faq-4_${ expandedFAQId }` }
-					onToggle={ onFAQToggle }
-					question={ translate( 'How much does the service cost?' ) }
-				>
-					{ translate(
-						'The service costs %(displayCost)s, plus one year of the %(planTitle)s hosting plan. If you choose to use an existing site that already has the plan, you will only be charged for the website design service.',
-						{
-							args: {
-								displayCost,
-								planTitle,
-							},
-						}
-					) }
+				<FoldableFAQ id="faq-4" question={ translate( 'How much does the service cost?' ) }>
+					<p>
+						{ translate(
+							'The service costs %(displayCost)s, plus one year of the %(planTitle)s hosting plan. If you choose to use an existing site that already has the plan, you will only be charged for the website design service.',
+							{
+								args: {
+									displayCost,
+									planTitle,
+								},
+							}
+						) }
+					</p>
 				</FoldableFAQ>
-				<FoldableFAQ
-					id="faq-5"
-					expanded={ expandedFAQId === 'faq-5' }
-					key={ `faq-5_${ expandedFAQId }` }
-					onToggle={ onFAQToggle }
-					question={ translate( 'What does the service include?' ) }
-				>
+				<FoldableFAQ id="faq-5" question={ translate( 'What does the service include?' ) }>
 					<ul>
 						<li>
 							{ translate(
-								'Custom page layouts for up to 5 pages, using the content you provide during sign up'
+								'Custom page layouts for up to 5 pages, using the content you provide during sign up.'
 							) }
 						</li>
 						<li>
 							{ translate(
-								'Theme selection (when the “Let us choose” option is selected during sign up)'
+								'Theme selection (when the “Let us choose” option is selected during sign up).'
 							) }
 						</li>
 						<li>{ translate( 'Color palette creation, based on your branding.' ) }</li>
 						<li>
 							{ translate(
-								'Your logo (provided by you) added to the site, and creation of a site icon'
+								'Your logo (provided by you) added to the site, and creation of a site icon.'
 							) }
 						</li>
 						<li>{ translate( 'Creation of a main navigation menu.' ) }</li>
@@ -318,35 +303,30 @@ export default function DIFMLanding( { onSubmit }: { onSubmit: () => void } ) {
 						<li>{ translate( 'Sourcing of additional professional images (when needed).' ) }</li>
 					</ul>
 				</FoldableFAQ>
-				<FoldableFAQ
-					id="faq-6"
-					expanded={ expandedFAQId === 'faq-6' }
-					key={ `faq-6_${ expandedFAQId }` }
-					onToggle={ onFAQToggle }
-					question={ translate( 'What will my finished site look like?' ) }
-				>
-					{ translate(
-						'Your finished site will be built using a WordPress.com theme. The layout of your site will be a mobile-friendly responsive design and the content will adjust to look great on every device. The professional website builder will create the layout of each page based on the content you provide during the signup process. Additional high-quality professional images may be sourced from Pexels, a vast open source library of stock photos, to make sure each page stands out. Custom CSS may be added for further design tweaks.'
-					) }
+				<FoldableFAQ id="faq-6" question={ translate( 'What will my finished site look like?' ) }>
+					<p>
+						{ translate(
+							'Your finished site will be built using a WordPress.com theme. The layout of your site will be a mobile-friendly responsive design and the content will adjust to look great on every device. The professional website builder will create the layout of each page based on the content you provide during the signup process. Additional high-quality professional images may be sourced from Pexels, a vast open source library of stock photos, to make sure each page stands out. Custom CSS may be added for further design tweaks.'
+						) }
+					</p>
 				</FoldableFAQ>
 				<FoldableFAQ
 					id="faq-7"
-					expanded={ expandedFAQId === 'faq-7' }
-					key={ `faq-7_${ expandedFAQId }` }
-					onToggle={ onFAQToggle }
 					question={ translate( 'What if I want changes to the finished site?' ) }
 				>
-					{ translate(
-						'Although revisions aren’t included with this service, you will be able to edit all content of the site using the WordPress editor. You will be able to change images, edit text, and also add additional pages and posts. You could even try a new theme for a different look, and you will still have the professionally designed page layouts. Your %(planTitle)s plan comes with access to live chat and priority email support, so you can always contact support if you need help customizing your new site or have questions about this service.',
-						{
-							args: {
-								planTitle,
-							},
-						}
-					) }
+					<p>
+						{ translate(
+							'Although revisions aren’t included with this service, you will be able to edit all content of the site using the WordPress editor. You will be able to change images, edit text, and also add additional pages and posts. You could even try a new theme for a different look, and you will still have the professionally designed page layouts. Your %(planTitle)s plan comes with access to live chat and priority email support, so you can always contact support if you need help customizing your new site or have questions about this service.',
+							{
+								args: {
+									planTitle,
+								},
+							}
+						) }
+					</p>
 				</FoldableFAQ>
 			</FAQSection>
-			<HireButton />
+			<CTASection />
 		</div>
 	);
 }
