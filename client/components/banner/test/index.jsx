68e3b81a000b9357cf1bfc765a45cebb02be47e3
brookewp <35543432+brookewp@users.noreply.github.com>
2022-06-23 11:45:04 -0700

    Components: Migrate `Banner` tests to `@testing-library/react` (#64048)
    * Migrated tests from enyzme to testing-library

--- client/components/banner/test/index.jsx
+++ client/components/banner/test/index.jsx
@@ -1,19 +1,37 @@
-jest.mock( 'calypso/blocks/dismissible-card', () => {
-	return function DismissibleCard() {
-		return null;
-	};
-} );
+/**
+ * @jest-environment jsdom
+ */
 
-jest.mock( 'calypso/lib/analytics/track-component-view', () => {
-	return function TrackComponentView() {
-		return null;
-	};
-} );
-
-import { Card, Button } from '@automattic/components';
-import { shallow } from 'enzyme';
-import PlanPrice from 'calypso/my-sites/plan-price/';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import preferencesReducer from 'calypso/state/preferences/reducer';
+import { renderWithProvider } from 'calypso/test-helpers/testing-library';
 import { Banner } from '../index';
+
+jest.mock( 'calypso/my-sites/plan-price', () => ( { rawPrice, original } ) => (
+	<div
+		data-testid="banner-price-plan-mock"
+		className={ original ? 'is-original' : 'is-discounted' }
+	>
+		{ rawPrice }
+	</div>
+) );
+
+jest.mock( 'calypso/state/analytics/actions', () => ( {
+	recordTracksEvent: jest.fn( () => ( {
+		type: 'ANALYTICS_EVENT_RECORD',
+	} ) ),
+} ) );
+
+function renderWithRedux( ui ) {
+	return renderWithProvider( ui, {
+		reducers: {
+			preferences: preferencesReducer,
+		},
+		initialState: { preferences: { remoteValues: {} } },
+	} );
+}
 
 const props = {
 	callToAction: null,
@@ -21,145 +39,153 @@ const props = {
 };
 
 describe( 'Banner basic tests', () => {
-	test( 'should not blow up and have proper CSS class', () => {
-		const comp = shallow( <Banner { ...props } /> );
-		expect( comp.find( '.banner' ) ).toHaveLength( 1 );
+	afterEach( () => {
+		jest.clearAllMocks();
 	} );
 
 	test( 'should render Card if dismissPreferenceName is null', () => {
-		const comp = shallow( <Banner { ...props } dismissPreferenceName={ null } /> );
-		expect( comp.find( Card ) ).toHaveLength( 1 );
-		expect( comp.find( 'DismissibleCard' ) ).toHaveLength( 0 );
+		const { container } = render( <Banner { ...props } dismissPreferenceName={ null } /> );
+		expect( container.firstChild ).toHaveClass( 'card' );
+		expect( container.firstChild ).not.toHaveClass( 'is-dismissible' );
 	} );
 
 	test( 'should render DismissibleCard if dismissPreferenceName is defined', () => {
-		const comp = shallow( <Banner { ...props } dismissPreferenceName={ 'banner-test' } /> );
-		expect( comp.find( Card ) ).toHaveLength( 0 );
-		expect( comp.find( 'DismissibleCard' ) ).toHaveLength( 1 );
-	} );
-
-	test( 'should have .has-call-to-action class if callToAction is defined', () => {
-		const comp = shallow( <Banner { ...props } callToAction={ 'Upgrade Now!' } /> );
-		expect( comp.find( '.has-call-to-action' ) ).toHaveLength( 1 );
-	} );
-
-	test( 'should not have .has-call-to-action class if callToAction is null', () => {
-		const comp = shallow( <Banner { ...props } callToAction={ null } /> );
-		expect( comp.find( '.has-call-to-action' ) ).toHaveLength( 0 );
+		renderWithRedux( <Banner { ...props } dismissPreferenceName="banner-test" /> );
+		expect( screen.getByLabelText( 'Dismiss' ) ).toBeVisible();
 	} );
 
 	test( 'should render a <Button /> when callToAction is specified', () => {
-		const comp = shallow( <Banner { ...props } callToAction={ 'Buy something!' } /> );
-		expect( comp.find( Button ) ).toHaveLength( 1 );
+		render( <Banner { ...props } callToAction="Buy something!" /> );
+		expect( screen.getByRole( 'button' ) ).toHaveTextContent( 'Buy something!' );
 	} );
 
 	test( 'should not render a <Button /> when callToAction is not specified', () => {
-		const comp = shallow( <Banner { ...props } /> );
-		expect( comp.find( Button ) ).toHaveLength( 0 );
+		render( <Banner { ...props } /> );
+		expect( screen.queryByRole( 'button' ) ).not.toBeInTheDocument();
 	} );
 
 	test( 'should have .is-jetpack class and JetpackLogo if jetpack prop is defined', () => {
 		const { plan, ...propsWithoutPlan } = props;
-		const comp = shallow( <Banner { ...propsWithoutPlan } jetpack /> );
-		expect( comp.find( '.is-jetpack' ) ).toHaveLength( 1 );
-		expect( comp.find( 'JetpackLogo' ) ).toHaveLength( 1 );
+		const { container } = render( <Banner { ...propsWithoutPlan } jetpack /> );
+		expect( container.firstChild ).toHaveClass( 'is-jetpack' );
+		expect( container.getElementsByClassName( 'jetpack-logo' ) ).toHaveLength( 1 );
 	} );
 
 	test( 'should render have .is-horizontal class if horizontal prop is defined', () => {
-		const comp = shallow( <Banner { ...props } horizontal /> );
-		expect( comp.find( '.is-horizontal' ) ).toHaveLength( 1 );
+		const { container } = render( <Banner { ...props } horizontal /> );
+		expect( container.firstChild ).toHaveClass( 'is-horizontal' );
 	} );
 
 	test( 'should render a <PlanPrice /> when price is specified', () => {
-		const comp = shallow( <Banner { ...props } price={ 100 } /> );
-		expect( comp.find( PlanPrice ) ).toHaveLength( 1 );
+		render( <Banner { ...props } price={ 100 } /> );
+		expect( screen.queryByTestId( 'banner-price-plan-mock' ) ).toHaveTextContent( '100' );
 	} );
 
 	test( 'should render two <PlanPrice /> components when there are two prices', () => {
-		const comp = shallow( <Banner { ...props } price={ [ 100, 80 ] } /> );
-		expect( comp.find( PlanPrice ) ).toHaveLength( 2 );
+		render( <Banner { ...props } price={ [ 100, 80 ] } /> );
+		const planPriceMock = screen.queryAllByTestId( 'banner-price-plan-mock' );
+		expect( planPriceMock ).toHaveLength( 2 );
+		expect( planPriceMock[ 0 ] ).toHaveTextContent( 100 );
+		expect( planPriceMock[ 0 ] ).toHaveAttribute( 'class', 'is-original' );
+		expect( planPriceMock[ 1 ] ).toHaveTextContent( 80 );
+		expect( planPriceMock[ 1 ] ).toHaveAttribute( 'class', 'is-discounted' );
 	} );
 
 	test( 'should render no <PlanPrice /> components when there are no prices', () => {
-		const comp = shallow( <Banner { ...props } /> );
-		expect( comp.find( PlanPrice ) ).toHaveLength( 0 );
+		render( <Banner { ...props } /> );
+		expect( screen.queryByTestId( 'banner-price-plan-mock' ) ).not.toBeInTheDocument();
 	} );
 
 	test( 'should render a .banner__description when description is specified', () => {
-		const comp = shallow( <Banner { ...props } description="test" /> );
-		expect( comp.find( '.banner__description' ) ).toHaveLength( 1 );
+		render( <Banner { ...props } description="test" /> );
+		expect( screen.getByText( 'test' ) ).toHaveClass( 'banner__description' );
 	} );
 
 	test( 'should not render a .banner__description when description is not specified', () => {
-		const comp = shallow( <Banner { ...props } /> );
-		expect( comp.find( '.banner__description' ) ).toHaveLength( 0 );
+		const { container } = render( <Banner { ...props } /> );
+		expect( container.getElementsByClassName( 'banner__description' ) ).toHaveLength( 0 );
 	} );
 
 	test( 'should render a .banner__list when list is specified', () => {
-		const comp = shallow( <Banner { ...props } list={ [ 'test1', 'test2' ] } /> );
-		expect( comp.find( '.banner__list' ) ).toHaveLength( 1 );
-		expect( comp.find( '.banner__list li' ) ).toHaveLength( 2 );
-		expect( comp.find( '.banner__list li' ).at( 0 ).text() ).toContain( 'test1' );
-		expect( comp.find( '.banner__list li' ).at( 1 ).text() ).toContain( 'test2' );
+		render( <Banner { ...props } list={ [ 'test1', 'test2' ] } /> );
+		expect( screen.queryByRole( 'list' ) ).toBeVisible();
+		expect( screen.queryAllByRole( 'listitem' ) ).toHaveLength( 2 );
+		expect( screen.queryAllByRole( 'listitem' )[ 0 ] ).toHaveTextContent( 'test1' );
+		expect( screen.queryAllByRole( 'listitem' )[ 1 ] ).toHaveTextContent( 'test2' );
 	} );
 
 	test( 'should not render a .banner__list when description is not specified', () => {
-		const comp = shallow( <Banner { ...props } /> );
-		expect( comp.find( '.banner__list' ) ).toHaveLength( 0 );
+		render( <Banner { ...props } /> );
+		expect( screen.queryByRole( 'list' ) ).not.toBeInTheDocument();
 	} );
 
 	test( 'should record Tracks event when event is specified', () => {
-		const comp = shallow( <Banner { ...props } event="test" /> );
-		expect( comp.find( 'TrackComponentView' ) ).toHaveLength( 1 );
+		renderWithProvider( <Banner { ...props } event="test" /> );
+		expect( recordTracksEvent ).toHaveBeenCalledWith(
+			'calypso_banner_cta_impression',
+			expect.objectContaining( {
+				cta_name: 'test',
+			} )
+		);
 	} );
 
 	test( 'should not record Tracks event when event is not specified', () => {
-		const comp = shallow( <Banner { ...props } /> );
-		expect( comp.find( 'TrackComponentView' ) ).toHaveLength( 0 );
+		renderWithProvider( <Banner { ...props } /> );
+		expect( recordTracksEvent ).not.toHaveBeenCalled();
 	} );
 
 	test( 'should render Card with href if href prop is passed', () => {
-		const comp = shallow( <Banner { ...props } href={ '/' } /> );
-		expect( comp.find( Card ) ).toHaveLength( 1 );
-		expect( comp.find( Card ).props().href ).toBe( '/' );
+		const { container } = render( <Banner { ...props } href="/" /> );
+		expect( container.firstChild ).toHaveClass( 'card' );
+		expect( container.firstChild ).toHaveAttribute( 'href', '/' );
 	} );
 
 	test( 'should render Card with no href if href prop is passed but disableHref is true', () => {
-		const comp = shallow( <Banner { ...props } href={ '/' } disableHref={ true } /> );
-		expect( comp.find( Card ) ).toHaveLength( 1 );
-		expect( comp.find( Card ).props().href ).toBeNull();
+		const { container } = render( <Banner { ...props } href="/" disableHref={ true } /> );
+		expect( container.firstChild ).toHaveClass( 'card' );
+		expect( container.firstChild ).not.toHaveAttribute( 'href' );
 	} );
 
 	test( 'should render Card with href if href prop is passed but disableHref is true and forceHref is true', () => {
-		const comp = shallow(
-			<Banner { ...props } href={ '/' } disableHref={ true } forceHref={ true } />
-		);
-		expect( comp.find( Card ) ).toHaveLength( 1 );
-		expect( comp.find( Card ).props().href ).toBe( '/' );
+		const { container } = render( <Banner { ...props } href="/" disableHref={ true } forceHref /> );
+		expect( container.firstChild ).toHaveClass( 'card' );
+		expect( container.firstChild ).toHaveAttribute( 'href', '/' );
 	} );
 
-	test( 'should render Card with no href and CTA button with href if href prop is passed and callToAction is also passed', () => {
-		const comp = shallow( <Banner { ...props } href={ '/' } callToAction="Go WordPress!" /> );
-		expect( comp.find( Card ) ).toHaveLength( 1 );
-		expect( comp.find( Card ).props().href ).toBeNull();
-		expect( comp.find( Card ).props().onClick ).toBeNull();
+	test( 'should render Card with no href and CTA button with href if href prop is passed and callToAction is also passed', async () => {
+		const handleClick = jest.fn();
+		const { container } = render(
+			<Banner { ...props } href="/" callToAction="Go WordPress!" onClick={ handleClick } />
+		);
+		const btnText = screen.getByText( 'Go WordPress!' );
+		await userEvent.click( btnText );
 
-		expect( comp.find( Button ) ).toHaveLength( 1 );
-		expect( comp.find( Button ).props().href ).toBe( '/' );
-		expect( comp.find( Button ).props().children ).toBe( 'Go\xA0WordPress!' ); //preventwidows adds \xA0 non-breaking space;
-		expect( comp.find( Button ).props().onClick ).toBe( comp.instance().handleClick );
+		expect( handleClick ).toHaveBeenCalledTimes( 1 );
+		expect( container.firstChild ).toHaveClass( 'card' );
+		expect( container.firstChild ).not.toHaveAttribute( 'href' );
+		expect( btnText ).toBeVisible();
+		expect( btnText ).toHaveAttribute( 'href', '/' );
 	} );
 
-	test( 'should render Card with href and CTA button with no href if href prop is passed and callToAction is also passed and forceHref is true', () => {
-		const comp = shallow(
-			<Banner { ...props } href={ '/' } callToAction="Go WordPress!" forceHref={ true } />
+	test( 'should render Card with href and CTA button with no href if href prop is passed and callToAction is also passed and forceHref is true', async () => {
+		const handleClick = jest.fn();
+		const { container } = render(
+			<Banner
+				{ ...props }
+				href="/"
+				callToAction="Go WordPress!"
+				forceHref
+				onClick={ handleClick }
+			/>
 		);
-		expect( comp.find( Card ) ).toHaveLength( 1 );
-		expect( comp.find( Card ).props().href ).toBe( '/' );
-		expect( comp.find( Card ).props().onClick ).toBe( comp.instance().handleClick );
+		const btn = screen.getByRole( 'button' );
+		await userEvent.click( container.firstChild );
 
-		expect( comp.find( Button ) ).toHaveLength( 1 );
-		expect( comp.find( Button ).props().href ).toBeUndefined();
-		expect( comp.find( Button ).props().children ).toBe( 'Go\xA0WordPress!' ); //preventWidows adds \xA0 non-breaking space;
+		expect( handleClick ).toHaveBeenCalledTimes( 1 );
+		expect( container.firstChild ).toHaveClass( 'card' );
+		expect( container.firstChild ).toHaveAttribute( 'href', '/' );
+		expect( btn ).toBeVisible();
+		expect( btn ).not.toHaveAttribute( 'href' );
+		expect( btn ).toHaveTextContent( 'Go WordPress!' );
 	} );
 } );
