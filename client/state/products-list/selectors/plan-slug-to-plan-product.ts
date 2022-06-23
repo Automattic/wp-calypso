9c87b6714a5614866aa099a97da353bf22f703d1
Payton Swick <payton@foolord.com>
2022-06-23 14:52:43 -0400

    Convert planSlugToPlanProduct to TypeScript

---
+++ client/state/products-list/selectors/plan-slug-to-plan-product.ts
@@ -0,0 +1,43 @@
+import {
+	applyTestFiltersToPlansList,
+	applyTestFiltersToProductsList,
+	getPlan,
+	getProductFromSlug,
+	objectIsProduct,
+} from '@automattic/calypso-products';
+import type { ProductListItem } from './get-products-list';
+
+export type TestFilteredPlan =
+	| ReturnType< typeof applyTestFiltersToPlansList >
+	| ReturnType< typeof applyTestFiltersToProductsList >;
+export interface PlanAndProduct {
+	planSlug: string;
+	plan: TestFilteredPlan | null;
+	product: ProductListItem | undefined;
+}
+
+/**
+ * Computes a plan object and a related product object based on plan slug/constant
+ */
+export const planSlugToPlanProduct = (
+	products: Record< string, ProductListItem >,
+	planOrProductSlug: string
+): PlanAndProduct => {
+	const plan = getPlan( planOrProductSlug ) ?? getProductFromSlug( planOrProductSlug );
+	// NOTE: while converting this to TypeScript, this next section showed up as
+	// heavily broken. I had to heavily change these next lines because they
+	// assume that `plan` is a string and it is very likely to be an object. I do
+	// not know if I chose the correct strings or if something more fundamental
+	// is wrong here but it's a little shocking that this ever worked at all.
+	const constantObj = objectIsProduct( plan )
+		? applyTestFiltersToProductsList( plan.product_slug )
+		: applyTestFiltersToPlansList(
+				typeof plan === 'string' ? plan : plan.getStoreSlug(),
+				undefined
+		  );
+	return {
+		planSlug: planOrProductSlug,
+		plan: plan === planOrProductSlug ? null : constantObj,
+		product: products[ planOrProductSlug ],
+	};
+};
