490d5284bd970bb41d7dd1aab0ddda2eb5a67aa7
Wesley Berrêdo <wberredo@gmail.com>
2022-06-23 13:03:58 -0400

    Remove the stack trace part from the the RequestError class.

--- client/lib/wporg/request-error.js
+++ client/lib/wporg/request-error.js
@@ -3,10 +3,5 @@ export class RequestError extends Error {
 		super( message );
 		this.name = 'RequestError';
 		this.response = response;
-
-		// Maintains proper stack trace for where our error was thrown (only available on V8)
-		if ( Error.captureStackTrace ) {
-			Error.captureStackTrace( this, RequestError );
-		}
 	}
 }
