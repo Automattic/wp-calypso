000f6f5d56041181ccfa52ddd959a08df3b15806
Wojtek Naruniec <wojtek@naruniec.me>
2022-06-23 11:05:43 +0200

    Add upsell nudge for sites that have AMP deprecated.
    I'm adding an upsell nudge that displays two versions of the nudge:
    - for a user with a free account
    - for a user who has paid plan but has not enabled hosting features yet.

--- client/my-sites/site-settings/amp/wpcom.jsx
+++ client/my-sites/site-settings/amp/wpcom.jsx
@@ -1,3 +1,4 @@
+import { FEATURE_SFTP, PLAN_WPCOM_PRO } from '@automattic/calypso-products';
 import { CompactCard } from '@automattic/components';
 import { localizeUrl } from '@automattic/i18n-utils';
 import { ToggleControl } from '@wordpress/components';
@@ -6,10 +7,12 @@ import page from 'page';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import UpsellNudge from 'calypso/blocks/upsell-nudge';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
 import SettingsSectionHeader from 'calypso/my-sites/site-settings/settings-section-header';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import siteHasFeature from 'calypso/state/selectors/site-has-feature';
+import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
 import './wpcom.scss';
 
@@ -46,6 +49,35 @@ class AmpWpcom extends Component {
 		page( '/customize/amp/' + this.props.siteSlug );
 	};
 
+	renderUpgradeNotice() {
+		const { hasSftpFeature, siteSlug, translate } = this.props;
+
+		if ( hasSftpFeature ) {
+			return (
+				<UpsellNudge
+					title={ translate( 'Install AMP plugin.' ) }
+					description={ translate( 'Install AMP plugin to use AMP features.' ) }
+					plan={ PLAN_WPCOM_PRO }
+					href={ `/plugins/amp/${ siteSlug }` }
+					forceHref={ true }
+					showIcon={ true }
+					forceDisplay
+				/>
+			);
+		}
+
+		return (
+			<UpsellNudge
+				title={ translate( 'Available on the Pro plan' ) }
+				description={ translate( 'Upgrade to the Pro plan to install AMP plugin.' ) }
+				plan={ PLAN_WPCOM_PRO }
+				href={ `/checkout/${ siteSlug }/pro` }
+				showIcon={ true }
+				forceDisplay
+			/>
+		);
+	}
+
 	render() {
 		const {
 			fields: {
@@ -62,8 +94,17 @@ class AmpWpcom extends Component {
 		const isDisabled = isRequestingSettings || isSavingSettings;
 		const isCustomizeEnabled = ! isDisabled && ampIsEnabled;
 
-		if ( ! ampIsSupported || ampIsDeprecated ) {
+		if ( ! ampIsSupported ) {
 			return null;
+		}
+
+		if ( ampIsDeprecated ) {
+			return (
+				<div className="amp__main site-settings__traffic-settings">
+					<SettingsSectionHeader title={ translate( 'Accelerated Mobile Pages (AMP)' ) } />
+					{ this.renderUpgradeNotice() }
+				</div>
+			);
 		}
 
 		return (
@@ -109,8 +150,14 @@ class AmpWpcom extends Component {
 }
 
 export default connect(
-	( state ) => ( {
-		siteSlug: getSelectedSiteSlug( state ),
-	} ),
+	( state ) => {
+		const siteId = getSelectedSiteId( state );
+		const hasSftpFeature = siteHasFeature( state, siteId, FEATURE_SFTP );
+
+		return {
+			siteSlug: getSelectedSiteSlug( state ),
+			hasSftpFeature,
+		};
+	},
 	{ recordTracksEvent }
 )( localize( AmpWpcom ) );
