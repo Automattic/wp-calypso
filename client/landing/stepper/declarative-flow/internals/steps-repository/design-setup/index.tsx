da08a05ffe3daf31bb5880428c03a68a3446cf85
Nelson <nelsonec87@gmail.com>
2022-06-24 16:16:28 -0300

    Consolidate changes in the same component

--- client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/index.tsx
@@ -1,6 +1,4 @@
-import { isEnabled } from '@automattic/calypso-config';
 import AnchorFmDesignPicker from './anchor-fm-design-picker';
-import SellerDesignPicker from './seller-design-picker';
 import SiteSetupDesignPicker from './site-setup-design-picker';
 import type { Step } from '../../types';
 
@@ -8,9 +6,7 @@ import type { Step } from '../../types';
  * The design picker step
  */
 const DesignSetup: Step = ( props ) => {
-	if ( isEnabled( 'signup/theme-preview-screen' ) ) {
-		return <SellerDesignPicker { ...props } />;
-	} else if ( 'anchor-fm' === props.flow ) {
+	if ( 'anchor-fm' === props.flow ) {
 		return <AnchorFmDesignPicker { ...props } />;
 	}
 
