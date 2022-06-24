79afe0af068008aa407d6b982403d95a35b64cec
Enej Bajgoric <enej.bajgoric@automattic.com>
2022-06-24 10:17:23 -0700

    QR Code Login: Start Requesting the QR Code data and auth url. (#63257)
    * Move login/qr-code-login -> login/qr-code-login-page
    
    * Move QR-code component to a QR code block.
    
    * Minor fixes
    
    * Minor updates to the styles
    
    * Update client/blocks/qr-code-login/index.jsx
    
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>
    
    * Update client/blocks/qr-code-login/index.jsx
    
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>
    
    * Simplify code
    
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>
    
    * Include all the fixes.
    
    * Fix external links to open in new windows.
    
    * Make the QR code scale to any browser width
    
    * Simplefy args
    
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>
    
    * Add Error state
    
    * simplify code
    
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>
    
    * fix typo
    
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>
    
    * remove the return values from fetchQRCode
    
    * Simplify error state display
    
    * Update QR code that uses the suggestions
    
    * fix up redirect param
    
    * Back button supports redirectTo
    
    * Update to say Jetpack App
    
    * Wording change to new label
    
    * Remove link to support for now
    
    * Update the logo to use Jetpack
    
    * Update the apps.wordpress.com/ campaign url
    
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>

---
+++ client/login/qr-code-login-page/index.jsx
@@ -0,0 +1,38 @@
+import { Card, Gridicon } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import AsyncLoad from 'calypso/components/async-load';
+import Main from 'calypso/components/main';
+import { login } from 'calypso/lib/paths';
+
+import './style.scss';
+
+function QrCodeLoginPlaceholder() {
+	return <Card className="qr-code-login-page__placeholder" />;
+}
+
+function QrCodeLoginPage( { locale, redirectTo } ) {
+	const translate = useTranslate();
+
+	return (
+		<Main className="qr-code-login-page">
+			<div className="qr-code-login-page__form">
+				<h1 className="qr-code-login-page__heading">{ translate( 'Login via the mobile app' ) }</h1>
+				<AsyncLoad
+					require="calypso/blocks/qr-code-login"
+					placeholder={ <QrCodeLoginPlaceholder /> }
+					size={ 300 }
+					locale={ locale }
+					redirectToAfterLoginUrl={ redirectTo }
+				/>
+				<div className="qr-code-login-page__footer">
+					<a href={ login( { locale, redirectTo } ) }>
+						<Gridicon icon="arrow-left" size={ 18 } />
+						{ translate( 'Enter a password instead' ) }
+					</a>
+				</div>
+			</div>
+		</Main>
+	);
+}
+
+export default QrCodeLoginPage;
