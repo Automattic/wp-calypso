4154c9e20139f8812b9f39f7f69419a788d94512
Philip Jackson <p-jackson@live.com>
2022-06-29 11:38:00 +1200

    Dashboard: Add `/sites-dashboard` section available behind a feature flag (#64965)

---
+++ client/sites-dashboard/index.ts
@@ -0,0 +1,13 @@
+import { isEnabled } from '@automattic/calypso-config';
+import page from 'page';
+import { makeLayout, render as clientRender } from 'calypso/controller';
+import { sitesDashboard } from './controller';
+
+export default function () {
+	if ( ! isEnabled( 'build/sites-dashboard' ) ) {
+		page( '/sites-dashboard', '/sites' );
+		return;
+	}
+
+	page( '/sites-dashboard', sitesDashboard, makeLayout, clientRender );
+}
