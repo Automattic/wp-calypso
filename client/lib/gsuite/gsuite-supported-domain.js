Automattic/wp-calypso

commit e0de8057db936e1807294cadb7f24c8555493f00

Make all registered domains eligible for Google Workspace within 15 mins after purchase (#64822)
* Make all registered domains eligible for Google Workspace within 15 mins after purchase

* More compact type definition

* Type fix

* Clarified comment

client/lib/gsuite/gsuite-supported-domain.js
@@ -4,15 +4,33 @@ import { hasGSuiteWithAnotherProvider } from './has-gsuite-with-another-provider
 import { hasGSuiteWithUs } from './has-gsuite-with-us';
 
 /**
+ * @typedef { import('calypso/lib/domains/types').ResponseDomain } ResponseDomain domain object
+ */
+
+/**
  * Filters a list of domains by the domains that eligible for G Suite.
  *
- * @param {Array} domains - list of domain objects
- * @returns {Array} - the list of domains that are eligible for G Suite
+ * @param {ResponseDomain[]} domains - list of domain objects
+ * @returns {ResponseDomain[]} - the list of domains that are eligible for G Suite
  */
 export function getGSuiteSupportedDomains( domains ) {
 	return domains.filter( function ( domain ) {
 		if ( hasGSuiteWithAnotherProvider( domain ) ) {
 			return false;
+		}
+
+		// If the domain is registered through us, there is a provisioning period when
+		// `hasWpcomNameservers` will be false. We still want to let users buy Google Workspace
+		// during that period, even if we normally wouldn't let them under these conditions.
+		// Therefore, we check those conditions and return true if the registration happened less
+		// than 15 minutes ago. 15 minutes is an arbitrary number.
+		if ( isRegisteredDomain( domain ) && ! domain.hasWpcomNameservers ) {
+			const registeredTimestamp = Date.parse( domain.registrationDate );
+			const timeSinceRegistration = Date.now() - registeredTimestamp;
+
+			if ( timeSinceRegistration < 15 * 60 * 1000 ) {
+				return true;
+			}
 		}
 
 		const isHostedOnWpcom =
@@ -29,7 +47,7 @@ export function getGSuiteSupportedDomains( domains ) {
 /**
  * Given a list of domains does one of them support G Suite
  *
- * @param {Array} domains - list of domain objects
+ * @param {ResponseDomain[]} domains - list of domain objects
  * @returns {boolean} - Does list of domains contain a G Suited supported domain
  */
 export function hasGSuiteSupportedDomain( domains ) {

client/my-sites/email/email-providers-comparison/price/google-workspace.tsx
@@ -46,7 +46,7 @@ const GoogleWorkspacePrice = ( {
 
 	const canPurchaseGSuite = useSelector( canUserPurchaseGSuite );
 
-	if ( ! domain && ! isDomainInCart ) {
+	if ( ! domain ) {
 		return null;
 	}
 

commit b5061090f8c396c422ef39430aa6f9e0d77f1571

Fix regression introduced in #64822

client/lib/gsuite/gsuite-supported-domain.js
@@ -47,9 +47,9 @@ export function getGSuiteSupportedDomains( domains ) {
 /**
  * Given a list of domains does one of them support G Suite
  *
- * @param {ResponseDomain[]} domains - list of domain objects
+ * @param {ResponseDomain?[]} domains - list of domain objects
  * @returns {boolean} - Does list of domains contain a G Suited supported domain
  */
 export function hasGSuiteSupportedDomain( domains ) {
-	return getGSuiteSupportedDomains( domains ).length > 0;
+	return getGSuiteSupportedDomains( domains.filter( Boolean ) ).length > 0;
 }

client/my-sites/email/email-providers-comparison/price/google-workspace.tsx
@@ -46,7 +46,7 @@ const GoogleWorkspacePrice = ( {
 
 	const canPurchaseGSuite = useSelector( canUserPurchaseGSuite );
 
-	if ( ! domain ) {
+	if ( ! domain && ! isDomainInCart ) {
 		return null;
 	}
 