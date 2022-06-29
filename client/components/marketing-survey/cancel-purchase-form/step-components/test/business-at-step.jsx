a8b2d32eccbda2f0141c72e2478095be0c24e76d
brookewp <brooke.kaminski@automattic.com>
2022-06-29 13:27:13 -0700

    migrate tests from enyzme to testing-library

--- client/components/marketing-survey/cancel-purchase-form/step-components/test/business-at-step.jsx
+++ client/components/marketing-survey/cancel-purchase-form/step-components/test/business-at-step.jsx
@@ -1,75 +1,84 @@
-import { shallow } from 'enzyme';
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BusinessATStep } from '../business-at-step';
 
 const noop = () => {};
 
-describe( 'BusinessATStep', () => {
-	describe( 'rendering translated content', () => {
-		let wrapper;
-		const translate = ( content ) => `Translated: ${ content }`;
+describe( 'rendering translated content', () => {
+	const translate = ( content ) => `Translated: ${ content }`;
 
-		beforeEach( () => {
-			wrapper = shallow( <BusinessATStep recordTracksEvent={ noop } translate={ translate } /> );
-		} );
-
-		test( 'should render translated heading content', () => {
-			expect( wrapper.find( 'FormSectionHeading' ).props().children ).toEqual(
-				'Translated: New! Install Custom Plugins and Themes'
-			);
-		} );
-
-		test( 'should render translated link content', () => {
-			expect( wrapper.find( 'FormFieldset > p' ).at( 0 ).props().children ).toEqual(
-				'Translated: Have a theme or plugin you need to install to build the site you want? ' +
-					'Now you can! ' +
-					'Learn more about {{pluginLink}}installing plugins{{/pluginLink}} and ' +
-					'{{themeLink}}uploading themes{{/themeLink}} today.'
-			);
-		} );
-
-		test( 'should render translated confirmation content', () => {
-			expect( wrapper.find( 'FormFieldset > p' ).at( 1 ).props().children ).toEqual(
-				'Translated: Are you sure you want to cancel your subscription and lose access to these new features?'
-			);
-		} );
+	test( 'should render translated heading content', () => {
+		const { container } = render(
+			<BusinessATStep recordTracksEvent={ noop } translate={ translate } />
+		);
+		expect( container.firstChild ).toHaveTextContent(
+			'Translated: New! Install Custom Plugins and Themes'
+		);
 	} );
 
-	describe( 'rendered links', () => {
-		const translate = ( content, params ) => {
-			if ( params && params.components ) {
-				return (
-					<div>
-						{ params.components.pluginLink }
-						{ params.components.themeLink }
-					</div>
-				);
-			}
-			return null;
-		};
-		let recordTracksEvent;
-		let wrapper;
+	test( 'should render translated link content', () => {
+		render( <BusinessATStep recordTracksEvent={ noop } translate={ translate } /> );
+		expect( screen.queryByRole( 'group' ) ).toHaveTextContent(
+			'Translated: Have a theme or plugin you need to install to build the site you want? ' +
+				'Now you can! ' +
+				'Learn more about {{pluginLink}}installing plugins{{/pluginLink}} and ' +
+				'{{themeLink}}uploading themes{{/themeLink}} today.'
+		);
+	} );
 
-		beforeEach( () => {
-			recordTracksEvent = jest.fn();
-			wrapper = shallow(
-				<BusinessATStep translate={ translate } recordTracksEvent={ recordTracksEvent } />
+	test( 'should render translated confirmation content', () => {
+		render( <BusinessATStep recordTracksEvent={ noop } translate={ translate } /> );
+		expect(
+			screen.getByText(
+				'Translated: Are you sure you want to cancel your subscription and lose access to these new features?'
+			)
+		).toBeVisible();
+	} );
+} );
+
+describe( 'rendered links', () => {
+	afterEach( () => {
+		jest.clearAllMocks();
+	} );
+
+	const translate = ( content, params ) => {
+		if ( params && params.components ) {
+			return (
+				<>
+					{ params.components.pluginLink }
+					{ params.components.themeLink }
+				</>
 			);
-		} );
+		}
+		return null;
+	};
+	const recordTracksEvent = jest.fn();
 
-		test( 'should fire tracks event for plugin support link when clicked', () => {
-			wrapper.find( 'a' ).at( 0 ).simulate( 'click' );
+	test( 'should fire tracks event for plugin support link when clicked', async () => {
+		render( <BusinessATStep translate={ translate } recordTracksEvent={ recordTracksEvent } /> );
+		const link = screen.queryAllByRole( 'link' )[ 0 ];
 
-			expect( recordTracksEvent ).toHaveBeenCalledWith(
-				'calypso_cancellation_business_at_plugin_support_click'
-			);
-		} );
+		link.addEventListener( 'click', ( event ) => event.preventDefault(), false );
+		await userEvent.click( link );
 
-		test( 'should fire tracks event for theme support link when clicked', () => {
-			wrapper.find( 'a' ).at( 1 ).simulate( 'click' );
+		expect( recordTracksEvent ).toHaveBeenCalledWith(
+			'calypso_cancellation_business_at_plugin_support_click'
+		);
+	} );
 
-			expect( recordTracksEvent ).toHaveBeenCalledWith(
-				'calypso_cancellation_business_at_theme_support_click'
-			);
-		} );
+	test( 'should fire tracks event for theme support link when clicked', async () => {
+		render( <BusinessATStep translate={ translate } recordTracksEvent={ recordTracksEvent } /> );
+		const link = screen.queryAllByRole( 'link' )[ 1 ];
+
+		link.addEventListener( 'click', ( event ) => event.preventDefault(), false );
+		await userEvent.click( link );
+
+		expect( recordTracksEvent ).toHaveBeenCalledWith(
+			'calypso_cancellation_business_at_theme_support_click'
+		);
 	} );
 } );
