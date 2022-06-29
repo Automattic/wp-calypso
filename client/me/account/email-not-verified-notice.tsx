973bb2867e27a03b7eb62a84f851cef9e7ab38ea
Michael Kelly <kikemelly@gmail.com>
2022-06-29 11:07:55 +0100

    Add/resend email option (#64970)
    * Send Email: sync feedback messages across codebase (#64345)
    
    * Add missing import to docs
    
    * Rework docs; fix lint issue
    
    * Send Email: some changes after code review (#64345)
    
    * Send Email: some changes after code review (#64345)

---
+++ client/me/account/email-not-verified-notice.tsx
@@ -0,0 +1,53 @@
+import { __ } from '@wordpress/i18n';
+import { useDispatch, useSelector } from 'react-redux';
+import Notice from 'calypso/components/notice';
+import NoticeAction from 'calypso/components/notice/notice-action';
+import { successNotice, errorNotice } from 'calypso/state/notices/actions';
+import { useSendEmailVerification } from '../../landing/stepper/hooks/use-send-email-verification';
+import './email-not-verified-notice.scss';
+import { isCurrentUserEmailVerified } from '../../state/current-user/selectors';
+
+const resendEmailNotice = 'resend-verification-email';
+
+const EmailNotVerifiedNotice = () => {
+	const dispatch = useDispatch();
+	const resendEmail = useSendEmailVerification();
+	const isVerified = useSelector( isCurrentUserEmailVerified );
+
+	if ( isVerified ) {
+		return null;
+	}
+
+	const handleResend = async () => {
+		try {
+			const result = await resendEmail();
+			if ( result.success ) {
+				dispatch(
+					successNotice( __( 'The verification email has been sent.' ), {
+						id: resendEmailNotice,
+						duration: 4000,
+					} )
+				);
+				return;
+			}
+		} catch ( Error ) {}
+		dispatch(
+			errorNotice( __( 'An error has occurred, please check your connection and retry.' ), {
+				id: resendEmailNotice,
+			} )
+		);
+	};
+
+	return (
+		<Notice
+			className="email-not-verified-notice"
+			showDismiss={ false }
+			status="is-warning"
+			text={ __( 'Your email has not been verified yet. ' ) }
+		>
+			<NoticeAction onClick={ handleResend }>{ __( 'Resend email' ) }</NoticeAction>
+		</Notice>
+	);
+};
+
+export default EmailNotVerifiedNotice;
