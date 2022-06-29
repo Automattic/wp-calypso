d0783691a1bcd9cbc927eb8cae4a65cffbe0c917
Yashwin <yashwinpoojary@gmail.com>
2022-06-29 10:39:31 +0530

    Remove refetchOnWindowFocus as it defaults to true

--- client/data/agency-dashboard/use-fetch-dashboard-sites.ts
+++ client/data/agency-dashboard/use-fetch-dashboard-sites.ts
@@ -44,7 +44,6 @@ const useFetchDashboardSites = (
 					perPage: data.per_page,
 				};
 			},
-			refetchOnWindowFocus: true,
 			onError: () =>
 				dispatch(
 					errorNotice( translate( 'Failed to retrieve your sites. Please try again later.' ) )
