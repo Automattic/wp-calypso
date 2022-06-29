90f9ab2c55e7723ebab3c991da796e29ccf59635
Christos <chriskmnds@gmail.com>
2022-06-29 17:09:51 +0300

    useAddOnFeatureSlugs. cleanup

--- client/my-sites/add-ons/hooks/use-add-on-purchase-status.ts
+++ client/my-sites/add-ons/hooks/use-add-on-purchase-status.ts
@@ -3,24 +3,23 @@ import { useSelector } from 'react-redux';
 import { getSitePurchases } from 'calypso/state/purchases/selectors/get-site-purchases';
 import siteHasFeature from 'calypso/state/selectors/site-has-feature';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
-import getFeatureSlug from '../utils/get-feature-slug';
+import useAddOnFeatureSlugs from './use-add-on-feature-slugs';
 
 /**
  * Returns whether add-on product has been purchased or included in site plan.
  */
 const useAddOnPurchaseStatus = ( addOnProductSlug: string ) => {
 	const translate = useTranslate();
+	const addOnFeatureSlugs = useAddOnFeatureSlugs( addOnProductSlug );
+
 	const { purchased, isSiteFeature } = useSelector( ( state ) => {
 		const selectedSite = getSelectedSite( state );
 		const sitePurchases = getSitePurchases( state, selectedSite?.ID );
-		const addOnFeatureSlug = getFeatureSlug( addOnProductSlug );
 		return {
-			purchased:
-				sitePurchases.filter( ( product ) => product.productSlug === addOnProductSlug ).length > 0,
+			purchased: sitePurchases.find( ( product ) => product.productSlug === addOnProductSlug ),
 			isSiteFeature:
 				selectedSite &&
-				addOnFeatureSlug &&
-				siteHasFeature( state, selectedSite?.ID, addOnFeatureSlug ),
+				addOnFeatureSlugs?.find( ( slug ) => siteHasFeature( state, selectedSite?.ID, slug ) ),
 		};
 	} );
 
