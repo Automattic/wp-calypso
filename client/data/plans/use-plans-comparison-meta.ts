fde369b57b5fa8bd6c717b0eef51d3963fa1df14
donlair <don.lair@gmail.com>
2022-06-30 17:15:21 -0400

    Launching treatment variation as new control  (#64762)
    * Launching treatment variation as new control (still has hardcoded prices)
    
    * Get the plans comparison metadata from backend
    
    * Fix type error
    
    * Use /product endpoint to get the price of the no-ads product
    
    Co-authored-by: taggon <gonom9@gmail.com>

---
+++ client/data/plans/use-plans-comparison-meta.ts
@@ -0,0 +1,21 @@
+import { useQuery } from 'react-query';
+import wpcom from 'calypso/lib/wp';
+
+export type PlansComparisonMetaData = {
+	currency: string;
+	bottom_theme_price: number;
+	bottom_theme_price_display: string;
+};
+
+export default function usePlansComparisonMeta( currency: string ) {
+	return useQuery< unknown, unknown, PlansComparisonMetaData >(
+		[ 'plans-comparison-meta', currency ],
+		() =>
+			wpcom.req.get(
+				{
+					path: '/plans-comparison-meta',
+				},
+				{ apiVersion: '1.2' }
+			)
+	);
+}
