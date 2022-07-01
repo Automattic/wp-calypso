cb850ddcea4b625ee80ed2cd1bf6e1ac594e1cce
Praveen <spraveenitpro@gmail.com>
2022-07-01 10:40:04 -0700

    Migrate head component from enzyme to Testing library to include feedback of removing snapshots

--- client/components/head/test/index.jsx
+++ client/components/head/test/index.jsx
@@ -3,24 +3,17 @@
  */
 
 import { render, screen } from '@testing-library/react';
-import renderer from 'react-test-renderer';
 import Head from '../';
 
 describe( 'Head', () => {
 	test( 'should render default title', () => {
 		render( <Head /> );
-		expect( screen.getByText( 'WordPress.com' ) ).toBeInTheDocument();
-		expect( screen.getByText( 'WordPress.com' ) ).not.toBeVisible();
-		const tree = renderer.create( <Head /> ).toJSON();
-		expect( tree ).toMatchSnapshot();
+		expect( screen.queryByText( 'WordPress.com' ) ).toBeInTheDocument();
 	} );
 
 	test( 'should render custom title', () => {
 		const title = 'Arbitrary Custom Title';
 		render( <Head title={ title } /> );
-		expect( screen.getByText( title ) ).toBeInTheDocument();
-		expect( screen.getByText( title ) ).not.toBeVisible();
-		const tree = renderer.create( <Head /> ).toJSON();
-		expect( tree ).toMatchSnapshot();
+		expect( screen.queryByText( title ) ).toBeInTheDocument();
 	} );
 } );
