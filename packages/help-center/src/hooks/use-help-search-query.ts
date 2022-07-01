94716701742e38cd9bb5c975d295793a82c3384b
Emanuele Buccelli <escapemanuele@gmail.com>
2022-07-01 16:22:30 +0200

    Search results showing

--- packages/help-center/src/hooks/use-help-search-query.ts
+++ packages/help-center/src/hooks/use-help-search-query.ts
@@ -2,6 +2,13 @@ import { useLocale } from '@automattic/i18n-utils';
 import { useQuery } from 'react-query';
 import wpcomRequest from 'wpcom-proxy-request';
 import type { LinksForSection } from '@automattic/data-stores';
+
+interface APIResponse {
+	search_results: {
+		wordpress_support_links: LinksForSection[];
+	};
+}
+
 export const useHelpSearchQuery = (
 	search: string,
 	queryOptions: Record< string, unknown > = {}
@@ -19,12 +26,12 @@ export const useHelpSearchQuery = (
 	return useQuery< { wordpress_support_links: LinksForSection[] } >(
 		[ 'help', search ],
 		() =>
-			wpcomRequest( {
+			wpcomRequest< APIResponse >( {
 				path: 'help-center/search',
 				query: params.toString(),
 				apiNamespace: 'wpcom/v2/',
 				apiVersion: '2',
-			} ),
+			} ).then( ( response ) => response?.search_results ),
 		{
 			enabled: !! search,
 			...queryOptions,
