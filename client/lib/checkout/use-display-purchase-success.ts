45e8df08526892a74432b4bf0674e06a2e8eaad5
Payton Swick <payton@foolord.com>
2022-07-01 18:21:58 -0400

    Remove notice from query string after showing message

--- client/lib/checkout/use-display-purchase-success.ts
+++ client/lib/checkout/use-display-purchase-success.ts
@@ -4,8 +4,8 @@ import { useDispatch } from 'react-redux';
 import { successNotice } from 'calypso/state/notices/actions';
 
 export function useDisplayPurchaseSuccess( areNoticesAllowed = true ): void {
-	const queryString = new URLSearchParams( window?.location?.search );
-	const noticeType = queryString.get( 'notice' ) ?? undefined;
+	const queryParams = new URLSearchParams( window?.location?.search );
+	const noticeType = queryParams.get( 'notice' ) ?? undefined;
 	const translate = useTranslate();
 	const reduxDispatch = useDispatch();
 	const shouldShowNotice = Boolean( areNoticesAllowed && noticeType === 'purchase-success' );
@@ -19,5 +19,11 @@ export function useDisplayPurchaseSuccess( areNoticesAllowed = true ): void {
 		lastShownNotice.current = noticeType;
 		const successMessage = translate( 'Your purchase has been completed!' );
 		reduxDispatch( successNotice( successMessage ) );
+
+		// Remove the notice query string from the URL after displaying the message
+		// so that it will not be included in bookmarks or browser history.
+		const currentUrl = new URL( window?.location?.href ?? 'https://wordpress.com' );
+		currentUrl.searchParams.delete( 'notice' );
+		window?.history?.replaceState?.( null, '', currentUrl );
 	}, [ shouldShowNotice, translate, reduxDispatch, noticeType ] );
 }
