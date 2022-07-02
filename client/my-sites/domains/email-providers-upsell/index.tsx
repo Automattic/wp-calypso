f8543518d2a83628c4e31f7da612b6307c17c20b
Olaseni Oluwunmi <277661+olaseni@users.noreply.github.com>
2022-07-02 01:35:12 +0100

    Update JSX implementation to TSX

--- client/my-sites/domains/email-providers-upsell/index.tsx
+++ client/my-sites/domains/email-providers-upsell/index.tsx
@@ -1,17 +1,24 @@
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
+import { IntervalLength } from 'calypso/my-sites/email/email-providers-comparison/interval-length';
 import EmailProvidersStackedComparison from 'calypso/my-sites/email/email-providers-stacked-comparison';
 
-export default function EmailProvidersUpsell( { domain, interval, provider } ) {
-	return (
-		<CalypsoShoppingCartProvider>
-			<EmailProvidersStackedComparison
-				comparisonContext="domain-upsell"
-				isDomainInCart={ true }
-				selectedDomainName={ domain }
-				selectedEmailProviderSlug={ provider }
-				selectedIntervalLength={ interval }
-				source="domain-upsell"
-			/>
-		</CalypsoShoppingCartProvider>
-	);
+interface EmailProvidersUpsellProps {
+	domain: string;
+	interval?: IntervalLength;
+	provider: string;
 }
+
+const EmailProvidersUpsell = ( { domain, interval, provider }: EmailProvidersUpsellProps ) => (
+	<CalypsoShoppingCartProvider>
+		<EmailProvidersStackedComparison
+			comparisonContext="domain-upsell"
+			isDomainInCart={ true }
+			selectedDomainName={ domain }
+			selectedEmailProviderSlug={ provider }
+			selectedIntervalLength={ interval }
+			source="domain-upsell"
+		/>
+	</CalypsoShoppingCartProvider>
+);
+
+export default EmailProvidersUpsell;
