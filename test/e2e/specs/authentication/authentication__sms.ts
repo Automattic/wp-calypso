f860ede8e6ead8d0a7dd4bf4b760b701b8065bdb
Edwin Takahashi <egao@outlook.com>
2022-07-05 15:01:41 -0700

    Move specs to new test group "Authentication" - Magic Link - TOTP - SMS OTP

--- test/e2e/specs/authentication/authentication__sms.ts
+++ test/e2e/specs/authentication/authentication__sms.ts
@@ -1,8 +1,7 @@
 /**
- * @group calypso-release
+ * @group authentication
  *
- * One possible problem with this spec is that SMS OTP can only be requested once per minute.
- * In addition, anecdotally it appears that any login attempt during this one-minute blackout period resets the timer, though this isn't confirmed.
+ * One real problem with this spec is that SMS OTP can only be requested once every 5 minutes as per the backend code.
  * What this means is that in rare cases where Pre-Release Tests are lined up one after another in the queue, it may result in an unexpected failure of this spec, which would appear to be a flaky test failure to the developer.
  *
  * It may be necessary to keep a close eye on this test and immediately pull the test from rotation if we find the flakiness exceeds an acceptable level.
