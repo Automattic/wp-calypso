22dc5b7e88a83551611367830d574e785671c38f
Jeremy Yip <jeremy.yip@automattic.com>
2022-07-05 09:56:46 -0700

    Editing Toolkit: Add feature flags to the help center (#65142)

---
+++ packages/help-center/src/contexts/FeatureFlagContext.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { useContext } from 'react';
+import { FeatureFlags } from '../types';
+
+// Lets typescript know about the feature flags global added through the help-center-script
+declare const helpCenterFeatureFlags: FeatureFlags;
+
+const FeatureFlagContext = React.createContext< FeatureFlags | undefined >( undefined );
+
+export const FeatureFlagProvider: React.FC< { children: JSX.Element } > = function ( {
+	children,
+} ) {
+	return (
+		<FeatureFlagContext.Provider value={ helpCenterFeatureFlags }>
+			{ children }
+		</FeatureFlagContext.Provider>
+	);
+};
+
+export function useFeatureFlags() {
+	const featureFlags = useContext( FeatureFlagContext );
+
+	// Trying to call hook outside of component that renders FeatureFlagProvider
+	if ( featureFlags === undefined ) {
+		throw new Error( 'useFeatureFlags must be used within a FeatureFlagProvider' );
+	}
+
+	return featureFlags;
+}
