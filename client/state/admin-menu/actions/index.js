34b88cf7216725ae5cdebd2de840fd4bd5fe90a4
mmtr <miguel.torres@automattic.com>
2022-07-06 15:28:13 +0200

    Update menu after purchasing a marketplace plugin

--- client/state/admin-menu/actions/index.js
+++ client/state/admin-menu/actions/index.js
@@ -1,6 +1,10 @@
 import 'calypso/state/admin-menu/init';
 import 'calypso/state/data-layer/wpcom/sites/admin-menu';
 import { ADMIN_MENU_REQUEST, ADMIN_MENU_RECEIVE } from 'calypso/state/action-types';
+import { requestLatestAtomicTransfer } from 'calypso/state/atomic/transfers/actions';
+import { getLatestAtomicTransfer } from 'calypso/state/atomic/transfers/selectors';
+import { fetchSitePlugins } from 'calypso/state/plugins/installed/actions';
+import { getPluginOnSite } from 'calypso/state/plugins/installed/selectors';
 
 export const requestAdminMenu = function requestAdminMenu( siteId ) {
 	return {
@@ -16,3 +20,36 @@ export const receiveAdminMenu = function receiveAdminMenu( siteId, menu ) {
 		menu,
 	};
 };
+
+export const updateAdminMenuAfterMarketplaceInstallation =
+	( siteId, productSlug ) => ( dispatch, getState ) => {
+		const { transfer } = getLatestAtomicTransfer( getState(), siteId );
+		const pluginOnSite = getPluginOnSite( getState(), siteId, productSlug );
+
+		// Wait for the Atomic transfer to be completed...
+		if ( transfer?.status !== 'completed' ) {
+			// Check again after 2s.
+			dispatch( requestLatestAtomicTransfer( siteId ) );
+			setTimeout(
+				() => dispatch( updateAdminMenuAfterMarketplaceInstallation( siteId, productSlug ) ),
+				2000
+			);
+			return;
+		}
+
+		// ...And for the plugin to be activated.
+		if ( ! pluginOnSite ) {
+			// Check again after 2s.
+			dispatch( fetchSitePlugins( siteId ) );
+			setTimeout(
+				() => dispatch( updateAdminMenuAfterMarketplaceInstallation( siteId, productSlug ) ),
+				2000
+			);
+			return;
+		}
+
+		// Wait for another 2s before updating the menu to allow the plugin to finish any potential setup.
+		setTimeout( () => {
+			dispatch( requestAdminMenu( siteId ) );
+		}, 2000 );
+	};
