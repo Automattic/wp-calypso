47f4561511653296a6f2f7d8467a2ce765a32b36
Robert Sreberski <robertsreberski@gmail.com>
2022-07-06 10:52:00 +0200

    Implement GA4 analytics for Jetpack Green (#64993)
    * Add gtag ts types
    
    * Remove redundant initializers for gtag
    
    * Assign a valid return type to cost-to-usd
    
    * Do not change properties in the cart project
    
    * Flag the function with the comment about wrong type of fn property
    
    * Implement GA4
    
    * Implement typed version of existing function
    
    * Remove redundant assertion (is already handled in record-purchase.js)
    
    * Handle the param to include only specific page views in Jetpack GA
    
    * Use USD jetpack cost in GaPurchase
    
    * Export functions individualy rather than export object
    
    * Allow send_page_view to be overwritten
    
    * Add `null` to return type of costToUsd()

---
+++ client/lib/analytics/utils/split-wpcom-jetpack-cart-info.ts
@@ -0,0 +1,42 @@
+import { isJetpackPlan, isJetpackProduct } from '@automattic/calypso-products';
+import { ResponseCart, ResponseCartProduct } from '@automattic/shopping-cart';
+import costToUSD from './cost-to-usd';
+
+export type WpcomJetpackCartInfo = {
+	wpcomProducts: ResponseCartProduct[];
+	jetpackProducts: ResponseCartProduct[];
+	containsWpcomProducts: boolean;
+	containsJetpackProducts: boolean;
+	jetpackCost: number;
+	wpcomCost: number;
+	jetpackCostUSD: number;
+	wpcomCostUSD: number;
+	totalCostUSD: number;
+};
+
+export function splitWpcomJetpackCartInfo( cart: ResponseCart ): WpcomJetpackCartInfo {
+	const jetpackCost = cart.products
+		.map( ( product ) =>
+			isJetpackPlan( product ) || isJetpackProduct( product ) ? product.cost : 0
+		)
+		.reduce( ( accumulator, cost ) => accumulator + cost, 0 );
+	const wpcomCost = cart.total_cost_integer / 100 - jetpackCost;
+	const wpcomProducts = cart.products.filter(
+		( product ) => ! isJetpackPlan( product ) && ! isJetpackProduct( product )
+	);
+	const jetpackProducts = cart.products.filter(
+		( product ) => isJetpackPlan( product ) || isJetpackProduct( product )
+	);
+
+	return {
+		wpcomProducts: wpcomProducts,
+		jetpackProducts: jetpackProducts,
+		containsWpcomProducts: 0 !== wpcomProducts.length,
+		containsJetpackProducts: 0 !== jetpackProducts.length,
+		jetpackCost: jetpackCost,
+		wpcomCost: wpcomCost,
+		jetpackCostUSD: costToUSD( jetpackCost, cart.currency ) ?? 0,
+		wpcomCostUSD: costToUSD( wpcomCost, cart.currency ) ?? 0,
+		totalCostUSD: costToUSD( cart.total_cost_integer / 100, cart.currency ) ?? 0,
+	};
+}
