dd2c4b565036e0511bdf6c5215c317c1560d9bee
Dean Sas <dean.sas@automattic.com>
2022-07-06 12:23:28 +0100

    Allow plugin slugs and store slugs to diverge
    hasMarketplaceProduct is sometimes called with product slugs and
    sometimes called with store product slugs. In both cases they are
    compared to the keys in the productList in the redux state, which are
    store product slugs.
    
    A product slug looks like: woocommerce-product-csv-import-suite
    
    A store product slug looks like: wc_product_csv_import_suite_yearly
    
    This change follows the approach proposed in pdh6GB-Pq-p2#comment-1538.
    Specifically, it always compares against the billing_product_slug and
    against the store product slug, removing the need to normalise slugs
    before comparing.
    
    A unit test has been added to support this, and one unit test removed.
    The removed test wasn't a valid test case - it was supplying an
    artificial slug which would never be supplied in normal circumstances,
    not matching either kind of slug.
    
    This fixes part of https://github.com/Automattic/dotcom-manage/issues/634

--- packages/calypso-products/src/has-marketplace-product.ts
+++ packages/calypso-products/src/has-marketplace-product.ts
@@ -1,20 +1,17 @@
-const cleanSlug = ( slug: string ) =>
-	slug && slug.replace( /_/g, '-' ).split( /-(monthly|yearly|2y)/ )[ 0 ];
-
 /**
- * Returns true if a list of products contains a marketplace product with the specified product slug.
+ * Returns true if a list of products contains a marketplace product with a matching product or store product slug.
  *
  * @param {object} productsList - List of products
- * @param {string} productSlug - internal product slug, eg 'jetpack_premium'
+ * @param {string} productSlug - internal product slug e.g. woocommerce-product-csv-import-suite or store product slug, e.g wc_product_csv_import_suite_yearly
  * @returns {boolean}
  */
 export const hasMarketplaceProduct = (
-	productsList: Record< string, { product_type: string; product_slug: string } >,
+	productsList: Record< string, { product_type: string; billing_product_slug: string } >,
 	productSlug: string
 ): boolean =>
 	Object.entries( productsList ).some(
-		( [ subscriptionSlug, { product_type } ] ) =>
-			cleanSlug( productSlug ) === cleanSlug( subscriptionSlug ) &&
+		( [ subscriptionSlug, { product_type, billing_product_slug } ] ) =>
+			( productSlug === subscriptionSlug || productSlug === billing_product_slug ) &&
 			// additional type check needed when called from JS context
 			typeof product_type === 'string' &&
 			product_type.startsWith( 'marketplace' )
