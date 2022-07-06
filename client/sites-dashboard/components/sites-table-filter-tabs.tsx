f7265e57df944600c2b23e1fe5bebc4b142e2a4a
sejas <antonio@sejas.es>
2022-07-06 15:37:27 +0200

    Sites Dashboard: style tabs with scroll in responsive

--- client/sites-dashboard/components/sites-table-filter-tabs.tsx
+++ client/sites-dashboard/components/sites-table-filter-tabs.tsx
@@ -1,5 +1,7 @@
+import styled from '@emotion/styled';
 import { TabPanel } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
+import SitesBadge from './sites-badge';
 import type { SiteData } from 'calypso/state/ui/selectors/site-data'; // eslint-disable-line no-restricted-imports
 
 interface SitesTableFilterTabsProps {
@@ -8,6 +10,37 @@ interface SitesTableFilterTabsProps {
 	className?: string;
 }
 
+interface FilteredSites {
+	[ name: string ]: SiteData[];
+}
+
+interface SiteTab extends Omit< TabPanel.Tab, 'title' > {
+	title: React.ReactChild;
+}
+
+const SitesTabPanel = styled( TabPanel )`
+	.components-tab-panel__tabs {
+		overflow-x: auto;
+		padding-bottom: 10px;
+	}
+
+	.components-tab-panel__tabs-item {
+		--wp-admin-theme-color: var( --studio-gray-100 );
+		color: var( --studio-gray-60 );
+		padding: 0;
+		margin-right: 24px;
+		font-size: 16px;
+		flex-shrink: 0;
+	}
+	.components-tab-panel__tabs-item:hover,
+	.components-tab-panel__tabs-item.is-active,
+	.components-tab-panel__tabs-item.is-active:focus,
+	.components-tab-panel__tabs-item:focus:not( :disabled ) {
+		box-shadow: inset 0 -2px 0 0 var( --wp-admin-theme-color );
+		color: var( --studio-gray-100 );
+	}
+`;
+
 export function SitesTableFilterTabs( {
 	allSites,
 	children: renderContents,
@@ -15,18 +48,32 @@ export function SitesTableFilterTabs( {
 }: SitesTableFilterTabsProps ) {
 	const { __ } = useI18n();
 
+	let tabs: SiteTab[] = [
+		{ name: 'all', title: __( 'All' ) },
+		{ name: 'launched', title: __( 'Launched' ) },
+		{ name: 'private', title: __( 'Private' ) },
+		{ name: 'coming-soon', title: __( 'Coming soon' ) },
+	];
+
+	const filteredSites: FilteredSites = tabs.reduce(
+		( acc, { name } ) => ( { ...acc, [ name ]: filterSites( allSites, name ) } ),
+		{}
+	);
+
+	tabs = tabs.map( ( { name, title } ) => ( {
+		name,
+		title: (
+			<>
+				{ title }
+				<SitesBadge>{ filteredSites[ name ].length }</SitesBadge>
+			</>
+		),
+	} ) );
+
 	return (
-		<TabPanel
-			className={ className }
-			tabs={ [
-				{ name: 'all', title: __( 'All' ) },
-				{ name: 'launched', title: __( 'Launched' ) },
-				{ name: 'private', title: __( 'Private' ) },
-				{ name: 'coming-soon', title: __( 'Coming soon' ) },
-			] }
-		>
-			{ ( tab ) => renderContents( filterSites( allSites, tab.name ) ) }
-		</TabPanel>
+		<SitesTabPanel className={ className } tabs={ tabs as TabPanel.Tab[] }>
+			{ ( tab ) => renderContents( filteredSites[ tab.name ] ) }
+		</SitesTabPanel>
 	);
 }
 
