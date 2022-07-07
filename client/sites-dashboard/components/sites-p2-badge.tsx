4e6aae1e1865cfe9a330a6aa0740d1e18de052bf
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-07-07 16:12:35 -0700

    Extend `SitesP2Badge` from `SitesLaunchStatusBadge`

--- client/sites-dashboard/components/sites-p2-badge.tsx
+++ client/sites-dashboard/components/sites-p2-badge.tsx
@@ -1,14 +1,9 @@
 import styled from '@emotion/styled';
+import SitesLaunchStatusBadge from './sites-launch-status-badge';
 
-const SitesLaunchStatusBadge = styled.span`
-	font-size: 12px;
-	font-weight: 400;
+const SitesP2Badge = styled( SitesLaunchStatusBadge )`
 	color: #fff;
 	background-color: #2b65f6;
-	padding: 0px 10px;
-	border-radius: 4px;
-	line-height: 20px;
-	display: inline-block;
 `;
 
-export default SitesLaunchStatusBadge;
+export default SitesP2Badge;
