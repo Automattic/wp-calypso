2b4ddeffa56e26d78327a0deb6db703bce86286f
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-07-07 06:31:06 -0700

    Sites Dashboard: Indicate launch status with labels

---
+++ client/sites-dashboard/components/sites-p2-badge.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const SitesLaunchStatusBadge = styled.span`
+	font-size: 12px;
+	font-weight: 400;
+	color: #fff;
+	background-color: #2b65f6;
+	padding: 0px 10px;
+	border-radius: 4px;
+	line-height: 20px;
+	display: inline-block;
+`;
+
+export default SitesLaunchStatusBadge;
--- client/sites-dashboard/components/sites-table-row.tsx
+++ client/sites-dashboard/components/sites-table-row.tsx
@@ -3,21 +3,12 @@ import { useI18n } from '@wordpress/react-i18n';
 import SiteIcon from 'calypso/blocks/site-icon';
 import EllipsisMenu from 'calypso/components/ellipsis-menu';
 import PopoverMenuItem from 'calypso/components/popover-menu/item';
+import SitesLaunchStatusBadge from './sites-launch-status-badge';
+import SitesP2Badge from './sites-p2-badge';
+import type { SiteData } from 'calypso/state/ui/selectors/site-data';
 
 interface SiteTableRowProps {
-	site: Site;
-}
-
-interface Site {
-	ID: number;
-	name: string;
-	slug: string;
-	URL: string;
-	plan: SitePlan;
-}
-
-interface SitePlan {
-	product_name_short: string;
+	site: SiteData;
 }
 
 const Row = styled.tr`
@@ -44,7 +35,6 @@ const SiteName = styled.h2`
 	font-size: 16px;
 	letter-spacing: -0.4px;
 	color: var( --studio-gray-100 );
-	margin-bottom: 8px;
 	a {
 		color: inherit;
 		&:hover {
@@ -68,7 +58,7 @@ const displaySiteUrl = ( siteUrl: string ) => {
 	return siteUrl.replace( 'https://', '' ).replace( 'http://', '' );
 };
 
-const VisitDashboardItem = ( site: Site ) => {
+const VisitDashboardItem = ( site: SiteData ) => {
 	const { __ } = useI18n();
 	return (
 		<PopoverMenuItem href={ getDashboardUrl( site.slug ) }>
@@ -77,8 +67,27 @@ const VisitDashboardItem = ( site: Site ) => {
 	);
 };
 
+const P2Badge = ( site: SiteData ) => {
+	if ( site.options && site.options.is_wpforteams_site ) {
+		return <SitesP2Badge>P2</SitesP2Badge>;
+	}
+	return null;
+};
+
+const LaunchStatusBadge = ( site: SiteData ) => {
+	const isComingSoon =
+		site.is_coming_soon || ( site.is_private && site.launch_status === 'unlaunched' );
+	if ( site.is_private && ! isComingSoon ) {
+		return <SitesLaunchStatusBadge>Private</SitesLaunchStatusBadge>;
+	} else if ( isComingSoon ) {
+		return <SitesLaunchStatusBadge>Coming soon</SitesLaunchStatusBadge>;
+	}
+	return null;
+};
+
 export default function SitesTableRow( { site }: SiteTableRowProps ) {
 	const { __ } = useI18n();
+
 	return (
 		<Row>
 			<td>
@@ -91,19 +100,26 @@ export default function SitesTableRow( { site }: SiteTableRowProps ) {
 						<SiteIcon siteId={ site.ID } size={ 50 } />
 					</a>
 					<div style={ { marginLeft: '20px' } }>
-						<SiteName>
-							<a href={ getDashboardUrl( site.slug ) } title={ __( 'Visit Dashboard' ) }>
-								{ site.name ? site.name : __( '(No Site Title)' ) }
-							</a>
-						</SiteName>
-						<SiteUrl
-							href={ site.URL }
-							target="_blank"
-							rel="noreferrer"
-							title={ __( 'Visit Site' ) }
-						>
-							{ displaySiteUrl( site.URL ) }
-						</SiteUrl>
+						<div style={ { display: 'flex', alignItems: 'center', marginBottom: '8px' } }>
+							<SiteName style={ { marginRight: '8px' } }>
+								<a href={ getDashboardUrl( site.slug ) } title={ __( 'Visit Dashboard' ) }>
+									{ site.name ? site.name : __( '(No Site Title)' ) }
+								</a>
+							</SiteName>
+							{ P2Badge( site ) }
+						</div>
+						<div>
+							<SiteUrl
+								href={ site.URL }
+								target="_blank"
+								rel="noreferrer"
+								title={ __( 'Visit Site' ) }
+								style={ { marginRight: '8px' } }
+							>
+								{ displaySiteUrl( site.URL ) }
+							</SiteUrl>
+							{ LaunchStatusBadge( site ) }
+						</div>
 					</div>
 				</div>
 			</td>
