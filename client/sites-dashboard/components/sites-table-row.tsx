ce86a8f4c75fee33ad39b73eca51d8f2fe9c6e42
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-07-08 07:25:54 -0700

    Sites Dashboard: Indicate launch status with labels (#65359)

--- client/sites-dashboard/components/sites-table-row.tsx
+++ client/sites-dashboard/components/sites-table-row.tsx
@@ -3,21 +3,12 @@ import { useI18n } from '@wordpress/react-i18n';
 import SiteIcon from 'calypso/blocks/site-icon';
 import EllipsisMenu from 'calypso/components/ellipsis-menu';
 import PopoverMenuItem from 'calypso/components/popover-menu/item';
+import SitesLaunchStatusBadge from './sites-launch-status-badge';
+import SitesP2Badge from './sites-p2-badge';
+import type { SiteData } from 'calypso/state/ui/selectors/site-data';
 
 interface SiteTableRowProps {
-	site: Site;
-}
-
-interface Site {
-	ID: number;
-	name: string;
-	slug: string;
-	URL: string;
-	plan: SitePlan;
-}
-
-interface SitePlan {
-	product_name_short: string;
+	site: SiteData;
 }
 
 const Row = styled.tr`
@@ -44,7 +35,6 @@ const SiteName = styled.h2`
 	font-size: 16px;
 	letter-spacing: -0.4px;
 	color: var( --studio-gray-100 );
-	margin-bottom: 8px;
 	a {
 		color: inherit;
 		&:hover {
@@ -68,7 +58,7 @@ const displaySiteUrl = ( siteUrl: string ) => {
 	return siteUrl.replace( 'https://', '' ).replace( 'http://', '' );
 };
 
-const VisitDashboardItem = ( site: Site ) => {
+const VisitDashboardItem = ( { site }: { site: SiteData } ) => {
 	const { __ } = useI18n();
 	return (
 		<PopoverMenuItem href={ getDashboardUrl( site.slug ) }>
@@ -79,6 +69,11 @@ const VisitDashboardItem = ( site: Site ) => {
 
 export default function SitesTableRow( { site }: SiteTableRowProps ) {
 	const { __ } = useI18n();
+
+	const isComingSoon =
+		site.is_coming_soon || ( site.is_private && site.launch_status === 'unlaunched' );
+	const isP2Site = site.options?.is_wpforteams_site;
+
 	return (
 		<Row>
 			<td>
@@ -91,26 +86,38 @@ export default function SitesTableRow( { site }: SiteTableRowProps ) {
 						<SiteIcon siteId={ site.ID } size={ 50 } />
 					</a>
 					<div style={ { marginLeft: '20px' } }>
-						<SiteName>
-							<a href={ getDashboardUrl( site.slug ) } title={ __( 'Visit Dashboard' ) }>
-								{ site.name ? site.name : __( '(No Site Title)' ) }
-							</a>
-						</SiteName>
-						<SiteUrl
-							href={ site.URL }
-							target="_blank"
-							rel="noreferrer"
-							title={ __( 'Visit Site' ) }
-						>
-							{ displaySiteUrl( site.URL ) }
-						</SiteUrl>
+						<div style={ { display: 'flex', alignItems: 'center', marginBottom: '8px' } }>
+							<SiteName style={ { marginRight: '8px' } }>
+								<a href={ getDashboardUrl( site.slug ) } title={ __( 'Visit Dashboard' ) }>
+									{ site.name ? site.name : __( '(No Site Title)' ) }
+								</a>
+							</SiteName>
+							{ isP2Site && <SitesP2Badge>P2</SitesP2Badge> }
+						</div>
+						<div>
+							<SiteUrl
+								href={ site.URL }
+								target="_blank"
+								rel="noreferrer"
+								title={ __( 'Visit Site' ) }
+								style={ { marginRight: '8px' } }
+							>
+								{ displaySiteUrl( site.URL ) }
+							</SiteUrl>
+							{ site.is_private && ! isComingSoon && (
+								<SitesLaunchStatusBadge>Private</SitesLaunchStatusBadge>
+							) }
+							{ isComingSoon && <SitesLaunchStatusBadge>Coming soon</SitesLaunchStatusBadge> }
+						</div>
 					</div>
 				</div>
 			</td>
 			<td className="sites-table-row__mobile-hidden">{ site.plan.product_name_short }</td>
 			<td className="sites-table-row__mobile-hidden"></td>
 			<td style={ { width: '20px' } }>
-				<EllipsisMenu>{ VisitDashboardItem( site ) }</EllipsisMenu>
+				<EllipsisMenu>
+					<VisitDashboardItem site={ site } />
+				</EllipsisMenu>
 			</td>
 		</Row>
 	);
