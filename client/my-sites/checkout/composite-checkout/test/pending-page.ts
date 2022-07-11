2ec853d0e3ac664812e0d1592d79fe2ca051f1a5
Payton Swick <payton@foolord.com>
2022-07-11 17:31:39 -0400

    Update tests for new signature and defaults

--- client/my-sites/checkout/composite-checkout/test/pending-page.ts
+++ client/my-sites/checkout/composite-checkout/test/pending-page.ts
@@ -5,7 +5,11 @@ describe( 'addUrlToPendingPageRedirect', () => {
 		const finalUrl = '/foo/bar/baz';
 		const siteSlug = 'example.com';
 		const orderId = '12345';
-		const actual = addUrlToPendingPageRedirect( finalUrl, siteSlug, orderId, 'relative' );
+		const actual = addUrlToPendingPageRedirect( finalUrl, {
+			siteSlug,
+			orderId,
+			urlType: 'relative',
+		} );
 		expect( actual ).toEqual(
 			`/checkout/thank-you/${ siteSlug }/pending/${ orderId }?redirectTo=${ encodeURIComponent(
 				finalUrl
@@ -17,7 +21,26 @@ describe( 'addUrlToPendingPageRedirect', () => {
 		const finalUrl = '/foo/bar/baz';
 		const siteSlug = 'example.com';
 		const orderId = '12345';
-		const actual = addUrlToPendingPageRedirect( finalUrl, siteSlug, orderId, 'absolute' );
+		const actual = addUrlToPendingPageRedirect( finalUrl, {
+			siteSlug,
+			orderId,
+			urlType: 'absolute',
+		} );
+		expect( actual ).toEqual(
+			`https://wordpress.com/checkout/thank-you/${ siteSlug }/pending/${ orderId }?redirectTo=${ encodeURIComponent(
+				finalUrl
+			) }`
+		);
+	} );
+
+	it( 'returns an absolute URL in default mode', () => {
+		const finalUrl = '/foo/bar/baz';
+		const siteSlug = 'example.com';
+		const orderId = '12345';
+		const actual = addUrlToPendingPageRedirect( finalUrl, {
+			siteSlug,
+			orderId,
+		} );
 		expect( actual ).toEqual(
 			`https://wordpress.com/checkout/thank-you/${ siteSlug }/pending/${ orderId }?redirectTo=${ encodeURIComponent(
 				finalUrl
@@ -27,9 +50,10 @@ describe( 'addUrlToPendingPageRedirect', () => {
 
 	it( 'returns a no-site URL if no siteSlug is provided', () => {
 		const finalUrl = '/foo/bar/baz';
-		const siteSlug = undefined;
 		const orderId = '12345';
-		const actual = addUrlToPendingPageRedirect( finalUrl, siteSlug, orderId, 'absolute' );
+		const actual = addUrlToPendingPageRedirect( finalUrl, {
+			orderId,
+		} );
 		expect( actual ).toEqual(
 			`https://wordpress.com/checkout/thank-you/no-site/pending/${ orderId }?redirectTo=${ encodeURIComponent(
 				finalUrl
@@ -40,8 +64,9 @@ describe( 'addUrlToPendingPageRedirect', () => {
 	it( 'returns a order ID placeholder when no order ID is provided', () => {
 		const finalUrl = '/foo/bar/baz';
 		const siteSlug = 'example.com';
-		const orderId = undefined;
-		const actual = addUrlToPendingPageRedirect( finalUrl, siteSlug, orderId, 'absolute' );
+		const actual = addUrlToPendingPageRedirect( finalUrl, {
+			siteSlug,
+		} );
 		expect( actual ).toEqual(
 			`https://wordpress.com/checkout/thank-you/${ siteSlug }/pending/:orderId?redirectTo=${ encodeURIComponent(
 				finalUrl
