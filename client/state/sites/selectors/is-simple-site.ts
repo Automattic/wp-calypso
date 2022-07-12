38a476f5b7f7341a56a334a723d49c0fa140c41b
Emanuele Buccelli <escapemanuele@gmail.com>
2022-07-12 17:27:46 +0200

    Fix is simple site

--- client/state/sites/selectors/is-simple-site.ts
+++ client/state/sites/selectors/is-simple-site.ts
@@ -1,4 +1,5 @@
 import { createSelector } from '@automattic/state-utils';
+import { getSite } from 'calypso/state/sites/selectors';
 import isJetpackSite from 'calypso/state/sites/selectors/is-jetpack-site';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { AppState } from 'calypso/types';
@@ -10,7 +11,9 @@ import { AppState } from 'calypso/types';
  * @returns {?boolean}               Whether the current site is a simple site or not
  */
 export default createSelector(
-	( state: AppState, siteId = getSelectedSiteId( state ) ): boolean | null =>
-		siteId ? ! isJetpackSite( state, siteId ) : false,
+	( state: AppState, siteId = getSelectedSiteId( state ) ): boolean | null => {
+		const site = getSite( state, siteId );
+		return !! site && ! site.is_wpcom_atomic && ! isJetpackSite( state, siteId );
+	},
 	( state: AppState, siteId = getSelectedSiteId( state ) ) => [ isJetpackSite( state, siteId ) ]
 );
