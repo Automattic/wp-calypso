b45bacbcc07ee13745ee0809a9dd8330e257230e
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-07-12 15:23:18 +0200

    Set email list on change

--- packages/subscriber/src/components/add-form/index.tsx
+++ packages/subscriber/src/components/add-form/index.tsx
@@ -1,10 +1,22 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import { Title, SubTitle, NextButton, SkipButton } from '@automattic/onboarding';
 import { TextControl, FormFileUpload } from '@wordpress/components';
-import React, { FunctionComponent } from 'react';
+import React, { FormEvent, FunctionComponent, useState } from 'react';
 import './style.scss';
 
 export const AddSubscriberForm: FunctionComponent = () => {
+	const [ emails, setEmails ] = useState< string[] >( [] );
+
+	function onFormSubmit( e: FormEvent ) {
+		e.preventDefault();
+	}
+
+	function setEmail( value: string, index: number ) {
+		const _emails = Array.from( emails );
+		_emails[ index ] = value;
+		setEmails( _emails );
+	}
+
 	return (
 		<div className={ 'add-subscriber' }>
 			<div className={ 'add-subscriber__title-container' }>
@@ -13,27 +25,21 @@ export const AddSubscriberForm: FunctionComponent = () => {
 				<SubTitle>You can invite some people to your list.</SubTitle>
 			</div>
 			<div className={ 'add-subscriber__form--container' }>
-				<form>
+				<form onSubmit={ onFormSubmit }>
 					<TextControl
 						placeholder={ 'sibling@email.com' }
-						value={ '' }
-						onChange={ () => {
-							/* on value change */
-						} }
+						value={ emails[ 0 ] || '' }
+						onChange={ ( value ) => setEmail( value, 0 ) }
 					/>
 					<TextControl
 						placeholder={ 'parents@email.com' }
-						value={ '' }
-						onChange={ () => {
-							/* on value change */
-						} }
+						value={ emails[ 1 ] || '' }
+						onChange={ ( value ) => setEmail( value, 1 ) }
 					/>
 					<TextControl
 						placeholder={ 'friend@email.com' }
-						value={ '' }
-						onChange={ () => {
-							/* on value change */
-						} }
+						value={ emails[ 2 ] || '' }
+						onChange={ ( value ) => setEmail( value, 2 ) }
 					/>
 
 					<label>
