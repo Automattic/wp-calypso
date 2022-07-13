e8fad0fc3477c1d7ec7d80bc049a57966243a6a3
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2022-07-13 15:12:56 -0300

    Add story

--- packages/search/src/search.stories.jsx
+++ packages/search/src/search.stories.jsx
@@ -1,4 +1,5 @@
 import { action } from '@storybook/addon-actions';
+import { Icon } from '@wordpress/components';
 import Search from './search';
 
 export default { title: 'Search', component: Search };
@@ -65,3 +66,33 @@ export const WithOverlayStyling = () => {
 };
 
 export const WithDefaultValue = () => <BoxedSearch defaultValue="a default search value" />;
+
+export const WithCustomSearchIcon = () => {
+	const customIcon = (
+		<svg
+			width="20"
+			height="20"
+			viewBox="0 0 20 20"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			style={ { paddingLeft: '16px', paddingRight: '10px' } }
+		>
+			<path
+				d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z"
+				stroke="#8C8F94"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+			<path
+				d="M17.5 17.5L13.875 13.875"
+				stroke="#8C8F94"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	);
+
+	return <BoxedSearch searchIcon={ <Icon icon={ customIcon } /> } />;
+};
