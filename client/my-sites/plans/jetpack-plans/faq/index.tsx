b930773fee123edb2c97588fc4428b6be591da2b
CodeyGuyDylan <dylan.munson@automattic.com>
2022-07-13 15:33:14 -0700

    Add or remove hashes when FAQ's are opened or closed
    We want to be able to link to FAQ's on the jetpack-connect pricing page and have them be open when linked to. Under the circumstance that somebody were to go to the link with a hash already in the URL, and then click a link that leads to the same FAQ, the FAQ would not be opened since no change in hash was detected. To prevent this, these changes remove the hash anytime the FAQ is closed, so if it were to be linked to again, it would then detect a hash changed and open up the FAQ. The hash's are also added to the URL when the FAQ is opened for user convenience if they were to want to send that link to somebody else with the FAQ already open

--- client/my-sites/plans/jetpack-plans/faq/index.tsx
+++ client/my-sites/plans/jetpack-plans/faq/index.tsx
@@ -1,11 +1,11 @@
 import { useTranslate } from 'i18n-calypso';
 import { useCallback } from 'react';
-import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import FoldableFAQ from 'calypso/components/foldable-faq';
 import { getAgenciesLink, getHelpLink } from 'calypso/my-sites/plans-features-main/jetpack-faq';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import getSelectedSiteId from 'calypso/state/ui/selectors/get-selected-site-id';
+import type { FC } from 'react';
 
 import './style.scss';
 
@@ -22,22 +22,38 @@ const jetpackGettingStartedLink = () => {
 	);
 };
 
-const JetpackFAQ: React.FC = () => {
+const JetpackFAQ: FC = () => {
 	const translate = useTranslate();
 	const dispatch = useDispatch();
 	const siteId = useSelector( getSelectedSiteId );
 
 	const onFaqToggle = useCallback(
-		( faqArgs: { id: string; isExpanded: boolean; height: number } ) => {
-			const { id, isExpanded } = faqArgs;
+		( faqArgs: { id: string; buttonId: string; isExpanded: boolean; height: number } ) => {
+			const { id, buttonId, isExpanded } = faqArgs;
 			const tracksArgs = {
 				site_id: siteId,
 				faq_id: id,
 			};
+
+			const removeHash = () => {
+				history.replaceState( '', document.title, location.pathname + location.search );
+			};
+
+			// Add expanded FAQ buttonId to the URL hash
+			const addHash = () => {
+				history.replaceState(
+					'',
+					document.title,
+					location.pathname + location.search + `#${ buttonId }`
+				);
+			};
+
 			if ( isExpanded ) {
+				addHash();
 				// FAQ opened
 				dispatch( recordTracksEvent( 'calypso_plans_faq_open', tracksArgs ) );
 			} else {
+				removeHash();
 				// FAQ closed
 				dispatch( recordTracksEvent( 'calypso_plans_faq_closed', tracksArgs ) );
 			}
@@ -51,9 +67,10 @@ const JetpackFAQ: React.FC = () => {
 			<section className="jetpack-faq">
 				<h2 className="jetpack-faq__heading">{ translate( 'Frequently Asked Questions' ) }</h2>
 				<FoldableFAQ
-					id="priority-support"
+					id="getting-started"
 					question={ translate( 'How do I start using Jetpack on my website?' ) }
 					onToggle={ onFaqToggle }
+					className="jetpack-faq__section"
 				>
 					{ translate(
 						'Learn everything you need to know about getting started with Jetpack {{gettingStartedLink}}here{{/gettingStartedLink}}.',
@@ -63,9 +80,20 @@ const JetpackFAQ: React.FC = () => {
 					) }
 				</FoldableFAQ>
 				<FoldableFAQ
+					id="backup-storage-limits"
+					question={ translate( 'How do backup storage limits work?' ) }
+					onToggle={ onFaqToggle }
+					className="jetpack-faq__section"
+				>
+					{ translate(
+						"If your site's Backup storage limit is reached, your older backups will be deleted. Depending on the size of your site and your site's Backup storage limit, your site's backup retention period may be reduced down to 7 days of your most recent backups. You will still be able to restore existing backups, but new site updates will not be backed up until you free up storage or upgrade your storage limit here"
+					) }
+				</FoldableFAQ>
+				<FoldableFAQ
 					id="priority-support"
 					question={ translate( 'Is priority support included in all plans?' ) }
 					onToggle={ onFaqToggle }
+					className="jetpack-faq__section"
 				>
 					{ translate(
 						'Yes, our expert Happiness Engineers provide priority support to all customers with paid plans and services! Have a question or a problem? Just {{helpLink}}contact support{{/helpLink}} and we’ll get back to you in no time.',
@@ -78,6 +106,7 @@ const JetpackFAQ: React.FC = () => {
 					id="cancellation-policy"
 					question={ translate( 'What is your cancellation policy?' ) }
 					onToggle={ onFaqToggle }
+					className="jetpack-faq__section"
 				>
 					{ translate(
 						'If you are dissatisfied for any reason, we offer full refunds within %(annualDays)d days for yearly plans, and within %(monthlyDays)d days for monthly plans. If you have a question about our paid plans, {{helpLink}}please let us know{{/helpLink}}!',
@@ -91,6 +120,7 @@ const JetpackFAQ: React.FC = () => {
 					id="volume-discount"
 					question={ translate( 'Do you have any discounts for multiple sites?' ) }
 					onToggle={ onFaqToggle }
+					className="jetpack-faq__section"
 				>
 					{ translate(
 						'Anyone with at least five websites can join our licensing platform and enjoy a 25% discount across all Jetpack products! You can learn more about our {{agenciesLink}}licensing platform and agency program here{{/agenciesLink}}.',
@@ -103,6 +133,7 @@ const JetpackFAQ: React.FC = () => {
 					id="wpcom-account"
 					question={ translate( 'Why do I need a WordPress.com account?' ) }
 					onToggle={ onFaqToggle }
+					className="jetpack-faq__section"
 				>
 					{ translate(
 						'Many of Jetpack’s core features make use of the WordPress.com cloud. In order to make sure everything' +
@@ -114,6 +145,7 @@ const JetpackFAQ: React.FC = () => {
 					id="hosting-requirements"
 					question={ translate( 'What are the hosting requirements?' ) }
 					onToggle={ onFaqToggle }
+					className="jetpack-faq__section"
 				>
 					{ translate(
 						'You should be running the latest version of WordPress and a publicly-accessible site with XML-RPC enabled.'
@@ -123,6 +155,7 @@ const JetpackFAQ: React.FC = () => {
 					id="multisite-network"
 					question={ translate( 'Does this work with a multisite network?' ) }
 					onToggle={ onFaqToggle }
+					className="jetpack-faq__section"
 				>
 					{ translate(
 						'Jetpack’s free features are compatible with WordPress Multisite networks. Paid features' +
@@ -134,6 +167,7 @@ const JetpackFAQ: React.FC = () => {
 					id="more-questions"
 					question={ translate( 'Have more questions?' ) }
 					onToggle={ onFaqToggle }
+					className="jetpack-faq__section"
 				>
 					{ translate(
 						'No problem! Feel free to {{helpLink}}get in touch{{/helpLink}} with our Happiness Engineers.',
