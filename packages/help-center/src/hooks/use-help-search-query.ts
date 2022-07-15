491c9d791d3eb7b3f3303f9d6a29a211e7939ce8
Omar Alshaker <omar@omaralshaker.com>
2022-07-15 12:08:57 +0200

    Send locale to search API

--- packages/help-center/src/hooks/use-help-search-query.ts
+++ packages/help-center/src/hooks/use-help-search-query.ts
@@ -9,6 +9,7 @@ interface APIFetchOptions {
 
 export const useHelpSearchQuery = (
 	search: string,
+	locale = 'en',
 	queryOptions: Record< string, unknown > = {}
 ) => {
 	return useQuery< { wordpress_support_links: LinksForSection[] } >(
@@ -16,7 +17,7 @@ export const useHelpSearchQuery = (
 		() =>
 			apiFetch( {
 				global: true,
-				path: `/wpcom/v2/help-center/search?query=${ search }`,
+				path: `/wpcom/v2/help-center/search?query=${ search }&locale=${ locale }`,
 			} as APIFetchOptions ),
 		{
 			enabled: !! search,
