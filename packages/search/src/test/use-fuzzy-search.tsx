d68ee4e7a87fcb81f5346cc768e3cc883239c87c
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2022-07-15 14:17:13 -0300

    Remove React import on test

--- packages/search/src/test/use-fuzzy-search.tsx
+++ packages/search/src/test/use-fuzzy-search.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import React from 'react';
 import Search, { useFuzzySearch } from '..';
 import type { UseFuzzySearchOptions } from '../use-fuzzy-search';
 
