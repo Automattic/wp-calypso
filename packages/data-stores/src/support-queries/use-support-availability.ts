942b932af30bb104091c55bd6fd35c184d96cb8b
Emanuele Buccelli <escapemanuele@gmail.com>
2022-07-19 12:14:57 +0200

    Remove support changes

--- packages/data-stores/src/support-queries/use-support-availability.ts
+++ packages/data-stores/src/support-queries/use-support-availability.ts
@@ -8,20 +8,18 @@ type ResponseType< T extends 'CHAT' | 'OTHER' > = T extends 'CHAT'
 
 export function useSupportAvailability< SUPPORT_TYPE extends 'CHAT' | 'OTHER' >(
 	supportType: SUPPORT_TYPE,
-	isSimpleSite = true
+	enabled = true
 ) {
 	return useQuery< ResponseType< SUPPORT_TYPE >, typeof Error >(
 		supportType === 'OTHER' ? 'otherSupportAvailability' : 'chatSupportAvailability',
 		async () =>
 			await wpcomRequest( {
-				path: isSimpleSite
-					? `help/eligibility/${ supportType === 'OTHER' ? 'all' : 'chat' }/mine`
-					: `help-center/support-availability/${ supportType === 'OTHER' ? 'all' : 'chat' }`,
+				path: `help/eligibility/${ supportType === 'OTHER' ? 'all' : 'chat' }/mine`,
 				apiNamespace: 'wpcom/v2/',
 				apiVersion: '2',
 			} ),
 		{
-			enabled: isSimpleSite,
+			enabled,
 			refetchOnWindowFocus: false,
 			keepPreviousData: true,
 		}
