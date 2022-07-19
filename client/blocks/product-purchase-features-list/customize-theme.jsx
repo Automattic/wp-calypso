309fa215f06dded2115f5bbf69a2e201f5700eb7
Anthony Grullon <agrullon95@gmail.com>
2022-07-19 16:25:16 -0400

    Plans page blockEditorSetting removed

--- client/blocks/product-purchase-features-list/customize-theme.jsx
+++ client/blocks/product-purchase-features-list/customize-theme.jsx
@@ -3,13 +3,13 @@ import { useSelector } from 'react-redux';
 import customizeImage from 'calypso/assets/images/illustrations/dashboard.svg';
 import QueryActiveTheme from 'calypso/components/data/query-active-theme';
 import PurchaseDetail from 'calypso/components/purchase-detail';
-import withBlockEditorSettings from 'calypso/data/block-editor/with-block-editor-settings';
+import withActiveTheme from 'calypso/data/themes/with-active-theme';
 import getCustomizeUrl from 'calypso/state/selectors/get-customize-url';
 import { getActiveTheme } from 'calypso/state/themes/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
-const CustomizeTheme = ( { translate, blockEditorSettings, areBlockEditorSettingsLoading } ) => {
-	const isFSEActive = blockEditorSettings?.is_fse_active;
+const CustomizeTheme = ( { translate, activeThemeData, isActiveThemeDataLoading } ) => {
+	const isFSEActive = activeThemeData?.[ 0 ]?.theme_supports[ 'block-templates' ] ?? false;
 	const siteId = useSelector( getSelectedSiteId );
 	const currentThemeId = useSelector( ( state ) => getActiveTheme( state, siteId ) );
 	const customizeLink = useSelector( ( state ) =>
@@ -23,20 +23,18 @@ const CustomizeTheme = ( { translate, blockEditorSettings, areBlockEditorSetting
 		<div className="product-purchase-features-list__item">
 			<QueryActiveTheme siteId={ siteId } />
 			<PurchaseDetail
-				isSubmitting={ areBlockEditorSettingsLoading }
+				isSubmitting={ isActiveThemeDataLoading }
 				icon={ <img alt="" src={ customizeImage } /> }
 				title={ translate( 'Advanced customization' ) }
 				description={ translate(
 					"Change your site's appearance in a few clicks, with an expanded " +
 						'selection of fonts and colors.'
 				) }
-				buttonText={
-					areBlockEditorSettingsLoading ? translate( 'Loading…' ) : customizeThemeButtonText
-				}
+				buttonText={ isActiveThemeDataLoading ? translate( 'Loading…' ) : customizeThemeButtonText }
 				href={ customizeLink }
 			/>
 		</div>
 	);
 };
 
-export default withBlockEditorSettings( localize( CustomizeTheme ) );
+export default withActiveTheme( localize( CustomizeTheme ) );
