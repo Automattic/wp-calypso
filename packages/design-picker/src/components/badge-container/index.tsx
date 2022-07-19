59c3dcdc16d721fba95ca7462783571ebf9bdb34
Andres Blanco <andresblanco@gmail.com>
2022-07-19 15:46:13 -0300

    add popover to fse badge

--- packages/design-picker/src/components/badge-container/index.tsx
+++ packages/design-picker/src/components/badge-container/index.tsx
@@ -1,5 +1,7 @@
+import { Popover } from '@automattic/components';
+import { useI18n } from '@wordpress/react-i18n';
+import { FunctionComponent, useRef, useState } from 'react';
 import PremiumBadge from '../premium-badge';
-import type { FunctionComponent } from 'react';
 
 import './style.scss';
 
@@ -23,6 +25,12 @@ const BadgeContainer: FunctionComponent< Props > = ( {
 		}
 	}
 
+	const divRef = useRef( null );
+	const [ isPopoverVisible, setIsPopoverVisible ] = useState( false );
+
+	const { __ } = useI18n();
+	const tooltipText = __( 'This theme is full site editing capable', __i18n_text_domain__ );
+
 	return (
 		<div className="badge-container">
 			<svg
@@ -31,6 +39,9 @@ const BadgeContainer: FunctionComponent< Props > = ( {
 				viewBox="0 0 16 16"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
+				ref={ divRef }
+				onMouseEnter={ () => setIsPopoverVisible( true ) }
+				onMouseLeave={ () => setIsPopoverVisible( false ) }
 			>
 				<path
 					d="M12.6667 2H3.33333C2.59695 2 2 2.59695 2 3.33333V12.6667C2 13.403 2.59695 14 3.33333 14H12.6667C13.403 14 14 13.403 14 12.6667V3.33333C14 2.59695 13.403 2 12.6667 2Z"
@@ -54,6 +65,14 @@ const BadgeContainer: FunctionComponent< Props > = ( {
 					strokeLinejoin="round"
 				/>
 			</svg>
+			<Popover
+				className="badge-container__fse-popover"
+				context={ divRef.current }
+				isVisible={ isPopoverVisible }
+				position="top left"
+			>
+				{ tooltipText }
+			</Popover>
 			{ getBadge() }
 		</div>
 	);
