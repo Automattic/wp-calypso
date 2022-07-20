3c7edc92b4dd02247e3999467c1c57a7c31a749d
Yashwin <yashwinpoojary@gmail.com>
2022-07-20 13:53:09 +0530

    Add sidebar navigation to the Plugins page

--- client/jetpack-cloud/sections/plugin-management/plugins-overview/index.tsx
+++ client/jetpack-cloud/sections/plugin-management/plugins-overview/index.tsx
@@ -1,8 +1,10 @@
 import config from '@automattic/calypso-config';
+import { useTranslate } from 'i18n-calypso';
 import page from 'page';
 import { ReactElement, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import JetpackLogo from 'calypso/components/jetpack-logo';
+import SidebarNavigation from 'calypso/components/sidebar-navigation';
 import SelectPartnerKey from 'calypso/jetpack-cloud/sections/partner-portal/primary/select-partner-key';
 import PluginsMain from 'calypso/my-sites/plugins/main';
 import {
@@ -12,7 +14,7 @@ import {
 	isAgencyUser,
 } from 'calypso/state/partner-portal/partner/selectors';
 
-import '../../../style.scss';
+import './style.scss';
 
 interface Props {
 	filter: string;
@@ -20,6 +22,8 @@ interface Props {
 }
 
 export default function PluginOverview( { filter, search }: Props ): ReactElement {
+	const translate = useTranslate();
+
 	const hasFetched = useSelector( hasFetchedPartner );
 	const isFetching = useSelector( isFetchingPartner );
 	const hasActiveKey = useSelector( hasActivePartnerKey );
@@ -40,7 +44,12 @@ export default function PluginOverview( { filter, search }: Props ): ReactElemen
 	}
 
 	if ( hasFetched ) {
-		return <PluginsMain isJetpackCloud filter={ filter } search={ search } />;
+		return (
+			<div className="plugins-overview__container">
+				<SidebarNavigation sectionTitle={ translate( 'Plugins' ) } />
+				<PluginsMain isJetpackCloud filter={ filter } search={ search } />
+			</div>
+		);
 	}
 
 	return (
