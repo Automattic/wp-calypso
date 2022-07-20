7f68fbbab2b6bc713e2178a3c7f1f9dc5a2e45ce
Edwin Takahashi <egao@outlook.com>
2022-07-20 13:50:16 -0700

    Spec: - split out the steps for better granularity - add the redirect portion to the test

--- test/e2e/specs/authentication/authentication__totp.ts
+++ test/e2e/specs/authentication/authentication__totp.ts
@@ -2,21 +2,54 @@
  * @group authentication
  */
 
-import { DataHelper, TestAccount } from '@automattic/calypso-e2e';
+import { DataHelper, LoginPage, SecretsManager, TOTPClient } from '@automattic/calypso-e2e';
 import { Page, Browser } from 'playwright';
 
 declare const browser: Browser;
 
 describe( DataHelper.createSuiteTitle( 'Authentication: TOTP' ), function () {
+	const credentials = SecretsManager.secrets.testAccounts.totpUser;
 	let page: Page;
-	let testAccount: TestAccount;
+	let loginPage: LoginPage;
 
 	beforeAll( async () => {
 		page = await browser.newPage();
 	} );
 
-	it( 'Authenticate as user with TOTP 2FA code', async function () {
-		testAccount = new TestAccount( 'totpUser' );
-		await testAccount.authenticate( page );
+	// This spec intentionally manually calls individual
+	// methods from LoginPage to separate out the steps that
+	// are bundled together in the TestAccount class.
+	it( 'Navigate to Login page', async function () {
+		await page.goto(
+			DataHelper.getCalypsoURL(
+				'log-in?site=e2eflowtestingtotp.wordpress.com&redirect_to=%2Fsettings%2Fgeneral'
+			)
+		);
+	} );
+
+	it( 'Enter username', async function () {
+		loginPage = new LoginPage( page );
+		await loginPage.fillUsername( credentials.username );
+		await loginPage.clickSubmit();
+	} );
+
+	it( 'Enter password', async function () {
+		await loginPage.fillPassword( credentials.password );
+	} );
+
+	it( 'Submit form', async function () {
+		await Promise.all( [ page.waitForNavigation(), loginPage.clickSubmit() ] );
+	} );
+
+	it( 'Enter 2FA code', async function () {
+		const totpClient = new TOTPClient( credentials.totpKey as string );
+		const code = totpClient.getToken();
+
+		await loginPage.submitVerificationCode( code );
+	} );
+
+	it( 'User is redirected to Settings > General Settings', async function () {
+		const redirectedURL = DataHelper.getCalypsoURL( '/settings/general' );
+		await page.waitForURL( `${ redirectedURL }/${ credentials.primarySite }` );
 	} );
 } );
