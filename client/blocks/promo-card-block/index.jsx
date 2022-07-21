dbde75fcc9a1bb9d63c14e8c4fa2f999f972c5e6
Rolando Perez <me@rolilink.com>
2022-07-21 19:57:49 +0200

    Loads the correct endpoint and checks for fetch status

--- client/blocks/promo-card-block/index.jsx
+++ client/blocks/promo-card-block/index.jsx
@@ -2,18 +2,20 @@ import { recordTracksEvent } from '@automattic/calypso-analytics';
 import page from 'page';
 import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import QueryProductsList from 'calypso/components/data/query-products-list';
+import QueryJetpackPlugins from 'calypso/components/data/query-jetpack-plugins';
 import PromoCard from 'calypso/components/promo-card';
 import TrackComponentView from 'calypso/lib/analytics/track-component-view';
-import { getPluginOnSite } from 'calypso/state/plugins/installed/selectors';
+import { getPluginOnSite, isRequestingForSites } from 'calypso/state/plugins/installed/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 const PromoCardBlock = ( props ) => {
 	const { productSlug, clickEvent, href, impressionEvent } = props;
+
 	const selectedSiteId = useSelector( ( state ) => getSelectedSiteId( state ) );
 	const selectedPlugin = useSelector( ( state ) =>
 		getPluginOnSite( state, selectedSiteId, productSlug )
 	);
+	const isFetching = useSelector( ( state ) => isRequestingForSites( state, [ selectedSiteId ] ) );
 
 	const onClick = useCallback( () => {
 		recordTracksEvent( clickEvent );
@@ -22,9 +24,9 @@ const PromoCardBlock = ( props ) => {
 
 	return (
 		<>
-			<QueryProductsList />
+			<QueryJetpackPlugins siteIds={ [ selectedSiteId ] } />
 			<TrackComponentView eventName={ impressionEvent } />
-			{ selectedPlugin ? <div></div> : <PromoCard { ...{ ...props, onClick } } /> }
+			{ selectedPlugin || isFetching ? <div></div> : <PromoCard { ...{ ...props, onClick } } /> }
 		</>
 	);
 };
