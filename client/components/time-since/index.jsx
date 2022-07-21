7a2e61292f773745c9e74ca6ecdaf342f390e408
Wojtek Naruniec <wojtek@naruniec.me>
2022-07-21 09:16:37 +0200

    Add JSDoc type annotations for TimeSince props to improve developer experience

--- client/components/time-since/index.jsx
+++ client/components/time-since/index.jsx
@@ -2,6 +2,16 @@ import { useMemo } from 'react';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import { useHumanDate } from 'calypso/lib/human-date';
 
+/**
+ * @typedef {Object}   TimeSinceProps
+ * @property {string}  date       The timestamp to render
+ * @property {string=} dateFormat Moment format string for the tooltip, default = 'll'
+ * @property {string=} className  Forwarded to underlying <time> element
+ */
+
+/**
+ * @param {TimeSinceProps} props
+ */
 function TimeSince( { className, date, dateFormat = 'll' } ) {
 	const moment = useLocalizedMoment();
 	const fullDate = useMemo( () => moment( date ).format( 'llll' ), [ moment, date ] );
