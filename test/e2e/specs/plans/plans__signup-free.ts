0cfd48535594bc323710c012715ba38b5e063dff
Edwin Takahashi <egao@outlook.com>
2022-07-21 13:08:15 -0700

    Plans: Free - revise the `afterAll` hook to use the `apiCloseAccount` reusable block. - revise steps where required to make it work with the legacy plans. - re-enable the spec for calypso-release group.

--- test/e2e/specs/plans/plans__signup-free.ts
+++ test/e2e/specs/plans/plans__signup-free.ts
@@ -1,4 +1,5 @@
 /**
+ * @group calypso-release
  */
 
 import {
@@ -12,6 +13,7 @@ import {
 	LoginPage,
 } from '@automattic/calypso-e2e';
 import { Page, Browser } from 'playwright';
+import { apiCloseAccount } from '../shared';
 import type { NewUserResponse } from '@automattic/calypso-e2e';
 
 declare const browser: Browser;
@@ -79,11 +81,6 @@ describe(
 				const plan = await sidebarComponent.getCurrentPlanName();
 				expect( plan ).toBe( 'Free' );
 			} );
-
-			it( 'Navigate to Upgrades > Plans', async function () {
-				sidebarComponent = new SidebarComponent( page );
-				await sidebarComponent.navigate( 'Upgrade', 'Plans' );
-			} );
 		} );
 
 		afterAll( async function () {
@@ -96,18 +93,11 @@ describe(
 				userDetails.body.bearer_token
 			);
 
-			const response = await restAPIClient.closeAccount( {
+			await apiCloseAccount( restAPIClient, {
 				userID: userDetails.body.user_id,
-				username: testUser.username,
+				username: userDetails.body.username,
 				email: testUser.email,
 			} );
-
-			if ( response.success !== true ) {
-				console.warn( `Failed to delete user ID ${ userDetails.body.user_id }` );
-			} else {
-				console.log( `Successfully deleted user ID ${ userDetails.body.user_id }` );
-			}
-			return response;
 		} );
 	}
 );
