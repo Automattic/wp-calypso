fafa167edcabb78f90b96242b15045341ca4e177
Wojtek Naruniec <wojtek@naruniec.me>
2022-07-21 18:13:57 +0200

    Refactor TimeSince component to TypeScript

--- client/components/time-since/index.tsx
+++ client/components/time-since/index.tsx
@@ -2,17 +2,13 @@ import { useMemo } from 'react';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import { useHumanDate } from 'calypso/lib/human-date';
 
-/**
- * @typedef {Object}   TimeSinceProps
- * @property {string}  date       The timestamp to render
- * @property {string=} dateFormat Moment format string for the tooltip, default = 'll'
- * @property {string=} className  Forwarded to underlying <time> element
- */
+interface TimeSinceProps {
+	date: string;
+	dateFormat?: string;
+	className?: string;
+}
 
-/**
- * @param {TimeSinceProps} props
- */
-function TimeSince( { className, date, dateFormat = 'll' } ) {
+function TimeSince( { className, date, dateFormat = 'll' }: TimeSinceProps ) {
 	const moment = useLocalizedMoment();
 	const fullDate = useMemo( () => moment( date ).format( 'llll' ), [ moment, date ] );
 	const humanDate = useHumanDate( date, dateFormat );
