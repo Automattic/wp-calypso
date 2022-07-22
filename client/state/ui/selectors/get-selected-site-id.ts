dd33856345ae5cc6d264feeee2b89eae75af2b82
Jeremy Yip <jeremy.yip@automattic.com>
2022-07-22 07:23:59 -0700

    Restore jsdoc description for selected site id selector

--- client/state/ui/selectors/get-selected-site-id.ts
+++ client/state/ui/selectors/get-selected-site-id.ts
@@ -1,6 +1,12 @@
 import { IAppState } from 'calypso/state/types';
 import 'calypso/state/ui/init';
 
+/**
+ * Returns the currently selected site ID.
+ *
+ * @param  {object}  state Global state tree
+ * @returns {?number}       Selected site ID
+ */
 export default function getSelectedSiteId( state: IAppState ): number | null {
 	return state.ui.selectedSiteId;
 }
