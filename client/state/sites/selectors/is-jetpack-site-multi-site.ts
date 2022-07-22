cca68468634778d93b95eecc2742429f113d11e3
Jeremy Yip <jeremy.yip@automattic.com>
2022-07-22 07:20:24 -0700

    Restore jsdoc description for jetpack multi site selector

--- client/state/sites/selectors/is-jetpack-site-multi-site.ts
+++ client/state/sites/selectors/is-jetpack-site-multi-site.ts
@@ -2,6 +2,14 @@ import getRawSite from 'calypso/state/selectors/get-raw-site';
 import { IAppState } from 'calypso/state/types';
 import isJetpackSite from './is-jetpack-site';
 
+/**
+ * Determines if the Jetpack site is part of multi-site.
+ * Returns null if the site is not known or is not a Jetpack site.
+ *
+ * @param  {object}   state  Global state tree
+ * @param  {number}   siteId Site ID
+ * @returns {?boolean}        true if the site is multi-site
+ */
 export default function isJetpackSiteMultiSite( state: IAppState, siteId: number | null ) {
 	const site = getRawSite( state, siteId );
 
