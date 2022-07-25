e46fa22c0c9cd29455e5d605c29e85735c2c78c5
heavyweight <heavyweight@users.noreply.github.com>
2022-07-25 14:59:15 +0200

    Checkout: show new HelpCenter independent of locale (#65934)

--- packages/help-center/src/utils.ts
+++ packages/help-center/src/utils.ts
@@ -2,10 +2,10 @@ import config from '@automattic/calypso-config';
 
 // function that tells us if we want to show the Help Center to the user, given that we're showing it to
 // only a certain percentage of users.
-export function shouldShowHelpCenterToUser( userId: number, locale: string ) {
-	const currentSegment = 100; //percentage of users that will see the Help Center, not the FAB
+export function shouldShowHelpCenterToUser( userId: number ) {
+	const currentSegment = 10; //percentage of users that will see the Help Center, not the FAB
 	const userSegment = userId % 100;
-	return userSegment < currentSegment && locale === 'en';
+	return userSegment < currentSegment;
 }
 
 export function shouldTargetWpcom( isSimpleSite: boolean ) {
