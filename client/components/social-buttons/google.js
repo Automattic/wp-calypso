95d11ea1a2b42e531e7ecb2039e1361a9755dbbb
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-07-25 12:49:47 -0700

    Manually revert 5532501c42629a5c34dd1d76679fc7677250f67f

--- client/components/social-buttons/google.js
+++ client/components/social-buttons/google.js
@@ -103,10 +103,6 @@ class GoogleSocialButton extends Component {
 		} );
 
 		this.setState( { isDisabled: false } );
-
-		if ( this.props.launchServiceOnLoad ) {
-			this.client.requestCode();
-		}
 	}
 
 	async loadGoogleIdentityServicesAPI() {
@@ -365,7 +361,6 @@ export default connect(
 	( state ) => ( {
 		isFormDisabled: isFormDisabled( state ),
 		authCodeFromRedirect: getInitialQueryArguments( state ).code,
-		launchServiceOnLoad: getInitialQueryArguments( state ).launch_service === 'google',
 	} ),
 	{
 		recordTracksEvent,
