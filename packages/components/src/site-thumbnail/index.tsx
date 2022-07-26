a8248c4af1cd3df7763d090101058d5c9ebc32e8
sejas <antonio@sejas.es>
2022-07-26 18:52:07 +0100

    Sites Dashboard: simplify useMshotsImg hook

--- packages/components/src/site-thumbnail/index.tsx
+++ packages/components/src/site-thumbnail/index.tsx
@@ -23,27 +23,26 @@ export const SiteThumbnail = ( {
 	backgroundColor,
 	children,
 	alt,
-	mShotsUrl,
+	mShotsUrl = '',
 	size = 'small',
 	mshotsOption = MSHOTS_OPTION,
 }: Props ) => {
-	const maybeImage = useMshotsImg( mShotsUrl ?? '', mshotsOption );
+	const { src, isLoading, imgRef } = useMshotsImg( mShotsUrl, mshotsOption );
 
-	const src = maybeImage?.src || '';
-	const visible = !! src && mShotsUrl;
-	const textColor = backgroundColor && getTextColorFromBackground( backgroundColor );
+	const color = backgroundColor && getTextColorFromBackground( backgroundColor );
 
 	const className = classnames(
 		'site-thumbnail',
-		visible ? 'site-thumbnail-visible' : '',
+		! isLoading ? 'site-thumbnail-visible' : '',
 		`site-thumbnail__size-${ size }`
 	);
 
 	const loader = mShotsUrl ? 'site-thumbnail-loader' : '';
 
 	return (
-		<div className={ className } style={ { backgroundColor, color: textColor } }>
-			{ ! visible ? <div className={ loader }>{ children }</div> : <img src={ src } alt={ alt } /> }
+		<div className={ className } style={ { backgroundColor, color } }>
+			{ isLoading && <div className={ loader }>{ children }</div> }
+			{ src && <img ref={ imgRef } src={ src } alt={ alt } loading="lazy" /> }
 		</div>
 	);
 };
