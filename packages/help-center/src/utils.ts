6ae5c33cd43b21ba6a840910493f9f36bb47769a
Omar Alshaker <omar@omaralshaker.com>
2022-07-26 15:59:06 +0200

    Use wpcom-proxy-request logic to determine whether to use wpcomRequest

--- packages/help-center/src/utils.ts
+++ packages/help-center/src/utils.ts
@@ -1,5 +1,24 @@
-import config from '@automattic/calypso-config';
-
+// list of valid origins for wpcom requests.
+// taken from wpcom-proxy-request (rest-proxy/provider-v2.0.js)
+const wpcomAllowedOrigins = [
+	'https://wordpress.com',
+	'https://cloud.jetpack.com',
+	'http://wpcalypso.wordpress.com', // for running docker on dev instances
+	'http://widgets.wp.com',
+	'https://widgets.wp.com',
+	'https://dev-mc.a8c.com',
+	'https://mc.a8c.com',
+	'https://dserve.a8c.com',
+	'http://calypso.localhost:3000',
+	'https://calypso.localhost:3000',
+	'http://jetpack.cloud.localhost:3000',
+	'https://jetpack.cloud.localhost:3000',
+	'http://calypso.localhost:3001',
+	'https://calypso.localhost:3001',
+	'https://calypso.live',
+	'http://127.0.0.1:41050',
+	'http://send.linguine.localhost:3000',
+];
 // function that tells us if we want to show the Help Center to the user, given that we're showing it to
 // only a certain percentage of users.
 export function shouldShowHelpCenterToUser( userId: number ) {
@@ -8,6 +27,23 @@ export function shouldShowHelpCenterToUser( userId: number ) {
 	return userSegment < currentSegment;
 }
 
+/**
+ * Shelved from rest-proxy/provider-v2.0.js.
+ * This returns true for all WPCOM origins except Atomic sites.
+ *
+ * @param origin
+ * @returns
+ */
+export function iAllowedOrigin( origin: string ) {
+	// sites in the allow-list and some subdomains of "calypso.live" and "wordpress.com"
+	// are allowed without further check
+	return (
+		wpcomAllowedOrigins.includes( origin ) ||
+		origin.match( /^https:\/\/[a-z0-9-]+\.calypso\.live$/ ) ||
+		origin.match( /^https:\/\/([a-z0-9-]+\.)+wordpress\.com$/ )
+	);
+}
+
 export function shouldTargetWpcom( isSimpleSite: boolean ) {
-	return isSimpleSite || window.location.hostname === config( 'hostname' );
+	return isSimpleSite || iAllowedOrigin( window.location.origin );
 }
