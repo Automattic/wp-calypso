49a20ec6c6ab836e2786b6a8b8b070e5da27bbd4
Atanas Angelov <hi@atanas.dev>
2022-07-26 19:51:52 +0300

    Licensing Portal: Pass new setup intent ids to the payment method creation api

--- client/jetpack-cloud/sections/partner-portal/payment-methods/assignment-processor-functions.ts
+++ client/jetpack-cloud/sections/partner-portal/payment-methods/assignment-processor-functions.ts
@@ -63,6 +63,7 @@ export async function assignNewCardProcessor(
 		const result = await saveCreditCard( {
 			token,
 			useAsPrimaryPaymentMethod: Boolean( useAsPrimaryPaymentMethod ),
+			stripeSetupIntentId,
 		} );
 
 		return makeSuccessResponse( result );
