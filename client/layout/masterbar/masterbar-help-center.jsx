295dedb2df78abec44a46637237cfab510046525
Emanuele Buccelli <escapemanuele@gmail.com>
2022-07-27 13:32:12 +0200

    Return to js

---
+++ client/layout/masterbar/masterbar-help-center.jsx
@@ -0,0 +1,40 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
+import { useHasSeenWhatsNewModalQuery } from '@automattic/data-stores';
+import { HelpIcon } from '@automattic/help-center';
+import classnames from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
+import { setHelpCenterVisible } from 'calypso/state/ui/actions';
+import { getSectionName } from 'calypso/state/ui/selectors';
+import isHelpCenterVisible from 'calypso/state/ui/selectors/help-center-is-visible';
+import Item from './item';
+
+const MasterbarHelpCenter = ( { siteId } ) => {
+	const { isLoading, data } = useHasSeenWhatsNewModalQuery( siteId );
+	const sectionName = useSelector( getSectionName );
+
+	const newItems = ! isLoading && ! data?.has_seen_whats_new_modal;
+
+	const helpCenterVisible = useSelector( isHelpCenterVisible );
+	const dispatch = useDispatch();
+
+	const handleToggleHelpCenter = () => {
+		recordTracksEvent( `calypso_inlinehelp_${ helpCenterVisible ? 'close' : 'show' }`, {
+			location: 'help-center',
+			section: sectionName,
+		} );
+
+		dispatch( setHelpCenterVisible( ! helpCenterVisible ) );
+	};
+
+	return (
+		<Item
+			onClick={ handleToggleHelpCenter }
+			className={ classnames( 'masterbar__item-help', {
+				'is-active': helpCenterVisible,
+			} ) }
+			icon={ <HelpIcon newItems={ newItems } /> }
+		/>
+	);
+};
+
+export default MasterbarHelpCenter;
