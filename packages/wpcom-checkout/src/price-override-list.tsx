960cd4df4202ab278c5c9be79d31631b27f28272
Payton Swick <payton@foolord.com>
2022-07-28 13:36:15 -0400

    Hard-code price override reasons from code

--- packages/wpcom-checkout/src/price-override-list.tsx
+++ packages/wpcom-checkout/src/price-override-list.tsx
@@ -1,3 +1,4 @@
+import { useTranslate } from 'i18n-calypso';
 import type { ResponseCartProduct, ProductCostOverride } from '@automattic/shopping-cart';
 
 export function PriceOverrideList( { product }: { product: ResponseCartProduct } ) {
@@ -14,5 +15,23 @@ export function PriceOverrideList( { product }: { product: ResponseCartProduct }
 }
 
 function PriceOverride( { override }: { override: ProductCostOverride } ) {
-	return <div>{ override.user_facing_reason }</div>;
+	const translate = useTranslate();
+	const percentage = 100 - ( override.new_price / override.old_price ) * 100;
+	switch ( override.reason ) {
+		case 'upsell_bundle_apply_discount':
+			return (
+				<div>{ translate( 'Prorated discount %(percentage)d%%', { args: { percentage } } ) }</div>
+			);
+		case 'domain volume discount':
+			return (
+				<div>
+					{ translate( 'Multiple domain discount %(percentage)d%%', { args: { percentage } } ) }
+				</div>
+			);
+		case 'apply_sale_coupon':
+			return <div>{ translate( 'Discount %(percentage)d%%', { args: { percentage } } ) }</div>;
+		case 'bundled domain credit':
+			return <div>{ translate( 'Bundled domain' ) }</div>;
+	}
+	return null;
 }
