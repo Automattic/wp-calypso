6f2d6e39ecac6b8e660e7702ad4a68cbb0886dd2
vykes-mac <47489215+vykes-mac@users.noreply.github.com>
2022-07-28 13:23:26 -0500

    Sites Dashboard: Site thumbnail component (#65868)
    * Create site thumbnail component.

---
+++ packages/components/src/site-thumbnail/index.tsx
@@ -0,0 +1,56 @@
+import classnames from 'classnames';
+import { ReactNode } from 'react';
+import './style.scss';
+import { MShotsOptions, useMshotsImg } from './use-mshots-img';
+import { getTextColorFromBackground } from './utils';
+
+const MSHOTS_OPTION: MShotsOptions = {
+	vpw: 1200,
+	vph: 1200,
+	w: 374 * 2,
+};
+
+type Props = {
+	backgroundColor?: string;
+	mShotsUrl?: string;
+	size?: 'small' | 'medium';
+	children?: ReactNode;
+	alt?: string;
+	mshotsOption?: MShotsOptions;
+};
+
+export const SiteThumbnail = ( {
+	backgroundColor,
+	children,
+	alt,
+	mShotsUrl = '',
+	size = 'small',
+	mshotsOption = MSHOTS_OPTION,
+}: Props ) => {
+	const { src, isLoading, isError, imgRef } = useMshotsImg( mShotsUrl, mshotsOption );
+
+	const color = backgroundColor && getTextColorFromBackground( backgroundColor );
+
+	const className = classnames(
+		'site-thumbnail',
+		isLoading ? 'site-thumbnail-loading' : 'site-thumbnail-visible',
+		`site-thumbnail__size-${ size }`
+	);
+
+	const loader = mShotsUrl && ! isError ? 'site-thumbnail-loader' : '';
+
+	return (
+		<div className={ className } style={ { backgroundColor, color } }>
+			{ isLoading && <div className={ loader }>{ children }</div> }
+			{ src && ! isError && (
+				<img
+					className="site-thumbnail__image"
+					ref={ imgRef }
+					src={ src }
+					alt={ alt }
+					loading="lazy"
+				/>
+			) }
+		</div>
+	);
+};
