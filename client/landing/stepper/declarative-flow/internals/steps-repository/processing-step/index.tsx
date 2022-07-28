0179f39274e9efffc1a74b4089c291167f2aea72
bangank36 <bangank36@gmail.com>
2022-07-28 08:24:13 +0700

    - Add hooks for the processing loading message

--- client/landing/stepper/declarative-flow/internals/steps-repository/processing-step/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/processing-step/index.tsx
@@ -1,10 +1,10 @@
-import { Onboard } from '@automattic/data-stores';
 import { StepContainer } from '@automattic/onboarding';
 import { useSelect } from '@wordpress/data';
 import { useI18n } from '@wordpress/react-i18n';
 import { ReactElement, useEffect, useState } from 'react';
 import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
-import { ONBOARD_STORE, STEPPER_INTERNAL_STORE } from 'calypso/landing/stepper/stores';
+import { useProcessingLoadingMessages } from 'calypso/landing/stepper/hooks/use-processing-loading-messages';
+import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { useInterval } from 'calypso/lib/interval';
 import type { Step } from '../../types';
@@ -16,24 +16,11 @@ export enum ProcessingResult {
 	FAILURE = 'failure',
 }
 
-const SiteIntent = Onboard.SiteIntent;
-
 const ProcessingStep: Step = function ( props ): ReactElement | null {
 	const { submit } = props.navigation;
 
 	const { __ } = useI18n();
-	let loadingMessages = [
-		{ title: __( 'Laying the foundations' ), duration: 2000 },
-		{ title: __( 'Turning on the lights' ), duration: 3000 },
-		{ title: __( 'Making it beautiful' ), duration: 2000 },
-		{ title: __( 'Personalizing your site' ), duration: 4000 },
-		{ title: __( 'Sprinkling some magic' ), duration: 4000 },
-		{ title: __( 'Securing your data' ), duration: 5000 },
-		{ title: __( 'Enabling encryption' ), duration: 3000 },
-		{ title: __( 'Optimizing your content' ), duration: 6000 },
-		{ title: __( 'Applying a shiny top coat' ), duration: 4000 },
-		{ title: __( 'Closing the loop' ), duration: 5000 },
-	];
+	const loadingMessages = useProcessingLoadingMessages();
 
 	const [ currentMessageIndex, setCurrentMessageIndex ] = useState( 0 );
 
@@ -45,27 +32,6 @@ const ProcessingStep: Step = function ( props ): ReactElement | null {
 	const progress = useSelect( ( select ) => select( ONBOARD_STORE ).getProgress() );
 	const progressTitle = useSelect( ( select ) => select( ONBOARD_STORE ).getProgressTitle() );
 	const stepProgress = useSelect( ( select ) => select( ONBOARD_STORE ).getStepProgress() );
-
-	const stepData = useSelect( ( select ) => select( STEPPER_INTERNAL_STORE ).getStepData() );
-	switch ( stepData.intent ) {
-		case SiteIntent.DIFM:
-			loadingMessages = [
-				{ title: __( 'Securing your data' ), duration: 5000 },
-				{ title: __( 'Enabling encryption' ), duration: 3000 },
-				{ title: __( 'Applying a shiny top coat' ), duration: 4000 },
-				{ title: __( 'Closing the loop' ), duration: 4000 },
-			];
-			break;
-		case SiteIntent.Sell:
-			loadingMessages = [
-				{ title: __( 'Sprinkling some magic' ), duration: 4000 },
-				{ title: __( 'Securing your data' ), duration: 5000 },
-				{ title: __( 'Enabling encryption' ), duration: 3000 },
-				{ title: __( 'Applying a shiny top coat' ), duration: 4000 },
-				{ title: __( 'Closing the loop' ), duration: 5000 },
-			];
-			break;
-	}
 
 	const getCurrentMessage = () => {
 		return progressTitle || loadingMessages[ currentMessageIndex ]?.title;
