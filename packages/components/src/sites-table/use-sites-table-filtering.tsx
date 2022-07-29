8860fbe1ee7e6a0bfd4cd70bcfae70b1703e1a42
Philip Jackson <p-jackson@live.com>
2022-07-29 13:29:08 +1200

    Re-instate fuzzy search ordering
    Fuzzy search leaves items in the table even when they don't technically
    match the search term. This means sorting by relevance is an important
    part of the fuzzy search experience. Otherwise you can end up with a
    perfect match appearing at the bottom of the list, and a row that has
    very little to do with the search term appearing at the top.

--- packages/components/src/sites-table/use-sites-table-filtering.tsx
+++ packages/components/src/sites-table/use-sites-table-filtering.tsx
@@ -27,9 +27,6 @@ export function useSitesTableFiltering(
 		data: allSites,
 		keys: [ 'URL', 'name', 'slug' ],
 		query: search,
-		options: {
-			shouldSort: false,
-		},
 	} );
 
 	const [ tabs, filteredByStatus ] = useMemo( () => {
