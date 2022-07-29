bd87a76c8b1e33eb70ef6f6c925ad5ed89b3686b
Ben Zhu <ben.zhu@a8c.com>
2022-07-29 15:30:06 +1200

    Add & style image

--- client/my-sites/promote-post/components/promoted-post/index.tsx
+++ client/my-sites/promote-post/components/promoted-post/index.tsx
@@ -2,8 +2,8 @@ import { CompactCard } from '@automattic/components';
 import EllipsisMenu from 'calypso/components/ellipsis-menu';
 import './style.scss';
 import PopoverMenuItem from 'calypso/components/popover-menu/item';
+import { Campaign } from 'calypso/data/promote-post/use-promote-post-campaigns-query';
 import { SmartStatuses } from 'calypso/my-sites/promote-post/components/promoted-post-list';
-import { Campaign } from 'calypso/state/promote-post/selectors';
 
 type Props = {
 	campaign: Campaign;
@@ -13,11 +13,16 @@ export default function PromotedPost( { campaign }: Props ) {
 	return (
 		<CompactCard className="promoted-post__card">
 			<div className="promoted-post__main">
-				<div className="promoted-post__subtitle">{ SmartStatuses[ campaign.status_smart ] }</div>
-				<div className="promoted-post__title">{ campaign.content_config.title }</div>
-				<div className="promoted-post__subtitle">
-					{ campaign.start_date } - { campaign.end_date }
-					todo stats 562 Reached, 10 Clicks, Spent $11 in 5 days
+				<div>
+					<div className="promoted-post__subtitle">{ SmartStatuses[ campaign.status_smart ] }</div>
+					<div className="promoted-post__title">{ campaign.content_config.title }</div>
+					<div className="promoted-post__subtitle">
+						{ campaign.start_date } - { campaign.end_date }
+						todo stats 562 Reached, 10 Clicks, Spent $11 in 5 days
+					</div>
+				</div>
+				<div className="promoted-post__image-container">
+					<img className="promoted-post__image" src={ campaign.content_image } alt="" />
 				</div>
 			</div>
 			<EllipsisMenu
