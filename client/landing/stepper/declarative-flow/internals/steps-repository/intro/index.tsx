a00930244dec6190ce6dad3bee61491642d3c740
Omar Alshaker <omar@omaralshaker.com>
2022-07-31 18:57:54 +0200

    Capitalize step component name

--- client/landing/stepper/declarative-flow/internals/steps-repository/intro/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/intro/index.tsx
@@ -1,12 +1,12 @@
 import cx from 'classnames';
 import { StepContainer } from 'calypso/../packages/onboarding/src';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
-import Intro from './intro';
+import IntroStep from './intro';
 import type { Step } from '../../types';
 
 import './styles.scss';
 
-const intro: Step = function intro( { navigation, flow } ) {
+const Intro: Step = function Intro( { navigation, flow } ) {
 	const { goNext, goBack } = navigation;
 
 	const handleGetStarted = () => {
@@ -21,10 +21,10 @@ const intro: Step = function intro( { navigation, flow } ) {
 			isHorizontalLayout={ false }
 			isWideLayout={ true }
 			isLargeSkipLayout={ false }
-			stepContent={ <Intro flowName={ flow } goNext={ handleGetStarted } /> }
+			stepContent={ <IntroStep flowName={ flow } goNext={ handleGetStarted } /> }
 			recordTracksEvent={ recordTracksEvent }
 		/>
 	);
 };
 
-export default intro;
+export default Intro;
