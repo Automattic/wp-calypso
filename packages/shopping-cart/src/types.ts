54d4ba1ba33917fd39fc01aca9d39134d58c3fd6
Panos Kountanis <panosktn@gmail.com>
2022-08-01 18:53:50 +0300

    Adding optional signup_flow into RequestCartProductExtra

--- packages/shopping-cart/src/types.ts
+++ packages/shopping-cart/src/types.ts
@@ -389,6 +389,7 @@ export interface RequestCartProductExtra extends ResponseCartProductExtra {
 	jetpackPurchaseToken?: string;
 	auth_code?: string;
 	privacy_available?: boolean;
+	signup_flow?: string;
 }
 
 export interface GSuiteProductUser {
