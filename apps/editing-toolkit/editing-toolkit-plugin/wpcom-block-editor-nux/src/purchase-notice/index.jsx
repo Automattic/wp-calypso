02e59ab6033970179add85fee615578c9244f4d6
Mark Biek <mark.biek@automattic.com>
2022-08-01 14:33:56 -0400

    Prevent unnecessary renders in PurchaseNotice
    * Provide appropriate dependencies to `useEffect`
    * Switch from `useState` to `useRef`

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/purchase-notice/index.jsx
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/purchase-notice/index.jsx
@@ -1,19 +1,19 @@
 import { useDispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { store as noticesStore } from '@wordpress/notices';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import './style.scss';
 
 function PurchaseNotice() {
-	const [ hasPaymentNotice, setHasPaymentNotice ] = useState( false );
+	const hasPaymentNotice = useRef( false );
 	const { createNotice } = useDispatch( noticesStore );
 
 	useEffect( () => {
 		const noticePattern = /[&?]notice=([\w_-]+)/;
 		const match = noticePattern.exec( document.location.search );
 		const notice = match && match[ 1 ];
-		if ( 'purchase-success' === notice && hasPaymentNotice === false ) {
-			setHasPaymentNotice( true );
+		if ( 'purchase-success' === notice && hasPaymentNotice.current === false ) {
+			hasPaymentNotice.current = true;
 			createNotice(
 				'info',
 				__(
@@ -26,7 +26,7 @@ function PurchaseNotice() {
 				}
 			);
 		}
-	} );
+	}, [ createNotice ] );
 
 	return null;
 }
