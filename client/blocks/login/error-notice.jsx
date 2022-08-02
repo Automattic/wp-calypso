0fbdc69d2a440afa8ca18d836913293759c2fa5b
Philip Jackson <p-jackson@live.com>
2022-08-03 02:20:17 +1200

    Move signurl logic into <ErrorNotice>
    The calculation of the signup url is delayed as late as possible so that
    it's only done on the client. Calculation of the url requires access to
    `window.location` which can't be used on the server.

--- client/blocks/login/error-notice.jsx
+++ client/blocks/login/error-notice.jsx
@@ -1,9 +1,11 @@
 import config from '@automattic/calypso-config';
+import { getUrlParts } from '@automattic/calypso-url';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import Notice from 'calypso/components/notice';
+import { getSignupUrl } from 'calypso/lib/login';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import {
 	getRequestError,
@@ -11,6 +13,9 @@ import {
 	getCreateSocialAccountError,
 	getRequestSocialAccountError,
 } from 'calypso/state/login/selectors';
+import { getCurrentOAuth2Client } from 'calypso/state/oauth2-clients/ui/selectors';
+import getCurrentQueryArguments from 'calypso/state/selectors/get-current-query-arguments';
+import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 
 class ErrorNotice extends Component {
 	static propTypes = {
@@ -18,7 +23,8 @@ class ErrorNotice extends Component {
 		requestAccountError: PropTypes.object,
 		requestError: PropTypes.object,
 		twoFactorAuthRequestError: PropTypes.object,
-		signupUrl: PropTypes.string,
+		isGutenboarding: PropTypes.bool.isRequired,
+		locale: PropTypes.string,
 	};
 
 	componentDidUpdate( prevProps ) {
@@ -55,6 +61,20 @@ class ErrorNotice extends Component {
 		);
 	}
 
+	getSignupUrl() {
+		const { currentQuery, currentRoute, oauth2Client, locale, isGutenboarding } = this.props;
+		const { pathname } = getUrlParts( window.location.href );
+
+		return getSignupUrl(
+			currentQuery,
+			currentRoute,
+			oauth2Client,
+			locale,
+			pathname,
+			isGutenboarding
+		);
+	}
+
 	render() {
 		const error = this.getError();
 
@@ -72,7 +92,7 @@ class ErrorNotice extends Component {
 
 		let message = error.message;
 
-		const { signupUrl } = this.props;
+		const signupUrl = this.getSignupUrl();
 
 		if ( error.code === 'unknown_user' && signupUrl ) {
 			message = this.props.translate(
@@ -129,6 +149,9 @@ export default connect(
 		requestAccountError: getRequestSocialAccountError( state ),
 		requestError: getRequestError( state ),
 		twoFactorAuthRequestError: getTwoFactorAuthRequestError( state ),
+		currentQuery: getCurrentQueryArguments( state ),
+		currentRoute: getCurrentRoute( state ),
+		oauth2Client: getCurrentOAuth2Client( state ),
 	} ),
 	{
 		recordTracksEvent,
