98ebde14abb9ab4ae50ebccb5402c35af382a498
brookewp <brooke.kaminski@automattic.com>
2022-08-02 15:02:51 -0700

    Update tests based on PR feedback

--- client/components/marketing-survey/cancel-purchase-form/step-components/test/upgrade-at-step.jsx
+++ client/components/marketing-survey/cancel-purchase-form/step-components/test/upgrade-at-step.jsx
@@ -6,32 +6,39 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { UpgradeATStep } from '../upgrade-at-step';
 
+jest.mock( '@automattic/components/src/button', () => ( { href, onClick, children } ) => (
+	<a href={ href } onClick={ onClick }>
+		{ children }
+	</a>
+) );
+
 const noop = () => {};
 const props = {
 	recordTracksEvent: jest.fn(),
 	selectedSite: { slug: 'site_slug' },
-	translate: ( content ) => `Translated: ${ content }`,
+	translate: jest.fn( ( content ) => content ),
 };
 
 describe( 'UpgradeATStep', () => {
 	test( 'should render translated heading content', () => {
 		const { container } = render( <UpgradeATStep { ...props } recordTracksEvent={ noop } /> );
-		expect( container.firstChild ).toHaveTextContent(
-			'Translated: New! Install Custom Plugins and Themes'
-		);
+		expect( props.translate ).toHaveBeenCalled();
+		expect( container.firstChild ).toHaveTextContent( 'New! Install Custom Plugins and Themes' );
 	} );
 
 	test( 'should render translated link content', () => {
 		render( <UpgradeATStep { ...props } recordTracksEvent={ noop } /> );
+		expect( props.translate ).toHaveBeenCalled();
 		expect( screen.queryByRole( 'group' ) ).toHaveTextContent(
-			'Translated: Did you know that you can now use third-party plugins and themes on the WordPress.com Business plan? ' +
+			'Did you know that you can now use third-party plugins and themes on the WordPress.com Business plan? ' +
 				'Claim a 25% discount when you upgrade your site today - {{b}}enter the code BIZC25 at checkout{{/b}}.'
 		);
 	} );
 
 	test( 'should render translated confirmation content', () => {
 		render( <UpgradeATStep { ...props } recordTracksEvent={ noop } /> );
-		expect( screen.queryByRole( 'link' ) ).toHaveTextContent( 'Translated: Upgrade My Site' );
+		expect( props.translate ).toHaveBeenCalled();
+		expect( screen.queryByRole( 'link' ) ).toHaveTextContent( 'Upgrade My Site' );
 	} );
 
 	test( 'should render button with link to business plan checkout', () => {
@@ -48,10 +55,11 @@ describe( 'UpgradeATStep', () => {
 		render(
 			<UpgradeATStep { ...props } translate={ noop } recordTracksEvent={ recordTracksEvent } />
 		);
+		const user = userEvent.setup();
 		const btn = screen.queryByRole( 'link' );
 
 		btn.addEventListener( 'click', ( event ) => event.preventDefault(), false );
-		await userEvent.click( btn );
+		await user.click( btn );
 
 		expect( recordTracksEvent ).toHaveBeenCalledWith(
 			'calypso_cancellation_upgrade_at_step_upgrade_click'
