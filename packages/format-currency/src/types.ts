201d4c77be010d1810cd8f2b998f4f37ed2e0a80
Payton Swick <payton@foolord.com>
2022-08-03 15:12:47 -0400

    Document each option

--- packages/format-currency/src/types.ts
+++ packages/format-currency/src/types.ts
@@ -1,9 +1,51 @@
 export interface FormatCurrencyOptions {
+	/**
+	 * The symbol separating the integer part of a decimal from its fraction.
+	 *
+	 * Will be set automatically by the currency code.
+	 */
 	decimal?: string;
+
+	/**
+	 * The symbol separating the thousands part of an amount from its hundreds.
+	 *
+	 * Will be set automatically by the currency code.
+	 */
 	grouping?: string;
+
+	/**
+	 * The symbol separating the thousands part of an amount from its hundreds.
+	 *
+	 * Will be set automatically by the currency code.
+	 */
 	precision?: number;
+
+	/**
+	 * The currency symbol.
+	 *
+	 * Will be set automatically by the currency code.
+	 */
 	symbol?: string;
+
+	/**
+	 * Forces any decimal zeros to be hidden if set.
+	 *
+	 * For example, the function will normally format `10.00` in `USD` as
+	 * `$10.00` but when this option is true, it will return `$10` instead.
+	 *
+	 * For currencies without decimals (eg: JPY), this has no effect.
+	 */
 	stripZeros?: boolean;
+
+	/**
+	 * Changes function to treat number as an integer in the currency's smallest unit.
+	 *
+	 * Since rounding errors are common in floating point math, sometimes a price
+	 * is provided as an integer in the smallest unit of a currency (eg: cents in
+	 * USD or yen in JPY). If this option is false, the function will format the
+	 * amount `1025` in `USD` as `$1,025.00`, but when the option is true, it
+	 * will return `$10.25` instead.
+	 */
 	isSmallestUnit?: boolean;
 }
 
