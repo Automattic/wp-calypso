b6ee8b00afa197955cc448f23f2241c88adcb0e9
Payton Swick <payton@foolord.com>
2022-08-03 16:46:26 -0400

    format-currency: Add isSmallestUnit option to support integer amounts (#66175)
    * Add smallestUnit to formatCurrency
    
    * Export types from format-currency package
    
    * Rename option to isSmallestUnit
    
    * Reformat docblock alignment
    
    * Document each option
    
    * Provide details in each function's documentation
    
    Removes the option JSDoc documentation in favor of the TS documentation
    and the function's prose.
    
    * Add docs for all exports to README
    
    * Clarify type of CurrencyObject.sign
    
    * Add tests for isSmallestUnit and floats

--- packages/format-currency/src/index.ts
+++ packages/format-currency/src/index.ts
@@ -6,18 +6,36 @@ export { getCurrencyDefaults };
 
 export { CURRENCIES } from './currencies';
 
+export * from './types';
+
 /**
- * Formats money with a given currency code
+ * Formats money with a given currency code.
  *
- * @param   {number}     number              number to format
- * @param   {string}     code                currency code e.g. 'USD'
- * @param   {object}     options             options object
- * @param   {string}     options.decimal     decimal symbol e.g. ','
- * @param   {string}     options.grouping    thousands separator
- * @param   {number}     options.precision   decimal digits
- * @param   {string}     options.symbol      currency symbol e.g. 'A$'
- * @param   {boolean}    options.stripZeros  whether to remove trailing zero cents
- * @returns {?string}                        A formatted string.
+ * The currency will define the properties to use for this formatting, but
+ * those properties can be overridden using the options. Be careful when doing
+ * this.
+ *
+ * For currencies that include decimals, this will always return the amount
+ * with decimals included, even if those decimals are zeros. To exclude the
+ * zeros, use the `stripZeros` option. For example, the function will normally
+ * format `10.00` in `USD` as `$10.00` but when this option is true, it will
+ * return `$10` instead.
+ *
+ * Since rounding errors are common in floating point math, sometimes a price
+ * is provided as an integer in the smallest unit of a currency (eg: cents in
+ * USD or yen in JPY). Set the `isSmallestUnit` to change the function to
+ * operate on integer numbers instead. If this option is not set or false, the
+ * function will format the amount `1025` in `USD` as `$1,025.00`, but when the
+ * option is true, it will return `$10.25` instead.
+ *
+ * Will return null if the currency code is unknown or if the number is not a
+ * number. Will also return null if `isSmallestUnit` is set and the number is
+ * not an integer.
+ *
+ * @param      {number}                   number     number to format; assumed to be a float unless isSmallestUnit is set.
+ * @param      {string}                   code       currency code e.g. 'USD'
+ * @param      {FormatCurrencyOptions}    options    options object
+ * @returns    {?string}                  A formatted string.
  */
 export default function formatCurrency(
 	number: number,
@@ -28,8 +46,17 @@ export default function formatCurrency(
 	if ( ! currencyDefaults || isNaN( number ) ) {
 		return null;
 	}
-	const { decimal, grouping, precision, symbol } = { ...currencyDefaults, ...options };
+	const { decimal, grouping, precision, symbol, isSmallestUnit } = {
+		...currencyDefaults,
+		...options,
+	};
 	const sign = number < 0 ? '-' : '';
+	if ( isSmallestUnit ) {
+		if ( ! Number.isInteger( number ) ) {
+			return null;
+		}
+		number = convertPriceForSmallestUnit( number, precision );
+	}
 	let value = numberFormat( Math.abs( number ), precision, decimal, grouping );
 
 	if ( options.stripZeros ) {
@@ -42,14 +69,31 @@ export default function formatCurrency(
 /**
  * Returns a formatted price object.
  *
- * @param   {number}     number              number to format
- * @param   {string}     code                currency code e.g. 'USD'
- * @param   {object}     options             options object
- * @param   {string}     options.decimal     decimal symbol e.g. ','
- * @param   {string}     options.grouping    thousands separator
- * @param   {number}     options.precision   decimal digits
- * @param   {string}     options.symbol      currency symbol e.g. 'A$'
- * @returns {?CurrencyObject}                        A formatted string e.g. { symbol:'$', integer: '$99', fraction: '.99', sign: '-' }
+ * The currency will define the properties to use for this formatting, but
+ * those properties can be overridden using the options. Be careful when doing
+ * this.
+ *
+ * For currencies that include decimals, this will always return the amount
+ * with decimals included, even if those decimals are zeros. To exclude the
+ * zeros, use the `stripZeros` option. For example, the function will normally
+ * format `10.00` in `USD` as `$10.00` but when this option is true, it will
+ * return `$10` instead.
+ *
+ * Since rounding errors are common in floating point math, sometimes a price
+ * is provided as an integer in the smallest unit of a currency (eg: cents in
+ * USD or yen in JPY). Set the `isSmallestUnit` to change the function to
+ * operate on integer numbers instead. If this option is not set or false, the
+ * function will format the amount `1025` in `USD` as `$1,025.00`, but when the
+ * option is true, it will return `$10.25` instead.
+ *
+ * Will return null if the currency code is unknown or if the number is not a
+ * number. Will also return null if `isSmallestUnit` is set and the number is
+ * not an integer.
+ *
+ * @param      {number}                   number     number to format; assumed to be a float unless isSmallestUnit is set.
+ * @param      {string}                   code       currency code e.g. 'USD'
+ * @param      {FormatCurrencyOptions}    options    options object
+ * @returns    {?CurrencyObject}          A formatted string e.g. { symbol:'$', integer: '$99', fraction: '.99', sign: '-' }
  */
 export function getCurrencyObject(
 	number: number,
@@ -60,8 +104,17 @@ export function getCurrencyObject(
 	if ( ! currencyDefaults || isNaN( number ) ) {
 		return null;
 	}
-	const { decimal, grouping, precision, symbol } = { ...currencyDefaults, ...options };
+	const { decimal, grouping, precision, symbol, isSmallestUnit } = {
+		...currencyDefaults,
+		...options,
+	};
 	const sign = number < 0 ? '-' : '';
+	if ( isSmallestUnit ) {
+		if ( ! Number.isInteger( number ) ) {
+			return null;
+		}
+		number = convertPriceForSmallestUnit( number, precision );
+	}
 	const absNumber = Math.abs( number );
 	const rawInteger = Math.floor( absNumber );
 	const integer = numberFormat( absNumber, precision, decimal, grouping ).split( decimal )[ 0 ];
@@ -88,4 +141,12 @@ export function getCurrencyObject(
 function stripZeros( number: string, decimal: string ): string {
 	const regex = new RegExp( `\\${ decimal }0+$` );
 	return number.replace( regex, '' );
+}
+
+function convertPriceForSmallestUnit( price: number, precision: number ): number {
+	return price / getSmallestUnitDivisor( precision );
+}
+
+function getSmallestUnitDivisor( precision: number ): number {
+	return 10 ** precision;
 }
