cb663a07ad8c865b2989495a60af058ada69c44a
Omar Alshaker <omar@omaralshaker.com>
2022-08-04 18:48:48 +0200

    Calypso-related style fixes

--- packages/help-center/src/hooks/use-still-need-help-url.tsx
+++ packages/help-center/src/hooks/use-still-need-help-url.tsx
@@ -1,18 +1,20 @@
 /* eslint-disable no-restricted-imports */
 import { useHas3PC, useSupportAvailability } from '@automattic/data-stores';
+import { shouldTargetWpcom } from '@automattic/help-center';
 import { useSelector } from 'react-redux';
 import getIsSimpleSite from 'calypso/state/sites/selectors/is-simple-site';
 
 export function useStillNeedHelpURL() {
 	const { hasCookies } = useHas3PC();
 	const isSimpleSite: boolean = useSelector( ( state ) => getIsSimpleSite( state ) );
-	const { data: supportAvailability } = useSupportAvailability( 'OTHER', isSimpleSite );
+	const canUseWpcomApis = Boolean( shouldTargetWpcom( isSimpleSite ) );
+	const { data: supportAvailability } = useSupportAvailability( 'OTHER', canUseWpcomApis );
 
 	// email support is available for all non-free users, let's use it as a proxy for free users
 	// TODO: check purchases instead
 	const isFreeUser = ! supportAvailability?.is_user_eligible_for_tickets;
 
-	if ( ! isSimpleSite ) {
+	if ( ! canUseWpcomApis ) {
 		return 'https://wordpress.com/help/contact';
 	}
 
