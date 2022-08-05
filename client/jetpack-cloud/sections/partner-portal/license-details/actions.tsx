860a93d8c66097442514be0ae8983d0060cdf213
Wilmerson da Silva <wilmersondasilva@gmail.com>
2022-08-05 14:58:09 -0300

    Licensing Portal: implement the unassign licenses feature (#66237)
    * Licensing Portal: create use-unassign-license-mutation hook
    
    This hook will make the HTTP request that unassign one license from a site.
    
    * Licensing Portal: create use-unassign-license-dialog component to users confirm they want to unassign a license
    
    * Licensing Portal: add a button to unassign licenses
    
    Currently, it is only possible to revoke licenses, and revoked licenses can't be used again. To allow the process of unlinking a license from a specific site, an "Unassign License" button was added.
    
    * Licensing Portal: fix the Assign License button style
    
    In some resolutions, the Assign License was not being shown.
    
    * Licensing Portal: update the Unassign License dialog design
    
    The design/copy was the same as the Revoke License dialog. This commit changes some colors and updates the copy for one more suitable for the unassign action.
    
    * Licensing Portal: create use-unassign-license-dialog component to users confirm they want to unassign a license
    
    * Licensing Portal: fix the translation done on Unassign License dialog
    
    * Licensing Portal: fix the links used on Unassign and Revoke License dialogs

--- client/jetpack-cloud/sections/partner-portal/license-details/actions.tsx
+++ client/jetpack-cloud/sections/partner-portal/license-details/actions.tsx
@@ -4,6 +4,7 @@ import { ReactElement, useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import RevokeLicenseDialog from 'calypso/jetpack-cloud/sections/partner-portal/revoke-license-dialog';
 import { LicenseState } from 'calypso/jetpack-cloud/sections/partner-portal/types';
+import UnassignLicenseDialog from 'calypso/jetpack-cloud/sections/partner-portal/unassign-license-dialog';
 import { getLicenseState } from 'calypso/jetpack-cloud/sections/partner-portal/utils';
 import { addQueryArgs } from 'calypso/lib/url';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
@@ -27,6 +28,7 @@ export default function LicenseDetailsActions( {
 	const translate = useTranslate();
 	const licenseState = getLicenseState( attachedAt, revokedAt );
 	const [ revokeDialog, setRevokeDialog ] = useState( false );
+	const [ unassignDialog, setUnassignDialog ] = useState( false );
 
 	const openRevokeDialog = useCallback( () => {
 		setRevokeDialog( true );
@@ -38,6 +40,16 @@ export default function LicenseDetailsActions( {
 		dispatch( recordTracksEvent( 'calypso_partner_portal_license_details_revoke_dialog_close' ) );
 	}, [ dispatch, setRevokeDialog ] );
 
+	const openUnassignDialog = useCallback( () => {
+		setUnassignDialog( true );
+		dispatch( recordTracksEvent( 'calypso_partner_portal_license_details_unassign_dialog_open' ) );
+	}, [ dispatch, setUnassignDialog ] );
+
+	const closeUnassignDialog = useCallback( () => {
+		setUnassignDialog( false );
+		dispatch( recordTracksEvent( 'calypso_partner_portal_license_details_unassign_dialog_close' ) );
+	}, [ dispatch, setUnassignDialog ] );
+
 	return (
 		<div className="license-details__actions">
 			{ licenseState !== LicenseState.Revoked && (
@@ -47,9 +59,16 @@ export default function LicenseDetailsActions( {
 			) }
 
 			{ licenseState === LicenseState.Detached && (
-				<Button href={ addQueryArgs( { key: licenseKey }, '/partner-portal/assign-license' ) }>
+				<Button
+					className="license-details__assign-button"
+					href={ addQueryArgs( { key: licenseKey }, '/partner-portal/assign-license' ) }
+				>
 					{ translate( 'Assign License' ) }
 				</Button>
+			) }
+
+			{ licenseState === LicenseState.Attached && (
+				<Button onClick={ openUnassignDialog }>{ translate( 'Unassign License' ) }</Button>
 			) }
 
 			{ revokeDialog && (
@@ -58,6 +77,15 @@ export default function LicenseDetailsActions( {
 					product={ product }
 					siteUrl={ siteUrl }
 					onClose={ closeRevokeDialog }
+				/>
+			) }
+
+			{ unassignDialog && (
+				<UnassignLicenseDialog
+					licenseKey={ licenseKey }
+					product={ product }
+					siteUrl={ siteUrl }
+					onClose={ closeUnassignDialog }
 				/>
 			) }
 		</div>
