f8c49213e82aecb2f05eaad545fb62d9d533025b
Anthony Grullon <agrullon95@gmail.com>
2022-08-05 13:34:22 -0400

    Test PR for launchpad stepper preview

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/index.tsx
@@ -1,8 +1,12 @@
 import { StepContainer } from '@automattic/onboarding';
+import styled from '@emotion/styled';
 import { useTranslate } from 'i18n-calypso';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
+import WebPreview from 'calypso/components/web-preview/component';
+import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import PreviewToolbar from 'calypso/signup/steps/design-picker/preview-toolbar';
 import type { Step } from '../../types';
 import './style.scss';
 
@@ -17,20 +21,44 @@ function PlaceHolderChecklist() {
 	);
 }
 
-function PlaceHolderPreview() {
-	return <div style={ { textAlign: 'center' } }>Preview here</div>;
+function PlaceHolderPreview( { hostName } ) {
+	const PreviewWrapper = styled.div`
+		height: 700px;
+	`;
+
+	return (
+		<PreviewWrapper>
+			<WebPreview
+				class="home-web-prev"
+				showDeviceSwitcher={ true }
+				showPreview
+				showSEO={ true }
+				isContentOnly
+				externalUrl={ hostName }
+				previewUrl={ 'https://' + hostName + '/?demo=true&iframe=true&theme_preview=true' }
+				toolbarComponent={ PreviewToolbar }
+				showClose={ false }
+				showEdit={ false }
+				showExternal={ false }
+				loadingMessage={ '{{strong}}One moment, please…{{/strong}} loading your site.' }
+			/>
+		</PreviewWrapper>
+	);
 }
 
 const Launchpad: Step = ( { navigation } ) => {
 	const translate = useTranslate();
 	const almostReadyToLaunchText = translate( 'Almost ready to launch' );
+	const hostName = useQuery().get( 'siteSlug' );
 
-	const stepContent = (
-		<div className="launchpad__content">
-			<PlaceHolderChecklist />
-			<PlaceHolderPreview />
-		</div>
-	);
+	const stepContent = ( hostName ) => {
+		return (
+			<div className="launchpad__content">
+				<PlaceHolderChecklist />
+				<PlaceHolderPreview hostName={ hostName } />
+			</div>
+		);
+	};
 
 	return (
 		<>
@@ -42,7 +70,7 @@ const Launchpad: Step = ( { navigation } ) => {
 				skipLabelText={ translate( 'Go to Admin' ) }
 				skipButtonAlign={ 'top' }
 				hideBack={ true }
-				stepContent={ stepContent }
+				stepContent={ stepContent( hostName ) }
 				formattedHeader={
 					<FormattedHeader
 						id={ 'launchpad-header' }
