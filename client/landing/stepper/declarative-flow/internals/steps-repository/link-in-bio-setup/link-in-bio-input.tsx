8f5ef2e3c959e3b521ca9d960a10a746452f3109
Renan Carvalho <renansscarvalho@gmail.com>
2022-08-05 11:29:34 +0200

    Add/site icon component (#66290)
    * Make intro step generic
    
    * Capitalize step component name
    
    * Use className instead of abusing stepName
    
    * Add link in bio setup step
    
    * Fix style on upload image component
    
    * Add translations on error messages
    
    * Replace unecessary newly created form input
    
    * Use css variables directly on classes
    
    * Add form submit skeleton
    
    * Fix padding variable on scss file
    
    * Fix handle submit function call
    
    * Use FormFieldSet instead of generic div
    
    * Integrate with site icon upload component
    
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>

--- client/landing/stepper/declarative-flow/internals/steps-repository/link-in-bio-setup/link-in-bio-input.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/link-in-bio-setup/link-in-bio-input.tsx
@@ -7,12 +7,13 @@ import FormLabel from 'calypso/components/forms/form-label';
 import FormInput from 'calypso/components/forms/form-text-input';
 
 type LinkInBioInputProps = {
-	onChange: () => void;
+	onChange: ( e: React.ChangeEvent< HTMLInputElement > ) => void;
 	errorMessage: string;
 	isValid: boolean;
 	inputValue: string;
 	label: string;
 	inputId: string;
+	disabled: boolean;
 };
 
 export const LinkInBioInput = ( {
@@ -22,10 +23,11 @@ export const LinkInBioInput = ( {
 	inputValue,
 	label,
 	inputId,
+	disabled,
 }: LinkInBioInputProps ) => {
 	return (
 		<div className="link-in-bio-setup-form__field">
-			<FormFieldset disabled={ false } className="site-options__form-fieldset">
+			<FormFieldset disabled={ disabled } className="site-options__form-fieldset">
 				<FormLabel htmlFor={ inputId }>{ label }</FormLabel>
 				<div
 					className={ classNames( 'link-in-bio-setup-form__input', {
