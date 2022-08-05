be1ef3b3985367ce04af93bdfab28234cef36195
sejas <antonio@sejas.es>
2022-08-05 17:32:51 +0100

    LoadingPlaceholder: create new package component with storybook

---
+++ packages/components/src/loading-placeholder/index.tsx
@@ -0,0 +1,2 @@
+export { default as LoadingPlaceholder } from './loading-placeholder';
+export type { LoadingPlaceholderProps } from './loading-placeholder';
---
+++ packages/components/src/loading-placeholder/loading-placeholder.tsx
@@ -0,0 +1,21 @@
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export interface LoadingPlaceholderProps {
+	delayMS?: number;
+}
+
+const puldeLightKeyframes = keyframes`
+	50% {
+		background-color: var( --color-neutral-0 );
+	}`;
+
+const LoadingPlaceholder = styled.div< LoadingPlaceholderProps >`
+	animation: ${ puldeLightKeyframes } 1.8s ease-in-out infinite;
+	background-color: var( --color-neutral-10 );
+	min-height: 18px;
+	width: 100%;
+	animation-delay: ${ ( props ) => props.delayMS }ms;
+`;
+
+export default LoadingPlaceholder;
