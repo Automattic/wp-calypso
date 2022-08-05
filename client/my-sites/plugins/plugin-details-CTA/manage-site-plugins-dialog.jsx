969ad9114c785a87f999e11c58406579cc272a17
Wesley Berrêdo <wberredo@gmail.com>
2022-08-05 17:12:40 -0400

    Add non-installed and need upgrade site list

--- client/my-sites/plugins/plugin-details-CTA/manage-site-plugins-dialog.jsx
+++ client/my-sites/plugins/plugin-details-CTA/manage-site-plugins-dialog.jsx
@@ -1,19 +1,41 @@
+import { FEATURE_INSTALL_PLUGINS } from '@automattic/calypso-products';
 import { Dialog, Button } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { useSelector } from 'react-redux';
 import PluginSiteList from 'calypso/my-sites/plugins/plugin-site-list';
 import { siteObjectsToSiteIds } from 'calypso/my-sites/plugins/utils';
-import { getSiteObjectsWithPlugin } from 'calypso/state/plugins/installed/selectors';
+import { getBillingInterval } from 'calypso/state/marketplace/billing-interval/selectors';
+import {
+	getSiteObjectsWithPlugin,
+	getSiteObjectsWithoutPlugin,
+} from 'calypso/state/plugins/installed/selectors';
 import getSelectedOrAllSitesWithPlugins from 'calypso/state/selectors/get-selected-or-all-sites-with-plugins';
+import siteHasFeature from 'calypso/state/selectors/site-has-feature';
 
 export function ManageSitePluginsDialog( { isVisible, onClose, plugin } ) {
 	const translate = useTranslate();
+
+	const billingPeriod = useSelector( getBillingInterval );
 
 	const sitesWithPlugins = useSelector( getSelectedOrAllSitesWithPlugins );
 	const siteIds = [ ...new Set( siteObjectsToSiteIds( sitesWithPlugins ) ) ];
 
 	const sitesWithPlugin = useSelector( ( state ) =>
 		getSiteObjectsWithPlugin( state, siteIds, plugin.slug )
+	);
+	const sitesWithoutPlugin = useSelector( ( state ) =>
+		getSiteObjectsWithoutPlugin( state, siteIds, plugin.slug )
+	);
+
+	const availableOnSites = useSelector( ( state ) =>
+		sitesWithoutPlugin.filter( ( site ) =>
+			siteHasFeature( state, site.ID, FEATURE_INSTALL_PLUGINS )
+		)
+	);
+	const upgradeNeededSites = useSelector( ( state ) =>
+		sitesWithoutPlugin.filter(
+			( site ) => ! siteHasFeature( state, site.ID, FEATURE_INSTALL_PLUGINS )
+		)
 	);
 
 	return (
@@ -24,7 +46,7 @@ export function ManageSitePluginsDialog( { isVisible, onClose, plugin } ) {
 			shouldCloseOnEsc
 		>
 			<PluginSiteList
-				className="manage-site-plugins-dialog__installed-on"
+				className="manage-site-plugins-dialog__plugin-list"
 				title={ translate( 'Installed on %d site', 'Installed on %d sites', {
 					comment: 'header for list of sites a plugin is installed on',
 					args: [ sitesWithPlugin.length ],
@@ -33,6 +55,22 @@ export function ManageSitePluginsDialog( { isVisible, onClose, plugin } ) {
 				sites={ sitesWithPlugin }
 				plugin={ plugin }
 				showAdditionalHeaders
+			/>
+
+			<PluginSiteList
+				className="manage-site-plugins-dialog__plugin-list"
+				title={ translate( 'Available sites' ) }
+				sites={ availableOnSites }
+				plugin={ plugin }
+				billingPeriod={ billingPeriod }
+			/>
+
+			<PluginSiteList
+				className="manage-site-plugins-dialog__plugin-list"
+				title={ translate( 'Upgrade needed' ) }
+				sites={ upgradeNeededSites }
+				plugin={ plugin }
+				billingPeriod={ billingPeriod }
 			/>
 
 			<Button className="manage-site-plugins-dialog__finish-button" onClick={ onClose } primary>
