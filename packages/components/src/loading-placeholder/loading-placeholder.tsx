1d2e67d719d9a70cbbe8d978afb2cdc07c214a4b
sejas <antonio@sejas.es>
2022-08-08 16:34:18 +0100

    Sites Management Page: fix typo on pulse loading animation

--- packages/components/src/loading-placeholder/loading-placeholder.tsx
+++ packages/components/src/loading-placeholder/loading-placeholder.tsx
@@ -5,13 +5,13 @@ export interface LoadingPlaceholderProps {
 	delayMS?: number;
 }
 
-const puldeLightKeyframes = keyframes`
+const pulseLightKeyframes = keyframes`
 	50% {
 		background-color: var( --color-neutral-0 );
 	}`;
 
 const LoadingPlaceholder = styled.div< LoadingPlaceholderProps >`
-	animation: ${ puldeLightKeyframes } 1.8s ease-in-out infinite;
+	animation: ${ pulseLightKeyframes } 1.8s ease-in-out infinite;
 	background-color: var( --color-neutral-10 );
 	min-height: 18px;
 	width: 100%;
