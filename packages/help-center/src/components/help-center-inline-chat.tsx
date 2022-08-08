5cf7a7066f6f2dedfae76dadd5702e573c937a65
Igor Klimer <klimeryk@users.noreply.github.com>
2022-08-08 18:38:19 +0000

    Revert "Help Center: Adjust Happychat endpoint in help center according to env (#66305)"
    This reverts commit 0ec4ed790f581183e8c8073d7741aad3a3638cdd.

--- packages/help-center/src/components/help-center-inline-chat.tsx
+++ packages/help-center/src/components/help-center-inline-chat.tsx
@@ -3,14 +3,12 @@
  */
 import './help-center-inline-chat.scss';
 
-const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
-
 const InlineChat: React.FC = () => {
 	return (
 		<iframe
 			className="help-center-inline-chat__iframe"
 			title="Happychat"
-			src={ `https://widgets.wp.com/calypso-happychat/?env=${ env }` }
+			src="https://widgets.wp.com/calypso-happychat/"
 		/>
 	);
 };
