bb3422358d062177bafae13fd789119c189509e8
vykes-mac <knrymcleish@gmail.com>
2022-08-08 11:26:03 -0500

    memoize url colors to prevent extraction computation all the time

--- packages/components/src/site-thumbnail/use-dominant-color.tsx
+++ packages/components/src/site-thumbnail/use-dominant-color.tsx
@@ -1,14 +1,22 @@
 import FastAverageColor, { FastAverageColorResult } from 'fast-average-color';
 import { useEffect, useState } from 'react';
 
+const colorMap = new Map< string, FastAverageColorResult >();
+
 export default function useDominantColor( url?: string ) {
-	const [ color, setColor ] = useState< FastAverageColorResult >();
+	const [ color, setColor ] = useState< FastAverageColorResult | undefined >(
+		( url && colorMap.get( url ) ) || undefined
+	);
 
 	useEffect( () => {
 		const fac = new FastAverageColor();
 
 		//get the dominant color of image from url
 		if ( url && url.length > 0 ) {
+			if ( colorMap.has( url ) ) {
+				return;
+			}
+
 			fac
 				.getColorAsync( url, {
 					ignoredColor: [
@@ -17,7 +25,10 @@ export default function useDominantColor( url?: string ) {
 					],
 					crossOrigin: 'anonymous',
 				} )
-				.then( ( color ) => setColor( color ) );
+				.then( ( color ) => {
+					colorMap.set( url, color );
+					setColor( color );
+				} );
 		}
 
 		return () => {
