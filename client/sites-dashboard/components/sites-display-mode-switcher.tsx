a4b2c02b4ee76946ce8e5d74b15937b3d166d7b3
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2022-08-09 16:12:41 -0300

    Site Management Dashboard: Use Gridicons instead of custom SVGs on display mode switcher (#66415)
    * Use Gridicon icons instead of custom SVGs
    
    * Do not style the buttons as the default styles are close enough

--- client/sites-dashboard/components/sites-display-mode-switcher.tsx
+++ client/sites-dashboard/components/sites-display-mode-switcher.tsx
@@ -1,43 +1,16 @@
+import { Gridicon } from '@automattic/components';
 import { css } from '@emotion/css';
-import styled from '@emotion/styled';
 import { Button } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
-import { ComponentProps } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { savePreference } from 'calypso/state/preferences/actions';
 import { getPreference, hasReceivedRemotePreferences } from 'calypso/state/preferences/selectors';
-import { SitesListIcon } from './sites-list-icon';
-import { SitesTilesIcon } from './sites-tiles-icon';
 
 const container = css( {
 	marginLeft: 'auto',
 	display: 'flex',
 	gap: '10px',
 } );
-
-const SiteSwitcherButton = styled( Button )< ComponentProps< typeof Button > >(
-	{ borderRadius: '4px' },
-	( props ) => {
-		if ( props.isPressed ) {
-			return {
-				color: '#FFF',
-				background: '#101517',
-
-				'&:hover': {
-					color: '#FFF !important',
-				},
-			};
-		}
-
-		return {
-			color: '#50575E',
-
-			'&:hover': {
-				background: '#F6F7F7',
-			},
-		};
-	}
-);
 
 type SitesDisplayMode = 'tile' | 'list' | 'none';
 
@@ -67,18 +40,18 @@ export const SitesDisplayModeSwitcher = () => {
 
 	return (
 		<div className={ container } role="radiogroup" aria-label={ __( 'Sites display mode' ) }>
-			<SiteSwitcherButton
+			<Button
 				role="radio"
 				aria-label={ __( 'Tile view' ) }
 				onClick={ () => onDisplayModeChange( 'tile' ) }
-				icon={ <SitesTilesIcon /> }
+				icon={ <Gridicon icon="grid" /> }
 				isPressed={ displayMode === 'tile' }
 			/>
-			<SiteSwitcherButton
+			<Button
 				role="radio"
 				aria-label={ __( 'List view' ) }
 				onClick={ () => onDisplayModeChange( 'list' ) }
-				icon={ <SitesListIcon /> }
+				icon={ <Gridicon icon="list-unordered" /> }
 				isPressed={ displayMode === 'list' }
 			/>
 		</div>
