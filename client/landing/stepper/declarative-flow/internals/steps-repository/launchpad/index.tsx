d7a7858b635b06c2f427ccfdfe96488c61fcf654
Anthony Grullon <agrullon95@gmail.com>
2022-08-09 18:49:11 -0400

    Add/launchpad site preview integration (#66424)
    * Initial Launchpad site preview integration
    * Styling prettier fix
    * Set phone as default device

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/index.tsx
@@ -2,24 +2,23 @@ import { StepContainer } from '@automattic/onboarding';
 import { useTranslate } from 'i18n-calypso';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
+import { useSiteSlugParam } from 'calypso/landing/stepper/hooks/use-site-slug-param';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import Checklist from './checklist';
+import LaunchpadSitePreview from './launchpad-site-preview';
 import { tasks } from './tasks';
 import type { Step } from '../../types';
 import './style.scss';
 
-function PlaceHolderPreview() {
-	return <div style={ { textAlign: 'center' } }>Preview here</div>;
-}
-
 const Launchpad: Step = ( { navigation } ) => {
 	const translate = useTranslate();
 	const almostReadyToLaunchText = translate( 'Almost ready to launch' );
+	const siteSlug = useSiteSlugParam();
 
 	const stepContent = (
 		<div className="launchpad__content">
 			<Checklist tasks={ tasks } />
-			<PlaceHolderPreview />
+			<LaunchpadSitePreview siteSlug={ siteSlug } />
 		</div>
 	);
 
