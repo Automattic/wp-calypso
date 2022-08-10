df85b0bf74e34f4b4a00e3dedbd898195c08bcbf
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2022-08-10 16:03:23 -0300

    Wait until preference is saved before allowing the user to toggle mode

--- client/sites-dashboard/components/sites-display-mode-switcher.tsx
+++ client/sites-dashboard/components/sites-display-mode-switcher.tsx
@@ -2,6 +2,7 @@ import { Gridicon } from '@automattic/components';
 import { css } from '@emotion/css';
 import { Button } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { savePreference } from 'calypso/state/preferences/actions';
 import { getPreference, hasReceivedRemotePreferences } from 'calypso/state/preferences/selectors';
@@ -32,8 +33,17 @@ export const SitesDisplayModeSwitcher = () => {
 	const { __ } = useI18n();
 	const dispatch = useDispatch();
 
-	const onDisplayModeChange = ( value: SitesDisplayMode ) => {
-		dispatch( savePreference( PREFERENCE_NAME, value ) );
+	const [ isSaving, setIsSaving ] = useState( false );
+
+	const onDisplayModeChange = async ( value: SitesDisplayMode ) => {
+		const saveDisplayMode = savePreference( PREFERENCE_NAME, value );
+
+		try {
+			setIsSaving( true );
+			await saveDisplayMode( dispatch );
+		} finally {
+			setIsSaving( false );
+		}
 	};
 
 	const displayMode = useSitesDisplayMode();
@@ -44,6 +54,7 @@ export const SitesDisplayModeSwitcher = () => {
 				role="radio"
 				aria-label={ __( 'Tile view' ) }
 				onClick={ () => onDisplayModeChange( 'tile' ) }
+				disabled={ isSaving }
 				icon={ <Gridicon icon="grid" /> }
 				isPressed={ displayMode === 'tile' }
 			/>
@@ -51,6 +62,7 @@ export const SitesDisplayModeSwitcher = () => {
 				role="radio"
 				aria-label={ __( 'List view' ) }
 				onClick={ () => onDisplayModeChange( 'list' ) }
+				disabled={ isSaving }
 				icon={ <Gridicon icon="list-unordered" /> }
 				isPressed={ displayMode === 'list' }
 			/>
