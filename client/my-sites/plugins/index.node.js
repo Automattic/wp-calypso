8b6f5d8dabd7c215d6988e9e12eea55cb5cd6d16
Claudiu Filip <claudiucelfilip@gmail.com>
2022-08-11 13:15:03 +0300

    Ensure the page loading placeholder is rendered SSR

--- client/my-sites/plugins/index.node.js
+++ client/my-sites/plugins/index.node.js
@@ -1,8 +1,7 @@
 import { getLanguageRouteParam } from '@automattic/i18n-utils';
-import { makeLayout } from 'calypso/controller';
 
 export default function ( router ) {
 	const langParam = getLanguageRouteParam();
 
-	router( [ `/${ langParam }/plugins/*`, `/${ langParam }/plugins` ], makeLayout );
+	router( [ `/${ langParam }/plugins`, `/${ langParam }/plugins/*` ] );
 }
