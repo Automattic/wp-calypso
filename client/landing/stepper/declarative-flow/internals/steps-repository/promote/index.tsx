ea59560198154fa7d5bcf76e9f95e0c6e6d97932
Andres Blanco <andresblanco@gmail.com>
2022-08-11 15:23:07 -0300

    added header

--- client/landing/stepper/declarative-flow/internals/steps-repository/promote/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/promote/index.tsx
@@ -1,10 +1,15 @@
+import { useTranslate } from 'i18n-calypso';
 import { StepContainer } from 'calypso/../packages/onboarding/src';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import PromoteStep from './promote';
 import type { Step } from '../../types';
+import './style.scss';
 
 const Promote: Step = function Promote( { navigation, flow } ) {
 	const { goNext, goBack } = navigation;
+	const translate = useTranslate();
+	const headerText = translate( 'Promote post' );
 
 	const handleGetStarted = () => {
 		// needs to be implemented
@@ -15,6 +20,7 @@ const Promote: Step = function Promote( { navigation, flow } ) {
 			stepName={ 'promote' }
 			goBack={ goBack }
 			isHorizontalLayout={ false }
+			formattedHeader={ <FormattedHeader headerText={ headerText } align={ 'center' } /> }
 			// isWideLayout={ true }
 			// isLargeSkipLayout={ false }
 			stepContent={ <PromoteStep flowName={ flow } goNext={ handleGetStarted } /> }
