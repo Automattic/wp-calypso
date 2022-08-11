fd0730c30ff13d3f6fc7691176e1ce606452af56
Marcelo Serpa <81248+fullofcaffeine@users.noreply.github.com>
2022-08-11 16:32:38 -0500

    [ETK/Error-Reporting][Sentry] Pass a dynamic release name based of the WPCOM git hash to Sentry (#66186)
    * Pass a dynamic release name based of the WPCOM git hash to Sentry
    
    Attempt to make the coupling with the WPCOM constant as localized as possible, while not tied WPCOM to the rest of the interface. This will make it slightly easier to generalize this feature to be used by other WP instances other than WPCOM in the future.
    
    * Remove ternary, constant should always be defined
    
    * Add failsafe releaseName in case hash constant is not defined
    
    * Fix `defined` call

--- apps/editing-toolkit/editing-toolkit-plugin/error-reporting/index.js
+++ apps/editing-toolkit/editing-toolkit-plugin/error-reporting/index.js
@@ -3,6 +3,7 @@ import apiFetch from '@wordpress/api-fetch';
 import { addAction } from '@wordpress/hooks';
 
 const shouldActivateSentry = window.A8C_ETK_ErrorReporting_Config?.shouldActivateSentry === 'true';
+
 /**
  * Errors that happened before this script had a chance to load
  * are captured in a global array. See `./index.php`.
@@ -11,12 +12,11 @@ const headErrors = window._jsErr || [];
 const headErrorHandler = window._headJsErrorHandler;
 
 function activateSentry() {
+	const SENTRY_RELEASE_NAME = window.A8C_ETK_ErrorReporting_Config.releaseName;
+
 	Sentry.init( {
 		dsn: 'https://658ae291b00242148af6b76494d4a49a@o248881.ingest.sentry.io/5876245',
-		// Set tracesSampleRate to 1.0 to capture 100%
-		// of transactions for performance monitoring.
-		// We recommend adjusting this value in production
-		release: 'wpcom-test-01',
+		release: SENTRY_RELEASE_NAME,
 	} );
 
 	// Capture exceptions from Gutenberg React Error Boundaries
