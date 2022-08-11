a82e637f9a797fc9488c6582e26c49b34057d8fe
Claudiu Filip <claudiucelfilip@gmail.com>
2022-08-11 12:18:02 +0300

    Remove unnecessary SSR routes

--- client/my-sites/plugins/index.node.js
+++ client/my-sites/plugins/index.node.js
@@ -1,36 +1,8 @@
 import { getLanguageRouteParam } from '@automattic/i18n-utils';
-import { makeLayout, ssrSetupLocale } from 'calypso/controller';
-import { setHrefLangLinks, setLocalizedCanonicalUrl } from 'calypso/controller/localized-links';
-import { browsePlugins, browsePluginsOrPlugin } from './controller';
+import { makeLayout } from 'calypso/controller';
 
 export default function ( router ) {
 	const langParam = getLanguageRouteParam();
-	router(
-		`/${ langParam }/plugins/browse/:category`,
-		ssrSetupLocale,
-		setHrefLangLinks,
-		setLocalizedCanonicalUrl,
-		browsePlugins,
-		makeLayout
-	);
 
-	router(
-		`/${ langParam }/plugins`,
-		ssrSetupLocale,
-		setHrefLangLinks,
-		setLocalizedCanonicalUrl,
-		browsePlugins,
-		makeLayout
-	);
-
-	router(
-		`/${ langParam }/plugins/:plugin`,
-		ssrSetupLocale,
-		setHrefLangLinks,
-		setLocalizedCanonicalUrl,
-		browsePluginsOrPlugin,
-		makeLayout
-	);
-
-	router( '/plugins/*', makeLayout );
+	router( [ `/${ langParam }/plugins/*`, `/${ langParam }/plugins` ], makeLayout );
 }
