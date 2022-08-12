920d174f0ffaed39bbe2b182391aa3bb888f66cd
Claudiu Filip <claudiucelfilip@gmail.com>
2022-08-12 11:52:31 +0300

    Restore SSR locale middleware

--- client/my-sites/plugins/index.node.js
+++ client/my-sites/plugins/index.node.js
@@ -1,7 +1,8 @@
 import { getLanguageRouteParam } from '@automattic/i18n-utils';
+import { ssrSetupLocale } from 'calypso/controller';
 
 export default function ( router ) {
 	const langParam = getLanguageRouteParam();
 
-	router( [ `/${ langParam }/plugins`, `/${ langParam }/plugins/*` ] );
+	router( [ `/${ langParam }/plugins`, `/${ langParam }/plugins/*` ], ssrSetupLocale );
 }
