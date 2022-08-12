1d26912d69c319b8d375bad52fb265c4d96fcb7c
Claudiu Filip <claudiucelfilip@gmail.com>
2022-08-12 16:49:40 +0300

    Move path localization to dedicated function

--- packages/i18n-utils/src/index.ts
+++ packages/i18n-utils/src/index.ts
@@ -1,4 +1,5 @@
 export { localizeUrl, useLocalizeUrl, withLocalizeUrl } from './localize-url';
+export { localizePath, useLocalizePath, withLocalizePath } from './localize-path';
 export { LocaleProvider, useLocale, withLocale, useIsEnglishLocale } from './locale-context';
 export * from './locales';
 export { default as guessTimezone } from './guess-timezone';
---
+++ packages/i18n-utils/src/localize-path.tsx
@@ -0,0 +1,54 @@
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { getLocaleSlug } from 'i18n-calypso';
+import { useCallback, ComponentType } from 'react';
+import { useLocale } from './locale-context';
+import { magnificentNonEnLocales, Locale } from './locales';
+
+function getDefaultLocale(): Locale {
+	return getLocaleSlug?.() ?? 'en';
+}
+
+export function localizePath(
+	path: string,
+	locale: Locale = getDefaultLocale(),
+	isLoggedIn = true
+): string {
+	if ( ! path.startsWith( '/' ) ) {
+		return path;
+	}
+	const shouldPrefix =
+		! isLoggedIn &&
+		magnificentNonEnLocales.includes( locale ) &&
+		! path.startsWith( `/${ locale }/` );
+
+	return shouldPrefix ? `/${ locale }${ path }` : path;
+}
+
+export function useLocalizePath() {
+	const providerLocale = useLocale();
+
+	return useCallback(
+		( path: string, locale?: Locale, isLoggedIn?: boolean ) => {
+			if ( locale ) {
+				return localizePath( path, locale, isLoggedIn );
+			}
+			return localizePath( path, providerLocale, isLoggedIn );
+		},
+		[ providerLocale ]
+	);
+}
+
+export const withLocalizePath = createHigherOrderComponent(
+	< OuterProps, >(
+		InnerComponent: ComponentType<
+			OuterProps & { localizePath: ReturnType< typeof useLocalizePath > }
+		>
+	) => {
+		return ( props: OuterProps ) => {
+			const localizePath = useLocalizePath();
+			const innerProps = { ...props, localizePath };
+			return <InnerComponent { ...innerProps } />;
+		};
+	},
+	'withLocalizePath'
+);
