ef0fbee079807a3f0be3eff5c19ee5e65e915dad
Daniel Walmsley <goldsounds@gmail.com>
2022-08-12 17:11:40 -0700

    Fix comment

--- client/lib/promote-post/index.ts
+++ client/lib/promote-post/index.ts
@@ -27,7 +27,7 @@ export async function loadDSPWidgetJS( onLoad?: () => void ) {
 		return;
 	}
 	const script = document.createElement( 'script' );
-	// cachebust once per minute
+	// cachebust once per second for now, we will reduce this rate once we're out of beta
 	script.src = config( 'dsp_widget_js_src' ) + '?ver=' + Math.round( Date.now() / 1000 );
 	script.async = true;
 	if ( onLoad ) {
