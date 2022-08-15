2a47ee5b51bdfcb6640024a1ec18eff0500ac8f7
Payton Swick <payton@foolord.com>
2022-08-15 19:32:17 -0400

    Remove unused getActivationCompletedLink

--- client/my-sites/checkout/checkout-thank-you/utils.ts
+++ client/my-sites/checkout/checkout-thank-you/utils.ts
@@ -1,8 +1,3 @@
-import {
-	JETPACK_BACKUP_PRODUCTS,
-	JETPACK_SCAN_PRODUCTS,
-	JETPACK_SEARCH_PRODUCTS,
-} from '@automattic/calypso-products';
 import { domainManagementEdit, domainManagementList } from 'calypso/my-sites/domains/paths';
 
 export function getDomainManagementUrl(
@@ -10,47 +5,4 @@ export function getDomainManagementUrl(
 	domain: string | undefined
 ): string {
 	return domain ? domainManagementEdit( slug, domain ) : domainManagementList( slug );
-}
-
-/*
- * Compute link to send a user after a site-less subscription has been successfully transferred
- * to the user site.
- *
- * @param productSlug string – The slug of a Jetpack product.
- * @param siteSlug string|null – The slug of a site.
- * @param wpAdminUrl string|null – The URL of a site's WP Admin.
- */
-export function getActivationCompletedLink(
-	productSlug: string,
-	siteSlug: string | null | undefined,
-	wpAdminUrl: string | null | undefined
-): string {
-	const baseJetpackCloudUrl = 'https://cloud.jetpack.com';
-
-	if ( ! siteSlug ) {
-		return `${ baseJetpackCloudUrl }/landing`;
-	}
-
-	if (
-		productSlug &&
-		( JETPACK_BACKUP_PRODUCTS as ReadonlyArray< string > ).includes( productSlug )
-	) {
-		return `${ baseJetpackCloudUrl }/backup/${ siteSlug }`;
-	}
-
-	if (
-		productSlug &&
-		( JETPACK_SEARCH_PRODUCTS as ReadonlyArray< string > ).includes( productSlug )
-	) {
-		return `${ baseJetpackCloudUrl }/jetpack-search/${ siteSlug }`;
-	}
-
-	if (
-		productSlug &&
-		( JETPACK_SCAN_PRODUCTS as ReadonlyArray< string > ).includes( productSlug )
-	) {
-		return `${ baseJetpackCloudUrl }/scan/${ siteSlug }`;
-	}
-
-	return wpAdminUrl || `${ baseJetpackCloudUrl }/landing/${ siteSlug }`;
 }
