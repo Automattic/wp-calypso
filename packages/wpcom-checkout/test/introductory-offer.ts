69c2b7a4f0b6b7408bf0d7b8d18b3a3adb535baf
Fredrik Rombach Ekelund <fredrik@fredrik.computer>
2022-08-15 09:04:13 +0200

    Adjust unit test per review suggestions

--- packages/wpcom-checkout/test/introductory-offer.ts
+++ packages/wpcom-checkout/test/introductory-offer.ts
@@ -1,9 +1,10 @@
-import { ResponseCartProduct } from '@automattic/shopping-cart';
+import { getEmptyResponseCartProduct } from '@automattic/shopping-cart';
 import { getItemIntroductoryOfferDisplay } from '../src';
 
-const emptyIntroductoryOffer = { introductory_offer_terms: {} } as ResponseCartProduct;
+const emptyIntroductoryOffer = getEmptyResponseCartProduct();
 
 const disabledIntroductoryOffer = {
+	...getEmptyResponseCartProduct(),
 	introductory_offer_terms: {
 		enabled: false,
 		interval_count: 3,
@@ -12,9 +13,10 @@ const disabledIntroductoryOffer = {
 		should_prorate_when_offer_ends: true,
 		transition_after_renewal_count: 0,
 	},
-} as ResponseCartProduct;
+};
 
 const disabledIntroductoryOfferForAReason = {
+	...getEmptyResponseCartProduct(),
 	introductory_offer_terms: {
 		enabled: false,
 		interval_count: 3,
@@ -23,9 +25,10 @@ const disabledIntroductoryOfferForAReason = {
 		should_prorate_when_offer_ends: true,
 		transition_after_renewal_count: 0,
 	},
-} as ResponseCartProduct;
+};
 
 const enabledIntroductoryOffer = {
+	...getEmptyResponseCartProduct(),
 	introductory_offer_terms: {
 		enabled: true,
 		interval_count: 3,
@@ -33,9 +36,9 @@ const enabledIntroductoryOffer = {
 		should_prorate_when_offer_ends: true,
 		transition_after_renewal_count: 0,
 	},
-} as ResponseCartProduct;
+};
 
-describe( 'Introductory Offer Utils', () => {
+describe( 'getItemIntroductoryOfferDisplay', () => {
 	test( 'should return null when introductory offer is empty', () => {
 		expect( getItemIntroductoryOfferDisplay( emptyIntroductoryOffer ) ).toBeNull();
 	} );
