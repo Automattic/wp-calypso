3008a53e916d1389a153be8f885b1a9c04f29de5
Jeremy Yip <jeremy.yip@automattic.com>
2022-08-16 12:05:45 -0700

    Replace my home url with launchpad url

--- client/state/admin-menu/selectors/index.js
+++ client/state/admin-menu/selectors/index.js
@@ -7,7 +7,29 @@ export function getAdminMenu( state, siteId ) {
 		return null;
 	}
 
-	return state.adminMenu.menus[ siteId ] || null;
+	const menusToFilter = state.adminMenu.menus[ siteId ];
+
+	if ( ! menusToFilter ) {
+		return null;
+	}
+
+	// TODO: Determine whether or not launchpad is still enabled
+	const launchpadEnabled = true;
+
+	if ( launchpadEnabled ) {
+		// Replace my home url with a redirection to launchpad feature
+		return menusToFilter.map( ( menu ) => {
+			if ( menu?.url?.match( /^\/home/ ) ) {
+				// TODO: Determine which onboarding flow user is handling
+				// We currently hardcode the flow as "newsletter"
+				menu.url = menu.url.replace( /^\/home\//, '/setup/launchpad?flow=newsletter&siteSlug=' );
+			}
+
+			return menu;
+		} );
+	}
+
+	return menusToFilter;
 }
 
 export function getIsRequestingAdminMenu( state ) {
