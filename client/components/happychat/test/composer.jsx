9cde6d6bdba0f446c70e5bc37b7906cc2763f209
Markus Dobmann <m.do@posteo.net>
2022-08-16 16:28:42 +0200

    `Happychat/Composer`: refactor tests to `@testing-library/react`

--- client/components/happychat/test/composer.jsx
+++ client/components/happychat/test/composer.jsx
@@ -2,18 +2,21 @@
  * @jest-environment jsdom
  */
 
-import { mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useState } from 'react';
 import Composer from '../composer';
 
 const noop = () => {};
 
 describe( '<Composer />', () => {
 	describe( 'onChange event', () => {
-		test( 'should call onSetCurrentMessage property and send a typing event if message is not empty', () => {
+		test( 'should call onSetCurrentMessage property and send a typing event if message is not empty', async () => {
+			const user = userEvent.setup();
 			const onSendNotTyping = jest.fn();
 			const onSendTyping = jest.fn();
 			const onSetCurrentMessage = jest.fn();
-			const wrapper = mount(
+			render(
 				<Composer
 					message={ 'hey' }
 					onSetCurrentMessage={ onSetCurrentMessage }
@@ -22,26 +25,34 @@ describe( '<Composer />', () => {
 					translate={ noop }
 				/>
 			);
-			wrapper.find( 'textarea' ).simulate( 'change', { target: { value: 'hey' } } );
+			const el = screen.getByRole( 'textbox', { name: /support request/i } );
+			await user.type( el, 'hey' );
 			expect( onSetCurrentMessage ).toHaveBeenCalled();
 			expect( onSendTyping ).toHaveBeenCalled();
 			expect( onSendNotTyping ).not.toHaveBeenCalled();
 		} );
 
-		test( 'should call onSetCurrentMessage property and send a noTyping event if message is empty', () => {
+		test( 'should call onSetCurrentMessage property and send a noTyping event if message is empty', async () => {
+			const user = userEvent.setup();
 			const onSendNotTyping = jest.fn();
 			const onSendTyping = jest.fn();
 			const onSetCurrentMessage = jest.fn();
-			const wrapper = mount(
-				<Composer
-					message={ '' }
+			const WrappedWithMessageState = ( props ) => {
+				const [ message, setMessage ] = useState( 'x' );
+				props.onSetCurrentMessage.mockImplementation( ( value ) => setMessage( value ) );
+				return <Composer { ...props } message={ message } />;
+			};
+			render(
+				<WrappedWithMessageState
 					onSetCurrentMessage={ onSetCurrentMessage }
 					onSendTyping={ onSendTyping }
 					onSendNotTyping={ onSendNotTyping }
 					translate={ noop }
 				/>
 			);
-			wrapper.find( 'textarea' ).simulate( 'change', { target: { value: '' } } );
+			const el = screen.getByRole( 'textbox', { name: /support request/i } );
+			await user.click( el );
+			await user.keyboard( '{Backspace}' );
 			expect( onSetCurrentMessage ).toHaveBeenCalled();
 			expect( onSendTyping ).not.toHaveBeenCalled();
 			expect( onSendNotTyping ).toHaveBeenCalled();
@@ -49,10 +60,11 @@ describe( '<Composer />', () => {
 	} );
 
 	describe( 'onKeyDown event', () => {
-		test( 'should call message and noTyping props if message is not empty', () => {
+		test( 'should call message and noTyping props if message is not empty', async () => {
+			const user = userEvent.setup();
 			const onSendMessage = jest.fn();
 			const onSendNotTyping = jest.fn();
-			const wrapper = mount(
+			render(
 				<Composer
 					message={ 'hey' }
 					onSendMessage={ onSendMessage }
@@ -60,23 +72,27 @@ describe( '<Composer />', () => {
 					translate={ noop }
 				/>
 			);
-			wrapper.find( 'textarea' ).simulate( 'keydown', { which: 13, preventDefault: () => {} } );
+			const btn = screen.getByRole( 'button' );
+			await user.click( btn );
 			expect( onSendMessage ).toHaveBeenCalled();
 			expect( onSendNotTyping ).toHaveBeenCalled();
 		} );
 
-		test( 'should call message and noTyping props if message is empty', () => {
+		test( 'should call message and noTyping props if message is empty', async () => {
+			const user = userEvent.setup();
 			const onSendMessage = jest.fn();
 			const onSendNotTyping = jest.fn();
-			const wrapper = mount(
+			render(
 				<Composer
 					message={ '' }
+					onSetCurrentMessage={ ( ...args ) => console.log( args ) }
 					onSendMessage={ onSendMessage }
 					onSendNotTyping={ onSendNotTyping }
 					translate={ noop }
 				/>
 			);
-			wrapper.find( 'textarea' ).simulate( 'keydown', { which: 13, preventDefault: () => {} } );
+			const btn = screen.getByRole( 'button' );
+			await user.click( btn );
 			expect( onSendMessage ).not.toHaveBeenCalled();
 			expect( onSendNotTyping ).not.toHaveBeenCalled();
 		} );
