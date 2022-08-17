f8729c8f4145a1e8e0aeed1b2682f2dfb0b4ddd8
Rolando Perez <me@rolilink.com>
2022-08-17 12:33:45 -0500

    Extracts Notice logic into the JetpackDisconnectedNotice component (#66631)
    * Extracts Notice logic into the PluginsNoticeSection component
    
    * adds readme file
    
    * Rename PluginsNoticeSection to JetpackDisconnectedNotice

--- client/my-sites/plugins/plugins-browser/index.jsx
+++ client/my-sites/plugins/plugins-browser/index.jsx
@@ -14,8 +14,6 @@ import QueryJetpackPlugins from 'calypso/components/data/query-jetpack-plugins';
 import QueryProductsList from 'calypso/components/data/query-products-list';
 import FixedNavigationHeader from 'calypso/components/fixed-navigation-header';
 import MainComponent from 'calypso/components/main';
-import Notice from 'calypso/components/notice';
-import NoticeAction from 'calypso/components/notice/notice-action';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
 import useScrollAboveElement from 'calypso/lib/use-scroll-above-element';
 import Categories from 'calypso/my-sites/plugins/categories';
@@ -25,16 +23,11 @@ import NoPermissionsError from 'calypso/my-sites/plugins/no-permissions-error';
 import PluginsAnnouncementModal from 'calypso/my-sites/plugins/plugins-announcement-modal';
 import SearchBoxHeader from 'calypso/my-sites/plugins/search-box-header';
 import { siteObjectsToSiteIds } from 'calypso/my-sites/plugins/utils';
-import {
-	recordTracksEvent,
-	recordGoogleEvent,
-	composeAnalytics,
-} from 'calypso/state/analytics/actions';
+import { recordTracksEvent, recordGoogleEvent } from 'calypso/state/analytics/actions';
 import { updateBreadcrumbs } from 'calypso/state/breadcrumb/actions';
 import { getBreadcrumbs } from 'calypso/state/breadcrumb/selectors';
 import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
 import getSelectedOrAllSitesJetpackCanManage from 'calypso/state/selectors/get-selected-or-all-sites-jetpack-can-manage';
-import getSiteConnectionStatus from 'calypso/state/selectors/get-site-connection-status';
 import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 import isVipSite from 'calypso/state/selectors/is-vip-site';
 import siteHasFeature from 'calypso/state/selectors/site-has-feature';
@@ -49,6 +42,7 @@ import {
 	getSelectedSite,
 	getSelectedSiteSlug,
 } from 'calypso/state/ui/selectors';
+import JetpackDisconnectedNotice from '../jetpack-disconnected-notice';
 import PluginsCategoryResultsPage from '../plugins-category-results-page';
 import PluginsDiscoveryPage from '../plugins-discovery-page';
 import PluginsSearchResultPage from '../plugins-search-results-page';
@@ -149,9 +143,7 @@ const PluginsBrowser = ( { trackPageViews = true, category, search, hideHeader }
 		( state ) =>
 			isJetpackSite( state, selectedSite?.ID ) && ! isAtomicSite( state, selectedSite?.ID )
 	);
-	const isSiteConnected = useSelector( ( state ) =>
-		getSiteConnectionStatus( state, selectedSite?.ID )
-	);
+
 	const isVip = useSelector( ( state ) => isVipSite( state, selectedSite?.ID ) );
 	const isRequestingSitesData = useSelector( isRequestingSites );
 	const noPermissionsError = useSelector(
@@ -249,12 +241,6 @@ const PluginsBrowser = ( { trackPageViews = true, category, search, hideHeader }
 		dispatch( updateBreadcrumbs( items ) );
 	}, [ siteSlug, search, category, categoryName, dispatch, translate ] );
 
-	const trackSiteDisconnect = () =>
-		composeAnalytics(
-			recordGoogleEvent( 'Jetpack', 'Clicked in site indicator to start Jetpack Disconnect flow' ),
-			recordTracksEvent( 'calypso_jetpack_site_indicator_disconnect_start' )
-		);
-
 	if ( ! isRequestingSitesData && noPermissionsError ) {
 		return <NoPermissionsError title={ translate( 'Plugins', { textOnly: true } ) } />;
 	}
@@ -295,25 +281,7 @@ const PluginsBrowser = ( { trackPageViews = true, category, search, hideHeader }
 					</div>
 				</FixedNavigationHeader>
 			) }
-			{ isSiteConnected === false && (
-				<Notice
-					icon="notice"
-					showDismiss={ false }
-					status="is-warning"
-					text={ translate( '%(siteName)s cannot be accessed.', {
-						textOnly: true,
-						args: { siteName: selectedSite.title },
-					} ) }
-				>
-					<NoticeAction
-						onClick={ trackSiteDisconnect }
-						href={ `/settings/disconnect-site/${ selectedSite.slug }?type=down` }
-					>
-						{ translate( 'I’d like to fix this now' ) }
-					</NoticeAction>
-				</Notice>
-			) }
-
+			<JetpackDisconnectedNotice />
 			<SearchBoxHeader
 				searchRef={ searchRef }
 				popularSearchesRef={ searchHeaderRef }
