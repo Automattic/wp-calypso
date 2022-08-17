93648de62e0f43184f162a1b1615f31764657b78
Andres Blanco <andresblanco@gmail.com>
2022-08-17 15:01:37 -0300

    gate behind flag

--- client/landing/stepper/declarative-flow/blazepress-flow.ts
+++ client/landing/stepper/declarative-flow/blazepress-flow.ts
@@ -1,5 +1,6 @@
+import { isEnabled } from '@automattic/calypso-config';
+import { AssertConditionResult, AssertConditionState, Flow } from './internals/types';
 import type { StepPath } from './internals/steps-repository';
-import type { Flow } from './internals/types';
 
 export const blazePressFlow: Flow = {
 	name: 'blazepress',
@@ -30,5 +31,19 @@ export const blazePressFlow: Flow = {
 		};
 
 		return { goNext, goBack, goToStep, submit };
+	},
+
+	useAssertConditions(): AssertConditionResult {
+		let result: AssertConditionResult = {
+			state: AssertConditionState.SUCCESS,
+		};
+
+		if ( ! isEnabled( 'promote-post' ) ) {
+			result = {
+				state: AssertConditionState.FAILURE,
+				message: 'this is not enabled yet',
+			};
+		}
+		return result;
 	},
 };
