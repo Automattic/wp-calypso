86653afced619fdac5d0897cfbe685294b71824d
Claudiu Filip <claudiucelfilip@gmail.com>
2022-08-17 15:04:24 +0300

    Remove dedicated relative path localizing function

--- packages/i18n-utils/src/index.ts
+++ packages/i18n-utils/src/index.ts
@@ -1,5 +1,4 @@
 export { localizeUrl, useLocalizeUrl, withLocalizeUrl } from './localize-url';
-export { localizePath, useLocalizePath, withLocalizePath } from './localize-path';
 export { LocaleProvider, useLocale, withLocale, useIsEnglishLocale } from './locale-context';
 export * from './locales';
 export { default as guessTimezone } from './guess-timezone';
