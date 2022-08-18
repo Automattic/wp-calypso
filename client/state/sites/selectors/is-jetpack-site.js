4617f90d15bd44d8d57a0b766e87c3dab4046f8a
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-08-18 15:49:31 +0530

    FIX | Jetpack sites with standalone plugins are mistakenly recognised as WPCOM sites in Calypso (#66648)
    * Convert isJetpackSite to TypeScript and enhance it
    
    * RIP isJetpackProductSite
    
    * RIP hasJetpackPluginActiveConnection
    
    * Add unit tests for isJetpackSite
    
    * Lord TS
    
    * Update is-jetpack-site.js
    
    * Improve isJetpackSite signature

--- client/state/sites/selectors/is-jetpack-site.js
+++ client/state/sites/selectors/is-jetpack-site.js
@@ -1,18 +1,39 @@
 import getRawSite from 'calypso/state/selectors/get-raw-site';
+import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 
 /**
  * Returns true if site is a Jetpack site, false if the site is hosted on
  * WordPress.com, or null if the site is unknown.
  *
- * @param  {object}   state  Global state tree
+ * @param {import('calypso/types').AppState} state Global state tree
  * @param  {?number}   siteId Site ID
- * @returns {?boolean}        Whether site is a Jetpack site
+ * @param  {?{ considerStandaloneProducts?: boolean }}  options Whether to consider sites with Jetpack standalone plugins installed
+ * @returns {boolean | null} Whether site is a Jetpack site
  */
-export default function isJetpackSite( state, siteId ) {
+export default function isJetpackSite(
+	state,
+	siteId,
+	options = { considerStandaloneProducts: true }
+) {
 	const site = getRawSite( state, siteId );
 	if ( ! site ) {
 		return null;
 	}
 
-	return site.jetpack;
+	// Sites with full Jetpack plugin have a boolean `jetpack` property.
+	if ( site.jetpack ) {
+		return true;
+	}
+
+	// if it's an atomic site, return false
+	if ( isAtomicSite( state, siteId ) ) {
+		return false;
+	}
+
+	// If we should not consider standalone products
+	if ( ! options.considerStandaloneProducts ) {
+		return false;
+	}
+
+	return Boolean( site.options?.jetpack_connection_active_plugins?.length );
 }
