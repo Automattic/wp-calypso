04a7e29dd52498dfede18ba12a954452d5a51c2c
Claudiu Filip <claudiucelfilip@gmail.com>
2022-08-18 17:29:42 +0300

    Update/plan features grid shuffle #2 (#66696)
    * Make plans grid support blank features
    
    * Add alternative feature list
    
    * Add info popover
    
    * Show feature sets based on experiment variation
    
    * Restrict experiment to EN locales
    
    * Address missassigned features and use dynamic prices
    
    * Implement dashes, feature grouping and UI tweaks
    
    * Fix left alignment for crossed feature text
    
    * Apply localized pricing to Basic CSS tools on mobile layout
    
    * Use title as tooltip for features with empty descriptions
    
    * Reshuffled the complete plan feature list
    
    * Reshuffled the complete plan feature list
    
    * Remove tooltips from signup flow plans page
    
    * Re-adding tooltips and pruning the feature list
    
    * Adding feature description for custom CSS add-on
    
    * Adding padding to discount message styling
    
    * Switch plan feature lists inside the same plans features comparison function
    
    * Temporary force the treatment variation for e2e test
    
    * Revert "Temporary force the treatment variation for e2e test"
    
    This reverts commit d2361a87e61b9446f08f286bca7ee8d9324485d4.
    
    Co-authored-by: Don Lair <don.lair@gmail.com>

--- client/my-sites/plan-features-comparison/item.jsx
+++ client/my-sites/plan-features-comparison/item.jsx
@@ -1,5 +1,7 @@
 import { Gridicon } from '@automattic/components';
+import { useMobileBreakpoint } from '@automattic/viewport-react';
 import classNames from 'classnames';
+import InfoPopover from 'calypso/components/info-popover';
 
 export function PlanFeaturesItem( props ) {
 	const itemInfoClasses = classNames( 'plan-features-comparison__item-info-container', {
@@ -10,6 +12,8 @@ export function PlanFeaturesItem( props ) {
 		'plan-features-comparison__item-checkmark': props.isFeatureAvailable,
 		'plan-features-comparison__item-cross': ! props.isFeatureAvailable,
 	} );
+	const isMobile = useMobileBreakpoint();
+
 	return (
 		<div className="plan-features-comparison__item plan-features-comparison__item-available">
 			{ props.annualOnlyContent && (
@@ -18,8 +22,18 @@ export function PlanFeaturesItem( props ) {
 				</div>
 			) }
 			<div className={ itemInfoClasses }>
-				<Gridicon className={ gridIconClasses } size={ 18 } icon={ icon } />
+				{ props.hideGridicon ? null : (
+					<Gridicon className={ gridIconClasses } size={ 18 } icon={ icon } />
+				) }
 				{ props.children }
+				{ props.hideInfoPopover ? null : (
+					<InfoPopover
+						className="plan-features-comparison__item-tip-info plan-features__item-tip-info"
+						position={ isMobile ? 'left' : 'right' }
+					>
+						{ props.description }
+					</InfoPopover>
+				) }
 			</div>
 		</div>
 	);
