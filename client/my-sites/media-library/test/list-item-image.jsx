6bd5a524cbb7f6a6b55a3cbf74342be761712cd1
vikneshwar <lviknesh1993@gmail.com>
2022-08-19 16:30:14 +0530

    Migrate media-library tests from enzyme to react-testing-library

--- client/my-sites/media-library/test/list-item-image.jsx
+++ client/my-sites/media-library/test/list-item-image.jsx
@@ -1,22 +1,18 @@
 /**
  * @jest-environment jsdom
  */
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import resize from 'calypso/lib/resize-image-url';
 import ListItemImage from 'calypso/my-sites/media-library/list-item-image';
 import fixtures from './fixtures';
 
 const WIDTH = 450;
 
+jest.mock( 'calypso/my-sites/media-library/media-image', () => {
+	return ( props ) => <div data-testid={ props.src }>{ props.children }</div>;
+} );
+
 describe( 'MediaLibraryListItem image', () => {
-	let wrapper;
-
-	beforeEach( () => {
-		if ( wrapper ) {
-			wrapper.unmount();
-		}
-	} );
-
 	const getResizedUrl = () =>
 		resize( fixtures.media[ 0 ].URL, { resize: `${ WIDTH },${ WIDTH }` } );
 	const getItem = ( itemPos, type ) => (
@@ -31,26 +27,26 @@ describe( 'MediaLibraryListItem image', () => {
 
 	describe( 'thumbnail display mode', () => {
 		test( 'defaults to photon when no thumbnail parameter is passed', () => {
-			wrapper = shallow( getItem( 0 ) );
+			render( getItem( 0 ) );
 
-			expect( wrapper.props().src ).toEqual( getResizedUrl() );
+			expect( screen.getByTestId( getResizedUrl() ) ).toBeInTheDocument();
 		} );
 		test( 'returns a resized private thumbnail for type MEDIA_IMAGE_RESIZER', () => {
-			wrapper = shallow( getItem( 0, 'MEDIA_IMAGE_RESIZER' ) );
+			render( getItem( 0, 'MEDIA_IMAGE_RESIZER' ) );
 
-			expect( wrapper.props().src ).toEqual( getResizedUrl() );
+			expect( screen.getByTestId( getResizedUrl() ) ).toBeInTheDocument();
 		} );
 
 		test( 'returns existing medium thumbnail for type MEDIA_IMAGE_THUMBNAIL', () => {
-			wrapper = shallow( getItem( 0, 'MEDIA_IMAGE_THUMBNAIL' ) );
+			render( getItem( 0, 'MEDIA_IMAGE_THUMBNAIL' ) );
 
-			expect( wrapper.props().src ).toEqual( fixtures.media[ 0 ].thumbnails.medium );
+			expect( screen.getByTestId( fixtures.media[ 0 ].thumbnails.medium ) ).toBeInTheDocument();
 		} );
 
 		test( 'returns resized thumbnail for type MEDIA_IMAGE_THUMBNAIL when no medium thumbnail', () => {
-			wrapper = shallow( getItem( 1, 'MEDIA_IMAGE_THUMBNAIL' ) );
+			render( getItem( 1, 'MEDIA_IMAGE_THUMBNAIL' ) );
 
-			expect( wrapper.props().src ).toEqual( getResizedUrl() );
+			expect( screen.getByTestId( getResizedUrl() ) ).toBeInTheDocument();
 		} );
 	} );
 } );
