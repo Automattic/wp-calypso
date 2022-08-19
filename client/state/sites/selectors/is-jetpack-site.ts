b36411aeb0a028f7b1f64a4e56abdc961c427a3f
Payton Swick <payton@foolord.com>
2022-08-19 17:21:19 -0400

    Allow options to be defined without considerStandaloneProducts

--- client/state/sites/selectors/is-jetpack-site.ts
+++ client/state/sites/selectors/is-jetpack-site.ts
@@ -2,6 +2,10 @@ import getRawSite from 'calypso/state/selectors/get-raw-site';
 import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 import type { AppState } from 'calypso/types';
 
+const defaultOptions = {
+	considerStandaloneProducts: true,
+};
+
 /**
  * Returns true if site is a Jetpack site, false if the site is hosted on
  * WordPress.com, or null if the site is unknown.
@@ -13,11 +17,14 @@ import type { AppState } from 'calypso/types';
 export default function isJetpackSite(
 	state: AppState,
 	siteId: number | undefined | null,
-	options = { considerStandaloneProducts: true }
+	options?: { considerStandaloneProducts?: boolean }
 ): boolean | null {
 	if ( ! siteId ) {
 		return null;
 	}
+
+	// Merge default options with options.
+	options = options ? { ...defaultOptions, ...options } : defaultOptions;
 
 	const site = getRawSite( state, siteId );
 	if ( ! site ) {
