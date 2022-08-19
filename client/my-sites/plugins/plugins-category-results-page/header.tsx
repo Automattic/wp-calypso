70872177801dfc18d2f82b9d5e269ef40db16522
Candy Tsai <candy02058912@gmail.com>
2022-08-19 16:26:50 +0800

    Update styles

--- client/my-sites/plugins/plugins-category-results-page/header.tsx
+++ client/my-sites/plugins/plugins-category-results-page/header.tsx
@@ -12,8 +12,8 @@ const Header = ( {
 	return (
 		<div className="plugin-category-results-header">
 			<h1 className="plugin-category-results-header__title">{ title }</h1>
-			<p className="plugin-category-results-header__count">{ count }</p>
 			{ subtitle && <h2 className="plugin-category-results-header__subtitle">{ subtitle }</h2> }
+			<p className="plugin-category-results-header__count">{ count }</p>
 		</div>
 	);
 };
