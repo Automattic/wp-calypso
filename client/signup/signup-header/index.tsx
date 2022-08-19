caaa0456fd60bb237667344dcefc9a249e7d825f
Tony Arcangelini <tony@arcangelini.com>
2022-08-20 01:11:24 +0200

    oops forgot a file

---
+++ client/signup/signup-header/index.tsx
@@ -0,0 +1,50 @@
+import { useFlowProgress } from '@automattic//onboarding';
+import { ProgressBar } from '@automattic/components';
+import classnames from 'classnames';
+import WordPressLogo from 'calypso/components/wordpress-logo';
+import './style.scss';
+
+interface Progress {
+	flowName: string;
+	stepName: string;
+}
+
+interface Props {
+	shouldShowLoadingScreen: boolean;
+	isReskinned: boolean;
+	rightComponent: Node;
+	pageTitle: string;
+	progressBar: Progress;
+}
+
+const SignupHeader = ( {
+	pageTitle,
+	shouldShowLoadingScreen,
+	isReskinned,
+	rightComponent,
+	progressBar,
+}: Props ) => {
+	const logoClasses = classnames( 'wordpress-logo', {
+		'is-large': shouldShowLoadingScreen && ! isReskinned,
+	} );
+	const flowProgress = useFlowProgress( progressBar );
+
+	return (
+		<div className="signup-header">
+			{ progressBar && (
+				<ProgressBar
+					className={ progressBar.flowName }
+					value={ flowProgress.position }
+					total={ flowProgress.count }
+				/>
+			) }
+			<WordPressLogo size={ 120 } className={ logoClasses } />
+			{ pageTitle && <h1>{ pageTitle }</h1> }
+			{ /* This should show a sign in link instead of
+			   the progressIndicator on the account step. */ }
+			<div className="signup-header__right">{ ! shouldShowLoadingScreen && rightComponent }</div>
+		</div>
+	);
+};
+
+export default SignupHeader;
