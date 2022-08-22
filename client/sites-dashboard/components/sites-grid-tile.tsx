5600f293951910f8982d0bcf0f9161359c0d65b7
vykes-mac <knrymcleish@gmail.com>
2022-08-22 17:41:55 -0500

    add animation to sites grid on hover

--- client/sites-dashboard/components/sites-grid-tile.tsx
+++ client/sites-dashboard/components/sites-grid-tile.tsx
@@ -6,6 +6,23 @@ const container = css( {
 	width: '100%',
 	flexDirection: 'column',
 	minWidth: 0,
+	transition: 'all 0.3s ease-in-out',
+	':hover': {
+		transform: 'scale(1.07, 1.07)',
+		':after': {
+			//opacity: 1,
+		},
+	},
+	':after': {
+		content: '""',
+		position: 'absolute',
+		zIndex: -1,
+		height: '100%',
+		width: '100%',
+		opacity: 0,
+		boxShadow: '0 5px 15px rgba(0,0,0,0.1)',
+		transition: 'opacity 0.2s ease-in-out',
+	},
 } );
 
 const primaryContainer = css( {
