c9013c7af3a3b22b2baefdc1acbc11e1a0a5c04b
Philip Jackson <p-jackson@live.com>
2022-08-23 09:19:58 +1200

    Sites Management Page: Set header font weight back to "normal" (and potentially set a border on sticky header) (#66792)
    Co-authored-by: Daniel Bachhuber <daniel.bachhuber@automattic.com>

--- client/sites-dashboard/components/sites-table.tsx
+++ client/sites-dashboard/components/sites-table.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { useI18n } from '@wordpress/react-i18n';
-import { useLayoutEffect, useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 import { MEDIA_QUERIES } from '../utils';
 import SitesTableRow from './sites-table-row';
 import SitesTableRowLoading from './sites-table-row-loading';
@@ -32,6 +32,11 @@ const THead = styled.thead< { top: number } >( ( { top } ) => ( {
 	background: '#fdfdfd',
 } ) );
 
+const headerShadow: React.CSSProperties = {
+	boxShadow: '0 0 13px -9px rgba(0, 0, 0, 0.45)',
+	clipPath: 'inset( 0 0 -10px 0 )',
+};
+
 const Row = styled.tr`
 	line-height: 2em;
 	border-bottom: 1px solid #eee;
@@ -43,7 +48,7 @@ const Row = styled.tr`
 		font-size: 14px;
 		line-height: 20px;
 		letter-spacing: -0.24px;
-		font-weight: 500;
+		font-weight: normal;
 		color: var( --studio-gray-60 );
 	}
 `;
@@ -51,8 +56,11 @@ const Row = styled.tr`
 export function SitesTable( { className, sites, isLoading = false }: SitesTableProps ) {
 	const { __ } = useI18n();
 
+	const headerRef = useRef< HTMLTableSectionElement >( null );
+	const [ isHeaderStuck, setIsHeaderStuck ] = useState( false );
 	const [ masterbarHeight, setMasterbarHeight ] = useState( 0 );
 
+	// Measure height of masterbar as we need it for the THead styles
 	useLayoutEffect( () => {
 		const masterbarElement = document.querySelector< HTMLDivElement >( 'header.masterbar' );
 
@@ -85,9 +93,34 @@ export function SitesTable( { className, sites, isLoading = false }: SitesTableP
 		};
 	}, [] );
 
+	// Detect when the header becomes "sticky" so we can show the shadow
+	useLayoutEffect( () => {
+		if ( ! headerRef.current || ! window.IntersectionObserver ) {
+			return;
+		}
+
+		const observer = new IntersectionObserver(
+			( [ entry ] ) => setIsHeaderStuck( entry.intersectionRatio < 1 ),
+			{
+				rootMargin: `-${ masterbarHeight + 1 }px 0px 0px 0px`,
+				threshold: [ 1 ],
+			}
+		);
+
+		observer.observe( headerRef.current );
+
+		return () => {
+			observer.disconnect();
+		};
+	}, [ masterbarHeight ] );
+
 	return (
 		<Table className={ className }>
-			<THead top={ masterbarHeight }>
+			<THead
+				top={ masterbarHeight }
+				ref={ headerRef }
+				style={ isHeaderStuck ? headerShadow : undefined }
+			>
 				<Row>
 					<th style={ { width: '50%' } }>{ __( 'Site' ) }</th>
 					<th style={ { width: '20%' } }>{ __( 'Plan' ) }</th>
