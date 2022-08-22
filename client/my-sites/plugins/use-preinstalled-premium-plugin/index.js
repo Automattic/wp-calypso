6e618ae8ee1e36ccd3c6231ec8aa3a7893628a32
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2022-08-22 16:58:51 +0300

    Check if the product is a Jetpack module and check if it's active.

--- client/my-sites/plugins/use-preinstalled-premium-plugin/index.js
+++ client/my-sites/plugins/use-preinstalled-premium-plugin/index.js
@@ -1,12 +1,12 @@
 import { useSelector } from 'react-redux';
 import { siteObjectsToSiteIds } from 'calypso/my-sites/plugins/utils';
 import { getBillingInterval } from 'calypso/state/marketplace/billing-interval/selectors';
-import { getPluginOnSite, getSitesWithPlugin } from 'calypso/state/plugins/installed/selectors';
+import { getSitesWithPlugin } from 'calypso/state/plugins/installed/selectors';
 import getSelectedOrAllSitesJetpackCanManage from 'calypso/state/selectors/get-selected-or-all-sites-jetpack-can-manage';
 import isPluginActive from 'calypso/state/selectors/is-plugin-active';
 import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
 import siteHasFeature from 'calypso/state/selectors/site-has-feature';
-import { isJetpackSite } from 'calypso/state/sites/selectors';
+import { isJetpackModuleActive, isJetpackSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { PREINSTALLED_PREMIUM_PLUGINS } from '../constants';
 import { getPeriodVariationValue } from '../plugin-price';
@@ -25,8 +25,8 @@ export default function usePreinstalledPremiumPlugin( pluginSlug ) {
 	);
 
 	const isPreinstalledPremiumPluginActive = useSelector( ( state ) => {
-		if ( preinstalledPremiumPlugin && selectedSiteId ) {
-			return true;
+		if ( ! preinstalledPremiumPlugin || ! selectedSiteId ) {
+			return false;
 		}
 
 		// Always active on simple sites
@@ -36,11 +36,16 @@ export default function usePreinstalledPremiumPlugin( pluginSlug ) {
 		) {
 			return true;
 		}
-		// Always active on atomic sites that don't have the plugin installed
-		const pluginOnSite = getPluginOnSite( state, selectedSiteId, pluginSlug );
-		if ( isSiteAutomatedTransfer( state, selectedSiteId ) && ! pluginOnSite ) {
+
+		// If the preinstalled premium plugin is also a Jetpack module, check if it's enabled.
+		if (
+			preinstalledPremiumPlugin.jetpack_module &&
+			isJetpackSite( state, selectedSiteId ) &&
+			isJetpackModuleActive( state, selectedSiteId, preinstalledPremiumPlugin.jetpack_module )
+		) {
 			return true;
 		}
+
 		return isPluginActive( state, selectedSiteId, pluginSlug );
 	} );
 
