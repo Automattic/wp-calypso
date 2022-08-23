ce8d1bf62a2629e13d4347bb2c3bedb5b52d9bae
Erick Danzer <edanzer@gmail.com>
2022-08-23 16:42:02 -0600

    Launchpad: Ensure Sidebar strings are dynamic and translated

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/sidebar.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/sidebar.tsx
@@ -2,6 +2,7 @@ import WordPressLogo from 'calypso/components/wordpress-logo';
 import { useFlowParam } from 'calypso/landing/stepper/hooks/use-flow-param';
 import Checklist from './checklist';
 import { tasks } from './tasks';
+import { getLaunchpadTranslations } from './translations';
 
 function getUrlInfo( url: string ) {
 	const urlWithoutProtocol = url.replace( /^https?:\/\//, '' );
@@ -18,6 +19,7 @@ const Sidebar = ( { siteSlug }: { siteSlug: string | null } ) => {
 	let siteName = '';
 	let topLevelDomain = '';
 	const flow = useFlowParam();
+	const translatedStrings = getLaunchpadTranslations( flow );
 
 	if ( siteSlug ) {
 		[ siteName, topLevelDomain ] = getUrlInfo( siteSlug );
@@ -27,7 +29,7 @@ const Sidebar = ( { siteSlug }: { siteSlug: string | null } ) => {
 		<div className="launchpad__sidebar">
 			<div className="launchpad__sidebar-header">
 				<WordPressLogo className="launchpad__sidebar-header-logo" size={ 24 } />
-				<span className="launchpad__sidebar-header-flow-name">Newsletter</span>
+				<span className="launchpad__sidebar-header-flow-name">{ translatedStrings.flowName }</span>
 			</div>
 			<div className="launchpad__sidebar-content-container">
 				<div className="launchpad__progress-bar-container">
@@ -37,10 +39,8 @@ const Sidebar = ( { siteSlug }: { siteSlug: string | null } ) => {
 					</div>
 				</div>
 				{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace*/ }
-				<h1 className="launchpad__sidebar-h1">Voilà! Your Newsletter is up and running!</h1>
-				<p className="launchpad__sidebar-description">
-					Keep up the momentum with these next steps.
-				</p>
+				<h1 className="launchpad__sidebar-h1">{ translatedStrings.sidebarTitle }</h1>
+				<p className="launchpad__sidebar-description">{ translatedStrings.sidebarSubtitle }</p>
 				<div className="launchpad__url-box">
 					<span>{ siteName }</span>
 					<span className="launchpad__url-box-top-level-domain">{ topLevelDomain }</span>
