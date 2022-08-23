d8d630e716a33aad10cdd3cb61eff8b36b11df8b
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-08-23 16:21:33 +0800

    Display FAQ section in the new pricing page. (#66810)
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>

--- client/my-sites/plans/jetpack-plans/product-store/index.tsx
+++ client/my-sites/plans/jetpack-plans/product-store/index.tsx
@@ -1,8 +1,14 @@
+import StoreFooter from 'calypso/jetpack-connect/store-footer';
 import { UserLicensesDialog } from './user-licenses-dialog';
 import type { ProductStoreProps } from './types';
 
 const ProductStore: React.FC< ProductStoreProps > = ( { enableUserLicensesDialog } ) => {
-	return <div>{ ( enableUserLicensesDialog || 'ok' ) && <UserLicensesDialog /> }</div>;
+	return (
+		<div>
+			{ enableUserLicensesDialog && <UserLicensesDialog /> }
+			<StoreFooter />
+		</div>
+	);
 };
 
 export default ProductStore;
