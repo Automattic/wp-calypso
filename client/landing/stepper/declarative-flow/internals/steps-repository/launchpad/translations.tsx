f9a25330d798855e38f6252f01850e4a091b277a
edanzer <edanzer@gmail.com>
2022-08-23 14:15:44 -0600

    Update text strings to match latest Figma

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/translations.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/translations.tsx
@@ -4,22 +4,22 @@ import { TranslatedLaunchpadStrings } from './types';
 export function getLaunchpadTranslations( flow: string | null ): TranslatedLaunchpadStrings {
 	const translatedStrings = {
 		flowName: translate( 'WordPress' ),
-		sidebarTitle: translate( 'Voilà! Your website is up and running!' ),
-		sidebarSubtitle: translate( 'Keep up the momentum with these next steps.' ),
+		sidebarTitle: translate( 'Voilà! Your website is ready to launch!' ),
+		sidebarSubtitle: translate( 'Keep up the momentum with these final steps.' ),
 	};
 
 	switch ( flow ) {
 		case 'newsletter':
 			translatedStrings.flowName = translate( 'Newsletter' );
-			translatedStrings.sidebarTitle = translate( 'Voilà! Your Newsletter is up and running!' );
+			translatedStrings.sidebarTitle = translate( 'Voilà! Your Newsletter is ready to launch!' );
 			break;
 		case 'link-in-bio':
 			translatedStrings.flowName = translate( 'Link in Bio' );
-			translatedStrings.sidebarTitle = translate( 'Voilà! Your Link in Bio is up and running!' );
+			translatedStrings.sidebarTitle = translate( 'Voilà! Your Link in Bio is ready to launch!' );
 			break;
 		case 'podcast':
 			translatedStrings.flowName = translate( 'Podcast' );
-			translatedStrings.sidebarTitle = translate( 'Voilà! Your Podcast is up and running!' );
+			translatedStrings.sidebarTitle = translate( 'Voilà! Your Podcast is ready to launch!' );
 			break;
 	}
 
