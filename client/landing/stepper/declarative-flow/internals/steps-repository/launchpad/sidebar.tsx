ca70eb587ce6b7ea8006c8d5e31a2570e175a5b1
edanzer <edanzer@gmail.com>
2022-08-23 10:50:54 -0600

    Make Launchpad sidebar text dynamic and translated

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/sidebar.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/sidebar.tsx
@@ -1,19 +1,23 @@
+import { useTranslate } from 'i18n-calypso';
 import WordPressLogo from 'calypso/components/wordpress-logo';
 import { useFlowParam } from 'calypso/landing/stepper/hooks/use-flow-param';
 import { useSite } from 'calypso/landing/stepper/hooks/use-site';
+import { useTranslateFlowName } from 'calypso/landing/stepper/hooks/use-translate-flow-name';
 import Checklist from './checklist';
 import { tasks } from './tasks';
 
 const Sidebar = ( { siteSlug }: { siteSlug: string | null } ) => {
+	const translate = useTranslate();
 	const site = useSite();
 	const url = site?.URL?.replace( /^https?:\/\//, '' );
 	const flow = useFlowParam();
+	const translatedFlowName = useTranslateFlowName( flow );
 
 	return (
 		<div className="launchpad__sidebar">
 			<div className="launchpad__sidebar-header">
 				<WordPressLogo className="launchpad__sidebar-header-logo" size={ 24 } />
-				<span className="launchpad__sidebar-header-flow-name">Newsletter</span>
+				<span className="launchpad__sidebar-header-flow-name">{ translatedFlowName }</span>
 			</div>
 			<div className="launchpad__sidebar-content-container">
 				<div className="launchpad__progress-bar-container">
@@ -23,9 +27,13 @@ const Sidebar = ( { siteSlug }: { siteSlug: string | null } ) => {
 					</div>
 				</div>
 				{ /* eslint-disable-next-line wpcalypso/jsx-classname-namespace*/ }
-				<h1 className="launchpad__sidebar-h1">Voilà! Your Newsletter is up and running!</h1>
+				<h1 className="launchpad__sidebar-h1">
+					{ translate( 'Voilà! Your %(translatedFlowName)s is up and running!', {
+						args: { translatedFlowName },
+					} ) }
+				</h1>
 				<p className="launchpad__sidebar-description">
-					Keep up the momentum with these next steps.
+					{ translate( 'Keep up the momentum with these next steps.' ) }
 				</p>
 				<div className="launchpad__url-box">{ url }</div>
 				<Checklist siteSlug={ siteSlug } tasks={ tasks } flow={ flow } />
