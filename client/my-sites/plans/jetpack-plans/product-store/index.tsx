1fb456be88170695d2349950620f7bced3c9b7c0
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-08-23 14:43:01 +0530

    Create and wire up JetpackFree component

--- client/my-sites/plans/jetpack-plans/product-store/index.tsx
+++ client/my-sites/plans/jetpack-plans/product-store/index.tsx
@@ -1,11 +1,22 @@
+import { useSelector } from 'react-redux';
 import StoreFooter from 'calypso/jetpack-connect/store-footer';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { JetpackFree } from './jetpack-free';
 import { UserLicensesDialog } from './user-licenses-dialog';
 import type { ProductStoreProps } from './types';
 
-const ProductStore: React.FC< ProductStoreProps > = ( { enableUserLicensesDialog } ) => {
+import './style.scss';
+
+const ProductStore: React.FC< ProductStoreProps > = ( {
+	enableUserLicensesDialog,
+	urlQueryArgs,
+} ) => {
+	const siteId = useSelector( getSelectedSiteId );
+
 	return (
-		<div>
-			{ enableUserLicensesDialog && <UserLicensesDialog /> }
+		<div className="jetpack-product-store">
+			{ enableUserLicensesDialog && <UserLicensesDialog siteId={ siteId } /> }
+			<JetpackFree urlQueryArgs={ urlQueryArgs } siteId={ siteId } />
 			<StoreFooter />
 		</div>
 	);
