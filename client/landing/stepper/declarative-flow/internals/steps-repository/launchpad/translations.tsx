2ce83091024fc7aee81279af082714270d78a851
edanzer <edanzer@gmail.com>
2022-08-23 16:25:05 -0600

    Remove Voila from sidebar title

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/translations.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/translations.tsx
@@ -4,22 +4,22 @@ import { TranslatedLaunchpadStrings } from './types';
 export function getLaunchpadTranslations( flow: string | null ): TranslatedLaunchpadStrings {
 	const translatedStrings = {
 		flowName: translate( 'WordPress' ),
-		sidebarTitle: translate( 'Voilà! Your website is ready to launch!' ),
+		sidebarTitle: translate( 'Your website is ready to launch!' ),
 		sidebarSubtitle: translate( 'Keep up the momentum with these final steps.' ),
 	};
 
 	switch ( flow ) {
 		case 'newsletter':
 			translatedStrings.flowName = translate( 'Newsletter' );
-			translatedStrings.sidebarTitle = translate( 'Voilà! Your Newsletter is ready to launch!' );
+			translatedStrings.sidebarTitle = translate( 'Your Newsletter is ready to launch!' );
 			break;
 		case 'link-in-bio':
 			translatedStrings.flowName = translate( 'Link in Bio' );
-			translatedStrings.sidebarTitle = translate( 'Voilà! Your Link in Bio is ready to launch!' );
+			translatedStrings.sidebarTitle = translate( 'Your Link in Bio is ready to launch!' );
 			break;
 		case 'podcast':
 			translatedStrings.flowName = translate( 'Podcast' );
-			translatedStrings.sidebarTitle = translate( 'Voilà! Your Podcast is ready to launch!' );
+			translatedStrings.sidebarTitle = translate( 'Your Podcast is ready to launch!' );
 			break;
 	}
 
