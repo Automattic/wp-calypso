c205f9809a1bd63b2908513dd81b3bc9819f88fb
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-08-24 19:05:55 +0800

    Fix incorrect type name.

--- client/my-sites/plans/jetpack-plans/product-store/types.ts
+++ client/my-sites/plans/jetpack-plans/product-store/types.ts
@@ -14,4 +14,4 @@ export interface ProductStoreProps extends Pick< BasePageProps, 'urlQueryArgs' >
 
 export type JetpackFreeProps = Pick< ProductStoreProps, 'urlQueryArgs' > & ProductStoreBaseProps;
 
-export type ProductSlugProps = Pick< ProductStoreProps, 'duration' > & ProductStoreBaseProps;
+export type ProductSlugsProps = Pick< ProductStoreProps, 'duration' > & ProductStoreBaseProps;
