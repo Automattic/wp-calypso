fb31ab2216576d671d6b64aa3998dfb29cd457fa
Yashwin <yashwinpoojary@gmail.com>
2022-08-25 16:20:57 +0530

    Update function description for getSitesWithSecondarySites

--- client/my-sites/plugins/plugin-management-v2/utils/get-sites-with-secondary-sites.ts
+++ client/my-sites/plugins/plugin-management-v2/utils/get-sites-with-secondary-sites.ts
@@ -3,6 +3,9 @@ import getNetworkSites from 'calypso/state/selectors/get-network-sites';
 import isConnectedSecondaryNetworkSite from 'calypso/state/selectors/is-connected-secondary-network-site';
 import type { SiteData } from 'calypso/state/ui/selectors/site-data';
 
+/**
+ * Returns an array of object which holds site and its secondary sites
+ */
 export function getSitesWithSecondarySites(
 	state: DefaultRootState,
 	sites: Array< SiteData | null | undefined >
