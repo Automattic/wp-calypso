1f3b0211e6ed5e6647a75f136ee7be1574fa1389
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-08-25 10:14:03 +0200

    Fix typecheck error

--- client/landing/stepper/declarative-flow/internals/steps-repository/importer/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/importer/index.tsx
@@ -56,7 +56,7 @@ export function withImporterWrapper( Importer: ImporterCompType ) {
 		const siteImports = useSelector( ( state ) => getImporterStatusForSiteId( state, siteId ) );
 		const isImporterStatusHydrated = useSelector( isImporterStatusHydratedSelector );
 
-		const fromSite = currentSearchParams.get( 'from' );
+		const fromSite = currentSearchParams.get( 'from' ) || '';
 		const fromSiteData = useSelector( getUrlData );
 		const stepNavigator = useStepNavigator( navigation, siteId, siteSlug, fromSite );
 
