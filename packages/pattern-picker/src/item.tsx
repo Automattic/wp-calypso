de6ac61ca068f0b3dfe9c8b24d3f0e0d8eebfe8a
Omar Alshaker <omar@omaralshaker.com>
2022-08-25 15:50:39 +0200

    Stepper: Change unfocused items blur value to 3px (#66852)

--- packages/pattern-picker/src/item.tsx
+++ packages/pattern-picker/src/item.tsx
@@ -6,15 +6,18 @@ type Props = {
 	className?: string;
 	style?: React.CSSProperties;
 	onClick: () => void;
+	autoFocus?: boolean;
 };
 
-export function Item( { className, style, onClick }: Props ) {
+export function Item( { className, style, onClick, autoFocus }: Props ) {
 	return (
 		<div>
 			<button
 				onClick={ onClick }
 				style={ style }
 				className={ cx( 'pattern-picker__item', className ) }
+				// eslint-disable-next-line jsx-a11y/no-autofocus
+				autoFocus={ autoFocus }
 			></button>
 		</div>
 	);
