94ea4e4f891025e22242630667f4dfa65052ba71
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-08-25 20:26:47 +0800

    Pricing page rework: Add 'See all features' section in Pricing bundle page. (#66964)
    * Add 'See all features' section in Pricing bundle page.
    
    * Restore dummy text to prevent existing unit test from failing.
    
    * Fix typo on track event name.
    
    * Set minimum button width to 225px and use default font size.
    
    * Use 'SF Pro Text' as font family and 16px as font-size for the button.

--- client/my-sites/plans/jetpack-plans/product-store/products.tsx
+++ client/my-sites/plans/jetpack-plans/product-store/products.tsx
@@ -1,10 +1,16 @@
+import { SeeAllFeatures } from './see-all-features';
 import type { ProductProps } from './types';
 
 const Products: React.FC< ProductProps > = ( { type } ) => {
 	return (
 		<div className="jetpack-product-store__product">
 			{ type === 'products' && <div>Product Component goes here</div> }
-			{ type === 'bundles' && <div>Bundle Component goes here</div> }
+			{ type === 'bundles' && (
+				<div>
+					Bundle Component goes here
+					<SeeAllFeatures />
+				</div>
+			) }
 		</div>
 	);
 };
