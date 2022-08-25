1155ad2fb12deeb2f0c55b31887031a21eb80843
Javier Olmo <43957544+jjolmo@users.noreply.github.com>
2022-08-25 18:16:39 +0200

    Promote Post report data tab (#66952)
    * Created Promote Post report data tab
    
    * Handle null audience_list
    
    Co-authored-by: Javier Olmo <javier.olmo@automattic.com>
    Co-authored-by: Daniel Walmsley <goldsounds@gmail.com>

--- client/my-sites/promote-post/components/posts-list-banner/index.tsx
+++ client/my-sites/promote-post/components/posts-list-banner/index.tsx
@@ -1,6 +1,6 @@
 import { Gridicon } from '@automattic/components';
 import { translate } from 'i18n-calypso';
-import megaphoneIllustration from 'calypso/assets/images/customer-home/illustration--megaphone.svg';
+import promoteMenuIllustration from 'calypso/assets/images/customer-home/illustration--promote-menu.svg';
 
 import './style.scss';
 
@@ -9,24 +9,24 @@ export default function PostsListBanner() {
 		<div className="posts-list-banner__container">
 			<div className="posts-list-banner__img">
 				<img
-					src={ megaphoneIllustration }
+					src={ promoteMenuIllustration }
 					alt=""
-					width={ 150 }
+					width={ 200 }
 					className="posts-list-banner__illustration"
 				/>
 			</div>
 			<div>
 				<div className="posts-list-banner__title wp-brand-font">
-					{ translate( 'All you need to start advertising' ) }
+					{ translate( 'Promote your work' ) }
 				</div>
 				<div className="posts-list-banner__description">
 					{ translate(
-						'Launch within minutes, reach specific audiences, and never go over your budget. Find best performing content and promote it.'
+						'Reach more people promoting a post or a page to the larger WordPress.com community of blogs and sites'
 					) }
 				</div>
 				<div className="posts-list-banner__footer">
 					<Gridicon icon="my-sites" />
-					{ translate( 'All with WordPress.com Ads' ) }
+					{ translate( 'WordPress.com Ads' ) }
 				</div>
 			</div>
 		</div>
