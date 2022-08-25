9e97ba35a4a31a2e671644afbbb36b155a7868ce
Javier Olmo <43957544+jjolmo@users.noreply.github.com>
2022-08-25 18:16:39 +0200

    Promote Post report data tab (#66952)
    * Created Promote Post report data tab
    
    * Handle null audience_list
    
    Co-authored-by: Javier Olmo <javier.olmo@automattic.com>
    Co-authored-by: Daniel Walmsley <goldsounds@gmail.com>

--- client/data/promote-post/use-promote-post-campaigns-query.ts
+++ client/data/promote-post/use-promote-post-campaigns-query.ts
@@ -7,6 +7,17 @@ export enum CampaignStatus {
 	TODO1 = 1,
 	TODO2 = 2,
 }
+
+export const AudienceListKeys = {
+	topics: 'topics',
+	countries: 'countries',
+	devices: 'devices',
+};
+
+export type AudienceList = {
+	[ key in keyof typeof AudienceListKeys ]: string;
+};
+
 export type Campaign = {
 	content_config: {
 		clickUrl: string;
@@ -17,7 +28,7 @@ export type Campaign = {
 	content_image: string;
 	start_date: string; // "2022-07-18T01:51:12.000Z"
 	end_date: string;
-	status_smart: CampaignStatus; // todo use real values
+	status_smart: CampaignStatus;
 	target_urns: string;
 	width: number;
 	height: number;
@@ -26,6 +37,16 @@ export type Campaign = {
 	budget_cents: number;
 	moderation_reason: string;
 	moderation_status: number | null;
+	type: string;
+	impressions_estimated_total: number;
+	impressions_total: number;
+	delivery_percent: number;
+	status: string;
+	target_url: string;
+	clicks_total: number;
+	spent_budget_cents: number;
+	deliver_margin_multiplier: number;
+	audience_list: AudienceList;
 };
 
 const useCampaignsQuery = ( siteId: number, queryOptions = {} ) => {
