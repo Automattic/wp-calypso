a9a962ab028f3f5c130bdfc0cb850ffea6f46237
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-08-26 10:57:38 +0530

    Pricing page rework: Create section and heading for most popular items (#66985)
    * Create component for MostPopular items
    
    * Render MostPopular component
    
    * Little bit of styling

--- client/my-sites/plans/jetpack-plans/product-store/products.tsx
+++ client/my-sites/plans/jetpack-plans/product-store/products.tsx
@@ -1,13 +1,21 @@
+import { useTranslate } from 'i18n-calypso';
+import { MostPopular } from './most-popular';
 import { SeeAllFeatures } from './see-all-features';
 import type { ProductProps } from './types';
 
 const Products: React.FC< ProductProps > = ( { type } ) => {
+	const translate = useTranslate();
+
 	return (
-		<div className="jetpack-product-store__product">
-			{ type === 'products' && <div>Product Component goes here</div> }
+		<div className="jetpack-product-store__products">
+			{ type === 'products' && (
+				<div>
+					<MostPopular heading={ translate( 'Most popular products' ) } items={ <></> } />
+				</div>
+			) }
 			{ type === 'bundles' && (
 				<div>
-					Bundle Component goes here
+					<MostPopular heading={ translate( 'Most popular bundles' ) } items={ <></> } />
 					<SeeAllFeatures />
 				</div>
 			) }
