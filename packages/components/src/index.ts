37fda4d69857ba3e14b5046142281064aa6c59f6
vykes-mac <knrymcleish@gmail.com>
2022-08-26 18:33:15 -0500

    create scroll to top button component to be reused

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -11,6 +11,7 @@ export { default as ResponsiveToolbarGroup } from './responsive-toolbar-group';
 export { default as Ribbon } from './ribbon';
 export { default as RootChild } from './root-child';
 export { default as ScreenReaderText } from './screen-reader-text';
+export { ScrollToTopButton } from './scroll-to-top-button';
 export { SiteThumbnail } from './site-thumbnail';
 export { default as Suggestions } from './suggestions';
 export { default as PaginationControl } from './pagination-control';
