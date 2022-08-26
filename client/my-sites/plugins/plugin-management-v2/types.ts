aa206140545d1a5aca46c7d7015cbe1a2bf946b9
Yashwin <yashwinpoojary@gmail.com>
2022-08-26 14:33:26 +0530

    Plugin Management: implement new plugin details page (#66858)
    * Implement new plugin details page
    
    * Fix empty screen on refreshing plugin details page
    
    * Fix styling which was affecting Calypso Blue
    
    * Fix styling in plugin details page
    
    * Fix details page link for single site view
    
    * Fix singular and plural words for site count
    
    * Update function description for getSitesWithSecondarySites
    
    * Update imports for SiteData
    
    * Use RTL equivalent properties
    
    * Fix overlay for site name

--- client/my-sites/plugins/plugin-management-v2/types.ts
+++ client/my-sites/plugins/plugin-management-v2/types.ts
@@ -23,6 +23,8 @@ export interface Plugin {
 	[ key: string ]: any;
 }
 
+export type SiteWithPlugin = { site: SiteDetails; secondarySites: Array< object > | null };
+
 export interface RowFormatterArgs {
 	item: any;
 	columnKey: string;
@@ -32,4 +34,7 @@ export interface RowFormatterArgs {
 }
 export interface PluginRowFormatterArgs extends RowFormatterArgs {
 	item: Plugin;
+}
+export interface SiteRowFormatterArgs extends RowFormatterArgs {
+	item: SiteDetails;
 }
