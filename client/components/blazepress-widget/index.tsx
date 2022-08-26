9a213cbb141f674ac6486e4a41f97c8ffecc4a18
Andres Blanco <andresblanco@gmail.com>
2022-08-26 14:31:27 -0300

    fix reloading

--- client/components/blazepress-widget/index.tsx
+++ client/components/blazepress-widget/index.tsx
@@ -44,7 +44,7 @@ const BlazePressWidget = ( props: BlazePressPromotionProps ) => {
 				);
 				setIsLoading( false );
 			} )();
-	}, [ isVisible, onClose, props.postId, props.siteId ] );
+	}, [ isVisible, props.postId, props.siteId ] );
 
 	const promoteWidgetStatus = usePromoteWidget();
 	if ( promoteWidgetStatus === PromoteWidgetStatus.DISABLED ) {
