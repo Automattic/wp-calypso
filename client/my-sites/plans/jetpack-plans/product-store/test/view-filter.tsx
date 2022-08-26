11230c7f6ccc1c380fe2f219e1fef11320d9f295
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-08-26 11:16:03 +0530

    Update Unit tests

--- client/my-sites/plans/jetpack-plans/product-store/test/view-filter.tsx
+++ client/my-sites/plans/jetpack-plans/product-store/test/view-filter.tsx
@@ -2,21 +2,21 @@
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
-import ProductFilter from '../product-filter';
-describe( '<ProductFilter />', () => {
+import { ViewFilter } from '../view-filter';
+describe( '<ViewFilter />', () => {
 	test( 'should render the filter with Products selected', async () => {
-		const setFilterType = jest.fn();
-		render( <ProductFilter filterType={ 'products' } setFilterType={ setFilterType } /> );
+		const setCurrentView = jest.fn();
+		render( <ViewFilter currentView={ 'products' } setCurrentView={ setCurrentView } /> );
 		expect( screen.getByRole( 'radio', { name: 'Products' } ) ).toBeChecked();
 	} );
 
 	test( 'should switch the filter on click', async () => {
 		const user = userEvent.setup();
-		const setFilterType = jest.fn();
-		render( <ProductFilter filterType={ 'products' } setFilterType={ setFilterType } /> );
+		const setCurrentView = jest.fn();
+		render( <ViewFilter currentView={ 'products' } setCurrentView={ setCurrentView } /> );
 		expect( screen.getByRole( 'radio', { name: 'Products' } ) ).toBeChecked();
 
 		await user.click( screen.getByRole( 'radio', { name: 'Bundles' } ) );
-		expect( setFilterType ).toHaveBeenLastCalledWith( 'bundles' );
+		expect( setCurrentView ).toHaveBeenLastCalledWith( 'bundles' );
 	} );
 } );
