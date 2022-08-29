a71f79e1e21750f4311f8ab5b81a56369fd1ee29
Omar Alshaker <omar@omaralshaker.com>
2022-08-29 14:20:50 +0200

    Use MShots for better perf

--- packages/pattern-picker/src/item.tsx
+++ packages/pattern-picker/src/item.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import './styles.scss';
+import { MShotsImage } from '@automattic/onboarding';
 import cx from 'classnames';
 import { Pattern } from './types';
 
@@ -19,11 +20,11 @@ export function Item( { style, onClick, pattern, className }: Props ) {
 				className={ cx( 'pattern-picker__item', className ) }
 			>
 				<div className="pattern-picker__item-iframe-wrapper">
-					<iframe
-						frameBorder="0"
-						scrolling="no"
-						title={ pattern.name }
-						src={ `https://public-api.wordpress.com/wpcom/v2/block-previews/pattern?stylesheet=pub%2Flynx&pattern_id=${ pattern.name }` }
+					<MShotsImage
+						url={ `https://public-api.wordpress.com/wpcom/v2/block-previews/pattern?stylesheet=pub%2Flynx&pattern_id=${ pattern.name }` }
+						options={ { w: 512, vpw: 512, vph: 1120 } }
+						alt={ pattern.title }
+						aria-labelledby=""
 					/>
 				</div>
 			</button>
