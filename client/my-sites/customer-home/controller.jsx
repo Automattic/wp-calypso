ded7031c684cb1add02238c3d4f9d35aa34162ef
Jeremy Yip <jeremy.yip@automattic.com>
2022-08-29 15:17:15 -0700

    Launchpad: Replace my home url with redirect to launchpad (#66613)
    * Force url redirect whenever /home/:siteid is visited and launchpad is active

--- client/my-sites/customer-home/controller.jsx
+++ client/my-sites/customer-home/controller.jsx
@@ -1,6 +1,9 @@
+import { isEnabled } from '@automattic/calypso-config';
 import page from 'page';
 import { canCurrentUserUseCustomerHome } from 'calypso/state/sites/selectors';
+import getSite from 'calypso/state/sites/selectors/get-site.js';
 import { getSelectedSiteSlug, getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { redirectToLaunchpad } from 'calypso/utils';
 import CustomerHome from './main';
 
 export default async function ( context, next ) {
@@ -20,9 +23,22 @@ export default async function ( context, next ) {
 export function maybeRedirect( context, next ) {
 	const state = context.store.getState();
 	const slug = getSelectedSiteSlug( state );
+
 	if ( ! canCurrentUserUseCustomerHome( state ) ) {
 		page.redirect( `/stats/day/${ slug }` );
 		return;
+	}
+
+	const siteId = getSelectedSiteId( state );
+	const site = getSite( state, siteId );
+	const options = site?.options;
+	const shouldRedirectToLaunchpad = options?.launchpad_screen === 'full';
+
+	if ( shouldRedirectToLaunchpad && isEnabled( 'signup/launchpad' ) ) {
+		// The new stepper launchpad onboarding flow isn't registered within the "page"
+		// client-side router, so page.redirect won't work. We need to use the
+		// traditional window.location Web API.
+		redirectToLaunchpad( slug, options?.site_intent );
 	}
 	next();
 }
