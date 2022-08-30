a04ab9fb2d552d8809d7ef9e5ed65b6fbfce60de
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-08-30 15:09:19 +0800

    Add Jetpack boost and CRM to displayable products.

--- client/my-sites/plans/jetpack-plans/product-store/hooks/use-products-to-display.ts
+++ client/my-sites/plans/jetpack-plans/product-store/hooks/use-products-to-display.ts
@@ -1,6 +1,9 @@
+import { JETPACK_BOOST_PRODUCTS, JETPACK_CRM_PRODUCTS } from '@automattic/calypso-products';
 import { useMemo } from 'react';
 import { MOST_POPULAR_PRODUCTS } from '../../constants';
 import { getProductsToDisplay } from '../../product-grid/utils';
+import slugToSelectorProduct from '../../slug-to-selector-product';
+import { SelectorProduct } from '../../types';
 import useGetPlansGridProducts from '../../use-get-plans-grid-products';
 import { ProductsListProps } from '../types';
 import { isolatePopularItems } from '../utils/isolate-popular-items';
@@ -9,14 +12,23 @@ export const useProductsToDisplay = ( { siteId, duration }: ProductsListProps ) 
 	const { availableProducts, purchasedProducts, includedInPlanProducts } =
 		useGetPlansGridProducts( siteId );
 
+	const allAvailableProducts = useMemo( () => {
+		// TODO - need to add Jetpack social product constants and data
+		const moreAvailableProducts = [ ...JETPACK_BOOST_PRODUCTS, ...JETPACK_CRM_PRODUCTS ].map(
+			slugToSelectorProduct
+		) as SelectorProduct[];
+
+		return [ ...availableProducts, ...moreAvailableProducts ] as SelectorProduct[];
+	}, [ availableProducts ] );
+
 	return useMemo( () => {
 		const allItems = getProductsToDisplay( {
 			duration,
-			availableProducts,
+			availableProducts: allAvailableProducts,
 			purchasedProducts,
 			includedInPlanProducts,
 		} );
 
 		return isolatePopularItems( allItems, MOST_POPULAR_PRODUCTS );
-	}, [ duration, availableProducts, purchasedProducts, includedInPlanProducts ] );
+	}, [ duration, allAvailableProducts, purchasedProducts, includedInPlanProducts ] );
 };
