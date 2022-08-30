eaa9f9ebf39824c4cedba1ceba9f87d5d559fbe3
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-08-30 14:56:51 +0530

    Pricing page rework: Add featured product cards (#67092)
    * Add assets
    
    * Add HeroImage component
    
    * Add FeaturedItemCard component
    
    * Create useCreateCheckout hooks to keep components clean
    
    * Enhance DisplayPrice component
    
    * Render FeaturedItemCard in product and bundle list
    
    * Style it up
    
    * Fix stylelints
    
    * Wire up isOwned prop
    
    * Add images for monthly
    
    * Fix featured card styles
    
    * Fix spacing
    
    * Fix content max-width
    
    * Minor tweaks
    
    * Make card title responsive
    
    * Fix strikthrough for original price

--- client/my-sites/plans/jetpack-plans/product-store/products-list.tsx
+++ client/my-sites/plans/jetpack-plans/product-store/products-list.tsx
@@ -1,15 +1,46 @@
 import { useTranslate } from 'i18n-calypso';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { FeaturedItemCard } from './featured-item-card';
+import { HeroImage } from './hero-image';
+import { useCreateCheckout } from './hooks/use-create-checkout';
 import { useProductsToDisplay } from './hooks/use-products-to-display';
 import { MostPopular } from './most-popular';
 import type { ProductsListProps } from './types';
 
-export const ProductsList: React.FC< ProductsListProps > = ( { duration, siteId } ) => {
+export const ProductsList: React.FC< ProductsListProps > = ( {
+	createCheckoutURL,
+	duration,
+	onClickPurchase,
+	siteId,
+} ) => {
 	const [ popularItems, otherItems ] = useProductsToDisplay( { duration, siteId } );
 	const translate = useTranslate();
 
+	const { getCheckoutURL, getOnClickPurchase, isOwned } = useCreateCheckout( {
+		createCheckoutURL,
+		onClickPurchase,
+		duration,
+		siteId,
+	} );
+
 	const mostPopularItems = popularItems.map( ( item ) => {
-		// TODO relace this with popular product card
-		return <div key={ item.productSlug }>{ item.displayName }</div>;
+		return (
+			<FeaturedItemCard
+				key={ item.productSlug }
+				hero={ <HeroImage item={ item } /> }
+				isOwned={ isOwned( item ) }
+				item={ item }
+				siteId={ siteId }
+				onClickMore={ () => {
+					recordTracksEvent( 'calypso_product_more_about_product_click', {
+						product: item.productSlug,
+					} );
+					// TODO: Open modal
+				} }
+				onClickPurchase={ getOnClickPurchase( item ) }
+				checkoutURL={ getCheckoutURL( item ) }
+			/>
+		);
 	} );
 
 	return (
