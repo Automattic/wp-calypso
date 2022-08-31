b7e095891d25841f711b5e187686e18085992e71
Lukasz Wilczak <lukasz.wilczak@automattic.com>
2022-08-31 14:15:16 +0200

    Make VideoModal shared component

--- client/components/videos-ui/video-modal.jsx
+++ client/components/videos-ui/video-modal.jsx
@@ -1,13 +1,11 @@
 import { useEffect } from 'react';
 import { BlankCanvas } from 'calypso/components/blank-canvas';
-import VideosUi from 'calypso/components/videos-ui';
+import VideosUi from 'calypso/components/videos-ui/index';
 import ModalFooterBar from 'calypso/components/videos-ui/modal-footer-bar';
 import ModalHeaderBar from 'calypso/components/videos-ui/modal-header-bar';
-import { COURSE_SLUGS } from 'calypso/data/courses';
-import '../blogging-quick-start/style.scss';
 
-const PaymentsFeaturesModal = ( props ) => {
-	const { isVisible = false, onClose = () => {} } = props;
+const VideoModal = ( props ) => {
+	const { isVisible = false, onClose = () => {}, courseSlug = false } = props;
 
 	// Scroll to top on initial load regardless of previous page position
 	useEffect( () => {
@@ -19,10 +17,10 @@ const PaymentsFeaturesModal = ( props ) => {
 	return (
 		isVisible && (
 			// eslint-disable-next-line wpcalypso/jsx-classname-namespace
-			<BlankCanvas className="blogging-quick-start-modal">
+			<BlankCanvas className="video-modal">
 				<BlankCanvas.Content>
 					<VideosUi
-						courseSlug={ COURSE_SLUGS.PAYMENTS_FEATURES }
+						courseSlug={ courseSlug }
 						HeaderBar={ ( headerProps ) => (
 							<ModalHeaderBar onClose={ onClose } { ...headerProps } />
 						) }
@@ -37,4 +35,4 @@ const PaymentsFeaturesModal = ( props ) => {
 	);
 };
 
-export default PaymentsFeaturesModal;
+export default VideoModal;
--- client/my-sites/customer-home/cards/education/earn/index.jsx
+++ client/my-sites/customer-home/cards/education/earn/index.jsx
@@ -1,12 +1,11 @@
 import { useTranslate } from 'i18n-calypso';
 import { connect } from 'react-redux';
 import earnCardPrompt from 'calypso/assets/images/customer-home/illustration--secondary-earn.svg';
+import VideoModal from 'calypso/components/videos-ui/video-modal';
 import { COURSE_SLUGS } from 'calypso/data/courses';
 import { useRouteModal } from 'calypso/lib/route-modal';
 import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import EducationalContent from '../educational-content';
-import PaymentsFeaturesModal from './payments-features-modal';
-
 export const EDUCATION_EARN = 'home-education-earn';
 
 const EducationEarn = ( { siteSlug } ) => {
@@ -26,13 +25,14 @@ const EducationEarn = ( { siteSlug } ) => {
 				{
 					calypsoLink: true,
 					url: `/earn/${ siteSlug }`,
-					ModalComponent: PaymentsFeaturesModal,
+					ModalComponent: VideoModal,
 					modalComponentProps: {
 						isVisible: isModalOpen,
 						onClose: ( event ) => {
 							event.preventDefault();
 							closeModal();
 						},
+						courseSlug: COURSE_SLUGS.PAYMENTS_FEATURES,
 					},
 					onClick: () => openModal(),
 					text: translate( 'Start making money' ),
