b5018b7fd6a7fffd217dc25249f33c8e9a2bdcee
Lukasz Wilczak <lukasz.wilczak@automattic.com>
2022-09-01 15:59:13 +0200

    Minor js code fixes

--- client/components/videos-ui/video-modal.jsx
+++ client/components/videos-ui/video-modal.jsx
@@ -1,12 +1,10 @@
 import { useEffect } from 'react';
 import { BlankCanvas } from 'calypso/components/blank-canvas';
-import VideosUi from 'calypso/components/videos-ui/index';
+import VideosUi from 'calypso/components/videos-ui';
 import ModalFooterBar from 'calypso/components/videos-ui/modal-footer-bar';
 import ModalHeaderBar from 'calypso/components/videos-ui/modal-header-bar';
 
-const VideoModal = ( props ) => {
-	const { isVisible = false, onClose = () => {}, courseSlug = false } = props;
-
+const VideoModal = ( { isVisible = false, onClose = () => {}, courseSlug } ) => {
 	// Scroll to top on initial load regardless of previous page position
 	useEffect( () => {
 		if ( isVisible ) {
@@ -16,8 +14,7 @@ const VideoModal = ( props ) => {
 
 	return (
 		isVisible && (
-			// eslint-disable-next-line wpcalypso/jsx-classname-namespace
-			<BlankCanvas className="video-modal">
+			<BlankCanvas className="videos-ui__modal">
 				<BlankCanvas.Content>
 					<VideosUi
 						courseSlug={ courseSlug }
