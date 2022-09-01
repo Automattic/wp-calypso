3eb1581fd017d02bc16a8dd2e0e6dc4803388337
Griffith Chen <griffith.chen@automattic.com>
2022-09-01 20:27:15 +0800

    Use wpcom/v2/starter-designs/get to retrieve theme details

---
+++ packages/data-stores/src/starter-designs-queries/use-starter-designs-get.ts
@@ -0,0 +1,27 @@
+import { stringify } from 'qs';
+import { useQuery, UseQueryResult, QueryOptions } from 'react-query';
+import wpcomRequest from 'wpcom-proxy-request';
+import type { Design } from '@automattic/design-picker/src/types';
+
+interface Options extends QueryOptions< Design, unknown > {
+	enabled?: boolean;
+}
+
+export function useStarterDesignsGet(
+	slug: string,
+	queryOptions: Options = {}
+): UseQueryResult< Design > {
+	return useQuery( [ 'starter-designs-get', slug ], () => fetchStarterDesignsGet( slug ), {
+		refetchOnMount: 'always',
+		staleTime: Infinity,
+		...queryOptions,
+	} );
+}
+
+function fetchStarterDesignsGet( slug: string ): Promise< Design > {
+	return wpcomRequest< Design >( {
+		apiNamespace: 'wpcom/v2',
+		path: '/starter-designs/get',
+		query: stringify( { slug } ),
+	} );
+}
