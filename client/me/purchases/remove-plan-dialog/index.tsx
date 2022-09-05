e1f72ce2377860480faeb618653a4e102f32e10b
Michele Cipriani <michelecipriani80@gmail.com>
2022-09-05 14:02:58 +0200

    cancellation modal style

--- client/me/purchases/remove-plan-dialog/index.tsx
+++ client/me/purchases/remove-plan-dialog/index.tsx
@@ -1,7 +1,7 @@
-import { Dialog } from '@automattic/components';
+import { Dialog, Gridicon } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { Fragment } from 'react';
-import FormSectionHeading from 'calypso/components/forms/form-section-heading';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { SiteExcerptData } from 'calypso/data/sites/site-excerpt-types';
 import { SiteScreenshot } from '../site-screenshot';
 
@@ -17,7 +17,6 @@ interface RemovePlanDialogProps {
 }
 
 export const RemovePlanDialog = ( {
-	planName,
 	closeDialog,
 	removePlan,
 	isDialogVisible,
@@ -28,13 +27,13 @@ export const RemovePlanDialog = ( {
 	const buttons = [
 		{
 			action: 'cancel',
-			label: translate( 'Cancel' ),
+			label: translate( 'Keep my plan' ),
 			onClick: closeDialog,
 		},
 		{
 			isPrimary: true,
 			action: 'removePlanAndAllSubscriptions',
-			label: translate( 'Remove Plan & All Subscriptions', {
+			label: translate( 'Cancel my plan', {
 				comment:
 					'This button removes the active plan and all active Marketplace subscriptions on the site',
 			} ),
@@ -54,24 +53,101 @@ export const RemovePlanDialog = ( {
 			onClose={ closeDialog }
 		>
 			<Fragment>
-				<FormSectionHeading>
-					{ translate( 'New cancel flow - Remove plan: %(plan)s', {
-						args: { plan: planName },
-					} ) }
-				</FormSectionHeading>
-
 				{ shouldUseSiteThumbnail && (
-					<SiteScreenshot
-						className="remove-plan-dialog__site-screenshot"
-						site={ site }
-						size={ 'medium' }
-					/>
+					<div className="remove-plan-dialog__grid">
+						<div className="remove-plan-dialog__grid-colmn1">
+							<SiteScreenshot
+								className="remove-plan-dialog__site-screenshot"
+								site={ site }
+								size={ 'medium' }
+							/>
+						</div>
+						<div className="remove-plan-dialog__grid-colmn2">
+							<FormattedHeader
+								brandFont
+								headerText={ translate( 'Are you sure you want to cancel your Business plan?' ) }
+								align="left"
+							/>
+							<p>{ translate( 'If you cancel your plan, you will lose:' ) }</p>
+							<ul className="remove-plan-dialog__list-plan-features">
+								<li>
+									<Gridicon
+										className="remove-plan-dialog__item-cross-small"
+										size={ 24 }
+										icon="cross-small"
+									/>
+									Your free domain for the first year
+								</li>
+								<li>
+									<Gridicon
+										className="remove-plan-dialog__item-cross-small"
+										size={ 24 }
+										icon="cross-small"
+									/>
+									Access to more than 50,000 plugins
+								</li>
+								<li>
+									<Gridicon
+										className="remove-plan-dialog__item-cross-small"
+										size={ 24 }
+										icon="cross-small"
+									/>
+									Advanced SEO tools
+								</li>
+								<li>
+									<Gridicon
+										className="remove-plan-dialog__item-cross-small"
+										size={ 24 }
+										icon="cross-small"
+									/>
+									Automated site backups and on-click restore
+								</li>
+								<li>
+									<Gridicon
+										className="remove-plan-dialog__item-cross-small"
+										size={ 24 }
+										icon="cross-small"
+									/>
+									SFTP and Database Acccess
+								</li>
+								<li>
+									<Gridicon
+										className="remove-plan-dialog__item-cross-small"
+										size={ 24 }
+										icon="cross-small"
+									/>
+									Access to live chat support
+								</li>
+								<li>
+									<Gridicon
+										className="remove-plan-dialog__item-cross-small"
+										size={ 24 }
+										icon="cross-small"
+									/>
+									and more
+								</li>
+							</ul>
+						</div>
+					</div>
 				) }
 
-				<p>
-					{ translate( 'Dialog content' ) }
-					<br />
-				</p>
+				{ ! shouldUseSiteThumbnail && (
+					<div className="remove-plan-dialog__grid">
+						<div className="remove-plan-dialog__grid-row">
+							<p>
+								{ translate( 'Dialog content' ) }
+								<br />
+								<ul>
+									<li>Item1 </li>
+									<li>Item2 </li>
+									<li>Item3 </li>
+									<li>Item4 </li>
+									<li>Item5 </li>
+								</ul>
+							</p>
+						</div>
+					</div>
+				) }
 			</Fragment>
 		</Dialog>
 	);
