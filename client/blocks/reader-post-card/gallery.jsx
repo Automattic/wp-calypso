b09f3acf3d4da55651c03b19c04ac9e43af35ded
Eoin Gallagher <eoin@automattic.com>
2022-09-05 18:53:21 +0100

    Update gallery to work as a slideshow

--- client/blocks/reader-post-card/gallery.jsx
+++ client/blocks/reader-post-card/gallery.jsx
@@ -6,61 +6,98 @@ import { isFeaturedImageInContent } from 'calypso/lib/post-normalizer/utils';
 import resizeImageUrl from 'calypso/lib/resize-image-url';
 import { imageIsBigEnoughForGallery } from 'calypso/state/reader/posts/normalization-rules';
 import { READER_CONTENT_WIDTH } from 'calypso/state/reader/posts/sizes';
+import { Component } from 'react';
+import classnames from 'classnames';
 
-function getGalleryWorthyImages( post ) {
-	const numberOfImagesToDisplay = 4;
-	const images = ( post.images && [ ...post.images ] ) || [];
-	const indexToRemove = isFeaturedImageInContent( post );
-	if ( indexToRemove ) {
-		images.splice( indexToRemove, 1 );
-	}
+const noop = () => {};
 
-	return images.filter( imageIsBigEnoughForGallery ).slice( 0, numberOfImagesToDisplay );
-}
+class PostGallery extends Component {
+	handleClick = ( event ) => {
+		event.preventDefault();
+		//event.stopPropagation();
+		console.log( event );
+	};
 
-const PostGallery = ( { post, children, isDiscover } ) => {
-	const imagesToDisplay = getGalleryWorthyImages( post );
-	const listItems = imagesToDisplay.map( ( image, index ) => {
-		const imageUrl = resizeImageUrl( image.src, {
-			w: READER_CONTENT_WIDTH / imagesToDisplay.length,
-		} );
-		const safeCssUrl = cssSafeUrl( imageUrl );
-		let imageStyle = { background: 'none' };
-		if ( safeCssUrl ) {
-			imageStyle = {
-				backgroundImage: 'url(' + safeCssUrl + ')',
-				backgroundSize: 'cover',
-				backgroundPosition: '50% 50%',
-				backgroundRepeat: 'no-repeat',
-			};
+	getGalleryWorthyImages = ( post ) => {
+		const numberOfImagesToDisplay = 4;
+		const images = ( post.images && [ ...post.images ] ) || [];
+		const indexToRemove = isFeaturedImageInContent( post );
+		if ( indexToRemove ) {
+			images.splice( indexToRemove, 1 );
 		}
+
+		return images.filter( imageIsBigEnoughForGallery ).slice( 0, numberOfImagesToDisplay );
+	};
+
+	render() {
+		const { post, children, isDiscover } = this.props;
+		const imagesToDisplay = this.getGalleryWorthyImages( post );
+		const listItems = imagesToDisplay.map( ( image, index ) => {
+			const imageUrl = resizeImageUrl( image.src, {
+				w: READER_CONTENT_WIDTH,
+			} );
+			const safeCssUrl = cssSafeUrl( imageUrl );
+			let imageStyle = { background: 'none' };
+			if ( safeCssUrl ) {
+				imageStyle = {
+					backgroundImage: 'url(' + safeCssUrl + ')',
+					backgroundSize: 'cover',
+					backgroundPosition: '50% 50%',
+					backgroundRepeat: 'no-repeat',
+				};
+			}
+
+			const classes = classnames( {
+				'reader-post-card__gallery-item': true,
+				hide: index > 0,
+				show: index === 0,
+			} );
+
+			return (
+				<li key={ `post-${ post.ID }-image-${ index }` } className={ classes }>
+					<div
+						className="reader-post-card__gallery-image"
+						style={ imageStyle }
+						onClick={ this.handleClick }
+					/>
+				</li>
+			);
+		} );
+		const circles = listItems.map( ( value, index ) => {
+			const classes = classnames( {
+				'reader-post-card__gallery-circle': true,
+				'is-selected': index === 0,
+			} );
+			return <div className={ classes }></div>;
+		} );
 		return (
-			<li key={ `post-${ post.ID }-image-${ index }` } className="reader-post-card__gallery-item">
-				<div className="reader-post-card__gallery-image" style={ imageStyle } />
-			</li>
-		);
-	} );
-	return (
-		<div className="reader-post-card__post">
-			<ul className="reader-post-card__gallery">{ listItems }</ul>
-			<div className="reader-post-card__post-details">
-				<AutoDirection>
-					<h2 className="reader-post-card__title">
-						<a className="reader-post-card__title-link" href={ post.URL }>
-							{ post.title }
-						</a>
-					</h2>
-				</AutoDirection>
-				<ReaderExcerpt post={ post } isDiscover={ isDiscover } />
-				{ children }
+			<div className="reader-post-card__post">
+				<ul className="reader-post-card__gallery">{ listItems }</ul>
+				<div className="reader-post-card__gallery-circles">{ circles }</div>
+				<div className="reader-post-card__post-details">
+					<AutoDirection>
+						<h2 className="reader-post-card__title">
+							<a className="reader-post-card__title-link" href={ post.URL }>
+								{ post.title }
+							</a>
+						</h2>
+					</AutoDirection>
+					<ReaderExcerpt post={ post } isDiscover={ isDiscover } />
+					{ children }
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 PostGallery.propTypes = {
 	post: PropTypes.object.isRequired,
 	isDiscover: PropTypes.bool,
+	onClick: PropTypes.func,
+};
+
+PostGallery.defaultProps = {
+	onClick: noop,
 };
 
 export default PostGallery;
