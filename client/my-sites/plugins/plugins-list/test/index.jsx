36acfb930655a42d29e9c0b58670df00562ed44d
Markus Dobmann <m.do@posteo.net>
2022-09-05 13:47:19 +0200

    `PluginsList`: refactor tests to `@testing-library/react`

--- client/my-sites/plugins/plugins-list/test/index.jsx
+++ client/my-sites/plugins/plugins-list/test/index.jsx
@@ -2,65 +2,93 @@
  * @jest-environment jsdom
  */
 
-import { shallow } from 'enzyme';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import plugins from 'calypso/state/plugins/reducer';
+import productsList from 'calypso/state/products-list/reducer';
+import { reducer as ui } from 'calypso/state/ui/reducer';
+import { renderWithProvider } from 'calypso/test-helpers/testing-library';
 import { PluginsList } from '..';
 import { sites, sitesObject } from './fixtures';
 
+const render = ( el, options ) =>
+	renderWithProvider( el, { ...options, reducers: { plugins, ui, productsList } } );
+
 describe( 'PluginsList', () => {
+	const pluginsList = [
+		{ sites: sitesObject, slug: 'hello', name: 'Hello Dolly' },
+		{ sites: sitesObject, slug: 'arbitrary-plugin', name: 'Arbitrary Plugin' },
+		{ sites: sitesObject, slug: 'jetpack', name: 'Jetpack' },
+	];
+
+	const props = {
+		plugins: pluginsList,
+		header: 'Plugins',
+		selectedSite: sites[ 0 ],
+		selectedSiteId: sites[ 0 ].ID,
+		isPlaceholder: false,
+		pluginUpdateCount: pluginsList.length,
+		inProgressStatuses: [],
+		pluginsOnSites: {
+			hello: pluginsList[ 0 ],
+			jetpack: pluginsList[ 1 ],
+		},
+		allSites: sites,
+		hasManagePlugins: true,
+		siteIsAtomic: true,
+		removePluginStatuses: () => {},
+	};
+
 	describe( 'rendering bulk actions', () => {
-		let renderedPluginsList;
-		let plugins;
-		let props;
-
-		beforeAll( () => {
-			plugins = [
-				{ sites: sitesObject, slug: 'hello', name: 'Hello Dolly' },
-				{ sites: sitesObject, slug: 'jetpack', name: 'Jetpack' },
-			];
-
-			props = {
-				plugins,
-				header: 'Plugins',
-				selectedSite: sites[ 0 ],
-				selectedSiteId: sites[ 0 ].ID,
-				isPlaceholder: false,
-				pluginUpdateCount: plugins.length,
-				inProgressStatuses: [],
-				pluginsOnSites: {
-					hello: plugins[ 0 ],
-					jetpack: plugins[ 1 ],
-				},
-				allSites: sites,
-				hasManagePlugins: true,
-				siteIsAtomic: true,
-			};
+		test( 'should select all plugins when toggled on (site is Atomic)', async () => {
+			const user = userEvent.setup();
+			render( <PluginsList { ...props } /> );
+			const toggleBtn = screen.queryByRole( 'button', { name: 'Edit All' } );
+			await user.click( toggleBtn );
+			// Jetpack is auto-managed on Atomic sites, therefore there's only 'Hello Dolly' in here
+			const boxes = screen.queryAllByRole( 'checkbox', { checked: true } );
+			expect( boxes ).toHaveLength( 2 );
+			expect( boxes[ 0 ] ).toHaveAttribute( 'title', 'Hello Dolly' );
 		} );
 
-		beforeEach( () => {
-			renderedPluginsList = shallow( <PluginsList { ...props } /> );
-		} );
+		test( 'should select all plugins when toggled on (site is Jetpack)', async () => {
+			const user = userEvent.setup();
+			render( <PluginsList { ...props } siteIsAtomic={ false } siteIsJetpack /> );
+			const toggleBtn = screen.queryByRole( 'button', { name: 'Edit All' } );
+			await user.click( toggleBtn );
 
-		test( 'should be intialized with no selectedPlugins', () => {
-			expect( renderedPluginsList.state().selectedPlugins ).toEqual( {} );
-		} );
-
-		test( 'should select all plugins when toggled on', () => {
-			renderedPluginsList.instance().toggleBulkManagement();
-			expect( renderedPluginsList.state().selectedPlugins ).toEqual( {
-				hello: expect.anything(),
-				jetpack: expect.anything(),
+			pluginsList.forEach( ( plugin ) => {
+				const box = screen.queryByRole( 'checkbox', { name: plugin.name } );
+				expect( box ).toBeVisible();
+				expect( box ).toBeChecked();
 			} );
 		} );
 
-		test( 'should always reset to all selected when toggled on', () => {
-			renderedPluginsList.instance().togglePlugin( plugins[ 0 ] );
-			expect( Object.keys( renderedPluginsList.state().selectedPlugins ) ).toHaveLength( 1 );
+		test( 'should always reset to all selected when toggled on', async () => {
+			const getSelectedPluginBoxes = () =>
+				pluginsList
+					.map( ( plugin ) =>
+						screen.queryByRole( 'checkbox', { name: plugin.name, checked: true } )
+					)
+					.filter( ( i ) => i );
 
-			renderedPluginsList.instance().toggleBulkManagement();
-			expect( renderedPluginsList.state().selectedPlugins ).toEqual( {
-				hello: expect.anything(),
-				jetpack: expect.anything(),
-			} );
+			const user = userEvent.setup();
+			render( <PluginsList { ...props } /> );
+
+			await user.click( screen.getByRole( 'button', { name: 'Edit All' } ) );
+
+			expect( getSelectedPluginBoxes() ).toHaveLength( 2 );
+
+			const dollyBox = screen.queryByRole( 'checkbox', { name: 'Hello Dolly' } );
+			await user.click( dollyBox );
+
+			expect( getSelectedPluginBoxes() ).toHaveLength( 1 );
+			expect( screen.queryByRole( 'checkbox', { name: 'Hello Dolly' } ) ).not.toBeChecked();
+
+			await user.click( screen.getByRole( 'button', { name: 'Close' } ) );
+			await user.click( screen.getByRole( 'button', { name: 'Edit All' } ) );
+
+			expect( getSelectedPluginBoxes() ).toHaveLength( 2 );
 		} );
 	} );
 } );
