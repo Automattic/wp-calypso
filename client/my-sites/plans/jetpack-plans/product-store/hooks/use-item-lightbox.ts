853dcf6f0f70b2dd4d2a08e3cf08c0790625e8f6
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-09-06 15:04:27 +0800

    Wrap clickMoreHandlerFactory with useCallback.

--- client/my-sites/plans/jetpack-plans/product-store/hooks/use-item-lightbox.ts
+++ client/my-sites/plans/jetpack-plans/product-store/hooks/use-item-lightbox.ts
@@ -1,17 +1,20 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { SelectorProduct } from '../../types';
 
 export const useItemLightbox = () => {
 	const [ currentItem, setCurrentItem ] = useState< SelectorProduct | null >( null );
-	const clickMoreHandlerFactory = ( item: SelectorProduct ): VoidFunction | undefined => {
-		return () => {
-			recordTracksEvent( 'calypso_product_more_about_product_click', {
-				product: item.productSlug,
-			} );
-			setCurrentItem( item );
-		};
-	};
+	const clickMoreHandlerFactory = useCallback(
+		( item: SelectorProduct ): VoidFunction | undefined => {
+			return () => {
+				recordTracksEvent( 'calypso_product_more_about_product_click', {
+					product: item.productSlug,
+				} );
+				setCurrentItem( item );
+			};
+		},
+		[]
+	);
 
 	return {
 		currentItem,
