7782febe215bfa4a9f78ae428f705f59830f3214
arthur <arthur.chu@automattic.com>
2022-09-06 14:36:13 +0800

    Only keep Site Build with Travel Agencies & Services category test case

--- test/e2e/specs/onboarding/setup__build.ts
+++ test/e2e/specs/onboarding/setup__build.ts
@@ -7,61 +7,6 @@ import { Browser, Page } from 'playwright';
 
 declare const browser: Browser;
 
-describe( DataHelper.createSuiteTitle( 'Site Build with "Something else" category' ), () => {
-	const credentials = SecretsManager.secrets.testAccounts.defaultUser;
-	const siteSlug = credentials.testSites?.primary?.url as string;
-
-	let page: Page;
-
-	beforeAll( async () => {
-		page = await browser.newPage();
-
-		const testAccount = new TestAccount( 'defaultUser' );
-		await testAccount.authenticate( page );
-	} );
-
-	describe( 'Onboarding', function () {
-		let startSiteFlow: StartSiteFlow;
-
-		beforeAll( async function () {
-			startSiteFlow = new StartSiteFlow( page );
-		} );
-
-		it( 'Enter Onboarding flow', async function () {
-			await page.goto( DataHelper.getCalypsoURL( '/setup', { siteSlug } ) );
-		} );
-
-		it( 'Select "Promote" goal', async function () {
-			await startSiteFlow.selectGoal( 'Promote' );
-			await startSiteFlow.clickButton( 'Continue' );
-		} );
-
-		it( 'Select "Something else" category', async function () {
-			await startSiteFlow.enterVertical( 'Something else' );
-			await startSiteFlow.clickButton( 'Continue' );
-		} );
-	} );
-
-	describe( 'Build', function () {
-		const themeName = 'Stewart';
-
-		let startSiteFlow: StartSiteFlow;
-
-		beforeAll( async function () {
-			startSiteFlow = new StartSiteFlow( page );
-		} );
-
-		it( 'Select theme', async function () {
-			await startSiteFlow.selectTheme( themeName );
-			await startSiteFlow.clickButton( `Start with ${ themeName }` );
-		} );
-
-		it( 'Land in Home dashboard', async function () {
-			await page.waitForURL( DataHelper.getCalypsoURL( `/home/${ siteSlug }` ) );
-		} );
-	} );
-} );
-
 describe(
 	DataHelper.createSuiteTitle( 'Site Build with "Travel Agencies & Services" category' ),
 	() => {
