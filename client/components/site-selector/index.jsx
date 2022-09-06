0eced8a4b5e40668c00d9c1a56d7de86466b6220
Philip Jackson <p-jackson@live.com>
2022-09-06 17:27:50 +1200

    Remove unnecessary labels from Manage sites button
    It's already an `<a>` tag with a visible text label, it doesn't require
    any additional markup.

--- client/components/site-selector/index.jsx
+++ client/components/site-selector/index.jsx
@@ -493,8 +493,6 @@ export class SiteSelector extends Component {
 						onClick={ this.onManageSitesClick }
 						href={ addQueryArgs( { search: this.props.searchTerm }, '/sites' ) }
 						className="site-selector__manage-sites"
-						aria-label={ this.props.translate( 'Go to Sites' ) }
-						title={ this.props.translate( 'Go to Sites' ) }
 					>
 						{ this.props.translate( 'Manage sites' ) }
 					</Button>
