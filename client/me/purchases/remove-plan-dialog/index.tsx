d67182163d8756c5a7e5ba265843b99f98410c74
Michele Cipriani <michelecipriani80@gmail.com>
2022-09-06 04:57:03 +0200

    plan cancellation modal style and feature list

--- client/me/purchases/remove-plan-dialog/index.tsx
+++ client/me/purchases/remove-plan-dialog/index.tsx
@@ -4,6 +4,7 @@ import { Fragment } from 'react';
 import FormattedHeader from 'calypso/components/formatted-header';
 import { SiteExcerptData } from 'calypso/data/sites/site-excerpt-types';
 import { SiteScreenshot } from '../site-screenshot';
+import getPlanFeatures from './get-plan-features';
 
 import './style.scss';
 
@@ -17,6 +18,7 @@ interface RemovePlanDialogProps {
 }
 
 export const RemovePlanDialog = ( {
+	planName,
 	closeDialog,
 	removePlan,
 	isDialogVisible,
@@ -41,9 +43,51 @@ export const RemovePlanDialog = ( {
 		},
 	];
 
+	/**
+	 * Istantiate plan variables.
+	 */
+	const planLabel = planName.replace( 'WordPress.com ', '' );
 	const isComingSoon = site.is_coming_soon;
 	const isPrivate = site.is_private;
 	const shouldUseSiteThumbnail = isComingSoon === false && isPrivate === false;
+
+	/**
+	 * Return the list of features that the user will lose by canceling their plan.
+	 *
+	 * @returns Fragment
+	 */
+	const FeaturesList = () => {
+		const planFeatures = getPlanFeatures(
+			/*planInCart,*/
+			translate /*,
+			hasDomainsInCart,
+			hasRenewalInCart,
+			nextDomainIsFree*/
+		);
+
+		return (
+			<Fragment>
+				<ul className="remove-plan-dialog__list-plan-features">
+					{ planFeatures.map( ( feature, index ) => {
+						return (
+							<li key={ index }>
+								<Gridicon
+									className="remove-plan-dialog__item-cross-small"
+									size={ 24 }
+									icon="cross-small"
+								/>
+								{ feature }
+							</li>
+						);
+					} ) }
+				</ul>
+			</Fragment>
+		);
+	};
+
+	const gridClassName = shouldUseSiteThumbnail
+		? 'remove-plan-dialog__grid --with-screenshot'
+		: 'remove-plan-dialog__grid';
 
 	return (
 		<Dialog
@@ -53,101 +97,35 @@ export const RemovePlanDialog = ( {
 			onClose={ closeDialog }
 		>
 			<Fragment>
-				{ shouldUseSiteThumbnail && (
-					<div className="remove-plan-dialog__grid">
-						<div className="remove-plan-dialog__grid-colmn1">
+				<button className="remove-plan-dialog__close-button" onClick={ closeDialog }>
+					<Gridicon
+						className="remove-plan-dialog__item-cross-small"
+						size={ 24 }
+						icon="cross-small"
+					/>
+				</button>
+				<div className={ gridClassName }>
+					{ shouldUseSiteThumbnail && (
+						<div className="remove-plan-dialog__grid-colmn">
 							<SiteScreenshot
 								className="remove-plan-dialog__site-screenshot"
 								site={ site }
 								size={ 'medium' }
 							/>
 						</div>
-						<div className="remove-plan-dialog__grid-colmn2">
-							<FormattedHeader
-								brandFont
-								headerText={ translate( 'Are you sure you want to cancel your Business plan?' ) }
-								align="left"
-							/>
-							<p>{ translate( 'If you cancel your plan, you will lose:' ) }</p>
-							<ul className="remove-plan-dialog__list-plan-features">
-								<li>
-									<Gridicon
-										className="remove-plan-dialog__item-cross-small"
-										size={ 24 }
-										icon="cross-small"
-									/>
-									Your free domain for the first year
-								</li>
-								<li>
-									<Gridicon
-										className="remove-plan-dialog__item-cross-small"
-										size={ 24 }
-										icon="cross-small"
-									/>
-									Access to more than 50,000 plugins
-								</li>
-								<li>
-									<Gridicon
-										className="remove-plan-dialog__item-cross-small"
-										size={ 24 }
-										icon="cross-small"
-									/>
-									Advanced SEO tools
-								</li>
-								<li>
-									<Gridicon
-										className="remove-plan-dialog__item-cross-small"
-										size={ 24 }
-										icon="cross-small"
-									/>
-									Automated site backups and on-click restore
-								</li>
-								<li>
-									<Gridicon
-										className="remove-plan-dialog__item-cross-small"
-										size={ 24 }
-										icon="cross-small"
-									/>
-									SFTP and Database Acccess
-								</li>
-								<li>
-									<Gridicon
-										className="remove-plan-dialog__item-cross-small"
-										size={ 24 }
-										icon="cross-small"
-									/>
-									Access to live chat support
-								</li>
-								<li>
-									<Gridicon
-										className="remove-plan-dialog__item-cross-small"
-										size={ 24 }
-										icon="cross-small"
-									/>
-									and more
-								</li>
-							</ul>
-						</div>
+					) }
+					<div className="remove-plan-dialog__grid-colmn">
+						<FormattedHeader
+							brandFont
+							headerText={ translate( 'Are you sure you want to cancel your %(label)s plan?', {
+								args: { label: planLabel },
+							} ) }
+							align="left"
+						/>
+						<p>{ translate( 'If you cancel your plan, you will lose:' ) }</p>
+						<FeaturesList />
 					</div>
-				) }
-
-				{ ! shouldUseSiteThumbnail && (
-					<div className="remove-plan-dialog__grid">
-						<div className="remove-plan-dialog__grid-row">
-							<p>
-								{ translate( 'Dialog content' ) }
-								<br />
-								<ul>
-									<li>Item1 </li>
-									<li>Item2 </li>
-									<li>Item3 </li>
-									<li>Item4 </li>
-									<li>Item5 </li>
-								</ul>
-							</p>
-						</div>
-					</div>
-				) }
+				</div>
 			</Fragment>
 		</Dialog>
 	);
