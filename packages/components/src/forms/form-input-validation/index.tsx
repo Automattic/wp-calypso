04aacb85fac7efb1aafcafef727bac1bf6029438
Emanuele Buccelli <escapemanuele@gmail.com>
2022-09-07 18:31:30 +0200

    Add size to icon

--- packages/components/src/forms/form-input-validation/index.tsx
+++ packages/components/src/forms/form-input-validation/index.tsx
@@ -39,7 +39,11 @@ const FormInputValidation: React.FC< Props > = ( {
 	return (
 		<div className={ classes } role="alert">
 			<span id={ id }>
-				{ icon ? <Gridicon size={ 24 } icon={ icon } /> : <Icon icon={ defaultIcon } /> }
+				{ icon ? (
+					<Gridicon size={ 24 } icon={ icon } />
+				) : (
+					<Icon size={ 24 } icon={ defaultIcon } />
+				) }
 				{ text }
 				{ children }
 			</span>
