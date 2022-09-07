3199434a0bfb27be472921fbc0e643f603bf85d7
Eoin Gallagher <eoin@automattic.com>
2022-09-07 10:03:31 +0100

    fix related cards width bug

--- client/state/reader/posts/sizes.js
+++ client/state/reader/posts/sizes.js
@@ -3,6 +3,7 @@
  */
 export const READER_CONTENT_WIDTH = 600;
 export const READER_FEATURED_MAX_IMAGE_HEIGHT = 300;
+export const READER_RELATED_IMAGE_WIDTH = 348;
 export const PHOTO_ONLY_MIN_WIDTH = 440;
 export const PHOTO_ONLY_MAX_CHARACTER_COUNT = 85;
 export const GALLERY_MIN_IMAGES = 5;
