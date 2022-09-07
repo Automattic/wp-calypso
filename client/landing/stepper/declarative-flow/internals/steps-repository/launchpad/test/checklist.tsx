df5e63c56c4346eb2a18e7a04dbb021647416f9d
Bogomil Stoynov <stoynov.bogomil@gmail.com>
2022-09-07 14:20:28 -0700

    add checklist unit tests

---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/test/checklist.tsx
@@ -0,0 +1,39 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { Task } from '../types';
+import Checklist from '../checklist';
+import { render } from '@testing-library/react';
+
+function getTask( taskData = {} ) {
+	const task: Task = {
+		id: 'foo_task',
+		isCompleted: false,
+		actionUrl: '#',
+		taskType: 'blog',
+		displayBadge: false,
+	};
+
+	return { ...task, ...taskData };
+}
+
+describe( 'Checklist', () => {
+	describe( 'when provided no tasks', () => {
+		it( 'then no tasks are rendered', () => {
+			const { container } = render( <Checklist tasks={ [] } /> );
+			const checklistItems = container.querySelectorAll( '.launchpad__checklist li' );
+			expect( checklistItems.length ).toBe( 0 );
+		} );
+	} );
+
+	describe( 'when a number of tasks are provided', () => {
+		it( 'then the same number of tasks are rendered', () => {
+			const { container } = render(
+				<Checklist tasks={ [ getTask( { id: 'task1' } ), getTask( { id: 'task2' } ) ] } />
+			);
+			const checklistItems = container.querySelectorAll( '.launchpad__checklist li' );
+			expect( checklistItems.length ).toBe( 2 );
+		} );
+	} );
+} );
