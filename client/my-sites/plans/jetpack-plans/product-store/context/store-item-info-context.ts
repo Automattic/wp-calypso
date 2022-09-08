67433e01bb81e9fc45d04ec0f9ac3f3ac9257b19
Andrii Lysenko <lysenkoa.work@gmail.com>
2022-09-08 12:45:25 +0300

    update: remove comment

--- client/my-sites/plans/jetpack-plans/product-store/context/store-item-info-context.ts
+++ client/my-sites/plans/jetpack-plans/product-store/context/store-item-info-context.ts
@@ -5,5 +5,4 @@ const StoreItemInfoContext = createContext< StoreItemInfo >( {} as StoreItemInfo
 
 export default StoreItemInfoContext;
 
-// A little hack here as we populate the default value in the component it is used.
 export const useStoreItemInfoContext = () => useContext( StoreItemInfoContext );
