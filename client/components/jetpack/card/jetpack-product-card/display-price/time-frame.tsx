a90b8e12d0b40d963424e22d9e0d3b321b881c8a
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-09-08 19:47:55 +0800

    Break time-frame component text with span element.

--- client/components/jetpack/card/jetpack-product-card/display-price/time-frame.tsx
+++ client/components/jetpack/card/jetpack-product-card/display-price/time-frame.tsx
@@ -1,6 +1,7 @@
+import { isEnabled } from '@automattic/calypso-config';
 import { TERM_MONTHLY } from '@automattic/calypso-products';
 import { useTranslate } from 'i18n-calypso';
-import { useMemo } from 'react';
+import { createElement, useMemo } from 'react';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import type { Duration } from 'calypso/my-sites/plans/jetpack-plans/types';
 import type { Moment } from 'moment';
@@ -17,11 +18,25 @@ const TimeFrame: React.FC< TimeFrameProps > = ( { expiryDate, billingTerm } ) =>
 		moment.isMoment( expiryDate ) && expiryDate.isValid() ? expiryDate : null;
 
 	const billingTermText = useMemo( () => {
+		const isNewFormat = isEnabled( 'jetpack/pricing-page-rework-v1' ); // TO-DO remove this flag once we have the new translation in production
+
 		if ( billingTerm === TERM_MONTHLY ) {
-			return translate( '/month, billed monthly' );
+			return isNewFormat
+				? translate( '/mo{{span}}nth{{/span}}, billed monthly', {
+						components: {
+							span: createElement( 'span' ),
+						},
+				  } )
+				: translate( '/month, billed monthly' );
 		}
 
-		return translate( '/month, billed yearly' );
+		return isNewFormat
+			? translate( '/mo{{span}}nth{{/span}}, billed yearly', {
+					components: {
+						span: createElement( 'span' ),
+					},
+			  } )
+			: translate( '/month, billed yearly' );
 	}, [ billingTerm, translate ] );
 
 	return productExpiryDate ? (
