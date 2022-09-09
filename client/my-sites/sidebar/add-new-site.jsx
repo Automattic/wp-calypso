dc3f8241361f89cbe77fe6c36b7d1b1ddae0ecd0
sejas <antonio@sejas.es>
2022-09-09 10:06:45 +0100

    Site Switcher: restore show Add New Site Button for users with only one site

--- client/my-sites/sidebar/add-new-site.jsx
+++ client/my-sites/sidebar/add-new-site.jsx
@@ -1,12 +1,18 @@
 import { Button } from '@automattic/components';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import TranslatableString from 'calypso/components/translatable/proptype';
 import { onboardingUrl } from 'calypso/lib/paths';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import { getCurrentUser } from 'calypso/state/current-user/selectors';
 import { setLayoutFocus } from 'calypso/state/ui/layout-focus/actions';
 
 export const AddNewSite = ( { title } ) => {
 	const reduxDispatch = useDispatch();
+
+	const visibleSiteCount = useSelector( getCurrentUser ).visible_site_count;
+	if ( visibleSiteCount > 1 ) {
+		return null;
+	}
 
 	const onClick = () => {
 		reduxDispatch( recordTracksEvent( 'calypso_add_new_wordpress_click' ) );
