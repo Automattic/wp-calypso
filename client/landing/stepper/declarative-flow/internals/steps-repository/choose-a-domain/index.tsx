7aedc90d2d6a752eae552182a8d2689b30e87e6f
thedebian <thedebian@users.noreply.github.com>
2022-09-09 12:09:49 +0200

    VideoPress onboarding, domain selection step

--- client/landing/stepper/declarative-flow/internals/steps-repository/choose-a-domain/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/choose-a-domain/index.tsx
@@ -1,26 +1,23 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import { StepContainer } from 'calypso/../packages/onboarding/src';
 import RegisterDomainStep from 'calypso/components/domains/register-domain-step';
-import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 import type { Step } from '../../types';
 
-import './style.scss';
-
 const ChooseADomain: Step = function ChooseADomain( { navigation, flow, data } ) {
 	const { goNext, goBack, submit } = navigation;
 	const isVideoPressFlow = 'videopress' === flow;
-
-	const onSkip = () => {
-		submit?.( { domainName: '' } );
-	};
 
 	const getDefaultStepContent = () => <h1>Choose a domain step</h1>;
 
 	const getVideoPressFlowStepContent = () => {
 		const onAddDomain = ( { domain_name }: any ) => {
 			submit?.( { domainName: domain_name } );
+		};
+
+		const onSkip = () => {
+			submit?.( { domainName: '' } );
 		};
 
 		const domainSuggestion = data?.domainName ?? data?.siteTitle ?? '';
@@ -37,34 +34,8 @@ const ChooseADomain: Step = function ChooseADomain( { navigation, flow, data } )
 					showAlreadyOwnADomain={ false }
 					onAddDomain={ onAddDomain }
 					onSkip={ onSkip }
-					promoTlds={ [ 'video', 'studio', 'productions', 'com' ] }
 				/>
 			</CalypsoShoppingCartProvider>
-		);
-	};
-
-	const getFormattedHeader = () => {
-		if ( ! isVideoPressFlow ) {
-			return undefined;
-		}
-
-		return (
-			<FormattedHeader
-				id={ 'choose-a-domain-header' }
-				headerText="Choose a domain"
-				subHeaderText={
-					<>
-						Make your video site shine with a custom domain. Not sure yet ?{ ' ' }
-						<button
-							className="button navigation-link step-container__navigation-link has-underline is-borderless"
-							onClick={ onSkip }
-						>
-							Decide later.
-						</button>
-					</>
-				}
-				align={ 'center' }
-			/>
 		);
 	};
 
