f4288af26e3c773804f2a6c57a517a2def16df90
thedebian <thedebian@users.noreply.github.com>
2022-09-09 12:09:49 +0200

    VideoPress onboarding, domain selection step

--- client/landing/stepper/declarative-flow/internals/steps-repository/choose-a-domain/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/choose-a-domain/index.tsx
@@ -1,10 +1,45 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import { StepContainer } from 'calypso/../packages/onboarding/src';
+import RegisterDomainStep from 'calypso/components/domains/register-domain-step';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 import type { Step } from '../../types';
 
-const ChooseADomain: Step = function ChooseADomain( { navigation } ) {
-	const { goNext, goBack } = navigation;
+const ChooseADomain: Step = function ChooseADomain( { navigation, flow, data } ) {
+	const { goNext, goBack, submit } = navigation;
+	const isVideoPressFlow = 'videopress' === flow;
+
+	const getDefaultStepContent = () => <h1>Choose a domain step</h1>;
+
+	const getVideoPressFlowStepContent = () => {
+		const onAddDomain = ( { domain_name }: any ) => {
+			submit?.( { domainName: domain_name } );
+		};
+
+		const onSkip = () => {
+			submit?.( { domainName: '' } );
+		};
+
+		const domainSuggestion = data?.domainName ?? data?.siteTitle ?? '';
+
+		return (
+			<CalypsoShoppingCartProvider>
+				<RegisterDomainStep
+					basePath={ '' }
+					suggestion={ domainSuggestion }
+					domainsWithPlansOnly={ false }
+					isSignupStep={ true }
+					includeWordPressDotCom={ true }
+					includeDotBlogSubdomain={ true }
+					showAlreadyOwnADomain={ false }
+					onAddDomain={ onAddDomain }
+					onSkip={ onSkip }
+				/>
+			</CalypsoShoppingCartProvider>
+		);
+	};
+
+	const stepContent = isVideoPressFlow ? getVideoPressFlowStepContent() : getDefaultStepContent();
 
 	return (
 		<StepContainer
@@ -14,7 +49,7 @@ const ChooseADomain: Step = function ChooseADomain( { navigation } ) {
 			isHorizontalLayout={ false }
 			isWideLayout={ true }
 			isLargeSkipLayout={ false }
-			stepContent={ <h1>Choose a domain step</h1> }
+			stepContent={ stepContent }
 			recordTracksEvent={ recordTracksEvent }
 		/>
 	);
