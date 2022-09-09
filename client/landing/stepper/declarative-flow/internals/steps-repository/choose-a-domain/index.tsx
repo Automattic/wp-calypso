b758262512d40193d76c0aa10d5ed3bc0a28355c
thedebian <thedebian@users.noreply.github.com>
2022-09-09 12:47:01 +0200

    Choose a domain styling

--- client/landing/stepper/declarative-flow/internals/steps-repository/choose-a-domain/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/choose-a-domain/index.tsx
@@ -1,23 +1,26 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import { StepContainer } from 'calypso/../packages/onboarding/src';
 import RegisterDomainStep from 'calypso/components/domains/register-domain-step';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 import type { Step } from '../../types';
 
+import './style.scss';
+
 const ChooseADomain: Step = function ChooseADomain( { navigation, flow, data } ) {
 	const { goNext, goBack, submit } = navigation;
 	const isVideoPressFlow = 'videopress' === flow;
+
+	const onSkip = () => {
+		submit?.( { domainName: '' } );
+	};
 
 	const getDefaultStepContent = () => <h1>Choose a domain step</h1>;
 
 	const getVideoPressFlowStepContent = () => {
 		const onAddDomain = ( { domain_name }: any ) => {
 			submit?.( { domainName: domain_name } );
-		};
-
-		const onSkip = () => {
-			submit?.( { domainName: '' } );
 		};
 
 		const domainSuggestion = data?.domainName ?? data?.siteTitle ?? '';
@@ -39,11 +42,37 @@ const ChooseADomain: Step = function ChooseADomain( { navigation, flow, data } )
 		);
 	};
 
+	const getFormattedHeader = () => {
+		if ( ! isVideoPressFlow ) {
+			return undefined;
+		}
+
+		return (
+			<FormattedHeader
+				id={ 'choose-a-domain-header' }
+				headerText="Choose a domain"
+				subHeaderText={
+					<>
+						Make your video site shine with a custom domain. Not sure yet ?{ ' ' }
+						<button
+							className="button navigation-link step-container__navigation-link has-underline is-borderless"
+							onClick={ onSkip }
+						>
+							Decide later.
+						</button>
+					</>
+				}
+				align={ 'center' }
+			/>
+		);
+	};
+
 	const stepContent = isVideoPressFlow ? getVideoPressFlowStepContent() : getDefaultStepContent();
 
 	return (
 		<StepContainer
 			stepName={ 'chooseADomain' }
+			shouldHideNavButtons={ isVideoPressFlow }
 			goBack={ goBack }
 			goNext={ goNext }
 			isHorizontalLayout={ false }
@@ -51,6 +80,7 @@ const ChooseADomain: Step = function ChooseADomain( { navigation, flow, data } )
 			isLargeSkipLayout={ false }
 			stepContent={ stepContent }
 			recordTracksEvent={ recordTracksEvent }
+			formattedHeader={ getFormattedHeader() }
 		/>
 	);
 };
