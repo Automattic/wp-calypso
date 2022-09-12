98514470f4bd0d3c7aaf621f822fe861dfdeb2b0
Antonio Sejas <antonio@sejas.es>
2022-09-12 09:31:00 +0100

    Site Switcher: update the new site button to show it at the bottom of the sidebar (#67322)
    * Add new `transparent` style to `@automattic/components` to use it on the SiteSwitcher and the Sidebar.
    * Add the new button styles to storybook.
    * Add new eslint rule to package/components to fix an error on stories.
    * Refactor Site Switcher `manage sites` button to use the new button style.
    * Keep the current behavior and show the button for users with only one site.
    * Show this button at the bottom of the calypso sidebar, admin menu.
    * Add plus icon for collapse mode, check the gif below.

--- client/my-sites/sidebar/add-new-site.jsx
+++ client/my-sites/sidebar/add-new-site.jsx
@@ -1,43 +1,36 @@
-import PropTypes from 'prop-types';
-import { Fragment } from 'react';
+import { Button } from '@automattic/components';
 import { useSelector, useDispatch } from 'react-redux';
 import TranslatableString from 'calypso/components/translatable/proptype';
-import SidebarCustomIcon from 'calypso/layout/sidebar/custom-icon';
-import SidebarItem from 'calypso/layout/sidebar/item';
-import SidebarSeparator from 'calypso/layout/sidebar/separator';
 import { onboardingUrl } from 'calypso/lib/paths';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { getCurrentUser } from 'calypso/state/current-user/selectors';
 import { setLayoutFocus } from 'calypso/state/ui/layout-focus/actions';
 
-export const AddNewSite = ( { title, icon } ) => {
+export const AddNewSite = ( { title } ) => {
 	const reduxDispatch = useDispatch();
+
 	const visibleSiteCount = useSelector( getCurrentUser ).visible_site_count;
 	if ( visibleSiteCount > 1 ) {
 		return null;
 	}
 
-	const onNavigate = () => {
+	const onClick = () => {
 		reduxDispatch( recordTracksEvent( 'calypso_add_new_wordpress_click' ) );
 		reduxDispatch( setLayoutFocus( 'content' ) );
 	};
 
 	return (
-		<Fragment>
-			<SidebarSeparator key={ 'add-new-site-separator' } />
-			<SidebarItem
-				label={ title }
-				link={ `${ onboardingUrl() }?ref=calypso-sidebar` }
-				customIcon={ <SidebarCustomIcon icon={ icon } /> }
-				onNavigate={ onNavigate }
-			/>
-		</Fragment>
+		<li className="sidebar__actions">
+			<Button transparent href={ `${ onboardingUrl() }?ref=calypso-sidebar` } onClick={ onClick }>
+				<span class="sidebar__action--collapsed dashicons dashicons-plus-alt"></span>
+				<span>{ title }</span>
+			</Button>
+		</li>
 	);
 };
 
 AddNewSite.propTypes = {
 	title: TranslatableString.isRequired,
-	icon: PropTypes.string.isRequired,
 };
 
 export default AddNewSite;
