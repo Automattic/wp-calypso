3d48d0e03eb3ea99c28f5c07d164e381442a431d
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-09-12 18:47:23 +0800

    Display normal and compact translation into separate blocks for better translation.

--- client/components/jetpack/card/jetpack-product-card/display-price/time-frame.tsx
+++ client/components/jetpack/card/jetpack-product-card/display-price/time-frame.tsx
@@ -1,7 +1,6 @@
-import { isEnabled } from '@automattic/calypso-config';
 import { TERM_MONTHLY } from '@automattic/calypso-products';
 import { useTranslate } from 'i18n-calypso';
-import { createElement, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import type { Duration } from 'calypso/my-sites/plans/jetpack-plans/types';
 import type { Moment } from 'moment';
@@ -18,25 +17,17 @@ const TimeFrame: React.FC< TimeFrameProps > = ( { expiryDate, billingTerm } ) =>
 		moment.isMoment( expiryDate ) && expiryDate.isValid() ? expiryDate : null;
 
 	const billingTermText = useMemo( () => {
-		const isNewFormat = isEnabled( 'jetpack/pricing-page-rework-v1' ); // TO-DO remove this flag once we have the new translation in production
-
 		if ( billingTerm === TERM_MONTHLY ) {
-			return isNewFormat
-				? translate( '/mo{{span}}nth{{/span}}, billed monthly', {
-						components: {
-							span: createElement( 'span' ),
-						},
-				  } )
-				: translate( '/month, billed monthly' );
+			return {
+				normal: translate( '/month, billed monthly' ),
+				compact: translate( '/mo, billed monthly' ),
+			};
 		}
 
-		return isNewFormat
-			? translate( '/mo{{span}}nth{{/span}}, billed yearly', {
-					components: {
-						span: createElement( 'span' ),
-					},
-			  } )
-			: translate( '/month, billed yearly' );
+		return {
+			normal: translate( '/month, billed yearly' ),
+			compact: translate( '/mo, billed yearly' ),
+		};
 	}, [ billingTerm, translate ] );
 
 	return productExpiryDate ? (
@@ -54,7 +45,10 @@ const TimeFrame: React.FC< TimeFrameProps > = ( { expiryDate, billingTerm } ) =>
 		</div>
 	) : (
 		<div>
-			<span className="display-price__billing-time-frame">{ billingTermText }</span>
+			<span className="display-price__billing-time-frame">
+				<span className="normal">{ billingTermText.normal }</span>
+				<span className="compact">{ billingTermText.compact }</span>
+			</span>
 		</div>
 	);
 };
