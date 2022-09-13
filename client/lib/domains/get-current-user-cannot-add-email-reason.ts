bdfd0c3a197c2706d45ff07ccd61ee8ef0a464c5
Fredrik Rombach Ekelund <fredrik@fredrik.computer>
2022-09-13 14:12:01 +0200

    Fix type error

--- client/lib/domains/get-current-user-cannot-add-email-reason.ts
+++ client/lib/domains/get-current-user-cannot-add-email-reason.ts
@@ -1,7 +1,7 @@
 import { canCurrentUserAddEmail } from './can-current-user-add-email';
 import type { ResponseDomain } from 'calypso/lib/domains/types';
 
-export function getCurrentUserCannotAddEmailReason( domain: ResponseDomain ) {
+export function getCurrentUserCannotAddEmailReason( domain: ResponseDomain | undefined ) {
 	if ( domain && ! canCurrentUserAddEmail( domain ) ) {
 		return domain.currentUserCannotAddEmailReason;
 	}
