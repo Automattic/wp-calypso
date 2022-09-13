9b1ffc305131a374adb4f695b1d0d02f942d0604
Niranjan Uma Shankar <niranjan.u@gmail.com>
2022-09-13 13:39:46 +0400

    Update FAQ copy

--- client/my-sites/plans-features-main/index.jsx
+++ client/my-sites/plans-features-main/index.jsx
@@ -24,6 +24,7 @@ import {
 	TERM_BIENNIALLY,
 	GROUP_WPCOM,
 	PLAN_PERSONAL,
+	TITAN_MAIL_MONTHLY_SLUG,
 } from '@automattic/calypso-products';
 import { Button } from '@automattic/components';
 import { isNewsletterOrLinkInBioFlow } from '@automattic/onboarding';
@@ -47,6 +48,7 @@ import PlanFeatures from 'calypso/my-sites/plan-features';
 import PlanFeaturesComparison from 'calypso/my-sites/plan-features-comparison';
 import isHappychatAvailable from 'calypso/state/happychat/selectors/is-happychat-available';
 import { selectSiteId as selectHappychatSiteId } from 'calypso/state/help/actions';
+import { getProductDisplayCost } from 'calypso/state/products-list/selectors';
 import { getByPurchaseId } from 'calypso/state/purchases/selectors';
 import canUpgradeToPlan from 'calypso/state/selectors/can-upgrade-to-plan';
 import getPreviousRoute from 'calypso/state/selectors/get-previous-route';
@@ -438,10 +440,10 @@ export class PlansFeaturesMain extends Component {
 	}
 
 	mayRenderFAQ() {
-		const { isInSignup } = this.props;
+		const { isInSignup, titanMonthlyRenewalCost } = this.props;
 
 		if ( isInSignup ) {
-			return <PlanFAQ />;
+			return <PlanFAQ titanMonthlyRenewalCost={ titanMonthlyRenewalCost } />;
 		}
 
 		return <WpcomFAQ />;
@@ -557,6 +559,7 @@ export default connect(
 		const currentPurchase = getByPurchaseId( state, currentPlan?.id );
 		const sitePlanSlug = sitePlan?.product_slug;
 		const eligibleForWpcomMonthlyPlans = isEligibleForWpComMonthlyPlan( state, siteId );
+		const titanMonthlyRenewalCost = getProductDisplayCost( state, TITAN_MAIL_MONTHLY_SLUG );
 
 		let customerType = chooseDefaultCustomerType( {
 			currentCustomerType: props.customerType,
@@ -586,6 +589,7 @@ export default connect(
 			siteSlug: getSiteSlug( state, get( props.site, [ 'ID' ] ) ),
 			sitePlanSlug,
 			eligibleForWpcomMonthlyPlans,
+			titanMonthlyRenewalCost,
 		};
 	},
 	{
--- client/my-sites/plans-features-main/plansStepFaq.jsx
+++ client/my-sites/plans-features-main/plansStepFaq.jsx
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import ExternalLinkWithTracking from 'calypso/components/external-link/with-tracking';
 import FoldableFAQComponent from 'calypso/components/foldable-faq';
 
 const FAQHeader = styled.h1`
@@ -13,62 +15,104 @@ const FoldableFAQ = styled( FoldableFAQComponent )`
 	}
 `;
 
-export default function PlanFAQ() {
+const PlanFAQ = ( { titanMonthlyRenewalCost } ) => {
 	return (
 		<div className="plan-faq">
 			<FAQHeader className="wp-brand-font">{ 'Frequently Asked Questions' }</FAQHeader>
-			<FoldableFAQ
-				id="faq-1"
-				question={ 'What domains are available? Can I use one I already own?' }
-				expanded={ true }
-			>
-				On WordPress.com, you can register new domains and can choose from more than 300 different
-				extensions such as .com, .org, .services etc. Domains registered elsewhere can be used on
-				WordPress.com using Domain Mapping or transferred to us to manage everything in one place.
-				The annual and biannual plans come included with a free domain for one year.
+			<FoldableFAQ id="faq-1" question={ 'Is hosting included?' } expanded={ true }>
+				Yes! All our plans include fast and reliable hosting that’s optimized for creating and
+				scaling a WordPress site.
 			</FoldableFAQ>
 			<FoldableFAQ id="faq-2" question={ 'Can I import content from another service?' }>
 				It is possible to import your blog content from a variety of other blogging platforms,
-				including Blogger, GoDaddy, Wix, Medium, Squarespace, Movable Type, Typepad, Xanga, and
-				more. You can also easily import your content from a self-hosted WordPress site.
+				including Blogger, GoDaddy, Wix, Squarespace, and more. You can also easily import your
+				content from a self-hosted WordPress site.
 			</FoldableFAQ>
-			<FoldableFAQ id="faq-3" question={ 'What is your refund policy?' }>
+			<FoldableFAQ id="faq-3" question={ 'Are traffic and bandwidth really unlimited?' }>
+				Absolutely, and you will never be hit with any surprise usage fees. With WordPress.com,
+				you’ll be hosted on our infinitely scalable and globally distributed server infrastructure,
+				which means your site will always be available and load fast, no matter how popular your
+				site becomes.
+			</FoldableFAQ>
+			<FoldableFAQ id="faq-4" question={ 'Can I use a domain I already own?' }>
+				Yes! You can connect your domain for free to any WordPress.com paid plan (we won’t charge
+				you a separate domain registration fee). You may either keep the domain at your current
+				registrar or transfer it to us, whichever you prefer.
+			</FoldableFAQ>
+			<FoldableFAQ id="faq-5" question={ 'Can I host multiple sites?' }>
+				Yes, you can host as many sites as you like, but they each need a separate plan. You can
+				choose the appropriate plan for each site individually so you’ll pay for only the features
+				you need. <br />
+				We have a dashboard that helps you manage all your WordPress.com and Jetpack-connected
+				websites, all from one simple and centralized admin tool.
+			</FoldableFAQ>
+			<FoldableFAQ id="faq-6" question={ 'What is your refund policy?' }>
 				If you aren't satisfied with our product, you can cancel anytime within the refund period
 				for a prompt and courteous refund, no questions asked. The refund timeframes are:{ ' ' }
 				<ul>
-					<li>14-days for annual WordPress.com plans</li>
-					<li>7-days for monthly WordPress.com plans</li>
+					<li>14 days for annual WordPress.com plans</li>
+					<li>7 days for monthly WordPress.com plans</li>
 					<li>96 hours for new domain registrations</li>
 				</ul>
 			</FoldableFAQ>
-			<FoldableFAQ id="faq-4" question={ 'Can I get an email account?' }>
-				Yes. We offer Professional Email which is a robust hosted email solution for any custom
-				domain registered through WordPress.com. You can also set up free email forwarding, or use
-				our Google Workspace integration to power your emails.{ ' ' }
+			<FoldableFAQ
+				id="faq-7"
+				question={ 'I want to transfer an existing site, will purchasing a plan now break it?' }
+			>
+				No it won’t! You’re welcome to create your new site with us before pointing the domain here.
+				That way your current site can stay “live” until your new one is ready.
+				<br />
+				We recommend getting a plan now because they have other features you might find useful,
+				including direct live chat and email support. Just avoid using the domain options until
+				you’re ready, and then you can connect or transfer the domain.
 			</FoldableFAQ>
-			<FoldableFAQ id="faq-5" question={ 'Is hosting included?' }>
-				All WordPress.com plans, including the free plan, come with fast, secure, and reliable
-				hosting.
+			<FoldableFAQ id="faq-8" question={ 'Can I get an email account?' }>
+				Absolutely! We offer a few different options to meet your needs. For most customers, our
+				Professional Email service is the smart choice. This robust hosted email solution is
+				available for any domain hosted with WordPress.com and starts at just{ ' ' }
+				{ titanMonthlyRenewalCost }/mo per mailbox.
+				<br />
+				We also offer a Google Workspace integration, and for users who need something simpler, you
+				can set up email forwarding for free.
 			</FoldableFAQ>
-			<FoldableFAQ id="faq-6" question={ 'How do I pay for my plan?' }>
-				You can pay for your brand new WordPress.com plan, add-ons, and domains using any major
-				credit card, debit card, or PayPal. In addition to these, we also support many local payment
-				methods in select countries.
+			<FoldableFAQ id="faq-9" question={ 'Can I install plugins?' }>
+				Yes! When you subscribe to the WordPress.com Business or eCommerce plans, you’ll be able to
+				search for and install over 50,000 available plugins in the WordPress repository.
 			</FoldableFAQ>
-			<FoldableFAQ id="faq-7" question={ 'Is monthly billing available for plans?' }>
-				Yes! Click on the "Pay monthly" tab above or select the monthly option during checkout to
-				pay for your plan in monthly billing cycles.
+			<FoldableFAQ id="faq-10" question={ 'Can I install my own theme?' }>
+				Yes! All plans give you access to our directory of free and premium themes that have been
+				handpicked and reviewed for quality by our team. With the WordPress.com Business or
+				eCommerce plan, you can upload and install any theme you'd like.
 			</FoldableFAQ>
-			<FoldableFAQ id="faq-8" question={ 'Can I create a blog in another language?' }>
-				Absolutely. You can change your blog language, which is how your readers will experience
-				your site and the Interface Language, which changes the admin tools language. The language
-				you use on your blog is up to you!
+			<FoldableFAQ id="faq-11" question={ 'Can you build a site for me?' }>
+				We sure can! If you need a hand launching your website, take a look at Built By
+				WordPress.com Express, our white glove site setup service. Our in-house experts will create
+				your site, and you’ll be ready to go live in four business days or less. To learn more,{ ' ' }
+				<ExternalLinkWithTracking
+					icon={ true }
+					href="https://wordpress.com/do-it-for-me/"
+					onClick={ () => {} }
+					tracksEventName="calypso_signup_step_plans_faq_difm_lp"
+				>
+					click here
+				</ExternalLinkWithTracking>
+				.
 			</FoldableFAQ>
-			<FoldableFAQ id="faq-8" question={ 'Can I make money with my website?' }>
-				Yes! You can sell individual items on your blog or create memberships to share select
-				content with your subscribers. You can also publish sponsored posts or use affiliate links
-				in your content, and apply to join WordAds, our advertising program.
+			<FoldableFAQ id="faq-12" question={ 'Can I talk to a live person?' }>
+				We’d love to chat with you! All paid plans include access to one-on-one support from our
+				team of WordPress experts (we call them Happiness Engineers). The Personal plan includes
+				email support while the Premium plan and above all include live chat support.
+				<br />
+				If you have pre-purchase questions, we offer live chat on the checkout page. Select the plan
+				that looks like the best fit for you and click the “Questions? Ask a Happiness Engineer”
+				link on the next page.
 			</FoldableFAQ>
 		</div>
 	);
-}
+};
+
+PlanFAQ.propTypes = {
+	price: PropTypes.string,
+};
+
+export default PlanFAQ;
