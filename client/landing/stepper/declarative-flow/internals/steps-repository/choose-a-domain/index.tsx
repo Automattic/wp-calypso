e23b7a97684a581a52c96701def4de7074fc56d7
Pierre <thedebian@users.noreply.github.com>
2022-09-13 12:03:13 +0200

    Videopress onboarding v2: domain selection (#67596)
    * Submit options step
    
    * VideoPress onboarding, domain selection step
    
    * Choose a domain styling
    
    * Add video, studio, productions, com as suggested tlds

--- client/landing/stepper/declarative-flow/internals/steps-repository/choose-a-domain/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/choose-a-domain/index.tsx
@@ -1,21 +1,87 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import { StepContainer } from 'calypso/../packages/onboarding/src';
+import RegisterDomainStep from 'calypso/components/domains/register-domain-step';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 import type { Step } from '../../types';
 
-const ChooseADomain: Step = function ChooseADomain( { navigation } ) {
-	const { goNext, goBack } = navigation;
+import './style.scss';
+
+const ChooseADomain: Step = function ChooseADomain( { navigation, flow, data } ) {
+	const { goNext, goBack, submit } = navigation;
+	const isVideoPressFlow = 'videopress' === flow;
+
+	const onSkip = () => {
+		submit?.( { domainName: '' } );
+	};
+
+	const getDefaultStepContent = () => <h1>Choose a domain step</h1>;
+
+	const getVideoPressFlowStepContent = () => {
+		const onAddDomain = ( { domain_name }: any ) => {
+			submit?.( { domainName: domain_name } );
+		};
+
+		const domainSuggestion = data?.domainName ?? data?.siteTitle ?? '';
+
+		return (
+			<CalypsoShoppingCartProvider>
+				<RegisterDomainStep
+					basePath={ '' }
+					suggestion={ domainSuggestion }
+					domainsWithPlansOnly={ false }
+					isSignupStep={ true }
+					includeWordPressDotCom={ true }
+					includeDotBlogSubdomain={ true }
+					showAlreadyOwnADomain={ false }
+					onAddDomain={ onAddDomain }
+					onSkip={ onSkip }
+					promoTlds={ [ 'video', 'studio', 'productions', 'com' ] }
+				/>
+			</CalypsoShoppingCartProvider>
+		);
+	};
+
+	const getFormattedHeader = () => {
+		if ( ! isVideoPressFlow ) {
+			return undefined;
+		}
+
+		return (
+			<FormattedHeader
+				id={ 'choose-a-domain-header' }
+				headerText="Choose a domain"
+				subHeaderText={
+					<>
+						Make your video site shine with a custom domain. Not sure yet ?{ ' ' }
+						<button
+							className="button navigation-link step-container__navigation-link has-underline is-borderless"
+							onClick={ onSkip }
+						>
+							Decide later.
+						</button>
+					</>
+				}
+				align={ 'center' }
+			/>
+		);
+	};
+
+	const stepContent = isVideoPressFlow ? getVideoPressFlowStepContent() : getDefaultStepContent();
 
 	return (
 		<StepContainer
 			stepName={ 'chooseADomain' }
+			shouldHideNavButtons={ isVideoPressFlow }
 			goBack={ goBack }
 			goNext={ goNext }
 			isHorizontalLayout={ false }
 			isWideLayout={ true }
 			isLargeSkipLayout={ false }
-			stepContent={ <h1>Choose a domain step</h1> }
+			stepContent={ stepContent }
 			recordTracksEvent={ recordTracksEvent }
+			formattedHeader={ getFormattedHeader() }
 		/>
 	);
 };
