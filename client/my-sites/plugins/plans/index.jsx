498c16b23c418ecdb6d90deba4a0b13e0be7f804
Louis Laugesen <louis.laugesen@gmail.com>
2022-09-13 15:16:52 +1000

    Use fixed nav instead

--- client/my-sites/plugins/plans/index.jsx
+++ client/my-sites/plugins/plans/index.jsx
@@ -1,25 +1,47 @@
 import { useTranslate } from 'i18n-calypso';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import DocumentHead from 'calypso/components/data/document-head';
+import FixedNavigationHeader from 'calypso/components/fixed-navigation-header';
 import MainComponent from 'calypso/components/main';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
-import useScrollAboveElement from 'calypso/lib/use-scroll-above-element';
-import PluginsNavigationHeader from '../plugins-navigation-header';
+import { appendBreadcrumb } from 'calypso/state/breadcrumb/actions';
+import { getBreadcrumbs } from 'calypso/state/breadcrumb/selectors';
+import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 const Plans = () => {
-	const { referenceRef: navigationHeaderRef } = useScrollAboveElement();
-
 	const translate = useTranslate();
+	const breadcrumbs = useSelector( getBreadcrumbs );
+	const selectedSite = useSelector( getSelectedSite );
+	const dispatch = useDispatch();
+	useEffect( () => {
+		if ( breadcrumbs.length === 0 ) {
+			dispatch(
+				appendBreadcrumb( {
+					label: translate( 'Plugins' ),
+					href: `/plugins/${ selectedSite?.slug || '' }`,
+					id: 'plugins',
+					helpBubble: translate(
+						'Add new functionality and integrations to your site with plugins.'
+					),
+				} )
+			);
+		}
+
+		dispatch(
+			appendBreadcrumb( {
+				label: translate( 'Plan Upgrade' ),
+				href: `/plugins/plans/${ selectedSite?.slug || '' }`,
+				id: `plugin-plans`,
+			} )
+		);
+	}, [ dispatch, translate, selectedSite, breadcrumbs.length ] );
 
 	return (
 		<MainComponent wideLayout>
 			<PageViewTracker path="/plugins/plans/:site" title="Plugins > Plan Upgrade" />
 			<DocumentHead title={ translate( 'Plugins > Plan Upgrade' ) } />
-
-			<PluginsNavigationHeader
-				navigationHeaderRef={ navigationHeaderRef }
-				categoryName={ translate( 'Plan Upgrade' ) }
-				category={ 'something needs to be here so that the top can show' }
-			/>
+			<FixedNavigationHeader navigationItems={ breadcrumbs } />
 		</MainComponent>
 	);
 };
