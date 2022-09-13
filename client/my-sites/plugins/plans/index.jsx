72d4b9c1c218750aa091a487bd8bcc8ed498f6f1
Louis Laugesen <louis.laugesen@gmail.com>
2022-09-13 15:04:17 +1000

    Revert "Remove nav header"
    This reverts commit 719e1b1fc7d00744c1a4e81d49d837bac02c3967.

--- client/my-sites/plugins/plans/index.jsx
+++ client/my-sites/plugins/plans/index.jsx
@@ -2,14 +2,24 @@ import { useTranslate } from 'i18n-calypso';
 import DocumentHead from 'calypso/components/data/document-head';
 import MainComponent from 'calypso/components/main';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import useScrollAboveElement from 'calypso/lib/use-scroll-above-element';
+import PluginsNavigationHeader from '../plugins-navigation-header';
 
 const Plans = () => {
+	const { referenceRef: navigationHeaderRef } = useScrollAboveElement();
+
 	const translate = useTranslate();
 
 	return (
 		<MainComponent wideLayout>
 			<PageViewTracker path="/plugins/plans/:site" title="Plugins > Plan Upgrade" />
 			<DocumentHead title={ translate( 'Plugins > Plan Upgrade' ) } />
+
+			<PluginsNavigationHeader
+				navigationHeaderRef={ navigationHeaderRef }
+				categoryName={ translate( 'Plan Upgrade' ) }
+				category={ 'something needs to be here so that the top can show' }
+			/>
 		</MainComponent>
 	);
 };
