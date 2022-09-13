d79f3988b871cdc5bba98075cc597e2169fe41c3
Matthew Reishus <mreishus@users.noreply.github.com>
2022-09-13 14:46:15 -0500

    Convert commented out test to it.skip

--- test/e2e/specs/onboarding/ftme__sell.ts
+++ test/e2e/specs/onboarding/ftme__sell.ts
@@ -168,10 +168,10 @@ describe( DataHelper.createSuiteTitle( 'FTME: Sell' ), function () {
 		// is false.
 		// When the flag is turned on for all environments, delete this.
 		// -mreishus 2022-09-13
-		// it( 'Continue with simple option', async function () {
-		// 	await page.waitForURL( /.*setup\/storeFeatures.*/ );
-		// 	await startSiteFlow.clickButton( 'Continue' );
-		// } );
+		it.skip( 'Continue with simple option', async function () {
+			await page.waitForURL( /.*setup\/storeFeatures.*/ );
+			await startSiteFlow.clickButton( 'Continue' );
+		} );
 
 		it( 'Select theme', async function () {
 			await startSiteFlow.selectTheme( themeName );
