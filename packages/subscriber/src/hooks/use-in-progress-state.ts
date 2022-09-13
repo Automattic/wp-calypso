c347fbcd421d392434c723896b7997fd4bcd053b
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-09-13 10:53:37 +0200

    Subscribers form: integrate record tracks events (#67555)
    * Introduce record add form events hook
    
    * Integrate recordAddFormEvents hook and fix bug with clearing import error state
    
    * Integrate recordTrackEvents with AddSubscriberForm usage
    
    * Remove obsolete progress bar simulation

--- packages/subscriber/src/hooks/use-in-progress-state.ts
+++ packages/subscriber/src/hooks/use-in-progress-state.ts
@@ -1,29 +1,9 @@
 import { useSelect } from '@wordpress/data';
-import { useState, useEffect } from 'react';
-import { IMPORT_PROGRESS_SIMULATION_DURATION } from '../config';
 import { SUBSCRIBER_STORE } from '../store';
 
-export function useInProgressState( simulationDuration = IMPORT_PROGRESS_SIMULATION_DURATION ) {
+export function useInProgressState() {
 	const addSelector = useSelect( ( s ) => s( SUBSCRIBER_STORE ).getAddSubscribersSelector() );
 	const importSelector = useSelect( ( s ) => s( SUBSCRIBER_STORE ).getImportSubscribersSelector() );
-	const IN_PROGRESS = addSelector?.inProgress || importSelector?.inProgress;
-	const [ inProgress, setInProgress ] = useState( IN_PROGRESS );
 
-	useEffect( () => {
-		let timer: number | undefined;
-
-		if ( inProgress ) {
-			timer = setTimeout( () => {
-				setInProgress( false );
-			}, simulationDuration );
-		}
-
-		return () => clearTimeout( timer );
-	}, [ IN_PROGRESS ] );
-
-	if ( IN_PROGRESS ) {
-		! inProgress && setInProgress( true );
-	}
-
-	return inProgress;
+	return addSelector?.inProgress || importSelector?.inProgress;
 }
