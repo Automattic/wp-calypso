123f8c17978ecbc803c23a7739bf011a222f45c3
Kevin Zoschke <kevin.zoschke@automattic.com>
2022-09-13 14:02:45 -0400

    Jetpack pricing page: add WAF to Scan & Security cards

--- packages/calypso-products/src/constants/features.ts
+++ packages/calypso-products/src/constants/features.ts
@@ -150,6 +150,7 @@ export const FEATURE_SCAN_V2 = 'scan-v2';
 export const FEATURE_PRODUCT_SCAN_DAILY_V2 = 'product-scan-daily-v2';
 export const FEATURE_PRODUCT_SCAN_REALTIME_V2 = 'product-scan-realtime-v2';
 export const FEATURE_ANTISPAM_V2 = 'antispam-v2';
+export const FEATURE_WAF = 'waf';
 export const FEATURE_ACTIVITY_LOG_1_YEAR_V2 = 'activity-log-1-year-v2';
 export const FEATURE_SEARCH_V2 = 'search-v2';
 export const FEATURE_PRODUCT_SEARCH_V2 = 'product-search-v2';
