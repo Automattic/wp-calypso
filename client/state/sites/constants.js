8c522000ffeb743cf9eaab59a6bb2fac94197b46
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2022-09-14 16:58:28 -0300

    Request user_interactions from the API

--- client/state/sites/constants.js
+++ client/state/sites/constants.js
@@ -22,6 +22,7 @@ export const SITE_REQUEST_FIELDS = [
 	'is_core_site_editor_enabled',
 	'is_wpcom_atomic',
 	'description',
+	'user_interactions',
 ].join();
 
 export const SITE_REQUEST_OPTIONS = [
