5f6ab79f4487262d59bf29f8ec750aa5c3bab393
Miguel San Segundo <miguel.san.segundo@automattic.com>
2022-09-14 08:28:54 +0200

    Pattern Assembler - Continue button (#67498)
    * fix css
    
    * fix css
    
    * Continue button
    
    * Code improvements and redirection
    
    * Update theme and page template
    
    * Fix redirection
    
    * Logic for choosing page template based on selected patterns
    
    * Code improvements
    
    * Remove patterns
    
    * Code improvements

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/constants.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/constants.ts
@@ -1,4 +1,4 @@
 export const PATTERN_SOURCE_SITE_ID = 174455321; // dotcompatterns
-export const STYLE_SHEET = 'pub/blank-canvas';
+export const STYLE_SHEET = 'pub/blank-canvas-blocks';
 export const PUBLIC_API_URL = 'https://public-api.wordpress.com';
 export const PREVIEW_PATTERN_URL = PUBLIC_API_URL + '/wpcom/v2/block-previews/pattern';
