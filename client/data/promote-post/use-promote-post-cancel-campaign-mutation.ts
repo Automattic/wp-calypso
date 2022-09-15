e83ded025e76d6b62c1dd542d3c238580ebfa54b
Ben Zhu <ben.zhu@a8c.com>
2022-09-15 15:29:24 +1200

    Adds error msg when cancellation fails.

--- client/data/promote-post/use-promote-post-cancel-campaign-mutation.ts
+++ client/data/promote-post/use-promote-post-cancel-campaign-mutation.ts
@@ -1,16 +1,22 @@
 import { useCallback } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
+import { useDispatch } from 'react-redux';
 import { Campaign } from 'calypso/data/promote-post/use-promote-post-campaigns-query';
 import { requestDSP } from 'calypso/lib/promote-post';
+import { errorNotice } from 'calypso/state/notices/actions';
 
 export const useCancelCampaignMutation = () => {
 	const queryClient = useQueryClient();
+	const dispatch = useDispatch();
 	const mutation = useMutation(
 		async ( { siteId, campaignId }: { siteId: number; campaignId: number } ) =>
 			await requestDSP< { results: Campaign[] } >( siteId, `/campaigns/${ campaignId }/stop` ),
 		{
 			onSuccess( data, { siteId } ) {
 				queryClient.invalidateQueries( [ 'promote-post-campaigns', siteId ] );
+			},
+			onError() {
+				dispatch( errorNotice( 'Something wrong happened, please try again later.' ) );
 			},
 		}
 	);
