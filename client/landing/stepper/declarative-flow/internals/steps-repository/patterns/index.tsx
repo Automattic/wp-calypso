77a498e27d8968c950fac0d8f955d35d7f3d7db0
heavyweight <kpapazov@gmail.com>
2022-09-16 11:43:56 +0200

    PatternsStep: async load pattern picker

--- client/landing/stepper/declarative-flow/internals/steps-repository/patterns/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/patterns/index.tsx
@@ -1,7 +1,7 @@
 import { StepContainer } from '@automattic/onboarding';
-import { PatternPicker } from '@automattic/pattern-picker';
 import { useDispatch } from '@wordpress/data';
 import { useI18n } from '@wordpress/react-i18n';
+import AsyncLoad from 'calypso/components/async-load';
 import FormattedHeader from 'calypso/components/formatted-header';
 import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
@@ -27,7 +27,13 @@ const Patterns: Step = function Patterns( { navigation } ) {
 			goNext={ goNext }
 			shouldHideNavButtons
 			isFullLayout={ true }
-			stepContent={ <PatternPicker onPick={ handleSubmit } /> }
+			stepContent={
+				<AsyncLoad
+					require="@automattic/pattern-picker"
+					placeholder={ null }
+					onPick={ handleSubmit }
+				/>
+			}
 			recordTracksEvent={ recordTracksEvent }
 			formattedHeader={
 				<FormattedHeader
