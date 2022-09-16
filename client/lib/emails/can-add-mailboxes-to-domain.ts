ed8548dcf7df918e4ad01c5a3965bcee3d9e0dd8
Volodymyr Makukha <nei.css@gmail.com>
2022-09-16 16:01:52 +0100

    Emails: fix apearance of mailbox upsell component for GW subscriptions

---
+++ client/lib/emails/can-add-mailboxes-to-domain.ts
@@ -0,0 +1,19 @@
+import { canCurrentUserAddEmail } from 'calypso/lib/domains';
+import { hasEmailForwards } from 'calypso/lib/domains/email-forwarding';
+import { getGSuiteProductSlug, getGSuiteSubscriptionStatus } from 'calypso/lib/gsuite';
+import { getTitanProductSlug } from 'calypso/lib/titan';
+import type { ResponseDomain } from 'calypso/lib/domains/types';
+
+/**
+ * Determines whether a domain has email provider and is everything set up to add new mailboxes
+ */
+export function canAddMailboxesToDomain( domain?: ResponseDomain ): boolean {
+	const domainIsNotProvisioning = getGSuiteProductSlug( domain ) || getTitanProductSlug( domain );
+	const isAwaitingGoogleTosAcceptance = getGSuiteSubscriptionStatus( domain ) === 'suspended';
+
+	return (
+		( domainIsNotProvisioning || hasEmailForwards( domain ) ) &&
+		! isAwaitingGoogleTosAcceptance &&
+		canCurrentUserAddEmail( domain )
+	);
+}
