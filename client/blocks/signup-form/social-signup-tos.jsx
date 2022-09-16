7268d6ce92ffcb4e9751d5a9a31bb96571f9381e
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-09-16 18:39:41 +0800

    Update woo signup style

--- client/blocks/signup-form/social-signup-tos.jsx
+++ client/blocks/signup-form/social-signup-tos.jsx
@@ -1,7 +1,31 @@
 import { localizeUrl } from '@automattic/i18n-utils';
 import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
+import { isWooOAuth2Client } from 'calypso/lib/oauth2-clients';
+import { getCurrentOAuth2Client } from 'calypso/state/oauth2-clients/ui/selectors';
 
 function SocialSignupToS( props ) {
+	if ( isWooOAuth2Client( props.oauth2Client ) ) {
+		return (
+			<p className="signup-form__social-buttons-tos">
+				{ props.translate(
+					"If you continue with Google or Apple and don't already have a WordPress.com account, you are creating an account and you agree to our {{tosLink}}Terms of Service{{/tosLink}}.",
+					{
+						components: {
+							tosLink: (
+								<a
+									href={ localizeUrl( 'https://wordpress.com/tos/' ) }
+									target="_blank"
+									rel="noopener noreferrer"
+								/>
+							),
+						},
+					}
+				) }
+			</p>
+		);
+	}
+
 	return (
 		<p className="signup-form__social-buttons-tos">
 			{ props.translate(
@@ -31,4 +55,6 @@ function SocialSignupToS( props ) {
 	);
 }
 
-export default localize( SocialSignupToS );
+export default connect( ( state ) => ( {
+	oauth2Client: getCurrentOAuth2Client( state ),
+} ) )( localize( SocialSignupToS ) );
