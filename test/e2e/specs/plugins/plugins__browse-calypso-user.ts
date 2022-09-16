2d1cd19d55a37e95873b106f8b8dbf735972ba52
Louis Laugesen <louis.laugesen@gmail.com>
2022-09-16 15:23:21 +1000

    Move test title usage to variables

--- test/e2e/specs/plugins/plugins__browse-calypso-user.ts
+++ test/e2e/specs/plugins/plugins__browse-calypso-user.ts
@@ -30,7 +30,7 @@ describe( DataHelper.createSuiteTitle( 'Plugins: Browse' ), function () {
 			await pluginsPage.visit();
 		} );
 
-		it.each( [ 'Must-have premium plugins', 'Our developers’ favorites', 'The free essentials' ] )(
+		it.each( [ PluginsPage.paidSection, PluginsPage.featuredSection, PluginsPage.freeSection ] )(
 			'Plugins page loads %s section',
 			async function ( section: string ) {
 				await pluginsPage.validateHasSection( section );
@@ -39,7 +39,7 @@ describe( DataHelper.createSuiteTitle( 'Plugins: Browse' ), function () {
 
 		it( 'Can browse all free plugins', async function () {
 			await pluginsPage.clickBrowseAllFreePlugins();
-			await pluginsPage.validateHasHeaderTitle( 'The free essentials' );
+			await pluginsPage.validateHasHeaderTitle( PluginsPage.freeSection );
 		} );
 
 		it( 'Can return via breadcrumb', async function () {
@@ -48,11 +48,11 @@ describe( DataHelper.createSuiteTitle( 'Plugins: Browse' ), function () {
 			} else {
 				await pluginsPage.clickBackBreadcrumb();
 			}
-			await pluginsPage.validateHasSection( 'Must-have premium plugins' );
+			await pluginsPage.validateHasSection( PluginsPage.paidSection );
 		} );
 		it( 'Can browse all premium plugins', async function () {
 			await pluginsPage.clickBrowseAllPaidPlugins();
-			await pluginsPage.validateHasHeaderTitle( 'Must-have premium plugins' );
+			await pluginsPage.validateHasHeaderTitle( PluginsPage.paidSection );
 		} );
 
 		it( 'Can return via breadcrumb from premium plugins', async function () {
@@ -61,7 +61,7 @@ describe( DataHelper.createSuiteTitle( 'Plugins: Browse' ), function () {
 			} else {
 				await pluginsPage.clickBackBreadcrumb();
 			}
-			await pluginsPage.validateHasSection( 'Must-have premium plugins' );
+			await pluginsPage.validateHasSection( PluginsPage.paidSection );
 		} );
 
 		it.each( [
@@ -96,7 +96,7 @@ describe( DataHelper.createSuiteTitle( 'Plugins: Browse' ), function () {
 			await pluginsPage.visit( credentials.testSites?.primary.url as string );
 		} );
 
-		it.each( [ 'Must-have premium plugins', 'Our developers’ favorites', 'The free essentials' ] )(
+		it.each( [ PluginsPage.paidSection, PluginsPage.featuredSection, PluginsPage.freeSection ] )(
 			'Plugins page loads %s section',
 			async function ( section: string ) {
 				await pluginsPage.validateHasSection( section );
