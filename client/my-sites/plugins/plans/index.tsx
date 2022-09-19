ad98906b9b6e98b9abd8b424c8428be552da9173
Candy Tsai <candy02058912@gmail.com>
2022-09-19 11:39:39 +0800

    Marketplace: Add plan features to plan information step

--- client/my-sites/plugins/plans/index.tsx
+++ client/my-sites/plugins/plans/index.tsx
@@ -1,22 +1,34 @@
 import { useTranslate } from 'i18n-calypso';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import DocumentHead from 'calypso/components/data/document-head';
 import FixedNavigationHeader from 'calypso/components/fixed-navigation-header';
 import FormattedHeader from 'calypso/components/formatted-header';
 import MainComponent from 'calypso/components/main';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import PlansFeaturesMain from 'calypso/my-sites/plans-features-main';
 import { appendBreadcrumb } from 'calypso/state/breadcrumb/actions';
 import { getBreadcrumbs } from 'calypso/state/breadcrumb/selectors';
+import getCurrentQueryArguments from 'calypso/state/selectors/get-current-query-arguments';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 import './style.scss';
 
 const Plans = () => {
+	const getIntervalType = ( intervalType = '' ) => {
+		if ( [ 'yearly', 'monthly' ].includes( intervalType ) ) {
+			return intervalType;
+		}
+
+		// Default value
+		return 'yearly';
+	};
 	const translate = useTranslate();
 	const breadcrumbs = useSelector( getBreadcrumbs );
 	const selectedSite = useSelector( getSelectedSite );
+	const currentQuery = useSelector( getCurrentQueryArguments );
 	const dispatch = useDispatch();
+	const [ intervalType, setIntervalType ] = useState( getIntervalType() );
 	useEffect( () => {
 		if ( breadcrumbs.length === 0 ) {
 			dispatch(
@@ -40,6 +52,14 @@ const Plans = () => {
 		);
 	}, [ dispatch, translate, selectedSite, breadcrumbs.length ] );
 
+	useEffect( () => {
+		setIntervalType( getIntervalType( currentQuery?.intervalType as string ) );
+	}, [ currentQuery ] );
+
+	const onSelectPlan = () => {
+		// show eligibility warnings
+	};
+
 	return (
 		<MainComponent wideLayout>
 			<PageViewTracker path="/plugins/plans/:site" title="Plugins > Plan Upgrade" />
@@ -51,6 +71,19 @@ const Plans = () => {
 				subHeaderText={ `Choose the plan that's right for you and reimagine what's possible with plugins` }
 				brandFont
 			/>
+			<div className="plans">
+				<PlansFeaturesMain
+					site={ selectedSite }
+					isInSignup={ false }
+					isInMarketplace
+					intervalType={ intervalType }
+					onUpgradeClick={ onSelectPlan }
+					flowName={ 'marketplace' }
+					shouldShowPlansFeatureComparison
+					showFAQ={ false }
+					isReskinned
+				/>
+			</div>
 		</MainComponent>
 	);
 };
