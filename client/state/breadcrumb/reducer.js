0e5306c4eff45780f43bf32a3480d7eeaca093a7
DustyReagan <dusty@dustyreagan.com>
2022-09-19 15:25:06 -0500

    Update and use BREADCRUMB_APPEND_ITEM

--- client/state/breadcrumb/reducer.js
+++ client/state/breadcrumb/reducer.js
@@ -11,15 +11,14 @@ export default ( state = [], action ) => {
 			return [];
 		case BREADCRUMB_UPDATE_LIST:
 			return [ ...action.items ];
-		case BREADCRUMB_APPEND_ITEM:
-			// If it is the same kind as the last item, replace it.
-			// eslint-disable-next-line no-case-declarations
-			const currentLastItem = state[ state.length - 1 ] || {};
-			if ( currentLastItem.id === action.item?.id ) {
-				state.pop();
+		case BREADCRUMB_APPEND_ITEM: {
+			// If the new item already exists, clear crumbs back to & including the existing item, & add the new item.
+			const existingItemIndex = state.findIndex( ( item ) => item.id === action.item?.id );
+			if ( existingItemIndex > -1 ) {
+				state = state.slice( 0, existingItemIndex );
 			}
-
 			return [ ...state, action.item ];
+		}
 	}
 
 	return state;
