f2d3f30c05d5b07e6961b0765dc23c5c1e7c3277
Ben Zhu <6070516+benzhu56@users.noreply.github.com>
2022-09-20 09:45:04 +1200

    Add select sites page for advertising. (#67950)
    Co-authored-by: Ben Zhu <ben.zhu@a8c.com>

--- client/my-sites/promote-post/index.js
+++ client/my-sites/promote-post/index.js
@@ -1,9 +1,11 @@
 import page from 'page';
 import { makeLayout, render as clientRender } from 'calypso/controller';
-import { navigation, siteSelection } from 'calypso/my-sites/controller';
+import { navigation, sites, siteSelection } from 'calypso/my-sites/controller';
 import { promotedPosts } from './controller';
 
 export default () => {
+	page( '/advertising', siteSelection, sites, makeLayout, clientRender );
+
 	page(
 		'/advertising/:site?/:tab?',
 		siteSelection,
