2ec07733414b030ebb7521872dfb9b79ed53fd82
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2022-09-20 08:26:06 -0300

    Sites Management Page: Update status and sorting dropdown labels (#67936)
    Co-authored-by: Daniel Bachhuber <daniel.bachhuber@automattic.com>

--- client/sites-dashboard/components/sites-content-controls.tsx
+++ client/sites-dashboard/components/sites-content-controls.tsx
@@ -1,5 +1,6 @@
 import { FilterableSiteLaunchStatuses, useSitesTableFiltering } from '@automattic/components';
 import styled from '@emotion/styled';
+import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import page from 'page';
 import { ComponentPropsWithoutRef } from 'react';
@@ -129,7 +130,12 @@ export const SitesContentControls = ( {
 				defaultValue={ initialSearch }
 			/>
 			<DisplayControls>
-				<ControlsSelectDropdown selectedText={ selectedStatus.title }>
+				<ControlsSelectDropdown
+					// Translators: `siteStatus` is one of the site statuses specified in the Sites page.
+					selectedText={ sprintf( __( 'Status: %(siteStatus)s' ), {
+						siteStatus: selectedStatus.title,
+					} ) }
+				>
 					{ statuses.map( ( { name, title, count } ) => (
 						<SelectDropdown.Item
 							key={ name }
