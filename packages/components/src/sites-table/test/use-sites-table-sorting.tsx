5c068e495e8dda8b02d6ee5dff52a5101fc7c129
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-09-20 13:49:43 -0700

    Sites Management Page: Use scoring algo to promote interaction recency (#68041)
    Co-authored-by: Luis Felipe Zaguini <luisfelipezaguini@gmail.com>

--- packages/components/src/sites-table/test/use-sites-table-sorting.tsx
+++ packages/components/src/sites-table/test/use-sites-table-sorting.tsx
@@ -35,6 +35,32 @@ describe( 'useSitesTableSorting', () => {
 			},
 		},
 	];
+	const frequentSites = [
+		{
+			ID: 1,
+			title: 'B',
+			options: {
+				updated_at: '2022-05-27T07:19:20+00:00',
+			},
+			user_interactions: [ '2022-09-20' ],
+		},
+		{
+			ID: 2,
+			title: 'A',
+			options: {
+				updated_at: '2022-07-13T17:17:12+00:00',
+			},
+			user_interactions: [ '2022-09-19' ],
+		},
+		{
+			ID: 3,
+			title: 'C',
+			options: {
+				updated_at: '2022-06-14T13:32:34+00:00',
+			},
+			user_interactions: [ '2022-09-19', '2022-09-16', '2022-09-13', '2022-09-09' ],
+		},
+	];
 
 	test( 'should not sort sites if unsupported sortKey is provided', () => {
 		const { result } = renderHook( () =>
@@ -101,9 +127,35 @@ describe( 'useSitesTableSorting', () => {
 		);
 
 		expect( result.current.sortedSites.length ).toBe( 3 );
-		expect( result.current.sortedSites[ 0 ].title ).toBe( 'B' );
-		expect( result.current.sortedSites[ 1 ].title ).toBe( 'A' );
+		expect( result.current.sortedSites[ 0 ].title ).toBe( 'C' );
+		expect( result.current.sortedSites[ 1 ].title ).toBe( 'B' );
+		expect( result.current.sortedSites[ 2 ].title ).toBe( 'A' );
+	} );
+
+	test( 'should pick the site more frequently interacted with ascending', () => {
+		const { result } = renderHook( () =>
+			useSitesTableSorting( frequentSites, {
+				sortKey: 'lastInteractedWith',
+				sortOrder: 'asc',
+			} )
+		);
+		expect( result.current.sortedSites.length ).toBe( 3 );
+		expect( result.current.sortedSites[ 0 ].title ).toBe( 'A' );
+		expect( result.current.sortedSites[ 1 ].title ).toBe( 'B' );
 		expect( result.current.sortedSites[ 2 ].title ).toBe( 'C' );
+	} );
+
+	test( 'should pick the site more frequently interacted with descending', () => {
+		const { result } = renderHook( () =>
+			useSitesTableSorting( frequentSites, {
+				sortKey: 'lastInteractedWith',
+				sortOrder: 'desc',
+			} )
+		);
+		expect( result.current.sortedSites.length ).toBe( 3 );
+		expect( result.current.sortedSites[ 0 ].title ).toBe( 'C' );
+		expect( result.current.sortedSites[ 1 ].title ).toBe( 'B' );
+		expect( result.current.sortedSites[ 2 ].title ).toBe( 'A' );
 	} );
 
 	test( 'should sort sites by updatedAt descending', () => {
@@ -138,9 +190,13 @@ describe( 'useSitesTableSorting', () => {
 		// Object equality is important for memoizing the site table row
 
 		const { result, rerender } = renderHook(
-			( { sortOrder } ) =>
-				useSitesTableSorting( filteredSites, { sortKey: 'updatedAt', sortOrder } ),
-			{ initialProps: { sortOrder: 'asc' as SitesTableSortOrder } }
+			( { sortKey, sortOrder } ) => useSitesTableSorting( filteredSites, { sortKey, sortOrder } ),
+			{
+				initialProps: {
+					sortKey: 'updatedAt' as SitesTableSortKey,
+					sortOrder: 'asc' as SitesTableSortOrder,
+				},
+			}
 		);
 
 		expect( result.current.sortedSites ).toHaveLength( 3 );
@@ -148,11 +204,26 @@ describe( 'useSitesTableSorting', () => {
 		expect( result.current.sortedSites[ 1 ] ).toBe( filteredSites[ 2 ] );
 		expect( result.current.sortedSites[ 2 ] ).toBe( filteredSites[ 1 ] );
 
-		rerender( { sortOrder: 'desc' } );
+		rerender( { sortKey: 'updatedAt', sortOrder: 'desc' } );
 
+		// A (1) -> C (2) -> B (0)
 		expect( result.current.sortedSites ).toHaveLength( 3 );
 		expect( result.current.sortedSites[ 0 ] ).toBe( filteredSites[ 1 ] );
 		expect( result.current.sortedSites[ 1 ] ).toBe( filteredSites[ 2 ] );
 		expect( result.current.sortedSites[ 2 ] ).toBe( filteredSites[ 0 ] );
+
+		// A (1) -> B (0) -> C (2)
+		rerender( { sortKey: 'alphabetically', sortOrder: 'asc' } );
+		expect( result.current.sortedSites ).toHaveLength( 3 );
+		expect( result.current.sortedSites[ 0 ] ).toBe( filteredSites[ 1 ] );
+		expect( result.current.sortedSites[ 1 ] ).toBe( filteredSites[ 0 ] );
+		expect( result.current.sortedSites[ 2 ] ).toBe( filteredSites[ 2 ] );
+
+		// A (1) -> B (0) -> C (2)
+		rerender( { sortKey: 'lastInteractedWith', sortOrder: 'desc' } );
+		expect( result.current.sortedSites ).toHaveLength( 3 );
+		expect( result.current.sortedSites[ 0 ] ).toBe( filteredSites[ 1 ] );
+		expect( result.current.sortedSites[ 1 ] ).toBe( filteredSites[ 0 ] );
+		expect( result.current.sortedSites[ 2 ] ).toBe( filteredSites[ 2 ] );
 	} );
 } );
