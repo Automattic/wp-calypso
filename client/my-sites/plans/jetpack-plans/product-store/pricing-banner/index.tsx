1d9cdcabea367122490ac3beac3407f081894454
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-09-20 12:40:34 +0800

    Temporarily remove Jetpack social from pricing intro banner calculation.

--- client/my-sites/plans/jetpack-plans/product-store/pricing-banner/index.tsx
+++ client/my-sites/plans/jetpack-plans/product-store/pricing-banner/index.tsx
@@ -1,3 +1,5 @@
+import { JETPACK_SOCIAL_PRODUCTS } from '@automattic/calypso-products';
+import { useMemo } from 'react';
 import IntroPricingBanner from 'calypso/components/jetpack/intro-pricing-banner';
 import useProductSlugs from '../hooks/use-product-slugs';
 import { ProductSlugsProps } from '../types';
@@ -7,9 +9,19 @@ import './style.scss';
 export const PricingBanner: React.FC< ProductSlugsProps > = ( { siteId, duration } ) => {
 	const productSlugs = useProductSlugs( { siteId, duration } );
 
+	// TO-DO: Once Jetpack Social pricing tiers and the introductory discount have been worked on, filtering should be removed.
+	const filteredProductSlugs = useMemo(
+		() =>
+			productSlugs.filter(
+				( productSlug ) =>
+					! ( JETPACK_SOCIAL_PRODUCTS as ReadonlyArray< string > ).includes( productSlug )
+			),
+		[ productSlugs ]
+	);
+
 	return (
 		<div className="jetpack-product-store__pricing-banner">
-			<IntroPricingBanner productSlugs={ productSlugs } siteId={ siteId ?? 'none' } />
+			<IntroPricingBanner productSlugs={ filteredProductSlugs } siteId={ siteId ?? 'none' } />
 		</div>
 	);
 };
