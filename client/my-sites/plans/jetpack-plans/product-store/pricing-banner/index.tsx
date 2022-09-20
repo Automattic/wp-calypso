61f449a992418c7ead1f11779b707a345935f2be
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-09-20 13:22:53 +0800

    Move filtering within the useProductSlugs hook.

--- client/my-sites/plans/jetpack-plans/product-store/pricing-banner/index.tsx
+++ client/my-sites/plans/jetpack-plans/product-store/pricing-banner/index.tsx
@@ -1,5 +1,3 @@
-import { JETPACK_SOCIAL_PRODUCTS } from '@automattic/calypso-products';
-import { useMemo } from 'react';
 import IntroPricingBanner from 'calypso/components/jetpack/intro-pricing-banner';
 import useProductSlugs from '../hooks/use-product-slugs';
 import { ProductSlugsProps } from '../types';
@@ -9,19 +7,9 @@ import './style.scss';
 export const PricingBanner: React.FC< ProductSlugsProps > = ( { siteId, duration } ) => {
 	const productSlugs = useProductSlugs( { siteId, duration } );
 
-	// TO-DO: Once Jetpack Social pricing tiers and the introductory discount have been worked on, filtering should be removed.
-	const filteredProductSlugs = useMemo(
-		() =>
-			productSlugs.filter(
-				( productSlug ) =>
-					! ( JETPACK_SOCIAL_PRODUCTS as ReadonlyArray< string > ).includes( productSlug )
-			),
-		[ productSlugs ]
-	);
-
 	return (
 		<div className="jetpack-product-store__pricing-banner">
-			<IntroPricingBanner productSlugs={ filteredProductSlugs } siteId={ siteId ?? 'none' } />
+			<IntroPricingBanner productSlugs={ productSlugs } siteId={ siteId ?? 'none' } />
 		</div>
 	);
 };
