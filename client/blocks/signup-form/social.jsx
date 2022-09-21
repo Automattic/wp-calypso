cf6ff6ae23ff293dcb3891867427ebfbd2a22323
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-09-21 15:36:06 +0800

    Revert "Add SSO options to wpcc signup flow (#67537)"
    This reverts commit 28b6e6ae247f53c54a8866d4b86316aedbb3ed21.

--- client/blocks/signup-form/social.jsx
+++ client/blocks/signup-form/social.jsx
@@ -1,5 +1,4 @@
 import config from '@automattic/calypso-config';
-import debugFactory from 'debug';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
@@ -7,13 +6,9 @@ import { connect } from 'react-redux';
 import AppleLoginButton from 'calypso/components/social-buttons/apple';
 import GoogleSocialButton from 'calypso/components/social-buttons/google';
 import { preventWidows } from 'calypso/lib/formatting';
-import { login } from 'calypso/lib/paths';
-import { isWpccFlow } from 'calypso/signup/utils';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import SocialSignupToS from './social-signup-tos';
-
-const debug = debugFactory( 'calypso:signup:social' );
 
 class SocialSignupForm extends Component {
 	static propTypes = {
@@ -23,8 +18,6 @@ class SocialSignupForm extends Component {
 		socialService: PropTypes.string,
 		socialServiceResponse: PropTypes.object,
 		disableTosText: PropTypes.bool,
-		flowName: PropTypes.string,
-		redirectToAfterLoginUrl: PropTypes.string,
 	};
 
 	static defaultProps = {
@@ -56,19 +49,10 @@ class SocialSignupForm extends Component {
 		this.props.handleResponse( 'google', tokens.access_token, tokens.id_token );
 	};
 
-	trackAndPossiblyRememberSocialSignup = ( service ) => {
+	trackSocialSignup = ( service ) => {
 		this.props.recordTracksEvent( 'calypso_signup_social_button_click', {
 			social_account_type: service,
 		} );
-
-		if ( isWpccFlow( this.props.flowName ) && this.props.redirectToAfterLoginUrl ) {
-			try {
-				// Persist the redirect URL for the redirect flow so that we can redirect the user back to original page after login. See ./client/blocks/login for more details.
-				window.sessionStorage.setItem( 'login_redirect_to', this.props.redirectToAfterLoginUrl );
-			} catch ( e ) {
-				debug( 'Set login_redirect_to session error', e );
-			}
-		}
 	};
 
 	shouldUseRedirectFlow() {
@@ -87,17 +71,10 @@ class SocialSignupForm extends Component {
 		return isPopup;
 	}
 
-	getRedirectUri = ( socialService ) => {
-		const origin = typeof window !== 'undefined' && window.location.origin;
-
-		// If the user is in the WPCC flow, we want to redirect user to login callback so that we can automatically log them in.
-		return isWpccFlow( this.props.flowName )
-			? `${ origin + login( { socialService } ) }`
-			: `${ origin }/start/user`;
-	};
-
 	render() {
 		const uxMode = this.shouldUseRedirectFlow() ? 'redirect' : 'popup';
+		const host = typeof window !== 'undefined' && window.location.host;
+		const redirectUri = `https://${ host }/start/user`;
 		const uxModeApple = config.isEnabled( 'sign-in-with-apple/redirect' ) ? 'redirect' : uxMode;
 
 		return (
@@ -117,8 +94,8 @@ class SocialSignupForm extends Component {
 							clientId={ config( 'google_oauth_client_id' ) }
 							responseHandler={ this.handleGoogleResponse }
 							uxMode={ uxMode }
-							redirectUri={ this.getRedirectUri( 'google' ) }
-							onClick={ () => this.trackAndPossiblyRememberSocialSignup( 'google' ) }
+							redirectUri={ redirectUri }
+							onClick={ () => this.trackSocialSignup( 'google' ) }
 							socialServiceResponse={
 								this.props.socialService === 'google' ? this.props.socialServiceResponse : null
 							}
@@ -130,20 +107,11 @@ class SocialSignupForm extends Component {
 							clientId={ config( 'apple_oauth_client_id' ) }
 							responseHandler={ this.handleAppleResponse }
 							uxMode={ uxModeApple }
-							redirectUri={ this.getRedirectUri( 'apple' ) }
-							onClick={ () => {
-								this.trackAndPossiblyRememberSocialSignup( 'apple' );
-							} }
+							redirectUri={ redirectUri }
+							onClick={ () => this.trackSocialSignup( 'apple' ) }
 							socialServiceResponse={
 								this.props.socialService === 'apple' ? this.props.socialServiceResponse : null
 							}
-							originalUrlPath={
-								// Set the original URL path for wpcc flow so that we can redirect the user back to /start/wpcc after Apple callback.
-								isWpccFlow( this.props.flowName ) ? window.location.pathname : null
-							}
-							// Attach the query string to the state so we can pass it back to the server to show the correct UI.
-							// We need this because Apple doesn't allow to have dynamic parameters in redirect_uri.
-							queryString={ isWpccFlow( this.props.flowName ) ? window.location.search : null }
 						/>
 
 						{ ! this.props.disableTosText && <SocialSignupToS /> }
