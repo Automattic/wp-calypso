82bac45607b0b9a5529d9163d5dfdab3292d5791
leonardost <leonardost@users.noreply.github.com>
2022-09-21 19:39:56 -0300

    Domains: Add explanation when trying to transfer domain that is in 60 days transfer lock (#67475)
    * Add messages explaining why recently registered domains cannot be transferred in
    
    * Do not show the recently registered domain notice when looking for a domain to register
    
    We should only show only the "<domain> is already registered. Do you own it?" notice
    in cases where a newly registered domain is searched.
    
    * Add new server_transfer_prohibited_not_transferrable status messages
    
    * Update wording of messages for transfer locked domains

--- client/components/domains/register-domain-step/index.jsx
+++ client/components/domains/register-domain-step/index.jsx
@@ -1439,9 +1439,16 @@ class RegisterDomainStep extends Component {
 	};
 
 	showAvailabilityErrorMessage( domain, error, errorData ) {
-		const { DOTBLOG_SUBDOMAIN, TRANSFERRABLE } = domainAvailability;
+		const {
+			DOTBLOG_SUBDOMAIN,
+			TRANSFERRABLE,
+			RECENT_REGISTRATION_LOCK_NOT_TRANSFERRABLE,
+			SERVER_TRANSFER_PROHIBITED_NOT_TRANSFERRABLE,
+		} = domainAvailability;
 		if (
 			( TRANSFERRABLE === error && this.state.lastDomainIsTransferrable ) ||
+			RECENT_REGISTRATION_LOCK_NOT_TRANSFERRABLE === error ||
+			SERVER_TRANSFER_PROHIBITED_NOT_TRANSFERRABLE === error ||
 			( this.props.isSignupStep && DOTBLOG_SUBDOMAIN === error )
 		) {
 			return;
@@ -1455,9 +1462,16 @@ class RegisterDomainStep extends Component {
 	}
 
 	showSuggestionErrorMessage( domain, error, errorData ) {
-		const { DOTBLOG_SUBDOMAIN, TRANSFERRABLE } = domainAvailability;
+		const {
+			DOTBLOG_SUBDOMAIN,
+			TRANSFERRABLE,
+			RECENT_REGISTRATION_LOCK_NOT_TRANSFERRABLE,
+			SERVER_TRANSFER_PROHIBITED_NOT_TRANSFERRABLE,
+		} = domainAvailability;
 		if (
 			( TRANSFERRABLE === error && this.state.lastDomainIsTransferrable ) ||
+			RECENT_REGISTRATION_LOCK_NOT_TRANSFERRABLE === error ||
+			SERVER_TRANSFER_PROHIBITED_NOT_TRANSFERRABLE === error ||
 			( this.props.isSignupStep && DOTBLOG_SUBDOMAIN === error )
 		) {
 			return;
