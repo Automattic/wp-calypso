26d0804090f048a2b043e4e7ab64d51c6d7230f2
Daniel López <daniel.lopez@automattic.com>
2022-09-22 18:57:03 +0200

    Emails: Disable select buttons, gather information for the domain owner and use it in the new link buttons

--- client/my-sites/email/email-providers-comparison/stacked/provider-cards/provider-card-props.ts
+++ client/my-sites/email/email-providers-comparison/stacked/provider-cards/provider-card-props.ts
@@ -14,7 +14,6 @@ export interface ProviderCardProps {
 	formFields?: ReactElement;
 	logo?: ReactElement | { path: string; className?: string };
 	onExpandedChange?: ( providerKey: string, expanded: boolean ) => void;
-	overrideToggleSelectorOnClick?: ( () => void ) | null;
 	priceBadge?: ReactElement | TranslateResult | null;
 	productName: TranslateResult;
 	providerKey: string;
