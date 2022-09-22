b928a039ec6c6d3d6f746126c4e761fea3d266d4
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-09-22 16:26:43 +0800

    Add sso options to wpcc signup flow (#68090)
    * Perform regular login after SSO signup in the wpcc flow
    
    The logic was added in bba4f232ca1974ebdb56b8425fec90a2f5b431cc
    
    However, it doesn't not automatically log the user anymore so remove the
    logic
    
    * Update oauth-client form style for woo
    
    * Enable social signup for wpcc flow
    
    * Update redirect uri and save redirectToAfterLoginUrl for wpcc flow with Apple login
    
    * Persist url query string in apple sso state
    
    So that we can show the same UI after redirecting back to the calypso
    
    * Make sure the tos link display with woo color
    
    * Make social button width the same size
    
    * Use winodw.location.origin directly for getRedirectUri instead
    
    * Redirect apple login to state.originalUrlPath
    
    * Add default values to originalUrlPath and queryString props
    
    * Fix social response handling
    
    * Fix userIsLoggedIn bug

--- client/signup/steps/user/index.jsx
+++ client/signup/steps/user/index.jsx
@@ -434,22 +434,22 @@ export class UserStep extends Component {
 	}
 
 	renderSignupForm() {
-		const { oauth2Client, wccomFrom, isReskinned } = this.props;
+		const { oauth2Client, isReskinned } = this.props;
 		let socialService;
 		let socialServiceResponse;
 		let isSocialSignupEnabled = this.props.isSocialSignupEnabled;
+
+		if ( isWooOAuth2Client( oauth2Client ) ) {
+			isSocialSignupEnabled = true;
+		}
+
 		const hashObject = this.props.initialContext && this.props.initialContext.hash;
-		if ( this.props.isSocialSignupEnabled && ! isEmpty( hashObject ) ) {
+		if ( isSocialSignupEnabled && ! isEmpty( hashObject ) ) {
 			const clientId = hashObject.client_id;
 			socialService = getSocialServiceFromClientId( clientId );
-
 			if ( socialService ) {
 				socialServiceResponse = hashObject;
 			}
-		}
-
-		if ( isWooOAuth2Client( oauth2Client ) && wccomFrom ) {
-			isSocialSignupEnabled = true;
 		}
 
 		return (
