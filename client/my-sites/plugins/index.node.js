f793ac39503fface4361d54c9333a27f453d6d6c
Claudiu Filip <claudiucelfilip@gmail.com>
2022-09-23 12:44:30 +0300

    Use server sanitizer for plugins content

--- client/my-sites/plugins/index.node.js
+++ client/my-sites/plugins/index.node.js
@@ -1,6 +1,8 @@
 import { getLanguageRouteParam } from '@automattic/i18n-utils';
 import { makeLayout, ssrSetupLocale } from 'calypso/controller';
 import { setHrefLangLinks, setLocalizedCanonicalUrl } from 'calypso/controller/localized-links';
+import { overrideSanitizeSectionRoot } from 'calypso/lib/plugins/sanitize-section-content';
+import { getServerRoot } from 'calypso/lib/plugins/server-dom-parser';
 import { isEnabled } from 'calypso/server/config';
 import { browsePlugins } from './controller';
 import { fetchPlugins, skipIfLoggedIn } from './controller-logged-out';
@@ -9,6 +11,8 @@ export default function ( router ) {
 	const langParam = getLanguageRouteParam();
 
 	if ( isEnabled( 'plugins/ssr-landing' ) ) {
+		overrideSanitizeSectionRoot( getServerRoot() );
+
 		router(
 			[ `/${ langParam }/plugins` ],
 			skipIfLoggedIn,
