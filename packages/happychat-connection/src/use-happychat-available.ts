3db4fcf2a430577eb8db791660e9a5048e1f985d
Omar Alshaker <omar@omaralshaker.com>
2022-09-23 13:23:31 +0200

    Fix commentary

--- packages/happychat-connection/src/use-happychat-available.ts
+++ packages/happychat-connection/src/use-happychat-available.ts
@@ -38,7 +38,8 @@ function getHCAvailabilityAndStatus( dataAuth: HappychatAuth ) {
 }
 
 /**
- * Opens a socket connection to Happychat to check if it's available and of the user has a session. Caches the answer for 10 minutes or until page refresh.
+ * Opens a socket connection to Happychat to check if it's available and if the user has an active session.
+ * By default, it caches the answer for 10 minutes or until page refresh.
  *
  * @param enabled on/off switch
  * @param staleTime time in ms to cache the result
