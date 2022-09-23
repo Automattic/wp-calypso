57327d1f24ad2f8c1a5644d96a66a98e75b9e704
Paulo Trentin <paulo@paulotrentin.com.br>
2022-09-23 09:12:33 -0300

    Fixed too wide css rules

--- client/my-sites/plugins/plans/index.tsx
+++ client/my-sites/plugins/plans/index.tsx
@@ -88,7 +88,7 @@ const Plans = ( { intervalType }: { intervalType: 'yearly' | 'monthly' } ) => {
 	};
 
 	return (
-		<MainComponent wideLayout>
+		<MainComponent className="plugin-plans" wideLayout>
 			<PageViewTracker path="/plugins/plans/:interval/:site" title="Plugins > Plan Upgrade" />
 			<DocumentHead title={ translate( 'Plugins > Plan Upgrade' ) } />
 			<FixedNavigationHeader navigationItems={ breadcrumbs } />
