5c851e7b9d10c2a7a3e08ee8ee72c4afba712a67
edanzer <edanzer@gmail.com>
2022-09-26 12:40:57 -0600

    Create common SetupForm component

---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/shared-components/setup-form/accent-color-control.tsx
@@ -0,0 +1,100 @@
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+import { Popover } from '@automattic/components';
+import { hasMinContrast, RGB } from '@automattic/onboarding';
+import { ColorPicker } from '@wordpress/components';
+import { Icon } from '@wordpress/icons';
+import { useI18n } from '@wordpress/react-i18n';
+import classNames from 'classnames';
+import { Dispatch, SetStateAction, useState, useRef } from 'react';
+import FormFieldset from 'calypso/components/forms/form-fieldset';
+import FormLabel from 'calypso/components/forms/form-label';
+import FormInput from 'calypso/components/forms/form-text-input';
+import { tip } from 'calypso/signup/icons';
+
+interface AccentColorControlProps {
+	accentColor: AccentColor;
+	setAccentColor: Dispatch< SetStateAction< AccentColor > >;
+}
+
+export type AccentColor = {
+	hex: string;
+	rgb: RGB;
+	default?: boolean;
+};
+
+/**
+ * Generates an inline SVG for the color picker swatch
+ *
+ * @param color the color in HEX
+ * @returns a value for background-image
+ */
+function generateSwatchSVG( color: string | undefined ) {
+	return `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Ccircle cx='12' cy='12' r='10' stroke='%23ccc' stroke-width='1' fill='${ encodeURIComponent(
+		color || '#fff'
+	) }'%3E%3C/circle%3E${
+		// render a line when a color isn't selected
+		! color
+			? `%3Cline x1='18' y1='4' x2='7' y2='20' stroke='%23ccc' stroke-width='1'%3E%3C/line%3E`
+			: ''
+	}%3C/svg%3E")`;
+}
+
+const AccentColorControl = ( { accentColor, setAccentColor }: AccentColorControlProps ) => {
+	const { __ } = useI18n();
+	const [ colorPickerOpen, setColorPickerOpen ] = useState< boolean >( false );
+	const accentColorRef = useRef< HTMLInputElement >( null );
+
+	return (
+		<>
+			<Popover
+				isVisible={ colorPickerOpen }
+				className="setup-form__accent-color-popover"
+				context={ accentColorRef.current }
+				position="top left"
+				onClose={ () => setColorPickerOpen( false ) }
+			>
+				<ColorPicker
+					disableAlpha
+					color={ accentColor.hex }
+					onChangeComplete={ ( { hex, rgb } ) =>
+						setAccentColor( { hex, rgb: rgb as unknown as RGB } )
+					}
+				/>
+			</Popover>
+			<FormFieldset>
+				<FormLabel htmlFor="accentColor">{ __( 'Accent Color' ) }</FormLabel>
+				<FormInput
+					inputRef={ accentColorRef }
+					className="setup-form__accent-color"
+					style={ {
+						backgroundImage: generateSwatchSVG( accentColor.hex ),
+						...( accentColor.default && { color: 'var( --studio-gray-30 )' } ),
+					} }
+					name="accentColor"
+					id="accentColor"
+					onFocus={ () => setColorPickerOpen( ! colorPickerOpen ) }
+					readOnly
+					value={ accentColor.hex }
+				/>
+			</FormFieldset>
+			<div
+				className={ classNames( 'setup-form__contrast-warning', {
+					'is-visible': ! hasMinContrast( accentColor.rgb ),
+				} ) }
+				aria-hidden={ hasMinContrast( accentColor.rgb ) }
+				role="alert"
+			>
+				<div className="setup-form__contrast-warning-icon-container">
+					<Icon icon={ tip } size={ 20 } />
+				</div>
+				<div>
+					{ __(
+						'The accent color chosen may make your buttons and links illegible. Consider picking a darker color.'
+					) }
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default AccentColorControl;
---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/shared-components/setup-form/index.tsx
@@ -0,0 +1,132 @@
+/* eslint-disable wpcalypso/jsx-classname-namespace */
+import { Button, FormInputValidation } from '@automattic/components';
+import { useI18n } from '@wordpress/react-i18n';
+import { Dispatch, FormEvent, SetStateAction, useEffect } from 'react';
+import { SiteDetails } from 'calypso/../packages/data-stores/src';
+import { ForwardedAutoresizingFormTextarea } from 'calypso/blocks/comments/autoresizing-form-textarea';
+import FormFieldset from 'calypso/components/forms/form-fieldset';
+import FormLabel from 'calypso/components/forms/form-label';
+import FormInput from 'calypso/components/forms/form-text-input';
+import { SiteIconWithPicker } from 'calypso/components/site-icon-with-picker';
+import { useSiteSlugParam } from 'calypso/landing/stepper/hooks/use-site-slug-param';
+import AccentColorControl, { AccentColor } from './accent-color-control';
+
+import './style.scss';
+
+interface SetupFormProps {
+	site: SiteDetails | null;
+	siteTitle: string;
+	setComponentSiteTitle: Dispatch< SetStateAction< string > >;
+	invalidSiteTitle: boolean;
+	setInvalidSiteTitle: Dispatch< SetStateAction< boolean > >;
+	tagline: string;
+	setTagline: Dispatch< SetStateAction< string > >;
+	accentColor?: AccentColor;
+	setAccentColor?: Dispatch< SetStateAction< AccentColor > >;
+	selectedFile: File | undefined;
+	setSelectedFile: Dispatch< SetStateAction< File | undefined > >;
+	setBase64Image: Dispatch< SetStateAction< string | null | undefined > >;
+	handleSubmit: ( event: FormEvent< Element > ) => void;
+	isLoading?: boolean;
+	isSubmitError?: boolean;
+}
+
+const SetupForm = ( {
+	site,
+	siteTitle,
+	setComponentSiteTitle,
+	invalidSiteTitle,
+	setInvalidSiteTitle,
+	tagline,
+	setTagline,
+	accentColor,
+	setAccentColor,
+	selectedFile,
+	setSelectedFile,
+	setBase64Image,
+	handleSubmit,
+	isLoading = false,
+	isSubmitError = false,
+}: SetupFormProps ) => {
+	const { __ } = useI18n();
+	const usesSite = !! useSiteSlugParam();
+
+	const imageFileToBase64 = ( file: Blob ) => {
+		const reader = new FileReader();
+		reader.readAsDataURL( file );
+		reader.onload = () => setBase64Image( reader.result as string );
+		reader.onerror = () => setBase64Image( null );
+	};
+
+	const onChange = ( event: React.FormEvent< HTMLInputElement > ) => {
+		switch ( event.currentTarget.name ) {
+			case 'setup-form-input-name':
+				return setComponentSiteTitle( event.currentTarget.value );
+			case 'setup-form-input-description':
+				return setTagline( event.currentTarget.value );
+		}
+	};
+
+	useEffect( () => {
+		if ( siteTitle.trim().length && invalidSiteTitle ) {
+			setInvalidSiteTitle( false );
+		}
+	}, [ siteTitle, invalidSiteTitle, setInvalidSiteTitle ] );
+
+	return (
+		<form className="setup-form__form" onSubmit={ handleSubmit }>
+			<SiteIconWithPicker
+				site={ site }
+				placeholderText={ __( 'Upload a profile image' ) }
+				onSelect={ ( file ) => {
+					setSelectedFile( file );
+					imageFileToBase64( file );
+				} }
+				disabled={ usesSite ? ! site : false }
+				selectedFile={ selectedFile }
+			/>
+			<FormFieldset>
+				<FormLabel htmlFor="setup-form-input-name">{ __( 'Site name' ) }</FormLabel>
+				<FormInput
+					name="setup-form-input-name"
+					id="setup-form-input-name"
+					value={ siteTitle }
+					onChange={ onChange }
+					placeholder={ __( 'My Site Name' ) }
+					isError={ invalidSiteTitle }
+				/>
+				{ invalidSiteTitle && (
+					<FormInputValidation
+						isError
+						text={ __( `Oops. Looks like your site doesn't have a name yet.` ) }
+					/>
+				) }
+			</FormFieldset>
+			<FormFieldset>
+				<FormLabel htmlFor="setup-form-input-description">{ __( 'Brief description' ) }</FormLabel>
+				<ForwardedAutoresizingFormTextarea
+					name="setup-form-input-description"
+					id="setup-form-input-description"
+					value={ tagline }
+					placeholder={ __( 'Add a short description here' ) }
+					enableAutoFocus={ false }
+					onChange={ onChange }
+				/>
+			</FormFieldset>
+			{ accentColor && setAccentColor && (
+				<AccentColorControl accentColor={ accentColor } setAccentColor={ setAccentColor } />
+			) }
+			<Button className="setup-form__submit" disabled={ isLoading } primary type="submit">
+				{ isLoading ? __( 'Loading' ) : __( 'Continue' ) }
+			</Button>
+			{ isSubmitError && (
+				<FormInputValidation
+					isError={ isSubmitError }
+					text={ __( 'Oops, there was an issue! Please try again.' ) }
+				/>
+			) }
+		</form>
+	);
+};
+
+export default SetupForm;
