3103128840d6fbaec6c7ac2ae5b221efa8452035
Eoin Gallagher <eoin@automattic.com>
2022-09-26 21:24:06 +0100

    need to use camelcase

--- client/reader/components/icons/comment-icon.jsx
+++ client/reader/components/icons/comment-icon.jsx
@@ -19,16 +19,16 @@ export default function ReaderCommentIcon( { iconSize } ) {
 				d="m12.2778 14.5556v-.75h-.75-8.00002c-.56801 0-1.02778-.4598-1.02778-1.0278v-8.00002c0-.56801.45977-1.02778 1.02778-1.02778h12.44442c.568 0 1.0278.45977 1.0278 1.02778v7.94842c0 .9051-.4384 1.7561-1.1748 2.2822l-3.5474 2.5341z"
 				fill="#fff"
 				stroke="#646970"
-				stroke-width="1.5"
+				strokeWidth="1.5"
 			/>
 			<path d="m10.5 6.5h4v4h4.5v-8.5h-8.5z" fill="#fff" />
 			<g fill="#606a73">
-				<path d="m5.625 6.875h7.25v.75h-7.25z" stroke="#646970" stroke-width=".75" />
-				<path d="m5.625 9.875h3.25v.75h-3.25z" stroke="#646970" stroke-width=".75" />
+				<path d="m5.625 6.875h7.25v.75h-7.25z" stroke="#646970" strokeWidth=".75" />
+				<path d="m5.625 9.875h3.25v.75h-3.25z" stroke="#646970" strokeWidth=".75" />
 				<path
-					clip-rule="evenodd"
+					clipRule="evenodd"
 					d="m17.75.75h-1.5v2.25h-2.25v1.5h2.25v2.25h1.5v-2.25h2.25v-1.5h-2.25z"
-					fill-rule="evenodd"
+					fillRule="evenodd"
 				/>
 			</g>
 		</svg>
--- client/reader/components/icons/commented-icon.jsx
+++ client/reader/components/icons/commented-icon.jsx
@@ -19,13 +19,13 @@ export default function ReaderCommentedIcon( { iconSize } ) {
 				d="m12.2778 14.5556v-.75h-.75-8.00002c-.56801 0-1.02778-.4598-1.02778-1.0278v-8.00002c0-.56801.45977-1.02778 1.02778-1.02778h12.44442c.568 0 1.0278.45977 1.0278 1.02778v7.94842c0 .9051-.4384 1.7561-1.1748 2.2822l-3.5474 2.5341z"
 				fill="#fff"
 				stroke="#008a20"
-				stroke-width="1.5"
+				strokeWidth="1.5"
 			/>
 			<path d="m10.5 6.5h4v3.5h4.5v-8h-8.5z" fill="#fff" />
 			<g stroke="#008a20">
-				<path d="m5.625 6.875h7.25v.75h-7.25z" stroke-width=".75" />
-				<path d="m5.625 9.875h3.25v.75h-3.25z" stroke-width=".75" />
-				<path d="m14 4 2 2 3.5-3.5" stroke-width="1.5" />
+				<path d="m5.625 6.875h7.25v.75h-7.25z" strokeWidth=".75" />
+				<path d="m5.625 9.875h3.25v.75h-3.25z" strokeWidth=".75" />
+				<path d="m14 4 2 2 3.5-3.5" strokeWidth="1.5" />
 			</g>
 		</svg>
 	);
