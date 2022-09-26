66351b1d31d7836ca964f98c6c1b777ce9be7663
Yashwin <yashwinpoojary@gmail.com>
2022-09-26 10:01:38 +0530

    Plugin Management: replace old tables in plugin details with the new design in Calypso Blue (#68095)
    * Replace old tables in plugin details with the new design in Calypso Blue
    
    * Add "Settings" and "Manage Subscription" as site actions
    
    `
    
    * Minor UI fixes & query user purchases

--- client/my-sites/plugins/manage-site-plugins-dialog/index.jsx
+++ client/my-sites/plugins/manage-site-plugins-dialog/index.jsx
@@ -1,22 +1,19 @@
-import { FEATURE_INSTALL_PLUGINS } from '@automattic/calypso-products';
 import { Dialog, Button } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { useSelector } from 'react-redux';
-import PluginSiteList from 'calypso/my-sites/plugins/plugin-site-list';
 import { siteObjectsToSiteIds } from 'calypso/my-sites/plugins/utils';
-import { getBillingInterval } from 'calypso/state/marketplace/billing-interval/selectors';
 import {
 	getSiteObjectsWithPlugin,
 	getSiteObjectsWithoutPlugin,
 } from 'calypso/state/plugins/installed/selectors';
+import { isFetching as isWporgPluginFetchingSelector } from 'calypso/state/plugins/wporg/selectors';
 import getSelectedOrAllSitesWithPlugins from 'calypso/state/selectors/get-selected-or-all-sites-with-plugins';
-import siteHasFeature from 'calypso/state/selectors/site-has-feature';
 import './style.scss';
+import PluginAvailableOnSitesList from '../plugin-management-v2/plugin-details-v2/plugin-available-on-sites-list';
+import SitesWithInstalledPluginsList from '../plugin-management-v2/plugin-details-v2/sites-with-installed-plugin-list';
 
 export const ManageSitePluginsDialog = ( { isVisible, onClose, plugin } ) => {
 	const translate = useTranslate();
-
-	const billingPeriod = useSelector( getBillingInterval );
 
 	const sitesWithPlugins = useSelector( getSelectedOrAllSitesWithPlugins );
 	const siteIds = [ ...new Set( siteObjectsToSiteIds( sitesWithPlugins ) ) ];
@@ -28,16 +25,7 @@ export const ManageSitePluginsDialog = ( { isVisible, onClose, plugin } ) => {
 		getSiteObjectsWithoutPlugin( state, siteIds, plugin.slug )
 	);
 
-	const availableOnSites = useSelector( ( state ) =>
-		sitesWithoutPlugin.filter( ( site ) =>
-			siteHasFeature( state, site.ID, FEATURE_INSTALL_PLUGINS )
-		)
-	);
-	const upgradeNeededSites = useSelector( ( state ) =>
-		sitesWithoutPlugin.filter(
-			( site ) => ! siteHasFeature( state, site.ID, FEATURE_INSTALL_PLUGINS )
-		)
-	);
+	const isLoading = useSelector( ( state ) => isWporgPluginFetchingSelector( state, plugin.slug ) );
 
 	return (
 		<Dialog
@@ -46,32 +34,17 @@ export const ManageSitePluginsDialog = ( { isVisible, onClose, plugin } ) => {
 			onClose={ onClose }
 			shouldCloseOnEsc
 		>
-			<PluginSiteList
-				className="manage-site-plugins-dialog__plugin-list"
-				title={ translate( 'Installed on %d site', 'Installed on %d sites', {
-					comment: 'header for list of sites a plugin is installed on',
-					args: [ sitesWithPlugin.length ],
-					count: sitesWithPlugin.length,
-				} ) }
+			<SitesWithInstalledPluginsList
+				isWpCom
 				sites={ sitesWithPlugin }
+				isLoading={ isLoading }
 				plugin={ plugin }
-				showAdditionalHeaders
 			/>
 
-			<PluginSiteList
-				className="manage-site-plugins-dialog__plugin-list"
-				title={ translate( 'Available sites' ) }
-				sites={ availableOnSites }
+			<PluginAvailableOnSitesList
+				sites={ sitesWithoutPlugin }
+				isLoading={ isLoading }
 				plugin={ plugin }
-				billingPeriod={ billingPeriod }
-			/>
-
-			<PluginSiteList
-				className="manage-site-plugins-dialog__plugin-list"
-				title={ translate( 'Upgrade needed' ) }
-				sites={ upgradeNeededSites }
-				plugin={ plugin }
-				billingPeriod={ billingPeriod }
 			/>
 
 			<Button className="manage-site-plugins-dialog__finish-button" onClick={ onClose } primary>
