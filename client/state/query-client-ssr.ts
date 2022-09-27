0c6a182fa24e687ca915b262b774793a75883100
Claudiu Filip <claudiucelfilip@gmail.com>
2022-09-27 19:33:56 +0300

    Provide only queries used for SSR in react-query hydration

--- client/state/query-client-ssr.ts
+++ client/state/query-client-ssr.ts
@@ -1,9 +1,19 @@
-import { QueryClient, QueryCache, dehydrate } from 'react-query';
+import { QueryClient, QueryCache, dehydrate, hashQueryKey, TQueryKey } from 'react-query';
 import { MAX_AGE, BASE_STALE_TIME } from 'calypso/state/initial-state';
 const sharedCache = new QueryCache();
 
+class QueryClientSSR extends QueryClient {
+	fetchedQueryKeys: { [ hash: string ]: boolean } = {};
+
+	fetchQuery( queryKey: TQueryKey, ...args: any[] ) {
+		const queryHash = hashQueryKey( queryKey );
+		this.fetchedQueryKeys[ queryHash ] = true;
+		return super.fetchQuery( queryKey, ...args );
+	}
+}
+
 export function createQueryClientSSR() {
-	const queryClient = new QueryClient( {
+	const queryClient = new QueryClientSSR( {
 		defaultOptions: { queries: { cacheTime: MAX_AGE, staleTime: BASE_STALE_TIME } },
 		queryCache: sharedCache,
 	} );
@@ -11,9 +21,14 @@ export function createQueryClientSSR() {
 	return queryClient;
 }
 
-export function dehydrateQueryClient( queryClient?: QueryClient ) {
+export function dehydrateQueryClient( queryClient?: QueryClientSSR ) {
 	if ( ! queryClient ) {
 		return null;
 	}
-	return dehydrate( queryClient );
+
+	return dehydrate( queryClient, {
+		shouldDehydrateQuery: ( { queryHash } ) => {
+			return !! queryClient.fetchedQueryKeys[ queryHash ];
+		},
+	} );
 }
