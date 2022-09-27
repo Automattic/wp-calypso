6cc886fc5440c454af97b813ec044e38aa4d784d
Rafael Agostini <rafael.agostini@automattic.com>
2022-09-26 22:48:02 -0500

    Remove Lodash usage and add optional chaining and nullish coalescing
    This commit also includes testing for the getJetpackCredentialsTestStatus()
    selector.

--- client/state/selectors/get-jetpack-credentials-test-status.js
+++ client/state/selectors/get-jetpack-credentials-test-status.js
@@ -1,7 +1,5 @@
-import { get } from 'lodash';
-
 import 'calypso/state/jetpack/init';
 
 export default function getJetpackCredentialsTestStatus( state, siteId, role ) {
-	return get( state, [ 'jetpack', 'credentials', 'testRequestStatus', siteId, role ], 'pending' );
+	return state.jetpack?.credentials?.testRequestStatus?.[ siteId ]?.[ role ] || 'pending';
 }
---
+++ client/state/selectors/test/fixtures/jetpack-credentials-test-status.js
@@ -0,0 +1,14 @@
+export const sites = {
+	1230000: {
+		main: 'pending',
+	},
+	2340000: {
+		main: 'valid',
+	},
+	3450000: {
+		main: 'invalid',
+	},
+	4560000: {
+		main: 'failure',
+	},
+};
