22ce52f19d05ad545f23d46e3e44d6f5b02629bb
Daniel López <daniel.lopez@automattic.com>
2022-09-28 12:17:19 +0200

    Emails: Code improvements, rel = external on login link, moved function queryString to a higher scope.

--- client/my-sites/email/email-non-domain-owner-message/index.tsx
+++ client/my-sites/email/email-non-domain-owner-message/index.tsx
@@ -28,7 +28,6 @@ export const EmailNonDomainOwnerMessage = ( props: EmailNonDomainOwnerMessagePro
 	const { domain, selectedSite, source, usePromoCard = true } = props;
 
 	const translate = useTranslate();
-	const dispatch = useDispatch();
 
 	const ownerUserName = useDomainOwnerUserName( selectedSite, domain );
 
