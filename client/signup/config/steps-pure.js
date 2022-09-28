d5e4f8caf3c71fa1767a181a1ac9141c586630d5
Matthew Reishus <mreishus@users.noreply.github.com>
2022-09-28 13:32:00 -0500

    Initial theme for ecommerce sites: twentytwentytwo (Redux) (#68411)

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -201,8 +201,8 @@ export function generateSteps( {
 			stepName: 'plans',
 			apiRequestFunction: addPlanToCart,
 			dependencies: [ 'siteSlug' ],
-			optionalDependencies: [ 'emailItem' ],
-			providesDependencies: [ 'cartItem' ],
+			optionalDependencies: [ 'emailItem', 'themeSlugWithRepo' ],
+			providesDependencies: [ 'cartItem', 'themeSlugWithRepo' ],
 			fulfilledStepCallback: isPlanFulfilled,
 		},
 		// the only unique thing about plans-newsletter is that it provides themeSlugWithRepo and comingSoon dependencies
