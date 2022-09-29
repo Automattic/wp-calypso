7f4cc1c3f2f9f476d48a14ade3c857f479aff447
James Tien <southp0105@gmail.com>
2022-09-29 17:06:55 +0800

    Refactor `dismissCard` action to be more readable.

--- client/blocks/dismissible-card/actions.js
+++ client/blocks/dismissible-card/actions.js
@@ -1,5 +1,12 @@
 import { savePreference, setPreference } from 'calypso/state/preferences/actions';
 import { getPreferenceKey } from './utils';
 
-export const dismissCard = ( preferenceName, temporary ) =>
-	( temporary ? setPreference : savePreference )( getPreferenceKey( preferenceName ), true );
+export const dismissCard = ( preferenceName, temporary ) => {
+	const prefKey = getPreferenceKey( preferenceName );
+
+	if ( temporary ) {
+		return setPreference( prefKey, true );
+	}
+
+	return savePreference( prefKey, true );
+};
