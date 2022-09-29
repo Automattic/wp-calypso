e8c97ef302503fa80a7fdf1eeeae5d15109f0338
Payton Swick <payton@foolord.com>
2022-09-29 16:28:30 -0400

    Convert getPlanPrice to TypeScript

--- client/state/products-list/selectors/get-plan-price.ts
+++ client/state/products-list/selectors/get-plan-price.ts
@@ -1,5 +1,7 @@
 import { getPlanRawPrice } from 'calypso/state/plans/selectors';
 import { getPlanDiscountedRawPrice } from 'calypso/state/sites/plans/selectors';
+import type { Plan } from '@automattic/calypso-products';
+import type { AppState } from 'calypso/types';
 
 /**
  * Computes a price based on plan slug/constant, including any discounts available.
@@ -8,9 +10,14 @@ import { getPlanDiscountedRawPrice } from 'calypso/state/sites/plans/selectors';
  * @param {number|undefined} siteId Site ID to consider
  * @param {object} planObject Plan object returned by getPlan() from @automattic/calypso-products
  * @param {boolean} isMonthly Flag - should return a monthly price?
- * @returns {number} Requested price
+ * @returns {number|null} Requested price
  */
-export const getPlanPrice = ( state, siteId, planObject, isMonthly ) => {
+export const getPlanPrice = (
+	state: AppState,
+	siteId: number | undefined,
+	planObject: Plan,
+	isMonthly?: boolean
+): number | null => {
 	return (
 		getPlanDiscountedRawPrice( state, siteId, planObject.getStoreSlug(), { isMonthly } ) ||
 		getPlanRawPrice( state, planObject.getProductId(), isMonthly )
