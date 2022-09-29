c5757a305b8254f39874286ac1a7920e8bc04aae
mmtr <miguel.torres@automattic.com>
2022-09-29 10:46:49 +0200

    Add styles

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-global-styles/notice.js
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-global-styles/notice.js
@@ -1,9 +1,11 @@
 import { Notice } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
+import './notice.scss';
+
 const GlobalStylesNotice = () => {
 	return (
-		<Notice status="warning" isDismissible={ false }>
+		<Notice status="warning" isDismissible={ false } className="wpcom-global-styles-notice">
 			{ __(
 				"Your style changes won't be public until you upgrade your plan. You can revert your styles.",
 				'full-site-editing'
