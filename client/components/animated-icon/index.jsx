db8e3486f458a92152e6a1e901ba9c2b1e82cd53
Siobhan <siobhan@automattic.com>
2022-09-30 21:01:15 +0100

    Make it easier to implement animated icons
    By separating the animated icon component from 'app-banner/index.jsx' to its own 'animated-icon/index.jsx' file, it will be easier to re-use the component's code and to implement animated icons elsewhere in Calypo's codebase.

---
+++ client/components/animated-icon/index.jsx
@@ -0,0 +1,33 @@
+import classNames from 'classnames';
+import lottie from 'lottie-web/build/player/lottie_light';
+import { useEffect, useRef } from 'react';
+
+const AnimatedIcon = ( { icon, className } ) => {
+	const iconEl = useRef();
+
+	useEffect( () => {
+		const reducedMotion = window.matchMedia( '(prefers-reduced-motion: reduce)' ).matches;
+
+		const animation = lottie.loadAnimation( {
+			container: iconEl.current,
+			renderer: 'svg',
+			loop: false,
+			autoplay: ! reducedMotion,
+			path: icon,
+		} );
+
+		if ( reducedMotion ) {
+			animation.addEventListener( 'config_ready', () => {
+				animation.goToAndPlay( animation.totalFrames, true );
+			} );
+		}
+
+		return () => animation.destroy();
+	}, [ icon ] );
+
+	return (
+		<div ref={ iconEl } className={ classNames( 'app-animated-icon__container', className ) }></div>
+	);
+};
+
+export default AnimatedIcon;
