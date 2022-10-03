4c7ae14fcc99f275c62a20576728d07c5b12fc92
Philip Jackson <p-jackson@live.com>
2022-10-03 19:26:55 +1300

    Remove debug logging

--- apps/editing-toolkit/editing-toolkit-plugin/coming-soon/coming-soon.php
+++ apps/editing-toolkit/editing-toolkit-plugin/coming-soon/coming-soon.php
@@ -17,9 +17,6 @@ function should_show_coming_soon_page() {
 		return false;
 	}
 
-	l( 'http referer ' . $_SERVER['HTTP_REFERER'] );
-	l( 'remote host ' . $_SERVER['REMOTE_HOST'] );
-
 	// mShots should be able to take screenshots of coming soon sites, in the
 	// same way RSS readers have access to coming soon sites.
 	// phpcs:ignore WordPress.Security.NonceVerification.Recommended
