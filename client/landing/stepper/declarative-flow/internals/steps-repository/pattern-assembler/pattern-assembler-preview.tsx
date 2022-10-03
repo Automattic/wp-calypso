a4be7ed7fb2ea13e71a710c64cfe34c3a883f6c7
Miguel <miguel.san.segundo@automattic.com>
2022-10-03 11:15:58 +0700

    Add Device preview clicks

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/pattern-assembler-preview.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/pattern-assembler-preview.tsx
@@ -101,6 +101,9 @@ const PatternAssemblerPreview = ( { header, sections = [], footer }: Props ) => 
 				translate={ translate }
 				recordTracksEvent={ recordTracksEvent }
 				scrollToSelector={ scrollToSelector }
+				onDeviceUpdate={ ( device: string ) => {
+					recordTracksEvent( 'calypso_signup_bcpa_preview_device_click', { device } );
+				} }
 			/>
 		</div>
 	);
