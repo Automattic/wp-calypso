9434fc11aa46e9d8b8b1feb4c22c1f930b89fcc1
Aneesh Devasthale <aneeshd16@users.noreply.github.com>
2022-10-03 11:07:41 +0530

    DIFM Landing: Add FAQ & update feature flag (#68372)
    * DIFM Landing: Add FAQ & update feature flag
    
    * Use getDIFMTieredPriceDetails function to get extra page cost
    
    * Fixes typo

--- client/my-sites/marketing/do-it-for-me/difm-landing.tsx
+++ client/my-sites/marketing/do-it-for-me/difm-landing.tsx
@@ -8,6 +8,7 @@ import {
 	isPremium,
 	isEcommerce,
 	isPro,
+	getDIFMTieredPriceDetails,
 } from '@automattic/calypso-products';
 import { Gridicon } from '@automattic/components';
 import formatCurrency from '@automattic/format-currency';
@@ -25,7 +26,7 @@ import scrollIntoViewport from 'calypso/lib/scroll-into-viewport';
 import { getCurrentUserCurrencyCode } from 'calypso/state/currency-code/selectors';
 import { incrementCounter } from 'calypso/state/persistent-counter/actions';
 import { requestProductsList } from 'calypso/state/products-list/actions';
-import { getProductCost } from 'calypso/state/products-list/selectors';
+import { getProductBySlug } from 'calypso/state/products-list/selectors';
 import { getSitePlan } from 'calypso/state/sites/selectors';
 import type { TranslateResult } from 'i18n-calypso';
 
@@ -247,14 +248,26 @@ export default function DIFMLanding( {
 	const translate = useTranslate();
 	const dispatch = useDispatch();
 
-	const productCost = useSelector( ( state ) => getProductCost( state, WPCOM_DIFM_LITE ) );
+	const product = useSelector( ( state ) => getProductBySlug( state, WPCOM_DIFM_LITE ) );
+	const productCost = product?.cost;
+
+	const difmTieredPriceDetails = getDIFMTieredPriceDetails( product );
+	const extraPageCost = difmTieredPriceDetails?.perExtraPagePrice;
+
 	const currencyCode = useSelector( getCurrentUserCurrencyCode );
-	const hasPriceDataLoaded = productCost && currencyCode;
+	const hasPriceDataLoaded = productCost && extraPageCost && currencyCode;
 
 	const [ isLoading, setIsLoading ] = useState( true );
 
 	const displayCost = hasPriceDataLoaded
 		? formatCurrency( productCost, currencyCode, { stripZeros: true } )
+		: '';
+
+	const extraPageDisplayCost = hasPriceDataLoaded
+		? formatCurrency( extraPageCost, currencyCode, {
+				stripZeros: true,
+				isSmallestUnit: true,
+		  } )
 		: '';
 
 	const faqHeader = useRef( null );
@@ -520,6 +533,23 @@ export default function DIFMLanding( {
 						</FoldableFAQ>
 						<FoldableFAQ
 							id="faq-6"
+							question={ translate(
+								'I need more than the included 5 pages. Can I purchase additional pages?'
+							) }
+						>
+							<p>
+								{ translate(
+									'Yes! Additional pages can be purchased for %(extraPageDisplayCost)s each.',
+									{
+										args: {
+											extraPageDisplayCost,
+										},
+									}
+								) }
+							</p>
+						</FoldableFAQ>
+						<FoldableFAQ
+							id="faq-7"
 							question={ translate( 'What will my finished site look like?' ) }
 						>
 							<p>
@@ -529,7 +559,7 @@ export default function DIFMLanding( {
 							</p>
 						</FoldableFAQ>
 						<FoldableFAQ
-							id="faq-7"
+							id="faq-8"
 							question={ translate( 'What if I want changes to the finished site?' ) }
 						>
 							<p>
