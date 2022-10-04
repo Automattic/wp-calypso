e655746067a9375e5052ab8e8abfcc3518f38b37
Rafael Agostini <rafael.agostini@automattic.com>
2022-10-03 21:27:16 -0500

    Add getAreJetpackCredentialsInvalid selector
    This selector is useful to validate if existing Jetpack Credentials are still valid. Also including unit tests for different scenarios.

---
+++ client/state/jetpack/credentials/test/selectors.js
@@ -0,0 +1,72 @@
+import getAreJetpackCredentialsInvalid from 'calypso/state/jetpack/credentials/selectors';
+import { sites as SITES_CREDENTIALS_FIXTURE } from 'calypso/state/selectors/test/fixtures/jetpack-credentials-test-status';
+
+describe( 'getAreJetpackCredentialsInvalid()', () => {
+	test( 'should return false when when siteId not exists in state', () => {
+		const stateIn = {
+			jetpack: {
+				credentials: {
+					testRequestStatus: SITES_CREDENTIALS_FIXTURE,
+				},
+			},
+		};
+
+		const siteId = 9990000;
+		const role = 'main';
+
+		expect( true ).toBe( true );
+		const output = getAreJetpackCredentialsInvalid( stateIn, siteId, role );
+		expect( output ).toEqual( false );
+	} );
+
+	test( 'should return false when test status is pending', () => {
+		const stateIn = {
+			jetpack: {
+				credentials: {
+					testRequestStatus: SITES_CREDENTIALS_FIXTURE,
+				},
+			},
+		};
+
+		const siteId = 1230000;
+		const role = 'main';
+
+		expect( true ).toBe( true );
+		const output = getAreJetpackCredentialsInvalid( stateIn, siteId, role );
+		expect( output ).toEqual( false );
+	} );
+
+	test( 'should return false when test status is valid', () => {
+		const stateIn = {
+			jetpack: {
+				credentials: {
+					testRequestStatus: SITES_CREDENTIALS_FIXTURE,
+				},
+			},
+		};
+
+		const siteId = 2340000;
+		const role = 'main';
+
+		expect( true ).toBe( true );
+		const output = getAreJetpackCredentialsInvalid( stateIn, siteId, role );
+		expect( output ).toEqual( false );
+	} );
+
+	test( 'should return true when test status is invalid', () => {
+		const stateIn = {
+			jetpack: {
+				credentials: {
+					testRequestStatus: SITES_CREDENTIALS_FIXTURE,
+				},
+			},
+		};
+
+		const siteId = 3450000;
+		const role = 'main';
+
+		expect( true ).toBe( true );
+		const output = getAreJetpackCredentialsInvalid( stateIn, siteId, role );
+		expect( output ).toEqual( true );
+	} );
+} );
