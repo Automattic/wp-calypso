f8a1ba6224b39d993afae5705d34263f86d4a8b3
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2022-10-05 12:58:52 +0200

    E2E: Remove accidental pause (#68638)

--- packages/calypso-e2e/src/lib/blocks/block-flows/layout-grid.ts
+++ packages/calypso-e2e/src/lib/blocks/block-flows/layout-grid.ts
@@ -47,7 +47,6 @@ export class LayoutGridBlockFlow implements BlockFlow {
 	 * @param {EditorContext} context The current context for the editor at the point of test execution.
 	 */
 	async configure( context: EditorContext ): Promise< void > {
-		await context.page.pause();
 		const twoColumnButtonLocator = context.editorLocator.locator( selectors.twoColumnButton );
 		await twoColumnButtonLocator.click();
 
