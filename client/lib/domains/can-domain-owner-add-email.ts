05f5de8dff275c58d833199224966988c9bcaa5c
Angela Wong <104910361+wongasy@users.noreply.github.com>
2022-10-05 08:37:10 -0400

    Clarify the purpose of canDomainOwnerAddEmail in the comment
    Co-authored-by: Stéphane Thomas <email@stephanethomas.com>

--- client/lib/domains/can-domain-owner-add-email.ts
+++ client/lib/domains/can-domain-owner-add-email.ts
@@ -3,7 +3,7 @@ import { EMAIL_WARNING_CODE_OTHER_USER_OWNS_EMAIL } from 'calypso/lib/emails/ema
 import type { ResponseDomain } from 'calypso/lib/domains/types';
 
 /**
- * Determines if domain owner can add email (i.e. domain owner is also the email owner).
+ * Determines if the domain owner can add email (i.e. if the owner of the domain subscription is also the owner of the email subscription).
  */
 export function canDomainOwnerAddEmail( domain: ResponseDomain | undefined ) {
 	const cannotAddEmailWarningReason = getCurrentUserCannotAddEmailReason( domain );
