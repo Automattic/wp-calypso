be5ef935ea0fcf61b5a8b9e3a1b96b6e44f2d94c
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2022-10-05 15:10:33 -0300

    Improve form validation

--- client/me/security-ssh-key/add-ssh-key-form.tsx
+++ client/me/security-ssh-key/add-ssh-key-form.tsx
@@ -1,8 +1,8 @@
-import { Button } from '@automattic/components';
+import { Button, FormInputValidation } from '@automattic/components';
 import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import i18n from 'i18n-calypso';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useReducer } from 'react';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormTextarea from 'calypso/components/forms/form-textarea';
@@ -18,20 +18,50 @@ const keyFormats = [
 	'ecdsa-sha2-nistp521',
 ];
 
+const initialState = {
+	touched: false,
+	isValid: false,
+	value: '',
+};
+
+const sshKeyValidation = new RegExp( `^(?:${ keyFormats.join( '|' ) })\\s.+` );
+
+type ReducerAction = { type: 'setValue'; value: string };
+
+const sshKeyFormReducer = ( state = initialState, action: ReducerAction ): typeof initialState => {
+	switch ( action.type ) {
+		case 'setValue':
+			return {
+				touched: true,
+				isValid: sshKeyValidation.test( action.value ),
+				value: action.value,
+			};
+
+		default:
+			return state;
+	}
+};
+
 interface AddSSHKeyFormProps {
 	addSSHKey( args: { name: string; key: string } ): void;
 }
 
 export const AddSSHKeyForm = ( { addSSHKey }: AddSSHKeyFormProps ) => {
-	const [ publicSSHKey, setPublicSSHKey ] = useState( '' );
+	const [ { isValid, touched, value }, dispatch ] = useReducer( sshKeyFormReducer, initialState );
 
 	const { __ } = useI18n();
+	const formats = new Intl.ListFormat( i18n.getLocaleSlug() ?? 'en', {
+		style: 'long',
+		type: 'disjunction',
+	} ).format( keyFormats );
+
+	const showSSHKeyError = touched && ! isValid;
 
 	return (
 		<form
 			onSubmit={ ( event ) => {
 				event.preventDefault();
-				addSSHKey( { name: 'default', key: publicSSHKey } );
+				addSSHKey( { name: 'default', key: value } );
 			} }
 		>
 			<FormFieldset>
@@ -39,23 +69,33 @@ export const AddSSHKeyForm = ( { addSSHKey }: AddSSHKeyFormProps ) => {
 				<FormTextarea
 					required
 					id={ PUBLIC_SSH_KEY_INPUT_ID }
-					value={ publicSSHKey }
+					value={ value }
+					isError={ showSSHKeyError }
 					placeholder={ sprintf(
 						// translators: "formats" is a list of SSH-key formats.
 						__( 'Paste the public key here. It should begin with %(formats)s…' ),
 						{
-							formats: new Intl.ListFormat( i18n.getLocaleSlug() ?? 'en', {
-								style: 'long',
-								type: 'disjunction',
-							} ).format( keyFormats ),
+							formats,
 						}
 					) }
 					onChange={ ( event: ChangeEvent< HTMLTextAreaElement > ) =>
-						setPublicSSHKey( event.target.value )
+						dispatch( { type: 'setValue', value: event.target.value } )
 					}
 				/>
+				{ showSSHKeyError && (
+					<FormInputValidation
+						isError
+						text={ sprintf(
+							// translators: "formats" is a list of SSH-key formats.
+							__( 'Invalid public key. It should begin with %(formats)s.' ),
+							{
+								formats,
+							}
+						) }
+					/>
+				) }
 			</FormFieldset>
-			<Button primary type="submit">
+			<Button primary type="submit" disabled={ ! isValid }>
 				{ __( 'Save SSH key' ) }
 			</Button>
 		</form>
