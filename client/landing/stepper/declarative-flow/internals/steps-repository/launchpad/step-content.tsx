7715179630591944a16ce8167593e9287a2cb39a
Anthony Grullon <agrullon95@gmail.com>
2022-10-06 21:39:17 +0200

    Add custom domain logic to launchpad sidebar url

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/step-content.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/step-content.tsx
@@ -1,4 +1,8 @@
+import { useGetDomainsQuery } from 'calypso/data/domains/use-get-domains-query';
 import { NavigationControls } from 'calypso/landing/stepper/declarative-flow/internals/types';
+import { useSite } from 'calypso/landing/stepper/hooks/use-site';
+import { ResponseDomain } from 'calypso/lib/domains/types';
+import { createSiteDomainObject } from 'calypso/state/sites/domains/assembler';
 import LaunchpadSitePreview from './launchpad-site-preview';
 import Sidebar from './sidebar';
 
@@ -9,11 +13,45 @@ type StepContentProps = {
 	goToStep?: NavigationControls[ 'goToStep' ];
 };
 
-const StepContent = ( { siteSlug, submit, goNext, goToStep }: StepContentProps ) => (
-	<div className="launchpad__content">
-		<Sidebar siteSlug={ siteSlug } submit={ submit } goNext={ goNext } goToStep={ goToStep } />
-		<LaunchpadSitePreview siteSlug={ siteSlug } />
-	</div>
-);
+function sortByRegistrationDateCompareFunction(
+	firstDomainObject: ResponseDomain,
+	secondDomainObject: ResponseDomain
+) {
+	if ( firstDomainObject.registrationDate > secondDomainObject.registrationDate ) {
+		return -1;
+	} else if ( firstDomainObject.registrationDate < secondDomainObject.registrationDate ) {
+		return 1;
+	}
+	return 1;
+}
+
+const StepContent = ( { siteSlug, submit, goNext, goToStep }: StepContentProps ) => {
+	const site = useSite();
+
+	const { data: allDomains = [] } = useGetDomainsQuery( site?.ID ?? null, {
+		retry: false,
+	} );
+
+	const domains = allDomains.map( createSiteDomainObject );
+
+	// Filter custom domains and sort by registration date.
+	// We want the first domain object to be the most recently registered domain
+	const nonWpcomDomains = domains
+		.filter( ( domain ) => ! domain.isWPCOMDomain )
+		.sort( sortByRegistrationDateCompareFunction );
+
+	const wpcomDomains = domains.filter( ( domain ) => domain.isWPCOMDomain );
+
+	const sidebarURL =
+		domains &&
+		( nonWpcomDomains?.length > 0 ? nonWpcomDomains[ 0 ]?.domain : wpcomDomains[ 0 ]?.domain );
+
+	return (
+		<div className="launchpad__content">
+			<Sidebar siteSlug={ sidebarURL } submit={ submit } goNext={ goNext } goToStep={ goToStep } />
+			<LaunchpadSitePreview siteSlug={ siteSlug } />
+		</div>
+	);
+};
 
 export default StepContent;
