2db16a6f70c59337e3a126fa9972c3ed0c615027
Tony Arcangelini <tony@arcangelini.com>
2022-10-07 13:20:35 +0200

    Remove show control

--- apps/editing-toolkit/editing-toolkit-plugin/help-center/src/admin-bar.js
+++ apps/editing-toolkit/editing-toolkit-plugin/help-center/src/admin-bar.js
@@ -1,5 +1,4 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
-import { useHappychatAvailable } from '@automattic/happychat-connection';
 import HelpCenter from '@automattic/help-center';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useEffect } from '@wordpress/element';
@@ -11,20 +10,15 @@ import CalypsoStateProvider from './CalypsoStateProvider';
 function AdminHelpCenterContent() {
 	const { setShowHelpCenter } = useDispatch( 'automattic/help-center' );
 	const show = useSelect( ( select ) => select( 'automattic/help-center' ).isHelpCenterShown() );
-	const { data } = useHappychatAvailable();
 	const button = document.getElementById( 'wp-admin-bar-help-center' );
 
 	useEffect( () => {
-		if ( data?.status === 'assigned' ) {
-			setShowHelpCenter( true );
-		}
-
 		if ( show ) {
 			button.classList.add( 'active' );
 		} else {
 			button.classList.remove( 'active' );
 		}
-	}, [ data, setShowHelpCenter, show, button ] );
+	}, [ setShowHelpCenter, show, button ] );
 
 	const handleToggleHelpCenter = () => {
 		if ( show ) {
@@ -43,7 +37,7 @@ function AdminHelpCenterContent() {
 
 	button.onclick = handleToggleHelpCenter;
 
-	return <>{ show && <HelpCenter handleClose={ () => setShowHelpCenter( false ) } /> }</>;
+	return <HelpCenter handleClose={ () => setShowHelpCenter( false ) } />;
 }
 
 ReactDOM.render(
