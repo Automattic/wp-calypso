3422663d578676d8fa1180e903f907bebcaaa605
Daniel López <daniel.lopez@automattic.com>
2022-10-07 11:20:41 +0200

    Rename const and exit earlier

--- client/lib/domains/is-domain-and-email-subscriptions-owned-by-different-users.ts
+++ client/lib/domains/is-domain-and-email-subscriptions-owned-by-different-users.ts
@@ -8,13 +8,18 @@ import type { ResponseDomain } from 'calypso/lib/domains/types';
 export function isDomainAndEmailSubscriptionsOwnedByDifferentUsers(
 	domain: ResponseDomain | undefined
 ): boolean {
-	const cannotAddEmailWarningReason = getCurrentUserCannotAddEmailReason( domain );
-	const cannotAddEmailWarningCode = cannotAddEmailWarningReason?.code ?? null;
+	//Early exit returns the result of evaluating: ! undefined && ! true
+	if ( ! domain ) {
+		return false;
+	}
+
+	const currentUserCannotAddEmailReason = getCurrentUserCannotAddEmailReason( domain );
+	const cannotAddEmailWarningCode = currentUserCannotAddEmailReason?.code ?? null;
 	const isDomainOwnerNotEmailOwner =
-		domain?.currentUserCanManage &&
+		domain.currentUserCanManage &&
 		EMAIL_WARNING_CODE_OTHER_USER_OWNS_EMAIL === cannotAddEmailWarningCode;
 	const isEmailOwnerNotDomainOwner =
-		! domain?.currentUserCanManage &&
+		! domain.currentUserCanManage &&
 		EMAIL_WARNING_CODE_OTHER_USER_OWNS_EMAIL !== cannotAddEmailWarningCode;
 
 	return ! isDomainOwnerNotEmailOwner && ! isEmailOwnerNotDomainOwner;
