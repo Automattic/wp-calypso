c0824df7b6580dbe697540afbc4832f80489fe61
Rafael Agostini <rafael.agostini@automattic.com>
2022-10-06 22:31:07 -0500

    Fix optional chaining to fetch jetpack creds state

--- client/state/jetpack/credentials/selectors.js
+++ client/state/jetpack/credentials/selectors.js
@@ -1,9 +1,9 @@
 import 'calypso/state/jetpack/init';
 
 export function getAreJetpackCredentialsInvalid( state, siteId, role ) {
-	return 'invalid' === state.jetpack.credentials.testStatus?.[ siteId ]?.[ role ];
+	return 'invalid' === state.jetpack?.credentials?.testStatus?.[ siteId ]?.[ role ];
 }
 
 export function isRequestingJetpackCredentialsTest( state, siteId, role ) {
-	return state.jetpack.credentials.testRequestStatus?.[ siteId ]?.[ role ] || false;
+	return state.jetpack?.credentials?.testRequestStatus?.[ siteId ]?.[ role ] || false;
 }
