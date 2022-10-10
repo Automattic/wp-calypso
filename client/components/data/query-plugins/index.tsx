f9e5994eaa328f78faf566e61cbf785e06826cb0
Paulo Trentin <paulo@paulotrentin.com.br>
2022-10-10 11:20:18 -0300

    Fix error on incognito navigation avoiding /me/plugins call at /plugins fix #68828

--- client/components/data/query-plugins/index.tsx
+++ client/components/data/query-plugins/index.tsx
@@ -1,6 +1,11 @@
+import { useSelector } from 'react-redux';
+import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import QueryAllJetpackSitesPlugins from '../query-all-jetpack-sites-plugins';
 import QueryJetpackPlugins from '../query-jetpack-plugins';
 
 export default function QueryPlugins( { siteId }: { siteId?: number } ) {
+	const isLoggedIn = useSelector( isUserLoggedIn );
+	if ( isLoggedIn === false ) return false;
+
 	return siteId ? <QueryJetpackPlugins siteIds={ [ siteId ] } /> : <QueryAllJetpackSitesPlugins />;
 }
