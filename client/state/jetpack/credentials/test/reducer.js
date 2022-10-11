388f873755886647c9bc0437bdb50d05cc495218
Rafael Agostini <rafael.agostini@automattic.com>
2022-10-11 10:15:52 -0500

    Jetpack Backup: Prevent restores with invalid creds (#68552)
    * Add getAreJetpackCredentialsInvalid selector
    
    This selector is useful to validate if existing Jetpack Credentials are still valid. Also including unit tests for different scenarios.
    
    * Remove default assertions
    
    * Disable restore buttons when creds are invalid
    
    This disable restore buttons on main backup page and action toolbar in activity log when credentials are invalid.
    
    * Display enable restore banner if creds are invalid
    
    * Fix credentials invalid selector imports
    
    * Add new selector for handling creds test requests
    
    Also refactoring current selector and unit tests related.
    
    * Fix request creds selector to default false value
    
    If there’s no value on testRequestStatus yet for specific siteId and role, we should return `false`.
    
    * Refactor unit tests for creds test selectors
    
    * Add QueryJetpackCredentialsStatus data component
    
    * Fix optional chaining to fetch jetpack creds state
    
    * Display credentials warning text when are invalid
    
    * Add test credentials status on backup page
    
    * Add test credentials status on activity card list
    
    * Add creds test and disable restores in ActivityLog
    
    This change include the option to test credentials on ActivityLog that is currently being used on Calypso Blue, and we are now disabling restore buttons here as well.
    
    * Update client/components/data/query-jetpack-credentials-status/README.md
    
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>
    
    * Remove unnecessary case condition
    
    * Rename areJetpackCredentialsInvalid selector
    
    This commit renames getAreJetpackCredentialsInvalid to areJetpackCredentialsInvalid on all instances, including unit tests.
    
    It also removes unnecessary optional chaining operators on Jetpack Credentials selectors.
    
    * Add test cases to support multiple sites data
    
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>

--- client/state/jetpack/credentials/test/reducer.js
+++ client/state/jetpack/credentials/test/reducer.js
@@ -1,5 +1,5 @@
 import { markCredentialsAsInvalid, markCredentialsAsValid, testCredentials } from '../actions';
-import { testRequestStatus } from '../reducer';
+import { testRequestStatus, testStatus } from '../reducer';
 
 describe( 'reducer', () => {
 	describe( 'testRequestStatus', () => {
@@ -7,23 +7,125 @@ describe( 'reducer', () => {
 			expect( testRequestStatus( undefined, {} ) ).toEqual( {} );
 		} );
 
-		test( 'status should be valid if the credentials are okay', () => {
+		test( 'should return true if we are requesting to test credentials', () => {
 			const stateIn = {};
-			const action = markCredentialsAsValid( 123000, 'main' );
-
+			const action = testCredentials( 123000, 'main' );
 			const stateOut = testRequestStatus( stateIn, action );
+
 			expect( stateOut ).toEqual( {
 				123000: {
+					main: true,
+				},
+			} );
+		} );
+
+		test( 'should return false if credentials are valid', () => {
+			const stateIn = {};
+			const action = markCredentialsAsValid( 123000, 'main' );
+			const stateOut = testRequestStatus( stateIn, action );
+
+			expect( stateOut ).toEqual( {
+				123000: {
+					main: false,
+				},
+			} );
+		} );
+
+		test( 'should return false if credentials are invalid', () => {
+			const stateIn = {};
+			const action = markCredentialsAsInvalid( 234000, 'main' );
+			const stateOut = testRequestStatus( stateIn, action );
+
+			expect( stateOut ).toEqual( {
+				234000: {
+					main: false,
+				},
+			} );
+		} );
+
+		test( 'should return true if credentials are valid (or invalid) and we request a new credentials test for same role', () => {
+			const stateIn = {
+				234000: {
+					main: false,
+				},
+			};
+			const action = testCredentials( 234000, 'main' );
+			const stateOut = testRequestStatus( stateIn, action );
+
+			expect( stateOut ).toEqual( {
+				234000: {
+					main: true,
+				},
+			} );
+		} );
+
+		test( 'should keep existing roles request statuses after requesting a new credentials test for a new role', () => {
+			const stateIn = {
+				123456: {
+					main: false,
+				},
+			};
+			const action = testCredentials( 123456, 'alternate' );
+			const stateOut = testRequestStatus( stateIn, action );
+
+			expect( stateOut ).toEqual( {
+				123456: {
+					main: false,
+					alternate: true,
+				},
+			} );
+		} );
+
+		test( 'should keep existing sites data after requesting a credentials test for a new site', () => {
+			const stateIn = {
+				123456: {
+					main: false,
+					alternate: false,
+				},
+				987654: {
+					main: false,
+				},
+			};
+			const action = testCredentials( 999000, 'main' );
+			const stateOut = testRequestStatus( stateIn, action );
+
+			expect( stateOut ).toEqual( {
+				123456: {
+					main: false,
+					alternate: false,
+				},
+				987654: {
+					main: false,
+				},
+				999000: {
+					main: true,
+				},
+			} );
+		} );
+	} );
+
+	describe( 'testStatus', () => {
+		test( 'should return the initial state', () => {
+			expect( testStatus( undefined, {} ) ).toEqual( {} );
+		} );
+
+		test( 'should be valid if the credentials are okay', () => {
+			const stateIn = {};
+			const action = markCredentialsAsValid( 345000, 'main' );
+
+			const stateOut = testStatus( stateIn, action );
+			expect( stateOut ).toEqual( {
+				345000: {
 					main: 'valid',
 				},
 			} );
 		} );
 
-		test( 'status should be invalid if the credentials are not okay', () => {
+		test( 'should be invalid if the credentials are not okay', () => {
 			const stateIn = {};
 			const action = markCredentialsAsInvalid( 234000, 'main' );
 
-			const stateOut = testRequestStatus( stateIn, action );
+			const stateOut = testStatus( stateIn, action );
 			expect( stateOut ).toEqual( {
 				234000: {
 					main: 'invalid',
@@ -31,31 +133,18 @@ describe( 'reducer', () => {
 			} );
 		} );
 
-		test( 'status should be pending if we started the credentials test process', () => {
-			const stateIn = {};
-			const action = testCredentials( 345000, 'main' );
-
-			const stateOut = testRequestStatus( stateIn, action );
-			expect( stateOut ).toEqual( {
-				345000: {
-					main: 'pending',
-				},
-			} );
-		} );
-
-		test( 'should keep existing roles statuses after requesting a new test', () => {
+		test( 'should keep previous value if we started a new credentials test for same role', () => {
 			const stateIn = {
 				456000: {
 					main: 'valid',
 				},
 			};
-			const action = testCredentials( 456000, 'alternate' );
+			const action = testCredentials( 456000, 'main' );
 
-			const stateOut = testRequestStatus( stateIn, action );
+			const stateOut = testStatus( stateIn, action );
 			expect( stateOut ).toEqual( {
 				456000: {
 					main: 'valid',
-					alternate: 'pending',
 				},
 			} );
 		} );
@@ -64,12 +153,11 @@ describe( 'reducer', () => {
 			const stateIn = {
 				567000: {
 					main: 'valid',
-					alternate: 'pending',
 				},
 			};
 			const action = markCredentialsAsValid( 567000, 'alternate' );
 
-			const stateOut = testRequestStatus( stateIn, action );
+			const stateOut = testStatus( stateIn, action );
 			expect( stateOut ).toEqual( {
 				567000: {
 					main: 'valid',
@@ -82,16 +170,75 @@ describe( 'reducer', () => {
 			const stateIn = {
 				678000: {
 					main: 'valid',
-					alternate: 'pending',
 				},
 			};
 			const action = markCredentialsAsInvalid( 678000, 'alternate' );
 
-			const stateOut = testRequestStatus( stateIn, action );
+			const stateOut = testStatus( stateIn, action );
 			expect( stateOut ).toEqual( {
 				678000: {
 					main: 'valid',
 					alternate: 'invalid',
+				},
+			} );
+		} );
+
+		test( 'should keep existing sites data after marking a role for a new site as valid', () => {
+			const stateIn = {
+				123000: {
+					main: 'valid',
+				},
+				456000: {
+					main: 'invalid',
+					alternate: 'valid',
+				},
+				999000: {
+					main: 'pending',
+				},
+			};
+			const action = markCredentialsAsValid( 999000, 'main' );
+
+			const stateOut = testStatus( stateIn, action );
+			expect( stateOut ).toEqual( {
+				123000: {
+					main: 'valid',
+				},
+				456000: {
+					main: 'invalid',
+					alternate: 'valid',
+				},
+				999000: {
+					main: 'valid',
+				},
+			} );
+		} );
+
+		test( 'should keep existing sites data after marking a role for a new site as invalid', () => {
+			const stateIn = {
+				123000: {
+					main: 'valid',
+				},
+				456000: {
+					main: 'invalid',
+					alternate: 'valid',
+				},
+				999000: {
+					main: 'pending',
+				},
+			};
+			const action = markCredentialsAsInvalid( 999000, 'main' );
+
+			const stateOut = testStatus( stateIn, action );
+			expect( stateOut ).toEqual( {
+				123000: {
+					main: 'valid',
+				},
+				456000: {
+					main: 'invalid',
+					alternate: 'valid',
+				},
+				999000: {
+					main: 'invalid',
 				},
 			} );
 		} );
