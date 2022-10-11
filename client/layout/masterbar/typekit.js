ab75b666e9be0c7f29e80e5935bd09fa6be141c9
Ilyas Foo <foo.ilyas@gmail.com>
2022-10-11 09:38:07 +0800

    Deploy NUX changes for WooCommerce.com wpcc (#68813)
    * Update woocommerce sign in and sign up screens  (#68180)
    
    * Add woo masterbar
    
    * Display masterbar for woo signup flow
    
    * Add or divider to two factor components for woo
    
    * Do not show loading screen for woo signup flow
    
    * Update ContinueAsUser component for woo
    
    * Update woo styles
    
    * Update signup screens for woocommerce
    
    * Add social tos to login
    
    * Update login styles
    
    * Update masterbar and other woo styles
    
    * Update woo signup style
    
    * Update signup/in styles for woo
    
    * Use woo style and terms
    
    * Fix social tos
    
    * Fix submit login form for partner signup flow
    
    * Update invalid email address message for woo
    
    * Update login form field labels
    
    * Set "letter-spacing: 0;" for woo
    
    * Fix mismatch styles
    
    * Hide the OR text when only one 2fa option
    
    * Update signup url for woo login screen
    
    * Fix layout and sub header styles
    
    * Update woo account creation font style (#68505)
    
    * Add woo typekit
    
    * Import woo typekit and use proxima-nova font
    
    * Change form label font weight from medium to regular
    
    * Update woo styles
    
    * Update woo masterbar font weight
    
    * Add client_id prop to calypso_signup_social_button_click event (#68564)
    
    * Add WooCommerce lost password screen (#68558)
    
    * Add lost password route and action prop
    
    * Add lost password screen
    
    * Add forgot password flow query param
    
    * Update lost password flow login texts
    
    * Update forgot password link
    
    * Update woo style
    
    * Fix lostpassword_flow query
    
    * Use page route directly for lost password link
    
    * Wrap lost password screen in a feature flag (#68699)
    
    * Add lost password route and action prop
    
    * Add lost password screen
    
    * Add forgot password flow query param
    
    * Update lost password flow login texts
    
    * Update forgot password link
    
    * Update woo style
    
    * Fix lostpassword_flow query
    
    * Use page route directly for lost password link
    
    * Add login/react-lost-password-screen feature flag
    
    * Wrap lost password screen in feature flag and cookie logic
    
    * Fix lint
    
    * Enable feature flag for login/react-lost-password-screen in staging and production environments (#68811)
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>

---
+++ client/layout/masterbar/typekit.js
@@ -0,0 +1,37 @@
+/* global Typekit */
+import { isCalypsoLive } from '@automattic/calypso-config';
+
+if ( typeof document !== 'undefined' && typeof isCalypsoLive === 'function' && ! isCalypsoLive() ) {
+	// Load fonts - https://helpx.adobe.com/fonts/using/embed-codes.html
+	( function ( d ) {
+		const config = {
+			kitId: 'ivy2obh',
+			scriptTimeout: 3000,
+			async: true,
+		};
+		const h = d.documentElement;
+		const t = setTimeout( function () {
+			h.className = h.className.replace( /\bwf-loading\b/g, '' ) + ' wf-inactive';
+		}, config.scriptTimeout );
+		const tk = d.createElement( 'script' );
+		let f = false;
+		const s = d.getElementsByTagName( 'script' )[ 0 ];
+		let a;
+
+		h.className += ' wf-loading';
+		tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+		tk.async = true;
+		tk.onload = tk.onreadystatechange = function () {
+			a = this.readyState;
+			if ( f || ( a && 'complete' !== a && 'loaded' !== a ) ) {
+				return;
+			}
+			f = true;
+			clearTimeout( t );
+			try {
+				Typekit.load( config );
+			} catch ( e ) {}
+		};
+		s.parentNode.insertBefore( tk, s );
+	} )( document );
+}
