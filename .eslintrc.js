bb5593d76d04eea27ac124a2b16f9106dee76ee2
Noah Allen <noahtallen@gmail.com>
2022-10-11 11:05:27 -0700

    Update comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -87,7 +87,7 @@ module.exports = {
 					'brace-style': 'off',
 					'comma-dangle': 'off',
 					'comma-spacing': 'off',
-					curly: 'error', // The base curly rule does not seem to apply to TS files as well.
+					curly: 'error', // The base curly rule does not seem to apply to TS files.
 					'default-param-last': 'off',
 					'dot-notation': 'off',
 					'func-call-spacing': 'off',
@@ -295,7 +295,7 @@ module.exports = {
 		// REST API objects include underscores
 		camelcase: 'off',
 
-		//
+		// Curly is not added by existing presets and is needed for WordPress style compatibility.
 		curly: 'error',
 
 		'no-constant-condition': [ 'error', { checkLoops: false } ],
