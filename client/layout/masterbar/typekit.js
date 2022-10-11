437443a78141926228a44b4f2946ce2330cc51a7
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-10-11 13:49:55 +0800

    Test typekit

--- client/layout/masterbar/typekit.js
+++ client/layout/masterbar/typekit.js
@@ -1,7 +1,7 @@
 /* global Typekit */
-import { isCalypsoLive } from '@automattic/calypso-config';
+// import { isCalypsoLive } from '@automattic/calypso-config';
 
-if ( typeof document !== 'undefined' && typeof isCalypsoLive === 'function' && ! isCalypsoLive() ) {
+if ( typeof document !== 'undefined' ) {
 	// Load fonts - https://helpx.adobe.com/fonts/using/embed-codes.html
 	( function ( d ) {
 		const config = {
