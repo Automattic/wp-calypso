13901080d3e502eb4006a7c17e3d484111bb0868
Gergely Csécsey <gcsecsey@gmail.com>
2022-10-12 14:23:58 +0200

    add category param to ES query

--- client/data/marketplace/types.ts
+++ client/data/marketplace/types.ts
@@ -83,6 +83,7 @@ export type ESDateRangeFilter = { range: Record< string, { gte: string; lt: stri
 export type SearchParams = {
 	query: string | undefined;
 	author: string | undefined;
+	category: string | undefined;
 	groupId: string;
 	pageHandle: string | undefined;
 	pageSize: number;
