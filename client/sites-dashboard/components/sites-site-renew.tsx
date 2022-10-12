8cca37a6eea1d9a404f53142c4162a0c264b6f5e
Antony Agrios <antonyagrios@gmail.com>
2022-10-12 12:07:23 +0300

    feature: Add site renew component to display expiration info

---
+++ client/sites-dashboard/components/sites-site-renew.tsx
@@ -0,0 +1,42 @@
+import { Gridicon } from '@automattic/components';
+import styled from '@emotion/styled';
+import { useI18n } from '@wordpress/react-i18n';
+import { SiteExcerptData } from 'calypso/data/sites/site-excerpt-types';
+
+interface SiteRenewProps {
+	site: SiteExcerptData;
+}
+
+const SiteRenewContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 4px;
+`;
+
+const SiteRenewLink = styled.a( {
+	whiteSpace: 'nowrap',
+	textDecoration: 'underline',
+} );
+
+const SiteRenewNotice = styled.div`
+	display: flex;
+	align-items: center;
+	color: #ea303f;
+	margin-top: -6px;
+`;
+
+export const SiteRenew = ( { site }: SiteRenewProps ) => {
+	const { __ } = useI18n();
+	const renewText = __( 'Renew' );
+	return (
+		<SiteRenewContainer>
+			<SiteRenewNotice>
+				<Gridicon icon="notice" />
+				{ `${ site.plan?.product_name_short } - Expired` }
+			</SiteRenewNotice>
+			<SiteRenewLink href={ `/checkout/${ site.slug }` } title={ renewText }>
+				{ renewText }
+			</SiteRenewLink>
+		</SiteRenewContainer>
+	);
+};
