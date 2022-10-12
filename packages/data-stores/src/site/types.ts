316b93af41e8ecbd14ae6dfff9a4c1e597beeb2f
Dan Roundhill <dan@automattic.com>
2022-10-12 16:29:19 -0600

    More eslint "any" disables for code not changed in this PR.

--- packages/data-stores/src/site/types.ts
+++ packages/data-stores/src/site/types.ts
@@ -264,6 +264,7 @@ export interface Cart {
 	messages: Record< 'errors' | 'success', unknown >;
 }
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface Domain {
 	a_records_required_for_mapping?: string[];
 	primary_domain: boolean;
@@ -319,6 +320,7 @@ export interface Domain {
 export interface SiteSettings {
 	[ key: string ]: any;
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
 
 export interface SiteLaunchState {
 	status: SiteLaunchStatus;
