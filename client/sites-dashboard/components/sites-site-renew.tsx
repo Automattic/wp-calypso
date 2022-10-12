04639c81d3dec4a63b94e4b5019f44a546b6f0ad
Antony Agrios <antonyagrios@gmail.com>
2022-10-12 13:10:58 +0300

    update: Add tracking on site renew component
    - Add track on click the renew button
    - Add track on view the component

--- client/sites-dashboard/components/sites-site-renew.tsx
+++ client/sites-dashboard/components/sites-site-renew.tsx
@@ -1,7 +1,12 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { Gridicon } from '@automattic/components';
 import styled from '@emotion/styled';
 import { useI18n } from '@wordpress/react-i18n';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import { SiteExcerptData } from 'calypso/data/sites/site-excerpt-types';
+const SITE_RENEW_NAG_IN_VIEW = 'calypso_sites_dashboard_site_renew_nag_inview';
+const SITE_RENEW_NAG_ON_CLICK = 'calypso_sites_dashboard_site_renew_nag_click';
 
 interface SiteRenewProps {
 	site: SiteExcerptData;
@@ -27,16 +32,30 @@ const SiteRenewNotice = styled.div`
 
 export const SiteRenew = ( { site }: SiteRenewProps ) => {
 	const { __ } = useI18n();
+	const { ref, inView: inViewOnce } = useInView( { triggerOnce: true } );
+
+	useEffect( () => {
+		if ( inViewOnce ) {
+			recordTracksEvent( SITE_RENEW_NAG_IN_VIEW );
+		}
+	}, [ inViewOnce ] );
+
 	const isSiteOwner = site.plan?.user_is_owner;
 	const renewText = __( 'Renew' );
 	return (
-		<SiteRenewContainer>
+		<SiteRenewContainer ref={ ref }>
 			<SiteRenewNotice>
 				<Gridicon icon="notice" />
 				{ `${ site.plan?.product_name_short } - Expired` }
 			</SiteRenewNotice>
 			{ isSiteOwner && (
-				<SiteRenewLink href={ `/checkout/${ site.slug }` } title={ renewText }>
+				<SiteRenewLink
+					onClick={ () => {
+						recordTracksEvent( SITE_RENEW_NAG_ON_CLICK );
+					} }
+					href={ `/checkout/${ site.slug }` }
+					title={ renewText }
+				>
 					{ renewText }
 				</SiteRenewLink>
 			) }
