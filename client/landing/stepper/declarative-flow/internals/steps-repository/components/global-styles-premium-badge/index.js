75d5e21d4516755e2fa5c345a6c0a133f7ec178d
mmtr <miguel.torres@automattic.com>
2022-10-12 17:43:27 +0200

    Improve rendering on mobile viewports

--- client/landing/stepper/declarative-flow/internals/steps-repository/components/global-styles-premium-badge/index.js
+++ client/landing/stepper/declarative-flow/internals/steps-repository/components/global-styles-premium-badge/index.js
@@ -1,31 +1,71 @@
 import { isEnabled } from '@automattic/calypso-config';
-import { Tooltip } from '@wordpress/components';
+import { Popover } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 import { Icon, starFilled } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 import Badge from 'calypso/components/badge';
+import { hasTouch } from 'calypso/lib/touch-detect';
 
 import './style.scss';
 
 const GlobalStylesPremiumBadge = () => {
 	const { __ } = useI18n();
+	const [ popoverAnchor, setPopoverAnchor ] = useState();
+	const [ isPopoverVisible, setIsPopoverVisible ] = useState( false );
 
 	if ( ! isEnabled( 'limit-global-styles' ) ) {
 		return null;
 	}
 
+	const isTouch = hasTouch();
+
 	return (
-		<Tooltip
-			text={ __(
-				'Upgrade to a paid plan for color changes to take affect and to unlock all premium design tools'
-			) }
-		>
-			<span className="global-styles-premium-badge">
+		<>
+			<button
+				ref={ setPopoverAnchor }
+				className="global-styles-premium-badge"
+				onClick={ () => {
+					if ( ! isTouch ) {
+						return;
+					}
+					setIsPopoverVisible( ( state ) => ! state );
+				} }
+				onMouseEnter={ () => {
+					if ( isTouch ) {
+						return;
+					}
+					setIsPopoverVisible( true );
+				} }
+				onMouseLeave={ () => {
+					if ( isTouch ) {
+						return;
+					}
+					setIsPopoverVisible( false );
+				} }
+				type="button"
+			>
 				<Badge type="info">
 					<Icon icon={ starFilled } size={ 18 } />
 					<span>{ __( 'Premium' ) }</span>
 				</Badge>
-			</span>
-		</Tooltip>
+			</button>
+			{ isPopoverVisible && (
+				<Popover
+					anchor={ popoverAnchor }
+					animate={ false }
+					className="global-styles-premium-badge__popover"
+					focusOnMount={ false }
+					offset={ 8 }
+					placement="bottom"
+				>
+					<span>
+						{ __(
+							'Upgrade to a paid plan for color changes to take affect and to unlock all premium design tools'
+						) }
+					</span>
+				</Popover>
+			) }
+		</>
 	);
 };
 
