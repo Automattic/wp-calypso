eb61f1b2554f4550d7b5c2b5442305187b103c69
mmtr <miguel.torres@automattic.com>
2022-10-14 16:49:38 +0200

    Allow custom classes in the premium badge component

--- client/components/premium-badge/index.js
+++ client/components/premium-badge/index.js
@@ -2,48 +2,59 @@ import { Popover } from '@automattic/components';
 import { useState } from '@wordpress/element';
 import { Icon, starFilled } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
+import classNames from 'classnames';
 import Badge from 'calypso/components/badge';
 import { hasTouch } from 'calypso/lib/touch-detect';
 
 import './style.scss';
 
-const PremiumBadge = ( { tooltipText } ) => {
+const PremiumBadge = ( { className, tooltipText } ) => {
 	const { __ } = useI18n();
 	const [ popoverAnchor, setPopoverAnchor ] = useState();
 	const [ isPopoverVisible, setIsPopoverVisible ] = useState( false );
 
 	const isTouch = hasTouch();
 
+	const classes = classNames( 'premium-badge', className );
+
+	const badge = (
+		<Badge type="info">
+			<Icon icon={ starFilled } size={ 18 } />
+			<span>{ __( 'Premium' ) }</span>
+		</Badge>
+	);
+
 	return (
 		<>
-			<button
-				ref={ setPopoverAnchor }
-				className="premium-badge"
-				onClick={ () => {
-					if ( ! isTouch ) {
-						return;
-					}
-					setIsPopoverVisible( ( state ) => ! state );
-				} }
-				onMouseEnter={ () => {
-					if ( isTouch ) {
-						return;
-					}
-					setIsPopoverVisible( true );
-				} }
-				onMouseLeave={ () => {
-					if ( isTouch ) {
-						return;
-					}
-					setIsPopoverVisible( false );
-				} }
-				type="button"
-			>
-				<Badge type="info">
-					<Icon icon={ starFilled } size={ 18 } />
-					<span>{ __( 'Premium' ) }</span>
-				</Badge>
-			</button>
+			{ tooltipText ? (
+				<button
+					ref={ setPopoverAnchor }
+					className={ classes }
+					onClick={ () => {
+						if ( ! isTouch ) {
+							return;
+						}
+						setIsPopoverVisible( ( state ) => ! state );
+					} }
+					onMouseEnter={ () => {
+						if ( isTouch ) {
+							return;
+						}
+						setIsPopoverVisible( true );
+					} }
+					onMouseLeave={ () => {
+						if ( isTouch ) {
+							return;
+						}
+						setIsPopoverVisible( false );
+					} }
+					type="button"
+				>
+					{ badge }
+				</button>
+			) : (
+				<div className={ classes }>{ badge }</div>
+			) }
 			{ tooltipText && (
 				<Popover
 					className="premium-badge__popover"
