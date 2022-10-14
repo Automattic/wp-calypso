db3ae68f4e334cf8d864efd622e76ad473f54f5e
heavyweight <kpapazov@gmail.com>
2022-10-14 13:21:36 +0200

    TailoredFlows: apply fixes after rebase

--- client/landing/stepper/declarative-flow/internals/steps-repository/components/setup-form/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/components/setup-form/index.tsx
@@ -63,15 +63,6 @@ const SetupForm = ( {
 		reader.onerror = () => setBase64Image( null );
 	};
 
-	const onChange = ( event: React.FormEvent< HTMLInputElement > ) => {
-		switch ( event.currentTarget.name ) {
-			case 'setup-form-input-name':
-				return setComponentSiteTitle( event.currentTarget.value );
-			case 'setup-form-input-description':
-				return setTagline( event.currentTarget.value );
-		}
-	};
-
 	useEffect( () => {
 		if ( siteTitle.trim().length && invalidSiteTitle ) {
 			setInvalidSiteTitle( false );
@@ -96,9 +87,8 @@ const SetupForm = ( {
 					name="setup-form-input-name"
 					id="setup-form-input-name"
 					value={ siteTitle }
-					onChange={ onChange }
+					onChange={ ( value ) => setComponentSiteTitle( value ) }
 					placeholder={ translatedText?.titlePlaceholder || __( 'My Site Name' ) }
-					isError={ invalidSiteTitle }
 				/>
 				{ invalidSiteTitle && (
 					<FormInputValidation
@@ -118,7 +108,9 @@ const SetupForm = ( {
 					value={ tagline }
 					placeholder={ translatedText?.taglinePlaceholder || __( 'Add a short description here' ) }
 					enableAutoFocus={ false }
-					onChange={ onChange }
+					onChange={ ( event: React.FormEvent< HTMLInputElement > ) =>
+						setTagline( event.currentTarget.value )
+					}
 				/>
 			</FormFieldset>
 			{ children }
