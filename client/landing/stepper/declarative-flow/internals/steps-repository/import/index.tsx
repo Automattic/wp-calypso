5528b72c43b81060f1621faa5e3c90d102038395
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-10-14 12:43:43 +0200

    Add/stepper import flow (#68817)
    * Define import focused flow
    
    * Import flow: include styles
    
    * Turn on import focus flow
    
    * Import focused flow: hide back nav btn from the import step

--- client/landing/stepper/declarative-flow/internals/steps-repository/import/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/import/index.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import { isEnabled } from '@automattic/calypso-config';
-import { StepContainer } from '@automattic/onboarding';
+import { StepContainer, IMPORT_FOCUSED_FLOW } from '@automattic/onboarding';
 import { useI18n } from '@wordpress/react-i18n';
 import React, { ReactElement } from 'react';
 import CaptureStep from 'calypso/blocks/import/capture';
 import CaptureStepRetired from 'calypso/blocks/import/capture-retired';
 import DocumentHead from 'calypso/components/data/document-head';
 import { useCurrentRoute } from 'calypso/landing/stepper/hooks/use-current-route';
+import { useFlowParam } from 'calypso/landing/stepper/hooks/use-flow-param';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { BASE_ROUTE } from './config';
 import { generateStepPath } from './helper';
@@ -18,8 +19,10 @@ const isEnabledImportLight = isEnabled( 'onboarding/import-light-url-screen' );
 export const ImportWrapper: Step = function ( props ) {
 	const { __ } = useI18n();
 	const { navigation, children, stepName } = props;
+	const flowName = useFlowParam();
 	const currentRoute = useCurrentRoute();
 	const shouldHideSkipBtn = currentRoute !== BASE_ROUTE;
+	const shouldHideBackBtn = flowName === IMPORT_FOCUSED_FLOW && currentRoute === BASE_ROUTE;
 
 	return (
 		<>
@@ -30,6 +33,7 @@ export const ImportWrapper: Step = function ( props ) {
 				flowName={ 'importer' }
 				className={ 'import__onboarding-page' }
 				hideSkip={ isEnabledImportLight || shouldHideSkipBtn }
+				hideBack={ shouldHideBackBtn }
 				hideFormattedHeader={ true }
 				goBack={ navigation.goBack }
 				goNext={ navigation.goNext }
