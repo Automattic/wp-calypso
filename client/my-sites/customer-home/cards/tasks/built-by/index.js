360fa385787966397281bffbfbcb69dc934f51d9
Ayebare Mucunguzi <ayebare11@gmail.com>
2022-10-17 16:43:00 +0300

    Removed unused import and fixed format issues

--- client/my-sites/customer-home/cards/tasks/built-by/index.js
+++ client/my-sites/customer-home/cards/tasks/built-by/index.js
@@ -1,13 +1,10 @@
 import { useTranslate } from 'i18n-calypso';
-import { useSelector } from 'react-redux';
 import announcementImage from 'calypso/assets/images/marketplace/diamond.svg';
 import { TASK_BUILT_BY } from 'calypso/my-sites/customer-home/cards/constants';
 import Task from 'calypso/my-sites/customer-home/cards/tasks/task';
-import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
 const BuiltBy = () => {
 	const translate = useTranslate();
-	const siteSlug = useSelector( ( state ) => getSelectedSiteSlug( state ) );
 
 	return (
 		<Task
@@ -15,8 +12,8 @@ const BuiltBy = () => {
 			description={ translate(
 				'We’re here to help – whether you want to create an online store, redesign your website, migrate your site or simply want to showcase your work. Every site is unique, and has unique needs. So, to get started, let us know about your site’s needs: Click on the link below and tell us some details about your project or business.'
 			) }
-			actionText={ translate( "Let's explore!" ) }
-			actionUrl={ `/plugins/${ siteSlug }` }
+			actionText={ translate( 'Get Started' ) }
+			actionUrl="https://builtbywp.com/?utm_medium=automattic_referred&utm_source=WordPresscom&utm_campaign=bb-card-home"
 			illustration={ announcementImage }
 			taskId={ TASK_BUILT_BY }
 		/>
