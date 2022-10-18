54d7b6a231ce4256a6932bb04052c384331eb989
Roberto Aranda <roberto.aranda@automattic.com>
2022-10-18 09:22:45 +0200

    Remove selector in because it is not needed

--- client/state/themes/selectors/is-upsell-card-displayed.js
+++ client/state/themes/selectors/is-upsell-card-displayed.js
@@ -1,5 +1,4 @@
 import 'calypso/state/themes/init';
-import { createSelector } from '@automattic/state-utils';
 
 /**
  * Returns if the Upsell Card is already displayed
@@ -7,7 +6,6 @@ import { createSelector } from '@automattic/state-utils';
  * @param  {object}  state  Global state tree
  * @returns {boolean}        The value of the upsellCardDisplayed slice of state
  */
-export const isUpsellCardDisplayed = createSelector(
-	( state ) => state.themes.upsellCardDisplayed,
-	( state ) => state.themes
-);
+export function isUpsellCardDisplayed( state ) {
+	return state.themes.upsellCardDisplayed;
+}
