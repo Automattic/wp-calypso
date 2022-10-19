844e665b27532ed3b08e25055e9112c2ef6a6973
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-10-19 16:30:38 +0200

    Add subscribers: Show notification once the import job is successfully submitted (#69249)
    * Import subscribers: show notification on submitting import

--- client/my-sites/people/followers-list/followers.jsx
+++ client/my-sites/people/followers-list/followers.jsx
@@ -17,6 +17,7 @@ import NoResults from 'calypso/my-sites/no-results';
 import PeopleListItem from 'calypso/my-sites/people/people-list-item';
 import PeopleListSectionHeader from 'calypso/my-sites/people/people-list-section-header';
 import { recordGoogleEvent } from 'calypso/state/analytics/actions';
+import { successNotice } from 'calypso/state/notices/actions';
 import isEligibleForSubscriberImporter from 'calypso/state/selectors/is-eligible-for-subscriber-importer';
 import InviteButton from '../invite-button';
 
@@ -141,6 +142,11 @@ class Followers extends Component {
 									recordTracksEvent={ recordTracksEvent }
 									onImportFinished={ () => {
 										this.props?.refetch?.();
+										this.props?.successNotice(
+											this.props.translate(
+												'Your subscriber list is being processed. Please check your email for status.'
+											)
+										);
 									} }
 								/>
 							</EmailVerificationGate>
@@ -263,4 +269,7 @@ const mapStateToProps = ( state ) => {
 	};
 };
 
-export default connect( mapStateToProps, { recordGoogleEvent } )( localize( Followers ) );
+export default connect( mapStateToProps, {
+	recordGoogleEvent,
+	successNotice,
+} )( localize( Followers ) );
