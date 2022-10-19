2cea73af1d28947533646e2464a224d56b1ffc3f
James Tien <southp0105@gmail.com>
2022-10-19 17:19:54 +0800

    Define `DOMAIN_FIRST_LINK_IN_BIO_FLOW`

--- packages/onboarding/src/utils/flows.ts
+++ packages/onboarding/src/utils/flows.ts
@@ -1,6 +1,7 @@
 export const NEWSLETTER_FLOW = 'newsletter';
 export const NEWSLETTER_POST_SETUP_FLOW = 'newsletter';
 export const LINK_IN_BIO_FLOW = 'link-in-bio';
+export const DOMAIN_FIRST_LINK_IN_BIO_FLOW = 'domain-first-link-in-bio';
 export const LINK_IN_BIO_POST_SETUP_FLOW = 'link-in-bio-post-setup';
 export const VIDEOPRESS_FLOW = 'videopress';
 export const IMPORT_FOCUSED_FLOW = 'import-focused';
@@ -13,6 +14,7 @@ export const isNewsletterOrLinkInBioFlow = ( flowName: string | null ) => {
 				NEWSLETTER_POST_SETUP_FLOW,
 				LINK_IN_BIO_FLOW,
 				LINK_IN_BIO_POST_SETUP_FLOW,
+				DOMAIN_FIRST_LINK_IN_BIO_FLOW,
 			].includes( flowName )
 	);
 };
