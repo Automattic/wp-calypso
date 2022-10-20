141995da0cea3d7083e387f1a1442a4189aff511
James Tien <southp0105@gmail.com>
2022-10-20 15:35:19 +0800

    Introduce variations of Link in Bio stepper flow based on whether there is a given tld.

--- client/landing/stepper/declarative-flow/link-in-bio.ts
+++ client/landing/stepper/declarative-flow/link-in-bio.ts
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import { recordFullStoryEvent } from 'calypso/lib/analytics/fullstory';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import wpcom from 'calypso/lib/wp';
+import { useQuery } from '../hooks/use-query';
 import { useSiteSlug } from '../hooks/use-site-slug';
 import { USER_STORE, ONBOARD_STORE } from '../stores';
 import { recordSubmitStep } from './internals/analytics/record-submit-step';
@@ -23,36 +24,27 @@ export const linkInBio: Flow = {
 		return [ 'intro', 'linkInBioSetup', 'patterns', 'processing', 'launchpad' ] as StepPath[];
 	},
 
-	useStepNavigation( _currentStep, navigate ) {
+	useSubmit( _currentStep, navigate ) {
 		const flowName = this.name;
-		const { setStepProgress } = useDispatch( ONBOARD_STORE );
-		const flowProgress = useFlowProgress( { stepName: _currentStep, flowName } );
-		setStepProgress( flowProgress );
-		const siteSlug = useSiteSlug();
 		const userIsLoggedIn = useSelect( ( select ) => select( USER_STORE ).isCurrentUserLoggedIn() );
 		const locale = useLocale();
 
-		// trigger guides on step movement, we don't care about failures or response
-		wpcom.req.post(
-			'guides/trigger',
-			{
-				apiNamespace: 'wpcom/v2/',
-			},
-			{
-				flow: flowName,
-				step: _currentStep,
-			}
-		);
+		const queryParams = useQuery();
+		const tld = queryParams.get( 'tld' );
+		const domain = queryParams.get( 'domain' );
 
-		const getStartUrl = () => {
-			return locale && locale !== 'en'
-				? `/start/account/user/${ locale }?variationName=${ flowName }&pageTitle=Link%20in%20Bio&redirect_to=/setup/patterns?flow=${ flowName }`
-				: `/start/account/user?variationName=${ flowName }&pageTitle=Link%20in%20Bio&redirect_to=/setup/patterns?flow=${ flowName }`;
-		};
-
+		// for the standard Link in Bio flow
 		function submit( providedDependencies: ProvidedDependencies = {} ) {
-			recordSubmitStep( providedDependencies, '', flowName, _currentStep );
-			const logInUrl = getStartUrl();
+			const updatedDependencies = {
+				...providedDependencies,
+				themeSlugWithRepo: 'pub/lynx',
+			};
+			const logInUrl =
+				locale && locale !== 'en'
+					? `/start/account/user/${ locale }?variationName=${ flowName }&pageTitle=Link%20in%20Bio&redirect_to=/setup/patterns?flow=${ flowName }`
+					: `/start/account/user?variationName=${ flowName }&pageTitle=Link%20in%20Bio&redirect_to=/setup/patterns?flow=${ flowName }`;
+
+			recordSubmitStep( updatedDependencies, '', flowName, _currentStep );
 
 			switch ( _currentStep ) {
 				case 'intro':
@@ -67,7 +59,7 @@ export const linkInBio: Flow = {
 				case 'linkInBioSetup':
 					return window.location.assign(
 						`/start/${ flowName }/domains?new=${ encodeURIComponent(
-							providedDependencies.siteTitle as string
+							updatedDependencies.siteTitle as string
 						) }&search=yes&hide_initial_query=yes`
 					);
 
@@ -75,8 +67,76 @@ export const linkInBio: Flow = {
 					return navigate( 'processing' );
 				}
 			}
-			return providedDependencies;
+			return updatedDependencies;
 		}
+
+		// when there is a designated tld, the domain step will come first, hence altering the flow accordingly
+		function submitDomainFirst( providedDependencies: ProvidedDependencies = {} ) {
+			const updatedDependencies = {
+				...providedDependencies,
+				themeSlugWithRepo: 'pub/lynx',
+			};
+
+			const logInUrl =
+				locale && locale !== 'en'
+					? `/start/link-in-bio-tld/${ locale }?variationName=${ flowName }&pageTitle=Link%20in%20Bio&tld=${ tld }&domain=${ domain }`
+					: `/start/link-in-bio-tld?variationName=${ flowName }&pageTitle=Link%20in%20Bio&tld=${ tld }&domain=${ domain }`;
+
+			recordSubmitStep( providedDependencies, '', flowName, _currentStep );
+
+			switch ( _currentStep ) {
+				case 'intro':
+					if ( userIsLoggedIn ) {
+						return window.location.assign(
+							`/start/link-in-bio-tld/domains?new=${ encodeURIComponent(
+								updatedDependencies.siteTitle as string
+							) }&search=yes&hide_initial_query=yes`
+						);
+						return navigate( 'patterns' );
+					}
+					return window.location.assign( logInUrl );
+
+				case 'patterns':
+					return navigate( 'linkInBioSetup' );
+
+				case 'linkInBioSetup':
+					return navigate( 'launchpad' );
+
+				case 'launchpad': {
+					return navigate( 'processing' );
+				}
+			}
+			return updatedDependencies;
+		}
+
+		if ( tld ) {
+			return submitDomainFirst;
+		}
+
+		return submit;
+	},
+
+	useStepNavigation( _currentStep, navigate ) {
+		const flowName = this.name;
+		const { setStepProgress } = useDispatch( ONBOARD_STORE );
+		const flowProgress = useFlowProgress( { stepName: _currentStep, flowName } );
+		const siteSlug = useSiteSlug();
+
+		setStepProgress( flowProgress );
+
+		// trigger guides on step movement, we don't care about failures or response
+		wpcom.req.post(
+			'guides/trigger',
+			{
+				apiNamespace: 'wpcom/v2/',
+			},
+			{
+				flow: flowName,
+				step: _currentStep,
+			}
+		);
+
+		const submit = this.useSubmit( _currentStep, navigate );
 
 		const goBack = () => {
 			return;
