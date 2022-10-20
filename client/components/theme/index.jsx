9c40c9ff499e57094fae230ecaf3e3a036b49423
Mark Biek <mark.biek@automattic.com>
2022-10-20 15:59:59 -0400

    Callout soft-launched themes on /themes

--- client/components/theme/index.jsx
+++ client/components/theme/index.jsx
@@ -50,6 +50,7 @@ export class Theme extends Component {
 			taxonomies: PropTypes.object,
 			update: PropTypes.object,
 			price: PropTypes.any,
+			soft_launched: PropTypes.bool,
 		} ),
 		// If true, highlight this theme as active
 		active: PropTypes.bool,
@@ -90,6 +91,7 @@ export class Theme extends Component {
 		isUpdating: PropTypes.bool,
 		isUpdated: PropTypes.bool,
 		errorOnUpdate: PropTypes.bool,
+		softLaunched: PropTypes.bool,
 	};
 
 	static defaultProps = {
@@ -309,6 +311,16 @@ export class Theme extends Component {
 		);
 	}
 
+	softLaunchedBanner = () => {
+		return (
+			<>
+				{ this.props.softLaunched && (
+					<div className="theme__info-soft-launched-banner">Soft Launched</div>
+				) }
+			</>
+		);
+	};
+
 	render() {
 		const {
 			active,
@@ -457,6 +469,8 @@ export class Theme extends Component {
 					>
 						<div className="theme__tooltip">{ themeDescription }</div>
 					</Tooltip>
+
+					{ this.softLaunchedBanner() }
 
 					<div className="theme__info">
 						<h2 className="theme__info-title">{ name }</h2>
