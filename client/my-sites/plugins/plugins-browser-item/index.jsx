84dbdb5b8b03baa69b2218249143ddb9136ba6f9
Roberto Aranda <roberto.aranda@automattic.com>
2022-10-20 12:27:12 +0200

    Handle SaaS products as Marketplace products (#68667)
    *Handle SaaS products as Marketplace products
    
    * Add isSaasProduct value to PluginPrice component
    
    It will be used in the plugin list
    
    * Update CTA button and billing interval
    
    Add a isSaasProduct selector
    
    * Change CTA button text
    
    Also conisder a SaaS product as a Marketplace product
    
    * Add an additional Upgrade to Business button in a card
    
    * Fix curly eslint rule
    
    * Add redirect urls to buttons
    
    * Revert CTAButton to previous version because SaaS logic is in parent
    
    * Change alignment of footer of plugin item in plugin list

--- client/my-sites/plugins/plugins-browser-item/index.jsx
+++ client/my-sites/plugins/plugins-browser-item/index.jsx
@@ -330,34 +330,47 @@ const InstalledInOrPricing = ( {
 	return (
 		<div className="plugins-browser-item__pricing">
 			<PluginPrice plugin={ plugin } billingPeriod={ IntervalLength.MONTHLY }>
-				{ ( { isFetching, price, period } ) =>
-					isFetching ? (
-						<div className="plugins-browser-item__pricing-placeholder">...</div>
-					) : (
+				{ ( { isFetching, price, period, isSaasProduct } ) => {
+					if ( isSaasProduct ) {
+						// SaaS products do not display a price
+						return (
+							<>
+								{ ! canInstallPlugins && isLoggedIn && (
+									<span className="plugins-browser-item__requires-plan-upgrade">
+										{ translate( 'Requires a plan upgrade' ) }
+									</span>
+								) }
+							</>
+						);
+					}
+					if ( isFetching ) {
+						return <div className="plugins-browser-item__pricing-placeholder">...</div>;
+					}
+					if ( price ) {
+						return (
+							<>
+								{ price + ' ' }
+								<span className="plugins-browser-item__period">{ period }</span>
+								{ shouldUpgrade && (
+									<div className="plugins-browser-item__period">
+										{ translate( 'Requires a plan upgrade' ) }
+									</div>
+								) }
+							</>
+						);
+					}
+
+					return (
 						<>
-							{ price ? (
-								<>
-									{ price + ' ' }
-									<span className="plugins-browser-item__period">{ period }</span>
-									{ shouldUpgrade && (
-										<div className="plugins-browser-item__period">
-											{ translate( 'Requires a plan upgrade' ) }
-										</div>
-									) }
-								</>
-							) : (
-								<>
-									{ translate( 'Free' ) }
-									{ ! canInstallPlugins && isLoggedIn && (
-										<span className="plugins-browser-item__requires-plan-upgrade">
-											{ translate( 'Requires a plan upgrade' ) }
-										</span>
-									) }
-								</>
+							{ translate( 'Free' ) }
+							{ ! canInstallPlugins && isLoggedIn && (
+								<span className="plugins-browser-item__requires-plan-upgrade">
+									{ translate( 'Requires a plan upgrade' ) }
+								</span>
 							) }
 						</>
-					)
-				}
+					);
+				} }
 			</PluginPrice>
 		</div>
 	);
