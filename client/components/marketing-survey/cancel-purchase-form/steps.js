54805f8aa27641bf93ea3f4566cbc187a6f62fa6
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2022-10-21 07:36:21 -0700

    Revert "Improve cacellation flow (#68993)" (#69340)
    This reverts commit 487c08a2a1a3511c41e5bbb4db6d6c98198148e7.

--- client/components/marketing-survey/cancel-purchase-form/steps.js
+++ client/components/marketing-survey/cancel-purchase-form/steps.js
@@ -2,5 +2,3 @@ export const ATOMIC_REVERT_STEP = 'atomic_revert_step';
 export const FEEDBACK_STEP = 'feedback_step';
 export const FINAL_STEP = 'final_step';
 export const INITIAL_STEP = 'initial_step';
-export const UPSELL_STEP = 'upsell_step';
-export const NEXT_ADVENTURE_STEP = 'next_adventure_step';
