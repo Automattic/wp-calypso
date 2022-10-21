1b1c864b25286a89a26f0b254ea425946d9c68b9
Claudiu Filip <claudiu.filip@automattic.com>
2022-10-21 10:11:31 +0300

    Plugins: skip SSR rendering if requests take too long (#68775)
    * Wrap Plugin landing prefetches in timebox
    
    * Skip markup cache if timebox timeout
    
    * Refactor timebox promise resolving
    
    * Skip SSR if prefetch timesout
    
    * Add product list to react-query cache
    
    * Increase timeout to 2 seconds
    
    * Add production flag
    
    * Remove unnecessary timeboxPromise resolve
    
    * Disable timebox for development and skip SSR for prefetch errors
    
    * Log all prefetch errors and use different timeout for bots
    
    * Use server-side logging approach
    
    * Small refactor for products-list fetch param
    
    * Fix typo in product endpoint
    
    * Disable timebox on development environments
    
    * Make SSR logging more consistent
    
    * Update unit tests with new logger behaviour
    
    * Remove key from timebox function call
    
    * Remove unnecessary timeout parameter from the prefetch timebox

--- client/my-sites/plugins/controller-logged-out.js
+++ client/my-sites/plugins/controller-logged-out.js
@@ -1,10 +1,15 @@
+import config from '@automattic/calypso-config';
 import {
 	getWPCOMFeaturedPluginsQueryParams,
 	getWPCOMPluginsQueryParams,
 } from 'calypso/data/marketplace/use-wpcom-plugins-query';
 import { getWPORGPluginsQueryParams } from 'calypso/data/marketplace/use-wporg-plugin-query';
-import { requestProductsList } from 'calypso/state/products-list/actions';
-import { getProductsList } from 'calypso/state/products-list/selectors';
+import wpcom from 'calypso/lib/wp';
+import { receiveProductsList } from 'calypso/state/products-list/actions';
+
+const PREFETCH_TIMEOUT = 2000;
+const PREFETCH_TIMEOUT_BOTS = 10000;
+const PREFETCH_TIMEOUT_ERROR = 'plugins prefetch timeout';
 
 function getQueryOptions( { path, lang } ) {
 	const props = {
@@ -36,11 +41,43 @@ const prefetchPopularPlugins = ( queryClient, options ) =>
 const prefetchFeaturedPlugins = ( queryClient ) =>
 	prefetchPluginsData( queryClient, getWPCOMFeaturedPluginsQueryParams() );
 
-const prefetchProductList = ( store ) => {
-	const productsList = getProductsList( store.getState() );
-	if ( Object.values( productsList ).length === 0 ) {
-		return requestProductsList( { type: 'all' } )( store.dispatch );
+const prefetchProductList = ( queryClient, store ) => {
+	const type = 'all';
+
+	return queryClient
+		.fetchQuery( [ 'products-list', type ], () => wpcom.req.get( '/products', { type } ) )
+		.then( ( productsList ) => {
+			return store.dispatch( receiveProductsList( productsList, type ) );
+		} );
+};
+
+const prefetchTimebox = ( prefetchPromises, context ) => {
+	const racingPromises = [ Promise.all( prefetchPromises ) ];
+	const isBot = context.res?.req?.useragent?.isBot;
+
+	if ( config.isEnabled( 'ssr/prefetch-timebox' ) ) {
+		const timeboxPromise = new Promise( ( _, reject ) =>
+			setTimeout(
+				reject,
+				isBot ? PREFETCH_TIMEOUT_BOTS : PREFETCH_TIMEOUT,
+				new Error( PREFETCH_TIMEOUT_ERROR )
+			)
+		);
+
+		racingPromises.push( timeboxPromise );
 	}
+
+	return Promise.race( racingPromises ).catch( ( err ) => {
+		if ( isBot ) {
+			context.res.status( 504 );
+		}
+		context.serverSideRender = false;
+
+		context.res.req.logger.error( {
+			feature: 'calypso_ssr',
+			message: err?.message,
+		} );
+	} );
 };
 
 export async function fetchPlugins( context, next ) {
@@ -54,12 +91,15 @@ export async function fetchPlugins( context, next ) {
 		...getQueryOptions( context ),
 	};
 
-	await Promise.all( [
-		prefetchProductList( store ),
-		prefetchPaidPlugins( queryClient, options ),
-		prefetchPopularPlugins( queryClient, options ),
-		prefetchFeaturedPlugins( queryClient, options ),
-	] );
+	await prefetchTimebox(
+		[
+			prefetchProductList( queryClient, store ),
+			prefetchPaidPlugins( queryClient, options ),
+			prefetchPopularPlugins( queryClient, options ),
+			prefetchFeaturedPlugins( queryClient, options ),
+		],
+		context
+	);
 
 	next();
 }
