7c1debf8f4ba39c3ee9f386727340bd82fbe57e5
Claudiu Filip <claudiucelfilip@gmail.com>
2022-10-24 11:39:35 +0300

    Allow Plugins Landing page SSR

--- client/my-sites/plugins/index.node.js
+++ client/my-sites/plugins/index.node.js
@@ -3,27 +3,24 @@ import { JSDOM } from 'jsdom';
 import { makeLayout, ssrSetupLocale } from 'calypso/controller';
 import { setHrefLangLinks, setLocalizedCanonicalUrl } from 'calypso/controller/localized-links';
 import { overrideSanitizeSectionRoot } from 'calypso/lib/plugins/sanitize-section-content';
-import { isEnabled } from 'calypso/server/config';
 import { browsePlugins } from './controller';
 import { fetchPlugins, skipIfLoggedIn } from './controller-logged-out';
 
 export default function ( router ) {
 	const langParam = getLanguageRouteParam();
 
-	if ( isEnabled( 'plugins/ssr-landing' ) ) {
-		overrideSanitizeSectionRoot( new JSDOM().window );
+	overrideSanitizeSectionRoot( new JSDOM().window );
 
-		router(
-			[ `/${ langParam }/plugins` ],
-			skipIfLoggedIn,
-			ssrSetupLocale,
-			fetchPlugins,
-			setHrefLangLinks,
-			setLocalizedCanonicalUrl,
-			browsePlugins,
-			makeLayout
-		);
-	}
+	router(
+		[ `/${ langParam }/plugins` ],
+		skipIfLoggedIn,
+		ssrSetupLocale,
+		fetchPlugins,
+		setHrefLangLinks,
+		setLocalizedCanonicalUrl,
+		browsePlugins,
+		makeLayout
+	);
 
 	router( [ `/${ langParam }/plugins`, `/${ langParam }/plugins/*` ], ssrSetupLocale );
 }
