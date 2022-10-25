d051af025b8b3a66bc335a516435a80059edcd7f
Noah Allen <noahtallen@gmail.com>
2022-10-25 21:02:43 +0200

    Statsd: record multiple events at once  (#68068)
    * Refactor statsd helper to accept multiple events
    
    * Remove server wrapper code in favor of directly accessing the library
    
    * Update single use of statsd to use the new library

--- client/server/lib/analytics/test/index.js
+++ client/server/lib/analytics/test/index.js
@@ -1,13 +1,6 @@
-import config from '@automattic/calypso-config';
 import UserAgent from 'express-useragent';
 import superagent from 'superagent';
-import { statsdTimingUrl, statsdCountingUrl } from 'calypso/lib/analytics/statsd-utils';
 import analytics from '../index';
-jest.mock( '@automattic/calypso-config', () => jest.fn() );
-jest.mock( 'calypso/lib/analytics/statsd-utils', () => ( {
-	statsdTimingUrl: jest.fn(),
-	statsdCountingUrl: jest.fn(),
-} ) );
 
 describe( 'Server-Side Analytics', () => {
 	describe( 'tracks.recordEvent', () => {
@@ -53,66 +46,6 @@ describe( 'Server-Side Analytics', () => {
 			expect( superagent.get ).toHaveBeenCalled();
 			const url = new URL( superagent.get.mock.calls[ 0 ][ 0 ] );
 			expect( url.searchParams.get( 'a' ) ).toBeNull();
-		} );
-	} );
-
-	describe( 'statsd.recordTiming', () => {
-		beforeAll( function () {
-			jest.spyOn( superagent, 'get' ).mockReturnValue( { end: () => {} } );
-		} );
-
-		afterEach( () => {
-			statsdTimingUrl.mockClear();
-			superagent.get.mockClear();
-		} );
-
-		test( 'sends an HTTP request to the statsd URL', () => {
-			config.mockReturnValue( true ); // server_side_boom_analytics_enabled
-			statsdTimingUrl.mockReturnValue( 'http://example.com/boom.gif' );
-
-			analytics.statsd.recordTiming( 'reader', 'page-render', 150 );
-
-			expect( statsdTimingUrl ).toHaveBeenCalledWith( 'reader', 'page-render', 150 );
-			expect( superagent.get ).toHaveBeenCalledWith( 'http://example.com/boom.gif' );
-		} );
-
-		test( 'does nothing if statsd analytics is not allowed', () => {
-			config.mockReturnValue( false ); // server_side_boom_analytics_enabled
-
-			analytics.statsd.recordTiming( 'reader', 'page-render', 150 );
-
-			expect( statsdTimingUrl ).not.toHaveBeenCalled();
-			expect( superagent.get ).not.toHaveBeenCalled();
-		} );
-	} );
-
-	describe( 'statsd.recordCounting', () => {
-		beforeAll( function () {
-			jest.spyOn( superagent, 'get' ).mockReturnValue( { end: () => {} } );
-		} );
-
-		afterEach( () => {
-			statsdCountingUrl.mockClear();
-			superagent.get.mockClear();
-		} );
-
-		test( 'sends an HTTP request to the statsd URL', () => {
-			config.mockReturnValue( true ); // server_side_boom_analytics_enabled
-			statsdCountingUrl.mockReturnValue( 'http://example.com/boom.gif' );
-
-			analytics.statsd.recordCounting( 'reader', 'page-count', 1 );
-
-			expect( statsdCountingUrl ).toHaveBeenCalledWith( 'reader', 'page-count', 1 );
-			expect( superagent.get ).toHaveBeenCalledWith( 'http://example.com/boom.gif' );
-		} );
-
-		test( 'does nothing if statsd analytics is not allowed', () => {
-			config.mockReturnValue( false ); // server_side_boom_analytics_enabled
-
-			analytics.statsd.recordCounting( 'reader', 'page-count', 1 );
-
-			expect( statsdCountingUrl ).not.toHaveBeenCalled();
-			expect( superagent.get ).not.toHaveBeenCalled();
 		} );
 	} );
 } );
