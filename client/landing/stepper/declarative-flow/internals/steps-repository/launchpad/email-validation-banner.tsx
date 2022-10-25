932356495a894e54a831a69283ad76a8cfd45a5c
Sharpirate <stoynov.bogomil@gmail.com>
2022-10-25 14:28:43 -0700

    adress comments

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/email-validation-banner.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/email-validation-banner.tsx
@@ -8,7 +8,14 @@ interface EmailValidationBannerProps {
 }
 
 const CheckmarkIcon = () => (
-	<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<svg
+		aria-hidden
+		width="20"
+		height="20"
+		viewBox="0 0 20 20"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
 		<g clipPath="url(#clip0_1228_17585)">
 			<path
 				d="M18.3333 9.23355V10.0002C18.3323 11.7972 17.7504 13.5458 16.6744 14.9851C15.5984 16.4244 14.086 17.4773 12.3628 17.9868C10.6395 18.4963 8.79768 18.4351 7.11202 17.8124C5.42636 17.1896 3.98717 16.0386 3.00909 14.5311C2.03101 13.0236 1.56645 11.2403 1.68469 9.44714C1.80293 7.65402 2.49763 5.94715 3.66519 4.58111C4.83275 3.21506 6.41061 2.26303 8.16345 1.867C9.91629 1.47097 11.7502 1.65216 13.3916 2.38355"
@@ -45,7 +52,7 @@ const EmailValidationBanner = ( { email, closeBanner }: EmailValidationBannerPro
 					{ translate(
 						'Make sure to validate the email we sent to %(email)s in order to publish and share your posts. {{resendEmailLink}}Resend email{{/resendEmailLink}} or {{changeEmailLink}}change email address{{/changeEmailLink}}',
 						{
-							args: { email: email },
+							args: { email },
 							components: {
 								resendEmailLink: (
 									<button
@@ -59,7 +66,11 @@ const EmailValidationBanner = ( { email, closeBanner }: EmailValidationBannerPro
 					) }
 				</p>
 			</div>
-			<button className="launchpad__email-validation-banner-close-button" onClick={ closeBanner }>
+			<button
+				className="launchpad__email-validation-banner-close-button"
+				aria-label="close"
+				onClick={ closeBanner }
+			>
 				<Gridicon
 					className="launchpad__email-validation-banner-close-button-close-icon"
 					icon="cross"
