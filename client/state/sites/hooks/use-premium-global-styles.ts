ab049fa49f20cef52b59bd7a13c5bce341feae6e
Richard Ortiz <rcrd.ortiz@gmail.com>
2022-10-25 14:40:25 +0200

    Refactored the code

--- client/state/sites/hooks/use-premium-global-styles.ts
+++ client/state/sites/hooks/use-premium-global-styles.ts
@@ -12,25 +12,6 @@ type GlobalStylesStatus = {
 	globalStylesInUse: boolean;
 };
 
-type Site = {
-	isCreated: boolean;
-	id: number;
-};
-
-type Onboard = {
-	stepProgress?: {
-		count: number;
-		progress: number;
-	};
-};
-
-const NEW_SITE_GLOBAL_STYLES_INFO: GlobalStylesStatus = {
-	// The next line should be replaced with true once the Gating global styles feature is live.
-	// That will make all non-created sites to limit global styles.
-	shouldLimitGlobalStyles: isEnabled( 'limit-global-styles' ),
-	globalStylesInUse: false,
-};
-
 // While we are loading the Global Styles Info we can't assume that we should limit global styles, or we would be
 // showing notices for paid sites until we fetch the data from the server.
 const DEFAULT_GLOBAL_STYLES_INFO: GlobalStylesStatus = {
@@ -38,32 +19,25 @@ const DEFAULT_GLOBAL_STYLES_INFO: GlobalStylesStatus = {
 	globalStylesInUse: false,
 };
 
-const getGSInfoEndpointForSite = ( siteId: number ) => ( {
-	path: `sites/${ siteId }/global-styles/status`,
-	apiNamespace: 'wpcom/v2/',
-} );
-
-const mapSiteIdToSite = ( siteId: number | null ) => ( {
-	isCreated: !! siteId,
-	id: siteId ?? 0,
-} );
-
-const mapResponseToGlobalStylesStatus = ( data: GlobalStylesStatus ): GlobalStylesStatus => ( {
-	...DEFAULT_GLOBAL_STYLES_INFO,
-	...data,
-} );
-
-const isOnboarding = ( onboard: Onboard ): boolean => onboard?.stepProgress !== undefined;
-
-const getGlobalStylesInfoForSite = ( site: Site ): GlobalStylesStatus => {
-	if ( ! site.isCreated ) {
-		return NEW_SITE_GLOBAL_STYLES_INFO;
+const getGlobalStylesInfoForSite = ( siteId: number | null ): GlobalStylesStatus => {
+	if ( siteId == null ) {
+		return {
+			// The next line should be replaced with true once the Gating global styles feature is live.
+			// That will make all non-created sites to limit global styles.
+			shouldLimitGlobalStyles: isEnabled( 'limit-global-styles' ),
+			globalStylesInUse: false,
+		};
 	}
 
-	const globalStylesInfoEndpoint = getGSInfoEndpointForSite( site.id );
 	return wpcom.req
-		.get( globalStylesInfoEndpoint )
-		.then( mapResponseToGlobalStylesStatus )
+		.get( {
+			path: `sites/${ siteId }/global-styles/status`,
+			apiNamespace: 'wpcom/v2/',
+		} )
+		.then( ( response: GlobalStylesStatus ) => ( {
+			...DEFAULT_GLOBAL_STYLES_INFO,
+			...response,
+		} ) )
 		.catch( () => DEFAULT_GLOBAL_STYLES_INFO );
 };
 
@@ -76,23 +50,20 @@ export function usePremiumGlobalStyles(): GlobalStylesStatus {
 
 	// When site id is null it means that the site hasn't been created yet.
 	const siteId = useSelector( ( state ) => {
-		if ( isOnboarding( onboard ) && ! siteIdParam && ! siteSlugParam ) {
-			return null;
-		}
+		const siteIdOrSlug =
+			onboard?.stepProgress !== undefined ? siteIdParam ?? siteSlugParam : selectedSiteId;
 
-		const siteIdOrSlug = selectedSiteId ?? siteIdParam ?? siteSlugParam;
 		if ( ! siteIdOrSlug ) {
 			return null;
 		}
+
 		const site = getSite( state, siteIdOrSlug );
 		return site?.ID ?? null;
 	} );
 
-	const site = mapSiteIdToSite( siteId );
-
 	const { data } = useQuery(
-		[ 'globalStylesInfo', site ],
-		() => getGlobalStylesInfoForSite( site ),
+		[ 'globalStylesInfo', siteId ],
+		() => getGlobalStylesInfoForSite( siteId ),
 		{
 			placeholderData: DEFAULT_GLOBAL_STYLES_INFO,
 			refetchOnWindowFocus: false,
