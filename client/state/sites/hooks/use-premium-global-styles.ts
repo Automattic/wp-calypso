6e666acfc34f6ab6bdd5ae3200685399536f11b3
Richard Ortiz <rcrd.ortiz@gmail.com>
2022-10-25 11:55:49 +0200

    We now check if we are on an onboarding flow that does not have a siteId. I also added some protection agains a malformed API valid response

--- client/state/sites/hooks/use-premium-global-styles.ts
+++ client/state/sites/hooks/use-premium-global-styles.ts
@@ -1,11 +1,13 @@
 import { isEnabled } from '@automattic/calypso-config';
+import { select } from '@wordpress/data';
 import { useQuery } from 'react-query';
 import { useSelector } from 'react-redux';
+import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import wpcom from 'calypso/lib/wp';
 import { getSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
-type GlobalStylesInfo = {
+type GlobalStylesStatus = {
 	shouldLimitGlobalStyles: boolean;
 	globalStylesInUse: boolean;
 };
@@ -15,7 +17,14 @@ type Site = {
 	id: number;
 };
 
-const NEW_SITE_GLOBAL_STYLES_INFO: GlobalStylesInfo = {
+type Onboard = {
+	stepProgress?: {
+		count: number;
+		progress: number;
+	};
+};
+
+const NEW_SITE_GLOBAL_STYLES_INFO: GlobalStylesStatus = {
 	// The next line should be replaced with true once the Gating global styles feature is live.
 	// That will make all non-created sites to limit global styles.
 	shouldLimitGlobalStyles: isEnabled( 'limit-global-styles' ),
@@ -24,7 +33,7 @@ const NEW_SITE_GLOBAL_STYLES_INFO: GlobalStylesInfo = {
 
 // While we are loading the Global Styles Info we can't assume that we should limit global styles, or we would be
 // showing notices for paid sites until we fetch the data from the server.
-const DEFAULT_GLOBAL_STYLES_INFO: GlobalStylesInfo = {
+const DEFAULT_GLOBAL_STYLES_INFO: GlobalStylesStatus = {
 	shouldLimitGlobalStyles: false,
 	globalStylesInUse: false,
 };
@@ -39,23 +48,38 @@ const mapSiteIdToSite = ( siteId: number | null ) => ( {
 	id: siteId ?? 0,
 } );
 
-const getGlobalStylesInfoForSite = ( site: Site ) => {
+const mapResponseToGlobalStylesStatus = ( data: GlobalStylesStatus ): GlobalStylesStatus => ( {
+	...DEFAULT_GLOBAL_STYLES_INFO,
+	...data,
+} );
+
+const isOnboarding = ( onboard: Onboard ): boolean => onboard?.stepProgress !== undefined;
+
+const getGlobalStylesInfoForSite = ( site: Site ): GlobalStylesStatus => {
 	if ( ! site.isCreated ) {
 		return NEW_SITE_GLOBAL_STYLES_INFO;
 	}
 
 	const globalStylesInfoEndpoint = getGSInfoEndpointForSite( site.id );
-	return wpcom.req.get( globalStylesInfoEndpoint ).catch( () => DEFAULT_GLOBAL_STYLES_INFO );
+	return wpcom.req
+		.get( globalStylesInfoEndpoint )
+		.then( mapResponseToGlobalStylesStatus )
+		.catch( () => DEFAULT_GLOBAL_STYLES_INFO );
 };
 
-export function usePremiumGlobalStyles(): GlobalStylesInfo {
+export function usePremiumGlobalStyles(): GlobalStylesStatus {
 	const params = new URLSearchParams( window.location.search );
 	const siteSlugParam = params.get( 'siteSlug' );
 	const siteIdParam = params.get( 'siteId' );
 	const selectedSiteId = useSelector( getSelectedSiteId );
+	const onboard = select( ONBOARD_STORE ).getState();
 
 	// When site id is null it means that the site hasn't been created yet.
 	const siteId = useSelector( ( state ) => {
+		if ( isOnboarding( onboard ) && ! siteIdParam && ! siteSlugParam ) {
+			return null;
+		}
+
 		const siteIdOrSlug = selectedSiteId ?? siteIdParam ?? siteSlugParam;
 		if ( ! siteIdOrSlug ) {
 			return null;
