2f1bbd2cffd601a0d7178c91cae289ca3d5bd375
Michael Cain <michael@michaeldcain.com>
2022-10-25 16:42:56 -0400

    Only show the link if promoted posts are enabled

--- client/my-sites/purchases/subscriptions/account-level-advertising-links.tsx
+++ client/my-sites/purchases/subscriptions/account-level-advertising-links.tsx
@@ -1,11 +1,18 @@
 import { CompactCard } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { useSelector } from 'react-redux';
+import { PromoteWidgetStatus, usePromoteWidget } from 'calypso/lib/promote-post';
 import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
 export default function AccountLevelAdvertisingLinks() {
 	const translate = useTranslate();
 	const selectedSiteSlug = useSelector( getSelectedSiteSlug );
+	const shouldShowAdvertisingOption = usePromoteWidget() === PromoteWidgetStatus.ENABLED;
+
+	if ( ! shouldShowAdvertisingOption ) {
+		return null;
+	}
+
 	return (
 		<>
 			<CompactCard href={ `/advertising/${ selectedSiteSlug }` }>
