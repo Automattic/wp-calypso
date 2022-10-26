e2af4b5851965f0271f7db1c0890ce4d0e6c3a5a
Renan <renansscarvalho@gmail.com>
2022-10-26 12:57:39 +0200

    Fix merge mistake

--- client/my-sites/plans-features-main/index.jsx
+++ client/my-sites/plans-features-main/index.jsx
@@ -119,6 +119,7 @@ export class PlansFeaturesMain extends Component {
 			isReskinned,
 			isFAQCondensedExperiment,
 			isPlansInsideStepper,
+			isPlanTaglineExperiment,
 		} = this.props;
 
 		const plans = this.getPlansForPlanFeatures();
