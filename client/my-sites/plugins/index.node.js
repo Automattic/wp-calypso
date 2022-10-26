91ca4332be90ca4c78fc36aa8129ebeb85fd41c8
Claudiu Filip <claudiucelfilip@gmail.com>
2022-10-26 10:29:18 +0300

    Restore flag and enable it on all environments

--- client/my-sites/plugins/index.node.js
+++ client/my-sites/plugins/index.node.js
@@ -3,24 +3,27 @@ import { JSDOM } from 'jsdom';
 import { makeLayout, ssrSetupLocale } from 'calypso/controller';
 import { setHrefLangLinks, setLocalizedCanonicalUrl } from 'calypso/controller/localized-links';
 import { overrideSanitizeSectionRoot } from 'calypso/lib/plugins/sanitize-section-content';
+import { isEnabled } from 'calypso/server/config';
 import { browsePlugins } from './controller';
 import { fetchPlugins, skipIfLoggedIn } from './controller-logged-out';
 
 export default function ( router ) {
 	const langParam = getLanguageRouteParam();
 
-	overrideSanitizeSectionRoot( new JSDOM().window );
+	if ( isEnabled( 'plugins/ssr-landing' ) ) {
+		overrideSanitizeSectionRoot( new JSDOM().window );
 
-	router(
-		[ `/${ langParam }/plugins` ],
-		skipIfLoggedIn,
-		ssrSetupLocale,
-		fetchPlugins,
-		setHrefLangLinks,
-		setLocalizedCanonicalUrl,
-		browsePlugins,
-		makeLayout
-	);
+		router(
+			[ `/${ langParam }/plugins` ],
+			skipIfLoggedIn,
+			ssrSetupLocale,
+			fetchPlugins,
+			setHrefLangLinks,
+			setLocalizedCanonicalUrl,
+			browsePlugins,
+			makeLayout
+		);
+	}
 
 	router( [ `/${ langParam }/plugins`, `/${ langParam }/plugins/*` ], ssrSetupLocale );
 }
