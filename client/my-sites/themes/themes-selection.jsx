07deb81cbf6cb8f75be8254ee9bb4b103a1e12c8
daledupreez <dale@automattic.com>
2022-10-27 13:35:15 +0200

    Fix theme preview options for bundled themes (#69510)

--- client/my-sites/themes/themes-selection.jsx
+++ client/my-sites/themes/themes-selection.jsx
@@ -133,6 +133,9 @@ class ThemesSelection extends Component {
 					secondaryOption = null;
 				} else if ( this.props.isThemeActive( themeId ) ) {
 					defaultOption = options.customize;
+				} else if ( options.upgradePlanForBundledThemes ) {
+					defaultOption = options.upgradePlanForBundledThemes;
+					secondaryOption = null;
 				} else if ( options.purchase ) {
 					defaultOption = options.purchase;
 				} else if ( options.upgradePlan ) {
