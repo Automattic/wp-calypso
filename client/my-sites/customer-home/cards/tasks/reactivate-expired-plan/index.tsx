0937fc25e2da1c03fded1f3c8bba6a5626e1d449
Philip Jackson <p-jackson@live.com>
2022-10-27 15:01:49 +1300

    Add support link to TASK_REACTIVATE_EXPIRED_PLAN

--- client/my-sites/customer-home/cards/tasks/reactivate-expired-plan/index.tsx
+++ client/my-sites/customer-home/cards/tasks/reactivate-expired-plan/index.tsx
@@ -1,6 +1,8 @@
+import { localizeUrl } from '@automattic/i18n-utils';
 import { translate } from 'i18n-calypso';
 import { useSelector } from 'react-redux';
 import expiredIllustration from 'calypso/assets/images/customer-home/disconnected-dark.svg';
+import InlineSupportLink from 'calypso/components/inline-support-link';
 import { TASK_REACTIVATE_EXPIRED_PLAN } from 'calypso/my-sites/customer-home/cards/constants';
 import Task from 'calypso/my-sites/customer-home/cards/tasks/task';
 import { getSelectedSiteSlug } from 'calypso/state/ui/selectors/index';
@@ -17,7 +19,7 @@ export const ReactivateExpiredPlan = () => {
 			isUrgent
 			title={ translate( 'Restore your plan' ) }
 			description={ translate(
-				'Your plan expired and your site reverted to the Free plan. If you would like to continue using your previous features, you must first purchase an eligible plan.{{lineBreak/}}No further action is needed if you wish to continue with the Free plan.',
+				'Your plan expired and your site reverted to the Free plan. {{supportLink}}Follow these steps to continue using your previous features{{/supportLink}}, beginning with purchasing an eligible plan.{{lineBreak/}}No further action is needed if you wish to continue with the Free plan.',
 				{
 					components: {
 						lineBreak: (
@@ -25,6 +27,20 @@ export const ReactivateExpiredPlan = () => {
 								<br />
 								<br />
 							</>
+						),
+						supportLink: (
+							<InlineSupportLink
+								supportPostId={ 222413 }
+								supportLink={ localizeUrl(
+									'https://wordpress.com/support/restore-your-site-after-the-plan-expires/'
+								) }
+								noWrap={ false }
+								showIcon={ false }
+								supportContext="reactivate"
+								tracksEvent="calypso_customer_home_reactivate_support_page_view"
+								statsGroup="calypso_customer_home"
+								statsName="menus_view_tutorial"
+							/>
 						),
 					},
 				}
