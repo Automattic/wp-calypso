80209ce01dc4739276c846a0f6b6183cdec2bc89
vikneshwar <viknesh.loganathan@automattic.com>
2022-10-28 01:45:17 +0530

    Add contextual trigger for Backup add-on products ( with dynamic add-on suggestion ) (#69449)
    * Jetpack backup add-on integration with dynamic upsell suggestion
    * Currently the suggestion works like this: 10GB -> 10GB, 1TB -> 1TB. This revision changes that.
    * We will dynamically show the upsell for the user based on how much storage they have lapsed from the base plan
    * For Ex: If the use has used more than 30GB in the 10GB base plan, we will suggest 100GB upgrade instead of 10GB
    
    * Jetpack Backup Add-on integration - remove logs
    
    * Jetpack Backup Add-on integration - update correct constants
    
    * Jetpack Backup Add-on integration - fix type

--- client/components/backup-storage-space/usage-warning/use-upsell-slug.ts
+++ client/components/backup-storage-space/usage-warning/use-upsell-slug.ts
@@ -4,8 +4,9 @@ import {
 	PLAN_JETPACK_SECURITY_T1_MONTHLY,
 	PLAN_JETPACK_SECURITY_T1_YEARLY,
 	PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_10GB_MONTHLY,
+	PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_100GB_MONTHLY,
 	PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_1TB_MONTHLY,
-	//useJetpack100GbStorageAmountText,
+	useJetpack100GbStorageAmountText,
 	useJetpack10GbStorageAmountText,
 	useJetpack1TbStorageAmountText,
 	PRODUCT_JETPACK_BACKUP_T2_MONTHLY,
@@ -19,11 +20,13 @@ import slugToSelectorProduct from 'calypso/my-sites/plans/jetpack-plans/slug-to-
 import { SelectorProduct } from 'calypso/my-sites/plans/jetpack-plans/types';
 import useItemPrice from 'calypso/my-sites/plans/jetpack-plans/use-item-price';
 import { getSitePurchases } from 'calypso/state/purchases/selectors';
+import { getRewindBytesAvailable, getRewindBytesUsed } from 'calypso/state/rewind/selectors';
 import type { Purchase } from 'calypso/lib/purchases/types';
 
 type SelectorProductWithStorage = SelectorProduct & {
 	storage: React.ReactChild;
 };
+
 const UPGRADEABLE_STORAGE_PRODUCT_SLUGS_T1 = [
 	PRODUCT_JETPACK_BACKUP_T1_MONTHLY,
 	PRODUCT_JETPACK_BACKUP_T1_YEARLY,
@@ -37,45 +40,91 @@ const UPGRADEABLE_STORAGE_PRODUCT_SLUGS_T2 = [
 	PLAN_JETPACK_SECURITY_T2_MONTHLY,
 	PLAN_JETPACK_SECURITY_T2_YEARLY,
 ];
+const BYTES_10GB = 10737418240;
+const BYTES_100GB = 107374182400;
+const BYTES_1TB = 1073741824000;
 
-export const isJetpackT1Product = ( { subscriptionStatus, productSlug }: Purchase ) => {
-	return (
-		subscriptionStatus === 'active' && UPGRADEABLE_STORAGE_PRODUCT_SLUGS_T1.includes( productSlug )
-	);
+const storageToUpsellProduct: Record< number, string > = {
+	[ BYTES_10GB ]: PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_10GB_MONTHLY,
+	[ BYTES_100GB ]: PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_100GB_MONTHLY,
+	[ BYTES_1TB ]: PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_1TB_MONTHLY,
 };
+export const isJetpackProductSlugMatch =
+	( slugList: Array< string > ) =>
+	( { subscriptionStatus, productSlug }: Purchase ) =>
+		subscriptionStatus === 'active' && slugList.includes( productSlug );
 
-export const isJetpackT2Product = ( { subscriptionStatus, productSlug }: Purchase ) => {
-	return (
-		subscriptionStatus === 'active' && UPGRADEABLE_STORAGE_PRODUCT_SLUGS_T2.includes( productSlug )
-	);
-};
-
-export default ( siteId: number | null ) => {
+export default ( siteId: number ) => {
 	const sitePurchases = useSelector( ( state ) => getSitePurchases( state, siteId ) );
+
 	const TEN_GIGABYTES = useJetpack10GbStorageAmountText();
-	//const HUNDRED_GIGABYTES = useJetpack100GbStorageAmountText();
+	const HUNDRED_GIGABYTES = useJetpack100GbStorageAmountText();
 	const ONE_TERABYTES = useJetpack1TbStorageAmountText();
 
 	const currencyCode = sitePurchases?.[ 0 ]?.currencyCode;
 
+	const bytesAvailable = useSelector( ( state ) => getRewindBytesAvailable( state, siteId ) );
+	const bytesUsed = useSelector( ( state ) => getRewindBytesUsed( state, siteId ) );
+
 	const upsellSlug = useMemo( () => {
-		if ( sitePurchases.some( isJetpackT1Product ) ) {
+		const ADD_ON_STORAGE_MAP: Record< string, React.ReactChild > = {
+			[ PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_10GB_MONTHLY ]: TEN_GIGABYTES,
+			[ PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_100GB_MONTHLY ]: HUNDRED_GIGABYTES,
+			[ PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_1TB_MONTHLY ]: ONE_TERABYTES,
+		};
+
+		//if usage has crossed over the storage limit, then dynamically calculate the upgrade option
+		if ( bytesUsed && bytesAvailable && bytesUsed > bytesAvailable ) {
+			const additionalBytesUsed = bytesUsed - bytesAvailable;
+
+			// add aditional 25% buffer
+			const additionalBytesNeeded = additionalBytesUsed + additionalBytesUsed * 0.25;
+
+			//Since 1TB is our max upgrade but the additional storage needed is greater than 1TB, then just return 1TB
+			if ( additionalBytesNeeded > BYTES_1TB ) {
+				return {
+					...slugToSelectorProduct( PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_1TB_MONTHLY ),
+					storage: ADD_ON_STORAGE_MAP[ PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_1TB_MONTHLY ],
+				} as SelectorProductWithStorage;
+			}
+			const matchedBytes =
+				Object.keys( storageToUpsellProduct )
+					.map( Number ) // .keys returns as strings, so convert it to a integer
+					.sort( ( a, b ) => a - b ) // order of keys is not always guaranteed, so sort it before we do the search
+					.find( ( bytes ) => bytes > additionalBytesNeeded ) ?? BYTES_10GB; // find the first add-on with storage aboev the current usage
+
+			const upsellSlugId = storageToUpsellProduct[ matchedBytes ];
 			return {
-				...slugToSelectorProduct( PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_10GB_MONTHLY ),
-				storage: TEN_GIGABYTES,
+				...slugToSelectorProduct( upsellSlugId ),
+				storage: ADD_ON_STORAGE_MAP[ upsellSlugId ],
 			} as SelectorProductWithStorage;
 		}
-		if ( sitePurchases.some( isJetpackT2Product ) ) {
+
+		//if usage is still within the storage limit then upgrade 10GB -> 10GB and 1TB -> 1TB
+		if ( sitePurchases.some( isJetpackProductSlugMatch( UPGRADEABLE_STORAGE_PRODUCT_SLUGS_T1 ) ) ) {
+			return {
+				...slugToSelectorProduct( PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_10GB_MONTHLY ),
+				storage: ADD_ON_STORAGE_MAP[ PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_10GB_MONTHLY ],
+			} as SelectorProductWithStorage;
+		}
+		if ( sitePurchases.some( isJetpackProductSlugMatch( UPGRADEABLE_STORAGE_PRODUCT_SLUGS_T2 ) ) ) {
 			return {
 				...slugToSelectorProduct( PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_1TB_MONTHLY ),
-				storage: ONE_TERABYTES,
+				storage: ADD_ON_STORAGE_MAP[ PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_1TB_MONTHLY ],
 			} as SelectorProductWithStorage;
 		}
 		return {
 			...slugToSelectorProduct( PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_10GB_MONTHLY ),
-			storage: TEN_GIGABYTES,
+			storage: ADD_ON_STORAGE_MAP[ PRODUCT_JETPACK_BACKUP_ADDON_STORAGE_10GB_MONTHLY ],
 		} as SelectorProductWithStorage;
-	}, [ sitePurchases, TEN_GIGABYTES, ONE_TERABYTES ] );
+	}, [
+		TEN_GIGABYTES,
+		HUNDRED_GIGABYTES,
+		ONE_TERABYTES,
+		sitePurchases,
+		bytesUsed,
+		bytesAvailable,
+	] );
 
 	const { originalPrice, isFetching } = useItemPrice(
 		siteId,
