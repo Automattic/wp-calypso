27031a2452b583f3c5887c025869d057b410c8fc
James Kenneth Guidaven <jkguidaven@gmail.com>
2022-10-28 15:15:24 +0800

    Rename buildKeyPairByProductSlugs to buildKeyValuePairByProductSlugs.

--- client/my-sites/checkout/checkout-thank-you/utils.ts
+++ client/my-sites/checkout/checkout-thank-you/utils.ts
@@ -12,7 +12,6 @@ import JetpackSocialPluginImage from 'calypso/assets/images/jetpack/jetpack-plug
 import JetpackPluginImage from 'calypso/assets/images/jetpack/licensing-activation-plugin-install.svg';
 import { domainManagementEdit, domainManagementList } from 'calypso/my-sites/domains/paths';
 
-<<<<<<< HEAD
 const buildKeyValuePairByProductSlugs = (
 	productSlugs: ReadonlyArray< string >,
 	value: string
@@ -26,6 +25,13 @@ const WPORG_PLUGIN_SLUG_MAP: Record< string, string > = {
 	...buildKeyValuePairByProductSlugs( JETPACK_SOCIAL_PRODUCTS, 'jetpack-social' ),
 	...buildKeyValuePairByProductSlugs( JETPACK_SEARCH_PRODUCTS, 'jetpack-search' ),
 	...buildKeyValuePairByProductSlugs( JETPACK_VIDEOPRESS_PRODUCTS, 'jetpack-videopress' ),
+};
+
+const JETPACK_PLUGIN_IMAGE_MAP: Record< string, string > = {
+	...buildKeyValuePairByProductSlugs( JETPACK_BACKUP_PRODUCTS, JetpackBackupPluginImage ),
+	...buildKeyValuePairByProductSlugs( JETPACK_BOOST_PRODUCTS, JetpackBoostPluginImage ),
+	...buildKeyValuePairByProductSlugs( JETPACK_SOCIAL_PRODUCTS, JetpackSocialPluginImage ),
+	...buildKeyValuePairByProductSlugs( JETPACK_SEARCH_PRODUCTS, JetpackSearchPluginImage ),
 };
 
 const JETPACK_SUPPORT_DOCS_MAP: Record< string, string > = {
@@ -45,31 +51,6 @@ const JETPACK_SUPPORT_DOCS_MAP: Record< string, string > = {
 		JETPACK_SEARCH_PRODUCTS,
 		'search/jetpack-search-plugin/#installing-jetpack-search-plugin'
 	),
-=======
-const buildKeyPairByProductSlugs = (
-	productSlugs: ReadonlyArray< string >,
-	wporgPluginSlug: string
-) => {
-	return productSlugs.reduce(
-		( map, productSlug ) => ( { ...map, [ productSlug ]: wporgPluginSlug } ),
-		{}
-	);
-};
-
-const WPORG_PLUGIN_SLUG_MAP: Record< string, string > = {
-	...buildKeyPairByProductSlugs( JETPACK_BACKUP_PRODUCTS, 'jetpack-backup' ),
-	...buildKeyPairByProductSlugs( JETPACK_BOOST_PRODUCTS, 'jetpack-boost' ),
-	...buildKeyPairByProductSlugs( JETPACK_SOCIAL_PRODUCTS, 'jetpack-social' ),
-	...buildKeyPairByProductSlugs( JETPACK_SEARCH_PRODUCTS, 'jetpack-search' ),
-	...buildKeyPairByProductSlugs( JETPACK_VIDEOPRESS_PRODUCTS, 'jetpack-videopress' ),
-};
-
-const JETPACK_PLUGIN_IMAGE_MAP: Record< string, string > = {
-	...buildKeyPairByProductSlugs( JETPACK_BACKUP_PRODUCTS, JetpackBackupPluginImage ),
-	...buildKeyPairByProductSlugs( JETPACK_BOOST_PRODUCTS, JetpackBoostPluginImage ),
-	...buildKeyPairByProductSlugs( JETPACK_SOCIAL_PRODUCTS, JetpackSocialPluginImage ),
-	...buildKeyPairByProductSlugs( JETPACK_SEARCH_PRODUCTS, JetpackSearchPluginImage ),
->>>>>>> af6c1edbff (Load specific Jetpack plugin image as activation footer image.)
 };
 
 export function getWPORGPluginLink( productSlug: string ): string {
@@ -78,17 +59,16 @@ export function getWPORGPluginLink( productSlug: string ): string {
 	return wporgPluginSlug ? `https://wordpress.org/plugins/${ wporgPluginSlug }/` : '';
 }
 
-<<<<<<< HEAD
 export function getJetpackPluginSupportDocLink( productSlug: string ): string {
 	const supportDoc = JETPACK_SUPPORT_DOCS_MAP[ productSlug ];
 	const DEFAULT_SUPPORT_DOC_LINK =
 		'https://jetpack.com/support/install-jetpack-and-connect-your-new-plan/';
 
 	return supportDoc ? `https://jetpack.com/support/${ supportDoc }` : DEFAULT_SUPPORT_DOC_LINK;
-=======
+}
+
 export function getJetpackPluginImage( productSlug: string ): string {
 	return JETPACK_PLUGIN_IMAGE_MAP[ productSlug ] ?? JetpackPluginImage;
->>>>>>> af6c1edbff (Load specific Jetpack plugin image as activation footer image.)
 }
 
 export function getDomainManagementUrl(
