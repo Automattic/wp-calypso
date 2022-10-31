2f09f6fa4055bc00f8917fbc46cf2f66f8d64c52
Bogomil Stoynov <stoynov.bogomil@gmail.com>
2022-10-31 10:31:09 -0700

    Launchpad: Email Validation Banner (#69252)
    * create email validation banner
    
    * handle email reset logic
    
    * convert close icon to <GridIcon />
    
    * fix string interpolation
    
    * abstract common logic into reusable hook
    
    * fix unit tests for email notice
    
    * remove console.log
    
    * adress comments
    
    * switch to a reusable checkmark icon

---
+++ client/landing/stepper/hooks/use-resend-email-verification.ts
@@ -0,0 +1,32 @@
+import { useTranslate } from 'i18n-calypso';
+import { useDispatch } from 'react-redux';
+import { successNotice, errorNotice } from 'calypso/state/notices/actions';
+import { useSendEmailVerification } from './use-send-email-verification';
+
+export function useResendEmailVerification() {
+	const resendEmailNotice = 'resend-verification-email';
+	const dispatch = useDispatch();
+	const resendEmail = useSendEmailVerification();
+	const translate = useTranslate();
+
+	return async () => {
+		try {
+			const result = await resendEmail();
+			if ( result.success ) {
+				dispatch(
+					successNotice( translate( 'Verification email resent. Please check your inbox.' ), {
+						id: resendEmailNotice,
+						duration: 4000,
+					} )
+				);
+				return;
+			}
+		} catch ( Error ) {
+			dispatch(
+				errorNotice( translate( "Couldn't resend verification email. Please try again." ), {
+					id: resendEmailNotice,
+				} )
+			);
+		}
+	};
+}
