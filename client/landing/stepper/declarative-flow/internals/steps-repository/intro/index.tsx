0a14527eab235a86bc7d584b2c6d87cafd0c3953
Kosta <heavyweight@users.noreply.github.com>
2022-11-01 08:40:04 +0100

    Add new newsletter intro content (#69388)
    * Add new newsletter intro content
    
    * TailoredFlows: adjust intro page for newsletter
    
    * TailoredFlows: newletter intro add new background and apply responsive fixes
    
    * TailoredFlows: newsletter intro fixes for mobile
    
    * TailoredFlows: newsletter intro use smaller background for mobile
    
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>

--- client/landing/stepper/declarative-flow/internals/steps-repository/intro/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/intro/index.tsx
@@ -1,14 +1,63 @@
-import { NEWSLETTER_FLOW } from '@automattic/onboarding';
+import { useLocale } from '@automattic/i18n-utils';
+import { LINK_IN_BIO_FLOW, NEWSLETTER_FLOW } from '@automattic/onboarding';
+import { createInterpolateElement, useMemo } from '@wordpress/element';
+import { useI18n } from '@wordpress/react-i18n';
 import cx from 'classnames';
 import { StepContainer } from 'calypso/../packages/onboarding/src';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
-import IntroStep from './intro';
+import IntroStep, { IntroContent } from './intro';
 import type { Step } from '../../types';
 
 import './styles.scss';
 
+const useIntroContent = ( flowName: string | null ): IntroContent => {
+	const { __, hasTranslation } = useI18n();
+	const locale = useLocale();
+
+	return useMemo( () => {
+		if ( flowName === LINK_IN_BIO_FLOW ) {
+			return {
+				title: createInterpolateElement(
+					__( 'You’re 3 minutes away from<br />a stand-out Link in Bio site.<br />Ready? ' ),
+					{ br: <br /> }
+				),
+				buttonText: __( 'Get started' ),
+			};
+		}
+
+		if (
+			locale === 'en' ||
+			[
+				`Sign in. Set up. Send out.`,
+				`You’re a few steps away from launching a beautiful Newsletter with<br />everything you’ll ever need to grow your audience.`,
+				'Start building your Newsletter',
+			].every( ( translation ) => hasTranslation( translation ) )
+		) {
+			return {
+				title: __( 'Sign in. Set up. Send out.' ),
+				text: createInterpolateElement(
+					__(
+						`You’re a few steps away from launching a beautiful Newsletter with<br />everything you’ll ever need to grow your audience.`
+					),
+					{ br: <br /> }
+				),
+				buttonText: __( 'Start building your Newsletter' ),
+			};
+		}
+		return {
+			title: createInterpolateElement(
+				__( 'You’re 3 minutes away from<br />a launch-ready Newsletter. ' ),
+				{ br: <br /> }
+			),
+			buttonText: __( 'Get started' ),
+		};
+	}, [ flowName, locale, __, hasTranslation ] );
+};
+
 const Intro: Step = function Intro( { navigation, flow } ) {
 	const { submit, goBack } = navigation;
+	const introContent = useIntroContent( flow );
+	const showNewNewsletterIntro = flow === NEWSLETTER_FLOW && 'text' in introContent;
 
 	const handleSubmit = () => {
 		submit?.();
@@ -16,14 +65,15 @@ const Intro: Step = function Intro( { navigation, flow } ) {
 	return (
 		<StepContainer
 			stepName="intro"
-			className={ cx( { 'is-newsletter': flow === NEWSLETTER_FLOW } ) }
+			className={ cx( { 'is-using-new-intro-design': showNewNewsletterIntro } ) }
 			goBack={ goBack }
 			isHorizontalLayout={ false }
 			isWideLayout={ true }
 			isLargeSkipLayout={ false }
-			stepContent={ <IntroStep flowName={ flow as string } onSubmit={ handleSubmit } /> }
+			stepContent={ <IntroStep introContent={ introContent } onSubmit={ handleSubmit } /> }
 			recordTracksEvent={ recordTracksEvent }
-			showJetpackPowered={ flow === NEWSLETTER_FLOW }
+			showHeaderJetpackPowered={ showNewNewsletterIntro }
+			showJetpackPowered={ flow === NEWSLETTER_FLOW && ! showNewNewsletterIntro }
 		/>
 	);
 };
