18ff1db0db27ed3525e50ae9adeba8f60e9ef6d1
Renan <renansscarvalho@gmail.com>
2022-11-01 09:11:28 +0100

    Pass dynamic flow name to component and style fix

--- client/landing/stepper/declarative-flow/internals/steps-repository/plans/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/plans/index.tsx
@@ -3,7 +3,7 @@ import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import PlansWrapper from './plans-wrapper';
 import type { Step } from '../../types';
 
-const plans: Step = function plans( { navigation } ) {
+const plans: Step = function plans( { navigation, flow } ) {
 	const { submit, goBack } = navigation;
 
 	const handleSubmit = () => {
@@ -12,15 +12,14 @@ const plans: Step = function plans( { navigation } ) {
 
 	return (
 		<StepContainer
-			// For now it only supports LiB flows
-			stepName={ 'link-in-bio' }
+			stepName="plans"
 			goBack={ goBack }
 			isHorizontalLayout={ false }
 			isWideLayout={ true }
 			hideFormattedHeader={ true }
 			isLargeSkipLayout={ false }
 			hideBack={ true }
-			stepContent={ <PlansWrapper flowName={ 'link-in-bio' } onSubmit={ handleSubmit } /> }
+			stepContent={ <PlansWrapper flowName={ flow } onSubmit={ handleSubmit } /> }
 			recordTracksEvent={ recordTracksEvent }
 		/>
 	);
