b74ca4d1c52fd9085d377b2a64ac70dd9e8379b3
Renan <renansscarvalho@gmail.com>
2022-11-01 10:39:05 +0100

    Improve style and remove unnecessary code changes

--- client/landing/stepper/declarative-flow/link-in-bio.ts
+++ client/landing/stepper/declarative-flow/link-in-bio.ts
@@ -30,11 +30,11 @@ export const linkInBio: Flow = {
 			'intro',
 			'linkInBioSetup',
 			'domains',
+			'plans',
 			'patterns',
 			'siteCreationStep',
 			'processing',
 			'launchpad',
-			'plans',
 		] as StepPath[];
 	},
 
@@ -44,11 +44,7 @@ export const linkInBio: Flow = {
 		const flowProgress = useFlowProgress( { stepName: _currentStep, flowName } );
 		setStepProgress( flowProgress );
 		const siteSlug = useSiteSlug();
-		const { userIsLoggedIn } = useSelect( ( select ) => {
-			return {
-				userIsLoggedIn: select( USER_STORE ).isCurrentUserLoggedIn(),
-			};
-		} );
+		const userIsLoggedIn = useSelect( ( select ) => select( USER_STORE ).isCurrentUserLoggedIn() );
 		const locale = useLocale();
 
 		// trigger guides on step movement, we don't care about failures or response
