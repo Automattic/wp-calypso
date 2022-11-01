4b94c1ac2b831af7e1bf09631c015d7bf6287f88
Emanuele Buccelli <escapemanuele@gmail.com>
2022-11-01 12:15:11 +0100

    Removed window.redux

--- client/landing/stepper/index.tsx
+++ client/landing/stepper/index.tsx
@@ -138,7 +138,6 @@ window.AppBoot = async () => {
 	const reduxStore = createReduxStore( initialState, initialReducer );
 	setStore( reduxStore, getStateFromCache( userId ) );
 	setupLocale( user, reduxStore );
-	window.redux = reduxStore;
 
 	user && initializeCalypsoUserStore( reduxStore, user as CurrentUser );
 
