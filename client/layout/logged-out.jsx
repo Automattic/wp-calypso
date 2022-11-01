8e080389a2c754f19de6d9888e73c66c6e3b3bca
Dev <dev.sa@a8c.com>
2022-10-31 21:43:53 -0400

    Address plugin class detection changes for Plugins Navbar in SSR #67707

--- client/layout/logged-out.jsx
+++ client/layout/logged-out.jsx
@@ -11,6 +11,7 @@ import wooDnaConfig from 'calypso/jetpack-connect/woo-dna-config';
 import MasterbarLoggedOut from 'calypso/layout/masterbar/logged-out';
 import MasterbarLogin from 'calypso/layout/masterbar/login';
 import OauthClientMasterbar from 'calypso/layout/masterbar/oauth-client';
+import UniversalNavbarHeader from 'calypso/layout/universal-navbar-header';
 import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
 import { isWpMobileApp } from 'calypso/lib/mobile-app';
 import { isCrowdsignalOAuth2Client, isWooOAuth2Client } from 'calypso/lib/oauth2-clients';
@@ -97,7 +98,7 @@ const LayoutLoggedOut = ( {
 	} else if ( config.isEnabled( 'jetpack-cloud' ) || isWpMobileApp() || isJetpackThankYou ) {
 		masterbar = null;
 	} else if ( sectionName === 'plugins' ) {
-		masterbar = <AsyncLoad require="calypso/layout/universal-navbar-header" placeholder={ null } />;
+		masterbar = <UniversalNavbarHeader />;
 	} else {
 		masterbar = (
 			<MasterbarLoggedOut
@@ -111,9 +112,6 @@ const LayoutLoggedOut = ( {
 	}
 
 	const bodyClass = [ 'font-smoothing-antialiased' ];
-	if ( sectionName === 'plugins' ) {
-		bodyClass.push( 'plugins-listing' );
-	}
 
 	return (
 		<div className={ classNames( 'layout', classes ) }>
