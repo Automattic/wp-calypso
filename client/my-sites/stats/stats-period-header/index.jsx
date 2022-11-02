12ace47e758e40c6a4630d7b5ddd29afbc109e8a
Dognose <dognose24@gmail.com>
2022-11-02 10:59:13 +0800

    Refactor StatsPeriodHeader to function component

--- client/my-sites/stats/stats-period-header/index.jsx
+++ client/my-sites/stats/stats-period-header/index.jsx
@@ -1,21 +1,7 @@
-import PropTypes from 'prop-types';
-import { PureComponent } from 'react';
 import './style.scss';
 
-class StatsPeriodHeader extends PureComponent {
-	static propTypes = {
-		children: PropTypes.array,
-	};
-
-	static defaultProps = {
-		children: [],
-	};
-
-	render() {
-		const { children } = this.props;
-
-		return <div className="stats__period-header">{ children }</div>;
-	}
-}
+const StatsPeriodHeader = ( { children } ) => {
+	return <div className="stats__period-header">{ children }</div>;
+};
 
 export default StatsPeriodHeader;
