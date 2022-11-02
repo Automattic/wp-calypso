c252e4b8b3f2b86dda9aac7d12ce1a9e5e878197
Emanuele Buccelli <escapemanuele@gmail.com>
2022-11-02 15:36:15 +0100

    Fix some types

--- packages/data-stores/src/onboard/reducer.ts
+++ packages/data-stores/src/onboard/reducer.ts
@@ -1,9 +1,8 @@
 import { combineReducers } from '@wordpress/data';
 import { SiteGoal } from './constants';
-import { DomainForm } from './types';
-import type { DomainSuggestion } from '../domain-suggestions/types';
 import type { FeatureId } from '../wpcom-features/types';
 import type { OnboardAction } from './actions';
+import type { DomainSuggestion, DomainForm } from './types';
 // somewhat hacky, but resolves the circular dependency issue
 import type { Design, FontPair, StyleVariation } from '@automattic/design-picker/src/types';
 import type { MinimalRequestCartProduct } from '@automattic/shopping-cart';
