2c320b9b49867116b0dd6d2f3a94638e66e4cd2b
“Chris <xristos3490@gmail.com>
2022-11-03 11:23:49 +0200

    Remove sparkline from ecommerce

--- client/my-sites/sidebar/sparkline.js
+++ client/my-sites/sidebar/sparkline.js
@@ -1,13 +1,16 @@
+import { isEcommercePlan } from '@automattic/calypso-products/src';
 import { memo } from 'react';
 import { useSelector } from 'react-redux';
 import StatsSparkline from 'calypso/blocks/stats-sparkline';
+import { getSitePlanSlug } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 export const MySitesSidebarUnifiedStatsSparkline = ( { slug } ) => {
 	const selectedSiteId = useSelector( getSelectedSiteId );
+	const sitePlanSlug = useSelector( ( state ) => getSitePlanSlug( state, selectedSiteId ) );
 	const isStats = typeof slug === 'string' && slug.includes( '-comstats' );
 
-	if ( ! isStats || ! selectedSiteId ) {
+	if ( ! isStats || ! selectedSiteId || isEcommercePlan( sitePlanSlug ) ) {
 		return null;
 	}
 
