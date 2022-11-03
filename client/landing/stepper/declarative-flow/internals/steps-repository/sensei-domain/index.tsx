905a1ff9d1620a4c01a9245b157379338f6d4e59
Mikey Arce <mikeyarce@gmail.com>
2022-11-03 11:36:56 -0700

    Update and style sensei domain select step

--- client/landing/stepper/declarative-flow/internals/steps-repository/sensei-domain/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/sensei-domain/index.tsx
@@ -1,14 +1,20 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 import { SenseiStepContainer } from '@automattic/onboarding';
 import { useSelect, useDispatch } from '@wordpress/data';
+import { useI18n } from '@wordpress/react-i18n';
 import RegisterDomainStep from 'calypso/components/domains/register-domain-step';
+import ReskinSideExplainer from 'calypso/components/domains/reskin-side-explainer';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { ONBOARD_STORE, PRODUCTS_LIST_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 import type { Step } from '../../types';
 
+import './style.scss';
+
 const SenseiDomain: Step = ( { navigation } ) => {
 	const { submit } = navigation;
+	const { __ } = useI18n();
 	const [ siteTitle, domain, productsList ] = useSelect( ( select ) => {
 		return [
 			select( ONBOARD_STORE ).getSelectedSiteTitle(),
@@ -34,18 +40,47 @@ const SenseiDomain: Step = ( { navigation } ) => {
 	return (
 		<SenseiStepContainer stepName="senseiDomain" recordTracksEvent={ recordTracksEvent }>
 			<CalypsoShoppingCartProvider>
-				<RegisterDomainStep
-					suggestion={ domainSuggestion }
-					domainsWithPlansOnly
-					isSignupStep
-					basePath=""
-					includeWordPressDotCom
-					onAddDomain={ onAddDomain }
-					onSkip={ onSkip }
-					products={ productsList }
-					useProvidedProductsList
-					showSkipButton
+				<FormattedHeader
+					id="choose-a-domain-header"
+					headerText="Choose a domain"
+					subHeaderText={
+						<>
+							{ __( 'Make your course site shine with a custom domain. Not sure yet ?' ) }
+							<button
+								className="button navigation-link step-container__navigation-link has-underline is-borderless"
+								onClick={ onSkip }
+							>
+								{ __( 'Decide later.' ) }
+							</button>
+						</>
+					}
+					align="center"
 				/>
+				<div className="container domains__step-content domains__step-content-domain-step">
+					<RegisterDomainStep
+						// vendor={ SENSEI_FLOW }
+						key="domainForm"
+						suggestion={ domainSuggestion }
+						domainsWithPlansOnly
+						isSignupStep={ true }
+						includeWordPressDotCom
+						onAddDomain={ onAddDomain }
+						onSkip={ onSkip }
+						products={ productsList }
+						useProvidedProductsList
+						showSkipButton
+						align="left"
+						isWideLayout={ true }
+					/>
+					<div className="domains__domain-side-content-container">
+						<div className="domains__domain-side-content domains__free-domain">
+							<ReskinSideExplainer onClick={ onSkip } type="free-domain-explainer" />
+						</div>
+						<div className="domains__domain-side-content">
+							<ReskinSideExplainer onClick={ onSkip } type="use-your-domain" />
+						</div>
+					</div>
+				</div>
 			</CalypsoShoppingCartProvider>
 		</SenseiStepContainer>
 	);
