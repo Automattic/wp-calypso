533b052200514d35124b682459e71ddb93d1a438
Dean Sas <dean.sas@automattic.com>
2022-11-04 09:43:28 +0000

    Support 'disassociated' retired themes (#69702)
    For some themes we have taken over maintenance and are renaming them to
    make it clear they're no longer related to the original authors. This
    change informs users what has happened.
    
    See pdgAPm-rI-p2#comment-576 for discussion.

--- client/my-sites/theme/main.jsx
+++ client/my-sites/theme/main.jsx
@@ -147,6 +147,10 @@ class ThemeSheet extends Component {
 		return !! this.props.screenshots;
 	};
 
+	// If a theme has been removed by a theme shop, then the theme will still exist and a8c will take over any support responsibilities.
+	isRemoved = () =>
+		!! this.props.taxonomies?.theme_status?.find( ( status ) => status.slug === 'removed' );
+
 	onButtonClick = () => {
 		const { defaultOption, id } = this.props;
 		defaultOption.action && defaultOption.action( id );
@@ -609,6 +613,15 @@ class ThemeSheet extends Component {
 					</Button>
 				</Card>
 
+				{ this.isRemoved() && (
+					<Card>
+						<p>
+							{ this.props.translate(
+								'This theme has been renamed to reflect that support for it is now provided directly by WordPress.com. The theme will continue to work as before.'
+							) }
+						</p>
+					</Card>
+				) }
 				<div className="theme__sheet-footer-line">
 					<Gridicon icon="my-sites" />
 				</div>
@@ -792,7 +805,12 @@ class ThemeSheet extends Component {
 			} );
 		}
 
+		const isRemoved = this.isRemoved();
+
 		const className = classNames( 'theme__sheet', { 'is-with-upsell-banner': hasUpsellBanner } );
+		const columnsClassName = classNames( 'theme__sheet-columns', {
+			'is-removed': isRemoved,
+		} );
 
 		return (
 			<Main className={ className }>
@@ -822,12 +840,14 @@ class ThemeSheet extends Component {
 				>
 					{ ! retired && ! hasWpOrgThemeUpsellBanner && ! isWPForTeamsSite && this.renderButton() }
 				</HeaderCake>
-				<div className="theme__sheet-columns">
+				<div className={ columnsClassName }>
 					<div className="theme__sheet-column-left">
 						{ ! retired && this.renderSectionContent( section ) }
 						{ retired && this.renderRetired() }
 					</div>
-					<div className="theme__sheet-column-right">{ this.renderScreenshot() }</div>
+					{ ! isRemoved && (
+						<div className="theme__sheet-column-right">{ this.renderScreenshot() }</div>
+					) }
 				</div>
 				<ThemePreview belowToolbar={ previewUpsellBanner } />
 				<PerformanceTrackerStop />
