4d3939c6e99c0c7845e6ac24df2d85ac7f5cec12
Jeremy Yip <jeremy.yip@automattic.com>
2022-11-04 16:13:21 -0700

    Add preliminary e2e tracks event specs

---
+++ test/e2e/specs/editor-tracking/editor-tracking__document-actions-events.ts
@@ -0,0 +1,176 @@
+/**
+ * @group editor-tracking
+ */
+
+import {
+	DataHelper,
+	envVariables,
+	getTestAccountByFeature,
+	envToFeatureKey,
+	TestAccount,
+	EditorTracksEventManager,
+	FullSiteEditorPage,
+} from '@automattic/calypso-e2e';
+import { Browser, Page } from 'playwright';
+
+declare const browser: Browser;
+
+describe(
+	DataHelper.createSuiteTitle( 'Editor tracking: Site editor document actions events' ),
+	function () {
+		const features = envToFeatureKey( envVariables );
+		const accountName = getTestAccountByFeature( { ...features, variant: 'siteEditor' } );
+
+		describe( 'wpcom_site_editor_document_actions_dropdown_open', function () {
+			let page: Page;
+			let testAccount: TestAccount;
+			let fullSiteEditorPage: FullSiteEditorPage;
+			let editorTracksEventManager: EditorTracksEventManager;
+
+			beforeAll( async () => {
+				page = await browser.newPage();
+
+				testAccount = new TestAccount( accountName );
+				await testAccount.authenticate( page );
+
+				editorTracksEventManager = new EditorTracksEventManager( page );
+				fullSiteEditorPage = new FullSiteEditorPage( page, { target: features.siteType } );
+			} );
+
+			it( 'Visit the site editor', async function () {
+				await fullSiteEditorPage.visit( testAccount.getSiteURL( { protocol: false } ) );
+				await fullSiteEditorPage.prepareForInteraction( { leaveWithoutSaving: true } );
+			} );
+
+			it( 'Open the document actions dropdown', async function () {
+				await fullSiteEditorPage.openDocumentActionsDropdown();
+			} );
+
+			it( '"wpcom_site_editor_document_actions_dropdown_open" event fires', async function () {
+				const eventDidFire = await editorTracksEventManager.didEventFire(
+					'wpcom_site_editor_document_actions_dropdown_open'
+				);
+				expect( eventDidFire ).toBe( true );
+			} );
+		} );
+
+		describe( 'wpcom_site_editor_document_actions_template_area_click header', function () {
+			let page: Page;
+			let testAccount: TestAccount;
+			let fullSiteEditorPage: FullSiteEditorPage;
+			let editorTracksEventManager: EditorTracksEventManager;
+
+			beforeAll( async () => {
+				page = await browser.newPage();
+
+				testAccount = new TestAccount( accountName );
+				await testAccount.authenticate( page );
+				editorTracksEventManager = new EditorTracksEventManager( page );
+				fullSiteEditorPage = new FullSiteEditorPage( page, { target: features.siteType } );
+			} );
+
+			it( 'Visit the site editor', async function () {
+				await fullSiteEditorPage.visit( testAccount.getSiteURL( { protocol: false } ) );
+				await fullSiteEditorPage.prepareForInteraction( { leaveWithoutSaving: true } );
+			} );
+
+			it( 'Open the document actions dropdown', async function () {
+				await fullSiteEditorPage.openDocumentActionsDropdown();
+			} );
+
+			it( 'Click the Header template area button in the document actions dropdown', async function () {
+				await fullSiteEditorPage.clickDocumentActionsDropdownItem(
+					'.edit-site-template-details__template-areas-item span:text("Header")'
+				);
+			} );
+
+			it( '"wpcom_site_editor_document_actions_template_area_click" event fires', async function () {
+				const eventDidFire = await editorTracksEventManager.didEventFire(
+					'wpcom_site_editor_document_actions_template_area_click',
+					{
+						matchingProperties: {
+							template_area: 'header',
+						},
+					}
+				);
+				expect( eventDidFire ).toBe( true );
+			} );
+		} );
+
+		// describe( 'wpcom_site_editor_document_actions_revert_click', function () {
+		// 	let page: Page;
+		// 	let testAccount: TestAccount;
+		// 	let fullSiteEditorPage: FullSiteEditorPage;
+		// 	let editorTracksEventManager: EditorTracksEventManager;
+
+		// 	beforeAll( async () => {
+		// 		page = await browser.newPage();
+
+		// 		testAccount = new TestAccount( accountName );
+		// 		await testAccount.authenticate( page );
+		// 		editorTracksEventManager = new EditorTracksEventManager( page );
+		// 		fullSiteEditorPage = new FullSiteEditorPage( page, { target: features.siteType } );
+		// 	} );
+
+		// 	it( 'Visit the site editor', async function () {
+		// 		await fullSiteEditorPage.visit( testAccount.getSiteURL( { protocol: false } ) );
+		// 		await fullSiteEditorPage.prepareForInteraction( { leaveWithoutSaving: true } );
+		// 	} );
+
+		// 	it( 'Open the document actions dropdown', async function () {
+		// 		await fullSiteEditorPage.openDocumentActionsDropdown();
+		// 	} );
+
+		// 	it( 'Click the template area clear customizations button in the document actions dropdown', async function () {
+		// 		await fullSiteEditorPage.clickDocumentActionsDropdownItem(
+		// 			'.edit-site-template-details__revert'
+		// 		);
+		// 	} );
+
+		// 	it( '"wpcom_site_editor_document_actions_revert_click" event fires', async function () {
+		// 		const eventDidFire = await editorTracksEventManager.didEventFire(
+		// 			'wpcom_site_editor_document_actions_revert_click'
+		// 		);
+		// 		expect( eventDidFire ).toBe( true );
+		// 	} );
+		// } );
+
+		describe( 'wpcom_site_editor_document_actions_show_all_click', function () {
+			let page: Page;
+			let testAccount: TestAccount;
+			let fullSiteEditorPage: FullSiteEditorPage;
+			let editorTracksEventManager: EditorTracksEventManager;
+
+			beforeAll( async () => {
+				page = await browser.newPage();
+
+				testAccount = new TestAccount( accountName );
+				await testAccount.authenticate( page );
+				editorTracksEventManager = new EditorTracksEventManager( page );
+				fullSiteEditorPage = new FullSiteEditorPage( page, { target: features.siteType } );
+			} );
+
+			it( 'Visit the site editor', async function () {
+				await fullSiteEditorPage.visit( testAccount.getSiteURL( { protocol: false } ) );
+				await fullSiteEditorPage.prepareForInteraction( { leaveWithoutSaving: true } );
+			} );
+
+			it( 'Open the document actions dropdown', async function () {
+				await fullSiteEditorPage.openDocumentActionsDropdown();
+			} );
+
+			it( 'Click the template area browse all button in the document actions dropdown', async function () {
+				await fullSiteEditorPage.clickDocumentActionsDropdownItem(
+					'.edit-site-template-details__show-all-button'
+				);
+			} );
+
+			it( '"wpcom_site_editor_document_actions_show_all_click" event fires', async function () {
+				const eventDidFire = await editorTracksEventManager.didEventFire(
+					'wpcom_site_editor_document_actions_show_all_click'
+				);
+				expect( eventDidFire ).toBe( true );
+			} );
+		} );
+	}
+);
