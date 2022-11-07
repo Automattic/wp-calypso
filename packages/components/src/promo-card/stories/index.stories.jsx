dd9ff7d993318e7d1a61d3d955d32a035615a64c
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2022-11-07 18:05:01 +0800

    First pass at PromoCard component.

---
+++ packages/components/src/promo-card/stories/index.stories.jsx
@@ -0,0 +1,26 @@
+import PromoCard from '../';
+
+export default {
+	component: PromoCard,
+	title: 'Promo Card',
+};
+
+const Template = ( args ) => <PromoCard { ...args } />;
+
+export const Default = Template.bind( {} );
+Default.args = {
+	data: {
+		title: 'Bring your stats with you using the Jetpack mobile app',
+		message: 'Visit wp.com/app or scan the QR code to download the Jetpack mobile app.',
+	},
+};
+
+export const Mobile = Template.bind( {} );
+Mobile.args = {
+	data: {
+		...Default.args.data,
+		message:
+			'Check your stats on-the-go and get real-time notifications with the Jetpack mobile app.',
+		cta: 'app-store',
+	},
+};
