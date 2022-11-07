a6517405a9f5972a3bfac05de21324a86de48b96
Dehan <dehandecroos@gmail.com>
2022-11-07 13:38:15 +0400

    remove calls to use experiment

--- client/landing/stepper/declarative-flow/internals/steps-repository/goals/goals.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/goals/goals.tsx
@@ -1,36 +1,53 @@
 import { Onboard } from '@automattic/data-stores';
 import { useIsEnglishLocale } from '@automattic/i18n-utils';
 import { useTranslate } from 'i18n-calypso';
-import { useExperiment } from 'calypso/lib/explat';
 import type { Goal } from './types';
-
-export const CALYPSO_BUILTBYEXPRESS_GOAL_TEXT_EXPERIMENT_NAME =
-	'calypso_builtbyexpress_goal_copy_change_202210';
-export const VARIATION_CONTROL = 'control';
-export const VARIATION_BUY = 'variation_buy';
-export const VARIATION_GET = 'variation_get';
 
 const SiteGoal = Onboard.SiteGoal;
 const HIDE_GOALS = [ SiteGoal.DIFM, SiteGoal.Import ];
 const shouldDisplayGoal = ( { key }: Goal ) => ! HIDE_GOALS.includes( key );
 
+// export const CALYPSO_BUILTBYEXPRESS_GOAL_TEXT_EXPERIMENT_NAME =
+// 	'calypso_builtbyexpress_goal_copy_change_202210';
+// export const VARIATION_CONTROL = 'control';
+// export const VARIATION_BUY = 'variation_buy';
+// export const VARIATION_GET = 'variation_get';
+
+const useBBEGoal = () => {
+	const translate = useTranslate();
+
+	// ************************************************************************
+	// ****  Experiment skeleton left in for future BBE copy change tests  ****
+	// ************************************************************************
+	//
+
+	// const [ , experimentAssignment ] = useExperiment(
+	// 	CALYPSO_BUILTBYEXPRESS_GOAL_TEXT_EXPERIMENT_NAME
+	// );
+	// const variationName = experimentAssignment?.variationName;
+
+	// let builtByExpressGoalDisplayText;
+	// switch ( variationName ) {
+	// 	case VARIATION_BUY:
+	// 		builtByExpressGoalDisplayText = translate( 'Buy a website' );
+	// 		break;
+	// 	case VARIATION_GET:
+	// 		builtByExpressGoalDisplayText = translate( 'Get a website quickly' );
+	// 		break;
+	// 	case VARIATION_CONTROL:
+	// 	default:
+	// 		builtByExpressGoalDisplayText = translate( 'Hire a professional to design my website' );
+	// }
+	//
+	// ************************************************************************
+
+	return translate( 'Get a website quickly' );
+};
+
 export const useGoals = ( displayAllGoals = false ): Goal[] => {
 	const translate = useTranslate();
 	const isEnglishLocale = useIsEnglishLocale();
-	const [ , experimentAssignment ] = useExperiment(
-		CALYPSO_BUILTBYEXPRESS_GOAL_TEXT_EXPERIMENT_NAME
-	);
-	const variationName = experimentAssignment?.variationName;
-
-	let builtByExpressGoalDisplayText;
-	switch ( variationName ) {
-		// Experiment skeleton left in for future copy change tests
-		case VARIATION_BUY:
-		case VARIATION_GET:
-		case VARIATION_CONTROL:
-		default:
-			builtByExpressGoalDisplayText = translate( 'Get a website quickly' );
-	}
+	const builtByExpressGoalDisplayText = useBBEGoal();
 
 	const goals = [
 		{
--- client/landing/stepper/declarative-flow/internals/steps-repository/goals/select-goals.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/goals/select-goals.tsx
@@ -2,9 +2,8 @@ import { Button } from '@automattic/components';
 import { Onboard } from '@automattic/data-stores';
 import styled from '@emotion/styled';
 import { useTranslate } from 'i18n-calypso';
-import { useExperiment } from 'calypso/lib/explat';
 import DIFMLink from './difm-link';
-import { CALYPSO_BUILTBYEXPRESS_GOAL_TEXT_EXPERIMENT_NAME, useGoals } from './goals';
+import { useGoals } from './goals';
 import ImportLink from './import-link';
 import SelectCard from './select-card';
 
@@ -45,11 +44,17 @@ export const SelectGoals = ( {
 }: SelectGoalsProps ) => {
 	const translate = useTranslate();
 	const goalOptions = useGoals( displayAllGoals );
-	let [ isBuiltByExpressExperimentLoading ] = useExperiment(
-		CALYPSO_BUILTBYEXPRESS_GOAL_TEXT_EXPERIMENT_NAME
-	);
-	// Experiment is currently inactive and will be run again in the future
-	isBuiltByExpressExperimentLoading = false;
+
+	// *******************************************************************************
+	// ****  Experiment skeleton left in for future BBE (Goal) copy change tests  ****
+	// *******************************************************************************
+	//
+	// let [ isBuiltByExpressExperimentLoading ] = useExperiment(
+	// 	CALYPSO_BUILTBYEXPRESS_GOAL_TEXT_EXPERIMENT_NAME
+	// );
+	//
+	// *******************************************************************************
+	const isBuiltByExpressExperimentLoading = false;
 
 	const addGoal = ( goal: Onboard.SiteGoal ) => {
 		const goalSet = new Set( selectedGoals );
