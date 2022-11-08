d0e18e3c77dbe211c79899536d19184b93024060
Jasper Kang <kangzjnet@gmail.com>
2022-11-08 13:31:41 +1300

    no need to hide masterbar anymore

--- apps/stats/src/app.jsx
+++ apps/stats/src/app.jsx
@@ -12,7 +12,6 @@ import wpcomApiMiddleware from 'calypso/state/data-layer/wpcom-api-middleware';
 import { setStore } from 'calypso/state/redux-store';
 import sites from 'calypso/state/sites/reducer';
 import { setLocale as setLocaleAction } from 'calypso/state/ui/language/actions';
-import { hideMasterbar as hideMasterbarAction } from 'calypso/state/ui/masterbar-visibility/actions';
 import { combineReducers, addReducerEnhancer } from 'calypso/state/utils';
 import { setupContextMiddleware } from './page-middleware/setup-context';
 import registerStatsPages from './routes';
@@ -23,10 +22,6 @@ const setLocale = ( dispatch ) => {
 	const defaultLocale = config( 'i18n_default_locale_slug' ) || 'en';
 	const siteLocale = config( 'i18n_locale_slug' );
 	dispatch( setLocaleAction( siteLocale ? siteLocale : defaultLocale ) );
-};
-
-const hideMasterbar = ( dispatch ) => {
-	dispatch( hideMasterbarAction() );
 };
 
 async function AppBoot() {
@@ -51,7 +46,6 @@ async function AppBoot() {
 
 	setStore( store );
 	setLocale( store.dispatch );
-	hideMasterbar( store.dispatch );
 	setupContextMiddleware( store, queryClient );
 
 	if ( ! window.location?.hash ) {
