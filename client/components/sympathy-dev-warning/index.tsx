295f682c1aa2c8959b7f6f8943d9514ddba38159
Dehan <dehandecroos@gmail.com>
2022-11-08 21:09:07 +0400

    Remove unnecessary state

--- client/components/sympathy-dev-warning/index.tsx
+++ client/components/sympathy-dev-warning/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getStoredItem, setStoredItem } from 'calypso/lib/browser-storage';
 import { IS_STATE_RANDOMLY_CLEARED_KEY } from 'calypso/state/initial-state';
@@ -6,40 +6,30 @@ import { warningNotice } from 'calypso/state/notices/actions';
 
 export function SympathyDevWarning() {
 	const dispatch = useDispatch();
-	const [ isStateRandomlyCleared, setIsStateRandomlyCleared ] = useState( false );
 
-	const loadIsClearedState = async () => {
-		const loadedState = await getStoredItem< boolean >( IS_STATE_RANDOMLY_CLEARED_KEY );
-		setIsStateRandomlyCleared( loadedState ?? false );
-	};
-
-	const notifyDeveloperAndCleanup = async () => {
-		setTimeout( () => {
-			// Read more about this (sympathy) :  https://github.com/Automattic/wp-calypso/pull/14121, p4TIVU-6Ed-p2
-			// TLDR: We try to clear state randomly to match closely to the UX of our user i.e. Sympathy.
-			// In other words, you cannot rely on user's persisted state to be reliable so we randomly clear it to simulate that experience.
-			dispatch(
-				warningNotice( 'DEV NOTICE: Persisted redux state was randomly cleared', {
-					id: 'Sympathy-Dev-Warning',
-					duration: 8000,
-					showDismiss: true,
-					displayOnNextPage: true,
-				} )
-			);
-		}, 1200 );
-		await setStoredItem( IS_STATE_RANDOMLY_CLEARED_KEY, false );
-		setIsStateRandomlyCleared( false );
-	};
-
-	useEffect( () => {
-		loadIsClearedState();
-	}, [] );
-
-	useEffect( () => {
+	const maybeNotifyDeveloperAndCleanup = async () => {
+		const isStateRandomlyCleared = await getStoredItem< boolean >( IS_STATE_RANDOMLY_CLEARED_KEY );
 		if ( isStateRandomlyCleared ) {
-			notifyDeveloperAndCleanup();
+			setTimeout( () => {
+				// Read more about this (sympathy) :  https://github.com/Automattic/wp-calypso/pull/14121, p4TIVU-6Ed-p2
+				// TLDR: We try to clear state randomly to match closely to the UX of our user i.e. Sympathy.
+				// In other words, you cannot rely on user's persisted state to be reliable so we randomly clear it to simulate that experience.
+				dispatch(
+					warningNotice( 'DEV NOTICE: Persisted redux state was randomly cleared', {
+						id: 'Sympathy-Dev-Warning',
+						duration: 8000,
+						showDismiss: true,
+						displayOnNextPage: true,
+					} )
+				);
+			}, 1200 );
+			await setStoredItem( IS_STATE_RANDOMLY_CLEARED_KEY, false );
 		}
-	}, [ isStateRandomlyCleared, dispatch, notifyDeveloperAndCleanup ] );
+	};
+
+	useEffect( () => {
+		maybeNotifyDeveloperAndCleanup();
+	}, [ maybeNotifyDeveloperAndCleanup ] );
 
 	return null;
 }
