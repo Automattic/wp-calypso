1a773669d991e818d7eec3b7efc6f3286f5005e6
Louis Laugesen <louis.laugesen@gmail.com>
2022-11-09 13:10:14 +1100

    Update client/my-sites/checkout/get-thank-you-page-url/index.ts
    Co-authored-by: Payton Swick <payton@foolord.com>

--- client/my-sites/checkout/get-thank-you-page-url/index.ts
+++ client/my-sites/checkout/get-thank-you-page-url/index.ts
@@ -126,7 +126,7 @@ export default function getThankYouPageUrl( {
 	jetpackTemporarySiteId,
 	adminPageRedirect,
 	domains,
-	isGiftPurchase = false,
+	isGiftPurchase,
 }: PostCheckoutUrlArguments ): string {
 	debug( 'starting getThankYouPageUrl' );
 
