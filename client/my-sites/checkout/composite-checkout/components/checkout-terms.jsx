4ed570db400170f3dab7d040375eae8ba904bd1b
Chris Diehl <jjchrisdiehl@gmail.com>
2022-11-08 16:44:35 -0800

    Add gift ToS conditional rendering

--- client/my-sites/checkout/composite-checkout/components/checkout-terms.jsx
+++ client/my-sites/checkout/composite-checkout/components/checkout-terms.jsx
@@ -1,5 +1,5 @@
 import { localize } from 'i18n-calypso';
-import { Component, Fragment } from 'react';
+import { Fragment } from 'react';
 import { hasRenewableSubscription } from 'calypso/lib/cart-values/cart-items';
 import AdditionalTermsOfServiceInCart from './additional-terms-of-service-in-cart';
 import BundledDomainNotice from './bundled-domain-notice';
@@ -7,6 +7,7 @@ import ConciergeRefundPolicy from './concierge-refund-policy';
 import DomainRegistrationAgreement from './domain-registration-agreement';
 import DomainRegistrationHsts from './domain-registration-hsts';
 import { EbanxTermsOfService } from './ebanx-terms-of-service';
+import { GiftingTermsOfService } from './gifting-terms-of-service';
 import { InternationalFeeNotice } from './international-fee-notice';
 import RefundPolicies from './refund-policies';
 import TermsOfService from './terms-of-service';
@@ -15,19 +16,12 @@ import TitanTermsOfService from './titan-terms-of-service';
 
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 
-class CheckoutTerms extends Component {
-	render() {
-		const { cart } = this.props;
+const CheckoutTerms = ( { translate, cart } ) => {
+	const renderGiftToS = () => <GiftingTermsOfService />;
+
+	const renderDefaultToS = () => {
 		return (
-			<Fragment>
-				<div className="checkout__terms" id="checkout-terms">
-					<strong>
-						{ this.props.translate( 'By checking out:', {
-							comment:
-								'Headline before a list of terms the customer is agreeing to on checkout. Screenshot: https://user-images.githubusercontent.com/1379730/55166390-66c09080-5145-11e9-9c13-6c1b3b693786.png',
-						} ) }
-					</strong>
-				</div>
+			<>
 				<TermsOfService hasRenewableSubscription={ hasRenewableSubscription( cart ) } />
 				<DomainRegistrationAgreement cart={ cart } />
 				<DomainRegistrationHsts cart={ cart } />
@@ -39,9 +33,22 @@ class CheckoutTerms extends Component {
 				<EbanxTermsOfService />
 				<InternationalFeeNotice />
 				<AdditionalTermsOfServiceInCart />
-			</Fragment>
+			</>
 		);
-	}
-}
+	};
+	return (
+		<Fragment>
+			<div className="checkout__terms" id="checkout-terms">
+				<strong>
+					{ translate( 'By checking out:', {
+						comment:
+							'Headline before a list of terms the customer is agreeing to on checkout. Screenshot: https://user-images.githubusercontent.com/1379730/55166390-66c09080-5145-11e9-9c13-6c1b3b693786.png',
+					} ) }
+				</strong>
+			</div>
+			{ cart.gift_details ? renderGiftToS() : renderDefaultToS() }
+		</Fragment>
+	);
+};
 
 export default localize( CheckoutTerms );
