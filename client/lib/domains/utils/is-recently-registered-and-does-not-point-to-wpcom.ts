549b7e66655aef984db1f903f1fd938f7fc47383
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2022-11-09 14:17:03 -0700

    Fix isRecentlyRegisteredAndDoesNotPointToWpcom function and add comments

--- client/lib/domains/utils/is-recently-registered-and-does-not-point-to-wpcom.ts
+++ client/lib/domains/utils/is-recently-registered-and-does-not-point-to-wpcom.ts
@@ -1,6 +1,14 @@
 import { isRecentlyRegistered } from 'calypso/lib/domains/utils/is-recently-registered';
 import type { ResponseDomain } from 'calypso/lib/domains/types';
 
+/**
+ * Checks if the domain was registered recently (less than 24 hours ago) and does not
+ * point to WPCOM.
+ *
+ * @param domain
+ * @returns boolean
+ */
 export function isRecentlyRegisteredAndDoesNotPointToWpcom( domain: ResponseDomain ): boolean {
-	return ! isRecentlyRegistered( domain.registrationDate ) && ! domain.pointsToWpcom;
+	const DAY_IN_MINUTES = 24 * 60;
+	return isRecentlyRegistered( domain.registrationDate, DAY_IN_MINUTES ) && ! domain.pointsToWpcom;
 }
