4a7310f106ee845b412ced51021bb3dd29e04947
Valter Lorran <valterlorran@hotmail.com>
2022-11-09 10:58:56 -0300

    Removing hardcoded product slugs for marketplace themes

--- packages/calypso-products/src/constants/marketplace-themes.ts
+++
@@ -1,3 +0,0 @@
-export const MARKETPLACE_THEME_TSUBAKI_TEST_YEARLY_SLUG = 'wp_mp_theme_tsubaki_test_yearly';
-
-export const MARKETPLACE_THEME_SLUGS = <const>[ MARKETPLACE_THEME_TSUBAKI_TEST_YEARLY_SLUG ];
