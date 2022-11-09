c5cdae60299fe5b74aaf4f0b2e395aa32f898628
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2022-11-09 15:14:25 -0700

    Prevent domains that do not point here from being set as primary only for 30 minutes

--- client/lib/domains/utils/is-recently-registered-and-does-not-point-to-wpcom.ts
+++ client/lib/domains/utils/is-recently-registered-and-does-not-point-to-wpcom.ts
@@ -2,13 +2,11 @@ import { isRecentlyRegistered } from 'calypso/lib/domains/utils/is-recently-regi
 import type { ResponseDomain } from 'calypso/lib/domains/types';
 
 /**
- * Checks if the domain was registered recently (less than 24 hours ago) and does not
- * point to WPCOM.
+ * Checks if the domain was registered recently and does not point to WPCOM.
  *
  * @param domain
  * @returns boolean
  */
 export function isRecentlyRegisteredAndDoesNotPointToWpcom( domain: ResponseDomain ): boolean {
-	const DAY_IN_MINUTES = 24 * 60;
-	return isRecentlyRegistered( domain.registrationDate, DAY_IN_MINUTES ) && ! domain.pointsToWpcom;
+	return isRecentlyRegistered( domain.registrationDate ) && ! domain.pointsToWpcom;
 }
