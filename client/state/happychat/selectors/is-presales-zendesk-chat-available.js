2b85136226407169800783fb05096a7afca6b6de
Yoav Farhi <yoav@farhi.org>
2022-11-09 09:39:48 +0200

    add selector for zendesk chat availablity + test

---
+++ client/state/happychat/selectors/is-presales-zendesk-chat-available.js
@@ -0,0 +1,13 @@
+import { get } from 'lodash';
+
+import 'calypso/state/happychat/init';
+
+/**
+ * Returns if presales zendesk chat is available.
+ *
+ * @param   {object}  state  Global state tree
+ * @returns {boolean}        true, when presales_zendesk is available
+ */
+export default function isPresalesZendeskChatAvailable( state ) {
+	return get( state, 'happychat.user.availability.presale_zendesk', false );
+}
