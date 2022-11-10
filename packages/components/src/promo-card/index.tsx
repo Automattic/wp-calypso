34b8d0d77020f89e430ffee4d4830fc3699cb00e
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2022-11-10 13:44:28 +0800

    Encapsulate the title & message values.
    This allows us to localize the string and apply the necessary links.

--- packages/components/src/promo-card/index.tsx
+++ packages/components/src/promo-card/index.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { useTranslate } from 'i18n-calypso';
 import './style.scss';
 import { AppStoreSVG, JetpackSVG, QRCodeSVG, WordPressSVG } from './svg-icons';
 
@@ -12,7 +13,33 @@ export type PromoCardProps = {
 };
 
 export default function PromoCard( { className, data }: PromoCardProps ) {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { title, message, isMobile } = data;
+	// Using useTranslate() with interpolation to set up the title/message.
+	// https://wpcalypso.wordpress.com/devdocs/packages/i18n-calypso/README.md
+	const translate = useTranslate();
+	const components = {
+		a: <a href="https://wp.com/apps" />,
+	};
+	const getTitle = () => {
+		return translate( 'Bring your stats with you using the {{a}}Jetpack{{/a}} mobile app', {
+			components,
+		} );
+	};
+	const getMessage = () => {
+		let string;
+		if ( isMobile ) {
+			string = translate(
+				'Check your stats on-the-go and get real-time notifications with the Jetpack mobile app.'
+			);
+		} else {
+			string = translate(
+				'Visit {{a}}wp.com/app{{/a}} or scan the QR code to download the Jetpack mobile app.',
+				{ components }
+			);
+		}
+		return string;
+	};
 	// ToDo: Fix mobile logic.
 	// Should this maybe come via a media query in the CSS?
 	// Also, does it make sense to push one app store over the other?
@@ -24,8 +51,8 @@ export default function PromoCard( { className, data }: PromoCardProps ) {
 					<WordPressSVG />
 					<JetpackSVG className="secondary" />
 				</div>
-				<p className="promo-card__title">{ title }</p>
-				<p className="promo-card__message">{ message }</p>
+				<p className="promo-card__title">{ getTitle() }</p>
+				<p className="promo-card__message">{ getMessage() }</p>
 			</div>
 			<div className="promo-rhs">
 				{ ! isMobile && <QRCodeSVG /> }
