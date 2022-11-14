2c835319b922cf31dc02f1f1f46f117682111cca
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2022-11-14 17:25:10 +0800

    Stats: Updates to PromoCard component. (#69917)
    * Use CSS variables for colours.
    * Add support for `className` property.
    * Replace `cta` prop with `isMobile`.
    * Encapsulate the `title` & `message` values.
    * Update props on PromoCard component.
    * Use appropriate version of Jetpack Green for links.
    * Update SVG icon data.

--- packages/components/src/promo-card/index.tsx
+++ packages/components/src/promo-card/index.tsx
@@ -1,29 +1,55 @@
+import classNames from 'classnames';
+import { useTranslate } from 'i18n-calypso';
 import './style.scss';
-import { AppStoreSVG, JetpackSVG, QRCodeSVG, WordPressSVG } from './svg-icons';
+import { AppStoreSVG, QRCodeSVG, WordPressJetpackSVG } from './svg-icons';
 
 export type PromoCardProps = {
-	data: {
-		title: string | null;
-		message: string | null;
-		cta?: string;
-	};
+	className?: string;
+	isMobile?: boolean;
 };
 
-export default function PromoCard( { data: { title, message, cta } }: PromoCardProps ) {
-	const isAppStore = cta === 'app-store';
+export default function PromoCard( { className, isMobile }: PromoCardProps ) {
+	// Using useTranslate() with interpolation to set up the title/message.
+	// https://wpcalypso.wordpress.com/devdocs/packages/i18n-calypso/README.md
+	const translate = useTranslate();
+	const components = {
+		a: <a href="https://wp.com/apps" />,
+	};
+	const getTitle = () => {
+		return translate( 'Bring your stats with you using the {{a}}Jetpack{{/a}} mobile app', {
+			components,
+		} );
+	};
+	const getMessage = () => {
+		let string;
+		if ( isMobile ) {
+			string = translate(
+				'Check your stats on-the-go and get real-time notifications with the Jetpack mobile app.'
+			);
+		} else {
+			string = translate(
+				'Visit {{a}}wp.com/app{{/a}} or scan the QR code to download the Jetpack mobile app.',
+				{ components }
+			);
+		}
+		return string;
+	};
+	// ToDo: Fix mobile logic.
+	// Should this maybe come via a media query in the CSS?
+	// Also, does it make sense to push one app store over the other?
+	// Might make more sense to have a custom SVG image that goes to wp.com/app.
 	return (
-		<div className="promo-card">
+		<div className={ classNames( 'promo-card', className ?? null ) }>
 			<div className="promo-lhs">
 				<div className="promo-card__icons">
-					<WordPressSVG />
-					<JetpackSVG className="secondary" />
+					<WordPressJetpackSVG />
 				</div>
-				<p className="promo-card__title">{ title }</p>
-				<p className="promo-card__message">{ message }</p>
+				<p className="promo-card__title">{ getTitle() }</p>
+				<p className="promo-card__message">{ getMessage() }</p>
 			</div>
 			<div className="promo-rhs">
-				{ ! isAppStore && <QRCodeSVG /> }
-				{ isAppStore && <AppStoreSVG /> }
+				{ ! isMobile && <QRCodeSVG /> }
+				{ isMobile && <AppStoreSVG /> }
 			</div>
 		</div>
 	);
