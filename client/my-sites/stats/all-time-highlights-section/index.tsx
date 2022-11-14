0a8a2e255dd54708164f97b6418f5903330fc827
Dognose <dognose24@gmail.com>
2022-11-14 23:37:57 +0800

    Refactor with memoization

--- client/my-sites/stats/all-time-highlights-section/index.tsx
+++ client/my-sites/stats/all-time-highlights-section/index.tsx
@@ -2,6 +2,7 @@ import { Card } from '@automattic/components';
 import { Icon, people, postContent, starEmpty, commentContent } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
 import moment from 'moment';
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import QuerySiteStats from 'calypso/components/data/query-site-stats';
 import {
@@ -72,87 +73,93 @@ export default function AllTimeHighlightsSection( { siteId }: { siteId: number }
 	const isStatsLoading = isStatsRequesting && ! views;
 	const isInsightsLoading = isInsightsRequesting && ! percent;
 
-	let bestViewsEverMonthDay;
-	let bestViewsEverYear;
-
-	if ( viewsBestDay && ! isStatsLoading ) {
-		bestViewsEverMonthDay = moment( viewsBestDay ).format( 'MMMM D' );
-		bestViewsEverYear = moment( viewsBestDay ).format( 'YYYY' );
-	}
-
-	const bestViewsEverPercent = Number.isFinite( viewsBestDayTotal )
-		? percentCalculator( viewsBestDayTotal, views )
-		: 0;
-
-	const infoItems = [
-		{
-			id: 'views',
-			icon: (
-				<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path
-						fillRule="evenodd"
-						clipRule="evenodd"
-						d="m4 13 .67.336.003-.005a2.42 2.42 0 0 1 .094-.17c.071-.122.18-.302.329-.52.298-.435.749-1.017 1.359-1.598C7.673 9.883 9.498 8.75 12 8.75s4.326 1.132 5.545 2.293c.61.581 1.061 1.163 1.36 1.599a8.29 8.29 0 0 1 .422.689l.002.005L20 13l.67-.336v-.003l-.003-.005-.008-.015-.028-.052a9.752 9.752 0 0 0-.489-.794 11.6 11.6 0 0 0-1.562-1.838C17.174 8.617 14.998 7.25 12 7.25S6.827 8.618 5.42 9.957c-.702.669-1.22 1.337-1.563 1.839a9.77 9.77 0 0 0-.516.845l-.008.015-.002.005-.001.002v.001L4 13Zm8 3a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Z"
-						fill="#00101C"
-					/>
-				</svg>
-			),
-			title: translate( 'Views' ),
-			count: views,
-		},
-		{ id: 'visitors', icon: people, title: translate( 'Visitors' ), count: visitors },
-		{ id: 'posts', icon: postContent, title: translate( 'Posts' ), count: posts },
-		{ id: 'likes', icon: starEmpty, title: translate( 'Likes' ), count: 13092212, hidden: true },
-		{ id: 'comments', icon: commentContent, title: translate( 'Comments' ), count: comments },
-	];
-
-	const mostPopularTimeItems = {
-		id: 'mostPopularTime',
-		loading: isInsightsLoading,
-		heading: translate( 'Most popular time' ),
-		items: [
-			{
-				id: 'bestDay',
-				header: translate( 'Best day' ),
-				content: day,
-				footer: translate( '%(percent)d%% of views', {
-					args: { percent: percent || 0 },
-					context: 'Stats: Percentage of views',
-				} ),
-			},
-			{
-				id: 'bestHour',
-				header: translate( 'Best hour' ),
-				content: hour,
-				footer: translate( '%(percent)d%% of views', {
-					args: { percent: hourPercent || 0 },
-					context: 'Stats: Percentage of views',
-				} ),
-			},
-		],
-	};
-
-	const bestViewsEverItems = {
-		id: 'bestViewsEver',
-		heading: translate( 'Best views ever' ),
-		items: [
-			{
-				id: 'day',
-				header: translate( 'Day' ),
-				content: bestViewsEverMonthDay,
-				footer: bestViewsEverYear,
-			},
+	const infoItems = useMemo( () => {
+		return [
 			{
 				id: 'views',
-				header: translate( 'Views' ),
-				content: formatNumber( viewsBestDayTotal, true ),
-				footer: translate( '%(percent)d%% of views', {
-					args: { percent: bestViewsEverPercent || 0 },
-					context: 'Stats: Percentage of views',
-				} ),
+				icon: (
+					<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path
+							fillRule="evenodd"
+							clipRule="evenodd"
+							d="m4 13 .67.336.003-.005a2.42 2.42 0 0 1 .094-.17c.071-.122.18-.302.329-.52.298-.435.749-1.017 1.359-1.598C7.673 9.883 9.498 8.75 12 8.75s4.326 1.132 5.545 2.293c.61.581 1.061 1.163 1.36 1.599a8.29 8.29 0 0 1 .422.689l.002.005L20 13l.67-.336v-.003l-.003-.005-.008-.015-.028-.052a9.752 9.752 0 0 0-.489-.794 11.6 11.6 0 0 0-1.562-1.838C17.174 8.617 14.998 7.25 12 7.25S6.827 8.618 5.42 9.957c-.702.669-1.22 1.337-1.563 1.839a9.77 9.77 0 0 0-.516.845l-.008.015-.002.005-.001.002v.001L4 13Zm8 3a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Z"
+							fill="#00101C"
+						/>
+					</svg>
+				),
+				title: translate( 'Views' ),
+				count: views,
 			},
-		],
-	};
+			{ id: 'visitors', icon: people, title: translate( 'Visitors' ), count: visitors },
+			{ id: 'posts', icon: postContent, title: translate( 'Posts' ), count: posts },
+			{ id: 'likes', icon: starEmpty, title: translate( 'Likes' ), count: 13092212, hidden: true },
+			{ id: 'comments', icon: commentContent, title: translate( 'Comments' ), count: comments },
+		];
+	}, [ comments, posts, translate, views, visitors ] );
+
+	const mostPopularTimeItems = useMemo( () => {
+		return {
+			id: 'mostPopularTime',
+			loading: isInsightsLoading,
+			heading: translate( 'Most popular time' ),
+			items: [
+				{
+					id: 'bestDay',
+					header: translate( 'Best day' ),
+					content: day,
+					footer: translate( '%(percent)d%% of views', {
+						args: { percent: percent || 0 },
+						context: 'Stats: Percentage of views',
+					} ),
+				},
+				{
+					id: 'bestHour',
+					header: translate( 'Best hour' ),
+					content: hour,
+					footer: translate( '%(percent)d%% of views', {
+						args: { percent: hourPercent || 0 },
+						context: 'Stats: Percentage of views',
+					} ),
+				},
+			],
+		};
+	}, [ day, hour, hourPercent, isInsightsLoading, percent, translate ] );
+
+	const bestViewsEverItems = useMemo( () => {
+		let bestViewsEverMonthDay = '';
+		let bestViewsEverYear = '';
+
+		if ( viewsBestDay && ! isStatsLoading ) {
+			bestViewsEverMonthDay = moment( viewsBestDay ).format( 'MMMM D' );
+			bestViewsEverYear = moment( viewsBestDay ).format( 'YYYY' );
+		}
+
+		const bestViewsEverPercent = Number.isFinite( viewsBestDayTotal )
+			? percentCalculator( viewsBestDayTotal, views )
+			: 0;
+
+		return {
+			id: 'bestViewsEver',
+			heading: translate( 'Best views ever' ),
+			items: [
+				{
+					id: 'day',
+					header: translate( 'Day' ),
+					content: bestViewsEverMonthDay,
+					footer: bestViewsEverYear,
+				},
+				{
+					id: 'views',
+					header: translate( 'Views' ),
+					content: formatNumber( viewsBestDayTotal, true ),
+					footer: translate( '%(percent)d%% of views', {
+						args: { percent: bestViewsEverPercent || 0 },
+						context: 'Stats: Percentage of views',
+					} ),
+				},
+			],
+		};
+	}, [ isStatsLoading, translate, views, viewsBestDay, viewsBestDayTotal ] );
 
 	return (
 		<div className="stats__all-time-highlights-section">
