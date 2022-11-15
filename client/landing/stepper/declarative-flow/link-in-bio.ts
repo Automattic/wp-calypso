f85f08248ca1fe06f3ff45c305a0b4fdb5b5d811
James Tien <southp0105@gmail.com>
2022-11-15 13:35:34 +0800

    Vary the step order of the Link in Bio flow based on the tld query parameter

--- client/landing/stepper/declarative-flow/link-in-bio.ts
+++ client/landing/stepper/declarative-flow/link-in-bio.ts
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import { recordFullStoryEvent } from 'calypso/lib/analytics/fullstory';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import wpcom from 'calypso/lib/wp';
+import { useQuery } from '../hooks/use-query';
 import { useSiteSlug } from '../hooks/use-site-slug';
 import { USER_STORE, ONBOARD_STORE } from '../stores';
 import { recordSubmitStep } from './internals/analytics/record-submit-step';
@@ -23,14 +24,11 @@ export const linkInBio: Flow = {
 		return [ 'intro', 'linkInBioSetup', 'patterns', 'processing', 'launchpad' ] as StepPath[];
 	},
 
-	useStepNavigation( _currentStep, navigate ) {
+	useSubmit( _currentStep, navigate ) {
 		const flowName = this.name;
-		const { setStepProgress } = useDispatch( ONBOARD_STORE );
-		const flowProgress = useFlowProgress( { stepName: _currentStep, flowName } );
-		setStepProgress( flowProgress );
-		const siteSlug = useSiteSlug();
 		const userIsLoggedIn = useSelect( ( select ) => select( USER_STORE ).isCurrentUserLoggedIn() );
 		const locale = useLocale();
+		const queryParams = useQuery();
 
 		// trigger guides on step movement, we don't care about failures or response
 		wpcom.req.post(
@@ -44,15 +42,17 @@ export const linkInBio: Flow = {
 			}
 		);
 
-		const getStartUrl = () => {
-			return locale && locale !== 'en'
-				? `/start/account/user/${ locale }?variationName=${ flowName }&pageTitle=Link%20in%20Bio&redirect_to=/setup/${ flowName }/patterns`
-				: `/start/account/user?variationName=${ flowName }&pageTitle=Link%20in%20Bio&redirect_to=/setup/${ flowName }/patterns`;
-		};
+		const tld = queryParams.get( 'tld' );
+		const newDomainSearch = queryParams.get( 'new' ); // be consistent with how the Domain step works
 
+		// for the standard Link in Bio flow
 		function submit( providedDependencies: ProvidedDependencies = {} ) {
+			const logInUrl =
+				locale && locale !== 'en'
+					? `/start/account/user/${ locale }?variationName=${ flowName }&pageTitle=Link%20in%20Bio&redirect_to=/setup/patterns?flow=${ flowName }`
+					: `/start/account/user?variationName=${ flowName }&pageTitle=Link%20in%20Bio&redirect_to=/setup/patterns?flow=${ flowName }`;
+
 			recordSubmitStep( providedDependencies, '', flowName, _currentStep );
-			const logInUrl = getStartUrl();
 
 			switch ( _currentStep ) {
 				case 'intro':
@@ -78,24 +78,84 @@ export const linkInBio: Flow = {
 			return providedDependencies;
 		}
 
+		// when there is a designated tld, the domain step will come first, hence altering the flow accordingly
+		function submitDomainFirst( providedDependencies: ProvidedDependencies = {} ) {
+			const logInUrl =
+				locale && locale !== 'en'
+					? `/start/link-in-bio-tld/${ locale }?variationName=${ flowName }&pageTitle=Link%20in%20Bio&tld=${ tld }&new=${ newDomainSearch }`
+					: `/start/link-in-bio-tld?variationName=${ flowName }&pageTitle=Link%20in%20Bio&tld=${ tld }&new=${ newDomainSearch }`;
+
+			recordSubmitStep( providedDependencies, '', flowName, _currentStep );
+
+			switch ( _currentStep ) {
+				case 'intro':
+					if ( userIsLoggedIn ) {
+						return window.location.assign(
+							`/start/link-in-bio-tld/domains?new=${ encodeURIComponent(
+								providedDependencies.siteTitle as string
+							) }&search=yes&hide_initial_query=yes`
+						);
+					}
+					return window.location.assign( logInUrl );
+
+				case 'patterns':
+					return navigate( 'linkInBioSetup' );
+
+				case 'linkInBioSetup':
+					return window.location.assign(
+						`/start/link-in-bio-tld/plans-link-in-bio?variationName=${ flowName }&pageTitle=Link%20in%20Bio&tld=${ tld }&new=${ newDomainSearch }`
+					);
+
+				case 'launchpad': {
+					return navigate( 'processing' );
+				}
+			}
+			return providedDependencies;
+		}
+
+		if ( tld ) {
+			return submitDomainFirst;
+		}
+
+		return submit;
+	},
+
+	useStepNavigation( _currentStep, navigate ) {
+		const flowName = this.name;
+		const { setStepProgress } = useDispatch( ONBOARD_STORE );
+		const flowProgress = useFlowProgress( { stepName: _currentStep, flowName } );
+		const siteSlug = useSiteSlug();
+
+		setStepProgress( flowProgress );
+
+		// trigger guides on step movement, we don't care about failures or response
+		wpcom.req.post(
+			'guides/trigger',
+			{
+				apiNamespace: 'wpcom/v2/',
+			},
+			{
+				flow: flowName,
+				step: _currentStep,
+			}
+		);
+
+		const submit = this.useSubmit( _currentStep, navigate );
+
 		const goBack = () => {
 			return;
 		};
-
 		const goNext = () => {
 			switch ( _currentStep ) {
 				case 'launchpad':
 					return window.location.assign( `/view/${ siteSlug }` );
-
 				default:
 					return navigate( 'intro' );
 			}
 		};
-
 		const goToStep = ( step: StepPath | `${ StepPath }?${ string }` ) => {
 			navigate( step );
 		};
-
 		return { goNext, goBack, goToStep, submit };
 	},
 };
