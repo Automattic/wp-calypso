77b8e9b3bdaed65e359032857ecd8d8be4e8b26c
Praveen <spraveenitpro@gmail.com>
2022-11-15 13:28:55 -0800

    Add Redux store to the initial test to fix errors

---
+++ client/me/purchases/manage-purchase/test/purchase-meta.js
@@ -0,0 +1,40 @@
+/**
+ * @jest-environment jsdom
+ */
+
+/* eslint-disable jest/no-conditional-expect */
+/* eslint-disable jest/valid-title */
+
+import { render, screen } from '@testing-library/react';
+import { Provider as ReduxProvider } from 'react-redux';
+import { createReduxStore } from 'calypso/state';
+import {
+	getSitePurchases,
+	getByPurchaseId,
+	hasLoadedUserPurchasesFromServer,
+	hasLoadedSitePurchasesFromServer,
+	getRenewableSitePurchases,
+} from 'calypso/state/purchases/selectors';
+import PurchaseMeta from '../purchase-meta';
+
+describe( 'PurchaseMetaPrice', () => {
+	const store = createReduxStore();
+	it( 'does render "Free with Plan"', () => {
+		const purchase = {
+			expiryStatus: 'included',
+			productSlug: 'wp_titan_mail_monthly',
+			productDisplayPrice: '<abbr title="United States Dollars">$</abbr>3.50',
+		};
+		render(
+			<ReduxProvider store={ store }>
+				<PurchaseMeta
+					purchase={ purchase }
+					hasLoadedPurchasesFromServer={ false }
+					purchaseId={ false }
+					siteSlug="test"
+				/>
+			</ReduxProvider>
+		);
+		expect( screen.getByText( /Free with Plan/ ) ).toBeInTheDocument();
+	} );
+} );
