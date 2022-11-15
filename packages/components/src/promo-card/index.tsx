f5104b461742284b5b4cf860fd169e2e165c82b2
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2022-11-15 17:00:49 +0800

    Remove unused SVG data for QR code.

--- packages/components/src/promo-card/index.tsx
+++ packages/components/src/promo-card/index.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
 import './style.scss';
-import { AppStoreSVG, QRCodeSVG, WordPressJetpackSVG } from './svg-icons';
+import { AppStoreSVG, WordPressJetpackSVG } from './svg-icons';
 
 export type PromoCardProps = {
 	className?: string;
@@ -12,18 +12,17 @@ export default function PromoCard( { className, isMobile }: PromoCardProps ) {
 	// Using useTranslate() with interpolation to set up the title/message.
 	// https://wpcalypso.wordpress.com/devdocs/packages/i18n-calypso/README.md
 	const translate = useTranslate();
+	const redirectLink = 'https://jetpack.com/app/';
 	const components = {
-		a: <a href="https://jetpack.com/app/" />,
+		a: <a href={ redirectLink } />,
 	};
 	// TODO: Fix mobile logic.
 	// Should this maybe come via a media query in the CSS?
 	// Also, does it make sense to push one app store over the other?
 	// Might make more sense to have a custom "app store" SVG image that goes to wp.com/app.
 
-	// TODO: Remove (or update) QRCodeSVG component/data.
+	// TODO: Include QR code in bundle?
 	// Pulling the QR code from the jetpack.com site for now.
-	// Site: https://jetpack.com/mobile/
-	// Image URL: https://s0.wp.com/wp-content/themes/a8c/jetpackme-new/images/landing-pages/jetpack-app-get-2x.png
 	return (
 		<div className={ classNames( 'promo-card', className ?? null ) }>
 			<div className="promo-lhs">
@@ -47,10 +46,9 @@ export default function PromoCard( { className, isMobile }: PromoCardProps ) {
 				</p>
 			</div>
 			<div className="promo-rhs">
-				{ false && ! isMobile && <QRCodeSVG /> }
 				{ isMobile && <AppStoreSVG /> }
 				{ ! isMobile && (
-					<a href="https://jetpack.com">
+					<a href={ redirectLink }>
 						<img
 							className="jetpack-qr-code"
 							src="https://s0.wp.com/wp-content/themes/a8c/jetpackme-new/images/landing-pages/jetpack-app-get-2x.png"
