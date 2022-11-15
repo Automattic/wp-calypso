babcd675aa640741783ed998bc223de538128cfe
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2022-11-16 03:43:08 +0800

    Stats: Update promo card links to Jetpack mobile app (#70028)
    * Update URLs to go to Jetpack mobile app.
    
    The design has wp.com/apps but that is the landing page for the WordPress mobile app. We want people using the Jetpack mobile app going forward.
    
    * Update QR code for Jetpack mobile app.
    
    * Remove unused SVG data for QR code.
    
    Co-authored-by: Jason Moon <jason.moon@automattic.com>

--- packages/components/src/promo-card/index.tsx
+++ packages/components/src/promo-card/index.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
 import './style.scss';
-import { AppStoreSVG, QRCodeSVG, WordPressJetpackSVG } from './svg-icons';
+import { AppStoreSVG, WordPressJetpackSVG } from './svg-icons';
 
 export type PromoCardProps = {
 	className?: string;
@@ -12,13 +12,17 @@ export default function PromoCard( { className, isMobile }: PromoCardProps ) {
 	// Using useTranslate() with interpolation to set up the title/message.
 	// https://wpcalypso.wordpress.com/devdocs/packages/i18n-calypso/README.md
 	const translate = useTranslate();
+	const redirectLink = 'https://jetpack.com/app/';
 	const components = {
-		a: <a href="https://wp.com/apps" />,
+		a: <a href={ redirectLink } />,
 	};
 	// TODO: Fix mobile logic.
 	// Should this maybe come via a media query in the CSS?
 	// Also, does it make sense to push one app store over the other?
 	// Might make more sense to have a custom "app store" SVG image that goes to wp.com/app.
+
+	// TODO: Include QR code in bundle?
+	// Pulling the QR code from the jetpack.com site for now.
 	return (
 		<div className={ classNames( 'promo-card', className ?? null ) }>
 			<div className="promo-lhs">
@@ -36,14 +40,22 @@ export default function PromoCard( { className, isMobile }: PromoCardProps ) {
 								'Check your stats on-the-go and get real-time notifications with the Jetpack mobile app.'
 						  )
 						: translate(
-								'Visit {{a}}wp.com/app{{/a}} or scan the QR code to download the Jetpack mobile app.',
+								'Visit {{a}}jetpack.com/app{{/a}} or scan the QR code to download the Jetpack mobile app.',
 								{ components }
 						  ) }
 				</p>
 			</div>
 			<div className="promo-rhs">
-				{ ! isMobile && <QRCodeSVG /> }
 				{ isMobile && <AppStoreSVG /> }
+				{ ! isMobile && (
+					<a href={ redirectLink }>
+						<img
+							className="jetpack-qr-code"
+							src="https://s0.wp.com/wp-content/themes/a8c/jetpackme-new/images/landing-pages/jetpack-app-get-2x.png"
+							alt="QR Code for Jetpack mobile app"
+						/>
+					</a>
+				) }
 			</div>
 		</div>
 	);
