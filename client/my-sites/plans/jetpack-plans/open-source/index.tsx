d9b3238d15373eeb956b5381b47c76e848e5168e
Kevin Zoschke <kevin.zoschke@automattic.com>
2022-11-15 15:41:13 -0500

    Make headings visually similar

--- client/my-sites/plans/jetpack-plans/open-source/index.tsx
+++ client/my-sites/plans/jetpack-plans/open-source/index.tsx
@@ -8,7 +8,7 @@ const OpenSourceSection: React.FC = () => {
 	return (
 		<div className="jetpack-open-source">
 			<img className="jetpack-open-source__icon" width="54" height="48" src={ icon } alt="" />
-			<h2 className="jetpack-open-source__title">{ translate( 'Five for the Future' ) }</h2>
+			<h3 className="jetpack-open-source__title">{ translate( 'Five for the Future' ) }</h3>
 			<p className="jetpack-open-source__desc">
 				{ translate(
 					'We invest profits from Jetpack back into the WordPress open source community through projects and event sponsorships.'
