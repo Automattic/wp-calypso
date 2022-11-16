59d05361ad8f7c0015ec08f6ffdd3d07b9b333df
Kevin Zoschke <kevin.zoschke@automattic.com>
2022-11-16 09:09:25 -0500

    Update FAQ storage item

--- client/my-sites/plans/jetpack-plans/faq/index.tsx
+++ client/my-sites/plans/jetpack-plans/faq/index.tsx
@@ -99,7 +99,7 @@ const JetpackFAQ: FC = () => {
 					className="jetpack-faq__section"
 				>
 					{ translate(
-						"If your site's VaultPress Backup storage limit is reached, your older backups will be deleted. Depending on the size of your site and your site's VaultPress Backup storage limit, your site's backup retention period may be reduced down to 7 days of your most recent backups. You will still be able to restore existing backups, but new site updates will not be backed up until you free up storage or upgrade your storage limit."
+						"If your site's backup storage limit is reached, your older backups will be deleted. Depending on the size of your site and your site's backup storage limit, your site's backup retention period may be reduced down to 7 days of your most recent backups. You will still be able to restore existing backups, but new site updates will not be backed up until you free up storage or upgrade your storage limit."
 					) }
 				</FoldableFAQ>
 				<FoldableFAQ
