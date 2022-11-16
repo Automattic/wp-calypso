e4a05fec15d860a71914b3ffa3ec4ea961c1f07f
Yoav Farhi <yoav@farhi.org>
2022-11-16 10:08:18 +0200

    fix code structure after commiting a suggestion

--- client/components/zendesk-chat/index.jsx
+++ client/components/zendesk-chat/index.jsx
@@ -5,13 +5,13 @@ const ZendeskChat = ( { chatId } ) => {
 		if ( ! chatId ) {
 			return;
 		}
-			const script = document.createElement( 'script' );
-			script.src = 'https://static.zdassets.com/ekr/snippet.js?key=' + chatId;
-			script.type = 'text/javascript';
-			script.id = 'ze-snippet';
 
-			document.getElementById( 'zendesk-chat-container' ).appendChild( script );
-		}
+		const script = document.createElement( 'script' );
+		script.src = 'https://static.zdassets.com/ekr/snippet.js?key=' + chatId;
+		script.type = 'text/javascript';
+		script.id = 'ze-snippet';
+
+		document.getElementById( 'zendesk-chat-container' ).appendChild( script );
 	}, [ chatId ] );
 
 	return <div id="zendesk-chat-container" />;
