d5619df6ebdaaeafc7057ecce6d3a972cda7af4b
Valter Lorran <valterlorran@hotmail.com>
2022-11-16 11:23:45 -0300

    Add selector to get products by billing slug

--- packages/data-stores/src/products-list/selectors.ts
+++ packages/data-stores/src/products-list/selectors.ts
@@ -1,5 +1,7 @@
 import { select } from '@wordpress/data';
+import { filter, values } from 'lodash';
 import { STORE_KEY } from './constants';
+import { ProductsListItem } from './types';
 import type { State } from './reducer';
 
 export const getState = ( state: State ) => state;
@@ -19,4 +21,28 @@ export const getProductBySlug = ( _state: State, slug: string ) => {
 	}
 
 	return products?.[ slug ];
+};
+
+/**
+ * Returns an array of products that matches the specified billing product slug.
+ *
+ * @param _state the state object
+ * @param billingProductSlug the product slug to match
+ * @returns ProductsListItem[]|undefined an array of products that matches the specified billing product slug or undefined if the products list is not loaded yet
+ */
+export const getProductsByBillingSlug = (
+	_state: State,
+	billingProductSlug: string
+): ProductsListItem[] | undefined => {
+	if ( ! billingProductSlug ) {
+		return undefined;
+	}
+
+	const products = select( STORE_KEY ).getProductsList();
+
+	if ( ! products ) {
+		return undefined;
+	}
+
+	return filter( values( products ), { billing_product_slug: billingProductSlug } );
 };
--- packages/data-stores/src/products-list/test/selectors.ts
+++ packages/data-stores/src/products-list/test/selectors.ts
@@ -45,6 +45,26 @@ describe( 'selectors', () => {
 				price_tiers: [],
 				price_tier_slug: '',
 			},
+			wp_mp_theme_tsubaki_test_yearly: {
+				product_id: 3001,
+				product_name: 'Tsubaki Third-Party Test',
+				product_slug: 'wp_mp_theme_tsubaki_test_yearly',
+				description: '',
+				product_type: 'marketplace_theme',
+				available: true,
+				billing_product_slug: 'wp-mp-theme-tsubaki-test',
+				is_domain_registration: false,
+				cost_display: 'US$100.00',
+				combined_cost_display: 'US$100.00',
+				cost: 100,
+				cost_smallest_unit: 10000,
+				currency_code: 'USD',
+				price_tier_list: [],
+				price_tier_usage_quantity: null,
+				product_term: 'year',
+				price_tiers: [],
+				price_tier_slug: '',
+			},
 		};
 
 		( wpcomRequest as jest.Mock ).mockResolvedValue( apiResponse );
@@ -65,5 +85,8 @@ describe( 'selectors', () => {
 
 		expect( select( store ).getProductsList() ).toEqual( apiResponse );
 		expect( select( store ).getProductBySlug( 'free_plan' ) ).toEqual( apiResponse[ 'free_plan' ] );
+		expect( select( store ).getProductsByBillingSlug( 'wp-mp-theme-tsubaki-test' ) ).toEqual( [
+			apiResponse[ 'wp_mp_theme_tsubaki_test_yearly' ],
+		] );
 	} );
 } );
