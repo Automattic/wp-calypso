bd2c54aba1e1f69f8bb6e8a8ceb8d5753a144fac
Matías Surdi <matias.emanuel.surdi@automattic.om>
2022-11-17 08:09:22 +0100

    Obtain option values in a more safe way following code review recommendation

--- apps/happy-blocks/src/pricing-plans/components/block-settings.tsx
+++ apps/happy-blocks/src/pricing-plans/components/block-settings.tsx
@@ -2,19 +2,32 @@ import { InspectorControls } from '@wordpress/block-editor';
 import { BlockEditProps } from '@wordpress/blocks';
 import { SelectControl, PanelRow, PanelBody } from '@wordpress/components';
 import { FunctionComponent } from 'react';
-import { BlockAttributes, PricingPlansConfiguration } from '../types';
+import { BlockAttributes, PricingPlan, PricingPlansConfiguration } from '../types';
 
 interface Props {
 	plans: PricingPlansConfiguration;
 }
 
+function getLastElementOrNull< T >( array: T[] ): T | null {
+	if ( ! Array.isArray( array ) || array.length === 0 ) {
+		return null;
+	}
+	return array[ array.length - 1 ];
+}
+
+function getPlanGroupFromPlanSlug( planSlug: string ): string {
+	return planSlug.replace( '-monthly', '' ).replace( '-yearly', '' );
+}
+
 const BlockSettings: FunctionComponent<
 	Pick< BlockEditProps< BlockAttributes >, 'attributes' | 'setAttributes' > & Props
 > = ( { attributes, setAttributes, plans } ) => {
-	const options = Object.values( plans ).map( ( plan ) => ( {
-		value: plan.billing.slice( -1 )[ 0 ].planSlug,
+	const options = Object.values( plans ).map( ( plan: PricingPlan ) => ( {
+		value: getLastElementOrNull( plan.billing )?.planSlug ?? '',
 		label: plan.label,
 	} ) );
+
+	const value = getPlanGroupFromPlanSlug( attributes.planSlug );
 
 	return (
 		<InspectorControls>
@@ -23,7 +36,7 @@ const BlockSettings: FunctionComponent<
 					<SelectControl
 						className="wp-block-a8c-pricing-plans__settings-plan"
 						label="Plan"
-						value={ attributes.planSlug }
+						value={ value }
 						options={ options }
 						onChange={ ( planSlug ) => setAttributes( { planSlug } ) }
 					/>
