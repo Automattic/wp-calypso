57d4e13ab9a4c83c02befedbb3bf321f44b4c3eb
Oli I <oikeme@users.noreply.github.com>
2022-11-17 12:21:58 -0700

    Update index.tsx
    Remove redundant constant for siteId

--- client/jetpack-cloud/sections/partner-portal/assign-license-form/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/assign-license-form/index.tsx
@@ -43,14 +43,12 @@ export default function AssignLicenseForm( {
 	search: string;
 } ) {
 	const translate = useTranslate();
-	const [ selectedSite, setSelectedSite ] = useState( null );
-	const [ selectedSiteId, setSelectedSiteId ] = useState( null );
+	const [ selectedSite, setSelectedSite ] = useState( { ID: 1, domain: '' } );
 	const licenseKey = getQueryArg( window.location.href, 'key' ) as string;
 	const products = getQueryArg( window.location.href, 'products' ) as string;
 	const licenseKeysArray = products !== undefined ? products.split( ',' ) : [ licenseKey ];
 	const onSelectSite = ( site: any ) => {
 		setSelectedSite( site );
-		setSelectedSiteId( site?.ID );
 	};
 	const [ assignLicenses, isLoading ] = useAssignMultipleLicenses( licenseKeysArray, selectedSite );
 
@@ -73,7 +71,7 @@ export default function AssignLicenseForm( {
 						label={ site.domain }
 						name="site_select"
 						disabled={ isLoading }
-						checked={ selectedSiteId === site.ID }
+						checked={ selectedSite?.ID === site.ID }
 					/>
 				</Card>
 			);
