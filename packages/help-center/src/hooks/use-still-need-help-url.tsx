50e112dc145e959e940bd5bb407f12814d889c70
Tony Arcangelini <33258733+arcangelini@users.noreply.github.com>
2022-11-17 13:03:08 +0100

    Help Center: extend happychat to non-Simple sites (#69486)
    * Help Center: update endpoints to work outside of WPCOM
    
    * added site data
    
    * working in atomic - needs plan details
    
    * typing issues
    
    * fixed typings
    
    * Final draft
    
    * Fix tc tests
    
    * help-search-query: safety check to avoid error on no results
    
    * help-center-contact: fix customer ticket meta
    
    * HelpCenter: add ticket endpoint for atomic
    
    * Fix type error is_at
    
    * Add sibyl support
    
    * HelpCenter: sm0l sibyl fixes
    
    * Address testing call issues
    
    * Remove zip, sorry
    
    * Fix plan bug
    
    * Update comment
    
    Co-authored-by: heavyweight <kpapazov@gmail.com>

--- packages/help-center/src/hooks/use-still-need-help-url.tsx
+++ packages/help-center/src/hooks/use-still-need-help-url.tsx
@@ -1,20 +1,12 @@
 /* eslint-disable no-restricted-imports */
 import { useSupportAvailability } from '@automattic/data-stores';
-import { canAccessWpcomApis } from 'wpcom-proxy-request';
 
 export function useStillNeedHelpURL() {
-	const { data: supportAvailability, isLoading } = useSupportAvailability(
-		'OTHER',
-		canAccessWpcomApis()
-	);
+	const { data: supportAvailability, isLoading } = useSupportAvailability( 'OTHER' );
 
 	// email support is available for all non-free users, let's use it as a proxy for free users
 	// TODO: check purchases instead
 	const isFreeUser = ! supportAvailability?.is_user_eligible_for_tickets;
-
-	if ( ! canAccessWpcomApis() ) {
-		return { url: 'https://wordpress.com/help/contact', isLoading };
-	}
 
 	if ( ! isFreeUser ) {
 		return { url: '/contact-options', isLoading };
