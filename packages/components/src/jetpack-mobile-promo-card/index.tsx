c78f935773c1b1cd186032cd912a0b0980f5b002
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2022-11-18 17:59:21 +0800

    Update title & message text based on promo type.

--- packages/components/src/jetpack-mobile-promo-card/index.tsx
+++ packages/components/src/jetpack-mobile-promo-card/index.tsx
@@ -20,15 +20,39 @@ export default function JetpackMobilePromoCard( {
 	// https://wpcalypso.wordpress.com/devdocs/packages/i18n-calypso/README.md
 	const translate = useTranslate();
 	const isWoo = promoType === 'woo';
-	const redirectLink = 'https://jetpack.com/app/';
+	const redirectLink = isWoo ? 'https://woo.com/mobile/' : 'https://jetpack.com/app/';
+	const linkClassName = isWoo ? 'woo' : 'jetpack';
 	const components = {
-		a: <a href={ redirectLink } />,
+		a: <a className={ linkClassName } href={ redirectLink } />,
 	};
 	// TODO: Fix mobile logic.
 	// Should this maybe come via a media query in the CSS?
 	// Also, does it make sense to push one app store over the other?
 	// Might make more sense to have a custom "app store" SVG image that goes to wp.com/app.
-
+	const getMessage = () => {
+		// Woo strings.
+		if ( isWoo ) {
+			if ( isMobile ) {
+				return translate(
+					'Check your stats on-the-go and get real-time notifications with the Woo mobile app.'
+				);
+			}
+			return translate(
+				'Visit {{a}}woo.com/mobile{{/a}} or scan the QR code to download the WooCommerce mobile app.',
+				{ components }
+			);
+		}
+		// Jetpack strings.
+		if ( isMobile ) {
+			return translate(
+				'Check your stats on-the-go and get real-time notifications with the Jetpack mobile app.'
+			);
+		}
+		return translate(
+			'Visit {{a}}jetpack.com/app{{/a}} or scan the QR code to download the Jetpack mobile app.',
+			{ components }
+		);
+	};
 	// TODO: Include QR code in bundle?
 	// Pulling the QR code from the jetpack.com site for now.
 	return (
@@ -38,20 +62,16 @@ export default function JetpackMobilePromoCard( {
 					<WordPressJetpackSVG />
 				</div>
 				<p className="promo-card__title">
-					{ translate( 'Bring your stats with you using the {{a}}Jetpack{{/a}} mobile app', {
-						components,
-					} ) }
+					{ isWoo &&
+						translate( 'Bring your Store stats with you using the {{a}}Woo{{/a}} mobile app', {
+							components,
+						} ) }
+					{ ! isWoo &&
+						translate( 'Bring your stats with you using the {{a}}Jetpack{{/a}} mobile app', {
+							components,
+						} ) }
 				</p>
-				<p className="promo-card__message">
-					{ isMobile
-						? translate(
-								'Check your stats on-the-go and get real-time notifications with the Jetpack mobile app.'
-						  )
-						: translate(
-								'Visit {{a}}jetpack.com/app{{/a}} or scan the QR code to download the Jetpack mobile app.',
-								{ components }
-						  ) }
-				</p>
+				<p className="promo-card__message">{ getMessage() }</p>
 			</div>
 			<div className="promo-rhs">
 				{ isMobile && <AppStoreSVG /> }
