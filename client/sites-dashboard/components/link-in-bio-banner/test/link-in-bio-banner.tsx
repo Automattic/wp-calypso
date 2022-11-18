82e833b0494e9eaeefd4109b3c353d133c123a2a
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2022-11-18 15:14:13 -0300

    Update tests

--- client/sites-dashboard/components/link-in-bio-banner/test/link-in-bio-banner.tsx
+++ client/sites-dashboard/components/link-in-bio-banner/test/link-in-bio-banner.tsx
@@ -1,55 +1,27 @@
 /**
  * @jest-environment jsdom
  */
-import * as viewport from '@automattic/viewport-react';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import { createReduxStore } from 'calypso/state/index';
 import { LinkInBioBanner } from '../link-in-bio-banner';
-import { LINK_IN_BIO_BANNER_PREFERENCE } from '../link-in-bio-banner-parts';
 
 describe( '<LinkInBioBanner>', () => {
 	test.each( [
 		//visible and right amount of rows - show
-		[ true, 1, 'row', 'row-banner', true, false, true ],
-		[ true, 1, 'grid', 'double-tile-banner', true, false, true ],
-		[ true, 2, 'grid', 'tile-banner', true, false, true ],
-		// dismissed but right amount of rows - don't show
-		[ false, 1, 'row', 'row-banner', false, false, true ],
-		[ false, 1, 'grid', 'double-tile-banner', false, false, true ],
-		[ false, 2, 'grid', 'row-banner', false, false, true ],
+		[ 1, 'row', 'row-banner', true ],
+		[ 2, 'grid', 'tile-banner', true ],
 		// visible but too many rows - don't show
-		[ true, 2, 'row', 'row-banner', false, false, true ],
-		[ true, 3, 'grid', 'double-tile-banner', false, false, true ],
-		[ true, 3, 'grid', 'tile-banner', false, false, true ],
-		// visible and right amount of rows but on mobile - always show tile-banner
-		[ true, 1, 'row', 'tile-banner', true, true, false ],
-		[ true, 1, 'grid', 'tile-banner', true, true, false ],
-		[ true, 2, 'grid', 'tile-banner', true, false, true ],
+		[ 3, 'row', 'row-banner', false ],
+		[ 3, 'grid', 'tile-banner', false ],
 		//visible and right amount of rows, but already has Link in Bio site - don't show
-		[ true, 1, 'row', 'row-banner', false, false, true, 'link-in-bio' ],
-		// should output tile-banner but not enough space - don't show
-		[ true, 2, 'grid', 'tile-banner', false, false, false ],
-		// should be double-tile-banner but space is right - show tile0-banner
-		[ true, 1, 'grid', 'tile-banner', true, false, false ],
+		[ 1, 'row', 'row-banner', false, 'link-in-bio' ],
 	] )(
-		'Parameters: visible: %s, site count: %s, type: %s, output: %s, expectValue: %s, isMobile: %s, isDesktop: %s, siteIntent: %s',
-		(
-			visible,
-			siteCount,
-			type,
-			output,
-			expectValue,
-			isMobile,
-			isDesktop,
-			siteIntent = 'other'
-		) => {
-			jest.spyOn( viewport, 'useMobileBreakpoint' ).mockReturnValue( isMobile );
-			jest.spyOn( viewport, 'useDesktopBreakpoint' ).mockReturnValue( isDesktop );
-
-			const store = createTestStore( visible, siteCount, siteIntent );
+		'Parameters: site count: %s, type: %s, output: %s, expectValue: %s, siteIntent: %s',
+		( siteCount, type, output, expectValue, siteIntent = 'other' ) => {
+			const store = createTestStore( siteCount, siteIntent );
 			const queryClient = new QueryClient( {
 				defaultOptions: {
 					queries: {
@@ -62,7 +34,7 @@ describe( '<LinkInBioBanner>', () => {
 			render(
 				<QueryClientProvider client={ queryClient }>
 					<Provider store={ store }>
-						<LinkInBioBanner displayMode={ type } />
+						<LinkInBioBanner displayMode={ type as 'row' | 'grid' } />
 					</Provider>
 				</QueryClientProvider>
 			);
@@ -77,10 +49,10 @@ describe( '<LinkInBioBanner>', () => {
 	);
 } );
 
-function createTestStore( visible: boolean, siteCount: number, site_intent: string ) {
-	const items: unknown = {};
+function createTestStore( siteCount: number, site_intent: string ) {
+	const items: Record< string, unknown > = {};
 
-	Array.from( { length: siteCount } ).forEach( ( value, index ) => {
+	Array.from( { length: siteCount } ).forEach( ( _, index ) => {
 		items[ `${ index + 1 }` ] = {
 			ID: index + 1,
 			options: {
@@ -95,17 +67,10 @@ function createTestStore( visible: boolean, siteCount: number, site_intent: stri
 					ID: 'test',
 				},
 			},
-			preferences: {
-				remoteValues: {
-					[ LINK_IN_BIO_BANNER_PREFERENCE ]: visible,
-				},
-			},
 			sites: {
 				items,
 			},
 		},
-		( state ) => {
-			return state;
-		}
+		( state ) => state
 	);
 }
