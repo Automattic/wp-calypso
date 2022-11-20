7960303a5cb5efac79f2d5aa454d35d6936fc788
Yoav Farhi <yoav@farhi.org>
2022-11-20 11:18:31 +0200

    Add util isCurrentPlanFlow
    This returns true for flows that match our current plans (yearly or monthly)

--- packages/onboarding/src/utils/flows.ts
+++ packages/onboarding/src/utils/flows.ts
@@ -6,6 +6,13 @@ export const LINK_IN_BIO_POST_SETUP_FLOW = 'link-in-bio-post-setup';
 export const VIDEOPRESS_FLOW = 'videopress';
 export const IMPORT_FOCUSED_FLOW = 'import-focused';
 export const ECOMMERCE_FLOW = 'ecommerce';
+export const BUSINESS_FLOW = 'business';
+export const PREMIUM_FLOW = 'premium';
+export const PERSONAL_FLOW = 'personal';
+export const ECOMMERCE_MONTHLY_FLOW = 'ecommerce-monthly';
+export const BUSINESS_MONTHLY_FLOW = 'business-monthly';
+export const PREMIUM_MONTHLY_FLOW = 'premium-monthly';
+export const PERSONAL_MONTHLY_FLOW = 'personal-monthly';
 
 export const isLinkInBioFlow = ( flowName: string | null ) => {
 	return Boolean(
@@ -33,5 +40,21 @@ export const isTailoredSignupFlow = ( flowName: string | null ) => {
 			( isNewsletterOrLinkInBioFlow( flowName ) ||
 				VIDEOPRESS_FLOW === flowName ||
 				ECOMMERCE_FLOW === flowName )
+	);
+};
+
+export const isCurrentPlanFlow = ( flowName: string | null ) => {
+	return Boolean(
+		flowName &&
+			[
+				ECOMMERCE_FLOW,
+				BUSINESS_FLOW,
+				PREMIUM_FLOW,
+				PERSONAL_FLOW,
+				ECOMMERCE_MONTHLY_FLOW,
+				BUSINESS_MONTHLY_FLOW,
+				PREMIUM_MONTHLY_FLOW,
+				PERSONAL_MONTHLY_FLOW,
+			].includes( flowName )
 	);
 };
