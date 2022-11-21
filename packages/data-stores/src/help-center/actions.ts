ccbae01a01b7f94e64374b00a51b776ef3bc822d
Emanuele Buccelli <escapemanuele@gmail.com>
2022-11-21 10:27:14 +0100

    Help Center: Not keeping HC minimized after reopening (#70143)

--- packages/data-stores/src/help-center/actions.ts
+++ packages/data-stores/src/help-center/actions.ts
@@ -1,11 +1,6 @@
+import { GeneratorReturnType } from '../mapped-types';
 import { SiteDetails } from '../site';
 import { Location, HelpCenterSite } from './types';
-
-export const setShowHelpCenter = ( show: boolean ) =>
-	( {
-		type: 'HELP_CENTER_SET_SHOW',
-		show,
-	} as const );
 
 export const setRouterState = ( history: Location[], index: number ) =>
 	( {
@@ -38,6 +33,18 @@ export const setIsMinimized = ( minimized: boolean ) =>
 		type: 'HELP_CENTER_SET_MINIMIZED',
 		minimized,
 	} as const );
+
+export const setShowHelpCenter = function* ( show: boolean ) {
+	if ( ! show ) {
+		// reset minimized state when the help center is closed
+		yield setIsMinimized( false );
+	}
+
+	return {
+		type: 'HELP_CENTER_SET_SHOW',
+		show,
+	} as const;
+};
 
 export const setSubject = ( subject: string ) =>
 	( {
@@ -86,18 +93,19 @@ export const resetStore = () =>
 		type: 'HELP_CENTER_RESET_STORE',
 	} as const );
 
-export type HelpCenterAction = ReturnType<
-	| typeof setShowHelpCenter
-	| typeof setSite
-	| typeof setSubject
-	| typeof setRouterState
-	| typeof resetRouterState
-	| typeof resetStore
-	| typeof setMessage
-	| typeof setUserDeclaredSite
-	| typeof setUserDeclaredSiteUrl
-	| typeof resetIframe
-	| typeof setIframe
-	| typeof setUnreadCount
-	| typeof setIsMinimized
->;
+export type HelpCenterAction =
+	| ReturnType<
+			| typeof setSite
+			| typeof setSubject
+			| typeof setRouterState
+			| typeof resetRouterState
+			| typeof resetStore
+			| typeof setMessage
+			| typeof setUserDeclaredSite
+			| typeof setUserDeclaredSiteUrl
+			| typeof resetIframe
+			| typeof setIframe
+			| typeof setUnreadCount
+			| typeof setIsMinimized
+	  >
+	| GeneratorReturnType< typeof setShowHelpCenter >;
