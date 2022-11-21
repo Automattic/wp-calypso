c69eb02569bdc122745744245598d1f06118d0c5
Yashwin <yashwinpoojary@gmail.com>
2022-11-21 10:56:22 +0530

    Partner Portal: filter already assigned products and plan to a site when issuing licenses (#70163)
    * Filter already assigned products and plan to a site when issuing licenses.
    
    * Minor code refactor
    
    Co-authored-by: Kristian Vitozev <kristian.vitozev@automattic.com>
    
    Co-authored-by: Kristian Vitozev <kristian.vitozev@automattic.com>

--- client/state/partner-portal/licenses/selectors.ts
+++ client/state/partner-portal/licenses/selectors.ts
@@ -6,6 +6,7 @@ import {
 } from 'calypso/state/partner-portal/types';
 // Required for modular state.
 import 'calypso/state/partner-portal/init';
+import type { AppState } from 'calypso/types';
 
 export function hasFetchedLicenses( state: PartnerPortalStore ): boolean {
 	return state.partnerPortal.licenses.hasFetched;
@@ -27,4 +28,23 @@ export function getLicenseCounts( state: PartnerPortalStore ): LicenseCounts {
 
 export function hasFetchedLicenseCounts( state: PartnerPortalStore ): boolean {
 	return state.partnerPortal.licenses.hasFetchedLicenseCounts;
+}
+
+// Returns the product IDs of the assigned plan(bundle) & all the products to a particular site in an array.
+export function getAssignedPlanAndProductIDsForSite(
+	state: AppState,
+	siteId: number
+): Array< number > {
+	const planAndProductIDs: number[] = [];
+	const currentSite = state?.sites?.items?.[ siteId ];
+	// Get the assigned plan(bundle) to a site.
+	const plan = currentSite?.plan.product_id;
+	if ( plan ) {
+		planAndProductIDs.push( plan );
+	}
+	// Get all assigned products to a site.
+	currentSite?.products.forEach( ( product: { product_id: string } ) =>
+		planAndProductIDs.push( parseInt( product.product_id ) )
+	);
+	return planAndProductIDs;
 }
