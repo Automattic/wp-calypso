0434fc14f781286d351f39405c9bfa4e1cf9f51b
Nelson <nelsonec87@gmail.com>
2022-11-22 12:18:28 -0300

    Added ecommerce flow with domains step

--- client/landing/stepper/declarative-flow/internals/steps-repository/index.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/index.ts
@@ -21,6 +21,7 @@ export { default as importerMedium } from './importer-medium';
 export { default as importerSquarespace } from './importer-squarespace';
 export { default as importerWordpress } from './importer-wordpress';
 export { default as businessInfo } from './business-info';
+export { default as checkPlan } from './check-plan';
 export { default as storeAddress } from './store-address';
 export { default as vertical } from './site-vertical';
 export { default as wooTransfer } from './woo-transfer';
@@ -48,6 +49,8 @@ export { default as getCurrentThemeSoftwareSets } from './get-current-theme-soft
 export { default as storeProfiler } from './store-profiler';
 export { default as designCarousel } from './design-carousel';
 export { default as domains } from './domains';
+export { default as setThemeStep } from './set-theme-step';
+export { default as waitForAtomic } from './wait-for-atomic';
 
 export type StepPath =
 	| 'courses'
@@ -79,6 +82,8 @@ export type StepPath =
 	| 'vertical'
 	| 'wooTransfer'
 	| 'wooInstallPlugins'
+	| 'waitForAtomic'
+	| 'checkPlan'
 	| 'error'
 	| 'wooConfirm'
 	| 'wooVerifyEmail'
@@ -96,6 +101,7 @@ export type StepPath =
 	| 'subscribers'
 	| 'getCurrentThemeSoftwareSets'
 	| 'designCarousel'
+	| 'setThemeStep'
 	| 'storeProfiler'
 	| 'chooseAPlan'
 	| 'videomakerSetup'
