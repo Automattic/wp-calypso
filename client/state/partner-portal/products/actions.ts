f87f9305332972aff218b3237e1def49df4b7e9d
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2022-11-22 12:59:56 -0800

    Import modular reducer

--- client/state/partner-portal/products/actions.ts
+++ client/state/partner-portal/products/actions.ts
@@ -4,6 +4,8 @@ import {
 	JETPACK_PARTNER_PORTAL_SELECTED_PRODUCT_SLUGS_REMOVE,
 	JETPACK_PARTNER_PORTAL_SELECTED_PRODUCT_SLUGS_CLEAR,
 } from 'calypso/state/action-types';
+// Required for modular state.
+import 'calypso/state/partner-portal/init';
 
 export function addSelectedProductSlugs( productSlugs: string | string[] ): AnyAction {
 	return { type: JETPACK_PARTNER_PORTAL_SELECTED_PRODUCT_SLUGS_ADD, productSlugs };
