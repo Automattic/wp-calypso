0b24d5505bd1a9ccd2f299748a896317c417673e
Matías Emanuel Surdi <112691742+msurdi-a8c@users.noreply.github.com>
2022-11-22 11:01:44 +0100

    Implement pricing plans block (#69918)
    * Implement pricing plans block
    
    * Add @wordpress/element to dependencies
    
    * Open link to checkout in new tab
    
    * Address linter warnings
    
    * Use home_url instead of get_site_url
    
    * Remove lodash
    
    * Change BEM naming following code review recommendation
    
    * Obtain option values in a more safe way following code review recommendation
    
    * Add missing translations
    
    * Follow the same css naming convention as the support-page happy block
    
    * Remove extra newline
    
    * Use existing variable for skeleton color
    
    * Add constant for font-family
    
    * Remove generic comment
    
    * Fix typo
    
    * Update saving computation
    
    * Loop over plans data only once
    
    * Re-implement pricing-plans hook to obtain plans info from @automattic/calypso-products
    
    * Define configData only if not already defined
    
    * Improve handling of inexistent/outdated product slugs
    
    * Wrap block header/detail on small screens
    
    * Use modern array syntax
    
    * Fix typo
    
    * Rename annually -> annual
    
    * Rename billings -> planChoices
    
    * Rename planBilling -> plan
    
    * Open 'Learn more' link in new tab
    
    * Baseline align pricing label and discount
    
    * Align input label and promo text by line height
    
    Co-authored-by: Matías Surdi <matias.emanuel.surdi@automattic.om>

---
+++ apps/happy-blocks/src/pricing-plans/components/block-settings.tsx
@@ -0,0 +1,36 @@
+import { InspectorControls } from '@wordpress/block-editor';
+import { BlockEditProps } from '@wordpress/blocks';
+import { SelectControl, PanelRow, PanelBody } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { FunctionComponent } from 'react';
+import usePlanOptions from '../hooks/plan-options';
+import { BlockPlan } from '../hooks/pricing-plans';
+import { BlockAttributes } from '../types';
+
+interface Props {
+	plans: BlockPlan[];
+}
+
+const BlockSettings: FunctionComponent<
+	Pick< BlockEditProps< BlockAttributes >, 'attributes' | 'setAttributes' > & Props
+> = ( { attributes, setAttributes, plans } ) => {
+	const options = usePlanOptions( plans );
+
+	return (
+		<InspectorControls>
+			<PanelBody title={ __( 'Basic', 'happy-blocks' ) } initialOpen={ true }>
+				<PanelRow>
+					<SelectControl
+						className="hb-pricing-plans-embed__settings-plan"
+						label={ __( 'Plan', 'happy-blocks' ) }
+						value={ attributes.productSlug }
+						options={ options }
+						onChange={ ( productSlug ) => setAttributes( { productSlug } ) }
+					/>
+				</PanelRow>
+			</PanelBody>
+		</InspectorControls>
+	);
+};
+
+export default BlockSettings;
---
+++ apps/happy-blocks/src/view.jsx
@@ -0,0 +1,16 @@
+// Placeholder for view only assets
+import './view.scss';
+import { render } from '@wordpress/element';
+import ViewPricingPlans from './pricing-plans/view';
+
+function renderPricingPlansBlock( el ) {
+	const attributes = JSON.parse( el.dataset.attributes ?? '{}' );
+
+	render( <ViewPricingPlans attributes={ attributes } />, el );
+}
+
+document.addEventListener( 'DOMContentLoaded', () => {
+	document
+		.querySelectorAll( '.a8c-happy-tools-pricing-plans-block-placeholder' )
+		.forEach( renderPricingPlansBlock );
+} );
