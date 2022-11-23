be7999b1520ebf358fe102a2c582c806a10f7ce0
nelsonec87 <nelsonec87@gmail.com>
2022-11-23 17:55:21 -0300

    Add Woo Tailored Onboarding (#70332)
    * Added ecommerce flow with domains step
    
    * Added default subtitle do domains step
    
    * resetting onboard store after intro

--- client/landing/stepper/declarative-flow/tailored-ecommerce-flow.ts
+++ client/landing/stepper/declarative-flow/tailored-ecommerce-flow.ts
@@ -1,13 +1,22 @@
+import { PLAN_ECOMMERCE, PLAN_ECOMMERCE_MONTHLY } from '@automattic/calypso-products';
 import { useLocale } from '@automattic/i18n-utils';
 import { useFlowProgress, ECOMMERCE_FLOW } from '@automattic/onboarding';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { useEffect } from 'react';
 import { recordFullStoryEvent } from 'calypso/lib/analytics/fullstory';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import {
+	setSignupCompleteSlug,
+	persistSignupDestination,
+	setSignupCompleteFlowName,
+} from 'calypso/signup/storageUtils';
+import { useSite } from '../hooks/use-site';
+import { useSiteSlugParam } from '../hooks/use-site-slug-param';
 import { USER_STORE, ONBOARD_STORE } from '../stores';
 import { recordSubmitStep } from './internals/analytics/record-submit-step';
 import type { StepPath } from './internals/steps-repository';
 import type { Flow, ProvidedDependencies } from './internals/types';
+import type { SiteDetailsPlan } from '@automattic/data-stores';
 
 export const ecommerceFlowRecurTypes = {
 	YEARLY: 'yearly',
@@ -22,16 +31,35 @@ export const ecommerceFlow: Flow = {
 			recordFullStoryEvent( 'calypso_signup_start_ecommerce', { flow: this.name } );
 		}, [] );
 
-		return [ 'intro', 'storeProfiler', 'designCarousel', 'processing' ] as StepPath[];
+		return [
+			'intro',
+			'storeProfiler',
+			'domains',
+			'designCarousel',
+			'siteCreationStep',
+			'processing',
+			'waitForAtomic',
+			'setThemeStep',
+			'checkPlan',
+		] as StepPath[];
 	},
 
 	useStepNavigation( _currentStepName, navigate ) {
 		const flowName = this.name;
-		const { setStepProgress } = useDispatch( ONBOARD_STORE );
+		const { setStepProgress, setPlanCartItem, resetOnboardStore } = useDispatch( ONBOARD_STORE );
 		const flowProgress = useFlowProgress( { stepName: _currentStepName, flowName } );
 		setStepProgress( flowProgress );
 		const userIsLoggedIn = useSelect( ( select ) => select( USER_STORE ).isCurrentUserLoggedIn() );
+		const { selectedDesign, recurType } = useSelect( ( select ) => ( {
+			selectedDesign: select( ONBOARD_STORE ).getSelectedDesign(),
+			recurType: select( ONBOARD_STORE ).getEcommerceFlowRecurType(),
+		} ) );
+		const selectedPlan =
+			recurType === ecommerceFlowRecurTypes.YEARLY ? PLAN_ECOMMERCE : PLAN_ECOMMERCE_MONTHLY;
+
 		const locale = useLocale();
+		const siteSlugParam = useSiteSlugParam();
+		const site = useSite();
 
 		const getStartUrl = () => {
 			return locale && locale !== 'en'
@@ -42,9 +70,55 @@ export const ecommerceFlow: Flow = {
 		function submit( providedDependencies: ProvidedDependencies = {} ) {
 			recordSubmitStep( providedDependencies, '', flowName, _currentStepName );
 			const logInUrl = getStartUrl();
+			const siteSlug = ( providedDependencies?.siteSlug as string ) || siteSlugParam || '';
 
 			switch ( _currentStepName ) {
+				case 'domains':
+					recordTracksEvent( 'calypso_signup_plan_select', {
+						product_slug: selectedPlan,
+						from_section: 'default',
+					} );
+
+					setPlanCartItem( { product_slug: selectedPlan } );
+					return navigate( 'siteCreationStep' );
+
+				case 'siteCreationStep':
+					return navigate( 'processing' );
+
+				case 'processing':
+					// Coming from setThemeStep
+					if ( providedDependencies?.selectedDesign ) {
+						return window.location.assign( `${ site?.URL }/wp-admin/admin.php?page=wc-admin` );
+					}
+
+					if ( providedDependencies?.finishedWaitingForAtomic ) {
+						return navigate( 'setThemeStep' );
+					}
+
+					if ( providedDependencies?.siteSlug ) {
+						const destination = `/setup/${ flowName }/checkPlan?siteSlug=${ siteSlug }`;
+						persistSignupDestination( destination );
+						setSignupCompleteSlug( siteSlug );
+						setSignupCompleteFlowName( flowName );
+
+						// The site is coming from the checkout already Atomic (and with the new URL)
+						// There's probably a better way of handling this change
+						const returnUrl = encodeURIComponent(
+							`/setup/${ flowName }/checkPlan?theme=${
+								selectedDesign?.slug
+							}&siteSlug=${ siteSlug.replace( '.wordpress.com', '.wpcomstaging.com' ) }`
+						);
+
+						return window.location.assign(
+							`/checkout/${ encodeURIComponent(
+								( siteSlug as string ) ?? ''
+							) }?redirect_to=${ returnUrl }&signup=1`
+						);
+					}
+					return navigate( `checkPlan?siteSlug=${ siteSlug }` );
+
 				case 'intro':
+					resetOnboardStore();
 					if ( userIsLoggedIn ) {
 						return navigate( 'storeProfiler' );
 					}
@@ -52,10 +126,28 @@ export const ecommerceFlow: Flow = {
 
 				case 'storeProfiler':
 					return navigate( 'designCarousel' );
+
 				case 'designCarousel':
+					return navigate( 'domains' );
+
+				case 'setThemeStep':
 					return navigate( 'processing' );
-				case 'processing':
-					return navigate( 'intro' );
+
+				case 'waitForAtomic':
+					return navigate( 'processing' );
+
+				case 'checkPlan':
+					// eCommerce Plan
+					if (
+						[ PLAN_ECOMMERCE, PLAN_ECOMMERCE_MONTHLY ].includes(
+							( providedDependencies?.currentPlan as SiteDetailsPlan )?.product_slug
+						)
+					) {
+						return navigate( 'waitForAtomic' );
+					}
+
+					// Not eCommerce Plan
+					return window.location.assign( `/setup/site-setup/goals?siteSlug=${ siteSlug }` );
 			}
 			return providedDependencies;
 		}
@@ -67,7 +159,6 @@ export const ecommerceFlow: Flow = {
 				default:
 					return navigate( 'intro' );
 			}
-			return;
 		};
 
 		const goNext = () => {
