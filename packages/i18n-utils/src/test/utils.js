5093fd06f7607e6a44cb668a25fcd2120a35c79a
Dev <dev.sa@a8c.com>
2022-11-23 02:12:17 -0500

    Add tests for getRealPathName method in utils

--- packages/i18n-utils/src/test/utils.js
+++ packages/i18n-utils/src/test/utils.js
@@ -1,4 +1,4 @@
-import { getMappedLanguageSlug } from '../';
+import { getMappedLanguageSlug, getRealPathName } from '../';
 
 jest.mock( '@automattic/calypso-config', () => ( {} ) );
 
@@ -11,5 +11,19 @@ describe( '#getMappedLanguageSlug', () => {
 		expect( getMappedLanguageSlug( 'en' ) ).toBe( 'en' );
 		expect( getMappedLanguageSlug( 'de' ) ).toBe( 'de' );
 		expect( getMappedLanguageSlug( 'he' ) ).toBe( 'he' );
+	} );
+} );
+
+describe( '#getRealPathName', () => {
+	test( 'should correctly return the real path when locale is at the beginning of the path', () => {
+		expect( getRealPathName( '/en/themes' ) ).toBe( 'themes' );
+		expect( getRealPathName( '/themes' ) ).toBe( 'themes' );
+		expect( getRealPathName( '/de/themes' ) ).toBe( 'themes' );
+	} );
+
+	test( 'should correctly return the real path when locale is at the end of the path', () => {
+		expect( getRealPathName( '/themes/en' ) ).toBe( 'themes' );
+		expect( getRealPathName( '/themes' ) ).toBe( 'themes' );
+		expect( getRealPathName( '/themes/de' ) ).toBe( 'themes' );
 	} );
 } );
