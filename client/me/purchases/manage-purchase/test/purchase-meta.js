643ff7c8e4528ea39935416a65ff66cd3e2e889e
Praveen <spraveenitpro@gmail.com>
2022-11-22 16:42:58 -0800

    Add tests to check bill_period_label to see if it renders the period as expected

--- client/me/purchases/manage-purchase/test/purchase-meta.js
+++ client/me/purchases/manage-purchase/test/purchase-meta.js
@@ -11,30 +11,29 @@ import { createReduxStore } from 'calypso/state';
 import PurchaseMeta from '../purchase-meta';
 
 describe( 'PurchaseMeta', () => {
-	const store = createReduxStore(
-		{
-			purchases: {
-				data: [
-					{
-						ID: 1,
-						expiry_status: 'included',
+	it( 'does render "Free with Plan"', () => {
+		const store = createReduxStore(
+			{
+				purchases: {
+					data: [
+						{
+							ID: 1,
+							expiry_status: 'included',
+						},
+					],
+				},
+				sites: {
+					requestingAll: false,
+				},
+				currentUser: {
+					id: 1,
+					user: {
+						primary_blog: 'example',
 					},
-				],
-			},
-			sites: {
-				requestingAll: false,
-			},
-			currentUser: {
-				id: 1,
-				user: {
-					primary_blog: 'example',
 				},
 			},
-		},
-		( state ) => state
-	);
-
-	it( 'does render "Free with Plan"', () => {
+			( state ) => state
+		);
 		render(
 			<ReduxProvider store={ store }>
 				<PurchaseMeta
@@ -46,5 +45,147 @@ describe( 'PurchaseMeta', () => {
 			</ReduxProvider>
 		);
 		expect( screen.getByText( /Free with Plan/ ) ).toBeInTheDocument();
+	} );
+
+	it( 'does render "/ year" in the price column when it is a yearly purchase', () => {
+		const store = createReduxStore(
+			{
+				purchases: {
+					data: [
+						{
+							ID: 1,
+							bill_period_label: 'per year',
+						},
+					],
+				},
+				sites: {
+					requestingAll: false,
+				},
+				currentUser: {
+					id: 1,
+					user: {
+						primary_blog: 'example',
+					},
+				},
+			},
+			( state ) => state
+		);
+		render(
+			<ReduxProvider store={ store }>
+				<PurchaseMeta
+					hasLoadedPurchasesFromServer={ true }
+					purchaseId={ 1 }
+					siteSlug="test"
+					isDataLoading={ false }
+				/>
+			</ReduxProvider>
+		);
+		expect( screen.getByText( /year/ ) ).toBeInTheDocument();
+	} );
+
+	it( 'does render "/ month" in the price column when it is a yearly purchase', () => {
+		const store = createReduxStore(
+			{
+				purchases: {
+					data: [
+						{
+							ID: 1,
+							bill_period_label: 'per month',
+						},
+					],
+				},
+				sites: {
+					requestingAll: false,
+				},
+				currentUser: {
+					id: 1,
+					user: {
+						primary_blog: 'example',
+					},
+				},
+			},
+			( state ) => state
+		);
+		render(
+			<ReduxProvider store={ store }>
+				<PurchaseMeta
+					hasLoadedPurchasesFromServer={ true }
+					purchaseId={ 1 }
+					siteSlug="test"
+					isDataLoading={ false }
+				/>
+			</ReduxProvider>
+		);
+		expect( screen.getByText( /month/ ) ).toBeInTheDocument();
+	} );
+	it( 'does render "/ week" in the price column when it is a yearly purchase', () => {
+		const store = createReduxStore(
+			{
+				purchases: {
+					data: [
+						{
+							ID: 1,
+							bill_period_label: 'per week',
+						},
+					],
+				},
+				sites: {
+					requestingAll: false,
+				},
+				currentUser: {
+					id: 1,
+					user: {
+						primary_blog: 'example',
+					},
+				},
+			},
+			( state ) => state
+		);
+		render(
+			<ReduxProvider store={ store }>
+				<PurchaseMeta
+					hasLoadedPurchasesFromServer={ true }
+					purchaseId={ 1 }
+					siteSlug="test"
+					isDataLoading={ false }
+				/>
+			</ReduxProvider>
+		);
+		expect( screen.getByText( /week/ ) ).toBeInTheDocument();
+	} );
+	it( 'does render "/ day" in the price column when it is a daily purchase', () => {
+		const store = createReduxStore(
+			{
+				purchases: {
+					data: [
+						{
+							ID: 1,
+							bill_period_label: 'per day',
+						},
+					],
+				},
+				sites: {
+					requestingAll: false,
+				},
+				currentUser: {
+					id: 1,
+					user: {
+						primary_blog: 'example',
+					},
+				},
+			},
+			( state ) => state
+		);
+		render(
+			<ReduxProvider store={ store }>
+				<PurchaseMeta
+					hasLoadedPurchasesFromServer={ true }
+					purchaseId={ 1 }
+					siteSlug="test"
+					isDataLoading={ false }
+				/>
+			</ReduxProvider>
+		);
+		expect( screen.getByText( /day/ ) ).toBeInTheDocument();
 	} );
 } );
