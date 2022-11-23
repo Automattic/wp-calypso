794d04a36b1e49d5353ad0b7a3d728f271a75167
bangank36 <bangank36@gmail.com>
2022-11-23 12:07:57 +0800

    Only one subject: filter

--- client/components/search-themes/index.tsx
+++ client/components/search-themes/index.tsx
@@ -46,11 +46,10 @@ const SearchThemes: React.FC< SearchThemesProps > = ( { query, onSearch, recordT
 
 	const updateInput = ( updatedInput: string ) => {
 		// Clean up duplicate criteria
-		const searchInput = updatedInput.replace(
-			/(\b(feature|column|subject):.*[^ ]\b)(?=.*\1)/gi,
-			''
-		);
-		setSearchInput( searchInput );
+		updatedInput = updatedInput.replace( /(\b(feature|column|subject):.*[^ ]\b)(?=.*\1)/gi, '' );
+		// Only allow one `subject:` filter
+		updatedInput = updatedInput.replace( /(subject):([\w-]*)(?=.*\1)/gi, '' );
+		setSearchInput( updatedInput );
 		setIsApplySearch( true );
 		searchRef.current?.clear();
 	};
