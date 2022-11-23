61d9f3b7f84782e54035fb5436f9608ad50151bf
Matías Surdi <matias.emanuel.surdi@automattic.om>
2022-11-23 16:13:51 +0100

    Create radio input groups by billing options instance

--- apps/happy-blocks/src/pricing-plans/components/billing-options.tsx
+++ apps/happy-blocks/src/pricing-plans/components/billing-options.tsx
@@ -1,4 +1,5 @@
-import { isWpComMonthlyPlan } from '@automattic/calypso-products';
+import { isWpComAnnualPlan, isWpComMonthlyPlan } from '@automattic/calypso-products';
+import { useInstanceId } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
 import { FunctionComponent } from 'react';
 import usePlanVariants from '../hooks/plan-variants';
@@ -17,6 +18,8 @@ interface Props {
 const BillingOptions: FunctionComponent< Props > = ( { plans, value, onChange } ) => {
 	const selectedPlan = plans?.find( ( plan ) => plan.productSlug === value ) ?? plans[ 0 ];
 	const { monthlyPlan, annualPlan, annualDiscount } = usePlanVariants( plans, selectedPlan );
+	const instanceId = useInstanceId( BillingOptions );
+	const id = `hb-plans-radio-control-${ instanceId }`;
 
 	if ( ! monthlyPlan || ! annualPlan ) {
 		return null;
@@ -29,8 +32,8 @@ const BillingOptions: FunctionComponent< Props > = ( { plans, value, onChange } 
 					<label className="hb-pricing-plans-embed__billing-option-label">
 						<input
 							className="hb-pricing-plans-embed__billing-option-input"
+							id={ id }
 							type="radio"
-							name="price"
 							value={ plan?.productSlug }
 							checked={ plan?.productSlug === value }
 							onChange={ () => onChange( plan?.productSlug ) }
@@ -38,7 +41,7 @@ const BillingOptions: FunctionComponent< Props > = ( { plans, value, onChange } 
 						{ isWpComMonthlyPlan( plan.productSlug )
 							? __( 'Monthly', 'happy-blocks' )
 							: __( 'Annually', 'happy-blocks' ) }
-						{ annualDiscount && isWpComMonthlyPlan( plan.productSlug ) && (
+						{ annualDiscount && isWpComAnnualPlan( plan.productSlug ) && (
 							<Promo>({ annualDiscount })</Promo>
 						) }
 					</label>
