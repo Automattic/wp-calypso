b7b8c79173f04acc49de5f76e3544d66e50758b8
Piotr Stankowski <piotr.stankowski@automattic.com>
2022-11-23 22:48:04 +0100

    Add comments to new price tier fields

--- packages/calypso-products/src/get-price-tier-for-units.ts
+++ packages/calypso-products/src/get-price-tier-for-units.ts
@@ -7,9 +7,27 @@ export interface PriceTierEntry {
 	maximum_price: number;
 	maximum_price_display?: string | null | undefined;
 	maximum_price_monthly_display?: string | null | undefined;
+	/**
+	 * If set, is used to transform the usage/quantity of units used to derive the number of units
+	 * we want to bill the customer for, before multiplying by the per_unit_fee.
+	 *
+	 * To put simply, the purpose of this attribute is to bill the customer at a different granularity compared to their usage.
+	 */
 	transform_quantity_divide_by?: number | null | undefined;
+	/**
+	 * Used for rounding the number of units we want to bill the customer for (which is derived after dividing the
+	 * usage/quantity of units by the `transform_quantity_divide_by` number).
+	 *
+	 * Used only when `transform_quantity_divide_by` is set. Possible values are: `up`, `down`
+	 */
 	transform_quantity_round?: string | null | undefined;
+	/**
+	 * The amount in the currency's smallest unit that this tier costs per unit.
+	 */
 	per_unit_fee?: number | null | undefined;
+	/**
+	 * The amount in the currency's smallest unit that this tier costs as a flat fee (for the entire tier).
+	 */
 	flat_fee?: number | null | undefined;
 }
 
