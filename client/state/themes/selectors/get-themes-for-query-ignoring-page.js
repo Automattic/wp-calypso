a6ac0b7e856e11a94a34c8a6caec4a7173ac5fa1
Griffith Chen <griffith.chen@automattic.com>
2022-11-24 14:19:30 +0800

    Data integrity check

--- client/state/themes/selectors/get-themes-for-query-ignoring-page.js
+++ client/state/themes/selectors/get-themes-for-query-ignoring-page.js
@@ -1,7 +1,11 @@
 import { isEnabled } from '@automattic/calypso-config';
 import { createSelector } from '@automattic/state-utils';
 import { flatMap } from 'lodash';
-import { getActiveTheme, getCanonicalTheme } from 'calypso/state/themes/selectors';
+import {
+	getActiveTheme,
+	getCanonicalTheme,
+	isRequestingActiveTheme,
+} from 'calypso/state/themes/selectors';
 import { getSerializedThemesQueryWithoutPage } from 'calypso/state/themes/utils';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
@@ -30,6 +34,7 @@ export const getThemesForQueryIgnoringPage = createSelector(
 
 		// If query is default, filter out recommended themes.
 		if ( ! ( query.search || query.filter || query.tier ) ) {
+			const selectedSiteId = state.ui ? getSelectedSiteId( state ) : null;
 			const recommendedThemes = state.themes.recommendedThemes.themes;
 			const themeIds = flatMap( recommendedThemes, ( theme ) => theme.id );
 
@@ -38,8 +43,10 @@ export const getThemesForQueryIgnoringPage = createSelector(
 			);
 
 			// Set active theme to be the first theme in the array.
-			if ( isEnabled( 'themes/showcase-i4/search-and-filter' ) ) {
-				const selectedSiteId = state.ui ? getSelectedSiteId( state ) : null;
+			if (
+				isEnabled( 'themes/showcase-i4/search-and-filter' ) &&
+				! isRequestingActiveTheme( state, selectedSiteId )
+			) {
 				const currentThemeId = getActiveTheme( state, selectedSiteId );
 				const currentTheme = getCanonicalTheme( state, selectedSiteId, currentThemeId );
 
