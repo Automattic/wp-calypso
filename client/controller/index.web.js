e18281d8c1fe67ca991c595ec78ee67d097d0e1b
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2022-11-25 16:38:51 -0300

    Update client/controller/index.web.js
    Co-authored-by: Marin Atanasov <8436925+tyxla@users.noreply.github.com>

--- client/controller/index.web.js
+++ client/controller/index.web.js
@@ -135,31 +135,30 @@ export function redirectLoggedOut( context, next ) {
 
 export function redirectLoggedOutForGifting( context, next ) {
 	const state = context.store.getState();
-	const userLoggedOut = ! isUserLoggedIn( state );
-
-	if ( userLoggedOut ) {
-		const loginParameters = {
-			redirectTo: context.path,
-		};
-
-		// Pass along "login_email" and "login_locale" parameters from the
-		// original URL, to ensure the login form is pre-filled with the
-		// correct email address and built with the correct language (when
-		// either of those are requested).
-		const login_email = getImmediateLoginEmail( state );
-		if ( login_email ) {
-			loginParameters.emailAddress = login_email;
-		}
-		const login_locale = getImmediateLoginLocale( state );
-		if ( login_locale ) {
-			loginParameters.locale = login_locale;
-		}
-
-		// force full page reload to avoid SSR hydration issues.
-		window.location = login( loginParameters );
+	if ( isUserLoggedIn( state ) ) {
+		next();	
 		return;
 	}
-	next();
+
+	const loginParameters = {
+		redirectTo: context.path,
+	};
+
+	// Pass along "login_email" and "login_locale" parameters from the
+	// original URL, to ensure the login form is pre-filled with the
+	// correct email address and built with the correct language (when
+	// either of those are requested).
+	const login_email = getImmediateLoginEmail( state );
+	if ( login_email ) {
+		loginParameters.emailAddress = login_email;
+	}
+	const login_locale = getImmediateLoginLocale( state );
+	if ( login_locale ) {
+		loginParameters.locale = login_locale;
+	}
+
+	// force full page reload to avoid SSR hydration issues.
+	window.location = login( loginParameters );
 }
 
 /**
