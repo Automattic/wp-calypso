df7f902d0f95de88875d2d22b59af4abbee58f1e
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2022-11-25 09:34:23 -0300

    Add tracks events

--- client/sites-dashboard/components/sites-dashboard-opt-in-banner.tsx
+++ client/sites-dashboard/components/sites-dashboard-opt-in-banner.tsx
@@ -1,8 +1,11 @@
 import { Button } from '@automattic/components';
 import styled from '@emotion/styled';
 import { useI18n } from '@wordpress/react-i18n';
+import { useDispatch } from 'react-redux';
 import Notice from 'calypso/components/notice';
 import { SiteExcerptData } from 'calypso/data/sites/site-excerpt-types';
+import TrackComponentView from 'calypso/lib/analytics/track-component-view';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 
 interface SitesDashboardOptInBannerProps {
 	sites: SiteExcerptData[];
@@ -34,24 +37,40 @@ const NoticeActions = styled.div( {
 	padding: '16px',
 } );
 
+const EVENT_PREFIX = 'calypso_sites_as_landing_page';
+
 export const SitesDashboardOptInBanner = ( { sites }: SitesDashboardOptInBannerProps ) => {
 	const { __ } = useI18n();
+	const dispatch = useDispatch();
 
 	if ( sites.length < 2 ) {
 		return null;
 	}
 
+	const handleAccept = () => {
+		dispatch( recordTracksEvent( `${ EVENT_PREFIX }_accepted` ) );
+	};
+
+	const handleDismiss = () => {
+		dispatch( recordTracksEvent( `${ EVENT_PREFIX }_rejected` ) );
+	};
+
 	return (
-		<SitesNotice
-			status="is-info"
-			text={ __( 'Do you want to make this page your default when visiting WordPress.com?' ) }
-			icon="heart"
-			showDismiss={ false }
-		>
-			<NoticeActions>
-				<Button borderless>{ __( 'No, thanks' ) }</Button>
-				<Button>{ __( 'Yes, make it my home' ) }</Button>
-			</NoticeActions>
-		</SitesNotice>
+		<>
+			<TrackComponentView eventName={ `${ EVENT_PREFIX }_seen` } />
+			<SitesNotice
+				status="is-info"
+				text={ __( 'Do you want to make this page your default when visiting WordPress.com?' ) }
+				icon="heart"
+				showDismiss={ false }
+			>
+				<NoticeActions>
+					<Button borderless onClick={ handleDismiss }>
+						{ __( 'No, thanks' ) }
+					</Button>
+					<Button onClick={ handleAccept }>{ __( 'Yes, make it my home' ) }</Button>
+				</NoticeActions>
+			</SitesNotice>
+		</>
 	);
 };
