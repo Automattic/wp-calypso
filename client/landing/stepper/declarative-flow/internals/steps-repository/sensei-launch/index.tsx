377773f3e8a3ee7667c5c21fb4c44f9b74d13cdd
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2022-11-25 11:20:47 +0200

    Add progress bar animation for launcher

--- client/landing/stepper/declarative-flow/internals/steps-repository/sensei-launch/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/sensei-launch/index.tsx
@@ -1,7 +1,7 @@
 import { SenseiStepContainer } from '@automattic/onboarding';
 import { useDispatch } from '@wordpress/data';
-import { useCallback, useEffect } from '@wordpress/element';
-//import { useI18n } from '@wordpress/react-i18n';
+import { useCallback, useEffect, useState } from '@wordpress/element';
+import { useI18n } from '@wordpress/react-i18n';
 import { useSelector, useDispatch as useRootDispatch } from 'react-redux';
 import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import { useSiteSlugParam } from 'calypso/landing/stepper/hooks/use-site-slug-param';
@@ -9,7 +9,7 @@ import { SITE_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { fetchSitePlugins } from 'calypso/state/plugins/installed/actions';
 import { getPlugins } from 'calypso/state/plugins/installed/selectors';
-//import { SenseiStepProgress } from '../sensei-setup/sensei-step-progress';
+import { Progress, SenseiStepProgress } from '../sensei-setup/sensei-step-progress';
 
 interface InstalledPlugin {
 	id: string;
@@ -18,11 +18,12 @@ interface InstalledPlugin {
 }
 
 const SenseiLaunch = () => {
-	//const { __ } = useI18n();
+	const { __ } = useI18n();
 	const site = useSite();
 	const siteId = site?.ID;
 	const launchpadScreen = site?.options.launchpad_screen;
 	const siteSlug = useSiteSlugParam();
+	const [ retries, setRetries ] = useState< number >( 0 );
 
 	const selectSitePlugins = useCallback(
 		( state ) => {
@@ -34,33 +35,54 @@ const SenseiLaunch = () => {
 	const { saveSiteSettings } = useDispatch( SITE_STORE );
 	const plugins: InstalledPlugin[] = useSelector( selectSitePlugins );
 	const launchpadUrl = `/setup/sensei/launchpad?siteSlug=${ siteSlug }&siteId=${ siteId }`;
+	const expectedRetries = 10;
 
 	useEffect( () => {
 		const intervalId = setInterval( () => {
 			const woothemesSensei = plugins.find( ( plugin ) => plugin.slug === 'woothemes-sensei' );
 			if ( ! woothemesSensei?.active ) {
+				setRetries( retries + 1 );
 				dispatch( fetchSitePlugins( siteId ) );
 				return;
 			}
 
+			setRetries( -1 );
 			clearInterval( intervalId );
-			if ( launchpadScreen !== 'full' ) {
-				saveSiteSettings( siteId as number, { launchpad_screen: 'full' } ).finally( () => {
+
+			setTimeout( () => {
+				if ( launchpadScreen !== 'full' ) {
+					saveSiteSettings( siteId as number, { launchpad_screen: 'full' } ).finally( () => {
+						window.location.replace( launchpadUrl );
+					} );
+				} else {
 					window.location.replace( launchpadUrl );
-				} );
-			} else {
-				window.location.replace( launchpadUrl );
-			}
+				}
+			}, 800 );
 		}, 3000 );
 
 		return () => clearInterval( intervalId );
 	}, [ plugins, launchpadUrl, dispatch, siteId, saveSiteSettings, launchpadScreen ] );
 
+	const progress: Progress = {
+		percentage: ( retries * 100 ) / expectedRetries,
+		title: __( 'Installing Sensei' ),
+	};
+
+	if ( retries > expectedRetries / 2 || retries < 0 ) {
+		progress.title = __( 'Setting up your new Sensei Home' );
+	}
+
+	if ( retries > ( expectedRetries * 2 ) / 3 ) {
+		const slowPercentage = 66.6 + ( retries * 10 ) / expectedRetries;
+		progress.percentage = slowPercentage > 90 ? 90 : slowPercentage;
+	} else if ( retries < 0 ) {
+		progress.percentage = 100;
+	}
+
 	return (
-		<SenseiStepContainer
-			stepName="senseiSetup"
-			recordTracksEvent={ recordTracksEvent }
-		></SenseiStepContainer>
+		<SenseiStepContainer stepName="senseiSetup" recordTracksEvent={ recordTracksEvent }>
+			<SenseiStepProgress progress={ progress } />
+		</SenseiStepContainer>
 	);
 };
 
