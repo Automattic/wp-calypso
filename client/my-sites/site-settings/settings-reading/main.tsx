79d040147476f91db9a9c93de3b345f83ad6846b
Maciej Grabowski <littlesoldiergraba@gmail.com>
2022-11-29 16:40:27 +0100

    Reading Settings: Create blank reading settings sections

--- client/my-sites/site-settings/settings-reading/main.tsx
+++ client/my-sites/site-settings/settings-reading/main.tsx
@@ -3,6 +3,9 @@ import { useTranslate } from 'i18n-calypso';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
 import Main from 'calypso/components/main';
+import { NewsletterSettingsSection } from './newsletter-settings-section';
+import { RssFeedSettingsSection } from './rss-feed-settings-section';
+import { SiteSettingsSection } from './site-settings-section';
 
 const isEnabled = config.isEnabled( 'settings/modernize-reading-settings' );
 
@@ -17,6 +20,11 @@ const ReadingSettings = () => {
 		<Main className="site-settings">
 			<DocumentHead title={ translate( 'Reading Settings' ) } />
 			<FormattedHeader brandFont headerText={ translate( 'Reading Settings' ) } align="left" />
+			<form>
+				<SiteSettingsSection />
+				<RssFeedSettingsSection />
+				<NewsletterSettingsSection />
+			</form>
 		</Main>
 	);
 };
