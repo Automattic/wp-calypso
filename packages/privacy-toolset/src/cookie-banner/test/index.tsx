1a570b30d65e0735be863444e1f61108abbbe161
Robert Sreberski <robertsreberski@gmail.com>
2022-11-29 12:02:19 -0600

    Add CookieBanner tests and snapshot

---
+++ packages/privacy-toolset/src/cookie-banner/test/index.tsx
@@ -0,0 +1,91 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, fireEvent, getByText, getByTestId } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { CookieBanner, CookieBannerProps } from '..';
+
+const genericContent: CookieBannerProps[ 'content' ] = {
+	simpleConsent: {
+		description: 'simple description',
+		acceptAllButton: 'acceptAllButton',
+		customizeButton: 'customizeButton',
+	},
+	customizedConsent: {
+		description: 'customized description',
+		categories: {
+			essential: {
+				name: 'essential name',
+				description: 'essential description',
+			},
+			analytics: {
+				name: 'analytics name',
+				description: 'analytics description',
+			},
+			advertising: {
+				name: 'advertising name',
+				description: 'advertising description',
+			},
+		},
+		acceptSelectionButton: 'acceptSelectionButton',
+	},
+};
+
+describe( 'CookieBanner', () => {
+	test( 'renders in simple consent correctly', () => {
+		const { container } = render(
+			<CookieBanner content={ genericContent } onAccept={ jest.fn } />
+		);
+		expect( container ).toMatchSnapshot();
+	} );
+	test( 'renders in customized consent correctly (after customize button click)', () => {
+		const { container } = render(
+			<CookieBanner content={ genericContent } onAccept={ jest.fn } />
+		);
+		fireEvent.click( getByText( container, 'customizeButton' ) );
+		expect( container ).toMatchSnapshot();
+	} );
+	test( 'fires onAccept with all buckets true (on accept all button click)', () => {
+		const onAccept = jest.fn();
+		const { container } = render(
+			<CookieBanner content={ genericContent } onAccept={ onAccept } />
+		);
+
+		fireEvent.click( getByText( container, 'acceptAllButton' ) );
+		expect( onAccept ).toHaveBeenCalledWith( {
+			essential: true,
+			analytics: true,
+			advertising: true,
+		} );
+	} );
+	test( 'fires onAccept with default selection (all true) (when no change is done in customized consent)', () => {
+		const onAccept = jest.fn();
+		const { container } = render(
+			<CookieBanner content={ genericContent } onAccept={ onAccept } />
+		);
+		fireEvent.click( getByText( container, 'customizeButton' ) );
+		fireEvent.click( getByText( container, 'acceptSelectionButton' ) );
+
+		expect( onAccept ).toHaveBeenCalledWith( {
+			essential: true,
+			analytics: true,
+			advertising: true,
+		} );
+	} );
+	test( 'fires onAccept with correct selection (when change is done in customized consent)', () => {
+		const onAccept = jest.fn();
+		const { container } = render(
+			<CookieBanner content={ genericContent } onAccept={ onAccept } />
+		);
+		fireEvent.click( getByText( container, 'customizeButton' ) );
+		fireEvent.click( getByTestId( container, 'essential-bucket-toggle' ) );
+		fireEvent.click( getByTestId( container, 'analytics-bucket-toggle' ) );
+
+		fireEvent.click( getByText( container, 'acceptSelectionButton' ) );
+		expect( onAccept ).toHaveBeenCalledWith( {
+			essential: false,
+			analytics: false,
+			advertising: true,
+		} );
+	} );
+} );
