ea0380a9ff947d5dae7c61338f197e7f2741d9ff
Vasyl Stashuk <vasyl.stashuk@gmail.com>
2022-11-29 18:34:31 +0100

    Remove content block (#70538)
    Co-authored-by: vasyas <vasyl@stashuk.com>

--- apps/happy-blocks/src/support-content/README.md
+++
@@ -1,3 +0,0 @@
-Content embed comes in two flavors:
-- `happy-blocks/support-page` for embedding support pages aka WordPress.com Guide
-- `happy-blocks/forum-topic` for embedding WordPress.com forum topics
--- apps/happy-blocks/src/view.jsx
+++ apps/happy-blocks/src/view.jsx
@@ -1,8 +1,6 @@
 // Placeholder for view only assets
-import './view.scss';
 import { render } from '@wordpress/element';
 import ViewPricingPlans from './pricing-plans/view';
-import { updateForumTopicDate } from './support-content/view';
 
 function renderPricingPlansBlock( el ) {
 	const attributes = JSON.parse( el.dataset.attributes ?? '{}' );
@@ -14,5 +12,4 @@ document.addEventListener( 'DOMContentLoaded', () => {
 	document
 		.querySelectorAll( '.a8c-happy-tools-pricing-plans-block-placeholder' )
 		.forEach( renderPricingPlansBlock );
-	document.querySelectorAll( '.wp-block-happy-blocks-forum-topic' ).forEach( updateForumTopicDate );
 } );
