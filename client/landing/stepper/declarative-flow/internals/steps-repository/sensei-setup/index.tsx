cc2895a5174b8688defc18b647b5c0b781216e64
Peter Kiss <peter@yscik.com>
2022-11-29 20:31:31 +0100

    Add animation to theme style previews

--- client/landing/stepper/declarative-flow/internals/steps-repository/sensei-setup/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/sensei-setup/index.tsx
@@ -1,8 +1,11 @@
 import { Gridicon } from '@automattic/components';
 import { SenseiStepContainer } from '@automattic/onboarding';
+import { useDesktopBreakpoint } from '@automattic/viewport-react';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useCallback, useState } from '@wordpress/element';
 import { useI18n } from '@wordpress/react-i18n';
+import classnames from 'classnames';
+import { CSSProperties } from 'react';
 import FormRadioWithThumbnail from 'calypso/components/forms/form-radio-with-thumbnail';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { ONBOARD_STORE } from '../../../../stores';
@@ -11,8 +14,45 @@ import type { Step } from 'calypso/landing/stepper/declarative-flow/internals/ty
 
 import './style.scss';
 
+type ThemeStyle = {
+	name: string;
+	title: string;
+};
+
+const styles: ThemeStyle[] = [
+	{ name: 'green', title: 'Green' },
+	{ name: 'blue', title: 'Blue' },
+	{ name: 'dark', title: 'Dark' },
+	{ name: 'gold', title: 'Gold' },
+];
+
+const ThemeStylePreviews = ( {
+	styles,
+	active,
+}: {
+	styles: ThemeStyle[];
+	active: string;
+	siteTitle?: string;
+} ) => {
+	const index = 1 + styles.findIndex( ( style ) => active === style.name );
+	return (
+		<div className="sensei-theme-previews">
+			<div className="sensei-theme-list" style={ { '--index': index } as CSSProperties }>
+				{ styles.map( ( { name } ) => {
+					return (
+						<div className={ classnames( 'sensei-theme-item', { selected: name === active } ) }>
+							<div className={ `sensei-theme ${ name }` }></div>
+						</div>
+					);
+				} ) }
+			</div>
+		</div>
+	);
+};
+
 const SenseiSetupStep: Step = ( { navigation } ) => {
 	const { __ } = useI18n();
+	const isDesktop = useDesktopBreakpoint();
 
 	const initialSiteTitle = useSelect( ( select ) =>
 		select( ONBOARD_STORE ).getSelectedSiteTitle()
@@ -30,24 +70,8 @@ const SenseiSetupStep: Step = ( { navigation } ) => {
 		dispatch.setSiteTitle( siteTitle );
 		goNext?.();
 	}, [ siteTitle, dispatch, goNext ] );
-	const thumbnailImages = [
-		{
-			style: 'green',
-			title: 'First item',
-		},
-		{
-			style: 'blue',
-			title: 'Second item',
-		},
-		{
-			style: 'black',
-			title: 'Third item',
-		},
-		{
-			style: 'sepia',
-			title: 'Fourth item',
-		},
-	];
+
+	const preview = <ThemeStylePreviews styles={ styles } active={ checked } />;
 
 	return (
 		<SenseiStepContainer stepName="senseiSetup" recordTracksEvent={ recordTracksEvent }>
@@ -69,21 +93,21 @@ const SenseiSetupStep: Step = ( { navigation } ) => {
 						<p>Choose a different theme style now, or customize colors and fonts later.</p>
 					</div>
 					<div className="sensei-theme-style-selector">
-						{ thumbnailImages.map( ( item, i ) => (
+						{ styles.map( ( item, i ) => (
 							<FormRadioWithThumbnail
 								label={ item.title }
 								key={ i }
-								value={ item.style }
+								value={ item.name }
 								thumbnail={ {
-									cssClass: `${ item.style } ${ item.style === checked ? 'checked' : '' }`,
+									cssClass: `${ item.name } ${ item.name === checked ? 'checked' : '' }`,
 								} }
-								checked={ item.style === checked }
+								checked={ item.name === checked }
 								onChange={ handleChange }
 								disabled={ undefined }
 							/>
 						) ) }
 					</div>
-					<div className={ `sensei-theme-small ${ checked }-small` }></div>
+					{ ! isDesktop && <div>{ preview }</div> }
 					<Button disabled={ ! siteTitle } onClick={ handleSubmit }>
 						{ __( 'Continue' ) }
 					</Button>
@@ -92,7 +116,7 @@ const SenseiSetupStep: Step = ( { navigation } ) => {
 						{ __( 'You can change all of this later, too.' ) }
 					</p>
 				</div>
-				<div className={ `sensei-theme ${ checked }` }></div>
+				{ isDesktop && preview }
 			</div>
 		</SenseiStepContainer>
 	);
