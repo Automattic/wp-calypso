b2cdfbed48e0e2a5086d901df80ca8e823cfbdc9
Jess Boctor <jessboctor@automattic.com>
2022-11-30 08:31:46 -0800

    Simplifies the domain gift refund policy. Suggested in discussion_r1036079794

--- client/my-sites/checkout/composite-checkout/components/refund-policies.tsx
+++ client/my-sites/checkout/composite-checkout/components/refund-policies.tsx
@@ -223,8 +223,8 @@ function RefundPolicyItem( { refundPolicy }: { refundPolicy: RefundPolicy } ) {
 
 		case RefundPolicy.GiftDomainPurchase:
 			text = translate(
-				'Please note: to receive a {{refundsSupportPage}}refund{{/refundsSupportPage}} for a domain gift, you must request the refund within 96 hours of the gift purchase',
-				{ components: { cancelDomainSupportPage, refundsSupportPage } }
+				'You understand that {{refundsSupportPage}}refunds{{/refundsSupportPage}} for a domain gift are limited to 96 hours after registration.',
+				{ components: { refundsSupportPage } }
 			);
 			break;
 
