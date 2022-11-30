c6206c20a266cbf7590a68b17c2b4380e26f48c7
Yoav Farhi <yoav@farhi.org>
2022-11-30 12:17:38 +0200

    Check for the existence of the chat key in the isPresalesZendeskChatEligible condition
    Co-authored-by: Igor Klimer <klimeryk@users.noreply.github.com>

--- client/my-sites/checkout/composite-checkout/components/checkout-help-link.tsx
+++ client/my-sites/checkout/composite-checkout/components/checkout-help-link.tsx
@@ -175,7 +175,7 @@ export default function CheckoutHelpLink() {
 	};
 
 	const zendeskPresalesChatKey: string | false = config( 'zendesk_presales_chat_key' );
-	const isPresalesZendeskChatEligible = presalesZendeskChatAvailable && isEnglishLocale;
+	const isPresalesZendeskChatEligible = presalesZendeskChatAvailable && isEnglishLocale && zendeskPresalesChatKey;
 
 	const hasDirectSupport = supportVariation !== SUPPORT_FORUM;
 
