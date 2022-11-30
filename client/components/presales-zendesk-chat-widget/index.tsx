e8fd340d2b9081c5a249ae8723915a9dc4334f5e
elliottprogrammer <bryan@elliottprogrammer.com>
2022-11-29 22:05:23 -0500

    Add re-usable Zendesk Chat widget (component)

---
+++ client/components/presales-zendesk-chat-widget/index.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+
+interface Props {
+	chatId: string | false;
+}
+
+const ZendeskChat = ( { chatId }: Props ) => {
+	useEffect( () => {
+		if ( ! chatId ) {
+			return;
+		}
+
+		const script = document.createElement( 'script' );
+		script.src = 'https://static.zdassets.com/ekr/snippet.js?key=' + chatId;
+		script.type = 'text/javascript';
+		script.id = 'ze-snippet';
+
+		const container = document.getElementById( 'zendesk-chat-container' );
+		if ( container ) {
+			container.appendChild( script );
+		}
+	}, [ chatId ] );
+
+	return <div id="zendesk-chat-container" />;
+};
+
+export default ZendeskChat;
