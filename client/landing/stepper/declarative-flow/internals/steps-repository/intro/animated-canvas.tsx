040f44f7250b6df188d2e15f6bde6ee92c806b45
Omar Alshaker <omar@omaralshaker.com>
2022-12-02 00:45:58 +0100

    More efficient version

--- client/landing/stepper/declarative-flow/internals/steps-repository/intro/animated-canvas.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/intro/animated-canvas.tsx
@@ -12,50 +12,52 @@ export function AnimatedCanvas( { className }: { className: string } ) {
 		if ( ! canvas ) {
 			return;
 		}
-		const ctx = canvas.getContext( '2d' );
+		const ctx = canvas.getContext( '2d' ) as CanvasRenderingContext2D;
 
 		canvas.width = width;
 		canvas.height = height;
 
-		const colors = [ '#2d4087', '#1c2b6e', '#425ca8' ];
-		const preRandomizedColors = Array.from(
-			{ length: height },
-			() => colors[ Math.floor( Math.random() * 3 ) ]
-		);
+		const colors = [ '#2d408755', '#1c536e54', '#425ca855' ];
 
 		let animationFactor = 1;
+
+		ctx.globalCompositeOperation = 'source-over';
+		ctx.fillStyle = '#141b3d';
+		ctx.fillRect( 0, 0, width, height );
 
 		function draw() {
 			if ( ctx && canvas ) {
 				ctx.globalCompositeOperation = 'source-over';
-				ctx.fillStyle = '#1a286c';
-				ctx.fillRect( 0, 0, canvas.width, canvas.height );
+				ctx.fillStyle = '#1a286c05';
+				ctx.fillRect( 0, 0, width, height );
 
 				ctx.globalCompositeOperation = 'multiply';
-
-				for ( let j = 0; j < height; j += 3 ) {
-					ctx.strokeStyle = preRandomizedColors[ j ];
-					ctx.lineWidth = j % 15;
+				let index = 0;
+				for ( let j = 0; j < height; j += height / 30 ) {
+					ctx.strokeStyle = colors[ index % 3 ];
+					ctx.lineWidth = ( 1 + Math.sin( index ) ) * 5;
 					ctx.beginPath();
-					for ( let x = 0; x < width + 50; x += 30 ) {
+					for ( let x = 0; x < width + 150; x += width / 100 ) {
 						const y1 =
 							j +
-							Math.sin( ( x + animationFactor * width ) / 100 ) * 30 +
+							Math.sin( ( x + animationFactor * width ) / 100 ) * ( height / 15 ) +
 							( 1 + Math.sin( animationFactor ) ) * 5;
 						const y2 = Math.sqrt( j * x * ( 1 + 2 * Math.sin( animationFactor ) ) ) - x / 2;
 						const y = y1 + y2;
-						ctx.lineTo( x, y );
+						ctx.lineTo( x - 100, y );
 					}
 					ctx.stroke();
 				}
+
 				animationFactor += 0.001;
-				animationRef.current = requestAnimationFrame( draw );
+				animationRef.current = window.setTimeout( draw, 70, false );
+				index = ( index + 0.2 ) % 3;
 			}
 		}
 
-		animationRef.current = requestAnimationFrame( draw );
+		draw();
 
-		return () => cancelAnimationFrame( animationRef.current );
+		return () => clearTimeout( animationRef.current );
 	}, [ width, height ] );
 
 	return <canvas className={ className } ref={ canvasRef } />;
