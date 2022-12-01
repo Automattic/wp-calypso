4e73621d4baaaf10aff19c9f8bbdd323ca5a0a8b
Dave Martin <davemart-in@users.noreply.github.com>
2022-12-01 14:08:31 -0500

    Add line clamping to right sidebar

--- client/reader/stream/reader-sidebar-followed-sites/item.jsx
+++ client/reader/stream/reader-sidebar-followed-sites/item.jsx
@@ -51,7 +51,7 @@ const ReaderSidebarFollowingItem = ( props ) => {
 				<Favicon site={ site } className="reader-sidebar-site_siteicon" size={ 32 } />
 
 				<span className="reader-sidebar-site_sitename">
-					{ site.name || formatUrlForDisplay( site.URL ) }
+					<span>{ site.name || formatUrlForDisplay( site.URL ) }</span>
 					<span className="reader-sidebar-site_updated">
 						{ site.last_updated > 0 && moment( new Date( site.last_updated ) ).fromNow() }
 					</span>
