d1889e048438890f38b16de476b5a70956c8da7b
Copons <Copons@users.noreply.github.com>
2022-12-02 12:24:38 +0000

    Also track the preview toggle button

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-global-styles/wpcom-global-styles-view.js
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-global-styles/wpcom-global-styles-view.js
@@ -2,30 +2,34 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 import './wpcom-global-styles-view.scss';
 
+function recordEvent( button ) {
+	recordTracksEvent( 'wpcom_launchbar_button_click', {
+		button,
+		blog_id: launchBarUserData?.blogId,
+	} );
+}
+
 ( function () {
 	const popoverToggle = document.querySelector( '.launch-bar-global-styles-toggle' );
 	const popover = document.querySelector( '.launch-bar-global-styles-popover' );
 	const upgradeButton = document.querySelector( '.launch-bar-global-styles-upgrade-button' );
+	const previewButton = document.querySelector( '.launch-bar-global-styles-preview-link' );
 
 	popoverToggle?.addEventListener( 'click', ( event ) => {
 		event.preventDefault();
-
-		recordTracksEvent( 'wpcom_launchbar_button_click', {
-			button: 'wpcom_global_styles_gating_notice',
-			blog_id: launchBarUserData?.blogId,
-		} );
-
+		recordEvent( 'wpcom_global_styles_gating_notice' );
 		popover?.classList.toggle( 'hidden' );
 	} );
 
 	upgradeButton?.addEventListener( 'click', ( event ) => {
 		event.preventDefault();
-
-		recordTracksEvent( 'wpcom_launchbar_button_click', {
-			button: 'wpcom_global_styles_gating_notice_upgrade',
-			blog_id: launchBarUserData?.blogId,
-		} );
-
+		recordEvent( 'wpcom_global_styles_gating_notice_upgrade' );
 		window.location = upgradeButton.href;
+	} );
+
+	previewButton?.addEventListener( 'click', ( event ) => {
+		event.preventDefault();
+		recordEvent( 'wpcom_global_styles_gating_notice_preview' );
+		window.location = previewButton.href;
 	} );
 } )();
