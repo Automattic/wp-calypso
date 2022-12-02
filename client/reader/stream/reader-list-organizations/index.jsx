5840503688b7cdf1ecb97121f0dacacc907c889a
Dave Martin <davemart-in@users.noreply.github.com>
2022-12-02 12:36:18 -0500

    Added organizations list to right sidebar

---
+++ client/reader/stream/reader-list-organizations/index.jsx
@@ -0,0 +1,59 @@
+import { localize } from 'i18n-calypso';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import ReaderListOrganizationsList from './list';
+
+export class ReaderListOrganizations extends Component {
+	static propTypes = {
+		organizations: PropTypes.array.isRequired,
+		path: PropTypes.string.isRequired,
+	};
+
+	fetchOrganization() {
+		const { organizations, path } = this.props;
+		const slug = path.split( '/' )[ 2 ];
+		let organization = organizations[ 0 ];
+
+		for ( let i = 0; i < organizations.length; i++ ) {
+			if ( organizations[ i ].slug === slug ) {
+				organization = organizations[ i ];
+				break;
+			}
+		}
+
+		return organization;
+	}
+
+	renderItems() {
+		const { path, translate } = this.props;
+		const organization = this.fetchOrganization();
+		// Note: No need to translate 'a8c', but 'p2' is a brand so it should be translated
+		const organizationSlug = organization.slug === 'a8c' ? 'a8c' : translate( 'p2' );
+
+		return (
+			<>
+				<h2>
+					{ translate( 'Following (%s)', { args: organizationSlug } ) }
+					<a href="/following/mark-all">{ translate( 'Mark all as seen' ) }</a>
+				</h2>
+				<ReaderListOrganizationsList
+					key={ organization.id }
+					path={ path }
+					organization={ organization }
+				/>
+			</>
+		);
+	}
+
+	render() {
+		const { organizations } = this.props;
+
+		if ( ! organizations ) {
+			return null;
+		}
+
+		return this.renderItems();
+	}
+}
+
+export default localize( ReaderListOrganizations );
