8c35cb64ad17760b36bb94d475a9f6e69a40b1e3
Robert Sreberski <robertsreberski@gmail.com>
2022-12-01 22:17:45 -0600

    Redesign props structure

--- packages/privacy-toolset/src/do-not-sell-dialog/index.tsx
+++ packages/privacy-toolset/src/do-not-sell-dialog/index.tsx
@@ -1,56 +1,49 @@
 import { FormToggle } from '@wordpress/components';
-import type { ChangeEvent } from 'react';
+
 import './styles.scss';
 
 export type DoNotSellDialogProps = {
-	title: string;
-	content: ( string | React.ReactNode )[];
-	toggleLabel: string;
-	closeButtonText: string;
+	content: {
+		title: string;
+		longDescription: ( string | React.ReactNode )[];
+		toggleLabel: string;
+		closeButton: string;
+	};
 	onClose: () => void;
-	isDoNotSellActive?: boolean;
-	onToggleDoNotSellSetting: ( isDoNotSellActive: boolean ) => void;
+	isActive?: boolean;
+	onToggleActive: ( isActive: boolean ) => void;
 };
 export const DoNotSellDialog = ( {
-	title,
 	content,
-	toggleLabel,
-	closeButtonText,
 	onClose,
-	isDoNotSellActive,
-	onToggleDoNotSellSetting,
-}: DoNotSellDialogProps ) => {
-	const handleChange = ( ev: ChangeEvent< HTMLInputElement > ) => {
-		onToggleDoNotSellSetting( ev.target.checked );
-	};
-
-	return (
-		<div className="do-not-sell">
-			<div className="do-not-sell__overlay" />
-			<div className="do-not-sell__dialog">
-				<div className="do-not-sell__header">
-					<h3 className="do-not-sell__title">{ title }</h3>
-					<button className="do-not-sell__close-button" onClick={ onClose } />
-				</div>
-				<div className="do-not-sell__content">
-					{ content.map( ( paragraph ) => (
-						<p>{ paragraph }</p>
-					) ) }
-				</div>
-				<label className="do-not-sell__preference">
-					<FormToggle
-						className="do-not-sell__checkbox"
-						onChange={ handleChange }
-						checked={ isDoNotSellActive }
-					/>
-					{ toggleLabel }
-				</label>
-				<div className="do-not-sell__footer">
-					<button className="do-not-sell__button" onClick={ onClose }>
-						{ closeButtonText }
-					</button>
-				</div>
+	isActive,
+	onToggleActive,
+}: DoNotSellDialogProps ) => (
+	<div className="do-not-sell">
+		<div className="do-not-sell__overlay" />
+		<div className="do-not-sell__dialog">
+			<div className="do-not-sell__header">
+				<h3 className="do-not-sell__title">{ content.title }</h3>
+				<button className="do-not-sell__close-button" onClick={ onClose } />
+			</div>
+			<div className="do-not-sell__content">
+				{ content.longDescription.map( ( paragraph, index ) => (
+					<p key={ `do-not-sell-${ index }` }>{ paragraph }</p>
+				) ) }
+			</div>
+			<label className="do-not-sell__preference">
+				<FormToggle
+					className="do-not-sell__checkbox"
+					onChange={ ( e ) => onToggleActive( e.currentTarget.checked ) }
+					checked={ isActive }
+				/>
+				{ content.toggleLabel }
+			</label>
+			<div className="do-not-sell__footer">
+				<button className="do-not-sell__button" onClick={ onClose }>
+					{ content.closeButton }
+				</button>
 			</div>
 		</div>
-	);
-};
+	</div>
+);
