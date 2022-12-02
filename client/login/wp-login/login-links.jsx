56fef3886f60cda9973c317d150e68daab3f9b8e
Michael Cain <michael@michaeldcain.com>
2022-12-01 21:40:41 -0500

    Persist redirect_to value for login page login links

--- client/login/wp-login/login-links.jsx
+++ client/login/wp-login/login-links.jsx
@@ -104,6 +104,7 @@ export class LoginLinks extends Component {
 			twoFactorAuthType: 'link',
 			signupUrl: this.props.query?.signup_url,
 			oauth2ClientId: this.props.oauth2ClientId,
+			redirect_to: this.props.query?.redirect_to,
 		};
 
 		if ( this.props.currentRoute === '/log-in/jetpack' ) {
@@ -342,6 +343,7 @@ export class LoginLinks extends Component {
 			<a
 				href={ addQueryArgs(
 					{
+						redirect_to: this.props.query?.redirect_to,
 						user_email: usernameOrEmail,
 					},
 					signupUrl
