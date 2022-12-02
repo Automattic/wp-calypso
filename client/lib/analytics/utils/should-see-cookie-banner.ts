bf2f95cd9728eefb1770418c32d301a1f227e359
Robert Sreberski <robertsreberski@gmail.com>
2022-12-02 09:23:48 -0600

    Fix rendering the banner on server side

--- client/lib/analytics/utils/should-see-cookie-banner.ts
+++ client/lib/analytics/utils/should-see-cookie-banner.ts
@@ -1,3 +1,4 @@
+import { isServer } from 'calypso/blocks/cookie-banner';
 import { isE2ETest } from 'calypso/lib/e2e';
 import { isWpMobileApp } from 'calypso/lib/mobile-app';
 import { TrackingPrefs } from './get-tracking-prefs';
@@ -21,7 +22,7 @@ export default function shouldSeeCookieBanner(
 	trackingPrefs?: TrackingPrefs
 ): boolean {
 	// the banner is not shown for pages embedded as web view inside the mobile app or during e2e tests
-	if ( isWpMobileApp() || isE2ETest() ) {
+	if ( isWpMobileApp() || ( ! isServer && isE2ETest() ) ) {
 		return false;
 	}
 
--- client/server/pages/index.js
+++ client/server/pages/index.js
@@ -99,7 +99,7 @@ function getDefaultContext( request, response, entrypoint = 'entry-main', sectio
 	performanceMark( request.context, 'getDefaultContext' );
 
 	const geoIPCountryCode = request.headers[ 'x-geoip-country-code' ];
-	const geoIPRegionName = 'unknown'; // We cannot get that information from the header at the moment (See: pMz3w-gsu-p2)
+	const geoIPRegionName = undefined; // We cannot get that information from the header at the moment (See: pMz3w-gsu-p2)
 	const trackingPrefs = parseTrackingPrefs(
 		request.cookies.sensitive_pixel_options,
 		request.cookies.sensitive_pixel_option
