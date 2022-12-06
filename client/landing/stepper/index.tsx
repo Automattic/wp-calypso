2ad878da9c52c5cb5032f88c7c001a89dc298c97
Robert Sreberski <robertsreberski@gmail.com>
2022-12-06 05:50:36 -0600

    Scope down the issue to GdprBanner import

--- client/landing/stepper/index.tsx
+++ client/landing/stepper/index.tsx
@@ -163,6 +163,9 @@ window.AppBoot = async () => {
 					<WindowLocaleEffectManager />
 					<BrowserRouter basename="setup">
 						<FlowSwitch user={ user as UserStore.CurrentUser } />
+						{ config.isEnabled( 'gdpr-banner' ) && (
+							<AsyncLoad require="calypso/blocks/gdpr-banner" placeholder={ null } />
+						) }
 						<AsyncLoad
 							require="calypso/components/global-notices"
 							placeholder={ null }
