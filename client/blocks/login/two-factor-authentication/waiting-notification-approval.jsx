e9e122f24fb71a1bfe139b3a0cef700c3dc84c96
Siobhan <siobhan@automattic.com>
2022-12-06 16:28:39 +0000

    Update confirmation prompt to reference Jetpack

--- client/blocks/login/two-factor-authentication/waiting-notification-approval.jsx
+++ client/blocks/login/two-factor-authentication/waiting-notification-approval.jsx
@@ -1,3 +1,4 @@
+import config from '@automattic/calypso-config';
 import { Card } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { Fragment } from 'react';
@@ -10,15 +11,22 @@ import './waiting-notification-approval.scss';
 export default function WaitingTwoFactorNotificationApproval( { switchTwoFactorAuthType } ) {
 	const translate = useTranslate();
 
+	const displayJetpackAppBranding = config.isEnabled( 'jetpack/app-branding' );
+
+	const confirmationPrompt = displayJetpackAppBranding
+		? translate(
+				'Notification sent! Confirm in your Jetpack or WordPress {{strong}}\u00A0mobile\u00A0app{{/strong}} to\u00A0continue.',
+				{ components: { strong: <strong /> } }
+		  )
+		: translate(
+				'Notification sent! Confirm in your {{strong}}WordPress\u00A0mobile\u00A0app{{/strong}} to\u00A0continue.',
+				{ components: { strong: <strong /> } }
+		  );
+
 	return (
 		<Fragment>
 			<Card compact>
-				<p className="two-factor-authentication__info">
-					{ translate(
-						'Notification sent! Confirm in your {{strong}}WordPress\u00A0mobile\u00A0app{{/strong}} to\u00A0continue.',
-						{ components: { strong: <strong /> } }
-					) }
-				</p>
+				<p className="two-factor-authentication__info">{ confirmationPrompt }</p>
 				<PushNotificationIllustration />
 			</Card>
 			<Divider>{ translate( 'or' ) }</Divider>
