512bd41a5731c17747e08beee9aecaabea742a83
Caroline Moore <calobee@gmail.com>
2022-12-06 08:49:55 -0500

    Remove subheader from design carousel step (#70782)

--- client/landing/stepper/declarative-flow/internals/steps-repository/design-carousel/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/design-carousel/index.tsx
@@ -39,9 +39,6 @@ const DesignCarousel: Step = function DesignCarousel( { navigation } ) {
 				<FormattedHeader
 					id="seller-step-header"
 					headerText={ __( 'Choose a design to start' ) }
-					subHeaderText={ __(
-						"Don't worry, you can change or customize this at any time in the future."
-					) }
 					align="center"
 				/>
 			}
