87e8e300a139b2e933d1d7a5ed01897c94bd452e
Maciej Grabowski <littlesoldiergraba@gmail.com>
2022-12-07 10:06:20 +0100

    Reading Settings: Adding 'Blogs post' setting to the new Reading Settings page

--- client/my-sites/site-settings/settings-reading/main.tsx
+++ client/my-sites/site-settings/settings-reading/main.tsx
@@ -1,13 +1,55 @@
 import config from '@automattic/calypso-config';
 import { useTranslate } from 'i18n-calypso';
+import { pick } from 'lodash';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
 import Main from 'calypso/components/main';
+import { SiteSettingsSection } from '../reading-site-settings';
+import wrapSettingsForm from '../wrap-settings-form';
 import { NewsletterSettingsSection } from './newsletter-settings-section';
 import { RssFeedSettingsSection } from './rss-feed-settings-section';
-import { SiteSettingsSection } from './site-settings-section';
 
 const isEnabled = config.isEnabled( 'settings/modernize-reading-settings' );
+
+// Settings are not typed yet, so we need to use `unknown` for now.
+type Settings = unknown;
+
+const getFormSettings = ( settings: Settings ) => pick( settings, [ 'posts_per_page' ] );
+
+type Fields = {
+	posts_per_page?: number;
+};
+
+type ReadingSettingsFormProps = {
+	fields: Fields;
+	onChangeField: ( field: string ) => ( event: React.ChangeEvent< HTMLInputElement > ) => void;
+	handleSubmitForm: ( event: React.FormEvent< HTMLFormElement > ) => void;
+	isRequestingSettings: boolean;
+	isSavingSettings: boolean;
+};
+
+const ReadingSettingsForm = wrapSettingsForm( getFormSettings )(
+	( {
+		fields,
+		onChangeField,
+		handleSubmitForm,
+		isRequestingSettings,
+		isSavingSettings,
+	}: ReadingSettingsFormProps ) => {
+		return (
+			<form onSubmit={ handleSubmitForm }>
+				<SiteSettingsSection
+					fields={ fields }
+					onChangeField={ onChangeField }
+					handleSubmitForm={ handleSubmitForm }
+					disabled={ isRequestingSettings || isSavingSettings }
+				/>
+				<RssFeedSettingsSection />
+				<NewsletterSettingsSection />
+			</form>
+		);
+	}
+);
 
 const ReadingSettings = () => {
 	const translate = useTranslate();
@@ -20,11 +62,7 @@ const ReadingSettings = () => {
 		<Main className="site-settings">
 			<DocumentHead title={ translate( 'Reading Settings' ) } />
 			<FormattedHeader brandFont headerText={ translate( 'Reading Settings' ) } align="left" />
-			<form>
-				<SiteSettingsSection />
-				<RssFeedSettingsSection />
-				<NewsletterSettingsSection />
-			</form>
+			<ReadingSettingsForm />
 		</Main>
 	);
 };
