25b1aa72cb98d1736f53d863837ca01e6e088220
Omar Alshaker <omar@omaralshaker.com>
2022-12-07 17:37:31 +0100

    Fix sites page when sites do not have titles

--- packages/sites/src/use-sites-list-sorting.tsx
+++ packages/sites/src/use-sites-list-sorting.tsx
@@ -158,8 +158,8 @@ function sortAlphabetically< T extends SiteDetailsForSorting >(
 	b: T,
 	sortOrder: SitesSortOrder
 ) {
-	const normalizedA = a.title.toLocaleLowerCase();
-	const normalizedB = b.title.toLocaleLowerCase();
+	const normalizedA = a.title?.toLocaleLowerCase();
+	const normalizedB = b.title?.toLocaleLowerCase();
 
 	if ( normalizedA > normalizedB ) {
 		return sortOrder === 'asc' ? 1 : -1;
