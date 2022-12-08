3dbba5169e2c82d8a46c2201028a9acb36e2da7f
Kev <kevin.zoschke@automattic.com>
2022-12-08 10:18:31 -0500

    Jetpack pricing: make prices screen reader friendly (#70719)

---
+++ client/components/jetpack/card/jetpack-product-card/display-price/price-aria-label.tsx
@@ -0,0 +1,47 @@
+import { formatCurrency } from '@automattic/format-currency';
+import { useTranslate } from 'i18n-calypso';
+import TimeFrame from './time-frame';
+import type { Duration } from 'calypso/my-sites/plans/jetpack-plans/types';
+
+interface Props {
+	currencyCode: string;
+	billingTerm: Duration;
+	isDiscounted: boolean;
+	finalPrice: number;
+	originalPrice?: number;
+	discountedPriceDuration?: number;
+}
+
+const formatOptions = { stripZeros: true };
+
+const DiscountedPriceLabel: React.FC< Props > = ( { originalPrice, finalPrice, currencyCode } ) => {
+	const translate = useTranslate();
+
+	return (
+		<>
+			{ translate( '%(finalPrice)s instead of %(originalPrice)s', {
+				args: {
+					finalPrice: formatCurrency( finalPrice, currencyCode, formatOptions ),
+					originalPrice: formatCurrency( originalPrice as number, currencyCode, formatOptions ),
+				},
+			} ) }
+		</>
+	);
+};
+
+export const PriceAriaLabel: React.FC< Props > = ( props ) => {
+	const { finalPrice, originalPrice, currencyCode, isDiscounted } = props;
+
+	return (
+		<span className="screen-reader-text">
+			{ isDiscounted && originalPrice ? (
+				<DiscountedPriceLabel { ...props } />
+			) : (
+				formatCurrency( finalPrice, currencyCode, formatOptions )
+			) }{ ' ' }
+			<TimeFrame { ...props } forScreenReader />
+		</span>
+	);
+};
+
+export default PriceAriaLabel;
