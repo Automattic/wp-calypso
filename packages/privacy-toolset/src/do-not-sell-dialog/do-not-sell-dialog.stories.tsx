5229c1c09229faea4ed62e2ad3e7d2f0e862eb8a
Robert Sreberski <robertsreberski@gmail.com>
2022-12-08 10:42:58 -0600

    Add aria roles, better longDescription prop, and content destruct

--- packages/privacy-toolset/src/do-not-sell-dialog/do-not-sell-dialog.stories.tsx
+++ packages/privacy-toolset/src/do-not-sell-dialog/do-not-sell-dialog.stories.tsx
@@ -16,11 +16,35 @@ export const Default: Story< DoNotSellDialogProps > = ( args ) => {
 Default.args = {
 	content: {
 		title: 'Do Not Sell or Share My Data',
-		longDescription: [
-			'Sed placerat, orci ullamcorper varius interdum, diam lectus vestibulum lacus, at varius nunc ipsum quis orci. Ut dui libero, ornare at luctus ac, bibendum sit amet risus. Donec tempus nisl nec arcu feugiat, sit amet fermentum velit varius. Nam sed malesuada enim, eu lobortis neque. Cras metus diam, sollicitudin non diam eu, bibendum tempor eros. Donec vehicula venenatis nunc semper pharetra. Nunc tristique nisl id tristique dictum.',
-			'Sed auctor est nec luctus egestas. Sed tincidunt auctor fringilla. In eleifend diam sed egestas semper. Donec vitae condimentum urna. Morbi non consectetur purus. Etiam faucibus, nunc id laoreet dapibus, arcu nisi sollicitudin turpis, sit amet iaculis lacus nulla eget magna. Suspendisse sed iaculis erat. Duis sed sem vitae velit pharetra convallis elementum vitae nibh. Proin ornare nunc pellentesque posuere ultrices. Fusce lacinia ligula id condimentum semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque viverra fringilla vehicula.',
-			'Aenean euismod, sem id tincidunt aliquet, augue ipsum dignissim dui, finibus pretium tellus massa sit amet ipsum. Aliquam auctor magna et dui feugiat, et imperdiet enim cursus. Donec at nulla id dolor pharetra rutrum. Vivamus ornare, massa ac finibus laoreet, neque turpis vulputate ante, a fringilla felis ante a lectus. Suspendisse pulvinar nisi eget est laoreet, a maximus velit finibus. Ut sed venenatis justo. Proin lobortis feugiat aliquam. Aliquam sem velit, tempus vel scelerisque sit amet, suscipit a tellus. Nullam a tellus eros. Quisque ac fermentum felis. Nam sagittis finibus erat.',
-		],
+		longDescription: (
+			<>
+				<p>
+					Sed placerat, orci ullamcorper varius interdum, diam lectus vestibulum lacus, at varius
+					nunc ipsum quis orci. Ut dui libero, ornare at luctus ac, bibendum sit amet risus. Donec
+					tempus nisl nec arcu feugiat, sit amet fermentum velit varius. Nam sed malesuada enim, eu
+					lobortis neque. Cras metus diam, sollicitudin non diam eu, bibendum tempor eros. Donec
+					vehicula venenatis nunc semper pharetra. Nunc tristique nisl id tristique dictum.
+				</p>
+				<p>
+					Sed auctor est nec luctus egestas. Sed tincidunt auctor fringilla. In eleifend diam sed
+					egestas semper. Donec vitae condimentum urna. Morbi non consectetur purus. Etiam faucibus,
+					nunc id laoreet dapibus, arcu nisi sollicitudin turpis, sit amet iaculis lacus nulla eget
+					magna. Suspendisse sed iaculis erat. Duis sed sem vitae velit pharetra convallis elementum
+					vitae nibh. Proin ornare nunc pellentesque posuere ultrices. Fusce lacinia ligula id
+					condimentum semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+					viverra fringilla vehicula.
+				</p>
+				<p>
+					Aenean euismod, sem id tincidunt aliquet, augue ipsum dignissim dui, finibus pretium
+					tellus massa sit amet ipsum. Aliquam auctor magna et dui feugiat, et imperdiet enim
+					cursus. Donec at nulla id dolor pharetra rutrum. Vivamus ornare, massa ac finibus laoreet,
+					neque turpis vulputate ante, a fringilla felis ante a lectus. Suspendisse pulvinar nisi
+					eget est laoreet, a maximus velit finibus. Ut sed venenatis justo. Proin lobortis feugiat
+					aliquam. Aliquam sem velit, tempus vel scelerisque sit amet, suscipit a tellus. Nullam a
+					tellus eros. Quisque ac fermentum felis. Nam sagittis finibus erat.
+				</p>
+			</>
+		),
 		toggleLabel: 'Do Not Sell or Share My Data',
 		closeButton: 'Close',
 	},
--- packages/privacy-toolset/src/do-not-sell-dialog/index.tsx
+++ packages/privacy-toolset/src/do-not-sell-dialog/index.tsx
@@ -5,7 +5,7 @@ import './styles.scss';
 export type DoNotSellDialogProps = {
 	content: {
 		title: string;
-		longDescription: ( string | React.ReactNode )[];
+		longDescription: React.ReactNode;
 		toggleLabel: string;
 		closeButton: string;
 	};
@@ -18,32 +18,45 @@ export const DoNotSellDialog = ( {
 	onClose,
 	isActive,
 	onToggleActive,
-}: DoNotSellDialogProps ) => (
-	<div className="do-not-sell">
-		<div className="do-not-sell__overlay" />
-		<div className="do-not-sell__dialog">
-			<div className="do-not-sell__header">
-				<h3 className="do-not-sell__title">{ content.title }</h3>
-				<button className="do-not-sell__close-button" onClick={ onClose } />
-			</div>
-			<div className="do-not-sell__content">
-				{ content.longDescription.map( ( paragraph, index ) => (
-					<p key={ `do-not-sell-${ index }` }>{ paragraph }</p>
-				) ) }
-			</div>
-			<label className="do-not-sell__preference">
-				<FormToggle
-					className="do-not-sell__checkbox"
-					onChange={ ( e ) => onToggleActive( e.currentTarget.checked ) }
-					checked={ isActive }
-				/>
-				{ content.toggleLabel }
-			</label>
-			<div className="do-not-sell__footer">
-				<button className="do-not-sell__button" onClick={ onClose }>
-					{ content.closeButton }
-				</button>
+}: DoNotSellDialogProps ) => {
+	const { title, longDescription, toggleLabel, closeButton } = content;
+	return (
+		<div
+			className="do-not-sell"
+			role="dialog"
+			aria-modal="true"
+			aria-labelledby="do-not-sell-title"
+			aria-describedby="do-not-sell-description"
+		>
+			<div className="do-not-sell__overlay" />
+			<div className="do-not-sell__dialog">
+				<div className="do-not-sell__header">
+					<h3 className="do-not-sell__title" id="do-not-sell-title">
+						{ title }
+					</h3>
+					<button
+						className="do-not-sell__close-button"
+						aria-label="Close dialog"
+						onClick={ onClose }
+					/>
+				</div>
+				<div className="do-not-sell__content" id="do-not-sell-description">
+					{ longDescription }
+				</div>
+				<label className="do-not-sell__preference">
+					<FormToggle
+						className="do-not-sell__checkbox"
+						onChange={ ( e ) => onToggleActive( e.currentTarget.checked ) }
+						checked={ isActive }
+					/>
+					{ toggleLabel }
+				</label>
+				<div className="do-not-sell__footer">
+					<button className="do-not-sell__button" onClick={ onClose }>
+						{ closeButton }
+					</button>
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	);
+};
