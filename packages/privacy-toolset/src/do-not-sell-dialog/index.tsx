04ff975e074f0f0ba4f76e7c56bff2e823248374
Robert Sreberski <robertsreberski@gmail.com>
2022-12-09 09:29:41 -0600

    Improve handling props on Do Not Sell React Modal

--- packages/privacy-toolset/src/do-not-sell-dialog/index.tsx
+++ packages/privacy-toolset/src/do-not-sell-dialog/index.tsx
@@ -1,6 +1,7 @@
 import { FormToggle } from '@wordpress/components';
 import { FormEventHandler } from 'react';
 import Modal from 'react-modal';
+import type { Props as ModalProps } from 'react-modal';
 
 import './styles.scss';
 
@@ -15,6 +16,10 @@ export type DoNotSellDialogProps = {
 	onClose: () => void;
 	isActive?: boolean;
 	onToggleActive: ( isActive: boolean ) => void;
+	modalProps: Omit<
+		ModalProps,
+		'className' | 'overlayClassName' | 'aria' | 'onRequestClose' | 'isOpen' | 'children'
+	>;
 };
 export const DoNotSellDialog = ( {
 	content,
@@ -22,20 +27,23 @@ export const DoNotSellDialog = ( {
 	onClose,
 	isActive,
 	onToggleActive,
+	modalProps,
 }: DoNotSellDialogProps ) => {
 	const { title, longDescription, toggleLabel, closeButton } = content;
 	const onFormToggleChange: FormEventHandler< HTMLInputElement > = ( e ) =>
 		onToggleActive( e.currentTarget.checked );
 	return (
 		<Modal
-			className="do-not-sell__dialog"
+			isOpen={ isOpen }
+			onRequestClose={ onClose }
 			overlayClassName="do-not-sell"
+			className="do-not-sell__dialog"
 			aria={ {
 				modal: true,
 				labelledby: 'do-not-sell-title',
+				describedby: 'do-not-sell-description',
 			} }
-			onRequestClose={ onClose }
-			isOpen={ isOpen }
+			{ ...modalProps }
 		>
 			<div className="do-not-sell__header">
 				<h3 className="do-not-sell__title" id="do-not-sell-title">
