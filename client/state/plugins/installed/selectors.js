9f9e6a727c5dd845f58b16e8ac78c84808845c42
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2022-12-09 15:45:35 -0800

    Add isRequesting to dependencies

--- client/state/plugins/installed/selectors.js
+++ client/state/plugins/installed/selectors.js
@@ -112,6 +112,11 @@ export const getPlugins = createSelector(
 		isRequestingForAllSites( state ),
 		...siteIds.map( ( siteId ) => isRequesting( state, siteId ) ),
 	],
+	( state, siteIds ) => [
+		state.plugins.installed.plugins,
+		isRequestingForAllSites( state ),
+		...siteIds.map( ( siteId ) => isRequesting( state, siteId ) ),
+	],
 	( state, siteIds, pluginFilter ) => {
 		return [ siteIds, pluginFilter ].flat().join( '-' );
 	}
