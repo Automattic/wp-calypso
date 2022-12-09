6349acd06d8348c3e8251cbfced44e2566c4a0a9
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2022-12-09 11:29:49 +0100

    Add Featured image email toggle to the new Reading settings page

---
+++ client/my-sites/site-settings/reading-newsletter-settings/FeaturedImageEmailSetting.tsx
@@ -0,0 +1,35 @@
+import { ToggleControl } from '@wordpress/components';
+import { useTranslate } from 'i18n-calypso';
+import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
+
+export const FEATURED_IMAGE_EMAIL_OPTION = 'featured_image_email_enabled';
+
+type FeaturedImageEmailSettingProps = {
+	value?: boolean;
+	handleToggle: ( field: string ) => ( ( isChecked: boolean ) => void ) | undefined;
+	disabled?: boolean;
+};
+
+export const FeaturedImageEmailSetting = ( {
+	value = false,
+	handleToggle,
+	disabled,
+}: FeaturedImageEmailSettingProps ) => {
+	const translate = useTranslate();
+
+	return (
+		<div className="featured-image-template-toggle-settings">
+			<ToggleControl
+				checked={ !! value }
+				onChange={ handleToggle( FEATURED_IMAGE_EMAIL_OPTION ) }
+				disabled={ disabled }
+				label={ translate( 'Enable featured image on your new post emails' ) }
+			/>
+			<FormSettingExplanation>
+				{ translate(
+					"Includes your post's featured image in the email sent out to your readers."
+				) }
+			</FormSettingExplanation>
+		</div>
+	);
+};
