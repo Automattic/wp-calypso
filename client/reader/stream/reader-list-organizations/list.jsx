b134d3ad3689025e8d814f8ceea11240a64a1f1b
Dave Martin <davemart-in@users.noreply.github.com>
2022-12-09 08:05:26 -0500

    Relocate organizations lists from left nav to right sidebar (#70783)
    * Remove followed sites dropdown from left nav
    
    * Added right sidebar for followed sites
    
    * Add line clamping to right sidebar
    
    * Added link to load more sites if you have more than 25 sites that you're following
    
    * Updated "load more sites" link styles
    
    * Removed unnecessary wrapWithMain
    
    * Remove left nav org drop downs & refactored reader-sidebar-followed-sites as reader-list-followed-sites
    
    * Added organizations list to right sidebar
    
    * Update div to ul
    
    * Removed Mark all as seen temp link
    
    * Fix bug when organization.slug isn't available
    
    * Moved org slug translation to a separate function
    
    * Refactored organization fetch function based on feedback from @tyxla
    
    * Removed slug from right sidebar header
    
    * Make sure feed doesn't show right sidebar and prevent error when organization doesn't exist

--- client/reader/stream/reader-list-organizations/list.jsx
+++ client/reader/stream/reader-list-organizations/list.jsx
@@ -1,23 +1,28 @@
 import { localize } from 'i18n-calypso';
+import { map } from 'lodash';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import AutomatticLogo from 'calypso/assets/images/icons/a8c-logo.svg';
 import P2Logo from 'calypso/assets/images/icons/p2-logo.svg';
 import SVGIcon from 'calypso/components/svg-icon';
-import SidebarItem from 'calypso/layout/sidebar/item';
-import ReaderSidebarHelper from 'calypso/reader/sidebar/helper';
 import { toggleReaderSidebarOrganization } from 'calypso/state/reader-ui/sidebar/actions';
+import { isOrganizationOpen } from 'calypso/state/reader-ui/sidebar/selectors';
 import getOrganizationSites from 'calypso/state/reader/follows/selectors/get-reader-follows-organization';
 import { AUTOMATTIC_ORG_ID } from 'calypso/state/reader/organizations/constants';
+import ReaderListOrganizationsListItem from './list-item';
 import '../style.scss';
 
-export class ReaderSidebarOrganizationsList extends Component {
+export class ReaderListOrganizationsList extends Component {
 	static propTypes = {
 		path: PropTypes.string.isRequired,
 		organization: PropTypes.object,
 		sites: PropTypes.array,
 		teams: PropTypes.array,
+	};
+
+	handleClick = () => {
+		this.props.toggleReaderSidebarOrganization( { organizationId: this.props.organization.id } );
 	};
 
 	renderIcon() {
@@ -28,32 +33,33 @@ export class ReaderSidebarOrganizationsList extends Component {
 		return <SVGIcon name="p2-logo" icon={ P2Logo } classes="sidebar__menu-icon" />;
 	}
 
+	renderSites() {
+		const { sites, path } = this.props;
+		return map(
+			sites,
+			( site ) =>
+				site && <ReaderListOrganizationsListItem key={ site.ID } path={ path } site={ site } />
+		);
+	}
+
 	render() {
-		const { organization, path } = this.props;
+		const { organization } = this.props;
 
 		if ( ! organization.sites_count ) {
 			return null;
 		}
-		return (
-			<SidebarItem
-				className={ ReaderSidebarHelper.itemLinkClass( '/read/' + organization.slug, path, {
-					'sidebar-streams__all': true,
-				} ) }
-				label={ organization.title }
-				link={ '/read/' + organization.slug }
-				customIcon={ this.renderIcon() }
-			/>
-		);
+		return <ul>{ this.renderSites() }</ul>;
 	}
 }
 
 export default connect(
 	( state, ownProps ) => {
 		return {
+			isOrganizationOpen: isOrganizationOpen( state, ownProps.organization.id ),
 			sites: getOrganizationSites( state, ownProps.organization.id ), // get p2 network organizations
 		};
 	},
 	{
 		toggleReaderSidebarOrganization,
 	}
-)( localize( ReaderSidebarOrganizationsList ) );
+)( localize( ReaderListOrganizationsList ) );
