cee0638b791d38b2aa68984bee5348d9eea4094d
Jason Moon <jason.moon@automattic.com>
2022-12-09 16:01:58 -0800

    Add more examples

--- packages/components/src/shortened-number/stories/index.stories.jsx
+++ packages/components/src/shortened-number/stories/index.stories.jsx
@@ -4,15 +4,18 @@ export default { title: 'Shortened Number' };
 
 const ShortenedNumberExample = ( { value } ) => (
 	<p>
-		{ value } becomes <ShortenedNumber value={ value } />
+		{ String( value ) } becomes <ShortenedNumber value={ value } />
 	</p>
 );
 
 export const Default = () => (
 	<div>
+		<ShortenedNumberExample value={ 0 } />
 		<ShortenedNumberExample value={ 123 } />
 		<ShortenedNumberExample value={ 12345 } />
 		<ShortenedNumberExample value={ 12345678 } />
 		<ShortenedNumberExample value={ 12345678901 } />
+		<ShortenedNumberExample value={ 1.23456 } />
+		<ShortenedNumberExample value="Apple" />
 	</div>
 );
