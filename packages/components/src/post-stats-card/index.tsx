981607c96083c3048dde1f071623c10daf5591a1
Jason Moon <jason.moon@automattic.com>
2022-12-09 17:42:22 -0800

    Update mobile layout and spacing

--- packages/components/src/post-stats-card/index.tsx
+++ packages/components/src/post-stats-card/index.tsx
@@ -39,17 +39,17 @@ export default function PostStatsCard( {
 				) }
 			</div>
 			<div className="post-stats-card__counts">
-				<div className="post-stats-card__view-count">
+				<div className="post-stats-card__count post-stats-card__count--view">
 					<div className="post-stats-card__count-header">Views</div>
 					{ /* TODO: Use shortened numbers */ }
 					<div className="post-stats-card__count-value">{ viewCount }</div>
 				</div>
-				<div className="post-stats-card__like-count">
+				<div className="post-stats-card__count post-stats-card__count--like">
 					<div className="post-stats-card__count-header">Likes</div>
 					{ /* TODO: Use shortened numbers */ }
 					<div className="post-stats-card__count-value">{ likeCount }</div>
 				</div>
-				<div className="post-stats-card__comment-count">
+				<div className="post-stats-card__count post-stats-card__count--comment">
 					<div className="post-stats-card__count-header">Comments</div>
 					{ /* TODO: Use shortened numbers */ }
 					<div className="post-stats-card__count-value">{ commentCount }</div>
