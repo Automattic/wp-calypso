1b7afee96406a4b3a83ff78429a83e398abb14c8
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2022-12-12 11:45:09 +0100

    Define subscribers and team-members types

---
+++ client/my-sites/people/subscribers/types.ts
@@ -0,0 +1,23 @@
+export type Follower = {
+	ID: number | string;
+	url?: string;
+	label: string;
+	login: string;
+	avatar: string;
+	avatar_URL: string;
+	date_subscribed: string;
+	follow_data?: any;
+};
+
+export type FollowersQueryData = {
+	followers: Follower[];
+	total: number;
+};
+
+export type FollowersQuery = {
+	data?: FollowersQueryData;
+	hasNextPage: boolean;
+	refetch: () => void;
+	fetchNextPage: () => void;
+	isFetchingNextPage: boolean;
+};
