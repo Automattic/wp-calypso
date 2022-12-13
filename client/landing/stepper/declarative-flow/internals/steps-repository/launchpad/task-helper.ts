8e3f5788237df23af3d8727ab7d27dd2a58b60d1
Dan Roundhill <dan@automattic.com>
2022-12-13 08:41:31 -0700

    VideoPress Tailored Onboarding: Update launchpad plan step to encourage plan purchase (#71026)
    * Add support for displaying a message that a VideoPress supported plan is required before uploading a video step.
    
    * Fix highlight of VideoPress row in plans page. Copy updates as well.
    
    * Fix variable name after rebase, and only fire global styles tracks event if not VideoPress flow.
    
    * Keep the `displayGlobalStylesWarning` separate from the VideoPress specific code for easier readability and flexibility. h/t @jgcaruso for the idea.

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-helper.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-helper.ts
@@ -1,4 +1,9 @@
-import { PLAN_PREMIUM, FEATURE_ADVANCED_DESIGN_CUSTOMIZATION } from '@automattic/calypso-products';
+import {
+	FEATURE_VIDEO_UPLOADS,
+	planHasFeature,
+	PLAN_PREMIUM,
+	FEATURE_ADVANCED_DESIGN_CUSTOMIZATION,
+} from '@automattic/calypso-products';
 import { dispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import { addQueryArgs } from '@wordpress/url';
@@ -45,6 +50,23 @@ export function getEnhancedTasks(
 		? `/page/${ siteSlug }/${ homePageId }`
 		: `/site-editor/${ siteSlug }`;
 
+	let planWarningText = displayGlobalStylesWarning
+		? translate(
+				'Your site contains custom colors that will only be visible once you upgrade to a Premium plan.'
+		  )
+		: '';
+
+	const isVideoPressFlowWithUnsupportedPlan =
+		isVideoPressFlow( flow ) && ! planHasFeature( productSlug as string, FEATURE_VIDEO_UPLOADS );
+
+	if ( isVideoPressFlowWithUnsupportedPlan ) {
+		planWarningText = translate(
+			'Upgrade to a plan with VideoPress support to upload your videos.'
+		);
+	}
+
+	const shouldDisplayWarning = displayGlobalStylesWarning || isVideoPressFlowWithUnsupportedPlan;
+
 	tasks &&
 		tasks.map( ( task ) => {
 			let taskData = {};
@@ -78,12 +100,7 @@ export function getEnhancedTasks(
 				case 'plan_selected':
 					taskData = {
 						title: translate( 'Choose a Plan' ),
-						subtitle: displayGlobalStylesWarning
-							? translate(
-									'Your site contains custom colors that will only be visible once you upgrade to a Premium plan.'
-							  )
-							: '',
-						disabled: isVideoPressFlow( flow ),
+						subtitle: planWarningText,
 						actionDispatch: () => {
 							recordTaskClickTracksEvent( flow, task.completed, task.id );
 							if ( displayGlobalStylesWarning ) {
@@ -92,16 +109,18 @@ export function getEnhancedTasks(
 								);
 							}
 							const plansUrl = addQueryArgs( `/plans/${ siteSlug }`, {
-								...( displayGlobalStylesWarning && {
+								...( shouldDisplayWarning && {
 									plan: PLAN_PREMIUM,
-									feature: FEATURE_ADVANCED_DESIGN_CUSTOMIZATION,
+									feature: isVideoPressFlowWithUnsupportedPlan
+										? FEATURE_VIDEO_UPLOADS
+										: FEATURE_ADVANCED_DESIGN_CUSTOMIZATION,
 								} ),
 							} );
 							window.location.assign( plansUrl );
 						},
-						badgeText: translatedPlanName,
-						completed: task.completed && ! displayGlobalStylesWarning,
-						warning: displayGlobalStylesWarning,
+						badgeText: isVideoPressFlowWithUnsupportedPlan ? null : translatedPlanName,
+						completed: task.completed && ! shouldDisplayWarning,
+						warning: shouldDisplayWarning,
 					};
 					break;
 				case 'subscribers_added':
@@ -214,7 +233,7 @@ export function getEnhancedTasks(
 						title: translate( 'Upload your first video' ),
 						actionUrl: launchpadUploadVideoLink,
 						completed: videoPressUploadCompleted,
-						disabled: videoPressUploadCompleted,
+						disabled: isVideoPressFlowWithUnsupportedPlan || videoPressUploadCompleted,
 						actionDispatch: () => {
 							recordTaskClickTracksEvent( flow, task.completed, task.id );
 							window.location.replace( launchpadUploadVideoLink );
