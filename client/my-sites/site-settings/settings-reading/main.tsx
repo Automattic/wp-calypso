3dc58cf91d4dd20712f28e669fb2dacff802c206
Maciej Grabowski <mashikag@users.noreply.github.com>
2022-12-13 16:40:26 +0100

    Reading Settings: Add "Syndication feeds" setting (#70944)
    * Reading Settings: Add 'Syndication feeds' setting to the new Reading Settings page
    
    * Update client/my-sites/site-settings/reading-rss-feed-settings/SyndicationFeedsSetting.tsx
    
    Co-authored-by: Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
    
    * Adding site's feed hyperlink to the setting's explanation note
    
    * Update client/my-sites/site-settings/reading-rss-feed-settings/index.tsx
    
    Co-authored-by: Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
    
    * Add max value of 350 feed posts. This is the max value enforced by the backend
    
    * Rename isSaving to isSavingSettings
    
    * Remove unused client/state/selectors/get-site-feed-url.ts
    
    Co-authored-by: Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>

--- client/my-sites/site-settings/settings-reading/main.tsx
+++ client/my-sites/site-settings/settings-reading/main.tsx
@@ -1,12 +1,15 @@
 import config from '@automattic/calypso-config';
 import { useTranslate } from 'i18n-calypso';
+import { connect } from 'react-redux';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
 import Main from 'calypso/components/main';
+import { getSiteUrl } from 'calypso/state/sites/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { NewsletterSettingsSection } from '../reading-newsletter-settings';
+import { RssFeedSettingsSection } from '../reading-rss-feed-settings';
 import { SiteSettingsSection } from '../reading-site-settings';
 import wrapSettingsForm from '../wrap-settings-form';
-import { RssFeedSettingsSection } from './rss-feed-settings-section';
 
 const isEnabled = config.isEnabled( 'settings/modernize-reading-settings' );
 
@@ -19,15 +22,25 @@ const getFormSettings = ( settings: Settings = {} ) => {
 	}
 
 	// @ts-expect-error Settings are not typed yet, so we need to use `unknown` for now.
-	const { posts_per_page, featured_image_email_enabled } = settings;
+	const { posts_per_page, posts_per_rss, featured_image_email_enabled } = settings;
 	return {
 		...( posts_per_page && { posts_per_page } ),
+		...( posts_per_rss && { posts_per_rss } ),
 		...( featured_image_email_enabled && { featured_image_email_enabled } ),
 	};
 };
 
+const connectComponent = connect( ( state ) => {
+	const siteId = getSelectedSiteId( state );
+	const siteUrl = siteId && getSiteUrl( state, siteId );
+	return {
+		...( siteUrl && { siteUrl } ),
+	};
+} );
+
 type Fields = {
 	posts_per_page?: number;
+	posts_per_rss?: number;
 	featured_image_email_enabled?: boolean;
 };
 
@@ -38,36 +51,48 @@ type ReadingSettingsFormProps = {
 	handleSubmitForm: ( event: React.FormEvent< HTMLFormElement > ) => void;
 	isRequestingSettings: boolean;
 	isSavingSettings: boolean;
+	siteUrl?: string;
 };
 
 const ReadingSettingsForm = wrapSettingsForm( getFormSettings )(
-	( {
-		fields,
-		onChangeField,
-		handleToggle,
-		handleSubmitForm,
-		isRequestingSettings,
-		isSavingSettings,
-	}: ReadingSettingsFormProps ) => {
-		return (
-			<form onSubmit={ handleSubmitForm }>
-				<SiteSettingsSection
-					fields={ fields }
-					onChangeField={ onChangeField }
-					handleSubmitForm={ handleSubmitForm }
-					disabled={ isRequestingSettings || isSavingSettings }
-				/>
-				<RssFeedSettingsSection />
-				<NewsletterSettingsSection
-					fields={ fields }
-					handleToggle={ handleToggle }
-					handleSubmitForm={ handleSubmitForm }
-					disabled={ isRequestingSettings || isSavingSettings }
-					isSavingSettings={ isSavingSettings }
-				/>
-			</form>
-		);
-	}
+	connectComponent(
+		( {
+			fields,
+			onChangeField,
+			handleSubmitForm,
+			handleToggle,
+			isRequestingSettings,
+			isSavingSettings,
+			siteUrl,
+		}: ReadingSettingsFormProps ) => {
+			const disabled = isRequestingSettings || isSavingSettings;
+			return (
+				<form onSubmit={ handleSubmitForm }>
+					<SiteSettingsSection
+						fields={ fields }
+						onChangeField={ onChangeField }
+						handleSubmitForm={ handleSubmitForm }
+						disabled={ disabled }
+					/>
+					<RssFeedSettingsSection
+						fields={ fields }
+						onChangeField={ onChangeField }
+						handleSubmitForm={ handleSubmitForm }
+						disabled={ disabled }
+						siteUrl={ siteUrl }
+						isSavingSettings={ isSavingSettings }
+					/>
+					<NewsletterSettingsSection
+						fields={ fields }
+						handleToggle={ handleToggle }
+						handleSubmitForm={ handleSubmitForm }
+						disabled={ disabled }
+						isSavingSettings={ isSavingSettings }
+					/>
+				</form>
+			);
+		}
+	)
 );
 
 const ReadingSettings = () => {
