073350922914beff2a5ae7b63e48f913e71f68eb
Jasper Kang <kangzjnet@gmail.com>
2022-12-13 17:04:59 +1300

    add jetpack.scss and move related style in

--- apps/odyssey-stats/src/app.jsx
+++ apps/odyssey-stats/src/app.jsx
@@ -18,6 +18,7 @@ import { setupContextMiddleware } from './page-middleware/setup-context';
 import registerStatsPages from './routes';
 
 import 'calypso/assets/stylesheets/style.scss';
+import './jetpack.scss';
 
 const setLocale = ( dispatch ) => {
 	const defaultLocale = config( 'i18n_default_locale_slug' ) || 'en';
