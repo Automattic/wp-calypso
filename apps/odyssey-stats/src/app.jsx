572a931b5fb913b51c77eef714285026eef2fc94
Jasper Kang <kangzjnet@gmail.com>
2022-12-13 19:46:17 +1300

    rename jetpack.scss -> wp-admin.scss

--- apps/odyssey-stats/src/app.jsx
+++ apps/odyssey-stats/src/app.jsx
@@ -18,7 +18,7 @@ import { setupContextMiddleware } from './page-middleware/setup-context';
 import registerStatsPages from './routes';
 
 import 'calypso/assets/stylesheets/style.scss';
-import './jetpack.scss';
+import './wp-admin.scss';
 
 const setLocale = ( dispatch ) => {
 	const defaultLocale = config( 'i18n_default_locale_slug' ) || 'en';
