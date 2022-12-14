91ea1bafa10480073e1c4752e5d00a36d6fc3b4b
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2022-12-14 21:08:02 +0800

    Send impression events to Tracks.
    Triggered based on the current view in the DotPager component.

--- client/my-sites/stats/mobile-promo-cards/index.jsx
+++ client/my-sites/stats/mobile-promo-cards/index.jsx
@@ -1,3 +1,4 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { translate } from 'i18n-calypso';
 import { MobilePromoCard } from 'calypso/../packages/components/src';
 import wordpressSeoIllustration from 'calypso/assets/images/illustrations/wordpress-seo-premium.svg';
@@ -8,9 +9,20 @@ import './style.scss';
 
 export default function MobilePromoCardWrapper( slug ) {
 	// do some stuff
-	const showMobileAppsPromo = true;
-	const showYoastPromo = true;
+	const showMobileAppsPromo = false;
+	const showYoastPromo = false;
 	const showBothPromoCards = true;
+	// handle view events here
+	const pagerDidSelectPage = ( index ) => {
+		const evenLookup = [
+			'calypso_stats_wordpress_seo_premium_banner_view',
+			'calypso_stats_traffic_mobile_cta_jetpack_view',
+		];
+		const eventName = evenLookup[ index ];
+		// console.log( `selected index: ${ index } -- event: ${ eventName }` );
+		// send an impression event
+		recordTracksEvent( eventName );
+	};
 	// render some stuff
 	return (
 		<>
@@ -41,7 +53,7 @@ export default function MobilePromoCardWrapper( slug ) {
 			{ showBothPromoCards && (
 				<div className="stats__promo-container">
 					<div className="stats__promo-card">
-						<DotPager className="stats__promo-pager">
+						<DotPager className="stats__promo-pager" onPageSelected={ pagerDidSelectPage }>
 							<div>
 								<PromoCardBlock
 									productSlug="wordpress-seo-premium"
