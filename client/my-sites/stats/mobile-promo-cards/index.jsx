57fd89f13e5d27d0b35ebfb94e7ce2ebaa8e3e8a
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2022-12-14 20:12:39 +0800

    Add MobilePromoCard to wrapper.
    Currently shows both cards separately without regard for environment.

--- client/my-sites/stats/mobile-promo-cards/index.jsx
+++ client/my-sites/stats/mobile-promo-cards/index.jsx
@@ -1,4 +1,5 @@
 import { translate } from 'i18n-calypso';
+import { MobilePromoCard } from 'calypso/../packages/components/src';
 import wordpressSeoIllustration from 'calypso/assets/images/illustrations/wordpress-seo-premium.svg';
 import PromoCardBlock from 'calypso/blocks/promo-card-block';
 
@@ -6,26 +7,35 @@ import './style.scss';
 
 export default function MobilePromoCardWrapper( slug ) {
 	// do some stuff
-	// const slug = 'hello-slug';
+	const showMobileAppsPromo = true;
+	const showYoastPromo = true;
 	// render some stuff
 	return (
 		<>
 			{ /** Promo Card is disabled for Odyssey because it doesn't make much sense in the context, which also removes an API call to `plugins`. */ }
-
-			<div className="stats-content-promo">
-				<PromoCardBlock
-					productSlug="wordpress-seo-premium"
-					impressionEvent="calypso_stats_wordpress_seo_premium_banner_view"
-					clickEvent="calypso_stats_wordpress_seo_premium_banner_click"
-					headerText={ translate( 'Increase site visitors with Yoast SEO Premium' ) }
-					contentText={ translate(
-						'Purchase Yoast SEO Premium to ensure that more people find your incredible content.'
-					) }
-					ctaText={ translate( 'Learn more' ) }
-					image={ wordpressSeoIllustration }
-					href={ `/plugins/wordpress-seo-premium/${ slug }` }
-				/>
-			</div>
+			{ showYoastPromo && (
+				<div className="stats-content-promo">
+					<PromoCardBlock
+						productSlug="wordpress-seo-premium"
+						impressionEvent="calypso_stats_wordpress_seo_premium_banner_view"
+						clickEvent="calypso_stats_wordpress_seo_premium_banner_click"
+						headerText={ translate( 'Increase site visitors with Yoast SEO Premium' ) }
+						contentText={ translate(
+							'Purchase Yoast SEO Premium to ensure that more people find your incredible content.'
+						) }
+						ctaText={ translate( 'Learn more' ) }
+						image={ wordpressSeoIllustration }
+						href={ `/plugins/wordpress-seo-premium/${ slug }` }
+					/>
+				</div>
+			) }
+			{ showMobileAppsPromo && (
+				<div className="stats__promo-container">
+					<div className="stats__promo-card">
+						<MobilePromoCard className="stats__promo-card-apps" />
+					</div>
+				</div>
+			) }
 		</>
 	);
 }
