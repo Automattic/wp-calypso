457796d4816a01937b228f159af603331f76811d
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2022-12-14 20:21:43 +0800

    Add DotPager component wrapping both cards.

--- client/my-sites/stats/mobile-promo-cards/index.jsx
+++ client/my-sites/stats/mobile-promo-cards/index.jsx
@@ -2,6 +2,7 @@ import { translate } from 'i18n-calypso';
 import { MobilePromoCard } from 'calypso/../packages/components/src';
 import wordpressSeoIllustration from 'calypso/assets/images/illustrations/wordpress-seo-premium.svg';
 import PromoCardBlock from 'calypso/blocks/promo-card-block';
+import DotPager from 'calypso/components/dot-pager';
 
 import './style.scss';
 
@@ -9,6 +10,7 @@ export default function MobilePromoCardWrapper( slug ) {
 	// do some stuff
 	const showMobileAppsPromo = true;
 	const showYoastPromo = true;
+	const showBothPromoCards = true;
 	// render some stuff
 	return (
 		<>
@@ -33,6 +35,31 @@ export default function MobilePromoCardWrapper( slug ) {
 				<div className="stats__promo-container">
 					<div className="stats__promo-card">
 						<MobilePromoCard className="stats__promo-card-apps" />
+					</div>
+				</div>
+			) }
+			{ showBothPromoCards && (
+				<div className="stats__promo-container">
+					<div className="stats__promo-card">
+						<DotPager className="stats__promo-pager">
+							<div>
+								<PromoCardBlock
+									productSlug="wordpress-seo-premium"
+									impressionEvent="calypso_stats_wordpress_seo_premium_banner_view"
+									clickEvent="calypso_stats_wordpress_seo_premium_banner_click"
+									headerText={ translate( 'Increase site visitors with Yoast SEO Premium' ) }
+									contentText={ translate(
+										'Purchase Yoast SEO Premium to ensure that more people find your incredible content.'
+									) }
+									ctaText={ translate( 'Learn more' ) }
+									image={ wordpressSeoIllustration }
+									href={ `/plugins/wordpress-seo-premium/${ slug }` }
+								/>
+							</div>
+							<div>
+								<MobilePromoCard className="stats__promo-card-apps" />
+							</div>
+						</DotPager>
 					</div>
 				</div>
 			) }
