da41b90dc938cb57de10c010a3dfe26ceb01f8db
DustyReagan <dusty@dustyreagan.com>
2022-12-16 16:13:55 -0600

    Add blaze banner to /stats/ page

--- client/my-sites/stats/mobile-promo-cards/index.jsx
+++ client/my-sites/stats/mobile-promo-cards/index.jsx
@@ -2,22 +2,22 @@ import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { MobilePromoCard } from '@automattic/components';
 import { translate } from 'i18n-calypso';
 import { useEffect } from 'react';
-import wordpressSeoIllustration from 'calypso/assets/images/illustrations/wordpress-seo-premium.svg';
+import blazeDropDownIllustration from 'calypso/assets/images/illustrations/blaze-drop-down.svg';
 import PromoCardBlock from 'calypso/blocks/promo-card-block';
 import DotPager from 'calypso/components/dot-pager';
 
 import './style.scss';
 
-const EVENT_YOAST_PROMO_VIEW = 'calypso_stats_wordpress_seo_premium_banner_view';
+const EVENT_BLAZE_PROMO_VIEW = 'calypso_stats_blaze_banner_view';
 const EVENT_MOBILE_PROMO_VIEW = 'calypso_stats_traffic_mobile_cta_jetpack_view';
 
 export default function MobilePromoCardWrapper( { isJetpack, isOdysseyStats, slug } ) {
-	// Yoast promo is disabled for Odyssey & self-hosted.
+	// Blaze promo is disabled for Odyssey & self-hosted.
 	// Mobile apps promos are always shown.
-	const showYoastPromo = ! isOdysseyStats && ! isJetpack;
-	const showBothPromoCards = showYoastPromo;
+	const showBlazePromo = ! isOdysseyStats && ! isJetpack;
+	const showBothPromoCards = showBlazePromo;
 	// Handle initial view event if not using the DotPager UI.
-	// We don't worry about the Yoast card as it sends on mount.
+	// We don't worry about the Blaze card as it sends on mount.
 	useEffect( () => {
 		if ( ! showBothPromoCards ) {
 			recordTracksEvent( EVENT_MOBILE_PROMO_VIEW );
@@ -26,15 +26,15 @@ export default function MobilePromoCardWrapper( { isJetpack, isOdysseyStats, slu
 	// Handle click events from promo card.
 	const promoCardDidReceiveClick = ( event ) => {
 		// Events need to incorporate the page and the click type.
-		// This will allow us to account for integraton across different pages.
+		// This will allow us to account for integration across different pages.
 		// FORMAT: calypso_stats_PAGE_mobile_cta_EVENT
 		const tracksEventName = `calypso_stats_traffic_mobile_cta_${ event.replaceAll( '-', '_' ) }`;
 		recordTracksEvent( tracksEventName );
 	};
 	// Handle view events from DotPager UI.
 	const pagerDidSelectPage = ( index ) => {
-		const evenLookup = [ EVENT_YOAST_PROMO_VIEW, EVENT_MOBILE_PROMO_VIEW ];
-		const eventName = evenLookup[ index ];
+		const eventLookup = [ EVENT_BLAZE_PROMO_VIEW, EVENT_MOBILE_PROMO_VIEW ];
+		const eventName = eventLookup[ index ];
 		recordTracksEvent( eventName );
 	};
 	// Render one or both promo cards.
@@ -55,16 +55,16 @@ export default function MobilePromoCardWrapper( { isJetpack, isOdysseyStats, slu
 					<div className="stats__promo-card">
 						<DotPager className="stats__promo-pager" onPageSelected={ pagerDidSelectPage }>
 							<PromoCardBlock
-								productSlug="wordpress-seo-premium"
-								impressionEvent={ EVENT_YOAST_PROMO_VIEW }
-								clickEvent="calypso_stats_wordpress_seo_premium_banner_click"
-								headerText={ translate( 'Increase site visitors with Yoast SEO Premium' ) }
+								productSlug="blaze"
+								impressionEvent={ EVENT_BLAZE_PROMO_VIEW }
+								clickEvent="calypso_stats_blaze_banner_click"
+								headerText={ translate( 'Reach new readers and customers' ) }
 								contentText={ translate(
-									'Purchase Yoast SEO Premium to ensure that more people find your incredible content.'
+									'Use WordPress Blaze to increase your reach by promoting your work to the larger WordPress.com community of blogs and sites. '
 								) }
-								ctaText={ translate( 'Learn more' ) }
-								image={ wordpressSeoIllustration }
-								href={ `/plugins/wordpress-seo-premium/${ slug }` }
+								ctaText={ translate( 'Get started' ) }
+								image={ blazeDropDownIllustration }
+								href={ `/advertising/${ slug || '' }` }
 							/>
 
 							<div>
