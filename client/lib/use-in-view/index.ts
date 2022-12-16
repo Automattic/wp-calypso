9abb24c43b0c4a1f470f6a2775e32ccf0c283a6c
bangank36 <bangank36@gmail.com>
2022-12-16 09:51:11 +0700

    Update deps

--- client/lib/use-in-view/index.ts
+++ client/lib/use-in-view/index.ts
@@ -1,49 +1,42 @@
 import { RefObject, useEffect, useRef } from 'react';
 
-type OptionalProps = {
-	[ key: string ]: unknown;
-};
-
 export function useInView< T extends Element >(
 	oneTimeCallback: () => void,
-	optionalProps?: OptionalProps
+	dependencies: any[] = []
 ): RefObject< T > {
 	const elementRef = useRef< T >( null );
 	const oneTimeCallbackRef = useRef< () => void >();
 
 	oneTimeCallbackRef.current = oneTimeCallback;
 
-	useEffect(
-		() => {
-			// We can't do anything without a valid reference to an element on the page
-			if ( ! elementRef.current ) {
-				return;
-			}
+	useEffect( () => {
+		// We can't do anything without a valid reference to an element on the page
+		if ( ! elementRef.current ) {
+			return;
+		}
 
-			const observer = new IntersectionObserver(
-				( entries ) => {
-					const [ entry ] = entries;
+		const observer = new IntersectionObserver(
+			( entries ) => {
+				const [ entry ] = entries;
 
-					if ( ! entry.isIntersecting ) {
-						return;
-					}
-
-					oneTimeCallbackRef.current?.();
-					observer.disconnect();
-				},
-				{
-					// Only fire the event when 100% of the element becomes visible
-					threshold: [ 1 ],
+				if ( ! entry.isIntersecting ) {
+					return;
 				}
-			);
 
-			observer.observe( elementRef.current );
+				oneTimeCallbackRef.current?.();
+				observer.disconnect();
+			},
+			{
+				// Only fire the event when 100% of the element becomes visible
+				threshold: [ 1 ],
+			}
+		);
 
-			// When the effect is dismounted, stop observing
-			return () => observer.disconnect();
-		},
-		optionalProps ? [ optionalProps ] : []
-	);
+		observer.observe( elementRef.current );
+
+		// When the effect is dismounted, stop observing
+		return () => observer.disconnect();
+	}, dependencies );
 
 	return elementRef;
 }
