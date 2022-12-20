d4c3f5c4e4859c7f1fe28d5bd316dcb4a430e5b4
arthur <arthur.chu@automattic.com>
2022-12-20 12:57:40 +0800

    Mock window.matchMedia

--- packages/page-pattern-modal/src/components/test/page-pattern-modal.tsx
+++ packages/page-pattern-modal/src/components/test/page-pattern-modal.tsx
@@ -45,6 +45,20 @@ beforeEach( () => {
 		category: 'text',
 		title: 'test block',
 	} );
+
+	Object.defineProperty( window, 'matchMedia', {
+		writable: true,
+		value: jest.fn().mockImplementation( ( query ) => ( {
+			matches: false,
+			media: query,
+			onchange: null,
+			addListener: jest.fn(), // deprecated
+			removeListener: jest.fn(), // deprecated
+			addEventListener: jest.fn(),
+			removeEventListener: jest.fn(),
+			dispatchEvent: jest.fn(),
+		} ) ),
+	} );
 } );
 
 afterEach( () => {
