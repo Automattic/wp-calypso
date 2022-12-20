95dfd2f13e5640371b9c3265858c2e0d6c23f445
mmtr <miguel.torres@automattic.com>
2022-12-20 15:52:22 +0100

    Use existing site

--- test/e2e/specs/fse/fse__limited-global-styles.ts
+++ test/e2e/specs/fse/fse__limited-global-styles.ts
@@ -5,109 +5,59 @@
 
 import {
 	DataHelper,
-	envVariables,
 	TestAccount,
-	getTestAccountByFeature,
-	envToFeatureKey,
 	FullSiteEditorPage,
-	SignupPickPlanPage,
-	SignupDomainPage,
-	NewSiteResponse,
-	RestAPIClient,
-	StartSiteFlow,
+	SecretsManager,
 } from '@automattic/calypso-e2e';
 import { Browser, Page } from 'playwright';
-import { apiDeleteSite } from '../shared';
 
 declare const browser: Browser;
 
 describe( DataHelper.createSuiteTitle( 'Site Editor: Limited Global Styles' ), function () {
 	let page: Page;
 	let fullSiteEditorPage: FullSiteEditorPage;
-	let testAccount: TestAccount;
-	let newSiteDetails: NewSiteResponse;
-	let siteCreatedFlag = false;
-
-	const features = envToFeatureKey( envVariables );
-	const accountName = getTestAccountByFeature( features );
+	const credentials = SecretsManager.secrets.testAccounts.simpleSiteFreePlanUser;
+	const siteSlug = credentials.testSites?.primary?.url as string;
 
 	beforeAll( async () => {
 		page = await browser.newPage();
 
-		testAccount = new TestAccount( accountName );
+		const testAccount = new TestAccount( 'simpleSiteFreePlanUser' );
 		await testAccount.authenticate( page );
 
-		fullSiteEditorPage = new FullSiteEditorPage( page, { target: features.siteType } );
+		fullSiteEditorPage = new FullSiteEditorPage( page );
 	} );
 
-	describe( 'Create site', function () {
-		it( 'Navigate to /start', async function () {
-			await page.goto( DataHelper.getCalypsoURL( 'start' ) );
-		} );
+	it( 'Visit the site editor', async function () {
+		await fullSiteEditorPage.visit( siteSlug );
+		await fullSiteEditorPage.prepareForInteraction( { leaveWithoutSaving: true } );
+	} );
 
-		it( 'Skip domain selection', async function () {
-			const signupDomainPage = new SignupDomainPage( page );
-			await signupDomainPage.skipDomainSelection();
-		} );
+	it( 'Open site styles', async function () {
+		await fullSiteEditorPage.openSiteStyles();
+	} );
 
-		it( `Select WordPress.com Free plan`, async function () {
-			const signupPickPlanPage = new SignupPickPlanPage( page );
-			newSiteDetails = await signupPickPlanPage.selectPlan( 'Free' );
+	it( 'Select the "Try it out" option in the upgrade modal', async function () {
+		await fullSiteEditorPage.tryGlobalStyles();
+	} );
 
-			siteCreatedFlag = true;
-		} );
-
-		it( 'Skip to dashboard', async function () {
-			const startSiteFlow = new StartSiteFlow( page );
-			await Promise.all( [
-				page.waitForNavigation( { url: /.*\/home\/.*/ } ),
-				startSiteFlow.clickButton( 'Skip to dashboard' ),
-			] );
+	it( 'Update the global background color', async function () {
+		await fullSiteEditorPage.setGlobalColorStyle( 'Background', {
+			colorName: 'Primary',
 		} );
 	} );
 
-	describe( 'Check Limited Global Styles upgrade flows', function () {
-		it( 'Visit the site editor', async function () {
-			await fullSiteEditorPage.visit( newSiteDetails.blog_details.site_slug );
-			await fullSiteEditorPage.prepareForInteraction( { leaveWithoutSaving: true } );
-		} );
-
-		it( 'Open site styles', async function () {
-			await fullSiteEditorPage.openSiteStyles();
-		} );
-
-		it( 'Select the "Try it out" option in the upgrade modal', async function () {
-			await fullSiteEditorPage.tryGlobalStyles();
-		} );
-
-		it( 'Update the global background color', async function () {
-			await fullSiteEditorPage.setGlobalColorStyle( 'Background', {
-				colorName: 'Primary',
-			} );
-		} );
-
-		it( 'Save the styles', async function () {
-			// On mobile, site styles is a popover panel that blocks the Save button.
-			// So let's always close site styles first to be safe. :)
-			await fullSiteEditorPage.closeSiteStyles();
-			await fullSiteEditorPage.save( { preSaveSelectors: [ '.wpcom-global-styles-notice' ] } );
-		} );
+	it( 'Save the styles', async function () {
+		// On mobile, site styles is a popover panel that blocks the Save button.
+		// So let's always close site styles first to be safe. :)
+		await fullSiteEditorPage.closeSiteStyles();
+		await fullSiteEditorPage.save( { preSaveSelectors: [ '.wpcom-global-styles-notice' ] } );
 	} );
 
-	afterAll( async function () {
-		if ( ! siteCreatedFlag ) {
-			return;
-		}
-
-		const restAPIClient = new RestAPIClient( {
-			username: testAccount.credentials.username,
-			password: testAccount.credentials.password,
-		} );
-
-		await apiDeleteSite( restAPIClient, {
-			url: newSiteDetails.blog_details.url,
-			id: newSiteDetails.blog_details.blogid,
-			name: newSiteDetails.blog_details.blogname,
-		} );
+	it( 'Reset styles to defaults', async function () {
+		await fullSiteEditorPage.openSiteStyles();
+		await fullSiteEditorPage.resetStylesToDefaults();
+		await fullSiteEditorPage.closeSiteStyles();
+		await fullSiteEditorPage.save();
 	} );
 } );
