95eff65bf4db8fa111bb9ac521c013895a5203b1
Even Tobiesen <e@gameover.ai>
2022-12-22 12:13:50 -0500

    Removing these types, ref. D96209-code

--- packages/shopping-cart/src/types.ts
+++ packages/shopping-cart/src/types.ts
@@ -572,10 +572,6 @@ export interface ResponseCartProduct {
 	is_gift_purchase?: boolean;
 
 	product_variants: ResponseCartProductVariant[];
-
-	// Temporary optional properties for the monthly pricing test
-	related_monthly_plan_cost_display?: string;
-	related_monthly_plan_cost_integer?: number;
 }
 
 export interface ResponseCartProductVariant {
