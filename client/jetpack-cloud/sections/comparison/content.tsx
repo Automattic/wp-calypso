580376abd9150de45681e02d6383fbf1a2a13489
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-12-23 18:36:31 +0530

    Wire everything up

--- client/jetpack-cloud/sections/comparison/content.tsx
+++ client/jetpack-cloud/sections/comparison/content.tsx
@@ -1,5 +1,27 @@
+import QueryIntroOffers from 'calypso/components/data/query-intro-offers';
+import QueryProductsList from 'calypso/components/data/query-products-list';
+import Main from 'calypso/components/main';
+import { TableWithStoreContext } from './table';
 import { ContentProps } from './types';
 
-export const Content: React.FC< ContentProps > = () => {
-	return <>Test</>;
+export const Content: React.FC< ContentProps > = ( {
+	nav,
+	header,
+	footer,
+	locale,
+	urlQueryArgs,
+} ) => {
+	return (
+		<>
+			{ nav }
+
+			<QueryProductsList type="jetpack" />
+			<QueryIntroOffers />
+			<Main>
+				{ header }
+				<TableWithStoreContext locale={ locale } urlQueryArgs={ urlQueryArgs } />
+			</Main>
+			{ footer }
+		</>
+	);
 };
