f414999505eff1c6ad3f4f7ced338e725d073f2a
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-12-29 15:22:24 +0530

    Jetpack Cloud | Fix login redirect on /features/comparison page

--- client/jetpack-cloud/sections/comparison/index.tsx
+++ client/jetpack-cloud/sections/comparison/index.tsx
@@ -1,5 +1,5 @@
 import page from 'page';
-import { makeLayout, render as clientRender } from 'calypso/controller';
+import { makeLayout, render as clientRender } from 'calypso/controller/index.web';
 import { setLocaleMiddleware } from 'calypso/controller/shared';
 import { loggedInSiteSelection } from 'calypso/my-sites/controller';
 import { jetpackComparisonContext } from './controller';
