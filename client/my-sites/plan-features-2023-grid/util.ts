087cf1bac5a64160da707e795f44e63080a992a8
Niranjan Uma Shankar <niranjan.u@gmail.com>
2022-12-29 20:45:34 +0530

    Add space

--- client/my-sites/plan-features-2023-grid/util.ts
+++ client/my-sites/plan-features-2023-grid/util.ts
@@ -9,13 +9,13 @@ import { translate } from 'i18n-calypso';
 export const getStorageStringFromFeature = ( storageFeature: string ) => {
 	switch ( storageFeature ) {
 		case FEATURE_1GB_STORAGE:
-			return translate( '1GB' );
+			return translate( '1 GB' );
 		case FEATURE_6GB_STORAGE:
-			return translate( '6GB' );
+			return translate( '6 GB' );
 		case FEATURE_13GB_STORAGE:
-			return translate( '13GB' );
+			return translate( '13 GB' );
 		case FEATURE_200GB_STORAGE:
-			return translate( '200GB' );
+			return translate( '200 GB' );
 		default:
 			return null;
 	}
