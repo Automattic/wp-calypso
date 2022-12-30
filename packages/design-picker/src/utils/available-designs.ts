8b6d517f38586e731076a19e0b48e6e158988680
Candy Tsai <candy02058912@gmail.com>
2022-12-30 17:26:11 +0800

    Fix unified design picker mShot config

--- packages/design-picker/src/utils/available-designs.ts
+++ packages/design-picker/src/utils/available-designs.ts
@@ -57,7 +57,8 @@ export const getMShotOptions = ( {
 	// Take care changing these values, as the design-picker CSS animations are written for these values (see the *__landscape and *__portrait classes)
 	return {
 		vpw: isMobile ? MOBILE_VIEWPORT_WIDTH : DEFAULT_VIEWPORT_WIDTH,
-		vph: scrollable ? 1600 : 1040,
+		// 901 renders well with all the current designs
+		vph: scrollable ? 1600 : 901,
 		// When `w` was 1200 it created a visual glitch on one thumbnail. #57261
 		w: highRes ? 1199 : 600,
 		screen_height: 3600,
