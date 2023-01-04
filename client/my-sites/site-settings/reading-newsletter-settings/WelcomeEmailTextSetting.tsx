e6d64b6a522d785ce78f0f3e9323b31e65fc080f
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2023-01-04 09:55:59 +0100

    Separate FormTextareas into two separate FormFieldsets

--- client/my-sites/site-settings/reading-newsletter-settings/WelcomeEmailTextSetting.tsx
+++ client/my-sites/site-settings/reading-newsletter-settings/WelcomeEmailTextSetting.tsx
@@ -39,37 +39,41 @@ export const WelcomeEmailTextSetting = ( {
 		};
 
 	return (
-		<FormFieldset>
-			<FormLabel>Welcome email text</FormLabel>
-			<FormTextarea
-				name=""
-				id=""
-				value={ value?.invitation }
-				onChange={ onChangeFieldSubscriptionOptions( 'invitation' ) }
-				disabled={ disabled }
-				autoCapitalize="none"
-				onClick={ null }
-				onKeyPress={ null }
-			/>
-			<FormSettingExplanation>
-				{ translate(
-					'The welcome message sent out to new readers when they subscribe to your blog.'
-				) }
-			</FormSettingExplanation>
-			<FormLabel>Comment follow email text</FormLabel>
-			<FormTextarea
-				name=""
-				id=""
-				value={ value?.comment_follow }
-				onChange={ onChangeFieldSubscriptionOptions( 'comment_follow' ) }
-				disabled={ disabled }
-				autoCapitalize="none"
-				onClick={ null }
-				onKeyPress={ null }
-			/>
-			<FormSettingExplanation>
-				{ translate( 'The email sent out when someone follows one of your posts.' ) }
-			</FormSettingExplanation>
-		</FormFieldset>
+		<>
+			<FormFieldset>
+				<FormLabel>Welcome email text</FormLabel>
+				<FormTextarea
+					name=""
+					id=""
+					value={ value?.invitation }
+					onChange={ onChangeFieldSubscriptionOptions( 'invitation' ) }
+					disabled={ disabled }
+					autoCapitalize="none"
+					onClick={ null }
+					onKeyPress={ null }
+				/>
+				<FormSettingExplanation>
+					{ translate(
+						'The welcome message sent out to new readers when they subscribe to your blog.'
+					) }
+				</FormSettingExplanation>
+			</FormFieldset>
+			<FormFieldset>
+				<FormLabel>Comment follow email text</FormLabel>
+				<FormTextarea
+					name=""
+					id=""
+					value={ value?.comment_follow }
+					onChange={ onChangeFieldSubscriptionOptions( 'comment_follow' ) }
+					disabled={ disabled }
+					autoCapitalize="none"
+					onClick={ null }
+					onKeyPress={ null }
+				/>
+				<FormSettingExplanation>
+					{ translate( 'The email sent out when someone follows one of your posts.' ) }
+				</FormSettingExplanation>
+			</FormFieldset>
+		</>
 	);
 };
