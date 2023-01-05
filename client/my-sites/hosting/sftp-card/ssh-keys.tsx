81febd13448177647319e4658e0921e00b719b16
sejas <antonio@sejas.es>
2023-01-05 00:24:39 +0000

    SSH: show old keys instead of spinner on tab focus

--- client/my-sites/hosting/sftp-card/ssh-keys.tsx
+++ client/my-sites/hosting/sftp-card/ssh-keys.tsx
@@ -33,11 +33,7 @@ const sshKeyAttachFailureNoticeId = 'ssh-key-attach-failure';
 function SshKeys( { siteId, siteSlug, username, disabled }: SshKeysProps ) {
 	const { __ } = useI18n();
 	const dispatch = useDispatch();
-	const {
-		data: keys,
-		isLoading: isLoadingKeys,
-		isFetching: isFetchingKeys,
-	} = useAtomicSshKeys( siteId, {
+	const { data: keys, isLoading: isLoadingKeys } = useAtomicSshKeys( siteId, {
 		enabled: ! disabled,
 	} );
 	const { data: userKeys, isLoading: isLoadingUserKeys } = useSSHKeyQuery();
@@ -80,15 +76,7 @@ function SshKeys( { siteId, siteSlug, username, disabled }: SshKeysProps ) {
 		return !! keys.find( ( { user_login } ) => user_login === username );
 	}, [ keys, username ] );
 
-	/*
-	 * isFetching keys is needed here, because when we update a key from me/security/ssh-key page,
-	 * and return to this page, the keys, although the query reruns, are not cleared.
-	 * isFetching returns true when refetching in the background. Happens when there is stale cache to display as a placeholder.
-	 * So we rely on that to display the loader.
-	 * Same goes with the map of the keys in the view below. We don't want to show the old keys
-	 * until the refetching is completed.
-	 * */
-	const isLoading = isLoadingKeys || isLoadingUserKeys || isFetchingKeys;
+	const isLoading = isLoadingKeys || isLoadingUserKeys;
 	const showKeysSelect = ! isLoading && ! userKeyIsAttached && userKeys && userKeys.length > 0;
 	const showLinkToAddUserKey = ! isLoading && ! userKeyIsAttached && userKeys?.length === 0;
 	const SSH_ADD_URL = addQueryArgs( '/me/security/ssh-key', {
@@ -102,15 +90,14 @@ function SshKeys( { siteId, siteSlug, username, disabled }: SshKeysProps ) {
 				{ __( 'SSH Keys' ) }
 			</label>
 
-			{ ! isFetchingKeys &&
-				keys?.map( ( sshKey ) => (
-					<SshKeyCard
-						key={ sshKey.sha256 }
-						sshKey={ sshKey }
-						deleteText={ __( 'Detach' ) }
-						siteId={ siteId }
-					/>
-				) ) }
+			{ keys?.map( ( sshKey ) => (
+				<SshKeyCard
+					key={ sshKey.sha256 }
+					sshKey={ sshKey }
+					deleteText={ __( 'Detach' ) }
+					siteId={ siteId }
+				/>
+			) ) }
 
 			{ isLoading && <Spinner /> }
 
