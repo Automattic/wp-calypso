787471d587ee3546a51f79ffb738ea7dbb8d138c
Tim Broddin <tim@wannabes.be>
2023-01-05 12:57:55 +0100

    Add email stats for devices & clients (#71668)
    * Changes in redux for Email Stats for new shape in endpoint
    
    * - Start on Geochart adaptation (WIP)
    
    * - Start on Geochart adaptation (WIP)
    
    * The map now renders!
    
    * Remove map for now
    
    * Remove reappeared email-chart-tabs
    
    * Remove reappeared email-chart-tabs
    
    * Readd email-chart-tabs selectors
    
    * Add list view and email card module
    
    * Add country flags
    
    * Change the condition of stats
    
    * Stylelint warnings
    
    * Add devices stats
    
    * Add clients stats
    
    + Cleanup of specific components per list
    
    * Add grid styling
    
    * Add title labels
    
    * Make multiple calls to email stats endpoint for response time
    
    * Fix loading state & fields
    
    Co-authored-by: Juan Adolfo Lucha García <juanlucha@gmail.com>

--- client/state/stats/emails/actions.js
+++ client/state/stats/emails/actions.js
@@ -5,7 +5,11 @@ import {
 	EMAIL_STATS_REQUEST_FAILURE,
 	EMAIL_STATS_REQUEST_SUCCESS,
 } from 'calypso/state/action-types';
-import { parseEmailChartData, parseEmailCountriesData } from 'calypso/state/stats/lists/utils';
+import {
+	parseEmailChartData,
+	parseEmailCountriesData,
+	parseEmailListData,
+} from 'calypso/state/stats/lists/utils';
 
 import 'calypso/state/stats/init';
 
@@ -64,6 +68,8 @@ export function requestEmailStats( siteId, postId, period, date, statType, quant
 							stats.countries[ item.period ],
 							stats[ 'countries-info' ]
 						),
+						devices: parseEmailListData( stats.devices[ item.period ] ),
+						clients: parseEmailListData( stats.clients[ item.period ] ),
 					};
 				} );
 
