d6780351c8ca75fc1d96daa4787a5220eb5e848d
vikneshwar <lviknesh1993@gmail.com>
2023-01-05 20:06:00 +0530

    Jetpack Cloud Cart Integration - fix cart banner position - Calculate the position of the cart in main banner and use the same for the banner trasition (in desktop) - When no item in cart hide the count instead of not displaying it

--- client/components/jetpack/intro-pricing-banner/index.tsx
+++ client/components/jetpack/intro-pricing-banner/index.tsx
@@ -1,6 +1,7 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
+import { useBreakpoint } from '@automattic/viewport-react';
 import { useTranslate } from 'i18n-calypso';
-import { useMemo } from 'react';
+import { useLayoutEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import CloudCart from 'calypso/jetpack-cloud/sections/pricing/jpcom-masterbar/cloud-cart';
 import useDetectWindowBoundary from 'calypso/lib/detect-window-boundary';
@@ -16,9 +17,40 @@ import people from './people.svg';
 const CALYPSO_MASTERBAR_HEIGHT = 47;
 const CLOUD_MASTERBAR_HEIGHT = 0;
 
+const useBoundingClientRect = ( eleQuery: string ) => {
+	const [ boundingClientRect, setBoundingClientRect ] = useState( {
+		bottom: 0,
+		height: 0,
+		left: 0,
+		right: 0,
+		top: 0,
+		width: 0,
+		x: 0,
+		y: 0,
+	} );
+
+	useLayoutEffect( () => {
+		const resizeListener = () => {
+			document.querySelectorAll( eleQuery ).forEach( ( node ) => {
+				const style = window.getComputedStyle( node, undefined );
+				if ( style[ 'display' ] !== 'none' ) {
+					setBoundingClientRect( node.getBoundingClientRect() );
+				}
+			} );
+		};
+
+		resizeListener();
+		window.addEventListener( 'resize', resizeListener );
+	}, [ eleQuery ] );
+
+	return boundingClientRect;
+};
+
 const IntroPricingBanner: React.FC = () => {
 	const translate = useTranslate();
 	const shouldShowCart = useSelector( isJetpackCloudCartEnabled );
+	const clientRect = useBoundingClientRect( '.header__content .header__jetpack-masterbar-cart' );
+	const isSmallScreen = useBreakpoint( '<660px' );
 
 	const windowBoundaryOffset = useMemo( () => {
 		if ( isJetpackCloud() || isConnectStore() ) {
@@ -66,7 +98,9 @@ const IntroPricingBanner: React.FC = () => {
 							{ preventWidows( translate( 'Explore Jetpack for Agencies' ) ) }
 						</a>
 					</div>
-					{ shouldShowCart && hasCrossed && <CloudCart /> }
+					{ shouldShowCart && hasCrossed && (
+						<CloudCart cartStyle={ isSmallScreen ? {} : { left: clientRect.left } } />
+					) }
 				</div>
 			</div>
 		</>
