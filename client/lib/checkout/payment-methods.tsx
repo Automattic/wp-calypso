d18b35dbcd3884fe111d9d5caa39a5f5b7c94edb
Damien Alleyne <31164683+d-alleyne@users.noreply.github.com>
2023-01-05 19:02:57 -0400

    Add context to "Discover" when referring to credit card name (#71719)
    * Add context to "Discover" when referring to credit card name
    
    The Discover credit card payment method needs to be
    translated differently from the other instances of
    the word "Discover".
    
    * Re-use an existing context

--- client/lib/checkout/payment-methods.tsx
+++ client/lib/checkout/payment-methods.tsx
@@ -90,7 +90,9 @@ export const PaymentMethodSummary = ( {
 			break;
 
 		case 'discover':
-			displayType = translate( 'Discover' );
+			displayType = translate( 'Discover', {
+				context: 'Name of credit card',
+			} );
 			break;
 
 		case 'jcb':
