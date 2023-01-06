b164a5a5acc861540e954eeddf2121471a88eb0c
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2023-01-06 16:16:14 +0800

    Minor syntax tweak.

--- client/my-sites/stats/navigation-arrows/index.jsx
+++ client/my-sites/stats/navigation-arrows/index.jsx
@@ -20,20 +20,18 @@ function NavigationArrows( {
 	return (
 		<div className={ classNames( 'stats-period-navigation', 'stats-year-navigation' ) }>
 			<button
-				className={ classNames(
-					'stats-period-navigation__previous',
-					disablePreviousArrow && 'is-disabled'
-				) }
+				className={ classNames( 'stats-period-navigation__previous', {
+					'is-disabled': disablePreviousArrow,
+				} ) }
 				onClick={ disablePreviousArrow ? undefined : () => handleClickPrevious() }
 				tabIndex={ ! disablePreviousArrow ? 0 : -1 }
 			>
 				<Icon className="gridicon" icon={ arrowLeft } />
 			</button>
 			<button
-				className={ classNames(
-					'stats-period-navigation__next',
-					disableNextArrow && 'is-disabled'
-				) }
+				className={ classNames( 'stats-period-navigation__next', {
+					'is-disabled': disableNextArrow,
+				} ) }
 				onClick={ disableNextArrow ? undefined : () => handleClickNext() }
 				tabIndex={ ! disableNextArrow ? 0 : -1 }
 			>
