4109ccec01c9c3aaa5bf0993ff6d9952b489e435
Andrii Lysenko <lysenkoa.work@gmail.com>
2023-01-09 13:34:59 -0800

    update: small refactor

--- client/state/sites/selectors/is-jetpack-cloud-cart-enabled.ts
+++ client/state/sites/selectors/is-jetpack-cloud-cart-enabled.ts
@@ -2,14 +2,15 @@ import { isEnabled } from '@automattic/calypso-config';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import getSiteSlug from './get-site-slug';
+import type { AppState } from 'calypso/types';
 
 /**
  * Returns true if we want to show Cart in Jetpack cloud
  *
  * @param  {object}   state  Global state tree
  */
-export default function isJetpackCloudCartEnabled( state ) {
+export default function isJetpackCloudCartEnabled( state: AppState ) {
 	const siteId = getSelectedSiteId( state );
 	const siteSlug = getSiteSlug( state, siteId );
-	return isEnabled( 'jetpack/pricing-page-cart' ) && isUserLoggedIn( state ) && siteSlug;
+	return Boolean( isEnabled( 'jetpack/pricing-page-cart' ) && isUserLoggedIn( state ) && siteSlug );
 }
