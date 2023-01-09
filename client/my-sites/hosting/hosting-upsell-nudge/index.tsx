c804b664f3388e67d3cb199da67eb4970fb32ac1
Philip Jackson <p-jackson@live.com>
2023-01-09 17:38:33 +1300

    Add icons to feature title

--- client/my-sites/hosting/hosting-upsell-nudge/index.tsx
+++ client/my-sites/hosting/hosting-upsell-nudge/index.tsx
@@ -1,7 +1,13 @@
 import { useTranslate } from 'i18n-calypso';
 import UpsellNudge from 'calypso/blocks/upsell-nudge';
-import './style.scss';
 import { preventWidows } from 'calypso/lib/formatting';
+import iconCloud from './icons/icon-cloud.svg';
+import iconComments from './icons/icon-comments.svg';
+import iconPHP from './icons/icon-php.svg';
+import iconServerRacks from './icons/icon-server-racks.svg';
+import iconSSH from './icons/icon-ssh.svg';
+import iconTerminal from './icons/icon-terminal.svg';
+import './style.scss';
 
 interface FeatureListItem {
 	title: string;
@@ -28,7 +34,7 @@ export function HostingUpsellNudge( { siteId }: { siteId: number | null } ) {
 			renderListItem={ ( { icon, title, description }: FeatureListItem ) => (
 				<>
 					<div className="hosting-upsell-nudge__feature-title">
-						{ icon } { title }
+						<img className="hosting-upsell-nudge__feature-icon" src={ icon } alt="" /> { title }
 					</div>
 					<div className="hosting-upsell-nudge__feature-description">
 						{ preventWidows( description ) }
@@ -48,40 +54,40 @@ function useFeatureList(): FeatureListItem[] {
 			description: translate(
 				`Access and edit your website's files directly by creating SFTP credentials and using an SFTP client`
 			),
-			icon: '',
+			icon: iconCloud,
 		},
 		{
 			title: translate( 'CLI Access' ),
 			description: translate( 'Get on the Command Line Interface and perform high level tasks' ),
-			icon: '',
+			icon: iconTerminal,
 		},
 		{
 			title: translate( 'SSH' ),
 			description: translate(
 				'Enable SSH to perform advanced site operations using the command line'
 			),
-			icon: '',
+			icon: iconSSH,
 		},
 		{
 			title: translate( 'Pick Your Datacenter' ),
 			description: translate(
 				'Select and change where you want to host your WordPress.com website'
 			),
-			icon: '',
+			icon: iconServerRacks,
 		},
 		{
 			title: translate( 'PhpMyAdmin' ),
 			description: translate(
 				"Be able to dive deep into your website's database columns and SQL tables"
 			),
-			icon: '',
+			icon: iconPHP,
 		},
 		{
 			title: translate( 'Live Support' ),
 			description: translate(
 				'Either have questions or need help, get instant support from our Happiness Engineers'
 			),
-			icon: '',
+			icon: iconComments,
 		},
 	];
 }
