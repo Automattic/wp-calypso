ab2b8f39063b58bcd2ac2a7e7fe44b8a5785686d
Payton Swick <payton@foolord.com>
2023-01-10 15:45:45 -0500

    Set default currency locale whenever locale action is created
    This is not a great place to put a side effect because it's an action
    creator, and should simply return an object. However, this is where we
    set the translation locale already, so we might as well do both.

---
+++ client/state/ui/language/actions.ts
@@ -0,0 +1,25 @@
+import { setDefaultLocale as setCurrencyLocale } from '@automattic/format-currency';
+import switchLocale from 'calypso/lib/i18n-utils/switch-locale';
+import { LOCALE_SET } from 'calypso/state/action-types';
+
+import 'calypso/state/ui/init';
+
+/**
+ * Set the ui locale
+ */
+export const setLocale = ( localeSlug: string, localeVariant?: string ) => {
+	/**
+	 * Also change the translation and currency locale.
+	 *
+	 * This is not a great place to put a side effect because it's an action
+	 * creator, and should simply return an object, but it's been here for a long
+	 * time since https://github.com/Automattic/wp-calypso/pull/18297
+	 */
+	setCurrencyLocale( localeVariant || localeSlug );
+	switchLocale( localeVariant || localeSlug );
+	return {
+		type: LOCALE_SET,
+		localeSlug,
+		localeVariant,
+	};
+};
