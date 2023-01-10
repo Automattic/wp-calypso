c3ddb7b0bfbbb6befe161bb8c60163f5cbe96468
Robert Sreberski <robertsreberski@gmail.com>
2023-01-10 17:56:58 +0100

    Improve initialization

--- client/lib/analytics/ad-tracking/setup.js
+++ client/lib/analytics/ad-tracking/setup.js
@@ -77,27 +77,15 @@ if ( typeof window !== 'undefined' ) {
  * Initializes Linkedin tracking.
  */
 function setupLinkedinInsight( partnerId ) {
-	if ( window._linkedin_data_partner_id && window.lintrk ) {
-		return;
-	}
-
 	window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
 	window._linkedin_data_partner_ids.push( partnerId );
 
-	( function ( l ) {
-		if ( ! l ) {
-			window.lintrk = function ( a, b ) {
-				window.lintrk.q.push( [ a, b ] );
-			};
-			window.lintrk.q = [];
-		}
-		const s = document.getElementsByTagName( 'script' )[ 0 ];
-		const b = document.createElement( 'script' );
-		b.type = 'text/javascript';
-		b.async = true;
-		b.src = 'https://snap.licdn.com/li.lms-analytics/insight.min.js';
-		s.parentNode.insertBefore( b, s );
-	} )( window.lintrk );
+	if ( ! window.lintrk ) {
+		window.lintrk = function ( a, b ) {
+			window.lintrk.q.push( [ a, b ] );
+		};
+		window.lintrk.q = [];
+	}
 }
 
 /**
