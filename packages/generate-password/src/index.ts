6122c091f95235a967a10c04df60d459c57bd7f8
Markus Dobmann <m.do@posteo.net>
2023-01-10 11:40:12 +0100

    make fn api more configurable

--- packages/generate-password/src/index.ts
+++ packages/generate-password/src/index.ts
@@ -1,3 +1,18 @@
+type CharacterPoolOptions = {
+	/**
+	 * Use digits 0-9 in character pool
+	 */
+	useNumbers?: boolean;
+	/**
+	 * Use characters !@#$%^&*() in character pool
+	 */
+	useSpecialChars?: boolean;
+	/**
+	 * Use characters -_ []{}<>~`+=,.;:/?| in character pool
+	 */
+	useExtraSpecialChars?: boolean;
+};
+
 /**
  * Forked from https://github.com/WordPress/wporg-two-factor/blob/trunk/settings/src/components/password.js#L139-L167
  *
@@ -6,18 +21,37 @@
  * This is just as secure as using the `generate-password` AJAX endpoint, but faster, and avoids introducing the
  * possibility of XHR failures, etc.
  */
-export function generatePassword() {
-	const characterPool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
-	const passwordLength = 24;
+export function generatePassword(
+	length = 24,
+	{
+		useNumbers = true,
+		useSpecialChars = true,
+		useExtraSpecialChars = false,
+	}: CharacterPoolOptions = {}
+) {
+	let characterPool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+	if ( useNumbers ) {
+		characterPool += '0123456789';
+	}
+
+	if ( useSpecialChars ) {
+		characterPool += '!@#$%^&*()';
+	}
+
+	if ( useExtraSpecialChars ) {
+		characterPool += '-_ []{}<>~`+=,.;:/?|';
+	}
+
 	const randomNumber = new Uint8Array( 1 );
 	let password = '';
 
-	// JS doesn't provide a way to generate a cryptographically secure random number within a range, so instead
+	// JS doesn't provide a way to generate cryptographically secure random number within a range, so instead
 	// we just throw out values that don't correspond to a character. This is a little bit slower than using a
 	// modulo operation, but it avoids introducing bias in the distribution. Realistically, it's easily performant
 	// in this context.
 	// @link https://dimitri.xyz/random-ints-from-random-bits/
-	for ( let i = 0; i < passwordLength; i++ ) {
+	for ( let i = 0; i < length; i++ ) {
 		do {
 			window.crypto.getRandomValues( randomNumber );
 		} while ( randomNumber[ 0 ] >= characterPool.length );
