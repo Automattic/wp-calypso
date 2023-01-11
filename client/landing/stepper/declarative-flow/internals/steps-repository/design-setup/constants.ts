7d7ec7cdf32c9920dca914a7603fa6b17dc3cdb6
Ashar Fuadi <ashar.fuadi@automattic.com>
2023-01-11 10:47:02 +0700

    Design Picker: Hide retiring designs

--- client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/constants.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/constants.ts
@@ -1,2 +1,19 @@
 export const DEFAULT_VARIATION_SLUG = 'default';
 export const STEP_NAME = 'design-setup';
+
+/**
+ * Designs that are to be retired soon.
+ * Remove designs from this list when the corresponding themes have actually been retired.
+ */
+export const RETIRING_DESIGN_SLUGS = [
+	'ames',
+	'antonia',
+	'appleton',
+	'barnett',
+	'blank-canvas-blocks',
+	'calvin',
+	'dorna',
+	'farrow',
+	'heiwa',
+	'meraki',
+];
