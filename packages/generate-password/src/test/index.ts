09ffa5e8281927cc4dc6e4c90d48fc917015ac0d
Markus Dobmann <m.do@posteo.net>
2023-01-11 10:57:35 +0100

    add some tests which test for non-occurrence of charsets

--- packages/generate-password/src/test/index.ts
+++ packages/generate-password/src/test/index.ts
@@ -3,7 +3,7 @@
  */
 // eslint-disable-next-line import/no-nodejs-modules
 import { webcrypto } from 'node:crypto';
-import { generatePassword } from '../';
+import { generatePassword, DIGITS, SPECIAL_CHARS, EXTRA_SPECIAL_CHARS } from '../';
 
 describe( 'generatePassword', () => {
 	const mockRandomValues = jest.fn( ( array ) => webcrypto.getRandomValues( array ) );
@@ -23,6 +23,21 @@ describe( 'generatePassword', () => {
 			expect( generatePassword( { length: 12 } ) ).toHaveLength( 12 );
 			expect( generatePassword( { length: 1337 } ) ).toHaveLength( 1337 );
 			expect( generatePassword( { length: 99 } ) ).toHaveLength( 99 );
+		} );
+
+		test( 'should not contain digits', () => {
+			const pw = generatePassword( { length: 1000, useNumbers: false } );
+			expect( pw ).not.toMatch( new RegExp( `[${ DIGITS }]`, 'g' ) );
+		} );
+
+		test( 'should not contain special characters', () => {
+			const pw = generatePassword( { length: 1000, useSpecialChars: false } );
+			expect( pw ).not.toMatch( new RegExp( `[${ SPECIAL_CHARS }]`, 'g' ) );
+		} );
+
+		test( 'should not contain extra special characters', () => {
+			const pw = generatePassword( { length: 1000, useExtraSpecialChars: false } );
+			expect( pw ).not.toMatch( new RegExp( `[${ EXTRA_SPECIAL_CHARS }]`, 'g' ) );
 		} );
 	} );
 } );
