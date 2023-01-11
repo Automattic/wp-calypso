60fc1fd0cb3d218cd14bb06bf03b899e2bd93ad5
Wayne Wang <iverson0217@gmail.com>
2023-01-11 17:55:54 +0800

    Introduce site creation step into importer-focused flow

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-creation-step/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-creation-step/index.tsx
@@ -6,6 +6,7 @@ import {
 	addPlanToCart,
 	createSiteWithCart,
 	isFreeFlow,
+	isMigrationFlow,
 } from '@automattic/onboarding';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useEffect } from 'react';
@@ -36,16 +37,22 @@ const SiteCreationStep: Step = function SiteCreationStep( { navigation, flow } )
 
 	const username = useSelector( ( state ) => getCurrentUserName( state ) );
 
-	const { setPendingAction } = useDispatch( ONBOARD_STORE );
+	const { setPendingAction, setIsMigrateFromWp } = useDispatch( ONBOARD_STORE );
 
-	const theme = isLinkInBioFlow( flow ) ? 'pub/lynx' : 'pub/lettre';
+	let theme: string;
+	if ( isMigrationFlow( flow ) ) {
+		theme = 'pub/zoologist';
+	} else {
+		theme = isLinkInBioFlow( flow ) ? 'pub/lynx' : 'pub/lettre';
+	}
 	const isPaidDomainItem = Boolean( domainCartItem?.product_slug );
 
 	// Default visibility is public
 	let siteVisibility = Site.Visibility.PublicIndexed;
 
 	// Link-in-bio flow defaults to "Coming Soon"
-	if ( isLinkInBioFlow( flow ) || isFreeFlow( flow ) ) {
+
+	if ( isLinkInBioFlow( flow ) || isFreeFlow( flow ) || isMigrationFlow( flow ) ) {
 		siteVisibility = Site.Visibility.PublicNotIndexed;
 	}
 
@@ -96,6 +103,10 @@ const SiteCreationStep: Step = function SiteCreationStep( { navigation, flow } )
 	}
 
 	useEffect( () => {
+		if ( isMigrationFlow( flow ) ) {
+			setIsMigrateFromWp( true );
+		}
+
 		if ( submit ) {
 			setPendingAction( createSite );
 			submit();
