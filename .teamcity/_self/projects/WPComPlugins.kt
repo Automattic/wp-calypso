72d983903910fbbd835a805ae13a79d90920b5ee
Jasper Kang <kangzjnet@gmail.com>
2023-01-12 15:55:18 +1300

    revert teamcity translation step

--- .teamcity/_self/projects/WPComPlugins.kt
+++ .teamcity/_self/projects/WPComPlugins.kt
@@ -158,17 +158,6 @@ private object OdysseyStats : WPComPluginBuild(
 	pluginSlug = "odyssey-stats",
 	archiveDir = "./dist/",
 	docsLink = "PejTkB-3N-p2",
-	buildSteps = {
-		bashNodeScript {
-			name = "Translate Odyssey Stats"
-			scriptContent = """
-				cd apps/odyssey-stats
-
-				# generate language files
-				yarn translate
-			"""
-		}
-	}
 )
 
 private object O2Blocks : WPComPluginBuild(
