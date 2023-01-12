80d1cd06aff5a35d2a05e20894765fbaa17e38a4
Ashar Fuadi <ashar.fuadi@automattic.com>
2023-01-12 09:49:20 +0700

    Remove retired blank-canvas-blocks

--- client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/constants.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/constants.ts
@@ -3,14 +3,13 @@ export const STEP_NAME = 'design-setup';
 
 /**
  * Designs that are to be retired soon.
- * Remove designs from this list when the corresponding themes have actually been retired.
+ * Designs can be removed from this list when the corresponding themes have actually been retired.
  */
 export const RETIRING_DESIGN_SLUGS = [
 	'ames',
 	'antonia',
 	'appleton',
 	'barnett',
-	'blank-canvas-blocks',
 	'calvin',
 	'dorna',
 	'farrow',
