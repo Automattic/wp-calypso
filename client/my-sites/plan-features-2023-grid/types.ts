560b2ae5884086de33e1d133f01b3dff0ea1f299
Payton Swick <payton@foolord.com>
2023-01-12 16:33:50 -0500

    Improve types of PlanProperties and PlanFeature

--- client/my-sites/plan-features-2023-grid/types.ts
+++ client/my-sites/plan-features-2023-grid/types.ts
@@ -6,27 +6,27 @@ export type PlanProperties = {
 	discountPrice: number | null;
 	features: any;
 	jpFeatures: any;
-	isLandingPage: boolean;
-	isPlaceholder: boolean;
+	isLandingPage?: boolean;
+	isPlaceholder?: boolean;
 	planConstantObj: any;
-	planName: any;
+	planName: string;
 	planObject: any;
-	product_name_short: any;
-	hideMonthly: any;
+	product_name_short: string;
+	hideMonthly?: boolean;
 	rawPrice: number | null;
 	rawPriceAnnual: number | null;
 	rawPriceForMonthlyPlan: number | null;
 	relatedMonthlyPlan: any;
 	annualPricePerMonth: number | null;
 	isMonthlyPlan: boolean;
-	tagline: any;
+	tagline: string;
 	storageOptions: any;
 };
 
 export interface PlanFeature {
 	availableForCurrentPlan?: boolean;
 	availableOnlyForAnnualPlans?: boolean;
-	getSlug: () => React.ReactNode;
+	getSlug: () => string;
 	getTitle: ( name?: string ) => React.ReactNode;
 	getDescription: () => React.ReactNode;
 }
