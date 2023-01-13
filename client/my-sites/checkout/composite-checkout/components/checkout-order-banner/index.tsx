fdb87c70ac753de0d151473bf9ddf046c0969a31
DustyReagan <dusty@dustyreagan.com>
2023-01-13 11:32:53 -0600

    Load banner faster by checking path instead of responseCart.is_gift_purchase

--- client/my-sites/checkout/composite-checkout/components/checkout-order-banner/index.tsx
+++ client/my-sites/checkout/composite-checkout/components/checkout-order-banner/index.tsx
@@ -6,9 +6,12 @@ import useCartKey from 'calypso/my-sites/checkout/use-cart-key';
 export function CheckoutOrderBanner() {
 	const cartKey = useCartKey();
 	const { responseCart } = useShoppingCart( cartKey );
-	const giftSiteSlug = responseCart.gift_details?.receiver_blog_slug;
+	const giftSiteSlug = responseCart.gift_details?.receiver_blog_slug ?? '';
 
-	if ( responseCart.is_gift_purchase && giftSiteSlug ) {
+	const path = window.location.pathname;
+
+	// Check the path instead of using responseCart.is_gift_purchase because it visually loads the banner faster.
+	if ( path.startsWith( '/checkout/' ) && path.includes( '/gift/' ) ) {
 		return <GiftingCheckoutBanner siteSlug={ giftSiteSlug } />;
 	}
 	return null;
