64d4f42cfe0c47ab6b72083800e756983872cbb2
Sharpirate <stoynov.bogomil@gmail.com>
2023-01-13 09:55:31 -0700

    add default props & fix iframe overflow

--- client/components/web-preview/content.jsx
+++ client/components/web-preview/content.jsx
@@ -427,7 +427,7 @@ export default class WebPreviewContent extends Component {
 								style={ {
 									...this.state.iframeStyle,
 									height: this.state.viewport?.height,
-									pointerEvents: this.props.isLaunchpad ? 'none' : 'all',
+									pointerEvents: this.props.isLaunchpad ? 'auto' : 'all',
 								} }
 								src="about:blank"
 								onLoad={ () => this.setLoaded( 'iframe-onload' ) }
@@ -436,32 +436,34 @@ export default class WebPreviewContent extends Component {
 								scrolling={ autoHeight ? 'no' : undefined }
 								tabIndex={ disableTabbing ? -1 : 0 }
 							/>
-							<div
-								className="web-preview__frame-edit-overlay"
-								style={ {
-									opacity: this.state.showIFrameOverlay ? '1' : '0',
-									background: this.state.showIFrameOverlay
-										? `rgba(16, 21, 23, 0.5)`
-										: 'rgba(16, 21, 23, 0)',
-									transition: 'background 0.2s ease',
-								} }
-							>
-								<button
+							{ this.props.isLaunchpad && (
+								<div
+									className="web-preview__frame-edit-overlay"
 									style={ {
-										position: 'relative',
-										top: this.state.showIFrameOverlay ? '0' : '15px',
-										transition: 'all 0.2s ease',
+										opacity: this.state.showIFrameOverlay ? '1' : '0',
+										background: this.state.showIFrameOverlay
+											? `rgba(16, 21, 23, 0.5)`
+											: 'rgba(16, 21, 23, 0)',
+										transition: 'background 0.2s ease',
 									} }
-									className="web-preview__frame-edit-button"
-									onClick={ () => {
-										window.location.assign( `/site-editor/${ this.props.externalUrl }` );
-									} }
-									onFocus={ () => this.setState( { showIFrameOverlay: true } ) }
-									onBlur={ () => this.setState( { showIFrameOverlay: false } ) }
 								>
-									Edit
-								</button>
-							</div>
+									<button
+										style={ {
+											position: 'relative',
+											top: this.state.showIFrameOverlay ? '0' : '15px',
+											transition: 'all 0.2s ease',
+										} }
+										className="web-preview__frame-edit-button"
+										onClick={ () => {
+											window.location.assign( `/site-editor/${ this.props.externalUrl }` );
+										} }
+										onFocus={ () => this.setState( { showIFrameOverlay: true } ) }
+										onBlur={ () => this.setState( { showIFrameOverlay: false } ) }
+									>
+										{ translate( 'Edit' ) }
+									</button>
+								</div>
+							) }
 						</div>
 					) }
 					{ 'seo' === this.state.device && (
@@ -550,8 +552,6 @@ WebPreviewContent.propTypes = {
 	scrollToSelector: PropTypes.string,
 	// Allow specific functionality for Launchpad
 	isLaunchpad: PropTypes.bool,
-	// Show Edit button on hover
-	showIFrameOverlay: PropTypes.bool,
 };
 
 WebPreviewContent.defaultProps = {
@@ -577,4 +577,5 @@ WebPreviewContent.defaultProps = {
 	autoHeight: false,
 	inlineCss: null,
 	scrollToSelector: null,
+	isLaunchpad: false,
 };
