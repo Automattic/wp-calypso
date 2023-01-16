da1a6dcc7031d3f5b79d6a6b2fc4cd7eb64a7f0a
mpkelly <kikemelly@gmail.com>
2023-01-16 14:56:08 +0300

    Fix UI on smaller screens

--- client/my-sites/hosting/github-authorize-card/index.tsx
+++ client/my-sites/hosting/github-authorize-card/index.tsx
@@ -11,8 +11,10 @@ export const GithubAuthorizeCard = () => {
 	const siteId = useSelector( getSelectedSiteId );
 	return (
 		<Card className="github-authorize-card">
-			<img className="github-icon" src={ iconGitHub } alt="" />
-			<CardHeading>{ translate( 'Connect GitHub' ) }</CardHeading>
+			<div className="github-authorize-card__header">
+				<img className="github-icon" src={ iconGitHub } alt="" />
+				<CardHeading>{ translate( 'Connect GitHub' ) }</CardHeading>
+			</div>
 			<div>
 				<p>{ translate( 'Connect to a GitHub repository and branch and deploy directly.' ) }</p>
 			</div>
