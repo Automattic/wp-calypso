571ffc85f5d5a6dbbb7431b192f568a8ade33ef2
Marin Atanasov <tyxla@abv.bg>
2023-01-17 18:08:45 +0200

    Fix more tests

--- client/my-sites/plugins/plugin-management-v2/test/plugin-row-formatter.test.tsx
+++ client/my-sites/plugins/plugin-management-v2/test/plugin-row-formatter.test.tsx
@@ -1,18 +1,20 @@
 /**
  * @jest-environment jsdom
  */
-import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import moment from 'moment';
 import React from 'react';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
 import { ACTIVATE_PLUGIN } from 'calypso/lib/plugins/constants';
+import documentHead from 'calypso/state/document-head/reducer';
+import plugins from 'calypso/state/plugins/reducer';
+import productsList from 'calypso/state/products-list/reducer';
+import siteConnection from 'calypso/state/site-connection/reducer';
+import { reducer as ui } from 'calypso/state/ui/reducer';
+import { renderWithProvider } from 'calypso/test-helpers/testing-library';
 import PluginRowFormatter from '../plugin-row-formatter';
 import { site, plugin } from './utils/constants';
 
-const initialState = {
+const initialReduxState = {
 	siteConnection: { items: { [ site.ID ]: true } },
 	sites: { items: { [ site.ID ]: site } },
 	currentUser: {
@@ -40,6 +42,13 @@ const initialState = {
 	},
 };
 
+const render = ( el ) =>
+	renderWithProvider( el, {
+		initialState: initialReduxState,
+		reducers: { ui, plugins, documentHead, productsList, siteConnection },
+		store: undefined,
+	} );
+
 const props = {
 	item: plugin,
 	columnKey: 'site-name',
@@ -48,17 +57,8 @@ const props = {
 };
 
 describe( '<PluginRowFormatter>', () => {
-	const middlewares = [ thunk ];
-
-	const mockStore = configureStore( middlewares );
-	const store = mockStore( initialState );
-
 	test( 'should render correctly and show site domain', () => {
-		const { container } = render(
-			<Provider store={ store }>
-				<PluginRowFormatter { ...props } />
-			</Provider>
-		);
+		const { container } = render( <PluginRowFormatter { ...props } /> );
 
 		const [ value ] = container.getElementsByClassName( 'plugin-row-formatter__row-container' );
 		expect( value.textContent ).toEqual( site.domain );
@@ -66,11 +66,7 @@ describe( '<PluginRowFormatter>', () => {
 
 	test( 'should render correctly and show plugin name', () => {
 		props.columnKey = 'plugin';
-		const { container } = render(
-			<Provider store={ store }>
-				<PluginRowFormatter { ...props } />
-			</Provider>
-		);
+		const { container } = render( <PluginRowFormatter { ...props } /> );
 
 		const [ value ] = container.getElementsByClassName( 'plugin-row-formatter__plugin-name' );
 		expect( value.textContent ).toEqual( plugin.name );
@@ -78,11 +74,7 @@ describe( '<PluginRowFormatter>', () => {
 
 	test( 'should render correctly and show site count', () => {
 		props.columnKey = 'sites';
-		const { container } = render(
-			<Provider store={ store }>
-				<PluginRowFormatter { ...props } />
-			</Provider>
-		);
+		const { container } = render( <PluginRowFormatter { ...props } /> );
 
 		const [ value ] = container.getElementsByClassName(
 			'plugin-row-formatter__sites-count-button'
@@ -95,11 +87,7 @@ describe( '<PluginRowFormatter>', () => {
 	test( 'should render correctly and show site count(singular) on small screen', () => {
 		props.columnKey = 'sites';
 		props.isSmallScreen = true;
-		const { getAllByText } = render(
-			<Provider store={ store }>
-				<PluginRowFormatter { ...props } />
-			</Provider>
-		);
+		const { getAllByText } = render( <PluginRowFormatter { ...props } /> );
 
 		const [ autoManagedSite ] = getAllByText(
 			`Installed on ${ Object.keys( plugin.sites ).length } site`
@@ -113,11 +101,7 @@ describe( '<PluginRowFormatter>', () => {
 			[ site.ID ]: { ID: site.ID, canUpdateFiles: true },
 			[ site.ID + 1 ]: { ID: site.ID + 1, canUpdateFiles: true },
 		};
-		const { getAllByText } = render(
-			<Provider store={ store }>
-				<PluginRowFormatter { ...props } />
-			</Provider>
-		);
+		const { getAllByText } = render( <PluginRowFormatter { ...props } /> );
 
 		const [ autoManagedSite ] = getAllByText(
 			`Installed on ${ Object.keys( plugin.sites ).length } sites`
@@ -127,11 +111,7 @@ describe( '<PluginRowFormatter>', () => {
 
 	test( 'should render correctly and show activate and toggle checked value', async () => {
 		props.columnKey = 'activate';
-		const { container } = render(
-			<Provider store={ store }>
-				<PluginRowFormatter { ...props } />
-			</Provider>
-		);
+		const { container } = render( <PluginRowFormatter { ...props } /> );
 
 		const [ value ] = container.getElementsByClassName( 'plugin-row-formatter__toggle' );
 		expect( value.textContent ).toEqual( 'Active' );
@@ -144,11 +124,7 @@ describe( '<PluginRowFormatter>', () => {
 
 	test( 'should render correctly and show auto-update and toggle checked value', async () => {
 		props.columnKey = 'autoupdate';
-		const { container } = render(
-			<Provider store={ store }>
-				<PluginRowFormatter { ...props } />
-			</Provider>
-		);
+		const { container } = render( <PluginRowFormatter { ...props } /> );
 
 		const [ value ] = container.getElementsByClassName( 'plugin-row-formatter__toggle' );
 		expect( value.textContent ).toEqual( 'Autoupdates' );
@@ -161,11 +137,7 @@ describe( '<PluginRowFormatter>', () => {
 
 	test( 'should render correctly and show last updated', () => {
 		props.columnKey = 'last-updated';
-		const { container } = render(
-			<Provider store={ store }>
-				<PluginRowFormatter { ...props } />
-			</Provider>
-		);
+		const { container } = render( <PluginRowFormatter { ...props } /> );
 
 		const [ value ] = container.getElementsByClassName( 'plugin-row-formatter__last-updated' );
 		expect( value.textContent ).toEqual(
@@ -175,11 +147,7 @@ describe( '<PluginRowFormatter>', () => {
 
 	test( 'should render correctly and show install button', () => {
 		props.columnKey = 'install';
-		const { getAllByText } = render(
-			<Provider store={ store }>
-				<PluginRowFormatter { ...props } />
-			</Provider>
-		);
+		const { getAllByText } = render( <PluginRowFormatter { ...props } /> );
 
 		const [ autoManagedSite ] = getAllByText( `Install` );
 		expect( autoManagedSite ).toBeInTheDocument();
