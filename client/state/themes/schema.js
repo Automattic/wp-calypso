6518801c9c3f9081fa8fe60d8b7c0e49ba5cbac0
Noah Allen <noahtallen@gmail.com>
2023-01-17 16:26:11 -0800

    Remove change to schema since it's not needed

--- client/state/themes/schema.js
+++ client/state/themes/schema.js
@@ -15,7 +15,7 @@ const themesSchema = {
 				author_uri: { type: 'string' },
 				autoupdate: { type: 'boolean' },
 				autoupdate_translation: { type: 'boolean' },
-				demo_uri: { type: 'string', required: false },
+				demo_uri: { type: 'string' },
 				description: { type: 'string' },
 				id: { type: 'string' },
 				name: { type: 'string' },
