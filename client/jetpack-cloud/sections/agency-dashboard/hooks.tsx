74605ff39bd8d4d0f3d2cedbc146a35b714c3527
Yashwin <yashwinpoojary@gmail.com>
2023-01-18 10:44:54 +0530

    Update monitor settings properties to match the backend API (#72204)

--- client/jetpack-cloud/sections/agency-dashboard/hooks.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/hooks.tsx
@@ -210,8 +210,8 @@ export function useUpdateMonitorSettings( sites: Array< { blog_id: number; url: 
 								monitor_settings: {
 									...site.monitor_settings,
 									monitor_deferment_time: data.settings.jetmon_defer_status_down_minutes,
-									email_notifications: data.settings.email_notifications,
-									wp_note_notifications: data.settings.wp_note_notifications,
+									monitor_user_email_notifications: data.settings.email_notifications,
+									monitor_user_wp_note_notifications: data.settings.wp_note_notifications,
 								},
 							};
 						}
