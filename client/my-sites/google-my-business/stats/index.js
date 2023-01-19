929261197ea7c923d725308ada03ee5e5b20caaf
Dognose <dognose24@users.noreply.github.com>
2023-01-20 06:09:32 +0800

    Stats: Enhance the Google My Business page with modernized layouts (#72188)
    * Adjust GMB page HTML structure with fresh layouts
    
    * Adjust GMB location layout styles
    
    * Refactor shared layout background colors
    
    * Fix stats card-heading offset

--- client/my-sites/google-my-business/stats/index.js
+++ client/my-sites/google-my-business/stats/index.js
@@ -9,6 +9,7 @@ import DocumentHead from 'calypso/components/data/document-head';
 import QueryKeyringConnections from 'calypso/components/data/query-keyring-connections';
 import QueryKeyringServices from 'calypso/components/data/query-keyring-services';
 import QuerySiteKeyrings from 'calypso/components/data/query-site-keyrings';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { withLocalizedMoment } from 'calypso/components/localized-moment';
 import Main from 'calypso/components/main';
 import Notice from 'calypso/components/notice';
@@ -193,67 +194,89 @@ class GoogleMyBusinessStats extends Component {
 		const { isLocationVerified, locationData, siteId, siteSlug, translate } = this.props;
 
 		return (
-			<Main wideLayout>
+			<Main fullWidthLayout>
 				<PageViewTracker
 					path="/google-my-business/stats/:site"
 					title="Google My Business > Stats"
 				/>
 
-				<DocumentHead title={ translate( 'Stats' ) } />
-
-				<StatsNavigation
-					selectedItem="googleMyBusiness"
-					siteId={ siteId }
-					slug={ siteSlug }
-					isLegacy
-				/>
+				<DocumentHead title={ translate( 'Jetpack Stats' ) } />
 
 				<QuerySiteKeyrings siteId={ siteId } />
 				<QueryKeyringConnections forceRefresh />
 				<QueryKeyringServices />
 
-				{ ! locationData && (
-					<Notice
-						status="is-error"
-						showDismiss={ false }
-						text={ translate( 'There is an error with your Google My Business account.' ) }
-					>
-						<NoticeAction href={ CALYPSO_CONTACT }>{ translate( 'Contact Support' ) }</NoticeAction>
-					</Notice>
-				) }
-
-				{ !! locationData && ! isLocationVerified && (
-					<Notice
-						status="is-error"
-						text={ translate(
-							'Your location has not been verified. ' +
-								'Statistics are not available until you have {{a}}verified your location{{/a}} with Google.',
+				<div className="stats">
+					<FormattedHeader
+						brandFont
+						className="stats__section-header modernized-header"
+						headerText={ translate( 'Google My Business' ) }
+						align="left"
+						subHeaderText={ translate(
+							'Integrate your business with Google and get stats on your locations. {{learnMoreLink}}Learn more{{/learnMoreLink}}',
 							{
 								components: {
-									a: (
+									learnMoreLink: (
 										<a
-											href="https://support.google.com/business/answer/7107242"
+											href="https://wordpress.com/support/google-my-business-integration/#checking-the-impact-of-your-google-my-business-connection"
 											target="_blank"
-											rel="noopener noreferrer"
+											rel="noreferrer noopener"
 										/>
 									),
 								},
 							}
 						) }
 					/>
-				) }
 
-				<GoogleMyBusinessLocation location={ locationData }>
-					<Button
-						href="https://business.google.com/"
-						onClick={ this.trackUpdateListingClick }
-						target="_blank"
-					>
-						{ translate( 'Update Listing' ) } <Gridicon icon="external" />
-					</Button>
-				</GoogleMyBusinessLocation>
+					<StatsNavigation selectedItem="googleMyBusiness" siteId={ siteId } slug={ siteSlug } />
 
-				{ this.renderStats() }
+					{ ! locationData && (
+						<Notice
+							status="is-error"
+							showDismiss={ false }
+							text={ translate( 'There is an error with your Google My Business account.' ) }
+						>
+							<NoticeAction href={ CALYPSO_CONTACT }>
+								{ translate( 'Contact Support' ) }
+							</NoticeAction>
+						</Notice>
+					) }
+
+					{ !! locationData && ! isLocationVerified && (
+						<Notice
+							status="is-error"
+							text={ translate(
+								'Your location has not been verified. ' +
+									'Statistics are not available until you have {{a}}verified your location{{/a}} with Google.',
+								{
+									components: {
+										a: (
+											<a
+												href="https://support.google.com/business/answer/7107242"
+												target="_blank"
+												rel="noopener noreferrer"
+											/>
+										),
+									},
+								}
+							) }
+						/>
+					) }
+
+					<div className="stats__gmb-location-wrapper">
+						<GoogleMyBusinessLocation location={ locationData }>
+							<Button
+								href="https://business.google.com/"
+								onClick={ this.trackUpdateListingClick }
+								target="_blank"
+							>
+								{ translate( 'Update Listing' ) } <Gridicon icon="external" />
+							</Button>
+						</GoogleMyBusinessLocation>
+					</div>
+
+					{ this.renderStats() }
+				</div>
 			</Main>
 		);
 	}
