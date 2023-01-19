4aba7ef90cffa261837f6d2c8bbb7f5b32b7f0c4
Candy Tsai <candy02058912@gmail.com>
2023-01-19 15:59:26 +0800

    Handle empty blocks case

--- client/my-sites/stats/mini-carousel/index.jsx
+++ client/my-sites/stats/mini-carousel/index.jsx
@@ -25,32 +25,45 @@ const MiniCarousel = ( { slug, isOdysseyStats } ) => {
 	// Yoast promo is disabled for Odyssey & self-hosted & non-traffic pages.
 	const showYoastPromo = ! isOdysseyStats && ! jetpackNonAtomic;
 
+	const blocks = [];
+
+	if ( showBlazePromo ) {
+		blocks.push(
+			<MiniCarouselBlock
+				clickEvent="calypso_stats_traffic_blaze_banner_click"
+				image={ <BlazeLogo className="mini-carousel-blaze" size={ 45 } /> }
+				headerText={ translate( 'Promote your content with Blaze' ) }
+				contentText={ translate(
+					'Grow your audience by promoting your content. Reach millions of users across Tumblr and WordPress.com'
+				) }
+				ctaText={ translate( 'Create campaign' ) }
+				href={ `/advertising/${ slug || '' }` }
+			/>
+		);
+	}
+
+	if ( showYoastPromo ) {
+		blocks.push(
+			<MiniCarouselBlock
+				clickEvent="calypso_stats_wordpress_seo_premium_banner_click"
+				image={ <img src={ YoastLogo } alt="" width={ 45 } height={ 45 } /> }
+				headerText={ translate( 'Increase your site visitors with Yoast SEO Premium' ) }
+				contentText={ translate(
+					'Purchase Yoast SEO Premium to ensure that more people find your incredible content.'
+				) }
+				ctaText={ translate( 'Get Yoast' ) }
+				href={ `/plugins/wordpress-seo-premium/${ slug || '' }` }
+			/>
+		);
+	}
+
+	if ( blocks.length === 0 ) {
+		return null;
+	}
+
 	return (
 		<DotPager className="mini-carousel" hasDynamicHeight>
-			{ showBlazePromo && (
-				<MiniCarouselBlock
-					clickEvent="calypso_stats_traffic_blaze_banner_click"
-					image={ <BlazeLogo className="mini-carousel-blaze" size={ 45 } /> }
-					headerText={ translate( 'Promote your content with Blaze' ) }
-					contentText={ translate(
-						'Grow your audience by promoting your content. Reach millions of users across Tumblr and WordPress.com'
-					) }
-					ctaText={ translate( 'Create campaign' ) }
-					href={ `/advertising/${ slug || '' }` }
-				/>
-			) }
-			{ showYoastPromo && (
-				<MiniCarouselBlock
-					clickEvent="calypso_stats_wordpress_seo_premium_banner_click"
-					image={ <img src={ YoastLogo } alt="" width={ 45 } height={ 45 } /> }
-					headerText={ translate( 'Increase your site visitors with Yoast SEO Premium' ) }
-					contentText={ translate(
-						'Purchase Yoast SEO Premium to ensure that more people find your incredible content.'
-					) }
-					ctaText={ translate( 'Get Yoast' ) }
-					href={ `/plugins/wordpress-seo-premium/${ slug || '' }` }
-				/>
-			) }
+			{ blocks }
 		</DotPager>
 	);
 };
