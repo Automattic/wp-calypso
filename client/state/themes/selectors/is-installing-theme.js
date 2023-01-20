e2a79d8e70d5d5e0e2e4a93e30e27da53d245d6f
mmtr <miguel.torres@automattic.com>
2023-01-20 17:16:08 +0100

    Themes: Show loading modal after installing Dotorg themes
    Rebasing Miguel's work on top of trunk, resolving a merge conflict in
    `is-installing-`theme`. trunk had corrected a typo in the jsdoc, this
    branch had removed that jsdoc & function as it's already in
    `suffix-theme-id-for-install`.
    
    Author:    mmtr <miguel.torres@automattic.com>

--- client/state/themes/selectors/is-installing-theme.js
+++ client/state/themes/selectors/is-installing-theme.js
@@ -1,27 +1,8 @@
 import { isJetpackSite } from 'calypso/state/sites/selectors';
+import { suffixThemeIdForInstall } from 'calypso/state/themes/actions/suffix-theme-id-for-install';
 import { getTheme } from 'calypso/state/themes/selectors/get-theme';
 
 import 'calypso/state/themes/init';
-
-/**
- * When wpcom themes are installed on Jetpack sites, the
- * theme id is suffixed with -wpcom. Some operations require
- * the use of this suffixed ID. This util function adds the
- * suffix if the site is jetpack and the theme is not yet
- * installed on the site.
- *
- * @param {Object} state	Global state tree
- * @param {string} themeId	Theme ID
- * @param {number} siteId	Site ID
- * @returns {string} 		Potentially suffixed theme ID
- */
-const getSuffixedThemeId = ( state, themeId, siteId ) => {
-	const siteIsJetpack = siteId && isJetpackSite( state, siteId );
-	if ( siteIsJetpack && ! getTheme( state, siteId, themeId ) ) {
-		return `${ themeId }-wpcom`;
-	}
-	return themeId;
-};
 
 /**
  * Whether the theme is currently being installed on the (Jetpack) site.
@@ -32,6 +13,9 @@ const getSuffixedThemeId = ( state, themeId, siteId ) => {
  * @returns {boolean}         True if theme installation is ongoing
  */
 export function isInstallingTheme( state, themeId, siteId ) {
-	const suffixedThemeId = getSuffixedThemeId( state, themeId, siteId );
+	let suffixedThemeId = themeId;
+	if ( isJetpackSite( state, siteId ) && ! getTheme( state, siteId, themeId ) ) {
+		suffixedThemeId = suffixThemeIdForInstall( state, siteId, themeId );
+	}
 	return state.themes.themeInstalls[ siteId ]?.[ suffixedThemeId ] ?? false;
 }
