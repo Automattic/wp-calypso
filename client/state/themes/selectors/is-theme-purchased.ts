d82c4bccbb789575d0066c096de40c2654d7fd20
Dale du Preez <dale@automattic.com>
2023-01-24 12:08:23 +0200

    Improve spacing in JSDoc and specify the marketplace function

--- client/state/themes/selectors/is-theme-purchased.ts
+++ client/state/themes/selectors/is-theme-purchased.ts
@@ -4,15 +4,16 @@ import type { AppState } from 'calypso/types';
 import 'calypso/state/themes/init';
 
 /**
- * Returns whether the theme has been purchased for the given site. Note that this only works for
- * premium themes that are added to A8c repos -- marketplace themes need to be checked via a different approach.
+ * Returns whether the theme has been purchased for the given site. Note that
+ * this only works for premium themes that are added to A8c repos --
+ * marketplace themes need to be checked via isMarketplaceThemeSubscribed().
  *
  * Use this selector alongside with the <QuerySitePurchases /> component.
  *
- * @param {AppState} state Global state tree
- * @param {string} themeId Theme ID
- * @param {number} siteId  Site ID
- * @returns {boolean}      True if the theme has been purchased for the site
+ * @param   {AppState} state   Global state tree
+ * @param   {string}   themeId Theme ID
+ * @param   {number}   siteId  Site ID
+ * @returns {boolean}          True if the theme has been purchased for the site
  */
 export function isThemePurchased(
 	state: AppState,
