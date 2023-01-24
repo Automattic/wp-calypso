cb5b9358984eee37bbd89d515eb84f6972081cbb
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2023-01-24 18:05:19 +0200

    Jetpack Cloud: Make `jetpack-cloud-pricing` section isomorphic

---
+++ client/jetpack-cloud/sections/pricing/index.node.ts
@@ -0,0 +1,9 @@
+import { getLanguageRouteParam } from '@automattic/i18n-utils';
+import { makeLayout, setLocaleMiddleware } from 'calypso/controller';
+import { serverRouter } from 'calypso/server/isomorphic-routing';
+
+export default ( router: ReturnType< typeof serverRouter > ) => {
+	const lang = getLanguageRouteParam();
+
+	router( `/${ lang }/pricing/*`, setLocaleMiddleware(), makeLayout );
+};
