24dcca0431f059410db269c2cd38bca479e8f1d4
Payton Swick <payton@foolord.com>
2023-01-24 19:58:05 -0500

    Add "x/year" to intro offer "after first renewal" text in PurchaseMeta

--- client/me/purchases/manage-purchase/purchase-meta-introductory-offer-detail.tsx
+++ client/me/purchases/manage-purchase/purchase-meta-introductory-offer-detail.tsx
@@ -1,3 +1,9 @@
+import {
+	PLAN_ANNUAL_PERIOD,
+	PLAN_BIENNIAL_PERIOD,
+	PLAN_MONTHLY_PERIOD,
+	PLAN_TRIENNIAL_PERIOD,
+} from '@automattic/calypso-products';
 import formatCurrency from '@automattic/format-currency';
 import { useTranslate } from 'i18n-calypso';
 import { isWithinIntroductoryOfferPeriod } from 'calypso/lib/purchases';
@@ -9,9 +15,26 @@ function PurchaseMetaIntroductoryOfferDetail( { purchase }: { purchase: Purchase
 	if ( ! isWithinIntroductoryOfferPeriod( purchase ) ) {
 		return null;
 	}
+
+	const getPeriod = () => {
+		switch ( purchase.billPeriodDays ) {
+			case PLAN_TRIENNIAL_PERIOD:
+				return translate( 'three years' );
+			case PLAN_BIENNIAL_PERIOD:
+				return translate( 'two years' );
+			case PLAN_ANNUAL_PERIOD:
+				return translate( 'year' );
+			case PLAN_MONTHLY_PERIOD:
+				return translate( 'month' );
+		}
+		return null;
+	};
+
 	if ( purchase?.introductoryOffer && purchase.introductoryOffer !== null ) {
+		const timePeriod = getPeriod();
 		let regularPriceText = null;
-		if ( purchase.introductoryOffer.isNextRenewalUsingOffer ) {
+
+		if ( ! timePeriod && purchase.introductoryOffer.isNextRenewalUsingOffer ) {
 			regularPriceText = translate(
 				'After the offer ends, the subscription price will be %(regularPrice)s',
 				{
@@ -23,7 +46,13 @@ function PurchaseMetaIntroductoryOfferDetail( { purchase }: { purchase: Purchase
 					},
 				}
 			);
-		} else if ( purchase.introductoryOffer.isNextRenewalProrated ) {
+		}
+
+		if (
+			! timePeriod &&
+			! purchase.introductoryOffer.isNextRenewalUsingOffer &&
+			purchase.introductoryOffer.isNextRenewalProrated
+		) {
 			regularPriceText = translate(
 				'After the first renewal, the subscription price will be %(regularPrice)s',
 				{
@@ -32,6 +61,40 @@ function PurchaseMetaIntroductoryOfferDetail( { purchase }: { purchase: Purchase
 							isSmallestUnit: true,
 							stripZeros: true,
 						} ),
+					},
+				}
+			);
+		}
+
+		if ( timePeriod && purchase.introductoryOffer.isNextRenewalUsingOffer ) {
+			regularPriceText = translate(
+				'After the offer ends, the subscription price will be %(regularPrice)s / %(timePeriod)',
+				{
+					args: {
+						regularPrice: formatCurrency( purchase.regularPriceInteger, purchase.currencyCode, {
+							isSmallestUnit: true,
+							stripZeros: true,
+						} ),
+						timePeriod,
+					},
+				}
+			);
+		}
+
+		if (
+			timePeriod &&
+			! purchase.introductoryOffer.isNextRenewalUsingOffer &&
+			purchase.introductoryOffer.isNextRenewalProrated
+		) {
+			regularPriceText = translate(
+				'After the first renewal, the subscription price will be %(regularPrice)s / %(timePeriod)',
+				{
+					args: {
+						regularPrice: formatCurrency( purchase.regularPriceInteger, purchase.currencyCode, {
+							isSmallestUnit: true,
+							stripZeros: true,
+						} ),
+						timePeriod,
 					},
 				}
 			);
