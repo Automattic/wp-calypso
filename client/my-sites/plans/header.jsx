17d37ff75097653dfa19ef972781bb34045cefd2
Paulo Trentin <paulo@paulotrentin.com.br>
2023-01-25 16:35:58 -0300

    Added upsell modal on plans page connected to skip button

--- client/my-sites/plans/header.jsx
+++ client/my-sites/plans/header.jsx
@@ -1,12 +1,15 @@
 import config from '@automattic/calypso-config';
-import { Button, Gridicon } from '@automattic/components';
+import { Button, Dialog, Gridicon } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
+import { useState } from 'react';
 import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import './header.scss';
 
 export default function PlansHeader() {
 	const translate = useTranslate();
+	const [ showDomainUpsellDialog, setShowDomainUpsellDialog ] = useState( false );
+
 	// TODO: We need to determine if this is a domain upsell and show the domain here.
 	const domainName = 'exampledomain.com';
 
@@ -30,15 +33,59 @@ export default function PlansHeader() {
 	const isDomainUpsell = config.isEnabled( 'is_domain_upsell' );
 
 	const onBackClick = () => {
-		recordTracksEvent( 'calypso_upgrade_nudge_back_click' );
+		recordTracksEvent( 'calypso_plans_page_domain_upsell_back_click' );
 		history.back();
 	};
 
-	const onSkipClick = () => {
-		recordTracksEvent( 'calypso_upgrade_nudge_skip_click' );
-		// TODO: Connect this to the skip modal.
-		alert( 'Skip Clicked' );
+	const onCloseDialog = () => {
+		setShowDomainUpsellDialog( false );
 	};
+
+	const onSkipClick = () => {
+		event.preventDefault();
+		recordTracksEvent( 'calypso_plans_page_domain_upsell_skip_click' );
+		setShowDomainUpsellDialog( true );
+	};
+
+	function renderDeleteDialog() {
+		const buttons = [
+			{ action: 'cancel', label: translate( 'That works for me' ) },
+			{ action: 'delete', label: translate( 'I want my domain as primary' ), isPrimary: true },
+		];
+
+		return (
+			<Dialog
+				additionalClassNames="planspage__domain-upsell"
+				isVisible={ showDomainUpsellDialog }
+				buttons={ buttons }
+				onClose={ onCloseDialog }
+				shouldCloseOnEsc={ true }
+			>
+				<header className="domain-upsell__modal-header">
+					<button onClick={ onCloseDialog }>
+						<Gridicon icon="cross" />
+					</button>
+				</header>
+
+				<h1>{ translate( 'You need a plan to have a primary custom domain' ) }</h1>
+				<p>
+					{ translate(
+						'Any domain you purchase without a plan will get redirected to %(domainName)s.',
+						{
+							args: {
+								domainName: domainName,
+							},
+						}
+					) }
+				</p>
+				<p>
+					{ translate(
+						'If you upgrade to a plan, you can use your custom domain name instead of having WordPress.com in your URL.'
+					) }
+				</p>
+			</Dialog>
+		);
+	}
 
 	if ( false === isDomainUpsell ) {
 		return (
@@ -53,6 +100,7 @@ export default function PlansHeader() {
 
 	return (
 		<>
+			{ renderDeleteDialog() }
 			<header className="formatted-header navigation">
 				<Button onClick={ onBackClick } className="inline-help__cancel-button" borderless>
 					<Gridicon icon="arrow-left" size={ 18 } />
