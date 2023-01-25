05069c040e159650f39f0cadf4c4371d62b0e772
Paulo Trentin <paulo@paulotrentin.com.br>
2023-01-25 11:43:02 -0300

    Added a feature flag to show the upsell domain on plans page

--- client/my-sites/plans/header.jsx
+++ client/my-sites/plans/header.jsx
@@ -1,3 +1,4 @@
+import config from '@automattic/calypso-config';
 import { Button, Gridicon } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import FormattedHeader from 'calypso/components/formatted-header';
@@ -7,9 +8,13 @@ import './header.scss';
 export default function PlansHeader() {
 	const translate = useTranslate();
 	// TODO: We need to determine if this is a domain upsell and show the domain here.
-	const domainName = 'a domain';
+	const domainName = 'exampledomain.com';
 
-	const description = translate(
+	const plansDescription = translate(
+		'See and compare the features available on each WordPress.com plan.'
+	);
+
+	const domainUpsellDescription = translate(
 		'With an annual plan, you can get {{strong}}%(domainName)s for free{{/strong}} for the first year, Jetpack essential features, live chat support, and all the features that will take your site to the next level.',
 		{
 			args: {
@@ -22,7 +27,7 @@ export default function PlansHeader() {
 	);
 
 	// TODO: We need to determine if this is a domain upsell.
-	const isDomainUpsell = false;
+	const isDomainUpsell = config.isEnabled( 'is_domain_upsell' );
 
 	const onBackClick = () => {
 		recordTracksEvent( 'calypso_upgrade_nudge_back_click' );
@@ -34,6 +39,17 @@ export default function PlansHeader() {
 		// TODO: Connect this to the skip modal.
 		alert( 'Skip Clicked' );
 	};
+
+	if ( false === isDomainUpsell ) {
+		return (
+			<FormattedHeader
+				brandFont
+				headerText={ translate( 'Plans' ) }
+				subHeaderText={ plansDescription }
+				align="left"
+			/>
+		);
+	}
 
 	return (
 		<>
@@ -55,7 +71,7 @@ export default function PlansHeader() {
 				className="header-text"
 				brandFont
 				headerText={ translate( 'Free for the first year!' ) }
-				subHeaderText={ description }
+				subHeaderText={ domainUpsellDescription }
 				align="left"
 			/>
 		</>
