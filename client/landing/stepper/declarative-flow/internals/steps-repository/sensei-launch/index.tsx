35db1f41e7d52fc77ee3df90058a45cdbcdb5082
Peter Kiss <peter@yscik.com>
2023-01-26 21:49:39 +0100

    Install additional plugins in Sensei launch

--- client/landing/stepper/declarative-flow/internals/steps-repository/sensei-launch/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/sensei-launch/index.tsx
@@ -1,12 +1,18 @@
 import { useCallback, useEffect, useState } from '@wordpress/element';
 import { useI18n } from '@wordpress/react-i18n';
+import React, { useMemo } from 'react';
 import { useSelector, useDispatch as useRootDispatch } from 'react-redux';
+import DocumentHead from 'calypso/components/data/document-head';
 import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
-import { fetchSitePlugins } from 'calypso/state/plugins/installed/actions';
+import {
+	fetchSitePlugins,
+	installPlugin as installPluginAction,
+} from 'calypso/state/plugins/installed/actions';
 import { getPlugins } from 'calypso/state/plugins/installed/selectors';
 import { SenseiStepContainer } from '../components/sensei-step-container';
 import { Progress, SenseiStepProgress } from '../components/sensei-step-progress';
+import { getSelectedPlugins, getSelectedPurposes, Plugin } from '../sensei-purpose/purposes';
 import type { Step } from '../../types';
 
 import './style.scss';
@@ -17,11 +23,12 @@ interface InstalledPlugin {
 	active: boolean;
 }
 
-const SenseiLaunch: Step = ( { navigation: { submit } } ) => {
-	const { __ } = useI18n();
-	const site = useSite();
-	const siteId = site?.ID;
-	const [ retries, setRetries ] = useState< number >( 0 );
+function useAtomicSitePlugins() {
+	const dispatch = useRootDispatch();
+
+	const [ pluginQueue, setPluginQueue ] = useState< Plugin[] >( [] );
+
+	const siteId = useSite()?.ID;
 
 	const selectSitePlugins = useCallback(
 		( state ) => {
@@ -29,35 +36,76 @@ const SenseiLaunch: Step = ( { navigation: { submit } } ) => {
 		},
 		[ siteId ]
 	);
-	const dispatch = useRootDispatch();
+
 	const plugins: InstalledPlugin[] = useSelector( selectSitePlugins );
+
+	const pollPlugins = useCallback(
+		() => dispatch( fetchSitePlugins( siteId ) ),
+		[ dispatch, siteId ]
+	);
+
+	const isPluginInstalled = useCallback(
+		( slug: string ) => plugins.find( ( plugin ) => plugin.slug === slug )?.active,
+		[ plugins ]
+	);
+
+	useEffect( () => {
+		if ( pluginQueue.length && plugins?.length ) {
+			for ( const plugin of pluginQueue ) {
+				dispatch( installPluginAction( siteId, plugin, true ) );
+			}
+			setPluginQueue( [] );
+		}
+	}, [ dispatch, plugins?.length, pluginQueue, siteId ] );
+
+	const queuePlugin = useCallback(
+		( plugin: Plugin ) => setPluginQueue( ( queue ) => [ ...queue, plugin ] ),
+		[]
+	);
+
+	return { pollPlugins, isPluginInstalled, queuePlugin };
+}
+
+const SENSEI_PRO_PLUGIN_SLUG = 'woothemes-sensei';
+
+const SenseiLaunch: Step = ( { navigation: { submit } } ) => {
+	const { __ } = useI18n();
+	const [ retries, setRetries ] = useState< number >( 0 );
+
 	const expectedRetries = 15;
 	const maxRetries = 40;
 
+	const { pollPlugins, isPluginInstalled, queuePlugin } = useAtomicSitePlugins();
+
+	const additionalPlugins = useMemo( () => getSelectedPlugins( getSelectedPurposes() ), [] );
+
+	useEffect( () => {
+		additionalPlugins.forEach( queuePlugin );
+	}, [ additionalPlugins, queuePlugin ] );
+
 	useEffect( () => {
 		const intervalId = setInterval( () => {
-			const woothemesSensei = plugins.find( ( plugin ) => plugin.slug === 'woothemes-sensei' );
-			if ( ! woothemesSensei?.active && retries < maxRetries ) {
+			if ( ! [ SENSEI_PRO_PLUGIN_SLUG ].every( isPluginInstalled ) && retries < maxRetries ) {
 				setRetries( retries + 1 );
-				dispatch( fetchSitePlugins( siteId ) );
+				pollPlugins();
 				return;
 			}
 
-			setRetries( -1 );
 			clearInterval( intervalId );
+			setRetries( maxRetries );
 
 			setTimeout( () => submit?.(), 800 );
 		}, 3000 );
 
 		return () => clearInterval( intervalId );
-	}, [ plugins, dispatch, siteId, retries, submit ] );
+	}, [ retries, isPluginInstalled, submit, pollPlugins ] );
 
 	const progress: Progress = {
 		percentage: ( retries * 100 ) / expectedRetries,
 		title: __( 'Installing Sensei' ),
 	};
 
-	if ( retries > expectedRetries / 2 || retries < 0 ) {
+	if ( retries > expectedRetries / 2 || maxRetries === retries ) {
 		progress.title = __( 'Setting up your new Sensei Home' );
 	}
 
@@ -70,9 +118,12 @@ const SenseiLaunch: Step = ( { navigation: { submit } } ) => {
 	}
 
 	return (
-		<SenseiStepContainer stepName="senseiSetup" recordTracksEvent={ recordTracksEvent }>
-			<SenseiStepProgress progress={ progress } />
-		</SenseiStepContainer>
+		<>
+			<DocumentHead title={ progress.title } />
+			<SenseiStepContainer stepName="senseiSetup" recordTracksEvent={ recordTracksEvent }>
+				<SenseiStepProgress progress={ progress } />
+			</SenseiStepContainer>
+		</>
 	);
 };
 
