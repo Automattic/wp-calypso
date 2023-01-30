a6b3d05cb9567a533b7efbc64c0f3445bcdcdc18
Miguel <miguel.san.segundo@automattic.com>
2023-01-30 13:47:57 +0700

    Disable trim_content to create posts in the user site without limit

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/index.tsx
@@ -257,7 +257,7 @@ const PatternAssembler: Step = ( { navigation, flow } ) => {
 						createCustomHomeTemplateContent( stylesheet, !! header, !! footer, !! sections.length )
 					)
 				)
-				.then( () => runThemeSetupOnSite( siteSlugOrId, design, { trimContent: true } ) )
+				.then( () => runThemeSetupOnSite( siteSlugOrId, design, { trimContent: false } ) )
 				.then( () => reduxDispatch( requestActiveTheme( site?.ID || -1 ) ) )
 		);
 
