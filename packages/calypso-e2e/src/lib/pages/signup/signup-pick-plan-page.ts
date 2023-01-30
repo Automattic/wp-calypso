a1fb735c02baf62221d327dec14784fe589f7366
Edwin Takahashi <egao@outlook.com>
2023-01-30 13:55:21 -0800

    packages/calypso-e2e/src/lib/pages/plans-page.ts - remove a `waitForNavigation` from plans-page.
    packages/calypso-e2e/src/lib/pages/signup/signup-pick-plan-page.ts
    - add wait for URL and load state.
    - output the response body as error log if something goes wrong.

--- packages/calypso-e2e/src/lib/pages/signup/signup-pick-plan-page.ts
+++ packages/calypso-e2e/src/lib/pages/signup/signup-pick-plan-page.ts
@@ -28,17 +28,26 @@ export class SignupPickPlanPage {
 	 * @returns {Promise<SiteDetails>} Details of the newly created site.
 	 */
 	async selectPlan( name: Plans ): Promise< NewSiteResponse > {
+		await Promise.all( [
+			this.page.waitForURL( /.*start\/plans.*/ ),
+			this.page.waitForLoadState(),
+		] );
+
 		const [ response ] = await Promise.all( [
 			this.page.waitForResponse( /.*sites\/new\?.*/ ),
+			// Should redirect to the Checkout cart and this may
+			// take some time.
+			this.page.waitForURL( /.*checkout.*/, { timeout: 30 * 1000 } ),
 			this.plansPage.selectPlan( name ),
 		] );
 
-		if ( ! response ) {
-			throw new Error( 'Failed to create new site when selecting a plan at signup.' );
-		}
-
 		const responseJSON = await response.json();
 		const body: NewSiteResponse = responseJSON.body;
+
+		if ( ! body.blog_details.blogid ) {
+			console.error( body );
+			throw new Error( 'Failed to create new site when selecting a plan at signup.' );
+		}
 
 		// Cast the blogID value to a number, in case it comes in as a string.
 		body.blog_details.blogid = Number( body.blog_details.blogid );
