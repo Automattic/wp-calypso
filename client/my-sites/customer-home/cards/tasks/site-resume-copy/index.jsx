73ab108857a38472fd23194958fb57cc0efc9a13
Wojtek Naruniec <wojtek@naruniec.me>
2023-01-30 20:24:40 +0100

    Add initial support for resuming Copy Site process

--- client/my-sites/customer-home/cards/tasks/site-resume-copy/index.jsx
+++ client/my-sites/customer-home/cards/tasks/site-resume-copy/index.jsx
@@ -1,13 +1,16 @@
 import { useTranslate } from 'i18n-calypso';
 import { connect } from 'react-redux';
 import siteCopyIllustration from 'calypso/assets/images/customer-home/illustration--import-complete.svg';
+import { useSiteCopy } from 'calypso/landing/stepper/hooks/use-site-copy';
 import { preventWidows } from 'calypso/lib/formatting';
 import { TASK_SITE_RESUME_COPY } from 'calypso/my-sites/customer-home/cards/constants';
 import Task from 'calypso/my-sites/customer-home/cards/tasks/task';
+import { getSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
-const SiteResumeCopy = ( { siteSlug } ) => {
+const SiteResumeCopy = ( { siteSlug, sourceSite, sourceSiteSlug } ) => {
 	const translate = useTranslate();
+	const { startSiteCopy } = useSiteCopy( sourceSite );
 
 	return (
 		<Task
@@ -19,7 +22,10 @@ const SiteResumeCopy = ( { siteSlug } ) => {
 				)
 			) }
 			actionText={ translate( 'Resume copying' ) }
-			actionUrl={ `/setup/copy-site/${ siteSlug }` }
+			actionUrl={ `/setup/copy-site?sourceSlug=${ sourceSiteSlug }&siteSlug=${ siteSlug }` }
+			actionOnClick={ () => {
+				startSiteCopy( 'calypso_resume_copy_site_click' );
+			} }
 			illustration={ siteCopyIllustration }
 			taskId={ TASK_SITE_RESUME_COPY }
 		/>
@@ -27,8 +33,12 @@ const SiteResumeCopy = ( { siteSlug } ) => {
 };
 
 const mapStateToProps = ( state ) => {
+	const sourceSiteSlug = 'source1public2cloning3test.wpcomstaging.com';
+
 	return {
 		siteSlug: getSelectedSiteSlug( state ),
+		sourceSite: getSite( state, sourceSiteSlug ),
+		sourceSiteSlug,
 	};
 };
 
