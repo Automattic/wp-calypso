be4f05874f582bc6177e76bb6393192f9bb4bdf4
Antonio Sejas <antonio@sejas.es>
2023-01-30 10:34:39 +0000

    Copy site: add flow validation to check if `sourceSlug` is a valid site (#72714)
    * Copy Site: update useSiteCopy to accept object with few properties
    
    * Copy Site: create function useIsValidSite that fetch and returns if a site is valid
    
    * Copy Site: move validation to useAssertConditions
    
    * Copy Site: fix empty redux using SITE_STORE.isSiteAtomic
    
    * Copy Site: check siteFeatures isRequesting and empty slug
    
    * Copy Site: add loading state
    
    * Copy Site: move initCopySite outside of setPendingAction due a bug executing the pending actions multiple times
    
    ---------
    
    Co-authored-by: Antony Agrios <antonyagrios@gmail.com>

--- client/landing/stepper/declarative-flow/copy-site.tsx
+++ client/landing/stepper/declarative-flow/copy-site.tsx
@@ -1,10 +1,10 @@
 import { useFlowProgress, COPY_SITE_FLOW } from '@automattic/onboarding';
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { addQueryArgs } from '@wordpress/url';
 import { translate } from 'i18n-calypso';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
-import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
+import { ONBOARD_STORE, SITE_STORE } from 'calypso/landing/stepper/stores';
 import { recordFullStoryEvent } from 'calypso/lib/analytics/fullstory';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import {
@@ -13,12 +13,59 @@ import {
 	persistSignupDestination,
 	setSignupCompleteFlowName,
 } from 'calypso/signup/storageUtils';
+import { useSiteCopy } from '../hooks/use-site-copy';
 import { recordSubmitStep } from './internals/analytics/record-submit-step';
 import AutomatedCopySite from './internals/steps-repository/automated-copy-site';
 import DomainsStep from './internals/steps-repository/domains';
 import ProcessingStep, { ProcessingResult } from './internals/steps-repository/processing-step';
 import SiteCreationStep from './internals/steps-repository/site-creation-step';
-import type { Flow, ProvidedDependencies } from './internals/types';
+import {
+	AssertConditionResult,
+	AssertConditionState,
+	Flow,
+	ProvidedDependencies,
+} from './internals/types';
+
+function useIsValidSite() {
+	const urlQueryParams = useQuery();
+	const sourceSlug = urlQueryParams.get( 'sourceSlug' );
+	const [ siteRequestStatus, setSiteRequestStatus ] = useState< 'init' | 'fetching' | 'finished' >(
+		'init'
+	);
+
+	const {
+		isFetchingSiteDetails,
+		isFetchingError,
+		site: sourceSite,
+	} = useSelect( ( select ) => {
+		if ( ! sourceSlug ) {
+			return {};
+		}
+		return {
+			isFetchingError: select( SITE_STORE ).getFetchingSiteError(),
+			isFetchingSiteDetails: select( SITE_STORE ).isFetchingSiteDetails(),
+			site: select( SITE_STORE ).getSite( sourceSlug ),
+		};
+	} );
+
+	useEffect( () => {
+		if ( isFetchingSiteDetails && siteRequestStatus === 'init' ) {
+			setSiteRequestStatus( 'fetching' );
+		} else if (
+			( ! isFetchingSiteDetails && siteRequestStatus === 'fetching' ) ||
+			sourceSite?.ID
+		) {
+			setSiteRequestStatus( 'finished' );
+		}
+	}, [ isFetchingSiteDetails, siteRequestStatus, sourceSite?.ID ] );
+
+	const { shouldShowSiteCopyItem, isFetching: isFetchingSiteCopy } = useSiteCopy( sourceSite );
+	return {
+		isValidSite: shouldShowSiteCopyItem,
+		hasFetchedSiteDetails: siteRequestStatus === 'finished' && ! isFetchingSiteCopy,
+		isFetchingError,
+	};
+}
 
 const copySite: Flow = {
 	name: COPY_SITE_FLOW,
@@ -32,13 +79,6 @@ const copySite: Flow = {
 			recordTracksEvent( 'calypso_signup_start', { flow: this.name } );
 			recordFullStoryEvent( 'calypso_signup_start_copy_site', { flow: this.name } );
 		}, [] );
-
-		const urlQueryParams = useQuery();
-		const sourceSlug = urlQueryParams.get( 'sourceSlug' );
-		if ( ! sourceSlug ) {
-			window.location.assign( `/sites` );
-			return [];
-		}
 
 		return [
 			{ slug: 'domains', component: DomainsStep },
@@ -131,6 +171,30 @@ const copySite: Flow = {
 		};
 
 		return { goNext, goBack, goToStep, submit };
+	},
+
+	useAssertConditions() {
+		let result: AssertConditionResult = { state: AssertConditionState.SUCCESS };
+
+		const urlQueryParams = useQuery();
+		const sourceSlug = urlQueryParams.get( 'sourceSlug' );
+		const { isValidSite, hasFetchedSiteDetails, isFetchingError } = useIsValidSite();
+
+		if ( ! sourceSlug || isFetchingError || ( ! isValidSite && hasFetchedSiteDetails ) ) {
+			window.location.assign( `/sites` );
+			result = {
+				state: AssertConditionState.FAILURE,
+				message: isFetchingError
+					? 'Copy Site flow couldn´t fetch source site details.'
+					: 'Copy Site flow requires a valid source site.',
+			};
+		} else if ( ! hasFetchedSiteDetails ) {
+			result = {
+				state: AssertConditionState.CHECKING,
+			};
+		}
+
+		return result;
 	},
 };
 
