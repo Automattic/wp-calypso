9ac94e1eb84efdd481e5f08ef30584afb7a1ee4b
Dehan <dehandecroos@gmail.com>
2023-01-30 12:41:13 +0530

    Cleanup unused groups

--- packages/calypso-products/src/constants/feature-group.ts
+++ packages/calypso-products/src/constants/feature-group.ts
@@ -3,7 +3,6 @@ export const FEATURE_GROUP_PERFORMANCE_BOOSTERS = 'feature-group-performance-boo
 export const FEATURE_GROUP_HIGH_AVAILABILITY = 'feature-group-high-availability';
 export const FEATURE_GROUP_DEVELOPER_TOOLS = 'feature-group-developer-tools';
 export const FEATURE_GROUP_SECURITY_AND_SAFETY = 'feature-group-security-and-safety';
-export const FEATURE_GROUP_INNOVATIVE_TECHNOLOGIES = 'feature-group-innovative-technologies';
 export const FEATURE_GROUP_THEMES_AND_CUSTOMIZATION = 'feature-group-themes-and-customization';
 export const FEATURE_GROUP_SUPERIOR_COMMERCE_SOLUTIONS =
 	'feature-group-superior-commerce-solutions';
