dede6a81a00e444914fb4e7e48bc8896c126edbd
Edwin Takahashi <egao@outlook.com>
2023-01-30 16:11:11 -0800

    CI: use 100% of available CPUs when running E2E tests. (#72690)
    * .teamcity/settings.kts
    - try increasing parallelization of jest to 24
    
    * .teamcity/settings.kts
    - rename `E2E_WORKERS` to `JEST_E2E_WORKERS`
    - set `JEST_E2E_WORKERS` to 100% of the CPU count
    
    ./teamcity/_self/
    - replace usage of `E2E_WORKERS`

--- .teamcity/_self/projects/MarTech.kt
+++ .teamcity/_self/projects/MarTech.kt
@@ -76,7 +76,7 @@ object ToSAcceptanceTracking: BuildType ({
 				mkdir temp
 
 				# Run suite.
-				xvfb-run yarn jest --reporters=jest-teamcity --reporters=default --maxWorkers=%E2E_WORKERS% --group=legal
+				xvfb-run yarn jest --reporters=jest-teamcity --reporters=default --maxWorkers=%JEST_E2E_WORKERS% --group=legal
 			"""
 			dockerImage = "%docker_image_e2e%"
 		}
