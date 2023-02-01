67b33f9925cdb4ac9d901f5e69faa2aec5c4a032
Rich Collier <rcollier@zion.local>
2023-02-01 09:45:58 +0100

    Jetpack Cloud: Add a third section to licensing that introduces WooCommerce products

--- client/jetpack-cloud/sections/partner-portal/issue-multiple-licenses-form/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/issue-multiple-licenses-form/index.tsx
@@ -56,7 +56,12 @@ export default function IssueMultipleLicensesForm( {
 		) || [];
 	const products =
 		allProducts?.filter(
-			( { family_slug }: { family_slug: string } ) => family_slug !== 'jetpack-packs'
+			( { family_slug }: { family_slug: string } ) =>
+				family_slug !== 'jetpack-packs' && ! family_slug.includes( 'woocommerce-extension' )
+		) || [];
+	const wooProducts =
+		allProducts?.filter( ( { family_slug }: { family_slug: string } ) =>
+			family_slug.includes( 'woocommerce-extension' )
 		) || [];
 
 	const hasPurchasedProductsWithoutBundle = useSelector( ( state ) =>
@@ -194,9 +199,10 @@ export default function IssueMultipleLicensesForm( {
 								/>
 							) ) }
 					</div>
+
 					<hr className="issue-multiple-licenses-form__separator" />
 					<p className="issue-multiple-licenses-form__description">
-						{ translate( 'Or select any of our {{strong}}recommended bundles{{/strong}}:', {
+						{ translate( 'Or select any of our recommended {{strong}}Jetpack bundles{{/strong}}:', {
 							components: { strong: <strong /> },
 						} ) }
 					</p>
@@ -208,6 +214,31 @@ export default function IssueMultipleLicensesForm( {
 									product={ productOption }
 									onSelectProduct={ onSelectProduct }
 									tabIndex={ 100 + ( products?.length || 0 ) + i }
+								/>
+							) ) }
+					</div>
+
+					<hr className="issue-multiple-licenses-form__separator" />
+					<p className="issue-multiple-licenses-form__description">
+						{ translate(
+							'Or select from the following {{strong}}WooCommerce extensions{{/strong}}:',
+							{
+								components: { strong: <strong /> },
+							}
+						) }
+					</p>
+					<div className="issue-multiple-licenses-form__bottom">
+						{ wooProducts &&
+							wooProducts.map( ( productOption, i ) => (
+								<LicenseProductCard
+									isMultiSelect
+									key={ productOption.slug }
+									product={ productOption }
+									onSelectProduct={ onSelectProduct }
+									isSelected={ selectedProductSlugs.includes( productOption.slug ) }
+									isDisabled={ disabledProductSlugs.includes( productOption.slug ) }
+									tabIndex={ 100 + i }
+									suggestedProduct={ suggestedProduct }
 								/>
 							) ) }
 					</div>
