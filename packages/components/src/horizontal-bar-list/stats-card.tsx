3f50bef1738f5a7f239959f2a6261cc76a6fe222
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2023-02-02 10:26:34 +1300

    Stats: Updating CSS class names

--- packages/components/src/horizontal-bar-list/stats-card.tsx
+++ packages/components/src/horizontal-bar-list/stats-card.tsx
@@ -23,16 +23,16 @@ const StatsCard = ( {
 	const translate = useTranslate();
 
 	const titleNode = titleURL ? (
-		<a href={ `${ titleURL }` } className={ `${ BASE_CLASS_NAME }--header--title` }>
+		<a href={ `${ titleURL }` } className={ `${ BASE_CLASS_NAME }-header__title` }>
 			{ title }
 		</a>
 	) : (
-		<div className={ `${ BASE_CLASS_NAME }--header--title` }>{ title }</div>
+		<div className={ `${ BASE_CLASS_NAME }-header__title` }>{ title }</div>
 	);
 
 	// On one line shows card title and value column header
 	const simpleHeaderNode = (
-		<div className={ `${ BASE_CLASS_NAME }--header` }>
+		<div className={ `${ BASE_CLASS_NAME }-header` }>
 			{ titleNode }
 			{ ! isEmpty && <div>{ metricLabel ?? translate( 'Views' ) }</div> }
 		</div>
@@ -41,19 +41,19 @@ const StatsCard = ( {
 	// Show Card title on one line and all other column header(s) below:
 	// (main item, optional additional columns and value)
 	const splitHeaderNode = (
-		<div className={ `${ BASE_CLASS_NAME }--header ${ BASE_CLASS_NAME }--header--split` }>
+		<div className={ `${ BASE_CLASS_NAME }-header ${ BASE_CLASS_NAME }-header--split` }>
 			{ titleNode }
 			<div className={ `${ BASE_CLASS_NAME }--column-header` }>
-				<div className={ `${ BASE_CLASS_NAME }--column-header-left` }>
+				<div className={ `${ BASE_CLASS_NAME }--column-header__left` }>
 					{ splitHeader && mainItemLabel }
 					{ additionalHeaderColumns && (
-						<div className={ `${ BASE_CLASS_NAME }--header--additional` }>
+						<div className={ `${ BASE_CLASS_NAME }-header__additional` }>
 							{ additionalHeaderColumns }
 						</div>
 					) }
 				</div>
 				{ ! isEmpty && (
-					<div className={ `${ BASE_CLASS_NAME }--column-header-right` }>
+					<div className={ `${ BASE_CLASS_NAME }--column-header__right` }>
 						{ metricLabel ?? translate( 'Views' ) }
 					</div>
 				) }
