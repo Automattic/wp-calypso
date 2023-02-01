4cb8fcef98dc2b8c4db1661a5faf6b5e4e58e4c8
Christos <chriskmnds@gmail.com>
2023-02-01 15:59:23 +0200

    Plan Grid 2023: Update crossouts of unavailable features (#72819)

--- client/my-sites/plan-features-2023-grid/features.tsx
+++ client/my-sites/plan-features-2023-grid/features.tsx
@@ -1,6 +1,6 @@
 import { getPlanClass, FEATURE_CUSTOM_DOMAIN } from '@automattic/calypso-products';
 import classNames from 'classnames';
-import { localize, useTranslate } from 'i18n-calypso';
+import { localize } from 'i18n-calypso';
 import { PlanFeaturesItem } from './item';
 import { Plans2023Tooltip } from './plans-2023-tooltip';
 import { TransformedFeatureObject } from './types';
@@ -10,19 +10,6 @@ const PlanFeatures2023GridFeatures: React.FC< {
 	planName: string;
 	domainName: string;
 } > = ( { features, planName, domainName } ) => {
-	const translate = useTranslate();
-	const annualPlansFeatureNotice = ( feature: TransformedFeatureObject ) => {
-		if ( ! feature.availableOnlyForAnnualPlans || feature.availableForCurrentPlan ) {
-			return '';
-		}
-
-		return (
-			<span className="plan-features-2023-grid__item-annual-plan">
-				{ translate( 'Included with annual plans' ) }
-			</span>
-		);
-	};
-
 	return (
 		<>
 			{ features.map( ( currentFeature, featureIndex ) => {
@@ -34,13 +21,16 @@ const PlanFeatures2023GridFeatures: React.FC< {
 					'is-available': currentFeature.availableForCurrentPlan,
 				} );
 				const itemTitleClasses = classNames( 'plan-features-2023-grid__item-title', {
-					'is-bold': currentFeature.getSlug() === FEATURE_CUSTOM_DOMAIN,
+					'is-bold':
+						currentFeature.getSlug() === FEATURE_CUSTOM_DOMAIN
+							? true
+							: ! currentFeature.availableForCurrentPlan,
 				} );
 				const key = `${ currentFeature.getSlug() }-${ featureIndex }`;
 
 				return (
 					<div key={ key } className={ divClasses }>
-						<PlanFeaturesItem annualOnlyContent={ annualPlansFeatureNotice( currentFeature ) }>
+						<PlanFeaturesItem>
 							<span className={ spanClasses } key={ key }>
 								<span className={ itemTitleClasses }>
 									<Plans2023Tooltip text={ currentFeature.getDescription?.() }>
