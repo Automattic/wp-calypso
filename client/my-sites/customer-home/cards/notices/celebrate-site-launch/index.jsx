211198d6f24237000b7ae32e8104d59e6f183303
Sharpirate <stoynov.bogomil@gmail.com>
2023-02-01 09:53:58 -0700

    early draft

--- client/my-sites/customer-home/cards/notices/celebrate-site-launch/index.jsx
+++ client/my-sites/customer-home/cards/notices/celebrate-site-launch/index.jsx
@@ -1,5 +1,8 @@
+import { ConfettiAnimation } from '@automattic/components';
+import { Button, Modal } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
-import { connect, useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import launchedIllustration from 'calypso/assets/images/customer-home/illustration--rocket.svg';
 import {
 	NOTICE_CELEBRATE_SITE_LAUNCH,
@@ -10,11 +13,14 @@ import { requestSiteChecklistTaskUpdate } from 'calypso/state/checklist/actions'
 import { savePreference } from 'calypso/state/preferences/actions';
 import getSiteTaskList from 'calypso/state/selectors/get-site-task-list';
 import isSiteChecklistComplete from 'calypso/state/selectors/is-site-checklist-complete';
-import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
 const CelebrateSiteLaunch = ( { isSiteSetupComplete, pendingSiteSetupTasks, siteId } ) => {
+	const [ modalIsOpen, setModalIsOpen ] = useState( true );
 	const translate = useTranslate();
 	const dispatch = useDispatch();
+	const siteSlug = useSelector( ( state ) => getSelectedSiteSlug( state ) );
+
 	const skipSiteSetup = () => {
 		// Mark all pending tasks as completed.
 		pendingSiteSetupTasks.forEach( ( task ) =>
@@ -31,27 +37,57 @@ const CelebrateSiteLaunch = ( { isSiteSetupComplete, pendingSiteSetupTasks, site
 	};
 
 	return (
-		<CelebrateNotice
-			actionText={
-				isSiteSetupComplete ? translate( "Show me what's next" ) : translate( 'Show site setup' )
-			}
-			description={
-				isSiteSetupComplete
-					? translate(
-							"Don't forget to share your hard work with everyone. Keep up the momentum with some guidance on what to do next."
-					  )
-					: translate(
-							"Don't forget to share your hard work with everyone. Then keep working through your site setup list."
-					  )
-			}
-			noticeId={ NOTICE_CELEBRATE_SITE_LAUNCH }
-			title={ translate( 'You launched your site!' ) }
-			illustration={ launchedIllustration }
-			showSkip={ true }
-			skipText={ isSiteSetupComplete ? translate( 'Dismiss' ) : translate( 'Skip site setup' ) }
-			onSkip={ ! isSiteSetupComplete ? skipSiteSetup : null }
-			tracksEventExtras={ { is_site_setup_complete: isSiteSetupComplete } }
-		/>
+		<>
+			<CelebrateNotice
+				actionText={
+					isSiteSetupComplete ? translate( "Show me what's next" ) : translate( 'Show site setup' )
+				}
+				description={
+					isSiteSetupComplete
+						? translate(
+								"Don't forget to share your hard work with everyone. Keep up the momentum with some guidance on what to do next."
+						  )
+						: translate(
+								"Don't forget to share your hard work with everyone. Then keep working through your site setup list."
+						  )
+				}
+				noticeId={ NOTICE_CELEBRATE_SITE_LAUNCH }
+				title={ translate( 'You launched your site!' ) }
+				illustration={ launchedIllustration }
+				showSkip={ true }
+				skipText={ isSiteSetupComplete ? translate( 'Dismiss' ) : translate( 'Skip site setup' ) }
+				onSkip={ ! isSiteSetupComplete ? skipSiteSetup : null }
+				tracksEventExtras={ { is_site_setup_complete: isSiteSetupComplete } }
+			/>
+
+			{ modalIsOpen && (
+				<Modal
+					onRequestClose={ () => setModalIsOpen( false ) }
+					// contentLabel={ label }
+					// overlayClassName={ backdropClassName } // We use flex here which react-modal doesn't
+					// className={ dialogClassName }
+					// htmlOpenClassName="ReactModal__Html--open"
+					// role="dialog"
+					// shouldCloseOnEsc
+				>
+					<ConfettiAnimation />
+					<h1>{ translate( 'Congrats, your site is live!' ) }</h1>
+					<p>
+						{ translate(
+							'Your site is live. Now you can head over to your site and share it with the world or keep working on it.'
+						) }
+					</p>
+					<div>
+						<Button isSecondary onClick={ () => setModalIsOpen( false ) }>
+							{ translate( 'Back to home' ) }
+						</Button>
+						<Button href={ siteSlug } isPrimary>
+							{ translate( 'View Site' ) }
+						</Button>
+					</div>
+				</Modal>
+			) }
+		</>
 	);
 };
 
