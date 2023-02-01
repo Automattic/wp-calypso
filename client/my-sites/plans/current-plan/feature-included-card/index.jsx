f79310c26228078ef7e56f33641f452007243f0d
Nelson <nelsonec87@gmail.com>
2023-01-31 22:22:07 -0300

    Refactored css class names

--- client/my-sites/plans/current-plan/feature-included-card/index.jsx
+++ client/my-sites/plans/current-plan/feature-included-card/index.jsx
@@ -9,15 +9,11 @@ class FeatureIncludedCard extends Component {
 
 		return (
 			<Card className="feature-included-card__card">
-				<img
-					className="feature-included-card__included-illustration"
-					alt={ title }
-					src={ illustration }
-				/>
-				<p className="feature-included-card__included-title">{ title }</p>
-				<p className="feature-included-card__included-text">{ text }</p>
+				<img className="feature-included-card__illustration" alt={ title } src={ illustration } />
+				<p className="feature-included-card__title">{ title }</p>
+				<p className="feature-included-card__text">{ text }</p>
 				{ showButton ? (
-					<Button className="feature-included-card__included-link" onClick={ buttonClick }>
+					<Button className="feature-included-card__link" onClick={ buttonClick }>
 						{ buttonText }
 					</Button>
 				) : null }
