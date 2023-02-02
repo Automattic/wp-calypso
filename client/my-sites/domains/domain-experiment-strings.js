46f96a5d323bc6826553bc327cfd8fb789d08409
Paulo Trentin <paulo@paulotrentin.com.br>
2023-02-02 11:06:42 -0300

    Added new domain flow strings

---
+++ client/my-sites/domains/domain-experiment-strings.js
@@ -0,0 +1,29 @@
+import { translate } from 'i18n-calypso';
+
+translate( 'Claim your domain' );
+translate(
+	'Stake your claim on your corner of the web with a custom %(domainName) that’s easy to find, share, and follow. Not sure yet?',
+	{
+		args: {
+			domainName: 'example.blog',
+		},
+	}
+);
+translate( 'Decide later.' );
+
+translate( 'Select a domain' );
+translate( 'Select a plan' );
+translate( 'Complete your purchase' );
+
+translate( 'Choose the perfect plan' );
+translate(
+	'With your annual plan, you’ll get %(domainName) {{strong}}free for the first year{{/strong}}. You’ll also unlock advanced features that make it easy to build and grow your site.',
+	{
+		args: {
+			domainName: 'example.blog',
+		},
+		components: {
+			strong: <strong />,
+		},
+	}
+);
