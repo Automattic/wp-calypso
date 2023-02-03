fb7c68e2dc0f6c6636dff9ae2a049cd422b9b42e
Payton Swick <payton@foolord.com>
2023-02-03 13:30:24 -0500

    Fix TAction extends in getInitialState

--- packages/state-utils/src/get-initial-state/index.ts
+++ packages/state-utils/src/get-initial-state/index.ts
@@ -1,6 +1,6 @@
 import type { Action, AnyAction, Reducer } from 'redux';
 
-export default function getInitialState< TState, TAction extends AnyAction = Action >(
+export default function getInitialState< TState, TAction extends Action = AnyAction >(
 	reducer: Reducer< TState, TAction >
 ): TState {
 	return reducer( undefined, { type: '@@calypso/INIT' } );
