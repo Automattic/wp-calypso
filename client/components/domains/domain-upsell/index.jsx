38d4f581b4fc29f4318eb801f37b7a3c257f18a0
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2023-02-03 01:27:57 -0300

    Add track events, CTA, dismiss & CSS fixes

--- client/components/domains/domain-upsell/index.jsx
+++ client/components/domains/domain-upsell/index.jsx
@@ -1,48 +1,75 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { isFreePlanProduct } from '@automattic/calypso-products';
 import { Gridicon } from '@automattic/components';
-import { useSelector } from 'react-redux';
+import { sprintf } from '@wordpress/i18n';
+import { useI18n } from '@wordpress/react-i18n';
+import page from 'page';
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import TrackComponentView from 'calypso/lib/analytics/track-component-view';
 import { isCurrentUserEmailVerified } from 'calypso/state/current-user/selectors';
+import { savePreference } from 'calypso/state/preferences/actions';
+import { getPreference, hasReceivedRemotePreferences } from 'calypso/state/preferences/selectors';
 import { getDomainsBySiteId } from 'calypso/state/sites/domains/selectors';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 import './style.scss';
 
-export default function DomainUpsell() {
+const DomainUpsell = () => {
+	const { __ } = useI18n();
+	const dispatch = useDispatch();
 	const site = useSelector( ( state ) => getSelectedSite( state ) );
 	const isEmailVerified = useSelector( ( state ) => isCurrentUserEmailVerified( state ) );
 	const siteDomains = useSelector( ( state ) => getDomainsBySiteId( state, site.ID ) );
+	const hasPreferences = useSelector( hasReceivedRemotePreferences );
+	const dismissPreference = `site_editor_domain_upsell-${ site.ID }`;
+	const isDismissed = useSelector( ( state ) => getPreference( state, dismissPreference ) );
 
-	if ( siteDomains.length > 1 || ! isEmailVerified || ! isFreePlanProduct( site.plan ) ) {
+	const getCtaClickHandler = useCallback( () => {
+		recordTracksEvent( 'calypso_site_editor_domain_upsell_search_click' );
+		page( sprintf( '/domains/add/%s?domainAndPlanPackage=true', site.domain ) );
+	}, [ site.domain ] );
+
+	const getDismissClickHandler = () => {
+		recordTracksEvent( 'calypso_site_editor_domain_upsell_search_dismiss' );
+		dispatch( savePreference( dismissPreference, 1 ) );
+	};
+
+	if (
+		! hasPreferences ||
+		isDismissed ||
+		siteDomains.length > 1 ||
+		! isEmailVerified ||
+		! isFreePlanProduct( site.plan )
+	) {
 		return null;
 	}
 
-	const getCtaClickHandler = () => {
-		// eslint-disable-next-line no-console
-		console.log( 'clicked' );
-	};
-
-	const getDismissClickHandler = () => {
-		// eslint-disable-next-line no-console
-		console.log( 'dismiss' );
-	};
-
 	return (
-		<div className="domain-upsell" id="domain-upsell">
-			<div className="domain-upsell__content">
-				<div className="domain-upsell__content-text">
-					<Gridicon icon="globe" size={ 16 } className="domain-upsell__icon" />
-					<span className="domain-upsell__domain-name">gdemichelisnoplan.wordpress.com</span>
-					<button className="domain-upsell__button" onClick={ getCtaClickHandler }>
-						Customize your domain
-					</button>
-					<Gridicon
-						icon="cross"
-						size={ 16 }
-						className="domain-upsell__dismiss-icon"
-						onClick={ getDismissClickHandler }
-					/>
+		<>
+			<TrackComponentView eventName="calypso_site_preview_domain_upsell_search_view" />
+			<div className="domain-upsell" id="domain-upsell">
+				<div className="domain-upsell__content">
+					<div className="domain-upsell__content-text">
+						<Gridicon icon="globe" size={ 16 } className="domain-upsell__icon" />
+						<span className="domain-upsell__domain-name">{ site.domain }</span>
+						<button className="domain-upsell__button" onClick={ getCtaClickHandler }>
+							<span className="domain-upsell__button-text-desktop">
+								{ __( 'Customize your domain' ) }
+							</span>
+							<span className="domain-upsell__button-text-mobile">{ __( 'Customize' ) }</span>
+						</button>
+						<Gridicon
+							icon="cross"
+							size={ 16 }
+							className="domain-upsell__dismiss-icon"
+							onClick={ getDismissClickHandler }
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
-}
+};
+
+export default DomainUpsell;
