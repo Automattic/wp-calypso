12baba35c698bdf768ace262991b5915299b83c8
Payton Swick <payton@foolord.com>
2023-02-03 13:32:34 -0500

    Fix TAction extends in serialize and deserialize

--- client/state/utils/serialize.ts
+++ client/state/utils/serialize.ts
@@ -1,13 +1,13 @@
 import { getInitialState } from '@automattic/state-utils';
 import type { Reducer, AnyAction, Action } from 'redux';
 
-export interface SerializableReducer< TState = any, TAction extends AnyAction = Action >
+export interface SerializableReducer< TState = any, TAction extends Action = AnyAction >
 	extends Reducer< TState, TAction > {
 	serialize?: ( state: TState ) => any;
 	deserialize?: ( persisted: any ) => TState;
 }
 
-export function serialize< TState, TAction extends AnyAction >(
+export function serialize< TState, TAction extends Action >(
 	reducer: SerializableReducer< TState, TAction >,
 	state: TState
 ): any {
@@ -18,7 +18,7 @@ export function serialize< TState, TAction extends AnyAction >(
 	return reducer.serialize( state );
 }
 
-export function deserialize< TState, TAction extends AnyAction = Action >(
+export function deserialize< TState, TAction extends Action = AnyAction >(
 	reducer: SerializableReducer< TState, TAction >,
 	persisted: any
 ): TState {
