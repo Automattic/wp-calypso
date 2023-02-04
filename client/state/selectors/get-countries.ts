8033e81328c1291374b6eb4f013b6181e834a1d4
Payton Swick <payton@foolord.com>
2023-02-04 18:48:28 -0500

    Alter getCountries selector type to cover all state types

--- client/state/selectors/get-countries.ts
+++ client/state/selectors/get-countries.ts
@@ -1,19 +1,16 @@
 import type { CountriesState } from '../countries/types';
 import type { IAppState } from '../types';
-import type { CountryListItem } from '@automattic/wpcom-checkout';
 
 import 'calypso/state/countries/init';
 
 /**
  * Retrieves the list of countries from the specified type.
  *
- * @param {Object} state - global state tree
- * @param {string} type - type of list ('domains, 'payments', or 'sms)
- * @returns {null|CountryListItem[]} the list of countries
+ * The country data types are 'domains, 'payments', or 'sms'.
  */
-export default function getCountries(
+export default function getCountries< TType extends keyof CountriesState >(
 	state: IAppState,
-	type: keyof CountriesState
-): CountryListItem[] | null {
+	type: TType
+) {
 	return state.countries?.[ type ] ?? null;
 }
