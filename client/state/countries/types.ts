b3d26859ee8d467ef2e9d22eaf683fdc4ad701fd
Payton Swick <payton@foolord.com>
2023-02-04 18:48:05 -0500

    Add accurate type for woocommerce countries state

--- client/state/countries/types.ts
+++ client/state/countries/types.ts
@@ -1,8 +1,16 @@
 import type { CountryListItem } from '@automattic/wpcom-checkout';
 
+// Example key: 'US:TX'
+export type WooCountryKey = string;
+
+// Example value: "United States (US) — Texas"
+export type WooCountryValue = string;
+
+export type WooCountryList = Record< WooCountryKey, WooCountryValue >;
+
 export interface CountriesState {
 	domains: CountryListItem[] | undefined;
 	payments: CountryListItem[] | undefined;
 	sms: CountryListItem[] | undefined;
-	woocommerce: CountryListItem[] | undefined;
+	woocommerce: WooCountryList | undefined;
 }
