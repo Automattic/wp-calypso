bdd4ab8b2bdda7afc10f3dd3343af9e3a71842fc
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2023-02-06 11:49:20 -0300

    Update component + track events

--- client/components/web-preview/content.jsx
+++ client/components/web-preview/content.jsx
@@ -9,7 +9,7 @@ import SeoPreviewPane from 'calypso/components/seo-preview-pane';
 import SpinnerLine from 'calypso/components/spinner-line';
 import { addQueryArgs } from 'calypso/lib/route';
 import { hasTouch } from 'calypso/lib/touch-detect';
-import DomainUpsell from '../domains/domain-upsell';
+import DomainUpsellCalout from '../domains/domain-upsell-calout';
 import Toolbar from './toolbar';
 
 import './style.scss';
@@ -392,7 +392,7 @@ export default class WebPreviewContent extends Component {
 					isSticky={ this.props.isStickyToolbar }
 				/>
 				{ this.props.belowToolbar }
-				<DomainUpsell />
+				{ isLoaded && <DomainUpsellCalout trackEvent="site_preview_domain_upsell_calout" /> }
 				{ ( ! isLoaded || this.state.isLoadingSubpage ) && <SpinnerLine /> }
 				<div
 					className="web-preview__placeholder"
