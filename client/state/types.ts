c32c586695e5a24bb236ef223722b2e16ac08244
Payton Swick <payton@foolord.com>
2023-02-06 17:45:30 -0500

    Make each IAppState slice optional for safety

--- client/state/types.ts
+++ client/state/types.ts
@@ -7,13 +7,21 @@ import type { AnyAction } from 'redux';
 import type { ThunkDispatch } from 'redux-thunk';
 
 /**
- * This global app state is currently incomplete and should be completed as each state slice becomes typed
+ * This global Redux app state.
+ *
+ * This is currently incomplete and should be completed as each state slice
+ * becomes typed.
+ *
+ * Each slice is registered dynamically by calling the `registerReducer()`
+ * function so please mark each slice as optional as it may not be defined when
+ * its selector is called (eg: in tests which do not implement dynamic
+ * reducers).
  */
 export interface IAppState extends DefaultRootState {
-	plugins: IPluginsState;
+	plugins?: IPluginsState;
 	marketplace: IMarketplaceState;
-	storedCards: StoredCardsState;
-	countries: CountriesState;
+	storedCards?: StoredCardsState;
+	countries?: CountriesState;
 }
 
 /**
