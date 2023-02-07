929fd0443138f862cf1b186b343fadcaedbc8b5c
jboland88 <18016357+jboland88@users.noreply.github.com>
2023-02-07 17:59:04 -0500

    Remove cross-out price for dollar offers, fix some styling issues

--- client/components/jetpack/card/jetpack-product-card/display-price/paid.tsx
+++ client/components/jetpack/card/jetpack-product-card/display-price/paid.tsx
@@ -104,6 +104,24 @@ const Paid: React.FC< OwnProps > = ( props ) => {
 		return <Placeholder { ...props } />;
 	}
 
+	const formattedOriginalPrice = PriceAsString( originalPrice, currencyCode );
+
+	let priceComponent = isDiscounted ? (
+		<DiscountedPrice { ...props } finalPrice={ finalPrice } />
+	) : (
+		<OriginalPrice { ...props } finalPrice={ finalPrice } />
+	);
+
+	// If the pricing has a limited discount duration, the original price is handled in the duration string
+	// In this case, we'll just show the final price and not the crossed-out price.
+	if ( discountedPriceDuration ) {
+		priceComponent = (
+			<span className="display-price__standalone-card-price">
+				<PlanPrice rawPrice={ finalPrice } currencyCode={ currencyCode } />
+			</span>
+		);
+	}
+
 	return (
 		<>
 			<PriceAriaLabel
@@ -111,14 +129,11 @@ const Paid: React.FC< OwnProps > = ( props ) => {
 				currencyCode={ currencyCode }
 				finalPrice={ finalPrice }
 				isDiscounted={ isDiscounted }
+				formattedOriginalPrice={ formattedOriginalPrice }
 			/>
 			<span className="display-price__prices" aria-hidden="true">
 				{ displayFrom && <span className="display-price__from">from</span> }
-				{ isDiscounted ? (
-					<DiscountedPrice { ...props } finalPrice={ finalPrice } />
-				) : (
-					<OriginalPrice { ...props } finalPrice={ finalPrice } />
-				) }
+				{ priceComponent }
 			</span>
 			{ tooltipText && (
 				<InfoPopover position="top" className="display-price__price-tooltip">
@@ -129,7 +144,7 @@ const Paid: React.FC< OwnProps > = ( props ) => {
 				<TimeFrame
 					billingTerm={ billingTerm }
 					discountedPriceDuration={ discountedPriceDuration }
-					formattedOriginalPrice={ PriceAsString( originalPrice, currencyCode ) }
+					formattedOriginalPrice={ formattedOriginalPrice }
 				/>
 			</span>
 		</>
