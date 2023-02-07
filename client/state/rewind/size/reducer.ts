db0daf5b7e98757d5d292819d1d1f66908777bb6
Rafael Agostini <rafael.agostini@automattic.com>
2023-02-06 22:01:59 -0500

    Add fetch API call, reducer and selector

--- client/state/rewind/size/reducer.ts
+++ client/state/rewind/size/reducer.ts
@@ -65,10 +65,22 @@ const daysOfBackupsSaved = (
 	return size.daysOfBackupsSaved ?? null;
 };
 
+const lastBackupSize = (
+	state: AppState = null,
+	{ type, size }: AnyAction
+): AppState | number | null => {
+	if ( type !== REWIND_SIZE_SET ) {
+		return state;
+	}
+
+	return size.lastBackupSize ?? null;
+};
+
 export default combineReducers( {
 	requestStatus,
 	bytesUsed,
 	minDaysOfBackupsAllowed,
 	daysOfBackupsAllowed,
 	daysOfBackupsSaved,
+	lastBackupSize,
 } );
--- client/state/rewind/test/selectors.js
+++ client/state/rewind/test/selectors.js
@@ -1,4 +1,4 @@
-import { getRewindStorageUsageLevel } from '../selectors';
+import { getRewindStorageUsageLevel, getBackupCurrentSiteSize } from '../selectors';
 import { StorageUsageLevels } from '../storage/types';
 
 describe( 'getRewindStorageUsageLevel()', () => {
@@ -29,6 +29,41 @@ describe( 'getRewindStorageUsageLevel()', () => {
 		'should return values from StorageUsageLevels if passed, StorageUsageLevels.Normal otherwise',
 		( { state, siteId, expected } ) => {
 			const output = getRewindStorageUsageLevel( state, siteId );
+			expect( output ).toBe( expected );
+		}
+	);
+} );
+
+describe( 'getBackupCurrentSiteSize()', () => {
+	it.each( [
+		{
+			state: {
+				rewind: {
+					123: {
+						size: {},
+					},
+				},
+			},
+			siteId: 123,
+			expected: null,
+		},
+		{
+			state: {
+				rewind: {
+					123: {
+						size: {
+							lastBackupSize: 1024,
+						},
+					},
+				},
+			},
+			siteId: 123,
+			expected: 1024,
+		},
+	] )(
+		'should return the lastBackupSize if passed, null otherwise',
+		( { state, siteId, expected } ) => {
+			const output = getBackupCurrentSiteSize( state, siteId );
 			expect( output ).toBe( expected );
 		}
 	);
