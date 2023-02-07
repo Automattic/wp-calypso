e6a7d35df4b56788080ab09ecb10f6f3b69cfa79
vykes-mac <knrymcleish@gmail.com>
2023-02-07 15:39:34 -0500

    add event to connect branch button

--- client/my-sites/hosting/github/github-connect-card/index.tsx
+++ client/my-sites/hosting/github/github-connect-card/index.tsx
@@ -1,40 +1,65 @@
 import { Button, Card, Spinner } from '@automattic/components';
-import { __ } from '@wordpress/i18n';
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { sprintf, __ } from '@wordpress/i18n';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import CardHeading from 'calypso/components/card-heading';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
 import FormSelect from 'calypso/components/forms/form-select';
 import FormTextInput from 'calypso/components/forms/form-text-input';
 import SocialLogo from 'calypso/components/social-logo';
+import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { RepoBranch, useGithubBranches } from './use-github-branches';
+import { useGithubConnectMutation } from './use-github-connect';
 import { Repo, useGithubRepos } from './use-github-repos';
 
 import './style.scss';
 
+const noticeOptions = {
+	duration: 3000,
+};
+
 export const GithubConnectCard = () => {
 	const siteId = useSelector( getSelectedSiteId );
+	const dispatch = useDispatch();
 
 	const [ selectedRepo, setSelectedRepo ] = useState< Repo >();
 	const [ selectedBranch, setSelectedBranch ] = useState< RepoBranch >();
-	const { data: repos, isLoading: isLoadingRepos } = useGithubRepos( siteId );
+	const [ basePath, setBasePath ] = useState< string >();
+	const { data: repos, isLoading: isLoadingRepos } = useGithubRepos( siteId, {
+		onSuccess( repos ) {
+			setSelectedRepo( repos[ 0 ] );
+		},
+	} );
 	const { data: branches, isLoading: isLoadingBranches } = useGithubBranches(
 		siteId,
-		selectedRepo?.name
+		selectedRepo?.name,
+		{
+			onSuccess( branches ) {
+				setSelectedBranch( branches[ 0 ] );
+			},
+		}
 	);
 
-	useEffect( () => {
-		if ( repos ) {
-			setSelectedRepo( selectedRepo || repos[ 0 ] );
-		}
-		if ( branches ) {
-			setSelectedBranch( branches[ 0 ] );
-		}
-	}, [ repos, branches, selectedRepo ] );
+	const { connectBranch, isLoading: isConnecting } = useGithubConnectMutation( siteId, {
+		onSuccess: () => {
+			dispatch( successNotice( __( 'Branch connected successfully' ), noticeOptions ) );
+		},
+		onError: ( error ) => {
+			dispatch(
+				errorNotice(
+					// translators: "reason" is why connecting the branch failed.
+					sprintf( __( 'Failed to connect: %(reason)s' ), { reason: error.message } ),
+					{
+						...noticeOptions,
+					}
+				)
+			);
+		},
+	} );
 
 	const showSpinner = ! repos && ! branches && ( isLoadingRepos || isLoadingBranches );
-
+	const disabled = isLoadingBranches || isConnecting;
 	const handleRepoSelect = ( repoName: string ) => {
 		const selectedRepo = repos?.find( ( repo ) => repo.full_name === repoName );
 		setSelectedRepo( selectedRepo );
@@ -45,16 +70,21 @@ export const GithubConnectCard = () => {
 		setSelectedBranch( selectedBranch );
 	};
 
+	const handleBasePathChange = ( event: React.ChangeEvent< HTMLInputElement > ) => {
+		setBasePath( event.currentTarget.value );
+	};
+
 	return (
 		<Card className="connect-branch-card">
 			<SocialLogo className="material-icon" icon="github" size={ 32 } />
 			<CardHeading>{ __( 'Connect Branch' ) }</CardHeading>
 			<div>
 				<p>{ __( 'Changes pushed to connected branch will be deployed automatically' ) }</p>
-				{ ! showSpinner ? (
+				{ showSpinner ? (
+					<Spinner />
+				) : (
 					<FormFieldset className="connect-branch__fields">
 						<FormSelect
-							id="repositories"
 							className="connect-branch__field"
 							onChange={ ( event ) => handleRepoSelect( event.currentTarget.value ) }
 							value={ selectedRepo?.full_name }
@@ -66,7 +96,6 @@ export const GithubConnectCard = () => {
 							) ) }
 						</FormSelect>
 						<FormSelect
-							id="branches"
 							className="connect-branch__field"
 							onChange={ ( event ) => handleBranchSelect( event.currentTarget.value ) }
 							value={ selectedBranch?.name }
@@ -79,15 +108,20 @@ export const GithubConnectCard = () => {
 						</FormSelect>
 						<Button
 							primary
-							busy={ isLoadingBranches }
+							busy={ disabled }
+							onClick={ () => {
+								connectBranch( {
+									repoName: selectedRepo?.name,
+									branchName: selectedBranch?.name,
+									basePath: basePath,
+								} );
+							} }
 							className="connect-branch__field"
-							disabled={ isLoadingBranches }
+							disabled={ disabled }
 						>
 							<span>{ __( 'Connect' ) }</span>
 						</Button>
 					</FormFieldset>
-				) : (
-					<Spinner />
 				) }
 				<div>
 					<span>
@@ -98,7 +132,12 @@ export const GithubConnectCard = () => {
 							) }
 						</small>
 					</span>
-					<FormTextInput id="root_path" placeholder="/wp-content" />
+					<FormTextInput
+						id="root_path"
+						placeholder="/wp-content"
+						dir="rtl"
+						onChange={ handleBasePathChange }
+					/>
 				</div>
 			</div>
 		</Card>
