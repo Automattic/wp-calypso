d8b14938653e8e682ad0dd01c2607d862decf853
Juan Adolfo Lucha García <juanlucha@gmail.com>
2023-02-08 17:33:27 +0100

    Proptypes for tab menu

--- client/my-sites/stats/stats-details-navigation/index.tsx
+++ client/my-sites/stats/stats-details-navigation/index.tsx
@@ -3,7 +3,18 @@ import SectionNav from 'calypso/components/section-nav';
 import NavItem from 'calypso/components/section-nav/item';
 import NavTabs from 'calypso/components/section-nav/tabs';
 
-const navItems = ( postId: number, period: string, statType: string, givenSiteId: number ) =>
+interface propTypes {
+	postId: number;
+	period?: string;
+	statType?: string;
+	givenSiteId: number;
+}
+const navItems = (
+	postId: number,
+	period: string | undefined = 'day',
+	statType: string | undefined,
+	givenSiteId: number
+) =>
 	[ 'highlights', 'opens', 'clicks' ].map( ( item ) => {
 		const selected = statType ? statType === item : 'highlights' === item;
 		const pathParam = [ 'opens', 'clicks' ].includes( item )
@@ -19,22 +30,19 @@ const navItems = ( postId: number, period: string, statType: string, givenSiteId
 		return <NavItem { ...attr }>{ item.charAt( 0 ).toUpperCase() + item.slice( 1 ) }</NavItem>;
 	} );
 
-const propTypes = {
-	postId: PropTypes.number.isRequired,
-	period: PropTypes.string,
-	statType: PropTypes.string,
-	givenSiteId: PropTypes.number.isRequired
-}
-
-export default function StatsDetailsNavigation( {
-	postId,
-	period = 'day',
-	statType,
-	givenSiteId,
-}: propTypes ) {
+function StatsDetailsNavigation( { postId, period, statType, givenSiteId }: propTypes ) {
 	return (
 		<SectionNav>
 			<NavTabs label="Stats">{ navItems( postId, period, statType, givenSiteId ) }</NavTabs>
 		</SectionNav>
 	);
 }
+
+StatsDetailsNavigation.propTypes = {
+	postId: PropTypes.number.isRequired,
+	period: PropTypes.string,
+	statType: PropTypes.string,
+	givenSiteId: PropTypes.number.isRequired,
+};
+
+export default StatsDetailsNavigation;
