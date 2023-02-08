f003536422c238bd3e3ffefc4cb83dab66529505
Neil Roberts <neil@onebahamas.com>
2023-02-08 10:21:22 -0600

    Add TypeScript types

--- client/components/domains/contact-details-form-fields/custom-form-fieldsets/utils.ts
+++ client/components/domains/contact-details-form-fields/custom-form-fieldsets/utils.ts
@@ -1,19 +1,19 @@
 import { translate } from 'i18n-calypso';
 
-export const STATE_LABEL = {
+export const STATE_LABEL: Record< string, string > = {
 	CA: translate( 'Province' ),
 };
-export const STATE_SELECT_TEXT = {
+export const STATE_SELECT_TEXT: Record< string, string > = {
 	CA: translate( 'Select Province' ),
 };
-export const POST_CODE_LABEL = {
+export const POST_CODE_LABEL: Record< string, string > = {
 	US: translate( 'ZIP code' ),
 };
 
-export function getStateLabelText( countryCode ) {
+export function getStateLabelText( countryCode: string ) {
 	return STATE_LABEL[ countryCode ] || translate( 'State' );
 }
 
-export function getPostCodeLabelText( countryCode ) {
+export function getPostCodeLabelText( countryCode: string ) {
 	return POST_CODE_LABEL[ countryCode ] || translate( 'Postal Code' );
 }
