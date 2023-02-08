47910db7c7bc3c8c1eec63a01f18ba6241919867
Payton Swick <payton@foolord.com>
2023-02-08 15:19:18 -0500

    Add types for billingTransactions state

---
+++ client/state/billing-transactions/types.ts
@@ -0,0 +1,64 @@
+export interface BillingTransaction {
+	id: string;
+	service: string;
+	amount: string;
+	tax_amount: string;
+	icon: string;
+	date: string;
+	desc: string;
+	org: string;
+	url: string;
+	support: string;
+	pay_ref: string;
+	pay_part: string;
+	cc_type: string;
+	cc_num: string;
+	cc_name: string;
+	cc_email: string;
+	credit: string;
+	items: BillingTransactionItem[];
+}
+
+export interface BillingTransactionItem {
+	id: string;
+	type: string;
+	type_localized: string;
+	domain: string;
+	site_id: string;
+	subtotal: string;
+	tax: string;
+	amount: string;
+	raw_subtotal: number;
+	raw_tax: number;
+	raw_amount: number;
+	currency: string;
+	licensed_quantity: number | null;
+	new_quantity: number | null;
+	product: string;
+	product_slug: string;
+	variation: string;
+	variation_slug: string;
+	months_per_renewal_interval: number;
+	wpcom_product_slug: string;
+}
+
+export interface UpcomingCharge {
+	amount: string;
+	blog_id: string;
+	date: string;
+	domain: string | null;
+	icon: string;
+	id: string;
+	interval: string;
+	plan: string;
+	product: string;
+	wpcom_product_slug: string;
+}
+
+export interface BillingTransactionsState {
+	items: { past: BillingTransaction[]; upcoming: UpcomingCharge[] };
+	requesting: boolean;
+	sendingReceiptEmail: boolean;
+	individualTransactions: unknown; // TODO: fill this in.
+	ui: unknown; // TODO: fill this in.
+}
