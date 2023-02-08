6b0a420b77de7856578ee85fe7a1c675ae30866d
Neil Roberts <neil@onebahamas.com>
2023-02-08 10:25:12 -0600

    Support city, state, organization in tax fields

--- client/my-sites/checkout/composite-checkout/components/tax-fields.tsx
+++ client/my-sites/checkout/composite-checkout/components/tax-fields.tsx
@@ -3,9 +3,15 @@ import {
 	Field,
 	tryToGuessPostalCodeFormat,
 	getCountryPostalCodeSupport,
+	getCountryTaxRequirements,
 } from '@automattic/wpcom-checkout';
 import styled from '@emotion/styled';
 import { useTranslate } from 'i18n-calypso';
+import {
+	getStateLabelText,
+	STATE_SELECT_TEXT,
+} from 'calypso/components/domains/contact-details-form-fields/custom-form-fieldsets/utils';
+import { StateSelect } from 'calypso/my-sites/domains/components/form';
 import { isValid } from '../types/wpcom-store-state';
 import CountrySelectMenu from './country-select-menu';
 import { LeftColumn, RightColumn } from './ie-fallback';
@@ -51,62 +57,144 @@ export default function TaxFields( {
 	isDisabled?: boolean;
 } ) {
 	const translate = useTranslate();
-	const { postalCode, countryCode } = taxInfo;
+	const { postalCode, countryCode, city, state, organization } = taxInfo;
 	const arePostalCodesSupported =
 		countriesList.length && countryCode?.value
 			? getCountryPostalCodeSupport( countriesList, countryCode.value )
 			: false;
+	const taxRequirements =
+		countriesList.length && countryCode?.value
+			? getCountryTaxRequirements( countriesList, countryCode.value )
+			: {};
 	const isVatSupported = config.isEnabled( 'checkout/vat-form' ) && allowVat;
+
+	const fields: JSX.Element[] = [
+		<CountrySelectMenu
+			translate={ translate }
+			onChange={ ( event: ChangeEvent< HTMLSelectElement > ) => {
+				onChange( {
+					countryCode: { value: event.target.value, errors: [], isTouched: true },
+					postalCode: updatePostalCodeForCountry( postalCode, countryCode, countriesList ),
+					city,
+					state,
+					organization,
+				} );
+			} }
+			isError={ countryCode?.isTouched && ! isValid( countryCode ) }
+			isDisabled={ isDisabled }
+			errorMessage={ countryCode?.errors[ 0 ] ?? translate( 'This field is required.' ) }
+			currentValue={ countryCode?.value }
+			countriesList={ countriesList }
+		/>,
+	];
+	if ( arePostalCodesSupported ) {
+		fields.push(
+			<Field
+				id={ section + '-postal-code' }
+				type="text"
+				label={ String( translate( 'Postal code' ) ) }
+				value={ postalCode?.value ?? '' }
+				disabled={ isDisabled }
+				onChange={ ( newValue: string ) => {
+					onChange( {
+						countryCode,
+						postalCode: updatePostalCodeForCountry(
+							{ value: newValue.toUpperCase(), errors: [], isTouched: true },
+							countryCode,
+							countriesList
+						),
+						city,
+						state,
+						organization,
+					} );
+				} }
+				autoComplete={ section + ' postal-code' }
+				isError={ postalCode?.isTouched && ! isValid( postalCode ) }
+				errorMessage={ postalCode?.errors[ 0 ] ?? String( translate( 'This field is required.' ) ) }
+			/>
+		);
+	}
+	if ( taxRequirements.city ) {
+		fields.push(
+			<Field
+				id={ section + '-city' }
+				type="text"
+				label={ String( translate( 'City' ) ) }
+				value={ city?.value ?? '' }
+				disabled={ isDisabled }
+				onChange={ ( newValue: string ) => {
+					onChange( {
+						countryCode,
+						postalCode,
+						city: { value: newValue, errors: [], isTouched: true },
+						state,
+						organization,
+					} );
+				} }
+				autoComplete={ section + ' city' }
+				isError={ city?.isTouched && ! isValid( city ) }
+				errorMessage={ city?.errors[ 0 ] ?? String( translate( 'This field is required.' ) ) }
+			/>
+		);
+	}
+	if ( taxRequirements.state ) {
+		fields.push(
+			<StateSelect
+				label={ getStateLabelText( countryCode?.value ?? '' ) }
+				countryCode={ countryCode?.value }
+				selectText={ STATE_SELECT_TEXT[ countryCode?.value ?? '' ] }
+				value={ state?.value }
+				onChange={ ( event: ChangeEvent< HTMLSelectElement > ) => {
+					onChange( {
+						countryCode,
+						postalCode,
+						city,
+						state: { value: event.target.value, errors: [], isTouched: true },
+						organization,
+					} );
+				} }
+			/>
+		);
+	}
+	if ( taxRequirements.organization ) {
+		fields.push(
+			<Field
+				id={ section + '-organization' }
+				type="text"
+				label={ String( translate( 'Organization' ) ) }
+				value={ organization?.value ?? '' }
+				disabled={ isDisabled }
+				onChange={ ( newValue: string ) => {
+					onChange( {
+						countryCode,
+						postalCode,
+						city,
+						state,
+						organization: { value: newValue, errors: [], isTouched: true },
+					} );
+				} }
+				autoComplete="organization"
+			/>
+		);
+	}
 
 	return (
 		<>
-			<FieldRow>
-				<LeftColumn>
-					<CountrySelectMenu
-						translate={ translate }
-						onChange={ ( event: ChangeEvent< HTMLSelectElement > ) => {
-							onChange( {
-								countryCode: { value: event.target.value, errors: [], isTouched: true },
-								postalCode: updatePostalCodeForCountry( postalCode, countryCode, countriesList ),
-							} );
-						} }
-						isError={ countryCode?.isTouched && ! isValid( countryCode ) }
-						isDisabled={ isDisabled }
-						errorMessage={ countryCode?.errors[ 0 ] ?? translate( 'This field is required.' ) }
-						currentValue={ countryCode?.value }
-						countriesList={ countriesList }
-					/>
-				</LeftColumn>
-
-				{ arePostalCodesSupported && (
-					<RightColumn>
-						<Field
-							id={ section + '-postal-code' }
-							type="text"
-							label={ String( translate( 'Postal code' ) ) }
-							value={ postalCode?.value ?? '' }
-							disabled={ isDisabled }
-							onChange={ ( newValue: string ) => {
-								onChange( {
-									countryCode,
-									postalCode: updatePostalCodeForCountry(
-										{ value: newValue.toUpperCase(), errors: [], isTouched: true },
-										countryCode,
-										countriesList
-									),
-								} );
-							} }
-							autoComplete={ section + ' postal-code' }
-							isError={ postalCode?.isTouched && ! isValid( postalCode ) }
-							errorMessage={
-								postalCode?.errors[ 0 ] ?? String( translate( 'This field is required.' ) )
-							}
-						/>
-					</RightColumn>
-				) }
-			</FieldRow>
+			{ fields
+				.filter( ( field, index ) => index % 2 === 0 )
+				.map( ( field, index ) => (
+					<FieldRow>
+						<LeftColumn>{ field }</LeftColumn>
+						{ fields[ index * 2 + 1 ] && <RightColumn>{ fields[ index * 2 + 1 ] }</RightColumn> }
+					</FieldRow>
+				) ) }
 			{ isVatSupported && (
-				<VatForm section={ section } isDisabled={ isDisabled } countryCode={ countryCode?.value } />
+				<VatForm
+					section={ section }
+					isDisabled={ isDisabled }
+					countryCode={ countryCode?.value }
+					excludeOrganizationField={ taxRequirements.organization }
+				/>
 			) }
 		</>
 	);
