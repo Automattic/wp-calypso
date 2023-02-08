beeaec77f4aa95ae07c14206d8e68ce7ed4798c4
Payton Swick <payton@foolord.com>
2023-02-08 18:32:30 -0500

    Convert getBillingTransactionsByType to TS

---
+++ client/state/selectors/get-billing-transactions-by-type.ts
@@ -0,0 +1,34 @@
+import getPastBillingTransactions from 'calypso/state/selectors/get-past-billing-transactions';
+import getUpcomingBillingTransactions from 'calypso/state/selectors/get-upcoming-billing-transactions';
+import type { IAppState } from '../types';
+import type {
+	BillingTransaction,
+	BillingTransactionsTypePast,
+	BillingTransactionsTypeUpcoming,
+	UpcomingCharge,
+} from 'calypso/state/billing-transactions/types';
+
+import 'calypso/state/billing-transactions/init';
+
+/**
+ * Returns billing transactions of the provided type.
+ * Returns null if the billing transactions have not been fetched yet.
+ */
+export function getBillingTransactionsByType(
+	state: IAppState,
+	transactionType: BillingTransactionsTypeUpcoming
+): UpcomingCharge[] | null;
+export function getBillingTransactionsByType(
+	state: IAppState,
+	transactionType: BillingTransactionsTypePast
+): BillingTransaction[] | null;
+export function getBillingTransactionsByType(
+	state: IAppState,
+	transactionType: BillingTransactionsTypeUpcoming | BillingTransactionsTypePast
+) {
+	return 'upcoming' === transactionType
+		? getUpcomingBillingTransactions( state )
+		: getPastBillingTransactions( state );
+}
+
+export default getBillingTransactionsByType;
