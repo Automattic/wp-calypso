e01cd93a6a470c54d3ffe63cddef03126bc63482
Even Tobiesen <e@gameover.ai>
2023-02-08 18:13:57 +0100

    Adding a dispatch for updating user settings for when users toggle the opt-out for Do not sell or share my data to persist this to the /me endpoint.

--- client/me/privacy/do-not-sell.tsx
+++ client/me/privacy/do-not-sell.tsx
@@ -1,12 +1,20 @@
 import { Card } from '@automattic/components';
 import { ToggleControl } from '@wordpress/components';
+import { useDispatch } from 'react-redux';
 import { useDoNotSellContent } from 'calypso/blocks/do-not-sell-dialog/use-do-not-sell-content';
 import SectionHeader from 'calypso/components/section-header';
 import { useDoNotSell } from 'calypso/lib/analytics/utils';
+import { saveUserSettings } from 'calypso/state/user-settings/actions';
 
 export const DoNotSellSetting = () => {
 	const { shouldSeeDoNotSell, isDoNotSell, onSetDoNotSell } = useDoNotSell();
 	const { title, toggleLabel, longDescription } = useDoNotSellContent();
+	const dispatch = useDispatch();
+
+	const handleDoNotSellToggle = ( isChecked: boolean ) => {
+		onSetDoNotSell( isChecked );
+		dispatch( saveUserSettings( { advertising_targeting_opt_out: isChecked } ) );
+	};
 
 	if ( ! shouldSeeDoNotSell ) {
 		return null;
@@ -18,7 +26,11 @@ export const DoNotSellSetting = () => {
 			<Card className="privacy__settings">
 				{ longDescription }
 				<hr />
-				<ToggleControl checked={ isDoNotSell } onChange={ onSetDoNotSell } label={ toggleLabel } />
+				<ToggleControl
+					checked={ isDoNotSell }
+					onChange={ handleDoNotSellToggle }
+					label={ toggleLabel }
+				/>
 			</Card>
 		</>
 	);
