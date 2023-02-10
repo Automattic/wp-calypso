7d1a4903d2de7df9359742b6346783b3d95e91e8
Jasper Kang <kangzjnet@gmail.com>
2023-02-10 17:33:11 +1300

    linting fix

--- client/state/sites/notices/actions.js
+++ client/state/sites/notices/actions.js
@@ -2,10 +2,13 @@ import { UPDATE_STATS_NOTICE_STATUS_DIRECT } from 'calypso/state/action-types';
 import 'calypso/state/data-layer/wpcom/sites/stats/notices';
 
 /**
- * Dismisses a jitm directly
+ * Dismisses a stats notice directly to Jetpack site.
  *
- * @param {string} id The id of the jitm to dismiss
- * @param {string} featureClass The feature class of the jitm to dismiss
+ * @param {number} siteId The id of the site
+ * @param {string} id The id of the notice to dismiss
+ * @param {string} status The status
+ * @param {number} postponed_for The number of seconds to postpone the notice
+ *
  * @returns {Object} The dismiss action
  */
 export const updateStatsNoticeStatusDirect = (
