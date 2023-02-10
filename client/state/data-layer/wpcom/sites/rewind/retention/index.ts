a020e6cb658874fba31e8d4f39c3d2ca993648c3
Rafael Agostini <rafael.agostini@automattic.com>
2023-02-09 21:59:49 -0500

    Add success and error notice when updating backup retention

--- client/state/data-layer/wpcom/sites/rewind/retention/index.ts
+++ client/state/data-layer/wpcom/sites/rewind/retention/index.ts
@@ -1,3 +1,4 @@
+import { translate } from 'i18n-calypso';
 import {
 	JETPACK_BACKUP_RETENTION_UPDATE,
 	JETPACK_BACKUP_RETENTION_UPDATE_ERROR,
@@ -6,6 +7,7 @@ import {
 import { registerHandlers } from 'calypso/state/data-layer/handler-registry';
 import { http } from 'calypso/state/data-layer/wpcom-http/actions';
 import { dispatchRequest } from 'calypso/state/data-layer/wpcom-http/utils';
+import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 import type { UpdateRequestActionType } from 'calypso/state/rewind/retention/types';
 
 export const updateRetention = ( action: UpdateRequestActionType ) =>
@@ -19,16 +21,26 @@ export const updateRetention = ( action: UpdateRequestActionType ) =>
 		action
 	);
 
-const onSuccess = ( { siteId }: UpdateRequestActionType ) => ( {
-	type: JETPACK_BACKUP_RETENTION_UPDATE_SUCCESS,
-	siteId,
-} );
+const onSuccess = ( { siteId }: UpdateRequestActionType ) => [
+	{
+		type: JETPACK_BACKUP_RETENTION_UPDATE_SUCCESS,
+		siteId,
+	},
+	successNotice( translate( 'Your new days of saved backups has been saved successfully!' ), {
+		duration: 5000,
+	} ),
+];
 
-const onError = ( { siteId }: UpdateRequestActionType, error: string ) => ( {
-	type: JETPACK_BACKUP_RETENTION_UPDATE_ERROR,
-	siteId,
-	error,
-} );
+const onError = ( { siteId }: UpdateRequestActionType, error: string ) => [
+	{
+		type: JETPACK_BACKUP_RETENTION_UPDATE_ERROR,
+		siteId,
+		error,
+	},
+	errorNotice( translate( 'Update settings failed. Please, try again.' ), {
+		duration: 5000,
+	} ),
+];
 
 registerHandlers( 'state/data-layer/wpcom/sites/rewind/retention', {
 	[ JETPACK_BACKUP_RETENTION_UPDATE ]: [
