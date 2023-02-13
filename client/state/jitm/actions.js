8d46e67d154ea836cac4644710951b1d4b46bc5e
Anna McPhee <30754158+annacmc@users.noreply.github.com>
2023-02-13 14:01:05 -0600

    Odyssey Stats: Add a dismissable nudge for customer feedback after three views of Odyssey Stats (#73114)
    * create basic skeleton for new notice
    
    * add state for isFeedbackNoticeDismissed
    
    * add new selector
    
    * import new selector
    
    * implement showFeedbackNotice const
    
    * implement dismissFeedbackNotice const
    
    * update notice box content & style
    
    * update typo error on return
    
    * add styling for info notice box link and button
    
    * update var(black) to var(--jp-black)
    
    * add classname to link and button elements
    
    * add todo/comment
    
    * add margins to action link styles
    
    * fix action button to work onClick
    
    * renamed several names
    
    * changed to use stats own endpoint
    
    * move stats notice actions out of jitm
    
    * refactor to use the new action
    
    * add actions to remind me later and survey button
    
    * replace the survey url with the real one
    
    * dismissing notice working
    
    * linting fix
    
    * fix returning componet returning null
    
    * fix cursor
    
    * fix naming to be consistent
    
    ---------
    
    Co-authored-by: Jasper Kang <kangzjnet@gmail.com>

--- client/state/jitm/actions.js
+++ client/state/jitm/actions.js
@@ -6,7 +6,6 @@ import {
 	JITM_FETCH,
 	JITM_SET,
 	JITM_OPEN_HELP_CENTER,
-	JITM_DISMISS_DIRECT,
 } from 'calypso/state/action-types';
 import 'calypso/state/data-layer/wpcom/sites/jitm';
 import 'calypso/state/jitm/init';
@@ -22,19 +21,6 @@ import 'calypso/state/jitm/init';
 export const dismissJITM = ( siteId, id, featureClass ) => ( {
 	type: JITM_DISMISS,
 	siteId,
-	id,
-	featureClass,
-} );
-
-/**
- * Dismisses a jitm directly
- *
- * @param {string} id The id of the jitm to dismiss
- * @param {string} featureClass The feature class of the jitm to dismiss
- * @returns {Object} The dismiss action
- */
-export const dismissJITMDirect = ( id, featureClass ) => ( {
-	type: JITM_DISMISS_DIRECT,
 	id,
 	featureClass,
 } );
