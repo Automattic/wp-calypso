7bca130291db0f8a0af6cb7262bc4cdbedeca99f
valterlorran <valterlorran@hotmail.com>
2023-02-14 09:54:26 -0300

    Marketplace Themes: Fixes the bug activating themes after resetting the site (#73113)
    * Create reducers, actions and selectors for handling the products reinstall
    
    * Add error to the state
    
    * Adding the request to reinstall the products in the activateTheme action
    
    * Fix activateTheme test

---
+++ client/state/marketplace/products-reinstall/selectors.ts
@@ -0,0 +1,29 @@
+import 'calypso/state/marketplace/init';
+import { IAppState } from 'calypso/state/types';
+import { IReinstallProductsStatus } from '../types';
+
+export function isReinstallingProducts( state: IAppState, siteId: number ) {
+	return (
+		state.marketplace.reinstallProducts?.[ siteId ]?.status === IReinstallProductsStatus.IN_PROGRESS
+	);
+}
+
+export function failedReinstallingProducts( state: IAppState, siteId: number ) {
+	return (
+		state.marketplace.reinstallProducts?.[ siteId ]?.status === IReinstallProductsStatus.FAILED
+	);
+}
+
+export function completedReinstallingProducts( state: IAppState, siteId: number ) {
+	return (
+		state.marketplace.reinstallProducts?.[ siteId ]?.status === IReinstallProductsStatus.COMPLETED
+	);
+}
+
+export function requestedReinstallProducts( state: IAppState, siteId: number ) {
+	return (
+		state.marketplace.reinstallProducts?.[ siteId ]?.status ===
+			IReinstallProductsStatus.COMPLETED ||
+		state.marketplace.reinstallProducts?.[ siteId ]?.status === IReinstallProductsStatus.FAILED
+	);
+}
--- client/state/marketplace/reducer.ts
+++ client/state/marketplace/reducer.ts
@@ -1,11 +1,13 @@
 import { withStorageKey } from '@automattic/state-utils';
 import { combineReducers } from 'calypso/state/utils';
 import billingInterval from './billing-interval/reducer';
+import reinstallProducts from './products-reinstall/reducer';
 import purchaseFlow from './purchase-flow/reducer';
 
 const combinedReducers = combineReducers( {
 	purchaseFlow,
 	billingInterval,
+	reinstallProducts,
 } );
 
 export default withStorageKey( 'marketplace', combinedReducers );
