83542a29c11c80465f5bb32dfd6718edf8561fad
Neil Roberts <neil@onebahamas.com>
2023-02-15 16:25:41 -0600

    Prioritize VAT organization over contact details

--- client/my-sites/checkout/composite-checkout/lib/update-cart-contact-details-for-checkout.ts
+++ client/my-sites/checkout/composite-checkout/lib/update-cart-contact-details-for-checkout.ts
@@ -41,7 +41,7 @@ export async function updateCartContactDetailsForCheckout(
 		postalCode: arePostalCodesSupported ? contactInfo.postalCode?.value : '',
 		subdivisionCode,
 		vatId: vatDetails.id ?? '',
-		organization: contactInfo.organization?.value ?? vatDetails.name ?? '',
+		organization: vatDetails.name ?? contactInfo.organization?.value ?? '',
 		address: vatDetails.address ?? '',
 		city: contactInfo.city?.value ?? '',
 		state: contactInfo.state?.value ?? '',
