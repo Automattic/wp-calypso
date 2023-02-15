2f214eb039a151008c295facc00f584f351edb03
Jason Moon <jason.moon@automattic.com>
2023-02-15 14:51:23 -0800

    Add more products for upsell

--- packages/components/src/jetpack-upsell-section/index.tsx
+++ packages/components/src/jetpack-upsell-section/index.tsx
@@ -24,9 +24,70 @@ export default function JetpackUpsellSection( {
 					'Protect your site from hackers and spam with automated backups, malware scanning, and spam filtering.'
 				),
 				href: 'https://jetpack.com/features/security/',
+				icon: 'lock',
 				isFree: false,
 				slug: 'security',
 				title: translate( 'Security' ),
+			},
+			{
+				description: translate(
+					'Save every single change and get back online quickly with one-click restores.'
+				),
+				href: 'https://jetpack.com/upgrade/backup/',
+				icon: 'cloud',
+				isFree: false,
+				slug: 'backup',
+				title: translate( 'Backup' ),
+			},
+			{
+				description: translate(
+					"Help your site visitors instantly find what they're looking for so they read and buy more."
+				),
+				href: 'https://jetpack.com/upgrade/search/',
+				icon: 'mag',
+				isFree: false,
+				slug: 'search',
+				title: translate( 'Search' ),
+			},
+			{
+				description: translate(
+					'Engage your visitors with high-quality, ad-free videos build specifically for WordPress.'
+				),
+				href: 'https://jetpack.com/videopress/',
+				icon: 'video',
+				isFree: false,
+				slug: 'video',
+				title: translate( 'VideoPress' ),
+			},
+			{
+				description: translate(
+					"Improve your site's performance and SEO in a few clicks with the free Jetpack Boost plugin."
+				),
+				href: 'https://jetpack.com/boost/',
+				icon: 'boost',
+				isFree: true,
+				slug: 'boost',
+				title: translate( 'Boost' ),
+			},
+			{
+				description: translate(
+					'Save time by auto-posting your content to social networks like Facebook, Twitter, and more.'
+				),
+				href: 'https://jetpack.com/social/',
+				icon: 'social',
+				isFree: true,
+				slug: 'social',
+				title: translate( 'Social' ),
+			},
+			{
+				description: translate(
+					'Sell more and get more leads with the free Jetpack CRM plugin built specifically for WordPress.'
+				),
+				href: 'https://jetpackcrm.com/',
+				icon: 'video',
+				isFree: true,
+				slug: 'crm',
+				title: translate( 'CRM' ),
 			},
 		],
 		[ translate ]
