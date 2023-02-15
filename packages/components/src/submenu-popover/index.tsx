b83def69a2c5b7bbfc68d7ca67a1eb63e954ba7f
sejas <antonio@sejas.es>
2023-02-15 13:07:23 +0000

    Developer UX: calculate submenu right space dynamically

--- packages/components/src/submenu-popover/index.tsx
+++ packages/components/src/submenu-popover/index.tsx
@@ -1,10 +1,21 @@
 import { Popover } from '@wordpress/components';
-import { useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
 
 interface SubmenuPopoverProps extends Popover.Props {
 	isVisible?: boolean;
 	offset?: number;
 	placement?: 'right-start' | 'left-start';
+}
+
+function useHasRightSpace( element: HTMLElement | undefined ) {
+	return useMemo( () => {
+		if ( ! element ) {
+			return true;
+		}
+		const calculatedThreshold = element.offsetWidth * 1.1;
+		const { right } = element.getBoundingClientRect();
+		return window.innerWidth - right > calculatedThreshold;
+	}, [ element ] );
 }
 
 export function useSubenuPopoverProps(
@@ -14,15 +25,19 @@ export function useSubenuPopoverProps(
 ) {
 	const { placement = 'right-start' } = options;
 	const [ isVisible, setIsVisible ] = useState( false );
+	const anchor = useRef< HTMLElement >();
+	const hasSpace = useHasRightSpace( anchor?.current );
 
 	const submenu: Partial< SubmenuPopoverProps > = {
 		isVisible,
 		placement,
+		offset: hasSpace ? 0 : anchor?.current?.offsetWidth,
 	};
 
 	const parent = {
+		ref: anchor,
 		onMouseOver: () => setIsVisible( true ),
-		onMouseLeave: () => setIsVisible( false ),
+		// onMouseLeave: () => setIsVisible( false ),
 	};
 
 	return {
