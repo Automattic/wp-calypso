121bab5a596a976f4820de66111f8be5efab4038
Jason Moon <jason.moon@automattic.com>
2023-02-15 15:13:23 -0800

    Use symlinked SVGs for product icons

--- packages/components/src/jetpack-upsell-section/index.tsx
+++ packages/components/src/jetpack-upsell-section/index.tsx
@@ -1,6 +1,12 @@
-import { Icon, info } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
 import { useMemo } from 'react';
+import SecurityIcon from '../../assets/jetpack-icon-lock.svg';
+import BackupIcon from '../../assets/jetpack-product-icon-backup.svg';
+import BoostIcon from '../../assets/jetpack-product-icon-boost.svg';
+import CrmIcon from '../../assets/jetpack-product-icon-crm.svg';
+import SearchIcon from '../../assets/jetpack-product-icon-search.svg';
+import SocialIcon from '../../assets/jetpack-product-icon-social.svg';
+import VideoPressIcon from '../../assets/jetpack-product-icon-videopress.svg';
 import Button from '../button';
 import Card from '../card';
 import './style.scss';
@@ -24,7 +30,7 @@ export default function JetpackUpsellSection( {
 					'Protect your site from hackers and spam with automated backups, malware scanning, and spam filtering.'
 				),
 				href: 'https://jetpack.com/features/security/',
-				icon: 'lock',
+				iconUrl: SecurityIcon,
 				isFree: false,
 				slug: 'security',
 				title: translate( 'Security' ),
@@ -34,7 +40,7 @@ export default function JetpackUpsellSection( {
 					'Save every single change and get back online quickly with one-click restores.'
 				),
 				href: 'https://jetpack.com/upgrade/backup/',
-				icon: 'cloud',
+				iconUrl: BackupIcon,
 				isFree: false,
 				slug: 'backup',
 				title: translate( 'Backup' ),
@@ -44,7 +50,7 @@ export default function JetpackUpsellSection( {
 					"Help your site visitors instantly find what they're looking for so they read and buy more."
 				),
 				href: 'https://jetpack.com/upgrade/search/',
-				icon: 'mag',
+				iconUrl: SearchIcon,
 				isFree: false,
 				slug: 'search',
 				title: translate( 'Search' ),
@@ -54,7 +60,7 @@ export default function JetpackUpsellSection( {
 					'Engage your visitors with high-quality, ad-free videos build specifically for WordPress.'
 				),
 				href: 'https://jetpack.com/videopress/',
-				icon: 'video',
+				iconUrl: VideoPressIcon,
 				isFree: false,
 				slug: 'video',
 				title: translate( 'VideoPress' ),
@@ -64,7 +70,7 @@ export default function JetpackUpsellSection( {
 					"Improve your site's performance and SEO in a few clicks with the free Jetpack Boost plugin."
 				),
 				href: 'https://jetpack.com/boost/',
-				icon: 'boost',
+				iconUrl: BoostIcon,
 				isFree: true,
 				slug: 'boost',
 				title: translate( 'Boost' ),
@@ -74,7 +80,7 @@ export default function JetpackUpsellSection( {
 					'Save time by auto-posting your content to social networks like Facebook, Twitter, and more.'
 				),
 				href: 'https://jetpack.com/social/',
-				icon: 'social',
+				iconUrl: SocialIcon,
 				isFree: true,
 				slug: 'social',
 				title: translate( 'Social' ),
@@ -84,7 +90,7 @@ export default function JetpackUpsellSection( {
 					'Sell more and get more leads with the free Jetpack CRM plugin built specifically for WordPress.'
 				),
 				href: 'https://jetpackcrm.com/',
-				icon: 'video',
+				iconUrl: CrmIcon,
 				isFree: true,
 				slug: 'crm',
 				title: translate( 'CRM' ),
@@ -103,11 +109,11 @@ export default function JetpackUpsellSection( {
 			<div className="jetpack-upsell-section__content">
 				{ /* Only upsell products that the customer does not own. */ }
 				{ PRODUCTS.filter( ( product ) => ! purchasedProducts?.includes( product.slug ) ).map(
-					( { title, description, href, isFree } ) => (
+					( { title, description, href, isFree, iconUrl } ) => (
 						<div className="jetpack-upsell-section__product">
 							<div className="jetpack-upsell-section__product-icon">
-								{ /* TODO: Add product-specific icons for each product */ }
-								<Icon className="gridicon" icon={ info } />
+								{ /* TODO: Add styling for icon div */ }
+								<img src={ iconUrl } alt={ translate( 'included' ) } width="24px" height="24px" />
 							</div>
 							<h3 className="jetpack-upsell-section__product-title">{ title }</h3>
 							<p className="jetpack-upsell-section__product-description">{ description }</p>
