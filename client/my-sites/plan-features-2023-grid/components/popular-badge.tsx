54c45f0d230f9545d5badcac7fd10fc9c154d751
Christos <chriskmnds@gmail.com>
2023-02-15 14:58:01 +0200

    useFeaturedLabel. border fixes for tablet

--- client/my-sites/plan-features-2023-grid/components/popular-badge.tsx
+++ client/my-sites/plan-features-2023-grid/components/popular-badge.tsx
@@ -1,25 +1,19 @@
-import { isBusinessPlan, isEcommercePlan, isPremiumPlan } from '@automattic/calypso-products';
+import { getPlanClass } from '@automattic/calypso-products';
 import classNames from 'classnames';
-import { useTranslate } from 'i18n-calypso';
 import PlanPill from 'calypso/components/plans/plan-pill';
+import useFeaturedLabel from '../hooks/use-featured-label';
 
 const PopularBadge: React.FunctionComponent< {
 	isInSignup: boolean;
 	planName: string;
 	additionalClassName?: string;
 } > = ( { isInSignup, planName, additionalClassName } ) => {
-	const translate = useTranslate();
-	const classes = classNames( 'plan-features-2023-grid__popular-badge', additionalClassName );
-
-	let featuredLabel;
-
-	if ( isBusinessPlan( planName ) ) {
-		featuredLabel = translate( 'Best for devs' );
-	} else if ( isEcommercePlan( planName ) ) {
-		featuredLabel = translate( 'Best for stores' );
-	} else if ( isPremiumPlan( planName ) ) {
-		featuredLabel = translate( 'Popular' );
-	}
+	const classes = classNames(
+		'plan-features-2023-grid__popular-badge',
+		getPlanClass( planName ),
+		additionalClassName
+	);
+	const featuredLabel = useFeaturedLabel( planName );
 
 	return (
 		<>
