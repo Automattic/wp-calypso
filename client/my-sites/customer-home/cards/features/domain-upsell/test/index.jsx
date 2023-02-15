0c01be7f30dfccf63f8fdd03d413d82b9fd411ab
Paulo Trentin <paulo@paulotrentin.com.br>
2023-02-15 18:01:43 -0300

    Finished test by checking the purchase button link mocking the http call

--- client/my-sites/customer-home/cards/features/domain-upsell/test/index.jsx
+++ client/my-sites/customer-home/cards/features/domain-upsell/test/index.jsx
@@ -3,6 +3,8 @@
  */
 
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import nock from 'nock';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import DomainUpsell from '../';
@@ -33,15 +35,33 @@ jest.mock( '@automattic/calypso-products', () => {
 	};
 } );
 
+jest.mock( '@automattic/domain-picker/src', () => {
+	return {
+		useDomainSuggestions: () => {
+			return {
+				allDomainSuggestions: [
+					{
+						is_free: false,
+						product_slug: 'mydomain.com',
+					},
+				],
+			};
+		},
+	};
+} );
+
 jest.mock( 'calypso/state/current-user/selectors', () => ( {
 	getCurrentUserId: jest.fn( () => 12 ),
 	isUserLoggedIn: jest.fn( () => true ),
 	isCurrentUserEmailVerified: jest.fn( () => true ),
 } ) );
 
+let pageLink = '';
+jest.mock( 'page', () => ( link ) => ( pageLink = link ) );
+
 describe( 'index', () => {
 	describe( 'verification nudge', () => {
-		test( 'Should show H3 content for the Home domain upsell and the correct /domains/add link', () => {
+		test( 'Should show H3 content for the Home domain upsell and test search domain button link', async () => {
 			const initialState = {};
 			const mockStore = configureStore();
 			const store = mockStore( initialState );
@@ -62,6 +82,28 @@ describe( 'index', () => {
 					link.href.endsWith( '/domains/add/example.wordpress.com?domainAndPlanPackage=true' )
 				)
 			).toBeTruthy();
+		} );
+
+		test( 'Should test the purchase button link', async () => {
+			nock.cleanAll();
+			nock( 'https://public-api.wordpress.com' )
+				.persist()
+				.post( '/rest/v1.1/me/shopping-cart/no-site' )
+				.reply( 200 );
+
+			const initialState = {};
+			const mockStore = configureStore();
+			const store = mockStore( initialState );
+
+			const { getByText } = render(
+				<Provider store={ store }>
+					<DomainUpsell />
+				</Provider>
+			);
+
+			const user = userEvent.setup();
+			await user.click( getByText( 'Buy this domain' ) );
+			expect( pageLink ).toBe( '/plans/yearly/example.wordpress.com?get_domain=example.blog' );
 		} );
 	} );
 } );
