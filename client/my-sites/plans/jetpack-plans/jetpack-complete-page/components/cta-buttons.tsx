5600c9bc226e0713749439ab6b411aeb71888acd
thestevek <steve.kryworuka@automattic.com>
2023-02-15 11:21:28 -0500

    complete button functionality, add handling for no selected site.

--- client/my-sites/plans/jetpack-plans/jetpack-complete-page/components/cta-buttons.tsx
+++ client/my-sites/plans/jetpack-plans/jetpack-complete-page/components/cta-buttons.tsx
@@ -1,28 +1,44 @@
 //imports
+import { PLAN_JETPACK_COMPLETE } from '@automattic/calypso-products';
 import { useTranslate } from 'i18n-calypso';
-//import Button component
 import { useSelector } from 'react-redux';
 import Button from 'calypso/components/forms/form-button';
 import getSelectedSiteSlug from 'calypso/state/ui/selectors/get-selected-site-slug';
 
-//create a new function to render cta buttons
 const CtaButtons = () => {
 	const translate = useTranslate();
+	const planToOffer = PLAN_JETPACK_COMPLETE;
 	const siteSlug = useSelector( getSelectedSiteSlug );
+
+	// handle no site selected
+	const getCompleteURL = () => {
+		if ( siteSlug ) {
+			return `/checkout/${ siteSlug }/${ planToOffer }`;
+		}
+		return `https://wordpress.com/checkout/${ planToOffer }`;
+	};
+
+	//Start for free with no site, just redirect to jetpack.com
+	const stayFreeURL = () => {
+		if ( siteSlug ) {
+			return `https://${ siteSlug }/wp-admin`;
+		}
+		return `https://jetpack.com/`;
+	};
 	return (
 		<div className="jetpack-complete-page__cta-buttons">
-			<Button className="jetpack-complete-page__get-complete-button" primary>
+			<Button
+				className="jetpack-complete-page__get-complete-button"
+				primary
+				href={ getCompleteURL }
+			>
 				{ translate( 'Get Complete' ) }
 			</Button>
-			<Button
-				className="jetpack-complete-page__start-free-button"
-				href={ `https://${ siteSlug }/wp-admin` }
-			>
+			<Button className="jetpack-complete-page__start-free-button" href={ stayFreeURL }>
 				{ translate( 'Start for free' ) }
 			</Button>
 		</div>
 	);
 };
 
-//export renderCtaButtons component as CtaButtons
 export default CtaButtons;
