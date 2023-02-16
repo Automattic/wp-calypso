7cc5f1dc1c489f29f5ecf77f29b91ca47a75c411
Even Tobiesen <e@gameover.ai>
2023-02-16 07:45:16 +0100

    Testing to add the 10 GB storage to addon screen/grid

--- client/my-sites/add-ons/hooks/use-add-on-feature-slugs.ts
+++ client/my-sites/add-ons/hooks/use-add-on-feature-slugs.ts
@@ -5,13 +5,15 @@ import {
 	WPCOM_FEATURES_PREMIUM_THEMES,
 	WPCOM_FEATURES_CUSTOM_DESIGN,
 	WPCOM_FEATURES_NO_ADVERTS,
+	PRODUCT_WPCOM_10_GB_STORAGE,
+	FEATURE_1GB_STORAGE,
 } from '@automattic/calypso-products';
 
 /**
  * Returns any relevant feature slugs for a given add-on.
  * Add-ons are currently uniquely identified by their product slugs.
  */
-const useAddOnFeatureSlugs = ( addOnProductSlug: string ) => {
+const useAddOnFeatureSlugs = ( addOnProductSlug: string ): string[] | null => {
 	switch ( addOnProductSlug ) {
 		case PRODUCT_WPCOM_UNLIMITED_THEMES:
 			return [ WPCOM_FEATURES_PREMIUM_THEMES ];
@@ -19,6 +21,8 @@ const useAddOnFeatureSlugs = ( addOnProductSlug: string ) => {
 			return [ WPCOM_FEATURES_CUSTOM_DESIGN ];
 		case PRODUCT_NO_ADS:
 			return [ WPCOM_FEATURES_NO_ADVERTS ];
+		case PRODUCT_WPCOM_10_GB_STORAGE:
+			return [ FEATURE_1GB_STORAGE ];
 		default:
 			return null;
 	}
