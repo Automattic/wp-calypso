bfa43f3a66cb31e6e4fd69c62e49b07cfe491eec
Christos <chriskmnds@gmail.com>
2023-02-16 11:58:05 +0200

    Data stores: plans-ui store

---
+++ packages/data-stores/src/plans-ui/index.ts
@@ -0,0 +1,30 @@
+import { registerStore } from '@wordpress/data';
+import { controls } from '@wordpress/data-controls';
+import * as actions from './actions';
+import { STORE_KEY } from './constants';
+import reducer, { State } from './reducer';
+import * as selectors from './selectors';
+import type { SelectFromMap, DispatchFromMap } from '../mapped-types';
+
+export type { State };
+
+let isRegistered = false;
+
+export function register(): typeof STORE_KEY {
+	if ( ! isRegistered ) {
+		isRegistered = true;
+		registerStore< State >( STORE_KEY, {
+			actions,
+			controls,
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			reducer: reducer as any,
+			selectors,
+		} );
+	}
+	return STORE_KEY;
+}
+
+declare module '@wordpress/data' {
+	function dispatch( key: typeof STORE_KEY ): DispatchFromMap< typeof actions >;
+	function select( key: typeof STORE_KEY ): SelectFromMap< typeof selectors >;
+}
