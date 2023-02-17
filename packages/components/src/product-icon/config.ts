2669568df3cd0fb79fad64fc5705b847da5922e7
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-02-17 10:20:41 -0600

    Add 3TB and 5TB monthly slugs to the list of supported slugs

--- packages/components/src/product-icon/config.ts
+++ packages/components/src/product-icon/config.ts
@@ -130,6 +130,8 @@ export type SupportedSlugs =
 	| 'jetpack_backup_addon_storage_10gb_monthly'
 	| 'jetpack_backup_addon_storage_100gb_monthly'
 	| 'jetpack_backup_addon_storage_1tb_monthly'
+	| 'jetpack_backup_addon_storage_3tb_monthly'
+	| 'jetpack_backup_addon_storage_5tb_monthly'
 	| 'jetpack_backup_addon_storage_10gb_yearly'
 	| 'jetpack_backup_addon_storage_100gb_yearly'
 	| 'jetpack_backup_addon_storage_1tb_yearly'
