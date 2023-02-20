8b1210a135ee63b455c06702bb3ed64a4a18bcc1
CodeyGuyDylan <dylan.munson@automattic.com>
2023-02-20 15:57:21 -0700

    Refactor product feature getter

--- client/my-sites/checkout/composite-checkout/lib/get-jetpack-product-features.ts
+++ client/my-sites/checkout/composite-checkout/lib/get-jetpack-product-features.ts
@@ -12,102 +12,138 @@ import {
 import { useTranslate } from 'i18n-calypso';
 import type { WithSnakeCaseSlug } from '@automattic/calypso-products';
 
+type productString =
+	| 'anti-spam'
+	| 'backup-t1'
+	| 'boost'
+	| 'scan'
+	| 'search'
+	| 'social-basic'
+	| 'social-advanced'
+	| 'support'
+	| 'videopress';
+
+function getFeatureStrings(
+	feature: productString,
+	translate: ReturnType< typeof useTranslate >
+): string[] {
+	switch ( feature ) {
+		case 'anti-spam':
+			return [
+				translate( 'Comment and form spam protection' ),
+				translate( '10K API calls per month' ),
+				translate( 'Akismet technology' ),
+				translate( 'Flexible API' ),
+			];
+		case 'backup-t1':
+			return [
+				translate( 'Real-time cloud backups' ),
+				translate( '10GB of backup storage' ),
+				translate( '30-day archive & activity log' ),
+				translate( 'One-click restores' ),
+			];
+		case 'boost':
+			return [
+				translate( 'Automated critical CSS' ),
+				translate( 'Site performance scores' ),
+				translate( 'One-click optimization' ),
+				translate( 'Deferred non-essential JavaScript' ),
+				translate( 'Optimized CSS loading' ),
+				translate( 'Lazy image loading' ),
+			];
+		case 'scan':
+			return [
+				translate( 'Website firewall (WAF beta)' ),
+				translate( 'Automated daily scanning' ),
+				translate( 'One-click fixes for most issues' ),
+				translate( 'Instant email threat notifications' ),
+			];
+		case 'search':
+			return [
+				translate( 'Instant search, filtering, and indexing' ),
+				translate( 'Highly relevant search results' ),
+				translate( 'Quick and accurate spelling correction' ),
+				translate( 'Support for 38 languages' ),
+			];
+		case 'social-basic':
+			return [
+				translate( 'Automatically share your posts' ),
+				translate( 'Posting to multiple channels at once' ),
+				translate( 'Scheduled posts' ),
+				translate( 'Sharing to Twitter, Facebook, LinkedIn, and Tumblr' ),
+				translate( 'Content recycling' ),
+			];
+		case 'social-advanced':
+			return [ translate( 'Engagement Optimizer' ) ];
+		case 'support':
+			return [ translate( 'Priority support' ) ];
+		case 'videopress':
+			return [
+				translate( '1TB of cloud-hosted video' ),
+				translate( 'Customizable video player' ),
+				translate( 'Fast-motion video' ),
+				translate( 'Global CDN' ),
+				translate( 'Powerful and reliable hosting infrastructure' ),
+				translate( 'Video and story blocks' ),
+				translate( 'Unlimited logins for team members' ),
+			];
+		default:
+			return [];
+	}
+}
+
 export default function getJetpackProductFeatures(
 	product: WithSnakeCaseSlug,
 	translate: ReturnType< typeof useTranslate >
 ): string[] {
-	const productFeatureStrings = {
-		antiSpam: [
-			translate( 'Comment and form spam protection' ),
-			translate( '10K API calls per month' ),
-			translate( 'Akismet technology' ),
-			translate( 'Flexible API' ),
-		],
-		backupT1: [
-			translate( 'Real-time cloud backups' ),
-			translate( '10GB of backup storage' ),
-			translate( '30-day archive & activity log' ),
-			translate( 'One-click restores' ),
-		],
-		boost: [
-			translate( 'Automated critical CSS' ),
-			translate( 'Site performance scores' ),
-			translate( 'One-click optimization' ),
-			translate( 'Deferred non-essential JavaScript' ),
-			translate( 'Optimized CSS loading' ),
-			translate( 'Lazy image loading' ),
-		],
-		scan: [
-			translate( 'Website firewall (WAF beta)' ),
-			translate( 'Automated daily scanning' ),
-			translate( 'One-click fixes for most issues' ),
-			translate( 'Instant email threat notifications' ),
-		],
-		search: [
-			translate( 'Instant search, filtering, and indexing' ),
-			translate( 'Highly relevant search results' ),
-			translate( 'Quick and accurate spelling correction' ),
-			translate( 'Support for 38 languages' ),
-		],
-		socialBasic: [
-			translate( 'Automatically share your posts' ),
-			translate( 'Posting to multiple channels at once' ),
-			translate( 'Scheduled posts' ),
-			translate( 'Sharing to Twitter, Facebook, LinkedIn, and Tumblr' ),
-			translate( 'Content recycling' ),
-		],
-		socialAdvanced: [ translate( 'Engagement Optimizer' ) ],
-		support: [ translate( 'Priority support' ) ],
-		videopress: [
-			translate( '1TB of cloud-hosted video' ),
-			translate( 'Customizable video player' ),
-			translate( 'Fast-motion video' ),
-			translate( 'Global CDN' ),
-			translate( 'Powerful and reliable hosting infrastructure' ),
-			translate( 'Video and story blocks' ),
-			translate( 'Unlimited logins for team members' ),
-		],
-	};
-
 	if ( isJetpackAntiSpamSlug( product.product_slug ) ) {
-		return productFeatureStrings.antiSpam;
+		return getFeatureStrings( 'anti-spam', translate );
 	}
 
 	if ( isJetpackBackupT1Slug( product.product_slug ) ) {
-		return [ ...productFeatureStrings.backupT1, ...productFeatureStrings.support ];
+		return [
+			...getFeatureStrings( 'backup-t1', translate ),
+			...getFeatureStrings( 'support', translate ),
+		];
 	}
 
 	if ( isJetpackBoostSlug( product.product_slug ) ) {
-		return productFeatureStrings.boost;
+		return getFeatureStrings( 'boost', translate );
 	}
 
 	if ( isJetpackScanSlug( product.product_slug ) ) {
-		return [ ...productFeatureStrings.scan, ...productFeatureStrings.support ];
+		return [
+			...getFeatureStrings( 'scan', translate ),
+			...getFeatureStrings( 'support', translate ),
+		];
 	}
 
 	if ( isJetpackSearchSlug( product.product_slug ) ) {
-		return productFeatureStrings.search;
+		return getFeatureStrings( 'search', translate );
 	}
 
 	if ( isJetpackSecuritySlug( product.product_slug ) ) {
 		return [
-			...productFeatureStrings.antiSpam,
-			...productFeatureStrings.backupT1,
-			...productFeatureStrings.scan,
-			...productFeatureStrings.support,
+			...getFeatureStrings( 'anti-spam', translate ),
+			...getFeatureStrings( 'backup-t1', translate ),
+			...getFeatureStrings( 'scan', translate ),
+			...getFeatureStrings( 'support', translate ),
 		];
 	}
 
 	if ( isJetpackSocialBasicSlug( product.product_slug ) ) {
-		return productFeatureStrings.socialBasic;
+		return getFeatureStrings( 'social-basic', translate );
 	}
 
 	if ( isJetpackSocialAdvancedSlug( product.product_slug ) ) {
-		return [ ...productFeatureStrings.socialBasic, ...productFeatureStrings.socialAdvanced ];
+		return [
+			...getFeatureStrings( 'social-basic', translate ),
+			...getFeatureStrings( 'social-advanced', translate ),
+		];
 	}
 
 	if ( isJetpackVideoPressSlug( product.product_slug ) ) {
-		return productFeatureStrings.videopress;
+		return getFeatureStrings( 'videopress', translate );
 	}
 
 	return [];
