56b51c090a1fc63be2369c44ca7c91e1c802510c
Griffith Chen <griffith.chen@automattic.com>
2023-02-20 11:11:30 +0800

    Theme Showcase: Set backPath on unmount (#73420)

--- client/my-sites/themes/theme-showcase.jsx
+++ client/my-sites/themes/theme-showcase.jsx
@@ -26,6 +26,7 @@ import getSiteFeaturesById from 'calypso/state/selectors/get-site-features';
 import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 import siteHasFeature from 'calypso/state/selectors/site-has-feature';
 import { getSiteSlug } from 'calypso/state/sites/selectors';
+import { setBackPath } from 'calypso/state/themes/actions';
 import {
 	arePremiumThemesEnabled,
 	getThemeFilterTerms,
@@ -118,6 +119,10 @@ class ThemeShowcase extends Component {
 		) {
 			this.scrollToSearchInput();
 		}
+	}
+
+	componentWillUnmount() {
+		this.props.setBackPath( this.constructUrl( { searchString: this.props.search } ) );
 	}
 
 	isStaticFilter = ( tabFilter ) => {
@@ -562,4 +567,4 @@ const mapStateToProps = ( state, { siteId, filter, tier, vertical } ) => {
 	};
 };
 
-export default connect( mapStateToProps, null )( localize( ThemeShowcase ) );
+export default connect( mapStateToProps, { setBackPath } )( localize( ThemeShowcase ) );
