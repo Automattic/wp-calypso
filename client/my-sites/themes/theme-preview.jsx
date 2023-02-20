f06b98ad28fe566dde1a68e419ec221099d06a3e
Griffith Chen <griffith.chen@automattic.com>
2023-02-20 12:04:24 +0800

    Revert new theme preview modal (#73467)

--- client/my-sites/themes/theme-preview.jsx
+++ client/my-sites/themes/theme-preview.jsx
@@ -1,27 +1,17 @@
-import { isEnabled } from '@automattic/calypso-config';
 import { Button } from '@automattic/components';
-import { getDesignPreviewUrl } from '@automattic/design-picker';
-import { createHigherOrderComponent } from '@wordpress/compose';
 import { localize } from 'i18n-calypso';
-import page from 'page';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import QueryCanonicalTheme from 'calypso/components/data/query-canonical-theme';
-import PremiumGlobalStylesUpgradeModal from 'calypso/components/premium-global-styles-upgrade-modal';
 import PulsingDot from 'calypso/components/pulsing-dot';
-import ThemePreviewModal from 'calypso/components/theme-preview-modal';
 import WebPreview from 'calypso/components/web-preview';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import isSiteWPForTeams from 'calypso/state/selectors/is-site-wpforteams';
-import { useSiteGlobalStylesStatus } from 'calypso/state/sites/hooks/use-site-global-styles-status';
-import { getSiteSlug, isJetpackSite } from 'calypso/state/sites/selectors';
-import { hideThemePreview, setThemePreviewOptions } from 'calypso/state/themes/actions';
+import { isJetpackSite } from 'calypso/state/sites/selectors';
+import { hideThemePreview } from 'calypso/state/themes/actions';
 import {
-	getCanonicalTheme,
 	getThemeDemoUrl,
-	getThemeFilterToTermTable,
 	getThemePreviewThemeOptions,
 	themePreviewVisibility,
 	isThemeActive,
@@ -29,10 +19,7 @@ import {
 	isActivatingTheme,
 } from 'calypso/state/themes/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
-import { getSubjectsFromTermTable, localizeThemesPath } from './helpers';
 import { connectOptions } from './theme-options';
-
-const isDefaultVariationSlug = ( slug ) => ! slug || slug === 'default';
 
 class ThemePreview extends Component {
 	static displayName = 'ThemePreview';
@@ -51,7 +38,6 @@ class ThemePreview extends Component {
 
 	state = {
 		showActionIndicator: false,
-		showUnlockStyleUpgradeModal: false,
 	};
 
 	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
@@ -72,34 +58,6 @@ class ThemePreview extends Component {
 	getSecondaryOption = () => {
 		const { isActive } = this.props;
 		return isActive ? null : this.props.themeOptions.secondary;
-	};
-
-	getStyleVariationOption = () => {
-		return this.props.themeOptions?.styleVariation;
-	};
-
-	getPremiumGlobalStylesEventProps = () => {
-		const { themeId } = this.props;
-		const styleVariationOption = this.getStyleVariationOption();
-		return {
-			theme: themeId,
-			style_variation: styleVariationOption?.slug,
-		};
-	};
-
-	shouldShowUnlockStyleButton = () => {
-		const { options, shouldLimitGlobalStyles, themeOptions } = this.props;
-		if ( ! themeOptions ) {
-			return false;
-		}
-
-		const primaryOption = this.getPrimaryOption();
-		const styleVariationOption = this.getStyleVariationOption();
-		return (
-			shouldLimitGlobalStyles &&
-			primaryOption?.key === options.activate.key &&
-			! isDefaultVariationSlug( styleVariationOption?.slug )
-		);
 	};
 
 	onPrimaryButtonClick = () => {
@@ -128,62 +86,9 @@ class ThemePreview extends Component {
 		! this.props.isJetpack && this.props.hideThemePreview();
 	};
 
-	onUnlockStyleButtonClick = () => {
-		this.props.recordTracksEvent(
-			'calypso_theme_preview_global_styles_gating_modal_show',
-			this.getPremiumGlobalStylesEventProps()
-		);
-
-		this.setState( { showUnlockStyleUpgradeModal: true } );
-	};
-
-	onPremiumGlobalStylesUpgradeModalCheckout = () => {
-		this.props.recordTracksEvent(
-			'calypso_theme_preview_global_styles_gating_modal_checkout_button_click',
-			this.getPremiumGlobalStylesEventProps()
-		);
-
-		this.setState( { showUnlockStyleUpgradeModal: false } );
-		page( `/checkout/${ this.props.siteSlug || '' }/premium` );
-	};
-
-	onPremiumGlobalStylesUpgradeModalTryStyle = () => {
-		this.props.recordTracksEvent(
-			'calypso_theme_preview_global_styles_gating_modal_try_button_click',
-			this.getPremiumGlobalStylesEventProps()
-		);
-
-		this.setState( { showUnlockStyleUpgradeModal: false } );
-		this.onPrimaryButtonClick();
-	};
-
-	onPremiumGlobalStylesUpgradeModalClose = () => {
-		this.props.recordTracksEvent(
-			'calypso_theme_preview_global_styles_gating_modal_close_button_click',
-			this.getPremiumGlobalStylesEventProps()
-		);
-
-		this.setState( { showUnlockStyleUpgradeModal: false } );
-	};
-
-	appendStyleVariationOptionToUrl = ( url ) => {
-		const styleVariationOption = this.getStyleVariationOption();
-		if ( ! styleVariationOption ) {
-			return url;
-		}
-
-		const [ base, query ] = url.split( '?' );
-		const params = new URLSearchParams( query );
-		params.set( 'style_variation', styleVariationOption.slug );
-		return `${ base }?${ params.toString() }`;
-	};
-
 	renderPrimaryButton = () => {
 		const primaryOption = this.getPrimaryOption();
-		let buttonHref = primaryOption.getUrl ? primaryOption.getUrl( this.props.themeId ) : null;
-		if ( buttonHref ) {
-			buttonHref = this.appendStyleVariationOptionToUrl( buttonHref );
-		}
+		const buttonHref = primaryOption.getUrl ? primaryOption.getUrl( this.props.themeId ) : null;
 
 		return (
 			<Button primary onClick={ this.onPrimaryButtonClick } href={ buttonHref }>
@@ -198,10 +103,7 @@ class ThemePreview extends Component {
 			return;
 		}
 
-		let buttonHref = secondaryButton.getUrl ? secondaryButton.getUrl( this.props.themeId ) : null;
-		if ( buttonHref ) {
-			buttonHref = this.appendStyleVariationOptionToUrl( buttonHref );
-		}
+		const buttonHref = secondaryButton.getUrl ? secondaryButton.getUrl( this.props.themeId ) : null;
 
 		return (
 			<Button onClick={ this.onSecondaryButtonClick } href={ buttonHref }>
@@ -210,70 +112,9 @@ class ThemePreview extends Component {
 		);
 	};
 
-	renderUnlockStyleButton = () => {
-		return (
-			<Button primary onClick={ this.onUnlockStyleButtonClick }>
-				{ this.props.translate( 'Unlock this style' ) }
-			</Button>
-		);
-	};
-
-	getPreviewUrl = () => {
-		const { demoUrl, locale, theme } = this.props;
-		if ( isEnabled( 'themes/showcase-i4/details-and-preview' ) && theme.stylesheet ) {
-			return getDesignPreviewUrl( { slug: theme.id, recipe: theme }, { language: locale } );
-		}
-
-		return demoUrl + '?demo=true&iframe=true&theme_preview=true';
-	};
-
-	onSelectVariation = ( variation ) => {
-		const { themeId, primary, secondary } = this.props.themeOptions;
-		this.props.recordTracksEvent( 'calypso_theme_preview_style_variation_click', {
-			theme: themeId,
-			style_variation: variation.slug,
-		} );
-
-		this.props.setThemePreviewOptions( themeId, primary, secondary, variation );
-	};
-
-	onClickCategory = ( category ) => {
-		const { filterToTermTable, isLoggedIn, locale, siteSlug, themeId } = this.props;
-		const subjectTermTable = getSubjectsFromTermTable( filterToTermTable );
-		const subject = subjectTermTable[ `subject:${ category.slug }` ];
-
-		if ( subject ) {
-			this.props.recordTracksEvent( 'calypso_theme_preview_category_click', {
-				theme: themeId,
-				category: category.slug,
-			} );
-
-			const path = `/themes/filter/${ subject }/${ siteSlug ?? '' }`;
-			window.location.href = localizeThemesPath( path, locale, ! isLoggedIn );
-		}
-	};
-
-	onClickDevice = ( device ) => {
-		this.props.recordTracksEvent( 'calypso_theme_preview_device_switcher_click', {
-			theme: this.props.themeId,
-			device,
-		} );
-	};
-
-	onClose = () => {
-		this.props.recordTracksEvent( 'calypso_theme_preview_close_click', {
-			theme: this.props.themeId,
-		} );
-		this.props.hideThemePreview();
-	};
-
 	render() {
-		const { theme, themeId, siteId, demoUrl, children, isWPForTeamsSite, shouldLimitGlobalStyles } =
-			this.props;
-		const { showActionIndicator, showUnlockStyleUpgradeModal } = this.state;
-		const selectedVariation = this.getStyleVariationOption();
-		const showUnlockStyleButton = this.shouldShowUnlockStyleButton();
-		const isNewDetailsAndPreview = isEnabled( 'themes/showcase-i4/details-and-preview' );
+		const { themeId, siteId, demoUrl, children, isWPForTeamsSite } = this.props;
+		const { showActionIndicator } = this.state;
 
 		if ( ! themeId || isWPForTeamsSite ) {
 			return null;
@@ -283,60 +124,25 @@ class ThemePreview extends Component {
 			<div>
 				<QueryCanonicalTheme siteId={ siteId } themeId={ themeId } />
 				{ children }
-				{ demoUrl &&
-					( isNewDetailsAndPreview ? (
-						<ThemePreviewModal
-							theme={ theme }
-							previewUrl={ this.getPreviewUrl() }
-							selectedVariation={ selectedVariation }
-							actionButtons={
-								<>
-									{ showUnlockStyleButton
-										? this.renderUnlockStyleButton()
-										: this.renderPrimaryButton() }
-									{ this.renderSecondaryButton() }
-								</>
-							}
-							shouldLimitGlobalStyles={ shouldLimitGlobalStyles }
-							onSelectVariation={ this.onSelectVariation }
-							onClickCategory={ this.onClickCategory }
-							onClose={ this.onClose }
-							recordDeviceClick={ this.onClickDevice }
-						/>
-					) : (
-						<WebPreview
-							showPreview={ true }
-							showExternal={ false }
-							showSEO={ false }
-							onClose={ this.props.hideThemePreview }
-							previewUrl={ this.getPreviewUrl() }
-							externalUrl={ this.props.demoUrl }
-							belowToolbar={ this.props.belowToolbar }
-						>
-							{ showActionIndicator && <PulsingDot active={ true } /> }
-							{ ! showActionIndicator && this.renderSecondaryButton() }
-							{ ! showActionIndicator && this.renderPrimaryButton() }
-						</WebPreview>
-					) ) }
-				<PremiumGlobalStylesUpgradeModal
-					checkout={ this.onPremiumGlobalStylesUpgradeModalCheckout }
-					tryStyle={ this.onPremiumGlobalStylesUpgradeModalTryStyle }
-					closeModal={ this.onPremiumGlobalStylesUpgradeModalClose }
-					isOpen={ showUnlockStyleUpgradeModal }
-				/>
+				{ demoUrl && (
+					<WebPreview
+						showPreview={ true }
+						showExternal={ false }
+						showSEO={ false }
+						onClose={ this.props.hideThemePreview }
+						previewUrl={ this.props.demoUrl + '?demo=true&iframe=true&theme_preview=true' }
+						externalUrl={ this.props.demoUrl }
+						belowToolbar={ this.props.belowToolbar }
+					>
+						{ showActionIndicator && <PulsingDot active={ true } /> }
+						{ ! showActionIndicator && this.renderSecondaryButton() }
+						{ ! showActionIndicator && this.renderPrimaryButton() }
+					</WebPreview>
+				) }
 			</div>
 		);
 	}
 }
-
-const withSiteGlobalStylesStatus = createHigherOrderComponent(
-	( Wrapped ) => ( props ) => {
-		const { siteId } = props;
-		const { shouldLimitGlobalStyles } = useSiteGlobalStylesStatus( siteId || -1 );
-		return <Wrapped { ...props } shouldLimitGlobalStyles={ shouldLimitGlobalStyles } />;
-	},
-	'withSiteGlobalStylesStatus'
-);
 
 // make all actions available to preview.
 const ConnectedThemePreview = connectOptions( ThemePreview );
@@ -352,19 +158,15 @@ export default connect(
 		const isJetpack = isJetpackSite( state, siteId );
 		const themeOptions = getThemePreviewThemeOptions( state );
 		return {
-			theme: getCanonicalTheme( state, siteId, themeId ),
 			themeId,
 			siteId,
-			siteSlug: getSiteSlug( state, siteId ),
 			isJetpack,
 			themeOptions,
-			filterToTermTable: getThemeFilterToTermTable( state ),
 			isInstalling: isInstallingTheme( state, themeId, siteId ),
 			isActive: isThemeActive( state, themeId, siteId ),
 			isActivating: isActivatingTheme( state, siteId ),
 			demoUrl: getThemeDemoUrl( state, themeId, siteId ),
 			isWPForTeamsSite: isSiteWPForTeams( state, siteId ),
-			isLoggedIn: isUserLoggedIn( state ),
 			options: [
 				'activate',
 				'preview',
@@ -380,5 +182,5 @@ export default connect(
 			],
 		};
 	},
-	{ hideThemePreview, recordTracksEvent, setThemePreviewOptions }
-)( withSiteGlobalStylesStatus( localize( ConnectedThemePreview ) ) );
+	{ hideThemePreview, recordTracksEvent }
+)( localize( ConnectedThemePreview ) );
