52bc1eebc0ad0baf72c326df4c5d1d3d8b90eb99
Edwin Takahashi <egao@outlook.com>
2023-02-20 17:44:44 -0800

    packages/calypso-e2e/src/lib/flows/ - add new POM to represent the Free Signup flow.

---
+++ packages/calypso-e2e/src/lib/flows/free-signup-flow.ts
@@ -0,0 +1,68 @@
+import { Page } from 'playwright';
+
+/**
+ * Represents the free user signup flow at setup/free/freeSetup.
+ *
+ * @see {@link https://wordpress.com/setup/free/freeSetup}
+ */
+export class FreeSignupFlow {
+	private page: Page;
+
+	/**
+	 * Constructs an instance of the flow.
+	 *
+	 * @param {Page} page The underlying page.
+	 */
+	constructor( page: Page ) {
+		this.page = page;
+	}
+
+	/**
+	 * Clicks on a button with the exact matching string.
+	 *
+	 * @param {string} buttonText Text on the button to click.
+	 */
+	async clickButton( buttonText: string ): Promise< void > {
+		await this.page.getByRole( 'button', { name: buttonText, exact: true } ).click();
+	}
+
+	/**
+	 * Enters the site name.
+	 *
+	 * Note that in this flow, the site name will form part of the domain name.
+	 *
+	 * Example:
+	 * 	freetestsite1234 -> freetestsite.wordpress.com
+	 *
+	 * @param {string} name Name of the site.
+	 */
+	async enterSitename( name: string ): Promise< void > {
+		await this.page.getByRole( 'textbox', { name: 'My WebSite' } ).fill( name );
+	}
+
+	/**
+	 * Enters description of the site.
+	 *
+	 * @param {string} description Description of the site.
+	 */
+	async enterDescription( description: string ): Promise< void > {
+		await this.page.getByLabel( 'Brief description' ).fill( description );
+	}
+
+	/**
+	 * Selects a style matching the exact name.
+	 *
+	 * @param {string} name Name of the style.
+	 */
+	async pickDesign( name: string ): Promise< void > {
+		await this.page.getByText( name, { exact: true } ).click();
+
+		// FSE styles present a screen to choose a style and a button to
+		// continue with default options, while normal themes only show
+		// a preview and a button to select the theme.
+		await Promise.race( [
+			this.clickButton( 'Continue' ),
+			this.clickButton( `Start with ${ name }` ),
+		] );
+	}
+}
