5cf6c4256baac19241d09c33beb6cb5bc7e0c86a
okmttdhr <okmttdhr@users.noreply.github.com>
2023-02-21 10:57:04 +0900

    Expose whether fetch is completed or not in useSiteIntent

--- apps/editing-toolkit/editing-toolkit-plugin/dotcom-fse/lib/site-intent/use-site-intent.js
+++ apps/editing-toolkit/editing-toolkit-plugin/dotcom-fse/lib/site-intent/use-site-intent.js
@@ -2,17 +2,24 @@ import apiFetch from '@wordpress/api-fetch';
 import { useState, useEffect, useCallback } from '@wordpress/element';
 
 const useSiteIntent = () => {
-	const [ siteIntent, setSiteIntent ] = useState( '' );
+	const [ siteIntent, setSiteIntent ] = useState( undefined );
+	const [ siteIntentFetched, setSiteIntentFetched ] = useState( false );
 
 	const fetchSiteIntent = useCallback( () => {
 		apiFetch( { path: '/wpcom/v2/site-intent' } )
-			.then( ( result ) => setSiteIntent( result.site_intent ) )
-			.catch( () => setSiteIntent( '' ) );
+			.then( ( result ) => {
+				setSiteIntent( result.site_intent );
+				setSiteIntentFetched( true );
+			} )
+			.catch( () => {
+				setSiteIntent( undefined );
+				setSiteIntentFetched( true );
+			} );
 	}, [] );
 
 	useEffect( () => {
 		fetchSiteIntent();
 	}, [ fetchSiteIntent ] );
-	return siteIntent;
+	return { siteIntent, siteIntentFetched };
 };
 export default useSiteIntent;
