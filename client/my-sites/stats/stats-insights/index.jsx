79913c76dadcf597da53ad433812bff8972508fb
Anna McPhee <anna.mcphee@automattic.com>
2023-02-21 10:25:50 -0600

    show summary link

--- client/my-sites/stats/stats-insights/index.jsx
+++ client/my-sites/stats/stats-insights/index.jsx
@@ -72,7 +72,7 @@ const StatsInsights = ( props ) => {
 							path="tags-categories"
 							moduleStrings={ moduleStrings.tags }
 							statType="statsTags"
-							hideSummaryLink
+							showSummaryLink
 							//hideNewModule // remove when cleaning 'stats/horizontal-bars-everywhere' FF
 						/>
 						<Comments path="comments" />
@@ -97,8 +97,8 @@ const StatsInsights = ( props ) => {
 									path="tags-categories"
 									moduleStrings={ moduleStrings.tags }
 									statType="statsTags"
-									hideSummaryLink
-									hideNewModule // remove when cleaning 'stats/horizontal-bars-everywhere' FF
+									// hideSummaryLink
+									// hideNewModule // remove when cleaning 'stats/horizontal-bars-everywhere' FF
 								/>
 								{ /** TODO: The feature depends on Jetpack Sharing module and is disabled for Odyssey for now. */ }
 								{ ! isOdysseyStats && <StatShares siteId={ siteId } /> }
