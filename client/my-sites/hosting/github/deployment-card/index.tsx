faefd14f98cad86c695537aca797607e25b37ab1
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2023-02-22 17:11:52 -0300

    GitHub integration: highlight that a different repo was connected when last deployed (#73680)

--- client/my-sites/hosting/github/deployment-card/index.tsx
+++ client/my-sites/hosting/github/deployment-card/index.tsx
@@ -1,15 +1,15 @@
 import { Button, Card, Spinner } from '@automattic/components';
 import { sprintf } from '@wordpress/i18n';
-import i18n, { useTranslate } from 'i18n-calypso';
+import { useTranslate } from 'i18n-calypso';
 import { useDispatch, useSelector } from 'react-redux';
 import CardHeading from 'calypso/components/card-heading';
 import SocialLogo from 'calypso/components/social-logo';
 import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
-import { DeploymentStatusBadge } from './deployment-status-badge';
-import { DeploymentStatusExplanation } from './deployment-status-explanation';
+import { LastDeploymentInformation } from './last-deployment-information';
 import { useDeploymentStatusQuery } from './use-deployment-status-query';
 import { useGithubDisconnectRepoMutation } from './use-disconnect-repo';
+
 import './style.scss';
 
 type DeploymentCardProps = {
@@ -22,21 +22,10 @@ const noticeOptions = {
 };
 
 export const DeploymentCard = ( { repo, branch, connectionId }: DeploymentCardProps ) => {
-	let deploymentTime = '';
-	let totalFailures = 0;
-
 	const siteId = useSelector( getSelectedSiteId );
 
 	const { data: deployment, isLoading } = useDeploymentStatusQuery( siteId, connectionId );
 	const translate = useTranslate();
-
-	if ( deployment ) {
-		totalFailures = deployment.move_failures.length + deployment.remove_failures.length;
-		deploymentTime = new Intl.DateTimeFormat( i18n.getLocaleSlug() ?? 'en', {
-			dateStyle: 'medium',
-			timeStyle: 'short',
-		} ).format( new Date( deployment.last_deployment_timestamp * 1000 ) );
-	}
 
 	const dispatch = useDispatch();
 
@@ -84,42 +73,11 @@ export const DeploymentCard = ( { repo, branch, connectionId }: DeploymentCardPr
 			<div>
 				{ isLoading && <Spinner /> }
 				{ deployment && (
-					<div style={ { marginBottom: '24px' } }>
-						<strong style={ { display: 'block', marginBottom: '8px' } }>
-							{ translate( 'Last deployment' ) }
-						</strong>
-						<div className="deployment-card__row">
-							<div className="deployment-card__column">
-								<span>{ deploymentTime }</span>
-							</div>
-							<div className="deployment-card__column">
-								<a
-									target="_blank"
-									href={ `https://github.com/${ repo }/commit/${ deployment.last_deployment_sha }` }
-									rel="noreferrer"
-								>
-									{ deployment.last_deployment_sha.substring( 0, 7 ) }
-								</a>
-							</div>
-							<div
-								className="deployment-card__column"
-								style={ { flexDirection: 'row', gap: '8px' } }
-							>
-								<DeploymentStatusBadge
-									status={ deployment.status }
-									totalFailures={ totalFailures }
-								/>
-							</div>
-						</div>
-						{ ( deployment.status === 'failed' || totalFailures > 0 ) && (
-							<DeploymentStatusExplanation
-								status={ deployment.status }
-								totalFailures={ totalFailures }
-								connectionId={ connectionId }
-								deploymentTimestamp={ deployment.last_deployment_timestamp }
-							/>
-						) }
-					</div>
+					<LastDeploymentInformation
+						deployment={ deployment }
+						connectedRepo={ repo }
+						connectionId={ connectionId }
+					/>
 				) }
 			</div>
 			<Button primary busy={ isDisconnecting } onClick={ () => disconnectRepo( siteId ) }>
