4ad6773cbf02cc82ec418c84948c71e8abf7d8cb
Nelson <nelsonec87@gmail.com>
2023-02-22 13:03:31 -0300

    Added <body> css class

--- client/my-sites/plans/ecommerce-trial/upgrade-confirmation/index.tsx
+++ client/my-sites/plans/ecommerce-trial/upgrade-confirmation/index.tsx
@@ -1,8 +1,10 @@
 import { useTranslate } from 'i18n-calypso';
 import Main from 'calypso/components/main';
-import './style.scss';
+import BodySectionCssClass from 'calypso/layout/body-section-css-class';
 import ConfirmationTask from './confirmation-task';
 import { getConfirmationTasks } from './confirmation-tasks';
+
+import './style.scss';
 
 const TrialUpgradeConfirmation = () => {
 	const translate = useTranslate();
@@ -10,23 +12,26 @@ const TrialUpgradeConfirmation = () => {
 	const tasks = getConfirmationTasks( { translate } );
 
 	return (
-		<Main wideLayout>
-			<div className="trial-upgrade-confirmation__header">
-				<h1 className="trial-upgrade-confirmation__title">
-					{ translate( 'Woo! Welcome to eCommerce' ) }
-				</h1>
-				<div className="trial-upgrade-confirmation__subtitle">
-					{ translate( 'Your purchase has been completed and you’re on the Commerce plan.' ) }
-					<br />
-					{ translate( "Now it's time to get creative. What would you like to do next?" ) }
+		<>
+			<BodySectionCssClass bodyClass={ [ 'trial-upgraded' ] } />
+			<Main wideLayout>
+				<div className="trial-upgrade-confirmation__header">
+					<h1 className="trial-upgrade-confirmation__title">
+						{ translate( 'Woo! Welcome to eCommerce' ) }
+					</h1>
+					<div className="trial-upgrade-confirmation__subtitle">
+						{ translate( 'Your purchase has been completed and you’re on the Commerce plan.' ) }
+						<br />
+						{ translate( "Now it's time to get creative. What would you like to do next?" ) }
+					</div>
 				</div>
-			</div>
-			<div className="trial-upgrade-confirmation__tasks">
-				{ tasks.map( ( task ) => (
-					<ConfirmationTask { ...task } />
-				) ) }
-			</div>
-		</Main>
+				<div className="trial-upgrade-confirmation__tasks">
+					{ tasks.map( ( task ) => (
+						<ConfirmationTask key={ task.title } { ...task } />
+					) ) }
+				</div>
+			</Main>
+		</>
 	);
 };
 
