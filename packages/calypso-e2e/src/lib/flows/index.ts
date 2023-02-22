0e930add83631c3a01a26c7bc67607698ee17c6e
Edwin Takahashi <egao@outlook.com>
2023-02-22 12:10:31 -0800

    Revert "E2E: revise the WordPress.com Free signup spec to the new, simplified signup flow. (#73618)"
    This reverts commit a7b7b62da5d485aa4a45813f731f79df2679f311.

--- packages/calypso-e2e/src/lib/flows/index.ts
+++ packages/calypso-e2e/src/lib/flows/index.ts
@@ -3,4 +3,3 @@ export * from './close-account-flow';
 export * from './start-import-flow';
 export * from './start-site-flow';
 export * from './change-ui-language-flow';
-export * from './free-signup-flow';
