bce54131006ccfc036ae8daf55cf39433c407fc5
Payton Swick <payton@foolord.com>
2023-02-23 11:14:34 -0500

    Add EU and GB

--- client/me/purchases/billing-history/vat-vendor-details.tsx
+++ client/me/purchases/billing-history/vat-vendor-details.tsx
@@ -23,15 +23,78 @@ interface VatVendorInfo {
 	vatId: string;
 }
 
+function isCountryInEu( country: string ): boolean {
+	const countries = [
+		'AT',
+		'BE',
+		'BG',
+		'CH',
+		'CY',
+		'CZ',
+		'DE',
+		'DK',
+		'EE',
+		'EL',
+		'ES',
+		'FI',
+		'FR',
+		'HR',
+		'HU',
+		'IE',
+		'IT',
+		'LT',
+		'LU',
+		'LV',
+		'MT',
+		'NL',
+		'PL',
+		'PT',
+		'RO',
+		'SE',
+		'SI',
+		'SK',
+		'XI',
+	];
+	return countries.includes( country );
+}
+
 function getVatVendorInfo(
 	country: string,
 	translate: ReturnType< typeof useTranslate >
 ): VatVendorInfo | undefined {
-	// TODO: add more countries
+	if ( isCountryInEu( country ) ) {
+		return {
+			country,
+			taxName: translate( 'VAT', { textOnly: true } ),
+			address: [
+				'Aut O’Mattic Ltd.',
+				'c/o Noone Casey',
+				'Grand Canal Dock, 25 Herbert Pl',
+				'Dublin, D02 AY86',
+				'Ireland',
+			],
+			vatId: 'IE3255131SH',
+		};
+	}
+
+	if ( country === 'GB' ) {
+		return {
+			country,
+			taxName: translate( 'VAT', { textOnly: true } ),
+			address: [
+				'Aut O’Mattic Ltd.',
+				'c/o Noone Casey',
+				'Grand Canal Dock, 25 Herbert Pl',
+				'Dublin, D02 AY86',
+				'Ireland',
+			],
+			vatId: 'UK 376 1703 88',
+		};
+	}
 
 	if ( country === 'CA' ) {
 		return {
-			country: 'CA',
+			country,
 			taxName: translate( 'GST', { textOnly: true } ),
 			address: [
 				'Aut O’Mattic Ltd.',
