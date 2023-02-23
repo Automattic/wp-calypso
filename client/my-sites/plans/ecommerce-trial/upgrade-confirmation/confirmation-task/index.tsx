97ce2524dbb240bec31d72135270162628f0b256
nelsonec87 <nelsonec87@gmail.com>
2023-02-23 14:49:10 -0300

    WooExpress: Added trial upgrade confirmation card (#73673)
    * Added trial confirmation illustrations
    
    * Added illustrations to code
    
    * Added task styles
    
    * Add padding for mobile version
    
    * Renamed woo image

--- client/my-sites/plans/ecommerce-trial/upgrade-confirmation/confirmation-task/index.tsx
+++ client/my-sites/plans/ecommerce-trial/upgrade-confirmation/confirmation-task/index.tsx
@@ -10,13 +10,17 @@ interface ConfirmationTaskProps {
 }
 
 const ConfirmationTask = ( props: ConfirmationTaskProps ) => {
-	const { title, subtitle } = props;
+	const { title, subtitle, illustration } = props;
 
 	return (
 		<Card className="confirmation-task__card">
-			<span>{ title }</span>
-			<br />
-			<span>{ subtitle }</span>
+			<img
+				className="confirmation-task__illustration"
+				src={ illustration }
+				alt={ title.toString() }
+			/>
+			<div className="confirmation-task__title">{ title }</div>
+			<div className="confirmation-task__subtitle">{ subtitle }</div>
 		</Card>
 	);
 };
--- client/my-sites/plans/ecommerce-trial/upgrade-confirmation/confirmation-tasks.ts
+++ client/my-sites/plans/ecommerce-trial/upgrade-confirmation/confirmation-tasks.ts
@@ -1,4 +1,10 @@
 import { translate as i18nTranslate } from 'i18n-calypso';
+import customDomain from 'calypso/assets/images/plans/wpcom/custom-domain.png';
+import customize from 'calypso/assets/images/plans/wpcom/customize.png';
+import manageWooCommerce from 'calypso/assets/images/plans/wpcom/manage-woocommerce.png';
+import marketing from 'calypso/assets/images/plans/wpcom/marketing.png';
+import promote from 'calypso/assets/images/plans/wpcom/promote.png';
+import wayToPay from 'calypso/assets/images/plans/wpcom/way-to-pay.png';
 
 type ConfirmationTasksProps = {
 	translate: typeof i18nTranslate;
@@ -7,36 +13,36 @@ type ConfirmationTasksProps = {
 export const getConfirmationTasks = ( { translate }: ConfirmationTasksProps ) => {
 	return [
 		{
-			illustration: 'TODO',
+			illustration: customDomain,
 			title: translate( 'Select your custom domain' ),
 			subtitle: translate(
 				'Enhance your brand and make your store more professional with a custom domain.'
 			),
 		},
 		{
-			illustration: 'TODO',
+			illustration: promote,
 			title: translate( 'Promote your products' ),
 			subtitle: translate( 'Grow your customer base by reaching millions of engaged shoppers.' ),
 		},
 		{
-			illustration: 'TODO',
+			illustration: wayToPay,
 			title: translate( 'Provide a way to pay ' ),
 			subtitle: translate(
 				'Set up one or more payment methods to make it easy for your customers to pay.'
 			),
 		},
 		{
-			illustration: 'TODO',
+			illustration: marketing,
 			title: translate( 'Create a marketing campaign' ),
 			subtitle: translate( 'Drive sales and build loyalty through automated marketing messages.' ),
 		},
 		{
-			illustration: 'TODO',
+			illustration: customize,
 			title: translate( 'Make your store stand out' ),
 			subtitle: translate( 'Keep customizing your store appearance and make it stand out.' ),
 		},
 		{
-			illustration: 'TODO',
+			illustration: manageWooCommerce,
 			title: translate( 'Manage your store on the go' ),
 			subtitle: translate( 'Manage your store anywhere with the free WooCommerce Mobile App.' ),
 		},
