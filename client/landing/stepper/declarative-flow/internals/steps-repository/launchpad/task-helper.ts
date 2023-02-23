1ced949712a8b32b39ce1dfdf7a5f7a1dbff539c
Jeremy Yip <jeremy.yip@automattic.com>
2023-02-23 10:47:06 -0800

    Launchpad: Update launchpad site editor links with canvas query param (#73465)
    * Update launchpad site editor links with canvas query param
    
    Currently, we always open the site editor in edit mode. Because there is value in defaulting to view mode, which keeps the navigation sidebar open by default, we're only defaulting to edit mode when certain query params are passed through, which have been specified by launchpad site editor links
    
    ---------
    
    Co-authored-by: mmtr <miguel.torres@automattic.com>

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-helper.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-helper.ts
@@ -52,7 +52,7 @@ export function getEnhancedTasks(
 	// send user to Home page editor, fallback to FSE if page id is not known
 	const launchpadUploadVideoLink = homePageId
 		? `/page/${ siteSlug }/${ homePageId }`
-		: `/site-editor/${ siteSlug }`;
+		: `/site-editor/${ siteSlug }?canvas=edit`;
 
 	let planWarningText = displayGlobalStylesWarning
 		? translate(
@@ -113,7 +113,7 @@ export function getEnhancedTasks(
 						completed: siteEditCompleted,
 						actionDispatch: () => {
 							recordTaskClickTracksEvent( flow, siteEditCompleted, task.id );
-							window.location.assign( `/site-editor/${ siteSlug }` );
+							window.location.assign( `/site-editor/${ siteSlug }?canvas=edit` );
 						},
 					};
 					break;
@@ -193,7 +193,7 @@ export function getEnhancedTasks(
 						completed: linkInBioLinksEditCompleted,
 						actionDispatch: () => {
 							recordTaskClickTracksEvent( flow, linkInBioLinksEditCompleted, task.id );
-							window.location.assign( `/site-editor/${ siteSlug }` );
+							window.location.assign( `/site-editor/${ siteSlug }?canvas=edit` );
 						},
 					};
 					break;
