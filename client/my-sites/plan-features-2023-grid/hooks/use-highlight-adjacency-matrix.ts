7fadecbb5dfbc2f13f099d62a9b9f8f5a7c2bf56
Christos <chriskmnds@gmail.com>
2023-02-23 14:22:40 +0200

    remove redundant memo

--- client/my-sites/plan-features-2023-grid/hooks/use-highlight-adjacency-matrix.ts
+++ client/my-sites/plan-features-2023-grid/hooks/use-highlight-adjacency-matrix.ts
@@ -1,5 +1,4 @@
 import { isBusinessPlan, isPremiumPlan } from '@automattic/calypso-products';
-import { useMemo } from '@wordpress/element';
 import { useSelector } from 'react-redux';
 import { getCurrentPlan } from 'calypso/state/sites/plans/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
@@ -17,47 +16,42 @@ const useHighlightIndices = ( visiblePlans: PlanProperties[] ) => {
 	const selectedSiteId = useSelector( getSelectedSiteId );
 	const currentPlan = useSelector( ( state ) => getCurrentPlan( state, selectedSiteId ) );
 
-	return useMemo( () => {
-		return visiblePlans.reduce< number[] >( ( acc, { planName }, index ) => {
-			const isHighlight =
-				isBusinessPlan( planName ) ||
-				isPremiumPlan( planName ) ||
-				currentPlan?.productSlug === planName;
+	return visiblePlans.reduce< number[] >( ( acc, { planName }, index ) => {
+		const isHighlight =
+			isBusinessPlan( planName ) ||
+			isPremiumPlan( planName ) ||
+			currentPlan?.productSlug === planName;
 
-			if ( isHighlight ) {
-				acc.push( index );
-			}
+		if ( isHighlight ) {
+			acc.push( index );
+		}
 
-			return acc;
-		}, [] );
-	}, [ currentPlan?.productSlug, visiblePlans ] );
+		return acc;
+	}, [] );
 };
 
 const useHighlightAdjacencyMatrix = ( visiblePlans: PlanProperties[] ) => {
 	const highlightIndices = useHighlightIndices( visiblePlans );
+	const adjacencyMatrix: HighlightAdjacencyMatrix = {};
 
-	return useMemo( () => {
-		const adjacencyMatrix: HighlightAdjacencyMatrix = {};
+	visiblePlans.forEach( ( { planName }, index ) => {
+		adjacencyMatrix[ planName ] = { leftOfHighlight: false, rightOfHighlight: false };
 
-		visiblePlans.forEach( ( { planName }, index ) => {
-			adjacencyMatrix[ planName ] = { leftOfHighlight: false, rightOfHighlight: false };
-
-			highlightIndices.forEach( ( highlightIndex ) => {
-				if ( highlightIndex === index - 1 ) {
-					adjacencyMatrix[ planName ].rightOfHighlight = true;
-				}
-				if ( highlightIndex === index + 1 ) {
-					adjacencyMatrix[ planName ].leftOfHighlight = true;
-				}
-			} );
+		highlightIndices.forEach( ( highlightIndex ) => {
+			if ( highlightIndex === index - 1 ) {
+				adjacencyMatrix[ planName ].rightOfHighlight = true;
+			}
+			if ( highlightIndex === index + 1 ) {
+				adjacencyMatrix[ planName ].leftOfHighlight = true;
+			}
 		} );
+	} );
 
-		if ( highlightIndices.length === 1 ) {
-			adjacencyMatrix[ visiblePlans[ highlightIndices[ 0 ] ].planName ].isOnlyHighlight = true;
-		}
+	if ( highlightIndices.length === 1 ) {
+		adjacencyMatrix[ visiblePlans[ highlightIndices[ 0 ] ].planName ].isOnlyHighlight = true;
+	}
 
-		return adjacencyMatrix;
-	}, [ highlightIndices, visiblePlans ] );
+	return adjacencyMatrix;
 };
 
 export default useHighlightAdjacencyMatrix;
