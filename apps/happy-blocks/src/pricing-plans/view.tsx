9933a8ad3b5cedb7e412c358c1b6da34db0f5c6a
Tony Arcangelini <tony@arcangelini.com>
2023-02-24 12:29:23 +0100

    Fix typing in view.tsx

--- apps/happy-blocks/src/pricing-plans/view.tsx
+++ apps/happy-blocks/src/pricing-plans/view.tsx
@@ -1,14 +1,12 @@
 import './view.scss';
 import domReady from '@wordpress/dom-ready';
 import { useState, render } from '@wordpress/element';
-import { FunctionComponent } from 'react';
 import PricingPlans from './components/pricing-plans';
 import Skeleton from './components/skeleton';
 import usePricingPlans from './hooks/pricing-plans';
-import { BlockAttributes } from './types';
+import type { BlockAttributes } from './types';
 
-// TODO: Added "any" to resolve type error with attributes
-const ViewPricingPlans: FunctionComponent< any > = ( { attributes } ) => {
+const ViewPricingPlans = ( { attributes }: { attributes: BlockAttributes } ) => {
 	const { data: plans, isLoading } = usePricingPlans();
 	const [ dummyAttributes, setDummyAttributes ] = useState( attributes );
 	const setAttributes = ( newValues: Partial< BlockAttributes > ) => {
