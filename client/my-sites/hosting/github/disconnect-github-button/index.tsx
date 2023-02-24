1f8feee4968373aa43fb4ea2e27dfe102e2f51d6
Philip Jackson <p-jackson@live.com>
2023-02-24 14:26:55 +1300

    Record tracks for unauthorizing with GitHub

--- client/my-sites/hosting/github/disconnect-github-button/index.tsx
+++ client/my-sites/hosting/github/disconnect-github-button/index.tsx
@@ -2,11 +2,12 @@ import { Button } from '@automattic/components';
 import { useI18n } from '@wordpress/react-i18n';
 import { useMutation, useQueryClient } from 'react-query';
 import { useDispatch, useSelector } from 'react-redux';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { deleteStoredKeyringConnection } from 'calypso/state/sharing/keyring/actions';
-import './style.scss';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { GITHUB_INTEGRATION_QUERY_KEY } from '../constants';
 import { GITHUB_CONNECTION_QUERY_KEY } from '../use-github-connection-query';
+import './style.scss';
 
 type Connection = Parameters< typeof deleteStoredKeyringConnection >[ 0 ];
 
@@ -21,6 +22,7 @@ export function DisconnectGitHubButton( { connection }: DisconnectGitHubButtonPr
 	const dispatch = useDispatch();
 	// Using ReactQuery to manage `isDisconnecting` state because it's not exposed from the Redux store.
 	const mutation = useMutation< unknown, unknown, Connection >( async ( c ) => {
+		dispatch( recordTracksEvent( 'calypso_hosting_github_unauthorize_click' ) );
 		await dispatch( deleteStoredKeyringConnection( c ) );
 		await queryClient.invalidateQueries( [
 			GITHUB_INTEGRATION_QUERY_KEY,
