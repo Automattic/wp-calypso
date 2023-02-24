fe322343971ddb0178a502c235329b06a25f3552
elliottprogrammer <bryan@elliottprogrammer.com>
2023-02-24 08:53:47 -0500

    Add 'View individual products' link.

---
+++ client/my-sites/plans/jetpack-plans/jetpack-complete-page/view-all-products-link/index.tsx
@@ -0,0 +1,26 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
+import { useTranslate } from 'i18n-calypso';
+
+import './style.scss';
+
+type Props = {
+	siteSlug?: string;
+};
+
+const ViewAllProductsLink: React.FC< Props > = ( { siteSlug } ) => {
+	const translate = useTranslate();
+
+	return (
+		<a
+			className="view-all-products-link"
+			onClick={ () => recordTracksEvent( 'calypso_view_individual_products_link_click' ) }
+			href={ `https://cloud.jetpack.com/pricing/${ siteSlug }` }
+			target="_blank"
+			rel="noreferrer"
+		>
+			{ translate( 'View individual products' ) }
+		</a>
+	);
+};
+
+export default ViewAllProductsLink;
