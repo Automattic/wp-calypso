f0aedf2c4b5317b76ab99b3edd41654445cf12e7
Edwin Takahashi <egao@outlook.com>
2023-02-23 16:45:34 -0800

    apps/o2-blocks/webpack.config.js - use BuildMetaPlugin

--- apps/o2-blocks/webpack.config.js
+++ apps/o2-blocks/webpack.config.js
@@ -2,8 +2,12 @@
  *WARNING: No ES6 modules here. Not transpiled! ****
  */
 
+const path = require( 'path' );
+const BuildMetaPlugin = require( '@automattic/calypso-apps-builder/build-meta-webpack-plugin.cjs' );
 const getBaseWebpackConfig = require( '@automattic/calypso-build/webpack.config.js' );
 const ReadableJsAssetsWebpackPlugin = require( '@wordpress/readable-js-assets-webpack-plugin' );
+
+const outputPath = path.join( __dirname, 'release-files' );
 
 function getWebpackConfig( env, argv ) {
 	const webpackConfig = getBaseWebpackConfig( { ...env, WP: true }, argv );
@@ -18,7 +22,11 @@ function getWebpackConfig( env, argv ) {
 			...webpackConfig.output,
 			filename: '[name].min.js',
 		},
-		plugins: [ ...webpackConfig.plugins, new ReadableJsAssetsWebpackPlugin() ],
+		plugins: [
+			...webpackConfig.plugins,
+			BuildMetaPlugin( { outputPath } ),
+			new ReadableJsAssetsWebpackPlugin(),
+		],
 	};
 }
 
