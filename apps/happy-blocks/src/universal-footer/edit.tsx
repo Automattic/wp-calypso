52ecee9ab7a4e149208d0b02df9c17edf619cbf2
Omar Alshaker <omar@omaralshaker.com>
2023-02-24 17:47:39 +0100

    Fix edit.js

--- apps/happy-blocks/src/universal-footer/edit.tsx
+++ apps/happy-blocks/src/universal-footer/edit.tsx
@@ -1,24 +1,7 @@
-import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, ColorPicker, Panel } from '@wordpress/components';
-import PlaceholderFooter from './placeholder';
-import type { FooterAttributes } from './types';
-import type { BlockEditProps } from '@wordpress/blocks';
+import { UniversalNavbarFooter } from '@automattic/wpcom-template-parts';
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+function noop() {}
 
-export default function Edit( { attributes, setAttributes }: BlockEditProps< FooterAttributes > ) {
-	return (
-		<>
-			<InspectorControls>
-				<Panel header="Footer Settings">
-					<PanelBody title="Logo Color" initialOpen={ true }>
-						<ColorPicker
-							color={ attributes.logoColor }
-							onChangeComplete={ ( color ) => setAttributes( { logoColor: color.hex } ) }
-						/>
-					</PanelBody>
-				</Panel>
-			</InspectorControls>
-
-			<PlaceholderFooter logoColor={ attributes.logoColor } />
-		</>
-	);
+export default function Edit() {
+	return <UniversalNavbarFooter currentRoute="/" isLoggedIn onLanguageChange={ noop } />;
 }
