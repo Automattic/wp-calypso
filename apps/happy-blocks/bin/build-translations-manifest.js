9b0939a81ec8b3e577131b58e003a12ed1f9be65
Tony Arcangelini <tony@arcangelini.com>
2023-02-27 22:35:26 +0100

    Happy Blocks: reconfigure webpack and artifact build

--- apps/happy-blocks/bin/build-translations-manifest.js
+++ apps/happy-blocks/bin/build-translations-manifest.js
@@ -3,37 +3,47 @@
 /* eslint-disable import/no-nodejs-modules */
 const fs = require( 'fs' );
 const path = require( 'path' );
+const glob = require( 'glob' );
 
 const PROJECT_DIR = path.resolve( __dirname, '..' );
 const ROOT_DIR = path.resolve( PROJECT_DIR, '..', '..' );
-const CHUNKS_MAP_PATH = path.resolve( PROJECT_DIR, 'dist', 'chunks-map.json' );
+const CHUNKS_MAP_PATHS = glob.sync( path.resolve( PROJECT_DIR, 'src/*/dist/', 'chunks-map.json' ) );
 const CALYPSO_STRINGS_PATH = path.resolve( PROJECT_DIR, 'dist', 'calypso-strings.pot' );
 const OUTPUT_PATH = path.resolve( PROJECT_DIR, 'dist', 'translations-manifest.json' );
-
-const chunksMap = JSON.parse(
-	fs.readFileSync( CHUNKS_MAP_PATH, {
-		encoding: 'utf8',
-	} )
-);
 
 const calypsoStrings = fs.readFileSync( CALYPSO_STRINGS_PATH, {
 	encoding: 'utf8',
 } );
 const stringRefs = new Set( calypsoStrings.match( /(?<=#:\s).+(?=:\d+)/gm ) );
+let translationsRefs = {};
 
-const translationsRefs = Object.fromEntries(
-	Object.entries( chunksMap ).map( ( [ key, refs ] ) => {
-		// Paths relative to the root directory and filtered to intersect with calypso-strings.pot refs.
-		const normalizedRefs = refs
-			.map( ( ref ) => path.relative( ROOT_DIR, ref ) )
-			.filter( ( ref ) => stringRefs.has( ref ) );
-		return [ key, normalizedRefs ];
-	} )
-);
+CHUNKS_MAP_PATHS.map( ( CHUNKS_MAP_PATH ) => {
+	const pathSegments = CHUNKS_MAP_PATH.split( path.sep );
+	const chunksMap = JSON.parse(
+		fs.readFileSync( CHUNKS_MAP_PATH, {
+			encoding: 'utf8',
+		} )
+	);
+
+	translationsRefs = {
+		...translationsRefs,
+		...Object.fromEntries(
+			Object.entries( chunksMap ).map( ( [ filename, refs ] ) => {
+				// Paths relative to the root directory and filtered to intersect with calypso-strings.pot refs.
+				const normalizedRefs = refs
+					.map( ( ref ) => path.relative( ROOT_DIR, ref ) )
+					.filter( ( ref ) => stringRefs.has( ref ) );
+				const key = 'src/' + pathSegments[ pathSegments.length - 3 ] + '/dist/' + filename;
+				return [ key, normalizedRefs ];
+			} )
+		),
+	};
+
+	fs.unlinkSync( CHUNKS_MAP_PATH );
+} );
 
 // Write translatios manifest file.
 fs.writeFileSync( OUTPUT_PATH, JSON.stringify( { references: translationsRefs } ) );
 
 // Clean up.
 fs.unlinkSync( CALYPSO_STRINGS_PATH );
-fs.unlinkSync( CHUNKS_MAP_PATH );
--- apps/happy-blocks/bin/prepArtifactForCI.sh
+++ apps/happy-blocks/bin/prepArtifactForCI.sh
@@ -3,10 +3,13 @@
 set -x
 
 # Copy dist files to the release directory.
-mkdir ../release-files
-cp -r ../dist ../release-files/dist/
+mkdir ../release-files/src
+for dist in ../src/*;
+	do mkdir -p ../release-files/$dist; 
+	cp -r $dist/dist/* ../release-files/$dist/; 
+done
 
 # Add the index.php file
 cp ../index.php ../release-files/
 
-printf "Finished configuration of the @automattic/happy-blocks plugin artifacts directory."
+printf "Finished configuration of the @automattic/happy-blocks plugin artifacts directory.\n"
