099d138b49240b47ea7a24881d76ae46bccf2c6d
Anthony Grullon <agrullon95@gmail.com>
2023-02-27 11:07:48 -0500

    update help center updated site options

--- packages/help-center/src/components/help-center-search.tsx
+++ packages/help-center/src/components/help-center-search.tsx
@@ -1,14 +1,10 @@
 /* eslint-disable wpcalypso/jsx-classname-namespace */
 /* eslint-disable no-restricted-imports */
-import { useSelect } from '@wordpress/data';
 import { useState, useCallback, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
 import InlineHelpSearchCard from 'calypso/blocks/inline-help/inline-help-search-card';
 import { decodeEntities, preventWidows } from 'calypso/lib/formatting';
-import { getSelectedSiteId } from 'calypso/state/ui/selectors';
-import { SITE_STORE } from '../stores';
 import { HelpCenterLaunchpad } from './help-center-launchpad';
 import { HelpCenterMoreResources } from './help-center-more-resources';
 import HelpCenterSearchResults from './help-center-search-results';
@@ -24,9 +20,7 @@ export const HelpCenterSearch = () => {
 
 	const [ searchQuery, setSearchQuery ] = useState( query || '' );
 
-	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
-	const site = useSelect( ( select ) => siteId && select( SITE_STORE ).getSite( siteId ) );
-	const launchpadEnabled = site && site?.options.launchpad_screen === 'full';
+	const launchpadEnabled = window?.helpCenterData?.currentSite?.launchpad_screen === 'full';
 
 	// Search query can be a query param, if the user searches or clears the search field
 	// we need to keep the query param up-to-date with that
