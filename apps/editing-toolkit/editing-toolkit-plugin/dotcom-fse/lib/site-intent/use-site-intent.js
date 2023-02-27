1e42acdfc8cd9b10c1e91d4763ac408e2c8220c4
okmttdhr <okmttdhr@users.noreply.github.com>
2023-02-27 11:08:55 +0900

    Add comment to replace unnecessary useSiteIntent

--- apps/editing-toolkit/editing-toolkit-plugin/dotcom-fse/lib/site-intent/use-site-intent.js
+++ apps/editing-toolkit/editing-toolkit-plugin/dotcom-fse/lib/site-intent/use-site-intent.js
@@ -1,6 +1,8 @@
 import apiFetch from '@wordpress/api-fetch';
 import { useState, useEffect, useCallback } from '@wordpress/element';
 
+// FIXME: We can use `useSiteIntent` from `@automattic/data-stores` and remove this.
+// https://github.com/Automattic/wp-calypso/pull/73565#discussion_r1113839120
 const useSiteIntent = () => {
 	const [ siteIntent, setSiteIntent ] = useState( undefined );
 	const [ siteIntentFetched, setSiteIntentFetched ] = useState( false );
