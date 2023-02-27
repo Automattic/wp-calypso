91133d594d38a059eace49dce27f67f3d2279cae
Rafael Agostini <rafael.agostini@automattic.com>
2023-02-26 22:44:02 -0500

    Add backup retention setting in Calypso Blue

--- client/my-sites/site-settings/settings-jetpack/main.jsx
+++ client/my-sites/site-settings/settings-jetpack/main.jsx
@@ -1,14 +1,18 @@
+import config from '@automattic/calypso-config';
 import { WPCOM_FEATURES_SCAN } from '@automattic/calypso-products';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import AdvancedCredentials from 'calypso/components/advanced-credentials';
+import BackupRetentionManagement from 'calypso/components/backup-retention-management';
 import DocumentHead from 'calypso/components/data/document-head';
 import QueryRewindState from 'calypso/components/data/query-rewind-state';
 import QuerySiteFeatures from 'calypso/components/data/query-site-features';
 import EmptyContent from 'calypso/components/empty-content';
 import FormattedHeader from 'calypso/components/formatted-header';
 import Main from 'calypso/components/main';
+import HasRetentionCapabilitiesSwitch from 'calypso/jetpack-cloud/sections/settings/has-retention-capabilities-switch';
+import AdvancedCredentialsLoadingPlaceholder from 'calypso/jetpack-cloud/sections/settings/loading';
 import JetpackDevModeNotice from 'calypso/my-sites/site-settings/jetpack-dev-mode-notice';
 import SiteSettingsNavigation from 'calypso/my-sites/site-settings/navigation';
 import getCurrentQueryArguments from 'calypso/state/selectors/get-current-query-arguments';
@@ -26,6 +30,7 @@ const SiteSettingsJetpack = ( {
 	host,
 	action,
 	translate,
+	retention,
 } ) => {
 	//todo: this check makes sense in Jetpack section?
 	if ( ! siteIsJetpack ) {
@@ -55,6 +60,16 @@ const SiteSettingsJetpack = ( {
 				align="left"
 			/>
 			<SiteSettingsNavigation site={ site } section="jetpack" />
+			{ config.isEnabled( 'jetpack/backup-retention-settings' ) ? (
+				// @TODO: Maybe we should move HasRetentionCapabilitiesSwitch to BackupRetentionManagement
+				// component
+				<HasRetentionCapabilitiesSwitch
+					siteId={ siteId }
+					trueComponent={ <BackupRetentionManagement defaultRetention={ retention } /> }
+					falseComponent={ null }
+					loadingComponent={ <AdvancedCredentialsLoadingPlaceholder /> } // Let's use the same placeholder for now
+				/>
+			) : null }
 			{ showCredentials && <AdvancedCredentials action={ action } host={ host } role="main" /> }
 		</Main>
 	);
@@ -72,6 +87,12 @@ export default connect( ( state ) => {
 	const siteId = getSelectedSiteId( state );
 	const { host, action } = getCurrentQueryArguments( state );
 
+	// This parameter is useful to redirect back from checkout page and select the retention period
+	// the customer previously selected.
+	const retention = Number.isInteger( Number( getCurrentQueryArguments( state ).retention ) )
+		? Number( getCurrentQueryArguments( state ).retention )
+		: undefined;
+
 	return {
 		site,
 		siteId,
@@ -82,5 +103,6 @@ export default connect( ( state ) => {
 			siteHasFeature( state, siteId, WPCOM_FEATURES_SCAN ),
 		host,
 		action,
+		retention,
 	};
 } )( localize( SiteSettingsJetpack ) );
