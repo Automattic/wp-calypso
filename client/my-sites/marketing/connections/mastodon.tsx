76cdc4edcc42989cf31e6fa0639873edd1748d92
Fernando Fernandes <fernandofernand3s@gmail.com>
2023-02-28 18:16:39 -0300

    Change component to use TypeScript

---
+++ client/my-sites/marketing/connections/mastodon.tsx
@@ -0,0 +1,151 @@
+import { FormInputValidation, Spinner } from '@automattic/components';
+import styled from '@emotion/styled';
+import { useTranslate } from 'i18n-calypso';
+import { useState, useEffect, useCallback, FormEvent, ChangeEvent } from 'react';
+import FormsButton from 'calypso/components/forms/form-button';
+import FormLabel from 'calypso/components/forms/form-label';
+import FormTextInput from 'calypso/components/forms/form-text-input';
+
+interface Props {
+	service: Service;
+	action: () => void;
+	connectAnother: () => void;
+	connections: Connection[];
+	isConnecting: boolean;
+}
+
+interface Service {
+	ID: string;
+	connect_URL: string;
+	description: string;
+	external_users_only: boolean;
+	genericon: {
+		class: string;
+		unicode: string;
+	};
+	icon: string;
+	jetpack_module_required: string;
+	jetpack_support: boolean;
+	label: string;
+	multiple_external_user_ID_support: boolean;
+	type: string;
+}
+
+interface Connection {
+	ID: number;
+	site_ID: number;
+	user_ID: number;
+	keyring_connection_ID: number;
+	keyring_connection_user_ID: number;
+	shared: boolean;
+	service: string;
+	label: string;
+	issued: string;
+	expires: string;
+	external_ID: string | null;
+	external_name: string | null;
+	external_display: string | null;
+	external_profile_picture: string | null;
+	external_profile_URL: string | null;
+	external_follower_count: number | null;
+	status: string;
+	refresh_URL: string;
+	meta: object;
+}
+
+const InstanceContainer = styled.div( {
+	alignItems: 'center',
+	display: 'flex',
+	gap: '10px',
+} );
+
+export const Mastodon: React.FC< Props > = ( {
+	service,
+	action,
+	connectAnother,
+	connections,
+	isConnecting,
+} ) => {
+	const translate = useTranslate();
+	const [ instance, setInstance ] = useState( '' );
+	const [ error, setError ] = useState( '' );
+
+	/**
+	 * Checks if username matches a valid Mastodon username.
+	 */
+	const isValidUsername = ( username: string ) =>
+		/@?\b([A-Z0-9_]+)@([A-Z0-9.-]+\.[A-Z]{2,})\b/gi.test( username );
+
+	const validateInstance = useCallback( () => {
+		if ( isValidUsername( instance ) || ! instance ) {
+			setError( '' );
+		} else {
+			setError( translate( 'This username is not valid.' ) );
+		}
+	}, [ instance, translate ] );
+
+	useEffect( () => {
+		validateInstance();
+	}, [ validateInstance ] );
+
+	const handleInstanceChange = ( e: ChangeEvent< HTMLInputElement > ) => {
+		setInstance( e.target.value.trim() );
+		validateInstance();
+	};
+
+	/**
+	 * Set the instance to the connect URL so that it can be used to connect to Mastodon.
+	 */
+	const setInstanceToConnectURL = () => {
+		const url = new URL( service.connect_URL );
+		url.searchParams.set( 'instance', instance );
+		service.connect_URL = url.toString();
+	};
+
+	/**
+	 * Handle the Connect account submission.
+	 */
+	const handleSubmit = ( e: FormEvent< HTMLFormElement > ) => {
+		e.preventDefault();
+		setInstanceToConnectURL();
+		connections.length >= 1 ? connectAnother() : action();
+	};
+
+	const showError = !! error;
+	return (
+		<div className="sharing-service-distributed-example">
+			<form onSubmit={ handleSubmit }>
+				<div className="sharing-service-example">
+					<FormLabel htmlFor="instance">{ translate( 'Enter your Mastodon username' ) }</FormLabel>
+					<InstanceContainer>
+						<FormTextInput
+							autoCapitalize="off"
+							autoCorrect="off"
+							spellCheck="false"
+							id="instance"
+							name="instance"
+							value={ instance }
+							isError={ showError }
+							onChange={ handleInstanceChange }
+						/>
+						{ showError && <Spinner /> }
+					</InstanceContainer>
+					{ showError && <FormInputValidation isError text={ error } /> }
+				</div>
+				<div className="sharing-service-example">
+					<FormsButton
+						primary
+						type="submit"
+						disabled={ ! isValidUsername( instance ) || showError || isConnecting }
+					>
+						{ connections.length >= 1
+							? translate( 'Connect one more account' )
+							: translate( 'Connect account' ) }
+					</FormsButton>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default Mastodon;
