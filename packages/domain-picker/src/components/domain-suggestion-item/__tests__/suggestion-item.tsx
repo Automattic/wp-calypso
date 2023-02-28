fc3568d97b2802a880f49d80699107c8039951f3
Igor Giussani <gius80@gmail.com>
2023-02-28 17:35:08 +0100

    Update unit test

--- packages/domain-picker/src/components/domain-suggestion-item/__tests__/suggestion-item.tsx
+++ packages/domain-picker/src/components/domain-suggestion-item/__tests__/suggestion-item.tsx
@@ -118,6 +118,35 @@ describe( 'conditional elements', () => {
 		// use `queryBy` to avoid throwing an error with `getBy`
 		expect( screen.queryByTestId( 'info-tooltip' ) ).toBeFalsy();
 	} );
+
+	it.skip( 'renders info tooltip for domains that require .gay information notice', async () => {
+		render(
+			<SuggestionItem { ...MOCK_SUGGESTION_ITEM_PARTIAL_PROPS } isDotGayNoticeRequired={ true } />
+		);
+
+		expect( screen.getByTestId( 'info-tooltip' ) ).toBeInTheDocument();
+	} );
+
+	it.skip( 'clicking info tooltip icon reveals popover for .gay information notice', async () => {
+		const testRequiredProps = {
+			domain: 'testdomain.gay',
+			cost: '€37.00',
+			railcarId: 'id',
+		};
+
+		render(
+			<SuggestionItem
+				{ ...testRequiredProps }
+				onSelect={ jest.fn() }
+				onRender={ jest.fn() }
+				isDotGayNoticeRequired={ true }
+			/>
+		);
+
+		fireEvent.click( screen.getByTestId( 'info-tooltip' ) );
+
+		expect( screen.queryByText( /LGBT content/i ) ).toBeTruthy();
+	} );
 	/*eslint-enable*/
 
 	it( 'should render a recommendation badge if given prop isRecommended true', async () => {
