72ae79a1ee0df53da67d805a5a52d82fcd796652
Payton Swick <payton@foolord.com>
2023-02-28 11:52:25 -0500

    Pass address to processor when submitting credit card

--- client/my-sites/checkout/composite-checkout/payment-methods/credit-card/credit-card-pay-button.tsx
+++ client/my-sites/checkout/composite-checkout/payment-methods/credit-card/credit-card-pay-button.tsx
@@ -65,6 +65,7 @@ export default function CreditCardPayButton( {
 							state: fields?.state?.value,
 							city: fields?.city?.value,
 							organization: fields?.organization?.value,
+							address: fields?.address1?.value, // TODO: is this address1 or address?
 							useForAllSubscriptions,
 							eventSource: 'checkout',
 						} );
