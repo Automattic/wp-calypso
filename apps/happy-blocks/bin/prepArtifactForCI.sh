5625cceb56ce9f32a9adb496d7f87600a7a4f83d
Tony Arcangelini <tony@arcangelini.com>
2023-02-28 18:30:21 +0100

    Update TC build

--- apps/happy-blocks/bin/prepArtifactForCI.sh
+++ apps/happy-blocks/bin/prepArtifactForCI.sh
@@ -3,14 +3,14 @@
 set -x
 
 # Copy build directories to the release directory.
-find ../block-library/* -type d -name "*" -prune |\
+find ./block-library/* -type d -name "*" -prune |\
 while read -r block;
 do
-	mkdir -p ../release-files/${block//\.\.\//}; 
-	cp -r $block/build/* ../release-files/${block//\.\.\//}/; 
+	mkdir -p ./release-files/${block//\.\.\//}; 
+	cp -r $block/build/* ./release-files/${block//\.\.\//}/; 
 done
 
 # Add the index.php file
-cp ../block-library/index.php ../release-files/
+cp ./index.php ./README.md ./dist/* ./release-files/
 
 printf "Finished configuration of the @automattic/happy-blocks plugin artifacts directory.\n"
