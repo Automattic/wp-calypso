0a7bdc239dd90ec42c352e305a1a0110aef7fedc
Jason Moon <jason.moon@automattic.com>
2023-02-28 12:06:36 -0800

    Replace WPCOM-specific code with Jetpack ones

--- client/my-sites/stats/jetpack-upsell-section/index.tsx
+++ client/my-sites/stats/jetpack-upsell-section/index.tsx
@@ -1,11 +1,5 @@
 import config from '@automattic/calypso-config';
 import {
-	JETPACK_BACKUP_PRODUCTS,
-	JETPACK_BOOST_PRODUCTS,
-	JETPACK_SEARCH_PRODUCTS,
-	JETPACK_SECURITY_PLANS,
-	JETPACK_SOCIAL_PRODUCTS,
-	JETPACK_VIDEOPRESS_PRODUCTS,
 	PLAN_JETPACK_SECURITY_T1_YEARLY,
 	PRODUCT_JETPACK_BACKUP_T1_YEARLY,
 	PRODUCT_JETPACK_BOOST,
@@ -15,37 +9,13 @@ import {
 } from '@automattic/calypso-products';
 import { JetpackUpsellCard } from '@automattic/components';
 import { useSelector } from 'react-redux';
-import QuerySitePlans from 'calypso/components/data/query-site-plans';
-import QuerySiteProducts from 'calypso/components/data/query-site-products';
 import { buildCheckoutURL } from 'calypso/my-sites/plans/jetpack-plans/get-purchase-url-callback';
-import { getSitePlan, getSiteProducts } from 'calypso/state/sites/selectors';
-import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import usePurchasedProducts from './use-purchased-products';
 
 const isOdysseyStats = config.isEnabled( 'is_running_in_jetpack_site' );
-interface SiteProduct {
-	productSlug: string;
-}
 
-function hasAnyProduct( siteProducts: SiteProduct[] | null, products: readonly string[] ) {
-	return siteProducts?.some( ( product ) => products.includes( product.productSlug ) );
-}
-
-function usePurchasedProucts( siteId: number | null ) {
-	const sitePlan = useSelector( ( state ) => getSitePlan( state, siteId ) );
-	const siteProducts = useSelector( ( state ) => getSiteProducts( state, siteId ) );
-
-	const products = [] as string[];
-	hasAnyProduct( siteProducts, JETPACK_BACKUP_PRODUCTS ) && products.push( 'backup' );
-	hasAnyProduct( siteProducts, JETPACK_BOOST_PRODUCTS ) && products.push( 'boost' );
-	hasAnyProduct( siteProducts, JETPACK_SEARCH_PRODUCTS ) && products.push( 'search' );
-	( JETPACK_SECURITY_PLANS as readonly string[] ).includes( sitePlan?.product_slug ?? '' ) &&
-		products.push( 'security' );
-	hasAnyProduct( siteProducts, JETPACK_SOCIAL_PRODUCTS ) && products.push( 'social' );
-	hasAnyProduct( siteProducts, JETPACK_VIDEOPRESS_PRODUCTS ) && products.push( 'video' );
-	return products;
-}
-
-const URL_PREFIX = 'https://wordpress.com';
+const CHECKOUT_URL_PREFIX = 'https://wordpress.com';
 const QUERY_VALUES = {
 	source: 'jetpack-stats-upsell-section',
 
@@ -54,39 +24,46 @@ const QUERY_VALUES = {
 };
 
 export default function JetpackUpsellSection() {
-	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
+	// NOTE: This will only work within Odyssey Stats.
 	const siteSlug = useSelector( ( state ) => getSelectedSiteSlug( state ) );
 
-	const purchasedProducts = usePurchasedProucts( siteId );
+	const { purchasedProducts, error, isLoading } = usePurchasedProducts();
+
+	// Exit early if we don't have and can't get the site purchase data.
+	// Also exit early if we're not in the Odyssey Stats environment.
+	if ( isLoading || error || ! isOdysseyStats ) {
+		return null;
+	}
+
+	// Build checkout URL prefixed with WordPress.com.
 	const upgradeUrls: Record< string, string > = ! siteSlug
 		? {}
 		: {
 				backup:
-					URL_PREFIX + buildCheckoutURL( siteSlug, PRODUCT_JETPACK_BACKUP_T1_YEARLY, QUERY_VALUES ),
-				boost: URL_PREFIX + buildCheckoutURL( siteSlug, PRODUCT_JETPACK_BOOST, QUERY_VALUES ),
-				search: URL_PREFIX + buildCheckoutURL( siteSlug, PRODUCT_JETPACK_SEARCH, QUERY_VALUES ),
+					CHECKOUT_URL_PREFIX +
+					buildCheckoutURL( siteSlug, PRODUCT_JETPACK_BACKUP_T1_YEARLY, QUERY_VALUES ),
+				boost:
+					CHECKOUT_URL_PREFIX + buildCheckoutURL( siteSlug, PRODUCT_JETPACK_BOOST, QUERY_VALUES ),
+				search:
+					CHECKOUT_URL_PREFIX + buildCheckoutURL( siteSlug, PRODUCT_JETPACK_SEARCH, QUERY_VALUES ),
 				security:
-					URL_PREFIX + buildCheckoutURL( siteSlug, PLAN_JETPACK_SECURITY_T1_YEARLY, QUERY_VALUES ),
+					CHECKOUT_URL_PREFIX +
+					buildCheckoutURL( siteSlug, PLAN_JETPACK_SECURITY_T1_YEARLY, QUERY_VALUES ),
 				social:
-					URL_PREFIX + buildCheckoutURL( siteSlug, PRODUCT_JETPACK_SOCIAL_BASIC, QUERY_VALUES ),
-				video: URL_PREFIX + buildCheckoutURL( siteSlug, PRODUCT_JETPACK_VIDEOPRESS, QUERY_VALUES ),
+					CHECKOUT_URL_PREFIX +
+					buildCheckoutURL( siteSlug, PRODUCT_JETPACK_SOCIAL_BASIC, QUERY_VALUES ),
+				video:
+					CHECKOUT_URL_PREFIX +
+					buildCheckoutURL( siteSlug, PRODUCT_JETPACK_VIDEOPRESS, QUERY_VALUES ),
 		  };
 
 	return (
-		isOdysseyStats && (
-			<div className="jetpack-upsell-section">
-				{ siteId && (
-					<>
-						<QuerySitePlans siteId={ siteId } />
-						<QuerySiteProducts siteId={ siteId } />
-					</>
-				) }
-				<JetpackUpsellCard
-					purchasedProducts={ purchasedProducts }
-					siteSlug={ siteSlug }
-					upgradeUrls={ upgradeUrls }
-				/>
-			</div>
-		)
+		<div className="jetpack-upsell-section">
+			<JetpackUpsellCard
+				purchasedProducts={ purchasedProducts }
+				siteSlug={ siteSlug }
+				upgradeUrls={ upgradeUrls }
+			/>
+		</div>
 	);
 }
---
+++ client/my-sites/stats/jetpack-upsell-section/use-purchased-products.tsx
@@ -0,0 +1,63 @@
+import {
+	JETPACK_BACKUP_PRODUCTS,
+	JETPACK_BOOST_PRODUCTS,
+	JETPACK_SEARCH_PRODUCTS,
+	JETPACK_SECURITY_PLANS,
+	JETPACK_SOCIAL_PRODUCTS,
+	JETPACK_VIDEOPRESS_PRODUCTS,
+} from '@automattic/calypso-products';
+import useSWR from 'swr';
+
+//
+// WARNING: This hook will only work within Odyssey Stats!
+// It depends on api_root and nonce from window.configData!
+// It also requires the existence of ${api_root}/jetpack/v4/site/purchases!
+//
+
+// Include data from stats-admin initialization for making resource requests.
+const purchasesFetcher = ( resource: string, options = {} ) =>
+	// Prefix resource with api_root for the site.
+	fetch( window.configData?.api_root + resource, {
+		...options,
+		credentials: 'include',
+		// Attach WordPress nonce to request.
+		headers: { 'X-WP-Nonce': window.configData?.nonce },
+	} )
+		.then( ( res ) => res.json() )
+		.then( ( json ) => JSON.parse( json.data ) );
+
+const KEY_SLUG_MAP = new Map( [
+	[ 'backup', JETPACK_BACKUP_PRODUCTS as readonly string[] ],
+	[ 'boost', JETPACK_BOOST_PRODUCTS as readonly string[] ],
+	[ 'search', JETPACK_SEARCH_PRODUCTS as readonly string[] ],
+	[ 'security', JETPACK_SECURITY_PLANS as readonly string[] ],
+	[ 'social', JETPACK_SOCIAL_PRODUCTS as readonly string[] ],
+	[ 'video', JETPACK_VIDEOPRESS_PRODUCTS as readonly string[] ],
+] );
+
+function formatResponse( responseData?: Record< string, string >[] ) {
+	const products = [] as string[];
+	// Find active purchase product slugs.
+	const purchasedProductSlugs =
+		responseData?.filter( ( p ) => p.active === '1' )?.map( ( p ) => p.product_slug ) ?? [];
+	// Append active product sulgs to the products array.
+	KEY_SLUG_MAP.forEach( ( value, key ) => {
+		if ( purchasedProductSlugs.some( ( slug ) => value.includes( slug ) ) ) {
+			products.push( key );
+		}
+	} );
+	return products;
+}
+
+export default function usePurchasedProducts() {
+	const {
+		data: responseData,
+		error,
+		isLoading,
+	} = useSWR( 'jetpack/v4/site/purchases', purchasesFetcher );
+	return {
+		purchasedProducts: formatResponse( responseData ),
+		error,
+		isLoading,
+	};
+}
