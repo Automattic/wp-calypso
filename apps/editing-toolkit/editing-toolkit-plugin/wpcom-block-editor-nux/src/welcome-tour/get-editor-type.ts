3fa59ec3f58a36bf6ed44486df5b01a96068fb46
okmttdhr <okmttdhr@users.noreply.github.com>
2023-03-01 14:56:40 +0900

    Check the existence of store to see editor type
    Beware when using this method to figure out if we are in the site editor.
    https://github.com/WordPress/gutenberg/issues/46616#issuecomment-1355301090
    https://github.com/Automattic/jetpack/blob/2e56d0d/projects/plugins/jetpack/extensions/shared/get-editor-type.js

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/welcome-tour/get-editor-type.ts
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/welcome-tour/get-editor-type.ts
@@ -1,17 +1,45 @@
 import { select } from '@wordpress/data';
 
-type EditorType = 'site' | 'post' | 'page';
+/**
+ * Post (Post Type: ‘post’)
+ * Page (Post Type: ‘page’)
+ * Attachment (Post Type: ‘attachment’)
+ * Revision (Post Type: ‘revision’)
+ * Navigation menu (Post Type: ‘nav_menu_item’)
+ * Block templates (Post Type: ‘wp_template’)
+ * Template parts (Post Type: ‘wp_template_part’)
+ *
+ * @see https://developer.wordpress.org/themes/basics/post-types/#default-post-types
+ */
+
+type PostType =
+	| 'post'
+	| 'page'
+	| 'post'
+	| 'page'
+	| 'attachment'
+	| 'revision'
+	| 'nav_menu_item'
+	| 'wp_template'
+	| 'wp_template_part'
+	| null;
+
+type EditorType = 'site' | PostType;
+
 export const getEditorType = (): EditorType | undefined => {
-	const editorSelector = select( 'core/editor' );
-	const postType = editorSelector.getCurrentPostType();
-	switch ( postType ) {
-		case null:
-			return 'site';
-		case 'post':
-			return 'post';
-		case 'page':
-			return 'page';
-		default:
-			return undefined;
+	/**
+	 * Beware when using this method to figure out if we are in the site editor.
+	 *
+	 * @see https://github.com/WordPress/gutenberg/issues/46616#issuecomment-1355301090
+	 * @see https://github.com/Automattic/jetpack/blob/2e56d0d/projects/plugins/jetpack/extensions/shared/get-editor-type.js
+	 */
+	if ( select( 'core/edit-site' ) ) {
+		return 'site';
 	}
+
+	if ( select( 'core/editor' ) ) {
+		return select( 'core/editor' ).getCurrentPostType() as PostType;
+	}
+
+	return undefined;
 };
