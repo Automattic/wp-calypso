a8758cdde5dd3c9d0ecfb71d94316c17067594be
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2023-03-02 11:15:15 +0200

    happy-blocks: Load translations data in i18n-calypso

--- apps/happy-blocks/block-library/universal-footer/view.tsx
+++ apps/happy-blocks/block-library/universal-footer/view.tsx
@@ -2,20 +2,25 @@ import { LocaleProvider } from '@automattic/i18n-utils';
 import { UniversalNavbarFooter } from '@automattic/wpcom-template-parts';
 import domReady from '@wordpress/dom-ready';
 import { render } from '@wordpress/element';
+import useI18nCalypsoTranslations from '../shared/use-i18n-calypso-translations';
 
-domReady( () => {
-	const isLoggedIn = document.body.classList.contains( 'logged-in' );
-	const block = document.querySelector( '.happy-blocks-universal-footer-block' );
-	const attributes = JSON.parse( ( block as HTMLElement )?.dataset?.attributes ?? `{}` );
-
-	render(
+function View( { isLoggedIn = false, attributes = {} as any } ) {
+	useI18nCalypsoTranslations();
+	return (
 		<LocaleProvider localeSlug={ attributes.locale }>
 			<UniversalNavbarFooter
 				currentRoute={ window.location.pathname }
 				onLanguageChange={ ( event ) => window.location.assign( event.target.value ) }
 				isLoggedIn={ isLoggedIn }
 			/>
-		</LocaleProvider>,
-		block
+		</LocaleProvider>
 	);
+}
+
+domReady( () => {
+	const isLoggedIn = document.body.classList.contains( 'logged-in' );
+	const block = document.querySelector( '.happy-blocks-universal-footer-block' );
+	const attributes = JSON.parse( ( block as HTMLElement )?.dataset?.attributes ?? `{}` );
+
+	render( <View isLoggedIn={ isLoggedIn } attributes={ attributes } />, block );
 } );
