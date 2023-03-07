60307512a569649c0869ac38d1cd8ce5e02526bd
rjchow <me@rjchow.com>
2023-03-07 16:43:40 +0800

    added components to subtitle

--- client/landing/stepper/declarative-flow/internals/steps-repository/processing-step/hooks/use-processing-loading-messages.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/processing-step/hooks/use-processing-loading-messages.tsx
@@ -2,6 +2,7 @@ import { Onboard } from '@automattic/data-stores';
 import { isWooExpressFlow } from '@automattic/onboarding';
 import { useSelect } from '@wordpress/data';
 import { useI18n } from '@wordpress/react-i18n';
+import WooPurpleHeart from 'calypso/assets/images/onboarding/woo-purple-heart.png';
 import { STEPPER_INTERNAL_STORE } from 'calypso/landing/stepper/stores';
 import type { LoadingMessage } from './types';
 
@@ -28,8 +29,13 @@ export function useProcessingLoadingMessages( flow?: string | null ): LoadingMes
 				return [
 					{
 						title: __( "Woo! We're creating your store" ),
-						subtitle: __(
-							"#FunWooFact: Did you know that Woo powers almost 4 million stores worldwide? You're in good company."
+						subtitle: (
+							<>
+								<strong>{ __( '#FunWooFact: ' ) }</strong>
+								{ __(
+									"Did you know that Woo powers almost 4 million stores worldwide? You're in good company."
+								) }
+							</>
 						),
 						duration: 15000,
 					},
@@ -38,15 +44,23 @@ export function useProcessingLoadingMessages( flow?: string | null ): LoadingMes
 				return [
 					{
 						title: __( 'Building the foundations' ),
-						subtitle: __(
-							'#FunWooFact: Did you know that Woo was founded by two South Africans and a Norwegian? Here are three alternative ways to say "store" in those countries - Winkel, ivenkile, and butikk.'
+						subtitle: (
+							<>
+								<strong>{ __( '#FunWooFact: ' ) }</strong>
+								{ __(
+									'Did you know that Woo was founded by two South Africans and a Norwegian? Here are three alternative ways to say "store" in those countries - Winkel, ivenkile, and butikk.'
+								) }
+							</>
 						),
 						duration: 15000,
 					},
 					{
 						title: __( 'Organizing the stock room' ),
-						subtitle: __(
-							'#FunWooFact: Are you Team Cat or Team Dog? The Woo team is split 50/50!'
+						subtitle: (
+							<>
+								<strong>{ __( '#FunWooFact: ' ) }</strong>
+								{ __( 'Are you Team Cat or Team Dog? The Woo team is split 50/50!' ) }
+							</>
 						),
 						duration: 15000,
 					},
@@ -55,21 +69,41 @@ export function useProcessingLoadingMessages( flow?: string | null ): LoadingMes
 				return [
 					{
 						title: __( 'Applying the finishing touches' ),
-						subtitle: __(
-							'#FunWooFact: There are more than 150 WooCommerce meetups held all over the world! A great way to meet fellow store owners.'
+						subtitle: (
+							<>
+								<strong>{ __( '#FunWooFact: ' ) }</strong>
+								{ __(
+									'There are more than 150 WooCommerce meetups held all over the world! A great way to meet fellow store owners.'
+								) }
+							</>
 						),
 						duration: 10000,
 					},
 					{
 						title: __( 'Turning on the lights' ),
-						subtitle: __(
-							'#FunWooFact: The Woo team is made up of over 350 talented individuals, distributed across 30+ countries.'
+						subtitle: (
+							<>
+								<strong>{ __( '#FunWooFact: ' ) }</strong>
+								{ __(
+									'The Woo team is made up of over 350 talented individuals, distributed across 30+ countries.'
+								) }
+							</>
 						),
 						duration: 15000,
 					},
 					{
 						title: __( 'Opening the doors' ),
-						subtitle: __( '#FunWooFact: Our favorite color is purple 💜' ),
+						subtitle: (
+							<>
+								<strong>{ __( '#FunWooFact: ' ) }</strong>
+								{ __( 'Our favorite color is purple ' ) }
+								<img
+									className="woo-inline-purple-heart"
+									alt="Woo Purple Heart Emoji"
+									src={ WooPurpleHeart }
+								/>
+							</>
+						),
 						// Set a very long duration to make sure it shows until the step is completed
 						duration: 150000,
 					},
