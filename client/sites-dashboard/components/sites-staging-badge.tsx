2337ad1eb43e6ee1c066889caf37ba91432936ff
Kateryna Kodonenko <kateryna@automattic.com>
2023-03-07 11:12:27 +0100

    SMP: add staging badges to grid and row

---
+++ client/sites-dashboard/components/sites-staging-badge.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import SitesLaunchStatusBadge from './sites-launch-status-badge';
+
+const SitesStagingBadge = styled( SitesLaunchStatusBadge )( {
+	backgroundColor: '#f2ca00',
+	color: '#513500',
+} );
+
+export default SitesStagingBadge;
