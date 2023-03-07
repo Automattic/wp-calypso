9ea69b2d112a361a25a8b6ae6270711a3fd09e6b
Nelson <nelsonec87@gmail.com>
2023-03-07 17:00:12 -0300

    Updated links from the trial My Plan page

--- client/my-sites/plans/current-plan/ecommerce-trial/index.tsx
+++ client/my-sites/plans/current-plan/ecommerce-trial/index.tsx
@@ -1,5 +1,5 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
-import { PLAN_ECOMMERCE_MONTHLY } from '@automattic/calypso-products';
+import { PLAN_WOOEXPRESS_MEDIUM_MONTHLY } from '@automattic/calypso-products';
 import { Button } from '@automattic/components';
 import { useMediaQuery } from '@wordpress/compose';
 import { useTranslate } from 'i18n-calypso';
@@ -43,17 +43,17 @@ const ECommerceTrialCurrentPlan = () => {
 	const displayAllIncluded = ! isMobile || showAllTrialFeaturesInMobileView;
 
 	/**
-	 * Redirects to the checkout page with the ecommerce plan.
+	 * Redirects to the checkout page with the WX Medium Plan.
 	 *
 	 * @param ctaPosition - The position of the CTA that triggered the redirect.
 	 */
-	const goToCheckoutWithEcommercePlan = ( ctaPosition: string ) => {
+	const goToCheckoutWithWooExpressPlan = ( ctaPosition: string ) => {
 		recordTracksEvent( `calypso_wooexpress_my_plan_cta`, {
 			cta_position: ctaPosition,
 		} );
 
 		const checkoutUrl = getECommerceTrialCheckoutUrl( {
-			productSlug: PLAN_ECOMMERCE_MONTHLY,
+			productSlug: PLAN_WOOEXPRESS_MEDIUM_MONTHLY,
 			siteSlug: selectedSite?.slug ?? '',
 		} );
 
@@ -163,7 +163,7 @@ const ECommerceTrialCurrentPlan = () => {
 		<Button
 			className="e-commerce-trial-current-plan__trial-card-cta"
 			primary
-			onClick={ () => goToCheckoutWithEcommercePlan( 'card' ) }
+			onClick={ () => goToCheckoutWithWooExpressPlan( 'card' ) }
 		>
 			{ translate( 'Upgrade now' ) }
 		</Button>
@@ -225,7 +225,7 @@ const ECommerceTrialCurrentPlan = () => {
 			<div className="e-commerce-trial-current-plan__cta-wrapper">
 				<Button
 					className="e-commerce-trial-current-plan__cta is-primary"
-					onClick={ () => goToCheckoutWithEcommercePlan( 'footer' ) }
+					onClick={ () => goToCheckoutWithWooExpressPlan( 'footer' ) }
 				>
 					{ translate( 'Upgrade now' ) }
 				</Button>
