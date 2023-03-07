25f49de7985c998fb0f84fa763b1054efd43acc2
Marin Atanasov <tyxla@abv.bg>
2023-03-07 16:31:18 +0200

    Fix bad import

--- packages/data-stores/src/onboard/index.ts
+++ packages/data-stores/src/onboard/index.ts
@@ -5,7 +5,7 @@ import * as actions from './actions';
 import { STORE_KEY } from './constants';
 import reducer, { State } from './reducer';
 import * as selectors from './selectors';
-import type { SelectFromMap } from '@automattic/data-stores';
+import type { SelectFromMap } from '../mapped-types';
 
 export type { State };
 export type OnboardSelect = SelectFromMap< typeof selectors >;
