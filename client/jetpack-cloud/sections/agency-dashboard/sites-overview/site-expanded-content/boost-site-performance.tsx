9567b6de92f8d51268fe65e7e564ed5dcc00b726
Yashwin <yashwinpoojary@gmail.com>
2023-03-08 12:18:55 +0530

    Move icons to icons file

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-expanded-content/boost-site-performance.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-expanded-content/boost-site-performance.tsx
@@ -2,6 +2,7 @@ import { Button } from '@automattic/components';
 import { Icon, help } from '@wordpress/icons';
 import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
+import { jetpackBoostDesktopIcon, jetpackBoostMobileIcon } from '../../icons';
 import { getBoostRating, getBoostRatingClass } from '../utils';
 import ExpandedCard from './expanded-card';
 import type { BoostData } from '../types';
@@ -53,16 +54,7 @@ export default function BoostSitePerformance( { boostData, hasBoost }: Props ) {
 								<Icon
 									size={ 26 }
 									className="site-expanded-content__device-icon"
-									icon={
-										<svg viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-											<path
-												fillRule="evenodd"
-												clipRule="evenodd"
-												d="M3.91113 6.5C3.91113 5.67157 4.58271 5 5.41113 5H14.0778C14.9062 5 15.5778 5.67157 15.5778 6.5V12.5H16.1195C16.7408 12.5 17.2445 13.0037 17.2445 13.625H2.24451C2.24451 13.0037 2.74819 12.5 3.36951 12.5H3.91113V6.5ZM5.41113 6.125H14.0778C14.2849 6.125 14.4528 6.29289 14.4528 6.5V12.2083H5.03613V6.5C5.03613 6.29289 5.20403 6.125 5.41113 6.125Z"
-												fill="#646970"
-											/>
-										</svg>
-									}
+									icon={ jetpackBoostDesktopIcon }
 								/>
 								<span className="site-expanded-content__device-score">{ desktopScore }</span>
 							</div>
@@ -70,17 +62,7 @@ export default function BoostSitePerformance( { boostData, hasBoost }: Props ) {
 								<Icon
 									className="site-expanded-content__device-icon"
 									size={ 26 }
-									icon={
-										<svg viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-											<path d="M10.4945 12.5H8.99451V13.625H10.4945V12.5Z" fill="#646970" />
-											<path
-												fillRule="evenodd"
-												clipRule="evenodd"
-												d="M5.99451 5C5.99451 4.17157 6.66608 3.5 7.49451 3.5H11.9945C12.8229 3.5 13.4945 4.17157 13.4945 5V14C13.4945 14.8284 12.8229 15.5 11.9945 15.5H7.49451C6.66608 15.5 5.99451 14.8284 5.99451 14V5ZM7.49451 4.625H11.9945C12.2016 4.625 12.3695 4.79289 12.3695 5V14C12.3695 14.2071 12.2016 14.375 11.9945 14.375H7.49451C7.2874 14.375 7.11951 14.2071 7.11951 14V5C7.11951 4.79289 7.2874 4.625 7.49451 4.625Z"
-												fill="#646970"
-											/>
-										</svg>
-									}
+									icon={ jetpackBoostMobileIcon }
 								/>
 								<span className="site-expanded-content__device-score">{ mobileScore }</span>
 							</div>
