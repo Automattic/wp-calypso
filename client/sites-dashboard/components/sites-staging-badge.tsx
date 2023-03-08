a029ae53e08c31078c94bf70cec6af1ce5fc751b
Kateryna Kodonenko <kateryna@automattic.com>
2023-03-08 11:01:57 +0100

    Site Switcher: add staging badge

--- client/sites-dashboard/components/sites-staging-badge.tsx
+++ client/sites-dashboard/components/sites-staging-badge.tsx
@@ -1,9 +1,17 @@
 import styled from '@emotion/styled';
 import SitesLaunchStatusBadge from './sites-launch-status-badge';
+interface SitesStagingBadgeProps {
+	secondary?: boolean;
+}
 
-const SitesStagingBadge = styled( SitesLaunchStatusBadge )( {
-	backgroundColor: '#f2ca00',
-	color: '#513500',
-} );
+const SitesStagingBadge = styled( SitesLaunchStatusBadge )( ( props: SitesStagingBadgeProps ) => ( {
+	color: '#4f3500',
+	backgroundColor: '#f0c930',
+	borderRadius: props.secondary ? 12 : 4,
+	'.current-site .site:hover &, .purchases-site .site:hover &': {
+		color: '#1C1300',
+		backgroundColor: '#C08C00',
+	},
+} ) );
 
 export default SitesStagingBadge;
