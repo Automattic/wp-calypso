e410f409bcb8e8651b6cbfb2bbf45361da7c5799
dpasque <dan.speckhard.pasque@automattic.com>
2023-03-08 10:43:39 -0600

    Test the media settings for all JP sites

--- test/e2e/specs/jetpack/settings__media.ts
+++ test/e2e/specs/jetpack/settings__media.ts
@@ -8,55 +8,78 @@ import {
 	SidebarComponent,
 	WritingSettingsPage,
 	envVariables,
+	getTestAccountByFeature,
+	envToFeatureKey,
+	WpAdminMediaSettingsPage,
 } from '@automattic/calypso-e2e';
 import { Page, Browser } from 'playwright';
-import { skipDescribeIf } from '../../jest-helpers';
 
 declare const browser: Browser;
 
-// This would be great to cover in WPCOM sites too, but it's in a different location,
-// so it will need extra handling.
-// Also, we're debugging an issue with the staging-only sites and this menu.
-// See p1677269954462129-slack-C01U2KGS2PQ
-skipDescribeIf( envVariables.JETPACK_TARGET !== 'remote-site' )(
-	DataHelper.createSuiteTitle( 'Jetpack Settings: Media' ),
-	function () {
-		let page: Page;
-		let writingSettingsPage: WritingSettingsPage;
+// We care about these settings in all Jetpack sites, but they are in a different place
+// depending on whether the site is a WPCOM site or a remote site.
+describe( DataHelper.createSuiteTitle( 'Jetpack Settings: Media' ), function () {
+	let page: Page;
 
-		beforeAll( async function () {
-			page = await browser.newPage();
+	const accountName = getTestAccountByFeature( envToFeatureKey( envVariables ) );
 
-			const testAccount = new TestAccount( 'jetpackRemoteSiteUser' );
-			await testAccount.authenticate( page );
+	beforeAll( async function () {
+		page = await browser.newPage();
+
+		const testAccount = new TestAccount( accountName );
+		await testAccount.authenticate( page );
+	} );
+
+	if ( envVariables.JETPACK_TARGET === 'remote-site' ) {
+		describe( 'Under writing settings for a remote site', function () {
+			let writingSettingsPage: WritingSettingsPage;
+
+			it( 'Navigate to Settings > Writing', async function () {
+				const sidebarComponent = new SidebarComponent( page );
+				await sidebarComponent.navigate( 'Settings', 'Writing' );
+				writingSettingsPage = new WritingSettingsPage( page );
+			} );
+
+			it( 'Toggle Carousel', async function () {
+				await writingSettingsPage.toggleOn(
+					'parent',
+					'Transform standard image galleries into full-screen slideshows'
+				);
+			} );
+
+			it( 'Toggle photo metadata', async function () {
+				await writingSettingsPage.toggleOn(
+					'child',
+					'Show photo metadata in carousel, when available'
+				);
+			} );
+
+			it( 'Select "Black" for carousel background color', async function () {
+				await writingSettingsPage.selectCarouselBackgroundColor( 'Black' );
+			} );
 		} );
+	} else {
+		// WPCOM Site
+		describe( 'Under media settings for a WPCOM site', function () {
+			let wpAdminMediaSettingsPage: WpAdminMediaSettingsPage;
 
-		it( 'Navigate to Settings > Writing', async function () {
-			const sidebarComponent = new SidebarComponent( page );
-			await sidebarComponent.navigate( 'Settings', 'Writing' );
-		} );
+			it( 'Navigate to Settings > Media', async function () {
+				const sidebarComponent = new SidebarComponent( page );
+				await sidebarComponent.navigate( 'Settings', 'Media' );
+				wpAdminMediaSettingsPage = new WpAdminMediaSettingsPage( page );
+			} );
 
-		it( 'Media setting section is visible', async function () {
-			writingSettingsPage = new WritingSettingsPage( page );
-			await writingSettingsPage.validateSection( 'Media' );
-		} );
+			it( 'Toggle Carousel', async function () {
+				await wpAdminMediaSettingsPage.toggleEnableCarousel();
+			} );
 
-		it( 'Enable Carousel', async function () {
-			await writingSettingsPage.toggleOn(
-				'parent',
-				'Transform standard image galleries into full-screen slideshows'
-			);
-		} );
+			it( 'Toggle photo metadata', async function () {
+				await wpAdminMediaSettingsPage.toggleCarouselMetadata();
+			} );
 
-		it( 'Enable photo metadata', async function () {
-			await writingSettingsPage.toggleOn(
-				'child',
-				'Show photo metadata in carousel, when available'
-			);
-		} );
-
-		it( 'Select "Black" for carousel background color', async function () {
-			await writingSettingsPage.selectCarouselBackgroundColor( 'Black' );
+			it( 'Select "Black" for carousel background color', async function () {
+				await wpAdminMediaSettingsPage.setBackGroundColor( 'Black' );
+			} );
 		} );
 	}
-);
+} );
