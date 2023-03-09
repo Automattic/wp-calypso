9c58372ab615477336deddd5b81607d6706e47d0
Erick Danzer <edanzer@gmail.com>
2023-03-09 11:14:51 -0700

    Launchpad: Add Verify Email to newsletter flow (#74135)

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/tasks.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/tasks.tsx
@@ -106,6 +106,12 @@ export const tasks: Task[] = [
 		disabled: false,
 		taskType: 'blog',
 	},
+	{
+		id: 'verify_email',
+		completed: false,
+		disabled: true,
+		taskType: 'blog',
+	},
 ];
 
 const linkInBioTaskList = [
@@ -117,7 +123,13 @@ const linkInBioTaskList = [
 ];
 
 export const launchpadFlowTasks: LaunchpadFlowTaskList = {
-	newsletter: [ 'setup_newsletter', 'plan_selected', 'subscribers_added', 'first_post_published' ],
+	newsletter: [
+		'setup_newsletter',
+		'plan_selected',
+		'subscribers_added',
+		'verify_email',
+		'first_post_published',
+	],
 	[ LINK_IN_BIO_FLOW ]: linkInBioTaskList,
 	[ LINK_IN_BIO_TLD_FLOW ]: linkInBioTaskList,
 	free: [
