01708ff5b180c7e78aecba6dea331064dda96477
Omar Alshaker <omar@omaralshaker.com>
2023-03-10 17:00:31 +0100

    Fix when alternates are missing

--- apps/happy-blocks/block-library/universal-footer/view.tsx
+++ apps/happy-blocks/block-library/universal-footer/view.tsx
@@ -1,8 +1,13 @@
+import { localizeUrl } from '@automattic/i18n-utils';
 import { PureUniversalNavbarFooter, LanguageOptions } from '@automattic/wpcom-template-parts';
 import domReady from '@wordpress/dom-ready';
 import { hydrate } from 'react-dom';
 
 domReady( () => {
+	const props: {
+		languageOptions?: LanguageOptions;
+	} = {};
+
 	const availableLanguages = Array.from(
 		document.querySelectorAll( 'link[hrefLang]:not( [hrefLang=x-default] )' )
 	) as HTMLLinkElement[];
@@ -14,8 +19,17 @@ domReady( () => {
 
 	function onLanguageChange( event: React.ChangeEvent< HTMLSelectElement > ) {
 		const selectedLanguage: string = event.target.value;
+		// if there's an alternate URL for the selected language, use that, else go home with the selected language
+		if ( languageOptions[ selectedLanguage ] ) {
+			window.location.href = languageOptions[ selectedLanguage ];
+		} else {
+			const localizedHomePage = localizeUrl( 'https://en.support.wordpress.com', selectedLanguage );
+			window.location.href = localizedHomePage;
+		}
+	}
 
-		window.location.href = languageOptions[ selectedLanguage ];
+	if ( availableLanguages.length > 1 ) {
+		props.languageOptions = languageOptions;
 	}
 
 	document.querySelectorAll( `.wp-block-happy-blocks-universal-footer` ).forEach( ( el ) => {
@@ -23,8 +37,8 @@ domReady( () => {
 		hydrate(
 			<PureUniversalNavbarFooter
 				onLanguageChange={ onLanguageChange }
-				languageOptions={ languageOptions }
 				locale={ locale }
+				{ ...props }
 			/>,
 			el
 		);
