4113d40634e97408b23ece33a4d371fdd3b2c1a7
Omar Alshaker <omar@omaralshaker.com>
2023-03-10 21:09:17 +0100

    Fix Help Center Icon Alignment

--- packages/help-center/src/components/help-icon.tsx
+++ packages/help-center/src/components/help-icon.tsx
@@ -25,7 +25,7 @@ const HelpIcon = forwardRef< SVGSVGElement >( ( _, ref ) => {
 					ref={ ref }
 					width="24"
 					height="24"
-					viewBox="0 -2 26 24"
+					viewBox="-2 -2 26 24"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
 				>
