6a1a5bb62a7eb9b2a9895e5a5635a7df228ad4cb
Anthony Grullon <agrullon95@gmail.com>
2023-03-12 14:12:09 -0400

    Add hook to use site launchpad endpoint

---
+++ client/data/sites/use-launchpad.ts
@@ -0,0 +1,25 @@
+import { useQuery } from 'react-query';
+import wpcom from 'calypso/lib/wp';
+
+export const fetchLaunchpad = ( siteSlug: string ) => {
+	const slug = encodeURIComponent( siteSlug as string );
+	return wpcom.req.get( {
+		path: `/sites/${ slug }/launchpad`,
+		apiNamespace: 'wpcom/v2',
+	} );
+};
+
+export const useLaunchpad = ( siteSlug: string, cache = true ) => {
+	const key = [ 'launchpad', siteSlug ];
+	return useQuery( key, () => fetchLaunchpad( siteSlug ), {
+		meta: {
+			persist: cache,
+		},
+		enabled: true,
+		initialData: {
+			checklist_statuses: {},
+			launchpad_screen: undefined,
+			site_intent: '',
+		},
+	} );
+};
