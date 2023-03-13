7c9c8c75899049f61154de7f74279d4cf36732a4
Payton Swick <payton@foolord.com>
2023-03-13 18:56:17 -0400

    Add detectGeolocation to LayoutLoggedOut

--- client/layout/logged-out.jsx
+++ client/layout/logged-out.jsx
@@ -1,9 +1,11 @@
 import config from '@automattic/calypso-config';
+import { detectGeolocation } from '@automattic/format-currency';
 import { useLocalizeUrl, removeLocaleFromPathLocaleInFront } from '@automattic/i18n-utils';
 import { UniversalNavbarHeader, UniversalNavbarFooter } from '@automattic/wpcom-template-parts';
 import classNames from 'classnames';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { connect, useSelector } from 'react-redux';
 import { CookieBannerContainerSSR } from 'calypso/blocks/cookie-banner';
 import AsyncLoad from 'calypso/components/async-load';
@@ -82,6 +84,10 @@ const LayoutLoggedOut = ( {
 		'is-wccom-oauth-flow': isWooOAuth2Client( oauth2Client ) && wccomFrom,
 		'is-p2-login': isP2Login,
 	};
+
+	useEffect( () => {
+		detectGeolocation();
+	}, [] );
 
 	let masterbar = null;
 
