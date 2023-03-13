5f8ce2be8401afb119bfdee8bf4ee66f034837bb
Payton Swick <payton@foolord.com>
2023-03-13 12:41:01 -0400

    Move vat ID stripping to own function

--- client/me/purchases/vat-info/use-vat-details.ts
+++ client/me/purchases/vat-info/use-vat-details.ts
@@ -36,6 +36,19 @@ async function setVatDetails( vatDetails: VatDetails ): Promise< VatDetails > {
 	} );
 }
 
+// Some countries prefix the VAT ID with the country code, but that's not
+// part of the ID as we need it formatted, so here we strip the country
+// code out if it is there.
+function stripCountryCodeFromVatId( id: string, country: string | undefined | null ): string {
+	const first2UppercasedChars = id.slice( 0, 2 ).toUpperCase();
+
+	if ( isNaN( Number( first2UppercasedChars ) ) && first2UppercasedChars === country ) {
+		return id.slice( 2 );
+	}
+
+	return id;
+}
+
 const emptyVatDetails = {};
 
 export default function useVatDetails(): VatDetailsManager {
@@ -50,11 +63,7 @@ export default function useVatDetails(): VatDetailsManager {
 		const { country, id } = data;
 
 		if ( !! id && id?.length > 1 ) {
-			const first2UppercasedChars = id.slice( 0, 2 ).toUpperCase();
-
-			if ( isNaN( Number( first2UppercasedChars ) ) && first2UppercasedChars === country ) {
-				return { ...data, id: id.slice( 2 ) };
-			}
+			return { ...data, id: stripCountryCodeFromVatId( id, country ) };
 		}
 
 		return data;
