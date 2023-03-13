80c842ff137b640ad358c9c4ab02b7c798e6705e
Payton Swick <payton@foolord.com>
2023-03-13 18:28:46 -0400

    Remove setCurrencySymbol from formatter tests

--- packages/format-currency/test/index.ts
+++ packages/format-currency/test/index.ts
@@ -120,18 +120,9 @@ describe( 'formatCurrency', () => {
 	} );
 
 	describe( 'specific currencies', () => {
-		beforeEach( () => {
-			formatter.setCurrencySymbol( 'USD', undefined );
-		} );
-
 		test( 'USD', () => {
 			const money = formatter.formatCurrency( 9800900.32, 'USD' );
 			expect( money ).toBe( '$9,800,900.32' );
-		} );
-		test( 'USD with the currency symbol overridden', () => {
-			formatter.setCurrencySymbol( 'USD', 'US$' );
-			const money = formatter.formatCurrency( 9800900.32, 'USD' );
-			expect( money ).toBe( 'US$9,800,900.32' );
 		} );
 		test( 'USD in Canadian English', () => {
 			const money = formatter.formatCurrency( 9800900.32, 'USD', { locale: 'en-CA' } );
@@ -291,19 +282,6 @@ describe( 'getCurrencyObject()', () => {
 			const money = formatter.getCurrencyObject( 9800900.32, 'USD' );
 			expect( money ).toEqual( {
 				symbol: '$',
-				symbolPosition: 'before',
-				integer: '9,800,900',
-				fraction: '.32',
-				sign: '',
-				hasNonZeroFraction: true,
-			} );
-		} );
-
-		test( 'USD with the currency symbol overridden', () => {
-			formatter.setCurrencySymbol( 'USD', 'US$' );
-			const money = formatter.getCurrencyObject( 9800900.32, 'USD' );
-			expect( money ).toEqual( {
-				symbol: 'US$',
 				symbolPosition: 'before',
 				integer: '9,800,900',
 				fraction: '.32',
