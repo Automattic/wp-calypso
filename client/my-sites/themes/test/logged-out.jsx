fc68d0ee3162385f05b897b4c5c8777d49e1eaa8
Markus Dobmann <m.do@posteo.net>
2023-03-13 13:15:48 +0100

    chore/calypso: migrate `react-query` imports (js,jsx)

--- client/my-sites/themes/test/logged-out.jsx
+++ client/my-sites/themes/test/logged-out.jsx
@@ -1,8 +1,8 @@
 /**
  * @jest-environment jsdom
  */
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen, waitFor } from '@testing-library/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider as ReduxProvider } from 'react-redux';
 import { createReduxStore } from 'calypso/state';
 import { setStore } from 'calypso/state/redux-store';
