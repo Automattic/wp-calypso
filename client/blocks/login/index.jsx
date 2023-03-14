9566e3179027cc4861aca6f8b6a7eedb8856f64d
arjunpotter17 <42664767+arjunpotter17@users.noreply.github.com>
2023-03-14 15:06:22 +0530

    Updated changes for WooCommerce account creation.
    Changed WooCommerce header and subHeader text for existing WP.com user, logged-in and logged-out

--- client/blocks/login/index.jsx
+++ client/blocks/login/index.jsx
@@ -334,13 +334,13 @@ class Login extends Component {
 						</p>
 					);
 				} else {
-					headerText = <h3>{ translate( 'Get started in minutes' ) }</h3>;
+					headerText = <h3>{ translate( "Let's get started" ) }</h3>;
 					postHeader = (
 						<p className="login__header-subtitle">
 							{ this.showContinueAsUser()
-								? translate( "First, select the account you'd like to use." )
+								? translate( "All Woo stores are powered by WordPress.com! Please, log in to continue. Don't have an account? {{a}}Sign up{{/a}}" )
 								: translate(
-										'First, log in with your WordPress.com account. Don’t have an account? {{signupLink}}Sign up{{/signupLink}}',
+										"All Woo stores are powered by WordPress.com! First, select the account you'd like to use",
 										{
 											components: {
 												signupLink: <a href={ this.getSignupUrl() } />,
