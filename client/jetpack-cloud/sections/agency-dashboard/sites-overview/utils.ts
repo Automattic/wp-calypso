8e3c0b3ed4e2e35f07ad85e215c182a296d899f8
Yashwin <yashwinpoojary@gmail.com>
2023-03-14 15:42:47 +0530

    Code refactor of util functions

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/utils.ts
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/utils.ts
@@ -506,35 +506,41 @@ export const getSiteCountText = ( sites: Array< Site > ) => {
 	} );
 };
 
-export const getBoostRating = ( boostScore: number ) => {
-	if ( boostScore > 90 ) {
-		return 'A';
-	}
-	if ( boostScore > 75 ) {
-		return 'B';
-	}
-	if ( boostScore > 50 ) {
-		return 'C';
-	}
-	if ( boostScore > 35 ) {
-		return 'D';
-	}
-	if ( boostScore > 25 ) {
-		return 'E';
+type BoostRating = 'A' | 'B' | 'C' | 'D' | 'E' | 'F';
+
+interface BoostThreshold {
+	threshold: number;
+	rating: BoostRating;
+}
+
+const BOOST_THRESHOLDS: BoostThreshold[] = [
+	{ threshold: 90, rating: 'A' },
+	{ threshold: 75, rating: 'B' },
+	{ threshold: 50, rating: 'C' },
+	{ threshold: 35, rating: 'D' },
+	{ threshold: 25, rating: 'E' },
+	{ threshold: 0, rating: 'F' },
+];
+
+export const getBoostRating = ( boostScore: number ): BoostRating => {
+	for ( const { threshold, rating } of BOOST_THRESHOLDS ) {
+		if ( boostScore > threshold ) {
+			return rating;
+		}
 	}
 	return 'F';
 };
 
-export const getBoostRatingClass = ( boostScore: number ) => {
-	if ( boostScore > 70 ) {
-		return 'boost-score-good';
-	}
+const GOOD_BOOST_SCORE_THRESHOLD = 75;
+const OKAY_BOOST_SCORE_THRESHOLD = 35;
 
-	if ( boostScore > 50 ) {
-		return 'boost-score-okay';
-	}
-
-	if ( boostScore ) {
-		return 'boost-score-bad';
+export const getBoostRatingClass = ( boostScore: number ): string => {
+	switch ( true ) {
+		case boostScore > GOOD_BOOST_SCORE_THRESHOLD:
+			return 'boost-score-good';
+		case boostScore > OKAY_BOOST_SCORE_THRESHOLD:
+			return 'boost-score-okay';
+		default:
+			return 'boost-score-bad';
 	}
 };
