e42cf1871373c5f1de455017c43ff50919966be3
thestevek <steve.kryworuka@automattic.com>
2023-03-14 12:20:59 -0400

    add unit test for addition to calypso-products

--- packages/calypso-products/test/get-all-products-for-plan.ts
+++ packages/calypso-products/test/get-all-products-for-plan.ts
@@ -6,28 +6,28 @@ describe( 'getAllProductsForPlan', () => {
 		expect( result ).toEqual( [] );
 	} );
 
-	test( 'returns an array of products included in the plan', () => {
-		const plan = {
-			id: 'basic-plan',
-			name: 'Basic Plan',
-			getProductsIncluded: () => [
-				{ id: 'product-1', name: 'Product 1' },
-				{ id: 'product-2', name: 'Product 2' },
-			],
-		};
-		const result = getAllProductsForPlan( plan );
-		expect( result ).toEqual( [
-			{ id: 'product-1', name: 'Product 1' },
-			{ id: 'product-2', name: 'Product 2' },
-		] );
-	} );
-
-	test( 'returns an empty array when the plan has no products included', () => {
-		const plan = {
-			id: 'free-plan',
-			name: 'Free Plan',
-		};
+	test( 'returns an empty array for Jetpack free', () => {
+		const plan = 'jetpack_free';
 		const result = getAllProductsForPlan( plan );
 		expect( result ).toEqual( [] );
+	} );
+	test( 'Checks for products that should exist in "jetpack_complete" plan', () => {
+		const plan = 'jetpack_complete';
+		const result = getAllProductsForPlan( plan );
+		expect( result ).toContain( `jetpack_backup_t2_yearly` );
+		expect( result ).toContain( `jetpack_scan` );
+		expect( result ).toContain( `jetpack_anti_spam` );
+		expect( result ).toContain( `jetpack_videopress` );
+		expect( result ).toContain( `jetpack_boost_yearly` );
+		expect( result ).toContain( `jetpack_social_advanced_yearly` );
+		expect( result ).toContain( `jetpack_search` );
+		expect( result ).toContain( `jetpack_crm` );
+	} );
+	test( 'checks for products included in "jetpack_security plan', () => {
+		const plan = 'jetpack_security_t1_yearly';
+		const result = getAllProductsForPlan( plan );
+		expect( result ).toContain( `jetpack_backup_t1_yearly` );
+		expect( result ).toContain( `jetpack_scan` );
+		expect( result ).toContain( `jetpack_anti_spam` );
 	} );
 } );
