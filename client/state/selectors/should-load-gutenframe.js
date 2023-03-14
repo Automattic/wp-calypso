f97b56a270fb8edb9a473d237c8bd19bd4268c7b
Matt Wiebe <wiebe@automattic.com>
2023-03-14 16:24:26 -0500

    More elegant uniframing

--- client/state/selectors/should-load-gutenframe.js
+++ client/state/selectors/should-load-gutenframe.js
@@ -1,8 +1,15 @@
+import { isEnabled } from '@automattic/calypso-config';
 import { isEligibleForGutenframe } from 'calypso/state/gutenberg-iframe-eligible/is-eligible-for-gutenframe';
 import { getPreferredEditorView } from 'calypso/state/selectors/get-preferred-editor-view';
 
-export const shouldLoadGutenframe = ( state, siteId, postType = 'post' ) =>
-	isEligibleForGutenframe( state, siteId ) &&
-	getPreferredEditorView( state, siteId, postType ) === 'default';
+export const shouldLoadGutenframe = ( state, siteId, postType = 'post' ) => {
+	if ( isEnabled( 'block-editor/non-iframed' ) ) {
+		return false;
+	}
+	return (
+		isEligibleForGutenframe( state, siteId ) &&
+		getPreferredEditorView( state, siteId, postType ) === 'default'
+	);
+};
 
 export default shouldLoadGutenframe;
