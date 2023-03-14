00d9a279202e9dad438f91eed3de976fe6a17999
Paulo Trentin <paulo@paulotrentin.com.br>
2023-03-14 15:45:45 -0300

    Created component test to check expire message rendering

---
+++ client/my-sites/checkout/checkout-thank-you/marketplace/test/marketplace-thank-you-plugin-section.jsx
@@ -0,0 +1,87 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { ThankYouPluginSection } from '../marketplace-thank-you-plugin-section';
+
+const sites = [];
+sites[ 1 ] = {
+	ID: 1,
+	URL: 'example.wordpress.com',
+};
+
+const initialState = {
+	purchases: {
+		hasLoadedUserPurchasesFromServer: true,
+	},
+	sites: {
+		items: sites,
+		domains: {
+			items: [ 'example.wordpress.com' ],
+		},
+	},
+	ui: {
+		selectedSiteId: 1,
+	},
+	currentUser: {
+		id: 12,
+		user: {
+			email_verified: true,
+		},
+	},
+};
+
+describe( 'index', () => {
+	test( "Plugin without a purchase, DOESN'T expire", async () => {
+		const mockStore = configureStore();
+		const store = mockStore( initialState );
+		const plugin = {
+			variations: [
+				{
+					yearly: {
+						product_id: 123,
+					},
+				},
+			],
+		};
+
+		render(
+			<Provider store={ store }>
+				<ThankYouPluginSection plugin={ plugin } />
+			</Provider>
+		);
+
+		expect( screen.getByText( "This plugin doesn't expire" ) ).toBeInTheDocument();
+	} );
+
+	test( 'Plugin with a purchase, MUST expire', async () => {
+		const mockStore = configureStore();
+		initialState.purchases.data = [
+			{
+				user_id: 12,
+				expiry_date: '2021-01-01T00:00:00+00:00',
+				product_id: 123,
+				blog_id: 1,
+			},
+		];
+		const store = mockStore( initialState );
+		const plugin = {
+			variations: {
+				yearly: {
+					product_id: 123,
+				},
+			},
+		};
+
+		render(
+			<Provider store={ store }>
+				<ThankYouPluginSection plugin={ plugin } />
+			</Provider>
+		);
+
+		expect( screen.getByText( 'Expires on January 1, 2021' ) ).toBeInTheDocument();
+	} );
+} );
