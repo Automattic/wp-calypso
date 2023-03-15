003bbb0e6f44a5b9d86d6f14fdeba826fb95bb96
Javier Olmo <43957544+jjolmo@users.noreply.github.com>
2023-03-15 12:07:16 +0100

    Drive blazepress top menu via widget (#74429)

--- client/components/blazepress-widget/index.tsx
+++ client/components/blazepress-widget/index.tsx
@@ -57,7 +57,9 @@ const BlazePressWidget = ( props: BlazePressPromotionProps ) => {
 	}, [ isVisible ] );
 
 	const handleShowCancel = ( show: boolean ) => setShowCancelButton( show );
-	const handleGetStartedMessageClose = () => setHiddenHeader( false );
+	const handleShowTopBar = ( show: boolean ) => {
+		setHiddenHeader( ! show );
+	};
 
 	const onClose = ( goToCampaigns?: boolean ) => {
 		queryClient.invalidateQueries( [ 'promote-post-campaigns', siteId ] );
@@ -99,7 +101,7 @@ const BlazePressWidget = ( props: BlazePressPromotionProps ) => {
 					},
 					widgetContainer.current,
 					handleShowCancel,
-					handleGetStartedMessageClose,
+					handleShowTopBar,
 					localeSlug
 				);
 				setIsLoading( false );
