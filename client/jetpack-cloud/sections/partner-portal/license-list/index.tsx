170e0ee7ab2e0157a85e88132f0941c2b82d0670
Rich Collier <rcollier@Richs-Mac-Studio.local>
2023-03-15 14:13:32 -0400

    remove console.log statement

--- client/jetpack-cloud/sections/partner-portal/license-list/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/license-list/index.tsx
@@ -49,8 +49,6 @@ export default function LicenseList() {
 	const showPagination = showLicenses && licenses.totalPages > 1;
 	const showNoResults = hasFetched && ! isFetching && licenses && licenses.items.length === 0;
 
-	console.log( licenses );
-
 	const onPageClick = useCallback(
 		( pageNumber: number ) => {
 			setPage( pageNumber );
