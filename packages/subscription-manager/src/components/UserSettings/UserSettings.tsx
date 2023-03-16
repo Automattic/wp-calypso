c613d29ce84c6fbd38221884af1b9000a01de5c1
Maciej Grabowski <littlesoldiergraba@gmail.com>
2023-03-16 13:31:26 +0100

    Apply PR suggestions.

--- packages/subscription-manager/src/components/UserSettings/UserSettings.tsx
+++ packages/subscription-manager/src/components/UserSettings/UserSettings.tsx
@@ -1,18 +1,18 @@
 type SubscriptionUserSettings = Partial< {
-	subscription_delivery_mail_option: 'html' | 'text';
-	subscription_delivery_day: number; // 0-6, 0 is Sunday
-	subscription_delivery_hour: number; // 0-23, 0 is midnight
-	subscription_delivery_email_blocked: boolean;
+	mail_option: 'html' | 'text';
+	delivery_day: number; // 0-6, 0 is Sunday
+	delivery_hour: number; // 0-23, 0 is midnight
+	blocked: boolean;
 } >;
 
 type UserSettingsProps = {
-	value?: SubscriptionUserSettings;
+	value: SubscriptionUserSettings;
 	onChange?: ( value: SubscriptionUserSettings ) => void;
-	loading?: boolean;
+	loading: boolean;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars -- until we start using any of these props
-const UserSettings = ( props: UserSettingsProps ) => (
+const UserSettings = ( { value = {}, loading = false }: UserSettingsProps ) => (
 	<div className="user-settings">User settings will go here</div>
 );
 
