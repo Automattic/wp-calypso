39edbc70e1f7acf5db370775ef62a872580346a3
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2023-03-17 12:27:46 +0100

    Add temporary redirect to unfinished tabs in Subscription Management

--- packages/subscription-manager/src/components/TabsSwitcher/TabsSwitcher.tsx
+++ packages/subscription-manager/src/components/TabsSwitcher/TabsSwitcher.tsx
@@ -20,7 +20,16 @@ type TabsSwitcherProps = {
 	tabs: TabItem[];
 };
 
-const getRoute = ( baseRoute: string, path: string ): string => `/${ baseRoute }/${ path }`;
+const getRoute = ( baseRoute: string, path: string ): string => {
+	// This is a temporary redirect to the re-skinned Subscription Management portal
+	// Once the new Commnets and Sites views are ready, this will be removed
+	const temporaryRedirect = [ 'comments', 'sites' ];
+	if ( temporaryRedirect.includes( path ) ) {
+		return `https://wordpress.com/email-subscriptions/?option=${ path }`;
+	}
+
+	return `/${ baseRoute }/${ path }`;
+};
 const getPath = ( route: string ) => route.substring( route.lastIndexOf( '/' ) + 1 );
 
 const TabsSwitcher = ( { baseRoute, tabs }: TabsSwitcherProps ) => {
