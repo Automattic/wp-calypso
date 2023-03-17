47ddc58655a914036e35e923537cc9de9ef5b096
Tim Broddin <tim@wannabes.be>
2023-03-17 16:12:42 +0100

    Fix type errors

--- packages/data-stores/src/reader/hooks/index.ts
+++ packages/data-stores/src/reader/hooks/index.ts
@@ -1,10 +1,14 @@
 import { useSelect } from '@wordpress/data';
 import { register as registerUserStore } from '../../user';
+import type { UserSelect } from '../../user';
 
 const USER_STORE = registerUserStore( { client_id: '', client_secret: '' } );
 
 export const useIsLoggedIn = () => {
-	return useSelect( ( select ) => select( USER_STORE ).isCurrentUserLoggedIn() ) as boolean;
+	return useSelect(
+		( select ) => ( select( USER_STORE ) as UserSelect ).isCurrentUserLoggedIn(),
+		[]
+	) as boolean;
 };
 
 export const useIsQueryEnabled = () => {
