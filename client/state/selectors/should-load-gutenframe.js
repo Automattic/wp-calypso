1c37a8992898193c3a6225149a2f4973fa47284e
Matt Wiebe <wiebe@automattic.com>
2023-03-17 13:41:56 -0500

    Never load gutenframe to make tests go boom?

--- client/state/selectors/should-load-gutenframe.js
+++ client/state/selectors/should-load-gutenframe.js
@@ -1,15 +1,5 @@
-import { isEnabled } from '@automattic/calypso-config';
-import { isEligibleForGutenframe } from 'calypso/state/gutenberg-iframe-eligible/is-eligible-for-gutenframe';
-import { getPreferredEditorView } from 'calypso/state/selectors/get-preferred-editor-view';
-
-export const shouldLoadGutenframe = ( state, siteId, postType = 'post' ) => {
-	if ( isEnabled( 'block-editor/non-iframed' ) ) {
-		return false;
-	}
-	return (
-		isEligibleForGutenframe( state, siteId ) &&
-		getPreferredEditorView( state, siteId, postType ) === 'default'
-	);
+export const shouldLoadGutenframe = () => {
+	return false;
 };
 
 export default shouldLoadGutenframe;
