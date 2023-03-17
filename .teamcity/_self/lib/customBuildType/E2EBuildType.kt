6a72a90bf9ed74ac0cc96ec4dc84ac72113eecc8
Edwin Takahashi <egao@outlook.com>
2023-03-17 12:41:03 -0700

    .teamcity/_self/lib/customBuildType/E2EBuildType.kt - increase shm to 6GB

--- .teamcity/_self/lib/customBuildType/E2EBuildType.kt
+++ .teamcity/_self/lib/customBuildType/E2EBuildType.kt
@@ -141,7 +141,7 @@ open class E2EBuildType(
 					xvfb-run yarn jest --reporters=jest-teamcity --reporters=default --maxWorkers=%JEST_E2E_WORKERS% --group=$testGroup
 				"""
 				dockerImage = "%docker_image_e2e%"
-				dockerRunParameters = "-u %env.UID% --shm-size=4g"
+				dockerRunParameters = "-u %env.UID% --shm-size=6g"
 			}
 
 			bashNodeScript {
