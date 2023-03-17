bd9f1f6c7d355c942f96a2ac57b2edf7fa6c4091
Candy Tsai <candy02058912@gmail.com>
2023-03-17 18:35:08 +0800

    Add truncation to domain for mobile

--- client/my-sites/customer-home/cards/tasks/domain-upsell/index.jsx
+++ client/my-sites/customer-home/cards/tasks/domain-upsell/index.jsx
@@ -183,7 +183,10 @@ export function RenderDomainUpsell( { isFreePlan, isMonthlyPlan, searchTerm, sit
 					d="M15 11h-.2V9c0-1.5-1.2-2.8-2.8-2.8S9.2 7.5 9.2 9v2H9c-.6 0-1 .4-1 1v4c0 .6.4 1 1 1h6c.6 0 1-.4 1-1v-4c0-.6-.4-1-1-1zm-1.8 0h-2.5V9c0-.7.6-1.2 1.2-1.2s1.2.6 1.2 1.2v2z"
 				/>
 				<text x="20" y="15">
-					{ domainSuggestionName }
+					{ /* max 24 chars in the domain name */ }
+					{ domainSuggestionName.length > 24
+						? `${ domainSuggestionName.slice( 0, 21 ) }...`
+						: domainSuggestionName }
 				</text>
 			</g>
 		</svg>
