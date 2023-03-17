7acae44af8e313f61fd29fa1fd31ca1a731a848a
DustyReagan <dusty@dustyreagan.com>
2023-03-17 14:21:06 -0500

    Remove auto submit

--- client/login/magic-login/handle-emailed-link-form.jsx
+++ client/login/magic-login/handle-emailed-link-form.jsx
@@ -74,20 +74,8 @@ class HandleEmailedLinkForm extends Component {
 		}
 	}
 
-	componentDidMount() {
-		if (
-			this.props.clientId === config( 'wpcom_signup_id' ) &&
-			! this.props.isImmediateLoginAttempt &&
-			! wooDnaConfig( this.props.initialQuery ).isWooDnaFlow()
-		) {
-			this.handleSubmit();
-		}
-	}
-
 	handleSubmit = ( event ) => {
-		if ( event ) {
-			event.preventDefault();
-		}
+		event.preventDefault();
 
 		this.setState( {
 			hasSubmitted: true,
