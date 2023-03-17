961151f6e5d627aee8a3ed7fc63d53e4cd1bc394
Tim Broddin <tim@wannabes.be>
2023-03-17 20:26:29 +0100

    Subscription management: add data fetch queries and conditional logic (#74522)
    * Add conditional data fetching logic & useSubscriptionManagerUserSettingsQuery
    
    * Add email field
    
    ---------
    
    Co-authored-by: Maciej Grabowski <mashikag@users.noreply.github.com>
    Co-authored-by: Maciej Grabowski <littlesoldiergraba@gmail.com>

---
+++ packages/data-stores/src/reader/types/index.ts
@@ -0,0 +1,9 @@
+export type EmailFormatType = 'html' | 'text';
+
+export type SubscriptionManagerUserSettings = Partial< {
+	mail_option: EmailFormatType;
+	delivery_day: number; // 0-6, 0 is Sunday
+	delivery_hour: number; // 0-23, 0 is midnight
+	blocked: boolean;
+	email: string;
+} >;
--- packages/subscription-manager/src/components/UserSettings/UserSettings.tsx
+++ packages/subscription-manager/src/components/UserSettings/UserSettings.tsx
@@ -7,6 +7,7 @@ type SubscriptionUserSettings = Partial< {
 	delivery_day: number; // 0-6, 0 is Sunday
 	delivery_hour: number; // 0-23, 0 is midnight
 	blocked: boolean;
+	email: string;
 } >;
 
 type UserSettingsProps = {
