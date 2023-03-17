1e32032712359387dd7d369b27673aaf52108de7
Noah Allen <noahtallen@gmail.com>
2023-03-17 14:21:01 -0700

    Add back Plan store I accidentally removed

--- client/landing/stepper/stores.ts
+++ client/landing/stepper/stores.ts
@@ -1,5 +1,5 @@
 import config from '@automattic/calypso-config';
-import { Onboard, Site, User, StepperInternal } from '@automattic/data-stores';
+import { Onboard, Site, User, StepperInternal, Plans } from '@automattic/data-stores';
 
 export const ONBOARD_STORE = Onboard.register();
 export const STEPPER_INTERNAL_STORE = StepperInternal.register();
@@ -13,3 +13,5 @@ export const USER_STORE = User.register( {
 	client_id: config( 'wpcom_signup_id' ),
 	client_secret: config( 'wpcom_signup_key' ),
 } );
+
+export const PLANS_STORE = Plans.register();
