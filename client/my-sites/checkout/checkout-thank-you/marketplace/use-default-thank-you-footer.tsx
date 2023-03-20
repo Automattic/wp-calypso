0f04e9826bd791256321ee5ff36c9220f5e5c3b9
Wesley Berrêdo <wberredo@gmail.com>
2023-03-20 15:32:10 -0300

    Split the logic for footer next steps

--- client/my-sites/checkout/checkout-thank-you/marketplace/use-default-thank-you-footer.tsx
+++ client/my-sites/checkout/checkout-thank-you/marketplace/use-default-thank-you-footer.tsx
@@ -3,13 +3,54 @@ import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import { ThankYouSectionProps } from 'calypso/components/thank-you/types';
+import { ThankYouSectionProps, ThankYouNextStepProps } from 'calypso/components/thank-you/types';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
 export function useDefaultThankYouFoooter( slugs: string[] ): ThankYouSectionProps {
 	const translate = useTranslate();
-	const siteId = useSelector( getSelectedSiteId );
 	const siteSlug = useSelector( getSelectedSiteSlug );
+
+	const footerSteps: FooterStep[] = [
+		{
+			key: 'thank_you_footer_support_guides',
+			title: translate( 'Support guides' ),
+			description: translate(
+				'Our guides will show you everything you need to know about plugins.'
+			),
+			link: 'https://wordpress.com/support/plugins/',
+			linkText: translate( 'Plugin Support' ),
+			eventKey: 'calypso_plugin_thank_you_plugin_support_click',
+		},
+		{
+			key: 'thank_you_footer_explore',
+			title: translate( 'Keep growing' ),
+			description: translate(
+				'Take your site to the next level. We have all the solutions to help you.'
+			),
+			link: `/plugins/${ siteSlug }`,
+			linkText: translate( 'Explore plugins' ),
+			eventKey: 'calypso_plugin_thank_you_explore_plugins_click',
+		},
+		{
+			key: 'thank_you_footer_support',
+			title: translate( 'How can we support you?' ),
+			description: translate( 'Our team is here if you need help, or if you have any questions.' ),
+			link: 'https://wordpress.com/help/contact',
+			linkText: translate( 'Ask a question' ),
+			eventKey: 'calypso_plugin_thank_you_ask_question_click',
+		},
+	];
+	const steps = useNextSteps( slugs, footerSteps );
+
+	return {
+		sectionKey: 'thank_you_footer',
+		nextStepsClassName: 'thank-you__footer',
+		nextSteps: steps,
+	};
+}
+
+function useNextSteps( slugs: string[], footerSteps: FooterStep[] ): ThankYouNextStepProps[] {
+	const siteId = useSelector( getSelectedSiteId );
 
 	const sendTrackEvent = useCallback(
 		( name: string ) => {
@@ -21,61 +62,30 @@ export function useDefaultThankYouFoooter( slugs: string[] ): ThankYouSectionPro
 		[ siteId, slugs ]
 	);
 
-	return {
-		sectionKey: 'thank_you_footer',
-		nextStepsClassName: 'thank-you__footer',
-		nextSteps: [
-			{
-				stepKey: 'thank_you_footer_support_guides',
-				stepTitle: translate( 'Support guides' ),
-				stepDescription: translate(
-					'Our guides will show you everything you need to know about plugins.'
-				),
-				stepCta: (
-					<Button
-						isLink
-						href="https://wordpress.com/support/plugins/"
-						target="_blank"
-						onClick={ () => sendTrackEvent( 'calypso_plugin_thank_you_plugin_support_click' ) }
-					>
-						{ translate( 'Plugin Support' ) }
-					</Button>
-				),
-			},
-			{
-				stepKey: 'thank_you_footer_explore',
-				stepTitle: translate( 'Keep growing' ),
-				stepDescription: translate(
-					'Take your site to the next level. We have all the solutions to help you.'
-				),
-				stepCta: (
-					<Button
-						isLink
-						href={ `/plugins/${ siteSlug }` }
-						target="_blank"
-						onClick={ () => sendTrackEvent( 'calypso_plugin_thank_you_explore_plugins_click' ) }
-					>
-						{ translate( 'Explore plugins' ) }
-					</Button>
-				),
-			},
-			{
-				stepKey: 'thank_you_footer_support',
-				stepTitle: translate( 'How can we support you?' ),
-				stepDescription: translate(
-					'Our team is here if you need help, or if you have any questions.'
-				),
-				stepCta: (
-					<Button
-						isLink
-						href="https://wordpress.com/help/contact"
-						target="_blank"
-						onClick={ () => sendTrackEvent( 'calypso_plugin_thank_you_ask_question_click' ) }
-					>
-						{ translate( 'Ask a question' ) }
-					</Button>
-				),
-			},
-		],
-	};
+	return footerSteps.map( ( step ) => {
+		return {
+			stepKey: step.key,
+			stepTitle: step.title,
+			stepDescription: step.description,
+			stepCta: (
+				<Button
+					isLink
+					href={ step.link }
+					target="_blank"
+					onClick={ () => sendTrackEvent( step.eventKey ) }
+				>
+					{ step.linkText }
+				</Button>
+			),
+		};
+	} );
 }
+
+type FooterStep = {
+	key: string;
+	title: string;
+	description: string;
+	link: string;
+	linkText: string;
+	eventKey: string;
+};
