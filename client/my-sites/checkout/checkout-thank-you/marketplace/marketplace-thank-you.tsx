327d785187a5d655d6cf4684bc3a5f25bfa45660
Wesley Berrêdo <wberredo@gmail.com>
2023-03-20 08:58:18 -0300

    Readd memoization

--- client/my-sites/checkout/checkout-thank-you/marketplace/marketplace-thank-you.tsx
+++ client/my-sites/checkout/checkout-thank-you/marketplace/marketplace-thank-you.tsx
@@ -37,7 +37,7 @@ const MarketplaceThankYou = ( {
 	const currentUser = useSelector( getCurrentUser );
 	const isRequestingPlugins = useSelector( ( state ) => isRequesting( state, siteId ) );
 
-	const allSlugs = [ ...pluginSlugs, ...themeSlugs ];
+	const allSlugs = useMemo( () => [ ...pluginSlugs, ...themeSlugs ], [ pluginSlugs, themeSlugs ] );
 	const defaultThankYouFooter = useDefaultThankYouFoooter( allSlugs );
 
 	const [ pluginsSection, allPluginsFetched ] = usePluginsThankYouData( pluginSlugs );
