bcb4642e7f285370c5902dd869f4e10a295144ff
Philip Jackson <p-jackson@live.com>
2023-03-22 11:07:37 +1300

    Logging: Add standard section heading to /site-logs (#74700)
    * Add standard section heading to /site-logs
    
    * Mock documentHead state in unit tests
    
    This allows the <DocumentHead> component to render correctly.

--- client/my-sites/site-logs/main.tsx
+++ client/my-sites/site-logs/main.tsx
@@ -1,10 +1,15 @@
+import { useI18n } from '@wordpress/react-i18n';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import DocumentHead from 'calypso/components/data/document-head';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import Main from 'calypso/components/main';
 import { useSiteLogsQuery } from 'calypso/data/hosting/use-site-logs-query';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 export function SiteLogs() {
+	const { __ } = useI18n();
 	const siteId = useSelector( getSelectedSiteId );
 	const moment = useLocalizedMoment();
 
@@ -36,10 +41,19 @@ export function SiteLogs() {
 		)
 		.join( '\n' );
 
+	const titleHeader = __( 'Site Logs' );
+
 	return (
-		<>
+		<Main wideLayout>
+			<DocumentHead title={ titleHeader } />
+			<FormattedHeader
+				brandFont
+				headerText={ titleHeader }
+				subHeaderText={ __( 'View server logs to troubleshoot or debug problems with your site.' ) }
+				align="left"
+			/>
 			<h2>Web logs</h2>
 			<pre>{ formattedLogs }</pre>
-		</>
+		</Main>
 	);
 }
