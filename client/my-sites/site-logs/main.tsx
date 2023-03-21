7c71211e4715408f04dbf92cbb9b8f91f62e4723
Philip Jackson <p-jackson@live.com>
2023-03-22 10:25:48 +1300

    Logging: `/site-logs` displays the 10 latest logs on mount (#74678)
    * /site-logs displays the 10 latest logs on mount
    
    This PR is far from completing the feature, but it serves as a proof of
    concept and an initial commit that other PRs can base their work off.
    
    * Latest logs appear at the top of view
    
    * Test that view fetchs 7 days worth of logs on mount
    
    * Test shouldn't break if we change which log type is the default

--- client/my-sites/site-logs/main.tsx
+++ client/my-sites/site-logs/main.tsx
@@ -1,12 +1,45 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import { useSiteLogsQuery } from 'calypso/data/hosting/use-site-logs-query';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 export function SiteLogs() {
 	const siteId = useSelector( getSelectedSiteId );
+	const moment = useLocalizedMoment();
+
+	const [ startTime ] = useState( moment().subtract( 7, 'd' ).unix() );
+	const [ endTime ] = useState( moment().unix() );
+
+	const { data } = useSiteLogsQuery( siteId, {
+		logType: 'web',
+		start: startTime,
+		end: endTime,
+		sort_order: 'desc',
+		page_size: 10,
+	} );
+
+	const formattedLogs = data?.logs
+		.map( ( log ) =>
+			Object.entries( log )
+				.sort( ( [ keyA ], [ keyB ] ) => {
+					if ( keyA === 'date' ) {
+						return -1;
+					}
+					if ( keyB === 'date' ) {
+						return 1;
+					}
+					return keyA.localeCompare( keyB );
+				} )
+				.map( ( [ key, value ] ) => `"${ key }" = ${ value }` )
+				.join( '\t' )
+		)
+		.join( '\n' );
 
 	return (
 		<>
-			<h2>Site logs for { siteId }</h2>
+			<h2>Web logs</h2>
+			<pre>{ formattedLogs }</pre>
 		</>
 	);
 }
