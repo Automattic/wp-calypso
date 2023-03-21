13bb094e9592783c252bd9a2af894eadf6eb226a
Jeremy Yip <jeremy.yip@automattic.com>
2023-03-21 16:17:44 -0700

    Show different heading and body text based on context

--- client/my-sites/customer-home/components/celebrate-launch-modal.jsx
+++ client/my-sites/customer-home/components/celebrate-launch-modal.jsx
@@ -1,13 +1,22 @@
-import { ConfettiAnimation } from '@automattic/components';
+import { ConfettiAnimation, Spinner } from '@automattic/components';
 import { Button, Modal } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import { useEffect } from 'react';
+import { useGetDomainsQuery } from 'calypso/data/domains/use-get-domains-query';
 import { omitUrlParams } from 'calypso/lib/url';
+import { createSiteDomainObject } from 'calypso/state/sites/domains/assembler';
 
 import './celebrate-launch-modal.scss';
 
 function CelebrateLaunchModal( { setModalIsOpen, site } ) {
 	const translate = useTranslate();
+	const isPaidPlan = ! site?.plan?.is_free;
+	const { data: allDomains = [], isFetching } = useGetDomainsQuery( site?.ID ?? null, {
+		retry: false,
+	} );
+
+	const domains = allDomains.map( createSiteDomainObject );
+	const hasCustomDomain = Boolean( domains.find( ( domain ) => ! domain.isWPCOMDomain ) );
 
 	useEffect( () => {
 		// remove the celebrateLaunch URL param without reloading the page as soon as the modal loads
@@ -18,6 +27,17 @@ function CelebrateLaunchModal( { setModalIsOpen, site } ) {
 			omitUrlParams( window.location.href, 'celebrateLaunch' )
 		);
 	}, [] );
+
+	let upsellHeading = translate( 'Free One-Year Domain' );
+	let upsellBody = translate( 'Get a custom domain by upgrading your plan.' );
+
+	if ( isPaidPlan && hasCustomDomain ) {
+		upsellHeading = 'Upsell something else';
+		upsellBody = 'Placeholder to upsell something else';
+	} else if ( isPaidPlan && ! hasCustomDomain ) {
+		upsellHeading = 'Claim your free domain';
+		upsellBody = 'Your plan includes a free domain for the first year';
+	}
 
 	return (
 		<Modal onRequestClose={ () => setModalIsOpen( false ) } className="launched__modal">
@@ -42,17 +62,21 @@ function CelebrateLaunchModal( { setModalIsOpen, site } ) {
 				</div>
 			</div>
 			<div className="launched__modal-upsell">
-				<div className="launched__modal-upsell-content">
-					<h2 className="launched__modal-upsell-heading">
-						{ translate( 'Claim your free domain' ) }
-					</h2>
-					<p className="launched__modal-upsell-body">
-						{ translate( 'Your plan includes a free domain for the first year' ) }
-					</p>
-				</div>
-				<Button isPrimary href={ `/domains/add/${ site.slug }` }>
-					<span>{ translate( 'Choose a domain' ) }</span>
-				</Button>
+				{ isFetching ? (
+					<Spinner />
+				) : (
+					<>
+						<div className="launched__modal-upsell-content">
+							{ upsellHeading && (
+								<h2 className="launched__modal-upsell-heading">{ upsellHeading }</h2>
+							) }
+							<p className="launched__modal-upsell-body">{ upsellBody }</p>
+						</div>
+						<Button isPrimary href={ `/domains/add/${ site.slug }` }>
+							<span>{ translate( 'Choose a domain' ) }</span>
+						</Button>
+					</>
+				) }
 			</div>
 		</Modal>
 	);
