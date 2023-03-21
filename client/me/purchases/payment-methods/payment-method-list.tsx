4d1901bbf6d2b5a8cfe3ef1db90222fdae804a35
Payton Swick <payton@foolord.com>
2023-03-21 19:45:01 -0400

    Use withStoredPaymentMethods in PaymentMethodList

--- client/me/purchases/payment-methods/payment-method-list.tsx
+++ client/me/purchases/payment-methods/payment-method-list.tsx
@@ -3,34 +3,26 @@ import { CheckoutProvider } from '@automattic/composite-checkout';
 import { localize, translate } from 'i18n-calypso';
 import page from 'page';
 import { Component } from 'react';
-import { connect } from 'react-redux';
-import QueryStoredCards from 'calypso/components/data/query-stored-cards';
 import SectionHeader from 'calypso/components/section-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import PaymentMethod from 'calypso/me/purchases/payment-methods/payment-method';
-import {
-	getAllStoredCards,
-	getUniquePaymentAgreements,
-	hasLoadedStoredCardsFromServer,
-	isFetchingStoredCards,
-} from 'calypso/state/stored-cards/selectors';
-import type { PaymentMethod as PaymentMethodType } from 'calypso/lib/checkout/payment-methods';
-import type { StoredCard } from 'calypso/my-sites/checkout/composite-checkout/types/stored-cards';
-import type { IAppState } from 'calypso/state/types';
+import { withStoredPaymentMethods } from 'calypso/my-sites/checkout/composite-checkout/hooks/use-stored-payment-methods';
+import type { StoredPaymentMethod } from 'calypso/lib/checkout/payment-methods';
+import type { WithStoredPaymentMethodsProps } from 'calypso/my-sites/checkout/composite-checkout/hooks/use-stored-payment-methods';
 
 import 'calypso/me/purchases/payment-methods/style.scss';
 
 interface PaymentMethodListProps {
 	addPaymentMethodUrl: string;
-	cards: StoredCard[];
-	paymentAgreements: PaymentMethodType[];
 	hasLoadedFromServer?: boolean;
 	isFetching?: boolean;
 	translate: typeof translate;
 }
 
-class PaymentMethodList extends Component< PaymentMethodListProps > {
-	renderPaymentMethods( paymentMethods: PaymentMethodType[] ) {
+class PaymentMethodList extends Component<
+	PaymentMethodListProps & WithStoredPaymentMethodsProps
+> {
+	renderPaymentMethods( paymentMethods: StoredPaymentMethod[] ) {
 		if ( this.props.isFetching && ! this.props.hasLoadedFromServer ) {
 			return (
 				<CompactCard className="payment-method-list__loader">
@@ -76,14 +68,10 @@ class PaymentMethodList extends Component< PaymentMethodListProps > {
 	}
 
 	render() {
-		let paymentMethods = this.props.cards;
-		if ( this.props.hasLoadedFromServer && this.props.paymentAgreements.length > 0 ) {
-			paymentMethods = paymentMethods.concat( this.props.paymentAgreements );
-		}
+		const paymentMethods = this.props.paymentMethodsState.paymentMethods;
 
 		return (
 			<div className="payment-method-list">
-				<QueryStoredCards />
 				<SectionHeader label={ this.props.translate( 'Manage Your Payment Methods' ) }>
 					{ this.renderAddPaymentMethodButton() }
 				</SectionHeader>
@@ -94,9 +82,7 @@ class PaymentMethodList extends Component< PaymentMethodListProps > {
 	}
 }
 
-export default connect( ( state: IAppState ) => ( {
-	cards: getAllStoredCards( state ),
-	paymentAgreements: getUniquePaymentAgreements( state ),
-	hasLoadedFromServer: hasLoadedStoredCardsFromServer( state ),
-	isFetching: isFetchingStoredCards( state ),
-} ) )( localize( PaymentMethodList ) );
+export default withStoredPaymentMethods( localize( PaymentMethodList ), {
+	type: 'all',
+	expired: true,
+} );
