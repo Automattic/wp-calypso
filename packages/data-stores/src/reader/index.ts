c7d51f6e451fce6aefc034e2e1fded9374bb17be
Tim Broddin <tim@wannabes.be>
2023-03-21 16:58:28 +0100

    Create client-side data interface for updating subscription settings. (#74718)
    * - Replace `fetchFromApi` with `callApi` because it does more than fetching
    - Add `useSubscriptionManagerUserSettingsMutation`
    
    * Better typings
    
    * Better typings

--- packages/data-stores/src/reader/index.ts
+++ packages/data-stores/src/reader/index.ts
@@ -1,4 +1,5 @@
 export { useSubscriptionManagerUserSettingsQuery } from './queries/use-subscription-manager-user-settings-query';
 export { useSubscriptionManagerSubscriptionsCountQuery } from './queries/use-subscription-manager-subscriptions-count-query';
+export { useSubscriptionManagerUserSettingsMutation } from './mutations/use-subscription-manager-user-settings-mutation';
 
 export type { SubscriptionManagerUserSettings, EmailFormatType } from './types';
