ff13ad513b650d16b8189b1677d815cd2731f0a4
Wojtek Naruniec <wojtek@naruniec.me>
2023-03-21 12:18:24 +0100

    Change explicit length check to type conversion

--- client/state/sites/selectors/has-jetpack-active-plugins.ts
+++ client/state/sites/selectors/has-jetpack-active-plugins.ts
@@ -4,5 +4,5 @@ import type { SiteDetails } from '@automattic/data-stores';
  * Check if site has active Jetpack plugins.
  */
 export default function hasJetpackActivePlugins( site: SiteDetails | null ) {
-	return site?.options?.jetpack_connection_active_plugins?.length > 0;
+	return !! site?.options?.jetpack_connection_active_plugins?.length;
 }
