abd1cf27d0eb571c596cb1d403ec9ec554d51d29
Renan <renansscarvalho@gmail.com>
2023-03-21 11:44:21 +0100

    Fix wrong flow name prop

--- client/landing/stepper/declarative-flow/internals/steps-repository/domains/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/domains/index.tsx
@@ -226,7 +226,7 @@ const DomainsStep: Step = function DomainsStep( { navigation, flow } ) {
 			hideBack={ ! isCopySiteFlow( flow ) }
 			backLabelText={ __( 'Back to sites' ) }
 			hideSkip={ true }
-			flowName={ isCopySiteFlow( flow ) ? ( flow as string ) : 'linkInBio' }
+			flowName={ isCopySiteFlow( flow ) ? ( flow as string ) : flow }
 			stepContent={ <div className="domains__content">{ renderContent() }</div> }
 			recordTracksEvent={ recordTracksEvent }
 			goBack={ () => exitFlow?.( '/sites' ) }
