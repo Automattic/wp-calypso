e5254fb5b46e5d5fa83ce7d6193e724dea18390d
Anna McPhee <anna.mcphee@automattic.com>
2023-03-22 16:31:21 -0600

    add sample selectors data basd on chartTabs

---
+++ client/state/stats/subscribers/selectors.js
@@ -0,0 +1,32 @@
+import { get } from 'lodash';
+import { QUERY_FIELDS } from 'calypso/state/stats/subscribers/constants';
+
+import 'calypso/state/stats/init';
+
+const EMPTY_RESULT = [];
+
+/**
+ * Returns the count records for a given site and period
+ *
+ * @param   {Object}  state    Global state tree
+ * @param   {number}  siteId   Site ID
+ * @param   {string}  period   Type of duration to include in the query (such as daily)
+ * @returns {Array}            Array of count objects
+ */
+export function getCountRecords( state, siteId, period ) {
+	return get( state, [ 'stats', 'subscribers', 'counts', siteId, period ], EMPTY_RESULT );
+}
+
+/**
+ * Returns an array of strings denoting the query fields that are still loading
+ *
+ * @param   {Object}  state    Global state tree
+ * @param   {number}  siteId   Site ID
+ * @param   {string}  period   Type of duration to include in the query (such as daily)
+ * @returns {Array}          	 Array of stat types as strings
+ */
+export function getLoadingTabs( state, siteId, period ) {
+	return QUERY_FIELDS.filter( ( type ) =>
+		get( state, [ 'stats', 'subscribers', 'isLoading', siteId, period, type ] )
+	);
+}
