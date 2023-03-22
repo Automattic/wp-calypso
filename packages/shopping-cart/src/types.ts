7324bf997c8f425de7b2b4bcab2c69dbda020bbe
Gergely Csécsey <gergely.csecsey@automattic.com>
2023-03-22 11:33:57 +0000

    update types, add comments

--- packages/shopping-cart/src/types.ts
+++ packages/shopping-cart/src/types.ts
@@ -562,10 +562,15 @@ export interface ResponseCartProductExtra {
 	afterPurchaseUrl?: string;
 	isJetpackCheckout?: boolean;
 
-	// Marketplace properties
+	/**
+	 * Marketplace properties
+	 *
+	 * These extra properties are always set for marketplace products.
+	 * `product_slug` is for identifying the product, and `product_type` is for identifying the type of the product.
+	 */
 	is_marketplace_product?: boolean;
 	product_slug?: string;
-	product_type?: string;
+	product_type?: 'marketplace_plugin' | 'marketplace_theme';
 }
 
 export interface ResponseCartGiftDetails {
