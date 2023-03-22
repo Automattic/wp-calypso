a4d3582b79403f02277392d90b1dac305c849a59
Roberto Aranda <roberto.aranda@automattic.com>
2023-03-22 10:33:58 +0100

    Rename variable to increase clarity

--- client/my-sites/checkout/checkout-thank-you/marketplace/marketplace-thank-you-theme-section.tsx
+++ client/my-sites/checkout/checkout-thank-you/marketplace/marketplace-thank-you-theme-section.tsx
@@ -88,8 +88,8 @@ export const ThankYouThemeSection = ( { theme }: { theme: any } ) => {
 	const dispatch = useDispatch();
 	const siteId = useSelector( getSelectedSiteId ) as number;
 	const isActive = useSelector( ( state ) => isThemeActive( state, theme.id, siteId ) );
-	const { data, isLoading } = useActiveThemeQuery( siteId, true );
-	const isFSEActive = data?.[ 0 ]?.theme_supports[ 'block-templates' ] ?? false;
+	const { data: activeThemeData, isLoading } = useActiveThemeQuery( siteId, true );
+	const isFSEActive = activeThemeData?.[ 0 ]?.theme_supports[ 'block-templates' ] ?? false;
 	const hasActivated = useSelector( ( state ) => hasActivatedTheme( state, siteId ) );
 	const isActivating = useSelector( ( state ) => isActivatingTheme( state, siteId ) );
 	const customizeUrl = useSelector( ( state ) =>
