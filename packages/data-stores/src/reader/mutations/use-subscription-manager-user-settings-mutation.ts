8f82845bdcc16c16c2ce9d3952e738d11126e787
Tim Broddin <tim@wannabes.be>
2023-03-22 17:42:02 +0100

    Initial work on the mutation

--- packages/data-stores/src/reader/mutations/use-subscription-manager-user-settings-mutation.ts
+++ packages/data-stores/src/reader/mutations/use-subscription-manager-user-settings-mutation.ts
@@ -1,11 +1,11 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import { callApi } from '../helpers';
 import { useIsLoggedIn } from '../hooks';
 import type { SubscriptionManagerUserSettings, EmailSettingsAPIResponse } from '../types';
 
 const useSubscriptionManagerUserSettingsMutation = () => {
 	const isLoggedIn = useIsLoggedIn();
-
+	const queryClient = useQueryClient();
 	return useMutation< SubscriptionManagerUserSettings, Error, SubscriptionManagerUserSettings >(
 		async ( data: SubscriptionManagerUserSettings ) => {
 			const { settings } = await callApi< EmailSettingsAPIResponse >( {
@@ -15,6 +15,11 @@ const useSubscriptionManagerUserSettingsMutation = () => {
 				isLoggedIn,
 			} );
 			return settings;
+		},
+		{
+			onSuccess: () => {
+				queryClient.invalidateQueries( [ 'read', 'email-settings', isLoggedIn ] );
+			},
 		}
 	);
 };
--- packages/subscription-manager/src/components/UserSettings/UserSettings.tsx
+++ packages/subscription-manager/src/components/UserSettings/UserSettings.tsx
@@ -1,3 +1,7 @@
+import { Reader } from '@automattic/data-stores';
+import { Button } from '@wordpress/components';
+import { useState, useCallback, useEffect } from '@wordpress/element';
+import { translate } from 'i18n-calypso';
 import { FormEvent } from 'react';
 import { BlockEmailsSetting } from '../fields/BlockEmailsSetting';
 import { DeliveryWindowInput } from '../fields/DeliveryWindowInput';
@@ -17,36 +21,61 @@ type SubscriptionUserSettings = Partial< {
 
 type UserSettingsProps = {
 	value?: SubscriptionUserSettings;
-	onChange?: ( value: SubscriptionUserSettings ) => void;
-	loading?: boolean;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars -- until we start using any of these props
-const UserSettings = ( { value = {}, loading = false, onChange }: UserSettingsProps ) => (
-	<div className="user-settings">
-		<EmailFormatInput
-			value={ value.mail_option ?? 'html' }
-			onChange={ ( evt: FormEvent< HTMLSelectElement > ) =>
-				onChange?.( { mail_option: evt.currentTarget.value as EmailFormatType } )
-			}
-		/>
-		<DeliveryWindowInput
-			dayValue={ value.delivery_day ?? 0 }
-			hourValue={ value.delivery_hour ?? 0 }
-			onDayChange={ ( evt: FormEvent< HTMLSelectElement > ) =>
-				onChange?.( { delivery_day: parseInt( evt.currentTarget.value ) as DeliveryWindowDayType } )
-			}
-			onHourChange={ ( evt: FormEvent< HTMLSelectElement > ) =>
-				onChange?.( {
-					delivery_hour: parseInt( evt.currentTarget.value ) as DeliveryWindowHourType,
-				} )
-			}
-		/>
-		<BlockEmailsSetting
-			value={ value.blocked ?? false }
-			onChange={ ( value ) => onChange?.( { blocked: !! value.target.value } ) }
-		/>
-	</div>
-);
+const UserSettings = ( { value = {} }: UserSettingsProps ) => {
+	const [ formState, setFormState ] = useState< SubscriptionUserSettings >( value );
+	const { mutate, isLoading } = Reader.useSubscriptionManagerUserSettingsMutation();
+
+	useEffect( () => {
+		// check if formState is empty object
+		if ( value ) {
+			setFormState( value );
+		}
+	}, [ value ] );
+
+	const onChange = ( newState: Partial< SubscriptionUserSettings > ) => {
+		setFormState( ( prevState ) => ( { ...prevState, ...newState } ) );
+	};
+
+	const onSubmit = useCallback( () => {
+		mutate( formState );
+	}, [ formState ] );
+
+	return (
+		<div className="user-settings">
+			<EmailFormatInput
+				value={ formState.mail_option ?? 'html' }
+				onChange={ ( evt: FormEvent< HTMLSelectElement > ) =>
+					onChange?.( { mail_option: evt.currentTarget.value as EmailFormatType } )
+				}
+			/>
+			<DeliveryWindowInput
+				dayValue={ formState.delivery_day ?? 0 }
+				hourValue={ formState.delivery_hour ?? 0 }
+				onDayChange={ ( evt: FormEvent< HTMLSelectElement > ) =>
+					onChange?.( {
+						delivery_day: parseInt( evt.currentTarget.value ) as DeliveryWindowDayType,
+					} )
+				}
+				onHourChange={ ( evt: FormEvent< HTMLSelectElement > ) =>
+					onChange?.( {
+						delivery_hour: parseInt( evt.currentTarget.value ) as DeliveryWindowHourType,
+					} )
+				}
+			/>
+			<BlockEmailsSetting
+				value={ formState.blocked ?? false }
+				onChange={ ( value ) => onChange?.( { blocked: !! value.target.value } ) }
+			/>
+			<Button disabled={ isLoading } onClick={ onSubmit } isPrimary>
+				{ translate( 'Save changes', {
+					context: 'Save the subscription management user changes',
+				} ) }
+			</Button>
+		</div>
+	);
+};
 
 export default UserSettings;
