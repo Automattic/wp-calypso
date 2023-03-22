fbf7c87794351da54b7a598d3f24c08fb9ac538f
Payton Swick <payton@foolord.com>
2023-03-22 17:05:00 -0400

    Add tests for PayPal

--- client/me/purchases/payment-methods/test/payment-method.tsx
+++ client/me/purchases/payment-methods/test/payment-method.tsx
@@ -9,17 +9,20 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider as ReduxProvider } from 'react-redux';
 import { createReduxStore } from 'calypso/state';
 import PaymentMethodList from '../payment-method-list';
-import type { StoredPaymentMethodCard } from '../../../../lib/checkout/payment-methods';
+import type {
+	StoredPaymentMethodCard,
+	StoredPaymentMethodPayPal,
+} from '../../../../lib/checkout/payment-methods';
 
 const card: StoredPaymentMethodCard = {
 	stored_details_id: '1234',
 	user_id: '5432',
-	name: 'Human Person',
+	name: 'Card Person',
 	country_code: 'US',
 	payment_partner: 'stripe',
 	payment_partner_reference: '',
 	payment_partner_source_id: '',
-	mp_ref: 'mock-mp-ref',
+	mp_ref: 'mock-mp-ref-1',
 	email: '',
 	card_expiry_year: '80',
 	card_expiry_month: '01',
@@ -38,25 +41,54 @@ const card: StoredPaymentMethodCard = {
 	card_zip: '',
 };
 
+const payPalAgreement: StoredPaymentMethodPayPal = {
+	stored_details_id: '1235',
+	user_id: '5432',
+	name: 'PayPal Person',
+	country_code: 'US',
+	payment_partner: 'paypal_express',
+	payment_partner_reference: '',
+	payment_partner_source_id: '',
+	mp_ref: 'mock-mp-ref-2',
+	email: '',
+	card_expiry_year: '',
+	card_expiry_month: '',
+	expiry: '0000-00-00',
+	remember: true,
+	source: '',
+	original_stored_details_id: '',
+	is_rechargable: true,
+	payment_type: '',
+	is_expired: false,
+	is_backup: false,
+	tax_location: null,
+};
+
 describe( 'PaymentMethod', () => {
-	let currentCards = [];
+	const currentData = { cards: [] };
 	beforeEach( () => {
 		nock.cleanAll();
-		currentCards = [ card ];
+		currentData.cards = [ card, payPalAgreement ];
 		nock( 'https://public-api.wordpress.com' )
 			.persist()
 			.get( new RegExp( '^/rest/v1.2/me/payment-methods' ) )
-			.reply( 200, () => currentCards );
+			.reply( 200, () => currentData.cards );
 		nock( 'https://public-api.wordpress.com' )
-			.post( `/rest/v1.1/me/stored-cards/${ card.stored_details_id }/delete` )
-			.reply( 200, () => {
-				currentCards = [];
+			.post( new RegExp( '/rest/v1.1/me/stored-cards/\\d+/delete' ) )
+			.reply( 200, ( uri ) => {
+				const matches = uri.match( new RegExp( '/stored-cards/(\\d+)/delete' ) );
+				const id = matches[ 1 ];
+				currentData.cards = currentData.cards.filter(
+					( oneCard ) => oneCard.stored_details_id !== id
+				);
 			} );
 		nock( 'https://public-api.wordpress.com' )
-			.get( `/rest/v1.1/me/payment-methods/${ card.stored_details_id }/is-backup` )
+			.persist()
+			.get( new RegExp( '^/rest/v1.1/me/payment-methods/\\d+/is-backup' ) )
 			.reply( 200 );
 		nock( 'https://public-api.wordpress.com' )
-			.get( `/rest/v1.1/me/payment-methods/${ card.stored_details_id }/tax-location` )
+			.persist()
+			.get( new RegExp( '^/rest/v1.1/me/payment-methods/\\d+/tax-location' ) )
 			.reply( 200 );
 	} );
 
@@ -71,6 +103,7 @@ describe( 'PaymentMethod', () => {
 			</ReduxProvider>
 		);
 		expect( await screen.findByText( `Mastercard ****${ card.card_last_4 }` ) ).toBeInTheDocument();
+		expect( await screen.findByText( payPalAgreement.name ) ).toBeInTheDocument();
 	} );
 
 	it( 'removes the card when deletion completes', async () => {
@@ -84,10 +117,35 @@ describe( 'PaymentMethod', () => {
 				</QueryClientProvider>
 			</ReduxProvider>
 		);
-		await user.click( await screen.findByText( 'Delete this payment method' ) );
+		await user.click(
+			await screen.findByLabelText( `Delete the "${ card.card_last_4 }" payment method` )
+		);
 		// Click confimation.
 		await user.click( await screen.findByText( 'Delete' ) );
 		await waitForElementToBeRemoved( () => screen.queryByText( card.name ) );
 		expect( screen.queryByText( `Mastercard ****${ card.card_last_4 }` ) ).not.toBeInTheDocument();
+		expect( await screen.findByText( payPalAgreement.name ) ).toBeInTheDocument();
+	} );
+
+	it( 'removes the PayPal agreement when deletion completes', async () => {
+		const user = userEvent.setup();
+		const store = createReduxStore();
+		const queryClient = new QueryClient();
+		render(
+			<ReduxProvider store={ store }>
+				<QueryClientProvider client={ queryClient }>
+					<PaymentMethodList addPaymentMethodUrl="" />
+				</QueryClientProvider>
+			</ReduxProvider>
+		);
+		expect( await screen.findByText( `Mastercard ****${ card.card_last_4 }` ) ).toBeInTheDocument();
+		await user.click(
+			await screen.findByLabelText( `Delete the "${ payPalAgreement.name }" payment method` )
+		);
+		// Click confimation.
+		await user.click( await screen.findByText( 'Delete' ) );
+		await waitForElementToBeRemoved( () => screen.queryByText( payPalAgreement.name ) );
+		expect( screen.queryByText( payPalAgreement.name ) ).not.toBeInTheDocument();
+		expect( await screen.findByText( `Mastercard ****${ card.card_last_4 }` ) ).toBeInTheDocument();
 	} );
 } );
