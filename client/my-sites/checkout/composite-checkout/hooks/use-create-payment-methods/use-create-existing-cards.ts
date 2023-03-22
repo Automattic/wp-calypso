ae05db80aff6a506e24bc3e926e8026441a73a42
Payton Swick <payton@foolord.com>
2023-03-22 16:09:37 -0400

    Filter non-cards out in useCreateExistingCards

--- client/my-sites/checkout/composite-checkout/hooks/use-create-payment-methods/use-create-existing-cards.ts
+++ client/my-sites/checkout/composite-checkout/hooks/use-create-payment-methods/use-create-existing-cards.ts
@@ -3,7 +3,10 @@ import { useMemoCompare } from 'calypso/lib/use-memo-compare';
 import { createExistingCardMethod } from 'calypso/my-sites/checkout/composite-checkout/payment-methods/existing-credit-card';
 import type { StripeLoadingError } from '@automattic/calypso-stripe';
 import type { PaymentMethod } from '@automattic/composite-checkout';
-import type { StoredPaymentMethod } from 'calypso/lib/checkout/payment-methods';
+import type {
+	StoredPaymentMethod,
+	StoredPaymentMethodCard,
+} from 'calypso/lib/checkout/payment-methods';
 
 export default function useCreateExistingCards( {
 	isStripeLoading,
@@ -24,23 +27,22 @@ export default function useCreateExistingCards( {
 	// card processor does require it (for 3DS cards), so we wait to create the
 	// payment methods until stripe is loaded.
 	const shouldLoad = ! isStripeLoading && ! stripeLoadingError;
+
+	const onlyStoredCards = storedCards.filter( isPaymentMethodACard );
+
 	// Memoize the cards by comparing their stored_details_id values, in case the
 	// objects themselves are recreated on each render.
-	const memoizedStoredCards: StoredPaymentMethod[] = useMemoCompare(
-		storedCards,
-		( prev, next ) => {
-			const prevIds = prev?.map( ( card ) => card.stored_details_id ) ?? [];
-			const nextIds = next?.map( ( card ) => card.stored_details_id ) ?? [];
-			return (
-				prevIds.length === nextIds.length &&
-				prevIds.every( ( id, index ) => id === nextIds[ index ] )
-			);
-		}
-	);
+	const memoizedStoredCards = useMemoCompare( onlyStoredCards, ( prev, next ) => {
+		const prevIds = prev.map( ( card ) => card.stored_details_id ) ?? [];
+		const nextIds = next.map( ( card ) => card.stored_details_id ) ?? [];
+		return (
+			prevIds.length === nextIds.length && prevIds.every( ( id, index ) => id === nextIds[ index ] )
+		);
+	} );
 
 	const existingCardMethods = useMemo( () => {
 		return (
-			memoizedStoredCards?.map( ( storedDetails ) =>
+			memoizedStoredCards.map( ( storedDetails ) =>
 				createExistingCardMethod( {
 					id: `existingCard-${ storedDetails.stored_details_id }`,
 					cardholderName: storedDetails.name,
@@ -59,4 +61,8 @@ export default function useCreateExistingCards( {
 	}, [ memoizedStoredCards, activePayButtonText, allowEditingTaxInfo, isTaxInfoRequired ] );
 
 	return shouldLoad ? existingCardMethods : [];
+}
+
+function isPaymentMethodACard( card: StoredPaymentMethod ): card is StoredPaymentMethodCard {
+	return 'card_last_4' in card;
 }
