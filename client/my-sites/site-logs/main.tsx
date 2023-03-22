a5208501e89fbc3cb4ba79449ec62add956c19f1
vykes-mac <knrymcleish@gmail.com>
2023-03-22 16:50:13 -0500

    render tabs and update logs based on selected tab.

--- client/my-sites/site-logs/main.tsx
+++ client/my-sites/site-logs/main.tsx
@@ -1,5 +1,5 @@
 import { useI18n } from '@wordpress/react-i18n';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
@@ -7,6 +7,7 @@ import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import Main from 'calypso/components/main';
 import { useSiteLogsQuery } from 'calypso/data/hosting/use-site-logs-query';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { SiteLogsTabPanel, tabs } from './components/site-logs-tab-panel';
 
 export function SiteLogs() {
 	const { __ } = useI18n();
@@ -16,13 +17,23 @@ export function SiteLogs() {
 	const [ startTime ] = useState( moment().subtract( 7, 'd' ).unix() );
 	const [ endTime ] = useState( moment().unix() );
 
-	const { data } = useSiteLogsQuery( siteId, {
-		logType: 'web',
+	const [ logType, setLogType ] = useState< 'php' | 'web' >( 'php' );
+
+	const { data, refetch } = useSiteLogsQuery( siteId, {
+		logType,
 		start: startTime,
 		end: endTime,
 		sort_order: 'desc',
 		page_size: 10,
 	} );
+
+	const handleTabSelected = useCallback(
+		( tabName ) => {
+			setLogType( tabName );
+			refetch();
+		},
+		[ refetch ]
+	);
 
 	const formattedLogs = data?.logs
 		.map( ( log ) =>
@@ -42,6 +53,8 @@ export function SiteLogs() {
 		.join( '\n' );
 
 	const titleHeader = __( 'Site Logs' );
+	const tabName = new URL( window.location.href ).searchParams.get( 'logType' ) ?? 'php';
+	const tab = tabs.find( ( tab ) => tab.name === tabName );
 
 	return (
 		<Main wideLayout>
@@ -52,8 +65,20 @@ export function SiteLogs() {
 				subHeaderText={ __( 'View server logs to troubleshoot or debug problems with your site.' ) }
 				align="left"
 			/>
-			<h2>Web logs</h2>
-			<pre>{ formattedLogs }</pre>
+
+			<SiteLogsTabPanel
+				initialTab={ tab }
+				onSelected={ ( tabName ) => {
+					handleTabSelected( tabName );
+				} }
+			>
+				{ ( tab ) => (
+					<>
+						<h2 style={ { marginTop: '24px' } }>{ tab.title }</h2>
+						<pre>{ formattedLogs }</pre>
+					</>
+				) }
+			</SiteLogsTabPanel>
 		</Main>
 	);
 }
