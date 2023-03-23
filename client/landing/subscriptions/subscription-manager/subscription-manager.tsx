af107974fe925c06232490aca009914b76934a3f
Maciej Grabowski <littlesoldiergraba@gmail.com>
2023-03-23 17:10:34 +0100

    change casing on file name

--- client/landing/subscriptions/subscription-manager/subscription-manager.tsx
+++ client/landing/subscriptions/subscription-manager/subscription-manager.tsx
@@ -1,7 +1,7 @@
 import { Reader } from '@automattic/data-stores';
 import SubscriptionManager from '@automattic/subscription-manager';
 import { useTranslate } from 'i18n-calypso';
-import Settings from '../tab-views/Settings';
+import Settings from '../tab-views/settings';
 
 const SitesView = () => <span>Sites View</span>;
 const CommentsView = () => <span>Comments View</span>;
