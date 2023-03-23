aa78b27cc3e51004d02739689e52528c9a818e01
Antony Agrios <antonyagrios@gmail.com>
2023-03-23 16:58:04 +0200

    Update: Remove site name from the staging notice card

--- client/my-sites/customer-home/cards/notices/staging-site/index.js
+++ client/my-sites/customer-home/cards/notices/staging-site/index.js
@@ -1,61 +1,21 @@
-import { Button } from '@wordpress/components';
-import { createInterpolateElement } from '@wordpress/element';
-import { __, sprintf } from '@wordpress/i18n';
-import { connect } from 'react-redux';
+import { __ } from '@wordpress/i18n';
 import stagingIllustration from 'calypso/assets/images/customer-home/illustration--staging.svg';
 import { NOTICE_STAGING_SITE } from 'calypso/my-sites/customer-home/cards/constants';
-import CelebrateNotice, {
-	CelebrateNoticePlaceholder,
-} from 'calypso/my-sites/customer-home/cards/notices/celebrate-notice';
-import { getSiteHomeUrl, getSiteTitle } from 'calypso/state/sites/selectors';
-import { getSelectedSite } from 'calypso/state/ui/selectors';
+import CelebrateNotice from 'calypso/my-sites/customer-home/cards/notices/celebrate-notice';
 
-const StagingSiteNotice = ( { productionSiteTitle, productionSiteHomeUrl } ) => {
+const StagingSiteNotice = () => {
 	return (
-		<>
-			{ productionSiteTitle && productionSiteHomeUrl ? (
-				<CelebrateNotice
-					description={ createInterpolateElement(
-						sprintf(
-							/* translators: %s - The WordPress.com production site. (ex.: wordpress.com/home/subdomain.wordpress.com) */
-							__(
-								'This is a staging site for <a>%s</a> that you can use to try out new plugins and themes, debug and troubleshoot changes, and fine-tune every aspect of your website without worrying about breaking your live site.'
-							),
-							productionSiteTitle
-						),
-						{
-							a: (
-								<Button
-									style={ { fontSize: 'inherit' } }
-									variant="link"
-									href={ productionSiteHomeUrl }
-								/>
-							),
-						}
-					) }
-					noticeId={ NOTICE_STAGING_SITE }
-					title={ __( 'Staging site' ) }
-					illustration={ stagingIllustration }
-					showAction={ false }
-					showSkip={ false }
-				/>
-			) : (
-				<CelebrateNoticePlaceholder />
+		<CelebrateNotice
+			description={ __(
+				'This is a staging site that you can use to try out new plugins and themes, debug and troubleshoot changes, and fine-tune every aspect of your website without worrying about breaking your live site.'
 			) }
-		</>
+			noticeId={ NOTICE_STAGING_SITE }
+			title={ __( 'Staging site' ) }
+			illustration={ stagingIllustration }
+			showAction={ false }
+			showSkip={ false }
+		/>
 	);
 };
 
-export default connect( ( state ) => {
-	const site = getSelectedSite( state );
-	let productionSiteHomeUrl = '';
-	let productionSiteTitle = '';
-	if ( site.options?.wpcom_production_blog_id ) {
-		productionSiteHomeUrl = getSiteHomeUrl( state, site.options?.wpcom_production_blog_id );
-		productionSiteTitle = getSiteTitle( state, site.options?.wpcom_production_blog_id );
-	}
-	return {
-		productionSiteTitle,
-		productionSiteHomeUrl,
-	};
-} )( StagingSiteNotice );
+export default StagingSiteNotice;
