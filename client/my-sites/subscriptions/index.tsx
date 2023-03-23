0302c6a62cd1ae75174cb710496031fe7f8bb74c
Tim Broddin <tim@wannabes.be>
2023-03-23 09:16:54 +0100

    Loading state

--- client/my-sites/subscriptions/index.tsx
+++ client/my-sites/subscriptions/index.tsx
@@ -17,14 +17,10 @@ const SubscriptionManagementPage = () => {
 	const locale = useLocale();
 
 	const SettingsView = () => {
-		const {
-			data: settings,
-			isLoading,
-			isFetched,
-		} = Reader.useSubscriptionManagerUserSettingsQuery();
-		return (
-			<SubscriptionManager.UserSettings loading={ ! isLoading && ! isFetched } value={ settings } />
-		);
+		const query = Reader.useSubscriptionManagerUserSettingsQuery();
+		const { data: settings, isIdle, isLoading } = query;
+
+		return <SubscriptionManager.UserSettings loading={ isLoading || isIdle } value={ settings } />;
 	};
 
 	return (
