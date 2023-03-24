d536d4aa362442cd2003b30af5c9422bb3b4373e
Griffith Chen <griffith.chen@automattic.com>
2023-03-24 10:48:51 +0800

    Improve Theme Showcase filters UI responsiveness

--- client/my-sites/themes/themes-toolbar-group/index.tsx
+++ client/my-sites/themes/themes-toolbar-group/index.tsx
@@ -28,6 +28,7 @@ const ThemesToolbarGroup: React.FC< ThemesToolbarGroupProps > = ( {
 			className="themes-toolbar-group"
 			initialActiveIndex={ activeIndex }
 			forceSwipe={ 'undefined' === typeof window }
+			swipeBreakpoint="<960px"
 			onClick={ ( index: number ) => onSelect( items[ index ]?.key ) }
 		>
 			{ items.map( ( item ) => (
