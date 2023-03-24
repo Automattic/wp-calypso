5d7ff6b403e1da7701c3d650c1f5f124fbdf6e9b
Payton Swick <payton@foolord.com>
2023-03-23 23:24:04 -0400

    Remove extends from withCartKey type

--- client/my-sites/checkout/with-cart-key.tsx
+++ client/my-sites/checkout/with-cart-key.tsx
@@ -4,9 +4,7 @@ import type { ComponentType } from 'react';
 
 export type WithCartKeyProps = { cartKey: CartKey };
 
-export default function withCartKey< P extends WithCartKeyProps = WithCartKeyProps >(
-	Component: ComponentType< P >
-) {
+export default function withCartKey< P >( Component: ComponentType< P > ) {
 	return function CartKeyWrapper( props: Omit< P, keyof WithCartKeyProps > ) {
 		const cartKey = useCartKey();
 		return <Component { ...( props as P ) } cartKey={ cartKey } />;
