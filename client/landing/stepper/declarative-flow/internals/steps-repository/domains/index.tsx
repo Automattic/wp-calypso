dc1be9633845f66ed22435a04fdd2b3409a3f4ff
Anthony Grullon <agrullon95@gmail.com>
2023-03-24 10:53:30 -0400

    Add back button to domains + plan selection screens

--- client/landing/stepper/declarative-flow/internals/steps-repository/domains/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/domains/index.tsx
@@ -14,6 +14,8 @@ import { useI18n } from '@wordpress/react-i18n';
 import { useState } from 'react';
 import { useDispatch as useReduxDispatch } from 'react-redux';
 import FormattedHeader from 'calypso/components/formatted-header';
+import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
+import { useSiteSlug } from 'calypso/landing/stepper/hooks/use-site-slug';
 import {
 	domainRegistration,
 	domainMapping,
@@ -43,6 +45,8 @@ const DomainsStep: Step = function DomainsStep( { navigation, flow } ) {
 	const [ showUseYourDomain, setShowUseYourDomain ] = useState( false );
 
 	const dispatch = useReduxDispatch();
+	const flowToReturnTo = useQuery().get( 'flowToReturnTo' );
+	const siteSlug = useSiteSlug();
 
 	const { submit, exitFlow } = navigation;
 
@@ -242,18 +246,22 @@ const DomainsStep: Step = function DomainsStep( { navigation, flow } ) {
 		if ( showUseYourDomain ) {
 			return setShowUseYourDomain( false );
 		}
+
+		if ( flow === DOMAIN_UPSELL_FLOW ) {
+			return exitFlow?.( `/setup/${ flowToReturnTo }/launchpad?siteSlug=${ siteSlug }` );
+		}
 		return exitFlow?.( '/sites' );
 	};
 
 	const getBackLabelText = () => {
-		if ( showUseYourDomain ) {
+		if ( flow === DOMAIN_UPSELL_FLOW ) {
 			return __( 'Back' );
 		}
 		return __( 'Back to sites' );
 	};
 
 	const shouldHideBackButton = () => {
-		if ( showUseYourDomain ) {
+		if ( flow === DOMAIN_UPSELL_FLOW ) {
 			return false;
 		}
 		return ! isCopySiteFlow( flow );
