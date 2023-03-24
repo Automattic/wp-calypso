6819b4db165913d61e68a5cd0c6414febe37b74d
Miguel <miguel.san.segundo@automattic.com>
2023-03-24 14:09:57 +0700

    Use same copy as in editor

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/notices/notices.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/notices/notices.tsx
@@ -16,15 +16,14 @@ const Notices = ( {
 
 export const getNoticeContent = ( action: string, pattern: Pattern ) => {
 	const actions: { [ key: string ]: any } = {
-		add: i18n.translate( 'Pattern "%(categoryName)s" added.', {
-			comment: 'A pattern is added to the list using the pattern category as name',
-			args: { categoryName: pattern.category?.label },
+		add: i18n.translate( 'Block pattern "%(patternName)s" inserted.', {
+			args: { patternName: pattern.name },
 		} ),
-		replace: i18n.translate( 'Pattern "%(categoryName)s" replaced.', {
-			args: { categoryName: pattern.category?.label },
+		replace: i18n.translate( 'Block pattern "%(patternName)s" replaced.', {
+			args: { patternName: pattern.name },
 		} ),
-		remove: i18n.translate( 'Pattern "%(categoryName)s" removed.', {
-			args: { categoryName: pattern.category?.label },
+		remove: i18n.translate( 'Block pattern "%(patternName)s" removed.', {
+			args: { patternName: pattern.name },
 		} ),
 	};
 
