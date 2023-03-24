b632be6385bbdf6c08b87d8e652d3143db551b32
Philip Jackson <p-jackson@live.com>
2023-03-24 17:36:13 +1300

    Logging: Move Site Logs styles to SCSS (#74867)
    * whitepace
    
    * Convert SiteLogsTabPanel styles to SCSS

--- client/my-sites/site-logs/components/site-logs-tab-panel/index.tsx
+++ client/my-sites/site-logs/components/site-logs-tab-panel/index.tsx
@@ -1,7 +1,8 @@
-import styled from '@emotion/styled';
 import { TabPanel } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import classnames from 'classnames';
 import page from 'page';
+import './style.scss';
 
 export const tabs = [
 	{ name: 'php', title: __( 'PHP Logs' ) },
@@ -17,27 +18,6 @@ interface SiteLogsTabPanelProps {
 	onSelected?: ( tabName: SiteLogsTab ) => void;
 }
 
-const LogsTabPanel = styled( TabPanel )`
-	.components-tab-panel__tabs {
-		overflow-x: auto;
-	}
-	.components-tab-panel__tabs-item {
-		--wp-admin-theme-color: var( --studio-gray-100 );
-		color: var( --studio-gray-60 );
-		padding: 0;
-		margin-right: 24px;
-		font-size: 16px;
-		flex-shrink: 0;
-	}
-	.components-tab-panel__tabs-item:hover,
-	.components-tab-panel__tabs-item.is-active,
-	.components-tab-panel__tabs-item.is-active:focus,
-	.components-tab-panel__tabs-item:focus:not( :disabled ) {
-		box-shadow: inset 0 -2px 0 0 var( --wp-admin-theme-color );
-		color: var( --studio-gray-100 );
-	}
-`;
-
 export const SiteLogsTabPanel = ( {
 	children: renderContents,
 	selectedTab = 'php',
@@ -45,9 +25,9 @@ export const SiteLogsTabPanel = ( {
 	onSelected,
 }: SiteLogsTabPanelProps ) => {
 	return (
-		<LogsTabPanel
+		<TabPanel
 			initialTabName={ selectedTab }
-			className={ className }
+			className={ classnames( 'site-logs-tab-panel', className ) }
 			tabs={ tabs }
 			onSelect={ ( tabName ) => {
 				onTabSelected( tabName );
@@ -55,7 +35,7 @@ export const SiteLogsTabPanel = ( {
 			} }
 		>
 			{ ( tab ) => renderContents( tab ) }
-		</LogsTabPanel>
+		</TabPanel>
 	);
 };
 
