6f3aec781c446ed89b16ceb39351c1ca0f653f57
Marcelo Vega <marcelo.vega@automattic.com>
2023-03-25 19:57:20 -0300

    AdvancedCredentials is rendered if user has not set the destination

--- client/my-sites/backup/clone-flow/test/index.jsx
+++ client/my-sites/backup/clone-flow/test/index.jsx
@@ -2,14 +2,28 @@
  * @jest-environment jsdom
  */
 import { render } from '@testing-library/react';
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import AdvancedCredentials from 'calypso/components/advanced-credentials';
+import StepProgress from 'calypso/components/step-progress';
 import getRewindState from 'calypso/state/selectors/get-rewind-state';
 import Loading from '../../rewind-flow/loading';
 import BackupCloneFlow from '../index';
 
-//jest.mock( '../../rewind-flow/loading' );
+jest.mock( 'i18n-calypso', () => ( {
+	...jest.requireActual( 'i18n-calypso' ),
+	localize: ( x ) => x,
+	translate: ( x ) => x,
+	useTranslate: jest.fn( () => ( text ) => text ),
+} ) );
+
+jest.mock( 'react', () => ( {
+	...jest.requireActual( 'react' ),
+	useState: jest.fn(),
+} ) );
+
 jest.mock( 'calypso/state/selectors/get-rewind-state' );
 
 jest.mock( 'calypso/state/selectors/get-site-gmt-offset', () =>
@@ -20,6 +34,10 @@ jest.mock( 'calypso/state/selectors/get-site-timezone-value', () =>
 	jest.fn().mockImplementation( () => null )
 );
 
+jest.mock( 'calypso/components/advanced-credentials', () =>
+	jest.fn().mockImplementation( () => null )
+);
+jest.mock( 'calypso/components/step-progress', () => jest.fn().mockImplementation( () => null ) );
 jest.mock( '../../rewind-flow/loading', () => jest.fn().mockImplementation( () => null ) );
 
 function createState( siteId = 1 ) {
@@ -52,6 +70,27 @@ function createState( siteId = 1 ) {
 	};
 }
 
+function setUseStateMockCloneFlow( {
+	userHasRequestedRestore = false,
+	userHasSetDestination = false,
+	cloneDestination = '',
+	userHasSetBackupPeriod = false,
+	backupPeriod = '',
+	backupDisplayDate = '',
+} ) {
+	useState
+		.mockReturnValueOnce( [
+			'{"themes":true,"plugins":true,"uploads":true,"sqls":true,"roots":true,"contents":true}',
+			jest.fn(),
+		] ) // rewindConfig
+		.mockReturnValueOnce( [ userHasRequestedRestore, jest.fn() ] )
+		.mockReturnValueOnce( [ userHasSetDestination, jest.fn() ] )
+		.mockReturnValueOnce( [ cloneDestination, jest.fn() ] )
+		.mockReturnValueOnce( [ userHasSetBackupPeriod, jest.fn() ] )
+		.mockReturnValueOnce( [ backupPeriod, jest.fn() ] )
+		.mockReturnValueOnce( [ backupDisplayDate, jest.fn() ] );
+}
+
 describe( 'BackupCloneFlow render', () => {
 	describe( 'Render loading it state is uninitialized', () => {
 		beforeEach( () => {
@@ -77,6 +116,31 @@ describe( 'BackupCloneFlow render', () => {
 			);
 
 			expect( Loading ).toHaveBeenCalledTimes( 1 );
+		} );
+
+		test( 'AdvancedCredentials is rendered if user has not set the destination', () => {
+			getRewindState.mockImplementation( () => ( {
+				state: 'active',
+			} ) );
+
+			// Mock UseState on BackupCloneFlow to set userHasSetDestination: false
+			setUseStateMockCloneFlow( { userHasSetDestination: false } );
+
+			const mockStore = configureStore();
+			const store = mockStore( createState() );
+			const queryClient = new QueryClient();
+
+			// Render component
+			render(
+				<Provider store={ store }>
+					<QueryClientProvider client={ queryClient }>
+						<BackupCloneFlow />
+					</QueryClientProvider>
+				</Provider>
+			);
+
+			expect( StepProgress ).toHaveBeenCalledTimes( 1 );
+			expect( AdvancedCredentials ).toHaveBeenCalledTimes( 1 );
 		} );
 	} );
 } );
