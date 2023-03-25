4f9f88587cad1e0de31d18228d0fec40cc8e7166
Marcelo Vega <marcelo.vega@automattic.com>
2023-03-25 18:41:29 -0300

    Again from scratch. RewindFlowLoading is rendered if state is not initialized

--- client/my-sites/backup/clone-flow/test/index.jsx
+++ client/my-sites/backup/clone-flow/test/index.jsx
@@ -1,54 +1,16 @@
 /**
  * @jest-environment jsdom
  */
-import { render, screen } from '@testing-library/react';
-import moment from 'moment';
-import { useState } from 'react';
-import { useDispatch, Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import {
-	FormMode,
-	INITIAL_FORM_STATE,
-	INITIAL_FORM_ERRORS,
-} from 'calypso/components/advanced-credentials/form';
-import { useLocalizedMoment } from 'calypso/components/localized-moment';
-import getJetpackCredentialsTestStatus from 'calypso/state/selectors/get-jetpack-credentials-test-status';
 import getRewindState from 'calypso/state/selectors/get-rewind-state';
-import isRequestingSiteCredentials from 'calypso/state/selectors/is-requesting-site-credentials';
+import Loading from '../../rewind-flow/loading';
 import BackupCloneFlow from '../index';
 
-jest.mock( 'react', () => ( {
-	...jest.requireActual( 'react' ),
-	useState: jest.fn(),
-} ) );
-
-jest.mock( 'react-redux', () => ( {
-	...jest.requireActual( 'react-redux' ),
-	useSelector: jest.fn().mockImplementation( ( selector ) => selector() ),
-	useDispatch: jest.fn().mockImplementation( () => () => {} ),
-} ) );
-
-jest.mock( 'calypso/lib/analytics/tracks', () => ( {} ) );
-jest.mock( 'calypso/lib/analytics/track-component-view', () =>
-	require( 'calypso/components/empty-component' )
-);
-jest.mock( 'calypso/lib/analytics/page-view-tracker', () =>
-	require( 'calypso/components/empty-component' )
-);
-
-jest.mock( 'calypso/components/localized-moment', () => ( {
-	...jest.requireActual( 'calypso/components/localized-moment' ),
-	useLocalizedMoment: jest.fn(),
-} ) );
-
-jest.mock( 'i18n-calypso', () => ( {
-	...jest.requireActual( 'i18n-calypso' ),
-	useTranslate: jest.fn( () => ( text ) => text ),
-} ) );
-
-jest.mock( 'calypso/data/activity-log/use-rewindable-activity-log-query', () =>
-	jest.fn().mockImplementation( () => [] )
-);
+//jest.mock( '../../rewind-flow/loading' );
+jest.mock( 'calypso/state/selectors/get-rewind-state' );
 
 jest.mock( 'calypso/state/selectors/get-site-gmt-offset', () =>
 	jest.fn().mockImplementation( () => null )
@@ -58,19 +20,7 @@ jest.mock( 'calypso/state/selectors/get-site-timezone-value', () =>
 	jest.fn().mockImplementation( () => null )
 );
 
-jest.mock( 'calypso/state/selectors/get-jetpack-credentials-test-status' );
-jest.mock( 'calypso/state/selectors/get-rewind-state' );
-jest.mock( 'calypso/state/selectors/is-requesting-site-credentials' );
-
-jest.mock( 'calypso/state/sites/selectors', () => ( {
-	getSiteSlug: jest.fn().mockImplementation( () => 'mysite.example' ),
-} ) );
-
-jest.mock( 'calypso/state/ui/selectors', () => ( {
-	...jest.requireActual( 'calypso/state/ui/selectors' ),
-	getSelectedSiteId: jest.fn().mockImplementation( () => 1 ),
-	getSelectedSiteSlug: jest.fn().mockImplementation( () => 'mysite.example' ),
-} ) );
+jest.mock( '../../rewind-flow/loading', () => jest.fn().mockImplementation( () => null ) );
 
 function createState( siteId = 1 ) {
 	return {
@@ -86,7 +36,15 @@ function createState( siteId = 1 ) {
 				[ siteId ]: {},
 			},
 		},
-		ui: { selectedSiteId: siteId },
+		ui: {
+			selectedSiteId: siteId,
+			section: {
+				name: 'backup"',
+				paths: [ '/backup' ],
+				module: 'calypso/my-sites/backup',
+				group: 'sites',
+			},
+		},
 		preferences: {
 			remoteValues: {},
 		},
@@ -94,93 +52,31 @@ function createState( siteId = 1 ) {
 	};
 }
 
-function setUseStateMockCloneFlow() {
-	useState
-		.mockReturnValueOnce( [
-			'{"themes":true,"plugins":true,"uploads":true,"sqls":true,"roots":true,"contents":true}',
-			jest.fn(),
-		] ) // rewindConfig
-		.mockReturnValueOnce( [ false, jest.fn() ] ) // userHasRequestedRestore;
-		.mockReturnValueOnce( [ false, jest.fn() ] ) // userHasSetDestination
-		.mockReturnValueOnce( [ '', jest.fn() ] ) // cloneDestination
-		.mockReturnValueOnce( [ false, jest.fn() ] ) // userHasSetBackupPeriod
-		.mockReturnValueOnce( [ '', jest.fn() ] ) // backupPeriod
-		.mockReturnValueOnce( [ '', jest.fn() ] ); // backupDisplayDate
-}
-
-function setUseStateMockAdvancedCredentials() {
-	useState
-		.mockReturnValueOnce( [ INITIAL_FORM_STATE, jest.fn() ] ) // setFormState
-		.mockReturnValueOnce( [ INITIAL_FORM_ERRORS, jest.fn() ] ) // setFormErrors;
-		.mockReturnValueOnce( [ FormMode.Password, jest.fn() ] ) // setFormMode
-		.mockReturnValueOnce( [ false, jest.fn() ] ) // setStartedWithoutConnection
-		.mockReturnValueOnce( [ false, jest.fn() ] ) // setTestCredentialsLoading
-		.mockReturnValueOnce( [ false, jest.fn() ] ); // setTestCredentialsResult
-}
-
-describe( 'BackupCloneFlow', () => {
-	describe( 'Render loading', () => {
+describe( 'BackupCloneFlow render', () => {
+	describe( 'Render loading it state is uninitialized', () => {
 		beforeEach( () => {
 			jest.clearAllMocks();
-			useLocalizedMoment.mockImplementation( () => moment );
-			//useTranslate.mockImplementation( mockUseTranslate );
 		} );
 
-		test( 'Show RewindFlowLoading if it is not initialized', () => {
-			// Mock dispatch
-			const mockedDispatch = jest.fn();
-			useDispatch.mockReturnValue( mockedDispatch );
-
+		test( 'RewindFlowLoading is rendered if state is not initialized', () => {
 			getRewindState.mockImplementation( () => ( {
 				state: 'uninitialized',
 			} ) );
 
 			const mockStore = configureStore();
 			const store = mockStore( createState() );
-
-			setUseStateMockCloneFlow();
-
-			// Render component
-			render(
-				<Provider store={ store }>
-					<BackupCloneFlow />
-				</Provider>
-			);
-
-			expect( screen.getByText( 'Loading restore status…' ) ).toBeVisible();
-		} );
-	} );
-
-	describe( 'Render setDestination', () => {
-		test( 'Advance credentials render renderCredentialsForm', () => {
-			// Mock dispatch
-			const mockedDispatch = jest.fn();
-			useDispatch.mockReturnValue( mockedDispatch );
-
-			getRewindState.mockImplementation( () => ( {
-				state: 'active',
-			} ) );
-
-			const mockStore = configureStore();
-			const store = mockStore( createState() );
-
-			setUseStateMockCloneFlow();
-			setUseStateMockAdvancedCredentials();
-			setUseStateMockAdvancedCredentials(); //I don't know but it is called twice
-			getJetpackCredentialsTestStatus.mockImplementation( () => 'pending' );
-			isRequestingSiteCredentials.mockImplementation( () => false );
+			const queryClient = new QueryClient();
 
 			// Render component
 			render(
 				<Provider store={ store }>
-					<BackupCloneFlow />
+					<QueryClientProvider client={ queryClient }>
+						<BackupCloneFlow />
+					</QueryClientProvider>
 				</Provider>
 			);
 
-			expect( screen.queryByText( /Loading restore status…/i ) ).toBeNull();
-			expect(
-				screen.queryByText( /Enter the server credentials from your hosting provider/i )
-			).toBeVisible();
+			expect( Loading ).toHaveBeenCalledTimes( 1 );
 		} );
 	} );
 } );
