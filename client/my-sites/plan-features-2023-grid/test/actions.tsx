2941e4ae20afabf45b14e218a29d7e108cc3a45c
Christos <chriskmnds@gmail.com>
2023-03-27 11:56:33 +0300

    unit/ui tests for upgrade actions

--- client/my-sites/plan-features-2023-grid/test/actions.tsx
+++ client/my-sites/plan-features-2023-grid/test/actions.tsx
@@ -12,8 +12,8 @@ jest.mock( '@wordpress/data', () => ( {
 	combineReducers: jest.fn(),
 	createReduxStore: jest.fn(),
 	register: jest.fn(),
+	useDispatch: jest.fn( () => ( { setShowDomainUpsellDialog: jest.fn() } ) ),
 } ) );
-jest.spyOn( '@wordpress/data', 'useDispatch' );
 jest.mock( '@wordpress/element', () => ( {
 	...jest.requireActual( '@wordpress/element' ),
 	useCallback: jest.fn(),
@@ -32,39 +32,101 @@ jest.mock( 'react-redux', () => ( {
 	useDispatch: jest.fn(),
 } ) );
 
+import {
+	PLAN_BUSINESS,
+	PLAN_BUSINESS_2_YEARS,
+	PLAN_BUSINESS_3_YEARS,
+	PLAN_BUSINESS_MONTHLY,
+	PLAN_PREMIUM,
+} from '@automattic/calypso-products';
 import { render, screen } from '@testing-library/react';
-import { useDispatch } from '@wordpress/data';
 import React from 'react';
 import PlanFeatures2023GridActions from '../actions';
 
 describe( 'PlanFeatures2023GridActions', () => {
 	beforeEach( () => {
 		jest.clearAllMocks();
-
-		// Re-mock dependencies
-		const mockedDispatch = jest.spyOn( useDispatch as jest.Mock, 'mockReturnValue' );
-		mockedDispatch.mockImplementation( () => ( { setShowDomainUpsellDialog: false } as any ) );
 	} );
 
-	test( 'should render', () => {
-		render(
-			<PlanFeatures2023GridActions
-				availableForPurchase={ false }
-				canUserPurchasePlan={ false }
-				className=""
-				currentSitePlanSlug="free_plan"
-				current={ false }
-				freePlan={ false }
-				manageHref=""
-				isInSignup={ false }
-				onUpgradeClick={ jest.fn() }
-				planName="free_plan"
-				planType="free_plan"
-				flowName=""
-				isWpcomEnterpriseGridPlan={ false }
-				selectedSiteSlug="foo"
-			/>
-		);
-		expect( screen.getByRole( 'button' ) ).toBeInTheDocument();
+	describe( 'Upgrade button', () => {
+		const upgrade = 'Upgrade';
+		const upgradeToYearly = 'Upgrade to Yearly';
+		const upgradeToBiennial = 'Upgrade to Biennial';
+		const upgradeToTriennial = 'Upgrade to Triennial';
+		const defaultProps = {
+			availableForPurchase: true,
+			canUserPurchasePlan: true,
+			className: '',
+			current: false,
+			freePlan: false,
+			manageHref: '',
+			isInSignup: false,
+			onUpgradeClick: jest.fn(),
+			flowName: 'foo-flow',
+			isWpcomEnterpriseGridPlan: false,
+			selectedSiteSlug: 'foo.wordpress.com',
+		};
+
+		test( `should render ${ upgrade } when current plan and grid plan do not match`, () => {
+			render(
+				<PlanFeatures2023GridActions
+					{ ...defaultProps }
+					currentSitePlanSlug={ PLAN_PREMIUM }
+					planName={ PLAN_BUSINESS }
+					planType={ PLAN_BUSINESS }
+				/>
+			);
+
+			const upgradeButton = screen.getByRole( 'button', { name: upgrade } );
+
+			expect( upgradeButton ).toBeEnabled();
+		} );
+
+		describe( 'when current plan matches grid plan on lower term', () => {
+			test( `should render ${ upgradeToYearly } when grid plan yearly`, () => {
+				render(
+					<PlanFeatures2023GridActions
+						{ ...defaultProps }
+						currentSitePlanSlug={ PLAN_BUSINESS_MONTHLY }
+						planName={ PLAN_BUSINESS }
+						planType={ PLAN_BUSINESS }
+					/>
+				);
+
+				const upgradeButton = screen.getByRole( 'button', { name: upgradeToYearly } );
+
+				expect( upgradeButton ).toBeEnabled();
+			} );
+
+			test( `should render ${ upgradeToBiennial } when grid plan 2-yearly`, () => {
+				render(
+					<PlanFeatures2023GridActions
+						{ ...defaultProps }
+						currentSitePlanSlug={ PLAN_BUSINESS_MONTHLY }
+						planName={ PLAN_BUSINESS_2_YEARS }
+						planType={ PLAN_BUSINESS_2_YEARS }
+					/>
+				);
+
+				const upgradeButton = screen.getByRole( 'button', { name: upgradeToBiennial } );
+
+				expect( upgradeButton ).toBeEnabled();
+			} );
+
+			test( `should render ${ upgradeToTriennial } when grid plan 3-yearly`, () => {
+				render(
+					<PlanFeatures2023GridActions
+						{ ...defaultProps }
+						currentSitePlanSlug={ PLAN_BUSINESS_MONTHLY }
+						planName={ PLAN_BUSINESS_3_YEARS }
+						planType={ PLAN_BUSINESS_3_YEARS }
+					/>
+				);
+
+				const upgradeButton = screen.getByRole( 'button', { name: upgradeToTriennial } );
+
+				expect( upgradeButton ).toBeEnabled();
+			} );
+		} );
 	} );
 } );
