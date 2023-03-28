3e68fd4470734d22e385f04e4fec58f0f1d2eaf1
jboland88 <18016357+jboland88@users.noreply.github.com>
2023-03-28 11:44:43 -0400

    Update translator comment

--- client/me/purchases/manage-purchase/auto-renew-toggle/auto-renew-disabling-dialog/index.jsx
+++ client/me/purchases/manage-purchase/auto-renew-toggle/auto-renew-disabling-dialog/index.jsx
@@ -163,7 +163,7 @@ class AutoRenewDisablingDialog extends Component {
 							expiryDate,
 						},
 						comment:
-							'%(productName)s is the name of a WordPress.com product. ' +
+							'%(productName)s is the name of an Akismet plan/ product. ' +
 							'%(siteDomain)s is a domain name, e.g. example.com, example.wordpress.com. ' +
 							'%(expiryDate)s is a date string, e.g. May 14, 2020',
 					}
