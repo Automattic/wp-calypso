68ab97924ff80d81c8da8a8fe4cda795d8bcf4b0
Juan Adolfo Lucha García <juanlucha@gmail.com>
2023-03-28 11:44:50 +0200

    Add/Sites view to Subscriptions Manager

---
+++ client/landing/subscriptions/tab-views/site-types.tsx
@@ -0,0 +1,9 @@
+// typo de dato con estos valores: id, name, icon, url, date, emailFrequency
+export type SiteType = {
+	id: number;
+	name: string;
+	icon: string;
+	url: string;
+	date: Date;
+	emailFrequency: string;
+};
