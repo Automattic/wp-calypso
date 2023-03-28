d7ff55631db340a4e284891238ee4fc435f41927
arthur <arthur.chu@automattic.com>
2023-03-28 14:52:33 +0800

    Site Assembler: Group the event names

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/pattern-large-preview.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/pattern-large-preview.tsx
@@ -6,6 +6,7 @@ import classnames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
 import { useRef, useEffect, useState, CSSProperties } from 'react';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { PATTERN_ASSEMBLER_EVENTS } from './events';
 import PatternActionBar from './pattern-action-bar';
 import { encodePatternId } from './utils';
 import type { Pattern } from './types';
@@ -148,7 +149,7 @@ const PatternLargePreview = ( {
 			isShowFrameBorder
 			frameRef={ frameRef }
 			onDeviceChange={ ( device ) => {
-				recordTracksEvent( 'calypso_signup_pattern_assembler_preview_device_click', { device } );
+				recordTracksEvent( PATTERN_ASSEMBLER_EVENTS.PREVIEW_DEVICE_CLICK, { device } );
 				// Wait for the animation to end in 200ms
 				window.setTimeout( updateViewportHeight, 205 );
 			} }
