14638b90cc9bef8d7b8cd370da47133dc3da678f
Dognose <dognose24@users.noreply.github.com>
2023-03-28 10:09:08 +0800

    Stats: Add loading placeholders to post stats cards on the Insights page (#74904)
    * Introduce isLoading support to PostStatsCard
    
    * Add loading check to LatestPostCard
    
    * Add loading check to MostPopularPost

--- client/my-sites/stats/all-time-highlights-section/latest-post-card.tsx
+++ client/my-sites/stats/all-time-highlights-section/latest-post-card.tsx
@@ -5,8 +5,8 @@ import { useSelector } from 'react-redux';
 import QueryPostStats from 'calypso/components/data/query-post-stats';
 import { decodeEntities, stripHTML } from 'calypso/lib/formatting';
 import { getCurrentUserLocale } from 'calypso/state/current-user/selectors';
-import { getPostsForQuery } from 'calypso/state/posts/selectors';
-import { getPostStat } from 'calypso/state/stats/posts/selectors';
+import { getPostsForQuery, isRequestingPostsForQuery } from 'calypso/state/posts/selectors';
+import { getPostStat, isRequestingPostStats } from 'calypso/state/stats/posts/selectors';
 
 const POST_STATS_CARD_TITLE_LIMIT = 60;
 
@@ -37,11 +37,21 @@ export default function LatestPostCard( {
 		getPostsForQuery( state, siteId, { status: 'publish', number: 1 } )
 	);
 
+	const isRequestingPosts = useSelector( ( state ) =>
+		isRequestingPostsForQuery( state, siteId, { status: 'publish', number: 1 } )
+	);
+
 	const latestPost = posts && posts.length ? posts[ 0 ] : null;
 
 	const lastesPostViewCount = useSelector( ( state ) =>
 		getPostStat( state, siteId, latestPost?.ID, 'views' )
 	);
+
+	const isRequestingLatestPostViewCount = useSelector( ( state ) =>
+		isRequestingPostStats( state, siteId, latestPost?.ID, [ 'views' ] )
+	);
+
+	const isLoadingLatestPost = isRequestingPosts || isRequestingLatestPostViewCount;
 
 	const latestPostData = {
 		date: latestPost?.date,
@@ -50,7 +60,7 @@ export default function LatestPostCard( {
 			stripHTML( textTruncator( latestPost?.title, POST_STATS_CARD_TITLE_LIMIT ) )
 		),
 		likeCount: latestPost?.like_count,
-		viewCount: lastesPostViewCount,
+		viewCount: lastesPostViewCount || 0,
 		commentCount: latestPost?.discussion?.comment_count,
 	};
 
@@ -60,16 +70,17 @@ export default function LatestPostCard( {
 				<QueryPostStats siteId={ siteId } postId={ latestPost.ID } fields={ [ 'views' ] } />
 			) }
 
-			{ latestPost && (
+			{ ( isLoadingLatestPost || latestPost ) && (
 				<PostStatsCard
 					heading={ translate( 'Latest post' ) }
 					likeCount={ latestPostData?.likeCount }
 					post={ latestPostData }
 					viewCount={ latestPostData?.viewCount }
 					commentCount={ latestPostData?.commentCount }
-					titleLink={ `/stats/post/${ latestPost.ID }/${ siteSlug }` }
-					uploadHref={ ! isOdysseyStats ? `/post/${ siteSlug }/${ latestPost.ID }` : undefined }
+					titleLink={ `/stats/post/${ latestPost?.ID }/${ siteSlug }` }
+					uploadHref={ ! isOdysseyStats ? `/post/${ siteSlug }/${ latestPost?.ID }` : undefined }
 					locale={ userLocale }
+					isLoading={ isLoadingLatestPost }
 				/>
 			) }
 		</>
