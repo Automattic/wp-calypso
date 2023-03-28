01922cf68e8b4f1a276254c1d329e955c193ba84
Anna McPhee <anna.mcphee@automattic.com>
2023-03-27 18:25:38 -0600

    update reducer code

--- client/state/stats/subscribers/reducer.js
+++ client/state/stats/subscribers/reducer.js
@@ -1,91 +1,22 @@
-import { pick, set, isEqual } from 'lodash';
-import { STATS_SUBSCRIBERS_REQUEST, STATS_SUBSCRIBERS_RECEIVE } from 'calypso/state/action-types';
 import {
-	combineReducers,
-	keyedReducer,
-	withSchemaValidation,
-	withPersistence,
-} from 'calypso/state/utils';
-import { QUERY_FIELDS } from './constants';
-import { countsSchema } from './schema';
+	// STATS_SUBSCRIBERS_RECEIVE,
+	// STATS_SUBSCRIBERS_REQUEST,
+	STATS_SUBSCRIBERS_REQUEST_SUCCESS,
+	// STATS_SUBSCRIBERS_REQUEST_FAILURE,
+} from 'calypso/state/action-types';
+// import { combineReducers } from 'calypso/state/utils';
 
-/**
- * Returns the updated count records state after an action has been dispatched.
- *
- * @param  {Object} state  Current state
- * @param  {Object} action Action payload
- * @returns {Object}        Updated state
- */
-const countsReducer = ( state = [], action ) => {
+const initialState = {
+	subscribers: [],
+};
+
+const SubscribersData = ( state = initialState.subscribers, action ) => {
 	switch ( action.type ) {
-		case STATS_SUBSCRIBERS_RECEIVE: {
-			// Workaround to prevent new data from being appended to previous data when range period differs.
-			// See https://github.com/Automattic/wp-calypso/pull/41441#discussion_r415918092
-			if (
-				action.data.length !== state.length ||
-				! isEqual( action.data[ 0 ].period, state[ 0 ].period )
-			) {
-				return action.data;
-			}
-
-			let areThereChanges = false;
-
-			const newState = action.data.reduce(
-				( nextState, recordFromApi ) => {
-					const index = nextState.findIndex( ( entry ) => entry.period === recordFromApi.period );
-					if ( index >= 0 ) {
-						const newRecord = { ...nextState[ index ], ...recordFromApi };
-						if ( ! isEqual( nextState[ index ], newRecord ) ) {
-							areThereChanges = true;
-							nextState[ index ] = newRecord;
-						}
-					} else {
-						areThereChanges = true;
-						nextState.push( recordFromApi );
-					}
-					return nextState;
-				},
-				[ ...state ]
-			);
-
-			// Avoid changing state if nothing's changed.
-			return areThereChanges ? newState : state;
-		}
+		case STATS_SUBSCRIBERS_REQUEST_SUCCESS:
+			return [ ...state, ...action.subscribers ];
 	}
+
 	return state;
 };
 
-export const counts = withSchemaValidation(
-	countsSchema,
-	keyedReducer( 'siteId', keyedReducer( 'period', withPersistence( countsReducer ) ) )
-);
-
-/**
- * Returns the loading state after an action has been dispatched.
- *
- * @param  {Object} state  Current state
- * @param  {Object} action Action payload
- * @returns {Object}        Updated state
- */
-const isLoadingReducer = ( state = {}, action ) => {
-	switch ( action.type ) {
-		case STATS_SUBSCRIBERS_REQUEST: {
-			return action.statFields.reduce(
-				( nextState, statField ) => set( nextState, statField, true ),
-				{ ...state }
-			);
-		}
-		case STATS_SUBSCRIBERS_RECEIVE: {
-			return Object.keys( pick( action.data[ 0 ], QUERY_FIELDS ) ).reduce(
-				( nextState, statField ) => set( nextState, statField, false ),
-				{ ...state }
-			);
-		}
-		// TODO: Add failure handling
-	}
-	return state;
-};
-
-export const isLoading = keyedReducer( 'siteId', keyedReducer( 'period', isLoadingReducer ) );
-
-export default combineReducers( { counts, isLoading } );
+export default SubscribersData;
