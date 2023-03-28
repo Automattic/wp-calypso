1aacb4d76a700705b335a66bd66f1889c1b4fa95
Philip Jackson <p-jackson@live.com>
2023-03-29 01:16:01 +1300

    Display pagination controls below site log table (#74935)
    * Display pagination controls below site log table
    
    * Add tests for pagination
    
    * Do not cache log results by scroll_id, use a dummy page index param instead
    
    The log requests are not idempotent. The scroll ID actually represets a
    query state that lives on the server for 10 minutes. We can't use the
    scroll ID as a mechanism for fetching the next page since it will be a
    consistent value across all pages.
    
    The page index is now used to store the log entry in ReactQuery. The
    page index isn't actually part of the request body, but it acts as a
    good key for persisting the logs.
    
    * Show previous page while loading next page
    
    This prevents the display from jumping while we're loading more data
    
    * Logging: reset page number when changing tab
    
    * Fix eslint duplicate selector issue
    
    ---------
    
    Co-authored-by: mpkelly <kikemelly@gmail.com>

--- client/data/hosting/use-site-logs-query.ts
+++ client/data/hosting/use-site-logs-query.ts
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import { UseQueryOptions, useQuery } from 'react-query';
 import wpcom from 'calypso/lib/wp';
 
 interface SiteLogsAPIResponse {
 	message: string;
 	data: {
-		total_results: number;
-		logs: Record< string, any >[];
+		total_results: number | { value: number; relation: string };
+		logs: Record< string, unknown >[];
 		scroll_id: string | null;
 	};
 }
 
-export type SiteLogsData = SiteLogsAPIResponse[ 'data' ];
+export type SiteLogsData = {
+	total_results: number;
+	logs: Record< string, unknown >[];
+	scroll_id: string | null;
+};
 
 export type SiteLogsTab = 'php' | 'web';
 
@@ -18,9 +23,9 @@ export interface SiteLogsParams {
 	logType: SiteLogsTab;
 	start: number;
 	end: number;
-	sort_order?: 'asc' | 'desc';
-	page_size?: number;
-	scroll_id?: string;
+	sortOrder?: 'asc' | 'desc';
+	pageSize?: number;
+	pageIndex?: number;
 }
 
 export function useSiteLogsQuery(
@@ -28,26 +33,45 @@ export function useSiteLogsQuery(
 	params: SiteLogsParams,
 	queryOptions: UseQueryOptions< SiteLogsAPIResponse, unknown, SiteLogsData > = {}
 ) {
-	return useQuery< SiteLogsAPIResponse, unknown, SiteLogsData >(
-		[
-			'site-logs',
-			siteId,
-			params.logType,
-			params.start,
-			params.end,
-			params.page_size,
-			params.scroll_id,
-		],
+	const [ scrollId, setScrollId ] = useState< string | undefined >();
+	const [ isFinishedPaging, setIsFinishedPaging ] = useState< boolean >( false );
+
+	const queryResult = useQuery< SiteLogsAPIResponse, unknown, SiteLogsData >(
+		buildQueryKey( siteId, params ),
 		() => {
 			const logTypeFragment = params.logType === 'php' ? 'error-logs' : 'logs';
 			const path = `/sites/${ siteId }/hosting/${ logTypeFragment }`;
-			return wpcom.req.post( { path, apiNamespace: 'wpcom/v2' }, params );
+			return wpcom.req.post(
+				{ path, apiNamespace: 'wpcom/v2' },
+				{
+					start: params.start,
+					end: params.end,
+					sort_order: params.sortOrder,
+					page_size: params.pageSize,
+					scroll_id: scrollId,
+				}
+			);
 		},
 		{
 			enabled: !! siteId,
 			staleTime: Infinity, // The logs within a specified time range never change.
-			select( data ) {
-				return data.data;
+			select( { data } ) {
+				return {
+					...data,
+					total_results:
+						typeof data.total_results === 'number' ? data.total_results : data.total_results.value,
+				};
+			},
+			onSuccess( data ) {
+				if ( data.scroll_id ) {
+					setScrollId( data.scroll_id );
+				} else {
+					setIsFinishedPaging( true );
+				}
+
+				if ( queryOptions.onSuccess ) {
+					return queryOptions.onSuccess( data );
+				}
 			},
 			meta: {
 				persist: false,
@@ -55,4 +79,26 @@ export function useSiteLogsQuery(
 			...queryOptions,
 		}
 	);
+
+	// The state represented by scroll ID will have already advanced to the next page, so we
+	// can't allow `refetch` to be used. Remember, the logs are fetched with a POST and the
+	// requests are not idempotent.
+	const { refetch, ...remainingQueryResults } = queryResult;
+
+	return {
+		isFinishedPaging,
+		...remainingQueryResults,
+	};
+}
+
+function buildQueryKey( siteId: number | null | undefined, params: SiteLogsParams ) {
+	return [
+		params.logType === 'php' ? 'site-logs-php' : 'site-logs-web',
+		siteId,
+		params.start,
+		params.end,
+		params.sortOrder,
+		params.pageSize,
+		params.pageIndex,
+	];
 }
