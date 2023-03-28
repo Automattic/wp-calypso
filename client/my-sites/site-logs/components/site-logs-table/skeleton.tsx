a8b968752d5547db557ae20474d0b983974a8918
Philip Jackson <p-jackson@live.com>
2023-03-29 09:31:33 +1300

    Arrange table skeleton cells in a grid

--- client/my-sites/site-logs/components/site-logs-table/skeleton.tsx
+++ client/my-sites/site-logs/components/site-logs-table/skeleton.tsx
@@ -3,18 +3,21 @@ import { LoadingPlaceholder } from '@automattic/components';
 export const Skeleton = () => {
 	return (
 		<div className="site-logs-table__skeleton">
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
-
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
-			<LoadingPlaceholder className="site-logs-table__skeleton-table-row" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
+			<LoadingPlaceholder className="site-logs-table__skeleton-table-cell" />
 		</div>
 	);
 };
