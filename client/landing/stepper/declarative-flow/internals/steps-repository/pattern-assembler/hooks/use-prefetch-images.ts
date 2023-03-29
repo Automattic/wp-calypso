574e14e8aa5980371e137ca88a0af55173d9f6ca
okmttdhr <okmttdhr@users.noreply.github.com>
2023-03-29 16:30:41 +0900

    Optimize the images

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/hooks/use-prefetch-images.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/hooks/use-prefetch-images.ts
@@ -21,17 +21,17 @@ export const usePrefetchImage = () => {
 
 	useEffect( () => {
 		const IMAGE_URLS = [
-			'https://dotcompatterns.files.wordpress.com/2022/07/header-mountains.jpeg',
+			'https://dotcompatterns.files.wordpress.com/2023/03/header-mountains.jpeg',
 
-			'https://blankcanvas3demo.files.wordpress.com/2023/01/mountains_landscape.jpg?w=1024',
-			'https://blankcanvas3demo.files.wordpress.com/2023/01/mountains_landscape.jpg',
-			'https://dotcompatterns.files.wordpress.com/2023/02/mountains_landscape.jpg?w=1024',
-			'https://dotcompatterns.files.wordpress.com/2023/02/mountains_landscape.jpg',
+			'https://blankcanvas3demo.files.wordpress.com/2023/03/mountains_landscape.jpeg?w=1024',
+			'https://blankcanvas3demo.files.wordpress.com/2023/03/mountains_landscape.jpeg',
+			'https://dotcompatterns.files.wordpress.com/2023/03/mountains_landscape.jpeg?w=1024',
+			'https://dotcompatterns.files.wordpress.com/2023/03/mountains_landscape.jpeg',
 
-			'https://dotcompatterns.files.wordpress.com/2023/02/mountains_portrait.jpg?w=640',
-			'https://dotcompatterns.files.wordpress.com/2023/02/mountains_portrait.jpg',
+			'https://dotcompatterns.files.wordpress.com/2023/03/mountains_portrait.jpeg?w=640',
+			'https://dotcompatterns.files.wordpress.com/2023/03/mountains_portrait.jpeg',
 
-			'https://dotcompatterns.files.wordpress.com/2023/02/mountains_square.jpg?w=800',
+			'https://dotcompatterns.files.wordpress.com/2023/03/mountains_square.jpeg?w=800',
 		];
 		IMAGE_URLS.forEach( ( url ) => prefetchImage( url ) );
 		return () => {
