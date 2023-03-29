40fc1f7cd5106147524d5160673c937c479aa418
Tim Broddin <tim@wannabes.be>
2023-03-29 15:12:01 +0200

    Export useSubscriptionManagerSubscriptionsQuery

--- packages/data-stores/src/reader/index.ts
+++ packages/data-stores/src/reader/index.ts
@@ -1,6 +1,7 @@
 // Queries
 export { useSubscriptionManagerUserSettingsQuery } from './queries/use-subscription-manager-user-settings-query';
 export { useSubscriptionManagerSubscriptionsCountQuery } from './queries/use-subscription-manager-subscriptions-count-query';
+export { useSubscriptionManagerSiteSubscriptionsQuery } from './queries/use-subscription-manager-site-subscriptions-query';
 
 // Mutations
 export { useSubscriptionManagerUserSettingsMutation } from './mutations/use-subscription-manager-user-settings-mutation';
