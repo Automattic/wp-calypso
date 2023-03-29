26aa33d12ab8535ff395fc44f09660fba823d4a1
vykes-mac <47489215+vykes-mac@users.noreply.github.com>
2023-03-29 13:49:02 -0500

    Update client/components/date-range/index.js
    Co-authored-by: Philip Jackson <p-jackson@live.com>

--- client/components/date-range/index.js
+++ client/components/date-range/index.js
@@ -180,7 +180,7 @@ export class DateRange extends Component {
 	isValidDate( date ) {
 		const { firstSelectableDate, lastSelectableDate } = this.props;
 
-		const epoch = this.props.moment( '01/01/1970' );
+		const epoch = this.props.moment( '01/01/1970', this.props.moment.localeData().longDateFormat( 'L' ) );
 
 		// By default check
 		// 1. Looks like a valid date
