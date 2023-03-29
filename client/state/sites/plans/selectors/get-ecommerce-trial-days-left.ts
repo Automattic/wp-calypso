bf3c84d89bef989638011802e0e2ba2c47abfb72
Dale du Preez <dale@automattic.com>
2023-03-29 17:03:28 +0200

    Simplify by using Math.floor()

--- client/state/sites/plans/selectors/get-ecommerce-trial-days-left.ts
+++ client/state/sites/plans/selectors/get-ecommerce-trial-days-left.ts
@@ -3,11 +3,11 @@ import { AppState } from 'calypso/types';
 import getECommerceTrialExpiration from './get-ecommerce-trial-expiration';
 
 /**
- * Get the number of days left in the ECommerce trial. If the trial is not active, returns null.
+ * Get the number of full days left in the ECommerce trial. If the trial is not active, returns null.
  *
  * @param {AppState} state - Global state tree
  * @param {number} siteId - Site ID
- * @returns {null|number} Exact number of days left in the trial, or null if the trial is not active.
+ * @returns {null|number} Number of days left in the trial, or null if the trial is not active.
  */
 export default function getECommerceTrialDaysLeft(
 	state: AppState,
@@ -18,27 +18,8 @@ export default function getECommerceTrialDaysLeft(
 		return null;
 	}
 
-	const currentTimeUtc = moment().utc();
-	let startTimeUtc;
-
-	// If the current time is 00:00:00.000 that should be our start time.
-	if (
-		currentTimeUtc.hour() === 0 &&
-		currentTimeUtc.minute() === 0 &&
-		currentTimeUtc.second() === 0 &&
-		currentTimeUtc.millisecond() === 0
-	) {
-		startTimeUtc = currentTimeUtc.clone();
-	} else {
-		// We need start time to be 00:00:00.000 of the next day so we only count full days.
-		startTimeUtc = currentTimeUtc
-			.clone()
-			.add( 1, 'days' )
-			.hour( 0 )
-			.minute( 0 )
-			.second( 0 )
-			.millisecond( 0 );
-	}
-
-	return trialExpirationDate.diff( startTimeUtc, 'days', true );
+	// Use Math.floor() so we get an integer number of days.
+	// The result of diff() will include partial days, like 13.7, which is not
+	// a useful return value from this function.
+	return Math.floor( trialExpirationDate.diff( moment().utc(), 'days', true ) );
 }
