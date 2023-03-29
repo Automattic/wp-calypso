dab7c2e7705fb5e7cf3d541aa3b5eeec6712e00e
Tim Broddin <tim@wannabes.be>
2023-03-29 13:51:37 +0200

    Add test

---
+++ packages/data-stores/src/reader/test/mutations.tsx
@@ -0,0 +1,61 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, waitFor } from '@testing-library/react';
+import { useEffect } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { callApi } from '../helpers';
+import { useSubscriptionManagerSiteDeliveryFrequencyMutation } from '../mutations/use-subscription-manager-site-delivery-frequency-mutation';
+
+// Mock the useIsLoggedIn function
+jest.mock( '../hooks', () => ( {
+	useIsLoggedIn: jest.fn().mockReturnValue( true ),
+} ) );
+
+// Mock the entire Helpers module
+jest.mock( '../helpers', () => ( {
+	callApi: jest.fn(),
+} ) );
+
+const client = new QueryClient();
+const Parent = ( { children } ) => (
+	<QueryClientProvider client={ client }>{ children }</QueryClientProvider>
+);
+
+describe( 'useSubscriptionManagerSiteDeliveryFrequencyMutation()', () => {
+	it( 'calls the right API', async () => {
+		const Skeleton = () => {
+			const { mutate } = useSubscriptionManagerSiteDeliveryFrequencyMutation();
+			useEffect( () => {
+				mutate( {
+					delivery_frequency: 'daily',
+					blog_id: '123',
+				} );
+			}, [ mutate ] );
+
+			return <p></p>;
+		};
+
+		( callApi as jest.Mock ).mockResolvedValue( {
+			success: true,
+		} );
+
+		render(
+			<Parent>
+				<Skeleton />
+			</Parent>
+		);
+
+		await waitFor( () =>
+			expect( callApi ).toHaveBeenCalledWith( {
+				apiVersion: '1.2',
+				path: '/read/site/123/post_email_subscriptions/update',
+				body: {
+					delivery_frequency: 'daily',
+				},
+				isLoggedIn: true,
+				method: 'POST',
+			} )
+		);
+	} );
+} );
