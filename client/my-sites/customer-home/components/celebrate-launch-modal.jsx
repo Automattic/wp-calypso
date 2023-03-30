f02f1d992ade8d495b2f7ee252053ddf47b27f1e
Jeremy Yip <jeremy.yip@automattic.com>
2023-03-30 11:39:18 -0700

    Launchpad: Add domain upsell to launchpad celebration modal (#74744)
    Co-authored-by: Sharpirate <stoynov.bogomil@gmail.com>

--- client/my-sites/customer-home/components/celebrate-launch-modal.jsx
+++ client/my-sites/customer-home/components/celebrate-launch-modal.jsx
@@ -1,13 +1,25 @@
-import { ConfettiAnimation, Gridicon } from '@automattic/components';
+import { Gridicon, ConfettiAnimation } from '@automattic/components';
 import { Button, Modal } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import ClipboardButton from 'calypso/components/forms/clipboard-button';
+import Tooltip from 'calypso/components/tooltip';
 import { omitUrlParams } from 'calypso/lib/url';
+import { createSiteDomainObject } from 'calypso/state/sites/domains/assembler';
 
 import './celebrate-launch-modal.scss';
 
-function CelebrateLaunchModal( { setModalIsOpen, site } ) {
+function CelebrateLaunchModal( { setModalIsOpen, site, allDomains } ) {
 	const translate = useTranslate();
+	const isPaidPlan = ! site?.plan?.is_free;
+	const isBilledMonthly = site?.plan?.product_slug?.includes( 'monthly' );
+
+	const transformedDomains = allDomains.map( createSiteDomainObject );
+	const [ clipboardCopied, setClipboardCopied ] = useState( false );
+	const clipboardButtonEl = useRef( null );
+	const hasCustomDomain = Boolean(
+		transformedDomains.find( ( domain ) => ! domain.isWPCOMDomain )
+	);
 
 	useEffect( () => {
 		// remove the celebrateLaunch URL param without reloading the page as soon as the modal loads
@@ -19,6 +31,57 @@ function CelebrateLaunchModal( { setModalIsOpen, site } ) {
 		);
 	}, [] );
 
+	function renderUpsellContent() {
+		let contentElement;
+		let buttonText;
+		let buttonHref;
+
+		if ( ! isPaidPlan && ! hasCustomDomain ) {
+			contentElement = (
+				<p>
+					{ translate(
+						'Supercharge your website with a {{strong}}custom address{{/strong}} that matches your blog, brand, or business.',
+						{ components: { strong: <strong /> } }
+					) }
+				</p>
+			);
+			buttonText = translate( 'Claim your domain' );
+			buttonHref = `/domains/add/${ site.slug }`;
+		} else if ( isPaidPlan && isBilledMonthly && ! hasCustomDomain ) {
+			contentElement = (
+				<p>
+					{ translate(
+						'Interested in a custom domain? It’s free for the first year when you switch to annual billing.'
+					) }
+				</p>
+			);
+			buttonText = translate( 'Claim your domain' );
+			buttonHref = `/domains/add/${ site.slug }`;
+		} else if ( isPaidPlan && ! hasCustomDomain ) {
+			contentElement = (
+				<p>
+					{ translate(
+						'Your paid plan includes a domain name {{strong}}free for one year{{/strong}}. Choose one that’s easy to remember and even easier to share.',
+						{ components: { strong: <strong /> } }
+					) }
+				</p>
+			);
+			buttonText = translate( 'Claim your free domain' );
+			buttonHref = `/domains/add/${ site.slug }`;
+		} else if ( isPaidPlan && hasCustomDomain ) {
+			return null;
+		}
+
+		return (
+			<div className="launched__modal-upsell">
+				<div className="launched__modal-upsell-content">{ contentElement }</div>
+				<Button isLarge isPrimary href={ buttonHref }>
+					<span>{ buttonText }</span>
+				</Button>
+			</div>
+		);
+	}
+
 	return (
 		<Modal onRequestClose={ () => setModalIsOpen( false ) } className="launched__modal">
 			<ConfettiAnimation />
@@ -28,25 +91,42 @@ function CelebrateLaunchModal( { setModalIsOpen, site } ) {
 						{ translate( 'Congrats, your site is live!' ) }
 					</h1>
 					<p className="launched__modal-body">
-						{ translate(
-							'Your site is live. Now you can head over to your site and share it with the world or keep working on it.'
-						) }
+						{ translate( 'Now you can head over to your site and share it with the world.' ) }
 					</p>
 				</div>
 				<div className="launched__modal-actions">
 					<div className="launched__modal-site">
-						<p className="launched__modal-domain">{ site.slug }</p>
+						<div className="launched__modal-domain">
+							<p className="launched__modal-domain-text">{ site.slug }</p>
+							<ClipboardButton
+								aria-label={ translate( 'Copy URL' ) }
+								text={ site.slug }
+								className="launchpad__clipboard-button"
+								borderless
+								compact
+								onCopy={ () => setClipboardCopied( true ) }
+								onMouseLeave={ () => setClipboardCopied( false ) }
+								ref={ clipboardButtonEl }
+							>
+								<Gridicon icon="clipboard" />
+							</ClipboardButton>
+							<Tooltip
+								context={ clipboardButtonEl.current }
+								isVisible={ clipboardCopied }
+								position="top"
+							>
+								{ translate( 'Copied to clipboard!' ) }
+							</Tooltip>
+						</div>
 
-						<Button isPrimary href={ site.URL } target="_blank">
-							{ translate( 'Visit site' ) }
+						<Button href={ site.URL } target="_blank" className="launched__modal-view-site">
+							<Gridicon icon="domains" size={ 18 } />
+							<span className="launched__modal-view-site-text">{ translate( 'View site' ) }</span>
 						</Button>
 					</div>
-					<Button className="launched__modal-customize" href={ `/domains/add/${ site.slug }` }>
-						<Gridicon icon="domains" size={ 16 } />
-						<span>{ translate( 'Customize your domain' ) }</span>
-					</Button>
 				</div>
 			</div>
+			{ renderUpsellContent() }
 		</Modal>
 	);
 }
