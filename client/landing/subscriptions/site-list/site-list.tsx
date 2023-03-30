854fc81bde6b42567debd765efdbe18381a9a53c
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2023-03-30 17:04:04 +0200

    Remove unnecessary `useSiteSubscriptionsQueryResponse` type definition

--- client/landing/subscriptions/site-list/site-list.tsx
+++ client/landing/subscriptions/site-list/site-list.tsx
@@ -4,7 +4,7 @@ import './styles.scss';
 import type { SiteSubscription } from '@automattic/data-stores/src/reader/types';
 
 type SiteListProps = {
-	sites: SiteSubscription[];
+	sites?: SiteSubscription[];
 };
 
 export default function SiteList( { sites }: SiteListProps ) {
--- client/landing/subscriptions/tab-views/sites.tsx
+++ client/landing/subscriptions/tab-views/sites.tsx
@@ -1,14 +1,8 @@
 import { SubscriptionManager } from '@automattic/data-stores';
 import SiteList from '../site-list/site-list';
-import type { SiteSubscription } from '@automattic/data-stores/src/reader/types';
-
-type useSiteSubscriptionsQueryResponse = {
-	data: SiteSubscription[];
-};
 
 export default function SitesView() {
-	const { data: sites } =
-		SubscriptionManager.useSiteSubscriptionsQuery() as useSiteSubscriptionsQueryResponse;
+	const { data: sites } = SubscriptionManager.useSiteSubscriptionsQuery();
 
 	return <SiteList sites={ sites } />;
 }
