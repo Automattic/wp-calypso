57dfae5ad23b1f78b5eb15992f1de6f532d1be48
okmttdhr <okmttdhr@users.noreply.github.com>
2023-03-30 14:40:36 +0900

    Make arrow icon accessible to RTL language

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/screen-category-list.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/screen-category-list.tsx
@@ -114,7 +114,11 @@ const ScreenCategoryList = ( {
 							} }
 						>
 							<span>{ label }</span>
-							<Icon icon={ chevronRight } size={ 24 } />
+							<Icon
+								className="screen-category-list__arrow-icon"
+								icon={ chevronRight }
+								size={ 24 }
+							/>
 						</Button>
 					);
 				} ) }
