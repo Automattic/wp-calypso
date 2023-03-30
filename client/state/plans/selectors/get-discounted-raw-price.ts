e256bbbb32a20a34f36cb5e024eeb9487c5c2cb9
Payton Swick <payton@foolord.com>
2023-03-30 14:42:05 -0400

    Convert getDiscountedRawPrice to TypeScript

--- client/state/plans/selectors/get-discounted-raw-price.ts
+++ client/state/plans/selectors/get-discounted-raw-price.ts
@@ -1,17 +1,20 @@
 import { calculateMonthlyPriceForPlan } from '@automattic/calypso-products';
 import { getPlan } from './plan';
+import type { IAppState } from 'calypso/state/types';
 
 import 'calypso/state/plans/init';
 
-/**
- * Returns a plan price
- *
- * @param  {Object}  state     global state
- * @param  {number}  productId the plan productId
- * @param  {boolean} isMonthly if true, returns monthly price
- * @returns {number|null}  plan price
- */
-export function getDiscountedRawPrice( state, productId, isMonthly = false ) {
+export function getDiscountedRawPrice(
+	state: IAppState,
+	productId: number,
+
+	/**
+	 * If true, attempt to calculate and return the monthly price. Note that this
+	 * is not precise as it relies on float division and could have rounding
+	 * errors.
+	 */
+	returnMonthly = false
+): number | null {
 	const plan = getPlan( state, productId );
 	const rawPrice = plan?.raw_price ?? -1;
 	const origCost = plan?.orig_cost ?? -1;
@@ -19,8 +22,11 @@ export function getDiscountedRawPrice( state, productId, isMonthly = false ) {
 	if ( rawPrice < 0 || origCost < 0 ) {
 		return null;
 	}
+	if ( ! plan ) {
+		return null;
+	}
 
-	return isMonthly
+	return returnMonthly
 		? calculateMonthlyPriceForPlan( plan.product_slug, plan.raw_price )
 		: plan.raw_price;
 }
