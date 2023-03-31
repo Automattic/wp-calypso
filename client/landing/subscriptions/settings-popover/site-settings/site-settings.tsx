31c6d6bcd0433bbf17b5d9f1e6fa5f19bd031c40
Paulo Cruz <paulo.cruz@automattic.com>
2023-03-31 13:36:04 -0300

    Apply DeliveryFrequencyInput on SiteSettings

--- client/landing/subscriptions/settings-popover/site-settings/site-settings.tsx
+++ client/landing/subscriptions/settings-popover/site-settings/site-settings.tsx
@@ -1,15 +1,25 @@
 import PopoverMenuItem from 'calypso/components/popover-menu/item';
 import Separator from 'calypso/components/popover-menu/separator';
 import SettingsPopover from '../settings-popover';
+import DeliveryFrequencyInput from './delivery-frequency-input';
 import UnfollowSiteButton from './unfollow-site-button';
+import type { SiteSubscriptionDeliveryFrequency } from '@automattic/data-stores/src/reader/types';
 
 type SiteSettingsProps = {
+	deliveryFrequency: SiteSubscriptionDeliveryFrequency;
+	onDeliveryFrequencyChange: ( value: SiteSubscriptionDeliveryFrequency ) => void;
 	onUnfollow: () => void;
 };
 
-const SiteSettings = ( { onUnfollow }: SiteSettingsProps ) => (
+const SiteSettings = ( {
+	deliveryFrequency,
+	onDeliveryFrequencyChange,
+	onUnfollow,
+}: SiteSettingsProps ) => (
 	<SettingsPopover>
-		<PopoverMenuItem itemComponent="div">Delivery Frequency Placeholder</PopoverMenuItem>
+		<PopoverMenuItem itemComponent="div">
+			<DeliveryFrequencyInput value={ deliveryFrequency } onChange={ onDeliveryFrequencyChange } />
+		</PopoverMenuItem>
 		<Separator />
 		<UnfollowSiteButton onUnfollow={ onUnfollow } />
 	</SettingsPopover>
