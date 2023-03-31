32d893027805db3706c6d253d512fa517aeb62de
Payton Swick <payton@foolord.com>
2023-03-31 12:44:23 -0400

    Convert getPlanRawDiscount to TS

--- client/state/sites/plans/selectors/get-plan-raw-discount.ts
+++ client/state/sites/plans/selectors/get-plan-raw-discount.ts
@@ -1,25 +1,38 @@
 import { calculateMonthlyPriceForPlan } from '@automattic/calypso-products';
 import { getSitePlan } from 'calypso/state/sites/plans/selectors/get-site-plan';
 import { isSitePlanDiscounted } from 'calypso/state/sites/plans/selectors/is-site-plan-discounted';
+import type { IAppState } from 'calypso/state/types';
 
 /**
  * Returns a plan raw discount. It's the value which was subtracted from the plan's original raw price.
  * Use getPlanDiscountedRawPrice if you need a plan's raw price after applying the discount.
- *
- * @param  {Object}  state        global state
- * @param  {number}  siteId       the site id
- * @param  {string}  productSlug  the plan product slug
- * @param  {boolean} isMonthly    if true, returns monthly price
- * @returns {number}               plan raw discount
  */
-export function getPlanRawDiscount( state, siteId, productSlug, { isMonthly = false } = {} ) {
+export function getPlanRawDiscount(
+	state: IAppState,
+	siteId: number,
+	productSlug: string,
+	{
+		returnMonthly,
+	}: {
+		/**
+		 * If true, attempt to calculate and return the monthly price. Note that this
+		 * is not precise as it relies on float division and could have rounding
+		 * errors.
+		 */
+		returnMonthly?: boolean;
+	} = {}
+) {
 	const plan = getSitePlan( state, siteId, productSlug );
+
+	if ( ! plan ) {
+		return null;
+	}
 
 	if ( ! isSitePlanDiscounted( state, siteId, productSlug ) ) {
 		return null;
 	}
 
-	return isMonthly
-		? calculateMonthlyPriceForPlan( productSlug, plan.rawDiscount )
+	return returnMonthly
+		? calculateMonthlyPriceForPlan( productSlug, parseFloat( plan.rawDiscount ) )
 		: plan.rawDiscount;
 }
