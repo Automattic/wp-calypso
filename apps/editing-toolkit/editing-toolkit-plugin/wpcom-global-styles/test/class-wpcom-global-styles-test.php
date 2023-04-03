05269ff9dbb2d4cc0a8f53f1206aa39a2564e658
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2023-04-03 12:51:31 +0300

    Try to fix the failing test (take 2).

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-global-styles/test/class-wpcom-global-styles-test.php
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-global-styles/test/class-wpcom-global-styles-test.php
@@ -17,17 +17,19 @@ class WPCOM_Global_Styles_Test extends TestCase {
 	 * Tests that Global Styles are blocked in the frontend.
 	 */
 	public function test_wpcom_block_global_styles_frontend() {
+		add_filter( 'wpcom_force_limit_global_styles', '__return_false' );
+
 		switch_theme( 'twentytwentythree' );
 		$user_cpt     = WP_Theme_JSON_Resolver::get_user_data_from_wp_global_styles( wp_get_theme(), true );
 		$decoded_data = json_decode( $user_cpt['post_content'], true );
 		unset( $decoded_data['isGlobalStylesUserThemeJSON'] );
 		$config = $decoded_data;
 
-		$config['styles']['color']['background'] = '#000000';
+		$config['styles']['color']['background'] = 'hotpink';
 
 		$theme_json = apply_filters( 'wp_theme_json_data_user', new WP_Theme_JSON_Data( $config, 'custom' ) );
 
-		$this->assertEquals( '#000000', $theme_json->get_data()['styles']['color']['background'] );
+		$this->assertEquals( 'hotpink', $theme_json->get_data()['styles']['color']['background'] );
 
 		add_filter( 'wpcom_force_limit_global_styles', '__return_true' );
 
