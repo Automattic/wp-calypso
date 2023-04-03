9ec35db90709e3a68da70c816c09eacd6a38cf5b
Paulo Cruz <paulo.cruz@automattic.com>
2023-04-03 10:56:29 -0300

    Subscription Management: Create DeliveryFrequencyInput component (#75180)
    * Update SettingsPopover to use it's own structure to avoid unwanted click behaviors from EllipsisMenu
    
    * Create DeliveryFrequencyInput component
    
    * Apply DeliveryFrequencyInput on SiteSettings
    
    * Integrate DeliveryFrequency mutation
    
    * Add temporary input label
    
    * Update client/landing/subscriptions/settings-popover/site-settings/delivery-frequency-input.tsx
    
    Co-authored-by: Juan Lucha <juanlucha@gmail.com>
    
    ---------
    
    Co-authored-by: Juan Lucha <juanlucha@gmail.com>

--- client/landing/subscriptions/site-list/site-row.tsx
+++ client/landing/subscriptions/site-list/site-row.tsx
@@ -3,7 +3,10 @@ import { SubscriptionManager } from '@automattic/data-stores';
 import { useMemo } from '@wordpress/element';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
 import { SiteSettings } from '../settings-popover';
-import type { SiteSubscription } from '@automattic/data-stores/src/reader/types';
+import type {
+	SiteSubscription,
+	SiteSubscriptionDeliveryFrequency,
+} from '@automattic/data-stores/src/reader/types';
 
 export default function SiteRow( {
 	blog_ID,
@@ -18,7 +21,8 @@ export default function SiteRow( {
 		() => moment( date_subscribed ).format( 'LL' ),
 		[ date_subscribed, moment ]
 	);
-	const deliveryFrequency = delivery_methods?.email?.post_delivery_frequency;
+	const deliveryFrequency = delivery_methods?.email
+		?.post_delivery_frequency as SiteSubscriptionDeliveryFrequency;
 	const hostname = useMemo( () => new URL( url ).hostname, [ url ] );
 	const siteIcon = useMemo( () => {
 		if ( site_icon ) {
@@ -27,6 +31,8 @@ export default function SiteRow( {
 		return <Gridicon className="icon" icon="globe" size={ 48 } />;
 	}, [ site_icon, name ] );
 
+	const { mutate: updateDeliveryFrequency } =
+		SubscriptionManager.useSiteDeliveryFrequencyMutation();
 	const { mutate: unFollow } = SubscriptionManager.useSiteUnfollowMutation();
 
 	return (
@@ -47,7 +53,13 @@ export default function SiteRow( {
 				{ deliveryFrequency }
 			</span>
 			<span className="actions" role="cell">
-				<SiteSettings onUnfollow={ () => unFollow( { blog_id: blog_ID } ) } />
+				<SiteSettings
+					deliveryFrequency={ deliveryFrequency }
+					onDeliveryFrequencyChange={ ( delivery_frequency ) =>
+						updateDeliveryFrequency( { blog_id: blog_ID, delivery_frequency } )
+					}
+					onUnfollow={ () => unFollow( { blog_id: blog_ID } ) }
+				/>
 			</span>
 		</li>
 	);
