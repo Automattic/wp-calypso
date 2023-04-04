1966d979a21a25cc2e67d8a33e550b62881f78ea
Miguel <miguel.san.segundo@automattic.com>
2023-04-04 09:06:16 +0700

    Simple category map

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/hooks/use-patterns-map-by-category.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/hooks/use-patterns-map-by-category.ts
@@ -3,19 +3,15 @@ import type { Pattern, Category } from '../types';
 
 const usePatternsMapByCategory = ( patterns: Pattern[], categories: Category[] ) => {
 	return useMemo( () => {
-		const categoriesMap = categories.reduce( ( map, category ) => {
-			return category.name
-				? {
-						...map,
-						[ category.name ]: [],
-				  }
-				: map;
-		}, {} ) as { [ key: string ]: Pattern[] };
+		const categoriesMap = {} as { [ key: string ]: Pattern[] };
 
 		patterns.forEach( ( pattern ) => {
 			Object.values( pattern.categories ).forEach( ( { slug } ) => {
 				if ( slug ) {
-					categoriesMap[ slug ]?.push( pattern );
+					if ( ! categoriesMap[ slug ] ) {
+						categoriesMap[ slug ] = [];
+					}
+					categoriesMap[ slug ].push( pattern );
 				}
 			} );
 		} );
