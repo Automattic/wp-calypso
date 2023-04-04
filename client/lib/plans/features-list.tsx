257c1036a90e241de8dfba4270ca2aff91847ccd
Valter Lorran <valterlorran@hotmail.com>
2023-04-04 15:22:37 -0300

    Add descriptions to the new features

--- client/lib/plans/features-list.tsx
+++ client/lib/plans/features-list.tsx
@@ -1895,50 +1895,89 @@ export const FEATURES_LIST: FeatureList = {
 	[ FEATURE_MARKETPLACE_SYNC_SOCIAL_MEDIA_INTEGRATION ]: {
 		getSlug: () => FEATURE_MARKETPLACE_SYNC_SOCIAL_MEDIA_INTEGRATION,
 		getTitle: () => i18n.translate( 'Marketplace sync and social media integrations' ),
+		getDescription: () => i18n.translate( 'Sync your store with marketplaces and social media.' ),
 	},
 	[ FEATURE_BACK_IN_STOCK_NOTIFICATIONS ]: {
 		getSlug: () => FEATURE_BACK_IN_STOCK_NOTIFICATIONS,
 		getTitle: () => i18n.translate( 'Back in stock notifications' ),
+		getDescription: () =>
+			i18n.translate( 'Notify customers when an out-of-stock item is back in stock.' ),
 	},
 	[ FEATURE_MARKETING_AUTOMATION ]: {
 		getSlug: () => FEATURE_MARKETING_AUTOMATION,
 		getTitle: () => i18n.translate( 'Marketing automation' ),
+		getDescription: () =>
+			i18n.translate(
+				'Automate marketing campaigns to send targeted and personalized messages to customers.'
+			),
 	},
 	[ FEATURE_AUTOMATED_EMAIL_TRIGGERS ]: {
 		getSlug: () => FEATURE_AUTOMATED_EMAIL_TRIGGERS,
 		getTitle: () => i18n.translate( 'Automated email triggers' ),
+		getDescription: () =>
+			i18n.translate(
+				'Set up automatic emails triggered by customer behavior, such as abandoned carts or completed purchases.'
+			),
 	},
 	[ FEATURE_CART_ABANDONMENT_EMAILS ]: {
 		getSlug: () => FEATURE_CART_ABANDONMENT_EMAILS,
 		getTitle: () => i18n.translate( 'Cart abandonment emails' ),
+		getDescription: () =>
+			i18n.translate(
+				'Send reminder emails to customers who have abandoned items in their cart to encourage them to complete their purchase.'
+			),
 	},
 	[ FEATURE_REFERRAL_PROGRAMS ]: {
 		getSlug: () => FEATURE_REFERRAL_PROGRAMS,
 		getTitle: () => i18n.translate( 'Referral programs' ),
+		getDescription: () =>
+			i18n.translate(
+				'Encourage existing customers to refer new customers by offering rewards or incentives.'
+			),
 	},
 	[ FEATURE_CUSTOMER_BIRTHDAY_EMAILS ]: {
 		getSlug: () => FEATURE_CUSTOMER_BIRTHDAY_EMAILS,
 		getTitle: () => i18n.translate( 'Customer birthday emails' ),
+		getDescription: () =>
+			i18n.translate(
+				'Send personalized birthday emails to customers with exclusive discounts or promotions.'
+			),
 	},
 	[ FEATURE_LOYALTY_POINTS_PROGRAMS ]: {
 		getSlug: () => FEATURE_LOYALTY_POINTS_PROGRAMS,
 		getTitle: () => i18n.translate( 'Loyalty points programs' ),
+		getDescription: () =>
+			i18n.translate(
+				'Reward customers for repeat purchases or other actions with loyalty points that can be redeemed for discounts or other benefits.'
+			),
 	},
 	[ FEATURE_OFFER_BULK_DISCOUNTS ]: {
 		getSlug: () => FEATURE_OFFER_BULK_DISCOUNTS,
 		getTitle: () => i18n.translate( 'Offer bulk discounts' ),
+		getDescription: () =>
+			i18n.translate( 'Offer discounts for customers who purchase multiple items at once.' ),
 	},
 	[ FEATURE_RECOMMEND_ADD_ONS ]: {
 		getSlug: () => FEATURE_RECOMMEND_ADD_ONS,
 		getTitle: () => i18n.translate( 'Recommend add-ons' ),
+		getDescription: () =>
+			i18n.translate(
+				'Recommend additional products to customers based on their purchase history.'
+			),
 	},
 	[ FEATURE_ASSEMBLED_PRODUCTS_AND_KITS ]: {
 		getSlug: () => FEATURE_ASSEMBLED_PRODUCTS_AND_KITS,
 		getTitle: () => i18n.translate( 'Assembled products and kits' ),
+		getDescription: () =>
+			i18n.translate( 'Sell products that are assembled from multiple components.' ),
 	},
 	[ FEATURE_MIN_MAX_ORDER_QUANTITY ]: {
 		getSlug: () => FEATURE_MIN_MAX_ORDER_QUANTITY,
 		getTitle: () => i18n.translate( 'Minimum/maximum order quantity' ),
+		getDescription: () =>
+			i18n.translate(
+				'Set minimum and maximum quantity limits for orders to prevent over-ordering or under-ordering.'
+			),
 	},
 	[ FEATURE_CUSTOM_STORE ]: {
 		getSlug: () => FEATURE_CUSTOM_STORE,
