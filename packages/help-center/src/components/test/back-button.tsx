e4af9baccf282cc48f460bd5ec777892e6904f4b
Maciej Grabowski <littlesoldiergraba@gmail.com>
2023-04-05 12:47:42 +0200

    Fix help-center tests

--- packages/help-center/src/components/test/back-button.tsx
+++ packages/help-center/src/components/test/back-button.tsx
@@ -11,7 +11,7 @@ import { BackButton } from '../back-button';
 const mockNavigate = jest.fn();
 jest.mock( 'react-router-dom', () => ( {
 	...jest.requireActual( 'react-router-dom' ),
-	useNavigate: mockNavigate,
+	useNavigate: () => mockNavigate,
 } ) );
 
 describe( 'BackButton', () => {
