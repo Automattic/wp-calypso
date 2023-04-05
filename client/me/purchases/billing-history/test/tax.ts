085f19e116916085ea1ac182d4c1601e5480b652
Payton Swick <payton@foolord.com>
2023-04-05 15:19:19 -0400

    Purchases: Use formatCurrency for receipts (#75252)
    * Add new integer properties to BillingTransaction
    
    * Use integer amounts and formatCurrency for all receipt prices
    
    * Update tests to include new properties

--- client/me/purchases/billing-history/test/tax.ts
+++ client/me/purchases/billing-history/test/tax.ts
@@ -4,17 +4,81 @@
 
 import { render, screen } from '@testing-library/react';
 import { translate } from 'i18n-calypso';
+import {
+	BillingTransaction,
+	BillingTransactionItem,
+} from 'calypso/state/billing-transactions/types';
 import { renderTransactionAmount, transactionIncludesTax } from '../utils';
+
+const mockTransaction: BillingTransaction = {
+	currency: 'USD',
+	address: '',
+	amount: '',
+	amount_integer: 0,
+	tax_country_code: '',
+	cc_email: '',
+	cc_name: '',
+	cc_num: '',
+	cc_type: '',
+	credit: '',
+	date: '',
+	desc: '',
+	icon: '',
+	id: '',
+	items: [],
+	org: '',
+	pay_part: '',
+	pay_ref: '',
+	service: '',
+	subtotal: '',
+	subtotal_integer: 0,
+	support: '',
+	tax: '',
+	tax_integer: 0,
+	url: '',
+};
+
+const mockItem: BillingTransactionItem = {
+	id: '',
+	type: '',
+	type_localized: '',
+	domain: '',
+	site_id: '',
+	subtotal: '',
+	subtotal_integer: 0,
+	tax_integer: 0,
+	amount_integer: 0,
+	tax: '',
+	amount: '',
+	raw_subtotal: 0,
+	raw_tax: 0,
+	raw_amount: 0,
+	currency: '',
+	licensed_quantity: null,
+	new_quantity: null,
+	product: '',
+	product_slug: '',
+	variation: '',
+	variation_slug: '',
+	months_per_renewal_interval: 0,
+	wpcom_product_slug: '',
+};
 
 describe( 'transactionIncludesTax', () => {
 	test( 'returns true for a transaction with tax', () => {
-		const transaction = {
+		const transaction: BillingTransaction = {
+			...mockTransaction,
 			subtotal: '$36.00',
 			tax: '$2.48',
 			amount: '$38.48',
+			subtotal_integer: 3600,
+			tax_integer: 248,
+			amount_integer: 3848,
 			items: [
 				{
+					...mockItem,
 					raw_tax: 2.48,
+					tax_integer: 248,
 				},
 			],
 		};
@@ -24,9 +88,16 @@ describe( 'transactionIncludesTax', () => {
 
 	test( 'returns false for a transaction without tax values', () => {
 		const transaction = {
+			...mockTransaction,
 			subtotal: '$36.00',
 			amount: '$38.48',
-			items: [ {} ],
+			subtotal_integer: 3600,
+			amount_integer: 3848,
+			items: [
+				{
+					...mockItem,
+				},
+			],
 		};
 
 		expect( transactionIncludesTax( transaction ) ).toBe( false );
@@ -34,11 +105,15 @@ describe( 'transactionIncludesTax', () => {
 
 	test( 'returns false for a transaction with zero tax values', () => {
 		const transaction = {
+			...mockTransaction,
 			subtotal: '$36.00',
 			tax: '$0.00',
 			amount: '$38.48',
+			subtotal_integer: 3600,
+			amount_integer: 3848,
 			items: [
 				{
+					...mockItem,
 					raw_tax: 0,
 				},
 			],
@@ -48,11 +123,16 @@ describe( 'transactionIncludesTax', () => {
 
 	test( 'returns false for a transaction without zero tax values in another currency', () => {
 		const transaction = {
+			...mockTransaction,
+			currency: 'EUR',
 			subtotal: '€36.00',
 			tax: '€0.00',
 			amount: '€38.48',
+			subtotal_integer: 3600,
+			amount_integer: 3848,
 			items: [
 				{
+					...mockItem,
 					raw_tax: 0,
 				},
 			],
@@ -64,93 +144,126 @@ describe( 'transactionIncludesTax', () => {
 
 test( 'tax shown if available', () => {
 	const transaction = {
+		...mockTransaction,
 		subtotal: '$36.00',
 		tax: '$2.48',
 		amount: '$38.48',
+		subtotal_integer: 3600,
+		tax_integer: 248,
+		amount_integer: 3848,
 		items: [
 			{
+				...mockItem,
 				raw_tax: 2.48,
+				tax_integer: 248,
 			},
 		],
 	};
 
-	render( renderTransactionAmount( transaction, { translate } ) );
+	render( renderTransactionAmount( transaction, { translate } ) as any );
 	expect( screen.getByText( /tax/ ) ).toBeInTheDocument();
 } );
 
 test( 'tax includes', () => {
 	const transaction = {
+		...mockTransaction,
 		subtotal: '$36.00',
 		tax: '$2.48',
 		amount: '$38.48',
+		subtotal_integer: 3600,
+		tax_integer: 248,
+		amount_integer: 3848,
 		items: [
 			{
+				...mockItem,
 				raw_tax: 2.48,
+				tax_integer: 248,
 			},
 		],
 	};
 
-	render( renderTransactionAmount( transaction, { translate, addingTax: false } ) );
+	render( renderTransactionAmount( transaction, { translate, addingTax: false } ) as any );
 	expect( screen.getByText( `(includes ${ transaction.tax } tax)` ) ).toBeInTheDocument();
 } );
 
 test( 'tax adding', () => {
 	const transaction = {
+		...mockTransaction,
 		subtotal: '$36.00',
 		tax: '$2.48',
 		amount: '$38.48',
+		subtotal_integer: 3600,
+		tax_integer: 248,
+		amount_integer: 3848,
 		items: [
 			{
+				...mockItem,
 				raw_tax: 2.48,
+				tax_integer: 248,
 			},
 		],
 	};
 
-	render( renderTransactionAmount( transaction, { translate, addingTax: true } ) );
+	render( renderTransactionAmount( transaction, { translate, addingTax: true } ) as any );
 	expect( screen.getByText( `(+${ transaction.tax } tax)` ) ).toBeInTheDocument();
 } );
 
 test( 'tax includes with localized tax name', () => {
 	const transaction = {
+		...mockTransaction,
 		subtotal: '$36.00',
 		tax: '$2.48',
 		amount: '$38.48',
 		tax_country_code: 'GB',
+		subtotal_integer: 3600,
+		tax_integer: 248,
+		amount_integer: 3848,
 		items: [
 			{
+				...mockItem,
 				raw_tax: 2.48,
+				tax_integer: 248,
 			},
 		],
 	};
 
-	render( renderTransactionAmount( transaction, { translate, addingTax: false } ) );
+	render( renderTransactionAmount( transaction, { translate, addingTax: false } ) as any );
 	expect( screen.getByText( `(includes ${ transaction.tax } VAT)` ) ).toBeInTheDocument();
 } );
 
 test( 'tax adding with localized tax name', () => {
 	const transaction = {
+		...mockTransaction,
 		subtotal: '$36.00',
 		tax: '$2.48',
 		amount: '$38.48',
 		tax_country_code: 'GB',
+		subtotal_integer: 3600,
+		tax_integer: 248,
+		amount_integer: 3848,
 		items: [
 			{
+				...mockItem,
 				raw_tax: 2.48,
+				tax_integer: 248,
 			},
 		],
 	};
 
-	render( renderTransactionAmount( transaction, { translate, addingTax: true } ) );
+	render( renderTransactionAmount( transaction, { translate, addingTax: true } ) as any );
 	expect( screen.getByText( `(+${ transaction.tax } VAT)` ) ).toBeInTheDocument();
 } );
 
 test( 'tax hidden if not available', () => {
 	const transaction = {
+		...mockTransaction,
 		subtotal: '$36.00',
 		tax: '$0.00',
 		amount: '$36.00',
-		items: [ {} ],
+		subtotal_integer: 3600,
+		amount_integer: 3600,
+		items: [ { ...mockItem } ],
 	};
 
-	expect( renderTransactionAmount( transaction, { translate } ) ).toEqual( '$36.00' );
+	expect( renderTransactionAmount( transaction, { translate } ) ).toEqual( '$36' );
 } );
