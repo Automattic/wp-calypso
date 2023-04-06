afc6392ccbd4f490e0bf93448ada8f9fb9c77650
Yashwin <yashwinpoojary@gmail.com>
2023-04-06 13:46:09 +0530

    Create SiteTableTooltip component

---
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-table-expand/site-table-tooltip.tsx
@@ -0,0 +1,28 @@
+import { useTranslate } from 'i18n-calypso';
+import { forwardRef } from 'react';
+import Tooltip from 'calypso/components/tooltip';
+
+interface Props {
+	siteId: number;
+	showTooltip: boolean;
+}
+
+const SiteTableTooltip = ( { siteId, showTooltip }: Props, ref: any ) => {
+	const translate = useTranslate();
+
+	const tooltipId = `site-table-expand-tooltip-${ siteId }`;
+
+	return (
+		<Tooltip
+			id={ tooltipId }
+			context={ ref.current }
+			isVisible={ showTooltip }
+			position="bottom left"
+			className="sites-overview__tooltip"
+		>
+			{ translate( 'Click here for more info' ) }
+		</Tooltip>
+	);
+};
+
+export default forwardRef( SiteTableTooltip );
