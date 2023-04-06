9d39168985b631a0338fc40623f9ffdf58ec00c4
Marin Atanasov <tyxla@abv.bg>
2023-04-06 14:47:07 +0300

    Plans Grid: Remove unused components

--- packages/plans-grid/src/index.ts
+++ packages/plans-grid/src/index.ts
@@ -1,4 +1,2 @@
 export { default as PlansIntervalToggle } from './plans-interval-toggle';
 export type { PlansIntervalToggleProps } from './plans-interval-toggle';
-export { default } from './plans-grid';
-export type { Props } from './plans-grid';
