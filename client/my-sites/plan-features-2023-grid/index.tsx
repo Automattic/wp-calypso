cba7c9a2489dee05cbb8c440549c9baffaf12363
Nelson <nelsonec87@gmail.com>
2023-04-07 12:49:57 +0200

    Use helper function

--- client/my-sites/plan-features-2023-grid/index.tsx
+++ client/my-sites/plan-features-2023-grid/index.tsx
@@ -17,7 +17,6 @@ import {
 	PLAN_ENTERPRISE_GRID_WPCOM,
 	isPremiumPlan,
 	PLAN_BIENNIAL_PERIOD,
-	PLAN_WOOEXPRESS_PLUS,
 	isWooExpressMediumPlan,
 	isWooExpressPlan,
 	PlanSlug,
@@ -795,7 +794,7 @@ export class PlanFeatures2023Grid extends Component<
 		const planProperties = planPropertiesObj.filter(
 			( properties ) =>
 				! isWpcomEnterpriseGridPlan( properties.planName ) &&
-				! ( properties.planName === PLAN_WOOEXPRESS_PLUS )
+				! isWooExpressPlusPlan( properties.planName )
 		);
 
 		return planProperties
