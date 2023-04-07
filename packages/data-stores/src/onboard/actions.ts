7429b47575c687cb55e3acef24102d5a741e7445
Marin Atanasov <8436925+tyxla@users.noreply.github.com>
2023-04-07 12:50:59 +0300

    Plans Grid: Remove unused components and data stores (#75388)
    * Plans Grid: Remove unused components
    
    * Cleanup unused dependencies
    
    * Remove unused WPCOMFeatures data store
    
    * Cleanup some unused styles and constants
    
    * Move types along

--- packages/data-stores/src/onboard/actions.ts
+++ packages/data-stores/src/onboard/actions.ts
@@ -3,11 +3,11 @@ import { dispatch, select } from '@wordpress/data-controls';
 import { __ } from '@wordpress/i18n';
 import { STORE_KEY as SITE_STORE } from '../site';
 import { CreateSiteParams, Visibility, NewSiteBlogDetails } from '../site/types';
-import { FeatureId } from '../wpcom-features/types';
 import { SiteGoal, STORE_KEY } from './constants';
 import { ProfilerData } from './types';
 import type { State } from '.';
 import type { DomainSuggestion } from '../domain-suggestions';
+import type { FeatureId } from '../shared-types';
 // somewhat hacky, but resolves the circular dependency issue
 import type { Design, FontPair, StyleVariation } from '@automattic/design-picker/src/types';
 import type { MinimalRequestCartProduct } from '@automattic/shopping-cart';
