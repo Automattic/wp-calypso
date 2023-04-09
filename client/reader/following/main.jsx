ecf921a73b2782fa0cd1dac5b5afbcce5f8b411c
Grant Kinney <grant.kinney@automattic.com>
2023-04-09 14:03:39 -0500

    Adds styles

--- client/reader/following/main.jsx
+++ client/reader/following/main.jsx
@@ -2,6 +2,7 @@ import IsoloatedEditor from '@automattic/isolated-block-editor';
 import SuggestionProvider from 'calypso/reader/search-stream/suggestion-provider';
 import Stream from 'calypso/reader/stream';
 import FollowingIntro from './intro';
+import '@automattic/isolated-block-editor/build-browser/core.css';
 import './style.scss';
 
 function FollowingStream( { ...props } ) {
