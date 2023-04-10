5b3789c952e1529e9dbb3510685e4a3788ec33de
Jasper Kang <jasper.kang@automattic.com>
2023-04-11 10:05:51 +1200

    Odyssey: more robust locale files loading (#75363)
    * more robust locale loading
    
    * remove Moment Locale Provider
    
    * Pass it to the next catch block if the language code is the same as the locale slug.
    
    * renamed getLanguageCodeFromLocale

--- apps/odyssey-stats/src/lib/set-locale.js
+++ apps/odyssey-stats/src/lib/set-locale.js
@@ -4,16 +4,46 @@ import moment from 'moment';
 
 const debug = debugFactory( 'apps:odyssey' );
 
-const DEFAULT_LOCALE = 'en';
+const DEFAULT_LANGUAGE = 'en';
+const DEFAULT_MOMENT_LOCALE = 'en';
+const ALWAYS_LOAD_WITH_LOCALE = [ 'zh' ];
 
-const loadMomentLocale = ( localeSlug ) => {
-	return import(
-		/* webpackChunkName: "moment-locale-[request]", webpackInclude: /\.js$/ */ `moment/locale/${ localeSlug }`
-	).then( () => moment.locale( localeSlug ) );
+const getLanguageCodeFromLocale = ( localeSlug ) => {
+	if ( localeSlug.indexOf( '-' ) > -1 ) {
+		return localeSlug.split( '-' )[ 0 ];
+	}
+	return localeSlug;
 };
 
-const loadLanguageFile = ( localeSlug ) => {
-	const url = `https://widgets.wp.com/odyssey-stats/v1/languages/${ localeSlug }-v1.1.json`;
+const loadMomentLocale = ( localeSlug, languageCode ) => {
+	return import( `moment/locale/${ localeSlug }` )
+		.catch( ( error ) => {
+			debug(
+				`Encountered an error loading moment locale file for ${ localeSlug }. Falling back to language datetime format.`,
+				error
+			);
+			// Fallback 1 to the language code.
+			if ( localeSlug !== languageCode ) {
+				localeSlug = languageCode;
+				return import( `moment/locale/${ localeSlug }` );
+			}
+			// Pass it to the next catch block if the language code is the same as the locale slug.
+			return Promise.reject( error );
+		} )
+		.catch( ( error ) => {
+			debug(
+				`Encountered an error loading moment locale file for ${ localeSlug }. Falling back to US datetime format.`,
+				error
+			);
+			// Fallback 2 to the default US date time format.
+			// Interestingly `en` here represents `en-us` locale.
+			localeSlug = DEFAULT_MOMENT_LOCALE;
+		} )
+		.then( () => moment.locale( localeSlug ) );
+};
+
+const loadLanguageFile = ( languageFileName ) => {
+	const url = `https://widgets.wp.com/odyssey-stats/v1/languages/${ languageFileName }-v1.1.json`;
 
 	return globalThis.fetch( url ).then( ( response ) => {
 		if ( response.ok ) {
@@ -28,16 +58,25 @@ const loadLanguageFile = ( localeSlug ) => {
 };
 
 export default ( localeSlug ) => {
-	if ( localeSlug === DEFAULT_LOCALE ) {
-		return Promise.resolve();
+	const languageCode = getLanguageCodeFromLocale( localeSlug );
+
+	// Load tranlation file if it's not English.
+	if ( languageCode !== DEFAULT_LANGUAGE ) {
+		const languageFileName = ALWAYS_LOAD_WITH_LOCALE.includes( languageCode )
+			? localeSlug
+			: languageCode;
+		// We don't have to wait for the language file to load before rendering the page, because i18n is using hooks to update translations.
+		loadLanguageFile( languageFileName )
+			.then( () => debug( `Loaded locale files for ${ languageCode } successfully.` ) )
+			.catch( ( error ) =>
+				debug(
+					`Encountered an error loading locale file for ${ languageCode }. Falling back to English.`,
+					error
+				)
+			);
 	}
 
-	return Promise.all( loadLanguageFile( localeSlug ), loadMomentLocale( localeSlug ) )
-		.then( () => debug( `Loaded locale file for ${ localeSlug } successfully.` ) )
-		.catch( ( error ) =>
-			debug(
-				`Encountered an error loading locale file for ${ localeSlug }. Falling back to English.`,
-				error
-			)
-		);
+	// We have to wait for moment locale to load before rendering the page, because otherwise the rendered date time wouldn't get re-rendered.
+	// This could be improved in the future with hooks.
+	return loadMomentLocale( localeSlug, languageCode );
 };
