79865f783458b0fb4fe015af2c965cad1ee1ed3b
augurlu <ali.ugurlu@automattic.com>
2023-04-10 11:38:46 +0300

    Update discount amount
    This is a quick patch to correct the discount amount

--- client/my-sites/plans/jetpack-plans/faq/index.tsx
+++ client/my-sites/plans/jetpack-plans/faq/index.tsx
@@ -146,7 +146,7 @@ const JetpackFAQ: FC = () => {
 							className="jetpack-faq__section"
 						>
 							{ translate(
-								'Anyone with at least five websites can join our licensing platform and enjoy a 25% discount across all Jetpack products! You can learn more about our {{agenciesLink}}licensing platform and agency program here{{/agenciesLink}}.',
+								'Anyone with at least five websites can join our licensing platform and enjoy up to 60% discount across all Jetpack products! You can learn more about our {{agenciesLink}}licensing platform and agency program here{{/agenciesLink}}.',
 								{
 									components: { agenciesLink: getAgenciesLink() },
 								}
