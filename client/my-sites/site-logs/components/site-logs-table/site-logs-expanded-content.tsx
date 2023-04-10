e1ff91c61a33322433b4b99749924b4bad19efac
mpkelly <kikemelly@gmail.com>
2023-04-10 14:19:23 +0700

    Use render function

--- client/my-sites/site-logs/components/site-logs-table/site-logs-expanded-content.tsx
+++ client/my-sites/site-logs/components/site-logs-table/site-logs-expanded-content.tsx
@@ -14,10 +14,30 @@ export default function SiteLogsExpandedContent( { log }: Props ) {
 						<strong>{ key }</strong>
 					</td>
 					<td>
-						<div className="site-logs-table__expanded-content-info">{ log[ key ] || '-' }</div>
+						<div className="site-logs-table__expanded-content-info">
+							{ renderCell( key, log[ key ] ) }
+						</div>
 					</td>
 				</tr>
 			) ) }
 		</div>
 	);
+}
+
+function renderCell( column: string, value: unknown ) {
+	if ( value === null || value === undefined || value === '' ) {
+		return '-';
+	}
+
+	switch ( typeof value ) {
+		case 'boolean':
+			return value.toString();
+
+		case 'number':
+		case 'string':
+			return value;
+
+		default:
+			JSON.stringify( value );
+	}
 }
