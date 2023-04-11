01d47ac5b2b5d68cca4ae4f05437774fd94779b4
Markus Dobmann <m.do@posteo.net>
2023-04-11 11:39:32 +0200

    leverage react-query key hashing

--- client/components/language-picker/site-language-picker.jsx
+++ client/components/language-picker/site-language-picker.jsx
@@ -35,7 +35,7 @@ const SiteLanguagePicker = ( { languages: origLanguages, ...restProps } ) => {
 		error,
 		isLoading,
 	} = useQuery(
-		[ 'wporg-translations-' + wpVersion ],
+		[ 'wporg-translations', wpVersion ],
 		async () => fetchWporgTranslationsList( wpVersion ),
 		{ enabled: !! siteIsJetpack }
 	);
