0a5e62cc630ca3029e30df9b6a542056fd15e615
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-04-11 08:39:50 -0400

    Remove preview type from TS types

--- packages/social-previews/src/types.ts
+++ packages/social-previews/src/types.ts
@@ -1,19 +1,9 @@
-import {
-	AUTO_SHARED_SOCIAL_POST_PREVIEW,
-	AUTO_SHARED_LINK_PREVIEW,
-	DEFAULT_LINK_PREVIEW,
-} from './constants';
-
 export type PreviewProps = {
 	url: string;
 	title: string;
 	description?: string;
 	customText?: string;
 	image?: string;
-	previewType:
-		| typeof AUTO_SHARED_SOCIAL_POST_PREVIEW
-		| typeof AUTO_SHARED_LINK_PREVIEW
-		| typeof DEFAULT_LINK_PREVIEW;
 };
 
 export type TextFormatter = ( text: string ) => string;
