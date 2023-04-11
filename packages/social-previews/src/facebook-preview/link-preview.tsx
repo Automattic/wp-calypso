919eca47ec609f95627a178af05f02b9cb1573d4
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-04-11 09:22:02 -0400

    Match FB previous various use cases

--- packages/social-previews/src/facebook-preview/link-preview.tsx
+++ packages/social-previews/src/facebook-preview/link-preview.tsx
@@ -1,9 +1,14 @@
-import { useTranslate } from 'i18n-calypso';
+import { __ } from '@wordpress/i18n';
+import { useCallback, useState } from 'react';
+import { TYPE_ARTICLE } from '../constants';
 import { baseDomain, facebookTitle, facebookDescription, facebookCustomText } from './helpers';
 import FacebookPostActions from './post/actions';
 import FacebookPostHeader from './post/header';
 import FacebookPostIcon from './post/icons';
 import type { FacebookPreviewProps } from './types';
+
+const LANDSCAPE_MODE = 'landscape';
+const PORTRAIT_MODE = 'portrait';
 
 const FacebookLinkPreview: React.FC< FacebookPreviewProps > = ( {
 	url,
@@ -12,8 +17,20 @@ const FacebookLinkPreview: React.FC< FacebookPreviewProps > = ( {
 	image,
 	user,
 	customText,
+	type,
 } ) => {
-	const translate = useTranslate();
+	const [ mode, setMode ] = useState( LANDSCAPE_MODE );
+	const isArticle = type === TYPE_ARTICLE;
+	const portraitMode = ( isArticle && ! image ) || mode === PORTRAIT_MODE;
+
+	const getModeClass = useCallback(
+		() => `is-${ portraitMode ? 'portrait' : 'landscape' }`,
+		[ portraitMode ]
+	);
+	const handleImageLoad = useCallback(
+		( { target } ) => setMode( target.width > target.height ? LANDSCAPE_MODE : PORTRAIT_MODE ),
+		[ setMode ]
+	);
 
 	return (
 		<div className="facebook-preview__post">
@@ -22,19 +39,39 @@ const FacebookLinkPreview: React.FC< FacebookPreviewProps > = ( {
 				{ customText && (
 					<p className="facebook-preview__custom-text">{ facebookCustomText( customText ) }</p>
 				) }
-				<div className="facebook-preview__image">
-					{ image && <img alt={ translate( 'Facebook Preview Thumbnail' ) } src={ image } /> }
-				</div>
-				<div className="facebook-preview__body">
-					<div className="facebook-preview__url">{ baseDomain( url ) }</div>
-					<div className="facebook-preview__title">{ facebookTitle( title ) }</div>
-					{ description && (
-						<div className="facebook-preview__description">
-							{ facebookDescription( description ) }
+				<div className={ `facebook-preview__body ${ getModeClass() }` }>
+					{ ( image || isArticle ) && (
+						<div
+							className={ `facebook-preview__image ${
+								image ? '' : 'is-empty'
+							} ${ getModeClass() }` }
+						>
+							{ image && (
+								<img
+									alt={ __( 'Facebook Preview Thumbnail' ) }
+									src={ image }
+									onLoad={ handleImageLoad }
+								/>
+							) }
 						</div>
 					) }
-					<div className="facebook-preview__info">
-						<FacebookPostIcon name="info" />
+					<div className="facebook-preview__text">
+						<div>
+							<div className="facebook-preview__url">{ baseDomain( url ) }</div>
+							<div className="facebook-preview__title">
+								{ facebookTitle( title ) || baseDomain( url ) }
+							</div>
+							<div className="facebook-preview__description">
+								{ description && facebookDescription( description ) }
+								{ isArticle &&
+									! description &&
+									// translators: Default description for a Facebook post
+									__( 'Visit the post for more.' ) }
+							</div>
+							<div className="facebook-preview__info">
+								<FacebookPostIcon name="info" />
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
