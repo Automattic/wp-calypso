23b98f5a9f1ed82acbd9e3de83d38111f1fba361
thedebian <thedebian@users.noreply.github.com>
2023-04-11 14:46:24 +0200

    VideoPress: add plan price on intro page

--- client/landing/stepper/declarative-flow/internals/steps-repository/intro/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/intro/index.tsx
@@ -1,3 +1,4 @@
+import { useLocale } from '@automattic/i18n-utils';
 import {
 	NEWSLETTER_FLOW,
 	ECOMMERCE_FLOW,
@@ -5,9 +6,14 @@ import {
 	FREE_FLOW,
 	isLinkInBioFlow,
 } from '@automattic/onboarding';
+import { useSelect } from '@wordpress/data';
 import { createInterpolateElement, useMemo } from '@wordpress/element';
+import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
+import { PlansSelect } from 'calypso/../packages/data-stores/src';
 import { StepContainer } from 'calypso/../packages/onboarding/src';
+import { useSupportedPlans } from 'calypso/../packages/plans-grid/src/hooks';
+import { PLANS_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import IntroStep, { IntroContent } from './intro';
 import VideoPressIntroModalContent from './videopress-intro-modal-content';
@@ -17,6 +23,43 @@ import './styles.scss';
 
 const useIntroContent = ( flowName: string | null ): IntroContent => {
 	const { __ } = useI18n();
+	const locale = useLocale();
+	const { supportedPlans } = useSupportedPlans( locale, 'ANNUALLY' );
+	const getPlanProduct = useSelect(
+		( select ) => ( select( PLANS_STORE ) as PlansSelect ).getPlanProduct,
+		[]
+	);
+	let videoPressGetStartedText = __( 'A home for all your videos.<br />Play. Roll. Share.' );
+
+	if ( VIDEOPRESS_FLOW === flowName ) {
+		let defaultSupportedPlan = supportedPlans.find( ( plan ) => {
+			return plan.periodAgnosticSlug === 'premium';
+		} );
+		if ( ! defaultSupportedPlan ) {
+			defaultSupportedPlan = supportedPlans.find( ( plan ) => {
+				return plan.periodAgnosticSlug === 'business';
+			} );
+		}
+
+		if ( defaultSupportedPlan ) {
+			const planProductObject = getPlanProduct(
+				defaultSupportedPlan.periodAgnosticSlug,
+				'ANNUALLY'
+			);
+
+			if ( planProductObject ) {
+				// eslint-disable-next-line @wordpress/valid-sprintf
+				videoPressGetStartedText = sprintf(
+					/* translators: Price displayed on VideoPress intro page "Get started" button. First %s is monthly price, second is annual price */
+					__(
+						'A home for all your videos.<br />Play. Roll. Share.<div>Get started for %s (%s billed annually).</div>'
+					),
+					planProductObject.price,
+					planProductObject.annualPrice
+				);
+			}
+		}
+	}
 
 	return useMemo( () => {
 		if ( isLinkInBioFlow( flowName ) ) {
@@ -50,10 +93,10 @@ const useIntroContent = ( flowName: string | null ): IntroContent => {
 
 		if ( flowName === VIDEOPRESS_FLOW ) {
 			return {
-				title: createInterpolateElement(
-					__( 'A home for all your videos.<br />Play. Roll. Share.' ),
-					{ br: <br /> }
-				),
+				title: createInterpolateElement( videoPressGetStartedText, {
+					br: <br />,
+					div: <div className="videopress-intro-pricing" />,
+				} ),
 				buttonText: __( 'Get started' ),
 				modal: {
 					buttonText: __( 'Learn more' ),
@@ -80,7 +123,7 @@ const useIntroContent = ( flowName: string | null ): IntroContent => {
 			),
 			buttonText: __( 'Get started' ),
 		};
-	}, [ flowName, __ ] );
+	}, [ flowName, __, videoPressGetStartedText ] );
 };
 
 const Intro: Step = function Intro( { navigation, flow } ) {
