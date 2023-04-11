aff5896df0fe5197d5eb14927bb49cd7a666e90b
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-04-11 08:50:25 -0400

    Replace forgotten translate calls by __

--- packages/social-previews/src/facebook-preview/index.tsx
+++ packages/social-previews/src/facebook-preview/index.tsx
@@ -1,20 +1,21 @@
-import { useTranslate } from 'i18n-calypso';
+import { __ } from '@wordpress/i18n';
 import FacebookLinkPreview from './link-preview';
 import type { FacebookPreviewProps } from './types';
 
 import './style.scss';
 
 const FacebookPreview: React.FC< FacebookPreviewProps > = ( props ) => {
-	const translate = useTranslate();
-
 	return (
 		<div className="social-preview facebook-preview">
 			<section className="social-preview__section facebook-preview__section">
 				<h3 className="social-preview__section-heading">
-					{ translate( 'Your post', { comment: 'Refers to a social post on Facebook' } ) }
+					{
+						// translators: refers to a social post on Facebook
+						__( 'Your post' )
+					}
 				</h3>
 				<p className="social-preview__section-desc">
-					{ translate( 'This is what your social post will look like on Facebook:' ) }
+					{ __( 'This is what your social post will look like on Facebook:' ) }
 				</p>
 				<FacebookLinkPreview { ...props } />
 			</section>
