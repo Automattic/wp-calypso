4b690212d8a2d42d25ef644189618e093d9a5fc8
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-04-11 08:48:32 -0400

    Add website/article types

--- packages/social-previews/src/facebook-preview/types.ts
+++ packages/social-previews/src/facebook-preview/types.ts
@@ -1,3 +1,4 @@
+import { TYPE_WEBSITE, TYPE_ARTICLE } from '../constants';
 import type { PreviewProps } from '../types';
 
 export type FacebookUser = {
@@ -7,4 +8,5 @@ export type FacebookUser = {
 
 export type FacebookPreviewProps = PreviewProps & {
 	user?: FacebookUser;
+	type?: typeof TYPE_WEBSITE | typeof TYPE_ARTICLE;
 };
