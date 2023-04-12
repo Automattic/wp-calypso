ba3ba458fee1098ad8d028a67c83fe2855b27f17
Jason Moon <jason.moon@automattic.com>
2023-04-12 16:00:21 -0700

    Update stories

--- packages/components/src/highlight-cards/stories/card.stories.jsx
+++ packages/components/src/highlight-cards/stories/card.stories.jsx
@@ -1,15 +1,37 @@
-import { Icon, info } from '@wordpress/icons';
+import { Icon, institution } from '@wordpress/icons';
 import BaseCard from '../base-card';
 import CountCard from '../count-card';
 
 export default { title: 'Highlight Card' };
 
-export const WithBaseCard = () => (
-	<BaseCard heading="Some Heading">
-		<div>Some Body</div>
-	</BaseCard>
+export const BaseCard_ = () => (
+	<>
+		<BaseCard heading={ <div>With Heading and Body</div> }>
+			<div>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+				cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+		</BaseCard>
+		<BaseCard>
+			<div>
+				This card only has a body. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+				eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+				nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+				irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+				anim id est laborum.
+			</div>
+		</BaseCard>
+		<BaseCard heading={ <div>With Only a Heading</div> }></BaseCard>
+	</>
 );
 
-export const WithCountCard = () => (
-	<CountCard heading="Some Heading" icon={ <Icon icon={ info } /> } value="123" />
+export const CountCard_ = () => (
+	<>
+		<CountCard heading="String Value" icon={ <Icon icon={ institution } /> } value="123456789" />
+		<CountCard heading="Number Value" icon={ <Icon icon={ institution } /> } value={ 123456789 } />
+	</>
 );
