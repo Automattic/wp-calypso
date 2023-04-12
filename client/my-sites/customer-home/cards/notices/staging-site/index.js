49afdd32df162526d21de211f968c4ed21016794
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2023-04-12 13:38:45 -0700

    Avoid using a Redux selector when we already have the data

--- client/my-sites/customer-home/cards/notices/staging-site/index.js
+++ client/my-sites/customer-home/cards/notices/staging-site/index.js
@@ -3,33 +3,21 @@ import { createInterpolateElement } from '@wordpress/element';
 import { __, sprintf } from '@wordpress/i18n';
 import { useSelector } from 'react-redux';
 import stagingIllustration from 'calypso/assets/images/customer-home/illustration--staging.svg';
+import { urlToSlug } from 'calypso/lib/url';
 import { NOTICE_STAGING_SITE } from 'calypso/my-sites/customer-home/cards/constants';
 import CelebrateNotice, {
 	CelebrateNoticePlaceholder,
 } from 'calypso/my-sites/customer-home/cards/notices/celebrate-notice';
 import { useProductionSiteDetail } from 'calypso/my-sites/hosting/staging-site-card/use-production-site-detail';
-import { getSiteHomeUrl } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
-
-const DEFAULT_PRODUCTION_DETAILS = {
-	name: '',
-	url: '',
-	id: '',
-};
 
 const StagingSiteNotice = () => {
 	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
-	const {
-		loading,
-		data: { name: productionSiteName, id: productionSiteId } = DEFAULT_PRODUCTION_DETAILS,
-	} = useProductionSiteDetail( siteId );
-
-	const productionHomeUrl = useSelector( ( state ) => getSiteHomeUrl( state, productionSiteId ) );
-	const shouldShowNotice = ! loading && productionSiteName && productionSiteId && productionHomeUrl;
+	const { loading, data: productionSite } = useProductionSiteDetail( siteId );
 
 	return (
 		<>
-			{ shouldShowNotice ? (
+			{ ! loading && productionSite ? (
 				<CelebrateNotice
 					description={ createInterpolateElement(
 						sprintf(
@@ -37,14 +25,14 @@ const StagingSiteNotice = () => {
 							__(
 								'This is a staging site for <a>%s</a> that you can use to try out new plugins and themes, debug and troubleshoot changes, and fine-tune every aspect of your website without worrying about breaking your production site.'
 							),
-							productionSiteName
+							productionSite.name
 						),
 						{
 							a: (
 								<Button
 									style={ { fontSize: 'inherit' } }
 									variant="link"
-									href={ productionHomeUrl }
+									href={ `/home/${ urlToSlug( productionSite.url ) }` }
 								/>
 							),
 						}
