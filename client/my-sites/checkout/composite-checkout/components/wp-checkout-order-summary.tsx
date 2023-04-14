5ae85cd52fd8f9f0f634e1bad60ca597e55d9dba
Philip Jackson <p-jackson@live.com>
2023-04-14 16:25:22 +1200

    Add refund window to hosting flow's checkout summary features list

--- client/my-sites/checkout/composite-checkout/components/wp-checkout-order-summary.tsx
+++ client/my-sites/checkout/composite-checkout/components/wp-checkout-order-summary.tsx
@@ -378,6 +378,7 @@ function CheckoutSummaryFlowFeaturesList( { flowName }: { flowName: string } ) {
 					</CheckoutSummaryFeaturesListItem>
 				);
 			} ) }
+			{ isHostingFlow( flowName ) && <CheckoutSummaryRefundWindows cart={ responseCart } /> }
 		</CheckoutSummaryFeaturesListWrapper>
 	);
 }
