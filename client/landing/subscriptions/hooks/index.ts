76a84cff025125e8263c3c8063090ab947e1826a
Paulo Cruz <paulo.cruz@automattic.com>
2023-04-17 18:47:31 -0300

    Create useSearch hook

--- client/landing/subscriptions/hooks/index.ts
+++ client/landing/subscriptions/hooks/index.ts
@@ -1,1 +1,3 @@
+export { default as usePopoverToggle } from './use-popover-toggle';
+export { default as useSearch } from './use-search';
 export { default as useSubheaderText } from './use-subheader-text';
