22daa8c9293c0398b250799b52c807239b1ded6a
artpi <artur.piszek@gmail.com>
2023-04-17 16:45:44 +0200

    query for ai search

---
+++ packages/data-stores/src/queries/use-jetpack-search-ai.ts
@@ -0,0 +1,32 @@
+import { useQuery } from 'react-query';
+import wpcomRequest from 'wpcom-proxy-request';
+
+type AIResponseURL = {
+	url: string;
+	title: string;
+};
+
+export type JetpackSearchAIResult = {
+	response: string;
+	step: string;
+	urls: AIResponseURL[];
+	terms: string[];
+};
+
+export function useJetpackSearchAIQuery( siteId: number, query: string, stopAt: string ) {
+	return useQuery< JetpackSearchAIResult[] >(
+		[ query ],
+		async () =>
+			await wpcomRequest( {
+				path: `/sites/${ siteId }/jetpack-search/ai/search`,
+				apiNamespace: 'wpcom/v2/',
+				apiVersion: '2',
+				query: `query=${ encodeURIComponent( query ) }&stop_at=${ stopAt }`,
+			} ),
+		{
+			refetchOnWindowFocus: false,
+			keepPreviousData: true,
+			enabled: !! query,
+		}
+	);
+}
