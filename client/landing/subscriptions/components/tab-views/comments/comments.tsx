56d852e43707c689c9274cee4ca81b13199e1c22
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2023-04-17 17:26:05 +0200

    Subscriptions Management: Create usePostSubscriptionsQuery hook (#75775)
    * Add `useCommentSubscriptionsQuery` hook
    
    * Clean up useInfiniteQuery cache key
    
    * Generate the cache key with the help of `useCacheKey`
    
    * Replace "comment" with "post" in types and elsewhere
    
    * Apply `useMemo` on data transformations

--- client/landing/subscriptions/components/tab-views/comments/comments.tsx
+++ client/landing/subscriptions/components/tab-views/comments/comments.tsx
@@ -1,43 +1,26 @@
+import { SubscriptionManager } from '@automattic/data-stores';
 import SearchInput from '@automattic/search';
 import { useTranslate } from 'i18n-calypso';
 import { CommentList } from 'calypso/landing/subscriptions/components/comment-list';
 import { SearchIcon } from 'calypso/landing/subscriptions/components/icons';
+import { Notice } from 'calypso/landing/subscriptions/components/notice';
 import TabView from '../tab-view';
-import type { PostSubscription } from '@automattic/data-stores/src/reader/types';
-
-const posts: PostSubscription[] = [
-	{
-		id: '1',
-		title: 'Alone at the Edge of the World',
-		excerpt:
-			'Susie Goodall wanted to circumnavigate the globe in a sailboat. She did it, but not without a few hiccups along the way.',
-		url: 'https://testsite2022.wordpress.com/2021/03/29/alone-at-the-edge-of-the-world/',
-		site_title: 'Test Site 2022',
-		site_icon: 'https://www.gravatar.com/avatar/',
-		site_url: 'https://testsite2022.wordpress.com',
-		date_subscribed: new Date( '2022-03-29T14:55:53+00:00' ),
-	},
-	{
-		id: '2',
-		title: '50 Years Ago, Stevie Wonder Heard the Future',
-		excerpt:
-			'On the anniversary of the landmark 1972 album “Talking Book,” the singer-songwriter reflects on the making of his masterpiece.',
-		url: 'https://testsite2023.wordpress.com/2021/03/29/50-years-ago-stevie-wonder-heard-the-future/',
-		site_icon: 'https://www.gravatar.com/avatar/',
-		site_title: 'April Site',
-		site_url: 'https://testsite2023.wordpress.com',
-		date_subscribed: new Date( '2023-01-04T17:55:53+00:00' ),
-	},
-];
-
-// repeat input array x times
-const repeat = ( arr: PostSubscription[], times: number ) =>
-	Array.from( { length: times }, () => arr ).flat() as PostSubscription[];
 
 const Comments = () => {
 	const translate = useTranslate();
+	const { data: posts, isLoading, error } = SubscriptionManager.usePostSubscriptionsQuery();
+
+	// todo: translate when we have agreed on the error message
+	const errorMessage = error ? 'An error occurred while fetching your subscriptions.' : '';
+
+	if ( ! isLoading && ( ! posts || ! posts.length ) ) {
+		return (
+			<Notice type="warning">{ translate( 'You are not subscribed to any comments.' ) }</Notice>
+		);
+	}
+
 	return (
-		<TabView errorMessage="" isLoading={ false }>
+		<TabView errorMessage={ errorMessage } isLoading={ isLoading }>
 			<div className="subscriptions-manager__list-actions-bar">
 				<SearchInput
 					placeholder={ translate( 'Search by post name…' ) }
@@ -47,7 +30,7 @@ const Comments = () => {
 				/>
 			</div>
 
-			<CommentList posts={ repeat( posts, 500 ) } />
+			<CommentList posts={ posts } />
 		</TabView>
 	);
 };
