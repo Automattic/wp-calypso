348c71ea113b66a57be8372bae99d50529f69889
arthur <arthur.chu@automattic.com>
2023-04-17 16:50:28 +0800

    Remove dead code

--- client/landing/stepper/hooks/use-checkout.ts
+++ client/landing/stepper/hooks/use-checkout.ts
@@ -36,14 +36,9 @@ const useCheckout = () => {
 
 		// If the plan is not provided, we might have added plan to the cart so we just go to the checkout page directly
 		if ( ! plan ) {
-			let checkoutUrl = `/checkout/${ encodeURIComponent( siteSlug ) }`;
-			if ( plan ) {
-				checkoutUrl += `/${ plan }`;
-			}
-
 			// The theme upsell link does not work with siteId and requires a siteSlug.
 			// See https://github.com/Automattic/wp-calypso/pull/64899
-			window.location.href = `${ checkoutUrl }?${ params }`;
+			window.location.href = `/checkout/${ encodeURIComponent( siteSlug ) }?${ params }`;
 		} else {
 			openCheckoutModal( [ plan ] );
 		}
