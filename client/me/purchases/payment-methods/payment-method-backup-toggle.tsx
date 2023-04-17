5f66215e9489ceb6ef63f76add8af70d04e21453
Markus Dobmann <m.do@posteo.net>
2023-04-17 08:03:41 +0200

    fix type errors related to query keys

--- client/me/purchases/payment-methods/payment-method-backup-toggle.tsx
+++ client/me/purchases/payment-methods/payment-method-backup-toggle.tsx
@@ -45,7 +45,7 @@ export default function PaymentMethodBackupToggle( { card }: { card: StoredPayme
 			queryClient.setQueryData( [ 'payment-method-backup-toggle', storedDetailsId ], data );
 
 			// Invalidate queries made by `useStoredPaymentMethods`.
-			queryClient.invalidateQueries( storedPaymentMethodsQueryKey );
+			queryClient.invalidateQueries( [ storedPaymentMethodsQueryKey ] );
 		},
 	} );
 	const toggleIsBackup = useCallback(
