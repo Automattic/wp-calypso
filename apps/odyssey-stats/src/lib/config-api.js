4a35c6c246c54ffc6829eabc29eee23f01eecbfa
Jasper Kang <kangzjnet@gmail.com>
2023-04-19 11:11:57 +1200

    adds jest tests

--- apps/odyssey-stats/src/lib/config-api.js
+++ apps/odyssey-stats/src/lib/config-api.js
@@ -6,135 +6,13 @@
  * - The package must be initialized before using with `initConfig`.
  */
 
-// The JSON is filtered by `apps/stats/filter-json-config-loader.js`.
-import productionConfig from '../../../../config/production.json';
+import ConfigApi from './create-odyssey-config';
 
-// Store the processed config data.
-let configData = {};
+const configApi = new ConfigApi();
 
-// configApi and configApi.* are all originated from https://github.com/Automattic/wp-calypso/blob/ca7d8fe3e0a5fb87b0659fbab659078ebbfbc7be/packages/create-calypso-config/src/index.ts
-const configApi = ( key ) => {
-	if ( key in configData ) {
-		return configData[ key ];
-	}
-	return undefined;
-};
-
-configApi.isEnabled = ( feature ) =>
-	( configData.features && !! configData.features[ feature ] ) || false;
-
-configApi.enabledFeatures = () => {
-	if ( ! configData.features ) {
-		return [];
-	}
-	return Object.entries( configData.features ).reduce(
-		( enabled, [ feature, isEnabled ] ) => ( isEnabled ? [ ...enabled, feature ] : enabled ),
-		[]
-	);
-};
-
-configApi.enable = ( feature ) => {
-	if ( configData.features ) {
-		configData.features[ feature ] = true;
-	}
-};
-
-configApi.disable = ( feature ) => {
-	if ( configData.features ) {
-		configData.features[ feature ] = false;
-	}
-};
-
-// Moved from https://github.com/Automattic/wp-calypso/blob/ca7d8fe3e0a5fb87b0659fbab659078ebbfbc7be/apps/odyssey-stats/src/load-config.js
-const overrideConfigDataFeatures = () => {
-	// Set is_running_in_jetpack_site to true if not specified (undefined or null).
-	productionConfig.features.is_running_in_jetpack_site =
-		configData.features?.is_running_in_jetpack_site ?? true;
-
-	// The option enables loading of the whole translation file, and could be optimized by setting it to `true`, which needs the translation chunks in place.
-	// @see https://github.com/Automattic/wp-calypso/blob/trunk/docs/translation-chunks.md
-	productionConfig.features[ 'use-translation-chunks' ] = false;
-
-	// Note: configData is hydrated in https://github.com/Automattic/jetpack/blob/d4d0f987cbf63a864b03b542b7813aabe87e0ed3/projects/packages/stats-admin/src/class-dashboard.php#L214
-	configData.features = productionConfig.features;
-};
-
-// Forked from https://github.com/Automattic/wp-calypso/blob/ca7d8fe3e0a5fb87b0659fbab659078ebbfbc7be/packages/calypso-config/src/index.ts#L60
-const applyFlags = ( flagsString, modificationMethod ) => {
-	const flags = flagsString.split( ',' );
-	flags.forEach( ( flagRaw ) => {
-		const flag = flagRaw.replace( /^[-+]/, '' );
-		const enabled = ! /^-/.test( flagRaw );
-		if ( configData.features ) {
-			configData.features[ flag ] = enabled;
-			// eslint-disable-next-line no-console
-			console.log(
-				'%cConfig flag %s via %s: %s',
-				'font-weight: bold;',
-				enabled ? 'enabled' : 'disabled',
-				modificationMethod,
-				flag
-			);
-		}
-	} );
-};
-
-const applyUrlFlags = () => {
-	const UrlMatch =
-		document.location.search && document.location.search.match( /[?&]flags=([^&]+)(&|$)/ );
-	if ( UrlMatch ) {
-		applyFlags( decodeURIComponent( UrlMatch[ 1 ] ), 'URL' );
-	}
-	const hashMatch =
-		document.location.hash && document.location.hash.match( /[?&]flags=([^&]+)(&|$)/ );
-	if ( hashMatch ) {
-		applyFlags( decodeURIComponent( hashMatch[ 1 ] ), 'HASH' );
-	}
-};
-
-// To be compatible with `@automattic/calypso-config`.
-export function isCalypsoLive() {
-	return false;
+export function initConfig( configKey = 'configData' ) {
+	configApi.init( configKey );
 }
-
-/**
- * Init config data
- *
- * @param {string} configKey the key to use for the config data.
- */
-export const initConfig = ( configKey = 'configData' ) => {
-	/**
-	 * Manages config flags for various deployment builds
-	 *
-	 * @module config/index
-	 */
-	if ( 'undefined' === typeof window ) {
-		throw new Error( 'Trying to initialize the configuration outside of a browser context.' );
-	}
-
-	if ( ! window[ configKey ] ) {
-		if ( 'development' === process.env.NODE_ENV ) {
-			// eslint-disable-next-line no-console
-			console.error(
-				'%cNo configuration was found: ' +
-					'%cPlease see ' +
-					'%cpackages/calypso-config/README.md ' +
-					'%cfor more information.',
-				'color: red; font-size: 120%', // error prefix
-				'color: white;', // message
-				'color: #0267ff;', // calypso-config README.md file reference
-				'color: white' // message
-			);
-		}
-	}
-	// Fallback to window.configData for backwards compatibility.
-	configData = window[ configKey ] ?? window.configData ?? configData;
-	overrideConfigDataFeatures();
-	applyUrlFlags();
-};
-
-export const getConfigData = () => configData;
-export const setConfigData = ( data ) => ( configData = data );
 
 export default configApi;
 export const isEnabled = configApi.isEnabled;
