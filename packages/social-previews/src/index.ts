0c406c02db6d0e5e6d96ec22f1d7f47d1caf6150
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-04-18 16:01:55 +0530

    Update index.ts

--- packages/social-previews/src/index.ts
+++ packages/social-previews/src/index.ts
@@ -1,5 +1,5 @@
 export { default as FacebookPreview } from './facebook-preview';
 export { default as TwitterPreview } from './twitter-preview';
 export { default as SearchPreview } from './search-preview';
-export { default as LinkedInPreview } from './linkedin-preview';
+export * from './linkedin-preview';
 export * from './constants';
