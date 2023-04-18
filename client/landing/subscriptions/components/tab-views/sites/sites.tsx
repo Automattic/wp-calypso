806a71ed3c293b34e504be63791099eda4c34c45
Paulo Cruz <paulo.cruz@automattic.com>
2023-04-18 10:08:02 -0300

    Apply useSearch to sites tab

--- client/landing/subscriptions/components/tab-views/sites/sites.tsx
+++ client/landing/subscriptions/components/tab-views/sites/sites.tsx
@@ -2,21 +2,19 @@ import config from '@automattic/calypso-config';
 import { SubscriptionManager } from '@automattic/data-stores';
 import SearchInput from '@automattic/search';
 import { useTranslate } from 'i18n-calypso';
-import { useState } from 'react';
-import { useDebounce } from 'use-debounce';
 import { SearchIcon } from 'calypso/landing/subscriptions/components/icons';
 import { Notice } from 'calypso/landing/subscriptions/components/notice';
 import { SiteList } from 'calypso/landing/subscriptions/components/site-list';
+import { useSearch } from 'calypso/landing/subscriptions/hooks';
 import TabView from '../tab-view';
 
 const isSearchEnabled = config.isEnabled( 'subscription-management/sites-search' );
 
 const Sites = () => {
 	const translate = useTranslate();
-	const [ searchTerm, setSearchTerm ] = useState( '' );
-	const [ debouncedSearchTerm ] = useDebounce( searchTerm, 300 );
+	const { searchTerm, handleSearch } = useSearch();
 	const { data, isLoading, error } = SubscriptionManager.useSiteSubscriptionsQuery( {
-		searchTerm: debouncedSearchTerm,
+		searchTerm,
 	} );
 	const { subscriptions, totalCount } = data ?? {};
 	// todo: translate when we have agreed on the error message
@@ -25,10 +23,6 @@ const Sites = () => {
 	if ( ! isLoading && ! totalCount ) {
 		return <Notice type="warning">{ translate( 'You are not subscribed to any sites.' ) }</Notice>;
 	}
-
-	const handleSearch = ( value: string ) => {
-		setSearchTerm( value );
-	};
 
 	return (
 		<TabView errorMessage={ errorMessage } isLoading={ isLoading }>
@@ -48,7 +42,7 @@ const Sites = () => {
 				<Notice type="warning">
 					{ translate( 'Sorry, no sites match {{italic}}%s.{{/italic}}', {
 						components: { italic: <i /> },
-						args: debouncedSearchTerm,
+						args: searchTerm,
 					} ) }
 				</Notice>
 			) }
