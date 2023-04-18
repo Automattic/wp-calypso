cf6ae83603926c96ef028f69c9c40d6a6b468c71
Paulo Trentin <paulo@paulotrentin.com.br>
2023-04-18 13:25:32 -0300

    Tests added and code cleanup

--- apps/wpcom-block-editor/src/wpcom/features/redirect-onboarding-user-after-publishing-post.js
+++ apps/wpcom-block-editor/src/wpcom/features/redirect-onboarding-user-after-publishing-post.js
@@ -2,30 +2,25 @@ import { select, subscribe } from '@wordpress/data';
 import domReady from '@wordpress/dom-ready';
 import { getQueryArg } from '@wordpress/url';
 
-const siteOrigin = getQueryArg( window.location.search, 'origin' );
-const showLaunchpad = getQueryArg( window.location.search, 'showLaunchpad' );
+export function redirectOnboardingUserAfterPublishingPost() {
+	const showLaunchpad = getQueryArg( window.location.search, 'showLaunchpad' );
 
-export const postWasPublished = async () =>
-	new Promise( ( resolve ) => {
-		const unsubscribe = subscribe( () => {
-			const isCurrentPostPublished = select( 'core/editor' ).isCurrentPostPublished();
-			const getCurrentPostRevisionsCount = select( 'core/editor' ).getCurrentPostRevisionsCount();
-
-			if ( isCurrentPostPublished && getCurrentPostRevisionsCount === 1 ) {
-				unsubscribe();
-				resolve();
-			}
-		} );
-	} );
-
-function redirectOnboardingUserAfterPublishingPost() {
 	if ( 'true' !== showLaunchpad ) {
 		return false;
 	}
 
-	postWasPublished().then( () => {
-		const siteSlug = window.location.hostname;
-		window.location.href = siteOrigin + '/setup/write/launchpad?siteSlug=' + siteSlug;
+	const siteOrigin = getQueryArg( window.location.search, 'origin' );
+	const siteSlug = window.location.hostname;
+
+	const unsubscribe = subscribe( () => {
+		const isCurrentPostPublished = select( 'core/editor' ).isCurrentPostPublished();
+		const getCurrentPostRevisionsCount = select( 'core/editor' ).getCurrentPostRevisionsCount();
+
+		if ( isCurrentPostPublished && getCurrentPostRevisionsCount === 1 ) {
+			unsubscribe();
+
+			window.location.href = siteOrigin + '/setup/write/launchpad?siteSlug=' + siteSlug;
+		}
 	} );
 }
 
