8537007b28aae8aab13a7f0309be4bdb14570464
Eoin Gallagher <eoin@automattic.com>
2023-04-18 15:21:31 +0100

    Use TagLink component to render list of tags in sidebar

--- client/reader/stream/reader-tag-sidebar/index.jsx
+++ client/reader/stream/reader-tag-sidebar/index.jsx
@@ -2,22 +2,30 @@ import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import TagLink from 'calypso/blocks/reader-post-card/tags-link';
 import formatNumberCompact from 'calypso/lib/format-number-compact';
 import '../style.scss';
 
 class ReaderTagSidebar extends Component {
 	static defaultProps = {
 		sitesPerPage: 25,
+		trendingTags: [],
+		relatedSites: [],
 	};
 
 	static propTypes = {
+		trendingTags: PropTypes.array,
+		relatedSites: PropTypes.array,
 		postCount: PropTypes.number,
 		authorCount: PropTypes.number,
 		sitesPerPage: PropTypes.number,
 	};
 
 	render() {
-		const { postCount, authorCount, translate } = this.props;
+		const { postCount, authorCount, translate, trendingTags } = this.props;
+		const tagLinks = trendingTags
+			.sort( ( a, b ) => b.count - a.count )
+			.map( ( trendingTag ) => <TagLink tag={ trendingTag.tag } key={ trendingTag.tag.slug } /> );
 
 		return (
 			<>
@@ -37,15 +45,13 @@ class ReaderTagSidebar extends Component {
 						</li>
 					</ul>
 				</div>
-				<div className="reader-tag-sidebar-related-tags">
-					<h1>{ translate( 'Trending Tags' ) }</h1>
-					<ul>
-						<li>
-							<a href="/tag/wordpress">WordPress</a>
-						</li>
-					</ul>
-					<a href="/tags">{ translate( 'See all tags' ) }</a>
-				</div>
+				{ tagLinks && (
+					<div className="reader-tag-sidebar-related-tags">
+						<h1>{ translate( 'Trending Tags' ) }</h1>
+						{ tagLinks }
+						<a href="/tags">{ translate( 'See all tags' ) }</a>
+					</div>
+				) }
 				<div className="reader-tag-sidebar-related-sites">
 					<h1>{ translate( 'Related Sites' ) }</h1>
 					<ul>
