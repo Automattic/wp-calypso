78083d1de12f6f5af49cfc5eda755562f4b83a50
James Gill <james.gill@automattic.com>
2023-04-18 12:12:13 +0100

    Add credits balance to calypso - Balance is just a placeholder until the API is finished - Balance should only show when set - Add placeholder support article - Build tests

---
+++ client/my-sites/promote-post/components/credit-balance/index.tsx
@@ -0,0 +1,41 @@
+import './style.scss';
+import { memo, useState } from '@wordpress/element';
+import { translate } from 'i18n-calypso';
+import InfoPopover from 'calypso/components/info-popover';
+import InlineSupportLink from 'calypso/components/inline-support-link';
+import { BlazeCreditStatus, useBlazeCredits } from 'calypso/lib/promote-post';
+
+const CreditBalance = () => {
+	const [ balance ] = useState( 0 ); // Todo: Fetch balance from the API
+	const creditsEnabled = useBlazeCredits() === BlazeCreditStatus.ENABLED;
+
+	if ( ! balance || ! creditsEnabled ) {
+		return null;
+	}
+
+	const tooltipText = translate(
+		'Available credits that will be automatically applied toward your next campaigns. {{learnMoreLink}}Learn more.{{/learnMoreLink}}',
+		{
+			components: {
+				learnMoreLink: <InlineSupportLink supportContext="blaze_credits" showIcon={ false } />,
+			},
+		}
+	);
+
+	return (
+		<div className="credit-balance__nav-item">
+			{ `Credits: $${ balance }` }
+			<InfoPopover
+				className="credit-balance__help-icon"
+				icon="help-outline"
+				position="right"
+				iconSize={ 18 }
+				showOnHover={ true }
+			>
+				{ tooltipText }
+			</InfoPopover>
+		</div>
+	);
+};
+
+export default memo( CreditBalance );
