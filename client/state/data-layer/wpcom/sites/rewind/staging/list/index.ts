e560b56aa22f5171c1717c70e1d7379469b0c689
Rafael Agostini <rafael.agostini@automattic.com>
2023-04-18 20:53:42 -0500

    Remove notices and unnecessary comments

--- client/state/data-layer/wpcom/sites/rewind/staging/list/index.ts
+++ client/state/data-layer/wpcom/sites/rewind/staging/list/index.ts
@@ -6,8 +6,6 @@ import {
 import { registerHandlers } from 'calypso/state/data-layer/handler-registry';
 import { http } from 'calypso/state/data-layer/wpcom-http/actions';
 import { dispatchRequest } from 'calypso/state/data-layer/wpcom-http/utils';
-//import fromApi from './from-api';
-import { successNotice, errorNotice } from 'calypso/state/notices/actions';
 import type { APIRewindStagingSiteList } from 'calypso/state/rewind/staging/types';
 import type { AnyAction } from 'redux';
 
@@ -34,10 +32,6 @@ const onSuccess = ( { siteId }: RequestActionType, response: APIRewindStagingSit
 		siteId,
 		stagingSitesList: response.sites_info,
 	},
-	//@TODO: Remove this notice. Just a temp solution to show success message while development is in progress
-	successNotice( 'Success fetching staging list.', {
-		duration: 5000,
-	} ),
 ];
 
 const onError = ( { siteId }: RequestActionType, error: string ) => [
@@ -46,10 +40,6 @@ const onError = ( { siteId }: RequestActionType, error: string ) => [
 		siteId,
 		error,
 	},
-	//@TODO: Remove this notice. Just a temp solution to show the error message while development is in progress
-	errorNotice( 'Could not retrieve staging sites. Please try again later.', {
-		duration: 5000,
-	} ),
 ];
 
 registerHandlers( 'state/data-layer/wpcom/sites/rewind/staging/list', {
