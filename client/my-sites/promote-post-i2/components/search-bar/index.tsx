f0543cabedb11b0da8eb173ba6dbfa0252910fcf
Timur Gogolev <timurns@gmail.com>
2023-04-19 16:14:30 +0300

    Provide more sort options

--- client/my-sites/promote-post-i2/components/search-bar/index.tsx
+++ client/my-sites/promote-post-i2/components/search-bar/index.tsx
@@ -16,22 +16,47 @@ type SortOption = {
 	value: string;
 };
 
+const SORT_OPTIONS_BY_TITLE = 'by_title';
+const SORT_OPTIONS_LAST_PUBLISHED = 'last_published';
+const SORT_OPTIONS_RECENTLY_UPDATED = 'recently_updated';
+const SORT_OPTIONS_MOST_LIKED = 'most_liked';
+const SORT_OPTIONS_MOST_COMMENTED = 'most_commented';
+const SORT_OPTIONS_MOST_VIEWED = 'most_viewed';
+
+const SORT_OPTIONS_DEFAULT = SORT_OPTIONS_RECENTLY_UPDATED;
+
 export default function SearchBar( props: Props ) {
 	const { mode } = props;
 
 	const sortOptions: Array< SortOption > = [
 		{
-			label: translate( 'Last published' ),
-			value: 'last_published',
+			label: translate( 'By title' ),
+			value: SORT_OPTIONS_BY_TITLE,
 		},
-		// { // TODO: Add the rest of sorting option after asking Bart
-		// 	label: translate( 'Most viewed' ),
-		// 	value: 'most_viewed',
-		// },
+		{
+			label: translate( 'Last published' ),
+			value: SORT_OPTIONS_LAST_PUBLISHED,
+		},
+		{
+			label: translate( 'Recently updated' ),
+			value: SORT_OPTIONS_RECENTLY_UPDATED,
+		},
+		{
+			label: translate( 'Most liked' ),
+			value: SORT_OPTIONS_MOST_LIKED,
+		},
+		{
+			label: translate( 'Most commented' ),
+			value: SORT_OPTIONS_MOST_COMMENTED,
+		},
+		{
+			label: translate( 'Most viewed' ),
+			value: SORT_OPTIONS_MOST_VIEWED,
+		},
 	];
 
 	const [ searchInput, setSearchInput ] = React.useState( '' );
-	const [ currentSortOption, setSortOption ] = React.useState( sortOptions[ 0 ].value );
+	const [ currentSortOption, setSortOption ] = React.useState( SORT_OPTIONS_DEFAULT );
 
 	const onSearch = () => {
 		return;
