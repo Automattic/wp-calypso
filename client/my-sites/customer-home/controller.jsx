273f7003427765748a3a5986a3691e89a89cfc80
Sharpirate <stoynov.bogomil@gmail.com>
2023-04-18 21:28:25 -0700

    update my home import path

--- client/my-sites/customer-home/controller.jsx
+++ client/my-sites/customer-home/controller.jsx
@@ -1,6 +1,6 @@
 import { isEcommerce } from '@automattic/calypso-products/src';
 import page from 'page';
-import { fetchLaunchpadChecklist } from 'calypso/../packages/help-center/src/hooks/use-launchpad';
+import { fetchLaunchpadChecklist } from 'calypso/../packages/help-center/src/hooks/use-launchpad-checklist';
 import { fetchLaunchpad } from 'calypso/data/sites/use-launchpad';
 import { areLaunchpadTasksCompleted } from 'calypso/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-helper';
 import { getQueryArgs } from 'calypso/lib/query-args';
