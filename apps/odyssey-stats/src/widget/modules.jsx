01b4ef7b4bc13bbb8a00bc58850b750e92cae63f
Jasper Kang <kangzjnet@gmail.com>
2023-04-20 15:43:40 +1200

    show button is akismet is not active

--- apps/odyssey-stats/src/widget/modules.jsx
+++ apps/odyssey-stats/src/widget/modules.jsx
@@ -3,16 +3,22 @@ import { protect, akismet } from '@automattic/components/src/icons';
 import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
 import useModuleDataQuery from '../hooks/use-module-data-query';
+import useMyJetpackProductsQuery from '../hooks/use-my-jetpack-products-query';
 
 import './modules.scss';
 
-function ModuleCard( { icon, title, value, isLoading, className = null } ) {
+function isProductActive( products, productSlug ) {
+	return products && products[ productSlug ]?.is_plugin_active;
+}
+
+function ModuleCard( { icon, title, value, className = null, active = true } ) {
 	return (
 		<div className={ classNames( 'stats-widget-module stats-widget-card', className ) }>
 			<div className="stats-widget-module__icon">{ icon }</div>
 			<div className="stats-widget-module__title">{ title }</div>
 			<div className="stats-widget-module__value">
-				{ isLoading ? '-' : <ShortenedNumber value={ value } /> }
+				{ active && <ShortenedNumber value={ value } /> }
+				{ ! active && <button>Activate</button> }
 			</div>
 		</div>
 	);
@@ -23,6 +29,8 @@ export default function Modules() {
 
 	const { data: akismetData, isFetching: isFetchingAkismet } = useModuleDataQuery( 'akismet' );
 	const { data: protectData, isFetching: isFetchingProtect } = useModuleDataQuery( 'protect' );
+	const { data: jetpackProducts, isFetching: isFetchingJetpackProducts } =
+		useMyJetpackProductsQuery();
 
 	return (
 		<div className="stats-widget-modules">
@@ -30,13 +38,15 @@ export default function Modules() {
 				icon={ protect }
 				title={ translate( 'Total blocked login attempts' ) }
 				value={ protectData }
-				isLoading={ isFetchingProtect }
+				isLoading={ isFetchingProtect || isFetchingJetpackProducts }
+				active={ isProductActive( jetpackProducts, 'protect' ) }
 			/>
 			<ModuleCard
 				icon={ akismet }
 				title={ translate( 'Total blocked spam comments' ) }
 				value={ akismetData }
-				isLoading={ isFetchingAkismet }
+				isLoading={ isFetchingAkismet || isFetchingJetpackProducts }
+				active={ isProductActive( jetpackProducts, 'anti-spam' ) }
 			/>
 		</div>
 	);
