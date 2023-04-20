002cbdd6dad01294143473b77267969777c532fb
arthur791004 <arthur.chu@automattic.com>
2023-04-20 15:21:02 +0800

    Pattern Assembler: Improve a11y and keyboard navigation (#75948)
    * Pattern Assembler: Improve category and pattern selection via keyboard
    
    * Pattern Assembler: Improve color and font variations selection via keyboard
    
    * Pattern Assembler: Focus on the back button when navigating to color or font screen
    
    * Fix types error
    
    * Fix types error
    
    * Focus on the first element of the PA screen
    
    * Fix types error

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/types.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/types.ts
@@ -2,14 +2,11 @@ export type Pattern = {
 	ID: number;
 	name: string;
 	title: string;
+	description?: string;
 	category?: Category;
-	categories: {
-		[ key: string ]: Category | undefined;
-	};
+	categories: Record< string, Category | undefined >;
 	key?: string;
-	pattern_meta?: {
-		[ key: string ]: boolean;
-	};
+	pattern_meta?: Record< string, boolean | undefined >;
 };
 
 export interface NavigatorLocation {
