81c392b56664a1cd3435ec782b71340f31b22df2
dpasque <dan.speckhard.pasque@automattic.com>
2023-04-20 17:13:16 -0500

    Use fallback accounts instead

--- test/e2e/specs/fse/fse__temp-smoke-test.ts
+++ test/e2e/specs/fse/fse__temp-smoke-test.ts
@@ -14,7 +14,6 @@ import {
 	FullSiteEditorPage,
 } from '@automattic/calypso-e2e';
 import { Browser, Page } from 'playwright';
-import { skipDescribeIf } from '../../jest-helpers';
 
 declare const browser: Browser;
 
@@ -25,46 +24,76 @@ declare const browser: Browser;
  * The goal here is to catch major breaks with the integration -- i.e. Calypso navigation no long working,
  * or getting a WSOD when trying to load the editor.
  */
-// None of our CoBlocks users use block themes.
-skipDescribeIf( envVariables.COBLOCKS_EDGE )(
-	DataHelper.createSuiteTitle( 'Site Editor Smoke Test' ),
-	function () {
-		let page: Page;
-		let fullSiteEditorPage: FullSiteEditorPage;
 
-		const features = envToFeatureKey( envVariables );
-		const accountName = getTestAccountByFeature( { ...features, variant: 'siteEditor' } );
+describe( DataHelper.createSuiteTitle( 'Site Editor Smoke Test' ), function () {
+	let page: Page;
+	let fullSiteEditorPage: FullSiteEditorPage;
 
-		beforeAll( async () => {
-			page = await browser.newPage();
+	const features = envToFeatureKey( envVariables );
+	const accountName = getTestAccountByFeature( { ...features, variant: 'siteEditor' }, [
+		// None of our CoBlocks users use block themes, so we need to fall back to the default Gutenberg users
+		// if COBLOCKS_EDGE is set.
+		{
+			gutenberg: 'stable',
+			coblocks: 'edge',
+			siteType: 'simple',
+			variant: 'siteEditor',
+			accountName: 'siteEditorSimpleSiteUser',
+		},
+		{
+			gutenberg: 'edge',
+			coblocks: 'edge',
+			siteType: 'simple',
+			variant: 'siteEditor',
+			accountName: 'siteEditorSimpleSiteEdgeUser',
+		},
+		{
+			gutenberg: 'stable',
+			coblocks: 'edge',
+			siteType: 'atomic',
+			variant: 'siteEditor',
+			accountName: 'siteEditorAtomicSiteUser',
+		},
+		{
+			gutenberg: 'edge',
+			coblocks: 'edge',
+			siteType: 'atomic',
+			variant: 'siteEditor',
+			accountName: 'siteEditorAtomicSiteEdgeUser',
+		},
+	] );
 
-			const testAccount = new TestAccount( accountName );
-			await testAccount.authenticate( page );
-		} );
+	console.log( accountName );
 
-		it( 'Navigate to Full Site Editor', async function () {
-			// Explicitly doing sidebar navigation to ensure Calypso navigation is intact.
-			const sidebarComponent = new SidebarComponent( page );
-			await sidebarComponent.navigate( 'Appearance', 'Editor' );
-		} );
+	beforeAll( async () => {
+		page = await browser.newPage();
 
-		it( 'Editor endpoint loads', async function () {
-			await page.waitForURL( /site-editor/ );
-		} );
+		const testAccount = new TestAccount( accountName );
+		await testAccount.authenticate( page );
+	} );
 
-		it( 'Open the Page template', async function () {
-			fullSiteEditorPage = new FullSiteEditorPage( page );
+	it( 'Navigate to Full Site Editor', async function () {
+		// Explicitly doing sidebar navigation to ensure Calypso navigation is intact.
+		const sidebarComponent = new SidebarComponent( page );
+		await sidebarComponent.navigate( 'Appearance', 'Editor' );
+	} );
 
-			await fullSiteEditorPage.prepareForInteraction();
+	it( 'Editor endpoint loads', async function () {
+		await page.waitForURL( /site-editor/ );
+	} );
 
-			await fullSiteEditorPage.ensureNavigationTopLevel();
-			await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Templates' );
-			await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Page' );
-			await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Edit' );
-		} );
+	it( 'Open the Page template', async function () {
+		fullSiteEditorPage = new FullSiteEditorPage( page );
 
-		it( 'Editor canvas loads', async function () {
-			await fullSiteEditorPage.waitUntilLoaded();
-		} );
-	}
-);
+		await fullSiteEditorPage.prepareForInteraction();
+
+		await fullSiteEditorPage.ensureNavigationTopLevel();
+		await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Templates' );
+		await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Page' );
+		await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Edit' );
+	} );
+
+	it( 'Editor canvas loads', async function () {
+		await fullSiteEditorPage.waitUntilLoaded();
+	} );
+} );
