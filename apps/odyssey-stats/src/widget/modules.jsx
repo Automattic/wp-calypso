d61ffb0313ff3ec67d666ff66d622498b423e270
Jasper Kang <kangzjnet@gmail.com>
2023-04-20 15:58:13 +1200

    add activation call

--- apps/odyssey-stats/src/widget/modules.jsx
+++ apps/odyssey-stats/src/widget/modules.jsx
@@ -2,6 +2,7 @@ import { ShortenedNumber } from '@automattic/components';
 import { protect, akismet } from '@automattic/components/src/icons';
 import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
+import wpcom from 'calypso/lib/wp';
 import useModuleDataQuery from '../hooks/use-module-data-query';
 import useMyJetpackProductsQuery from '../hooks/use-my-jetpack-products-query';
 
@@ -11,14 +12,14 @@ function isProductActive( products, productSlug ) {
 	return products && products[ productSlug ]?.is_plugin_active;
 }
 
-function ModuleCard( { icon, title, value, className = null, active = true } ) {
+function ModuleCard( { icon, title, value, activateProduct, className = null, active = true } ) {
 	return (
 		<div className={ classNames( 'stats-widget-module stats-widget-card', className ) }>
 			<div className="stats-widget-module__icon">{ icon }</div>
 			<div className="stats-widget-module__title">{ title }</div>
 			<div className="stats-widget-module__value">
 				{ active && <ShortenedNumber value={ value } /> }
-				{ ! active && <button>Activate</button> }
+				{ ! active && <button onClick={ activateProduct }>Activate</button> }
 			</div>
 		</div>
 	);
@@ -27,10 +28,24 @@ function ModuleCard( { icon, title, value, className = null, active = true } ) {
 export default function Modules() {
 	const translate = useTranslate();
 
-	const { data: akismetData, isFetching: isFetchingAkismet } = useModuleDataQuery( 'akismet' );
-	const { data: protectData, isFetching: isFetchingProtect } = useModuleDataQuery( 'protect' );
-	const { data: jetpackProducts, isFetching: isFetchingJetpackProducts } =
-		useMyJetpackProductsQuery();
+	const { data: akismetData } = useModuleDataQuery( 'akismet' );
+	const { data: protectData } = useModuleDataQuery( 'protect' );
+	const { data: jetpackProducts } = useMyJetpackProductsQuery();
+
+	const activateProduct = ( productSlug ) => () => {
+		return wpcom.req
+			.post( {
+				apiNamespace: 'my-jetpack/v1',
+				path: `/site/products/${ productSlug }`,
+			} )
+			.then( () => {
+				if ( jetpackProducts && jetpackProducts[ productSlug ]?.post_activation_url ) {
+					window.location.href = jetpackProducts[ productSlug ]?.post_activation_url;
+					return;
+				}
+				window.location.reload();
+			} );
+	};
 
 	return (
 		<div className="stats-widget-modules">
@@ -38,15 +53,15 @@ export default function Modules() {
 				icon={ protect }
 				title={ translate( 'Total blocked login attempts' ) }
 				value={ protectData }
-				isLoading={ isFetchingProtect || isFetchingJetpackProducts }
 				active={ isProductActive( jetpackProducts, 'protect' ) }
+				activateProduct={ activateProduct( 'protect' ) }
 			/>
 			<ModuleCard
 				icon={ akismet }
 				title={ translate( 'Total blocked spam comments' ) }
 				value={ akismetData }
-				isLoading={ isFetchingAkismet || isFetchingJetpackProducts }
 				active={ isProductActive( jetpackProducts, 'anti-spam' ) }
+				activateProduct={ activateProduct( 'anti-spam' ) }
 			/>
 		</div>
 	);
