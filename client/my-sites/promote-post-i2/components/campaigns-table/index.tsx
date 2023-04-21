731ea0242a4b88af8d5c58827de87799ecee4d4f
Timur Gogolev <timurns@gmail.com>
2023-04-21 19:53:06 +0300

    Change layout settings to support the banner

--- client/my-sites/promote-post-i2/components/campaigns-table/index.tsx
+++ client/my-sites/promote-post-i2/components/campaigns-table/index.tsx
@@ -11,7 +11,7 @@ export default function CampaignsTable( props: Props ) {
 	const { campaigns } = props;
 	return (
 		<div>
-			<table className="promote-post__table">
+			<table className="campaigns-list__table">
 				<thead>
 					<tr>
 						<th key="campaign">Campaign</th>
