e0c7367e08e85789650c4600a063e8cc9b5ee60b
Rafael Agostini <rafael.agostini@automattic.com>
2023-04-21 15:54:12 -0500

    Add selectors for fetching staging site info
    Add isRequestingStagingSiteInfo, hasFetchedStagingSiteInfo and getBackupStagingSiteInfo.

---
+++ client/state/rewind/selectors/get-backup-staging-site-info.ts
@@ -0,0 +1,14 @@
+import { APIRewindStagingSiteInfo } from 'calypso/state/rewind/staging/types';
+import type { AppState } from 'calypso/types';
+
+/**
+ * Returns the staging site info related to given site ID.
+ *
+ * @param state The application state.
+ * @param siteId The site for which to fetch the staging info.
+ * @returns APIRewindStagingSiteInfo object if staging site info is available; otherwise, null.
+ */
+const getBackupStagingSiteInfo = ( state: AppState, siteId: number ): APIRewindStagingSiteInfo[] =>
+	state.rewind[ siteId ]?.staging?.site?.info ?? null;
+
+export default getBackupStagingSiteInfo;
