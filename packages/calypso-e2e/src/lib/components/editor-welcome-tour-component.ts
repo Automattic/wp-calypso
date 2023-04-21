177d6c869290dc86cab7e83576a49056d791bf23
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2023-04-21 15:10:01 +0200

    Refactor EditorWelcomeTourComponent

--- packages/calypso-e2e/src/lib/components/editor-welcome-tour-component.ts
+++ packages/calypso-e2e/src/lib/components/editor-welcome-tour-component.ts
@@ -1,22 +1,17 @@
-import { Page, Locator } from 'playwright';
-import { EXTENDED_EDITOR_WAIT_TIMEOUT } from '../pages/editor-page';
+import { Page } from 'playwright';
+import { EditorWindow } from './editor-window';
 
 /**
  * Represents the welcome tour that shows in a popover when the editor loads.
  */
-export class EditorWelcomeTourComponent {
-	private page: Page;
-	private editor: Locator;
-
+export class EditorWelcomeTourComponent extends EditorWindow {
 	/**
 	 * Creates an instance of the component.
 	 *
 	 * @param {Page} page Object representing the base page.
-	 * @param {Locator} editor Frame-safe locator to the editor.
 	 */
-	constructor( page: Page, editor: Locator ) {
-		this.page = page;
-		this.editor = editor;
+	constructor( page: Page ) {
+		super( page );
 	}
 
 	/**
@@ -25,15 +20,7 @@ export class EditorWelcomeTourComponent {
 	 * @see {@link https://github.com/Automattic/wp-calypso/issues/57660}
 	 */
 	async forceToggleWelcomeTour( show = true ): Promise< void > {
-		// Locator API doesn't have waitForFunction yet. We need a Frame for now.
-		const editorElement = await this.editor.elementHandle( {
-			timeout: EXTENDED_EDITOR_WAIT_TIMEOUT,
-		} );
-		const editorFrame = await editorElement?.ownerFrame();
-		if ( ! editorFrame ) {
-			return;
-		}
-
+		const editorFrame = await this.getEditorFrame();
 		await editorFrame.waitForFunction( async () => {
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			const welcomeGuide = ( window as any )?.wp?.data?.select( 'automattic/wpcom-welcome-guide' );
