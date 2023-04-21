591452840640812d9f1c7f56bf4425443e371e67
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2023-04-21 13:29:04 +0200

    Create EditorWindow base class

---
+++ packages/calypso-e2e/src/lib/components/editor-window.ts
@@ -0,0 +1,60 @@
+import { Page, Frame } from 'playwright';
+
+/** */
+export class EditorWindow {
+	public page: Page;
+
+	/** */
+	constructor( page: Page ) {
+		this.page = page;
+	}
+
+	/** */
+	async getEditorFrame(): Promise< Page | Frame > {
+		const editorFrame = await Promise.race( [
+			( async () => {
+				const calypsoIframe = this.page.locator( 'iframe[src*="calypsoify"]' );
+				await calypsoIframe.waitFor();
+
+				return this.page.frame( { url: '/calypsoify/' } );
+			} )(),
+			( async () => {
+				const editorBody = this.page.locator( 'body.block-editor-page' );
+				await editorBody.waitFor();
+
+				return this.page;
+			} )(),
+		] );
+
+		if ( ! editorFrame ) {
+			throw new Error( 'Editor frame unavailable' );
+		}
+
+		return editorFrame;
+	}
+
+	/** */
+	async getEditorCanvas(): Promise< Page | Frame > {
+		const editorFrame = await this.getEditorFrame();
+		const editorCanvas = await Promise.race( [
+			( async () => {
+				const canvasIframe = editorFrame.locator( 'iframe[name="editor-canvas"]' );
+				await canvasIframe.waitFor();
+
+				return this.page.frame( 'editor-canvas' );
+			} )(),
+			( async () => {
+				const canvasWrapper = this.page.locator( '.editor-styles-wrapper' );
+				await canvasWrapper.waitFor();
+
+				return editorFrame;
+			} )(),
+		] );
+
+		if ( ! editorCanvas ) {
+			throw new Error( 'Editor canvas unavailable' );
+		}
+
+		return editorCanvas;
+	}
+}
