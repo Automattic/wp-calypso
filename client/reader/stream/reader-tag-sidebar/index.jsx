e920b8e2391081e65775ff78b804cd4b8e0b6a2e
Eoin Gallagher <eoin@automattic.com>
2023-04-22 14:17:11 +0100

    Use tag stats to display post and site count totals (#76093)
    * Add query to get tag stats
    
    * stash query and markup
    
    * Fix tag stats request
    
    * Render post and site totals with style
    
    * Bumped space between two stats by a smidge
    
    ---------
    
    Co-authored-by: Grant Kinney <grant.kinney@automattic.com>
    Co-authored-by: Dave Martin <davemart-in@users.noreply.github.com>

--- client/reader/stream/reader-tag-sidebar/index.jsx
+++ client/reader/stream/reader-tag-sidebar/index.jsx
@@ -1,12 +1,15 @@
 import { useTranslate } from 'i18n-calypso';
 import TagLink from 'calypso/blocks/reader-post-card/tag-link';
 import { useRelatedMetaByTag } from 'calypso/data/reader/use-related-meta-by-tag';
+import { useTagStats } from 'calypso/data/reader/use-tag-stats';
+import formatNumberCompact from 'calypso/lib/format-number-compact';
 import ReaderListFollowingItem from 'calypso/reader/stream/reader-list-followed-sites/item';
 import '../style.scss';
 
 const ReaderTagSidebar = ( { tag } ) => {
 	const translate = useTranslate();
 	const relatedMetaByTag = useRelatedMetaByTag( tag );
+	const tagStats = useTagStats( tag );
 	if ( relatedMetaByTag === undefined ) {
 		return null;
 	}
@@ -20,6 +23,22 @@ const ReaderTagSidebar = ( { tag } ) => {
 
 	return (
 		<>
+			{ tagStats && (
+				<div className="reader-tag-sidebar-stats">
+					<div className="reader-tag-sidebar-stats__item">
+						<span className="reader-tag-sidebar-stats__count">
+							{ formatNumberCompact( tagStats?.data?.total_posts ) }
+						</span>
+						<span className="reader-tag-sidebar-stats__title">{ translate( 'Posts' ) }</span>
+					</div>
+					<div className="reader-tag-sidebar-stats__item">
+						<span className="reader-tag-sidebar-stats__count">
+							{ formatNumberCompact( tagStats?.data?.total_sites ) }
+						</span>
+						<span className="reader-tag-sidebar-stats__title">{ translate( 'Sites' ) }</span>
+					</div>
+				</div>
+			) }
 			{ tagLinks && (
 				<div className="reader-tag-sidebar-related-tags">
 					<h2>{ translate( 'Related Tags' ) }</h2>
