871236c2efc6411da4300b84f154df33388ada1c
Eru Penkman <roo2@protonmail.com>
2023-04-26 15:37:40 +1000

    css style

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/post-published-sharing-modal/index.tsx
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/post-published-sharing-modal/index.tsx
@@ -2,10 +2,11 @@ import { Modal, Button } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { useEffect, useRef, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { Icon, globe, link as linkIcon } from '@wordpress/icons';
 import React from 'react';
 import { selectors as wpcomWelcomeGuideSelectors } from '../store';
-import type { SelectFromMap } from '@automattic/data-stores';
 import postPublishedImage from './images/illo-share.svg';
+import type { SelectFromMap } from '@automattic/data-stores';
 
 import './style.scss';
 
@@ -81,20 +82,33 @@ const PostPublishedSharingModal: React.FC = () => {
 			title=""
 			onRequestClose={ closeModal }
 		>
-			<div>
-				<h1> { __( 'Congratulations!', 'full-site-editing' ) } </h1>
-				<div>
-					{ __(
-						'Your post is now live and was delivered successfully to subscribed readers',
-						'full-site-editing'
-					) }
+			<div className="wpcom-block-editor-post-published-sharing-modal__inner">
+				<div className="wpcom-block-editor-post-published-sharing-modal__left">
+					<h1> { __( 'Congratulations!', 'full-site-editing' ) } </h1>
+					<p>
+						{ __(
+							'Your post is now live and was delivered successfully to subscribed readers',
+							'full-site-editing'
+						) }
+					</p>
+					<Button onClick={ handleViewPostClick }>
+						{ ' ' }
+						<Icon icon={ globe } /> { __( 'View Post', 'full-site-editing' ) }
+					</Button>
+					<Button onClick={ () => null }>
+						{ ' ' }
+						<Icon icon={ linkIcon } /> { __( 'View Post', 'full-site-editing' ) }
+					</Button>
+					<hr />
+					<h2>{ __( 'Share with:', 'full-site-editing' ) }</h2>
 				</div>
-				<Button onClick={ handleViewPostClick }>{ __( 'View Post', 'full-site-editing' ) }</Button>
-				<hr />
-				<h2>Share with:</h2>
-			</div>
-			<div>
-				<img src={ postPublishedImage } />
+				<div className="wpcom-block-editor-post-published-sharing-modal__right">
+					<img
+						className="wpcom-block-editor-post-published-sharing-modal__image"
+						src={ postPublishedImage }
+						alt={ __( 'Share Post', 'full-site-editing' ) }
+					/>
+				</div>
 			</div>
 		</Modal>
 	);
