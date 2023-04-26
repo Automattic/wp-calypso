d65ba1f828898ab9ff9481492dbf77e0b453c3ed
Rafael Agostini <rafael.agostini@automattic.com>
2023-04-26 13:37:33 +0100

    Refactor StagingToggleControl in main component

--- client/components/jetpack-staging-sites-management/index.tsx
+++ client/components/jetpack-staging-sites-management/index.tsx
@@ -1,4 +1,5 @@
 import { Card } from '@automattic/components';
+import { ToggleControl } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 import { useTranslate } from 'i18n-calypso';
 import { FunctionComponent, useCallback, useEffect } from 'react';
@@ -10,7 +11,6 @@ import hasFetchedStagingSiteInfo from 'calypso/state/rewind/selectors/has-fetche
 import isRequestingStagingSiteInfo from 'calypso/state/rewind/selectors/is-requesting-staging-site-info';
 import { requestUpdateBackupStagingFlag } from 'calypso/state/rewind/staging/actions';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
-import StagingToggleControl from './staging-toggle-control';
 import './style.scss';
 
 const JetpackStagingSitesManagement: FunctionComponent = () => {
@@ -67,40 +67,43 @@ const JetpackStagingSitesManagement: FunctionComponent = () => {
 			></path>
 		</svg>
 	);
+
+	const StagingToggleControl = ToggleControl as React.ComponentType<
+		ToggleControl.Props & {
+			disabled?: boolean;
+		}
+	>;
+
 	return (
 		<>
 			<QueryBackupStagingSite siteId={ siteId } />
-			{ hasFetched && stagingInfo && (
-				<>
-					<div className="jetpack-staging-sites-management">
-						<Card compact={ true } className="setting-title">
-							<h3>{ translate( 'Setting up this site as a staging site' ) }</h3>
-							<div className="staging-icon">{ stagingIcon }</div>
-						</Card>
-						<Card className="setting-content">
-							<div className="setting-option">
-								<div className="setting-option__toggle">
-									<StagingToggleControl
-										loading={ isLoading }
-										checked={ stagingToggle }
-										onChange={ toggleStagingFlag }
-									/>
-								</div>
-								<div className="setting-option__description">
-									{ translate(
-										'{{strong}}Set this site as a Staging Site.{{/strong}} You will be able to copy any site to this staging site and test your changes safely.',
-										{
-											components: {
-												strong: <strong />,
-											},
-										}
-									) }
-								</div>
-							</div>
-						</Card>
+			<div className="jetpack-staging-sites-management">
+				<Card compact={ true } className="setting-title">
+					<h3>{ translate( 'Set up as staging site' ) }</h3>
+					<div className="staging-icon">{ stagingIcon }</div>
+				</Card>
+				<Card className="setting-content">
+					<div className="setting-option">
+						<div className="setting-option__toggle">
+							<StagingToggleControl
+								disabled={ isLoading }
+								checked={ isStaging }
+								onChange={ toggleStagingFlag }
+							/>
+						</div>
+						<div className="setting-option__description">
+							{ translate(
+								'{{strong}}Set this site as a Staging Site.{{/strong}} You will be able to copy any site to this staging site and test your changes safely.',
+								{
+									components: {
+										strong: <strong />,
+									},
+								}
+							) }
+						</div>
 					</div>
-				</>
-			) }
+				</Card>
+			</div>
 		</>
 	);
 };
