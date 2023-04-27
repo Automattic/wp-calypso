41388c640d8ccdb9302b5fd39dfaa0f5a457afcc
Dehan <dehandecroos@gmail.com>
2023-04-27 01:56:13 -0400

    Revert "Unify hook calls"
    This reverts commit 9add02615948db9134f997671b443d97ed4a491c.

--- client/my-sites/plans-features-main/hooks/use-plan-upgrade-credits.ts
+++ client/my-sites/plans-features-main/hooks/use-plan-upgrade-credits.ts
@@ -1,24 +1,21 @@
 import { useSelector } from 'react-redux';
-import { getPlanPrices } from 'calypso/state/plans/selectors';
+import { getPlanDiscountedRawPrice } from 'calypso/state/sites/plans/selectors';
+import { getSitePlanRawPrice } from 'calypso/state/sites/plans/selectors/get-site-plan-raw-price';
 import isPlanAvailableForPurchase from 'calypso/state/sites/plans/selectors/is-plan-available-for-purchase';
-import type { PlanSlug } from '@automattic/calypso-products';
 
 /**
  * Calculate available plan credits given a set of displayed plans
  *
  * @param {*} siteId the selected site id
- * @param {PlanSlug[]}  plans Plans that are considered for the given calculation
+ * @param {string[]}  plans Plans that are considered for the given calculation
  * @returns {number} The amount of credits available for the given plans
  */
-export function usePlanUpgradeCredits( siteId: number | undefined, plans: PlanSlug[] ): number {
+export function usePlanUpgradeCredits( siteId: number | undefined, plans: string[] ): number {
 	const plansDetails = useSelector( ( state ) =>
-		plans.map( ( planName: PlanSlug ) => ( {
+		plans.map( ( planName ) => ( {
 			isPlanAvailableForPurchase: isPlanAvailableForPurchase( state, siteId ?? 0, planName ),
-			...getPlanPrices( state, {
-				siteId: null,
-				planSlug: planName,
-				returnMonthly: false,
-			} ),
+			planDiscountedRawPrice: getPlanDiscountedRawPrice( state, siteId ?? 0, planName ),
+			sitePlanRawPrice: getSitePlanRawPrice( state, siteId ?? 0, planName ),
 		} ) )
 	);
 	if ( ! siteId ) {
@@ -26,16 +23,17 @@ export function usePlanUpgradeCredits( siteId: number | undefined, plans: PlanSl
 	}
 
 	const creditsPerPlan = plansDetails.map(
-		( { isPlanAvailableForPurchase, planDiscountedRawPrice, rawPrice } ) => {
+		( { isPlanAvailableForPurchase, planDiscountedRawPrice, sitePlanRawPrice } ) => {
 			if ( ! isPlanAvailableForPurchase ) {
 				return 0;
 			}
-			if ( typeof planDiscountedRawPrice !== 'number' || typeof rawPrice !== 'number' ) {
+			if ( typeof planDiscountedRawPrice !== 'number' || typeof sitePlanRawPrice !== 'number' ) {
 				return 0;
 			}
 
-			return rawPrice - planDiscountedRawPrice;
+			return sitePlanRawPrice - planDiscountedRawPrice;
 		}
 	);
+
 	return Math.max( ...creditsPerPlan );
 }
