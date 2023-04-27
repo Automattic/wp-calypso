20f3b765ff3d717e198fa2d4217272ab04d543b6
Markus Dobmann <m.do@posteo.net>
2023-04-27 11:05:23 +0200

    chore(odyssey-stats): upgrade to `react-query` to `v4`

--- apps/odyssey-stats/src/page-middleware/layout.jsx
+++ apps/odyssey-stats/src/page-middleware/layout.jsx
@@ -1,4 +1,4 @@
-import { QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Provider as ReduxProvider } from 'react-redux';
 import CalypsoI18nProvider from 'calypso/components/calypso-i18n-provider';
 import { RouteProvider } from 'calypso/components/route';
