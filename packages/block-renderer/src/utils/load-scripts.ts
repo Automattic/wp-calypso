b9e2a0355f3d5f3b3e369ca1a149a61576282fc0
arthur <arthur.chu@automattic.com>
2023-04-28 19:23:42 +0800

    Block Renderer: Support custom scripts

---
+++ packages/block-renderer/src/utils/load-scripts.ts
@@ -0,0 +1,25 @@
+const loadScript = async ( element: HTMLElement, { id, src, textContent }: HTMLScriptElement ) => {
+	return new Promise( ( resolve, reject ) => {
+		const script = element.ownerDocument.createElement( 'script' ) as HTMLScriptElement;
+		script.id = id;
+		if ( src ) {
+			script.src = src;
+			script.onload = () => resolve( script );
+			script.onerror = () => reject();
+		} else {
+			script.textContent = textContent;
+			resolve();
+		}
+
+		element.appendChild( script );
+	} );
+};
+
+const loadScripts = async ( element: HTMLElement, scripts: HTMLScriptElement[] ) => {
+	return scripts.reduce(
+		( promise, script ): Promise< any > => promise.then( () => loadScript( element, script ) ),
+		Promise.resolve()
+	);
+};
+
+export default loadScripts;
