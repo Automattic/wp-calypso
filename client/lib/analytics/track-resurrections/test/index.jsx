9ae8d902250f144d1b809b0ecc635e837217e15e
Ayebare Mucunguzi <ayebare11@gmail.com>
2023-04-28 11:45:46 +0300

    fixed component location in import

--- client/lib/analytics/track-resurrections/test/index.jsx
+++ client/lib/analytics/track-resurrections/test/index.jsx
@@ -6,7 +6,7 @@ import { render } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { useSelector } from 'react-redux';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import TrackResurrections from './TrackResurrections';
+import TrackResurrections from '../TrackResurrections';
 
 jest.mock( 'react-redux', () => ( {
 	useSelector: jest.fn(),
