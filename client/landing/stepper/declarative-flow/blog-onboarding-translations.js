b5063de93969ae95b19794945b43a5d71ec466b9
DustyReagan <dusty@dustyreagan.com>
2023-04-28 13:53:34 -0500

    Add more translations

--- client/landing/stepper/declarative-flow/blog-onboarding-translations.js
+++ client/landing/stepper/declarative-flow/blog-onboarding-translations.js
@@ -19,10 +19,10 @@ translate( 'Make it yours.' );
 translate( 'Give your blog a name' );
 translate( 'A catchy name to make your blog memorable' );
 translate( 'Add a brief description' );
-translate( 'Let people know what your blog’s about' );
+translate( "Let people know what your blog's about" );
+translate( 'Save and continue' );
 
 /* Pick a domain page */
-translate( 'Your domain. Your identity.' );
 translate(
 	'Help your blog stand out with a custom domain. Not sure yet? {{laterLink}}Decide later.{{/laterLink}}',
 	{
@@ -30,11 +30,20 @@ translate(
 	}
 );
 
-/* Pick a plan page */
-translate( "There's a plan for you." );
+/* Pick a domain modal */
+translate( 'Custom domains require a paid plan.' );
+translate(
+	'Your previous selection of Free plan comes with a wordpress.blog subdomain. If you’d rather keep your custom address, choose any plan, from Personal up.'
+);
+translate( 'Upgrade to Personal' );
+translate( 'Continue with Free plan' );
+translate( 'Free for one year' );
+
+/* Pick a plan */
+translate( 'Free domain for the first year' );
 
 /* Post purchase UI */
-translate( "Your blog's ready" );
+translate( "Your blog's ready!" );
 translate( 'Now it’s time to connect your social accounts.' );
 translate( 'Visit your blog' );
 translate( 'Connect to social' );
