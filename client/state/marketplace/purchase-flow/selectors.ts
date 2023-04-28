9162d6aadcbf2eedfb211bd53bce28308ababaf2
Dehan <dehandecroos@gmail.com>
2023-04-28 15:34:41 -0400

    Fix default state

--- client/state/marketplace/purchase-flow/selectors.ts
+++ client/state/marketplace/purchase-flow/selectors.ts
@@ -1,16 +1,8 @@
 import 'calypso/state/marketplace/init';
-import {
-	IPurchaseFlowState,
-	MARKETPLACE_ASYNC_PROCESS_STATUS,
-} from 'calypso/state/marketplace/types';
+import { IPurchaseFlowState } from 'calypso/state/marketplace/types';
 import { IAppState } from 'calypso/state/types';
+import { defaultState } from './reducer';
 
 export function getPurchaseFlowState( state: IAppState ): IPurchaseFlowState {
-	return (
-		state?.marketplace?.purchaseFlow ?? {
-			primaryDomain: null,
-			productSlugInstalled: null,
-			pluginInstallationStatus: MARKETPLACE_ASYNC_PROCESS_STATUS.UNKNOWN,
-		}
-	);
+	return state?.marketplace?.purchaseFlow ?? defaultState;
 }
