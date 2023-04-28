517916752a523257fe97f435225569db767a2f5c
DustyReagan <dusty@dustyreagan.com>
2023-04-28 13:26:47 -0500

    Add preemptive translations

---
+++ client/landing/stepper/declarative-flow/blog-onboarding-translations.js
@@ -0,0 +1,40 @@
+import { translate } from 'i18n-calypso';
+import { createElement } from 'react';
+
+/* Binary selection page */
+translate( 'Start writing' );
+translate( 'Pick a design first' );
+
+/* Launchpad page */
+translate( "Your blog's almost ready." );
+translate( "Your blog's ready to launch!" );
+translate( 'Keep the momentum going with these final steps.' );
+translate( 'Write your first post' );
+translate( 'Set up your blog' );
+translate( 'Launch your blog' );
+translate( 'Checkout and launch' );
+
+/* Setup blog page */
+translate( 'Make it yours.' );
+translate( 'Give your blog a name' );
+translate( 'A catchy name to make your blog memorable' );
+translate( 'Add a brief description' );
+translate( 'Let people know what your blog’s about' );
+
+/* Pick a domain page */
+translate( 'Your domain. Your identity.' );
+translate(
+	'Help your blog stand out with a custom domain. Not sure yet? {{laterLink}}Decide later.{{/laterLink}}',
+	{
+		components: { strong: createElement( 'a' ) },
+	}
+);
+
+/* Pick a plan page */
+translate( "There's a plan for you." );
+
+/* Post purchase UI */
+translate( "Your blog's ready" );
+translate( 'Now it’s time to connect your social accounts.' );
+translate( 'Visit your blog' );
+translate( 'Connect to social' );
