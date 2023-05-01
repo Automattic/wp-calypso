207ca3f21ffd1c337b5798c7d75e18313b0b307b
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2023-05-01 15:36:22 -0300

    Rollback unverified emails

--- client/my-sites/customer-home/cards/features/domain-upsell/index.jsx
+++ client/my-sites/customer-home/cards/features/domain-upsell/index.jsx
@@ -16,6 +16,7 @@ import { addQueryArgs } from 'calypso/lib/url';
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 import useCartKey from 'calypso/my-sites/checkout/use-cart-key';
 import { isStagingSite } from 'calypso/sites-dashboard/utils';
+import { isCurrentUserEmailVerified } from 'calypso/state/current-user/selectors';
 import { savePreference } from 'calypso/state/preferences/actions';
 import { getPreference, hasReceivedRemotePreferences } from 'calypso/state/preferences/selectors';
 import { getDomainsBySite } from 'calypso/state/sites/domains/selectors';
@@ -25,6 +26,8 @@ import { getSelectedSite, getSelectedSiteSlug } from 'calypso/state/ui/selectors
 import './style.scss';
 
 export default function DomainUpsell() {
+	const isEmailVerified = useSelector( isCurrentUserEmailVerified );
+
 	const selectedSite = useSelector( getSelectedSite );
 	const selectedSiteSlug = useSelector( getSelectedSiteSlug );
 
@@ -47,7 +50,9 @@ export default function DomainUpsell() {
 
 	const shouldNotShowUpselDismissed = ! hasPreferences || isDismissed;
 
-	if ( shouldNotShowUpselDismissed || siteDomainsLength || isStagingSite( selectedSite ) ) {
+	const shouldNotShowMyHomeUpsell = siteDomainsLength || ! isEmailVerified;
+
+	if ( shouldNotShowUpselDismissed || shouldNotShowMyHomeUpsell || isStagingSite( selectedSite ) ) {
 		return null;
 	}
 
