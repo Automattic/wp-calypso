225e943ee8c3d245c2924b341030bf45481dd982
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2023-05-02 11:23:24 +0200

    Add comment to clarify new definition of ToggleControl prop type

--- client/landing/subscriptions/components/settings-popover/site-settings/notify-me-of-new-posts-toggle.tsx
+++ client/landing/subscriptions/components/settings-popover/site-settings/notify-me-of-new-posts-toggle.tsx
@@ -2,6 +2,8 @@ import { ToggleControl as OriginalToggleControl } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import PopoverMenuItem from 'calypso/components/popover-menu/item';
 
+// This is a fix to get around the fact that the original ToggleControl component doesn't support the disabled prop.
+// TODO: Remove this when the original ToggleControl component supports the disabled prop.
 const ToggleControl = OriginalToggleControl as React.ComponentType<
 	OriginalToggleControl.Props & {
 		disabled?: boolean;
