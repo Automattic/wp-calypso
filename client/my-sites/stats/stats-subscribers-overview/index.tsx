1eefb073b897344ef62dc25dc4bd0561ff6e19c6
Anna McPhee <anna.mcphee@automattic.com>
2023-05-02 14:50:47 -0500

    fix after disaster rebase

--- client/my-sites/stats/stats-subscribers-overview/index.tsx
+++ client/my-sites/stats/stats-subscribers-overview/index.tsx
@@ -1,53 +1,25 @@
 import { CountComparisonCard } from '@automattic/components';
+import { UseQueryResult } from '@tanstack/react-query';
 import { translate } from 'i18n-calypso';
 import React from 'react';
+import useSubscribersQuery from 'calypso/my-sites/stats/hooks/use-subscribers-query';
+
+interface SubscribersData {
+	period: string;
+	subscribers: number;
+	subscribers_change: number;
+}
+
+interface SubscribersDataResult {
+	data: SubscribersData[];
+	unit: string;
+	date: string;
+}
 
 interface SubscribersOverviewProps {
-	siteID: number;
+	siteId: number;
 }
 
-<<<<<<< HEAD
-function SubscribersOverviewCardStats( siteID: number ) {
-	const overviewCardStats = [
-		{
-			heading: translate( 'Today' ),
-			count: 1410,
-		},
-		{
-			heading: translate( '30 days ago' ),
-			count: 1080,
-			previousCount: 1080,
-		},
-		{
-			heading: translate( '60 days ago' ),
-			count: 1000,
-			previousCount: 1080,
-		},
-		{
-			heading: translate( '90 days ago' ),
-			count: 970,
-			previousCount: 1080,
-		},
-	];
-	// Use siteID to fetch or filter data if needed
-	return overviewCardStats;
-}
-
-const SubscribersOverview: React.FC< SubscribersOverviewProps > = ( { siteID } ) => {
-	const overviewCardStats = SubscribersOverviewCardStats( siteID );
-	return (
-		<div className="overview-cards-list">
-			{ overviewCardStats.map( ( overviewCardStat ) => (
-				<CountComparisonCard
-					key={ overviewCardStat.heading }
-					heading={ overviewCardStat.heading }
-					count={ overviewCardStat.count }
-					previousCount={ overviewCardStat.previousCount }
-					showValueTooltip
-					icon={ false }
-				/>
-			) ) }
-=======
 function extractCountsAtIndexes( subscribersData: SubscribersData[], indexes: number[] ): number[] {
 	return indexes.map( ( index ) => subscribersData[ index ]?.subscribers || 0 );
 }
@@ -72,7 +44,7 @@ function SubscribersOverviewCardStats( subscribersData: number[] ) {
 				: translate( '%d days ago', { args: day } ) ) as string,
 			count: count,
 			...( index !== 0 && {
-				previousCount: todayCount,
+				previousCount: todayCount - subscribersData[ index ],
 			} ),
 		};
 
@@ -117,17 +89,8 @@ const SubscribersOverview: React.FC< SubscribersOverviewProps > = ( { siteId } )
 					) ) }
 				</div>
 			) }
->>>>>>> f7b4867265 (Pass data to cards + fix i88n issue)
 		</div>
 	);
 };
-
-function SubscribersOverview() {
-	return (
-		<div className="subscribers-overview">
-			<SubscribersOverviewCards />
-		</div>
-	);
-}
 
 export default SubscribersOverview;
