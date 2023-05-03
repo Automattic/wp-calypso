c43d02c196d7aa5927d730184e34d53eee18babb
Jasper Kang <kangzjnet@gmail.com>
2023-05-04 10:34:06 +1200

    move files to ts

--- apps/odyssey-stats/src/lib/can-current-user.ts
+++ apps/odyssey-stats/src/lib/can-current-user.ts
@@ -1,5 +1,5 @@
 import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
 import config from './config-api';
 
-export default ( siteId, capability ) =>
+export default ( siteId: number, capability: string ) =>
 	canCurrentUser( config( 'intial_state' ) ?? {}, siteId, capability );
