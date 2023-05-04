0c2a3839ce8afaab8f9089e0f89316c2167062e8
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-05-04 13:00:00 +0800

    Revert "Pricing page: Add Backup FAQs info on the product lightbox. (#76025)" (#76571)
    This reverts commit a7ab1166d3b3ceecfb0e3077e25a86d74a61e89c.

--- packages/calypso-products/src/product-values.ts
+++ packages/calypso-products/src/product-values.ts
@@ -12,7 +12,6 @@ export { getJetpackProductShortName } from './get-jetpack-product-short-name';
 export { getJetpackProductTagline } from './get-jetpack-product-tagline';
 export { getJetpackProductWhatIsIncluded } from './get-jetpack-product-what-is-included';
 export { getJetpackProductBenefits } from './get-jetpack-product-benefits';
-export { getJetpackProductFAQs } from './get-jetpack-product-faqs';
 export { getJetpackProductRecommendedFor } from './get-jetpack-product-recommended-for';
 export { getProductClass } from './get-product-class';
 export { getProductTermVariants } from './get-product-term-variants';
