8119f273ecd3156b8201270281e74e0913ddbf0e
Valter Lorran <valterlorran@hotmail.com>
2023-05-04 17:01:57 -0300

    Add tests for the isValidWooExpressUpsell function

--- client/lib/analytics/ad-tracking/woo/test/is-wooexpress-upgrade.ts
+++ client/lib/analytics/ad-tracking/woo/test/is-wooexpress-upgrade.ts
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 import { PLAN_BUSINESS, PLAN_WOOEXPRESS_MEDIUM } from '@automattic/calypso-products';
+=======
+>>>>>>> 3ac2bf0cf1 (Add tests for the isValidWooExpressUpsell function)
 import { type ResponseCartProduct, getEmptyResponseCartProduct } from '@automattic/shopping-cart';
 import { isValidWooExpressUpsell } from '../is-wooexpress-upgrade';
 
@@ -6,7 +9,11 @@ describe( 'isValidWooExpressUpsell', () => {
 	test( 'should return true if the product is a Woo Express plan', () => {
 		const product: ResponseCartProduct = {
 			...getEmptyResponseCartProduct(),
+<<<<<<< HEAD
 			product_slug: PLAN_WOOEXPRESS_MEDIUM,
+=======
+			product_slug: 'wooexpress-medium-bundle-yearly',
+>>>>>>> 3ac2bf0cf1 (Add tests for the isValidWooExpressUpsell function)
 		};
 
 		expect( isValidWooExpressUpsell( product ) ).toBe( true );
@@ -14,7 +21,11 @@ describe( 'isValidWooExpressUpsell', () => {
 	test( 'should return false if the product is not a Woo Express plan', () => {
 		const product: ResponseCartProduct = {
 			...getEmptyResponseCartProduct(),
+<<<<<<< HEAD
 			product_slug: PLAN_BUSINESS,
+=======
+			product_slug: 'business-bundle',
+>>>>>>> 3ac2bf0cf1 (Add tests for the isValidWooExpressUpsell function)
 		};
 
 		expect( isValidWooExpressUpsell( product ) ).toBe( false );
