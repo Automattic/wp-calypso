bb326ab418565e1599bcf84a559c8f7634b83339
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-05-05 12:17:43 +0530

    Fix social previews build error (#76622)
    * Fix translation minification error
    
    * Update tsconfig.json
    
    * Add tslib dependency

--- packages/social-previews/src/index.ts
+++ packages/social-previews/src/index.ts
@@ -1,4 +1,4 @@
-export { default as FacebookPreview } from './facebook-preview';
+export * from './facebook-preview';
 export * from './google-search-preview';
 export * from './twitter-preview';
 export * from './linkedin-preview';
