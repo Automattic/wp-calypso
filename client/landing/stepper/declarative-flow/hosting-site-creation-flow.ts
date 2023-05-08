c2fb52b5c3e96c912987c12627a8a7b2ccf4eae6
Philip Jackson <p-jackson@live.com>
2023-05-08 18:16:36 +1200

    New /setup/new-hosted-site flow, for creating sites with hosting features enabled

---
+++ client/landing/stepper/declarative-flow/hosting-site-creation-flow.ts
@@ -0,0 +1,64 @@
+import { HOSTING_SITE_CREATION_FLOW } from '@automattic/onboarding';
+import { addQueryArgs } from '@wordpress/url';
+import {
+	setSignupCompleteSlug,
+	persistSignupDestination,
+	setSignupCompleteFlowName,
+} from 'calypso/signup/storageUtils';
+import { recordSubmitStep } from './internals/analytics/record-submit-step';
+import type { Flow, ProvidedDependencies } from './internals/types';
+
+const hosting: Flow = {
+	name: HOSTING_SITE_CREATION_FLOW,
+	useSteps() {
+		return [
+			{ slug: 'plans', asyncComponent: () => import( './internals/steps-repository/plans' ) },
+			{
+				slug: 'siteCreationStep',
+				asyncComponent: () => import( './internals/steps-repository/site-creation-step' ),
+			},
+			{
+				slug: 'processing',
+				asyncComponent: () => import( './internals/steps-repository/processing-step' ),
+			},
+		];
+	},
+	useStepNavigation( _currentStepSlug, navigate ) {
+		const flowName = this.name;
+
+		const submit = ( providedDependencies: ProvidedDependencies = {} ) => {
+			recordSubmitStep( providedDependencies, '', flowName, _currentStepSlug );
+
+			switch ( _currentStepSlug ) {
+				case 'plans':
+					return navigate( 'siteCreationStep' );
+
+				case 'siteCreationStep':
+					return navigate( 'processing' );
+
+				case 'processing': {
+					const destination = addQueryArgs( '/sites', {
+						'new-site': providedDependencies.siteSlug,
+					} );
+					persistSignupDestination( destination );
+					setSignupCompleteSlug( providedDependencies?.siteSlug );
+					setSignupCompleteFlowName( flowName );
+
+					return window.location.assign(
+						addQueryArgs(
+							`/checkout/${ encodeURIComponent(
+								( providedDependencies?.siteSlug as string ) ?? ''
+							) }`,
+							{ redirect_to: destination }
+						)
+					);
+				}
+			}
+			return providedDependencies;
+		};
+
+		return { submit };
+	},
+};
+
+export default hosting;
