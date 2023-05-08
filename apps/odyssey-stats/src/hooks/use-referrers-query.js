7dfd2014f7cd11551ebf88d32a2ddbed1f234b6d
Dognose <dognose24@users.noreply.github.com>
2023-05-09 05:30:02 +0800

    Stats Widget: Add external link support to Top Referrer items (#76638)
    * Add external link support to referrer links
    
    * yarn dedupe
    
    * Apply the first result as the nested group data

--- apps/odyssey-stats/src/hooks/use-referrers-query.js
+++ apps/odyssey-stats/src/hooks/use-referrers-query.js
@@ -10,12 +10,28 @@ export default function useReferrersQuery( siteId, period, num, date, summarize 
 		[ 'stats-widget', 'referrers', siteId, period, num, date, summarize, max ],
 		() => queryReferrers( siteId, { period, num, date, summarize, max } ),
 		{
-			select: ( data ) =>
-				data?.summary?.groups.map( ( group ) => ( {
-					...group,
-					title: group.name,
-					views: group.total,
-				} ) ),
+			select: ( data ) => {
+				// The groups' views count may not be in descending order
+				// since we use the first result for nest groups.
+				return data?.summary?.groups.map( ( group ) => {
+					// Get the first result as the nested group's data.
+					if ( Array.isArray( group.results ) && group.results.length > 0 ) {
+						const subGroup = group.results[ 0 ];
+
+						return {
+							...subGroup,
+							title: subGroup.name,
+							views: subGroup.views,
+						};
+					}
+
+					return {
+						...group,
+						title: group.name,
+						views: group.results.views,
+					};
+				} );
+			},
 			staleTime: 5 * 60 * 1000,
 		}
 	);
