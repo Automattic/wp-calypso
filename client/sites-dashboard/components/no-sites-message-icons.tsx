aa6066ccad2bac8b0ffd620a7c689463946410ce
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2023-05-08 18:13:14 -0300

    Add new empty state

---
+++ client/sites-dashboard/components/no-sites-message-icons.tsx
@@ -0,0 +1,370 @@
+export const CreateSiteIcon = () => (
+	<svg
+		width="187"
+		height="159"
+		viewBox="0 0 187 159"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<g filter="url(#filter0_d_104_13638)">
+			<rect x="3.5" y="2.11328" width="180" height="152.773" rx="3.02521" fill="white" />
+			<path
+				d="M3.5 5.78678C3.5 3.75796 5.14468 2.11328 7.1735 2.11328H179.827C181.855 2.11328 183.5 3.75796 183.5 5.78678V18.7519H3.5V5.78678Z"
+				fill="#0675C4"
+			/>
+			<circle cx="11.8192" cy="10.4325" r="2.26891" fill="white" />
+			<circle cx="19.3822" cy="10.4325" r="2.26891" fill="white" />
+			<circle cx="26.9452" cy="10.4325" r="2.26891" fill="white" />
+			<rect
+				opacity="0.5"
+				x="18.626"
+				y="33.8779"
+				width="45.8824"
+				height="31.2605"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="18.626"
+				y="71.1885"
+				width="45.8824"
+				height="14.1176"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="18.626"
+				y="88.3315"
+				width="45.8824"
+				height="14.1176"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="18.626"
+				y="108.5"
+				width="45.8824"
+				height="31.2605"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="70.5586"
+				y="33.8779"
+				width="45.8824"
+				height="6.05042"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="70.5586"
+				y="42.9536"
+				width="45.8824"
+				height="6.05042"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="70.5586"
+				y="52.0293"
+				width="45.8824"
+				height="6.05042"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="70.5586"
+				y="64.1299"
+				width="45.8824"
+				height="75.6303"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="122.491"
+				y="33.8779"
+				width="45.8824"
+				height="35.0325"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="122.491"
+				y="74.9609"
+				width="45.8824"
+				height="35.0325"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="122.491"
+				y="116.044"
+				width="45.8824"
+				height="5.88867"
+				rx="2.94434"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="122.491"
+				y="124.958"
+				width="45.8824"
+				height="5.88867"
+				rx="2.94434"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="122.491"
+				y="133.872"
+				width="45.8824"
+				height="5.88867"
+				rx="2.94434"
+				fill="#BBE0FA"
+			/>
+			<rect
+				x="3.5"
+				y="2.11328"
+				width="180"
+				height="152.773"
+				rx="3.02521"
+				stroke="#0675C4"
+				strokeWidth="1.51261"
+			/>
+		</g>
+		<defs>
+			<filter
+				id="filter0_d_104_13638"
+				x="0.743652"
+				y="0.356934"
+				width="185.512"
+				height="158.286"
+				filterUnits="userSpaceOnUse"
+				colorInterpolationFilters="sRGB"
+			>
+				<feFlood floodOpacity="0" result="BackgroundImageFix" />
+				<feColorMatrix
+					in="SourceAlpha"
+					type="matrix"
+					values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+					result="hardAlpha"
+				/>
+				<feOffset dy="1" />
+				<feGaussianBlur stdDeviation="1" />
+				<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
+				<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_104_13638" />
+				<feBlend
+					mode="normal"
+					in="SourceGraphic"
+					in2="effect1_dropShadow_104_13638"
+					result="shape"
+				/>
+			</filter>
+		</defs>
+	</svg>
+);
+
+export const ImportSiteIcon = () => (
+	<svg
+		width="187"
+		height="159"
+		viewBox="0 0 187 159"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<g filter="url(#filter0_d_104_13673)">
+			<rect x="3.25586" y="2.11328" width="140.672" height="111.933" rx="3.02521" fill="#F6F7F7" />
+			<path
+				d="M3.25586 5.78678C3.25586 3.75796 4.90054 2.11328 6.92936 2.11328H140.255C142.283 2.11328 143.928 3.75796 143.928 5.78678V18.7519H3.25586V5.78678Z"
+				fill="#8C8F94"
+			/>
+			<circle cx="11.5755" cy="10.4325" r="2.26891" fill="#F6F7F7" />
+			<circle cx="19.139" cy="10.4325" r="2.26891" fill="#F6F7F7" />
+			<circle cx="26.7015" cy="10.4325" r="2.26891" fill="#F6F7F7" />
+			<rect
+				x="3.25586"
+				y="2.11328"
+				width="140.672"
+				height="111.933"
+				rx="3.02521"
+				stroke="#8C8F94"
+				strokeWidth="1.51261"
+			/>
+		</g>
+		<g filter="url(#filter1_d_104_13673)">
+			<rect x="24.4326" y="28.0542" width="158.824" height="127.059" rx="3.02521" fill="white" />
+			<path
+				d="M24.4326 31.7277C24.4326 29.6989 26.0773 28.0542 28.1061 28.0542H179.583C181.611 28.0542 183.256 29.6989 183.256 31.7277V44.6929H24.4326V31.7277Z"
+				fill="#0675C4"
+			/>
+			<circle cx="32.7523" cy="36.3734" r="2.26891" fill="white" />
+			<circle cx="40.3148" cy="36.3734" r="2.26891" fill="white" />
+			<circle cx="47.8783" cy="36.3734" r="2.26891" fill="white" />
+			<rect
+				opacity="0.5"
+				x="39.5586"
+				y="59.8188"
+				width="27.605"
+				height="31.0893"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="39.5586"
+				y="96.9585"
+				width="27.605"
+				height="5.88867"
+				rx="2.94434"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="39.5586"
+				y="108.897"
+				width="27.605"
+				height="31.0893"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="73.2139"
+				y="59.8188"
+				width="27.605"
+				height="80.1681"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="106.87"
+				y="59.8188"
+				width="27.605"
+				height="56.2899"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="106.87"
+				y="122.159"
+				width="27.605"
+				height="5.88867"
+				rx="2.94434"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="106.87"
+				y="134.098"
+				width="27.605"
+				height="5.88867"
+				rx="2.94434"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="140.525"
+				y="59.8188"
+				width="27.605"
+				height="31.0893"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="140.525"
+				y="96.9585"
+				width="27.605"
+				height="31.0893"
+				rx="3.02521"
+				fill="#BBE0FA"
+			/>
+			<rect
+				opacity="0.5"
+				x="140.525"
+				y="134.099"
+				width="27.605"
+				height="5.88867"
+				rx="2.94434"
+				fill="#BBE0FA"
+			/>
+			<rect
+				x="24.4326"
+				y="28.0542"
+				width="158.824"
+				height="127.059"
+				rx="3.02521"
+				stroke="#0675C4"
+				strokeWidth="1.51261"
+			/>
+		</g>
+		<defs>
+			<filter
+				id="filter0_d_104_13673"
+				x="0.5"
+				y="0.356934"
+				width="146.185"
+				height="117.445"
+				filterUnits="userSpaceOnUse"
+				colorInterpolationFilters="sRGB"
+			>
+				<feFlood floodOpacity="0" result="BackgroundImageFix" />
+				<feColorMatrix
+					in="SourceAlpha"
+					type="matrix"
+					values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+					result="hardAlpha"
+				/>
+				<feOffset dy="1" />
+				<feGaussianBlur stdDeviation="1" />
+				<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
+				<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_104_13673" />
+				<feBlend
+					mode="normal"
+					in="SourceGraphic"
+					in2="effect1_dropShadow_104_13673"
+					result="shape"
+				/>
+			</filter>
+			<filter
+				id="filter1_d_104_13673"
+				x="21.6768"
+				y="26.2979"
+				width="164.336"
+				height="132.571"
+				filterUnits="userSpaceOnUse"
+				colorInterpolationFilters="sRGB"
+			>
+				<feFlood floodOpacity="0" result="BackgroundImageFix" />
+				<feColorMatrix
+					in="SourceAlpha"
+					type="matrix"
+					values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+					result="hardAlpha"
+				/>
+				<feOffset dy="1" />
+				<feGaussianBlur stdDeviation="1" />
+				<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0" />
+				<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_104_13673" />
+				<feBlend
+					mode="normal"
+					in="SourceGraphic"
+					in2="effect1_dropShadow_104_13673"
+					result="shape"
+				/>
+			</filter>
+		</defs>
+	</svg>
+);
--- client/sites-dashboard/components/no-sites-message.tsx
+++ client/sites-dashboard/components/no-sites-message.tsx
@@ -1,8 +1,10 @@
+import { isEnabled } from '@automattic/calypso-config';
 import { localizeUrl } from '@automattic/i18n-utils';
 import styled from '@emotion/styled';
 import { createInterpolateElement } from '@wordpress/element';
 import { useI18n } from '@wordpress/react-i18n';
 import EmptyContent from 'calypso/components/empty-content';
+import { CreateSiteIcon, ImportSiteIcon } from './no-sites-message-icons';
 
 const NoSitesLayout = styled( EmptyContent )`
 	display: flex;
@@ -31,6 +33,33 @@ const Title = styled.div`
 	font-size: 32px;
 	margin-block-end: 20px;
 `;
+
+const SetupLink = styled.a( {
+	boxSizing: 'border-box',
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	padding: '40px 32px 16px 32px',
+	gap: '12px',
+	background: '#FFFFFF',
+	border: '1px solid #DCDCDE',
+	borderRadius: '4px',
+	transition: '0.25s',
+
+	fontWeight: 500,
+	fontSize: '14px',
+	lineHeight: '20px',
+	color: 'var(--studio-gray-100)',
+
+	'&:hover, &:visited': {
+		color: 'var(--studio-gray-100)',
+	},
+
+	'&:hover, &:focus': {
+		border: '1px solid #A7AAAD',
+		boxShadow: '0px 3px 8px rgba(0, 0, 0, 0.12), 0px 3px 1px rgba(0, 0, 0, 0.04)',
+	},
+} );
 
 type SitesContainerProps = {
 	status: string;
@@ -157,6 +186,31 @@ export const NoSitesMessage = ( { status, statusSiteCount }: SitesContainerProps
 				}
 				illustration=""
 			/>
+		);
+	}
+
+	if ( isEnabled( 'hosting-onboarding-i2' ) ) {
+		return (
+			<NoSitesLayout
+				title={ <Title css={ { marginBlockEnd: 0 } }> { __( 'Add a site to start' ) } </Title> }
+				line={
+					<SecondaryText>
+						{ __( 'Create a brand new site or import an existing one' ) }
+					</SecondaryText>
+				}
+				illustration=""
+			>
+				<div css={ { display: 'flex', gap: 16 } }>
+					<SetupLink href="/setup/new-hosted-site">
+						<CreateSiteIcon />
+						{ __( 'Create a site' ) }
+					</SetupLink>
+					<SetupLink href="/setup/import-focused">
+						<ImportSiteIcon />
+						{ __( 'Import a site' ) }
+					</SetupLink>
+				</div>
+			</NoSitesLayout>
 		);
 	}
 
