6196dc51e75828dd142ed10bb5d37152b754d9f6
Kev <kevin.zoschke@automattic.com>
2023-05-10 02:19:06 -0400

    Social Previews: Add Tumblr preview as shared by others (#76460)
    * Social previews: add Facebook preview for post w/ custom image
    
    * Remove duplicated style
    
    * Social Previews: add Tumblr default link preview
    
    * Fix render of default preview
    
    * Revert CSS changes
    
    * Update text domain
    
    * Use better names for components
    
    ---------
    
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>

--- packages/social-previews/src/tumblr-preview/full-preview.tsx
+++ packages/social-previews/src/tumblr-preview/full-preview.tsx
@@ -1,9 +1,11 @@
+import { ExternalLink } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { SectionHeading } from '../shared/section-heading';
-import TumblrLinkPreview from './link-preview';
+import { TumblrLinkPreview } from './link-preview';
+import { TumblrPostPreview } from './post-preview';
 import { TumblrPreviewProps } from './types';
 
-const TumblrFullPreview: React.FC< TumblrPreviewProps > = ( props ) => {
+export const TumblrFullPreview: React.FC< TumblrPreviewProps > = ( props ) => {
 	return (
 		<div className="social-preview tumblr-preview">
 			<section className="social-preview__section tumblr-preview__section">
@@ -16,10 +18,27 @@ const TumblrFullPreview: React.FC< TumblrPreviewProps > = ( props ) => {
 				<p className="social-preview__section-desc">
 					{ __( 'This is what your social post will look like on Tumblr:', 'social-previews' ) }
 				</p>
+				<TumblrPostPreview { ...props } />
+			</section>
+			<section className="social-preview__section tumblr-preview__section">
+				<SectionHeading level={ props.headingsLevel }>
+					{
+						// translators: refers to a link on Tumblr
+						__( 'Link preview', 'social-previews' )
+					}
+				</SectionHeading>
+				<p className="social-preview__section-desc">
+					{ __(
+						'This is what it will look like when someone shares the link to your WordPress post on Tumblr.',
+						'social-previews'
+					) }
+					&nbsp;
+					<ExternalLink href="https://jetpack.com/support/jetpack-social-image-generator">
+						{ __( 'Learn more about links', 'social-previews' ) }
+					</ExternalLink>
+				</p>
 				<TumblrLinkPreview { ...props } />
 			</section>
 		</div>
 	);
 };
-
-export default TumblrFullPreview;
--- packages/social-previews/src/tumblr-preview/post/header/index.tsx
+++ packages/social-previews/src/tumblr-preview/post/header/index.tsx
@@ -1,12 +1,10 @@
 import { __ } from '@wordpress/i18n';
 import TumblrPostIcon from '../icons';
-import type { TumblrUser } from '../../types';
+import type { TumblrPreviewProps } from '../../types';
 
 import './styles.scss';
 
-type Props = {
-	user?: TumblrUser;
-};
+type Props = Pick< TumblrPreviewProps, 'user' >;
 
 const TumblrPostHeader: React.FC< Props > = ( { user } ) => (
 	<div className="tumblr-preview__post-header">
