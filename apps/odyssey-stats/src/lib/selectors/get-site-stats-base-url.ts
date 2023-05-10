b04339907260e95d4190d7b556da3d7a38b6ee20
Jasper Kang <kangzjnet@gmail.com>
2023-05-11 10:11:48 +1200

    use isSiteWpcomAtomic instead

--- apps/odyssey-stats/src/lib/selectors/get-site-stats-base-url.ts
+++ apps/odyssey-stats/src/lib/selectors/get-site-stats-base-url.ts
@@ -1,9 +1,9 @@
-import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
+import isSiteWpcomAtomic from 'calypso/state/selectors/is-site-wpcom-atomic';
 import config from '../config-api';
 import getOdysseyStatsBaseUrl from './get-odyssey-stats-base-url';
 
 const getSiteStatsBaseUrl = ( siteId: number ): string =>
-	isSiteAutomatedTransfer( config( 'intial_state' ), siteId )
+	isSiteWpcomAtomic( config( 'intial_state' ), siteId )
 		? 'https://wordpress.com'
 		: `${ getOdysseyStatsBaseUrl() }#!`;
 
