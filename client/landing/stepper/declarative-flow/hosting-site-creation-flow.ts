18363d247d0a942aaee95550bbf93ec12e24298d
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2023-05-11 16:10:48 -0300

    Wire site title preference

--- client/landing/stepper/declarative-flow/hosting-site-creation-flow.ts
+++ client/landing/stepper/declarative-flow/hosting-site-creation-flow.ts
@@ -1,10 +1,13 @@
 import { HOSTING_SITE_CREATION_FLOW } from '@automattic/onboarding';
+import { useDispatch } from '@wordpress/data';
 import { addQueryArgs } from '@wordpress/url';
+import { useEffect } from 'react';
 import {
 	setSignupCompleteSlug,
 	persistSignupDestination,
 	setSignupCompleteFlowName,
 } from 'calypso/signup/storageUtils';
+import { ONBOARD_STORE } from '../stores';
 import { recordSubmitStep } from './internals/analytics/record-submit-step';
 import type { Flow, ProvidedDependencies } from './internals/types';
 import './internals/hosting-site-creation-flow.scss';
@@ -29,14 +32,18 @@ const hosting: Flow = {
 		];
 	},
 	useStepNavigation( _currentStepSlug, navigate ) {
+		const { setSiteTitle } = useDispatch( ONBOARD_STORE );
+
 		const flowName = this.name;
 
 		const submit = ( providedDependencies: ProvidedDependencies = {} ) => {
 			recordSubmitStep( providedDependencies, '', flowName, _currentStepSlug );
 
 			switch ( _currentStepSlug ) {
-				case 'options':
+				case 'options': {
+					setSiteTitle( providedDependencies.siteTitle );
 					return navigate( 'plans' );
+				}
 				case 'plans':
 					return navigate( 'siteCreationStep' );
 
@@ -65,6 +72,18 @@ const hosting: Flow = {
 		};
 
 		return { submit };
+	},
+	useSideEffect() {
+		const { resetOnboardStore } = useDispatch( ONBOARD_STORE );
+
+		useEffect(
+			() => {
+				resetOnboardStore();
+			},
+			// We only need to reset the store when the flow is mounted.
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+			[]
+		);
 	},
 };
 
