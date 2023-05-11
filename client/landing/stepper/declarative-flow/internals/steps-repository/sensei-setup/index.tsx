3c660c1363a5b160113c3bbc2c379774b9d25e77
Renatho De Carli Rosa <renatho@gmail.com>
2023-05-11 19:00:46 -0300

    Align action to the bottom

--- client/landing/stepper/declarative-flow/internals/steps-repository/sensei-setup/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/sensei-setup/index.tsx
@@ -124,13 +124,15 @@ const SenseiSetup: Step = ( { navigation } ) => {
 						) ) }
 					</div>
 					{ ! isDesktop && preview }
-					<button className="sensei-button" disabled={ ! siteTitle } onClick={ handleSubmit }>
-						{ __( 'Continue' ) }
-					</button>
-					<p className="sensei-style-notice">
-						<Gridicon icon="notice-outline" />
-						{ __( 'You can change all of this later, too.' ) }
-					</p>
+					<div className="sensei-onboarding-action">
+						<button className="sensei-button" disabled={ ! siteTitle } onClick={ handleSubmit }>
+							{ __( 'Continue' ) }
+						</button>
+						<p className="sensei-style-notice">
+							<Gridicon icon="notice-outline" />
+							{ __( 'You can change all of this later, too.' ) }
+						</p>
+					</div>
 				</div>
 				{ isDesktop && preview }
 			</div>
