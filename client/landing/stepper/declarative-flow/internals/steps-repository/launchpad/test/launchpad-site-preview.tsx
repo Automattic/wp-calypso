51a55b0b1f9900f13076e11e07c64a15833bac44
sixhours <calobee@gmail.com>
2023-05-11 11:21:43 -0400

    Update unit test.

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/test/launchpad-site-preview.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/test/launchpad-site-preview.tsx
@@ -4,17 +4,25 @@ import { getEnableEditOverlay } from '../launchpad-site-preview';
 describe( 'getEnableEditOverlay', () => {
 	test( 'should return false if flow is a newsletter flow', () => {
 		const flow = NEWSLETTER_FLOW;
-		expect( getEnableEditOverlay( flow ) ).toBe( false );
+		const isFSETheme = true;
+		expect( getEnableEditOverlay( flow, isFSETheme ) ).toBe( false );
 	} );
 
 	test( 'should return false if flow is a start writing flow', () => {
 		const flow = START_WRITING_FLOW;
-		expect( getEnableEditOverlay( flow ) ).toBe( false );
+		const isFSETheme = true;
+		expect( getEnableEditOverlay( flow, isFSETheme ) ).toBe( false );
+	} );
+
+	test( 'should return false if theme is not FSE enabled', () => {
+		const isFSETheme = false;
+		expect( getEnableEditOverlay( null, isFSETheme ) ).toBe( false );
 	} );
 
 	test( 'default return true if flow is not specified or null', () => {
 		const flow = 'nonexistent-flow';
-		expect( getEnableEditOverlay( null ) ).toBe( true );
-		expect( getEnableEditOverlay( flow ) ).toBe( true );
+		const isFSETheme = true;
+		expect( getEnableEditOverlay( null, isFSETheme ) ).toBe( true );
+		expect( getEnableEditOverlay( flow, isFSETheme ) ).toBe( true );
 	} );
 } );
