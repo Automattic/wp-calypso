f156e4107e05a6e5156bede312fb8c75b213a3f1
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-05-11 12:45:46 +0530

    Add Twitter multiple previews

--- packages/social-previews/src/twitter-preview/text.tsx
+++ packages/social-previews/src/twitter-preview/text.tsx
@@ -1,11 +1,9 @@
 import { preparePreviewText } from '../helpers';
 import { TextProps } from './types';
 
-export const Text: React.FC< TextProps > = ( { text, cardUrl } ) => {
+export const Text: React.FC< TextProps > = ( { text, url } ) => {
 	// If the text ends with the card URL, remove it.
-	const deCardedText = text.endsWith( cardUrl )
-		? text.substring( 0, text.lastIndexOf( cardUrl ) )
-		: text;
+	const deCardedText = text.endsWith( url ) ? text.substring( 0, text.lastIndexOf( url ) ) : text;
 
 	const __html = preparePreviewText( deCardedText, { platform: 'twitter' } );
 
