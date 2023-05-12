e04423bfb0ee6a452347d3bc5a7c8135d8af0bf8
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2023-05-12 00:09:46 -0300

    Add conditions to show Callout

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-domain-upsell-callout/class-wpcom-domain-upsell-callout.php
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-domain-upsell-callout/class-wpcom-domain-upsell-callout.php
@@ -38,9 +38,14 @@ class WPCOM_Domain_Upsell_Callout {
 	}
 
 	/**
-	 * Enqueue block editor assets.
+	 * Check if should show the callout and enqueue the script and style.
 	 */
 	public function enqueue_script_and_style() {
+		// Don't show the callout if the user has a custom domain, is unverified, or is on a P2.
+		if ( $this->should_not_show_callout() ) {
+			return;
+		}
+
 		$asset_file          = include plugin_dir_path( __FILE__ ) . 'dist/wpcom-domain-upsell-callout.asset.php';
 		$script_dependencies = $asset_file['dependencies'];
 		$version             = $asset_file['version'];
@@ -71,13 +76,50 @@ class WPCOM_Domain_Upsell_Callout {
 	}
 
 	/**
-	 * Get current site details.
+	 * Check if the callout should be shown.
+	 *
+	 * @return boolean
 	 */
-	public function get_current_site() {
-		return array(
-			'launchpad_screen' => get_option( 'launchpad_screen' ),
-			'site_intent'      => get_option( 'site_intent' ),
-		);
+	private function should_not_show_callout() {
+		$user_id          = get_current_user_id();
+		$blog_id          = get_current_blog_id();
+		$email_unverified = \Email_Verification::is_email_unverified( $user_id );
+
+		return $this->blog_has_custom_domain( $blog_id ) || $email_unverified || $this->blog_is_p2( $blog_id );
 	}
+
+	/**
+	 * Check if the blog has a custom domain.
+	 *
+	 * @param int $blog_id // The blog id.
+	 * @return boolean
+	 */
+	private function blog_has_custom_domain( $blog_id ) {
+		$domain_mappings = \Domain_Mapping::find_by_blog_id( $blog_id ) ?? array();
+
+		foreach ( $domain_mappings as $domain_mapping ) {
+			if ( ! $domain_mapping->get_domain()->is_wpcom_tld() ) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Check if the blog is a P2.
+	 *
+	 * @param int $blog_id // The blog id.
+	 * @return boolean
+	 */
+	private function blog_is_p2( $blog_id ) {
+		if ( defined( 'IS_ATOMIC' ) && IS_ATOMIC ) {
+			$is_p2 = false;
+		} else {
+			$is_p2 = \WPForTeams\is_wpforteams_site( $blog_id );
+		}
+		return $is_p2;
+	}
+
 }
+
 add_action( 'init', array( __NAMESPACE__ . '\WPCOM_Domain_Upsell_Callout', 'init' ) );
