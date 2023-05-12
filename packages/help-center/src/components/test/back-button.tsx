29efb4c69039b30d0000d79ea27eab156e50d84c
Igor Klimer <github@gpg.klimer.eu>
2023-05-12 18:06:48 +0000

    Less mocking, more real testing

--- packages/help-center/src/components/test/back-button.tsx
+++ packages/help-center/src/components/test/back-button.tsx
@@ -13,9 +13,6 @@ const mockNavigate = jest.fn();
 jest.mock( 'react-router-dom', () => ( {
 	...jest.requireActual( 'react-router-dom' ),
 	useNavigate: () => mockNavigate,
-	useLocation: jest.fn().mockImplementation( () => ( {
-		key: 'somethingrandom',
-	} ) ),
 } ) );
 
 const testEntries = [ { pathname: '/' }, { pathname: '/contact-form' } ];
