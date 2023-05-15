7ac5d299c12fa3f0d2a0cac7b400db5d132ebdac
arthur791004 <arthur.chu@automattic.com>
2023-05-15 11:21:49 +0800

    Pattern Assembler: May 2023 Update copy (#76528)
    * Pattern Assembler: Update copy
    
    * Address feedback
    
    * Address feedback

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/screen-font-pairings.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/screen-font-pairings.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@automattic/components';
 import { FontPairingVariations } from '@automattic/global-styles';
-import { useHasEnTranslation } from '@automattic/i18n-utils';
 import { __experimentalNavigatorBackButton as NavigatorBackButton } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import NavigatorHeader from './navigator-header';
@@ -24,23 +23,14 @@ const ScreenFontPairings = ( {
 	onDoneClick,
 }: Props ) => {
 	const translate = useTranslate();
-	const hasEnTranslation = useHasEnTranslation();
 
 	return (
 		<>
 			<NavigatorHeader
 				title={ translate( 'Fonts' ) }
-				description={
-					hasEnTranslation(
-						'Choose from our curated font pairings when you upgrade to the Premium plan or above.'
-					)
-						? translate(
-								'Choose from our curated font pairings when you upgrade to the Premium plan or above.'
-						  )
-						: translate(
-								'Select from our hand-picked font pairings or expanded library when you upgrade to the Premium plan or higher.'
-						  )
-				}
+				description={ translate(
+					'Choose from our curated font pairings when you upgrade to the Premium plan or above.'
+				) }
 				isPremium
 				onBack={ onBack }
 			/>
@@ -59,7 +49,7 @@ const ScreenFontPairings = ( {
 					onClick={ onDoneClick }
 					primary
 				>
-					{ translate( 'Save' ) }
+					{ translate( 'Save fonts' ) }
 				</NavigatorBackButton>
 			</div>
 		</>
