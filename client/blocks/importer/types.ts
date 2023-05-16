ebb14347e7486ceed60c651f39374a0620944fea
Wayne Wang <iverson0217@gmail.com>
2023-05-16 15:35:28 +0800

    Update content only link and remove unused functions

--- client/blocks/importer/types.ts
+++ client/blocks/importer/types.ts
@@ -17,7 +17,6 @@ export type StepNavigator = {
 	goToWpAdminWordPressPluginPage?: () => void;
 	navigate?: ( path: string ) => void;
 	goToAddDomainPage?: () => void;
-	goToImportListStep?: () => void;
 };
 
 export interface ImportError {
