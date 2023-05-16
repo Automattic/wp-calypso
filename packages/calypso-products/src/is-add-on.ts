68d8fe868b6a020a618fe64b62b2d6ac6a58b90a
Sharpirate <stoynov.bogomil@gmail.com>
2023-05-16 12:49:52 +0300

    add tiered volume check

--- packages/calypso-products/src/is-add-on.ts
+++ packages/calypso-products/src/is-add-on.ts
@@ -1,9 +1,15 @@
 import { isCustomDesign } from './is-custom-design';
 import { isNoAds } from './is-no-ads';
+import { isTieredVolumeSpaceAddon } from './is-tiered-volume-space-addon';
 import { isUnlimitedThemes } from './is-unlimited-themes';
 import type { WithSnakeCaseSlug, WithCamelCaseSlug } from './types';
 
 export function isAddOn( product: WithSnakeCaseSlug | WithCamelCaseSlug ): boolean {
 	// Right now the definition of an "add-on" just comes from a hardcoded list.
-	return isCustomDesign( product ) || isNoAds( product ) || isUnlimitedThemes( product );
+	return (
+		isCustomDesign( product ) ||
+		isNoAds( product ) ||
+		isUnlimitedThemes( product ) ||
+		isTieredVolumeSpaceAddon( product )
+	);
 }
