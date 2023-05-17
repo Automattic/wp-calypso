482e721a44c7ccf1e0cec36068c044444c1067ef
edanzer <edanzer@gmail.com>
2023-05-17 14:25:37 -0600

    Remove unneeded changes from hook

--- client/landing/stepper/declarative-flow/internals/steps-repository/components/setup-form/hooks/use-setup-form-initial-values.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/components/setup-form/hooks/use-setup-form-initial-values.ts
@@ -5,15 +5,12 @@ const useSetupFormInitialValues = () => {
 	const site = useSite();
 	const [ siteTitle, setComponentSiteTitle ] = useState( '' );
 	const [ tagline, setTagline ] = useState( '' );
-	const [ paidSubscribers, setPaidSubscribers ] = useState( false );
 
 	useEffect( () => {
 		if ( site?.name || site?.description ) {
 			setComponentSiteTitle( site?.name || '' );
 			setTagline( site?.description || '' );
 		}
-
-		// @TODO: pull goals && set paid subscribers truthy if need
 	}, [ site?.name, site?.description ] );
 
 	return {
@@ -21,8 +18,6 @@ const useSetupFormInitialValues = () => {
 		setComponentSiteTitle,
 		tagline,
 		setTagline,
-		paidSubscribers,
-		setPaidSubscribers,
 	};
 };
 
