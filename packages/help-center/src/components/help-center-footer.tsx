3747b2721484d792ccb80d33274aadc3b7c487e2
Daniel Walmsley <goldsounds@gmail.com>
2023-05-17 15:19:18 -0700

    Fetch all support articles from the same endpoint (#76734)
    * Hack Calypso help to pull all articles from the same endpoint
    
    * checkpoint
    
    * Linting
    
    * Fix missing variables (need to keep siteIntent for later
    
    * Revert unrelated change
    
    * Revert more unrelated changes
    
    * Remove disabled tests that were breaking the build
    
    * Armwrestling the type system
    
    * Revert unrelated changes
    
    * Add more tracks events
    
    * Remove more Sibyl parts
    
    * Remove all trace of Sibyl for now
    
    * Add more tracking, thumbs up/down, and button state tweaks
    
    * Add external/internal icons to search results
    
    * Add better styling, remove warnings, better type hints
    
    * Remove unrelated changes
    
    * Better (but not perfect) state handling
    
    * Revert unnecessary changes
    
    * Fix small formatting issue
    
    * Set email subject/message from search query
    
    * More informative error
    
    * Make search result titles more consistent
    
    * Fix naming of contact form tracks events
    
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>
    
    * Remove stray and incorrect comment
    
    ---------
    
    Co-authored-by: Omar Alshaker <omar@omaralshaker.com>

--- packages/help-center/src/components/help-center-footer.tsx
+++ packages/help-center/src/components/help-center-footer.tsx
@@ -1,15 +1,13 @@
 import { CardFooter } from '@wordpress/components';
-import classnames from 'classnames';
 import { Route, Routes } from 'react-router-dom';
 import { HelpCenterContactButton } from './help-center-contact-page';
 
 const HelpCenterFooter: React.FC = () => {
-	const className = classnames( 'help-center__container-footer' );
-
 	return (
-		<CardFooter className={ className }>
+		<CardFooter className="help-center__container-footer">
 			<Routes>
 				<Route path="/" element={ <HelpCenterContactButton /> } />
+				<Route path="*" element={ null } />
 			</Routes>
 		</CardFooter>
 	);
