2d44e5bcc73b7e184c25332b21bbe5baebbe14ac
Eoin Gallagher <eoin@automattic.com>
2023-05-17 14:07:09 +0100

    Reader: Update related posts to pass variable size param (#76891)
    * Update related posts to pass variable size param
    
    * fix tests
    
    * fix tests

--- client/state/reader/related-posts/actions.js
+++ client/state/reader/related-posts/actions.js
@@ -11,7 +11,7 @@ import { SCOPE_ALL, SCOPE_SAME, SCOPE_OTHER } from './utils';
 
 import 'calypso/state/reader/init';
 
-export function requestRelatedPosts( siteId, postId, scope = SCOPE_ALL ) {
+export function requestRelatedPosts( siteId, postId, scope = SCOPE_ALL, size = 2 ) {
 	return function ( dispatch ) {
 		dispatch( {
 			type: READER_RELATED_POSTS_REQUEST,
@@ -33,11 +33,11 @@ export function requestRelatedPosts( siteId, postId, scope = SCOPE_ALL ) {
 		}
 
 		if ( scope === SCOPE_SAME ) {
-			query.size_local = 2;
+			query.size_local = size;
 			query.size_global = 0;
 		} else if ( scope === SCOPE_OTHER ) {
 			query.size_local = 0;
-			query.size_global = 2;
+			query.size_global = size;
 		}
 
 		return wpcom.req.get( `/read/site/${ siteId }/post/${ postId }/related`, query ).then(
