65caa5f73138295c619ca0c414c599e38d8b09d9
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2023-05-18 08:00:05 +0200

    Hosting onboarding: add site options step to `/setup/new-hosted-site` (#76854)
    * Add data center picker to site options step
    
    * Add site options image
    
    * Fetch suggestions
    
    * Add suggestion to the step if site title is empty
    
    * Wire site title preference
    
    * Only reset store if browsing the first step
    
    * Wire data center preference
    
    * Hide data center picker if choosing the recommended one
    
    * Move the data center picker to its own block
    
    * Reset flow for every fresh load
    
    * Submit plan object instead of spreading the properites
    
    * Fix go back behavior
    
    * Branch site options to multiple components according to the chosen flow
    
    * Wrap data center description on smaller screens
    
    * Separate data picker styles
    
    * Do not disable form submissions and display the errors instead
    
    * Hide scrollbar
    
    * Address coloring issues
    
    * Use new endpoint
    
    * Import MinimalRequestCartProduct as type only
    
    * Reinstate the onChange guard
    
    The refactor in 9124a2f96bee4531f3d97cf1c4e2de0c769aadd4 removed a guard that prevented changes to state until the site had finished loading.
    
    ---------
    
    Co-authored-by: Philip Jackson <p-jackson@live.com>

--- client/blocks/data-center-picker/index.tsx
+++ client/blocks/data-center-picker/index.tsx
@@ -10,12 +10,14 @@ import dfwImg from 'calypso/assets/images/data-center-picker/dfw-240x180.png';
 import worldImg from 'calypso/assets/images/data-center-picker/world-1082x180.png';
 import ExternalLink from 'calypso/components/external-link';
 import FormRadiosBar from 'calypso/components/forms/form-radios-bar';
+import FormSelect from 'calypso/components/forms/form-select';
 
 interface ExternalProps {
 	value: string;
 	onChange: ( newValue: string ) => void;
 	onClickHidePicker?: () => void;
 	onClickShowPicker?: () => void;
+	compact?: boolean;
 }
 
 type Props = ExternalProps & LocalizeProps;
@@ -55,7 +57,7 @@ const DataCenterOptions = [
 	},
 ];
 
-const Form = styled.form( {
+const Form = styled.div( {
 	maxWidth: '564px',
 } );
 
@@ -82,6 +84,44 @@ const AutomaticFormLabel = styled.label( {
 	cursor: 'pointer',
 } );
 
+const CompactLabel = styled.label( {
+	cursor: 'pointer',
+	display: 'flex',
+	padding: '24px 16px',
+	border: '2px solid #DCDCDE',
+	borderRadius: '4px',
+
+	'&:hover': {
+		background: '#F9FBFD',
+	},
+
+	'&:has(input[type=radio]:checked)': {
+		background: '#F9FBFD',
+		borderColor: '#0675C4',
+	},
+} );
+
+const PickerOptions = styled.div( { display: 'flex', flexDirection: 'column', gap: '16px' } );
+
+const PickerOption = styled.div( {
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'flex-start',
+	alignItems: 'flex-start',
+	gap: '8px',
+} );
+
+const PickerOptionLabel = styled.span( { lineHeight: '20px', fontWeight: 600, color: '#101517' } );
+
+const PickerRadio = styled.input( {
+	margin: '2px 16px 0 0',
+} );
+
+const CompactPickerSelect = styled( FormSelect )( {
+	width: 'fit-content',
+	borderRadius: '4px',
+} );
+
 const FormLabelThumbnail = styled.div( {
 	display: 'flex',
 	width: '100%',
@@ -103,20 +143,92 @@ const FormLabelThumbnailImg = styled.img( {
 	objectFit: 'cover',
 } );
 
+const RECOMMENDED_DATA_CENTER = '';
+
 const DataCenterPicker = ( {
 	onChange,
 	onClickHidePicker = () => null,
 	onClickShowPicker = () => null,
 	translate,
 	value,
+	compact = false,
 }: Props ) => {
 	const [ isFormShowing, setIsFormShowing ] = useState( false );
 
 	const onCancel = () => {
-		onChange( '' );
+		onChange( RECOMMENDED_DATA_CENTER );
 		onClickHidePicker();
 		setIsFormShowing( false );
 	};
+
+	const isRecommendedDataCenterPicked = value === RECOMMENDED_DATA_CENTER;
+
+	if ( compact ) {
+		return (
+			<div className="data-center-picker">
+				<FormHeadingContainer>
+					<FormHeading>{ translate( 'Primary data center' ) }</FormHeading>
+				</FormHeadingContainer>
+				<FormDescription className="data-center-picker__description">
+					{ translate(
+						'Your site will replicate in real-time to a second data center in a different region. {{supportLink}}Learn more{{/supportLink}}.',
+						{
+							components: {
+								supportLink: (
+									<ExternalLink
+										target="_blank"
+										href={ localizeUrl(
+											'https://wordpress.com/support/choose-your-sites-primary-data-center/'
+										) }
+									/>
+								),
+							},
+						}
+					) }
+				</FormDescription>
+				<PickerOptions className="data-center-picker__options" role="radiogroup">
+					<CompactLabel>
+						<PickerRadio
+							className="form-radio"
+							type="radio"
+							name="geo_affinity"
+							onChange={ () => onChange( RECOMMENDED_DATA_CENTER ) }
+							checked={ isRecommendedDataCenterPicked }
+						/>
+						<PickerOption>
+							<PickerOptionLabel>{ translate( 'Optimal data center' ) }</PickerOptionLabel>
+							<span>{ translate( 'Automatically place my site in the optimal data center' ) }</span>
+						</PickerOption>
+					</CompactLabel>
+					<CompactLabel>
+						<PickerRadio
+							className="form-radio"
+							type="radio"
+							name="geo_affinity"
+							onChange={ () => onChange( DataCenterOptions[ 0 ].value ) }
+							checked={ ! isRecommendedDataCenterPicked }
+						/>
+						<PickerOption>
+							<PickerOptionLabel>{ translate( 'Custom data center' ) }</PickerOptionLabel>
+							<span>{ translate( 'Choose the data center that best suits your needs.' ) }</span>
+							{ ! isRecommendedDataCenterPicked && (
+								<CompactPickerSelect
+									onChange={ ( event ) => onChange( event.currentTarget.value ) }
+									value={ value }
+								>
+									{ DataCenterOptions.map( ( option ) => (
+										<option key={ option.value } value={ option.value }>
+											{ option.label }
+										</option>
+									) ) }
+								</CompactPickerSelect>
+							) }
+						</PickerOption>
+					</CompactLabel>
+				</PickerOptions>
+			</div>
+		);
+	}
 
 	return (
 		<div>
@@ -177,8 +289,8 @@ const DataCenterPicker = ( {
 								type="radio"
 								name="geo_affinity"
 								value=""
-								checked={ value === '' }
-								onChange={ () => onChange( '' ) }
+								checked={ isRecommendedDataCenterPicked }
+								onChange={ () => onChange( RECOMMENDED_DATA_CENTER ) }
 							/>
 							<span>{ translate( 'Automatically place my site in the optimal data center' ) }</span>
 						</AutomaticFormLabel>
