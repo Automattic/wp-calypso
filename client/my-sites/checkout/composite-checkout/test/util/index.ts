ac70fe0aa0663d412c8bce47221de31530d77440
Payton Swick <payton@foolord.com>
2023-05-18 13:48:44 -0400

    Update checkout mock country data to include tax_vendor_info

--- client/my-sites/checkout/composite-checkout/test/util/index.ts
+++ client/my-sites/checkout/composite-checkout/test/util/index.ts
@@ -83,6 +83,12 @@ export const countryList: CountryListItem[] = [
 		name: 'Australia',
 		has_postal_codes: true,
 		vat_supported: false,
+		tax_vendor_info: {
+			country_code: 'AU',
+			tax_name: 'GST',
+			vat_id: 'AU-vat-id',
+			address: [ 'VAT Address' ],
+		},
 	},
 	{
 		code: 'ES',
@@ -90,6 +96,12 @@ export const countryList: CountryListItem[] = [
 		has_postal_codes: true,
 		vat_supported: true,
 		tax_country_codes: [ 'ES' ],
+		tax_vendor_info: {
+			country_code: 'ES',
+			tax_name: 'VAT',
+			vat_id: 'ES-vat-id',
+			address: [ 'VAT Address' ],
+		},
 	},
 	{
 		code: 'CA',
@@ -99,6 +111,12 @@ export const countryList: CountryListItem[] = [
 		tax_needs_subdivision: true,
 		vat_supported: true,
 		tax_country_codes: [ 'CA' ],
+		tax_vendor_info: {
+			country_code: 'CA',
+			tax_name: 'VAT',
+			vat_id: 'CA-vat-id',
+			address: [ 'VAT Address' ],
+		},
 	},
 	{
 		code: 'CH',
@@ -115,6 +133,12 @@ export const countryList: CountryListItem[] = [
 		tax_needs_organization: true, // added for testing, not present in API data
 		vat_supported: true,
 		tax_country_codes: [ 'GB', 'XI' ],
+		tax_vendor_info: {
+			country_code: 'GB',
+			tax_name: 'VAT',
+			vat_id: 'GB-vat-id',
+			address: [ 'VAT Address' ],
+		},
 	},
 	{
 		code: 'IN',
@@ -129,6 +153,12 @@ export const countryList: CountryListItem[] = [
 		has_postal_codes: true,
 		tax_needs_organization: true,
 		vat_supported: false,
+		tax_vendor_info: {
+			country_code: 'JP',
+			tax_name: 'VAT',
+			vat_id: 'JP-vat-id',
+			address: [ 'VAT Address' ],
+		},
 	},
 	{
 		code: 'NO',
