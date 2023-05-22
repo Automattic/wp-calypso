475f897b919d2cc92b9d2ef73573f6544a87998b
Miguel <miguel.san.segundo@automattic.com>
2023-05-22 14:24:51 +0700

    Clean patterns-data file

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/patterns-data.ts
+++
@@ -1,9 +0,0 @@
-import useCategoryAll from './hooks/use-category-all';
-import useDotcomPatterns from './hooks/use-dotcom-patterns';
-
-const useAllPatterns = ( lang: string | undefined ) => {
-	const dotcomPatterns = useDotcomPatterns( lang );
-	return useCategoryAll( dotcomPatterns );
-};
-
-export { useAllPatterns };
