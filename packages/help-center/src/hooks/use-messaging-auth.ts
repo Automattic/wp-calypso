ba5f7fc1c0872878cc52e4cad782c6391760ac66
Igor Klimer <github@gpg.klimer.eu>
2023-05-22 15:49:15 +0000

    Missed a spot

--- packages/help-center/src/hooks/use-messaging-auth.ts
+++ packages/help-center/src/hooks/use-messaging-auth.ts
@@ -29,7 +29,7 @@ function requestMessagingAuth() {
 		  } as APIFetchOptions );
 }
 
-export default function useMessagingAuth( zendeskKey: string, enabled: boolean ) {
+export default function useMessagingAuth( zendeskKey: string | false, enabled: boolean ) {
 	return useQuery< MessagingAuth >( [ 'getMessagingAuth', zendeskKey ], requestMessagingAuth, {
 		staleTime: 7 * 24 * 60 * 60 * 1000, // 1 week (JWT is actually 2 weeks, but lets be on the safe side)
 		enabled,
