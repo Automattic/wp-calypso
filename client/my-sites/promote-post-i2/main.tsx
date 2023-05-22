ee2ae43cbffdb8187e82704f3c78dc6339b673c3
Ben Zhu <ben.zhu@a8c.com>
2023-05-22 16:48:46 +1200

    Enable widget v2 and turn on for wpcalypso so we can test

--- client/my-sites/promote-post-i2/main.tsx
+++ client/my-sites/promote-post-i2/main.tsx
@@ -5,7 +5,7 @@ import { useTranslate } from 'i18n-calypso';
 import { debounce } from 'lodash';
 import moment from 'moment';
 import { useEffect, useMemo, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import DocumentHead from 'calypso/components/data/document-head';
 import QueryPosts from 'calypso/components/data/query-posts';
 import QuerySiteStats from 'calypso/components/data/query-site-stats';
@@ -19,6 +19,8 @@ import useCampaignsStatsQuery from 'calypso/data/promote-post/use-promote-post-c
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
 import memoizeLast from 'calypso/lib/memoize-last';
 import { isWpMobileApp } from 'calypso/lib/mobile-app';
+import { recordDSPEntryPoint } from 'calypso/lib/promote-post';
+import { useRouteModal } from 'calypso/lib/route-modal';
 import CampaignsList from 'calypso/my-sites/promote-post-i2/components/campaigns-list';
 import PostsList from 'calypso/my-sites/promote-post-i2/components/posts-list';
 import PostsListBanner from 'calypso/my-sites/promote-post-i2/components/posts-list-banner';
@@ -92,6 +94,9 @@ export default function PromotedPosts( { tab }: Props ) {
 	const [ expandedCampaigns, setExpandedCampaigns ] = useState< number[] >( [] );
 	const [ alreadyScrolled, setAlreadyScrolled ] = useState< boolean >( false );
 	const selectedSiteId = selectedSite?.ID || 0;
+	const dispatch = useDispatch();
+	const keyValue = 'post-0'; // post 0 means to open post selector in widget
+	const { openModal } = useRouteModal( 'blazepress-widget', keyValue );
 
 	const products = useSelector( ( state ) => {
 		const products = getPostsForQuery( state, selectedSiteId, queryProducts );
@@ -276,6 +281,11 @@ export default function PromotedPosts( { tab }: Props ) {
 		</div>
 	);
 
+	const onClickPromote = async () => {
+		openModal();
+		dispatch( recordDSPEntryPoint( 'promoted_posts-header' ) );
+	};
+
 	return (
 		<Main wideLayout className="promote-post-i2">
 			<DocumentHead title={ translate( 'Advertising - Redesign page!' ) } />
@@ -295,6 +305,9 @@ export default function PromotedPosts( { tab }: Props ) {
 				<div className="promote-post-i2__top-bar-buttons">
 					<Button compact className="posts-list-banner__learn-more">
 						<InlineSupportLink supportContext="advertising" showIcon={ false } />
+					</Button>
+					<Button primary onClick={ onClickPromote }>
+						{ translate( 'Promote' ) }
 					</Button>
 				</div>
 			</div>
