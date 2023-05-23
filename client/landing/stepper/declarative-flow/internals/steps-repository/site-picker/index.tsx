d62076db5378bc90e66a842d6100b2dc4b14302c
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-05-23 10:11:59 +0200

    Import focused flow: Site picker: create and select site implementation (#77188)
    * Hide navigation back button
    
    For the back button, it doesn't make sense to exist since the previous step could be the login screen, or in
    a case when the user is logged in, this will be the first step of the flow.
    
    * Site picker: introduce onCreateSiteClick prop callback
    
    * Site picker: Handle create site use case
    
    * Site picker: Introduce select site handler
    
    * Rename callback method
    
    * Site picker: Implement site select handler
    
    * Remove headerText constant
    
    * Move SITE_PICKER_FILTER_CONFIG to the dedicated constants file
    
    * Adapt sitePicker step from the migrationHandler point of view

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-picker/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-picker/index.tsx
@@ -3,6 +3,7 @@ import {
 	DEFAULT_SITE_LAUNCH_STATUS_GROUP_VALUE,
 	GroupableSiteLaunchStatuses,
 } from '@automattic/sites';
+import { useI18n } from '@wordpress/react-i18n';
 import React from 'react';
 import DocumentHead from 'calypso/components/data/document-head';
 import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
@@ -10,36 +11,46 @@ import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { SitesDashboardQueryParams } from 'calypso/sites-dashboard/components/sites-content-controls';
 import SitePicker from './site-picker';
 import type { Step } from '../../types';
+import type { SiteExcerptData } from 'calypso/data/sites/site-excerpt-types';
 
 import './styles.scss';
 
 const SitePickerStep: Step = function SitePickerStep( { navigation } ) {
-	const headerText = 'Pick your destination';
+	const { __ } = useI18n();
 	const page = Number( useQuery().get( 'page' ) ) || 1;
 	const search = useQuery().get( 'search' ) || '';
 	const status =
 		( useQuery().get( 'status' ) as GroupableSiteLaunchStatuses ) ||
 		DEFAULT_SITE_LAUNCH_STATUS_GROUP_VALUE;
 
-	const goBack = () => {
-		// Go back logic goes here
-	};
 	const onQueryParamChange = ( params: Partial< SitesDashboardQueryParams > ) => {
-		navigation.submit?.( { queryParams: params } );
+		navigation.submit?.( { action: 'update-query', queryParams: params } );
+	};
+
+	const createNewSite = () => {
+		navigation.submit?.( { action: 'create-site' } );
+	};
+
+	const selectSite = ( site: SiteExcerptData ) => {
+		navigation.submit?.( { action: 'select-site', site } );
 	};
 
 	return (
 		<>
-			<DocumentHead title={ headerText } />
+			<DocumentHead title={ __( 'Pick your destination' ) } />
 			<StepContainer
 				stepName="site-picker"
-				hideSkip={ true }
-				goBack={ goBack }
+				hideBack={ true }
+				hideSkip={ false }
+				skipLabelText={ __( 'Skip and create a new site' ) }
+				goNext={ createNewSite }
 				stepContent={
 					<SitePicker
 						page={ page }
 						search={ search }
 						status={ status }
+						onCreateSite={ createNewSite }
+						onSelectSite={ selectSite }
 						onQueryParamChange={ onQueryParamChange }
 					/>
 				}
