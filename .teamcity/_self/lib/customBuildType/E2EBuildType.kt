5c4ea122cb1b6fce44165dc8d29eb88fd965b231
dpasque <dan.speckhard.pasque@automattic.com>
2023-05-23 14:11:15 -0500

    Just directly reference the vcs ID

--- .teamcity/_self/lib/customBuildType/E2EBuildType.kt
+++ .teamcity/_self/lib/customBuildType/E2EBuildType.kt
@@ -84,7 +84,7 @@ open class E2EBuildType(
 
 		vcs {
 			root(Settings.WpCalypso)
-			root(AbsoluteId("%WPCOM_VCS_ROOT_ID%"), "-:.")
+			root(AbsoluteId("wpcom"), "-:.")
 			cleanCheckout = true
 		}
 
