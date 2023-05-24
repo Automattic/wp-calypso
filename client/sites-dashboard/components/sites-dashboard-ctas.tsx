9929f9dc0fb9dcfc6d275bee3e1b897ef4ef2856
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2023-05-24 13:21:50 -0300

    Gate new-hosted-site flow for developer accounts

--- client/sites-dashboard/components/sites-dashboard-ctas.tsx
+++ client/sites-dashboard/components/sites-dashboard-ctas.tsx
@@ -1,6 +1,8 @@
 import { isEnabled } from '@automattic/calypso-config';
 import { useI18n } from '@wordpress/react-i18n';
+import { useSelector } from 'react-redux';
 import { addQueryArgs } from 'calypso/lib/url';
+import getUserSetting from 'calypso/state/selectors/get-user-setting';
 import {
 	CreateSiteIcon,
 	CREATE_SITE_ICON_SPARKLES,
@@ -11,8 +13,10 @@ import { SparklingCTA } from './sparkling-cta';
 
 export const CreateSiteCTA = () => {
 	const { __ } = useI18n();
+	const isDevAccount = useSelector( ( state ) => getUserSetting( state, 'is_dev_account' ) );
 
-	const newSiteURL = isEnabled( 'hosting-onboarding-i2' ) ? '/setup/new-hosted-site' : '/start';
+	const newSiteURL =
+		isEnabled( 'hosting-onboarding-i2' ) && isDevAccount ? '/setup/new-hosted-site' : '/start';
 
 	return (
 		<SparklingCTA
