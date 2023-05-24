3155dc4f93e4192d8eb721b5f02d72016a468d5e
arthur <arthur.chu@automattic.com>
2023-05-24 18:22:45 +0800

    Use ExperimentalBlockEditorProvider to initialize Iframe

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/pattern-assembler-container.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/pattern-assembler-container.tsx
@@ -1,5 +1,6 @@
 import { BlockRendererProvider, PatternsRendererProvider } from '@automattic/block-renderer';
 import { PLACEHOLDER_SITE_ID } from '@automattic/data-stores/src/site/constants';
+import { ExperimentalBlockEditorProvider } from '@automattic/global-styles';
 import StepperLoader from '../../components/stepper-loader';
 import type { SiteInfo } from '@automattic/block-renderer';
 
@@ -12,6 +13,14 @@ interface Props {
 	isNewSite?: boolean;
 }
 
+// Use fake assets to eliminate the compatStyles without the id.
+// See https://github.com/WordPress/gutenberg/blob/f77958cfc13c02b0c0e6b9b697b43bbcad4ba40b/packages/block-editor/src/components/iframe/index.js#L127
+const BLOCK_EDITOR_SETTINGS = {
+	__unstableResolvedAssets: {
+		styles: '<style id="" />',
+	},
+};
+
 const PatternAssemblerContainer = ( {
 	siteId,
 	stylesheet,
@@ -20,26 +29,26 @@ const PatternAssemblerContainer = ( {
 	siteInfo,
 	isNewSite,
 }: Props ) => (
-	<BlockRendererProvider
-		siteId={ siteId }
-		stylesheet={ stylesheet }
-		useInlineStyles
-		placeholder={ <StepperLoader /> }
-	>
-		<PatternsRendererProvider
-			// Site used to render site-related things on the previews,
-			// such as the logo, title, tagline, pages, posts, etc.
-			// For the newly created site, we use the placeholder site to render the content.
-			// Otherwise, we use the current site to display the site-related blocks.
-			siteId={ isNewSite ? PLACEHOLDER_SITE_ID : siteId }
+	<ExperimentalBlockEditorProvider settings={ BLOCK_EDITOR_SETTINGS }>
+		<BlockRendererProvider
+			siteId={ siteId }
 			stylesheet={ stylesheet }
-			patternIds={ patternIds }
-			// Use siteInfo to overwrite site-related things such as title, and tagline.
-			siteInfo={ siteInfo }
+			useInlineStyles
+			placeholder={ <StepperLoader /> }
 		>
-			{ children }
-		</PatternsRendererProvider>
-	</BlockRendererProvider>
+			<PatternsRendererProvider
+				// Site used to render site-related things on the previews,
+				// such as the logo, title, and tagline.
+				siteId={ PLACEHOLDER_SITE_ID }
+				stylesheet={ stylesheet }
+				patternIds={ patternIds }
+				// Use siteInfo to overwrite site-related things such as title, and tagline.
+				siteInfo={ siteInfo }
+			>
+				{ children }
+			</PatternsRendererProvider>
+		</BlockRendererProvider>
+	</ExperimentalBlockEditorProvider>
 );
 
 export default PatternAssemblerContainer;
