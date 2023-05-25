4a17cce852234001dbeb3d181d805bce0335d0b8
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2023-05-25 00:35:26 -0300

    Adding await when cleaning the siteIntent

--- client/landing/stepper/declarative-flow/start-writing.ts
+++ client/landing/stepper/declarative-flow/start-writing.ts
@@ -102,7 +102,7 @@ const startWriting: Flow = {
 					// If the user's site has just been launched.
 					if ( providedDependencies?.blogLaunched && providedDependencies?.siteSlug ) {
 						// Remove the site_intent.
-						setIntentOnSite( providedDependencies?.siteSlug, '' );
+						await setIntentOnSite( providedDependencies?.siteSlug, '' );
 
 						// If the user launched their site with a plan or domain in their cart, redirect them to
 						// checkout before sending them home.
