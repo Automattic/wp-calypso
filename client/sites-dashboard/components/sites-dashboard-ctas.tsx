06afe89af0b6399c77be2addf0551ec8e3e2b5eb
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2023-05-25 20:31:22 -0300

    Add source query parameter to import URL

--- client/sites-dashboard/components/sites-dashboard-ctas.tsx
+++ client/sites-dashboard/components/sites-dashboard-ctas.tsx
@@ -1,7 +1,9 @@
 import { useI18n } from '@wordpress/react-i18n';
+import { addQueryArgs } from '@wordpress/url';
 import { useSelector } from 'react-redux';
 import getCurrentQueryArguments from 'calypso/state/selectors/get-current-query-arguments';
 import { useSitesDashboardCreateSiteUrl } from '../hooks/use-sites-dashboard-create-site-url';
+import { TRACK_SOURCE_NAME } from '../utils';
 import { EmptyStateCTA } from './empty-state-cta';
 
 export const CreateSiteCTA = () => {
@@ -31,7 +33,10 @@ export const MigrateSiteCTA = () => {
 		<EmptyStateCTA
 			description={ __( 'Bring a site to WordPress.com' ) }
 			label={ __( 'Migrate a site' ) }
-			target="/start/import"
+			target={ addQueryArgs( '/start/import', {
+				source: TRACK_SOURCE_NAME,
+				ref: 'calypso-nosites',
+			} ) }
 		/>
 	);
 };
