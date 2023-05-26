0b0cf8260ed73fa2f83b3f36d493c85bac732e5c
Jeroen Pfeil <jeroen.pfeil@automattic.com>
2023-05-26 16:02:38 +0700

    Remove redundant classes and fix behaviour of back button

--- client/landing/stepper/declarative-flow/internals/steps-repository/migration-error/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/migration-error/index.tsx
@@ -6,37 +6,31 @@ import { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 
 const MigrationError: Step = function ( props ) {
-	const { navigation } = props;
-	const { submit, goBack } = navigation;
+	const { submit } = props.navigation;
 	const translate = useTranslate();
+
+	function goToMigrationHandler() {
+		submit?.( { url: 'migrationHandler' } );
+	}
 
 	function renderError() {
 		return (
-			<>
-				<ErrorMessage
-					onBackToStart={ () => submit?.( { url: 'migrationHandler' } ) }
-					onBackToStartText={ translate( 'Try again' ) }
-				/>
-			</>
+			<ErrorMessage
+				onBackToStart={ goToMigrationHandler }
+				onBackToStartText={ translate( 'Try again' ) }
+			/>
 		);
 	}
 	return (
 		<StepContainer
-			className={ classnames(
-				'import__onboarding-page',
-				'import-layout__center',
-				'importer-wrapper',
-				'import__error-message',
-				'importer-wrapper__wordpress',
-				'is-wide-layout'
-			) }
-			stepName="importer-step"
+			className={ classnames( 'import__onboarding-page', 'import__error-message' ) }
+			stepName="migration-error"
 			hideSkip={ true }
 			hideFormattedHeader={ true }
 			isWideLayout={ true }
-			goBack={ goBack }
 			stepContent={ renderError() }
 			recordTracksEvent={ recordTracksEvent }
+			goBack={ goToMigrationHandler }
 		/>
 	);
 };
