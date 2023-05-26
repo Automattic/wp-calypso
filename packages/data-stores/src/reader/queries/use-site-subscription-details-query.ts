6640c9cde0f0800d82b41d8e03b60da9a8803c8c
Maciej Grabowski <mashikag@users.noreply.github.com>
2023-05-26 13:02:33 +0200

    Subscriptions management: Add 'Subscription not found' state (#77396)
    * Subscriptions management: Add 'Subscription not found' state
    
    * Fix some missed type adjustments
    
    * Fix type issues and use the correct (consistent) type of blog ID in react-query keys
    
    * Fix import issue
    
    * Fix import
    
    * Pass the blog_id from URL params to site subscription details

--- packages/data-stores/src/reader/queries/use-site-subscription-details-query.ts
+++ packages/data-stores/src/reader/queries/use-site-subscription-details-query.ts
@@ -1,16 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import { callApi } from '../helpers';
 import { useCacheKey, useIsLoggedIn, useIsQueryEnabled } from '../hooks';
-import type { SiteSubscriptionDetails, SiteSubscriptionDetailsAPIResponse } from '../types';
+import type { SiteSubscriptionDetailsResponse } from '../types';
 
 const useSiteSubscriptionDetailsQuery = ( siteId: string ) => {
 	const { isLoggedIn } = useIsLoggedIn();
 	const enabled = useIsQueryEnabled();
 	const cacheKey = useCacheKey( [ 'read', 'site-subscription-details', siteId ] );
-	return useQuery< SiteSubscriptionDetails >( {
+	return useQuery( {
 		queryKey: cacheKey,
 		queryFn: async () => {
-			const subscriptionDetails = await callApi< SiteSubscriptionDetailsAPIResponse >( {
+			const subscriptionDetails = await callApi< SiteSubscriptionDetailsResponse >( {
 				path: '/read/sites/' + siteId + '/subscription-details',
 				isLoggedIn,
 				apiNamespace: 'wpcom/v2',
