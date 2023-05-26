aa6311ebba7413b063f2d04e24e9474095b021fe
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2023-05-26 17:07:20 -0300

    Reset state on slug change

--- client/sites-dashboard/components/with-atomic-transfer.tsx
+++ client/sites-dashboard/components/with-atomic-transfer.tsx
@@ -8,16 +8,7 @@ const PollAtomicTransfer = ( { site, children }: WithAtomicTransferProps ) => {
 	return children( result );
 };
 
-type AtomicTransferResult =
-	| { wasTransferring: false }
-	| ( { wasTransferring: true } & ReturnType< typeof useCheckSiteTransferStatus > );
-
-interface WithAtomicTransferProps {
-	site: SiteExcerptData;
-	children: ( props: AtomicTransferResult ) => React.ReactElement | null;
-}
-
-export const WithAtomicTransfer = ( { site, children }: WithAtomicTransferProps ) => {
+const MaybePollAtomicTransfer = ( { site, children }: WithAtomicTransferProps ) => {
 	if ( site.is_wpcom_atomic ) {
 		return children( { wasTransferring: false } );
 	}
@@ -35,4 +26,17 @@ export const WithAtomicTransfer = ( { site, children }: WithAtomicTransferProps 
 	}
 
 	return <PollAtomicTransfer site={ site } children={ children } />;
+};
+
+type AtomicTransferResult =
+	| { wasTransferring: false }
+	| ( { wasTransferring: true } & ReturnType< typeof useCheckSiteTransferStatus > );
+
+interface WithAtomicTransferProps {
+	site: SiteExcerptData;
+	children: ( props: AtomicTransferResult ) => React.ReactElement | null;
+}
+
+export const WithAtomicTransfer = ( props: WithAtomicTransferProps ) => {
+	return <MaybePollAtomicTransfer key={ props.site.slug } { ...props } />;
 };
