febbd8173cb14db2739b2d9043fe1d5e58281878
Paulo Trentin <paulo@paulotrentin.com.br>
2023-05-26 10:24:30 -0300

    Improved docs

--- client/state/selectors/is-blaze-enabled.js
+++ client/state/selectors/is-blaze-enabled.js
@@ -3,7 +3,8 @@ import { getSiteOption } from 'calypso/state/sites/selectors';
 /**
  * Returns if the specified site can Blaze posts.
  *
- * @param  {Object}  state Global state tree
+ * @param  {Object}  state    Global state tree
+ * @param  {number}  siteId   Site ID
  * @returns {boolean}  True if the site can blaze posts, false otherwise.
  */
 export default function isBlazeEnabled( state, siteId ) {
