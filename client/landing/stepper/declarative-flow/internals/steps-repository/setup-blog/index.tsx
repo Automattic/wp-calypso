55be137de83f012093a92fe4f7878d1990e03b02
Dusty Reagan <dusty@dustyreagan.com>
2023-05-26 11:52:41 -0500

    Blog onboarding: Create design first flow (#77222)
    * Create design first flow
    
    * Design first flow proof of concept
    
    * Added start-writing path to design-first flow steps
    
    * Hidden admin dashboard link and updated flow title
    
    * Unit tests for design-first flow added
    
    * Doesn't show the domain at the sidebar before the user chooses one
    
    * Disabled Launch site untill plan is selected
    
    * Removed Writing post task while we don't have it in the design flow
    
    * Design flow uses the new pricing grid system
    
    * Update getPlanTypes for design-first flow
    
    * Review adjustments
    
    * Refactor on conditions for start-writing and design-first as isBlogOnboarding
    
    * Using a more broad naming on content function
    
    ---------
    
    Co-authored-by: Paulo Trentin <paulo@paulotrentin.com.br>

--- client/landing/stepper/declarative-flow/internals/steps-repository/setup-blog/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/setup-blog/index.tsx
@@ -1,4 +1,4 @@
-import { StepContainer, isStartWritingFlow } from '@automattic/onboarding';
+import { StepContainer } from '@automattic/onboarding';
 import { useDispatch } from '@wordpress/data';
 import { createInterpolateElement } from '@wordpress/element';
 import { useTranslate } from 'i18n-calypso';
@@ -36,8 +36,7 @@ const SetupBlog: Step = ( { navigation, flow } ) => {
 	const { siteTitle, setComponentSiteTitle, tagline, setTagline } = useSetupFormInitialValues();
 
 	useEffect( () => {
-		// Clear site title and show placeholder for the flows below
-		if ( isStartWritingFlow( flow ) && siteTitle === 'Site Title' ) {
+		if ( siteTitle === 'Site Title' ) {
 			setComponentSiteTitle( '' );
 		}
 	}, [ flow, setComponentSiteTitle, siteTitle ] );
