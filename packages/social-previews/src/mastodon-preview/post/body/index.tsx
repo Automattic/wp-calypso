e0cc61baaa8707848dac70b4ae6a1285763f7595
Kevin Zoschke <kevin.zoschke@automattic.com>
2023-05-26 10:28:57 -0400

    Social-previews: add hashtag support for Mastodon

--- packages/social-previews/src/mastodon-preview/post/body/index.tsx
+++ packages/social-previews/src/mastodon-preview/post/body/index.tsx
@@ -1,10 +1,17 @@
-import { stripHtmlTags } from '../../../helpers';
-import { mastodonBody, mastodonUrl } from '../../helpers';
+import { stripHtmlTags, preparePreviewText } from '../../../helpers';
+import { mastodonUrl, BODY_LENGTH, URL_LENGTH } from '../../helpers';
 import type { MastodonPreviewProps } from '../../types';
 
 import './styles.scss';
 
 type Props = MastodonPreviewProps & { children?: React.ReactNode };
+
+const body = ( text: string, offset = 0 ) => {
+	return preparePreviewText( text, {
+		platform: 'mastodon',
+		maxChars: BODY_LENGTH - URL_LENGTH - offset,
+	} );
+};
 
 const MastonPostBody: React.FC< Props > = ( props ) => {
 	const { title, description, customText, url, children } = props;
@@ -12,18 +19,18 @@ const MastonPostBody: React.FC< Props > = ( props ) => {
 	let bodyTxt;
 
 	if ( customText ) {
-		bodyTxt = <p>{ mastodonBody( customText ) }</p>;
+		bodyTxt = <p>{ body( customText ) }</p>;
 	} else if ( description ) {
 		const renderedTitle = stripHtmlTags( title );
 
 		bodyTxt = (
 			<>
 				<p>{ renderedTitle }</p>
-				<p>{ mastodonBody( description, renderedTitle.length ) }</p>
+				<p>{ body( description, renderedTitle.length ) }</p>
 			</>
 		);
 	} else {
-		bodyTxt = <p>{ mastodonBody( title ) }</p>;
+		bodyTxt = <p>{ body( title ) }</p>;
 	}
 
 	return (
