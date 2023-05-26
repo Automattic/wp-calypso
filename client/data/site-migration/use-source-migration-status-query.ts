96729bb9fe20f8fe6be5e66f2d7a200210c140d3
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-05-26 12:57:48 +0200

    Import flow: Replace useSiteQuery with useSourceMigrationStatusQuery (#77395)
    * Extend useSourceMigrationStatusQuery to support siteSlug as well
    
    * Replace obsolete useQuery for fetching source site id

--- client/data/site-migration/use-source-migration-status-query.ts
+++ client/data/site-migration/use-source-migration-status-query.ts
@@ -1,19 +1,21 @@
 import { SourceSiteMigrationDetails } from '@automattic/data-stores/src/site';
 import { useQuery } from '@tanstack/react-query';
 import wp from 'calypso/lib/wp';
-import type { SiteId } from 'calypso/types';
+import type { SiteId, SiteSlug } from 'calypso/types';
 
-export const useSourceMigrationStatusQuery = ( sourceId: SiteId | undefined ) => {
+export const useSourceMigrationStatusQuery = (
+	sourceIdOrSlug: SiteId | SiteSlug | null | undefined
+) => {
 	return useQuery( {
-		queryKey: [ 'source-migration-status', sourceId ],
+		queryKey: [ 'source-migration-status', sourceIdOrSlug ],
 		queryFn: (): Promise< SourceSiteMigrationDetails > =>
 			wp.req.get( {
-				path: '/migrations/from-source/' + encodeURIComponent( sourceId as number ),
+				path: '/migrations/from-source/' + encodeURIComponent( sourceIdOrSlug as string ),
 				apiNamespace: 'wpcom/v2',
 			} ),
 		meta: {
 			persist: false,
 		},
-		enabled: !! sourceId,
+		enabled: !! sourceIdOrSlug,
 	} );
 };
