0418ea5f4958e7c8d9b26c189d075d093ce7370d
Paulo Marcos Trentin <paulo@paulotrentin.com.br>
2023-05-26 11:09:45 -0300

    Updated isBlazeEnabled selector to avoid null is not an object error (#77438)
    * Updated isBlazeEnabled selector to avoid null is not an object error
    
    * Added unit tests and exported isBlazeEnabled as a common selector
    
    * PR review improvements
    
    * Improved docs
    
    * Used non primary WordPress site ID as test siteId

---
+++ client/state/selectors/is-blaze-enabled.js
@@ -0,0 +1,12 @@
+import { getSiteOption } from 'calypso/state/sites/selectors';
+
+/**
+ * Returns if the specified site can Blaze posts.
+ *
+ * @param  {Object}  state    Global state tree
+ * @param  {number}  siteId   Site ID
+ * @returns {boolean}  True if the site can blaze posts, false otherwise.
+ */
+export default function isBlazeEnabled( state, siteId ) {
+	return !! getSiteOption( state, siteId, 'can_blaze' ) || false;
+}
