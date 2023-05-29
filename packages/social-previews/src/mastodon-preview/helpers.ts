9597dee9f276076c02d2355c44c63535b1cd8138
Kev <kevin.zoschke@automattic.com>
2023-05-29 10:20:38 -0400

    Social previews: fix Mastodon hashtag (#77456)
    * Social previews: fix Mastodon hashtag
    
    * Fix address display

--- packages/social-previews/src/mastodon-preview/helpers.ts
+++ packages/social-previews/src/mastodon-preview/helpers.ts
@@ -6,10 +6,14 @@ import {
 	preparePreviewText,
 	Formatter,
 } from '../helpers';
+import { DEFAULT_MASTODON_INSTANCE } from './constants';
+import { MastodonAddressDetails } from './types';
 
 const TITLE_LENGTH = 200;
 const BODY_LENGTH = 500;
 const URL_LENGTH = 30;
+
+const ADDRESS_PATTERN = /^@([^@]*)@([^@]*)$/i;
 
 export const mastodonTitle: Formatter = ( text ) =>
 	firstValid(
@@ -17,13 +21,25 @@ export const mastodonTitle: Formatter = ( text ) =>
 		hardTruncation( TITLE_LENGTH )
 	)( stripHtmlTags( text ) ) || '';
 
-export const mastodonBody = ( text: string, offset = 0 ) => {
+export const mastodonBody = ( text: string, options: { offset: number; instance: string } ) => {
+	const { instance, offset } = options;
+
 	return preparePreviewText( text, {
 		platform: 'mastodon',
 		maxChars: BODY_LENGTH - URL_LENGTH - offset,
+		hashtagDomain: instance,
 	} );
 };
 
 export const mastodonUrl: Formatter = ( text ) =>
 	firstValid( shortEnough( URL_LENGTH ), hardTruncation( URL_LENGTH ) )( stripHtmlTags( text ) ) ||
 	'';
+
+export const getMastodonAddressDetails = ( address: string ): MastodonAddressDetails => {
+	const matches = address.match( ADDRESS_PATTERN );
+
+	return {
+		username: matches?.[ 1 ] || '',
+		instance: matches?.[ 2 ] || DEFAULT_MASTODON_INSTANCE,
+	};
+};
