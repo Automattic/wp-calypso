721dd867221bffb4f888aa00f0ea537fa88be3f9
Matt Wiebe <wiebe@automattic.com>
2023-05-29 15:20:03 -0500

    More elegant react-query usage

--- client/my-sites/site-settings/reading-fediverse-settings/hooks.js
+++ client/my-sites/site-settings/reading-fediverse-settings/hooks.js
@@ -2,28 +2,20 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import wpcom from 'calypso/lib/wp';
 
 export const useActivityPubStatus = ( blogId ) => {
-	const key = [ 'activitypub/status', blogId ];
-	const path = `/sites/${ blogId }/activitypub/status`;
+	const queryKey = [ 'activitypub/status', blogId ];
+	const reqArgs = {
+		path: `/sites/${ blogId }/activitypub/status`,
+		apiNamespace: 'wpcom/v2',
+	};
 
 	const { data, isInitialLoading, isError } = useQuery( {
-		queryKey: key,
-		queryFn: () => {
-			return wpcom.req.get( {
-				path,
-				apiNamespace: 'wpcom/v2',
-			} );
-		},
+		queryKey,
+		queryFn: () => wpcom.req.get( reqArgs ),
 	} );
 	const queryClient = useQueryClient();
 	const { mutate, isLoading } = useMutation( {
-		mutationFn: ( enabled ) => {
-			return wpcom.req.post( {
-				path,
-				body: { enabled },
-				apiNamespace: 'wpcom/v2',
-			} );
-		},
-		onSuccess: ( { enabled } ) => queryClient.setQueryData( key, { enabled } ),
+		mutationFn: ( enabled ) => wpcom.req.post( { body: { enabled }, ...reqArgs } ),
+		onSuccess: ( responseData ) => queryClient.setQueryData( queryKey, responseData ),
 	} );
 
 	return {
