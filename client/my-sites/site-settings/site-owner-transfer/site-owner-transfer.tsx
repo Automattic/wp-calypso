f65bd046ba299359dfb2b2212f4fae1ac4f14cdd
Antony Agrios <antonyagrios@gmail.com>
2023-05-29 16:33:56 +0300

    Site transfer: Add container component
    In order to work more effectively to [Site Transfers: Implement the final design for the initiating transfer UI #2553](https://github.com/Automattic/dotcom-forge/issues/2553), this pr adds a container component so the work of the above issue can be easily split
    
    Related to https://github.com/Automattic/dotcom-forge/issues/2553
    
    Test
    
    1. Create a simple site
    2. Navigate to `Settings > General > Transfer your site`
    3. Make sure that everything is working as before. (You can visit wpcalypso for reference)

---
+++ client/my-sites/site-settings/site-owner-transfer/site-owner-transfer.tsx
@@ -0,0 +1,41 @@
+import { useTranslate } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
+import ActionPanel from 'calypso/components/action-panel';
+import FormattedHeader from 'calypso/components/formatted-header';
+import HeaderCake from 'calypso/components/header-cake';
+import Main from 'calypso/components/main';
+import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import StartSiteOwnerTransfer from './start-site-owner-transfer';
+
+const SiteOwnerTransfer = () => {
+	const selectedSiteId = useSelector( ( state ) => getSelectedSiteId( state ) );
+	const selectedSiteSlug = useSelector( ( state ) => getSelectedSiteSlug( state ) );
+
+	const translate = useTranslate();
+	if ( ! selectedSiteId || ! selectedSiteSlug ) {
+		return null;
+	}
+
+	return (
+		<Main>
+			<FormattedHeader
+				headerText={ translate( 'Site Transfer' ) }
+				subHeaderText={ translate( 'Transfer your site to another WordPress.com user.' ) }
+				align="left"
+			/>
+			<PageViewTracker
+				path="/settings/start-site Transfer/:site"
+				title="Settings > Start Site Transfer"
+			/>
+			<HeaderCake backHref={ '/settings/general/' + selectedSiteSlug } isCompact={ true }>
+				<h1>{ translate( 'Site Transfer' ) }</h1>
+			</HeaderCake>
+			<ActionPanel>
+				<StartSiteOwnerTransfer />
+			</ActionPanel>
+		</Main>
+	);
+};
+
+export default SiteOwnerTransfer;
