f52f095802ce9041a6f798c388bc937da8dba9c2
Wayne Wang <iverson0217@gmail.com>
2023-05-30 19:57:21 +0800

    Adjust unauthorized component

--- client/blocks/importer/components/not-authorized/index.tsx
+++ client/blocks/importer/components/not-authorized/index.tsx
@@ -8,11 +8,14 @@ import React, { useEffect } from 'react';
 interface Props {
 	onBackToStart?: () => void;
 	onStartBuilding?: () => void;
+	onStartBuildingText?: string;
 }
 
 const NotAuthorized: React.FunctionComponent< Props > = ( props ) => {
 	const { __ } = useI18n();
-	const { onBackToStart, onStartBuilding } = props;
+	const { onBackToStart, onStartBuilding, onStartBuildingText } = props;
+
+	const startBuildingText = onStartBuildingText ?? __( 'Start building' );
 
 	useEffect( () => {
 		recordTracksEvent( 'calypso_site_importer_unauthorized' );
@@ -27,7 +30,7 @@ const NotAuthorized: React.FunctionComponent< Props > = ( props ) => {
 
 					<div className="import__buttons-group">
 						{ onStartBuilding && (
-							<NextButton onClick={ onStartBuilding }>{ __( 'Start building' ) }</NextButton>
+							<NextButton onClick={ onStartBuilding }>{ startBuildingText }</NextButton>
 						) }
 						{ onBackToStart && (
 							<div>
