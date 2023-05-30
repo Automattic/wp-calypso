35e0b820cf475af3c4f9bcf6cb3e756d262cf5f4
Nelson <nelsonec87@gmail.com>
2023-05-30 19:38:34 -0300

    Moved styles to package

--- packages/launchpad/src/checklist-item/index.tsx
+++ packages/launchpad/src/checklist-item/index.tsx
@@ -1,8 +1,10 @@
 import { Button, Gridicon } from '@automattic/components';
 import classnames from 'classnames';
 import { translate, useRtl } from 'i18n-calypso';
-import Badge from './badge';
-import { Task } from './types';
+import Badge from '../badge';
+import { Task } from '../types';
+
+import './style.scss';
 
 const ChecklistItem = ( { task, isPrimaryAction }: { task: Task; isPrimaryAction?: boolean } ) => {
 	const isRtl = useRtl();
@@ -18,7 +20,7 @@ const ChecklistItem = ( { task, isPrimaryAction }: { task: Task; isPrimaryAction
 
 	return (
 		<li
-			className={ classnames( 'launchpad__task', {
+			className={ classnames( 'checklist-item__task', {
 				completed: completed,
 				pending: ! completed,
 				enabled: ! disabled,
@@ -27,7 +29,7 @@ const ChecklistItem = ( { task, isPrimaryAction }: { task: Task; isPrimaryAction
 		>
 			{ isPrimaryAction ? (
 				<Button
-					className="launchpad__checklist-primary-button"
+					className="checklist-item__checklist-primary-button"
 					disabled={ disabled }
 					data-task={ id }
 					onClick={ handlePrimaryAction }
@@ -36,33 +38,33 @@ const ChecklistItem = ( { task, isPrimaryAction }: { task: Task; isPrimaryAction
 				</Button>
 			) : (
 				<Button
-					className="launchpad__checklist-item"
+					className="checklist-item__task-content"
 					disabled={ disabled }
 					data-task={ id }
 					onClick={ actionDispatch }
 				>
 					{ completed && (
 						// show checkmark for completed tasks regardless if they are disabled or kept active
-						<div className="launchpad__checklist-item-checkmark-container">
+						<div className="checklist-item__checkmark-container">
 							<Gridicon
 								aria-label={ translate( 'Task complete' ) }
-								className="launchpad__checklist-item-checkmark"
+								className="checklist-item__checkmark"
 								icon="checkmark"
 								size={ 18 }
 							/>
 						</div>
 					) }
-					<span className="launchpad__checklist-item-text">{ title }</span>
+					<span className="checklist-item__text">{ title }</span>
 					{ task.badge_text ? <Badge type="info-blue">{ task.badge_text }</Badge> : null }
 					{ shouldDisplayChevron && (
 						<Gridicon
 							aria-label={ translate( 'Task enabled' ) }
-							className="launchpad__checklist-item-chevron"
+							className="checklist-item__chevron"
 							icon={ `chevron-${ isRtl ? 'left' : 'right' }` }
 							size={ 18 }
 						/>
 					) }
-					{ subtitle && <p className="launchpad__checklist-item-subtext">{ subtitle }</p> }
+					{ subtitle && <p className="checklist-item__subtext">{ subtitle }</p> }
 				</Button>
 			) }
 		</li>
@@ -71,8 +73,8 @@ const ChecklistItem = ( { task, isPrimaryAction }: { task: Task; isPrimaryAction
 
 ChecklistItem.Placeholder = () => {
 	return (
-		<div className="launchpad__checklist-item is-placeholder">
-			<div className="launchpad__checklist-item-content">
+		<div className="checklist-item__task-content is-placeholder">
+			<div className="checklist-item__content">
 				<div></div>
 			</div>
 		</div>
