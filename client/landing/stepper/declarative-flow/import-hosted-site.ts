8d9a1601fe53cca5f08117b9544b3e8bc31f3cbf
mpkelly <kikemelly@gmail.com>
2023-05-31 19:11:29 +0700

    Remove unused code from flow

--- client/landing/stepper/declarative-flow/import-hosted-site.ts
+++ client/landing/stepper/declarative-flow/import-hosted-site.ts
@@ -1,38 +1,24 @@
 import { isEnabled } from '@automattic/calypso-config';
-import { Design, isBlankCanvasDesign } from '@automattic/design-picker';
+import { isBlankCanvasDesign } from '@automattic/design-picker';
 import { IMPORT_HOSTED_SITE_FLOW } from '@automattic/onboarding';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { ImporterMainPlatform } from 'calypso/blocks/import/types';
-import useAddTempSiteToSourceOptionMutation from 'calypso/data/site-migration/use-add-temp-site-mutation';
-import { useSourceMigrationStatusQuery } from 'calypso/data/site-migration/use-source-migration-status-query';
-import { useSiteExcerptsQuery } from 'calypso/data/sites/use-site-excerpts-query';
-import { SITE_PICKER_FILTER_CONFIG } from 'calypso/landing/stepper/constants';
 import MigrationError from 'calypso/landing/stepper/declarative-flow/internals/steps-repository/migration-error';
 import { ProcessingResult } from 'calypso/landing/stepper/declarative-flow/internals/steps-repository/processing-step/constants';
 import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
 import { useSiteSlugParam } from 'calypso/landing/stepper/hooks/use-site-slug-param';
 import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { useSiteSetupFlowProgress } from '../hooks/use-site-setup-flow-progress';
-import DesignSetup from './internals/steps-repository/design-setup';
 import ImportList from './internals/steps-repository/import-list';
 import ImportReady from './internals/steps-repository/import-ready';
 import ImportReadyNot from './internals/steps-repository/import-ready-not';
 import ImportReadyPreview from './internals/steps-repository/import-ready-preview';
 import ImportReadyWpcom from './internals/steps-repository/import-ready-wpcom';
 import ImportWithSiteAddressStep from './internals/steps-repository/import-with-site-address';
-import ImporterBlogger from './internals/steps-repository/importer-blogger';
-import ImporterMedium from './internals/steps-repository/importer-medium';
-import ImporterSquarespace from './internals/steps-repository/importer-squarespace';
-import ImporterWix from './internals/steps-repository/importer-wix';
 import ImporterWordpress from './internals/steps-repository/importer-wordpress';
-import MigrationHandler from './internals/steps-repository/migration-handler';
-import PatternAssembler from './internals/steps-repository/pattern-assembler';
 import ProcessingStep from './internals/steps-repository/processing-step';
-import SiteCreationStep from './internals/steps-repository/site-creation-step';
-import SitePickerStep from './internals/steps-repository/site-picker';
 import { Flow, ProvidedDependencies } from './internals/types';
 import type { OnboardSelect } from '@automattic/data-stores';
-import type { SiteExcerptData } from 'calypso/data/sites/site-excerpt-types';
 
 const importHostedSiteFlow: Flow = {
 	name: IMPORT_HOSTED_SITE_FLOW,
@@ -45,28 +31,16 @@ const importHostedSiteFlow: Flow = {
 			{ slug: 'importReadyNot', component: ImportReadyNot },
 			{ slug: 'importReadyWpcom', component: ImportReadyWpcom },
 			{ slug: 'importReadyPreview', component: ImportReadyPreview },
-			{ slug: 'importerWix', component: ImporterWix },
-			{ slug: 'importerBlogger', component: ImporterBlogger },
-			{ slug: 'importerMedium', component: ImporterMedium },
-			{ slug: 'importerSquarespace', component: ImporterSquarespace },
 			{ slug: 'importerWordpress', component: ImporterWordpress },
-			{ slug: 'designSetup', component: DesignSetup },
-			{ slug: 'patternAssembler', component: PatternAssembler },
 			{ slug: 'processing', component: ProcessingStep },
-			{ slug: 'siteCreationStep', component: SiteCreationStep },
-			{ slug: 'migrationHandler', component: MigrationHandler },
-			{ slug: 'sitePicker', component: SitePickerStep },
 			{ slug: 'error', component: MigrationError },
 		];
 	},
 
 	useStepNavigation( _currentStep, navigate ) {
 		const { setStepProgress, setPendingAction } = useDispatch( ONBOARD_STORE );
-		const { data: sites } = useSiteExcerptsQuery( SITE_PICKER_FILTER_CONFIG );
-		const { addTempSiteToSourceOption } = useAddTempSiteToSourceOptionMutation();
 		const urlQueryParams = useQuery();
 		const fromParam = urlQueryParams.get( 'from' );
-		const { data: migrationStatus } = useSourceMigrationStatusQuery( fromParam );
 		const siteSlugParam = useSiteSlugParam();
 		const selectedDesign = useSelect(
 			( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getSelectedDesign(),
@@ -92,25 +66,6 @@ const importHostedSiteFlow: Flow = {
 			return navigate( 'processing' );
 		};
 
-		const handleMigrationRedirects = ( providedDependencies: ProvidedDependencies = {} ) => {
-			const userHasSite = sites && sites.length > 0;
-
-			if ( providedDependencies?.status === 'inactive' ) {
-				// This means they haven't kick off the migration before, so we send them to select/create a new site
-				if ( ! providedDependencies?.targetBlogId ) {
-					return userHasSite ? navigate( 'sitePicker' ) : navigate( 'siteCreationStep' );
-				}
-				// For some reason, the admin role is mismatch, we want to select/create a new site for them as well
-				if ( providedDependencies?.isAdminOnTarget === false ) {
-					return userHasSite ? navigate( 'sitePicker' ) : navigate( 'siteCreationStep' );
-				}
-			}
-			// For those who hasn't paid or in the middle of the migration process, we sent them to the importerWordPress step
-			return navigate(
-				`importerWordpress?siteSlug=${ providedDependencies?.targetBlogSlug }&from=${ fromParam }&option=everything`
-			);
-		};
-
 		const submit = ( providedDependencies: ProvidedDependencies = {}, ...params: string[] ) => {
 			switch ( _currentStep ) {
 				case 'importReady': {
@@ -131,10 +86,6 @@ const importHostedSiteFlow: Flow = {
 					return navigate( providedDependencies?.url as string );
 				}
 
-				case 'importerWix':
-				case 'importerBlogger':
-				case 'importerMedium':
-				case 'importerSquarespace':
 				case 'importerWordpress': {
 					if ( providedDependencies?.type === 'redirect' ) {
 						return exitFlow( providedDependencies?.url as string );
@@ -142,21 +93,6 @@ const importHostedSiteFlow: Flow = {
 
 					return navigate( providedDependencies?.url as string );
 				}
-
-				case 'designSetup': {
-					const _selectedDesign = providedDependencies?.selectedDesign as Design;
-					if ( _selectedDesign?.design_type === 'assembler' ) {
-						return navigate( 'patternAssembler' );
-					}
-
-					return navigate( 'processing' );
-				}
-
-				case 'patternAssembler':
-					return navigate( 'processing' );
-
-				case 'siteCreationStep':
-					return navigate( 'processing' );
 
 				case 'processing': {
 					const processingResult = params[ 0 ] as ProcessingResult;
@@ -188,51 +124,8 @@ const importHostedSiteFlow: Flow = {
 					return exitFlow( `/home/${ siteSlugParam }` );
 				}
 
-				case 'migrationHandler': {
-					return handleMigrationRedirects( providedDependencies );
-				}
-
 				case 'error':
 					return navigate( providedDependencies?.url as string );
-
-				case 'sitePicker': {
-					switch ( providedDependencies?.action ) {
-						case 'update-query': {
-							const newQueryParams =
-								( providedDependencies?.queryParams as { [ key: string ]: string } ) || {};
-
-							Object.keys( newQueryParams ).forEach( ( key ) => {
-								newQueryParams[ key ]
-									? urlQueryParams.set( key, newQueryParams[ key ] )
-									: urlQueryParams.delete( key );
-							} );
-
-							return navigate( `sitePicker?${ urlQueryParams.toString() }` );
-						}
-
-						case 'select-site': {
-							const selectedSite = providedDependencies.site as SiteExcerptData;
-
-							if ( selectedSite && migrationStatus ) {
-								// Store temporary target blog id to source site option
-								selectedSite &&
-									migrationStatus?.source_blog_id &&
-									addTempSiteToSourceOption( selectedSite.ID, migrationStatus.source_blog_id );
-
-								urlQueryParams.set( 'siteSlug', selectedSite.slug );
-								urlQueryParams.set( 'option', 'everything' );
-
-								return navigate( `importerWordpress?${ urlQueryParams.toString() }` );
-							}
-						}
-
-						case 'create-site':
-							return navigate( 'siteCreationStep' );
-
-						default:
-							return navigate( `migrationHandler` );
-					}
-				}
 			}
 		};
 
@@ -254,12 +147,7 @@ const importHostedSiteFlow: Flow = {
 				case 'importReadyNot':
 				case 'importReadyWpcom':
 				case 'importReadyPreview':
-				case 'importerWix':
-				case 'importerBlogger':
-				case 'importerMedium':
-				case 'importerSquarespace':
 				case 'importerWordpress':
-				case 'designSetup':
 					return navigate( `import?siteSlug=${ siteSlugParam }` );
 			}
 		};
@@ -275,8 +163,6 @@ const importHostedSiteFlow: Flow = {
 
 		const goToStep = ( step: string ) => {
 			switch ( step ) {
-				case 'goals':
-					return exitFlow( `/setup/site-setup/goals?siteSlug=${ siteSlugParam }` );
 				case 'import':
 					return navigate( `import?siteSlug=${ siteSlugParam }` );
 				default:
