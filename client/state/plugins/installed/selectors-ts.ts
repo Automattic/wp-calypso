6b4b541ac8b432b1cda1f9b8cf167941b66bbc58
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2023-05-30 18:01:32 -0700

    Create an isPluginActive action with the TypeScript selectors

--- client/state/plugins/installed/selectors-ts.ts
+++ client/state/plugins/installed/selectors-ts.ts
@@ -446,3 +446,25 @@ export const getPluginStatusesByType = createSelector(
 	},
 	( state: AppState ) => state.plugins.installed.status
 );
+
+/**
+ * Returns a Boolean indicating if a site has a particular plugin that
+ * is active. This is useful for jetpack connected sites.
+ *
+ * @param {Object} state - Global state tree
+ * @param {number} siteId - Site ID
+ * @param {string} pluginSlug - Plugin slug
+ * @returns {boolean} - truthiness of a site having an active plugin
+ */
+export const isPluginActive = createSelector(
+	( state: AppState, siteId: number, pluginSlug: string ) => {
+		const sitePlugin = getAllPluginsIndexedBySiteId( state )?.[ siteId ]?.[ pluginSlug ];
+
+		if ( ! sitePlugin ) {
+			return false;
+		}
+
+		return sitePlugin.sites[ siteId ].active;
+	},
+	( state: AppState ) => [ getAllPluginsIndexedBySiteId( state ) ]
+) as ( state: AppState, siteId: number, pluginSlug: string ) => boolean;
