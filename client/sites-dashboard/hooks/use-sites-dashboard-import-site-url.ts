709c3da8b35005309dcf8197aa20e1d39f5a9b7c
mpkelly <kikemelly@gmail.com>
2023-05-31 13:49:45 +0700

    Initial commit. Very rough WIP.

--- client/sites-dashboard/hooks/use-sites-dashboard-import-site-url.ts
+++ client/sites-dashboard/hooks/use-sites-dashboard-import-site-url.ts
@@ -1,14 +1,18 @@
 import { addQueryArgs } from 'calypso/lib/url';
+import { useSelector } from 'calypso/state';
+import getUserSetting from 'calypso/state/selectors/get-user-setting';
 import { TRACK_SOURCE_NAME } from '../utils';
 
 export const useSitesDashboardImportSiteUrl = (
 	additionalParameters: Record< string, string >
 ) => {
+	const isDevAccount = useSelector( ( state ) => getUserSetting( state, 'is_dev_account' ) );
+
 	return addQueryArgs(
 		{
 			source: TRACK_SOURCE_NAME,
 			...additionalParameters,
 		},
-		'/start/import'
+		isDevAccount ? '/setup/import-hosted-site' : '/start/import'
 	);
 };
