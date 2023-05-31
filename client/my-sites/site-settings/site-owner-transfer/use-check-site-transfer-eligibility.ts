63169ccc2de1aa2a22f8789fbba81b25be83b875
Antony Agrios <antonyagrios@gmail.com>
2023-05-31 15:18:45 +0300

    Update: endpoint url

--- client/my-sites/site-settings/site-owner-transfer/use-check-site-transfer-eligibility.ts
+++ client/my-sites/site-settings/site-owner-transfer/use-check-site-transfer-eligibility.ts
@@ -23,7 +23,7 @@ export const useCheckSiteTransferEligibility = (
 		async ( { newSiteOwner }: MutationVariables ) =>
 			wp.req.post(
 				{
-					path: `/sites/${ siteId }/site-owner-transfer/transfer-eligibility`,
+					path: `/sites/${ siteId }/site-owner-transfer/eligibility`,
 					apiNamespace: 'wpcom/v2',
 				},
 				{ new_site_owner: newSiteOwner }
