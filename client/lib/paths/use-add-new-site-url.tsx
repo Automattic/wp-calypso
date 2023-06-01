72aa58da5c8ff1f979baa1ddb5ef5340e8fb691d
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2023-06-01 19:27:11 -0300

    Dev accounts: use consistent new site url in site selector, sidebar and sites dashboard (#77691)
    * Use hook in sidebar
    
    * Move from useSitesDashboardCreateSiteUrl to useAddNewSiteUrl
    
    * Drop -cta suffix from ref in sites page CTA
    
    * Correct source and refs
    
    * Correct Jetpack Cloud ref and source
    
    * Fix tests

---
+++ client/lib/paths/use-add-new-site-url.tsx
@@ -0,0 +1,20 @@
+import config from '@automattic/calypso-config';
+import { useSelector } from 'react-redux';
+import { Primitive } from 'utility-types';
+import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
+import { addQueryArgs } from 'calypso/lib/url';
+import getUserSetting from 'calypso/state/selectors/get-user-setting';
+
+export const useAddNewSiteUrl = ( queryParameters: Record< string, Primitive > ) => {
+	const isDevAccount = useSelector( ( state ) => getUserSetting( state, 'is_dev_account' ) );
+
+	return addQueryArgs(
+		queryParameters,
+		// eslint-disable-next-line no-nested-ternary
+		isJetpackCloud()
+			? config( 'jetpack_connect_url' )
+			: isDevAccount
+			? '/setup/new-hosted-site'
+			: config( 'signup_url' )
+	);
+};
