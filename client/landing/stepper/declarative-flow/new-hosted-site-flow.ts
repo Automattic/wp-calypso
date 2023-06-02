1f47a543808c7b09897706d65fa74d338e22ae08
Philip Jackson <p-jackson@live.com>
2023-06-02 17:06:41 +1200

    new-hosted-site doesn't navigate to final URL until AT is complete

--- client/landing/stepper/declarative-flow/new-hosted-site-flow.ts
+++ client/landing/stepper/declarative-flow/new-hosted-site-flow.ts
@@ -72,9 +72,25 @@ const hosting: Flow = {
 			}
 
 			if ( _currentStepSlug === 'processing' ) {
-				const destination = addQueryArgs( '/sites', {
-					'new-site': providedDependencies.siteId,
-				} );
+				// Purchasing these plans will trigger an atomic transfer, so go to stepper flow where we wait for it to complete.
+				const goingAtomic =
+					providedDependencies.goToCheckout &&
+					planCartItem?.product_slug &&
+					[
+						'business-bundle',
+						'business-bundle-monthly',
+						'ecommerce-bundle',
+						'ecommerce-bundle-monthly',
+					].includes( planCartItem.product_slug );
+
+				const destination = goingAtomic
+					? addQueryArgs( '/setup/transferring-hosted-site', {
+							siteId: providedDependencies.siteId,
+					  } )
+					: addQueryArgs( '/sites', {
+							'new-site': providedDependencies.siteId,
+					  } );
+
 				persistSignupDestination( destination );
 				setSignupCompleteSlug( providedDependencies?.siteSlug );
 				setSignupCompleteFlowName( flowName );
