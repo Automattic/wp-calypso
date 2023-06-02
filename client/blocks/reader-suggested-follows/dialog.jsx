2b1a2ee3f56e725fbea623765b353b4c6c22550b
Eoin Gallagher <eoin@automattic.com>
2023-06-02 14:42:03 +0100

    update comment

--- client/blocks/reader-suggested-follows/dialog.jsx
+++ client/blocks/reader-suggested-follows/dialog.jsx
@@ -9,7 +9,7 @@ import './style.scss';
 const ReaderSuggestedFollowsDialog = ( { onClose, siteId, postId, isVisible } ) => {
 	const translate = useTranslate();
 	const { data, isLoading } = useRelatedSites( siteId, postId );
-	// If we are no longer loading and still have no data, don't show the dialog
+	// If we are no longer loading and no data available, don't show the dialog
 	if ( ! isLoading && ! data ) {
 		return null;
 	}
