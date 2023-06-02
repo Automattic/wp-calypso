49f693af5bc366e0a2aa6438b5f10b01e01e1d15
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-06-02 12:22:44 +0530

    Social Preview | Improve defaults & Fix multiple empty lines (#77522)
    * Add default avatars
    
    * Remove constant files
    
    * Improve defaults for LinkedIn preview
    
    * Improve defaults for Twitter preview
    
    * Bump the version
    
    * Prepare the patch release and update changelog
    
    * Fix multiple empty lines issue in preview text
    
    * Update regex for new lines
    
    Co-authored-by: Paul Bunkham <paul@dobit.co.uk>
    
    * Use beta version for now
    
    * Update CHANGELOG.md
    
    * Use % for border-radius
    
    * Bump version
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Paul Bunkham <paul@dobit.co.uk>

--- packages/social-previews/src/twitter-preview/constants.ts
+++
@@ -1,6 +0,0 @@
-export const DEFAULT_PROPS = {
-	date: Date.now() as number | Date,
-	name: 'Account Name',
-	profileImage: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_bigger.png',
-	screenName: '@account',
-};
--- packages/social-previews/src/twitter-preview/link-preview.tsx
+++ packages/social-previews/src/twitter-preview/link-preview.tsx
@@ -1,16 +1,9 @@
-import { DEFAULT_PROPS } from './constants';
 import { TwitterPostPreview } from './post-preview';
 import { TwitterPreviewProps } from './types';
 
-type OptionalProps = Partial< typeof DEFAULT_PROPS >;
-
-export type TwitterLinkPreviewProps = Omit< TwitterPreviewProps, keyof OptionalProps > &
-	OptionalProps;
-
-export const TwitterLinkPreview: React.FC< TwitterLinkPreviewProps > = ( props ) => {
+export const TwitterLinkPreview: React.FC< TwitterPreviewProps > = ( props ) => {
 	return (
 		<TwitterPostPreview
-			{ ...DEFAULT_PROPS }
 			{ ...props }
 			// Override the props that are irrelevant to link preview
 			text=""
