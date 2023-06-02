9ff08b971339a54310992d693caf8983a7c6d7cc
Jason Moon <noreply@jsnmoon.com>
2023-06-02 16:35:48 -0700

    Tweak enhancer

--- client/state/analytics/actions/enhance-with-odyssey-flag.js
+++ client/state/analytics/actions/enhance-with-odyssey-flag.js
@@ -1,11 +1,11 @@
 import config from '@automattic/calypso-config';
 import { set } from 'lodash';
-import { ANALYTICS_EVENT_RECORD } from 'calypso/state/action-types';
+import { ANALYTICS_EVENT_RECORD, ANALYTICS_PAGE_VIEW_RECORD } from 'calypso/state/action-types';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 /**
- * Enhances any Redux action that denotes the recording of an analytics event with an additional property which
- * specifies whether the site is running as an Odyssey application (i.e., Calypso-in-Jetpack application).
+ * Enhances any Redux action that denotes the recording of an analytics event with an additional property if
+ * the site is running as an Odyssey application (i.e., Calypso-in-Jetpack application).
  *
  * @param {Object} action - Redux action as a plain object
  * @returns {import('redux').AnyAction} the new Redux action
@@ -14,19 +14,10 @@ import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 export function enhanceWithOdysseyFlag( action, getState ) {
 	const isOdyssey = config.isEnabled( 'is_running_in_jetpack_site' );
 
-	if ( action.type === ANALYTICS_EVENT_RECORD ) {
+	if ( action.type === ANALYTICS_EVENT_RECORD || action.type === ANALYTICS_PAGE_VIEW_RECORD ) {
 		if ( isOdyssey ) {
-			set(
-				action,
-				'meta.analytics[0].payload.blog_id',
-				config.isEnabled( 'is_running_in_jetpack_site' )
-			);
-
-			set(
-				action,
-				'meta.analytics[0].payload.properties.is_odyssey',
-				getSelectedSiteId( getState() )
-			);
+			set( action, 'meta.analytics[0].payload.blog_id', getSelectedSiteId( getState() ) );
+			set( action, 'meta.analytics[0].payload.is_odyssey', true );
 		}
 	}
 
