64d78eecb03a33bebe5da54bd767abfa552782c2
Rafael Agostini <rafael.agostini@automattic.com>
2023-06-02 17:14:41 -0500

    Add translation for page header

--- client/my-sites/backup/backup-contents-page/index.tsx
+++ client/my-sites/backup/backup-contents-page/index.tsx
@@ -1,4 +1,5 @@
 import { Card } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
 import { FunctionComponent } from 'react';
 import ActionButtons from 'calypso/components/jetpack/daily-backup-status/action-buttons';
 import cloudIcon from 'calypso/components/jetpack/daily-backup-status/status-card/icons/cloud-success.svg';
@@ -11,6 +12,7 @@ interface OwnProps {
 }
 
 const BackupContentsPage: FunctionComponent< OwnProps > = ( { rewindId } ) => {
+	const translate = useTranslate();
 	const getDisplayDate = useGetDisplayDate();
 	const moment = useLocalizedMoment();
 	const displayDate = getDisplayDate( moment.unix( rewindId ), false );
@@ -22,7 +24,9 @@ const BackupContentsPage: FunctionComponent< OwnProps > = ( { rewindId } ) => {
 					<div className="contents-page__header">
 						<div className="status-card__message-head">
 							<img src={ cloudIcon } alt="" role="presentation" />
-							<div className="status-card__hide-mobile">Backup contents from:</div>
+							<div className="status-card__hide-mobile">
+								{ translate( 'Backup contents from:' ) }
+							</div>
 						</div>
 						<div className="status-card__title">{ displayDate }</div>
 						<ActionButtons
