371f5dd36d8f7785e31b415b96ffff30a4b581f4
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-06-05 12:30:41 +0200

    Import flow: Improve logic of showing loading screen

--- client/landing/stepper/declarative-flow/internals/steps-repository/importer/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/importer/index.tsx
@@ -30,9 +30,12 @@ import {
 	isImporterStatusHydrated as isImporterStatusHydratedSelector,
 } from 'calypso/state/imports/selectors';
 import { analyzeUrl } from 'calypso/state/imports/url-analyzer/actions';
-import { getUrlData } from 'calypso/state/imports/url-analyzer/selectors';
+import { getUrlData, isAnalyzing } from 'calypso/state/imports/url-analyzer/selectors';
 import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
-import { hasAllSitesList } from 'calypso/state/sites/selectors';
+import {
+	hasAllSitesList,
+	isRequestingSite as isRequestingSiteSelector,
+} from 'calypso/state/sites/selectors';
 import { StepProps } from '../../types';
 import { useAtomicTransferQueryParamUpdate } from './hooks/use-atomic-transfer-query-param-update';
 import { useInitialQueryRun } from './hooks/use-initial-query-run';
@@ -62,6 +65,9 @@ export function withImporterWrapper( Importer: ImporterCompType ) {
 		const canImport = useSelector( ( state ) => canCurrentUser( state, siteId, 'manage_options' ) );
 		const siteImports = useSelector( ( state ) => getImporterStatusForSiteId( state, siteId ) );
 		const hasAllSitesFetched = useSelector( ( state ) => hasAllSitesList( state ) );
+		const isRequestingSite: boolean = useSelector( ( state ) =>
+			isRequestingSiteSelector( state, siteId )
+		);
 		const isImporterStatusHydrated = useSelector( isImporterStatusHydratedSelector );
 		const isMigrateFromWp = useSelect(
 			( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getIsMigrateFromWp(),
@@ -69,19 +75,18 @@ export function withImporterWrapper( Importer: ImporterCompType ) {
 		);
 		const fromSite = currentSearchParams.get( 'from' ) || '';
 		const fromSiteData = useSelector( getUrlData );
+		const fromSiteDataIsAnalyzing = useSelector( isAnalyzing );
 		const stepNavigator = useStepNavigator( flow, navigation, siteId, siteSlug, fromSite );
 
 		/**
 	 	↓ Effects
 		 */
+		useEffect( () => {
+			! siteId && site?.ID && setSiteId( site.ID );
+		}, [ siteId, site ] );
 		useAtomicTransferQueryParamUpdate( siteId );
 		useEffect( fetchImporters, [ siteId ] );
 		useEffect( checkFromSiteData, [ fromSiteData?.url ] );
-		useEffect( () => {
-			if ( ! siteId && site?.ID ) {
-				setSiteId( site?.ID );
-			}
-		}, [ siteId, site ] );
 
 		if ( ! importer ) {
 			stepNavigator.goToImportCapturePage?.();
@@ -131,7 +136,12 @@ export function withImporterWrapper( Importer: ImporterCompType ) {
 		}
 
 		function isLoading(): boolean {
-			return ! isImporterStatusHydrated || ! hasAllSitesFetched;
+			return (
+				! isImporterStatusHydrated ||
+				! hasAllSitesFetched ||
+				fromSiteDataIsAnalyzing ||
+				isRequestingSite
+			);
 		}
 
 		function checkFromSiteData(): void {
