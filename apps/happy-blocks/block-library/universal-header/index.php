14d941ef78ae76e9bd7884c800ba9aefa004040f
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2023-06-06 18:44:29 -0300

    add fixme__ to new copy

--- apps/happy-blocks/block-library/universal-header/index.php
+++ apps/happy-blocks/block-library/universal-header/index.php
@@ -156,8 +156,8 @@ $happy_blocks_is_english = ( 0 === stripos( get_locale(), 'en' ) );
 								</a>
 							</li>
 							<li>
-								<a role="menuitem" class="x-dropdown-link x-link" href="<?php echo esc_url( localized_wpcom_url( '//wordpress.com/create-a-course/' ) ); ?>" title="<?php echo esc_attr( __( 'Course Maker', 'happy-blocks' ) ); ?>" tabindex="-1">
-									<?php echo esc_html( __( 'Course Maker', 'happy-blocks' ) ); ?>
+								<a role="menuitem" class="x-dropdown-link x-link" href="<?php echo esc_url( localized_wpcom_url( '//wordpress.com/create-a-course/' ) ); ?>" title="<?php echo esc_attr( fixme__( 'Course Maker', __( 'Course', 'happy-blocks' ) ) ); ?>" tabindex="-1">
+									<?php echo esc_html( fixme__( 'Course Maker', __( 'Course', 'happy-blocks' ) ) ); ?>
 								</a>
 							</li>
 						</ul>
@@ -336,8 +336,8 @@ $happy_blocks_is_english = ( 0 === stripos( get_locale(), 'en' ) );
 									</a>
 								</li>
 								<li class="x-menu-grid-item">
-									<a role="menuitem" class="x-menu-link x-link" href="<?php echo esc_url( localized_wpcom_url( '//wordpress.com/create-a-course/' ) ); ?>" title="<?php echo esc_attr( __( 'Course Maker', 'happy-blocks' ) ); ?>" tabindex="-1">
-										<?php echo esc_html( __( 'Course Maker', 'happy-blocks' ) ); ?>
+									<a role="menuitem" class="x-menu-link x-link" href="<?php echo esc_url( localized_wpcom_url( '//wordpress.com/create-a-course/' ) ); ?>" title="<?php echo esc_attr( fixme__( 'Course Maker', __( 'Course', 'happy-blocks' ) ) ); ?>" tabindex="-1">
+										<?php echo esc_html( fixme__( 'Course Maker', __( 'Course', 'happy-blocks' ) ) ); ?>
 									</a>
 								</li>
 								<li class="x-menu-grid-item">
