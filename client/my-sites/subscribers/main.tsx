08ba40afcf6494fe253ba5fcaba2736aeeceec2b
Paulo Cruz <paulo.cruz@automattic.com>
2023-06-06 12:01:59 -0300

    Add EmptyListView to the Subscribers page

--- client/my-sites/subscribers/main.tsx
+++ client/my-sites/subscribers/main.tsx
@@ -2,6 +2,7 @@ import config from '@automattic/calypso-config';
 import { translate } from 'i18n-calypso';
 import DocumentHead from 'calypso/components/data/document-head';
 import Main from 'calypso/components/main';
+import { EmptyListView } from './components/empty-list-view';
 
 export const Subscribers = () => {
 	const isSubscribersPageEnabled = config.isEnabled( 'subscribers-page' );
@@ -14,6 +15,7 @@ export const Subscribers = () => {
 		<Main>
 			<DocumentHead title={ translate( 'Subscribers' ) } />
 			Subscribers
+			<EmptyListView />
 		</Main>
 	);
 };
