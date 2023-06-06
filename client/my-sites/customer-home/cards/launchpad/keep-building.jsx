0b976414125a52e346568de01dd5fefaec18c96f
Nelson <nelsonec87@gmail.com>
2023-06-06 17:10:07 -0300

    Added complementary styles to Customer home launchpad

--- client/my-sites/customer-home/cards/launchpad/keep-building.jsx
+++ client/my-sites/customer-home/cards/launchpad/keep-building.jsx
@@ -1,10 +1,33 @@
+import { CircularProgressBar } from '@automattic/components';
 import { Launchpad } from '@automattic/launchpad';
+import { useTranslate } from 'i18n-calypso';
 import { connect } from 'react-redux';
 import { getSiteSlug } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
+import './style.scss';
+
 const LaunchpadKeepBuilding = ( { siteSlug } ) => {
-	return <Launchpad siteSlug={ siteSlug } checklistSlug="keep-building" />;
+	const translate = useTranslate();
+
+	return (
+		<div className="launchpad-keep-building">
+			<div className="launchpad-keep-building__header">
+				<h2 className="launchpad-keep-building__title">
+					{ translate( 'Next steps for your site' ) }
+				</h2>
+				<div className="launchpad-keep-building__progress-bar-container">
+					<CircularProgressBar
+						size={ 40 }
+						enableDesktopScaling
+						currentStep={ 2 }
+						numberOfSteps={ 5 }
+					/>
+				</div>
+			</div>
+			<Launchpad siteSlug={ siteSlug } />
+		</div>
+	);
 };
 
 const ConnectedLaunchpadKeepBuilding = connect( ( state ) => {
