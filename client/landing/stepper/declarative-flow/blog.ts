8df1c1208f476aea357047d86e3e5bac43ee84e6
Louis Laugesen <louis.laugesen@gmail.com>
2023-06-06 12:11:04 +1000

    Remove login redirects and username reference

--- client/landing/stepper/declarative-flow/blog.ts
+++ client/landing/stepper/declarative-flow/blog.ts
@@ -1,13 +1,5 @@
-import { useLocale } from '@automattic/i18n-utils';
 import { translate } from 'i18n-calypso';
-import { redirect } from 'calypso/landing/stepper/declarative-flow/internals/steps-repository/import/util';
-import {
-	AssertConditionResult,
-	AssertConditionState,
-	Flow,
-} from 'calypso/landing/stepper/declarative-flow/internals/types';
-import { useSelector } from 'calypso/state';
-import { getCurrentUserSiteCount, isUserLoggedIn } from 'calypso/state/current-user/selectors';
+import { Flow } from 'calypso/landing/stepper/declarative-flow/internals/types';
 
 const Blog: Flow = {
 	name: 'blog',
@@ -23,52 +15,8 @@ const Blog: Flow = {
 		];
 	},
 
-	useStepNavigation( navigate ) {
-		const goBack = () => {
-			return;
-		};
-
-		const goNext = () => {
-			return;
-		};
-
-		const goToStep = ( step: string ) => {
-			navigate( step );
-		};
-
-		return { goNext, goBack, goToStep };
-	},
-
-	useAssertConditions(): AssertConditionResult {
-		const flowName = this.name;
-		const isLoggedIn = useSelector( isUserLoggedIn );
-		const currentUserSiteCount = useSelector( getCurrentUserSiteCount );
-		const locale = useLocale();
-		const userAlreadyHasSites = currentUserSiteCount && currentUserSiteCount > 0;
-
-		const logInUrl =
-			locale && locale !== 'en'
-				? `/start/account/user/${ locale }?redirect_to=/setup/blog`
-				: `/start/account/user?redirect_to=/setup/blog`;
-
-		let result: AssertConditionResult = { state: AssertConditionState.SUCCESS };
-
-		if ( ! isLoggedIn ) {
-			redirect( logInUrl );
-			result = {
-				state: AssertConditionState.CHECKING,
-				message: `${ flowName } requires a logged in user`,
-			};
-		} else if ( userAlreadyHasSites ) {
-			// This prevents a bunch of sites being created accidentally.
-			redirect( `/` );
-			result = {
-				state: AssertConditionState.CHECKING,
-				message: `${ flowName } requires no preexisting sites`,
-			};
-		}
-
-		return result;
+	useStepNavigation() {
+		return {};
 	},
 };
 
