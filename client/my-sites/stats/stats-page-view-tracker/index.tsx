bd9c3ca1d84489cba1ec36393d08f3bc045d823a
Jason Moon <noreply@jsnmoon.com>
2023-06-06 12:32:12 -0700

    Simplify StatsPageViewTracker

--- client/my-sites/stats/stats-page-view-tracker/index.tsx
+++ client/my-sites/stats/stats-page-view-tracker/index.tsx
@@ -1,13 +1,8 @@
-import debugFactory from 'debug';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
-import { isRequestingSite, isRequestingSites } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import useTracks from './use-tracks';
-import type { AppState } from 'calypso/types';
-
-const debug = debugFactory( 'calypso:my-sites:stats:StatsPageViewTracker' );
 
 export interface StatsPageViewTrackerProps {
 	path: string;
@@ -20,29 +15,18 @@ export interface StatsPageViewTrackerProps {
 // In addition, this component will fire a page view event unique to the stats pages.
 export default function StatsPageViewTracker( props: StatsPageViewTrackerProps ) {
 	const selectedSiteId = useSelector( getSelectedSiteId );
-	const isLoadingSite = useSelector(
-		( state: AppState ) =>
-			! selectedSiteId || isRequestingSites( state ) || isRequestingSite( state, selectedSiteId )
-	);
 
 	// Tracking function is updated upon props change to handle these cases:
 	// - when the selected site ID changes.
-	// - when the selected site has been loaded.
 	// - when the path, title, properties, or options have changed.
-	const { queuePageView } = useTracks( {
+	const { recordPageView } = useTracks( {
 		...props,
-		isLoadingSite,
 		selectedSiteId,
 	} );
 
 	useEffect( () => {
-		debug( 'Component has mounted.' );
-		queuePageView();
-	}, [ queuePageView ] );
-
-	useEffect( () => {
-		queuePageView();
-	}, [ selectedSiteId, queuePageView ] );
+		recordPageView();
+	}, [ selectedSiteId, recordPageView ] );
 
 	return <PageViewTracker { ...props } />;
 }
--- client/my-sites/stats/stats-page-view-tracker/use-tracks.tsx
+++ client/my-sites/stats/stats-page-view-tracker/use-tracks.tsx
@@ -14,7 +14,6 @@ const CALYPSO_STATS_PAGE_VIEW_EVENT_NAME = 'calypso_stats_page_view';
 const ODYSSEY_STATS_PAGE_VIEW_EVENT_NAME = 'odyssey_stats_page_view';
 
 interface useTracksProps extends StatsPageViewTrackerProps {
-	isLoadingSite: boolean;
 	selectedSiteId: number | null;
 }
 
@@ -26,13 +25,11 @@ export default function useTracks( {
 	title,
 
 	// These are the props from the useSelector calls in StatsPageViewTracker.
-	isLoadingSite,
 	selectedSiteId,
 }: useTracksProps ) {
 	const dispatch = useDispatch() as ThunkDispatch< Store, void, AnyAction >;
 
 	const recordPageView = useCallback( () => {
-		debug( `Recording Stats Page View: "${ title }" at "${ path }"` );
 		const recordEvent = withEnhancers( recordTracksEvent, [ enhanceWithSiteType ] );
 
 		const isOdysseyStats = config.isEnabled( 'is_running_in_jetpack_site' );
@@ -46,18 +43,10 @@ export default function useTracks( {
 			properties,
 			title,
 		};
+
+		debug( `Recording: "${ title }" at "${ path }"` );
 		dispatch( recordEvent( eventName, eventProperties ) );
 	}, [ dispatch, options, path, properties, selectedSiteId, title ] );
 
-	const queuePageView = useCallback( () => {
-		if ( isLoadingSite ) {
-			debug(
-				`Page view not queued: "${ title }" at "${ path }". Selected site has not been loaded.`
-			);
-			return;
-		}
-		recordPageView();
-	}, [ isLoadingSite, path, recordPageView, title ] );
-
-	return { queuePageView };
+	return { recordPageView };
 }
