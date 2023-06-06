8876dadca07bb5d88006a983e69e00d9a98e7176
Paulo Cruz <paulo.cruz@automattic.com>
2023-06-06 11:59:28 -0300

    Add icons for Empty view CTAs

---
+++ client/my-sites/subscribers/components/empty-list-view/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as GrowAudienceIcon } from './grow-audience-icon';
+export { default as ImportSubscribersIcon } from './import-subscribers-icon';
+export { default as TurnIntoSubscribersIcon } from './turn-into-subscribers-icon';
