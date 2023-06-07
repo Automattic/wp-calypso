4c616cea26b2099d1092b3f0af0145f713f77fbe
Dognose <dognose24@gmail.com>
2023-06-07 13:07:48 +0800

    Add TODOs for a more flexible layout

--- client/my-sites/stats/site.jsx
+++ client/my-sites/stats/site.jsx
@@ -206,6 +206,7 @@ class StatsSite extends Component {
 			'stats__module-list',
 			'stats__module-list--traffic',
 			'stats__module--unified',
+			// @TODO: Refactor hidden modules with a more flexible layout (e.g., Flexbox) to fit mass configuration to moduels in the future.
 			{
 				'stats__module-list--traffic-no-authors': this.isModuleHidden( 'authors' ),
 				'stats__module-list--traffic-no-videos': this.isModuleHidden( 'videos' ),
