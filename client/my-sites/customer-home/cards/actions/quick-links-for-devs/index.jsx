a1b37317c05f8b058985bd8994bf6b8728392d95
vykes-mac <knrymcleish@gmail.com>
2023-06-07 12:37:06 -0500

    refactor to use selectors instead of connect

--- client/my-sites/customer-home/cards/actions/quick-links-for-devs/index.jsx
+++ client/my-sites/customer-home/cards/actions/quick-links-for-devs/index.jsx
@@ -1,38 +1,39 @@
 import { useTranslate } from 'i18n-calypso';
 import { useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useDebouncedCallback } from 'use-debounce';
 import FoldableCard from 'calypso/components/foldable-card';
 import { savePreference } from 'calypso/state/preferences/actions';
 import { getPreference } from 'calypso/state/preferences/selectors';
 import { canCurrentUser } from 'calypso/state/selectors/can-current-user';
-import { getSelectedEditor } from 'calypso/state/selectors/get-selected-editor';
 import isSiteAtomic from 'calypso/state/selectors/is-site-wpcom-atomic';
-import {
-	getSiteFrontPage,
-	getCustomizerUrl,
-	getSiteOption,
-	isNewSite,
-} from 'calypso/state/sites/selectors';
 import getSiteAdminUrl from 'calypso/state/sites/selectors/get-site-admin-url';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import ActionBox from '../quick-links/action-box';
 import '../quick-links/style.scss';
 
-export const QuickLinks = ( {
-	isExpanded,
-	updateHomeQuickLinksToggleStatus,
-	siteSlug,
-	siteAdminUrl,
-	canManageSite,
-	isAtomic,
-} ) => {
+const QuickLinks = () => {
 	const translate = useTranslate();
+	const siteId = useSelector( getSelectedSiteId );
+	const siteSlug = useSelector( ( state ) => getSelectedSiteSlug( state ) );
+	const isAtomic = useSelector( ( state ) => isSiteAtomic( state, siteId ) );
+	const siteAdminUrl = useSelector( ( state ) => getSiteAdminUrl( state, siteId ) );
+	const canManageSite = useSelector( ( state ) =>
+		canCurrentUser( state, siteId, 'manage_options' )
+	);
+	const isExpanded = useSelector(
+		( state ) => getPreference( state, 'homeQuickLinksToggleStatus' ) !== 'collapsed'
+	);
+
+	const dispatch = useDispatch();
+	const updateToggleStatus = ( status ) => {
+		dispatch( savePreference( 'homeQuickLinksToggleStatus', status ) );
+	};
 	const [
 		debouncedUpdateHomeQuickLinksToggleStatus,
 		,
 		flushDebouncedUpdateHomeQuickLinksToggleStatus,
-	] = useDebouncedCallback( updateHomeQuickLinksToggleStatus, 1000 );
+	] = useDebouncedCallback( updateToggleStatus, 1000 );
 
 	const quickLinks = (
 		<div className="quick-links-for-devs__boxes quick-links__boxes">
@@ -101,31 +102,4 @@ export const QuickLinks = ( {
 	);
 };
 
-const mapStateToProps = ( state ) => {
-	const siteId = getSelectedSiteId( state );
-	const isClassicEditor = getSelectedEditor( state, siteId ) === 'classic';
-	const isStaticHomePage =
-		! isClassicEditor && 'page' === getSiteOption( state, siteId, 'show_on_front' );
-	const siteSlug = getSelectedSiteSlug( state );
-	const staticHomePageId = getSiteFrontPage( state, siteId );
-	const editHomePageUrl = isStaticHomePage && `/page/${ siteSlug }/${ staticHomePageId }`;
-
-	return {
-		menusUrl: getCustomizerUrl( state, siteId, 'menus' ),
-		isNewlyCreatedSite: isNewSite( state, siteId ),
-		canManageSite: canCurrentUser( state, siteId, 'manage_options' ),
-		siteSlug,
-		isStaticHomePage,
-		isAtomic: isSiteAtomic( state, siteId ),
-		editHomePageUrl,
-		siteAdminUrl: getSiteAdminUrl( state, siteId ),
-		isExpanded: getPreference( state, 'homeQuickLinksToggleStatus' ) !== 'collapsed',
-	};
-};
-
-const mapDispatchToProps = {
-	updateHomeQuickLinksToggleStatus: ( status ) =>
-		savePreference( 'homeQuickLinksToggleStatus', status ),
-};
-
-export default connect( mapStateToProps, mapDispatchToProps )( QuickLinks );
+export default QuickLinks;
