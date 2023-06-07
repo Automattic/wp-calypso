19429264405a78f97a4b5188a84f59c0b3f5e193
Dognose <dognose24@users.noreply.github.com>
2023-06-08 03:49:34 +0800

    Stats: Apply module toggling API along with modules layout changing (#77866)
    * Add modules settings Redux files
    
    * Add data layer along with API requests
    
    * Apply API requests to settings toggle panel
    
    * Apply hidden modules to traffic page
    
    * Refactor hidable modules sharing between pages
    
    * Adjust layout for hidden modules
    
    * Refactor module toggle button for position on mobile
    
    * Adjust gap by rows for divider to the next section
    
    * Revert "Refactor module toggle button for position on mobile"
    
    This reverts commit 361c868d49601ffd5f5435f166e6ab7c1389a09b.
    
    * Update popover z-index for header
    
    * Fix default state of pageModules
    
    * Add TODOs for a more flexible layout

---
+++ client/state/stats/module-toggles/selectors.ts
@@ -0,0 +1,27 @@
+import { get } from 'lodash';
+
+import 'calypso/state/stats/init';
+
+const EMPTY_RESULT = {};
+
+/**
+ * Returns module toggles for a given site
+ *
+ * @param   {Object}  state    Global state tree
+ * @param   {number}  siteId   Site ID
+ * @returns {Object}           Highlights object; see schema.
+ */
+export function getModuleToggles( state: object, siteId: number, pageName: string ) {
+	return get( state, [ 'stats', 'moduleToggles', 'data', siteId, pageName ], EMPTY_RESULT );
+}
+
+/**
+ * Returns whether or not the module toggles are being loaded
+ *
+ * @param   {Object}  state    Global state tree
+ * @param   {number}  siteId   Site ID
+ * @returns {boolean}          	 Array of stat types as strings
+ */
+export function isLoading( state: object, siteId: number ) {
+	return get( state, [ 'stats', 'moduleToggles', 'isLoading', siteId ] );
+}
