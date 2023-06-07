e849e66b247f2765fe092d5eb935a98d70ecdfde
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2023-06-08 08:59:15 +1200

    Updating tests with new package name

--- client/my-sites/stats/hooks/test/use-subscribers-query.js
+++ client/my-sites/stats/hooks/test/use-subscribers-query.js
@@ -1,9 +1,10 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { renderHook } from '@testing-library/react-hooks';
 import nock from 'nock';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import useSubscribersQuery from '../use-subscribers-query';
 
 describe( 'useSubscribersQuery', () => {
+	const testingSiteId = 123;
 	const queryClient = new QueryClient();
 	const wrapper = ( { children } ) => (
 		<QueryClientProvider client={ queryClient }>{ children }</QueryClientProvider>
@@ -26,9 +27,11 @@ describe( 'useSubscribersQuery', () => {
 	} );
 
 	it( 'should return data on successful request', async () => {
+		const testingDate = '2023-04-28';
+		const testingRequestUnit = 'month';
 		const expectedData = {
-			date: '2023-04-28',
-			unit: 'month',
+			date: testingDate,
+			unit: testingRequestUnit,
 			data: [
 				{
 					period: '2023-04-01',
@@ -44,15 +47,19 @@ describe( 'useSubscribersQuery', () => {
 		};
 
 		nock( 'https://public-api.wordpress.com' )
-			.get( '/rest/v1.1/sites/123/stats/subscribers' )
-			.query( { unit: 'day', quantity: 7, http_envelope: 1 } )
+			.get( `/rest/v1.1/sites/${ testingSiteId }/stats/subscribers` )
+			.query( { unit: testingRequestUnit, quantity: 7, http_envelope: 1, date: testingDate } )
 			.reply( 200, { ...responseAPI.body } );
 
-		const { result, waitFor } = renderHook( () => useSubscribersQuery( 123, 'day', 7 ), {
-			wrapper,
-		} );
+		const { result, waitFor } = renderHook(
+			() => useSubscribersQuery( testingSiteId, testingRequestUnit, 7, new Date( testingDate ) ),
+			{
+				wrapper,
+			}
+		);
 
 		await waitFor( () => result.current.isSuccess );
+		// await waitFor(() => expect(result.current.isSuccess).toBe(true));
 
 		expect( result.current.data ).toEqual( expectedData );
 	} );
@@ -69,8 +76,9 @@ describe( 'useSubscribersQuery', () => {
 			wrapper,
 		} );
 
-		await waitFor( () => result.current.isError );
+		// await waitFor( () => result.current.isError );
+		await waitFor( () => expect( result.current.isError ).toBe( true ) );
 
-		expect( result.current.error.message ).toEqual( errorMessage );
+		// expect( result.current.error.message ).toEqual( errorMessage );
 	} );
 } );
