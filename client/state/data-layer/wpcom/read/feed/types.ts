84e52e58fc1679ea31d71fbf7370233fa9b4cb82
Maciej Grabowski <littlesoldiergraba@gmail.com>
2023-06-07 10:00:50 +0200

    Create RecommendedSites component as per the new design - interactions, and TrainTracks not included yet

---
+++ client/state/data-layer/wpcom/read/feed/types.ts
@@ -0,0 +1,14 @@
+export type Feed = {
+	URL: string;
+	blog_ID: number;
+	description: string;
+	feed_ID: number;
+	feed_URL: string;
+	image: string;
+	is_following: boolean;
+	last_update: string;
+	name: string;
+	organization_id: number;
+	subscribers_count: number;
+	unseen_count: number;
+};
