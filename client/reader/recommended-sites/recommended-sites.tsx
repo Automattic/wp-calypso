87349930f6c5549a1de6311982cfb3019f934d67
Maciej Grabowski <littlesoldiergraba@gmail.com>
2023-06-08 13:15:37 +0200

    Removing the solution with ResizeObserver. It feels like a bit too much complexity for too little value at the moment. However, if at one point we will allow for collapsing the sidebar, it feels like we should introduce the ResizeObserver then.

--- client/reader/recommended-sites/recommended-sites.tsx
+++ client/reader/recommended-sites/recommended-sites.tsx
@@ -1,7 +1,7 @@
-import { BreakpointValues } from '@automattic/viewport';
+import { useBreakpoint } from '@automattic/viewport-react';
 import { __experimentalHStack as HStack } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import DotPager from 'calypso/components/dot-pager';
 import { requestRecommendedSites } from 'calypso/state/reader/recommended-sites/actions';
@@ -19,61 +19,15 @@ type RecommendedSite = {
 	feedId: number;
 };
 
-const shouldShowDotPagerView = ( containerWidth: number ) =>
-	containerWidth < BreakpointValues.Px660;
-
 const RecommendedSitesResponsiveContainer: React.FC = ( { children } ) => {
-	const divRef = useRef< HTMLDivElement >( null );
-	const [ showDotPagerView, setShowDotPagerView ] = useState< boolean >();
-
-	useEffect( () => {
-		// Ensure the ref is current and ResizeObserver API is supported
-		if ( divRef.current && typeof ResizeObserver !== 'undefined' ) {
-			// Implement ResizeObserver to track the width changes of the container.
-			// This approach offers an improved user experience as it accounts for the Calypso sidebar
-			// that collapses at certain points, expanding the container width despite a decrease in window width.
-			// Depending solely on viewport breakpoints doesn't yield satisfactory visual results, as it doesn't
-			// consider these dynamic UI changes.
-			setShowDotPagerView( shouldShowDotPagerView( divRef.current.offsetWidth ) );
-			const observer = new ResizeObserver( ( [ entry ] ) => {
-				if ( ! entry ) {
-					return;
-				}
-
-				setShowDotPagerView( shouldShowDotPagerView( entry.contentRect.width ) );
-			} );
-			observer.observe( divRef.current );
-
-			return () => {
-				if ( observer ) {
-					observer.disconnect();
-				}
-			};
-		}
-	}, [] ); // Empty dependency array ensures this runs on mount and unmount only
-
-	const wrappedChildren = useMemo( () => {
-		if ( showDotPagerView === undefined ) {
-			// Do not display anything until the current container width is determined,
-			// i.e. until the ResizeObserver callback is triggered
-			return null;
-		}
-
-		if ( showDotPagerView ) {
-			return <DotPager isClickEnabled={ true }>{ children }</DotPager>;
-		}
-
-		return (
-			<HStack className="recommended-sites__horizontal-list" spacing={ 6 } as="ul">
-				{ children }
-			</HStack>
-		);
-	}, [ children, showDotPagerView ] );
-
+	const displayAsDotPager = useBreakpoint( '<1040px' );
+	if ( displayAsDotPager ) {
+		return <DotPager isClickEnabled>{ children }</DotPager>;
+	}
 	return (
-		<div ref={ divRef } style={ { width: '100%', height: '100%' } }>
-			{ wrappedChildren }
-		</div>
+		<HStack className="recommended-sites__horizontal-list" spacing={ 6 } as="ul">
+			{ children }
+		</HStack>
 	);
 };
 
