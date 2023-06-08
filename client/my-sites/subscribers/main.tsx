c9ec3b640845237064e98892f3a66abb61bcff65
Paulo Cruz <paulo.cruz@automattic.com>
2023-06-08 19:12:10 -0300

    Add Pagination component to main page

--- client/my-sites/subscribers/main.tsx
+++ client/my-sites/subscribers/main.tsx
@@ -2,9 +2,24 @@ import config from '@automattic/calypso-config';
 import { translate } from 'i18n-calypso';
 import DocumentHead from 'calypso/components/data/document-head';
 import Main from 'calypso/components/main';
+import Pagination from 'calypso/components/pagination';
+import { useSelector } from 'calypso/state';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { usePagination } from './hooks';
+import { useSubscribersQuery } from './queries';
 
-export const Subscribers = () => {
+type SubscribersProps = {
+	page: number;
+	pageChanged: ( page: number ) => void;
+};
+
+const DEFAULT_PER_PAGE = 10;
+
+export const Subscribers = ( { page, pageChanged }: SubscribersProps ) => {
 	const isSubscribersPageEnabled = config.isEnabled( 'subscribers-page' );
+	const selectedSiteId = useSelector( getSelectedSiteId );
+	const { data, isFetching } = useSubscribersQuery( selectedSiteId, page, 1 );
+	const { pageClickCallback } = usePagination( page, pageChanged, isFetching );
 
 	if ( ! isSubscribersPageEnabled ) {
 		return null;
@@ -14,6 +29,16 @@ export const Subscribers = () => {
 		<Main>
 			<DocumentHead title={ translate( 'Subscribers' ) } />
 			Subscribers
+			{ data?.subscribers.map( ( subscriber ) => (
+				<div key={ subscriber.email_address }>{ subscriber.email_address }</div>
+			) ) }
+			<Pagination
+				className="subscribers__pagination"
+				page={ page }
+				perPage={ data?.per_page ?? DEFAULT_PER_PAGE }
+				total={ data?.total }
+				pageClick={ pageClickCallback }
+			/>
 		</Main>
 	);
 };
