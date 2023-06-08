7f22cbf3efc1a940cd5dbd3c63dfd7a6fd4f8f7d
Paulo Cruz <paulo.cruz@automattic.com>
2023-06-08 19:09:29 -0300

    Update getSubscribersCacheKey

--- client/my-sites/subscribers/helpers/index.ts
+++ client/my-sites/subscribers/helpers/index.ts
@@ -1,7 +1,14 @@
-const getSubscribersCacheKey = ( siteId: number, currentPage?: number ) => {
+const getSubscribersCacheKey = (
+	siteId: number | null,
+	currentPage?: number,
+	perPage?: number
+) => {
 	const cacheKey = [ 'subscribers', siteId ];
 	if ( currentPage ) {
 		cacheKey.push( currentPage );
+	}
+	if ( perPage ) {
+		cacheKey.push( 'per-page', perPage );
 	}
 	return cacheKey;
 };
--- client/my-sites/subscribers/queries/use-subscribers-query.tsx
+++ client/my-sites/subscribers/queries/use-subscribers-query.tsx
@@ -3,9 +3,9 @@ import wpcom from 'calypso/lib/wp';
 import { getSubscribersCacheKey } from '../helpers';
 import type { SubscriberEndpointResponse } from '../types';
 
-const useSubscribersQuery = ( siteId: number, page = 1, perPage = 10 ) => {
+const useSubscribersQuery = ( siteId: number | null, page = 1, perPage = 10 ) => {
 	return useQuery< SubscriberEndpointResponse >( {
-		queryKey: getSubscribersCacheKey( siteId, page ),
+		queryKey: getSubscribersCacheKey( siteId, page, perPage ),
 		queryFn: () =>
 			wpcom.req.get( {
 				path: `/sites/${ siteId }/subscribers?per_page=${ perPage }&page=${ page }`,
