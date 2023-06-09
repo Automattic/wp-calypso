16c37e0ccd26d9106e3a01719732728670188aca
Yashwin <yashwinpoojary@gmail.com>
2023-06-09 12:23:27 +0530

    Implement add phone number to monitor SMS notifications

--- client/jetpack-cloud/sections/agency-dashboard/downtime-monitoring/configure-sms-notification/index.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/downtime-monitoring/configure-sms-notification/index.tsx
@@ -1,25 +1,31 @@
 import { Button } from '@automattic/components';
 import { Icon, plus } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
+import type { StateMonitorSettingsSMS } from '../../sites-overview/types';
 
 interface Props {
 	toggleModal: () => void;
+	allPhoneItems: Array< StateMonitorSettingsSMS >;
 }
 
-export default function ConfigureSMSNotification( { toggleModal }: Props ) {
+export default function ConfigureSMSNotification( { toggleModal, allPhoneItems }: Props ) {
 	const translate = useTranslate();
 
-	const handleAddEmailClick = () => {
+	const handleAddPhoneClick = () => {
 		// Record event
 		toggleModal();
 	};
 
 	return (
 		<div className="configure-contact__card-container">
+			{ allPhoneItems.map( ( item ) => (
+				// TODO: Replace with the correct component
+				<li key={ item.phoneNumberFull }>{ item.phoneNumberFull }</li>
+			) ) }
 			<Button
 				compact
 				className="configure-contact__button"
-				onClick={ handleAddEmailClick }
+				onClick={ handleAddPhoneClick }
 				aria-label={ translate( 'Add phone number' ) }
 			>
 				<Icon size={ 18 } icon={ plus } />
