0b3ed576246142c97bcbeb636f8189afc3c121d3
Juan Lucha <juanlucha@gmail.com>
2023-06-09 14:46:20 +0200

    Subscribers Page: some fixes based on feedback (#78011)
    * Subscribers Page: some fixes based on feedback
    
    * Fixed a small typo
    
    ---------
    
    Co-authored-by: Tim Broddin <tim@wannabes.be>

--- client/my-sites/subscribers/subscriber-list/subscriber-row.tsx
+++ client/my-sites/subscribers/subscriber-list/subscriber-row.tsx
@@ -1,22 +1,16 @@
 import { Gridicon } from '@automattic/components';
-import moment from 'moment';
 import FormCheckbox from 'calypso/components/forms/form-checkbox';
+import TimeSince from 'calypso/components/time-since';
 import { Subscriber } from '../types';
 import { SubscriberProfile } from './subscriber-profile';
-
-type SubscriberRowProps = Subscriber & {
-	locale: string;
-};
 
 export const SubscriberRow = ( {
 	display_name,
 	email_address,
-	subscriptions,
 	openRate,
 	date_subscribed,
 	avatar,
-	locale,
-}: SubscriberRowProps ) => {
+}: Subscriber ) => {
 	return (
 		<li className="subscriber-row row" role="row">
 			<div className="subscriber-list__checkbox-column hidden" role="cell">
@@ -25,14 +19,12 @@ export const SubscriberRow = ( {
 			<span className="subscriber-list__profile-column" role="cell">
 				<SubscriberProfile avatar={ avatar } displayName={ display_name } email={ email_address } />
 			</span>
-			<span className="subscriber-list__subscription-type-column hidden" role="cell">
-				{ subscriptions && subscriptions.map( ( subscription ) => <div>{ subscription }</div> ) }
-			</span>
+			<span className="subscriber-list__subscription-type-column hidden" role="cell"></span>
 			<span className="subscriber-list__rate-column hidden" role="cell">
 				{ openRate }
 			</span>
 			<span className="subscriber-list__since-column" role="cell">
-				{ moment( date_subscribed ).locale( locale ).format( 'LL' ) }
+				<TimeSince date={ date_subscribed } dateFormat="LL" />
 			</span>
 			<span className="subscriber-list__menu-column" role="cell">
 				<Gridicon icon="ellipsis" size={ 24 } />
