a71dd423e93a7154f8b7964d5bcb837bc9cf19c5
Jasper Kang <jasper.kang@automattic.com>
2023-06-09 15:31:36 +1200

    Stats: show one tooltip at a time (#77983)
    * introduce conflit notice group
    
    * remove unused continue
    
    * foundActiveNotice needs to be reset for every group
    
    * revert changes

--- client/my-sites/stats/hooks/use-notice-visibility-query.ts
+++ client/my-sites/stats/hooks/use-notice-visibility-query.ts
@@ -9,12 +9,37 @@ export type Notices = {
 	traffic_page_settings: boolean;
 };
 
-export function queryNotices( siteId: number | null ): Promise< Notices > {
-	return wpcom.req.get( {
+// These notices are mutually exclusive, so if one is active, the other should be hidden.
+// The IDs are sorted by priory from high to low.
+const CONFLICT_NOTICE_ID_GROUPS: Record< string, Array< keyof Notices > > = {
+	settings_tool_tips: [ 'traffic_page_settings', 'traffic_page_highlights_module_settings' ],
+};
+
+/**
+ * Only allow one notice in a conflict group to be active at a time.
+ */
+const processConflictNotices = ( notices: Notices ): Notices => {
+	for ( const conflictNoticeGroup in CONFLICT_NOTICE_ID_GROUPS ) {
+		let foundActiveNotice = false;
+		for ( const confilictNoticeId of CONFLICT_NOTICE_ID_GROUPS[ conflictNoticeGroup ] ) {
+			if ( foundActiveNotice ) {
+				notices[ confilictNoticeId ] = false;
+			} else if ( notices?.[ confilictNoticeId ] ) {
+				foundActiveNotice = true;
+			}
+		}
+	}
+	return notices;
+};
+
+export async function queryNotices( siteId: number | null ): Promise< Notices > {
+	const payload = await wpcom.req.get( {
 		method: 'GET',
 		apiNamespace: 'wpcom/v2',
 		path: `/sites/${ siteId }/jetpack-stats-dashboard/notices`,
 	} );
+
+	return processConflictNotices( payload );
 }
 
 export default function useNoticeVisibilityQuery( siteId: number | null, noticeId: string ) {
@@ -22,7 +47,7 @@ export default function useNoticeVisibilityQuery( siteId: number | null, noticeI
 		queryKey: [ 'stats', 'notices-visibility', siteId ],
 		queryFn: () => queryNotices( siteId ),
 		select: ( payload: Record< string, boolean > ): boolean => !! payload?.[ noticeId ],
-		staleTime: 1000 * 60 * 1, // 1 minutes
+		staleTime: 1000 * 30, // 30 seconds
 		retry: 1,
 		retryDelay: 3 * 1000, // 3 seconds
 	} );
