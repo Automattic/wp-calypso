3968c133b3d8611f4e82451dec20e08f1106378c
Wesley Berrêdo <wberredo@gmail.com>
2023-06-09 15:41:16 -0300

    Replace Automattic's name from the translate string to a variable containing Automattic

--- packages/wpcom-template-parts/src/utils.ts
+++ packages/wpcom-template-parts/src/utils.ts
@@ -1,21 +1,27 @@
 import { translate as translateMethod } from 'i18n-calypso';
 
 export const getAutomatticBrandingNoun = ( translate: typeof translateMethod ) => {
+	const AutomatticBrand = 'Automattic';
+	const options = {
+		description:
+			'Branding to be shown on the Footer of the page, Automattic\'s variable will always contains the word "Automattic"',
+		args: { Automattic: AutomatticBrand },
+	};
 	const automatticRoger = [
-		translate( 'An Automattic brainchild' ),
-		translate( 'An Automattic contraption' ),
-		translate( 'An Automattic creation' ),
-		translate( 'An Automattic experiment' ),
-		translate( 'An Automattic invention' ),
-		translate( 'An Automattic joint' ),
-		translate( 'An Automattic medley' ),
-		translate( 'An Automattic opus' ),
-		translate( 'An Automattic production' ),
-		translate( 'An Automattic ruckus' ),
-		translate( 'An Automattic thingamajig' ),
+		translate( 'An %(Automattic)s brainchild', options ),
+		translate( 'An %(Automattic)s contraption', options ),
+		translate( 'An %(Automattic)s creation', options ),
+		translate( 'An %(Automattic)s experiment', options ),
+		translate( 'An %(Automattic)s invention', options ),
+		translate( 'An %(Automattic)s joint', options ),
+		translate( 'An %(Automattic)s medley', options ),
+		translate( 'An %(Automattic)s opus', options ),
+		translate( 'An %(Automattic)s production', options ),
+		translate( 'An %(Automattic)s ruckus', options ),
+		translate( 'An %(Automattic)s thingamajig', options ),
 	];
 
-	const branding = automatticRoger[ Math.floor( Math.random() * ( 10 - 0 + 1 ) + 0 ) ];
+	const branding = automatticRoger[ Math.floor( Math.random() * ( 10 - 0 + 1 ) + 0 ) ]?.toString();
 
 	return {
 		prefix: branding.split( 'Automattic' )[ 0 ],
