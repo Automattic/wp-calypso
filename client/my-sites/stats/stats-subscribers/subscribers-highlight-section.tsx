fd023597e9cceec893e79265e5aa7d936c19e22f
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2023-06-13 11:48:15 +1200

    Stats: Release subscribers section to the public (#77928)
    Co-authored-by: Jason Moon <noreply@jsnmoon.com>

--- client/my-sites/stats/stats-subscribers/subscribers-highlight-section.tsx
+++ client/my-sites/stats/stats-subscribers/subscribers-highlight-section.tsx
@@ -1,7 +1,5 @@
-import { CountComparisonCard, Popover } from '@automattic/components';
-import { Icon, info } from '@wordpress/icons';
+import { CountComparisonCard } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
-import { useRef, useState } from 'react';
 import useSubscribersTotalsQueries from '../hooks/use-subscribers-totals-query';
 import './subscribers-highlight-section.scss';
 
@@ -41,34 +39,11 @@ function useSubscriberHighlights( subscribersTotals: SubscribersTotalsData ) {
 
 function SubscriberHighlightsHeader() {
 	const translate = useTranslate();
-	const [ isTooltipVisible, setTooltipVisible ] = useState( false );
-	const infoReferenceElement = useRef( null );
 	const localizedTitle = translate( 'All time stats', {
 		comment: 'Heading for Subscribers page highlights section',
 	} );
-	return (
-		<h1 className="highlight-cards-heading">
-			{ localizedTitle }{ ' ' }
-			<span
-				className="info-wrapper"
-				ref={ infoReferenceElement }
-				onMouseEnter={ () => setTooltipVisible( true ) }
-				onMouseLeave={ () => setTooltipVisible( false ) }
-			>
-				<Icon className="info-icon" icon={ info } />
-			</span>
-			<Popover
-				className="tooltip tooltip--darker tooltip-subscribers-page highlight-card-tooltip"
-				isVisible={ isTooltipVisible }
-				position="bottom right"
-				context={ infoReferenceElement.current }
-			>
-				<div className="highlight-card-tooltip-content">
-					<p>Tooltip content goes here.</p>
-				</div>
-			</Popover>
-		</h1>
-	);
+
+	return <h1 className="highlight-cards-heading">{ localizedTitle }</h1>;
 }
 
 function SubscriberHighlightsListing( {
@@ -85,7 +60,6 @@ function SubscriberHighlightsListing( {
 					key={ highlight.heading }
 					heading={ highlight.heading }
 					count={ highlight.count }
-					//previousCount={ highlight.previousCount }
 					showValueTooltip
 				/>
 			) ) }
