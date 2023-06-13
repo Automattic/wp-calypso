2feb17f1bf3b29af14c4d031e6c12d4aa3a7f849
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2023-06-13 14:43:52 +0200

    Initial commit that adds the basic dialog

--- client/my-sites/subscribers/components/subscriber-list/subscriber-list.tsx
+++ client/my-sites/subscribers/components/subscriber-list/subscriber-list.tsx
@@ -1,7 +1,8 @@
-import { useTranslate } from 'i18n-calypso';
+import { useTranslate, translate } from 'i18n-calypso';
 import FormCheckbox from 'calypso/components/forms/form-checkbox';
 import { Subscriber } from '../../types';
 import { SubscriberRow } from './subscriber-row';
+import accept from 'calypso/lib/accept';
 import './styles.scss';
 
 type SubscriberListProps = {
@@ -9,6 +10,38 @@ type SubscriberListProps = {
 };
 
 const noop = () => undefined;
+
+const unsubscribe = ( subscriber: Subscriber ) => {
+	const subscriberHasPlans = !! subscriber?.plans?.length;
+	const confirmButtonLabel = subscriberHasPlans
+		? translate( 'Manage', { context: 'Navigate to the Earns page button text.' } )
+		: translate( 'Unsubscribe', { context: 'Confirm Unsubscribe subscriber button text.' } );
+
+	const confirmMessage = subscriberHasPlans
+		? translate(
+				'Looks like you’re trying to remove a paid subscriber %s. You’ll need to remove their paid subscription to continue.',
+				{ args: [ subscriber.display_name ], comment: "%s is the subscriber's public display name" }
+		  )
+		: translate(
+				'Are you sure you want to remove %s from your subscribers? They will no longer receive any notifications from your site.',
+				{ args: [ subscriber.display_name ], comment: "%s is the subscriber's public display name" }
+		  );
+
+	accept(
+		<div>
+			<p>{ confirmMessage }</p>
+		</div>,
+		( accepted: boolean ) => {
+			if ( accepted ) {
+				// todo: run mutation that removes the subscriber depedning on if they have plans or not. If they are a paid subscriber, navigate to the Earns page.
+				// todo: maybe track the action
+			} else {
+				// todo: maybe track the action
+			}
+		},
+		confirmButtonLabel
+	);
+};
 
 export const SubscriberList = ( { subscribers }: SubscriberListProps ) => {
 	const translate = useTranslate();
@@ -37,7 +70,7 @@ export const SubscriberList = ( { subscribers }: SubscriberListProps ) => {
 					key={ subscriber.subscription_id }
 					subscriber={ subscriber }
 					onView={ noop }
-					onUnsubscribe={ noop }
+					onUnsubscribe={ () => unsubscribe( subscriber ) }
 				/>
 			) ) }
 		</ul>
