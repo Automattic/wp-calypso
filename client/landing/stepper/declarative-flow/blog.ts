d56028efa7b5437f84664ca7b758895b77806c82
Louis Laugesen <louis.laugesen@gmail.com>
2023-06-14 01:40:33 +1000

    Typescript fix (#78090)
    Stepper navigation isn't required for this flow.
    
    Co-authored-by: Dusty Reagan <dusty@dustyreagan.com>

--- client/landing/stepper/declarative-flow/blog.ts
+++ client/landing/stepper/declarative-flow/blog.ts
@@ -23,20 +23,8 @@ const Blog: Flow = {
 		];
 	},
 
-	useStepNavigation( _currentStep, navigate ) {
-		const goBack = () => {
-			return;
-		};
-
-		const goNext = () => {
-			return;
-		};
-
-		const goToStep = ( step: string ) => {
-			navigate( step );
-		};
-
-		return { goNext, goBack, goToStep };
+	useStepNavigation() {
+		return {};
 	},
 
 	useAssertConditions(): AssertConditionResult {
