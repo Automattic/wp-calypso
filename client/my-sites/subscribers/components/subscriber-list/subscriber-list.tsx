f36f090fb2f96ff1307c24c6e70344a31b026a53
Paulo Cruz <paulo.cruz@automattic.com>
2023-06-14 17:48:10 -0300

    Apply UnsubscribeModal to the subscribers page

--- client/my-sites/subscribers/components/subscriber-list/subscriber-list.tsx
+++ client/my-sites/subscribers/components/subscriber-list/subscriber-list.tsx
@@ -1,50 +1,19 @@
-import { useTranslate, translate } from 'i18n-calypso';
+import { useTranslate } from 'i18n-calypso';
 import FormCheckbox from 'calypso/components/forms/form-checkbox';
-import accept from 'calypso/lib/accept';
 import { Subscriber } from '../../types';
 import { SubscriberRow } from './subscriber-row';
 import './styles.scss';
 
 type SubscriberListProps = {
 	subscribers: Subscriber[];
+	onUnsubscribe: ( subscriber: Subscriber ) => void;
 };
 
 const noop = () => undefined;
 
-const unsubscribe = ( subscriber: Subscriber ) => {
-	const subscriberHasPlans = !! subscriber?.plans?.length;
-	const confirmButtonLabel = subscriberHasPlans
-		? translate( 'Manage', { context: 'Navigate to the Earns page button text.' } )
-		: translate( 'Unsubscribe', { context: 'Confirm Unsubscribe subscriber button text.' } );
-
-	const confirmMessage = subscriberHasPlans
-		? translate(
-				'Looks like you’re trying to remove a paid subscriber %s. You’ll need to remove their paid subscription to continue.',
-				{ args: [ subscriber.display_name ], comment: "%s is the subscriber's public display name" }
-		  )
-		: translate(
-				'Are you sure you want to remove %s from your subscribers? They will no longer receive any notifications from your site.',
-				{ args: [ subscriber.display_name ], comment: "%s is the subscriber's public display name" }
-		  );
-
-	accept(
-		<div>
-			<p>{ confirmMessage }</p>
-		</div>,
-		( accepted: boolean ) => {
-			if ( accepted ) {
-				// todo: run mutation that removes the subscriber depedning on if they have plans or not. If they are a paid subscriber, navigate to the Earns page.
-				// todo: maybe track the action
-			} else {
-				// todo: maybe track the action
-			}
-		},
-		confirmButtonLabel
-	);
-};
-
-export const SubscriberList = ( { subscribers }: SubscriberListProps ) => {
+export const SubscriberList = ( { subscribers, onUnsubscribe }: SubscriberListProps ) => {
 	const translate = useTranslate();
+
 	return (
 		<ul className="subscriber-list" role="table">
 			<li className="row header" role="row">
@@ -70,7 +39,7 @@ export const SubscriberList = ( { subscribers }: SubscriberListProps ) => {
 					key={ subscriber.subscription_id }
 					subscriber={ subscriber }
 					onView={ noop }
-					onUnsubscribe={ () => unsubscribe( subscriber ) }
+					onUnsubscribe={ onUnsubscribe }
 				/>
 			) ) }
 		</ul>
