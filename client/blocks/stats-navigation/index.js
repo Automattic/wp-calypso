53ed1ffe2768b11cec00a6d870fde57502b21001
Dognose <dognose24@gmail.com>
2023-06-14 13:51:18 +0800

    Fix module settings button position on mobile

--- client/blocks/stats-navigation/index.js
+++ client/blocks/stats-navigation/index.js
@@ -179,20 +179,20 @@ class StatsNavigation extends Component {
 					) }
 
 					{ ! config.isEnabled( 'stats/subscribers-section' ) && <SubscribersCount /> }
-
-					{ isModuleSettingsSupported && AVAILABLE_PAGE_MODULES[ this.props.selectedItem ] && (
-						<PageModuleToggler
-							availableModules={ AVAILABLE_PAGE_MODULES[ this.props.selectedItem ] }
-							pageModules={ pageModules }
-							onToggleModule={ this.onToggleModule }
-							isTooltipShown={ showSettingsTooltip && ! isPageSettingsTooltipDismissed }
-							onTooltipDismiss={ this.onTooltipDismiss }
-						/>
-					) }
 				</SectionNav>
 
 				{ isLegacy && showIntervals && (
 					<Intervals selected={ interval } pathTemplate={ pathTemplate } standalone />
+				) }
+
+				{ isModuleSettingsSupported && AVAILABLE_PAGE_MODULES[ this.props.selectedItem ] && (
+					<PageModuleToggler
+						availableModules={ AVAILABLE_PAGE_MODULES[ this.props.selectedItem ] }
+						pageModules={ pageModules }
+						onToggleModule={ this.onToggleModule }
+						isTooltipShown={ showSettingsTooltip && ! isPageSettingsTooltipDismissed }
+						onTooltipDismiss={ this.onTooltipDismiss }
+					/>
 				) }
 			</div>
 		);
