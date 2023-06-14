Automattic/wp-calypso

commit 5f382d584a840cabfac394a6522a9d45e5dd37f0

Checkout: remove annual features from the "what's included" list when picking a monthly plan on tailored flows (#75777)
* Plans: remove annual features from monthly plans when browsing tailored flows

* Rebase

* Only apply filtering on getFlowPlanFeatures

* Revert getPlanFeatureAccessor changes

* Rework to use `nextDomainIsFree` and include more info about domain purchase where possible.

* Fix type error

* Fix another type error

---------

Co-authored-by: mpkelly <kikemelly@gmail.com>

client/my-sites/checkout/composite-checkout/components/wp-checkout-order-summary.tsx
@@ -178,7 +178,12 @@ function CheckoutSummaryFeaturesWrapper( props: {
 	}
 
 	if ( signupFlowName && shouldUseFlowFeatureList ) {
-		return <CheckoutSummaryFlowFeaturesList flowName={ signupFlowName } />;
+		return (
+			<CheckoutSummaryFlowFeaturesList
+				flowName={ signupFlowName }
+				nextDomainIsFree={ nextDomainIsFree }
+			/>
+		);
 	}
 
 	return <CheckoutSummaryFeaturesList siteId={ siteId } nextDomainIsFree={ nextDomainIsFree } />;
@@ -379,14 +384,39 @@ function CheckoutSummaryFeaturesList( props: {
 	);
 }
 
-function CheckoutSummaryFlowFeaturesList( { flowName }: { flowName: string } ) {
+function CheckoutSummaryFlowFeaturesList( {
+	flowName,
+	nextDomainIsFree,
+}: {
+	flowName: string;
+	nextDomainIsFree: boolean;
+} ) {
 	const cartKey = useCartKey();
 	const { responseCart } = useShoppingCart( cartKey );
 	const planInCart = responseCart.products.find( ( product ) => isPlan( product ) );
-	const planFeatures = getFlowPlanFeatures( flowName, planInCart );
+	const hasDomainsInCart = responseCart.products.some(
+		( product ) => isDomainProduct( product ) || isDomainTransfer( product )
+	);
+	const domains = responseCart.products.filter(
+		( product ) => isDomainProduct( product ) || isDomainTransfer( product )
+	);
+	const hasRenewalInCart = responseCart.products.some(
+		( product ) => product.extra.purchaseType === 'renewal'
+	);
+	const planFeatures = getFlowPlanFeatures(
+		flowName,
+		planInCart,
+		hasDomainsInCart,
+		hasRenewalInCart,
+		nextDomainIsFree
+	);
 
 	return (
 		<CheckoutSummaryFeaturesListWrapper>
+			{ hasDomainsInCart &&
+				domains.map( ( domain ) => {
+					return <CheckoutSummaryFeaturesListDomainItem domain={ domain } key={ domain.uuid } />;
+				} ) }
 			{ planFeatures.map( ( feature ) => {
 				return (
 					<CheckoutSummaryFeaturesListItem key={ `feature-list-${ feature.getSlug() }` }>

client/my-sites/checkout/composite-checkout/lib/get-flow-plan-features.ts
@@ -1,4 +1,8 @@
-import { getPlan } from '@automattic/calypso-products';
+import {
+	applyTestFiltersToPlansList,
+	getPlan,
+	FEATURE_CUSTOM_DOMAIN,
+} from '@automattic/calypso-products';
 import { ResponseCartProduct } from '@automattic/shopping-cart';
 import { getPlanFeaturesObject } from 'calypso/lib/plans/features-list';
 import {
@@ -8,15 +12,24 @@ import {
 
 export default function getFlowPlanFeatures(
 	flowName: string,
-	plan: ResponseCartProduct | undefined
+	product: ResponseCartProduct | undefined,
+	hasDomainsInCart: boolean,
+	hasRenewalInCart: boolean,
+	nextDomainIsFree: boolean
 ) {
-	const productSlug = plan?.product_slug;
+	const productSlug = product?.product_slug;
 
 	if ( ! productSlug ) {
 		return [];
 	}
 
-	const planConstantObj = getPlan( productSlug );
+	const plan = getPlan( productSlug );
+
+	if ( ! plan ) {
+		return [];
+	}
+
+	const planConstantObj = applyTestFiltersToPlansList( plan, undefined );
 
 	if ( ! planConstantObj ) {
 		return [];
@@ -33,10 +46,15 @@ export default function getFlowPlanFeatures(
 	}
 
 	const highlightedFeatures = getHighlightedFeatures( flowName, planConstantObj );
-	return getPlanFeaturesObject( featureAccessor() ).map( ( feature ) => {
-		return {
-			...feature,
-			isHighlightedFeature: highlightedFeatures.includes( feature.getSlug() ),
-		};
-	} );
+	const showFreeDomainFeature = ! hasDomainsInCart && ! hasRenewalInCart && nextDomainIsFree;
+	return getPlanFeaturesObject( ( featureAccessor as () => string[] )() )
+		.filter( ( feature ) => {
+			return showFreeDomainFeature || feature.getSlug() !== FEATURE_CUSTOM_DOMAIN;
+		} )
+		.map( ( feature ) => {
+			return {
+				...feature,
+				isHighlightedFeature: highlightedFeatures.includes( feature.getSlug() ),
+			};
+		} );
 }

client/my-sites/plan-features-comparison/util.ts
@@ -15,7 +15,7 @@ const linkInBioFeatures = ( flowName: string, plan: IncompleteWPcomPlan ) => {
 };
 
 const hostingFeatures = ( flowName: string, plan: IncompleteWPcomPlan ) => {
-	return isHostingFlow( flowName ) && plan.getHostingSignupFeatures?.( plan.term );
+	return isHostingFlow( flowName ) && plan.getHostingSignupFeatures;
 };
 
 const signupFlowDefaultFeatures = (

commit 496597e3e7f65ebf7e8af6487858f52b19624bf3

Reverse error introduced in #75777 (#77736)
* Plans: remove annual features from monthly plans when browsing tailored flows

* Rebase

* Only apply filtering on getFlowPlanFeatures

* Revert getPlanFeatureAccessor changes

* Rework to use `nextDomainIsFree` and include more info about domain purchase where possible.

* Fix type error

* Fix another type error

* Fix issue introduced during merge where function signature was changed incorrectly.

---------

Co-authored-by: Luis Felipe Zaguini <luisfelipezaguini@gmail.com>

client/my-sites/checkout/composite-checkout/components/wp-checkout-order-summary.tsx
@@ -178,7 +178,12 @@ function CheckoutSummaryFeaturesWrapper( props: {
 	}
 
 	if ( signupFlowName && shouldUseFlowFeatureList ) {
-		return <CheckoutSummaryFlowFeaturesList flowName={ signupFlowName } />;
+		return (
+			<CheckoutSummaryFlowFeaturesList
+				flowName={ signupFlowName }
+				nextDomainIsFree={ nextDomainIsFree }
+			/>
+		);
 	}
 
 	return <CheckoutSummaryFeaturesList siteId={ siteId } nextDomainIsFree={ nextDomainIsFree } />;
@@ -379,14 +384,39 @@ function CheckoutSummaryFeaturesList( props: {
 	);
 }
 
-function CheckoutSummaryFlowFeaturesList( { flowName }: { flowName: string } ) {
+function CheckoutSummaryFlowFeaturesList( {
+	flowName,
+	nextDomainIsFree,
+}: {
+	flowName: string;
+	nextDomainIsFree: boolean;
+} ) {
 	const cartKey = useCartKey();
 	const { responseCart } = useShoppingCart( cartKey );
 	const planInCart = responseCart.products.find( ( product ) => isPlan( product ) );
-	const planFeatures = getFlowPlanFeatures( flowName, planInCart );
+	const hasDomainsInCart = responseCart.products.some(
+		( product ) => isDomainProduct( product ) || isDomainTransfer( product )
+	);
+	const domains = responseCart.products.filter(
+		( product ) => isDomainProduct( product ) || isDomainTransfer( product )
+	);
+	const hasRenewalInCart = responseCart.products.some(
+		( product ) => product.extra.purchaseType === 'renewal'
+	);
+	const planFeatures = getFlowPlanFeatures(
+		flowName,
+		planInCart,
+		hasDomainsInCart,
+		hasRenewalInCart,
+		nextDomainIsFree
+	);
 
 	return (
 		<CheckoutSummaryFeaturesListWrapper>
+			{ hasDomainsInCart &&
+				domains.map( ( domain ) => {
+					return <CheckoutSummaryFeaturesListDomainItem domain={ domain } key={ domain.uuid } />;
+				} ) }
 			{ planFeatures.map( ( feature ) => {
 				return (
 					<CheckoutSummaryFeaturesListItem key={ `feature-list-${ feature.getSlug() }` }>

client/my-sites/checkout/composite-checkout/lib/get-flow-plan-features.ts
@@ -1,4 +1,8 @@
-import { getPlan } from '@automattic/calypso-products';
+import {
+	applyTestFiltersToPlansList,
+	getPlan,
+	FEATURE_CUSTOM_DOMAIN,
+} from '@automattic/calypso-products';
 import { ResponseCartProduct } from '@automattic/shopping-cart';
 import { getPlanFeaturesObject } from 'calypso/lib/plans/features-list';
 import {
@@ -8,15 +12,24 @@ import {
 
 export default function getFlowPlanFeatures(
 	flowName: string,
-	plan: ResponseCartProduct | undefined
+	product: ResponseCartProduct | undefined,
+	hasDomainsInCart: boolean,
+	hasRenewalInCart: boolean,
+	nextDomainIsFree: boolean
 ) {
-	const productSlug = plan?.product_slug;
+	const productSlug = product?.product_slug;
 
 	if ( ! productSlug ) {
 		return [];
 	}
 
-	const planConstantObj = getPlan( productSlug );
+	const plan = getPlan( productSlug );
+
+	if ( ! plan ) {
+		return [];
+	}
+
+	const planConstantObj = applyTestFiltersToPlansList( plan, undefined );
 
 	if ( ! planConstantObj ) {
 		return [];
@@ -33,10 +46,15 @@ export default function getFlowPlanFeatures(
 	}
 
 	const highlightedFeatures = getHighlightedFeatures( flowName, planConstantObj );
-	return getPlanFeaturesObject( featureAccessor() ).map( ( feature ) => {
-		return {
-			...feature,
-			isHighlightedFeature: highlightedFeatures.includes( feature.getSlug() ),
-		};
-	} );
+	const showFreeDomainFeature = ! hasDomainsInCart && ! hasRenewalInCart && nextDomainIsFree;
+	return getPlanFeaturesObject( featureAccessor() )
+		.filter( ( feature ) => {
+			return showFreeDomainFeature || feature.getSlug() !== FEATURE_CUSTOM_DOMAIN;
+		} )
+		.map( ( feature ) => {
+			return {
+				...feature,
+				isHighlightedFeature: highlightedFeatures.includes( feature.getSlug() ),
+			};
+		} );
 }