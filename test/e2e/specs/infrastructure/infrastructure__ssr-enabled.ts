bab0099dd35b7f63e2442d87d1a95c5b934eed51
Edwin Takahashi <egao@outlook.com>
2023-06-15 17:33:22 +0900

    test/e2e/specs/infrastructure/infrastructure__ssr-enabled.ts - fix test cases

--- test/e2e/specs/infrastructure/infrastructure__ssr-enabled.ts
+++ test/e2e/specs/infrastructure/infrastructure__ssr-enabled.ts
@@ -14,18 +14,16 @@ describe( DataHelper.createSuiteTitle( 'Server-side Rendering' ), function () {
 		page = await browser.newPage();
 	} );
 
-	describe.each`
-		endpoint
-		${ 'log-in' }
-		${ 'themes' }
-		${ 'theme/twentytwentythree' }
-	`( 'Check SSR endoint: $endpoint', function ( { endpoint } ) {
-		it( 'Navigate to $endpoint', async function () {
-			await page.goto( endpoint, { timeout: 20 * 1000 } );
-		} );
+	describe.each( [ 'log-in', 'themes', 'theme/twentytwentythree' ] )(
+		'Check SSR endoint: %s',
+		function ( endpoint ) {
+			it( 'Navigate to $endpoint', async function () {
+				await page.goto( DataHelper.getCalypsoURL( endpoint ), { timeout: 20 * 1000 } );
+			} );
 
-		it( 'SSR is present for $endpoint', async function () {
-			await page.locator( '#wpcom[data-calypso-ssr="true"]' ).waitFor( { timeout: 15 * 1000 } );
-		} );
-	} );
+			it( `SSR is present for ${ endpoint }`, async function () {
+				await page.locator( '#wpcom[data-calypso-ssr="true"]' ).waitFor( { timeout: 15 * 1000 } );
+			} );
+		}
+	);
 } );
