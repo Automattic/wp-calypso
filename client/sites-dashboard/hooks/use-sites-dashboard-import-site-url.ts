10fea0a3c8f7d0d6b7b408ccdfd876189910c109
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2023-06-16 16:19:20 -0300

    Hosting onboarding: forward `hosting-flow` query parameter to onboarding flow after clicking the CTA (#78345)
    * Add hosting-flow=true query parameter to onboarding flow if it exists in the /sites url
    
    * Only check if the page currently has the query parameter, not if it initially had

--- client/sites-dashboard/hooks/use-sites-dashboard-import-site-url.ts
+++ client/sites-dashboard/hooks/use-sites-dashboard-import-site-url.ts
@@ -1,6 +1,8 @@
 import { addQueryArgs } from 'calypso/lib/url';
 import { useSelector } from 'calypso/state';
+import getCurrentQueryArguments from 'calypso/state/selectors/get-current-query-arguments';
 import getUserSetting from 'calypso/state/selectors/get-user-setting';
+import { AppState } from 'calypso/types';
 import { TRACK_SOURCE_NAME } from '../utils';
 
 export const useSitesDashboardImportSiteUrl = (
@@ -8,10 +10,15 @@ export const useSitesDashboardImportSiteUrl = (
 ) => {
 	const isDevAccount = useSelector( ( state ) => getUserSetting( state, 'is_dev_account' ) );
 
+	const isHostingFlow = useSelector(
+		( state: AppState ) => getCurrentQueryArguments( state )?.[ 'hosting-flow' ] === 'true'
+	);
+
 	return addQueryArgs(
 		{
 			source: TRACK_SOURCE_NAME,
 			...additionalParameters,
+			'hosting-flow': isHostingFlow ? true : null,
 		},
 		isDevAccount ? '/setup/import-hosted-site' : '/start/import'
 	);
