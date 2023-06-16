1ac2a8e666b0a7bbe0445e9069c8a9a608f1248f
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2023-06-16 15:31:06 -0300

    Add hosting-flow=true query parameter to onboarding flow if it exists in the /sites url

--- client/lib/paths/use-add-new-site-url.tsx
+++ client/lib/paths/use-add-new-site-url.tsx
@@ -1,25 +1,25 @@
 import config from '@automattic/calypso-config';
 import { useSelector } from 'react-redux';
 import { Primitive } from 'utility-types';
+import { isInHostingFlow } from 'calypso/landing/stepper/utils/is-in-hosting-flow';
 import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
 import { addQueryArgs } from 'calypso/lib/url';
-import getInitialQueryArguments from 'calypso/state/selectors/get-initial-query-arguments';
 import getUserSetting from 'calypso/state/selectors/get-user-setting';
-import type { AppState } from 'calypso/types';
 
 export const useAddNewSiteUrl = ( queryParameters: Record< string, Primitive > ) => {
 	const isDevAccount = useSelector( ( state ) => getUserSetting( state, 'is_dev_account' ) );
 
-	const isInHostingFlow = useSelector(
-		( state: AppState ) => getInitialQueryArguments( state )?.[ 'hosting-flow' ] === 'true'
-	);
+	const hostingFlow = useSelector( isInHostingFlow );
 
 	return addQueryArgs(
-		queryParameters,
+		{
+			...queryParameters,
+			'hosting-flow': hostingFlow ? true : null,
+		},
 		// eslint-disable-next-line no-nested-ternary
 		isJetpackCloud()
 			? config( 'jetpack_connect_url' )
-			: isDevAccount || isInHostingFlow
+			: isDevAccount || hostingFlow
 			? '/setup/new-hosted-site'
 			: config( 'signup_url' )
 	);
--- client/sites-dashboard/hooks/use-sites-dashboard-import-site-url.ts
+++ client/sites-dashboard/hooks/use-sites-dashboard-import-site-url.ts
@@ -1,3 +1,4 @@
+import { isInHostingFlow } from 'calypso/landing/stepper/utils/is-in-hosting-flow';
 import { addQueryArgs } from 'calypso/lib/url';
 import { useSelector } from 'calypso/state';
 import getUserSetting from 'calypso/state/selectors/get-user-setting';
@@ -8,10 +9,13 @@ export const useSitesDashboardImportSiteUrl = (
 ) => {
 	const isDevAccount = useSelector( ( state ) => getUserSetting( state, 'is_dev_account' ) );
 
+	const hostingFlow = useSelector( isInHostingFlow );
+
 	return addQueryArgs(
 		{
 			source: TRACK_SOURCE_NAME,
 			...additionalParameters,
+			'hosting-flow': hostingFlow ? true : null,
 		},
 		isDevAccount ? '/setup/import-hosted-site' : '/start/import'
 	);
