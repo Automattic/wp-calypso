40da42a5bdf4531862c53417c4f4f4fc24093faa
Rafael Agostini <rafael.agostini@automattic.com>
2023-06-16 13:34:42 -0500

    Add `Go back` button to backup contents page

--- client/my-sites/backup/backup-contents-page/index.tsx
+++ client/my-sites/backup/backup-contents-page/index.tsx
@@ -1,4 +1,6 @@
 import { Card } from '@automattic/components';
+import { Button } from '@wordpress/components';
+import { arrowLeft, Icon } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
 import { FunctionComponent } from 'react';
 import DocumentHead from 'calypso/components/data/document-head';
@@ -10,7 +12,9 @@ import Main from 'calypso/components/main';
 import SidebarNavigation from 'calypso/components/sidebar-navigation';
 import isJetpackCloud from 'calypso/lib/jetpack/is-jetpack-cloud';
 import { useSelector } from 'calypso/state';
+import getSiteSlug from 'calypso/state/sites/selectors/get-site-slug';
 import isJetpackSiteMultiSite from 'calypso/state/sites/selectors/is-jetpack-site-multi-site';
+import { backupMainPath } from '../paths';
 import FileBrowser from './file-browser';
 import './style.scss';
 
@@ -26,12 +30,20 @@ const BackupContentsPage: FunctionComponent< OwnProps > = ( { rewindId, siteId }
 	const displayDate = getDisplayDate( moment.unix( rewindId ), false );
 
 	const isMultiSite = useSelector( ( state ) => isJetpackSiteMultiSite( state, siteId ) );
+	const siteSlug = useSelector( ( state ) => getSiteSlug( state, siteId ) );
 
 	return (
 		<>
 			<Main className="backup-contents-page">
 				<DocumentHead title={ translate( 'Backup contents' ) } />
 				{ isJetpackCloud() && <SidebarNavigation /> }
+				<Button
+					isLink
+					className="backup-contents-page__back-button is-borderless"
+					href={ backupMainPath( siteSlug ) }
+				>
+					<Icon icon={ arrowLeft } size={ 16 } /> { translate( 'Go Back' ) }
+				</Button>
 				<Card>
 					<div className="backup-contents-page__header daily-backup-status status-card">
 						<div className="status-card__message-head">
