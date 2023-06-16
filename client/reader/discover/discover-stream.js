b1250fa09fc3f1d9e04d23e8a94b59d9279a11c7
Addison-Stavlo <stavz01@gmail.com>
2023-06-16 09:27:25 -0400

    add segmented control for tags list

--- client/reader/discover/discover-stream.js
+++ client/reader/discover/discover-stream.js
@@ -1,11 +1,20 @@
 import { useLocale } from '@automattic/i18n-utils';
 import { useQuery } from '@tanstack/react-query';
+import { translate } from 'i18n-calypso';
+import { useState } from 'react';
+import SegmentedControl from 'calypso/components/segmented-control';
 import wpcom from 'calypso/lib/wp';
 import Stream from 'calypso/reader/stream';
+import { useSelector } from 'calypso/state';
+import { getReaderFollowedTags } from 'calypso/state/reader/tags/selectors';
+
+const DEFAULT_TAB = 'recommended-tab';
 
 const DiscoverStream = ( props ) => {
 	const locale = useLocale();
-	const { data: recommendedTags } = useQuery( {
+	const followedTags = useSelector( getReaderFollowedTags ) || [];
+	const [ selectedTab, setSelectedTab ] = useState( DEFAULT_TAB );
+	const { data: interestTags } = useQuery( {
 		queryKey: [ 'read/interests', locale ],
 		queryFn: () =>
 			wpcom.req.get(
@@ -21,11 +30,44 @@ const DiscoverStream = ( props ) => {
 			return data.interests;
 		},
 	} );
-	console.log( 'recommendedTags', recommendedTags ); // We should filter out followed tags...
 
-	// This is where we will render a horizontal scrollbar with tabs. The tab selected will
-	// determine what we render below. We will return the default Stream for the recommended tab,
-	// and figure out how to change this to tag streams when other tabs are selected.
-	return <Stream { ...props } />;
+	// Filter followed tags out of interestTags to get recommendedTags.
+	const followedTagSlugs = followedTags.map( ( tag ) => tag.slug );
+	const recommendedTags = interestTags.filter( ( tag ) => ! followedTagSlugs.includes( tag.slug ) );
+
+	const DiscoverNavigation = () => (
+		<SegmentedControl
+			primary
+			className="discover-stream__tab-control"
+			// TODO, move styles to a scss file and update further.
+			style={ { overflowX: 'scroll' } }
+		>
+			<SegmentedControl.Item
+				key={ DEFAULT_TAB }
+				selected={ DEFAULT_TAB === selectedTab }
+				onClick={ () => setSelectedTab( DEFAULT_TAB ) }
+			>
+				{ translate( 'Recommended' ) }
+			</SegmentedControl.Item>
+			{ recommendedTags.map( ( tag ) => {
+				return (
+					<SegmentedControl.Item
+						key={ tag.slug }
+						selected={ tag.slug === selectedTab }
+						onClick={ () => setSelectedTab( tag.slug ) }
+					>
+						{ tag.title }
+					</SegmentedControl.Item>
+				);
+			} ) }
+		</SegmentedControl>
+	);
+
+	return (
+		<>
+			<DiscoverNavigation />
+			<Stream { ...props } />;
+		</>
+	);
 };
 export default DiscoverStream;
