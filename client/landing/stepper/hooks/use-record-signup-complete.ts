75e55fea11dbf4ab8341ae70b76f97d559418fc2
James Tien <southp0105@gmail.com>
2023-06-16 21:56:15 +0800

    Fill in the missing props of the signup completion event as much as possible

--- client/landing/stepper/hooks/use-record-signup-complete.ts
+++ client/landing/stepper/hooks/use-record-signup-complete.ts
@@ -1,37 +1,55 @@
+import {
+	isDomainRegistration,
+	isDomainTransfer,
+	isDomainMapping,
+} from '@automattic/calypso-products';
 import { useSelect } from '@wordpress/data';
 import { useCallback } from 'react';
-import { USER_STORE } from 'calypso/landing/stepper/stores';
+import { USER_STORE, ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { recordSignupComplete } from 'calypso/lib/analytics/signup';
 import { useSite } from './use-site';
-import type { UserSelect } from '@automattic/data-stores';
+import type { UserSelect, OnboardSelect } from '@automattic/data-stores';
 
 export const useRecordSignupComplete = ( flow: string | null ) => {
 	const site = useSite();
 	const siteId = site?.ID || null;
 	const theme = site?.options?.theme_slug || '';
-	const siteCount = useSelect(
-		( select ) => ( select( USER_STORE ) as UserSelect ).getCurrentUser(),
-		[]
-	)?.site_count;
+	const { domainCartItem, planCartItem, siteCount } = useSelect( ( select ) => {
+		return {
+			siteCount: ( select( USER_STORE ) as UserSelect ).getCurrentUser()?.site_count,
+			domainCartItem: ( select( ONBOARD_STORE ) as OnboardSelect ).getDomainCartItem(),
+			planCartItem: ( select( ONBOARD_STORE ) as OnboardSelct ).getPlanCartItem(),
+		};
+	}, [] );
 
 	return useCallback( () => {
+		// FIXME: once moving to the Stepper verion of User step,
+		// wire the value of `isNewUser()` from the user store.
+		const isNewUser = ! siteCount;
+
+		// FIXME:
+		// currently it's impossible to derive this data since it requires
+		// the length of registration, so I use isNewUser here as an approximation
+		const isNew7DUserSite = isNewUser;
+
 		recordSignupComplete(
 			{
 				flow,
-				siteId: siteId,
-				isNewUser: ! siteCount,
-				hasCartItems: false,
-				isNew7DUserSite: '',
+				siteId,
+				isNewUser,
+				hasCartItems: domainCartItem !== null || planCartItem !== null,
+				isNew7DUserSite,
 				theme,
 				intent: flow,
 				startingPoint: flow,
 				isBlankCanvas: theme?.includes( 'blank-canvas' ),
-				domainProductSlug: '',
-				planProductSlug: '',
-				isMapping: false,
-				isTransfer: false,
+				domainProductSlug:
+					domainCartItem && isDomainRegistration( domainCartItem ) && domainCartItem.product_slug,
+				planProductSlug: planCartItem && planCartItem.product_slug,
+				isMapping: domainCartItem && isDomainMapping( domainCartItem ),
+				isTransfer: domainCartItem && isDomainTransfer( domainCartItem ),
 			},
 			true
 		);
-	}, [ flow, siteId, siteCount, theme ] );
+	}, [ flow, siteId, siteCount, theme, domainCartItem, planCartItem ] );
 };
