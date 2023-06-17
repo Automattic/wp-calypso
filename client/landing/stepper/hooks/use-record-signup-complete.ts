33c87ca0d5390927b80847723a6ac81367740afd
James Tien <southp0105@gmail.com>
2023-06-17 10:32:53 +0800

    Fix product slug derivation logics and remove `now`

--- client/landing/stepper/hooks/use-record-signup-complete.ts
+++ client/landing/stepper/hooks/use-record-signup-complete.ts
@@ -32,24 +32,35 @@ export const useRecordSignupComplete = ( flow: string | null ) => {
 		// the length of registration, so I use isNewUser here as an approximation
 		const isNew7DUserSite = isNewUser;
 
-		recordSignupComplete(
-			{
-				flow,
-				siteId,
-				isNewUser,
-				hasCartItems: domainCartItem !== null || planCartItem !== null,
-				isNew7DUserSite,
-				theme,
-				intent: flow,
-				startingPoint: flow,
-				isBlankCanvas: theme?.includes( 'blank-canvas' ),
-				domainProductSlug:
-					domainCartItem && isDomainRegistration( domainCartItem ) && domainCartItem.product_slug,
-				planProductSlug: planCartItem && planCartItem.product_slug,
-				isMapping: domainCartItem && isDomainMapping( domainCartItem ),
-				isTransfer: domainCartItem && isDomainTransfer( domainCartItem ),
-			},
-			true
-		);
+		// FIXME:
+		// the domain store considers a free domain a domain registration and will populate a domainCartItem object.
+		// Thus the only way to check whether it's a free domain for now is whether domainCarItem is undefined or the product_slug is an empty string.
+		// This behavior worths a revisit. It's also why we can't check hasCartItems as simply as domainCartItem || planCartItem
+		const domainProductSlug =
+			domainCartItem &&
+			isDomainRegistration( domainCartItem ) &&
+			( domainCartItem.product_slug === '' ? undefined : domainCarItem.product_slug );
+		const hasCartItems = Boolean( domainProductSlug || planCartItem ); // see the function `dependenciesContainCartItem()
+
+		// TBD:
+		// When there is no plan put in the cart, `planCartItem` is `null` instead of `undefined` like domainCartItem.
+		// It worths a investigation of whether the both should behave the same so this code can be simplified.
+		const planProductSlug = planCartItem !== null ? planCartItem.product_slug : undefined;
+
+		recordSignupComplete( {
+			flow,
+			siteId,
+			isNewUser,
+			hasCartItems,
+			isNew7DUserSite,
+			theme,
+			intent: flow,
+			startingPoint: flow,
+			isBlankCanvas: theme?.includes( 'blank-canvas' ),
+			planProductSlug,
+			domainProductSlug,
+			isMapping: domainCartItem && isDomainMapping( domainCartItem ),
+			isTransfer: domainCartItem && isDomainTransfer( domainCartItem ),
+		} );
 	}, [ flow, siteId, siteCount, theme, domainCartItem, planCartItem ] );
 };
