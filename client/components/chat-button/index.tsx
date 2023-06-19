Automattic/wp-calypso

commit 095af745c3645bb80873a130ab211cdb84a69dd9

Remove useHappychat hook (#78173)
Part of efforts to sunset Happy Chat and remove all references: pdm0RZ-BU-p2.

- Remove useHappychat hook and replace its usages with hooks from https://github.com/Automattic/wp-calypso/pull/77499.
- Introduce a new ChatButton that is a much simplified and refactored version of HappychatButton. I'll replace all other uses of the HappychatButton component and remove it fully in a follow-up.

client/components/chat-button/index.tsx
@@ -1,0 +1,134 @@
+import { Button, Gridicon, Spinner } from '@automattic/components';
+import { HelpCenter } from '@automattic/data-stores';
+import { useChatStatus, useChatWidget } from '@automattic/help-center/src/hooks';
+import { useDispatch as useDataStoreDispatch } from '@wordpress/data';
+import { useI18n } from '@wordpress/react-i18n';
+import classnames from 'classnames';
+import type { MessagingGroup } from '@automattic/help-center/src/hooks/use-messaging-availability';
+import type { FC } from 'react';
+import './style.scss';
+
+type ChatIntent = 'SUPPORT' | 'PRESALES' | 'PRECANCELLATION';
+
+type Props = {
+	borderless?: boolean;
+	chatIntent?: ChatIntent;
+	className?: string;
+	floating?: boolean;
+	initialMessage: string;
+	onClick?: () => void;
+	onError?: () => void;
+	primary?: boolean;
+	siteUrl?: string;
+	withOffset?: boolean;
+};
+
+const HELP_CENTER_STORE = HelpCenter.register();
+
+function getMessagingGroupForIntent( chatIntent: ChatIntent ): MessagingGroup {
+	switch ( chatIntent ) {
+		case 'PRESALES':
+			return 'wpcom_presales';
+
+		case 'PRECANCELLATION':
+		case 'SUPPORT':
+		default:
+			return 'wpcom_messaging';
+	}
+}
+
+const ChatButton: FC< Props > = ( {
+	borderless = true,
+	chatIntent = 'SUPPORT',
+	children,
+	className = '',
+	floating = false,
+	initialMessage,
+	onClick,
+	onError,
+	primary = false,
+	siteUrl,
+	withOffset = false,
+} ) => {
+	const { __ } = useI18n();
+
+	const messagingGroup = getMessagingGroupForIntent( chatIntent );
+	const {
+		canConnectToZendesk,
+		hasActiveChats,
+		isChatAvailable,
+		isEligibleForChat,
+		isPrecancellationChatOpen,
+		isPresalesChatOpen,
+	} = useChatStatus( messagingGroup );
+	const { setShowHelpCenter, setInitialRoute } = useDataStoreDispatch( HELP_CENTER_STORE );
+
+	function shouldShowChatButton() {
+		if ( isEligibleForChat && hasActiveChats ) {
+			return true;
+		}
+
+		switch ( chatIntent ) {
+			case 'PRESALES':
+				if ( ! isPresalesChatOpen ) {
+					return false;
+				}
+				break;
+
+			case 'PRECANCELLATION':
+				if ( ! isPrecancellationChatOpen ) {
+					return false;
+				}
+				break;
+			default:
+				break;
+		}
+
+		if ( isEligibleForChat && isChatAvailable ) {
+			return true;
+		}
+	}
+
+	const { isOpeningChatWidget, openChatWidget } = useChatWidget();
+
+	const handleClick = () => {
+		if ( canConnectToZendesk ) {
+			openChatWidget( initialMessage, siteUrl, onError, onClick );
+		} else {
+			setInitialRoute( '/contact-form?mode=CHAT' );
+			setShowHelpCenter( true );
+			onClick?.();
+		}
+	};
+
+	const classes = classnames( 'chat-button', className, {
+		'is-floating': floating,
+		'with-offset': withOffset,
+	} );
+
+	if ( ! shouldShowChatButton() ) {
+		return null;
+	}
+
+	function getChildren() {
+		if ( isOpeningChatWidget ) {
+			return <Spinner />;
+		}
+
+		return children || <Gridicon icon="chat" />;
+	}
+
+	return (
+		<Button
+			className={ classes }
+			primary={ primary }
+			borderless={ borderless }
+			onClick={ handleClick }
+			title={ __( 'Support Chat' ) }
+		>
+			{ getChildren() }
+		</Button>
+	);
+};
+
+export default ChatButton;

client/components/chat-button/style.scss
@@ -1,0 +1,36 @@
+.chat-button.is-floating {
+	position: fixed;
+	right: 24px;
+	bottom: 24px;
+	z-index: z-index("root", ".floating-help");
+	line-height: 0;
+	padding: 1px;
+	border-radius: 100%;
+	background: var(--color-primary);
+	box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+
+	.gridicon {
+		fill: var(--color-text-inverted);
+		margin: 2px;
+		top: 0;
+		height: 24px;
+		width: 24px;
+	}
+
+	&.is-active {
+		background: var(--color-primary);
+		border-color: var(--color-primary-40);
+	}
+
+	&.has-unread {
+		background-color: var(--color-accent);
+		border-color: var(--color-accent);
+	}
+
+	// Make chat button appear above the FAB icon
+	// when the latter is already shown.
+	&.with-offset {
+		bottom: calc(24px + 60px);
+		right: calc(24px + 5px);
+	}
+}

client/components/marketing-survey/cancel-purchase-form/index.jsx
@@ -4,7 +4,6 @@ import {
 	isWpComMonthlyPlan,
 } from '@automattic/calypso-products';
 import { WPCOM_FEATURES_BACKUPS } from '@automattic/calypso-products/src';
-import { SUPPORT_HAPPYCHAT } from '@automattic/help-center';
 import { Button as GutenbergButton, CheckboxControl } from '@wordpress/components';
 import { localize } from 'i18n-calypso';
 import { shuffle } from 'lodash';
@@ -24,15 +23,12 @@ import { submitSurvey } from 'calypso/lib/purchases/actions';
 import wpcom from 'calypso/lib/wp';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { fetchAtomicTransfer } from 'calypso/state/atomic-transfer/actions';
-import hasActiveHappychatSession from 'calypso/state/happychat/selectors/has-active-happychat-session';
-import isHappychatAvailable from 'calypso/state/happychat/selectors/is-happychat-available';
 import {
 	getDowngradePlanRawPrice,
 	getDowngradePlanToMonthlyRawPrice,
 	willAtomicSiteRevertAfterPurchaseDeactivation,
 } from 'calypso/state/purchases/selectors';
 import getAtomicTransfer from 'calypso/state/selectors/get-atomic-transfer';
-import getSupportVariation from 'calypso/state/selectors/get-inline-help-support-variation';
 import getSiteImportEngine from 'calypso/state/selectors/get-site-import-engine';
 import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
 import siteHasFeature from 'calypso/state/selectors/site-has-feature';
@@ -668,14 +664,14 @@ class CancelPurchaseForm extends Component {
 				} );
 			case 'offsite_redirect':
 				/* 	Translators: If canceled product is site redirect,
-					displays canceled product and domain site is being directed to 
+					displays canceled product and domain site is being directed to
 					eg: "Remove product: Site Redirect to redirectedsite.com" */
 				return translate( '%(headerTitle)s: %(productName)s to %(purchaseMeta)s', {
 					args: { headerTitle, productName, purchaseMeta: meta },
 				} );
 			default:
 				/* Translators: If canceled product is site plan or other product,
-					displays plan or product being canceled and primary address of product being canceled 
+					displays plan or product being canceled and primary address of product being canceled
 					eg: "Cancel plan: WordPress.com Business for primarydomain.com" */
 				return translate( '%(headerTitle)s: %(productName)s for %(siteSlug)s', {
 					args: { headerTitle, productName, siteSlug: slug },
@@ -683,10 +679,8 @@ class CancelPurchaseForm extends Component {
 		}
 	}
 	render() {
-		const { isChatActive, isChatAvailable, purchase, site, supportVariation } = this.props;
+		const { purchase, site } = this.props;
 		const { surveyStep } = this.state;
-		const shouldShowChatButton =
-			( isChatAvailable || isChatActive ) && supportVariation === SUPPORT_HAPPYCHAT;
 
 		if ( ! surveyStep ) {
 			return null;
@@ -705,29 +699,17 @@ class CancelPurchaseForm extends Component {
 									{ this.getCanceledProduct() }
 								</span>
 							) }
-							{ shouldShowChatButton && (
-								<PrecancellationChatButton
-									icon="chat_bubble"
-									onClick={ this.closeDialog }
-									purchase={ purchase }
-									surveyStep={ surveyStep }
-									atBottom={ false }
-								/>
-							) }
+							<PrecancellationChatButton
+								icon="chat_bubble"
+								onClick={ this.closeDialog }
+								purchase={ purchase }
+								surveyStep={ surveyStep }
+							/>
 						</BlankCanvas.Header>
 						<BlankCanvas.Content>{ this.surveyContent() }</BlankCanvas.Content>
 						<BlankCanvas.Footer>
 							<div className="cancel-purchase-form__actions">
 								<div className="cancel-purchase-form__buttons">{ this.renderStepButtons() }</div>
-								{ shouldShowChatButton && (
-									<PrecancellationChatButton
-										icon="chat_bubble"
-										onClick={ this.closeDialog }
-										purchase={ purchase }
-										surveyStep={ surveyStep }
-										atBottom={ true }
-									/>
-								) }
 							</div>
 						</BlankCanvas.Footer>
 					</BlankCanvas>
@@ -739,13 +721,10 @@ class CancelPurchaseForm extends Component {
 
 export default connect(
 	( state, { purchase, linkedPurchases } ) => ( {
-		isChatAvailable: isHappychatAvailable( state ),
-		isChatActive: hasActiveHappychatSession( state ),
 		isAtomicSite: isSiteAutomatedTransfer( state, purchase.siteId ),
 		isImport: !! getSiteImportEngine( state, purchase.siteId ),
 		downgradePlanToPersonalPrice: getDowngradePlanRawPrice( state, purchase ),
 		downgradePlanToMonthlyPrice: getDowngradePlanToMonthlyRawPrice( state, purchase ),
-		supportVariation: getSupportVariation( state ),
 		site: getSite( state, purchase.siteId ),
 		willAtomicSiteRevert: willAtomicSiteRevertAfterPurchaseDeactivation(
 			state,

client/components/marketing-survey/cancel-purchase-form/precancellation-chat-button/index.jsx
@@ -1,69 +1,0 @@
-import { isDomainRegistration, isPlan } from '@automattic/calypso-products';
-import classNames from 'classnames';
-import { localize } from 'i18n-calypso';
-import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { connect } from 'react-redux';
-import HappychatButton from 'calypso/components/happychat/button';
-import MaterialIcon from 'calypso/components/material-icon';
-import { hasIncludedDomain } from 'calypso/lib/purchases';
-import { recordTracksEvent } from 'calypso/state/analytics/actions';
-import isPrecancellationChatAvailable from 'calypso/state/happychat/selectors/is-precancellation-chat-available';
-import './style.scss';
-
-class PrecancellationChatButton extends Component {
-	static propTypes = {
-		icon: PropTypes.string,
-		purchase: PropTypes.object.isRequired,
-		surveyStep: PropTypes.string,
-		onClick: PropTypes.func.isRequired,
-		translate: PropTypes.func.isRequired,
-		atBottom: PropTypes.bool,
-		className: PropTypes.string,
-	};
-
-	static defaultProps = {
-		surveyStep: '',
-	};
-
-	handleClick = () => {
-		const { purchase, surveyStep, onClick } = this.props;
-
-		this.props.recordTracksEvent( 'calypso_precancellation_chat_click', {
-			survey_step: surveyStep,
-			purchase: purchase.productSlug,
-			is_plan: isPlan( purchase ),
-			is_domain_registration: isDomainRegistration( purchase ),
-			has_included_domain: hasIncludedDomain( purchase ),
-		} );
-
-		onClick();
-	};
-
-	render() {
-		const { isAvailable, icon, translate, atBottom, className } = this.props;
-
-		if ( ! isAvailable ) {
-			return null;
-		}
-
-		return (
-			<HappychatButton
-				className={ classNames( 'precancellation-chat-button__main-button', className, {
-					'at-bottom': atBottom,
-				} ) }
-				onClick={ this.handleClick }
-			>
-				{ icon && <MaterialIcon icon={ icon } /> }
-				{ translate( 'Need help? Chat with us' ) }
-			</HappychatButton>
-		);
-	}
-}
-
-export default connect(
-	( state ) => ( {
-		isAvailable: isPrecancellationChatAvailable( state ),
-	} ),
-	{ recordTracksEvent }
-)( localize( PrecancellationChatButton ) );

client/components/marketing-survey/cancel-purchase-form/precancellation-chat-button/index.tsx
@@ -1,0 +1,65 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
+import { isDomainRegistration, isPlan } from '@automattic/calypso-products';
+import { useI18n } from '@wordpress/react-i18n';
+import classNames from 'classnames';
+import ChatButton from 'calypso/components/chat-button';
+import MaterialIcon from 'calypso/components/material-icon';
+import { hasIncludedDomain } from 'calypso/lib/purchases';
+import { useSelector } from 'calypso/state';
+import { getSiteUrl } from 'calypso/state/sites/selectors';
+import type { Purchase } from 'calypso/lib/purchases/types';
+import type { FC } from 'react';
+import './style.scss';
+
+type Props = {
+	icon?: string;
+	purchase: Purchase;
+	surveyStep?: string;
+	onClick: () => void;
+	className?: string;
+};
+
+const PrecancellationChatButton: FC< Props > = ( {
+	icon,
+	purchase,
+	surveyStep = '',
+	onClick,
+	className,
+} ) => {
+	const { __ } = useI18n();
+	const siteUrl =
+		useSelector( ( state ) => getSiteUrl( state, purchase.siteId ) ) || 'Unknown site';
+
+	const handleClick = () => {
+		recordTracksEvent( 'calypso_precancellation_chat_click', {
+			survey_step: surveyStep,
+			purchase: purchase.productSlug,
+			is_plan: isPlan( purchase ),
+			is_domain_registration: isDomainRegistration( purchase ),
+			has_included_domain: hasIncludedDomain( purchase ),
+		} );
+
+		onClick();
+	};
+
+	const initialMessage =
+		'User is contacting us from the pre-cancellation flow.\n' +
+		"Product they're attempting to cancel: " +
+		purchase.productName +
+		` (${ purchase.domain })`;
+
+	return (
+		<ChatButton
+			chatIntent="PRECANCELLATION"
+			initialMessage={ initialMessage }
+			siteUrl={ siteUrl }
+			className={ classNames( 'precancellation-chat-button__main-button', className ) }
+			onClick={ handleClick }
+		>
+			{ icon && <MaterialIcon icon={ icon } /> }
+			{ __( 'Need help? Chat with us' ) }
+		</ChatButton>
+	);
+};
+
+export default PrecancellationChatButton;

client/me/purchases/remove-purchase/index.jsx
@@ -11,7 +11,7 @@ import {
 	isTitanMail,
 	isAkismetProduct,
 } from '@automattic/calypso-products';
-import { Button, CompactCard, Gridicon } from '@automattic/components';
+import { CompactCard, Gridicon } from '@automattic/components';
 import classNames from 'classnames';
 import { localize } from 'i18n-calypso';
 import page from 'page';
@@ -233,14 +233,6 @@ class RemovePurchase extends Component {
 			className="remove-domain-dialog__chat-button"
 		/>
 	);
-
-	getContactUsButton = () => {
-		return (
-			<Button className="remove-purchase__support-link-button" href="/help/contact/">
-				{ this.props.translate( 'Contact Us' ) }
-			</Button>
-		);
-	};
 
 	shouldShowNonPrimaryDomainWarning() {
 		const { hasNonPrimaryDomainsFlag, isAtomicSite, hasCustomPrimaryDomain, purchase } = this.props;

client/components/marketing-survey/cancel-purchase-form/step-components/educational-content-step.tsx
@@ -1,3 +1,4 @@
+import { useChatWidget } from '@automattic/help-center/src/hooks';
 import { useLocalizeUrl } from '@automattic/i18n-utils';
 import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
@@ -9,7 +10,6 @@ import imgLoadingTime from 'calypso/assets/images/cancellation/loading-time.png'
 import imgSEO from 'calypso/assets/images/cancellation/seo.png';
 import FormattedHeader from 'calypso/components/formatted-header';
 import MaterialIcon from 'calypso/components/material-icon';
-import useHappyChat from '../use-happychat';
 import type { UpsellType } from '../get-upsell-type';
 import type { SiteDetails } from '@automattic/data-stores';
 import type { TranslateResult } from 'i18n-calypso';
@@ -72,8 +72,8 @@ type StepProps = {
 
 export default function EducationalCotnentStep( { type, site, ...props }: StepProps ) {
 	const translate = useTranslate();
-	const happyChat = useHappyChat();
 	const localizeUrl = useLocalizeUrl();
+	const { isOpeningChatWidget, openChatWidget } = useChatWidget();
 
 	switch ( type ) {
 		case 'education:loading-time':
@@ -250,14 +250,15 @@ export default function EducationalCotnentStep( { type, site, ...props }: StepPr
 										),
 										chat: (
 											<Button
+												isBusy={ isOpeningChatWidget }
+												disabled={ isOpeningChatWidget }
 												onClick={ () => {
 													page( `/domains/manage/${ site.slug }` );
-													const userInfo = happyChat.getUserInfo( { site } );
-													happyChat.open();
-													happyChat.sendUserInfo( {
-														...userInfo,
-														cancellationReason: props.cancellationReason,
-													} );
+													openChatWidget(
+														"User is contacting us from pre-cancellation form. Cancellation reason they've given: " +
+															props.cancellationReason,
+														site.URL
+													);
 												} }
 												isLink
 											/>

client/components/marketing-survey/cancel-purchase-form/step-components/upsell-step.tsx
@@ -1,6 +1,7 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { getPlan } from '@automattic/calypso-products';
 import formatCurrency from '@automattic/format-currency';
+import { useChatWidget } from '@automattic/help-center/src/hooks';
 import { Button } from '@wordpress/components';
 import { useTranslate, numberFormat } from 'i18n-calypso';
 import page from 'page';
@@ -14,7 +15,6 @@ import imgSwitchPlan from 'calypso/assets/images/cancellation/switch-plan.png';
 import FormattedHeader from 'calypso/components/formatted-header';
 import { useSelector } from 'calypso/state';
 import { getCurrentUserCurrencyCode } from 'calypso/state/currency-code/selectors';
-import useHappyChat from '../use-happychat';
 import type { UpsellType } from '../get-upsell-type';
 import type { SiteDetails } from '@automattic/data-stores';
 import type { Purchase } from 'calypso/lib/purchases/types';
@@ -110,12 +110,12 @@ type StepProps = {
 export default function UpsellStep( { upsell, site, purchase, ...props }: StepProps ) {
 	const translate = useTranslate();
 	const currencyCode = useSelector( getCurrentUserCurrencyCode ) || 'USD';
-	const happyChat = useHappyChat();
 	const numberOfPluginsThemes = numberFormat( 50000, 0 );
 	const discountRate = 25;
 	const couponCode = 'BIZWPC25';
 	const builtByURL = 'https://wordpress.com/website-design-service/?ref=wpcom-cancel-flow';
 	const { refundAmount } = props;
+	const { openChatWidget } = useChatWidget();
 
 	switch ( upsell ) {
 		case 'live-chat:plans':
@@ -132,11 +132,11 @@ export default function UpsellStep( { upsell, site, purchase, ...props }: StepPr
 						} );
 						page( getLiveChatUrl( upsell, site, purchase ) );
 
-						const userInfo = happyChat.getUserInfo( { site } );
-
-						happyChat.open();
-						happyChat.setChatTag( 'cancelation_chat_prompt' );
-						happyChat.sendUserInfo( { ...userInfo, cancellationReason: props.cancellationReason } );
+						openChatWidget(
+							"User is contacting us from pre-cancellation form. Cancellation reason they've given: " +
+								props.cancellationReason,
+							site.URL
+						);
 						props.closeDialog();
 					} }
 					onDecline={ props.onDeclineUpsell }

commit a28b56c174578f60d473dc875e6fb4c6778c8c3a

Purchases: Fix styling of chat button in auto-renwal dialog
Fixes #78379

Regression possibly introduced in #78173.

client/components/chat-button/index.tsx
@@ -6,7 +6,6 @@ import { useI18n } from '@wordpress/react-i18n';
 import classnames from 'classnames';
 import type { MessagingGroup } from '@automattic/help-center/src/hooks/use-messaging-availability';
 import type { FC } from 'react';
-import './style.scss';
 
 type ChatIntent = 'SUPPORT' | 'PRESALES' | 'PRECANCELLATION';
 
@@ -14,13 +13,11 @@ type Props = {
 	borderless?: boolean;
 	chatIntent?: ChatIntent;
 	className?: string;
-	floating?: boolean;
 	initialMessage: string;
 	onClick?: () => void;
 	onError?: () => void;
 	primary?: boolean;
 	siteUrl?: string;
-	withOffset?: boolean;
 };
 
 const HELP_CENTER_STORE = HelpCenter.register();
@@ -42,13 +39,11 @@ const ChatButton: FC< Props > = ( {
 	chatIntent = 'SUPPORT',
 	children,
 	className = '',
-	floating = false,
 	initialMessage,
 	onClick,
 	onError,
 	primary = false,
 	siteUrl,
-	withOffset = false,
 } ) => {
 	const { __ } = useI18n();
 
@@ -101,10 +96,7 @@ const ChatButton: FC< Props > = ( {
 		}
 	};
 
-	const classes = classnames( 'chat-button', className, {
-		'is-floating': floating,
-		'with-offset': withOffset,
-	} );
+	const classes = classnames( 'chat-button', className );
 
 	if ( ! shouldShowChatButton() ) {
 		return null;

client/components/chat-button/style.scss
@@ -1,36 +1,0 @@
-.chat-button.is-floating {
-	position: fixed;
-	right: 24px;
-	bottom: 24px;
-	z-index: z-index("root", ".floating-help");
-	line-height: 0;
-	padding: 1px;
-	border-radius: 100%;
-	background: var(--color-primary);
-	box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-
-	.gridicon {
-		fill: var(--color-text-inverted);
-		margin: 2px;
-		top: 0;
-		height: 24px;
-		width: 24px;
-	}
-
-	&.is-active {
-		background: var(--color-primary);
-		border-color: var(--color-primary-40);
-	}
-
-	&.has-unread {
-		background-color: var(--color-accent);
-		border-color: var(--color-accent);
-	}
-
-	// Make chat button appear above the FAB icon
-	// when the latter is already shown.
-	&.with-offset {
-		bottom: calc(24px + 60px);
-		right: calc(24px + 5px);
-	}
-}

client/components/marketing-survey/cancel-auto-renewal-form/index.jsx
@@ -128,7 +128,13 @@ class CancelAutoRenewalForm extends Component {
 			onClick: this.onSubmit,
 		};
 
-		const chat = <PrecancellationChatButton purchase={ purchase } onClick={ onClose } />;
+		const chat = (
+			<PrecancellationChatButton
+				purchase={ purchase }
+				onClick={ onClose }
+				className="cancel-auto-renewal-form__chat-button"
+			/>
+		);
 
 		return [ skip, submit, chat ];
 	};

client/me/purchases/remove-purchase/style.scss
@@ -35,7 +35,8 @@
 }
 
 .dialog__action-buttons {
-	.remove-domain-dialog__chat-button.precancellation-chat-button__main-button.button {
+	.remove-domain-dialog__chat-button.precancellation-chat-button__main-button.button,
+	.cancel-auto-renewal-form__chat-button.precancellation-chat-button__main-button.button {
 		display: block;
 		float: left;
 		font-size: 0.875rem;