d303ce9136bf49ea6c93d2109b022c96c76d94bd
mmtr <1233880+mmtr@users.noreply.github.com>
2023-06-19 16:18:23 +0200

    Use regex

--- client/my-sites/themes/themes-selection.jsx
+++ client/my-sites/themes/themes-selection.jsx
@@ -348,7 +348,7 @@ export const ConnectedThemesSelection = connect(
 			// We limit the WP.org themes to one page only.
 			page: 1,
 			// WP.com theme filters don't match WP.org ones, so we add them to the search term.
-			search: filter ? search + ' ' + filter.replaceAll( '+', ' ' ).replaceAll( '-', ' ' ) : search,
+			search: filter ? `${ search } ${ filter.replace( /[+-]/g, ' ' ) }` : search,
 		};
 		const wpOrgThemes = shouldFetchWpOrgThemes
 			? getThemesForQueryIgnoringPage( state, 'wporg', wpOrgQuery ) || []
