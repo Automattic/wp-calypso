b393789961c8ccc948ef1fa6decb7cdd3891dd7b
Paulo Cruz <paulo.cruz@automattic.com>
2023-06-19 16:22:10 -0300

    Navigate to details page

--- client/my-sites/subscribers/controller.js
+++ client/my-sites/subscribers/controller.js
@@ -1,6 +1,7 @@
 import page from 'page';
 import { addQueryArgs } from 'calypso/lib/route';
 import { sanitizeInt } from './helpers';
+import SubscribersPage from './main';
 import SubscriberDetailsPage from './subscriber-details-page';
 
 const pageChanged = ( path ) => ( pageNumber ) => {
@@ -14,6 +15,12 @@ export function subscribers( context, next ) {
 	const { path, query } = context;
 	const pageNumber = sanitizeInt( query.page ) ?? 1;
 
+	context.primary = (
+		<SubscribersPage pageNumber={ pageNumber } pageChanged={ pageChanged( path ) } />
+	);
+
+	next();
+}
 
 export function subscriberDetails( context, next ) {
 	const { path } = context;
