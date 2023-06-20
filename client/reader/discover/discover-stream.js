81fe5b1f10a618f62b6ca8345d81cb4285983041
Addison-Stavlo <stavz01@gmail.com>
2023-06-20 10:46:22 -0400

    raise nav above the render chain

--- client/reader/discover/discover-stream.js
+++ client/reader/discover/discover-stream.js
@@ -1,22 +1,45 @@
 import { useLocale } from '@automattic/i18n-utils';
 import { useQuery } from '@tanstack/react-query';
-import { translate } from 'i18n-calypso';
-import { useState } from 'react';
-import SegmentedControl from 'calypso/components/segmented-control';
 import wpcom from 'calypso/lib/wp';
 import Stream from 'calypso/reader/stream';
 import { useSelector } from 'calypso/state';
+import { getSelectedTab } from 'calypso/state/reader/discover/selectors';
 import { getReaderFollowedTags } from 'calypso/state/reader/tags/selectors';
-import DiscoverNav2 from './discover-navigation';
+import DiscoverNavigation from './discover-navigation';
 
 import './discover-stream.scss';
 
 const DEFAULT_TAB = 'recommended';
 
+const DiscoverFeed = ( { followedTagSlugs, ...props } ) => {
+	const selectedTab = useSelector( getSelectedTab );
+	const isDefaultTab = selectedTab === DEFAULT_TAB;
+
+	let streamKey = `discover:${ selectedTab }`;
+	// We want a different stream key for recommended depending on the followed tags that are available.
+	if ( isDefaultTab ) {
+		if ( ! followedTagSlugs.length ) {
+			streamKey += '-no-tags';
+		} else {
+			// Ensures a different key depending on the users followed tags list. So the stream can
+			// update when the user follows/unfollows other tags.
+			streamKey += followedTagSlugs.reduce( ( acc, val ) => acc + `-${ val }`, '' );
+		}
+	}
+
+	const streamProps = {
+		...props,
+		isDiscoverTags: ! isDefaultTab,
+		streamKey,
+	};
+
+	return <Stream { ...streamProps } />;
+};
+
 const DiscoverStream = ( props ) => {
 	const locale = useLocale();
 	const followedTags = useSelector( getReaderFollowedTags ) || [];
-	const [ selectedTab, setSelectedTab ] = useState( DEFAULT_TAB );
+
 	const { data: interestTags = [] } = useQuery( {
 		queryKey: [ 'read/interests', locale ],
 		queryFn: () =>
@@ -38,54 +61,10 @@ const DiscoverStream = ( props ) => {
 	const followedTagSlugs = followedTags.map( ( tag ) => tag.slug );
 	const recommendedTags = interestTags.filter( ( tag ) => ! followedTagSlugs.includes( tag.slug ) );
 
-	const isDefaultTab = selectedTab === DEFAULT_TAB;
-
-	const DiscoverNavigation = () => (
-		<SegmentedControl primary className="discover-stream__tab-control">
-			<SegmentedControl.Item
-				key={ DEFAULT_TAB }
-				selected={ DEFAULT_TAB === selectedTab }
-				onClick={ () => setSelectedTab( DEFAULT_TAB ) }
-			>
-				{ translate( 'Recommended' ) }
-			</SegmentedControl.Item>
-			{ recommendedTags.map( ( tag ) => {
-				return (
-					<SegmentedControl.Item
-						key={ tag.slug }
-						selected={ tag.slug === selectedTab }
-						onClick={ () => setSelectedTab( tag.slug ) }
-					>
-						{ tag.title }
-					</SegmentedControl.Item>
-				);
-			} ) }
-		</SegmentedControl>
-	);
-
-	let streamKey = `discover:${ selectedTab }`;
-	// We want a different stream key for recommended depending on the followed tags that are available.
-	if ( isDefaultTab ) {
-		if ( ! followedTags.length ) {
-			streamKey += '-no-tags';
-		} else {
-			// Ensures a different key depending on the users followed tags list. So the stream can
-			// update when the user follows/unfollows other tags.
-			streamKey += followedTagSlugs.reduce( ( acc, val ) => acc + `-${ val }`, '' );
-		}
-	}
-
-	const streamProps = {
-		...props,
-		isDiscoverTags: ! isDefaultTab,
-		streamKey,
-		streamHeader: <DiscoverNavigation />,
-	};
-
 	return (
 		<>
-			<DiscoverNav2 recommendedTags={ recommendedTags } />
-			<Stream { ...streamProps } />
+			<DiscoverNavigation recommendedTags={ recommendedTags } key="discover-navigation" />
+			<DiscoverFeed { ...props } followedTagSlugs={ followedTagSlugs } />
 		</>
 	);
 };
