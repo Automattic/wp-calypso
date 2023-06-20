1f99ec0c229b9a9d31916c606958e1d1730a7c7b
Miguel Torres <1233880+mmtr@users.noreply.github.com>
2023-06-20 09:57:58 +0200

    Theme showcase: Only show WP.org themes when the tier is not set (#78386)
    Excludes the WP.org themes from the showcase when a tier is set, since they are not good for any of the tiers:
    
    - They aren't "Free" as they need a Business plan.
    - They aren't "Premium" as they are not included in the Premium plan.
    - They aren't "Paid" as users don’t need a separate payment to get them.

--- client/my-sites/themes/themes-selection.jsx
+++ client/my-sites/themes/themes-selection.jsx
@@ -342,7 +342,11 @@ export const ConnectedThemesSelection = connect(
 
 		const themes = getThemesForQueryIgnoringPage( state, sourceSiteId, query ) || [];
 
-		const shouldFetchWpOrgThemes = forceWpOrgSearch && sourceSiteId !== 'wporg' && !! search; // Only fetch WP.org themes when searching a term.
+		const shouldFetchWpOrgThemes =
+			forceWpOrgSearch &&
+			sourceSiteId !== 'wporg' &&
+			!! search && // Only fetch WP.org themes when searching a term.
+			! tier; // WP.org themes are not a good fit for any of the tiers.
 		const wpOrgQuery = {
 			...query,
 			// We limit the WP.org themes to one page only.
