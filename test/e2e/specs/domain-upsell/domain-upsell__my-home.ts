4f033763802f5c41f25b77013861845158e5ac20
Edwin Takahashi <egao@outlook.com>
2023-06-20 15:15:42 +0900

    specs/domain-upsell/domain-upsell__my-home.ts - better handling of the asynchronous load of the home dashboard.

--- test/e2e/specs/domain-upsell/domain-upsell__my-home.ts
+++ test/e2e/specs/domain-upsell/domain-upsell__my-home.ts
@@ -2,19 +2,13 @@
  * @group calypso-pr
  */
 
-import {
-	CartCheckoutPage,
-	TestAccount,
-	BrowserManager,
-	RestAPIClient,
-} from '@automattic/calypso-e2e';
+import { TestAccount, BrowserManager, RestAPIClient } from '@automattic/calypso-e2e';
 import { Page, Browser } from 'playwright';
 
 declare const browser: Browser;
 
 describe( 'Domain: Upsell (Home)', function () {
-	let cartCheckoutPage: CartCheckoutPage;
-	let domain: string;
+	let suggestedDomain: string;
 	let page: Page;
 
 	beforeAll( async function () {
@@ -31,17 +25,40 @@ describe( 'Domain: Upsell (Home)', function () {
 		await testAccount.authenticate( page );
 	} );
 
-	it( 'Domain upsell CTA is present', async function () {
+	it( 'Wait for Home dashboard to fully load', async function () {
+		// There is nothing in the front-end to key off to confirm whether the
+		// Home dashboard has fully loaded.
+		// This is a serious compromise to get the ball moving on this refactor.
+		// See: https://github.com/Automattic/wp-calypso/issues/78414
+		try {
+			// Suppress errors because the intention here is to add some wait
+			// for the Home dashboard to finish loading.
+			await page.waitForLoadState( 'networkidle' );
+		} catch {
+			// noop
+		}
+	} );
+
+	it( 'Domain upsell card is present', async function () {
 		await page
 			.getByRole( 'main' )
 			.getByRole( 'heading', { name: /domain/ } )
 			.waitFor();
-
-		domain = await page.locator( '.domain-upsell-illustration' ).innerText();
 	} );
 
-	it( 'Click on domain upsell CTA button', async function () {
+	it( 'Domain upsell card has suggested domain', async function () {
+		const locator = page.locator( '.domain-upsell-illustration' );
+		await locator.waitFor();
+		suggestedDomain = await locator.innerText();
+
+		expect( suggestedDomain ).not.toBe( '' );
+	} );
+
+	it( 'Click to begin searching for a domain', async function () {
 		await page.getByRole( 'main' ).getByRole( 'button', { name: 'Get this domain' } ).click();
+
+		// The test user does not have a plan so the Plans upsell page will load.
+		await page.waitForURL( /plans\/yearly/ );
 	} );
 
 	it( 'Choose the Free plan', async function () {
@@ -52,8 +69,10 @@ describe( 'Domain: Upsell (Home)', function () {
 		await page.getByRole( 'button', { name: 'That works for me' } ).click();
 	} );
 
-	it( 'Secure checkout loads with the selected domain', async function () {
-		cartCheckoutPage = new CartCheckoutPage( page );
-		await cartCheckoutPage.validateCartItem( domain );
+	it( 'Secure checkout loads', async function () {
+		// Intentionally not checking whether the selected domain loads here,
+		// because this spec can run in parallel in many branches thus leading
+		// to a race condition.
+		await page.waitForURL( /checkout/ );
 	} );
 } );
