fd4151c99068a840c5d293545f0a0eb0b8ba5ba1
Ashar Fuadi <ashar.fuadi@automattic.com>
2023-06-21 11:30:23 +0700

    Reset timer for a notice with new content

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/notices/notices.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/notices/notices.tsx
@@ -1,37 +1,39 @@
-import { SnackbarList, withNotices, NoticeList } from '@wordpress/components';
+import { SnackbarList, withNotices } from '@wordpress/components';
 import i18n from 'i18n-calypso';
-import { useEffect } from 'react';
+import { ReactNode, useEffect, useRef } from 'react';
 import type { Pattern } from '../types';
 import './notices.scss';
 
 const NOTICE_TIMEOUT = 5000;
-
-type Notice = NoticeList.Notice & {
-	timer?: ReturnType< typeof setTimeout >;
-};
 
 const Notices = ( {
 	noticeList,
 	noticeOperations,
 }: Pick< withNotices.Props, 'noticeList' | 'noticeOperations' > ) => {
 	const onRemoveNotice = ( id: string ) => {
-		const notice = noticeList.find( ( notice ) => id === notice.id ) as Notice;
-		if ( notice?.timer ) {
-			clearTimeout( notice.timer );
-			delete notice.timer;
-		}
 		noticeOperations.removeNotice( id );
 	};
 
+	const timersByContentRef = useRef< Map< ReactNode, ReturnType< typeof setTimeout > > >(
+		new Map()
+	);
 	useEffect( () => {
-		const lastNotice = noticeList[ noticeList.length - 1 ] as Notice;
-
-		if ( lastNotice?.id && ! lastNotice?.timer ) {
-			lastNotice.timer = setTimeout(
-				() => noticeOperations.removeNotice( lastNotice.id ),
-				NOTICE_TIMEOUT
-			);
-		}
+		timersByContentRef.current.forEach( ( timer, content ) => {
+			if ( ! noticeList.some( ( notice ) => notice.content === content ) ) {
+				clearTimeout( timer );
+				timersByContentRef.current.delete( content );
+			}
+		} );
+		noticeList.forEach( ( notice ) => {
+			if ( ! timersByContentRef.current.has( notice.content ) ) {
+				timersByContentRef.current.set(
+					notice.content,
+					setTimeout( () => {
+						noticeOperations.removeNotice( notice.id );
+					}, NOTICE_TIMEOUT )
+				);
+			}
+		} );
 	}, [ noticeList, noticeOperations ] );
 
 	return <SnackbarList notices={ noticeList } onRemove={ onRemoveNotice } />;
