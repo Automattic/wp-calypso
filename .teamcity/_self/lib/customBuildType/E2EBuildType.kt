c9c5cb102f3eef50cfa79d496f23ad6bad450f5b
Edwin Takahashi <egao@outlook.com>
2023-06-22 17:18:22 +0900

    .teamcity/_self/lib/customBuildType/E2EBuildType.kt - add a second command to retry failed tests.

--- .teamcity/_self/lib/customBuildType/E2EBuildType.kt
+++ .teamcity/_self/lib/customBuildType/E2EBuildType.kt
@@ -147,6 +147,9 @@ open class E2EBuildType(
 
 					# Run suite.
 					xvfb-run yarn jest --reporters=jest-teamcity --reporters=default --maxWorkers=%JEST_E2E_WORKERS% --group=$testGroup
+
+					# Retry failed tests only.
+					xvfb-run yarn jest --reporters=jest-teamcity --reporters=default --maxWorkers=%JEST_E2E_WORKERS% --group=$testGroup --onlyFailures
 				"""
 				dockerImage = "%docker_image_e2e%"
 				dockerRunParameters = "-u %env.UID% --shm-size=4g"
