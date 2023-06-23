2782433ea850cd2bf4cc7c463bba6d9375addbb5
Juan Adolfo Lucha García <juanlucha@gmail.com>
2023-06-23 11:24:14 +0200

    Updated the Subscribers sorting to the changes in the endpoint

--- client/my-sites/subscribers/queries/use-subscribers-query.tsx
+++ client/my-sites/subscribers/queries/use-subscribers-query.tsx
@@ -2,7 +2,7 @@ import { SubscribersSortBy } from '@automattic/data-stores/src/reader';
 import { useQuery } from '@tanstack/react-query';
 import wpcom from 'calypso/lib/wp';
 import { getSubscribersCacheKey } from '../helpers';
-import type { Subscriber, SubscriberEndpointResponse } from '../types';
+import type { SubscriberEndpointResponse } from '../types';
 
 type SubscriberQueryParams = {
 	siteId: number | null;
@@ -19,42 +19,15 @@ const useSubscribersQuery = ( {
 	search,
 	sortTerm = SubscribersSortBy.DateSubscribed,
 }: SubscriberQueryParams ) => {
-	const sortByDateSubscribed = ( a: Subscriber, b: Subscriber ) => {
-		const aDate = new Date( a.date_subscribed );
-		const bDate = new Date( b.date_subscribed );
-		return aDate instanceof Date && bDate instanceof Date ? aDate.getTime() - bDate.getTime() : 0;
-	};
-
-	const sortByName = ( a: Subscriber, b: Subscriber ) =>
-		a.display_name.localeCompare( b.display_name );
-
-	const getSortFunction = ( sortTerm: SubscribersSortBy ) => {
-		switch ( sortTerm ) {
-			case SubscribersSortBy.DateSubscribed:
-				return sortByDateSubscribed;
-			case SubscribersSortBy.Name:
-				return sortByName;
-			default:
-				return undefined;
-		}
-	};
-
-	const sort = getSortFunction( sortTerm );
-
 	return useQuery< SubscriberEndpointResponse >( {
 		queryKey: getSubscribersCacheKey( siteId, page, perPage, search, sortTerm ),
 		queryFn: () =>
-			wpcom.req
-				.get( {
-					path: `/sites/${ siteId }/subscribers?per_page=${ perPage }&page=${ page }${
-						search ? `&search=${ encodeURIComponent( search ) }` : ''
-					}`,
-					apiNamespace: 'wpcom/v2',
-				} )
-				.then( ( response: SubscriberEndpointResponse ) => {
-					const subscribers = response.subscribers.sort( sort );
-					return { ...response, subscribers };
-				} ),
+			wpcom.req.get( {
+				path: `/sites/${ siteId }/subscribers?per_page=${ perPage }&page=${ page }${
+					search ? `&search=${ encodeURIComponent( search ) }` : ''
+				}${ sortTerm ? `&sort=${ sortTerm }` : '' }`,
+				apiNamespace: 'wpcom/v2',
+			} ),
 		enabled: !! siteId,
 		keepPreviousData: true,
 	} );
