644fa7610e0a3a6436adf0ef61f5fc177c41337a
Edwin Takahashi <egao@outlook.com>
2023-06-23 17:44:49 +0900

    packages/calypso-e2e/src/lib/components/notifications-component.ts - refactor the component POM to use modern meethods.
    packages/calypso-e2e/src/lib/test-account.ts
    - add new flag for `authenticate` call to allow waiting for the full load.
    
    test/e2e/specs/infrastructure/notification__actions.ts
    - refactor the spec to only perform one action, not to perform and undo the action.

--- test/e2e/specs/infrastructure/notification__actions.ts
+++ test/e2e/specs/infrastructure/notification__actions.ts
@@ -7,123 +7,120 @@ import {
 	NavbarComponent,
 	NotificationsComponent,
 	RestAPIClient,
-	ReaderResponse,
-	SecretsManager,
 	TestAccount,
 	NewCommentResponse,
-	SidebarComponent,
+	PostResponse,
 } from '@automattic/calypso-e2e';
 import { Page, Browser } from 'playwright';
 
 declare const browser: Browser;
 
-describe( DataHelper.createSuiteTitle( 'Notifications' ), function () {
-	const commentingUser = 'commentingUser';
-	const notificationsUser = 'notificationsUser';
+describe( 'Notifications: Actions', function () {
 	const comment = DataHelper.getRandomPhrase() + ' notification-actions-spec';
 
-	let testAccount: TestAccount;
+	// TestAccount and RestAPI instances.
+	let commentingUser: TestAccount;
+	let notificationsUser: TestAccount;
+	let commentingUserRestAPIClient: RestAPIClient;
+	let notificationUserRestAPIClient: RestAPIClient;
+
+	// API responses.
+	let newPost: PostResponse;
+	let newComment: NewCommentResponse;
+
 	let notificationsComponent: NotificationsComponent;
-	let restAPIClient: RestAPIClient;
-	let siteID: number;
-	let postID: number;
-	let commentID: number;
 	let page: Page;
 
 	beforeAll( async function () {
-		restAPIClient = new RestAPIClient( SecretsManager.secrets.testAccounts.commentingUser );
+		// Create an instance of RestAPI as the user making the comment.
+		commentingUser = new TestAccount( 'commentingUser' );
+		commentingUserRestAPIClient = new RestAPIClient( commentingUser.credentials );
 
-		// Get the latest post in the reader stream.
-		const readerFeed: ReaderResponse = await restAPIClient.getReaderFeed();
-		siteID = readerFeed.posts[ 0 ].site_ID;
-		postID = readerFeed.posts[ 0 ].ID;
+		// Create an instance of RestAPI as the user receiving notification.
+		notificationsUser = new TestAccount( 'notificationsUser' );
+		notificationUserRestAPIClient = new RestAPIClient( notificationsUser.credentials );
 
-		// Leave a comment on the post.
-		const response: NewCommentResponse = await restAPIClient.createComment(
-			siteID,
-			postID,
+		// Create a new post and store the response.
+		newPost = await notificationUserRestAPIClient.createPost(
+			notificationsUser.credentials.testSites?.primary.id as number,
+			{ title: DataHelper.getRandomPhrase() }
+		);
+
+		// Create a new comment on the post as the commentingUser and
+		// store the response.
+		newComment = await commentingUserRestAPIClient.createComment(
+			notificationsUser.credentials.testSites?.primary.id as number,
+			newPost.ID,
 			comment
 		);
-		commentID = response.ID;
 
-		// Log in as the notification user.
+		// Log in as the user receiving the notification.
 		page = await browser.newPage();
-		testAccount = new TestAccount( notificationsUser );
-		await testAccount.authenticate( page );
+		await notificationsUser.authenticate( page, { waitForFullLoad: true } );
 	} );
 
-	describe( `View notification as ${ notificationsUser }`, function () {
-		beforeAll( async function () {
-			// Wait for the `SidebarComponent` to fully render and be ready
-			// before attempting to interact with the notifications button.
-			const sidebarComponent = new SidebarComponent( page );
-			await sidebarComponent.waitForSidebarInitialization();
-		} );
-
-		it( 'Open notification using keyboard shortcut', async function () {
-			const navbarComponent = new NavbarComponent( page );
-			await navbarComponent.openNotificationsPanel( { useKeyboard: true } );
-		} );
-
-		it( `See and click notification for the comment left by ${ commentingUser }`, async function () {
-			notificationsComponent = new NotificationsComponent( page );
-			await notificationsComponent.clickNotification( comment );
-		} );
+	it( 'Open Notifications panel', async function () {
+		const navbarComponent = new NavbarComponent( page );
+		await navbarComponent.openNotificationsPanel();
 	} );
 
-	describe( 'Approve comment from notification', function () {
-		it( 'Approve comment', async function () {
-			await notificationsComponent.clickNotificationAction( 'Approve' );
-		} );
-
-		it( 'Unapprove comment', async function () {
-			await notificationsComponent.clickNotificationAction( 'Approve' );
-		} );
+	it( 'Click notification for the comment', async function () {
+		notificationsComponent = new NotificationsComponent( page );
+		await notificationsComponent.openNotification( comment );
 	} );
 
-	describe( 'Like comment from notification', function () {
-		it( 'Like comment', async function () {
-			await notificationsComponent.clickNotificationAction( 'Like' );
-		} );
-
-		it( 'Unlike comment', async function () {
-			await notificationsComponent.clickNotificationAction( 'Liked' );
-		} );
+	it( 'Approve comment', async function () {
+		await notificationsComponent.clickNotificationAction( 'Approve' );
 	} );
 
-	describe( 'Mark comment as spam from notification', function () {
-		it( 'Mark comment as spam', async function () {
-			await notificationsComponent.clickNotificationAction( 'Spam' );
-		} );
-
-		it( 'Undo mark comment as spam', async function () {
-			await notificationsComponent.clickUndo();
-		} );
+	it( 'Like comment', async function () {
+		await notificationsComponent.clickNotificationAction( 'Like' );
 	} );
 
-	describe( 'Trash comment from notification', function () {
-		it( 'Trash comment', async function () {
-			await notificationsComponent.clickNotificationAction( 'Trash' );
-		} );
-
-		it( 'Undo trash comment', async function () {
-			await notificationsComponent.clickUndo();
-		} );
+	it( 'Mark comment as spam', async function () {
+		await notificationsComponent.clickNotificationAction( 'Spam' );
+		await notificationsComponent.clickUndo();
 	} );
 
-	// Delete test comment that was created.
+	it( 'Trash comment', async function () {
+		await notificationsComponent.clickNotificationAction( 'Trash' );
+	} );
+
 	afterAll( async function () {
-		if ( typeof commentID === 'undefined' ) {
-			console.log( 'commentid ' + commentID );
+		if ( ! newComment ) {
 			return;
 		}
 
+		// Clean up the comment.
 		try {
-			restAPIClient = new RestAPIClient( SecretsManager.secrets.testAccounts.notificationsUser );
-			await restAPIClient.deleteComment( siteID, commentID );
-			console.log( 'Successfully cleaned up comment.' );
+			await notificationUserRestAPIClient.deleteComment(
+				notificationsUser.credentials.testSites?.primary.id as number,
+				newComment.ID
+			);
 		} catch ( e: unknown ) {
-			console.warn( `Failed to clean up test comment from user ${ commentingUser }` );
+			console.warn(
+				`Failed to clean up test comment in notification_action spec for site ${
+					notificationsUser.credentials.testSites?.primary.id as number
+				}, comment ${ newComment.ID }`
+			);
+		}
+
+		if ( ! newPost ) {
+			return;
+		}
+
+		// Clean up the post.
+		try {
+			await notificationUserRestAPIClient.deletePost(
+				notificationsUser.credentials.testSites?.primary.id as number,
+				newPost.ID
+			);
+		} catch ( e: unknown ) {
+			console.warn(
+				`Failed to clean up test comment in notification_action spec for site ${
+					notificationsUser.credentials.testSites?.primary.id as number
+				}, comment ${ newComment.ID }`
+			);
 		}
 	} );
 } );
