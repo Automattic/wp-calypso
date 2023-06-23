ab53e35ee2a832dcf5393c308128f40165a007ea
Candy Tsai <candy02058912@gmail.com>
2023-06-24 03:31:19 +0800

    Blog onboarding: Complete checkout before launch (#78185)
    * Checkout then launch flow idea
    
    * Update flow
    
    * Add respective launch step to design flow
    
    * Pass siteSlug in site-launch step
    
    * Remove cart from design-first stepper

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-helper.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-helper.tsx
@@ -10,7 +10,9 @@ import {
 	isDesignFirstFlow,
 	isNewsletterFlow,
 	isStartWritingFlow,
+	replaceProductsInCart,
 } from '@automattic/onboarding';
+import { MinimalRequestCartProduct } from '@automattic/shopping-cart';
 import { ExternalLink } from '@wordpress/components';
 import { dispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
@@ -18,6 +20,7 @@ import { addQueryArgs } from '@wordpress/url';
 import { translate } from 'i18n-calypso';
 import { PLANS_LIST } from 'calypso/../packages/calypso-products/src/plans-list';
 import { NavigationControls } from 'calypso/landing/stepper/declarative-flow/internals/types';
+import useCheckout from 'calypso/landing/stepper/hooks/use-checkout';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { isVideoPressFlow } from 'calypso/signup/utils';
 import { ONBOARD_STORE, SITE_STORE } from '../../../../stores';
@@ -44,7 +47,8 @@ export function getEnhancedTasks(
 	flow: string | null = '',
 	isEmailVerified = false,
 	checklistStatuses: LaunchpadStatuses = {},
-	planCartProductSlug?: string | null
+	planCartItem?: MinimalRequestCartProduct | null,
+	domainCartItem?: MinimalRequestCartProduct | null
 ) {
 	if ( ! tasks ) {
 		return [];
@@ -53,7 +57,8 @@ export function getEnhancedTasks(
 	const enhancedTaskList: Task[] = [];
 
 	const productSlug =
-		( isBlogOnboardingFlow( flow ) ? planCartProductSlug : null ) ?? site?.plan?.product_slug;
+		( isBlogOnboardingFlow( flow ) ? planCartItem?.product_slug : null ) ??
+		site?.plan?.product_slug;
 
 	const translatedPlanName = productSlug ? PLANS_LIST[ productSlug ].getTitle() : '';
 
@@ -346,18 +351,33 @@ export function getEnhancedTasks(
 						actionDispatch: () => {
 							if ( site?.ID ) {
 								const { setPendingAction, setProgressTitle } = dispatch( ONBOARD_STORE );
-								const { launchSite } = dispatch( SITE_STORE );
-
 								setPendingAction( async () => {
+									setProgressTitle( __( 'Directing to checkout' ) );
+									// If user selected products during onboarding, update cart and redirect to checkout
+									const onboardingCartItems = [ planCartItem, domainCartItem ].filter(
+										Boolean
+									) as MinimalRequestCartProduct[];
+									if ( onboardingCartItems.length ) {
+										await replaceProductsInCart( siteSlug as string, onboardingCartItems );
+										const { goToCheckout } = useCheckout();
+										goToCheckout( {
+											flowName: flow ?? '',
+											stepName: 'blog_launched',
+											siteSlug: siteSlug ?? '',
+											destination: `/setup/${ flow }/site-launch?siteSlug=${ siteSlug }`,
+											cancelDestination: '/home',
+										} );
+										return { goToCheckout: true };
+									}
+									// Launch blog if no items in cart
+									const { launchSite } = dispatch( SITE_STORE );
 									setProgressTitle( __( 'Launching blog' ) );
 									await launchSite( site.ID );
-
 									// Waits for half a second so that the loading screen doesn't flash away too quickly
 									await new Promise( ( res ) => setTimeout( res, 500 ) );
 									recordTaskClickTracksEvent( flow, task.completed, task.id );
 									return { blogLaunched: true, siteSlug };
 								} );
-
 								submit?.();
 							}
 						},
