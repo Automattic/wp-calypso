2103b984edee5202b9ae4897b1c7f14a4ffb18c6
Sebastian Barbosa <sebabarbosa@gmail.com>
2023-06-23 16:58:03 -0300

    Fix unit tests

--- client/my-sites/promote-post-i2/test/utils.test.tsx
+++ client/my-sites/promote-post-i2/test/utils.test.tsx
@@ -4,62 +4,32 @@
 import { getShortDateString, getLongDateString } from '../utils';
 
 describe( 'Promote Post Utils', () => {
-	test( 'returns short formatted date', () => {
-		const now = new Date();
-		const currentYear = now.getFullYear();
-		const currentMonth = now.getMonth() + 1;
-		const currentDate = now.getDate();
+	const timestamp = 1687521600000; // 2023-06-23 12:00:00 GMT+0000
 
-		const yesterday = new Date( now.getTime() - 3600 * 24 * 1000 );
-		const yesterdayYear = yesterday.getFullYear();
-		const yesterdayMonth = yesterday.getMonth() + 1;
-		const yesterdayDate = yesterday.getDate();
-		const yesterdayHours = yesterday.getHours();
-		const yesterdayMinutes = yesterday.getMinutes();
-		const yesterdaySeconds = yesterday.getSeconds();
-
-		const previousYear = currentYear - 1;
-
-		const testDataset = [
-			{
-				// hours ago
-				value: `${ currentYear }-${ currentMonth }-${ currentDate } 12:34:56`,
-				expected: 'hours ago',
-			},
-			{
-				// 1 day ago
-				value: `${ yesterdayYear }-${ yesterdayMonth }-${ yesterdayDate } ${ yesterdayHours }:${ yesterdayMinutes }:${ yesterdaySeconds }`,
-				expected: '1 day ago',
-			},
-			{
-				// Jun 15
-				value: `${ currentYear }-06-15 12:34:56`,
-				expected: 'Jun 15',
-			},
-			{
-				// Jun 15, 2022
-				value: `${ previousYear }-06-15 12:34:56`,
-				expected: `Jun 15, ${ previousYear }`,
-			},
-		];
-
-		for ( const item of testDataset ) {
-			expect( getShortDateString( item.value ) ).toBe( item.expected );
-		}
+	beforeAll( () => {
+		jest.useFakeTimers().setSystemTime( timestamp );
 	} );
 
-	test( 'returns long formatted date', () => {
-		const previousYear = new Date().getFullYear() - 1;
+	test.each( [
+		[ '2023-06-23T11:57:00+00:00', '3 minutes ago' ],
+		[ '2023-06-23T06:00:00+00:00', '6 hours ago' ],
+		[ '2023-06-22T12:00:00+00:00', 'a day ago' ],
+		[ '2023-06-20T12:00:00+00:00', '3 days ago' ],
+		[ '2023-05-17T12:00:00+00:00', 'May 17' ],
+		[ '2021-01-10T12:00:00+00:00', 'Jan 10, 2021' ],
+	] )( '[%s] returns correct short formatted date: %b', async ( date, expected ) => {
+		expect( getShortDateString( date ) ).toBe( expected );
+	} );
 
-		const testDataset = [
-			{
-				value: `${ previousYear }-06-15 12:34:56`,
-				expected: `Jun 15, ${ previousYear } at 12:34 PM`,
-			},
-		];
+	test.each( [
+		[ '2023-06-23T11:57:00+00:00', 'Today at 11:57 AM' ],
+		[ '2022-05-23T06:00:00+00:00', 'May 23, 2022 at 6:00 AM' ],
+	] )( '[%s] returns correct long formatted date: %b', async ( date, expected ) => {
+		expect( getLongDateString( date ) ).toBe( expected );
+	} );
 
-		for ( const item of testDataset ) {
-			expect( getLongDateString( item.value ) ).toBe( item.expected );
-		}
+	test( "formats date that doesn't have timezones", () => {
+		expect( getShortDateString( '2023-06-23 11:55' ) ).toBe( '5 minutes ago' );
+		expect( getLongDateString( '2021-03-22 16:10:00' ) ).toBe( 'Mar 22, 2021 at 4:10 PM' );
 	} );
 } );
