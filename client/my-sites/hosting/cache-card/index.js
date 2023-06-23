72ca27b315b91d195541aafc5c76c404658f399a
Antony Agrios <antonyagrios@gmail.com>
2023-06-23 18:19:28 +0300

    Edge Cache: Add edge cache toggle button (#78425)
    * Edge Cache: Add edge cache toggle button
    
    * Update: fix styling
    
    * Update: Update feature flag to avoid potential conflicts
    
    * Update: endpoint url
    
    * Update: change UI based on figma
    
    * Update: add optimistic update to mutation + minor fixes
    
    * Update: fix tests
    
    * Update: make endpoint use boolean instead of int
    
    * Update: fix bug where data where not refetched on mount
    
    * Update: fix lint errors
    
    * Update: Add loader component to handle initial loading
    
    * Update: fix loader and removed unecessary styles

--- client/my-sites/hosting/cache-card/index.js
+++ client/my-sites/hosting/cache-card/index.js
@@ -1,4 +1,9 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
+import config from '@automattic/calypso-config';
 import { Button, Card } from '@automattic/components';
+import styled from '@emotion/styled';
+import { ToggleControl } from '@wordpress/components';
+import { useDispatch } from '@wordpress/data';
 import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
 import CardHeading from 'calypso/components/card-heading';
@@ -8,42 +13,125 @@ import { clearWordPressCache } from 'calypso/state/hosting/actions';
 import getRequest from 'calypso/state/selectors/get-request';
 import { shouldRateLimitAtomicCacheClear } from 'calypso/state/selectors/should-rate-limit-atomic-cache-clear';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { EdgeCacheLoadingPlaceholder } from './edge-cache-loading-placeholder';
+import { useClearEdgeCacheMutation } from './use-clear-edge-cache';
+import { useEdgeCacheQuery } from './use-edge-cache';
+import { useToggleEdgeCacheMutation } from './use-toggle-edge-cache';
 
-const CacheCard = ( {
+const Hr = styled.hr( {
+	marginBottom: '16px',
+	marginTop: '16px',
+	width: '100%',
+	color: '#e0e0e0',
+} );
+
+const EdgeCacheDescription = styled.p( {
+	fontSize: '16px',
+	marginBottom: '16px',
+} );
+
+const EdgeCacheNotice = styled.p( {
+	color: '#646970',
+	marginTop: '18px',
+} );
+
+const CardBody = styled.div( {
+	display: 'flex',
+	flexDirection: 'column',
+} );
+
+const ToggleContainer = styled.div( {
+	fontSize: '14px',
+	label: {
+		fontSize: '14px',
+	},
+} );
+
+const ToggleLabel = styled.p( {
+	marginBottom: '9px',
+	fontWeight: 600,
+	fontSize: '14px',
+} );
+
+export const CacheCard = ( {
 	disabled,
 	shouldRateLimitCacheClear,
 	clearAtomicWordPressCache,
-	isClearingCache,
+	isClearingWordpressCache,
 	siteId,
 	translate,
 } ) => {
+	const dispatch = useDispatch();
+	const showEdgeCache = config.isEnabled( 'yolo/edge-cache-i1' );
+	const {
+		isLoading: getEdgeCacheLoading,
+		data: isEdgeCacheActive,
+		isInitialLoading: getEdgeCacheInitialLoading,
+	} = useEdgeCacheQuery( siteId, {
+		enabled: showEdgeCache,
+	} );
+
+	const { toggleEdgeCache, isLoading: toggleEdgeCacheLoading } = useToggleEdgeCacheMutation(
+		siteId,
+		{
+			onSettled: ( ...args ) => {
+				const active = args[ 2 ];
+				dispatch(
+					recordTracksEvent(
+						active
+							? 'calypso_hosting_configuration_edge_cache_enable'
+							: 'calypso_hosting_configuration_edge_cache_disable',
+						{
+							site_id: siteId,
+						}
+					)
+				);
+			},
+		}
+	);
+	const { clearEdgeCache, isLoading: clearEdgeCacheLoading } = useClearEdgeCacheMutation( siteId, {
+		onSuccess: () => {
+			dispatch(
+				recordTracksEvent( 'calypso_hosting_configuration_clear_wordpress_cache', {
+					site_id: siteId,
+				} )
+			);
+		},
+	} );
+
+	const isClearingCache = isClearingWordpressCache || clearEdgeCacheLoading;
+
 	const clearCache = () => {
+		if ( isEdgeCacheActive ) {
+			clearEdgeCache();
+		}
 		clearAtomicWordPressCache( siteId, 'Manually clearing again.' );
 	};
 
 	const getClearCacheContent = () => {
 		return (
 			<div>
-				<p>
-					{ translate(
-						'Be careful, clearing the cache may make your site unresponsive while it is being rebuilt. {{a}}Learn more{{/a}}.',
-						{
-							components: {
-								a: <InlineSupportLink supportContext="hosting-clear-cache" showIcon={ false } />,
-							},
-						}
-					) }
-				</p>
 				<Button
 					primary
 					onClick={ clearCache }
 					busy={ isClearingCache }
-					disabled={ disabled || isClearingCache || shouldRateLimitCacheClear }
+					disabled={
+						disabled ||
+						isClearingCache ||
+						shouldRateLimitCacheClear ||
+						getEdgeCacheLoading ||
+						toggleEdgeCacheLoading
+					}
 				>
 					<span>{ translate( 'Clear cache' ) }</span>
 				</Button>
+				<EdgeCacheNotice>
+					{ translate(
+						'Be careful, clearing the cache may make your site less responsive while it is being rebuilt.'
+					) }
+				</EdgeCacheNotice>
 				{ shouldRateLimitCacheClear && (
-					<p class="form-setting-explanation">
+					<p className="form-setting-explanation">
 						{ translate( 'You cleared the cache recently. Please wait a minute and try again.' ) }
 					</p>
 				) }
@@ -54,8 +142,39 @@ const CacheCard = ( {
 	return (
 		<Card className="cache-card">
 			<MaterialIcon icon="autorenew" size={ 32 } />
-			<CardHeading id="cache">{ translate( 'Cache' ) }</CardHeading>
-			{ getClearCacheContent() }
+			<CardHeading id="cache" size={ 20 }>
+				{ translate( 'Cache' ) }
+			</CardHeading>
+			<CardBody>
+				<EdgeCacheDescription>
+					{ translate( 'Manage your site’s server-side caching. {{a}}Learn more{{/a}}.', {
+						components: {
+							a: <InlineSupportLink supportContext="hosting-clear-cache" showIcon={ false } />,
+						},
+					} ) }
+				</EdgeCacheDescription>
+				{ showEdgeCache && (
+					<>
+						<ToggleContainer>
+							{ getEdgeCacheInitialLoading ? (
+								<EdgeCacheLoadingPlaceholder />
+							) : (
+								<>
+									<ToggleLabel>{ translate( 'Edge cache' ) }</ToggleLabel>
+									<ToggleControl
+										disabled={ clearEdgeCacheLoading || getEdgeCacheLoading }
+										checked={ isEdgeCacheActive }
+										onChange={ toggleEdgeCache }
+										label={ translate( 'Enable edge caching for faster content delivery' ) }
+									/>
+									<Hr />
+								</>
+							) }
+						</ToggleContainer>
+					</>
+				) }
+				{ getClearCacheContent() }
+			</CardBody>
 		</Card>
 	);
 };
@@ -66,7 +185,8 @@ export default connect(
 
 		return {
 			shouldRateLimitCacheClear: shouldRateLimitAtomicCacheClear( state, siteId ),
-			isClearingCache: getRequest( state, clearWordPressCache( siteId ) )?.isLoading ?? false,
+			isClearingWordpressCache:
+				getRequest( state, clearWordPressCache( siteId ) )?.isLoading ?? false,
 			siteId,
 		};
 	},
