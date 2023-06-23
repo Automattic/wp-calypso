1cea6fbc617fb44e632a5778294f76c810cc7142
Yan Sern <yansern@yan.sr>
2023-06-23 14:58:20 +0300

    Comment out tracks for other PRs.

--- client/landing/subscriptions/tracks/index.ts
+++ client/landing/subscriptions/tracks/index.ts
@@ -1,6 +1,6 @@
 export { default as useRecordSubscriptionsTracksEvent } from './use-record-subscriptions-tracks-event';
-export { default as useRecordSiteSubscribed } from './use-record-site-subscribed';
-export { default as useRecordSiteUnsubscribed } from './use-record-site-unsubscribed';
+// export { default as useRecordSiteSubscribed } from './use-record-site-subscribed';
+// export { default as useRecordSiteUnsubscribed } from './use-record-site-unsubscribed';
 export { default as useRecordSiteIconClicked } from './use-record-site-icon-clicked';
 export { default as useRecordSiteTitleClicked } from './use-record-site-title-clicked';
 export { default as useRecordSiteUrlClicked } from './use-record-site-url-clicked';
@@ -8,6 +8,6 @@ export { default as useRecordCommentEmailsToggle } from './use-record-comment-em
 export { default as useRecordNotificationsToggle } from './use-record-notifications-toggle';
 export { default as useRecordPostEmailsSetFrequency } from './use-record-post-emails-set-frequency';
 export { default as useRecordPostEmailsToggle } from './use-record-post-emails-toggle';
-export { default as useRecordRecommendedSiteSubscribed } from './use-record-recommended-site-subscribed';
-export { default as useRecordRecommendedSiteUnsubscribed } from './use-record-recommended-site-unsubscribed';
-export { default as useRecordRecommendedSiteDismissed } from './use-record-recommended-site-dismissed';
+// export { default as useRecordRecommendedSiteSubscribed } from './use-record-recommended-site-subscribed';
+// export { default as useRecordRecommendedSiteUnsubscribed } from './use-record-recommended-site-unsubscribed';
+// export { default as useRecordRecommendedSiteDismissed } from './use-record-recommended-site-dismissed';
