5244cb0aa889f3b7a44af59f0f97e8e765833910
Omar Alshaker <omar@omaralshaker.com>
2023-06-24 23:27:56 +0200

    Avoid checking more than needed

--- client/landing/stepper/declarative-flow/internals/steps-repository/bulk-domain-transfer-domains/domain-code-pair.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/bulk-domain-transfer-domains/domain-code-pair.tsx
@@ -1,115 +1,13 @@
 import { FormInputValidation } from '@automattic/components';
-import { useIsDomainsUnlocked, useIsDomainCodeValid } from '@automattic/data-stores';
-import { doesStringResembleDomain } from '@automattic/onboarding';
 import { Button } from '@wordpress/components';
 import { trash } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
 import { useEffect } from 'react';
-import { useDebounce } from 'use-debounce';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormExplanation from 'calypso/components/forms/form-setting-explanation';
 import FormInput from 'calypso/components/forms/form-text-input';
-
-function useValidationMessage( domain: string, auth: string ) {
-	const { __ } = useI18n();
-
-	const [ domainDebounced ] = useDebounce( domain, 500 );
-	const [ authDebounced ] = useDebounce( auth, 500 );
-
-	const hasGoodDomain = doesStringResembleDomain( domainDebounced );
-	const hasGoodAuthCode = hasGoodDomain && authDebounced.trim().length > 0;
-
-	const passedLocalValidation = hasGoodDomain && hasGoodAuthCode;
-
-	const { data: isDomainUnlocked, isInitialLoading: isLoadingLock } = useIsDomainsUnlocked(
-		domainDebounced,
-		{
-			enabled: passedLocalValidation,
-		}
-	);
-
-	const { data: isDomainCodeValid, isInitialLoading: isLoadingCode } = useIsDomainCodeValid(
-		{
-			domain: domainDebounced,
-			auth: authDebounced,
-		},
-		{
-			enabled: passedLocalValidation && isDomainUnlocked?.unlocked,
-		}
-	);
-
-	if ( ! hasGoodDomain ) {
-		return {
-			valid: false,
-			loading: false,
-			message: __( 'Please enter a valid domain name.' ),
-		};
-	}
-
-	if ( ! hasGoodAuthCode ) {
-		return {
-			valid: false,
-			loading: false,
-			message: __( 'Please enter a valid auth code.' ),
-		};
-	}
-
-	// local validation passed, but we're still loading
-	if ( isLoadingLock ) {
-		return {
-			valid: false,
-			loading: true,
-			message: __( 'Checking domain lock status.' ),
-		};
-	}
-
-	if ( isLoadingCode ) {
-		return {
-			valid: false,
-			loading: true,
-			message: __( 'Checking domain authentication code.' ),
-		};
-	}
-
-	// final success
-	if ( isDomainCodeValid?.success ) {
-		return {
-			valid: true,
-			loading: false,
-			message: __( 'This domain is unlocked and ready to be transferred.' ),
-		};
-	}
-
-	// partial success
-	if ( isDomainUnlocked?.unlocked ) {
-		return {
-			valid: false,
-			loading: false,
-			message: __( 'This domain is unlocked but the authentication code seems incorrect.' ),
-		};
-	} else if ( isDomainUnlocked?.unlocked === null ) {
-		return {
-			valid: false,
-			loading: false,
-			message: __( 'This domain does not seem to be registered.' ),
-		};
-	} else if ( isDomainUnlocked?.unlocked === false ) {
-		return {
-			valid: false,
-			loading: false,
-			message: __( 'This domain does not seem to be unlocked.' ),
-		};
-	}
-
-	return {
-		valid: false,
-		loading: false,
-		message: __(
-			'An unknown error occurred while checking the domain transferability. Please try again or contact support'
-		),
-	};
-}
+import { useValidationMessage } from './use-validation-message';
 
 type Props = {
 	id: string;
