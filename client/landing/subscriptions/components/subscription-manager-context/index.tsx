7a7b305cee616308ce6597d4bb01c2fbb142e640
Yan Sern <yansern@yan.sr>
2023-06-24 14:40:18 +0300

    Add helper props isSubscriptionsPortal & isReaderPortal.

--- client/landing/subscriptions/components/subscription-manager-context/index.tsx
+++ client/landing/subscriptions/components/subscription-manager-context/index.tsx
@@ -6,6 +6,8 @@ export type Portal = typeof SubscriptionsPortal | typeof ReaderPortal;
 
 export interface SubscriptionManagerContext {
 	portal: Portal;
+	isSubscriptionsPortal: boolean;
+	isReaderPortal: boolean;
 }
 
 const SubscriptionManagerContext = createContext< SubscriptionManagerContext >(
@@ -15,8 +17,13 @@ const SubscriptionManagerContext = createContext< SubscriptionManagerContext >(
 export const SubscriptionManagerContextProvider: React.FunctionComponent<
 	SubscriptionManagerContext
 > = ( { children, ...context } ) => {
+	const helpers = {
+		isSubscriptionsPortal: context.portal === SubscriptionsPortal,
+		isReaderPortal: context.portal === ReaderPortal,
+	};
+
 	return (
-		<SubscriptionManagerContext.Provider value={ context }>
+		<SubscriptionManagerContext.Provider value={ { ...context, ...helpers } }>
 			{ children }
 		</SubscriptionManagerContext.Provider>
 	);
