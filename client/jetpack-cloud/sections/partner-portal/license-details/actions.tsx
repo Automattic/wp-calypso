ee5d4d02c95861875b33c1a804aaaa9d20c85cc8
Atanas Angelov <hi@atanas.dev>
2023-06-27 14:08:53 +0300

    Jetpack Pro Dashboard: Remove the ability to detach licenses

--- client/jetpack-cloud/sections/partner-portal/license-details/actions.tsx
+++ client/jetpack-cloud/sections/partner-portal/license-details/actions.tsx
@@ -4,7 +4,6 @@ import { useCallback, useState } from 'react';
 import useLicenseDownloadUrlMutation from 'calypso/components/data/query-jetpack-partner-portal-licenses/use-license-download-url-mutation';
 import RevokeLicenseDialog from 'calypso/jetpack-cloud/sections/partner-portal/revoke-license-dialog';
 import { LicenseState, LicenseType } from 'calypso/jetpack-cloud/sections/partner-portal/types';
-import UnassignLicenseDialog from 'calypso/jetpack-cloud/sections/partner-portal/unassign-license-dialog';
 import { addQueryArgs } from 'calypso/lib/url';
 import { useDispatch } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
@@ -30,7 +29,6 @@ export default function LicenseDetailsActions( {
 	const dispatch = useDispatch();
 	const translate = useTranslate();
 	const [ revokeDialog, setRevokeDialog ] = useState( false );
-	const [ unassignDialog, setUnassignDialog ] = useState( false );
 	const debugUrl = siteUrl ? `https://jptools.wordpress.com/debug/?url=${ siteUrl }` : null;
 	const downloadUrl = useLicenseDownloadUrlMutation( licenseKey );
 
@@ -43,16 +41,6 @@ export default function LicenseDetailsActions( {
 		setRevokeDialog( false );
 		dispatch( recordTracksEvent( 'calypso_partner_portal_license_details_revoke_dialog_close' ) );
 	}, [ dispatch, setRevokeDialog ] );
-
-	const openUnassignDialog = useCallback( () => {
-		setUnassignDialog( true );
-		dispatch( recordTracksEvent( 'calypso_partner_portal_license_details_unassign_dialog_open' ) );
-	}, [ dispatch, setUnassignDialog ] );
-
-	const closeUnassignDialog = useCallback( () => {
-		setUnassignDialog( false );
-		dispatch( recordTracksEvent( 'calypso_partner_portal_license_details_unassign_dialog_close' ) );
-	}, [ dispatch, setUnassignDialog ] );
 
 	const download = useCallback( () => {
 		downloadUrl.mutate( null, {
@@ -92,13 +80,7 @@ export default function LicenseDetailsActions( {
 				</Button>
 			) }
 
-			{ licenseState === LicenseState.Attached && licenseType === LicenseType.Partner && (
-				<Button compact onClick={ openUnassignDialog }>
-					{ translate( 'Unassign' ) }
-				</Button>
-			) }
-
-			{ licenseState === LicenseState.Detached && licenseType === LicenseType.Partner && (
+			{ licenseState !== LicenseState.Revoked && licenseType === LicenseType.Partner && (
 				<Button compact onClick={ openRevokeDialog } scary>
 					{ translate( 'Revoke' ) }
 				</Button>
@@ -121,15 +103,6 @@ export default function LicenseDetailsActions( {
 					product={ product }
 					siteUrl={ siteUrl }
 					onClose={ closeRevokeDialog }
-				/>
-			) }
-
-			{ unassignDialog && (
-				<UnassignLicenseDialog
-					licenseKey={ licenseKey }
-					product={ product }
-					siteUrl={ siteUrl }
-					onClose={ closeUnassignDialog }
 				/>
 			) }
 		</div>
