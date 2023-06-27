b04623bcc05621c75fce434ddbbb1d2b95dd0dbf
Eru Penkman <roo2@protonmail.com>
2023-06-27 15:35:24 +1000

    use setlocale middleware on search page

--- client/reader/search/index.js
+++ client/reader/search/index.js
@@ -1,5 +1,7 @@
+import { getLanguageRouteParam } from '@automattic/i18n-utils';
 import page from 'page';
 import { makeLayout, render as clientRender } from 'calypso/controller';
+import { setLocaleMiddleware } from 'calypso/controller/shared';
 import { sidebar, updateLastRoute } from 'calypso/reader/controller';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import { fetchTrendingTags } from '../tags/controller';
@@ -13,11 +15,13 @@ const fetchTrendingTagsIfLoggedOut = ( context, next ) => {
 };
 
 export default function () {
+	const langParam = getLanguageRouteParam();
 	// Old recommendations page
 	page( '/recommendations', '/read/search' );
 
 	page(
-		'/read/search',
+		[ '/read/search', `/${ langParam }/read/search` ],
+		setLocaleMiddleware(),
 		fetchTrendingTagsIfLoggedOut,
 		updateLastRoute,
 		sidebar,
