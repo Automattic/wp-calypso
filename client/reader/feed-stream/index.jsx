40abdedfdc404de3934a22dc13e477d730b0d2f9
Grant Kinney <grant.kinney@automattic.com>
2023-06-27 14:06:17 -0500

    Reader: updates feed stream to use functional component

--- client/reader/feed-stream/index.jsx
+++ client/reader/feed-stream/index.jsx
@@ -1,7 +1,4 @@
-import { localize } from 'i18n-calypso';
-import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useTranslate } from 'i18n-calypso';
 import ReaderFeedHeader from 'calypso/blocks/reader-feed-header';
 import DocumentHead from 'calypso/components/data/document-head';
 import QueryReaderFeed from 'calypso/components/data/query-reader-feed';
@@ -10,6 +7,7 @@ import FeedError from 'calypso/reader/feed-error';
 import { getSiteName } from 'calypso/reader/get-helpers';
 import SiteBlocked from 'calypso/reader/site-blocked';
 import Stream from 'calypso/reader/stream';
+import { useSelector } from 'calypso/state';
 import { getFeed } from 'calypso/state/reader/feeds/selectors';
 import { getReaderFollowForFeed } from 'calypso/state/reader/follows/selectors';
 import { isSiteBlocked } from 'calypso/state/reader/site-blocks/selectors';
@@ -19,78 +17,66 @@ import EmptyContent from './empty';
 // If the blog_ID of a reader feed is 0, that means no site exists for it.
 const getReaderSiteId = ( feed ) => ( feed && feed.blog_ID === 0 ? null : feed && feed.blog_ID );
 
-class FeedStream extends Component {
-	static propTypes = {
-		feedId: PropTypes.number.isRequired,
-		className: PropTypes.string,
-		showBack: PropTypes.bool,
-	};
+export default function FeedStream( props ) {
+	const { feedId, className = 'is-site-stream', showBack = true } = props;
+	const translate = useTranslate();
 
-	static defaultProps = {
-		showBack: true,
-		className: 'is-site-stream',
-	};
-	constructor( props ) {
-		super( props );
-		this.title = props.translate( 'Loading Feed' );
-	}
-	render() {
-		const { feed, site, siteId, isBlocked } = this.props;
+	const feed = useSelector( ( state ) => {
+		const _feed = getFeed( state, feedId );
 
-		const emptyContent = <EmptyContent />;
-		const title = getSiteName( { feed, site } ) || this.title;
-
-		if ( isBlocked ) {
-			return <SiteBlocked title={ title } siteId={ siteId } />;
+		if ( _feed ) {
+			_feed.site_icon = getReaderFollowForFeed( state, parseInt( feedId ) )?.site_icon;
 		}
 
-		if ( ( feed && feed.is_error ) || ( site && site.is_error ) ) {
-			return <FeedError sidebarTitle={ title } />;
-		}
+		return _feed;
+	} );
 
-		return (
-			<Stream
-				{ ...this.props }
-				listName={ title }
-				emptyContent={ emptyContent }
-				showPostHeader={ false }
-				showSiteNameOnCards={ false }
-				useCompactCards={ true }
-				showFollowButton={ false }
-			>
-				<DocumentHead
-					title={ this.props.translate( '%s ‹ Reader', {
-						args: title,
-						comment: '%s is the section name. For example: "My Likes"',
-					} ) }
-				/>
-				<ReaderFeedHeader
-					feed={ feed }
-					site={ site }
-					showBack={ this.props.showBack }
-					streamKey={ this.props.streamKey }
-				/>
-				{ ! feed && <QueryReaderFeed feedId={ this.props.feedId } /> }
-				{ siteId && <QueryReaderSite siteId={ siteId } /> }
-			</Stream>
-		);
+	const { isBlocked, site, siteId } = useSelector( ( state ) => {
+		const _siteId = getReaderSiteId( feed );
+
+		return {
+			isBlocked: _siteId && isSiteBlocked( state, _siteId ),
+			site: _siteId && getSite( state, _siteId ),
+			siteId: _siteId,
+		};
+	} );
+
+	const emptyContent = <EmptyContent />;
+	const title = getSiteName( { feed, site } ) || translate( 'Loading Feed' );
+
+	if ( isBlocked ) {
+		return <SiteBlocked title={ title } siteId={ siteId } />;
 	}
+
+	if ( ( feed && feed.is_error ) || ( site && site.is_error ) ) {
+		return <FeedError sidebarTitle={ title } />;
+	}
+
+	return (
+		<Stream
+			{ ...props }
+			className={ className }
+			listName={ title }
+			emptyContent={ emptyContent }
+			showPostHeader={ false }
+			showSiteNameOnCards={ false }
+			useCompactCards={ true }
+			showFollowButton={ false }
+		>
+			<DocumentHead
+				title={ translate( '%s ‹ Reader', {
+					args: title,
+					comment: '%s is the section name. For example: "My Likes"',
+				} ) }
+			/>
+			<ReaderFeedHeader
+				feed={ feed }
+				site={ site }
+				showBack={ showBack }
+				streamKey={ props.streamKey }
+			/>
+			{ ! feed && <QueryReaderFeed feedId={ feedId } /> }
+			{ siteId && <QueryReaderSite siteId={ siteId } /> }
+		</Stream>
+	);
 }
-
-export default connect( ( state, ownProps ) => {
-	const feed = getFeed( state, ownProps.feedId );
-	const siteId = getReaderSiteId( feed );
-
-	// Add site icon to feed object so have icon for external feeds
-	if ( feed ) {
-		const follow = getReaderFollowForFeed( state, parseInt( ownProps.feedId ) );
-		feed.site_icon = follow?.site_icon;
-	}
-
-	return {
-		feed,
-		siteId,
-		site: siteId && getSite( state, siteId ),
-		isBlocked: isSiteBlocked( state, siteId ),
-	};
-} )( localize( FeedStream ) );
