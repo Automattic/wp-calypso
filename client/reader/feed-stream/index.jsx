041f1738fc6b40cb56290dbec4417ef411252706
Grant Kinney <grant.kinney@automattic.com>
2023-06-28 18:31:32 -0500

    Reader: add sidebar to feed stream

--- client/reader/feed-stream/index.jsx
+++ client/reader/feed-stream/index.jsx
@@ -1,21 +1,37 @@
 import { useTranslate } from 'i18n-calypso';
 import ReaderFeedHeader from 'calypso/blocks/reader-feed-header';
 import DocumentHead from 'calypso/components/data/document-head';
+import QueryPostCounts from 'calypso/components/data/query-post-counts';
 import QueryReaderFeed from 'calypso/components/data/query-reader-feed';
 import QueryReaderSite from 'calypso/components/data/query-reader-site';
+import { useSiteTags } from 'calypso/data/site-tags/use-site-tags';
 import FeedError from 'calypso/reader/feed-error';
 import { getSiteName } from 'calypso/reader/get-helpers';
 import SiteBlocked from 'calypso/reader/site-blocked';
 import Stream from 'calypso/reader/stream';
 import { useSelector } from 'calypso/state';
+import { getAllPostCount } from 'calypso/state/posts/counts/selectors';
 import { getFeed } from 'calypso/state/reader/feeds/selectors';
 import { getReaderFollowForFeed } from 'calypso/state/reader/follows/selectors';
 import { isSiteBlocked } from 'calypso/state/reader/site-blocks/selectors';
 import { getSite } from 'calypso/state/reader/sites/selectors';
 import EmptyContent from './empty';
+import FeedStreamSidebar from './sidebar';
 
 // If the blog_ID of a reader feed is 0, that means no site exists for it.
 const getReaderSiteId = ( feed ) => ( feed && feed.blog_ID === 0 ? null : feed && feed.blog_ID );
+
+const getFollowerCount = ( feed, site ) => {
+	if ( site && site.subscribers_count ) {
+		return site.subscribers_count;
+	}
+
+	if ( feed && feed.subscribers_count > 0 ) {
+		return feed.subscribers_count;
+	}
+
+	return null;
+};
 
 export default function FeedStream( props ) {
 	const { feedId, className = 'is-site-stream', showBack = true } = props;
@@ -32,18 +48,21 @@ export default function FeedStream( props ) {
 		return _feed;
 	} );
 
-	const { isBlocked, site, siteId } = useSelector( ( state ) => {
+	const { isBlocked, postCount, site, siteId } = useSelector( ( state ) => {
 		const _siteId = getReaderSiteId( feed );
 
 		return {
 			isBlocked: _siteId && isSiteBlocked( state, _siteId ),
+			postCount: _siteId && getAllPostCount( state, _siteId, 'post', 'publish' ),
 			site: _siteId && getSite( state, _siteId ),
 			siteId: _siteId,
 		};
 	} );
 
+	const siteTags = useSiteTags( siteId );
 	const emptyContent = <EmptyContent />;
 	const title = getSiteName( { feed, site } ) || translate( 'Loading Feed' );
+	const followerCount = getFollowerCount( feed, site );
 
 	if ( isBlocked ) {
 		return <SiteBlocked title={ title } siteId={ siteId } />;
@@ -53,16 +72,25 @@ export default function FeedStream( props ) {
 		return <FeedError sidebarTitle={ title } />;
 	}
 
+	const streamSidebar = (
+		<FeedStreamSidebar
+			followerCount={ followerCount }
+			postCount={ postCount }
+			tags={ siteTags.data }
+		/>
+	);
+
 	return (
 		<Stream
 			{ ...props }
 			className={ className }
 			listName={ title }
 			emptyContent={ emptyContent }
+			showFollowButton={ false }
 			showPostHeader={ false }
 			showSiteNameOnCards={ false }
+			streamSidebar={ streamSidebar }
 			useCompactCards={ true }
-			showFollowButton={ false }
 		>
 			<DocumentHead
 				title={ translate( '%s ‹ Reader', {
@@ -76,6 +104,7 @@ export default function FeedStream( props ) {
 				showBack={ showBack }
 				streamKey={ props.streamKey }
 			/>
+			{ siteId && <QueryPostCounts siteId={ siteId } type="post" /> }
 			{ ! feed && <QueryReaderFeed feedId={ feedId } /> }
 			{ siteId && <QueryReaderSite siteId={ siteId } /> }
 		</Stream>
