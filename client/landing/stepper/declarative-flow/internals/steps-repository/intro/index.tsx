f0212b9acd8832ca72d1a24726998b92560a450f
edanzer <edanzer@gmail.com>
2023-06-28 14:14:01 -0600

    Fix typos

--- client/landing/stepper/declarative-flow/internals/steps-repository/intro/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/intro/index.tsx
@@ -88,7 +88,7 @@ const useIntroContent = ( flowName: string | null ): IntroContent => {
 			return {
 				title: __( 'Write. Grow. Earn. This is Newsletter.' ),
 				text: __(
-					'Unlimitd subscribers. Beauiful design. And everything you need to grow your audience. Powered by WordPress.com'
+					'Unlimited subscribers. Beautiful design. And everything you need to grow your audience. Powered by WordPress.com'
 				),
 				buttonText: __( 'Launch my newsletter' ),
 			};
