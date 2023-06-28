358ffdb4d6547b8a79e8c8cc5435861cf4f2324c
Juan Lucha <juanlucha@gmail.com>
2023-06-28 11:17:42 +0200

    Subscribers Page: tests for SubscriberList and SubscriberRow (#78700)

--- client/my-sites/subscribers/components/subscriber-profile/test/subscriber-profile.test.tsx
+++ client/my-sites/subscribers/components/subscriber-profile/test/subscriber-profile.test.tsx
@@ -3,7 +3,7 @@
  */
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { SubscriberProfile } from '../../components/subscriber-profile';
+import SubscriberProfile from '../subscriber-profile';
 
 const testName = 'Test Name';
 const testEmail = 'test@test.com';
