4d6faba2ddc84e8bad4da40aabbf57839609a8a3
Jasper Kang <kangzjnet@gmail.com>
2023-06-29 16:19:28 +1200

    remove unused code

--- client/state/sites/selectors/has-site-product-jetpack-stats-paid.ts
+++ client/state/sites/selectors/has-site-product-jetpack-stats-paid.ts
@@ -1,10 +1,10 @@
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
-import { hasJetpackStatsPurchase } from './has-jetpack-stats-purchase';
 import type { AppState } from 'calypso/types';
+import hasSiteProductJetpackStats from './has-site-product-jetpack-stats';
 
 export const hasPaidJetpackStats = (
 	state: AppState,
 	siteId = getSelectedSiteId( state )
 ): boolean => {
-	return hasJetpackStatsPurchase( state, true, siteId );
+	return hasSiteProductJetpackStats( state, true, siteId );
 };
---
+++ client/state/sites/selectors/has-site-product-jetpack-stats.ts
@@ -0,0 +1,20 @@
+import { productHasStats } from 'calypso/blocks/jetpack-benefits/feature-checks';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { AppState } from 'calypso/types';
+import getSitePlan from './get-site-plan';
+import getSiteProducts from './get-site-products';
+
+const hasSiteProductJetpackStats = (
+	state: AppState,
+	onlyPaid = false,
+	siteId = getSelectedSiteId( state )
+): boolean => {
+	const siteHasStatsProduct = getSiteProducts( state, siteId )?.some( ( purchase ) =>
+		productHasStats( purchase?.productSlug, onlyPaid )
+	);
+	const sitePlan = getSitePlan( state, siteId );
+	const siteHasStatsPlan = productHasStats( sitePlan?.product_slug as string, onlyPaid );
+	return siteHasStatsPlan || !! siteHasStatsProduct;
+};
+
+export default hasSiteProductJetpackStats;
