7973604542eb147ff1852b860c75c0e1c1ef8954
Noah Allen <noahtallen@gmail.com>
2023-06-28 18:12:12 -0700

    Delete several @wordpress module declarations that duplicate bundled types

--- packages/whats-new/types.d.ts
+++ packages/whats-new/types.d.ts
@@ -3,32 +3,6 @@ interface HTMLElement {
 	role: string;
 }
 
-declare module '@wordpress/notices' {
-	export type Status = 'error' | 'info' | 'success' | 'warning';
-	export const store: string;
-}
-
-declare module '@wordpress/rich-text' {
-	interface Format {
-		type: string;
-		attributes?: Record< string, string > | undefined;
-		unregisteredAttributes?: Record< string, string > | undefined;
-		object?: boolean | undefined;
-	}
-
-	export interface Value {
-		activeFormats?: readonly Format[] | undefined;
-		end?: number | undefined;
-		formats: ReadonlyArray< Format[] | undefined >;
-		replacements: ReadonlyArray< Format[] | undefined >;
-		start?: number | undefined;
-		text: string;
-	}
-
-	// Fixed node_modules/@wordpress/components/build-types/autocomplete/types.d.ts(6,15): error TS2305: Module '"@wordpress/rich-text"' has no exported member 'RichTextValue'.
-	export type RichTextValue = Record< string, unknown >;
-}
-
 declare module '@automattic/whats-new' {
 	type FunctionProps = {
 		onClose: () => void;
