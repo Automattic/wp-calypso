10fdf2a04a4f0bd8c1a853307a04a38391f591b1
Noah Allen <noahtallen@gmail.com>
2023-06-28 18:42:19 -0700

    Add back html element hotfix

--- packages/whats-new/types.d.ts
+++ packages/whats-new/types.d.ts
@@ -1,3 +1,8 @@
+// TODO: Attempt removing after https://github.com/Automattic/wp-calypso/pull/78711
+// Fixed node_modules/@wordpress/components/build-types/dropdown-menu/types.d.ts(43,24): error TS2339: Property 'role' does not exist on type 'HTMLElement'.
+interface HTMLElement {
+	role: string;
+}
 declare module '@automattic/whats-new' {
 	type FunctionProps = {
 		onClose: () => void;
