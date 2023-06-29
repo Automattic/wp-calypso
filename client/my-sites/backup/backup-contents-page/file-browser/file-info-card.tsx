1bc75ae4c7868265738c28972ec571df92450145
Rafael Agostini <rafael.agostini@automattic.com>
2023-06-29 13:21:56 -0500

    Add download and preview files actions

--- client/my-sites/backup/backup-contents-page/file-browser/file-info-card.tsx
+++ client/my-sites/backup/backup-contents-page/file-browser/file-info-card.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useCallback, useState } from 'react';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import wp from 'calypso/lib/wp';
 import { FileBrowserItem } from './types';
 import { useBackupPathInfoQuery } from './use-backup-path-info-query';
 import { convertBytes } from './util';
+
 interface FileInfoCardProps {
 	siteId: number;
 	item: FileBrowserItem;
@@ -26,6 +29,47 @@ const FileInfoCard: FunctionComponent< FileInfoCardProps > = ( { siteId, item } 
 
 	const modifiedTime = fileInfo?.mtime ? moment.unix( fileInfo.mtime ).format( 'lll' ) : null;
 	const size = fileInfo?.size ? convertBytes( fileInfo.size ) : null;
+
+	const [ fileContent, setFileContent ] = useState( '' );
+
+	const downloadFile = useCallback( () => {
+		const manifestPath = window.btoa( item.manifestPath ?? '' );
+
+		wp.req
+			.get( {
+				path: `/sites/${ siteId }/rewind/backup/${ item.period }/file/${ manifestPath }/url`,
+				apiNamespace: 'wpcom/v2',
+			} )
+			.then( ( response: { url: string } ) => {
+				const downloadUrl = new URL( response.url );
+				downloadUrl.searchParams.append( 'disposition', 'attachment' );
+				window.open( downloadUrl, '_blank' );
+			} );
+	}, [ siteId, item ] );
+
+	const previewFile = () => {
+		const manifestPath = window.btoa( item.manifestPath ?? '' );
+
+		//Get the download URL
+		wp.req
+			.get( {
+				path: `/sites/${ siteId }/rewind/backup/${ item.period }/file/${ manifestPath }/url`,
+				apiNamespace: 'wpcom/v2',
+			} )
+			.then( ( response: { url: string } ) => {
+				const downloadUrl = response.url.replace( 'https://public-api.wordpress.com/wpcom/v2', '' );
+
+				// Get the file content
+				wp.req
+					.get( {
+						path: downloadUrl,
+						apiNamespace: 'wpcom/v2',
+					} )
+					.then( ( response: any ) => {
+						setFileContent( response );
+					} );
+			} );
+	};
 
 	// Do not display file info if the item hasChildren (it could be a directory, plugins, themes, etc.)
 	if ( item.hasChildren ) {
@@ -87,7 +131,21 @@ const FileInfoCard: FunctionComponent< FileInfoCardProps > = ( { siteId, item } 
 					</div>
 				) }
 			</div>
-			<div className="file-card__actions"></div>
+			<div className="file-card__actions">
+				<Button
+					variant="secondary"
+					className="file-card__action"
+					onClick={ downloadFile }
+					text="Download"
+				/>
+				<Button
+					variant="secondary"
+					className="file-card__action"
+					onClick={ previewFile }
+					text="Preview file"
+				/>
+			</div>
+			<div className="file-card__preview">{ fileContent }</div>
 		</div>
 	);
 };
