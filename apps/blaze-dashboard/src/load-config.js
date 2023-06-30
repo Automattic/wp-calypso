70949b8a24f42a187be437e9430a65891abc3e4b
Sebastián Barbosa <sebabarbosa@gmail.com>
2023-06-30 15:34:37 -0300

    Add tracks to the Blaze Dashboard app (#78807)
    * Add tracks to the Blaze Dashboard app
    
    * Fixes a typo in a comment

--- apps/blaze-dashboard/src/load-config.js
+++ apps/blaze-dashboard/src/load-config.js
@@ -16,9 +16,14 @@ productionConfig.features[ 'promote-post/widget-i2' ] = true;
 // Sets the advertising path prefix for this app
 productionConfig.advertising_dashboard_path_prefix = '/advertising';
 
-// Note: configData is hydrated in Jetpack: projects/packages/blaze/src/class-dashboard-config-data.php - method `get_data`
-// TODO: link to Github when code in Jetpack is merged
+// Note: configData is hydrated in Jetpack: https://github.com/Automattic/jetpack/blob/60b0dac0dc5ad7edec2b86edb57b65a3a98ec42d/projects/packages/blaze/src/class-dashboard-config-data.php#L31
 window.configData = {
 	...window.configData,
 	...productionConfig,
 };
+
+// Fixes a typo in Jetpack config response
+if ( window.configData.intial_state ) {
+	window.configData.initial_state = window.configData.intial_state;
+	delete window.configData.intial_state;
+}
