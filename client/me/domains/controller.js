088a0998126b4653f0996330e73584d86ec5c853
Jeremy Yip <jeremy.yip@automattic.com>
2023-06-29 17:52:55 -0700

    Add all domains page to me/domains

--- client/me/domains/controller.js
+++ client/me/domains/controller.js
@@ -1,7 +1,9 @@
-import { createElement } from 'react';
-import DomainsComponent from 'calypso/me/domains/main';
+import DomainManagementData from 'calypso/components/data/domain-management';
+import DomainManagement from 'calypso/my-sites/domains/domain-management/index.jsx';
 
 export function domains( context, next ) {
-	context.primary = createElement( DomainsComponent );
+	context.primary = (
+		<DomainManagementData component={ DomainManagement.AllDomains } context={ context } />
+	);
 	next();
 }
