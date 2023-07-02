d753341448318c8b7a5d1fa5f568cd3b8b46a9be
Daniel López <daniel.lopez@automattic.com>
2023-07-02 13:20:16 +0200

    Import type correcly

--- client/odysseus/context/index.tsx
+++ client/odysseus/context/index.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
+import type { ReactNode } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 const noop = () => {};
