8dcc18d96ac514159e7f1dddd5bb51bf42132ce2
Eru Penkman <roo2@protonmail.com>
2023-07-03 11:29:12 +1000

    reader add signup button to tags page

--- client/reader/stream/reader-tag-sidebar/index.jsx
+++ client/reader/stream/reader-tag-sidebar/index.jsx
@@ -1,9 +1,12 @@
+import { getUrlParts } from '@automattic/calypso-url';
+import { Button } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { useDispatch } from 'react-redux';
 import TagLink from 'calypso/blocks/reader-post-card/tag-link';
 import { useRelatedMetaByTag } from 'calypso/data/reader/use-related-meta-by-tag';
 import { useTagStats } from 'calypso/data/reader/use-tag-stats';
 import formatNumberCompact from 'calypso/lib/format-number-compact';
+import { createAccountUrl } from 'calypso/lib/paths';
 import { recordAction, recordGaEvent } from 'calypso/reader/stats';
 import ReaderListFollowingItem from 'calypso/reader/stream/reader-list-followed-sites/item';
 import { recordReaderTracksEvent } from 'calypso/state/reader/analytics/actions';
@@ -17,6 +20,12 @@ const ReaderTagSidebar = ( { tag } ) => {
 	if ( relatedMetaByTag === undefined ) {
 		return null;
 	}
+	const showCreateAccountPrompt = true;
+	const { pathname } = getUrlParts( window.location.href );
+	const signupUrl = createAccountUrl( {
+		redirectTo: pathname,
+		ref: 'reader-tag-sidebar',
+	} );
 
 	const handleTagSidebarClick = () => {
 		recordAction( 'clicked_reader_sidebar_tag' );
@@ -31,6 +40,11 @@ const ReaderTagSidebar = ( { tag } ) => {
 	const trackTagsPageLinkClick = () => {
 		recordAction( 'clicked_reader_sidebar_tags_page_link' );
 		dispatch( recordReaderTracksEvent( 'calypso_reader_sidebar_tags_page_link_clicked' ) );
+	};
+
+	const trackSignupClick = () => {
+		recordAction( 'clicked_reader_sidebar_signup' );
+		dispatch( recordReaderTracksEvent( 'calypso_reader_sidebar_signup_clicked' ) );
 	};
 
 	const tagLinks = relatedMetaByTag.data?.related_tags?.map( ( relatedTag ) => (
@@ -71,6 +85,13 @@ const ReaderTagSidebar = ( { tag } ) => {
 				<div className="reader-tag-sidebar-related-sites">
 					<h2>{ translate( 'Related Sites' ) }</h2>
 					{ relatedSitesLinks }
+				</div>
+			) }
+			{ showCreateAccountPrompt && (
+				<div className="reader-tag-sidebar-create-account-prompt">
+					<Button primary onClick={ trackSignupClick } href={ signupUrl }>
+						{ translate( 'Join the WordPress.com community' ) }
+					</Button>
 				</div>
 			) }
 		</>
