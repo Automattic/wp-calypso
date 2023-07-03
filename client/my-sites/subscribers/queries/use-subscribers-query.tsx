0a21439866017149bc579e8b9783e737cef63160
Juan Adolfo Lucha García <juanlucha@gmail.com>
2023-07-03 16:31:38 +0200

    Added filters to Subscribers Page

--- client/my-sites/subscribers/queries/use-subscribers-query.tsx
+++ client/my-sites/subscribers/queries/use-subscribers-query.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
+import { useCallback } from 'react';
 import wpcom from 'calypso/lib/wp';
-import { SubscribersSortBy } from '../constants';
+import { SubscribersFilterBy, SubscribersSortBy } from '../constants';
 import { getSubscribersCacheKey } from '../helpers';
-import type { SubscriberEndpointResponse } from '../types';
+import type { Subscriber, SubscriberEndpointResponse } from '../types';
 
 type SubscriberQueryParams = {
 	siteId: number | null;
@@ -10,6 +11,7 @@ type SubscriberQueryParams = {
 	perPage?: number;
 	search?: string;
 	sortTerm?: SubscribersSortBy;
+	filterOption?: SubscribersFilterBy;
 };
 
 const useSubscribersQuery = ( {
@@ -18,16 +20,37 @@ const useSubscribersQuery = ( {
 	perPage = 10,
 	search,
 	sortTerm = SubscribersSortBy.DateSubscribed,
+	filterOption = SubscribersFilterBy.All,
 }: SubscriberQueryParams ) => {
+	const filterFunction = useCallback(
+		( item: Subscriber ) => {
+			switch ( filterOption ) {
+				case SubscribersFilterBy.Email:
+					return item.user_id === 0;
+				case SubscribersFilterBy.WPCOM:
+					return item.user_id !== 0;
+				case SubscribersFilterBy.All:
+				default:
+					return true;
+			}
+		},
+		[ filterOption ]
+	);
+
 	return useQuery< SubscriberEndpointResponse >( {
-		queryKey: getSubscribersCacheKey( siteId, page, perPage, search, sortTerm ),
+		queryKey: getSubscribersCacheKey( siteId, page, perPage, search, sortTerm, filterOption ),
 		queryFn: () =>
-			wpcom.req.get( {
-				path: `/sites/${ siteId }/subscribers?per_page=${ perPage }&page=${ page }${
-					search ? `&search=${ encodeURIComponent( search ) }` : ''
-				}${ sortTerm ? `&sort=${ sortTerm }` : '' }`,
-				apiNamespace: 'wpcom/v2',
-			} ),
+			wpcom.req
+				.get( {
+					path: `/sites/${ siteId }/subscribers?per_page=${ perPage }&page=${ page }${
+						search ? `&search=${ encodeURIComponent( search ) }` : ''
+					}${ sortTerm ? `&sort=${ sortTerm }` : '' }`,
+					apiNamespace: 'wpcom/v2',
+				} )
+				.then( ( response: SubscriberEndpointResponse ) => {
+					const subscribers = response.subscribers.filter( filterFunction );
+					return { ...response, subscribers };
+				} ),
 		enabled: !! siteId,
 		keepPreviousData: true,
 	} );
