b67b6fdc00e29b78d89c6e118c69f45d1d372410
Paulo Cruz <paulo.cruz@automattic.com>
2023-07-04 18:09:56 -0300

    Update AddSubscribersModal style

--- client/my-sites/subscribers/components/add-subscribers-modal/add-subscribers-modal.tsx
+++ client/my-sites/subscribers/components/add-subscribers-modal/add-subscribers-modal.tsx
@@ -3,9 +3,13 @@ import { isEnabled } from '@automattic/calypso-config';
 import { AddSubscriberForm } from '@automattic/subscriber';
 import { Modal } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
+import { useState } from 'react';
+import { LoadingBar } from 'calypso/components/loading-bar';
+import './style.scss';
 
 type AddSubscribersModalProps = {
 	siteId: number;
+	siteTitle: string;
 	showModal: boolean;
 	onClose: () => void;
 	onAddFinished: () => void;
@@ -13,29 +17,55 @@ type AddSubscribersModalProps = {
 
 const AddSubscribersModal = ( {
 	siteId,
+	siteTitle,
 	showModal,
 	onClose,
 	onAddFinished,
 }: AddSubscribersModalProps ) => {
 	const translate = useTranslate();
-	const title = translate( 'Add subscribers' );
+	const modalTitle = translate( 'Add subscribers to %s', {
+		args: [ siteTitle ],
+		comment: "%s is the site's title",
+	} );
+
+	const [ isUploading, setIsUploading ] = useState( false );
+	const onImportStarted = ( hasFile: boolean ) => setIsUploading( hasFile );
+	const onImportFinished = () => {
+		setIsUploading( false );
+		onAddFinished();
+	};
+
+	if ( ! showModal ) {
+		return null;
+	}
 
 	return (
-		<>
-			{ showModal && (
-				<Modal title={ title } onRequestClose={ onClose }>
-					<AddSubscriberForm
-						siteId={ siteId }
-						submitBtnAlwaysEnable={ true }
-						onImportFinished={ onAddFinished }
-						showTitle={ false }
-						showSubtitle={ false }
-						showCsvUpload={ isEnabled( 'subscriber-csv-upload' ) }
-						recordTracksEvent={ recordTracksEvent }
-					/>
-				</Modal>
+		<Modal title={ modalTitle } onRequestClose={ onClose } overlayClassName="add-subscribers-modal">
+			{ isUploading && (
+				<>
+					<LoadingBar progress={ 0.5 } />
+					<span className="add-subscribers-modal__loading-text">
+						{ translate( 'Uploading CSV file' ) }...
+					</span>
+				</>
 			) }
-		</>
+
+			{ ! isUploading && (
+				<label className="add-subscribers-modal__label">{ translate( 'Email or username' ) }</label>
+			) }
+
+			<AddSubscriberForm
+				siteId={ siteId }
+				submitBtnAlwaysEnable={ true }
+				onImportStarted={ onImportStarted }
+				onImportFinished={ onImportFinished }
+				showTitle={ false }
+				showSubtitle={ false }
+				showCsvUpload={ isEnabled( 'subscriber-csv-upload' ) }
+				recordTracksEvent={ recordTracksEvent }
+				hidden={ isUploading }
+			/>
+		</Modal>
 	);
 };
 
