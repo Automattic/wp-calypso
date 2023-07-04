d8e7c3bf670c71f27d7fb917c4c5c03444f9b2be
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2023-07-04 19:03:43 +0200

    Add new Users page copy with language fallback

--- client/my-sites/people/subscribers-team/index.tsx
+++ client/my-sites/people/subscribers-team/index.tsx
@@ -1,4 +1,6 @@
-import { localizeUrl } from '@automattic/i18n-utils';
+import { isEnabled } from '@automattic/calypso-config';
+import { localizeUrl, useLocale } from '@automattic/i18n-utils';
+import { useI18n } from '@wordpress/react-i18n/';
 import { useTranslate } from 'i18n-calypso';
 import FormattedHeader from 'calypso/components/formatted-header';
 import InlineSupportLink from 'calypso/components/inline-support-link';
@@ -48,6 +50,48 @@ function SubscribersTeam( props: Props ) {
 	) as unknown as FollowersQuery;
 	const usersQuery = useUsersQuery( site?.ID, teamFetchOptions ) as unknown as UsersQuery;
 
+	const locale = useLocale();
+	const { hasTranslation } = useI18n();
+
+	const getSubheaderText = () => {
+		const subHeaderTextOld = translate(
+			'Invite subscribers and team members to your site and manage their access settings. {{learnMore}}Learn more{{/learnMore}}.',
+			{
+				components: {
+					learnMore: (
+						<InlineSupportLink
+							showIcon={ false }
+							supportLink={ localizeUrl( 'https://wordpress.com/support/invite-people/' ) }
+						/>
+					),
+				},
+			}
+		);
+
+		const subHeaderTextNew = translate(
+			'Invite team members to your site and manage their access settings. {{learnMore}}Learn more{{/learnMore}}.',
+			{
+				components: {
+					learnMore: (
+						<InlineSupportLink
+							showIcon={ false }
+							supportLink={ localizeUrl( 'https://wordpress.com/support/invite-people/' ) }
+						/>
+					),
+				},
+			}
+		);
+
+		const useNewSubHeaderText =
+			isEnabled( 'subscribers-page-new' ) &&
+			( locale === 'en' ||
+				hasTranslation(
+					'Invite team members to your site and manage their access settings. {{learnMore}}Learn more{{/learnMore}}.'
+				) );
+
+		return useNewSubHeaderText ? subHeaderTextNew : subHeaderTextOld;
+	};
+
 	return (
 		<Main>
 			<ScreenOptionsTab wpAdminPath="users.php" />
@@ -55,19 +99,7 @@ function SubscribersTeam( props: Props ) {
 				brandFont
 				className="people__page-heading"
 				headerText={ translate( 'Users' ) }
-				subHeaderText={ translate(
-					'Invite subscribers and team members to your site and manage their access settings. {{learnMore}}Learn more{{/learnMore}}.',
-					{
-						components: {
-							learnMore: (
-								<InlineSupportLink
-									showIcon={ false }
-									supportLink={ localizeUrl( 'https://wordpress.com/support/invite-people/' ) }
-								/>
-							),
-						},
-					}
-				) }
+				subHeaderText={ getSubheaderText() }
 				align="left"
 				hasScreenOptions
 			/>
