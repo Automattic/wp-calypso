6de33c2bbf622d58f7cab95605e2eaf41b700e9d
Jasper Kang <kangzjnet@gmail.com>
2023-07-04 15:03:19 +1200

    hasSiteProductJetpackStatsFree

---
+++ client/state/sites/selectors/has-site-product-jetpack-stats-free.ts
@@ -0,0 +1,15 @@
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import hasSiteProductJetpackStats from './has-site-product-jetpack-stats';
+import hasSiteProductJetpackStatsPaid from './has-site-product-jetpack-stats-paid';
+import type { AppState } from 'calypso/types';
+
+const hasSiteProductJetpackStatsFree = (
+	state: AppState,
+	siteId = getSelectedSiteId( state )
+): boolean => {
+	return (
+		hasSiteProductJetpackStats( state, false, siteId ) && ! hasSiteProductJetpackStatsPaid( state )
+	);
+};
+
+export default hasSiteProductJetpackStatsFree;
