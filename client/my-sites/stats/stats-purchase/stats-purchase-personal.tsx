58d69d20d268715f1e1b929c2967cc52c71652ae
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2023-07-06 11:59:36 +1200

    Stats: Updating Stats purchase page (#78763)
    * Stats: CSS updates
    
    * Stats: Reset Panel CSS
    
    * Stats: Cleanup
    
    * Stats: Fixing amount when switching plans
    
    * Stats: Updating translations and CSS
    
    * Stats: Fixing plan value when swapping plans
    
    * Stats: CSS refactoring
    
    * Stats: Fixing spacing
    
    * Stats: Design feedback
    
    * Stats: Adding emoji
    
    * Stats: Adding missing feature description
    
    * Stats: Addjusting emoji display
    
    * Stats: Fixing onClick type
    
    * Stats: Fixing onClick type

--- client/my-sites/stats/stats-purchase/stats-purchase-personal.tsx
+++ client/my-sites/stats/stats-purchase/stats-purchase-personal.tsx
@@ -1,54 +1,111 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { PricingSlider, RenderThumbFunction } from '@automattic/components';
 import { Button } from '@wordpress/components';
+import { useTranslate } from 'i18n-calypso';
+import { COMPONENT_CLASS_NAME } from './stats-purchase-wizard';
 
 interface PersonalPurchaseProps {
 	subscriptionValue: number;
 	setSubscriptionValue: ( value: number ) => number;
+	handlePlanSwap: ( e: React.MouseEvent< HTMLAnchorElement, MouseEvent > ) => void;
 }
 
-const PersonalPurchase = ( { subscriptionValue, setSubscriptionValue }: PersonalPurchaseProps ) => {
-	// TODO: Add translate calls to copy.
+const AVERAGE_PRICE_INFO = '$6';
+
+const PersonalPurchase = ( {
+	subscriptionValue,
+	setSubscriptionValue,
+	handlePlanSwap,
+}: PersonalPurchaseProps ) => {
+	const translate = useTranslate();
+
 	const sliderLabel = ( ( props, state ) => {
-		return <div { ...props }>${ state?.valueNow || subscriptionValue }/month</div>;
+		let emoji;
+
+		if ( subscriptionValue < 10 ) {
+			emoji = String.fromCodePoint( 0x1f60a ); /* Smiling face emoji */
+		} else if ( subscriptionValue < 40 ) {
+			emoji = String.fromCodePoint( 0x2764, 0xfe0f ); /* Heart emoji */
+		} else if ( subscriptionValue >= 40 ) {
+			emoji = String.fromCodePoint( 0x1f525 ); /* Fire emoji */
+		}
+
+		return (
+			<div { ...props }>
+				${ state?.valueNow || subscriptionValue }/{ translate( 'month' ) }{ ' ' }
+				{ subscriptionValue > 0 && emoji }
+			</div>
+		);
 	} ) as RenderThumbFunction;
 
 	return (
 		<div>
+			{ subscriptionValue < 10 && (
+				<div className={ `${ COMPONENT_CLASS_NAME }__notice` }>
+					{ translate(
+						'This plan is for personal sites only. If your site is used for a commercial activity, {{Button}}you will need to choose a commercial paln{{/Button}}.',
+						{
+							components: {
+								Button: <Button variant="link" href="#" onClick={ ( e ) => handlePlanSwap( e ) } />,
+							},
+						}
+					) }
+				</div>
+			) }
 			<PricingSlider
+				className={ `${ COMPONENT_CLASS_NAME }__slider` }
 				value={ subscriptionValue }
 				renderThumb={ sliderLabel }
 				onChange={ setSubscriptionValue }
+				maxValue={ 50 }
 			/>
 
-			<p className="average-price">The average person pays $6 per month</p>
+			<p className={ `${ COMPONENT_CLASS_NAME }__average-price` }>
+				{ translate( 'The average person pays %(value)s per month', {
+					args: {
+						value: AVERAGE_PRICE_INFO,
+					},
+				} ) }
+			</p>
 
-			<div className="benefits">
+			<div className={ `${ COMPONENT_CLASS_NAME }__benefits` }>
 				{ subscriptionValue === 0 ? (
-					<ul className="not-included">
-						<li>No access to upcoming features</li>
-						<li>No priority support</li>
-						<li>You’ll see upsells and ads in the Stats page</li>
+					<ul className={ `${ COMPONENT_CLASS_NAME }__benefits--not-included` }>
+						<li>{ translate( 'No access to upcoming features' ) }</li>
+						<li>{ translate( 'No priority support' ) }</li>
+						<li>{ translate( "You'll see upsells and ads in the Stats page" ) }</li>
 					</ul>
 				) : (
-					<ul className="included">
-						<li>Instant access to upcoming features</li>
-						<li>Priority support</li>
-						<li>Ad-free experience</li>
-						{ subscriptionValue >= 90 && <li>You’re one of the top supporters — thank you!</li> }
+					<ul className={ `${ COMPONENT_CLASS_NAME }__benefits--included` }>
+						<li>{ translate( 'Instant access to upcoming features' ) }</li>
+						<li>{ translate( 'Priority support' ) }</li>
+						<li>{ translate( 'Ad-free experience' ) }</li>
 					</ul>
 				) }
 			</div>
 
 			<p>
-				By clicking the button below, you agree to our <a href="#">Terms of Service</a> and to{ ' ' }
-				<a href="#">share details</a> with WordPress.com.
+				{ translate(
+					`By clicking the button below, you agree to our {{a}}Terms of Service{{/a}} and to {{b}}share details{{/b}} with WordPress.com.`,
+					{
+						components: {
+							a: <Button variant="link" href="#" />,
+							b: <Button variant="link" href="#" />,
+						},
+					}
+				) }
 			</p>
 
 			{ subscriptionValue === 0 ? (
-				<Button isSecondary>Continue with Jetpack Stats for free</Button>
+				<Button variant="primary">{ translate( 'Continue with Jetpack Stats for free' ) }</Button>
 			) : (
-				<Button isPrimary>Get Jetpack Stats for ${ subscriptionValue } per month</Button>
+				<Button variant="primary">
+					{ translate( 'Get Jetpack Stats for %(value)s per month', {
+						args: {
+							value: subscriptionValue,
+						},
+					} ) }
+				</Button>
 			) }
 		</div>
 	);
