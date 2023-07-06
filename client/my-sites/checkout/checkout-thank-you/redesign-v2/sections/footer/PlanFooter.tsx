8c2993da05b7c8fe54442c905ebd6d0d5e5e95f0
Paulo Trentin <paulo@paulotrentin.com.br>
2023-07-06 11:23:50 -0300

    Minor fixes: - Copy fix - Added siteSlug after /themes - Left alignment at mobile devices

--- client/my-sites/checkout/checkout-thank-you/redesign-v2/sections/footer/PlanFooter.tsx
+++ client/my-sites/checkout/checkout-thank-you/redesign-v2/sections/footer/PlanFooter.tsx
@@ -1,17 +1,23 @@
 import { translate } from 'i18n-calypso';
 import PurchaseDetail from 'calypso/components/purchase-detail';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { useSelector } from 'calypso/state';
+import { getSiteSlug } from 'calypso/state/sites/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 const PlanFooter = () => {
+	const siteId = useSelector( getSelectedSiteId );
+	const siteSlug = useSelector( ( state ) => getSiteSlug( state, siteId ) );
+
 	return (
 		<div>
 			<PurchaseDetail
 				title={ translate( 'A site refresh' ) }
 				description={ translate(
-					'A new look and feel can help you stand our from the crowd. Get a new theme and make an impression.'
+					'A new look and feel can help you stand out from the crowd. Get a new theme and make an impression.'
 				) }
 				buttonText={ translate( 'Find your new theme' ) }
-				href="/themes"
+				href={ `/themes/${ siteSlug }` }
 				onClick={ recordTracksEvent( 'calypso_plan_thank_you_theme_click' ) }
 			/>
 
