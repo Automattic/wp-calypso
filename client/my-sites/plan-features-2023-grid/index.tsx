bc38557a1436b5ea31e81c1df9ab92e1fdfc8ac3
Jeremy Yip <jeremy.yip@automattic.com>
2023-07-06 18:54:16 -0700

    Refine storage option and storage feature naming

--- client/my-sites/plan-features-2023-grid/index.tsx
+++ client/my-sites/plan-features-2023-grid/index.tsx
@@ -866,9 +866,8 @@ export class PlanFeatures2023Grid extends Component<
 	}
 
 	renderStorageAddOnDropdown( properties: PlanProperties ) {
-		const { planName, storageOptions, storageAddOns } = properties;
+		const { planName, storageOptions } = properties;
 		const { selectedStorage } = this.state;
-		const allStorageOptions = [ ...storageOptions, ...storageAddOns ];
 
 		return (
 			<SelectDropdown
@@ -883,7 +882,7 @@ export class PlanFeatures2023Grid extends Component<
 						: getStorageStringFromFeature( storageOptions[ 0 ] )
 				}
 			>
-				{ allStorageOptions.map( ( storageFeature ) => {
+				{ storageOptions.map( ( storageFeature ) => {
 					return (
 						<SelectDropdown.Item
 							key={ `${ planName } ${ storageFeature }` }
@@ -918,16 +917,15 @@ export class PlanFeatures2023Grid extends Component<
 					return null;
 				}
 
-				const { planName, storageOptions, storageAddOns } = properties;
+				const { planName, storageOptions, storageFeatures } = properties;
 				const canUpgradeStorageForPlan =
-					storageAddOns.length > 0 &&
-					storageOptions.length === 1 &&
+					storageOptions.length > 1 &&
 					intervalType === 'yearly' &&
 					isEnabled( 'plans/upgradeable-storage' );
 
 				const storageJSX = canUpgradeStorageForPlan
 					? this.renderStorageAddOnDropdown( properties )
-					: storageOptions.map( ( storageFeature: string ) => {
+					: storageFeatures.map( ( storageFeature: string ) => {
 							if ( storageFeature.length <= 0 ) {
 								return;
 							}
@@ -1101,7 +1099,9 @@ const ConnectedPlanFeatures2023Grid = connect(
 				isWpcomEnterpriseGridPlan( plan ) && planConstantObj.getPathSlug
 					? planConstantObj.getPathSlug()
 					: planObject?.product_name_short ?? '';
-			const storageOptions =
+
+			// TODO: Rename get2023PricingGridSignupStorageOptions to get2023PricingGridSignupStorageFeatures
+			const storageFeatures =
 				( planConstantObj.get2023PricingGridSignupStorageOptions &&
 					planConstantObj.get2023PricingGridSignupStorageOptions() ) ||
 				[];
@@ -1109,6 +1109,7 @@ const ConnectedPlanFeatures2023Grid = connect(
 				( planConstantObj.get2023PricingGridSignupStorageAddOns &&
 					planConstantObj.get2023PricingGridSignupStorageAddOns() ) ||
 				[];
+			const storageOptions = [ ...storageFeatures, ...storageAddOns ];
 			const availableForPurchase = isInSignup || isPlanAvailableForPurchase( state, siteId, plan );
 			const isCurrentPlan = currentSitePlanSlug === plan;
 			const isVisible = visiblePlans?.indexOf( plan ) !== -1;
@@ -1133,7 +1134,7 @@ const ConnectedPlanFeatures2023Grid = connect(
 				isMonthlyPlan,
 				tagline,
 				storageOptions,
-				storageAddOns,
+				storageFeatures,
 				billingPeriod,
 				showMonthlyPrice,
 			};
