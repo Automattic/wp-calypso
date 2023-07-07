4af633c93e504269bd3526deec0cf0da1a774fef
Andrés Blanco <email@gmail.com>
2023-07-07 18:01:06 -0300

    add track event when preview is clicked

--- client/my-sites/customer-home/cards/features/site-preview/index.tsx
+++ client/my-sites/customer-home/cards/features/site-preview/index.tsx
@@ -1,5 +1,6 @@
 import { useI18n } from '@wordpress/react-i18n';
 import { addQueryArgs } from '@wordpress/url';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { SiteItemThumbnail } from 'calypso/sites-dashboard/components/sites-site-item-thumbnail';
 import { SiteUrl, Truncated } from 'calypso/sites-dashboard/components/sites-site-url';
 import { useSelector } from 'calypso/state';
@@ -21,7 +22,19 @@ const SitePreview = (): JSX.Element => {
 
 	return (
 		<div className="home-site-preview">
-			<a className="home-site-preview__thumbnail-wrapper" href={ editSiteSlug }>
+			<a
+				onClick={ ( event ) => {
+					event.preventDefault();
+
+					recordTracksEvent( 'calypso_customer_home_site_preview_clicked', {
+						context: 'customer-home',
+					} );
+
+					window.location.href = event.currentTarget.href;
+				} }
+				className="home-site-preview__thumbnail-wrapper"
+				href={ editSiteSlug }
+			>
 				<div className="theme-card__image-label"> { __( 'Edit site' ) } </div>
 				<SiteItemThumbnail
 					displayMode="tile"
