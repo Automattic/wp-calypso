751805c630dbafd2a2159a38c1024c295a6ca70a
Panos Kountanis <panosktn@gmail.com>
2023-07-10 11:57:56 +0300

    Remove unimplemented step.

--- client/landing/stepper/declarative-flow/videopress-tv.ts
+++ client/landing/stepper/declarative-flow/videopress-tv.ts
@@ -1,15 +1,9 @@
-import { SiteSelect } from '@automattic/data-stores';
-import { useLocale } from '@automattic/i18n-utils';
 import { useFlowProgress, VIDEOPRESS_TV_FLOW } from '@automattic/onboarding';
-import { useSelect, useDispatch } from '@wordpress/data';
+import { useDispatch } from '@wordpress/data';
 import { translate } from 'i18n-calypso';
-import { useState } from 'react';
-import { useNewSiteVisibility } from 'calypso/landing/stepper/hooks/use-selected-plan';
-import { useSiteSlug } from '../hooks/use-site-slug';
-import { SITE_STORE, USER_STORE, ONBOARD_STORE } from '../stores';
+import { ONBOARD_STORE } from '../stores';
 import './internals/videopress.scss';
 import type { Flow, ProvidedDependencies } from './internals/types';
-import type { OnboardSelect, UserSelect } from '@automattic/data-stores';
 
 const videopressTv: Flow = {
 	name: VIDEOPRESS_TV_FLOW,
@@ -22,10 +16,6 @@ const videopressTv: Flow = {
 				slug: 'intro',
 				asyncComponent: () => import( './internals/steps-repository/intro' ),
 			},
-			{
-				slug: 'processing',
-				asyncComponent: () => import( './internals/steps-repository/processing-step' ),
-			},
 		];
 	},
 
@@ -35,17 +25,6 @@ const videopressTv: Flow = {
 			if ( ! document.body.classList.contains( 'is-videopress-tv-stepper' ) ) {
 				document.body.classList.add( 'is-videopress-tv-stepper' );
 			}
-
-			// Also target processing step for background images
-			const processingStepClassName = 'is-processing-step';
-			const hasProcessingStepClass = document.body.classList.contains( processingStepClassName );
-			if ( 'processing' === _currentStep ) {
-				if ( ! hasProcessingStepClass ) {
-					document.body.classList.add( processingStepClassName );
-				}
-			} else if ( hasProcessingStepClass ) {
-				document.body.classList.remove( processingStepClassName );
-			}
 		}
 
 		const name = this.name;
@@ -53,28 +32,6 @@ const videopressTv: Flow = {
 			useDispatch( ONBOARD_STORE );
 		const flowProgress = useFlowProgress( { stepName: _currentStep, flowName: name } );
 		setStepProgress( flowProgress );
-		const _siteSlug = useSiteSlug();
-		const userIsLoggedIn = useSelect(
-			( select ) => ( select( USER_STORE ) as UserSelect ).isCurrentUserLoggedIn(),
-			[]
-		);
-		const locale = useLocale();
-
-		const { createVideoPressSite, setSelectedSite, setPendingAction, setProgress } =
-			useDispatch( ONBOARD_STORE );
-		const currentUser = useSelect(
-			( select ) => ( select( USER_STORE ) as UserSelect ).getCurrentUser(),
-			[]
-		);
-		const siteDescription = useSelect(
-			( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getSelectedSiteDescription(),
-			[]
-		);
-		const visibility = useNewSiteVisibility();
-		const { getNewSite } = useSelect( ( select ) => select( SITE_STORE ) as SiteSelect, [] );
-		const { saveSiteSettings, setIntentOnSite } = useDispatch( SITE_STORE );
-
-		const [ isSiteCreationPending, setIsSiteCreationPending ] = useState( false );
 
 		const clearOnboardingSiteOptions = () => {
 			setSiteTitle( '' );
@@ -83,86 +40,26 @@ const videopressTv: Flow = {
 			setSelectedDesign( undefined );
 		};
 
-		const addVideoPressPendingAction = () => {
-			// only allow one call to this action to occur
-			if ( isSiteCreationPending ) {
-				return;
-			}
-
-			setIsSiteCreationPending( true );
-
-			setPendingAction( async () => {
-				setProgress( 0 );
-				try {
-					// Todo: vptv-specific site.
-					await createVideoPressSite( {
-						username: currentUser ? currentUser?.username : '',
-						languageSlug: locale,
-						visibility,
-					} );
-				} catch ( e ) {
-					return;
-				}
-				setProgress( 0.5 );
-
-				const newSite = getNewSite();
-				if ( ! newSite ) {
-					return;
-				}
-
-				setSelectedSite( newSite.blogid );
-				setIntentOnSite( newSite.site_slug, VIDEOPRESS_TV_FLOW );
-				saveSiteSettings( newSite.blogid, {
-					launchpad_screen: 'full',
-					blogdescription: siteDescription,
-				} );
-
-				setProgress( 0.75 );
-			} );
-		};
-
 		switch ( _currentStep ) {
 			case 'intro':
 				clearOnboardingSiteOptions();
 				break;
-			case 'processing':
-				if ( ! _siteSlug ) {
-					addVideoPressPendingAction();
-				}
-				break;
 		}
 
 		async function submit( providedDependencies: ProvidedDependencies = {} ) {
-			recordSubmitStep( providedDependencies, 'anchor-fm', flowName, _currentStep );
 			switch ( _currentStep ) {
 				case 'intro':
 					return navigate( 'processing' );
-
-				case 'processing':
-					if ( userIsLoggedIn ) {
-						return navigate( 'options' );
-					}
-
-					return window.location.replace(
-						`/start/videopress-account/user/${ locale }?variationName=${ name }&flow=${ name }&pageTitle=Video%20Portfolio&redirect_to=/setup/videopress/options`
-					);
 			}
 			return providedDependencies;
 		}
 
 		const goBack = () => {
-			switch ( _currentStep ) {
-				case 'chooseADomain':
-					return navigate( 'options' );
-			}
 			return;
 		};
 
 		const goNext = () => {
 			switch ( _currentStep ) {
-				case 'processing':
-					return window.location.replace( `/view/${ _siteSlug }` );
-
 				default:
 					return navigate( 'intro' );
 			}
