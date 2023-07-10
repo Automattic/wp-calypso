e7a3151804abf66ef5a130284bd09cdac12bf7ef
Jessie Ross <j@scjr.me>
2023-07-10 14:41:17 +0800

    Add try catch and improve compatibility of polyfill

--- packages/explat-client/src/internal/local-storage.ts
+++ packages/explat-client/src/internal/local-storage.ts
@@ -1,15 +1,16 @@
 /**
  * LocalStorage polyfill from https://gist.github.com/juliocesar/926500
  * length and key methods were added to allow for implementing removeExpiredExperimentAssignments
+ * Altered to match window.localStorage signature exactly
  * Exported only for testing purposes
  */
-export const polyfilledLocalStorage = {
+export const polyfilledLocalStorage: typeof window.localStorage = {
 	_data: {} as Record< string, string >,
 	setItem: function ( id: string, val: string ): void {
 		this._data[ id ] = val;
 	},
-	getItem: function ( id: string ): string | undefined {
-		return this._data.hasOwnProperty( id ) ? this._data[ id ] : undefined;
+	getItem: function ( id: string ): string | null {
+		return this._data.hasOwnProperty( id ) ? this._data[ id ] : null;
 	},
 	removeItem: function ( id: string ): void {
 		delete this._data[ id ];
@@ -20,8 +21,9 @@ export const polyfilledLocalStorage = {
 	get length(): number {
 		return Object.keys( this._data ).length;
 	},
-	key: function ( index: number ): string | undefined {
-		return Object.keys( this._data )[ index ];
+	key: function ( index: number ): string | null {
+		const keys = Object.keys( this._data );
+		return index in keys ? keys[ index ] : null;
 	},
 };
 
@@ -29,9 +31,12 @@ export const polyfilledLocalStorage = {
  * A polyfilled LocalStorage.
  * The polyfill is required at least for testing.
  */
-const localStorage =
-	typeof window !== 'undefined' && window.localStorage
-		? window.localStorage
-		: polyfilledLocalStorage;
+let localStorage = polyfilledLocalStorage;
+try {
+	localStorage =
+		typeof window !== 'undefined' && window.localStorage
+			? window.localStorage
+			: polyfilledLocalStorage;
+} catch ( e ) {}
 
 export default localStorage;
