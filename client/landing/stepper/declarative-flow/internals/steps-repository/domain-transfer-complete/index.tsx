08943f6bd010b370751ca6e4acad54f83b7878e9
Allison Levine <allison.levine@automattic.com>
2023-07-11 11:48:44 -0400

    Update Complete page punctuation.

--- client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-complete/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-complete/index.tsx
@@ -52,13 +52,13 @@ const Complete: Step = function Complete( { flow } ) {
 					<FormattedHeader
 						id="domains-header"
 						headerText={ _n(
-							'Congrats on your domain transfer',
-							'Congrats on your domain transfers',
+							'Congrats on your domain transfer!',
+							'Congrats on your domain transfers!',
 							newlyTransferredDomains?.length || storedDomainsAmount
 						) }
 						subHeaderText={ _n(
-							"We've got it from here! We'll let you know when your newly transferred domain is ready to use!",
-							"We've got it from here! We'll let you know when your newly transferred domains are ready to use!",
+							"We've got it from here! We'll let you know when your newly transferred domain is ready to use.",
+							"We've got it from here! We'll let you know when your newly transferred domains are ready to use.",
 							newlyTransferredDomains?.length || storedDomainsAmount
 						) }
 						align="center"
