488440e36e2129d4f679abe1fa726b02145efd22
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2023-07-11 16:59:08 +0200

    Fix lint error

--- client/my-sites/subscribers/components/add-subscribers-modal/add-subscribers-modal.tsx
+++ client/my-sites/subscribers/components/add-subscribers-modal/add-subscribers-modal.tsx
@@ -5,9 +5,9 @@ import { AddSubscriberForm } from '@automattic/subscriber';
 import { Modal } from '@wordpress/components';
 import { useI18n } from '@wordpress/react-i18n/';
 import { useTranslate } from 'i18n-calypso';
-import { useSubscribersPage } from 'calypso/my-sites/subscribers/components/subscribers-page/subscribers-page-context';
 import { useState } from 'react';
 import { LoadingBar } from 'calypso/components/loading-bar';
+import { useSubscribersPage } from 'calypso/my-sites/subscribers/components/subscribers-page/subscribers-page-context';
 import './style.scss';
 
 type AddSubscribersModalProps = {
@@ -15,14 +15,12 @@ type AddSubscribersModalProps = {
 	siteTitle: string;
 };
 
-const AddSubscribersModal = ( {
-	siteId,
-	siteTitle,
-}: AddSubscribersModalProps ) => {
+const AddSubscribersModal = ( { siteId, siteTitle }: AddSubscribersModalProps ) => {
 	const translate = useTranslate();
 	const locale = useLocale();
 	const { hasTranslation } = useI18n();
-	const { showAddSubscribersModal, setShowAddSubscribersModal, addSubscribersCallback } = useSubscribersPage();
+	const { showAddSubscribersModal, setShowAddSubscribersModal, addSubscribersCallback } =
+		useSubscribersPage();
 
 	const modalTitle =
 		locale.startsWith( 'en' ) || hasTranslation( 'Add subscribers to %s' )
