9af426020500aaafbf5efe138672fac94d874f8a
John Caruso <4081020+jgcaruso@users.noreply.github.com>
2023-07-11 10:50:04 -0400

    VideoPress HQ: implement options step (#79201)
    * add support for checking logged in user when starting flow at /options
    
    * copy style from videopress onboarding
    
    * add persistence to options step
    
    * create site and redirect upon completion (#79207)

--- client/landing/stepper/declarative-flow/videopress-tv.ts
+++ client/landing/stepper/declarative-flow/videopress-tv.ts
@@ -1,9 +1,14 @@
+import { SiteSelect } from '@automattic/data-stores';
 import { useLocale } from '@automattic/i18n-utils';
 import { useFlowProgress, VIDEOPRESS_TV_FLOW } from '@automattic/onboarding';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { translate } from 'i18n-calypso';
-import { ONBOARD_STORE, USER_STORE } from '../stores';
+import { useEffect, useState } from 'react';
+import { useNewSiteVisibility } from 'calypso/landing/stepper/hooks/use-selected-plan';
+import { useSiteSlug } from '../hooks/use-site-slug';
+import { ONBOARD_STORE, SITE_STORE, USER_STORE } from '../stores';
 import './internals/videopress.scss';
+import ProcessingStep from './internals/steps-repository/processing-step';
 import SiteOptions from './internals/steps-repository/site-options';
 import type { Flow, ProvidedDependencies } from './internals/types';
 import type { UserSelect } from '@automattic/data-stores';
@@ -20,6 +25,7 @@ const videopressTv: Flow = {
 				asyncComponent: () => import( './internals/steps-repository/intro' ),
 			},
 			{ slug: 'options', component: SiteOptions },
+			{ slug: 'processing', component: ProcessingStep },
 		];
 	},
 
@@ -32,23 +38,83 @@ const videopressTv: Flow = {
 		}
 
 		const name = this.name;
-		const { setStepProgress } = useDispatch( ONBOARD_STORE );
+		const { createVideoPressTvSite, setPendingAction, setProgress, setStepProgress } =
+			useDispatch( ONBOARD_STORE );
 		const flowProgress = useFlowProgress( { stepName: _currentStep, flowName: name } );
 		const locale = useLocale();
 		const userIsLoggedIn = useSelect(
 			( select ) => ( select( USER_STORE ) as UserSelect ).isCurrentUserLoggedIn(),
 			[]
 		);
+		const { setSiteDescription, setSiteTitle } = useDispatch( ONBOARD_STORE );
+		const _siteSlug = useSiteSlug();
+		const [ isSiteCreationPending, setIsSiteCreationPending ] = useState( false );
+		const { getNewSite } = useSelect( ( select ) => select( SITE_STORE ) as SiteSelect, [] );
+		const visibility = useNewSiteVisibility();
 
 		setStepProgress( flowProgress );
 
-		switch ( _currentStep ) {
-			case 'intro':
-				break;
-			case 'options':
-				// todo: validate user logged in
-				break;
-		}
+		const clearOnboardingSiteOptions = () => {
+			setSiteTitle( '' );
+			setSiteDescription( '' );
+		};
+
+		const stepValidateUserIsLoggedIn = () => {
+			if ( ! userIsLoggedIn ) {
+				navigate( 'intro' );
+				return false;
+			}
+			return true;
+		};
+
+		const addVideoPressPendingAction = () => {
+			// only allow one call to this action to occur
+			if ( isSiteCreationPending ) {
+				return;
+			}
+
+			setIsSiteCreationPending( true );
+
+			setPendingAction( async () => {
+				setProgress( 0 );
+				try {
+					await createVideoPressTvSite( {
+						languageSlug: locale,
+						visibility,
+					} );
+				} catch ( e ) {
+					return;
+				}
+				setProgress( 0.5 );
+
+				const newSite = getNewSite();
+				if ( ! newSite || ! newSite.site_slug ) {
+					return;
+				}
+
+				setProgress( 1 );
+
+				// go to the new site, and let the "empty state" act as the remaining of onboarding.
+				window.location.href = `https://${ newSite.site_slug }`;
+			} );
+		};
+
+		// needs to be wrapped in a useEffect because validation can call `navigate` which needs to be called in a useEffect
+		useEffect( () => {
+			switch ( _currentStep ) {
+				case 'intro':
+					clearOnboardingSiteOptions();
+					break;
+				case 'options':
+					stepValidateUserIsLoggedIn();
+					break;
+				case 'processing':
+					if ( ! _siteSlug ) {
+						addVideoPressPendingAction();
+					}
+					break;
+			}
+		} );
 
 		async function submit( providedDependencies: ProvidedDependencies = {} ) {
 			switch ( _currentStep ) {
@@ -60,6 +126,12 @@ const videopressTv: Flow = {
 					return window.location.replace(
 						`/start/videopress-account/user/${ locale }?variationName=${ name }&flow=${ name }&pageTitle=VideoPress.TV&redirect_to=/setup/videopress-tv/options`
 					);
+				case 'options': {
+					const { siteTitle, tagline } = providedDependencies;
+					setSiteTitle( siteTitle );
+					setSiteDescription( tagline );
+					return navigate( 'processing' );
+				}
 			}
 			return providedDependencies;
 		}
