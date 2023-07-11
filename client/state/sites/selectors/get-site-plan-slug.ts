fe884938a5e94f9871f0ebe04de9bff080e203a8
Christos <chriskmnds@gmail.com>
2023-07-11 16:22:42 +0300

    fix unit test

--- client/state/sites/selectors/get-site-plan-slug.ts
+++ client/state/sites/selectors/get-site-plan-slug.ts
@@ -3,5 +3,9 @@ import type { PlanSlug } from '@automattic/calypso-products';
 import type { AppState } from 'calypso/types';
 
 export default function getSitePlanSlug( state: AppState, siteId?: number | null ) {
-	return siteId ? ( getSitePlan( state, siteId )?.product_slug as PlanSlug ) : null;
+	if ( ! siteId ) {
+		return null;
+	}
+
+	return ( getSitePlan( state, siteId )?.product_slug as PlanSlug ) ?? null;
 }
