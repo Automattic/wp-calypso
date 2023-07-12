bb758368bd0cead66ac2cdad37029b22a67d73b6
Dognose <dognose24@gmail.com>
2023-07-12 14:35:04 +0800

    Update site products with jetpack purchases fetch

--- client/my-sites/stats/stats-notices/index.tsx
+++ client/my-sites/stats/stats-notices/index.tsx
@@ -1,6 +1,8 @@
 import config from '@automattic/calypso-config';
-import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import version_compare from 'calypso/lib/version-compare';
+import { SITE_PURCHASES_UPDATE } from 'calypso/state/action-types';
 import getJetpackStatsAdminVersion from 'calypso/state/sites/selectors/get-jetpack-stats-admin-version';
 import hasSiteProductJetpackStatsPaid from 'calypso/state/sites/selectors/has-site-product-jetpack-stats-paid';
 import isJetpackSite from 'calypso/state/sites/selectors/is-jetpack-site';
@@ -21,6 +23,28 @@ const NewStatsNotices = ( { siteId, isOdysseyStats }: StatsNoticesProps ) => {
 	const isSiteJetpackNotAtomic = useSelector( ( state ) =>
 		isJetpackSite( state, siteId, { treatAtomicAsJetpackSite: false } )
 	);
+
+	const reduxDispatch = useDispatch();
+	// TODO: Display error messages on the notice.
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [ error, setError ] = useState< Error | null >( null );
+
+	// Update site purchases in Redux store for Odyssey Stats.
+	useEffect( () => {
+		if ( isOdysseyStats ) {
+			wpcom.req
+				.get( { path: '/site/purchases', apiNamespace: 'jetpack/v4' } )
+				.then( ( res: { data: string } ) => JSON.parse( res.data ) )
+				.then( ( purchases: Record< string, string >[] ) => {
+					reduxDispatch( {
+						type: SITE_PURCHASES_UPDATE,
+						siteId,
+						purchases: purchases,
+					} );
+				} )
+				.catch( ( error: Error ) => setError( error ) );
+		}
+	}, [ isOdysseyStats, reduxDispatch, siteId ] );
 
 	return (
 		<>
