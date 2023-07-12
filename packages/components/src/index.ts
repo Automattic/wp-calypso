3d349e277d7cc0f15c8750f51b0a8606973159d3
sixhours <calobee@gmail.com>
2023-07-12 16:09:08 -0400

    Update BadgeProps to break out BadgeType props list

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,9 +1,5 @@
 export { default as Badge } from './badge';
-<<<<<<< HEAD
 export type { BadgeType } from './badge';
-=======
-export type { BadgeProps } from './badge';
->>>>>>> 4cfb7445d90 (Fix typescript errors with badge type)
 export { default as Button } from './button';
 export * as Animation from './animation';
 export { default as Card } from './card';
