18f1785aa90b9a7869f2ee0e3fdf963ac8dd19de
Tim Broddin <tim@wannabes.be>
2023-07-13 13:59:01 +0200

    Add 'subscription-management-redirect-following' feature flag
    A new feature flag 'subscription-management-redirect-following' has been added to all configuration files. This flag allows us to toggle redirection behavior for the subscription management feature. It's initially set to 'false' in all environments except development.json, where it's set to 'true' for testing purposes. Concurrent changes have been made in 'client/reader/following/controller.js' to incorporate the behavior driven by this flag.

--- client/reader/following/controller.js
+++ client/reader/following/controller.js
@@ -1,8 +1,39 @@
+import { isEnabled } from '@automattic/calypso-config';
+import i18n from 'i18n-calypso';
 import page from 'page';
+import AsyncLoad from 'calypso/components/async-load';
+import { sectionify } from 'calypso/lib/route';
+import { trackPageLoad, setPageTitle } from 'calypso/reader/controller-helper';
+
+const analyticsPageTitle = 'Reader';
 
 const exported = {
 	followingManage( context, next ) {
-		page.redirect( '/read/subscriptions' );
+		if ( isEnabled( 'subscription-management-redirect-following' ) ) {
+			page.redirect( '/read/subscriptions' );
+		} else {
+			const basePath = sectionify( context.path );
+			const fullAnalyticsPageTitle = analyticsPageTitle + ' > Manage Followed Sites';
+			const mcKey = 'following_manage';
+			const { q: sitesQuery, s: subsQuery, sort: subsSortOrder, showMoreResults } = context.query;
+
+			setPageTitle( context, i18n.translate( 'Manage followed sites' ) );
+
+			trackPageLoad( basePath, fullAnalyticsPageTitle, mcKey );
+
+			context.primary = (
+				<AsyncLoad
+					require="calypso/reader/following-manage"
+					key="following-manage"
+					initialFollowUrl={ context.query.follow }
+					sitesQuery={ sitesQuery }
+					subsQuery={ subsQuery }
+					showMoreResults={ Boolean( showMoreResults ) }
+					subsSortOrder={ subsSortOrder }
+					context={ context }
+				/>
+			);
+		}
 		next();
 	},
 };
