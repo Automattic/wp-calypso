0c1179796b65f87c904e419cd49e377771a90adf
Tim Broddin <tim@wannabes.be>
2023-07-13 13:42:14 +0200

    Redirect /following/manage to /read/subscriptions

--- client/reader/following/controller.js
+++ client/reader/following/controller.js
@@ -1,37 +1,12 @@
-import i18n from 'i18n-calypso';
-import AsyncLoad from 'calypso/components/async-load';
-import { sectionify } from 'calypso/lib/route';
-import { trackPageLoad, setPageTitle } from 'calypso/reader/controller-helper';
-
-const analyticsPageTitle = 'Reader';
+import page from 'page';
 
 const exported = {
 	followingManage( context, next ) {
-		const basePath = sectionify( context.path );
-		const fullAnalyticsPageTitle = analyticsPageTitle + ' > Manage Followed Sites';
-		const mcKey = 'following_manage';
-		const { q: sitesQuery, s: subsQuery, sort: subsSortOrder, showMoreResults } = context.query;
-
-		setPageTitle( context, i18n.translate( 'Manage followed sites' ) );
-
-		trackPageLoad( basePath, fullAnalyticsPageTitle, mcKey );
-
-		context.primary = (
-			<AsyncLoad
-				require="calypso/reader/following-manage"
-				key="following-manage"
-				initialFollowUrl={ context.query.follow }
-				sitesQuery={ sitesQuery }
-				subsQuery={ subsQuery }
-				showMoreResults={ Boolean( showMoreResults ) }
-				subsSortOrder={ subsSortOrder }
-				context={ context }
-			/>
-		);
+		page.redirect( '/read/subscriptions' );
 		next();
 	},
 };
 
 export default exported;
 
-export const { followingEdit, followingManage } = exported;
+export const { followingManage } = exported;
