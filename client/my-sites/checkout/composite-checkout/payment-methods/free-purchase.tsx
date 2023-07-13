62325a0991d3fdb6896a97f7c95fd9aa736b6931
Payton Swick <payton@foolord.com>
2023-07-13 15:23:09 -0400

    Change free purchase submit button to always be "Complete Checkout"

--- client/my-sites/checkout/composite-checkout/payment-methods/free-purchase.tsx
+++ client/my-sites/checkout/composite-checkout/payment-methods/free-purchase.tsx
@@ -7,7 +7,6 @@ import { useI18n } from '@wordpress/react-i18n';
 import useCartKey from 'calypso/my-sites/checkout/use-cart-key';
 import WordPressLogo from '../components/wordpress-logo';
 import type { PaymentMethod, ProcessPayment } from '@automattic/composite-checkout';
-import type { ResponseCart } from '@automattic/shopping-cart';
 
 export function createFreePaymentMethod(): PaymentMethod {
 	return {
@@ -28,8 +27,6 @@ function FreePurchaseSubmitButton( {
 	onClick?: ProcessPayment;
 } ) {
 	const { formStatus } = useFormStatus();
-	const cartKey = useCartKey();
-	const { responseCart } = useShoppingCart( cartKey );
 
 	// This must be typed as optional because it's injected by cloning the
 	// element in CheckoutSubmitButton, but the uncloned element does not have
@@ -52,18 +49,12 @@ function FreePurchaseSubmitButton( {
 			isBusy={ FormStatus.SUBMITTING === formStatus }
 			fullWidth
 		>
-			<ButtonContents formStatus={ formStatus } responseCart={ responseCart } />
+			<ButtonContents formStatus={ formStatus } />
 		</Button>
 	);
 }
 
-function ButtonContents( {
-	formStatus,
-	responseCart,
-}: {
-	formStatus: FormStatus;
-	responseCart: ResponseCart;
-} ) {
+function ButtonContents( { formStatus }: { formStatus: FormStatus } ) {
 	const { __ } = useI18n();
 
 	if ( formStatus === FormStatus.SUBMITTING ) {
@@ -71,15 +62,6 @@ function ButtonContents( {
 	}
 
 	if ( formStatus === FormStatus.READY ) {
-		if ( doesPurchaseHaveFullCredits( responseCart ) ) {
-			const total = formatCurrency(
-				responseCart.sub_total_with_taxes_integer,
-				responseCart.currency,
-				{ isSmallestUnit: true, stripZeros: true }
-			);
-			/* translators: %s is the total to be paid in localized currency */
-			return <>{ sprintf( __( 'Pay %s with credits' ), total ) }</>;
-		}
 		return <>{ __( 'Complete Checkout' ) }</>;
 	}
 
