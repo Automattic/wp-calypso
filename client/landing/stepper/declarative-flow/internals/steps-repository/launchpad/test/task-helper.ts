50bfeb002e50a09cb261dc7c62e7e1bee419f6a6
edanzer <edanzer@gmail.com>
2023-07-13 09:19:49 -0600

    Revert changes to launchpad tests

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/test/task-helper.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/test/task-helper.ts
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
  */
+import { PLAN_PREMIUM } from '@automattic/calypso-products';
 import { getEnhancedTasks } from '../task-helper';
 import { buildTask } from './lib/fixtures';
 
@@ -15,7 +16,7 @@ describe( 'Task Helpers', () => {
 				];
 				expect(
 					// eslint-disable-next-line @typescript-eslint/no-empty-function
-					getEnhancedTasks( fakeTasks, 'fake.wordpress.com', null, () => {} )
+					getEnhancedTasks( fakeTasks, 'fake.wordpress.com', null, () => {}, false, PLAN_PREMIUM )
 				).toEqual( fakeTasks );
 			} );
 		} );
@@ -30,6 +31,8 @@ describe( 'Task Helpers', () => {
 						null,
 						// eslint-disable-next-line @typescript-eslint/no-empty-function
 						() => {},
+						false,
+						PLAN_PREMIUM,
 						// eslint-disable-next-line @typescript-eslint/no-empty-function
 						() => {},
 						'newsletter',
@@ -49,6 +52,8 @@ describe( 'Task Helpers', () => {
 						null,
 						// eslint-disable-next-line @typescript-eslint/no-empty-function
 						() => {},
+						false,
+						PLAN_PREMIUM,
 						// eslint-disable-next-line @typescript-eslint/no-empty-function
 						() => {},
 						'start-writing'
