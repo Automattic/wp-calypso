6523dbb2b2f7130846a3166a3d862bd5c8774521
Anna McPhee <anna.mcphee@automattic.com>
2023-07-13 17:50:51 -0600

    add banner display to stats-notices index

--- client/my-sites/stats/stats-notices/index.tsx
+++ client/my-sites/stats/stats-notices/index.tsx
@@ -6,6 +6,7 @@ import hasSiteProductJetpackStatsPaid from 'calypso/state/sites/selectors/has-si
 import isJetpackSite from 'calypso/state/sites/selectors/is-jetpack-site';
 import DoYouLoveJetpackStatsNotice from './do-you-love-jetpack-stats-notice';
 import FeedbackNotice from './feedback-notice';
+import FreePlanPurchaseSuccessJetpackStatsNotice from './free-plan-purchase-success-notice';
 import LegacyStatsNotices from './legacy-notices';
 import OptOutNotice from './opt-out-notice';
 import { StatsNoticesProps } from './types';
@@ -33,7 +34,16 @@ const NewStatsNotices = ( { siteId, isOdysseyStats }: StatsNoticesProps ) => {
 
 	return (
 		<>
+<<<<<<< HEAD
 			{ showPaidStatsNotice && <DoYouLoveJetpackStatsNotice siteId={ siteId } /> }
+=======
+			{ config.isEnabled( 'stats/paid-stats' ) && isSiteJetpackNotAtomic && ! hasPaidStats && (
+				<>
+					<DoYouLoveJetpackStatsNotice siteId={ siteId } />
+					<FreePlanPurchaseSuccessJetpackStatsNotice siteId={ siteId } />
+				</>
+			) }
+>>>>>>> 9a4ffb0a11 (add banner display to stats-notices index)
 			{ isOdysseyStats && <OptOutNotice siteId={ siteId } /> }
 			{ isOdysseyStats && <FeedbackNotice siteId={ siteId } /> }
 		</>
