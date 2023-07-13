8118c8b684bb3c3945470d7bb4a4782769573456
sixhours <calobee@gmail.com>
2023-07-13 10:04:20 -0400

    Update branch with BadgeType rather than all BadgeProps

--- client/my-sites/hosting/github/deployment-card/deployment-status-badge.tsx
+++ client/my-sites/hosting/github/deployment-card/deployment-status-badge.tsx
@@ -1,11 +1,7 @@
 import { Badge } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { useMemo } from 'react';
-<<<<<<< HEAD
 import type { BadgeType } from '@automattic/components';
-=======
-import type { BadgeProps } from '@automattic/components';
->>>>>>> 7e2b101506b (Fix type errors)
 
 interface DeploymentStatusBadgeProps {
 	status: string;
@@ -15,11 +11,7 @@ interface DeploymentStatusBadgeProps {
 export const DeploymentStatusBadge = ( { status, totalFailures }: DeploymentStatusBadgeProps ) => {
 	const translate = useTranslate();
 
-<<<<<<< HEAD
 	const { type, message } = useMemo< { type: BadgeType; message: string } >( () => {
-=======
-	const { type, message } = useMemo< { type: BadgeProps[ 'type' ]; message: string } >( () => {
->>>>>>> 7e2b101506b (Fix type errors)
 		if ( status === 'failed' ) {
 			return {
 				type: 'error',
