4b62ce5ab6d2f05d46e1806929529823e2af06b6
Renan Carvalho <renan.carvalho@automattic.com>
2023-07-13 14:48:39 +0200

    Bulk domain transfer: Update/add copies (#79348)

--- client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-complete/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-complete/index.tsx
@@ -69,11 +69,20 @@ const Complete: Step = function Complete( { flow } ) {
 							'Congrats on your domain transfers!',
 							newlyTransferredDomains?.length || storedDomainsAmount
 						) }
-						subHeaderText={ _n(
-							"We've got it from here! We'll let you know when your newly transferred domain is ready to use.",
-							"We've got it from here! We'll let you know when your newly transferred domains are ready to use.",
-							newlyTransferredDomains?.length || storedDomainsAmount
-						) }
+						subHeaderText={
+							<>
+								<span>
+									{ _n(
+										"We got it from here! We'll let you know when your newly transferred domain is ready to use!",
+										"We got it from here! We'll let you know when your newly transferred domains are ready to use!",
+										newlyTransferredDomains?.length || storedDomainsAmount
+									) }
+								</span>
+								<span className="formatted-header-subtitle__bold">
+									{ __( 'Domain transfers may take up to 5-10 days.' ) }
+								</span>
+							</>
+						}
 						align="center"
 						children={
 							<div className="domain-header-buttons">
