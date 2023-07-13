728cf24ae2d2ca952a4d6524035c6b5f4a30738e
Tim Broddin <tim@wannabes.be>
2023-07-13 16:01:16 +0200

    This commit adds new copy for the FormattedHeader of Subscription Management

--- client/landing/subscriptions/hooks/use-subheader-text.tsx
+++ client/landing/subscriptions/hooks/use-subheader-text.tsx
@@ -1,26 +1,43 @@
 import { SubscriptionManager } from '@automattic/data-stores';
+import { useI18n } from '@wordpress/react-i18n';
 import { useTranslate } from 'i18n-calypso';
 import { useMemo } from 'react';
 
 const useSubheaderText = () => {
 	const emailAddress = SubscriptionManager.useSubscriberEmailAddress();
 	const translate = useTranslate();
-
+	const { hasTranslation } = useI18n();
 	return useMemo( () => {
 		if ( emailAddress ) {
-			return translate(
-				"Manage the WordPress.com newsletter and blogs you've subscribed to with {{span}}%(emailAddress)s{{/span}}.",
-				{
-					args: {
-						emailAddress: emailAddress,
-					},
-					components: {
-						span: <span className="email-address" />,
-					},
-				}
-			);
+			return hasTranslation(
+				'Manage WordPress.com blogs and subscriptions you’ve subscribed with {{span}}%(emailAddress)s{{/span}}.'
+			)
+				? translate(
+						'Manage WordPress.com blogs and subscriptions you’ve subscribed with {{span}}%(emailAddress)s{{/span}}.',
+						{
+							args: {
+								emailAddress: emailAddress,
+							},
+							components: {
+								span: <span className="email-address" />,
+							},
+						}
+				  )
+				: translate(
+						"Manage the WordPress.com newsletter and blogs you've subscribed to with {{span}}%(emailAddress)s{{/span}}.",
+						{
+							args: {
+								emailAddress: emailAddress,
+							},
+							components: {
+								span: <span className="email-address" />,
+							},
+						}
+				  );
 		}
-		return translate( 'Manage your WordPress.com blog and newsletter subscriptions.' );
+		return hasTranslation( 'Manage your blog and newsletter subscriptions' )
+			? translate( 'Manage your blog and newsletter subscriptions' )
+			: translate( 'Manage your WordPress.com blog and newsletter subscriptions.' );
 	}, [ emailAddress, translate ] );
 };
 
