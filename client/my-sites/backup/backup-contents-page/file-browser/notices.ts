8050915e77767765b26d9f91db6558735ba7a585
Rafael Agostini <rafael.agostini@automattic.com>
2023-07-14 17:02:19 -0500

    Add notices file in backup browser

---
+++ client/my-sites/backup/backup-contents-page/file-browser/notices.ts
@@ -0,0 +1,15 @@
+import { translate } from 'i18n-calypso';
+import { errorNotice } from 'calypso/state/notices/actions';
+
+const NOTICE_PERSISTENT = true;
+const NOTICE_DURATION = 5000;
+
+export const onPreparingDownloadError = () => {
+	return errorNotice(
+		translate( 'There was an error preparing your download. Please, try again.' ),
+		{
+			duration: NOTICE_DURATION,
+			isPersistent: NOTICE_PERSISTENT,
+		}
+	);
+};
