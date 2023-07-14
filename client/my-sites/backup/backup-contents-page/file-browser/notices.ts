c32972c86056005010a20aeb4542f3bf1e702fdf
Rafael Agostini <rafael.agostini@automattic.com>
2023-07-14 18:15:10 -0500

    Add error handling when retrieving file info

--- client/my-sites/backup/backup-contents-page/file-browser/notices.ts
+++ client/my-sites/backup/backup-contents-page/file-browser/notices.ts
@@ -4,6 +4,16 @@ import { errorNotice } from 'calypso/state/notices/actions';
 const NOTICE_PERSISTENT = true;
 const NOTICE_DURATION = 5000;
 
+export const onRetrievingFileInfoError = () => {
+	return errorNotice(
+		translate( 'There was an error retrieving your file information. Please, try again.' ),
+		{
+			duration: NOTICE_DURATION,
+			isPersistent: NOTICE_PERSISTENT,
+		}
+	);
+};
+
 export const onProcessingDownloadError = () => {
 	return errorNotice(
 		translate( 'There was an error processing your download. Please, try again.' ),
