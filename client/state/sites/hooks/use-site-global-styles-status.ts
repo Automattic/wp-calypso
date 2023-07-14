1e49d09d7a58fb4771b1ba8ec726febe3a85b7e9
mmtr <1233880+mmtr@users.noreply.github.com>
2023-07-14 13:20:49 +0200

    Add last missing Promise.resolve

--- client/state/sites/hooks/use-site-global-styles-status.ts
+++ client/state/sites/hooks/use-site-global-styles-status.ts
@@ -67,15 +67,13 @@ const getGlobalStylesInfoForSite = ( siteId: number | null ): Promise< GlobalSty
 	}
 
 	if ( siteId === null ) {
-		return new Promise( ( resolve ) =>
-			loadExperimentAssignment( 'calypso_global_styles_personal_v2' ).then(
-				( experimentAssignment ) =>
-					resolve( {
-						shouldLimitGlobalStyles: true,
-						globalStylesInUse: false,
-						globalStylesInPersonalPlan: experimentAssignment.variationName === 'treatment',
-					} )
-			)
+		return loadExperimentAssignment( 'calypso_global_styles_personal_v2' ).then(
+			( experimentAssignment ) =>
+				Promise.resolve( {
+					shouldLimitGlobalStyles: true,
+					globalStylesInUse: false,
+					globalStylesInPersonalPlan: experimentAssignment.variationName === 'treatment',
+				} )
 		);
 	}
 
