3fd7da378a81a4f789d7e2e431a3a998b05414d2
James Tien <southp0105@gmail.com>
2023-07-17 15:35:38 +0800

    Remove `isOnboardingMediaFlow` since there is already one, and restructure the theme determining code.

--- packages/onboarding/src/utils/flows.ts
+++ packages/onboarding/src/utils/flows.ts
@@ -100,10 +100,6 @@ export const isCopySiteFlow = ( flowName: string | null ) => {
 	return Boolean( flowName && [ COPY_SITE_FLOW ].includes( flowName ) );
 };
 
-export const isOnboardingMediaFlow = ( flowName?: string | null ) => {
-	return Boolean( flowName && [ ONBOARDING_PM_FLOW ].includes( flowName ) );
-};
-
 export const isWooExpressFlow = ( flowName: string | null ) => {
 	return Boolean( flowName && [ WOOEXPRESS_FLOW ].includes( flowName ) );
 };
