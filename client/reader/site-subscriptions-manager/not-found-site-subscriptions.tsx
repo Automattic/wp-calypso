8322a583137b73e9a42148cc71836e3160a4c12c
Tim Broddin <tim@wannabes.be>
2023-07-17 16:37:50 +0200

    "Handle empty search term in site subscriptions"
    The existing return block of the 'not-found-site-subscriptions' component was missing an edge case for an empty search term. Ensured that a generic 'No results found.' message would be displayed when no search term is input, providing better feedback in the UI. This change makes the search behavior more predictable and user friendly.

--- client/reader/site-subscriptions-manager/not-found-site-subscriptions.tsx
+++ client/reader/site-subscriptions-manager/not-found-site-subscriptions.tsx
@@ -8,14 +8,17 @@ const NotFoundSiteSubscriptions = () => {
 
 	return (
 		<div className="not-found-site-subscriptions">
-			{
-				/* translators: the string is the exact text that the user entered into the search input in site subscriptions manager in Reader */
-				translate( 'No results found for “%s”.', {
-					args: searchTerm,
-					comment:
-						"When users type something into the search field of their site subscriptions manager in Reader, they'll see this message if their search doesn't find any of the websites they're currently subscribed to.",
-				} )
-			}{ ' ' }
+			{ searchTerm
+				? /* translators: the string is the exact text that the user entered into the search input in site subscriptions manager in Reader */
+				  translate( 'No results found for “%s”.', {
+						args: searchTerm,
+						comment:
+							"When users type something into the search field of their site subscriptions manager in Reader, they'll see this message if their search doesn't find any of the websites they're currently subscribed to.",
+				  } )
+				: translate( 'No results found.', {
+						comment:
+							"When users type something into the search field of their site subscriptions manager in Reader, they'll see this message if their search doesn't find any of the websites they're currently subscribed to.",
+				  } ) }{ ' ' }
 			{ ( readFeedSearch?.feedItems.length ?? 0 ) > 0 &&
 				translate( 'Here are some other sites that match your search.' ) }
 		</div>
