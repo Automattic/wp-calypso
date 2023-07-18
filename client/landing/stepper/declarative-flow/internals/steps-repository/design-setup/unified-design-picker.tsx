0db74d6ad769230795ae248acb4239c0dd6dce89
arthur <arthur.chu@automattic.com>
2023-07-18 21:43:15 +0800

    Design Preview: Track colors & fonts selection

--- client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/unified-design-picker.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/design-setup/unified-design-picker.tsx
@@ -57,7 +57,12 @@ import UpgradeModal from './upgrade-modal';
 import getThemeIdFromDesign from './utils/get-theme-id-from-design';
 import type { Step, ProvidedDependencies } from '../../types';
 import './style.scss';
-import type { OnboardSelect, SiteSelect, StarterDesigns } from '@automattic/data-stores';
+import type {
+	OnboardSelect,
+	SiteSelect,
+	StarterDesigns,
+	GlobalStyles,
+} from '@automattic/data-stores';
 import type { Design, StyleVariation } from '@automattic/design-picker';
 import type { GlobalStylesObject } from '@automattic/global-styles';
 
@@ -212,18 +217,18 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 
 	function getEventPropsByDesign(
 		design: Design,
-		styleVariation?: StyleVariation,
-		colorVariation?: GlobalStylesObject | null,
-		fontVariation?: GlobalStylesObject | null
+		options: {
+			styleVariation?: StyleVariation;
+			colorVariation?: GlobalStylesObject | null;
+			fontVariation?: GlobalStylesObject | null;
+		} = {}
 	) {
 		return {
 			...getDesignEventProps( {
+				...options,
 				flow,
 				intent,
 				design,
-				styleVariation,
-				colorVariation,
-				fontVariation,
 			} ),
 			category: categorization.selection,
 			...( design.recipe?.pattern_ids && { pattern_ids: design.recipe.pattern_ids.join( ',' ) } ),
@@ -243,13 +248,13 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 	function recordPreviewStyleVariation( design: Design, styleVariation?: StyleVariation ) {
 		recordTracksEvent(
 			'calypso_signup_design_preview_style_variation_preview_click',
-			getEventPropsByDesign( design, styleVariation )
+			getEventPropsByDesign( design, { styleVariation } )
 		);
 	}
 
 	function onChangeVariation( design: Design, styleVariation?: StyleVariation ) {
 		recordTracksEvent( 'calypso_signup_design_picker_style_variation_button_click', {
-			...getEventPropsByDesign( design, styleVariation ),
+			...getEventPropsByDesign( design, { styleVariation } ),
 			...getVirtualDesignProps( design ),
 		} );
 	}
@@ -263,6 +268,22 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 
 	function recordDesignPreviewSelectScreen( screenSlug: string ) {
 		recordTracksEvent( 'calypso_signup_design_preview_screen_select', { name: screenSlug } );
+	}
+
+	function handleSelectColorVariation( colorVariation: GlobalStyles | null ) {
+		setSelectedColorVariation( colorVariation );
+		recordTracksEvent(
+			'calypso_signup_design_preview_color_variation_preview_click',
+			getEventPropsByDesign( selectedDesign!, { colorVariation } )
+		);
+	}
+
+	function handleSelectFontVariation( fontVariation: GlobalStyles | null ) {
+		setSelectedFontVariation( fontVariation );
+		recordTracksEvent(
+			'calypso_signup_design_preview_font_variation_preview_click',
+			getEventPropsByDesign( selectedDesign!, { fontVariation } )
+		);
 	}
 
 	// ********** Logic for unlocking a selected premium design
@@ -306,7 +327,11 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 		if ( selectedDesign ) {
 			recordTracksEvent(
 				'calypso_signup_design_preview_unlock_theme_click',
-				getEventPropsByDesign( selectedDesign, selectedStyleVariation )
+				getEventPropsByDesign( selectedDesign, {
+					styleVariation: selectedStyleVariation,
+					colorVariation: selectedColorVariation,
+					fontVariation: selectedFontVariation,
+				} )
 			);
 		}
 
@@ -361,7 +386,11 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 		if ( selectedDesign && numOfSelectedGlobalStyles ) {
 			recordTracksEvent(
 				'calypso_signup_design_global_styles_gating_modal_show',
-				getEventPropsByDesign( selectedDesign, selectedStyleVariation )
+				getEventPropsByDesign( selectedDesign, {
+					styleVariation: selectedStyleVariation,
+					colorVariation: selectedColorVariation,
+					fontVariation: selectedFontVariation,
+				} )
 			);
 			setShowPremiumGlobalStylesModal( true );
 		}
@@ -372,7 +401,11 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 		if ( selectedDesign && numOfSelectedGlobalStyles ) {
 			recordTracksEvent(
 				'calypso_signup_design_global_styles_gating_modal_close_button_click',
-				getEventPropsByDesign( selectedDesign, selectedStyleVariation )
+				getEventPropsByDesign( selectedDesign, {
+					styleVariation: selectedStyleVariation,
+					colorVariation: selectedColorVariation,
+					fontVariation: selectedFontVariation,
+				} )
 			);
 			setShowPremiumGlobalStylesModal( false );
 		}
@@ -383,7 +416,11 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 		if ( selectedDesign && numOfSelectedGlobalStyles && siteSlugOrId ) {
 			recordTracksEvent(
 				'calypso_signup_design_global_styles_gating_modal_checkout_button_click',
-				getEventPropsByDesign( selectedDesign, selectedStyleVariation )
+				getEventPropsByDesign( selectedDesign, {
+					styleVariation: selectedStyleVariation,
+					colorVariation: selectedColorVariation,
+					fontVariation: selectedFontVariation,
+				} )
 			);
 
 			goToCheckout( {
@@ -404,7 +441,11 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 		if ( selectedDesign && numOfSelectedGlobalStyles ) {
 			recordTracksEvent(
 				'calypso_signup_design_global_styles_gating_modal_try_button_click',
-				getEventPropsByDesign( selectedDesign, selectedStyleVariation )
+				getEventPropsByDesign( selectedDesign, {
+					styleVariation: selectedStyleVariation,
+					colorVariation: selectedColorVariation,
+					fontVariation: selectedFontVariation,
+				} )
 			);
 			pickDesign();
 		}
@@ -498,7 +539,11 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 		if ( isPreviewingDesign ) {
 			recordTracksEvent(
 				'calypso_signup_design_preview_exit',
-				getEventPropsByDesign( selectedDesign as Design, selectedStyleVariation )
+				getEventPropsByDesign( selectedDesign as Design, {
+					styleVariation: selectedStyleVariation,
+					colorVariation: selectedColorVariation,
+					fontVariation: selectedFontVariation,
+				} )
 			);
 
 			resetPreview();
@@ -615,9 +660,9 @@ const UnifiedDesignPickerStep: Step = ( { navigation, flow, stepName } ) => {
 					stylesheet={ selectedDesign.recipe?.stylesheet }
 					isVirtual={ selectedDesign.is_virtual }
 					selectedColorVariation={ selectedColorVariation }
-					onSelectColorVariation={ setSelectedColorVariation }
+					onSelectColorVariation={ handleSelectColorVariation }
 					selectedFontVariation={ selectedFontVariation }
-					onSelectFontVariation={ setSelectedFontVariation }
+					onSelectFontVariation={ handleSelectFontVariation }
 					onGlobalStylesChange={ setGlobalStyles }
 					onNavigatorPathChange={ ( path?: string ) => setShouldHideActionButtons( path !== '/' ) }
 					onSelectScreen={ recordDesignPreviewSelectScreen }
