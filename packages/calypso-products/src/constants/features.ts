42327ec5c2d06480318e370db5ed8364f9541217
Luiz Kowalski <lhkowalski@gmail.com>
2023-07-18 10:07:19 -0300

    Add-Ons: Hide Jetpack AI add-on card for sites that already support the feature (#79495)
    * Use the site active features to decide if the add-on should be showed
    
    * Use a constant instead of the literal feature name
    
    * Move feature flag to staging level, so we can test it easily

--- packages/calypso-products/src/constants/features.ts
+++ packages/calypso-products/src/constants/features.ts
@@ -237,6 +237,7 @@ export const FEATURE_WOOCOMMERCE = 'woocommerce';
 export const FEATURE_SOCIAL_MEDIA_TOOLS = 'social-media-tools';
 
 // From class-wpcom-features.php in WPCOM
+export const WPCOM_FEATURES_AI_ASSISTANT = 'ai-assistant';
 export const WPCOM_FEATURES_AKISMET = 'akismet';
 export const WPCOM_FEATURES_ANTISPAM = 'antispam';
 export const WPCOM_FEATURES_ATOMIC = 'atomic';
