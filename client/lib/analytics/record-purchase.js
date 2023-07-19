9b3f7681553688fcbea3f23dc0497feeac46663d
Jeremy Yip <jeremy.yip@automattic.com>
2023-07-19 09:52:26 -0700

    Google Ads: Add delay to conversion event to avoid request cancellation (#79500)
    After the recordPurchase analytics method is called, there is a page redirect. This will cancel the google analytics conversion event because the redirect happens so quickly. We add a delay to provide time for the gtag event to resolve.

--- client/lib/analytics/record-purchase.js
+++ client/lib/analytics/record-purchase.js
@@ -5,7 +5,7 @@ import { debug, TRACKING_IDS } from './ad-tracking/constants';
 import { gaRecordEvent } from './ga';
 import { mayWeTrackByTracker } from './tracker-buckets';
 
-export function recordPurchase( { cart, orderId, sitePlanSlug } ) {
+export async function recordPurchase( { cart, orderId, sitePlanSlug } ) {
 	if ( cart.total_cost >= 0.01 ) {
 		const usdValue = costToUSD( cart.total_cost, cart.currency );
 
@@ -32,5 +32,10 @@ export function recordPurchase( { cart, orderId, sitePlanSlug } ) {
 		];
 		debug( 'recordOrderInGoogleAds: WPCom Domain Transfer Purchase', params );
 		window.gtag( ...params );
+
+		// The page redirect to the domain transfer success page happens too quickly and
+		// cancels the conversion event before it's fired. We delay execution by 1 second
+		// to ensure that the conversion event is recorded
+		return new Promise( ( resolve ) => setTimeout( resolve, 1000 ) );
 	}
 }
