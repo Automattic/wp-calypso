8b9d507c72c866e76e5eaf9e8c10ae96c57fa3a6
Payton Swick <payton@foolord.com>
2023-07-19 14:44:39 -0400

    Make loading full-screen

--- client/my-sites/checkout/composite-checkout/components/checkout-submitting-full-page.tsx
+++ client/my-sites/checkout/composite-checkout/components/checkout-submitting-full-page.tsx
@@ -1,5 +1,30 @@
-import { LoadingContent } from '@automattic/composite-checkout';
+import { styled } from '@automattic/wpcom-checkout';
+import { useTranslate } from 'i18n-calypso';
 import { useEffect, useState } from 'react';
+import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
+
+const SubmittingWrapper = styled.div`
+	position: absolute;
+	left: 0;
+	top: 0;
+	background: white;
+	height: 100%;
+	width: 100%;
+	z-index: 100000;
+	margin: 0;
+	padding: 1em;
+	padding-top: 32vh;
+	text-align: center;
+`;
+
+const SubmittingTitle = styled.div`
+	font-family: 'Recoleta', 'Noto Serif', Georgia, 'Times New Roman', Times, serif;
+	font-size: 1.625rem;
+	line-height: 40px;
+	text-align: center;
+	vertical-align: middle;
+	margin: 0;
+`;
 
 export function CheckoutSubmittingFullPage() {
 	const showLoadingInfoThresholdMs = 2000;
@@ -12,17 +37,18 @@ export function CheckoutSubmittingFullPage() {
 			clearTimeout( timer );
 		};
 	}, [] );
+	const translate = useTranslate();
 
 	if ( ! shouldShowLoadingInfo ) {
 		return null;
 	}
 
 	return (
-		<>
-			<div>
-				<h2>Still submitting, please wait…</h2>
-			</div>
-			<LoadingContent />
-		</>
+		<SubmittingWrapper>
+			<SubmittingTitle>
+				{ translate( "Almost there – we're currently finalizing your order." ) }
+			</SubmittingTitle>
+			<LoadingEllipsis />
+		</SubmittingWrapper>
 	);
 }
