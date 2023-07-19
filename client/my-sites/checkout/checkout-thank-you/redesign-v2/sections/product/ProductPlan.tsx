187d8baf2f8789ca649407649361d9080236b8be
Candy Tsai <candy02058912@gmail.com>
2023-07-19 17:32:45 +0800

    Add tracks and admin url for ecommerce

--- client/my-sites/checkout/checkout-thank-you/redesign-v2/sections/product/ProductPlan.tsx
+++ client/my-sites/checkout/checkout-thank-you/redesign-v2/sections/product/ProductPlan.tsx
@@ -1,7 +1,9 @@
+import { isEcommercePlan } from '@automattic/calypso-products';
 import { Button, Spinner } from '@automattic/components';
 import { translate } from 'i18n-calypso';
 import moment from 'moment';
 import { useEffect, useMemo, useState } from 'react';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { getPurchaseByProductSlug } from 'calypso/lib/purchases/utils';
 import { useSelector } from 'calypso/state';
 import {
@@ -10,6 +12,7 @@ import {
 	isFetchingSitePurchases,
 } from 'calypso/state/purchases/selectors';
 import { ReceiptPurchase } from 'calypso/state/receipts/types';
+import { getSiteWooCommerceUrl } from 'calypso/state/sites/selectors';
 
 type ProductPlanProps = {
 	siteSlug: string;
@@ -26,6 +29,12 @@ const ProductPlan = ( { siteSlug, primaryPurchase, siteID }: ProductPlanProps ) 
 	const productPurchase = useMemo(
 		() => getPurchaseByProductSlug( purchases, primaryPurchase.productSlug ),
 		[ primaryPurchase.productSlug, purchases ]
+	);
+	// TODO: Move data logic out of the component.
+	const siteHomeUrl = useSelector( ( state ) =>
+		isEcommercePlan( primaryPurchase.productSlug )
+			? getSiteWooCommerceUrl( state, siteID ) ?? `/home/${ siteSlug }`
+			: `/home/${ siteSlug }`
 	);
 
 	useEffect( () => {
@@ -59,10 +68,19 @@ const ProductPlan = ( { siteSlug, primaryPurchase, siteID }: ProductPlanProps ) 
 				) }
 			</div>
 			<div className="checkout-thank-you__header-details-buttons">
-				<Button primary href={ `/home/${ siteSlug }` }>
+				<Button
+					primary
+					href={ siteHomeUrl }
+					onClick={ () => recordTracksEvent( 'calypso_plan_thank_you_home_cta_click' ) }
+				>
 					{ translate( 'Let’s work on the site' ) }
 				</Button>
-				<Button href={ `/plans/my-plan/${ siteSlug }` }>{ translate( 'Manage plan' ) }</Button>
+				<Button
+					href={ `/plans/my-plan/${ siteSlug }` }
+					onClick={ () => recordTracksEvent( 'calypso_plan_thank_you_plan_click' ) }
+				>
+					{ translate( 'Manage plan' ) }
+				</Button>
 			</div>
 		</div>
 	);
