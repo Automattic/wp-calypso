a11a6d75cbae2074979263d1812506ff8fa5935f
sixhours <calobee@gmail.com>
2023-07-19 11:22:15 -0400

    Update types export, revert change to type requirement

--- packages/launchpad/src/index.ts
+++ packages/launchpad/src/index.ts
@@ -2,4 +2,4 @@ export { default as Checklist } from './checklist';
 export { default as ChecklistItem } from './checklist-item';
 export { default as Launchpad } from './launchpad';
 export { setUpActionsForTasks } from './setup-actions';
-export * from './types';
+export type * from './types';
--- packages/launchpad/src/types.ts
+++ packages/launchpad/src/types.ts
@@ -1,7 +1,7 @@
 export interface Task {
 	id: string;
-	completed?: boolean;
-	disabled?: boolean;
+	completed: boolean;
+	disabled: boolean;
 	title?: string;
 	subtitle?: string | React.ReactNode | null;
 	badge_text?: string;
