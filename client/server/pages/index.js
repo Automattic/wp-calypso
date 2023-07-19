133be561bff709ec166197784854e43ca4e9e4d4
escapemanuele <escapemanuele@gmail.com>
2023-07-19 18:00:08 +0200

    Redirect /help/courses

--- client/server/pages/index.js
+++ client/server/pages/index.js
@@ -832,6 +832,11 @@ function wpcomPages( app ) {
 		const redirectUrl = '/setup/domain-transfer';
 		res.redirect( 301, redirectUrl );
 	} );
+
+	app.get( '/help/courses', function ( req, res ) {
+		const redirectUrl = 'https://wordpress.com/learn/courses';
+		res.redirect( 301, redirectUrl );
+	} );
 }
 
 export default function pages() {
