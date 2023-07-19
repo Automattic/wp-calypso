9653c9b2adba09f458ddd49a42b4537801a893ae
Anna McPhee <anna.mcphee@automattic.com>
2023-07-18 23:03:48 -0600

    remove unneeded display and dismissal logic from notices

--- client/my-sites/stats/stats-notices/free-plan-purchase-success-notice.tsx
+++ client/my-sites/stats/stats-notices/free-plan-purchase-success-notice.tsx
@@ -2,8 +2,6 @@ import config from '@automattic/calypso-config';
 import NoticeBanner from '@automattic/components/src/notice-banner';
 import { useTranslate } from 'i18n-calypso';
 import { useState } from 'react';
-import useNoticeVisibilityMutation from 'calypso/my-sites/stats/hooks/use-notice-visibility-mutation';
-import useNoticeVisibilityQuery from 'calypso/my-sites/stats/hooks/use-notice-visibility-query';
 import { StatsNoticeProps } from './types';
 
 const getStatsPurchaseURL = ( siteId: number | null ) => {
@@ -18,21 +16,13 @@ const getStatsPurchaseURL = ( siteId: number | null ) => {
 const FreePlanPurchaseSuccessJetpackStatsNotice = ( { siteId }: StatsNoticeProps ) => {
 	const translate = useTranslate();
 	const [ noticeDismissed, setNoticeDismissed ] = useState( false );
-	const { data: showNotice } = useNoticeVisibilityQuery( siteId, 'free_plan_purchase_success' );
-	const { mutateAsync: postponeNoticeAsync } = useNoticeVisibilityMutation(
-		siteId,
-		'free_plan_purchase_success',
-		'postponed',
-		30 * 24 * 3600
-	);
 
 	const dismissNotice = () => {
 		setNoticeDismissed( true );
-		postponeNoticeAsync();
 	};
 
-	if ( noticeDismissed || ! showNotice ) {
-		// return null; this is for testing purposes. TODO uncomment
+	if ( noticeDismissed ) {
+		return null;
 	}
 
 	return (
--- client/my-sites/stats/stats-notices/paid-plan-purchase-success-notice.tsx
+++ client/my-sites/stats/stats-notices/paid-plan-purchase-success-notice.tsx
@@ -2,30 +2,17 @@
 import NoticeBanner from '@automattic/components/src/notice-banner';
 import { useTranslate } from 'i18n-calypso';
 import { useState } from 'react';
-import useNoticeVisibilityMutation from 'calypso/my-sites/stats/hooks/use-notice-visibility-mutation';
-import useNoticeVisibilityQuery from 'calypso/my-sites/stats/hooks/use-notice-visibility-query';
-import { StatsNoticeProps } from './types';
 
-const PaidPlanPurchaseSuccessJetpackStatsNotice = ( { siteId }: StatsNoticeProps ) => {
+const PaidPlanPurchaseSuccessJetpackStatsNotice = () => {
 	const translate = useTranslate();
 	const [ noticeDismissed, setNoticeDismissed ] = useState( false );
-	const { data: showNotice } = useNoticeVisibilityQuery( siteId, 'paid_plan_purchase_success' );
-	const { mutateAsync: postponeNoticeAsync } = useNoticeVisibilityMutation(
-		siteId,
-		'paid_plan_purchase_success',
-		'postponed',
-		30 * 24 * 3600
-	);
 
 	const dismissNotice = () => {
 		setNoticeDismissed( true );
-		postponeNoticeAsync();
 	};
 
-	if ( noticeDismissed || ! showNotice ) {
-		console.log( 'paidNotice noticeDismissed', noticeDismissed );
-		console.log( 'paidNotice showNotice', showNotice );
-		// return null;
+	if ( noticeDismissed ) {
+		return null;
 	}
 
 	return (
