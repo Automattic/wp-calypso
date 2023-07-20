687a3c33ca9f6ae2f695e35bed4fc8f6948838f8
Emanuele Buccelli <escapemanuele@gmail.com>
2023-07-20 15:33:13 +0200

    Global content footer: update copy and style (#79524)
    * update global content footer
    
    * Apply feedback

--- apps/happy-blocks/block-library/support-content-footer/index.php
+++ apps/happy-blocks/block-library/support-content-footer/index.php
@@ -18,10 +18,10 @@ $image_dir = 'https://wordpress.com/wp-content/a8c-plugins/happy-blocks/block-li
 	<div class="support-content-cta">
 		<div class="support-content-cta-left">
 			<h4 class="support-content-resource__title">
-				<?php esc_html_e( 'Let us build your website', 'happy-blocks' ); ?>
+				<?php esc_html_e( 'Your site, built for you', 'happy-blocks' ); ?>
 			</h4>
 			<p>
-				<?php esc_html_e( 'Our professional website-building service can create the site of your dreams, no matter the scope of your project - from small websites and personal blogs to large-scale custom development and migrations.', 'happy-blocks' ); ?>
+				<?php esc_html_e( 'Sit back as our Built by WordPress.com team of experts builds a site you\'ll fall in love with. From single page sites to full stores, they\'ll help you make it happen.', 'happy-blocks' ); ?>
 			</p>
 			<div class="resource-link">
 				<a href="<?php echo esc_url( 'https://wordpress.com/website-design-service/?ref=banner-learn' ); ?>">
@@ -97,9 +97,9 @@ $image_dir = 'https://wordpress.com/wp-content/a8c-plugins/happy-blocks/block-li
 			</p>
 		</div>
 		<div class="support-content-subscribe">
-			<p><?php esc_html_e( 'Sign up for educational resources updates:', 'happy-blocks' ); ?></p>
+			<p><?php esc_html_e( 'Get the latest learning in your inbox::', 'happy-blocks' ); ?></p>
 			<form action="https://subscribe.wordpress.com" method="post" accept-charset="utf-8" data-blog="<?php echo get_current_blog_id(); ?>" data-post_access_level="everybody" id="subscribe-blog">
-				<input class="support-content-subscribe-email" required="required" type="email" name="email" placeholder="<?php esc_html_e( 'Enter your best email', 'happy-blocks' ); ?>"  id="subscribe-field">
+				<input class="support-content-subscribe-email" required="required" type="email" name="email" placeholder="<?php esc_html_e( 'Type your email', 'happy-blocks' ); ?>"  id="subscribe-field">
 				<input type="hidden" name="action" value="subscribe">
 				<input type="hidden" name="blog_id" value="<?php echo get_current_blog_id(); ?>">
 				<input type="hidden" name="sub-type" value="subscribe-block">
