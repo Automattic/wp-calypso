4f8d772358449cf6332fd237ddef150f97616034
Candy Tsai <candy02058912@gmail.com>
2023-07-20 17:04:36 +0800

    Add presales chat bubble to domain transfer

--- client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-domains/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-domains/index.tsx
@@ -2,6 +2,7 @@ import { useI18n } from '@wordpress/react-i18n';
 import { StepContainer } from 'calypso/../packages/onboarding/src';
 import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { usePresalesChat } from 'calypso/lib/presales-chat';
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 import TransferDomains from './domains';
 import type { Step } from '../../types';
@@ -11,6 +12,8 @@ import './styles.scss';
 const Intro: Step = function Intro( { navigation, flow } ) {
 	const { submit, goBack } = navigation;
 	const { __ } = useI18n();
+
+	usePresalesChat( 'wpcom' );
 
 	const handleSubmit = () => {
 		submit?.();
