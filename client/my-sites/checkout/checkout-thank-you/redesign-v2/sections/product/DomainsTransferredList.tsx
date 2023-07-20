19e4c65d2e283cbca09e26ab73d17c6f71b47dc1
escapemanuele <escapemanuele@gmail.com>
2023-07-20 13:45:21 +0200

    Removed reset store

--- client/my-sites/checkout/checkout-thank-you/redesign-v2/sections/product/DomainsTransferredList.tsx
+++ client/my-sites/checkout/checkout-thank-you/redesign-v2/sections/product/DomainsTransferredList.tsx
@@ -1,7 +1,5 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
-import { useDispatch as useWpDataDispatch } from '@wordpress/data';
 import { useI18n } from '@wordpress/react-i18n';
-import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import type { ReceiptPurchase } from 'calypso/state/receipts/types';
 import './style.scss';
 
@@ -11,7 +9,6 @@ type Props = {
 
 const DomainsTransferredList = ( { purchases }: Props ) => {
 	const { __, _n } = useI18n();
-	const { resetOnboardStore } = useWpDataDispatch( ONBOARD_STORE );
 
 	const handleUserClick = ( destination: string ) => {
 		recordTracksEvent( 'calypso_domain_transfer_complete_click', {
@@ -19,19 +16,12 @@ const DomainsTransferredList = ( { purchases }: Props ) => {
 		} );
 	};
 
-	const clearDomainsStore = () => {
-		recordTracksEvent( 'calypso_domain_transfer_complete_click', {
-			destination: '/setup/domain-transfer',
-		} );
-		resetOnboardStore();
-	};
-
 	return (
 		<>
 			<div className="domain-header-buttons">
 				<a
 					href="/setup/domain-transfer"
-					onClick={ clearDomainsStore }
+					onClick={ () => handleUserClick( '/setup/domain-transfer' ) }
 					className="components-button is-secondary"
 				>
 					{ __( 'Transfer more domains' ) }
