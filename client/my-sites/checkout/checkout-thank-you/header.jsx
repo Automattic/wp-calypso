bda9175954511b7a0a062fe8b89f7b47fecc7099
Eric Binnion <ericbinnion@gmail.com>
2023-07-21 17:30:36 -0500

    Domain Transfer: Update header copy to improve alignment with content below

--- client/my-sites/checkout/checkout-thank-you/header.jsx
+++ client/my-sites/checkout/checkout-thank-you/header.jsx
@@ -102,8 +102,8 @@ export class CheckoutThankYouHeader extends PureComponent {
 				<>
 					<div>
 						{ _n(
-							"We got it from here! We'll send an email when your newly transferred domain is ready to use.",
-							"We got it from here! We'll send an email when your newly transferred domains are ready to use.",
+							"We got it from here! We'll send an email when your domain is ready to use.",
+							"We got it from here! We'll send an email when your domains are ready to use.",
 							purchases?.length
 						) }
 					</div>
