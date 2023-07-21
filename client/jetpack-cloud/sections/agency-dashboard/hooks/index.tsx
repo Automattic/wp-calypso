94bed65560c11ad08060e9bbfb4acada1552984f
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-07-21 08:52:30 -0500

    useDashboardAddRemoveLicense: move to separate file, keep index export

--- client/jetpack-cloud/sections/agency-dashboard/hooks/index.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/hooks/index.tsx
@@ -1,29 +1,10 @@
 import { useCallback, useState, useRef } from 'react';
-import { useDispatch, useSelector } from 'calypso/state';
-import { selectLicense, unselectLicense } from 'calypso/state/jetpack-agency-dashboard/actions';
-import { hasSelectedLicensesOfType } from 'calypso/state/jetpack-agency-dashboard/selectors';
-import type { AllowedTypes } from '../sites-overview/types';
 
 export { default as useToggleActivateMonitor } from './use-toggle-activate-monitor';
 export { default as useUpdateMonitorSettings } from './use-update-monitor-settings';
 export { default as useJetpackAgencyDashboardRecordTrackEvent } from './use-jetpack-agency-dashboard-record-track-event';
 export { default as useDashboardShowLargeScreen } from './use-dashboard-show-large-screen';
-
-export const useDashboardAddRemoveLicense = ( siteId: number, type: AllowedTypes ) => {
-	const dispatch = useDispatch();
-
-	const isLicenseSelected = useSelector( ( state ) =>
-		hasSelectedLicensesOfType( state, siteId, type )
-	);
-
-	const handleAddLicenseAction = () => {
-		isLicenseSelected
-			? dispatch( unselectLicense( siteId, type ) )
-			: dispatch( selectLicense( siteId, type ) );
-	};
-
-	return { isLicenseSelected, handleAddLicenseAction };
-};
+export { default as useDashboardAddRemoveLicense } from './use-dashboard-add-remove-license';
 
 const TIMEOUT_DURATION = 10000;
 
