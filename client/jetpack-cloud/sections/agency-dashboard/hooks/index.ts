c02d538a6afe60961a4685f373fa32c34fafbbe7
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-07-21 08:59:41 -0500

    useDashboardShowLargeScreen: move definition closer to sole usage, remove from shared index

--- client/jetpack-cloud/sections/agency-dashboard/hooks/index.ts
+++ client/jetpack-cloud/sections/agency-dashboard/hooks/index.ts
@@ -1,6 +1,5 @@
 export { default as useToggleActivateMonitor } from './use-toggle-activate-monitor';
 export { default as useUpdateMonitorSettings } from './use-update-monitor-settings';
 export { default as useJetpackAgencyDashboardRecordTrackEvent } from './use-jetpack-agency-dashboard-record-track-event';
-export { default as useDashboardShowLargeScreen } from './use-dashboard-show-large-screen';
 export { default as useDashboardAddRemoveLicense } from './use-dashboard-add-remove-license';
 export { default as useShowVerifiedBadge } from './use-show-verified-badge';
