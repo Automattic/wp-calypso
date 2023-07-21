a21b620fe586c3db6776f1443cd54f80fd6a080b
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-07-21 08:48:48 -0500

    useJetpackAgencyDashboardRecordTrackEvent: move to separate file, keep index export

--- client/jetpack-cloud/sections/agency-dashboard/hooks/index.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/hooks/index.tsx
@@ -1,61 +1,15 @@
 import { useBreakpoint } from '@automattic/viewport-react';
 import { useCallback, useState, useEffect, RefObject, useRef } from 'react';
 import { useDispatch, useSelector } from 'calypso/state';
-import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { selectLicense, unselectLicense } from 'calypso/state/jetpack-agency-dashboard/actions';
 import { hasSelectedLicensesOfType } from 'calypso/state/jetpack-agency-dashboard/selectors';
-import type { AllowedTypes, Site } from '../sites-overview/types';
+import type { AllowedTypes } from '../sites-overview/types';
 
 const DESKTOP_BREAKPOINT = '>1280px';
 
 export { default as useToggleActivateMonitor } from './use-toggle-activate-monitor';
 export { default as useUpdateMonitorSettings } from './use-update-monitor-settings';
-
-export function useJetpackAgencyDashboardRecordTrackEvent(
-	sites: Array< Site > | null,
-	isLargeScreen?: boolean
-) {
-	const dispatch = useDispatch();
-
-	const buildEventName = useCallback(
-		( action: string ) =>
-			`calypso_jetpack_agency_dashboard_${ action }_${
-				isLargeScreen ? 'large_screen' : 'small_screen'
-			}`,
-		[ isLargeScreen ]
-	);
-
-	const buildSiteProperties = useCallback( () => {
-		if ( ! sites?.length ) {
-			return {};
-		}
-		if ( sites.length === 1 ) {
-			const { blog_id, url } = sites[ 0 ];
-			return {
-				selected_site_id: blog_id,
-				selected_site_url: url,
-			};
-		}
-		return {
-			selected_site_count: sites.length,
-		};
-	}, [ sites ] );
-
-	const dispatchTrackingEvent = useCallback(
-		( action: string, args = {} ) => {
-			const name = buildEventName( action );
-
-			const properties = {
-				...buildSiteProperties(),
-				...args,
-			};
-			dispatch( recordTracksEvent( name, properties ) );
-		},
-		[ buildEventName, buildSiteProperties, dispatch ]
-	);
-
-	return dispatchTrackingEvent;
-}
+export { default as useJetpackAgencyDashboardRecordTrackEvent } from './use-jetpack-agency-dashboard-record-track-event';
 
 export const useDashboardShowLargeScreen = (
 	siteTableRef: RefObject< HTMLTableElement >,
