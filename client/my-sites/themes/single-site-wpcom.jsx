625c326466576a13cb0bf23c96dd4853593949cf
mmtr <1233880+mmtr@users.noreply.github.com>
2023-07-21 16:51:50 +0200

    Use new feature

--- client/my-sites/themes/single-site-wpcom.jsx
+++ client/my-sites/themes/single-site-wpcom.jsx
@@ -5,7 +5,7 @@ import {
 	PLAN_PERSONAL,
 	PLAN_PREMIUM,
 	PLAN_BUSINESS,
-	FEATURE_PREMIUM_THEMES,
+	FEATURE_PREMIUM_THEMES_V2,
 } from '@automattic/calypso-products';
 import { connect } from 'react-redux';
 import UpsellNudge from 'calypso/blocks/upsell-nudge';
@@ -35,7 +35,7 @@ const ConnectedSingleSiteWpcom = connectOptions( ( props ) => {
 					<UpsellNudge
 						className="themes__showcase-banner"
 						event="calypso_themes_list_premium_themes"
-						feature={ FEATURE_PREMIUM_THEMES }
+						feature={ FEATURE_PREMIUM_THEMES_V2 }
 						plan={ PLAN_PREMIUM }
 						title={ translate( 'Unlock premium themes with our Premium and Business plans!' ) }
 						callToAction={ translate( 'Upgrade now' ) }
