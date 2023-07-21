f1a3a4544483e6732a665b9828f2525732156016
leomattic <leo.franco@automattic.com>
2023-07-20 19:29:50 -0500

    remove debug message

--- packages/calypso-analytics/src/tracks.ts
+++ packages/calypso-analytics/src/tracks.ts
@@ -41,7 +41,6 @@ let _superProps: any; // Added to all Tracks events.
 let _loadTracksResult = Promise.resolve(); // default value for non-BOM environments.
 
 if ( typeof document !== 'undefined' ) {
-	debug( 'Loading Tracks script: stats.wp.com/w.js?63' );
 	_loadTracksResult = loadScript( '//stats.wp.com/w.js?63' );
 }
 
