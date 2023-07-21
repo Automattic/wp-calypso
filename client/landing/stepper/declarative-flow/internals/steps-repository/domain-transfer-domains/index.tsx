c05788a3cf876574a07d78b62e77bc8c52880286
Paulo Trentin <paulo@paulotrentin.com.br>
2023-07-21 17:02:27 -0300

    Added isEnglishLocale condition to translated subheader

--- client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-domains/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-domains/index.tsx
@@ -1,3 +1,4 @@
+import { useIsEnglishLocale } from '@automattic/i18n-utils';
 import { hasTranslation } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import { StepContainer } from 'calypso/../packages/onboarding/src';
@@ -12,20 +13,22 @@ import './styles.scss';
 const Intro: Step = function Intro( { navigation, flow } ) {
 	const { submit, goBack } = navigation;
 	const { __ } = useI18n();
+	const isEnglishLocale = useIsEnglishLocale();
 
 	const handleSubmit = () => {
 		submit?.();
 	};
 
-	const getTranslatedSubHeaderText = hasTranslation(
-		'Enter your domain names and authorization codes below. You can transfer up to fifty domains at a time.'
-	)
-		? __(
-				'Enter your domain names and authorization codes below. You can transfer up to fifty domains at a time.'
-		  )
-		: __(
-				'Enter your domain names and authorization codes below. You can transfer up to 50 domains at a time.'
-		  );
+	const getTranslatedSubHeaderText =
+		hasTranslation(
+			'Enter your domain names and authorization codes below. You can transfer up to fifty domains at a time.'
+		) || isEnglishLocale
+			? __(
+					'Enter your domain names and authorization codes below. You can transfer up to fifty domains at a time.'
+			  )
+			: __(
+					'Enter your domain names and authorization codes below. You can transfer up to 50 domains at a time.'
+			  );
 
 	return (
 		<StepContainer
