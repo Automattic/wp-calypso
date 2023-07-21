976b44cac6cd591b8c68b613b9163a6f5a77c5e6
Yashwin <yashwinpoojary@gmail.com>
2023-07-21 08:32:09 +0530

    Address PR comments

--- client/jetpack-cloud/sections/hooks/use-show-banners.ts
+++ client/jetpack-cloud/sections/hooks/use-show-banners.ts
@@ -1,6 +1,8 @@
 import { ReactElement, useCallback } from 'react';
 import type { Preference } from 'calypso/jetpack-cloud/sections/agency-dashboard/sites-overview/types';
 
+const ONE_DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
+
 interface Banner {
 	component: () => ReactElement;
 	preference: Preference;
@@ -10,13 +12,13 @@ interface Banner {
 
 // Hook to show banner based on priority and preference(dismiss/view date)
 export const useShowBanners = ( banners: Array< Banner > ) => {
-	const showBanner = useCallback( ( banner: Banner ) => {
+	const shouldShowBanner = useCallback( ( banner: Banner ) => {
 		if ( ! banner.preference?.view_date ) {
 			return true;
 		}
 		const currentDate = new Date() as unknown as number;
 		const daysDifference = Math.floor(
-			( currentDate - Number( banner.preference.view_date ) ) / ( 1000 * 60 * 60 * 24 )
+			( currentDate - Number( banner.preference.view_date ) ) / ONE_DAY_IN_MILLISECONDS
 		);
 		// Show banner only for the specified number of days
 		if ( banner.showDays ) {
@@ -28,9 +30,10 @@ export const useShowBanners = ( banners: Array< Banner > ) => {
 	// Find the banner which is not dismissed and has not been viewed in the last 7 days(if showDays is specified)
 	const banner = useCallback( () => {
 		return banners.find(
-			( banner ) => ! banner.preference?.dismiss && ! banner.hideBanner && showBanner( banner )
+			( banner ) =>
+				! banner.preference?.dismiss && ! banner.hideBanner && shouldShowBanner( banner )
 		);
-	}, [ banners, showBanner ] )();
+	}, [ banners, shouldShowBanner ] )();
 
 	if ( banner ) {
 		return banner.component();
