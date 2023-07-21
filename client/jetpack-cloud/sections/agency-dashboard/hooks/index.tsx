6c8d496c3a41b772379e43406d8ee6630c09764a
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-07-21 08:50:39 -0500

    useDashboardShowLargeScreen: move to separate file, keep index export

--- client/jetpack-cloud/sections/agency-dashboard/hooks/index.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/hooks/index.tsx
@@ -1,51 +1,13 @@
-import { useBreakpoint } from '@automattic/viewport-react';
-import { useCallback, useState, useEffect, RefObject, useRef } from 'react';
+import { useCallback, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'calypso/state';
 import { selectLicense, unselectLicense } from 'calypso/state/jetpack-agency-dashboard/actions';
 import { hasSelectedLicensesOfType } from 'calypso/state/jetpack-agency-dashboard/selectors';
 import type { AllowedTypes } from '../sites-overview/types';
 
-const DESKTOP_BREAKPOINT = '>1280px';
-
 export { default as useToggleActivateMonitor } from './use-toggle-activate-monitor';
 export { default as useUpdateMonitorSettings } from './use-update-monitor-settings';
 export { default as useJetpackAgencyDashboardRecordTrackEvent } from './use-jetpack-agency-dashboard-record-track-event';
-
-export const useDashboardShowLargeScreen = (
-	siteTableRef: RefObject< HTMLTableElement >,
-	containerRef: { current: { clientWidth: number } }
-) => {
-	const isDesktop = useBreakpoint( DESKTOP_BREAKPOINT );
-
-	const [ isOverflowing, setIsOverflowing ] = useState( false );
-
-	const checkIfOverflowing = useCallback( () => {
-		const siteTableEle = siteTableRef ? siteTableRef.current : null;
-
-		if ( siteTableEle ) {
-			if ( siteTableEle.clientWidth > containerRef?.current?.clientWidth ) {
-				setTimeout( () => {
-					setIsOverflowing( true );
-				}, 1 );
-			}
-		}
-	}, [ siteTableRef, containerRef ] );
-
-	useEffect( () => {
-		window.addEventListener( 'resize', checkIfOverflowing );
-		return () => {
-			window.removeEventListener( 'resize', checkIfOverflowing );
-		};
-	}, [ checkIfOverflowing ] );
-
-	useEffect( () => {
-		checkIfOverflowing();
-		// Do not add checkIfOverflowing to the dependency array as it will cause an infinite loop
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [] );
-
-	return isDesktop && ! isOverflowing;
-};
+export { default as useDashboardShowLargeScreen } from './use-dashboard-show-large-screen';
 
 export const useDashboardAddRemoveLicense = ( siteId: number, type: AllowedTypes ) => {
 	const dispatch = useDispatch();
