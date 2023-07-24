bb91e90ec89e7863d158344418768c4c831ed316
Claudiu Filip <claudiucelfilip@gmail.com>
2023-07-24 14:19:24 +0300

    Add link click event in nav menu

--- packages/wpcom-template-parts/src/universal-header-navigation/menu-items.tsx
+++ packages/wpcom-template-parts/src/universal-header-navigation/menu-items.tsx
@@ -1,3 +1,5 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
+import page from 'page';
 import { ClickableItemProps, MenuItemProps } from '../types';
 
 export const NonClickableItem = ( { content, className }: MenuItemProps ) => {
@@ -6,6 +8,46 @@ export const NonClickableItem = ( { content, className }: MenuItemProps ) => {
 			{ content } <span className="x-nav-link-chevron" aria-hidden="true"></span>
 		</button>
 	);
+};
+
+const getParentElement = ( node: HTMLElement | null, pattern: RegExp ) => {
+	let parent = node;
+	while ( parent && ! parent.className.match( pattern ) ) {
+		if ( parent === document.body ) {
+			return null;
+		}
+		parent = parent.parentElement;
+	}
+
+	return parent;
+};
+
+const clickNavLinkEvent = ( target: HTMLElement ) => {
+	const props: { [ key: string ]: string | number } = {};
+
+	const container = getParentElement( target, /container/ );
+	const section = getParentElement( target, /section/ );
+
+	props.container_id = container?.id || '';
+	props.container_class = container?.className || '';
+	props.container = props.container_id || props.container_class || '';
+
+	props.section_id = section?.id || '';
+	props.section_class = section?.className || '';
+	props.section = props.section_id || props.section_class || '';
+
+	props.id = target.id || '';
+	props.class = target.className || '';
+
+	props.href = target.getAttribute( 'href' ) || '';
+	props.target = target.getAttribute( 'target' ) || '';
+	props.text = target.innerText || '';
+
+	const currentPage = page.current || '';
+	props.path = currentPage || '';
+	props.lp_name = currentPage.replace( /^\//, '' );
+
+	recordTracksEvent( 'calypso_link_click', props );
 };
 
 export const ClickableItem = ( {
@@ -24,6 +66,11 @@ export const ClickableItem = ( {
 	if ( className ) {
 		liClassName = liClassName + ' ' + className;
 	}
+
+	const onClick = ( event: React.MouseEvent< HTMLElement > ) => {
+		const target = event.currentTarget;
+		clickNavLinkEvent( target );
+	};
 	return (
 		<li className={ liClassName }>
 			<a
@@ -33,6 +80,7 @@ export const ClickableItem = ( {
 				title={ titleValue }
 				tabIndex={ -1 }
 				target={ target }
+				onClick={ onClick }
 			>
 				{ content }
 			</a>
