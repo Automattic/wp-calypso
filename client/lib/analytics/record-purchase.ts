c0a866becc2969e8b533624f3892de18493fdffc
Payton Swick <payton@foolord.com>
2023-07-24 18:42:05 -0400

    Convert recordOrder/recordPurchase to TypeScript

--- client/lib/analytics/record-purchase.ts
+++ client/lib/analytics/record-purchase.ts
@@ -1,8 +1,17 @@
 import { recordOrder } from 'calypso/lib/analytics/ad-tracking';
 import { costToUSD } from 'calypso/lib/analytics/utils';
 import { gaRecordEvent } from './ga';
+import type { ResponseCart } from '@automattic/shopping-cart';
 
-export async function recordPurchase( { cart, orderId, sitePlanSlug } ) {
+export async function recordPurchase( {
+	cart,
+	orderId,
+	sitePlanSlug,
+}: {
+	cart: ResponseCart;
+	orderId: number | null | undefined;
+	sitePlanSlug: string | null | undefined;
+} ) {
 	if ( cart.total_cost >= 0.01 ) {
 		const usdValue = costToUSD( cart.total_cost, cart.currency );
 
