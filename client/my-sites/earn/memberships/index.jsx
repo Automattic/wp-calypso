daa43d2af7266033964c336ba3754019ee22a753
edanzer <edanzer@gmail.com>
2023-07-25 17:15:40 -0600

    Ensure earn page redirect to Launchpad in some cases

--- client/my-sites/earn/memberships/index.jsx
+++ client/my-sites/earn/memberships/index.jsx
@@ -50,7 +50,7 @@ import {
 	getSelectedSiteSlug,
 } from 'calypso/state/ui/selectors';
 import CommissionFees from '../components/commission-fees';
-import { ADD_NEWSLETTER_PAYMENT_PLAN_HASH } from './constants';
+import { ADD_NEWSLETTER_PAYMENT_PLAN_HASH, LAUNCHPAD_HASH } from './constants';
 
 import './style.scss';
 
@@ -64,12 +64,20 @@ class MembershipsSection extends Component {
 		disconnectedConnectedAccountId: null,
 	};
 	componentDidMount() {
+		this.navigateToLaunchpad();
 		this.fetchNextSubscriberPage( false, true );
 	}
 	componentDidUpdate( prevProps ) {
+		this.navigateToLaunchpad();
 		if ( prevProps.siteId !== this.props.siteId ) {
 			// Site Id changed
 			this.fetchNextSubscriberPage( false, true );
+		}
+	}
+	navigateToLaunchpad() {
+		const shouldGoToLaunchpad = this.props?.query?.stripe_connect_success === 'launchpad';
+		if ( shouldGoToLaunchpad ) {
+			window.location.assign( `/setup/newsletter/launchpad?siteSlug=${ this.props.siteSlug }` );
 		}
 	}
 	renderEarnings() {
@@ -697,10 +705,25 @@ class MembershipsSection extends Component {
 	}
 }
 
+/**
+ * Source is used to add data to the Stripe Connect URL. On a successful
+ * connection, this source is used to redirect the user the appropriate place.
+ */
+const getSource = () => {
+	if ( window.location.hash === ADD_NEWSLETTER_PAYMENT_PLAN_HASH ) {
+		return 'earn-newsletter';
+	}
+	if ( window.location.hash === LAUNCHPAD_HASH ) {
+		return 'launchpad';
+	}
+	return 'calypso';
+};
+
 const mapStateToProps = ( state ) => {
 	const site = getSelectedSite( state );
 	const siteId = getSelectedSiteId( state );
 	const earnings = getEarningsWithDefaultsForSiteId( state, siteId );
+	const source = getSource();
 
 	return {
 		site,
@@ -721,8 +744,7 @@ const mapStateToProps = ( state ) => {
 			siteHasFeature( state, siteId, FEATURE_DONATIONS ) ||
 			siteHasFeature( state, siteId, FEATURE_RECURRING_PAYMENTS ),
 		isJetpack: isJetpackSite( state, siteId ),
-		source:
-			window.location.hash === ADD_NEWSLETTER_PAYMENT_PLAN_HASH ? 'earn-newsletter' : 'calypso',
+		source,
 	};
 };
 
