277149a53f904e0074d12a317917249b2e9a9130
Kateryna Kodonenko <kateryna@automattic.com>
2023-07-26 09:45:30 +0200

    Add checks for Atomic site, JP site and flag feature

--- client/my-sites/site-metrics/controller.tsx
+++ client/my-sites/site-metrics/controller.tsx
@@ -1,4 +1,8 @@
+import { isEnabled } from '@automattic/calypso-config';
 import { PageViewTracker } from 'calypso/lib/analytics/page-view-tracker';
+import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
+import { isJetpackSite } from 'calypso/state/sites/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { SiteMetrics } from './main';
 
 export const siteMetrics: PageJS.Callback = ( context, next ) => {
@@ -8,5 +12,27 @@ export const siteMetrics: PageJS.Callback = ( context, next ) => {
 			<SiteMetrics />
 		</>
 	);
+	next();
+};
+
+export const redirectHomeIfIneligible: PageJS.Callback = ( context, next ) => {
+	const state = context.store.getState();
+	const siteId = getSelectedSiteId( state );
+
+	if ( ! isEnabled( 'yolo/hosting-metrics-i1' ) ) {
+		context.page.replace( `/home/${ context.params.siteId }` );
+		return;
+	}
+
+	if ( ! isAtomicSite( state, siteId ) ) {
+		context.page.replace( `/home/${ context.params.siteId }` );
+		return;
+	}
+
+	if ( isJetpackSite( state, siteId, { treatAtomicAsJetpackSite: false } ) ) {
+		context.page.replace( `/stats/day/${ context.params.siteId }` );
+		return;
+	}
+
 	next();
 };
--- client/my-sites/site-metrics/index.ts
+++ client/my-sites/site-metrics/index.ts
@@ -1,19 +1,18 @@
-import { isEnabled } from '@automattic/calypso-config';
 import page from 'page';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 import { navigation, siteSelection, sites } from 'calypso/my-sites/controller';
-import { siteMetrics } from './controller';
+import { redirectHomeIfIneligible, siteMetrics } from './controller';
 
 export default function () {
-	if ( ! isEnabled( 'yolo/hosting-metrics-i1' ) ) {
-		// Redirect to Customer Home if flag isn't enabled.
-		page( '/site-metrics/:siteId', ( { params: { siteId } } ) => {
-			page.replace( `/home/${ siteId }` );
-		} );
-		return;
-	}
-
 	page( '/site-metrics', siteSelection, sites, makeLayout, clientRender );
 
-	page( '/site-metrics/:siteId', siteSelection, navigation, siteMetrics, makeLayout, clientRender );
+	page(
+		'/site-metrics/:siteId',
+		siteSelection,
+		redirectHomeIfIneligible,
+		navigation,
+		siteMetrics,
+		makeLayout,
+		clientRender
+	);
 }
