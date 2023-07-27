6716330c9b466a9d58e20843d56435654c3b132a
Jasper Kang <kangzjnet@gmail.com>
2023-07-28 11:33:42 +1200

    comments

--- client/my-sites/stats/stats-notices/lib/remove-stats-purchase-success-param.ts
+++ client/my-sites/stats/stats-notices/lib/remove-stats-purchase-success-param.ts
@@ -1,12 +1,10 @@
 const removeStatsPurchaseSuccessParam = ( url: string ) => {
-	// For Odyssey Stats, we simply update the hash which is equavalent to updating the URL.
-	const currentUrl = new URL( url );
-
 	// Delete param in GET params.
+	const currentUrl = new URL( url );
 	currentUrl.searchParams.delete( 'statsPurchaseSuccess' );
 
-	const hashUrl = new URL( currentUrl.hash.substring( 2 ), window.location.origin );
 	// Delete param in hash URL.
+	const hashUrl = new URL( currentUrl.hash.substring( 2 ), window.location.origin );
 	hashUrl.searchParams.delete( 'statsPurchaseSuccess' );
 	if ( hashUrl.search ) {
 		currentUrl.hash = `#!${ hashUrl.pathname }${ hashUrl.search }`;
