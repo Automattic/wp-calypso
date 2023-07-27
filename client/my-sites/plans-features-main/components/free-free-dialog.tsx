a3484a70c0f3f49e5eefcd8853b0966557517a94
Dehan <dehandecroos@gmail.com>
2023-07-27 15:41:35 -0400

    Fix cosmetic issues

--- client/my-sites/plans-features-main/components/free-free-dialog.tsx
+++ client/my-sites/plans-features-main/components/free-free-dialog.tsx
@@ -2,17 +2,33 @@ import { Button, Dialog, Gridicon } from '@automattic/components';
 import { css, Global } from '@emotion/react';
 import styled from '@emotion/styled';
 import { useTranslate } from 'i18n-calypso';
-import { DialogContainer, Heading } from './free-plan-paid-domain-dialog';
+import { formatCurrency } from 'calypso/../packages/format-currency/src';
+import { useSelector } from 'calypso/state';
+import { getCurrentUserCurrencyCode } from 'calypso/state/currency-code/selectors';
+import { DialogContainer } from './free-plan-paid-domain-dialog';
+
+export const Heading = styled.div`
+	font-family: Recoleta;
+	color: var( --studio-gray-100 );
+	font-size: 22px;
+	line-height: 26px;
+	letter-spacing: 0.38px;
+	margin-bottom: 12px;
+	@media ( min-width: 780px ) {
+		font-size: 32px;
+		line-height: 40px;
+		letter-spacing: -0.32px;
+	}
+`;
 
 const List = styled.ul`
 	list-style: none;
-	margin: 20px 0 20px;
+	margin: 20px 0 12px;
 	font-weight: 600;
 	font-size: 14px;
 `;
 const ListItem = styled.li`
 	display: flex;
-	margin-bottom: 8px;
 	& div:first-of-type {
 		margin: 0 8px 0 8px;
 	}
@@ -26,6 +42,8 @@ const ButtonRow = styled.div`
 const TextBox = styled.div< { fontSize?: number; bold?: boolean; color?: string } >`
 	font-size: ${ ( { fontSize } ) => fontSize || 14 }px;
 	font-weight: ${ ( { bold } ) => ( bold ? 600 : 400 ) };
+	line-height: 20px;
+	margin-bottom: 8px;
 	color: ${ ( { color } ) => {
 		if ( color === 'gray' ) {
 			return 'var(--studio-gray-50)';
@@ -44,9 +62,14 @@ export const StyledButton = styled( Button )`
 	&.is-borderless {
 		text-decoration: underline;
 		border: none;
-		color: #101517;
 		font-weight: 600;
 		padding: 0;
+		color: ${ ( { color } ) => {
+			if ( color === 'gray' ) {
+				return 'var(--studio-gray-50)';
+			}
+			return 'var(--color-text)';
+		} };
 	}
 
 	&.is-primary,
@@ -87,6 +110,7 @@ export function FreeFreeDialog( {
 	onPlanSelected: () => void;
 } ) {
 	const translate = useTranslate();
+	const userCurrencyCode = useSelector( getCurrentUserCurrencyCode ) || 'USD';
 
 	return (
 		<Dialog
@@ -159,10 +183,13 @@ export function FreeFreeDialog( {
 				</TextBox>
 				<TextBox>
 					{ translate(
-						'As a bonus, you will get a custom domain {{strong}} %(domaiName)s {{/strong}} for free for {{break}}{{/break}} one year (%(domainPrice)s value).',
+						'As a bonus, you will get a custom domain like "{{strong}} %(domaiName)s {{/strong}}" for free for {{break}}{{/break}} one year (%(domainPrice)s value).',
 						{
 							components: { strong: <strong />, break: <br /> },
-							args: { domaiName: 'somedomain.com', domainPrice: '$18' },
+							args: {
+								domaiName: 'mysite.com',
+								domainPrice: formatCurrency( 12, userCurrencyCode ),
+							},
 						}
 					) }
 				</TextBox>
@@ -171,7 +198,7 @@ export function FreeFreeDialog( {
 						{ translate( 'Get the Personal plan' ) }
 					</StyledButton>
 
-					<StyledButton onClick={ onFreePlanSelected } borderless>
+					<StyledButton onClick={ onFreePlanSelected } borderless color="gray">
 						{ translate( 'Continue with Free' ) }
 					</StyledButton>
 				</ButtonRow>
