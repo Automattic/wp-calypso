d45b7c467d4f73d545a95aefbc2af76d080c869f
Eric Binnion <ericbinnion@gmail.com>
2023-07-27 08:21:04 -0500

    Domain Transfer: Update checkout step copy to include copy used elsewhere (#79895)
    * Domain Transfer: Update checkout step copy to include copy used elsewhere
    
    * Domain Transfer: Update copy on step 1 checkout

--- client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-intro/intro.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-intro/intro.tsx
@@ -1,3 +1,4 @@
+import { useIsEnglishLocale } from '@automattic/i18n-utils';
 import { IntentScreen } from '@automattic/onboarding';
 import { Button } from '@wordpress/components';
 import { Icon, unlock, plus, payment } from '@wordpress/icons';
@@ -9,7 +10,8 @@ interface Props {
 }
 
 const Intro: React.FC< Props > = ( { onSubmit } ) => {
-	const { __ } = useI18n();
+	const { __, hasTranslation } = useI18n();
+	const isEnglishLocale = useIsEnglishLocale();
 
 	return (
 		<>
@@ -49,9 +51,16 @@ const Intro: React.FC< Props > = ( { onSubmit } ) => {
 						badge: __( 'Google Domains: Free' ),
 						description: (
 							<p>
-								{ __(
-									'Review your payment and contact details. Google Domains transfers and the first year are free.'
-								) }
+								{ isEnglishLocale ||
+								hasTranslation(
+									"Review your payment and contact details. If you're transferring a domain from Google, we'll absorb the cost and give you an extra year of free registration."
+								)
+									? __(
+											"Review your payment and contact details. If you're transferring a domain from Google, we'll absorb the cost and give you an extra year of free registration."
+									  )
+									: __(
+											'Review your payment and contact details. Google Domains transfers and the first year are free.'
+									  ) }
 							</p>
 						),
 						icon: <Icon icon={ payment } />,
