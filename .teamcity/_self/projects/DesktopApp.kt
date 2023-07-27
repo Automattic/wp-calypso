d7ea4f0f13d546d7c295add0019bde47fb089141
Edwin Takahashi <egao@outlook.com>
2023-07-27 15:44:24 +0900

    _self/projects/DesktopApp.kt - import

--- .teamcity/_self/projects/DesktopApp.kt
+++ .teamcity/_self/projects/DesktopApp.kt
@@ -13,6 +13,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.notifications
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.retryBuild
 
 object DesktopApp : Project({
 	id("WpDesktop")
