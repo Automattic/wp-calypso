b4875fc1e264388d03616a6202c75a4d56486524
Jasper Kang <kangzjnet@gmail.com>
2023-07-28 11:54:28 +1200

    add unit tests

--- client/my-sites/stats/stats-notices/lib/remove-stats-purchase-success-param.ts
+++ client/my-sites/stats/stats-notices/lib/remove-stats-purchase-success-param.ts
@@ -3,13 +3,12 @@ const removeStatsPurchaseSuccessParam = ( url: string ) => {
 	const currentUrl = new URL( url );
 	currentUrl.searchParams.delete( 'statsPurchaseSuccess' );
 
-	// Delete param in hash URL.
-	const hashUrl = new URL( currentUrl.hash.substring( 2 ), window.location.origin );
-	hashUrl.searchParams.delete( 'statsPurchaseSuccess' );
-	if ( hashUrl.search ) {
+	// Delete param in hash URL if any.
+	if ( currentUrl.hash ) {
+		const hashUrl = new URL( currentUrl.hash.substring( 2 ), window.location.origin );
+		hashUrl.searchParams.delete( 'statsPurchaseSuccess' );
 		currentUrl.hash = `#!${ hashUrl.pathname }${ hashUrl.search }`;
 	}
-
 	return currentUrl.toString();
 };
 
