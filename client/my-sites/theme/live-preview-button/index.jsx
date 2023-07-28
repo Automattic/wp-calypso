48610108e8f97397eda802dfdf5584a1b7e69fd3
okmttdhr <okmttdhr@users.noreply.github.com>
2023-07-28 11:21:21 +0900

    Add a comment about scenarios where the Live Preview does not work

--- client/my-sites/theme/live-preview-button/index.jsx
+++ client/my-sites/theme/live-preview-button/index.jsx
@@ -55,6 +55,17 @@ const isNotCompatibleThemes = ( stylesheet ) => {
 /**
  * Live Preview leveraging Gutenberg's Block Theme Previews
  *
+ * The scenarios where the Live Preview does NOT support;
+ * - On both Simple and Atomic sites;
+ *   - If the theme is users' active theme.
+ *   - If the theme is not FullSiteEditing compatible.
+ *   - If the theme has a static page as a homepage.
+ * - On Atomic sites;
+ *   - If the theme is not installed on Atomic sites.
+ * - On Simple sites;
+ *   - If the theme is a 3rd party theme.
+ *   - If the theme is not included in a plan.
+ *
  * @see pbxlJb-3Uv-p2
  */
 export const LivePreviewButton = ( {
