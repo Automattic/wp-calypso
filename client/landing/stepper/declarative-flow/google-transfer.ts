3641731bb101bfcca0462e8ee4483633bfe6146b
Omar Alshaker <omar@omaralshaker.com>
2023-07-29 14:33:02 +0200

    Simplify the flow

--- client/landing/stepper/declarative-flow/google-transfer.ts
+++ client/landing/stepper/declarative-flow/google-transfer.ts
@@ -1,29 +1,9 @@
-import { useLocale } from '@automattic/i18n-utils';
-import { DOMAIN_TRANSFER, GOOGLE_TRANSFER } from '@automattic/onboarding';
-import { useSelect } from '@wordpress/data';
-import { translate } from 'i18n-calypso';
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import {
-	clearSignupDestinationCookie,
-	setSignupCompleteSlug,
-	setSignupCompleteFlowName,
-} from 'calypso/signup/storageUtils';
-import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
-import { USER_STORE } from '../stores';
-import { recordSubmitStep } from './internals/analytics/record-submit-step';
-import {
-	Flow,
-	ProvidedDependencies,
-	AssertConditionResult,
-	AssertConditionState,
-} from './internals/types';
-import type { UserSelect } from '@automattic/data-stores';
+import { GOOGLE_TRANSFER } from '@automattic/onboarding';
+import domainTransfer from './domain-transfer';
+import { Flow } from './internals/types';
+
 const googleDomainTransfer: Flow = {
-	name: DOMAIN_TRANSFER,
-	get title() {
-		return translate( 'Bulk domain transfer' );
-	},
+	...domainTransfer,
 	variantSlug: GOOGLE_TRANSFER,
 	useSteps() {
 		return [
@@ -40,79 +20,6 @@ const googleDomainTransfer: Flow = {
 				asyncComponent: () => import( './internals/steps-repository/processing-step' ),
 			},
 		];
-	},
-
-	useAssertConditions( navigate ): AssertConditionResult {
-		const isLoggedIn = useSelector( isUserLoggedIn );
-
-		useEffect( () => {
-			if ( ! isLoggedIn && navigate ) {
-				navigate( 'intro' );
-			}
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-		}, [ isLoggedIn ] );
-
-		return { state: AssertConditionState.SUCCESS };
-	},
-
-	useStepNavigation( _currentStepSlug, navigate ) {
-		const flowName = this.name;
-		const userIsLoggedIn = useSelect(
-			( select ) => ( select( USER_STORE ) as UserSelect ).isCurrentUserLoggedIn(),
-			[]
-		);
-		const locale = useLocale();
-		const variantSlug = this.variantSlug;
-
-		const logInUrl =
-			locale && locale !== 'en'
-				? `/start/account/user/${ locale }?variationName=${ variantSlug }&pageTitle=Bulk+Transfer&redirect_to=/setup/${ variantSlug }/domains`
-				: `/start/account/user?variationName=${ variantSlug }&pageTitle=Bulk+Transfer&redirect_to=/setup/${ variantSlug }/domains`;
-
-		const submit = ( providedDependencies: ProvidedDependencies = {} ) => {
-			recordSubmitStep( providedDependencies, '', flowName, _currentStepSlug, variantSlug );
-
-			switch ( _currentStepSlug ) {
-				case 'intro':
-					clearSignupDestinationCookie();
-
-					if ( userIsLoggedIn ) {
-						return navigate( 'domains' );
-					}
-					return window.location.assign( logInUrl );
-				case 'domains': {
-					// go to processing step without pushing it to history
-					// so the back button would go back to domains step
-					return navigate( 'processing', undefined );
-				}
-				case 'processing': {
-					setSignupCompleteSlug( providedDependencies?.siteSlug );
-					setSignupCompleteFlowName( variantSlug );
-
-					const checkoutBackURL = new URL( '/setup/domain-transfer/domains', window.location.href );
-
-					// use replace instead of assign to remove the processing URL from history
-					return window.location.replace(
-						`/checkout/no-site?signup=0&isDomainOnly=1&checkoutBackUrl=${ encodeURIComponent(
-							checkoutBackURL.href
-						) }`
-					);
-				}
-				default:
-					return;
-			}
-		};
-
-		const goBack = () => {
-			switch ( _currentStepSlug ) {
-				case 'domains':
-					return navigate( 'intro' );
-				default:
-					return;
-			}
-		};
-
-		return { goBack, submit };
 	},
 };
 
