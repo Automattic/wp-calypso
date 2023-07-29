78098315265f7daa7d63b9c6a3a682611f5b3f6a
Omar Alshaker <omar@omaralshaker.com>
2023-07-29 15:20:23 +0200

    Simplify flow

--- client/landing/stepper/declarative-flow/google-transfer.ts
+++ client/landing/stepper/declarative-flow/google-transfer.ts
@@ -5,22 +5,6 @@ import { Flow } from './internals/types';
 const googleDomainTransfer: Flow = {
 	...domainTransfer,
 	variantSlug: GOOGLE_TRANSFER,
-	useSteps() {
-		return [
-			{
-				slug: 'intro',
-				asyncComponent: () => import( './internals/steps-repository/domain-transfer-intro' ),
-			},
-			{
-				slug: 'domains',
-				asyncComponent: () => import( './internals/steps-repository/domain-transfer-domains' ),
-			},
-			{
-				slug: 'processing',
-				asyncComponent: () => import( './internals/steps-repository/processing-step' ),
-			},
-		];
-	},
 };
 
 export default googleDomainTransfer;
