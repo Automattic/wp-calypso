f25713850c572f6928e8f2fb6b5065113fa2873f
Jasper Kang <kangzjnet@gmail.com>
2023-08-01 09:40:13 +1200

    comment

--- client/my-sites/stats/stats-notices/index.tsx
+++ client/my-sites/stats/stats-notices/index.tsx
@@ -88,7 +88,7 @@ const PostPurchaseNotices = ( {
 		setParamRemoved( true );
 		const newUrlObj = removeStatsPurchaseSuccessParam( window.location.href, !! isOdysseyStats );
 		if ( isOdysseyStats ) {
-			// We need to update the page base if it changed. Otherwise, then pagejs won't be able to find the routes.
+			// We need to update the page base if it changed. Otherwise, pagejs won't be able to find the routes.
 			page.base( `${ newUrlObj.pathname }${ newUrlObj.search }` );
 		}
 		// Odyssey would try to hack the URL on load to remove duplicate params. We need to wait for that to finish.
