77e4db465e734551fc065d5f2ca7ec5ecaceb580
Paulo Trentin <paulo@paulotrentin.com.br>
2023-07-31 14:23:43 -0300

    Removed unnecessary mock on tests

--- client/my-sites/checkout/composite-checkout/test/netbanking.tsx
+++ client/my-sites/checkout/composite-checkout/test/netbanking.tsx
@@ -65,11 +65,6 @@ function ResetNetbankingStoreFields() {
 	} );
 }
 
-jest.mock( '@automattic/shopping-cart', () => ( {
-	...jest.requireActual( '@automattic/shopping-cart' ),
-	useShoppingCart: jest.fn( () => true ),
-} ) );
-
 describe( 'Netbanking payment method', () => {
 	it( 'renders a netbanking option', async () => {
 		const paymentMethod = getPaymentMethod();
