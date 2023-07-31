83fe0c1f6627d017ef7f1976c46071a7a5798b1e
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-07-31 13:27:49 -0500

    getLicenseState: move to separate file in ./lib

--- client/jetpack-cloud/sections/partner-portal/utils.ts
+++ client/jetpack-cloud/sections/partner-portal/utils.ts
@@ -2,7 +2,6 @@ import sortBy from 'lodash/sortBy';
 import {
 	LicenseFilter,
 	LicenseSortField,
-	LicenseState,
 } from 'calypso/jetpack-cloud/sections/partner-portal/types';
 import {
 	APIPartner,
@@ -19,29 +18,7 @@ import {
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export function noop() {}
 
-/**
- * Get the state of a license based on its properties.
- * For example, if a license is neither attached to a site and revoked then it is both detached and revoked but
- * the dominant state would be considered to be revoked as it is of higher importance.
- *
- * @param {string | null} attachedAt Date the license was attached on, if any.
- * @param {string | null} revokedAt Date the license was revoked on, if any.
- * @returns {LicenseState} State matching one of the `LicenseState` values.
- */
-export function getLicenseState(
-	attachedAt: string | null,
-	revokedAt: string | null
-): LicenseState {
-	if ( revokedAt ) {
-		return LicenseState.Revoked;
-	}
-
-	if ( attachedAt ) {
-		return LicenseState.Attached;
-	}
-
-	return LicenseState.Detached;
-}
+export { default as getLicenseState } from './lib/get-license-state';
 
 /**
  * Convert a value to a the enum member with that value or a fallback.
