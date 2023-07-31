adb5b14ae090c4c44e2f0365a0aad93e18439007
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2023-07-31 06:40:01 -0700

    First pass at some UI to change the WordPress version

--- client/my-sites/hosting/web-server-settings-card/index.js
+++ client/my-sites/hosting/web-server-settings-card/index.js
@@ -7,18 +7,25 @@ import CardHeading from 'calypso/components/card-heading';
 import QuerySiteGeoAffinity from 'calypso/components/data/query-site-geo-affinity';
 import QuerySitePhpVersion from 'calypso/components/data/query-site-php-version';
 import QuerySiteStaticFile404 from 'calypso/components/data/query-site-static-file-404';
+import QuerySiteWpVersion from 'calypso/components/data/query-site-wp-version';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormSelect from 'calypso/components/forms/form-select';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
 import FormTextInput from 'calypso/components/forms/form-text-input';
 import MaterialIcon from 'calypso/components/material-icon';
-import { updateAtomicPhpVersion, updateAtomicStaticFile404 } from 'calypso/state/hosting/actions';
+import {
+	updateAtomicPhpVersion,
+	updateAtomicStaticFile404,
+	updateAtomicWpVersion,
+} from 'calypso/state/hosting/actions';
 import { getAtomicHostingGeoAffinity } from 'calypso/state/selectors/get-atomic-hosting-geo-affinity';
 import { getAtomicHostingPhpVersion } from 'calypso/state/selectors/get-atomic-hosting-php-version';
 import { getAtomicHostingStaticFile404 } from 'calypso/state/selectors/get-atomic-hosting-static-file-404';
+import { getAtomicHostingWpVersion } from 'calypso/state/selectors/get-atomic-hosting-wp-version';
 import getRequest from 'calypso/state/selectors/get-request';
 import { isFetchingAtomicHostingGeoAffinity } from 'calypso/state/selectors/is-fetching-atomic-hosting-geo-affinity';
+import { isFetchingAtomicHostingWpVersion } from 'calypso/state/selectors/is-fetching-atomic-hosting-wp-version';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 import './style.scss';
@@ -46,20 +53,26 @@ const WebServerSettingsCard = ( {
 	isGettingGeoAffinity,
 	isGettingPhpVersion,
 	isGettingStaticFile404,
+	isGettingWpVersion,
 	isUpdatingPhpVersion,
 	isUpdatingStaticFile404,
+	isUpdatingWpVersion,
 	siteId,
 	geoAffinity,
 	staticFile404,
 	translate,
 	updatePhpVersion,
 	updateStaticFile404,
+	updateWpVersion,
 	phpVersion,
+	wpVersion,
 } ) => {
 	const [ selectedPhpVersion, setSelectedPhpVersion ] = useState( '' );
+	const [ selectedWpVersion, setSelectedWpVersion ] = useState( '' );
 	const [ selectedStaticFile404, setSelectedStaticFile404 ] = useState( '' );
 
-	const isLoading = isGettingGeoAffinity || isGettingPhpVersion || isGettingStaticFile404;
+	const isLoading =
+		isGettingGeoAffinity || isGettingPhpVersion || isGettingStaticFile404 || isGettingWpVersion;
 
 	const getGeoAffinityContent = () => {
 		if ( isGettingGeoAffinity || ! geoAffinity ) {
@@ -139,6 +152,19 @@ const WebServerSettingsCard = ( {
 		];
 	};
 
+	const getWpVersions = () => {
+		return [
+			{
+				label: translate( 'Latest' ),
+				value: 'latest',
+			},
+			{
+				label: translate( 'Beta' ),
+				value: 'beta',
+			},
+		];
+	};
+
 	const getPhpVersionContent = () => {
 		if ( isGettingPhpVersion ) {
 			return;
@@ -183,6 +209,51 @@ const WebServerSettingsCard = ( {
 						disabled={ isUpdatingPhpVersion }
 					>
 						<span>{ translate( 'Update PHP version' ) }</span>
+					</Button>
+				) }
+			</FormFieldset>
+		);
+	};
+
+	const getWpVersionContent = () => {
+		if ( isGettingWpVersion ) {
+			return;
+		}
+
+		const isWpVersionButtonDisabled =
+			disabled || ! selectedWpVersion || selectedWpVersion === wpVersion;
+		const selectedWpVersionValue =
+			selectedWpVersion || wpVersion || ( disabled && recommendedValue );
+
+		return (
+			<FormFieldset>
+				<FormLabel>{ translate( 'WordPress version' ) }</FormLabel>
+				<FormSelect
+					disabled={ disabled || isUpdatingPhpVersion }
+					className="web-server-settings-card__wp-version-select"
+					onChange={ ( event ) => setSelectedWpVersion( event.target.value ) }
+					value={ selectedWpVersionValue }
+				>
+					{ getWpVersions().map( ( option ) => {
+						return (
+							<option
+								disabled={ option.value === phpVersion }
+								value={ option.value }
+								key={ option.label }
+							>
+								{ option.label }
+							</option>
+						);
+					} ) }
+				</FormSelect>
+				{ ! isWpVersionButtonDisabled && (
+					<Button
+						className="web-server-settings-card__wp-set-version"
+						onClick={ () => updateWpVersion( siteId, selectedWpVersion ) }
+						busy={ isUpdatingWpVersion }
+						disabled={ isUpdatingWpVersion }
+					>
+						<span>{ translate( 'Update WordPress version' ) }</span>
 					</Button>
 				) }
 			</FormFieldset>
@@ -298,6 +369,7 @@ const WebServerSettingsCard = ( {
 		<Card className="web-server-settings-card">
 			<QuerySiteGeoAffinity siteId={ siteId } />
 			<QuerySitePhpVersion siteId={ siteId } />
+			<QuerySiteWpVersion siteId={ siteId } />
 			<QuerySiteStaticFile404 siteId={ siteId } />
 			<MaterialIcon icon="build" size={ 32 } />
 			<CardHeading id="web-server-settings">{ translate( 'Web server settings' ) }</CardHeading>
@@ -308,6 +380,7 @@ const WebServerSettingsCard = ( {
 			</p>
 			{ ! isLoading && getGeoAffinityContent() }
 			{ ! isLoading && getPhpVersionContent() }
+			{ ! isLoading && getWpVersionContent() }
 			{ ! isLoading && getStaticFile404Content() }
 			{ isLoading && getPlaceholderContent() }
 		</Card>
@@ -319,24 +392,30 @@ export default connect(
 		const siteId = getSelectedSiteId( state );
 		const geoAffinity = getAtomicHostingGeoAffinity( state, siteId );
 		const phpVersion = getAtomicHostingPhpVersion( state, siteId );
+		const wpVersion = getAtomicHostingWpVersion( state, siteId );
 		const staticFile404 = getAtomicHostingStaticFile404( state, siteId );
 
 		return {
 			isGettingGeoAffinity: isFetchingAtomicHostingGeoAffinity( state, siteId ),
 			isGettingPhpVersion: ! props.disabled && ! phpVersion,
+			isGettingWpVersion: isFetchingAtomicHostingWpVersion( state, siteId ),
 			isGettingStaticFile404: ! props.disabled && ! staticFile404,
 			isUpdatingPhpVersion:
 				getRequest( state, updateAtomicPhpVersion( siteId, null ) )?.isLoading ?? false,
 			isUpdatingStaticFile404:
 				getRequest( state, updateAtomicStaticFile404( siteId, null ) )?.isLoading ?? false,
+			isUpdatingWpVersion:
+				getRequest( state, updateAtomicWpVersion( siteId, null ) )?.isLoading ?? false,
 			siteId,
 			geoAffinity,
 			staticFile404,
 			phpVersion,
+			wpVersion,
 		};
 	},
 	{
 		updatePhpVersion: updateAtomicPhpVersion,
+		updateWpVersion: updateAtomicWpVersion,
 		updateStaticFile404: updateAtomicStaticFile404,
 	}
 )( localize( WebServerSettingsCard ) );
