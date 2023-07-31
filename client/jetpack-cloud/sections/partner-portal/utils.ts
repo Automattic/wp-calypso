6d3ddfc7fd3021177bb6ea7142d484315eb51447
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-07-31 15:16:51 -0500

    valueToEnum: move to separate file in ./lib

--- client/jetpack-cloud/sections/partner-portal/utils.ts
+++ client/jetpack-cloud/sections/partner-portal/utils.ts
@@ -4,6 +4,7 @@ import {
 	LicenseSortField,
 } from 'calypso/jetpack-cloud/sections/partner-portal/types';
 import { APIProductFamily, APIProductFamilyProduct } from 'calypso/state/partner-portal/types';
+import valueToEnum from './lib/value-to-enum';
 
 /**
  * Noop which can be reused (e.g. in equality checks).
@@ -14,25 +15,7 @@ import { APIProductFamily, APIProductFamilyProduct } from 'calypso/state/partner
 export function noop() {}
 
 export { default as getLicenseState } from './lib/get-license-state';
-
-/**
- * Convert a value to a the enum member with that value or a fallback.
- * This is a hack around TypeScript's poor support of enums as types.
- *
- * @example const enumMember = valueToEnum< SomeEnumType >( SomeEnumType, 'foo', SomeEnumType.SomeMember );
- * @template T
- * @param {Record< string, * >} enumType Enum type to search in.
- * @param {*} value The enum value we are looking to get the member for.
- * @param {*} fallback The fallback value in case value is not a member of enumType.
- * @returns {T} T for value or fallback
- */
-export function valueToEnum< T >(
-	enumType: Record< string, unknown >,
-	value: unknown,
-	fallback: unknown
-): T {
-	return Object.values( enumType ).includes( value ) ? ( value as T ) : ( fallback as T );
-}
+export { default as valueToEnum } from './lib/value-to-enum';
 
 const internalFilterMap = {
 	[ LicenseFilter.NotRevoked ]: '',
