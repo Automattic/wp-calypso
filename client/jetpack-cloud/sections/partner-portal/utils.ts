6fff6b411041d0b06a5742052292986e796a9474
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-07-31 15:31:28 -0500

    Public/internal license filters/sorts: move to separate files in ./lib

--- client/jetpack-cloud/sections/partner-portal/utils.ts
+++ client/jetpack-cloud/sections/partner-portal/utils.ts
@@ -1,92 +1,14 @@
-import {
-	LicenseFilter,
-	LicenseSortField,
-} from 'calypso/jetpack-cloud/sections/partner-portal/types';
-import valueToEnum from './lib/value-to-enum';
-
 export { noop, LICENSE_INFO_MODAL_ID } from './lib/constants';
 export { default as getLicenseState } from './lib/get-license-state';
 export { default as valueToEnum } from './lib/value-to-enum';
-
-const internalFilterMap = {
-	[ LicenseFilter.NotRevoked ]: '',
-	[ LicenseFilter.Attached ]: 'assigned',
-	[ LicenseFilter.Detached ]: 'unassigned',
-} as { [ key: string ]: string };
-
-const publicFilterMap = {
-	[ internalFilterMap[ LicenseFilter.Attached ] ]: LicenseFilter.Attached,
-	[ internalFilterMap[ LicenseFilter.Detached ] ]: LicenseFilter.Detached,
-} as { [ key: string ]: LicenseFilter };
-
-/**
- * Convert a public license filter to its internal representation.
- * Public filter differences are entirely cosmetic.
- *
- * @param {string} publicFilter Public filter value (e.g. "assigned").
- * @param {LicenseFilter} fallback Filter to return if publicFilter is invalid.
- * @returns {LicenseFilter} Internal filter.
- */
-export function publicToInternalLicenseFilter(
-	publicFilter: string,
-	fallback: LicenseFilter
-): LicenseFilter {
-	return valueToEnum< LicenseFilter >(
-		LicenseFilter,
-		publicFilterMap[ publicFilter ] || publicFilter,
-		fallback
-	);
-}
-
-/**
- * Convert an internal license filter to its public representation.
- * Public filter differences are entirely cosmetic.
- *
- * @param {LicenseFilter} internalFilter Internal filter (e.g. LicenseFilter.Attached).
- * @returns {string} Public filter.
- */
-export function internalToPublicLicenseFilter( internalFilter: LicenseFilter ): string {
-	return internalFilterMap[ internalFilter ] || internalFilter;
-}
-
-const internalSortFieldMap = {
-	[ LicenseSortField.AttachedAt ]: 'assigned_on',
-} as { [ key: string ]: string };
-
-const publicSortFieldMap = {
-	[ internalSortFieldMap[ LicenseSortField.AttachedAt ] ]: LicenseSortField.AttachedAt,
-} as { [ key: string ]: LicenseSortField };
-
-/**
- * Convert a public license sort field to its internal representation.
- * Public sort field differences are entirely cosmetic.
- *
- * @param {string} publicSortField Public sort field value (e.g. "assigned_on").
- * @param {LicenseSortField} fallback Sort field to return if publicSortField is invalid.
- * @returns {LicenseSortField} Internal sort field.
- */
-export function publicToInternalLicenseSortField(
-	publicSortField: string,
-	fallback: LicenseSortField
-): LicenseSortField {
-	return valueToEnum< LicenseSortField >(
-		LicenseSortField,
-		publicSortFieldMap[ publicSortField ] || publicSortField,
-		fallback
-	);
-}
-
-/**
- * Convert an internal license sort field to its public representation.
- * Public sort field differences are entirely cosmetic.
- *
- * @param {LicenseSortField} internalSortField Internal sort field (e.g. LicenseSortField.AttachedAt).
- * @returns {string} Public sort field.
- */
-export function internalToPublicLicenseSortField( internalSortField: LicenseSortField ): string {
-	return internalSortFieldMap[ internalSortField ] || internalSortField;
-}
-
+export {
+	publicToInternalLicenseFilter,
+	internalToPublicLicenseFilter,
+} from './lib/license-filters';
+export {
+	publicToInternalLicenseSortField,
+	internalToPublicLicenseSortField,
+} from './lib/license-sorters';
 export { default as ensurePartnerPortalReturnUrl } from './lib/ensure-partner-portal-return-url';
 export { default as formatApiPartner } from './lib/format-api-partner';
 export { default as getProductTitle } from './lib/get-product-title';
