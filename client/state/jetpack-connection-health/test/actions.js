4d575d5b2ebd6bfd53c5adc9934d110332712c61
Wojtek Naruniec <wojtek@naruniec.me>
2023-07-31 11:52:13 +0200

    Add tests and readme for the new state

---
+++ client/state/jetpack-connection-health/test/actions.js
@@ -0,0 +1,29 @@
+import {
+	JETPACK_CONNECTION_HEALTHY,
+	JETPACK_CONNECTION_MAYBE_UNHEALTHY,
+} from 'calypso/state/action-types';
+import { setJetpackConnectionHealthy, setJetpackConnectionMaybeUnhealthy } from '../actions';
+
+describe( 'action', () => {
+	describe( 'setJetpackConnectionHealthy', () => {
+		test( 'should return a jetpack healthy connection action', () => {
+			const requestAction = setJetpackConnectionHealthy( 1 );
+
+			expect( requestAction ).toEqual( {
+				type: JETPACK_CONNECTION_HEALTHY,
+				siteId: 1,
+			} );
+		} );
+	} );
+
+	describe( 'setJetpackConnectionMaybeUnhealthy', () => {
+		test( 'should return a jetpack maybe unhealthy connection action', () => {
+			const setTransferAction = setJetpackConnectionMaybeUnhealthy( 1 );
+
+			expect( setTransferAction ).toEqual( {
+				type: JETPACK_CONNECTION_MAYBE_UNHEALTHY,
+				siteId: 1,
+			} );
+		} );
+	} );
+} );
