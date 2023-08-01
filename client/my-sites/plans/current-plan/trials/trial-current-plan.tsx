1847724d8894b95782da0e41e14f52603514b617
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2023-08-01 17:32:22 +0200

    Rename component CSS classes

--- client/my-sites/plans/current-plan/trials/trial-current-plan.tsx
+++ client/my-sites/plans/current-plan/trials/trial-current-plan.tsx
@@ -1,5 +1,9 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
-import { PLAN_WOOEXPRESS_MEDIUM_MONTHLY } from '@automattic/calypso-products';
+import {
+	PLAN_BUSINESS_MONTHLY,
+	PLAN_ECOMMERCE_TRIAL_MONTHLY,
+	PLAN_WOOEXPRESS_MEDIUM_MONTHLY,
+} from '@automattic/calypso-products';
 import { Button } from '@automattic/components';
 import { useMediaQuery } from '@wordpress/compose';
 import { useTranslate } from 'i18n-calypso';
@@ -27,10 +31,20 @@ const TrialCurrentPlan = () => {
 		setShowAllTrialFeaturesInMobileView( true );
 	};
 
+	const currentPlanSlug = selectedSite?.plan?.product_slug ?? '';
+	const isEcommerceTrial = currentPlanSlug === PLAN_ECOMMERCE_TRIAL_MONTHLY;
+	// const isMigrationTrial = currentPlanSlug === PLAN_MIGRATION_TRIAL_MONTHLY;
+
 	const isMobile = useMediaQuery( '(max-width: 480px)' );
 	const displayAllIncluded = ! isMobile || showAllTrialFeaturesInMobileView;
+	const bodyClass = isEcommerceTrial
+		? [ 'is-ecommerce-trial-plan' ]
+		: [ 'is-migration-trial-plan' ];
 
-	const targetPlan = PLAN_WOOEXPRESS_MEDIUM_MONTHLY;
+	const targetPlan = isEcommerceTrial ? PLAN_WOOEXPRESS_MEDIUM_MONTHLY : PLAN_BUSINESS_MONTHLY;
+	const trackEvent = isEcommerceTrial
+		? 'calypso_wooexpress_my_plan_cta'
+		: 'calypso_migration_my_plan_cta';
 
 	/**
 	 * Redirects to the checkout page with Plan on cart.
@@ -38,7 +52,7 @@ const TrialCurrentPlan = () => {
 	 * @param ctaPosition - The position of the CTA that triggered the redirect.
 	 */
 	const goToCheckoutWithPlan = ( ctaPosition: string ) => {
-		recordTracksEvent( `calypso_wooexpress_my_plan_cta`, {
+		recordTracksEvent( trackEvent, {
 			cta_position: ctaPosition,
 		} );
 
@@ -52,7 +66,7 @@ const TrialCurrentPlan = () => {
 
 	const bannerCallToAction = (
 		<Button
-			className="e-commerce-trial-current-plan__trial-card-cta"
+			className="trial-current-plan__trial-card-cta"
 			primary
 			onClick={ () => goToCheckoutWithPlan( 'card' ) }
 		>
@@ -62,21 +76,21 @@ const TrialCurrentPlan = () => {
 
 	return (
 		<>
-			<BodySectionCssClass bodyClass={ [ 'is-ecommerce-trial-plan' ] } />
+			<BodySectionCssClass bodyClass={ bodyClass } />
 
-			<div className="e-commerce-trial-current-plan__banner-wrapper">
+			<div className="trial-current-plan__banner-wrapper">
 				<ECommerceTrialBanner callToAction={ bannerCallToAction } />
 			</div>
 
-			<h2 className="e-commerce-trial-current-plan__section-title">
+			<h2 className="trial-current-plan__section-title">
 				{ translate( 'What’s included in your free trial' ) }
 			</h2>
-			<div className="e-commerce-trial-current-plan__included-wrapper">
+			<div className="trial-current-plan__included-wrapper">
 				<EcommerceTrialIncluded displayAll={ displayAllIncluded } />
 
 				{ ! displayAllIncluded && (
 					<Button
-						className="e-commerce-trial-current-plan__included-view-all"
+						className="trial-current-plan__included-view-all"
 						onClick={ viewAllIncludedFeatures }
 					>
 						{ translate( 'View all' ) }
@@ -84,22 +98,26 @@ const TrialCurrentPlan = () => {
 				) }
 			</div>
 
-			<h2 className="e-commerce-trial-current-plan__section-title">
-				{ translate( 'Ready to start selling?' ) }
-			</h2>
-			<p className="e-commerce-trial-current-plan__section-subtitle">
-				{ translate(
-					'Upgrade your free trial to launch your store and get the next-level features you need to grow.'
-				) }
-			</p>
+			{ isEcommerceTrial && (
+				<>
+					<h2 className="trial-current-plan__section-title">
+						{ translate( 'Ready to start selling?' ) }
+					</h2>
+					<p className="trial-current-plan__section-subtitle">
+						{ translate(
+							'Upgrade your free trial to launch your store and get the next-level features you need to grow.'
+						) }
+					</p>
 
-			<div className="e-commerce-trial-current-plan__more-wrapper">
-				<EcommerceTrialNotIncluded />
-			</div>
+					<div className="trial-current-plan__more-wrapper">
+						<EcommerceTrialNotIncluded />
+					</div>
+				</>
+			) }
 
-			<div className="e-commerce-trial-current-plan__cta-wrapper">
+			<div className="trial-current-plan__cta-wrapper">
 				<Button
-					className="e-commerce-trial-current-plan__cta is-primary"
+					className="trial-current-plan__cta is-primary"
 					onClick={ () => goToCheckoutWithPlan( 'footer' ) }
 				>
 					{ translate( 'Upgrade now' ) }
