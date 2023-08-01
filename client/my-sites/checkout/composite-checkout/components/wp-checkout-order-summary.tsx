fc82357927a74dc8b456b1baa84ab5f2c91b5fd5
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2023-08-01 09:25:23 -0600

    Add akismet enterprise benefits to checkout (#80052)
    * Add Feature strings for Akismet Enterprise plans
    
    * Add benefits for Akismet Enterprise in checkout
    
    * Update unit tests

--- client/my-sites/checkout/composite-checkout/components/wp-checkout-order-summary.tsx
+++ client/my-sites/checkout/composite-checkout/components/wp-checkout-order-summary.tsx
@@ -522,6 +522,16 @@ function CheckoutSummaryAkismetProductFeatures( { product }: { product: Response
 	const translate = useTranslate();
 	const productFeatures = getAkismetProductFeatures( product, translate );
 
+	let yearlySavingsPercentage = 0;
+
+	// If intro offer is not present and there are only two variants, then show the yearly savings.
+	if ( ! product?.introductory_offer_terms?.enabled && product?.product_variants.length === 2 ) {
+		const monthlyCost = product.product_variants[ 0 ].price_before_discounts_integer;
+		const yearlyCost = product.product_variants[ 1 ].price_before_discounts_integer;
+
+		yearlySavingsPercentage = Math.round( ( 1 - yearlyCost / ( monthlyCost * 12 ) ) * 100 );
+	}
+
 	return (
 		<>
 			{ productFeatures.map( ( feature ) => {
@@ -532,6 +542,18 @@ function CheckoutSummaryAkismetProductFeatures( { product }: { product: Response
 					</CheckoutSummaryFeaturesListItem>
 				);
 			} ) }
+
+			{ yearlySavingsPercentage > 0 && (
+				<CheckoutSummaryFeaturesListItem>
+					<WPCheckoutCheckIcon id="yearly_savings" />
+					{ translate( '%(yearlySavingsPercentage)s%% price reduction for yearly term', {
+						args: {
+							yearlySavingsPercentage,
+						},
+						comment: 'the percentage the user saves by buying yearly',
+					} ) }
+				</CheckoutSummaryFeaturesListItem>
+			) }
 		</>
 	);
 }
