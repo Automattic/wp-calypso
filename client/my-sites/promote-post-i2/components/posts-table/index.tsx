830474a377d29fa67c98bd6fcfcb6ff53a1e7448
Vassilis Dimitrakis <vassilis.dimitrakis@automattic.com>
2023-08-02 10:56:26 +0300

    Adds an experiment component in the posts table - adds an experiment component to use (with different styles than the default one)

--- client/my-sites/promote-post-i2/components/posts-table/index.tsx
+++ client/my-sites/promote-post-i2/components/posts-table/index.tsx
@@ -1,7 +1,9 @@
 import '../campaigns-table/style.scss';
 
 import { BlazablePost } from 'calypso/data/promote-post/types';
+import { Experiment } from 'calypso/lib/explat';
 import PostItem from 'calypso/my-sites/promote-post-i2/components/post-item';
+import PostItemExperiment from 'calypso/my-sites/promote-post-i2/components/post-item-experiment';
 import { ItemsLoading, SingleItemLoading } from '../campaigns-table';
 import PostsListHeader from '../posts-list/header';
 
@@ -14,6 +16,14 @@ interface Props {
 export default function PostsTable( props: Props ) {
 	const { posts, isLoading, isFetchingPageResults } = props;
 
+	const DefaultItem = posts.map( ( post: BlazablePost ) => {
+		return <PostItemExperiment key={ `post-id${ post.ID }` } post={ post } />;
+	} );
+
+	const ExperimentItem = posts.map( ( post: BlazablePost ) => {
+		return <PostItem key={ `post-id${ post.ID }` } post={ post } />;
+	} );
+
 	return (
 		<table className="promote-post-i2__table">
 			<PostsListHeader />
@@ -23,9 +33,12 @@ export default function PostsTable( props: Props ) {
 					<ItemsLoading />
 				) : (
 					<>
-						{ posts.map( ( post: BlazablePost ) => {
-							return <PostItem key={ `post-id${ post.ID }` } post={ post } />;
-						} ) }
+						<Experiment
+							name="experiment_name"
+							defaultExperience={ DefaultItem }
+							treatmentExperience={ ExperimentItem }
+							loadingExperience={ <></> }
+						/>
 						{ isFetchingPageResults && <SingleItemLoading /> }
 					</>
 				) }
