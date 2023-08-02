5e6ad613a37fa62c34869a8e06710cef7bd35705
Heyde Moura <moura.heyde@gmail.com>
2023-08-02 10:24:58 -0300

    Revert "Remove CardHeading from license assingment form"
    This reverts commit 881d86ba74cd42ac27956374df5f06f635e9b045.

--- client/jetpack-cloud/sections/partner-portal/primary/assign-license/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/primary/assign-license/index.tsx
@@ -1,6 +1,7 @@
 import { getQueryArg } from '@wordpress/url';
 import { useTranslate } from 'i18n-calypso';
 import { useEffect, useLayoutEffect } from 'react';
+import CardHeading from 'calypso/components/card-heading';
 import DocumentHead from 'calypso/components/data/document-head';
 import Main from 'calypso/components/main';
 import SidebarNavigation from 'calypso/components/sidebar-navigation';
@@ -58,6 +59,11 @@ export default function AssignLicense( {
 				} ) }
 			/>
 			<SidebarNavigation />
+			<CardHeading size={ 36 }>
+				{ translate( 'Assign your license', 'Assign your licenses', {
+					count: licenseKeysArray.length,
+				} ) }
+			</CardHeading>
 			<AssignLicenseForm sites={ sites } currentPage={ currentPage } search={ search } />
 		</Main>
 	);
