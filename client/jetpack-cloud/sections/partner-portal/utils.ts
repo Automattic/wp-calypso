db2027a83a2ce1a940ef3a97d50002405593a788
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-08-02 15:23:10 -0500

    isWooCommerceProduct: Move to separate file in ./lib

--- client/jetpack-cloud/sections/partner-portal/utils.ts
+++ client/jetpack-cloud/sections/partner-portal/utils.ts
@@ -14,17 +14,8 @@ export { default as formatApiPartner } from './lib/format-api-partner';
 export { default as getProductTitle } from './lib/get-product-title';
 export { default as selectAlphabeticallySortedProductOptions } from './lib/select-alphabetically-sorted-product-options';
 export { default as isJetpackBundle } from './lib/is-jetpack-bundle';
+export { default as isWooCommerceProduct } from './lib/is-woocommerce-product';
 export { default as areLicenseKeysAssignableToMultisite } from './lib/are-license-keys-assignable-to-multisite';
-
-/**
- * Provided a license key or a product slug, can we trust that the product is a WooCommerce product
- *
- * @param keyOrSlug string
- * @returns boolean True if WC product, false if not
- */
-export function isWooCommerceProduct( keyOrSlug: string ) {
-	return keyOrSlug.startsWith( 'woocommerce' );
-}
 
 /**
  * Provided a license key or a product slug, can we trust that the product is a WordPress.com hosting product
