5e95317baba8aa4bb4630ce509c1b5f86a145c3a
Dale du Preez <dale@automattic.com>
2023-08-02 15:05:41 +0200

    Reinstate custom handling and add logging for that code path

--- packages/launchpad/src/setup-actions.ts
+++ packages/launchpad/src/setup-actions.ts
@@ -3,6 +3,7 @@ import { addQueryArgs } from '@wordpress/url';
 import type { LaunchpadTaskActionsProps, Task } from './types';
 
 export const setUpActionsForTasks = ( {
+	siteSlug,
 	tasks,
 	tracksData,
 	extraActions,
@@ -30,6 +31,7 @@ export const setUpActionsForTasks = ( {
 	// Add actions to sorted tasks.
 	return sortedTasks.map( ( task: Task ) => {
 		let action: () => void;
+		let logMissingCalypsoPath = false;
 
 		if ( uiContext === 'calypso' && task.calypso_path !== undefined ) {
 			let targetPath = task.calypso_path;
@@ -48,7 +50,74 @@ export const setUpActionsForTasks = ( {
 						setShareSiteModalIsOpen?.( true );
 					};
 					break;
+
+				case 'site_title':
+					logMissingCalypsoPath = true;
+					action = () => {
+						window.location.assign( `/settings/general/${ siteSlug }` );
+					};
+					break;
+
+				case 'domain_claim':
+				case 'domain_upsell':
+				case 'domain_customize':
+					logMissingCalypsoPath = true;
+					action = () => {
+						window.location.assign( `/domains/add/${ siteSlug }` );
+					};
+					break;
+				case 'drive_traffic':
+					logMissingCalypsoPath = true;
+					action = () => {
+						const url = isMobile()
+							? `/marketing/connections/${ siteSlug }`
+							: `/marketing/connections/${ siteSlug }?tour=marketingConnectionsTour`;
+						window.location.assign( url );
+					};
+					break;
+				case 'add_new_page':
+					logMissingCalypsoPath = true;
+					action = () => {
+						window.location.assign( `/page/${ siteSlug }` );
+					};
+					break;
+				case 'edit_page':
+					logMissingCalypsoPath = true;
+					action = () => {
+						window.location.assign( `/pages/${ siteSlug }` );
+					};
+					break;
+				case 'update_about_page':
+					logMissingCalypsoPath = true;
+					action = () => {
+						window.location.assign( `/page/${ siteSlug }/${ task?.extra_data?.about_page_id }` );
+					};
+					break;
+				case 'customize_welcome_message':
+					logMissingCalypsoPath = true;
+					action = () => {
+						window.location.assign( `/settings/reading/${ siteSlug }#newsletter-settings` );
+					};
+					break;
+				case 'manage_subscribers':
+					logMissingCalypsoPath = true;
+					action = () => {
+						window.location.assign( `/subscribers/${ siteSlug }` );
+					};
+					break;
+				default:
+					logMissingCalypsoPath = true;
+					break;
 			}
+		}
+
+		if ( logMissingCalypsoPath ) {
+			recordTracksEvent( 'calypso_launchpad_task_missing_calypso_path', {
+				checklist_slug: checklistSlug,
+				task_id: task.id,
+				is_completed: task.completed,
+				context: launchpadContext,
+			} );
 		}
 
 		const actionDispatch = () => {
