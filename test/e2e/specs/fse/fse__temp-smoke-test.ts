c1a10f35a9a91b499d0f5eafc37c229a80cea6c1
dpasque <dan.speckhard.pasque@automattic.com>
2023-08-03 10:35:49 -0500

    IGNORE testing a change on CI

--- test/e2e/specs/fse/fse__temp-smoke-test.ts
+++ test/e2e/specs/fse/fse__temp-smoke-test.ts
@@ -5,7 +5,6 @@
  */
 
 import {
-	DataHelper,
 	envVariables,
 	SidebarComponent,
 	TestAccount,
@@ -25,73 +24,121 @@ declare const browser: Browser;
  * or getting a WSOD when trying to load the editor.
  */
 
-describe( DataHelper.createSuiteTitle( 'Site Editor Smoke Test' ), function () {
-	let page: Page;
-	let fullSiteEditorPage: FullSiteEditorPage;
+// describe( DataHelper.createSuiteTitle( 'Site Editor Smoke Test' ), function () {
+// 	let page: Page;
+// 	let fullSiteEditorPage: FullSiteEditorPage;
 
-	const features = envToFeatureKey( envVariables );
-	const accountName = getTestAccountByFeature( { ...features, variant: 'siteEditor' }, [
-		// None of our CoBlocks users use block themes, so we need to fall back to the default Gutenberg users
-		// if COBLOCKS_EDGE is set.
-		{
-			gutenberg: 'stable',
-			coblocks: 'edge',
-			siteType: 'simple',
-			variant: 'siteEditor',
-			accountName: 'siteEditorSimpleSiteUser',
-		},
-		{
-			gutenberg: 'edge',
-			coblocks: 'edge',
-			siteType: 'simple',
-			variant: 'siteEditor',
-			accountName: 'siteEditorSimpleSiteEdgeUser',
-		},
-		{
-			gutenberg: 'stable',
-			coblocks: 'edge',
-			siteType: 'atomic',
-			variant: 'siteEditor',
-			accountName: 'siteEditorAtomicSiteUser',
-		},
-		{
-			gutenberg: 'edge',
-			coblocks: 'edge',
-			siteType: 'atomic',
-			variant: 'siteEditor',
-			accountName: 'siteEditorAtomicSiteEdgeUser',
-		},
-	] );
+// 	const features = envToFeatureKey( envVariables );
+// 	const accountName = getTestAccountByFeature( { ...features, variant: 'siteEditor' }, [
+// 		// None of our CoBlocks users use block themes, so we need to fall back to the default Gutenberg users
+// 		// if COBLOCKS_EDGE is set.
+// 		{
+// 			gutenberg: 'stable',
+// 			coblocks: 'edge',
+// 			siteType: 'simple',
+// 			variant: 'siteEditor',
+// 			accountName: 'siteEditorSimpleSiteUser',
+// 		},
+// 		{
+// 			gutenberg: 'edge',
+// 			coblocks: 'edge',
+// 			siteType: 'simple',
+// 			variant: 'siteEditor',
+// 			accountName: 'siteEditorSimpleSiteEdgeUser',
+// 		},
+// 		{
+// 			gutenberg: 'stable',
+// 			coblocks: 'edge',
+// 			siteType: 'atomic',
+// 			variant: 'siteEditor',
+// 			accountName: 'siteEditorAtomicSiteUser',
+// 		},
+// 		{
+// 			gutenberg: 'edge',
+// 			coblocks: 'edge',
+// 			siteType: 'atomic',
+// 			variant: 'siteEditor',
+// 			accountName: 'siteEditorAtomicSiteEdgeUser',
+// 		},
+// 	] );
 
-	beforeAll( async () => {
-		page = await browser.newPage();
+// 	beforeAll( async () => {
+// 		page = await browser.newPage();
 
-		const testAccount = new TestAccount( accountName );
-		await testAccount.authenticate( page );
+// 		const testAccount = new TestAccount( accountName );
+// 		await testAccount.authenticate( page );
+// 	} );
+
+// 	it( 'Navigate to Full Site Editor', async function () {
+// 		// Explicitly doing sidebar navigation to ensure Calypso navigation is intact.
+// 		const sidebarComponent = new SidebarComponent( page );
+// 		await sidebarComponent.navigate( 'Appearance', 'Editor' );
+// 	} );
+
+// 	it( 'Editor endpoint loads', async function () {
+// 		await page.waitForURL( /site-editor/ );
+// 	} );
+
+// 	it( 'Open the Page template', async function () {
+// 		fullSiteEditorPage = new FullSiteEditorPage( page );
+
+// 		await fullSiteEditorPage.prepareForInteraction();
+
+// 		await fullSiteEditorPage.ensureNavigationTopLevel();
+// 		await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Templates' );
+// 		await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Index' );
+// 		await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Edit' );
+// 	} );
+
+// 	it( 'Editor canvas loads', async function () {
+// 		await fullSiteEditorPage.waitUntilLoaded();
+// 	} );
+// } );
+
+for ( let i = 0; i < 3; i++ ) {
+	run( i );
+}
+
+function run( iteration: number ) {
+	const suiteName = iteration ? `Site Editor Smoke Test ${ iteration }` : 'Site Editor Smoke Test';
+	describe( suiteName, function () {
+		let page: Page;
+		let fullSiteEditorPage: FullSiteEditorPage;
+
+		const features = envToFeatureKey( envVariables );
+		const accountName = getTestAccountByFeature( { ...features, variant: 'siteEditor' } );
+
+		beforeAll( async () => {
+			page = await browser.newPage();
+
+			const testAccount = new TestAccount( accountName );
+			await testAccount.authenticate( page );
+		} );
+
+		it( 'Navigate to Full Site Editor', async function () {
+			// Explicitly doing sidebar navigation to ensure Calypso navigation is intact.
+			const sidebarComponent = new SidebarComponent( page );
+			await sidebarComponent.navigate( 'Appearance', 'Editor' );
+		} );
+
+		it( 'Editor endpoint loads', async function () {
+			await page.waitForURL( /site-editor/ );
+			throw new Error( 'Boom' );
+		} );
+
+		it( 'Open the Page template', async function () {
+			fullSiteEditorPage = new FullSiteEditorPage( page );
+
+			await fullSiteEditorPage.prepareForInteraction();
+
+			await fullSiteEditorPage.ensureNavigationTopLevel();
+			await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Templates' );
+			await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Index' );
+			await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Edit' );
+		} );
+
+		it( 'Editor canvas loads', async function () {
+			await fullSiteEditorPage.waitUntilLoaded();
+		} );
 	} );
-
-	it( 'Navigate to Full Site Editor', async function () {
-		// Explicitly doing sidebar navigation to ensure Calypso navigation is intact.
-		const sidebarComponent = new SidebarComponent( page );
-		await sidebarComponent.navigate( 'Appearance', 'Editor' );
-	} );
-
-	it( 'Editor endpoint loads', async function () {
-		await page.waitForURL( /site-editor/ );
-	} );
-
-	it( 'Open the Page template', async function () {
-		fullSiteEditorPage = new FullSiteEditorPage( page );
-
-		await fullSiteEditorPage.prepareForInteraction();
-
-		await fullSiteEditorPage.ensureNavigationTopLevel();
-		await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Templates' );
-		await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Index' );
-		await fullSiteEditorPage.clickFullSiteNavigatorButton( 'Edit' );
-	} );
-
-	it( 'Editor canvas loads', async function () {
-		await fullSiteEditorPage.waitUntilLoaded();
-	} );
-} );
+}
