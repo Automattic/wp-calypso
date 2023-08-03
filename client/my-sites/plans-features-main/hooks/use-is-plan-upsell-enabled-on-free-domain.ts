4e6b7fc745c6f170faccb5d0310e0df142ef0938
Dehan <dehandecroos@gmail.com>
2023-08-02 22:31:28 -0400

    Use the internal isEligible mechanism to whitelist the experiment

--- client/my-sites/plans-features-main/hooks/use-is-plan-upsell-enabled-on-free-domain.ts
+++ client/my-sites/plans-features-main/hooks/use-is-plan-upsell-enabled-on-free-domain.ts
@@ -8,14 +8,9 @@ export const useIsPlanUpsellEnabledOnFreeDomain = (
 	const ONBOARDING_PM_EXPERIMENT = 'calypso_gf_signup_onboarding_pm_free_free_dont_miss_out_modal';
 	const relevantExperiment =
 		flowName === 'onboarding' ? ONBOARDING_EXPERIMENT : ONBOARDING_PM_EXPERIMENT;
-	const [ isLoading, experimentAssignment ] = useExperiment( relevantExperiment );
-
-	if ( ! [ 'onboarding', 'onboarding-pm' ].includes( flowName ?? '' ) ) {
-		return {
-			isLoading: false,
-			result: false,
-		};
-	}
+	const [ isLoading, experimentAssignment ] = useExperiment( relevantExperiment, {
+		isEligible: [ 'onboarding', 'onboarding-pm' ].includes( flowName ?? '' ),
+	} );
 	return {
 		isLoading,
 		result: experimentAssignment?.variationName === 'treatment',
