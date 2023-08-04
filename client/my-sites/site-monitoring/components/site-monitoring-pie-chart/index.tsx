532021493f5aab30880f5dfbc8105c7c6b6a4db8
sejas <antonio@sejas.es>
2023-08-04 10:42:32 +0100

    Metrics: fix legend for Pie charts

--- client/my-sites/site-monitoring/components/site-monitoring-pie-chart/index.tsx
+++ client/my-sites/site-monitoring/components/site-monitoring-pie-chart/index.tsx
@@ -15,13 +15,14 @@ export const SiteMonitoringPieChart = ( { title, className, data }: Props ) => {
 	if ( className ) {
 		classes.push( className );
 	}
+
 	return (
 		<div className={ classnames( classes ) }>
 			<header className="site-monitoring__chart-header">
 				<h2 className="site-monitoring__chart-title">{ title }</h2>
 			</header>
 			<PieChart data={ data } />
-			<PieChartLegend data={ data } />
+			<PieChartLegend data={ data } onlyPercent />
 		</div>
 	);
 };
