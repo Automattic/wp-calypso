ece5cb2bd5df8d1eed813be93a1a9c775bb268c8
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2023-08-04 05:17:22 -0700

    Use abstraction to avoid repetitive code

--- client/my-sites/site-monitoring/components/time-range-picker/index.jsx
+++ client/my-sites/site-monitoring/components/time-range-picker/index.jsx
@@ -10,32 +10,14 @@ export function calculateTimeRange( selectedOption ) {
 	let start;
 	let end;
 
-	switch ( selectedOption ) {
-		case '0':
-			// 6 hours
-			start = moment().subtract( 6, 'hours' ).unix();
-			end = now;
-			break;
-		case '1':
-			// 24 hours
-			start = moment().subtract( 24, 'hours' ).unix();
-			end = now;
-			break;
-		case '2':
-			// 3 days
-			start = moment().subtract( 3, 'days' ).unix();
-			end = now;
-			break;
-		case '3':
-			// 7 days
-			start = moment().subtract( 7, 'days' ).unix();
-			end = now;
-			break;
-		default:
-			// Default to 24 hours in case of invalid selection
-			start = moment().subtract( 24, 'hours' ).unix();
-			end = now;
-			break;
+	const bits = selectedOption.split( '-' );
+	if ( bits.length === 2 && [ 'days', 'hours' ].includes( bits[ 1 ] ) ) {
+		start = moment().subtract( parseInt( bits[ 0 ] ), bits[ 1 ] ).unix();
+		end = now;
+	} else {
+		// Default to 24 hours in case of invalid selection
+		start = moment().subtract( 24, 'hours' ).unix();
+		end = now;
 	}
 
 	return { start, end };
@@ -43,7 +25,7 @@ export function calculateTimeRange( selectedOption ) {
 
 export const TimeDateChartControls = ( { onTimeRangeChange } ) => {
 	const translate = useTranslate();
-	const [ selectedOption, setSelectedOption ] = useState( '1' ); // Default selected option is '1' (24 hours)
+	const [ selectedOption, setSelectedOption ] = useState( '24-hours' ); // Default selected option is '1' (24 hours)
 
 	// Event handler to handle changes in the SegmentedControl selection
 	const handleOptionClick = ( newSelectedOption ) => {
@@ -54,40 +36,43 @@ export const TimeDateChartControls = ( { onTimeRangeChange } ) => {
 		onTimeRangeChange( timeRange );
 	};
 
+	const options = [
+		{
+			value: '6-hours',
+			label: translate( '6 hours' ),
+		},
+		{
+			value: '24-hours',
+			label: translate( '24 hours' ),
+		},
+		{
+			value: '3-days',
+			label: translate( '3 days' ),
+		},
+		{
+			value: '7-days',
+			label: translate( '7 days' ),
+		},
+	];
+
 	return (
 		<div className="site-monitoring-time-range-picker__container">
 			<div className="site-monitoring-time-range-picker__heading">
 				{ translate( 'Time range' ) }
 			</div>
 			<SegmentedControl primary className="site-monitoring-time-range-picker__controls">
-				<SegmentedControl.Item
-					value="0"
-					selected={ selectedOption === '0' }
-					onClick={ () => handleOptionClick( '0' ) }
-				>
-					{ translate( '6 hours' ) }
-				</SegmentedControl.Item>
-				<SegmentedControl.Item
-					value="1"
-					selected={ selectedOption === '1' }
-					onClick={ () => handleOptionClick( '1' ) }
-				>
-					{ translate( '24 hours' ) }
-				</SegmentedControl.Item>
-				<SegmentedControl.Item
-					value="2"
-					selected={ selectedOption === '2' }
-					onClick={ () => handleOptionClick( '2' ) }
-				>
-					{ translate( '3 days' ) }
-				</SegmentedControl.Item>
-				<SegmentedControl.Item
-					value="3"
-					selected={ selectedOption === '3' }
-					onClick={ () => handleOptionClick( '3' ) }
-				>
-					{ translate( '7 days' ) }
-				</SegmentedControl.Item>
+				{ options.map( ( option ) => {
+					return (
+						<SegmentedControl.Item
+							key={ option.value }
+							value={ option.value }
+							selected={ selectedOption === option.value }
+							onClick={ () => handleOptionClick( option.value ) }
+						>
+							{ option.label }
+						</SegmentedControl.Item>
+					);
+				} ) }
 			</SegmentedControl>
 		</div>
 	);
