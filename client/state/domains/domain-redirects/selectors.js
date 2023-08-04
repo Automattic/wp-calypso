870d6990175c7538d030c1e11d98e9574c821723
Louis Laugesen <louis.laugesen@gmail.com>
2023-08-04 15:58:09 +1000

    Copy site-redirect to domain-redirects

---
+++ client/state/domains/domain-redirects/selectors.js
@@ -0,0 +1,7 @@
+import { initialStateForSite } from './reducer';
+
+import 'calypso/state/domains/init';
+
+export function getSiteRedirectLocation( state, siteId ) {
+	return state.domains.siteRedirect[ siteId ] || initialStateForSite;
+}
