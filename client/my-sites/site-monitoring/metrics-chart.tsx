c17888f96e9377b3e52bd0d5a3a49ef5b5cd4e7e
katinthehatsite <katerynakodonenko@gmail.com>
2023-08-04 14:41:55 +0200

    Hosting Metrics: Add a time range picker (#80088)
    ---------
    
    Co-authored-by: Kateryna Kodonenko <kateryna@automattic.com>
    Co-authored-by: Daniel Bachhuber <daniel.bachhuber@automattic.com>

--- client/my-sites/site-monitoring/metrics-chart.tsx
+++ client/my-sites/site-monitoring/metrics-chart.tsx
@@ -3,7 +3,7 @@ import useScaleGradient from '@automattic/components/src/chart-uplot/hooks/use-s
 import getGradientFill from '@automattic/components/src/chart-uplot/lib/get-gradient-fill';
 import getPeriodDateFormat from '@automattic/components/src/chart-uplot/lib/get-period-date-format';
 import { getLocaleSlug, numberFormat, useTranslate } from 'i18n-calypso';
-import { useMemo, useRef } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import uPlot from 'uplot';
 import UplotReact from 'uplot-react';
 
@@ -39,124 +39,125 @@ export default function UplotChartMetrics( {
 	const uplot = useRef< uPlot | null >( null );
 	const uplotContainer = useRef( null );
 	const { spline } = uPlot.paths;
-
 	const scaleGradient = useScaleGradient( fillColor );
 
-	const options = useMemo< uPlot.Options >( () => {
-		const defaultOptions: uPlot.Options = {
-			class: 'calypso-uplot-chart',
-			...DEFAULT_DIMENSIONS,
-			// Set incoming dates as UTC.
-			tzDate: ( ts ) => uPlot.tzDate( new Date( ts * 1e3 ), 'Etc/UTC' ),
-			fmtDate: () => {
-				return ( date ) => {
-					const chatHour = formatChatHour( date );
-					return `${ chatHour }`;
-				};
-			},
-			axes: [
-				{
-					// x-axis
-					grid: {
-						show: false,
-					},
-					ticks: {
-						stroke: '#646970',
-						width: 1,
-						size: 3,
-					},
+	const [ options ] = useState< uPlot.Options >(
+		useMemo( () => {
+			const defaultOptions: uPlot.Options = {
+				class: 'calypso-uplot-chart',
+				...DEFAULT_DIMENSIONS,
+				// Set incoming dates as UTC.
+				tzDate: ( ts ) => uPlot.tzDate( new Date( ts * 1e3 ), 'Etc/UTC' ),
+				fmtDate: () => {
+					return ( date ) => {
+						const chatHour = formatChatHour( date );
+						return `${ chatHour }`;
+					};
 				},
-				{
-					// y-axis
-					side: 1, // sets y axis side to left
-					gap: 8,
-					space: 40,
-					size: 50,
-					grid: {
-						stroke: 'rgba(220, 220, 222, 0.5)', // #DCDCDE with 0.5 opacity
-						width: 1,
+				axes: [
+					{
+						// x-axis
+						grid: {
+							show: false,
+						},
+						ticks: {
+							stroke: '#646970',
+							width: 1,
+							size: 3,
+						},
 					},
-					ticks: {
-						show: false,
+					{
+						// y-axis
+						side: 1, // sets y axis side to left
+						gap: 8,
+						space: 40,
+						size: 50,
+						grid: {
+							stroke: 'rgba(220, 220, 222, 0.5)', // #DCDCDE with 0.5 opacity
+							width: 1,
+						},
+						ticks: {
+							show: false,
+						},
 					},
-				},
-			],
-			cursor: {
-				x: false,
-				y: false,
-				points: {
-					size: ( u, seriesIdx ) => ( u.series[ seriesIdx ].points?.size || 1 ) * 2,
-					width: ( u, seriesIdx, size ) => size / 4,
-					stroke: ( u, seriesIdx ) => {
-						const stroke = u.series[ seriesIdx ]?.points?.stroke;
-						return typeof stroke === 'function'
-							? ( stroke( u, seriesIdx ) as CanvasRenderingContext2D[ 'strokeStyle' ] )
-							: ( stroke as CanvasRenderingContext2D[ 'strokeStyle' ] );
-					},
-					fill: () => '#fff',
-				},
-			},
-			series: [
-				{
-					label: translate( 'Time period' ),
-					value: ( self: uPlot, rawValue: number ) => {
-						// outputs legend content - value available when mouse is hovering the chart
-						if ( ! rawValue ) {
-							return '-';
-						}
-
-						return getPeriodDateFormat(
-							period,
-							new Date( rawValue * 1000 ),
-							getLocaleSlug() || 'en'
-						);
-					},
-				},
-				{
-					fill: solidFill ? fillColor : getGradientFill( fillColor, scaleGradient ),
-					label: translate( 'HTTP requests per sec' ),
-					stroke: '#0675C4',
-					width: 2,
-					paths: ( u, seriesIdx, idx0, idx1 ) => {
-						return spline?.()( u, seriesIdx, idx0, idx1 ) || null;
-					},
+				],
+				cursor: {
+					x: false,
+					y: false,
 					points: {
-						show: false,
+						size: ( u, seriesIdx ) => ( u.series[ seriesIdx ].points?.size || 1 ) * 2,
+						width: ( u, seriesIdx, size ) => size / 4,
+						stroke: ( u, seriesIdx ) => {
+							const stroke = u.series[ seriesIdx ]?.points?.stroke;
+							return typeof stroke === 'function'
+								? ( stroke( u, seriesIdx ) as CanvasRenderingContext2D[ 'strokeStyle' ] )
+								: ( stroke as CanvasRenderingContext2D[ 'strokeStyle' ] );
+						},
+						fill: () => '#fff',
 					},
-					value: ( self: uPlot, rawValue: number ) => {
-						if ( ! rawValue ) {
-							return '-';
+				},
+				series: [
+					{
+						label: translate( 'Time period' ),
+						value: ( self: uPlot, rawValue: number ) => {
+							// outputs legend content - value available when mouse is hovering the chart
+							if ( ! rawValue ) {
+								return '-';
+							}
+
+							return getPeriodDateFormat(
+								period,
+								new Date( rawValue * 1000 ),
+								getLocaleSlug() || 'en'
+							);
+						},
+					},
+					{
+						fill: solidFill ? fillColor : getGradientFill( fillColor, scaleGradient ),
+						label: translate( 'HTTP requests per sec' ),
+						stroke: '#0675C4',
+						width: 2,
+						paths: ( u, seriesIdx, idx0, idx1 ) => {
+							return spline?.()( u, seriesIdx, idx0, idx1 ) || null;
+						},
+						points: {
+							show: false,
+						},
+						value: ( self: uPlot, rawValue: number ) => {
+							if ( ! rawValue ) {
+								return '-';
+							}
+
+							return numberFormat( rawValue, 0 );
+						},
+					},
+				],
+				legend: {
+					isolate: true,
+					mount: ( self: uPlot, el: HTMLElement ) => {
+						// If legendContainer is defined, move the legend into it.
+						if ( legendContainer?.current ) {
+							legendContainer?.current.append( el );
 						}
-
-						return numberFormat( rawValue, 0 );
 					},
 				},
-			],
-			legend: {
-				isolate: true,
-				mount: ( self: uPlot, el: HTMLElement ) => {
-					// If legendContainer is defined, move the legend into it.
-					if ( legendContainer?.current ) {
-						legendContainer?.current.append( el );
-					}
-				},
-			},
-		};
+			};
 
-		return {
-			...defaultOptions,
-			...( typeof propOptions === 'object' ? propOptions : {} ),
-		};
-	}, [
-		fillColor,
-		legendContainer,
-		period,
-		propOptions,
-		scaleGradient,
-		solidFill,
-		spline,
-		translate,
-	] );
+			return {
+				...defaultOptions,
+				...( typeof propOptions === 'object' ? propOptions : {} ),
+			};
+		}, [
+			fillColor,
+			legendContainer,
+			period,
+			propOptions,
+			scaleGradient,
+			solidFill,
+			spline,
+			translate,
+		] )
+	);
 
 	useResize( uplot, uplotContainer );
 
