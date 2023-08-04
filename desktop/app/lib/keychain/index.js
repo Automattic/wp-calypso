3ac68cec1873d7575da947b2cae97667443410e5
Edwin Takahashi <egao@outlook.com>
2023-08-04 11:32:03 +0900

    app/lib/keychain/index.js - only import safeStorage from electron. - fix eslint issue.

--- desktop/app/lib/keychain/index.js
+++ desktop/app/lib/keychain/index.js
@@ -1,24 +1,24 @@
-const electron = require( 'electron' );
-const electronStore = require( 'electron-store' );
+const { safeStorage } = require( 'electron' );
+const ElectronStore = require( 'electron-store' );
 
 const keychainService = 'WordPress.com';
-const store = new electronStore.Store( {
+const store = new ElectronStore( {
 	name: keychainService,
 	watch: true,
 } );
 
 async function write( key, value ) {
-	if ( ! electron.safeStorage.isEncryptionAvailable() ) {
+	if ( ! safeStorage.isEncryptionAvailable() ) {
 		return;
 	}
-	const buffer = electron.safeStorage.encryptString( value );
+	const buffer = safeStorage.encryptString( value );
 	store.set( key, buffer.toString( 'latin1' ) );
 }
 
 async function read( key ) {
 	if ( store.has( key ) ) {
 		const buffer = store.get( key );
-		return electron.safeStorage.decryptString( Buffer.from( buffer, 'latin1' ) );
+		return safeStorage.decryptString( Buffer.from( buffer, 'latin1' ) );
 	}
 	return null;
 }
