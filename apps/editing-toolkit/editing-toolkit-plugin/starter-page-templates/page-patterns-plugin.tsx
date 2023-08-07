9df139cd3e451fe82dbc7c5b04884a9e2f067f72
valterlorran <valterlorran@hotmail.com>
2023-08-07 08:20:23 -0300

    Launchpad: Add the layout category to meta (#80186)
    * Add the layout category to meta
    
    * Registers the layout category meta
    
    * The selectedCategory parameter can be null
    
    * Apply feedback
    
    * Change the auth check
    
    * Fix param type

--- apps/editing-toolkit/editing-toolkit-plugin/starter-page-templates/page-patterns-plugin.tsx
+++ apps/editing-toolkit/editing-toolkit-plugin/starter-page-templates/page-patterns-plugin.tsx
@@ -62,13 +62,18 @@ export function PagePatternsPlugin( props: PagePatternsPluginProps ) {
 	}, [] );
 
 	const savePatternChoice = useCallback(
-		( name: string ) => {
+		( name: string, selectedCategory: string | null ) => {
 			// Save selected pattern slug in meta.
 			const currentMeta = getMeta() as Record< string, unknown >;
+			const currentCategory =
+				( Array.isArray( currentMeta._wpcom_template_layout_category ) &&
+					currentMeta._wpcom_template_layout_category ) ||
+				[];
 			editPost( {
 				meta: {
 					...currentMeta,
 					_starter_page_template: name,
+					_wpcom_template_layout_category: [ ...currentCategory, selectedCategory ],
 				},
 			} );
 		},
