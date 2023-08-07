640ccce52f0bfbc539ba1837dc5916f29a1e792b
edanzer <edanzer@gmail.com>
2023-08-07 17:18:45 -0600

    Temporarily remove link to subscribe modal

--- client/my-sites/site-settings/reading-newsletter-settings/SubscribeModalSetting.tsx
+++ client/my-sites/site-settings/reading-newsletter-settings/SubscribeModalSetting.tsx
@@ -1,11 +1,6 @@
 import { ToggleControl } from '@wordpress/components';
-import { addQueryArgs } from '@wordpress/url';
 import { useTranslate } from 'i18n-calypso';
-import { useSelector } from 'react-redux';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
-import getSiteEditorUrl from 'calypso/state/selectors/get-site-editor-url';
-import { getSiteOption } from 'calypso/state/sites/selectors';
-import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 export const SUBSCRIBE_MODAL_OPTION = 'sm_enabled';
 
@@ -21,20 +16,6 @@ export const SubscribeModalSetting = ( {
 	disabled,
 }: SubscribeModalSettingProps ) => {
 	const translate = useTranslate();
-	const selectedSite = useSelector( getSelectedSite );
-	const siteEditorUrl = useSelector( ( state: object ) =>
-		getSiteEditorUrl( state, selectedSite?.ID || null )
-	);
-	const themeSlug = useSelector( ( state ) =>
-		getSiteOption( state, selectedSite?.ID, 'theme_slug' )
-	);
-	const subscribeModalEditorUrl = themeSlug
-		? addQueryArgs( siteEditorUrl, {
-				postType: 'wp_template_part',
-				postId: `${ themeSlug }//subscribe-modal`,
-				canvas: 'edit',
-		  } )
-		: siteEditorUrl;
 
 	return (
 		<>
@@ -46,12 +27,7 @@ export const SubscribeModalSetting = ( {
 			/>
 			<FormSettingExplanation>
 				{ translate(
-					'Grow your subscriber list by enabling a popup modal with a subscribe form. This will show as readers scroll. {{link}}Edit the modal{{/link}}.',
-					{
-						components: {
-							link: <a href={ subscribeModalEditorUrl } target="_blank" rel="noreferrer" />,
-						},
-					}
+					'Grow your subscriber list by enabling a popup modal with a subscribe form. This will show as readers scroll.'
 				) }
 			</FormSettingExplanation>
 		</>
