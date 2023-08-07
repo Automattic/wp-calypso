03ccb2ebce5edaac10fc9f9a8ca8211994ae6211
Miguel <miguel.san.segundo@automattic.com>
2023-08-07 14:06:41 +0700

    Update header description in sections screen

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/screen-category-list.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/screen-category-list.tsx
@@ -80,7 +80,7 @@ const ScreenCategoryList = ( {
 								'Replace the selected pattern by choosing from the list of categories below.'
 						  )
 						: translate(
-								'Find the right patterns for your homepage by exploring the pattern categories below.'
+								'Find the right section patterns for your homepage by exploring the categories below.'
 						  )
 				}
 				onBack={ onBack }
