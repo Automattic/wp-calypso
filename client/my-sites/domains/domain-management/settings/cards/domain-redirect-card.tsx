ff69e488a2d161ab0bccc03199dde36168e9fcdb
Candy Tsai <candy02058912@gmail.com>
2023-08-07 18:39:49 +0800

    Fix TS errors and covert to function component

--- client/my-sites/domains/domain-management/settings/cards/domain-redirect-card.tsx
+++ client/my-sites/domains/domain-management/settings/cards/domain-redirect-card.tsx
@@ -1,8 +1,7 @@
 import { localizeUrl } from '@automattic/i18n-utils';
-import { localize } from 'i18n-calypso';
-import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { localize, translate } from 'i18n-calypso';
+import { useEffect, useState } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
 import FormButton from 'calypso/components/forms/form-button';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
 import FormSelect from 'calypso/components/forms/form-select';
@@ -18,41 +17,36 @@ import { getDomainRedirect } from 'calypso/state/domains/domain-redirects/select
 import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 import './style.scss';
 
-class DomainRedirectCard extends Component {
-	static propTypes = {
-		redirect: PropTypes.object.isRequired,
-		domainName: PropTypes.string.isRequired,
-		targetUrl: PropTypes.string.isRequired,
+interface DomainRedirectCardProps {
+	domainName: string;
+	redirect: ReturnType< typeof getDomainRedirect >;
+	targetUrl: string;
+}
+
+type PropsFromRedux = ConnectedProps< typeof connector >;
+
+const DomainRedirectCard = ( props: DomainRedirectCardProps & PropsFromRedux ) => {
+	const [ targetUrl, setTargetUrl ] = useState( '' );
+	const [ protocol, setProtocol ] = useState( props.redirect.secure ? 'https' : 'http' );
+
+	useEffect( () => {
+		props.fetchDomainRedirect( props.domainName );
+		return () => {
+			props.closeDomainRedirectNotice( props.domainName );
+		};
+	}, [ props.domainName ] );
+
+	const handleChange = ( event: React.ChangeEvent< HTMLInputElement > ) => {
+		setTargetUrl( withoutHttp( event.target.value ) );
 	};
 
-	state = {
-		targetUrl: '',
-		protocol: this.props.redirect.secure ? 'https' : 'http',
-	};
-
-	componentDidMount() {
-		this.props.fetchDomainRedirect( this.props.domainName );
-	}
-
-	componentWillUnmount() {
-		this.props.closeDomainRedirectNotice( this.props.domainName );
-	}
-
-	handleChange = ( event ) => {
-		const targetUrl = withoutHttp( event.target.value );
-		this.setState( { targetUrl } );
-	};
-
-	handleSubmit = ( event ) => {
+	const handleSubmit = ( event: React.FormEvent< HTMLFormElement > ) => {
 		event.preventDefault();
 		let targetHost = '';
 		let targetPath = '';
 		let secure = true;
 		try {
-			const url = new URL(
-				this.state.protocol + '://' + this.state.targetUrl,
-				'https://_invalid_.domain'
-			);
+			const url = new URL( protocol + '://' + targetUrl, 'https://_invalid_.domain' );
 			if ( url.origin !== 'https://_invalid_.domain' ) {
 				targetHost = url.hostname;
 				targetPath = url.pathname + url.search + url.hash;
@@ -62,97 +56,94 @@ class DomainRedirectCard extends Component {
 			// ignore
 		}
 
-		this.props
+		props
 			.updateDomainRedirect(
-				this.props.domainName,
+				props.domainName,
 				targetHost,
 				targetPath,
 				null, // forwardPaths not supported yet
 				secure
 			)
-			.then( ( success ) => {
+			.then( ( success: boolean ) => {
 				if ( success ) {
-					this.props.fetchDomainRedirect( this.props.domainName );
+					props.fetchDomainRedirect( props.domainName );
 
-					this.props.successNotice( this.props.translate( 'Site redirect updated successfully.' ), {
+					props.successNotice( translate( 'Site redirect updated successfully.' ), {
 						duration: 5000,
 						id: `site-redirect-update-notification`,
 					} );
 				} else {
-					this.props.errorNotice( this.props.redirect.notice.text );
+					props.errorNotice( props.redirect.notice.text );
 				}
 			} );
 		return false;
 	};
-
-	handleChangeProtocol = ( event ) => {
-		this.setState( { protocol: event.target.value } );
+	const handleChangeProtocol = ( event: React.ChangeEvent< HTMLSelectElement > ) => {
+		setProtocol( event.currentTarget.value );
 	};
+	const { redirect } = props;
+	const { isUpdating, isFetching } = redirect;
+	const prefix = (
+		<>
+			<FormSelect
+				name="protocol"
+				id="protocol-type"
+				value={ protocol }
+				onChange={ handleChangeProtocol }
+			>
+				<option value="https">https://</option>
+				<option value="http">http://</option>
+			</FormSelect>
+		</>
+	);
 
-	render() {
-		const { redirect, translate } = this.props;
-		const { isUpdating, isFetching } = redirect;
-		const prefix = (
-			<>
-				<FormSelect
-					name="protocol"
-					id="protocol-type"
-					value={ this.state.protocol }
-					onChange={ this.handleChangeProtocol }
-				>
-					<option value="https">https://</option>
-					<option value="http">http://</option>
-				</FormSelect>
-			</>
-		);
-		return (
-			<form onSubmit={ this.handleSubmit }>
-				<FormFieldset className="domain-redirect-card__fields">
-					<FormTextInputWithAffixes
-						disabled={ isFetching || isUpdating }
-						name="destination"
-						noWrap
-						onChange={ this.handleChange }
-						prefix={ prefix }
-						value={ this.state.targetUrl || this.props.targetUrl }
-						id="domain-redirect__input"
-					/>
+	return (
+		<form onSubmit={ handleSubmit }>
+			<FormFieldset className="domain-redirect-card__fields">
+				<FormTextInputWithAffixes
+					disabled={ isFetching || isUpdating }
+					name="destination"
+					noWrap
+					onChange={ handleChange }
+					prefix={ prefix }
+					value={ targetUrl || props.targetUrl }
+					id="domain-redirect__input"
+				/>
 
-					<p className="domain-redirect-card__explanation">
-						{ translate(
-							'Requests to your domain will receive an HTTP redirect here. ' +
-								'{{learnMoreLink}}Learn more{{/learnMoreLink}}',
-							{
-								components: {
-									learnMoreLink: (
-										<a
-											href={ localizeUrl( DOMAIN_REDIRECT ) }
-											target="_blank"
-											rel="noopener noreferrer"
-										/>
-									),
-								},
-							}
-						) }
-					</p>
-				</FormFieldset>
-				<FormButton
-					disabled={
-						isFetching ||
-						isUpdating ||
-						( this.props.redirect?.targetUrl === this.state.targetUrl &&
-							( this.props.redirect?.secure ? 'https' : 'http' ) === this.state.protcol )
-					}
-				>
-					{ translate( 'Update' ) }
-				</FormButton>
-			</form>
-		);
-	}
-}
+				<p className="domain-redirect-card__explanation">
+					{ translate(
+						'Requests to your domain will receive an HTTP redirect here. ' +
+							'{{learnMoreLink}}Learn more{{/learnMoreLink}}',
+						{
+							components: {
+								learnMoreLink: (
+									<a
+										href={ localizeUrl( DOMAIN_REDIRECT ) }
+										target="_blank"
+										rel="noopener noreferrer"
+									/>
+								),
+							},
+						}
+					) }
+				</p>
+			</FormFieldset>
+			<FormButton
+				disabled={
+					isFetching ||
+					isUpdating ||
+					( props.redirect?.targetUrl === targetUrl &&
+						( props.redirect?.secure ? 'https' : 'http' ) === protocol )
+				}
+			>
+				{ translate( 'Update' ) }
+			</FormButton>
+		</form>
+	);
+};
 
-export default connect(
-	( state, ownProps ) => {
+const connector = connect(
+	( state, ownProps: DomainRedirectCardProps ) => {
 		const redirect = getDomainRedirect( state, ownProps.domainName );
 		let targetUrl = '';
 		try {
@@ -177,4 +168,6 @@ export default connect(
 		successNotice,
 		errorNotice,
 	}
-)( localize( DomainRedirectCard ) );
+);
+
+export default connector( localize( DomainRedirectCard ) );
