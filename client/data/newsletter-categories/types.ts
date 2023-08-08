6c470d0c268c8c6c336cde5be21600322a1739f5
Tim Broddin <tim@wannabes.be>
2023-08-08 13:14:44 +0200

    Newsletter categories: adds react-query to fetch all categories (#80306)
    * This commit adds the query for fetching categories.
    
    * Fix failing test
    
    * Fix failing test
    
    * Add extra test
    
    * Typo in test description

--- client/data/newsletter-categories/types.ts
+++ client/data/newsletter-categories/types.ts
@@ -1,3 +1,16 @@
+export type Category = {
+	id: number;
+	count: number;
+	description: string;
+	link: string;
+	name: string;
+	slug: string;
+	taxonomy: string;
+	parent: number;
+	meta: any;
+	_links: any;
+};
+
 export type NewsletterCategories = {
 	newsletterCategories: NewsletterCategory[];
 };
