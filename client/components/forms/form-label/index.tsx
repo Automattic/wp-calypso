c423854d785768c222c16f6337fe97116266db5f
Jeremy Yip <jeremy.yip@automattic.com>
2023-08-08 16:09:10 -0700

    Create form label shim

--- client/components/forms/form-label/index.tsx
+++ client/components/forms/form-label/index.tsx
@@ -1,38 +1,3 @@
-import classnames from 'classnames';
-import { useTranslate } from 'i18n-calypso';
-import { Children, FunctionComponent, LabelHTMLAttributes } from 'react';
-
-import './style.scss';
-
-export interface Props {
-	optional?: boolean;
-	required?: boolean;
-}
-
-type LabelProps = LabelHTMLAttributes< HTMLLabelElement >;
-
-const FormLabel: FunctionComponent< Props & LabelProps > = ( {
-	children,
-	required,
-	optional,
-	className, // Via LabelProps
-	...labelProps
-} ) => {
-	const translate = useTranslate();
-
-	const hasChildren: boolean = Children.count( children ) > 0;
-
-	return (
-		<label { ...labelProps } className={ classnames( className, 'form-label' ) }>
-			{ children }
-			{ hasChildren && required && (
-				<small className="form-label__required">{ translate( 'Required' ) }</small>
-			) }
-			{ hasChildren && optional && (
-				<small className="form-label__optional">{ translate( 'Optional' ) }</small>
-			) }
-		</label>
-	);
-};
+import { FormLabel } from '@automattic/components';
 
 export default FormLabel;
