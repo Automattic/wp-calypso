089a6596b716859a8c508b623a1526d091021761
Wojtek Naruniec <wojtek@naruniec.me>
2023-08-08 10:07:13 +0200

    Use existing error constants for consistency

--- client/components/jetpack/connection-health/constants.js
+++ client/components/jetpack/connection-health/constants.js
@@ -1,1 +1,2 @@
+export const UNKNOWN_ERROR = 'unknown_error';
 export const DNS_ERROR = 'dns_error';
--- client/components/jetpack/connection-health/index.tsx
+++ client/components/jetpack/connection-health/index.tsx
@@ -1,7 +1,7 @@
 import { localizeUrl } from '@automattic/i18n-utils';
 import { useTranslate } from 'i18n-calypso';
 import { useState } from 'react';
-import { DNS_ERROR } from './constants';
+import { DNS_ERROR, UNKNOWN_ERROR } from './constants';
 import { ErrorNotice } from './error-notice';
 import { useCheckJetpackConnectionHealth } from './use-check-jetpack-connection-health';
 
@@ -33,7 +33,7 @@ export const JetpackConnectionHealthBanner = ( { siteId }: Props ) => {
 	if ( jetpackConnectionHealth?.error === DNS_ERROR ) {
 		return (
 			<ErrorNotice
-				errorType="dns"
+				errorType={ DNS_ERROR }
 				errorText={ translate(
 					"Your domain is not properly set up to point to your site. Reset your domain's A records in the Domains section to fix this."
 				) }
@@ -47,7 +47,7 @@ export const JetpackConnectionHealthBanner = ( { siteId }: Props ) => {
 
 	return (
 		<ErrorNotice
-			errorType="default"
+			errorType={ UNKNOWN_ERROR }
 			errorText={ translate( 'Jetpack is unable to communicate with your site.' ) }
 			noticeActionHref={ localizeUrl(
 				'https://wordpress.com/support/why-is-my-site-down/#theres-an-issue-with-your-sites-jetpack-connection'
