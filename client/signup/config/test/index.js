f99fac79b9446565112355fbeeb6ecd61f64929e
Dehan <dehandecroos@gmail.com>
2023-08-09 11:26:53 -0400

    Review fixe

--- client/signup/config/test/index.js
+++ client/signup/config/test/index.js
@@ -20,6 +20,18 @@ describe( 'index', () => {
 		}
 	} );
 
+	/***
+	 * Not having the same dependencies has been a source of bugs in the signup framework, where the flow would fail in production.
+	 * - p1691481660481399-slack-C04U5A26MJB
+	 * - p1689619382168349-slack-C02FMH4G8
+	 * - https://github.com/Automattic/wp-calypso/pull/17523
+	 *
+	 * This tests makes sure that any change that introduces a dependency
+	 * is also introduced in all linked steps to a given module.
+	 * Eg: if a dependency was added to the `domain-only` step, then then same dependency
+	 * needs to be added to all steps that are linked by the module `domains` ( as mapped in `calypso/signup/config/step-components` ).
+	 * This dependency can be added as an optionalDependency if the dependency is provided conditionally
+	 */
 	test( 'All steps should have the same dependencies provided', () => {
 		const stepModuleMap = getStepModuleMap();
 		const moduleStepMap = {};
@@ -31,45 +43,35 @@ describe( 'index', () => {
 		} );
 		const allStepDefinitions = generateSteps();
 
-		Object.values( moduleStepMap ).forEach( ( moduleRelatedSteps ) => {
-			const allDependenciesProvidedForModule = new Set();
-			if ( moduleRelatedSteps.length > 1 && allStepDefinitions ) {
+		Object.values( moduleStepMap )
+			.filter( ( moduleRelatedSteps ) => moduleRelatedSteps.length > 1 )
+			.forEach( ( moduleRelatedSteps ) => {
+				/**
+				 * Filter out a unique list of all dependencies
+				 * provided by all steps that are linked to a module
+				 */
+				let allDependenciesProvidedForModule = new Set();
 				moduleRelatedSteps.forEach( ( step ) => {
 					( allStepDefinitions[ step ]?.providesDependencies ?? [] ).forEach( ( d ) =>
 						allDependenciesProvidedForModule.add( d )
 					);
 				} );
-				const allDependenciesSortedArray = Array.from( allDependenciesProvidedForModule );
-				allDependenciesSortedArray.sort();
+				allDependenciesProvidedForModule = Array.from( allDependenciesProvidedForModule );
 
-				moduleRelatedSteps.forEach( ( step ) => {
-					if ( allStepDefinitions[ step ] ) {
-						const stepProvidedDependencies = (
-							allStepDefinitions[ step ]?.providesDependencies ?? []
-						).slice();
-						stepProvidedDependencies.sort();
+				/**
+				 * Go over all steps that are linked to a module
+				 * and make sure that they all provide the same dependencies
+				 */
+				moduleRelatedSteps
+					.filter( ( step ) => allStepDefinitions[ step ]?.providesDependencies )
+					.forEach( ( step ) => {
+						const stepProvidedDependencies = allStepDefinitions[ step ].providesDependencies;
 
-						/***
-					 * Not having the same dependencies has been a source of bugs in the signup framework
-
-					 * - p1691481660481399-slack-C04U5A26MJB
-					 * - p1689619382168349-slack-C02FMH4G8
-					 * - https://github.com/Automattic/wp-calypso/pull/17523
-					 *
-					 * Where the flow would fail in production. This tests makes sure that any change that introduces a dependency
-					 * is also introduced in all linked steps to a given module.
-					 * Eg: if a dependency was added to the `domain-only` step, then then same dependency
-					 * needs to be added to all steps that are linked by the module `domains` ( as mapped in `calypso/signup/config/step-components` ).
-					 * This dependency can be added as an optionalDependency if the dependency is provided conditionally
-					 */
-						// eslint-disable-next-line jest/no-conditional-expect
 						expect( { step, deps: stepProvidedDependencies } ).toEqual( {
 							step,
-							deps: allDependenciesSortedArray,
+							deps: expect.arrayContaining( allDependenciesProvidedForModule ),
 						} );
-					}
-				} );
-			}
-		} );
+					} );
+			} );
 	} );
 } );
