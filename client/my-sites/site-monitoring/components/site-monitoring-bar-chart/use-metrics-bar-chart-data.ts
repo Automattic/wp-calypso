a48b4ff21fee60dff7af47d438eb889b550ba2df
Antonio Sejas <antonio@sejas.es>
2023-08-09 21:15:36 +0100

    Hosting metrics: Add tooltips to bar chart legend (#80393)

--- client/my-sites/site-monitoring/components/site-monitoring-bar-chart/use-metrics-bar-chart-data.ts
+++ client/my-sites/site-monitoring/components/site-monitoring-bar-chart/use-metrics-bar-chart-data.ts
@@ -4,7 +4,6 @@ import { TimeRange } from '../../metrics-tab';
 import { PeriodData, useSiteMetricsQuery } from '../../use-metrics-query';
 
 const STATUS_CODES = [ 200, 401, 400, 404, 500 ];
-const FILL_COLORS = [ '#68B3E8', '#A7AAAD', '#F2D76B', '#09B585', '#F283AA' ];
 
 interface UseMetricsBarChartDataParams {
 	siteId: number | null;
@@ -94,6 +93,5 @@ export function useMetricsBarChartData( { siteId, timeRange }: UseMetricsBarChar
 			}
 			return getLongDate( date, locale );
 		} ),
-		fillColors: FILL_COLORS,
 	};
 }
