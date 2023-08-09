511104274bca34f5ff998dc2d9ad624ff73dacac
CodeyGuyDylan <dylan.munson@automattic.com>
2023-08-09 09:06:20 -0600

    Display jetpack logo and gravatar user section when data is loading or if nothing is returned

--- client/jetpack-cloud/sections/pricing/jpcom-masterbar/index.tsx
+++ client/jetpack-cloud/sections/pricing/jpcom-masterbar/index.tsx
@@ -97,236 +97,232 @@ const JetpackComMasterbar: React.FC< Props > = ( { pathname } ) => {
 		<>
 			{ jetpackSaleCoupon && <JetpackSaleBanner coupon={ jetpackSaleCoupon } /> }
 
-			{ menuDataStatus === 'success' ? (
-				<div className="jpcom-masterbar">
-					<header className="header js-header force-opaque">
-						<div className="header__content-wrapper">
-							<div
-								className="header__content-background-wrapper-sentinal"
-								{ ...outerDivProps }
-							></div>
-							<div className={ classes }>
-								<nav className="header__content is-sticky">
-									<a className="header__skip" href={ `#${ MAIN_CONTENT_ID }` }>
-										{ translate( 'Skip to main content' ) }
-									</a>
-									<ExternalLink
-										className="header__home-link"
-										href={ localizeUrl( JETPACK_COM_BASE_URL, locale ) }
-										aria-label={ translate( 'Jetpack home' ) }
-										onClick={ () => recordTracksEvent( 'calypso_jetpack_nav_logo_click' ) }
-									>
-										<JetpackLogo full size={ 38 } />
-									</ExternalLink>
-									{ shouldShowCart && <CloudCart /> }
-									<a
-										className="header__mobile-btn mobile-btn js-mobile-btn"
-										href="#mobile-menu"
-										aria-expanded="false"
-									>
-										<span className="mobile-btn__icon" aria-hidden="true">
-											<span className="mobile-btn__inner"></span>
-										</span>
-										<span className="mobile-btn__label">{ translate( 'Menu' ) }</span>
-									</a>
-									<div className="header__nav-wrapper js-mobile-menu" id="mobile-menu">
-										<ul className="header__sections-list js-nav-list">
-											{ sections &&
-												sections.sort( sortByMenuOrder ).map( ( { label, id, href, items } ) => {
-													const hasChildren = Object.keys( items ).length > 0;
-													const Tag = hasChildren ? 'a' : ExternalLink;
+			<div className="jpcom-masterbar">
+				<header className="header js-header force-opaque">
+					<div className="header__content-wrapper">
+						<div className="header__content-background-wrapper-sentinal" { ...outerDivProps }></div>
+						<div className={ classes }>
+							<nav className="header__content is-sticky">
+								<a className="header__skip" href={ `#${ MAIN_CONTENT_ID }` }>
+									{ translate( 'Skip to main content' ) }
+								</a>
+								<ExternalLink
+									className="header__home-link"
+									href={ localizeUrl( JETPACK_COM_BASE_URL, locale ) }
+									aria-label={ translate( 'Jetpack home' ) }
+									onClick={ () => recordTracksEvent( 'calypso_jetpack_nav_logo_click' ) }
+								>
+									<JetpackLogo full size={ 38 } />
+								</ExternalLink>
+								{ shouldShowCart && <CloudCart /> }
+								<a
+									className="header__mobile-btn mobile-btn js-mobile-btn"
+									href="#mobile-menu"
+									aria-expanded="false"
+								>
+									<span className="mobile-btn__icon" aria-hidden="true">
+										<span className="mobile-btn__inner"></span>
+									</span>
+									<span className="mobile-btn__label">{ translate( 'Menu' ) }</span>
+								</a>
+								<div className="header__nav-wrapper js-mobile-menu" id="mobile-menu">
+									<ul className="header__sections-list js-nav-list">
+										{ menuDataStatus === 'success' && sections ? (
+											sections.sort( sortByMenuOrder ).map( ( { label, id, href, items } ) => {
+												const hasChildren = Object.keys( items ).length > 0;
+												const Tag = hasChildren ? 'a' : ExternalLink;
 
-													return (
-														<li
-															className={ classNames( {
-																'is-active':
-																	pathname &&
-																	href &&
-																	href !== '#' &&
-																	new URL( trailingslashit( href ) ).pathname ===
-																		trailingslashit( pathname ),
-															} ) }
-															key={ href || id }
+												return (
+													<li
+														className={ classNames( {
+															'is-active':
+																pathname &&
+																href &&
+																href !== '#' &&
+																new URL( trailingslashit( href ) ).pathname ===
+																	trailingslashit( pathname ),
+														} ) }
+														key={ href || id }
+													>
+														<Tag
+															className={ hasChildren ? 'header__menu-btn js-menu-btn' : '' }
+															href={ href ? localizeUrl( href, locale ) : `#${ id }` }
+															aria-expanded={ hasChildren ? false : undefined }
+															onClick={ href ? onLinkClick : undefined }
 														>
-															<Tag
-																className={ hasChildren ? 'header__menu-btn js-menu-btn' : '' }
-																href={ href ? localizeUrl( href, locale ) : `#${ id }` }
-																aria-expanded={ hasChildren ? false : undefined }
-																onClick={ href ? onLinkClick : undefined }
-															>
-																{ label }
-																{ hasChildren && <Gridicon icon="chevron-down" size={ 18 } /> }
-															</Tag>
-															{ hasChildren && (
-																<div id={ id } className="header__submenu js-menu" tabIndex={ -1 }>
-																	<div className="header__submenu-content">
-																		<button className="header__back-btn js-menu-back">
-																			<Gridicon icon="chevron-left" size={ 18 } />
-																			{ translate( 'Back' ) }
-																		</button>
-																		<ul className="header__submenu-categories-list">
-																			{ Array.from( Object.values( items ) )
-																				.sort( sortByMenuOrder )
-																				.map( ( { label, tagline, href, items } ) => {
-																					return (
-																						<li key={ href }>
-																							<ExternalLink
-																								className="header__submenu-category header__submenu-link"
-																								href={ localizeUrl( href, locale ) }
-																								onClick={ onLinkClick }
-																							>
-																								<span className="header__submenu-label">
-																									<span className="header__submenu-chevron">
-																										<Gridicon icon="chevron-right" size={ 18 } />
-																									</span>
-																									{ label }
-																								</span>
-																								{ tagline && (
-																									<span className="header__submenu-tagline">
-																										{ tagline }
-																									</span>
-																								) }
-																							</ExternalLink>
-																							<ul className="header__submenu-links-list">
-																								{ Array.from( Object.values( items ) )
-																									.sort( sortByMenuOrder )
-																									.map( ( { label, tagline, href } ) => (
-																										<li key={ href }>
-																											<ExternalLink
-																												className="header__submenu-link"
-																												href={ localizeUrl( href, locale ) }
-																												onClick={ onLinkClick }
-																											>
-																												<span className="header__submenu-label">
-																													{ label }
-																												</span>
-																												{ tagline && (
-																													<span className="header__submenu-tagline">
-																														{ tagline }
-																													</span>
-																												) }
-																											</ExternalLink>
-																										</li>
-																									) ) }
-																							</ul>
-																						</li>
-																					);
-																				} ) }
-																		</ul>
-
-																		<hr className="header__submenu-section-separator" />
-
-																		<div className="header__submenu-bottom-section">
-																			<div className="header__submenu-bundles">
-																				<p className="header__submenu-category-heading">
-																					{ bundles?.label }
-																				</p>
-
-																				<ul className="header__submenu-links-list">
-																					{ bundles &&
-																						Array.from( Object.values( bundles.items ) )
-																							.sort( sortByMenuOrder )
-																							.map( ( { label, tagline, href } ) => (
-																								<li key={ `bundles-${ href }` }>
-																									<ExternalLink
-																										className="header__submenu-link"
-																										href={ localizeUrl( href, locale ) }
-																										onClick={ onLinkClick }
-																									>
-																										<span className="header__submenu-label">
-																											{ label }
-																										</span>
-																										{ tagline && (
-																											<span className="header__submenu-tagline">
-																												{ tagline }
-																											</span>
-																										) }
-																									</ExternalLink>
-																								</li>
-																							) ) }
-																				</ul>
-																			</div>
-																			<div className="header__submenu-apps-wrapper">
-																				<p className="header__submenu-category-heading">
-																					{ appsLink.categoryLabel }
-																				</p>
-
-																				<ul className="header__submenu-links-list">
-																					<li>
+															{ label }
+															{ hasChildren && <Gridicon icon="chevron-down" size={ 18 } /> }
+														</Tag>
+														{ hasChildren && (
+															<div id={ id } className="header__submenu js-menu" tabIndex={ -1 }>
+																<div className="header__submenu-content">
+																	<button className="header__back-btn js-menu-back">
+																		<Gridicon icon="chevron-left" size={ 18 } />
+																		{ translate( 'Back' ) }
+																	</button>
+																	<ul className="header__submenu-categories-list">
+																		{ Array.from( Object.values( items ) )
+																			.sort( sortByMenuOrder )
+																			.map( ( { label, tagline, href, items } ) => {
+																				return (
+																					<li key={ href }>
 																						<ExternalLink
-																							className="header__submenu-link"
-																							href={ localizeUrl( appsLink.href, locale ) }
+																							className="header__submenu-category header__submenu-link"
+																							href={ localizeUrl( href, locale ) }
 																							onClick={ onLinkClick }
 																						>
 																							<span className="header__submenu-label">
-																								{ appsLink.label }
+																								<span className="header__submenu-chevron">
+																									<Gridicon icon="chevron-right" size={ 18 } />
+																								</span>
+																								{ label }
 																							</span>
-																							<span className="header__submenu-tagline">
-																								{ appsLink.tagline }
-																							</span>
+																							{ tagline && (
+																								<span className="header__submenu-tagline">
+																									{ tagline }
+																								</span>
+																							) }
 																						</ExternalLink>
+																						<ul className="header__submenu-links-list">
+																							{ Array.from( Object.values( items ) )
+																								.sort( sortByMenuOrder )
+																								.map( ( { label, tagline, href } ) => (
+																									<li key={ href }>
+																										<ExternalLink
+																											className="header__submenu-link"
+																											href={ localizeUrl( href, locale ) }
+																											onClick={ onLinkClick }
+																										>
+																											<span className="header__submenu-label">
+																												{ label }
+																											</span>
+																											{ tagline && (
+																												<span className="header__submenu-tagline">
+																													{ tagline }
+																												</span>
+																											) }
+																										</ExternalLink>
+																									</li>
+																								) ) }
+																						</ul>
 																					</li>
-																				</ul>
-																			</div>
+																				);
+																			} ) }
+																	</ul>
+
+																	<hr className="header__submenu-section-separator" />
+
+																	<div className="header__submenu-bottom-section">
+																		<div className="header__submenu-bundles">
+																			<p className="header__submenu-category-heading">
+																				{ bundles?.label }
+																			</p>
+
+																			<ul className="header__submenu-links-list">
+																				{ bundles &&
+																					Array.from( Object.values( bundles.items ) )
+																						.sort( sortByMenuOrder )
+																						.map( ( { label, tagline, href } ) => (
+																							<li key={ `bundles-${ href }` }>
+																								<ExternalLink
+																									className="header__submenu-link"
+																									href={ localizeUrl( href, locale ) }
+																									onClick={ onLinkClick }
+																								>
+																									<span className="header__submenu-label">
+																										{ label }
+																									</span>
+																									{ tagline && (
+																										<span className="header__submenu-tagline">
+																											{ tagline }
+																										</span>
+																									) }
+																								</ExternalLink>
+																							</li>
+																						) ) }
+																			</ul>
+																		</div>
+																		<div className="header__submenu-apps-wrapper">
+																			<p className="header__submenu-category-heading">
+																				{ appsLink.categoryLabel }
+																			</p>
+
+																			<ul className="header__submenu-links-list">
+																				<li>
+																					<ExternalLink
+																						className="header__submenu-link"
+																						href={ localizeUrl( appsLink.href, locale ) }
+																						onClick={ onLinkClick }
+																					>
+																						<span className="header__submenu-label">
+																							{ appsLink.label }
+																						</span>
+																						<span className="header__submenu-tagline">
+																							{ appsLink.tagline }
+																						</span>
+																					</ExternalLink>
+																				</li>
+																			</ul>
 																		</div>
 																	</div>
 																</div>
-															) }
-														</li>
-													);
-												} ) }
-										</ul>
-										{ shouldShowCart && <CloudCart /> }
-										<ul className="header__actions-list">
-											<li
-												className={ classNames( 'header__user-menu user-menu ', {
-													'is-logged-in': isLoggedIn,
-												} ) }
-											>
-												{ isLoggedIn ? (
-													<>
-														<a className="user-menu__btn js-user-menu-btn" href="#profile">
-															<Gravatar user={ user } className="user-menu__avatar" />
-														</a>
-														<div id="profile" className="user-menu__tooltip js-user-menu">
-															<div className="tooltip">
-																<span className="user-menu__greetings">
-																	{ translate( 'Hi, %s!', {
-																		args: user?.display_name || user?.username,
-																	} ) }
-																</span>
-																<ul className="user-menu__list">
-																	<li>
-																		<a
-																			className="js-manage-sites"
-																			href={ localizeUrl( '/', locale ) }
-																		>
-																			{ translate( 'Manage your sites' ) }
-																		</a>
-																	</li>
-																</ul>
 															</div>
-														</div>
-													</>
-												) : (
-													<a
-														className="header__action-link js-login"
-														href={ localizeUrl( '/login/', locale ) }
-													>
-														{ translate( 'Log in' ) }
+														) }
+													</li>
+												);
+											} )
+										) : (
+											<></>
+										) }
+									</ul>
+									{ shouldShowCart && <CloudCart /> }
+									<ul className="header__actions-list">
+										<li
+											className={ classNames( 'header__user-menu user-menu ', {
+												'is-logged-in': isLoggedIn,
+											} ) }
+										>
+											{ isLoggedIn ? (
+												<>
+													<a className="user-menu__btn js-user-menu-btn" href="#profile">
+														<Gravatar user={ user } className="user-menu__avatar" />
 													</a>
-												) }
-											</li>
-										</ul>
-									</div>
-								</nav>
-							</div>
+													<div id="profile" className="user-menu__tooltip js-user-menu">
+														<div className="tooltip">
+															<span className="user-menu__greetings">
+																{ translate( 'Hi, %s!', {
+																	args: user?.display_name || user?.username,
+																} ) }
+															</span>
+															<ul className="user-menu__list">
+																<li>
+																	<a
+																		className="js-manage-sites"
+																		href={ localizeUrl( '/', locale ) }
+																	>
+																		{ translate( 'Manage your sites' ) }
+																	</a>
+																</li>
+															</ul>
+														</div>
+													</div>
+												</>
+											) : (
+												<a
+													className="header__action-link js-login"
+													href={ localizeUrl( '/login/', locale ) }
+												>
+													{ translate( 'Log in' ) }
+												</a>
+											) }
+										</li>
+									</ul>
+								</div>
+							</nav>
 						</div>
-					</header>
-				</div>
-			) : (
-				<></>
-			) }
+					</div>
+				</header>
+			</div>
 		</>
 	);
 	/* eslint-enable wpcalypso/jsx-classname-namespace */
