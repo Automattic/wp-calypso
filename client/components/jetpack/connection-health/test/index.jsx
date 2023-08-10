fb50be35ae977121e991ab19da1348ad991b973c
Wojtek Naruniec <wojtek@naruniec.me>
2023-08-10 16:11:58 +0200

    Add tests for three existing component states

---
+++ client/components/jetpack/connection-health/test/index.jsx
@@ -0,0 +1,87 @@
+/**
+ * @jest-environment jsdom
+ */
+import { screen } from '@testing-library/react';
+import { merge } from 'lodash';
+import {
+	DNS_ERROR,
+	FATAL_ERROR,
+	UNKNOWN_ERROR,
+} from 'calypso/components/jetpack/connection-health/constants';
+import { jetpackConnectionHealth } from 'calypso/state/jetpack-connection-health/reducer';
+import { renderWithProvider } from 'calypso/test-helpers/testing-library';
+import { JetpackConnectionHealthBanner } from '..';
+
+const mockError = jest.fn().mockReturnValue( UNKNOWN_ERROR );
+
+jest.mock(
+	'calypso/components/jetpack/connection-health/use-check-jetpack-connection-health',
+	() => ( {
+		useCheckJetpackConnectionHealth: jest.fn( () => ( {
+			data: { is_healthy: false, error: mockError() },
+		} ) ),
+	} )
+);
+
+const initialReduxState = {
+	sites: {
+		items: { 1: { ID: 1, title: 'Test Site', jetpack: true } },
+	},
+};
+
+const render = ( el, options = {} ) =>
+	renderWithProvider( el, {
+		...options,
+		initialState: merge( initialReduxState, options.initialState ),
+		reducers: { jetpackConnectionHealth },
+	} );
+
+describe( 'JetpackConnectionHealthBanner', () => {
+	describe( 'component rendering', () => {
+		test( 'shows generic message', () => {
+			const initialState = {
+				jetpackConnectionHealth: {
+					1: { jetpack_connection_problem: true },
+				},
+			};
+
+			render( <JetpackConnectionHealthBanner siteId={ 1 } />, { initialState } );
+
+			expect(
+				screen.getByText( /Jetpack is unable to communicate with your site./i )
+			).toBeVisible();
+		} );
+
+		test( 'shows fatal error message', () => {
+			mockError.mockReturnValue( FATAL_ERROR );
+
+			const initialState = {
+				jetpackConnectionHealth: {
+					1: { jetpack_connection_problem: true },
+				},
+			};
+
+			render( <JetpackConnectionHealthBanner siteId={ 1 } />, { initialState } );
+
+			expect(
+				screen.getByText(
+					/Jetpack is unable to communicate with your site due to a critical error that has occurred there/i
+				)
+			).toBeVisible();
+		} );
+
+		test( 'shows dns error message', () => {
+			mockError.mockReturnValue( DNS_ERROR );
+
+			const initialState = {
+				jetpackConnectionHealth: {
+					1: { jetpack_connection_problem: true },
+				},
+			};
+
+			render( <JetpackConnectionHealthBanner siteId={ 1 } />, { initialState } );
+
+			expect( screen.getByText( /Jetpack is unable to connect to your domain./i ) ).toBeVisible();
+		} );
+	} );
+} );
