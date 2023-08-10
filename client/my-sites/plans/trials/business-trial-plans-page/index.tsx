2d5d67e32fe5d4904e67eeb5d742c5d72adf1815
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2023-08-10 07:59:09 +0200

    Change default plan grid

--- client/my-sites/plans/trials/business-trial-plans-page/index.tsx
+++ client/my-sites/plans/trials/business-trial-plans-page/index.tsx
@@ -1,26 +1,36 @@
-import { PLAN_BUSINESS } from '@automattic/calypso-products';
+import { PLAN_BUSINESS, getPlanPath } from '@automattic/calypso-products';
+import { MinimalRequestCartProduct } from '@automattic/shopping-cart';
+import { useCallback } from 'react';
 import BodySectionCssClass from 'calypso/layout/body-section-css-class';
+import { getTrialCheckoutUrl } from 'calypso/lib/trials/get-trial-checkout-url';
 import PlansFeaturesMain from 'calypso/my-sites/plans-features-main';
-import { IntervalType } from 'calypso/my-sites/plans-features-main/types';
 import TrialBanner from '../trial-banner';
-import type { Site } from 'calypso/my-sites/scan/types';
 
 import './style.scss';
 
 interface BusinessTrialPlansPageProps {
-	redirectToAddDomainFlow: boolean;
-	domainAndPlanPackage: boolean;
-	intervalType: IntervalType;
-	customerType: string;
-	selectedFeature: string;
-	redirectTo: string;
-	selectedSite: Site;
-	isDomainAndPlanPackageFlow: boolean;
-	isDomainUpsell: boolean;
-	isDomainUpsellSuggested: boolean;
+	siteId: number | null;
+	siteSlug: string;
+	triggerTracksEvent?: ( planSlug: string ) => void;
 }
 
 const BusinessTrialPlansPage = ( props: BusinessTrialPlansPageProps ) => {
+	const { siteId, siteSlug, triggerTracksEvent } = props;
+
+	const onUpgradeClick = useCallback(
+		( cartItem?: MinimalRequestCartProduct | null ) => {
+			const upgradePlanSlug = cartItem?.product_slug ?? PLAN_BUSINESS;
+
+			triggerTracksEvent?.( upgradePlanSlug );
+
+			const planPath = getPlanPath( upgradePlanSlug ) ?? '';
+			const checkoutUrl = getTrialCheckoutUrl( { productSlug: planPath, siteSlug } );
+
+			page( checkoutUrl );
+		},
+		[ siteSlug, triggerTracksEvent ]
+	);
+
 	return (
 		<>
 			<BodySectionCssClass bodyClass={ [ 'is-migration-trial-plan' ] } />
@@ -30,18 +40,13 @@ const BusinessTrialPlansPage = ( props: BusinessTrialPlansPageProps ) => {
 			</div>
 
 			<PlansFeaturesMain
-				redirectToAddDomainFlow={ props.redirectToAddDomainFlow }
-				hidePlanTypeSelector={ false }
-				hideFreePlan={ true }
-				customerType={ props.customerType }
-				intervalType={ props.intervalType }
-				selectedFeature={ props.selectedFeature }
-				siteId={ props.selectedSite?.ID }
-				plansWithScroll={ false }
+				siteId={ siteId }
+				onUpgradeClick={ onUpgradeClick }
+				intervalType="yearly"
+				hidePlanTypeSelector={ true }
+				hideUnavailableFeatures={ true }
 				selectedPlan={ PLAN_BUSINESS }
-				hidePlansFeatureComparison={ props.isDomainAndPlanPackageFlow }
-				showLegacyStorageFeature={ false }
-				isSpotlightOnCurrentPlan={ ! props.isDomainAndPlanPackageFlow }
+				intent="plans-business-trial"
 			/>
 		</>
 	);
