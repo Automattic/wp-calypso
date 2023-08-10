362e6f1f4350cac14478d89db1ee19cb061605e0
Ian Ramos <5714212+IanRamosC@users.noreply.github.com>
2023-08-10 19:04:55 -0300

    Add Stats Yearly to indirect checkout map

--- client/my-sites/plans/jetpack-plans/constants.ts
+++ client/my-sites/plans/jetpack-plans/constants.ts
@@ -46,6 +46,7 @@ import {
 	PRODUCT_JETPACK_STATS_FREE,
 } from '@automattic/calypso-products';
 import { translate } from 'i18n-calypso';
+import { PRODUCT_JETPACK_STATS_YEARLY } from './../../../../packages/calypso-products/src/constants/jetpack';
 import buildCardFeaturesFromItem from './build-card-features-from-item';
 import type { SelectorProduct } from './types';
 import type { JetpackPlanSlug, JetpackPurchasableItemSlug } from '@automattic/calypso-products';
@@ -204,6 +205,19 @@ export const INDIRECT_CHECKOUT_PRODUCT_STATS = (): SelectorProduct => ( {
 	productSlug: PRODUCT_JETPACK_STATS_MONTHLY,
 	costProductSlug: PRODUCT_JETPACK_STATS_MONTHLY,
 	term: TERM_MONTHLY,
+	displayTerm: TERM_MONTHLY,
+	monthlyProductSlug: PRODUCT_JETPACK_STATS_MONTHLY,
+	displayPrice: STATS_COMMERCIAL_PRICE,
+	displayCurrency: STATS_COMMERCIAL_CURRENCY,
+} );
+
+export const INDIRECT_CHECKOUT_PRODUCT_STATS_YEARLY = (): SelectorProduct => ( {
+	...INDIRECT_CHECKOUT_PRODUCT_STATS_PWYW_YEARLY(),
+	displayName: translate( 'Stats (Commercial)' ),
+	shortName: translate( 'Stats (Commercial)' ),
+	productSlug: PRODUCT_JETPACK_STATS_YEARLY,
+	costProductSlug: PRODUCT_JETPACK_STATS_YEARLY,
+	term: TERM_ANNUALLY,
 	displayTerm: TERM_ANNUALLY,
 	monthlyProductSlug: PRODUCT_JETPACK_STATS_MONTHLY,
 	displayPrice: STATS_COMMERCIAL_PRICE,
@@ -221,6 +235,7 @@ export const INDIRECT_CHECKOUT_PRODUCT_STATS_FREE = (): SelectorProduct => ( {
 
 // List of products showcased in the Plans grid but not sold via checkout URL directly.
 export const INDIRECT_CHECKOUT_PRODUCTS_LIST = [
+	PRODUCT_JETPACK_STATS_YEARLY,
 	PRODUCT_JETPACK_STATS_MONTHLY,
 	PRODUCT_JETPACK_STATS_PWYW_YEARLY,
 	PRODUCT_JETPACK_STATS_FREE,
@@ -228,6 +243,7 @@ export const INDIRECT_CHECKOUT_PRODUCTS_LIST = [
 
 // Indirect checkout Product slugs to SelectorProduct.
 export const INDIRECT_CHECKOUT_PRODUCTS_SLUG_MAP: Record< string, () => SelectorProduct > = {
+	[ PRODUCT_JETPACK_STATS_YEARLY ]: INDIRECT_CHECKOUT_PRODUCT_STATS_YEARLY,
 	[ PRODUCT_JETPACK_STATS_MONTHLY ]: INDIRECT_CHECKOUT_PRODUCT_STATS,
 	[ PRODUCT_JETPACK_STATS_PWYW_YEARLY ]: INDIRECT_CHECKOUT_PRODUCT_STATS_PWYW_YEARLY,
 	[ PRODUCT_JETPACK_STATS_FREE ]: INDIRECT_CHECKOUT_PRODUCT_STATS_FREE,
