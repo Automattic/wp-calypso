09ef160e9c6eecef5b0cfde92cb9ea144b8943e7
Eru Penkman <roo2@protonmail.com>
2023-08-11 15:56:50 +1000

    i18n on /tags page

--- client/reader/tags/index.web.js
+++ client/reader/tags/index.web.js
@@ -1,10 +1,15 @@
+import { getLanguageRouteParam } from '@automattic/i18n-utils';
 import { makeLayout, render as clientRender } from 'calypso/controller';
+import { setLocaleMiddleware } from 'calypso/controller/shared';
 import { sidebar } from '../controller';
 import { tagsListing, fetchTrendingTags, fetchAlphabeticTags } from './controller';
 
 export default function ( router ) {
+	const langParam = getLanguageRouteParam();
+
 	router(
-		'/tags',
+		[ '/tags', `/${ langParam }/tags` ],
+		setLocaleMiddleware(),
 		fetchTrendingTags,
 		fetchAlphabeticTags,
 		sidebar,
