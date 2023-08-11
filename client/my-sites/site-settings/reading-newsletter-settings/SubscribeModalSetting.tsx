98e96aa936291145362b7a4ef4b1b4a434fc9456
Mikael Korpela <mikael@ihminen.org>
2023-08-11 16:22:26 +0300

    Revert "Subscribe Modal: Temporarily remove edit link (#80297)"
    This reverts commit 3bd60c600fdf25be763a802e65e5d8a2b007ec51.

--- client/my-sites/site-settings/reading-newsletter-settings/SubscribeModalSetting.tsx
+++ client/my-sites/site-settings/reading-newsletter-settings/SubscribeModalSetting.tsx
@@ -1,6 +1,11 @@
 import { ToggleControl } from '@wordpress/components';
+import { addQueryArgs } from '@wordpress/url';
 import i18n, { getLocaleSlug, useTranslate } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
+import getSiteEditorUrl from 'calypso/state/selectors/get-site-editor-url';
+import { getSiteOption } from 'calypso/state/sites/selectors';
+import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 export const SUBSCRIBE_MODAL_OPTION = 'sm_enabled';
 
@@ -24,6 +29,20 @@ export const SubscribeModalSetting = ( {
 	disabled,
 }: SubscribeModalSettingProps ) => {
 	const translate = useTranslate();
+	const selectedSite = useSelector( getSelectedSite );
+	const siteEditorUrl = useSelector( ( state: object ) =>
+		getSiteEditorUrl( state, selectedSite?.ID || null )
+	);
+	const themeSlug = useSelector( ( state ) =>
+		getSiteOption( state, selectedSite?.ID, 'theme_slug' )
+	);
+	const subscribeModalEditorUrl = themeSlug
+		? addQueryArgs( siteEditorUrl, {
+				postType: 'wp_template_part',
+				postId: `${ themeSlug }//subscribe-modal`,
+				canvas: 'edit',
+		  } )
+		: siteEditorUrl;
 
 	return (
 		<>
@@ -40,10 +59,20 @@ export const SubscribeModalSetting = ( {
 			<FormSettingExplanation>
 				{ isModalToggleHelpTranslated
 					? translate(
-							'Grow your subscriber list by enabling a pop-up modal with a subscribe form. This will show as readers scroll.'
+							'Grow your subscriber list by enabling a pop-up modal with a subscribe form. This will show as readers scroll. {{link}}Edit the modal{{/link}}.',
+							{
+								components: {
+									link: <a href={ subscribeModalEditorUrl } target="_blank" rel="noreferrer" />,
+								},
+							}
 					  )
 					: translate(
-							'Grow your subscriber list by enabling a popup modal with a subscribe form. This will show as readers scroll.'
+							'Grow your subscriber list by enabling a popup modal with a subscribe form. This will show as readers scroll. {{link}}Edit the modal{{/link}}.',
+							{
+								components: {
+									link: <a href={ subscribeModalEditorUrl } target="_blank" rel="noreferrer" />,
+								},
+							}
 					  ) }
 			</FormSettingExplanation>
 		</>
