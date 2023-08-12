56be7f42f1041814b665216c1da8d1d9bfc54c79
Jeremy Yip <jeremy.yip@automattic.com>
2023-08-11 18:12:35 -0700

    Automattic Components: Temporarily shim old calypso form label component (#80350)
    * Create form label shim
    
    * Add inline comment to shim

--- client/components/forms/form-label/index.tsx
+++ client/components/forms/form-label/index.tsx
@@ -1,38 +1,8 @@
-import classnames from 'classnames';
-import { useTranslate } from 'i18n-calypso';
-import { Children, FunctionComponent, LabelHTMLAttributes } from 'react';
+import { FormLabel } from '@automattic/components';
 
-import './style.scss';
-
-export interface Props {
-	optional?: boolean;
-	required?: boolean;
-}
-
-type LabelProps = LabelHTMLAttributes< HTMLLabelElement >;
-
-const FormLabel: FunctionComponent< Props & LabelProps > = ( {
-	children,
-	required,
-	optional,
-	className, // Via LabelProps
-	...labelProps
-} ) => {
-	const translate = useTranslate();
-
-	const hasChildren: boolean = Children.count( children ) > 0;
-
-	return (
-		<label { ...labelProps } className={ classnames( className, 'form-label' ) }>
-			{ children }
-			{ hasChildren && required && (
-				<small className="form-label__required">{ translate( 'Required' ) }</small>
-			) }
-			{ hasChildren && optional && (
-				<small className="form-label__optional">{ translate( 'Optional' ) }</small>
-			) }
-		</label>
-	);
-};
+// We're in the process of migrating to @automattic/components. Because of this, we're using this wrapper
+// component to point references to the old count component to the new one in @automattic/components.
+// This allows us to transition in smaller pieces and without risking updates to the old Calypso component in the interim.
+// See https://github.com/Automattic/martech/issues/2028 for more details.
 
 export default FormLabel;
