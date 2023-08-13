1843b12a1a6e4e5f5b077ca71ea66ecd8326bd15
Jeremy Yip <jeremy.yip@automattic.com>
2023-08-13 11:47:28 -0700

    Update plan slug type

--- packages/data-stores/src/wpcom-plans-ui/selectors.ts
+++ packages/data-stores/src/wpcom-plans-ui/selectors.ts
@@ -1,7 +1,8 @@
+import { PlanSlug } from '@automattic/calypso-products';
 import type { State } from './reducer';
 
 export const isDomainUpsellDialogShown = ( state: State ) => !! state.showDomainUpsellDialog;
-export const getStorageAddOnForPlan = ( state: State ) => ( plan: string ) => {
+export const getStorageAddOnForPlan = ( state: State ) => ( planSlug: PlanSlug ) => {
 	const selectedStorageAddOnsForPlans = state?.selectedStorageAddOnsForPlans;
-	return selectedStorageAddOnsForPlans ? selectedStorageAddOnsForPlans[ plan ] : null;
+	return selectedStorageAddOnsForPlans ? selectedStorageAddOnsForPlans[ planSlug ] : null;
 };
