f1326503b0c8cd1e4b41bbd711f15d8706219f64
Jeremy Yip <jeremy.yip@automattic.com>
2023-08-13 12:34:28 -0700

    Remove unnecessary currying

--- packages/data-stores/src/wpcom-plans-ui/selectors.ts
+++ packages/data-stores/src/wpcom-plans-ui/selectors.ts
@@ -2,5 +2,5 @@ import { PlanSlug } from '@automattic/calypso-products';
 import type { State } from './reducer';
 
 export const isDomainUpsellDialogShown = ( state: State ) => !! state.showDomainUpsellDialog;
-export const getStorageAddOnForPlan = ( state: State ) => ( planSlug: PlanSlug ) =>
+export const getStorageAddOnForPlan = ( state: State, planSlug: PlanSlug ) =>
 	state.selectedStorageAddOnsForPlans?.[ planSlug ];
