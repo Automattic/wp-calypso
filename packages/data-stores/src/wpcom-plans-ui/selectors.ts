89c939f3115901f7dca553fdbfb427c76cbfc8c3
Jeremy Yip <jeremy.yip@automattic.com>
2023-08-13 12:30:38 -0700

    Fix optional chaining for getStorageAddOnForPlan selector
    State doesn't initialize as undefined -- it's the substate selectedStorageAddOnsForPlans that does. We update the optional chaining of the getStorageAddOnForPlan selector to reflect this

--- packages/data-stores/src/wpcom-plans-ui/selectors.ts
+++ packages/data-stores/src/wpcom-plans-ui/selectors.ts
@@ -2,7 +2,5 @@ import { PlanSlug } from '@automattic/calypso-products';
 import type { State } from './reducer';
 
 export const isDomainUpsellDialogShown = ( state: State ) => !! state.showDomainUpsellDialog;
-export const getStorageAddOnForPlan = ( state: State ) => ( planSlug: PlanSlug ) => {
-	const selectedStorageAddOnsForPlans = state?.selectedStorageAddOnsForPlans;
-	return selectedStorageAddOnsForPlans ? selectedStorageAddOnsForPlans[ planSlug ] : null;
-};
+export const getStorageAddOnForPlan = ( state: State ) => ( planSlug: PlanSlug ) =>
+	state.selectedStorageAddOnsForPlans?.[ planSlug ];
