e91bf031dfecb0b810049cecd7996773a1f2e0a0
Jacopo Tomasone <Copons@users.noreply.github.com>
2023-08-14 11:13:35 +0100

    Revert "Logged-Out Theme Showcase: Update SEO Content (#79845)" (#80549)
    This reverts commit 582351337d83c469f8ad3ce2459be2f967e14b72.

--- client/my-sites/themes/theme-showcase.jsx
+++ client/my-sites/themes/theme-showcase.jsx
@@ -40,7 +40,6 @@ import {
 } from 'calypso/state/themes/selectors';
 import { getThemesBookmark } from 'calypso/state/themes/themes-ui/selectors';
 import EligibilityWarningModal from './atomic-transfer-dialog';
-import getThemeShowcaseLoggedOutSeoContent from './get-theme-showcase-logged-out-seo-content';
 import {
 	addTracking,
 	appendStyleVariationToThemesPath,
@@ -451,6 +450,7 @@ class ThemeShowcase extends Component {
 			filter,
 			isLoggedIn,
 			pathName,
+			title,
 			filterString,
 			isMultisite,
 			locale,
@@ -459,34 +459,9 @@ class ThemeShowcase extends Component {
 		} = this.props;
 		const canonicalUrl = 'https://wordpress.com' + pathName;
 
-		const {
-			title: documentHeadTitle,
-			metaDescription: metaDescription,
-			header: themesHeaderTitle,
-			description: themesHeaderDescription,
-		} = isLoggedIn
-			? {
-					title: this.props.title,
-					metaDescription: this.props.description,
-					header: translate( 'Themes' ),
-					description: translate(
-						'Select or update the visual design for your site. {{learnMoreLink}}Learn more{{/learnMoreLink}}.',
-						{
-							components: {
-								learnMoreLink: <InlineSupportLink supportContext="themes" showIcon={ false } />,
-							},
-						}
-					),
-			  }
-			: getThemeShowcaseLoggedOutSeoContent( filter, tier );
-
 		const metas = [
-			{
-				name: 'description',
-				property: 'og:description',
-				content: metaDescription || themesHeaderDescription,
-			},
-			{ property: 'og:title', content: documentHeadTitle },
+			{ name: 'description', property: 'og:description', content: this.props.description },
+			{ property: 'og:title', content: title },
 			{ property: 'og:url', content: canonicalUrl },
 			{ property: 'og:type', content: 'website' },
 			{ property: 'og:site_name', content: 'WordPress.com' },
@@ -542,13 +517,35 @@ class ThemeShowcase extends Component {
 
 		return (
 			<div className="theme-showcase">
-				<DocumentHead title={ documentHeadTitle } meta={ metas } />
+				<DocumentHead title={ title } meta={ metas } />
 				<PageViewTracker
 					path={ this.props.analyticsPath }
 					title={ this.props.analyticsPageTitle }
 					properties={ { is_logged_in: isLoggedIn } }
 				/>
-				<ThemesHeader title={ themesHeaderTitle } description={ themesHeaderDescription }>
+				<ThemesHeader
+					title={
+						isLoggedIn
+							? translate( 'Themes' )
+							: translate( 'Find the perfect theme for your website' )
+					}
+					description={
+						isLoggedIn
+							? translate(
+									'Select or update the visual design for your site. {{learnMoreLink}}Learn more{{/learnMoreLink}}.',
+									{
+										components: {
+											learnMoreLink: (
+												<InlineSupportLink supportContext="themes" showIcon={ false } />
+											),
+										},
+									}
+							  )
+							: translate(
+									'Beautiful and responsive WordPress.com themes. Choose from free and premium options for all types of websites. Then, activate the one that is right for you.'
+							  )
+					}
+				>
 					{ isLoggedIn && (
 						<>
 							<div className="themes__install-theme-button-container">
