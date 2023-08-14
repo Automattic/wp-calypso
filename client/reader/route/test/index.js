b1a7bca78fcf8d999eaa582b7026fbc543d7731a
Chris R <chris.rosser@automattic.com>
2023-08-15 10:21:28 +1200

    Reader: remove remaining old Discover code - again
    This reverts commit 126ba4f900e6f22aba055a7b33948856ff5611e4.

--- client/reader/route/test/index.js
+++ client/reader/route/test/index.js
@@ -1,4 +1,3 @@
-import config from '@automattic/calypso-config';
 import * as route from '../';
 
 describe( 'index', () => {
@@ -16,19 +15,11 @@ describe( 'index', () => {
 		test( 'should return site URL', () => {
 			expect( route.getSiteUrl( 1234 ) ).toEqual( '/read/blogs/1234' );
 		} );
-
-		test( 'should return pretty URL for discover', () => {
-			expect( route.getSiteUrl( config( 'discover_blog_id' ) ) ).toEqual( '/discover' );
-		} );
 	} );
 
 	describe( 'getFeedUrl', () => {
 		test( 'should return site URL', () => {
 			expect( route.getFeedUrl( 1234 ) ).toEqual( '/read/feeds/1234' );
-		} );
-
-		test( 'should return pretty URL for discover', () => {
-			expect( route.getFeedUrl( config( 'discover_feed_id' ) ) ).toEqual( '/discover' );
 		} );
 	} );
 } );
