8b1f735ba497883bb02bb70602eaf17ceb54fd40
Anna McPhee <anna.mcphee@automattic.com>
2023-08-15 17:17:28 -0600

    add tooltip

--- client/my-sites/stats/stats-reach/index.jsx
+++ client/my-sites/stats/stats-reach/index.jsx
@@ -1,6 +1,8 @@
 import config from '@automattic/calypso-config';
+import { Popover } from '@automattic/components';
 import { localize } from 'i18n-calypso';
 import { get, reduce } from 'lodash';
+import React, { useState, useRef } from 'react';
 import { connect } from 'react-redux';
 import QuerySiteStats from 'calypso/components/data/query-site-stats';
 import { getSiteSlug } from 'calypso/state/sites/selectors';
@@ -22,6 +24,9 @@ export const StatsReach = ( props ) => {
 		siteSlug,
 		isOdysseyStats,
 	} = props;
+
+	const [ isPopoverVisible, setPopoverVisible ] = useState( false );
+	const socialRef = useRef( null );
 
 	const isLoadingFollowData = ! followData;
 	const wpcomFollowCount = get( followData, 'total_wpcom', 0 );
@@ -48,7 +53,15 @@ export const StatsReach = ( props ) => {
 
 	const socialData = {
 		value: publicizeFollowCount,
-		label: translate( 'Social' ),
+		label: (
+			<span
+				ref={ socialRef }
+				onMouseEnter={ () => setPopoverVisible( true ) }
+				onMouseLeave={ () => setPopoverVisible( false ) }
+			>
+				{ translate( 'Social Test' ) }
+			</span>
+		),
 	};
 
 	if ( ! isOdysseyStats ) {
@@ -100,6 +113,17 @@ export const StatsReach = ( props ) => {
 					)
 				}
 			/>
+
+			<Popover
+				className="tooltip tooltip--darker highlight-card-tooltip highlight-card__settings-tooltip"
+				isVisible={ isPopoverVisible }
+				position="bottom left"
+				context={ socialRef.current }
+			>
+				<div className="highlight-card-tooltip-content">
+					<p>{ translate( 'This represents the count of social media subscribers.' ) }</p>
+				</div>
+			</Popover>
 		</>
 	);
 };
