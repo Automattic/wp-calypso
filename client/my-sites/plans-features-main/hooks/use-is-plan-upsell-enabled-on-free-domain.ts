e7988f2f5ef1a017c87c972933936e4672b54354
Dehan <dehandecroos@gmail.com>
2023-08-14 20:31:20 -0400

    Fix experiment ids'

--- client/my-sites/plans-features-main/hooks/use-is-plan-upsell-enabled-on-free-domain.ts
+++ client/my-sites/plans-features-main/hooks/use-is-plan-upsell-enabled-on-free-domain.ts
@@ -5,9 +5,9 @@ const useIsPlanUpsellEnabledOnFreeDomain = (
 	flowName?: string | null,
 	hasPaidDomain?: boolean
 ): DataResponse< boolean > => {
-	const ONBOARDING_EXPERIMENT = 'calypso_gf_signup_onboarding_free_free_dont_miss_out_modal_v2';
+	const ONBOARDING_EXPERIMENT = 'calypso_gf_signup_onboarding_free_free_dont_miss_out_modal_v3';
 	const ONBOARDING_PM_EXPERIMENT =
-		'calypso_gf_signup_onboarding_pm_free_free_dont_miss_out_modal_v2';
+		'calypso_gf_signup_onboarding_pm_free_free_dont_miss_out_modal_v3';
 	const relevantExperiment =
 		flowName === 'onboarding' ? ONBOARDING_EXPERIMENT : ONBOARDING_PM_EXPERIMENT;
 	const [ isLoading, experimentAssignment ] = useExperiment( relevantExperiment, {
