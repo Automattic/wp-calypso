c798761f7345595991f7a6a5fa37b7c353383fd9
edanzer <edanzer@gmail.com>
2023-08-15 09:19:11 -0600

    Simplify launchpad payment plans modal

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/sidebar.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/sidebar.tsx
@@ -249,7 +249,7 @@ const Sidebar = ( { sidebarDomain, siteSlug, submit, goToStep, flow }: SidebarPr
 				{ showPlansModal && site?.ID && (
 					<RecurringPaymentsPlanAddEditModal
 						closeDialog={ () => setShowPlansModal( false ) }
-						product={ { subscribe_as_site_subscriber: true } }
+						product={ { subscribe_as_site_subscriber: true, price: 5 } }
 						// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 						// @ts-ignore - Underlying component is JS class component with props supplied by connect() and mapstatetoprops.
 						siteId={ site.ID }
