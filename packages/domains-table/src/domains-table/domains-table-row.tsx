671f51cfb2bc4ebedb3d728683a7b84831d83325
Philip Jackson <p-jackson@live.com>
2023-08-16 15:42:15 +1200

    Change link location based on domain type

--- packages/domains-table/src/domains-table/domains-table-row.tsx
+++ packages/domains-table/src/domains-table/domains-table-row.tsx
@@ -18,20 +18,22 @@ export function DomainsTableRow( { domain, fetchSiteDomains }: DomainsTableRowPr
 		}
 	);
 
-	const { primaryDomain } = useMemo(
-		() => ( {
-			primaryDomain: data?.domains?.find( ( d ) => d.primary_domain )?.domain,
-		} ),
-		[ data ]
-	);
+	const { siteSlug } = useMemo( () => {
+		const primaryDomain = data?.domains?.find( ( d ) => d.primary_domain );
+		const unmappedDomain = data?.domains?.find( ( d ) => d.wpcom_domain );
+		const siteSlug =
+			primaryDomain?.type === 'redirect' ? unmappedDomain?.domain : primaryDomain?.domain;
+
+		return {
+			// Fall back to the site's ID if we're still loading detailed domain data
+			siteSlug: siteSlug || domain.blog_id.toString( 10 ),
+		};
+	}, [ data, domain.blog_id ] );
 
 	return (
 		<tr key={ domain.domain }>
 			<td>
-				<a
-					className="domains-table__domain-link"
-					href={ getDomainManagementLink( domain, primaryDomain ) }
-				>
+				<a className="domains-table__domain-link" href={ domainManagementLink( domain, siteSlug ) }>
 					{ domain.domain }
 				</a>
 			</td>
@@ -39,9 +41,26 @@ export function DomainsTableRow( { domain, fetchSiteDomains }: DomainsTableRowPr
 	);
 }
 
-function getDomainManagementLink(
-	{ blog_id, domain }: PartialDomainData,
-	sitePrimaryDomain: string | undefined
-) {
-	return `/domains/manage/all/${ domain }/edit/${ sitePrimaryDomain ?? blog_id }`;
+function domainManagementLink( { domain, type }: PartialDomainData, siteSlug: string ) {
+	const viewSlug = domainManagementViewSlug( type );
+
+	// Encodes only real domain names and not parameter placeholders
+	if ( ! domain.startsWith( ':' ) ) {
+		// Encodes domain names so addresses with slashes in the path (e.g. used in site redirects) don't break routing.
+		// Note they are encoded twice since page.js decodes the path by default.
+		domain = encodeURIComponent( encodeURIComponent( domain ) );
+	}
+
+	return `/domains/manage/all/${ domain }/${ viewSlug }/${ siteSlug }`;
+}
+
+function domainManagementViewSlug( type: PartialDomainData[ 'type' ] ) {
+	switch ( type ) {
+		case 'transfer':
+			return 'transfer/in';
+		case 'redirect':
+			return 'redirect';
+		default:
+			return 'edit';
+	}
 }
