87e85c8b5a4aa093edb4c3bc089c87cd758aa905
edanzer <edanzer@gmail.com>
2023-08-15 18:33:52 -0600

    Resolve type error when invoking component

--- client/my-sites/earn/components/add-edit-plan-modal/index.tsx
+++ client/my-sites/earn/components/add-edit-plan-modal/index.tsx
@@ -26,18 +26,18 @@ import type { ChangeEvent } from 'react';
 import './style.scss';
 
 type Product = {
-	ID: string;
-	currency: string;
-	price: number;
-	title: string;
-	interval: string;
-	buyer_can_change_amount: boolean;
-	multiple_per_user: boolean;
-	welcome_email_content: string;
-	subscribe_as_site_subscriber: boolean;
-	renewal_schedule: string;
-	type: string;
-	is_editable: boolean;
+	ID?: string;
+	currency?: string;
+	price?: number;
+	title?: string;
+	interval?: string;
+	buyer_can_change_amount?: boolean;
+	multiple_per_user?: boolean;
+	welcome_email_content?: string;
+	subscribe_as_site_subscriber?: boolean;
+	renewal_schedule?: string;
+	type?: string;
+	is_editable?: boolean;
 };
 
 type RecurringPaymentsPlanAddEditModalProps = {
