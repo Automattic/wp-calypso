fbcc7c11d0a7716987553c8a9ed90801356e00f7
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2023-08-16 19:36:35 -0300

    Implement basic site-specific domains table

---
+++ client/my-sites/domains/domain-management/list/bulk-site-domains.tsx
@@ -0,0 +1,51 @@
+import { useSiteDomainsQuery } from '@automattic/data-stores';
+import { DomainsTable } from '@automattic/domains-table';
+import { useTranslate } from 'i18n-calypso';
+import { UsePresalesChat } from 'calypso/components/data/domain-management';
+import InlineSupportLink from 'calypso/components/inline-support-link';
+import Main from 'calypso/components/main';
+import BodySectionCssClass from 'calypso/layout/body-section-css-class';
+import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import { useSelector } from 'calypso/state';
+import { getSelectedSiteSlug } from 'calypso/state/ui/selectors';
+import DomainHeader from '../components/domain-header';
+import OptionsDomainButton from './options-domain-button';
+
+interface BulkSiteDomainsProps {
+	analyticsPath: string;
+	analyticsTitle: string;
+}
+
+export default function BulkSiteDomains( props: BulkSiteDomainsProps ) {
+	const siteSlug = useSelector( getSelectedSiteSlug );
+	const { data } = useSiteDomainsQuery( siteSlug );
+	const translate = useTranslate();
+
+	const item = {
+		label: translate( 'Domains' ),
+		helpBubble: translate(
+			'Manage the domains connected to your site. {{learnMoreLink}}Learn more{{/learnMoreLink}}.',
+			{
+				components: {
+					learnMoreLink: <InlineSupportLink supportContext="domains" showIcon={ false } />,
+				},
+			}
+		),
+	};
+
+	const buttons = [
+		<OptionsDomainButton key="breadcrumb_button_1" specificSiteActions allDomainsList />,
+	];
+
+	return (
+		<>
+			<PageViewTracker path={ props.analyticsPath } title={ props.analyticsTitle } />
+			<Main wideLayout>
+				<BodySectionCssClass bodyClass={ [ 'edit__body-white' ] } />
+				<DomainHeader items={ [ item ] } buttons={ buttons } mobileButtons={ buttons } />
+				<DomainsTable domains={ data?.domains } />
+			</Main>
+			<UsePresalesChat />
+		</>
+	);
+}
