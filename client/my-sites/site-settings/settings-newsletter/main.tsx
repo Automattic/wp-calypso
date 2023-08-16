1a8e75d27428e6d5770301634c47e2557bb2fe19
Maciej Grabowski <littlesoldiergraba@gmail.com>
2023-08-16 12:19:55 +0200

    Small clean-up

--- client/my-sites/site-settings/settings-newsletter/main.tsx
+++ client/my-sites/site-settings/settings-newsletter/main.tsx
@@ -1,12 +1,7 @@
 import { useTranslate } from 'i18n-calypso';
-import { connect } from 'react-redux';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
 import Main from 'calypso/components/main';
-import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
-import { getSiteUrl, isJetpackSite } from 'calypso/state/sites/selectors';
-import { IAppState } from 'calypso/state/types';
-import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { NewsletterSettingsSection } from '../reading-newsletter-settings';
 import wrapSettingsForm from '../wrap-settings-form';
 
@@ -22,7 +17,7 @@ type Fields = {
 	sm_enabled?: boolean;
 };
 
-const getFormSettings = ( settings: unknown & Fields ) => {
+const getFormSettings = ( settings?: Fields ) => {
 	if ( ! settings ) {
 		return {};
 	}
@@ -42,61 +37,42 @@ const getFormSettings = ( settings: unknown & Fields ) => {
 	};
 };
 
-const connectComponent = connect( ( state: IAppState ) => {
-	const siteId = getSelectedSiteId( state );
-	const siteUrl = siteId && getSiteUrl( state, siteId );
-	const siteIsJetpack = isJetpackSite( state, siteId );
-	const isAtomic = isSiteAutomatedTransfer( state, siteId );
-	return {
-		...( siteUrl && { siteUrl } ),
-		siteIsJetpack,
-		isAtomic,
-	};
-} );
-
 type NewsletterSettingsFormProps = {
 	fields: Fields;
-	onChangeField: ( field: string ) => ( event: React.ChangeEvent< HTMLInputElement > ) => void;
-	handleAutosavingToggle: ( field: string ) => () => void;
 	handleToggle: ( field: string ) => ( value: boolean ) => void;
 	handleSubmitForm: ( event: React.FormEvent< HTMLFormElement > ) => void;
-	isAtomic: boolean | null;
 	isRequestingSettings: boolean;
 	isSavingSettings: boolean;
 	settings: { subscription_options?: SubscriptionOptions };
-	siteIsJetpack: boolean | null;
-	siteUrl?: string;
 	updateFields: ( fields: Fields ) => void;
 };
 
 const NewsletterSettingsForm = wrapSettingsForm( getFormSettings )(
-	connectComponent(
-		( {
-			fields,
-			handleSubmitForm,
-			handleToggle,
-			isRequestingSettings,
-			isSavingSettings,
-			settings,
-			updateFields,
-		}: NewsletterSettingsFormProps ) => {
-			const disabled = isRequestingSettings || isSavingSettings;
-			const savedSubscriptionOptions = settings?.subscription_options;
-			return (
-				<form onSubmit={ handleSubmitForm }>
-					<NewsletterSettingsSection
-						fields={ fields }
-						handleToggle={ handleToggle }
-						handleSubmitForm={ handleSubmitForm }
-						disabled={ disabled }
-						isSavingSettings={ isSavingSettings }
-						savedSubscriptionOptions={ savedSubscriptionOptions }
-						updateFields={ updateFields }
-					/>
-				</form>
-			);
-		}
-	)
+	( {
+		fields,
+		handleSubmitForm,
+		handleToggle,
+		isRequestingSettings,
+		isSavingSettings,
+		settings,
+		updateFields,
+	}: NewsletterSettingsFormProps ) => {
+		const disabled = isRequestingSettings || isSavingSettings;
+		const savedSubscriptionOptions = settings?.subscription_options;
+		return (
+			<form onSubmit={ handleSubmitForm }>
+				<NewsletterSettingsSection
+					fields={ fields }
+					handleToggle={ handleToggle }
+					handleSubmitForm={ handleSubmitForm }
+					disabled={ disabled }
+					isSavingSettings={ isSavingSettings }
+					savedSubscriptionOptions={ savedSubscriptionOptions }
+					updateFields={ updateFields }
+				/>
+			</form>
+		);
+	}
 );
 
 const NewsletterSettings = () => {
