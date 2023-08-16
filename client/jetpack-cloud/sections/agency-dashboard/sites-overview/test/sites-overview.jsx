68a3d5cc365c180b03d2114b7a3eef40f87f546b
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-08-16 13:39:39 -0500

    Mock query hooks instead of QueryClient

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/test/sites-overview.jsx
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/test/sites-overview.jsx
@@ -3,16 +3,21 @@
  */
 
 import { isWithinBreakpoint } from '@automattic/viewport';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { render, act } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import useFetchDashboardSites from 'calypso/data/agency-dashboard/use-fetch-dashboard-sites';
+import useFetchMonitorVerfiedContacts from 'calypso/data/agency-dashboard/use-fetch-monitor-verified-contacts';
+import useProductsQuery from 'calypso/state/partner-portal/licenses/hooks/use-products-query';
 import SitesOverviewContext from '../context';
 import SitesOverview from '../index';
 
 jest.mock( '@automattic/viewport' );
+jest.mock( 'calypso/data/agency-dashboard/use-fetch-dashboard-sites' );
+jest.mock( 'calypso/data/agency-dashboard/use-fetch-monitor-verified-contacts' );
+jest.mock( 'calypso/state/partner-portal/licenses/hooks/use-products-query' );
 
 const createFakeState = ( changes = {} ) => ( {
 	sites: {},
@@ -43,30 +48,19 @@ const Wrapper = ( { state = createFakeState(), context = createFakeContext() } )
 	const mockStore = configureStore( [ thunk ] );
 	const store = mockStore( state );
 
-	const queryClient = new QueryClient();
-	queryClient.setQueryData(
-		[ 'jetpack-agency-dashboard-sites', context.search, 1, context.filter, context.sort ],
-		{
-			sites: [],
-			total: 1,
-			perPage: 1,
-			totalFavorites: 1,
-		}
-	);
-
 	return (
 		<Provider store={ store }>
-			<QueryClientProvider client={ queryClient }>
-				<SitesOverviewContext.Provider value={ context }>
-					<SitesOverview />
-				</SitesOverviewContext.Provider>
-			</QueryClientProvider>
+			<SitesOverviewContext.Provider value={ context }>
+				<SitesOverview />
+			</SitesOverviewContext.Provider>
 		</Provider>
 	);
 };
 
 describe( '<SitesOverview>', () => {
-	beforeAll( () => {
+	beforeEach( () => {
+		jest.resetAllMocks();
+
 		window.IntersectionObserver = jest.fn( () => ( {
 			observe: jest.fn(),
 			disconnect: jest.fn(),
@@ -76,9 +70,21 @@ describe( '<SitesOverview>', () => {
 			takeRecords: jest.fn(),
 			unobserve: jest.fn(),
 		} ) );
+
+		useFetchDashboardSites.mockReturnValue( {
+			data: { sites: [], total: 1, perPage: 1, totalFavorites: 1 },
+		} );
+
+		useFetchMonitorVerfiedContacts.mockReturnValue( {
+			data: {},
+		} );
+
+		useProductsQuery.mockReturnValue( {
+			data: {},
+		} );
 	} );
 
-	test( 'Show the correct empty state message when there are no sites and no applied filters in All tab', async () => {
+	test( 'Show the correct empty state message when there are no sites and no applied filters in All tab', () => {
 		const { getAllByText } = render( <Wrapper /> );
 		const [ dashboardHeading ] = getAllByText( 'Dashboard' );
 		expect( dashboardHeading ).toBeInTheDocument();
@@ -92,8 +98,6 @@ describe( '<SitesOverview>', () => {
 			"Let's get started with the Jetpack Pro Dashboard"
 		);
 		expect( emptyStateMessage ).toBeInTheDocument();
-
-		await act( () => Promise.resolve() );
 	} );
 
 	test( 'Show the correct empty state message when there are no sites and has applied filters in All tab', async () => {
@@ -105,12 +109,9 @@ describe( '<SitesOverview>', () => {
 			'No results found. Please try refining your search.'
 		);
 		expect( emptyStateMessage ).toBeInTheDocument();
-
-		const promise = Promise.resolve();
-		await act( () => promise );
 	} );
 
-	test( 'Show the correct empty state message when there are no sites and has applied filters in Favorites tab', async () => {
+	test( 'Show the correct empty state message when there are no sites and has applied filters in Favorites tab', () => {
 		const { getAllByText } = render(
 			<Wrapper
 				context={ createFakeContext( { search: 'test', filter: { showOnlyFavorites: true } } ) }
@@ -121,42 +122,34 @@ describe( '<SitesOverview>', () => {
 			'No results found. Please try refining your search.'
 		);
 		expect( emptyStateMessage ).toBeInTheDocument();
-
-		await act( () => Promise.resolve() );
 	} );
 
-	test( 'Show the correct empty state message when there are no sites and no applied filters in Favorites tab', async () => {
+	test( 'Show the correct empty state message when there are no sites and no applied filters in Favorites tab', () => {
 		const { getAllByText } = render(
 			<Wrapper context={ createFakeContext( { filter: { showOnlyFavorites: true } } ) } />
 		);
 
 		const [ emptyStateMessage ] = getAllByText( "You don't have any favorites yet." );
 		expect( emptyStateMessage ).toBeInTheDocument();
-
-		await act( () => Promise.resolve() );
 	} );
 
-	test( 'Do not show the Add X Licenses button when license count is 0', async () => {
+	test( 'Do not show the Add X Licenses button when license count is 0', () => {
 		const { queryByText } = render( <Wrapper /> );
 
 		const addLicensesButton = queryByText( 'Add 1 license' );
 		expect( addLicensesButton ).not.toBeInTheDocument();
-
-		await act( () => Promise.resolve() );
 	} );
 
-	test( 'Show the add site and issue license buttons', async () => {
+	test( 'Show the add site and issue license buttons', () => {
 		const { getAllByText } = render( <Wrapper /> );
 
 		const [ addSiteButton ] = getAllByText( 'Add New Site' );
 		const [ issueLicenseButton ] = getAllByText( 'Issue License' );
 		expect( addSiteButton ).toBeInTheDocument();
 		expect( issueLicenseButton ).toBeInTheDocument();
-
-		await act( () => Promise.resolve() );
 	} );
 
-	test( 'Show the Add x Licenses button when a license is selected', async () => {
+	test( 'Show the Add x Licenses button when a license is selected', () => {
 		const { getAllByText } = render(
 			<Wrapper
 				state={ createFakeState( {
@@ -167,12 +160,10 @@ describe( '<SitesOverview>', () => {
 
 		const [ issueLicenseButton ] = getAllByText( 'Issue 1 license' );
 		expect( issueLicenseButton ).toBeInTheDocument();
-
-		await act( () => Promise.resolve() );
 	} );
 
 	// the default view for tests is mobile, widescreen buttons behave a bit differently
-	test( 'Swap the issue x buttons and add site/add new license buttons on large screen when a license is selected', async () => {
+	test( 'Swap the issue x buttons and add site/add new license buttons on large screen when a license is selected', () => {
 		//set screen to widescreen for this test
 		isWithinBreakpoint.mockReturnValue( true );
 
@@ -188,10 +179,5 @@ describe( '<SitesOverview>', () => {
 		const issueLicenseButton = queryByText( 'Issue 1 license' );
 		expect( addSiteButton ).not.toBeInTheDocument();
 		expect( issueLicenseButton ).toBeInTheDocument();
-
-		// set screen back to mobile for rest of tests
-		isWithinBreakpoint.mockReturnValue( false );
-
-		await act( () => Promise.resolve() );
 	} );
 } );
