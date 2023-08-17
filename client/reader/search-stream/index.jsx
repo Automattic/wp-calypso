8b186d7a428fe29cfa4cd76cffc067c88c8722ea
Addison-Stavlo <stavz01@gmail.com>
2023-08-17 08:54:08 -0400

    remove border from post and site headers for logged out search stream

--- client/reader/search-stream/index.jsx
+++ client/reader/search-stream/index.jsx
@@ -218,6 +218,7 @@ class SearchStream extends React.Component {
 							selected={ searchType }
 							onSelection={ this.handleSearchTypeSelection }
 							wideDisplay={ wideDisplay }
+							isLoggedIn={ isLoggedIn }
 						/>
 					) }
 				</div>
