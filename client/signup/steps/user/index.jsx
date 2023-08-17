1f7f48bd83e43a659f10da40fa7ce22d54b2530a
Chris Runnells <chris.runnells@automattic.com>
2023-08-17 13:31:39 -1000

    Update instances of Woo where we mean Woo Express in login and signup flows

--- client/signup/steps/user/index.jsx
+++ client/signup/steps/user/index.jsx
@@ -204,7 +204,7 @@ export class UserStep extends Component {
 						  );
 			} else if ( isWooOAuth2Client( oauth2Client ) && ! wccomFrom ) {
 				subHeaderText = translate(
-					'All Woo stores are powered by WordPress.com.{{br/}}Please create an account to continue. Already registered? {{a}}Log in{{/a}}',
+					'All Woo Express stores are powered by WordPress.com.{{br/}}Please create an account to continue. Already registered? {{a}}Log in{{/a}}',
 					{
 						components: {
 							a: <a href={ loginUrl } />,
