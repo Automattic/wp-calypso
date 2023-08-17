358cc39a1588e726ef5aea3c4f9e80cbbc57a39f
Tim Broddin <tim@wannabes.be>
2023-08-17 13:00:29 +0200

    Add function to format subscriber rates
    Added a function to handle formatting of subscriber stats rates in `useSubscriberStatsRate.ts`. The function ensures that integer rates are returned as integers and decimal rates are rounded to 1 decimal place. This change provides a consistent data type for the returned rates and improves readability.

--- client/my-sites/subscribers/hooks/use-subscriber-stats-rate.ts
+++ client/my-sites/subscribers/hooks/use-subscriber-stats-rate.ts
@@ -1,15 +1,22 @@
 import { useMemo } from 'react';
 import type { SubscriberStats } from '../types';
 
+function formatNumber( num: number ) {
+	if ( Number.isInteger( num ) ) {
+		return num;
+	}
+	return parseFloat( num.toFixed( 1 ) );
+}
 const useSubscriberStatsRate = (
 	subscriberStats: SubscriberStats | undefined,
 	property: keyof Omit< SubscriberStats, 'emails_sent' >
 ) => {
 	return useMemo( () => {
 		if ( subscriberStats && subscriberStats.emails_sent && subscriberStats[ property ] ) {
-			return ( ( subscriberStats[ property ] / subscriberStats.emails_sent ) * 100 ).toFixed( 1 );
+			const rate = ( subscriberStats[ property ] / subscriberStats.emails_sent ) * 100;
+			return formatNumber( rate );
 		}
-		return 0;
+		return 0; // Note: This now returns 0 as a number, not '0' as a string.
 	}, [ subscriberStats, property ] );
 };
 
