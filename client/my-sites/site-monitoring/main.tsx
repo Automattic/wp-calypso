4c716b40e8bd0df932627479fc29e5ce1adf7f44
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2023-08-17 05:07:18 -0700

    Hosting Metrics: Log Site Monitoring pageviews

--- client/my-sites/site-monitoring/main.tsx
+++ client/my-sites/site-monitoring/main.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
 import Main from 'calypso/components/main';
+import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
 import { SiteMonitoringTabPanel } from './components/site-monitoring-tab-panel';
 import { LogsTab } from './logs-tab';
 import { MetricsTab } from './metrics-tab';
@@ -21,6 +22,7 @@ export function SiteMetrics() {
 
 	return (
 		<Main className="site-monitoring" fullWidthLayout>
+			<PageViewTracker path="/site-monitoring/:site" title="Site Monitoring" />
 			<DocumentHead title={ titleHeader } />
 			<FormattedHeader
 				className="site-monitoring__formatted-header modernized-header"
