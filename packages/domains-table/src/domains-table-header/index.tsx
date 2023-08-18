e65457ed30006fd6239c70962e44c7866f096017
vykes-mac <knrymcleish@gmail.com>
2023-08-17 20:43:10 -0500

    create domains header component for domains table

---
+++ packages/domains-table/src/domains-table-header/index.tsx
@@ -0,0 +1,85 @@
+import { Button } from '@automattic/components';
+import { DomainData } from '@automattic/data-stores';
+import { Icon } from '@wordpress/components';
+import { chevronDown, chevronUp } from '@wordpress/icons';
+import classNames from 'classnames';
+import './style.scss';
+
+export type DomainsTableColumn =
+	| {
+			name: string;
+			label: string;
+			isSortable: true;
+			initialSortDirection: 'asc' | 'desc';
+			supportsOrderSwitching?: boolean;
+			sortFunctions?: [ ( first: DomainData, second: DomainData, sortOrder: number ) => number ];
+			headerComponent?: React.ReactNode;
+	  }
+	| {
+			name: string;
+			label: string;
+			isSortable?: false;
+			initialSortDirection?: never;
+			supportsOrderSwitching?: never;
+			sortFunctions?: [ ( first: DomainData, second: DomainData, sortOrder: number ) => number ];
+			headerComponent?: React.ReactNode;
+	  };
+
+type DomainsTableHeaderProps = {
+	columns: DomainsTableColumn[];
+	activeSortKey: string;
+	activeSortDirection: 'asc' | 'desc';
+	onChangeSortOrder: ( selectedColumn: DomainsTableColumn ) => void;
+	headerClasses?: string;
+};
+
+export const DomainsTableHeader = ( {
+	columns,
+	activeSortKey,
+	activeSortDirection,
+	onChangeSortOrder,
+	headerClasses,
+}: DomainsTableHeaderProps ) => {
+	const listHeaderClasses = classNames(
+		'domains-table-header',
+		'domains-table-header__desktop',
+		headerClasses
+	);
+
+	const renderSortIcon = (
+		column: DomainsTableColumn,
+		sortKey: string,
+		activeSortDirection: string
+	) => {
+		if ( ! column?.isSortable ) {
+			return null;
+		}
+
+		const isActiveColumn = sortKey === column.name;
+		const columnSortOrder = isActiveColumn ? activeSortDirection : column.initialSortDirection;
+
+		return <Icon icon={ columnSortOrder === 'asc' ? chevronDown : chevronUp } size={ 16 } />;
+	};
+
+	return (
+		<thead className={ listHeaderClasses }>
+			<tr>
+				{ columns.map( ( column, index ) => (
+					<th key={ `item-${ index }` }>
+						<Button
+							plain
+							onClick={ () => onChangeSortOrder( column ) }
+							className={ classNames( 'list__header-column', {
+								'is-sortable': column?.isSortable,
+							} ) }
+							tabIndex={ column?.isSortable ? 0 : -1 }
+						>
+							{ column?.headerComponent || column?.label }
+							{ renderSortIcon( column, activeSortKey, activeSortDirection ) }
+						</Button>
+					</th>
+				) ) }
+			</tr>
+		</thead>
+	);
+};
