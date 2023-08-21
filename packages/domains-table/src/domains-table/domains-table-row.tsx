2e95e72528f7f64bef2f67d04c2a226d5f31457a
Philip Jackson <p-jackson@live.com>
2023-08-21 20:05:24 +1200

    DomainsTableRow gets the site slug from the site object
    Assuming that the primary domain can be used as the site slug was
    incorrect, there are times where this isn't the case. Using test data
    exposed this.

--- packages/domains-table/src/domains-table/domains-table-row.tsx
+++ packages/domains-table/src/domains-table/domains-table-row.tsx
@@ -1,11 +1,15 @@
-import { useSiteDomainsQuery } from '@automattic/data-stores';
+import { useSiteDomainsQuery, useSiteQuery } from '@automattic/data-stores';
 import { CheckboxControl } from '@wordpress/components';
 import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import { useMemo } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { PrimaryDomainLabel } from '../primary-domain-label';
-import type { PartialDomainData, SiteDomainsQueryFnData } from '@automattic/data-stores';
+import type {
+	PartialDomainData,
+	SiteDomainsQueryFnData,
+	SiteDetails,
+} from '@automattic/data-stores';
 
 interface DomainsTableRowProps {
 	domain: PartialDomainData;
@@ -16,6 +20,7 @@ interface DomainsTableRowProps {
 	fetchSiteDomains?: (
 		siteIdOrSlug: number | string | null | undefined
 	) => Promise< SiteDomainsQueryFnData >;
+	fetchSite?: ( siteIdOrSlug: number | string | null | undefined ) => Promise< SiteDetails >;
 }
 
 export function DomainsTableRow( {
@@ -24,29 +29,39 @@ export function DomainsTableRow( {
 	isSelected,
 	onSelect,
 	fetchSiteDomains,
+	fetchSite,
 }: DomainsTableRowProps ) {
 	const { __ } = useI18n();
 	const { ref, inView } = useInView( { triggerOnce: true } );
 
-	const { data } = useSiteDomainsQuery( domain.blog_id, {
+	const { data: allSiteDomains } = useSiteDomainsQuery( domain.blog_id, {
 		enabled: inView,
 		...( fetchSiteDomains && { queryFn: () => fetchSiteDomains( domain.blog_id ) } ),
 	} );
 
-	const { siteSlug, primaryDomain } = useMemo( () => {
-		const primaryDomain = data?.domains?.find( ( d ) => d.primary_domain );
-		const unmappedDomain = data?.domains?.find( ( d ) => d.wpcom_domain );
-		const siteSlug =
-			primaryDomain?.type === 'redirect' ? unmappedDomain?.domain : primaryDomain?.domain;
+	const isPrimaryDomain = useMemo(
+		() => allSiteDomains?.domains?.find( ( d ) => d.primary_domain )?.domain === domain.domain,
+		[ allSiteDomains, domain.domain ]
+	);
 
-		return {
-			// Fall back to the site's ID if we're still loading detailed domain data
-			siteSlug: siteSlug || domain.blog_id.toString( 10 ),
-			primaryDomain,
-		};
-	}, [ data, domain.blog_id ] );
+	const { data: site } = useSiteQuery( domain.blog_id, {
+		enabled: inView,
+		...( fetchSite && { queryFn: () => fetchSite( domain.blog_id ) } ),
+	} );
 
-	const isPrimaryDomain = primaryDomain?.domain === domain.domain;
+	const siteSlug = useMemo( () => {
+		if ( ! site?.URL ) {
+			// Fall back to the site's ID if we're still loading detailed site data
+			return domain.blog_id.toString( 10 );
+		}
+
+		if ( site.options.is_redirect && site.options.unmapped_url ) {
+			return new URL( site.options.unmapped_url ).host;
+		}
+
+		return new URL( site.URL ).host.replace( /\//g, '::' );
+	}, [ site, domain.blog_id ] );
+
 	const isManageableDomain = ! domain.wpcom_domain;
 	const shouldDisplayPrimaryDomainLabel = ! isAllSitesView && isPrimaryDomain;
 
