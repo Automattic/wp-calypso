81b7a9f2a8a7667dea9e49f76b3ae51009816b7a
CodeyGuyDylan <dylan.munson@automattic.com>
2023-08-21 10:16:47 -0600

    Move bundle view to it's own component

--- client/jetpack-cloud/sections/pricing/jpcom-masterbar/components/bundles-section.tsx
+++ client/jetpack-cloud/sections/pricing/jpcom-masterbar/components/bundles-section.tsx
@@ -1,55 +1,16 @@
-import { useLocale, localizeUrl } from '@automattic/i18n-utils';
-import { Fragment } from 'react';
-import ExternalLink from 'calypso/components/external-link';
-import AntispamIcon from '../icons/jetpack-bundle-icon-antispam';
-import BackupIcon from '../icons/jetpack-bundle-icon-backup';
-import BoostIcon from '../icons/jetpack-bundle-icon-boost';
-import CRMIcon from '../icons/jetpack-bundle-icon-crm';
-import ScanIcon from '../icons/jetpack-bundle-icon-scan';
-import SearchIcon from '../icons/jetpack-bundle-icon-search';
-import SocialIcon from '../icons/jetpack-bundle-icon-social';
-import VideopressIcon from '../icons/jetpack-bundle-icon-videopress';
-import { onLinkClick, sortByMenuOrder } from '../utils';
+import { sortByMenuOrder } from '../utils';
+import Bundle from './bundle';
 import type { MenuItem } from 'calypso/data/jetpack/use-jetpack-masterbar-data-query';
-import type { FC, MouseEvent } from 'react';
+import type { FC } from 'react';
 
 interface BundlesSectionProps {
 	bundles: MenuItem | null;
 }
 
 const BundlesSection: FC< BundlesSectionProps > = ( { bundles } ) => {
-	const locale = useLocale();
-
 	if ( ! bundles || Array( bundles.items ).length < 1 ) {
 		return null;
 	}
-
-	const getBundleIcons = ( bundle: string ) => {
-		// Using a soft match in case the menu item gets deleted and recreated in wp-admin
-		// causing the name to change to `complete-2` or something similar.
-		if ( bundle.includes( 'complete' ) ) {
-			return [
-				<BackupIcon />,
-				<AntispamIcon />,
-				<ScanIcon />,
-				<SearchIcon />,
-				<SocialIcon />,
-				<VideopressIcon />,
-				<CRMIcon />,
-				<BoostIcon />,
-			];
-		}
-
-		if ( bundle.includes( 'security' ) ) {
-			return [ <BackupIcon />, <AntispamIcon />, <ScanIcon /> ];
-		}
-
-		return [];
-	};
-
-	const onBundlesLinkClick = ( e: MouseEvent< HTMLAnchorElement > ) => {
-		onLinkClick( e, 'calypso_jetpack_nav_bundles_click' );
-	};
 
 	return (
 		<div className="header__submenu-bottom-section">
@@ -61,22 +22,7 @@ const BundlesSection: FC< BundlesSectionProps > = ( { bundles } ) => {
 						Array.from( Object.values( bundles.items ) )
 							.sort( sortByMenuOrder )
 							.map( ( { id, label, href } ) => (
-								<li key={ `bundles-${ href }-${ label }` }>
-									<ExternalLink
-										className="header__submenu-link"
-										href={ localizeUrl( href, locale ) }
-										onClick={ onBundlesLinkClick }
-									>
-										<p className="header__submenu-label">
-											<span>{ label }</span>
-										</p>
-										<div className="header__submenu-bundle-icons">
-											{ getBundleIcons( id ).map( ( icon, index ) => (
-												<Fragment key={ `bundle-icon-${ id }${ index }` }>{ icon }</Fragment>
-											) ) }
-										</div>
-									</ExternalLink>
-								</li>
+								<Bundle key={ `bundles-${ href }-${ label }` } bundle={ { id, label, href } } />
 							) ) }
 				</ul>
 			</div>
