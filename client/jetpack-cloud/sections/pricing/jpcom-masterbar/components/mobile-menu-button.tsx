fa47d5928c52565a5d51b61cc8ec872058d743dd
CodeyGuyDylan <dylan.munson@automattic.com>
2023-08-21 10:40:20 -0600

    remove unnecessary prefix from mobile button

--- client/jetpack-cloud/sections/pricing/jpcom-masterbar/components/mobile-menu-button.tsx
+++ client/jetpack-cloud/sections/pricing/jpcom-masterbar/components/mobile-menu-button.tsx
@@ -12,7 +12,7 @@ interface MobileMenuButtonProps {
 const MobileMenuButton: FC< MobileMenuButtonProps > = ( { isOpen, setIsOpen, mobileMenu } ) => {
 	const translate = useTranslate();
 
-	const mobileMenuToggle = useCallback( () => {
+	const menuToggle = useCallback( () => {
 		setIsOpen( ( open ) => {
 			if ( ! open ) {
 				document.body.classList.add( 'no-scroll' );
@@ -23,38 +23,38 @@ const MobileMenuButton: FC< MobileMenuButtonProps > = ( { isOpen, setIsOpen, mob
 		} );
 	}, [ setIsOpen ] );
 
-	const mobileOnKeyDown = useCallback(
+	const onKeyDown = useCallback(
 		( e: KeyboardEvent ) => {
 			if ( e.key === 'Escape' && isOpen ) {
-				mobileMenuToggle();
+				menuToggle();
 			}
 		},
-		[ mobileMenuToggle, isOpen ]
+		[ menuToggle, isOpen ]
 	);
 
-	const onMobileButtonClick = ( e: MouseEvent ) => {
+	const onButtonClick = ( e: MouseEvent ) => {
 		e.preventDefault();
 
-		mobileMenuToggle();
+		menuToggle();
 	};
 
 	const onBlur = () => {
-		closeOnFocusOut( mobileMenu, isOpen, mobileMenuToggle );
+		closeOnFocusOut( mobileMenu, isOpen, menuToggle );
 	};
 
 	useEffect( () => {
-		document.addEventListener( 'keydown', mobileOnKeyDown );
+		document.addEventListener( 'keydown', onKeyDown );
 
 		return () => {
-			document.removeEventListener( 'keydown', mobileOnKeyDown );
+			document.removeEventListener( 'keydown', onKeyDown );
 		};
-	}, [ mobileOnKeyDown ] );
+	}, [ onKeyDown ] );
 
 	return (
 		<a
 			className="header__mobile-btn mobile-btn js-mobile-btn"
 			href="#mobile-menu"
-			onClick={ onMobileButtonClick }
+			onClick={ onButtonClick }
 			onBlur={ onBlur }
 			aria-expanded={ isOpen }
 		>
