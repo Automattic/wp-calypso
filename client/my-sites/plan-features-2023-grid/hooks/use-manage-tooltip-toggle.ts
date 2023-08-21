ba720ebc694b343350b8b98f9ba0fab1946b8610
niranjan-uma-shankar <niranjan.u@gmail.com>
2023-08-21 16:25:13 +0530

    Simplify - use closest()

--- client/my-sites/plan-features-2023-grid/hooks/use-manage-tooltip-toggle.ts
+++ client/my-sites/plan-features-2023-grid/hooks/use-manage-tooltip-toggle.ts
@@ -10,21 +10,13 @@ export function useManageTooltipToggle(): [ string, Dispatch< SetStateAction< st
 			return;
 		}
 
-		// Do not close tooltip if the user touches a tooltip container.
-		// Since the plan logos are an SVG, we are checking the event target's grandparent class to determine the tooltip container.
+		// Close all tooltips if the user touches anywhere outside a tooltip container.
+		// Since the plan logos are an SVG, we are checking the event target's parent classes to determine the tooltip container.
 		const closeAllTooltips = ( event: TouchEvent ) => {
 			const eventTarget = event.target as Element;
-			if (
-				eventTarget.classList?.contains( 'plans-2023-tooltip__hover-area-container' ) ||
-				eventTarget.parentElement?.parentElement?.classList?.contains(
-					'plans-2023-tooltip__hover-area-container'
-				)
-			) {
-				return;
+			if ( ! eventTarget.closest( '.plans-2023-tooltip__hover-area-container' ) ) {
+				setActiveTooltipId( '' );
 			}
-
-			// Close all toolltips in mobile if the user touches anywhere on the plan card.
-			setActiveTooltipId( '' );
 		};
 
 		document.addEventListener( 'touchstart', closeAllTooltips );
