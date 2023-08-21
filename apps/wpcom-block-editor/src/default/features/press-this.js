70a53ec763ab12568e71bd61694f223ce8f7eb73
Eru Penkman <roo2@protonmail.com>
2023-08-21 10:07:22 +1000

    pass text along to reblog editor

--- apps/wpcom-block-editor/src/default/features/press-this.js
+++ apps/wpcom-block-editor/src/default/features/press-this.js
@@ -3,7 +3,7 @@ import { dispatch } from '@wordpress/data';
 import { getQueryArgs } from '@wordpress/url';
 import { isEditorReady } from '../../utils';
 
-const { url, comment_content, comment_author } = getQueryArgs( window.location.href );
+const { url, title, text, comment_content, comment_author } = getQueryArgs( window.location.href );
 
 if ( url ) {
 	( async () => {
@@ -12,12 +12,21 @@ if ( url ) {
 
 		const blocks = [];
 
+		if ( text ) {
+			const link = `<a href="${ url }">${ title }</a>`;
+			blocks.push(
+				createBlock( 'core/quote', {
+					value: `<p>${ text }</p>`,
+					citation: link,
+				} )
+			);
+		}
+
 		if ( comment_content ) {
 			blocks.push(
 				createBlock( 'core/quote', { value: comment_content, citation: comment_author } )
 			);
 		}
-
 		blocks.push( createBlock( 'core/embed', { url, type: 'wp-embed' } ) );
 
 		dispatch( 'core/editor' ).resetEditorBlocks( blocks );
