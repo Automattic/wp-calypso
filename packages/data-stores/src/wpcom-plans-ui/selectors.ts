e7889c43f1e543b97605906c449451751fd27355
Jeremy Yip <jeremy.yip@automattic.com>
2023-08-21 14:11:03 -0700

    Make storage options naming consistent in wpcom-plans-ui data store

--- packages/data-stores/src/wpcom-plans-ui/selectors.ts
+++ packages/data-stores/src/wpcom-plans-ui/selectors.ts
@@ -2,5 +2,5 @@ import { PlanSlug } from '@automattic/calypso-products';
 import type { State } from './reducer';
 
 export const isDomainUpsellDialogShown = ( state: State ) => !! state.showDomainUpsellDialog;
-export const getStorageAddOnForPlan = ( state: State, planSlug: PlanSlug ) =>
-	state.selectedStorageAddOnsForPlans?.[ planSlug ];
+export const getSelectedStorageOptionsForPlan = ( state: State, planSlug: PlanSlug ) =>
+	state.selectedStorageOptionForPlans?.[ planSlug ];
