48826f53903bd636b6b080ceff3a688e587138f1
CodeyGuyDylan <dylan.munson@automattic.com>
2023-08-21 10:53:59 -0600

    Split up user menu into local components

--- client/jetpack-cloud/sections/pricing/jpcom-masterbar/components/user-menu.tsx
+++ client/jetpack-cloud/sections/pricing/jpcom-masterbar/components/user-menu.tsx
@@ -5,7 +5,77 @@ import { useState, useCallback } from 'react';
 import Gravatar from 'calypso/components/gravatar';
 import { useSelector } from 'calypso/state';
 import { isUserLoggedIn, getCurrentUser } from 'calypso/state/current-user/selectors';
+import type { UserData } from 'calypso/lib/user/user';
 import type { MouseEvent as ReactMouseEvent, FC } from 'react';
+
+interface MenuButtonProps {
+	isMenuOpen: boolean;
+	user: UserData | null;
+	toggleUserMenu: () => void;
+}
+
+interface ProfileProps {
+	isMenuOpen: boolean;
+	user: UserData | null;
+	toggleUserMenu: () => void;
+}
+
+interface TooltipProps {
+	user: UserData | null;
+}
+
+const MenuButton: FC< MenuButtonProps > = ( { isMenuOpen, user, toggleUserMenu } ) => {
+	const onUserBtnClick = ( e: ReactMouseEvent< HTMLAnchorElement, MouseEvent > ) => {
+		e.preventDefault();
+		e.stopPropagation();
+
+		toggleUserMenu();
+	};
+
+	return (
+		<a
+			className="user-menu__btn js-user-menu-btn"
+			href="#profile"
+			onClick={ onUserBtnClick }
+			aria-expanded={ isMenuOpen }
+		>
+			<Gravatar user={ user } className="user-menu__avatar" />
+		</a>
+	);
+};
+
+const Tooltip: FC< TooltipProps > = ( { user } ) => {
+	const locale = useLocale();
+	const translate = useTranslate();
+
+	return (
+		<div className="tooltip">
+			<span className="user-menu__greetings">
+				{ translate( 'Hi, %s!', {
+					args: user?.display_name || user?.username,
+				} ) }
+			</span>
+			<ul className="user-menu__list">
+				<li>
+					<a className="js-manage-sites" href={ localizeUrl( '/', locale ) }>
+						{ translate( 'Manage your sites' ) }
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+const Profile: FC< ProfileProps > = ( { isMenuOpen, user, toggleUserMenu } ) => (
+	<div
+		id="profile"
+		className="user-menu__tooltip js-user-menu js"
+		hidden={ ! isMenuOpen }
+		onBlur={ toggleUserMenu }
+	>
+		<Tooltip user={ user } />
+	</div>
+);
 
 const UserMenu: FC = () => {
 	const locale = useLocale();
@@ -28,13 +98,6 @@ const UserMenu: FC = () => {
 		setIsMenuOpen( ( open ) => ! open );
 	}, [ isMenuOpen ] );
 
-	const onUserBtnClick = ( e: ReactMouseEvent< HTMLAnchorElement, MouseEvent > ) => {
-		e.preventDefault();
-		e.stopPropagation();
-
-		toggleUserMenu();
-	};
-
 	return (
 		<ul className="header__actions-list">
 			<li
@@ -42,38 +105,9 @@ const UserMenu: FC = () => {
 					'is-logged-in': isLoggedIn,
 				} ) }
 			>
+				<MenuButton isMenuOpen={ isMenuOpen } user={ user } toggleUserMenu={ toggleUserMenu } />
 				{ isLoggedIn ? (
-					<>
-						<a
-							className="user-menu__btn js-user-menu-btn"
-							href="#profile"
-							onClick={ onUserBtnClick }
-							aria-expanded={ isMenuOpen }
-						>
-							<Gravatar user={ user } className="user-menu__avatar" />
-						</a>
-						<div
-							id="profile"
-							className="user-menu__tooltip js-user-menu js"
-							hidden={ ! isMenuOpen }
-							onBlur={ toggleUserMenu }
-						>
-							<div className="tooltip">
-								<span className="user-menu__greetings">
-									{ translate( 'Hi, %s!', {
-										args: user?.display_name || user?.username,
-									} ) }
-								</span>
-								<ul className="user-menu__list">
-									<li>
-										<a className="js-manage-sites" href={ localizeUrl( '/', locale ) }>
-											{ translate( 'Manage your sites' ) }
-										</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</>
+					<Profile isMenuOpen={ isMenuOpen } user={ user } toggleUserMenu={ toggleUserMenu } />
 				) : (
 					<a className="header__action-link js-login" href={ localizeUrl( '/login/', locale ) }>
 						{ translate( 'Log in' ) }
