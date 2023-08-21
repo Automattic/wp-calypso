0884b251faa2f0f2d03ec4ef0d717a2e0ff6970b
niranjan-uma-shankar <niranjan.u@gmail.com>
2023-08-21 14:41:53 +0530

    Fix bug with plan logo tooltip

--- client/my-sites/plan-features-2023-grid/hooks/use-manage-tooltip-toggle.ts
+++ client/my-sites/plan-features-2023-grid/hooks/use-manage-tooltip-toggle.ts
@@ -10,12 +10,21 @@ export function useManageTooltipToggle(): [ string, Dispatch< SetStateAction< st
 			return;
 		}
 
-		// Close all toolltips in mobile if the user touches anywhere on the plan card.
+		// Do not close tooltip if the user touches a tooltip container.
+		// Since the plan logos are an SVG, we are checking the event target's grandparent class to determine the tooltip container.
 		const closeAllTooltips = ( event: TouchEvent ) => {
 			const eventTarget = event.target as Element;
-			if ( ! eventTarget?.classList.contains( 'plans-2023-tooltip__hover-area-container' ) ) {
-				setActiveTooltipId( '' );
+			if (
+				eventTarget.classList?.contains( 'plans-2023-tooltip__hover-area-container' ) ||
+				eventTarget.parentElement?.parentElement?.classList?.contains(
+					'plans-2023-tooltip__hover-area-container'
+				)
+			) {
+				return;
 			}
+
+			// Close all toolltips in mobile if the user touches anywhere on the plan card.
+			setActiveTooltipId( '' );
 		};
 
 		document.addEventListener( 'touchstart', closeAllTooltips );
