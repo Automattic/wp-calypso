9d6d507745ac589f32c4067216424df322156329
vykes-mac <knrymcleish@gmail.com>
2023-08-21 20:32:27 -0500

    add sort functions for site colum

--- packages/domains-table/src/utils.ts
+++ packages/domains-table/src/utils.ts
@@ -1,4 +1,4 @@
-import { DomainData } from '@automattic/data-stores';
+import { DomainData, SiteDetails } from '@automattic/data-stores';
 
 export const getSimpleSortFunctionBy =
 	( column: keyof DomainData ) => ( first: DomainData, second: DomainData, sortOrder: number ) => {
@@ -21,3 +21,37 @@ export const getSimpleSortFunctionBy =
 export const getReverseSimpleSortFunctionBy =
 	( column: keyof DomainData ) => ( first: DomainData, second: DomainData, sortOrder: number ) =>
 		getSimpleSortFunctionBy( column )( first, second, sortOrder ) * -1;
+
+export const getSiteSortFunctions = () => {
+	return [
+		( first: DomainData, second: DomainData, sortOrder: number, sites?: SiteDetails[] ) => {
+			// sort all domain olny sites after/before the other sites
+			const firstSite = sites && sites[ first?.blog_id ];
+			const secondSite = sites && sites[ second?.blog_id ];
+
+			if ( firstSite?.options?.is_domain_only && secondSite?.options?.is_domain_only ) {
+				return 0;
+			}
+
+			if ( firstSite?.options?.is_domain_only ) {
+				return 1 * sortOrder;
+			}
+
+			if ( secondSite?.options?.is_domain_only ) {
+				return -1 * sortOrder;
+			}
+
+			return 0;
+		},
+		( first: DomainData, second: DomainData, sortOrder: number, sites?: SiteDetails[] ) => {
+			const firstSite = sites && sites[ first?.blog_id ];
+			const secondSite = sites && sites[ second?.blog_id ];
+
+			const firstTitle = firstSite?.title || firstSite?.slug;
+			const secondTitle = secondSite?.title || secondSite?.slug;
+
+			return ( firstTitle ?? '' ).localeCompare( secondTitle ?? '' ) * sortOrder;
+		},
+		getSimpleSortFunctionBy( 'domain' ),
+	];
+};
