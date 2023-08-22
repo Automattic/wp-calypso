b0c7055ec1001685fa474f162ac7f71a2bd9114d
Edwin Takahashi <egao@outlook.com>
2023-08-22 12:51:40 +0900

    packages/calypso-e2e/src/lib/pages/stats-page.ts - add new methods to Stats page to interact with various functionalities.
    test/e2e/specs/stats/stats__insights.ts
    - expand spec to interact with more of the Stats module.

--- packages/calypso-e2e/src/lib/pages/stats-page.ts
+++ packages/calypso-e2e/src/lib/pages/stats-page.ts
@@ -1,13 +1,22 @@
-import { Page } from 'playwright';
+import { Locator, Page } from 'playwright';
+import { getCalypsoURL } from '../../data-helper';
 import { clickNavTab } from '../../element-helper';
 
-export type StatsTabs = 'Traffic' | 'Insights' | 'Store';
+export type StatsTabs = 'Traffic' | 'Insights' | 'Subscribers';
+type StatsTrafficActivityCategory = 'Views' | 'Visitors' | 'Likes' | 'Comments';
+type HighlightStatsPeriod = '7-day' | '30-day';
+type StatsPeriod = 'Days' | 'Weeks' | 'Months' | 'Years';
+
+const selectors = {
+	highlightPeriodSelectButton: '.highlight-cards-heading__settings-action',
+};
 
 /**
  * Represents the Statistics page.
  */
 export class StatsPage {
 	private page: Page;
+	private anchor: Locator;
 
 	/**
 	 * Constructs an instance of the page.
@@ -16,6 +25,28 @@ export class StatsPage {
 	 */
 	constructor( page: Page ) {
 		this.page = page;
+		this.anchor = this.page.getByRole( 'main' );
+	}
+
+	/**
+	 *
+	 * @param siteSlug
+	 */
+	async visit( siteSlug?: string ) {
+		await this.page.goto( getCalypsoURL( `/stats/${ siteSlug }` ) );
+	}
+
+	/**
+	 *
+	 */
+	private async dismissModal() {
+		// Sometimes a modal appears when accessing Stats > Traffic.
+		const dismissModalButton = this.page.getByRole( 'button', { name: 'Got it' } );
+
+		if ( ( await dismissModalButton.count() ) > 0 ) {
+			await dismissModalButton.click();
+			await dismissModalButton.waitFor( { state: 'hidden' } );
+		}
 	}
 
 	/**
@@ -25,12 +56,113 @@ export class StatsPage {
 	 * @returns {Promise<void>} No return value.
 	 */
 	async clickTab( name: StatsTabs ): Promise< void > {
-		const dismissModalButton = this.page.getByRole( 'button', { name: 'Got it' } );
-
-		if ( ( await dismissModalButton.count() ) > 0 ) {
-			await dismissModalButton.click();
-			await dismissModalButton.waitFor( { state: 'hidden' } );
-		}
+		await this.dismissModal();
 		await clickNavTab( this.page, name );
+
+		// Wait for the expected URL scheme to load.
+		if ( name === 'Traffic' ) {
+			await this.page.waitForURL( /stats\/day/ );
+		}
+		if ( name === 'Insights' ) {
+			await this.page.waitForURL( /stats\/insights/ );
+		}
+		if ( name === 'Subscribers' ) {
+			await this.page.waitForURL( /stats\/subscribers/ );
+		}
+	}
+
+	// Traffic
+
+	/**
+	 *
+	 * @param period
+	 */
+	async selectHighlightPeriod( period: HighlightStatsPeriod ): Promise< void > {
+		await this.dismissModal();
+
+		const switcherButton = this.anchor.locator( selectors.highlightPeriodSelectButton );
+		await switcherButton.click();
+
+		// Tooltips live outside the normal DOM tree.
+		const tooltip = this.page.getByRole( 'tooltip', { name: /highlights/ } );
+		await tooltip.waitFor();
+		await tooltip.getByRole( 'button', { name: period } ).click();
+
+		// Dismiss.
+		await switcherButton.click();
+	}
+
+	/**
+	 *
+	 * @param period
+	 */
+	async selectStatsPeriod( period: StatsPeriod ) {
+		const target = this.anchor.getByRole( 'radiogroup' ).getByRole( 'radio', { name: period } );
+		await target.click();
+
+		if ( ! ( await target.isChecked() ) ) {
+			throw new Error( `Failed to select the Stats Period ${ period }` );
+		}
+	}
+
+	/**
+	 *
+	 * @param category
+	 */
+	async showTrafficOfType( category: StatsTrafficActivityCategory ): Promise< void > {
+		await this.page.locator( '.chart__bars' ).waitFor();
+
+		const target = this.anchor
+			.locator( '.stats-tabs' )
+			.getByRole( 'listitem' )
+			.filter( { hasText: category } );
+		await target.waitFor();
+		await target.click();
+
+		await this.page.waitForURL( new RegExp( `tab=${ category }`, 'i' ) );
+
+		const classes = await target.getAttribute( 'class' );
+		if ( ! classes?.includes( 'is-selected' ) ) {
+			throw new Error( `Failed to click and filter traffic data category to ${ category }.` );
+		}
+	}
+
+	// Insights
+
+	/**
+	 *
+	 */
+	async clickViewAllAnnualInsights() {
+		await this.anchor.getByRole( 'link', { name: 'View all annual insights' } ).click();
+
+		await this.page.waitForURL( /annualstats/ );
+	}
+
+	/**
+	 *
+	 * @param year
+	 */
+	async annualInsightPresentForYear( year: number ) {
+		await this.page
+			.getByRole( 'main' )
+			.getByRole( 'table' )
+			.getByRole( 'rowheader' )
+			.filter( { hasText: year.toString() } )
+			.waitFor();
+	}
+
+	// Subscribers
+
+	/**
+	 *
+	 * @param type
+	 */
+	async selectSubscriberType( type: 'WordPress.com' | 'Email' ) {
+		const target = this.anchor.getByRole( 'radiogroup' ).getByRole( 'radio', { name: type } );
+		await target.click();
+
+		if ( ! ( await target.isChecked() ) ) {
+			throw new Error( `Failed to select the Subscriber type ${ type }` );
+		}
 	}
 }
--- test/e2e/specs/stats/stats__insights.ts
+++ test/e2e/specs/stats/stats__insights.ts
@@ -1,34 +1,94 @@
 /**
  * @group calypso-pr
+ * @group jetpack-wpcom-integration
  */
 
-import { DataHelper, StatsPage, SidebarComponent, TestAccount } from '@automattic/calypso-e2e';
+import {
+	DataHelper,
+	StatsPage,
+	SidebarComponent,
+	TestAccount,
+	getTestAccountByFeature,
+	envToFeatureKey,
+	envVariables,
+} from '@automattic/calypso-e2e';
 import { Page, Browser } from 'playwright';
 
 declare const browser: Browser;
 
 describe( DataHelper.createSuiteTitle( 'Stats' ), function () {
 	let page: Page;
+	let testAccount: TestAccount;
+	let statsPage: StatsPage;
 
-	describe.each`
-		siteType      | accountName
-		${ 'Atomic' } | ${ 'atomicUser' }
-	`( 'View Insights ($siteType)', function ( { accountName } ) {
-		beforeAll( async () => {
-			page = await browser.newPage();
+	const accountName = getTestAccountByFeature( envToFeatureKey( envVariables ) );
 
-			const testAccount = new TestAccount( accountName );
+	beforeAll( async () => {
+		page = await browser.newPage();
+
+		testAccount = new TestAccount( accountName );
+		if ( accountName === 'jetpackAtomicEcommPlanUser' ) {
+			// eCommerce plan sites attempt to load Calypso, but with
+			// third-party cookies disabled the fallback route to WP-Admin
+			// kicks in after some time.
+			await testAccount.authenticate( page, { url: /wp-admin/ } );
+		} else {
 			await testAccount.authenticate( page );
+		}
+	} );
+
+	it( 'Navigate to Stats', async function () {
+		const sidebarComponent = new SidebarComponent( page );
+		await sidebarComponent.navigate( 'Stats' );
+
+		statsPage = new StatsPage( page );
+	} );
+
+	describe( 'Traffic', function () {
+		it( 'Click on the Traffic tab', async function () {
+			await statsPage.clickTab( 'Traffic' );
 		} );
 
-		it( 'Navigate to Stats', async function () {
-			const sidebarComponent = new SidebarComponent( page );
-			await sidebarComponent.navigate( 'Stats' );
+		it( 'View 30-day highlights', async function () {
+			await statsPage.selectHighlightPeriod( '30-day' );
 		} );
 
+		it( 'Select "Months" stats period', async function () {
+			await statsPage.selectStatsPeriod( 'Months' );
+		} );
+
+		it( 'Filter traffic activity to Likes', async function () {
+			await statsPage.showTrafficOfType( 'Likes' );
+		} );
+
+		it( 'Filter traffic activity to Comments', async function () {
+			await statsPage.showTrafficOfType( 'Views' );
+		} );
+	} );
+
+	describe( 'Insights', function () {
 		it( 'Click on Insights tab', async function () {
-			const statsPage = new StatsPage( page );
 			await statsPage.clickTab( 'Insights' );
+		} );
+
+		it( 'Click link to see all annual insights', async function () {
+			await statsPage.clickViewAllAnnualInsights();
+
+			await statsPage.annualInsightPresentForYear( 2023 );
+		} );
+
+		it( 'Go back', async function () {
+			await page.goBack();
+		} );
+	} );
+
+	describe( 'Subscribers', function () {
+		it( 'Click on Subscribers tab', async function () {
+			await statsPage.clickTab( 'Subscribers' );
+		} );
+
+		it( 'Click link to see all annual insights', async function () {
+			await statsPage.selectSubscriberType( 'Email' );
 		} );
 	} );
 } );
