c54b2ebad03e07aeb04efe08abc071595365f6b3
Ashar Fuadi <ashar.fuadi@automattic.com>
2023-08-22 21:39:47 +0700

    Assembler: move recipe states to URL

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/hooks/use-recipe.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/hooks/use-recipe.ts
@@ -1,34 +1,62 @@
 import { useColorPaletteVariations, useFontPairingVariations } from '@automattic/global-styles';
 import { keyBy } from '@automattic/js-utils';
-import { useDispatch, useSelect } from '@wordpress/data';
-import { useState, useEffect, useRef, useCallback } from 'react';
+import { useSelect } from '@wordpress/data';
+import { useState, useEffect, useRef } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { ONBOARD_STORE } from '../../../../../stores';
 import { decodePatternId } from '../utils';
 import type { Pattern, Category } from '../types';
 import type { OnboardSelect } from '@automattic/data-stores';
-import type { Design } from '@automattic/design-picker/src/types';
 import type { GlobalStylesObject } from '@automattic/global-styles';
 
 const useRecipe = ( siteId = 0, patterns: Pattern[], categories: Category[] ) => {
-	const incrementIndexRef = useRef( 0 );
-	const [ header, setHeader ] = useState< Pattern | null >( null );
-	const [ footer, setFooter ] = useState< Pattern | null >( null );
-	const [ sections, setSections ] = useState< Pattern[] >( [] );
-	const [ colorVariation, setColorVariation ] = useState< GlobalStylesObject | null >( null );
-	const [ fontVariation, setFontVariation ] = useState< GlobalStylesObject | null >( null );
+	const [ searchParams, setSearchParams ] = useSearchParams();
+
+	const header_pattern_ids = ( searchParams.get( 'header_pattern_ids' ) || '' )
+		.split( ',' )
+		.filter( Boolean );
+
+	const footer_pattern_ids = ( searchParams.get( 'footer_pattern_ids' ) || '' )
+		.split( ',' )
+		.filter( Boolean );
+
+	const section_pattern_ids = ( searchParams.get( 'pattern_ids' ) || '' )
+		.split( ',' )
+		.filter( Boolean );
+
+	const color_variation_title = searchParams.get( 'color_variation_title' );
+	const font_variation_title = searchParams.get( 'font_variation_title' );
+
+	const patternsById = keyBy( patterns, 'ID' );
+	const categoriesByName = keyBy( categories, 'name' );
+
+	const header =
+		header_pattern_ids.length === 0
+			? null
+			: patternsById[ decodePatternId( header_pattern_ids[ 0 ] ) ];
+
+	const footer =
+		footer_pattern_ids.length === 0
+			? null
+			: patternsById[ decodePatternId( footer_pattern_ids[ 0 ] ) ];
+
+	const sections = section_pattern_ids
+		.map( ( patternId ) => patternsById[ decodePatternId( patternId ) ] )
+		.filter( Boolean )
+		.map( ( pattern: Pattern ) => {
+			const [ firstCategory ] = Object.keys( pattern.categories );
+			const category = categoriesByName[ firstCategory ];
+			return {
+				...pattern,
+				category,
+			};
+		} );
+
 	const selectedDesign = useSelect(
 		( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getSelectedDesign(),
 		[]
 	);
-
-	const {
-		stylesheet = '',
-		header_pattern_ids = [],
-		footer_pattern_ids = [],
-		pattern_ids = [],
-		color_variation_title = '',
-		font_variation_title = '',
-	} = selectedDesign?.recipe || {};
+	const { stylesheet = '' } = selectedDesign?.recipe || {};
 
 	const colorVariations = useColorPaletteVariations( siteId, stylesheet, {
 		enabled: !! color_variation_title,
@@ -38,96 +66,113 @@ const useRecipe = ( siteId = 0, patterns: Pattern[], categories: Category[] ) =>
 		enabled: !! font_variation_title,
 	} );
 
-	const { setSelectedDesign } = useDispatch( ONBOARD_STORE );
+	const colorVariation =
+		( colorVariations || [] ).find( ( { title } ) => title === color_variation_title ) || null;
 
-	const selectedDesignRef = useRef< Design | undefined >( selectedDesign );
+	const fontVariation =
+		( fontVariations || [] ).find( ( { title } ) => title === font_variation_title ) || null;
 
-	selectedDesignRef.current = {
-		...selectedDesign,
-		recipe: {
-			...selectedDesign?.recipe,
-			header_pattern_ids: header ? [ header.ID ] : [],
-			footer_pattern_ids: footer ? [ footer.ID ] : [],
-			pattern_ids: sections.map( ( section ) => section.ID ),
-			color_variation_title: colorVariation?.title,
-			font_variation_title: fontVariation?.title,
-		},
-	} as Design;
+	const setHeader = ( pattern: Pattern | null ) => {
+		setSearchParams(
+			( currentSearchParams ) => {
+				if ( pattern === null ) {
+					currentSearchParams.delete( 'header_pattern_ids' );
+				} else {
+					currentSearchParams.set( 'header_pattern_ids', '' + pattern.ID );
+				}
+				return currentSearchParams;
+			},
+			{ replace: true }
+		);
+	};
 
+	const setFooter = ( pattern: Pattern | null ) => {
+		setSearchParams(
+			( currentSearchParams ) => {
+				if ( pattern === null ) {
+					currentSearchParams.delete( 'footer_pattern_ids' );
+				} else {
+					currentSearchParams.set( 'footer_pattern_ids', '' + pattern.ID );
+				}
+				return currentSearchParams;
+			},
+			{ replace: true }
+		);
+	};
+
+	const incrementIndexRef = useRef( 0 );
 	const generateKey = ( pattern: Pattern ) => {
 		incrementIndexRef.current++;
 		return `${ incrementIndexRef.current }-${ pattern.ID }`;
 	};
 
-	const snapshotRecipe = useCallback( () => setSelectedDesign( selectedDesignRef.current ), [] );
-
-	/**
-	 * Initialize the default value from the recipe of the selected design when both patterns and categories are ready
-	 */
+	const [ keyedSections, setKeyedSections ] = useState< Pattern[] >( [] );
 	useEffect( () => {
-		if ( patterns.length === 0 || categories.length === 0 || ! selectedDesignRef.current?.recipe ) {
-			return;
-		}
+		const initialKeyedSections = sections.map( ( pattern ) => ( {
+			...pattern,
+			key: generateKey( pattern ),
+		} ) );
 
-		const patternsById = keyBy( patterns, 'ID' );
-		const categoriesByName = keyBy( categories, 'name' );
-		const selectedHeader = patternsById[ decodePatternId( header_pattern_ids[ 0 ] ) ];
-		const selectedFooter = patternsById[ decodePatternId( footer_pattern_ids[ 0 ] ) ];
-
-		if ( selectedHeader ) {
-			setHeader( selectedHeader );
-		}
-
-		if ( selectedFooter ) {
-			setFooter( selectedFooter );
-		}
-
-		setSections(
-			pattern_ids
-				.map( ( patternId: string | number ) => patternsById[ decodePatternId( patternId ) ] )
-				.filter( Boolean )
-				.map( ( pattern: Pattern ) => {
-					const [ firstCategory ] = Object.keys( pattern.categories );
-					const category = categoriesByName[ firstCategory ];
-					return {
-						...pattern,
-						key: generateKey( pattern ),
-						category,
-					};
-				} )
-		);
+		setKeyedSections( initialKeyedSections );
 	}, [ patterns.length, categories.length ] );
 
-	useEffect( () => {
-		if ( ! colorVariations || ! color_variation_title ) {
-			return;
-		}
-
-		const initialColorVariation = colorVariations.find(
-			( { title } ) => title === color_variation_title
+	const setSections = ( patterns: Pattern[] ) => {
+		setSearchParams(
+			( currentSearchParams ) => {
+				if ( patterns.length === 0 ) {
+					currentSearchParams.delete( 'pattern_ids' );
+				} else {
+					currentSearchParams.set( 'pattern_ids', patterns.map( ( p ) => p.ID ).join( ',' ) );
+				}
+				return currentSearchParams;
+			},
+			{ replace: true }
 		);
-		if ( initialColorVariation ) {
-			setColorVariation( initialColorVariation );
-		}
-	}, [ colorVariations ] );
 
-	useEffect( () => {
-		if ( ! fontVariations || ! font_variation_title ) {
-			return;
-		}
-
-		const initialFontVariation = fontVariations.find(
-			( { title } ) => title === font_variation_title
+		setKeyedSections(
+			patterns.map( ( pattern ) =>
+				pattern.key
+					? pattern
+					: {
+							...pattern,
+							key: generateKey( pattern ),
+					  }
+			)
 		);
-		if ( initialFontVariation ) {
-			setFontVariation( initialFontVariation );
-		}
-	}, [ fontVariations ] );
+	};
+
+	const setColorVariation = ( variation: GlobalStylesObject | null ) => {
+		setSearchParams(
+			( currentSearchParams ) => {
+				if ( variation?.title ) {
+					currentSearchParams.set( 'color_variation_title', variation?.title );
+				} else {
+					currentSearchParams.delete( 'color_variation_title' );
+				}
+				return currentSearchParams;
+			},
+			{ replace: true }
+		);
+	};
+
+	const setFontVariation = ( variation: GlobalStylesObject | null ) => {
+		setSearchParams(
+			( currentSearchParams ) => {
+				if ( variation?.title ) {
+					currentSearchParams.set( 'font_variation_title', variation?.title );
+				} else {
+					currentSearchParams.delete( 'font_variation_title' );
+				}
+				return currentSearchParams;
+			},
+			{ replace: true }
+		);
+	};
 
 	return {
 		header,
 		footer,
-		sections,
+		sections: keyedSections,
 		colorVariation,
 		fontVariation,
 		setHeader,
@@ -135,8 +180,6 @@ const useRecipe = ( siteId = 0, patterns: Pattern[], categories: Category[] ) =>
 		setSections,
 		setColorVariation,
 		setFontVariation,
-		generateKey,
-		snapshotRecipe,
 	};
 };
 
