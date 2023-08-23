11e020bf16a63b0263bb5ea5245f79fdc8f779d3
Dehan <dehandecroos@gmail.com>
2023-08-22 20:41:17 -0400

    Props should be optional

--- client/my-sites/plans-features-main/components/loading-placeholder.tsx
+++ client/my-sites/plans-features-main/components/loading-placeholder.tsx
@@ -4,10 +4,10 @@ import styled from '@emotion/styled';
  * Simple placeholder that renders as a pulsing line.
  */
 export const LoadingPlaceHolder = styled.div< {
-	display: 'block' | 'inline-block';
-	width: string;
-	height: string;
-	borderRadius: string;
+	display?: 'block' | 'inline-block';
+	width?: string;
+	height?: string;
+	borderRadius?: string;
 } >`
 	margin: 0;
 	background: var( --color-neutral-10 );
