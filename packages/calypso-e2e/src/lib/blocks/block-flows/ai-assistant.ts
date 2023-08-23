6874f5cfec79968c4913f3a09296ad57dffebe69
Edwin Takahashi <egao@outlook.com>
2023-08-23 16:06:06 +0900

    packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts - refactor methods to work with both block-based and non-block-based themes. - add waits throughout the methods

--- packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts
+++ packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts
@@ -45,113 +45,103 @@ export class AIAssistantFlow implements BlockFlow {
 	 */
 	async configure( context: EditorContext ): Promise< void > {
 		const editorCanvas = await context.editorPage.getEditorCanvas();
+		const block = editorCanvas.getByRole( 'document', {
+			name: `Block: ${ this.blockSidebarName }`,
+		} );
 
-		await this.initialQuery( editorCanvas );
+		await this.enterInput( block );
+		await this.clickButtonOnBlock( block, 'Send' );
+		await this.waitForQuery( editorCanvas );
 
 		if ( this.configurationData.tone ) {
-			await this.changeTone( editorCanvas );
+			await this.clickToolbarButton( 'Change tone', this.configurationData.tone, context );
+			await this.waitForQuery( editorCanvas );
 		}
 
 		if ( this.configurationData.improve ) {
-			await this.improve( editorCanvas );
+			await this.clickToolbarButton( 'Improve', this.configurationData.improve, context );
+			await this.waitForQuery( editorCanvas );
 		}
 
-		await this.accept( editorCanvas );
+		await this.clickButtonOnBlock( block, 'Accept' );
+		await block.waitFor( { state: 'detached' } );
+		this.generatedText = await editorCanvas.locator( '.is-selected' ).innerText();
 	}
 
 	/**
-	 * Fill the text input with the initial query and submit.
+	 * Clicks on the toolbar button, then in the dropdown that appears, clicks an option.
 	 *
-	 * @param {Locator} editorCanvas Locator to the editor.
+	 * @param {string} buttonName Name of the button to click.
+	 * @param {string} optionName Name of the option in the dropdown to click.
+	 * @param {EditorContext} context The current context for the editor at the point of
+	 * test execution.
 	 */
-	private async initialQuery( editorCanvas: Locator ) {
-		const anchor = editorCanvas.getByRole( 'document', {
-			name: `Block: ${ this.blockSidebarName }`,
-		} );
+	private async clickToolbarButton(
+		buttonName: string,
+		optionName: string,
+		context: EditorContext
+	) {
+		// Block-based and non-block-based themes strike again.
+		// For block-based themes (eg. Twenty Twenty-Three) a given block's toolbar
+		// lives "outside" of the DOM tree returned by `getEditorCanvas`.
+		// For non-block-based themes (eg. Twenty Twenty-One) a block's toolbar
+		// lives in the same DOM tree as the locator returned by `getEditorCanvas`.
+		// However, the locator returned by `getEditorParent` has the toolbar inside
+		// the DOM tree in both cases.
+		const editorParent = await context.editorPage.getEditorParent();
 
-		// Fill in the query into the input field.
-		await anchor
+		const toolbar = editorParent.getByRole( 'toolbar', { name: 'Block tools' } );
+		await toolbar.waitFor();
+		// Yes, yuck, I know. But Playwright often tries to act on the buttons sooner
+		// than it should, so we need to wait for the bounding box of the element here
+		// to be stable before attempting to click on the toolbar.
+		const elementHandle = await toolbar.elementHandle();
+		await elementHandle?.waitForElementState( 'stable' );
+
+		const button = toolbar.getByRole( 'button', { name: buttonName } );
+		await button.waitFor();
+		await button.click();
+
+		const option = editorParent.getByRole( 'menuitem', { name: optionName } );
+		await option.waitFor();
+		await option.click();
+	}
+
+	/**
+	 * Clicks on a button on the block.
+	 *
+	 * @param {Locator} block Locator to the block.
+	 * @param {string}name Name of the button to click.
+	 */
+	private async clickButtonOnBlock( block: Locator, name: string ) {
+		await block.getByRole( 'button', { name: name } ).click();
+	}
+
+	/**
+	 * Enters the query into the input on the block.
+	 *
+	 * @param {Locator} block Locator to the block.
+	 */
+	private async enterInput( block: Locator ) {
+		await block
 			.getByRole( 'textbox', { name: 'Ask Jetpack AI' } )
 			.fill( this.configurationData.query );
-
-		await anchor.getByRole( 'button', { name: 'Send' } ).click();
-
-		// Wait for the query and text generation to complete.
-		await this.waitForQuery( anchor );
-	}
-
-	/**
-	 * Clicks on the tone specified in the configuration data.
-	 *
-	 * @param {Locator} editorCanvas Locator to the editor.
-	 */
-	private async changeTone( editorCanvas: Locator ) {
-		const anchor = editorCanvas.getByRole( 'toolbar', { name: 'Block tools' } );
-
-		const changeToneButton = anchor.getByRole( 'button', { name: 'Change tone' } );
-		await changeToneButton.waitFor();
-		await changeToneButton.click();
-
-		await editorCanvas
-			.getByRole( 'menu', { name: 'Change tone' } )
-			.getByRole( 'menuitem', { name: this.configurationData.tone } )
-			.click();
-
-		// Wait for the query and text generation to complete.
-		await this.waitForQuery( anchor );
-	}
-
-	/**
-	 * Clicks on the improve option specified in the configuration data.
-	 *
-	 * @param {Locator} editorCanvas Locator to the editor.
-	 */
-	private async improve( editorCanvas: Locator ) {
-		const anchor = editorCanvas.getByRole( 'toolbar', { name: 'Block tools' } );
-
-		const improveButton = anchor.getByRole( 'button', { name: 'Improve' } );
-		await improveButton.waitFor();
-		await improveButton.click();
-
-		await editorCanvas
-			.getByRole( 'menu', { name: 'Improve' } )
-			.getByRole( 'menuitem', { name: this.configurationData.improve } )
-			.click();
-
-		// Wait for the query and text generation to complete.
-		await this.waitForQuery( anchor );
-	}
-
-	/**
-	 * Accepts the generated text.
-	 *
-	 * @param {Locator} editorCanvas Locator to the editor.
-	 */
-	private async accept( editorCanvas: Locator ) {
-		const anchor = editorCanvas.getByRole( 'document', {
-			name: `Block: ${ this.blockSidebarName }`,
-		} );
-
-		// Accept the query and confirm the AI Assistant block is replaced.
-		await anchor.getByRole( 'button', { name: 'Accept' } ).click();
-		await anchor.waitFor( { state: 'detached' } );
-
-		this.generatedText = await editorCanvas.locator( '.is-selected' ).innerText();
 	}
 
 	/**
 	 * Helper method to wait for the query to complete.
 	 *
-	 * @param {Locator} anchor The root locator of the block.
+	 * @param {Locator} block Locator to the block.
 	 */
-	private async waitForQuery( anchor: Locator ) {
+	private async waitForQuery( block: Locator ) {
 		await Promise.all( [
-			anchor
+			block
 				.getByRole( 'button', { name: 'Stop request' } )
 				.waitFor( { state: 'detached', timeout: 15 * 1000 } ),
-			anchor
+			block
 				.getByRole( 'textbox', { name: 'Ask Jetpack AI', disabled: true } )
 				.waitFor( { state: 'detached', timeout: 15 * 1000 } ),
+			block.locator( '.components-spinner' ).waitFor( { state: 'detached' } ),
 		] );
 	}
 
