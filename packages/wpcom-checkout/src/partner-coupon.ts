d438e8b8c29be210236211335994b415288f9329
Payton Swick <payton@foolord.com>
2023-08-23 16:14:43 -0400

    Replace getSublabel in getPartnerCoupon

--- packages/wpcom-checkout/src/partner-coupon.ts
+++ packages/wpcom-checkout/src/partner-coupon.ts
@@ -1,4 +1,19 @@
-import { getSublabel } from './checkout-labels';
+import {
+	isAddOn,
+	isPlan,
+	isDomainTransfer,
+	isDomainProduct,
+	isDotComPlan,
+	isGoogleWorkspace,
+	isGSuiteOrExtraLicenseProductSlug,
+	isTitanMail,
+	isJetpackProductSlug,
+	isMonthlyProduct,
+	isYearly,
+	isBiennially,
+	isTriennially,
+} from '@automattic/calypso-products';
+import { isWpComProductRenewal as isRenewal } from './is-wpcom-product-renewal';
 import type { ResponseCartProduct } from '@automattic/shopping-cart';
 
 /**
@@ -15,9 +30,70 @@ export function getPartnerCoupon( {
 	coupon: string;
 	products?: ResponseCartProduct[];
 } ): boolean {
-	const productHasSublabel =
-		products && products.some( ( product: ResponseCartProduct ) => !! getSublabel( product ) );
+	const isAllowed =
+		products &&
+		products.some( ( product: ResponseCartProduct ) => !! isAllowedPartnerProduct( product ) );
 	const isPartnerCoupon = coupon.startsWith( 'IONOS_' );
 
-	return ( productHasSublabel && isPartnerCoupon ) || false;
+	return ( isAllowed && isPartnerCoupon ) || false;
+}
+
+function isAllowedPartnerProduct( product: ResponseCartProduct ): boolean {
+	const isRenewalItem = isRenewal( product );
+	const { meta, product_name: productName, product_slug: productSlug } = product;
+
+	if ( isDotComPlan( product ) && isRenewalItem ) {
+		return true;
+	}
+
+	if ( isPlan( product ) || isJetpackProductSlug( productSlug ) ) {
+		return true;
+	}
+
+	if ( isGoogleWorkspace( product ) || isGSuiteOrExtraLicenseProductSlug( productSlug ) ) {
+		return true;
+	}
+
+	if ( isTitanMail( product ) ) {
+		return true;
+	}
+
+	if ( meta && ( isDomainProduct( product ) || isDomainTransfer( product ) ) ) {
+		if ( ! isRenewalItem ) {
+			if ( productName ) {
+				return true;
+			}
+			return false;
+		}
+
+		if ( productName ) {
+			return true;
+		}
+	}
+
+	if ( isAddOn( product ) && ! isRenewalItem ) {
+		return true;
+	}
+
+	if ( isRenewalItem ) {
+		return true;
+	}
+
+	if ( isMonthlyProduct( product ) ) {
+		return true;
+	}
+
+	if ( isYearly( product ) ) {
+		return true;
+	}
+
+	if ( isBiennially( product ) ) {
+		return true;
+	}
+
+	if ( isTriennially( product ) ) {
+		return true;
+	}
+
+	return false;
 }
