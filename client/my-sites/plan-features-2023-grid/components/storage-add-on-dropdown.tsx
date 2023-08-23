a3b863abfe8fbed24a85e1a3a85ef1d1ec5a5946
Jeremy Yip <jeremy.yip@automattic.com>
2023-08-23 16:27:52 -0700

    Add storage add on cost to dropdown options

--- client/my-sites/plan-features-2023-grid/components/storage-add-on-dropdown.tsx
+++ client/my-sites/plan-features-2023-grid/components/storage-add-on-dropdown.tsx
@@ -2,6 +2,7 @@ import { WpcomPlansUI } from '@automattic/data-stores';
 import { CustomSelectControl } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { TranslateResult, useTranslate } from 'i18n-calypso';
+import { usePlansGridContext } from '../grid-context';
 import { getStorageStringFromFeature } from '../util';
 import type { PlanSlug, StorageOption, WPComStorageAddOnSlug } from '@automattic/calypso-products';
 
@@ -12,8 +13,10 @@ type StorageAddOnDropdownProps = {
 
 export const StorageAddOnDropdown = ( { planSlug, storageOptions }: StorageAddOnDropdownProps ) => {
 	const translate = useTranslate();
+	const { gridPlansIndex } = usePlansGridContext();
+	const { storageAddOnsForPlan } = gridPlansIndex[ planSlug ];
 	const { setSelectedStorageOptionForPlan } = useDispatch( WpcomPlansUI.store );
-	const selectedStorage = useSelect(
+	const selectedStorageOptionForPlan = useSelect(
 		( select ) => {
 			return select( WpcomPlansUI.store ).getSelectedStorageOptionForPlan( planSlug );
 		},
@@ -22,11 +25,15 @@ export const StorageAddOnDropdown = ( { planSlug, storageOptions }: StorageAddOn
 
 	// TODO: Consider transforming storageOptions outside of this component
 	const selectControlOptions = storageOptions.reduce( ( acc, storageOption ) => {
+		const cost = storageAddOnsForPlan?.find( ( addOn ) => {
+			return addOn?.featureSlugs?.includes( storageOption.slug );
+		} )?.costData?.formattedMonthlyCost;
 		const title = getStorageStringFromFeature( storageOption.slug );
+		const name = `${ title } ${ cost ? cost : '' }`;
 		if ( title ) {
 			acc.push( {
 				key: storageOption?.slug,
-				name: title,
+				name,
 			} );
 		}
 
@@ -34,7 +41,7 @@ export const StorageAddOnDropdown = ( { planSlug, storageOptions }: StorageAddOn
 	}, [] as { key: string; name: TranslateResult }[] );
 
 	const defaultStorageOption = storageOptions.find( ( storageOption ) => ! storageOption?.isAddOn );
-	const selectedOptionKey = selectedStorage || defaultStorageOption?.slug || '';
+	const selectedOptionKey = selectedStorageOptionForPlan || defaultStorageOption?.slug || '';
 	const selectedOption = {
 		key: selectedOptionKey,
 		name: getStorageStringFromFeature( selectedOptionKey ),
