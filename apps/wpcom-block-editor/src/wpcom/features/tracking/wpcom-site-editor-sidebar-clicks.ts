6b0cffd325cd481a7589f8297a745cad2909bd38
okmttdhr <okmttdhr@users.noreply.github.com>
2023-08-24 11:34:29 +0900

    Change the event name to more generic

--- apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-site-editor-sidebar-clicks.ts
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-site-editor-sidebar-clicks.ts
@@ -6,7 +6,10 @@ export const wpcomSiteEditorSidebarNavigationClick = () => {
 		// \2f is the encoded slash.
 		selector: '#\\2fnavigation',
 		type: 'click',
-		handler: () => tracksRecordEvent( 'wpcom_site_editor_sidebar_navigation_click' ),
+		handler: () =>
+			tracksRecordEvent( 'wpcom_block_editor_nav_sidebar_main_item_click', {
+				item_type: 'navigation',
+			} ),
 	};
 };
 
@@ -16,7 +19,10 @@ export const wpcomSiteEditorSidebarPagesClick = () => {
 		// \2f is the encoded slash.
 		selector: '#\\2fpage',
 		type: 'click',
-		handler: () => tracksRecordEvent( 'wpcom_site_editor_sidebar_pages_click' ),
+		handler: () =>
+			tracksRecordEvent( 'wpcom_block_editor_nav_sidebar_main_item_click', {
+				item_type: 'pages',
+			} ),
 	};
 };
 
@@ -26,7 +32,10 @@ export const wpcomSiteEditorSidebarPatternsClick = () => {
 		// \2f is the encoded slash.
 		selector: '#\\2fpatterns',
 		type: 'click',
-		handler: () => tracksRecordEvent( 'wpcom_site_editor_sidebar_patterns_click' ),
+		handler: () =>
+			tracksRecordEvent( 'wpcom_block_editor_nav_sidebar_main_item_click', {
+				item_type: 'patterns',
+			} ),
 	};
 };
 
@@ -36,7 +45,10 @@ export const wpcomSiteEditorSidebarStylesClick = () => {
 		// \2f is the encoded slash.
 		selector: '#\\2fwp_global_styles',
 		type: 'click',
-		handler: () => tracksRecordEvent( 'wpcom_site_editor_sidebar_styles_click' ),
+		handler: () =>
+			tracksRecordEvent( 'wpcom_block_editor_nav_sidebar_main_item_click', {
+				item_type: 'styles',
+			} ),
 	};
 };
 
@@ -46,6 +58,9 @@ export const wpcomSiteEditorSidebarTemplatesClick = () => {
 		// \2f is the encoded slash.
 		selector: `#\\2fwp_template`,
 		type: 'click',
-		handler: () => tracksRecordEvent( 'wpcom_site_editor_sidebar_templates_click' ),
+		handler: () =>
+			tracksRecordEvent( 'wpcom_block_editor_nav_sidebar_main_item_click', {
+				item_type: 'templates',
+			} ),
 	};
 };
