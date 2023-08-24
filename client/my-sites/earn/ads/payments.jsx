f95f3387fd8ed147f9e5ee3b9386c32a51722de6
Erick Danzer <edanzer@gmail.com>
2023-08-24 14:23:06 -0600

    Earn: Refactor ad payments component to functional (#81034)

--- client/my-sites/earn/ads/payments.jsx
+++ client/my-sites/earn/ads/payments.jsx
@@ -1,41 +1,22 @@
 import { Badge, Card } from '@automattic/components';
 import classNames from 'classnames';
-import { localize } from 'i18n-calypso';
-import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useTranslate } from 'i18n-calypso';
 import QueryWordadsPayments from 'calypso/components/data/query-wordads-payments';
 import QueryWordadsSettings from 'calypso/components/data/query-wordads-settings';
 import Notice from 'calypso/components/notice';
 import { CALYPSO_CONTACT } from 'calypso/lib/url/support';
+import { useSelector } from 'calypso/state';
 import { getWordadsSettings } from 'calypso/state/selectors/get-wordads-settings';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { getWordAdsPayments } from 'calypso/state/wordads/payments/selectors';
 
-class WordAdsPayments extends Component {
-	static propTypes = {
-		siteId: PropTypes.number,
-		payments: PropTypes.arrayOf(
-			PropTypes.shape( {
-				id: PropTypes.number.isRequired,
-				paymentDate: PropTypes.string,
-				amount: PropTypes.string,
-				status: PropTypes.string,
-				paypalEmail: PropTypes.string.isRequired,
-				description: PropTypes.string,
-			} )
-		).isRequired,
-	};
+const WordAdsPayments = ( { numberFormat } ) => {
+	const translate = useTranslate();
+	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
+	const payments = useSelector( ( state ) => getWordAdsPayments( state, siteId ) );
+	const wordAdsSettings = useSelector( ( state ) => getWordadsSettings( state, siteId ) );
 
-	checkSize( obj ) {
-		if ( ! obj ) {
-			return 0;
-		}
-
-		return Object.keys( obj ).length;
-	}
-
-	statusToType( status ) {
+	function statusToType( status ) {
 		const map = {
 			paid: 'success',
 			pending: 'info',
@@ -44,12 +25,11 @@ class WordAdsPayments extends Component {
 		return map[ status ] || 'warning';
 	}
 
-	paymentsTable( payments, type ) {
-		const { numberFormat, translate } = this.props;
+	function paymentsTable( currentPayments, type ) {
 		const rows = [];
 		const classes = classNames( 'payments_history' );
 
-		payments.forEach( ( payment ) => {
+		currentPayments.forEach( ( payment ) => {
 			rows.push(
 				<tr key={ type + '-' + payment.id }>
 					<td className="ads__payments-history-value">
@@ -63,10 +43,7 @@ class WordAdsPayments extends Component {
 					</td>
 					<td className="ads__payments-history-value">${ numberFormat( payment.amount, 2 ) }</td>
 					<td className="ads__payments-history-value">
-						<Badge
-							className="ads__payments-history-badge"
-							type={ this.statusToType( payment.status ) }
-						>
+						<Badge className="ads__payments-history-badge" type={ statusToType( payment.status ) }>
 							{ payment.status }
 						</Badge>
 					</td>
@@ -101,21 +78,19 @@ class WordAdsPayments extends Component {
 		);
 	}
 
-	notices( payments, wordAdsSettings ) {
-		const { translate } = this.props;
-
-		if ( ! payments || ! wordAdsSettings ) {
+	function notices( currentPayments, currentWordAdsSettings ) {
+		if ( ! currentPayments || ! currentWordAdsSettings ) {
 			return null;
 		}
 
-		if ( ! wordAdsSettings.paypal ) {
+		if ( ! currentWordAdsSettings.paypal ) {
 			return null;
 		}
 
 		let hasMismatch = false;
 
-		payments.forEach( ( payment ) => {
-			if ( payment.status === 'pending' && payment.paypalEmail !== wordAdsSettings.paypal ) {
+		currentPayments.forEach( ( payment ) => {
+			if ( payment.status === 'pending' && payment.paypalEmail !== currentWordAdsSettings.paypal ) {
 				hasMismatch = true;
 			}
 		} );
@@ -137,9 +112,7 @@ class WordAdsPayments extends Component {
 		) : null;
 	}
 
-	empty() {
-		const { translate } = this.props;
-
+	function empty() {
 		return (
 			<Card>
 				{ translate(
@@ -149,28 +122,14 @@ class WordAdsPayments extends Component {
 		);
 	}
 
-	render() {
-		const { siteId, payments, wordAdsSettings } = this.props;
-		return (
-			<div>
-				<QueryWordadsSettings siteId={ siteId } />
-				<QueryWordadsPayments siteId={ siteId } />
-				{ this.notices( payments, wordAdsSettings ) }
-				{ payments && payments.length > 0
-					? this.paymentsTable( payments, 'wordads' )
-					: this.empty() }
-			</div>
-		);
-		/* eslint-enable wpcalypso/jsx-classname-namespace */
-	}
-}
+	return (
+		<div>
+			<QueryWordadsSettings siteId={ siteId } />
+			<QueryWordadsPayments siteId={ siteId } />
+			{ notices( payments, wordAdsSettings ) }
+			{ payments && payments.length > 0 ? paymentsTable( payments, 'wordads' ) : empty() }
+		</div>
+	);
+};
 
-export default connect( ( state ) => {
-	const siteId = getSelectedSiteId( state );
-
-	return {
-		siteId,
-		payments: getWordAdsPayments( state, siteId ),
-		wordAdsSettings: getWordadsSettings( state, siteId ),
-	};
-} )( localize( WordAdsPayments ) );
+export default WordAdsPayments;
