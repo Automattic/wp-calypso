7b514087743efe43ff916e750b603a81c3097aeb
Ashar Fuadi <ashar.fuadi@automattic.com>
2023-08-24 12:32:54 +0700

    Explain hidden logic more clearly

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/hooks/use-recipe.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/hooks/use-recipe.ts
@@ -1,7 +1,7 @@
 import { useColorPaletteVariations, useFontPairingVariations } from '@automattic/global-styles';
 import { keyBy } from '@automattic/js-utils';
 import { useSelect } from '@wordpress/data';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { ONBOARD_STORE } from '../../../../../stores';
 import { decodePatternId } from '../utils';
@@ -28,17 +28,55 @@ const useRecipe = ( siteId = 0, patterns: Pattern[], categories: Category[] ) =>
 	const header = header_pattern_id ? patternsById[ decodePatternId( header_pattern_id ) ] : null;
 	const footer = footer_pattern_id ? patternsById[ decodePatternId( footer_pattern_id ) ] : null;
 
-	const sections = section_pattern_ids
-		.map( ( patternId ) => patternsById[ decodePatternId( patternId ) ] )
-		.filter( Boolean )
-		.map( ( pattern: Pattern ) => {
-			const [ firstCategory ] = Object.keys( pattern.categories );
-			const category = categoriesByName[ firstCategory ];
-			return {
-				...pattern,
-				category,
-			};
-		} );
+	/* Adds a unique "key" prop to each section pattern,
+	   so that when the patterns are rendered in a list,
+	   they are not re-rendered unnecessarily when the list
+	   is manipulated (some elements get appended / removed).
+	*/
+	const [ keyedSections, setKeyedSections ] = useState< Pattern[] >( [] );
+
+	/* To sync the keyed sections with the search params,
+	   we first initialize the sections from the search params.
+	*/
+	const initialSections = useMemo(
+		() =>
+			section_pattern_ids
+				.map( ( patternId ) => patternsById[ decodePatternId( patternId ) ] )
+				.filter( Boolean )
+				.map( ( pattern: Pattern ) => {
+					const [ firstCategory ] = Object.keys( pattern.categories );
+					const category = categoriesByName[ firstCategory ];
+					return {
+						...pattern,
+						category,
+					};
+				} ),
+		/* We initialize the sections (at most) once when the patterns and categories are finally loaded
+		   (hence the ".length"s in the dependency array).
+
+		   We are ignoring the changes from the search params (section_pattern_ids),
+	       since after this point, the changes will be handled by setSections().
+	    */
+		[ patterns.length, categories.length ] // eslint-disable-line react-hooks/exhaustive-deps
+	);
+
+	const uniqueSectionPatternKeyRef = useRef( 0 );
+	const generateSectionPatternKey = ( pattern: Pattern ) => {
+		uniqueSectionPatternKeyRef.current++;
+		return `${ uniqueSectionPatternKeyRef.current }-${ pattern.ID }`;
+	};
+
+	/* Generates the keys to the initial sections and set them as the initial value of keyedSections.
+	   After this point, the changes will be handled by setSections().
+    */
+	useEffect( () => {
+		const initialKeyedSections = initialSections.map( ( pattern ) => ( {
+			...pattern,
+			key: generateSectionPatternKey( pattern ),
+		} ) );
+
+		setKeyedSections( initialKeyedSections );
+	}, [ initialSections ] );
 
 	const selectedDesign = useSelect(
 		( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getSelectedDesign(),
@@ -88,29 +126,6 @@ const useRecipe = ( siteId = 0, patterns: Pattern[], categories: Category[] ) =>
 		);
 	};
 
-	const incrementIndexRef = useRef( 0 );
-	const generateKey = ( pattern: Pattern ) => {
-		incrementIndexRef.current++;
-		return `${ incrementIndexRef.current }-${ pattern.ID }`;
-	};
-
-	// Adds a unique "key" prop to each section pattern,
-	// so that when the patterns are rendered in a list,
-	// they are not re-rendered unnecessarily when the list
-	// is manipulated (some elements get appended / removed).
-	const [ keyedSections, setKeyedSections ] = useState< Pattern[] >( [] );
-
-	// Initializes the keys once,
-	// when the patterns and categories are finally loaded.
-	useEffect( () => {
-		const initialKeyedSections = sections.map( ( pattern ) => ( {
-			...pattern,
-			key: generateKey( pattern ),
-		} ) );
-
-		setKeyedSections( initialKeyedSections );
-	}, [ patterns.length, categories.length ] );
-
 	const setSections = ( patterns: Pattern[] ) => {
 		setSearchParams(
 			( currentSearchParams ) => {
@@ -130,7 +145,7 @@ const useRecipe = ( siteId = 0, patterns: Pattern[], categories: Category[] ) =>
 					? pattern
 					: {
 							...pattern,
-							key: generateKey( pattern ),
+							key: generateSectionPatternKey( pattern ),
 					  }
 			)
 		);
