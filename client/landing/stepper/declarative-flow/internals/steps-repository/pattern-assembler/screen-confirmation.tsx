a7a826c2ec090c06a465dc709798702561340b53
Miguel <miguel.san.segundo@automattic.com>
2023-08-25 14:10:59 +0700

    Fix pending translations

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/screen-confirmation.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/screen-confirmation.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@automattic/components';
+import { useIsEnglishLocale } from '@automattic/i18n-utils';
 import { NavigatorHeader } from '@automattic/onboarding';
 import {
 	__experimentalVStack as VStack,
 	__experimentalHStack as HStack,
 } from '@wordpress/components';
 import { Icon, image, verse, layout } from '@wordpress/icons';
-import { useTranslate } from 'i18n-calypso';
+import i18n, { useTranslate } from 'i18n-calypso';
 import { PATTERN_ASSEMBLER_EVENTS } from './events';
 import NavigatorTitle from './navigator-title';
 import './screen-confirmation.scss';
@@ -17,22 +18,35 @@ interface Props {
 
 const ScreenConfirmation = ( { onConfirm, recordTracksEvent }: Props ) => {
 	const translate = useTranslate();
+	const isEnglishLocale = useIsEnglishLocale();
 
 	const list = [
 		{
 			icon: image,
 			title: translate( 'Upload images' ),
-			description: translate( 'Showcase your photos in their best light.' ),
+			description:
+				isEnglishLocale || i18n.hasTranslation( 'Showcase your photos in their best light.' )
+					? translate( 'Showcase your photos in their best light.' )
+					: null,
 		},
 		{
 			icon: verse,
 			title: translate( 'Start writing' ),
-			description: translate( 'Get things going and share your insights.' ),
+			description:
+				isEnglishLocale || i18n.hasTranslation( 'Get things going and share your insights.' )
+					? translate( 'Get things going and share your insights.' )
+					: null,
 		},
 		{
 			icon: layout,
-			title: translate( 'Customize every detail' ),
-			description: translate( 'Make your site even more unique.' ),
+			title:
+				isEnglishLocale || i18n.hasTranslation( 'Customize every detail' )
+					? translate( 'Customize every detail' )
+					: translate( 'Customize in editor' ),
+			description:
+				isEnglishLocale || i18n.hasTranslation( 'Make your site even more unique.' )
+					? translate( 'Make your site even more unique.' )
+					: null,
 		},
 	];
 
@@ -40,7 +54,12 @@ const ScreenConfirmation = ( { onConfirm, recordTracksEvent }: Props ) => {
 		<>
 			<NavigatorHeader
 				title={ <NavigatorTitle title={ translate( 'Great job!' ) } /> }
-				description={ translate( 'Time to add some content and bring your site to life!' ) }
+				description={
+					isEnglishLocale ||
+					i18n.hasTranslation( 'Time to add some content and bring your site to life!' )
+						? translate( 'Time to add some content and bring your site to life!' )
+						: translate( 'Bring your site to life with some content.' )
+				}
 				onBack={ () => {
 					recordTracksEvent( PATTERN_ASSEMBLER_EVENTS.SCREEN_BACK_CLICK, {
 						screen_from: 'confirmation',
@@ -65,7 +84,9 @@ const ScreenConfirmation = ( { onConfirm, recordTracksEvent }: Props ) => {
 			</div>
 			<div className="screen-container__footer">
 				<Button className="pattern-assembler__button" primary onClick={ onConfirm }>
-					{ translate( 'Start adding content' ) }
+					{ isEnglishLocale || i18n.hasTranslation( 'Start adding content' )
+						? translate( 'Start adding content' )
+						: translate( 'Continue' ) }
 				</Button>
 			</div>
 		</>
