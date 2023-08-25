45eb84e2b2603033060cb4161ca73d3465ef1902
Edwin Takahashi <egao@outlook.com>
2023-08-25 15:13:11 +0900

    packages/calypso-e2e/src/lib/pages/editor-page.ts - add cookie dismissal

--- packages/calypso-e2e/src/lib/pages/editor-page.ts
+++ packages/calypso-e2e/src/lib/pages/editor-page.ts
@@ -19,6 +19,7 @@ import {
 	EditorPopoverMenuComponent,
 	TemplateCategory,
 	BlockToolbarButtonIdentifier,
+	CookieBannerComponent,
 } from '../components';
 import { BlockInserter, OpenInlineInserter } from './shared-types';
 import type {
@@ -60,6 +61,7 @@ export class EditorPage {
 	private editorBlockToolbarComponent: EditorBlockToolbarComponent;
 	private editorTemplateModalComponent: EditorTemplateModalComponent;
 	private editorPopoverMenuComponent: EditorPopoverMenuComponent;
+	private cookieBannerComponent: CookieBannerComponent;
 
 	/**
 	 * Constructs an instance of the component.
@@ -89,6 +91,7 @@ export class EditorPage {
 		);
 		this.editorTemplateModalComponent = new EditorTemplateModalComponent( page, this.editor );
 		this.editorPopoverMenuComponent = new EditorPopoverMenuComponent( page, this.editor );
+		this.cookieBannerComponent = new CookieBannerComponent( page, this.editor );
 	}
 
 	//#region Generic and Shell Methods
@@ -125,6 +128,9 @@ export class EditorPage {
 
 		// Dismiss the Welcome Tour.
 		await this.editorWelcomeTourComponent.forceDismissWelcomeTour();
+
+		// Accept the Cookie banner.
+		await this.cookieBannerComponent.acceptCookie();
 	}
 
 	/**
