5da02e6b2070860f79cc6ab0799f7e7c28deedf8
Gergely Csécsey <gergely.csecsey@automattic.com>
2023-08-25 15:21:24 +0100

    add price hook

--- client/my-sites/plugins/related-plugins/index.tsx
+++ client/my-sites/plugins/related-plugins/index.tsx
@@ -3,7 +3,10 @@ import { useTranslate } from 'i18n-calypso';
 import { useMemo } from 'react';
 import { RelatedPlugin } from 'calypso/data/marketplace/types';
 import { useGetRelatedPlugins } from 'calypso/data/marketplace/use-get-related-plugins';
+import { IntervalLength } from 'calypso/my-sites/marketplace/components/billing-interval-switcher/constants';
 import PluginIcon from 'calypso/my-sites/plugins/plugin-icon/plugin-icon';
+import { PluginPrice } from 'calypso/my-sites/plugins/plugin-price';
+import PreinstalledPremiumPluginPriceDisplay from 'calypso/my-sites/plugins/plugin-price/preinstalled-premium-plugin-price-display';
 import { useSelector } from 'calypso/state';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 import './style.scss';
@@ -44,27 +47,44 @@ function RelatedPlugins( { slug, size = 4, seeAllLink, options }: RelatedPluginP
 	);
 }
 
-function RelatedPluginCard( {
-	plugin: { icon, title, excerpt, slug },
-}: {
-	plugin: RelatedPlugin;
-} ): JSX.Element {
+function RelatedPluginCard( { plugin }: { plugin: RelatedPlugin } ): JSX.Element {
+	const translate = useTranslate();
 	const selectedSite = useSelector( getSelectedSite );
 
 	const pluginLink = useMemo( () => {
-		let url = '/plugins/' + slug;
+		let url = '/plugins/' + plugin.slug;
 		if ( selectedSite ) {
 			url += '/' + selectedSite.slug;
 		}
 		return url;
-	}, [ slug, selectedSite ] );
+	}, [ plugin.slug, selectedSite ] );
 
 	return (
 		<a className="related-plugins-item" href={ pluginLink }>
-			<PluginIcon image={ icon } className="related-plugins-item__icon" />
+			<PluginIcon image={ plugin.icon } className="related-plugins-item__icon" />
 			<div className="related-plugins-item__details">
-				<h3 className="related-plugins-item__title">{ title }</h3>
-				<div className="related-plugins-item__excerpt">{ excerpt }</div>
+				<h3 className="related-plugins-item__title">{ plugin.title }</h3>
+				<div className="related-plugins-item__excerpt">{ plugin.excerpt }</div>
+				<PluginPrice plugin={ plugin } billingPeriod={ IntervalLength.MONTHLY }>
+					{ ( { isFetching, price, period } ) => {
+						if ( isFetching ) {
+							return <div className="related-plugins-item__price-placeholder">...</div>;
+						}
+
+						if ( price ) {
+							return (
+								<PreinstalledPremiumPluginPriceDisplay
+									className="related-plugins-item__price"
+									period={ period }
+									pluginSlug={ plugin.slug }
+									price={ price }
+								/>
+							);
+						}
+
+						return <>{ translate( 'Free' ) }</>;
+					} }
+				</PluginPrice>
 			</div>
 		</a>
 	);
