00273932e93201bd03e63bea5fd3e9e4545121ab
arthur <arthur.chu@automattic.com>
2023-08-25 10:27:35 +0800

    Update copy

--- client/components/premium-global-styles-upgrade-modal/use-global-styles-upgrade-translations.tsx
+++ client/components/premium-global-styles-upgrade-modal/use-global-styles-upgrade-translations.tsx
@@ -1,7 +1,5 @@
 import { PLAN_PREMIUM, getPlan } from '@automattic/calypso-products';
 import { useTranslate } from 'i18n-calypso';
-import { useSelector } from 'calypso/state';
-import { getProductBySlug } from 'calypso/state/products-list/selectors';
 
 interface Props {
 	numOfSelectedGlobalStyles?: number;
@@ -9,8 +7,8 @@ interface Props {
 
 const useGlobalStylesUpgradeTranslations = ( { numOfSelectedGlobalStyles = 1 }: Props ) => {
 	const translate = useTranslate();
-	const planProduct = useSelector( ( state ) => getProductBySlug( state, PLAN_PREMIUM ) );
 	const plan = getPlan( PLAN_PREMIUM );
+	const planTitle = plan?.getTitle() ?? '';
 	const features = [
 		<strong>{ translate( 'Free domain for one year' ) }</strong>,
 		<strong>{ translate( 'Premium themes' ) }</strong>,
@@ -24,9 +22,12 @@ const useGlobalStylesUpgradeTranslations = ( { numOfSelectedGlobalStyles = 1 }: 
 		featuresTitle: translate( 'Included with your Premium plan' ),
 		features: features,
 		description: translate(
-			'You’ve selected a custom style that will only be visible to visitors after upgrading to the Premium plan or higher.',
-			'You’ve selected custom styles that will only be visible to visitors after upgrading to the Premium plan or higher.',
-			{ count: numOfSelectedGlobalStyles }
+			'You’ve selected a custom style that will only be visible to visitors after upgrading to the %(planTitle)s plan or higher.',
+			'You’ve selected custom styles that will only be visible to visitors after upgrading to the %(planTitle)s plan or higher.',
+			{
+				count: numOfSelectedGlobalStyles,
+				args: { planTitle },
+			}
 		),
 		promotion: translate(
 			'Upgrade now to unlock your custom style and get access to tons of other features. Or you can decide later and try it out first.',
@@ -35,11 +36,8 @@ const useGlobalStylesUpgradeTranslations = ( { numOfSelectedGlobalStyles = 1 }: 
 		),
 		cancel: translate( 'Decide later' ),
 		upgrade: translate( 'Upgrade plan' ),
-		upgradeWithPlan: translate( 'Get %(planTitle)s for %(displayPrice)s/month', {
-			args: {
-				planTitle: plan?.getTitle() ?? '',
-				displayPrice: planProduct?.cost_per_month_display ?? '',
-			},
+		upgradeWithPlan: translate( 'Get %(planTitle)s', {
+			args: { planTitle },
 		} ),
 	};
 };
