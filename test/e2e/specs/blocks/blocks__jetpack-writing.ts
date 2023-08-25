ed55094e8c0d40c9a98a1e5eb6b6b48bd9086197
Edwin Takahashi <egao@outlook.com>
2023-08-25 15:04:52 +0900

    Jetpack E2E: add flow for AI Assistant block. (#80899)
    * packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts
    packages/calypso-e2e/src/lib/blocks/block-flows/index.ts
    - add new block testing flow.
    
    test/e2e/specs/blocks/blocks__jetpack-earn.ts
    - add new test case.
    
    * packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts
    - add some guards
    
    * packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts
    - add some more guards
    
    * packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts
    - additional waits
    - fix JSDoc length.
    
    * packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts
    - refactor methods to work with both block-based and non-block-based themes.
    - add waits throughout the methods
    
    * Move tests to a new file `jetpack writing`
    
    * packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts
    - newline
    
    * packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts
    - refactor block flow to use already-existing methods.
    
    test/e2e/specs/blocks/blocks__jetpack-writing.ts
    - update spec
    
    packages/calypso-e2e/src/lib/pages/editor-page.ts
    packages/calypso-e2e/src/lib/components/editor-popover-menu-component.ts
    packages/calypso-e2e/src/lib/components/editor-block-toolbar-component.ts
    - expose method from the subcomponents into EditorPage.
    
    * packages/calypso-e2e/src/lib/blocks/block-flows/ai-assistant.ts
    - fix typo
    
    * Remove use of trial
    
    * packages/calypso-e2e/src/lib/components/editor-popover-menu-component.ts
    - remove narrowing of selector

---
+++ test/e2e/specs/blocks/blocks__jetpack-writing.ts
@@ -0,0 +1,17 @@
+/**
+ * @group gutenberg
+ * @group jetpack-wpcom-integration
+ */
+import { BlockFlow, AIAssistantFlow } from '@automattic/calypso-e2e';
+import { createBlockTests } from './shared/block-smoke-testing';
+
+const blockFlows: BlockFlow[] = [
+	new AIAssistantFlow( {
+		query: 'In two short sentences, tell me about Vancouver, Canada.',
+		tone: 'Passionate',
+		improve: 'Make shorter',
+		keywords: [ 'Vancouver, Canada' ],
+	} ),
+];
+
+createBlockTests( 'Blocks: Jetpack Writing', blockFlows );
