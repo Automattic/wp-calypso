4c675a05951ba05b3d4cb4de52aa9191feffb55f
Fabien MILLERAND <millerf@automattic.com>
2023-08-25 21:27:10 +0200

    Launchpad sidebar

--- client/my-sites/earn/memberships/constants.ts
+++ client/my-sites/earn/memberships/constants.ts
@@ -1,3 +1,4 @@
 export const ADD_NEW_PAYMENT_PLAN_HASH = '#add-new-payment-plan';
 export const ADD_NEWSLETTER_PAYMENT_PLAN_HASH = '#add-newsletter-payment-plan';
 export const LAUNCHPAD_HASH = '#launchpad';
+ 
