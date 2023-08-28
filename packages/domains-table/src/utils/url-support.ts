6b6f8d2693a37a06e4132d2fd8671032c327ea6f
vykes-mac <knrymcleish@gmail.com>
2023-08-28 13:48:05 -0500

    localize hardcoded url

--- packages/domains-table/src/utils/url-support.ts
+++ packages/domains-table/src/utils/url-support.ts
@@ -1,5 +1,6 @@
-// eslint-disable-next-line wpcalypso/i18n-unlocalized-url
-const root = 'https://wordpress.com/support';
+import { localizeUrl } from '@automattic/i18n-utils';
+
+const root = localizeUrl( 'https://wordpress.com/support' );
 export const SETTING_PRIMARY_DOMAIN = `${ root }/domains/set-a-primary-address/`;
 export const GDPR_POLICIES = `${ root }/your-site-and-the-gdpr/`;
 export const INCOMING_DOMAIN_TRANSFER_STATUSES_IN_PROGRESS = `${ root }/incoming-domain-transfer/status-and-failed-transfers/#pending`;
