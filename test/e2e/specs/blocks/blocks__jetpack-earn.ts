b8eeb4611255cf290fffb13d27483af2a264c074
Edwin Takahashi <egao@outlook.com>
2023-08-28 15:44:07 +0900

    test/e2e/specs/blocks/blocks__jetpack-earn.ts - add comments - restore original flows

--- test/e2e/specs/blocks/blocks__jetpack-earn.ts
+++ test/e2e/specs/blocks/blocks__jetpack-earn.ts
@@ -15,25 +15,25 @@ import {
 import { createBlockTests } from './shared/block-smoke-testing';
 
 const blockFlows: BlockFlow[] = [
-	// new PayWithPaypalBlockFlow( {
-	// 	name: 'Test Paypal Block',
-	// 	price: 900,
-	// 	email: 'test@wordpress.com',
-	// } ),
-	// new OpenTableFlow( {
-	// 	restaurant: 'Miku Restaurant - Vancouver',
-	// } ),
-	// new DonationsFormFlow(
-	// 	{
-	// 		frequency: 'Yearly',
-	// 		currency: 'CAD',
-	// 	},
-	// 	{
-	// 		frequency: 'Yearly',
-	// 		customAmount: 50,
-	// 		predefinedAmount: 5,
-	// 	}
-	// ),
+	new PayWithPaypalBlockFlow( {
+		name: 'Test Paypal Block',
+		price: 900,
+		email: 'test@wordpress.com',
+	} ),
+	new OpenTableFlow( {
+		restaurant: 'Miku Restaurant - Vancouver',
+	} ),
+	new DonationsFormFlow(
+		{
+			frequency: 'Yearly',
+			currency: 'CAD',
+		},
+		{
+			frequency: 'Yearly',
+			customAmount: 50,
+			predefinedAmount: 5,
+		}
+	),
 ];
 
 // We're just skipping the Payments Button test for now due to this bug:
@@ -44,7 +44,9 @@ const blockFlows: BlockFlow[] = [
 // 	blockFlows.push( new PaymentsBlockFlow( { buttonText: 'Donate to Me' } ) );
 // }
 
-//
+// The Ad block is only available on more premium plans that imply AT.
+// Furthermore, private sites are not eligible to monetize due to the site
+// being, well, private.
 if (
 	envVariables.JETPACK_TARGET === 'wpcom-deployment' &&
 	envVariables.TEST_ON_ATOMIC === true &&
