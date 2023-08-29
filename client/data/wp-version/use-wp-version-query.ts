36e373916801f9a570005f23695c1484e43b2f22
Roberto Aranda <roberto.aranda@automattic.com>
2023-08-29 10:11:45 +0200

    Add comments to useWPVersion hook

--- client/data/wp-version/use-wp-version-query.ts
+++ client/data/wp-version/use-wp-version-query.ts
@@ -5,6 +5,15 @@ import { MAX_AGE } from 'calypso/state/initial-state';
 type ResponseWPVersions = { offers: [ { current: string } ] };
 type DataWPVersions = string[];
 
+/**
+ * A custom hook that fetches the latest WordPress version and returns it as a query result.
+ *
+ * @param {Object} options - An object containing options for the query.
+ * @param {number} options.staleTime - The time in milliseconds that the query should be considered fresh.
+ *                                     It defaults to MAX_AGE, which is 1 week.
+ * @returns {Object} A query result object containing the latest WordPress versions
+ *                   in the form of array of versions, e.g. [6.3", "6.2.2", "6.1.3", "6.0.5", "5.9.7"]
+ */
 export const useWPVersion = ( {
 	staleTime = MAX_AGE,
 }: UseQueryOptions = {} ): UseQueryResult< DataWPVersions > => {
