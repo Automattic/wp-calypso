495801ab7ecc15e98d3b5fae29dcb9c0cd40f2a6
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2023-08-29 14:39:21 -0600

    Separate jetpack and wordpress 2 year plan upsell and clear up design

--- client/my-sites/checkout/composite-checkout/components/jetpack-checkout-sidebar-plan-upsell/index.tsx
+++ client/my-sites/checkout/composite-checkout/components/jetpack-checkout-sidebar-plan-upsell/index.tsx
@@ -1,10 +1,11 @@
-import { isPlan, isJetpackPlan } from '@automattic/calypso-products';
+import { isJetpackProduct, isJetpackPlan } from '@automattic/calypso-products';
 import { FormStatus, useFormStatus } from '@automattic/composite-checkout';
 import formatCurrency from '@automattic/format-currency';
 import { useShoppingCart } from '@automattic/shopping-cart';
 import { createElement, createInterpolateElement } from '@wordpress/element';
 import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
+import classNames from 'classnames';
 import debugFactory from 'debug';
 import PromoCard from 'calypso/components/promo-section/promo-card';
 import PromoCardCTA from 'calypso/components/promo-section/promo-card/cta';
@@ -12,35 +13,72 @@ import useCartKey from 'calypso/my-sites/checkout/use-cart-key';
 import { useDispatch } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { useGetProductVariants } from '../../hooks/product-variants';
-import {
-	getItemVariantCompareToPrice,
-	getItemVariantDiscountPercentage,
-} from '../item-variation-picker/util';
+import { getItemVariantDiscountPercentage } from '../item-variation-picker/util';
+import type { FC } from 'react';
 
 import './style.scss';
 
 const debug = debugFactory( 'calypso:checkout-sidebar-plan-upsell' );
 
-export function CheckoutSidebarPlanUpsell() {
+interface UpsellLineProps {
+	label: string;
+	value?: string;
+	boldLabel?: boolean;
+	boldValue?: boolean;
+	isTitle?: boolean;
+}
+
+const UpsellLine: FC< UpsellLineProps > = ( {
+	label,
+	value,
+	boldLabel = false,
+	boldValue = false,
+	isTitle = false,
+} ) => {
+	const labelMarkup = boldLabel ? <strong>{ label }</strong> : label;
+	const valueMarkup = boldValue ? <strong>{ value }</strong> : value;
+	return (
+		<>
+			<div
+				className={ classNames(
+					'checkout-sidebar-plan-upsell__plan-grid-cell',
+					isTitle ? 'section-title' : ''
+				) }
+			>
+				{ labelMarkup }
+			</div>
+			<div
+				className={ classNames(
+					'checkout-sidebar-plan-upsell__plan-grid-cell',
+					isTitle ? 'section-title' : ''
+				) }
+			>
+				{ valueMarkup }
+			</div>
+		</>
+	);
+};
+
+const JetpackCheckoutSidebarPlanUpsell: FC = () => {
 	const { formStatus } = useFormStatus();
-	const reduxDispatch = useDispatch();
 	const isFormLoading = FormStatus.READY !== formStatus;
+	const reduxDispatch = useDispatch();
 	const { __ } = useI18n();
 	const cartKey = useCartKey();
 	const { responseCart, replaceProductInCart } = useShoppingCart( cartKey );
 	const plan = responseCart.products.find(
-		( product ) => isPlan( product ) && ! isJetpackPlan( product )
+		( product ) => isJetpackProduct( product ) || isJetpackPlan( product )
 	);
+
 	const variants = useGetProductVariants( plan );
 
 	if ( ! plan ) {
-		debug( 'no plan found in cart' );
+		debug( 'no jetpack plan found in cart' );
 		return null;
 	}
 
-	const biennialVariant = variants?.find( ( product ) => product.termIntervalInMonths === 24 );
-	const triennialVariant = variants?.find( ( product ) => product.termIntervalInMonths === 36 );
 	const currentVariant = variants?.find( ( product ) => product.productId === plan.product_id );
+	const biennialVariant = variants?.find( ( product ) => product.termIntervalInMonths === 24 );
 
 	if ( ! biennialVariant ) {
 		debug( 'plan in cart has no biennial variant; variants are', variants );
@@ -57,12 +95,6 @@ export function CheckoutSidebarPlanUpsell() {
 		return null;
 	}
 
-	// If the current plan is a triennial plan, we don't want to show an upsell.
-	if ( triennialVariant?.productId === plan.product_id ) {
-		debug( 'plan is triennial. hide upsell.' );
-		return null;
-	}
-
 	const onUpgradeClick = () => {
 		if ( isFormLoading ) {
 			return;
@@ -73,7 +105,7 @@ export function CheckoutSidebarPlanUpsell() {
 		};
 		debug( 'switching from', plan.product_slug, 'to', newPlan.product_slug );
 		reduxDispatch(
-			recordTracksEvent( 'calypso_checkout_sidebar_upsell_click', {
+			recordTracksEvent( 'calypso_jetpack_checkout_sidebar_upsell_click', {
 				upsell_type: 'biennial-plan',
 				switching_from: plan.product_slug,
 				switching_to: newPlan.product_slug,
@@ -82,10 +114,6 @@ export function CheckoutSidebarPlanUpsell() {
 		replaceProductInCart( plan.uuid, newPlan );
 	};
 
-	const compareToPriceForVariantTerm = getItemVariantCompareToPrice(
-		biennialVariant,
-		currentVariant
-	);
 	const percentSavings = getItemVariantDiscountPercentage( biennialVariant, currentVariant );
 	if ( percentSavings === 0 ) {
 		debug( 'percent savings is too low', percentSavings );
@@ -97,6 +125,10 @@ export function CheckoutSidebarPlanUpsell() {
 		biennialVariant.introductoryTerm === 'year' &&
 		currentVariant.introductoryInterval === 1 &&
 		currentVariant.introductoryTerm === 'year';
+	const currencyConfig = {
+		stripZeros: true,
+		isSmallestUnit: true,
+	};
 
 	const cardTitle = createInterpolateElement(
 		sprintf(
@@ -108,48 +140,56 @@ export function CheckoutSidebarPlanUpsell() {
 	);
 
 	return (
-		<PromoCard title={ cardTitle } className="checkout-sidebar-plan-upsell">
+		<PromoCard title={ cardTitle } className="checkout-sidebar-plan-upsell jetpack">
 			<div className="checkout-sidebar-plan-upsell__plan-grid">
-				{ isComparisonWithIntroOffer && (
-					<>
-						<div className="checkout-sidebar-plan-upsell__plan-grid-cell"></div>
-						<div className="checkout-sidebar-plan-upsell__plan-grid-cell">
-							<strong>{ __( 'Two-year cost' ) }</strong>
-						</div>
-					</>
-				) }
-				<div className="checkout-sidebar-plan-upsell__plan-grid-cell">
-					{ currentVariant.variantLabel }
-				</div>
-				<div className="checkout-sidebar-plan-upsell__plan-grid-cell">
-					{ formatCurrency(
-						currentVariant.priceInteger +
-							( isComparisonWithIntroOffer ? currentVariant.priceBeforeDiscounts : 0 ),
+				<UpsellLine label={ __( 'Yearly plan' ) } boldLabel isTitle />
+
+				<UpsellLine
+					label={ __( 'Year 1' ) }
+					value={ formatCurrency(
+						isComparisonWithIntroOffer
+							? currentVariant.priceInteger
+							: currentVariant.priceBeforeDiscounts,
 						currentVariant.currency,
-						{
-							stripZeros: true,
-							isSmallestUnit: true,
-						}
+						currencyConfig
 					) }
-				</div>
-				<div className="checkout-sidebar-plan-upsell__plan-grid-cell">
-					{ biennialVariant.variantLabel }
-				</div>
-				<div className="checkout-sidebar-plan-upsell__plan-grid-cell">
-					{ compareToPriceForVariantTerm && (
-						<del className="checkout-sidebar-plan-upsell__do-not-pay">
-							{ formatCurrency( compareToPriceForVariantTerm, currentVariant.currency, {
-								stripZeros: true,
-								isSmallestUnit: true,
-							} ) }
-						</del>
+				/>
+
+				<UpsellLine
+					label={ __( 'Year 2' ) }
+					value={ formatCurrency(
+						currentVariant.priceBeforeDiscounts,
+						currentVariant.currency,
+						currencyConfig
 					) }
-					{ formatCurrency( biennialVariant.priceInteger, biennialVariant.currency, {
-						stripZeros: true,
-						isSmallestUnit: true,
-					} ) }
-				</div>
+				/>
+
+				<UpsellLine
+					label={ __( 'Total' ) }
+					value={ formatCurrency(
+						currentVariant.priceBeforeDiscounts +
+							( isComparisonWithIntroOffer
+								? currentVariant.priceInteger
+								: currentVariant.priceBeforeDiscounts ),
+						currentVariant.currency,
+						currencyConfig
+					) }
+					boldValue
+				/>
+
+				<UpsellLine label={ __( '2-year plan' ) } boldLabel isTitle />
+
+				<UpsellLine
+					label={ __( 'Years 1 and 2' ) }
+					value={ formatCurrency(
+						biennialVariant.priceInteger,
+						biennialVariant.currency,
+						currencyConfig
+					) }
+					boldValue
+				/>
 			</div>
+
 			<PromoCardCTA
 				cta={ {
 					disabled: isFormLoading,
@@ -160,4 +200,6 @@ export function CheckoutSidebarPlanUpsell() {
 			/>
 		</PromoCard>
 	);
-}
+};
+
+export default JetpackCheckoutSidebarPlanUpsell;
