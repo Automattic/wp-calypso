10465df137e0193ecfecc64acf65e2fcc9815af0
Anna McPhee <anna.mcphee@automattic.com>
2023-08-28 21:10:55 -0600

    add new notice to notice definitions

--- client/my-sites/stats/stats-notices/all-notice-definitions.ts
+++ client/my-sites/stats/stats-notices/all-notice-definitions.ts
@@ -1,5 +1,6 @@
 import config from '@automattic/calypso-config';
 import { NoticeIdType } from 'calypso/my-sites/stats/hooks/use-notice-visibility-query';
+import CommercialSiteUpgradeNotice from './commercial-site-upgrade-notice';
 import DoYouLoveJetpackStatsNotice from './do-you-love-jetpack-stats-notice';
 import FreePlanPurchaseSuccessJetpackStatsNotice from './free-plan-purchase-success-notice';
 import PaidPlanPurchaseSuccessJetpackStatsNotice from './paid-plan-purchase-success-notice';
@@ -31,6 +32,42 @@ const ALL_STATS_NOTICES: StatsNoticeType[] = [
 	{
 		component: DoYouLoveJetpackStatsNotice,
 		noticeId: 'do_you_love_jetpack_stats',
+		isVisibleFunc: ( {
+			isOdysseyStats,
+			isWpcom,
+			isVip,
+			isP2,
+			isOwnedByTeam51,
+			hasPaidStats,
+			isSiteJetpackNotAtomic,
+		}: StatsNoticeProps ) => {
+			// Gate notices for WPCOM sites behind a flag.
+			const showUpgradeNoticeForWpcomSites =
+				config.isEnabled( 'stats/paid-wpcom-stats' ) &&
+				isWpcom &&
+				! isVip &&
+				! isP2 &&
+				! isOwnedByTeam51;
+
+			// Show the notice if the site is Jetpack or it is Odyssey Stats.
+			const showUpgradeNoticeOnOdyssey = config.isEnabled( 'stats/paid-stats' ) && isOdysseyStats;
+
+			const showUpgradeNoticeForJetpackNotAtomic =
+				config.isEnabled( 'stats/paid-stats' ) && isSiteJetpackNotAtomic;
+
+			return !! (
+				( showUpgradeNoticeOnOdyssey ||
+					showUpgradeNoticeForJetpackNotAtomic ||
+					showUpgradeNoticeForWpcomSites ) &&
+				// Show the notice if the site has not purchased the paid stats product.
+				! hasPaidStats
+			);
+		},
+		disabled: false,
+	},
+	{
+		component: CommercialSiteUpgradeNotice,
+		noticeId: 'commercial_site_upgrade',
 		isVisibleFunc: ( {
 			isOdysseyStats,
 			isWpcom,
