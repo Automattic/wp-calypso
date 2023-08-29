700ae9702cada32c55fdb4fd0d0abfaea30f41b8
Roberto Aranda <roberto.aranda@automattic.com>
2023-08-29 10:27:35 +0200

    Add comments to usePluginIsMaintained hook

--- client/my-sites/plugins/use-plugin-is-maintained/index.js
+++ client/my-sites/plugins/use-plugin-is-maintained/index.js
@@ -3,6 +3,14 @@ import version_compare from 'calypso/lib/version-compare';
 
 const WP_VERSIONS_TO_CHECK = 3;
 
+/**
+ * Checks if a plugin is tested at least WP_VERSIONS_TO_CHECK versions below current WordPress version.
+ * If the plugin was tested in an older than WP_VERSIONS_TO_CHECK versions, it is considered not maintained.
+ *
+ * @param {string} testedVersion - The version of the plugin to check.
+ * @returns {boolean} True if the plugin was tested less than WP_VERSIONS_TO_CHECK WordPress versions ago,
+ *                    false otherwise.
+ */
 export default function usePluginIsMaintained( testedVersion ) {
 	const { data: wpVersions } = useWPVersion();
 	const wpVersionToCheck = wpVersions?.[ WP_VERSIONS_TO_CHECK ];
