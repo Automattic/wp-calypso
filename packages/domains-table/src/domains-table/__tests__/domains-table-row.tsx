144c1fbaae527fa2158cdc2ed34da8c0551de528
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2023-08-31 18:31:41 -0300

    Domain management: add connect to an existing site as a secondary action (#81243)
    * Remove connect site action from site cell
    
    * Add action menu
    
    * Unified tests
    
    * Add site actions test

--- packages/domains-table/src/domains-table/__tests__/domains-table-row.tsx
+++ packages/domains-table/src/domains-table/__tests__/domains-table-row.tsx
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import { screen, waitFor } from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import { renderWithProvider, testDomain, testPartialDomain } from '../../test-utils';
 import { DomainsTableRow } from '../domains-table-row';
@@ -219,4 +219,149 @@ test( 'transfer domains link to the transfer management interface', async () => 
 		'href',
 		'/domains/manage/example.com/transfer/in/example.com'
 	);
+} );
+
+test( 'when a site is associated with a domain, display its name', async () => {
+	const [ primaryPartial, primaryFull ] = testDomain( {
+		domain: 'primary-domain.blog',
+		blog_id: 123,
+		primary_domain: true,
+	} );
+
+	const fetchSiteDomains = jest.fn().mockResolvedValue( {
+		domains: [ primaryFull ],
+	} );
+
+	const fetchSite = jest.fn().mockResolvedValue( { ID: 123, name: 'Primary Domain Blog' } );
+
+	render(
+		<DomainsTableRow
+			domain={ primaryPartial }
+			fetchSiteDomains={ fetchSiteDomains }
+			fetchSite={ fetchSite }
+			isAllSitesView
+			isSelected={ false }
+			onSelect={ noop }
+		/>
+	);
+
+	await waitFor( () => expect( screen.queryByText( 'Primary Domain Blog' ) ).toBeInTheDocument() );
+} );
+
+describe( 'site linking ctas', () => {
+	beforeAll( () => {
+		global.ResizeObserver = require( 'resize-observer-polyfill' );
+	} );
+
+	test( 'when a site is not associated with a domain, display the site linking ctas', async () => {
+		const [ primaryPartial, primaryFull ] = testDomain( {
+			domain: 'primary-domain.blog',
+			blog_id: 123,
+			primary_domain: true,
+			current_user_can_create_site_from_domain_only: true,
+		} );
+
+		const fetchSiteDomains = jest.fn().mockResolvedValue( {
+			domains: [ primaryFull ],
+		} );
+
+		const fetchSite = jest.fn().mockResolvedValue( {
+			ID: 123,
+			URL: 'https://primarydomainblog.wordpress.com',
+			options: { is_redirect: false },
+		} );
+
+		render(
+			<DomainsTableRow
+				domain={ primaryPartial }
+				fetchSiteDomains={ fetchSiteDomains }
+				fetchSite={ fetchSite }
+				isAllSitesView
+				isSelected={ false }
+				onSelect={ noop }
+			/>
+		);
+
+		await waitFor( () => {
+			const createLink = screen.queryByText( 'Add site' );
+
+			expect( createLink ).toBeInTheDocument();
+			expect( createLink ).toHaveAttribute(
+				'href',
+				'/start/site-selected/?siteSlug=primarydomainblog.wordpress.com&siteId=123'
+			);
+		} );
+
+		const domainActionsButton = screen.getByLabelText( 'Domain actions' );
+
+		expect( domainActionsButton ).toBeInTheDocument();
+		fireEvent.click( domainActionsButton );
+
+		await waitFor( () => {
+			const connectAction = screen.getByText( 'Connect to an existing site' );
+
+			// The link itself is wrapped with a span element.
+			expect( connectAction.closest( '[role=menuitem]' ) ).toHaveAttribute(
+				'href',
+				'/domains/manage/all/primary-domain.blog/transfer/other-site/primarydomainblog.wordpress.com'
+			);
+		} );
+	} );
+} );
+
+describe( 'registered until cell', () => {
+	let dateTimeFormatSpy;
+
+	beforeAll( () => {
+		const OriginalTimeFormat = Intl.DateTimeFormat;
+		dateTimeFormatSpy = jest.spyOn( global.Intl, 'DateTimeFormat' );
+		dateTimeFormatSpy.mockImplementation(
+			( locale, options ) => new OriginalTimeFormat( locale, { ...options, timeZone: 'UTC' } )
+		);
+	} );
+
+	afterAll( () => {
+		dateTimeFormatSpy.mockClear();
+	} );
+
+	test( 'when a domain is registered, display its expiration date', () => {
+		const partialDomain = testPartialDomain( {
+			domain: 'example.com',
+			blog_id: 123,
+			wpcom_domain: false,
+			has_registration: true,
+			expiry: '2024-08-01T00:00:00+00:00',
+		} );
+
+		render(
+			<DomainsTableRow
+				domain={ partialDomain }
+				isAllSitesView
+				isSelected={ false }
+				onSelect={ noop }
+			/>
+		);
+
+		expect( screen.getByText( 'Aug 1, 2024' ) ).toBeInTheDocument();
+	} );
+
+	test( 'when its not a registered domain, do not display an expiration date', () => {
+		const partialDomain = testPartialDomain( {
+			domain: 'example.wordpress.com',
+			blog_id: 123,
+			wpcom_domain: true,
+			has_registration: false,
+		} );
+
+		render(
+			<DomainsTableRow
+				domain={ partialDomain }
+				isAllSitesView
+				isSelected={ false }
+				onSelect={ noop }
+			/>
+		);
+
+		expect( screen.getByText( '-' ) ).toBeInTheDocument();
+	} );
 } );
