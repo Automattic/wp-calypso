7e80bb13baa205a3631e3c8c19700c30e30176b0
Jasper Kang <jasper.kang@automattic.com>
2023-08-31 14:28:08 +1200

    Update client/my-sites/stats/stats-notices/commercial-site-upgrade-notice.tsx

--- client/my-sites/stats/stats-notices/commercial-site-upgrade-notice.tsx
+++ client/my-sites/stats/stats-notices/commercial-site-upgrade-notice.tsx
@@ -8,7 +8,7 @@ import { StatsNoticeProps } from './types';
 
 const getStatsPurchaseURL = ( siteId: number | null, isOdysseyStats: boolean ) => {
 	const from = isOdysseyStats ? 'jetpack' : 'calypso';
-	const purchasePath = `/stats/purchase/${ siteId }?flags=stats/paid-stats&from=${ from }-stats-upgrade-notice`;
+	const purchasePath = `/stats/purchase/${ siteId }?flags=stats/paid-stats&from=${ from }-stats-commercial-site-upgrade-notice`;
 	if ( ! isOdysseyStats ) {
 		return purchasePath;
 	}
