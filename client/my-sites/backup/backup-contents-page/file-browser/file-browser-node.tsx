37d3d4367188fc65c9afb2a9d869f643aa605564
John Webb <john.webb@automattic.com>
2023-08-31 18:46:08 -0400

    Added ID to the redux include/exclude lists

--- client/my-sites/backup/backup-contents-page/file-browser/file-browser-node.tsx
+++ client/my-sites/backup/backup-contents-page/file-browser/file-browser-node.tsx
@@ -84,9 +84,9 @@ const FileBrowserNode: FunctionComponent< FileBrowserNodeProps > = ( {
 					addChildNodes(
 						siteId,
 						path,
-						backupFiles
-							.filter( shouldAddChildNode )
-							.map( ( childItem: FileBrowserItem ) => childItem.name )
+						backupFiles.filter( shouldAddChildNode ).map( ( childItem: FileBrowserItem ) => {
+							return { id: childItem.id ?? '', path: childItem.name };
+						} )
 					)
 				);
 			}
