7266198d9bd7289241748f22bfe234b7204d6311
Gergely Csécsey <gergely.csecsey@automattic.com>
2023-09-01 13:13:48 +0100

    add eligibility modal to thank you theme section

--- client/my-sites/checkout/checkout-thank-you/marketplace/marketplace-thank-you-theme-section.tsx
+++ client/my-sites/checkout/checkout-thank-you/marketplace/marketplace-thank-you-theme-section.tsx
@@ -7,6 +7,7 @@ import { useCallback } from 'react';
 import QueryActiveTheme from 'calypso/components/data/query-active-theme';
 import { useActiveThemeQuery } from 'calypso/data/themes/use-active-theme-query';
 import ActivationModal from 'calypso/my-sites/themes/activation-modal';
+import EligibilityWarningModal from 'calypso/my-sites/themes/atomic-transfer-dialog';
 import { useSelector, useDispatch } from 'calypso/state';
 import getCustomizeUrl from 'calypso/state/selectors/get-customize-url';
 import getSiteUrl from 'calypso/state/sites/selectors/get-site-url';
@@ -147,6 +148,7 @@ export const ThankYouThemeSection = ( { theme }: { theme: any } ) => {
 		<ThemeSectionContainer>
 			<QueryActiveTheme siteId={ siteId } />
 			<ActivationModal source="details" />
+			<EligibilityWarningModal />
 			<ThemeSectionContent>
 				<ThemeNameSectionWrapper>
 					<ThemeSectionName>
