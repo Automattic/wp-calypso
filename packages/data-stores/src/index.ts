2c4dae633a304cf3b0d4b8d909a3f68f5481b1ef
Jeremy Yip <jeremy.yip@automattic.com>
2023-09-02 17:04:43 -0700

    Fix addon package types export

--- packages/data-stores/src/index.ts
+++ packages/data-stores/src/index.ts
@@ -13,6 +13,7 @@ import * as User from './user';
 import * as WpcomPlansUI from './wpcom-plans-ui';
 export { useHappinessEngineersQuery } from './queries/use-happiness-engineers-query';
 export { useSiteIntent } from './queries/use-site-intent';
+export * from './add-ons/types';
 export * from './starter-designs-queries';
 export * from './site/types';
 export * from './templates';
