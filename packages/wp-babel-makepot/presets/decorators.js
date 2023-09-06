0919241316cfcfd6a19e28ee6d4c4d0811941ddf
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2023-09-06 17:28:08 +0300

    Use legacy version for decorators

--- packages/wp-babel-makepot/presets/decorators.js
+++ packages/wp-babel-makepot/presets/decorators.js
@@ -3,7 +3,7 @@ const defaultPreset = require( './default' );
 module.exports = {
 	...defaultPreset,
 	plugins: [
-		[ '@babel/plugin-proposal-decorators', { version: '2023-05' } ],
+		[ '@babel/plugin-proposal-decorators', { version: 'legacy' } ],
 		...defaultPreset.plugins,
 	],
 };
