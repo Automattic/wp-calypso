38f96fe7f52eb58a23d374e8a9e3c5e11c045bd0
DustyReagan <dusty@dustyreagan.com>
2023-09-06 18:53:06 -0500

    Fix eslint error for translation

--- client/my-sites/domains/domain-management/settings/index.tsx
+++ client/my-sites/domains/domain-management/settings/index.tsx
@@ -399,18 +399,20 @@ const Settings = ( {
 			return null;
 		}
 
-		let domainForwardingTitle = 'Domain Forwarding';
+		let translatedTitle;
 		if (
 			englishLocales.includes( getLocaleSlug() || '' ) ||
 			i18n.hasTranslation( 'Domain forwarding' )
 		) {
-			domainForwardingTitle = 'Domain forwarding';
+			translatedTitle = translate( 'Domain forwarding', { textOnly: true } );
+		} else {
+			translatedTitle = translate( 'Domain Forwarding', { textOnly: true } );
 		}
 
 		return (
 			<Accordion
 				className="domain-forwarding-card__accordion"
-				title={ translate( domainForwardingTitle, { textOnly: true } ) }
+				title={ translatedTitle }
 				subtitle={ translate( 'Forward your domain to another' ) }
 			>
 				<DomainForwardingCard domain={ domain } />
