d7bc7caa68c5b3f2634578cd30cbb5fead87ca71
Dan Speckhard Pasque <dan.speckhard.pasque@automattic.com>
2023-09-06 13:16:22 -0500

    Add forms AI e2e block smoke test (#81298)

--- packages/calypso-e2e/src/lib/blocks/block-flows/index.ts
+++ packages/calypso-e2e/src/lib/blocks/block-flows/index.ts
@@ -24,6 +24,7 @@ export * from './all-form-fields';
 export * from './form-patterns';
 export * from './ad';
 export * from './paywall';
+export * from './form-ai';
 
 /* Types */
 export * from './types';
