501c2a37221fb935e87c788ced5037f754d74300
Edwin Takahashi <egao@outlook.com>
2023-09-07 17:28:59 +0900

    packages/calypso-e2e/src/lib/pages/subscribers-page.ts - remove unnecessary code.
    packages/calypso-e2e/src/jest-playwright-config/environment.ts
    - remove route abort, which is implemented elsewhere.

--- packages/calypso-e2e/src/lib/pages/subscribers-page.ts
+++ packages/calypso-e2e/src/lib/pages/subscribers-page.ts
@@ -1,8 +1,6 @@
 import { Locator, Page } from 'playwright';
 import { getCalypsoURL } from '../../data-helper';
 
-type NewSubscribersList = readonly [ string, string?, string? ];
-
 /**
  * Represents the Users > Subscribers page.
  */
@@ -66,35 +64,5 @@ export class SubscribersPage {
 			.getByRole( 'row' )
 			.filter( { hasText: identifier } )
 			.waitFor( { state: 'detached' } );
-	}
-
-	/**
-	 * Adds subscribers from the Subscribers listing page.
-	 *
-	 * @param {NewSubscribersList} subscribers List of emails to add, up to 3.
-	 */
-	async addSubscribers( subscribers: NewSubscribersList ) {
-		// Click on the 'Add Subscriber' primary button.
-		await this.anchor.getByRole( 'button', { name: 'Add subscribers' } ).click();
-
-		// Wait for the dialog box.
-		const dialog = this.page.getByRole( 'dialog', { name: /Add subscribers/ } );
-		await dialog.waitFor();
-
-		const inputLocators = await dialog.getByRole( 'textbox' ).all();
-
-		// Map each input box (up to 3) to the list of emails to add.
-		// Note, the resulting mapping array will be the same length as the
-		// length of `subscribers` input.
-		const mapping: [ string, Locator ][] = subscribers.map( function ( subscriber, index ) {
-			return [ subscriber as string, inputLocators[ index ] ];
-		} );
-
-		// Fill in email detail for each input.
-		for ( const [ subscriber, locator ] of mapping ) {
-			await locator.fill( subscriber );
-		}
-
-		await dialog.getByRole( 'button', { name: 'Add subscribers' } ).click();
 	}
 }
