d439983145f8cf28b68816ba740db0f211e80696
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-09-11 19:03:58 +0800

    Add tracking events when loading Create site page.

--- client/jetpack-cloud/sections/partner-portal/primary/wpcom-atomic-hosting/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/primary/wpcom-atomic-hosting/index.tsx
@@ -1,6 +1,9 @@
 import { Card } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
+import { useEffect } from 'react';
 import CardHeading from 'calypso/components/card-heading';
+import { useDispatch } from 'calypso/state';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import Layout from '../../layout';
 import LayoutHeader from '../../layout/header';
 import CardContent from './card-content';
@@ -9,12 +12,18 @@ import './style.scss';
 
 export default function WPCOMAtomicHosting() {
 	const translate = useTranslate();
+	const dispatch = useDispatch();
 	const title = translate( 'Create a new WordPress.com site' );
 
 	const plansToBeDisplayed = [
 		'JETPACK_HOSTING_WPCOM_BUSINESS',
 		'JETPACK_HOSTING_WPCOM_ECOMMERCE',
 	]; // Get the plans from the API
+
+	useEffect( () => {
+		// Track page view
+		dispatch( recordTracksEvent( 'calypso_partner_portal_wpcom_atomic_hosting_page_view' ) );
+	}, [ dispatch ] );
 
 	return (
 		<Layout title={ title } wide>
