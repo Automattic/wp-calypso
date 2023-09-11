8de09b49305e7977c179bf3995ab2824271cbcb2
Dognose <dognose24@gmail.com>
2023-09-11 12:02:05 +0800

    Check paid plans only for WPCOM sites

--- client/state/sites/selectors/has-site-product-jetpack-stats.ts
+++ client/state/sites/selectors/has-site-product-jetpack-stats.ts
@@ -2,6 +2,7 @@ import { camelOrSnakeSlug } from '@automattic/calypso-products';
 import { productHasStats } from 'calypso/blocks/jetpack-benefits/feature-checks';
 import { getSitePurchases } from 'calypso/state/purchases/selectors';
 import isSiteOnPaidPlan from 'calypso/state/selectors/is-site-on-paid-plan';
+import isSiteWpcom from 'calypso/state/selectors/is-site-wpcom';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { AppState } from 'calypso/types';
 import getSitePlan from './get-site-plan';
@@ -12,18 +13,22 @@ const hasSiteProductJetpackStats = (
 	onlyPaid = false,
 	siteId = getSelectedSiteId( state )
 ): boolean => {
+	const isWpcomSite = !! isSiteWpcom( state, siteId );
+
 	const siteHasStatsProduct = getSitePurchases( state, siteId )?.some(
 		( product ) =>
 			productHasStats( camelOrSnakeSlug( product ), onlyPaid ) && product.expiryStatus !== 'expired'
 	);
+
 	const sitePlan = getSitePlan( state, siteId );
 	const siteHasStatsPlan = productHasStats( sitePlan?.product_slug as string, onlyPaid );
 
-	// Check whether sites have paid plans of Jetpack or WPCOM.
-	// TODO: Determine the proper way to check WPCOM plans for supporting Stats.
+	// Check whether sites have paid plans of WPCOM.
 	const siteHasPaidPlan = isSiteOnPaidPlan( state, siteId || 0 );
+	// TODO: Determine the proper way to check WPCOM plans for supporting Stats.
+	const wpcomSiteHasPaidPlan = isWpcomSite && siteHasPaidPlan;
 
-	return siteHasStatsPlan || !! siteHasStatsProduct || siteHasPaidPlan;
+	return siteHasStatsPlan || !! siteHasStatsProduct || wpcomSiteHasPaidPlan;
 };
 
 export default hasSiteProductJetpackStats;
