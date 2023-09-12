3abfe3ba5f7fca72d1032842c9aac0a229a199b4
Edwin Takahashi <egao@outlook.com>
2023-09-12 15:43:16 +0900

    test/e2e/specs/blocks/blocks__jetpack-other.ts - add exception to the Mpa block flow

--- test/e2e/specs/blocks/blocks__jetpack-other.ts
+++ test/e2e/specs/blocks/blocks__jetpack-other.ts
@@ -2,13 +2,24 @@
  * @group gutenberg
  * @group jetpack-wpcom-integration
  */
-import { BlockFlow, StarRatingBlock, MapFlow, GifFlow } from '@automattic/calypso-e2e';
+import {
+	BlockFlow,
+	StarRatingBlock,
+	MapFlow,
+	GifFlow,
+	envVariables,
+} from '@automattic/calypso-e2e';
 import { createBlockTests } from './shared/block-smoke-testing';
 
 const blockFlows: BlockFlow[] = [
 	new StarRatingBlock( { rating: 3.5 } ),
-	new MapFlow( { address: '1455 Quebec St, Vancouver, BC V6A 3Z7' } ),
 	new GifFlow( { query: 'https://giphy.com/embed/MDJ9IbxxvDUQM' } ),
 ];
+
+// Private sites change behaivor of the Map block.
+// @see: https://github.com/Automattic/jetpack/issues/32991
+if ( envVariables.ATOMIC_VARIATION !== 'private' ) {
+	blockFlows.push( new MapFlow( { address: '1455 Quebec St, Vancouver, BC V6A 3Z7' } ) );
+}
 
 createBlockTests( 'Blocks: Other Jetpack Blocks', blockFlows );
