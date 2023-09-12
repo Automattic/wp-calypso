c44d1dd640a6383f3391551252ce161da70aaa74
arthur <arthur.chu@automattic.com>
2023-09-12 15:17:53 +0800

    Centralize steps

---
+++ client/landing/stepper/declarative-flow/internals/steps.tsx
@@ -0,0 +1,146 @@
+const STEPS = {
+	BLOGGER_STARTING_POINT: {
+		slug: 'bloggerStartingPoint',
+		asyncComponent: () => import( './steps-repository/blogger-starting-point' ),
+	},
+
+	BUSINESS_INFO: {
+		slug: 'businessInfo',
+		asyncComponent: () => import( './steps-repository/business-info' ),
+	},
+
+	COURSES: { slug: 'courses', asyncComponent: () => import( './steps-repository/courses' ) },
+
+	DESIGN_SETUP: {
+		slug: 'designSetup',
+		asyncComponent: () => import( './steps-repository/design-setup' ),
+	},
+
+	DIFM_STARTING_POINT: {
+		slug: 'difmStartingPoint',
+		asyncComponent: () => import( './steps-repository/difm-starting-point' ),
+	},
+
+	EDIT_EMAIL: {
+		slug: 'editEmail',
+		asyncComponent: () => import( './steps-repository/edit-email' ),
+	},
+
+	ERROR: { slug: 'error', asyncComponent: () => import( './steps-repository/error-step' ) },
+
+	GOALS: { slug: 'goals', asyncComponent: () => import( './steps-repository/goals' ) },
+
+	IMPORT: { slug: 'import', asyncComponent: () => import( './steps-repository/import' ) },
+
+	IMPORT_LIGHT: {
+		slug: 'importLight',
+		asyncComponent: () => import( './steps-repository/import-light' ),
+	},
+
+	IMPORT_LIST: {
+		slug: 'importList',
+		asyncComponent: () => import( './steps-repository/import-list' ),
+	},
+
+	IMPORT_READY: {
+		slug: 'importReady',
+		asyncComponent: () => import( './steps-repository/import-ready' ),
+	},
+
+	IMPORT_READY_NOT: {
+		slug: 'importReadyNot',
+		asyncComponent: () => import( './steps-repository/import-ready-not' ),
+	},
+
+	IMPORT_READY_PREVIEW: {
+		slug: 'importReadyPreview',
+		asyncComponent: () => import( './steps-repository/import-ready-preview' ),
+	},
+
+	IMPORT_READY_WPCOM: {
+		slug: 'importReadyWpcom',
+		asyncComponent: () => import( './steps-repository/import-ready-wpcom' ),
+	},
+
+	IMPORTER_BLOGGER: {
+		slug: 'importerBlogger',
+		asyncComponent: () => import( './steps-repository/importer-blogger' ),
+	},
+
+	IMPORTER_MEDIUM: {
+		slug: 'importerMedium',
+		asyncComponent: () => import( './steps-repository/importer-medium' ),
+	},
+
+	IMPORTER_SQUARESPACE: {
+		slug: 'importerSquarespace',
+		asyncComponent: () => import( './steps-repository/importer-squarespace' ),
+	},
+
+	IMPORTER_WIX: {
+		slug: 'importerWix',
+		asyncComponent: () => import( './steps-repository/importer-wix' ),
+	},
+
+	IMPORTER_WORDPRESS: {
+		slug: 'importerWordpress',
+		asyncComponent: () => import( './steps-repository/importer-wordpress' ),
+	},
+
+	INTENT: {
+		slug: 'intent',
+		asyncComponent: () => import( './steps-repository/intent-step' ),
+	},
+
+	MIGRATION_TRAIL: {
+		slug: 'migrationTrial',
+		asyncComponent: () => import( './steps-repository/migration-trial' ),
+	},
+
+	OPTIONS: {
+		slug: 'options',
+		asyncComponent: () => import( './steps-repository/site-options' ),
+	},
+
+	PATTERN_ASSEMBLER: {
+		slug: 'patternAssembler',
+		asyncComponent: () => import( './steps-repository/pattern-assembler' ),
+	},
+
+	PROCESSING: {
+		slug: 'processing',
+		asyncComponent: () => import( './steps-repository/processing-step' ),
+	},
+
+	STORE_ADDRESS: {
+		slug: 'storeAddress',
+		asyncComponent: () => import( './steps-repository/store-address' ),
+	},
+
+	VERIFY_EMAIL: {
+		slug: 'verifyEmail',
+		asyncComponent: () => import( './steps-repository/import-verify-email' ),
+	},
+
+	WOO_CONFIRM: {
+		slug: 'wooConfirm',
+		asyncComponent: () => import( './steps-repository/woo-confirm' ),
+	},
+
+	WOO_INSTALL_PLUGINS: {
+		slug: 'wooInstallPlugins',
+		asyncComponent: () => import( './steps-repository/woo-install-plugins' ),
+	},
+
+	WOO_TRANSFER: {
+		slug: 'wooTransfer',
+		asyncComponent: () => import( './steps-repository/woo-transfer' ),
+	},
+
+	WOO_VERIFY_EMAIL: {
+		slug: 'wooVerifyEmail',
+		asyncComponent: () => import( './steps-repository/woo-verify-email' ),
+	},
+};
+
+export default STEPS;
