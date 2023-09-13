233218b635a4198b2dd4ad0b5e3f03453734656b
mpkelly <kikemelly@gmail.com>
2023-09-13 10:28:11 +0700

    merge wth trunk

--- packages/domains-table/src/domains-table-filters/index.tsx
+++ packages/domains-table/src/domains-table-filters/index.tsx
@@ -1,5 +1,9 @@
+import { Button } from '@automattic/components';
 import SearchControl, { SearchIcon } from '@automattic/search';
+import { isMobile } from '@automattic/viewport';
 import { useI18n } from '@wordpress/react-i18n';
+import { useState } from 'react';
+
 import './style.scss';
 
 export interface DomainsTableFilter {
@@ -13,18 +17,41 @@ interface DomainsTableFiltersProps {
 
 export const DomainsTableFilters = ( { onSearch, filter }: DomainsTableFiltersProps ) => {
 	const { __ } = useI18n();
+	const [ search, setSearch ] = useState( filter.query );
+
+	const isMobileDevice = isMobile();
+
+	const handleSearchChange = ( query: string ) => {
+		if ( query === '' ) {
+			setSearch( '' );
+			onSearch( '' );
+		} else if ( isMobileDevice ) {
+			setSearch( query );
+		} else {
+			onSearch( query );
+		}
+	};
+
+	const handleSearch = () => {
+		onSearch( search );
+	};
 
 	return (
 		<div className="domains-table-filter">
 			<SearchControl
 				searchIcon={ <SearchIcon /> }
 				className="domains-table-filter__search"
-				onSearch={ ( query ) => onSearch( query.trim() ) }
+				onSearch={ handleSearchChange }
 				defaultValue={ filter.query }
 				isReskinned
 				placeholder={ __( 'Search by domain…' ) }
 				disableAutocorrect={ true }
 			/>
+			{ isMobileDevice && (
+				<Button primary={ Boolean( search ) } onClick={ handleSearch }>
+					{ __( 'Filter' ) }
+				</Button>
+			) }
 		</div>
 	);
 };
--- packages/domains-table/src/domains-table/domains-table-mobile-cards.tsx
+++ packages/domains-table/src/domains-table/domains-table-mobile-cards.tsx
@@ -1,10 +1,32 @@
+import { CheckboxControl } from '@wordpress/components';
+import { useI18n } from '@wordpress/react-i18n';
 import { useDomainsTable } from './domains-table';
 import { DomainsTableMobileCard } from './domains-table-mobile-card';
 
 export const DomainsTableMobileCards = () => {
-	const { filteredData } = useDomainsTable();
+	const { __ } = useI18n();
 
-	return filteredData.map( ( domain ) => (
-		<DomainsTableMobileCard key={ domain.domain } domain={ domain } />
-	) );
+	const { filteredData, canSelectAnyDomains, changeBulkSelection, getBulkSelectionStatus } =
+		useDomainsTable();
+	const bulkSelectionStatus = getBulkSelectionStatus();
+
+	return (
+		<div className="domains-table-mobile-cards">
+			<div className="domains-table__bulk-action-container">
+				{ canSelectAnyDomains && (
+					<CheckboxControl
+						data-testid="domains-select-all-checkbox"
+						__nextHasNoMarginBottom
+						onChange={ changeBulkSelection }
+						indeterminate={ bulkSelectionStatus === 'some-domains' }
+						checked={ bulkSelectionStatus === 'all-domains' }
+						aria-label={ __( 'Select all tick boxes for domains in table', __i18n_text_domain__ ) }
+					/>
+				) }
+			</div>
+			{ filteredData.map( ( domain ) => (
+				<DomainsTableMobileCard key={ domain.domain } domain={ domain } />
+			) ) }
+		</div>
+	);
 };
