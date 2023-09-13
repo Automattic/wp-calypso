8225a5731e4bdddcb94ea3fa2bdf61460ac8f022
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-09-13 11:44:47 +0530

    Social Previews | Hide Mastodon post preview when no connection

--- packages/social-previews/src/mastodon-preview/previews.tsx
+++ packages/social-previews/src/mastodon-preview/previews.tsx
@@ -6,48 +6,59 @@ import { MastodonLinkPreview } from './link-preview';
 import { MastodonPostPreview } from './post-preview';
 import { MastodonPreviewProps } from './types';
 
-export type MastodonPreviewsProps = MastodonPreviewProps & SocialPreviewsBaseProps;
+export type MastodonPreviewsProps = MastodonPreviewProps &
+	SocialPreviewsBaseProps & {
+		isSocialPost: boolean;
+	};
 
-export const MastodonPreviews: React.FC< MastodonPreviewsProps > = ( props ) => {
-	const { headingLevel, isSocialPost } = props;
-
+export const MastodonPreviews: React.FC< MastodonPreviewsProps > = ( {
+	headingLevel,
+	isSocialPost,
+	hidePostPreview,
+	hideLinkPreview,
+	...props
+} ) => {
 	return (
 		<div className="social-preview mastodon-preview">
-			<section className="social-preview__section mastodon-preview__section">
-				<SectionHeading level={ headingLevel }>
-					{
-						// translators: refers to a social post on Mastodon
-						__( 'Your post', 'social-previews' )
-					}
-				</SectionHeading>
-				<p className="social-preview__section-desc">
-					{ __( 'This is what your social post will look like on Mastodon:', 'social-previews' ) }
-				</p>
-				{ isSocialPost ? (
-					<MastodonPostPreview { ...props } />
-				) : (
-					<MastodonLinkPreview { ...props } />
-				) }
-			</section>
-			<section className="social-preview__section mastodon-preview__section">
-				<SectionHeading level={ headingLevel }>
-					{
-						// translators: refers to a link to a Mastodon post
-						__( 'Link preview', 'social-previews' )
-					}
-				</SectionHeading>
-				<p className="social-preview__section-desc">
-					{ __(
-						'This is what it will look like when someone shares the link to your WordPress post on Mastodon.',
-						'social-previews'
+			{ ! hidePostPreview && (
+				<section className="social-preview__section mastodon-preview__section">
+					<SectionHeading level={ headingLevel }>
+						{
+							// translators: refers to a social post on Mastodon
+							__( 'Your post', 'social-previews' )
+						}
+					</SectionHeading>
+					<p className="social-preview__section-desc">
+						{ __( 'This is what your social post will look like on Mastodon:', 'social-previews' ) }
+					</p>
+					{ isSocialPost ? (
+						<MastodonPostPreview { ...props } />
+					) : (
+						<MastodonLinkPreview { ...props } />
 					) }
-					&nbsp;
-					<ExternalLink href="https://jetpack.com/support/jetpack-social-image-generator">
-						{ __( 'Learn more about links', 'social-previews' ) }
-					</ExternalLink>
-				</p>
-				<MastodonLinkPreview { ...props } />
-			</section>
+				</section>
+			) }
+			{ ! hideLinkPreview && (
+				<section className="social-preview__section mastodon-preview__section">
+					<SectionHeading level={ headingLevel }>
+						{
+							// translators: refers to a link to a Mastodon post
+							__( 'Link preview', 'social-previews' )
+						}
+					</SectionHeading>
+					<p className="social-preview__section-desc">
+						{ __(
+							'This is what it will look like when someone shares the link to your WordPress post on Mastodon.',
+							'social-previews'
+						) }
+						&nbsp;
+						<ExternalLink href="https://jetpack.com/support/jetpack-social-image-generator">
+							{ __( 'Learn more about links', 'social-previews' ) }
+						</ExternalLink>
+					</p>
+					<MastodonLinkPreview { ...props } user={ undefined } />
+				</section>
+			) }
 		</div>
 	);
 };
