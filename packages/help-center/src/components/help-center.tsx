a15f5a150b4f2a3eba035ab05ad8efc8e192c597
Chris Diehl <jjchrisdiehl@gmail.com>
2023-09-13 15:33:51 -0700

    Fix chat trigger issues

--- packages/help-center/src/components/help-center.tsx
+++ packages/help-center/src/components/help-center.tsx
@@ -2,11 +2,12 @@
 /**
  * External Dependencies
  */
+import { isMobile } from '@automattic/viewport';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { createPortal, useEffect, useRef } from '@wordpress/element';
 import { useSelector } from 'react-redux';
 import getPrimarySiteId from 'calypso/state/selectors/get-primary-site-id';
-import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { getSelectedSiteId, getSectionName } from 'calypso/state/ui/selectors';
 /**
  * Internal Dependencies
  */
@@ -27,6 +28,8 @@ function useMessagingBindings( hasActiveChats: boolean, isMessagingScriptLoaded:
 		};
 	}, [] );
 
+	const section = useSelector( ( state ) => getSectionName( state ) );
+
 	useEffect( () => {
 		if ( typeof window.zE !== 'function' || ! isMessagingScriptLoaded ) {
 			return;
@@ -37,13 +40,17 @@ function useMessagingBindings( hasActiveChats: boolean, isMessagingScriptLoaded:
 		} );
 		window.zE( 'messenger:on', 'close', function () {
 			setShowMessagingWidget( false );
+
+			if ( isMobile() && section === 'checkout' ) {
+				setShowMessagingLauncher( false );
+			}
 		} );
 		window.zE( 'messenger:on', 'unreadMessages', function ( count ) {
 			if ( Number( count ) > 0 ) {
 				setShowMessagingLauncher( true );
 			}
 		} );
-	}, [ isMessagingScriptLoaded, setShowMessagingLauncher, setShowMessagingWidget ] );
+	}, [ isMessagingScriptLoaded, setShowMessagingLauncher, setShowMessagingWidget, section ] );
 
 	useEffect( () => {
 		if ( typeof window.zE !== 'function' || ! isMessagingScriptLoaded ) {
@@ -103,10 +110,14 @@ const HelpCenter: React.FC< Container > = ( { handleClose, hidden } ) => {
 
 	useStillNeedHelpURL();
 
-	const { hasActiveChats, isEligibleForChat } = useChatStatus( 'wpcom_messaging', false );
+	const { hasActiveChats, isEligibleForChat, isPresalesChatOpen } = useChatStatus(
+		'wpcom_messaging',
+		false
+	);
+
 	const { isMessagingScriptLoaded } = useZendeskMessaging(
 		'zendesk_support_chat_key',
-		isHelpCenterShown || isEligibleForChat || hasActiveChats,
+		( isHelpCenterShown && isEligibleForChat ) || isPresalesChatOpen || hasActiveChats,
 		isEligibleForChat && hasActiveChats
 	);
 
