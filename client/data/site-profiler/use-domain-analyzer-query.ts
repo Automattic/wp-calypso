9fd881a7a856f894ccbddacd6e5e7c8a8f2584e9
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-09-14 15:10:48 +0200

    Create DomainAnalyzerQueryResponse type

--- client/data/site-profiler/use-domain-analyzer-query.ts
+++ client/data/site-profiler/use-domain-analyzer-query.ts
@@ -1,5 +1,5 @@
-import { SourceSiteMigrationDetails } from '@automattic/data-stores/src/site';
 import { useQuery } from '@tanstack/react-query';
+import { DomainAnalyzerQueryResponse } from 'calypso/data/site-profiler/types';
 import wp from 'calypso/lib/wp';
 
 export interface MigrationStatusError {
@@ -10,7 +10,7 @@ export interface MigrationStatusError {
 export const useDomainAnalyzerQuery = ( domain: string ) => {
 	return useQuery( {
 		queryKey: [ 'domain-', domain ],
-		queryFn: (): Promise< SourceSiteMigrationDetails > =>
+		queryFn: (): Promise< DomainAnalyzerQueryResponse > =>
 			wp.req.get( {
 				path: '/site-profiler/' + encodeURIComponent( domain ),
 				apiNamespace: 'wpcom/v2',
