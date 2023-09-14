d431e4a99294c385d50e6f0fd41573f5b437cc9e
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-09-14 14:04:06 +0200

    Introduce site-profiler feature flag

--- client/site-profiler/controller.tsx
+++ client/site-profiler/controller.tsx
@@ -1,7 +1,13 @@
+import config from '@automattic/calypso-config';
+import page from 'page';
 import Main from 'calypso/components/main';
 import SiteProfiler from 'calypso/site-profiler/components/site-profiler';
 
 export function siteProfilerContext( context: PageJS.Context, next: () => void ): void {
+	if ( ! config.isEnabled( 'site-profiler' ) ) {
+		page.redirect( '/' );
+	}
+
 	context.primary = (
 		<Main wideLayout>
 			<SiteProfiler />
