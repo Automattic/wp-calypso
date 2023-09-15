6b42b21c62b45afb708502df6d84581e937245cc
vykes-mac <knrymcleish@gmail.com>
2023-09-14 21:43:43 -0500

    refactor to make translators comment clear

--- packages/domains-table/src/domains-table/domains-table-expires-renew-cell.tsx
+++ packages/domains-table/src/domains-table/domains-table-expires-renew-cell.tsx
@@ -1,12 +1,12 @@
 import { Gridicon } from '@automattic/components';
 import { PartialDomainData } from '@automattic/data-stores';
 import { useLocale } from '@automattic/i18n-utils';
+import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import moment from 'moment';
 
 interface DomainsTableExpiresRewnewsOnCellProps {
 	domain: PartialDomainData;
-	isMobile?: boolean;
 }
 
 export const DomainsTableExpiresRewnewsOnCell = ( {
@@ -15,16 +15,31 @@ export const DomainsTableExpiresRewnewsOnCell = ( {
 	const localeSlug = useLocale();
 	const isExpired = domain.expiry && moment( domain.expiry ).utc().isBefore( moment().utc() );
 	const { __ } = useI18n();
-	const notice = isExpired ? __( 'Expired' ) : __( 'Renews' );
+
+	const expiryDate = domain.has_registration
+		? new Intl.DateTimeFormat( localeSlug, { dateStyle: 'medium' } ).format(
+				new Date( domain.expiry )
+		  )
+		: null;
+
+	const notice = isExpired
+		? sprintf(
+				/* translators: %s - The date on which the domain was expired */
+				__( 'Expired %s' ),
+				expiryDate
+		  )
+		: sprintf(
+				/* translators: %s - The future date on which domain renews */
+				__( 'Renews %s' ),
+				expiryDate
+		  );
 
 	return (
 		<div className="domains-table-row__renews-on-cell">
-			{ domain.has_registration ? (
+			{ expiryDate ? (
 				<>
 					<Gridicon icon={ isExpired ? 'notice-outline' : 'reblog' } size={ 18 } />
-					{ `${ notice } ${ new Intl.DateTimeFormat( localeSlug, {
-						dateStyle: 'medium',
-					} ).format( new Date( domain.expiry ) ) }` }
+					{ notice }
 				</>
 			) : (
 				'-'
