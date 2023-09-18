a41252a468de8a6cb19c69d7d3e1895aef6694e4
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-09-18 12:58:48 +0200

    Integrate useHostingProviderQuery and show data inside the hosting information block

--- client/site-profiler/components/hosting-information/index.tsx
+++ client/site-profiler/components/hosting-information/index.tsx
@@ -1,7 +1,37 @@
-export default function HostingInformation() {
+import { DNS, HostingProvider } from 'calypso/data/site-profiler/types';
+
+interface Props {
+	dns: DNS[];
+	hostingProvider?: HostingProvider;
+}
+
+export default function HostingInformation( props: Props ) {
+	const { dns = [], hostingProvider } = props;
+	const aRecordIps = dns.filter( ( x ) => x.type === 'A' && x.ip );
+
 	return (
-		<>
+		<div className="hosting-information">
 			<h3>Hosting information</h3>
-		</>
+			<ul className="hosting-information-details result-list">
+				<li>
+					<div className="name">Provider</div>
+					<div>{ hostingProvider?.name }</div>
+				</li>
+				<li>
+					<div className="name">Support</div>
+					<div>Contact support</div>
+				</li>
+				<li>
+					<div className="name">A Records</div>
+					<div>
+						<ul>
+							{ aRecordIps.map( ( x, i ) => (
+								<li key={ i }>{ x.ip }</li>
+							) ) }
+						</ul>
+					</div>
+				</li>
+			</ul>
+		</div>
 	);
 }
