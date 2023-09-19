7504068fdc6ecc0efe8ff6fca87c9a6421e45c6f
Maciej Grabowski <littlesoldiergraba@gmail.com>
2023-09-19 11:18:48 +0200

    Use the appropriate isJetpackModuleActive selector; Render QueryJetpackModules with the NewsletterSettingsForm;

--- client/my-sites/site-settings/settings-newsletter/main.tsx
+++ client/my-sites/site-settings/settings-newsletter/main.tsx
@@ -1,10 +1,11 @@
 import { useTranslate } from 'i18n-calypso';
 import SubscriptionsModuleBanner from 'calypso/blocks/subscriptions-module-banner';
 import DocumentHead from 'calypso/components/data/document-head';
+import QueryJetpackModules from 'calypso/components/data/query-jetpack-modules';
 import FormattedHeader from 'calypso/components/formatted-header';
 import Main from 'calypso/components/main';
 import { useSelector } from 'calypso/state';
-import { isJetpackModuleActive } from 'calypso/state/sites/selectors';
+import isJetpackModuleActive from 'calypso/state/selectors/is-jetpack-module-active';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { NewsletterSettingsSection } from '../reading-newsletter-settings';
 import wrapSettingsForm from '../wrap-settings-form';
@@ -79,18 +80,21 @@ const NewsletterSettingsForm = wrapSettingsForm( getFormSettings )(
 		const savedSubscriptionOptions = settings?.subscription_options;
 
 		return (
-			<form onSubmit={ handleSubmitForm }>
-				<NewsletterSettingsSection
-					fields={ fields }
-					handleAutosavingToggle={ handleAutosavingToggle }
-					handleToggle={ handleToggle }
-					handleSubmitForm={ handleSubmitForm }
-					disabled={ disabled }
-					isSavingSettings={ isSavingSettings }
-					savedSubscriptionOptions={ savedSubscriptionOptions }
-					updateFields={ updateFields }
-				/>
-			</form>
+			<>
+				{ siteId && <QueryJetpackModules siteId={ siteId } /> }
+				<form onSubmit={ handleSubmitForm }>
+					<NewsletterSettingsSection
+						fields={ fields }
+						handleAutosavingToggle={ handleAutosavingToggle }
+						handleToggle={ handleToggle }
+						handleSubmitForm={ handleSubmitForm }
+						disabled={ disabled }
+						isSavingSettings={ isSavingSettings }
+						savedSubscriptionOptions={ savedSubscriptionOptions }
+						updateFields={ updateFields }
+					/>
+				</form>
+			</>
 		);
 	}
 );
