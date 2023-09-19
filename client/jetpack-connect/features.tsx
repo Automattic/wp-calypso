a15d9d388d907b8013fa8e1230243b36e97d7515
moon <moon.kyong@automattic.com>
2023-09-19 12:26:44 -0700

    Make features required props

--- client/jetpack-connect/features.tsx
+++ client/jetpack-connect/features.tsx
@@ -1,6 +1,5 @@
 import { Icon, check } from '@wordpress/icons';
 import classNames from 'classnames';
-import { useTranslate } from 'i18n-calypso';
 
 export const JetpackFeatures = ( {
 	className,
@@ -11,22 +10,6 @@ export const JetpackFeatures = ( {
 	col1Features: string[];
 	col2Features: string[];
 } ) => {
-	const translate = useTranslate();
-
-	col1Features = col1Features || [
-		translate( 'Speed up images and photos' ),
-		translate( 'Prevent brute force attacks' ),
-		translate( 'Secure user authentication' ),
-		translate( 'Enhanced site stats and insights' ),
-	];
-
-	col2Features = col2Features || [
-		translate( 'Daily or real-time backups' ),
-		translate( 'Keep plugins auto-updated' ),
-		translate( 'Immediate downtime alerts' ),
-		translate( 'Bulk site management from one dashboard' ),
-	];
-
 	return (
 		<div className={ classNames( 'jetpack-connect__features_wrapper', className ) }>
 			<ul className="jetpack-connect__features">
