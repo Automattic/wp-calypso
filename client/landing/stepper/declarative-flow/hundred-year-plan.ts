bc8fec48ad832fc28f801976b3e7784faa865006
Aneesh Devasthale <aneeshd16@users.noreply.github.com>
2023-09-19 14:51:43 +0530

    100 year plan: New/Existing site flow (#81723)
    * 100 year plan: New/Existing site flow
    
    * Fix styling
    
    * Removed BBE check
    
    * Do not add existing site domain to the cart
    
    * Adjust max-height for site selector on small screens to prevent overflow and ensure all sites are visible.
    
    * Capital P dangit
    
    * Submit siteId and siteSlug instead of the site object

--- client/landing/stepper/declarative-flow/hundred-year-plan.ts
+++ client/landing/stepper/declarative-flow/hundred-year-plan.ts
@@ -1,6 +1,6 @@
 import { PLAN_100_YEARS, getPlan } from '@automattic/calypso-products';
 import { UserSelect } from '@automattic/data-stores';
-import { HUNDRED_YEAR_PLAN_FLOW } from '@automattic/onboarding';
+import { HUNDRED_YEAR_PLAN_FLOW, addProductsToCart } from '@automattic/onboarding';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { translate } from 'i18n-calypso';
 import { useEffect } from 'react';
@@ -9,11 +9,11 @@ import {
 	setSignupCompleteSlug,
 	setSignupCompleteFlowName,
 } from 'calypso/signup/storageUtils';
+import { SiteId, SiteSlug } from 'calypso/types';
 import { ONBOARD_STORE, USER_STORE } from '../stores';
 import { useLoginUrl } from '../utils/path';
 import { recordSubmitStep } from './internals/analytics/record-submit-step';
-import { ProvidedDependencies } from './internals/types';
-import type { Flow } from './internals/types';
+import type { ProvidedDependencies, Flow } from './internals/types';
 
 const HundredYearPlanFlow: Flow = {
 	name: HUNDRED_YEAR_PLAN_FLOW,
@@ -21,6 +21,42 @@ const HundredYearPlanFlow: Flow = {
 		return translate( '100-year Plan' );
 	},
 	useSteps() {
+		const currentUser = useSelect(
+			( select ) => ( select( USER_STORE ) as UserSelect ).getCurrentUser(),
+			[]
+		);
+
+		if ( currentUser?.site_count ) {
+			return [
+				{
+					slug: 'new-or-existing-site',
+					asyncComponent: () => import( './internals/steps-repository/new-or-existing-site' ),
+				},
+				{
+					slug: 'site-picker',
+					asyncComponent: () =>
+						import( './internals/steps-repository/hundred-year-plan-site-picker' ),
+				},
+				{
+					slug: 'setup',
+					asyncComponent: () => import( './internals/steps-repository/hundred-year-plan-setup' ),
+				},
+				{
+					slug: 'domains',
+					asyncComponent: () => import( './internals/steps-repository/domains' ),
+				},
+				{
+					slug: 'processing',
+					asyncComponent: () => import( './internals/steps-repository/processing-step' ),
+				},
+
+				{
+					slug: 'siteCreationStep',
+					asyncComponent: () => import( './internals/steps-repository/site-creation-step' ),
+				},
+			];
+		}
+
 		return [
 			{
 				slug: 'setup',
@@ -52,7 +88,7 @@ const HundredYearPlanFlow: Flow = {
 			( select ) => ( select( USER_STORE ) as UserSelect ).isCurrentUserLoggedIn(),
 			[]
 		);
-		const { setPlanCartItem } = useDispatch( ONBOARD_STORE );
+		const { setPlanCartItem, setPendingAction } = useDispatch( ONBOARD_STORE );
 
 		const logInUrl = useLoginUrl( {
 			variationName: flowName,
@@ -68,7 +104,37 @@ const HundredYearPlanFlow: Flow = {
 		function submit( providedDependencies: ProvidedDependencies = {} ) {
 			recordSubmitStep( providedDependencies, '', flowName, _currentStep );
 
+			const updateCartForExistingSite = async () => {
+				if ( ! providedDependencies?.siteSlug || ! providedDependencies?.siteId ) {
+					return;
+				}
+
+				const siteSlug: SiteSlug = providedDependencies.siteSlug as SiteSlug;
+				const siteId: SiteId = providedDependencies.siteSlug as SiteId;
+
+				const productsToAdd = [
+					{
+						product_slug: PLAN_100_YEARS,
+					},
+				];
+				await addProductsToCart( siteSlug, HUNDRED_YEAR_PLAN_FLOW, productsToAdd );
+
+				return {
+					siteId,
+					siteSlug,
+					goToCheckout: true,
+				};
+			};
+
 			switch ( _currentStep ) {
+				case 'new-or-existing-site':
+					if ( 'new-site' === providedDependencies?.newExistingSiteChoice ) {
+						return navigate( 'setup' );
+					}
+					return navigate( 'site-picker' );
+				case 'site-picker':
+					setPendingAction( updateCartForExistingSite );
+					return navigate( 'processing' );
 				case 'setup':
 					return navigate( 'domains' );
 				case 'domains':
--- client/lib/presales-chat/index.ts
+++ client/lib/presales-chat/index.ts
@@ -74,6 +74,10 @@ export function usePresalesChat( keyType: KeyType, enabled = true, skipAvailabil
 		isLoggedIn
 	);
 
+	const openChat = () => {
+		window.zE( 'messenger', 'open' );
+	};
+
 	useEffect( () => {
 		// presales chat is always shown by default
 		if ( enabled && isPresalesChatAvailable && isMessagingScriptLoaded ) {
@@ -85,5 +89,6 @@ export function usePresalesChat( keyType: KeyType, enabled = true, skipAvailabil
 		isChatActive: isPresalesChatAvailable && isEligibleForPresalesChat,
 		isLoading: isLoadingAvailability,
 		isPresalesChatAvailable,
+		openChat,
 	};
 }
