386bd5ca29014153efff512ce30979af37b07311
Konstantin Obenland <obenland@gmx.de>
2023-09-20 08:59:19 -0500

    FSE: Avoid breaking sites even if Jetpack is unavailable
    Fixes #79956, #77573.

--- apps/editing-toolkit/editing-toolkit-plugin/help-center/class-help-center.php
+++ apps/editing-toolkit/editing-toolkit-plugin/help-center/class-help-center.php
@@ -42,6 +42,10 @@ class Help_Center {
 			return;
 		}
 
+		if ( ! function_exists( 'wpcom_get_site_purchases' ) ) {
+			return;
+		}
+
 		$this->asset_file = include plugin_dir_path( __FILE__ ) . 'dist/help-center.asset.php';
 		$this->version    = $this->asset_file['version'];
 
@@ -136,19 +140,21 @@ class Help_Center {
 			$site    = $user->primary_blog;
 			switch_to_blog( $site );
 		} else {
-			$site = \Jetpack_Options::get_option( 'id' );
+			/*
+			 * Atomic sites have the WP.com blog ID stored as a Jetpack option. This code deliberately
+			 * doesn't use `Jetpack_Options::get_option` so it works even when Jetpack has not been loaded.
+			 */
+			$jetpack_options = get_option( 'jetpack_options' );
+			if ( is_array( $jetpack_options ) && isset( $jetpack_options['id'] ) ) {
+				$site = (int) $jetpack_options['id'];
+			} else {
+				$site = get_current_blog_id();
+			}
 		}
 
-		$logo_id  = get_option( 'site_logo' );
-		$products = wpcom_get_site_purchases();
-		$plan     = array_pop(
-			array_filter(
-				$products,
-				function ( $product ) {
-					return 'bundle' === $product->product_type;
-				}
-			)
-		);
+		$logo_id = get_option( 'site_logo' );
+		$bundles = wp_list_filter( wpcom_get_site_purchases(), array( 'product_type' => 'bundle' ) );
+		$plan    = array_pop( $bundles );
 
 		$return_data = array(
 			'ID'               => $site,
