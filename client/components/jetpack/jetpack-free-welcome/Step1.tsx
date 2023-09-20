d102dd7759f0aa04a81563ae8b9a6525d8f8efb6
jboland88 <18016357+jboland88@users.noreply.github.com>
2023-09-20 12:03:08 -0400

    Add download link to the Jetpack welcome screen, update naming for Jetpack Social (#81789)

--- client/components/jetpack/jetpack-free-welcome/Step1.tsx
+++ client/components/jetpack/jetpack-free-welcome/Step1.tsx
@@ -8,15 +8,33 @@ export const Step1 = () => {
 
 	const jetpackInstallInstructionsLink =
 		'https://jetpack.com/support/getting-started-with-jetpack/';
+	const jetpackDownloadLink = 'https://wordpress.org/plugins/jetpack/';
 
 	return (
 		<>
 			<h2>{ translate( 'Install Jetpack' ) }</h2>
 			<p>
 				{ translate(
-					'Download Jetpack or install it directly from your site by following the {{a}}instructions we put together here{{/a}}.',
+					'{{downloadLink}}Download Jetpack{{/downloadLink}} or install it directly from your site by following the {{a}}instructions we put together here{{/a}}.',
 					{
 						components: {
+							downloadLink: (
+								<a
+									target="_blank"
+									rel="noopener noreferrer"
+									onClick={ () =>
+										dispatch(
+											recordTracksEvent(
+												'calypso_siteless_free_page_download_jetpack_link_clicked',
+												{
+													product_slug: 'jetpack_free',
+												}
+											)
+										)
+									}
+									href={ jetpackDownloadLink }
+								/>
+							),
 							a: (
 								<a
 									target="_blank"
