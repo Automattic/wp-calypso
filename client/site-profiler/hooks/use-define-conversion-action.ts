4d21bb574d9a0ec4a3b371e5066bc8cb32523ce9
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-09-21 20:55:02 +0200

    Define one more conversion action

--- client/site-profiler/hooks/use-define-conversion-action.ts
+++ client/site-profiler/hooks/use-define-conversion-action.ts
@@ -1,21 +1,26 @@
-import type { HostingProvider } from 'calypso/data/site-profiler/types';
+import type { HostingProvider, WhoIs } from 'calypso/data/site-profiler/types';
 
 export type CONVERSION_ACTION =
 	| 'register-domain'
 	| 'transfer-domain'
+	| 'transfer-google-domain'
 	| 'transfer-hosting'
 	| 'transfer-domain-hosting'
 	| 'idle';
 
 export default function useDefineConversionAction(
 	domain: string,
+	whois?: WhoIs,
 	isDomainAvailable?: boolean,
-	registrar = '',
 	hostingProvider?: HostingProvider
 ): CONVERSION_ACTION | undefined {
 	const isWpDomain = domain.toLowerCase().includes( 'wordpress.com' );
 	const isWpAtomicDomain = domain.toLowerCase().includes( 'wpcomstaging.com' );
-	const isA8cRegistrar = registrar.toLowerCase().includes( 'automattic' );
+	const isA8cRegistrar = whois?.registrar.toLowerCase().includes( 'automattic' );
+	const isGoogleRegistrar =
+		whois?.registrar.toLowerCase().includes( 'google' ) ||
+		whois?.registrar_whois_server.toLowerCase().includes( 'google' ) ||
+		whois?.registrant_organization.toLowerCase().includes( 'google' );
 
 	const isA8cDomain = isA8cRegistrar || isWpDomain || isWpAtomicDomain;
 	const isA8cHosting = hostingProvider?.slug === 'automattic';
@@ -24,6 +29,8 @@ export default function useDefineConversionAction(
 		return 'register-domain';
 	} else if ( isA8cDomain && ! isA8cHosting ) {
 		return 'transfer-hosting';
+	} else if ( ! isA8cDomain && isGoogleRegistrar ) {
+		return 'transfer-google-domain';
 	} else if ( ! isA8cDomain && isA8cHosting ) {
 		return 'transfer-domain';
 	} else if ( ! isA8cDomain && ! isA8cHosting ) {
