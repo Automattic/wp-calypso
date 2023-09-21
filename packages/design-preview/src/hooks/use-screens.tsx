000a82d4e41eb45aac6cfa4153df278a24cf1504
Miguel San Segundo <1881481+miksansegundo@users.noreply.github.com>
2023-09-21 19:14:53 +0700

    Design picker & Assembler - Fix vertical and horizontal spacings for style, font and color variations  (#81906)
    * Fix theme styles, fonts and color variation spacings
    
    * Update vertical spacing in assembler

--- packages/design-preview/src/hooks/use-screens.tsx
+++ packages/design-preview/src/hooks/use-screens.tsx
@@ -65,20 +65,18 @@ const useScreens = ( {
 						label: translate( 'Styles' ),
 						path: '/style-variations',
 						content: (
-							<div className="design-preview__sidebar-variations">
-								<div className="design-preview__sidebar-variations-grid">
-									<GlobalStylesVariations
-										key="style-variations"
-										globalStylesVariations={ variations as GlobalStylesObject[] }
-										selectedGlobalStylesVariation={ selectedVariation as GlobalStylesObject }
-										splitDefaultVariation={ splitDefaultVariation }
-										displayFreeLabel={ splitDefaultVariation }
-										showOnlyHoverViewDefaultVariation={ false }
-										onSelect={ ( globalStyleVariation: GlobalStylesObject ) =>
-											onSelectVariation( globalStyleVariation as StyleVariation )
-										}
-									/>
-								</div>
+							<div className="design-preview__sidebar-variations design-preview__sidebar-variations--styles">
+								<GlobalStylesVariations
+									key="style-variations"
+									globalStylesVariations={ variations as GlobalStylesObject[] }
+									selectedGlobalStylesVariation={ selectedVariation as GlobalStylesObject }
+									splitDefaultVariation={ splitDefaultVariation }
+									displayFreeLabel={ splitDefaultVariation }
+									showOnlyHoverViewDefaultVariation={ false }
+									onSelect={ ( globalStyleVariation: GlobalStylesObject ) =>
+										onSelectVariation( globalStyleVariation as StyleVariation )
+									}
+								/>
 							</div>
 						),
 						actionText: translate( 'Save styles' ),
