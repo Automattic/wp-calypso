Automattic/wp-calypso

commit a50a51d20953f4bcbfddd0117ff381bbc72ae4cb

Add presales chat bubble to domain transfer (#79679)
* Add presales chat bubble to domain transfer

* Add bubble to

* isEligibleForPresalesChat will not activate while loading Zendesk (#79748)

---------

Co-authored-by: Gabriel Demichelis <gabrieldemichelis@gmail.com>
Co-authored-by: Paulo Marcos Trentin <paulo@paulotrentin.com.br>

client/lib/presales-chat/index.ts
@@ -1,4 +1,8 @@
-import { useMessagingAvailability, useZendeskMessaging } from '@automattic/help-center/src/hooks';
+import {
+	useChatStatus,
+	useMessagingAvailability,
+	useZendeskMessaging,
+} from '@automattic/help-center/src/hooks';
 import { useIsEnglishLocale } from '@automattic/i18n-utils';
 import { useSelector } from 'react-redux';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
@@ -36,7 +40,8 @@ function getGroupName( keyType: KeyType ) {
 
 export function usePresalesChat( keyType: KeyType, enabled = true, skipAvailabilityCheck = false ) {
 	const isEnglishLocale = useIsEnglishLocale();
-	const isEligibleForPresalesChat = enabled && isEnglishLocale;
+	const { canConnectToZendesk } = useChatStatus();
+	const isEligibleForPresalesChat = enabled && isEnglishLocale && canConnectToZendesk;
 
 	const group = getGroupName( keyType );
 

client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-domains/index.tsx
@@ -4,6 +4,7 @@ import { useI18n } from '@wordpress/react-i18n';
 import { StepContainer } from 'calypso/../packages/onboarding/src';
 import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { usePresalesChat } from 'calypso/lib/presales-chat';
 import CalypsoShoppingCartProvider from 'calypso/my-sites/checkout/calypso-shopping-cart-provider';
 import TransferDomains from './domains';
 import type { Step } from '../../types';
@@ -14,6 +15,8 @@ const Intro: Step = function Intro( { navigation, flow } ) {
 	const { submit, goBack } = navigation;
 	const { __ } = useI18n();
 	const isEnglishLocale = useIsEnglishLocale();
+
+	usePresalesChat( 'wpcom' );
 
 	const handleSubmit = () => {
 		submit?.();

client/landing/stepper/declarative-flow/internals/steps-repository/domain-transfer-intro/index.tsx
@@ -2,6 +2,7 @@ import { useI18n } from '@wordpress/react-i18n';
 import { StepContainer } from 'calypso/../packages/onboarding/src';
 import FormattedHeader from 'calypso/components/formatted-header';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { usePresalesChat } from 'calypso/lib/presales-chat';
 import IntroStep from './intro';
 import type { Step } from '../../types';
 
@@ -10,6 +11,8 @@ import './styles.scss';
 const Intro: Step = function Intro( { navigation } ) {
 	const { submit } = navigation;
 	const { __ } = useI18n();
+
+	usePresalesChat( 'wpcom' );
 
 	const handleSubmit = () => {
 		submit?.();

packages/help-center/src/hooks/use-chat-status.ts
@@ -28,7 +28,7 @@ export default function useChatStatus(
 	const { status: zendeskStatus } = useZendeskConfig( isEligibleForChat );
 
 	return {
-		canConnectToZendesk: zendeskStatus !== 'error',
+		canConnectToZendesk: zendeskStatus !== 'error' && zendeskStatus !== 'loading',
 		hasActiveChats,
 		isChatAvailable: Boolean( chatAvailability?.is_available ),
 		isEligibleForChat,

commit 4b31e73724ae499a99eab4a0fd3a4d7b7af9a4fa

Ensure no extra availability calls are made
Bug introduced in https://github.com/Automattic/wp-calypso/pull/79679/files#r1334699181

client/lib/presales-chat/index.ts
@@ -53,12 +53,11 @@ function getGroupName( keyType: KeyType ) {
 export function usePresalesChat( keyType: KeyType, enabled = true, skipAvailabilityCheck = false ) {
 	const isEnglishLocale = useIsEnglishLocale();
 	const isWpMobileAppUser = isWpMobileApp();
+	const group = getGroupName( keyType );
 
-	const { canConnectToZendesk } = useChatStatus();
+	const { canConnectToZendesk } = useChatStatus( group, enabled );
 	const isEligibleForPresalesChat =
 		enabled && isEnglishLocale && canConnectToZendesk && ! isWpMobileAppUser;
-
-	const group = getGroupName( keyType );
 
 	const { data: chatAvailability, isInitialLoading: isLoadingAvailability } =
 		useMessagingAvailability( group, isEligibleForPresalesChat && ! skipAvailabilityCheck );