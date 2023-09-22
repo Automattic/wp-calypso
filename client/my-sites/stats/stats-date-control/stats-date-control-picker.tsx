946b6f1e44a219c9c11630686653b90c8af248b4
Anna McPhee <anna.mcphee@automattic.com>
2023-09-22 16:42:15 -0600

    add working shortcuts to list

--- client/my-sites/stats/stats-date-control/stats-date-control-picker.tsx
+++ client/my-sites/stats/stats-date-control/stats-date-control-picker.tsx
@@ -7,17 +7,66 @@ import DateControlPickerShortcuts from './stats-date-control-picker-shortcuts';
 import { DateControlPickerProps } from './types';
 
 const DateControlPicker = ( { slug, queryParams }: DateControlPickerProps ) => {
+	const redirectToQueryDate = ( daysToSubtract: number ) => {
+		const startDate = calculateQueryDate( daysToSubtract );
+		const query = qs.stringify( Object.assign( {}, queryParams, { startDate } ), {
+			addQueryPrefix: true,
+		} );
+		const period = 'day'; // TODO: adjust this as needed
+		const url = `/stats/${ period }/${ slug }`;
+		const href = `${ url }${ query }`;
+
+		page( href );
+	};
+
 	// shortcut list will come from props
 	const shortcutList = [
 		{
-			id: 'test',
-			label: 'Test shortcut',
-			onClick: () => {
-				// eslint-disable-next-line no-console
-				console.log( 'clicking' );
-			},
+			id: 'today',
+			label: 'Today',
+			onClick: () => redirectToQueryDate( 0 ),
+		},
+		{
+			id: 'yesterday',
+			label: 'Yesterday',
+			onClick: () => redirectToQueryDate( 1 ),
+		},
+		{
+			id: 'last-7-days',
+			label: 'Last 7 Days',
+			onClick: () => redirectToQueryDate( 7 ),
+		},
+		{
+			id: 'last-30-days',
+			label: 'Last 30 Days',
+			onClick: () => redirectToQueryDate( 30 ),
+		},
+		{
+			id: 'last-year',
+			label: 'Last Year',
+			onClick: () => redirectToQueryDate( 365 ),
+		},
+		{
+			id: 'all-time',
+			label: 'All Time',
+			onClick: () => {}, // eslint-disable-line @typescript-eslint/no-empty-function
+			// This is empty because I don't yet fully understand what 'all time' will show
+		},
+		{
+			id: 'custom-range',
+			label: 'Custom Range',
+			onClick: () => {}, // eslint-disable-line @typescript-eslint/no-empty-function
+			// Keep this empty, since the custom range is handled by the DateControlPickerDate component.
 		},
 	];
+
+	// calculate the date to query for based on the number of days to subtract
+	function calculateQueryDate( daysToSubtract: number ) {
+		const today = new Date();
+		const date = new Date( today );
+		date.setDate( date.getDate() - daysToSubtract );
+		return date.toISOString().split( 'T' )[ 0 ];
+	}
 
 	// TODO: remove placeholder values
 	const [ inputStartDate, setInputStartDate ] = useState( new Date().toISOString().slice( 0, 10 ) );
