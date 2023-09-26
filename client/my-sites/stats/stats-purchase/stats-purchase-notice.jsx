da82dea721b534e204c4d711b23936de76f5cffb
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2023-09-26 14:03:14 +1000

    Stats: Header wording update

--- client/my-sites/stats/stats-purchase/stats-purchase-notice.jsx
+++ client/my-sites/stats/stats-purchase/stats-purchase-notice.jsx
@@ -38,10 +38,10 @@ const StatsCommercialOwned = ( { siteSlug } ) => {
 
 	return (
 		<>
-			<h1>{ translate( 'You have already purchased Jetpack Stats Commercial!' ) }</h1>
+			<h1>{ translate( 'You have already purchased Jetpack Stats!' ) }</h1>
 			<p>
 				{ translate(
-					'You have already purchased a license or a plan that supports this product, and it has been successfully activated. You now have access to:'
+					'You have already purchased a commercial license or a plan that supports this product, and it has been successfully activated. You now have access to:'
 				) }
 			</p>
 			<StatsBenefitsCommercial />
@@ -67,10 +67,10 @@ const StatsPWYWOwnedNotice = ( { siteId, siteSlug } ) => {
 
 	return (
 		<StatsSingleItemPagePurchaseFrame>
-			<h1>{ translate( 'You have already purchased Jetpack Stats Personal Plan!' ) }</h1>
+			<h1>{ translate( 'You have already purchased Jetpack Stats!' ) }</h1>
 			<p>
 				{ translate(
-					'You have already purchased a license for this product, and it has been successfully activated. You now have access to:'
+					'You have already purchased a personal license for this product, and it has been successfully activated. You now have access to:'
 				) }
 			</p>
 			<StatsBenefitsPersonal />
