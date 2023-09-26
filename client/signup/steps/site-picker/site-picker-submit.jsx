3f74e90c370d3556549407125d73fa223cf5f7c9
Dehan <dehandecroos@gmail.com>
2023-09-26 11:39:27 -0400

    Add dependencies to site picker step

--- client/signup/steps/site-picker/site-picker-submit.jsx
+++ client/signup/steps/site-picker/site-picker-submit.jsx
@@ -14,17 +14,20 @@ export class SitePickerSubmit extends Component {
 		const hasPaidPlan = siteHasPaidPlan( selectedSite );
 		const { ID: siteId, slug: siteSlug } = selectedSite;
 
-		this.props.submitSignupStep( {
-			stepName,
-			stepSectionName,
-			siteId,
-			siteSlug,
-		} );
+		this.props.submitSignupStep(
+			{
+				stepName,
+				stepSectionName,
+				siteId,
+				siteSlug,
+			},
+			{ themeSlugWithRepo: 'pub/twentysixteen' }
+		);
 
 		if ( hasPaidPlan ) {
 			this.props.submitSignupStep(
 				{ stepName: 'plans-site-selected', wasSkipped: true },
-				{ cartItems: null }
+				{ cartItem: null, themeSlugWithRepo: 'pub/twentysixteen' }
 			);
 
 			goToStep( 'user' );
