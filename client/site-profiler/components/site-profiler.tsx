39997f10adaaee2add161c0500cd9f1b76c9330d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-09-26 19:04:51 +0200

    Update heading block bg color to match design

--- client/site-profiler/components/site-profiler.tsx
+++ client/site-profiler/components/site-profiler.tsx
@@ -84,7 +84,7 @@ export default function SiteProfiler() {
 				</LayoutBlock>
 			) }
 
-			<LayoutBlock isMonoBg>
+			<LayoutBlock isMonoBg={ !! data }>
 				<HostingIntro />
 			</LayoutBlock>
 		</>
