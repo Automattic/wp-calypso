2cc4889b3d155f92daac3d0b9f82159d58955e98
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2023-09-27 12:01:13 +0200

    Fix: rename object.

--- client/site-profiler/components/domain-information/index.tsx
+++ client/site-profiler/components/domain-information/index.tsx
@@ -48,7 +48,7 @@ export default function DomainInformation( props: Props ) {
 		} );
 	};
 
-	const fWhois: { [ name: string ]: boolean } = {};
+	const filteredWhois: { [ name: string ]: boolean } = {};
 
 	// Check if there are redacted whois fields
 	for ( const key in whois ) {
@@ -65,7 +65,7 @@ export default function DomainInformation( props: Props ) {
 			++fieldsRedacted;
 		}
 
-		fWhois[ key ] = ! isRedacted;
+		filteredWhois[ key ] = ! isRedacted;
 	}
 
 	return (
@@ -73,13 +73,13 @@ export default function DomainInformation( props: Props ) {
 			<h3>{ translate( 'Domain information' ) }</h3>
 
 			<ul className="domain-information-details result-list">
-				{ fWhois.domain_name && (
+				{ filteredWhois.domain_name && (
 					<li>
 						<div className="name">{ translate( 'Domain name' ) }</div>
 						<div>{ whois.domain_name }</div>
 					</li>
 				) }
-				{ fWhois.registrar && (
+				{ filteredWhois.registrar && (
 					<li>
 						<div className="name">{ translate( 'Registrar' ) }</div>
 						<div>
@@ -95,25 +95,25 @@ export default function DomainInformation( props: Props ) {
 						</div>
 					</li>
 				) }
-				{ fWhois.creation_date && (
+				{ filteredWhois.creation_date && (
 					<li>
 						<div className="name">{ translate( 'Registered on' ) }</div>
 						<div>{ moment.utc( whois.creation_date ).format( momentFormat ) }</div>
 					</li>
 				) }
-				{ fWhois.registry_expiry_date && (
+				{ filteredWhois.registry_expiry_date && (
 					<li>
 						<div className="name">{ translate( 'Expires on' ) }</div>
 						<div>{ moment.utc( whois.registry_expiry_date ).format( momentFormat ) }</div>
 					</li>
 				) }
-				{ fWhois.updated_date && (
+				{ filteredWhois.updated_date && (
 					<li>
 						<div className="name">{ translate( 'Updated on' ) }</div>
 						<div>{ moment.utc( whois.updated_date ).format( momentFormat ) }</div>
 					</li>
 				) }
-				{ fWhois.name_server && whois.name_server && (
+				{ filteredWhois.name_server && whois.name_server && (
 					<li>
 						<div className="name">{ translate( 'Name servers' ) }</div>
 						<div>
@@ -132,7 +132,7 @@ export default function DomainInformation( props: Props ) {
 							<strong>{ translate( 'Registrant contact' ) }</strong>
 						</h4>
 						<ul>
-							{ fWhois.registrant_name && (
+							{ filteredWhois.registrant_name && (
 								<li>
 									{ translate(
 										'{{strong}}Name:{{/strong}} %s',
@@ -140,7 +140,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.registrant_organization && (
+							{ filteredWhois.registrant_organization && (
 								<li>
 									{ translate(
 										'{{strong}}Organization:{{/strong}} %s',
@@ -148,7 +148,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.registrant_street && (
+							{ filteredWhois.registrant_street && (
 								<li>
 									{ translate(
 										'{{strong}}Street:{{/strong}} %s',
@@ -156,7 +156,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.registrant_city && (
+							{ filteredWhois.registrant_city && (
 								<li>
 									{ translate(
 										'{{strong}}City:{{/strong}} %s',
@@ -164,7 +164,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.registrant_state && (
+							{ filteredWhois.registrant_state && (
 								<li>
 									{ translate(
 										'{{strong}}State:{{/strong}} %s',
@@ -172,7 +172,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.registrant_postal_code && (
+							{ filteredWhois.registrant_postal_code && (
 								<li>
 									{ translate(
 										'{{strong}}Postal Code:{{/strong}} %s',
@@ -180,7 +180,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.registrant_country && (
+							{ filteredWhois.registrant_country && (
 								<li>
 									{ translate(
 										'{{strong}}Country:{{/strong}} %s',
@@ -188,7 +188,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.registrant_phone && (
+							{ filteredWhois.registrant_phone && (
 								<li>
 									{ translate(
 										'{{strong}}Phone:{{/strong}} %s',
@@ -196,7 +196,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.registrant_email && whois.registrant_email && (
+							{ filteredWhois.registrant_email && whois.registrant_email && (
 								<li>
 									{ whois.registrant_email.includes( '@' ) && (
 										<a href={ `mailto:${ whois.registrant_email }` }>{ translate( 'Email' ) }</a>
@@ -212,12 +212,12 @@ export default function DomainInformation( props: Props ) {
 							<strong>{ translate( 'Administrative contact' ) }</strong>
 						</h4>
 						<ul>
-							{ fWhois.admin_name && (
+							{ filteredWhois.admin_name && (
 								<li>
 									{ translate( '{{strong}}Name:{{/strong}} %s', contactArgs( whois.admin_name ) ) }
 								</li>
 							) }
-							{ fWhois.admin_organization && (
+							{ filteredWhois.admin_organization && (
 								<li>
 									{ translate(
 										'{{strong}}Organization:{{/strong}} %s',
@@ -225,7 +225,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.admin_street && (
+							{ filteredWhois.admin_street && (
 								<li>
 									{ translate(
 										'{{strong}}Street:{{/strong}} %s',
@@ -233,12 +233,12 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.admin_city && (
+							{ filteredWhois.admin_city && (
 								<li>
 									{ translate( '{{strong}}City:{{/strong}} %s', contactArgs( whois.admin_city ) ) }
 								</li>
 							) }
-							{ fWhois.admin_state && (
+							{ filteredWhois.admin_state && (
 								<li>
 									{ translate(
 										'{{strong}}State:{{/strong}} %s',
@@ -246,7 +246,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.admin_postal_code && (
+							{ filteredWhois.admin_postal_code && (
 								<li>
 									{ translate(
 										'{{strong}}Postal Code:{{/strong}} %s',
@@ -254,7 +254,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.admin_country && (
+							{ filteredWhois.admin_country && (
 								<li>
 									{ translate(
 										'{{strong}}Country:{{/strong}} %s',
@@ -262,7 +262,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.admin_phone && (
+							{ filteredWhois.admin_phone && (
 								<li>
 									{ translate(
 										'{{strong}}Phone:{{/strong}} %s',
@@ -270,10 +270,10 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.admin_email && whois.admin_email?.includes( '@' ) && (
+							{ filteredWhois.admin_email && whois.admin_email?.includes( '@' ) && (
 								<a href={ `mailto:${ whois.admin_email }` }>{ translate( 'Email' ) }</a>
 							) }
-							{ fWhois.admin_email &&
+							{ filteredWhois.admin_email &&
 								whois.admin_email &&
 								urlRegex.test( whois.admin_email ) &&
 								linkifyUrlFromText( whois.admin_email ) }
@@ -284,12 +284,12 @@ export default function DomainInformation( props: Props ) {
 							<strong>{ translate( 'Technical contact' ) }</strong>
 						</h4>
 						<ul>
-							{ fWhois.tech_name && (
+							{ filteredWhois.tech_name && (
 								<li>
 									{ translate( '{{strong}}Name:{{/strong}} %s', contactArgs( whois.tech_name ) ) }
 								</li>
 							) }
-							{ fWhois.tech_organization && (
+							{ filteredWhois.tech_organization && (
 								<li>
 									{ translate(
 										'{{strong}}Organization:{{/strong}} %s',
@@ -297,7 +297,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.tech_street && (
+							{ filteredWhois.tech_street && (
 								<li>
 									{ translate(
 										'{{strong}}Street:{{/strong}} %s',
@@ -305,17 +305,17 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.tech_city && (
+							{ filteredWhois.tech_city && (
 								<li>
 									{ translate( '{{strong}}City:{{/strong}} %s', contactArgs( whois.tech_city ) ) }
 								</li>
 							) }
-							{ fWhois.tech_state && (
+							{ filteredWhois.tech_state && (
 								<li>
 									{ translate( '{{strong}}State:{{/strong}} %s', contactArgs( whois.tech_state ) ) }
 								</li>
 							) }
-							{ fWhois.tech_postal_code && (
+							{ filteredWhois.tech_postal_code && (
 								<li>
 									{ translate(
 										'{{strong}}Postal Code:{{/strong}} %s',
@@ -323,7 +323,7 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.tech_country && (
+							{ filteredWhois.tech_country && (
 								<li>
 									{ translate(
 										'{{strong}}Country:{{/strong}} %s',
@@ -331,15 +331,15 @@ export default function DomainInformation( props: Props ) {
 									) }
 								</li>
 							) }
-							{ fWhois.tech_phone && (
+							{ filteredWhois.tech_phone && (
 								<li>
 									{ translate( '{{strong}}Phone:{{/strong}} %s', contactArgs( whois.tech_phone ) ) }
 								</li>
 							) }
-							{ fWhois.tech_email && whois.tech_email?.includes( '@' ) && (
+							{ filteredWhois.tech_email && whois.tech_email?.includes( '@' ) && (
 								<a href={ `mailto:${ whois.tech_email }` }>{ translate( 'Email' ) }</a>
 							) }
-							{ fWhois.tech_email &&
+							{ filteredWhois.tech_email &&
 								whois.tech_email &&
 								urlRegex.test( whois.tech_email ) &&
 								linkifyUrlFromText( whois.tech_email ) }
