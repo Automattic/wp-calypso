d8fffa168b8042cff5c940a7bb3159e527dbfa86
Antony Agrios <antonyagrios@gmail.com>
2023-09-27 12:23:09 +0300

    Update: Refactor use-staging-sync to use one endpoint for push to staging

--- client/my-sites/hosting/staging-site-card/use-staging-sync.ts
+++ client/my-sites/hosting/staging-site-card/use-staging-sync.ts
@@ -1,8 +1,11 @@
-import { useMutation, UseMutationOptions, useIsMutating } from '@tanstack/react-query';
+import { useMutation, UseMutationOptions, useIsMutating, MutationKey } from '@tanstack/react-query';
 import { useCallback, useMemo } from 'react';
 import wp from 'calypso/lib/wp';
 
-type MutationVariables = number;
+type MutationVariables = {
+	targetSiteId: number;
+	sourceSiteId: number;
+};
 
 interface MutationResponse {
 	message: string;
@@ -14,18 +17,17 @@ interface MutationError {
 }
 
 export const PUSH_TO_STAGING = 'push-to-staging-site-mutation-key';
-
 const pushSyncMutation =
-	( sitePath: string, key: string ) =>
-	< X, Y, Z, W >( sourceSiteId: number, options: UseMutationOptions< X, Y, Z, W > ) => {
+	( sitePath: string, key: MutationKey ) =>
+	< X, Y, Z, W >( options: UseMutationOptions< X, Y, Z, W > ) => {
 		const mutation = useMutation( {
-			mutationFn: async ( targetSiteId ) =>
+			mutationFn: async ( { targetSiteId, sourceSiteId } ) =>
 				wp.req.post( {
 					path: `/sites/${ sourceSiteId }/${ sitePath }/${ targetSiteId }`,
 					apiNamespace: 'wpcom/v2',
 				} ),
 			...options,
-			mutationKey: [ key, sourceSiteId ],
+			mutationKey: key,
 			onSuccess: async ( ...args ) => {
 				options.onSuccess?.( ...args );
 			},
@@ -35,30 +37,55 @@ const pushSyncMutation =
 		// isMutating is returning a number. Greater than 0 means we have some pending mutations for
 		// the provided key. This is preserved across different pages, while isLoading it's not.
 		// TODO: Remove that when react-query v5 is out. They seem to have added isPending variable for this.
-		const isLoading = useIsMutating( { mutationKey: [ key, sourceSiteId ] } ) > 0;
+		const isLoading = useIsMutating( { mutationKey: key } ) > 0;
 
-		const pushTo = useCallback( ( args: MutationVariables ) => mutate( args ), [ mutate ] );
+		const pushTo = useCallback( ( args: Z ) => mutate( args ), [ mutate ] );
 
 		return { pushTo, isLoading };
 	};
 
 export const usePushToStagingMutation = (
-	productionSiteid: number,
+	productionSiteId: number,
 	options: UseMutationOptions< MutationResponse, MutationError, MutationVariables >
 ) => {
-	const usePushToStagingHook = pushSyncMutation( 'staging-site/push-to-staging', PUSH_TO_STAGING );
-	const data = usePushToStagingHook( productionSiteid, options );
-	return useMemo( () => ( { ...data, pushToStaging: data.pushTo } ), [ data ] );
+	const usePushToStagingHook = pushSyncMutation( 'staging-site/push-to-staging', [
+		PUSH_TO_STAGING,
+		productionSiteId,
+	] as MutationKey );
+	const data = usePushToStagingHook( options );
+	return useMemo(
+		() => ( {
+			...data,
+			pushToStaging: ( stagingSiteId: number ) =>
+				data.pushTo( {
+					sourceSiteId: productionSiteId,
+					targetSiteId: stagingSiteId,
+				} ),
+		} ),
+		[ data, productionSiteId ]
+	);
 };
 
 export const usePullFromProductionMutation = (
 	stagingSiteId: number,
 	options: UseMutationOptions< MutationResponse, MutationError, MutationVariables >
 ) => {
-	const usePushToStagingHook = pushSyncMutation(
-		'staging-site/pull-from-production',
-		PUSH_TO_STAGING
+	const usePushToStagingHook = pushSyncMutation( 'staging-site/pull-from-production', [
+		PUSH_TO_STAGING,
+		stagingSiteId,
+	] as MutationKey );
+	const data = usePushToStagingHook( options );
+
+	return useMemo(
+		() => ( {
+			...data,
+			pullFromProduction: ( productionSiteid: number ) => {
+				data.pushTo( {
+					sourceSiteId: productionSiteid,
+					targetSiteId: stagingSiteId,
+				} );
+			},
+		} ),
+		[ data, stagingSiteId ]
 	);
-	const data = usePushToStagingHook( stagingSiteId, options );
-	return useMemo( () => ( { ...data, pullFromProduction: data.pushTo } ), [ data ] );
 };
