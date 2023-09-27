5741c351c52eaa8e477aa8eac5e6507d9ddb2971
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-09-27 13:21:40 +0200

    Site profiler: Update copy, reorder data to reflect design changes (#82195)
    * Reorder 'host' <-> 'ip' columns
    
    * Update heading block bg color to match design
    
    * Update heading copy to match design

--- client/site-profiler/components/site-profiler.tsx
+++ client/site-profiler/components/site-profiler.tsx
@@ -86,7 +86,7 @@ export default function SiteProfiler() {
 				</LayoutBlock>
 			) }
 
-			<LayoutBlock isMonoBg>
+			<LayoutBlock isMonoBg={ !! data }>
 				<HostingIntro />
 			</LayoutBlock>
 		</>
