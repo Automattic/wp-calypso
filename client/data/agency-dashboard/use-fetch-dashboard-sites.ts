c6857b4a4910729e04f0517a02329702340df2ab
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-09-28 13:31:53 -0500

    Expose `provisioning_blog_ids` in the sites API response

--- client/data/agency-dashboard/use-fetch-dashboard-sites.ts
+++ client/data/agency-dashboard/use-fetch-dashboard-sites.ts
@@ -56,6 +56,7 @@ const useFetchDashboardSites = (
 			),
 		select: ( data ) => {
 			return {
+				provisioningBlogIds: data.provisioning_blog_ids,
 				sites: data.sites.map( ( site: Site ) => {
 					// Since the "sites" API includes the "is_connected" property in the cache of the query set by
 					// the "useFetchTestConnection" hook, we are setting it here again since the "sites" API gets called
