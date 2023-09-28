8596e2507b8ea5c38fd3ec96b21a3c491ba633a7
Philip Jackson <p-jackson@users.noreply.github.com>
2023-09-28 23:46:06 +1300

    Domains Table: Update loading placeholder redesign (#82312)
    * Renamed isFetchingDomains prop
    
    In React Query parlance "fetching" means any time we're fetching data
    from the server, even if we already have the data in the cache.
    "loading" means we're fetching for the first time.
    
    `isLoading` is a better name because we only show the placeholder for
    the first fetch.
    
    * Use new rounded placeholder design in domains table
    
    * Show placeholder instead of column headers while loading

--- packages/domains-table/src/domains-table/domains-table-mobile-card-loading.tsx
+++ packages/domains-table/src/domains-table/domains-table-mobile-card-loading.tsx
@@ -1,22 +1,22 @@
-import { LoadingPlaceholder } from '@automattic/components';
+import { DomainsTablePlaceholder } from './domains-table-placeholder';
 
 export default function DomainsTableMobileCardLoading() {
 	return (
 		<div className="domains-table-mobile-card-loading-placeholder">
 			<div>
-				<LoadingPlaceholder delayMS={ 50 } />
+				<DomainsTablePlaceholder delayMS={ 50 } />
 				<div className="domains-table-mobile-card-loading-placeholder-actions">
-					<LoadingPlaceholder delayMS={ 50 } />
+					<DomainsTablePlaceholder delayMS={ 50 } />
 				</div>
 			</div>
 			<div>
-				<LoadingPlaceholder delayMS={ 50 } />
-				<LoadingPlaceholder delayMS={ 50 } />
+				<DomainsTablePlaceholder delayMS={ 50 } />
+				<DomainsTablePlaceholder delayMS={ 50 } />
 			</div>
 			<div>
-				<LoadingPlaceholder delayMS={ 50 } />
+				<DomainsTablePlaceholder delayMS={ 50 } />
 				<div className="domains-table-mobile-card-loading-placeholder-status">
-					<LoadingPlaceholder delayMS={ 50 } />
+					<DomainsTablePlaceholder delayMS={ 50 } />
 				</div>
 			</div>
 		</div>
