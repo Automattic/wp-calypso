7d5726cfb85466a5de7fffaa8264359102be7dbc
Yashwin <yashwinpoojary@gmail.com>
2023-09-28 09:57:43 +0530

    Add notice with a link to Jetpack Boost

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content/site-boost-column/boost-license-info-modal.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content/site-boost-column/boost-license-info-modal.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { useContext, useEffect, useMemo } from 'react';
+import ExternalLink from 'calypso/components/external-link';
 import SitesOverviewContext from '../../context';
 import useInstallBoost from '../../hooks/use-install-boost';
 import LicenseInfoModal from '../../license-info-modal';
@@ -34,6 +35,10 @@ export default function BoostLicenseInfoModal( { onClose, siteId, siteUrl }: Pro
 		// TODO: Track events here
 	};
 
+	const onJetpackBoostClick = () => {
+		// TODO: Track events here
+	};
+
 	const inProgress = status === 'loading';
 
 	useEffect( () => {
@@ -50,13 +55,32 @@ export default function BoostLicenseInfoModal( { onClose, siteId, siteUrl }: Pro
 			siteId={ siteId }
 			onCtaClick={ handlePurchaseBoost }
 			extraContent={
-				<Button
-					disabled={ inProgress }
-					className="site-boost-column__extra-button"
-					onClick={ handleInstallBoost }
-				>
-					{ translate( 'Start Free' ) }
-				</Button>
+				<>
+					<Button
+						disabled={ inProgress }
+						className="site-boost-column__extra-button"
+						onClick={ handleInstallBoost }
+					>
+						{ translate( 'Start Free' ) }
+					</Button>
+					<div className="site-boost-column__notice">
+						{ translate( 'Proceeding installs {{jetpackBoostLink/}} on your website.', {
+							args: { siteUrl },
+							comment: '%(siteUrl)s is the site url. Eg: example.com',
+							components: {
+								jetpackBoostLink: (
+									<ExternalLink
+										href="https://wordpress.org/plugins/jetpack-boost/"
+										onClick={ onJetpackBoostClick }
+										icon={ true }
+									>
+										{ translate( 'Jetpack Boost' ) }
+									</ExternalLink>
+								),
+							},
+						} ) }
+					</div>
+				</>
 			}
 			isDisabled={ inProgress }
 		/>
