42e539568224e2f8cb912b8551d8a3b2379968af
Maciej Grabowski <mashikag@users.noreply.github.com>
2023-09-28 08:48:27 +0200

    Subscriptions Management: Add View feed button to each site subscription row (#82236)
    * Subscriptions Management: Add View feed button to each site subscription row
    
    * Add tracking the button's clicks
    
    * Remove unmibgious style definition for .resubscribe-button, a class that is not used anywhere in the UI
    
    * Fix up styles formatting
    
    * Fix up tests after adding tracks broke them
    
    * Change the order of the unsubscribe button in the site subscription row popover

---
+++ client/landing/subscriptions/tracks/use-record-view-feed-button-clicked.tsx
@@ -0,0 +1,16 @@
+import useRecordSubscriptionsTracksEvent from './use-record-subscriptions-tracks-event';
+
+const useRecordViewFeedButtonClicked = () => {
+	const recordSubscriptionsTracksEvent = useRecordSubscriptionsTracksEvent();
+
+	const recordViewFeedButtonClicked = ( tracksProps: {
+		blogId: string | null;
+		feedId: string;
+	} ) => {
+		recordSubscriptionsTracksEvent( 'calypso_subscriptions_view_feed_button_clicked', tracksProps );
+	};
+
+	return recordViewFeedButtonClicked;
+};
+
+export default useRecordViewFeedButtonClicked;
