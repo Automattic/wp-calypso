61a6eeb4913716f0841adc03f55d2fe2bb69d47f
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2023-09-28 09:05:16 +0200

    Fix: new URL detection.

--- client/site-profiler/hooks/use-domain-query-param.ts
+++ client/site-profiler/hooks/use-domain-query-param.ts
@@ -8,10 +8,34 @@ export default function useDomainQueryParam( sanitize = true ) {
 	const queryParams = useQuery();
 
 	useEffect( () => {
-		const _domain = queryParams.get( 'domain' ) || '';
-		const urlPattern = /^(https?:\/\/)?([a-z0-9-]+(\.[a-z0-9-]+)+(:\d{2,5})?(\/[^\s]*)?)$/i;
+		const _domain = ( queryParams.get( 'domain' ) || '' ).trim();
 
-		setIsValid( _domain ? urlPattern.test( _domain ) : undefined );
+		try {
+			if ( _domain ) {
+				// Only allow domains with a dot in them (not localhost, for example).
+				if ( ! _domain.includes( '.' ) ) {
+					throw new Error( 'Invalid domain' );
+				}
+
+				let normalised = _domain;
+
+				// If the domain doesn't start with http:// or https://, add https://
+				if ( ! normalised.startsWith( 'http://' ) && ! normalised.startsWith( 'https://' ) ) {
+					normalised = 'https://' + normalised;
+				}
+
+				// Test if we can parse the URL. If we can't, it's invalid.
+				const url = new URL( normalised );
+
+				// Check if the protocol is 'http' or 'https'.
+				setIsValid( url.protocol === 'http:' || url.protocol === 'https:' );
+			} else {
+				setIsValid( undefined );
+			}
+		} catch ( e ) {
+			setIsValid( false );
+		}
+
 		setDomain( sanitize ? getFixedDomainSearch( extractDomainFromInput( _domain ) ) : _domain );
 	}, [ queryParams ] );
 
