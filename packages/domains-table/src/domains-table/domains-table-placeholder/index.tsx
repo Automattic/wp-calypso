b45db091d0f75fc30449b59bdb6dc3ca48f245f0
Philip Jackson <p-jackson@live.com>
2023-09-28 19:15:43 +1300

    Show placeholder instead of column headers while loading

--- packages/domains-table/src/domains-table/domains-table-placeholder/index.tsx
+++ packages/domains-table/src/domains-table/domains-table-placeholder/index.tsx
@@ -3,13 +3,18 @@ import classnames from 'classnames';
 import type { ComponentProps } from 'react';
 import './style.scss';
 
+interface DomainsTablePlaceholderProps extends ComponentProps< typeof LoadingPlaceholder > {
+	isHeader?: boolean;
+}
+
 export function DomainsTablePlaceholder( {
 	className,
+	isHeader,
 	...rest
-}: ComponentProps< typeof LoadingPlaceholder > ) {
+}: DomainsTablePlaceholderProps ) {
 	return (
 		<LoadingPlaceholder
-			className={ classnames( 'domains-table-placeholder', className ) }
+			className={ classnames( 'domains-table-placeholder', { 'is-header': isHeader }, className ) }
 			{ ...rest }
 		/>
 	);
