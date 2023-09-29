176f45fc4ef21319964dc06acf780f0cd8a2569a
Antony Agrios <antonyagrios@gmail.com>
2023-09-29 12:05:33 +0300

    Staging Sites: Add staging to production sync (#82169)
    * Staging Sites: Add staging to production sync support
    
    * Update client/my-sites/hosting/staging-site-card/card-content/manage-staging-site-card-content.tsx
    
    Co-authored-by: Wojtek Naruniec <wojtek@naruniec.me>
    
    * Update client/my-sites/hosting/staging-site-card/card-content/manage-staging-site-card-content.tsx
    
    Co-authored-by: Wojtek Naruniec <wojtek@naruniec.me>
    
    * Update client/my-sites/hosting/staging-site-card/staging-site-production-card.tsx
    
    Co-authored-by: Wojtek Naruniec <wojtek@naruniec.me>
    
    * Update client/my-sites/hosting/staging-site-card/staging-site-production-card.tsx
    
    Co-authored-by: Wojtek Naruniec <wojtek@naruniec.me>
    
    * Update client/my-sites/hosting/staging-site-card/staging-site-production-card.tsx
    
    Co-authored-by: Wojtek Naruniec <wojtek@naruniec.me>
    
    * Update: Move sync buttons to a separate row
    
    * Update: Move sync buttons to a separate row for staging site
    
    * Update: Make wording consistent
    
    ---------
    
    Co-authored-by: Wojtek Naruniec <wojtek@naruniec.me>

--- client/my-sites/hosting/staging-site-card/use-staging-sync.ts
+++ client/my-sites/hosting/staging-site-card/use-staging-sync.ts
@@ -11,6 +11,7 @@ interface PushStagingMutationError {
 }
 
 export const PUSH_TO_STAGING = 'push-to-staging-site-mutation-key';
+export const PULL_FROM_STAGING = 'pull-from-staging-site-mutation-key';
 
 export const usePushToStagingMutation = (
 	productionSiteId: number,
@@ -37,4 +38,31 @@ export const usePushToStagingMutation = (
 	const isLoading = useIsMutating( { mutationKey: [ PUSH_TO_STAGING, stagingSiteId ] } ) > 0;
 
 	return { pushToStaging: mutate, isLoading };
+};
+
+export const usePullFromStagingMutation = (
+	productionSiteId: number,
+	stagingSiteId: number,
+	options: UseMutationOptions< PushStagingMutationResponse, PushStagingMutationError >
+) => {
+	const mutation = useMutation( {
+		mutationFn: async () =>
+			wp.req.post( {
+				path: `/sites/${ productionSiteId }/staging-site/pull-from-staging/${ stagingSiteId }`,
+				apiNamespace: 'wpcom/v2',
+			} ),
+		...options,
+		mutationKey: [ PULL_FROM_STAGING, stagingSiteId ],
+		onSuccess: async ( ...args ) => {
+			options.onSuccess?.( ...args );
+		},
+	} );
+
+	const { mutate } = mutation;
+	// isMutating is returning a number. Greater than 0 means we have some pending mutations for
+	// the provided key. This is preserved across different pages, while isLoading it's not.
+	// TODO: Remove that when react-query v5 is out. They seem to have added isPending variable for this.
+	const isLoading = useIsMutating( { mutationKey: [ PULL_FROM_STAGING, stagingSiteId ] } ) > 0;
+
+	return { pullFromStaging: mutate, isLoading };
 };
