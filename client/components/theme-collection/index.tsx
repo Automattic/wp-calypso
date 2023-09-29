ebd8dbf537ab07d92548be8e56acaf7d35b345ec
Dean Sas <dean@deansas.org>
2023-09-29 10:47:54 +0100

    First pass of theme collections as carousels
    Still more work to be done, some mentioned in code, others are things
    like support for multiple carousels on a single page.

--- client/components/theme-collection/index.tsx
+++ client/components/theme-collection/index.tsx
@@ -1,6 +1,14 @@
-import { ReactElement } from 'react';
-import { ThemeBlock } from 'calypso/components/themes-list';
+import { Button } from '@wordpress/components';
+import { Icon, chevronLeft, chevronRight } from '@wordpress/icons';
+import { ReactElement, useEffect, useRef } from 'react';
 import './style.scss';
+import Swiper from 'swiper';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+import { ThemeBlock } from 'calypso/components/themes-list';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/keyboard';
+import 'swiper/css/mousewheel';
 
 interface ThemeCollectionProps {
 	heading: string;
@@ -29,30 +37,71 @@ export default function ThemeCollection( {
 	getPrice,
 	isInstalling,
 }: ThemeCollectionProps ): ReactElement {
+	const swiperInstance = useRef< Swiper | null >( null );
+
+	useEffect( () => {
+		if ( themes.length > 0 ) {
+			const el = document.querySelector( '.swiper-container' ) as HTMLElement;
+			if ( el ) {
+				swiperInstance.current = new Swiper( el, {
+					cssMode: true,
+					mousewheel: true,
+					keyboard: {
+						enabled: true,
+						onlyInViewport: false,
+					},
+					navigation: {
+						nextEl: '.theme-collection__carousel-nav-button--next',
+						prevEl: '.theme-collection__carousel-nav-button--previous',
+					},
+					threshold: 5,
+					slideToClickedSlide: true,
+					slidesPerView: 'auto',
+					spaceBetween: 20,
+					modules: [ Navigation, Keyboard, Mousewheel ],
+				} );
+			}
+		}
+
+		return () => {
+			swiperInstance.current?.destroy();
+		};
+	}, [ themes ] );
+
 	return (
-		<div className="theme-collection__container">
+		<div className="theme-collection__container ">
 			<h2>{ heading }</h2>
 			{ subheading }
-			<div className="theme-collection__list-wrapper">
-				{ themes.map( ( theme, index ) => (
-					<div
-						key={ `theme-collection-container-${ collectionSlug }-${ index }` }
-						className="theme--collection__list-item"
-					>
-						<ThemeBlock
-							getScreenshotUrl={ getScreenshotUrl }
-							index={ index }
-							collectionSlug={ collectionSlug }
-							theme={ theme }
-							siteId={ siteId }
-							isActive={ isActive }
-							getButtonOptions={ getButtonOptions }
-							getActionLabel={ getActionLabel }
-							getPrice={ getPrice }
-							isInstalling={ isInstalling }
-						/>
-					</div>
-				) ) }
+			<div className="swiper-container">
+				<div className="theme-collection__carousel-controls">
+					<Button className="theme-collection__carousel-nav-button theme-collection__carousel-nav-button--previous">
+						<Icon icon={ chevronLeft } />
+					</Button>
+					<Button className="theme-collection__carousel-nav-button theme-collection__carousel-nav-button--next">
+						<Icon icon={ chevronRight } />
+					</Button>
+				</div>
+				<div className="theme-collection__list-wrapper swiper-wrapper">
+					{ themes.map( ( theme, index ) => (
+						<div
+							key={ `theme-collection-container-${ collectionSlug }-${ index }` }
+							className="theme--collection__list-item swiper-slide"
+						>
+							<ThemeBlock
+								getScreenshotUrl={ getScreenshotUrl }
+								index={ index }
+								collectionSlug={ collectionSlug }
+								theme={ theme }
+								siteId={ siteId }
+								isActive={ isActive }
+								getButtonOptions={ getButtonOptions }
+								getActionLabel={ getActionLabel }
+								getPrice={ getPrice }
+								isInstalling={ isInstalling }
+							/>
+						</div>
+					) ) }
+				</div>
 			</div>
 		</div>
 	);
