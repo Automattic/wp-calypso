1fb523df50351b8d1eadd59ce59e2c11ece52743
jornp <75777864+jornp@users.noreply.github.com>
2023-09-30 13:11:51 +0200

    i18n: Test translation flows

--- client/components/translator-invite/index.jsx
+++ client/components/translator-invite/index.jsx
@@ -62,6 +62,21 @@ export class TranslatorInvite extends Component {
 		return null;
 	}
 
+	getTestStrings() {
+		const test1 = this.props.translate( 'This text is used to test translations.' );
+		const test2 = this.props.translate( "We've been testing this for %d days.", {
+			args: [ 99999 ],
+			comment: 'The number of days we have been testing.',
+		} );
+
+		return (
+			<div>
+				<div>{ test1 }</div>
+				<div>{ test2 }</div>
+			</div>
+		);
+	}
+
 	render() {
 		const { locale } = this.props;
 
