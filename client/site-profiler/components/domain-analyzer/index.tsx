7678470761c831a1cee5aff9bed667f267adb3ea
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-10-02 12:22:13 +0200

    Site profiler: Unify the way of showing error messages

--- client/site-profiler/components/domain-analyzer/index.tsx
+++ client/site-profiler/components/domain-analyzer/index.tsx
@@ -10,12 +10,16 @@ interface Props {
 	isBusy?: boolean;
 	isBusyForWhile?: boolean;
 	isDomainValid?: boolean;
+	domainFetchingError?: Error;
 	onFormSubmit: ( domain: string ) => void;
 }
 
 export default function DomainAnalyzer( props: Props ) {
 	const translate = useTranslate();
-	const { domain, isBusy, isBusyForWhile, isDomainValid, onFormSubmit } = props;
+	const { domain, isBusy, isBusyForWhile, isDomainValid, domainFetchingError, onFormSubmit } =
+		props;
+
+	const showError = isDomainValid === false || domainFetchingError;
 
 	const onSubmit = ( e: FormEvent< HTMLFormElement > ) => {
 		e.preventDefault();
@@ -36,7 +40,7 @@ export default function DomainAnalyzer( props: Props ) {
 			</p>
 
 			<form
-				className={ classnames( 'domain-analyzer--form', { 'is-error': isDomainValid === false } ) }
+				className={ classnames( 'domain-analyzer--form', { 'is-error': showError } ) }
 				onSubmit={ onSubmit }
 			>
 				<div className="domain-analyzer--form-container">
@@ -63,11 +67,12 @@ export default function DomainAnalyzer( props: Props ) {
 				<div className="domain-analyzer--msg">
 					<p
 						className={ classnames( 'error', {
-							'vis-hidden': isDomainValid || isDomainValid === undefined,
+							'vis-hidden': ! showError,
 						} ) }
 					>
 						<Icon icon={ info } size={ 20 } />{ ' ' }
-						{ translate( 'Please enter a valid website address' ) }
+						{ isDomainValid === false && translate( 'Please enter a valid website address' ) }
+						{ domainFetchingError && domainFetchingError.message }
 					</p>
 				</div>
 			</form>
