b2c0ec4a6c2f35026efbacbbbfde60e7587961bb
Eru Penkman <roo2@users.noreply.github.com>
2023-10-02 22:36:07 +1000

    reader: Fix posts with oddly sized images (#81885)
    * use getImagesFromPostToDisplay check to determine gallery eligibility
    * use constant for max gallery images

--- client/state/reader/posts/sizes.js
+++ client/state/reader/posts/sizes.js
@@ -9,6 +9,7 @@ export const READER_RELATED_IMAGE_WIDTH = 348;
 export const PHOTO_ONLY_MIN_WIDTH = 440;
 export const PHOTO_ONLY_MAX_CHARACTER_COUNT = 85;
 export const GALLERY_MIN_IMAGES = 5;
+export const GALLERY_MAX_IMAGES = 10;
 export const GALLERY_MIN_IMAGE_WIDTH = 300;
 export const MIN_IMAGE_WIDTH = 144;
 export const MIN_IMAGE_HEIGHT = 72;
