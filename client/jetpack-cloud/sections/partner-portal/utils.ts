bdc689ac6eb85174e43f4dd4915241fd27bb2fea
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-10-02 14:44:32 -0500

    isWpcomHostingProduct: Move to separate file in ./lib

--- client/jetpack-cloud/sections/partner-portal/utils.ts
+++ client/jetpack-cloud/sections/partner-portal/utils.ts
@@ -15,21 +15,11 @@ export { default as getProductTitle } from './lib/get-product-title';
 export { default as selectAlphabeticallySortedProductOptions } from './lib/select-alphabetically-sorted-product-options';
 export { default as isJetpackBundle } from './lib/is-jetpack-bundle';
 export { default as isWooCommerceProduct } from './lib/is-woocommerce-product';
+export { default as isWpcomHostingProduct } from './lib/is-wpcom-hosting-product';
 export { default as areLicenseKeysAssignableToMultisite } from './lib/are-license-keys-assignable-to-multisite';
 
 /**
- * Provided a license key or a product slug, can we trust that the product is a WordPress.com hosting product
- *
- * @param keyOrSlug string
- * @returns boolean True if wpcom hosting product, false if not
- */
-export function isWpcomHostingProduct( keyOrSlug: string ) {
-	return keyOrSlug.startsWith( 'wpcom-hosting' );
-}
-
-/**
  * Provided a license key, return the product slug
- *
  * @param licenseKey string
  * @returns string Product slug
  */
