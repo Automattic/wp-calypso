5ddc1261a9194f8eccd8641d03a4e45e80e99f2e
Addison-Stavlo <stavz01@gmail.com>
2023-10-03 11:45:18 -0400

    better handle missing items

--- client/blocks/reader-post-card/byline.jsx
+++ client/blocks/reader-post-card/byline.jsx
@@ -56,6 +56,10 @@ class PostByline extends Component {
 		const streamUrl = getStreamUrl( feedId, siteId );
 		const siteIcon = get( site, 'icon.img' );
 
+		// Use the siteName if not showing it elsewhere, otherwise use the slug.
+		const bylineSiteName = ! showSiteName ? siteName : siteSlug;
+		const showDate = post.date && post.URL;
+
 		/* eslint-disable wpcalypso/jsx-gridicon-size */
 		return (
 			<div className="reader-post-card__byline ignore-click">
@@ -83,7 +87,7 @@ class PostByline extends Component {
 						</div>
 					) }
 					<div className="reader-post-card__author-and-timestamp">
-						{ ( shouldDisplayAuthor || ( post.date && post.URL ) ) && (
+						{ ( shouldDisplayAuthor || bylineSiteName || showDate ) && (
 							<span className="reader-post-card__byline-secondary">
 								{ shouldDisplayAuthor && (
 									<>
@@ -96,12 +100,12 @@ class PostByline extends Component {
 										>
 											{ post.author.name }
 										</ReaderAuthorLink>
-										{ post.date && post.URL && (
+										{ ( bylineSiteName || showDate ) && (
 											<span className="reader-post-card__byline-secondary-bullet">·</span>
 										) }
 									</>
 								) }
-								{ post.date && post.URL && (
+								{ bylineSiteName && (
 									<>
 										<a
 											className="reader-post-card__byline-secondary-item"
@@ -110,20 +114,23 @@ class PostByline extends Component {
 											target="_blank"
 											rel="noopener noreferrer"
 										>
-											{ /* Use the siteName if not showing it above, otherwise use the slug */ }
-											{ ! showSiteName ? siteName : siteSlug }
+											{ bylineSiteName }
 										</a>
-										<span className="reader-post-card__byline-secondary-bullet">·</span>
-										<a
-											className="reader-post-card__byline-secondary-item"
-											onClick={ this.recordDateClick }
-											href={ post.URL }
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											<TimeSince date={ post.date } />
-										</a>
+										{ showDate && (
+											<span className="reader-post-card__byline-secondary-bullet">·</span>
+										) }
 									</>
+								) }
+								{ showDate && (
+									<a
+										className="reader-post-card__byline-secondary-item"
+										onClick={ this.recordDateClick }
+										href={ post.URL }
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<TimeSince date={ post.date } />
+									</a>
 								) }
 							</span>
 						) }
