d2c223a4a33a98c5d876aca333b5e3a33d768cf5
Dale du Preez <dale@automattic.com>
2023-10-04 17:11:44 +0200

    Explore adding some basic context to the launchpad navigator

--- packages/launchpad-navigator/src/floating-navigator/index.tsx
+++ packages/launchpad-navigator/src/floating-navigator/index.tsx
@@ -1,7 +1,8 @@
-import { Card } from '@automattic/components';
+import { Card, Button } from '@automattic/components';
 import { LaunchpadNavigator } from '@automattic/data-stores';
 import { DefaultWiredLaunchpad } from '@automattic/launchpad';
 import { select } from '@wordpress/data';
+import { useTranslate } from 'i18n-calypso';
 
 import './style.scss';
 
@@ -11,6 +12,7 @@ export type FloatingNavigatorProps = {
 
 const FloatingNavigator = ( { siteSlug }: FloatingNavigatorProps ) => {
 	const launchpadContext = 'launchpad-navigator';
+	const translate = useTranslate();
 	const checklistSlug = select( LaunchpadNavigator.store ).getActiveChecklistSlug() || null;
 
 	if ( ! checklistSlug ) {
@@ -19,11 +21,15 @@ const FloatingNavigator = ( { siteSlug }: FloatingNavigatorProps ) => {
 
 	return (
 		<Card className="launchpad-navigator__floating-navigator">
+			<h2>{ translate( 'Next steps for your site' ) }</h2>
 			<DefaultWiredLaunchpad
 				siteSlug={ siteSlug }
 				checklistSlug={ checklistSlug }
 				launchpadContext={ launchpadContext }
 			/>
+			<div className="launchpad-navigator__floating-navigator-actions">
+				<Button disabled>{ translate( 'Older tasks' ) }</Button>
+			</div>
 		</Card>
 	);
 };
