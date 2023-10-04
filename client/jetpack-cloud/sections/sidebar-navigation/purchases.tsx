79d0bfd344dccccd92f77f9bfa6b1fa7f8399c7c
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-10-04 14:08:32 -0500

    Jetpack Manage: Add placeholder for new Purchases sidebar nav

---
+++ client/jetpack-cloud/sections/sidebar-navigation/purchases.tsx
@@ -0,0 +1,8 @@
+import NewSidebar from 'calypso/jetpack-cloud/components/sidebar';
+
+// This sidebar is what Jetpack Manage customers will see when they select the
+// Purchases item from the top-level navigation sidebar. It'll display options
+// like Licenses, Invoices, Billing, etc.
+const PurchasesSidebar = () => <NewSidebar />;
+
+export default PurchasesSidebar;
