d6ca70720aa39314fd48fe126f394be14a765e77
Valter Lorran <valterlorran@hotmail.com>
2023-10-04 14:37:13 -0300

    Add the task actions to the navigator

--- packages/launchpad-navigator/src/floating-navigator/index.tsx
+++ packages/launchpad-navigator/src/floating-navigator/index.tsx
@@ -1,5 +1,8 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { Card } from '@automattic/components';
-import { Launchpad } from '@automattic/launchpad';
+import { LaunchpadNavigator, useLaunchpad } from '@automattic/data-stores';
+import { Launchpad, type Task, setUpActionsForTasks } from '@automattic/launchpad';
+import { useDispatch } from '@wordpress/data';
 
 import './style.scss';
 
@@ -8,9 +11,32 @@ export type FloatingNavigatorProps = {
 };
 
 const FloatingNavigator = ( { siteSlug }: FloatingNavigatorProps ) => {
+	const launchpadContext = 'navigator';
+	const checklistSlug = 'intent-build';
+
+	const {
+		data: { checklist },
+	} = useLaunchpad( siteSlug, checklistSlug );
+
+	const tasklistCompleted = checklist?.every( ( task: Task ) => task.completed ) || false;
+
+	const tracksData = { recordTracksEvent, checklistSlug, tasklistCompleted, launchpadContext };
+
+	const { setActiveChecklist } = useDispatch( LaunchpadNavigator.store );
+
+	const taskFilter = ( tasks: Task[] ) => {
+		return setUpActionsForTasks( {
+			tasks,
+			siteSlug,
+			tracksData,
+			extraActions: {
+				setActiveChecklist,
+			},
+		} );
+	};
 	return (
 		<Card className="launchpad-navigator__floating-navigator">
-			<Launchpad siteSlug={ siteSlug } checklistSlug="intent-build" />
+			<Launchpad siteSlug={ siteSlug } checklistSlug={ checklistSlug } taskFilter={ taskFilter } />
 		</Card>
 	);
 };
