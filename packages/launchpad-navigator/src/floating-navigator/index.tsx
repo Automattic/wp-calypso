d75809ec1fa77857b4c70c62bbb868939a8c75a5
Valter Lorran <valterlorran@hotmail.com>
2023-10-04 17:04:39 -0300

    Add a wrapper to the Launchpad component to prevent code repetitions

--- packages/launchpad-navigator/src/floating-navigator/index.tsx
+++ packages/launchpad-navigator/src/floating-navigator/index.tsx
@@ -1,8 +1,5 @@
-import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { Card } from '@automattic/components';
-import { LaunchpadNavigator, useLaunchpad } from '@automattic/data-stores';
-import { Launchpad, type Task, setUpActionsForTasks } from '@automattic/launchpad';
-import { useDispatch } from '@wordpress/data';
+import { LaunchpadWrapper } from '@automattic/launchpad';
 
 import './style.scss';
 
@@ -14,29 +11,13 @@ const FloatingNavigator = ( { siteSlug }: FloatingNavigatorProps ) => {
 	const launchpadContext = 'navigator';
 	const checklistSlug = 'intent-build';
 
-	const {
-		data: { checklist },
-	} = useLaunchpad( siteSlug, checklistSlug );
-
-	const tasklistCompleted = checklist?.every( ( task: Task ) => task.completed ) || false;
-
-	const tracksData = { recordTracksEvent, checklistSlug, tasklistCompleted, launchpadContext };
-
-	const { setActiveChecklist } = useDispatch( LaunchpadNavigator.store );
-
-	const taskFilter = ( tasks: Task[] ) => {
-		return setUpActionsForTasks( {
-			tasks,
-			siteSlug,
-			tracksData,
-			extraActions: {
-				setActiveChecklist,
-			},
-		} );
-	};
 	return (
 		<Card className="launchpad-navigator__floating-navigator">
-			<Launchpad siteSlug={ siteSlug } checklistSlug={ checklistSlug } taskFilter={ taskFilter } />
+			<LaunchpadWrapper
+				siteSlug={ siteSlug }
+				checklistSlug={ checklistSlug }
+				launchpadContext={ launchpadContext }
+			/>
 		</Card>
 	);
 };
