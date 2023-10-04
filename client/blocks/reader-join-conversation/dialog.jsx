1ef0d82f5a2b1a24f737620943a29ff9f8562ac5
Eoin Gallagher <eoin@automattic.com>
2023-10-04 14:20:25 +0100

    stash

--- client/blocks/reader-join-conversation/dialog.jsx
+++ client/blocks/reader-join-conversation/dialog.jsx
@@ -3,22 +3,15 @@ import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import { useState } from 'react';
 import LoginBlock from 'calypso/blocks/login';
+//import WPLogin from 'calypso/login/wp-login';
 
 import './style.scss';
 
 const ReaderJoinConversationDialog = ( { onClose, isVisible } ) => {
 	const translate = useTranslate();
-	// use state to track if button is clicked
-	// if button is clicked, show login form
-	// if login form is submitted, close dialog
-	// if login form is closed, close dialog
-	// if dialog is closed, reset state
 	const [ isLoginVisible, setIsLoginVisible ] = useState( false );
 	const showLogin = () => {
 		setIsLoginVisible( true );
-	};
-	const hideLogin = () => {
-		setIsLoginVisible( false );
 	};
 	return (
 		<Dialog
@@ -37,21 +30,18 @@ const ReaderJoinConversationDialog = ( { onClose, isVisible } ) => {
 						{ translate( 'Sign in to like, comment, reblog, and follow your favorite blogs.' ) }
 					</p>
 					<Button
-						isPrimary
+						isTertiary
 						onClick={ showLogin }
 						className="reader-join-conversation-dialog__create-account-button"
 					>
 						{ translate( 'Create a new account' ) }
 					</Button>
+					<Button isLink onClick={ showLogin } className="reader-join-conversation-dialog__login">
+						{ translate( 'Log in' ) }
+					</Button>
 				</div>
 			) }
-			{ isLoginVisible && (
-				<LoginBlock
-					className="reader-join-conversation-dialog__login-block"
-					onClose={ hideLogin }
-					onSuccess={ hideLogin }
-				/>
-			) }
+			{ isLoginVisible && <LoginBlock disableAutoFocus /> }
 		</Dialog>
 	);
 };
