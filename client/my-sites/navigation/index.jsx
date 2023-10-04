6d67d37ebe4fc28f3cb56a637dabf45f2a9d20b7
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-10-04 14:08:13 -0500

    Jetpack Cloud: Add (flag-gated) new sidebar nav for when a site is selected

--- client/my-sites/navigation/index.jsx
+++ client/my-sites/navigation/index.jsx
@@ -22,7 +22,16 @@ class MySitesNavigation extends Component {
 		let sitePickerProps = {};
 
 		if ( config.isEnabled( 'jetpack-cloud' ) ) {
-			asyncSidebar = <AsyncLoad require="calypso/components/jetpack/sidebar" { ...asyncProps } />;
+			if ( config.isEnabled( 'jetpack/new-navigation' ) ) {
+				asyncSidebar = (
+					<AsyncLoad
+						require="calypso/jetpack-cloud/sections/sidebar-navigation/manage-selected-site"
+						{ ...asyncProps }
+					/>
+				);
+			} else {
+				asyncSidebar = <AsyncLoad require="calypso/components/jetpack/sidebar" { ...asyncProps } />;
+			}
 
 			sitePickerProps = {
 				showManageSitesButton: false,
