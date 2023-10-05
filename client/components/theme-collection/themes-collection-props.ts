2dfbcab561cae7fbc733595e237601ba405942eb
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2023-10-05 19:22:27 +0300

    Remove unused props script.

--- client/components/theme-collection/themes-collection-props.ts
+++
@@ -1,8 +0,0 @@
-export interface ThemesCollectionProps {
-	getActionLabel: () => string;
-	getButtonOptions: () => void;
-	getPrice: () => string;
-	getScreenshotUrl: ( themeId: string ) => string;
-	isActive: () => boolean;
-	isInstalling: () => boolean;
-}
