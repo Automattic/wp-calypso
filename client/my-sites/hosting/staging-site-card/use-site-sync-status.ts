082c495e600941504292415ee1e66aa5c1697957
Antony Agrios <antonyagrios@gmail.com>
2023-10-09 14:46:15 +0300

    Update: Add success notice and refactor
    - Eliminate duplicate logic by refactor sync component
    - Add success notice.

--- client/my-sites/hosting/staging-site-card/use-site-sync-status.ts
+++ client/my-sites/hosting/staging-site-card/use-site-sync-status.ts
@@ -1,11 +1,13 @@
 import { useTranslate } from 'i18n-calypso';
 import { useEffect, useMemo, useCallback, useRef } from 'react';
 import { useDispatch, useSelector } from 'calypso/state';
+import { removeNotice, successNotice } from 'calypso/state/notices/actions';
 import { fetchSyncStatus, setSyncInProgress } from 'calypso/state/sync/actions';
 import { SiteSyncStatus } from 'calypso/state/sync/constants';
 import { getSyncStatus, getSyncProgress, getSyncSiteType } from 'calypso/state/sync/selectors';
 import { getIsSyncingInProgress } from 'calypso/state/sync/selectors/get-is-syncing-in-progress';
 import { getSyncStatusError } from 'calypso/state/sync/selectors/get-sync-status-error';
+const stagingSiteSyncSuccess = 'staging-site-sync-success';
 
 export const useCheckSyncStatus = ( siteId: number ) => {
 	const dispatch = useDispatch();
@@ -32,6 +34,12 @@ export const useCheckSyncStatus = ( siteId: number ) => {
 			return;
 		}
 
+		if ( syncStatus === SiteSyncStatus.COMPLETED ) {
+			dispatch(
+				successNotice( translate( 'Site synced successfully.' ), { id: stagingSiteSyncSuccess } )
+			);
+		}
+
 		if ( isSyncInProgress === true && ! intervalIdRef.current ) {
 			intervalIdRef.current = setInterval( () => dispatch( fetchSyncStatus( siteId ) ), 5000 );
 		}
@@ -45,6 +53,7 @@ export const useCheckSyncStatus = ( siteId: number ) => {
 		}
 		clearIntervalId();
 		dispatch( setSyncInProgress( siteId, true ) );
+		dispatch( removeNotice( stagingSiteSyncSuccess ) );
 	}, [ dispatch, siteId, clearIntervalId ] );
 
 	// Fetch the status once on mount to avoid waiting the interval delay
