ec86373da782fd46fa2861ae70a43a951fa18ee5
Paulo Marcos Trentin <paulo@paulotrentin.com.br>
2023-10-09 08:41:16 -0300

    Updated the logic to only remove primary-cta when the mini-cart is showing (#82763)

--- client/components/domains/domain-registration-suggestion/index.jsx
+++ client/components/domains/domain-registration-suggestion/index.jsx
@@ -18,6 +18,7 @@ import {
 	getDomainPriceRule,
 	hasDomainInCart,
 	isPaidDomain,
+	getDomainRegistrations,
 } from 'calypso/lib/cart-values/cart-items';
 import {
 	getDomainPrice,
@@ -192,7 +193,10 @@ class DomainRegistrationSuggestion extends Component {
 			buttonStyles = { ...buttonStyles, disabled: true };
 		}
 
-		if ( shouldUseMultipleDomainsInCart( flowName, suggestion ) ) {
+		if (
+			shouldUseMultipleDomainsInCart( flowName, suggestion ) &&
+			getDomainRegistrations( cart ).length > 0
+		) {
 			buttonStyles = { ...buttonStyles, primary: false };
 		}
 
