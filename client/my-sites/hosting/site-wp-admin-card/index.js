945fe589525f75dca284aaa8d3d7d2b3c0887639
Kateryna Kodonenko <kateryna@automattic.com>
2023-10-10 14:27:03 +0200

    Add toggle to the wp-admin card

--- client/my-sites/hosting/site-wp-admin-card/index.js
+++ client/my-sites/hosting/site-wp-admin-card/index.js
@@ -1,13 +1,28 @@
 /* eslint-disable wpcalypso/jsx-gridicon-size */
 import { Card, Gridicon } from '@automattic/components';
-import { CardBody } from '@wordpress/components';
+import styled from '@emotion/styled';
+import { CardBody, ToggleControl } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import CardHeading from 'calypso/components/card-heading';
 
-import './style.scss';
+const ToggleContainer = styled.div( {
+	fontSize: '14px',
+	label: {
+		fontSize: '14px',
+	},
+} );
+
+const ToggleLabel = styled.p( {
+	marginBottom: '9px',
+	fontWeight: 600,
+	fontSize: '14px',
+} );
 
 export default function SiteWpAdminCard() {
 	const translate = useTranslate();
+	const defaultWpAdminToggleDescription = translate(
+		'Default the site interface and navigation to wp-admin'
+	);
 	return (
 		<Card className="sitewpadmin-card">
 			<Gridicon icon="my-sites" size={ 32 } />
@@ -16,6 +31,10 @@ export default function SiteWpAdminCard() {
 			</CardHeading>
 			<CardBody>
 				<p>{ translate( 'Switch your site interface and navigation to wp-admin.' ) }</p>
+				<ToggleContainer>
+					<ToggleLabel>{ translate( 'Default to wp-admin' ) }</ToggleLabel>
+					<ToggleControl label={ defaultWpAdminToggleDescription } />
+				</ToggleContainer>
 			</CardBody>
 		</Card>
 	);
