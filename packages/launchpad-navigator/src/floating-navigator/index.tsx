73df64b5d15e01ed50541ab87ef4c7f0627c6e1a
Dale du Preez <dale@automattic.com>
2023-10-10 17:06:00 +0200

    Add X to close Launchpad Navigator modal

--- packages/launchpad-navigator/src/floating-navigator/index.tsx
+++ packages/launchpad-navigator/src/floating-navigator/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Button } from '@automattic/components';
+import { Card, Button, Gridicon } from '@automattic/components';
 import { LaunchpadNavigator } from '@automattic/data-stores';
 import { DefaultWiredLaunchpad } from '@automattic/launchpad';
 import { select } from '@wordpress/data';
@@ -6,11 +6,14 @@ import { useTranslate } from 'i18n-calypso';
 
 import './style.scss';
 
+type ToggleLaunchpadIsVisible = ( shouldBeVisible: boolean ) => void;
+
 export type FloatingNavigatorProps = {
 	siteSlug: string | null;
+	toggleLaunchpadIsVisible?: ToggleLaunchpadIsVisible;
 };
 
-const FloatingNavigator = ( { siteSlug }: FloatingNavigatorProps ) => {
+const FloatingNavigator = ( { siteSlug, toggleLaunchpadIsVisible }: FloatingNavigatorProps ) => {
 	const launchpadContext = 'launchpad-navigator';
 	const translate = useTranslate();
 	const checklistSlug = select( LaunchpadNavigator.store ).getActiveChecklistSlug() || null;
@@ -19,9 +22,21 @@ const FloatingNavigator = ( { siteSlug }: FloatingNavigatorProps ) => {
 		return null;
 	}
 
+	const setLaunchpadIsVisible = toggleLaunchpadIsVisible || ( () => {} );
+
 	return (
 		<Card className="launchpad-navigator__floating-navigator">
-			<h2>{ translate( 'Next steps for your site' ) }</h2>
+			<div className="launchpad-navigator__floating-navigator-header">
+				<h2>{ translate( 'Next steps for your site' ) }</h2>
+				<Button
+					aria-label={ translate( 'Close task list modal' ) }
+					borderless
+					className="launchpad-navigator__floating-navigator-close-button"
+					onClick={ () => setLaunchpadIsVisible( false ) }
+				>
+					<Gridicon icon="cross" size={ 18 } />
+				</Button>
+			</div>
 			<DefaultWiredLaunchpad
 				siteSlug={ siteSlug }
 				checklistSlug={ checklistSlug }
