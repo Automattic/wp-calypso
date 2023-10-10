8ca2755da6f11c74b1adec4a3767f37c7ce4231c
Yashwin <yashwinpoojary@gmail.com>
2023-10-10 15:18:48 +0530

    Create a component to show guided tours

---
+++ client/jetpack-cloud/components/guided-tour/index.tsx
@@ -0,0 +1,92 @@
+import { Popover, Button } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'calypso/state';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import { getJetpackDashboardPreference as getPreference } from 'calypso/state/jetpack-agency-dashboard/selectors';
+import { savePreference } from 'calypso/state/preferences/actions';
+import { preferencesLastFetchedTimestamp } from 'calypso/state/preferences/selectors';
+
+import './style.scss';
+
+export interface Tour {
+	id: string;
+	title: string;
+	description: string;
+}
+
+interface Props {
+	tours: Tour[];
+	preferenceName: string;
+}
+
+const GuidedTour = ( { tours, preferenceName }: Props ) => {
+	const translate = useTranslate();
+	const dispatch = useDispatch();
+
+	const [ currentStep, setCurrentStep ] = useState( 0 );
+	const [ targetElement, setTargetElement ] = useState< HTMLElement | null >( null );
+	const [ isVisible, setIsVisible ] = useState( false );
+
+	const preference = useSelector( ( state ) => getPreference( state, preferenceName ) );
+	const hasFetched = !! useSelector( preferencesLastFetchedTimestamp );
+
+	const isDismissed = preference?.dismiss;
+
+	const { title, description, id } = tours[ currentStep ];
+
+	// Set the target element for the popover
+	useEffect( () => {
+		if ( id ) {
+			const element = document.querySelector( `#${ id }` ) as HTMLElement | null;
+			setTargetElement( element );
+		}
+	}, [ id ] );
+
+	// Show the popover after a short delay to allow the popover to be positioned correctly
+	useEffect( () => {
+		if ( targetElement && ! isDismissed && hasFetched ) {
+			setTimeout( () => {
+				setIsVisible( true );
+				dispatch(
+					recordTracksEvent( 'calypso_jetpack_cloud_start_tour', {
+						tour: preferenceName,
+					} )
+				);
+			}, 100 );
+		}
+	}, [ dispatch, isDismissed, preferenceName, targetElement, hasFetched ] );
+
+	const endTour = () => {
+		dispatch( savePreference( preferenceName, { ...preference, dismiss: true } ) );
+		dispatch(
+			recordTracksEvent( 'calypso_jetpack_cloud_end_tour', {
+				tour: preferenceName,
+			} )
+		);
+	};
+
+	const nextStep = () => {
+		if ( currentStep < tours.length - 1 ) {
+			setCurrentStep( currentStep + 1 );
+		} else {
+			endTour();
+		}
+	};
+
+	if ( isDismissed ) {
+		return null;
+	}
+
+	return (
+		<Popover isVisible={ isVisible } context={ targetElement } className="guided-tour__popover">
+			<h2 className="guided-tour__popover-heading">{ title }</h2>
+			<p className="guided-tour__popover-description">{ description }</p>
+			<Button className="guided-tour__popover-button" onClick={ nextStep }>
+				{ translate( 'Got it' ) }
+			</Button>
+		</Popover>
+	);
+};
+
+export default GuidedTour;
