c92d106d9d7aad07591953e36a73b4c37f6c540e
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-10-11 15:21:00 +0200

    Invert flow check and prioritize hosting-site-flow

--- client/landing/stepper/declarative-flow/internals/steps-repository/trial-acknowledge/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/trial-acknowledge/index.tsx
@@ -1,29 +1,22 @@
-import { StepContainer, isAnyMigrationFlow } from '@automattic/onboarding';
-import { useSelect } from '@wordpress/data';
-import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
+import { StepContainer, NEW_HOSTED_SITE_FLOW } from '@automattic/onboarding';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { HostingTrialAcknowledge } from './hosting-trial-acknowledge';
 import { MigrationTrialAcknowledge } from './migration-trial-acknowledge';
-import type { OnboardSelect } from '@automattic/data-stores';
 import type { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
 import './style.scss';
 
 const TrialAcknowledge: Step = function TrialAcknowledge( { navigation, flow, stepName } ) {
 	const { goBack } = navigation;
-	const intent = useSelect(
-		( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getIntent(),
-		[]
-	);
 
 	const getStepContent = () => {
-		if ( isAnyMigrationFlow( flow ) || intent === 'import' ) {
+		if ( flow === NEW_HOSTED_SITE_FLOW ) {
 			return (
-				<MigrationTrialAcknowledge flow={ flow } stepName={ stepName } navigation={ navigation } />
+				<HostingTrialAcknowledge flow={ flow } navigation={ navigation } stepName={ stepName } />
 			);
 		}
 
 		return (
-			<HostingTrialAcknowledge flow={ flow } navigation={ navigation } stepName={ stepName } />
+			<MigrationTrialAcknowledge flow={ flow } stepName={ stepName } navigation={ navigation } />
 		);
 	};
 
