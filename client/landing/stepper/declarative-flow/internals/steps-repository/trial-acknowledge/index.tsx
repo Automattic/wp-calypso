3e7b5c9790b8f448778c318ac8e6652a53e6c224
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-10-11 12:53:37 +0200

    Stepper: site-setup-flow: fix showing a proper TrialAcknowledge CTA

--- client/landing/stepper/declarative-flow/internals/steps-repository/trial-acknowledge/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/trial-acknowledge/index.tsx
@@ -1,15 +1,22 @@
 import { StepContainer, isAnyMigrationFlow } from '@automattic/onboarding';
+import { useSelect } from '@wordpress/data';
+import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { HostingTrialAcknowledge } from './hosting-trial-acknowledge';
 import { MigrationTrialAcknowledge } from './migration-trial-acknowledge';
+import type { OnboardSelect } from '@automattic/data-stores';
 import type { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
 import './style.scss';
 
 const TrialAcknowledge: Step = function TrialAcknowledge( { navigation, flow, stepName } ) {
 	const { goBack } = navigation;
+	const intent = useSelect(
+		( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getIntent(),
+		[]
+	);
 
 	const getStepContent = () => {
-		if ( isAnyMigrationFlow( flow ) ) {
+		if ( isAnyMigrationFlow( flow ) || intent === 'import' ) {
 			return (
 				<MigrationTrialAcknowledge flow={ flow } stepName={ stepName } navigation={ navigation } />
 			);
