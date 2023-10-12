0f713b7195180271d70b569763497cc5bb809f24
Wayne Wang <iverson0217@gmail.com>
2023-10-12 22:04:54 +0800

    Remove unused file

--- client/site-profiler/utils/trim-domain.ts
+++
@@ -1,7 +0,0 @@
-export default function trimDomain( value: string ) {
-	if ( ! value ) {
-		return '';
-	}
-	const decodedValue = decodeURIComponent( value );
-	return decodedValue.replace( /\s/g, '' );
-}
