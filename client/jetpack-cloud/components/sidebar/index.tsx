34463eda91e459bb2de8b268c2b9c0fb86ec12b0
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-10-12 17:50:06 +0800

    Add WP-Admin link.

--- client/jetpack-cloud/components/sidebar/index.tsx
+++ client/jetpack-cloud/components/sidebar/index.tsx
@@ -1,8 +1,14 @@
 import classNames from 'classnames';
+import { useTranslate } from 'i18n-calypso';
+import JetpackIcons from 'calypso/components/jetpack/sidebar/menu-items/jetpack-icons';
 import SiteSelector from 'calypso/components/site-selector';
+import SidebarItem from 'calypso/layout/sidebar/item';
 import Sidebar from 'calypso/layout/sidebar-v2';
 import SidebarFooter from 'calypso/layout/sidebar-v2/footer';
 import SidebarMain from 'calypso/layout/sidebar-v2/main';
+import { useSelector } from 'calypso/state';
+import getJetpackAdminUrl from 'calypso/state/sites/selectors/get-jetpack-admin-url';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import Header from './header';
 
 import './style.scss';
@@ -15,36 +21,55 @@ type Props = {
 	className?: string;
 	isJetpackManage?: boolean;
 };
-const JetpackCloudSidebar = ( { className, isJetpackManage = false }: Props ) => (
-	<Sidebar className={ classNames( 'jetpack-cloud-sidebar', className ) }>
-		<Header forceAllSitesView={ isJetpackManage } />
+const JetpackCloudSidebar = ( { className, isJetpackManage }: Props ) => {
+	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
+	const jetpackAdminUrl = useSelector( ( state ) =>
+		siteId ? getJetpackAdminUrl( state, siteId ) : null
+	);
 
-		<SidebarMain>
-			<ul role="menu" className="jetpack-cloud-sidebar__navigation-list">
-				<li
-					className={ classNames(
-						'jetpack-cloud-sidebar__navigation-item',
-						'jetpack-cloud-sidebar__navigation-item--highlighted'
-					) }
-				>
-					Navigation items
-				</li>
-				<li className="jetpack-cloud-sidebar__navigation-item">Will go here</li>
-			</ul>
-		</SidebarMain>
+	const translate = useTranslate();
 
-		<SidebarFooter>Footer v2</SidebarFooter>
+	return (
+		<Sidebar className={ classNames( 'jetpack-cloud-sidebar', className ) }>
+			<Header forceAllSitesView={ isJetpackManage } />
 
-		<SiteSelector
-			showAddNewSite
-			showAllSites={ isJetpackManage }
-			isJetpackAgencyDashboard={ isJetpackManage }
-			className="jetpack-cloud-sidebar__site-selector"
-			allSitesPath="/dashboard"
-			siteBasePath="/landing"
-			wpcomSiteBasePath="https://wordpress.com/home"
-		/>
-	</Sidebar>
-);
+			<SidebarMain>
+				<ul role="menu" className="jetpack-cloud-sidebar__navigation-list">
+					<li
+						className={ classNames(
+							'jetpack-cloud-sidebar__navigation-item',
+							'jetpack-cloud-sidebar__navigation-item--highlighted'
+						) }
+					>
+						Navigation items
+					</li>
+					<li className="jetpack-cloud-sidebar__navigation-item">Will go here</li>
+				</ul>
+			</SidebarMain>
+
+			<SidebarFooter>
+				{ jetpackAdminUrl && (
+					<SidebarItem
+						label={ translate( 'WP Admin', {
+							comment: 'Jetpack Cloud sidebar navigation item',
+						} ) }
+						link={ jetpackAdminUrl }
+						customIcon={ <JetpackIcons icon="wordpress" /> }
+					/>
+				) }
+			</SidebarFooter>
+
+			<SiteSelector
+				showAddNewSite
+				showAllSites={ isJetpackManage }
+				isJetpackAgencyDashboard={ isJetpackManage }
+				className="jetpack-cloud-sidebar__site-selector"
+				allSitesPath="/dashboard"
+				siteBasePath="/landing"
+				wpcomSiteBasePath="https://wordpress.com/home"
+			/>
+		</Sidebar>
+	);
+};
 
 export default JetpackCloudSidebar;
