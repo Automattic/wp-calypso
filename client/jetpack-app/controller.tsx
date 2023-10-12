5a7dbaa1eddf9765d643683d9433a3c13e8534c9
Povilas Staskus <povilas.staskus@gmail.com>
2023-10-12 11:23:57 +0300

    Redirect to home if jetpack-app/ route is not accessed via the app

--- client/jetpack-app/controller.tsx
+++ client/jetpack-app/controller.tsx
@@ -1,3 +1,5 @@
+import page from 'page';
+import { isWpMobileApp } from 'calypso/lib/mobile-app';
 import JetpackAppPlans from './plans/main';
 
 export function jetpackAppPlans( context: PageJS.Context, next: () => void ) {
@@ -9,4 +11,12 @@ export function jetpackAppPlans( context: PageJS.Context, next: () => void ) {
 	);
 
 	next();
+}
+
+export function redirectIfNotJetpackApp( _context: PageJS.Context, next: () => void ) {
+	if ( ! isWpMobileApp() ) {
+		page.redirect( '/' );
+	} else {
+		next();
+	}
 }
--- client/jetpack-app/index.ts
+++ client/jetpack-app/index.ts
@@ -1,10 +1,16 @@
 import page from 'page';
 import { render as clientRender } from 'calypso/controller';
-import { jetpackAppPlans } from './controller';
+import { jetpackAppPlans, redirectIfNotJetpackApp } from './controller';
 import { makeJetpackAppLayout } from './page-middleware/layout';
 
 export default function () {
-	page( '/jetpack-app/plans', jetpackAppPlans, makeJetpackAppLayout, clientRender );
+	page(
+		'/jetpack-app/plans',
+		redirectIfNotJetpackApp,
+		jetpackAppPlans,
+		makeJetpackAppLayout,
+		clientRender
+	);
 
 	// Default to /plans page until we have more pages
 	page( '/jetpack-app', '/jetpack-app/plans' );
