e83c4fa87192e83198e4ac5caedc929c6c8345de
Eoin Gallagher <eoin@automattic.com>
2023-10-12 14:25:20 +0100

    Update comment form likes to use join conversation dialog too

--- client/blocks/comments/post-comment.jsx
+++ client/blocks/comments/post-comment.jsx
@@ -1,3 +1,4 @@
+import config from '@automattic/calypso-config';
 import { getUrlParts } from '@automattic/calypso-url';
 import { Gridicon } from '@automattic/components';
 import classnames from 'classnames';
@@ -7,6 +8,7 @@ import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import ConversationCaterpillar from 'calypso/blocks/conversation-caterpillar';
+import ReaderJoinConversationDialog from 'calypso/blocks/reader-join-conversation/dialog';
 import Gravatar from 'calypso/components/gravatar';
 import TimeSince from 'calypso/components/time-since';
 import { decodeEntities } from 'calypso/lib/formatting';
@@ -99,16 +101,33 @@ class PostComment extends PureComponent {
 	state = {
 		showReplies: false,
 		showFull: false,
+		showJoinConversationModal: false,
 	};
 
 	handleToggleRepliesClick = () => {
 		this.setState( { showReplies: ! this.state.showReplies } );
 	};
 
+	hideJoinConversationModal = () => {
+		this.setState( { showJoinConversationModal: false } );
+	};
+
+	showJoinConversationModal = () => {
+		this.setState( { showJoinConversationModal: true } );
+	};
+
+	onLoggedOut = () => {
+		if ( config.isEnabled( 'reader/login-window' ) ) {
+			return this.showJoinConversationModal();
+		}
+		// Redirect to create account page
+		const { pathname } = getUrlParts( window.location.href );
+		return navigate( createAccountUrl( { redirectTo: pathname, ref: 'reader-lp' } ) );
+	};
+
 	handleReply = () => {
 		if ( ! this.props.isLoggedIn ) {
-			const { pathname } = getUrlParts( window.location.href );
-			return navigate( createAccountUrl( { redirectTo: pathname, ref: 'reader-lp' } ) );
+			return this.onLoggedOut();
 		}
 		this.props.onReplyClick( this.props.commentId );
 		this.setState( { showReplies: true } ); // show the comments when replying
@@ -462,6 +481,7 @@ class PostComment extends PureComponent {
 					activeReplyCommentId={ this.props.activeReplyCommentId }
 					commentId={ this.props.commentId }
 					handleReply={ this.handleReply }
+					onLoggedOut={ this.onLoggedOut }
 					onReplyCancel={ this.props.onReplyCancel }
 					showReadMore={ overflowY && ! this.state.showFull && showReadMoreInActions }
 					onReadMore={ this.onReadMore }
@@ -477,6 +497,12 @@ class PostComment extends PureComponent {
 					/>
 				) }
 				{ this.renderRepliesList() }
+				{ config.isEnabled( 'reader/login-window' ) && (
+					<ReaderJoinConversationDialog
+						onClose={ this.hideJoinConversationModal }
+						isVisible={ this.state.showJoinConversationModal }
+					/>
+				) }
 			</li>
 		);
 	}
