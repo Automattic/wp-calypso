c41b79a5e8c6e658ce7d162bf39846ccd2c13ee9
Edwin Takahashi <egao@outlook.com>
2023-10-13 15:44:38 +0900

    test/e2e/specs/tools/advertising__promote.ts (#82974)
    - don't define a new variable

--- test/e2e/specs/tools/advertising__promote.ts
+++ test/e2e/specs/tools/advertising__promote.ts
@@ -34,9 +34,8 @@ declare const browser: Browser;
 skipDescribeIf( envVariables.ATOMIC_VARIATION === 'private' )(
 	DataHelper.createSuiteTitle( 'Advertising: Promote' ),
 	function () {
-		const pageTitle = DataHelper.getRandomPhrase();
 		// The input has a limit, so let's stay way under to be safe!
-		const pageTitleInAd = pageTitle.slice( 0, 20 );
+		const pageTitle = DataHelper.getRandomPhrase().slice( 0, 20 );
 		const snippet = Array( 2 ).fill( DataHelper.getRandomPhrase() ).toString();
 
 		let newPostDetails: PostResponse;
@@ -106,12 +105,12 @@ skipDescribeIf( envVariables.ATOMIC_VARIATION === 'private' )(
 		} );
 
 		it( 'Enter title and snippet', async function () {
-			await blazeCampaignPage.enterText( 'Page title', pageTitleInAd );
+			await blazeCampaignPage.enterText( 'Page title', pageTitle );
 			await blazeCampaignPage.enterText( 'Ad text', snippet );
 		} );
 
 		it( 'Validate preview', async function () {
-			await blazeCampaignPage.validatePreview( { title: pageTitleInAd, snippet: snippet } );
+			await blazeCampaignPage.validatePreview( { title: pageTitle, snippet: snippet } );
 		} );
 
 		afterAll( async function () {
