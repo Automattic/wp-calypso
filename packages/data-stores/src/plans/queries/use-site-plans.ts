018a61c97b576961f38bd6e1782e3e74f32fa180
Christos <chriskmnds@gmail.com>
2023-10-13 15:15:00 +0300

    Plans 2023: Increase staleTime in useSitePlans to avoid loader popping up

--- packages/data-stores/src/plans/queries/use-site-plans.ts
+++ packages/data-stores/src/plans/queries/use-site-plans.ts
@@ -68,7 +68,7 @@ function useSitePlans( { siteId }: Props ) {
 			}, {} );
 		}, [] ),
 		refetchOnWindowFocus: false,
-		staleTime: 1000 * 60 * 5, // 5 minutes
+		staleTime: 1000 * 60 * 50, // 50 minutes
 		enabled: !! siteId,
 	} );
 }
