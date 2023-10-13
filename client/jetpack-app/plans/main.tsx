024721487111371fb409fbf09d1da9bf26916a0e
Povilas Staskus <povilas.staskus@automattic.com>
2023-10-13 14:15:56 +0300

    Track free and paid plan selection in Jetpack App Plans Page (#82719)

--- client/jetpack-app/plans/main.tsx
+++ client/jetpack-app/plans/main.tsx
@@ -1,13 +1,14 @@
 import { getPlan, PLAN_FREE } from '@automattic/calypso-products';
 import { addQueryArgs } from '@wordpress/url';
 import { useTranslate } from 'i18n-calypso';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import QueryPlans from 'calypso/components/data/query-plans';
 import FormattedHeader from 'calypso/components/formatted-header';
 import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
 import Main from 'calypso/components/main';
 import { getPlanCartItem } from 'calypso/lib/cart-values/cart-items';
 import PlansFeaturesMain from 'calypso/my-sites/plans-features-main';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { getPlanSlug } from 'calypso/state/plans/selectors';
 import type { Plan } from '@automattic/calypso-products';
 import type { MinimalRequestCartProduct } from '@automattic/shopping-cart';
@@ -61,6 +62,7 @@ const Header: React.FC< HeaderProps > = ( { paidDomainName } ) => {
 };
 
 const JetpackAppPlans: React.FC< JetpackAppPlansProps > = ( { paidDomainName, originalUrl } ) => {
+	const dispatch = useDispatch();
 	const planSlug = useSelector( ( state: AppState ) =>
 		getPlanSlug( state, getPlan( PLAN_FREE )?.getProductId() || 0 )
 	) as string | null;
@@ -74,9 +76,20 @@ const JetpackAppPlans: React.FC< JetpackAppPlansProps > = ( { paidDomainName, or
 
 		if ( ! productSlug ) {
 			args = { plan_slug: PLAN_FREE };
+
+			dispatch(
+				recordTracksEvent( 'calypso_signup_free_plan_select', { from_section: 'default' } )
+			);
 		} else {
 			const plan = getPlan( productSlug ) as Plan;
 			args = { plan_id: plan.getProductId(), plan_slug: productSlug };
+
+			dispatch(
+				recordTracksEvent( 'calypso_signup_plan_select', {
+					product_slug: productSlug,
+					from_section: 'default',
+				} )
+			);
 		}
 
 		window.location.href = addQueryArgs( originalUrl, args );
