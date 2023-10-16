d6d3b478398deaa9e88f070bf1d475026666697a
Paulo Trentin <paulo@paulotrentin.com.br>
2023-10-16 17:19:13 -0300

    Created NavigationHeader component example for UI Components inside /devdocs

--- client/components/navigation-header/docs/example.jsx
+++ client/components/navigation-header/docs/example.jsx
@@ -1,25 +1,59 @@
-import styled from '@emotion/styled';
-import NavigationHeader from 'calypso/components/fixed-navigation-header';
+import { Button } from '@automattic/components';
+import { Component } from 'react';
+import NavigationHeader from 'calypso/components/navigation-header';
 
-const NavigationHeaderStyled = styled( NavigationHeader )`
-	position: relative;
-	top: inherit;
-	left: inherit;
-	width: 100%;
-`;
-
-const NavigationHeaderExample = () => {
-	const navigationItems = [
+class NavigationHeaderExample extends Component {
+	navigationItems = [
 		{ label: 'Plugins', href: `/plugins` },
 		{ label: 'Search', href: `/plugins?s=woo` },
 	];
-	return (
-		<NavigationHeaderStyled navigationItems={ navigationItems }>
-			Some Children Elements
-		</NavigationHeaderStyled>
-	);
-};
+	state = {
+		page: 1,
+		compact: false,
+	};
 
-NavigationHeaderExample.displayName = 'NavigationHeader';
+	updatePage = ( page ) => {
+		this.setState( { page } );
+	};
+
+	toggleCompact = () => {
+		this.setState( { compact: ! this.state.compact } );
+	};
+
+	render() {
+		return (
+			<div>
+				<NavigationHeader
+					compactBreadcrumb={ this.state.compact }
+					navigationItems={ [] }
+					mobileItem={ null }
+					title="My Home"
+					subtitle="Your hub for posting, editing, and growing your site."
+				>
+					<Button target="_blank">Visit site</Button>
+				</NavigationHeader>
+
+				<NavigationHeader
+					compactBreadcrumb={ this.state.compact }
+					navigationItems={ [
+						{ label: 'Domains', href: `/domains` },
+						{
+							label: 'thisisanexample.wordpress.com',
+							href: `/domains/thisisanexample.wordpress.com`,
+						},
+						{
+							label: 'Transfer',
+							href: `/domains/thisisanexample.wordpress.com/transfer`,
+						},
+					] }
+					mobileItem={ null }
+					title="Title example"
+					subtitle="Subtitle example"
+				></NavigationHeader>
+			</div>
+		);
+	}
+}
+NavigationHeaderExample.displayName = 'NavigationHeaderExample';
 
 export default NavigationHeaderExample;
