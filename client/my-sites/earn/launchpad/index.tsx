a6c48377075d058b5a3a35af3f535cd26d6564e0
Erick Danzer <edanzer@gmail.com>
2023-10-16 19:30:33 +0200

    Earn: Add Launchpad  (#81930)

---
+++ client/my-sites/earn/launchpad/index.tsx
@@ -0,0 +1,66 @@
+import { CircularProgressBar } from '@automattic/components';
+import { LaunchpadNavigator } from '@automattic/data-stores';
+import { Launchpad, Task, setUpActionsForTasks } from '@automattic/launchpad';
+import { useDispatch } from '@wordpress/data';
+import { useTranslate } from 'i18n-calypso';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { useSelector } from 'calypso/state';
+import { getSelectedSite } from 'calypso/state/ui/selectors';
+
+type EarnLaunchpadProps = {
+	tasklistCompleted: boolean;
+	numberOfSteps: number;
+	completedSteps: number;
+};
+
+const EarnLaunchpad = ( {
+	tasklistCompleted,
+	numberOfSteps,
+	completedSteps,
+}: EarnLaunchpadProps ) => {
+	const translate = useTranslate();
+
+	const site = useSelector( ( state ) => getSelectedSite( state ) );
+
+	const { setActiveChecklist } = useDispatch( LaunchpadNavigator.store );
+
+	const tracksData = {
+		recordTracksEvent,
+		checklistSlug: 'earn',
+		tasklistCompleted,
+		launchpadContext: 'earn',
+	};
+
+	const taskFilter = ( tasks: Task[] ) => {
+		return setUpActionsForTasks( {
+			tasks,
+			siteSlug: site?.slug ?? null,
+			tracksData,
+			extraActions: { setActiveChecklist },
+		} );
+	};
+
+	return (
+		<div className="earn__launchpad">
+			<div className="earn__launchpad-header">
+				<div className="earn__launchpad-progress-bar-container">
+					<CircularProgressBar
+						size={ 40 }
+						enableDesktopScaling
+						numberOfSteps={ numberOfSteps }
+						currentStep={ completedSteps }
+					/>
+				</div>
+				<h2 className="earn__launchpad-title">
+					{ translate( 'Create your paid offering in two steps.' ) }
+				</h2>
+				<p className="earn__launchpad-description">
+					{ translate( 'Let your fans support your art, writing, or project directly.' ) }
+				</p>
+			</div>
+			<Launchpad siteSlug={ site?.slug ?? null } checklistSlug="earn" taskFilter={ taskFilter } />
+		</div>
+	);
+};
+
+export default EarnLaunchpad;
