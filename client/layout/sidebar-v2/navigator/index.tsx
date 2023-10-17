730ffd69eafd8c73a44ec27e6a6fff2da78bba1e
Yashwin <yashwinpoojary@gmail.com>
2023-10-17 14:04:46 +0530

    Update navigator background color

--- client/layout/sidebar-v2/navigator/index.tsx
+++ client/layout/sidebar-v2/navigator/index.tsx
@@ -3,11 +3,17 @@ export * from './navigator-menu-item';
 
 import { __experimentalNavigatorProvider as NavigatorProvider } from '@wordpress/components';
 
+import './style.scss';
+
 interface Props {
 	initialPath: string;
 	children: React.ReactNode;
 }
 
 export const SidebarNavigator = ( { initialPath, children }: Props ) => {
-	return <NavigatorProvider initialPath={ initialPath }>{ children }</NavigatorProvider>;
+	return (
+		<NavigatorProvider className="sidebar-v2__navigator" initialPath={ initialPath }>
+			{ children }
+		</NavigatorProvider>
+	);
 };
