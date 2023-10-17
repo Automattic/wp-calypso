daf99406f027815e54557b9c0894b44a9618d8fa
Yashwin <yashwinpoojary@gmail.com>
2023-10-17 11:00:49 +0530

    Export other navigations components from the index.tsx file

--- client/layout/sidebar-v2/navigator/index.tsx
+++ client/layout/sidebar-v2/navigator/index.tsx
@@ -1,3 +1,6 @@
+export * from './navigator-menu';
+export * from './navigator-menu-item';
+
 import { __experimentalNavigatorProvider as NavigatorProvider } from '@wordpress/components';
 
 interface Props {
