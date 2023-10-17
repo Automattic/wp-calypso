ea5f13285c5e56800ce8c39b0a8e8a19b922f23f
Yashwin <yashwinpoojary@gmail.com>
2023-10-17 10:59:51 +0530

    Create SidebarNavigatorMenuItem component

---
+++ client/layout/sidebar-v2/navigator/navigator-menu-item.tsx
@@ -0,0 +1,55 @@
+import {
+	__experimentalNavigatorButton as NavigatorButton,
+	__experimentalItem as Item,
+	__experimentalHStack as HStack,
+	FlexBlock,
+} from '@wordpress/components';
+import { Icon, chevronRightSmall } from '@wordpress/icons';
+import classnames from 'classnames';
+
+import './style.scss';
+
+const ICON_SIZE = 24;
+
+interface Props {
+	icon: JSX.Element;
+	path: string;
+	link: string;
+	title: string;
+	onClickMenuItem: ( path: string ) => void;
+	withChevron?: boolean;
+}
+
+export const SidebarNavigatorMenuItem = ( {
+	icon,
+	path,
+	link,
+	title,
+	onClickMenuItem,
+	withChevron,
+}: Props ) => {
+	const pathname = window.location.pathname;
+
+	const SidebarItem = ( { children }: { children?: JSX.Element } ) => {
+		return (
+			<Item
+				className={ classnames( 'sidebar-v2__menu-item', {
+					'is-active': pathname === link,
+				} ) }
+				onClick={ () => onClickMenuItem( link ) }
+			>
+				<HStack justify="flex-start">
+					{ icon && <Icon style={ { fill: 'currentcolor' } } icon={ icon } size={ ICON_SIZE } /> }
+					<FlexBlock>{ children }</FlexBlock>
+					{ withChevron && <Icon icon={ chevronRightSmall } size={ ICON_SIZE } /> }
+				</HStack>
+			</Item>
+		);
+	};
+
+	return (
+		<NavigatorButton as={ SidebarItem } path={ path }>
+			{ title }
+		</NavigatorButton>
+	);
+};
