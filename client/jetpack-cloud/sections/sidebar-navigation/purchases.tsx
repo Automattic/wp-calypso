be3167d415749daeb5b0c2a5cba4ee75bdc581bc
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-10-17 10:16:35 -0500

    Jetpack Cloud: Add menu items to the Purchases sidebar view

--- client/jetpack-cloud/sections/sidebar-navigation/purchases.tsx
+++ client/jetpack-cloud/sections/sidebar-navigation/purchases.tsx
@@ -1,11 +1,65 @@
+import { chevronLeft, formatListBulletsRTL, payment, receipt, store, tag } from '@wordpress/icons';
+import { useTranslate } from 'i18n-calypso';
+import page from 'page';
 import NewSidebar from 'calypso/jetpack-cloud/components/sidebar';
 
-// This sidebar is what Jetpack Manage customers will see when they select the
-// Purchases item from the top-level navigation sidebar. It'll display options
-// like Licenses, Invoices, Billing, etc.
-// FIXME: Add menu items
 const PurchasesSidebar = () => {
-	return <NewSidebar path="/partner-portal" menuItems={ [] } />;
+	const translate = useTranslate();
+
+	const onClickMenuItem = ( path: string ) => {
+		page.redirect( path );
+	};
+
+	const menuItems = [
+		{
+			icon: store,
+			path: '/partner-portal',
+			link: '/partner-portal/billing',
+			title: translate( 'Billing' ),
+			onClickMenuItem: onClickMenuItem,
+		},
+		{
+			icon: payment,
+			path: '/partner-portal',
+			link: '/partner-portal/payment-methods',
+			title: translate( 'Payment Methods' ),
+			onClickMenuItem: onClickMenuItem,
+		},
+		{
+			icon: receipt,
+			path: '/partner-portal',
+			link: '/partner-portal/invoices',
+			title: translate( 'Invoices' ),
+			onClickMenuItem: onClickMenuItem,
+		},
+		{
+			icon: tag,
+			path: '/partner-portal',
+			link: '/partner-portal/prices',
+			title: translate( 'Prices' ),
+			onClickMenuItem: onClickMenuItem,
+		},
+		{
+			icon: formatListBulletsRTL,
+			path: '/partner-portal',
+			link: '/partner-portal/company-details',
+			title: translate( 'Company Details' ),
+			onClickMenuItem: onClickMenuItem,
+		},
+	];
+	return (
+		<NewSidebar
+			isJetpackManage
+			path="/partner-portal"
+			menuItems={ menuItems }
+			description={ translate( 'Manage all your billing related settings from one place.' ) }
+			backButtonProps={ {
+				label: translate( 'Purchases' ),
+				icon: chevronLeft,
+				onClick: () => onClickMenuItem( '/dashboard' ),
+			} }
+		/>
+	);
 };
 
 export default PurchasesSidebar;
