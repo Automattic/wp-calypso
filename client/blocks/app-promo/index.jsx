013ba9670c6721609f018962fa40c19c9bbe2e3d
Enej Bajgoric <enej.bajgoric@automattic.com>
2023-10-17 16:31:42 -0700

    App Promo
    Add new options and QR Code component

--- client/blocks/app-promo/index.jsx
+++ client/blocks/app-promo/index.jsx
@@ -1,55 +1,78 @@
 import { Card, Button } from '@automattic/components';
 import { useLocalizeUrl } from '@automattic/i18n-utils';
+import classNames from 'classnames';
 import { useTranslate, useRtl } from 'i18n-calypso';
 import wpToJpImageRtl from 'calypso/assets/images/jetpack/wp-to-jp-rtl.svg';
 import wpToJpImage from 'calypso/assets/images/jetpack/wp-to-jp.svg';
+import QrCode from 'calypso/blocks/app-promo/qr-code';
 import AppsBadge from 'calypso/blocks/get-apps/apps-badge';
 import CardHeading from 'calypso/components/card-heading';
 import userAgent from 'calypso/lib/user-agent';
-
+import { useSelector } from 'calypso/state';
+import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import './style.scss';
 
-export const AppPromo = () => {
+export const AppPromo = ( {
+	title = false,
+	iconSize = 29,
+	source = 'calypso-customer-home',
+	subheader = false,
+	className = 'customer-home__card',
+	hasQRCode = false,
+} ) => {
 	const isRtl = useRtl();
 	const translate = useTranslate();
 	const localizeUrl = useLocalizeUrl();
-	const { isDesktop, isiPad, isiPod, isiPhone, isAndroid } = userAgent;
+	const isLoggedIn = useSelector( isUserLoggedIn );
+	const iconWidth = Math.ceil( ( 49 / 29 ) * iconSize );
+
+	const { isDesktop, isiPad, isiPod, isiPhone, isAndroid, isMobile } = userAgent;
 	const isIos = isiPad || isiPod || isiPhone;
 
-	const showIosBadge = ! isAndroid && ! isDesktop;
-	const showAndroidBadge = ! isIos && ! isDesktop;
-	const showBadge = showIosBadge || showAndroidBadge;
+	const showIosBadge = ! isAndroid && ! isDesktop && userAgent;
+	const showAndroidBadge = ! isIos && ! isDesktop && userAgent;
+	const showBadge = ( showIosBadge || showAndroidBadge ) && isMobile;
+	if ( title === false ) {
+		title = translate( 'Get our mobile app' );
+	}
+
+	if ( subheader === false ) {
+		subheader = translate( 'Everything you need to build and grow your site from any device.' );
+	}
+
+	className = classNames( className, 'app-promo' );
 
 	return (
-		<Card className="app-promo customer-home__card">
+		<Card className={ className }>
 			<img
 				className="app-promo__icon"
 				src={ isRtl ? wpToJpImageRtl : wpToJpImage }
-				width="49"
-				height="29"
+				width={ iconWidth }
+				height={ iconSize }
 				alt="WordPress and Jetpack app"
 			/>
 			<div className="app-promo__title">
-				<CardHeading tagName="h2">{ translate( 'Get our mobile app' ) }</CardHeading>
-				<h3 className="app-promo__subheader">
-					{ translate( 'Everything you need to build and grow your site from any device.' ) }
-				</h3>
+				<CardHeading tagName="h2">{ title }</CardHeading>
+				<h3 className="app-promo__subheader">{ subheader }</h3>
 			</div>
-			{ showBadge ? (
+
+			{ showBadge && (
 				<div className="app-promo__app-badges">
 					<AppsBadge
 						storeName={ showIosBadge ? 'ios' : 'android' }
-						utm_source="calypso-customer-home"
+						utm_source={ source }
 					></AppsBadge>
 				</div>
-			) : (
+			) }
+			{ isLoggedIn && ! showBadge && (
 				<Button
 					className="app-promo__link-button is-link"
-					href={ localizeUrl( 'https://apps.wordpress.com/get/?campaign=calypso-customer-home' ) }
+					href={ localizeUrl( `https://apps.wordpress.com/get/?campaign=${ source }` ) }
 				>
 					{ translate( 'Get the Jetpack app' ) }
 				</Button>
 			) }
+			{ hasQRCode && ! showBadge && <QrCode source={ source } size={ 100 } /> }
 		</Card>
 	);
 };
---
+++ client/blocks/app-promo/qr-code.jsx
@@ -0,0 +1,35 @@
+import { useLocalizeUrl } from '@automattic/i18n-utils';
+import { useTranslate } from 'i18n-calypso';
+import { QRCodeSVG } from 'qrcode.react';
+
+import './style.scss';
+
+export const QrCode = ( { source = 'calypso-customer-home', size = 150 } ) => {
+	const translate = useTranslate();
+	const localizeUrl = useLocalizeUrl();
+	return (
+		<div className="app-promo__qr-code">
+			<QRCodeSVG
+				value={ localizeUrl( `https://apps.wordpress.com/get?campaign=${ source }` ) }
+				size={ size }
+			/>
+			<p className="get-apps__card-text">
+				{ translate(
+					'Visit {{a}}wp.com/app{{/a}} from your mobile device, or scan the code to download the Jetpack mobile app.',
+					{
+						components: {
+							a: (
+								<a
+									className="get-apps__jetpack-branded-link"
+									href={ localizeUrl( `https://apps.wordpress.com/get?campaign=${ source }` ) }
+								/>
+							),
+						},
+					}
+				) }
+			</p>
+		</div>
+	);
+};
+
+export default QrCode;
