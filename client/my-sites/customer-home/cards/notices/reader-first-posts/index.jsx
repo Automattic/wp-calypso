a1b15c8a63f8aa8b5a3696f5406c2cb46d2d02cd
Caroline Moore <carolinemoore@Carolines-MacBook-Pro-2.local>
2023-10-17 10:46:14 -0400

    Add wireframe of card for nudge

---
+++ client/my-sites/customer-home/cards/notices/reader-first-posts/index.jsx
@@ -0,0 +1,19 @@
+import { Card, Button } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import { NOTICE_READER_FIRST_POSTS } from 'calypso/my-sites/customer-home/cards/constants';
+
+const ReaderFirstPosts = () => {
+	const translate = useTranslate();
+	// @TODO: Add Tracks events.
+	return (
+		<Card className="reader-first-posts__nudge">
+			<h2>{ translate( 'Looking for inspiration?' ) }</h2>
+			<p>{ translate( 'See what other brand new sites are writing about.' ) }</p>
+			<Button className="reader-first-posts__button" href="/discover?selectedTab=firstposts">
+				{ translate( 'Take a look' ) }
+			</Button>
+		</Card>
+	);
+};
+
+export default ReaderFirstPosts;
