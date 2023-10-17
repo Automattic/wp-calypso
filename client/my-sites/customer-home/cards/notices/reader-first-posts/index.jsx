3df14f49df2226a0c74511469bc3e618e3be27b5
Caroline Moore <calobee@gmail.com>
2023-10-17 10:56:43 -0400

    Update button with onclick

--- client/my-sites/customer-home/cards/notices/reader-first-posts/index.jsx
+++ client/my-sites/customer-home/cards/notices/reader-first-posts/index.jsx
@@ -1,15 +1,24 @@
 import { Card, Button } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
+import page from 'page';
+//import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 //import { NOTICE_READER_FIRST_POSTS } from 'calypso/my-sites/customer-home/cards/constants';
 
 const ReaderFirstPosts = () => {
 	const translate = useTranslate();
-	// @TODO: Add Tracks events.
+
+	const clickButton = () => {
+		// @TODO: Add Tracks events.
+		//recordTracksEvent( 'calypso_home_reader_first_posts_nudge_click' );
+
+		page.redirect( '/discover?selectedTab=first-posts' );
+	};
+
 	return (
 		<Card className="reader-first-posts__nudge">
 			<h2>{ translate( 'Looking for inspiration?' ) }</h2>
 			<p>{ translate( 'See what other brand new sites are writing about.' ) }</p>
-			<Button className="reader-first-posts__button" href="/discover?selectedTab=firstposts">
+			<Button primary onClick={ () => clickButton() } className="reader-first-posts__button">
 				{ translate( 'Take a look' ) }
 			</Button>
 		</Card>
