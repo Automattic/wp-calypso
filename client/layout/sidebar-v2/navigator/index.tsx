bfc510dab034f39bafd049e15d5afae234bc1007
Yashwin <yashwinpoojary@gmail.com>
2023-10-17 10:56:57 +0530

    Create SidebarNavigator component

---
+++ client/layout/sidebar-v2/navigator/index.tsx
@@ -0,0 +1,10 @@
+import { __experimentalNavigatorProvider as NavigatorProvider } from '@wordpress/components';
+
+interface Props {
+	initialPath: string;
+	children: React.ReactNode;
+}
+
+export const SidebarNavigator = ( { initialPath, children }: Props ) => {
+	return <NavigatorProvider initialPath={ initialPath }>{ children }</NavigatorProvider>;
+};
