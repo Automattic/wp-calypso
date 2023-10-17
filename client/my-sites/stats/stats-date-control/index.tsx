bee3984d6b2d6bb2ae5c903fb836060c320e185e
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2023-10-17 19:21:46 +0800

    Move shortcuts outside of component.

--- client/my-sites/stats/stats-date-control/index.tsx
+++ client/my-sites/stats/stats-date-control/index.tsx
@@ -7,6 +7,45 @@ import './style.scss';
 
 const COMPONENT_CLASS_NAME = 'stats-date-control';
 
+const shortcutList = [
+	{
+		id: 'today',
+		label: 'Today',
+		offset: 0,
+		range: 0,
+	},
+	{
+		id: 'yesterday',
+		label: 'Yesterday',
+		offset: 1,
+		range: 0,
+	},
+	{
+		id: 'last-7-days',
+		label: 'Last 7 Days',
+		offset: 0,
+		range: 7,
+	},
+	{
+		id: 'last-30-days',
+		label: 'Last 30 Days',
+		offset: 0,
+		range: 30,
+	},
+	{
+		id: 'last-year',
+		label: 'Last Year',
+		offset: 0,
+		range: 365,
+	},
+	{
+		id: 'all-time',
+		label: 'All Time',
+		offset: 0,
+		range: 400, // TODO: Don't hard code this value.
+	},
+];
+
 const StatsDateControl = ( {
 	slug,
 	queryParams,
@@ -14,45 +53,6 @@ const StatsDateControl = ( {
 	pathTemplate,
 	onChangeChartQuantity,
 }: StatsDateControlProps ) => {
-	const shortcutList = [
-		{
-			id: 'today',
-			label: 'Today',
-			offset: 0,
-			range: 0,
-		},
-		{
-			id: 'yesterday',
-			label: 'Yesterday',
-			offset: 1,
-			range: 0,
-		},
-		{
-			id: 'last-7-days',
-			label: 'Last 7 Days',
-			offset: 0,
-			range: 7,
-		},
-		{
-			id: 'last-30-days',
-			label: 'Last 30 Days',
-			offset: 0,
-			range: 30,
-		},
-		{
-			id: 'last-year',
-			label: 'Last Year',
-			offset: 0,
-			range: 365,
-		},
-		{
-			id: 'all-time',
-			label: 'All Time',
-			offset: 0,
-			range: 400, // TODO: Don't hard code this value.
-		},
-	];
-
 	const handleApply = ( startDate: string, endDate: string ) => {
 		// calculate offset between start and end to influcence the number of points for the chart
 		// TODO: take period into account
