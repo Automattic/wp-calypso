6fcba168a08eeaab4a753445e169ddb182529e00
Richard Ortiz <rcrd.ortiz@gmail.com>
2023-10-17 12:47:20 +0200

    Theme Discovery: Mobile Design + other style fixes and improvements (#82987)
    * Implemented the mobile design we have in figma except for the background colors. Adapted the currently implemented design to the figma design and fixed some styling issues I found along the way.
    
    * Removed the right overflow for xlarge resolutions
    
    * Increased top margin between the theme showcase header and the first collection carousel

--- client/components/theme-collection/index.tsx
+++ client/components/theme-collection/index.tsx
@@ -66,8 +66,18 @@ export default function ThemeCollection( {
 						},
 						threshold: 5,
 						slideToClickedSlide: true,
-						slidesPerView: 'auto',
-						spaceBetween: 20,
+						slidesPerView: 1.2,
+						breakpoints: {
+							// break-small in Gutenberg breakpoints
+							'600': {
+								slidesPerView: 2.2,
+							},
+							// break-xlarge in Gutenberg breakpoints
+							'1080': {
+								slidesPerView: 3,
+							},
+						},
+						spaceBetween: 32,
 						modules: [ Navigation, Keyboard, Mousewheel ],
 					} );
 					setSwiperLoaded( true );
