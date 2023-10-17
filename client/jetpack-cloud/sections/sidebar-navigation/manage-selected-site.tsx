8cc685b718985e7a72024aaad20022a232678642
Yashwin <yashwinpoojary@gmail.com>
2023-10-17 11:33:44 +0530

    Add the required props to NewSidebar component

--- client/jetpack-cloud/sections/sidebar-navigation/manage-selected-site.tsx
+++ client/jetpack-cloud/sections/sidebar-navigation/manage-selected-site.tsx
@@ -2,6 +2,7 @@ import NewSidebar from 'calypso/jetpack-cloud/components/sidebar';
 
 // This sidebar is what people will see when they pick a site from the site
 // selector. It'll display menu options like Activity Log, Backup, Social, etc.
-const ManageSelectedSiteSidebar = () => <NewSidebar />;
+// FIXME: Add menu items and the right path
+const ManageSelectedSiteSidebar = () => <NewSidebar path="/" menuItems={ [] } />;
 
 export default ManageSelectedSiteSidebar;
