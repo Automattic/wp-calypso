0f470665ed64b514c828e0dc5f236d5b21924b24
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-10-17 13:07:57 +0800

    Jetpack Manage: Add sidebar v2 layout for the new navigation. (#82929)
    * Add initial Sidebar V2 layout components.
    
    * Update Jetpack cloud sidebar to use the layout.
    
    * Add rest of the layout region.
    
    * Add WP-Admin link.
    
    * Fix broken style after rebase.
    
    * Fix main container scrollable.
    
    * Make footer conditionally rendered.
    
    * Export sub-components from index.tsx

--- client/jetpack-cloud/components/sidebar/index.tsx
+++ client/jetpack-cloud/components/sidebar/index.tsx
@@ -1,6 +1,17 @@
 import classNames from 'classnames';
+import { useTranslate } from 'i18n-calypso';
+import JetpackIcons from 'calypso/components/jetpack/sidebar/menu-items/jetpack-icons';
 import SiteSelector from 'calypso/components/site-selector';
-import Header from './header';
+import SidebarItem from 'calypso/layout/sidebar/item';
+import Sidebar, {
+	SidebarV2Main as SidebarMain,
+	SidebarV2Footer as SidebarFooter,
+} from 'calypso/layout/sidebar-v2';
+import { useSelector } from 'calypso/state';
+import getJetpackAdminUrl from 'calypso/state/sites/selectors/get-jetpack-admin-url';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import SidebarHeader from './header';
+
 import './style.scss';
 
 // This is meant to be the "base" sidebar component. All context-specific sidebars
@@ -11,33 +22,55 @@ type Props = {
 	className?: string;
 	isJetpackManage?: boolean;
 };
-const Sidebar = ( { className, isJetpackManage = false }: Props ) => (
-	<nav className={ classNames( 'jetpack-cloud-sidebar', className ) }>
-		<Header forceAllSitesView={ isJetpackManage } />
-		<div className="jetpack-cloud-sidebar__main">
-			<ul className="jetpack-cloud-sidebar__navigation-list">
-				<li
-					className={ classNames(
-						'jetpack-cloud-sidebar__navigation-item',
-						'jetpack-cloud-sidebar__navigation-item--highlighted'
-					) }
-				>
-					Navigation items
-				</li>
-				<li className="jetpack-cloud-sidebar__navigation-item">Will go here</li>
-			</ul>
-		</div>
+const JetpackCloudSidebar = ( { className, isJetpackManage }: Props ) => {
+	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
+	const jetpackAdminUrl = useSelector( ( state ) =>
+		siteId ? getJetpackAdminUrl( state, siteId ) : null
+	);
 
-		<SiteSelector
-			showAddNewSite
-			showAllSites={ isJetpackManage }
-			isJetpackAgencyDashboard={ isJetpackManage }
-			className="jetpack-cloud-sidebar__site-selector"
-			allSitesPath="/dashboard"
-			siteBasePath="/landing"
-			wpcomSiteBasePath="https://wordpress.com/home"
-		/>
-	</nav>
-);
+	const translate = useTranslate();
 
-export default Sidebar;
+	return (
+		<Sidebar className={ classNames( 'jetpack-cloud-sidebar', className ) }>
+			<SidebarHeader forceAllSitesView={ isJetpackManage } />
+
+			<SidebarMain>
+				<ul role="menu" className="jetpack-cloud-sidebar__navigation-list">
+					<li
+						className={ classNames(
+							'jetpack-cloud-sidebar__navigation-item',
+							'jetpack-cloud-sidebar__navigation-item--highlighted'
+						) }
+					>
+						Navigation items
+					</li>
+					<li className="jetpack-cloud-sidebar__navigation-item">Will go here</li>
+				</ul>
+			</SidebarMain>
+
+			{ ! isJetpackManage && (
+				<SidebarFooter>
+					<SidebarItem
+						label={ translate( 'WP Admin', {
+							comment: 'Jetpack Cloud sidebar navigation item',
+						} ) }
+						link={ jetpackAdminUrl }
+						customIcon={ <JetpackIcons icon="wordpress" /> }
+					/>
+				</SidebarFooter>
+			) }
+
+			<SiteSelector
+				showAddNewSite
+				showAllSites={ isJetpackManage }
+				isJetpackAgencyDashboard={ isJetpackManage }
+				className="jetpack-cloud-sidebar__site-selector"
+				allSitesPath="/dashboard"
+				siteBasePath="/landing"
+				wpcomSiteBasePath="https://wordpress.com/home"
+			/>
+		</Sidebar>
+	);
+};
+
+export default JetpackCloudSidebar;
