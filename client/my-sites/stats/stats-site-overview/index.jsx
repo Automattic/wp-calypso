31fcae96d762d7bccdbc9a78a76126f8b675142e
DustyReagan <dusty@dustyreagan.com>
2023-10-18 14:54:43 -0500

    Remove test code

--- client/my-sites/stats/stats-site-overview/index.jsx
+++ client/my-sites/stats/stats-site-overview/index.jsx
@@ -38,7 +38,6 @@ class StatsSiteOverview extends Component {
 		return (
 			<div>
 				<SectionHeader label={ title } href={ headerPath } />
-				aa
 				<Card className="stats__overview stats-module is-site-overview">
 					<StatsTabs borderless>
 						<StatsTab
