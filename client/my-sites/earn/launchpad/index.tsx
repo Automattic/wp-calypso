286eff17fa11881359de6480591ad634a4277bf5
edanzer <edanzer@gmail.com>
2023-10-18 08:39:43 -0600

    Filter earn launchpad on frontend for atomic

--- client/my-sites/earn/launchpad/index.tsx
+++ client/my-sites/earn/launchpad/index.tsx
@@ -1,7 +1,8 @@
 import { CircularProgressBar } from '@automattic/components';
-import { DefaultWiredLaunchpad } from '@automattic/launchpad';
+import { Launchpad, Task } from '@automattic/launchpad';
 import { useTranslate } from 'i18n-calypso';
 import { useSelector } from 'calypso/state';
+import { getConnectUrlForSiteId } from 'calypso/state/memberships/settings/selectors';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 type EarnLaunchpadProps = {
@@ -13,6 +14,39 @@ const EarnLaunchpad = ( { numberOfSteps, completedSteps }: EarnLaunchpadProps ) 
 	const translate = useTranslate();
 
 	const site = useSelector( ( state ) => getSelectedSite( state ) );
+
+	const stripeConnectUrl = useSelector( ( state ) =>
+		getConnectUrlForSiteId( state, site?.ID ?? 0 )
+	);
+
+	const taskFilter = ( tasks: Task[] ): Task[] => {
+		if ( ! tasks ) {
+			return [];
+		}
+
+		return tasks.map( ( task ) => {
+			switch ( task.id ) {
+				case 'stripe_connected':
+					return {
+						...task,
+						actionDispatch: () => {
+							window.location.assign( stripeConnectUrl );
+						},
+					};
+				case 'paid_offer_created':
+					return {
+						...task,
+						actionDispatch: () => {
+							window.location.assign(
+								`/earn/payments-plans/${ site?.slug }?launchpad=add-product#add-newsletter-payment-plan`
+							);
+						},
+					};
+				default:
+					return task;
+			}
+		} );
+	};
 
 	return (
 		<div className="earn__launchpad">
@@ -32,11 +66,7 @@ const EarnLaunchpad = ( { numberOfSteps, completedSteps }: EarnLaunchpadProps ) 
 					{ translate( 'Let your fans support your art, writing, or project directly.' ) }
 				</p>
 			</div>
-			<DefaultWiredLaunchpad
-				siteSlug={ site?.slug ?? null }
-				checklistSlug="earn"
-				launchpadContext="earn"
-			/>
+			<Launchpad siteSlug={ site?.slug ?? null } checklistSlug="earn" taskFilter={ taskFilter } />
 		</div>
 	);
 };
