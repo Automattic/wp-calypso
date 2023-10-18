bf05aeb329b7a1284700a1f2020fb8ff1833e040
Jeremy Yip <jeremy.yip@automattic.com>
2023-10-18 13:19:18 -0700

    init 6

--- client/my-sites/plans/p2-plans-main.jsx
+++ client/my-sites/plans/p2-plans-main.jsx
@@ -1,30 +1,29 @@
-import { PLAN_P2_FREE, PLAN_P2_PLUS } from '@automattic/calypso-products';
 import { localize } from 'i18n-calypso';
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import QueryPlans from 'calypso/components/data/query-plans';
 import QuerySitePlans from 'calypso/components/data/query-site-plans';
 import QuerySites from 'calypso/components/data/query-sites';
-import PlanFeatures from 'calypso/my-sites/plan-features';
+import PlansFeaturesMain from '../plans-features-main';
 
 export class P2PlansMain extends Component {
 	render() {
-		const { selectedFeature, selectedPlan, redirectTo, siteId } = this.props;
+		const { selectedFeature, selectedPlan, siteId } = this.props;
 
 		return (
 			<>
 				<QueryPlans />
 				<QuerySites siteId={ siteId } />
 				<QuerySitePlans siteId={ siteId } />
-				<PlanFeatures
-					plans={ [ PLAN_P2_FREE, PLAN_P2_PLUS ] }
-					redirectTo={ redirectTo }
-					visiblePlans={ [ PLAN_P2_FREE, PLAN_P2_PLUS ] }
+				<PlansFeaturesMain
+					siteId={ siteId }
 					selectedFeature={ selectedFeature }
 					selectedPlan={ selectedPlan }
-					siteId={ siteId }
+					intervalType={ [ 'monthly' ] }
+					hidePlansFeatureComparison={ true }
+					hidePlanTypeSelector={ true }
+					intent="plans-p2"
 					isInSignup={ false }
-					nonDotBlogDomains={ [] }
 				/>
 			</>
 		);
