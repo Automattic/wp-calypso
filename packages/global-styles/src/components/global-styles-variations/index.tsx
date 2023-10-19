4dc0d2f997b5dc9484319e8b46402613539e6407
Griffith Chen <griffith.chen@automattic.com>
2023-10-19 15:36:29 +0800

    Update packages/global-styles/src/components/global-styles-variations/index.tsx
    Co-authored-by: arthur791004 <arthur.chu@automattic.com>

--- packages/global-styles/src/components/global-styles-variations/index.tsx
+++ packages/global-styles/src/components/global-styles-variations/index.tsx
@@ -150,7 +150,7 @@ const GlobalStylesVariations = ( {
 							{ ! splitDefaultVariation ? (
 								<>
 									<span>{ headerText }</span>
-									<PremiumBadge
+									{ ! splitDefaultVariation && <PremiumBadge
 										shouldHideTooltip
 										shouldCompactWithAnimation
 										labelText={ translate( 'Included in your plan' ) }
