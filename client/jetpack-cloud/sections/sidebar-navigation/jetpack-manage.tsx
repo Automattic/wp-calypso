1d67121065c789c7c3876caef7b8181f0f3b35fe
Yashwin <yashwinpoojary@gmail.com>
2023-10-19 08:31:28 +0530

    Move the common functions to an util function for sidebar in lib

--- client/jetpack-cloud/sections/sidebar-navigation/jetpack-manage.tsx
+++ client/jetpack-cloud/sections/sidebar-navigation/jetpack-manage.tsx
@@ -1,61 +1,55 @@
 import { plugins, key, currencyDollar, category } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
-import page from 'page';
 import NewSidebar from 'calypso/jetpack-cloud/components/sidebar';
-import { itemLinkMatches } from 'calypso/my-sites/sidebar/utils';
 import {
 	JETPACK_MANAGE_DASHBOARD_LINK,
 	JETPACK_MANAGE_PLUGINS_LINK,
 	JETPACK_MANAGE_LICENCES_LINK,
 	JETPACK_MANAGE_BILLING_LINK,
 } from './lib/constants';
+import { redirectPage, isMenuItemSelected } from './lib/sidebar';
+import type { MenuItemProps } from './types';
 
 const JetpackManageSidebar = () => {
 	const translate = useTranslate();
 
 	const onClickMenuItem = ( path: string ) => {
-		page.redirect( path );
+		// TODO: Track event when user clicks on a menu item
+		redirectPage( path );
 	};
 
-	const isSelected = ( link: string ) => {
-		const pathname = window.location.pathname;
-		return itemLinkMatches( link, pathname );
-	};
+	const createItem = ( props: MenuItemProps ) => ( {
+		...props,
+		onClickMenuItem,
+		isSelected: isMenuItemSelected( props.link ),
+	} );
 
 	const menuItems = [
-		{
+		createItem( {
 			icon: category,
 			path: '/',
 			link: JETPACK_MANAGE_DASHBOARD_LINK,
 			title: translate( 'Sites Management' ),
-			onClickMenuItem: onClickMenuItem,
-			isSelected: isSelected( JETPACK_MANAGE_DASHBOARD_LINK ),
-		},
-		{
+		} ),
+		createItem( {
 			icon: plugins,
 			path: '/',
 			link: JETPACK_MANAGE_PLUGINS_LINK,
 			title: translate( 'Plugin Management' ),
-			onClickMenuItem: onClickMenuItem,
-			isSelected: isSelected( JETPACK_MANAGE_PLUGINS_LINK ),
-		},
-		{
+		} ),
+		createItem( {
 			icon: key,
 			path: '/',
 			link: JETPACK_MANAGE_LICENCES_LINK,
 			title: translate( 'Licenses' ),
-			onClickMenuItem: onClickMenuItem,
-			isSelected: isSelected( JETPACK_MANAGE_LICENCES_LINK ),
-		},
-		{
+		} ),
+		createItem( {
 			icon: currencyDollar,
 			path: '/partner-portal/',
 			link: JETPACK_MANAGE_BILLING_LINK,
 			title: translate( 'Purchases' ),
-			onClickMenuItem: onClickMenuItem,
 			withChevron: true,
-			isSelected: isSelected( JETPACK_MANAGE_BILLING_LINK ),
-		},
+		} ),
 	];
 	return <NewSidebar isJetpackManage path="/" menuItems={ menuItems } />;
 };
