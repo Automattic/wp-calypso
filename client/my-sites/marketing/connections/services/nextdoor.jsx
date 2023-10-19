dcdf7d2fd894391d4c50a2e735ba8ed6b1a364ff
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-10-19 13:39:41 +0530

    Nextdoor: Add the connection to Calypso (#83227)
    * Add placeholder image
    
    * Add Nextdoor example UI
    
    * Remove "NEW" badge from Mastodon and Instagram

--- client/my-sites/marketing/connections/services/nextdoor.jsx
+++ client/my-sites/marketing/connections/services/nextdoor.jsx
@@ -1,11 +1,16 @@
 import { SharingService, connectFor } from 'calypso/my-sites/marketing/connections/service';
 
-export class Mastodon extends SharingService {
+export class Nextdoor extends SharingService {
 	static propTypes = {
 		// This foreign propTypes access should be safe because we expect all of them to be removed
 		// eslint-disable-next-line react/forbid-foreign-prop-types
 		...SharingService.propTypes,
 	};
+
+	static defaultProps = {
+		...SharingService.defaultProps,
+		isNew: true,
+	};
 }
 
-export default connectFor( Mastodon, ( state, props ) => props );
+export default connectFor( Nextdoor, ( state, props ) => props );
