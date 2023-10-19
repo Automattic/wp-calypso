192fb44ce78defc9263e83ba2f855ad02d9485a6
Antony Agrios <antonyagrios@gmail.com>
2023-10-19 15:10:30 +0300

    Staging Sites: Hide activity log banners during staging site sync (#83233)
    * Staging Sites: dont show banners in activity log screen on sync
    
    * Fix: react render warning

--- client/state/sync/selectors/index.ts
+++ client/state/sync/selectors/index.ts
@@ -8,5 +8,7 @@ export { getSyncStatus } from 'calypso/state/sync/selectors/get-sync-status';
 export { getSyncProgress } from 'calypso/state/sync/selectors/get-sync-progress';
 export { getIsSyncingInProgress } from 'calypso/state/sync/selectors/get-is-syncing-in-progress';
 export { getSyncStatusError } from 'calypso/state/sync/selectors/get-sync-status-error';
-export { getSyncSiteType } from './get-sync-site-type';
+export { getSyncTargetSite } from './get-sync-target-site';
+export { getSyncSourceSite } from './get-sync-source-site';
+export { getSyncRestoreId } from './get-sync-restore-id';
 export { default as isFetchingSyncStatus } from 'calypso/state/sync/selectors/is-fetching-site-sync-status';
