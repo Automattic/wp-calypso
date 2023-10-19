91d6f74920c7ef80f4eefcdd01d95d9cf14ba19f
Caroline Moore <calobee@gmail.com>
2023-10-19 09:27:48 -0400

    Add Tracking (#83121)

--- client/my-sites/customer-home/cards/notices/reader-first-posts/index.jsx
+++ client/my-sites/customer-home/cards/notices/reader-first-posts/index.jsx
@@ -1,21 +1,29 @@
 import { Card, Button } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import page from 'page';
-//import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
-//import { NOTICE_READER_FIRST_POSTS } from 'calypso/my-sites/customer-home/cards/constants';
+import TrackComponentView from 'calypso/lib/analytics/track-component-view';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { NOTICE_READER_FIRST_POSTS } from 'calypso/my-sites/customer-home/cards/constants';
 
 const ReaderFirstPosts = () => {
 	const translate = useTranslate();
 
 	const clickButton = () => {
-		// @TODO: Add Tracks events.
-		//recordTracksEvent( 'calypso_home_reader_first_posts_nudge_click' );
+		recordTracksEvent( 'calypso_my_home_reader_first_posts_nudge_click', {
+			id: NOTICE_READER_FIRST_POSTS,
+		} );
 
 		page.redirect( '/discover?selectedTab=firstposts' );
 	};
 
 	return (
 		<Card className="reader-first-posts__nudge">
+			<TrackComponentView
+				eventName="calypso_my_home_reader_first_posts_nudge_view"
+				eventProperties={ {
+					id: NOTICE_READER_FIRST_POSTS,
+				} }
+			/>
 			<h2>{ translate( 'Looking for inspiration?' ) }</h2>
 			<p>{ translate( 'See what other brand new sites are writing about.' ) }</p>
 			<Button primary onClick={ () => clickButton() } className="reader-first-posts__button">
