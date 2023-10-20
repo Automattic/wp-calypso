37e48996a013ea1121ab7b69ab0b1fd5327c08e3
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-10-20 08:51:53 -0500

    Jetpack Cloud: Add sidebar navigation items for Purchases view (#83108)

--- client/jetpack-cloud/sections/sidebar-navigation/lib/constants.ts
+++ client/jetpack-cloud/sections/sidebar-navigation/lib/constants.ts
@@ -1,8 +1,12 @@
 export const JETPACK_MANAGE_DASHBOARD_LINK = '/dashboard';
 export const JETPACK_MANAGE_PLUGINS_LINK = '/plugins/manage';
-export const JETPACK_MANAGE_LICENCES_LINK = '/partner-portal/licenses';
-export const JETPACK_MANAGE_BILLING_LINK = '/partner-portal/billing';
-
 export const JETPACK_CLOUD_ACTIVITY_LOG_LINK = '/activity-log';
 export const JETPACK_CLOUD_SEARCH_LINK = '/jetpack-search';
 export const JETPACK_CLOUD_SOCIAL_LINK = '/jetpack-social';
+export const JETPACK_MANAGE_PARTNER_PORTAL_LINK = '/partner-portal';
+export const JETPACK_MANAGE_LICENCES_LINK = `${ JETPACK_MANAGE_PARTNER_PORTAL_LINK }/licenses`;
+export const JETPACK_MANAGE_BILLING_LINK = `${ JETPACK_MANAGE_PARTNER_PORTAL_LINK }/billing`;
+export const JETPACK_MANAGE_COMPANY_DETAILS_LINK = `${ JETPACK_MANAGE_PARTNER_PORTAL_LINK }/company-details`;
+export const JETPACK_MANAGE_INVOICES_LINK = `${ JETPACK_MANAGE_PARTNER_PORTAL_LINK }/invoices`;
+export const JETPACK_MANAGE_PAYMENT_METHODS_LINK = `${ JETPACK_MANAGE_PARTNER_PORTAL_LINK }/payment-methods`;
+export const JETPACK_MANAGE_PRICES_LINK = `${ JETPACK_MANAGE_PARTNER_PORTAL_LINK }/prices`;
