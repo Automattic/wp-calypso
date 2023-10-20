eec9b7a9d7e1fc058a76d105c78ffbdf9a7236e0
Miguel San Segundo <1881481+miksansegundo@users.noreply.github.com>
2023-10-20 16:47:09 +0700

    Update creatio to creatio 2 for pattern previews (#83289)

--- packages/data-stores/src/site/constants.ts
+++ packages/data-stores/src/site/constants.ts
@@ -1,3 +1,3 @@
 export const STORE_KEY = 'automattic/site';
 
-export const PLACEHOLDER_SITE_ID = 220580624; // creatiodemo.wordpress.com
+export const PLACEHOLDER_SITE_ID = 224076220; // creatio2demo.wordpress.com
