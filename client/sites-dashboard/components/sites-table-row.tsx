6b525a90a800dc1bb447aa8308d90110bf7df520
Kateryna Kodonenko <kateryna@automattic.com>
2023-10-23 14:37:09 +0200

    Add fallback to table row

--- client/sites-dashboard/components/sites-table-row.tsx
+++ client/sites-dashboard/components/sites-table-row.tsx
@@ -190,7 +190,7 @@ export default memo( function SitesTableRow( { site }: SiteTableRowProps ) {
 							<SiteName
 								href={
 									hasHostingFeatures && siteDefaultInterface( site ) === 'wp-admin'
-										? getSiteWpAdminUrl( site )
+										? getSiteWpAdminUrl( site ) || getDashboardUrl( site.slug )
 										: getDashboardUrl( site.slug )
 								}
 								title={ __( 'Visit Dashboard' ) }
