3278e37a87d790a4727ddb182b8dc2c68ce2f2aa
Eoin Gallagher <eoin@automattic.com>
2023-10-25 17:32:54 +0100

    Add Join conversation modal for logged in actions (#83031)
    * Add reader-join-conversation modal
    
    * Add login window component
    
    * stash
    
    * Strip out redundant code
    
    * fix logic around show/hiding modal
    
    * add createAccount flow
    
    * remove passing domain for testing and try to get working with wordpress.com domain instead
    
    * Add logic to check dev enviroment to handle redirects to sandboxed domain
    
    * add feature flag and move onLoggedOut functionality to handle logged out state
    
    * fix modal styling
    
    * small refactor - move open window functionality into its own function
    
    * Add track events for when user click actions from join conversation dialog
    
    * add example and readme
    
    * Update comment form likes to use join conversation dialog too
    
    * stash redux changes
    
    * fix login link padding
    
    * Add reducers to save/clear last logged in action
    
    * tidy up code to address some feedback
    
    * remove unused login window component
    
    * update reducer to track last action callback instead of string
    
    * refactor to pass logged in action details to reducer
    
    * rebase for isReaderTagEmbedPage
    
    * fix unit test
    
    * fix bug in redirect in create account flow
    
    * fix track events - pass loggedOutAction details in track event props
    
    * No camel case - change comment like action type label
    
    * Update string to be clearer
    
    * update terminology around the last action that requires a user to be logged in
    
    * add tests for reducer, actions and selectors
    
    * Update comment form to trigger join conversation dialog
    
    * Update logged out behaviour to open create account page in new tab when on Reader embed tag page
    
    * Redirect in new tab for replies on reader embed tag page

--- client/blocks/comments/post-comment.jsx
+++ client/blocks/comments/post-comment.jsx
@@ -1,3 +1,4 @@
+import config from '@automattic/calypso-config';
 import { getUrlParts } from '@automattic/calypso-url';
 import { Gridicon } from '@automattic/components';
 import classnames from 'classnames';
@@ -20,6 +21,7 @@ import { expandComments } from 'calypso/state/comments/actions';
 import { PLACEHOLDER_STATE, POST_COMMENT_DISPLAY_TYPES } from 'calypso/state/comments/constants';
 import { getCurrentUser, isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import { recordReaderTracksEvent } from 'calypso/state/reader/analytics/actions';
+import { registerLastActionRequiresLogin } from 'calypso/state/reader-ui/actions';
 import CommentActions from './comment-actions';
 import PostCommentForm from './form';
 import PostCommentContent from './post-comment-content';
@@ -106,6 +108,23 @@ class PostComment extends PureComponent {
 		this.setState( { showReplies: ! this.state.showReplies } );
 	};
 
+	onLikeToggle = () => {
+		if ( ! this.props.isLoggedIn ) {
+			// Redirect to create account page when not logged in and the login window component is not enabled
+			const { pathname } = getUrlParts( window.location.href );
+			if ( isReaderTagEmbedPage( window.location ) ) {
+				return window.open(
+					createAccountUrl( { redirectTo: pathname, ref: 'reader-lp' } ),
+					'_blank'
+				);
+			}
+			// Do not redirect to create account page when not logged in and the login window component is enabled
+			if ( ! config.isEnabled( 'reader/login-window' ) ) {
+				return navigate( createAccountUrl( { redirectTo: pathname, ref: 'reader-lp' } ) );
+			}
+		}
+	};
+
 	handleReply = () => {
 		if ( ! this.props.isLoggedIn ) {
 			const { pathname } = getUrlParts( window.location.href );
@@ -115,10 +134,20 @@ class PostComment extends PureComponent {
 					'_blank'
 				);
 			}
-			return navigate( createAccountUrl( { redirectTo: pathname, ref: 'reader-lp' } ) );
+			// Do not redirect to create account page when not logged in and the login window component is enabled
+			if ( ! config.isEnabled( 'reader/login-window' ) ) {
+				return navigate( createAccountUrl( { redirectTo: pathname, ref: 'reader-lp' } ) );
+			}
+			this.props.registerLastActionRequiresLogin( {
+				type: 'reply',
+				siteId: this.props.post.site_ID,
+				postId: this.props.post.ID,
+				commentId: this.props.commentId,
+			} );
+		} else {
+			this.props.onReplyClick( this.props.commentId );
+			this.setState( { showReplies: true } ); // show the comments when replying
 		}
-		this.props.onReplyClick( this.props.commentId );
-		this.setState( { showReplies: true } ); // show the comments when replying
 	};
 
 	handleAuthorClick = ( event ) => {
@@ -469,6 +498,7 @@ class PostComment extends PureComponent {
 					activeReplyCommentId={ this.props.activeReplyCommentId }
 					commentId={ this.props.commentId }
 					handleReply={ this.handleReply }
+					onLikeToggle={ this.onLikeToggle }
 					onReplyCancel={ this.props.onReplyCancel }
 					showReadMore={ overflowY && ! this.state.showFull && showReadMoreInActions }
 					onReadMore={ this.onReadMore }
@@ -494,7 +524,7 @@ const ConnectedPostComment = connect(
 		currentUser: getCurrentUser( state ),
 		isLoggedIn: isUserLoggedIn( state ),
 	} ),
-	{ expandComments, recordReaderTracksEvent }
+	{ expandComments, recordReaderTracksEvent, registerLastActionRequiresLogin }
 )( withDimensions( PostComment ) );
 
 export default ConnectedPostComment;
