60a58cab142e7284b771793cb4b6ec95273a1abe
Mikael Korpela <mikael@ihminen.org>
2023-10-26 17:38:45 +0300

    Welcome message: allow for all sites to modify

--- client/my-sites/site-settings/settings-newsletter/EmailsTextSetting.tsx
+++ client/my-sites/site-settings/settings-newsletter/EmailsTextSetting.tsx
@@ -4,8 +4,6 @@ import FormLabel from 'calypso/components/forms/form-label';
 import FormLegend from 'calypso/components/forms/form-legend';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
 import FormTextarea from 'calypso/components/forms/form-textarea';
-import { useSelector } from 'calypso/state';
-import { isSimpleSite as isSimpleSiteSelector } from 'calypso/state/sites/selectors';
 import { SubscriptionOptions } from '../settings-reading/main';
 
 type EmailsTextSettingProps = {
@@ -20,7 +18,6 @@ type SubscriptionOption = {
 
 export const EmailsTextSetting = ( { value, disabled, updateFields }: EmailsTextSettingProps ) => {
 	const translate = useTranslate();
-	const isSimpleSite = useSelector( isSimpleSiteSelector );
 
 	const updateSubscriptionOptions =
 		( option: string ) => ( event: React.ChangeEvent< HTMLInputElement > ) => {
@@ -44,6 +41,37 @@ export const EmailsTextSetting = ( { value, disabled, updateFields }: EmailsText
 				<FormLegend>
 					{ translate( 'These settings change the emails sent from your site to your readers' ) }
 				</FormLegend>
+
+				<FormLabel htmlFor="welcome_email_message">
+					{ translate( 'Welcome email message' ) }
+				</FormLabel>
+				<FormTextarea
+					name="welcome_email_message"
+					id="welcome_email_message"
+					value={ value?.welcome }
+					onChange={ updateSubscriptionOptions( 'welcome' ) }
+					disabled={ disabled }
+					autoCapitalize="none"
+				/>
+				<FormSettingExplanation>
+					{ translate( 'The email sent out when someone confirms their subscription.' ) }
+				</FormSettingExplanation>
+
+				<FormLabel htmlFor="comment_follow_email_message">
+					{ translate( 'Comment follow email message' ) }
+				</FormLabel>
+				<FormTextarea
+					name="comment_follow_email_message"
+					id="comment_follow_email_message"
+					value={ value?.comment_follow }
+					onChange={ updateSubscriptionOptions( 'comment_follow' ) }
+					disabled={ disabled }
+					autoCapitalize="none"
+				/>
+				<FormSettingExplanation>
+					{ translate( 'The email sent out when someone follows one of your posts.' ) }
+				</FormSettingExplanation>
+
 				<FormLabel htmlFor="confirmation_email_message">
 					{ translate( 'Confirmation email message' ) }
 				</FormLabel>
@@ -59,40 +87,6 @@ export const EmailsTextSetting = ( { value, disabled, updateFields }: EmailsText
 					{ translate(
 						'The ability to customize the confirmation email message had to be disabled to prevent abuse. It will revert to the default message for all new subscribers.'
 					) }
-				</FormSettingExplanation>
-
-				{ isSimpleSite && (
-					<>
-						<FormLabel htmlFor="welcome_email_message">
-							{ translate( 'Welcome email message' ) }
-						</FormLabel>
-						<FormTextarea
-							name="welcome_email_message"
-							id="welcome_email_message"
-							value={ value?.welcome }
-							onChange={ updateSubscriptionOptions( 'welcome' ) }
-							disabled={ disabled }
-							autoCapitalize="none"
-						/>
-						<FormSettingExplanation>
-							{ translate( 'The email sent out when someone confirms their subscription.' ) }
-						</FormSettingExplanation>
-					</>
-				) }
-
-				<FormLabel htmlFor="comment_follow_email_message">
-					{ translate( 'Comment follow email message' ) }
-				</FormLabel>
-				<FormTextarea
-					name="comment_follow_email_message"
-					id="comment_follow_email_message"
-					value={ value?.comment_follow }
-					onChange={ updateSubscriptionOptions( 'comment_follow' ) }
-					disabled={ disabled }
-					autoCapitalize="none"
-				/>
-				<FormSettingExplanation>
-					{ translate( 'The email sent out when someone follows one of your posts.' ) }
 				</FormSettingExplanation>
 			</FormFieldset>
 		</div>
