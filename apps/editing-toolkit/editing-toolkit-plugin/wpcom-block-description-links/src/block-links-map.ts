cdc68be4ff2915667b88283865f231a442c0ed72
Tony Arcangelini <tony@arcangelini.com>
2023-10-26 18:43:25 +0200

    Blogroll: add support link

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-description-links/src/block-links-map.ts
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-description-links/src/block-links-map.ts
@@ -6,6 +6,7 @@ const blockLinks: { [ key: string ]: string } = {
 	 */
 	'core/template-part':
 		'https://wordpress.com/support/full-site-editing/theme-blocks/template-part-block/',
+
 	'core/site-title':
 		'https://wordpress.com/support/full-site-editing/theme-blocks/site-title-block/',
 
@@ -131,6 +132,8 @@ const blockLinks: { [ key: string ]: string } = {
 	/**
 	 * Jetpack Blocks
 	 */
+	'jetpack/blogroll': 'https://wordpress.com/support/wordpress-editor/blocks/blogroll-block/',
+
 	'jetpack/timeline': 'https://wordpress.com/support/wordpress-editor/blocks/timeline-block/',
 
 	'jetpack/story': 'https://wordpress.com/support/wordpress-editor/blocks/story-block/',
