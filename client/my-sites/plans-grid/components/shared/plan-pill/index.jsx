db4acec4997faa3610c536df38890d21e7ec9652
Christos <chriskmnds@gmail.com>
2023-10-26 12:29:39 +0300

    update css class. plan-pill -> plans-grid__plan-pill

--- client/my-sites/plans-grid/components/shared/plan-pill/index.jsx
+++ client/my-sites/plans-grid/components/shared/plan-pill/index.jsx
@@ -1,7 +1,7 @@
 import './style.scss';
 
 export default ( props ) => (
-	<div className={ `plan-pill${ props.isInSignup ? ' is-in-signup' : '' }` }>
+	<div className={ `plans-grid__plan-pill${ props.isInSignup ? ' is-in-signup' : '' }` }>
 		{ props.children }
 	</div>
 );
