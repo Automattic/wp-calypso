61933f9ae8434386b4e998ece85aaf25f92ca9ad
Grzegorz Chudzinski-Pawlowski <112354940+grzegorz-cp@users.noreply.github.com>
2023-10-27 08:30:27 +1100

    Stats: Adding date picker events

--- client/components/stats-date-control/stats-date-control-picker.tsx
+++ client/components/stats-date-control/stats-date-control-picker.tsx
@@ -1,8 +1,10 @@
+import config from '@automattic/calypso-config';
 import { Popover } from '@automattic/components';
 import { Button } from '@wordpress/components';
 import { Icon, calendar } from '@wordpress/icons';
 import React, { useState, useRef } from 'react';
 import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import DateControlPickerDate from './stats-date-control-picker-date';
 import DateControlPickerShortcuts from './stats-date-control-picker-shortcuts';
 import { DateControlPickerProps, DateControlPickerShortcut } from './types';
@@ -17,6 +19,8 @@ const DateControlPicker = ( {
 	onApply,
 }: DateControlPickerProps ) => {
 	const moment = useLocalizedMoment();
+	const isOdysseyStats = config.isEnabled( 'is_running_in_jetpack_site' );
+
 	// Pull dates from provided range.
 	const [ inputStartDate, setInputStartDate ] = useState(
 		moment( dateRange.chartStart ).format( 'YYYY-MM-DD' )
@@ -67,9 +71,20 @@ const DateControlPicker = ( {
 		onShortcut( shortcut );
 	};
 
+	const togglePopoverVisibility = () => {
+		const event_from = isOdysseyStats ? 'jetpack_odyssey' : 'calypso';
+
+		if ( popoverOpened ) {
+			// record an event for opening the date picker
+			recordTracksEvent( `${ event_from }_stats_date_picker_opened` );
+		}
+
+		togglePopoverOpened( ! popoverOpened );
+	};
+
 	return (
 		<div className="stats-date-control-picker">
-			<Button onClick={ () => togglePopoverOpened( ! popoverOpened ) } ref={ infoReferenceElement }>
+			<Button onClick={ togglePopoverVisibility } ref={ infoReferenceElement }>
 				{ buttonLabel }
 				<Icon className="gridicon" icon={ calendar } />
 			</Button>
