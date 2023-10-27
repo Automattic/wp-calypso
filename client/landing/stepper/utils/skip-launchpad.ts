a9ea91643566011e048cee432fa2996e509e7df7
Dale du Preez <dale@automattic.com>
2023-10-27 11:56:51 +0200

    Add skipLaunchpad() helper and update flows to call it

---
+++ client/landing/stepper/utils/skip-launchpad.ts
@@ -0,0 +1,29 @@
+import { updateLaunchpadSettings } from '@automattic/data-stores';
+
+type SkipLaunchpadProps = {
+	checklistSlug?: string | null;
+	setActiveChecklist: ( siteSlug: string, checklistSlug: string ) => Promise< void >;
+	siteId: string | null;
+	siteSlug: string | null;
+};
+
+export const skipLaunchpad = async ( {
+	checklistSlug,
+	setActiveChecklist,
+	siteId,
+	siteSlug,
+}: SkipLaunchpadProps ) => {
+	if ( siteSlug ) {
+		if ( checklistSlug ) {
+			// If we're making both API calls, allow them to happen concurrently.
+			await Promise.allSettled( [
+				updateLaunchpadSettings( siteSlug, { launchpad_screen: 'skipped' } ),
+				setActiveChecklist( siteSlug, checklistSlug ),
+			] );
+		} else {
+			await updateLaunchpadSettings( siteSlug, { launchpad_screen: 'skipped' } );
+		}
+	}
+
+	return window.location.assign( `/home/${ siteId ?? siteSlug }` );
+};
