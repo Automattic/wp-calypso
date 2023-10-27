c944e77e58c3b5c8de9c7c25b2b54820f10da0f9
edanzer <edanzer@gmail.com>
2023-10-26 19:41:27 -0600

    Update styling of magic login confirmation

--- client/login/magic-login/emailed-login-link-successfully.jsx
+++ client/login/magic-login/emailed-login-link-successfully.jsx
@@ -1,4 +1,4 @@
-import { Card, Gridicon } from '@automattic/components';
+import { Card } from '@automattic/components';
 import { localize } from 'i18n-calypso';
 import page from 'page';
 import PropTypes from 'prop-types';
@@ -45,18 +45,24 @@ class EmailedLoginLinkSuccessfully extends Component {
 		const { translate, emailAddress } = this.props;
 		const line = [
 			emailAddress
-				? translate( 'We just emailed a link to %(emailAddress)s.', {
-						args: {
-							emailAddress,
-						},
-				  } )
+				? translate(
+						'If you have a WordPress.com account, we’ve sent an email to {{span}} %(emailAddress)s {{/span}} with a link you can use to sign in.',
+						{
+							args: {
+								emailAddress,
+							},
+							components: {
+								span: <span className="magic-login__confirmation-email" />,
+							},
+						}
+				  )
 				: translate( 'We just emailed you a link.' ),
 			' ',
 			translate( 'Please check your inbox and click the link to log in.' ),
 		];
 
 		return (
-			<div>
+			<div className="magic-login__confirmation">
 				<RedirectWhenLoggedIn
 					redirectTo="/help"
 					replaceCurrentLocation={ true }
@@ -78,8 +84,7 @@ class EmailedLoginLinkSuccessfully extends Component {
 						} ) }
 						onClick={ this.onClickBackLink }
 					>
-						<Gridicon icon="arrow-left" size={ 18 } />
-						{ translate( 'Back to login' ) }
+						{ translate( 'Enter a password instead' ) }
 					</a>
 				</div>
 			</div>
