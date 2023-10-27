6b075b7378495e1c918f49e5a351392378edcb4e
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-10-27 15:07:34 +0200

    Update UpgradePlan screen to match a new design

--- client/blocks/importer/wordpress/import-everything/pre-migration/upgrade-plan-details.tsx
+++ client/blocks/importer/wordpress/import-everything/pre-migration/upgrade-plan-details.tsx
@@ -1,54 +1,39 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { getPlan, PLAN_BUSINESS } from '@automattic/calypso-products';
-import { check, plus, Icon } from '@wordpress/icons';
+import { getPlan, PLAN_BUSINESS, PLAN_BUSINESS_MONTHLY } from '@automattic/calypso-products';
+import { CloudLogo, Button } from '@automattic/components';
+import { Title } from '@automattic/onboarding';
 import { useI18n } from '@wordpress/react-i18n';
 import classnames from 'classnames';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import ButtonGroup from 'calypso/components/button-group';
 import QueryPlans from 'calypso/components/data/query-plans';
-import { getFeatureByKey } from 'calypso/lib/plans/features-list';
 import PlanPrice from 'calypso/my-sites/plan-price';
+import { Plans2023Tooltip } from 'calypso/my-sites/plans-grid/components/plans-2023-tooltip';
+import { useManageTooltipToggle } from 'calypso/my-sites/plans-grid/hooks/use-manage-tooltip-toggle';
 import { useSelector } from 'calypso/state';
 import { getCurrentUserCurrencyCode } from 'calypso/state/currency-code/selectors';
 import { getPlanRawPrice } from 'calypso/state/plans/selectors';
+import { UpgradePlanFeatureList } from './upgrade-plan-feature-list';
 
-export const UpgradePlanDetails = () => {
+interface Props {
+	children: React.ReactNode;
+}
+
+export const UpgradePlanDetails = ( props: Props ) => {
 	const { __ } = useI18n();
-	const initialFeaturesNumber = 6;
+	const [ activeTooltipId, setActiveTooltipId ] = useManageTooltipToggle();
 
-	const planType = PLAN_BUSINESS;
-	const plan = getPlan( planType );
+	const plan = getPlan( PLAN_BUSINESS );
+	const { children } = props;
+	const [ selectedPlan, setSelectedPlan ] = useState<
+		typeof PLAN_BUSINESS | typeof PLAN_BUSINESS_MONTHLY
+	>( PLAN_BUSINESS_MONTHLY );
+	const plan = getPlan( selectedPlan );
 	const planId = plan?.getProductId();
-	const [ visibleFeaturesNum, setVisibleFeatureNum ] = useState( initialFeaturesNumber );
-
-	const features =
-		plan && 'getPlanCompareFeatures' in plan
-			? ( plan?.getPlanCompareFeatures?.() || [] )
-					.map( ( feature: string ) => getFeatureByKey( feature )?.getTitle() )
-					.filter( ( x ) => x )
-			: [];
 
 	const currencyCode = useSelector( getCurrentUserCurrencyCode );
 	const rawPrice = useSelector( ( state ) => getPlanRawPrice( state, planId as number, true ) );
-
-	function renderFeatureList() {
-		return (
-			<ul className={ classnames( 'import__details-list' ) }>
-				{ features.slice( 0, visibleFeaturesNum ).map( ( feature, i ) => (
-					<li className={ classnames( 'import__upgrade-plan-feature' ) } key={ i }>
-						<Icon size={ 20 } icon={ check } />
-						<span>{ feature }</span>
-					</li>
-				) ) }
-				{ visibleFeaturesNum < features.length && (
-					<li className={ classnames( 'import__upgrade-plan-feature-more' ) }>
-						<button onClick={ () => setVisibleFeatureNum( features.length ) }>
-							<Icon size={ 20 } icon={ plus } /> { __( 'See more' ) }
-						</button>
-					</li>
-				) }
-			</ul>
-		);
-	}
 
 	useEffect( () => {
 		recordTracksEvent( 'calypso_site_importer_migration_plan_display' );
@@ -58,14 +43,55 @@ export const UpgradePlanDetails = () => {
 		<div className={ classnames( 'import__upgrade-plan' ) }>
 			<QueryPlans />
 
+			<div className={ classnames( 'import__upgrade-plan-period-switcher' ) }>
+				<ButtonGroup>
+					<Button
+						borderless={ true }
+						className={ classnames( { selected: selectedPlan === PLAN_BUSINESS_MONTHLY } ) }
+						onClick={ () => setSelectedPlan( PLAN_BUSINESS_MONTHLY ) }
+					>
+						{ __( 'Pay monthly' ) }
+					</Button>
+					<Button
+						borderless={ true }
+						className={ classnames( { selected: selectedPlan === PLAN_BUSINESS } ) }
+						onClick={ () => setSelectedPlan( PLAN_BUSINESS ) }
+					>
+						{ __( 'Pay annually' ) }
+					</Button>
+				</ButtonGroup>
+			</div>
+
 			<div className={ classnames( 'import__upgrade-plan-container' ) }>
-				<div className={ classnames( 'import__upgrade-plan-price' ) }>
-					<h3 className={ classnames( 'plan-title' ) }>{ plan?.getTitle() }</h3>
-					<PlanPrice rawPrice={ rawPrice ?? undefined } currencyCode={ currencyCode } />
-					<span className={ classnames( 'plan-time-frame' ) }>{ plan?.getBillingTimeFrame() }</span>
+				<div className={ classnames( 'import__upgrade-plan-header' ) }>
+					<Plans2023Tooltip
+						text={ __(
+							'WP Cloud gives you the tools you need to add scalable, highly available, extremely fast WordPress hosting.'
+						) }
+						id="wp-cloud-logo"
+						setActiveTooltipId={ setActiveTooltipId }
+						activeTooltipId={ activeTooltipId }
+					>
+						<CloudLogo />
+					</Plans2023Tooltip>
+					<Title className="plan-title" tagName="h2">
+						{ plan?.getTitle() }
+					</Title>
+					<small>{ __( 'Unlock the power of WordPress with plugins and cloud tools.' ) }</small>
 				</div>
+
+				<div className={ classnames( 'import__upgrade-plan-price' ) }>
+					<PlanPrice rawPrice={ rawPrice ?? undefined } currencyCode={ currencyCode } />
+					<span className={ classnames( 'plan-time-frame' ) }>
+						<small>{ plan?.getBillingTimeFrame() }</small>
+						<small>{ __( 'Refundable within 14 days. No questions asked.' ) }</small>
+					</span>
+				</div>
+
+				<div className={ classnames( 'import__upgrade-plan-cta' ) }>{ children }</div>
+
 				<div className={ classnames( 'import__upgrade-plan-details' ) }>
-					{ renderFeatureList() }
+					<UpgradePlanFeatureList plan={ plan } />
 				</div>
 			</div>
 		</div>
--- client/blocks/importer/wordpress/import-everything/pre-migration/upgrade-plan.tsx
+++ client/blocks/importer/wordpress/import-everything/pre-migration/upgrade-plan.tsx
@@ -1,12 +1,11 @@
 import { isEnabled } from '@automattic/calypso-config';
 import { getPlan, PLAN_BUSINESS } from '@automattic/calypso-products';
-import { Button, Popover } from '@automattic/components';
+import { Button } from '@automattic/components';
 import { SiteDetails } from '@automattic/data-stores';
 import { Title, SubTitle, NextButton } from '@automattic/onboarding';
 import { useTranslate } from 'i18n-calypso';
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { convertToFriendlyWebsiteName } from 'calypso/blocks/import/util';
 import useCheckEligibilityMigrationTrialPlan from 'calypso/data/plans/use-check-eligibility-migration-trial-plan';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import UpgradePlanDetails from './upgrade-plan-details';
@@ -27,7 +26,6 @@ export const PreMigrationUpgradePlan: React.FunctionComponent< Props > = ( props
 	const translate = useTranslate();
 	const plan = getPlan( PLAN_BUSINESS );
 	const {
-		sourceSiteUrl,
 		targetSite,
 		startImport,
 		onFreeTrialClick,
@@ -42,8 +40,6 @@ export const PreMigrationUpgradePlan: React.FunctionComponent< Props > = ( props
 		migrationTrialEligibility?.eligible ||
 		// If the user's email is unverified, we still want to show the trial plan option
 		migrationTrialEligibility?.error_code === 'email-unverified';
-	const [ popoverVisible, setPopoverVisible ] = useState( false );
-	const trialBtnRef: React.RefObject< HTMLButtonElement > = useRef( null );
 
 	useEffect( () => {
 		dispatch(
@@ -56,65 +52,48 @@ export const PreMigrationUpgradePlan: React.FunctionComponent< Props > = ( props
 			<div className="import__heading-title">
 				<Title>{ translate( 'Upgrade your plan' ) }</Title>
 				<SubTitle>
-					{ translate( 'Migrating themes, plugins, users, and settings requires a %(plan)s plan', {
+					{ translate( 'Migrating themes, plugins, users, and settings requires a %(plan)s plan.', {
 						args: {
 							plan: plan?.getTitle() ?? '',
 						},
 					} ) }
+					<br />
+					{ ! isEligibleForTrialPlan &&
+						translate(
+							'To just migrate the content, use the {{link}}free content-only import option{{/link}}.',
+							{
+								components: {
+									link: <Button borderless={ true } onClick={ onContentOnlyClick } />,
+								},
+							}
+						) }
 				</SubTitle>
 			</div>
-			<p>
-				{ translate(
-					'Your entire site %(from)s will be migrated to %(to)s, overriding the content in your destination site',
-					{
-						args: {
-							from: convertToFriendlyWebsiteName( sourceSiteUrl ),
-							to: convertToFriendlyWebsiteName( targetSite.URL ),
-						},
-					}
-				) }
-			</p>
-			<UpgradePlanDetails />
-			<div className="import__footer-button-container">
+
+			<UpgradePlanDetails>
 				<NextButton isBusy={ isBusy } onClick={ () => startImport() }>
 					{ translate( 'Upgrade and migrate' ) }
 				</NextButton>
 				{ isEnabled( 'plans/migration-trial' ) && (
-					<Button
-						ref={ trialBtnRef }
-						busy={ isBusy }
-						borderless={ true }
-						className="action-buttons__borderless"
-						onClick={ () => isEligibleForTrialPlan && onFreeTrialClick() }
-						onFocus={ () => ! isEligibleForTrialPlan && setPopoverVisible( true ) }
-						onBlur={ () => setPopoverVisible( false ) }
-						onMouseEnter={ () => ! isEligibleForTrialPlan && setPopoverVisible( true ) }
-						onMouseLeave={ () => setPopoverVisible( false ) }
-					>
-						{ translate( 'Try it for free' ) }
-					</Button>
+					<>
+						<Button
+							busy={ isBusy }
+							disabled={ ! isEligibleForTrialPlan }
+							transparent={ true }
+							onClick={ onFreeTrialClick }
+						>
+							{ translate( 'Try 7-days for free' ) }
+						</Button>
+						{ ! isEligibleForTrialPlan && (
+							<small>
+								{ translate(
+									'Free trials are a one-time offer and you’ve already enrolled in one in the past.'
+								) }
+							</small>
+						) }
+					</>
 				) }
-				{ ! isEligibleForTrialPlan && (
-					<Button
-						borderless={ true }
-						className="action-buttons__borderless"
-						onClick={ onContentOnlyClick }
-					>
-						{ translate( 'Use the content-only import option' ) }
-					</Button>
-				) }
-			</div>
-
-			<Popover
-				className="info-popover__tooltip info-popover__tooltip--trial-plan"
-				focusOnShow={ false }
-				context={ trialBtnRef.current }
-				isVisible={ popoverVisible }
-			>
-				{ translate(
-					'Free trials are a one-time offer and you’ve already enrolled in one in the past.'
-				) }
-			</Popover>
+			</UpgradePlanDetails>
 		</div>
 	);
 };
