f307925718131dba3f948476c34e8f28ac5e3da3
Jeremy Yip <jeremy.yip@automattic.com>
2023-10-27 16:45:35 -0700

    Storage Add-ons: Expose add-on upsells to plans page (#83005)
    * Expose add-on upsells to plans page
    
    * Add selected storage add-on to checkout cart
    
    * Add storage add-on upsells to spotlight plans
    
    * Query product list on load
    
    The product list is needed to derive information about storage add-ons on the /plans page
    
    * Display purchased or default storage option on load

--- client/my-sites/add-ons/hooks/use-add-on-checkout-link.ts
+++ client/my-sites/add-ons/hooks/use-add-on-checkout-link.ts
@@ -1,3 +1,4 @@
+import { useCallback } from '@wordpress/element';
 import { useSelector } from 'react-redux';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 
@@ -10,20 +11,23 @@ import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 const useAddOnCheckoutLink = (): ( ( addOnSlug: string, quantity?: number ) => string ) => {
 	const selectedSite = useSelector( getSelectedSite );
+	const checkoutLinkCallback = useCallback(
+		( addOnSlug: string, quantity?: number ): string => {
+			// If no site is provided, return the checkout link with the add-on (will render site-selector).
+			if ( ! selectedSite ) {
+				return `/checkout/${ addOnSlug }`;
+			}
 
-	return ( addOnSlug: string, quantity?: number ): string => {
-		// If no site is provided, return the checkout link with the add-on (will render site-selector).
-		if ( ! selectedSite ) {
-			return `/checkout/${ addOnSlug }`;
-		}
+			const checkoutLinkFormat = `/checkout/${ selectedSite?.slug }/${ addOnSlug }`;
 
-		const checkoutLinkFormat = `/checkout/${ selectedSite?.slug }/${ addOnSlug }`;
-
-		if ( quantity ) {
-			return checkoutLinkFormat + `:-q-${ quantity }`;
-		}
-		return checkoutLinkFormat;
-	};
+			if ( quantity ) {
+				return checkoutLinkFormat + `:-q-${ quantity }`;
+			}
+			return checkoutLinkFormat;
+		},
+		[ selectedSite ]
+	);
+	return checkoutLinkCallback;
 };
 
 export default useAddOnCheckoutLink;
