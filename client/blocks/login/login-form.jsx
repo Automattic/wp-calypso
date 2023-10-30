0f4f5f6d106b0a6d52826e2b1a5a92dc06aec861
escapemanuele <escapemanuele@gmail.com>
2023-10-30 12:40:53 +0100

    Update copy and handle translation

--- client/blocks/login/login-form.jsx
+++ client/blocks/login/login-form.jsx
@@ -629,6 +629,24 @@ export class LoginForm extends Component {
 
 		const magicLoginPageLinkWithEmail = addQueryArgs( { email_address: emailAddress }, loginLink );
 
+		if (
+			hasTranslation(
+				'It seems you entered an incorrect password. Want to get a {{magicLoginLink}}login link{{/magicLoginLink}} via email?'
+			) ||
+			englishLocales.includes( this.props.locale )
+		) {
+			return this.props.translate(
+				'It seems you entered an incorrect password. Want to get a {{magicLoginLink}}login link{{/magicLoginLink}} via email?',
+				{
+					components: {
+						magicLoginLink: (
+							<a href={ magicLoginPageLinkWithEmail } onClick={ this.handleMagicLoginClick } />
+						),
+					},
+				}
+			);
+		}
+
 		return this.props.translate(
 			'Would you like to {{magicLoginLink}}receive a login link{{/magicLoginLink}}?',
 			{
@@ -828,10 +846,7 @@ export class LoginForm extends Component {
 							/>
 
 							{ requestError && requestError.field === 'password' && (
-								<FormInputValidation isError text={ requestError.message }>
-									{ ' ' }
-									{ this.renderMagicLoginLink() }
-								</FormInputValidation>
+								<FormInputValidation isError text={ this.renderMagicLoginLink() } />
 							) }
 						</div>
 					</div>
