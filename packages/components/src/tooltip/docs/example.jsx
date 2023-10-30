61c325d829d3df0770447f32ba8977375690c988
Christos <chriskmnds@gmail.com>
2023-10-30 09:31:23 +0200

    Tooltip to packages (Step 1): Add Tooltip to components package

---
+++ packages/components/src/tooltip/docs/example.jsx
@@ -0,0 +1,85 @@
+import { PureComponent, createRef } from 'react';
+import FormLabel from 'calypso/components/forms/form-label';
+import FormSelect from 'calypso/components/forms/form-select';
+import TooltipComponent from 'calypso/components/tooltip';
+
+class Tooltip extends PureComponent {
+	tooltipRef = createRef();
+	state = {
+		position: 'bottom right',
+		show: false,
+	};
+
+	open = () => {
+		this.setState( { show: true } );
+	};
+
+	close = () => {
+		this.setState( { show: false } );
+	};
+
+	changePosition = ( event ) => {
+		this.setState( { position: event.target.value } );
+	};
+
+	render() {
+		const size = 30;
+
+		return (
+			<div>
+				<FormLabel>
+					Position
+					<FormSelect value={ this.state.position } onChange={ this.changePosition }>
+						<option value="top">top</option>
+						<option value="top left">top left</option>
+						<option value="top right">top right</option>
+						<option value="left">left</option>
+						<option value="right">right</option>
+						<option value="bottom">bottom</option>
+						<option value="bottom left">bottom left</option>
+						<option value="bottom right">bottom right</option>
+					</FormSelect>
+				</FormLabel>
+
+				<hr />
+
+				<div>
+					Tooltip context&nbsp;
+					<span
+						style={ {
+							width: size,
+							height: size,
+							lineHeight: `${ size }px`,
+							display: 'inline-block',
+							borderRadius: parseInt( size / 2 ),
+							backgroundColor: '#444',
+							color: 'white',
+							fontSize: '12px',
+							cursor: 'pointer',
+							textAlign: 'center',
+						} }
+						onMouseEnter={ this.open }
+						onMouseLeave={ this.close }
+						onClick={ this.close }
+						ref={ this.tooltipRef }
+					>
+						T
+					</span>
+					<TooltipComponent
+						id="tooltip__example"
+						isVisible={ this.state.show }
+						onClose={ this.close }
+						position={ this.state.position }
+						context={ this.tooltipRef.current }
+					>
+						<div style={ { padding: '10px' } }>Simple Tooltip Instance</div>
+					</TooltipComponent>
+				</div>
+			</div>
+		);
+	}
+}
+
+Tooltip.displayName = 'Tooltip';
+
+export default Tooltip;
