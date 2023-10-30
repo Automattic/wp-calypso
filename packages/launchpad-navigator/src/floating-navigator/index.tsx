cd95707bc156f949c1cd591dcfd75592df3d809a
valterlorran <valterlorran@hotmail.com>
2023-10-30 07:56:32 -0300

    Launchpad: Use the Modal component on the Launchpad (#83552)
    * Use the Modal component on the Launchpad navigator
    
    * fix the styles for the title

--- packages/launchpad-navigator/src/floating-navigator/index.tsx
+++ packages/launchpad-navigator/src/floating-navigator/index.tsx
@@ -1,7 +1,6 @@
-import { Card, Gridicon } from '@automattic/components';
 import { LaunchpadNavigator } from '@automattic/data-stores';
 import { DefaultWiredLaunchpad } from '@automattic/launchpad';
-import { Button } from '@wordpress/components';
+import { Modal } from '@wordpress/components';
 import { select } from '@wordpress/data';
 import { useTranslate } from 'i18n-calypso';
 
@@ -26,23 +25,17 @@ const FloatingNavigator = ( { siteSlug, toggleLaunchpadIsVisible }: FloatingNavi
 	const setLaunchpadIsVisible = toggleLaunchpadIsVisible || ( () => {} );
 
 	return (
-		<Card className="launchpad-navigator__floating-navigator">
-			<div className="launchpad-navigator__floating-navigator-header">
-				<h2>{ translate( 'Next steps for your site' ) }</h2>
-				<Button
-					aria-label={ translate( 'Close task list modal' ) }
-					className="launchpad-navigator__floating-navigator-close-button"
-					onClick={ () => setLaunchpadIsVisible( false ) }
-				>
-					<Gridicon icon="cross" size={ 18 } />
-				</Button>
-			</div>
+		<Modal
+			title={ translate( 'Next steps for your site' ) }
+			className="launchpad-navigator__floating-navigator"
+			onRequestClose={ () => setLaunchpadIsVisible( false ) }
+		>
 			<DefaultWiredLaunchpad
 				siteSlug={ siteSlug }
 				checklistSlug={ checklistSlug }
 				launchpadContext={ launchpadContext }
 			/>
-		</Card>
+		</Modal>
 	);
 };
 
