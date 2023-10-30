f2195b15b0213779711149a9be0fdd152f41177b
gogdzl <diego.rodrigues@automattic.com>
2023-10-30 17:31:45 -0300

    Set initial value to managedSites

--- client/jetpack-cloud/sections/partner-portal/company-details-form/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/company-details-form/index.tsx
@@ -74,7 +74,7 @@ export default function CompanyDetailsForm( {
 	const [ contactPerson, setContactPerson ] = useState( initialValues.contactPerson ?? '' );
 	const [ companyWebsite, setCompanyWebsite ] = useState( initialValues.companyWebsite ?? '' );
 	const [ companyType, setCompanyType ] = useState( initialValues.companyType ?? '' );
-	const [ managedSites, setManagedSites ] = useState( initialValues.managedSites ?? '' );
+	const [ managedSites, setManagedSites ] = useState( initialValues.managedSites ?? '1-5' );
 	const [ partnerProgramOptIn, setPartnerProgramOptIn ] = useState( false );
 
 	const [ showPartnerProgramOptIn, setShowPartnerProgramOptIn ] = useState( false );
