d2327f22791a25c72bca80c8b3e4c6743d223b30
sejas <antonio@sejas.es>
2023-10-31 16:46:37 +0000

    Sync admin update, notice and isUpdating

--- client/my-sites/hosting/site-admin-interface-card/use-select-interface-mutation.ts
+++ client/my-sites/hosting/site-admin-interface-card/use-select-interface-mutation.ts
@@ -1,7 +1,9 @@
 import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import wp from 'calypso/lib/wp';
 import { useDispatch, useSelector } from 'calypso/state';
 import { requestAdminMenu } from 'calypso/state/admin-menu/actions';
+import { getIsRequestingAdminMenu } from 'calypso/state/admin-menu/selectors';
 import getRawSite from 'calypso/state/selectors/get-raw-site';
 import { receiveSite, requestSite } from 'calypso/state/sites/actions';
 
@@ -17,6 +19,11 @@ interface MutationError {
 	message: string;
 }
 
+interface UseSiteInterfaceMutationOptions
+	extends UseMutationOptions< MutationResponse, MutationError, string > {
+	onSuccess?: () => void;
+}
+
 function waitMs( ms: number ) {
 	return new Promise( ( resolve ) => {
 		setTimeout( () => {
@@ -27,11 +34,20 @@ function waitMs( ms: number ) {
 
 export const useSiteInterfaceMutation = (
 	siteId: number,
-	options: UseMutationOptions< MutationResponse, MutationError, string > = {}
+	options: UseSiteInterfaceMutationOptions = {}
 ) => {
 	const dispatch = useDispatch();
 	const site = useSelector( ( state ) => getRawSite( state, siteId ) );
+	const isRequestingMenu = useSelector( ( state ) => getIsRequestingAdminMenu( state ) );
+	const [ hasSuccessfullyFinished, setHasSuccessfullyFinished ] = useState( false );
+	useEffect( () => {
+		if ( hasSuccessfullyFinished && ! isRequestingMenu ) {
+			setHasSuccessfullyFinished( false );
+			options?.onSuccess?.();
+		}
+	}, [ hasSuccessfullyFinished, isRequestingMenu, options ] );
 	const queryKey = [ SET_SITE_INTERFACE_MUTATION_KEY, siteId ];
+
 	const mutation = useMutation< MutationResponse, MutationError, string >( {
 		mutationFn: async ( value: string ) => {
 			const response = await wp.req.post(
@@ -49,7 +65,8 @@ export const useSiteInterfaceMutation = (
 		},
 		mutationKey: queryKey,
 		onSuccess: ( ...params ) => {
-			options?.onSuccess?.( ...params );
+			dispatch( requestAdminMenu( siteId ) );
+			setHasSuccessfullyFinished( true );
 			const [ data ] = params;
 			if ( ! data?.interface || ! site ) {
 				throw new Error( 'Invalid response from hosting/admin-interface' );
@@ -60,7 +77,6 @@ export const useSiteInterfaceMutation = (
 			};
 			// Apply the new interface option to the site on redux store
 			dispatch( receiveSite( { ...site, options: newOptions } ) );
-			dispatch( requestAdminMenu( siteId ) );
 		},
 		onMutate: options?.onMutate,
 		onError( _err: MutationError, _newActive: string, prevValue: unknown ) {
@@ -74,7 +90,8 @@ export const useSiteInterfaceMutation = (
 	const { mutate } = mutation;
 
 	return {
-		setSiteInterface: mutate,
 		...mutation,
+		setSiteInterface: mutate,
+		isLoading: mutation.isLoading || isRequestingMenu,
 	};
 };
