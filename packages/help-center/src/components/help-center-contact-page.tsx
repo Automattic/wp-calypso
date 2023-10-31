453d3512324a205ec3251088dcf6ec7305a0543a
Jorge Calle <jorge.calle@automattic.com>
2023-10-31 12:20:55 -0500

    Remove comment

--- packages/help-center/src/components/help-center-contact-page.tsx
+++ packages/help-center/src/components/help-center-contact-page.tsx
@@ -129,7 +129,6 @@ export const HelpCenterContactPage: FC = () => {
 			<div className="help-center-contact-page__content">
 				<h3>{ __( 'Contact our WordPress.com experts', __i18n_text_domain__ ) }</h3>
 				{ supportActivity && <HelpCenterActiveTicketNotice tickets={ supportActivity } /> }
-				{ /* Easter */ }
 				<GMClosureNotice
 					displayAt="2023-11-06 00:00Z"
 					closesAt="2023-11-11 00:00Z"
