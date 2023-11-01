a06e94e1a82ca562762dde5055128d7aacdec8fc
Chris Diehl <jjchrisdiehl@gmail.com>
2023-10-31 17:40:06 -0700

    Add feature flag for checkout-version

--- packages/wpcom-checkout/src/checkout-version-checker.ts
+++ packages/wpcom-checkout/src/checkout-version-checker.ts
@@ -1,3 +1,5 @@
+import config from '@automattic/calypso-config';
+
 export function hasCheckoutVersion( version: string ) {
 	/**
 	 * This function can be used to query the checkoutVersion parameter from the URL. Useful for A/B testing.
@@ -8,10 +10,11 @@ export function hasCheckoutVersion( version: string ) {
 	if ( typeof window === 'undefined' ) {
 		return false;
 	}
-
+	const isCheckoutVersioned = config.isEnabled( 'checkout/checkout-version' );
 	const urlParams = new URLSearchParams( window.location.search );
+
 	const checkoutVersion = urlParams.get( 'checkoutVersion' );
-	if ( checkoutVersion === version ) {
+	if ( isCheckoutVersioned && checkoutVersion === version ) {
 		return true;
 	}
 	return false;
