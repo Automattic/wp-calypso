d0ca17f9c7eac90d862ada32ee936011f5f534a3
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-11-02 17:32:59 +0530

    Social previews | Add Nextdoor preview (#83746)
    * Add Nextdoor icons
    
    * Add Nextdoor post preview
    
    * Add link preview
    
    * Expose preview components
    
    * Update CHANGELOG.md
    
    * Wire up Nextdoor preview for Calypso posts UI
    
    * Bump version

---
+++ packages/social-previews/src/nextdoor-preview/constants.ts
@@ -0,0 +1,2 @@
+export const FEED_TEXT_MAX_LENGTH = 160;
+export const FEED_TEXT_MAX_LINES = 2;
---
+++ packages/social-previews/src/nextdoor-preview/icons/chevron-icon.tsx
@@ -0,0 +1,11 @@
+export function ChevronIcon() {
+	return (
+		<svg width="20" height="20" viewBox="0 0 20 20" aria-hidden="true">
+			<path
+				fill="#dfe1e4"
+				fillRule="evenodd"
+				d="M7.127 13.876a.732.732 0 1 0 1.035 1.035l4.75-4.749a.732.732 0 0 0 0-1.035L8.123 4.34A.732.732 0 0 0 7.09 5.375l4.27 4.27-4.232 4.23Z"
+			></path>
+		</svg>
+	);
+}
