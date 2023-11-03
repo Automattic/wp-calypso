ac1d81d50dac93783934c46427ae4da0fe908a31
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2023-11-03 14:38:04 +0200

    calypso-e2e: Update translateFromPage utility to accept context (#83860)

--- packages/calypso-e2e/src/lib/utils/translate.ts
+++ packages/calypso-e2e/src/lib/utils/translate.ts
@@ -3,12 +3,21 @@ import { Locator } from 'playwright';
 /**
  * Translate string by evaluating the `wp.i18n__` translate function from the page.
  */
-export async function translateFromPage( locator: Locator, string: string ): Promise< string > {
+export async function translateFromPage(
+	locator: Locator,
+	string: string,
+	context?: string
+): Promise< string > {
 	return (
 		locator.evaluate(
 			// eslint-disable-next-line @wordpress/i18n-no-variables
-			( _el, string ) => ( window as any )?.wp?.i18n?.__( string ),
-			string
+			( _el, [ string, context ] ) =>
+				context === undefined
+					? // eslint-disable-next-line @wordpress/i18n-no-variables
+					  ( window as any )?.wp?.i18n?.__( string )
+					: // eslint-disable-next-line @wordpress/i18n-no-variables
+					  ( window as any )?.wp?.i18n?._x( string, context ),
+			[ string, context ]
 		) || Promise.resolve( string )
 	);
 }
