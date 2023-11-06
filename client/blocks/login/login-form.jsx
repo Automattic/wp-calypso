d4162f099cbda40510750ed0fa8baaa1ca625389
Renan <renansscarvalho@gmail.com>
2023-11-06 09:23:43 -0300

    Check user type

--- client/blocks/login/login-form.jsx
+++ client/blocks/login/login-form.jsx
@@ -193,13 +193,13 @@ export class LoginForm extends Component {
 	}
 
 	isUsernameOrEmailView() {
-		const { hasAccountTypeLoaded, socialAccountIsLinking } = this.props;
+		const { hasAccountTypeLoaded, socialAccountIsLinking, userEmail, accountType } = this.props;
 
 		return (
-			! userEmail &&
+			userEmail && accountType !== 'passwordless',
 			! socialAccountIsLinking &&
-			! hasAccountTypeLoaded &&
-			! ( this.props.isWoo && ! this.props.isPartnerSignup )
+				! hasAccountTypeLoaded &&
+				! ( this.props.isWoo && ! this.props.isPartnerSignup )
 		);
 	}
 
