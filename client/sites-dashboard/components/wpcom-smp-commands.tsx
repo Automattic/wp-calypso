10b98cfcc20a1ce7f057e849f28d5b5f617ac041
sejas <antonio@sejas.es>
2023-11-06 14:00:09 +0000

    Restore dns command with nested sites picker

--- client/sites-dashboard/components/wpcom-smp-commands.tsx
+++ client/sites-dashboard/components/wpcom-smp-commands.tsx
@@ -4,9 +4,12 @@ import {
 	commentAuthorAvatar as profileIcon,
 } from '@wordpress/icons';
 import MaterialIcon from 'calypso/components/material-icon';
+import { SiteExcerptData } from 'calypso/data/sites/site-excerpt-types';
 import { navigate } from 'calypso/lib/navigate';
+import { isCustomDomain, isNotAtomicJetpack } from '../utils';
 
 export const generateCommandsArrayWpcom = ( {
+	setSelectedCommandName,
 	createSiteUrl,
 	__,
 }: {
@@ -14,6 +17,13 @@ export const generateCommandsArrayWpcom = ( {
 	createSiteUrl: string;
 	__: ( text: string ) => string;
 } ) => {
+	const setStateCallback =
+		( actionName: string ) =>
+		( { setSearch }: { setSearch: ( search: string ) => void } ) => {
+			setSearch( '' );
+			setSelectedCommandName( actionName );
+		};
+
 	const commands = [
 		{
 			name: 'addNewSite',
@@ -68,6 +78,22 @@ export const generateCommandsArrayWpcom = ( {
 			callback: ( { close }: { close: () => void } ) => {
 				close();
 				navigate( `domains/manage` );
+			},
+			icon: domainsIcon,
+		},
+		{
+			name: 'manageDns',
+			label: __( 'Manage DNS records' ),
+			searchLabel: __( 'manage dns' ),
+			context: 'Managing DNS records',
+			callback: setStateCallback( 'manageDns' ),
+			siteFunctions: {
+				onClick: ( { site, close }: { site: SiteExcerptData; close: () => void } ) => {
+					close();
+					navigate( `/domains/manage/${ site.slug }/dns/${ site.slug }` );
+				},
+				filter: ( site: SiteExcerptData ) =>
+					isCustomDomain( site.slug ) && ! isNotAtomicJetpack( site ),
 			},
 			icon: domainsIcon,
 		},
