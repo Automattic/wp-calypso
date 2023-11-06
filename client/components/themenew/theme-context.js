7d726052a007a88545677530b4078027f7522e89
Copons <Copons@users.noreply.github.com>
2023-11-06 19:13:35 +0000

    Fix self-hosted themes

--- client/components/themenew/theme-context.js
+++ client/components/themenew/theme-context.js
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState } from 'react';
 import { useSelector } from 'calypso/state';
 import { getTheme } from 'calypso/state/themes/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 const ThemeContext = createContext( {
 	position: 1,
@@ -12,8 +13,14 @@ const ThemeContext = createContext( {
 export const useThemeContext = () => useContext( ThemeContext );
 
 export function ThemeContextProvider( { children, position, themeId } ) {
+	/**
+	 * @todo Generalize this selector
+	 */
 	const theme = useSelector(
-		( state ) => getTheme( state, 'wpcom', themeId ) || getTheme( state, 'wporg', themeId )
+		( state ) =>
+			getTheme( state, 'wpcom', themeId ) ||
+			getTheme( state, 'wporg', themeId ) ||
+			getTheme( state, getSelectedSiteId( state ), themeId )
 	);
 
 	const [ selectedStyleVariation, setSelectedStyleVariation ] = useState();
