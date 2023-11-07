0948e8ffb3eaa8a548d70bd75554ca1053f52d51
Tim Broddin <tim@wannabes.be>
2023-11-07 16:50:19 +0100

    Update modal opening mechanism to respond to URL hash changes

--- client/my-sites/subscribers/components/add-subscribers-modal/add-subscribers-modal.tsx
+++ client/my-sites/subscribers/components/add-subscribers-modal/add-subscribers-modal.tsx
@@ -20,10 +20,22 @@ const AddSubscribersModal = ( { site }: AddSubscribersModalProps ) => {
 		useSubscribersPage();
 
 	useEffect( () => {
-		// Open "add subscribers" modal by default via URL
-		if ( window.location.hash === '#add-subscribers' ) {
-			setShowAddSubscribersModal( true );
-		}
+		const handleHashChange = () => {
+			// Open "add subscribers" via URL hash
+			if ( window.location.hash === '#add-subscribers' ) {
+				setShowAddSubscribersModal( true );
+			}
+		};
+
+		// Listen to the hashchange event
+		window.addEventListener( 'hashchange', handleHashChange );
+
+		// Make it work on load as well
+		handleHashChange();
+
+		return () => {
+			window.removeEventListener( 'hashchange', handleHashChange );
+		};
 	}, [] );
 
 	const modalTitle = translate( 'Add subscribers to %s', {
