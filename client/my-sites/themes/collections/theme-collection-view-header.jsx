7d8f6b427216b2b1347551989af7143f46a1c574
Richard Ortiz <rcrd.ortiz@gmail.com>
2023-11-07 14:25:16 +0100

    The LiTS now displays the collection view as specified in the designs

--- client/my-sites/themes/collections/theme-collection-view-header.jsx
+++ client/my-sites/themes/collections/theme-collection-view-header.jsx
@@ -1,32 +1,49 @@
 import { Button } from '@wordpress/components';
 import { chevronLeft } from '@wordpress/icons';
+import classNames from 'classnames';
 import { translate } from 'i18n-calypso';
 import page from 'page';
+import NavigationHeader from 'calypso/components/navigation-header';
 import { preventWidows } from 'calypso/lib/formatting';
 import { THEME_COLLECTIONS } from 'calypso/my-sites/themes/collections/collection-definitions';
 import './theme-collection-view-header.scss';
 
-export default function ThemeCollectionViewHeader( { backUrl, filter, tier } ) {
+export default function ThemeCollectionViewHeader( { backUrl, filter, tier, isLoggedIn } ) {
 	const keyParts = [ tier, filter ];
 	const key = keyParts.filter( ( part ) => !! part ).join( '-' ) || 'recommended';
-	const { fullTitle, description } = THEME_COLLECTIONS[ key ];
+	const { title, fullTitle, description } = THEME_COLLECTIONS[ key ];
+
+	const classnames = classNames( 'theme-collection-view-header', {
+		'is-logged-in': isLoggedIn,
+	} );
+
+	const navigationItems = [
+		{ label: 'Themes', href: backUrl },
+		{ label: title, href: '' },
+	];
 
 	return (
-		<div className="theme-collection-view-header">
-			<Button
-				className="theme-collection-view-header__back"
-				icon={ chevronLeft }
-				onClick={ () => page( backUrl ) }
-				variant="link"
-			>
-				{ translate( 'Back' ) }
-			</Button>
-			{ fullTitle && <h2 className="theme-collection-view-header__title">{ fullTitle }</h2> }
-			{ description && (
-				<div className="theme-collection-view-header__description">
-					{ preventWidows( description ) }
-				</div>
+		<div className={ classnames }>
+			{ isLoggedIn ? (
+				<NavigationHeader navigationItems={ navigationItems } />
+			) : (
+				<Button
+					className="theme-collection-view-header__back"
+					icon={ chevronLeft }
+					onClick={ () => page( backUrl ) }
+					variant="link"
+				>
+					{ translate( 'Back' ) }
+				</Button>
 			) }
+			<div className="theme-collection-view-header__info">
+				{ fullTitle && <h2 className="theme-collection-view-header__title">{ fullTitle }</h2> }
+				{ description && (
+					<div className="theme-collection-view-header__description">
+						{ preventWidows( description ) }
+					</div>
+				) }
+			</div>
 		</div>
 	);
 }
