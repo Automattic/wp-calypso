1a8dda5d7c1661ad89fdd1978d081369fb733c30
Richard Ortiz <rcrd.ortiz@gmail.com>
2023-11-08 13:27:58 +0100

    Updated the collection header to use breadcrumbs and FormattedHeader components

--- client/my-sites/themes/collections/theme-collection-view-header.jsx
+++ client/my-sites/themes/collections/theme-collection-view-header.jsx
@@ -1,17 +1,16 @@
-import { Button } from '@wordpress/components';
-import { chevronLeft } from '@wordpress/icons';
 import classNames from 'classnames';
-import { translate } from 'i18n-calypso';
-import page from 'page';
-import NavigationHeader from 'calypso/components/navigation-header';
+import React from 'react';
+import Breadcrumb from 'calypso/components/breadcrumb';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { preventWidows } from 'calypso/lib/formatting';
+import CollectionBackButton from 'calypso/my-sites/themes/collections/collection-back-button';
 import { THEME_COLLECTIONS } from 'calypso/my-sites/themes/collections/collection-definitions';
 import './theme-collection-view-header.scss';
 
 export default function ThemeCollectionViewHeader( { backUrl, filter, tier, isLoggedIn } ) {
 	const keyParts = [ tier, filter ];
 	const key = keyParts.filter( ( part ) => !! part ).join( '-' ) || 'recommended';
-	const { title, fullTitle, description } = THEME_COLLECTIONS[ key ];
+	const { title, description } = THEME_COLLECTIONS[ key ];
 
 	const classnames = classNames( 'theme-collection-view-header', {
 		'is-logged-in': isLoggedIn,
@@ -25,25 +24,15 @@ export default function ThemeCollectionViewHeader( { backUrl, filter, tier, isLo
 	return (
 		<div className={ classnames }>
 			{ isLoggedIn ? (
-				<NavigationHeader navigationItems={ navigationItems } />
+				<Breadcrumb items={ navigationItems } hideWhenOnlyOneLevel />
 			) : (
-				<Button
-					className="theme-collection-view-header__back"
-					icon={ chevronLeft }
-					onClick={ () => page( backUrl ) }
-					variant="link"
-				>
-					{ translate( 'Back' ) }
-				</Button>
+				<CollectionBackButton backUrl={ backUrl } />
 			) }
-			<div className="theme-collection-view-header__info">
-				{ fullTitle && <h2 className="theme-collection-view-header__title">{ fullTitle }</h2> }
-				{ description && (
-					<div className="theme-collection-view-header__description">
-						{ preventWidows( description ) }
-					</div>
-				) }
-			</div>
+			<FormattedHeader
+				align="left"
+				headerText={ title }
+				subHeaderText={ preventWidows( description ) }
+			/>
 		</div>
 	);
 }
