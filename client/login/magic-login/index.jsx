0a3a46e7b05bec7b9f9d3cab0fa8ba6b63cfac3d
Renan <renansscarvalho@gmail.com>
2023-11-08 17:05:11 +0100

    Show loading while sending email

--- client/login/magic-login/index.jsx
+++ client/login/magic-login/index.jsx
@@ -10,6 +10,7 @@ import { connect } from 'react-redux';
 import AppPromo from 'calypso/blocks/app-promo';
 import GlobalNotices from 'calypso/components/global-notices';
 import JetpackHeader from 'calypso/components/jetpack-header';
+import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
 import LocaleSuggestions from 'calypso/components/locale-suggestions';
 import Main from 'calypso/components/main';
 import {
@@ -452,6 +453,14 @@ class MagicLogin extends Component {
 		);
 	}
 
+	renderLoading() {
+		return (
+			<div className="magic-login__loading">
+				<LoadingEllipsis />
+			</div>
+		);
+	}
+
 	render() {
 		const { oauth2Client, showCheckYourEmail } = this.props;
 
@@ -488,7 +497,11 @@ class MagicLogin extends Component {
 
 				<GlobalNotices id="notices" />
 
-				<RequestLoginEmailForm { ...requestLoginEmailFormProps } />
+				{ this.props.isSendingEmail ? (
+					this.renderLoading()
+				) : (
+					<RequestLoginEmailForm { ...requestLoginEmailFormProps } />
+				) }
 
 				{ this.renderLinks() }
 			</Main>
