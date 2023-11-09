b459fb529dc66cf30bcdbff13427d9d94ee8406d
escapemanuele <escapemanuele@gmail.com>
2023-11-09 12:08:42 +0100

    Switch to useTranslate

--- client/lib/authentication/index.tsx
+++ client/lib/authentication/index.tsx
@@ -1,12 +1,12 @@
-import { useI18n } from '@wordpress/react-i18n';
+import { useTranslate } from 'i18n-calypso';
 import './style.scss';
 
 export const FormDivider = () => {
-	const { __ } = useI18n();
+	const translate = useTranslate();
 
 	return (
 		<div className="auth-form__separator">
-			<div className="auth-form__separator-text">{ __( 'or' ) }</div>
+			<div className="auth-form__separator-text">{ translate( 'or' ) }</div>
 		</div>
 	);
 };
