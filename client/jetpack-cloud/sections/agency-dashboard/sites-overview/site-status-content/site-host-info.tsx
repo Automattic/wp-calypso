226c92e2da8b8886bcfecf51dede619e6ec14cd7
Yashwin <yashwinpoojary@gmail.com>
2023-11-09 16:38:22 +0530

    Create SiteHostInfo component

---
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content/site-host-info.tsx
@@ -0,0 +1,52 @@
+import { isEnabled } from '@automattic/calypso-config';
+import { WordPressLogo } from '@automattic/components';
+import classNames from 'classnames';
+import { useTranslate } from 'i18n-calypso';
+import useWPPlanName from './hooks/use-wp-plan-name';
+import type { Site } from '../types';
+
+export const SiteHostInfo = ( {
+	site,
+	isLargeScreen = false,
+}: {
+	site: Site;
+	isLargeScreen?: boolean;
+} ) => {
+	const translate = useTranslate();
+
+	const isWPCOMAtomicSiteCreationEnabled = isEnabled(
+		'jetpack/pro-dashboard-wpcom-atomic-hosting'
+	);
+
+	const isWPCOMAtomicSite = site.is_atomic;
+
+	const planName = useWPPlanName( site.active_paid_subscription_slugs );
+
+	return (
+		isWPCOMAtomicSiteCreationEnabled &&
+		( isLargeScreen ? (
+			<div className="site-host-info">
+				<WordPressLogo
+					className={ classNames( 'wordpress-logo', { 'is-visible': isWPCOMAtomicSite } ) }
+					size={ 18 }
+				/>
+			</div>
+		) : (
+			isWPCOMAtomicSite && (
+				<div className="site-card__expanded-content-list site-card__content-list-no-error">
+					<div className="site-card__expanded-content-header">
+						<span className="site-card__expanded-content-key">{ translate( 'Host' ) }</span>
+						<span className="site-card__expanded-content-value">
+							<span className="site-card__expanded-content-status">
+								<div className="site-host-info">
+									<WordPressLogo className="wordpress-logo is-visible" size={ 18 } />
+								</div>
+								{ planName && <span className="site-host-info__plan-name">{ planName }</span> }
+							</span>
+						</span>
+					</div>
+				</div>
+			)
+		) )
+	);
+};
