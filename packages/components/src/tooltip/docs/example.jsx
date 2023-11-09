8c64107e69821769886ddcf4e4ee12ff3611a874
Christos <chriskmnds@gmail.com>
2023-11-09 13:41:32 +0200

    ignore code style warnings

--- packages/components/src/tooltip/docs/example.jsx
+++ packages/components/src/tooltip/docs/example.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { PureComponent, createRef } from 'react';
 import TooltipComponent from '../';
 import { FormLabel } from '../../forms';
