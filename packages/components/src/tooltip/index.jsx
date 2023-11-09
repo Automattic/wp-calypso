07808594f1c702fc78c982093d7f30767e07f5a8
Christos <chriskmnds@gmail.com>
2023-11-09 14:05:23 +0200

    Tooltip to packages (Step 1): Add Tooltip to components package (#83644)
    * Tooltip to packages (Step 1): Add Tooltip to components package
    
    * fix references
    
    * add storybook demo
    
    cleanup
    
    * Add yarn lock update for viewport-react package
    
    * more fancy storybook
    
    * ignore code style warnings
    
    ---------
    
    Co-authored-by: Jeremy Yip <jeremy.yip@automattic.com>

---
+++ packages/components/src/tooltip/index.jsx
@@ -0,0 +1,56 @@
+import { useMobileBreakpoint } from '@automattic/viewport-react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import Popover from '../popover';
+
+import './style.scss';
+
+function Tooltip( props ) {
+	const isMobile = useMobileBreakpoint();
+
+	if ( ! props.showOnMobile && isMobile ) {
+		return null;
+	}
+
+	const classes = classnames( [ 'tooltip', props.className ], {
+		[ `is-${ props.status }` ]: props.status,
+	} );
+
+	return (
+		<Popover
+			autoPosition={ props.autoPosition }
+			className={ classes }
+			context={ props.context }
+			id={ props.id }
+			isVisible={ props.isVisible }
+			position={ props.position }
+			showDelay={ props.showDelay }
+			hideArrow
+		>
+			{ props.children }
+		</Popover>
+	);
+}
+
+Tooltip.propTypes = {
+	autoPosition: PropTypes.bool,
+	className: PropTypes.string,
+	id: PropTypes.string,
+	isVisible: PropTypes.bool,
+	position: PropTypes.string,
+	status: PropTypes.string,
+	showDelay: PropTypes.number,
+	showOnMobile: PropTypes.bool,
+	hideArrow: PropTypes.bool,
+	children: PropTypes.node,
+	context: PropTypes.any,
+};
+
+Tooltip.defaultProps = {
+	showDelay: 100,
+	position: 'top',
+	showOnMobile: false,
+	hideArrow: false,
+};
+
+export default Tooltip;
