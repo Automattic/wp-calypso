40315fc114b22aaac5ea0842abe0ce039286b2f6
Louis Laugesen <louis.laugesen@gmail.com>
2023-11-10 16:02:57 +1100

    Add product_slugs as a query option

--- client/components/data/query-products-list/index.jsx
+++ client/components/data/query-products-list/index.jsx
@@ -16,13 +16,23 @@ const request =
 		dispatch( requestProductsList( props ) );
 	};
 
-export function useQueryProductsList( { type = 'all', currency, persist } = {} ) {
+/**
+ *
+ * @param {Object} props 			The list of component props.
+ * @param {string} [props.type] 	The type of products to request:
+ *									"jetpack" for Jetpack products only, or undefined for all products.
+ * @param {string} [props.currency] The currency code to override the currency used on the account.
+ * @param {boolean} [props.persist] Set to true to persist the products list in the store.
+ * @param {string} [props.product_slugs] A comma-separated list of product to return. If not specified all products are returned.
+ * @returns {null} 					No visible output.
+ */
+export function useQueryProductsList( { type = 'all', currency, persist, product_slugs } = {} ) {
 	const dispatch = useDispatch();
 
 	// Only runs on mount.
 	useEffect( () => {
-		dispatch( request( { type, currency, persist } ) );
-	}, [ dispatch, type, persist, currency ] );
+		dispatch( request( { type, currency, persist, product_slugs } ) );
+	}, [ dispatch, type, persist, currency, product_slugs ] );
 
 	return null;
 }
@@ -34,8 +44,9 @@ export function useQueryProductsList( { type = 'all', currency, persist } = {} )
  *									"jetpack" for Jetpack products only, or undefined for all products.
  * @param {string} [props.currency] The currency code to override the currency used on the account.
  * @param {boolean} [props.persist] Set to true to persist the products list in the store.
+ * @param {string} [props.product_slugs] A comma-separated list of product to return. If not specified all products are returned.
  * @returns {null} 					No visible output.
  */
-export default function QueryProductsList( { type = 'all', currency, persist } ) {
-	return useQueryProductsList( { type, currency, persist } );
+export default function QueryProductsList( { type = 'all', currency, persist, product_slugs } ) {
+	return useQueryProductsList( { type, currency, persist, product_slugs } );
 }
