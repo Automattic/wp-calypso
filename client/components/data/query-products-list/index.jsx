eb583779a47e168e92e25e07a685907782a993a2
Louis Laugesen <louis.laugesen@gmail.com>
2023-11-10 16:13:13 +1100

    redef

--- client/components/data/query-products-list/index.jsx
+++ client/components/data/query-products-list/index.jsx
@@ -23,16 +23,16 @@ const request =
  *									"jetpack" for Jetpack products only, or undefined for all products.
  * @param {string} [props.currency] The currency code to override the currency used on the account.
  * @param {boolean} [props.persist] Set to true to persist the products list in the store.
- * @param {string} [props.product_slugs] A comma-separated list of product to return. If not specified all products are returned.
+ * @param {string} [props.productSlugs] A comma-separated list of product to return. If not specified all products are returned.
  * @returns {null} 					No visible output.
  */
-export function useQueryProductsList( { type = 'all', currency, persist, product_slugs } = {} ) {
+export function useQueryProductsList( { type = 'all', currency, persist, productSlugs } = {} ) {
 	const dispatch = useDispatch();
 
 	// Only runs on mount.
 	useEffect( () => {
-		dispatch( request( { type, currency, persist, product_slugs } ) );
-	}, [ dispatch, type, persist, currency, product_slugs ] );
+		dispatch( request( { type, currency, persist, product_slugs: productSlugs } ) );
+	}, [ dispatch, type, persist, currency, productSlugs ] );
 
 	return null;
 }
@@ -44,9 +44,9 @@ export function useQueryProductsList( { type = 'all', currency, persist, product
  *									"jetpack" for Jetpack products only, or undefined for all products.
  * @param {string} [props.currency] The currency code to override the currency used on the account.
  * @param {boolean} [props.persist] Set to true to persist the products list in the store.
- * @param {string} [props.product_slugs] A comma-separated list of product to return. If not specified all products are returned.
+ * @param {string} [props.productSlugs] A comma-separated list of product to return. If not specified all products are returned.
  * @returns {null} 					No visible output.
  */
-export default function QueryProductsList( { type = 'all', currency, persist, product_slugs } ) {
-	return useQueryProductsList( { type, currency, persist, product_slugs } );
+export default function QueryProductsList( { type = 'all', currency, persist, productSlugs } ) {
+	return useQueryProductsList( { type, currency, persist, productSlugs } );
 }
