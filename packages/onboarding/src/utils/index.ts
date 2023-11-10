cc8b53390667e6362376e3c3beb6e91940dcc292
Anthony Grullon <agrullon95@gmail.com>
2023-11-10 18:42:36 +0700

    Move domain suggestion logic into packages/onboarding

--- packages/onboarding/src/utils/index.ts
+++ packages/onboarding/src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './contrastChecker';
 export * from './flows';
 export * from './use-data-loss-warning';
 export { doesStringResembleDomain } from './is-domain';
+export * from './domain-suggester';
