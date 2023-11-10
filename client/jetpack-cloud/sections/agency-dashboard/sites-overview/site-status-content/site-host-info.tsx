d87223efcdb35903f57db03cdf12c45c50a37340
Yashwin <yashwinpoojary@gmail.com>
2023-11-10 10:35:31 +0530

    Add tooltip for large screen view

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content/site-host-info.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content/site-host-info.tsx
@@ -2,6 +2,8 @@ import { isEnabled } from '@automattic/calypso-config';
 import { WordPressLogo } from '@automattic/components';
 import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
+import { useRef, useState } from 'react';
+import Tooltip from 'calypso/components/tooltip';
 import useWPPlanName from './hooks/use-wp-plan-name';
 import type { Site } from '../types';
 
@@ -14,6 +16,10 @@ export const SiteHostInfo = ( {
 } ) => {
 	const translate = useTranslate();
 
+	const [ showPopover, setShowPopover ] = useState( false );
+
+	const ref = useRef< HTMLDivElement | null >( null );
+
 	const isWPCOMAtomicSiteCreationEnabled = isEnabled(
 		'jetpack/pro-dashboard-wpcom-atomic-hosting'
 	);
@@ -22,15 +28,32 @@ export const SiteHostInfo = ( {
 
 	const planName = useWPPlanName( site.active_paid_subscription_slugs );
 
+	const props = {
+		className: 'site-host-info',
+		...( isWPCOMAtomicSite && {
+			ref,
+			role: 'button',
+			tabIndex: 0,
+			onMouseEnter: () => setShowPopover( true ),
+			onMouseLeave: () => setShowPopover( false ),
+			onMouseDown: () => setShowPopover( false ),
+		} ),
+	};
+
 	return (
 		isWPCOMAtomicSiteCreationEnabled &&
 		( isLargeScreen ? (
-			<div className="site-host-info">
-				<WordPressLogo
-					className={ classNames( 'wordpress-logo', { 'is-visible': isWPCOMAtomicSite } ) }
-					size={ 18 }
-				/>
-			</div>
+			<>
+				<div { ...props }>
+					<WordPressLogo
+						className={ classNames( 'wordpress-logo', { 'is-visible': isWPCOMAtomicSite } ) }
+						size={ 18 }
+					/>
+				</div>
+				<Tooltip context={ ref.current } isVisible={ showPopover }>
+					{ planName }
+				</Tooltip>
+			</>
 		) : (
 			isWPCOMAtomicSite && (
 				<div className="site-card__expanded-content-list site-card__content-list-no-error">
