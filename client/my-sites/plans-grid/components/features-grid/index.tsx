580f6da2e09a732af7d07be1720e05d2c9038334
Jeremy Yip <jeremy.yip@automattic.com>
2023-11-10 12:10:42 -0800

    Storage Add-ons: Release upsell to all onboarding flows (#83771)
    * Remove conditional guards for storage add-on upsells
    
    * Process storage add-on cart item for stepper flows

--- client/my-sites/plans-grid/components/features-grid/index.tsx
+++ client/my-sites/plans-grid/components/features-grid/index.tsx
@@ -511,14 +511,7 @@ class FeaturesGrid extends Component< FeaturesGridType > {
 	}
 
 	renderPlanStorageOptions( renderedGridPlans: GridPlan[], options?: PlanRowOptions ) {
-		const {
-			translate,
-			intervalType,
-			isInSignup,
-			flowName,
-			onStorageAddOnClick,
-			showUpgradeableStorage,
-		} = this.props;
+		const { translate, intervalType, onStorageAddOnClick, showUpgradeableStorage } = this.props;
 
 		return renderedGridPlans.map( ( { planSlug, features: { storageOptions } } ) => {
 			if ( ! options?.isTableCell && isWpcomEnterpriseGridPlan( planSlug ) ) {
@@ -527,15 +520,9 @@ class FeaturesGrid extends Component< FeaturesGridType > {
 
 			const shouldRenderStorageTitle =
 				storageOptions.length > 0 &&
-				( storageOptions.length === 1 ||
-					intervalType !== 'yearly' ||
-					! showUpgradeableStorage ||
-					( isInSignup && ! ( flowName === 'onboarding' ) ) );
-
+				( storageOptions.length === 1 || intervalType !== 'yearly' || ! showUpgradeableStorage );
 			const canUpgradeStorageForPlan = isStorageUpgradeableForPlan( {
-				flowName: flowName ?? '',
 				intervalType,
-				isInSignup,
 				showUpgradeableStorage,
 				storageOptions,
 			} );
