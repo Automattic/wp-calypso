6b2dc7d5616bb55986bd64e876700a5f69774fee
Anthony Grullon <agrullon95@gmail.com>
2023-11-10 18:57:52 +0700

    Update exports

--- packages/onboarding/src/utils/index.ts
+++ packages/onboarding/src/utils/index.ts
@@ -2,4 +2,4 @@ export * from './contrastChecker';
 export * from './flows';
 export * from './use-data-loss-warning';
 export { doesStringResembleDomain } from './is-domain';
-export * from './domain-suggester';
+export { suggestEmailCorrection, extractDomainWithExtension } from './domain-suggester';
