9923e07581584aa39c56087f6c449060bf862861
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2023-11-13 23:57:17 +0100

    Add skip to dashboard link to top-right

--- client/my-sites/plans/ecommerce-trial/business-upgrade-confirmation/index.tsx
+++ client/my-sites/plans/ecommerce-trial/business-upgrade-confirmation/index.tsx
@@ -1,17 +1,32 @@
+import { StepContainer } from '@automattic/onboarding';
 import { useTranslate } from 'i18n-calypso';
+import page from 'page';
+import { useLayoutEffect } from 'react';
 import QueryJetpackPlugins from 'calypso/components/data/query-jetpack-plugins';
 import Main from 'calypso/components/main';
 import BodySectionCssClass from 'calypso/layout/body-section-css-class';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
 import BusinessTrialIncluded from 'calypso/my-sites/plans/current-plan/trials/business-trial-included';
-import { useSelector } from 'calypso/state';
+import { useDispatch, useSelector } from 'calypso/state';
+import { hideMasterbar, showMasterbar } from 'calypso/state/ui/actions';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 import './style.scss';
 
+const noop = () => {};
+
 const BusinessUpgradeConfirmation = () => {
 	const selectedSite = useSelector( getSelectedSite );
+	const dispatch = useDispatch();
 	const translate = useTranslate();
+
+	useLayoutEffect( () => {
+		dispatch( hideMasterbar() );
+
+		return () => {
+			dispatch( showMasterbar() );
+		};
+	}, [ dispatch ] );
 
 	return (
 		<>
@@ -22,24 +37,37 @@ const BusinessUpgradeConfirmation = () => {
 					path="/plans/my-plan/trial-upgraded/:site"
 					title="Plans > Business Trial Post Upgrade Actions"
 				/>
-				<div className="trial-upgrade-confirmation__header">
-					<h1 className="trial-upgrade-confirmation__title">
-						{ translate( 'Welcome to the Business plan' ) }
-					</h1>
-					<div className="trial-upgrade-confirmation__subtitle">
-						<span className="trial-upgrade-confirmation__subtitle-line">
-							{ translate(
-								"Your purchase is complete, and you're now on the {{strong}}Business plan{{/strong}}. It's time to take your website to the next level. What would you like to do next?",
-								{
-									components: { strong: <strong /> },
-								}
-							) }
-						</span>
-					</div>
-				</div>
-				<div className="trial-upgrade-confirmation__tasks">
-					<BusinessTrialIncluded displayAll={ true } displayOnlyActionableItems />
-				</div>
+				<StepContainer
+					stepName="business-trial-upgraded"
+					hideBack
+					skipLabelText={ translate( 'Skip to dashboard' ) }
+					goNext={ () => page.redirect( `/home/${ selectedSite?.slug }` ) }
+					recordTracksEvent={ noop }
+					stepContent={
+						<>
+							<div className="trial-upgrade-confirmation__header">
+								<h1 className="trial-upgrade-confirmation__title">
+									{ translate( 'Welcome to the Business plan' ) }
+								</h1>
+								<div className="trial-upgrade-confirmation__subtitle">
+									<span className="trial-upgrade-confirmation__subtitle-line">
+										{ translate(
+											"Your purchase is complete, and you're now on the {{strong}}Business plan{{/strong}}. It's time to take your website to the next level—here are some options.",
+											{
+												components: {
+													strong: <strong />,
+												},
+											}
+										) }
+									</span>
+								</div>
+							</div>
+							<div className="trial-upgrade-confirmation__tasks">
+								<BusinessTrialIncluded displayAll={ true } displayOnlyActionableItems />
+							</div>
+						</>
+					}
+				/>
 			</Main>
 		</>
 	);
