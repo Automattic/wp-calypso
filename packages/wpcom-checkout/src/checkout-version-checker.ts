db7a5b48594ea7443e10c44d79e76ee075712592
Chris Diehl <jjchrisdiehl@gmail.com>
2023-11-15 13:21:39 -0800

    Add helper functions for feature flags

--- packages/wpcom-checkout/src/checkout-version-checker.ts
+++ packages/wpcom-checkout/src/checkout-version-checker.ts
@@ -19,3 +19,37 @@ export function hasCheckoutVersion( version: string ) {
 	}
 	return false;
 }
+
+/**
+ * Use hasFeatureFlagEnabled to check if a feature flag is enabled.
+ * @param flag - the environment config flag to check for
+ * @returns Boolean
+ */
+
+export function hasFeatureFlagEnabled( flag: string ) {
+	const featureFlag = config.isEnabled( flag );
+	if ( featureFlag ) {
+		return true;
+	}
+	return false;
+}
+
+/**
+ * Use hasQueryStringParameter to check if a query string parameter exists and has a specific value.
+ * @param param - the query string parameter to check
+ * @param value - the value to check for
+ * @returns - Boolean
+ * @example - hasQueryStringParameter( 'checkoutVersion', 'v2' )
+ */
+
+export function hasQueryStringParameter( param: string, value: string ) {
+	if ( typeof window === 'undefined' ) {
+		return false;
+	}
+	const urlParams = new URLSearchParams( window.location.search );
+	const paramValue = urlParams.get( param );
+	if ( paramValue === value ) {
+		return true;
+	}
+	return false;
+}
