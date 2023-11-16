a4805be77a8631cf3811e5e325766a8d0e41c04c
Marko Andrijasevic <marko.andrijasevic@automattic.com>
2023-11-16 15:20:20 +0100

    Test: run tests with Bun
    Runs part of the tests in the codebase that contain '.test.' in their
    file name with Bun.

--- test/build-tools/jest.config.js
+++ test/build-tools/jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
 	...base,
 	cacheDirectory: path.join( __dirname, '../../.cache/jest' ),
 	rootDir: '../../build-tools',
+	testPathIgnorePatterns: [ '.test.' ], // Files with .spec. in the name will be tested with bun
 };
