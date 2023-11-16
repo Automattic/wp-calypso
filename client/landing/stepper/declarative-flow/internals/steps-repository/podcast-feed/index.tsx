1eaa5e2075afdca11c4072abb1b86f234454adf8
Mikael Korpela <mikael@ihminen.org>
2023-11-16 16:08:24 +0100

    Fix categories

--- client/landing/stepper/declarative-flow/internals/steps-repository/podcast-feed/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/podcast-feed/index.tsx
@@ -2,14 +2,15 @@ import { StepContainer } from '@automattic/onboarding';
 import { TextControl, Button } from '@wordpress/components';
 import { useSelect } from '@wordpress/data'; // useDispatch
 import { useI18n } from '@wordpress/react-i18n';
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState, ChangeEvent } from 'react';
 import FormattedHeader from 'calypso/components/formatted-header';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormSettingExplanation from 'calypso/components/forms/form-setting-explanation';
 import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
-import PodcastTopicSelector from './podcast-topic-selector';
+import TopicsSelector from 'calypso/my-sites/site-settings/podcasting-details//topics-selector';
+// import PodcastTopicSelector from './podcast-topic-selector';
 import type { Step } from '../../types';
 import type { OnboardSelect } from '@automattic/data-stores';
 import './style.scss';
@@ -21,11 +22,10 @@ const PodcastFeed: Step = ( { navigation } ) => {
 	// const { setUserRealName, setPodcastCategories } = useDispatch( ONBOARD_STORE );
 
 	const [ name, setName ] = useState( '' );
-	/*
 	const [ podcastCategory1, setPodcastCategory1 ] = useState( '' );
 	const [ podcastCategory2, setPodcastCategory2 ] = useState( '' );
 	const [ podcastCategory3, setPodcastCategory3 ] = useState( '' );
-	*/
+
 	const state = useSelect( ( select ) => select( ONBOARD_STORE ) as OnboardSelect, [] ).getState();
 
 	useEffect( () => {
@@ -42,8 +42,8 @@ const PodcastFeed: Step = ( { navigation } ) => {
 		// setUserRealName( name );
 		//setPodcastCategories( [ podcastCategory1, podcastCategory2, podcastCategory3 ] );
 
-		//submit?.( { name, podcastCategory1, podcastCategory2, podcastCategory3 } );
-		submit?.();
+		submit?.( { name, podcastCategory1, podcastCategory2, podcastCategory3 } );
+		//submit?.();
 	};
 
 	return (
@@ -63,7 +63,7 @@ const PodcastFeed: Step = ( { navigation } ) => {
 			stepContent={
 				<form onSubmit={ handleSubmit } className="setup-form__form">
 					<FormFieldset>
-						<FormLabel htmlFor="setup-form-input-name">{ __( 'Your name' ) }</FormLabel>
+						<FormLabel htmlFor="setup-form-input-name">{ __( 'Podcast host' ) }</FormLabel>
 						<TextControl
 							name="setup-form-input-name"
 							id="setup-form-input-name"
@@ -72,7 +72,7 @@ const PodcastFeed: Step = ( { navigation } ) => {
 							placeholder={ __( 'Your name' ) }
 						/>
 					</FormFieldset>
-
+					{ name } { podcastCategory1 } { podcastCategory2 } { podcastCategory3 }
 					<FormFieldset>
 						<FormLabel htmlFor="podcasting_category_1">{ __( 'Podcast topics' ) }</FormLabel>
 						<FormSettingExplanation>
@@ -80,6 +80,7 @@ const PodcastFeed: Step = ( { navigation } ) => {
 								'Choose up to three categories how your podcast should be categorized within Apple Podcasts and other podcasting services.'
 							) }
 						</FormSettingExplanation>
+						{ /*
 						<PodcastTopicSelector
 							fields={ [] }
 							handleSelect={ ( event ) => {
@@ -106,6 +107,38 @@ const PodcastFeed: Step = ( { navigation } ) => {
 							} }
 							category="podcasting_category_3"
 						/>
+						*/ }
+						<TopicsSelector
+							id="podcasting_category_1"
+							name="podcasting_category_1"
+							onChange={ ( event: ChangeEvent< HTMLInputElement > ) => {
+								// eslint-disable-next-line no-console
+								console.log( 'PodcastTopicSelector 1', event );
+								setPodcastCategory1( event.target.value );
+							} }
+							value={ podcastCategory1 }
+						/>
+						<TopicsSelector
+							id="podcasting_category_2"
+							name="podcasting_category_2"
+							onChange={ ( event: ChangeEvent< HTMLInputElement > ) => {
+								// eslint-disable-next-line no-console
+								console.log( 'PodcastTopicSelector 2', event );
+								setPodcastCategory2( event.target.value );
+							} }
+							value={ podcastCategory2 }
+						/>
+						<TopicsSelector
+							id="podcasting_category_3"
+							name="podcasting_category_3"
+							onChange={ ( event: ChangeEvent< HTMLInputElement > ) => {
+								// eslint-disable-next-line no-console
+								console.log( 'PodcastTopicSelector 3', event );
+								setPodcastCategory3( event.target.value );
+							} }
+							value={ podcastCategory3 }
+						/>
+
 						<Button className="setup-form__submit" variant="primary" type="submit">
 							{ __( 'Continue' ) }
 						</Button>
