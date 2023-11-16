2f06d29e41f715e1a665eae417708093d7b9e510
Valter Lorran <valterlorran@hotmail.com>
2023-11-16 20:15:05 +0100

    Adds the tags header to the customer home reader component

--- client/my-sites/customer-home/cards/features/reader/index.tsx
+++ client/my-sites/customer-home/cards/features/reader/index.tsx
@@ -1,13 +1,51 @@
+import { useLocale } from '@automattic/i18n-utils';
+import { useQuery } from '@tanstack/react-query';
+import withDimensions from 'calypso/lib/with-dimensions';
+import wpcom from 'calypso/lib/wp';
 import { trackScrollPage } from 'calypso/reader/controller-helper';
+import DiscoverNavigation from 'calypso/reader/discover/discover-navigation';
+import { DEFAULT_TAB } from 'calypso/reader/discover/helper';
 import Stream from 'calypso/reader/stream';
 
-const ReaderCard = () => {
+import './style.scss';
+
+interface ReaderCardProps {
+	width: number;
+}
+
+const ReaderCard = ( props: ReaderCardProps ) => {
+	const locale = useLocale();
+
+	const { data: interestTags = [] } = useQuery( {
+		queryKey: [ 'read/interests', locale ],
+		queryFn: () =>
+			wpcom.req.get(
+				{
+					path: `/read/interests`,
+					apiNamespace: 'wpcom/v2',
+				},
+				{
+					_locale: locale,
+				}
+			),
+		select: ( data ) => {
+			return data.interests;
+		},
+	} );
+
 	return (
-		<Stream
-			streamKey="discover:recommended--dailyprompt"
-			trackScrollPage={ trackScrollPage.bind( null ) }
-		/>
+		<div className="customer-home-reader-card">
+			<DiscoverNavigation
+				width={ props.width }
+				selectedTab={ DEFAULT_TAB }
+				recommendedTags={ interestTags }
+			/>
+			<Stream
+				streamKey="discover:recommended--dailyprompt"
+				trackScrollPage={ trackScrollPage.bind( null ) }
+			/>
+		</div>
 	);
 };
 
-export default ReaderCard;
+export default withDimensions( ReaderCard );
