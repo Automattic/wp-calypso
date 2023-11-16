817f375a50b168fedaef607635f8c57fd8c0e218
Marko Andrijasevic <marko.andrijasevic@automattic.com>
2023-11-16 15:25:03 +0100

    Update comment

--- test/build-tools/jest.config.js
+++ test/build-tools/jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
 	...base,
 	cacheDirectory: path.join( __dirname, '../../.cache/jest' ),
 	rootDir: '../../build-tools',
-	testPathIgnorePatterns: [ '.test.' ], // Files with .spec. in the name will be tested with bun
+	testPathIgnorePatterns: [ '.test.' ], // Files with .test. in the name will be tested with Bun
 };
