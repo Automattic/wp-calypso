b915282ef9d483d45601a054e25837ae963007a4
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-11-16 14:50:05 +0800

    Refactor implementation to capture correct reference of site table.

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-content/hooks/use-dashboard-show-large-screen.ts
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-content/hooks/use-dashboard-show-large-screen.ts
@@ -9,35 +9,36 @@ const useDashboardShowLargeScreen = (
 ) => {
 	const isDesktop = useBreakpoint( DESKTOP_BREAKPOINT );
 
+	const [ overflowingBreakpoint, setOverflowingBreakpoint ] = useState( 0 );
 	const [ isOverflowing, setIsOverflowing ] = useState( false );
-	const [ tableWidth, setTableWidth ] = useState( 0 );
 
 	const checkIfOverflowing = useCallback( () => {
-		setIsOverflowing( tableWidth > containerRef?.current?.clientWidth );
-	}, [ containerRef, tableWidth ] );
-
-	// We will need to remember the table width once to properly check if it is overflowing
-	useEffect( () => {
 		const siteTableEle = siteTableRef ? siteTableRef.current : null;
 
-		if ( ! tableWidth && siteTableEle ) {
-			setTableWidth( siteTableEle.clientWidth );
+		if ( siteTableEle ) {
+			if ( siteTableEle.clientWidth > containerRef?.current?.clientWidth ) {
+				// We will need to remember the breakpoint where we overflowed so that we can
+				// check if we are still overflowing when the window is resized to bigger size
+				setOverflowingBreakpoint( siteTableEle.clientWidth );
+				setIsOverflowing( true );
+			}
+		} else if ( overflowingBreakpoint < containerRef?.current?.clientWidth ) {
+			setIsOverflowing( false );
 		}
-	}, [ siteTableRef, tableWidth ] );
+	}, [ siteTableRef, overflowingBreakpoint, containerRef ] );
 
-	useEffect(
-		() => {
-			checkIfOverflowing();
+	useEffect( () => {
+		window.addEventListener( 'resize', checkIfOverflowing );
+		return () => {
+			window.removeEventListener( 'resize', checkIfOverflowing );
+		};
+	}, [ checkIfOverflowing ] );
 
-			window.addEventListener( 'resize', checkIfOverflowing );
-			return () => {
-				window.removeEventListener( 'resize', checkIfOverflowing );
-			};
-		},
+	useEffect( () => {
+		checkIfOverflowing();
 		// Do not add checkIfOverflowing to the dependency array as it will cause an infinite loop
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-		[]
-	);
+	}, [] );
 
 	return isDesktop && ! isOverflowing;
 };
