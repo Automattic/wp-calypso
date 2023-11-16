ac24dbf3613922f8cef6ae0ddfc2ed744eb24dca
Gabriel Demichelis <gabrieldemichelis@gmail.com>
2023-11-16 19:06:10 +0100

    Add header & CSS

--- client/my-sites/customer-home/cards/features/reader/index.tsx
+++ client/my-sites/customer-home/cards/features/reader/index.tsx
@@ -1,12 +1,35 @@
+import { useTranslate } from 'i18n-calypso';
+import iconReaderLightbulb from 'calypso/assets/images/customer-home/reader-lightbulb.svg';
 import { trackScrollPage } from 'calypso/reader/controller-helper';
 import Stream from 'calypso/reader/stream';
 
+import './style.scss';
+
 const ReaderCard = () => {
+	const translate = useTranslate();
+
 	return (
-		<Stream
-			streamKey="discover:recommended--dailyprompt"
-			trackScrollPage={ trackScrollPage.bind( null ) }
-		/>
+		<>
+			<div className="reader-card customer-home__card">
+				<div className="reader-card__header">
+					<h2 className="reader-card__title">
+						<div className="reader-card__title-icon">
+							<img src={ iconReaderLightbulb } alt="" />
+						</div>
+						<span>{ translate( 'Increase Views by Engaging with Others' ) }</span>
+					</h2>
+					<span className="reader-card__subtitle">
+						{ translate(
+							'You have more chances to pomp up your views when joining new conversations.'
+						) }
+					</span>
+				</div>
+				<Stream
+					streamKey="discover:recommended--dailyprompt"
+					trackScrollPage={ trackScrollPage.bind( null ) }
+				/>
+			</div>
+		</>
 	);
 };
 
