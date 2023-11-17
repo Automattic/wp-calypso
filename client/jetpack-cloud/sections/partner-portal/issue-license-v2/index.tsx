a755eb99632757495b8ef6bf26e4e8caf656f358
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-11-17 15:55:39 +0800

    Add initial header contents.

--- client/jetpack-cloud/sections/partner-portal/issue-license-v2/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/issue-license-v2/index.tsx
@@ -1,7 +1,39 @@
+import { Button } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import Layout from 'calypso/jetpack-cloud/components/layout';
+import LayoutBody from 'calypso/jetpack-cloud/components/layout/body';
+import LayoutHeader, {
+	LayoutHeaderActions,
+	LayoutHeaderSubtitle as Subtitle,
+	LayoutHeaderTitle as Title,
+} from 'calypso/jetpack-cloud/components/layout/header';
+import LayoutTop from 'calypso/jetpack-cloud/components/layout/top';
+
 export default function IssueLicenseV2() {
+	const translate = useTranslate();
 	return (
-		<div>
-			<h1>Issue License</h1>
-		</div>
+		<Layout
+			className="issue-license-v2"
+			title={ translate( 'Issue a new License' ) }
+			wide
+			withBorder
+		>
+			<LayoutTop>
+				<LayoutHeader>
+					<Title>{ translate( 'Issue product licenses' ) } </Title>
+					<Subtitle>
+						{ translate( 'Select single product licenses or save when you issue in bulk' ) }
+					</Subtitle>
+
+					<LayoutHeaderActions>
+						<Button primary>Issue license </Button>
+					</LayoutHeaderActions>
+				</LayoutHeader>
+			</LayoutTop>
+
+			<LayoutBody>
+				<div>body here</div>
+			</LayoutBody>
+		</Layout>
 	);
 }
