d5e7b12d8491267d415a78f1ca56fdd4e1e1211d
Panos Kountanis <panosktn@gmail.com>
2023-11-21 20:19:06 +0200

    Update e2e tests for trial.

--- test/e2e/specs/onboarding/signup__videopress-tailored.ts
+++ test/e2e/specs/onboarding/signup__videopress-tailored.ts
@@ -10,7 +10,6 @@ import {
 	NewUserResponse,
 	RestAPIClient,
 	DomainSearchComponent,
-	CartCheckoutPage,
 } from '@automattic/calypso-e2e';
 import { Page, Browser } from 'playwright';
 import { apiCloseAccount } from '../shared';
@@ -24,7 +23,6 @@ describe( DataHelper.createSuiteTitle( 'Signup: Tailored VideoPress' ), () => {
 	const emailClient = new EmailClient();
 	let page: Page;
 	let domainSearchComponent: DomainSearchComponent;
-	let cartCheckoutPage: CartCheckoutPage;
 	let newUserDetails: NewUserResponse;
 
 	beforeAll( async () => {
@@ -40,20 +38,8 @@ describe( DataHelper.createSuiteTitle( 'Signup: Tailored VideoPress' ), () => {
 
 		it( 'Click Get Started', async function () {
 			// Return a button "key" for the button that is found on page.
-			// Only 1 should be present.
-			const foundButtonKey = await Promise.any( [
-				page.waitForSelector( 'text=Get started' ).then( () => 'get-started-button' ),
-				page.waitForSelector( 'img[alt="Showcase your work"]' ).then( () => 'portfolio-button' ),
-			] );
-
-			// if Get Started is present, its the normal flow, otherwise its the User Intent flow
-			// -- if user intent, lets choose the item that sends us to the normal flow
-			if ( 'get-started-button' === foundButtonKey ) {
-				await Promise.all( [ page.waitForNavigation(), page.click( 'text=Get started' ) ] );
-			} else {
-				await page.click( 'img[alt="Showcase your work"]' );
-				await page.click( 'button.intro__button' );
-			}
+			const startButtonText = 'Start a free trial';
+			await page.getByText( startButtonText ).click();
 		} );
 
 		it( 'Navigate VideoMaker Setup', async function () {
@@ -92,40 +78,15 @@ describe( DataHelper.createSuiteTitle( 'Signup: Tailored VideoPress' ), () => {
 		} );
 
 		it( 'Skip selecting a domain', async function () {
-			await Promise.all( [ page.waitForNavigation(), page.click( 'text=Decide later' ) ] );
+			await page.getByText( 'Decide later' ).click();
 		} );
 
-		it( 'Land in checkout cart', async function () {
-			cartCheckoutPage = new CartCheckoutPage( page );
-			const totalAmount = await cartCheckoutPage.getCheckoutTotalAmount();
-			expect( totalAmount ).toBeGreaterThan( 0 );
+		it( 'Land in processing', async function () {
+			await page.waitForURL( /.*processing.*/, { timeout: 20 * 1000 } );
 		} );
-
-		it( 'Enter billing and payment details', async function () {
-			const paymentDetails = DataHelper.getTestPaymentDetails();
-			await cartCheckoutPage.enterBillingDetails( paymentDetails );
-			await cartCheckoutPage.enterPaymentDetails( paymentDetails );
+		it( 'Navigate to site editor', async function () {
+			await page.waitForURL( /.*site-editor.*/, { timeout: 20 * 1000 } );
 		} );
-
-		it( 'Make purchase', async function () {
-			await cartCheckoutPage.purchase( { timeout: 90 * 1000 } );
-		} );
-
-		it( 'Skip upsell if present', async function () {
-			const selector = 'button[data-e2e-button="decline"]';
-			const locator = page.locator( selector );
-
-			try {
-				await locator.click( { timeout: 2 * 1000 } );
-			} catch {
-				// noop
-			}
-		} );
-
-		it( 'Navigate to launchpad', async function () {
-			await page.waitForURL( /.*launchpad.*/ );
-		} );
-
 		// This happens after we finish signup.
 		it( 'Get activation link', async function () {
 			const message = await emailClient.getLastMatchingMessage( {
