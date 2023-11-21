1a63a6ccebbaf8a0ba105fdb1df86c0e21d38c6e
Jarda Snajdr <jsnajdr@gmail.com>
2023-11-21 13:37:18 +0100

    Jetpack Cloud Sidebar: remove the redirectPage helper

--- client/jetpack-cloud/sections/sidebar-navigation/lib/sidebar.ts
+++
@@ -1,9 +0,0 @@
-import page from '@automattic/calypso-router';
-
-/**
- * Redirects to the specified path.
- * @param path The path to redirect to.
- */
-export const redirectPage = ( path: string ) => {
-	page( path );
-};
