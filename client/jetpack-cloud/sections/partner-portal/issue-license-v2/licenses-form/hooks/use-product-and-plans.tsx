61419b12a9eca1897fcfbe28ab79c53581f7d076
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-11-22 14:18:41 +0800

    Jetpack Manage: Rearrange products and plans listing in the Issue License page based on the new design. (#84363)
    * Convert current licenses container to grid box.
    
    * Rearrange license listing based on the new design.
    
    * Move products and plans logic to a hook.
    
    * Hide Woo extensions and VaultPress backup addons on bundle license view.
    
    * Fix hook performance issue.
    
    * Fix some errors in the nav component.
    
    * Code improvements.

---
+++ client/jetpack-cloud/sections/partner-portal/issue-license-v2/licenses-form/hooks/use-product-and-plans.tsx
@@ -0,0 +1,99 @@
+import { getQueryArg } from '@wordpress/url';
+import { useEffect, useMemo } from 'react';
+import {
+	isJetpackBundle,
+	isWooCommerceProduct,
+	isWpcomHostingProduct,
+} from 'calypso/jetpack-cloud/sections/partner-portal/lib';
+import { useDispatch, useSelector } from 'calypso/state';
+import useProductsQuery from 'calypso/state/partner-portal/licenses/hooks/use-products-query';
+import { getAssignedPlanAndProductIDsForSite } from 'calypso/state/partner-portal/licenses/selectors';
+import {
+	addSelectedProductSlugs,
+	clearSelectedProductSlugs,
+} from 'calypso/state/partner-portal/products/actions';
+import type { SiteDetails } from '@automattic/data-stores';
+
+type Props = {
+	selectedSite?: SiteDetails | null;
+};
+
+export default function useProductAndPlans( { selectedSite }: Props ) {
+	const { data, isLoading: isLoadingProducts } = useProductsQuery();
+	const dispatch = useDispatch();
+
+	useEffect( () => {
+		// If the user comes from the flow for adding a new payment method during an attempt to issue a license
+		// after the payment method is added, we will make an attempt to issue the chosen license automatically.
+		const defaultProductSlugs = getQueryArg( window.location.href, 'products' )
+			?.toString()
+			.split( ',' );
+		// Select the slugs included in the URL
+		defaultProductSlugs &&
+			dispatch(
+				addSelectedProductSlugs(
+					// Filter the bundles and select only individual products
+					defaultProductSlugs.filter( ( slug ) => ! isJetpackBundle( slug ) )
+				)
+			);
+
+		// Clear all selected slugs when navigating away from the page to avoid persisting the data.
+		return () => {
+			dispatch( clearSelectedProductSlugs() );
+		};
+	}, [ dispatch ] );
+
+	const addedPlanAndProducts = useSelector( ( state ) =>
+		selectedSite ? getAssignedPlanAndProductIDsForSite( state, selectedSite.ID ) : null
+	);
+
+	return useMemo( () => {
+		let allProductsAndBundles = data;
+
+		// Filter products & plan that are already assigned to a site
+		if ( selectedSite && addedPlanAndProducts && allProductsAndBundles ) {
+			allProductsAndBundles = allProductsAndBundles.filter(
+				( product ) => ! addedPlanAndProducts.includes( product.product_id )
+			);
+		}
+
+		const bundles =
+			allProductsAndBundles?.filter(
+				( { family_slug }: { family_slug: string } ) => family_slug === 'jetpack-packs'
+			) || [];
+		const backupAddons =
+			allProductsAndBundles
+				?.filter(
+					( { family_slug }: { family_slug: string } ) => family_slug === 'jetpack-backup-storage'
+				)
+				.sort( ( a, b ) => a.product_id - b.product_id ) || [];
+		const products =
+			allProductsAndBundles?.filter(
+				( { family_slug }: { family_slug: string } ) =>
+					family_slug !== 'jetpack-packs' &&
+					family_slug !== 'jetpack-backup-storage' &&
+					! isWooCommerceProduct( family_slug ) &&
+					! isWpcomHostingProduct( family_slug )
+			) || [];
+		const wooExtensions =
+			allProductsAndBundles?.filter( ( { family_slug }: { family_slug: string } ) =>
+				isWooCommerceProduct( family_slug )
+			) || [];
+
+		// We need the suggested products (i.e., the products chosen from the dashboard) to properly
+		// track if the user purchases a different set of products.
+		const suggestedProductSlugs = getQueryArg( window.location.href, 'product_slug' )
+			?.toString()
+			.split( ',' );
+
+		return {
+			isLoadingProducts,
+			allProductsAndBundles,
+			bundles,
+			backupAddons,
+			products,
+			wooExtensions,
+			suggestedProductSlugs,
+		};
+	}, [ addedPlanAndProducts, data, isLoadingProducts, selectedSite ] );
+}
--- client/jetpack-cloud/sections/partner-portal/issue-license-v2/licenses-form/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/issue-license-v2/licenses-form/index.tsx
@@ -1,25 +1,14 @@
-import { getQueryArg } from '@wordpress/url';
 import { useTranslate } from 'i18n-calypso';
-import { useCallback, useEffect, useMemo, useContext } from 'react';
+import { useCallback, useContext } from 'react';
 import QueryProductsList from 'calypso/components/data/query-products-list';
-import {
-	isJetpackBundle,
-	isWooCommerceProduct,
-	isWpcomHostingProduct,
-} from 'calypso/jetpack-cloud/sections/partner-portal/lib';
 import LicenseBundleCard from 'calypso/jetpack-cloud/sections/partner-portal/license-bundle-card';
 import LicenseProductCard from 'calypso/jetpack-cloud/sections/partner-portal/license-product-card';
-import TotalCost from 'calypso/jetpack-cloud/sections/partner-portal/primary/total-cost';
-import { useDispatch, useSelector } from 'calypso/state';
-import useProductsQuery from 'calypso/state/partner-portal/licenses/hooks/use-products-query';
-import { getAssignedPlanAndProductIDsForSite } from 'calypso/state/partner-portal/licenses/selectors';
-import {
-	addSelectedProductSlugs,
-	clearSelectedProductSlugs,
-} from 'calypso/state/partner-portal/products/actions';
+import { useSelector } from 'calypso/state';
 import { getDisabledProductSlugs } from 'calypso/state/partner-portal/products/selectors';
 import IssueLicenseContext from '../context';
 import useSubmitForm from '../hooks/use-submit-form';
+import useProductAndPlans from './hooks/use-product-and-plans';
+import LicensesFormSection from './sections';
 import type { AssignLicenceProps } from '../../types';
 import type {
 	APIProductFamilyProduct,
@@ -34,79 +23,22 @@ export default function LicensesForm( {
 	quantity = 1,
 }: AssignLicenceProps ) {
 	const translate = useTranslate();
-	const dispatch = useDispatch();
-
-	const { data, isLoading: isLoadingProducts } = useProductsQuery();
 
 	const { selectedLicenses, setSelectedLicenses } = useContext( IssueLicenseContext );
 
-	let allProducts = data;
-	const addedPlanAndProducts = useSelector( ( state ) =>
-		selectedSite ? getAssignedPlanAndProductIDsForSite( state, selectedSite.ID ) : null
-	);
-
-	// Filter products & plan that are already assigned to a site
-	if ( selectedSite && addedPlanAndProducts && allProducts ) {
-		allProducts = allProducts.filter(
-			( product ) => ! addedPlanAndProducts.includes( product.product_id )
-		);
-	}
-
-	const bundles =
-		allProducts?.filter(
-			( { family_slug }: { family_slug: string } ) => family_slug === 'jetpack-packs'
-		) || [];
-	const backupAddons =
-		allProducts
-			?.filter(
-				( { family_slug }: { family_slug: string } ) => family_slug === 'jetpack-backup-storage'
-			)
-			.sort( ( a, b ) => a.product_id - b.product_id ) || [];
-	const products =
-		allProducts?.filter(
-			( { family_slug }: { family_slug: string } ) =>
-				family_slug !== 'jetpack-packs' &&
-				family_slug !== 'jetpack-backup-storage' &&
-				! isWooCommerceProduct( family_slug ) &&
-				! isWpcomHostingProduct( family_slug )
-		) || [];
-	const wooExtensions =
-		allProducts?.filter( ( { family_slug }: { family_slug: string } ) =>
-			isWooCommerceProduct( family_slug )
-		) || [];
-
-	// If the user comes from the flow for adding a new payment method during an attempt to issue a license
-	// after the payment method is added, we will make an attempt to issue the chosen license automatically.
-	const defaultProductSlugs = useMemo(
-		() => getQueryArg( window.location.href, 'products' )?.toString().split( ',' ),
-		[]
-	);
-
-	useEffect( () => {
-		// Select the slugs included in the URL
-		defaultProductSlugs &&
-			dispatch(
-				addSelectedProductSlugs(
-					// Filter the bundles and select only individual products
-					defaultProductSlugs.filter( ( slug ) => ! isJetpackBundle( slug ) )
-				)
-			);
-
-		// Clear all selected slugs when navigating away from the page to avoid persisting the data.
-		return () => {
-			dispatch( clearSelectedProductSlugs() );
-		};
-	}, [ dispatch, defaultProductSlugs ] );
+	const {
+		allProductsAndBundles,
+		isLoadingProducts,
+		bundles,
+		backupAddons,
+		products,
+		wooExtensions,
+		suggestedProductSlugs,
+	} = useProductAndPlans( { selectedSite } );
 
 	const disabledProductSlugs = useSelector< PartnerPortalStore, string[] >( ( state ) =>
-		getDisabledProductSlugs( state, allProducts ?? [] )
+		getDisabledProductSlugs( state, allProductsAndBundles ?? [] )
 	);
-
-	// We need the suggested products (i.e., the products chosen from the dashboard) to properly
-	// track if the user purchases a different set of products.
-	const suggestedProductSlugs = getQueryArg( window.location.href, 'product_slug' )
-		?.toString()
-		.split( ',' );
 
 	const handleSelectBundleLicense = useCallback(
 		( product: APIProductFamilyProduct ) => {
@@ -144,6 +76,16 @@ export default function LicensesForm( {
 		[ handleSelectBundleLicense ]
 	);
 
+	const isSelected = useCallback(
+		( slug: string ) =>
+			selectedLicenses.some(
+				( license ) => license.slug === slug && license.quantity === quantity
+			),
+		[ quantity, selectedLicenses ]
+	);
+
+	const isSingleLicenseView = quantity === 1;
+
 	if ( isLoadingProducts ) {
 		return (
 			<div className="licenses-form">
@@ -152,37 +94,38 @@ export default function LicensesForm( {
 		);
 	}
 
-	const selectedSiteDomain = selectedSite?.domain;
-
-	const isSelected = ( slug: string ) =>
-		selectedLicenses.findIndex(
-			( license ) => license.slug === slug && license.quantity === quantity
-		) !== -1;
-
 	return (
 		<div className="licenses-form">
 			<QueryProductsList type="jetpack" currency="USD" />
-			<div className="licenses-form__top">
-				<p className="licenses-form__description">
-					{ selectedSiteDomain
-						? translate(
-								'Select the Jetpack products you would like to add to {{strong}}%(selectedSiteDomain)s{{/strong}}:',
-								{
-									args: { selectedSiteDomain },
-									components: { strong: <strong /> },
-								}
-						  )
-						: translate(
-								'Select the Jetpack products you would like to issue a new license for:'
-						  ) }
-				</p>
-				<div className="licenses-form__controls">
-					<TotalCost />
-				</div>
-			</div>
-			<div className="licenses-form__bottom">
-				{ products &&
-					products.map( ( productOption, i ) => (
+
+			{ bundles && (
+				<LicensesFormSection
+					title={ translate( 'Plans' ) }
+					description={ translate(
+						'Save big with comprehensive bundles of Jetpack security, performance, and growth tools.'
+					) }
+				>
+					{ bundles.map( ( productOption, i ) => (
+						<LicenseBundleCard
+							key={ productOption.slug }
+							product={ productOption }
+							isBusy={ ! isReady }
+							isDisabled={ ! isReady }
+							onSelectProduct={ onSelectBundle }
+							tabIndex={ 100 + ( products?.length || 0 ) + i }
+						/>
+					) ) }
+				</LicensesFormSection>
+			) }
+
+			{ products && (
+				<LicensesFormSection
+					title={ translate( 'Products' ) }
+					description={ translate(
+						'Mix and match powerful security, performance, and growth tools for your sites.'
+					) }
+				>
+					{ products.map( ( productOption, i ) => (
 						<LicenseProductCard
 							isMultiSelect
 							key={ productOption.slug }
@@ -194,70 +137,51 @@ export default function LicensesForm( {
 							suggestedProduct={ suggestedProduct }
 						/>
 					) ) }
-			</div>
-			{ bundles && (
-				<>
-					<hr className="licenses-form__separator" />
-					<p className="licenses-form__description">
-						{ translate( 'Or select any of our {{strong}}recommended bundles{{/strong}}:', {
-							components: { strong: <strong /> },
-						} ) }
-					</p>
-					<div className="licenses-form__bottom">
-						{ bundles.map( ( productOption, i ) => (
-							<LicenseBundleCard
-								key={ productOption.slug }
-								product={ productOption }
-								isBusy={ ! isReady }
-								isDisabled={ ! isReady }
-								onSelectProduct={ onSelectBundle }
-								tabIndex={ 100 + ( products?.length || 0 ) + i }
-							/>
-						) ) }
-					</div>
-				</>
+				</LicensesFormSection>
 			) }
-			{ wooExtensions.length > 0 && (
-				<>
-					<hr className="licenses-form__separator" />
-					<p className="licenses-form__description">{ translate( 'WooCommerce Extensions:' ) }</p>
-					<div className="licenses-form__bottom">
-						{ wooExtensions.map( ( productOption, i ) => (
-							<LicenseProductCard
-								isMultiSelect
-								key={ productOption.slug }
-								product={ productOption }
-								onSelectProduct={ onSelectProduct }
-								isSelected={ isSelected( productOption.slug ) }
-								isDisabled={ disabledProductSlugs.includes( productOption.slug ) }
-								tabIndex={ 100 + i }
-								suggestedProduct={ suggestedProduct }
-							/>
-						) ) }
-					</div>
-				</>
+
+			{ isSingleLicenseView && wooExtensions.length > 0 && (
+				<LicensesFormSection
+					title={ translate( 'WooCommerce Extensions' ) }
+					description={ translate(
+						'You must have WooCommerce installed to utilize these paid extensions.'
+					) }
+				>
+					{ wooExtensions.map( ( productOption, i ) => (
+						<LicenseProductCard
+							isMultiSelect
+							key={ productOption.slug }
+							product={ productOption }
+							onSelectProduct={ onSelectProduct }
+							isSelected={ isSelected( productOption.slug ) }
+							isDisabled={ disabledProductSlugs.includes( productOption.slug ) }
+							tabIndex={ 100 + i }
+							suggestedProduct={ suggestedProduct }
+						/>
+					) ) }
+				</LicensesFormSection>
 			) }
-			{ backupAddons.length > 0 && (
-				<>
-					<hr className="licenses-form__separator" />
-					<p className="licenses-form__description">
-						{ translate( 'VaultPress Backup Add-on Storage:' ) }
-					</p>
-					<div className="licenses-form__bottom">
-						{ backupAddons.map( ( productOption, i ) => (
-							<LicenseProductCard
-								isMultiSelect
-								key={ productOption.slug }
-								product={ productOption }
-								onSelectProduct={ onSelectProduct }
-								isSelected={ isSelected( productOption.slug ) }
-								isDisabled={ disabledProductSlugs.includes( productOption.slug ) }
-								tabIndex={ 100 + i }
-								suggestedProduct={ suggestedProduct }
-							/>
-						) ) }
-					</div>
-				</>
+
+			{ isSingleLicenseView && backupAddons.length > 0 && (
+				<LicensesFormSection
+					title={ translate( 'VaultPress Backup Add-ons' ) }
+					description={ translate(
+						'Add additional storage to your current VaultPress Backup plans.'
+					) }
+				>
+					{ backupAddons.map( ( productOption, i ) => (
+						<LicenseProductCard
+							isMultiSelect
+							key={ productOption.slug }
+							product={ productOption }
+							onSelectProduct={ onSelectProduct }
+							isSelected={ isSelected( productOption.slug ) }
+							isDisabled={ disabledProductSlugs.includes( productOption.slug ) }
+							tabIndex={ 100 + i }
+							suggestedProduct={ suggestedProduct }
+						/>
+					) ) }
+				</LicensesFormSection>
 			) }
 		</div>
 	);
