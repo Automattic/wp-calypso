3a38f77e199d5e1b9511fa1160ffd2c13d08bf4b
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-11-22 16:45:22 +0800

    Add new product filter select component.

---
+++ client/jetpack-cloud/sections/partner-portal/issue-license-v2/licenses-form/product-filter-select.tsx
@@ -0,0 +1,96 @@
+import { SelectDropdown } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import { useEffect, useMemo } from 'react';
+import {
+	PRODUCT_FILTER_ALL,
+	PRODUCT_FILTER_PLANS,
+	PRODUCT_FILTER_PRODUCTS,
+	PRODUCT_FILTER_VAULTPRESS_BACKUP_ADDONS,
+	PRODUCT_FILTER_WOOCOMMERCE_EXTENSIONS,
+} from '../constants';
+
+type Props = {
+	selectedProductFilter: string | null;
+	onProductFilterSelect: ( value: string | null ) => void;
+	isSingleLicense?: boolean;
+};
+
+type Option = {
+	value?: string | null;
+	label: string;
+};
+
+const getOptionByFuction = ( options: Option[], value: string | null ): Option => {
+	return options.find( ( option ) => option.value === value ) ?? options[ 0 ];
+};
+
+export default function ProductFilterSelect( {
+	selectedProductFilter,
+	onProductFilterSelect,
+	isSingleLicense,
+}: Props ) {
+	const translate = useTranslate();
+
+	const productFilterOptions = useMemo< Option[] >( () => {
+		const options = [
+			{
+				value: PRODUCT_FILTER_ALL,
+				label: translate( 'All' ),
+			},
+			{
+				value: PRODUCT_FILTER_PLANS,
+				label: translate( 'Plans' ),
+			},
+			{
+				value: PRODUCT_FILTER_PRODUCTS,
+				label: translate( 'Products' ),
+			},
+		];
+
+		if ( isSingleLicense ) {
+			options.push(
+				{
+					value: PRODUCT_FILTER_WOOCOMMERCE_EXTENSIONS,
+					label: translate( 'WooCommerce Extensions' ),
+				},
+				{
+					value: PRODUCT_FILTER_VAULTPRESS_BACKUP_ADDONS,
+					label: translate( 'VaultPress Backup Add-ons' ),
+				}
+			);
+		}
+
+		return options;
+	}, [ isSingleLicense, translate ] );
+
+	const currentSelectedOption = getOptionByFuction( productFilterOptions, selectedProductFilter );
+
+	const selectedText = translate( '{{b}}Products{{/b}} : %(productFilter)s', {
+		args: {
+			productFilter: currentSelectedOption.label as string,
+		},
+		components: {
+			b: <b />,
+		},
+	} );
+
+	useEffect( () => {
+		if (
+			! isSingleLicense &&
+			( selectedProductFilter === PRODUCT_FILTER_WOOCOMMERCE_EXTENSIONS ||
+				selectedProductFilter === PRODUCT_FILTER_VAULTPRESS_BACKUP_ADDONS )
+		) {
+			onProductFilterSelect( PRODUCT_FILTER_ALL );
+		}
+	}, [ isSingleLicense, onProductFilterSelect, selectedProductFilter ] );
+
+	return (
+		<SelectDropdown
+			className="licenses-form__product-filter-select"
+			selectedText={ selectedText }
+			options={ productFilterOptions }
+			onSelect={ ( option: { value: string | null } ) => onProductFilterSelect( option.value ) }
+			compact
+		/>
+	);
+}
