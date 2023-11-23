d0f2016cecc08dadeda5f629685b51ca790bda2f
Chris Diehl <jjchrisdiehl@gmail.com>
2023-11-23 11:25:38 -0800

    Add eligibility for wpcom checkout only

--- client/my-sites/checkout/src/hooks/use-tos-foldable-card.tsx
+++ client/my-sites/checkout/src/hooks/use-tos-foldable-card.tsx
@@ -1,8 +1,22 @@
+import isAkismetCheckout from 'calypso/lib/akismet/is-akismet-checkout';
 import { useExperiment } from 'calypso/lib/explat';
+import isJetpackCheckout from 'calypso/lib/jetpack/is-jetpack-checkout';
+import { useSelector } from 'calypso/state';
+import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
+import { isJetpackSite } from 'calypso/state/sites/selectors';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
 export function useToSFoldableCard(): boolean {
+	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
+
+	const isJetpackNotAtomic = useSelector(
+		( state ) => isJetpackSite( state, siteId ) && ! isSiteAutomatedTransfer( state, siteId )
+	);
+
+	const isWPcomCheckout = ! isJetpackCheckout() && ! isAkismetCheckout() && ! isJetpackNotAtomic;
 	const [ isLoadingExperimentAssignment, experimentAssignment ] = useExperiment(
-		'wp_web_checkout_tos_foldable_card_v1'
+		'wp_web_checkout_tos_foldable_card_v1',
+		{ isEligible: isWPcomCheckout }
 	);
 
 	if ( ! isLoadingExperimentAssignment ) {
