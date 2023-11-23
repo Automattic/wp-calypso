d1be7b00f74fb818146fe9c5a4578dac9c38990d
Karen Attfield <karenlattfield@gmail.com>
2023-11-23 13:36:19 +0000

    Adding a redirect to the overview page on form submission, and dashboard otherwise

--- client/jetpack-cloud/sections/agency-signup/agency-signup-form/index.tsx
+++ client/jetpack-cloud/sections/agency-signup/agency-signup-form/index.tsx
@@ -7,7 +7,7 @@ import QueryJetpackPartnerPortalPartner from 'calypso/components/data/query-jetp
 import CompanyDetailsForm from 'calypso/jetpack-cloud/sections/partner-portal/company-details-form';
 import formatApiPartner from 'calypso/jetpack-cloud/sections/partner-portal/lib/format-api-partner';
 import TextPlaceholder from 'calypso/jetpack-cloud/sections/partner-portal/text-placeholder';
-import { dashboardPath } from 'calypso/lib/jetpack/paths';
+import { dashboardPath, overviewPath } from 'calypso/lib/jetpack/paths';
 import { useDispatch, useSelector } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { errorNotice, removeNotice } from 'calypso/state/notices/actions';
@@ -75,9 +75,12 @@ export default function AgencySignupForm() {
 		[ notificationId, partner?.id, createPartner.mutate, dispatch ]
 	);
 
-	// Redirect the user if they are already a partner or the form was submitted successfully.
+	// Redirect the user to the dashboard if they are already a partner,
+	// or the overview page if the form was submitted successfully.
 	useEffect( () => {
-		if ( partner ) {
+		if ( createPartner.isSuccess ) {
+			page.redirect( overviewPath() );
+		} else if ( partner ) {
 			page.redirect( dashboardPath() );
 		}
 	} );
