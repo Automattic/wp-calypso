05c4846a5aa9b518242fe4a033a59d608d4ea893
escapemanuele <escapemanuele@gmail.com>
2023-11-27 15:30:16 +0100

    Refactor SocialAuthToS

--- client/blocks/authentication/social/social-tos.jsx
+++ client/blocks/authentication/social/social-tos.jsx
@@ -5,85 +5,54 @@ import { isWooOAuth2Client } from 'calypso/lib/oauth2-clients';
 import { getCurrentOAuth2Client } from 'calypso/state/oauth2-clients/ui/selectors';
 import isWooCommerceCoreProfilerFlow from 'calypso/state/selectors/is-woocommerce-core-profiler-flow';
 
+const toSLinks = {
+	components: {
+		tosLink: (
+			<a
+				href={ localizeUrl( 'https://wordpress.com/tos/' ) }
+				target="_blank"
+				rel="noopener noreferrer"
+			/>
+		),
+		privacyLink: (
+			<a
+				href={ localizeUrl( 'https://automattic.com/privacy/' ) }
+				target="_blank"
+				rel="noopener noreferrer"
+			/>
+		),
+	},
+};
+
+function getToSComponent( content ) {
+	return <p className="auth-form__social-buttons-tos">{ content }</p>;
+}
+
 function SocialAuthToS( props ) {
+	let content = props.translate(
+		'If you continue with Google or Apple, you agree to our' +
+			' {{tosLink}}Terms of Service{{/tosLink}}, and have' +
+			' read our {{privacyLink}}Privacy Policy{{/privacyLink}}.',
+		toSLinks
+	);
+
 	if ( props.isWooCoreProfilerFlow ) {
-		return (
-			<p className="auth-form__social-buttons-tos">
-				{ props.translate(
-					'If you continue with Google or Apple, you agree to our' +
-						' {{tosLink}}Terms of Service{{/tosLink}}, and have' +
-						' read our {{privacyLink}}Privacy Policy{{/privacyLink}}.',
-					{
-						components: {
-							tosLink: (
-								<a
-									href={ localizeUrl( 'https://wordpress.com/tos/' ) }
-									target="_blank"
-									rel="noopener noreferrer"
-								/>
-							),
-							privacyLink: (
-								<a
-									href={ localizeUrl( 'https://automattic.com/privacy/' ) }
-									target="_blank"
-									rel="noopener noreferrer"
-								/>
-							),
-						},
-					}
-				) }
-			</p>
+		content = props.translate(
+			'If you continue with Google or Apple, you agree to our' +
+				' {{tosLink}}Terms of Service{{/tosLink}}, and have' +
+				' read our {{privacyLink}}Privacy Policy{{/privacyLink}}.',
+			toSLinks
 		);
 	}
 
 	if ( isWooOAuth2Client( props.oauth2Client ) ) {
-		return (
-			<p className="auth-form__social-buttons-tos">
-				{ props.translate(
-					"If you continue with Google or Apple and don't already have a WordPress.com account, you are creating an account and you agree to our {{tosLink}}Terms of Service{{/tosLink}}.",
-					{
-						components: {
-							tosLink: (
-								<a
-									href={ localizeUrl( 'https://wordpress.com/tos/' ) }
-									target="_blank"
-									rel="noopener noreferrer"
-								/>
-							),
-						},
-					}
-				) }
-			</p>
+		content = props.translate(
+			"If you continue with Google or Apple and don't already have a WordPress.com account, you are creating an account and you agree to our {{tosLink}}Terms of Service{{/tosLink}}.",
+			toSLinks
 		);
 	}
 
-	return (
-		<p className="auth-form__social-buttons-tos">
-			{ props.translate(
-				'If you continue with Google or Apple, you agree to our' +
-					' {{tosLink}}Terms of Service{{/tosLink}}, and have' +
-					' read our {{privacyLink}}Privacy Policy{{/privacyLink}}.',
-				{
-					components: {
-						tosLink: (
-							<a
-								href={ localizeUrl( 'https://wordpress.com/tos/' ) }
-								target="_blank"
-								rel="noopener noreferrer"
-							/>
-						),
-						privacyLink: (
-							<a
-								href={ localizeUrl( 'https://automattic.com/privacy/' ) }
-								target="_blank"
-								rel="noopener noreferrer"
-							/>
-						),
-					},
-				}
-			) }
-		</p>
-	);
+	return getToSComponent( content );
 }
 
 export default connect( ( state ) => ( {
