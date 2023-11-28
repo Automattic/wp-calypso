ecf529ee042d49a59d6d8b1f4e46fb84d424d863
Jarda Snajdr <jsnajdr@gmail.com>
2023-11-28 12:15:35 +0100

    getSiteWooCommerceUrl: don't use page to navigate, remove identical selector

--- client/my-sites/woocommerce/index.js
+++ client/my-sites/woocommerce/index.js
@@ -8,7 +8,7 @@ import {
 	getSiteOption,
 	getSiteWooCommerceUrl,
 } from 'calypso/state/sites/selectors';
-import main from './main';
+import Main from './main';
 
 import './style.scss';
 
@@ -32,10 +32,10 @@ function setup( context, next ) {
 	// instead of passive redirect.
 	if ( getSiteOption( state, siteId, 'is_wpcom_store' ) ) {
 		const redirectUrl = getSiteWooCommerceUrl( state, siteId );
-		window.location.href = redirectUrl;
+		window.location = redirectUrl;
 		return;
 	}
 
-	context.primary = createElement( main );
+	context.primary = createElement( Main );
 	next();
 }
