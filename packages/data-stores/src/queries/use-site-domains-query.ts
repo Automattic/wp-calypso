f211ed6a95ce79fd03166cb7576dec9ed0d077d0
Markus Dobmann <m.do@posteo.net>
2023-11-28 12:02:42 +0100

    types: omit `queryKey` if we expose `UseQueryOptions` /2

--- packages/data-stores/src/queries/use-site-domains-query.ts
+++ packages/data-stores/src/queries/use-site-domains-query.ts
@@ -117,14 +117,14 @@ export interface SiteDomainsQueryFnData {
 
 export function useSiteDomainsQuery< TError = unknown, TData = SiteDomainsQueryFnData >(
 	siteIdOrSlug: number | string | null | undefined,
-	options: UseQueryOptions< SiteDomainsQueryFnData, TError, TData > = {}
+	options: Omit< UseQueryOptions< SiteDomainsQueryFnData, TError, TData >, 'queryKey' > = {}
 ) {
 	return useQuery( getSiteDomainsQueryObject( siteIdOrSlug, options ) );
 }
 
 export function getSiteDomainsQueryObject< TError = unknown, TData = SiteDomainsQueryFnData >(
 	siteIdOrSlug: number | string | null | undefined,
-	options: UseQueryOptions< SiteDomainsQueryFnData, TError, TData > = {}
+	options: Omit< UseQueryOptions< SiteDomainsQueryFnData, TError, TData >, 'queryKey' > = {}
 ): UseQueryOptions< SiteDomainsQueryFnData, TError, TData > {
 	return {
 		queryKey: [ 'site-domains', siteIdOrSlug ],
