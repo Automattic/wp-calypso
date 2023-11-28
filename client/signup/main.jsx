Automattic/wp-calypso

commit 98f2e2535fa09ef2e04e6dc57680fce18ecdd605

Fix track event flow name in signup page in sensei flow, update step name of launch (#84191)
* Add flow param in redirect URL, make it single line

* Add main flow name in context

* Override flow property in track if mainflow is there

* Change all step name to kebab case

* Fix extra slash

* Revert "Change all step name to kebab case"

This reverts commit abd16781d8f19c1a201ba43baad214c565b2eadf.

* Move flow from prop

* Update wrong stepname in senseiLaunch

client/signup/main.jsx
@@ -330,7 +330,9 @@ class Signup extends Component {
 	};
 
 	getRecordProps() {
-		const { signupDependencies, hostingFlow } = this.props;
+		const { signupDependencies, hostingFlow, queryObject } = this.props;
+		const flow = queryObject?.flow;
+
 		let theme = get( signupDependencies, 'selectedDesign.theme' );
 
 		if ( ! theme && signupDependencies.themeParameter ) {
@@ -345,6 +347,7 @@ class Signup extends Component {
 			intent: get( signupDependencies, 'intent' ),
 			starting_point: get( signupDependencies, 'startingPoint' ),
 			is_in_hosting_flow: hostingFlow,
+			...( flow ? { flow } : {} ),
 		};
 	}
 

client/landing/stepper/declarative-flow/internals/steps-repository/sensei-launch/index.tsx
@@ -87,7 +87,7 @@ const SenseiLaunch: Step = ( { navigation: { submit } } ) => {
 	return (
 		<>
 			<DocumentHead title={ progress.title } />
-			<SenseiStepContainer stepName="senseiSetup" recordTracksEvent={ recordTracksEvent }>
+			<SenseiStepContainer stepName="senseiLaunch" recordTracksEvent={ recordTracksEvent }>
 				<SenseiStepProgress progress={ progress } />
 			</SenseiStepContainer>
 		</>

client/landing/stepper/declarative-flow/sensei.ts
@@ -18,9 +18,9 @@ import { AssertConditionState, Flow } from './internals/types';
 import './internals/sensei.scss';
 
 function getStartUrl( step: string, locale: string ) {
-	return locale && locale !== 'en'
-		? `/start/account/user/${ locale }?redirect_to=/setup/${ SENSEI_FLOW }/${ step }`
-		: `/start/account/user?redirect_to=/setup/${ SENSEI_FLOW }/${ step }`;
+	const localeUrlPart = locale && locale !== 'en' ? `/${ locale }` : '';
+
+	return `/start/account/user${ localeUrlPart }?redirect_to=/setup/${ SENSEI_FLOW }/${ step }&flow=${ SENSEI_FLOW }`;
 }
 
 const sensei: Flow = {

commit 0e9c409beac1e0734442babf8ceef0ec5043f0f4

Fix hosting signup tracks bug introduced by #84191

client/signup/main.jsx
@@ -347,7 +347,7 @@ class Signup extends Component {
 			intent: get( signupDependencies, 'intent' ),
 			starting_point: get( signupDependencies, 'startingPoint' ),
 			is_in_hosting_flow: hostingFlow,
-			...( flow ? { flow } : {} ),
+			...( flow && flow !== 'new-hosted-site' ? { flow } : {} ),
 		};
 	}
 