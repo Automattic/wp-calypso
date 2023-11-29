92b32228bca293ddeb029a05722d1fc30ddf5683
James Tien <southp0105@gmail.com>
2023-11-29 17:43:47 +0800

    Clean up unnecessary styling code.

--- client/my-sites/plans-features-main/components/pressable-promo-banner.tsx
+++ client/my-sites/plans-features-main/components/pressable-promo-banner.tsx
@@ -5,13 +5,13 @@ import { useTranslate } from 'i18n-calypso';
 const Banner = styled.div`
 	display: flex;
 	justify-content: start;
-	height: 128px;
+	min-height: 128px;
 	padding: 0px, 24px, 0px, 0px;
 	border-radius: 4px;
 	border: 1px solid #dcdcde;
-	margin-left: 20px;
-	margin-right: 20px;
+	margin: 24px 20px 24px 20px;
 	padding-right: 20px;
+	padding-left: 20px;
 	align-items: center;
 	gap: 16px;
 `;
@@ -22,7 +22,12 @@ const LogoContainer = styled.div`
 `;
 
 const TextContainer = styled.div`
+	display: flex;
 	flex-basis: 1200px;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
 `;
 
 const CtaContainer = styled.div`
@@ -30,44 +35,32 @@ const CtaContainer = styled.div`
 `;
 
 const Subtitle = styled.h4`
-	font-family: SF Pro Text;
 	font-size: 12px;
 	font-weight: 400;
-	line-height: 20px;
-	letter-spacing: 0em;
 	text-align: left;
 `;
 
 const Title = styled.h2`
-	font-family: SF Pro Display;
 	font-size: 20px;
 	font-weight: 500;
-	line-height: 26px;
-	letter-spacing: 0.3799999952316284px;
 	text-align: left;
 `;
 
 const Description = styled.p`
-	font-family: SF Pro Text;
 	font-size: 14px;
 	font-weight: 400;
-	line-height: 20px;
-	letter-spacing: -0.15000000596046448px;
 	text-align: left;
 `;
 
 const CtaButton = styled( Button )`
-	font-family: SF Pro Display;
 	font-size: 14px;
 	font-weight: 500;
 	line-height: 20px;
-	letter-spacing: 0.3199999928474426px;
 	text-align: center;
 	width: 199px;
 	height: 40px;
 	padding: 10px, 24px, 10px, 24px;
 	border-radius: 4px;
-	gap: 8px;
 	background-color: #000000;
 	color: #ffffff;
 	box-shadow: 0px 1px 2px 0px #0000000d;
