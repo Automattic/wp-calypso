671c184f9381a586efe5434c1df96314390e70b7
Eoin Gallagher <eoin@automattic.com>
2023-11-29 14:13:34 +0000

    Return number of tags added so we can handle case where none have been added

--- apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/sharing-modal/use-add-tags-to-post.ts
+++ apps/editing-toolkit/editing-toolkit-plugin/wpcom-block-editor-nux/src/sharing-modal/use-add-tags-to-post.ts
@@ -5,22 +5,22 @@ type HasAddedTagsResult = {
 	success: boolean;
 };
 
-// OnSaveTags is a function callback that takes the number of tags added
-// to the post as an argument.
 type OnSaveTagsCallback = ( addedTags: number ) => void;
 const useAddTagsToPost = ( postId: number, tags: string[], onSaveTags: OnSaveTagsCallback ) => {
 	async function saveTags() {
+		let addedTags = 0;
 		try {
 			const result: HasAddedTagsResult = await apiFetch( {
 				method: 'POST',
 				path: `/wpcom/v2/read/sites/${ window._currentSiteId }/posts/${ postId }/tags/add`,
 				data: { tags },
 			} );
-			onSaveTags( result.added_tags ?? 0 );
+			addedTags = result.added_tags ?? 0;
 		} catch ( error ) {
 			// eslint-disable-next-line no-console
-			console.error( 'Error: Unable to add tags. Reason: %s', error );
+			console.error( 'Error: Unable to add tags. Reason: %s', JSON.stringify( error ) );
 		}
+		onSaveTags( addedTags );
 	}
 	return { saveTags };
 };
