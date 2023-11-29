6eca83b767f9490165440bb53ce063cae6c05e47
Ashar Fuadi <ashar.fuadi@automattic.com>
2023-11-29 19:46:19 +0700

    Live Preview: Suppress the template part hint in the editor sidebar

--- apps/wpcom-block-editor/src/wpcom/features/live-preview/index.tsx
+++ apps/wpcom-block-editor/src/wpcom/features/live-preview/index.tsx
@@ -21,6 +21,7 @@ const LivePreviewNotice: FC< {
 	previewingThemeName: string;
 } > = ( { previewingThemeName } ) => {
 	const { createWarningNotice, removeNotice } = useDispatch( 'core/notices' );
+	const { set: setPreferences } = useDispatch( 'core/preferences' );
 
 	const siteEditorStore = useSelect( ( select ) => select( 'core/edit-site' ), [] );
 	const dashboardLink =
@@ -37,6 +38,11 @@ const LivePreviewNotice: FC< {
 			removeNotice( NOTICE_ID );
 			return;
 		}
+
+		// Suppress the "Looking for template parts?" notice in the Site Editor sidebar.
+		// The preference name is defined in https://github.com/WordPress/gutenberg/blob/d47419499cd58e20db25c370cdbf02ddf7cffce0/packages/edit-site/src/components/sidebar-navigation-screen-main/template-part-hint.js#L9.
+		setPreferences( 'core', 'isTemplatePartMoveHintVisible', false );
+
 		createWarningNotice(
 			sprintf(
 				// translators: %s: theme name
