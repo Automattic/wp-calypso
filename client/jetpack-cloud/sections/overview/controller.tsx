74f4d92c1357970b1819651d507d26aa843722a3
Clemen <javacodia@gmail.com>
2023-12-01 21:59:32 +0000

    Jetpack Mange: Add a layout for overview page with a right sidebar (#84778)
    * Add the content-sidebar component
    
    * Add mock content for the right sidebar
    
    * Add mock content for the Overview main block
    
    * Use the ContentSidebar in the Overview context

--- client/jetpack-cloud/sections/overview/controller.tsx
+++ client/jetpack-cloud/sections/overview/controller.tsx
@@ -1,5 +1,7 @@
 import { type Callback, type Context } from '@automattic/calypso-router';
+import ContentSidebar from 'calypso/jetpack-cloud/components/content-sidebar';
 import Overview from 'calypso/jetpack-cloud/sections/overview/primary/overview';
+import OverviewSidebar from 'calypso/jetpack-cloud/sections/overview/sidebar';
 import JetpackManageSidebar from 'calypso/jetpack-cloud/sections/sidebar-navigation/jetpack-manage';
 
 const setSidebar = ( context: Context ): void => {
@@ -8,6 +10,8 @@ const setSidebar = ( context: Context ): void => {
 
 export const overviewContext: Callback = ( context, next ) => {
 	setSidebar( context );
-	context.primary = <Overview />;
+	context.primary = (
+		<ContentSidebar mainContent={ <Overview /> } rightSidebar={ <OverviewSidebar /> } />
+	);
 	next();
 };
