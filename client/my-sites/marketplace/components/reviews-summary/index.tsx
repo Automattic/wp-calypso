4362e3ed264a3c810f872b95a15e432062c8dc04
Roberto Aranda <roberto.aranda@automattic.com>
2023-12-01 18:07:45 +0100

    Add Rating component and reviews link to Themes

--- client/my-sites/marketplace/components/reviews-summary/index.tsx
+++ client/my-sites/marketplace/components/reviews-summary/index.tsx
@@ -1,8 +1,14 @@
+import { isEnabled } from '@automattic/calypso-config';
 import { Button } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import { useState } from 'react';
-import { type ProductProps } from 'calypso/data/marketplace/use-marketplace-reviews';
+import Rating from 'calypso/components/rating';
+import {
+	useMarketplaceReviewsQuery,
+	type ProductProps,
+} from 'calypso/data/marketplace/use-marketplace-reviews';
 import { ReviewsModal } from 'calypso/my-sites/marketplace/components/reviews-modal';
+import './styles.scss';
 
 type Props = ProductProps & {
 	productName: string;
@@ -11,6 +17,30 @@ type Props = ProductProps & {
 export const ReviewsSummary = ( { slug, productName, productType }: Props ) => {
 	const translate = useTranslate();
 	const [ isVisible, setIsVisible ] = useState( false );
+
+	const { data: marketplaceReviews } = useMarketplaceReviewsQuery( {
+		productType,
+		slug,
+	} );
+	// TODO: The averageRating will come from the server. Calculating it here temporarily.
+	let averageRating = null;
+	let numberOfReviews = null;
+	if (
+		isEnabled( 'marketplace-reviews-show' ) &&
+		marketplaceReviews &&
+		! ( 'message' in marketplaceReviews ) &&
+		marketplaceReviews.length > 0
+	) {
+		const totalReviews = marketplaceReviews.reduce(
+			( acc, review ) => acc + review.meta.wpcom_marketplace_rating,
+			0
+		);
+
+		numberOfReviews = marketplaceReviews.length;
+
+		averageRating = totalReviews / numberOfReviews;
+		averageRating = ( averageRating * 100 ) / 5; // Normalize to 100
+	}
 
 	return (
 		<>
@@ -22,7 +52,20 @@ export const ReviewsSummary = ( { slug, productName, productType }: Props ) => {
 				productType={ productType }
 				buttons={ [] }
 			/>
-			<div className="theme__sheet-reviews">
+			<div className="reviews-summary__container">
+				<div>
+					{ averageRating && <Rating rating={ averageRating } /> }
+					{ numberOfReviews && (
+						<Button borderless className="reviews-summary__number-reviews-link is-link">
+							{ translate( '%(numberOfReviews)d review', '%(numberOfReviews)d reviews', {
+								count: numberOfReviews,
+								args: {
+									numberOfReviews,
+								},
+							} ) }
+						</Button>
+					) }
+				</div>
 				<Button onClick={ () => setIsVisible( true ) }>{ translate( 'Add Review' ) }</Button>
 			</div>
 		</>
