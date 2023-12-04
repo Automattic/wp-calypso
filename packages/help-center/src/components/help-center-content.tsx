a29a50879f1ebcb6cc70307fbf6bf10907409db3
Daniel López <daniel.lopez@automattic.com>
2023-12-04 11:48:45 +0100

    Test removing Odie

--- packages/help-center/src/components/help-center-content.tsx
+++ packages/help-center/src/components/help-center-content.tsx
@@ -6,37 +6,25 @@ import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { CardBody } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { useEffect, useRef } from '@wordpress/element';
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
-import { OdieAssistantProvider } from 'calypso/odie/context';
 import { getSectionName } from 'calypso/state/ui/selectors';
 /**
  * Internal Dependencies
  */
-import { useIsWapuuEnabled } from '../hooks';
 import { HELP_CENTER_STORE } from '../stores';
 import { HelpCenterContactForm } from './help-center-contact-form';
 import { HelpCenterContactPage } from './help-center-contact-page';
 import { HelpCenterEmbedResult } from './help-center-embed-result';
-import { HelpCenterOdie } from './help-center-odie';
 import { HelpCenterSearch } from './help-center-search';
 import { SuccessScreen } from './ticket-success-screen';
 import type { HelpCenterSelect } from '@automattic/data-stores';
 
 const HelpCenterContent: React.FC< { isRelative?: boolean } > = () => {
-	const [ searchTerm, setSearchTerm ] = useState( '' );
 	const location = useLocation();
 	const navigate = useNavigate();
 	const containerRef = useRef< HTMLDivElement >( null );
 	const section = useSelector( getSectionName );
-	const isWapuuEnabled = useIsWapuuEnabled();
-	const { isMinimized } = useSelect( ( select ) => {
-		const store = select( HELP_CENTER_STORE ) as HelpCenterSelect;
-		return {
-			isMinimized: store.getIsMinimized(),
-		};
-	}, [] );
 
 	useEffect( () => {
 		recordTracksEvent( 'calypso_helpcenter_page_open', {
@@ -63,7 +51,6 @@ const HelpCenterContent: React.FC< { isRelative?: boolean } > = () => {
 
 	// reset the scroll location on navigation, TODO: unless there's an anchor
 	useEffect( () => {
-		setSearchTerm( '' );
 		if ( containerRef.current ) {
 			containerRef.current.scrollTo( 0, 0 );
 		}
@@ -72,27 +59,11 @@ const HelpCenterContent: React.FC< { isRelative?: boolean } > = () => {
 	return (
 		<CardBody ref={ containerRef } className="help-center__container-content">
 			<Routes>
-				<Route path="/" element={ <HelpCenterSearch onSearchChange={ setSearchTerm } /> } />
+				<Route path="/" element={ <HelpCenterSearch /> } />
 				<Route path="/post" element={ <HelpCenterEmbedResult /> } />
 				<Route path="/contact-options" element={ <HelpCenterContactPage /> } />
 				<Route path="/contact-form" element={ <HelpCenterContactForm /> } />
 				<Route path="/success" element={ <SuccessScreen /> } />
-				<Route
-					path="/odie"
-					element={
-						<OdieAssistantProvider
-							botNameSlug="wpcom-support-chat"
-							botSetting="supportDocs"
-							botName="Wapuu"
-							enabled={ isWapuuEnabled }
-							isMinimized={ isMinimized }
-							initialUserMessage={ searchTerm }
-							extraContactOptions={ <HelpCenterContactPage hideHeaders /> }
-						>
-							<HelpCenterOdie />
-						</OdieAssistantProvider>
-					}
-				/>
 			</Routes>
 		</CardBody>
 	);
