b9ce9f806cc2f6288a67b11b4d779350dbbf0319
Dehan <dehandecroos@gmail.com>
2023-12-04 11:29:54 -0500

    Add comment

--- client/my-sites/plans-grid/components/plan-type-selector/components/customer-type-toggle.tsx
+++ client/my-sites/plans-grid/components/plan-type-selector/components/customer-type-toggle.tsx
@@ -7,6 +7,13 @@ import generatePath from '../utils';
 
 type CustomerTypeProps = Pick< PlanTypeSelectorProps, 'customerType' | 'isInSignup' | 'title' >;
 
+/**
+ * Not sure where this is exactly used
+ * First brought to a standalone component in: https://github.com/Automattic/wp-calypso/pull/46466
+ * First introduction in https://github.com/Automattic/wp-calypso/pull/27125
+ *
+ * Probably can be removed.
+ */
 export const CustomerTypeToggle: React.FunctionComponent< CustomerTypeProps > = ( props ) => {
 	const translate = useTranslate();
 	const { customerType, title } = props;
