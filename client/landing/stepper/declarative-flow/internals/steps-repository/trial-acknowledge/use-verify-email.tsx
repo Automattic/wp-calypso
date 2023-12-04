822aec2c80cb2bd7675b9ada99e3ce4ccb9e73e7
vykes-mac <47489215+vykes-mac@users.noreply.github.com>
2023-12-04 18:00:31 -0500

    Fix: Verify email hook throws null user exception  (#84830)
    * fix: user is possible null as a result exception is thrown when accessing email_verified property
    
    * add tests to verify exceptions are properly handled
    
    * Remove `as UserData` which incorrectly removed the possibility of nulls
    
    ---------
    
    Co-authored-by: Philip Jackson <p-jackson@live.com>

--- client/landing/stepper/declarative-flow/internals/steps-repository/trial-acknowledge/use-verify-email.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/trial-acknowledge/use-verify-email.tsx
@@ -2,7 +2,6 @@ import { useI18n } from '@wordpress/react-i18n';
 import { useEffect, useState } from 'react';
 import { useSendEmailVerification } from 'calypso/landing/stepper/hooks/use-send-email-verification';
 import { EVERY_FIVE_SECONDS } from 'calypso/lib/interval';
-import { UserData } from 'calypso/lib/user/user';
 import { useDispatch, useSelector } from 'calypso/state';
 import { fetchCurrentUser } from 'calypso/state/current-user/actions';
 import { getCurrentUser } from 'calypso/state/current-user/selectors';
@@ -10,14 +9,15 @@ import { successNotice, warningNotice } from 'calypso/state/notices/actions';
 
 export const useVerifyEmail = () => {
 	const dispatch = useDispatch();
-	const user = useSelector( getCurrentUser ) as UserData;
+	const user = useSelector( getCurrentUser );
+	const safeUser = user ?? { email_verified: false, email: '' };
 	const sendEmail = useSendEmailVerification();
 	const { __ } = useI18n();
 
 	const [ data, setData ] = useState( {
-		isVerified: user.email_verified,
+		isVerified: safeUser.email_verified,
 		isSending: false,
-		email: user.email,
+		email: safeUser.email,
 		resent: false,
 	} );
 
@@ -47,7 +47,7 @@ export const useVerifyEmail = () => {
 
 	useEffect( () => {
 		const interval = setInterval( () => {
-			if ( ! user.email_verified ) {
+			if ( ! safeUser.email_verified ) {
 				dispatch( fetchCurrentUser() );
 			} else {
 				setData( ( prevState ) => ( {
@@ -57,7 +57,7 @@ export const useVerifyEmail = () => {
 			}
 		}, EVERY_FIVE_SECONDS );
 		return () => clearInterval( interval );
-	}, [ user.email_verified, dispatch ] );
+	}, [ safeUser.email_verified, dispatch ] );
 
 	return {
 		isVerified: data.isVerified,
