2912940d2975fde1d6b462c30e916f491f062f54
tbradsha <32492176+tbradsha@users.noreply.github.com>
2023-12-05 11:09:28 -0700

    Add GetHelpNav to overview page

--- client/jetpack-cloud/sections/overview/primary/sidebar/index.tsx
+++ client/jetpack-cloud/sections/overview/primary/sidebar/index.tsx
@@ -1,3 +1,4 @@
+import GetHelpNav from '../../get-help-nav';
 import QuickLinksNav from '../quick-links-nav';
 import './style.scss';
 
@@ -10,18 +11,7 @@ export default function OverviewSidebar() {
 				Watch a short intro video
 			</section>
 			<QuickLinksNav />
-			<section className="get-help">
-				<h3>Get help</h3>
-				<ul>
-					<li> Get started guide </li>
-					<li> All sites management </li>
-					<li> Issuing, assigning, and revoking licenses </li>
-					<li> Billing/payment FAQs </li>
-					<li> Managing plugins </li>
-				</ul>
-				<div className="button-placeholder">Contact support</div>
-				Provide product feedback
-			</section>
+			<GetHelpNav />
 		</>
 	);
 }
