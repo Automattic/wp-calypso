39b359a881f68c0af500c691cadff480b55b78b1
Philip Jackson <p-jackson@live.com>
2023-12-06 12:10:53 +1300

    useFreeHostingTrialAssignment uses onboarding store flag

--- client/my-sites/plans-features-main/hooks/use-free-hosting-trial-assignment.ts
+++ client/my-sites/plans-features-main/hooks/use-free-hosting-trial-assignment.ts
@@ -1,23 +1,31 @@
 import { isEnabled } from '@automattic/calypso-config';
+import { useSelect } from '@wordpress/data';
+import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { useExperiment } from 'calypso/lib/explat';
+import type { OnboardSelect } from '@automattic/data-stores';
 import type { PlansIntent } from 'calypso/my-sites/plans-grid/hooks/npm-ready/data-store/use-grid-plans';
 
 export function useFreeHostingTrialAssignment( intent: PlansIntent | undefined ): {
 	isLoadingHostingTrialExperiment: boolean;
 	isAssignedToHostingTrialExperiment: boolean;
 } {
+	const hasHostingTrialOnboardingFlag = useSelect(
+		( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).isHostingTrialAvailable(),
+		[]
+	);
+
+	const isForcedToTreatment = isEnabled( 'plans/hosting-trial' ) || hasHostingTrialOnboardingFlag;
+
 	const [ isLoadingHostingTrialExperiment, experimentAssignment ] = useExperiment(
 		'wpcom_hosting_business_plan_free_trial_v2',
 		{
-			isEligible: intent === 'plans-new-hosted-site' && ! isEnabled( 'plans/hosting-trial' ),
+			isEligible: intent === 'plans-new-hosted-site' && ! isForcedToTreatment,
 		}
 	);
 
-	if ( isEnabled( 'plans/hosting-trial' ) ) {
+	if ( isForcedToTreatment ) {
 		return {
 			isLoadingHostingTrialExperiment: false,
-
-			// The plans/hosting-trial flag forces the user to be treated as if they're in the treatment group.
 			isAssignedToHostingTrialExperiment: intent === 'plans-new-hosted-site',
 		};
 	}
