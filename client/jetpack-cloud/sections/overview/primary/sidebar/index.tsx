7b3586fce8210e8d5223bf7c9f5bcc13d35e3005
tbradsha <32492176+tbradsha@users.noreply.github.com>
2023-12-05 11:27:41 -0700

    Add video

--- client/jetpack-cloud/sections/overview/primary/sidebar/index.tsx
+++ client/jetpack-cloud/sections/overview/primary/sidebar/index.tsx
@@ -1,15 +1,21 @@
+import { useTranslate } from 'i18n-calypso';
 import GetHelpNav from '../get-help-nav';
 import QuickLinksNav from '../quick-links-nav';
 import './style.scss';
 
 export default function OverviewSidebar() {
+	const translate = useTranslate();
 	return (
 		<>
-			<section className="intro-video">
-				<div className="video-placeholder"></div>
-				<p>Intro video</p>
-				Watch a short intro video
-			</section>
+			<iframe
+				className="intro-video"
+				title={ translate( 'Jetpack Manage Overview' ) }
+				aria-label={ translate( 'Jetpack Manage Overview' ) }
+				src={
+					'https://video.wordpress.com/embed/Z9piKY9s?hd=1&amp;autoPlay=0&amp;permalink=1&amp;loop=0&amp;preloadContent=metadata&amp;muted=0&amp;playsinline=0&amp;controls=1&amp;cover=1%27'
+				}
+				allowFullScreen
+			></iframe>
 			<QuickLinksNav />
 			<GetHelpNav />
 		</>
