dce30bb05655b7f35dc719b56114653e2d8deb24
Kristian Vitozev <kristian.vitozev@automattic.com>
2023-12-05 12:49:39 +0200

    Update empty state copy

--- client/jetpack-cloud/sections/partner-portal/issue-license-v2/licenses-form/index.tsx
+++ client/jetpack-cloud/sections/partner-portal/issue-license-v2/licenses-form/index.tsx
@@ -2,6 +2,7 @@ import { useTranslate } from 'i18n-calypso';
 import { useCallback, useContext, useState, useMemo } from 'react';
 import QueryProductsList from 'calypso/components/data/query-products-list';
 import LicenseProductCard from 'calypso/jetpack-cloud/sections/partner-portal/license-product-card';
+import { JETPACK_CONTACT_SUPPORT } from 'calypso/lib/url/support';
 import { useSelector } from 'calypso/state';
 import { getDisabledProductSlugs } from 'calypso/state/partner-portal/products/selectors';
 import LicenseMultiProductCard from '../../license-multi-product-card';
@@ -206,7 +207,16 @@ export default function LicensesForm( {
 			</div>
 
 			{ isSearchResultEmpty && (
-				<h3>{ translate( 'No results found. Please try refining your search.' ) } </h3>
+				<p>
+					{ translate(
+						"Sorry, we couldn't find a product with that name. Please refine your search, or {{link}}contact our support team{{/link}} if you continue to experience an issue.",
+						{
+							components: {
+								link: <a target="_blank" href={ JETPACK_CONTACT_SUPPORT } rel="noreferrer" />,
+							},
+						}
+					) }
+				</p>
 			) }
 
 			{ plans.length > 0 && (
