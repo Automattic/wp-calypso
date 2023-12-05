21e6f1e8b4146a7ba710542f6f713b0617db9acb
sejas <antonio@sejas.es>
2023-12-05 11:02:21 +0000

    Remove duplicated commands introduced by a conflict

--- client/sites-dashboard/components/wpcom-smp-commands.tsx
+++ client/sites-dashboard/components/wpcom-smp-commands.tsx
@@ -245,44 +245,6 @@ export const useCommandsArrayWpcom = ( {
 			icon: wordpressIcon,
 		},
 		{
-			name: 'addJetpack',
-			label: __( 'Add Jetpack to a self-hosted site' ),
-			searchLabel: [
-				_x(
-					'Add Jetpack to a self-hosted site',
-					'Keyword for Add Jetpack to a self-hosted site command'
-				),
-				_x( 'connect jetpack', 'Keyword for Add Jetpack to a self-hosted site command' ),
-			].join( ' ' ),
-			callback: ( { close }: { close: () => void } ) => {
-				close();
-				navigate( `/jetpack/connect?cta_from=command-palette` );
-			},
-			icon: <JetpackLogo className="gridicon" size={ 18 } />,
-		},
-		{
-			name: 'importSite',
-			label: __( 'Import site to WordPress.com' ),
-			searchLabel: [
-				_x( 'Import site to WordPress.com', 'Keyword for Import site to WordPress.com command' ),
-				_x( 'migrate site', 'Keyword for Import site to WordPress.com command' ),
-			].join( ' ' ),
-			callback: ( { close }: { close: () => void } ) => {
-				close();
-				navigate( `/start/import?source=command-palette` );
-			},
-			icon: arrowDownIcon,
-		},
-		{
-			name: 'openReader',
-			label: __( 'Open reader' ),
-			callback: ( { close }: { close: () => void } ) => {
-				close();
-				navigate( `/read` );
-			},
-			icon: <Gridicon icon="reader" />,
-		},
-		{
 			name: 'clearCache',
 			label: __( 'Clear cache' ),
 			callback: setStateCallback( 'clearCache', __( 'Select a site to clear cache' ) ),
