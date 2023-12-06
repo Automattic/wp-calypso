1c3cc736a1f067328203b63625683659952e3c78
DustyReagan <dusty@dustyreagan.com>
2023-12-06 08:41:17 -0600

    Copy update and remove dead code

--- client/signup/steps/domains/index.jsx
+++ client/signup/steps/domains/index.jsx
@@ -664,7 +664,7 @@ export class RenderDomainsStep extends Component {
 				.catch( () => {
 					this.setState( {
 						replaceDomainFailedMessage: this.props.translate(
-							'Sorry, there was an error adding that domain. Please try again later.'
+							'Sorry, there was a problem adding that domain. Please try again later.'
 						),
 					} );
 					this.props.shoppingCartManager.reloadFromServer();
@@ -741,7 +741,7 @@ export class RenderDomainsStep extends Component {
 				.catch( () => {
 					this.setState( {
 						replaceDomainFailedMessage: this.props.translate(
-							'Sorry, we had a problem removing that domain. Please refresh the page and try again.'
+							'Sorry, there was a problem removing that domain. Please try again later.'
 						),
 					} );
 					this.setState( { isCartPendingUpdateDomain: null } );
@@ -1357,11 +1357,9 @@ export class RenderDomainsStep extends Component {
 		if ( this.props.step && 'invalid' === this.props.step.status ) {
 			content = (
 				<div className="domains__step-section-wrapper">
-					{ this.props.step && 'invalid' === this.props.step.status && (
-						<Notice status="is-error" showDismiss={ false }>
-							{ this.props.step.errors.message }
-						</Notice>
-					) }
+					<Notice status="is-error" showDismiss={ false }>
+						{ this.props.step.errors.message }
+					</Notice>
 					{ content }
 				</div>
 			);
