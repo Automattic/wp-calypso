d4daf7fb465cfa02960a2698a3c7b58b0ccf657f
arthur <arthur.chu@automattic.com>
2023-12-06 14:45:50 +0800

    Site Picker List Step: Submit the selected site immediately

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-picker-list/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-picker-list/index.tsx
@@ -1,10 +1,8 @@
 import { StepContainer, isSiteAssemblerFlow } from '@automattic/onboarding';
-import { useSelect } from '@wordpress/data';
+import { select } from '@wordpress/data';
 import { useTranslate } from 'i18n-calypso';
-import { useEffect, useState } from 'react';
 import QuerySites from 'calypso/components/data/query-sites';
 import FormattedHeader from 'calypso/components/formatted-header';
-import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
 import SiteSelector from 'calypso/components/site-selector';
 import { SITE_STORE } from 'calypso/landing/stepper/stores';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
@@ -18,27 +16,11 @@ const SitePicker: Step = function SitePicker( { navigation, flow } ) {
 	const translate = useTranslate();
 	const { submit, goBack } = navigation;
 
-	const [ selectedSiteId, setSelectedSiteId ] = useState< SiteId | null >( null );
-
-	const site = useSelect(
-		( select ) =>
-			( selectedSiteId && ( select( SITE_STORE ) as SiteSelect ).getSite( selectedSiteId ) ) ||
-			null,
-		[ selectedSiteId ]
-	);
-
-	useEffect( () => {
-		if ( ! site ) {
-			return;
-		}
+	const selectSite = ( siteId: SiteId ) => {
+		const site = ( select( SITE_STORE ) as SiteSelect ).getSite( siteId );
 		const siteSlug = new URL( site?.URL || '' ).host;
-		const siteId = site?.ID;
 
 		submit?.( { siteSlug, siteId } );
-	}, [ site ] );
-
-	const selectSite = ( siteId: SiteId ) => {
-		setSelectedSiteId( siteId );
 	};
 
 	const filter = ( site: SiteDetails ) => {
@@ -58,8 +40,7 @@ const SitePicker: Step = function SitePicker( { navigation, flow } ) {
 				stepContent={
 					<div className="site-picker__container">
 						<QuerySites allSites />
-						{ ! selectedSiteId && <SiteSelector filter={ filter } onSiteSelect={ selectSite } /> }
-						{ selectedSiteId && <LoadingEllipsis /> }
+						<SiteSelector filter={ filter } onSiteSelect={ selectSite } />
 					</div>
 				}
 				formattedHeader={
