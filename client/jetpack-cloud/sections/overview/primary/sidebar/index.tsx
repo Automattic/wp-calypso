76e5b7370bd80fc89f47f0156bac2ac7dce407fc
tbradsha <32492176+tbradsha@users.noreply.github.com>
2023-12-07 11:50:27 -0700

    Jetpack Manage: add contact support button and video to overview page sidebar (#84978)
    * Add video
    
    * Add button
    
    * Move things into sections for better styling control
    
    * Remove placeholder styles
    
    * Move styles closer to Figma
    
    * Move button into last sidebar section
    
    * Add tracks to button
    
    * Make button open in new window

--- client/jetpack-cloud/sections/overview/primary/sidebar/index.tsx
+++ client/jetpack-cloud/sections/overview/primary/sidebar/index.tsx
@@ -1,17 +1,48 @@
+import { Button } from '@automattic/components';
+import { localizeUrl } from '@automattic/i18n-utils';
+import { useTranslate } from 'i18n-calypso';
+import { useDispatch } from 'calypso/state';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import GetHelpNav from '../get-help-nav';
 import QuickLinksNav from '../quick-links-nav';
 import './style.scss';
 
 export default function OverviewSidebar() {
+	const dispatch = useDispatch();
+	const translate = useTranslate();
 	return (
 		<>
-			<section className="intro-video">
-				<div className="video-placeholder"></div>
-				<p>Intro video</p>
-				Watch a short intro video
+			<section>
+				<iframe
+					className="intro-video"
+					title={ translate( 'Jetpack Manage Overview' ) }
+					aria-label={ translate( 'Jetpack Manage Overview' ) }
+					src={
+						'https://video.wordpress.com/embed/Z9piKY9s?hd=1&amp;autoPlay=0&amp;permalink=1&amp;loop=0&amp;preloadContent=metadata&amp;muted=0&amp;playsinline=0&amp;controls=1&amp;cover=1%27'
+					}
+					allowFullScreen
+				></iframe>
 			</section>
-			<QuickLinksNav />
-			<GetHelpNav />
+
+			<section>
+				<QuickLinksNav />
+			</section>
+
+			<section>
+				<GetHelpNav />
+				<Button
+					className="contact-support-button"
+					href={ localizeUrl( 'https://jetpack.com/contact-support/' ) }
+					onClick={ () =>
+						dispatch(
+							recordTracksEvent( 'calypso_jetpack_manage_overview_contact_support_button_click' )
+						)
+					}
+					target="_blank"
+				>
+					{ translate( 'Contact support' ) }
+				</Button>
+			</section>
 		</>
 	);
 }
