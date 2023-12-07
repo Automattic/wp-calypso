5be9d8553b5cd4cc1a6e416beabe400c5c157438
tbradsha <32492176+tbradsha@users.noreply.github.com>
2023-12-07 08:13:25 -0700

    Move things into sections for better styling control

--- client/jetpack-cloud/sections/overview/primary/sidebar/index.tsx
+++ client/jetpack-cloud/sections/overview/primary/sidebar/index.tsx
@@ -9,25 +9,35 @@ export default function OverviewSidebar() {
 	const translate = useTranslate();
 	return (
 		<>
-			<iframe
-				className="intro-video"
-				title={ translate( 'Jetpack Manage Overview' ) }
-				aria-label={ translate( 'Jetpack Manage Overview' ) }
-				src={
-					'https://video.wordpress.com/embed/Z9piKY9s?hd=1&amp;autoPlay=0&amp;permalink=1&amp;loop=0&amp;preloadContent=metadata&amp;muted=0&amp;playsinline=0&amp;controls=1&amp;cover=1%27'
-				}
-				allowFullScreen
-			></iframe>
-			<QuickLinksNav />
-			<GetHelpNav />
+			<section>
+				<iframe
+					className="intro-video"
+					title={ translate( 'Jetpack Manage Overview' ) }
+					aria-label={ translate( 'Jetpack Manage Overview' ) }
+					src={
+						'https://video.wordpress.com/embed/Z9piKY9s?hd=1&amp;autoPlay=0&amp;permalink=1&amp;loop=0&amp;preloadContent=metadata&amp;muted=0&amp;playsinline=0&amp;controls=1&amp;cover=1%27'
+					}
+					allowFullScreen
+				></iframe>
+			</section>
 
-			<Button
-				href={ localizeUrl( 'https://jetpack.com/contact-support/' ) }
-				className="contact-support-button"
-				onClick={ () => {} }
-			>
-				{ translate( 'Contact support' ) }
-			</Button>
+			<section>
+				<QuickLinksNav />
+			</section>
+
+			<section>
+				<GetHelpNav />
+			</section>
+
+			<section>
+				<Button
+					href={ localizeUrl( 'https://jetpack.com/contact-support/' ) }
+					className="contact-support-button"
+					onClick={ () => {} }
+				>
+					{ translate( 'Contact support' ) }
+				</Button>
+			</section>
 		</>
 	);
 }
