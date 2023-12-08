43fdc9ac7b132a840ee2f66fc585f45de4535e6d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2023-12-08 12:57:34 +0100

    Credentials: replace onboarding title comp with formatedHeader

--- client/blocks/importer/wordpress/import-everything/pre-migration/credentials.tsx
+++ client/blocks/importer/wordpress/import-everything/pre-migration/credentials.tsx
@@ -1,7 +1,7 @@
 import { SiteDetails } from '@automattic/data-stores';
-import { Title } from '@automattic/onboarding';
 import { useTranslate } from 'i18n-calypso';
 import React, { useState } from 'react';
+import FormattedHeader from 'calypso/components/formatted-header';
 import { CredentialsForm } from './credentials-form';
 import { CredentialsHelper } from './credentials-helper';
 import type { StartImportTrackingProps } from './types';
@@ -33,10 +33,9 @@ export function Credentials( props: Props ) {
 
 	return (
 		<div className="import__pre-migration import__import-everything import__import-everything--redesign">
-			<div className="import__heading-title">
-				<Title>{ translate( 'You are ready to migrate' ) }</Title>
+			<div className="import__header">
+				<FormattedHeader headerText={ translate( 'You are ready to migrate' ) } />
 			</div>
-
 			<div className="pre-migration__form-container pre-migration__credentials-form">
 				<div className="pre-migration__form">
 					<CredentialsForm
