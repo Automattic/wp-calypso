a6b042bf20ee6ca088193de6d494cfc2059a1e2a
Dehan <dehandecroos@gmail.com>
2023-12-08 14:25:58 -0500

    Finalized discount change

--- client/my-sites/plans-grid/components/plan-type-selector/components/interval-type-dropdown.tsx
+++ client/my-sites/plans-grid/components/plan-type-selector/components/interval-type-dropdown.tsx
@@ -1,9 +1,14 @@
 import styled from '@emotion/styled';
 import { CustomSelectControl } from '@wordpress/components';
-import { IntervalTypeProps } from '../types';
-import generatePath from '../utils';
+import useIntervalOptions from '../hooks/use-interval-options';
+import { IntervalTypeProps, SupportedUrlFriendlyTermType } from '../types';
 
 const AddOnOption = styled.a`
+	& span.name,
+	&:visited span.name,
+	&:hover span.name {
+		color: var( --color-text );
+	}
 	.discount {
 		color: var( --studio-green-40 );
 		display: inline-block;
@@ -17,67 +22,27 @@ const AddOnOption = styled.a`
 const StyledCustomSelectControl = styled( CustomSelectControl )`
 	&,
 	&:visited,
-	&:hover .name {
+	&:hover span.name {
 		color: var( --color-text );
 	}
 	.components-custom-select-control__button {
-		min-width: 195px;
+		min-width: 225px;
 	}
 	.components-custom-select-control__menu {
 		margin: 0;
 	}
 `;
 
-const optionsMap: Record<
-	string,
-	{ key: string; name: string; discountText: string; url: string; termInMonths: number }
-> = {
-	yearly: { key: 'yearly', name: 'Pay yearly', discountText: '55% off', url: '', termInMonths: 12 },
-	'2yearly': {
-		key: '2yearly',
-		name: 'Pay every 2 years',
-		discountText: '63% off',
-		url: '',
-		termInMonths: 24,
-	},
-	'3yearly': {
-		key: '3yearly',
-		name: 'Pay every 3 years',
-		discountText: '69% off',
-		url: '',
-		termInMonths: 36,
-	},
-	monthly: { key: 'monthly', name: 'Pay monthly', discountText: '', url: '', termInMonths: 1 },
-};
-const optionsList = Object.values( optionsMap );
-
 export const IntervalTypeDropdown: React.FunctionComponent< IntervalTypeProps > = ( props ) => {
 	const { intervalType } = props;
-	const additionalPathProps = {
-		...( props.redirectTo ? { redirect_to: props.redirectTo } : {} ),
-		...( props.selectedPlan ? { plan: props.selectedPlan } : {} ),
-		...( props.selectedFeature ? { feature: props.selectedFeature } : {} ),
-	};
-
-	const isDomainUpsellFlow = new URLSearchParams( window.location.search ).get( 'domain' );
-
-	const isDomainAndPlanPackageFlow = new URLSearchParams( window.location.search ).get(
-		'domainAndPlanPackage'
-	);
-
-	const isJetpackAppFlow = new URLSearchParams( window.location.search ).get( 'jetpackAppPlans' );
-	const selectOptonsList = optionsList.map( ( option ) => ( {
+	const supportedIntervalType = (
+		[ 'yearly', '2yearly', '3yearly', 'monthly' ].includes( intervalType ) ? intervalType : 'yearly'
+	) as SupportedUrlFriendlyTermType;
+	const optionsList = useIntervalOptions( props );
+	const selectOptionsList = Object.values( optionsList ).map( ( option ) => ( {
 		key: option.key,
 		name: (
-			<AddOnOption
-				href={ generatePath( props, {
-					intervalType: option.key,
-					domain: isDomainUpsellFlow,
-					domainAndPlanPackage: isDomainAndPlanPackageFlow,
-					jetpackAppPlans: isJetpackAppFlow,
-					...additionalPathProps,
-				} ) }
-			>
+			<AddOnOption href={ option.url }>
 				<span className="name"> { option.name } </span>
 				<span className="discount"> { option.discountText } </span>
 			</AddOnOption>
@@ -86,8 +51,8 @@ export const IntervalTypeDropdown: React.FunctionComponent< IntervalTypeProps > 
 	return (
 		<StyledCustomSelectControl
 			label=""
-			options={ selectOptonsList }
-			value={ optionsMap[ intervalType ] }
+			options={ selectOptionsList }
+			value={ selectOptionsList.find( ( { key } ) => key === supportedIntervalType ) }
 		/>
 	);
 };
---
+++ client/my-sites/plans-grid/components/plan-type-selector/hooks/use-term-vise-max-dicounts.ts
@@ -0,0 +1,97 @@
+import {
+	PlanSlug,
+	URL_FRIENDLY_TERMS_MAPPING,
+	UrlFriendlyTermType,
+	getPlanMultipleTermsVariantSlugs,
+	getPlanSlugForTermVariant,
+	isMonthly,
+	isWpComPlan,
+	isWpcomEnterpriseGridPlan,
+} from '@automattic/calypso-products';
+import { UsePricingMetaForGridPlans } from 'calypso/my-sites/plans-grid/hooks/npm-ready/data-store/use-grid-plans';
+
+export default function useTermViseMaxDiscounts(
+	plans: PlanSlug[],
+	urlFriendlyTerms: UrlFriendlyTermType[] = [],
+	usePricingMetaForGridPlans: UsePricingMetaForGridPlans
+): Record< UrlFriendlyTermType, number > {
+	const termDefinitionsMapping = urlFriendlyTerms.map( ( urlFriendlyTerm ) => ( {
+		urlFriendlyTerm,
+		term: URL_FRIENDLY_TERMS_MAPPING[ urlFriendlyTerm ],
+	} ) );
+	const terms = termDefinitionsMapping.map( ( { term } ) => term );
+	if ( terms.length !== urlFriendlyTerms.length ) {
+		throw new Error( 'Invalid term passed in:' + JSON.stringify( urlFriendlyTerms ) );
+	}
+
+	const allRelatedPlanSlugs = plans
+		.filter( ( planSlug ) => ! isWpcomEnterpriseGridPlan( planSlug ) )
+		.map( ( plan ) => getPlanMultipleTermsVariantSlugs( plan, terms ) )
+		.flat()
+		.filter( Boolean )
+		.filter( isWpComPlan ) as PlanSlug[];
+
+	const wpcomMonthlyPlanSlugs = allRelatedPlanSlugs.filter( isMonthly );
+	const wpcomNonMonthlyPlans = allRelatedPlanSlugs.filter(
+		( planSlug ) => ! isMonthly( planSlug )
+	);
+
+	const monthlyPlansPricing = usePricingMetaForGridPlans( {
+		planSlugs: wpcomMonthlyPlanSlugs,
+		withoutProRatedCredits: true,
+		storageAddOns: null,
+	} );
+	const nonMonthlyPlansPricing = usePricingMetaForGridPlans( {
+		planSlugs: wpcomNonMonthlyPlans,
+		withoutProRatedCredits: true,
+		storageAddOns: null,
+	} );
+
+	const getTermInMonths = ( term: UrlFriendlyTermType ): number => {
+		switch ( term ) {
+			case '3yearly':
+				return 36;
+			case '2yearly':
+				return 24;
+			case 'yearly':
+				return 12;
+			case 'monthly':
+			default:
+				return 1;
+		}
+	};
+
+	const termViseMaxDiscount: Record< UrlFriendlyTermType, number > = {} as Record<
+		UrlFriendlyTermType,
+		number
+	>;
+	termDefinitionsMapping.forEach( ( termMapping ) => {
+		const termDiscounts = wpcomMonthlyPlanSlugs.map( ( monthlyPlanSlug ) => {
+			const variantPlanSlug = getPlanSlugForTermVariant( monthlyPlanSlug, termMapping.term );
+			if ( ! variantPlanSlug ) {
+				return 0;
+			}
+
+			const monthlyPlanPricing = monthlyPlansPricing?.[ monthlyPlanSlug ];
+			const variantPlanPricing = nonMonthlyPlansPricing?.[ variantPlanSlug ];
+
+			const monthlyPlanCost = monthlyPlanPricing?.originalPrice.full;
+
+			if ( ! monthlyPlanCost || ! variantPlanPricing ) {
+				return 0;
+			}
+
+			const variantTermPrice =
+				variantPlanPricing.discountedPrice.full || variantPlanPricing.originalPrice.full || 0;
+			const variantTermInMonths = getTermInMonths( termMapping.urlFriendlyTerm );
+			const variantTermMonthlyCost = variantTermPrice / variantTermInMonths;
+
+			return Math.floor( ( ( monthlyPlanCost - variantTermMonthlyCost ) * 100 ) / monthlyPlanCost );
+		} );
+		termViseMaxDiscount[ termMapping.urlFriendlyTerm ] = termDiscounts.length
+			? Math.max( ...termDiscounts )
+			: 0;
+	} );
+
+	return termViseMaxDiscount;
+}
