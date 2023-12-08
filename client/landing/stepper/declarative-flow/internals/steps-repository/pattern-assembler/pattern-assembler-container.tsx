488d4b13609bce4ddf870532c411240822d6ea99
Miguel <miguel.san.segundo@automattic.com>
2023-12-09 01:48:51 +0700

    Fix flags

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/pattern-assembler-container.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/pattern-assembler-container.tsx
@@ -1,4 +1,5 @@
 import { BlockRendererProvider, PatternsRendererProvider } from '@automattic/block-renderer';
+import { isEnabled } from '@automattic/calypso-config';
 import { PLACEHOLDER_SITE_ID } from '@automattic/data-stores/src/site/constants';
 import { useMemo } from 'react';
 import StepperLoader from '../../components/stepper-loader';
@@ -46,7 +47,8 @@ const PatternAssemblerContainer = ( {
 				// such as the logo, title, tagline, pages, posts, etc.
 				// For the newly created site, we use the placeholder site to render the content.
 				// Otherwise, we use the current site to display the site-related blocks.
-				siteId={ isNewSite ? PLACEHOLDER_SITE_ID : siteId }
+				// For Horizon, forcing to use the content from Assembler demo site to help testing with any existing site.
+				siteId={ isNewSite || isEnabled( 'pattern-assembler/v2' ) ? PLACEHOLDER_SITE_ID : siteId }
 				stylesheet={ stylesheet }
 				patternIdsByCategory={ patternIdsByCategory }
 				// Use siteInfo to overwrite site-related things such as title, and tagline.
--- packages/data-stores/src/site/constants.ts
+++ packages/data-stores/src/site/constants.ts
@@ -1,3 +1,7 @@
+import { isEnabled } from '@automattic/calypso-config';
+
 export const STORE_KEY = 'automattic/site';
 
-export const PLACEHOLDER_SITE_ID = 224076220; // creatio2demo.wordpress.com
+export const PLACEHOLDER_SITE_ID = isEnabled( 'pattern-assembler/v2' )
+	? 226011606 // assemblerdemo.wordpress.com
+	: 224076220; // creatio2demo.wordpress.com
