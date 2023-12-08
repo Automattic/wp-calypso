64235512e34a7c3cc59b2f3f5971b03d6e527bf4
Jeremy Yip <jeremy.yip@automattic.com>
2023-12-08 10:44:14 -0800

    Add comment about plan upgradeability without a given site Id

--- client/my-sites/plans-features-main/hooks/use-plan-upgradeability-check.ts
+++ client/my-sites/plans-features-main/hooks/use-plan-upgradeability-check.ts
@@ -11,6 +11,11 @@ type PlanUpgradeability = {
 	[ planSlug in PlanSlug ]: boolean;
 };
 
+/**
+ * Returns a dictionary of plan slugs and whether or not they are available for purchase.
+ * Note that if there is no selectedSiteId, then we assume that we are in onboarding or
+ * signup, which will, by default, make the plan purchaseable.
+ */
 const usePlanUpgradeabilityCheck = ( { planSlugs }: Props ): PlanUpgradeability => {
 	const selectedSiteId = useSelector( getSelectedSiteId );
 	const planUpgradeability = useSelector( ( state ) => {
