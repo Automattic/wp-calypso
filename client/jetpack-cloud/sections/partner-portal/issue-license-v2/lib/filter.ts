d70ed9eee96a8197c2619fab39b5f4852a3f2062
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-12-11 16:16:30 +0800

    Add support for included products in the Products search filter.

---
+++ client/jetpack-cloud/sections/partner-portal/issue-license-v2/lib/filter.ts
@@ -0,0 +1,36 @@
+import { APIProductFamilyProduct } from 'calypso/state/partner-portal/types';
+
+/* check if product matches the search query
+ *
+ * @param product - product to check
+ * @param productSearchQuery - search query to match against
+ *
+ * @returns boolean - true if product matches the search query, false otherwise
+ */
+export const isProductMatch = ( product: APIProductFamilyProduct, productSearchQuery: string ) => {
+	const nameFilter = ( name: string ) =>
+		name.toLowerCase().includes( productSearchQuery.toLowerCase() );
+
+	if ( product.slug.startsWith( 'jetpack-complete' ) ) {
+		return [
+			'Jetpack VaultPress Backup',
+			'Jetpack Scan',
+			'Jetpack Akismet Anti-spam',
+			'Jetpack VideoPress',
+			'Jetpack Boost',
+			'Jetpack Social',
+			'Jetpack Search',
+			'Jetpack Stats',
+			'Jetpack CRM',
+			'Jetpack Creator',
+		].some( nameFilter );
+	}
+
+	if ( product.slug.startsWith( 'jetpack-security' ) ) {
+		return [ 'Jetpack VaultPress Backup', 'Jetpack Scan', 'Jetpack Akismet Anti-spam' ].some(
+			nameFilter
+		);
+	}
+
+	return nameFilter( product.name );
+};
