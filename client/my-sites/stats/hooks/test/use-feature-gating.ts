f3022c5344d1185c35258e57cf03c6d645e76469
Candy Tsai <candy02058912@gmail.com>
2023-12-11 13:52:41 +0800

    Add unit test

---
+++ client/my-sites/stats/hooks/test/use-feature-gating.ts
@@ -0,0 +1,38 @@
+import { FEATURE_STATS_PAID } from '@automattic/calypso-products';
+import { checkFeatureGating } from '../use-feature-gating';
+
+jest.mock( '@automattic/calypso-config', () => {
+	const config = () => 'development';
+	config.isEnabled = ( property: string ) => property === 'stats/paid-wpcom-v2';
+	return config;
+} );
+
+describe( 'checkFeatureGating', () => {
+	const siteId = 123;
+	const mockState = {
+		sites: {
+			features: {
+				// mock site id 123
+				[ siteId ]: {
+					data: {
+						active: [ FEATURE_STATS_PAID ],
+					},
+				},
+			},
+		},
+	};
+
+	it( 'should allow access when paid stats are enabled and the feature type is included', () => {
+		const featureType = 'statsSearchTerms';
+		// Mock siteHasFeature and isEnabled functions as necessary
+		const result = checkFeatureGating( mockState, siteId, featureType );
+		expect( result ).toBe( true );
+	} );
+
+	it( 'should not allow access when paid stats are enabled and the feature type is not included', () => {
+		const featureType = 'nonExistentFeature';
+		// Mock siteHasFeature and isEnabled functions as necessary
+		const result = checkFeatureGating( mockState, siteId, featureType );
+		expect( result ).toBe( false );
+	} );
+} );
