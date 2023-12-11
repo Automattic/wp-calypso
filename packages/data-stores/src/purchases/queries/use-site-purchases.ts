5fd380426109d624188af8f9b6e017846d0cf28c
Christos <chriskmnds@gmail.com>
2023-12-11 15:24:41 +0200

    clean

--- packages/data-stores/src/purchases/queries/use-site-purchases.ts
+++ packages/data-stores/src/purchases/queries/use-site-purchases.ts
@@ -1,45 +1,41 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, type UseQueryResult } from '@tanstack/react-query';
 import wpcomRequest from 'wpcom-proxy-request';
 import { createPurchaseObject } from '../lib/assembler';
 import useQueryKeysFactory from './lib/use-query-keys-factory';
 import type { RawPurchase, Purchase } from '../types';
 
-export interface PurchasesIndex {
+interface PurchasesIndex {
 	[ purchaseId: number ]: Purchase;
 }
 
-interface Props< T > {
+interface Props {
 	siteId?: string | number | null;
-	select?: ( data: PurchasesIndex ) => T;
 }
 
 /**
  * Fetches all purchases for a given site, transformed into a map of purchaseId => Purchase
- * - The generic T allows to define generic select functions that can be used to select a subset of the data
  * @param {Object} props - The properties for the function
  * @param props.siteId Site ID
- * @param props.select Optional selector function to subscribe to a subset of the data
  * @returns Query result
  */
-function useSitePurchases< T >( { siteId, select }: Props< T > ) {
+function useSitePurchases( { siteId }: Props ): UseQueryResult< PurchasesIndex > {
 	const queryKeys = useQueryKeysFactory();
 
 	return useQuery( {
 		queryKey: queryKeys.sitePurchases( siteId ),
-		queryFn: async (): Promise< PurchasesIndex > => {
-			const purchases = await wpcomRequest( {
+		queryFn: async () => {
+			const purchases: RawPurchase[] = await wpcomRequest( {
 				path: `/sites/${ encodeURIComponent( siteId as string ) }/purchases`,
 				apiVersion: '1.1',
 			} );
 
 			return Object.fromEntries(
-				purchases.map( ( rawPurchase: RawPurchase ) => {
+				purchases.map( ( rawPurchase ) => {
 					const purchase = createPurchaseObject( rawPurchase );
 					return [ purchase.id, purchase ];
 				} )
 			);
 		},
-		select,
 		staleTime: 1000 * 60 * 5, // 5 minutes
 		enabled: !! siteId,
 	} );
