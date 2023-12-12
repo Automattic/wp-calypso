5ef40a86519bc859f48d2bf3793c4b365f41a72b
Miguel <miguel.san.segundo@automattic.com>
2023-12-12 13:51:20 +0700

    Revert

--- packages/data-stores/src/site/constants.ts
+++ packages/data-stores/src/site/constants.ts
@@ -2,10 +2,6 @@ import { isEnabled } from '@automattic/calypso-config';
 
 export const STORE_KEY = 'automattic/site';
 
-export const PLACEHOLDER_SITE_ID = isEnabled( 'pattern-assembler/v2' )
-	? 226011606 // assemblerdemo.wordpress.com
-	: 224076220; // creatio2demo.wordpress.com
-
 export const getPlaceholderSiteID = () =>
 	isEnabled( 'pattern-assembler/v2' )
 		? '226011606' // assemblerdemo
