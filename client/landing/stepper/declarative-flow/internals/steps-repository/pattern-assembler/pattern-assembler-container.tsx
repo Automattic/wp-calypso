b730502f5a9c383641dbdf755e88026a22bf8236
Miguel <miguel.san.segundo@automattic.com>
2023-12-12 13:43:38 +0700

    Turn PATTERN_SOURCE_SITE_ID into function in data-store package

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/pattern-assembler-container.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/pattern-assembler-container.tsx
@@ -1,6 +1,6 @@
 import { BlockRendererProvider, PatternsRendererProvider } from '@automattic/block-renderer';
 import { isEnabled } from '@automattic/calypso-config';
-import { PLACEHOLDER_SITE_ID } from '@automattic/data-stores/src/site/constants';
+import { getPatternSourceSiteID } from '@automattic/data-stores/src/site/constants';
 import { useMemo } from 'react';
 import StepperLoader from '../../components/stepper-loader';
 import { encodePatternId } from './utils';
@@ -48,7 +48,9 @@ const PatternAssemblerContainer = ( {
 				// For the newly created site, we use the placeholder site to render the content.
 				// Otherwise, we use the current site to display the site-related blocks.
 				// For Horizon, forcing to use the content from Assembler demo site to help testing with any existing site.
-				siteId={ isNewSite || isEnabled( 'pattern-assembler/v2' ) ? PLACEHOLDER_SITE_ID : siteId }
+				siteId={
+					isNewSite || isEnabled( 'pattern-assembler/v2' ) ? getPatternSourceSiteID() : siteId
+				}
 				stylesheet={ stylesheet }
 				patternIdsByCategory={ patternIdsByCategory }
 				// Use siteInfo to overwrite site-related things such as title, and tagline.
--- packages/data-stores/src/site/constants.ts
+++ packages/data-stores/src/site/constants.ts
@@ -2,6 +2,7 @@ import { isEnabled } from '@automattic/calypso-config';
 
 export const STORE_KEY = 'automattic/site';
 
-export const PLACEHOLDER_SITE_ID = isEnabled( 'pattern-assembler/v2' )
-	? 226011606 // assemblerdemo.wordpress.com
-	: 224076220; // creatio2demo.wordpress.com
+export const getPatternSourceSiteID = () =>
+	isEnabled( 'pattern-assembler/v2' )
+		? '226765597' // assemblerv2patterns
+		: '174455321'; // dotcompatterns
