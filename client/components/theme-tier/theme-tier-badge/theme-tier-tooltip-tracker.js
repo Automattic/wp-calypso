57d4eb825d5b5b9ee182f542fc5e97607b1be482
Jacopo Tomasone <Copons@users.noreply.github.com>
2023-12-12 16:08:31 +0000

    Theme Showcase: Add Theme Tier Context and Tooltip (#85105)
    * Add a new ThemeTierBadgeContext to simplify data propagation around theme labels and tooltips.
    * Add tooltips to all existing ThemeTierBadge variations.
    * Simplify them by using the new context.

--- client/components/theme-tier/theme-tier-badge/theme-tier-tooltip-tracker.js
+++ client/components/theme-tier/theme-tier-badge/theme-tier-tooltip-tracker.js
@@ -2,12 +2,12 @@ import { recordTracksEvent } from '@automattic/calypso-analytics';
 import { useEffect } from 'react';
 import { useThemeTierBadgeContext } from './theme-tier-badge-context';
 
-export default function ThemeTierBadgeTracker() {
+export default function ThemeTierTooltipTracker() {
 	const { themeId } = useThemeTierBadgeContext();
 
 	useEffect( () => {
 		recordTracksEvent( 'calypso_upgrade_nudge_impression', {
-			cta_name: 'theme-upsell',
+			cta_name: 'theme-upsell-popup',
 			theme: themeId,
 		} );
 	}, [ themeId ] );
