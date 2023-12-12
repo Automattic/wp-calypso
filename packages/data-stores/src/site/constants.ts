06fce70e98830f9969a5c1f6f1bebddf630090b4
Miguel San Segundo <1881481+miksansegundo@users.noreply.github.com>
2023-12-12 17:21:02 +0700

    Assembler Level-Up: Use new patterns under feature flag (#84977)
    * Add feature flag pattern-assembler/v2
    
    * Fetch v2 patterns and add comment for pages
    
    * Assembler v2 shows v1 patterns without priority
    
    * Use wp_block post type patterns from assemblerv2patterns
    
    * Fix flags
    
    * Revert reverse
    
    * Revert flag assembler-first changes
    
    * Turn PATTERN_SOURCE_SITE_ID into function
    
    * Turn PATTERN_SOURCE_SITE_ID into function in data-store package
    
    * Turn PLACEHOLDER_SITE_ID to a function avoid race-condition issues
    
    * Revert
    
    * Assembler-first flow uses the Assembler theme when pattern-assembler/v2 is enabled

--- packages/data-stores/src/site/constants.ts
+++ packages/data-stores/src/site/constants.ts
@@ -1,3 +1,8 @@
+import { isEnabled } from '@automattic/calypso-config';
+
 export const STORE_KEY = 'automattic/site';
 
-export const PLACEHOLDER_SITE_ID = 224076220; // creatio2demo.wordpress.com
+export const getPlaceholderSiteID = () =>
+	isEnabled( 'pattern-assembler/v2' )
+		? '226011606' // assemblerdemo
+		: '224076220'; // creatio2demo
