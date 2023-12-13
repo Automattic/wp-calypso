a7dca1b56f55e6fcd57e0d2fe63bf053412a078b
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-12-13 14:31:00 +0800

    Remove redundancy on the product titles.

---
+++ client/jetpack-cloud/sections/partner-portal/lib/get-product-short-title.ts
@@ -0,0 +1,21 @@
+import getProductTitle from './get-product-title';
+import getProductVariantShortTitle from './get-product-variant-short-title';
+
+/**
+ * Get the short version of the product title
+ * @param product Product
+ * @param removeVariant if we need to remove variant information
+ * @returns Product title
+ */
+export default function getProductShortTitle(
+	product: string,
+	removeVariant: boolean = false
+): string {
+	const title = getProductTitle( product, removeVariant );
+
+	if ( title.startsWith( 'VaultPress Backup' ) ) {
+		return getProductVariantShortTitle( title );
+	}
+
+	return title.replaceAll( /(?:Woocommerce\s|[)(])/gi, '' );
+}
