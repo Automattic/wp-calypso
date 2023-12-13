a6e1ca8a8599d5a0e5b3dd2ff9fcaa5aa53af70e
rafaelgallani <galani.rafael@gmail.com>
2023-12-13 00:08:35 -0300

    Change `isSubdomain()` logic

--- client/lib/domains/is-subdomain.js
+++ client/lib/domains/is-subdomain.js
@@ -1,8 +1,13 @@
+import { getRootDomain } from 'calypso/lib/domains/utils';
+
 export function isSubdomain( domainName ) {
-	return (
+	domainName = domainName.startsWith( 'www.' ) ? domainName.slice( 4 ) : domainName;
+	const isValidSubdomain = Boolean(
 		domainName &&
-		domainName.match(
-			/^([a-z0-9_]([a-z0-9\-_]*[a-z0-9_])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\.[a-z]{2,63}$/
-		)
+			domainName.match(
+				/^([a-z0-9_]([a-z0-9\-_]*[a-z0-9_])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\.[a-z]{2,63}$/
+			)
 	);
+
+	return isValidSubdomain && getRootDomain( domainName ) !== domainName;
 }
