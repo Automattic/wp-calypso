043d3ef70e1103bf706c9d44fcfa069c332c7411
Douglas <douglas.henri@automattic.com>
2023-12-13 14:31:14 -0300

    remove localized string

--- packages/jetpack-ai-calypso/src/logo-generator/hooks/use-logo.ts
+++ packages/jetpack-ai-calypso/src/logo-generator/hooks/use-logo.ts
@@ -1,18 +1,10 @@
 /**
- * External dependencies
- */
-import { __, sprintf } from '@wordpress/i18n';
-/**
  * Types
  */
 import { UseLogoProps } from '../../types';
 
 const useLogo = ( { subject = 'World' }: UseLogoProps ) => {
-	const message: string = sprintf(
-		/* translators: %s is the subject */
-		__( 'Hello %s', 'jetpack-ai-calypso' ),
-		subject
-	);
+	const message: string = `Hello ${ subject }`;
 
 	return {
 		message,
