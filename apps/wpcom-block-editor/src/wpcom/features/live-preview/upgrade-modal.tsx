5913a12074862b1fc9674733b54da1cf2051d554
okmttdhr <okmttdhr@users.noreply.github.com>
2023-12-13 12:50:23 +0900

    Override the tooltip

--- apps/wpcom-block-editor/src/wpcom/features/live-preview/upgrade-modal.tsx
+++ apps/wpcom-block-editor/src/wpcom/features/live-preview/upgrade-modal.tsx
@@ -32,42 +32,92 @@ export const LivePreviewUpgradeModal: FC< { themeId: string; upgradePlan: () => 
 	 * For more context, see the discussion on adding an official customization method: https://github.com/WordPress/gutenberg/pull/56807.
 	 */
 	useEffect( () => {
-		const handler: EventListener = ( e ) => {
+		const saveButtonClickHandler: EventListener = ( e ) => {
 			e.stopPropagation();
 			setIsThemeUpgradeModalOpen( true );
 		};
-
 		const overrideSaveButtonClick = ( selector: string ) => {
 			const button = document.querySelector( selector );
 			if ( button ) {
 				button.textContent = __( 'Upgrade now', 'wpcom-live-preview' );
-				button.addEventListener( 'click', handler );
+				button.addEventListener( 'click', saveButtonClickHandler );
 			}
 		};
+
+		/**
+		 * This overrides the tooltip text for the save button.
+		 *
+		 * The tooltip is shown after a delay.
+		 * So we observe the DOM changes to avoid being fragile to the delay.
+		 * The observer is activated only when the user hovers over the button to optimize performance.
+		 */
+		const observer = new MutationObserver( ( mutations ) => {
+			mutations.forEach( ( mutation ) => {
+				mutation.addedNodes.forEach( ( node ) => {
+					if ( node.nodeType === Node.ELEMENT_NODE ) {
+						const tooltip = ( node as Element ).querySelector( '.components-tooltip' );
+						if ( tooltip ) {
+							tooltip.textContent = __( 'Upgrade now ⌘S', 'wpcom-live-preview' );
+						}
+					}
+				} );
+			} );
+		} );
+		const startObserver = () => {
+			observer.observe( document.body, { childList: true } );
+		};
+		const stopObserver = () => {
+			observer.disconnect();
+		};
+		const overrideSaveButtonHover = ( selector: string ) => {
+			const button = document.querySelector( selector );
+			if ( button ) {
+				button.addEventListener( 'mouseover', startObserver );
+				button.addEventListener( 'mouseout', stopObserver );
+			}
+		};
+
 		if ( canvasMode === 'view' ) {
 			overrideSaveButtonClick( SAVE_HUB_SAVE_BUTTON_SELECTOR );
+			overrideSaveButtonHover( SAVE_HUB_SAVE_BUTTON_SELECTOR );
 			return;
 		}
 		if ( canvasMode === 'edit' ) {
 			overrideSaveButtonClick( HEADER_SAVE_BUTTON_SELECTOR );
+			overrideSaveButtonHover( HEADER_SAVE_BUTTON_SELECTOR );
 			return;
 		}
 
-		// Override the keyboard shortcut (⌘S) for saving.
-		document.addEventListener( 'keydown', function ( event ) {
-			if ( event.key === 's' && ( event.metaKey || event.ctrlKey ) ) {
-				event.preventDefault();
-				handler( event );
+		// This overrides the keyboard shortcut (⌘S) for saving.
+		const overrideSaveButtonKeyboardShortcut = ( e: KeyboardEvent ) => {
+			if ( e.key === 's' && ( e.metaKey || e.ctrlKey ) ) {
+				e.preventDefault();
+				e.stopPropagation();
+				setIsThemeUpgradeModalOpen( true );
 			}
-		} );
+		};
+		document.addEventListener( 'keydown', overrideSaveButtonKeyboardShortcut );
 
 		return () => {
 			document
 				.querySelector( SAVE_HUB_SAVE_BUTTON_SELECTOR )
-				?.removeEventListener( 'click', handler );
+				?.removeEventListener( 'click', saveButtonClickHandler );
 			document
 				.querySelector( HEADER_SAVE_BUTTON_SELECTOR )
-				?.removeEventListener( 'click', handler );
+				?.removeEventListener( 'click', saveButtonClickHandler );
+			document
+				.querySelector( SAVE_HUB_SAVE_BUTTON_SELECTOR )
+				?.removeEventListener( 'mouseover', startObserver );
+			document
+				.querySelector( HEADER_SAVE_BUTTON_SELECTOR )
+				?.removeEventListener( 'mouseover', startObserver );
+			document
+				.querySelector( SAVE_HUB_SAVE_BUTTON_SELECTOR )
+				?.removeEventListener( 'mouseout', stopObserver );
+			document
+				.querySelector( HEADER_SAVE_BUTTON_SELECTOR )
+				?.removeEventListener( 'mouseout', stopObserver );
+			document.removeEventListener( 'keydown', overrideSaveButtonKeyboardShortcut );
 		};
 	}, [ canvasMode ] );
 
