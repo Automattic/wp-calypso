ca67f99eb48848314c1af7f0d8c4c9f134d7f4d2
tbradsha <32492176+tbradsha@users.noreply.github.com>
2023-12-13 10:55:37 -0700

    Use new component

--- client/jetpack-cloud/sections/overview/primary/intro-video/index.tsx
+++ client/jetpack-cloud/sections/overview/primary/intro-video/index.tsx
@@ -1,18 +1,12 @@
 import { useTranslate } from 'i18n-calypso';
-import styles from './style.module.css';
+import VideoPressIframe from 'calypso/components/jetpack/videopress-iframe';
 
 export default function IntroVideo() {
 	const translate = useTranslate();
 
-	return (
-		<iframe
-			className={ styles[ 'intro-video' ] }
-			title={ translate( 'Jetpack Manage Overview' ) }
-			aria-label={ translate( 'Jetpack Manage Overview' ) }
-			src={
-				'https://video.wordpress.com/embed/Z9piKY9s?hd=1&amp;autoPlay=0&amp;permalink=1&amp;loop=0&amp;preloadContent=metadata&amp;muted=0&amp;playsinline=0&amp;controls=1&amp;cover=1%27'
-			}
-			allowFullScreen
-		></iframe>
-	);
+	const videoURL =
+		'https://video.wordpress.com/embed/Z9piKY9s?hd=1&amp;autoPlay=0&amp;permalink=1&amp;loop=0&amp;preloadContent=metadata&amp;muted=0&amp;playsinline=0&amp;controls=1&amp;cover=1%27';
+	const title = translate( 'Jetpack Manage Overview' );
+
+	return <VideoPressIframe videoURL={ videoURL } title={ title } />;
 }
