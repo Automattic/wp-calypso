4afb7298f2a9c99bd7ab081f9c7065a7bef99613
Antony Agrios <antonyagrios@gmail.com>
2023-12-13 13:25:56 +0200

    Update: fix builds

--- client/state/staging-site/selectors/index.ts
+++ client/state/staging-site/selectors/index.ts
@@ -5,3 +5,4 @@ import 'calypso/state/staging-site/init';
  */
 export { getStagingSiteInfo } from 'calypso/state/staging-site/selectors/get-staging-site-info';
 export { getStagingSiteStatus } from 'calypso/state/staging-site/selectors/get-staging-site-status';
+export { getIsStagingSiteStatusComplete } from 'calypso/state/staging-site/selectors/get-is-staging-site-status-complete';
