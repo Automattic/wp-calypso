6d1b22dcf1b7f0d7985abf476152bf792f176f6f
Wesley Berrêdo <Wberredo@gmail.com>
2023-12-13 07:38:25 -0400

    Marketplace Reviews: Update list layout (#85167)

--- client/my-sites/marketplace/components/reviews-list/index.tsx
+++ client/my-sites/marketplace/components/reviews-list/index.tsx
@@ -18,6 +18,9 @@ export const MarketplaceReviewsList = forwardRef<
 	const translate = useTranslate();
 	const { data: reviews } = useMarketplaceReviewsQuery( props );
 
+	// TODO: Get the proper value as a URL to the profile picture
+	const authorProfilePic = null;
+
 	if ( ! isEnabled( 'marketplace-reviews-show' ) ) {
 		return null;
 	}
@@ -29,10 +32,23 @@ export const MarketplaceReviewsList = forwardRef<
 		return null;
 	}
 
+	if ( Array.isArray( reviews ) && reviews?.length === 0 ) {
+		return (
+			<div className="marketplace-reviews-list__no-reviews">
+				<h2 className="marketplace-reviews-list__no-reviews-title">
+					{ translate( 'No reviews yet' ) }
+				</h2>
+				<h3 className="marketplace-reviews-list__no-reviews-subtitle">
+					{ translate(
+						'There are no reviews for this plugin at the moment. Your feedback could be the first to guide others.'
+					) }
+				</h3>
+			</div>
+		);
+	}
+
 	return (
 		<div className="marketplace-reviews-list__container" ref={ ref }>
-			<h2 className="marketplace-reviews-list__title">{ translate( 'Customer reviews' ) }</h2>
-
 			<div className="marketplace-reviews-list__customer-reviews">
 				{ Array.isArray( reviews ) &&
 					reviews.map( ( review: MarketplaceReviewResponse ) => (
@@ -40,22 +56,40 @@ export const MarketplaceReviewsList = forwardRef<
 							className="marketplace-reviews-list__review-container"
 							key={ `review-${ review.id }` }
 						>
-							<div className="marketplace-reviews-list__author">{ review.author_name }</div>
-							<div className="marketplace-reviews-list__rating-data">
-								<Rating rating={ review.meta.wpcom_marketplace_rating * 20 } />
+							<div className="marketplace-reviews-list__review-container-header">
+								<div className="marketplace-reviews-list__profile-picture">
+									{ authorProfilePic ? (
+										<img
+											className="marketplace-reviews-list__profile-picture-img"
+											src={ authorProfilePic }
+											alt={ translate( "%(reviewer)s's profile picture", {
+												comment: 'Alt description for the profile picture of a reviewer',
+												args: { reviewer: review.author_name },
+											} ).toString() }
+										/>
+									) : (
+										<div className="marketplace-reviews-list__profile-picture-placeholder" />
+									) }
+								</div>
 
-								<div
-									// sanitized with sanitizeSectionContent
-									// eslint-disable-next-line react/no-danger
-									dangerouslySetInnerHTML={ {
-										__html: sanitizeSectionContent( review.content.rendered ),
-									} }
-									className="marketplace-reviews-list__content"
-								></div>
+								<div className="marketplace-reviews-list__rating-data">
+									<div className="marketplace-reviews-list__author">{ review.author_name }</div>
+
+									<Rating rating={ review.meta.wpcom_marketplace_rating * 20 } />
+								</div>
+								<div className="marketplace-reviews-list__date">
+									{ moment( review.date ).format( 'll' ) }
+								</div>
 							</div>
-							<div className="marketplace-reviews-list__date">
-								{ moment( review.date ).format( 'll' ) }
-							</div>
+
+							<div
+								// sanitized with sanitizeSectionContent
+								// eslint-disable-next-line react/no-danger
+								dangerouslySetInnerHTML={ {
+									__html: sanitizeSectionContent( review.content.rendered ),
+								} }
+								className="marketplace-reviews-list__content"
+							></div>
 						</div>
 					) ) }
 			</div>
