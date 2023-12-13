57e760de610f9c03a61a2b7f12d7b05404f8a22b
Christian Gastrell <cgastrell@gmail.com>
2023-12-13 14:10:10 -0300

    test single component solution for AI logo generator modal

--- packages/jetpack-ai-calypso/src/logo-generator/components/generator-modal.tsx
+++ packages/jetpack-ai-calypso/src/logo-generator/components/generator-modal.tsx
@@ -1,6 +1,8 @@
 /**
  * External dependencies
  */
+import { CompactCard, Gridicon } from '@automattic/components';
+import { Modal } from '@wordpress/components';
 import React from 'react';
 /**
  * Internal dependencies
@@ -9,7 +11,29 @@ import useLogo from '../hooks/use-logo';
 import './generator-modal.scss';
 
 export const GeneratorModal: React.FC = () => {
-	const { message } = useLogo( {} );
+	const [ modalIsOpen, setModalIsOpen ] = React.useState( false );
+	// const { message } = useLogo( {} );
 
-	return <div className="message">{ message }</div>;
+	/**
+	 * TODO:
+	 *  - click handler should be exposed so it can be tampered with from the outside (aka my-site/customer-home)
+	 *  - label same as above
+	 *  - classes for action box are taken as-is from the original component, not sure how to solve that (keep in sync) since actionbox is not exported
+	 */
+	return (
+		<CompactCard onClick={ () => setModalIsOpen( true ) } className="quick-links__action-box">
+			<div className="quick-links__action-box-image" aria-hidden="true">
+				<Gridicon className="quick-links__action-box-icon" icon="types" />
+			</div>
+			<div className="quick-links__action-box-text">
+				<span className="quick-links__action-box-label">Create a logo for your site with AI</span>
+			</div>
+			{ modalIsOpen && (
+				<Modal onRequestClose={ () => setModalIsOpen( false ) } className="launched__modal">
+					Some content
+				</Modal>
+			) }
+		</CompactCard>
+	);
+	// return <div className="message">{ message }</div>;
 };
