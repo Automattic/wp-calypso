255ccf5fb6cc1c3d0e8843a026283119a20a7208
James Kenneth Guidaven <jkguidaven@gmail.com>
2023-12-14 16:37:54 +0800

    Get rid entirely with useURLQueryParams hook.

--- client/jetpack-cloud/sections/partner-portal/issue-license-v2/hooks/use-product-bundle-size.ts
+++ client/jetpack-cloud/sections/partner-portal/issue-license-v2/hooks/use-product-bundle-size.ts
@@ -1,8 +1,7 @@
-import { addQueryArgs, getQueryArgs } from '@wordpress/url';
+import { addQueryArgs, getQueryArgs, removeQueryArgs } from '@wordpress/url';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import useProductsQuery from 'calypso/state/partner-portal/licenses/hooks/use-products-query';
 import { APIProductFamilyProduct } from 'calypso/state/partner-portal/types';
-import { useURLQueryParams } from '../../hooks';
 
 const BUNDLE_SIZE_PARAM_KEY = 'bundle_size';
 
@@ -30,39 +29,33 @@ export function useProductBundleSize() {
 
 	const supportedBundleSizes = getSupportedBundleSizes( products );
 
-	const { resetParams, getParamValue } = useURLQueryParams();
-
 	const [ selectedSize, setSelectedSize ] = useState< number | undefined >( undefined );
 
 	// When products are changed, we need to reevaluate if selected bundle size is still valid
 	useEffect( () => {
-		setSelectedSize(
-			parseLocationHash( supportedBundleSizes, getParamValue( BUNDLE_SIZE_PARAM_KEY ) )
-		);
-	}, [ getParamValue, supportedBundleSizes ] );
+		const { [ BUNDLE_SIZE_PARAM_KEY ]: bundleSize } = getQueryArgs( window.location.href );
+		setSelectedSize( parseLocationHash( supportedBundleSizes, bundleSize?.toString() ) );
+	}, [ supportedBundleSizes ] );
 
 	const setSelectedSizeAndLocationHash = useCallback(
 		( size: number ) => {
-			if ( size === 1 ) {
-				resetParams( [ BUNDLE_SIZE_PARAM_KEY ] );
-			} else {
-				/* Ideally we would like to use setParams from useURLQueryParams hook but
-				 * for a reason it causes the Page context to throw exception when
-				 * popping the history stack (browser back button). This implementation is
-				 * a workaround for that issue.
-				 */
-				window.history.pushState(
-					null,
-					'',
-					addQueryArgs( '', {
-						...getQueryArgs( window.location.href ),
-						[ BUNDLE_SIZE_PARAM_KEY ]: `${ size }`,
-					} )
-				);
+			if ( size === selectedSize ) {
+				return;
 			}
+
+			const queryArgs =
+				size === 1
+					? removeQueryArgs( window.location.href, BUNDLE_SIZE_PARAM_KEY )
+					: addQueryArgs( window.location.href, {
+							...getQueryArgs( window.location.href ),
+							[ BUNDLE_SIZE_PARAM_KEY ]: `${ size }`,
+					  } );
+
+			window.history.pushState( null, '', queryArgs );
+
 			setSelectedSize( size );
 		},
-		[ resetParams ]
+		[ selectedSize ]
 	);
 
 	return useMemo( () => {
