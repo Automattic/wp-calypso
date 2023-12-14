1b374b4fb41fa76606de863901ce059bef644a47
okmttdhr <okmttdhr@users.noreply.github.com>
2023-12-14 16:24:12 +0900

    Add a comment about build settings change

--- apps/wpcom-block-editor/webpack.config.js
+++ apps/wpcom-block-editor/webpack.config.js
@@ -59,6 +59,10 @@ function getWebpackConfig(
 			...webpackConfig.plugins.filter(
 				( plugin ) => plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
 			),
+			/**
+			 * This is needed for import-ing ThemeUpgradeModal,
+			 * which is directly due to the use of NODE_DEBUG in the package called `util`.
+			 */
 			new webpack.DefinePlugin( {
 				'process.env.NODE_DEBUG': JSON.stringify( process.env.NODE_DEBUG || false ),
 			} ),
