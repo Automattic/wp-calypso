ba366860a5f335a23eb13cb54f61ba28d6d1eee4
Payton Swick <payton@foolord.com>
2023-12-14 17:01:01 -0500

    Show contact step when logged-out

--- client/my-sites/checkout/src/lib/get-contact-details-type.ts
+++ client/my-sites/checkout/src/lib/get-contact-details-type.ts
@@ -3,13 +3,13 @@ import {
 	isGoogleWorkspaceProductSlug,
 	isGSuiteProductSlug,
 } from '@automattic/calypso-products';
+import { doesPurchaseHaveFullCredits, type ContactDetailsType } from '@automattic/wpcom-checkout';
 import {
 	hasDomainRegistration,
 	hasTransferProduct,
 	hasOnlyRenewalItems,
 } from 'calypso/lib/cart-values/cart-items';
 import type { ResponseCart } from '@automattic/shopping-cart';
-import type { ContactDetailsType } from '@automattic/wpcom-checkout';
 
 export default function getContactDetailsType( responseCart: ResponseCart ): ContactDetailsType {
 	const hasDomainProduct =
@@ -34,6 +34,27 @@ export default function getContactDetailsType( responseCart: ResponseCart ): Con
 
 	if ( hasNewGSuite && ! hasOnlyRenewals ) {
 		return 'gsuite';
+	}
+
+	const isPurchaseFree = responseCart.total_cost_integer === 0;
+	const isFullCredits = doesPurchaseHaveFullCredits( responseCart );
+	const doesCartContainOnlyOneTimePurchases = responseCart.products.every(
+		( product ) => product.is_one_time_purchase
+	);
+	const isCartForSite = responseCart.cart_key !== 'no-user' && responseCart.cart_key !== 'no-site';
+
+	// Hide contact step entirely for free purchases (that are not free because
+	// of credits) for carts that contain only one-time purchases. In that
+	// situation, there's no need to collect tax information since we will not
+	// be charging the user now or ever. However, we must still show the
+	// contact step for logged-out carts because in those cases we need to
+	// collect an email address that is part of the tax form.
+	//
+	// The backend has similar conditions which hide the credit card payment
+	// method (for which we need tax info from the contact step) if the cart
+	// has only one-time purchases.
+	if ( isPurchaseFree && doesCartContainOnlyOneTimePurchases && ! isFullCredits && isCartForSite ) {
+		return 'none';
 	}
 
 	return 'tax';
