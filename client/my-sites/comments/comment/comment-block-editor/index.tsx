693298560fb788ca9d12bdf84fdc2f60562ad1f4
Omar Alshaker <omar@omaralshaker.com>
2023-12-19 17:38:46 +0100

    Fix CSS

--- client/my-sites/comments/comment/comment-block-editor/index.tsx
+++ client/my-sites/comments/comment/comment-block-editor/index.tsx
@@ -1,14 +1,15 @@
 import { serialize, type BlockInstance, parse } from '@wordpress/blocks';
+import { useEffect } from 'react';
+import { useSelector } from 'calypso/state';
+import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { addApiMiddleware } from './api';
 import { Editor } from './editor';
 import { loadBlocksWithCustomizations } from './load-blocks';
 import { loadTextFormatting } from './load-text-formatting';
-
 import './editor-style.scss';
 
 loadBlocksWithCustomizations();
 loadTextFormatting();
-// addApiMiddleware();
 
 export const CommentBlockEditor = ( {
 	onChange,
@@ -17,11 +18,21 @@ export const CommentBlockEditor = ( {
 	onChange: ( comment: string ) => void;
 	commentContent: string;
 } ) => {
+	const siteId = useSelector( getSelectedSiteId );
+
+	useEffect( () => {
+		if ( siteId ) {
+			addApiMiddleware( siteId );
+		}
+	}, [ siteId ] );
+
 	return (
-		<div className="editor__wrapper">
+		<div className="editor__wrapper ">
 			<Editor
 				initialContent={ commentContent ? parse( commentContent ) : [] }
-				saveContent={ ( content: BlockInstance[] ) => onChange( serialize( content ) ) }
+				saveContent={ ( content: BlockInstance[] ) =>
+					onChange( { target: { value: serialize( content ) } } )
+				}
 			/>
 		</div>
 	);
