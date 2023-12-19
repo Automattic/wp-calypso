c567e4f14cfa8eda6d89b5e94313a376dc27f46e
Douglas <douglas.henri@automattic.com>
2023-12-19 18:13:56 -0300

    control generator modal with props

--- packages/jetpack-ai-calypso/src/logo-generator/components/generator-modal.tsx
+++ packages/jetpack-ai-calypso/src/logo-generator/components/generator-modal.tsx
@@ -1,8 +1,8 @@
 /**
  * External dependencies
  */
-import { CompactCard, Gridicon } from '@automattic/components';
 import { Modal } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 import React from 'react';
 /**
  * Internal dependencies
@@ -10,30 +10,26 @@ import React from 'react';
 import useLogo from '../hooks/use-logo';
 import './generator-modal.scss';
 
-export const GeneratorModal: React.FC = () => {
-	const [ modalIsOpen, setModalIsOpen ] = React.useState( false );
+export const GeneratorModal: React.FC< { isOpen: boolean; onClose: () => void } > = ( {
+	isOpen,
+	onClose,
+} ) => {
 	const { message } = useLogo( {} );
 
-	/**
-	 * TODO:
-	 *  - click handler should be exposed so it can be tampered with from the outside (aka my-site/customer-home)
-	 *  - label same as above
-	 *  - classes for action box are taken as-is from the original component, not sure how to solve that (keep in sync) since actionbox is not exported
-	 */
 	return (
-		<CompactCard onClick={ () => setModalIsOpen( true ) } className="quick-links__action-box">
-			<div className="quick-links__action-box-image" aria-hidden="true">
-				<Gridicon className="quick-links__action-box-icon" icon="types" />
-			</div>
-			<div className="quick-links__action-box-text">
-				<span className="quick-links__action-box-label">Create a logo for your site with AI</span>
-			</div>
-			{ modalIsOpen && (
-				<Modal onRequestClose={ () => setModalIsOpen( false ) } className="launched__modal">
-					{ message }
+		<>
+			{ isOpen && (
+				<Modal
+					className="jetpack-ai-logo-generator-modal"
+					onRequestClose={ onClose }
+					shouldCloseOnClickOutside={ false }
+					shouldCloseOnEsc={ false }
+					title={ __( 'Jetpack AI Logo Generator', 'jetpack' ) }
+					size="large"
+				>
+					<div className="jetpack-ai-logo-generator-modal__body">{ message }</div>
 				</Modal>
 			) }
-		</CompactCard>
+		</>
 	);
-	// return <div className="message">{ message }</div>;
 };
