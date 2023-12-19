04b2c97a2d4ec69d8fc94b5be170060e61905a6b
rafaelgallani <galani.rafael@gmail.com>
2023-12-19 14:20:33 -0300

    Remove unnecessary empty string check

--- client/lib/domains/is-subdomain.ts
+++ client/lib/domains/is-subdomain.ts
@@ -6,10 +6,9 @@ export function isSubdomain( domainName: string ) {
 	}
 	domainName = domainName.startsWith( 'www.' ) ? domainName.slice( 4 ) : domainName;
 	const isValidSubdomain = Boolean(
-		domainName &&
-			domainName.match(
-				/^([a-z0-9_]([a-z0-9\-_]*[a-z0-9_])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\.[a-z]{2,63}$/
-			)
+		domainName.match(
+			/^([a-z0-9_]([a-z0-9\-_]*[a-z0-9_])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\.[a-z]{2,63}$/
+		)
 	);
 
 	return isValidSubdomain && getRootDomain( domainName ) !== domainName;
