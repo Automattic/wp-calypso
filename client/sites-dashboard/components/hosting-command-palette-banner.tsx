999c14f5bdaf913e22ae2b0ab8a6540e4dc77ae8
sejas <antonio@sejas.es>
2023-12-20 19:18:14 +0000

    Add animation

--- client/sites-dashboard/components/hosting-command-palette-banner.tsx
+++ client/sites-dashboard/components/hosting-command-palette-banner.tsx
@@ -1,9 +1,13 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 import { __ } from '@wordpress/i18n';
+import classNames from 'classnames';
+import { useEffect, useMemo, useState } from 'react';
 import DismissibleCard from 'calypso/blocks/dismissible-card';
+import { useCommandsArrayWpcom } from './wpcom-smp-commands';
 
 const Root = styled.div( {
-	marginBottom: 20,
+	marginBottom: 25,
 	'.hosting-command-palette-banner': {
 		display: 'flex',
 		alignItems: 'center',
@@ -18,6 +22,75 @@ const Root = styled.div( {
 	'.dismissible-card__close-button': {
 		top: 20,
 		right: 20,
+	},
+	'.card.is-card-link:not(a):active, .card.is-card-link:not(a):focus, .card.is-card-link:not(a):hover':
+		{
+			color: 'var(--color-primary)',
+		},
+} );
+
+const fadeIn = keyframes`
+  0% {
+    transform: translateX(15%);
+    opacity: 0;
+  }
+  50% {
+	transform: translateX(-1%);
+	opacity: 1;
+  }
+  100% {
+    transform: translateX(0);
+  }
+`;
+
+const fadeOut = keyframes`
+  0% {
+    transform: translateX(0%);
+	opacity: 1;
+  }
+  50% {
+    opacity: 0;
+	transform: translateX(-15%);
+  }
+  100% {
+	transform: translateX(-50%);
+  }
+`;
+
+const CommandBoxWrapper = styled.div( {
+	marginLeft: 'auto',
+	display: 'grid',
+	gridTemplateColumns: '1fr',
+	flexGrow: 1,
+	paddingRight: 44,
+} );
+
+const CommandBox = styled.div( {
+	gridRowStart: 1,
+	gridColumnStart: 1,
+	border: '1px solid var(--studio-gray-5)',
+	borderRadius: 2,
+	display: 'flex',
+	justifyContent: 'flex-start',
+	alignItems: 'center',
+	opacity: 0,
+	transition: 'opacity 1s ease-in-out',
+	marginLeft: 'auto',
+	minWidth: 253,
+	fontSize: 13,
+	lineHeight: '20px',
+	letterSpacing: -0.15,
+	'&.command-box__fadeIn': {
+		animation: `${ fadeIn } 1s ease-in-out`,
+		opacity: 1,
+	},
+	'&.command-box__fadeOut': {
+		animation: `${ fadeOut } 1s ease-in-out`,
+	},
+	svg: {
+		width: 24,
+		height: 24,
+		margin: 8,
 	},
 } );
 
@@ -67,6 +140,73 @@ const ShortcutIcon = ( { size }: ShortcutIconProps ) => {
 	return <StyledShortcut size={ size }>{ shortcut }</StyledShortcut>;
 };
 
+const AnimatedCommand = () => {
+	const commands = useCommandsArrayWpcom( {
+		setSelectedCommandName: () => {},
+	} );
+	const prefersReducedMotion = false;
+	const [ currentCommandIndex, setCurrentCommandIndex ] = useState( 0 );
+	const commandNames = useMemo( () => {
+		if ( prefersReducedMotion ) {
+			return [];
+		}
+		return [
+			'registerDomain',
+			'openReader',
+			'addNewPost',
+			'viewMySites',
+			'openHostingConfiguration',
+			'changePHPVersion',
+			'manageStagingSites',
+		].map( ( commandName ) => {
+			const command = commands.find( ( command ) => command.name === commandName );
+			return {
+				icon: command?.icon,
+				label: command?.label,
+			};
+		} );
+	}, [ commands, prefersReducedMotion ] );
+
+	useEffect( () => {
+		if ( prefersReducedMotion ) {
+			return;
+		}
+
+		const interval = setInterval( () => {
+			setCurrentCommandIndex( ( prevIndex ) => ( prevIndex + 1 ) % commandNames.length );
+		}, 3000 );
+
+		return () => clearInterval( interval );
+	}, [ prefersReducedMotion, commandNames.length ] );
+
+	if ( prefersReducedMotion ) {
+		return null;
+	}
+	return (
+		<CommandBoxWrapper>
+			{ commandNames.map( ( commandName, index ) => {
+				const prevIndex = ( currentCommandIndex - 1 + commandNames.length ) % commandNames.length;
+				if ( currentCommandIndex !== index && prevIndex !== index ) {
+					return null;
+				}
+
+				return (
+					<CommandBox
+						className={ classNames( {
+							'command-box__fadeIn': currentCommandIndex === index,
+							'command-box__fadeOut': prevIndex === index,
+						} ) }
+						key={ index }
+					>
+						{ commandName?.icon }
+						{ commandName?.label }
+					</CommandBox>
+				);
+			} ) }
+		</CommandBoxWrapper>
+	);
+};
+
 export function HostingCommandPaletteBanner( { className }: HostingCommandPaletteBannerProps ) {
 	return (
 		<Root>
@@ -82,6 +222,7 @@ export function HostingCommandPaletteBanner( { className }: HostingCommandPalett
 						<ShortcutIcon size="small" /> <span>{ __( 'to launch the Command Palette.' ) }</span>
 					</BannerDescription>
 				</div>
+				<AnimatedCommand />
 			</DismissibleCard>
 		</Root>
 	);
