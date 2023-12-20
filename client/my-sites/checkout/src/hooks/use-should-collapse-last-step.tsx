e4aaa9f72e2f3d3810bdbf95bc975909e1aa0012
Chris Diehl <jjchris.diehl@gmail.com>
2023-12-20 11:07:22 -0800

    Checkout: Update hook to deploy 'Collapse Last Step' treatment (#85516)
    * Update hook to deploy treatment
    
    * Update calls to useShouldCollapseLastStep
    
    * Add payment-method-step to test mock
    
    * Remove loading check for experiment
    
    ---------
    
    Co-authored-by: Chris Diehl <jjchrisdiehl@gmail.com>

--- client/my-sites/checkout/src/hooks/use-should-collapse-last-step.tsx
+++ client/my-sites/checkout/src/hooks/use-should-collapse-last-step.tsx
@@ -1,12 +1,11 @@
 import isAkismetCheckout from 'calypso/lib/akismet/is-akismet-checkout';
-import { useExperiment } from 'calypso/lib/explat';
 import isJetpackCheckout from 'calypso/lib/jetpack/is-jetpack-checkout';
 import { useSelector } from 'calypso/state';
 import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
 import { isJetpackSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 
-export function useShouldCollapseLastStep(): 'loading' | 'collapse' | 'no-collapse' {
+export function useShouldCollapseLastStep(): boolean {
 	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
 
 	const isJetpackNotAtomic = useSelector(
@@ -14,17 +13,10 @@ export function useShouldCollapseLastStep(): 'loading' | 'collapse' | 'no-collap
 	);
 
 	const isWPcomCheckout = ! isJetpackCheckout() && ! isAkismetCheckout() && ! isJetpackNotAtomic;
-	const [ isLoadingExperimentAssignment, experimentAssignment ] = useExperiment(
-		'wp_web_checkout_collapse_payment_method_2',
-		{ isEligible: isWPcomCheckout }
-	);
 
-	if ( isLoadingExperimentAssignment ) {
-		return 'loading';
+	if ( isWPcomCheckout ) {
+		return true;
 	}
 
-	if ( experimentAssignment?.variationName === 'treatment' ) {
-		return 'collapse';
-	}
-	return 'no-collapse';
+	return false;
 }
