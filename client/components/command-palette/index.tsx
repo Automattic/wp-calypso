9cbf236a40292f2698f741f99c75d4eb0b760a40
sejas <antonio@sejas.es>
2023-12-21 12:35:19 +0000

    Add calypso_hosting_command_palette_not_found event

--- client/components/command-palette/index.tsx
+++ client/components/command-palette/index.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { Modal, TextHighlight, __experimentalHStack as HStack } from '@wordpress/components';
+import { useDebounce } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
 import { Icon, search as inputIcon, chevronLeft as backIcon } from '@wordpress/icons';
 import { cleanForSlug } from '@wordpress/url';
@@ -199,6 +200,40 @@ function CommandInput( {
 	);
 }
 
+interface NotFoundMessageProps {
+	selectedCommandName: string;
+	search: string;
+	emptyListNotice?: string;
+	currentRoute: string | null;
+}
+
+const NotFoundMessage = ( {
+	selectedCommandName,
+	search,
+	emptyListNotice,
+	currentRoute,
+}: NotFoundMessageProps ) => {
+	const dispatch = useDispatch();
+	const trackNotFoundDebounced = useDebounce( () => {
+		dispatch(
+			recordTracksEvent( 'calypso_hosting_command_palette_not_found', {
+				command: selectedCommandName,
+				current_route: currentRoute,
+				search_text: search,
+			} )
+		);
+	}, 600 );
+
+	useEffect( () => {
+		if ( search ) {
+			trackNotFoundDebounced();
+		}
+		return trackNotFoundDebounced.cancel;
+	}, [ search, trackNotFoundDebounced ] );
+
+	return <>{ emptyListNotice || __( 'No results found.' ) }</>;
+};
+
 const CommandPalette = () => {
 	const [ placeHolderOverride, setPlaceholderOverride ] = useState( '' );
 	const [ search, setSearch ] = useState( '' );
@@ -302,8 +337,6 @@ const CommandPalette = () => {
 		}
 	};
 
-	const isLoading = false;
-
 	return (
 		<Modal
 			className="commands-command-menu"
@@ -334,11 +367,14 @@ const CommandPalette = () => {
 						/>
 					</div>
 					<Command.List ref={ commandListRef }>
-						{ ! isLoading && (
-							<StyledCommandsEmpty>
-								{ emptyListNotice || __( 'No results found.' ) }
-							</StyledCommandsEmpty>
-						) }
+						<StyledCommandsEmpty>
+							<NotFoundMessage
+								selectedCommandName={ selectedCommandName }
+								search={ search }
+								emptyListNotice={ emptyListNotice }
+								currentRoute={ currentRoute }
+							/>
+						</StyledCommandsEmpty>
 						<CommandMenuGroup
 							search={ search }
 							close={ ( commandName, isExecuted ) => {
