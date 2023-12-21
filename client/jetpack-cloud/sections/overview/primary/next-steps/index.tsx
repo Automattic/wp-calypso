c8d3ce2ccb56ba102f7c79ae2fa1343268962444
tbradsha <32492176+tbradsha@users.noreply.github.com>
2023-12-21 14:55:12 -0700

    Add tracks

--- client/jetpack-cloud/sections/overview/primary/next-steps/index.tsx
+++ client/jetpack-cloud/sections/overview/primary/next-steps/index.tsx
@@ -1,46 +1,59 @@
 import { CircularProgressBar } from '@automattic/components';
 import { Checklist, type Task } from '@automattic/launchpad';
 import { useTranslate } from 'i18n-calypso';
+import { useDispatch } from 'calypso/state';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 
 import './style.scss';
 
 export default function NextSteps( { onDismiss = () => {} } ) {
+	const dispatch = useDispatch();
 	const translate = useTranslate();
+
+	const tracksPrefix = 'calypso_jetpack_manage_overview_next_steps';
 
 	const tasks: Task[] = [
 		{
 			calypso_path: '',
-			completed: false,
+			completed: true,
 			disabled: false,
-			actionDispatch: () => {},
-			id: 'jpmanage-overview-next-steps-get-familiar',
+			actionDispatch: () => {
+				dispatch( recordTracksEvent( tracksPrefix + '_get_familiar_click' ) );
+			},
+			id: 'get_familiar',
 			title: 'Get familiar with the sites management dashboard',
 			useCalypsoPath: true,
 		},
 		{
 			calypso_path: '',
-			completed: false,
+			completed: true,
 			disabled: false,
-			actionDispatch: () => {},
-			id: 'jpmanage-overview-next-steps-add-sites',
+			actionDispatch: () => {
+				dispatch( recordTracksEvent( tracksPrefix + '_add_sites_click' ) );
+			},
+			id: 'add_sites',
 			title: 'Learn how to add new sites',
 			useCalypsoPath: true,
 		},
 		{
 			calypso_path: '',
-			completed: false,
+			completed: true,
 			disabled: false,
-			actionDispatch: () => {},
-			id: 'jpmanage-overview-next-steps-bulk-editing',
+			actionDispatch: () => {
+				dispatch( recordTracksEvent( tracksPrefix + '_bulk_editing_click' ) );
+			},
+			id: 'bulk_editing',
 			title: 'Learn bulk editing and enabling downtime monitoring',
 			useCalypsoPath: true,
 		},
 		{
 			calypso_path: '',
-			completed: false,
+			completed: true,
 			disabled: false,
-			actionDispatch: () => {},
-			id: 'jpmanage-overview-next-steps-plugin_management',
+			actionDispatch: () => {
+				dispatch( recordTracksEvent( tracksPrefix + '_plugin_management_click' ) );
+			},
+			id: 'plugin_management',
 			title: 'Explore plugin management',
 			useCalypsoPath: true,
 		},
@@ -67,7 +80,13 @@ export default function NextSteps( { onDismiss = () => {} } ) {
 					{ translate(
 						"Right now there's nothing left for you to do. We'll let you know when anything needs your attention."
 					) }{ ' ' }
-					<button className="dismiss" onClick={ onDismiss }>
+					<button
+						className="dismiss"
+						onClick={ () => {
+							dispatch( recordTracksEvent( tracksPrefix + '_dismiss_click' ) );
+							onDismiss();
+						} }
+					>
 						{ translate( 'Hide' ) }
 					</button>
 				</p>
