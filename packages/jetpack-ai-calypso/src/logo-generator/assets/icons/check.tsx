e51856b15febcfe5b36c1b72d4593601884851c7
Douglas <douglas.henri@automattic.com>
2023-12-21 16:25:02 -0300

    fix svg properties

--- packages/jetpack-ai-calypso/src/logo-generator/assets/icons/check.tsx
+++ packages/jetpack-ai-calypso/src/logo-generator/assets/icons/check.tsx
@@ -9,8 +9,8 @@ export default ( { color = '#3858E9' } ) => {
 			className="jetpack-ai-logo-generator-icon"
 		>
 			<path
-				fill-rule="evenodd"
-				clip-rule="evenodd"
+				fillRule="evenodd"
+				clipRule="evenodd"
 				d="M17.9291 7.96836L10.7308 17.6492L6.2145 14.2911L7.10952 13.0873L10.4221 15.5504L16.7253 7.07333L17.9291 7.96836Z"
 				fill={ color }
 			/>
