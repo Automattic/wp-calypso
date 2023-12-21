c778920c667fd9b1f6194d942bb64b399168dfa2
cleacos <javacodia@gmail.com>
2023-12-21 12:39:35 +0000

    Remove todo task

--- client/jetpack-cloud/components/content-sidebar/index.tsx
+++ client/jetpack-cloud/components/content-sidebar/index.tsx
@@ -10,7 +10,6 @@ type Props = {
 
 const ContentSidebar = ( { mainContent, rightSidebar }: Props ) => {
 	return (
-		// todo: We have to add here the header for the mobile menu
 		<Main wideLayout className="content-sidebar">
 			<div className="content-sidebar__main-content">{ mainContent }</div>
 			<aside className="content-sidebar__right-sidebar">{ rightSidebar }</aside>
