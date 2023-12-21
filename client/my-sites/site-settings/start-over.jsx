9b5ae4182c3044dc387e5fce138a5a15f4a00384
mpkelly <kikemelly@gmail.com>
2023-12-21 09:49:50 +0700

    Self-service site reset: launch to prod

--- client/my-sites/site-settings/start-over.jsx
+++ client/my-sites/site-settings/start-over.jsx
@@ -1,11 +1,9 @@
-import { isEnabled } from '@automattic/calypso-config';
-import { Button, Gridicon } from '@automattic/components';
+import { Button } from '@automattic/components';
 import {
 	useSiteResetContentSummaryQuery,
 	useSiteResetMutation,
 	useSiteResetStatusQuery,
 } from '@automattic/data-stores';
-import { useLocalizeUrl } from '@automattic/i18n-utils';
 import { useQueryClient } from '@tanstack/react-query';
 import { createInterpolateElement, useState } from '@wordpress/element';
 import { sprintf } from '@wordpress/i18n';
@@ -13,9 +11,7 @@ import { localize } from 'i18n-calypso';
 import { connect } from 'react-redux';
 import ActionPanel from 'calypso/components/action-panel';
 import ActionPanelBody from 'calypso/components/action-panel/body';
-import ActionPanelFigure from 'calypso/components/action-panel/figure';
 import ActionPanelFooter from 'calypso/components/action-panel/footer';
-import ActionPanelTitle from 'calypso/components/action-panel/title';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormTextInput from 'calypso/components/forms/form-text-input';
 import HeaderCake from 'calypso/components/header-cake';
@@ -25,87 +21,12 @@ import Main from 'calypso/components/main';
 import NavigationHeader from 'calypso/components/navigation-header';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
 import { EVERY_FIVE_SECONDS, Interval } from 'calypso/lib/interval';
-import { EMPTY_SITE } from 'calypso/lib/url/support';
 import { useDispatch, useSelector } from 'calypso/state';
 import { errorNotice, successNotice } from 'calypso/state/notices/actions';
 import isUnlaunchedSite from 'calypso/state/selectors/is-unlaunched-site';
 import { getSite, getSiteDomain, isJetpackSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import { BuiltByUpsell } from './built-by-upsell-banner';
-
-const StartOver = ( {
-	translate,
-	selectedSiteSlug,
-	siteDomain,
-	isAtomic,
-	site,
-	isUnlaunchedSite: isUnlaunchedSiteProp,
-} ) => {
-	const localizeUrl = useLocalizeUrl();
-	if ( isEnabled( 'settings/self-serve-site-reset' ) ) {
-		return (
-			<SiteResetCard
-				translate={ translate }
-				selectedSiteSlug={ selectedSiteSlug }
-				siteDomain={ siteDomain }
-				isAtomic={ isAtomic }
-				site={ site }
-				isUnlaunchedSite={ isUnlaunchedSiteProp }
-			/>
-		);
-	}
-	return (
-		<div
-			className="main main-column" // eslint-disable-line wpcalypso/jsx-classname-namespace
-			role="main"
-		>
-			<PageViewTracker path="/settings/start-over/:site" title="Settings > Start Over" />
-			<HeaderCake backHref={ '/settings/general/' + selectedSiteSlug }>
-				<h1>{ translate( 'Start Over' ) }</h1>
-			</HeaderCake>
-			<ActionPanel>
-				<ActionPanelBody>
-					<ActionPanelFigure inlineBodyText={ true }>
-						<img src="/calypso/images/wordpress/logo-stars.svg" alt="" width="170" height="143" />
-					</ActionPanelFigure>
-					<ActionPanelTitle>{ translate( 'Start Over' ) }</ActionPanelTitle>
-					<p>
-						{ translate(
-							"If you want a site but don't want any of the posts and pages you have now, our support " +
-								'team can delete your posts, pages, media, and comments for you.'
-						) }
-					</p>
-					<p>
-						{ translate(
-							'This will keep your site and URL active, but give you a fresh start on your content ' +
-								'creation. Just contact us to have your current content cleared out.'
-						) }
-					</p>
-					<p>
-						{ translate(
-							'Alternatively, you can delete all content from your site by following the steps here.'
-						) }
-					</p>
-				</ActionPanelBody>
-				<ActionPanelFooter>
-					<Button
-						className="action-panel__support-button is-external" // eslint-disable-line wpcalypso/jsx-classname-namespace
-						href={ localizeUrl( EMPTY_SITE ) }
-					>
-						{ translate( 'Follow the steps' ) }
-						<Gridicon icon="external" size={ 48 } />
-					</Button>
-					<Button
-						className="action-panel__support-button" // eslint-disable-line wpcalypso/jsx-classname-namespace
-						href="/help/contact"
-					>
-						{ translate( 'Contact support' ) }
-					</Button>
-				</ActionPanelFooter>
-			</ActionPanel>
-		</div>
-	);
-};
 
 function SiteResetCard( {
 	translate,
@@ -408,4 +329,4 @@ export default connect( ( state ) => {
 		isAtomic: isJetpackSite( state, siteId ),
 		isUnlaunchedSite: isUnlaunchedSite( state, siteId ),
 	};
-} )( localize( StartOver ) );
+} )( localize( SiteResetCard ) );
