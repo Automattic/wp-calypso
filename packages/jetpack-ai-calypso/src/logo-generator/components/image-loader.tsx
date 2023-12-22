3b5e4e37354f99e7181bb566cdc89e6da065fe6d
Douglas Henri <dhasilva@users.noreply.github.com>
2023-12-22 13:16:36 -0300

    AI Logo generator: Add initial components (#85670)
    * add image loader and first load screen
    
    * add classnames dependency
    
    * add assets
    
    * add logo presenter component
    
    * update generator modal UI
    
    * fix svg properties
    
    * reset demo timeout when modal is closed
    
    * refactor css files
    
    * update test

---
+++ packages/jetpack-ai-calypso/src/logo-generator/components/image-loader.tsx
@@ -0,0 +1,19 @@
+/**
+ * Internal dependencies
+ */
+import classnames from 'classnames';
+import loader from '../assets/images/loader.gif';
+/**
+ * Types
+ */
+import type React from 'react';
+
+export const ImageLoader: React.FC< { className?: string } > = ( { className = null } ) => {
+	return (
+		<img
+			src={ loader }
+			alt="Loading"
+			className={ classnames( 'jetpack-ai-logo-generator-modal__loader', className ) }
+		/>
+	);
+};
