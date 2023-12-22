f09be914b947f40b3b5c82b61d7a1e7bbc9c1ef2
Karen Attfield <karenlattfield@gmail.com>
2023-12-22 14:56:42 +0000

    Adding default values to const itemData, to help prevent a type error in tests

--- client/jetpack-cloud/sections/overview/primary/product-grid/product-item.tsx
+++ client/jetpack-cloud/sections/overview/primary/product-grid/product-item.tsx
@@ -9,14 +9,33 @@ import { SelectorProduct } from 'calypso/my-sites/plans/jetpack-plans/types';
 interface Props {
 	productData: ProductData;
 	productSlug: string;
+	tagline: string;
+	iconSlug: string;
+	displayName: string;
 	onMoreAboutClick: ( slug: string ) => void;
 }
 
-const ProductItem: React.FC< Props > = ( { productData, productSlug, onMoreAboutClick } ) => {
+const ProductItem: React.FC< Props > = ( {
+	productData,
+	productSlug,
+	tagline,
+	iconSlug,
+	displayName,
+	onMoreAboutClick,
+} ) => {
 	const itemData: SelectorProduct = {
 		moreAboutUrl: productData.url,
 		shortName: <>{ productData.name }</>,
 		productSlug: productSlug,
+		iconSlug: iconSlug,
+		type: 'item-type-plan',
+		displayName: displayName,
+		tagline: tagline,
+		description: undefined,
+		term: 'TERM_ANNUALLY',
+		features: {
+			items: [],
+		},
 	};
 
 	const displayDescription = (
