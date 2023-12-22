7b209253d9f6f9a3834a648a6c95e03e38d5f7ba
Yashwin <yashwinpoojary@gmail.com>
2023-12-22 12:43:16 +0530

    Add description for the useWPPlanName hook

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content/hooks/use-wp-plan-name.ts
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-status-content/hooks/use-wp-plan-name.ts
@@ -6,6 +6,14 @@ import {
 } from '@automattic/calypso-products';
 import { getPlan } from '@automattic/calypso-products/src';
 
+/**
+ * Custom hook to get the name of a WordPress.com plan.
+ *
+ * This hook iterates over an array of plan slugs and returns the name of the first plan that matches one of the WordPress.com plans.
+ * The matching is done by checking if the plan slug starts with the slug of a WordPress.com plan.
+ * @param {Array<string>} plans - An array of plan slugs.
+ * @returns {string} The name of the first matching WordPress.com plan, or an empty string if no matching plan is found.
+ */
 const PLANS = [ PLAN_BUSINESS, PLAN_ECOMMERCE, PLAN_PREMIUM, PLAN_PERSONAL ];
 
 const useWPPlanName = ( plans: Array< string > ) => {
