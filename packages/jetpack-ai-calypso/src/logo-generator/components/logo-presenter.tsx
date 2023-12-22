8c30becc260a102471d6c42a10fd269e61062c97
Douglas Henri <dhasilva@users.noreply.github.com>
2023-12-22 13:16:36 -0300

    AI Logo generator: Add initial components (#85670)
    * add image loader and first load screen
    
    * add classnames dependency
    
    * add assets
    
    * add logo presenter component
    
    * update generator modal UI
    
    * fix svg properties
    
    * reset demo timeout when modal is closed
    
    * refactor css files
    
    * update test

---
+++ packages/jetpack-ai-calypso/src/logo-generator/components/logo-presenter.tsx
@@ -0,0 +1,89 @@
+import { Icon } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import classnames from 'classnames';
+import { useState } from 'react';
+/**
+ * Internal dependencies
+ */
+import LogoIcon from '../assets/icons/logo';
+import MediaIcon from '../assets/icons/media';
+import sample from '../assets/images/sample-logo.png';
+import './logo-presenter.scss';
+/**
+ * Types
+ */
+import { ImageLoader } from './image-loader';
+import type React from 'react';
+
+const SaveInLibraryButton: React.FC = () => {
+	const [ saved, setSaved ] = useState( false );
+
+	return (
+		<button
+			className={ classnames( 'jetpack-ai-logo-generator-modal-presenter__action', {
+				clickable: ! saved,
+			} ) }
+			onClick={ () => {
+				if ( ! saved ) {
+					setSaved( true );
+				}
+			} }
+		>
+			<Icon icon={ <MediaIcon /> } />
+			{ saved ? (
+				<span className="action-text">{ __( 'Saved', 'jetpack' ) }</span>
+			) : (
+				<span className="action-text">{ __( 'Save in Library', 'jetpack' ) }</span>
+			) }
+		</button>
+	);
+};
+
+const UseOnSiteButton: React.FC = () => {
+	return (
+		<button
+			className={ classnames( 'jetpack-ai-logo-generator-modal-presenter__action', 'clickable' ) }
+		>
+			<Icon icon={ <LogoIcon /> } />
+			<span className="action-text">{ __( 'Use on Site', 'jetpack' ) }</span>
+		</button>
+	);
+};
+
+export const LogoPresenter: React.FC< { description?: string; loading?: boolean } > = ( {
+	description,
+	loading = false,
+} ) => {
+	return (
+		<div className="jetpack-ai-logo-generator-modal-presenter">
+			<div className="jetpack-ai-logo-generator-modal-presenter__container">
+				{ loading ? (
+					<>
+						<ImageLoader className="jetpack-ai-logo-generator-modal-presenter__logo" />
+						<span className="jetpack-ai-logo-generator-modal-presenter__loading-text">
+							{ __( 'Generating new logo…', 'jetpack' ) }
+						</span>
+					</>
+				) : (
+					<>
+						<img
+							src={ sample }
+							alt={ description }
+							className="jetpack-ai-logo-generator-modal-presenter__logo"
+						/>
+						<div className="jetpack-ai-logo-generator-modal-presenter__action-wrapper">
+							<span className="jetpack-ai-logo-generator-modal-presenter__description">
+								{ description }
+							</span>
+							<div className="jetpack-ai-logo-generator-modal-presenter__actions">
+								<SaveInLibraryButton />
+								<UseOnSiteButton />
+							</div>
+						</div>
+					</>
+				) }
+			</div>
+			<div className="jetpack-ai-logo-generator-modal-presenter__rectangle" />
+		</div>
+	);
+};
