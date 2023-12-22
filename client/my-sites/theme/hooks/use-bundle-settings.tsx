900e945c06585b0d0312cd0df0354b3b4337140c
Christos <chriskmnds@gmail.com>
2023-12-22 05:19:45 +0200

    Plans rename: remove hasTranslation calls from multiple places - 2 (#85632)

--- client/my-sites/theme/hooks/use-bundle-settings.tsx
+++ client/my-sites/theme/hooks/use-bundle-settings.tsx
@@ -1,7 +1,6 @@
 import { PLAN_BUSINESS, getPlan } from '@automattic/calypso-products';
-import { useIsEnglishLocale } from '@automattic/i18n-utils';
 import { ExternalLink } from '@wordpress/components';
-import i18n, { useTranslate, TranslateResult } from 'i18n-calypso';
+import { useTranslate, TranslateResult } from 'i18n-calypso';
 import { type FC, useMemo } from 'react';
 import { useSelector } from 'calypso/state';
 import { getThemeSoftwareSet } from 'calypso/state/themes/selectors';
@@ -40,7 +39,6 @@ const SenseiIcon = () => (
 
 export function useBundleSettings( themeSoftware?: string ): BundleSettingsHookReturn {
 	const translate = useTranslate();
-	const isEnglishLocale = useIsEnglishLocale();
 	const businessPlanName = getPlan( PLAN_BUSINESS )?.getTitle() || '';
 
 	const bundleSettings = useMemo( () => {
@@ -54,18 +52,10 @@ export function useBundleSettings( themeSoftware?: string ): BundleSettingsHookR
 					designPickerBadgeTooltip: translate(
 						'This theme comes bundled with WooCommerce, the best way to sell online.'
 					),
-					bannerUpsellDescription:
-						isEnglishLocale ||
-						i18n.hasTranslation(
-							'This theme comes bundled with the WooCommerce plugin. Upgrade to a %(businessPlanName)s plan to select this theme and unlock all its features.'
-						)
-							? ( translate(
-									'This theme comes bundled with the WooCommerce plugin. Upgrade to a %(businessPlanName)s plan to select this theme and unlock all its features.',
-									{ args: { businessPlanName } }
-							  ) as string )
-							: translate(
-									'This theme comes bundled with the WooCommerce plugin. Upgrade to a Business plan to select this theme and unlock all its features.'
-							  ),
+					bannerUpsellDescription: translate(
+						'This theme comes bundled with the WooCommerce plugin. Upgrade to a %(businessPlanName)s plan to select this theme and unlock all its features.',
+						{ args: { businessPlanName } }
+					) as string,
 					bundledPluginMessage: translate(
 						'This theme comes bundled with the {{link}}WooCommerce{{/link}} plugin.',
 						{
@@ -102,7 +92,7 @@ export function useBundleSettings( themeSoftware?: string ): BundleSettingsHookR
 			default:
 				return null;
 		}
-	}, [ themeSoftware, translate, isEnglishLocale, businessPlanName ] );
+	}, [ themeSoftware, translate, businessPlanName ] );
 
 	return bundleSettings;
 }
