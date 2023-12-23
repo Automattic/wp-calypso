ebdcb73977708c2c5496be061ac5e99ae2e95fa7
Tony Arcangelini <tony@arcangelini.com>
2023-12-23 17:45:46 +0100

    Fix API and dedupe

--- packages/comment-block-editor/src/components/api/index.ts
+++ packages/comment-block-editor/src/components/api/index.ts
@@ -1,27 +1,27 @@
 import apiFetch from '@wordpress/api-fetch';
-// import wpcomRequest from 'wpcom-proxy-request';
-// import defaultFetchHandler from './default';
+import wpcomRequest from 'wpcom-proxy-request';
+import defaultFetchHandler from './default';
 
 export function addApiMiddleware() {
-	// // Add a middleware to handle oEmbed requests.
-	// apiFetch.setFetchHandler( ( options ) => {
-	// 	const { path } = options;
+	// Add a middleware to handle oEmbed requests.
+	apiFetch.setFetchHandler( ( options ) => {
+		const { path } = options;
 
-	// 	if ( path?.startsWith( '/oembed/1.0/proxy' ) ) {
-	// 		const url = new URL( 'https://wordpress.com' + path );
-	// 		const embedUrl = url.searchParams.get( 'url' );
+		if ( path?.startsWith( '/oembed/1.0/proxy' ) ) {
+			const url = new URL( 'https://wordpress.com' + path );
+			const embedUrl = url.searchParams.get( 'url' );
 
-	// 		return wpcomRequest( {
-	// 			path: '/verbum/embed',
-	// 			query: `embed_url=${ embedUrl }&embed_nonce=${ encodeURIComponent(
-	// 				VerbumComments.embedNonce
-	// 			) }`,
-	// 			apiNamespace: 'wpcom/v2',
-	// 		} );
-	// 	}
+			return wpcomRequest( {
+				path: '/verbum/embed',
+				query: `embed_url=${ embedUrl }&embed_nonce=${ encodeURIComponent(
+					VerbumComments.embedNonce
+				) }`,
+				apiNamespace: 'wpcom/v2',
+			} );
+		}
 
-	// 	return defaultFetchHandler( options );
-	// } );
+		return defaultFetchHandler( options );
+	} );
 
 	// Add a middleware to handle calls to site that are not available.
 	apiFetch.use( ( options, next ): any => {
