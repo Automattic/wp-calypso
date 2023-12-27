f1ef10a05a30e7580ae8e5fbf5d5ea81033e45e1
Griffith Chen <griffith.chen@automattic.com>
2023-12-27 11:56:05 +0800

    Update how zoom works in the Assembler large preview

--- packages/components/src/device-switcher/icons.tsx
+++ packages/components/src/device-switcher/icons.tsx
@@ -1,4 +1,4 @@
-import { SVG, Rect } from '@wordpress/components';
+import { SVG, Path, Rect } from '@wordpress/components';
 
 export const computer = (
 	<SVG width="36" height="36" viewBox="0 0 36 36">
@@ -18,5 +18,14 @@ export const phone = (
 	<SVG width="24" height="24" viewBox="0 0 24 24">
 		<Rect x="6" y="3" width="12" height="18" rx="1.25" stroke="currentColor" strokeWidth="1.5" />
 		<Rect x="11" y="17" width="2" height="1.5" fill="currentColor" />
+	</SVG>
+);
+
+export const globe = (
+	<SVG width="24" height="24" viewBox="0 0 24 24">
+		<Path
+			d="M12 3.3c-4.8 0-8.8 3.9-8.8 8.8 0 4.8 3.9 8.8 8.8 8.8 4.8 0 8.8-3.9 8.8-8.8s-4-8.8-8.8-8.8zm6.5 5.5h-2.6C15.4 7.3 14.8 6 14 5c2 .6 3.6 2 4.5 3.8zm.7 3.2c0 .6-.1 1.2-.2 1.8h-2.9c.1-.6.1-1.2.1-1.8s-.1-1.2-.1-1.8H19c.2.6.2 1.2.2 1.8zM12 18.7c-1-.7-1.8-1.9-2.3-3.5h4.6c-.5 1.6-1.3 2.9-2.3 3.5zm-2.6-4.9c-.1-.6-.1-1.1-.1-1.8 0-.6.1-1.2.1-1.8h5.2c.1.6.1 1.1.1 1.8s-.1 1.2-.1 1.8H9.4zM4.8 12c0-.6.1-1.2.2-1.8h2.9c-.1.6-.1 1.2-.1 1.8 0 .6.1 1.2.1 1.8H5c-.2-.6-.2-1.2-.2-1.8zM12 5.3c1 .7 1.8 1.9 2.3 3.5H9.7c.5-1.6 1.3-2.9 2.3-3.5zM10 5c-.8 1-1.4 2.3-1.8 3.8H5.5C6.4 7 8 5.6 10 5zM5.5 15.3h2.6c.4 1.5 1 2.8 1.8 3.7-1.8-.6-3.5-2-4.4-3.7zM14 19c.8-1 1.4-2.2 1.8-3.7h2.6C17.6 17 16 18.4 14 19z"
+			fill="currentColor"
+		/>
 	</SVG>
 );
