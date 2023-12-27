f6a457052640758ff143c7567fa102992757a38e
Ashar Fuadi <ashar.fuadi@automattic.com>
2023-12-27 14:59:16 +0700

    Assembler: Replace activation page with confirmation page (#85410)
    * Assembler: remove activation page
    
    * Update copy
    
    * Update copy
    
    * Fix isNewSite logic
    
    * Update copy
    
    * Prevent content flashing when fetching active theme
    
    * Update copy
    
    * Import type

--- client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/hooks/use-screen.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/hooks/use-screen.tsx
@@ -6,14 +6,13 @@ import { NAVIGATOR_PATHS } from '../constants';
 import type { ScreenName } from '../types';
 
 export type UseScreenOptions = {
-	isNewSite?: boolean;
 	shouldUnlockGlobalStyles?: boolean;
 };
 
 export type Screen = {
 	name: string;
 	title: string;
-	description: TranslateResult;
+	description?: TranslateResult;
 	continueLabel: string;
 	/** The label for going back from the next screen */
 	backLabel?: string;
@@ -87,17 +86,9 @@ const useScreen = ( screenName: ScreenName, options: UseScreenOptions = {} ): Sc
 			backLabel: hasEnTranslation( 'premium styles' ) ? translate( 'premium styles' ) : undefined,
 			initialPath: NAVIGATOR_PATHS.UPSELL,
 		},
-		activation: {
-			name: 'activation',
-			title: translate( 'Activate this theme' ),
-			description: translate( 'Activating this theme will result in the following changes.' ),
-			continueLabel: translate( 'Activate' ),
-			initialPath: NAVIGATOR_PATHS.ACTIVATION,
-		},
 		confirmation: {
 			name: 'confirmation',
 			title: translate( 'Great job!' ),
-			description: translate( 'Time to add some content and bring your site to life!' ),
 			continueLabel: translate( 'Start adding content' ),
 			initialPath: NAVIGATOR_PATHS.CONFIRMATION,
 		},
@@ -108,22 +99,14 @@ const useScreen = ( screenName: ScreenName, options: UseScreenOptions = {} ): Sc
 		styles: screens.main,
 		pages: screens.styles,
 		upsell: screens.pages,
-		activation: options.shouldUnlockGlobalStyles ? screens.upsell : screens.pages,
 		confirmation: options.shouldUnlockGlobalStyles ? screens.upsell : screens.pages,
 	};
 
 	const nextScreens = {
 		main: screens.styles,
 		styles: screens.pages,
-		pages: ( () => {
-			if ( options.shouldUnlockGlobalStyles ) {
-				return screens.upsell;
-			}
-
-			return options.isNewSite ? screens.confirmation : screens.activation;
-		} )(),
-		upsell: options.isNewSite ? screens.confirmation : screens.activation,
-		activation: null,
+		pages: options.shouldUnlockGlobalStyles ? screens.upsell : screens.confirmation,
+		upsell: screens.confirmation,
 		confirmation: null,
 	};
 
