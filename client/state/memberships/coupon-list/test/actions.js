5e0f6833ad1d1ae2cbcd4497d9138f40444a7a58
Chris McCluskey <chris.mccluskey@automattic.com>
2023-12-28 14:23:03 -0800

    Fix tests

--- client/state/memberships/coupon-list/test/actions.js
+++ client/state/memberships/coupon-list/test/actions.js
@@ -1,15 +1,5 @@
-import {
-	MEMBERSHIPS_COUPONS_LIST,
-	MEMBERSHIPS_COUPON_ADD,
-	MEMBERSHIPS_COUPON_DELETE,
-	MEMBERSHIPS_COUPON_RECIEVE,
-} from 'calypso/state/action-types';
-import {
-	requestCoupons,
-	receiveUpdateCoupon,
-	receiveDeleteCoupon,
-	requestAddCoupon,
-} from '../actions';
+import { MEMBERSHIPS_COUPONS_LIST, MEMBERSHIPS_COUPON_DELETE } from 'calypso/state/action-types';
+import { requestCoupons, receiveUpdateCoupon, receiveDeleteCoupon } from '../actions';
 
 describe( 'actions', () => {
 	describe( 'requestCoupons()', () => {
@@ -26,7 +16,7 @@ describe( 'actions', () => {
 			expect( receiveUpdateCoupon( 123, {} ) ).toEqual( {
 				coupon: {},
 				siteId: 123,
-				type: MEMBERSHIPS_COUPON_RECIEVE,
+				type: MEMBERSHIPS_COUPON_DELETE,
 			} );
 		} );
 	} );
@@ -37,28 +27,6 @@ describe( 'actions', () => {
 				couponId: 456,
 				siteId: 123,
 				type: MEMBERSHIPS_COUPON_DELETE,
-			} );
-		} );
-	} );
-
-	describe( 'requestAddCoupon', () => {
-		test( 'should return an action object with the new coupon', () => {
-			requestAddCoupon( 123, {}, 'coupon added' )( ( foo ) => console.log( foo ) );
-			expect( requestAddCoupon( 123, {}, 'coupon added' ) ).toEqual( {
-				coupon: {},
-				siteId: 123,
-				type: MEMBERSHIPS_COUPON_ADD,
-			} );
-			requestAddCoupon(
-				123,
-				{},
-				'coupon added'
-			)( ( result ) => {
-				expect( result ).toEqual( {
-					coupon: {},
-					siteId: 123,
-					type: MEMBERSHIPS_COUPON_ADD,
-				} );
 			} );
 		} );
 	} );
