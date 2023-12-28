43b1cd96fdaecd4a6296458d45d39f70c662095b
Douglas Henri <dhasilva@users.noreply.github.com>
2023-12-28 12:08:26 -0300

    AI Logo generator: Add history carousel linked to the store (#85808)
    * fix lint issues
    
    * update store with logo generation data and actions
    
    * small adjustments
    
    * add history-carousel component
    
    * link logo presenter to store
    
    * link logo presenter to store and update CSS

--- packages/jetpack-ai-calypso/src/logo-generator/store/index.ts
+++ packages/jetpack-ai-calypso/src/logo-generator/store/index.ts
@@ -7,10 +7,7 @@ import { createReduxStore, register } from '@wordpress/data';
  */
 import actions from './actions';
 import reducer from './reducer';
-/**
- * Types
- */
-import type { AiFeatureProps, LogoGeneratorStateProp } from './types';
+import selectors from './selectors';
 
 export const STORE_NAME = 'jetpack-ai/logo-generator';
 
@@ -22,30 +19,7 @@ const jetpackAiLogoGeneratorStore = createReduxStore( STORE_NAME, {
 
 	reducer,
 
-	selectors: {
-		/**
-		 * Return the AI Assistant feature.
-		 * @param {LogoGeneratorStateProp} state - The app state tree.
-		 * @returns {AiFeatureProps}       The AI Assistant feature data.
-		 */
-		getAiAssistantFeature( state: LogoGeneratorStateProp ): Partial< AiFeatureProps > {
-			// Clean up the _meta property.
-			const data = { ...state.features.aiAssistantFeature };
-			delete data._meta;
-
-			return data;
-		},
-
-		/**
-		 * Get the isRequesting flag for the AI Assistant feature.
-		 *
-		 * @param {LogoGeneratorStateProp} state - The app state tree.
-		 * @returns {boolean}              The isRequesting flag.
-		 */
-		getIsRequestingAiAssistantFeature( state: LogoGeneratorStateProp ): boolean {
-			return state.features.aiAssistantFeature?._meta?.isRequesting ?? false;
-		},
-	},
+	selectors,
 
 	resolvers: {
 		getAiAssistantFeature: ( siteId: string ) =>
