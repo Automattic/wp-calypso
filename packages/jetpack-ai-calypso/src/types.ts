e665876e5a23bbdee177592bdf1eebe2bd33a989
Douglas Henri <dhasilva@users.noreply.github.com>
2023-12-29 12:09:51 -0300

    AI Logo Generator: Save logo to media library (#85843)
    * add saveToMediaLibrary function
    
    * add selectors to useLogo hook
    
    * link save button to saveToLibrary function

--- packages/jetpack-ai-calypso/src/types.ts
+++ packages/jetpack-ai-calypso/src/types.ts
@@ -1,11 +1,31 @@
+/**
+ * Types
+ */
 import type { SiteDetails } from '@automattic/data-stores';
-
-export type UseLogoProps = {
-	subject?: string;
-};
 
 export interface GeneratorModalProps {
 	siteDetails?: SiteDetails;
 	isOpen: boolean;
 	onClose: () => void;
 }
+
+export type SaveToMediaLibraryProps = {
+	siteId: string | number;
+	url: string;
+	attrs?: {
+		caption?: string;
+		description?: string;
+		title?: string;
+		alt?: string;
+	};
+};
+
+export type SaveToMediaLibraryResponseProps = {
+	code: number;
+	media: [
+		{
+			ID: number;
+			URL: string;
+		},
+	];
+};
