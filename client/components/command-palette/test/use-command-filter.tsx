c79fcc9f039224bf64c07ad466eda466b28eb156
Kateryna Kodonenko <kateryna@automattic.com>
2023-12-29 13:15:50 -0500

    Add tests case for no match

--- client/components/command-palette/test/use-command-filter.tsx
+++ client/components/command-palette/test/use-command-filter.tsx
@@ -42,4 +42,12 @@ describe( 'useCommandFilter', () => {
 		const value = beforeSeparator + COMMAND_SEPARATOR + afterSeparator;
 		expect( commandFilter( value, search ) ).toBe( 0.5 );
 	} );
+
+	test( 'should return 0 where there is no match either for label or searchLabel', () => {
+		const search = 'CACHE';
+		const beforeSeparator = 'Open hosting configuration';
+		const afterSeparator = 'hosting manage';
+		const value = beforeSeparator + COMMAND_SEPARATOR + afterSeparator;
+		expect( commandFilter( value, search ) ).toBe( 0 );
+	} );
 } );
