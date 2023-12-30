1f3232b35a19ae2b87edd574a3ee0154b815186a
Rafael Agostini <rafael.agostini@automattic.com>
2023-12-29 21:30:37 -0600

    Add check for preflight test status in getDoesRewindNeedCredentials

--- client/state/selectors/get-does-rewind-need-credentials.js
+++ client/state/selectors/get-does-rewind-need-credentials.js
@@ -1,11 +1,16 @@
+import { getPreflightStatus } from 'calypso/state/rewind/preflight/selectors';
+import { PreflightTestStatus } from 'calypso/state/rewind/preflight/types';
 import getRewindState from 'calypso/state/selectors/get-rewind-state';
 
 /**
- * Get the entire Rewind state object.
- * @param {Object} state Global state tree
- * @param {?number|string} siteId the site ID
- * @returns {Object} Rewind state object
+ * Returns true if the rewind state is awaiting credentials and the preflight test has failed.
+ * @param {Object} state - Global state tree
+ * @param {number|string} siteId - The unique identifier for the site
+ * @returns {boolean} Returns true if the rewind functionality is in the 'awaitingCredentials' state
+ *                    and the preflight test has failed. Otherwise, returns false.
  */
 export default function getDoesRewindNeedCredentials( state, siteId ) {
-	return [ 'awaitingCredentials' ].includes( getRewindState( state, siteId ).state );
+	const preflightStatus = getPreflightStatus( state, siteId );
+	const rewindState = getRewindState( state, siteId ).state;
+	return preflightStatus === PreflightTestStatus.FAILED && rewindState === 'awaitingCredentials';
 }
