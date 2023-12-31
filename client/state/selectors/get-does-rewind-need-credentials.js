e4021e0708ea618d4a0ad040a6518d1153b749cd
Rafael Agostini <rafael.agostini@automattic.com>
2023-12-31 15:09:23 -0600

    Add logic to check credentials necessity based on preflight status

--- client/state/selectors/get-does-rewind-need-credentials.js
+++ client/state/selectors/get-does-rewind-need-credentials.js
@@ -1,16 +1,41 @@
+import {
+	hasJetpackCredentials,
+	areJetpackCredentialsInvalid,
+} from 'calypso/state/jetpack/credentials/selectors';
 import { getPreflightStatus } from 'calypso/state/rewind/preflight/selectors';
 import { PreflightTestStatus } from 'calypso/state/rewind/preflight/types';
-import getRewindState from 'calypso/state/selectors/get-rewind-state';
 
 /**
- * Returns true if the rewind state is awaiting credentials and the preflight test has failed.
- * @param {Object} state - Global state tree
- * @param {number|string} siteId - The unique identifier for the site
- * @returns {boolean} Returns true if the rewind functionality is in the 'awaitingCredentials' state
- *                    and the preflight test has failed. Otherwise, returns false.
+ * Determines whether the Jetpack Backup feature requires credentials for a given site.
+ * This function evaluates the necessity of credentials based on two factors:
+ * the preflight status and the state of the credentials (whether they exist and are valid).
+ *
+ * The function returns true in the following scenarios:
+ * - The site does not have credentials and the preflight status is failed. This implies
+ * that credentials are needed and must be provided for restores to function properly.
+ * - The site has credentials but they are invalid, and the preflight status is failed.
+ * This suggests that the existing credentials are not correct or have issues, and
+ * new valid credentials are required.
+ *
+ * In all other cases, the function returns false, indicating that either the credentials
+ * are valid or that the preflight status does not need credentials.
+ * @param {Object} state         - The current state object of the application.
+ * @param {number|string} siteId - The unique identifier for the site being evaluated.
+ * @returns {boolean}            - Returns true if it requires credentials for the site under
+ *                                 the specified conditions; otherwise, returns false.
  */
 export default function getDoesRewindNeedCredentials( state, siteId ) {
 	const preflightStatus = getPreflightStatus( state, siteId );
-	const rewindState = getRewindState( state, siteId ).state;
-	return preflightStatus === PreflightTestStatus.FAILED && rewindState === 'awaitingCredentials';
+	const hasCredentials = hasJetpackCredentials( state, siteId );
+	const areCredentialsInvalid = areJetpackCredentialsInvalid( state, siteId, 'main' );
+
+	if ( ! hasCredentials && preflightStatus === PreflightTestStatus.FAILED ) {
+		return true;
+	}
+
+	if ( hasCredentials && areCredentialsInvalid && preflightStatus === PreflightTestStatus.FAILED ) {
+		return true;
+	}
+
+	return false;
 }
