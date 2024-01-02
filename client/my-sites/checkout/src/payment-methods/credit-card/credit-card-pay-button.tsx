304489bffa6a3006094dc9d8e5d41a57ddd34a37
Payton Swick <payton@foolord.com>
2024-01-02 11:30:27 -0500

    Add logging for this new error

--- client/my-sites/checkout/src/payment-methods/credit-card/credit-card-pay-button.tsx
+++ client/my-sites/checkout/src/payment-methods/credit-card/credit-card-pay-button.tsx
@@ -7,7 +7,9 @@ import { useI18n } from '@wordpress/react-i18n';
 import debugFactory from 'debug';
 import { useDispatch } from 'react-redux';
 import { validatePaymentDetails } from 'calypso/lib/checkout/validation';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import { errorNotice } from 'calypso/state/notices/actions';
+import { logStashEvent } from '../../lib/analytics';
 import { actions, selectors } from './store';
 import type { WpcomCreditCardSelectors } from './store';
 import type { CardFieldState, CardStoreType } from './types';
@@ -82,6 +84,14 @@ export default function CreditCardPayButton( {
 									'Something seems to be wrong with the credit card form. Please try again or contact support for help.'
 								)
 							);
+							reduxDispatch(
+								recordTracksEvent( 'calypso_checkout_card_missing_element', {
+									error: 'No card number element found on page when submtting form.',
+								} )
+							);
+							logStashEvent( 'calypso_checkout_card_missing_element', {
+								error: 'No card number element found on page when submtting form.',
+							} );
 							return;
 						}
 						onClick( {
