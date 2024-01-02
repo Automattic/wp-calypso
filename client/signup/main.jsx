618ff93da0ce06faeafb438557265456dff25e09
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-01-02 19:45:06 +0100

    Use step name as part of class name to ensure it wont break the UI

--- client/signup/main.jsx
+++ client/signup/main.jsx
@@ -821,7 +821,7 @@ class Signup extends Component {
 
 		return (
 			<div className="signup__step" key={ stepKey }>
-				<div className="signup__step is-user">
+				<div className={ `signup__step is-${ stepName }` }>
 					{ shouldRenderLocaleSuggestions && (
 						<LocaleSuggestions path={ this.props.path } locale={ this.props.locale } />
 					) }
