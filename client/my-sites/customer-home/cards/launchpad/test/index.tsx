efa38b43f2eb912654fd1d6a7e68bb301dbd2d2d
Gabriel Caires <gabriel.caires@automattic.com>
2024-01-03 17:07:38 +0000

    Add tests to customer-home/launchpad

---
+++ client/my-sites/customer-home/cards/launchpad/test/index.tsx
@@ -0,0 +1,190 @@
+/**
+ * @jest-environment jsdom
+ */
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import apiFetch from '@wordpress/api-fetch';
+import React from 'react';
+import { reducer as ui } from 'calypso/state/ui/reducer';
+import { renderWithProvider } from 'calypso/test-helpers/testing-library';
+import CustomerHomeLaunchPad from '../';
+
+const sites = [
+	{},
+	{
+		ID: 1,
+		URL: 'example.wordpress.com',
+		plan: {
+			product_slug: 'free_plan',
+		},
+		options: {},
+	},
+];
+
+const initialState = {
+	sites: {
+		items: sites,
+		domains: {
+			items: [ 'example.wordpress.com' ],
+		},
+	},
+	ui: {
+		selectedSiteId: 1,
+	},
+	currentUser: {
+		id: 12,
+		user: {
+			email_verified: true,
+		},
+	},
+};
+
+// Due to an unknown issue, the following mock is required to <prevent> the following error:
+// TypeError: (0 , _wpcomProxyRequest.canAccessWpcomApis) is not a function
+// Probably there is a test leaking a mock or something like that.
+jest.mock( 'wpcom-proxy-request', () => ( {
+	...jest.requireActual( 'wpcom-proxy-request' ),
+} ) );
+
+jest.mock( '@wordpress/api-fetch' );
+
+const render = ( el ) => {
+	return renderWithProvider( el, {
+		initialState,
+		reducers: { ui },
+	} );
+};
+
+const DEFAULT_TAKS_RESPONSE = {
+	site_intent: 'build',
+	launchpad_screen: 'off',
+	checklist_statuses: {
+		design_completed: true,
+		site_theme_selected: true,
+		site_launched: true,
+		site_edited: true,
+	},
+	checklist: [
+		{
+			id: 'setup_general',
+			title: 'Task 1',
+			completed: false,
+			disabled: true,
+			subtitle: '',
+			badge_text: '',
+			isLaunchTask: false,
+			calypso_path: '/settings/general/gabrielcaires0.wordpress.com',
+		},
+		{
+			id: 'design_selected',
+			title: 'Task 2',
+			completed: true,
+			disabled: false,
+			subtitle: '',
+			badge_text: '',
+			isLaunchTask: false,
+			calypso_path: '/setup/update-design/designSetup?siteSlug=gabrielcaires0.wordpress.com',
+		},
+	],
+	is_enabled: false,
+	is_dismissed: false,
+	is_dismissable: true,
+	title: 'Some cool title for you task list',
+};
+
+describe( 'CustomerHomeLaunchPad', () => {
+	beforeAll( () => {
+		jest.mocked( apiFetch ).mockResolvedValue( DEFAULT_TAKS_RESPONSE );
+	} );
+
+	it( 'renders the launchpad', async () => {
+		render( <CustomerHomeLaunchPad checklistSlug="some-check-list" /> );
+
+		expect( await screen.findByText( 'Some cool title for you task list' ) ).toBeVisible();
+	} );
+
+	it( 'renders the task list', async () => {
+		render( <CustomerHomeLaunchPad checklistSlug="some-check-list" /> );
+
+		expect( await screen.findByText( 'Task 1' ) ).toBeVisible();
+		expect( await screen.findByText( 'Task 2' ) ).toBeVisible();
+	} );
+
+	it( 'renders the dissmiss settings menu when the tasks are not completed', async () => {
+		render( <CustomerHomeLaunchPad checklistSlug="some-check-list" /> );
+
+		expect( await screen.findByTitle( 'Skip settings' ) ).toBeVisible();
+	} );
+
+	describe( 'when the launchpad was previously dismissed', () => {
+		it( 'doesnt renders the launchpad', () => {
+			const data = {
+				...DEFAULT_TAKS_RESPONSE,
+				is_dismissed: true,
+			};
+			jest.mocked( apiFetch ).mockResolvedValueOnce( data );
+
+			render( <CustomerHomeLaunchPad checklistSlug="some-check-list" /> );
+
+			expect( screen.queryByText( 'Some cool title for you task list' ) ).not.toBeInTheDocument();
+		} );
+	} );
+
+	describe( 'when all tasks are completed', () => {
+		const data = {
+			...DEFAULT_TAKS_RESPONSE,
+			checklist: [
+				{
+					id: 'setup_general',
+					title: 'Task 1',
+					completed: true,
+					disabled: true,
+					subtitle: '',
+					badge_text: '',
+					isLaunchTask: false,
+					calypso_path: '/settings/general/gabrielcaires0.wordpress.com',
+				},
+			],
+		};
+
+		beforeEach( () => {
+			jest.mocked( apiFetch ).mockResolvedValueOnce( data );
+		} );
+
+		it( "doesn't renders skip button", async () => {
+			render( <CustomerHomeLaunchPad checklistSlug="some-check-list" /> );
+
+			expect( await screen.queryByTitle( 'Skip settings' ) ).not.toBeInTheDocument();
+		} );
+
+		it( 'renders the dismiss button', async () => {
+			render( <CustomerHomeLaunchPad checklistSlug="some-check-list" /> );
+
+			expect( await screen.findByText( /Dismiss guide/ ) ).toBeVisible();
+		} );
+
+		it( 'hides the card when the user clicks on the dismiss guide button', async () => {
+			render( <CustomerHomeLaunchPad checklistSlug="some-check-list" /> );
+
+			userEvent.click( await screen.findByText( /Dismiss guide/ ) );
+			expect( screen.queryByText( 'Some cool title for you task list' ) ).not.toBeInTheDocument();
+		} );
+	} );
+
+	describe( 'when the launchpad is NOT dismissable', () => {
+		const dismissableTask = {
+			...DEFAULT_TAKS_RESPONSE,
+			is_dismissable: false,
+		};
+
+		beforeAll( () => {
+			jest.mocked( apiFetch ).mockResolvedValue( dismissableTask );
+		} );
+
+		it( 'renders the dissmiss settings menu when the tasks are not completed', () => {
+			render( <CustomerHomeLaunchPad checklistSlug="some-check-list" /> );
+
+			expect( screen.queryByTitle( 'Skip settings' ) ).not.toBeInTheDocument();
+		} );
+	} );
+} );
