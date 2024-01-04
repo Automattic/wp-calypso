6249f98ab727a815cfec9306abccc461fccc50e2
niranjan-uma-shankar <niranjan.u@gmail.com>
2024-01-04 12:19:46 +0400

    Add storage addon to cart in business signup flow

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -36,6 +36,7 @@ export function generateSteps( {
 	launchSiteApi = noop,
 	isPlanFulfilled = noop,
 	isAddOnsFulfilled = noop,
+	maybeAddStorageAddonToCart = noop,
 	isDomainFulfilled = noop,
 	maybeRemoveStepForUserlessCheckout = noop,
 	createSiteAndAddDIFMToCart = noop,
@@ -891,6 +892,15 @@ export function generateSteps( {
 			fulfilledStepCallback: isAddOnsFulfilled,
 			dependencies: [ 'siteSlug' ],
 			providesDependencies: [ 'cartItem' ],
+		},
+
+		'storage-addon': {
+			stepName: 'storage-addon',
+			apiRequestFunction: addAddOnsToCart,
+			fulfilledStepCallback: maybeAddStorageAddonToCart,
+			dependencies: [ 'siteSlug' ],
+			providesDependencies: [ 'cartItem' ],
+			optionalDependencies: [ 'cartItem' ],
 		},
 
 		'difm-site-picker': {
