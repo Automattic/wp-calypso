320d0d9d70a18f779d9a90c80bdbfc6758ee8412
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-01-05 16:38:57 +0100

    `PremiumGlobalStylesUpgradeModal`: Copy plan price from `ScreenUpsell`

--- client/components/premium-global-styles-upgrade-modal/index.tsx
+++ client/components/premium-global-styles-upgrade-modal/index.tsx
@@ -1,9 +1,18 @@
 import { PLAN_PREMIUM } from '@automattic/calypso-products';
-import { Button, Gridicon, Dialog, ScreenReaderText } from '@automattic/components';
+import {
+	Button,
+	Gridicon,
+	LoadingPlaceholder,
+	Dialog,
+	ScreenReaderText,
+} from '@automattic/components';
+import { formatCurrency } from '@automattic/format-currency';
 import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
 import QueryProductsList from 'calypso/components/data/query-products-list';
 import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
+import PlanPrice from 'calypso/my-sites/plan-price';
+import usePricingMetaForGridPlans from 'calypso/my-sites/plans-features-main/hooks/data-store/use-pricing-meta-for-grid-plans';
 import { useSelector } from 'calypso/state';
 import { getProductBySlug } from 'calypso/state/products-list/selectors';
 import useGlobalStylesUpgradeTranslations from './use-global-styles-upgrade-translations';
@@ -31,6 +40,14 @@ export default function PremiumGlobalStylesUpgradeModal( {
 	const premiumPlanProduct = useSelector( ( state ) => getProductBySlug( state, PLAN_PREMIUM ) );
 	const translations = useGlobalStylesUpgradeTranslations( { numOfSelectedGlobalStyles } );
 	const isLoading = ! premiumPlanProduct;
+	const pricingMeta = usePricingMetaForGridPlans( {
+		planSlugs: [ PLAN_PREMIUM ],
+		storageAddOns: null,
+	} );
+
+	const pricing = pricingMeta?.[ PLAN_PREMIUM ];
+	const isPricingLoaded =
+		pricing?.currencyCode && pricing?.originalPrice.monthly && pricing?.originalPrice.full;
 
 	const featureList = (
 		<div className="upgrade-modal__included">
@@ -82,7 +99,55 @@ export default function PremiumGlobalStylesUpgradeModal( {
 								</Button>
 							</div>
 						</div>
-						<div className="upgrade-modal__col">{ featureList }</div>
+						<div className="upgrade-modal__col">
+							<div className="upgrade-modal__plan">
+								<div className="upgrade-modal__plan-heading">
+									{ translate( '%(planTitle)s plan', {
+										args: { planTitle: translations.planTitle },
+									} ) }
+								</div>
+								{ isPricingLoaded ? (
+									<PlanPrice
+										className="upgrade-modal__plan-price"
+										currencyCode={ pricing?.currencyCode }
+										rawPrice={ pricing?.originalPrice?.monthly }
+										displayPerMonthNotation={ false }
+										isLargeCurrency
+										isSmallestUnit
+									/>
+								) : (
+									<LoadingPlaceholder style={ { height: '48px' } } />
+								) }
+								<div className="upgrade-modal__plan-billing-time-frame">
+									{ translate(
+										'per month, {{span}}%(rawPrice)s{{/span}} billed annually, excl. taxes',
+										{
+											args: {
+												rawPrice: isPricingLoaded
+													? formatCurrency(
+															pricing?.originalPrice.full ?? 0,
+															pricing?.currencyCode ?? '',
+															{
+																stripZeros: true,
+																isSmallestUnit: true,
+															}
+													  )
+													: '',
+											},
+											comment: 'excl. taxes is short for excluding taxes',
+											components: {
+												span: isPricingLoaded ? (
+													<span />
+												) : (
+													<LoadingPlaceholder style={ { display: 'inline-block', width: '30%' } } />
+												),
+											},
+										}
+									) }
+								</div>
+							</div>
+							{ featureList }
+						</div>
 						<Button className="upgrade-modal__close" borderless onClick={ () => closeModal() }>
 							<Gridicon icon="cross" size={ 12 } />
 							<ScreenReaderText>{ translate( 'Close modal' ) }</ScreenReaderText>
