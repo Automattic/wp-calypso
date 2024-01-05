aa0f4203228c82d0f15d2aaa7a1187ef5854a655
VassilisDimitrakis <vassilis.dimitrakis@automattic.com>
2024-01-05 18:27:47 +0200

    Add a new banner for blaze on woo express implementation (#85946)
    * Add a new banner for blaze on woo express implementation
    
    * Import and use the existing blaze svg graph.
    Refactor a bit the graphic so that colors are now props and can be applied when using the component
    
    * Fix alignment of items in relation to the new blaze svg

--- client/my-sites/promote-post-i2/components/woo-banner/index.tsx
+++ client/my-sites/promote-post-i2/components/woo-banner/index.tsx
@@ -1,23 +1,24 @@
 import { translate } from 'i18n-calypso';
 import './style.scss';
-import photoBanner from 'calypso/assets/images/blaze/wp-blaze-banner@3x.png';
+import photoBanner from 'calypso/assets/images/blaze/woo-blaze-banner@3x.png';
 import BlazeLogo from 'calypso/components/blaze-logo';
 import cssSafeUrl from 'calypso/lib/css-safe-url';
-export default function PostsListBanner() {
+
+export default function WooBanner() {
 	return (
-		<div className="posts-list-banner__container">
+		<div className="posts-list-woo-banner__container">
 			<div className="posts-list-banner__content">
 				<section className="posts-list-banner__text-section">
 					<div className="posts-list-banner__header">
-						<BlazeLogo size={ 16 } className="blaze" colorStart="#E65054" colorEnd="#E65054" />
+						<BlazeLogo size={ 16 } className="blaze" colorStart="#8052B2" colorEnd="#8052B2" />
 						{ translate( 'Powered by Blaze' ) }
 					</div>
 					<div className="posts-list-banner__title wp-brand-font">
-						{ translate( 'Transform content to an ad with a click.' ) }
+						{ translate( 'Transform your content to an ad with a click.' ) }
 					</div>
 					<div className="posts-list-banner__description">
 						{ translate(
-							'Grow your audience by promoting your content across Tumblr and WordPress.com.'
+							'Increase your sales by promoting your products and pages across millions of blogs and sites.'
 						) }
 					</div>
 				</section>
