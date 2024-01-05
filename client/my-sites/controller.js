ac5d0442b1df0963a719374ddbffd2cefdfb8d00
Julian Bustamante <julian.bustamante@pixelula.com>
2024-01-04 20:12:46 -0500

    Update noSite funciton to handle marketplace siteless checkout

--- client/my-sites/controller.js
+++ client/my-sites/controller.js
@@ -505,6 +505,7 @@ export function noSite( context, next ) {
 	const isDomainOnlyFlow = context.query?.isDomainOnly === '1' || ! siteFragment;
 	const isJetpackCheckoutFlow = context.pathname.includes( '/checkout/jetpack' );
 	const isAkismetCheckoutFlow = context.pathname.includes( '/checkout/akismet' );
+	const isMarketplaceSitelessCheckoutFlow = context.pathname.includes( '/checkout/marketplace' );
 	const isDomainsManage = context.pathname === '/domains/manage/';
 	const isGiftCheckoutFlow = context.pathname.includes( '/gift/' );
 	const isRenewal = context.pathname.includes( '/renew/' );
@@ -513,6 +514,7 @@ export function noSite( context, next ) {
 		! isDomainOnlyFlow &&
 		! isJetpackCheckoutFlow &&
 		! isAkismetCheckoutFlow &&
+		! isMarketplaceSitelessCheckoutFlow &&
 		! isGiftCheckoutFlow &&
 		! isDomainsManage &&
 		// We allow renewals without a site through because we want to show these
