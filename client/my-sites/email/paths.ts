4b2221332ea48ea756891fdfef1fa8a1f225bf78
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2024-01-08 08:58:55 +0100

    Fix types

--- client/my-sites/email/paths.ts
+++ client/my-sites/email/paths.ts
@@ -5,14 +5,18 @@ import { isUnderDomainManagementAll, domainManagementRoot } from 'calypso/my-sit
 type QueryStringParameters = { [ key: string ]: string | undefined };
 
 type EmailPathUtilityFunction = (
-	siteName: string,
-	domainName: string,
+	siteName: string | null | undefined,
+	domainName: string | null | undefined,
 	relativeTo?: string,
 	urlParameters?: QueryStringParameters
 ) => string;
 
 export const emailManagementPrefix = '/email';
 export const emailManagementAllSitesPrefix = '/email/all';
+
+export function isUnderEmailManagementAll( path?: string | null ) {
+	return path?.startsWith( emailManagementAllSitesPrefix + '/' );
+}
 
 // Builds a URL query string from an object. Handles null values.
 const buildQueryString = ( parameters?: QueryStringParameters ) =>
@@ -36,20 +40,18 @@ export const emailManagementAddEmailForwards: EmailPathUtilityFunction = (
 	domainName,
 	relativeTo,
 	urlParameters
-) => {
-	return emailManagementEdit( siteName, domainName, 'forwarding/add', relativeTo, urlParameters );
-};
+) => getEmailManagementPath( siteName, domainName, 'forwarding/add', relativeTo, urlParameters );
 
 // Retrieves the URL of the Add New Mailboxes page either for G Suite or Google Workspace
 export function emailManagementAddGSuiteUsers(
-	siteName: string,
-	domainName: string,
+	siteName: string | null | undefined,
+	domainName: string | null | undefined,
 	productType: typeof GOOGLE_WORKSPACE_PRODUCT_TYPE | typeof GSUITE_PRODUCT_TYPE,
 	relativeTo?: string,
 	source?: string
 ) {
 	if ( domainName ) {
-		return emailManagementEdit( siteName, domainName, productType + '/add-users', relativeTo, {
+		return getEmailManagementPath( siteName, domainName, productType + '/add-users', relativeTo, {
 			source,
 		} );
 	}
@@ -62,33 +64,28 @@ export const emailManagementManageTitanAccount: EmailPathUtilityFunction = (
 	domainName,
 	relativeTo,
 	urlParameters
-) => {
-	return emailManagementEdit( siteName, domainName, 'titan/manage', relativeTo, urlParameters );
-};
+) => getEmailManagementPath( siteName, domainName, 'titan/manage', relativeTo, urlParameters );
 
 export const emailManagementManageTitanMailboxes: EmailPathUtilityFunction = (
 	siteName,
 	domainName,
 	relativeTo,
 	urlParameters
-) => {
-	return emailManagementEdit(
+) =>
+	getEmailManagementPath(
 		siteName,
 		domainName,
 		'titan/manage-mailboxes',
 		relativeTo,
 		urlParameters
 	);
-};
 
 export const emailManagementNewTitanAccount: EmailPathUtilityFunction = (
 	siteName,
 	domainName,
 	relativeTo,
 	urlParameters
-) => {
-	return emailManagementEdit( siteName, domainName, 'titan/new', relativeTo, urlParameters );
-};
+) => getEmailManagementPath( siteName, domainName, 'titan/new', relativeTo, urlParameters );
 
 // Retrieves the URL to set up Titan mailboxes
 export const emailManagementTitanSetUpMailbox: EmailPathUtilityFunction = (
@@ -96,131 +93,72 @@ export const emailManagementTitanSetUpMailbox: EmailPathUtilityFunction = (
 	domainName,
 	relativeTo,
 	urlParameters
-) => {
-	return emailManagementEdit(
-		siteName,
-		domainName,
-		'titan/set-up-mailbox',
-		relativeTo,
-		urlParameters
-	);
-};
+) =>
+	getEmailManagementPath( siteName, domainName, 'titan/set-up-mailbox', relativeTo, urlParameters );
 
-export function emailManagementTitanSetUpThankYou(
-	siteName: string,
-	domainName: string,
+export const emailManagementTitanSetUpThankYou = (
+	siteName: string | null | undefined,
+	domainName: string | null | undefined,
 	emailAddress?: string,
 	relativeTo?: string
-) {
-	return emailManagementEdit(
+) =>
+	getEmailManagementPath(
 		siteName,
 		domainName,
 		'titan/set-up-mailbox/thank-you',
 		relativeTo,
 		emailAddress ? { email: emailAddress } : {}
 	);
-}
 
 export const emailManagementTitanControlPanelRedirect: EmailPathUtilityFunction = (
 	siteName,
 	domainName,
 	relativeTo,
 	urlParameters
-) => {
-	return emailManagementEdit(
-		siteName,
-		domainName,
-		'titan/control-panel',
-		relativeTo,
-		urlParameters
-	);
-};
+) =>
+	getEmailManagementPath( siteName, domainName, 'titan/control-panel', relativeTo, urlParameters );
 
 export const emailManagement = (
-	siteName?: string | null,
-	domainName?: string | null,
+	siteName: string | null | undefined,
+	domainName: string | null | undefined,
 	relativeTo?: string | null,
 	urlParameters?: QueryStringParameters
-) => {
-	let path;
-
-	if ( siteName && domainName ) {
-		path = emailManagementEdit( siteName, domainName, 'manage', relativeTo, urlParameters );
-	} else if ( siteName ) {
-		path = '/email/' + siteName + buildQueryString( urlParameters );
-	} else {
-		path = '/email';
-	}
-
-	return path;
-};
+) => getEmailManagementPath( siteName, domainName, 'manage', relativeTo, urlParameters );
 
 export const emailManagementForwarding: EmailPathUtilityFunction = (
 	siteName,
 	domainName,
 	relativeTo
-) => {
-	return emailManagementEdit( siteName, domainName, 'forwarding', relativeTo );
-};
+) => getEmailManagementPath( siteName, domainName, 'forwarding', relativeTo );
 
 // Retrieves the URL of the Email Comparison page
-export function emailManagementPurchaseNewEmailAccount(
-	siteName: string,
-	domainName: string,
+export const emailManagementPurchaseNewEmailAccount = (
+	siteName: string | null | undefined,
+	domainName: string | null | undefined,
 	relativeTo?: string,
 	source?: string,
 	emailProviderSlug?: string,
 	intervalLength?: string
-) {
-	return emailManagementEdit( siteName, domainName, 'purchase', relativeTo, {
+) =>
+	getEmailManagementPath( siteName, domainName, 'purchase', relativeTo, {
 		interval: intervalLength,
 		provider: emailProviderSlug,
 		source,
 	} );
-}
 
 // Retrieves the URL of the Email In-Depth Comparison page
-export function emailManagementInDepthComparison(
-	siteName: string,
-	domainName: string,
+export const emailManagementInDepthComparison = (
+	siteName: string | null | undefined,
+	domainName: string | null | undefined,
 	relativeTo?: string,
 	source?: string,
 	intervalLength?: string
-) {
-	return emailManagementEdit( siteName, domainName, 'compare', relativeTo, {
+) =>
+	getEmailManagementPath( siteName, domainName, 'compare', relativeTo, {
 		interval: intervalLength,
 		referrer: relativeTo,
 		source,
 	} );
-}
-
-export function emailManagementEdit(
-	siteName: string,
-	domainName: string,
-	slug?: string | null,
-	relativeTo?: string | null,
-	urlParameters?: QueryStringParameters
-) {
-	slug = slug || 'manage';
-
-	// Encodes only real domain names and not parameter placeholders
-	if ( domainName.startsWith( ':' ) ) {
-		// Encodes domain names so addresses with slashes in the path (e.g. used in site redirects) don't break routing.
-		// Note they are encoded twice since page.js decodes the path by default.
-		domainName = encodeURIComponent( encodeURIComponent( domainName ) );
-	}
-
-	return (
-		resolveRootPath( relativeTo ) +
-		'/' +
-		domainName +
-		'/' +
-		slug +
-		'/' +
-		siteName +
-		buildQueryString( urlParameters )
-	);
-}
 
 export function emailManagementMailboxes( siteName?: string ) {
 	if ( siteName ) {
@@ -229,6 +167,36 @@ export function emailManagementMailboxes( siteName?: string ) {
 	return `/mailboxes`;
 }
 
-export function isUnderEmailManagementAll( path?: string | null ) {
-	return path?.startsWith( emailManagementAllSitesPrefix + '/' );
+function getEmailManagementPath(
+	siteName: string | null | undefined,
+	domainName: string | null | undefined,
+	slug: string,
+	relativeTo?: string | null,
+	urlParameters?: QueryStringParameters
+) {
+	if ( siteName && domainName ) {
+		// Encodes only real domain names and not parameter placeholders
+		if ( domainName.startsWith( ':' ) ) {
+			// Encodes domain names so addresses with slashes in the path (e.g. used in site redirects) don't break routing.
+			// Note they are encoded twice since page.js decodes the path by default.
+			domainName = encodeURIComponent( encodeURIComponent( domainName ) );
+		}
+
+		return (
+			resolveRootPath( relativeTo ) +
+			'/' +
+			domainName +
+			'/' +
+			slug +
+			'/' +
+			siteName +
+			buildQueryString( urlParameters )
+		);
+	}
+
+	if ( siteName ) {
+		return '/email/' + siteName + buildQueryString( urlParameters );
+	}
+
+	return '/email';
 }
