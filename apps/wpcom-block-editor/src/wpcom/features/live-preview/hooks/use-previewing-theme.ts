7979d37e37a60a3dde795474f7049efd07319075
arthur791004 <arthur.chu@automattic.com>
2024-01-08 19:01:18 +0800

    Live Preview: Revert changes that introduce the @wordpress/router (#86104)
    * Revert "Live Preview: Make sure the routerPrivateApis is available (#86095)"
    
    This reverts commit 2f16d8d0a0201462596df1b17b2cab1b88784fbe.
    
    * Revert "Live Preview: Fix the preview notice stuck after the activation (#86089)"
    
    This reverts commit 7704d9f2687c87a0590a5bac664b8f2723c1ffa4.

--- apps/wpcom-block-editor/src/wpcom/features/live-preview/hooks/use-previewing-theme.ts
+++ apps/wpcom-block-editor/src/wpcom/features/live-preview/hooks/use-previewing-theme.ts
@@ -1,9 +1,8 @@
 import config from '@automattic/calypso-config';
 import { useSelect } from '@wordpress/data';
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import wpcom from 'calypso/lib/wp';
 import { currentlyPreviewingTheme, PREMIUM_THEME, WOOCOMMERCE_THEME } from '../utils';
-import useLocation from './use-location';
 import type { Theme } from 'calypso/types';
 
 /**
@@ -43,26 +42,20 @@ const getThemeFeature = ( theme?: Theme ) => {
 	return undefined;
 };
 
-export const usePreviewingThemeSlug = () => {
-	const location = useLocation();
-	const previewingThemeSlug = useMemo( () => currentlyPreviewingTheme(), [ location?.search ] );
-	return previewingThemeSlug;
-};
-
 export const usePreviewingTheme = () => {
-	const previewingThemeSlug = usePreviewingThemeSlug();
-	const { previewingThemeName } = useSelect(
-		( select ) => {
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			const previewingTheme = ( select( 'core' ) as any ).getTheme( previewingThemeSlug );
+	const { previewingThemeSlug, previewingThemeName } = useSelect( ( select ) => {
+		// This needs to be inside `useSelect`, so that we can recompute `previewingThemeSlug` when the active theme changes.
+		// This is a workaround because we're not listening to the changes to the `wp_theme_preview` param in the URL.
+		const previewingThemeSlug = currentlyPreviewingTheme();
 
-			return {
-				previewingThemeSlug,
-				previewingThemeName: previewingTheme?.name?.rendered || previewingThemeSlug,
-			};
-		},
-		[ previewingThemeSlug ]
-	);
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const previewingTheme = ( select( 'core' ) as any ).getTheme( previewingThemeSlug );
+
+		return {
+			previewingThemeSlug,
+			previewingThemeName: previewingTheme?.name?.rendered || previewingThemeSlug,
+		};
+	}, [] );
 
 	const previewingThemeId =
 		( previewingThemeSlug as string )?.split( '/' )?.[ 1 ] || previewingThemeSlug;
