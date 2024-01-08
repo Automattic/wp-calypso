5530d3e4f1db775b2c4d0b34e8e55a3ae85460e8
Ashar Fuadi <ashar.fuadi@automattic.com>
2024-01-08 12:04:11 +0700

    Design Picker Cleanup: delete unused design-setup-site step from signup framework (#86041)

--- client/signup/config/step-components.js
+++ client/signup/config/step-components.js
@@ -68,7 +68,6 @@ const stepNameToModuleName = {
 	'plans-personal-2y': 'plans',
 	'plans-premium-2y': 'plans',
 	'plans-theme-preselected': 'plans-theme-preselected',
-	'design-setup-site': 'design-picker',
 	'new-or-existing-site': 'new-or-existing-site',
 	'difm-site-picker': 'difm-site-picker',
 	'difm-design-setup-site': 'design-picker',
