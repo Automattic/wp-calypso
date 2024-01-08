a052096ec5a87f2e73c29b27c72635cd8653bc5f
Vassilis Dimitrakis <vassilis.dimitrakis@automattic.com>
2024-01-08 17:02:32 +0200

    Send props to header components

--- apps/blaze-dashboard/src/components/generic-header/index.jsx
+++ apps/blaze-dashboard/src/components/generic-header/index.jsx
@@ -2,8 +2,8 @@ import config from '@automattic/calypso-config';
 import JetpackBlazeHeader from '../jetpack-blaze-header';
 import WooBlazeHeader from '../woo-blaze-header';
 
-const GenericHeader = () => {
+const GenericHeader = ( props ) => {
 	const isWooBlaze = config.isEnabled( 'is_running_in_woo_site' );
-	return isWooBlaze ? <WooBlazeHeader /> : <JetpackBlazeHeader />;
+	return isWooBlaze ? <WooBlazeHeader { ...props } /> : <JetpackBlazeHeader { ...props } />;
 };
 export default GenericHeader;
