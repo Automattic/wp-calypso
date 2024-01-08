33dd89e8eb7aafd722861b90952b0d58ce226e5e
arthur <arthur.chu@automattic.com>
2024-01-08 18:49:32 +0800

    Revert "Live Preview: Make sure the routerPrivateApis is available (#86095)"
    This reverts commit 2f16d8d0a0201462596df1b17b2cab1b88784fbe.

--- apps/wpcom-block-editor/src/wpcom/features/live-preview/hooks/use-location.ts
+++ apps/wpcom-block-editor/src/wpcom/features/live-preview/hooks/use-location.ts
@@ -4,7 +4,7 @@ import { getUnlock } from '../utils';
 const unlock = getUnlock();
 
 let useLocation = () => null;
-if ( unlock && routerPrivateApis && unlock( routerPrivateApis ) ) {
+if ( unlock && unlock( routerPrivateApis ) ) {
 	useLocation = unlock( routerPrivateApis ).useLocation;
 }
 
