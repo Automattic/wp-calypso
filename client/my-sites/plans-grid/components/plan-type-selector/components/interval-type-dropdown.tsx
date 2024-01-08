c97076561d9a5083df100cc31e22ec89ddb493b5
Dehan <dehandecroos@gmail.com>
2024-01-08 16:29:06 -0500

    Remove  popover

--- client/my-sites/plans-grid/components/plan-type-selector/components/interval-type-dropdown.tsx
+++ client/my-sites/plans-grid/components/plan-type-selector/components/interval-type-dropdown.tsx
@@ -1,11 +1,7 @@
 import styled from '@emotion/styled';
 import { CustomSelectControl } from '@wordpress/components';
-import { translate } from 'i18n-calypso';
-import { useState } from 'react';
 import useIntervalOptions from '../hooks/use-interval-options';
-import useMaxDiscount from '../hooks/use-max-discount';
 import { IntervalTypeProps, SupportedUrlFriendlyTermType } from '../types';
-import PopupMessages from './popup-messages';
 
 const AddOnOption = styled.a`
 	& span.name,
@@ -39,20 +35,11 @@ const AddOnOption = styled.a`
 `;
 
 export const IntervalTypeDropdown: React.FunctionComponent< IntervalTypeProps > = ( props ) => {
-	const {
-		intervalType,
-		hideDiscountLabel,
-		showBiennialToggle,
-		usePricingMetaForGridPlans,
-		isInSignup,
-	} = props;
+	const { intervalType } = props;
 	const supportedIntervalType = (
 		[ 'yearly', '2yearly', '3yearly', 'monthly' ].includes( intervalType ) ? intervalType : 'yearly'
 	) as SupportedUrlFriendlyTermType;
-	const [ spanRef, setSpanRef ] = useState< HTMLSpanElement >();
 	const optionsList = useIntervalOptions( props );
-	const maxDiscount = useMaxDiscount( props.plans, usePricingMetaForGridPlans );
-	const popupIsVisible = Boolean( intervalType === 'monthly' && isInSignup && props.plans.length );
 
 	const selectOptionsList = Object.values( optionsList ).map( ( option ) => ( {
 		key: option.key,
@@ -66,10 +53,7 @@ export const IntervalTypeDropdown: React.FunctionComponent< IntervalTypeProps > 
 
 	return (
 		<>
-			<div
-				className="plan-type-selector__interval-type-dropdown-container"
-				ref={ intervalType === 'monthly' ? ( ref ) => ref && ! spanRef && setSpanRef( ref ) : null }
-			>
+			<div className="plan-type-selector__interval-type-dropdown-container">
 				<CustomSelectControl
 					className="plan-type-selector__interval-type-dropdown"
 					label=""
@@ -77,17 +61,6 @@ export const IntervalTypeDropdown: React.FunctionComponent< IntervalTypeProps > 
 					value={ selectOptionsList.find( ( { key } ) => key === supportedIntervalType ) }
 				/>
 			</div>
-			{ ! showBiennialToggle && hideDiscountLabel ? null : (
-				<PopupMessages context={ spanRef } isVisible={ popupIsVisible }>
-					{ translate(
-						'Save up to %(maxDiscount)d%% by paying annually and get a free domain for one year',
-						{
-							args: { maxDiscount },
-							comment: 'Will be like "Save up to 30% by paying annually..."',
-						}
-					) }
-				</PopupMessages>
-			) }
 		</>
 	);
 };
