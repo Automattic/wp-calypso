c86ee66b47a59dd44489b909b07a2ed3368a34eb
Aurorum <43215253+Aurorum@users.noreply.github.com>
2024-01-08 17:42:26 +0000

    Address code review

--- client/my-sites/site-settings/header-code/index.jsx
+++ client/my-sites/site-settings/header-code/index.jsx
@@ -1,16 +1,15 @@
 import { FEATURE_INSTALL_PLUGINS, getPlan, PLAN_BUSINESS } from '@automattic/calypso-products';
 import { Button, Card, Gridicon } from '@automattic/components';
-import { html } from '@codemirror/lang-html';
-import CodeMirror from '@uiw/react-codemirror';
 import classNames from 'classnames';
-import { localize } from 'i18n-calypso';
+import { useTranslate } from 'i18n-calypso';
 import { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import illustration404 from 'calypso/assets/images/illustrations/illustration-404.svg';
 import UpsellNudge from 'calypso/blocks/upsell-nudge';
 import DocumentHead from 'calypso/components/data/document-head';
 import QuerySiteSettings from 'calypso/components/data/query-site-settings';
 import EmptyContent from 'calypso/components/empty-content';
+import FormTextarea from 'calypso/components/forms/form-textarea';
 import HeaderCake from 'calypso/components/header-cake';
 import InlineSupportLink from 'calypso/components/inline-support-link';
 import Main from 'calypso/components/main';
@@ -30,52 +29,54 @@ import { isJetpackSite } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 import './style.scss';
 
-const HeaderCodeSettings = ( {
-	errorMessage,
-	hasPlanFeature,
-	headCode,
-	isAdmin,
-	isJetpack,
-	isRequestingSettings,
-	isSaveFailure,
-	isSaveSuccess,
-	isSavingSettings,
-	saveSettings,
-	siteId,
-	siteSlug,
-	successMessage,
-	translate,
-} ) => {
-	const [ htmlCode, setHtmlCode ] = useState( '' );
+const HeaderCodeSettings = () => {
+	const translate = useTranslate();
+	const dispatch = useDispatch();
 
-	useEffect( () => {
-		if ( 'string' === typeof headCode && headCode.length ) {
-			setHtmlCode( headCode );
-		}
+	const [ editedCode, setEditedCode ] = useState( '' );
 
-		// Necessary for if switching to a site without any code.
-		if ( ! headCode ) {
-			setHtmlCode( '' );
-		}
-	}, [ headCode, siteId ] );
+	const siteId = useSelector( ( state ) => getSelectedSiteId( state ) );
+	const hasPlanFeature = useSelector( ( state ) =>
+		siteHasFeature( state, siteId, FEATURE_INSTALL_PLUGINS )
+	);
+	const headCode = useSelector( ( state ) =>
+		getSiteSetting( state, siteId, 'jetpack_header_code' )
+	);
+	const isAdmin = useSelector( ( state ) => canCurrentUser( state, siteId, 'manage_options' ) );
+	const isJetpack = useSelector( ( state ) => isJetpackSite( state, siteId ) );
+	const isRequestingSettings = useSelector( ( state ) =>
+		isRequestingSiteSettings( state, siteId )
+	);
+	const isSaveFailure = useSelector( ( state ) => getSiteSettingsSaveError( state, siteId ) );
+	const isSaveSuccess = useSelector( ( state ) => isSiteSettingsSaveSuccessful( state, siteId ) );
+	const isSavingSettings = useSelector( ( state ) => isSavingSiteSettings( state, siteId ) );
+	const siteSlug = useSelector( ( state ) => getSelectedSiteSlug( state, siteId ) );
+	const saveSettings = () => {
+		dispatch(
+			saveSiteSettings( siteId, {
+				jetpack_header_code: editedCode,
+			} )
+		);
+	};
+	const noticeOptions = {
+		duration: 4000,
+	};
+
+	const successMessage = ( message ) => {
+		dispatch( successNotice( message, noticeOptions ) );
+	};
+
+	const errorMessage = ( message ) => {
+		dispatch( errorNotice( message, noticeOptions ) );
+	};
 
 	useEffect( () => {
 		if ( isSaveSuccess ) {
-			successMessage( translate( 'Code updated successfully!' ), {
-				duration: 4000,
-			} );
+			successMessage( translate( 'Code updated successfully!' ) );
 		} else if ( isSaveFailure && ! isSavingSettings ) {
-			errorMessage( translate( 'There was an error saving your code.' ), {
-				duration: 4000,
-			} );
+			errorMessage( translate( 'There was an error saving your code.' ) );
 		}
 	}, [ isSaveSuccess, isSavingSettings, isSaveFailure ] );
-
-	const handleSaveSettings = () => {
-		saveSettings( siteId, {
-			jetpack_header_code: htmlCode,
-		} );
-	};
 
 	const isSupported = isJetpack || hasPlanFeature;
 
@@ -124,33 +125,30 @@ const HeaderCodeSettings = ( {
 								}
 							) }
 						</p>
-						<div
+
+						<FormTextarea
 							className={ classNames( 'header-code__editor', {
 								'is-loading': isRequestingSettings,
 							} ) }
-						>
-							<CodeMirror
-								value={ htmlCode }
-								onChange={ ( value ) => setHtmlCode( value ) }
-								height="235px"
-								extensions={ [ html() ] }
-							/>
-						</div>
+							defaultValue={ headCode }
+							onChange={ ( input ) => setEditedCode( input.target.value ) }
+						/>
 						<div className="header-code__warning">
 							<Gridicon icon="info" size={ 18 } />
 							<p className="header-code__warning-text">
-								{ translate( "This code will be run for all your site's viewers." ) }{ ' ' }
 								{ translate(
-									'Please make sure that you understand any code and trust its source before adding it here.'
+									"This code will be run for all your site's viewers. Please make sure that you understand any code and trust its source before adding it here."
 								) }
 							</p>
 						</div>
 						<div className="header-code__button">
 							<Button
-								disabled={ headCode === htmlCode || ! isSupported || isRequestingSettings }
+								disabled={
+									headCode === editedCode || ! editedCode || ! isSupported || isRequestingSettings
+								}
 								primary
 								busy={ isSavingSettings }
-								onClick={ handleSaveSettings }
+								onClick={ saveSettings }
 							>
 								{ translate( 'Save' ) }
 							</Button>
@@ -162,21 +160,4 @@ const HeaderCodeSettings = ( {
 	);
 };
 
-export default connect(
-	( state ) => {
-		const siteId = getSelectedSiteId( state );
-		return {
-			hasPlanFeature: siteHasFeature( state, siteId, FEATURE_INSTALL_PLUGINS ),
-			headCode: getSiteSetting( state, siteId, 'jetpack_header_code' ),
-			isAdmin: canCurrentUser( state, siteId, 'manage_options' ),
-			isJetpack: isJetpackSite( state, siteId ),
-			isRequestingSettings: isRequestingSiteSettings( state, siteId ),
-			isSaveFailure: getSiteSettingsSaveError( state, siteId ),
-			isSaveSuccess: isSiteSettingsSaveSuccessful( state, siteId ),
-			isSavingSettings: isSavingSiteSettings( state, siteId ),
-			siteId,
-			siteSlug: getSelectedSiteSlug( state, siteId ),
-		};
-	},
-	{ saveSettings: saveSiteSettings, successMessage: successNotice, errorMessage: errorNotice }
-)( localize( HeaderCodeSettings ) );
+export default HeaderCodeSettings;
