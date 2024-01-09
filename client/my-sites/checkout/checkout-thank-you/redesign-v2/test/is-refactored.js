7cfacf088ee837f08ede7075a4c60a344ab1ab90
wongasy <angela.wong@automattic.com>
2024-01-09 14:41:17 -0500

    Fixed unit tests and add new tests

---
+++ client/my-sites/checkout/checkout-thank-you/redesign-v2/test/is-refactored.js
@@ -0,0 +1,45 @@
+import { isRefactored } from '../utils';
+
+describe( 'isRefactored', () => {
+	it( 'should return false if there is a failed purchase', () => {
+		const props = {
+			receipt: {
+				data: {
+					failedPurchases: [
+						{
+							productSlug: 'test-product',
+						},
+					],
+				},
+			},
+		};
+		expect( isRefactored( props ) ).toBe( false );
+	} );
+
+	it( 'should return true if there are multiple purchases that only contains domains', () => {
+		const props = {
+			receipt: {
+				data: {
+					purchases: [
+						{ productSlug: 'domain_map' },
+						{ productSlug: 'dotblog_domain', isDomainRegistration: true },
+					],
+					failedPurchases: [],
+				},
+			},
+		};
+		expect( isRefactored( props ) ).toBe( true );
+	} );
+
+	it( 'should return false if the purchase is not supported', () => {
+		const props = {
+			receipt: {
+				data: {
+					purchases: [ { productSlug: 'jetpack-personal' } ],
+					failedPurchases: [],
+				},
+			},
+		};
+		expect( isRefactored( props ) ).toBe( false );
+	} );
+} );
