489661c38fbdfb53bdb41732f6200ad26a7144f7
Jasper Kang <kangzjnet@gmail.com>
2024-01-11 11:29:28 +1300

    change support doc link

--- client/my-sites/stats/const.js
+++ client/my-sites/stats/const.js
@@ -1,6 +1,5 @@
 // No need to localize this URL, since it's used as the prefix for other localized URLs.
 // eslint-disable-next-line wpcalypso/i18n-unlocalized-url
-export const SUPPORT_URL = 'https://wordpress.com/support/stats/';
-export const UNDERSTAND_YOUR_TRAFFIC_SUPPORT_URL =
-	// eslint-disable-next-line wpcalypso/i18n-unlocalized-url
-	'https://wordpress.com/support/stats/understand-your-sites-traffic/';
+export const SUPPORT_URL = 'https://wordpress.com/support/stats/understand-your-sites-traffic/';
+// eslint-disable-next-line wpcalypso/i18n-unlocalized-url
+export const SUBSCRIBERS_SUPPORT_URL = 'https://wordpress.com/support/subscribers/';
