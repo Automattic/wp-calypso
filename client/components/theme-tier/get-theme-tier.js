b06db65cc382c6bf1dc80d453091dd2c3a02d033
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2024-01-10 18:42:33 +0200

    Remove retained benefits logic (which will be moved in a separate PR)

--- client/components/theme-tier/get-theme-tier.js
+++ client/components/theme-tier/get-theme-tier.js
@@ -1,22 +1,17 @@
 import config from '@automattic/calypso-config';
 import siteHasFeature from 'calypso/state/selectors/site-has-feature';
-import { getCanonicalTheme } from 'calypso/state/themes/selectors';
+import { getTheme } from 'calypso/state/themes/selectors';
 
 export default ( state, siteId, themeId ) => {
-	const theme = getCanonicalTheme( state, siteId, themeId );
+	const theme = getTheme( state, 'wpcom', themeId );
 	const themeTier = theme?.theme_tier || {};
 	const isThemeAllowedOnSite =
 		config.isEnabled( 'themes/tiers' ) && themeTier?.feature
 			? siteHasFeature( state, siteId, themeTier.feature )
 			: true;
 
-	const themeHasRetainedBenefits =
-		! isThemeAllowedOnSite &&
-		theme.retained_benefits?.is_eligible &&
-		siteHasFeature( state, siteId, theme.retained_benefits.tier.feature );
-
 	return {
-		themeTier: theme?.retained_benefits?.is_eligible ? theme.retained_benefits.tier : themeTier,
-		isThemeAllowedOnSite: isThemeAllowedOnSite || themeHasRetainedBenefits,
+		themeTier,
+		isThemeAllowedOnSite,
 	};
 };
