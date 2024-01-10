cab55b9cce4db0ab0cf8de4dad6bcc950667a8e3
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2024-01-10 18:56:11 +0200

    Add retained benefits

--- client/components/theme-tier/use-theme-tier.js
+++ client/components/theme-tier/use-theme-tier.js
@@ -1,11 +1,5 @@
-import { createSelector } from '@automattic/state-utils';
 import getThemeTier from 'calypso/components/theme-tier/get-theme-tier';
 import { useSelector } from 'calypso/state';
 
 export default ( siteId, themeId ) =>
-	useSelector(
-		createSelector(
-			( state ) => getThemeTier( state, siteId, themeId ),
-			( state ) => state.themes.queries[ siteId ]
-		)
-	);
+	useSelector( ( state ) => getThemeTier( state, siteId, themeId ) );
