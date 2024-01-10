2d57ba56dc0f94824e1da7e41292d21bcbc42bae
Tim Broddin <tim@wannabes.be>
2024-01-10 13:33:35 +0100

    Update Launchpad to full to make it not redirect

--- client/my-sites/checkout/checkout-thank-you/redesign-v2/sections/product/ProductPlan.tsx
+++ client/my-sites/checkout/checkout-thank-you/redesign-v2/sections/product/ProductPlan.tsx
@@ -1,8 +1,10 @@
 import { Button, Spinner } from '@automattic/components';
 import { useLaunchpad } from '@automattic/data-stores';
+import { useDispatch as useWPDispatch } from '@wordpress/data';
 import { translate } from 'i18n-calypso';
 import moment from 'moment';
 import { useEffect, useMemo, useState } from 'react';
+import { SITE_STORE } from 'calypso/landing/stepper/stores';
 import { getPurchaseByProductSlug } from 'calypso/lib/purchases/utils';
 import { useSelector } from 'calypso/state';
 import {
@@ -30,6 +32,11 @@ const ProductPlan = ( { siteSlug, primaryPurchase, siteID }: ProductPlanProps ) 
 		[ primaryPurchase.productSlug, purchases ]
 	);
 
+	const [ launchPadScreenUpdated, setLaunchPadScreenUpdated ] = useState< boolean >( false );
+	const [ redirectTo, setRedirectTo ] = useState< 'home' | 'launchpad' >( 'home' );
+
+	const { saveSiteSettings } = useWPDispatch( SITE_STORE );
+
 	useEffect( () => {
 		if ( ! isLoadingPurchases && productPurchase ) {
 			setExpirationDate(
@@ -40,14 +47,42 @@ const ProductPlan = ( { siteSlug, primaryPurchase, siteID }: ProductPlanProps ) 
 		}
 	}, [ isLoadingPurchases, productPurchase ] );
 
-	const { data: launchpad, isLoading } = useLaunchpad( siteSlug );
+	const { data: launchpad, isLoading } = useLaunchpad( siteSlug, 'intent-build' );
+
 	const hasRemainingTasks =
 		launchpad && launchpad.checklist
-			? launchpad.checklist.filter( ( item ) => item?.completed === false ).length > 0
+			? launchpad.checklist.filter(
+					( item ) => item?.completed === false && item?.disabled === false
+			  ).length > 0
 			: false;
-	const letsWorkHref = hasRemainingTasks
-		? `/setup/build/launchpad?siteSlug=${ siteSlug }&siteId=${ siteID }&showLaunchpad=true`
-		: `/home/${ siteSlug }`;
+
+	useEffect( () => {
+		// when launchpad is off or skipped, we need to update the launchpad_screen option to full
+		// otherwise the full screen launchpad will redirect to /home straight away
+		if ( launchpad ) {
+			if ( launchpad.launchpad_screen !== 'full' ) {
+				saveSiteSettings( siteID, { launchpad_screen: 'full' } ).then( () => {
+					setLaunchPadScreenUpdated( true );
+				} );
+			} else if ( launchpad.launchpad_screen === 'full' ) {
+				setLaunchPadScreenUpdated( true );
+			}
+		}
+
+		if ( launchpad && launchpad.is_enabled && hasRemainingTasks ) {
+			setRedirectTo( 'launchpad' );
+		} else {
+			setRedirectTo( 'home' );
+		}
+	}, [ launchpad ] );
+
+	const letsWorkHref =
+		redirectTo === 'launchpad' && hasRemainingTasks && launchPadScreenUpdated
+			? `/setup/build/launchpad?siteSlug=${ siteSlug }&showLaunchpad=true`
+			: `/home/${ siteSlug }`;
+
+	const letsworkButtonBusy =
+		isLoading || ( redirectTo === 'launchpad' && ! launchPadScreenUpdated );
 
 	return (
 		<div className="checkout-thank-you__header-details">
@@ -70,7 +105,7 @@ const ProductPlan = ( { siteSlug, primaryPurchase, siteID }: ProductPlanProps ) 
 				) }
 			</div>
 			<div className="checkout-thank-you__header-details-buttons">
-				<Button busy={ isLoading } primary href={ letsWorkHref }>
+				<Button busy={ letsworkButtonBusy } primary href={ letsWorkHref }>
 					{ translate( 'Let’s work on the site' ) }
 				</Button>
 				<Button href={ `/plans/my-plan/${ siteSlug }` }>{ translate( 'Manage plan' ) }</Button>
