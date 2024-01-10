aa07ab759a073fa315c7f9a247b73d8f356c4e9f
Bogdan Ungureanu <bogdanungureanu21@gmail.com>
2024-01-10 19:13:12 +0200

    Remove type

--- client/types.ts
+++ client/types.ts
@@ -45,15 +45,6 @@ export interface Theme {
 	style_variations: StyleVariation[];
 	stylesheet: string;
 	supportDocumentation: string;
-	retained_benefits: {
-		is_eligible: boolean;
-		eligibility_expiration_date: string | null;
-		tier: null | {
-			slug: string;
-			feature: string;
-			platform: string;
-		};
-	};
 	tags: string[];
 	taxonomies?: {
 		theme_feature?: ThemeFeature[];
