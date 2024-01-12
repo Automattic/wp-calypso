08075be6100cdd9963af6cd18fd596b6d5f3d307
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-01-12 10:44:56 +0100

    Import flows: Refactoring: useQuery instead of redux; update CTA text... (#85784)
    * Rename obsolete "Start building" to "Back to goals" button text
    
    * Update generateStepPath helper method supporting goals step
    
    * Ready steps:  Update logic to allow direct step access
    Removed dependency of the capture step, which, before this change, provided the urlData. Now, the fetching urlData logic is there.
    
    * Ready component: Update effect dependencies
    
    * useAnalyzeUrlQuery: Update stale time to 5 seconds
    This allows us to get cached analyzed data for the same URL between steps.
    
    * site-setup flow: Match goToStep logic with import-focused-flow
    We need this to keep only siteSlug queryParam for the 'import' step.
    
    * Capture step: Update logic proving fromUrl as query param
    This allows us to have direct access support of ready screens, getting rid of urlData dependency from the capture screen.
    
    * Capture screen: Get rid of redux connect data; simplify component

--- client/landing/stepper/declarative-flow/internals/steps-repository/import-ready-wpcom/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/import-ready-wpcom/index.tsx
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { ReadyAlreadyOnWPCOMStep } from 'calypso/blocks/import/ready';
+import { useAnalyzeUrlQuery } from 'calypso/data/site-profiler/use-analyze-url-query';
 import { Step } from 'calypso/landing/stepper/declarative-flow/internals/types';
+import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
-import { useSelector } from 'calypso/state';
-import { getUrlData } from 'calypso/state/imports/url-analyzer/selectors';
 import { ImportWrapper } from '../import';
 import { BASE_ROUTE } from '../import/config';
 import { generateStepPath } from '../import/helper';
 
-const ImportReadyNot: Step = function ImportStep( props ) {
+const ImportReadyWpcom: Step = function ImportStep( props ) {
 	const { navigation } = props;
-	const urlData = useSelector( getUrlData );
+	const fromUrl = useQuery().get( 'from' ) || '';
+	const { data: urlData, isFetched, isFetching } = useAnalyzeUrlQuery( fromUrl );
 
-	/**
-	 ↓ Effects
-	 */
-	if ( ! urlData ) {
-		goToHomeStep();
-		return null;
-	}
-
-	function goToHomeStep() {
+	const goToHomeStep = useCallback( () => {
 		navigation.goToStep?.( BASE_ROUTE );
-	}
+	}, [ navigation ] );
+
+	// redirect to home step if urlData is not available
+	useEffect( () => {
+		isFetched && ! urlData && goToHomeStep();
+	}, [ isFetched ] );
 
 	return (
 		<ImportWrapper { ...props }>
-			<ReadyAlreadyOnWPCOMStep
-				urlData={ urlData }
-				goToStep={ ( step, section ) => navigation.goToStep?.( generateStepPath( step, section ) ) }
-				recordTracksEvent={ recordTracksEvent }
-			/>
+			{ ! isFetching && urlData && (
+				<ReadyAlreadyOnWPCOMStep
+					urlData={ urlData }
+					goToStep={ ( step, section ) =>
+						navigation.goToStep?.( generateStepPath( step, section ) )
+					}
+					recordTracksEvent={ recordTracksEvent }
+				/>
+			) }
 		</ImportWrapper>
 	);
 };
 
-export default ImportReadyNot;
+export default ImportReadyWpcom;
