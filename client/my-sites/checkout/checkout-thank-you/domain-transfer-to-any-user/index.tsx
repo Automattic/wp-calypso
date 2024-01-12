819319681df9bc5445506eac00e57e4e8b672680
wongasy <angela.wong@automattic.com>
2024-01-12 10:22:08 -0500

    Recommit changes that got messed up during rebase

--- client/my-sites/checkout/checkout-thank-you/domain-transfer-to-any-user/index.tsx
+++ client/my-sites/checkout/checkout-thank-you/domain-transfer-to-any-user/index.tsx
@@ -1,13 +1,13 @@
-import { ConfettiAnimation } from '@automattic/components';
+import { Button } from '@wordpress/components';
 import { translate } from 'i18n-calypso';
 import emailImage from 'calypso/assets/images/thank-you-upsell/email.svg';
+import Main from 'calypso/components/main';
+import ThankYouV2 from 'calypso/components/thank-you-v2';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { getEmailManagementPath } from 'calypso/my-sites/email/paths';
-import ThankYouLayout from '../redesign-v2/ThankYouLayout';
-import DomainTransferToAnyUserFooter from '../redesign-v2/sections/footer/DomainTransferToAnyUserFooter';
-import DefaultThankYouHeader from '../redesign-v2/sections/header/Default';
-import ProductDomain from '../redesign-v2/sections/product/ProductDomain';
-import DefaultSubHeader from '../redesign-v2/sections/subheader/Default';
-import DefaultUpsell from '../redesign-v2/sections/upsell/Default';
+import MasterbarStyled from '../redesign-v2/masterbar-styled';
+import getDomainFooterDetails from '../redesign-v2/pages/content/get-domain-footer-details';
+import ThankYouDomainProduct from '../redesign-v2/products/domain-product';
 
 interface DomainTransferToAnyUserContainerProps {
 	domain: string;
@@ -16,31 +16,40 @@ interface DomainTransferToAnyUserContainerProps {
 const DomainTransferToAnyUser: React.FC< DomainTransferToAnyUserContainerProps > = ( {
 	domain,
 } ) => {
+	const upsellProps = {
+		title: translate( 'Professional email' ),
+		description: translate(
+			'85% of people trust an email address with a custom domain name over a generic one.'
+		),
+		icon: emailImage,
+		action: (
+			<Button
+				href={ getEmailManagementPath( domain, domain ) }
+				onClick={ () =>
+					recordTracksEvent( 'calypso_domain_transfer_thank_you_professional_email_click' )
+				}
+			>
+				{ translate( 'Add email' ) }
+			</Button>
+		),
+	};
+
+	const products = [ <ThankYouDomainProduct domainName={ domain } key="transferred-domain" /> ];
+
 	return (
-		<ThankYouLayout>
-			<DefaultThankYouHeader>
-				{ translate( 'Your domain transfer is underway' ) }
-			</DefaultThankYouHeader>
-			<DefaultSubHeader>
-				{ translate(
+		<Main className="is-redesign-v2">
+			<MasterbarStyled canGoBack={ false } />
+
+			<ThankYouV2
+				title={ translate( 'Your domain transfer is underway' ) }
+				subtitle={ translate(
 					'Domain transfers can take a few minutes, we’ll email you once it’s set up.'
 				) }
-			</DefaultSubHeader>
-			<ProductDomain domain={ domain } />
-			<DomainTransferToAnyUserFooter />
-			<DefaultUpsell
-				title={ translate( 'Professional email' ) }
-				description={ translate(
-					'85% of people trust an email address with a custom domain name over a generic one.'
-				) }
-				meshColor="blue"
-				icon={ emailImage }
-				href={ getEmailManagementPath( domain, domain ) }
-				buttonText={ translate( 'Add email' ) }
-				trackEvent="calypso_domain_transfer_thank_you_professional_email_click"
+				products={ products }
+				footerDetails={ getDomainFooterDetails() }
+				upsellProps={ upsellProps }
 			/>
-			<ConfettiAnimation delay={ 1000 } />
-		</ThankYouLayout>
+		</Main>
 	);
 };
 
