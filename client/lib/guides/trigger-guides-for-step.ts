bd16c8c0857f4186d945b9feb85204d32136bda0
Aneesh Devasthale <aneesh.devasthale@automattic.com>
2024-01-12 14:31:29 +0530

    Guides: Add in-memory caching to prevent duplicate requests on triggerGuidesForStep function

--- client/lib/guides/trigger-guides-for-step.ts
+++ client/lib/guides/trigger-guides-for-step.ts
@@ -1,7 +1,18 @@
 import wpcom from 'calypso/lib/wp';
 
+/**
+ * In-memory cache to prevent duplicate requests.
+ * Users can go through a flow multiple times, and this will be reset on a page reload.
+ */
+const previousRequests = new Set< string >();
+
 // trigger guides on step movement, we don't care about failures or response
 export function triggerGuidesForStep( flowName: string, stepName?: string ) {
+	const key = flowName + ':' + ( stepName || '' );
+	if ( previousRequests.has( key ) ) {
+		return;
+	}
+	previousRequests.add( key );
 	wpcom.req
 		.post(
 			'guides/trigger',
