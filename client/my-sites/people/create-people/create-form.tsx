cb39d9ca8467d9b9a66e64516de52273ca5a7038
escapemanuele <escapemanuele@gmail.com>
2024-01-16 15:33:02 +0100

    Add first and last name

--- client/my-sites/people/create-people/create-form.tsx
+++ client/my-sites/people/create-people/create-form.tsx
@@ -1,8 +1,13 @@
 import { Button, FormInputValidation } from '@automattic/components';
 import { localizeUrl } from '@automattic/i18n-utils';
+import { suggestEmailCorrection } from '@automattic/onboarding';
 import { useState } from '@wordpress/element';
+import { Icon, check } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
+import { useDebouncedCallback } from 'use-debounce';
+import emailValidator from 'email-validator';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
+import ValidationFieldset from 'calypso/signup/validation-fieldset';
 import FormLabel from 'calypso/components/forms/form-label';
 import FormTextInput from 'calypso/components/forms/form-text-input';
 import FormTextarea from 'calypso/components/forms/form-textarea';
@@ -26,10 +31,85 @@ const CreateForm = ( { siteId }: CreateFormProps ) => {
 	const includeSubscribers = isAtomic;
 
 	const [ role, setRole ] = useState( defaultUserRole );
+	const [ showMsg, setShowMsg ] = useState( false );
+	const [ message, setMessage ] = useState( '' );
+	const [ formState, setFormState ] = useState( {
+		email: '',
+		firstName: '',
+		lastName: '',
+	} );
+	const [ errorMessages, setErrorMessages ] = useState( null );
+	const [ submitting, setSubmitting ] = useState( false );
 
 	const onFormSubmit = ( e: FormEvent ) => {
 		e.preventDefault();
+		setSubmitting( true );
 		console.log( 'SUBMIT' );
+	};
+
+	const handleAcceptDomainSuggestion = ( newEmail, newDomain, oldDomain ) => {
+		setFormState( {
+			...formState,
+			email: newEmail,
+		} );
+
+		setErrorMessages( null );
+		// this.props.recordTracksEvent( 'calypso_signup_domain_suggestion_confirmation', {
+		// 	original_domain: JSON.stringify( oldDomain ),
+		// 	suggested_domain: JSON.stringify( newDomain ),
+		// } );
+	};
+
+	const handleEmailDomainSuggestionError = ( newEmail, oldEmail, newDomain, oldDomain ) => {
+		// this.props.recordTracksEvent( 'calypso_signup_domain_suggestion_generated', {
+		// 	original_domain: JSON.stringify( oldDomain ),
+		// 	suggested_domain: JSON.stringify( newDomain ),
+		// } );
+		setErrorMessages( [
+			translate( 'Did you mean {{emailSuggestion/}}?', {
+				components: {
+					emailSuggestion: (
+						<Button
+							plain={ true }
+							className="signup-form__domain-suggestion-confirmation"
+							onClick={ () => {
+								handleAcceptDomainSuggestion( newEmail, newDomain, oldDomain );
+							} }
+						>
+							{ newEmail }
+						</Button>
+					),
+				},
+			} ),
+		] );
+	};
+
+	const [ debouncedEmailSuggestion ] = useDebouncedCallback( ( email ) => {
+		if ( emailValidator.validate( email ) ) {
+			const { newEmail, oldEmail, newDomain, oldDomain, wasCorrected } =
+				suggestEmailCorrection( email );
+			if ( wasCorrected ) {
+				handleEmailDomainSuggestionError( newEmail, oldEmail, newDomain, oldDomain );
+				return;
+			}
+		}
+	}, 1000 );
+
+	const onEmailInputChange = ( { target: { value } } ) => {
+		debouncedEmailSuggestion( value );
+
+		setFormState( {
+			...formState,
+			email: value,
+		} );
+		setErrorMessages( null );
+	};
+
+	const handleInputChange = ( { target: { name, value } } ) => {
+		setFormState( {
+			...formState,
+			[ name ]: value,
+		} );
 	};
 
 	function getRoleLearnMoreLink() {
@@ -58,6 +138,57 @@ const CreateForm = ( { siteId }: CreateFormProps ) => {
 				explanation={ getRoleLearnMoreLink() }
 				formControlType="select"
 			/>
+			<ValidationFieldset errorMessages={ errorMessages }>
+				<FormLabel htmlFor="email">{ translate( 'Email' ) }</FormLabel>
+				<FormTextInput
+					// eslint-disable-next-line jsx-a11y/no-autofocus
+					autoFocus
+					autoCapitalize="off"
+					autoCorrect="off"
+					id="email"
+					type="email"
+					disabled={ submitting }
+					value={ formState.email }
+					placeholder={ translate( 'Email' ) }
+					onChange={ onEmailInputChange }
+				/>
+			</ValidationFieldset>
+
+			<FormLabel htmlFor="firstName">{ translate( 'First name' ) }</FormLabel>
+			<FormTextInput
+				autoCorrect="off"
+				className="signup-form__input"
+				disabled={ submitting }
+				id="firstName"
+				name="firstName"
+				value={ formState.firstName }
+				// isError={ formState.isFieldInvalid( this.state.form, 'firstName' ) }
+				// isValid={ formState.isFieldValid( this.state.form, 'firstName' ) }
+				// onBlur={ this.handleBlur }
+				onChange={ handleInputChange }
+			/>
+			{ /* 
+			{ formState.isFieldInvalid( this.state.form, 'firstName' ) && (
+				<FormInputValidation isError text={ this.getErrorMessagesWithLogin( 'firstName' ) } />
+			) } */ }
+
+			<FormLabel htmlFor="lastName">{ translate( 'Last name' ) }</FormLabel>
+			<FormTextInput
+				autoCorrect="off"
+				className="signup-form__input"
+				disabled={ submitting }
+				id="lastName"
+				name="lastName"
+				value={ formState.lastName }
+				// isError={ formState.isFieldInvalid( this.state.form, 'lastName' ) }
+				// isValid={ formState.isFieldValid( this.state.form, 'lastName' ) }
+				// onBlur={ this.handleBlur }
+				onChange={ handleInputChange }
+			/>
+
+			{ /* { formState.isFieldInvalid( this.state.form, 'lastName' ) && (
+				<FormInputValidation isError text={ this.getErrorMessagesWithLogin( 'lastName' ) } />
+			) } */ }
 
 			<FormFieldset>
 				{ ! showMsg && (
@@ -78,7 +209,7 @@ const CreateForm = ( { siteId }: CreateFormProps ) => {
 							autoFocus
 							id="message"
 							value={ message }
-							placeholder={ translate( 'This message will be sent along with invitation emails.' ) }
+							placeholder={ translate( 'This message will be sent to the new created user.' ) }
 							onChange={ ( e: ChangeEvent< HTMLInputElement > ) => setMessage( e.target.value ) }
 						/>
 					</>
