8b5aca7b81e296074fa39d9a4031d30676a1d7f4
Douglas Henri <dhasilva@users.noreply.github.com>
2024-01-16 14:20:50 -0300

    AI Logo Generator: Add upgrade screen for free users (#86433)
    * add UpgradeScreen component
    
    * add UpgradeScreen to GeneratorModal component

---
+++ packages/jetpack-ai-calypso/src/logo-generator/components/upgrade-screen.tsx
@@ -0,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import { Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+/**
+ * Internal dependencies
+ */
+import './upgrade-screen.scss';
+/**
+ * Types
+ */
+import type React from 'react';
+
+export const UpgradeScreen: React.FC< { onCancel: () => void; upgradeURL: string } > = ( {
+	onCancel,
+	upgradeURL,
+} ) => {
+	const upgradeMessage = __(
+		'Upgrade your Jetpack AI for access to exclusive features, including logo generation. This upgrade will also increase the amount of requests you can use in all AI-powered features.',
+		'jetpack'
+	);
+
+	return (
+		<div className="jetpack-ai-logo-generator-modal__upgrade-message-wrapper">
+			<div className="jetpack-ai-logo-generator-modal__upgrade-message">
+				<span className="jetpack-ai-logo-generator-modal__loading-message">{ upgradeMessage }</span>
+				&nbsp;
+				<Button variant="link" href="https://jetpack.com/ai/" target="_blank">
+					{ __( 'Learn more', 'jetpack' ) }
+				</Button>
+			</div>
+			<div className="jetpack-ai-logo-generator-modal__upgrade-actions">
+				<Button variant="tertiary" onClick={ onCancel }>
+					{ __( 'Cancel', 'jetpack' ) }
+				</Button>
+				<Button variant="primary" href={ upgradeURL } target="_blank" onClick={ onCancel }>
+					{ __( 'Upgrade', 'jetpack' ) }
+				</Button>
+			</div>
+		</div>
+	);
+};
