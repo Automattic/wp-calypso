c38edbfd46bf158e34dd7ca8491f61de813a4066
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-01-17 13:19:48 +0800

    Jetpack Manage: Implement a loading state for the new Payment method page. (#86443)

---
+++ client/jetpack-cloud/sections/partner-portal/stored-credit-placeholder-card/index.tsx
@@ -0,0 +1,10 @@
+import './style.scss';
+
+export default function StoreCreditPlaceholderCard() {
+	return (
+		<div className="store-credit-placeholder-card">
+			<div className="store-credit-placeholder-card-content"></div>
+			<div className="store-credit-placeholder-card-footer"></div>
+		</div>
+	);
+}
