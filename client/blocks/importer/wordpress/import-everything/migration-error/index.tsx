dd116c5917fc0a53b5959ac87fbe41e4843616ee
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-01-18 00:04:25 +0100

    Create MigrationErrorHint component

--- client/blocks/importer/wordpress/import-everything/migration-error/index.tsx
+++ client/blocks/importer/wordpress/import-everything/migration-error/index.tsx
@@ -1,12 +1,53 @@
 import { MigrationStatusError } from '@automattic/data-stores';
+import { localizeUrl } from '@automattic/i18n-utils';
 import { NextButton, SubTitle, Title } from '@automattic/onboarding';
 import { useTranslate } from 'i18n-calypso';
 import useErrorTitle from './use-error-title';
+import './style.scss';
 
 interface Props {
 	status: MigrationStatusError;
 	resetMigration: () => void;
 }
+
+export const MigrationErrorHint = () => {
+	const translate = useTranslate();
+	const jetpackSupportUrl = localizeUrl(
+		'https://jetpack.com/support/getting-started-with-jetpack/'
+	);
+	const wordPressSupportUrl = localizeUrl(
+		'https://wordpress.com/support/invite-people/user-roles/#administrator'
+	);
+
+	return (
+		<div className="migration-error--hint">
+			<p>{ translate( 'To continue, please:' ) }</p>
+			<ol>
+				<li>
+					{ translate(
+						'Please ensure the {{a}}Jetpack plugin is active and connected{{/a}} on the source website.',
+						{
+							components: {
+								a: <a href={ jetpackSupportUrl } />,
+							},
+						}
+					) }
+				</li>
+				<li>
+					{ translate(
+						'{{a}}Check that your WordPress.com account exists{{/a}} as administrator.',
+						{
+							components: {
+								a: <a href={ wordPressSupportUrl } />,
+							},
+						}
+					) }
+				</li>
+				<li>{ translate( 'Try again or contact our Happiness Engineers' ) }</li>
+			</ol>
+		</div>
+	);
+};
 
 export const MigrationError = ( props: Props ) => {
 	const translate = useTranslate();
