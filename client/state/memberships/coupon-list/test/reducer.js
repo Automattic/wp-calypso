c4e251356a8e7de923f35d79628a2a86fac30ad0
Chris McCluskey <chris.mccluskey@automattic.com>
2024-01-17 10:55:11 -0800

    Remove schema tests, cleanup reducer and selector tests

--- client/state/memberships/coupon-list/test/reducer.js
+++ client/state/memberships/coupon-list/test/reducer.js
@@ -7,26 +7,22 @@ import {
 import { items } from '../reducer';
 
 describe( 'reducer', () => {
-	describe( 'items()', () => {
-		test( 'should default to an empty object', () => {
-			expect( items( undefined, {} ) ).toEqual( {} );
-		} );
+	test( 'items() should default to an empty object', () => {
+		expect( items( undefined, {} ) ).toEqual( {} );
 	} );
 
-	describe( 'MEMBERSHIPS_COUPONS_RECEIVE', () => {
-		test( 'should properly add coupons for site', () => {
-			const coupons = [ { ID: 1 }, { ID: 2 } ];
-			const siteId = 1;
-			const expectation = { 1: coupons };
+	test( 'MEMBERSHIPS_COUPONS_RECEIVE should properly add existing coupons for site', () => {
+		const coupons = [ { ID: 1 }, { ID: 2 } ];
+		const siteId = 1;
+		const expectation = { 1: coupons };
 
-			expect( items( {}, { type: MEMBERSHIPS_COUPONS_RECEIVE, siteId, coupons } ) ).toEqual(
-				expectation
-			);
-		} );
+		expect( items( {}, { type: MEMBERSHIPS_COUPONS_RECEIVE, siteId, coupons } ) ).toEqual(
+			expectation
+		);
 	} );
 
 	describe( 'MEMBERSHIPS_COUPON_RECEIVE', () => {
-		test( 'should properly add coupons for site', () => {
+		test( 'should properly add a new coupon for site', () => {
 			const existingCoupons = [ { ID: 1 }, { ID: 2 } ];
 			const existingState = deepFreeze( { 1: existingCoupons } );
 			const newCoupon = { ID: 3 };
@@ -36,20 +32,27 @@ describe( 'reducer', () => {
 				items( existingState, { type: MEMBERSHIPS_COUPON_RECEIVE, siteId, coupon: newCoupon } )
 			).toEqual( expectation );
 		} );
-	} );
 
-	describe( 'MEMBERSHIPS_COUPON_DELETE', () => {
-		test( 'should properly delete coupons for specified site', () => {
-			const existingCoupons = [ { ID: 1 }, { ID: 2 }, { ID: 3 } ];
-			const existingState = { 1: existingCoupons };
-			const couponToDelete = { ID: 3 };
-			const siteId = '1';
-			const expectation = {
-				1: existingCoupons.filter( ( coupon ) => coupon.ID !== couponToDelete.ID ),
-			};
+		test( 'should properly add a new coupon for a different site', () => {
+			const existingCouponsForSite1 = [ { ID: 1 }, { ID: 2 }, { ID: 3 } ];
+			const existingState = deepFreeze( { 1: existingCouponsForSite1 } );
+			const newCoupon = { ID: 4 };
+			const siteId = '2';
+			const expectation = { 1: existingCouponsForSite1, 2: [ newCoupon ] };
 			expect(
-				items( existingState, { type: MEMBERSHIPS_COUPON_DELETE, siteId, coupon: couponToDelete } )
+				items( existingState, { type: MEMBERSHIPS_COUPON_RECEIVE, siteId, coupon: newCoupon } )
 			).toEqual( expectation );
 		} );
+	} );
+
+	test( 'MEMBERSHIPS_COUPON_DELETE should properly delete coupons for specified site', () => {
+		const existingCoupons = [ { ID: 1 }, { ID: 2 }, { ID: 3 } ];
+		const existingState = { 1: existingCoupons };
+		const couponToDelete = { ID: 3 };
+		const siteId = '1';
+		const expectation = { 1: [ { ID: 1 }, { ID: 2 } ] };
+		expect(
+			items( existingState, { type: MEMBERSHIPS_COUPON_DELETE, siteId, coupon: couponToDelete } )
+		).toEqual( expectation );
 	} );
 } );
