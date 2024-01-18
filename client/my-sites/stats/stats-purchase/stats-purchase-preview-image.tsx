d830d65c161cf1c1b74c9c6a5e56bacf770ccea6
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2024-01-18 16:42:34 +0800

    Comment on Odyssey detection.

--- client/my-sites/stats/stats-purchase/stats-purchase-preview-image.tsx
+++ client/my-sites/stats/stats-purchase/stats-purchase-preview-image.tsx
@@ -1,6 +1,7 @@
 import statsPurchasePreviewPNG from 'calypso/assets/images/stats/purchase-preview.png';
 
 const StatsPurchasePreviewImage = () => {
+	// See stats-purchase-svg.tsx for Odyssey/DotCom detection logic if needed.
 	return <img src={ statsPurchasePreviewPNG } alt="alt text goes here" />;
 };
 
