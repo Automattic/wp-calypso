e286894d0c4c1249e0a7e284124a39f38f8f543c
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2024-01-18 16:59:29 +0800

    Add HiDPI support.

--- client/my-sites/stats/stats-purchase/stats-purchase-preview-image.tsx
+++ client/my-sites/stats/stats-purchase/stats-purchase-preview-image.tsx
@@ -1,8 +1,13 @@
 import statsPurchasePreviewPNG from 'calypso/assets/images/stats/purchase-preview.png';
+import statsPurchasePreviewPNG2xRetina from 'calypso/assets/images/stats/purchase-preview@2x.png';
 
 const StatsPurchasePreviewImage = () => {
+	// Determine which image to use based on device pixel ratio.
+	const previewImage =
+		window.devicePixelRatio > 1 ? statsPurchasePreviewPNG2xRetina : statsPurchasePreviewPNG;
+
 	// See stats-purchase-svg.tsx for Odyssey/DotCom detection logic if needed.
-	return <img src={ statsPurchasePreviewPNG } alt="alt text goes here" />;
+	return <img src={ previewImage } alt="alt text goes here" />;
 };
 
 export default StatsPurchasePreviewImage;
