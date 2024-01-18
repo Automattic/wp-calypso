0b50a3a7f59515d23f531b303d604aa4a1973282
arthur <arthur.chu@automattic.com>
2024-01-18 17:15:28 +0800

    Onboarding: Change the default theme of write flow to Hey

--- client/landing/stepper/constants.ts
+++ client/landing/stepper/constants.ts
@@ -1,11 +1,11 @@
 import type { Design } from '@automattic/design-picker';
 
 export const WRITE_INTENT_DEFAULT_DESIGN: Design = {
-	slug: 'livro',
-	title: 'Livro',
+	slug: 'hey',
+	title: 'hey',
 	is_premium: false,
 	categories: [],
-	theme: 'livro',
+	theme: 'hey',
 };
 
 export const SITE_PICKER_FILTER_CONFIG = [ 'wpcom', 'atomic' ];
