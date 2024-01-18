1b4b5892b5777ebd8353d8c982c8fc755ebcc814
Tim Broddin <tim@wannabes.be>
2024-01-18 11:19:25 +0100

    Link the Let's work on your site button to the full screen launchpad

--- client/my-sites/checkout/checkout-thank-you/redesign-v2/products/plan-product.tsx
+++ client/my-sites/checkout/checkout-thank-you/redesign-v2/products/plan-product.tsx
@@ -1,8 +1,11 @@
+import { useLaunchpad } from '@automattic/data-stores';
 import { Button } from '@wordpress/components';
+import { useDispatch as useWPDispatch } from '@wordpress/data';
 import { translate } from 'i18n-calypso';
 import moment from 'moment';
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import ThankYouProduct from 'calypso/components/thank-you-v2/product';
+import { SITE_STORE } from 'calypso/landing/stepper/stores';
 import { getPurchaseByProductSlug } from 'calypso/lib/purchases/utils';
 import { useSelector } from 'calypso/state';
 import {
@@ -39,6 +42,62 @@ export default function ThankYouPlanProduct( {
 			? translate( 'Expires on %s', { args: moment( productPurchase.expiryDate ).format( 'LL' ) } )
 			: '';
 
+	const [ letsWorkButtonBusy, setLetsWorkButtonBusy ] = useState< boolean >( false );
+	const [ redirectTo, setRedirectTo ] = useState< 'home' | 'launchpad' >( 'home' );
+
+	const { saveSiteSettings } = useWPDispatch( SITE_STORE );
+
+	const { data: launchpad, isLoading } = useLaunchpad( siteSlug, 'intent-build' );
+
+	const hasRemainingTasks =
+		launchpad && launchpad.checklist
+			? launchpad.checklist.filter(
+					( item ) => item?.completed === false && item?.disabled === false
+			  ).length > 0
+			: false;
+
+	useEffect( () => {
+		if (
+			launchpad &&
+			launchpad.is_enabled &&
+			hasRemainingTasks &&
+			launchpad.launchpad_screen !== 'off'
+		) {
+			setRedirectTo( 'launchpad' );
+		} else {
+			setRedirectTo( 'home' );
+		}
+	}, [ launchpad ] );
+
+	useEffect( () => {
+		setLetsWorkButtonBusy( isLoading );
+	}, [ isLoading ] );
+
+	const letsWorkHref =
+		redirectTo === 'launchpad' && hasRemainingTasks
+			? `/setup/build/launchpad?siteSlug=${ siteSlug }&showLaunchpad=true`
+			: `/home/${ siteSlug }`;
+
+	const letsWorkButtonOnClick = useCallback(
+		( e: React.MouseEvent< HTMLElement > ) => {
+			if ( redirectTo === 'launchpad' ) {
+				e.preventDefault();
+				setLetsWorkButtonBusy( true );
+
+				const redirect = async () => {
+					if ( launchpad.launchpad_screen === 'skipped' ) {
+						await saveSiteSettings( siteId, { launchpad_screen: 'full' } );
+					}
+					setLetsWorkButtonBusy( false );
+					window.location.assign( letsWorkHref );
+				};
+
+				redirect();
+			}
+		},
+		[ launchpad, letsWorkHref, redirectTo ]
+	);
+
 	return (
 		<ThankYouProduct
 			name={ translate( '%(productName)s plan', {
@@ -50,7 +109,12 @@ export default function ThankYouPlanProduct( {
 			details={ expirationDate }
 			actions={
 				<>
-					<Button variant="primary" href={ `/home/${ siteSlug }` }>
+					<Button
+						isBusy={ letsWorkButtonBusy }
+						variant="primary"
+						href={ letsWorkHref }
+						onClick={ letsWorkButtonOnClick }
+					>
 						{ translate( 'Let’s work on the site' ) }
 					</Button>
 					<Button variant="secondary" href={ `/plans/my-plan/${ siteSlug }` }>
