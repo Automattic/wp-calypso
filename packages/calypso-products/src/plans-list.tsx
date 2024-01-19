6351a1768bf193b20bdc5be99613a9552026494b
oswian <oswian123@gmail.com>
2024-01-19 16:57:48 +1100

    Only enable 'woo commission fee' for ecommerce plans

--- packages/calypso-products/src/plans-list.tsx
+++ packages/calypso-products/src/plans-list.tsx
@@ -594,7 +594,6 @@ const getPlanFreeDetails = (): IncompleteWPcomPlan => ( {
 		FEATURE_PAYMENT_TRANSACTION_FEES_10,
 		FEATURE_GROUP_PAYMENT_TRANSACTION_FEES,
 		FEATURE_COMMISSION_FEE_STANDARD_FEATURES,
-		FEATURE_COMMISSION_FEE_WOO_FEATURES,
 	],
 	get2023PricingGridSignupJetpackFeatures: () => [
 		FEATURE_PAID_SUBSCRIBERS_JP,
@@ -1116,6 +1115,7 @@ const getPlanEcommerceDetails = (): IncompleteWPcomPlan => ( {
 		FEATURE_COMMISSION_FEE_STANDARD_FEATURES,
 		FEATURE_COMMISSION_FEE_WOO_FEATURES,
 	],
+	get2023PlanComparisonFeatureOverride: () => [ FEATURE_COMMISSION_FEE_WOO_FEATURES ],
 	getHostingSignupFeatures: ( term ) => () =>
 		compact( [
 			term !== TERM_MONTHLY && FEATURE_CUSTOM_DOMAIN,
