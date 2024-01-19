6b57b1aad44385d75c450966ad79373d91cb0777
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-01-19 16:52:18 +0100

    Simplify function

--- packages/design-picker/src/utils/__tests__/is-locked-style-variation.ts
+++ packages/design-picker/src/utils/__tests__/is-locked-style-variation.ts
@@ -1,18 +1,14 @@
 import { isLockedStyleVariation } from '../is-locked-style-variation';
-import { defaultStyleVariation } from './fixtures/default-style-variation';
-import { nonDefaultStyleVariation } from './fixtures/non-default-style-variation';
-import { nonPremiumDesign } from './fixtures/non-premium-design';
-import { premiumDesign } from './fixtures/premium-design';
 
 describe( 'isLockedStyleVariation', () => {
-	it( 'should return true when shouldLimitGlobalStyles is true, design is not premium, and selectedStyleVariation is not a default global styles variation slug', () => {
-		const design = nonPremiumDesign;
-		const selectedStyleVariation = nonDefaultStyleVariation;
+	it( 'should return true when shouldLimitGlobalStyles is true, theme is not premium, and selectedStyleVariationSlug is not default', () => {
+		const isPremium = false;
+		const selectedStyleVariationSlug = 'ember';
 		const shouldLimitGlobalStyles = true;
 
 		const result = isLockedStyleVariation( {
-			design,
-			selectedStyleVariation,
+			isPremium,
+			selectedStyleVariationSlug,
 			shouldLimitGlobalStyles,
 		} );
 
@@ -20,41 +16,41 @@ describe( 'isLockedStyleVariation', () => {
 	} );
 
 	it( 'should return false when shouldLimitGlobalStyles is false', () => {
-		const design = nonPremiumDesign;
-		const selectedStyleVariation = nonDefaultStyleVariation;
+		const isPremium = false;
+		const selectedStyleVariationSlug = 'ember';
 		const shouldLimitGlobalStyles = false;
 
 		const result = isLockedStyleVariation( {
-			design,
-			selectedStyleVariation,
+			isPremium,
+			selectedStyleVariationSlug,
 			shouldLimitGlobalStyles,
 		} );
 
 		expect( result ).toBe( false );
 	} );
 
-	it( 'should return false when design is premium', () => {
-		const design = premiumDesign;
-		const selectedStyleVariation = undefined;
+	it( 'should return false when theme is premium', () => {
+		const isPremium = true;
+		const selectedStyleVariationSlug = undefined;
 		const shouldLimitGlobalStyles = true;
 
 		const result = isLockedStyleVariation( {
-			design,
-			selectedStyleVariation,
+			isPremium,
+			selectedStyleVariationSlug,
 			shouldLimitGlobalStyles,
 		} );
 
 		expect( result ).toBe( false );
 	} );
 
-	it( 'should return false when selectedStyleVariation is a default global styles variation slug', () => {
-		const design = nonPremiumDesign;
-		const selectedStyleVariation = defaultStyleVariation;
+	it( 'should return false when selectedStyleVariationSlug is default', () => {
+		const isPremium = false;
+		const selectedStyleVariationSlug = 'default';
 		const shouldLimitGlobalStyles = true;
 
 		const result = isLockedStyleVariation( {
-			design,
-			selectedStyleVariation,
+			isPremium,
+			selectedStyleVariationSlug,
 			shouldLimitGlobalStyles,
 		} );
 
--- packages/design-picker/src/utils/is-locked-style-variation.ts
+++ packages/design-picker/src/utils/is-locked-style-variation.ts
@@ -2,18 +2,18 @@ import { Design, StyleVariation } from '../types';
 import { isDefaultGlobalStylesVariationSlug } from './global-styles';
 
 export function isLockedStyleVariation( {
-	design,
-	selectedStyleVariation,
+	isPremium,
+	selectedStyleVariationSlug,
 	shouldLimitGlobalStyles,
 }: {
-	design: Design;
-	selectedStyleVariation?: StyleVariation;
+	isPremium: Design[ 'is_premium' ];
+	selectedStyleVariationSlug?: StyleVariation[ 'slug' ];
 	shouldLimitGlobalStyles?: boolean;
 } ) {
 	return (
 		( shouldLimitGlobalStyles &&
-			! design.is_premium &&
-			! isDefaultGlobalStylesVariationSlug( selectedStyleVariation?.slug ) ) ??
+			! isPremium &&
+			! isDefaultGlobalStylesVariationSlug( selectedStyleVariationSlug ) ) ??
 		false
 	);
 }
