5f8b03040a5e09ce7dc0b71a8d2dfac9477f7414
andregardi <33497086+andregardi@users.noreply.github.com>
2024-01-19 14:33:52 -0300

    Close notifications on accessibility mode. (#86603)
    * Close notifications on accessibility mode.
    
    * Add accessbility escape key instruction to notifications list.
    
    * Fix list description.
    
    ---------
    
    Co-authored-by: André Gardi <andre.gardi@automattic.com>

--- apps/notifications/src/panel/templates/note-list.jsx
+++ apps/notifications/src/panel/templates/note-list.jsx
@@ -339,11 +339,21 @@ export class NoteList extends Component {
 			'is-empty-list': emptyNoteList,
 		} );
 
+		const notificationsListAriaProps = {
+			[ 'aria-live' ]: 'polite',
+			[ 'aria-description' ]: this.props.translate(
+				'Press the Escape key to close the notifications, or continue navigating to read them.'
+			),
+		};
+
 		return (
 			<div className={ classes } id="wpnc__note-list">
 				<FilterBar controller={ this.props.filterController } />
+				<button className="screen-reader-text" onClick={ this.props.closePanel }>
+					{ this.props.translate( 'Close notifications' ) }
+				</button>
 				<div ref={ this.storeScrollableContainer } className={ listViewClasses }>
-					<ol ref={ this.storeNoteList } className="wpnc__notes" aria-live="polite">
+					<ol ref={ this.storeNoteList } className="wpnc__notes" { ...notificationsListAriaProps }>
 						<StatusBar
 							statusClasses={ this.state.statusClasses }
 							statusMessage={ this.state.statusMessage }
