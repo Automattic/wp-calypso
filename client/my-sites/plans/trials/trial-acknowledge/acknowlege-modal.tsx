44ca25acd6479b66e038a59fa06c14335091b369
vykes-mac <knrymcleish@gmail.com>
2024-01-22 18:06:11 -0500

    show loading indicator when adding hosting trial in the modal

--- client/my-sites/plans/trials/trial-acknowledge/acknowlege-modal.tsx
+++ client/my-sites/plans/trials/trial-acknowledge/acknowlege-modal.tsx
@@ -1,7 +1,9 @@
 import { PLAN_HOSTING_TRIAL_MONTHLY } from '@automattic/calypso-products';
 import { Modal } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 import useAddHostingTrialMutation from 'calypso/data/hosting/use-add-hosting-trial-mutation';
 import { useDispatch, useSelector } from 'calypso/state';
+import { errorNotice } from 'calypso/state/notices/actions';
 import { initiateThemeTransfer } from 'calypso/state/themes/actions';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import { HostingTrialAcknowledgement } from './hosting-acknowledge';
@@ -15,22 +17,38 @@ export const TrialAcknowledgeModal = ( {
 	setOpenModal,
 	trialRequested,
 }: TrialAcknowledgeModalProps ) => {
-	const { mutateAsync: addHostingTrial } = useAddHostingTrialMutation();
+	const { mutateAsync: addHostingTrial, isPending } = useAddHostingTrialMutation();
 	const dispatch = useDispatch();
 	const siteId = useSelector( getSelectedSiteId ) as number;
 
-	const createTrialSite = async () => {
-		setOpenModal( false );
-		await addHostingTrial( { siteId, planSlug: PLAN_HOSTING_TRIAL_MONTHLY } );
-		dispatch( initiateThemeTransfer( siteId, null, '', '', 'hosting' ) );
-		trialRequested();
+	const createTrialSite = () => {
+		addHostingTrial(
+			{ siteId, planSlug: PLAN_HOSTING_TRIAL_MONTHLY },
+			{
+				onSuccess: () => {
+					dispatch( initiateThemeTransfer( siteId, null, '', '', 'hosting' ) );
+					trialRequested();
+					setOpenModal( false );
+				},
+				onError: () => {
+					dispatch( errorNotice( __( 'Error starting your free trial.' ) ) );
+				},
+			}
+		);
+	};
+
+	const closeModal = () => {
+		if ( ! isPending ) {
+			setOpenModal( false );
+		}
 	};
 
 	return (
-		<Modal onRequestClose={ () => setOpenModal( false ) } shouldCloseOnClickOutside={ false }>
+		<Modal onRequestClose={ closeModal } shouldCloseOnClickOutside={ false }>
 			<HostingTrialAcknowledgement
 				showFeatureList={ false }
 				onStartTrialClick={ createTrialSite }
+				CTAButtonState={ { isBusy: isPending, disabled: isPending } }
 			/>
 		</Modal>
 	);
