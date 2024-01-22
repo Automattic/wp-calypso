b0e39d3419eb1f7aa6ea2107b9db85793d9b1b63
Christopher Smith <chris@chriswsmith.info>
2024-01-22 15:56:24 -0600

    Add back required prop

--- client/my-sites/earn/components/add-edit-coupon-modal/index.tsx
+++ client/my-sites/earn/components/add-edit-coupon-modal/index.tsx
@@ -466,6 +466,7 @@ const RecurringPaymentsCouponAddEditModal = ( {
 					) }
 					{ isCharacterMaxReached && (
 						<FormInputValidation
+							isError={ false }
 							isWarning
 							text={ translate(
 								'The coupon code maximum length of 20 characters has been reached'
