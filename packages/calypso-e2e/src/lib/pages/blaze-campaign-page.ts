1ce2828a3f378d1ac8878fcaa896692dbc340e54
Sebastian Barbosa <sebabarbosa@gmail.com>
2024-01-22 10:24:28 -0300

    Address PR comments

--- packages/calypso-e2e/src/lib/pages/blaze-campaign-page.ts
+++ packages/calypso-e2e/src/lib/pages/blaze-campaign-page.ts
@@ -43,14 +43,14 @@ export class BlazeCampaignPage {
 	 * @param {TestFile} path TestFile object.
 	 */
 	async uploadImage( path: TestFile ) {
-		await this.page.getByRole( 'region', { name: 'Appearance' } ).waitFor( { timeout: 15 * 1000 } );
+		const fileChooserPromise = this.page.waitForEvent( 'filechooser', { timeout: 15_000 } );
 
-		const fileChooserPromiser = this.page.waitForEvent( 'filechooser' );
 		await this.page
 			.getByRole( 'region', { name: 'Appearance' } )
 			.getByRole( 'button', { name: 'Upload' } )
 			.click();
-		const fileChooser = await fileChooserPromiser;
+
+		const fileChooser = await fileChooserPromise;
 		await fileChooser.setFiles( path.fullpath );
 
 		await this.page.getByRole( 'region', { name: 'Appearance' } ).locator( 'img' ).waitFor();
