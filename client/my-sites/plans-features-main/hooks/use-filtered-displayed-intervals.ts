54d77bf93c4ccd78c13bbb6bf0afd4c6e1b05957
Jeremy Yip <jeremy.yip@automattic.com>
2024-01-23 11:54:10 -0800

    Add monthly interval to onboarding pm flow if no custom domain selected

---
+++ client/my-sites/plans-features-main/hooks/use-filtered-displayed-intervals.ts
@@ -0,0 +1,50 @@
+import {
+	getPlan,
+	getBillingMonthsForTerm,
+	isFreePlan,
+	URL_FRIENDLY_TERMS_MAPPING,
+	UrlFriendlyTermType,
+} from '@automattic/calypso-products';
+import { PlansIntent } from '@automattic/plans-grid-next';
+import { useMemo } from 'react';
+
+interface Props {
+	intent?: PlansIntent;
+	displayedIntervals: UrlFriendlyTermType[];
+	paidDomainName?: string;
+	productSlug?: string;
+}
+
+const useFilteredDisplayedIntervals = ( {
+	intent,
+	displayedIntervals,
+	paidDomainName,
+	productSlug,
+}: Props ) => {
+	return useMemo( () => {
+		let filteredIntervals = displayedIntervals;
+
+		// Hide interval terms that are less than the current plan's term in months
+		if ( productSlug && ! isFreePlan( productSlug ) ) {
+			const currentPlanIntervalInMonths = getBillingMonthsForTerm(
+				getPlan( productSlug )?.term || ''
+			);
+
+			filteredIntervals = displayedIntervals.filter( ( intervalType ) => {
+				const intervalInMonths = getBillingMonthsForTerm(
+					URL_FRIENDLY_TERMS_MAPPING[ intervalType ] || ''
+				);
+
+				return intervalInMonths >= currentPlanIntervalInMonths;
+			} );
+		}
+
+		if ( intent === 'plans-paid-media' && ! paidDomainName ) {
+			filteredIntervals = [ ...filteredIntervals, 'monthly' ];
+		}
+
+		return filteredIntervals;
+	}, [ productSlug, displayedIntervals, intent, paidDomainName ] );
+};
+
+export default useFilteredDisplayedIntervals;
