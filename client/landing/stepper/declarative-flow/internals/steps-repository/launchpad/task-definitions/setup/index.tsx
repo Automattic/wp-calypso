548e83d2b75cb4e447916c39eec142492b5c21e9
Gabriel Caires <gabriel.caires@automattic.com>
2024-01-24 13:45:53 +0000

    Launchpad: Migrate the `build`  setup flow (#86753)
    * Migrate setup_general task to use the new structure
    
    Enable migration on all development envs
    
    Remove unused task
    
    Remove unused flags
    
    * Fix wrong design-first flag

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/setup/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/setup/index.tsx
@@ -27,7 +27,23 @@ const getSetupBlog: TaskAction = ( task, flow, context ): Task => {
 	};
 };
 
+const getSetupGeneralTask: TaskAction = ( task, flow, context ): Task => {
+	const { siteInfoQueryArgs } = context;
+
+	return {
+		...task,
+		disabled: false,
+		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
+		calypso_path: addQueryArgs( `/setup/update-options/options`, {
+			...siteInfoQueryArgs,
+			flowToReturnTo: flow,
+		} ),
+		useCalypsoPath: true,
+	};
+};
+
 export const actions = {
 	setup_free: getSetupFreeTask,
 	setup_blog: getSetupBlog,
+	setup_general: getSetupGeneralTask,
 };
