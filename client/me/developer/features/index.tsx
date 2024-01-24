1d39649ed9dceb9295a2a6d9eccf86550ce9fd18
Volodymyr Makukha <nei.css@gmail.com>
2024-01-24 17:19:29 +0000

    chore: last fixes

--- client/me/developer/features/index.tsx
+++ client/me/developer/features/index.tsx
@@ -12,8 +12,8 @@ export const DeveloperFeatures = () => {
 	return (
 		<>
 			<div className="developer-features-list">
-				{ features.map( ( { title, description, linkLearnMore } ) => (
-					<Card className="developer-features-list__item">
+				{ features.map( ( { title, description, linkLearnMore }, index ) => (
+					<Card className="developer-features-list__item" key={ index }>
 						<div className="developer-features-list__item-title">{ title }</div>
 						<div className="developer-features-list__item-description">{ description }</div>
 						{ linkLearnMore && (
