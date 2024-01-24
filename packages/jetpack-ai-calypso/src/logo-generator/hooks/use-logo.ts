71f8b3c85cb880a31fbac7697d700baac4fe863b
Douglas <douglas.henri@automattic.com>
2024-01-24 11:59:16 -0300

    add dependency use

--- packages/jetpack-ai-calypso/src/logo-generator/hooks/use-logo.ts
+++ packages/jetpack-ai-calypso/src/logo-generator/hooks/use-logo.ts
@@ -1,13 +1,27 @@
 /**
+ * External dependencies
+ */
+import { recordTracksEvent } from '@automattic/calypso-analytics';
+/**
  * Types
  */
 import { UseLogoProps } from '../../types';
+import type { SiteDetails } from '@automattic/data-stores';
 
 const useLogo = ( { subject = 'World' }: UseLogoProps ) => {
 	const message: string = `Hello ${ subject }`;
+	const details: SiteDetails | Record< string, unknown > = {};
+
+	const test = () => {
+		recordTracksEvent( 'calypso_event_that_will_never_be_recorded', {
+			foo: 'bar',
+		} );
+	};
 
 	return {
 		message,
+		details,
+		test,
 	};
 };
 
