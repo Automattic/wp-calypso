ffb73dcf3df46a4e1bb5dab5b6acf4f31e83323d
Gabriel Caires <gabriel.caires@automattic.com>
2024-01-25 13:36:20 +0000

    Move verify_email task

---
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/email/index.tsx
@@ -0,0 +1,18 @@
+import { Task } from '@automattic/launchpad';
+import { recordTaskClickTracksEvent } from '../../tracking';
+import { TaskAction } from '../../types';
+
+const getVerifyEmail: TaskAction = ( task, flow, context ): Task => {
+	const { isEmailVerified } = context;
+
+	return {
+		...task,
+		completed: isEmailVerified || false,
+		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
+		useCalypsoPath: true,
+	};
+};
+
+export const actions = {
+	verify_email: getVerifyEmail,
+};
