1eeb8b5e161a6ba5c7ab54f1714827766ac92772
Yoav Farhi <yoav@farhi.org>
2024-01-29 11:09:10 +0200

    add a new signature option to Window.ze to handle setting conversationTags

--- packages/help-center/src/components/types.d.ts
+++ packages/help-center/src/components/types.d.ts
@@ -2,10 +2,13 @@
 declare const __i18n_text_domain__: string;
 interface Window {
 	helpCenterData: any;
-	zE?: (
-		action: string,
-		value: string,
-		handler?: ( callback: ( data: string | number ) => void ) => void
-	) => void;
+	zE?: {
+		( action: string, value: string, conversationTags: string[] ): void;
+		(
+			action: string,
+			value: string,
+			handler?: ( callback: ( data: string | number ) => void ) => void
+		): void;
+	};
 }
 declare module '*.jpg';
