79a880d9006d6cb2934ebe96b0ff95f0a309af7c
mpkelly <kikemelly@gmail.com>
2024-01-29 16:16:30 +0700

    GitHub deployments: support connecting/disconnecting GH

---
+++ client/my-sites/hosting/github-app/use-github-connection-query.ts
@@ -0,0 +1,35 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import wp from 'calypso/lib/wp';
+import { GITHUB_INTEGRATION_QUERY_KEY } from './constants';
+
+export const GITHUB_CONNECTION_QUERY_KEY = 'github-connection';
+
+export interface GithubConnectionData {
+	ID: number;
+	connected: boolean;
+	external_profile_picture: string;
+	repo: string;
+	branch: string;
+	base_path: string;
+	label: string;
+	external_name: string;
+}
+
+export const useGithubConnectionQuery = (
+	siteId: number | null,
+	options?: UseQueryOptions< GithubConnectionData >
+) => {
+	return useQuery< GithubConnectionData >( {
+		queryKey: [ GITHUB_INTEGRATION_QUERY_KEY, siteId, GITHUB_CONNECTION_QUERY_KEY ],
+		queryFn: (): GithubConnectionData =>
+			wp.req.get( {
+				path: `/sites/${ siteId }/hosting/github-app/connections`,
+				apiNamespace: 'wpcom/v2',
+			} ),
+		enabled: !! siteId,
+		meta: {
+			persist: false,
+		},
+		...options,
+	} );
+};
