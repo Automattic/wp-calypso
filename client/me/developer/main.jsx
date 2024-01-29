ccb5b9f53cffaa11b1e561c783978b03b92d5454
Volodymyr Makukha <nei.css@gmail.com>
2024-01-29 16:58:03 +0100

    Render a list of developer features (#86648)
    * feat: add features to developer(me) page
    
    * Update developer features copy
    
    * removed outdated string
    
    * chore: fix copy
    
    * style: desktop
    
    * style: mobile
    
    * chore: translations comments
    
    * chore: copy fix
    
    * chore: copy fix
    
    * chore: address feedbacks
    
    * chore: fix mobile title width
    
    * chore: revert spaces for em-dash
    
    * chore: fix fonts
    
    * chore: fix fonts
    
    * chore: fix track event
    
    * style: background from var
    
    * style: paddings
    
    * chore: last fixes
    
    * chore: add keys
    
    * chore: add ids
    
    * style: move toggle to top
    
    ---------
    
    Co-authored-by: wongasy <angela.wong@automattic.com>

--- client/me/developer/main.jsx
+++ client/me/developer/main.jsx
@@ -1,5 +1,4 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
-import { Card } from '@automattic/components';
 import { ToggleControl } from '@wordpress/components';
 import classnames from 'classnames';
 import { localize } from 'i18n-calypso';
@@ -15,6 +14,7 @@ import withFormBase from 'calypso/me/form-base/with-form-base';
 import ReauthRequired from 'calypso/me/reauth-required';
 import { recordGoogleEvent } from 'calypso/state/analytics/actions';
 import { isFetchingUserSettings } from 'calypso/state/user-settings/selectors';
+import { DeveloperFeatures } from './features/index';
 import { getIAmDeveloperCopy } from './get-i-am-a-developer-copy';
 
 import './style.scss';
@@ -30,7 +30,7 @@ class Developer extends Component {
 
 	render() {
 		return (
-			<Main className="developer">
+			<Main className="developer" wideLayout>
 				<PageViewTracker path="/me/developer" title="Me > Developer" />
 				<ReauthRequired twoStepAuthorization={ twoStepAuthorization } />
 				<NavigationHeader
@@ -39,24 +39,25 @@ class Developer extends Component {
 					subtitle={ this.props.translate(
 						'Take WordPress.com further with early access to new developer features.'
 					) }
+					className="developer__header"
 				/>
 
-				<Card className="developer__settings">
-					<form onChange={ this.props.submitForm }>
-						<FormFieldset
-							className={ classnames( {
-								'developer__is_dev_account-fieldset-is-loading': this.props.isFetchingUserSettings,
-							} ) }
-						>
-							<ToggleControl
-								disabled={ this.props.isFetchingUserSettings || this.props.isUpdatingUserSettings }
-								checked={ this.props.getSetting( 'is_dev_account' ) }
-								onChange={ this.toggleIsDevAccount }
-								label={ getIAmDeveloperCopy( this.props.translate ) }
-							/>
-						</FormFieldset>
-					</form>
-				</Card>
+				<form onChange={ this.props.submitForm }>
+					<FormFieldset
+						className={ classnames( 'developer__is_dev_account-fieldset', {
+							'is-loading': this.props.isFetchingUserSettings,
+						} ) }
+					>
+						<ToggleControl
+							disabled={ this.props.isFetchingUserSettings || this.props.isUpdatingUserSettings }
+							checked={ this.props.getSetting( 'is_dev_account' ) }
+							onChange={ this.toggleIsDevAccount }
+							label={ getIAmDeveloperCopy( this.props.translate ) }
+						/>
+					</FormFieldset>
+				</form>
+
+				<DeveloperFeatures />
 			</Main>
 		);
 	}
