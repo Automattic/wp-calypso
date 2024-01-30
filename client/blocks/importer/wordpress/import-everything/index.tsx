b0e7e426ee71fe333255d57d2dfeb85d16c65d32
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2024-01-30 15:48:17 +0100

    Import everything: Improve MigrationProgress and MigrationError screens (#85897)
    * Add new types
    
    * Minor refactoring of migration selectors
    
    * Add error descriptions
    
    * Add missing error code
    
    * Add progress messages
    
    * Create MigrationError component
    Also, there is a useErrorTitle hook that has isolated logic for generating error titles.
    
    * Prepare secondary button style
    
    * Prepare Get help button with chat support integration
    
    * Created MigrationProgress component
    
    * Integrate MigrationProcess screen and remove duplicated data
    
    * Fix missing migration status value
    
    * Adjust progress bar styles
    
    * Create MigrationState type and apply in existing components
    
    * Fix typing error
    
    * Handle restoring step based on the current step name
    
    * Handle backing up step based on the current step name
    
    * Update copy
    
    * Prepare MigrationError for providing specific error guidance
    
    * Add logic to force ErrorScreen rendering providing query param
    
    * Cover 'old_jetpack' error message
    
    * Cover 'forbidden' and 'error' generic error message
    
    * Cover 'error-backup-queue' error message
    
    * Cover 'source_site_multisite' error message
    
    * Cover 'source_site_is_atomic' error message
    
    * Cover 'source_site_is_protected' error message
    
    * Cover 'target_site_is_protected' error message
    
    * Cover 'no_start_user_admin_on_source' and 'no_start_user_admin_on_target' error message
    
    * Cover 'no_start_source_in_progress' and 'no_start_target_in_progress' error message
    
    * Cover 'error-backup-fail-not-accessible' error message
    
    * Cover 'error-get-master-user' error message
    
    * Set fallback if live chat is not available
    
    * Reorder renders; code aesthetic change
    
    * Remove the empty useTitle hook file
    
    * Remove unused util function
    
    * Provide missing {{a}} tag in the translate function
    
    * Add missing error status code to the initial chat support message
    
    * Add an additional check for opening chat support widget
    
    * Extend getHelp callback dependency list
    
    * Rename the hook; it was a copy/paste oversight
    
    * Add missing links and adjust subtitle link style
    
    * Update 'old_jetpack' error copy
    
    * Update 'forbidden' error copy
    
    * Update 'error-backup-queue' error copy
    
    * Update 'source_site_multisite' error copy and new CTA
    
    * Update 'source_site_is_atomic' error copy
    
    * Update 'source_site_is_protected' error copy
    
    * Update 'target_site_is_protected' error copy
    
    * Update 'no_start_user_admin_on_source' | 'no_start_user_admin_on_target' errors copy
    
    * Update 'no_start_source_in_progress' | 'no_start_target_in_progress' errors copy
    
    * Update 'error-backup-fail-not-accessible' error copy
    
    * Update 'error-get-master-user' error copy
    
    * Update 'default' error copy
    
    * Remove unsuded constants
    
    * Cover 'wpcom_migration_plugin_incompatible' error
    
    * Update copy; fix typo
    
    ---------
    
    Co-authored-by: Bogdan Nikolic <bogdan.nikolic87@gmail.com>

--- client/blocks/importer/wordpress/import-everything/index.tsx
+++ client/blocks/importer/wordpress/import-everything/index.tsx
@@ -1,13 +1,16 @@
-import { ProgressBar } from '@automattic/components';
-import { Hooray, Progress, SubTitle, Title, NextButton } from '@automattic/onboarding';
+import {
+	MigrationStatus,
+	type MigrationStatusError,
+	type SiteDetails,
+} from '@automattic/data-stores';
+import { Hooray, SubTitle, Title } from '@automattic/onboarding';
 import { createElement, createInterpolateElement } from '@wordpress/element';
-import { sprintf } from '@wordpress/i18n';
 import { localize } from 'i18n-calypso';
 import { get } from 'lodash';
 import { connect } from 'react-redux';
 import PreMigrationScreen from 'calypso/blocks/importer/wordpress/import-everything/pre-migration';
 import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
-import { EVERY_FIVE_SECONDS, EVERY_TEN_SECONDS, Interval } from 'calypso/lib/interval';
+import { EVERY_FIVE_SECONDS, Interval } from 'calypso/lib/interval';
 import { SectionMigrate } from 'calypso/my-sites/migrate/section-migrate';
 import { isMigrationTrialSite } from 'calypso/sites-dashboard/utils';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
@@ -18,12 +21,12 @@ import { getSite, getSiteAdminUrl, isJetpackSite } from 'calypso/state/sites/sel
 import { IAppState } from 'calypso/state/types';
 import DomainInfo from '../../components/domain-info';
 import DoneButton from '../../components/done-button';
-import GettingStartedVideo from '../../components/getting-started-video';
 import NotAuthorized from '../../components/not-authorized';
 import { isTargetSitePlanCompatible } from '../../util';
-import { MigrationStatus, WPImportOption } from '../types';
-import { retrieveMigrateSource, clearMigrateSource } from '../utils';
-import type { SiteDetails } from '@automattic/data-stores';
+import { WPImportOption, type MigrationState } from '../types';
+import { clearMigrateSource, retrieveMigrateSource } from '../utils';
+import MigrationError from './migration-error';
+import MigrationProgress from './migration-progress';
 import type { UrlData } from 'calypso/blocks/import/types';
 import type { StepNavigator } from 'calypso/blocks/importer/types';
 
@@ -38,17 +41,12 @@ interface Props {
 	sourceUrlAnalyzedData?: UrlData | null;
 }
 
-interface State {
-	migrationStatus: string;
-	percent: number | null;
-}
-
 type ExtraParams = {
 	[ key: string ]: any;
 };
 
 export class ImportEverything extends SectionMigrate {
-	componentDidUpdate( prevProps: any, prevState: State ) {
+	componentDidUpdate( prevProps: any, prevState: MigrationState ) {
 		super.componentDidUpdate( prevProps, prevState );
 		this.recordMigrationStatusChange( prevState );
 	}
@@ -78,7 +76,7 @@ export class ImportEverything extends SectionMigrate {
 			 */
 			this.setState(
 				{
-					migrationStatus: 'inactive',
+					migrationStatus: MigrationStatus.INACTIVE,
 					errorMessage: '',
 				},
 				this.updateFromAPI
@@ -102,7 +100,7 @@ export class ImportEverything extends SectionMigrate {
 		}
 	};
 
-	recordMigrationStatusChange = ( prevState: State ) => {
+	recordMigrationStatusChange = ( prevState: MigrationState ) => {
 		const trackEventProps = {
 			source_site_id: this.props.sourceSiteId,
 			source_site_url: this.props.sourceUrlAnalyzedData?.url,
@@ -205,52 +203,14 @@ export class ImportEverything extends SectionMigrate {
 	}
 
 	renderMigrationProgress() {
-		const { translate, sourceSite, targetSite } = this.props;
-
 		return (
 			<div className="import-layout__center">
-				<Progress>
-					<Interval onTick={ this.updateFromAPI } period={ EVERY_TEN_SECONDS } />
-					<div className="import__heading import__heading-center">
-						<Title>
-							{ ( MigrationStatus.BACKING_UP === this.state.migrationStatus ||
-								MigrationStatus.NEW === this.state.migrationStatus ) &&
-								sprintf( translate( 'Backing up %(website)s' ), { website: sourceSite.slug } ) +
-									'...' }
-							{ MigrationStatus.RESTORING === this.state.migrationStatus &&
-								sprintf( translate( 'Restoring to %(website)s' ), { website: targetSite.slug } ) +
-									'...' }
-						</Title>
-						<ProgressBar compact={ true } value={ this.state.percent ? this.state.percent : 0 } />
-						<SubTitle>
-							{ translate(
-								"This may take a few minutes. We'll notify you by email when it's done."
-							) }
-						</SubTitle>
-					</div>
-				</Progress>
-				<GettingStartedVideo />
-			</div>
-		);
-	}
-
-	renderMigrationProgressSimple() {
-		const { translate } = this.props;
-
-		return (
-			<div className="import-layout__center">
-				<Progress className="onboarding-progress-simple">
-					<Interval onTick={ this.updateFromAPI } period={ EVERY_TEN_SECONDS } />
-					<div className="import__heading import__heading-center">
-						<Title>{ translate( 'We’re safely migrating all your data' ) }</Title>
-						<ProgressBar compact={ true } value={ this.state.percent ? this.state.percent : 0 } />
-						<SubTitle tagName="h3">
-							{ translate(
-								'Feel free to close this window. We’ll email you when your new site is ready.'
-							) }
-						</SubTitle>
-					</div>
-				</Progress>
+				<MigrationProgress
+					status={ this.state.migrationStatus as MigrationStatus }
+					fetchStatus={ this.updateFromAPI }
+					// cast to unknown to avoid type error caused by passing state as props from jsx
+					details={ this.state as unknown as MigrationState }
+				/>
 			</div>
 		);
 	}
@@ -266,23 +226,19 @@ export class ImportEverything extends SectionMigrate {
 		);
 	}
 
-	renderMigrationError() {
-		const { translate } = this.props;
-
+	renderMigrationError( status: MigrationStatusError | null = null ) {
+		const { stepNavigator } = this.props;
 		return (
 			<div className="import-layout__center">
 				<div>
-					<div className="import__heading import__heading-center">
-						<Title>{ translate( 'Import failed' ) }</Title>
-						<SubTitle>
-							{ translate( 'There was an error with your import.' ) }
-							<br />
-							{ translate( 'Please try again soon or contact support for help.' ) }
-						</SubTitle>
-						<div className="import__buttons-group">
-							<NextButton onClick={ this.resetMigration }>{ translate( 'Try again' ) }</NextButton>
-						</div>
-					</div>
+					<MigrationError
+						sourceSiteUrl={ this.props.sourceSite?.URL }
+						targetSiteUrl={ this.props.targetSite.URL }
+						status={ status || this.state.migrationErrorStatus }
+						resetMigration={ this.resetMigration }
+						goToImportCapturePage={ () => stepNavigator?.goToImportCapturePage?.() }
+						goToImportContentOnlyPage={ () => stepNavigator?.goToImportContentOnlyPage?.() }
+					/>
 				</div>
 			</div>
 		);
@@ -344,6 +300,10 @@ export class ImportEverything extends SectionMigrate {
 	}
 
 	render() {
+		if ( this.props.forceError ) {
+			return this.renderMigrationError( this.props.forceError );
+		}
+
 		switch ( this.state.migrationStatus ) {
 			case MigrationStatus.UNKNOWN:
 				return this.renderLoading();
@@ -353,8 +313,9 @@ export class ImportEverything extends SectionMigrate {
 
 			case MigrationStatus.NEW:
 			case MigrationStatus.BACKING_UP:
+			case MigrationStatus.BACKING_UP_QUEUED:
 			case MigrationStatus.RESTORING:
-				return this.renderMigrationProgressSimple();
+				return this.renderMigrationProgress();
 
 			case MigrationStatus.DONE:
 				return this.renderMigrationComplete();
@@ -371,6 +332,8 @@ export class ImportEverything extends SectionMigrate {
 export const connector = connect(
 	( state: IAppState, ownProps: Partial< Props > ) => {
 		return {
+			// to test different error states, we can pass in a migrationStatus and force an error
+			forceError: get( getCurrentQueryArguments( state ), 'forceError', '' ),
 			isTargetSiteAtomic: !! isSiteAutomatedTransfer( state, ownProps.targetSiteId as number ),
 			isTargetSiteJetpack: !! isJetpackSite( state, ownProps.targetSiteId as number ),
 			isTargetSitePlanCompatible: !! isTargetSitePlanCompatible( ownProps.targetSite ),
