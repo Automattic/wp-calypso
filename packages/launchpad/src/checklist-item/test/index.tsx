293fc19fdf6c7df859c8b961f05744e48b9c48f4
Gabriel Caires <gabriel.caires@automattic.com>
2024-01-31 12:20:22 +0000

    Centralize the component render
    It enable us to have more flexibility to add/remove required props

--- packages/launchpad/src/checklist-item/test/index.tsx
+++ packages/launchpad/src/checklist-item/test/index.tsx
@@ -1,43 +1,50 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import ChecklistItem from '..';
+import ChecklistItem, { type Props } from '..';
 import { buildTask } from '../../test/lib/fixtures';
 import '@testing-library/jest-dom';
 
 describe( 'ChecklistItem', () => {
+	const defaultProps = { task: buildTask( {} ), onClick: jest.fn() };
+
+	const renderComponent = ( props: Partial< Props > ) => {
+		render( <ChecklistItem { ...defaultProps } { ...props } /> );
+	};
+
 	describe( 'when the task requires a badge', () => {
 		it( 'displays a badge', () => {
 			const badge_text = 'Badge Text';
-			render( <ChecklistItem task={ buildTask( { badge_text } ) } /> );
-			expect( screen.getByText( badge_text ) ).toBeTruthy();
+			renderComponent( { task: buildTask( { badge_text } ) } );
+			expect( screen.getByText( badge_text ) ).toBeVisible();
 		} );
 	} );
 
 	describe( 'when the task is completed', () => {
 		it( 'shows the task completed icon', () => {
-			render( <ChecklistItem task={ buildTask( { completed: true } ) } /> );
+			renderComponent( { task: buildTask( { completed: true } ) } );
 			const taskCompleteIcon = screen.queryByLabelText( 'Task complete' );
 			expect( taskCompleteIcon ).toBeTruthy();
 		} );
 		it( 'hides the task enabled icon', () => {
-			render( <ChecklistItem task={ buildTask( { completed: true } ) } /> );
+			renderComponent( { task: buildTask( { completed: true } ) } );
 			const taskEnabledIcon = screen.queryByLabelText( 'Task enabled' );
 			expect( taskEnabledIcon ).toBeFalsy();
 		} );
+
 		it( 'disables the task', () => {
-			render( <ChecklistItem task={ buildTask( { completed: true } ) } /> );
+			renderComponent( { task: buildTask( { completed: true } ) } );
 			const taskButton = screen.queryByRole( 'button' );
 			expect( taskButton ).toHaveAttribute( 'disabled' );
 		} );
 
 		describe( 'and the task is kept enabled', () => {
 			it( 'hides the task enabled icon', () => {
-				render( <ChecklistItem task={ buildTask( { completed: true, disabled: false } ) } /> );
+				renderComponent( { task: buildTask( { completed: true, disabled: false } ) } );
 				const taskEnabledIcon = screen.queryByLabelText( 'Task enabled' );
 				expect( taskEnabledIcon ).toBeFalsy();
 			} );
 			it( 'enables the task', () => {
-				render( <ChecklistItem task={ buildTask( { completed: true, disabled: false } ) } /> );
+				renderComponent( { task: buildTask( { completed: true, disabled: false } ) } );
 				const taskButton = screen.queryByRole( 'button' );
 				expect( taskButton ).not.toHaveAttribute( 'disabled' );
 			} );
@@ -46,7 +53,7 @@ describe( 'ChecklistItem', () => {
 
 	describe( 'when a task is incomplete', () => {
 		it( 'hides the task complete icon', () => {
-			render( <ChecklistItem task={ buildTask( { completed: false } ) } /> );
+			renderComponent( { task: buildTask( { completed: false } ) } );
 			const taskCompleteIcon = screen.queryByLabelText( 'Task complete' );
 			expect( taskCompleteIcon ).toBeFalsy();
 		} );
@@ -55,17 +62,15 @@ describe( 'ChecklistItem', () => {
 	describe( 'and the task depends on the completion of other tasks', () => {
 		it( 'hides the task enabled icon', () => {
 			const otherTaskCompleted = false;
-			render(
-				<ChecklistItem task={ buildTask( { completed: false, disabled: ! otherTaskCompleted } ) } />
-			);
+			renderComponent( { task: buildTask( { completed: true, disabled: ! otherTaskCompleted } ) } );
+
 			const taskEnabledIcon = screen.queryByLabelText( 'Task enabled' );
 			expect( taskEnabledIcon ).toBeFalsy();
 		} );
 		it( 'disables the task', () => {
 			const otherTaskCompleted = false;
-			render(
-				<ChecklistItem task={ buildTask( { completed: false, disabled: ! otherTaskCompleted } ) } />
-			);
+			renderComponent( { task: buildTask( { completed: true, disabled: ! otherTaskCompleted } ) } );
+
 			const taskButton = screen.queryByRole( 'button' );
 			expect( taskButton ).toHaveAttribute( 'disabled' );
 		} );
@@ -73,12 +78,12 @@ describe( 'ChecklistItem', () => {
 
 	describe( 'and the task does not depend on the completion of other tasks', () => {
 		it( 'shows the task enabled icon', () => {
-			render( <ChecklistItem task={ buildTask( { completed: false, disabled: false } ) } /> );
+			renderComponent( { task: buildTask( { completed: false, disabled: false } ) } );
 			const taskEnabledIcon = screen.queryByLabelText( 'Task enabled' );
 			expect( taskEnabledIcon ).toBeTruthy();
 		} );
 		it( 'enables the task', () => {
-			render( <ChecklistItem task={ buildTask( { completed: false, disabled: false } ) } /> );
+			renderComponent( { task: buildTask( { completed: false, disabled: false } ) } );
 			const taskButton = screen.queryByRole( 'button' );
 			expect( taskButton ).not.toHaveAttribute( 'disabled' );
 		} );
@@ -86,9 +91,7 @@ describe( 'ChecklistItem', () => {
 
 	describe( 'when a task is a primary action', () => {
 		it( 'displays a primary button', () => {
-			render(
-				<ChecklistItem task={ buildTask( { completed: false } ) } isPrimaryAction={ true } />
-			);
+			renderComponent( { isPrimaryAction: true } );
 			const taskButton = screen.queryByRole( 'button' );
 			expect( taskButton?.className ).toContain( 'checklist-item__checklist-primary-button' );
 		} );
