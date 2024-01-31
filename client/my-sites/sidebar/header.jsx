a98108a189a114e559fb14dc735e0440abfb9478
Eoin Gallagher <eoin@automattic.com>
2024-01-31 14:47:22 +0000

    refactor sidebar menu items into their own components

--- client/my-sites/sidebar/header.jsx
+++ client/my-sites/sidebar/header.jsx
@@ -1,4 +1,9 @@
+import { useTranslate } from 'i18n-calypso';
+import AsyncLoad from 'calypso/components/async-load';
+import SidebarNotifications from 'calypso/my-sites/sidebar/menu-items/notifications/notifications';
+
 export const MySitesSidebarUnifiedHeader = () => {
+	const translate = useTranslate();
 	return (
 		<div className="sidebar__header">
 			<a href="/sites" className="link-logo">
@@ -6,15 +11,24 @@ export const MySitesSidebarUnifiedHeader = () => {
 				<span className="dotcom"></span>
 			</a>
 			<span className="gap"></span>
-			<a href="#" className="link-search">
-				<span className="search"></span>
-			</a>
-			<a href="#" className="link-help">
-				<span className="help"></span>
-			</a>
-			<a href="#" className="link-notifications">
-				<span className="bell"></span>
-			</a>
+			<button className="search" />
+			<AsyncLoad
+				require="./menu-items/help-center"
+				tooltip={ translate( 'Help' ) }
+				placeholder={
+					<div className="link-help">
+						<span className="help"></span>
+					</div>
+				}
+			/>
+			<SidebarNotifications
+				isShowing={ false }
+				isActive={ true }
+				className="sidebar__item-notifications"
+				tooltip={ translate( 'Manage your notifications' ) }
+			>
+				<span className="sidebar__item-notifications-label">{ translate( 'Notifications' ) }</span>
+			</SidebarNotifications>
 		</div>
 	);
 };
