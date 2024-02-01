795bb1fb29ab7cd6c52aaeb607352156880b1366
Karen Attfield <karenlattfield@gmail.com>
2024-02-01 13:16:30 +0000

    Jetpack Manage: Pricing page -adding a more about link leading to a LicenseLightbox (#87050)

--- client/jetpack-cloud/sections/manage/pricing/license-products-list/featured-license-multi-item-card.tsx
+++ client/jetpack-cloud/sections/manage/pricing/license-products-list/featured-license-multi-item-card.tsx
@@ -1,16 +1,24 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
+import page from '@automattic/calypso-router';
 import { Button } from '@automattic/components';
 import 'calypso/my-sites/plans/jetpack-plans/product-store/featured-item-card/style.scss';
+import { addQueryArgs } from '@wordpress/url';
 import { useTranslate } from 'i18n-calypso';
 import { useCallback, useState } from 'react';
+import { useSelector } from 'react-redux';
 import MultipleChoiceQuestion from 'calypso/components/multiple-choice-question';
 import { useProductDescription } from 'calypso/jetpack-cloud/sections/partner-portal/hooks';
+import { useURLQueryParams } from 'calypso/jetpack-cloud/sections/partner-portal/hooks/index';
+import { LICENSE_INFO_MODAL_ID } from 'calypso/jetpack-cloud/sections/partner-portal/lib';
 import getProductShortTitle from 'calypso/jetpack-cloud/sections/partner-portal/lib/get-product-short-title';
 import getProductVariantShortTitle from 'calypso/jetpack-cloud/sections/partner-portal/lib/get-product-variant-short-title';
+import LicenseLightbox from 'calypso/jetpack-cloud/sections/partner-portal/license-lightbox/index';
+import LicenseLightboxLink from 'calypso/jetpack-cloud/sections/partner-portal/license-lightbox-link/index';
 import { HeroImageAPIFamily } from 'calypso/my-sites/plans/jetpack-plans/product-store/hero-image';
 import { useDispatch } from 'calypso/state';
+import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
+import { isAgencyUser } from 'calypso/state/partner-portal/partner/selectors';
 import { APIProductFamilyProduct } from 'calypso/state/partner-portal/types';
-import GetIssueLicenseURL from './get-issue-license-url';
 import { ItemPrice } from './item-price';
 import './style.scss';
 
@@ -18,7 +26,6 @@ type FeaturedLicenseMultiItemCardProps = {
 	variants: APIProductFamilyProduct[];
 	bundleSize?: number;
 	ctaAsPrimary?: boolean;
-	moreInfoLink?: React.ReactNode;
 	isCondensedVersion?: boolean;
 	isCtaDisabled?: boolean;
 	isCtaExternal?: boolean;
@@ -29,22 +36,27 @@ export const FeaturedLicenseMultiItemCard = ( {
 	variants,
 	bundleSize,
 	ctaAsPrimary,
-	moreInfoLink,
 	isCtaDisabled,
 	isCtaExternal,
 	onClickCta,
 }: FeaturedLicenseMultiItemCardProps ) => {
 	const translate = useTranslate();
 	const dispatch = useDispatch();
-
+	const isLoggedIn = useSelector( isUserLoggedIn );
+	const isAgency = useSelector( isAgencyUser );
 	const [ variant, setVariant ] = useState( variants[ 0 ] );
+	const { setParams, resetParams, getParamValue } = useURLQueryParams();
+	const modalParamValue = getParamValue( LICENSE_INFO_MODAL_ID );
 	const variantSlug = variant.slug;
+
+	const [ showLightbox, setShowLightbox ] = useState( modalParamValue === variantSlug );
+
 	const title = getProductShortTitle( variant, true );
 	const ctaLabel = translate( 'Get' );
 	const ctaAriaLabel = ctaLabel + ' ' + variant.name;
 
 	const price = <ItemPrice bundleSize={ bundleSize } item={ variant } />;
-	const { description: productDescription } = useProductDescription( variant.slug );
+	const { description: productDescription } = useProductDescription( variantSlug );
 	const hero = <HeroImageAPIFamily item={ variant } />;
 
 	const onChangeOption = useCallback(
@@ -67,39 +79,102 @@ export const FeaturedLicenseMultiItemCard = ( {
 		answerText: getProductVariantShortTitle( option.name ),
 	} ) );
 
-	return (
-		<div className="featured-item-card">
-			<div className="featured-item-card--hero">{ hero }</div>
+	const onShowLightbox = useCallback(
+		( e: React.MouseEvent< HTMLElement > ) => {
+			e.stopPropagation();
 
-			<div className="featured-item-card--body">
-				<div>
-					<h3 className="featured-item-card--title">{ title }</h3>
-					<MultipleChoiceQuestion
-						name={ `${ variant.family_slug }-variant-options` }
-						question={ translate( 'Select variant:' ) }
-						answers={ variantOptions }
-						selectedAnswerId={ variantSlug }
-						onAnswerChange={ onChangeOption }
-						shouldShuffleAnswers={ false }
-					/>
-					<div className="featured-item-card--price">{ price }</div>
-					<div className="featured-item-card--desc">{ productDescription }</div>
-				</div>
-				<div className="featured-item-card--footer">
-					{ moreInfoLink }
-					<Button
-						className="featured-item-card--cta"
-						primary={ ctaAsPrimary }
-						onClick={ onClickCta }
-						disabled={ isCtaDisabled }
-						target={ isCtaExternal ? '_blank' : undefined }
-						href={ isCtaDisabled ? '#' : GetIssueLicenseURL( variantSlug, bundleSize ) }
-						aria-label={ ctaAriaLabel }
-					>
-						{ ctaLabel }
-					</Button>
+			setParams( [
+				{
+					key: LICENSE_INFO_MODAL_ID,
+					value: variantSlug,
+				},
+			] );
+			setShowLightbox( true );
+			dispatch(
+				recordTracksEvent( 'calypso_jetpack_manage_more_about_product_view', {
+					product: variantSlug,
+				} )
+			);
+		},
+		[ dispatch, variantSlug, setParams ]
+	);
+
+	const getIssueLicenseURL = useCallback(
+		( variantSlug: string, bundleSize: number | undefined ) => {
+			if ( isLoggedIn && ! isAgency ) {
+				return addQueryArgs( `/manage/signup/`, {
+					product_slug: variantSlug,
+					source: 'manage-pricing-page',
+					bundle_size: bundleSize,
+				} );
+			}
+			return addQueryArgs( `/partner-portal/issue-license/`, {
+				product_slug: variantSlug,
+				source: 'manage-pricing-page',
+				bundle_size: bundleSize,
+			} );
+		},
+		[ isLoggedIn, isAgency ]
+	);
+
+	const onSelectProduct = useCallback( () => {
+		page( getIssueLicenseURL( variantSlug, bundleSize ) );
+	}, [ bundleSize, getIssueLicenseURL, variantSlug ] );
+
+	const onHideLightbox = useCallback( () => {
+		resetParams( [ LICENSE_INFO_MODAL_ID ] );
+		setShowLightbox( false );
+	}, [ resetParams ] );
+
+	const moreInfoLink = <LicenseLightboxLink productName={ title } onClick={ onShowLightbox } />;
+
+	return (
+		<>
+			<div className="featured-item-card">
+				<div className="featured-item-card--hero">{ hero }</div>
+
+				<div className="featured-item-card--body">
+					<div>
+						<h3 className="featured-item-card--title">{ title }</h3>
+						<MultipleChoiceQuestion
+							name={ `${ variant.family_slug }-variant-options` }
+							question={ translate( 'Select variant:' ) }
+							answers={ variantOptions }
+							selectedAnswerId={ variant.slug }
+							onAnswerChange={ onChangeOption }
+							shouldShuffleAnswers={ false }
+						/>
+						<div className="featured-item-card--price">{ price }</div>
+						<div className="featured-item-card--desc">{ productDescription }</div>
+						{ moreInfoLink }
+					</div>
+					<div className="featured-item-card--footer">
+						<Button
+							className="featured-item-card--cta"
+							primary={ ctaAsPrimary }
+							onClick={ onClickCta }
+							disabled={ isCtaDisabled }
+							target={ isCtaExternal ? '_blank' : undefined }
+							href={ isCtaDisabled ? '#' : getIssueLicenseURL( variantSlug, bundleSize ) }
+							aria-label={ ctaAriaLabel }
+						>
+							{ ctaLabel }
+						</Button>
+					</div>
 				</div>
 			</div>
-		</div>
+			{ showLightbox && (
+				<LicenseLightbox
+					product={ variant }
+					quantity={ bundleSize }
+					ctaLabel={ translate( 'Select License' ) }
+					isCTAExternalLink={ false }
+					isCTAPrimary={ true }
+					isDisabled={ false }
+					onActivate={ onSelectProduct }
+					onClose={ onHideLightbox }
+				/>
+			) }
+		</>
 	);
 };
