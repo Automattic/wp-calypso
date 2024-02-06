ab424f24dcdfaa79d854606fb40d6ecab366f513
Igor Giussani <igor.giussani@automattic.com>
2024-02-06 11:17:21 +0100

    Allow subdomains in glue records (#87191)
    * Allow subdomains in glue records
    
    * Add length check

--- client/my-sites/domains/domain-management/settings/cards/glue-records-card.tsx
+++ client/my-sites/domains/domain-management/settings/cards/glue-records-card.tsx
@@ -164,7 +164,12 @@ export default function GlueRecordsCard( { domain }: { domain: ResponseDomain } 
 		if ( ! nameserver ) {
 			return false;
 		}
-		if ( ! nameserver.match( /^[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?$/ ) ) {
+		if (
+			nameserver.length > 50 ||
+			! nameserver.match(
+				/^([A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)(\.[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/
+			)
+		) {
 			return false;
 		}
 		return true;
