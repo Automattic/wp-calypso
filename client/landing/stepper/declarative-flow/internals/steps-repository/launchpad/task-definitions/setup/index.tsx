a37f790f782aa773af30a57f02970b0afed75755
Gabriel Caires <gabriel.caires@automattic.com>
2024-02-06 13:07:47 +0000

    Revert "Revert "Launchpad: Clean up task-helper (#86960)" (#87204)"
    This reverts commit 144e30d0777abde8537db44f8720f55a671ef07c.

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/setup/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/setup/index.tsx
@@ -1,40 +1,43 @@
 import { type Task } from '@automattic/launchpad';
 import { isBlogOnboardingFlow } from '@automattic/onboarding';
 import { addQueryArgs } from '@wordpress/url';
+import { getSiteIdOrSlug } from '../../task-helper';
 import { recordTaskClickTracksEvent } from '../../tracking';
 import { type TaskAction } from '../../types';
 
 export const getSetupFreeTask: TaskAction = ( task, flow, context ): Task => {
-	const { siteInfoQueryArgs } = context;
+	const { site, siteSlug } = context;
 
 	return {
 		...task,
 		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
-		calypso_path: addQueryArgs( `/setup/${ flow }/freePostSetup`, siteInfoQueryArgs ),
+		calypso_path: addQueryArgs( `/setup/${ flow }/freePostSetup`, {
+			...getSiteIdOrSlug( flow, site, siteSlug ),
+		} ),
 		useCalypsoPath: true,
 	};
 };
 
 export const getSetupBlogTask: TaskAction = ( task, flow, context ): Task => {
-	const { siteInfoQueryArgs } = context;
+	const { site, siteSlug } = context;
 
 	return {
 		...task,
 		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
-		calypso_path: addQueryArgs( task.calypso_path, siteInfoQueryArgs ),
+		calypso_path: addQueryArgs( task.calypso_path, { ...getSiteIdOrSlug( flow, site, siteSlug ) } ),
 		disabled: task.completed && ! isBlogOnboardingFlow( flow ),
 		useCalypsoPath: true,
 	};
 };
 
 export const getSetupNewsletterTask: TaskAction = ( task, flow, context ): Task => {
-	const { siteInfoQueryArgs } = context;
+	const { site, siteSlug } = context;
 
 	return {
 		...task,
 		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
 		calypso_path: addQueryArgs( task.calypso_path, {
-			...siteInfoQueryArgs,
+			...getSiteIdOrSlug( flow, site, siteSlug ),
 			flowToReturnTo: flow,
 		} ),
 		useCalypsoPath: true,
@@ -42,25 +45,25 @@ export const getSetupNewsletterTask: TaskAction = ( task, flow, context ): Task 
 };
 
 export const getSetupVideoPressTask: TaskAction = ( task, flow, context ): Task => {
-	const { siteInfoQueryArgs } = context;
+	const { site, siteSlug } = context;
 
 	return {
 		...task,
 		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
-		calypso_path: addQueryArgs( task.calypso_path, siteInfoQueryArgs ),
+		calypso_path: addQueryArgs( task.calypso_path, { ...getSiteIdOrSlug( flow, site, siteSlug ) } ),
 		useCalypsoPath: true,
 	};
 };
 
 export const getSetupGeneralTask: TaskAction = ( task, flow, context ): Task => {
-	const { siteInfoQueryArgs } = context;
+	const { site, siteSlug } = context;
 
 	return {
 		...task,
 		disabled: false,
 		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
 		calypso_path: addQueryArgs( `/setup/update-options/options`, {
-			...siteInfoQueryArgs,
+			...getSiteIdOrSlug( flow, site, siteSlug ),
 			flowToReturnTo: flow,
 		} ),
 		useCalypsoPath: true,
