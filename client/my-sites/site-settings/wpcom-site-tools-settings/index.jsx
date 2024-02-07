fc2971762c927f917a9cd6488f30feaa68873b59
Candy Tsai <candy02058912@gmail.com>
2024-02-08 05:45:28 +0800

    Back buttons goes back to correct settings page for transfer, reset, delete site pages (#87254)

--- client/my-sites/site-settings/wpcom-site-tools-settings/index.jsx
+++ client/my-sites/site-settings/wpcom-site-tools-settings/index.jsx
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 import isSiteWpcomStaging from 'calypso/state/selectors/is-site-wpcom-staging';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import SiteTools from '../site-tools';
+import { SOURCE_SETTINGS_SITE_TOOLS } from '../site-tools/utils';
 
 const SiteSettingsGeneral = ( { isWpcomStagingSite } ) => (
 	<div className="site-settings__main general-settings">
-		{ ! isWpcomStagingSite && <SiteTools headerTitle={ translate( 'Other tools' ) } /> }
+		{ ! isWpcomStagingSite && (
+			<SiteTools headerTitle={ translate( 'Other tools' ) } source={ SOURCE_SETTINGS_SITE_TOOLS } />
+		) }
 	</div>
 );
 
