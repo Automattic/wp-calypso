3d1297cfba8fbd275ca6dac24ff5be794d8aa910
tbradsha <32492176+tbradsha@users.noreply.github.com>
2024-02-07 11:26:23 -0700

    Add new language route

--- client/jetpack-cloud/sections/manage/pricing/index.js
+++ client/jetpack-cloud/sections/manage/pricing/index.js
@@ -1,11 +1,20 @@
 import page from '@automattic/calypso-router';
 import { makeLayout, render as clientRender } from 'calypso/controller';
+import { setLocaleMiddleware } from 'calypso/controller/shared';
 import { jetpackManagePricingPath } from 'calypso/lib/jetpack/paths';
 import * as controller from './controller';
 
 export default function () {
 	page(
 		jetpackManagePricingPath(),
+		controller.jetpackManagePricingContext,
+		makeLayout,
+		clientRender
+	);
+
+	page(
+		`/:lang` + jetpackManagePricingPath(),
+		setLocaleMiddleware(),
 		controller.jetpackManagePricingContext,
 		makeLayout,
 		clientRender
