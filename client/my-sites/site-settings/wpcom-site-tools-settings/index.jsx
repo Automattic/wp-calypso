a77fb4a2b53a4f1a7fb3b84a88bce315c4c5bae8
Candy Tsai <candy02058912@gmail.com>
2024-02-07 19:04:18 +0800

    Modularize Launch Site and add Launch Site button to Site Tools

--- client/my-sites/site-settings/wpcom-site-tools-settings/index.jsx
+++ client/my-sites/site-settings/wpcom-site-tools-settings/index.jsx
@@ -1,11 +1,22 @@
 import { translate } from 'i18n-calypso';
 import { connect } from 'react-redux';
+import isAtomicSite from 'calypso/state/selectors/is-site-automated-transfer';
 import isSiteWpcomStaging from 'calypso/state/selectors/is-site-wpcom-staging';
+import getIsUnlaunchedSite from 'calypso/state/selectors/is-unlaunched-site';
+import { getSiteOption } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId } from 'calypso/state/ui/selectors';
 import SiteTools from '../site-tools';
+import LaunchSite from '../site-visibility/launch-site';
 
-const SiteSettingsGeneral = ( { isWpcomStagingSite } ) => (
+const SiteSettingsGeneral = ( {
+	isWpcomStagingSite,
+	isAtomicAndEditingToolkitDeactivated,
+	isUnlaunchedSite,
+} ) => (
 	<div className="site-settings__main general-settings">
+		{ isUnlaunchedSite && ! isAtomicAndEditingToolkitDeactivated && ! isWpcomStagingSite && (
+			<LaunchSite />
+		) }
 		{ ! isWpcomStagingSite && <SiteTools headerTitle={ translate( 'Other tools' ) } /> }
 	</div>
 );
@@ -14,5 +25,9 @@ export default connect( ( state ) => {
 	const siteId = getSelectedSiteId( state );
 	return {
 		isWpcomStagingSite: isSiteWpcomStaging( state, siteId ),
+		isAtomicAndEditingToolkitDeactivated:
+			isAtomicSite( state, siteId ) &&
+			getSiteOption( state, siteId, 'editing_toolkit_is_active' ) === false,
+		isUnlaunchedSite: getIsUnlaunchedSite( state, siteId ),
 	};
 } )( SiteSettingsGeneral );
