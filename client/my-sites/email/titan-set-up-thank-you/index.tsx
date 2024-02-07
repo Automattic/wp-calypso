a03c124a4af5e77ef3a0912baf28e82ef844dafa
Volodymyr Makukha <nei.css@gmail.com>
2024-02-07 12:48:32 +0000

    fix: page wo mailboxes

--- client/my-sites/email/titan-set-up-thank-you/index.tsx
+++ client/my-sites/email/titan-set-up-thank-you/index.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import ThankYouV2 from 'calypso/components/thank-you-v2';
 import { TITAN_CONTROL_PANEL_CONTEXT_GET_MOBILE_APP } from 'calypso/lib/titan/constants';
 import { ThankYouEmailProduct } from 'calypso/my-sites/checkout/checkout-thank-you/redesign-v2/products/email-product';
 import { recordEmailAppLaunchEvent } from 'calypso/my-sites/email/email-management/home/utils';
-import { getTitanControlPanelRedirectPath } from 'calypso/my-sites/email/paths';
+import {
+	getTitanControlPanelRedirectPath,
+	getTitanSetUpMailboxPath,
+} from 'calypso/my-sites/email/paths';
 import { useSelector } from 'calypso/state';
 import getCurrentRoute from 'calypso/state/selectors/get-current-route';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
@@ -66,19 +70,43 @@ const TitanSetUpThankYou = ( {
 		},
 	];
 
-	const products = ! emailAddress ? undefined : (
-		<ThankYouEmailProduct
-			domainName={ domainName }
-			siteSlug={ selectedSiteSlug }
-			emailAddress={ emailAddress }
-		/>
-	);
+	let title;
+	let subtitle;
+	let headerButtons;
+	let products;
+
+	if ( emailAddress ) {
+		title = translate( 'Say hello to your new email address' );
+		subtitle = translate( "All set! Now it's time to update your contact details." );
+		products = (
+			<ThankYouEmailProduct
+				domainName={ domainName }
+				siteSlug={ selectedSiteSlug }
+				emailAddress={ emailAddress }
+			/>
+		);
+	} else {
+		title = translate( 'Almost done, set up your Professional Email' );
+		subtitle = translate(
+			'Complete your professional email setup to start sending and receiving emails from your custom domain today.'
+		);
+		headerButtons = (
+			<Button
+				href={ getTitanSetUpMailboxPath( selectedSiteSlug, domainName ) }
+				className="manage-all-domains22"
+				variant="primary"
+			>
+				{ translate( 'Set up mailbox' ) }
+			</Button>
+		);
+	}
 
 	return (
 		<>
 			<ThankYouV2
-				title={ translate( 'Say hello to your new email address' ) }
-				subtitle={ translate( "All set! Now it's time to update your contact details." ) }
+				title={ title }
+				subtitle={ subtitle }
+				headerButtons={ headerButtons }
 				products={ products }
 				footerDetails={ footerDetails }
 			/>
