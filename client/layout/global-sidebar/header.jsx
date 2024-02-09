b2f90d206da0cc82cbc447d34792c749a914cf6b
Eoin Gallagher <eoin@automattic.com>
2024-02-09 11:59:24 +0000

    remove logo svg as not needed

--- client/layout/global-sidebar/header.jsx
+++ client/layout/global-sidebar/header.jsx
@@ -6,7 +6,6 @@ export const GlobalSidebarHeader = () => {
 	return (
 		<div className="sidebar__header">
 			<a href="/sites" className="link-logo">
-				<span className="logo"></span>
 				<span className="dotcom"></span>
 			</a>
 			<span className="gap"></span>
