85535662bf847336a6b38b4f412b124332ff9562
Louis Laugesen <louis.laugesen@gmail.com>
2024-02-09 20:36:45 +1100

    Menu

--- client/my-sites/sidebar/static-data/fallback-menu.js
+++ client/my-sites/sidebar/static-data/fallback-menu.js
@@ -41,20 +41,14 @@ export default function buildFallbackResponse( {
 	showSiteMonitoring = false,
 	showGithubDeployments = false,
 } = {} ) {
-	let mailboxes = [];
-	if ( shouldShowMailboxes ) {
-		mailboxes = [
-			{
-				icon: 'dashicons-email',
-				slug: 'mailboxes',
-				title: translate( 'My Mailboxes' ),
-				type: 'menu-item',
-				url: `/mailboxes/${ siteDomain }`,
-			},
-		];
-	}
-
 	const fallbackResponse = [
+		{
+			icon: 'dashicons-dashboard',
+			slug: 'wp-admin',
+			title: translate( 'Dashboard' ),
+			type: 'menu-item',
+			url: `https://${ siteDomain }/wp-admin/`,
+		},
 		{
 			icon: 'dashicons-admin-home',
 			slug: 'home',
@@ -63,104 +57,97 @@ export default function buildFallbackResponse( {
 			url: `/home/${ siteDomain }`,
 		},
 		{
-			icon: 'dashicons-chart-bar',
-			slug: 'stats',
-			title: translate( 'Stats' ),
+			icon: JETPACK_ICON,
+			slug: 'jetpack',
+			title: translate( 'Jetpack' ),
 			type: 'menu-item',
-			url: `/stats/day/${ siteDomain }`,
-		},
-		{
-			icon: 'dashicons-cart',
-			slug: 'upgrades',
-			title: translate( 'Upgrades' ),
-			type: 'menu-item',
-			url: `/plans/${ siteDomain }`,
+			url: `/activity-log/${ siteDomain }`,
 			children: [
 				{
-					parent: 'upgrades',
-					slug: 'Plans',
-					title: translate( 'Plans' ),
+					parent: 'jetpack',
+					slug: 'jetpack-activity-log',
+					title: translate( 'Activity Log' ),
 					type: 'submenu-item',
-					url: `/plans/${ siteDomain }`,
+					url: `/activity-log/${ siteDomain }`,
 				},
-				...( shouldShowAddOns
+				{
+					parent: 'jetpack',
+					slug: 'jetpack-backup',
+					title: translate( 'VaultPress Backup' ),
+					type: 'submenu-item',
+					url: `/backup/${ siteDomain }`,
+				},
+				{
+					parent: 'jetpack',
+					slug: 'subscribers',
+					title: translate( 'Subscribers' ),
+					type: 'submenu-item',
+					url: `/subscribers/${ siteDomain }`,
+				},
+				{
+					parent: 'jetpack',
+					slug: 'tools-earn',
+					title: translate( 'Monetize' ),
+					type: 'menu-item',
+					url: `/earn/${ siteDomain }`,
+				},
+				...( config.isEnabled( 'settings/newsletter-settings-page' )
 					? [
 							{
-								parent: 'upgrades',
-								slug: 'Add-Ons',
-								title: translate( 'Add-Ons' ),
+								parent: 'jetpack',
+								slug: 'options-newsletter-php',
+								title: translate( 'Newsletter' ),
 								type: 'submenu-item',
-								url: `/add-ons/${ siteDomain }`,
+								url: `/settings/newsletter/${ siteDomain }`,
 							},
 					  ]
 					: [] ),
 				{
-					parent: 'upgrades',
-					slug: 'Domains',
-					title: translate( 'Domains' ),
+					parent: 'jetpack',
+					slug: 'options-podcasting-php',
+					title: translate( 'Podcasting' ),
 					type: 'submenu-item',
-					url: `/domains/manage/${ siteDomain }`,
-				},
-				{
-					parent: 'upgrades',
-					slug: 'Emails',
-					title: translate( 'Emails' ),
-					type: 'submenu-item',
-					url: `/email/${ siteDomain }`,
-				},
-				{
-					parent: 'upgrades',
-					slug: 'Purchases',
-					title: translate( 'Purchases' ),
-					type: 'submenu-item',
-					url: `/purchases/subscriptions/${ siteDomain }`,
-				},
-			],
-		},
-		...mailboxes,
-		{
-			icon: 'dashicons-admin-post',
-			slug: 'edit-php',
-			title: translate( 'Posts' ),
-			type: 'menu-item',
-			url: `/posts/${ siteDomain }`,
-			children: [
-				{
-					parent: 'edit.php',
-					slug: 'edit-php',
-					title: translate( 'All Posts' ),
-					type: 'submenu-item',
-					url: `/posts/${ siteDomain }`,
-				},
-				{
-					parent: 'edit.php',
-					slug: 'post-new-php',
-					title: translate( 'Add New', { context: 'post' } ),
-					type: 'submenu-item',
-					url: `/post/${ siteDomain }`,
-				},
-				{
-					parent: 'edit.php',
-					slug: 'edit-tags-phptaxonomycategory',
-					title: translate( 'Categories' ),
-					type: 'submenu-item',
-					url: `/settings/taxonomies/category/${ siteDomain }`,
-				},
-				{
-					parent: 'edit.php',
-					slug: 'edit-tags-phptaxonomypost_tag',
-					title: translate( 'Tags' ),
-					type: 'submenu-item',
-					url: `/settings/taxonomies/post_tag/${ siteDomain }`,
+					url: `/settings/podcasting/${ siteDomain }`,
 				},
 			],
 		},
 		{
-			icon: 'dashicons-admin-media',
-			slug: 'upload-php',
-			title: translate( 'Media' ),
-			type: 'menu-item',
-			url: `/media/${ siteDomain }`,
+			slug: 'Plans',
+			title: translate( 'Plans' ),
+			type: 'submenu-item',
+			icon: 'dashicons-cart',
+			url: `/plans/${ siteDomain }`,
+		},
+		...( shouldShowAddOns
+			? [
+					{
+						slug: 'Add-Ons',
+						title: translate( 'Add-Ons' ),
+						type: 'submenu-item',
+						url: `/add-ons/${ siteDomain }`,
+					},
+			  ]
+			: [] ),
+		{
+			slug: 'Domains',
+			title: translate( 'Domains' ),
+			type: 'submenu-item',
+			icon: 'dashicons-admin-site',
+			url: `/domains/manage/${ siteDomain }`,
+		},
+		{
+			slug: 'Emails',
+			title: translate( 'Emails' ),
+			type: 'submenu-item',
+			url: `/email/${ siteDomain }`,
+			icon: 'dashicons-email',
+		},
+		{
+			slug: 'Purchases',
+			title: translate( 'Purchases' ),
+			type: 'submenu-item',
+			icon: 'dashicons-money-alt',
+			url: `/purchases/subscriptions/${ siteDomain }`,
 		},
 		...( shouldShowLinks
 			? [
@@ -197,241 +184,12 @@ export default function buildFallbackResponse( {
 			  ]
 			: [] ),
 		{
-			icon: 'dashicons-admin-page',
-			slug: 'edit-phppost_typepage',
-			title: translate( 'Pages' ),
-			type: 'menu-item',
-			url: `/pages/${ siteDomain }`,
-			children: [
-				{
-					parent: 'edit.php?post_type=page',
-					slug: 'edit-phppost_typepage',
-					title: translate( 'All Pages' ),
-					type: 'submenu-item',
-					url: `/pages/${ siteDomain }`,
-				},
-				{
-					parent: 'edit.php?post_type=page',
-					slug: 'post-new-phppost_typepage',
-					title: translate( 'Add New', { context: 'page' } ),
-					type: 'submenu-item',
-					url: `/page/${ siteDomain }`,
-				},
-			],
+			slug: 'themes-php',
+			title: translate( 'Themes' ),
+			type: 'submenu-item',
+			url: `/themes/${ siteDomain }`,
+			icon: 'dashicons-admin-appearance',
 		},
-		...( shouldShowTestimonials
-			? [
-					{
-						icon: 'dashicons-admin-links',
-						slug: 'testimonials',
-						title: translate( 'Testimonials' ),
-						type: 'menu-item',
-						url: `/types/jetpack-testimonial/${ siteDomain }`,
-						children: [
-							{
-								parent: 'testimonials',
-								slug: 'testimonials',
-								title: translate( 'All Testimonials' ),
-								type: 'submenu-item',
-								url: `/types/jetpack-testimonial/${ siteDomain }`,
-							},
-							{
-								parent: 'testimonials',
-								slug: 'testimonials-add',
-								title: translate( 'Add New', { context: 'testimonial' } ),
-								type: 'submenu-item',
-								url: `/edit/jetpack-testimonial/${ siteDomain }`,
-							},
-						],
-					},
-			  ]
-			: [] ),
-		...( shouldShowPortfolio
-			? [
-					{
-						icon: 'dashicons-admin-links',
-						slug: 'portfolio',
-						title: translate( 'Portfolio' ),
-						type: 'menu-item',
-						url: `/types/jetpack-portfolio/${ siteDomain }`,
-						children: [
-							{
-								parent: 'portfolio',
-								slug: 'portfolio',
-								title: translate( 'All Projects' ),
-								type: 'submenu-item',
-								url: `/types/jetpack-portfolio/${ siteDomain }`,
-							},
-							{
-								parent: 'portfolio',
-								slug: 'portfolio-add',
-								title: translate( 'Add New', { context: 'portfolio' } ),
-								type: 'submenu-item',
-								url: `/edit/jetpack-portfolio/${ siteDomain }`,
-							},
-							{
-								parent: 'portfolio',
-								slug: 'portfolio-types',
-								title: translate( 'Project Types' ),
-								type: 'submenu-item',
-								url: `https://${ siteDomain }/wp-admin/edit-tags.php?taxonomy=jetpack-portfolio-type&post_type=jetpack-portfolio`,
-							},
-							{
-								parent: 'portfolio',
-								slug: 'portfolio-tags',
-								title: translate( 'Project Tags' ),
-								type: 'submenu-item',
-								url: `https://${ siteDomain }/wp-admin/edit-tags.php?taxonomy=jetpack-portfolio-tag&post_type=jetpack-portfolio`,
-							},
-						],
-					},
-			  ]
-			: [] ),
-		{
-			icon: 'dashicons-admin-comments',
-			slug: 'edit-comments-php',
-			title: translate( 'Comments' ),
-			type: 'menu-item',
-			url: `/comments/all/${ siteDomain }`,
-		},
-		{
-			icon: 'dashicons-feedback',
-			slug: 'feedback',
-			title: translate( 'Feedback' ),
-			type: 'menu-item',
-			url: `https://${ siteDomain }/wp-admin/?page=feedback`,
-			children: [
-				{
-					parent: 'feedback',
-					slug: 'edit-phppost_typefeedback',
-					title: translate( 'Feedback' ),
-					type: 'submenu-item',
-					url: `https://${ siteDomain }/wp-admin/edit.php?post_type=feedback`,
-				},
-				{
-					parent: 'feedback',
-					slug: 'polls',
-					title: translate( 'Polls' ),
-					type: 'submenu-item',
-					url: `https://${ siteDomain }/wp-admin/admin.php?page=polls`,
-				},
-				{
-					parent: 'feedback',
-					slug: 'ratings',
-					title: translate( 'Ratings' ),
-					type: 'submenu-item',
-					url: `https://${ siteDomain }/wp-admin/admin.php?page=ratings`,
-				},
-			],
-		},
-		{
-			type: 'separator',
-		},
-		{
-			icon: JETPACK_ICON,
-			slug: 'jetpack',
-			title: translate( 'Jetpack' ),
-			type: 'menu-item',
-			url: `/activity-log/${ siteDomain }`,
-			children: [
-				{
-					parent: 'jetpack',
-					slug: 'jetpack-activity-log',
-					title: translate( 'Activity Log' ),
-					type: 'submenu-item',
-					url: `/activity-log/${ siteDomain }`,
-				},
-				{
-					parent: 'jetpack',
-					slug: 'jetpack-backup',
-					title: translate( 'VaultPress Backup' ),
-					type: 'submenu-item',
-					url: `/backup/${ siteDomain }`,
-				},
-			],
-		},
-		{
-			type: 'separator',
-		},
-		// Add WooCommerce here
-		...( shouldShowWooCommerce
-			? [
-					{
-						icon: WOOCOMMERCE_ICON,
-						slug: 'woo-php',
-						title: translate( 'WooCommerce' ),
-						type: 'menu-item',
-						url: `https://${ siteDomain }/wp-admin/admin.php?page=wc-admin`,
-					},
-					{
-						type: 'separator',
-					},
-			  ]
-			: [] ),
-		...( shouldShowThemes
-			? [
-					{
-						icon: 'dashicons-admin-appearance',
-						slug: 'themes-php',
-						title: translate( 'Appearance' ),
-						type: 'menu-item',
-						url: `/themes/${ siteDomain }`,
-						children: [
-							{
-								parent: 'themes.php',
-								slug: 'themes-php',
-								title: translate( 'Themes' ),
-								type: 'submenu-item',
-								url: `/themes/${ siteDomain }`,
-							},
-							{
-								parent: 'themes.php',
-								slug: 'themes-customize',
-								title: translate( 'Customize' ),
-								type: 'submenu-item',
-								url: `/customize/${ siteDomain }`,
-							},
-
-							...( shouldShowApperanceHeader
-								? [
-										{
-											parent: 'themes.php',
-											slug: 'themes-header',
-											title: translate( 'Header' ),
-											type: 'submenu-item',
-											url: `https://${ siteDomain }/wp-admin/customize.php?return=%2Fwp-admin%2F&autofocus%5Bcontrol%5D=header_image`,
-										},
-								  ]
-								: [] ),
-							...( shouldShowApperanceBackground
-								? [
-										{
-											parent: 'themes.php',
-											slug: 'themes-background',
-											title: translate( 'Background' ),
-											type: 'submenu-item',
-											url: `https://${ siteDomain }/wp-admin/customize.php?return=%2Fwp-admin%2F&autofocus%5Bcontrol%5D=background_image`,
-										},
-								  ]
-								: [] ),
-							{
-								parent: 'themes.php',
-								slug: 'themes-widgets',
-								title: translate( 'Widgets' ),
-								type: 'submenu-item',
-								url: `/customize/${ siteDomain }?autofocus[panel]=widgets`,
-							},
-							{
-								parent: 'themes.php',
-								slug: 'themes-menus',
-								title: translate( 'Menus' ),
-								type: 'submenu-item',
-								url: `/customize/${ siteDomain }?autofocus[panel]=nav_menus`,
-							},
-						],
-					},
-			  ]
-			: [] ),
 		{
 			icon: 'dashicons-admin-plugins',
 			slug: 'plugins',
@@ -439,202 +197,31 @@ export default function buildFallbackResponse( {
 			type: 'menu-item',
 			url: `/plugins/${ siteDomain }`,
 		},
+
+		...( showSiteMonitoring
+			? [
+					{
+						slug: 'tools-site-monitoring',
+						title: translate( 'Site Monitoring' ),
+						type: 'submenu-item',
+						icon: 'dashicons-visibility',
+						url: `/site-monitoring/${ siteDomain }`,
+					},
+			  ]
+			: [] ),
 		{
-			icon: 'dashicons-admin-users',
-			slug: 'users-php',
-			title: translate( 'Users' ),
-			type: 'menu-item',
-			url: `/people/team/${ siteDomain }`,
-			children: [
-				{
-					parent: 'users.php',
-					slug: 'users-all-people',
-					title: translate( 'All Users' ),
-					type: 'submenu-item',
-					url: `/people/team/${ siteDomain }`,
-				},
-				{
-					parent: 'users.php',
-					slug: 'users-add-new',
-					title: translate( 'Add New User', { context: 'user' } ),
-					type: 'submenu-item',
-					url: `/people/new/${ siteDomain }`,
-				},
-				{
-					parent: 'users.php',
-					slug: 'subscribers',
-					title: translate( 'Subscribers' ),
-					type: 'submenu-item',
-					url: `/subscribers/${ siteDomain }`,
-				},
-				{
-					parent: 'users.php',
-					slug: 'users-my-profile',
-					title: translate( 'My Profile' ),
-					type: 'submenu-item',
-					url: `/me/`,
-				},
-				{
-					parent: 'users.php',
-					slug: 'users-account-settings',
-					title: translate( 'Account Settings' ),
-					type: 'submenu-item',
-					url: `/me/account`,
-				},
-			],
+			slug: 'options-hosting-configuration-php',
+			icon: 'dashicons-admin-generic',
+			title: translate( 'Hosting Configuration' ),
+			type: 'submenu-item',
+			url: `/hosting-config/${ siteDomain }`,
 		},
 		{
 			icon: 'dashicons-admin-tools',
-			slug: 'tools-php',
-			title: translate( 'Tools' ),
-			type: 'menu-item',
-			url: `/marketing/tools/${ siteDomain }`,
-			children: [
-				{
-					parent: 'tools.php',
-					slug: 'tools-marketing',
-					title: translate( 'Marketing' ),
-					type: 'menu-item',
-					url: `/marketing/tools/${ siteDomain }`,
-				},
-				{
-					parent: 'tools.php',
-					slug: 'tools-earn',
-					title: translate( 'Monetize' ),
-					type: 'menu-item',
-					url: `/earn/${ siteDomain }`,
-				},
-				{
-					parent: 'tools.php',
-					slug: 'tools-import',
-					title: translate( 'Import' ),
-					type: 'submenu-item',
-					url: `/import/${ siteDomain }`,
-				},
-				{
-					parent: 'tools.php',
-					slug: 'tools-export',
-					title: translate( 'Export' ),
-					type: 'submenu-item',
-					url: `/export/${ siteDomain }`,
-				},
-				...( showGithubDeployments
-					? [
-							{
-								parent: 'tools.php',
-								slug: 'tools-github-deployments',
-								title: translate( 'GitHub Deployments' ),
-								type: 'submenu-item',
-								url: `/github-deployments/${ siteDomain }`,
-							},
-					  ]
-					: [] ),
-				...( showSiteMonitoring
-					? [
-							{
-								parent: 'tools.php',
-								slug: 'tools-site-monitoring',
-								title: translate( 'Site Monitoring' ),
-								type: 'submenu-item',
-								url: `/site-monitoring/${ siteDomain }`,
-							},
-					  ]
-					: [] ),
-			],
-		},
-		{
-			icon: 'dashicons-admin-settings',
 			slug: 'options-general-php',
-			title: translate( 'Settings' ),
+			title: translate( 'Site Tools' ),
 			type: 'menu-item',
 			url: `/settings/general/${ siteDomain }`,
-			children: [
-				{
-					parent: 'options-general.php',
-					slug: 'options-general-php',
-					title: translate( 'General' ),
-					type: 'submenu-item',
-					url: `/settings/general/${ siteDomain }`,
-				},
-				{
-					parent: 'options-general.php',
-					slug: 'options-reading-php',
-					title: translate( 'Reading' ),
-					type: 'submenu-item',
-					url: `/settings/reading/${ siteDomain }`,
-				},
-				...( config.isEnabled( 'settings/newsletter-settings-page' )
-					? [
-							{
-								parent: 'options-general.php',
-								slug: 'options-newsletter-php',
-								title: translate( 'Newsletter' ),
-								type: 'submenu-item',
-								url: `/settings/newsletter/${ siteDomain }`,
-							},
-					  ]
-					: [] ),
-				{
-					parent: 'options-podcasting.php',
-					slug: 'options-podcasting-php',
-					title: translate( 'Podcasting' ),
-					type: 'submenu-item',
-					url: `/settings/podcasting/${ siteDomain }`,
-				},
-				{
-					parent: 'options-general.php',
-					slug: 'options-domains-php',
-					title: translate( 'Domains' ),
-					type: 'submenu-item',
-					url: `/domains/manage/${ siteDomain }`,
-				},
-				{
-					parent: 'options-general.php',
-					slug: 'options-media-php',
-					title: translate( 'Media' ),
-					type: 'submenu-item',
-					url: `https://${ siteDomain }/wp-admin/options-media.php`,
-				},
-				{
-					parent: 'options-general.php',
-					slug: 'options-sharing-php',
-					title: translate( 'Sharing' ),
-					type: 'submenu-item',
-					url: `https://${ siteDomain }/wp-admin/options-general.php?page=sharing`,
-				},
-				{
-					parent: 'options-general.php',
-					slug: 'options-hosting-configuration-php',
-					title: translate( 'Hosting Configuration' ),
-					type: 'submenu-item',
-					url: `/hosting-config/${ siteDomain }`,
-				},
-				{
-					parent: 'options-general.php',
-					slug: 'options-polls-php',
-					title: translate( 'Polls' ),
-					type: 'submenu-item',
-					url: `https://${ siteDomain }/wp-admin/options-general.php?page=polls&action=options`,
-				},
-				{
-					parent: 'options-general.php',
-					slug: 'options-ratings-php',
-					title: translate( 'Ratings' ),
-					type: 'submenu-item',
-					url: `https://${ siteDomain }/wp-admin/options-general.php?page=ratings&action=options`,
-				},
-				...( shouldShowAdControl
-					? [
-							{
-								parent: 'options-general.php',
-								slug: 'options-ad-control',
-								title: translate( 'AdControl' ),
-								type: 'submenu-item',
-								url: `https://${ siteDomain }/wp-admin/options-general.php?page=adcontrol`,
-							},
-					  ]
-					: [] ),
-			],
 		},
 	];
 
