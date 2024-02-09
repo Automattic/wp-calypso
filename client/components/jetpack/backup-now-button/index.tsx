2fb0d68391ad6f12897b48272302ab75596976d1
Rafael Agostini <rafael.agostini@automattic.com>
2024-02-09 18:06:41 -0500

    Jetpack Backup: Add BackupNowButton component scaffolding (#87348)
    * Add BackupNowButton scaffolding
    
    * Rename toolTiptext to tooltipText
    
    * Remove the button to be rendered inside of another button
    
    * Remove label property

---
+++ client/components/jetpack/backup-now-button/index.tsx
@@ -0,0 +1,35 @@
+import { Button, Tooltip } from '@wordpress/components';
+import { FunctionComponent } from 'react';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
+
+interface Props {
+	children?: React.ReactNode;
+	disabled?: boolean;
+	tooltipText?: string;
+	trackEventName?: string;
+	variant: 'primary' | 'secondary' | 'tertiary';
+}
+
+const BackupNowButton: FunctionComponent< Props > = ( {
+	children,
+	disabled = false,
+	tooltipText,
+	trackEventName,
+	variant,
+} ) => {
+	const handleClick = () => {
+		if ( trackEventName ) {
+			recordTracksEvent( trackEventName );
+		}
+	};
+
+	const button = (
+		<Button variant={ variant } onClick={ handleClick } disabled={ disabled }>
+			{ children }
+		</Button>
+	);
+
+	return <>{ tooltipText ? <Tooltip text={ tooltipText }>{ button }</Tooltip> : button }</>;
+};
+
+export default BackupNowButton;
