66ee8be0c2f62563c3fd90bd4e4433ae8a6deeca
Eoin Gallagher <eoin@automattic.com>
2024-02-09 13:53:52 +0000

    fix scrolling issue on reader

--- client/layout/global-sidebar/notifications/bell-icon.jsx
+++ client/layout/global-sidebar/notifications/bell-icon.jsx
@@ -7,7 +7,7 @@ export const BellIcon = ( { newItems, active } ) => {
 				className="sidebar__menu-icon sidebar_svg-notifications"
 				fill="none"
 				height="20"
-				viewBox="0 0 20 20"
+				viewBox="2 2 24 24"
 				width="20"
 				xmlns="http://www.w3.org/2000/svg"
 			>
