6b588d39abb70cd57b13b2d04d17f881175349b7
Rafael Agostini <rafael.agostini@automattic.com>
2024-02-09 21:33:13 -0500

    Add `copy site` button to BackupActionsToolbar

--- client/components/jetpack/backup-actions-toolbar/index.tsx
+++ client/components/jetpack/backup-actions-toolbar/index.tsx
@@ -1,10 +1,37 @@
 import config from '@automattic/calypso-config';
+// import { Button } from '@automattic/components';
+import { Button, Tooltip } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import { FunctionComponent } from 'react';
+import { useDispatch } from 'react-redux';
+import { backupClonePath } from 'calypso/my-sites/backup/paths';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import BackupNowButton from '../backup-now-button';
 
-const BackupActionsToolbar: FunctionComponent = () => {
+interface Props {
+	siteSlug: 'string';
+}
+
+const BackupActionsToolbar: FunctionComponent< Props > = ( { siteSlug } ) => {
 	const translate = useTranslate();
+	const dispatch = useDispatch();
+
+	const copySite = (
+		<Tooltip
+			text={ translate(
+				'To test your site changes, migrate or keep your data safe in another site'
+			) }
+		>
+			<Button
+				className="backup__clone-button"
+				href={ backupClonePath( siteSlug ) }
+				onClick={ () => dispatch( recordTracksEvent( 'calypso_jetpack_backup_copy_site' ) ) }
+				variant="secondary"
+			>
+				{ translate( 'Copy site' ) }
+			</Button>
+		</Tooltip>
+	);
 
 	const backupNow = (
 		<BackupNowButton variant="primary" trackEventName="calypso_jetpack_backup_now">
@@ -12,7 +39,12 @@ const BackupActionsToolbar: FunctionComponent = () => {
 		</BackupNowButton>
 	);
 
-	return <>{ config.isEnabled( 'jetpack/backup-on-demand' ) && backupNow }</>;
+	return (
+		<>
+			{ copySite }
+			{ config.isEnabled( 'jetpack/backup-on-demand' ) && backupNow }
+		</>
+	);
 };
 
 export default BackupActionsToolbar;
