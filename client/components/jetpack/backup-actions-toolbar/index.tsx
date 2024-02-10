9e061415947f7a4badec0b20ede58be120349891
Rafael Agostini <rafael.agostini@automattic.com>
2024-02-09 21:58:36 -0500

    Add some styles for the actions toolbar

--- client/components/jetpack/backup-actions-toolbar/index.tsx
+++ client/components/jetpack/backup-actions-toolbar/index.tsx
@@ -3,10 +3,11 @@ import config from '@automattic/calypso-config';
 import { Button, Tooltip } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
 import { FunctionComponent } from 'react';
-import { useDispatch } from 'react-redux';
 import { backupClonePath } from 'calypso/my-sites/backup/paths';
+import { useDispatch } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import BackupNowButton from '../backup-now-button';
+import './style.scss';
 
 interface Props {
 	siteSlug: 'string';
@@ -40,10 +41,10 @@ const BackupActionsToolbar: FunctionComponent< Props > = ( { siteSlug } ) => {
 	);
 
 	return (
-		<>
+		<div className="jetpack-backup__actions-toolbar">
 			{ copySite }
 			{ config.isEnabled( 'jetpack/backup-on-demand' ) && backupNow }
-		</>
+		</div>
 	);
 };
 
