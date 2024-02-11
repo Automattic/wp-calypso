55e76fca38eacef6fb5db0ae68cac8fa6fdc093c
Payton Swick <payton@foolord.com>
2024-02-11 15:49:31 -0500

    Remove makeManualResponse from remaining processes

--- packages/composite-checkout/src/lib/invalid-payment-processor-response-error.ts
+++ packages/composite-checkout/src/lib/invalid-payment-processor-response-error.ts
@@ -1,6 +1,6 @@
 export default class InvalidPaymentProcessorResponseError extends Error {
 	constructor( paymentProcessorId: string ) {
-		const message = `Unknown payment processor response for processor "${ paymentProcessorId }". Payment processor functions should return one of makeManualResponse, makeSuccessResponse, makeRedirectResponse, or makeErrorResponse.`;
+		const message = `Unknown payment processor response for processor "${ paymentProcessorId }". Payment processor functions should return one of makeSuccessResponse, makeRedirectResponse, or makeErrorResponse.`;
 		super( message );
 		this.name = 'InvalidPaymentProcessorResponseError';
 	}
