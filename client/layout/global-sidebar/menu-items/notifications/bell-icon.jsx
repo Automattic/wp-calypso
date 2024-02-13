5c1a5b60503854bf029131ccc2a54c81e46831ec
Eoin Gallagher <eoin@automattic.com>
2024-02-13 22:16:44 +0000

    fix notification styling

--- client/layout/global-sidebar/menu-items/notifications/bell-icon.jsx
+++ client/layout/global-sidebar/menu-items/notifications/bell-icon.jsx
@@ -25,20 +25,20 @@ export const BellIcon = ( { newItems, active } ) => {
 		<svg
 			className="sidebar__menu-icon sidebar_svg-notifications"
 			fill="none"
-			height="20"
-			viewBox="0 0 20 20"
-			width="20"
+			height="24"
+			viewBox="0 0 24 24"
+			width="24"
 			xmlns="http://www.w3.org/2000/svg"
 		>
 			<path
 				d="M5 6.5C5 3.73858 7.23858 1.5 10 1.5V1.5C12.7614 1.5 15 3.73858 15 6.5V11.86L17.5 14.5H2.5L5 11.86V6.5Z"
-				stroke="#A2AAB2"
+				stroke="#fff"
 				strokeWidth="1.5"
 				strokeLinejoin="round"
 			/>
 			<path
 				d="M12 16.5C12 17.6046 11.1046 18.5 10 18.5C8.89543 18.5 8 17.6046 8 16.5"
-				stroke="#A2AAB2"
+				stroke="#fff"
 				strokeWidth="1.5"
 				strokeLinejoin="round"
 			/>
