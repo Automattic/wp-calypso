f6bdca2f3dbdb8af7b5702e9d7f05007de896074
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2024-02-13 15:47:17 -0300

    GitHub Deployments: Query repository branches from installation (#87429)

--- client/my-sites/github-deployments/use-github-repository-branches-query.ts
+++ client/my-sites/github-deployments/use-github-repository-branches-query.ts
@@ -1,23 +1,34 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
+import { addQueryArgs } from '@wordpress/url';
 import wp from 'calypso/lib/wp';
 import { GITHUB_DEPLOYMENTS_QUERY_KEY } from './constants';
 
 export const GITHUB_ACCOUNTS_QUERY_KEY = 'github-repository-branches';
 
 export const useGithubRepositoryBranchesQuery = (
-	repository: string,
-	options?: UseQueryOptions< string[] >
+	installation_id: number,
+	repository_owner: string,
+	repository_name: string
 ) => {
 	return useQuery< string[] >( {
-		queryKey: [ GITHUB_DEPLOYMENTS_QUERY_KEY, GITHUB_ACCOUNTS_QUERY_KEY, repository ],
+		queryKey: [
+			GITHUB_DEPLOYMENTS_QUERY_KEY,
+			GITHUB_ACCOUNTS_QUERY_KEY,
+			installation_id,
+			repository_owner,
+			repository_name,
+		],
 		queryFn: (): string[] =>
 			wp.req.get( {
-				path: `/hosting/github/repository/branches?repository=${ repository }`,
+				path: addQueryArgs( '/hosting/github/repository/branches', {
+					installation_id,
+					repository_owner,
+					repository_name,
+				} ),
 				apiNamespace: 'wpcom/v2',
 			} ),
 		meta: {
 			persist: false,
 		},
-		...options,
 	} );
 };
