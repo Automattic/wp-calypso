c818de86b6b7a9096487a318802410e2c98d4ced
Eoin Gallagher <eoin@automattic.com>
2024-02-13 12:22:06 +0000

    Update client/layout/global-sidebar/hooks/use-global-sidebar.js
    Co-authored-by: Louis Laugesen <louis.laugesen@gmail.com>

--- client/layout/global-sidebar/hooks/use-global-sidebar.js
+++ client/layout/global-sidebar/hooks/use-global-sidebar.js
@@ -1,9 +1,13 @@
+import { isEnabled } from '@automattic/calypso-config';
+
 export const useGlobalSidebar = ( siteId, sectionGroup ) => {
-	// TODO: need to expand on logic here - could add feature flag or some environment variable to control this
-	const shouldShowGlobalSidebar =
-		sectionGroup === 'me' ||
-		sectionGroup === 'reader' ||
-		sectionGroup === 'sites-dashboard' ||
-		( sectionGroup === 'sites' && ! siteId );
+	let shouldShowGlobalSidebar = false;
+	if ( isEnabled( 'layout/dotcom-nav-redesign' ) ) {
+		shouldShowGlobalSidebar =
+			sectionGroup === 'me' ||
+			sectionGroup === 'reader' ||
+			sectionGroup === 'sites-dashboard' ||
+			( sectionGroup === 'sites' && ! siteId );
+	}
 	return { shouldShowGlobalSidebar };
 };
