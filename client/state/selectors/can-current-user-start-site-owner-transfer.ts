b23c56b294394bc9213935b7175682f586ee8e52
Rahul <rahul.gavande@automattic.com>
2024-02-13 16:43:39 +0530

    add import

--- client/state/selectors/can-current-user-start-site-owner-transfer.ts
+++ client/state/selectors/can-current-user-start-site-owner-transfer.ts
@@ -1,3 +1,4 @@
+import { getCurrentUserId } from 'calypso/state/current-user/selectors';
 import isSiteAutomatedTransfer from 'calypso/state/selectors/is-site-automated-transfer';
 import isSiteP2Hub from 'calypso/state/selectors/is-site-p2-hub';
 import isSiteWPForTeams from 'calypso/state/selectors/is-site-wpforteams';
