3602933d545b59f5c4ad0be76380116927c888b7
cpapazoglou <hrrsppzgl@gmail.com>
2024-02-13 11:57:25 +0200

    Site Transfers: adds cta actions depending on plan.

--- client/my-sites/site-settings/site-owner-transfer/site-transferred.tsx
+++ client/my-sites/site-settings/site-owner-transfer/site-transferred.tsx
@@ -1,4 +1,5 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
+import { PLAN_BUSINESS, PLAN_FREE } from '@automattic/calypso-products';
 import { Button } from '@automattic/components';
 import { Global, css } from '@emotion/react';
 import styled from '@emotion/styled';
@@ -12,7 +13,11 @@ import Main from 'calypso/components/main';
 import ThankYouV2 from 'calypso/components/thank-you-v2';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
 import MasterbarStyled from 'calypso/my-sites/checkout/checkout-thank-you/redesign-v2/masterbar-styled';
+import { getAddNewPaymentMethodUrlFor } from 'calypso/my-sites/purchases/paths';
 import { useSelector } from 'calypso/state';
+import isPrivateSite from 'calypso/state/selectors/is-private-site';
+import isSiteComingSoon from 'calypso/state/selectors/is-site-coming-soon';
+import { getSitePlanSlug } from 'calypso/state/sites/selectors';
 import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
 const Preview = styled.img`
@@ -20,18 +25,28 @@ const Preview = styled.img`
 	border-radius: 20px;
 `;
 
+const MainStyled = styled( Main )`
+	padding: 0 20px;
+`;
+
 const SiteTransferred = () => {
 	useQueryUserPurchases();
+	const translate = useTranslate();
 	const [ mShotUrl, setMShotUrl ] = useState( '' );
 	const siteId = useSelector( getSelectedSiteId );
+	const sitePlanSlug = useSelector( ( state ) => getSitePlanSlug( state, siteId ) ?? '' );
+	const siteSlug = useSelector( getSelectedSiteSlug ) || '';
+	const isPrivate = useSelector( ( state ) => ( siteId ? isPrivateSite( state, siteId ) : true ) );
+	const isComingSoon = useSelector( ( state ) =>
+		siteId ? isSiteComingSoon( state, siteId ) : true
+	);
+	const isPrivateOrComingSoon = isPrivate || isComingSoon;
+	const hasBillingPlan = sitePlanSlug !== PLAN_FREE;
 	const { width } = useWindowDimensions();
 	const mShotParams: MShotParams = {
 		scale: 2,
 		vpw: width <= 640 ? 640 : undefined,
 	};
-	const siteSlug = useSelector( getSelectedSiteSlug );
-	const translate = useTranslate();
-	const hasBillingPlan = true;
 
 	const sendTrackEvent = useCallback(
 		( name: string ) => {
@@ -61,28 +76,39 @@ const SiteTransferred = () => {
 		http.send();
 	};
 
-	checkScreenshot(
-		`https://s0.wp.com/mshots/v1/${ siteSlug }?${ Object.entries( mShotParams )
-			.filter( ( entry ) => !! entry[ 1 ] )
-			.map( ( [ key, val ] ) => key + '=' + val )
-			.join( '&' ) }`
-	);
+	if ( ! isPrivateOrComingSoon ) {
+		checkScreenshot(
+			`https://s0.wp.com/mshots/v1/${ siteSlug }?${ Object.entries( mShotParams )
+				.filter( ( entry ) => !! entry[ 1 ] )
+				.map( ( [ key, val ] ) => key + '=' + val )
+				.join( '&' ) }`
+		);
+	}
 
 	const thankYouHeaderAction = (
 		<Button
 			primary
-			href={ `https://${ siteSlug }/wp-admin/plugins.php` }
+			href={
+				hasBillingPlan
+					? getAddNewPaymentMethodUrlFor( siteSlug )
+					: `/checkout/${ siteSlug }/${ PLAN_BUSINESS }`
+			}
 			onClick={ () => {
-				sendTrackEvent( 'calypso_plugin_thank_you_setup_plugins_click' );
+				sendTrackEvent( 'calypso_transferred_site_next_step_click' );
 			} }
 		>
-			{ hasBillingPlan ? translate( 'Setup my billing details' ) : translate( 'Upgrade my plan' ) }
+			{ hasBillingPlan
+				? translate( 'Update billing details' )
+				: translate( 'Proceed to Checkout' ) }
 		</Button>
 	);
+	const title = hasBillingPlan
+		? translate( "You're now in charge." )
+		: translate( 'Your site is not on a plan.' );
 
 	const subtitle = hasBillingPlan
 		? translate(
-				"To ensure uninterrupted service, please update your billing details in the next step. After that, you're all set to explore and manage your site."
+				"Continue to full ownership by signing up for our exclusive plan. Enter your billing details and maintain your site's momentum!"
 		  )
 		: translate(
 				'To keep your site active and access all features, please upgrade your plan in the next step and enjoy the full experience of your site.'
@@ -101,14 +127,12 @@ const SiteTransferred = () => {
 				` }
 			/>
 			<MasterbarStyled canGoBack={ false } />
-			<Main>
-				<ThankYouV2
-					title={ translate( 'You now have full control' ) }
-					subtitle={ subtitle }
-					headerButtons={ thankYouHeaderAction }
-				/>
-				{ mShotUrl && <Preview src={ mShotUrl } alt="Website screenshot preview" /> }
-			</Main>
+			<MainStyled>
+				<ThankYouV2 title={ title } subtitle={ subtitle } headerButtons={ thankYouHeaderAction } />
+				{ ! isPrivateOrComingSoon && mShotUrl && (
+					<Preview src={ mShotUrl } alt="Website screenshot preview" />
+				) }
+			</MainStyled>
 		</>
 	);
 };
