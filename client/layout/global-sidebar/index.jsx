30208a64b631f96f2c43f4d6c58b75a88411e24a
Eoin Gallagher <eoin@automattic.com>
2024-02-14 16:25:36 +0000

    Add Global sidebar (#87454)
    * Add basic global sidebar menu
    
    * fix build error with window undefined in onTouch library
    
    * update my-sites to use global sidebar
    
    * hide masterbar if global sidebar is visible
    
    * add global sidebar to secondary in layout if enabled
    
    * hide masterbar if global sidebar is shown in layout
    
    * enable feature flag to show global sidebar in my-sites and domain manage pages
    
    * fix typo
    
    * fix issue with handleScroll messing up sidebar positioning due to no masterbar

---
+++ client/layout/global-sidebar/index.jsx
@@ -0,0 +1,43 @@
+import { Spinner, Gridicon } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
+import useSiteMenuItems from 'calypso/my-sites/sidebar/use-site-menu-items';
+import { getIsRequestingAdminMenu } from 'calypso/state/admin-menu/selectors';
+import Sidebar from '../sidebar';
+import './style.scss';
+
+const GlobalSidebar = ( { children, onClick = undefined, className = '', ...props } ) => {
+	const menuItems = useSiteMenuItems();
+	const isRequestingMenu = useSelector( getIsRequestingAdminMenu );
+	const translate = useTranslate();
+
+	/**
+	 * If there are no menu items and we are currently requesting some,
+	 * then show a spinner. The check for menuItems is necessary because
+	 * we may choose to render the menu from statically stored JSON data
+	 * and therefore we need to be ready to render.
+	 */
+	if ( ! menuItems && isRequestingMenu ) {
+		return <Spinner className="sidebar__menu-loading" />;
+	}
+
+	return (
+		<div className="global-sidebar">
+			<div className="sidebar__body">
+				<Sidebar className={ className } { ...props } onClick={ onClick }>
+					{ props.requireBackLink && (
+						<div className="sidebar__back-link">
+							<a href="/sites">
+								<Gridicon icon="chevron-left" size={ 24 } />
+								<span className="sidebar__back-link-text">{ translate( 'Back' ) }</span>
+							</a>
+						</div>
+					) }
+					{ children }
+				</Sidebar>
+			</div>
+		</div>
+	);
+};
+
+export default GlobalSidebar;
