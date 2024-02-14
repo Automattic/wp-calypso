f3a00e486edf23fc0d82c7e7f52ddb6b870e26e8
Yashwin Poojary <yashwinpoojary@gmail.com>
2024-02-14 10:52:30 +0530

    Automattic for Agencies: Implement OAuth Authentication (#87380)

--- client/boot/common.js
+++ client/boot/common.js
@@ -51,6 +51,8 @@ import { setupLocale } from './locale';
 
 const debug = debugFactory( 'calypso' );
 
+const isA8CForAgenciesEnabled = config.isEnabled( 'a8c-for-agencies' );
+
 const setupContextMiddleware = ( reduxStore, reactQueryClient ) => {
 	page( '*', ( context, next ) => {
 		const parsed = getUrlParts( context.canonicalPath );
@@ -134,7 +136,7 @@ function saveOauthFlags() {
 
 function authorizePath() {
 	const redirectUri = new URL(
-		isJetpackCloud() ? '/connect/oauth/token' : '/api/oauth/token',
+		isJetpackCloud() || isA8CForAgenciesEnabled ? '/connect/oauth/token' : '/api/oauth/token',
 		window.location
 	);
 	redirectUri.search = new URLSearchParams( {
