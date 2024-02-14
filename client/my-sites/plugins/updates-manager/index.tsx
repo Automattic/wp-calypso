b76b51cd5087afbbdfb8db25b348911701d18a9d
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-02-14 19:40:46 +0100

    Add initial prototype version

--- client/my-sites/plugins/updates-manager/index.tsx
+++ client/my-sites/plugins/updates-manager/index.tsx
@@ -1,7 +1,60 @@
+import { Button, IconButton, Card, CardHeader, CardBody, CardFooter } from '@wordpress/components';
+import { trash, edit } from '@wordpress/icons';
+import { navItems } from 'calypso/blocks/stats-navigation/constants';
+import MainComponent from 'calypso/components/main';
+import NavigationHeader from 'calypso/components/navigation-header';
+
 export const UpdatesManager = () => {
 	return (
-		<div>
-			<h1>Updates Manager</h1>
-		</div>
+		<MainComponent wideLayout>
+			<NavigationHeader
+				className="stats__section-header modernized-header"
+				title="Plugin updates manager"
+				subtitle="Effortlessly schedule plugin auto-updates with built-in rollback logic."
+				screenReader={ navItems.traffic?.label }
+				navigationItems={ [] }
+			></NavigationHeader>
+			<Card>
+				<CardHeader>Schedules</CardHeader>
+				<CardBody>
+					<table>
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Time</th>
+								<th>Frequency</th>
+								<th>Plugins</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Move to WordPress.com plugin</td>
+								<td>12:00 AM</td>
+								<td>Daily</td>
+								<td>1</td>
+								<td>
+									<IconButton icon={ edit } label="Edit" />
+									<IconButton icon={ trash } label="Remove" />
+								</td>
+							</tr>
+							<tr>
+								<td>Security plugins</td>
+								<td>12:00 AM</td>
+								<td>Daily</td>
+								<td>3</td>
+								<td>
+									<IconButton icon={ edit } label="Edit" />
+									<IconButton icon={ trash } label="Remove" />
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</CardBody>
+				<CardFooter>
+					<Button variant="primary">Add</Button>
+				</CardFooter>
+			</Card>
+		</MainComponent>
 	);
 };
