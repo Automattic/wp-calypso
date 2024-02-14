4d637eba27e2800976d36ebf0515944a3d0194c8
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-02-14 14:26:43 +0800

    Initial Sidebar implementation.

--- client/a8c-for-agencies/controller.tsx
+++ client/a8c-for-agencies/controller.tsx
@@ -1,8 +1,8 @@
 import { type Callback } from '@automattic/calypso-router';
+import Sidebar from './components/sidebar';
 
 export const a8cForAgenciesContext: Callback = ( context, next ) => {
-	context.header = <div>Header</div>;
-	context.secondary = <div>Secondary</div>;
+	context.secondary = <Sidebar path="/" menuItems={ [] } />;
 	context.primary = <div>Hello, World!</div>;
 
 	next();
