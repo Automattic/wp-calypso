de854b4c025be67ae46cddb891b5a601439e3505
Mikael Korpela <mikael@ihminen.org>
2024-02-15 14:31:42 +0200

    Revert "A4A: Add Sidebar navigation component (#87450)" (#87489)
    This reverts commit 38a00c6b0a9e75fc888bdaf2c933b3f912749cf4.

--- client/a8c-for-agencies/controller.tsx
+++ client/a8c-for-agencies/controller.tsx
@@ -1,75 +1,9 @@
-import page, { type Callback } from '@automattic/calypso-router';
-import { category, chevronLeft, home } from '@wordpress/icons';
-import Sidebar from './components/sidebar';
+import { type Callback } from '@automattic/calypso-router';
 
-export const redirectToOverviewContext: Callback = () => {
-	page( '/overview' );
-};
-
-export const overviewContext: Callback = ( context, next ) => {
-	context.secondary = (
-		<Sidebar
-			path="/"
-			menuItems={ [
-				{
-					icon: home,
-					path: '/',
-					link: '/overview',
-					title: 'Overview',
-					trackEventProps: {
-						menu_item: 'A4A / Overview',
-					},
-					isSelected: true,
-				},
-				{
-					icon: category,
-					path: '/',
-					link: '/sites',
-					title: 'Sites',
-					trackEventProps: {
-						menu_item: 'Sites / Overview',
-					},
-					isSelected: false,
-				},
-			] }
-			withUserProfileFooter
-		/>
-	);
-	context.primary = <div>Overview page</div>;
-
-	next();
-};
-
-export const sitesContext: Callback = ( context, next ) => {
-	context.secondary = (
-		<Sidebar
-			path="/"
-			title="Sites"
-			description="sample text"
-			backButtonProps={ {
-				label: 'Back to overview',
-				icon: chevronLeft,
-				onClick: () => {
-					page( '/overview' );
-				},
-			} }
-			menuItems={ [
-				{
-					icon: category,
-					path: '/',
-					link: '/sites',
-					title: 'Needs attention',
-					trackEventProps: {
-						menu_item: 'Sites / Overview',
-					},
-					isSelected: false,
-				},
-			] }
-			withSiteSelector
-			withGetHelpLink
-		/>
-	);
-	context.primary = <div>Sites page</div>;
+export const a8cForAgenciesContext: Callback = ( context, next ) => {
+	context.header = <div>Header</div>;
+	context.secondary = <div>Secondary</div>;
+	context.primary = <div>Hello, World!</div>;
 
 	next();
 };
