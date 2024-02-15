c581621c11e6e5c4f0ab17eca8e812e380e82ad3
Javier Olmo <43957544+jjolmo@users.noreply.github.com>
2024-02-15 17:59:10 +0100

    Change the order direction of the campaigns list in calypso (#87504)

--- client/data/promote-post/use-promote-post-campaigns-query-paged.ts
+++ client/data/promote-post/use-promote-post-campaigns-query-paged.ts
@@ -32,7 +32,7 @@ const useCampaignsQueryPaged = (
 	return useInfiniteQuery( {
 		queryKey: [ 'promote-post-campaigns', siteId, searchQueryParams ],
 		queryFn: async ( { pageParam } ) => {
-			const searchCampaignsUrl = `/search/campaigns/site/${ siteId }?order=asc&order_by=post_date&page=${ pageParam }${ searchQueryParams }`;
+			const searchCampaignsUrl = `/search/campaigns/site/${ siteId }?order=desc&order_by=created_at&page=${ pageParam }${ searchQueryParams }`;
 			const resultQuery = await requestDSPHandleErrors< CampaignQueryResult >(
 				siteId,
 				searchCampaignsUrl
