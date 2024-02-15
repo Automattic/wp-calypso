723f770f75597f276f36141756aeffe7aef2c11c
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-02-15 20:04:12 +0800

    Remove temporary path.

--- client/a8c-for-agencies/index.tsx
+++ client/a8c-for-agencies/index.tsx
@@ -1,9 +1,7 @@
 import page from '@automattic/calypso-router';
 import { makeLayout, render as clientRender } from 'calypso/controller';
-import { overviewContext, redirectToOverviewContext, sitesContext } from './controller';
+import { redirectToOverviewContext } from './controller';
 
 export default function () {
 	page( '/', redirectToOverviewContext, makeLayout, clientRender );
-	page( '/overview', overviewContext, makeLayout, clientRender );
-	page( '/sites', sitesContext, makeLayout, clientRender );
 }
--- client/a8c-for-agencies/sections/sites/controller.tsx
+++ client/a8c-for-agencies/sections/sites/controller.tsx
@@ -1,8 +1,37 @@
-import { type Callback } from '@automattic/calypso-router';
+import page, { type Callback } from '@automattic/calypso-router';
+import { category, chevronLeft } from '@wordpress/icons';
+import Sidebar from '../../components/sidebar';
 
 export const sitesContext: Callback = ( context, next ) => {
 	context.header = <div>Header</div>;
-	context.secondary = <div>Secondary</div>;
+	context.secondary = (
+		<Sidebar
+			path="/"
+			title="Sites"
+			description="sample text"
+			backButtonProps={ {
+				label: 'Back to overview',
+				icon: chevronLeft,
+				onClick: () => {
+					page( '/overview' );
+				},
+			} }
+			menuItems={ [
+				{
+					icon: category,
+					path: '/',
+					link: '/sites',
+					title: 'Needs attention',
+					trackEventProps: {
+						menu_item: 'Sites / Overview',
+					},
+					isSelected: false,
+				},
+			] }
+			withSiteSelector
+			withGetHelpLink
+		/>
+	);
 	context.primary = <div>sites</div>;
 
 	next();
