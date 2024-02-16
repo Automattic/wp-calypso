d1e194f7c3092c606ab958de8d6ac84c885a54f7
Brian Willis <briowill@gmail.com>
2024-02-16 16:51:01 -0500

    Add product slug to products api call

--- client/components/data/query-products-list/index.jsx
+++ client/components/data/query-products-list/index.jsx
@@ -16,13 +16,18 @@ const request =
 		dispatch( requestProductsList( props ) );
 	};
 
-export function useQueryProductsList( { type = 'all', currency, persist } = {} ) {
+export function useQueryProductsList( {
+	type = 'all',
+	currency,
+	persist,
+	product_slug_list,
+} = {} ) {
 	const dispatch = useDispatch();
-
 	// Only runs on mount.
 	useEffect( () => {
-		dispatch( request( { type, currency, persist } ) );
-	}, [ dispatch, type, persist, currency ] );
+		const product_slugs = product_slug_list?.join( ',' );
+		dispatch( request( { type, currency, persist, product_slugs } ) );
+	}, [ dispatch, type, persist, currency, product_slug_list ] );
 
 	return null;
 }
@@ -34,8 +39,14 @@ export function useQueryProductsList( { type = 'all', currency, persist } = {} )
  *									"jetpack" for Jetpack products only, or undefined for all products.
  * @param {string} [props.currency] The currency code to override the currency used on the account.
  * @param {boolean} [props.persist] Set to true to persist the products list in the store.
+ * @param {string[]} [props.product_slug_list] Indicates the specific products being requested. Optional.
  * @returns {null} 					No visible output.
  */
-export default function QueryProductsList( { type = 'all', currency, persist } ) {
-	return useQueryProductsList( { type, currency, persist } );
+export default function QueryProductsList( {
+	type = 'all',
+	currency,
+	persist,
+	product_slug_list,
+} ) {
+	return useQueryProductsList( { type, currency, persist, product_slug_list } );
 }
