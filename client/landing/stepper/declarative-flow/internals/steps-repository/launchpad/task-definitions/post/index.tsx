d41944b0a0df839ce263c95ca6f6bd603902ab6d
Gabriel Caires <gabriel.caires@automattic.com>
2024-02-16 11:30:47 +0000

    Remove tracking from the post module

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/post/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/post/index.tsx
@@ -1,9 +1,7 @@
 import { Task } from '@automattic/launchpad';
 import { isBlogOnboardingFlow, isNewsletterFlow } from '@automattic/onboarding';
 import { addQueryArgs } from '@wordpress/url';
-import { recordTaskClickTracksEvent } from '../../tracking';
 import { TaskAction } from '../../types';
-
 
 export const getFirstPostPublished: TaskAction = ( task, flow, context ): Task => {
 	const { siteSlug, isEmailVerified } = context;
@@ -11,14 +9,15 @@ export const getFirstPostPublished: TaskAction = ( task, flow, context ): Task =
 
 	return {
 		...task,
-		disabled: mustVerifyEmailBeforePosting || ( task.completed && isBlogOnboardingFlow( flow || null ) ) ||
+		disabled:
+			mustVerifyEmailBeforePosting ||
+			( task.completed && isBlogOnboardingFlow( flow || null ) ) ||
 			false,
 		calypso_path: ! isBlogOnboardingFlow( flow || null )
 			? `/post/${ siteSlug }`
 			: addQueryArgs( `https://${ siteSlug }/wp-admin/post-new.php`, {
 					origin: window.location.origin,
-			 } ),
-		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
+			  } ),
 		useCalypsoPath: true,
 	};
 };
@@ -31,7 +30,6 @@ const getFirstPostPublishedNewsletterTask: TaskAction = ( task, flow, context ):
 		...task,
 		isLaunchTask: true,
 		disabled: mustVerifyEmailBeforePosting || false,
-		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
 		useCalypsoPath: true,
 	};
 };
