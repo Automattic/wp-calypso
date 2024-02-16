4fd5998dcc1a513e33d1e89de7f7b35393fdd473
Michael Kelly <mike.kelly@a8c.com>
2024-02-16 15:31:12 +0700

    GitHub deployments: add feedback and navigation after creating new repo (#87523)
    * GitHub deployments: add feedback and navigation after creating new repo
    
    * Revert reordering change made yesterday
    
    * Show progress button when request in progress

--- client/my-sites/github-deployments/components/repositories/create-repository/index.tsx
+++ client/my-sites/github-deployments/components/repositories/create-repository/index.tsx
@@ -1,23 +1,64 @@
+import page from '@automattic/calypso-router';
+import { sprintf } from '@wordpress/i18n';
 import { useI18n } from '@wordpress/react-i18n';
 import ActionPanel from 'calypso/components/action-panel';
 import ActionPanelBody from 'calypso/components/action-panel/body';
 import HeaderCake from 'calypso/components/header-cake';
 import Main from 'calypso/components/main';
-import { useSelector } from 'calypso/state/index';
-import { getSelectedSiteId } from 'calypso/state/ui/selectors/index';
+import { indexPage } from 'calypso/my-sites/github-deployments/routes';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
+import { useDispatch, useSelector } from 'calypso/state/index';
+import { errorNotice, successNotice } from 'calypso/state/notices/actions';
+import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors/index';
 import { CreateRepositoryForm } from './create-repository-form';
 import {
 	MutationVariables,
 	useCreateCodeDeploymentAndRepository,
 } from './use-create-code-deployment-and-repository';
+
 import './style.scss';
+
+const noticeOptions = {
+	duration: 3000,
+};
 
 export const CreateRepository = () => {
 	const { __ } = useI18n();
 	const siteId = useSelector( getSelectedSiteId );
+	const siteSlug = useSelector( getSelectedSiteSlug );
 
-	const { createDeploymentAndRepository } = useCreateCodeDeploymentAndRepository(
-		siteId as number
+	const goToDeployments = () => {
+		page( indexPage( siteSlug! ) );
+	};
+
+	const dispatch = useDispatch();
+
+	const { createDeploymentAndRepository, isPending } = useCreateCodeDeploymentAndRepository(
+		siteId as number,
+		{
+			onSuccess: () => {
+				goToDeployments();
+				dispatch( successNotice( __( 'Deployment created.' ), noticeOptions ) );
+			},
+			onError: ( error ) => {
+				dispatch(
+					errorNotice(
+						// translators: "reason" is why connecting the branch failed.
+						sprintf( __( 'Failed to create repository: %(reason)s' ), { reason: error.message } ),
+						{
+							...noticeOptions,
+						}
+					)
+				);
+			},
+			onSettled: ( _, error ) => {
+				dispatch(
+					recordTracksEvent( 'calypso_hosting_github_create_repository_success', {
+						connected: ! error,
+					} )
+				);
+			},
+		}
 	);
 
 	function handleCreateRepository( args: MutationVariables ) {
@@ -31,7 +72,10 @@ export const CreateRepository = () => {
 			</HeaderCake>
 			<ActionPanel>
 				<ActionPanelBody>
-					<CreateRepositoryForm onRepositoryCreated={ handleCreateRepository } />
+					<CreateRepositoryForm
+						onRepositoryCreated={ handleCreateRepository }
+						isPending={ isPending }
+					/>
 				</ActionPanelBody>
 			</ActionPanel>
 		</Main>
