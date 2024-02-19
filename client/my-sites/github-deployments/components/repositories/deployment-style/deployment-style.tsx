34f0bdcb6deb8a36fd082b07b09160aa5f7b1e73
Paulo Trentin <paulo@paulotrentin.com.br>
2024-02-19 09:30:26 -0300

    Copy fix

--- client/my-sites/github-deployments/components/repositories/deployment-style/deployment-style.tsx
+++ client/my-sites/github-deployments/components/repositories/deployment-style/deployment-style.tsx
@@ -134,7 +134,7 @@ export const DeploymentStyle = ( { onDefineStyle, onValidationChange }: Deployme
 
 						<SupportInfo
 							text={ __(
-								'A workflow is a configurable automated process that will run one or more jobs. Workflows are defined by a YAML file checked in to your repository and will run when triggered by an event in your repository, or they can be triggered manually, or at a defined schedule.'
+								'A workflow is a configurable automated process that will run one or more jobs. Workflows are defined by a YAML file checked into your repository and will run when triggered by an event in your repository, or they can be triggered manually or at a defined schedule.'
 							) }
 							link="https://docs.github.com/en/actions/using-workflows"
 							privacyLink={ false }
