6ae3fde2b9d13ae806844b0ccae902d9cb1d1185
Ashar Fuadi <ashar.fuadi@automattic.com>
2024-02-19 16:42:50 +0700

    Gate redirect with admin interface as well

--- client/my-sites/customer-home/controller.jsx
+++ client/my-sites/customer-home/controller.jsx
@@ -8,7 +8,11 @@ import { fetchSitePlugins } from 'calypso/state/plugins/installed/actions';
 import { getPluginOnSite } from 'calypso/state/plugins/installed/selectors';
 import isJetpackModuleActive from 'calypso/state/selectors/is-jetpack-module-active';
 import { isSiteOnWooExpressEcommerceTrial } from 'calypso/state/sites/plans/selectors';
-import { canCurrentUserUseCustomerHome, getSiteUrl } from 'calypso/state/sites/selectors';
+import {
+	canCurrentUserUseCustomerHome,
+	getSiteOption,
+	getSiteUrl,
+} from 'calypso/state/sites/selectors';
 import {
 	getSelectedSiteSlug,
 	getSelectedSiteId,
@@ -42,12 +46,14 @@ export async function maybeRedirect( context, next ) {
 
 	const siteId = getSelectedSiteId( state );
 	const site = getSelectedSite( state );
+	const adminInterface = getSiteOption( state, siteId, 'wpcom_admin_interface' );
+
 	const isSiteLaunched = site?.launch_status === 'launched' || false;
 	let fetchPromise;
 
 	if (
 		isSiteOnWooExpressEcommerceTrial( state, siteId ) &&
-		! isEnabled( 'layout/dotcom-nav-redesign' )
+		! ( isEnabled( 'layout/dotcom-nav-redesign' ) && adminInterface === 'wp-admin' )
 	) {
 		// Pre-fetch plugins and modules to avoid flashing content prior deciding whether to redirect.
 		fetchPromise = Promise.allSettled( [
