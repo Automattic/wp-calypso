d8a8c0e0f16d880176f5837627cdf5911c2f6c3f
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2024-02-19 19:05:57 +0800

    Add localized string support.

--- client/my-sites/stats/stats-module-utm/index.jsx
+++ client/my-sites/stats/stats-module-utm/index.jsx
@@ -2,15 +2,13 @@ import StatsModule from '../stats-module';
 import statsStrings from '../stats-strings';
 
 const StatsModuleUTM = ( props ) => {
-	// ToDo: Add typing/testing of period & query props.
-	// Currently accepts undefined without issue.
 	const { period, query } = props;
 	const moduleStrings = statsStrings();
 
 	return (
 		<StatsModule
 			path="utm"
-			moduleStrings={ moduleStrings.authors }
+			moduleStrings={ moduleStrings.utm }
 			period={ period }
 			query={ query }
 			statType="statsTopAuthors"
