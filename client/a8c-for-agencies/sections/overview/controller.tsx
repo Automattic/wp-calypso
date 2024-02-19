867e55d23297222ac3c6e7991a047c059010cc8d
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-02-19 15:57:00 +0800

    Update controllers to use sidebar menu.

--- client/a8c-for-agencies/sections/overview/controller.tsx
+++ client/a8c-for-agencies/sections/overview/controller.tsx
@@ -1,37 +1,9 @@
 import { type Callback } from '@automattic/calypso-router';
-import { category, home } from '@wordpress/icons';
-import Sidebar from '../../components/sidebar';
+import MainSidebar from '../../components/sidebar-menu/main';
 
 export const overviewContext: Callback = ( context, next ) => {
 	context.header = <div>Header</div>;
-	context.secondary = (
-		<Sidebar
-			path="/"
-			menuItems={ [
-				{
-					icon: home,
-					path: '/',
-					link: '/overview',
-					title: 'Overview',
-					trackEventProps: {
-						menu_item: 'A4A / Overview',
-					},
-					isSelected: true,
-				},
-				{
-					icon: category,
-					path: '/',
-					link: '/sites',
-					title: 'Sites',
-					trackEventProps: {
-						menu_item: 'Sites / Overview',
-					},
-					isSelected: false,
-				},
-			] }
-			withUserProfileFooter
-		/>
-	);
+	context.secondary = <MainSidebar path={ context.path } />;
 	context.primary = <div>Overview</div>;
 
 	next();
--- client/a8c-for-agencies/sections/sites/controller.tsx
+++ client/a8c-for-agencies/sections/sites/controller.tsx
@@ -1,37 +1,9 @@
-import page, { type Callback } from '@automattic/calypso-router';
-import { category, chevronLeft } from '@wordpress/icons';
-import Sidebar from '../../components/sidebar';
+import { type Callback } from '@automattic/calypso-router';
+import SitesSidebar from '../../components/sidebar-menu/sites';
 
 export const sitesContext: Callback = ( context, next ) => {
 	context.header = <div>Header</div>;
-	context.secondary = (
-		<Sidebar
-			path="/"
-			title="Sites"
-			description="sample text"
-			backButtonProps={ {
-				label: 'Back to overview',
-				icon: chevronLeft,
-				onClick: () => {
-					page( '/overview' );
-				},
-			} }
-			menuItems={ [
-				{
-					icon: category,
-					path: '/',
-					link: '/sites',
-					title: 'Needs attention',
-					trackEventProps: {
-						menu_item: 'Sites / Overview',
-					},
-					isSelected: false,
-				},
-			] }
-			withSiteSelector
-			withGetHelpLink
-		/>
-	);
+	context.secondary = <SitesSidebar path={ context.path } />;
 	context.primary = <div>sites</div>;
 
 	next();
@@ -39,7 +11,7 @@ export const sitesContext: Callback = ( context, next ) => {
 
 export const sitesFavoriteContext: Callback = ( context, next ) => {
 	context.header = <div>Header</div>;
-	context.secondary = <div>Secondary</div>;
+	context.secondary = <SitesSidebar path={ context.path } />;
 	context.primary = <div>sites favorite</div>;
 
 	next();
