7fba72d721f73fe33d1a6053895426b685ef914d
Christos <chriskmnds@gmail.com>
2024-02-19 13:21:11 +0200

    update relevant terms in type

--- packages/data-stores/src/products-list/types.ts
+++ packages/data-stores/src/products-list/types.ts
@@ -1,5 +1,6 @@
 import * as actions from './actions';
 import type { DispatchFromMap } from '../mapped-types';
+import type { PriceTierEntry } from '@automattic/calypso-products';
 
 export interface Dispatch {
 	dispatch: DispatchFromMap< typeof actions >;
@@ -9,17 +10,18 @@ export interface ProductsListItem {
 	available: boolean;
 	combined_cost_display: string;
 	cost: number;
+	cost_smallest_unit: number;
 	currency_code: string;
 	description: string;
 	is_domain_registration: boolean;
-	price_tier_list: any;
+	price_tier_list: PriceTierEntry[];
 	price_tier_lug: string;
 	price_tier_usage_quantity: number | null;
 	price_tiers: any;
 	product_id: number;
 	product_name: string;
 	product_slug: string;
-	product_term: string;
+	product_term?: string;
 	product_type: string;
 }
 
