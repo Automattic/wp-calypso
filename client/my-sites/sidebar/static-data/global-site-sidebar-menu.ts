8b880c4ff1eee8c4a5f497c7da6dd02085626ed8
arthur791004 <arthur.chu@automattic.com>
2024-02-20 15:30:25 +0800

    Untangle: Update styles for the all sites menu item (#87625)

--- client/my-sites/sidebar/static-data/global-site-sidebar-menu.ts
+++ client/my-sites/sidebar/static-data/global-site-sidebar-menu.ts
@@ -19,6 +19,7 @@ export default function globalSiteSidebarMenu( {
 			title: translate( 'All sites' ),
 			type: 'menu-item',
 			url: `/sites`,
+			className: 'sidebar__menu-item-all-sites',
 		},
 		{
 			type: 'current-site',
