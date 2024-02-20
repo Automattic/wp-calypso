4c3dc57c8f4a399586fef0717ce2566c022d3f20
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-02-20 16:04:58 +0100

    Match initial screen with the design

--- client/blocks/plugins-update-manager/index.tsx
+++ client/blocks/plugins-update-manager/index.tsx
@@ -1,15 +1,31 @@
 import {
+	__experimentalText as Text,
 	__experimentalConfirmDialog as ConfirmDialog,
 	Button,
 	Card,
 	CardBody,
-	CardFooter,
 	CardHeader,
+	DropdownMenu,
+	Tooltip,
 	Modal,
 } from '@wordpress/components';
-import { edit, trash } from '@wordpress/icons';
+import { createInterpolateElement } from '@wordpress/element';
+import { Icon, arrowLeft, info } from '@wordpress/icons';
 import { useState } from 'react';
 import { ScheduleForm } from './schedule-form';
+
+import './styles.scss';
+
+const ellipsis = (
+	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path
+			fillRule="evenodd"
+			clipRule="evenodd"
+			d="M11 18.9999L11 16.9999L13 16.9999L13 18.9999L11 18.9999ZM11 12.9999L11 10.9999L13 10.9999L13 12.9999L11 12.9999ZM11 6.99988L11 4.99988L13 4.99988L13 6.99988L11 6.99988Z"
+			fill="#1E1E1E"
+		/>
+	</svg>
+);
 
 export const PluginsUpdateManager = () => {
 	const [ isConfirmOpen, setIsConfirmOpen ] = useState( false );
@@ -29,60 +45,96 @@ export const PluginsUpdateManager = () => {
 			<ConfirmDialog isOpen={ isConfirmOpen } onConfirm={ closeConfirm } onCancel={ closeConfirm }>
 				Are you sure you want to delete this schedule?
 			</ConfirmDialog>
-			<Card>
-				<CardHeader>Schedules</CardHeader>
+			<Card className="plugins-update-manager">
+				<CardHeader size="extraSmall">
+					<Button icon={ arrowLeft }>Back</Button>
+					<Text>Schedules</Text>
+					<div className="placeholder"></div>
+				</CardHeader>
 				<CardBody>
 					<table>
 						<thead>
 							<tr>
 								<th>Name</th>
-								<th>Time</th>
-								<th style={ { textAlign: 'center' } }>Frequency</th>
-								<th style={ { textAlign: 'center' } }>Plugins</th>
-								<th style={ { textAlign: 'end' } }>Actions</th>
+								<th>Next Update</th>
+								<th>Frequency</th>
+								<th>Plugins</th>
+								<th></th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
-								<td>Move to WordPress.com plugin</td>
+								<td className="name">Move to WordPress.com plugin</td>
 								<td>Feb 28 2023 7:00 PM UTC</td>
-								<td style={ { textAlign: 'center' } }>Daily</td>
-								<td style={ { textAlign: 'center' } }>1</td>
+								<td>Daily</td>
+								<td>
+									1
+									<Tooltip
+										text="Move to WordPress.com plugin"
+										position="middle right"
+										delay={ 0 }
+										hideOnClick={ false }
+									>
+										<Icon className="icon-info" icon={ info } size={ 16 } />
+									</Tooltip>
+								</td>
 								<td style={ { textAlign: 'end' } }>
-									<Button icon={ edit } label="Edit" />
-									<Button
-										icon={ trash }
-										label="Remove"
-										onClick={ () => {
-											setIsConfirmOpen( true );
-										} }
+									<DropdownMenu
+										popoverProps={ { position: 'bottom left' } }
+										controls={ [
+											{
+												title: 'Remove',
+												onClick: () => setIsConfirmOpen( true ),
+											},
+										] }
+										icon={ ellipsis }
+										label="More"
 									/>
 								</td>
 							</tr>
 							<tr>
-								<td>Security plugins</td>
+								<td className="name">Security plugins</td>
 								<td>Feb 28 2023 7:00 PM UTC</td>
-								<td style={ { textAlign: 'center' } }>Daily</td>
-								<td style={ { textAlign: 'center' } }>3</td>
+								<td>Daily</td>
+								<td>
+									3
+									<Tooltip
+										className="selected-plugins"
+										text={ createInterpolateElement(
+											'<div>Move to WordPress.com<br />Akismet<br />Gravity Forms</div>',
+											{
+												div: <div className="tooltip--selected-plugins" />,
+												br: <br />,
+											}
+										) }
+										position="middle right"
+										delay={ 0 }
+										hideOnClick={ false }
+									>
+										<Icon className="icon-info" icon={ info } size={ 16 } />
+									</Tooltip>
+								</td>
 								<td style={ { textAlign: 'end' } }>
-									<Button icon={ edit } label="Edit" />
-									<Button
-										icon={ trash }
-										label="Remove"
-										onClick={ () => {
-											setIsConfirmOpen( true );
-										} }
+									<DropdownMenu
+										popoverProps={ { position: 'bottom left' } }
+										controls={ [
+											{
+												title: 'Remove',
+												onClick: () => setIsConfirmOpen( true ),
+											},
+										] }
+										icon={ ellipsis }
+										label="More"
 									/>
 								</td>
 							</tr>
 						</tbody>
 					</table>
+					<Text as="p" isBlock align="center">
+						<Icon className="icon-info" icon={ info } size={ 16 } />
+						The current feature implementation only allows to set up two schedules.
+					</Text>
 				</CardBody>
-				<CardFooter>
-					<Button variant="primary" onClick={ () => setIsModalOpen( true ) }>
-						Create a new schedule
-					</Button>
-				</CardFooter>
 			</Card>
 		</>
 	);
