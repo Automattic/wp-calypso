2ac12ff887f17ab0c1fc96e8aa5f4fcd5e6e6935
Stéphane Thomas <email@stephanethomas.com>
2024-02-20 18:08:35 +0100

    Add skeleton of new `Patterns` page (#87647)

---
+++ client/my-sites/patterns/patterns.tsx
@@ -0,0 +1,18 @@
+import Main from 'calypso/components/main';
+
+import './style.scss';
+
+export default () => {
+	return (
+		<Main fullWidthLayout isLoggedOut>
+			<div>
+				<h1>Build your perfect site with patterns</h1>
+
+				<p>
+					Hundreds of expertly designed, fully responsive patterns allow you to craft a beautiful
+					site in minutes.
+				</p>
+			</div>
+		</Main>
+	);
+};
