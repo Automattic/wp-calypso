22e1107a60338d29dda69b304c190badcb2a9f7a
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-02-20 20:08:58 +0100

    Update schedule-form to match design

--- client/blocks/plugins-update-manager/schedule-form.tsx
+++ client/blocks/plugins-update-manager/schedule-form.tsx
@@ -1,12 +1,13 @@
 import {
 	TextControl,
-	TimePicker,
 	RadioControl,
+	SearchControl,
 	CheckboxControl,
 	__experimentalVStack as VStack,
 	Flex,
 	FlexItem,
 } from '@wordpress/components';
+import classnames from 'classnames';
 import { useState } from 'react';
 
 import './schedule-form.scss';
@@ -22,57 +23,76 @@ export const ScheduleForm = () => {
 				direction={ [ 'column', 'row' ] }
 				expanded={ true }
 				align="start"
-				gap={ 8 }
+				gap={ 12 }
 			>
 				<FlexItem>
-					<TextControl label="Name" value={ name } onChange={ setName } />
-					<TimePicker onChange={ function noRefCheck() {} } />
-					<RadioControl
-						label="Frequency"
-						onChange={ setFrequency }
-						options={ [
-							{
-								label: 'Daily',
-								value: 'daily',
-							},
-							{
-								label: 'Weekly',
-								value: 'weekly',
-							},
-						] }
-						selected={ frequency }
-					/>
+					<div className="form-field">
+						<label htmlFor="name">Name</label>
+						<TextControl id="name" value={ name } onChange={ setName } __next40pxDefaultSize />
+					</div>
+					<div className="form-field">
+						<label htmlFor="frequency">Update every</label>
+						<div className={ classnames( 'radio-option', { selected: frequency === 'daily' } ) }>
+							<RadioControl
+								name="frequency"
+								onChange={ setFrequency }
+								options={ [
+									{
+										label: 'Daily',
+										value: 'daily',
+									},
+								] }
+								selected={ frequency }
+							></RadioControl>
+						</div>
+						<div className={ classnames( 'radio-option', { selected: frequency === 'weekly' } ) }>
+							<RadioControl
+								name="frequency"
+								onChange={ setFrequency }
+								options={ [
+									{
+										label: 'Weekly',
+										value: 'weekly',
+									},
+								] }
+								selected={ frequency }
+							></RadioControl>
+						</div>
+					</div>
 				</FlexItem>
 				<FlexItem>
-					<VStack>
-						<label htmlFor="select-all">Plugins</label>
-						<CheckboxControl
-							id="select-all"
-							__nextHasNoMarginBottom
-							label="Select all"
-							onChange={ function noRefCheck() {} }
-						/>
-						<CheckboxControl
-							__nextHasNoMarginBottom
-							label="Move to WordPress.com"
-							onChange={ function noRefCheck() {} }
-						/>
-						<CheckboxControl
-							__nextHasNoMarginBottom
-							label="Akismet"
-							onChange={ function noRefCheck() {} }
-						/>
-						<CheckboxControl
-							__nextHasNoMarginBottom
-							label="Advance custom fields"
-							onChange={ function noRefCheck() {} }
-						/>
-						<CheckboxControl
-							__nextHasNoMarginBottom
-							label="Gravity forms"
-							onChange={ function noRefCheck() {} }
-						/>
-					</VStack>
+					<div className="form-field">
+						<label htmlFor="select-all">Select plugins</label>
+						<VStack className="checkbox-options">
+							<SearchControl onChange={ function noRefCheck() {} } />
+							<CheckboxControl
+								id="select-all"
+								__nextHasNoMarginBottom
+								label="Select all"
+								onChange={ function noRefCheck() {} }
+							/>
+							<CheckboxControl
+								__nextHasNoMarginBottom
+								label="Move to WordPress.com"
+								onChange={ function noRefCheck() {} }
+							/>
+							<CheckboxControl
+								__nextHasNoMarginBottom
+								label="Akismet"
+								onChange={ function noRefCheck() {} }
+							/>
+							<CheckboxControl
+								__nextHasNoMarginBottom
+								label="Advance custom fields"
+								onChange={ function noRefCheck() {} }
+							/>
+							<CheckboxControl
+								__nextHasNoMarginBottom
+								label="Gravity forms"
+								onChange={ function noRefCheck() {} }
+							/>
+						</VStack>
+					</div>
 				</FlexItem>
 			</Flex>
 		</form>
