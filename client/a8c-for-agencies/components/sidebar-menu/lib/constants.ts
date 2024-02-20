8798655b84a4a53f60a57a908a081b90a9146469
Yashwin Poojary <yashwinpoojary@gmail.com>
2024-02-20 14:58:27 +0530

    Automattic for Agencies: Add Marketplace menu item (#87637)

--- client/a8c-for-agencies/components/sidebar-menu/lib/constants.ts
+++ client/a8c-for-agencies/components/sidebar-menu/lib/constants.ts
@@ -3,3 +3,4 @@ export const A4A_SITES_LINK = '/sites';
 export const A4A_SITES_LINK_NEEDS_ATTENTION = '/sites?issue_types=all_issues';
 export const A4A_SITES_LINK_FAVORITE = '/sites/favorite';
 export const A4A_PLUGINS_LINK = '/plugins';
+export const A4A_MARKETPLACE_LINK = '/marketplace';
