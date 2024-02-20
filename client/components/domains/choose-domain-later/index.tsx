0ef945fa0dce2705169da5b233dc3e21dde9e0d6
James Tien <southp0105@gmail.com>
2024-02-20 18:11:38 +0800

    Remove unnecessary React import

--- client/components/domains/choose-domain-later/index.tsx
+++ client/components/domains/choose-domain-later/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useExperiment } from 'calypso/lib/explat';
 import ReskinSideExplainer from '../reskin-side-explainer';
 
