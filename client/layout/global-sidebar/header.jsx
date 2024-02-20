2b4600eacc3e53c184a7a4f0ea17815b2b378f7b
Addison Stavlo <Stavz01@gmail.com>
2024-02-20 13:47:06 -0500

    update to ellipsis character (#87661)

--- client/layout/global-sidebar/header.jsx
+++ client/layout/global-sidebar/header.jsx
@@ -14,7 +14,7 @@ export const GlobalSidebarHeader = () => {
 				<span className="dotcom"></span>
 			</a>
 			<span className="gap"></span>
-			<SidebarSearch tooltip={ translate( 'Jump to...' ) } />
+			<SidebarSearch tooltip={ translate( 'Jump to…' ) } />
 			<SidebarNotifications
 				isShowing={ false }
 				isActive={ true }
