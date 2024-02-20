bec9dde66b3c10a6501ad94266e5e9883cd4874f
Gabriel Caires <gabriel.caires@automattic.com>
2024-02-20 18:48:58 +0000

    Remove tracking from the bio module

--- client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/bio/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/launchpad/task-definitions/bio/index.tsx
@@ -5,7 +5,6 @@ import { __ } from '@wordpress/i18n';
 import { addQueryArgs } from '@wordpress/url';
 import { ONBOARD_STORE } from 'calypso/landing/stepper/stores';
 import { getSiteIdOrSlug } from '../../task-helper';
-import { recordTaskClickTracksEvent } from '../../tracking';
 import { type TaskAction } from '../../types';
 
 export const getSetupLinkInBioTask: TaskAction = ( task, flow, context ) => {
@@ -13,7 +12,6 @@ export const getSetupLinkInBioTask: TaskAction = ( task, flow, context ) => {
 
 	return {
 		...task,
-		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
 		calypso_path: addQueryArgs( `/setup/link-in-bio-post-setup/linkInBioPostSetup`, {
 			...getSiteIdOrSlug( flow, site, siteSlug ),
 		} ),
@@ -21,7 +19,7 @@ export const getSetupLinkInBioTask: TaskAction = ( task, flow, context ) => {
 	};
 };
 
-export const getLinkInBioLaunchedTask: TaskAction = ( task, flow, context ) => {
+export const getLinkInBioLaunchedTask: TaskAction = ( task, _, context ) => {
 	const { siteSlug, site, submit } = context;
 	return {
 		...task,
@@ -37,7 +35,6 @@ export const getLinkInBioLaunchedTask: TaskAction = ( task, flow, context ) => {
 
 					// Waits for half a second so that the loading screen doesn't flash away too quickly
 					await new Promise( ( res ) => setTimeout( res, 500 ) );
-					recordTaskClickTracksEvent( task, flow, context );
 					return { goToHome: true, siteSlug };
 				} );
 
@@ -47,10 +44,9 @@ export const getLinkInBioLaunchedTask: TaskAction = ( task, flow, context ) => {
 	};
 };
 
-export const getLinksAddedTask: TaskAction = ( task, flow, context ) => {
+export const getLinksAddedTask: TaskAction = ( task ) => {
 	return {
 		...task,
-		actionDispatch: () => recordTaskClickTracksEvent( task, flow, context ),
 		calyso_path: addQueryArgs( task.calypso_path, { canvas: 'edit' } ),
 		useCalypsoPath: true,
 	};
