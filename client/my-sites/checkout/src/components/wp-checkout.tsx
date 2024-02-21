51b7627a07be0bb1f39292ca38a2088bfc4438a2
Chris Diehl <jjchris.diehl@gmail.com>
2024-02-20 17:18:06 -0800

    Checkoutv2: Style and functionality cleanup (#87318)
    * Update button fonts
    
    * Update coupon field styles
    
    * Update discount text size
    
    * Update sidebar total styles
    
    * Add styles for sidebar tablets
    
    * Translate Coupon Code text
    
    * Clean up tablet styling
    
    * Remove Recoleta from total on v1
    
    * Fix style leak for tablets

--- client/my-sites/checkout/src/components/wp-checkout.tsx
+++ client/my-sites/checkout/src/components/wp-checkout.tsx
@@ -768,27 +768,72 @@ const CheckoutSummaryTitlePrice = styled.span`
 
 const CheckoutSummaryBody = styled.div`
 	box-sizing: border-box;
-	display: none;
 	margin: 0 auto;
 	max-width: 600px;
 	width: 100%;
-	padding: 24px;
+	display: none;
+
+	${ hasCheckoutVersion( '2' ) ? `padding: 32px 24px 24px 24px;` : 'padding: 24px;' }
 
 	.is-visible & {
-		display: block;
+		${ hasCheckoutVersion( '2' )
+			? ` display: grid;
+			grid-template-areas:
+			"preview"
+			"review"
+			"summary"
+			"nudge"
+			"features";`
+			: `display: block;` };
+	}
+
+	& .checkout-site-preview {
+		grid-area: preview;
+		display: none;
+	}
+
+	& .checkout-review-order {
+		grid-area: review;
 	}
 
 	@media ( ${ ( props ) => props.theme.breakpoints.tabletUp } ) {
-		padding: 24px;
+		${ hasCheckoutVersion( '2' ) ? `padding: 50px 24px 24px 24px;` : 'padding: 24px;' }
+
+		.is-visible & {
+			${ hasCheckoutVersion( '2' ) &&
+			`grid-template-areas:
+			"preview preview"
+			"review review"
+			"summary summary"
+			"features nudge"
+			` };
+		}
 	}
 
 	@media ( ${ ( props ) => props.theme.breakpoints.desktopUp } ) {
-		display: block;
 		max-width: 328px;
 		padding: 0;
 
+		.is-visible &,
+		& {
+			${ hasCheckoutVersion( '2' )
+				? `grid-template-areas:
+					"preview"
+					"review"
+					"summary"
+					"nudge"
+					"features";
+					display: grid;
+			`
+				: `display: block;` };
+		}
+
 		& .card {
 			box-shadow: none;
+		}
+
+		& .checkout-site-preview {
+			display: block;
 		}
 	}
 `;
@@ -796,6 +841,7 @@ const CheckoutSummaryBody = styled.div`
 const CheckoutSidebarNudgeWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
+	${ hasCheckoutVersion( '2' ) && `grid-area: nudge` };
 
 	& > * {
 		max-width: 288px;
