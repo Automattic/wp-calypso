859078c98819eeca095333d487ca429811b09d23
Stéphane Thomas <email@stephanethomas.com>
2024-02-22 17:31:28 +0100

    Fix type error in unit tests

--- client/my-sites/patterns/index.web.ts
+++ client/my-sites/patterns/index.web.ts
@@ -4,18 +4,20 @@ import {
 	makeLayout,
 	redirectWithoutLocaleParamInFrontIfLoggedIn,
 	render as clientRender,
-} from 'calypso/controller';
+} from 'calypso/controller/index.web';
 import { fetchPatterns, renderPatterns } from 'calypso/my-sites/patterns/controller';
 
 export default function ( router: typeof clientRouter ) {
 	const langParam = getLanguageRouteParam();
 
 	router(
-		[ '/patterns', `/${ langParam }/patterns` ],
+		`/${ langParam }/patterns`,
 		redirectWithoutLocaleParamInFrontIfLoggedIn,
 		fetchPatterns,
 		renderPatterns,
 		makeLayout,
 		clientRender
 	);
+
+	router( '/patterns', fetchPatterns, renderPatterns, makeLayout, clientRender );
 }
