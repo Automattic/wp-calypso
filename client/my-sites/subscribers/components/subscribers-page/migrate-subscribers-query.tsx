c5b2077992e351d37426312789d4edf29fa8bd43
Miguel <miguel.san.segundo@automattic.com>
2024-02-23 22:44:07 +0700

    Fix for response in Calypso green

--- client/my-sites/subscribers/components/subscribers-page/migrate-subscribers-query.tsx
+++ client/my-sites/subscribers/components/subscribers-page/migrate-subscribers-query.tsx
@@ -4,12 +4,22 @@ type Response = {
 	success: boolean;
 	message: string;
 };
+
+type ResponseWithBody = {
+	body: Response;
+};
+
 function migrateSubscribers( sourceSiteId: number, targetSiteId: number ): Promise< Response > {
-	return wpcom.req.post(
-		`/jetpack-blogs/${ encodeURIComponent( targetSiteId ) }/source/${ encodeURIComponent(
-			sourceSiteId
-		) }/migrate?http_envelope=1`
-	);
+	return wpcom.req
+		.post(
+			`/jetpack-blogs/${ encodeURIComponent( targetSiteId ) }/source/${ encodeURIComponent(
+				sourceSiteId
+			) }/migrate?http_envelope=1`
+		)
+		.then( ( data: ResponseWithBody & Response ) => {
+			// In Calypso green the response has body
+			return data.body ?? data;
+		} );
 }
 
 export { migrateSubscribers };
