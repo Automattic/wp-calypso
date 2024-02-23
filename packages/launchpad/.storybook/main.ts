8c1817a7711d3b4ada6186bb63db3cb360e4a2a7
Gabriel Caires <gabriel.caires@automattic.com>
2024-02-23 13:16:11 +0000

    Disable telemetry

--- packages/launchpad/.storybook/main.ts
+++ packages/launchpad/.storybook/main.ts
@@ -1,4 +1,3 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
 import storybookDefaultConfig from '@automattic/calypso-storybook';
 const config = storybookDefaultConfig();
 
@@ -8,6 +7,10 @@ export default {
 	typescript: {
 		...config.typescript,
 		reactDocgen: 'react-docgen-typescript',
+	},
+	core: {
+		...config.core,
+		disableTelemetry: true,
 	},
 	addons: [ ...( config.addons ?? [] ), '@storybook/addon-a11y' ],
 };
