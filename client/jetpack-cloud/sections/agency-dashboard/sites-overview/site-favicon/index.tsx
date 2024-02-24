3ed799648f009783e62bb02fdf561ec15ebf815b
tbradsha <32492176+tbradsha@users.noreply.github.com>
2024-02-23 18:18:37 -0700

    Add optional support for site color

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-favicon/index.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-favicon/index.tsx
@@ -12,10 +12,12 @@ interface SiteFaviconProps {
 }
 
 const SiteFavicon = ( { site, size = 40, className = '' }: SiteFaviconProps ) => {
+	const siteColor = site.site_color ?? 'linear-gradient(45deg, #ff0056, #ff8a78, #57b7ff, #9c00d4)';
+
 	const defaultFavicon = site.is_atomic ? (
 		<WordPressLogo className="wpcom-favicon" size={ size * 0.8 } />
 	) : (
-		<div className="no-favicon" />
+		<div className="no-favicon" style={ { background: siteColor } } />
 	);
 
 	return (
