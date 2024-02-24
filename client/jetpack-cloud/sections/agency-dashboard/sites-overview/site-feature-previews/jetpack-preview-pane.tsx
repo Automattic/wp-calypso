de8c293a53f59ea6aa7939288014fc6704e285f5
cleacos <javacodia@gmail.com>
2024-02-24 03:49:03 +0000

    Jetpack context wrapper for the SitePreviewPane

---
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-feature-previews/jetpack-preview-pane.tsx
@@ -0,0 +1,63 @@
+import { useTranslate } from 'i18n-calypso';
+import { useState } from 'react';
+import SitePreviewPane from 'calypso/jetpack-cloud/sections/agency-dashboard/sites-overview/site-preview-pane';
+import { SitePreviewPaneProps } from '../site-preview-pane/types';
+import { JetpackBackupPreview } from './jetpack-backup';
+import { JetpackBoostPreview } from './jetpack-boost';
+import { JetpackMonitorPreview } from './jetpack-monitor';
+import { JetpackStatsPreview } from './jetpack-stats';
+
+export function JetpackPreviewPane( { site, closeSitePreviewPane }: SitePreviewPaneProps ) {
+	const translate = useTranslate();
+
+	const [ selectedFeatureId, setSelectedFeatureId ] = useState( 'jetpack_boost' );
+
+	// Jetpack features: Boost, Backup, Monitor, Stats
+	const features = [
+		{
+			id: 'jetpack_boost',
+			tab: {
+				label: translate( 'Boost' ),
+				selected: selectedFeatureId === 'jetpack_boost',
+				onTabClick: () => setSelectedFeatureId( 'jetpack_boost' ),
+			},
+			preview: <JetpackBoostPreview site={ site } />,
+		},
+		{
+			id: 'jetpack_backup',
+			tab: {
+				label: translate( 'Backup' ),
+				selected: selectedFeatureId === 'jetpack_backup',
+				onTabClick: () => setSelectedFeatureId( 'jetpack_backup' ),
+			},
+			preview: <JetpackBackupPreview site={ site } />,
+		},
+		{
+			id: 'jetpack_monitor',
+			tab: {
+				label: translate( 'Monitor' ),
+				selected: selectedFeatureId === 'jetpack_monitor',
+				onTabClick: () => setSelectedFeatureId( 'jetpack_monitor' ),
+			},
+			preview: <JetpackMonitorPreview site={ site } />,
+		},
+		{
+			id: 'jetpack_stats',
+			tab: {
+				label: translate( 'Stats' ),
+				selected: selectedFeatureId === 'jetpack_stats',
+				onTabClick: () => setSelectedFeatureId( 'jetpack_stats' ),
+			},
+			preview: <JetpackStatsPreview site={ site } />,
+		},
+	];
+
+	return (
+		<SitePreviewPane
+			site={ site }
+			closeSitePreviewPane={ closeSitePreviewPane }
+			features={ features }
+			selectedFeatureId={ selectedFeatureId }
+		/>
+	);
+}
