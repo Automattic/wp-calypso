0659072f30face4a16b3e2a4a9dc00e9c47cff41
cleacos <javacodia@gmail.com>
2024-02-26 10:32:35 +0000

    Update PreviewPaneTabs to use className and the field visible.

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-preview-pane/site-preview-pane-tabs/index.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-preview-pane/site-preview-pane-tabs/index.tsx
@@ -1,26 +1,32 @@
 import { Button } from '@automattic/components';
+import classNames from 'classnames';
 import { FeatureTabInterface } from '../types';
 
 import './style.scss';
 
 export default function SitePreviewPaneTabs( {
 	featureTabs,
+	className,
 }: {
 	featureTabs: FeatureTabInterface[];
+	className?: string;
 } ) {
 	return (
 		<>
-			<div className="site-preview__tabs">
-				{ featureTabs.map( ( featureTab: FeatureTabInterface ) => (
-					<Button
-						key={ featureTab.label }
-						className={ 'site-preview__tab' + ( featureTab.selected ? ' is-selected' : '' ) }
-						onClick={ () => featureTab.onTabClick?.() }
-						borderless
-					>
-						{ featureTab.label }
-					</Button>
-				) ) }
+			<div className={ classNames( 'site-preview__tabs', className ) }>
+				{ featureTabs.map(
+					( featureTab: FeatureTabInterface ) =>
+						featureTab.visible && (
+							<Button
+								key={ featureTab.label }
+								className={ 'site-preview__tab' + ( featureTab.selected ? ' is-selected' : '' ) }
+								onClick={ () => featureTab.onTabClick?.() }
+								borderless
+							>
+								{ featureTab.label }
+							</Button>
+						)
+				) }
 			</div>
 		</>
 	);
