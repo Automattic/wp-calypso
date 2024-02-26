8b8dbac34030b7abbf30dcf582813a3c437aa252
cleacos <javacodia@gmail.com>
2024-02-26 10:38:18 +0000

    Update JetpackPreviewPane to use the createFeaturePreview helper function

--- client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-feature-previews/jetpack-preview-pane.tsx
+++ client/jetpack-cloud/sections/agency-dashboard/sites-overview/site-feature-previews/jetpack-preview-pane.tsx
@@ -1,13 +1,18 @@
+import page from '@automattic/calypso-router';
 import { useTranslate } from 'i18n-calypso';
-import { useMemo, useState } from 'react';
-import SitePreviewPane from 'calypso/jetpack-cloud/sections/agency-dashboard/sites-overview/site-preview-pane';
+import React, { useMemo, useState } from 'react';
+import SitePreviewPane, { createFeaturePreview } from '../site-preview-pane';
 import { SitePreviewPaneProps } from '../site-preview-pane/types';
 import { JetpackBackupPreview } from './jetpack-backup';
 import { JetpackBoostPreview } from './jetpack-boost';
 import { JetpackMonitorPreview } from './jetpack-monitor';
 import { JetpackStatsPreview } from './jetpack-stats';
 
-export function JetpackPreviewPane( { site, closeSitePreviewPane }: SitePreviewPaneProps ) {
+export function JetpackPreviewPane( {
+	site,
+	closeSitePreviewPane,
+	className,
+}: SitePreviewPaneProps ) {
 	const translate = useTranslate();
 
 	const [ selectedFeatureId, setSelectedFeatureId ] = useState( 'jetpack_boost' );
@@ -15,46 +20,54 @@ export function JetpackPreviewPane( { site, closeSitePreviewPane }: SitePreviewP
 	// Jetpack features: Boost, Backup, Monitor, Stats
 	const features = useMemo(
 		() => [
-			{
-				id: 'jetpack_boost',
-				tab: {
-					label: 'Boost',
-					selected: selectedFeatureId === 'jetpack_boost',
-					onTabClick: () => setSelectedFeatureId( 'jetpack_boost' ),
-				},
-				enabled: true,
-				preview: <JetpackBoostPreview site={ site } />,
-			},
-			{
-				id: 'jetpack_backup',
-				tab: {
-					label: 'Backup',
-					selected: selectedFeatureId === 'jetpack_backup',
-					onTabClick: () => setSelectedFeatureId( 'jetpack_backup' ),
-				},
-				enabled: true,
-				preview: <JetpackBackupPreview site={ site } />,
-			},
-			{
-				id: 'jetpack_monitor',
-				tab: {
-					label: 'Monitor',
-					selected: selectedFeatureId === 'jetpack_monitor',
-					onTabClick: () => setSelectedFeatureId( 'jetpack_monitor' ),
-				},
-				enabled: true,
-				preview: <JetpackMonitorPreview site={ site } />,
-			},
-			{
-				id: 'jetpack_stats',
-				tab: {
-					label: 'Stats',
-					selected: selectedFeatureId === 'jetpack_stats',
-					onTabClick: () => setSelectedFeatureId( 'jetpack_stats' ),
-				},
-				enabled: true,
-				preview: <JetpackStatsPreview site={ site } />,
-			},
+			createFeaturePreview(
+				'jetpack_boost',
+				'Boost',
+				true,
+				selectedFeatureId,
+				setSelectedFeatureId,
+				<JetpackBoostPreview site={ site } />
+			),
+			createFeaturePreview(
+				'jetpack_backup',
+				'Backup',
+				true,
+				selectedFeatureId,
+				setSelectedFeatureId,
+				<JetpackBackupPreview site={ site } />
+			),
+			createFeaturePreview(
+				'jetpack_monitor',
+				'Monitor',
+				true,
+				selectedFeatureId,
+				setSelectedFeatureId,
+				<JetpackMonitorPreview site={ site } />
+			),
+			createFeaturePreview(
+				'jetpack_stats',
+				'Stats',
+				true,
+				selectedFeatureId,
+				setSelectedFeatureId,
+				<JetpackStatsPreview site={ site } />
+			),
+			createFeaturePreview(
+				'jetpack_scan',
+				'Scan',
+				true,
+				selectedFeatureId,
+				() => page( '/scan/' + site.url ),
+				null
+			),
+			createFeaturePreview(
+				'jetpack_plugins',
+				'Plugins',
+				true,
+				selectedFeatureId,
+				() => page( '/plugins/manage/' + site.url ),
+				null
+			),
 		],
 		[ site, selectedFeatureId, translate ]
 	);
@@ -64,6 +77,7 @@ export function JetpackPreviewPane( { site, closeSitePreviewPane }: SitePreviewP
 			site={ site }
 			closeSitePreviewPane={ closeSitePreviewPane }
 			features={ features }
+			className={ className }
 		/>
 	);
 }
