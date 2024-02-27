071e2f0463735ea8fa41b95f768912b266e44c74
Dehan <dehandecroos@gmail.com>
2024-02-27 13:08:52 -0500

    Track order of shuffled elements

--- client/signup/steps/new-user-survey/components.tsx
+++ client/signup/steps/new-user-survey/components.tsx
@@ -1,6 +1,6 @@
 import { Card, FormLabel } from '@automattic/components';
 import styled from '@emotion/styled';
-import { ReactNode, useMemo } from 'react';
+import { Children, ReactNode, isValidElement, useEffect } from 'react';
 import FormTextInput from 'calypso/components/forms/form-text-input';
 
 export type Viewport = 'desktop' | 'mobile' | 'tablet';
@@ -55,18 +55,77 @@ export const ButtonContainer = styled.div`
 	padding-top: 10px;
 `;
 
+export const getCheckboxKey = ( child: ReactNode ): string => {
+	if ( ! isValidElement( child ) || ! child.props.children ) {
+		return '';
+	}
+	const shuffledChildren = Children.toArray( child.props.children );
+	const [ checkboxNode ] = shuffledChildren;
+
+	if ( isValidElement( checkboxNode ) && checkboxNode.props.name ) {
+		return checkboxNode.props.name;
+	}
+	return '';
+};
+
+type ShuffleProps = {
+	/**
+	 * An array of ReactNode elements, representing the children to be shuffled.
+	 */
+	children: Array< ReactNode >;
+
+	/**
+	 * An array of strings or null, indicating the current order of children.
+	 * If null, the children will be shuffled initially.
+	 */
+	childOrder: string[] | null;
+
+	/**
+	 * A state setter function responsible for updating the child order.
+	 * @param {string[] | null} value - The new child order or null to shuffle the children.
+	 */
+	setChildOrder: React.Dispatch< React.SetStateAction< string[] | null > >;
+
+	/**
+	 * A function that extracts a unique key from each child element, used for maintaining consistent order.
+	 * @param {ReactNode} child - The child element for which the key is to be extracted.
+	 * @returns {string} The key extracted from the child element.
+	 */
+	getChildKey: ( child: ReactNode ) => string;
+};
 /**
- *	Shuffle the children so that element order is randomized
+ * A controlled component which shuffles children by random order based on provided keys.
+ * It ensures the order of children remains consistent when specified keys are provided.
+ * @param {ShuffleProps} props - The props for the Shuffle component.
+ * @returns {ReactNode} The shuffled children.
  */
-export const Shuffle = ( props: { children: Array< ReactNode > } ) => {
-	const { children } = props;
+export const Shuffle = ( props: ShuffleProps ) => {
+	const { children, setChildOrder, childOrder, getChildKey } = props;
 
-	const shuffledChildren = useMemo( () => {
-		const clonedChildren = [ ...children ];
-		clonedChildren.sort( () => Math.random() - Math.random() );
-		return clonedChildren;
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [ children.length ] );
+	useEffect( () => {
+		setChildOrder( ( prevChildOrder ) => {
+			// If the previous child order is null, shuffle the children based on provided keys
+			if ( prevChildOrder === null || prevChildOrder.length !== Children.count( children ) ) {
+				const newChildOrder = Children.toArray( children ).map( ( child ) => {
+					return getChildKey( child );
+				} );
+				newChildOrder.sort( () => Math.random() - Math.random() );
+				return newChildOrder;
+			}
+			// If the previous child order exists, maintain it
+			return prevChildOrder;
+		} );
+	}, [ children, setChildOrder, getChildKey ] );
 
-	return shuffledChildren;
+	// If a child order exists, sort the children based on that order
+	let sortedChildrenClone = children;
+	if ( childOrder !== null ) {
+		sortedChildrenClone = [ ...children ];
+		sortedChildrenClone.sort( ( child1, child2 ) => {
+			return (
+				childOrder.indexOf( getChildKey( child1 ) ) - childOrder.indexOf( getChildKey( child2 ) )
+			);
+		} );
+	}
+	return sortedChildrenClone;
 };
