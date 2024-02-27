7cf7256e9eef2c2f3a351c00f97ce1f693a6167d
vykes-mac <knrymcleish@gmail.com>
2024-02-27 13:56:45 -0500

    fix typo

--- client/my-sites/github-deployments/deployments/no-results.tsx
+++ client/my-sites/github-deployments/deployments/no-results.tsx
@@ -3,7 +3,7 @@ import { __ } from '@wordpress/i18n';
 export const NoResults = () => {
 	return (
 		<div className="github-deployments-list__no-results">
-			<p>{ __( 'No Results found' ) }</p>
+			<p>{ __( 'No results found' ) }</p>
 			<p>{ __( 'Please connect a repository to your site to see deployments.' ) }</p>
 		</div>
 	);
