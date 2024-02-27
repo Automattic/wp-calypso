94f8f07ea1750d6c122f86acc7397fbc4f070f9a
vykes-mac <knrymcleish@gmail.com>
2024-02-26 19:30:39 -0500

    add css styling for no results in deployments list

--- client/my-sites/github-deployments/deployments/no-results.tsx
+++ client/my-sites/github-deployments/deployments/no-results.tsx
@@ -2,7 +2,7 @@ import { __ } from '@wordpress/i18n';
 
 export const NoResults = () => {
 	return (
-		<div className="github-deployments-repositories__no-results">
+		<div className="github-deployments-list__no-results">
 			<p>{ __( 'No Results found' ) }</p>
 			<p>{ __( 'Please connect a repository to your site to see deployments.' ) }</p>
 		</div>
