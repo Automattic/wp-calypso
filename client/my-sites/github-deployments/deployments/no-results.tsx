57dc6d4a36a524eef18d480d1d204dd1d69da31f
vykes-mac <knrymcleish@gmail.com>
2024-02-26 19:21:58 -0500

    add a no results component for deployments filter

---
+++ client/my-sites/github-deployments/deployments/no-results.tsx
@@ -0,0 +1,10 @@
+import { __ } from '@wordpress/i18n';
+
+export const NoResults = () => {
+	return (
+		<div className="github-deployments-repositories__no-results">
+			<p>{ __( 'No Results found' ) }</p>
+			<p>{ __( 'Please connect a repository to your site to see deployments.' ) }</p>
+		</div>
+	);
+};
