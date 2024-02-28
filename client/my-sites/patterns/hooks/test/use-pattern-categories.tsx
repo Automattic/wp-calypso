988ee3279ab3d24ef5a32035b202f490d89bf796
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2024-02-28 16:37:27 +0100

    Fix tests

--- client/my-sites/patterns/hooks/test/use-pattern-categories.tsx
+++ client/my-sites/patterns/hooks/test/use-pattern-categories.tsx
@@ -6,18 +6,19 @@ import { renderHook, waitFor } from '@testing-library/react';
 import React from 'react';
 import wpcom from 'calypso/lib/wp';
 import { usePatternCategories } from '../use-pattern-categories';
+import type { Category } from 'calypso/landing/stepper/declarative-flow/internals/steps-repository/pattern-assembler/types';
 
 jest.mock( 'calypso/lib/wp', () => ( { req: { get: jest.fn() } } ) );
 
 describe( 'usePatternCategories', () => {
-	const queryClient = new QueryClient( { defaultOptions: { queries: { retry: false } } } );
-
 	let wrapper: React.FC< React.PropsWithChildren< any > >;
 
 	beforeEach( () => {
 		( wpcom.req.get as jest.MockedFunction< typeof wpcom.req.get > ).mockReset();
 
-		wrapper = ( { children } ) => (
+		const queryClient = new QueryClient( { defaultOptions: { queries: { retry: false } } } );
+
+		wrapper = ( { children }: React.PropsWithChildren< any > ) => (
 			<QueryClientProvider client={ queryClient }>{ children }</QueryClientProvider>
 		);
 	} );
@@ -42,7 +43,7 @@ describe( 'usePatternCategories', () => {
 	} );
 
 	test( 'returns the expected data when successful', async () => {
-		const categories = [
+		const categories: Category[] = [
 			{
 				name: 'about',
 				title: 'À propos',
