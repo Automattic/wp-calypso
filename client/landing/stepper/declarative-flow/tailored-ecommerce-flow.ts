b176ab3d55d5ac22def1db86a388c25ce5a0fe49
daledupreez <dale@automattic.com>
2024-02-28 10:10:09 +0200

    Allow Stepper flows to return custom signup start event props (#87960)
    * Allow Stepper flows to return custom signup start event props
    * Add docs for new hook to Stepper README

--- client/landing/stepper/declarative-flow/tailored-ecommerce-flow.ts
+++ client/landing/stepper/declarative-flow/tailored-ecommerce-flow.ts
@@ -48,20 +48,16 @@ function getPlanFromRecurType( recurType: string ) {
 
 const ecommerceFlow: Flow = {
 	name: ECOMMERCE_FLOW,
-	/* This is technically a signup flow, but we're manually triggering an event in useSteps()
-	 * for now, so we're disabling the centralised tracking for calypso_signup_start events.
-	 */
-	isSignupFlow: false,
-	useSteps() {
-		const recurType = useSelect(
+	isSignupFlow: true,
+	useSignupStartEventProps() {
+		const recur = useSelect(
 			( select ) => ( select( ONBOARD_STORE ) as OnboardSelect ).getEcommerceFlowRecurType(),
 			[]
 		);
 
-		useEffect( () => {
-			recordTracksEvent( 'calypso_signup_start', { flow: this.name, recur: recurType } );
-		}, [] );
-
+		return { recur };
+	},
+	useSteps() {
 		return [
 			{
 				slug: 'storeProfiler',
