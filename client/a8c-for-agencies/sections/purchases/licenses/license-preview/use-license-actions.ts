43127db37075bb9fbb64d31da578ba92c184c225
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-02-29 17:19:45 +0800

    Fix PR comments.

--- client/a8c-for-agencies/sections/purchases/licenses/license-preview/use-license-actions.ts
+++ client/a8c-for-agencies/sections/purchases/licenses/license-preview/use-license-actions.ts
@@ -6,7 +6,6 @@ import {
 	LicenseAction,
 	LicenseType,
 } from 'calypso/jetpack-cloud/sections/partner-portal/types';
-import { urlToSlug } from 'calypso/lib/url/http-utils';
 import { useDispatch } from 'calypso/state';
 import { recordTracksEvent } from 'calypso/state/analytics/actions';
 
@@ -25,8 +24,6 @@ export default function useLicenseActions(
 			return [];
 		}
 
-		const siteSlug = urlToSlug( siteUrl );
-
 		const handleClickMenuItem = ( eventName: string ) => {
 			dispatch( recordTracksEvent( eventName ) );
 		};
@@ -35,44 +32,42 @@ export default function useLicenseActions(
 		return [
 			{
 				name: translate( 'Set up site' ),
-				href: `https://wordpress.com/home/${ siteSlug }`,
-				onClick: () => handleClickMenuItem( 'calypso_jetpack_licenses_site_set_up_click' ),
+				href: '', // FIXME: Add correct URL
+				onClick: () => handleClickMenuItem( 'calypso_a4a_licenses_site_set_up_click' ),
 				isExternalLink: true,
 				isEnabled: true,
 			},
 			{
 				name: translate( 'Change domain' ),
-				href: `https://wordpress.com/domains/manage/${ siteSlug }`,
-				onClick: () => handleClickMenuItem( 'calypso_jetpack_licenses_change_domain_click' ),
+				href: '', // FIXME: Add correct URL
+				onClick: () => handleClickMenuItem( 'calypso_a4a_licenses_change_domain_click' ),
 				isExternalLink: true,
 				isEnabled: true,
 			},
 			{
 				name: translate( 'Hosting configuration' ),
-				href: `https://wordpress.com/hosting-config/${ siteSlug }`,
-				onClick: () =>
-					handleClickMenuItem( 'calypso_jetpack_licenses_hosting_configuration_click' ),
+				href: '', // FIXME: Add correct URL
+				onClick: () => handleClickMenuItem( 'calypso_a4a_licenses_hosting_configuration_click' ),
 				isExternalLink: true,
 				isEnabled: true,
 			},
 			{
 				name: translate( 'Edit site in WP Admin' ),
-				href: `${ siteUrl }/wp-admin/admin.php?page=jetpack#/dashboard`,
-				onClick: () => handleClickMenuItem( 'calypso_jetpack_licenses_edit_site_click' ),
+				href: '', // FIXME: Add correct URL
+				onClick: () => handleClickMenuItem( 'calypso_a4a_licenses_edit_site_click' ),
 				isExternalLink: true,
 				isEnabled: true,
 			},
 			{
 				name: translate( 'Debug site' ),
-				href: `https://jptools.wordpress.com/debug/?url=${ siteUrl }`,
-				onClick: () => handleClickMenuItem( 'calypso_jetpack_licenses_debug_site_click' ),
+				href: '', // FIXME: Add correct URL
+				onClick: () => handleClickMenuItem( 'calypso_a4a_licenses_debug_site_click' ),
 				isExternalLink: true,
 				isEnabled: licenseState === LicenseState.Attached,
 			},
 			{
 				name: translate( 'Revoke' ),
-				onClick: () =>
-					handleClickMenuItem( 'calypso_jetpack_licenses_hosting_configuration_click' ),
+				onClick: () => handleClickMenuItem( 'calypso_a4a_licenses_hosting_configuration_click' ),
 				type: 'revoke',
 				isEnabled:
 					( isChildLicense
