3aa165698a53548247358a26fc8b8936565f36e1
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-02-29 18:10:47 +0100

    Create module config

---
+++ client/blocks/plugins-update-manager/config.ts
@@ -0,0 +1,2 @@
+export const MAX_SCHEDULES = 2;
+export const MAX_SELECTABLE_PLUGINS = 10;
--- client/blocks/plugins-update-manager/schedule-form.const.ts
+++ client/blocks/plugins-update-manager/schedule-form.const.ts
@@ -1,5 +1,3 @@
-export const MAX_SELECTABLE_PLUGINS = 10;
-
 export const DAILY_OPTION = {
 	label: 'Daily',
 	value: 'daily',
