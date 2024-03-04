d96192df0f8eabed7d4a10ebb5d86ec2730dc829
Andrés Blanco <email@gmail.com>
2024-03-04 16:55:25 -0300

    first commit

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-import-or-migrate/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-import-or-migrate/index.tsx
@@ -1,6 +1,10 @@
 import { Button } from '@automattic/components';
 import { StepContainer } from '@automattic/onboarding';
 import { useTranslate } from 'i18n-calypso';
+import {
+	StandAloneComparisonGrid,
+	Column,
+} from 'calypso/../packages/components/src/standalone-comparison-grid';
 import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import type { Step } from '../../types';
@@ -22,25 +26,49 @@ const SiteMigrationImportOrMigrate: Step = function ( { navigation } ) {
 	};
 
 	const stepContent = (
-		<div className="migration-info">
-			<div className="migration-info-column">
-				<h2>{ translate( 'Import' ) }</h2>
-				<Button primary onClick={ () => handleSubmit( 'import' ) }>
-					{ translate( 'Import my website content' ) }
-				</Button>
-			</div>
-			<div className="migration-info-column">
-				<h2>{ translate( 'Migrate' ) }</h2>
-				<Button
-					primary
-					onClick={ () => {
-						handleSubmit( canInstallPlugins ? 'migrate' : 'upgrade' );
-					} }
-				>
-					{ canInstallPlugins ? translate( 'Migrate my site' ) : translate( 'Upgrade to migrate' ) }
-				</Button>
-			</div>
-		</div>
+		<StandAloneComparisonGrid>
+			<Column
+				title={ translate( 'Import' ) }
+				introCopy={ translate( 'Import posts, pages, and media from our supported platforms.' ) }
+				features={ [
+					translate( 'Import your content from a WordPress export file' ),
+					translate( 'Import your content from Blogger' ),
+					translate( 'Import your content from Medium' ),
+					translate( 'Import your content from Squarespace' ),
+					translate( 'Import your content from Substack' ),
+					translate( 'Import your content from Wix' ),
+					translate( 'Import your content from other platforms' ),
+				] }
+				controls={
+					<Button primary onClick={ () => handleSubmit( 'import' ) }>
+						{ translate( 'Import my website content' ) }
+					</Button>
+				}
+			/>
+			<Column
+				title={ translate( 'Migrate' ) }
+				introCopy={ translate(
+					'This will copy your existing WordPress site to your new WordPress.com site, including your content, media, plugins, and theme.'
+				) }
+				features={ [
+					translate( 'Import your content' ),
+					translate( 'Import your theme' ),
+					translate( 'Import your plugins' ),
+				] }
+				controls={
+					<Button
+						primary
+						onClick={ () => {
+							handleSubmit( canInstallPlugins ? 'migrate' : 'upgrade' );
+						} }
+					>
+						{ canInstallPlugins
+							? translate( 'Migrate my site' )
+							: translate( 'Upgrade to migrate' ) }
+					</Button>
+				}
+			/>
+		</StandAloneComparisonGrid>
 	);
 
 	return (
