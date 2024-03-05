5a83fa8dfc8e1dd7a3a8a74b6ff9cc9906ccc118
James Tien <southp0105@gmail.com>
2024-03-06 05:57:54 +0800

    Add specific impression & skip button click events to the signup survey form (#88176)
    * Add the event `calypso_signup_new_user_survey_skip_button_click` upon
    clicking the skip button.
    
    * Add the event `calypso_signup_new_user_survey_view`

--- client/signup/steps/new-user-survey/index.tsx
+++ client/signup/steps/new-user-survey/index.tsx
@@ -6,6 +6,7 @@ import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import CardHeading from 'calypso/components/card-heading';
 import FormInputCheckbox from 'calypso/components/forms/form-checkbox';
 import FormFieldset from 'calypso/components/forms/form-fieldset';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { useExperiment } from 'calypso/lib/explat';
 import StepWrapper from 'calypso/signup/step-wrapper';
 import {
@@ -90,6 +91,13 @@ function SurveyForm( props: Props ) {
 	const translate = useTranslate();
 	const isMobileViewport = useMobileBreakpoint();
 	const isDesktopViewport = useDesktopBreakpoint();
+
+	useEffect( () => {
+		if ( isExperimentLoading ) {
+			return;
+		}
+		recordTracksEvent( 'calypso_signup_new_user_survey_view' );
+	}, [ isExperimentLoading ] );
 
 	if ( isExperimentLoading ) {
 		return null;
@@ -313,6 +321,7 @@ export default function NewUserSurvey( props: Props ) {
 
 	const handleSkip = () => {
 		const { stepName, goToNextStep, submitSignupStep } = props;
+		recordTracksEvent( 'calypso_signup_new_user_survey_skip_button_click' );
 		submitSignupStep( { stepName, wasSkipped: true } );
 		goToNextStep();
 	};
