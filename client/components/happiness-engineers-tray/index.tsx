14c9cbd2053661f62bf6480f532a9e3332c8d49c
vykes-mac <knrymcleish@gmail.com>
2024-03-05 15:54:44 -0500

    memoize happiness engineer gravatars to prevent constant rotating when parent component re-renders

--- client/components/happiness-engineers-tray/index.tsx
+++ client/components/happiness-engineers-tray/index.tsx
@@ -1,25 +1,29 @@
 import { Gravatar } from '@automattic/components';
 import { useHappinessEngineersQuery } from '@automattic/data-stores';
 import { shuffle } from 'lodash';
+import { useMemo } from 'react';
 
 import './style.scss';
 
 export const HappinessEngineersTray = ( { count = 5, shuffled = true } ) => {
 	const { data } = useHappinessEngineersQuery();
 
-	return (
-		<div className="happiness-engineers-tray">
-			{ data &&
-				( shuffled ? shuffle( data ) : data )
-					.slice( 0, count )
-					.map( ( user ) => (
-						<Gravatar
-							key={ user.avatar_URL }
-							user={ user }
-							size={ 42 }
-							className="happiness-engineers-tray__gravatar"
-						/>
-					) ) }
-		</div>
-	);
+	const gravatars = useMemo( () => {
+		if ( ! data ) {
+			return null;
+		}
+
+		const filteredHappinessEngineers = ( shuffled ? shuffle( data ) : data ).slice( 0, count );
+
+		return filteredHappinessEngineers.map( ( user ) => (
+			<Gravatar
+				key={ user.avatar_URL }
+				user={ user }
+				size={ 42 }
+				className="happiness-engineers-tray__gravatar"
+			/>
+		) );
+	}, [ data, count, shuffled ] );
+
+	return <div className="happiness-engineers-tray">{ gravatars }</div>;
 };
