551383155dbe55bb724c5b2590d4141a5403aa14
Damien Alleyne <31164683+d-alleyne@users.noreply.github.com>
2024-03-05 10:11:37 -0400

    Implement code review suggestion

--- client/my-sites/theme/theme-features-card.js
+++ client/my-sites/theme/theme-features-card.js
@@ -1,18 +1,17 @@
 import { Card } from '@automattic/components';
-import { useLocalizeUrl } from '@automattic/i18n-utils';
 import { getLocaleSlug, localize } from 'i18n-calypso';
 import { get, isEmpty } from 'lodash';
 import { connect } from 'react-redux';
 import QueryThemeFilters from 'calypso/components/data/query-theme-filters';
 import SectionHeader from 'calypso/components/section-header';
-import { useIsLoggedIn } from 'calypso/lib/request-with-subkey-fallback';
+import { localizeThemesPath } from 'calypso/my-sites/themes/helpers';
+import { useSelector } from 'calypso/state';
+import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 import { isAmbiguousThemeFilterTerm } from 'calypso/state/themes/selectors';
 import { isDelistedTaxonomyTermSlug } from 'calypso/state/themes/utils';
 
 const ThemeFeaturesCard = ( { isWpcomTheme, siteSlug, features, translate, onClick } ) => {
-	const localizeUrl = useLocalizeUrl();
-	const { isLoggedIn } = useIsLoggedIn();
-
+	const isLoggedIn = useSelector( isUserLoggedIn );
 	if ( isEmpty( features ) ) {
 		return null;
 	}
@@ -23,27 +22,22 @@ const ThemeFeaturesCard = ( { isWpcomTheme, siteSlug, features, translate, onCli
 			<SectionHeader label={ translate( 'Features' ) } />
 			<Card>
 				<ul className="theme__sheet-features-list">
-					{ features.map( ( { name, slug, term } ) => (
-						<li
-							key={ 'theme-features-item-' + slug }
-							role="presentation"
-							onClick={ () => onClick?.( slug ) }
-						>
-							{ ! isWpcomTheme ? (
-								<span>{ name }</span>
-							) : (
-								<a
-									href={ localizeUrl(
-										`https://wordpress.com/themes/filter/${ term }/${ siteSlug || '' }`,
-										getLocaleSlug(),
-										isLoggedIn
-									) }
-								>
-									{ name }
-								</a>
-							) }
-						</li>
-					) ) }
+					{ features.map( ( { name, slug, term } ) => {
+						const themePath = localizeThemesPath(
+							`/themes/filter/${ term }/${ siteSlug || '' }`,
+							getLocaleSlug(),
+							! isLoggedIn
+						);
+						return (
+							<li
+								key={ 'theme-features-item-' + slug }
+								role="presentation"
+								onClick={ () => onClick?.( slug ) }
+							>
+								{ ! isWpcomTheme ? <span>{ name }</span> : <a href={ themePath }>{ name }</a> }
+							</li>
+						);
+					} ) }
 				</ul>
 			</Card>
 		</div>
