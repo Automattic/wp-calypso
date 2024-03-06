d276e4d9acfee9d8ccdcf46fa059d61b57df9db8
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2024-03-06 06:34:02 -0300

    GitHub Deployments: Remove create repository functionality

--- client/my-sites/github-deployments/components/repositories/browse-repositories.tsx
+++ client/my-sites/github-deployments/components/repositories/browse-repositories.tsx
@@ -1,10 +1,6 @@
-import page from '@automattic/calypso-router';
-import { Card, Button } from '@automattic/components';
+import { Card } from '@automattic/components';
 import { useI18n } from '@wordpress/react-i18n';
 import { ComponentProps, useState } from 'react';
-import { createRepositoryPage } from 'calypso/my-sites/github-deployments/routes';
-import { useSelector } from 'calypso/state/index';
-import { getSelectedSiteSlug } from 'calypso/state/ui/selectors/index';
 import { GitHubInstallationsDropdown } from '../installations-dropdown';
 import { useLiveInstallations } from '../installations-dropdown/use-live-installations';
 import { GitHubLoadingPlaceholder } from '../loading-placeholder';
@@ -21,8 +17,6 @@ export const GitHubBrowseRepositories = ( {
 	initialInstallationId,
 	onSelectRepository,
 }: GitHubBrowseRepositoriesProps ) => {
-	const siteSlug = useSelector( getSelectedSiteSlug );
-
 	const { __ } = useI18n();
 	const { installation, setInstallation, installations, onNewInstallationRequest } =
 		useLiveInstallations( {
@@ -30,10 +24,6 @@ export const GitHubBrowseRepositories = ( {
 		} );
 
 	const [ query, setQuery ] = useState( '' );
-
-	function handleCreateRepository() {
-		page( createRepositoryPage( siteSlug! ) );
-	}
 
 	function handleQueryChange( query: string ) {
 		setQuery( query );
@@ -71,9 +61,6 @@ export const GitHubBrowseRepositories = ( {
 					onChange={ setInstallation }
 				/>
 				<SearchRepos value={ query } onChange={ handleQueryChange } />
-				<Button onClick={ handleCreateRepository } css={ { height: '100%', marginLeft: 'auto' } }>
-					{ __( 'Create new repository' ) }
-				</Button>
 			</div>
 			{ renderContent() }
 		</div>
