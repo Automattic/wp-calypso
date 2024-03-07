4aa23088ee3747c42b5d8aa418cfab0f146c5c24
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-03-07 18:31:27 +0800

    Add general layout.

--- client/a8c-for-agencies/sections/purchases/payment-methods/payment-method-add/index.tsx
+++ client/a8c-for-agencies/sections/purchases/payment-methods/payment-method-add/index.tsx
@@ -1,3 +1,4 @@
+import { Card } from '@automattic/components';
 import { useTranslate } from 'i18n-calypso';
 import Layout from 'calypso/a8c-for-agencies/components/layout';
 import LayoutBody from 'calypso/a8c-for-agencies/components/layout/body';
@@ -10,8 +11,11 @@ import LayoutStepper from 'calypso/a8c-for-agencies/components/layout/stepper';
 import LayoutTop from 'calypso/a8c-for-agencies/components/layout/top';
 import MobileSidebarNavigation from 'calypso/a8c-for-agencies/components/sidebar/mobile-sidebar-navigation';
 import { A4A_PAYMENT_METHODS_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
+import PaymentMethodStripeInfo from 'calypso/jetpack-cloud/sections/partner-portal/payment-method-stripe-info';
 import { usePaymentMethodStepper } from 'calypso/jetpack-cloud/sections/partner-portal/primary/payment-method-add-v2/hooks/use-payment-method-stepper';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+
+import './style.scss';
 
 type Props = {
 	withAssignLicense?: boolean;
@@ -56,7 +60,15 @@ export default function PaymentMethodAdd( { withAssignLicense }: Props ) {
 				</LayoutHeader>
 			</LayoutTop>
 
-			<LayoutBody>Payment Method Add</LayoutBody>
+			<LayoutBody>
+				<div className="payment-method-add__content">
+					<Card className="payment-method-add__card payment-form">test</Card>
+
+					<Card className="payment-method-add__card aside">
+						<PaymentMethodStripeInfo />
+					</Card>
+				</div>
+			</LayoutBody>
 		</Layout>
 	);
 }
