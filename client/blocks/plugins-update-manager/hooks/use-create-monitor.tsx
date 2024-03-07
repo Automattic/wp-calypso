c385527c03266db7331116e7aa2fc512f06abe86
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2024-03-07 22:48:18 +0100

    Move declarations to hook

--- client/blocks/plugins-update-manager/hooks/use-create-monitor.tsx
+++ client/blocks/plugins-update-manager/hooks/use-create-monitor.tsx
@@ -1,39 +1,21 @@
-import { wpcomRequest } from '@automattic/data-stores/src/wpcom-request-controls';
+import {
+	UpdateMonitorSettings,
+	useCreateMonitorSettingsMutation,
+} from 'calypso/data/plugins/use-monitor-settings-mutation';
 import { useSelector } from 'calypso/state';
 import { JETPACK_MODULE_ACTIVATE_SUCCESS } from 'calypso/state/action-types';
 import { activateModule } from 'calypso/state/jetpack/modules/actions';
 import getSiteUrl from 'calypso/state/selectors/get-site-url';
 import { getSiteId } from 'calypso/state/sites/selectors';
+import { SiteSlug } from 'calypso/types';
 import { CRON_CHECK_INTERVAL } from '../schedule-form.const';
 
-export type UpdateMonitorURLOptions = {
-	status_down_webhook_url: string;
-};
-
-export type UpdateMonitorURL = {
-	monitor_url: string;
-	check_interval: number;
-	options: UpdateMonitorURLOptions;
-};
-
-export type UpdateMonitorSettings = {
-	wp_note_notifications?: boolean;
-	email_notifications?: boolean;
-	sms_notifications?: boolean;
-	jetmon_defer_status_down_minutes?: number;
-	urls?: UpdateMonitorURL[];
-};
-
-export type UpdateMonitorSettingsCreate = {
-	success: boolean;
-	settings: UpdateMonitorSettings;
-};
-
-export function useCreateMonitor( siteSlug: string ) {
+export function useCreateMonitor( siteSlug: SiteSlug ) {
 	const siteId = useSelector( ( state ) => getSiteId( state, siteSlug ) );
 	const siteUrl = useSelector( ( state ) =>
 		getSiteUrl( state, getSiteId( state, siteSlug ) as number )
 	);
+	const { createMonitorSettings } = useCreateMonitorSettingsMutation( siteSlug );
 
 	const createMonitor = () => {
 		activateModule(
@@ -41,26 +23,21 @@ export function useCreateMonitor( siteSlug: string ) {
 			'monitor',
 			true
 		)( ( args: { type: string } ) => {
-			// The home URL needs to be one of the URLs monitored.
-			// Monitoring the wp-cron.php file to ensure that the cron jobs are running.
 			if ( args.type === JETPACK_MODULE_ACTIVATE_SUCCESS ) {
-				wpcomRequest( {
-					path: `/sites/${ siteSlug }/jetpack-monitor-settings`,
-					apiNamespace: 'wpcom/v2',
-					method: 'POST',
-					body: {
-						urls: [
-							{
-								check_interval: CRON_CHECK_INTERVAL,
-								monitor_url: siteUrl,
-							},
-							{
-								check_interval: CRON_CHECK_INTERVAL,
-								monitor_url: siteUrl + '/wp-cron.php',
-							},
-						],
-					},
-				} );
+				createMonitorSettings( {
+					urls: [
+						{
+							// The home URL needs to be one of the URLs monitored.
+							check_interval: CRON_CHECK_INTERVAL,
+							monitor_url: siteUrl,
+						},
+						{
+							// Monitoring the wp-cron.php file to ensure that the cron jobs are running.
+							check_interval: CRON_CHECK_INTERVAL,
+							monitor_url: siteUrl + '/wp-cron.php',
+						},
+					],
+				} as UpdateMonitorSettings );
 			}
 		} );
 	};
