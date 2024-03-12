59dc25954a2cc1545c6bc3724a2ef3075cbfa9eb
gogdzl <diego.rodrigues@automattic.com>
2024-03-12 20:19:37 -0300

    Add Subtitle and Actions to the page

--- client/a8c-for-agencies/sections/sites/sites-dashboard/index.tsx
+++ client/a8c-for-agencies/sections/sites/sites-dashboard/index.tsx
@@ -1,12 +1,16 @@
 import page from '@automattic/calypso-router';
+import { translate } from 'i18n-calypso';
 import React, { useContext, useEffect } from 'react';
 import Layout from 'calypso/a8c-for-agencies/components/layout';
 import LayoutBody from 'calypso/a8c-for-agencies/components/layout/body';
 import LayoutHeader, {
 	LayoutHeaderTitle as Title,
+	LayoutHeaderSubtitle as Subtitle,
+	LayoutHeaderActions as Actions,
 } from 'calypso/a8c-for-agencies/components/layout/header';
 import LayoutTop from 'calypso/a8c-for-agencies/components/layout/top';
 import MobileSidebarNavigation from 'calypso/a8c-for-agencies/components/sidebar/mobile-sidebar-navigation';
+import SiteTopHeaderButtons from 'calypso/jetpack-cloud/sections/agency-dashboard/sites-overview/site-top-header-buttons';
 import { A4A_SITES_DASHBOARD_DEFAULT_CATEGORY } from '../constants';
 import SitesDashboardContext from '../sites-dashboard-context';
 
@@ -35,8 +39,6 @@ export default function SitesDashboard() {
 		setHideListing( false );
 	};
 
-	const currentPage = 1;
-
 	useEffect( () => {
 		// We need a category in the URL if we have a selected site
 		if ( selectedSiteUrl && ! category ) {
@@ -56,12 +58,16 @@ export default function SitesDashboard() {
 		<Layout title="Sites" wide sidebarNavigation={ <MobileSidebarNavigation /> }>
 			<LayoutTop>
 				<LayoutHeader>
-					<Title>Sites</Title>
+					<Title>{ translate( 'Sites' ) }</Title>
+					<Subtitle>{ translate( 'Manage all your Jetpack sites from one location' ) }</Subtitle>
+					<Actions>
+						{ /* TODO: This component is from Jetpack Manage and it was not ported yet, just using it here as a placeholder, it looks broken but it is enough for our purposes at the moment. */ }
+						<SiteTopHeaderButtons />
+					</Actions>
 				</LayoutHeader>
 			</LayoutTop>
 			<LayoutBody>
 				<div style={ { display: 'flex' } }>
-					<div style={ { border: '1px solid black', flex: 1 } }>{ currentPage }</div>
 					<div
 						style={ {
 							border: '1px solid red',
