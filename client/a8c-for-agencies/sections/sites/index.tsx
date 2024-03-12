cd4f15bc839f328caf0cd0137cbf99865b50d7b0
gogdzl <diego.rodrigues@automattic.com>
2024-03-12 19:35:55 -0300

    Remove the favorite route

--- client/a8c-for-agencies/sections/sites/index.tsx
+++ client/a8c-for-agencies/sections/sites/index.tsx
@@ -1,9 +1,8 @@
 import page from '@automattic/calypso-router';
 import { makeLayout, render as clientRender } from 'calypso/controller';
-import { sitesContext, sitesFavoriteContext } from './controller';
+import { sitesContext } from './controller';
 
 export default function () {
-	page( '/sites/favorite', sitesFavoriteContext, makeLayout, clientRender );
 	page( '/sites/:category/:siteUrl/:feature', sitesContext, makeLayout, clientRender );
 	page( '/sites/:category/:siteUrl', sitesContext, makeLayout, clientRender );
 	page( '/sites/:category', sitesContext, makeLayout, clientRender );
