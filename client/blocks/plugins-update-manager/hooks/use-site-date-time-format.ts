cf43e0d07e56487bf020b9d35b0fe87a1cc603ef
Bogdan Nikolic <bogdan.nikolic87@gmail.com>
2024-03-13 18:15:10 +0100

    Extend useSiteDataTimeFormat hook with prepareTimestamp function

--- client/blocks/plugins-update-manager/hooks/use-site-date-time-format.ts
+++ client/blocks/plugins-update-manager/hooks/use-site-date-time-format.ts
@@ -1,9 +1,19 @@
+import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import { phpToMomentDatetimeFormat } from 'calypso/my-sites/site-settings/date-time-format/utils';
 import { useSiteSettings } from './use-site-settings';
 
 export function useSiteDateTimeFormat( siteSlug: string ) {
+	const moment = useLocalizedMoment();
 	const { getSiteSetting } = useSiteSettings( siteSlug );
 	const dateFormat = getSiteSetting( 'date_format' );
 	const timeFormat = getSiteSetting( 'time_format' );
+
+	const prepareTimestamp = ( unixTimestamp: number ) => {
+		const ts = unixTimestamp * 1000;
+		const format = `${ dateFormat } ${ timeFormat }`;
+
+		return phpToMomentDatetimeFormat( moment( ts ), format );
+	};
 
 	const isAmPmPhpTimeFormat = ( formatString: string ) => {
 		// Regular expression to check for AM/PM indicators
@@ -15,6 +25,7 @@ export function useSiteDateTimeFormat( siteSlug: string ) {
 	return {
 		dateFormat,
 		timeFormat,
+		prepareTimestamp,
 		isAmPmPhpTimeFormat,
 	};
 }
