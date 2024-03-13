d17e76ce0f11a9fed2b3ab25826683b3c7624d43
Addison-Stavlo <stavz01@gmail.com>
2024-03-13 12:44:22 -0400

    add componentDidUpdate to panel

--- apps/notifications/src/panel/Notifications.jsx
+++ apps/notifications/src/panel/Notifications.jsx
@@ -91,6 +91,37 @@ export class Notifications extends PureComponent {
 		store.dispatch( { type: 'APP_IS_READY' } );
 	}
 
+	componentDidUpdate( prevProps ) {
+		debug( 'Panel did update', {
+			isShowing: this.props.isShowing,
+			isVisible: this.props.isVisible,
+			wpcom: this.props.wpcom,
+			prevShowing: prevProps.isShowing,
+			prevVis: prevProps.isVisible,
+			prevWPcom: prevProps.wpcom,
+		} );
+
+		if ( prevProps.wpcom !== this.props.wpcom ) {
+			initAPI( this.props.wpcom );
+		}
+
+		if ( ! this.props.isShowing && prevProps.isShowing ) {
+			// unselect the note so keyhandlers don't steal keystrokes
+			store.dispatch( actions.ui.unselectNote() );
+		}
+
+		if ( prevProps.isShowing !== this.props.isShowing ) {
+			store.dispatch( { type: SET_IS_SHOWING, isShowing: this.props.isShowing } );
+		}
+
+		if (
+			prevProps.isShowing !== this.props.isShowing ||
+			prevProps.isVisible !== this.props.isVisible
+		) {
+			client.setVisibility( { isShowing: this.props.isShowing, isVisible: this.props.isVisible } );
+		}
+	}
+
 	// @TODO: Please update https://github.com/Automattic/wp-calypso/issues/58453 if you are refactoring away from UNSAFE_* lifecycle methods!
 	UNSAFE_componentWillReceiveProps( { isShowing, isVisible, wpcom } ) {
 		debug( 'Panel will recieve props', {
