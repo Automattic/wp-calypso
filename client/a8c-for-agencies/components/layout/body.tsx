d801582b895c8b90378c03c2be602f1c0b6aca76
cleacos <javacodia@gmail.com>
2024-03-13 23:46:14 +0000

    Add className support to LayoutBody

--- client/a8c-for-agencies/components/layout/body.tsx
+++ client/a8c-for-agencies/components/layout/body.tsx
@@ -1,12 +1,14 @@
+import classNames from 'classnames';
 import { ReactNode } from 'react';
 
 type Props = {
 	children: ReactNode;
+	className?: string;
 };
 
-export default function LayoutBody( { children }: Props ) {
+export default function LayoutBody( { children, className }: Props ) {
 	return (
-		<div className="a4a-layout__body">
+		<div className={ classNames( 'a4a-layout__body', className ) }>
 			<div className="a4a-layout__body-wrapper">{ children }</div>
 		</div>
 	);
