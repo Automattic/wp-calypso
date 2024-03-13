0ef4108dd96f4494b8f74a67644b1878ee417f6a
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-03-13 15:23:41 +0800

    Move hooks and lib folder outside the Payment method overview folder.

--- client/a8c-for-agencies/sections/purchases/payment-methods/lib/get-stripe-configuration.ts
+++ client/a8c-for-agencies/sections/purchases/payment-methods/lib/get-stripe-configuration.ts
@@ -4,6 +4,7 @@ import type { GetStripeConfigurationArgs } from '@automattic/calypso-stripe';
 export async function getStripeConfiguration(
 	requestArgs: GetStripeConfigurationArgs & { needs_intent?: boolean }
 ) {
+	// FIXME: Update this to the correct endpoint.
 	return await wp.req.get(
 		{
 			apiNamespace: 'wpcom/v2',
