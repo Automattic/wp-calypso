15ed91820977eb741c847d0a142b77a79bd32b05
Tim Broddin <tim@wannabes.be>
2024-03-14 14:29:34 +0100

    Plugin update manager: Persist queries

--- client/data/plugins/use-update-schedules-query.ts
+++ client/data/plugins/use-update-schedules-query.ts
@@ -30,9 +30,6 @@ export const useUpdateScheduleQuery = (
 					...response[ id ],
 				} ) )
 			),
-		meta: {
-			persist: false,
-		},
 		enabled: !! siteSlug && isEligibleForFeature,
 		retry: false,
 		refetchOnWindowFocus: false,
