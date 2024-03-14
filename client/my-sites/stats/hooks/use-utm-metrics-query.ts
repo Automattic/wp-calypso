092e37aefc63739862469594a12f265ddd7633ce
Dognose <dognose24@gmail.com>
2024-03-14 23:21:40 +0800

    Apply date and period to the UTM metrics query

--- client/my-sites/stats/hooks/use-utm-metrics-query.ts
+++ client/my-sites/stats/hooks/use-utm-metrics-query.ts
@@ -4,13 +4,18 @@ import { getSiteSlug } from 'calypso/state/sites/selectors';
 import { requestMetrics } from 'calypso/state/stats/utm-metrics/actions';
 import { getMetrics, isLoading } from 'calypso/state/stats/utm-metrics/selectors';
 
-export default function useUTMMetricsQuery( siteId: number, UTMParam: string, postId?: number ) {
+export default function useUTMMetricsQuery(
+	siteId: number,
+	UTMParam: string,
+	query: object,
+	postId?: number
+) {
 	const dispatch = useDispatch();
 	const siteSlug = useSelector( ( state ) => getSiteSlug( state, siteId ) ) as string;
 
 	useEffect( () => {
-		dispatch( requestMetrics( siteId, UTMParam, postId, siteSlug ) );
-	}, [ dispatch, siteId, UTMParam, postId, siteSlug ] );
+		dispatch( requestMetrics( siteId, UTMParam, query, postId, siteSlug ) );
+	}, [ dispatch, siteId, UTMParam, query, postId, siteSlug ] );
 
 	const isFetching = useSelector( ( state ) => isLoading( state, siteId ) );
 	const metrics = useSelector( ( state ) => getMetrics( state, siteId, postId ) );
