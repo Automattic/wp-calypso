ad7e0f0962b95bd961cd512479542cbd76aa8b24
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-03-14 14:30:35 +0800

    Update marketplace routing to include dedicated pages for Products and Hosting.

--- client/a8c-for-agencies/sections/marketplace/controller.tsx
+++ client/a8c-for-agencies/sections/marketplace/controller.tsx
@@ -1,12 +1,24 @@
 import { type Callback } from '@automattic/calypso-router';
+import page from '@automattic/calypso-router';
 import getSites from 'calypso/state/selectors/get-sites';
-import MainSidebar from '../../components/sidebar-menu/main';
+import MarketplaceSidebar from '../../components/sidebar-menu/marketplace';
 import AssignLicense from './assign-license';
 import IssueLicense from './issue-license';
 
-export const marketplaceContext: Callback = ( context, next ) => {
+export const marketplaceContext: Callback = () => {
+	page.redirect( '/purchases/licenses' );
+};
+
+export const marketplaceProductsContext: Callback = ( context, next ) => {
 	const { site_id, product_slug } = context.query;
-	context.secondary = <MainSidebar path={ context.path } />;
+	context.secondary = <MarketplaceSidebar path={ context.path } />;
+	context.primary = <IssueLicense siteId={ site_id } suggestedProduct={ product_slug } />;
+	next();
+};
+
+export const marketplaceHostingContext: Callback = ( context, next ) => {
+	const { site_id, product_slug } = context.query;
+	context.secondary = <MarketplaceSidebar path={ context.path } />;
 	context.primary = <IssueLicense siteId={ site_id } suggestedProduct={ product_slug } />;
 	next();
 };
@@ -17,7 +29,7 @@ export const assignLicenseContext: Callback = ( context, next ) => {
 	const sites = getSites( state );
 	const currentPage = parseInt( page ) || 1;
 
-	context.secondary = <MainSidebar path={ context.path } />;
+	context.secondary = <MarketplaceSidebar path={ context.path } />;
 	context.primary = (
 		<AssignLicense sites={ sites } currentPage={ currentPage } search={ search || '' } />
 	);
--- client/a8c-for-agencies/sections/marketplace/index.tsx
+++ client/a8c-for-agencies/sections/marketplace/index.tsx
@@ -1,8 +1,15 @@
 import page from '@automattic/calypso-router';
 import { makeLayout, render as clientRender } from 'calypso/controller';
-import { assignLicenseContext, marketplaceContext } from './controller';
+import {
+	assignLicenseContext,
+	marketplaceContext,
+	marketplaceHostingContext,
+	marketplaceProductsContext,
+} from './controller';
 
 export default function () {
 	page( '/marketplace', marketplaceContext, makeLayout, clientRender );
+	page( '/marketplace/products', marketplaceProductsContext, makeLayout, clientRender );
+	page( '/marketplace/hosting', marketplaceHostingContext, makeLayout, clientRender );
 	page( '/marketplace/assign-license', assignLicenseContext, makeLayout, clientRender );
 }
