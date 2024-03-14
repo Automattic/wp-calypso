1f1602e7d288049916aa73283aff5546be915007
cleacos <javacodia@gmail.com>
2024-03-14 08:15:51 +0000

    Restore the previous LayoutBody without classNames, finally, it's not necessary for now.

--- client/a8c-for-agencies/components/layout/body.tsx
+++ client/a8c-for-agencies/components/layout/body.tsx
@@ -1,14 +1,12 @@
-import classNames from 'classnames';
 import { ReactNode } from 'react';
 
 type Props = {
 	children: ReactNode;
-	className?: string;
 };
 
-export default function LayoutBody( { children, className }: Props ) {
+export default function LayoutBody( { children }: Props ) {
 	return (
-		<div className={ classNames( 'a4a-layout__body', className ) }>
+		<div className="a4a-layout__body">
 			<div className="a4a-layout__body-wrapper">{ children }</div>
 		</div>
 	);
