31c77aa5bd50091ed4bf661f7e632fd4628aeb83
Przemek Kuliga <pkuliga@gmail.com>
2024-03-15 13:55:44 +0100

    Settings: Fix the "Invalid site Specified" error (#88557)

--- client/my-sites/site-settings/wrap-settings-form.jsx
+++ client/my-sites/site-settings/wrap-settings-form.jsx
@@ -31,7 +31,7 @@ import {
 	getSiteSettings,
 } from 'calypso/state/site-settings/selectors';
 import { isJetpackSite } from 'calypso/state/sites/selectors';
-import { getSelectedSiteId } from 'calypso/state/ui/selectors';
+import { getSelectedSiteId, getSelectedSiteSlug } from 'calypso/state/ui/selectors';
 
 const debug = debugFactory( 'calypso:site-settings' );
 
@@ -352,6 +352,7 @@ const wrapSettingsForm = ( getFormSettings ) => ( SettingsForm ) => {
 	const connectComponent = connect(
 		( state, { fields } ) => {
 			const siteId = getSelectedSiteId( state );
+			const siteSlug = getSelectedSiteSlug( state );
 			let isSavingSettings = isSavingSiteSettings( state, siteId );
 			const isSaveRequestSuccessful = isSiteSettingsSaveSuccessful( state, siteId );
 			let settings = getSiteSettings( state, siteId );
@@ -407,6 +408,7 @@ const wrapSettingsForm = ( getFormSettings ) => ( SettingsForm ) => {
 				settings,
 				settingsFields,
 				siteId,
+				siteSlug,
 				saveInstantSearchRequest,
 			};
 		},
