4a53952802b3ce2580e2b50382301b6c256329df
Aneesh Devasthale <aneesh.devasthale@automattic.com>
2024-03-15 10:50:27 +0530

    update faq copy on difm landing

--- client/my-sites/marketing/do-it-for-me/difm-landing.tsx
+++ client/my-sites/marketing/do-it-for-me/difm-landing.tsx
@@ -494,7 +494,7 @@ export default function DIFMLanding( {
 						<FoldableFAQ
 							id="faq-1"
 							expanded={ true }
-							question={ translate( 'What is Built By WordPress.com Express, and who is it for?' ) }
+							question={ translate( 'What is Express Website Design Service, and who is it for?' ) }
 						>
 							<p>
 								{ translate(
