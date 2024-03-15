6e5ad4d522f1c936b43e76d5ca5d0792f7fd8a47
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-03-15 18:09:04 +0800

    Add Shopping cart badge counter

--- client/a8c-for-agencies/sections/marketplace/shopping-cart/index.tsx
+++ client/a8c-for-agencies/sections/marketplace/shopping-cart/index.tsx
@@ -1,9 +1,13 @@
+import { Badge } from '@automattic/components';
 import { Button } from '@wordpress/components';
 import { Icon } from '@wordpress/icons';
+import classNames from 'classnames';
 import { useState } from 'react';
 import { SelectedLicenseProp as SelectedItem } from '../issue-license/types';
 import ShoppingCartIcon from './shopping-cart-icon';
 import ShoppingCartMenu from './shopping-cart-menu';
+
+import './style.scss';
 
 type Props = {
 	onCheckout: () => void;
@@ -22,6 +26,13 @@ export default function ShoppingCart( { onCheckout, onRemoveItem, items }: Props
 		<div className="shopping-cart">
 			<Button className="shopping-cart__button" onClick={ toggleShoppingCart }>
 				<Icon className="shopping-cart__button-icon" icon={ <ShoppingCartIcon /> } />
+
+				<Badge
+					className={ classNames( 'shopping-cart__button-badge', { 'is-hidden': ! items.length } ) }
+					type="error"
+				>
+					{ items.length }
+				</Badge>
 			</Button>
 
 			{ showShoppingCart && (
