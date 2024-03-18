3743eef79de3dc56b716974c7f20ce6268202e26
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-03-18 20:45:34 +0800

    Fix mobile view.

--- client/a8c-for-agencies/sections/marketplace/checkout/index.tsx
+++ client/a8c-for-agencies/sections/marketplace/checkout/index.tsx
@@ -1,5 +1,7 @@
+import page from '@automattic/calypso-router';
 import { Button } from '@wordpress/components';
 import { useTranslate } from 'i18n-calypso';
+import { useCallback } from 'react';
 import Layout from 'calypso/a8c-for-agencies/components/layout';
 import LayoutBody from 'calypso/a8c-for-agencies/components/layout/body';
 import LayoutHeader, {
@@ -8,6 +10,8 @@ import LayoutHeader, {
 import LayoutTop from 'calypso/a8c-for-agencies/components/layout/top';
 import MobileSidebarNavigation from 'calypso/a8c-for-agencies/components/sidebar/mobile-sidebar-navigation';
 import { A4A_MARKETPLACE_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
+import { useDispatch } from 'calypso/state';
+import { recordTracksEvent } from 'calypso/state/analytics/actions';
 import useShoppingCart from '../hooks/use-shopping-cart';
 import { ShoppingCartItem } from '../types';
 import PricingSummary from './pricing-summary';
@@ -17,7 +21,26 @@ import './style.scss';
 
 export default function Checkout() {
 	const translate = useTranslate();
+	const dispatch = useDispatch();
+
 	const { selectedItems, setSelectedItems } = useShoppingCart();
+
+	const onCheckout = useCallback( () => {
+		dispatch( recordTracksEvent( 'calypso_a4a_marketplace_checkout_checkout_click' ) );
+
+		// FIXME: Issue license
+	}, [ dispatch ] );
+
+	const onEmptyCart = useCallback( () => {
+		dispatch( recordTracksEvent( 'calypso_a4a_marketplace_checkout_empty_cart_click' ) );
+		setSelectedItems( [] );
+		page( A4A_MARKETPLACE_LINK );
+	}, [ dispatch, setSelectedItems ] );
+
+	const onContinueShopping = useCallback( () => {
+		dispatch( recordTracksEvent( 'calypso_a4a_marketplace_checkout_continue_shopping_click' ) );
+		page( A4A_MARKETPLACE_LINK );
+	}, [ dispatch ] );
 
 	return (
 		<Layout
@@ -69,7 +92,7 @@ export default function Checkout() {
 						/>
 
 						<div className="checkout__aside-actions">
-							<Button variant="primary">
+							<Button variant="primary" onClick={ onCheckout } disabled={ ! selectedItems.length }>
 								{ translate( 'Purchase %(count)d plan', 'Purchase %(count)d plans', {
 									context: 'button label',
 									count: selectedItems.length,
@@ -79,9 +102,13 @@ export default function Checkout() {
 								} ) }
 							</Button>
 
-							<Button variant="secondary">{ translate( 'Continue shopping' ) }</Button>
+							<Button variant="secondary" onClick={ onContinueShopping }>
+								{ translate( 'Continue shopping' ) }
+							</Button>
 
-							<Button variant="link">{ translate( 'Empty cart' ) }</Button>
+							<Button variant="link" onClick={ onEmptyCart }>
+								{ translate( 'Empty cart' ) }
+							</Button>
 						</div>
 					</div>
 				</div>
