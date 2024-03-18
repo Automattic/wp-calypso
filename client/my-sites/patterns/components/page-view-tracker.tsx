f756d5d511e496825a9e2ed0f035f94815ed0534
Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
2024-03-18 11:54:16 +0100

    Track calypso_pattern_library_search

--- client/my-sites/patterns/components/page-view-tracker.tsx
+++ client/my-sites/patterns/components/page-view-tracker.tsx
@@ -1,6 +1,9 @@
+import { useEffect } from 'react';
 import PageViewTracker from 'calypso/lib/analytics/page-view-tracker';
+import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { useSelector } from 'calypso/state';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
+import getUserSetting from 'calypso/state/selectors/get-user-setting';
 
 type PatternsPageViewTrackerProps = {
 	category: string;
@@ -9,6 +12,19 @@ type PatternsPageViewTrackerProps = {
 
 export function PatternsPageViewTracker( { category, searchTerm }: PatternsPageViewTrackerProps ) {
 	const isLoggedIn = useSelector( isUserLoggedIn );
+	const isDevAccount = useSelector( ( state ) => getUserSetting( state, 'is_dev_account' ) );
+
+	// `PageViewTracker` pushes a new event only when `path` changes, so we track
+	// `calypso_pattern_library_search` to have accurate data on which search terms are used
+	useEffect( () => {
+		if ( searchTerm ) {
+			recordTracksEvent( 'calypso_pattern_library_search', {
+				is_logged_in: isLoggedIn ? '1' : '0',
+				search_term: searchTerm,
+				user_is_dev_account: isDevAccount ? '1' : '0',
+			} );
+		}
+	}, [ isDevAccount, isLoggedIn, searchTerm ] );
 
 	let path: string = '';
 	const properties: Record< string, string > = {
@@ -26,9 +42,5 @@ export function PatternsPageViewTracker( { category, searchTerm }: PatternsPageV
 		properties.search_term = searchTerm;
 	}
 
-	const key = path + category + searchTerm;
-
-	return (
-		<PageViewTracker key={ key } path={ path } properties={ properties } title="Pattern Library" />
-	);
+	return <PageViewTracker path={ path } properties={ properties } title="Pattern Library" />;
 }
