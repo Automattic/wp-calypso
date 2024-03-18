6e5fad6cf678a96769c5a23a683d8f266aba7e78
Robert Sreberski <robertsreberski@gmail.com>
2024-03-18 17:34:48 +0100

    Add page view event, and exclude Akismet and Jetpack happening at the same time

--- client/lib/analytics/ad-tracking/load-tracking-scripts.js
+++ client/lib/analytics/ad-tracking/load-tracking-scripts.js
@@ -234,8 +234,14 @@ function initFacebook() {
 	// Also initialize the FB pixel for Jetpack & Akismet.
 	// However, disable auto-config for this secondary pixel ID.
 	// See: <https://developers.facebook.com/docs/facebook-pixel/api-reference#automatic-configuration>
-	window.fbq( 'set', 'autoConfig', false, TRACKING_IDS.facebookJetpackInit );
-	window.fbq( 'set', 'autoConfig', false, TRACKING_IDS.facebookAkismetInit );
-	window.fbq( 'init', TRACKING_IDS.facebookJetpackInit, advancedMatching );
-	window.fbq( 'init', TRACKING_IDS.facebookAkismetInit, advancedMatching );
+	if ( isJetpackCheckout() ) {
+		window.fbq( 'set', 'autoConfig', false, TRACKING_IDS.facebookJetpackInit );
+		window.fbq( 'init', TRACKING_IDS.facebookJetpackInit, advancedMatching );
+		window.fbq( 'track', 'PageView' ); // When autoConfig=false, page view tracking must be manually triggered
+	}
+	if ( isAkismetCheckout() ) {
+		window.fbq( 'set', 'autoConfig', false, TRACKING_IDS.facebookAkismetInit );
+		window.fbq( 'init', TRACKING_IDS.facebookAkismetInit, advancedMatching );
+		window.fbq( 'track', 'PageView' ); // When autoConfig=false, page view tracking must be manually triggered
+	}
 }
