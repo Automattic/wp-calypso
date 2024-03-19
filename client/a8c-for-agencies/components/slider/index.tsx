6a0cecfdcf428b0ea2236e9068bae54450187a3c
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-03-19 19:12:41 +0800

    Add discount information.

--- client/a8c-for-agencies/components/slider/index.tsx
+++ client/a8c-for-agencies/components/slider/index.tsx
@@ -5,6 +5,7 @@ import './style.scss';
 export type Option = {
 	label: string;
 	value: number;
+	sub?: string;
 };
 
 type Props = {
@@ -13,9 +14,10 @@ type Props = {
 	onChange?: ( value: Option ) => void;
 	value: number;
 	label?: string;
+	sub?: string;
 };
 
-export default function A4ASlider( { className, options, onChange, value, label }: Props ) {
+export default function A4ASlider( { className, options, onChange, value, label, sub }: Props ) {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const onSliderChange = ( event: any ) => {
 		onChange?.( options[ event.target.value ] );
@@ -23,7 +25,12 @@ export default function A4ASlider( { className, options, onChange, value, label 
 
 	return (
 		<div className={ classNames( 'a4a-slider', className ) }>
-			{ label && <div className="a4a-slider__label">{ label }</div> }
+			{ label && (
+				<div className="a4a-slider__label-container">
+					<div className="a4a-slider__label">{ label }</div>
+					<div className="a4a-slider__sub">{ sub }</div>
+				</div>
+			) }
 
 			<div className="a4a-slider__input">
 				<input
@@ -40,6 +47,7 @@ export default function A4ASlider( { className, options, onChange, value, label 
 							<div className="a4a-slider__marker" key={ `slider-option-${ index }` }>
 								<div className="a4a-slider__marker-line"></div>
 								<div className="a4a-slider__marker-label">{ option.label }</div>
+								{ option.sub && <div className="a4a-slider__marker-sub">{ option.sub }</div> }
 							</div>
 						);
 					} ) }
--- client/a8c-for-agencies/sections/marketplace/products-overview/product-listing/volume-price-selector.tsx
+++ client/a8c-for-agencies/sections/marketplace/products-overview/product-listing/volume-price-selector.tsx
@@ -7,6 +7,18 @@ type Props = {
 	selectedBundleSize: number;
 };
 
+const getDiscountPercentage = ( bundleSize: number ) => {
+	// FIXME: Need to calculate based on average discount per bundle size.
+	return {
+		1: '',
+		5: '8%',
+		10: '20%',
+		20: '40%',
+		50: '70%',
+		100: '80%',
+	}[ bundleSize ];
+};
+
 export default function VolumePriceSelector( {
 	availableBundleSizes,
 	onBundleSizeChange,
@@ -17,6 +29,7 @@ export default function VolumePriceSelector( {
 	const options = availableBundleSizes.map( ( size ) => {
 		return {
 			label: `${ size }`,
+			sub: getDiscountPercentage( size ),
 			value: size,
 		};
 	} );
@@ -28,6 +41,7 @@ export default function VolumePriceSelector( {
 	return (
 		<A4ASlider
 			label={ translate( 'Volume pricing' ) }
+			sub={ translate( 'Discount' ) }
 			className="product-listing__volume-price-selector"
 			value={ options.findIndex( ( { value } ) => selectedBundleSize === value ) }
 			options={ options }
