cf1511dbe37b2882a3a79e19a97c0e551cb0143e
Andrii <lysenkoa.work@gmail.com>
2024-03-19 07:19:01 -0700

    update content-sidebar with suggestions

--- client/a8c-for-agencies/components/content-sidebar/index.tsx
+++ client/a8c-for-agencies/components/content-sidebar/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Main from 'calypso/components/main';
-import SidebarNavigation from 'calypso/jetpack-cloud/sections/partner-portal/sidebar-navigation';
 
 import './style.scss';
 
@@ -12,11 +10,10 @@ type Props = {
 const ContentSidebar = ( { mainContent, rightSidebar }: Props ) => {
 	return (
 		<>
-			<SidebarNavigation />
-			<Main wideLayout className="content-sidebar">
+			<div className="content-sidebar">
 				<div className="content-sidebar__main-content">{ mainContent }</div>
 				<aside className="content-sidebar__right-sidebar">{ rightSidebar }</aside>
-			</Main>
+			</div>
 		</>
 	);
 };
