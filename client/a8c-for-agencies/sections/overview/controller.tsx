58f7b796230dd6d43ebcc2e7d2f2b4d28cb1e772
Nate Weller <nate.weller@automattic.com>
2024-03-19 11:06:11 -0600

    A4A: Update the overview header title to "Agency HQ Overview" (#88639)
    * Update the overview header title to 'Agency HQ Overview'
    
    * Add overview component and use translate function for header title
    
    * Reuse 'Agency HQ Overview' string for Layout title prop

--- client/a8c-for-agencies/sections/overview/controller.tsx
+++ client/a8c-for-agencies/sections/overview/controller.tsx
@@ -1,29 +1,10 @@
 import { type Callback } from '@automattic/calypso-router';
-import Layout from 'calypso/a8c-for-agencies/components/layout';
-import LayoutBody from 'calypso/a8c-for-agencies/components/layout/body';
-import LayoutHeader, {
-	LayoutHeaderSubtitle as Subtitle,
-	LayoutHeaderTitle as Title,
-} from 'calypso/a8c-for-agencies/components/layout/header';
-import LayoutTop from 'calypso/a8c-for-agencies/components/layout/top';
-import MobileSidebarNavigation from 'calypso/a8c-for-agencies/components/sidebar/mobile-sidebar-navigation';
 import MainSidebar from '../../components/sidebar-menu/main';
+import Overview from './overview';
 
 export const overviewContext: Callback = ( context, next ) => {
 	context.secondary = <MainSidebar path={ context.path } />;
-	context.primary = (
-		<Layout title="Overview" wide sidebarNavigation={ <MobileSidebarNavigation /> }>
-			<LayoutTop>
-				<LayoutHeader>
-					<Title>Overview</Title>
-					<Subtitle>Overview of your agency</Subtitle>
-				</LayoutHeader>
-			</LayoutTop>
-			<LayoutBody>
-				<div>test</div>
-			</LayoutBody>
-		</Layout>
-	);
+	context.primary = <Overview />;
 
 	next();
 };
