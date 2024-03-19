9e1da5d55f02f29eafa83757a9edca668deb1e9a
Kristian Vitozev <kristian.vitozev@automattic.com>
2024-03-19 13:43:57 +0200

    Add missing files

---
+++ client/a8c-for-agencies/sections/signup/primary/agency-signup/index.tsx
@@ -0,0 +1,11 @@
+import DocumentHead from 'calypso/components/data/document-head';
+import Main from 'calypso/components/main';
+
+export default function AgencySignup() {
+	return (
+		<Main className="agency-signup">
+			<DocumentHead title="Sign up for Automattic for Agencies" />
+			<h2>Sign up for Automattic for Agencies</h2>
+		</Main>
+	);
+}
