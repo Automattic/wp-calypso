b62e7463e6f73679187ba01fd15d0272b47a731c
Kristian Vitozev <kristian.vitozev@automattic.com>
2024-03-19 14:36:34 +0200

    Update layout

--- client/a8c-for-agencies/sections/signup/primary/agency-signup/index.tsx
+++ client/a8c-for-agencies/sections/signup/primary/agency-signup/index.tsx
@@ -1,11 +1,21 @@
-import DocumentHead from 'calypso/components/data/document-head';
-import Main from 'calypso/components/main';
+import Layout from 'calypso/a8c-for-agencies/components/layout';
+import LayoutBody from 'calypso/a8c-for-agencies/components/layout/body';
+import LayoutHeader, {
+	LayoutHeaderTitle as Title,
+} from 'calypso/a8c-for-agencies/components/layout/header';
+import LayoutTop from 'calypso/a8c-for-agencies/components/layout/top';
 
 export default function AgencySignup() {
 	return (
-		<Main className="agency-signup">
-			<DocumentHead title="Sign up for Automattic for Agencies" />
-			<h2>Sign up for Automattic for Agencies</h2>
-		</Main>
+		<Layout title="Sign Up" wide>
+			<LayoutTop>
+				<LayoutHeader>
+					<Title>Sign up for Automattic for Agencies</Title>
+				</LayoutHeader>
+			</LayoutTop>
+			<LayoutBody>
+				<div>Form</div>
+			</LayoutBody>
+		</Layout>
 	);
 }
