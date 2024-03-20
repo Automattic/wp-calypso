ca4dffa549759caccd25594c98f0d783d576605a
Tim Broddin <tim@wannabes.be>
2024-03-20 11:26:53 +0100

    oops

--- client/state/query-client.ts
+++ client/state/query-client.ts
@@ -57,7 +57,7 @@ export async function hydrateBrowserState(
 
 					return storePersistedStateItem( storeKey, state );
 				},
-				SERIALIZE_THROTTLE * 20,
+				SERIALIZE_THROTTLE,
 				{ leading: false, trailing: true }
 			),
 			restoreClient: () => getPersistedStateItem( storeKey ),
