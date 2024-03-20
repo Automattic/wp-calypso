d7cffdc0cd6abb7e56acc6feda062ade5a532f8d
Fredrik Rombach Ekelund <fredrik@fredrik.computer>
2024-03-20 11:34:27 +0100

    Fix `LocalizedLink` SSR bug (#88705)

--- client/my-sites/patterns/components/localized-link.tsx
+++ client/my-sites/patterns/components/localized-link.tsx
@@ -1,10 +1,13 @@
-import { addLocaleToPathLocaleInFront } from '@automattic/i18n-utils';
+import { addLocaleToPathLocaleInFront, useLocale } from '@automattic/i18n-utils';
 import { useSelector } from 'calypso/state';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 
 export function LocalizedLink( { children, href = '', ...props }: JSX.IntrinsicElements[ 'a' ] ) {
 	const isLoggedIn = useSelector( isUserLoggedIn );
-	const localizedHref = ! isLoggedIn ? addLocaleToPathLocaleInFront( href ) : href;
+	// `addLocaleToPathLocaleInFront` retrieves the active locale differently from `useLocale`.
+	// Crucially, it doesn't work with SSR unless we pass the `useLocale` value explicitly
+	const locale = useLocale();
+	const localizedHref = ! isLoggedIn ? addLocaleToPathLocaleInFront( href, locale ) : href;
 
 	return (
 		<a { ...props } href={ localizedHref }>
