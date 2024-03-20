3e97a3bb6c42ffd0095bd57dc1ef5672d88d72b1
Marin Atanasov <tyxla@abv.bg>
2024-03-20 19:21:51 +0200

    Login: Filter redirect URLs

--- client/login/redirect-logged-in/index.web.js
+++ client/login/redirect-logged-in/index.web.js
@@ -1,6 +1,30 @@
 import safeProtocolUrl from 'calypso/lib/safe-protocol-url';
 import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 
+/**
+ * For this context, we consider external URLs that are NOT:
+ * - Relative paths (`/test`)
+ * - Absolute URLs on https://wordpress.com/*
+ * @param {string} url URL to check
+ * @returns {boolean}
+ */
+function isExternalUrl( url ) {
+	if ( url.startsWith( '/' ) ) {
+		return false;
+	}
+
+	try {
+		const urlObject = new URL( url );
+		if ( urlObject.hostname === 'wordpress.com' && urlObject.protocol === 'https:' ) {
+			return false;
+		}
+	} catch {
+		return true;
+	}
+
+	return true;
+}
+
 export default function redirectLoggedIn( context, next ) {
 	const userLoggedIn = isUserLoggedIn( context.store.getState() );
 
@@ -8,7 +32,7 @@ export default function redirectLoggedIn( context, next ) {
 		// force full page reload to avoid SSR hydration issues.
 		// Redirect parameters should have higher priority.
 		let url = safeProtocolUrl( context?.query?.redirect_to );
-		if ( ! url || url === 'http:' ) {
+		if ( ! url || url === 'http:' || isExternalUrl( url ) ) {
 			url = '/';
 		}
 		window.location = url;
