8dfc2a31e81cc2c49c0c0fa9933aba958c38f7e5
Yashwin <yashwinpoojary@gmail.com>
2024-03-20 16:56:02 +0530

    Update the A4A Sign up flow

--- client/a8c-for-agencies/sections/signup/controller.tsx
+++ client/a8c-for-agencies/sections/signup/controller.tsx
@@ -1,21 +1,5 @@
-import { Context, type Callback } from '@automattic/calypso-router';
-import page from '@automattic/calypso-router';
-import { A4A_SITES_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
-import { getActiveAgency } from 'calypso/state/a8c-for-agencies/agency/selectors';
+import { type Callback } from '@automattic/calypso-router';
 import AgencySignUp from './primary/agency-signup';
-
-export function requireNoPartnerRecordContext( context: Context, next: () => void ): void {
-	const state = context.store.getState();
-	const partner = getActiveAgency( state );
-
-	// Users who already have a partner record should be redirected away from the signup form.
-	if ( partner ) {
-		page.redirect( A4A_SITES_LINK );
-		return;
-	}
-
-	next();
-}
 
 export const signUpContext: Callback = ( context, next ) => {
 	context.primary = <AgencySignUp />;
--- client/a8c-for-agencies/sections/signup/index.ts
+++ client/a8c-for-agencies/sections/signup/index.ts
@@ -3,11 +3,5 @@ import { makeLayout, render as clientRender } from 'calypso/controller';
 import * as controller from './controller';
 
 export default function () {
-	page(
-		'/signup',
-		controller.requireNoPartnerRecordContext,
-		controller.signUpContext,
-		makeLayout,
-		clientRender
-	);
+	page( '/signup', controller.signUpContext, makeLayout, clientRender );
 }
