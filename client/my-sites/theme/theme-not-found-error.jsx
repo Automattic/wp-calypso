fc1486cf244be8672ca8295833e67d8e987d813f
Stéphane Thomas <email@stephanethomas.com>
2024-03-21 18:00:38 +0100

    Fix locale on 404 page for Themes

--- client/my-sites/theme/theme-not-found-error.jsx
+++ client/my-sites/theme/theme-not-found-error.jsx
@@ -1,8 +1,12 @@
+import { addLocaleToPathLocaleInFront } from '@automattic/i18n-utils';
 import { useTranslate } from 'i18n-calypso';
+import { useSelector } from 'react-redux';
 import EmptyContent from 'calypso/components/empty-content';
 import Main from 'calypso/components/main';
+import { isUserLoggedIn } from 'calypso/state/current-user/selectors';
 
 export default function ThemeNotFoundError() {
+	const isLoggedIn = useSelector( isUserLoggedIn );
 	const translate = useTranslate();
 
 	const title = translate( 'Looking for great WordPress designs?', {
@@ -19,7 +23,7 @@ export default function ThemeNotFoundError() {
 				title={ title }
 				line={ line }
 				action={ translate( 'View the showcase' ) }
-				actionURL="/themes"
+				actionURL={ isLoggedIn ? '/themes' : addLocaleToPathLocaleInFront( '/themes' ) }
 			/>
 		</Main>
 	);
