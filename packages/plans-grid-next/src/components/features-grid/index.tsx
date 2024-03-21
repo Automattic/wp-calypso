2bb08ae2e3277ebdf12a9a02f5e128573bda693e
Christos <chriskmnds@gmail.com>
2024-03-21 12:50:53 +0100

    Plans Next: Fix content overflow in comparison grid

--- packages/plans-grid-next/src/components/features-grid/index.tsx
+++ packages/plans-grid-next/src/components/features-grid/index.tsx
@@ -275,7 +275,7 @@ const FeaturesGrid = ( {
 	};
 
 	return (
-		<>
+		<div className="plans-grid-next-features-grid">
 			{ 'small' !== gridSize && <SpotlightPlan { ...spotlightPlanProps } /> }
 			<div className="plan-features">
 				<div className="plan-features-2023-grid__content">
@@ -298,7 +298,7 @@ const FeaturesGrid = ( {
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
