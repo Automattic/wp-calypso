2fd2ae69dfd9e59c10aced9e50de89e17c82e937
Gabriel Caires <gabriel.caires@automattic.com>
2024-03-22 19:08:02 +0000

    Update layout

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-instructions/index.tsx
@@ -1,184 +1,128 @@
-import { Button } from '@automattic/components';
 import { StepContainer } from '@automattic/onboarding';
-import { ClipboardButton } from '@wordpress/components';
-import { addQueryArgs } from '@wordpress/url';
-import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
-import { useState } from 'react';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
+import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
 import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
 import { useSite } from 'calypso/landing/stepper/hooks/use-site';
+import { useSiteMigrationKey } from 'calypso/landing/stepper/hooks/use-site-migraiton-key';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
-import wpcom from 'calypso/lib/wp';
+import { ShowHideInput } from './show-hide-input';
 import type { Step } from '../../types';
 import './style.scss';
+
+const removeDuplicatedSlashes = ( url: string ) => url.replace( /(https?:\/\/)|(\/)+/g, '$1$2' );
+
+const getInstallationURL = ( fromUrl: string ) => {
+	return removeDuplicatedSlashes(
+		`${ fromUrl }/wp-admin/plugin-install.php?s=%2522migrate%2520guru%2522&tab=search&type=term`
+	);
+};
+
+const getMigrateGuruPageURL = ( siteURL: string ) =>
+	removeDuplicatedSlashes( `${ siteURL }/wp-admin/admin.php?page=migrateguru` );
+
+const Loading = () => {
+	return (
+		<div className="loading">
+			<div className="loading__content">
+				<LoadingEllipsis />
+			</div>
+		</div>
+	);
+};
 
 const SiteMigrationInstructions: Step = function () {
 	const translate = useTranslate();
 	const site = useSite();
 	const siteId = site?.ID;
 	const fromUrl = useQuery().get( 'from' ) || '';
-	const sourceSiteUrl = fromUrl
-		? addQueryArgs( fromUrl + '/wp-admin/admin.php', { page: 'migrateguru' } )
-		: '';
-	const destSiteUrl = fromUrl
-		? addQueryArgs( site?.URL + '/wp-admin/admin.php', { page: 'migrateguru' } )
-		: '';
-	const [ buttonTextCopy, setButtonTextCopy ] = useState( false );
-	const onCopy = () => {
-		recordTracksEvent( 'calypso_migration_instructions_key_copy' );
-		setButtonTextCopy( true );
-		setTimeout( () => {
-			setButtonTextCopy( false );
-		}, 2000 );
-	};
-	const [ siteMigrationKey, setSiteMigrationKey ] = useState< string | null >( null );
-	const [ siteMigrationKeyError, setSiteMigrationKeyError ] = useState< string | null >( null );
-	const [ hideSiteMigrationKey, setHideSiteMigrationKey ] = useState( true );
 
-	const getMigrationKey = async () => {
-		try {
-			const response = await wpcom.req.get(
-				`/sites/${ siteId }/atomic-migration-status/migrate-guru-key?http_envelope=1`,
-				{
-					apiNamespace: 'wpcom/v2',
-				}
-			);
-
-			setSiteMigrationKey( response?.migration_key );
-			recordTracksEvent( 'calypso_migration_instructions_key_retrieved' );
-		} catch ( error ) {
-			setSiteMigrationKeyError( error as string );
-			recordTracksEvent( 'calypso_migration_instructions_key_error' );
-		}
-	};
+	const {
+		data: { migrationKey } = {},
+		isSuccess,
+		isError,
+		isFetching,
+		isFetched,
+	} = useSiteMigrationKey( siteId );
 
 	const stepContent = (
-		<div>
+		<div className="site-migration-instructions__content">
 			<ol className="site-migration-instructions__list">
 				<li>
-					{ translate( 'Install the {{a}}Migrate Guru plugin{{/a}} on your existing site.', {
-						components: {
-							a: (
-								<a
-									href="https://wordpress.org/plugins/migrate-guru/"
-									target="_blank"
-									rel="noreferrer"
-								/>
-							),
-						},
-					} ) }
-				</li>
-				<li>
-					<div
-						className={ classNames( 'site-migration-instructions__list-migration-key-item', {
-							expanded: siteMigrationKey,
-							error: siteMigrationKeyError,
-						} ) }
-					>
-						{ ! siteMigrationKey && (
-							<>
-								{ ! siteMigrationKeyError && (
-									<Button
-										primary
-										compact
-										className="site-migration-instructions__get-key-button component-button"
-										onClick={ getMigrationKey }
-									>
-										{ translate( 'Get migration key', {
-											components: {
-												strong: <strong />,
-											},
-										} ) }
-									</Button>
-								) }
-								{ siteMigrationKeyError && (
-									<>
-										{ translate(
-											'We were unable to retrieve your migration key. To get the key manually, go to the {{a}}Migrate Guru page on the new WordPress.com site{{/a}}.',
-											{
-												components: {
-													a: <a href={ destSiteUrl } target="_blank" rel="noreferrer" />,
-												},
-											}
-										) }
-									</>
-								) }
-							</>
-						) }
-						{ siteMigrationKey && ! siteMigrationKeyError && (
-							<>
-								{ translate(
-									'Click {{strong}}Copy key{{/strong}} below to copy your migration key - you will need that in a few minutes to start the migration. This key is unique to your site and will only be available once.',
-									{
-										components: {
-											strong: <strong />,
-										},
-									}
-								) }
-								<div className="site-migration-instructions__migration-key">
-									<code className="site-migration-instructions__key">
-										{ hideSiteMigrationKey
-											? '*'.repeat( siteMigrationKey.length - 5 )
-											: siteMigrationKey }
-									</code>
-									<Button
-										onClick={ () => {
-											setHideSiteMigrationKey( ! hideSiteMigrationKey );
-										} }
-										className="site-migration-instructions__show-key-button components-button"
-									>
-										{ hideSiteMigrationKey ? translate( 'Show key' ) : translate( 'Hide key' ) }
-									</Button>
-									<ClipboardButton
-										text={ siteMigrationKey }
-										className="site-migration-instructions__copy-key-button is-secondary"
-										onCopy={ onCopy }
-									>
-										{ buttonTextCopy ? translate( 'Copied!' ) : translate( 'Copy key' ) }
-									</ClipboardButton>
-								</div>
-							</>
-						) }
-					</div>
+					{ translate(
+						'Install and active the {{a}}Migrate Guru plugin{{/a}} on your existing site.',
+						{
+							components: {
+								a: <a href={ getInstallationURL( fromUrl ) } target="_blank" rel="noreferrer" />,
+							},
+						}
+					) }
 				</li>
 				<li>
 					{ translate(
 						'Go to the {{a}}Migrate Guru page on the source site{{/a}}, enter your email address, and click {{strong}}Migrate{{/strong}}.',
 						{
 							components: {
-								a: <a href={ sourceSiteUrl } target="_blank" rel="noreferrer" />,
+								a: <a href={ getMigrateGuruPageURL( fromUrl ) } target="_blank" rel="noreferrer" />,
 								strong: <strong />,
 							},
 						}
 					) }
 				</li>
 				<li>
-					{ translate(
-						'You will see a screen showing multiple hosting providers. Select {{em}}Automattic{{/em}} as the destination host.',
-						{
-							components: {
-								em: <em />,
-							},
-						}
-					) }
+					{ translate( 'When asked to select a destination host, pick {{em}}“Automattic”{{/em}}.', {
+						components: {
+							em: <em />,
+						},
+					} ) }
 				</li>
-				<li>
-					{ translate(
-						'Find the {{em}}Migrate Guru Migration Key{{/em}} field, and paste the key you copied in step 2. Then click {{strong}}Migrate{{/strong}} to start your migration.',
-						{
-							components: {
-								em: <em />,
-								strong: <strong />,
-							},
-						}
-					) }
-				</li>
-				<li>{ translate( 'Migrate Guru will send you an email when the migration finishes.' ) }</li>
+				{ isSuccess && migrationKey && (
+					<li>
+						{ translate(
+							'Copy and paste the migration key below in the {{em}}“Migrate Guru Migration Key”{{/em}} field and click {{strong}}Migrate{{/strong}}.',
+							{
+								components: {
+									em: <em />,
+									strong: <strong />,
+								},
+							}
+						) }
+						<ShowHideInput value={ migrationKey } />
+					</li>
+				) }
+				{ isError && (
+					<li>
+						{ translate(
+							'Go to the {{a}}Migrate Guru page on the new WordPress.com site{{/a}} and copy the migration key. Then paste it on the {{em}}"Migrate Guru Migration Key"{{/em}} field of your existing site and click {{strong}}"Migrate"{{/strong}}.',
+							{
+								components: {
+									em: <em />,
+									a: (
+										<a
+											href={ getMigrateGuruPageURL( site!.URL ) }
+											target="_blank"
+											rel="noreferrer"
+										/>
+									),
+									strong: <strong />,
+								},
+							}
+						) }
+					</li>
+				) }
 			</ol>
+			<p>
+				{ translate(
+					'And you are done! When the migration finishes, Migrate Guru will send you an email.'
+				) }
+			</p>
 		</div>
 	);
+
+	if ( isFetching || ! isFetched ) {
+		return <Loading />;
+	}
 
 	return (
 		<>
@@ -190,14 +134,13 @@ const SiteMigrationInstructions: Step = function () {
 				hideSkip={ true }
 				hideBack={ true }
 				formattedHeader={
-					<>
-						<FormattedHeader
-							id="site-migration-instructions-header"
-							headerText={ translate( 'Ready to migrate your site?' ) }
-							align="center"
-						/>
-						<p>{ translate( 'Follow these steps to get started.' ) }</p>
-					</>
+					<FormattedHeader
+						id="site-migration-instructions-header"
+						headerText={ translate( 'Ready to migrate your site?' ) }
+						align="center"
+						subHeaderText={ translate( 'Follow these steps to get started.' ) }
+						subHeaderAlign="center"
+					/>
 				}
 				stepContent={ stepContent }
 				recordTracksEvent={ recordTracksEvent }
