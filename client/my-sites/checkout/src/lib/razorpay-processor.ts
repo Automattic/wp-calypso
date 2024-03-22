f9ad4cc3030bba449c5415be92f4b4b8a0cce7f8
nbloomf <nathan.bloomfield@automattic.com>
2024-03-22 15:26:01 -0500

    Add ondismiss event and flatten event payloads

--- client/my-sites/checkout/src/lib/razorpay-processor.ts
+++ client/my-sites/checkout/src/lib/razorpay-processor.ts
@@ -13,6 +13,7 @@ import {
 } from '@automattic/composite-checkout';
 import {
 	ManagedContactDetails,
+	WPCOMTransactionEndpointRequestPayload,
 	WPCOMTransactionEndpointResponseRedirect,
 } from '@automattic/wpcom-checkout';
 import debugFactory from 'debug';
@@ -97,18 +98,29 @@ export default async function razorpayProcessor(
 					response as WPCOMTransactionEndpointResponseRedirect,
 					( result ) => {
 						transactionOptions.reduxDispatch(
-							recordTracksEvent( 'calypso_checkout_razorpay_modal_complete', {
-								transactionData: formattedTransactionData,
-							} )
+							recordTracksEvent(
+								'calypso_checkout_razorpay_modal_complete',
+								constructTracksProps( formattedTransactionData, {} )
+							)
 						);
 						return resolve( { bd_order_id: response.order_id.toString(), ...result } );
+					},
+					() => {
+						transactionOptions.reduxDispatch(
+							recordTracksEvent(
+								'calypso_checkout_razorpay_modal_dismissed',
+								constructTracksProps( formattedTransactionData, {} )
+							)
+						);
+						return resolve( {} );
 					}
 				);
 				debug( 'Opening Razorpay modal with options', razorpayOptions );
 				transactionOptions.reduxDispatch(
-					recordTracksEvent( 'calypso_checkout_razorpay_modal_open', {
-						transactionData: formattedTransactionData,
-					} )
+					recordTracksEvent(
+						'calypso_checkout_razorpay_modal_open',
+						constructTracksProps( formattedTransactionData, {} )
+					)
 				);
 				const razorpay = new window.Razorpay( razorpayOptions );
 				razorpay.open();
@@ -140,9 +152,10 @@ export default async function razorpayProcessor(
 
 			if ( confirmationResult.success === true ) {
 				transactionOptions.reduxDispatch(
-					recordTracksEvent( 'calypso_checkout_razorpay_confirmation_success', {
-						confirmationResult,
-					} )
+					recordTracksEvent(
+						'calypso_checkout_razorpay_confirmation_success',
+						constructTracksProps( formattedTransactionData, confirmationResult )
+					)
 				);
 				return makeSuccessResponse( confirmationResult );
 			}
@@ -180,7 +193,8 @@ function combineRazorpayOptions(
 	razorpayConfiguration: RazorpayConfiguration,
 	contactDetails: ManagedContactDetails | undefined,
 	txnResponse: WPCOMTransactionEndpointResponseRedirect,
-	handler: ( response: RazorpayModalResponse ) => void
+	handler: ( response: RazorpayModalResponse ) => void,
+	ondismiss: () => void
 ): RazorpayOptions {
 	if ( ! txnResponse.razorpay_order_id ) {
 		throw new Error( 'Missing order_id for razorpay transaction' );
@@ -195,7 +209,7 @@ function combineRazorpayOptions(
 
 	options.handler = handler;
 	const modal = options.modal ?? {};
-	modal.ondismiss = handler;
+	modal.ondismiss = ondismiss;
 	options.modal = modal;
 
 	debug( 'Constructing Razorpay prefill object using contact details', contactDetails );
@@ -206,4 +220,18 @@ function combineRazorpayOptions(
 	options.prefill = prefill;
 
 	return options;
+}
+
+function constructTracksProps(
+	transactionData: WPCOMTransactionEndpointRequestPayload,
+	confirmationResult: object
+): object {
+	const tracksProps = {
+		blog_id: transactionData.cart.blog_id,
+		price_integer:
+			'price_integer' in confirmationResult ? confirmationResult.price_integer : 'unset',
+		receipt_id: 'receipt_id' in confirmationResult ? confirmationResult.receipt_id : 'unset',
+		success: 'success' in confirmationResult ? confirmationResult.success : 'unset',
+	};
+	return tracksProps;
 }
