d52f523a25d4ecec717d16696f2544a85eeaff88
Dan Luu <danluu@gmail.com>
2024-03-21 21:05:54 -0700

    Reduce size of images in designSetup by passing smaller arguments to mShot.
    This is a follow-up to @niranjan-uma-shankar finding that the design
    picker has a 70 MB payload. We can see in our funnel data that this
    causes a lot of users, including new users, to drop the process of
    creating a site at this step. When segmenting users, we can see that
    users are relatively more likely to drop out if they have a slow
    connection or a slow device, which supports the idea that 70 MB is too
    large a payload for many users.
    
    This commit reduces the payload size to 16 MB, which is still too large
    a payload for many users, but will allow more users to be able to pick
    a theme and create a site. There are a number of possible longer-term
    solutions that could get this payload down to the point where almost
    every user will be able to pick a theme, but those will be more involved
    fixes.

--- packages/design-picker/src/utils/available-designs.ts
+++ packages/design-picker/src/utils/available-designs.ts
@@ -4,25 +4,24 @@ import type { MShotsOptions } from '@automattic/onboarding';
 
 type MShotInputOptions = {
 	scrollable?: boolean;
-	highRes?: boolean;
 	isMobile?: boolean;
 };
 
 // Used for both prefetching and loading design screenshots
 export const getMShotOptions = ( {
 	scrollable,
-	highRes,
 	isMobile,
 }: MShotInputOptions = {} ): MShotsOptions => {
 	// Take care changing these values, as the design-picker CSS animations are written for these values (see the *__landscape and *__portrait classes)
+
+	// These settings are arbitrary. Switching from the previous settings (1199x3600) to these settings reduces page weight from 70 MB to 16 MB,
+	// These settings are intended to, at best, be temporary. A long-term solution for this should probably have values determined by A/B tests,
+	// end up serving WEBPs instead of JPEGs, spend fewer bits on parts of images that are not displayed, and possibly display fewer images.
 	return {
 		vpw: isMobile ? MOBILE_VIEWPORT_WIDTH : DEFAULT_VIEWPORT_WIDTH,
-		// 1040 renders well with all the current designs, more details in the links below.
-		// See: #77052
 		vph: scrollable ? 1600 : 1040,
-		// When `w` was 1200 it created a visual glitch on one thumbnail. #57261
-		w: highRes ? 1199 : 600,
-		screen_height: 3600,
+		w: 500,
+		screen_height: 1000,
 	};
 };
 
