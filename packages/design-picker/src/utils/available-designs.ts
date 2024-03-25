20dc10428526a693f3599cf09b3c34ebeba39f82
Dan Luu <danluu@gmail.com>
2024-03-25 09:36:42 -0700

    Increase image size slightly over previous experiment
    This requests 600x1100, which should give us 100% visual fidelity on
    a 1x display. This will have some perceptual difference in quality
    on a 2x/retina display.

--- packages/design-picker/src/utils/available-designs.ts
+++ packages/design-picker/src/utils/available-designs.ts
@@ -4,24 +4,26 @@ import type { MShotsOptions } from '@automattic/onboarding';
 
 type MShotInputOptions = {
 	scrollable?: boolean;
+	highRes?: boolean;
 	isMobile?: boolean;
 };
 
 // Used for both prefetching and loading design screenshots
 export const getMShotOptions = ( {
 	scrollable,
+	highRes,
 	isMobile,
 }: MShotInputOptions = {} ): MShotsOptions => {
 	// Take care changing these values, as the design-picker CSS animations are written for these values (see the *__landscape and *__portrait classes)
 
-	// These settings are arbitrary. Switching from the previous settings (1199x3600) to these settings reduces page weight from 70 MB to 16 MB,
+	// These settings are arbitrary. Switching from the previous settings (1199x3600) to these settings reduces page weight from 70 MB to ~5 MB,
 	// These settings are intended to, at best, be temporary. A long-term solution for this should probably have values determined by A/B tests,
 	// end up serving WEBPs instead of JPEGs, spend fewer bits on parts of images that are not displayed, and possibly display fewer images.
 	return {
 		vpw: isMobile ? MOBILE_VIEWPORT_WIDTH : DEFAULT_VIEWPORT_WIDTH,
 		vph: scrollable ? 1600 : 1040,
-		w: 500,
-		screen_height: 1000,
+		w: highRes ? 600 : 600,
+		screen_height: 1100,
 	};
 };
 
