015ceb796d3a8326174e5d30b5a8d07f512feebb
chad1008 <13856531+chad1008@users.noreply.github.com>
2024-03-25 13:09:28 -0400

    prep copy-paste-info for l10n

--- client/my-sites/patterns/components/copy-paste-info/index.tsx
+++ client/my-sites/patterns/components/copy-paste-info/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslate } from 'i18n-calypso';
 import { PatternsSection } from 'calypso/my-sites/patterns/components/section';
 import ImgCopyPaste from './images/copy-paste.svg';
 import ImgEdit from './images/edit.svg';
@@ -7,6 +8,7 @@ import ImgStyle from './images/style.svg';
 import './style.scss';
 
 export function PatternsCopyPasteInfo() {
+	const translateNotYet = useTranslate();
 	return (
 		<PatternsSection
 			bodyFullWidth
@@ -21,9 +23,13 @@ export function PatternsCopyPasteInfo() {
 							<img src={ ImgCopyPaste } alt="" />
 						</div>
 
-						<div className="section-patterns-info__item-title">Copy-paste your way</div>
+						<div className="section-patterns-info__item-title">
+							{ translateNotYet( 'Copy-paste your way' ) }
+						</div>
 						<div className="section-patterns-info__item-description">
-							Paste patterns directly into the WordPress editor to fully customize them.
+							{ translateNotYet(
+								'Paste patterns directly into the WordPress editor to fully customize them.'
+							) }
 						</div>
 					</div>
 
@@ -32,10 +38,13 @@ export function PatternsCopyPasteInfo() {
 							<img src={ ImgStyle } alt="" />
 						</div>
 
-						<div className="section-patterns-info__item-title">Bring your style with you</div>
+						<div className="section-patterns-info__item-title">
+							{ translateNotYet( 'Bring your style with you' ) }
+						</div>
 						<div className="section-patterns-info__item-description">
-							Patterns replicate the typography and color palette from your site to ensure every
-							page is on-brand.
+							{ translateNotYet(
+								'Patterns replicate the typography and color palette from your site to ensure every page is on-brand.'
+							) }
 						</div>
 					</div>
 
@@ -44,10 +53,13 @@ export function PatternsCopyPasteInfo() {
 							<img src={ ImgEdit } alt="" />
 						</div>
 
-						<div className="section-patterns-info__item-title">Make it yours</div>
+						<div className="section-patterns-info__item-title">
+							{ translateNotYet( 'Make it yours' ) }
+						</div>
 						<div className="section-patterns-info__item-description">
-							Patterns are collections of regular WordPress blocks, so you can edit every detail,
-							however you want.
+							{ translateNotYet(
+								'Patterns are collections of regular WordPress blocks, so you can edit every detail, however you want.'
+							) }
 						</div>
 					</div>
 
@@ -56,10 +68,13 @@ export function PatternsCopyPasteInfo() {
 							<img src={ ImgResponsive } alt="" />
 						</div>
 
-						<div className="section-patterns-info__item-title">Responsive by design</div>
+						<div className="section-patterns-info__item-title">
+							{ translateNotYet( 'Responsive by design' ) }
+						</div>
 						<div className="section-patterns-info__item-description">
-							All patterns are fully responsive to ensure they look fantastic on any device or
-							screen.
+							{ translateNotYet(
+								'All patterns are fully responsive to ensure they look fantastic on any device or screen.'
+							) }
 						</div>
 					</div>
 				</div>
