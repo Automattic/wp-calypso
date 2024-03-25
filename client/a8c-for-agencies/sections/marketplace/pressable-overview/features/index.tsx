0357db31d3dcb460cec6c1482c8e01f3dcaba7a8
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-03-25 19:17:44 +0800

    Add missing feature icons and fix mobile responsiveness

--- client/a8c-for-agencies/sections/marketplace/pressable-overview/features/index.tsx
+++ client/a8c-for-agencies/sections/marketplace/pressable-overview/features/index.tsx
@@ -1,3 +1,4 @@
+import { Icon, blockMeta, info, layout, levelUp, shuffle, tip } from '@wordpress/icons';
 import { useTranslate } from 'i18n-calypso';
 
 import './style.scss';
@@ -9,68 +10,87 @@ export default function PressableOverviewFeatures() {
 		<>
 			<div className="pressable-overview-features">
 				<div className="pressable-overview-features__item">
-					<div className="pressable-overview-features__item-title">
-						{ translate( 'Intuitive Control Panel' ) }
-					</div>
-					<div className="pressable-overview-features__item-description">
-						{ translate(
-							`Although it's won awards for being so easy to use, our interface is a powerhouse that delivers for even the most technical of users.`
-						) }
+					<Icon className="pressable-overview-features__item-icon" icon={ layout } size={ 48 } />
+
+					<div className="pressable-overview-features__item-content">
+						<div className="pressable-overview-features__item-title">
+							{ translate( 'Intuitive Control Panel' ) }
+						</div>
+						<div className="pressable-overview-features__item-description">
+							{ translate(
+								`Although it's won awards for being so easy to use, our interface is a powerhouse that delivers for even the most technical of users.`
+							) }
+						</div>
 					</div>
 				</div>
 
 				<div className="pressable-overview-features__item">
-					<div className="pressable-overview-features__item-title">
-						{ translate( 'Easy Migrations' ) }
-					</div>
-					<div className="pressable-overview-features__item-description">
-						{ translate(
-							`We'll migrate your sites for free or you can use our powerful plugin to do it yourself - we're here to help.`
-						) }
-					</div>
-				</div>
-
-				<div className="pressable-overview-features__item">
-					<div className="pressable-overview-features__item-title">
-						{ translate( 'Flexible Upgrades & Downgrades' ) }
-					</div>
-					<div className="pressable-overview-features__item-description">
-						{ translate(
-							'Need to make a change? No problem. Our plans are flexible, so they grow with your business.'
-						) }
+					<Icon className="pressable-overview-features__item-icon" icon={ info } size={ 48 } />
+					<div className="pressable-overview-features__item-content">
+						<div className="pressable-overview-features__item-title">
+							{ translate( '24/7 Expert Support' ) }
+						</div>
+						<div className="pressable-overview-features__item-description">
+							{ translate(
+								'When you win, we win. That’s why our team of WordPress professionals is always available to help.'
+							) }
+						</div>
 					</div>
 				</div>
 
 				<div className="pressable-overview-features__item">
-					<div className="pressable-overview-features__item-title">
-						{ translate( '24/7 Expert Support' ) }
-					</div>
-					<div className="pressable-overview-features__item-description">
-						{ translate(
-							'When you win, we win. That’s why our team of WordPress professionals is always available to help.'
-						) }
-					</div>
-				</div>
-
-				<div className="pressable-overview-features__item">
-					<div className="pressable-overview-features__item-title">
-						{ translate( '30-Day Money-Back Guarantee' ) }
-					</div>
-					<div className="pressable-overview-features__item-description">
-						{ translate(
-							'We’re so sure you’ll be satisfied with Pressable that we offer you the world’s best WordPress hosting with no-strings-attached.'
-						) }
+					<Icon className="pressable-overview-features__item-icon" icon={ blockMeta } size={ 48 } />
+					<div className="pressable-overview-features__item-content">
+						<div className="pressable-overview-features__item-title">
+							{ translate( 'Easy Migrations' ) }
+						</div>
+						<div className="pressable-overview-features__item-description">
+							{ translate(
+								`We'll migrate your sites for free or you can use our powerful plugin to do it yourself - we're here to help.`
+							) }
+						</div>
 					</div>
 				</div>
 
 				<div className="pressable-overview-features__item">
-					<div className="pressable-overview-features__item-title">
-						{ translate( '100% Uptime Guarantee' ) }
+					<Icon className="pressable-overview-features__item-icon" icon={ levelUp } size={ 48 } />
+					<div className="pressable-overview-features__item-content">
+						<div className="pressable-overview-features__item-title">
+							{ translate( '30-Day Money-Back Guarantee' ) }
+						</div>
+						<div className="pressable-overview-features__item-description">
+							{ translate(
+								'We’re so sure you’ll be satisfied with Pressable that we offer you the world’s best WordPress hosting with no-strings-attached.'
+							) }
+						</div>
 					</div>
-					<div className="pressable-overview-features__item-description">
-						{ translate(
-							'You need reliability - we promise it. Our cloud-based architecture ensures success by making your site available all day, every day.'
-						) }
+				</div>
+
+				<div className="pressable-overview-features__item">
+					<Icon className="pressable-overview-features__item-icon" icon={ shuffle } size={ 48 } />
+					<div className="pressable-overview-features__item-content">
+						<div className="pressable-overview-features__item-title">
+							{ translate( 'Flexible Upgrades & Downgrades' ) }
+						</div>
+						<div className="pressable-overview-features__item-description">
+							{ translate(
+								'Need to make a change? No problem. Our plans are flexible, so they grow with your business.'
+							) }
+						</div>
+					</div>
+				</div>
+
+				<div className="pressable-overview-features__item">
+					<Icon className="pressable-overview-features__item-icon" icon={ tip } size={ 48 } />
+					<div className="pressable-overview-features__item-content">
+						<div className="pressable-overview-features__item-title">
+							{ translate( '100% Uptime Guarantee' ) }
+						</div>
+						<div className="pressable-overview-features__item-description">
+							{ translate(
+								'You need reliability - we promise it. Our cloud-based architecture ensures success by making your site available all day, every day.'
+							) }
+						</div>
 					</div>
 				</div>
 			</div>
