039011aa5021132342287460903d197b7a883343
Dan Luu <danluu@gmail.com>
2024-03-25 18:20:45 -0700

    Switch back to 500 wide, but keep 1100 depth

--- packages/design-picker/src/utils/available-designs.ts
+++ packages/design-picker/src/utils/available-designs.ts
@@ -19,10 +19,12 @@ export const getMShotOptions = ( {
 	// These settings are arbitrary. Switching from the previous settings (1199x3600) to these settings reduces page weight from 70 MB to ~5 MB,
 	// These settings are intended to, at best, be temporary. A long-term solution for this should probably have values determined by A/B tests,
 	// end up serving WEBPs instead of JPEGs, spend fewer bits on parts of images that are not displayed, and possibly display fewer images.
+	//
+	// See #88786 for more info.
 	return {
 		vpw: isMobile ? MOBILE_VIEWPORT_WIDTH : DEFAULT_VIEWPORT_WIDTH,
 		vph: scrollable ? 1600 : 1040,
-		w: highRes ? 600 : 600,
+		w: highRes ? 500 : 500, // Stubbed out. See #88786 for more info.
 		screen_height: 1100,
 	};
 };
