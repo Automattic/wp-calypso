3c11052410b7ba87d64010cc0232c9ca60940ef8
Jasper Kang <kangzjnet@gmail.com>
2024-03-28 10:55:16 +1300

    we do not want to enable for existig sites

--- client/my-sites/stats/hooks/use-site-complusory-plan-selection-qualified-check.ts
+++ client/my-sites/stats/hooks/use-site-complusory-plan-selection-qualified-check.ts
@@ -7,11 +7,9 @@ export default function useSiteComplusoryPlanSelectionQualifiedCheck( siteId: nu
 	) as string;
 	const isNewSite =
 		siteCreatedTimeStamp && new Date( siteCreatedTimeStamp ) > new Date( '2024-01-31' ); // Targeting new sites
-	const isExistingSampleSite = siteId && siteId % 100 < 1; // Targeting 1% of existing sites
 
 	return {
 		isNewSite,
-		isExistingSampleSite,
-		isQualified: isNewSite || isExistingSampleSite,
+		isQualified: isNewSite,
 	};
 }
