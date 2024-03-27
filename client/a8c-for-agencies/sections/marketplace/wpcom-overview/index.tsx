aefd48c00194fb016d8816e818e446aca6aa58bb
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-03-27 14:12:57 +0800

    A4A: Add Hosting section. (#88755)
    * Convert filter search component as reusable component.
    
    * Move listing section as a shared component.
    
    * Move useProductAndPlans as a shared hook.
    
    * Make listing section icon as optional.
    
    * Add Pressable filter support in useProductAndPlans hook.
    
    * Add Hosting section skeleton.
    
    * Remove unused CSS codes.
    
    * Implement Hosting card.
    
    * Fix explore button redirect.
    
    * Add WPCOM and Pressable section.
    
    * Address PR comments.

--- client/a8c-for-agencies/sections/marketplace/wpcom-overview/index.tsx
+++ client/a8c-for-agencies/sections/marketplace/wpcom-overview/index.tsx
@@ -1,35 +1,52 @@
 import page from '@automattic/calypso-router';
-import classNames from 'classnames';
 import { useTranslate } from 'i18n-calypso';
 import Layout from 'calypso/a8c-for-agencies/components/layout';
 import LayoutBody from 'calypso/a8c-for-agencies/components/layout/body';
 import LayoutHeader, {
-	LayoutHeaderTitle as Title,
+	LayoutHeaderBreadcrumb as Breadcrumb,
 	LayoutHeaderActions as Actions,
 } from 'calypso/a8c-for-agencies/components/layout/header';
 import LayoutTop from 'calypso/a8c-for-agencies/components/layout/top';
 import MobileSidebarNavigation from 'calypso/a8c-for-agencies/components/sidebar/mobile-sidebar-navigation';
-import { A4A_MARKETPLACE_CHECKOUT_LINK } from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
+import {
+	A4A_MARKETPLACE_CHECKOUT_LINK,
+	A4A_MARKETPLACE_HOSTING_LINK,
+	A4A_MARKETPLACE_LINK,
+} from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
 import useShoppingCart from '../hooks/use-shopping-cart';
 import ShoppingCart from '../shopping-cart';
-import HostingList from './hosting-list';
 
-export default function Hosting() {
+export default function WpcomOverview() {
 	const translate = useTranslate();
 
 	const { selectedCartItems, onRemoveCartItem } = useShoppingCart();
 
 	return (
 		<Layout
-			className={ classNames( 'hosting-overview' ) }
-			title={ translate( 'Hosting Marketplace' ) }
+			className="wpcom-overview"
+			title={ translate( 'Wordpress.com hosting' ) }
 			wide
 			withBorder
+			compact
 			sidebarNavigation={ <MobileSidebarNavigation /> }
 		>
 			<LayoutTop>
 				<LayoutHeader>
-					<Title>{ translate( 'Marketplace' ) }</Title>
+					<Breadcrumb
+						items={ [
+							{
+								label: translate( 'Marketplace' ),
+								href: A4A_MARKETPLACE_LINK,
+							},
+							{
+								label: translate( 'Hosting' ),
+								href: A4A_MARKETPLACE_HOSTING_LINK,
+							},
+							{
+								label: translate( 'Wordpress.com hosting' ),
+							},
+						] }
+					/>
 
 					<Actions>
 						<ShoppingCart
@@ -43,9 +60,7 @@ export default function Hosting() {
 				</LayoutHeader>
 			</LayoutTop>
 
-			<LayoutBody>
-				<HostingList />
-			</LayoutBody>
+			<LayoutBody>Wordpress.com hosting here</LayoutBody>
 		</Layout>
 	);
 }
