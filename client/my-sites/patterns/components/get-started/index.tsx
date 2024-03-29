f23e840ded54e1750d1c5a9293f8c931fcddcfc7
Chad Chadbourne <13856531+chad1008@users.noreply.github.com>
2024-03-29 14:49:52 -0400

    Pattern Library: Prep strings for localization (#88871)
    * prep copy-paste-info for l10n
    
    * prep get-access-modal for l10n
    
    * prep get-started for l10n
    
    * prep header for l10n
    
    * prep pattern-preview for l10n
    
    * snake_case for consistency
    
    * more SEO friendly H1 text
    
    * update title tags
    
    * the one that got away...
    
    * proper ellipsis for translation
    
    * dynamic page titles for each category
    
    * remove variables from `translate` args
    
    * ready for translation
    
    * translate `copy-paste-info` title and description
    
    * translate pattern count string
    
    * Translate singular/plural in CategoryGallery
    
    * Apply changes from #88974
    
    * Add comments
    
    * Add more comments, no useMemo
    
    * Fix type error
    
    * clean up category not found strings
    
    * Update client/my-sites/patterns/components/pattern-preview/index.tsx
    
    Co-authored-by: Stéphane Thomas <email@stephanethomas.com>
    
    * add translator comments
    
    * More translation comments
    
    * One more comment
    
    * Even more comments
    
    * Update client/my-sites/patterns/components/pattern-library/index.tsx
    
    Co-authored-by: Fredrik Rombach Ekelund <fredrik@fredrik.computer>
    
    * fix broken quotes from last suggestion
    
    * move `DocumentHead` call and SEO content into isolated component
    
    ---------
    
    Co-authored-by: Fredrik Rombach Ekelund <fredrik.rombach.ekelund@automattic.com>
    Co-authored-by: Stéphane Thomas <email@stephanethomas.com>
    Co-authored-by: Fredrik Rombach Ekelund <fredrik@fredrik.computer>

--- client/my-sites/patterns/components/get-started/index.tsx
+++ client/my-sites/patterns/components/get-started/index.tsx
@@ -1,5 +1,6 @@
 import { localizeUrl } from '@automattic/i18n-utils';
 import { Button } from '@wordpress/components';
+import { useTranslate } from 'i18n-calypso';
 import imagePreviewPublish from 'calypso/my-sites/patterns/components/get-started/images/preview-publish.png';
 import imagePageLayouts from 'calypso/my-sites/patterns/components/get-started/images/understand-page-layouts.png';
 import imageBlockPatterns from 'calypso/my-sites/patterns/components/get-started/images/use-block-patterns.png';
@@ -8,16 +9,20 @@ import { PatternsSection } from 'calypso/my-sites/patterns/components/section';
 import './style.scss';
 
 export function PatternsGetStarted() {
+	const translate = useTranslate();
 	return (
 		<PatternsSection
 			bodyFullWidth
-			description="Take a look at our how-to guides to get started with patterns."
+			description={ translate( 'Take a look at our how-to guides to get started with patterns.' ) }
 			theme="dark"
-			title="All about patterns"
+			title={ translate( 'All about patterns', {
+				comment: 'Heading text in a section with informative links about block patterns',
+				textOnly: true,
+			} ) }
 		>
 			<div className="patterns-get-started__buttons">
 				<Button className="patterns-get-started__start-button" href="/start">
-					Build a site
+					{ translate( 'Build a site' ) }
 				</Button>
 			</div>
 
@@ -35,8 +40,13 @@ export function PatternsGetStarted() {
 							height="675"
 							loading="lazy"
 						/>
-						<div className="patterns-get-started__item-name">Video tutorial</div>
-						<div className="patterns-get-started__item-description">Block Patterns</div>
+						<div className="patterns-get-started__item-name">{ translate( 'Video tutorial' ) }</div>
+						<div className="patterns-get-started__item-description">
+							{ translate( 'Block Patterns', {
+								comment:
+									'This string is a copy of the page title from wordpress.com/support/wordpress-editor/block-pattern/',
+							} ) }
+						</div>
 					</a>
 
 					<a
@@ -51,8 +61,13 @@ export function PatternsGetStarted() {
 							height="675"
 							loading="lazy"
 						/>
-						<div className="patterns-get-started__item-name">Video tutorial</div>
-						<div className="patterns-get-started__item-description">Use Pre-Made Page Layouts</div>
+						<div className="patterns-get-started__item-name">{ translate( 'Video tutorial' ) }</div>
+						<div className="patterns-get-started__item-description">
+							{ translate( 'Use Pre-Made Page Layouts', {
+								comment:
+									'This string is a copy of the page title from wordpress.com/support/wordpress-editor/page-layouts/',
+							} ) }
+						</div>
 					</a>
 
 					<a
@@ -67,8 +82,13 @@ export function PatternsGetStarted() {
 							height="639"
 							loading="lazy"
 						/>
-						<div className="patterns-get-started__item-name">Free course</div>
-						<div className="patterns-get-started__item-description">Design Your Homepage</div>
+						<div className="patterns-get-started__item-name">{ translate( 'Free course' ) }</div>
+						<div className="patterns-get-started__item-description">
+							{ translate( 'Design Your Homepage', {
+								comment:
+									'This string is a copy of the page title from wordpress.com/learn/webinars/compelling-homepages/',
+							} ) }
+						</div>
 					</a>
 				</div>
 			</div>
