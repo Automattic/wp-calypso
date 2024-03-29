75e1015179a9126270ebce7d9df4a38b789ab8ac
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-03-29 21:13:47 +0800

    Add Landing page to fetch Agency information.

--- client/a8c-for-agencies/sections/landing/landing.tsx
+++ client/a8c-for-agencies/sections/landing/landing.tsx
@@ -1,25 +1,56 @@
+import page from '@automattic/calypso-router';
+import { Spinner } from '@wordpress/components';
+import { getQueryArg } from '@wordpress/url';
 import { useTranslate } from 'i18n-calypso';
+import { useEffect } from 'react';
 import Layout from 'calypso/a8c-for-agencies/components/layout';
 import LayoutBody from 'calypso/a8c-for-agencies/components/layout/body';
 import LayoutHeader, {
 	LayoutHeaderTitle as Title,
 } from 'calypso/a8c-for-agencies/components/layout/header';
 import LayoutTop from 'calypso/a8c-for-agencies/components/layout/top';
-
-import './style.scss';
+import {
+	A4A_OVERVIEW_LINK,
+	A4A_SIGNUP_LINK,
+} from 'calypso/a8c-for-agencies/components/sidebar-menu/lib/constants';
+import { useSelector } from 'calypso/state';
+import { getActiveAgency, hasFetchedAgency } from 'calypso/state/a8c-for-agencies/agency/selectors';
 
 export default function Landing() {
 	const translate = useTranslate();
-	const title = translate( 'Landing page.' );
+	const title = translate( 'Automattic for Agencies' );
+
+	const hasFetched = useSelector( hasFetchedAgency );
+	const agency = useSelector( getActiveAgency );
+
+	useEffect( () => {
+		if ( ! hasFetched ) {
+			return;
+		}
+
+		if ( agency ) {
+			const returnQuery = getQueryArg( window.location.href, 'return' ) as string;
+
+			if ( returnQuery ) {
+				page.redirect( returnQuery );
+			}
+
+			page.redirect( A4A_OVERVIEW_LINK );
+		}
+
+		page.redirect( A4A_SIGNUP_LINK );
+	}, [ agency, hasFetched ] );
 
 	return (
 		<Layout title={ title } wide>
 			<LayoutTop>
-				<LayoutHeader className="a4a-overview-header">
+				<LayoutHeader>
 					<Title>{ title }</Title>
 				</LayoutHeader>
 			</LayoutTop>
-			<LayoutBody>Fetching..</LayoutBody>
+			<LayoutBody>
+				<Spinner />
+			</LayoutBody>
 		</Layout>
 	);
 }
