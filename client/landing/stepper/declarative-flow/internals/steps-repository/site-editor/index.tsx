6b6bde19e65506719c21f98f4cd7934a72c3bef5
artpi <artur.piszek@gmail.com>
2024-03-29 15:32:15 +0100

    Successfully install assembler theme

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-editor/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-editor/index.tsx
@@ -1,12 +1,16 @@
 import { StepContainer } from '@automattic/onboarding';
 import debugFactory from 'debug';
 import { useTranslate } from 'i18n-calypso';
+import { ThunkAction } from 'redux-thunk';
 import DocumentHead from 'calypso/components/data/document-head';
 import FormattedHeader from 'calypso/components/formatted-header';
 import { NavigationControls } from 'calypso/landing/stepper/declarative-flow/internals/types';
 import { useSite } from 'calypso/landing/stepper/hooks/use-site';
 import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
+import { useDispatch as useReduxDispatch } from 'calypso/state';
+import { activateOrInstallThenActivate } from 'calypso/state/themes/actions';
 import type { Step } from '../../types';
+import type { AnyAction } from 'redux';
 
 type SiteEditorProps = {
 	navigation: NavigationControls;
@@ -14,14 +18,29 @@ type SiteEditorProps = {
 };
 const debug = debugFactory( 'stepper:ai-assembler' );
 
-const SiteEditor: Step = ( { navigation }: SiteEditorProps ) => {
+const SiteEditor: Step = ( { flow }: SiteEditorProps ) => {
 	const translate = useTranslate();
 	const site = useSite();
+	const reduxDispatch = useReduxDispatch();
 
-	if ( site?.URL ) {
+	let conditions: PromiseLike< string > = Promise.resolve( '' );
+
+	if ( site && flow === 'ai-assembler' ) {
+		conditions = reduxDispatch(
+			activateOrInstallThenActivate( 'assembler', site.ID, 'assembler', false ) as ThunkAction<
+				PromiseLike< string >,
+				any,
+				any,
+				AnyAction
+			>
+		);
+	}
+
+	if ( site && site.URL ) {
 		debug( 'Redirecting to site editor' );
-		window.location.replace( site.URL + '/wp-admin/site-editor.php' );
-		return;
+		conditions.then( () => {
+			window.location.replace( site.URL + '/wp-admin/site-editor.php' );
+		} );
 	}
 
 	return (
@@ -29,11 +48,11 @@ const SiteEditor: Step = ( { navigation }: SiteEditorProps ) => {
 			<DocumentHead title={ translate( 'Edit your site' ) } />
 			<StepContainer
 				stepName="site-editor"
-				goNext={ navigation.goNext }
 				isFullLayout={ true }
 				skipLabelText={ translate( 'Skip for now' ) }
 				skipButtonAlign="top"
 				hideBack={ true }
+				hideNext={ true }
 				stepContent={ <></> }
 				formattedHeader={
 					<FormattedHeader
