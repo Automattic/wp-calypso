b0d3d611805bf4d3dbd2369df43d2e61e54fee6e
Yashwin Poojary <yashwinpoojary@gmail.com>
2024-04-02 12:17:28 +0530

    Automattic for Agencies: Check if payment method is required and redirect to add payment page (#88998)
    * Check if payment method is required and redirect to add payment page
    
    * fix go back button

--- client/a8c-for-agencies/sections/purchases/payment-methods/hooks/use-return-url.ts
+++ client/a8c-for-agencies/sections/purchases/payment-methods/hooks/use-return-url.ts
@@ -1,7 +1,10 @@
 import page from '@automattic/calypso-router';
 import { getQueryArg } from '@wordpress/url';
 import { useEffect } from 'react';
-import { A4A_PURCHASES_LINK } from '../../../../components/sidebar-menu/lib/constants';
+import {
+	A4A_PURCHASES_LINK,
+	A4A_MARKETPLACE_CHECKOUT_LINK,
+} from '../../../../components/sidebar-menu/lib/constants';
 
 type Props = {
 	redirect: boolean;
@@ -12,7 +15,9 @@ export function useReturnUrl( { redirect }: Props ) {
 		if ( redirect ) {
 			const returnQuery = getQueryArg( window.location.href, 'return' ) as string;
 			const returnUrl =
-				returnQuery && returnQuery.startsWith( A4A_PURCHASES_LINK )
+				returnQuery &&
+				( returnQuery.startsWith( A4A_PURCHASES_LINK ) ||
+					returnQuery.startsWith( A4A_MARKETPLACE_CHECKOUT_LINK ) )
 					? returnQuery
 					: A4A_PURCHASES_LINK;
 
