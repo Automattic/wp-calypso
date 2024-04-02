04c5e8f9d9d1c9572a85eb5634070bc84a93c37c
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-04-02 17:34:44 +0800

    A4A: Add requireAccessContext to all restricted pages. (#89096)
    * Add requireAccessContext to all restircted pages.
    
    * Fix bug with redirect.

--- client/a8c-for-agencies/sections/plugins/index.tsx
+++ client/a8c-for-agencies/sections/plugins/index.tsx
@@ -1,7 +1,8 @@
 import page from '@automattic/calypso-router';
+import { requireAccessContext } from 'calypso/a8c-for-agencies/controller';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 import { pluginsContext } from './controller';
 
 export default function () {
-	page( '/plugins', pluginsContext, makeLayout, clientRender );
+	page( '/plugins', requireAccessContext, pluginsContext, makeLayout, clientRender );
 }
