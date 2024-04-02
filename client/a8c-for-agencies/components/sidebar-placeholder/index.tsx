ccc3aca804111cef38d322189ba8ecae560bd3d1
James Kenneth Guidaven <jkguidaven@gmail.com>
2024-04-02 20:02:07 +0800

    Add Sidebar placeholder.

---
+++ client/a8c-for-agencies/components/sidebar-placeholder/index.tsx
@@ -0,0 +1,26 @@
+import Sidebar, {
+	SidebarV2Header as SidebarHeader,
+	SidebarV2Main as SidebarMain,
+	SidebarV2Footer as SidebarFooter,
+} from 'calypso/layout/sidebar-v2';
+
+import './style.scss';
+
+export default function A4ASidebarPlaceholder() {
+	return (
+		<Sidebar className="a4a-sidebar a4a-sidebar-placeholder">
+			<SidebarHeader className="a4a-sidebar__header">
+				<div className="a4a-sidebar-placeholder__header-icon"></div>
+			</SidebarHeader>
+
+			<SidebarMain>
+				<div className="a4a-sidebar-placeholder__menu-item"></div>
+				<div className="a4a-sidebar-placeholder__menu-item"></div>
+			</SidebarMain>
+
+			<SidebarFooter>
+				<div className="a4a-sidebar-placeholder__menu-item"></div>
+			</SidebarFooter>
+		</Sidebar>
+	);
+}
