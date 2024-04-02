db483a316c89d75489720fbba75a0fbb58009de0
Yashwin Poojary <yashwinpoojary@gmail.com>
2024-04-02 11:38:16 +0530

    Automattic for Agencies: Fix the issue with Pressable plan list items not showing up (#89076)

--- client/a8c-for-agencies/sections/marketplace/checkout/product-info.tsx
+++ client/a8c-for-agencies/sections/marketplace/checkout/product-info.tsx
@@ -1,6 +1,9 @@
+import formatNumber from '@automattic/components/src/number-formatters/lib/format-number';
 import { useTranslate } from 'i18n-calypso';
+import pressableIcon from 'calypso/assets/images/pressable/pressable-icon.svg';
 import { useLicenseLightboxData } from 'calypso/jetpack-cloud/sections/partner-portal/license-lightbox/hooks/use-license-lightbox-data';
 import getProductIcon from 'calypso/my-sites/plans/jetpack-plans/product-store/utils/get-product-icon';
+import getPressablePlan from '../pressable-overview/lib/get-pressable-plan';
 import type { ShoppingCartItem } from '../types';
 
 export default function ProductInfo( { product }: { product: ShoppingCartItem } ) {
@@ -8,19 +11,46 @@ export default function ProductInfo( { product }: { product: ShoppingCartItem } 
 
 	const { title, product: productInfo } = useLicenseLightboxData( product );
 
-	if ( ! productInfo ) {
+	let productIcon =
+		productInfo?.productSlug && getProductIcon( { productSlug: productInfo?.productSlug } );
+	let productTitle = title;
+	let productDescription = productInfo?.lightboxDescription;
+
+	if ( product.family_slug === 'pressable-hosting' ) {
+		const presablePlan = getPressablePlan( product.slug );
+		if ( ! presablePlan ) {
+			return null;
+		}
+
+		productIcon = pressableIcon;
+		productTitle = product.name;
+		productDescription = translate(
+			'Plan with %(install)d WordPress installs, %(visits)s visits per month, and %(storage)dGB of storage per month.',
+			{
+				args: {
+					install: presablePlan.install,
+					visits: formatNumber( presablePlan.visits ),
+					storage: presablePlan.storage,
+				},
+				comment:
+					'The `install`, `visits` & `storage` are the count of WordPress installs, visits per month, and storage per month in the plan description.',
+			}
+		);
+	}
+
+	if ( ! productDescription ) {
 		return null;
 	}
 
 	return (
 		<div className="product-info">
 			<div className="product-info__icon">
-				<img src={ getProductIcon( { productSlug: productInfo.productSlug } ) } alt={ title } />
+				<img src={ productIcon } alt={ title } />
 			</div>
 			<div className="product-info__text-content">
 				<div className="product-info__header">
-					<label htmlFor={ title } className="product-info__label">
-						{ title }
+					<label htmlFor={ productTitle } className="product-info__label">
+						{ productTitle }
 					</label>
 					<span className="product-info__count">
 						{ translate( '%(numLicenses)d plan', '%(numLicenses)d plans', {
@@ -32,7 +62,7 @@ export default function ProductInfo( { product }: { product: ShoppingCartItem } 
 						} ) }
 					</span>
 				</div>
-				<p className="product-info__description">{ productInfo.lightboxDescription }</p>
+				<p className="product-info__description">{ productDescription }</p>
 			</div>
 		</div>
 	);
