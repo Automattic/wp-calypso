56104ef3b0d6f73e241d0050a6e88957275e234b
Tim Broddin <tim@wannabes.be>
2024-04-04 16:02:42 +0200

    Plugin Update Manager: Create useUpdateScheduleLogsQuery (#89200)
    * Create query
    
    * Prepare for changes in https://github.com/Automattic/jetpack/pull/36736

--- client/blocks/plugins-update-manager/schedule-logs.tsx
+++ client/blocks/plugins-update-manager/schedule-logs.tsx
@@ -1,9 +1,20 @@
+import { useUpdateScheduleLogsQuery } from 'calypso/data/plugins/use-update-schedule-logs-query';
+import { useSiteSlug } from './hooks/use-site-slug';
+
 interface Props {
 	scheduleId: string;
 	onNavBack?: () => void;
 }
 export const ScheduleLogs = ( props: Props ) => {
 	const { scheduleId } = props;
+	const siteSlug = useSiteSlug();
 
-	return <>Schedule Logs: { scheduleId }</>;
+	const { data: logs = [] } = useUpdateScheduleLogsQuery( siteSlug, scheduleId );
+
+	return (
+		<>
+			Schedule Logs: { scheduleId }
+			<pre>{ JSON.stringify( logs, null, 4 ) }</pre>
+		</>
+	);
 };
