82134e2552ef2d2d734c09e29013245020eda702
Tim Broddin <tim@wannabes.be>
2024-04-04 11:40:40 +0200

    Create query

---
+++ client/data/plugins/use-update-schedule-logs-query.ts
@@ -0,0 +1,49 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import wpcomRequest from 'wpcom-proxy-request';
+import type { SiteSlug } from 'calypso/types';
+
+type ScheduleLogsApiReturn = {
+	timestamp: string;
+	action: string;
+	message: string | null;
+	context: unknown;
+}[][];
+
+type ScheduleLogs = {
+	date: Date;
+	timestamp: string;
+	action: string;
+	message: string | null;
+	context: unknown;
+}[][];
+
+export const useUpdateScheduleLogsQuery = (
+	siteSlug: SiteSlug,
+	scheduleId: string
+): UseQueryResult< ScheduleLogs > => {
+	return useQuery< ScheduleLogsApiReturn, Error, ScheduleLogs >( {
+		queryKey: [ 'scheduled-logs', scheduleId, siteSlug ],
+		queryFn: () =>
+			wpcomRequest( {
+				path: `/sites/${ siteSlug }/update-schedules/${ scheduleId }/logs`,
+				apiNamespace: 'wpcom/v2',
+				method: 'GET',
+			} ),
+		enabled: !! siteSlug && !! scheduleId,
+		retry: false,
+		refetchOnWindowFocus: false,
+		select: ( data: ScheduleLogsApiReturn ): ScheduleLogs => {
+			return data
+				.map( ( run ) => {
+					return run.map( ( log ) => ( {
+						date: new Date( parseInt( log.timestamp, 10 ) * 1000 ),
+						timestamp: log.timestamp,
+						action: log.action,
+						message: log.message,
+						context: log.context,
+					} ) );
+				} )
+				.reverse();
+		},
+	} );
+};
