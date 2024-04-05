d398e250f596a1aa0f56f23c7a9c55c6bbf94771
Paulo Cruz <paulo.cruz@automattic.com>
2024-04-05 12:16:02 -0300

    Update save answer endpoint URL

--- client/landing/stepper/declarative-flow/internals/steps-repository/ecommerce-segmentation-survey/mutations/use-save-answers-mutation.ts
+++ client/landing/stepper/declarative-flow/internals/steps-repository/ecommerce-segmentation-survey/mutations/use-save-answers-mutation.ts
@@ -15,7 +15,7 @@ const useSaveAnswersMutation = ( { surveyKey, blogId }: SaveAnswersHookArgs ) =>
 	return useMutation( {
 		mutationFn: async ( { answerKeys, questionKey }: SaveAnswersMutationArgs ) => {
 			return wpcom.req.post( {
-				path: `/segmentation-survey`,
+				path: `/segmentation-survey/answers`,
 				apiNamespace: 'wpcom/v2',
 				body: {
 					blog_id: blogId,
