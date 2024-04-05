93db03af85d697a66e73f1366a3c02e18714995e
Dale du Preez <dale@automattic.com>
2024-04-05 15:23:40 +0200

    Remove locale checks for redirects to site-migration flow

--- client/landing/stepper/declarative-flow/helpers/test/index.ts
+++ client/landing/stepper/declarative-flow/helpers/test/index.ts
@@ -6,15 +6,6 @@ describe( 'DeclarativeFlowHelpers', () => {
 			shouldRedirectToSiteMigration(
 				STEPS.IMPORT_LIST.slug,
 				'wordpress',
-				'en',
-				STEPS.SITE_MIGRATION_IDENTIFY.slug
-			)
-		).toBe( true );
-		expect(
-			shouldRedirectToSiteMigration(
-				STEPS.IMPORT_LIST.slug,
-				'wordpress',
-				'en-gb',
 				STEPS.SITE_MIGRATION_IDENTIFY.slug
 			)
 		).toBe( true );
@@ -31,18 +22,6 @@ describe( 'DeclarativeFlowHelpers', () => {
 			shouldRedirectToSiteMigration(
 				STEPS.IMPORT_LIST.slug,
 				'other-platform',
-				'en',
-				STEPS.SITE_MIGRATION_IDENTIFY.slug
-			)
-		).toBe( false );
-	} );
-
-	it( 'returns false when locale is not en', () => {
-		expect(
-			shouldRedirectToSiteMigration(
-				STEPS.IMPORT_LIST.slug,
-				'wordpress',
-				'es',
 				STEPS.SITE_MIGRATION_IDENTIFY.slug
 			)
 		).toBe( false );
@@ -53,7 +32,6 @@ describe( 'DeclarativeFlowHelpers', () => {
 			shouldRedirectToSiteMigration(
 				STEPS.IMPORT_LIST.slug,
 				STEPS.SITE_MIGRATION_IDENTIFY.slug,
-				'en',
 				null
 			)
 		).toBe( false );
@@ -61,7 +39,7 @@ describe( 'DeclarativeFlowHelpers', () => {
 
 	it( 'returns false when the origin is not the site-migration-identify', () => {
 		expect(
-			shouldRedirectToSiteMigration( STEPS.IMPORT_LIST.slug, 'wordpress', 'en', 'other-origin' )
+			shouldRedirectToSiteMigration( STEPS.IMPORT_LIST.slug, 'wordpress', 'other-origin' )
 		).toBe( false );
 	} );
 } );
