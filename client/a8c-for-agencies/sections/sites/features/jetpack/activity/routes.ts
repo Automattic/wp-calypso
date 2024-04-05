2c4bac1734f843ccefa77645363179896c832810
cleacos <javacodia@gmail.com>
2024-04-05 13:57:17 +0100

    Remove not handled route to /activity-log

--- client/a8c-for-agencies/sections/sites/features/jetpack/activity/routes.ts
+++ client/a8c-for-agencies/sections/sites/features/jetpack/activity/routes.ts
@@ -2,12 +2,6 @@ import page from '@automattic/calypso-router';
 import { JETPACK_ACTIVITY_ID } from 'calypso/a8c-for-agencies/sections/sites/features/features';
 
 export default function () {
-	page( '/activity-log/', ( context, next ) => {
-		//todo: get the current selected feature family instead of the hardcoded 'overview'
-		page.replace( '/sites', context.state, true, true );
-		next();
-	} );
-
 	// Intercept the activity log route and redirect to the activity log feature
 	page( '/activity-log/:site', ( context, next ) => {
 		const { site } = context.params;
