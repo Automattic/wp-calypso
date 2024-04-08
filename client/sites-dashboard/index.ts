f3b18e8d6756aca11d42af681fa32c007680eccb
Addison-Stavlo <stavz01@gmail.com>
2024-04-08 09:28:41 -0400

    use routes from the new files

--- client/sites-dashboard/index.ts
+++ client/sites-dashboard/index.ts
@@ -1,3 +1,4 @@
+import { isEnabled } from '@automattic/calypso-config';
 import page from '@automattic/calypso-router';
 import { makeLayout, render as clientRender } from 'calypso/controller';
 import { getSiteBySlug, getSiteHomeUrl } from 'calypso/state/sites/selectors';
@@ -6,23 +7,29 @@ import {
 	sanitizeQueryParameters,
 	sitesDashboard,
 } from './controller';
+import siteDashboardRoute from './sections/sites';
 
 export default function () {
-	// Maintain old `/sites/:id` URLs by redirecting them to My Home
-	page( '/sites/:site', ( context ) => {
-		const state = context.store.getState();
-		const site = getSiteBySlug( state, context.params.site );
-		// The site may not be loaded into state yet.
-		const siteId = site?.ID;
-		page.redirect( getSiteHomeUrl( state, siteId ) );
-	} );
+	if ( isEnabled( 'layout/dotcom-nav-redesign-v2' ) ) {
+		siteDashboardRoute();
+	} else {
+		console.log( 'This is the OLD sites dashboard...' );
+		// Maintain old `/sites/:id` URLs by redirecting them to My Home
+		page( '/sites/:site', ( context ) => {
+			const state = context.store.getState();
+			const site = getSiteBySlug( state, context.params.site );
+			// The site may not be loaded into state yet.
+			const siteId = site?.ID;
+			page.redirect( getSiteHomeUrl( state, siteId ) );
+		} );
 
-	page(
-		'/sites',
-		maybeRemoveCheckoutSuccessNotice,
-		sanitizeQueryParameters,
-		sitesDashboard,
-		makeLayout,
-		clientRender
-	);
+		page(
+			'/sites',
+			maybeRemoveCheckoutSuccessNotice,
+			sanitizeQueryParameters,
+			sitesDashboard,
+			makeLayout,
+			clientRender
+		);
+	}
 }
