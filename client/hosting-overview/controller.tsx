5864746a1160b02682966b26516d16c4585f44ed
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-04-08 17:13:16 +0200

    Add  component with

--- client/hosting-overview/controller.tsx
+++ client/hosting-overview/controller.tsx
@@ -1,10 +1,15 @@
+import HostingOverview from 'calypso/hosting-overview/components/hosting-overview';
 import MySitesNavigation from 'calypso/my-sites/navigation';
 import type { Context as PageJSContext } from '@automattic/calypso-router';
 
 export default function hostingOverview( context: PageJSContext, next: () => void ) {
 	context.secondary = <MySitesNavigation path={ context.path } />;
 
-	context.primary = <div>foo</div>;
+	context.primary = (
+		<>
+			<HostingOverview />
+		</>
+	);
 
 	next();
 }
