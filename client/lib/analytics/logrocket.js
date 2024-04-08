3ecfa1b129f8e802c89dd57db2304fc87b4ec888
Rafael Agostini <rafael.agostini@automattic.com>
2024-04-07 23:01:22 -0500

    Add LogRocket

---
+++ client/lib/analytics/logrocket.js
@@ -0,0 +1,69 @@
+import { getCurrentUser } from '@automattic/calypso-analytics';
+import config from '@automattic/calypso-config';
+import debug from 'debug';
+import { TRACKING_IDS } from './ad-tracking/constants';
+import { mayWeTrackByTracker } from './tracker-buckets';
+
+const logRocketDebug = debug( 'calypso:analytics:logrocket' );
+
+let logRocketScriptLoaded = false;
+
+export function mayWeLoadLogRocketScript() {
+	return config.isEnabled( 'logrocket_enabled' ) && mayWeTrackByTracker( 'logrocket' );
+}
+
+export function maybeAddLogRocketScript() {
+	if ( logRocketScriptLoaded || ! mayWeLoadLogRocketScript() ) {
+		logRocketDebug( 'Not loading LogRocket script' );
+		return;
+	}
+
+	const script = document.createElement( 'script' );
+	script.src = 'https://cdn.logrocket.io/LogRocket.min.js';
+	script.crossOrigin = 'anonymous';
+	script.async = true;
+
+	script.onload = () => {
+		logRocketDebug( 'LogRocket script loaded' );
+		if ( window.LogRocket ) {
+			window.LogRocket.init( TRACKING_IDS.logRocket, {
+				network: {
+					isEnabled: false, // We don't need to record network calls for now
+				},
+			} );
+
+			logRocketScriptLoaded = true;
+		}
+	};
+
+	script.onerror = () => {
+		logRocketDebug( 'Error loading LogRocket script' );
+	};
+
+	document.head.appendChild( script );
+}
+
+function maybeIdentifyUser() {
+	if ( ! window.LogRocket ) {
+		return;
+	}
+
+	const currentUser = getCurrentUser();
+
+	if ( currentUser ) {
+		logRocketDebug( 'maybeIdentifyUser:', currentUser );
+		window.LogRocket.identify( currentUser.hashedPii.ID );
+	}
+}
+
+export function recordLogRocketEvent( name, props ) {
+	maybeAddLogRocketScript();
+
+	if ( ! window.LogRocket || ! name ) {
+		return;
+	}
+
+	logRocketDebug( 'recordLogRocketEvent:', { name, props } );
+	window.LogRocket.track( name, props );
+	maybeIdentifyUser();
+}
