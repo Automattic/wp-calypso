0c8d8d5d853a299542c71afb6c35bc12f7d452cd
Anthony Grullon <agrullon95@gmail.com>
2024-04-09 09:33:41 -0400

    add 3 testimonials to upgrade plan screen for english locale

--- client/blocks/importer/wordpress/upgrade-plan/upgrade-plan-hosting-details.tsx
+++ client/blocks/importer/wordpress/upgrade-plan/upgrade-plan-hosting-details.tsx
@@ -1,9 +1,14 @@
+import { useIsEnglishLocale } from '@automattic/i18n-utils';
+import { useState } from '@wordpress/element';
 import { Icon } from '@wordpress/icons';
 import { useI18n } from '@wordpress/react-i18n';
-import { UpgradePlanHostingDetailsList } from './constants';
+import { UpgradePlanHostingDetailsList, UpgradePlanHostingTestimonials } from './constants';
+import { UpgradePlanHostingDetailsTooltip } from './upgrade-plan-hosting-details-tooltip';
 
 export const UpgradePlanHostingDetails = () => {
 	const { __ } = useI18n();
+	const isEnglishLocale = useIsEnglishLocale();
+	const [ activeTooltipId, setActiveTooltipId ] = useState( '' );
 
 	return (
 		<div className="import__upgrade-plan-hosting-details">
@@ -32,6 +37,33 @@ export const UpgradePlanHostingDetails = () => {
 					) ) }
 				</ul>
 			</div>
+			{ isEnglishLocale && (
+				<div className="import__upgrade-plan-hosting-details-testimonials-container">
+					<p>{ __( '100% loved by our best customers' ) }</p>
+					<div className="import__upgrade-plan-hosting-details-testimonials">
+						{ UpgradePlanHostingTestimonials.map(
+							( { customerName, customerTestimonial, customerInfo, customerImage }, i ) => (
+								<UpgradePlanHostingDetailsTooltip
+									key={ i }
+									id={ `testimonial-${ i }` }
+									setActiveTooltipId={ setActiveTooltipId }
+									activeTooltipId={ activeTooltipId }
+									customerName={ customerName }
+									customerInfo={ customerInfo }
+									customerTestimonial={ customerTestimonial }
+									hideArrow={ false }
+								>
+									<img
+										className="import__upgrade-plan-hosting-details-testimonials-image"
+										src={ customerImage }
+										alt={ customerName }
+									/>
+								</UpgradePlanHostingDetailsTooltip>
+							)
+						) }
+					</div>
+				</div>
+			) }
 		</div>
 	);
 };
