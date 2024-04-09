87836326b505a16e0bb4cd1ba0b565d5276832b9
AllTerrainDeveloper <daniel.lopez@automattic.com>
2024-04-09 16:09:39 +0200

    Fix build

--- packages/odie-client/jest.config.js
+++ packages/odie-client/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
 	preset: '../../test/packages/jest-preset.js',
 	testEnvironment: 'jsdom',
 	testMatch: [ '<rootDir>/**/__tests__/**/*.[jt]s?(x)', '!**/.eslintrc.*' ],
-	transformIgnorePatterns: [ 'node_modules/(?!gridicons)(?!react-markdown)(?!.*\\.svg)' ],
+	transformIgnorePatterns: [ 'node_modules/(?!gridicons)(?!.*\\.svg)' ],
 };
