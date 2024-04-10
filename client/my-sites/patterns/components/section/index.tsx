09c57dc633214a1921d1e3b997b138fdf4dee364
Fredrik Rombach Ekelund <fredrik@fredrik.computer>
2024-04-10 09:53:25 +0200

    Pattern Library: Design tweaks (#89353)
    * Pattern library design tweaks
    
    * Taller type toggle on mobile
    
    * Address review comments

--- client/my-sites/patterns/components/section/index.tsx
+++ client/my-sites/patterns/components/section/index.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { preventWidows } from 'calypso/lib/formatting';
 
 import './style.scss';
 
@@ -20,13 +21,13 @@ export const PatternsSection = ( {
 	return (
 		<section
 			className={ classNames( 'patterns-section', {
-				'dark-theme': theme === 'dark',
-				'body-full-width': bodyFullWidth,
+				'patterns-section--dark': theme === 'dark',
+				'patterns-section--full-width-body': bodyFullWidth,
 			} ) }
 		>
 			<div className="patterns-section__header">
 				<h2>{ title }</h2>
-				<div className="patterns-section__header-description">{ description }</div>
+				<div className="patterns-section__header-description">{ preventWidows( description ) }</div>
 			</div>
 			<div className="patterns-section__body">{ children }</div>
 		</section>
