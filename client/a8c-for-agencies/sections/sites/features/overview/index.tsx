1ba4c7509717dfba684873e7bb314cf877be4f3c
cleacos <javacodia@gmail.com>
2024-04-10 12:35:00 +0100

    Update the A4A Overview family context to use Jetpack features and Hosting Overview

--- client/a8c-for-agencies/sections/sites/features/overview/index.tsx
+++ client/a8c-for-agencies/sections/sites/features/overview/index.tsx
@@ -1,6 +1,160 @@
+import { isEnabled } from '@automattic/calypso-config';
+import { useTranslate } from 'i18n-calypso';
+import React, { useCallback, useContext, useEffect, useMemo } from 'react';
+import SiteDetails from 'calypso/a8c-for-agencies/sections/sites/features/a4a/site-details';
+import {
+	JETPACK_ACTIVITY_ID,
+	JETPACK_BACKUP_ID,
+	JETPACK_BOOST_ID,
+	JETPACK_MONITOR_ID,
+	JETPACK_PLUGINS_ID,
+	JETPACK_SCAN_ID,
+	JETPACK_STATS_ID,
+	A4A_SITE_DETAILS_ID,
+	HOSTING_OVERVIEW_ID,
+} from 'calypso/a8c-for-agencies/sections/sites/features/features';
 import { PreviewPaneProps } from 'calypso/a8c-for-agencies/sections/sites/site-preview-pane/types';
-import { JetpackPreviewPane } from '../jetpack/jetpack-preview-pane';
+import SitesDashboardContext from 'calypso/a8c-for-agencies/sections/sites/sites-dashboard-context';
+import { useJetpackAgencyDashboardRecordTrackEvent } from 'calypso/jetpack-cloud/sections/agency-dashboard/hooks';
+import { A4A_SITES_DASHBOARD_DEFAULT_FEATURE } from '../../constants';
+import SitePreviewPane, { createFeaturePreview } from '../../site-preview-pane';
+import HostingOverviewPreview from '../hosting/overview';
+import { JetpackActivityPreview } from '../jetpack/activity';
+import { JetpackBackupPreview } from '../jetpack/backup';
+import { JetpackBoostPreview } from '../jetpack/jetpack-boost';
+import { JetpackMonitorPreview } from '../jetpack/jetpack-monitor';
+import { JetpackPluginsPreview } from '../jetpack/jetpack-plugins';
+import { JetpackStatsPreview } from '../jetpack/jetpack-stats';
+import { JetpackScanPreview } from '../jetpack/scan';
 
-export function OverviewFamily( props: PreviewPaneProps ) {
-	return <JetpackPreviewPane { ...props } />;
+import '../jetpack/style.scss';
+
+export function OverviewFamily( {
+	site,
+	closeSitePreviewPane,
+	className,
+	isSmallScreen = false,
+	hasError = false,
+}: PreviewPaneProps ) {
+	const translate = useTranslate();
+	const recordEvent = useJetpackAgencyDashboardRecordTrackEvent( [ site ], ! isSmallScreen );
+
+	const trackEvent = useCallback(
+		( eventName: string ) => {
+			recordEvent( eventName );
+		},
+		[ recordEvent ]
+	);
+
+	const { selectedSiteFeature, setSelectedSiteFeature } = useContext( SitesDashboardContext );
+
+	useEffect( () => {
+		if ( selectedSiteFeature === undefined ) {
+			setSelectedSiteFeature( A4A_SITES_DASHBOARD_DEFAULT_FEATURE );
+		}
+		return () => {
+			setSelectedSiteFeature( undefined );
+		};
+	}, [] );
+
+	// Jetpack features: Boost, Backup, Monitor, Stats
+	const features = useMemo(
+		() => [
+			createFeaturePreview(
+				JETPACK_BOOST_ID,
+				'Boost',
+				true,
+				selectedSiteFeature,
+				setSelectedSiteFeature,
+				<JetpackBoostPreview site={ site } trackEvent={ trackEvent } hasError={ hasError } />
+			),
+			createFeaturePreview(
+				JETPACK_BACKUP_ID,
+				'Backup',
+				true,
+				selectedSiteFeature,
+				setSelectedSiteFeature,
+				<JetpackBackupPreview site={ site } />
+			),
+			createFeaturePreview(
+				JETPACK_SCAN_ID,
+				'Scan',
+				true,
+				selectedSiteFeature,
+				setSelectedSiteFeature,
+				<JetpackScanPreview site={ site } />
+			),
+			createFeaturePreview(
+				JETPACK_MONITOR_ID,
+				'Monitor',
+				true,
+				selectedSiteFeature,
+				setSelectedSiteFeature,
+				<JetpackMonitorPreview site={ site } trackEvent={ trackEvent } hasError={ hasError } />
+			),
+			createFeaturePreview(
+				JETPACK_PLUGINS_ID,
+				translate( 'Plugins' ),
+				true,
+				selectedSiteFeature,
+				setSelectedSiteFeature,
+				<JetpackPluginsPreview
+					link={ site.url_with_scheme + '/wp-admin/plugins.php' }
+					linkLabel={ translate( 'Manage Plugins in wp-admin' ) }
+					featureText={ translate( 'Manage all plugins installed on %(siteUrl)s', {
+						args: { siteUrl: site.url },
+					} ) }
+					captionText={ translate(
+						"Note: We are currently working to make this section function from the Automattic for Agencies dashboard. In the meantime, you'll be taken to WP-Admin."
+					) }
+				/>
+			),
+			createFeaturePreview(
+				JETPACK_STATS_ID,
+				'Stats',
+				true,
+				selectedSiteFeature,
+				setSelectedSiteFeature,
+				<JetpackStatsPreview site={ site } trackEvent={ trackEvent } />
+			),
+			createFeaturePreview(
+				JETPACK_ACTIVITY_ID,
+				translate( 'Activity' ),
+				true,
+				selectedSiteFeature,
+				setSelectedSiteFeature,
+				<JetpackActivityPreview site={ site } />
+			),
+			createFeaturePreview(
+				HOSTING_OVERVIEW_ID,
+				translate( 'Hosting' ),
+				true,
+				selectedSiteFeature,
+				setSelectedSiteFeature,
+				<HostingOverviewPreview site={ site } />
+			),
+			...( isEnabled( 'a4a/site-details-pane' )
+				? [
+						createFeaturePreview(
+							A4A_SITE_DETAILS_ID,
+							translate( 'Details' ),
+							true,
+							selectedSiteFeature,
+							setSelectedSiteFeature,
+							<SiteDetails site={ site } />
+						),
+				  ]
+				: [] ),
+		],
+		[ selectedSiteFeature, setSelectedSiteFeature, site, trackEvent, hasError, translate ]
+	);
+
+	return (
+		<SitePreviewPane
+			site={ site }
+			closeSitePreviewPane={ closeSitePreviewPane }
+			features={ features }
+			className={ className }
+		/>
+	);
 }
