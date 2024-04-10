7a7b1d70ccec5b559f070ed40a32628f8fd09c31
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-04-10 17:28:41 +0200

    Remove unused

--- client/hosting-overview/components/active-domains-card.tsx
+++ client/hosting-overview/components/active-domains-card.tsx
@@ -1,15 +1,15 @@
 import { Button, Card } from '@automattic/components';
 import { useSiteDomainsQuery } from '@automattic/data-stores';
-import { DomainsTable, ResponseDomain, useDomainsTable } from '@automattic/domains-table';
+import { DomainsTable } from '@automattic/domains-table';
 import { useTranslate } from 'i18n-calypso';
 import { FC } from 'react';
 import { fetchSiteDomains } from 'calypso/my-sites/domains/domain-management/domains-table-fetch-functions';
-import { useSelector, useDispatch } from 'calypso/state';
+import { useSelector } from 'calypso/state';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 const ActiveDomainsCard: FC = () => {
 	const site = useSelector( getSelectedSite );
-	const { data, isLoading, refetch } = useSiteDomainsQuery( site?.ID, {
+	const { data, isLoading } = useSiteDomainsQuery( site?.ID, {
 		queryFn: () => fetchSiteDomains( site?.ID ),
 	} );
 	const translate = useTranslate();
@@ -36,17 +36,6 @@ const ActiveDomainsCard: FC = () => {
 				domains={ data?.domains }
 				isAllSitesView={ false }
 				siteSlug={ site?.slug ?? null }
-				// domainStatusPurchaseActions={ purchaseActions }
-				// userCanSetPrimaryDomains={ userCanSetPrimaryDomains }
-				// currentUserCanBulkUpdateContactInfo={ ! isInSupportSession }
-				// onDomainAction={ ( action, domain ) => { } }
-				// footer={ null }
-				// fetchAllDomains={ fetchAllDomains }
-				// fetchSite={ fetchSite }
-				// fetchSiteDomains={ fetchSiteDomains }
-				// createBulkAction={ createBulkAction }
-				// fetchBulkActionStatus={ fetchBulkActionStatus }
-				// deleteBulkActionStatus={ deleteBulkActionStatus }
 			/>
 		</Card>
 	);
