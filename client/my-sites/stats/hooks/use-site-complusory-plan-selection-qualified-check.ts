784c98da932de7ed9a257a470eaabe7c029d11b0
Jasper Kang <kangzjnet@gmail.com>
2024-04-10 16:45:55 +1200

    find max view per month for the last 3 months + current month

--- client/my-sites/stats/hooks/use-site-complusory-plan-selection-qualified-check.ts
+++ client/my-sites/stats/hooks/use-site-complusory-plan-selection-qualified-check.ts
@@ -2,21 +2,23 @@ import { useSelector } from 'calypso/state';
 import { getSiteOption } from 'calypso/state/sites/selectors';
 import usePlanUsageQuery from './use-plan-usage-query';
 
-const MIN_VIEWS_TO_APPLY_PAYWALL = 1000;
+const MIN_MONTHLY_VIEWS_TO_APPLY_PAYWALL = 1000;
 
 export default function useSiteComplusoryPlanSelectionQualifiedCheck( siteId: number | null ) {
 	const siteCreatedTimeStamp = useSelector( ( state ) =>
 		getSiteOption( state, siteId, 'created_at' )
 	) as string;
 	const { isPending, data: usageInfo } = usePlanUsageQuery( siteId );
-	const recentViews = Math.max(
+	const recentMonthlyViews = Math.max(
 		usageInfo?.recent_usages[ 0 ]?.views_count ?? 0,
+		usageInfo?.recent_usages[ 1 ]?.views_count ?? 0,
+		usageInfo?.recent_usages[ 2 ]?.views_count ?? 0,
 		usageInfo?.current_usage?.views_count ?? 0
 	);
 	const isNewSite =
 		siteCreatedTimeStamp && new Date( siteCreatedTimeStamp ) > new Date( '2024-01-31' ); // Targeting new sites
 	const isExistingSampleSite =
-		siteId && siteId % 100 < 50 && recentViews > MIN_VIEWS_TO_APPLY_PAYWALL; // Targeting 50% of existing sites with views higher than 1000/mth.
+		siteId && siteId % 100 < 50 && recentMonthlyViews > MIN_MONTHLY_VIEWS_TO_APPLY_PAYWALL; // Targeting 50% of existing sites with views higher than 1000/mth.
 
 	return {
 		isNewSite,
