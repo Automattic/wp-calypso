67031539eda69199770deb1fadedf52e9025e7b6
Anthony Grullon <anthony.grullon@automattic.com>
2024-04-10 08:28:08 -0400

    Site Migration: Add 3 testimonials to upgrade plan screen for english locale (#89362)

--- client/blocks/importer/wordpress/upgrade-plan/constants.tsx
+++ client/blocks/importer/wordpress/upgrade-plan/constants.tsx
@@ -1,44 +1,33 @@
 import { shield, trendingUp, chartBar } from '@wordpress/icons';
 import { translate } from 'i18n-calypso';
+import customerImageAjitBohra from 'calypso/assets/images/migrations/customer-testimonials/ajit-bohra.jpg';
+import customerImageChrisCoyier from 'calypso/assets/images/migrations/customer-testimonials/chris-coyier.jpg';
+import customerImageEmmaLucasCopley from 'calypso/assets/images/migrations/customer-testimonials/emma-lucas-copley.jpg';
 
 export const UpgradePlanHostingTestimonials = [
 	{
-		customerImage: null,
-		customerName: 'WP Hosting Benchmarks',
-		customerTestimonial: translate(
-			`Perfect uptime on both monitors. WordPress.com also managed to pickup the fastest WP Bench score and an A+ SSL grade. It’s easy to say WordPress.com handled these tests without blinking.`
-		),
-	},
-	{
-		customerImage: null,
-		customerName: 'Tommaso Pirola',
-		customerTestimonial: translate(
-			`"So we actually migrated to WordPress.com paying 15 euros a month [at that time]. So, from 450 to 15. In website speed and reliability and uptime, we never had a problem ever since."`
-		),
-		customerInfo: translate( `Longterm customer recounting switch from AWS` ),
-	},
-	{
-		customerImage: null,
-		customerName: 'Deepak Kumar',
-		customerTestimonial: translate(
-			`"Deepak’s website, currently garnering over 2.9 million monthly pageviews, is efficiently handled by WordPress.com. Deepak is especially pleased with the platform’s capability to manage high-traffic volumes seamlessly."`
-		),
-		customerInfo: translate( `Creator plan customer case study` ),
-	},
-	{
-		customerImage: null,
-		customerName: 'Antony Angel',
+		customerImage: customerImageAjitBohra,
+		customerName: 'Ajit Bohra',
 		customerTestimonial: translate(
 			`"We aimed to provide clients with a reliable hosting service we could endorse without hesitation, ultimately resulting in satisfied clients. We found that service with WordPress.com."`
 		),
-		customerInfo: 'AntonyAgnel.com',
+		customerInfo: 'WordPress.com News',
 	},
 	{
-		customerImage: null,
+		customerImage: customerImageChrisCoyier,
 		customerName: 'Chris Coyier',
 		customerTestimonial: translate(
 			`"With WordPress.com, you’re in good hands. Your site has the speed it needs. It won’t go offline. And it’s not going to be hacked. When it comes to the difficult stuff — it’s all taken care of."`
 		),
+		customerInfo: 'chriscoyier.net',
+	},
+	{
+		customerImage: customerImageEmmaLucasCopley,
+		customerName: 'Emma Lucas-Copley',
+		customerTestimonial: translate(
+			`"7 years ago I decided to FINALLY begin my book blog, and I opened my Wordpress account after reading so many great reviews. I've never looked back. With their easy to use website features, fantastic support team and great collection of themes to choose from, my website was simple to build, update and maintain."`
+		),
+		customerInfo: 'papyrusandpeppermint.com',
 	},
 ];
 
