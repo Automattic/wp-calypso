37ce75a526e00b466d3a17f5678a8fd45b062882
Dean Sas <dean.sas@automattic.com>
2024-04-10 09:33:29 +0100

    Command Palette: Add a switch site command (#88913)
    This command makes it faster for users with many sites to move between them, and allow them to repeat actions across several sites without leaving the keyboard.
    
    ---------
    
    Co-authored-by: mmtr <1233880+mmtr@users.noreply.github.com>

--- client/switch-site/controller.tsx
+++ client/switch-site/controller.tsx
@@ -2,6 +2,6 @@ import { SiteSwitch } from './components/site-switch';
 import type { Context as PageJSContext } from '@automattic/calypso-router';
 
 export default function siteSwitcher( context: PageJSContext, next: () => void ) {
-	context.primary = <SiteSwitch redirectTo={ context.query.route.trim() } />;
+	context.primary = <SiteSwitch redirectTo={ context.query.route?.trim() ?? '' } />;
 	next();
 }
