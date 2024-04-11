0eaa0c6a930e184b9116aea863a0126d0381c749
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2024-04-11 16:05:14 +0200

    Plan price in place

--- client/hosting-overview/components/plan-card.tsx
+++ client/hosting-overview/components/plan-card.tsx
@@ -1,19 +1,39 @@
-import { Button, Card } from '@automattic/components';
+import { PLAN_PREMIUM, PlanSlug } from '@automattic/calypso-products';
+import { Button, Card, PlanPrice } from '@automattic/components';
+import { usePricingMetaForGridPlans } from '@automattic/data-stores/src/plans';
+import { formatCurrency } from '@automattic/format-currency';
 import { useTranslate } from 'i18n-calypso';
 import { FC } from 'react';
 import { useSelector } from 'react-redux';
+import QuerySitePlans from 'calypso/components/data/query-site-plans';
+import { useLocalizedMoment } from 'calypso/components/localized-moment';
+import useCheckPlanAvailabilityForPurchase from 'calypso/my-sites/plans-features-main/hooks/use-check-plan-availability-for-purchase';
+import { getCurrentPlan } from 'calypso/state/sites/plans/selectors';
 import { getSelectedSite } from 'calypso/state/ui/selectors';
 
 const PlanCard: FC = () => {
+	const moment = useLocalizedMoment();
 	const site = useSelector( getSelectedSite );
-	const planName = site?.plan?.product_name_short ?? '';
-
+	const sitePlan = site?.plan;
+	const planName = sitePlan?.product_name_short ?? '';
+	const planSlug = ( sitePlan?.product_slug || '' ) as PlanSlug;
+	const plan = useSelector( ( state ) => getCurrentPlan( state, site?.ID ) );
+	const pricing = usePricingMetaForGridPlans( {
+		coupon: undefined,
+		planSlugs: [ planSlug ],
+		selectedSiteId: site?.ID,
+		storageAddOns: null,
+		useCheckPlanAvailabilityForPurchase,
+	} );
 	const translate = useTranslate();
 
 	console.debug( 'site', site );
-	console.debug( 'planName', planName );
+	console.debug( 'plan', plan );
+	console.debug( 'pricingMeta', pricing );
+
 	return (
 		<>
+			<QuerySitePlans siteId={ site?.ID } />
 			<Card className="hosting-overview__card">
 				<div>
 					<div className="hosting-overview__plan-card-header">
@@ -27,7 +47,33 @@ const PlanCard: FC = () => {
 							{ translate( 'Manage plan' ) }
 						</Button>
 					</div>
-					<div>[Plan price goes here]</div>
+					<PlanPrice
+						className="hosting-overview__plan-price"
+						currencyCode={ plan?.currencyCode }
+						displayPerMonthNotation
+						isSmallestUnit
+						rawPrice={ pricing?.[ planSlug ].originalPrice.monthly }
+					/>
+					<div className="hosting-overview__plan-info">
+						{ translate( '{{span}}%(rawPrice)s{{/span}} billed annually.', {
+							args: {
+								rawPrice: formatCurrency(
+									pricing?.[ planSlug ].originalPrice.full ?? 0,
+									plan?.currencyCode ?? '',
+									{
+										stripZeros: true,
+										isSmallestUnit: true,
+									}
+								),
+							},
+							components: {
+								span: <span />,
+							},
+						} ) }
+					</div>
+					<div className="hosting-overview__plan-info">
+						{ translate( 'Expires on %s.', { args: moment( plan?.expiryDate ).format( 'LL' ) } ) }
+					</div>
 				</div>
 			</Card>
 		</>
