0d54e1b4880573269d918309501b9f022b1028ff
Daniel <daniel.lopez@automattic.com>
2024-04-11 12:50:27 +0200

    [Odie] Add a new fallback for wp-admin to check Wapuu's aviability (#89038)

--- packages/help-center/src/hooks/use-still-need-help-url.tsx
+++ packages/help-center/src/hooks/use-still-need-help-url.tsx
@@ -3,9 +3,20 @@
 import { useSupportAvailability } from '../data/use-support-availability';
 import { useIsWapuuEnabled } from './use-is-wapuu-enabled';
 
+/**
+ * This function is used as a fallback for users navigating through wp-admin.
+ * It checks if the 'flags' URL parameter is set to 'wapuu' when useIsWapuuEnabled() fails to do so.
+ * @returns boolean
+ */
+export function isWapuuFlagSetInURL(): boolean {
+	const currentUrl = window.location.href;
+	const urlParams = new URLSearchParams( new URL( currentUrl ).search );
+	return urlParams.get( 'flags' ) === 'wapuu';
+}
+
 export function useStillNeedHelpURL() {
 	const { data: supportAvailability, isLoading } = useSupportAvailability( 'OTHER' );
-	const isWapuuEnabled = useIsWapuuEnabled();
+	const isWapuuEnabled = useIsWapuuEnabled() || isWapuuFlagSetInURL();
 	const isFreeUser = ! supportAvailability?.is_user_eligible_for_tickets;
 
 	if ( ! isFreeUser ) {
