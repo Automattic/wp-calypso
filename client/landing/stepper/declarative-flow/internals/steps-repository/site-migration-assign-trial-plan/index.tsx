3d0ad6669c1f8df5e22bfb291699b055f8c667f6
Caroline Moore <calobee@gmail.com>
2024-04-11 11:51:49 -0400

    Remove rendered layout

--- client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-assign-trial-plan/index.tsx
+++ client/landing/stepper/declarative-flow/internals/steps-repository/site-migration-assign-trial-plan/index.tsx
@@ -1,23 +1,15 @@
 import { PLAN_MIGRATION_TRIAL_MONTHLY } from '@automattic/calypso-products';
-import { StepContainer, Title } from '@automattic/onboarding';
-import { useI18n } from '@wordpress/react-i18n';
 import { useEffect } from 'react';
-import DocumentHead from 'calypso/components/data/document-head';
-import { LoadingEllipsis } from 'calypso/components/loading-ellipsis';
 import useAddHostingTrialMutation, {
 	HOSTING_INTENT_MIGRATE,
 } from 'calypso/data/hosting/use-add-hosting-trial-mutation';
 import { useSite } from 'calypso/landing/stepper/hooks/use-site';
-import { recordTracksEvent } from 'calypso/lib/analytics/tracks';
 import { useDispatch } from 'calypso/state';
 import { requestSite } from 'calypso/state/sites/actions';
-import type { Step } from '../../types';
-
-import './style.scss';
+import { Step } from '../../types';
 
 const SiteMigrationAssignTrialPlanStep: Step = ( { navigation } ) => {
 	const { submit } = navigation;
-	const { __ } = useI18n();
 	const site = useSite();
 
 	const dispatch = useDispatch();
@@ -28,7 +20,7 @@ const SiteMigrationAssignTrialPlanStep: Step = ( { navigation } ) => {
 			submit?.();
 		},
 		onError: () => {
-			// If the trial fails to be added, submit with error dependency.
+			// If adding the trial fails, submit with error dependency.
 			submit?.( { error: isError } );
 		},
 	} );
@@ -44,29 +36,7 @@ const SiteMigrationAssignTrialPlanStep: Step = ( { navigation } ) => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [] );
 
-	return (
-		<>
-			<DocumentHead title={ __( 'Adding your free trial' ) } />
-			<StepContainer
-				shouldHideNavButtons={ true }
-				hideFormattedHeader={ true }
-				stepName="site-migration-assign-trial-plan"
-				isHorizontalLayout={ true }
-				recordTracksEvent={ recordTracksEvent }
-				stepContent={
-					<>
-						<div className="site-migration-assign-trial-plan__center">
-							<div className="site-migration-assign-trial-plan__header site-migration-assign-trial-plan__loading">
-								<Title>{ __( 'Adding your free trial' ) }</Title>
-								<LoadingEllipsis />
-							</div>
-						</div>
-					</>
-				}
-				showFooterWooCommercePowered={ false }
-			/>
-		</>
-	);
+	return null;
 };
 
 export default SiteMigrationAssignTrialPlanStep;
