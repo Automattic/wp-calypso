9747501b97d31b30cccb87a32b9bdcac567ae767
Ivan Ottinger <25105483+ivan-ottinger@users.noreply.github.com>
2024-04-12 16:54:38 +0200

    Refactor `checked` calculation

--- client/components/survey-container/components/question-single-selection.tsx
+++ client/components/survey-container/components/question-single-selection.tsx
@@ -5,31 +5,34 @@ import { QuestionSelectionType } from './question-step';
 const QuestionSingleSelection = ( { onChange, question, value }: QuestionSelectionType ) => {
 	return (
 		<div className="question-options__container">
-			{ question.options.map( ( option, index ) => (
-				<div
-					key={ index }
-					className={ `question-options__option-control components-radio-control__option ${
-						value.includes( option.value ) ? 'checked' : ''
-					}` }
-					onClick={ () => onChange( question.key, [ option.value ] ) }
-				>
-					<input
-						type="radio"
-						id={ `option-${ option.value }` }
-						name={ question.key }
-						value={ option.value }
-						onChange={ () => onChange( question.key, [ option.value ] ) }
-						checked={ value.includes( option.value ) }
-						className="form-radio"
-					/>
-					<div className="question-options__option-label">
-						<label htmlFor={ `option-${ option.value }` }>{ option.label }</label>
-						{ option.helpText && (
-							<span className="question-options__option-help-text">{ option.helpText }</span>
-						) }
+			{ question.options.map( ( option, index ) => {
+				const isSelected = value.includes( option.value );
+				return (
+					<div
+						key={ index }
+						className={ `question-options__option-control components-radio-control__option ${
+							isSelected ? 'checked' : ''
+						}` }
+						onClick={ () => onChange( question.key, [ option.value ] ) }
+					>
+						<input
+							type="radio"
+							id={ `option-${ option.value }` }
+							name={ question.key }
+							value={ option.value }
+							onChange={ () => onChange( question.key, [ option.value ] ) }
+							checked={ isSelected }
+							className="form-radio"
+						/>
+						<div className="question-options__option-label">
+							<label htmlFor={ `option-${ option.value }` }>{ option.label }</label>
+							{ option.helpText && (
+								<span className="question-options__option-help-text">{ option.helpText }</span>
+							) }
+						</div>
 					</div>
-				</div>
-			) ) }
+				);
+			} ) }
 		</div>
 	);
 };
