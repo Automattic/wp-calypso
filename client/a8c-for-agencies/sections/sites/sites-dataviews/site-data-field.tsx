424a4e0f9d4d41cbe2bf3a7db75a4d1c38e74b6e
Karen Attfield <karenlattfield@gmail.com>
2024-04-12 16:31:27 +0100

    Making sure the site name and URL field in the sites table clearly shows if a site is unreachable

--- client/a8c-for-agencies/sections/sites/sites-dataviews/site-data-field.tsx
+++ client/a8c-for-agencies/sections/sites/sites-dataviews/site-data-field.tsx
@@ -1,4 +1,6 @@
 import { Button } from '@automattic/components';
+import { useTranslate } from 'i18n-calypso';
+import useFetchTestConnection from 'calypso/data/agency-dashboard/use-fetch-test-connection';
 import TextPlaceholder from 'calypso/jetpack-cloud/sections/partner-portal/text-placeholder';
 import SiteFavicon from '../site-favicon';
 import { Site } from '../types';
@@ -10,14 +12,31 @@ interface SiteDataFieldProps {
 }
 
 const SiteDataField = ( { isLoading, site, onSiteTitleClick }: SiteDataFieldProps ) => {
+	const translate = useTranslate();
+	const blogId = site.blog_id;
+	const isConnectionHealthy = site.is_connection_healthy;
+	const { data } = useFetchTestConnection( true, isConnectionHealthy, blogId );
+	const isSiteConnected = data?.connected ?? true;
+
 	if ( isLoading ) {
 		return <TextPlaceholder />;
 	}
 
 	return (
-		<Button className="sites-dataviews__site" onClick={ () => onSiteTitleClick( site ) } borderless>
+		<Button
+			className={
+				isSiteConnected ? 'sites-dataviews__site' : 'sites-dataviews__site is-disconnected'
+			}
+			onClick={ isSiteConnected ? () => onSiteTitleClick( site ) : undefined }
+			borderless
+		>
 			<SiteFavicon site={ site } />
 			<div className="sites-dataviews__site-name">
+				{ isSiteConnected ? null : (
+					<div className="sites-dataviews__site-disconnected">
+						{ translate( 'Site unreachable' ) }
+					</div>
+				) }
 				{ site.blogname }
 				<div className="sites-dataviews__site-url">{ site.url }</div>
 			</div>
