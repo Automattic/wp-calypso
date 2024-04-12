c0c66ec0cc07f211fb439a79357cb7a391038c69
Anthony Grullon <agrullon95@gmail.com>
2024-04-12 12:45:09 -0400

    Add skeleton for identifying host

--- client/blocks/importer/wordpress/upgrade-plan/upgrade-plan-hosting-details.tsx
+++ client/blocks/importer/wordpress/upgrade-plan/upgrade-plan-hosting-details.tsx
@@ -2,72 +2,85 @@ import { useIsEnglishLocale } from '@automattic/i18n-utils';
 import { useState } from '@wordpress/element';
 import { hasTranslation } from '@wordpress/i18n';
 import { Icon } from '@wordpress/icons';
-import { useI18n } from '@wordpress/react-i18n';
+import { useTranslate } from 'i18n-calypso';
+import { useHostingProviderQuery } from 'calypso/data/site-profiler/use-hosting-provider-query';
 import { UpgradePlanHostingDetailsList, UpgradePlanHostingTestimonials } from './constants';
 import { UpgradePlanHostingDetailsTooltip } from './upgrade-plan-hosting-details-tooltip';
 
 export const UpgradePlanHostingDetails = () => {
-	const { __ } = useI18n();
+	const translate = useTranslate();
 	const isEnglishLocale = useIsEnglishLocale();
 	const [ activeTooltipId, setActiveTooltipId ] = useState( '' );
 
 	const headerMainText =
 		hasTranslation( 'Why should you host with us?' ) || isEnglishLocale
-			? __( 'Why should you host with us?' )
-			: __( 'Why you should host with us?' );
+			? translate( 'Why should you host with us?' )
+			: translate( 'Why you should host with us?' );
+
+	const { data: hostingProviderData } = useHostingProviderQuery( 'wenxinwendyju.com', true );
+	const hostingProviderName = hostingProviderData?.hosting_provider?.name;
 
 	return (
 		<div className="import__upgrade-plan-hosting-details">
-			<div className="import__upgrade-plan-hosting-details-header">
-				<p className="import__upgrade-plan-hosting-details-header-main">{ headerMainText }</p>
-				<p className="import__upgrade-plan-hosting-details-header-subtext">
-					{ __( 'Check our performance, compared to the average WordPress host' ) }
-				</p>
-			</div>
-			<div className="import__upgrade-plan-hosting-details-list">
-				<ul>
-					{ UpgradePlanHostingDetailsList.map( ( { title, description, icon }, i ) => (
-						<li key={ i }>
-							<Icon
-								className="import__upgrade-plan-hosting-details-list-icon"
-								icon={ icon }
-								size={ 24 }
-							/>
-							<div className="import__upgrade-plan-hosting-details-list-stats">
-								<p className="import__upgrade-plan-hosting-details-list-stats-title">{ title }</p>
-								<span className="import__upgrade-plan-hosting-details-list-stats-description">
-									{ description }
-								</span>
-							</div>
-						</li>
-					) ) }
-				</ul>
-			</div>
-			{ isEnglishLocale && (
-				<div className="import__upgrade-plan-hosting-details-testimonials-container">
-					<p>{ __( '100% loved by our best customers' ) }</p>
-					<div className="import__upgrade-plan-hosting-details-testimonials">
-						{ UpgradePlanHostingTestimonials.map(
-							( { customerName, customerTestimonial, customerInfo, customerImage }, i ) => (
-								<UpgradePlanHostingDetailsTooltip
-									key={ i }
-									id={ `testimonial-${ i }` }
-									setActiveTooltipId={ setActiveTooltipId }
-									activeTooltipId={ activeTooltipId }
-									customerName={ customerName }
-									customerInfo={ customerInfo }
-									customerTestimonial={ customerTestimonial }
-									hideArrow={ false }
-								>
-									<img
-										className="import__upgrade-plan-hosting-details-testimonials-image"
-										src={ customerImage }
-										alt={ customerName }
-									/>
-								</UpgradePlanHostingDetailsTooltip>
-							)
-						) }
+			<div className="import__upgrade-plan-hosting-details-card-container">
+				<div className="import__upgrade-plan-hosting-details-header">
+					<p className="import__upgrade-plan-hosting-details-header-main">{ headerMainText }</p>
+					<p className="import__upgrade-plan-hosting-details-header-subtext">
+						{ translate( 'Check our performance, compared to the average WordPress host' ) }
+					</p>
+				</div>
+				<div className="import__upgrade-plan-hosting-details-list">
+					<ul>
+						{ UpgradePlanHostingDetailsList.map( ( { title, description, icon }, i ) => (
+							<li key={ i }>
+								<Icon
+									className="import__upgrade-plan-hosting-details-list-icon"
+									icon={ icon }
+									size={ 24 }
+								/>
+								<div className="import__upgrade-plan-hosting-details-list-stats">
+									<p className="import__upgrade-plan-hosting-details-list-stats-title">{ title }</p>
+									<span className="import__upgrade-plan-hosting-details-list-stats-description">
+										{ description }
+									</span>
+								</div>
+							</li>
+						) ) }
+					</ul>
+				</div>
+				{ isEnglishLocale && (
+					<div className="import__upgrade-plan-hosting-details-testimonials-container">
+						<p>{ translate( '100% loved by our best customers' ) }</p>
+						<div className="import__upgrade-plan-hosting-details-testimonials">
+							{ UpgradePlanHostingTestimonials.map(
+								( { customerName, customerTestimonial, customerInfo, customerImage }, i ) => (
+									<UpgradePlanHostingDetailsTooltip
+										key={ i }
+										id={ `testimonial-${ i }` }
+										setActiveTooltipId={ setActiveTooltipId }
+										activeTooltipId={ activeTooltipId }
+										customerName={ customerName }
+										customerInfo={ customerInfo }
+										customerTestimonial={ customerTestimonial }
+										hideArrow={ false }
+									>
+										<img
+											className="import__upgrade-plan-hosting-details-testimonials-image"
+											src={ customerImage }
+											alt={ customerName }
+										/>
+									</UpgradePlanHostingDetailsTooltip>
+								)
+							) }
+						</div>
 					</div>
+				) }
+			</div>
+			{ hostingProviderName && hostingProviderName !== 'Unknown' && (
+				<div className="import__upgrade-plan-hosting-details-identified-host">
+					{ translate( "We've identified %(hostingProviderName)s as your host.", {
+						args: { hostingProviderName },
+					} ) }
 				</div>
 			) }
 		</div>
